e0f8a75942fef6db1725ccb9a55ce6f6e320b592
shrikant@chromium.org <shrikant@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-30 00:53:30 +0000

    This change modifies behavior of crash dialog in case of aura metro. If metro viewer instance was connected before crash then we won't show crash dialog.
    BUG=273185
    R=cpu,ananta
    TBR=sky
    TESTS:Please see comment#2 in bug description and check if you see restart dialog when crashed from metro mode.
    
    Review URL: https://chromiumcodereview.appspot.com/23405002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220460 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/env_vars.h
+++ chrome/common/env_vars.h
@@ -12,6 +12,7 @@ namespace env_vars {
 
 extern const char kHeadless[];
 extern const char kLogFileName[];
+extern const char kMetroConnected[];
 extern const char kSessionLogDir[];
 extern const char kShowRestart[];
 extern const char kRestartInfo[];
