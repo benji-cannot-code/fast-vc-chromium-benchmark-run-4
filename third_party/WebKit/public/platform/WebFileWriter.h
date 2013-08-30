bd8d5645c6426c8d6065ac0d6d919316e7b76e77
michaeln@chromium.org <michaeln@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-30 22:48:50 +0000

    Couple of minor changes to the WebKitAPI. This is a small step in a larger work-in-progress. See the bug for details.
    * Distinguish more clearly between blobURLs and fileSystemURLs in WebBlobData and the very similar WebHTTPBody element structs.
    
    * Put in place some of the new API to identify blob data by uuid instead of blob instances by URL. What's in place enough for Blink to call out to Chromium using the new id scheme. What's not in place is enough for Chrome to call into Blink using the new scheme as Blink doesn't know how to do that yet. Blink doesn't use the new API yet but Chromium is nearly ready for it to start doing so after https://codereview.chromium.org/23223003/ lands.
    
    BUG=174200
    
    Review URL: https://chromiumcodereview.appspot.com/23703003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157024 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebFileWriter.h
+++ third_party/WebKit/public/platform/WebFileWriter.h
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebKit {
 
+class WebString;
 class WebURL;
 
 class WebFileWriter {
@@ -45,7 +46,8 @@ public:
 
     // Only one write or one truncate operation can be in progress at a time.
     // These functions are asynchronous and will report results through the WebFileWriter's associated WebFileWriterClient.
-    virtual void write(long long position, const WebURL& blobURL) = 0;
+    virtual void write(long long position, const WebURL& blobURL) { } // DEPRECATED
+    virtual void write(long long position, const WebString& blobUUID) { }
     virtual void truncate(long long length) = 0;
 
     // Cancel will attempt to abort a running write or truncate. However, it may not be possible to cancel an in-progress action, or the call may have come in too late. Partial writes are possible.
