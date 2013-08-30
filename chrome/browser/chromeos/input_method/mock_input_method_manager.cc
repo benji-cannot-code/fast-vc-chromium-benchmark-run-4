48a71eebe5fa2d2c9b49c9e0c25398af05fd326c
alemate@chromium.org <alemate@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-30 02:17:54 +0000

    Default keyboard layout should be set considering owners preference.
    BUG=chromium:280383
    
    Review URL: https://chromiumcodereview.appspot.com/23608003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220482 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/input_method/mock_input_method_manager.cc
+++ chrome/browser/chromeos/input_method/mock_input_method_manager.cc
@@ -114,6 +114,9 @@ void MockInputMethodManager::SetEnabledExtensionImes(
     std::vector<std::string>* ids) {
 }
 
+void MockInputMethodManager::SetInputMethodDefault() {
+}
+
 bool MockInputMethodManager::SwitchToNextInputMethod() {
   return true;
 }
