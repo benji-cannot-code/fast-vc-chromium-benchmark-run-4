d066565dab5b9068c07d348bf08e59f197d3d9e3
pdr@chromium.org <pdr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-30 02:35:53 +0000

    Add ImageObserverDisabler for disabling the image observer in SVG images
    This change adds a new class for disabling the image observer in
    SVGImage.cpp which is used for hiding the fact that we re-use a single
    SVG document in multiple instances. This is a cleanup in prep for a
    larger patch in crbug.com/279445.
    
    Also in this patch is a cleanup making SVGImage::frameView() private
    to prevent well meaning coders from accidentally leaking the SVG
    document's FrameView.
    
    R=esprehn
    BUG=279445
    
    Review URL: https://chromiumcodereview.appspot.com/23490014
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156951 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/graphics/SVGImage.h
+++ third_party/WebKit/Source/core/svg/graphics/SVGImage.h
@@ -50,7 +50,6 @@ public:
     static bool isInSVGImage(const Element*);
 
     RenderBox* embeddedContentBox() const;
-    FrameView* frameView() const;
 
     virtual bool isSVGImage() const OVERRIDE { return true; }
     virtual IntSize size() const OVERRIDE { return m_intrinsicSize; }
@@ -67,10 +66,14 @@ public:
     virtual PassRefPtr<NativeImageSkia> nativeImageForCurrentFrame() OVERRIDE;
 
 private:
+    friend class AccessibilityRenderObject;
     friend class SVGImageChromeClient;
     friend class SVGImageForContainer;
 
     virtual ~SVGImage();
+
+    // Returns the SVG image document's frame.
+    FrameView* frameView() const;
 
     virtual String filenameExtension() const OVERRIDE;
 
@@ -99,6 +102,27 @@ private:
     OwnPtr<Page> m_page;
     IntSize m_intrinsicSize;
 };
+
+class ImageObserverDisabler {
+    WTF_MAKE_NONCOPYABLE(ImageObserverDisabler);
+public:
+    ImageObserverDisabler(Image* image)
+        : m_image(image)
+    {
+        ASSERT(m_image->imageObserver());
+        m_observer = m_image->imageObserver();
+        m_image->setImageObserver(0);
+    }
+
+    ~ImageObserverDisabler()
+    {
+        m_image->setImageObserver(m_observer);
+    }
+private:
+    Image* m_image;
+    ImageObserver* m_observer;
+};
+
 }
 
 #endif // SVGImage_h
