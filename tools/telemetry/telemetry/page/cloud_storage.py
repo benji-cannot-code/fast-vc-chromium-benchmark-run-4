780b04954079cb9b117297af59bf387f1d17dcf3
chrisgao@chromium.org <chrisgao@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-30 03:30:49 +0000

    [telemetry] Add IEDriverServer to Google Storage
    BUG=266170
    TEST= del tools\telemetry\bin\IEDriverServer_32.exe && python tools\perf\run_measurement --browser=internet-explorer octane
    
    Review URL: https://chromiumcodereview.appspot.com/23791002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220496 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/page/cloud_storage.py
+++ tools/telemetry/telemetry/page/cloud_storage.py
@@ -18,6 +18,7 @@ from telemetry.core import util
 
 
 DEFAULT_BUCKET = 'chromium-wpr'
+CHROMIUM_TELEMETRY_BUCKET = 'chromium-telemetry'
 
 
 _GSUTIL_URL = 'http://storage.googleapis.com/pub/gsutil.tar.gz'
