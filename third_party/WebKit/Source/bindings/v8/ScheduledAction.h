d3a85f3a1f2a7ac90fb767dff16218ee9e268f2a
yusukesuzuki@chromium.org <yusukesuzuki@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-30 17:57:34 +0000

    Wrong header dependency of ScheduledAction
    ScheduledAction.h should include UnsafePersistent.h
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/23593004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157001 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/ScheduledAction.h
+++ third_party/WebKit/Source/bindings/v8/ScheduledAction.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "bindings/v8/ScopedPersistent.h"
 #include "bindings/v8/ScriptSourceCode.h"
+#include "bindings/v8/UnsafePersistent.h"
 #include <v8.h>
 #include "wtf/Forward.h"
 #include "wtf/Vector.h"
