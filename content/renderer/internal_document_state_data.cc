2814bd4c6b60f7e75b4b0cc80584f70e852d126d
blundell@chromium.org <blundell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-31 01:20:01 +0000

    Remove unneeded includes of PasswordForm in the content layer
    Follow-up to https://chromiumcodereview.appspot.com/19705013.
    
    BUG=263121
    R=jam
    
    Review URL: https://chromiumcodereview.appspot.com/23564006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220713 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/internal_document_state_data.cc
+++ content/renderer/internal_document_state_data.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/renderer/internal_document_state_data.h"
 
-#include "content/public/common/password_form.h"
 #include "content/public/renderer/document_state.h"
 #include "content/renderer/fetchers/alt_error_page_resource_fetcher.h"
 #include "third_party/WebKit/public/web/WebDataSource.h"
--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/renderer/content_renderer_client.h"
 #include "content/public/renderer/document_state.h"
 #include "content/public/renderer/navigation_state.h"
-#include "content/public/renderer/password_form_conversion_utils.h"
 #include "content/renderer/browser_plugin/browser_plugin.h"
 #include "content/renderer/browser_plugin/browser_plugin_manager.h"
 #include "content/renderer/internal_document_state_data.h"
