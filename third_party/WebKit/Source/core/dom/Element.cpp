c0a9dfe11d865a67c50c82f0cd64c44c52aff262
ch.dumez@sisa.samsung.com <ch.dumez@sisa.samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-31 22:37:16 +0000

    Have CustomElement::didEnterDocument() / didLeaveDocument() take a reference
    Have CustomElement::didEnterDocument() / didLeaveDocument() take a Document
    reference instead of a pointer since it cannot be NULL. It also avoids
    having the call sites convert the Document reference into a pointer.
    
    BUG=281400
    
    Review URL: https://chromiumcodereview.appspot.com/23523012
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157043 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Element.cpp
+++ third_party/WebKit/Source/core/dom/Element.cpp
@@ -1233,7 +1233,7 @@ Node::InsertionNotificationRequest Element::insertedInto(ContainerNode* insertio
         elementRareData()->clearClassListValueForQuirksMode();
 
     if (isUpgradedCustomElement() && inDocument())
-        CustomElement::didEnterDocument(this, &document());
+        CustomElement::didEnterDocument(this, document());
 
     TreeScope* scope = insertionPoint->treeScope();
     if (scope != treeScope())
@@ -1302,7 +1302,7 @@ void Element::removedFrom(ContainerNode* insertionPoint)
             document().accessSVGExtensions()->removeElementFromPendingResources(this);
 
         if (isUpgradedCustomElement())
-            CustomElement::didLeaveDocument(this, &insertionPoint->document());
+            CustomElement::didLeaveDocument(this, insertionPoint->document());
     }
 
     if (hasRareData())
