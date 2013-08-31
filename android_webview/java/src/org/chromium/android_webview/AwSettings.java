5f2cad303bf1933d89aa33ac272a4c29d9ae6968
mnaganov@chromium.org <mnaganov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-31 01:09:56 +0000

    [Android WebView] Wire up Blink's UseLegacyBackgroundSizeShorthandBehavior setting
    R=benm@chromium.org
    BUG=277157
    TEST=AwSettingsTest#testUseLegacyBackgroundSizeShorthandBehavior
    
    Review URL: https://chromiumcodereview.appspot.com/23483005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220710 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/java/src/org/chromium/android_webview/AwSettings.java
+++ android_webview/java/src/org/chromium/android_webview/AwSettings.java
@@ -83,7 +83,7 @@ public class AwSettings {
     private String mDefaultVideoPosterURL;
     private float mInitialPageScalePercent = 0;
 
-    private final boolean mSupportDeprecatedTargetDensityDPI;
+    private final boolean mSupportLegacyQuirks;
 
     private final boolean mPasswordEchoEnabled;
 
@@ -198,7 +198,7 @@ public class AwSettings {
             mPasswordEchoEnabled = Settings.System.getInt(context.getContentResolver(),
                     Settings.System.TEXT_SHOW_PASSWORD, 1) == 1;
 
-            mSupportDeprecatedTargetDensityDPI = supportsLegacyQuirks;
+            mSupportLegacyQuirks = supportsLegacyQuirks;
         }
         // Defer initializing the native side until a native WebContents instance is set.
     }
@@ -1037,8 +1037,8 @@ public class AwSettings {
     }
 
     @CalledByNative
-    private boolean getSupportDeprecatedTargetDensityDPILocked() {
-        return mSupportDeprecatedTargetDensityDPI;
+    private boolean getSupportLegacyQuirksLocked() {
+        return mSupportLegacyQuirks;
     }
 
     /**
