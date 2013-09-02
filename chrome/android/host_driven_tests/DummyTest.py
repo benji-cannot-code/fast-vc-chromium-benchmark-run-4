fdf7408330930d05dd1a18acdcc4ae95e511d2c8
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-02 00:24:02 +0000

    Revert 220750 "Add test_server_setup.py and add support for addi..."
    check_perms failing on linux cros bots:
    http://chromegw.corp.google.com/i/chromium.chromiumos/builders/Linux%20ChromiumOS%20Full/builds/24739
    http://chromegw.corp.google.com/i/chromium.chromiumos/builders/Linux%20ChromiumOS%20Full/builds/24739/steps/check_perms/logs/stdio
    
    
    
    > Add test_server_setup.py and add support for additional flags.
    > 
    > Whenever a host driven test need to setup a server as part of a test,
    > the command line needs of the application to be instrumented needs to
    > change.
    > 
    > This CL adds a helper class for setting up a test server, and also adds
    > support for tests to setup additional flags (typically the URL for how
    > to connect to the test server).
    > 
    > Depends on: https://codereview.chromium.org/23467004/
    > 
    > BUG=272584
    > NOTRY=true
    > 
    > Review URL: https://chromiumcodereview.appspot.com/23726004
    
    TBR=nyquist@chromium.org
    
    Review URL: https://codereview.chromium.org/23606011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220769 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/android/host_driven_tests/DummyTest.py
+++ chrome/android/host_driven_tests/DummyTest.py
@@ -3,11 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Host-driven java_tests which exercise dummy functionality.
-
-This test class is only here to ensure that the test framework for host driven
-tests work.
-"""
+import os
 
 from pylib.host_driven import test_case
 from pylib.host_driven import tests_annotations
