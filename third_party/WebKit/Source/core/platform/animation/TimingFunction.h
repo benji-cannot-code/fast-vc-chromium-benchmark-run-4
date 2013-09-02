afb5f3e2d19f892a91b3da61ed51e862107fcc39
steveblock@chromium.org <steveblock@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-02 01:52:42 +0000

    Cache UnitBezier in CubicBezierTimingFunction
    R=dstockwell
    
    Review URL: https://chromiumcodereview.appspot.com/23818002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157052 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/animation/TimingFunction.h
+++ third_party/WebKit/Source/core/platform/animation/TimingFunction.h
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define TimingFunction_h
 
 #include "core/platform/graphics/UnitBezier.h"
+#include "wtf/OwnPtr.h"
+#include "wtf/PassOwnPtr.h"
 #include "wtf/PassRefPtr.h"
 #include "wtf/RefCounted.h"
 #include <algorithm>
@@ -136,7 +138,9 @@ public:
 
     virtual double evaluate(double fraction, double accuracy) const
     {
-        return UnitBezier(m_x1, m_y1, m_x2, m_y2).solve(fraction, accuracy);
+        if (!m_bezier)
+            m_bezier = adoptPtr(new UnitBezier(m_x1, m_y1, m_x2, m_y2));
+        return m_bezier->solve(fraction, accuracy);
     }
 
     virtual bool operator==(const TimingFunction& other) const
@@ -174,6 +178,7 @@ private:
     double m_x2;
     double m_y2;
     TimingFunctionPreset m_timingFunctionPreset;
+    mutable OwnPtr<UnitBezier> m_bezier;
 };
 
 class StepsTimingFunction : public TimingFunction {
