1f9ebd46fed5316adcf5ce5e914e6f36b3597bea
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-02 21:33:55 +0000

    Remove unnecessary #ifdef's from managed user code.
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/23798003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220868 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/managed_mode/managed_user_service_unittest.cc
+++ chrome/browser/managed_mode/managed_user_service_unittest.cc
@@ -245,7 +245,6 @@ TEST_F(ManagedUserServiceExtensionTest, NoContentPacks) {
             url_filter->GetFilteringBehaviorForURL(url));
 }
 
-#if !defined(OS_CHROMEOS)
 TEST_F(ManagedUserServiceExtensionTest, InstallContentPacks) {
   ManagedUserService* managed_user_service =
       ManagedUserServiceFactory::GetForProfile(profile_.get());
@@ -302,10 +301,8 @@ TEST_F(ManagedUserServiceExtensionTest, InstallContentPacks) {
   EXPECT_EQ(ASCIIToUTF16("Homestar Runner"), sites[1].name);
   EXPECT_EQ(string16(), sites[2].name);
 
-#if defined(ENABLE_CONFIGURATION_POLICY)
   EXPECT_EQ(ManagedModeURLFilter::ALLOW,
             url_filter->GetFilteringBehaviorForURL(example_url));
-#endif
   EXPECT_EQ(ManagedModeURLFilter::WARN,
             url_filter->GetFilteringBehaviorForURL(moose_url));
 
@@ -333,12 +330,10 @@ TEST_F(ManagedUserServiceExtensionTest, InstallContentPacks) {
   EXPECT_TRUE(site_names.count(std::string()) == 1u);
   EXPECT_TRUE(site_names.count("Moose") == 1u);
 
-#if defined(ENABLE_CONFIGURATION_POLICY)
   EXPECT_EQ(ManagedModeURLFilter::ALLOW,
             url_filter->GetFilteringBehaviorForURL(example_url));
   EXPECT_EQ(ManagedModeURLFilter::ALLOW,
             url_filter->GetFilteringBehaviorForURL(moose_url));
-#endif
 
   // Disable the first content pack.
   service_->DisableExtension(extension->id(),
@@ -354,10 +349,6 @@ TEST_F(ManagedUserServiceExtensionTest, InstallContentPacks) {
 
   EXPECT_EQ(ManagedModeURLFilter::WARN,
             url_filter->GetFilteringBehaviorForURL(example_url));
-#if defined(ENABLE_CONFIGURATION_POLICY)
   EXPECT_EQ(ManagedModeURLFilter::ALLOW,
             url_filter->GetFilteringBehaviorForURL(moose_url));
-#endif
 }
-
-#endif  // OS_CHROMEOS
