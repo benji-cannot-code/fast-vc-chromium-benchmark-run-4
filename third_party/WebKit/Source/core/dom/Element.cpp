0d6bdd26228478ec8ba1ba60e7c506580ddc7fec
ch.dumez@sisa.samsung.com <ch.dumez@sisa.samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-02 10:23:26 +0000

    Have Node::diff() take a Document reference in argument
    Have Node::diff() take a Document reference in argument instead of a pointer
    as it can never be NULL. This avoid avoids having to convert a reference
    into a pointer at call sites.
    
    R=tkent
    BUG=281400
    
    Review URL: https://chromiumcodereview.appspot.com/23549008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157077 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Element.cpp
+++ third_party/WebKit/Source/core/dom/Element.cpp
@@ -1481,7 +1481,7 @@ Node::StyleChange Element::recalcOwnStyle(StyleChange change)
     CSSAnimationUpdateScope cssAnimationUpdateScope(this);
     RefPtr<RenderStyle> oldStyle = renderStyle();
     RefPtr<RenderStyle> newStyle = styleForRenderer();
-    StyleChange localChange = oldStyle ? Node::diff(oldStyle.get(), newStyle.get(), &document()) : Reattach;
+    StyleChange localChange = oldStyle ? Node::diff(oldStyle.get(), newStyle.get(), document()) : Reattach;
 
     if (localChange == Reattach) {
         AttachContext reattachContext;
