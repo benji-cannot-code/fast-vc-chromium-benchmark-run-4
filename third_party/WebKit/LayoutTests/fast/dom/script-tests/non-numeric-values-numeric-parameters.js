fca78ce5232d292957835be50480c936c4aa1f51
ch.dumez@sisa.samsung.com <ch.dumez@sisa.samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-02 06:48:36 +0000

    Make MediaList API arguments mandatory
    Make MediaList API arguments mandatory to match the specification:
    http://www.w3.org/TR/DOM-Level-2-Style/stylesheets.html#StyleSheets-MediaList
    
    This behavior is consistent with Firefox 23 and IE10. It is confusing what
    the result is supposed to be when the arguments are omitted.
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/23621013
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157063 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/dom/script-tests/non-numeric-values-numeric-parameters.js
+++ third_party/WebKit/LayoutTests/fast/dom/script-tests/non-numeric-values-numeric-parameters.js
@@ -279,7 +279,7 @@ shouldBe("nonNumericPolicy('document.createEvent(\"KeyboardEvent\").initKeyboard
 
 // MediaList
 
-shouldBe("nonNumericPolicy('createMediaList().item(x)')", "'any type allowed'");
+shouldBe("nonNumericPolicy('createMediaList().item(x)')", "'any type allowed (but not omitted)'");
 
 // MouseEvent
 
