950e343702f07ff5fd7a95ba3af8a4bb7fe087ac
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-02 12:01:56 +0000

    [Files.app] Change the widget of the CWS widget in Suggest app dialog.
    This patch changes the width: 450px -> 735px.
    
    BUG=240152
    TEST=manual
    R=hirono@chromium.org
    
    Review URL: https://codereview.chromium.org/23840004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220835 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/suggest_apps_dialog.js
+++ chrome/browser/resources/file_manager/js/suggest_apps_dialog.js
@@ -15,13 +15,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @type {number}
  * @const
  */
-var WEBVIEW_WIDTH = 400;
+var WEBVIEW_WIDTH = 735;
 /**
  * The height of the widget (in pixel).
  * @type {number}
  * @const
  */
 var WEBVIEW_HEIGHT = 480;
+
+/**
+ * The mergin inside the widget (in pixel).
+ * @type {number}
+ * @const
+ */
+var WIDGET_MARGIN = 8;
 
 /**
  * The widget of the spinner box (in pixel).
@@ -316,11 +323,15 @@ SuggestAppsDialog.prototype.onWidgetLoaded_ = function(event) {
   this.webviewContainer_.classList.add('loaded');
   this.state_ = SuggestAppsDialog.State.INITIALIZED;
 
-  this.webviewContainer_.style.width = WEBVIEW_WIDTH + 'px';
-  this.webviewContainer_.style.height = WEBVIEW_HEIGHT + 'px';
+  this.webviewContainer_.style.width =
+      (WEBVIEW_WIDTH + WIDGET_MARGIN * 2) + 'px';
+  this.webviewContainer_.style.height =
+      (WEBVIEW_HEIGHT + WIDGET_MARGIN * 2) + 'px';
 
-  this.webview_.style.width = WEBVIEW_WIDTH + 'px';
-  this.webview_.style.height = WEBVIEW_HEIGHT + 'px';
+  this.webview_.style.width =
+      (WEBVIEW_WIDTH + WIDGET_MARGIN * 2) + 'px';
+  this.webview_.style.height =
+      (WEBVIEW_HEIGHT + WIDGET_MARGIN * 2) + 'px';
   this.webview_.focus();
 };
 
