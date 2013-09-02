4c35fdaee2f32f5de4f248ce22d41e15df852add
rune@opera.com <rune@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-02 21:31:22 +0000

    Recompute percentage based @viewport on resize.
    Percentage values were resolved to px lengths at cascade time for @viewport
    widths and heights. This change keeps them as percentage values and recomputes
    the actual viewport from the initial viewport and the percentage value when
    the intial viewport changes (window resize). Likewise for viewport relative
    units (vw, vh, vmin, vmax).
    
    This fixes constrain-006 and constrain-007 in the w3c testsuite.
    
    BUG=269132
    
    Review URL: https://chromiumcodereview.appspot.com/22549002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157091 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/resolver/ViewportStyleResolver.h
+++ third_party/WebKit/Source/core/css/resolver/ViewportStyleResolver.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ViewportStyleResolver_h
 
 #include "CSSPropertyNames.h"
+#include "core/platform/Length.h"
 #include "wtf/RefCounted.h"
 #include "wtf/RefPtr.h"
 
@@ -59,6 +60,7 @@ private:
     explicit ViewportStyleResolver(Document*);
 
     float getViewportArgumentValue(CSSPropertyID) const;
+    Length getViewportLengthValue(CSSPropertyID) const;
 
     Document* m_document;
     RefPtr<MutableStylePropertySet> m_propertySet;
