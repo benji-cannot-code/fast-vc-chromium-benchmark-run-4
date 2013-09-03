33e47c5c66d716542039a9ecee5f084d2df9e9b1
stevenjb@chromium.org <stevenjb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-03 22:35:00 +0000

    Add createNetwork to networkingPrivate API
    BUG=280334
    For c/b/e/extension_function_histogram_value.h:
    TBR=gauravsh@chromium.org, gspencer@chromium.org, pneubeck@chromium.org
    
    Review URL: https://codereview.chromium.org/23640007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221046 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/api_test/networking/test.js
+++ chrome/test/data/extensions/api_test/networking/test.js
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 var callbackPass = chrome.test.callbackPass;
 var callbackFail = chrome.test.callbackFail;
 var assertTrue = chrome.test.assertTrue;
+var assertFalse = chrome.test.assertFalse;
 var assertEq = chrome.test.assertEq;
 
 // Test properties for the verification API.
@@ -83,6 +84,29 @@ var availableTests = [
     chrome.networkingPrivate.getProperties(
       "nonexistent_path",
       callbackFail("Error.DBusFailed"));
+  },
+  function createNetwork() {
+    chrome.networkingPrivate.createNetwork(
+      false,  // shared
+      { "Type": "WiFi",
+        "GUID": "ignored_guid",
+        "WiFi": {
+          "SSID": "wifi_created",
+          "Security": "WEP-PSK"
+        }
+      },
+      callbackPass(function(guid) {
+        assertFalse(guid == "");
+        assertFalse(guid == "ignored_guid");
+        chrome.networkingPrivate.getProperties(
+          guid,
+          callbackPass(function(properties) {
+            assertEq("WiFi", properties.Type);
+            assertEq(guid, properties.GUID);
+            assertEq("wifi_created", properties.WiFi.SSID);
+            assertEq("WEP-PSK", properties.WiFi.Security);
+          }));
+      }));
   },
   function getVisibleNetworks() {
     chrome.networkingPrivate.getVisibleNetworks(
