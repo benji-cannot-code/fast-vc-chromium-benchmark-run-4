3f98b045291b4a4f5ba0fa02f241e429a6eff102
sievers@chromium.org <sievers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-03 22:57:33 +0000

    Android: Mark gl_tests non-experimental
    They have been running fine on the FYI bot, let's move them over
    to the main waterfall.
    
    BUG=180432, 243797, 281565
    R=frankf@chromium.org
    
    Review URL: https://codereview.chromium.org/23530017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221055 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/pylib/gtest/gtest_config.py
+++ build/android/pylib/gtest/gtest_config.py
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 # Add new suites here before upgrading them to the stable list below.
 EXPERIMENTAL_TEST_SUITES = [
-    'gl_tests',
 ]
 
 # Do not modify this list without approval of an android owner.
@@ -20,6 +19,7 @@ STABLE_TEST_SUITES = [
     'cc_unittests',
     'components_unittests',
     'content_unittests',
+    'gl_tests',
     'gpu_unittests',
     'ipc_tests',
     'media_unittests',
