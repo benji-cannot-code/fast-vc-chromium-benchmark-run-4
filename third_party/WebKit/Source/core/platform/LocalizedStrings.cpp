fdc47d093ff72fd6220eade5a543a0c4d73ffb2e
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-03 09:32:55 +0000

    Move platform/chromium/LocalizedStringsChromium.cpp to platform/LocalizedStrings.cpp.
    No need to use chromium directory and Chromium suffix.
    
    BUG=none
    TEST=none; no behavior changes.
    
    Review URL: https://chromiumcodereview.appspot.com/23715005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157113 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/LocalizedStrings.cpp
+++ third_party/WebKit/Source/core/platform/LocalizedStrings.cpp
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/platform/NotImplemented.h"
 #include "core/platform/graphics/IntSize.h"
 #include "core/platform/text/DateTimeFormat.h"
-
 #include "public/platform/Platform.h"
 #include "public/platform/WebLocalizedString.h"
 #include "public/platform/WebString.h"
