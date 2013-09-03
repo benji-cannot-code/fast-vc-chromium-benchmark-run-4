be8a7f382117f18b662fe244c3c8c83c0bd32e39
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-03 06:56:12 +0000

    file_manager: Remove unused/unnecessary function parameters
    List of file paths was not necessary in some places.
    
    BUG=278037
    TEST=none
    R=kinaba@chromium.org
    
    Review URL: https://codereview.chromium.org/23879002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220903 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/extensions/file_manager/private_api_tasks.cc
+++ chrome/browser/chromeos/extensions/file_manager/private_api_tasks.cc
@@ -160,7 +160,6 @@ bool FileBrowserPrivateGetFileTasksFunction::RunImpl() {
   // file paths.
   PathAndMimeTypeSet path_mime_set;
   std::vector<GURL> file_urls;
-  std::vector<base::FilePath> file_paths;
   for (size_t i = 0; i < files_list->GetSize(); ++i) {
     std::string file_url_str;
     if (!files_list->GetString(i, &file_url_str))
@@ -179,7 +178,6 @@ bool FileBrowserPrivateGetFileTasksFunction::RunImpl() {
     const base::FilePath file_path = file_system_url.path();
 
     file_urls.push_back(file_url);
-    file_paths.push_back(file_path);
 
     // If MIME type is not provided, guess it from the file path.
     if (mime_type.empty())
@@ -192,7 +190,6 @@ bool FileBrowserPrivateGetFileTasksFunction::RunImpl() {
   file_manager::file_tasks::FindAllTypesOfTasks(profile_,
                                                 path_mime_set,
                                                 file_urls,
-                                                file_paths,
                                                 &tasks);
   // Convert the tasks into JSON format.
   ListValue* result_list = new ListValue();
