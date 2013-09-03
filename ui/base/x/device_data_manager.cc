9692684471ecead831ac3f0e00df5b2246182280
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-03 22:37:46 +0000

    x11: Fix some includes.
    These includes aren't necessary, and incorrect for GTK builds.
    
    BUG=none
    R=derat@chromium.org
    
    Review URL: https://codereview.chromium.org/23919002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221048 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/x/device_data_manager.cc
+++ ui/base/x/device_data_manager.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/memory/singleton.h"
-#include "base/message_loop/message_pump_aurax11.h"
 #include "ui/base/events/event_constants.h"
 #include "ui/base/events/event_utils.h"
 #include "ui/base/touch/touch_factory_x11.h"
--- ui/base/x/x11_atom_cache.cc
+++ ui/base/x/x11_atom_cache.cc
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <X11/Xatom.h>
 
+#include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
-#include "base/message_loop/message_pump_aurax11.h"
 
 namespace ui {
 
