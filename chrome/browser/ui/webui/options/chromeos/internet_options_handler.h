00dc0788d50e596db8b5dc9ada5ab1b11c9833db
stevenjb@chromium.org <stevenjb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-03 22:51:29 +0000

    Always show VPN list in internet settings page
    Also:
    Update settings UI when default or connected network changes
    
    BUG=280612
    R=armansito@chromium.org, pneubeck@chromium.org
    
    Review URL: https://codereview.chromium.org/23583017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221054 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options/chromeos/internet_options_handler.h
+++ chrome/browser/ui/webui/options/chromeos/internet_options_handler.h
@@ -88,6 +88,8 @@ class InternetOptionsHandler
   // NetworkStateHandlerObserver
   virtual void NetworkManagerChanged() OVERRIDE;
   virtual void NetworkListChanged() OVERRIDE;
+  virtual void NetworkConnectionStateChanged(
+      const chromeos::NetworkState* network) OVERRIDE;
   virtual void NetworkPropertiesUpdated(
       const chromeos::NetworkState* network) OVERRIDE;
 
@@ -143,6 +145,9 @@ class InternetOptionsHandler
   void FillNetworkInfo(base::DictionaryValue* dictionary);
 
   content::NotificationRegistrar registrar_;
+
+  // Keep track of the service path for the network shown in the Details view.
+  std::string details_path_;
 
   // Weak pointer factory so we can start connections at a later time
   // without worrying that they will actually try to happen after the lifetime
