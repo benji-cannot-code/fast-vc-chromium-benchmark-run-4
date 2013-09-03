b655109679c082226bf195dcc25107d597fc0cab
kinaba@chromium.org <kinaba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-03 08:59:56 +0000

    Expose only .is_directory in drive::FileSystem::Search results.
    * Other fields in ResourceEntry are unused.
    (the is_directory field seems to be used in the custom binding of
    the private API for creating a FileEntry object.)
    
    * ResourceEntry returned from ConvertToResourceEntry() is incomplete
    and hence should not be exposed (base_name() or maybe local_id() in
    the future will not be filled only inside ResourceMetadata).
    
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/23483028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220914 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/file_system/search_operation_unittest.cc
+++ chrome/browser/chromeos/drive/file_system/search_operation_unittest.cc
@@ -51,8 +51,7 @@ TEST_F(SearchOperationTest, ContentSearch) {
   EXPECT_EQ(ARRAYSIZE_UNSAFE(kExpectedResults), results->size());
   for (size_t i = 0; i < results->size(); i++) {
     EXPECT_EQ(kExpectedResults[i].path, results->at(i).path.AsUTF8Unsafe());
-    EXPECT_EQ(kExpectedResults[i].is_directory,
-              results->at(i).entry.file_info().is_directory());
+    EXPECT_EQ(kExpectedResults[i].is_directory, results->at(i).is_directory);
   }
 }
 
@@ -94,7 +93,7 @@ TEST_F(SearchOperationTest, ContentSearchWithNewEntry) {
     EXPECT_EQ(kExpectedResultsBeforeLoad[i].path,
               results->at(i).path.AsUTF8Unsafe());
     EXPECT_EQ(kExpectedResultsBeforeLoad[i].is_directory,
-              results->at(i).entry.file_info().is_directory());
+              results->at(i).is_directory);
   }
 
   // Load the change from FakeDriveService.
@@ -118,7 +117,7 @@ TEST_F(SearchOperationTest, ContentSearchWithNewEntry) {
     EXPECT_EQ(kExpectedResultsAfterLoad[i].path,
               results->at(i).path.AsUTF8Unsafe());
     EXPECT_EQ(kExpectedResultsAfterLoad[i].is_directory,
-              results->at(i).entry.file_info().is_directory());
+              results->at(i).is_directory);
   }
 }
 
