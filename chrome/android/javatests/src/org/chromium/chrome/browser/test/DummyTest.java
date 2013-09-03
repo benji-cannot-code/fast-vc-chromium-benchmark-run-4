53f654a09d4d2b7170bc6e888dc50b099d0f2d17
frankf@chromium.org <frankf@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-03 17:09:20 +0000

    [Android] Add HostDrivenTest java annotation.
    BUG=282102
    R=bulach@chromium.org, craigdh@chromium.org, nyquist@chromium.org
    
    Review URL: https://codereview.chromium.org/23572019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220962 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/android/javatests/src/org/chromium/chrome/browser/test/DummyTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/test/DummyTest.java
@@ -5,13 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.test;
 
-import org.chromium.base.test.util.DisabledTest;
+import org.chromium.base.test.util.HostDrivenTest;
 import org.chromium.chrome.testshell.ChromiumTestShellTestBase;
 
 /**
  * Dummy test suite for verifying the host-driven test framework.
  */
 public class DummyTest extends ChromiumTestShellTestBase {
-    @DisabledTest
+    @HostDrivenTest
     public void testPass() {}
 }
