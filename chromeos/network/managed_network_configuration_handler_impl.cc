benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit c6d5676f041dc6c6c0ec3237c6fa1d88b5ab1e32

Remove a dependency of PolicyApplicator on ManagedNetworkConfigurationHandler.
Instead of accessing the policy map of the ManagedNetworkConfigurationHandler (MNCH), constructor of the PolicyApplicator now copies the map.

This prepares for separating PolicyApplicator from MNCH.

BUG=279268

Review URL: https://chromiumcodereview.appspot.com/22824047

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220878 0039d316-1c4b-4281-b951-d872f2087c98

chromeos/network/managed_network_configuration_handler_impl.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/network/managed_network_configuration_handler_impl.h"
 
+#include <utility>
 #include <vector>
 
 #include "base/bind.h"
@@ -322,6 +323,7 @@ class ManagedNetworkConfigurationHandlerImpl::PolicyApplicator
   PolicyApplicator(
       base::WeakPtr<ManagedNetworkConfigurationHandlerImpl> handler,
       const NetworkProfile& profile,
+      const GuidToPolicyMap& all_policies,
       std::set<std::string>* modified_policies);
 
   void Run();
@@ -357,6 +359,7 @@ class ManagedNetworkConfigurationHandlerImpl::PolicyApplicator
   std::set<std::string> remaining_policies_;
   base::WeakPtr<ManagedNetworkConfigurationHandlerImpl> handler_;
   NetworkProfile profile_;
+  GuidToPolicyMap all_policies_;
 
   DISALLOW_COPY_AND_ASSIGN(PolicyApplicator);
 };
@@ -670,9 +673,7 @@ void ManagedNetworkConfigurationHandlerImpl::SetPolicy(
   }
 
   scoped_refptr<PolicyApplicator> applicator = new PolicyApplicator(
-      weak_ptr_factory_.GetWeakPtr(),
-      *profile,
-      &modified_policies);
+      weak_ptr_factory_.GetWeakPtr(), *profile, policies, &modified_policies);
   applicator->Run();
 }
 
@@ -694,9 +695,7 @@ void ManagedNetworkConfigurationHandlerImpl::OnProfileAdded(
   }
 
   scoped_refptr<PolicyApplicator> applicator = new PolicyApplicator(
-      weak_ptr_factory_.GetWeakPtr(),
-      profile,
-      &policy_guids);
+      weak_ptr_factory_.GetWeakPtr(), profile, *policies, &policy_guids);
   applicator->Run();
 }
 
@@ -809,9 +808,14 @@ void ManagedNetworkConfigurationHandlerImpl::OnPolicyApplied(
 ManagedNetworkConfigurationHandlerImpl::PolicyApplicator::PolicyApplicator(
     base::WeakPtr<ManagedNetworkConfigurationHandlerImpl> handler,
     const NetworkProfile& profile,
+    const GuidToPolicyMap& all_policies,
     std::set<std::string>* modified_policies)
     : handler_(handler), profile_(profile) {
   remaining_policies_.swap(*modified_policies);
+  for (GuidToPolicyMap::const_iterator it = all_policies.begin();
+       it != all_policies.end(); ++it) {
+    all_policies_.insert(std::make_pair(it->first, it->second->DeepCopy()));
+  }
 }
 
 void ManagedNetworkConfigurationHandlerImpl::PolicyApplicator::Run() {
@@ -893,21 +897,17 @@ void ManagedNetworkConfigurationHandlerImpl::PolicyApplicator::GetEntryCallback(
                      (ui_data->onc_source() == onc::ONC_SOURCE_DEVICE_POLICY ||
                       ui_data->onc_source() == onc::ONC_SOURCE_USER_POLICY);
 
-  // The relevant policy must have been initialized, otherwise we hadn't Run
-  // this PolicyApplicator.
-  const GuidToPolicyMap& policies = *handler_->GetPoliciesForProfile(profile_);
-
   const base::DictionaryValue* new_policy = NULL;
   if (was_managed) {
     // If we have a GUID that might match a current policy, do a lookup using
     // that GUID at first. In particular this is necessary, as some networks
     // can't be matched to policies by properties (e.g. VPN).
-    new_policy = GetByGUID(policies, old_guid);
+    new_policy = GetByGUID(all_policies_, old_guid);
   }
 
   if (!new_policy) {
     // If we didn't find a policy by GUID, still a new policy might match.
-    new_policy = FindMatchingPolicy(policies, *onc_part);
+    new_policy = FindMatchingPolicy(all_policies_, *onc_part);
   }
 
   if (new_policy) {
@@ -996,22 +996,15 @@ ManagedNetworkConfigurationHandlerImpl::PolicyApplicator::~PolicyApplicator() {
 
   VLOG(2) << "Create new managed network configurations in profile"
           << profile_.ToDebugString() << ".";
-  // All profile entries were compared to policies. |configureGUIDs_| contains
-  // all matched policies. From the remainder of policies, new configurations
-  // have to be created.
-
-  // The relevant policy must have been initialized, otherwise we hadn't Run
-  // this PolicyApplicator.
-  const GuidToPolicyMap& policies = *handler_->GetPoliciesForProfile(profile_);
+  // All profile entries were compared to policies. |remaining_policies_|
+  // contains all modified policies that didn't match any entry. For these
+  // remaining policies, new configurations have to be created.
 
   for (std::set<std::string>::iterator it = remaining_policies_.begin();
        it != remaining_policies_.end();
        ++it) {
-    const base::DictionaryValue* policy = GetByGUID(policies, *it);
-    if (!policy) {
-      LOG(ERROR) << "Policy " << *it << " doesn't exist anymore.";
-      continue;
-    }
+    const base::DictionaryValue* policy = GetByGUID(all_policies_, *it);
+    DCHECK(policy);
 
     VLOG(1) << "Creating new configuration managed by policy " << *it
             << " in profile " << profile_.ToDebugString() << ".";
@@ -1019,6 +1012,8 @@ ManagedNetworkConfigurationHandlerImpl::PolicyApplicator::~PolicyApplicator() {
     CreateAndWriteNewShillConfiguration(
         *it, *policy, NULL /* no user settings */);
   }
+
+  STLDeleteValues(&all_policies_);
 }
 
 }  // namespace chromeos

commit 40f598ada5515519da5ed284c8f33fe14018a69d

Fix heap leak in ManagedNetworkConfigurationHandlerImpl.
The leak was introduced by r220878.

Also reverts commit r220892 "Suppress leaks in chromeos::ManagedNetworkConfigurationHandlerImpl".

BUG=284191

Review URL: https://chromiumcodereview.appspot.com/23878004

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220942 0039d316-1c4b-4281-b951-d872f2087c98

chromeos/network/managed_network_configuration_handler_impl.cc
@@ -352,9 +352,13 @@ class ManagedNetworkConfigurationHandlerImpl::PolicyApplicator
       const base::DictionaryValue& policy,
       const base::DictionaryValue* user_settings);
 
-  // Creates new entries for all remaining policies, i.e. for which not matching
-  // entry was found.
+  // Called once all Profile entries are processed. Calls
+  // ApplyRemainingPolicies.
   virtual ~PolicyApplicator();
+
+  // Creates new entries for all remaining policies, i.e. for which no matching
+  // Profile entry was found.
+  void ApplyRemainingPolicies();
 
   std::set<std::string> remaining_policies_;
   base::WeakPtr<ManagedNetworkConfigurationHandlerImpl> handler_;
@@ -985,6 +989,12 @@ void ManagedNetworkConfigurationHandlerImpl::PolicyApplicator::
 }
 
 ManagedNetworkConfigurationHandlerImpl::PolicyApplicator::~PolicyApplicator() {
+  ApplyRemainingPolicies();
+  STLDeleteValues(&all_policies_);
+}
+
+void ManagedNetworkConfigurationHandlerImpl::PolicyApplicator::
+    ApplyRemainingPolicies() {
   if (!handler_) {
     LOG(WARNING) << "Handler destructed during policy application to profile "
                  << profile_.ToDebugString();
@@ -1012,8 +1022,6 @@ ManagedNetworkConfigurationHandlerImpl::PolicyApplicator::~PolicyApplicator() {
     CreateAndWriteNewShillConfiguration(
         *it, *policy, NULL /* no user settings */);
   }
-
-  STLDeleteValues(&all_policies_);
 }
 
 }  // namespace chromeos
40f598ada5515519da5ed284c8f33fe14018a69d
pneubeck@chromium.org <pneubeck@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-03 14:55:09 +0000

    Fix heap leak in ManagedNetworkConfigurationHandlerImpl.
    The leak was introduced by r220878.
    
    Also reverts commit r220892 "Suppress leaks in chromeos::ManagedNetworkConfigurationHandlerImpl".
    
    BUG=284191
    
    Review URL: https://chromiumcodereview.appspot.com/23878004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220942 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/network/managed_network_configuration_handler_impl.cc
+++ chromeos/network/managed_network_configuration_handler_impl.cc
@@ -352,9 +352,13 @@ class ManagedNetworkConfigurationHandlerImpl::PolicyApplicator
       const base::DictionaryValue& policy,
       const base::DictionaryValue* user_settings);
 
-  // Creates new entries for all remaining policies, i.e. for which not matching
-  // entry was found.
+  // Called once all Profile entries are processed. Calls
+  // ApplyRemainingPolicies.
   virtual ~PolicyApplicator();
+
+  // Creates new entries for all remaining policies, i.e. for which no matching
+  // Profile entry was found.
+  void ApplyRemainingPolicies();
 
   std::set<std::string> remaining_policies_;
   base::WeakPtr<ManagedNetworkConfigurationHandlerImpl> handler_;
@@ -985,6 +989,12 @@ void ManagedNetworkConfigurationHandlerImpl::PolicyApplicator::
 }
 
 ManagedNetworkConfigurationHandlerImpl::PolicyApplicator::~PolicyApplicator() {
+  ApplyRemainingPolicies();
+  STLDeleteValues(&all_policies_);
+}
+
+void ManagedNetworkConfigurationHandlerImpl::PolicyApplicator::
+    ApplyRemainingPolicies() {
   if (!handler_) {
     LOG(WARNING) << "Handler destructed during policy application to profile "
                  << profile_.ToDebugString();
@@ -1012,8 +1022,6 @@ ManagedNetworkConfigurationHandlerImpl::PolicyApplicator::~PolicyApplicator() {
     CreateAndWriteNewShillConfiguration(
         *it, *policy, NULL /* no user settings */);
   }
-
-  STLDeleteValues(&all_policies_);
 }
 
 }  // namespace chromeos
