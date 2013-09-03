4539afe38a7d28363ed72f33d2ab78f6d5169669
hidehiko@chromium.org <hidehiko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-03 04:40:02 +0000

    Adds GetResourceListInDirectoryByWapi to JobScheduler.
    As a short term solution of performance regression by switching Drive API,
    we'll keep to use GData WAPI for Fast Fetch. This is its preparation.
    
    BUG=283622
    TEST=Ran unit_tests
    
    Review URL: https://chromiumcodereview.appspot.com/23864002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220894 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/job_list.cc
+++ chrome/browser/chromeos/drive/job_list.cc
@@ -61,6 +61,10 @@ std::string JobTypeToString(JobType type) {
       return "TYPE_UPLOAD_EXISTING_FILE";
     case TYPE_CREATE_FILE:
       return "TYPE_CREATE_FILE";
+    case TYPE_GET_RESOURCE_LIST_IN_DIRECTORY_BY_WAPI:
+      return "TYPE_GET_RESOURCE_LIST_IN_DIRECTORY_BY_WAPI";
+    case TYPE_GET_REMAINING_RESOURCE_LIST:
+      return "TYPE_GET_REMAINING_RESOURCE_LIST";
   }
   NOTREACHED();
   return "(unknown job type)";
@@ -137,6 +141,8 @@ bool IsActiveFileTransferJobInfo(const JobInfo& job_info) {
     case TYPE_REMOVE_RESOURCE_FROM_DIRECTORY:
     case TYPE_ADD_NEW_DIRECTORY:
     case TYPE_CREATE_FILE:
+    case TYPE_GET_RESOURCE_LIST_IN_DIRECTORY_BY_WAPI:
+    case TYPE_GET_REMAINING_RESOURCE_LIST:
       return false;
     case TYPE_DOWNLOAD_FILE:
     case TYPE_UPLOAD_NEW_FILE:
