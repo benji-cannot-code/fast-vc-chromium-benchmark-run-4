087605a0d695262071bcc5e5cc372e68b4e48326
eseidel@chromium.org <eseidel@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-04 17:48:42 +0000

    Revert r155355 as it caused CJK glyphs to render as boxes for users with Droid Sans installed.
    I've filed http://crbug.com/285303 about fixing the underlying issue
    that this change made more obvious.
    
    Unfortunately this change will cause a large perf regression for CJK text.
    It's just reverting a large perf gain we got with the previous change.
    
    This reverts:
    https://codereview.chromium.org/21497002
    
    BUG=272006
    R=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/23622018
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157226 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/linux/WebFontFamily.h
+++ third_party/WebKit/public/platform/linux/WebFontFamily.h
@@ -38,12 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebKit {
 
 struct WebFontFamily {
-    WebFontFamily()
-        : isBold(false)
-        , isItalic(false)
-    {
-    }
-
     WebCString name;
     bool isBold;
     bool isItalic;
