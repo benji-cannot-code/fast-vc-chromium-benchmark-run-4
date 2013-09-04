2360513a657b11ddeada00453608750f9fdbb279
gyuyoung.kim@samsung.com <gyuyoung.kim@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-04 07:38:49 +0000

    Introduce toSVGViewElement(), and refactor setupInitialView() using it
    As a step to clean-up static_cast<SVGXXX>, toSVGTextElement() is added to clean-up
    static_cast<SVGTextElement*>. Besides this cl does refactoring duplicated condition checking
    in setupInitialView().
    
    BUG=N/A
    
    Review URL: https://chromiumcodereview.appspot.com/23636006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157206 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGViewElement.h
+++ third_party/WebKit/Source/core/svg/SVGViewElement.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SVGViewElement_h
 #define SVGViewElement_h
 
+#include "SVGNames.h"
 #include "core/svg/SVGAnimatedBoolean.h"
 #include "core/svg/SVGAnimatedPreserveAspectRatio.h"
 #include "core/svg/SVGAnimatedRect.h"
@@ -65,6 +66,12 @@ private:
     SVGZoomAndPanType m_zoomAndPan;
     SVGStringList m_viewTarget;
 };
+
+inline SVGViewElement* toSVGViewElement(Node* node)
+{
+    ASSERT_WITH_SECURITY_IMPLICATION(!node || node->hasTagName(SVGNames::viewTag));
+    return static_cast<SVGViewElement*>(node);
+}
 
 } // namespace WebCore
 
