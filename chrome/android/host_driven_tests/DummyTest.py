2e055676efd98b8197464c5cf9607122c9cda1ac
nyquist@chromium.org <nyquist@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-04 02:32:38 +0000

    Add test_server_setup.py and add support for additional flags.
    Whenever a host driven test need to setup a server as part of a test,
    the command line needs of the application to be instrumented needs to
    change.
    
    This CL adds a helper class for setting up a test server, and also adds
    support for tests to setup additional flags (typically the URL for how
    to connect to the test server).
    
    Reverted in: http://crrev.com/220769
    
    Relanding after re-upload to the same patch.
    
    BUG=272584
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=220750
    
    Review URL: https://chromiumcodereview.appspot.com/23726004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221122 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/android/host_driven_tests/DummyTest.py
+++ chrome/android/host_driven_tests/DummyTest.py
@@ -3,7 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import os
+"""Host-driven java_tests which exercise dummy functionality.
+
+This test class is only here to ensure that the test framework for host driven
+tests work.
+"""
 
 from pylib.host_driven import test_case
 from pylib.host_driven import tests_annotations
