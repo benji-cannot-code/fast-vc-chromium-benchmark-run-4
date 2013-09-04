0d6daacb3dc1085930ae7595ea78c0d6c54a6cff
kaznacheev@chromium.org <kaznacheev@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-04 15:41:00 +0000

    DevTools: Add public frame-related methods to Page domain.
    Added frameAttached event and made existing frameNavigated and
    frameDetached public. Also made Page.Frame type public as it is
    used by frameNavigated.
    
    BUG=None
    
    Review URL: https://chromiumcodereview.appspot.com/23565006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157222 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ResourceTreeModel.js
+++ third_party/WebKit/Source/devtools/front_end/ResourceTreeModel.js
@@ -672,6 +672,10 @@ WebInspector.PageDispatcher.prototype = {
         this._resourceTreeModel.dispatchEventToListeners(WebInspector.ResourceTreeModel.EventTypes.Load, time);
     },
 
+    frameAttached: function(frameId)
+    {
+    },
+
     frameNavigated: function(frame)
     {
         this._resourceTreeModel._frameNavigated(frame);
