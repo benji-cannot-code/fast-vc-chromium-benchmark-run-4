31c1802b485cdd9e25889ed30258a38edc82bb1a
jyasskin@chromium.org <jyasskin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-04 17:18:31 +0000

    Add a function to send scripts from a browsertest to an extension's background page.
    This lets browsertests and apitests use more obvious code in some
    cases, especially when they need to alternate between the background
    page and the test running code.
    
    Review URL: https://chromiumcodereview.appspot.com/23874006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221216 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_browsertest.cc
+++ chrome/browser/extensions/extension_browsertest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/chrome_notification_types.h"
+#include "chrome/browser/extensions/browsertest_util.h"
 #include "chrome/browser/extensions/component_loader.h"
 #include "chrome/browser/extensions/crx_installer.h"
 #include "chrome/browser/extensions/extension_creator.h"
@@ -675,6 +676,13 @@ extensions::ExtensionHost* ExtensionBrowserTest::FindHostWithPath(
   }
   EXPECT_EQ(expected_hosts, num_hosts);
   return host;
+}
+
+std::string ExtensionBrowserTest::ExecuteScriptInBackgroundPage(
+    const std::string& extension_id,
+    const std::string& script) {
+  return extensions::browsertest_util::ExecuteScriptInBackgroundPage(
+      profile(), extension_id, script);
 }
 
 void ExtensionBrowserTest::Observe(
