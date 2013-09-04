84725f740c7e10356b90d91bda54e1e80a5e0421
davidben@chromium.org <davidben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-04 21:12:31 +0000

    Fix and re-enable PrerenderTaskManager test
    The NOTREACHED it hit a year ago was removed from the code in r205020. But the
    test in the meantime has broken because TaskManagerModel now caches values.
    Explicitly call TaskManagerModel::Refresh before querying values.
    
    This makes TaskManagerModel::Refresh public.
    
    BUG=131836
    TEST=PrerenderBrowserTest.PrerenderTaskManager
    
    Review URL: https://chromiumcodereview.appspot.com/23523024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221270 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/task_manager/task_manager.h
+++ chrome/browser/task_manager/task_manager.h
@@ -307,6 +307,9 @@ class TaskManagerModel : public base::RefCountedThreadSafe<TaskManagerModel> {
   // changes to the model.
   void ModelChanged();
 
+   // Updates the values for all rows.
+  void Refresh();
+
   void NotifyResourceTypeStats(
         base::ProcessId renderer_id,
         const WebKit::WebCache::ResourceTypeStats& stats);
@@ -450,9 +453,6 @@ class TaskManagerModel : public base::RefCountedThreadSafe<TaskManagerModel> {
 
   // Callback from the timer to refresh. Invokes Refresh() as appropriate.
   void RefreshCallback();
-
-   // Updates the values for all rows.
-  void Refresh();
 
   void RefreshVideoMemoryUsageStats();
 
