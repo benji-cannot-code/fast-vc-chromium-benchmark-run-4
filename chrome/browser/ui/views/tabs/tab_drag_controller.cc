7a99481cd022a1a2c2e550436536c04cc9dff027
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-04 14:29:39 +0000

    Removes debugging code added @ 219682
    TBR=pkasting@chromium.org
    BUG=275931
    TEST=none
    
    Review URL: https://codereview.chromium.org/23460016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221188 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/tabs/tab_drag_controller.cc
+++ chrome/browser/ui/views/tabs/tab_drag_controller.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/auto_reset.h"
 #include "base/callback.h"
 #include "base/command_line.h"
-#include "base/debug/alias.h"
 #include "base/i18n/rtl.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/extensions/extension_function_dispatcher.h"
@@ -377,13 +376,11 @@ TabDragController::TabDragController()
       tab_strip_to_attach_to_after_exit_(NULL),
       move_loop_widget_(NULL),
       destroyed_(NULL),
-      is_mutating_(false),
-      saving_focus_(false) {
+      is_mutating_(false) {
   instance_ = this;
 }
 
 TabDragController::~TabDragController() {
-  CHECK(!saving_focus_);
   if (instance_ == this)
     instance_ = NULL;
 
@@ -750,14 +747,6 @@ void TabDragController::SaveFocus() {
   DCHECK(!old_focused_view_);  // This should only be invoked once.
   DCHECK(source_tabstrip_);
   old_focused_view_ = source_tabstrip_->GetFocusManager()->GetFocusedView();
-  // TODO(sky): used in tracking crash; see 275931.
-  const char* old_focused_view_class_name =
-      old_focused_view_ ? old_focused_view_->GetClassName() : NULL;
-  base::debug::Alias(old_focused_view_class_name);
-  const int old_focused_view_id =
-      old_focused_view_ ? old_focused_view_->id() : 0;
-  base::debug::Alias(&old_focused_view_id);
-  base::AutoReset<bool> setter(&saving_focus_, true);
   source_tabstrip_->GetFocusManager()->SetFocusedView(source_tabstrip_);
 }
 
