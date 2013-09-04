4d5c992992f7ada73b661e65f6ed3c099e7e4287
jdduke@chromium.org <jdduke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-04 16:41:52 +0000

    [Android] Provide a valid initial size for the overscroll effect
    Chrome on Android's overscroll effect is created on-demand, the first time a
    user overscrolls.  Rather than wait for an update from the compositor to give
    the effect a valid size, provide the created effect the last valid content size.
    This prevents the initial overscroll hitch that may occur if the compositor is
    otherwise inactive when the first overscroll update is received.
    
    BUG=284523
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/23494021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221200 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/android/overscroll_glow.h
+++ content/browser/android/overscroll_glow.h
@@ -31,7 +31,7 @@ class OverscrollGlow {
   // If |enabled| is false, the effect will be be deactivated until
   // SetEnabled(true) is called.
   // The caller should attach |root_layer| to the desired layer tree.
-  static scoped_ptr<OverscrollGlow> Create(bool enabled);
+  static scoped_ptr<OverscrollGlow> Create(bool enabled, gfx::SizeF size);
 
   // Force loading of any necessary resources.  This function is thread-safe.
   static void EnsureResources();
@@ -80,7 +80,10 @@ class OverscrollGlow {
  private:
   enum Axis { AXIS_X, AXIS_Y };
 
-  OverscrollGlow(bool enabled, const SkBitmap& edge, const SkBitmap& glow);
+  OverscrollGlow(bool enabled,
+                 gfx::SizeF size,
+                 const SkBitmap& edge,
+                 const SkBitmap& glow);
 
   void Pull(base::TimeTicks current_time,
             gfx::Vector2dF added_overscroll);
