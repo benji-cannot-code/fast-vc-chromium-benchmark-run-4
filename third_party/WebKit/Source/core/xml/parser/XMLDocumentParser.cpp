c1aa9b970a5b4c6f2bcfed73fe835395ccd3af95
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-04 00:49:05 +0000

    XMLDocumentParser should lazy attach.
    import-non-document-node.xml: whitespace text node change.
    dynamic-caption-add-before-child.xhtml: this is a progression.
    svg/custom/resource-invalidation-crash.svg: not clear what's going on here,
    but since this is just testing for a crash with some weird patterns,
    just filing a bug for now. crbug.com/284881
    
    BUG=276689
    R=eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/23779004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157175 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/xml/parser/XMLDocumentParser.cpp
+++ third_party/WebKit/Source/core/xml/parser/XMLDocumentParser.cpp
@@ -374,7 +374,7 @@ void XMLDocumentParser::enterText()
     ASSERT(m_bufferedText.size() == 0);
     ASSERT(!m_leafTextNode);
     m_leafTextNode = Text::create(&m_currentNode->document(), "");
-    m_currentNode->parserAppendChild(m_leafTextNode.get(), DeprecatedAttachNow);
+    m_currentNode->parserAppendChild(m_leafTextNode.get());
 }
 
 void XMLDocumentParser::exitText()
@@ -385,7 +385,7 @@ void XMLDocumentParser::exitText()
     if (!m_leafTextNode)
         return;
 
-    m_leafTextNode->appendData(toString(m_bufferedText.data(), m_bufferedText.size()), DeprecatedAttachNow);
+    m_leafTextNode->appendData(toString(m_bufferedText.data(), m_bufferedText.size()));
     m_bufferedText.clear();
     m_leafTextNode = 0;
 }
@@ -986,7 +986,7 @@ void XMLDocumentParser::startElementNs(const AtomicString& localName, const Atom
     if (scriptLoader)
         m_scriptStartPosition = textPosition();
 
-    m_currentNode->parserAppendChild(newElement.get(), DeprecatedAttachNow);
+    m_currentNode->parserAppendChild(newElement.get());
 
     const ContainerNode* currentNode = m_currentNode;
     if (newElement->hasTagName(HTMLNames::templateTag))
@@ -1140,7 +1140,7 @@ void XMLDocumentParser::processingInstruction(const String& target, const String
 
     pi->setCreatedByParser(true);
 
-    m_currentNode->parserAppendChild(pi.get(), DeprecatedAttachNow);
+    m_currentNode->parserAppendChild(pi.get());
 
     pi->finishParsingChildren();
 
@@ -1164,7 +1164,7 @@ void XMLDocumentParser::cdataBlock(const String& text)
     exitText();
 
     RefPtr<CDATASection> newNode = CDATASection::create(&m_currentNode->document(), text);
-    m_currentNode->parserAppendChild(newNode.get(), DeprecatedAttachNow);
+    m_currentNode->parserAppendChild(newNode.get());
 }
 
 void XMLDocumentParser::comment(const String& text)
@@ -1180,7 +1180,7 @@ void XMLDocumentParser::comment(const String& text)
     exitText();
 
     RefPtr<Comment> newNode = Comment::create(&m_currentNode->document(), text);
-    m_currentNode->parserAppendChild(newNode.get(), DeprecatedAttachNow);
+    m_currentNode->parserAppendChild(newNode.get());
 }
 
 enum StandaloneInfo {
@@ -1223,7 +1223,7 @@ void XMLDocumentParser::internalSubset(const String& name, const String& externa
     }
 
     if (document())
-        document()->parserAppendChild(DocumentType::create(document(), name, externalID, systemID), DeprecatedAttachNow);
+        document()->parserAppendChild(DocumentType::create(document(), name, externalID, systemID));
 }
 
 static inline XMLDocumentParser* getParser(void* closure)
