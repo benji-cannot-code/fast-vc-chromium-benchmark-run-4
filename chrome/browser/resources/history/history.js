51b3f4ac7fe817b06643039a9186f666bd790a00
dubroy@chromium.org <dubroy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-04 01:43:19 +0000

    Revert 219018 "History: Prevent search box from disappearing whe..."
    > History: Prevent search box from disappearing when search term is deleted.
    > 
    > Also removed unnecessary calls to updateNavBar_().
    > 
    > BUG=263870
    > TEST=See repro steps in bug.
    > R=sergiu@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/22929031
    
    TBR=dubroy@chromium.org
    
    Review URL: https://codereview.chromium.org/23463015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221109 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/history/history.js
+++ chrome/browser/resources/history/history.js
@@ -930,14 +930,15 @@ HistoryView.prototype.getOffset = function() {
  */
 HistoryView.prototype.onModelReady = function(doneLoading) {
   this.displayResults_(doneLoading);
-  this.updateNavBar_();
 
-  if (isMobileVersion()) {
-    // Hide the search field if it is empty and there are no results.
-    var hasResults = this.model_.visits_.length > 0;
-    var isSearch = this.model_.getSearchText().length > 0;
-    $('search-field').hidden = !(hasResults || isSearch);
-  }
+  // Allow custom styling based on whether there are any results on the page.
+  // To make this easier, add a class to the body if there are any results.
+  if (this.model_.visits_.length)
+    document.body.classList.add('has-results');
+  else
+    document.body.classList.remove('has-results');
+
+  this.updateNavBar_();
 };
 
 /**
@@ -1298,6 +1299,7 @@ HistoryView.prototype.displayResults_ = function(doneLoading) {
           createElementWithClassName('div', 'no-results-message'));
       noResults.textContent = loadTimeData.getString('noResults');
       this.resultDiv_.appendChild(resultsFragment);
+      this.updateNavBar_();
       return;
     }
 
@@ -1325,6 +1327,7 @@ HistoryView.prototype.displayResults_ = function(doneLoading) {
     // Add all the days and their visits to the page.
     this.resultDiv_.appendChild(resultsFragment);
   }
+  this.updateNavBar_();
 };
 
 /**
@@ -1333,10 +1336,9 @@ HistoryView.prototype.displayResults_ = function(doneLoading) {
  */
 HistoryView.prototype.updateNavBar_ = function() {
   this.updateRangeButtons_();
-
-  // Managed users have the control bar on top, don't show it on the bottom
-  // as well.
   if (!loadTimeData.getBoolean('isManagedProfile')) {
+    // Managed users have the control bar on top, don't show it on the bottom
+    // as well.
     $('newest-button').hidden = this.pageIndex_ == 0;
     $('newer-button').hidden = this.pageIndex_ == 0;
     $('older-button').hidden =
