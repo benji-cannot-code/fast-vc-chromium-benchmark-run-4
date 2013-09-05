9c5d8f13fdb81517b82b21e28b58c79490430083
vivek.vg@samsung.com <vivek.vg@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-05 21:09:53 +0000

    DOMParser.parseFromString() should support creating HTML Document with mimetype text/html
    The specification http://domparsing.spec.whatwg.org/#the-domparser-interface for DOMParser.parseFromString()
    allows the document creation if the mimeType is one of the supported types: 
    ["text/html", "text/xml", "application/xml", "application/xhtml+xml", "image/svg+xml"]
    
    Also script elements get marked unexecutable and the contents of noscript get parsed as markup.
    
    R=tkent, abarth, eseidel
    
    BUG=265379
    
    Review URL: https://chromiumcodereview.appspot.com/23903014
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157322 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/xml/DOMParser.cpp
+++ third_party/WebKit/Source/core/xml/DOMParser.cpp
@@ -21,14 +21,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/xml/DOMParser.h"
 
 #include "core/dom/DOMImplementation.h"
-#include "core/dom/Document.h"
 #include "wtf/text/WTFString.h"
 
 namespace WebCore {
 
 PassRefPtr<Document> DOMParser::parseFromString(const String& str, const String& contentType)
 {
-    if (!DOMImplementation::isXMLMIMEType(contentType))
+    if (!DOMImplementation::isXMLMIMEType(contentType) && contentType != "text/html")
         return 0;
 
     RefPtr<Document> doc = DOMImplementation::createDocument(contentType, 0, KURL(), false);
