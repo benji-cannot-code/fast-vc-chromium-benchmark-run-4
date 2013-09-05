348897f8aec0f154a32ad5f95e2d019b9588edf8
johnjbarton@chromium.org <johnjbarton@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-05 19:06:49 +0000

    Add preprocessor to reloadOptions in chrome.devtools.inspectedWindow.reload().
    Add options.preprocessor to ExtensionServer.js.
    
    Add test case to extensions-reload.html to verify preprocessingScript is
    run.
    
    BUG=229971
    
    Review URL: https://chromiumcodereview.appspot.com/23514035
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157318 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/externs.js
+++ third_party/WebKit/Source/devtools/front_end/externs.js
@@ -355,6 +355,7 @@ function ExtensionDescriptor() {
 function ExtensionReloadOptions() {
     this.ignoreCache = false;
     this.injectedScript = "";
+    this.preprocessingScript = "";
     this.userAgent = "";
 }
 
