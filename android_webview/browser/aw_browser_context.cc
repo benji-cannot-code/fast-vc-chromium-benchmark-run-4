ca845096cfc363a0f29d551d7dcaf7884f876271
joth@chromium.org <joth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-05 03:11:23 +0000

    [Android WebView] Remove some log spam
    SpecialStoragePolicy is not required, so no need to log about it.
    
    NOTRY=true
    BUG=285570
    
    Review URL: https://chromiumcodereview.appspot.com/23579008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221335 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/browser/aw_browser_context.cc
+++ android_webview/browser/aw_browser_context.cc
@@ -248,8 +248,7 @@ AwBrowserContext::GetGeolocationPermissionContext() {
 }
 
 quota::SpecialStoragePolicy* AwBrowserContext::GetSpecialStoragePolicy() {
-  // TODO(boliu): Implement this so we are not relying on default behavior.
-  NOTIMPLEMENTED();
+  // Intentionally returning NULL as 'Extensions' and 'Apps' not supported.
   return NULL;
 }
 
