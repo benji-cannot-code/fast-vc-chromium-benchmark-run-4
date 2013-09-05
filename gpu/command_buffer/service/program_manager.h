1d47d3f1fe3968c64ab3933cf51d3fb1bbbf9186
zmo@chromium.org <zmo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-05 01:32:53 +0000

    Apply varying packing rule.
    BUG=249018
    TEST=gpu_unittests, webgl conformance tests
    
    Review URL: https://chromiumcodereview.appspot.com/23534007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221326 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/program_manager.h
+++ gpu/command_buffer/service/program_manager.h
@@ -196,6 +196,10 @@ class GPU_EXPORT Program : public base::RefCounted<Program> {
   // is not declared in vertex shader.
   bool DetectVaryingsMismatch() const;
 
+  // Return false if varyings can't be packed into the max available
+  // varying registers.
+  bool CheckVaryingsPacking() const;
+
   // Visible for testing
   const LocationMap& bind_attrib_location_map() const {
     return bind_attrib_location_map_;
@@ -330,7 +334,8 @@ class GPU_EXPORT Program : public base::RefCounted<Program> {
 // need to be shared by multiple GLES2Decoders.
 class GPU_EXPORT ProgramManager {
  public:
-  explicit ProgramManager(ProgramCache* program_cache);
+  explicit ProgramManager(ProgramCache* program_cache,
+                          uint32 max_varying_vectors);
   ~ProgramManager();
 
   // Must call before destruction.
@@ -372,11 +377,18 @@ class GPU_EXPORT ProgramManager {
                        ShaderTranslator* translator,
                        FeatureInfo* feature_info);
 
+  uint32 max_varying_vectors() const {
+    return max_varying_vectors_;
+  }
+
  private:
   friend class Program;
 
   void StartTracking(Program* program);
   void StopTracking(Program* program);
+
+  void RemoveProgramInfoIfUnused(
+      ShaderManager* shader_manager, Program* program);
 
   // Info for each "successfully linked" program by service side program Id.
   // TODO(gman): Choose a faster container.
@@ -396,8 +408,7 @@ class GPU_EXPORT ProgramManager {
 
   ProgramCache* program_cache_;
 
-  void RemoveProgramInfoIfUnused(
-      ShaderManager* shader_manager, Program* program);
+  uint32 max_varying_vectors_;
 
   DISALLOW_COPY_AND_ASSIGN(ProgramManager);
 };
