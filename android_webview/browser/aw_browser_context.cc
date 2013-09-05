d8f86b1f601f75a79d051d8a887f1cd9baa19e58
pneubeck@chromium.org <pneubeck@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-05 14:46:12 +0000

    Revert 221409 "Fix threading issues in aw form database"
    Broke Android Tests:
    http://build.chromium.org/p/chromium.linux/buildstatus?builder=Android%20Tests%20%28dbg%29&number=14173
    
    > Fix threading issues in aw form database
    > 
    > BUG=285584
    > 
    > Review URL: https://chromiumcodereview.appspot.com/23803005
    
    TBR=sgurun@chromium.org
    
    Review URL: https://codereview.chromium.org/23679005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221423 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/browser/aw_browser_context.cc
+++ android_webview/browser/aw_browser_context.cc
@@ -24,8 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "net/url_request/url_request_context.h"
 
-using content::BrowserThread;
-
 namespace android_webview {
 
 namespace {
@@ -44,11 +42,11 @@ class AwResourceContext : public content::ResourceContext {
 
   // content::ResourceContext implementation.
   virtual net::HostResolver* GetHostResolver() OVERRIDE {
-    DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+    DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::IO));
     return getter_->GetURLRequestContext()->host_resolver();
   }
   virtual net::URLRequestContext* GetRequestContext() OVERRIDE {
-    DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+    DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::IO));
     return getter_->GetURLRequestContext();
   }
   virtual bool AllowMicAccess(const GURL& origin) OVERRIDE {
@@ -76,9 +74,6 @@ AwBrowserContext::AwBrowserContext(
       user_pref_service_ready_(false) {
   DCHECK(g_browser_context == NULL);
   g_browser_context = this;
-
-  form_database_service_.reset(
-      new AwFormDatabaseService(context_storage_path_));
 }
 
 AwBrowserContext::~AwBrowserContext() {
@@ -152,6 +147,10 @@ AwQuotaManagerBridge* AwBrowserContext::GetQuotaManagerBridge() {
 }
 
 AwFormDatabaseService* AwBrowserContext::GetFormDatabaseService() {
+  if (!form_database_service_) {
+    form_database_service_.reset(
+        new AwFormDatabaseService(context_storage_path_));
+  }
   return form_database_service_.get();
 }
 
