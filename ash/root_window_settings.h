6450635422f8b9d35ad35b6bcb29656ad9434bde
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-05 20:13:55 +0000

    fix macro, comments and unused includes
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/23591028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221499 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/root_window_settings.h
+++ ash/root_window_settings.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef ASH_WM_ROOT_WINDOW_SETTINGS_H_
-#define ASH_WM_ROOT_WINDOW_SETTINGS_H_
+#ifndef ASH_ROOT_WINDOW_SETTINGS_H_
+#define ASH_ROOT_WINDOW_SETTINGS_H_
 
 #include "ash/ash_export.h"
 #include "base/basictypes.h"
@@ -36,8 +36,8 @@ struct RootWindowSettings {
   RootWindowController* controller;
 };
 
-// Initializes and creates RootWindowSettings for |root|, and returns
-// the property.
+// Initializes and returns RootWindowSettings for |root|.
+// It is owned by the |root|.
 RootWindowSettings* InitRootWindowSettings(aura::RootWindow* root);
 
 // Returns the RootWindowSettings for |root|.
@@ -50,4 +50,4 @@ GetRootWindowSettings(const aura::RootWindow* root);
 }  // namespace internal
 }  // namespace ash
 
-#endif  // ASH_WM_ROOT_WINDOW_SETTINGS_H_
+#endif  // ASH_ROOT_WINDOW_SETTINGS_H_
