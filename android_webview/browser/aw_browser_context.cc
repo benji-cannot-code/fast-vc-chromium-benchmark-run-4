d5461d8f10500d9e543912a211a966a5e43b7884
sgurun@chromium.org <sgurun@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-05 13:36:47 +0000

    Fix threading issues in aw form database
    BUG=285584
    
    Review URL: https://chromiumcodereview.appspot.com/23803005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221409 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/browser/aw_browser_context.cc
+++ android_webview/browser/aw_browser_context.cc
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "net/url_request/url_request_context.h"
 
+using content::BrowserThread;
+
 namespace android_webview {
 
 namespace {
@@ -42,11 +44,11 @@ class AwResourceContext : public content::ResourceContext {
 
   // content::ResourceContext implementation.
   virtual net::HostResolver* GetHostResolver() OVERRIDE {
-    DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::IO));
+    DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
     return getter_->GetURLRequestContext()->host_resolver();
   }
   virtual net::URLRequestContext* GetRequestContext() OVERRIDE {
-    DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::IO));
+    DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
     return getter_->GetURLRequestContext();
   }
   virtual bool AllowMicAccess(const GURL& origin) OVERRIDE {
@@ -74,6 +76,9 @@ AwBrowserContext::AwBrowserContext(
       user_pref_service_ready_(false) {
   DCHECK(g_browser_context == NULL);
   g_browser_context = this;
+
+  form_database_service_.reset(
+      new AwFormDatabaseService(context_storage_path_));
 }
 
 AwBrowserContext::~AwBrowserContext() {
@@ -147,10 +152,6 @@ AwQuotaManagerBridge* AwBrowserContext::GetQuotaManagerBridge() {
 }
 
 AwFormDatabaseService* AwBrowserContext::GetFormDatabaseService() {
-  if (!form_database_service_) {
-    form_database_service_.reset(
-        new AwFormDatabaseService(context_storage_path_));
-  }
   return form_database_service_.get();
 }
 
