a0d62a4507c231988990d3782695c1d17da48987
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-05 00:14:36 +0000

    GTTF: Add command-line switches for number of jobs and batch limit.
    BUG=236893
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/23760003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221315 0039d316-1c4b-4281-b951-d872f2087c98

--- base/test/test_switches.cc
+++ base/test/test_switches.cc
@@ -9,6 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // TODO(phajdan.jr): Clean up the switch names.
 const char switches::kTestLargeTimeout[] = "test-large-timeout";
 
+// Maximum number of tests to run in a single batch.
+const char switches::kTestLauncherBatchLimit[] = "test-launcher-batch-limit";
+
+// Number of parallel test launcher jobs.
+const char switches::kTestLauncherJobs[] = "test-launcher-jobs";
+
 // Path to test results file in our custom test launcher format.
 const char switches::kTestLauncherOutput[] = "test-launcher-output";
 
