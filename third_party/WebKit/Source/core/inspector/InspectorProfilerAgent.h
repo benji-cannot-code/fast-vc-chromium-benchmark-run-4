6a9c714a8e27652eed9bb30412439f5bb4101053
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-05 13:07:00 +0000

    Allow configuring CPU profiler sampling interval
    Added a setting for selecting higher sampling rate for CPU profiler 0.1ms sampling interval vs. default 1ms one.
    
    BUG=v8:2814
    R=loislo@chromium.org
    
    Review URL: https://codereview.chromium.org/23444035
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157301 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorProfilerAgent.h
+++ third_party/WebKit/Source/core/inspector/InspectorProfilerAgent.h
@@ -63,6 +63,7 @@ public:
 
     virtual void enable(ErrorString*);
     virtual void disable(ErrorString*);
+    virtual void setSamplingInterval(ErrorString*, int);
     virtual void start(ErrorString* = 0);
     virtual void stop(ErrorString*, RefPtr<TypeBuilder::Profiler::ProfileHeader>& header);
 
--- third_party/WebKit/Source/devtools/front_end/ProfilesPanel.js
+++ third_party/WebKit/Source/devtools/front_end/ProfilesPanel.js
@@ -436,6 +436,9 @@ WebInspector.ProfilesPanel = function(name, type)
     this._registerShortcuts();
 
     WebInspector.ContextMenu.registerProvider(this);
+
+    this._configureCpuProfilerSamplingInterval();
+    WebInspector.settings.highResolutionCpuProfiling.addChangeListener(this._configureCpuProfilerSamplingInterval, this);
 }
 
 WebInspector.ProfilesPanel.prototype = {
@@ -471,6 +474,17 @@ WebInspector.ProfilesPanel.prototype = {
     _registerShortcuts: function()
     {
         this.registerShortcuts(WebInspector.ProfilesPanelDescriptor.ShortcutKeys.StartStopRecording, this.toggleRecordButton.bind(this));
+    },
+
+    _configureCpuProfilerSamplingInterval: function()
+    {
+        var intervalUs = WebInspector.settings.highResolutionCpuProfiling.get() ? 100 : 1000;
+        ProfilerAgent.setSamplingInterval(intervalUs, didChangeInterval.bind(this));
+        function didChangeInterval(error)
+        {
+            if (error)
+                WebInspector.showErrorMessage(error)
+        }
     },
 
     /**
