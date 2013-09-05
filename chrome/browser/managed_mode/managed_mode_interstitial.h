19a68eb79c64adf05705b8682b330c7c0d97a00d
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-05 15:52:10 +0000

    Watch for filtering pref changes in ManagedModeInterstitial and unblock requests if they become allowed.
    BUG=283379
    
    Review URL: https://chromiumcodereview.appspot.com/23533014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221442 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/managed_mode/managed_mode_interstitial.h
+++ chrome/browser/managed_mode/managed_mode_interstitial.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/compiler_specific.h"
-#include "base/memory/weak_ptr.h"
+#include "base/prefs/pref_change_registrar.h"
 #include "content/public/browser/interstitial_page_delegate.h"
 #include "url/gurl.h"
 
@@ -36,6 +36,12 @@ class ManagedModeInterstitial : public content::InterstitialPageDelegate {
   virtual void OnProceed() OVERRIDE;
   virtual void OnDontProceed() OVERRIDE;
 
+  // Returns whether the blocked URL is now allowed. Called initially before the
+  // interstitial is shown (to catch race conditions), or when the URL filtering
+  // prefs change.
+  bool ShouldProceed();
+
+  void OnFilteringPrefsChanged();
   void DispatchContinueRequest(bool continue_request);
 
   // Owns the interstitial, which owns us.
@@ -43,11 +49,12 @@ class ManagedModeInterstitial : public content::InterstitialPageDelegate {
 
   content::InterstitialPage* interstitial_page_;  // Owns us.
 
+  PrefChangeRegistrar pref_change_registrar_;
+
   // The UI language. Used for formatting the URL for display.
   std::string languages_;
   GURL url_;
 
-  base::WeakPtrFactory<ManagedModeInterstitial> weak_ptr_factory_;
   base::Callback<void(bool)> callback_;
 
   DISALLOW_COPY_AND_ASSIGN(ManagedModeInterstitial);
