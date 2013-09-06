08e5b111d3d03ce126a996d4ae2e9723f5dce3fc
kenneth.r.christiansen@intel.com <kenneth.r.christiansen@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-06 14:12:18 +0000

    The content='...' parsing error is not a viewport error, so keep it separate
    The parsing algorithm for the meta content attribute reports an error
    for presense of ';'. It is currently using viewport error reporting, though
    it is not tied to the viewport meta tag, but meta tags in general.
    
    Review URL: https://chromiumcodereview.appspot.com/24025002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157367 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/ViewportArguments.h
+++ third_party/WebKit/Source/core/dom/ViewportArguments.h
@@ -39,7 +39,6 @@ namespace WebCore {
 class Document;
 
 enum ViewportErrorCode {
-    InvalidKeyValuePairSeparatorError,
     UnrecognizedViewportArgumentKeyError,
     UnrecognizedViewportArgumentValueError,
     TruncatedViewportArgumentValueError,
