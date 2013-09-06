91cdea1adc70a01e638bc0f99a1ec97f8a2e6997
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-06 06:22:06 +0000

    RootWindowController::GetFullscreenWindow() should return the active one.
    It returned the fullscreen window (if any) in the active workspace,
    and a workspace could contain only one fullscreen window at most.
    However, now there's only one desktop workspace which can contain
    multiple fullscreen windows.
    
    To follow the past state, it would be better to return the active
    fullscreen window first.
    
    TEST=covered by ash_unittests
    R=oshima@chromium.org, sky@chromium.org
    BUG=None
    
    Review URL: https://chromiumcodereview.appspot.com/23534049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221602 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/shelf/shelf_layout_manager.cc
+++ ash/shelf/shelf_layout_manager.cc
@@ -526,7 +526,7 @@ bool ShelfLayoutManager::FullscreenWithMinimalChrome() const {
   RootWindowController* controller = GetRootWindowController(root_window_);
   if (!controller)
     return false;
-  const aura::Window* window = controller->GetFullscreenWindow();
+  const aura::Window* window = controller->GetTopmostFullscreenWindow();
   if (!window)
     return false;
   if (!window->GetProperty(kFullscreenUsesMinimalChromeKey))
--- chrome/browser/fullscreen_chromeos.cc
+++ chrome/browser/fullscreen_chromeos.cc
@@ -10,5 +10,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 bool IsFullScreenMode() {
   ash::internal::RootWindowController* controller =
       ash::internal::RootWindowController::ForActiveRootWindow();
-  return controller && controller->GetFullscreenWindow();
+  return controller && controller->GetTopmostFullscreenWindow();
 }
