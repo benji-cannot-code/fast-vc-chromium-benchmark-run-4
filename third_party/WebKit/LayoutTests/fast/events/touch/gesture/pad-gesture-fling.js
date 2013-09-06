d755a688185f3aaa9f734d4a63cc94e0fd741bef
bokan@chromium.org <bokan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-06 17:54:22 +0000

    Made pad-gesture-fling layout test deterministic
    BUG=261277
    
    Review URL: https://chromiumcodereview.appspot.com/23711005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157379 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/events/touch/gesture/pad-gesture-fling.js
+++ third_party/WebKit/LayoutTests/fast/events/touch/gesture/pad-gesture-fling.js
@@ -1,18 +1,25 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 description("Tests basic use of GestureFlingStart");
 
-var expectedWheelEventsOccurred = "2";
 var actualWheelEventsOccurred = 0;
+var cumulativeScrollX = 0;
+var cumulativeScrollY = 0;
+
+var minimumWheelEventsExpected = "6";
+var minimumScrollXExpected = "10";
+var minimumScrollYExpected = "10";
 
 function recordWheelEvent(event)
 {
-    shouldBe('event.clientX', "10");
-    shouldBe('event.clientY', "11");
+    if (event.clientX != 10)
+      debug('FAIL: clientX != 10');
 
-    // Test deliberately does not equality check wheelDeltas to not be fragile in the face of curve adjustment.
-    shouldBeTrue("event.wheelDeltaX > 5");
-    shouldBeTrue("event.wheelDeltaY > 5");
+    if (event.clientY != 11)
+      debug('FAIL: event.clientY != 11');
+
     actualWheelEventsOccurred++;
+    cumulativeScrollX += event.wheelDeltaX;
+    cumulativeScrollY += event.wheelDeltaY;
 }
 
 document.addEventListener("mousewheel", recordWheelEvent);
@@ -25,7 +32,9 @@ if (window.testRunner && window.eventSender && window.eventSender.gestureFlingSt
 }
 
 setTimeout(function() {
-    shouldBe('actualWheelEventsOccurred', expectedWheelEventsOccurred);
+    shouldBeGreaterThanOrEqual('actualWheelEventsOccurred', minimumWheelEventsExpected);
+    shouldBeGreaterThanOrEqual('cumulativeScrollX', minimumScrollXExpected);
+    shouldBeGreaterThanOrEqual('cumulativeScrollY', minimumScrollYExpected);
 }, 100);
 
 if (window.testRunner)
