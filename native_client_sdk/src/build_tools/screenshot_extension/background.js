b7e731f688fe7d6fc91ec95af26b00bed94753fd
binji@chromium.org <binji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-06 21:00:10 +0000

    [NaCl SDK] Screen capture extension for use with SDK visual testing.
    BUG=none
    R=sbc@chromium.org
    
    Review URL: https://codereview.chromium.org/23458018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221777 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ native_client_sdk/src/build_tools/screenshot_extension/background.js
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function takeScreenshot(onScreenshot) {
+  console.log('Taking screenshot.');
+  chrome.tabs.captureVisibleTab(null, {format: 'png'}, function(img) {
+    console.log('Got screenshot, returning...');
+    onScreenshot(img);
+  });
+}
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if (changeInfo.status != 'complete')
+    return;
+
+  chrome.tabs.executeScript(tabId,
+                            {file: 'injected.js', runAt: 'document_start'});
+});
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      takeScreenshot(sendResponse);
+
+      // Keep the sendResponse channel open, so a response can be sent
+      // asynchronously.
+      return true;
+    });
