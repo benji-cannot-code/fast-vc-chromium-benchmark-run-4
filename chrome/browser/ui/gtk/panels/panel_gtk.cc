bc88ecaf193a8bff4be8e8a861dc8f7a25363ad6
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-06 20:16:55 +0000

    Switch to doing user-resizing via system for panels on Mac
    We used to track and handle user-resizing by ourselves. This brings up a few problems and makes the user-resizing hard to use on Mac. For example, the user-resizing could not be triggered outside or on the edge. Switching to doing it via system solve all these problems.
    
    Also removed EnableResizeByMouse which is not really used in our code.
    
    BUG=282652,282657
    TEST=manual test by user-resizing panels
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=221651
    
    Review URL: https://chromiumcodereview.appspot.com/23918002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221765 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/panels/panel_gtk.cc
+++ chrome/browser/ui/gtk/panels/panel_gtk.cc
@@ -1069,9 +1069,6 @@ void PanelGtk::SetPanelAlwaysOnTop(bool on_top) {
     gtk_window_unstick(window_);
 }
 
-void PanelGtk::EnableResizeByMouse(bool enable) {
-}
-
 void PanelGtk::UpdatePanelMinimizeRestoreButtonVisibility() {
   titlebar_->UpdateMinimizeRestoreButtonVisibility();
 }
