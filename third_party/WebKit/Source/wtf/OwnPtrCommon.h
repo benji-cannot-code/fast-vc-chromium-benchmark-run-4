2292079de2c697ae43d0d1919bacc87009a1c782
mikhail.pozdnyakov@intel.com <mikhail.pozdnyakov@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-06 12:11:37 +0000

    Remove Windows GDI specializations for deleteOwnedPtr
    We do not need them for many reasons:
    - they are not used
    - they are not even defined
    - they could not work even if they were defined (after r157328)
    
    Review URL: https://chromiumcodereview.appspot.com/23527003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157363 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/OwnPtrCommon.h
+++ third_party/WebKit/Source/wtf/OwnPtrCommon.h
@@ -29,16 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WTF_OwnPtrCommon_h
 #define WTF_OwnPtrCommon_h
 
-#if OS(WINDOWS)
-typedef struct HBITMAP__* HBITMAP;
-typedef struct HBRUSH__* HBRUSH;
-typedef struct HDC__* HDC;
-typedef struct HFONT__* HFONT;
-typedef struct HPALETTE__* HPALETTE;
-typedef struct HPEN__* HPEN;
-typedef struct HRGN__* HRGN;
-#endif
-
 namespace WTF {
 
     template <typename T> inline void deleteOwnedPtr(T* ptr)
@@ -47,16 +37,6 @@ namespace WTF {
         if (sizeof(known))
             delete ptr;
     }
-
-#if OS(WINDOWS)
-    void deleteOwnedPtr(HBITMAP);
-    void deleteOwnedPtr(HBRUSH);
-    void deleteOwnedPtr(HDC);
-    void deleteOwnedPtr(HFONT);
-    void deleteOwnedPtr(HPALETTE);
-    void deleteOwnedPtr(HPEN);
-    void deleteOwnedPtr(HRGN);
-#endif
 
 } // namespace WTF
 
