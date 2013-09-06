26793f205c57b1475a43430856714d3f59064334
henrika@chromium.org <henrika@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-06 10:59:36 +0000

    Revert 221651 "Switch to doing user-resizing via system for pane..."
    > Switch to doing user-resizing via system for panels on Mac
    > 
    > We used to track and handle user-resizing by ourselves. This brings up a few problems and makes the user-resizing hard to use on Mac. For example, the user-resizing could not be triggered outside or on the edge. Switching to doing it via system solve all these problems.
    > 
    > Also removed EnableResizeByMouse which is not really used in our code.
    > 
    > BUG=282652,282657
    > TEST=manual test by user-resizing panels
    > 
    > Review URL: https://chromiumcodereview.appspot.com/23918002
    
    TBR=jianli@chromium.org
    
    Review URL: https://codereview.chromium.org/23922003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221674 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/panels/panel_cocoa.mm
+++ chrome/browser/ui/cocoa/panels/panel_cocoa.mm
@@ -262,6 +262,10 @@ void PanelCocoa::SetPanelAlwaysOnTop(bool on_top) {
   [controller_ updateWindowCollectionBehavior];
 }
 
+void PanelCocoa::EnableResizeByMouse(bool enable) {
+  [controller_ enableResizeByMouse:enable];
+}
+
 void PanelCocoa::UpdatePanelMinimizeRestoreButtonVisibility() {
   [controller_ updateTitleBarMinimizeRestoreButtonVisibility];
 }
