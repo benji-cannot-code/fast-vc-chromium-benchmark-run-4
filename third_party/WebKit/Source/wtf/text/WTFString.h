c0c96b8749d1410a5eb7c4a7cc8a28337d8f51e7
mikhail.pozdnyakov@intel.com <mikhail.pozdnyakov@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-06 09:42:10 +0000

    String vector's comparison works wrong
    The problem happened because SimpleClassVectorTraits was used as a vector trait for WTF::String class, hence 'memcmp' was used for string vector's comparison, which is apparently wrong.
    This CL corrects vector trait for WTF::String and also introduces a unit test to verify that the problem is not happening any more.
    
    Review URL: https://chromiumcodereview.appspot.com/23819029
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157360 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/text/WTFString.h
+++ third_party/WebKit/Source/wtf/text/WTFString.h
@@ -683,7 +683,9 @@ template<> struct DefaultHash<String> {
     typedef StringHash Hash;
 };
 
-template <> struct VectorTraits<String> : SimpleClassVectorTraits { };
+template <> struct VectorTraits<String> : SimpleClassVectorTraits {
+    static const bool canCompareWithMemcmp = false;
+};
 
 // Shared global empty string.
 WTF_EXPORT const String& emptyString();
