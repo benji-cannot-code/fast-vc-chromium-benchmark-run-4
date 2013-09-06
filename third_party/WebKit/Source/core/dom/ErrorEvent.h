987eeefd7e41eb58b7cd5870a2bb3e0af6957b9c
mkwst@chromium.org <mkwst@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-06 17:37:34 +0000

    Trigger `window.onerror` only for exceptions thrown in the same world.
    This patch addresses half of crbug.com/225513 by grabbing the currently
    active DOMWrapperWorld when creating ErrorEvent objects, and checking
    that we're still in the same world when triggering `window.onerror`
    handlers.
    
    The next step will be to adjust event dispatching to ensure that 'error'
    event listeners only trigger in the same world as the exception. I'll address
    that in a separate patch; this patch adds several FAILing test results to make
    that gap clear.
    
    BUG=225513
    
    Review URL: https://chromiumcodereview.appspot.com/21071003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157376 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/ErrorEvent.h
+++ third_party/WebKit/Source/core/dom/ErrorEvent.h
@@ -32,7 +32,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ErrorEvent_h
 #define ErrorEvent_h
 
+#include "bindings/v8/DOMWrapperWorld.h"
 #include "core/dom/Event.h"
+#include "wtf/RefPtr.h"
 #include "wtf/text/WTFString.h"
 
 namespace WebCore {
@@ -52,17 +54,17 @@ public:
     {
         return adoptRef(new ErrorEvent);
     }
-    static PassRefPtr<ErrorEvent> create(const String& message, const String& fileName, unsigned lineNumber, unsigned columnNumber)
+    static PassRefPtr<ErrorEvent> create(const String& message, const String& fileName, unsigned lineNumber, unsigned columnNumber, PassRefPtr<DOMWrapperWorld> world)
     {
-        return adoptRef(new ErrorEvent(message, fileName, lineNumber, columnNumber));
+        return adoptRef(new ErrorEvent(message, fileName, lineNumber, columnNumber, world));
     }
     static PassRefPtr<ErrorEvent> create(const AtomicString& type, const ErrorEventInit& initializer)
     {
         return adoptRef(new ErrorEvent(type, initializer));
     }
-    static PassRefPtr<ErrorEvent> createSanitizedError()
+    static PassRefPtr<ErrorEvent> createSanitizedError(PassRefPtr<DOMWrapperWorld> world)
     {
-        return adoptRef(new ErrorEvent("Script error.", String(), 0, 0));
+        return adoptRef(new ErrorEvent("Script error.", String(), 0, 0, world));
     }
     virtual ~ErrorEvent();
 
@@ -77,11 +79,13 @@ public:
 
     virtual const AtomicString& interfaceName() const;
 
+    PassRefPtr<DOMWrapperWorld> world() const { return m_world; }
+
     void setUnsanitizedMessage(const String&);
 
 private:
     ErrorEvent();
-    ErrorEvent(const String& message, const String& fileName, unsigned lineNumber, unsigned columnNumber);
+    ErrorEvent(const String& message, const String& fileName, unsigned lineNumber, unsigned columnNumber, PassRefPtr<DOMWrapperWorld>);
     ErrorEvent(const AtomicString&, const ErrorEventInit&);
 
     String m_unsanitizedMessage;
@@ -89,6 +93,8 @@ private:
     String m_fileName;
     unsigned m_lineNumber;
     unsigned m_columnNumber;
+
+    RefPtr<DOMWrapperWorld> m_world;
 };
 
 } // namespace WebCore
--- third_party/WebKit/Source/core/dom/ScriptExecutionContext.cpp
+++ third_party/WebKit/Source/core/dom/ScriptExecutionContext.cpp
@@ -242,7 +242,7 @@ bool ScriptExecutionContext::dispatchErrorEvent(PassRefPtr<ErrorEvent> event, Ac
 
     RefPtr<ErrorEvent> errorEvent = event;
     if (shouldSanitizeScriptError(errorEvent->filename(), corsStatus))
-        errorEvent = ErrorEvent::createSanitizedError();
+        errorEvent = ErrorEvent::createSanitizedError(errorEvent->world());
 
     ASSERT(!m_inDispatchErrorEvent);
     m_inDispatchErrorEvent = true;
