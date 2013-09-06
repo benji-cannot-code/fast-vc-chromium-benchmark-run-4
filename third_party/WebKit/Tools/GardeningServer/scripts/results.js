d6768365c8b24a7c563e7db12d58949cbdf93bce
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-06 20:55:46 +0000

    Remove the duplicate 'layout-test-results' path from the archived results.
    TBR=ojan@chromium.org, leviw@chromium.org
    BUG=285572
    
    Review URL: https://codereview.chromium.org/23694024
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157385 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/GardeningServer/scripts/results.js
+++ third_party/WebKit/Tools/GardeningServer/scripts/results.js
@@ -190,8 +190,7 @@ function resultsDirectoryURL(platform, builderName)
 {
     if (config.useLocalResults)
         return '/localresult?path=';
-    // FIXME: The duplicate "layout-test-results" is due to crbug.com/285572.
-    return resultsDirectoryListingURL(platform, builderName) + 'results/layout-test-results/layout-test-results/';
+    return resultsDirectoryListingURL(platform, builderName) + 'results/layout-test-results/';
 }
 
 function resultsDirectoryListingURL(platform, builderName)
