842821ee4f83e9fd42fd0e6db39478db397278cb
rvargas@chromium.org <rvargas@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-06 07:46:48 +0000

    Remove redundant exit-detection code.
    Now we detect unexpected process termination for all chrome processess so the
    code for renderers and flash is redundant.
    
    BUG=275818
    TEST=none
    R=cpu@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/23687007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221630 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/renderer_main_platform_delegate_win.cc
+++ content/renderer/renderer_main_platform_delegate_win.cc
@@ -44,14 +44,6 @@ void SkiaPreCacheFontCharacters(const LOGFONT& logfont,
   }
 }
 
-void InitExitInterceptions() {
-  // If code subsequently tries to exit using exit(), _exit(), abort(), or
-  // ExitProcess(), force a crash (since otherwise these would be silent
-  // terminations and fly under the radar).
-  base::win::SetShouldCrashOnProcessDetach(true);
-  base::win::SetAbortBehaviorForCrashReporting();
-}
-
 #if !defined(NDEBUG)
 LRESULT CALLBACK WindowsHookCBT(int code, WPARAM w_param, LPARAM l_param) {
   CHECK_NE(code, HCBT_CREATEWND)
@@ -59,7 +51,6 @@ LRESULT CALLBACK WindowsHookCBT(int code, WPARAM w_param, LPARAM l_param) {
   return CallNextHookEx(NULL, code, w_param, l_param);
 }
 #endif  // !NDEBUG
-
 
 }  // namespace
 
@@ -81,8 +72,6 @@ void RendererMainPlatformDelegate::PlatformInitialize() {
   PCHECK(
       SetWindowsHookEx(WH_CBT, WindowsHookCBT, NULL, ::GetCurrentThreadId()));
 #endif  // !NDEBUG
-
-  InitExitInterceptions();
 
   const CommandLine& command_line = parameters_.command_line;
 
@@ -110,9 +99,6 @@ void RendererMainPlatformDelegate::PlatformInitialize() {
 }
 
 void RendererMainPlatformDelegate::PlatformUninitialize() {
-  // At this point we are shutting down in a normal code path, so undo our
-  // hack to crash on exit.
-  base::win::SetShouldCrashOnProcessDetach(false);
 }
 
 bool RendererMainPlatformDelegate::InitSandboxTests(bool no_sandbox) {
