6423ee7fa82642959e0e5b05c3dd0ed5ebf22639
sivachandra@chromium.org <sivachandra@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-06 06:42:24 +0000

    [Android Buildbot] Add a GPU builder cum tester.
    BUG=256576
    
    Review URL: https://chromiumcodereview.appspot.com/23601004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221610 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/buildbot/bb_device_steps.py
+++ build/android/buildbot/bb_device_steps.py
@@ -67,8 +67,8 @@ INSTRUMENTATION_TESTS = dict((suite.name, suite) for suite in [
       'webview:android_webview/test/data/device_files'),
     ])
 
-VALID_TESTS = set(['chromedriver', 'ui', 'unit', 'webkit', 'webkit_layout',
-                   'webrtc'])
+VALID_TESTS = set(['chromedriver', 'gpu', 'ui', 'unit', 'webkit',
+                   'webkit_layout', 'webrtc'])
 
 RunCmd = bb_utils.RunCmd
 
@@ -304,9 +304,16 @@ def RunWebRTCTests(options):
   RunTestSuites(options, gtest_config.WEBRTC_TEST_SUITES)
 
 
+def RunGPUTests(_):
+  bb_annotations.PrintNamedStep('gpu_tests')
+  RunCmd(['content/test/gpu/run_gpu_test',
+          '--browser=android-content-shell', 'pixel'])
+
+
 def GetTestStepCmds():
   return [
       ('chromedriver', RunChromeDriverTests),
+      ('gpu', RunGPUTests),
       ('unit', RunUnitTests),
       ('ui', RunInstrumentationTests),
       ('webkit', RunWebkitTests),
