21f07e8c0e56571b2f5070356e20ee7f068a1a90
jdduke@chromium.org <jdduke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-07 07:07:52 +0000

    Add InputEvent and EventPacket types for batched input delivery
    With the BufferedInputRouter, browser input events will be batched into packets
    before being sent to the renderer.  Add a common InputEvent type that provides
    id'ed carriage of either WebInputEvents or general IPC input messages. Also add
    a composite EventPacket type for batched InputEvent transport and dispatch.
    
    BUG=245499
    
    Review URL: https://chromiumcodereview.appspot.com/19624005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221905 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ content/common/input/input_event.cc
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/common/input/input_event.h"
+
+#include "base/logging.h"
+
+namespace content {
+
+InputEvent::InputEvent() : id_(0) {}
+
+InputEvent::~InputEvent() {}
+
+scoped_ptr<InputEvent> InputEvent::Create(int64 id,
+                                          scoped_ptr<Payload> payload) {
+  scoped_ptr<InputEvent> event(new InputEvent());
+  event->Initialize(id, payload.Pass());
+  return event.Pass();
+}
+
+bool InputEvent::Initialize(int64 id, scoped_ptr<Payload> payload) {
+  id_ = id;
+  payload_ = payload.Pass();
+  return valid();
+}
+
+}  // namespace content
