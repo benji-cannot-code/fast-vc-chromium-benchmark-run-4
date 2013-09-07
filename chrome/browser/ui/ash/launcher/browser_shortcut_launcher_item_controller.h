50c0c4c8fabe47ebc0984247f7fb69784506595c
simon.hong81@gmail.com <simon.hong81@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-07 00:07:38 +0000

    Handling browser item status in BrowserShortcutLauncherItemController
    Move the role of handling browser item status from ChromeLauncherController
    to BrowserShortcutLauncherItemController.
    
    R=skuhne@chromium.org
    BUG=NONE
    TEST=browser_tests, unit_tests
    
    Review URL: https://chromiumcodereview.appspot.com/23534022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221823 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/ash/launcher/browser_shortcut_launcher_item_controller.h
+++ chrome/browser/ui/ash/launcher/browser_shortcut_launcher_item_controller.h
@@ -28,6 +28,9 @@ class BrowserShortcutLauncherItemController : public LauncherItemController {
 
   virtual ~BrowserShortcutLauncherItemController();
 
+  // Updates the activation state of the Broswer item.
+  void UpdateBrowserItemState();
+
   // LauncherItemController overrides:
   virtual string16 GetTitle() OVERRIDE;
   virtual bool IsCurrentlyShownInWindow(aura::Window* window) const OVERRIDE;
@@ -55,6 +58,10 @@ class BrowserShortcutLauncherItemController : public LauncherItemController {
 
   // Activate a browser - or advance to the next one on the list.
   void ActivateOrAdvanceToNextBrowser();
+
+  // Returns true when the given |browser| is listed in the browser application
+  // list.
+  bool IsBrowserRepresentedInBrowserList(Browser* browser);
 
   Profile* profile_;
 
