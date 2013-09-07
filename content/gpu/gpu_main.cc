8c19191a2fd27ebf2e471e08c49916407916d9ea
apatrick@chromium.org <apatrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-07 02:09:07 +0000

    Warm up DWM prior to lowering token.
    DWM was introduced with Windows Vista. DwmFlush seems to be sufficient to warm it up before lowering the token. DWM is required to present to a window with Vista and later and this allows us to do so with the GPU process sandbox enabled.
    
    Enabling the sandbox for this case is not in this patch. Other things are required, such as making the sandbox broker allow certain event objects to be opened from the GPU process.
    
    Review URL: https://chromiumcodereview.appspot.com/23992006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221859 0039d316-1c4b-4281-b951-d872f2087c98

--- content/gpu/gpu_main.cc
+++ content/gpu/gpu_main.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdlib.h>
 
 #if defined(OS_WIN)
+#include <dwmapi.h>
 #include <windows.h>
 #endif
 
@@ -36,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gpu_switching_manager.h"
 
 #if defined(OS_WIN)
+#include "base/win/windows_version.h"
 #include "base/win/scoped_com_initializer.h"
 #include "content/common/gpu/media/dxva_video_decode_accelerator.h"
 #include "sandbox/win/src/sandbox.h"
@@ -375,6 +377,25 @@ bool WarmUpSandbox(const CommandLine& command_line) {
     TRACE_EVENT0("gpu", "Initialize DXVA");
     // Initialize H/W video decoding stuff which fails in the sandbox.
     DXVAVideoDecodeAccelerator::PreSandboxInitialization();
+  }
+
+  {
+    TRACE_EVENT0("gpu", "Warm up DWM");
+
+    // DWM was introduced with Windows Vista. DwmFlush seems to be sufficient
+    // to warm it up before lowering the token. DWM is required to present to
+    // a window with Vista and later and this allows us to do so with the
+    // GPU process sandbox enabled.
+    if (base::win::GetVersion() >= base::win::VERSION_VISTA) {
+      HMODULE module = LoadLibrary(L"dwmapi.dll");
+      if (module) {
+        typedef HRESULT (WINAPI *DwmFlushFunc)();
+        DwmFlushFunc dwm_flush = reinterpret_cast<DwmFlushFunc>(
+            GetProcAddress(module, "DwmFlush"));
+        if (dwm_flush)
+          dwm_flush();
+      }
+    }
   }
 #endif
   return true;
