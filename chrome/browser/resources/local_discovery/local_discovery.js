62b79128916e0555ffbde3e213a0d18024da4a4c
noamsml@chromium.org <noamsml@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-07 07:33:20 +0000

    Added UMA actions to devices page
    Added UMA actions for devices page, including for when it is opened, when "add
    printers" is clicked, when "register" is clicked, when a registration is
    canceled, when a registration fails, and when a registration succeeds.
    
    BUG=286157
    
    Review URL: https://chromiumcodereview.appspot.com/23508003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221913 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/local_discovery/local_discovery.js
+++ chrome/browser/resources/local_discovery/local_discovery.js
@@ -87,6 +87,7 @@ cr.define('local_discovery', function() {
      * Register the device.
      */
     register: function() {
+      recordUmaAction('DevicesPage_RegisterClicked');
       chrome.send('registerDevice', [this.info.service_name]);
       setRegisterPage('register-page-adding1');
     }
@@ -143,9 +144,10 @@ cr.define('local_discovery', function() {
   }
 
   /**
-   * Hide the register overlay.
+   * Show the register overlay.
    */
   function showRegisterOverlay() {
+    recordUmaAction('DevicesPage_AddPrintersClicked');
     $('register-overlay').classList.add('showing');
     $('overlay').hidden = false;
     uber.invokeMethodOnParent('beginInterceptingEvents');
@@ -153,13 +155,12 @@ cr.define('local_discovery', function() {
   }
 
   /**
-   * Show the register overlay.
+   * Hide the register overlay.
    */
   function hideRegisterOverlay() {
     $('register-overlay').classList.remove('showing');
     $('overlay').hidden = true;
     uber.invokeMethodOnParent('stopInterceptingEvents');
-    chrome.send('cancelRegistration');
   }
 
   /**
@@ -177,6 +178,7 @@ cr.define('local_discovery', function() {
    */
   function onRegistrationFailed() {
     setRegisterPage('register-page-error');
+    recordUmaAction('DevicesPage_RegisterFailure');
   }
 
   /**
@@ -256,6 +258,7 @@ cr.define('local_discovery', function() {
   function onRegistrationSuccess() {
     hideRegisterOverlay();
     requestPrinterList();
+    recordUmaAction('DevicesPage_RegisterSuccess');
   }
 
   /**
@@ -293,25 +296,42 @@ cr.define('local_discovery', function() {
    * @param {string} device_id ID of device.
    */
   function manageCloudDevice(device_id) {
+    recordUmaAction('DevicesPage_ManageClicked');
     chrome.send('openCloudPrintURL',
                 [PRINTER_MANAGEMENT_PAGE_PREFIX + device_id]);
   }
 
+  /**
+   * Record an action in UMA.
+   * @param {string} actionDesc The name of the action to be logged.
+   */
+  function recordUmaAction(actionDesc) {
+    chrome.send('metricsHandler:recordAction', [actionDesc]);
+  }
+
+  /**
+   * Cancel the registration.
+   */
+  function cancelRegistration() {
+    hideRegisterOverlay();
+    chrome.send('cancelRegistration');
+    recordUmaAction('DevicesPage_RegisterCancel');
+  }
 
   document.addEventListener('DOMContentLoaded', function() {
     uber.onContentFrameLoaded();
 
     cr.ui.overlay.setupOverlay($('overlay'));
     cr.ui.overlay.globalInitialization();
-    $('overlay').addEventListener('cancelOverlay', hideRegisterOverlay);
+    $('overlay').addEventListener('cancelOverlay', cancelRegistration);
 
     var cancelButtons = document.querySelectorAll('.register-cancel');
     var cancelButtonsLength = cancelButtons.length;
     for (var i = 0; i < cancelButtonsLength; i++) {
-      cancelButtons[i].addEventListener('click', hideRegisterOverlay);
+      cancelButtons[i].addEventListener('click', cancelRegistration);
     }
 
-    $('register-error-exit').addEventListener('click', hideRegisterOverlay);
+    $('register-error-exit').addEventListener('click', cancelRegistration);
 
     $('add-printers-button').addEventListener('click',
                                               showRegisterOverlay);
@@ -324,6 +344,7 @@ cr.define('local_discovery', function() {
     uber.invokeMethodOnParent('setTitle', {title: title});
 
     chrome.send('start');
+    recordUmaAction('DevicesPage_Opened');
     requestPrinterList();
   });
 
