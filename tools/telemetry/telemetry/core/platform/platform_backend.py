0ba0b0e50d4256842544099772ab9c60b874cba4
edmundyan@chromium.org <edmundyan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-07 04:59:21 +0000

    Add cpu_stats for the browser
    Implementing linux_backend for now.
    
    BUG=263959
    
    Review URL: https://chromiumcodereview.appspot.com/23717016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221897 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/platform/platform_backend.py
+++ tools/telemetry/telemetry/core/platform/platform_backend.py
@@ -32,6 +32,12 @@ class PlatformBackend(object):
   def GetSystemCommitCharge(self):
     raise NotImplementedError()
 
+  def GetCpuStats(self, pid):  # pylint: disable=W0613
+    return {}
+
+  def GetCpuTimestamp(self):  # pylint: disable=W0613
+    return {}
+
   def GetMemoryStats(self, pid):  # pylint: disable=W0613
     return {}
 
