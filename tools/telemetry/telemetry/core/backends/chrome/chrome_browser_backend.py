521193766f1e35556df4b67f4ab835dc5734ca45
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-07 00:56:03 +0000

    dont_override_profile, profile_type, profile_dir, extra_browser_args, extra_wpr_args moved to browser_options.
    BUG=269131
    TEST=manual
    
    Review URL: https://chromiumcodereview.appspot.com/23749004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221841 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/backends/chrome/chrome_browser_backend.py
+++ tools/telemetry/telemetry/core/backends/chrome/chrome_browser_backend.py
@@ -47,7 +47,7 @@ class ChromeBrowserBackend(browser_backend.BrowserBackend):
     self.webpagereplay_remote_http_port = self.webpagereplay_local_http_port
     self.webpagereplay_remote_https_port = self.webpagereplay_local_https_port
 
-    if (finder_options.dont_override_profile and
+    if (self.browser_options.dont_override_profile and
         not options_for_unittests.AreSet()):
       sys.stderr.write('Warning: Not overriding profile. This can cause '
                        'unexpected effects due to profile-specific settings, '
@@ -75,7 +75,7 @@ class ChromeBrowserBackend(browser_backend.BrowserBackend):
 
   def GetBrowserStartupArgs(self):
     args = []
-    args.extend(self.finder_options.extra_browser_args)
+    args.extend(self.browser_options.extra_browser_args)
     args.append('--disable-background-networking')
     args.append('--metrics-recording-only')
     args.append('--no-first-run')
