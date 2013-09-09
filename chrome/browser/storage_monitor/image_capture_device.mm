ab72d0a0439dff235f134254ad3381dee0705744
gbillock@chromium.org <gbillock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-09 22:32:10 +0000

    [StorageMonitor] Handle EjectDevice call for MTP devices
    This change includes intercept code to dispatch eject calls on
    MTP devices for all platforms. By and large, MTP devices don't need
    specific ejection APIs to be called -- the devices are safe to remove
    without notice. So the implementations mainly just simulate their
    removal and signal OK to the caller. When the device is then actually
    removed, the removal will be swallowed without additional notification.
    
    R=thestig@chromium.org
    BUG=257179
    
    Review URL: https://chromiumcodereview.appspot.com/23383009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222112 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/storage_monitor/image_capture_device.mm
+++ chrome/browser/storage_monitor/image_capture_device.mm
@@ -93,6 +93,10 @@ base::FilePath PathForCameraItem(ICCameraItem* item) {
   listener_.reset();
 }
 
+- (void)eject {
+  [camera_ requestEjectOrDisconnect];
+}
+
 - (void)downloadFile:(const std::string&)name
            localPath:(const base::FilePath&)localPath {
   DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
--- chrome/browser/storage_monitor/storage_monitor_win.cc
+++ chrome/browser/storage_monitor/storage_monitor_win.cc
@@ -99,7 +99,6 @@ void StorageMonitorWin::EjectDevice(
     const std::string& device_id,
     base::Callback<void(EjectStatus)> callback) {
   StorageInfo::Type type;
-
   if (!StorageInfo::CrackDeviceId(device_id, &type, NULL)) {
     callback.Run(EJECT_FAILURE);
     return;
