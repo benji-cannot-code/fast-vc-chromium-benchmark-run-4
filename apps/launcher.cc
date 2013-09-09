6a972baa778a1cc8c86feddfb0684c66444d2ff6
hidehiko@chromium.org <hidehiko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-09 10:28:51 +0000

    Replace direct use of DriveIntegrationServiceFactory with drive::util::GetFileSystemByProfile.
    DriveIntegrationServiceFactory::GetForProfile will return the instance
    regardless of whether the Drive is enabled or not. GetFileSystemByProfile will
    still be looking at enabled state.
    
    BUG=284972
    TEST=Ran unit_tests
    
    Review URL: https://chromiumcodereview.appspot.com/23739005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221998 0039d316-1c4b-4281-b951-d872f2087c98

--- apps/launcher.cc
+++ apps/launcher.cc
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_util.h"
 
 #if defined(OS_CHROMEOS)
-#include "chrome/browser/chromeos/drive/drive_integration_service.h"
 #include "chrome/browser/chromeos/drive/file_errors.h"
 #include "chrome/browser/chromeos/drive/file_system_interface.h"
 #include "chrome/browser/chromeos/drive/file_system_util.h"
@@ -195,14 +194,14 @@ class PlatformAppPathLauncher
   void GetMimeTypeAndLaunchForDriveFile() {
     DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
 
-    drive::DriveIntegrationService* service =
-        drive::DriveIntegrationServiceFactory::FindForProfile(profile_);
-    if (!service) {
+    drive::FileSystemInterface* file_system =
+        drive::util::GetFileSystemByProfile(profile_);
+    if (!file_system) {
       LaunchWithNoLaunchData();
       return;
     }
 
-    service->file_system()->GetFileByPath(
+    file_system->GetFileByPath(
         drive::util::ExtractDrivePath(file_path_),
         base::Bind(&PlatformAppPathLauncher::OnGotDriveFile, this));
   }
