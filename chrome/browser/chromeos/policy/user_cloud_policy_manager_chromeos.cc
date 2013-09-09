c11ab09d231df4f77741d271dde14f34300e9a50
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-09 10:40:38 +0000

    Fix order of UMA sampling when handling errors on initial policy fetch.
    BUG=271321
    
    Review URL: https://chromiumcodereview.appspot.com/23847005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221999 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/policy/user_cloud_policy_manager_chromeos.cc
+++ chrome/browser/chromeos/policy/user_cloud_policy_manager_chromeos.cc
@@ -236,12 +236,11 @@ void UserCloudPolicyManagerChromeOS::OnRegistrationStateChanged(
 void UserCloudPolicyManagerChromeOS::OnClientError(
     CloudPolicyClient* cloud_policy_client) {
   DCHECK_EQ(client(), cloud_policy_client);
-  CancelWaitForPolicyFetch();
-
   if (wait_for_policy_fetch_) {
     UMA_HISTOGRAM_SPARSE_SLOWLY(kUMAInitialFetchClientError,
                                 cloud_policy_client->status());
   }
+  CancelWaitForPolicyFetch();
 }
 
 void UserCloudPolicyManagerChromeOS::OnComponentCloudPolicyRefreshNeeded() {
@@ -307,7 +306,6 @@ void UserCloudPolicyManagerChromeOS::OnOAuth2PolicyTokenFetched(
 
 void UserCloudPolicyManagerChromeOS::OnInitialPolicyFetchComplete(
     bool success) {
-
   const base::Time now = base::Time::Now();
   UMA_HISTOGRAM_TIMES(kUMAInitialFetchDelayPolicyFetch,
                       now - time_client_registered_);
