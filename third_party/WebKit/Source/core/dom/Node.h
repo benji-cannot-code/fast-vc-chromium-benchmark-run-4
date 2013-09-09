5d42a16285cf255214fcbbbbf1681064024663a7
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-09 00:00:30 +0000

    Remove ShouldSetAttached flag to lazyAttach
    This flag does not seem needed, there's no apparent reason that frames
    would want to do setNeedsStyleRecalc(LazyAttachStyleChange) but not
    setAttached().
    
    Review URL: https://chromiumcodereview.appspot.com/24066002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157423 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Node.h
+++ third_party/WebKit/Source/core/dom/Node.h
@@ -408,12 +408,8 @@ public:
     bool isV8CollectableDuringMinorGC() const { return getFlag(V8CollectableDuringMinorGCFlag); }
     void setV8CollectableDuringMinorGC(bool flag) { setFlag(flag, V8CollectableDuringMinorGCFlag); }
 
-    enum ShouldSetAttached {
-        SetAttached,
-        DoNotSetAttached
-    };
-    void lazyAttach(ShouldSetAttached = SetAttached);
-    void lazyReattach(ShouldSetAttached = SetAttached);
+    void lazyAttach();
+    void lazyReattach();
 
     virtual void setFocus(bool flag);
     virtual void setActive(bool flag = true, bool pause = false);
@@ -914,7 +910,7 @@ inline void Node::lazyReattachIfAttached()
         lazyReattach();
 }
 
-inline void Node::lazyReattach(ShouldSetAttached shouldSetAttached)
+inline void Node::lazyReattach()
 {
     if (styleChangeType() == LazyAttachStyleChange)
         return;
@@ -924,7 +920,7 @@ inline void Node::lazyReattach(ShouldSetAttached shouldSetAttached)
 
     if (attached())
         detach(context);
-    lazyAttach(shouldSetAttached);
+    lazyAttach();
 }
 
 inline bool shouldRecalcStyle(StyleRecalcChange change, const Node* node)
