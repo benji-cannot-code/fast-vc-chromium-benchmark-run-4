f7c8905b2f05ceb30e9e1a1dc6205b0a7513224e
tburkard@chromium.org <tburkard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-09 20:29:36 +0000

    Add options to disable each of the two aspects of the prerender service (current URL based hinting and candidate URL upload). R=jam@chromium.org
    Review URL: https://codereview.chromium.org/23506038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222082 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prerender/prerender_field_trial.cc
+++ chrome/browser/prerender/prerender_field_trial.cc
@@ -48,6 +48,10 @@ const char kPrerenderLaunchKeyName[] = "PrerenderLaunch";
 const char kPrerenderAlwaysControlKeyName[] = "PrerenderAlwaysControl";
 const char kPrerenderQueryPrerenderServiceKeyName[] =
     "PrerenderQueryPrerenderService";
+const char kPrerenderQueryPrerenderServiceCurrentURLKeyName[] =
+    "PrerenderQueryPrerenderServiceCurrentURL";
+const char kPrerenderQueryPrerenderServiceCandidateURLsKeyName[] =
+    "PrerenderQueryPrerenderServiceCandidateURLs";
 const char kPrerenderServiceBehaviorIDKeyName[] = "PrerenderServiceBehaviorID";
 const char kPrerenderServiceFetchTimeoutKeyName[] =
     "PrerenderServiceFetchTimeoutMs";
@@ -372,6 +376,16 @@ bool ShouldQueryPrerenderService(Profile* profile) {
   return IsUnencryptedSyncEnabled(profile) &&
       GetLocalPredictorSpecValue(kPrerenderQueryPrerenderServiceKeyName) ==
       kEnabledGroup;
+}
+
+bool ShouldQueryPrerenderServiceForCurrentURL() {
+  return GetLocalPredictorSpecValue(
+      kPrerenderQueryPrerenderServiceCurrentURLKeyName) != kDisabledGroup;
+}
+
+bool ShouldQueryPrerenderServiceForCandidateURLs() {
+  return GetLocalPredictorSpecValue(
+      kPrerenderQueryPrerenderServiceCandidateURLsKeyName) != kDisabledGroup;
 }
 
 string GetPrerenderServiceURLPrefix() {
