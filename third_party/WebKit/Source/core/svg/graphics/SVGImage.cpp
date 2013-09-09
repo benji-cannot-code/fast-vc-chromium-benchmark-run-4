7c06e621e2f02979581c05d93745525df1ba4b25
pdr@chromium.org <pdr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-09 04:37:48 +0000

    Assert that loading is complete in SVGImage::hasSingleSecurityOrigin()
    This patch adds an assert that the "inner page" in an SVGImage has
    loaded and does not have any pending subresource loads.
    
    NOTRY=true
    BUG=249037
    
    Review URL: https://chromiumcodereview.appspot.com/22999031
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157433 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/graphics/SVGImage.cpp
+++ third_party/WebKit/Source/core/svg/graphics/SVGImage.cpp
@@ -85,6 +85,9 @@ bool SVGImage::currentFrameHasSingleSecurityOrigin() const
         return true;
 
     Frame* frame = m_page->mainFrame();
+
+    RELEASE_ASSERT(frame->document()->loadEventFinished());
+
     SVGSVGElement* rootElement = toSVGDocument(frame->document())->rootElement();
     if (!rootElement)
         return true;
