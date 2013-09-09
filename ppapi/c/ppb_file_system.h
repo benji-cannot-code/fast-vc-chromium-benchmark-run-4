8b7b637a437ffc4a237b534d53cf41d945b3302c
dalecurtis@google.com <dalecurtis@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-09 22:35:47 +0000

    Add PPAPI interfaces for platform verification.
    Interfaces only at this point.  Implementation to follow.  API mirrors
    that which is provided to the CDMs in http://crrev.com/221019
    
    CanChallengePlatform() can be synchronous since it will just check a
    command line flag passed to the process.
    
    BUG=270294
    TEST=none
    R=ddorwin@chromium.org, dmichael@chromium.org
    
    Review URL: https://codereview.chromium.org/23569005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222113 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/c/ppb_file_system.h
+++ ppapi/c/ppb_file_system.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * found in the LICENSE file.
  */
 
-/* From ppb_file_system.idl modified Thu May  2 16:44:38 2013. */
+/* From ppb_file_system.idl modified Thu Jun 13 14:30:40 2013. */
 
 #ifndef PPAPI_C_PPB_FILE_SYSTEM_H_
 #define PPAPI_C_PPB_FILE_SYSTEM_H_
@@ -67,9 +67,7 @@ struct PPB_FileSystem_1_0 {
    * @param[in] expected_size The expected size of the file system. Note that
    * this does not request quota; to do that, you must either invoke
    * requestQuota from JavaScript:
-
-
-    * http://www.html5rocks.com/en/tutorials/file/filesystem/#toc-requesting-quota
+   * http://www.html5rocks.com/en/tutorials/file/filesystem/#toc-requesting-quota
    * or set the unlimitedStorage permission for Chrome Web Store apps:
    * http://code.google.com/chrome/extensions/manifest.html#permissions
    *
