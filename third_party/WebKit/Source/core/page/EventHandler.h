143fb7cf8db5620d65d1ff04e8131a4c09b6697b
dcheng@chromium.org <dcheng@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-09 20:34:37 +0000

    Remove EventHandler::handleMouseUp().
    This used to call EventHandler::eventActivatedView(), which was removed
    in r149444.
    
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/23600018
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157471 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/EventHandler.h
+++ third_party/WebKit/Source/core/page/EventHandler.h
@@ -255,7 +255,6 @@ private:
 
     bool handleDrag(const MouseEventWithHitTestResults&, CheckDragHysteresis);
     bool tryStartDrag(const MouseEventWithHitTestResults&);
-    bool handleMouseUp(const MouseEventWithHitTestResults&);
     void clearDragState();
 
     bool dispatchDragSrcEvent(const AtomicString& eventType, const PlatformMouseEvent&);
