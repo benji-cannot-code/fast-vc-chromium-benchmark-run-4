52ae4eb1db9029448c7bb42fef4d37bbfd60fe45
rouslan@chromium.org <rouslan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-09 21:20:30 +0000

    Revert 222074 "Refactor KeySystems code to call a function to po..."
    Revision 222074 did not go through the commit queue. Failing
    contet_unittests on win-aura:
    
      KeySystemsTest.ClearKey_Basic
      KeySystemsTest.ClearKey_Parent
      KeySystemsTest.ExternalClearKey_Parent
      KeySystemsTest.Widevine_Basic
      KeySystemsTest.Widevine_Parent
    
    > Refactor KeySystems code to call a function to populate the info. (re-land r221932)
    > 
    > This allows KeySystems to call GetContentClient()->AddKeySystems() so we can
    > move key system information to chrome/. The new ContentClient API is defined and
    > called, but we still rely on key_systems_info.cc.
    > 
    > BUG=224793
    > TEST=Existing content_unittests and content_browsertests pass.
    > TBR=jam@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/23484019
    
    TBR=ddorwin@chromium.org
    
    Review URL: https://codereview.chromium.org/23708020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222091 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/renderer/content_renderer_client.h
+++ content/public/renderer/content_renderer_client.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_PUBLIC_RENDERER_CONTENT_RENDERER_CLIENT_H_
 
 #include <string>
-#include <vector>
 
 #include "base/memory/weak_ptr.h"
 #include "base/strings/string16.h"
@@ -53,7 +52,6 @@ namespace content {
 
 class RenderView;
 class SynchronousCompositor;
-struct KeySystemInfo;
 struct WebPluginInfo;
 
 // Embedder API for participating in renderer logic.
@@ -246,10 +244,6 @@ class CONTENT_EXPORT ContentRendererClient {
 
   // Returns true if the page at |url| can use Pepper MediaStream APIs.
   virtual bool AllowPepperMediaStreamAPI(const GURL& url);
-
-  // Gives the embedder a chance to register the key system(s) it supports by
-  // populating |key_systems|.
-  virtual void AddKeySystems(std::vector<KeySystemInfo>* key_systems);
 
   // Returns true if we should report a detailed message (including a stack
   // trace) for console [logs|errors|exceptions]. |source| is the WebKit-
