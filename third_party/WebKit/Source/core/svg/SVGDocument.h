ff8dae58f5df7ea2e973df15b23041afa5c66d73
seokju.kwon@gmail.com <seokju.kwon@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-09 07:09:21 +0000

    Remove obsolete forward declarations from SVGDocument.
    Additionally, add explicit keyword to constructor that takes one argument.
    
    BUG=None
    
    Review URL: https://chromiumcodereview.appspot.com/24069002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157442 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGDocument.h
+++ third_party/WebKit/Source/core/svg/SVGDocument.h
@@ -27,8 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-class DOMImplementation;
-class SVGElement;
 class SVGSVGElement;
 
 class SVGDocument FINAL : public Document {
@@ -49,7 +47,7 @@ public:
     void updatePan(const FloatPoint& pos) const;
 
 private:
-    SVGDocument(const DocumentInit&);
+    explicit SVGDocument(const DocumentInit&);
 
     virtual bool childShouldCreateRenderer(const Node& child) const;
 
