6604e89b8c143c6dac926b1af357fa1ae49f504c
stevet@chromium.org <stevet@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-10 15:31:56 +0000

    Revert 222257 "Revert 222235 ""Redirecting URLs to Packaged Apps..."
    Reason for revert: The first revert didn't actually fix the issues with the Chrome Frame net test failures, so I'm putting the patch back in.
    
    > Revert 222235 ""Redirecting URLs to Packaged Apps" implementatio..."
    > 
    > > "Redirecting URLs to Packaged Apps" implementation: revised
    > > 
    > > Design proposal: https://docs.google.com/document/d/1r-RoOv2URfZBYrT_B6notQ6MeMqZRd1EP1AITuzJCAc/edit?usp=sharing
    > > 
    > > * Support for url_handlers in manifest
    > > * New kind of onLaunched event with navigation info in launch data
    > > * Intercept/redirect top-level browser-initiated navigations (bookmarks, omnibox, etc.)
    > > * Intercept/redirect top-frame navigations in tabs and app windows. 
    > > 
    > > This is a rework of https://codereview.chromium.org/22944002/.
    > > 
    > > BUG=111422
    > > 
    > > Review URL: https://chromiumcodereview.appspot.com/23847004
    > 
    > Reason for revert:
    > This is the most likely cause of the chrome_frame_net_tests failures (various tests), since no other changes in the blamelist were touching URLRequest bits.
    > 
    > First failure here: http://build.chromium.org/p/chromium.win/builders/Chrome%20Frame%20Tests%20%28ie6%29/builds/46826
    > 
    > TBR=sergeygs@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/23600025
    
    TBR=stevet@chromium.org
    
    Review URL: https://codereview.chromium.org/23597024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222258 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/platform_apps/url_handlers/launchers/call_window_open/main.js
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.test.getConfig(function(config) {
+  var port = config ? config.testServer.port : '0';
+  var href =
+      'http://localhost:' + port +
+      '/extensions/platform_apps/url_handlers/common/target.html';
+  // This click should be intercepted and redirected to the handler app
+  // (pre-installed by the CPP side of the test before launching this).
+  window.open(href);
+
+  chrome.test.sendMessage("Launcher done");
+});
