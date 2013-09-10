9cd775605bb26b12d989fa4d0e3281adb09c0480
cevans@chromium.org <cevans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-10 04:18:40 +0000

    Revert 157417 "Reorder the destruction sequence of Vector() to d..."
    > Reorder the destruction sequence of Vector() to do less work.
    > 
    > This is a subset of previously reviewed CL,
    > https://codereview.chromium.org/23531010/ that had to be reverted due to
    > unknown reasons for a performance regression. Hence, relanding in pieces.
    > 
    > R=abarth@chromium.org, tsepez@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/23451016
    
    TBR=cevans@chromium.org
    
    Review URL: https://codereview.chromium.org/23726029
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157496 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/Vector.h
+++ third_party/WebKit/Source/wtf/Vector.h
@@ -280,6 +280,19 @@ static const size_t kInitialVectorSize = WTF_VECTOR_INITIAL_SIZE;
             m_buffer = static_cast<T*>(fastRealloc(m_buffer, sizeToAllocate));
         }
 
+        void deallocateBuffer(T* bufferToDeallocate)
+        {
+            if (!bufferToDeallocate)
+                return;
+
+            if (m_buffer == bufferToDeallocate) {
+                m_buffer = 0;
+                m_capacity = 0;
+            }
+
+            fastFree(bufferToDeallocate);
+        }
+
         T* buffer() { return m_buffer; }
         const T* buffer() const { return m_buffer; }
         size_t capacity() const { return m_capacity; }
@@ -330,21 +343,6 @@ static const size_t kInitialVectorSize = WTF_VECTOR_INITIAL_SIZE;
         ~VectorBuffer()
         {
             deallocateBuffer(buffer());
-            m_buffer = 0;
-        }
-
-        void deallocateBuffer(T* bufferToDeallocate)
-        {
-            if (LIKELY(!bufferToDeallocate))
-                return;
-
-            fastFree(bufferToDeallocate);
-        }
-
-        void clearBufferPointer()
-        {
-            m_buffer = 0;
-            m_capacity = 0;
         }
 
         void swap(VectorBuffer<T, 0>& other)
@@ -358,6 +356,7 @@ static const size_t kInitialVectorSize = WTF_VECTOR_INITIAL_SIZE;
         using Base::allocateBuffer;
         using Base::shouldReallocateBuffer;
         using Base::reallocateBuffer;
+        using Base::deallocateBuffer;
 
         using Base::buffer;
         using Base::capacity;
@@ -409,14 +408,7 @@ static const size_t kInitialVectorSize = WTF_VECTOR_INITIAL_SIZE;
         {
             if (LIKELY(bufferToDeallocate == inlineBuffer()))
                 return;
-
-            fastFree(bufferToDeallocate);
-        }
-
-        void clearBufferPointer()
-        {
-            m_buffer = 0;
-            m_capacity = 0;
+            Base::deallocateBuffer(bufferToDeallocate);
         }
 
         bool shouldReallocateBuffer(size_t newCapacity) const
@@ -506,7 +498,7 @@ static const size_t kInitialVectorSize = WTF_VECTOR_INITIAL_SIZE;
 
         ~Vector()
         {
-            if (UNLIKELY(m_size))
+            if (m_size)
                 shrink(0);
         }
 
@@ -883,8 +875,6 @@ static const size_t kInitialVectorSize = WTF_VECTOR_INITIAL_SIZE;
             Base::allocateBuffer(newCapacity);
             if (begin() != oldBuffer)
                 TypeOperations::move(oldBuffer, oldEnd, begin());
-        } else {
-            Base::clearBufferPointer();
         }
 
         Base::deallocateBuffer(oldBuffer);
