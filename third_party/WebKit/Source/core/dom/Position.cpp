f6898fc2722c49603a07b980121ac0bcb85a98dc
dmazzoni@chromium.org <dmazzoni@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-10 04:09:01 +0000

    Allow selection to skip over contenteditable
    BUG=264539
    
    Review URL: https://chromiumcodereview.appspot.com/20572005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157492 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Position.cpp
+++ third_party/WebKit/Source/core/dom/Position.cpp
@@ -38,6 +38,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/editing/htmlediting.h"
 #include "core/html/HTMLHtmlElement.h"
 #include "core/html/HTMLTableElement.h"
+#include "core/page/Frame.h"
+#include "core/page/Settings.h"
 #include "core/platform/Logging.h"
 #include "core/rendering/InlineIterator.h"
 #include "core/rendering/InlineTextBox.h"
@@ -900,8 +902,11 @@ bool Position::isCandidate() const
                 return atFirstEditingPositionForNode() && !Position::nodeIsUserSelectNone(deprecatedNode());
             return m_anchorNode->rendererIsEditable() && !Position::nodeIsUserSelectNone(deprecatedNode()) && atEditingBoundary();
         }
-    } else
-        return m_anchorNode->rendererIsEditable() && !Position::nodeIsUserSelectNone(deprecatedNode()) && atEditingBoundary();
+    } else {
+        Frame* frame = m_anchorNode->document().frame();
+        bool caretBrowsing = frame->settings() && frame->settings()->caretBrowsingEnabled();
+        return (caretBrowsing || m_anchorNode->rendererIsEditable()) && !Position::nodeIsUserSelectNone(deprecatedNode()) && atEditingBoundary();
+    }
 
     return false;
 }
