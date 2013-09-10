a6ff72a45aa32457fd348634d46856337d1e5156
jered@chromium.org <jered@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-10 02:16:14 +0000

    InstantExtended(gtk): Hide top match if told to so.
    CL 17114003 and 22410004 added a way for Mac and views to hide the top
    omnibox match when it is a verbatim match we do not want to show. This
    implements that for GTK too so it'll work on all desktop platforms.
    
    BUG=252823
    TEST=Manually. Run with no flags and verify that when you type 'faceb'
    in the omnibox, the popup shows a search suggestion for 'faceb'. Run
    again with the flags
    --enable-instant-extended-api
    --force-fieldtrials='InstantExtended/Group1 hide_verbatim:1/'
    and verify that you do not see this suggestion. Also check that with the
    flags set, hovering on the top suggestion works and that there are no
    off-by-one problems when clicking on suggestions.
    
    Review URL: https://chromiumcodereview.appspot.com/22679003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222186 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/omnibox/omnibox_popup_view_gtk.h
+++ chrome/browser/ui/gtk/omnibox/omnibox_popup_view_gtk.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/window_open_disposition.h"
 #include "ui/gfx/font.h"
 
+class AutocompleteResult;
 class GtkThemeService;
 class OmniboxEditModel;
 class OmniboxPopupModel;
@@ -41,6 +42,9 @@ class OmniboxPopupViewGtk : public OmniboxPopupView,
                       GtkWidget* location_bar);
   virtual ~OmniboxPopupViewGtk();
 
+  // Initializes the view.
+  virtual void Init();
+
   // Overridden from OmniboxPopupView:
   virtual bool IsOpen() const OVERRIDE;
   virtual void InvalidateLine(size_t line) OVERRIDE;
@@ -53,6 +57,23 @@ class OmniboxPopupViewGtk : public OmniboxPopupView,
   virtual void Observe(int type,
                        const content::NotificationSource& source,
                        const content::NotificationDetails& details) OVERRIDE;
+
+ protected:
+  // Convert a y-coordinate to the closest line / result.
+  size_t LineFromY(int y) const;
+
+  // Return a Rect for the space for a result line.  This excludes the border,
+  // but includes the padding.  This is the area that is colored for a
+  // selection.
+  gfx::Rect GetRectForLine(size_t line, int width) const;
+
+  // Returns the number of hidden matches at the top of the popup. This is
+  // non-zero when a verbatim match like search-what-you-typed is present but
+  // should not be shown.
+  size_t GetHiddenMatchCount() const;
+
+  // Returns the current autocomplete result.
+  virtual const AutocompleteResult& GetResult() const;
 
  private:
   // Be friendly for unit tests.
@@ -67,18 +88,16 @@ class OmniboxPopupViewGtk : public OmniboxPopupView,
       const GdkColor* url_color,
       const std::string& prefix_text);
 
-  void Show(size_t num_results);
-  void Hide();
+  virtual void Show(size_t num_results);
+  virtual void Hide();
 
   // Restack the popup window directly above the browser's toplevel window.
   void StackWindow();
 
-  // Convert a y-coordinate to the closest line / result.
-  size_t LineFromY(int y);
-
   // Accept a line of the results, for example, when the user clicks a line.
   void AcceptLine(size_t line, WindowOpenDisposition disposition);
 
+  // Returns the appropriate icon to display beside |match|.
   gfx::Image IconForMatch(const AutocompleteMatch& match,
                           bool selected,
                           bool is_selected_keyword);
