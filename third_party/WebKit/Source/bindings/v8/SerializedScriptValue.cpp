5c9d093fd6ab156490a8e524881f40725c76765b
ch.dumez@sisa.samsung.com <ch.dumez@sisa.samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-10 21:08:57 +0000

    Always pass v8::Isolate to v8::Number::New()
    Always pass v8::Isolate to v8::Number::New() as this is now the preferred way
    of constructing a Number:
    https://code.google.com/p/v8/issues/detail?id=2487
    
    R=haraken
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/23799009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157550 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/SerializedScriptValue.cpp
+++ third_party/WebKit/Source/bindings/v8/SerializedScriptValue.cpp
@@ -1701,7 +1701,7 @@ private:
         double number;
         if (!doReadNumber(&number))
             return false;
-        *value = v8::Number::New(number);
+        *value = v8::Number::New(m_isolate, number);
         return true;
     }
 
