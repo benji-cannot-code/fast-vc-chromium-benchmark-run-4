acf8285c181fe86072bda36e3889a80b0e1b695f
jsbell@chromium.org <jsbell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-10 17:32:47 +0000

    Use Vector as SSV's Writer buffer, to avoid mysterious realloc issue
    A Blink@155225 changed SSV to serialize using StringBuffer instead of
    Vector, but this seems to hit an issue with fastRealloc calls. Revert
    back to Vector while we try and track down the root cause.
    
    BUG=266837
    R=abarth@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/23621033
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157531 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/SerializedScriptValue.cpp
+++ third_party/WebKit/Source/bindings/v8/SerializedScriptValue.cpp
@@ -537,7 +537,7 @@ public:
     {
         COMPILE_ASSERT(sizeof(BufferValueType) == 2, BufferValueTypeIsTwoBytes);
         fillHole();
-        String data = String::adopt(m_buffer);
+        String data = String(m_buffer.data(), m_buffer.size());
         data.impl()->truncateAssumingIsolated((m_position + 1) / sizeof(BufferValueType));
         return data;
     }
@@ -647,18 +647,10 @@ private:
         m_position += length;
     }
 
-    static unsigned expandedCapacity(unsigned capacity, unsigned newLength)
-    {
-        static const unsigned minimumCapacity = 16;
-        return std::max(capacity, std::max(minimumCapacity, newLength * 2));
-    }
-
     void ensureSpace(unsigned extra)
     {
         COMPILE_ASSERT(sizeof(BufferValueType) == 2, BufferValueTypeIsTwoBytes);
-        unsigned oldCapacity = m_buffer.length() * sizeof(BufferValueType);
-        unsigned newCapacity = expandedCapacity(oldCapacity, m_position + extra);
-        m_buffer.resize((newCapacity + 1) / sizeof(BufferValueType)); // "+ 1" to round up.
+        m_buffer.resize((m_position + extra + 1) / sizeof(BufferValueType)); // "+ 1" to round up.
     }
 
     void fillHole()
@@ -672,7 +664,7 @@ private:
 
     uint8_t* byteAt(int position)
     {
-        return reinterpret_cast<uint8_t*>(m_buffer.characters()) + position;
+        return reinterpret_cast<uint8_t*>(m_buffer.data()) + position;
     }
 
     int v8StringWriteOptions()
@@ -680,7 +672,7 @@ private:
         return v8::String::NO_NULL_TERMINATION;
     }
 
-    StringBuffer<BufferValueType> m_buffer;
+    Vector<BufferValueType> m_buffer;
     unsigned m_position;
     v8::Isolate* m_isolate;
 };
