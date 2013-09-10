fd784ee5088c2dc1c28afdaaef5a6b800857cedb
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-10 16:36:38 +0000

    Change the signature of WebCryptoImpl::digest().
    Use an "unsigned int" rather than a "size_t". This is the chromium side for https://codereview.chromium.org/23537027/.
    
    Review URL: https://chromiumcodereview.appspot.com/23484028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222278 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/webcrypto_impl_nss.cc
+++ content/renderer/webcrypto_impl_nss.cc
@@ -14,10 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-bool WebCryptoImpl::digestInternal(
+bool WebCryptoImpl::DigestInternal(
     const WebKit::WebCryptoAlgorithm& algorithm,
     const unsigned char* data,
-    size_t data_size,
+    unsigned data_size,
     WebKit::WebArrayBuffer* buffer) {
   HASH_HashType hash_type = HASH_AlgNULL;
 
--- content/renderer/webcrypto_impl_openssl.cc
+++ content/renderer/webcrypto_impl_openssl.cc
@@ -7,10 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-bool WebCryptoImpl::digestInternal(
+bool WebCryptoImpl::DigestInternal(
     const WebKit::WebCryptoAlgorithm& algorithm,
     const unsigned char* data,
-    size_t data_size,
+    unsigned data_size,
     WebKit::WebArrayBuffer* buffer) {
   // TODO(bryaneyler): Placeholder for OpenSSL implementation.
   // Issue http://crbug.com/267888.
