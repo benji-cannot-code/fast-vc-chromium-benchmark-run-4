85329be5b36f2d4d8346aa40e096fb9f001a7522
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-10 16:43:34 +0000

    Run Origin-related operations at higher priority in SyncFS
    requestFileSystem latency (while heavy sync is runing in the background) in msec
    
    before this change (20 runs):
    ave: 1437.04
    min: 422.638
    max: 7106.868
    
    after this change (20 runs):
    ave: 730.764
    min: 343.862
    max: 1133.181
    
    BUG=254810
    
    Review URL: https://chromiumcodereview.appspot.com/23710018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222282 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/sync_task_manager.h
+++ chrome/browser/sync_file_system/sync_task_manager.h
@@ -6,7 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_SYNC_FILE_SYSTEM_SYNC_TASK_MANAGER_H_
 #define CHROME_BROWSER_SYNC_FILE_SYSTEM_SYNC_TASK_MANAGER_H_
 
-#include <deque>
+#include <queue>
+#include <vector>
 
 #include "base/callback.h"
 #include "base/memory/scoped_ptr.h"
@@ -29,6 +30,12 @@ class SyncTaskManager
   class TaskToken;
   typedef base::Callback<void(const SyncStatusCallback& callback)> Task;
 
+  enum Priority {
+    PRIORITY_LOW,
+    PRIORITY_MED,
+    PRIORITY_HIGH,
+  };
+
   class Client {
    public:
     virtual ~Client() {}
@@ -49,10 +56,16 @@ class SyncTaskManager
   // service status. This should not be called more than once.
   void Initialize(SyncStatusCode status);
 
+  // Schedules a task at PRIORITY_MED.
   void ScheduleTask(const Task& task,
                     const SyncStatusCallback& callback);
   void ScheduleSyncTask(scoped_ptr<SyncTask> task,
                         const SyncStatusCallback& callback);
+
+  // Schedules a task at the given priority.
+  void ScheduleTaskAtPriority(const Task& task,
+                              Priority priority,
+                              const SyncStatusCallback& callback);
 
   // Runs the posted task only when we're idle.
   void ScheduleTaskIfIdle(const Task& task);
@@ -62,6 +75,21 @@ class SyncTaskManager
                       SyncStatusCode status);
 
  private:
+  struct PendingTask {
+    base::Closure task;
+    Priority priority;
+    int64 seq;
+
+    PendingTask();
+    PendingTask(const base::Closure& task, Priority pri, int seq);
+    ~PendingTask();
+  };
+
+  struct PendingTaskComparator {
+    bool operator()(const PendingTask& left,
+                    const PendingTask& right) const;
+  };
+
   // This should be called when an async task needs to get a task token.
   scoped_ptr<TaskToken> GetToken(const tracked_objects::Location& from_here);
 
@@ -71,13 +99,17 @@ class SyncTaskManager
       scoped_ptr<TaskToken> token,
       const SyncStatusCallback& callback);
 
+  void PushPendingTask(const base::Closure& closure, Priority priority);
+
   base::WeakPtr<Client> client_;
 
   SyncStatusCode last_operation_status_;
   scoped_ptr<SyncTask> running_task_;
-  std::deque<base::Closure> pending_tasks_;
   SyncStatusCallback current_callback_;
 
+  std::priority_queue<PendingTask, std::vector<PendingTask>,
+                      PendingTaskComparator> pending_tasks_;
+  int64 pending_task_seq_;
 
   // Absence of |token_| implies a task is running. Incoming tasks should
   // wait for the task to finish in |pending_tasks_| if |token_| is null.
