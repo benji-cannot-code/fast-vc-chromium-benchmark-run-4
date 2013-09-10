fa00efa68ca43abf6e0310f1fd408af6cfa1d0bd
jamiewalch@chromium.org <jamiewalch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-10 17:31:18 +0000

    Add support for i18n-title.
    Previously, we only had one tool-tip defined in the HTML markup, and its l10n
    was hard-coded. This CL adds a generic mechanism, uses it for the local host
    edit button and adds a tool-tip to the host list refresh button.
    
    Review URL: https://chromiumcodereview.appspot.com/23923009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222295 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/webapp/l10n.js
+++ remoting/webapp/l10n.js
@@ -7,8 +7,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 var l10n = l10n || {};
 
 /**
+ * Localize a tag, returning the tag itself and logging an error if no
+ * translation exists.
+ *
+ * @param {string} tag The localization tag.
+ * @param {(string|Array)=} opt_substitutions An optional set of substitution
+ *     strings corresponding to the "placeholders" attributes in messages.json.
+ * @return {string} The translated tag.
+ */
+l10n.getTranslationOrError = function(tag, opt_substitutions) {
+  var translation = chrome.i18n.getMessage(tag, opt_substitutions);
+  if (translation) {
+    return translation;
+  }
+  console.error('Missing translation for "' + tag + '"');
+  return tag;
+};
+
+/**
  * Localize an element by setting its innerText according to the specified tag
  * and an optional set of substitutions.
+ *
  * @param {Element} element The element to localize.
  * @param {string} tag The localization tag.
  * @param {(string|Array)=} opt_substitutions An optional set of substitution
@@ -19,11 +38,7 @@ var l10n = l10n || {};
  */
 l10n.localizeElementFromTag = function(element, tag, opt_substitutions,
                                        opt_asHtml) {
-  var translation = chrome.i18n.getMessage(tag, opt_substitutions);
-  if (!translation) {
-    console.error('Missing translation for "' + tag + '":', element);
-    translation = tag;  // Make errors more obvious
-  }
+  var translation = l10n.getTranslationOrError(tag, opt_substitutions);
   if (opt_asHtml) {
     element.innerHTML = translation;
   } else {
@@ -35,6 +50,7 @@ l10n.localizeElementFromTag = function(element, tag, opt_substitutions,
 /**
  * Localize an element by setting its innerText according to its i18n-content
  * attribute, and an optional set of substitutions.
+ *
  * @param {Element} element The element to localize.
  * @param {(string|Array)=} opt_substitutions An optional set of substitution
  *     strings corresponding to the "placeholders" attributes in messages.json.
@@ -57,7 +73,7 @@ l10n.localizeElement = function(element, opt_substitutions, opt_asHtml) {
  * HTML iff there are any substitutions.
  */
 l10n.localize = function() {
-  var elements = document.querySelectorAll('[i18n-content]');
+  var elements = document.querySelectorAll('[i18n-content],[i18n-title]');
   for (var i = 0; i < elements.length; ++i) {
     /** @type {Element} */ var element = elements[i];
     var substitutions = [];
@@ -79,10 +95,12 @@ l10n.localize = function() {
         break;
       }
     }
-    l10n.localizeElement(element, substitutions, substitutions.length != 0);
-    // Localize tool-tips
-    // TODO(jamiewalch): Move this logic to the html document.
-    var editButton = document.getElementById('this-host-rename');
-    editButton.title = chrome.i18n.getMessage(/*i18n-content*/'TOOLTIP_RENAME');
+    var titleTag = element.getAttribute('i18n-title');
+    if (titleTag) {
+      element.title = l10n.getTranslationOrError(titleTag, substitutions);
+    } else {
+      l10n.localizeElement(element, substitutions,
+                           substitutions.length != 0);
+    }
   }
 };
