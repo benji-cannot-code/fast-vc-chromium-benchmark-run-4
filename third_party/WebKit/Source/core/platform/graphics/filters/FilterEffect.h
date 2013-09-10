372af4593a8d30ec12256c538c6ec54bab99c0c8
senorblanco@chromium.org <senorblanco@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-10 17:14:24 +0000

    Implement filter primitive subregion on accelerated reference filters for FEColorMatrix, FEGaussianBlur and FE*Lighting. This patch also adds crop tests for all the filters, both software and hardware versions. The software results are correct; bringing the remainder of the hardware results into alignment with software results will be the work of future patches (Skia and Blink-side).
    The patch adds a crop offset to the SkiaImageFilterBuilder, which is used to communicate the offset computed by the Blink filter chain when inflating its results for filter bounds. This code may be removed in the future once Skia takes on this responsibility and knows how much each filter inflates its result.
    
    BUG=229910
    
    Review URL: https://chromiumcodereview.appspot.com/22794007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157529 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/graphics/filters/FilterEffect.h
+++ third_party/WebKit/Source/core/platform/graphics/filters/FilterEffect.h
@@ -143,6 +143,7 @@ public:
     void setEffectBoundaries(const FloatRect& effectBoundaries) { m_effectBoundaries = effectBoundaries; }
 
     Filter* filter() { return m_filter; }
+    const Filter* filter() const { return m_filter; }
 
     bool clipsToBounds() const { return m_clipsToBounds; }
     void setClipsToBounds(bool value) { m_clipsToBounds = value; }
@@ -170,6 +171,7 @@ protected:
 
     // If a pre-multiplied image, check every pixel for validity and correct if necessary.
     void forceValidPreMultipliedPixels();
+    SkIRect getCropRect(const FloatSize& cropOffset) const;
 
 private:
     virtual void applySoftware() = 0;
--- third_party/WebKit/Source/core/platform/graphics/filters/SkiaImageFilterBuilder.h
+++ third_party/WebKit/Source/core/platform/graphics/filters/SkiaImageFilterBuilder.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define SkiaImageFilterBuilder_h
 
 #include "core/platform/graphics/ColorSpace.h"
+#include "core/platform/graphics/FloatSize.h"
 #include "wtf/HashMap.h"
 
 class SkImageFilter;
@@ -46,10 +47,15 @@ public:
 
     PassRefPtr<SkImageFilter> transformColorSpace(
         SkImageFilter* input, ColorSpace srcColorSpace, ColorSpace dstColorSpace);
+
+    void setCropOffset(const FloatSize& cropOffset) { m_cropOffset = cropOffset; };
+    FloatSize cropOffset() { return m_cropOffset; }
+
 private:
     typedef std::pair<FilterEffect*, ColorSpace> FilterColorSpacePair;
     typedef HashMap<FilterColorSpacePair, RefPtr<SkImageFilter> > FilterBuilderHashMap;
     FilterBuilderHashMap m_map;
+    FloatSize m_cropOffset;
 };
 
 } // namespace WebCore
