benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 90e5afd2c85f9ee21c9a71ca74eaf11e10b74dad

Telemetry: Write chrome command line file as root if it doesn't exist.
On Android, Telemetry sets the chrome command line file before running any pages.
If the file doesn't already exist, it tries to write it as a normal user, rather
than root, which fails. This patch explicitly checks if the file exists. If it
doesn't exist it writes the file as root.

BUG=284468

Review URL: https://chromiumcodereview.appspot.com/23726018

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221426 0039d316-1c4b-4281-b951-d872f2087c98

tools/telemetry/telemetry/core/backends/chrome/android_browser_backend.py
@@ -206,8 +206,16 @@ class AndroidBrowserBackend(chrome_browser_backend.ChromeBrowserBackend):
 
   def _SetCommandLineFile(self, file_contents):
     def IsProtectedFile(name):
-      ls_output = self._adb.RunShellCommand('ls -l %s' % name)[0].split()
-      return ls_output[1] == 'root'
+      if self._adb.Adb().FileExistsOnDevice(name):
+        ls_output = self._adb.RunShellCommand('ls -l %s' % name)[0]
+        return ls_output == 'opendir failed, Permission denied' or \
+            ls_output.split()[1] == 'root'
+      else:
+        parent_name = os.path.dirname(name)
+        if parent_name != '':
+          return IsProtectedFile(parent_name)
+        else:
+          return True
 
     if IsProtectedFile(self._backend_settings.cmdline_file):
       if not self._adb.Adb().CanAccessProtectedFileContents():

commit 053e7b28df27ed669000541c5fd107f5ff320dba

Telemetry: fix permission check when setting Android command line.
This is to fix a bug introduced by r221426 on the WebView telemetry bot.
The issue was related to the fact that ls -l can return no output if the
target folder exists but is empty.
This change makes the IsProtectedFile logic inside _SetCommandLineFile
more reliable, introducing the IsFileWritableOnDevice method in adb
commands.

BUG=284468
NOTRY=true

Review URL: https://chromiumcodereview.appspot.com/23477053

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222349 0039d316-1c4b-4281-b951-d872f2087c98

build/android/pylib/android_commands.py
@@ -1467,6 +1467,29 @@ class AndroidCommands(object):
 
       return False
 
+  def IsFileWritableOnDevice(self, file_name):
+    """Checks whether the given file (or directory) is writable on the device.
+
+    Args:
+      file_name: Full path of file/directory to check.
+
+    Returns:
+      True if writable, False otherwise.
+    """
+    assert '"' not in file_name, 'file_name cannot contain double quotes'
+    try:
+      status = self._adb.SendShellCommand(
+          '\'test -w "%s"; echo $?\'' % (file_name))
+      if 'test: not found' not in status:
+        return int(status) == 0
+      raise errors.AbortError('"test" binary not found. OS too old.')
+
+    except ValueError:
+      if IsDeviceAttached(self._device):
+        raise errors.DeviceUnresponsiveError('Device may be offline.')
+
+      return False
+
   def TakeScreenshot(self, host_file):
     """Saves a screenshot image to |host_file| on the host.
 

tools/telemetry/telemetry/core/backends/chrome/android_browser_backend.py
@@ -209,9 +209,7 @@ class AndroidBrowserBackend(chrome_browser_backend.ChromeBrowserBackend):
   def _SetCommandLineFile(self, file_contents):
     def IsProtectedFile(name):
       if self._adb.Adb().FileExistsOnDevice(name):
-        ls_output = self._adb.RunShellCommand('ls -l %s' % name)[0]
-        return ls_output == 'opendir failed, Permission denied' or \
-            ls_output.split()[1] == 'root'
+        return not self._adb.Adb().IsFileWritableOnDevice(name)
       else:
         parent_name = os.path.dirname(name)
         if parent_name != '':