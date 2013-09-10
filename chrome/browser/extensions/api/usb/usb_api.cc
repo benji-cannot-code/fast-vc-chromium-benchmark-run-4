c1cab8adbf3adb93596ceb6b1baa438628684982
ikarienator@chromium.org <ikarienator@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-10 21:54:57 +0000

    Fixes extensions::UsbFindDevicesFunction::AsyncWorkStart()
    Buggy algorithm iterating through devices.
    
    BUG=288070
    
    Review URL: https://chromiumcodereview.appspot.com/23532052
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222356 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/usb/usb_api.cc
+++ chrome/browser/extensions/api/usb/usb_api.cc
@@ -303,10 +303,10 @@ const char* ConvertTransferStatusToErrorString(const UsbTransferStatus status) {
 
 #if defined(OS_CHROMEOS)
 void RequestUsbDevicesAccessHelper(
-    std::vector<scoped_refptr<UsbDevice> >* devices,
+    ScopedDeviceVector devices,
     std::vector<scoped_refptr<UsbDevice> >::iterator i,
     int interface_id,
-    const base::Closure& callback,
+    const base::Callback<void(ScopedDeviceVector result)>& callback,
     bool success) {
   if (success) {
     ++i;
@@ -314,20 +314,22 @@ void RequestUsbDevicesAccessHelper(
     i = devices->erase(i);
   }
   if (i == devices->end()) {
-    callback.Run();
+    callback.Run(devices.Pass());
     return;
   }
-  (*i)->RequestUsbAcess(interface_id,
-                        base::Bind(RequestUsbDevicesAccessHelper, devices, i,
-                                   interface_id, callback));
+  (*i)->RequestUsbAcess(interface_id, base::Bind(RequestUsbDevicesAccessHelper,
+                                                 base::Passed(devices.Pass()),
+                                                 i, interface_id, callback));
 }
 
 void RequestUsbDevicesAccess(
-    std::vector<scoped_refptr<UsbDevice> >* devices,
+    ScopedDeviceVector devices,
     int interface_id,
-    const base::Closure& callback) {
-  RequestUsbDevicesAccessHelper(devices, devices->begin(), interface_id,
-                               callback, true);
+    const base::Callback<void(ScopedDeviceVector result)>& callback) {
+  (*devices->begin())->RequestUsbAcess(
+      interface_id,
+      base::Bind(RequestUsbDevicesAccessHelper, base::Passed(devices.Pass()),
+                 devices->begin(), interface_id, callback));
 }
 #endif  // OS_CHROMEOS
 
@@ -562,16 +564,12 @@ void UsbFindDevicesFunction::AsyncWorkStart() {
   }
 
 #if defined(OS_CHROMEOS)
-  if (parameters_->options.interface_id.get()) {
-    int interface_id = *parameters_->options.interface_id.get();
-    RequestUsbDevicesAccess(devices.get(), interface_id,
-                            base::Bind(&UsbFindDevicesFunction::OpenDevices,
-                                       this,
-                                       base::Passed(devices.Pass())));
-    return;
-  }
-#endif  // OS_CHROMEOS
+  RequestUsbDevicesAccess(
+      devices.Pass(), interface_id,
+      base::Bind(&UsbFindDevicesFunction::OpenDevices, this));
+#else
   OpenDevices(devices.Pass());
+#endif  // OS_CHROMEOS
 }
 
 void UsbFindDevicesFunction::OpenDevices(ScopedDeviceVector devices) {
