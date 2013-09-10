a60019e240d9f55489f4022283dc205d995189e4
yosin@chromium.org <yosin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-10 08:17:36 +0000

    Introduce Position::inDocument()
    This patch introduces Position::inDocument() to replace Position::anchorNode()->inDocument() for shorter code pattern and safety for avoiding null reference.
    
    BUG=n/a
    TEST=n/a; no behavior changes
    R=tkent@chromium.org
    
    Review URL: https://codereview.chromium.org/23450030
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157513 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/Editor.cpp
+++ third_party/WebKit/Source/core/editing/Editor.cpp
@@ -2099,8 +2099,7 @@ void Editor::respondToChangedSelection(const VisibleSelection& oldSelection, Fra
         if (shouldCheckSpellingAndGrammar
             && closeTyping
             && oldSelection.isContentEditable()
-            && oldSelection.start().deprecatedNode()
-            && oldSelection.start().anchorNode()->inDocument()
+            && oldSelection.start().inDocument()
             && !isSelectionInTextField(oldSelection)) {
             spellCheckOldSelection(oldSelection, newAdjacentWords, newSelectedSentence);
         }
