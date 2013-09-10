5f73b789309c9f5ab9046a48111aad44f99ff968
joi@chromium.org <joi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-10 10:08:18 +0000

    Revert 222226 "App windows with ids should remember fullscreen s..."
    > App windows with ids should remember fullscreen state.
    >
    > BUG=242790
    > TEST=PlatformAppBrowserTest.ShellWindowRestoreState
    > TEST=1. Install window-state-sample: https://chrome.google.com/webstore/detail/window-state-sample/hcbhfbnaaancmblfhdknlnojpafjohbi; 2. Run the app and create a new window with id. 3. Fullscreen the new window by clicking "Fullscreen" of "chrome.app.window" section; 4. Close the window (You can use "Alt+F4" etc.) and recreate the new window with the same id. 5. A fullscreen window should be shown.
    >
    > Review URL: https://chromiumcodereview.appspot.com/23672029
    
    Reason for revert: Causes
    PlatformAppBrowserTest.ShellWindowRestoreState to fail on Mac OS X
    10.6.
    
    TBR=zhchbin@gmail.com
    
    Review URL: https://codereview.chromium.org/23707028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222247 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/platform_apps/restore_state/test.js
+++ chrome/test/data/extensions/platform_apps/restore_state/test.js
@@ -7,17 +7,10 @@ var assertState = function(win) {
   if (win.id == 'normal') {
     chrome.test.assertFalse(win.isMinimized());
     chrome.test.assertFalse(win.isMaximized());
-    chrome.test.assertFalse(win.isFullscreen());
   }
   if (win.id == 'maximized') {
     chrome.test.assertFalse(win.isMinimized());
     chrome.test.assertTrue(win.isMaximized());
-    chrome.test.assertFalse(win.isFullscreen());
-  }
-  if (win.id == 'fullscreen') {
-    chrome.test.assertFalse(win.isMinimized());
-    chrome.test.assertFalse(win.isMaximized());
-    chrome.test.assertTrue(win.isFullscreen());
   }
 }
 
@@ -48,9 +41,6 @@ chrome.app.runtime.onLaunched.addListener(function() {
     },
     function testRestoreMaximized() {
       testRestoreState('maximized');
-    },
-    function testRestoreFullscreen() {
-      testRestoreState('fullscreen');
     },
     // Minimize and fullscreen behavior are platform dependent.
   ]);
