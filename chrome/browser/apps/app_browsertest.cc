931bca8d9ec46ea896dc0d0205ba6aec895c18af
sergeygs@chromium.org <sergeygs@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-10 09:11:39 +0000

    "Redirecting URLs to Packaged Apps" implementation: revised
    Design proposal: https://docs.google.com/document/d/1r-RoOv2URfZBYrT_B6notQ6MeMqZRd1EP1AITuzJCAc/edit?usp=sharing
    
    * Support for url_handlers in manifest
    * New kind of onLaunched event with navigation info in launch data
    * Intercept/redirect top-level browser-initiated navigations (bookmarks, omnibox, etc.)
    * Intercept/redirect top-frame navigations in tabs and app windows. 
    
    This is a rework of https://codereview.chromium.org/22944002/.
    
    BUG=111422
    
    Review URL: https://chromiumcodereview.appspot.com/23847004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222235 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/apps/app_browsertest.cc
+++ chrome/browser/apps/app_browsertest.cc
@@ -14,8 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/prefs/pref_service.h"
 #include "base/stl_util.h"
 #include "base/strings/utf_string_conversions.h"
-#include "base/test/test_timeouts.h"
-#include "base/threading/platform_thread.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/apps/app_browsertest_util.h"
 #include "chrome/browser/automation/automation_util.h"
---
+++ chrome/test/data/extensions/platform_apps/url_handlers/launchers/click_mismatching_blank_link/main.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.test.getConfig(function(config) {
+  var port = config ? config.testServer.port : '0';
+
+  var link = document.getElementById('mismatching_link');
+  link.href =
+      'http://localhost:' + port +
+      '/extensions/platform_apps/url_handlers/common/mismatching_target.html';
+
+  // This click should be intercepted and redirected to the handler app
+  // (pre-installed by the CPP side of the test before launching this).
+  var clickEvent = document.createEvent('MouseEvents');
+  clickEvent.initMouseEvent('click', true, true, window,
+                            0, 0, 0, 0, 0, false, false,
+                            false, false, 0, null);
+  link.dispatchEvent(clickEvent);
+
+  chrome.test.sendMessage("Launcher done");
+});
