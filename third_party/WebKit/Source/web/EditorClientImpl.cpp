48c1254e115effc2781900e4859aaeaadb9014f5
l.gombos@samsung.com <l.gombos@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-10 04:21:15 +0000

    Avoid using !OS() constructs. Simplify guards to take advantage of the facts that !OS(POSIX) means OS(WIN).
    Also fix a bug spotted for OS(ANDROID). Make OS(ANDROID) behave the same way as OS(WIN) and OS(MACOSX) inside EventHandler::shouldTurnVerticalTicksIntoHorizontal().
    
    Review URL: https://chromiumcodereview.appspot.com/23654015
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157500 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/EditorClientImpl.cpp
+++ third_party/WebKit/Source/web/EditorClientImpl.cpp
@@ -770,10 +770,10 @@ bool EditorClientImpl::spellingUIIsShowing()
 
 bool EditorClientImpl::supportsGlobalSelection()
 {
-#if OS(POSIX) && !OS(MACOSX)
-    return true;
-#else
+#if OS(WIN) || OS(MACOSX)
     return false;
+#else
+    return true;
 #endif
 }
 
