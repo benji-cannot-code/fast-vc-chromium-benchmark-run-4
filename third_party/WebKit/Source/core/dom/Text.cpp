c8915c4bd02c67cbdfe0ac32555fcacda6de18b2
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-10 04:29:51 +0000

    Add const version of toText(), and use it.
    We'd like to avoid bare static_cast.
    
    BUG=none
    TEST=none; no behavior changes.
    
    Review URL: https://chromiumcodereview.appspot.com/23658029
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157501 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Text.cpp
+++ third_party/WebKit/Source/core/dom/Text.cpp
@@ -89,7 +89,7 @@ static const Text* earliestLogicallyAdjacentTextNode(const Text* t)
     while ((n = n->previousSibling())) {
         Node::NodeType type = n->nodeType();
         if (type == Node::TEXT_NODE || type == Node::CDATA_SECTION_NODE) {
-            t = static_cast<const Text*>(n);
+            t = toText(n);
             continue;
         }
 
@@ -104,7 +104,7 @@ static const Text* latestLogicallyAdjacentTextNode(const Text* t)
     while ((n = n->nextSibling())) {
         Node::NodeType type = n->nodeType();
         if (type == Node::TEXT_NODE || type == Node::CDATA_SECTION_NODE) {
-            t = static_cast<const Text*>(n);
+            t = toText(n);
             continue;
         }
 
@@ -123,8 +123,7 @@ String Text::wholeText() const
     for (const Node* n = startText; n != onePastEndText; n = n->nextSibling()) {
         if (!n->isTextNode())
             continue;
-        const Text* t = static_cast<const Text*>(n);
-        const String& data = t->data();
+        const String& data = toText(n)->data();
         if (std::numeric_limits<unsigned>::max() - data.length() < resultLength)
             CRASH();
         resultLength += data.length();
@@ -134,8 +133,7 @@ String Text::wholeText() const
     for (const Node* n = startText; n != onePastEndText; n = n->nextSibling()) {
         if (!n->isTextNode())
             continue;
-        const Text* t = static_cast<const Text*>(n);
-        result.append(t->data());
+        result.append(toText(n)->data());
     }
     ASSERT(result.length() == resultLength);
 
