45da49a1d17569d6a16119c86c6d9b2a77a6be1d
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-10 01:50:11 +0000

    QuicCryptoClientConfig::ProcessServerHello should learn about updated source address tokens in ServerHello messages.
    Merge internal CL: 51981820
    
    List output parameters after input parameters.
    
    Merge internal CL: 52069947
    
    R=rch@chromium.org,rtenneti@chromium.org
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/23766019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222175 0039d316-1c4b-4281-b951-d872f2087c98

--- net/quic/crypto/crypto_handshake.cc
+++ net/quic/crypto/crypto_handshake.cc
@@ -793,9 +793,9 @@ QuicErrorCode QuicCryptoClientConfig::FillClientHello(
 }
 
 QuicErrorCode QuicCryptoClientConfig::ProcessRejection(
-    CachedState* cached,
     const CryptoHandshakeMessage& rej,
     QuicWallTime now,
+    CachedState* cached,
     QuicCryptoNegotiatedParameters* out_params,
     string* error_details) {
   DCHECK(error_details != NULL);
@@ -857,6 +857,7 @@ QuicErrorCode QuicCryptoClientConfig::ProcessRejection(
 QuicErrorCode QuicCryptoClientConfig::ProcessServerHello(
     const CryptoHandshakeMessage& server_hello,
     QuicGuid guid,
+    CachedState* cached,
     QuicCryptoNegotiatedParameters* out_params,
     string* error_details) {
   DCHECK(error_details != NULL);
@@ -864,6 +865,12 @@ QuicErrorCode QuicCryptoClientConfig::ProcessServerHello(
   if (server_hello.tag() != kSHLO) {
     *error_details = "Bad tag";
     return QUIC_INVALID_CRYPTO_MESSAGE_TYPE;
+  }
+
+  // Learn about updated source address tokens.
+  StringPiece token;
+  if (server_hello.GetStringPiece(kSourceAddressTokenTag, &token)) {
+    cached->set_source_address_token(token);
   }
 
   // TODO(agl):
