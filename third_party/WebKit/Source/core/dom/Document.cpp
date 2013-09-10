benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 8779fd978660b9c0d033399d343d81ed34802700

Fix writingmode and direction inheriting for the Document
The old code spread logic out in multiple places that tries to have the
document inherit the writing mode of <html> and <html> inherit the
writing mode of <body>. This logic breaks in the face of lazy attaching
because the renderers are not necessarily there.

Instead, compute these values ahead of time in Document::recalcStyle
and avoid the <html> or <body> renderers needing to reach up and modify
the document's style. This way, the logic works even if we haven't created
the renderers yet, which is needed for lazyAttach.

The ElementResolveContext fixes a bug where we do the wrong thing if the 
documentElement lacks a renderer. The code in StyleResolver::pseudoStyleForElement
that resets bits on the document seems like it's just wrong. We should
only reset those bits when recalculating the documentElement's style.

Review URL: https://chromiumcodereview.appspot.com/23468006

git-svn-id: svn://svn.chromium.org/blink/trunk@157174 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/dom/Document.cpp
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/v8/ScriptController.h"
 #include "core/accessibility/AXObjectCache.h"
 #include "core/animation/DocumentTimeline.h"
+#include "core/css/CSSFontSelector.h"
 #include "core/css/CSSStyleDeclaration.h"
 #include "core/css/CSSStyleSheet.h"
 #include "core/css/FontLoader.h"
@@ -51,6 +52,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/css/StylePropertySet.h"
 #include "core/css/StyleSheetContents.h"
 #include "core/css/StyleSheetList.h"
+#include "core/css/resolver/FontBuilder.h"
 #include "core/css/resolver/StyleResolver.h"
 #include "core/dom/Attr.h"
 #include "core/dom/BeforeUnloadEvent.h"
@@ -78,6 +80,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/NodeFilter.h"
 #include "core/dom/NodeIterator.h"
 #include "core/dom/NodeRareData.h"
+#include "core/dom/NodeRenderStyle.h"
 #include "core/dom/NodeRenderingTraversal.h"
 #include "core/dom/NodeTraversal.h"
 #include "core/dom/NodeWithIndex.h"
@@ -1603,6 +1606,78 @@ void Document::updateDistributionForNodeIfNeeded(Node* node)
         root->recalcDistribution();
 }
 
+void Document::setStyleDependentState(RenderStyle* documentStyle)
+{
+    const Pagination& pagination = view()->pagination();
+    if (pagination.mode != Pagination::Unpaginated) {
+        Pagination::setStylesForPaginationMode(pagination.mode, documentStyle);
+        documentStyle->setColumnGap(pagination.gap);
+        if (renderView()->hasColumns())
+            renderView()->updateColumnInfoFromStyle(documentStyle);
+    }
+
+    // Seamless iframes want to inherit their font from their parent iframe, so early return before setting the font.
+    if (shouldDisplaySeamlesslyWithParent())
+        return;
+
+    FontBuilder fontBuilder;
+    fontBuilder.initForStyleResolve(*this, documentStyle, isSVGDocument());
+    RefPtr<CSSFontSelector> selector = m_styleResolver ? m_styleResolver->fontSelector() : 0;
+    fontBuilder.createFontForDocument(selector, documentStyle);
+}
+
+void Document::inheritHtmlAndBodyElementStyles(StyleChange change)
+{
+    RenderView* renderView = this->renderView();
+
+    if (!documentElement() || !frame() || !view())
+        return;
+
+    RefPtr<RenderStyle> documentElementStyle = documentElement()->renderStyle();
+    if (!documentElementStyle || documentElement()->needsStyleRecalc() || change == Force)
+        documentElementStyle = styleResolver()->styleForElement(documentElement());
+
+    RefPtr<RenderStyle> bodyStyle = 0;
+    if (body()) {
+        bodyStyle = body()->renderStyle();
+        if (!bodyStyle || body()->needsStyleRecalc() || documentElement()->needsStyleRecalc() || change == Force)
+            bodyStyle = styleResolver()->styleForElement(body(), documentElementStyle.get());
+    }
+
+    WritingMode rootWritingMode = documentElementStyle->writingMode();
+    bool isHorizontalWritingMode = documentElementStyle->isHorizontalWritingMode();
+    TextDirection rootDirection = documentElementStyle->direction();
+
+    if (!writingModeSetOnDocumentElement() && body()) {
+        rootWritingMode = bodyStyle->writingMode();
+        isHorizontalWritingMode = bodyStyle->isHorizontalWritingMode();
+    }
+
+    if (!directionSetOnDocumentElement() && body())
+        rootDirection = bodyStyle->direction();
+
+    RefPtr<RenderStyle> documentStyle = renderView->style();
+    if (documentStyle->writingMode() != rootWritingMode || documentStyle->direction() != rootDirection) {
+        RefPtr<RenderStyle> newStyle = RenderStyle::clone(documentStyle.get());
+        newStyle->setWritingMode(rootWritingMode);
+        newStyle->setDirection(rootDirection);
+        renderView->setStyle(newStyle);
+        setStyleDependentState(newStyle.get());
+    }
+
+    if (body()) {
+        if (RenderStyle* style = body()->renderStyle()) {
+            if (style->direction() != rootDirection || style->writingMode() != rootWritingMode)
+                body()->setNeedsStyleRecalc();
+        }
+    }
+
+    if (RenderStyle* style = documentElement()->renderStyle()) {
+        if (style->direction() != rootDirection || style->writingMode() != rootWritingMode)
+            documentElement()->setNeedsStyleRecalc();
+    }
+}
+
 void Document::recalcStyle(StyleChange change)
 {
     // we should not enter style recalc while painting
@@ -1650,16 +1725,15 @@ void Document::recalcStyle(StyleChange change)
         if (styleChangeType() >= SubtreeStyleChange)
             change = Force;
 
-        // Recalculating the root style (on the document) is not needed in the common case.
         if ((change == Force) || (shouldDisplaySeamlesslyWithParent() && (change >= Inherit))) {
-            // style selector may set this again during recalc
             m_hasNodesWithPlaceholderStyle = false;
-
             RefPtr<RenderStyle> documentStyle = StyleResolver::styleForDocument(*this, m_styleResolver ? m_styleResolver->fontSelector() : 0);
-            StyleChange ch = Node::diff(documentStyle.get(), renderer()->style(), *this);
-            if (ch != NoChange)
+            StyleChange localChange = Node::diff(documentStyle.get(), renderer()->style(), *this);
+            if (localChange != NoChange)
                 renderer()->setStyle(documentStyle.release());
         }
+
+        inheritHtmlAndBodyElementStyles(change);
 
         for (Node* n = firstChild(); n; n = n->nextSibling()) {
             if (!n->isElementNode())

third_party/WebKit/Source/core/dom/Document.h
@@ -485,6 +485,8 @@ public:
     PassRefPtr<CSSStyleDeclaration> createCSSStyleDeclaration();
     PassRefPtr<Text> createEditingTextNode(const String&);
 
+    void setStyleDependentState(RenderStyle* documentStyle);
+    void inheritHtmlAndBodyElementStyles(StyleChange);
     void recalcStyle(StyleChange = NoChange);
     void updateStyleIfNeeded();
     void updateStyleForNodeIfNeeded(Node*);

third_party/WebKit/LayoutTests/TestExpectations
@@ -1222,6 +1222,14 @@ crbug.com/276861 [ Win ] fast/filesystem/op-restricted-unicode.html [ Failure Pa
 crbug.com/278318 [ Win7 Lion Debug Release SnowLeopard Debug Release Linux ] editing/spelling/spellcheck-editable-on-focus.html [ Failure Pass ]
 crbug.com/282426 editing/spelling/spellcheck-disable-enable.html [ Failure ]
 
+crbug.com/282708 fast/regions/changing-writing-mode-2.html [ ImageOnlyFailure ]
+crbug.com/282708 fast/regions/changing-writing-mode-5.html [ ImageOnlyFailure ]
+crbug.com/282708 fast/regions/changing-writing-mode.html [ ImageOnlyFailure ]
+crbug.com/282708 fast/regions/subtree-with-horiz-bt.html [ ImageOnlyFailure ]
+crbug.com/282708 fast/regions/subtree-with-horiz-tb.html [ ImageOnlyFailure ]
+crbug.com/282708 fast/regions/subtree-with-vert-lr.html [ ImageOnlyFailure ]
+crbug.com/282708 fast/regions/subtree-with-vert-rl.html [ ImageOnlyFailure ]
+
 crbug.com/278497 [ Linux SnowLeopard Debug ] http/tests/security/contentSecurityPolicy/1.1/plugintypes-invalid.html [ Failure Pass  ]
 
 crbug.com/280203 [ Mac ] compositing/overflow/nested-scrolling.html [ ImageOnlyFailure ]

third_party/WebKit/Source/core/css/resolver/ElementResolveContext.cpp
@@ -44,6 +44,8 @@ ElementResolveContext::ElementResolveContext(Element* element)
     Node* documentElement = document().documentElement();
     RenderStyle* documentStyle = document().renderStyle();
     m_rootElementStyle = documentElement && element != documentElement ? documentElement->renderStyle() : documentStyle;
+    if (!m_rootElementStyle)
+        m_rootElementStyle = documentStyle;
 }
 
 } // namespace WebCore

third_party/WebKit/Source/core/css/resolver/StyleResolver.cpp
@@ -504,7 +504,7 @@ bool StyleResolver::styleSharingCandidateMatchesRuleSet(const ElementResolveCont
     return collector.hasAnyMatchingRules(ruleSet);
 }
 
-PassRefPtr<RenderStyle> StyleResolver::styleForDocument(const Document& document, CSSFontSelector* fontSelector)
+PassRefPtr<RenderStyle> StyleResolver::styleForDocument(Document& document, CSSFontSelector* fontSelector)
 {
     const Frame* frame = document.frame();
 
@@ -530,45 +530,7 @@ PassRefPtr<RenderStyle> StyleResolver::styleForDocument(const Document& document
     // This overrides any -webkit-user-modify inherited from the parent iframe.
     documentStyle->setUserModify(document.inDesignMode() ? READ_WRITE : READ_ONLY);
 
-    Element* docElement = document.documentElement();
-    RenderObject* docElementRenderer = docElement ? docElement->renderer() : 0;
-    if (docElementRenderer) {
-        // Use the direction and writing-mode of the body to set the
-        // viewport's direction and writing-mode unless the property is set on the document element.
-        // If there is no body, then use the document element.
-        RenderObject* bodyRenderer = document.body() ? document.body()->renderer() : 0;
-        if (bodyRenderer && !document.writingModeSetOnDocumentElement())
-            documentStyle->setWritingMode(bodyRenderer->style()->writingMode());
-        else
-            documentStyle->setWritingMode(docElementRenderer->style()->writingMode());
-        if (bodyRenderer && !document.directionSetOnDocumentElement())
-            documentStyle->setDirection(bodyRenderer->style()->direction());
-        else
-            documentStyle->setDirection(docElementRenderer->style()->direction());
-    }
-
-    if (frame) {
-        if (FrameView* frameView = frame->view()) {
-            const Pagination& pagination = frameView->pagination();
-            if (pagination.mode != Pagination::Unpaginated) {
-                Pagination::setStylesForPaginationMode(pagination.mode, documentStyle.get());
-                documentStyle->setColumnGap(pagination.gap);
-                if (RenderView* view = document.renderView()) {
-                    if (view->hasColumns())
-                        view->updateColumnInfoFromStyle(documentStyle.get());
-                }
-            }
-        }
-    }
-
-    // Seamless iframes want to inherit their font from their parent iframe, so early return before setting the font.
-    if (seamlessWithParent)
-        return documentStyle.release();
-
-    FontBuilder fontBuilder;
-    fontBuilder.initForStyleResolve(document, documentStyle.get(), document.isSVGDocument());
-    fontBuilder.createFontForDocument(fontSelector, documentStyle.get());
-
+    document.setStyleDependentState(documentStyle.get());
     return documentStyle.release();
 }
 
@@ -917,8 +879,6 @@ PassRefPtr<RenderStyle> StyleResolver::pseudoStyleForElement(Element* e, const P
     if (!e)
         return 0;
 
-    if (e == document().documentElement())
-        resetDirectionAndWritingModeOnDocument(document());
     StyleResolverState state(document(), e, parentStyle);
 
     if (pseudoStyleRequest.allowsInheritance(state.parentStyle())) {

third_party/WebKit/Source/core/css/resolver/StyleResolver.h
@@ -203,7 +203,7 @@ public:
     PassRefPtr<RenderStyle> defaultStyleForElement();
     PassRefPtr<RenderStyle> styleForText(Text*);
 
-    static PassRefPtr<RenderStyle> styleForDocument(const Document&, CSSFontSelector* = 0);
+    static PassRefPtr<RenderStyle> styleForDocument(Document&, CSSFontSelector* = 0);
 
     // FIXME: This only has 5 callers and should be removed. Callers should be explicit about
     // their dependency on Document* instead of grabbing one through StyleResolver.

third_party/WebKit/Source/core/dom/Element.cpp
@@ -1325,6 +1325,10 @@ void Element::attach(const AttachContext& context)
             data->resetStyleState();
     }
 
+    // FIXME: Remove this once we lazyAttach everywhere.
+    if (!document().inStyleRecalc() && (this == document().documentElement() || this == document().body()))
+        document().setNeedsStyleRecalc(LocalStyleChange);
+
     NodeRenderingContext(this, context.resolvedStyle).createRendererForElementIfNeeded();
 
     createPseudoElementIfNeeded(BEFORE);

third_party/WebKit/Source/core/rendering/RenderBox.cpp
@@ -277,33 +277,8 @@ void RenderBox::styleDidChange(StyleDifference diff, const RenderStyle* oldStyle
         }
     }
 
-    bool isBodyRenderer = isBody();
-    bool isRootRenderer = isRoot();
-
-    if (isRootRenderer || isBodyRenderer) {
-        // Propagate the new writing mode and direction up to the RenderView.
-        RenderView* viewRenderer = view();
-        RenderStyle* viewStyle = viewRenderer->style();
-        if (viewStyle->direction() != newStyle->direction() && (isRootRenderer || !document().directionSetOnDocumentElement())) {
-            viewStyle->setDirection(newStyle->direction());
-            if (isBodyRenderer)
-                document().documentElement()->renderer()->style()->setDirection(newStyle->direction());
-            setNeedsLayoutAndPrefWidthsRecalc();
-        }
-
-        if (viewStyle->writingMode() != newStyle->writingMode() && (isRootRenderer || !document().writingModeSetOnDocumentElement())) {
-            viewStyle->setWritingMode(newStyle->writingMode());
-            viewRenderer->setHorizontalWritingMode(newStyle->isHorizontalWritingMode());
-            viewRenderer->markAllDescendantsWithFloatsForLayout();
-            if (isBodyRenderer) {
-                document().documentElement()->renderer()->style()->setWritingMode(newStyle->writingMode());
-                document().documentElement()->renderer()->setHorizontalWritingMode(newStyle->isHorizontalWritingMode());
-            }
-            setNeedsLayoutAndPrefWidthsRecalc();
-        }
-
-        frame()->view()->recalculateScrollbarOverlayStyle();
-    }
+    if (isRoot() || isBody())
+        document().view()->recalculateScrollbarOverlayStyle();
 
     updateShapeOutsideInfoAfterStyleChange(style()->shapeOutside(), oldStyle ? oldStyle->shapeOutside() : 0);
     updateGridPositionAfterStyleChange(oldStyle);

commit 117d7c89cfd7466cf6599e9e971f09561c879d79

When there are no more stylesheets schedule the recalc async
Document::didRemoveAllPendingStylesheet gets called whenever the
pending stylesheet counter goes to zero. This can happen many times
in a document if it's inserting inline stylesheets dynamically which
then causes many recalcStyle(Force) to happen.

This patch switches didRemoveAllPendingStylesheet to instead schedule
an async recalc style. This prevents the "waterfall of recalc styles"
you can see in apps that append many <style> elements while they're
loading.

I also removed code that scheduled a relayout when adding or removing
stylesheets since it doesn't appear needed or make sense to do this
outside the normal recalc style flow. In addition making this change
appears to fix the fast/regions/ writing mode tests that were broken
by r157174 where ojan and I fixed writing modes for lazy attach.

BUG=236063,282708

Review URL: https://chromiumcodereview.appspot.com/14604003

git-svn-id: svn://svn.chromium.org/blink/trunk@157497 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/dom/Document.cpp
@@ -386,6 +386,7 @@ Document::Document(const DocumentInit& initializer, DocumentClassFlags documentC
     , m_lastStyleResolverAccessTime(0)
     , m_didCalculateStyleResolver(false)
     , m_ignorePendingStylesheets(false)
+    , m_evaluateMediaQueriesOnStyleRecalc(false)
     , m_needsNotifyRemoveAllPendingStylesheet(false)
     , m_hasNodesWithPlaceholderStyle(false)
     , m_pendingSheetLayout(NoLayoutWithPendingSheets)
@@ -1693,6 +1694,11 @@ void Document::recalcStyle(StyleRecalcChange change)
     TRACE_EVENT0("webkit", "Document::recalcStyle");
     TRACE_EVENT_SCOPED_SAMPLING_STATE("Blink", "RecalcStyle");
 
+    if (m_evaluateMediaQueriesOnStyleRecalc) {
+        m_evaluateMediaQueriesOnStyleRecalc = false;
+        evaluateMediaQueryList();
+    }
+
     updateDistributionIfNeeded();
 
     // FIXME: We should update style on our ancestor chain before proceeding (especially for seamless),
@@ -2829,7 +2835,7 @@ void Document::didRemoveAllPendingStylesheet()
 {
     m_needsNotifyRemoveAllPendingStylesheet = false;
 
-    styleResolverChanged(RecalcStyleImmediately, AnalyzedStyleUpdate);
+    styleResolverChanged(RecalcStyleDeferred, AnalyzedStyleUpdate);
     executeScriptsWaitingForResourcesIfNeeded();
 
     if (m_gotoAnchorNeededAfterStylesheetsLoad && view())
@@ -3304,34 +3310,21 @@ void Document::styleResolverChanged(StyleResolverUpdateType updateType, StyleRes
 
     bool needsRecalc = m_styleSheetCollections->updateActiveStyleSheets(updateMode);
 
-    if (updateType >= RecalcStyleDeferred) {
-        setNeedsStyleRecalc();
-        return;
-    }
-
     if (didLayoutWithPendingStylesheets() && !m_styleSheetCollections->hasPendingSheets()) {
+        // We need to manually repaint because we avoid doing all repaints in layout or style
+        // recalc while sheets are still loading to avoid FOUC.
         m_pendingSheetLayout = IgnoreLayoutWithPendingSheets;
-        if (renderer())
-            renderView()->repaintViewAndCompositedLayers();
+        renderView()->repaintViewAndCompositedLayers();
     }
 
     if (!needsRecalc)
         return;
 
-    // This recalcStyle initiates a new recalc cycle. We need to bracket it to
-    // make sure animations get the correct update time
-    {
-        AnimationUpdateBlock animationUpdateBlock(m_frame ? m_frame->animation() : 0);
-        recalcStyle(Force);
-    }
+    m_evaluateMediaQueriesOnStyleRecalc = true;
+    setNeedsStyleRecalc();
 
-    if (renderer()) {
-        renderer()->setNeedsLayoutAndPrefWidthsRecalc();
-        if (view())
-            view()->scheduleRelayout();
-    }
-
-    evaluateMediaQueryList();
+    if (updateType == RecalcStyleImmediately)
+        updateStyleIfNeeded();
 }
 
 void Document::notifySeamlessChildDocumentsOfStylesheetUpdate() const

third_party/WebKit/Source/core/dom/Document.h
@@ -1168,6 +1168,7 @@ private:
     // But sometimes you need to ignore pending stylesheet count to
     // force an immediate layout when requested by JS.
     bool m_ignorePendingStylesheets;
+    bool m_evaluateMediaQueriesOnStyleRecalc;
 
     // If we do ignore the pending stylesheet count, then we need to add a boolean
     // to track that this happened so that we can do a full repaint when the stylesheets

third_party/WebKit/LayoutTests/TestExpectations
@@ -1208,14 +1208,6 @@ crbug.com/276861 [ Win ] fast/filesystem/op-restricted-unicode.html [ Failure Pa
 
 crbug.com/278318 [ Win7 Lion Debug Release SnowLeopard Debug Release Linux ] editing/spelling/spellcheck-editable-on-focus.html [ Failure Pass ]
 
-crbug.com/282708 fast/regions/changing-writing-mode-2.html [ ImageOnlyFailure ]
-crbug.com/282708 fast/regions/changing-writing-mode-5.html [ ImageOnlyFailure ]
-crbug.com/282708 fast/regions/changing-writing-mode.html [ ImageOnlyFailure ]
-crbug.com/282708 fast/regions/subtree-with-horiz-bt.html [ ImageOnlyFailure ]
-crbug.com/282708 fast/regions/subtree-with-horiz-tb.html [ ImageOnlyFailure ]
-crbug.com/282708 fast/regions/subtree-with-vert-lr.html [ ImageOnlyFailure ]
-crbug.com/282708 fast/regions/subtree-with-vert-rl.html [ ImageOnlyFailure ]
-
 crbug.com/278497 [ Linux SnowLeopard Debug ] http/tests/security/contentSecurityPolicy/1.1/plugintypes-invalid.html [ Failure Pass  ]
 
 crbug.com/280203 [ Mac ] compositing/overflow/nested-scrolling.html [ ImageOnlyFailure ]