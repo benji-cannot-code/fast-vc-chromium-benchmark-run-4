46ce05638e8c660886c37a074104627d973b621f
eroman@chromium.org <eroman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-10 18:10:54 +0000

    WebCrypto: [refactor] Use "unsigned" rather than "size_t" for buffer sizes in the public API.
    Justification:
    
    I have noticed some instances where the Chromium-side implementation is directly passing this "size_t" as the buffer length to NSS.
    
    NSS however expects buffer lengths as "unsigned int", and the C++ compiler implicitly converts the "size_t" to an "unsigned int". This conversion is dangerous, since theoretically it could result in truncation of the buffer length given to NSS. (Under LP64 datamodel for instance).
    
    Chromium's compilation settings don't warn about such issues.
    
    In practice this isn't a problem, and likely never will be, since in Blink ArrayBuffers are limited to lengths of 2^32-1.
    
    It is therefore a good idea to pass a narrower datatype in the API to make this obvious. Embedder shouldn't have to worry about dealing with buffer sizes in the full range of size_t.
    
    BUG=245025
    
    Review URL: https://chromiumcodereview.appspot.com/23537027
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157532 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebCryptoAlgorithmParams.h
+++ third_party/WebKit/public/platform/WebCryptoAlgorithmParams.h
@@ -62,7 +62,7 @@ private:
 
 class WebCryptoAesCbcParams : public WebCryptoAlgorithmParams {
 public:
-    WebCryptoAesCbcParams(unsigned char* iv, size_t ivSize)
+    WebCryptoAesCbcParams(unsigned char* iv, unsigned ivSize)
         : WebCryptoAlgorithmParams(WebCryptoAlgorithmParamsTypeAesCbcParams)
         , m_iv(iv, ivSize)
     {
@@ -146,7 +146,7 @@ private:
 
 class WebCryptoRsaKeyGenParams : public WebCryptoAlgorithmParams {
 public:
-    WebCryptoRsaKeyGenParams(unsigned modulusLength, const unsigned char* publicExponent, size_t publicExponentSize)
+    WebCryptoRsaKeyGenParams(unsigned modulusLength, const unsigned char* publicExponent, unsigned publicExponentSize)
         : WebCryptoAlgorithmParams(WebCryptoAlgorithmParamsTypeRsaKeyGenParams)
         , m_modulusLength(modulusLength)
         , m_publicExponent(publicExponent, publicExponentSize)
