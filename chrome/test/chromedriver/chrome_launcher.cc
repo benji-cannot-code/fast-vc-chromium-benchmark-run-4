881a528823c62d8443d0476d877f1ced654b7592
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-11 15:26:48 +0000

    [chromedriver] Use data:, instead of about:blank for the start page.
    about:blank causes Chrome to focus the url bar, which breaks tests depending
    on focus.
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/23464065
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222563 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/chromedriver/chrome_launcher.cc
+++ chrome/test/chromedriver/chrome_launcher.cc
@@ -116,7 +116,7 @@ Status PrepareCommandLine(int port,
   switches.SetFromSwitches(capabilities.switches);
 
   if (!switches.HasSwitch("user-data-dir")) {
-    command.AppendArg("about:blank");
+    command.AppendArg("data:,");
     if (!user_data_dir->CreateUniqueTempDir())
       return Status(kUnknownError, "cannot create temp dir for user data dir");
     switches.SetSwitch("user-data-dir", user_data_dir->path().value());
