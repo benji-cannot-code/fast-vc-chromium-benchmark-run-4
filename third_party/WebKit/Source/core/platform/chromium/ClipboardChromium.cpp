dfa685f5929f3280fd43825a72d0a2851b925bdf
ch.dumez@sisa.samsung.com <ch.dumez@sisa.samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-11 14:54:20 +0000

    Have V8Clipboard::clearDataMethodCustom() use V8TRYCATCH_FOR_V8STRINGRESOURCE_VOID()
    Have V8Clipboard::clearDataMethodCustom() use V8TRYCATCH_FOR_V8STRINGRESOURCE_VOID() macro
    instead of toWebCoreString() so that ClipboardChromium::clearData() is not called if
    the string conversion failed.
    
    Also remove a wrong ASSERT_NOT_REACHED() from ClipboardChromium::clearData() that was
    causing us to crash whenever this method was called in a debug build.
    ClipboardData.clearData() was not covered by layout tests until now which is why we
    did not notice it before.
    
    TEST=fast/events/clipboard-dataTransferItemList.html
    R=haraken
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/23496045
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157601 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/chromium/ClipboardChromium.cpp
+++ third_party/WebKit/Source/core/platform/chromium/ClipboardChromium.cpp
@@ -247,8 +247,6 @@ void ClipboardChromium::clearData(const String& type)
         return;
 
     m_dataObject->clearData(normalizeType(type));
-
-    ASSERT_NOT_REACHED();
 }
 
 void ClipboardChromium::clearAllData()
