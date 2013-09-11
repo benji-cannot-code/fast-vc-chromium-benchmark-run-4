0cdced5e0fbf27ac374c61f92f73afc73f666e25
ager@chromium.org <ager@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-11 03:15:06 +0000

    Remove the document member on FontLoader. The FontLoader is already a ContextLifecycleObserver and therefore has access to the document.
    R=abarth@chromium.org
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/23618041
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157568 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/FontLoader.h
+++ third_party/WebKit/Source/core/css/FontLoader.h
@@ -74,7 +74,7 @@ public:
     using RefCounted<FontLoader>::ref;
     using RefCounted<FontLoader>::deref;
 
-    Document* document() const { return m_document; }
+    Document* document() const;
 
     void didLayout();
     void beginFontLoading(CSSFontFaceRule*);
@@ -109,7 +109,6 @@ private:
     bool resolveFontStyle(const String&, Font&);
     void timerFired(Timer<FontLoader>*);
 
-    Document* m_document;
     EventTargetData m_eventTargetData;
     unsigned m_loadingCount;
     Vector<RefPtr<Event> > m_pendingEvents;
