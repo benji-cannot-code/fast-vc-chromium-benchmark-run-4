1334fad0da744da145d47e07c82b1ade78ceac4b
Yukawa@chromium.org <Yukawa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-11 17:58:51 +0000

    Stop overriding InputMethodBase::SetFocusedTextInputClient
    With this CL, neither InputMethodTSF nor InputMethodIMM32
    no longer overrides default implementation.
    
    This CL also relaxes the condition of IsWindowFocused so that
    it can work when the attached window becomes active but has
    not received WM_FOCUS yet.
    
    This is a preparation CL for the main fix of Issue 287620. There
    should be no new regression with this CL as far as I've tested.
    
    In short, the main goal of this CL is just cleaning up the existing
    code w/o any regression so that we can keep the main fix of
    Issue 287620 as simple and minimum as possible.
    
    BUG=287620
    TEST=manually done with/without --enable-text-services-framework on Aura/non-Aura Windows 7.
    
    Review URL: https://chromiumcodereview.appspot.com/23475033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222590 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/ime/input_method_imm32.h
+++ ui/base/ime/input_method_imm32.h
@@ -30,7 +30,6 @@ class UI_EXPORT InputMethodIMM32 : public InputMethodWin {
   virtual void OnTextInputTypeChanged(const TextInputClient* client) OVERRIDE;
   virtual void OnCaretBoundsChanged(const TextInputClient* client) OVERRIDE;
   virtual void CancelComposition(const TextInputClient* client) OVERRIDE;
-  virtual void SetFocusedTextInputClient(TextInputClient* client) OVERRIDE;
   virtual bool IsCandidatePopupOpen() const OVERRIDE;
 
  protected:
@@ -71,10 +70,6 @@ class UI_EXPORT InputMethodIMM32 : public InputMethodWin {
 
   // Enables or disables the IME according to the current text input type.
   void UpdateIMEState();
-
-  // Returns true if the Win32 native window bound to |client| has Win32 input
-  // focus.
-  bool IsWindowFocused(const TextInputClient* client) const;
 
   bool enabled_;
 
