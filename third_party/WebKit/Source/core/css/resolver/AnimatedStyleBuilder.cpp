59a3acf2ae15e794cf23931fd8cef2ea540c052c
noel@chromium.org <noel@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-11 09:00:36 +0000

    Web Animations CSS: Templatize the integer round clamp routines
    Use a template to implement the animatiableValueTo<some int type>
    routines, and use the template.  Note: I'll remove old routines
    in a followup patch (patch diff is easier to read that way).
    
    No behavioral change, with existing tests playing coverage cop.
    
    Review URL: https://chromiumcodereview.appspot.com/23967009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157590 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/resolver/AnimatedStyleBuilder.cpp
+++ third_party/WebKit/Source/core/css/resolver/AnimatedStyleBuilder.cpp
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/css/resolver/StyleResolverState.h"
 #include "core/rendering/style/RenderStyle.h"
 #include "wtf/MathExtras.h"
+#include "wtf/TypeTraits.h"
 
 namespace WebCore {
 
@@ -74,6 +75,12 @@ unsigned short animatableValueToUnsignedShort(const AnimatableValue* value)
 int animatableValueToInt(const AnimatableValue* value)
 {
     return clampTo<int>(round(toAnimatableNumber(value)->toDouble()));
+}
+
+template<typename T> T animatableValueRoundClampTo(const AnimatableValue* value)
+{
+    COMPILE_ASSERT(WTF::IsInteger<T>::value, ShouldUseIntegralTypeTWhenRoundingValues);
+    return clampTo<T>(round(toAnimatableNumber(value)->toDouble()));
 }
 
 LengthBox animatableValueToLengthBox(const AnimatableValue* value, const StyleResolverState& state)
@@ -106,7 +113,7 @@ void AnimatedStyleBuilder::applyProperty(CSSPropertyID property, StyleResolverSt
         style->setVisitedLinkBorderBottomColor(toAnimatableColor(value)->visitedLinkColor());
         return;
     case CSSPropertyBorderBottomWidth:
-        style->setBorderBottomWidth(animatableValueToUnsigned(value));
+        style->setBorderBottomWidth(animatableValueRoundClampTo<unsigned>(value));
         return;
     case CSSPropertyBorderImageOutset:
         style->setBorderImageOutset(animatableValueToLengthBox(value, state));
@@ -125,21 +132,21 @@ void AnimatedStyleBuilder::applyProperty(CSSPropertyID property, StyleResolverSt
         style->setVisitedLinkBorderLeftColor(toAnimatableColor(value)->visitedLinkColor());
         return;
     case CSSPropertyBorderLeftWidth:
-        style->setBorderLeftWidth(animatableValueToUnsigned(value));
+        style->setBorderLeftWidth(animatableValueRoundClampTo<unsigned>(value));
         return;
     case CSSPropertyBorderRightColor:
         style->setBorderRightColor(toAnimatableColor(value)->color());
         style->setVisitedLinkBorderRightColor(toAnimatableColor(value)->visitedLinkColor());
         return;
     case CSSPropertyBorderRightWidth:
-        style->setBorderRightWidth(animatableValueToUnsigned(value));
+        style->setBorderRightWidth(animatableValueRoundClampTo<unsigned>(value));
         return;
     case CSSPropertyBorderTopColor:
         style->setBorderTopColor(toAnimatableColor(value)->color());
         style->setVisitedLinkBorderTopColor(toAnimatableColor(value)->visitedLinkColor());
         return;
     case CSSPropertyBorderTopWidth:
-        style->setBorderTopWidth(animatableValueToUnsigned(value));
+        style->setBorderTopWidth(animatableValueRoundClampTo<unsigned>(value));
         return;
     case CSSPropertyBottom:
         style->setBottom(animatableValueToLength(value, state));
@@ -195,7 +202,7 @@ void AnimatedStyleBuilder::applyProperty(CSSPropertyID property, StyleResolverSt
         style->setOutlineOffset(animatableValueToInt(value));
         return;
     case CSSPropertyOutlineWidth:
-        style->setOutlineWidth(animatableValueToUnsignedShort(value));
+        style->setOutlineWidth(animatableValueRoundClampTo<unsigned short>(value));
         return;
     case CSSPropertyPaddingBottom:
         style->setPaddingBottom(animatableValueToLength(value, state));
