f7f8e95d01dfc10d67f5c9b74a739f813b41941c
enne@chromium.org <enne@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-11 23:15:50 +0000

    Fix memory leaks in PageOverlay
    R=jamesr@chromium.org
    BUG=289039
    
    Review URL: https://chromiumcodereview.appspot.com/24075006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157629 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/PageOverlay.h
+++ third_party/WebKit/Source/web/PageOverlay.h
@@ -49,7 +49,7 @@ class PageOverlay {
 public:
     static PassOwnPtr<PageOverlay> create(WebViewImpl*, WebPageOverlay*);
 
-    ~PageOverlay();
+    ~PageOverlay() { }
 
     WebPageOverlay* overlay() const { return m_overlay; }
     void setOverlay(WebPageOverlay* overlay) { m_overlay = overlay; }
@@ -69,7 +69,7 @@ private:
 
     WebViewImpl* m_viewImpl;
     WebPageOverlay* m_overlay;
-    OverlayGraphicsLayerClientImpl* m_layerClient;
+    OwnPtr<WebCore::GraphicsLayerClient> m_layerClient;
     OwnPtr<WebCore::GraphicsLayer> m_layer;
     int m_zOrder;
 };
