ad185ae117154512a93033f60fe7d0d52bd0c599
cdn@chromium.org <cdn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-11 04:09:12 +0000

    Do not allow non web accessible resources to be linked.
    BUG=141462
    TEST=ExtensionResourceRequestPolicyTest.LinkToWebAccessibleResources
    
    Review URL: https://chromiumcodereview.appspot.com/22935006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222464 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/browser/content_browser_client.cc
+++ content/public/browser/content_browser_client.cc
@@ -61,6 +61,11 @@ bool ContentBrowserClient::CanCommitURL(RenderProcessHost* process_host,
   return true;
 }
 
+bool ContentBrowserClient::ShouldAllowOpenURL(SiteInstance* site_instance,
+                                              const GURL& url) {
+  return true;
+}
+
 bool ContentBrowserClient::IsSuitableHost(RenderProcessHost* process_host,
                                           const GURL& site_url) {
   return true;
