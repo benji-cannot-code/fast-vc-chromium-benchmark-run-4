f520140b4662929f94e4636e5138acd43d6dfdef
marja@chromium.org <marja@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-11 15:39:35 +0000

    Fix: Don't call HandleScope default ctor.
    It's actually already removed from bleeding edge V8. This call was introduced by
    r222039 ( https://chromiumcodereview.appspot.com/23548019 ).
    
    BUG=236173
    
    Review URL: https://chromiumcodereview.appspot.com/24098003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222569 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/translate/translate_script_browsertest.cc
+++ chrome/renderer/translate/translate_script_browsertest.cc
@@ -113,7 +113,7 @@ class TranslateScriptBrowserTest : public ChromeRenderViewTest {
 
   double ExecuteScriptAndGetNumberResult(const std::string& script) {
     WebScriptSource source = WebScriptSource(ASCIIToUTF16(script));
-    v8::HandleScope handle_scope;
+    v8::HandleScope handle_scope(v8::Isolate::GetCurrent());
     v8::Handle<v8::Value> result =
         GetMainFrame()->executeScriptAndReturnValue(source);
     if (result.IsEmpty() || !result->IsNumber()) {
@@ -127,7 +127,7 @@ class TranslateScriptBrowserTest : public ChromeRenderViewTest {
 
   bool ExecuteScriptAndGetBoolResult(const std::string& script) {
     WebScriptSource source = WebScriptSource(ASCIIToUTF16(script));
-    v8::HandleScope handle_scope;
+    v8::HandleScope handle_scope(v8::Isolate::GetCurrent());
     v8::Handle<v8::Value> result =
         GetMainFrame()->executeScriptAndReturnValue(source);
     if (result.IsEmpty() || !result->IsBoolean()) {
