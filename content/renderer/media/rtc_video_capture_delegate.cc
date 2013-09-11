b638c78697b6a0e08fe86602f7dd879b63ac4db0
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-11 18:26:31 +0000

    Remove NOTIMPLEMENTED()s from RtcVideoCaptureDelegate.
    These are spamming the logs on Chrome OS.
    
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/23451035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222596 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/rtc_video_capture_delegate.cc
+++ content/renderer/media/rtc_video_capture_delegate.cc
@@ -58,7 +58,6 @@ void RtcVideoCaptureDelegate::OnStopped(media::VideoCapture* capture) {
 }
 
 void RtcVideoCaptureDelegate::OnPaused(media::VideoCapture* capture) {
-  NOTIMPLEMENTED();
 }
 
 void RtcVideoCaptureDelegate::OnError(media::VideoCapture* capture,
@@ -95,13 +94,11 @@ void RtcVideoCaptureDelegate::OnBufferReady(
 void RtcVideoCaptureDelegate::OnDeviceInfoReceived(
     media::VideoCapture* capture,
     const media::VideoCaptureParams& device_info) {
-  NOTIMPLEMENTED();
 }
 
 void RtcVideoCaptureDelegate::OnDeviceInfoChanged(
     media::VideoCapture* capture,
     const media::VideoCaptureParams& device_info) {
-  NOTIMPLEMENTED();
 }
 
 void RtcVideoCaptureDelegate::OnBufferReadyOnCaptureThread(
