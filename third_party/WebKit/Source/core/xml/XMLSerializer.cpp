a84395caaa04bbf9385d71e829456db02a4362ff
vivek.vg@samsung.com <vivek.vg@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-11 22:54:07 +0000

    XMLSerializer.serializeToString() should throw exception for invalid node value.
    FF and Opera 12 (pre blink/webkit) throw exception in case the node value is invalid.
    Blink sends an empty string as the result of serializeToString operation with invalid node.
    We should match with other browsers for throwing it as an exception.
    
    R=eseidel, abarth
    
    Review URL: https://chromiumcodereview.appspot.com/23532055
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157617 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/xml/XMLSerializer.cpp
+++ third_party/WebKit/Source/core/xml/XMLSerializer.cpp
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  *  Copyright (C) 2003, 2006 Apple Inc. All rights reserved.
  *  Copyright (C) 2006 Samuel Weinig (sam@webkit.org)
+ *  Copyright (C) 2013 Samsung Electronics. All rights reserved.
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
@@ -31,8 +32,10 @@ namespace WebCore {
 
 String XMLSerializer::serializeToString(Node* node, ExceptionState& es)
 {
-    if (!node)
+    if (!node) {
+        es.throwDOMException(TypeError, "Invalid node value.");
         return String();
+    }
 
     return createMarkup(node);
 }
