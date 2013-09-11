777f3e0aa03b49fc5a49c321ac3329b4a270b146
dtrainor@chromium.org <dtrainor@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-11 16:43:25 +0000

    Expose the ability to perform a search from CVC.
    Allow contextual search attempts to be piped out through the ContentViewClient.
    
    BUG=249041
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/23654021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222573 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/search_engines/template_url_service_android.cc
+++ chrome/browser/search_engines/template_url_service_android.cc
@@ -20,7 +20,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_source.h"
 #include "jni/TemplateUrlService_jni.h"
 
+using base::android::ConvertJavaStringToUTF16;
 using base::android::ConvertUTF16ToJavaString;
+using base::android::ConvertUTF8ToJavaString;
 
 namespace {
 
@@ -137,6 +139,45 @@ TemplateUrlServiceAndroid::GetPrepopulatedTemplateUrlAt(JNIEnv* env,
 
 bool TemplateUrlServiceAndroid::IsPrepopulatedTemplate(TemplateURL* url) {
   return url->prepopulate_id() > 0;
+}
+
+base::android::ScopedJavaLocalRef<jstring>
+TemplateUrlServiceAndroid::GetUrlForSearchQuery(JNIEnv* env,
+                                                jobject obj,
+                                                jstring jquery) {
+  const TemplateURL* default_provider =
+      template_url_service_->GetDefaultSearchProvider();
+
+  string16 query(ConvertJavaStringToUTF16(env, jquery));
+
+  std::string url;
+  if (default_provider &&
+      default_provider->url_ref().SupportsReplacement() && !query.empty()) {
+    url = default_provider->url_ref().ReplaceSearchTerms(
+        TemplateURLRef::SearchTermsArgs(query));
+  }
+
+  return ConvertUTF8ToJavaString(env, url);
+}
+
+base::android::ScopedJavaLocalRef<jstring>
+TemplateUrlServiceAndroid::ReplaceSearchTermsInUrl(JNIEnv* env,
+                                                   jobject obj,
+                                                   jstring jquery,
+                                                   jstring jcurrent_url) {
+  TemplateURL* default_provider =
+      template_url_service_->GetDefaultSearchProvider();
+
+  string16 query(ConvertJavaStringToUTF16(env, jquery));
+  GURL current_url(ConvertJavaStringToUTF16(env, jcurrent_url));
+  GURL destination_url(current_url);
+  if (default_provider && !query.empty()) {
+    bool refined_query = default_provider->ReplaceSearchTermsInURL(current_url,
+        TemplateURLRef::SearchTermsArgs(query), &destination_url);
+    if (refined_query)
+      return ConvertUTF8ToJavaString(env, destination_url.spec());
+  }
+  return base::android::ScopedJavaLocalRef<jstring>(env, NULL);
 }
 
 static jint Init(JNIEnv* env, jobject obj) {
