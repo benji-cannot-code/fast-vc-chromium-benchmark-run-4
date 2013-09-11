40d6124c9091c409b7812306821607ec1f137947
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-11 11:58:41 +0000

    Except windows with an ExtensionHost from popup blocking.
    Previously, ExtensionHost::AddNewContents just didn't check the popup blocker. Since popups are now blocked before AddNewContents is invoked, we need to check whether the request is from a View with an ExtensionHost as host.
    
    BUG=285806
    R=mpcomplete@chromium.org
    TEST=manually, as described in the bug
    
    Review URL: https://chromiumcodereview.appspot.com/23447016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222496 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_system.h"
 #include "chrome/browser/extensions/extension_web_ui.h"
 #include "chrome/browser/extensions/extension_webkit_preferences.h"
+#include "chrome/browser/extensions/process_map.h"
 #include "chrome/browser/extensions/suggest_permission_util.h"
 #include "chrome/browser/geolocation/chrome_access_token_store.h"
 #include "chrome/browser/google/google_util.h"
@@ -1975,10 +1976,10 @@ bool ChromeContentBrowserClient::CanCreateWindow(
 
   // If the opener is trying to create a background window but doesn't have
   // the appropriate permission, fail the attempt.
-  if (container_type == WINDOW_CONTAINER_TYPE_BACKGROUND) {
-    ProfileIOData* io_data = ProfileIOData::FromResourceContext(context);
-    ExtensionInfoMap* map = io_data->GetExtensionInfoMap();
+  ProfileIOData* io_data = ProfileIOData::FromResourceContext(context);
+  ExtensionInfoMap* map = io_data->GetExtensionInfoMap();
 
+  if (container_type == WINDOW_CONTAINER_TYPE_BACKGROUND) {
     if (!map->SecurityOriginHasAPIPermission(
             source_origin,
             render_process_id,
@@ -2016,6 +2017,14 @@ bool ChromeContentBrowserClient::CanCreateWindow(
 
   if (is_guest)
     return true;
+
+  // Extensions are whitelisted for popup blocking.
+  const Extension* extension =
+      map->extensions().GetExtensionOrAppByURL(opener_url);
+  if (extension &&
+      map->process_map().Contains(extension->id(), render_process_id)) {
+    return true;
+  }
 
   HostContentSettingsMap* content_settings =
       ProfileIOData::FromResourceContext(context)->GetHostContentSettingsMap();
