c5d452851ea4792acfd25c6582681553deba852c
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-11 00:33:49 +0000

    Remove NOTIMPLEMENTED() in FindBarHost::AudibleAlert() for ChromeOS.
    BUG=None
    TEST=None
    R=jennyz,jamescook
    TBR=sky
    
    Review URL: https://chromiumcodereview.appspot.com/23819045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222422 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/find_bar_host_aura.cc
+++ chrome/browser/ui/views/find_bar_host_aura.cc
@@ -11,9 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 void FindBarHost::AudibleAlert() {
 #if defined(OS_WIN)
   MessageBeep(MB_OK);
-#else
-  // TODO(mukai):
-  NOTIMPLEMENTED();
 #endif
 }
 
