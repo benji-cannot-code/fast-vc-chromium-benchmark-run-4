1d53f42021958cd6e815391f2c7618c0f23053ac
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-11 03:58:55 +0000

    Do not wait for extensions to load in oobe screen.
    Regressed here: http://crrev.com/221621
    
    BUG=288259
    TEST=manual
    
    Review URL: https://chromiumcodereview.appspot.com/23819048
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222458 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/backends/chrome/chrome_browser_backend.py
+++ tools/telemetry/telemetry/core/backends/chrome/chrome_browser_backend.py
@@ -111,7 +111,7 @@ class ChromeBrowserBackend(browser_backend.BrowserBackend):
 
     return args
 
-  def _WaitForBrowserToComeUp(self, timeout=None):
+  def _WaitForBrowserToComeUp(self, wait_for_extensions=True, timeout=None):
     def IsBrowserUp():
       try:
         self.Request('', timeout=timeout)
@@ -143,7 +143,7 @@ class ChromeBrowserBackend(browser_backend.BrowserBackend):
         if not res:
           return False
       return True
-    if self._supports_extensions:
+    if wait_for_extensions and self._supports_extensions:
       util.WaitFor(AllExtensionsLoaded, timeout=30)
 
   def _PostBrowserStartupInitialization(self):
