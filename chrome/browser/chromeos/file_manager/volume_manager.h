305b9c04c531e4c4fadaaa0b6d544ba698b2de54
hidehiko@chromium.org <hidehiko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-12 11:03:03 +0000

    Fix compile error on ChromeOS Builder.
    Caused by missing scoped_ptr include. Adds it.
    
    BUG=279276
    TEST=Ran unit_tests
    TBR=kinaba
    
    Review URL: https://codereview.chromium.org/23591038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222757 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/file_manager/volume_manager.h
+++ chrome/browser/chromeos/file_manager/volume_manager.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/files/file_path.h"
+#include "base/memory/scoped_ptr.h"
 #include "base/observer_list.h"
 #include "chromeos/disks/disk_mount_manager.h"
 #include "components/browser_context_keyed_service/browser_context_keyed_service.h"
