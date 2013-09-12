a74ed3bd6d85ff0d232f0ff692b4a0e4e8e31978
justinlin@chromium.org <justinlin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-12 00:10:40 +0000

    Implement service discovery layer for chrome.mdns.
    BUG=280900
    TBR=mpcomplete
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=222606
    
    Review URL: https://chromiumcodereview.appspot.com/23828004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222668 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/extensions/api/mdns/dns_sd_delegate.cc
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/extensions/api/mdns/dns_sd_delegate.h"
+
+namespace extensions {
+
+DnsSdService::DnsSdService() {}
+DnsSdService::~DnsSdService() {}
+
+}  // namespace extensions
