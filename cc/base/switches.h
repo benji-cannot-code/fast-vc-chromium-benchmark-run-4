93ef175b97fab3963528b650e6b4f8f23dd23c96
alokp@chromium.org <alokp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-12 23:06:33 +0000

    Expose LCD text setting to about:flags.
    BUG=287900
    
    Review URL: https://chromiumcodereview.appspot.com/23578029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222903 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/base/switches.h
+++ cc/base/switches.h
@@ -18,9 +18,11 @@ namespace switches {
 
 // Switches for the renderer compositor only.
 CC_EXPORT extern const char kBackgroundColorInsteadOfCheckerboard[];
+CC_EXPORT extern const char kDisableLCDText[];
 CC_EXPORT extern const char kDisableImplSidePainting[];
 CC_EXPORT extern const char kDisableThreadedAnimation[];
 CC_EXPORT extern const char kDisableCompositedAntialiasing[];
+CC_EXPORT extern const char kEnableLCDText[];
 CC_EXPORT extern const char kEnableImplSidePainting[];
 CC_EXPORT extern const char kEnableTopControlsPositionCalculation[];
 CC_EXPORT extern const char kForceDirectLayerDrawing[];
@@ -67,6 +69,7 @@ CC_EXPORT extern const char kUIShowNonOccludingRects[];
 // Unit test related.
 CC_EXPORT extern const char kCCLayerTreeTestNoTimeout[];
 
+CC_EXPORT bool IsLCDTextEnabled();
 CC_EXPORT bool IsImplSidePaintingEnabled();
 CC_EXPORT bool IsMapImageEnabled();
 
