472621e5e9106ef236ab502dd27214f7170761a4
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-12 06:09:42 +0000

    rAc: Remove unused Edit functionality.
    Biggest change is updating the tests.
    
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/23691051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222727 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/autofill/autofill_dialog_controller_impl.cc
+++ chrome/browser/ui/autofill/autofill_dialog_controller_impl.cc
@@ -1515,24 +1515,6 @@ gfx::Image AutofillDialogControllerImpl::ExtraSuggestionIconForSection(
   return gfx::Image();
 }
 
-void AutofillDialogControllerImpl::EditClickedForSection(
-    DialogSection section) {
-  ScopedViewUpdates updates(view_.get());
-  scoped_ptr<DataModelWrapper> model = CreateWrapper(section);
-  SetEditingExistingData(section, true);
-
-  DetailInputs* inputs = MutableRequestedFieldsForSection(section);
-  for (DetailInputs::iterator it = inputs->begin(); it != inputs->end(); ++it) {
-    it->editable = InputIsEditable(*it, section);
-  }
-  model->FillInputs(inputs);
-
-  UpdateSection(section);
-
-  GetMetricLogger().LogDialogUiEvent(
-      common::DialogSectionToUiEditEvent(section));
-}
-
 gfx::Image AutofillDialogControllerImpl::IconForField(
     ServerFieldType type, const string16& user_input) const {
   ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
@@ -2183,6 +2165,8 @@ void AutofillDialogControllerImpl::OnDidGetFullWallet(
 
     case wallet::VERIFY_CVV:
       SuggestionsUpdated();
+      view_->UpdateButtonStrip();
+      view_->UpdateNotificationArea();
       break;
 
     default:
