84cfee1faa16948a1e02f66d9648c26344b068a9
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-12 22:52:01 +0000

    add missing #include of <algorithm>, needed on VS2013 for std::min
    R=penghuang@chromium.org
    BUG=288948
    
    Review URL: https://chromiumcodereview.appspot.com/23872017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222896 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/ime/win/tsf_text_store.cc
+++ ui/base/ime/win/tsf_text_store.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <InputScope.h>
 #include <OleCtl.h>
 
+#include <algorithm>
+
 #include "base/win/scoped_variant.h"
 #include "ui/base/ime/text_input_client.h"
 #include "ui/base/ime/win/tsf_input_scope.h"
