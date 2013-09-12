1e1a9fd5b3826dda11fda472da05474fd045b1de
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-12 00:27:19 +0000

    Exempt extension processes from popup blocking.
    BUG=285806
    R=bauerb@chromium.org
    TEST=manual, as described in the bug
    
    Review URL: https://chromiumcodereview.appspot.com/23435005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222672 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_system.h"
 #include "chrome/browser/extensions/extension_web_ui.h"
 #include "chrome/browser/extensions/extension_webkit_preferences.h"
+#include "chrome/browser/extensions/process_map.h"
 #include "chrome/browser/extensions/suggest_permission_util.h"
 #include "chrome/browser/geolocation/chrome_access_token_store.h"
 #include "chrome/browser/google/google_util.h"
@@ -1954,12 +1955,12 @@ bool ChromeContentBrowserClient::CanCreateWindow(
 
   *no_javascript_access = false;
 
+  ProfileIOData* io_data = ProfileIOData::FromResourceContext(context);
+  ExtensionInfoMap* map = io_data->GetExtensionInfoMap();
+
   // If the opener is trying to create a background window but doesn't have
   // the appropriate permission, fail the attempt.
   if (container_type == WINDOW_CONTAINER_TYPE_BACKGROUND) {
-    ProfileIOData* io_data = ProfileIOData::FromResourceContext(context);
-    ExtensionInfoMap* map = io_data->GetExtensionInfoMap();
-
     if (!map->SecurityOriginHasAPIPermission(
             source_origin,
             render_process_id,
@@ -1996,6 +1997,10 @@ bool ChromeContentBrowserClient::CanCreateWindow(
   }
 
   if (is_guest)
+    return true;
+
+  // Exempt extension processes from popup blocking.
+  if (map->process_map().Contains(render_process_id))
     return true;
 
   HostContentSettingsMap* content_settings =
