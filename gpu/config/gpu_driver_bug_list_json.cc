0aacc21174d9420fef4309fd4372a8b88724e9f9
skyostil@chromium.org <skyostil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-12 13:07:19 +0000

    gpu: Re-enable virtual contexts on Qualcomm
    Non-virtual contexts on Qualcomm GPUs sometimes cause out-of-order
    frames to be displayed, so turn GL context virtualization back on.
    
    BUG=289461
    
    Review URL: https://chromiumcodereview.appspot.com/23460029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222769 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/config/gpu_driver_bug_list_json.cc
+++ gpu/config/gpu_driver_bug_list_json.cc
@@ -86,7 +86,7 @@ const char kGpuDriverBugListJson[] = LONG_STRING_CONST(
 {
   "name": "gpu driver bug list",
   // Please update the version number whenever you change this file.
-  "version": "2.10",
+  "version": "2.11",
   "entries": [
     {
       "id": 1,
@@ -575,6 +575,21 @@ const char kGpuDriverBugListJson[] = LONG_STRING_CONST(
       "vendor_id": "0x10de",
       "features": [
         "init_gl_position_in_vertex_shader"
+      ]
+    },
+    {
+      "id": 38,
+      "cr_bugs": [289461],
+      "description": "Non-virtual contexts on Qualcomm sometimes cause out-of-order frames",
+      "os": {
+        "type": "android"
+      },
+      "gl_vendor": {
+        "op": "beginwith",
+        "value": "Qualcomm"
+      },
+      "features": [
+        "use_virtualized_gl_contexts"
       ]
     }
   ]
