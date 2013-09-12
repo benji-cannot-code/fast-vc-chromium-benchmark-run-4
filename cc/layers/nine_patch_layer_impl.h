f9e89ae8082d2e3d7a24b711360b3019394607d9
powei@chromium.org <powei@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-12 06:18:19 +0000

    Update the nine patch layer to use UI resources
    The old nine-patch layer used priority resource manager for requesting textures.
    This patch updates the nine-patch layer to use the UI resource manager. 
    this patch clarifies the semantics of the aperture in both image and layer
    The new semantics corresponds to existing logic on the android-side.
    
    Changes have been made to UIResourceBitmap to use SkPixelRef as ref-counted
    of the bitmap content.
    
    The android-side changes:
    https://gerrit-int.chromium.org/#/c/43103/
    
    BUG=276482,276487
    
    Review URL: https://chromiumcodereview.appspot.com/22870016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222732 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/layers/nine_patch_layer_impl.h
+++ cc/layers/nine_patch_layer_impl.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/base/cc_export.h"
 #include "cc/layers/layer_impl.h"
 #include "cc/resources/resource_provider.h"
+#include "cc/resources/ui_resource_client.h"
 #include "ui/gfx/rect.h"
 #include "ui/gfx/size.h"
 
@@ -28,8 +29,37 @@ class CC_EXPORT NinePatchLayerImpl : public LayerImpl {
   }
   virtual ~NinePatchLayerImpl();
 
-  void SetResourceId(unsigned id) { resource_id_ = id; }
-  void SetLayout(gfx::Size image_bounds, gfx::Rect aperture);
+
+  void SetUIResourceId(UIResourceId uid);
+
+  // The bitmap stretches out the bounds of the layer.  The following picture
+  // illustrates the parameters associated with the dimensions.
+  //
+  // Layer space layout              Bitmap space layout
+  //
+  // ------------------------       ~~~~~~~~~~ W ~~~~~~~~~~
+  // |          :           |       :     :                |
+  // |          C           |       :     Y                |
+  // |          :           |       :     :                |
+  // |     ------------     |       :~~X~~------------     |
+  // |     |          |     |       :     |          :     |
+  // |     |          |     |       :     |          :     |
+  // |~~A~~|          |~~B~~|       H     |          Q     |
+  // |     |          |     |       :     |          :     |
+  // |     ------------     |       :     ~~~~~P~~~~~      |
+  // |          :           |       :                      |
+  // |          D           |       :                      |
+  // |          :           |       :                      |
+  // ------------------------       ------------------------
+  //
+  // |image_bounds| = (W, H)
+  // |image_aperture| = (X, Y, P, Q)
+  // |border| = (A, C, A + B, C + D)
+  // |fill_center| indicates whether to draw the center quad or not.
+  void SetLayout(gfx::Size image_bounds,
+                 gfx::Rect image_aperture,
+                 gfx::Rect border,
+                 bool fill_center);
 
   virtual scoped_ptr<LayerImpl> CreateLayerImpl(LayerTreeImpl* tree_impl)
       OVERRIDE;
@@ -40,7 +70,6 @@ class CC_EXPORT NinePatchLayerImpl : public LayerImpl {
   virtual void AppendQuads(QuadSink* quad_sink,
                            AppendQuadsData* append_quads_data) OVERRIDE;
   virtual ResourceProvider::ResourceId ContentsResourceId() const OVERRIDE;
-  virtual void DidLoseOutputSurface() OVERRIDE;
 
   virtual base::DictionaryValue* LayerTreeAsJson() const OVERRIDE;
 
@@ -57,7 +86,12 @@ class CC_EXPORT NinePatchLayerImpl : public LayerImpl {
   // image space.
   gfx::Rect image_aperture_;
 
-  ResourceProvider::ResourceId resource_id_;
+  // An inset border that the patches will be mapped to.
+  gfx::Rect border_;
+
+  bool fill_center_;
+
+  UIResourceId ui_resource_id_;
 
   DISALLOW_COPY_AND_ASSIGN(NinePatchLayerImpl);
 };
