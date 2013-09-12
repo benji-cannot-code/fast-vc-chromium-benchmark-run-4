d97ad623200f90cbf709323baf3b459e28f7bc98
avayvod@chromium.org <avayvod@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-12 00:38:06 +0000

    Added a new runtime setting for RequireUserGestureForFullscreenRestriction.
    Control the restriction requiring user gesture for the media element to enter
    fullscreen with the new mediaFullscreenRequiresUserGesture setting.
    
    BUG=288678
    
    Review URL: https://chromiumcodereview.appspot.com/23440027
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157643 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLMediaElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLMediaElement.cpp
@@ -259,7 +259,7 @@ HTMLMediaElement::HTMLMediaElement(const QualifiedName& tagName, Document& docum
     , m_lastTimeUpdateEventWallTime(0)
     , m_lastTimeUpdateEventMovieTime(numeric_limits<double>::max())
     , m_loadState(WaitingForSource)
-    , m_restrictions(RequireUserGestureForFullscreenRestriction | RequirePageConsentToLoadMediaRestriction)
+    , m_restrictions(RequirePageConsentToLoadMediaRestriction)
     , m_preload(MediaPlayer::Auto)
     , m_displayMode(Unknown)
     , m_cachedTime(MediaPlayer::invalidTime())
@@ -301,9 +301,14 @@ HTMLMediaElement::HTMLMediaElement(const QualifiedName& tagName, Document& docum
     LOG(Media, "HTMLMediaElement::HTMLMediaElement");
     ScriptWrappable::init(this);
 
-    if (document.settings() && document.settings()->mediaPlaybackRequiresUserGesture()) {
-        addBehaviorRestriction(RequireUserGestureForRateChangeRestriction);
-        addBehaviorRestriction(RequireUserGestureForLoadRestriction);
+    if (document.settings()) {
+        if (document.settings()->mediaPlaybackRequiresUserGesture()) {
+            addBehaviorRestriction(RequireUserGestureForRateChangeRestriction);
+            addBehaviorRestriction(RequireUserGestureForLoadRestriction);
+        }
+        if (document.settings()->mediaFullscreenRequiresUserGesture()) {
+            addBehaviorRestriction(RequireUserGestureForFullscreenRestriction);
+        }
     }
 
     setHasCustomStyleCallbacks();
