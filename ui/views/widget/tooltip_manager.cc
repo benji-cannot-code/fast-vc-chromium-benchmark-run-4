445166928fa0259dda87cd7877de75dd0d4a813f
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-12 21:44:28 +0000

    add missing #include of <algorithm>, needed on VS2013 for std::max
    R=sadrul@chromium.org
    BUG=288948
    
    Review URL: https://chromiumcodereview.appspot.com/23619040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222875 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/tooltip_manager.cc
+++ ui/views/widget/tooltip_manager.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/views/widget/tooltip_manager.h"
 
+#include <algorithm>
 #include <vector>
 
 #include "base/strings/string_split.h"
