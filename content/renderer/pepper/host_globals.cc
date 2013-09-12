e76b99f03db8774c9f2c303912df8237bac780ac
dmichael@chromium.org <dmichael@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-12 19:08:54 +0000

    PPAPI: Purposely leak ProxyLock, fix shutdown race
    This CL:
    - Makes callbacks returned by RunWhileLocked acquire the lock when they are destroyed if they are not run. This eliminates some potential race conditions.
    - Because MessageLoops can be destroyed after the PpapiGlobals (and thus the ProxyLock), this means we need to make the ProxyLock live longer. So we leak it so that it lives all the way through shutdown.
    
    This issue depends on: https://codereview.chromium.org/19492014/
    
    BUG=
    R=yzshen@chromium.org
    
    Review URL: https://codereview.chromium.org/19492014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222829 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/pepper/host_globals.cc
+++ content/renderer/pepper/host_globals.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/render_thread_impl.h"
 #include "ppapi/shared_impl/api_id.h"
 #include "ppapi/shared_impl/id_assignment.h"
+#include "ppapi/shared_impl/proxy_lock.h"
 #include "third_party/WebKit/public/platform/WebString.h"
 #include "third_party/WebKit/public/web/WebConsoleMessage.h"
 #include "third_party/WebKit/public/web/WebDocument.h"
@@ -82,13 +83,9 @@ HostGlobals::HostGlobals()
       resource_tracker_(ResourceTracker::SINGLE_THREADED) {
   DCHECK(!host_globals_);
   host_globals_ = this;
-}
-
-HostGlobals::HostGlobals(
-    ppapi::PpapiGlobals::PerThreadForTest per_thread_for_test)
-    : ppapi::PpapiGlobals(per_thread_for_test),
-      resource_tracker_(ResourceTracker::SINGLE_THREADED) {
-  DCHECK(!host_globals_);
+  // We do not support calls off of the main thread on the host side, and thus
+  // do not lock.
+  ppapi::ProxyLock::DisableLocking();
 }
 
 HostGlobals::~HostGlobals() {
@@ -140,11 +137,6 @@ std::string HostGlobals::GetCmdLine() {
 
 void HostGlobals::PreCacheFontForFlash(const void* logfontw) {
   // Not implemented in-process.
-}
-
-base::Lock* HostGlobals::GetProxyLock() {
-  // We do not lock on the host side.
-  return NULL;
 }
 
 void HostGlobals::LogWithSource(PP_Instance instance,
