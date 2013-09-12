b5451e173b1caf93f8390c7d2df8403e2c1bad2c
sammc@chromium.org <sammc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-12 23:05:04 +0000

    Make permission details expandable in the GTK permissions dialog.
    This makes the retained files, permission details and OAuth grant
    details hidden by default and expandable to show the full details. This
    also makes the permission dialog scrollable.
    
    BUG=70422
    
    Review URL: https://chromiumcodereview.appspot.com/23442014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222900 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_install_prompt.h
+++ chrome/browser/extensions/extension_install_prompt.h
@@ -107,7 +107,6 @@ class ExtensionInstallPrompt
     string16 GetPermissionsHeading() const;
     string16 GetOAuthHeading() const;
     string16 GetRetainedFilesHeading() const;
-    string16 GetRetainedFilesHeadingWithCount() const;
 
     bool ShouldShowPermissions() const;
 
