31f269e6c72b90cdc2eae153530e845c5425a326
gavinp@chromium.org <gavinp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-12 01:50:47 +0000

    Track entries pending Doom in SimpleCache backend.
    Building on Philippe's recent work with doom operations on entries,
    this tracks entries which don't exist in the backend.
    
    The new SimpleBackendImpl::entries_pending_doom_ tracks entries that
    are in the process of being doomed. While entries are in this state,
    it is not legal to create a new active entry, instead the backend
    should wait until the doom is complete.
    
    There's still an outstanding issue of mass dooms (evictions, doombetween...)
    not using this new mechanism, which is to be fixed in a followup CL.
    
    R=rdsmith,pliard,ttuttle,pasko
    BUG=289542
    
    Review URL: https://chromiumcodereview.appspot.com/23486006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222695 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/backend_unittest.cc
+++ net/disk_cache/backend_unittest.cc
@@ -3192,7 +3192,9 @@ TEST_F(DiskCacheBackendTest, SimpleDoomBetween) {
 }
 
 // See http://crbug.com/237450.
-TEST_F(DiskCacheBackendTest, FLAKY_SimpleCacheDoomAll) {
+// TODO(gavinp): Consider enabling this test again when
+// https://codereview.chromium.org/23823002/ lands.
+TEST_F(DiskCacheBackendTest, DISABLED_SimpleCacheDoomAll) {
   SetSimpleCacheMode();
   BackendDoomAll();
 }
