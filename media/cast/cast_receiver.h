938bf7aa4c9e9f5849d23f106cbae1e62d0cd3f5
pwestin@google.com <pwestin@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-12 00:05:01 +0000

    Added cast sender impl and made required modifications to the pacer.
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/24051002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222666 0039d316-1c4b-4281-b951-d872f2087c98

--- media/cast/cast_receiver.h
+++ media/cast/cast_receiver.h
@@ -2,6 +2,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2013 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+//
+// This is the main interface for the cast receiver. All configuration are done
+// at creation.
 
 #ifndef MEDIA_CAST_CAST_RECEIVER_H_
 #define MEDIA_CAST_CAST_RECEIVER_H_
@@ -13,8 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace media {
 namespace cast {
 
-class FrameReceiver {
+// This Class is thread safe.
+class FrameReceiver : public base::RefCountedThreadSafe<FrameReceiver>{
  public:
+  // TODO(pwestin): These functions must be updated.
   virtual bool GetRawVideoFrame(I420VideoFrame* video_frame,
                                 base::TimeTicks* render_time) = 0;
 
@@ -33,24 +38,24 @@ class FrameReceiver {
 
   virtual void ReleaseCodedAudioFrame(uint8 frame_id) = 0;
 
-protected:
   virtual ~FrameReceiver() {}
 };
 
+// This Class is thread safe.
 class CastReceiver {
  public:
   static CastReceiver* CreateCastReceiver(
+      scoped_refptr<CastThread> cast_thread,
       const AudioReceiverConfig& audio_config,
       const VideoReceiverConfig& video_config,
       PacketSender* const packet_sender);
 
   // All received RTP and RTCP packets for the call should be inserted to this
-  // PacketReceiver. The PacketReceiver pointer is valid as long as the
-  // CastReceiver instance exists.
-  virtual PacketReceiver* packet_receiver() = 0;
+  // PacketReceiver.
+  virtual scoped_refptr<PacketReceiver> packet_receiver() = 0;
 
   // Polling interface to get audio and video frames from the CastReceiver.
-  virtual FrameReceiver* frame_receiver() = 0;
+  virtual scoped_refptr<FrameReceiver> frame_receiver() = 0;
 
   virtual ~CastReceiver() {};
 };
--- media/cast/cast_sender.gyp
+++ media/cast/cast_sender.gyp
@@ -13,10 +13,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     {
       'target_name': 'cast_sender_impl',
       'type': 'static_library',
+      'include_dirs': [
+        '<(DEPTH)/',
+        '<(DEPTH)/third_party/',
+        '<(DEPTH)/third_party/webrtc/',
+      ],
       'sources': [
         'cast_sender.h',
-#        'cast_sender_impl.cc',
-#        'cast_sender_impl.h',
+        'cast_sender_impl.cc',
+        'cast_sender_impl.h',
       ], # source
       'dependencies': [
         'audio_sender',
