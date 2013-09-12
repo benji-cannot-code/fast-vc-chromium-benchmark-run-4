f4a1fa7e4a990db4c4ca09f3c32ccafc3d611c91
noms@chromium.org <noms@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-12 23:32:16 +0000

    Redesign the avatar menu bubble to match the new mocks. This is behind the --new-profile-management flag.
    Screenshots: https://drive.google.com/folderview?id=0B1B1Up4p2NRMcUVwc3pvQXAzWHc&usp=sharing
    
    BUG=287883
    
    Review URL: https://chromiumcodereview.appspot.com/23644005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222908 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_info_cache.cc
+++ chrome/browser/profiles/profile_info_cache.cc
@@ -41,6 +41,7 @@ namespace {
 const char kNameKey[] = "name";
 const char kShortcutNameKey[] = "shortcut_name";
 const char kGAIANameKey[] = "gaia_name";
+const char kGAIAGivenNameKey[] = "gaia_given_name";
 const char kUseGAIANameKey[] = "use_gaia_name";
 const char kUserNameKey[] = "user_name";
 const char kAvatarIconKey[] = "avatar_icon";
@@ -285,8 +286,11 @@ size_t ProfileInfoCache::GetIndexOfProfileWithPath(
 
 string16 ProfileInfoCache::GetNameOfProfileAtIndex(size_t index) const {
   string16 name;
-  if (IsUsingGAIANameOfProfileAtIndex(index))
-    name = GetGAIANameOfProfileAtIndex(index);
+  if (IsUsingGAIANameOfProfileAtIndex(index)) {
+    string16 given_name = GetGAIAGivenNameOfProfileAtIndex(index);
+    name = given_name.empty() ? GetGAIANameOfProfileAtIndex(index) : given_name;
+  }
+
   if (name.empty())
     GetInfoForProfileAtIndex(index)->GetString(kNameKey, &name);
   return name;
@@ -336,6 +340,13 @@ bool ProfileInfoCache::GetBackgroundStatusOfProfileAtIndex(
 string16 ProfileInfoCache::GetGAIANameOfProfileAtIndex(size_t index) const {
   string16 name;
   GetInfoForProfileAtIndex(index)->GetString(kGAIANameKey, &name);
+  return name;
+}
+
+string16 ProfileInfoCache::GetGAIAGivenNameOfProfileAtIndex(
+    size_t index) const {
+  string16 name;
+  GetInfoForProfileAtIndex(index)->GetString(kGAIAGivenNameKey, &name);
   return name;
 }
 
@@ -543,6 +554,18 @@ void ProfileInfoCache::SetGAIANameOfProfileAtIndex(size_t index,
                       observer_list_,
                       OnProfileNameChanged(profile_path, old_display_name));
   }
+}
+
+void ProfileInfoCache::SetGAIAGivenNameOfProfileAtIndex(
+    size_t index,
+    const string16& name) {
+  if (name == GetGAIAGivenNameOfProfileAtIndex(index))
+    return;
+
+  scoped_ptr<DictionaryValue> info(GetInfoForProfileAtIndex(index)->DeepCopy());
+  info->SetString(kGAIAGivenNameKey, name);
+  // This takes ownership of |info|.
+  SetInfoForProfileAtIndex(index, info.release());
 }
 
 void ProfileInfoCache::SetIsUsingGAIANameOfProfileAtIndex(size_t index,
--- chrome/browser/profiles/profile_window.cc
+++ chrome/browser/profiles/profile_window.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if !defined(OS_IOS)
 #include "chrome/browser/ui/browser_finder.h"
+#include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/startup/startup_browser_creator.h"
 #endif  // !defined (OS_IOS)
@@ -85,6 +86,16 @@ void SwitchToProfile(
       string16(),
       string16(),
       std::string());
+}
+
+void CloseGuestProfileWindows() {
+  ProfileManager* profile_manager = g_browser_process->profile_manager();
+  Profile* profile = profile_manager->GetProfileByPath(
+      ProfileManager::GetGuestProfilePath());
+
+  if (profile) {
+    BrowserList::CloseAllBrowsersWithProfile(profile);
+  }
 }
 
 }  // namespace profiles
