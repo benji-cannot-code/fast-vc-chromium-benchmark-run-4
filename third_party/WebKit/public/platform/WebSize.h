368e6db994f710c06798131a245a3804686f12c3
jamesr@chromium.org <jamesr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-12 23:27:35 +0000

    Compile fixes for VS2013
    TBR=abarth
    
    Review URL: https://chromiumcodereview.appspot.com/24076012
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157714 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebSize.h
+++ third_party/WebKit/public/platform/WebSize.h
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if WEBKIT_IMPLEMENTATION
 #include "core/platform/graphics/IntSize.h"
 #else
+#include <algorithm>
 #include <cmath>
 #include <ui/gfx/size.h>
 #include <ui/gfx/vector2d.h>
