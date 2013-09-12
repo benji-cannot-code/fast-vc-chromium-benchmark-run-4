ca2bae3f3babc23f60f106ff69ae30076929b419
toyoshim@chromium.org <toyoshim@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-12 08:11:03 +0000

    Web MIDI: tentative sysex check for receiving messages
    Strict MIDI sequence checker should be introduced and shared between
    sender side and receiver side. But, tentatively enable it with minimum
    change in receiver side to confirm a browser side change works.
    
    BUG=163795
    TEST=LayoutTests/webmidi
    
    Review URL: https://chromiumcodereview.appspot.com/23648006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157664 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/webmidi/MIDIInput.cpp
+++ third_party/WebKit/Source/modules/webmidi/MIDIInput.cpp
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 #include "modules/webmidi/MIDIInput.h"
+
+#include "modules/webmidi/MIDIAccess.h"
 #include "modules/webmidi/MIDIMessageEvent.h"
 
 namespace WebCore {
@@ -60,8 +62,8 @@ void MIDIInput::didReceiveMIDIData(unsigned portIndex, const unsigned char* data
         unsigned char status = data[i];
         unsigned char strippedStatus = status & 0xf0;
 
-        // FIXME: support System Exclusive.
-        if (strippedStatus >= 0xf0)
+        // FIXME: integrate sending side filtering and implement more extensive filtering.
+        if (strippedStatus >= 0xf0 && !m_access->sysExEnabled())
             break;
 
         // All non System Exclusive messages have a total size of 3 except for Program Change and Channel Pressure.
