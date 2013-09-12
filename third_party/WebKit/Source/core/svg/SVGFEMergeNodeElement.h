8ff56e27c305e488360403bb765750fe123dab67
gyuyoung.kim@samsung.com <gyuyoung.kim@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-12 05:54:43 +0000

    Add toSVGFEMergeNodeElement() fuction, and use it.
    This function has type checking assertions for helping to find bad-cast bugs.
    
    BUG=N/A
    
    Review URL: https://chromiumcodereview.appspot.com/23581013
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157660 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGFEMergeNodeElement.h
+++ third_party/WebKit/Source/core/svg/SVGFEMergeNodeElement.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SVGFEMergeNodeElement_h
 #define SVGFEMergeNodeElement_h
 
+#include "SVGNames.h"
 #include "core/svg/SVGAnimatedString.h"
 #include "core/svg/SVGElement.h"
 
@@ -44,6 +45,12 @@ private:
         DECLARE_ANIMATED_STRING(In1, in1)
     END_DECLARE_ANIMATED_PROPERTIES
 };
+
+inline SVGFEMergeNodeElement* toSVGFEMergeNodeElement(Node* node)
+{
+    ASSERT_WITH_SECURITY_IMPLICATION(!node || node->hasTagName(SVGNames::feMergeNodeTag));
+    return static_cast<SVGFEMergeNodeElement*>(node);
+}
 
 } // namespace WebCore
 
