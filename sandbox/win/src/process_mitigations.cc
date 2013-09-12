af4f5f4210e1b8e0a3f0e5a4f001fa704500a9a2
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-12 22:23:20 +0000

    add missing #include of <algorithm>, needed on VS2013 for std::min
    TBR=jschuh@chromium.org
    BUG=288948
    
    Review URL: https://chromiumcodereview.appspot.com/23618047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222886 0039d316-1c4b-4281-b951-d872f2087c98

--- sandbox/win/src/process_mitigations.cc
+++ sandbox/win/src/process_mitigations.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "sandbox/win/src/process_mitigations.h"
 
+#include <algorithm>
+
 #include "base/win/windows_version.h"
 #include "sandbox/win/src/nt_internals.h"
 #include "sandbox/win/src/sandbox_utils.h"
