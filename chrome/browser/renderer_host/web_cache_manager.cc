4626e7905303f214a7d34b495a8aaa886d19d4b1
pliard@chromium.org <pliard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-12 14:40:47 +0000

    Limit max dead resources capacity to 512 KBytes on low-end Android devices.
    Manual browsing on a few web sites showed that dead resources do not contribute
    enough to the hit rate to justify the memory cost on low-end devices (where
    every 100KBytes matters).
    This is a short term workaround with a direct impact for decreasing memory
    usage. More long term efforts will follow.
    
    BUG=280984,285374
    R=darin@chromium.org
    
    Review URL: https://codereview.chromium.org/23576011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222784 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/web_cache_manager.cc
+++ chrome/browser/renderer_host/web_cache_manager.cc
@@ -24,6 +24,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/render_process_host.h"
 
+#if defined(OS_ANDROID)
+#include "base/android/sys_utils.h"
+#endif
+
 using base::Time;
 using base::TimeDelta;
 using WebKit::WebCache;
@@ -323,6 +327,10 @@ void WebCacheManager::EnactStrategy(const AllocationStrategy& strategy) {
 
       // We allow the dead objects to consume up to half of the cache capacity.
       size_t max_dead_capacity = capacity / 2;
+#if defined(OS_ANDROID)
+      if (base::android::SysUtils::IsLowEndDevice())
+        max_dead_capacity = std::min(512 * 1024U, max_dead_capacity);
+#endif
 
       host->Send(new ChromeViewMsg_SetCacheCapacities(min_dead_capacity,
                                                       max_dead_capacity,
