7b1f2cc80b198f14302153de25d21f38ebefc6ed
dmazzoni@chromium.org <dmazzoni@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-13 02:43:16 +0000

    Keep track of accessibility ids serialized to avoid duplicates/loops.
    This fixes a top renderer crash, see bug for details.
    
    BUG=290233
    
    Review URL: https://chromiumcodereview.appspot.com/23637013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222969 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/accessibility/renderer_accessibility_complete.h
+++ content/renderer/accessibility/renderer_accessibility_complete.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_RENDERER_ACCESSIBILITY_RENDERER_ACCESSIBILITY_COMPLETE_H_
 #define CONTENT_RENDERER_ACCESSIBILITY_RENDERER_ACCESSIBILITY_COMPLETE_H_
 
+#include <set>
 #include <vector>
 
 #include "base/containers/hash_tables.h"
@@ -76,8 +77,11 @@ class CONTENT_EXPORT RendererAccessibilityComplete
   // Serialize the given accessibility object |obj| and append it to
   // |dst|, and then recursively also serialize any *new* children of
   // |obj|, based on what object ids we know the browser already has.
+  // The set of ids serialized is added to |ids_serialized|, and any
+  // ids previously in that set are not serialized again.
   void SerializeChangedNodes(const WebKit::WebAXObject& obj,
-                             std::vector<AccessibilityNodeData>* dst);
+                             std::vector<AccessibilityNodeData>* dst,
+                             std::set<int>* ids_serialized);
 
   // Clear the given node and recursively delete all of its descendants
   // from the browser tree. (Does not delete |browser_node|).
