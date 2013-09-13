04408f48a0921d9125882c557c9a8c0248242a67
ch.dumez@sisa.samsung.com <ch.dumez@sisa.samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-13 17:29:44 +0000

    Remove Document::getElementById()
    Remove Document::getElementById() as it was merely calling TreeScope::getElementById().
    Document already inherits from TreeScope, therefore this indirection is not needed.
    
    R=haraken
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/23995011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157763 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -618,11 +618,6 @@ void Document::dispose()
     lifecycleNotifier()->notifyDocumentWasDisposed();
 }
 
-Element* Document::getElementById(const AtomicString& id) const
-{
-    return TreeScope::getElementById(id);
-}
-
 SelectorQueryCache* Document::selectorQueryCache()
 {
     if (!m_selectorQueryCache)
