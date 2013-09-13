0b4ef4efcef1ad0dbb7b6bda37acbddd3f4843ea
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-13 23:50:58 +0000

    aura: Fix WebContentsViewAuraTest.
    Chain into ContentBrowserTest::SetUp from WebContentsViewAuraTest::SetUp() to make
    sure the tests are run correctly. Fix up the regressions from the test not running
    correctly during the last couple of weeks.
    
    BUG=none
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/23684009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@223174 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/web_contents/web_contents_screenshot_manager.cc
+++ content/browser/web_contents/web_contents_screenshot_manager.cc
@@ -150,7 +150,8 @@ void WebContentsScreenshotManager::OnScreenshotTaken(int unique_id,
   }
 
   if (!success || bitmap.empty() || bitmap.isNull()) {
-    ClearScreenshot(entry);
+    if (!ClearScreenshot(entry))
+      OnScreenshotSet(entry);
     return;
   }
 
@@ -194,7 +195,8 @@ void WebContentsScreenshotManager::OnScreenshotEncodeComplete(
 }
 
 void WebContentsScreenshotManager::OnScreenshotSet(NavigationEntryImpl* entry) {
-  PurgeScreenshotsIfNecessary();
+  if (entry->screenshot().get())
+    PurgeScreenshotsIfNecessary();
 }
 
 bool WebContentsScreenshotManager::ClearScreenshot(NavigationEntryImpl* entry) {
