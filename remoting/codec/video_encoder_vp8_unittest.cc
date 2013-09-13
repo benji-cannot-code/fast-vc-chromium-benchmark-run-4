0ff18d7e40543af3b16a3d3e641a16891744d4a7
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-13 22:43:41 +0000

    Simplify VideoEncoder interface.
    This should also avoid assert in the linked bug.
    
    BUG=284775
    R=wez@chromium.org
    
    Review URL: https://codereview.chromium.org/23477059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@223152 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/codec/video_encoder_vp8_unittest.cc
+++ remoting/codec/video_encoder_vp8_unittest.cc
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <limits>
 #include <vector>
 
-#include "base/bind.h"
-#include "base/callback.h"
 #include "base/memory/scoped_ptr.h"
 #include "remoting/codec/codec_test.h"
 #include "remoting/proto/video.pb.h"
@@ -29,12 +27,6 @@ TEST(VideoEncoderVp8Test, TestVideoEncoder) {
   TestVideoEncoder(&encoder, false);
 }
 
-class VideoEncoderCallback {
- public:
-  void DataAvailable(scoped_ptr<VideoPacket> packet) {
-  }
-};
-
 // Test that calling Encode with a differently-sized media::ScreenCaptureData
 // does not leak memory.
 TEST(VideoEncoderVp8Test, TestSizeChangeNoLeak) {
@@ -42,28 +34,19 @@ TEST(VideoEncoderVp8Test, TestSizeChangeNoLeak) {
   int width = 1000;
 
   VideoEncoderVp8 encoder;
-  VideoEncoderCallback callback;
 
   scoped_ptr<webrtc::DesktopFrame> frame(new webrtc::BasicDesktopFrame(
       webrtc::DesktopSize(width, height)));
 
-  encoder.Encode(frame.get(), base::Bind(&VideoEncoderCallback::DataAvailable,
-                                         base::Unretained(&callback)));
+  scoped_ptr<VideoPacket> packet = encoder.Encode(*frame);
+  EXPECT_TRUE(packet);
 
   height /= 2;
   frame.reset(new webrtc::BasicDesktopFrame(
       webrtc::DesktopSize(width, height)));
-  encoder.Encode(frame.get(), base::Bind(&VideoEncoderCallback::DataAvailable,
-                                         base::Unretained(&callback)));
+  packet = encoder.Encode(*frame);
+  EXPECT_TRUE(packet);
 }
-
-class VideoEncoderDpiCallback {
- public:
-  void DataAvailable(scoped_ptr<VideoPacket> packet) {
-    EXPECT_EQ(packet->format().x_dpi(), 96);
-    EXPECT_EQ(packet->format().y_dpi(), 97);
-  }
-};
 
 // Test that the DPI information is correctly propagated from the
 // media::ScreenCaptureData to the VideoPacket.
@@ -72,14 +55,13 @@ TEST(VideoEncoderVp8Test, TestDpiPropagation) {
   int width = 32;
 
   VideoEncoderVp8 encoder;
-  VideoEncoderDpiCallback callback;
 
   scoped_ptr<webrtc::DesktopFrame> frame(new webrtc::BasicDesktopFrame(
       webrtc::DesktopSize(width, height)));
   frame->set_dpi(webrtc::DesktopVector(96, 97));
-  encoder.Encode(frame.get(),
-                 base::Bind(&VideoEncoderDpiCallback::DataAvailable,
-                            base::Unretained(&callback)));
+  scoped_ptr<VideoPacket> packet = encoder.Encode(*frame);
+  EXPECT_EQ(packet->format().x_dpi(), 96);
+  EXPECT_EQ(packet->format().y_dpi(), 97);
 }
 
 }  // namespace remoting
