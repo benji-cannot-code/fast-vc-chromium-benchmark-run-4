572bc9456ea15a206603658020928cc4e0fdcca7
glider@chromium.org <glider@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-13 12:30:01 +0000

    Do not run BMPImageDecoderTest.DecodingSlow under TSan v2 (now for real!)
    TBR=jamesr@chromium.org
    
    Review URL: https://codereview.chromium.org/23944010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@223032 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/bmp_image_decoder_unittest.cc
+++ content/renderer/bmp_image_decoder_unittest.cc
@@ -32,6 +32,6 @@ TEST_F(BMPImageDecoderTest, DecodingFast) {
 #else
 #define MAYBE_DecodingSlow DecodingSlow
 #endif
-TEST_F(BMPImageDecoderTest, DecodingSlow) {
+TEST_F(BMPImageDecoderTest, MAYBE_DecodingSlow) {
   TestDecoding(TEST_BIGGER, kThresholdSize);
 }
