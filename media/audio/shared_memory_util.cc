a8b727d2543854851cbca2a9fb7c73e6c25be447
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-13 00:47:14 +0000

    add missing #includes of <algorithm>, needed on VS2013 for std::min
    R=dalecurtis@chromium.org
    BUG=288948
    
    Review URL: https://chromiumcodereview.appspot.com/23983033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222940 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/shared_memory_util.cc
+++ media/audio/shared_memory_util.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/audio/shared_memory_util.h"
 
+#include <algorithm>
+
 #include "base/atomicops.h"
 #include "base/logging.h"
 
--- media/base/bit_reader.cc
+++ media/base/bit_reader.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/base/bit_reader.h"
 
+#include <algorithm>
+
 namespace media {
 
 BitReader::BitReader(const uint8* data, off_t size)
