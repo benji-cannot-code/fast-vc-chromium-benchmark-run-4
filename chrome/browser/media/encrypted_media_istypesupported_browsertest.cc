4932f0e205f8e65540dc7be3e0c697e4482eb92b
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-13 00:16:38 +0000

    Fix KeySystemsTest on Android.
    BUG=288541
    TEST=verified on device.
    R=ddorwin@chromium.org
    
    Review URL: https://codereview.chromium.org/23819054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222929 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media/encrypted_media_istypesupported_browsertest.cc
+++ chrome/browser/media/encrypted_media_istypesupported_browsertest.cc
@@ -43,18 +43,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // defined(ENABLE_PEPPER_CDMS)
 
 // Expectations for Widevine.
-#if defined(WIDEVINE_CDM_AVAILABLE)
+#if defined(WIDEVINE_CDM_AVAILABLE) && \
+    !defined(DISABLE_WIDEVINE_CDM_CANPLAYTYPE)  // See http://crbug.com/237627
 #if defined(OS_LINUX) && !defined(OS_CHROMEOS)
 // TODO(ddorwin): Remove after bots switch to Precise.
 #define EXPECT_WV(a) \
     EXPECT_EQ((std::string(gnu_get_libc_version()) != "2.11.1"), (a))
 #else
-// See http://crbug.com/237627.
-#if defined(DISABLE_WIDEVINE_CDM_CANPLAYTYPE)
-#define EXPECT_WV EXPECT_FALSE
-#else
 #define EXPECT_WV EXPECT_TRUE
-#endif  // defined(DISABLE_WIDEVINE_CDM_CANPLAYTYPE)
 #endif  // defined(OS_LINUX) && !defined(OS_CHROMEOS)
 
 #if defined(WIDEVINE_CDM_CENC_SUPPORT_AVAILABLE)
@@ -85,13 +81,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define EXPECT_WVAAC EXPECT_FALSE
 #endif  // defined(WIDEVINE_CDM_CENC_SUPPORT_AVAILABLE)
 
-#else  // !defined(WIDEVINE_CDM_AVAILABLE)
+#else  // defined(WIDEVINE_CDM_AVAILABLE) &&
+       // !defined(DISABLE_WIDEVINE_CDM_CANPLAYTYPE)
 #define EXPECT_WV EXPECT_FALSE
 #define EXPECT_WVCENC EXPECT_FALSE
 #define EXPECT_WVAVC1 EXPECT_FALSE
 #define EXPECT_WVAVC1AAC EXPECT_FALSE
 #define EXPECT_WVAAC EXPECT_FALSE
-#endif  // defined(WIDEVINE_CDM_AVAILABLE)
+#endif  // defined(WIDEVINE_CDM_AVAILABLE) &&
+        // !defined(DISABLE_WIDEVINE_CDM_CANPLAYTYPE)
 
 namespace chrome {
 
