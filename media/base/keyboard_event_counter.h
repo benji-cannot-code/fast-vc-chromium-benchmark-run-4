e181907644627fd6c91176c364d8a6e726563a4f
jiayl@chromium.org <jiayl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-14 21:28:11 +0000

    Adds the UserInputMonitor implementation for Windows.
    BUG=274623
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=223223
    
    Review URL: https://chromiumcodereview.appspot.com/23702008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@223260 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/keyboard_event_counter.h
+++ media/base/keyboard_event_counter.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 
 #include "base/synchronization/lock.h"
+#include "media/base/media_export.h"
 #include "ui/base/events/event_constants.h"
 #include "ui/base/keycodes/keyboard_codes.h"
 
@@ -18,7 +19,7 @@ namespace media {
 // calls it receives from the client.
 // Multiple key down events for the same key are counted as one keypress until
 // the same key is released.
-class KeyboardEventCounter {
+class MEDIA_EXPORT KeyboardEventCounter {
  public:
   KeyboardEventCounter();
   ~KeyboardEventCounter();
