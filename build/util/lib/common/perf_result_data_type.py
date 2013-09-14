0cb98cd880b46a926a0955e4a50421da1db96cac
rschoen@google.com <rschoen@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-14 00:53:33 +0000

    Copy perf_tests_helper.py into build/util/lib
    Sorry for the delay on this one. Based off advice in https://codereview.chromium.org/22754003. Just a simple copy of the file, and removing the (seemingly unnecessary?) import of android_commands.
    
    Will remove dependencies on old one as part of above mentioned patch.
    
    Review URL: https://chromiumcodereview.appspot.com/23578019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@223190 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ build/util/lib/common/perf_result_data_type.py
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+DEFAULT = 'default'
+UNIMPORTANT = 'unimportant'
+HISTOGRAM = 'histogram'
+UNIMPORTANT_HISTOGRAM = 'unimportant-histogram'
+INFORMATIONAL = 'informational'
+
+ALL_TYPES = [DEFAULT, UNIMPORTANT, HISTOGRAM, UNIMPORTANT_HISTOGRAM,
+             INFORMATIONAL]
+
+
+def IsValidType(datatype):
+  return datatype in ALL_TYPES
+
+
+def IsHistogram(datatype):
+  return (datatype == HISTOGRAM or datatype == UNIMPORTANT_HISTOGRAM)
