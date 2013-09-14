68d58c3613471edea078f49315150b6a51d3ac16
skuhne@chromium.org <skuhne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-14 11:49:52 +0000

    Fixing problem of app list showing up in wrong location on second screen
    BUG=288725
    TEST=visual
    
    Review URL: https://chromiumcodereview.appspot.com/23440036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@223238 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/app_list_controller.cc
+++ ash/wm/app_list_controller.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_switches.h"
 #include "ash/launcher/launcher.h"
 #include "ash/root_window_controller.h"
+#include "ash/screen_ash.h"
 #include "ash/shelf/shelf_layout_manager.h"
 #include "ash/shell.h"
 #include "ash/shell_delegate.h"
@@ -163,11 +164,16 @@ void AppListController::SetVisible(bool visible, aura::Window* window) {
     // will be released with AppListView on close.
     app_list::AppListView* view = new app_list::AppListView(
         Shell::GetInstance()->delegate()->CreateAppListViewDelegate());
-    aura::Window* container = GetRootWindowController(window->GetRootWindow())->
+    aura::RootWindow* root_window = window->GetRootWindow();
+    aura::Window* container = GetRootWindowController(root_window)->
         GetContainer(kShellWindowId_AppListContainer);
     if (ash::switches::UseAlternateShelfLayout()) {
       gfx::Rect applist_button_bounds = Launcher::ForWindow(container)->
           GetAppListButtonView()->GetBoundsInScreen();
+      // We need the location of the button within the local screen.
+      applist_button_bounds = ash::ScreenAsh::ConvertRectFromScreen(
+          root_window,
+          applist_button_bounds);
       view->InitAsBubbleAttachedToAnchor(
           container,
           pagination_model_.get(),
