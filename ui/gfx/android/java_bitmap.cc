4f0301690af6becadbd0c0bfdcb04efa4fbccbd5
bulach@chromium.org <bulach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-16 13:54:24 +0000

    Android: Remove JNIEnv::NewStringUTF call.
    Use the base/ wrapper instead.
    
    BUG=290232
    
    Review URL: https://chromiumcodereview.appspot.com/23597042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@223336 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/android/java_bitmap.cc
+++ ui/gfx/android/java_bitmap.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <android/bitmap.h>
 
+#include "base/android/jni_string.h"
 #include "base/logging.h"
 #include "jni/BitmapHelper_jni.h"
 #include "skia/ext/image_operations.h"
@@ -65,7 +66,8 @@ ScopedJavaLocalRef<jobject> ConvertToJavaBitmap(const SkBitmap* skbitmap) {
 static ScopedJavaLocalRef<jobject> CreateJavaBitmapFromResource(
     const char* name, gfx::Size requested_size) {
   JNIEnv* env = AttachCurrentThread();
-  ScopedJavaLocalRef<jstring> jname(env, env->NewStringUTF(name));
+  ScopedJavaLocalRef<jstring> jname(
+      base::android::ConvertUTF8ToJavaString(env, name));
   return ui::Java_BitmapHelper_decodeDrawableResource(env,
                                                       jname.obj(),
                                                       requested_size.width(),
