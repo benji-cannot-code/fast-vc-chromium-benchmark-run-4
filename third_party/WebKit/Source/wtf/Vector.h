2d97ea27f300d50b72710b7851d0d2e9420552b4
cevans@chromium.org <cevans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-16 18:11:21 +0000

    Merge Vector's clearBufferPointer with restoreInlineBufferIfNeeded.
    This is possible now that deallocateBuffer was simplified. This leads to
    simpler and maybe slightly faster code.
    
    Also rename clearBufferPointer to resetBufferPointer to be clearer.
    
    I believe this might also fix a bug in Deque that I recently introduced,
    relating to leaving the buffer pointer NULL for a Deque with inline
    storage.
    
    Review URL: https://chromiumcodereview.appspot.com/23581014
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157840 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/Vector.h
+++ third_party/WebKit/Source/wtf/Vector.h
@@ -341,7 +341,7 @@ static const size_t kInitialVectorSize = WTF_VECTOR_INITIAL_SIZE;
             fastFree(bufferToDeallocate);
         }
 
-        void clearBufferPointer()
+        void resetBufferPointer()
         {
             m_buffer = 0;
             m_capacity = 0;
@@ -352,8 +352,6 @@ static const size_t kInitialVectorSize = WTF_VECTOR_INITIAL_SIZE;
             std::swap(m_buffer, other.m_buffer);
             std::swap(m_capacity, other.m_capacity);
         }
-
-        void restoreInlineBufferIfNeeded() { }
 
         using Base::allocateBuffer;
         using Base::shouldReallocateBuffer;
@@ -405,10 +403,10 @@ static const size_t kInitialVectorSize = WTF_VECTOR_INITIAL_SIZE;
             fastFree(bufferToDeallocate);
         }
 
-        void clearBufferPointer()
+        void resetBufferPointer()
         {
-            m_buffer = 0;
-            m_capacity = 0;
+            m_buffer = inlineBuffer();
+            m_capacity = inlineCapacity;
         }
 
         void allocateBuffer(size_t newCapacity)
@@ -416,10 +414,8 @@ static const size_t kInitialVectorSize = WTF_VECTOR_INITIAL_SIZE;
             // FIXME: This should ASSERT(!m_buffer) to catch misuse/leaks.
             if (newCapacity > inlineCapacity)
                 Base::allocateBuffer(newCapacity);
-            else {
-                m_buffer = inlineBuffer();
-                m_capacity = inlineCapacity;
-            }
+            else
+                resetBufferPointer();
         }
 
         bool shouldReallocateBuffer(size_t newCapacity) const
@@ -453,14 +449,6 @@ static const size_t kInitialVectorSize = WTF_VECTOR_INITIAL_SIZE;
                 std::swap(m_buffer, other.m_buffer);
                 std::swap(m_capacity, other.m_capacity);
             }
-        }
-
-        void restoreInlineBufferIfNeeded()
-        {
-            if (m_buffer)
-                return;
-            m_buffer = inlineBuffer();
-            m_capacity = inlineCapacity;
         }
 
         using Base::buffer;
@@ -638,7 +626,6 @@ static const size_t kInitialVectorSize = WTF_VECTOR_INITIAL_SIZE;
         using Base::allocateBuffer;
         using Base::shouldReallocateBuffer;
         using Base::reallocateBuffer;
-        using Base::restoreInlineBufferIfNeeded;
     };
 
     template<typename T, size_t inlineCapacity>
@@ -907,11 +894,10 @@ static const size_t kInitialVectorSize = WTF_VECTOR_INITIAL_SIZE;
             if (begin() != oldBuffer)
                 TypeOperations::move(oldBuffer, oldEnd, begin());
         } else {
-            Base::clearBufferPointer();
+            Base::resetBufferPointer();
         }
 
         Base::deallocateBuffer(oldBuffer);
-        Base::restoreInlineBufferIfNeeded();
     }
 
     // Templatizing these is better than just letting the conversion happen implicitly,
