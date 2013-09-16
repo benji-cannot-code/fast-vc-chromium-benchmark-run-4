d3012e2e7f184f504c4aa70462865cc3ad0f3241
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-16 23:18:43 +0000

    Remove default case from autofill_metrics.cc::GetFieldTypeGroupMetric
    BUG=267983
    TEST=compiles
    R=estade@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/23629046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@223463 0039d316-1c4b-4281-b951-d872f2087c98

--- components/autofill/core/browser/autofill_metrics.cc
+++ components/autofill/core/browser/autofill_metrics.cc
@@ -57,6 +57,7 @@ enum FieldTypeGroupForMetrics {
   CREDIT_CARD_NUMBER,
   CREDIT_CARD_DATE,
   CREDIT_CARD_TYPE,
+  PASSWORD,
   NUM_FIELD_TYPE_GROUPS_FOR_METRICS
 };
 
@@ -86,13 +87,14 @@ int GetFieldTypeGroupMetric(const ServerFieldType field_type,
                             const int num_possible_metrics) {
   DCHECK_LT(metric, num_possible_metrics);
 
-  FieldTypeGroupForMetrics group;
+  FieldTypeGroupForMetrics group = AMBIGUOUS;
   switch (AutofillType(field_type).group()) {
     case ::autofill::NO_GROUP:
       group = AMBIGUOUS;
       break;
 
     case ::autofill::NAME:
+    case ::autofill::NAME_BILLING:
       group = NAME;
       break;
 
@@ -101,7 +103,8 @@ int GetFieldTypeGroupMetric(const ServerFieldType field_type,
       break;
 
     case ::autofill::ADDRESS_HOME:
-      switch (field_type) {
+    case ::autofill::ADDRESS_BILLING:
+      switch (AutofillType(field_type).GetStorableType()) {
         case ADDRESS_HOME_LINE1:
           group = ADDRESS_LINE_1;
           break;
@@ -123,6 +126,7 @@ int GetFieldTypeGroupMetric(const ServerFieldType field_type,
         default:
           NOTREACHED();
           group = AMBIGUOUS;
+          break;
       }
       break;
 
@@ -131,6 +135,7 @@ int GetFieldTypeGroupMetric(const ServerFieldType field_type,
       break;
 
     case ::autofill::PHONE_HOME:
+    case ::autofill::PHONE_BILLING:
       group = PHONE;
       break;
 
@@ -145,14 +150,23 @@ int GetFieldTypeGroupMetric(const ServerFieldType field_type,
         case ::autofill::CREDIT_CARD_TYPE:
           group = CREDIT_CARD_TYPE;
           break;
-        default:
+        case ::autofill::CREDIT_CARD_EXP_MONTH:
+        case ::autofill::CREDIT_CARD_EXP_2_DIGIT_YEAR:
+        case ::autofill::CREDIT_CARD_EXP_4_DIGIT_YEAR:
+        case ::autofill::CREDIT_CARD_EXP_DATE_2_DIGIT_YEAR:
+        case ::autofill::CREDIT_CARD_EXP_DATE_4_DIGIT_YEAR:
           group = CREDIT_CARD_DATE;
+          break;
+        default:
+          NOTREACHED();
+          group = AMBIGUOUS;
+          break;
       }
       break;
 
-    default:
-      NOTREACHED();
-      group = AMBIGUOUS;
+    case ::autofill::PASSWORD_FIELD:
+      group = PASSWORD;
+      break;
   }
 
   // Interpolate the |metric| with the |group|, so that all metrics for a given
