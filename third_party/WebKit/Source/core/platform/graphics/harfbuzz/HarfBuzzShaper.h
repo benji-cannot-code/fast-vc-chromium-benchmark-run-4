f8b59d10dffefcb1e19ba2a810cc6aa19f650ac7
simonhatch@chromium.org <simonhatch@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-16 17:56:29 +0000

    This CL adds a small cache (capped at 256 elements, evicts entries using lru) to hold the shaping results of previous HarfBuzzRuns. This avoids calls to hb_shape, which makes up the majority of time spent in profiles of pages with complex script (such as amarchoti from tough_layout_cases).
    On my z620, this is a ~35% gain for amarchoti on linux, and more than 40% on a nexus 7.
    
    
    Without patch:
    *RESULT times: page_load_time= [45972,43913,44008,43318,43044,43399,43074,43503,42807,42366] ms
    Avg times: 43540.400000ms
    Sd  times: 984.437030ms
    
    
    With patch:
    *RESULT times: page_load_time= [27737,25984,24754,24705,24304,24904,24236,24313,24995,24726] ms
    Avg times: 25065.800000ms
    Sd  times: 1063.958416ms
    
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/23717049
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157835 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/graphics/harfbuzz/HarfBuzzShaper.h
+++ third_party/WebKit/Source/core/platform/graphics/harfbuzz/HarfBuzzShaper.h
@@ -68,12 +68,16 @@ public:
 private:
     class HarfBuzzRun {
     public:
+        HarfBuzzRun(const HarfBuzzRun&);
+        ~HarfBuzzRun();
+
         static PassOwnPtr<HarfBuzzRun> create(const SimpleFontData* fontData, unsigned startIndex, unsigned numCharacters, TextDirection direction, hb_script_t script)
         {
             return adoptPtr(new HarfBuzzRun(fontData, startIndex, numCharacters, direction, script));
         }
 
         void applyShapeResult(hb_buffer_t*);
+        void copyShapeResultAndGlyphPositions(const HarfBuzzRun&);
         void setGlyphAndPositions(unsigned index, uint16_t glyphId, float advance, float offsetX, float offsetY);
         void setWidth(float width) { m_width = width; }
 
@@ -149,6 +153,8 @@ private:
     int m_toIndex;
 
     float m_totalWidth;
+
+    friend struct CachedShapingResults;
 };
 
 } // namespace WebCore
