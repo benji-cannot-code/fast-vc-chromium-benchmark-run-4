aef98699f001c9be78486d13dc6f5bad7614407b
nasko@chromium.org <nasko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-16 23:03:28 +0000

    Revert 223397 "Enable GPU blacklist in tests."
    > Enable GPU blacklist in tests.
    > 
    > Partial re-land of https://codereview.chromium.org/22198004/) -- Always enable FCM on Windows -- Part 1/3.
    > 
    > On top of fixing some of the issues there; only lands the part that enables the GPU blacklist in tests as the former CL is too hard to land all at once.
    > 
    > Also keeping --skip-gpu-data-loading around for now to be able to deal with failures caused in layout_tests by this on their own later (this needs to land ASAP and layout_tests don't need this ASAP).
    > 
    > Another CL will follow to always enable FCM on non-blacklisted Windows machines.
    > 
    > This part re-enables loading the blacklist in tests (and adds a content_browsertest to make sure that the configuration we expect to be testing is indeed the one we are testing -- this uncertainty is basically the only reason the blacklist was explicitly disabled before).
    > 
    > This CL also cleans up compositor_util.cc which was enforcing the blacklist twice.
    > 
    > The original plan was to do this only for Windows as Mac/Linux was causing trouble, but it turns out to be harder to do it only on Windows; so taking care of http://crbug.com/277242 in this CL too after all... 
    > 
    > BUG=233830, 267038, 190942, 277242
    > TBR=jcivelli, piman 
    > 
    > Originally Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=219132 
    > Then Reverted: https://src.chromium.org/viewvc/chrome?view=rev&revision=219159
    > 
    > Then re-committed (part 1/3): https://src.chromium.org/viewvc/chrome?view=rev&revision=221114
    > Reverted in: https://src.chromium.org/viewvc/chrome?view=rev&revision=221145
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=222105
    > 
    > Review URL: https://chromiumcodereview.appspot.com/23534006
    
    TBR=gab@chromium.org
    
    Review URL: https://codereview.chromium.org/24074005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@223456 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/gpu/gpu_data_manager_impl_private.h
+++ content/browser/gpu/gpu_data_manager_impl_private.h
@@ -248,9 +248,6 @@ class CONTENT_EXPORT GpuDataManagerImplPrivate {
 
   bool use_software_compositor_;
 
-  // True if all future Initialize calls should be ignored.
-  bool finalized_;
-
   DISALLOW_COPY_AND_ASSIGN(GpuDataManagerImplPrivate);
 };
 
--- gpu/config/gpu_control_list.h
+++ gpu/config/gpu_control_list.h
@@ -97,9 +97,6 @@ class GPU_EXPORT GpuControlList {
   // Register whether "all" is recognized as all features.
   void set_supports_feature_type_all(bool supported);
 
-  // Enables logging of control list decisions.
-  void enable_control_list_logging() { control_list_logging_enabled_ = true; }
-
  private:
   friend class GpuControlListEntryTest;
   friend class MachineModelInfoTest;
@@ -295,9 +292,6 @@ class GPU_EXPORT GpuControlList {
         const base::DictionaryValue* value, bool top_level,
         const FeatureMap& feature_map,
         bool supports_feature_type_all);
-
-    // Logs a control list match for this rule.
-    void LogControlListMatch() const;
 
     // Determines if a given os/gc/machine_model/driver is included in the
     // Entry set.
@@ -502,8 +496,6 @@ class GPU_EXPORT GpuControlList {
   // The features a GpuControlList recognizes and handles.
   FeatureMap feature_map_;
   bool supports_feature_type_all_;
-
-  bool control_list_logging_enabled_;
 };
 
 }  // namespace gpu
