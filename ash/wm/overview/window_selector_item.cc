dfaa5bbfddd03887af79ac1a90c268d0e692f3a0
flackr@chromium.org <flackr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-17 20:09:24 +0000

    Add panels as a single group of windows per display for overview window cycling.
    BUG=264734
    TEST=WindowSelectorTest.*, WindowTest.IsVisibleRespectsSetVisible
    
    Review URL: https://chromiumcodereview.appspot.com/23654037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@223665 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ ash/wm/overview/window_selector_item.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/wm/overview/window_selector_item.h"
+
+namespace ash {
+
+WindowSelectorItem::WindowSelectorItem() {
+}
+
+WindowSelectorItem::~WindowSelectorItem() {
+}
+
+void WindowSelectorItem::SetBounds(aura::RootWindow* root_window,
+                                   const gfx::Rect& target_bounds) {
+  bounds_ = target_bounds;
+  SetItemBounds(root_window, target_bounds);
+}
+
+}  // namespace ash
