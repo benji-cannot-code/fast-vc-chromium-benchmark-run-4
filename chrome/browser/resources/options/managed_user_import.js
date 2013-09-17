949c008db5f42ec24424ceaea7f27a45e02da67d
ibraaaa@chromium.org <ibraaaa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-17 20:21:28 +0000

    Update error handling for supervised user import overlay
    Updates the import overlay to to handle two different sign-in errors:
    - If there is no GAIA account connected to chrome or if it gets disconnected when the import overlay is opened or if a sign-in error occurs, the managed user list is cleared and an error message reports this.
    - If any of the above errors gets solved either if the overlay is opened or is closed, then the managed user list is updated and error messages are hidden.
    
    
    BUG=292419
    
    Review URL: https://chromiumcodereview.appspot.com/23597046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@223672 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/managed_user_import.js
+++ chrome/browser/resources/options/managed_user_import.js
@@ -76,7 +76,7 @@ cr.define('options', function() {
      * @override
      */
     didShowPage: function() {
-      chrome.send('requestExistingManagedUsers');
+      chrome.send('requestManagedUserImportUpdate');
 
       this.updateImportInProgress_(false);
       $('managed-user-import-error-bubble').hidden = true;
@@ -158,7 +158,8 @@ cr.define('options', function() {
     },
 
     /**
-     * Adds all the existing |managedUsers| to the list.
+     * Adds all the existing |managedUsers| to the list. If |managedUsers|
+     * is undefined, then the list is cleared.
      * @param {Array.<Object>} managedUsers An array of managed user objects.
      *     Each object is of the form:
      *       managedUser = {
@@ -171,6 +172,11 @@ cr.define('options', function() {
      * @private
      */
     receiveExistingManagedUsers_: function(managedUsers) {
+      if (!managedUsers) {
+        $('managed-user-list').dataModel = null;
+        return;
+      }
+
       managedUsers.sort(function(a, b) {
         return a.name.localeCompare(b.name);
       });
@@ -178,6 +184,13 @@ cr.define('options', function() {
       $('managed-user-list').dataModel = new ArrayDataModel(managedUsers);
       if (managedUsers.length == 0)
         this.onError_(loadTimeData.getString('noExistingManagedUsers'));
+    },
+
+    /**
+     * @private
+     */
+    hideErrorBubble_: function() {
+      $('managed-user-import-error-bubble').hidden = true;
     },
 
     /**
@@ -206,6 +219,7 @@ cr.define('options', function() {
 
   // Forward public APIs to private implementations.
   [
+    'hideErrorBubble',
     'onError',
     'onSuccess',
     'receiveExistingManagedUsers',
