41ea8fb2b389c2034598a0714ccf53d7fdd46a0d
rkc@chromium.org <rkc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-17 21:46:36 +0000

    Send version info with Chrome feedback reports. When we moved to the new UI, we removed all hacks which added data to feedback reports based on #ifdefs - as a result of which we lost the OS version and Chrome Version for reports from Chrome. This CL adds that data back. Also we now don't duplicate data between product specific data and the system info zip file. This can lead to having an attached zip file with just 2-3 pieces of data (as with Chrome) and there doesn't seem to be any reason to have the data in two places at the same time. Note, this doesn't change anything on the about:system page, so any diagnostic uses of having all the system info together in the same place is unchanged, it is only on the feedback report that we have removed duplicate data.
    R=asargent@chromium.org, xiyuan@chromium.org
    BUG=290395
    
    Review URL: https://codereview.chromium.org/23851040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@223703 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/feedback_private/feedback_service_nonchromeos.cc
+++ chrome/browser/extensions/api/feedback_private/feedback_service_nonchromeos.cc
@@ -7,12 +7,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/memory/weak_ptr.h"
+#include "base/sys_info.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/signin/signin_manager.h"
 #include "chrome/browser/signin/signin_manager_factory.h"
+#include "chrome/common/chrome_version_info.h"
 
 using extensions::api::feedback_private::SystemInformation;
+
+namespace {
+
+const char kChromeVersionTag[] = "CHROME VERSION";
+const char kOsVersionTag[] = "OS VERSION";
+
+}
 
 namespace extensions {
 
@@ -61,8 +70,16 @@ void FeedbackServiceImpl::GetSystemInformation(
   system_information_callback_ = callback;
 
   SystemInformationList sys_info_list;
-  // TODO(rkc): Figure out what other Chrome system information we can add and
-  // add it here.
+
+  chrome::VersionInfo version_info;
+  FeedbackService::PopulateSystemInfo(
+      &sys_info_list, kChromeVersionTag, version_info.CreateVersionString());
+
+  std::string os_version = base::SysInfo::OperatingSystemName() + ": " +
+                           base::SysInfo::OperatingSystemVersion();
+  FeedbackService::PopulateSystemInfo(
+      &sys_info_list, kOsVersionTag, os_version);
+
   system_information_callback_.Run(sys_info_list);
 }
 
