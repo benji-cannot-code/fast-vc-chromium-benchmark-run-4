b8e81ade1ee020c73041889507a60e847bfed9b5
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-17 16:54:24 +0000

    This changes the current directory for script execution to be the build directory rather than the current BUILDfile directory. This makes things more consistent and simplifies some implementation.
    It adds a new function to_build_path() for buildfiles to convert files to the build directory for this kind of call.
    
    I removed all of the built-in relative* variables that were required to support this, and added one for target_out_dir and root_out_dir.
    
    I added some tests and a test helper object since I was doing this kind of Scope setup a lot.
    
    R=scottmg@chromium.org
    
    Review URL: https://codereview.chromium.org/23606031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@223622 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/gn/test_with_scope.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "tools/gn/test_with_scope.h"
+
+TestWithScope::TestWithScope()
+    : build_settings_(),
+      toolchain_(Label(SourceDir("//toolchain/"), "tc")),
+      settings_(&build_settings_, &toolchain_, std::string()),
+      scope_(&settings_) {
+}
+
+TestWithScope::~TestWithScope() {
+}
