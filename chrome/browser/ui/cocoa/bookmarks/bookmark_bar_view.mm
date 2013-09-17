953ccc8737330a20f5e7f031d411e70511e9a935
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-17 18:59:16 +0000

    [Mac] Show context menu on empty BMB item.
    This matches other platforms.
    
    Fixed at altitude 12000 meters.
    
    BUG=110939
    TEST=See bug.
    
    Review URL: https://chromiumcodereview.appspot.com/23522055
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@223650 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/bookmarks/bookmark_bar_view.mm
+++ chrome/browser/ui/cocoa/bookmarks/bookmark_bar_view.mm
@@ -263,7 +263,7 @@ using content::UserMetricsAction;
 }
 
 - (NSMenu*)menu {
-  return [[controller_ menuController] menuForBookmarkNode:NULL];
+  return [[controller_ menuController] menuForBookmarkBar];
 }
 
 - (void)setController:(id)controller {
