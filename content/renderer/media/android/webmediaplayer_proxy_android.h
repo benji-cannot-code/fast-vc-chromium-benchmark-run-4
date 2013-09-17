b093489d1c8a02d4097775f8ac443ce06fba3925
wonsik@chromium.org <wonsik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-17 00:35:03 +0000

    Fix TV build
    BUG=NONE
    R=qinmin@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/24065004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@223487 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/android/webmediaplayer_proxy_android.h
+++ content/renderer/media/android/webmediaplayer_proxy_android.h
@@ -17,6 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/media_keys.h"
 #include "url/gurl.h"
 
+#if defined(GOOGLE_TV)
+#include "ui/gfx/rect_f.h"
+#endif
+
 namespace content {
 
 class RendererDemuxerAndroid;
