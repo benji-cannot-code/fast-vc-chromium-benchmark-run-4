0dd8b42c3c91854053d2a01fb737d1b15f813fb1
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-17 23:05:29 +0000

    Ignores the first profile for ChromeOS.
    As is described in the comment, the login screen creates a dummy profile
    and then the real profile for the user is created when login. So the first
    profile has to be ignored in ChromeOS.
    
    BUG=286517
    R=dewittj@chromium.org, nkostylev@chromium.org
    TEST=manually
    
    Review URL: https://chromiumcodereview.appspot.com/23904007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@223730 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/notifications/message_center_settings_controller.cc
+++ chrome/browser/notifications/message_center_settings_controller.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_CHROMEOS)
 #include "ash/system/system_notifier.h"
+#include "chrome/browser/chromeos/profiles/profile_helper.h"
 #endif
 
 using message_center::Notifier;
@@ -371,6 +372,7 @@ void MessageCenterSettingsController::RebuildNotifierGroups() {
   current_notifier_group_ = 0;
 
   const size_t count = profile_info_cache_->GetNumberOfProfiles();
+
   for (size_t i = 0; i < count; ++i) {
     scoped_ptr<message_center::ProfileNotifierGroup> group(
         new message_center::ProfileNotifierGroup(
@@ -379,7 +381,16 @@ void MessageCenterSettingsController::RebuildNotifierGroups() {
             profile_info_cache_->GetUserNameOfProfileAtIndex(i),
             i,
             profile_info_cache_->GetPathOfProfileAtIndex(i)));
-    if (group->profile() != NULL)
-      notifier_groups_.push_back(group.release());
+    if (group->profile() == NULL)
+      continue;
+
+#if defined(OS_CHROMEOS)
+    // In ChromeOS, the login screen first creates a dummy profile which is not
+    // actually used, and then the real profile for the user is created when
+    // login (or turns into kiosk mode). This profile should be skipped.
+    if (chromeos::ProfileHelper::IsSigninProfile(group->profile()))
+      continue;
+#endif
+    notifier_groups_.push_back(group.release());
   }
 }
