dc1855b702c6bc127cd37578da7c1fcd8a6bc1b5
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-17 16:43:56 +0000

    x11: Prefer using ui::GetXDisplay() over getting the Display from the X11 message-pump.
    BUG=none
    R=oshima@chromium.org
    
    Review URL: https://codereview.chromium.org/23622035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@223621 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/display/mirror_window_controller.cc
+++ ash/display/mirror_window_controller.cc
@@ -35,6 +35,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/image/image_skia_operations.h"
 #include "ui/gfx/native_widget_types.h"
 
+#if defined(USE_X11)
+#include "ui/base/x/x11_util.h"
+#endif
+
 namespace ash {
 namespace internal {
 namespace {
@@ -44,8 +48,7 @@ namespace {
 void DisableInput(XID window) {
   long event_mask = ExposureMask | VisibilityChangeMask |
       StructureNotifyMask | PropertyChangeMask;
-  XSelectInput(base::MessagePumpX11::GetDefaultXDisplay(),
-               window, event_mask);
+  XSelectInput(ui::GetXDisplay(), window, event_mask);
 }
 #endif
 
