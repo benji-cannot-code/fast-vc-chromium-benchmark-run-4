ec57a853dc04441a7657cad5c720f4b576e9f809
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-17 01:58:46 +0000

    Add support for retina/high-dpi mac layout test baselines.
    For now, we define "mac-retina" as a different port name and treat it as
    if it is a different version of the operating system. This will allow us
    to have dedicated baselines for it and then fall back to the ML/10.8
    baselines for everything else.
    
    This will break if anyone is running 10.7 on a Retina-class device or when
    we need to support both 10.8 and 10.9, but is probably good enough for now.
    
    R=ojan@chromium.org
    BUG=292886
    
    Review URL: https://chromiumcodereview.appspot.com/23757056
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157872 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/common/system/platforminfo_mock.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/system/platforminfo_mock.py
@@ -29,9 +29,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 
 class MockPlatformInfo(object):
-    def __init__(self, os_name='mac', os_version='snowleopard'):
+    def __init__(self, os_name='mac', os_version='snowleopard', is_highdpi=False):
         self.os_name = os_name
         self.os_version = os_version
+        self._is_highdpi = is_highdpi
 
     def is_mac(self):
         return self.os_name == 'mac'
@@ -41,6 +42,9 @@ class MockPlatformInfo(object):
 
     def is_win(self):
         return self.os_name == 'win'
+
+    def is_highdpi(self):
+        return self._is_highdpi
 
     def is_cygwin(self):
         return self.os_name == 'cygwin'
