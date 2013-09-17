bc154181a00591cdefbca9b8d8f3ba0e791ba0d3
pneubeck@chromium.org <pneubeck@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-17 17:40:05 +0000

    Add ethernet to ONC validation and Shill translation.
    BUG=126870
    (for networking_private_api_nonchromeos.cc)
    TBR=gspencer@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/23506040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@223630 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/network/onc/onc_signature.cc
+++ chromeos/network/onc/onc_signature.cc
@@ -145,7 +145,6 @@ const OncFieldSignature vpn_fields[] = {
 
 const OncFieldSignature ethernet_fields[] = {
   { kRecommended, &kRecommendedSignature },
-  // Not supported, yet.
   { ethernet::kAuthentication, &kStringSignature },
   { ethernet::kEAP, &kEAPSignature },
   { NULL }
