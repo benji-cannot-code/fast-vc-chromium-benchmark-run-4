cd72a364edf3d0695d9783ff4bf5531d54cce1c0
zturner@chromium.org <zturner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-18 01:16:16 +0000

    Initial submit of ChromeDebug extension.
    This is a re-upload of a previous patch due to some cs files 
    breaking git's copy/rename detection, so a new issue had to be 
    created and uploaded with --no-find-copies.
    
    New to this upload is the addition of a LICENSE file (which 
    is displayed when you try to install the extension), as well 
    as copyright headers in source files.
    
    BUG=0
    
    Review URL: https://chromiumcodereview.appspot.com/23480085
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@223758 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/win/ChromeDebug/ChromeDebug/GlobalSuppressions.cs
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+﻿// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project. Project-level
+// suppressions either have no target or are given a specific target
+// and scoped to a namespace, type, member, etc.
+//
+// To add a suppression to this file, right-click the message in the
+// Error List, point to "Suppress Message(s)", and click "In Project
+// Suppression File". You do not need to add suppressions to this
+// file manually.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design",
+                "CA1017:MarkAssembliesWithComVisible")]
---
+++ tools/win/ChromeDebug/README.txt
@@ -0,0 +1,6 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+Usage Instructions:
+
+1) Compile with VS2012 or VS2013.
+2) Double click the .vsix in the output directory.
+3) Choose the version of VS to install to.
