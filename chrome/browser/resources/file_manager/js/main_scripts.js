0205b02d5090abc3008d3c69ab0f30c92257db9a
hirono@chromium.org <hirono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-18 15:42:19 +0000

    Move the breadcrumbs management from the FileManager class to PreviewPanel class.
    The breadcrumbs is visually located in the preview panel so we should also put
    it in the PreviewPanel class.
    
    BUG=284215
    TEST=manually
    
    Review URL: https://chromiumcodereview.appspot.com/24169004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@223882 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/main_scripts.js
+++ chrome/browser/resources/file_manager/js/main_scripts.js
@@ -79,7 +79,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //<include src="path_util.js"/>
 //<include src="util.js"/>
 //<include src="action_choice_util.js"/>
-//<include src="breadcrumbs_controller.js"/>
 //<include src="butter_bar.js"/>
 //<include src="cws_container_client.js"/>
 //<include src="directory_contents.js"/>
@@ -105,6 +104,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //<include src="suggest_apps_dialog.js"/>
 //<include src="text_measure.js"/>
 //<include src="tree.css.js"/>
+//<include src="ui/breadcrumbs_controller.js"/>
 //<include src="ui/preview_panel.js"/>
 //<include src="ui/progress_center_panel.js"/>
 //<include src="url_constants.js"/>
