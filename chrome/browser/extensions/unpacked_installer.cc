5ebe809172ce4394aa4462d27cc5c9f08dd07cde
DHNishi@gmail.com <DHNishi@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-18 21:51:25 +0000

    Make loading an unpacked extension produce an error if it contains a malformed messages.json.
    Continued from Patrick Riordan's 16279004
    
    BUG=148530
    
    Review URL: https://chromiumcodereview.appspot.com/23460008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@223947 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/unpacked_installer.cc
+++ chrome/browser/extensions/unpacked_installer.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/extensions/api/plugins/plugins_handler.h"
 #include "chrome/common/extensions/extension.h"
 #include "chrome/common/extensions/extension_file_util.h"
+#include "chrome/common/extensions/extension_l10n_util.h"
 #include "content/public/browser/browser_thread.h"
 #include "extensions/common/id_util.h"
 #include "extensions/common/manifest.h"
@@ -136,7 +137,11 @@ bool UnpackedInstaller::LoadFromCommandLine(const base::FilePath& path_in,
   installer_.set_extension(extension_file_util::LoadExtension(
       extension_path_, Manifest::COMMAND_LINE, GetFlags(), &error).get());
 
-  if (!installer_.extension().get()) {
+  if (!installer_.extension().get() ||
+      !extension_l10n_util::ValidateExtensionLocales(
+          extension_path_,
+          installer_.extension()->manifest()->value(),
+          &error)) {
     ReportExtensionLoadError(error);
     return false;
   }
@@ -252,7 +257,11 @@ void UnpackedInstaller::LoadWithFileAccess(int flags) {
   installer_.set_extension(extension_file_util::LoadExtension(
       extension_path_, Manifest::UNPACKED, flags, &error).get());
 
-  if (!installer_.extension().get()) {
+  if (!installer_.extension().get() ||
+      !extension_l10n_util::ValidateExtensionLocales(
+          extension_path_,
+          installer_.extension()->manifest()->value(),
+          &error)) {
     BrowserThread::PostTask(
         BrowserThread::UI,
         FROM_HERE,
