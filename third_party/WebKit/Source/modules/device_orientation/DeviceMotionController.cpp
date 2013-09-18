8872576918c7a331393a8ab8995bcc51e874f671
ch.dumez@sisa.samsung.com <ch.dumez@sisa.samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-18 15:31:48 +0000

    Add page visibility observer support for Device Motion
    Stop firing 'ondevicemotion' events when a tab goes invisible to optimize
    battery life.
    
    R=timvolodine@chromium.org
    BUG=263411
    TEST=fast/dom/DeviceMotion/page-visibility.html
    
    Review URL: https://chromiumcodereview.appspot.com/23705010
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157968 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/device_orientation/DeviceMotionController.cpp
+++ third_party/WebKit/Source/modules/device_orientation/DeviceMotionController.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "RuntimeEnabledFeatures.h"
 #include "core/dom/Document.h"
 #include "core/dom/EventNames.h"
+#include "core/page/Page.h"
 #include "modules/device_orientation/DeviceMotionData.h"
 #include "modules/device_orientation/DeviceMotionDispatcher.h"
 #include "modules/device_orientation/DeviceMotionEvent.h"
@@ -40,6 +41,8 @@ namespace WebCore {
 DeviceMotionController::DeviceMotionController(Document* document)
     : DOMWindowLifecycleObserver(document->domWindow())
     , DeviceSensorEventController(document)
+    , PageLifecycleObserver(document->page())
+    , m_hasEventListener(false)
 {
 }
 
@@ -96,19 +99,36 @@ bool DeviceMotionController::isNullEvent(Event* event)
 
 void DeviceMotionController::didAddEventListener(DOMWindow*, const AtomicString& eventType)
 {
-    if (eventType == eventNames().devicemotionEvent && RuntimeEnabledFeatures::deviceMotionEnabled())
-        startUpdating();
+    if (eventType == eventNames().devicemotionEvent && RuntimeEnabledFeatures::deviceMotionEnabled()) {
+        if (page() && page()->visibilityState() == PageVisibilityStateVisible)
+            startUpdating();
+        m_hasEventListener = true;
+    }
 }
 
 void DeviceMotionController::didRemoveEventListener(DOMWindow*, const AtomicString& eventType)
 {
-    if (eventType == eventNames().devicemotionEvent)
+    if (eventType == eventNames().devicemotionEvent) {
         stopUpdating();
+        m_hasEventListener = false;
+    }
 }
 
 void DeviceMotionController::didRemoveAllEventListeners(DOMWindow*)
 {
     stopUpdating();
+    m_hasEventListener = false;
+}
+
+void DeviceMotionController::pageVisibilityChanged()
+{
+    if (!m_hasEventListener)
+        return;
+
+    if (page()->visibilityState() == PageVisibilityStateVisible)
+        startUpdating();
+    else
+        stopUpdating();
 }
 
 } // namespace WebCore
--- third_party/WebKit/Source/modules/device_orientation/DeviceMotionController.h
+++ third_party/WebKit/Source/modules/device_orientation/DeviceMotionController.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/dom/Event.h"
 #include "core/page/DOMWindowLifecycleObserver.h"
+#include "core/page/PageLifecycleObserver.h"
 #include "core/platform/Supplementable.h"
 #include "modules/device_orientation/DeviceSensorEventController.h"
 
@@ -38,7 +39,7 @@ namespace WebCore {
 class DeviceMotionData;
 class DOMWindow;
 
-class DeviceMotionController : public DeviceSensorEventController, public Supplement<ScriptExecutionContext>, public DOMWindowLifecycleObserver {
+class DeviceMotionController : public DeviceSensorEventController, public Supplement<ScriptExecutionContext>, public DOMWindowLifecycleObserver, public PageLifecycleObserver {
 
 public:
     virtual ~DeviceMotionController();
@@ -48,19 +49,24 @@ public:
 
     void didChangeDeviceMotion(DeviceMotionData*);
 
-    // Inherited from DOMWindowLifecycleObserver
-    virtual void didAddEventListener(DOMWindow*, const AtomicString&) OVERRIDE;
-    virtual void didRemoveEventListener(DOMWindow*, const AtomicString&) OVERRIDE;
-    virtual void didRemoveAllEventListeners(DOMWindow*) OVERRIDE;
-
 private:
     explicit DeviceMotionController(Document*);
     virtual void registerWithDispatcher() OVERRIDE;
     virtual void unregisterWithDispatcher() OVERRIDE;
 
+    // Inherited from DOMWindowLifecycleObserver.
+    virtual void didAddEventListener(DOMWindow*, const AtomicString&) OVERRIDE;
+    virtual void didRemoveEventListener(DOMWindow*, const AtomicString&) OVERRIDE;
+    virtual void didRemoveAllEventListeners(DOMWindow*) OVERRIDE;
+
+    // Inherited from PageLifecycleObserver.
+    virtual void pageVisibilityChanged() OVERRIDE;
+
     virtual bool hasLastData() OVERRIDE;
     virtual PassRefPtr<Event> getLastEvent() OVERRIDE;
     virtual bool isNullEvent(Event*) OVERRIDE;
+
+    bool m_hasEventListener;
 };
 
 } // namespace WebCore
