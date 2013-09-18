8700e9fde63b45980037ab2b5e4d82836fcec160
ddorwin@chromium.org <ddorwin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-18 01:27:29 +0000

    Quickly fail if an encrypted media browser test gets an EME exception or error.
    Also, test the WV parent when WV is registered.
    
    Review URL: https://chromiumcodereview.appspot.com/23548036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@223762 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/media/media_browsertest.h
+++ content/browser/media/media_browsertest.h
@@ -3,20 +3,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifndef CONTENT_BROWSER_MEDIA_MEDIA_BROWSERTEST_H_
+#define CONTENT_BROWSER_MEDIA_MEDIA_BROWSERTEST_H_
+
+#include <utility>
+#include <vector>
+
 #include "content/test/content_browser_test.h"
 
 namespace content {
+
+class TitleWatcher;
 
 // Class used to automate running media related browser tests. The functions
 // assume that media files are located under files/media/ folder known to
 // the test http server.
 class MediaBrowserTest : public ContentBrowserTest {
  public:
+  typedef std::pair<const char*, const char*> StringPair;
+
+  // Common test results.
   static const char kEnded[];
   static const char kError[];
   static const char kFailed[];
-
-  typedef std::pair<const char*, const char*> StringPair;
 
   virtual void SetUp() OVERRIDE;
 
@@ -31,6 +40,10 @@ class MediaBrowserTest : public ContentBrowserTest {
   // Opens a URL and waits for the document title to match either one of the
   // default strings or the expected string.
   void RunTest(const GURL& gurl, const char* expected);
+
+  virtual void AddWaitForTitles(content::TitleWatcher* title_watcher);
 };
 
 } // namespace content
+
+#endif  // CONTENT_BROWSER_MEDIA_MEDIA_BROWSERTEST_H_
