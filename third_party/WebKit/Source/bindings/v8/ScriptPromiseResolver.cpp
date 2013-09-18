d7127c604fa846cd0354e2616ccba8f98faa543b
ch.dumez@sisa.samsung.com <ch.dumez@sisa.samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-18 09:52:10 +0000

    Pass isolate to ScriptPromise and ScriptString constructors
    Pass isolate to ScriptPromise and ScriptString constructors to avoid calls
    to v8::Isolate::GetCurrent(). Also add an m_isolate member to ScriptValue
    to avoid avoid calls to v8::Isolate::GetCurrent() and simplify the API.
    
    R=haraken
    BUG=263412
    
    Review URL: https://chromiumcodereview.appspot.com/23513066
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157950 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/ScriptPromiseResolver.cpp
+++ third_party/WebKit/Source/bindings/v8/ScriptPromiseResolver.cpp
@@ -49,7 +49,7 @@ ScriptPromiseResolver::ScriptPromiseResolver(v8::Handle<v8::Object> creationCont
     ASSERT(RuntimeEnabledFeatures::promiseEnabled());
     v8::Local<v8::Object> promise, resolver;
     V8PromiseCustom::createPromise(creationContext, &promise, &resolver, isolate);
-    m_promise = ScriptPromise(promise);
+    m_promise = ScriptPromise(promise, isolate);
     m_resolver.set(isolate, resolver);
 }
 
--- third_party/WebKit/Source/core/inspector/ScriptArguments.cpp
+++ third_party/WebKit/Source/core/inspector/ScriptArguments.cpp
@@ -78,7 +78,7 @@ bool ScriptArguments::getFirstArgumentAsString(String& result, bool checkForNull
         return false;
     }
 
-    result = value.toString(globalState());
+    result = value.toString();
     return true;
 }
 
