9b92f30746d3233452e90d36adbd4d1934356091
xians@chromium.org <xians@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-18 17:21:32 +0000

    Fix the audio quality test in browser tests. The problem is that WebAudio API CreateMediaStreamDestination() can be called without getUserMedia, then the capturer_ and the source provider might not be available for the track.
    BUG=294027
    TEST=browser_tests --gtest_filter="*WebrtcAudio*" --run-manual
    
    Review URL: https://chromiumcodereview.appspot.com/24047009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@223896 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/webaudio_capturer_source.cc
+++ content/renderer/media/webaudio_capturer_source.cc
@@ -67,7 +67,8 @@ void WebAudioCapturerSource::Start(
     WebRtcLocalAudioSourceProvider* source_provider) {
   DCHECK(thread_checker_.CalledOnValidThread());
   DCHECK(track);
-  DCHECK(source_provider);
+  // |source_provider| may be NULL if no getUserMedia has been called before
+  // calling CreateMediaStreamDestination.
   // The downstream client should be configured the same as what WebKit
   // is feeding it.
   track->SetCaptureFormat(params_);
@@ -113,8 +114,10 @@ void WebAudioCapturerSource::consumeAudio(
   int volume = 0;
   bool key_pressed = false;
   while (fifo_->frames() >= capture_frames) {
-    source_provider_->GetAudioProcessingParams(
-        &delay_ms, &volume, &key_pressed);
+    if (source_provider_) {
+      source_provider_->GetAudioProcessingParams(
+          &delay_ms, &volume, &key_pressed);
+    }
     fifo_->Consume(capture_bus_.get(), 0, capture_frames);
     track_->Capture(capture_bus_.get(), delay_ms, volume, key_pressed);
   }
