292401d83fec349a1c65edf73c39d109f1219dda
dominikg@chromium.org <dominikg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-18 22:57:57 +0000

    Telemetry: Add pinch page action.
    Add a new page action for Telemetry: pinch. It hooks up to the pinch gesture
    implementation exposed through chrome.gpuBenchmarking.pinchBy().
    
    The action is performed by doing a single pinch gesture centered around the
    middle of the screen. The number of pixels covered by the gesture is currently
    hard-coded.
    
    BUG=264810
    
    Review URL: https://chromiumcodereview.appspot.com/23707037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@223971 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/page/actions/scroll.js
+++ tools/telemetry/telemetry/page/actions/scroll.js
@@ -12,28 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 (function() {
   var MAX_SCROLL_LENGTH_PIXELS = 5000;
 
-  var getTimeMs = (function() {
-    if (window.performance)
-      return (performance.now       ||
-              performance.mozNow    ||
-              performance.msNow     ||
-              performance.oNow      ||
-              performance.webkitNow).bind(window.performance);
-    else
-      return function() { return new Date().getTime(); };
-  })();
-
-  var requestAnimationFrame = (function() {
-    return window.requestAnimationFrame       ||
-           window.webkitRequestAnimationFrame ||
-           window.mozRequestAnimationFrame    ||
-           window.oRequestAnimationFrame      ||
-           window.msRequestAnimationFrame     ||
-           function(callback) {
-             window.setTimeout(callback, 1000 / 60);
-           };
-  })().bind(window);
-
   function ScrollGestureOptions(opt_options) {
     if (opt_options) {
       this.element_ = opt_options.element;
