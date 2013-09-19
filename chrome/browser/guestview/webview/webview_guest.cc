fdd293fb4cdf3f0f5a255c432b49e0bf10363b3c
machenbach@chromium.org <machenbach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-19 19:36:46 +0000

    [Sheriff] Revert "[Sheriff] Revert "<webview>: Enable drag-n-drop on chromeos (always) and in dev/canary for all other platforms.""
    This reverts commit r224110 since tests turned out to be flaky.
    
    TBR=lazyboy@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/24278002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224184 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/guestview/webview/webview_guest.cc
+++ chrome/browser/guestview/webview/webview_guest.cc
@@ -5,12 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/guestview/webview/webview_guest.h"
 
+#include "base/command_line.h"
 #include "chrome/browser/extensions/api/web_request/web_request_api.h"
 #include "chrome/browser/extensions/extension_renderer_state.h"
 #include "chrome/browser/extensions/script_executor.h"
 #include "chrome/browser/favicon/favicon_tab_helper.h"
 #include "chrome/browser/guestview/guestview_constants.h"
 #include "chrome/browser/guestview/webview/webview_constants.h"
+#include "chrome/common/chrome_version_info.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/native_web_keyboard_event.h"
 #include "content/public/browser/notification_details.h"
@@ -21,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/storage_partition.h"
 #include "content/public/browser/user_metrics.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/common/content_switches.h"
 #include "content/public/common/result_codes.h"
 #include "net/base/net_errors.h"
 
@@ -210,6 +213,22 @@ bool WebViewGuest::HandleKeyboardEvent(
   }
 #endif
   return false;
+}
+
+bool WebViewGuest::IsDragAndDropEnabled() {
+#if defined(OS_CHROMEOS)
+  return true;
+#else
+  chrome::VersionInfo::Channel channel = chrome::VersionInfo::GetChannel();
+  if (channel != chrome::VersionInfo::CHANNEL_STABLE &&
+      channel != chrome::VersionInfo::CHANNEL_BETA) {
+    // Drag and drop is enabled in canary and dev channel.
+    return true;
+  }
+
+  return CommandLine::ForCurrentProcess()->HasSwitch(
+      switches::kEnableBrowserPluginDragDrop);
+#endif
 }
 
 void WebViewGuest::LoadProgressed(double progress) {
