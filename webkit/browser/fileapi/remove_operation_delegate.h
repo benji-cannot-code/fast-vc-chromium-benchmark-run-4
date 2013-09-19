293135e7e6d0e32221e39460d91b8cd55b79f4b4
hidehiko@chromium.org <hidehiko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-19 18:14:01 +0000

    Simplifies RemoveOperationDelegate implementation.
    RecursiveOperationDelegate supports PostProcessDirectory(), so
    RemoveOperationDelegate implementation can be simplified with the moethd.
    
    BUG=282107
    TEST=Ran content_unittests and unit_tests
    
    Review URL: https://chromiumcodereview.appspot.com/23844007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224168 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/browser/fileapi/remove_operation_delegate.h
+++ webkit/browser/fileapi/remove_operation_delegate.h
@@ -12,8 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace fileapi {
 
-class RemoveOperationDelegate
-    : public RecursiveOperationDelegate {
+class RemoveOperationDelegate : public RecursiveOperationDelegate {
  public:
   RemoveOperationDelegate(FileSystemContext* file_system_context,
                           const FileSystemURL& url,
@@ -34,15 +33,10 @@ class RemoveOperationDelegate
   void DidTryRemoveFile(base::PlatformFileError error);
   void DidRemoveFile(const StatusCallback& callback,
                      base::PlatformFileError error);
-  void RemoveNextDirectory(base::PlatformFileError error);
 
   FileSystemURL url_;
   StatusCallback callback_;
-
-  std::stack<FileSystemURL> to_remove_directories_;
-
   base::WeakPtrFactory<RemoveOperationDelegate> weak_factory_;
-
   DISALLOW_COPY_AND_ASSIGN(RemoveOperationDelegate);
 };
 
