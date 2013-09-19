516a7f0fde0db50a41298ae6079c2286a8bcbc35
noel@chromium.org <noel@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-19 02:49:23 +0000

    Web Animations CSS: remove unnecessary class CSSValue declaration
    The code already #includes "core/css/CSSValue.h" so no need to declare
    the class a second time.
    
    Review URL: https://chromiumcodereview.appspot.com/24072007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157995 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/AnimatableValue.h
+++ third_party/WebKit/Source/core/animation/AnimatableValue.h
@@ -37,8 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-class CSSValue;
-
 class AnimatableValue : public RefCounted<AnimatableValue> {
 public:
     virtual ~AnimatableValue() { }
