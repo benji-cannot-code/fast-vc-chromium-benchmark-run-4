7d1e1dcfdab85f3d8e07a145ac2b037a204ac039
harrym@chromium.org <harrym@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-19 01:21:26 +0000

    Hit bounds of status area increased to fill shelf. Unit tests added for above. Position of status area and launcher items (App list/Overflow) updated to spec. Removed duplicate variable for positioning (App list, Overflow Button and status area now share an inset constant).
    BUG=288294, 288296, 288399
    
    Review URL: https://chromiumcodereview.appspot.com/23928004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224015 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/tray/system_tray_unittest.cc
+++ ash/system/tray/system_tray_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "ash/root_window_controller.h"
+#include "ash/shelf/shelf_layout_manager.h"
 #include "ash/shelf/shelf_widget.h"
 #include "ash/shell.h"
 #include "ash/system/status_area_widget.h"
@@ -27,8 +28,6 @@ namespace ash {
 namespace test {
 
 namespace {
-
-const int kStatusTrayOffsetFromScreenEdgeForTest = 4;
 
 SystemTray* GetSystemTray() {
   return Shell::GetPrimaryRootWindowController()->shelf()->
@@ -296,44 +295,41 @@ TEST_F(SystemTrayTest, BubbleCreationTypesTest) {
   EXPECT_EQ(widget, test_item->default_view()->GetWidget());
 }
 
-// Tests that the tray is laid out properly in the widget to make sure that the
-// tray extends to the correct edge of the screen.
+// Tests that the tray is laid out properly and is fully contained within
+// the shelf.
 TEST_F(SystemTrayTest, TrayBoundsInWidget) {
+  internal::ShelfLayoutManager* manager =
+      Shell::GetPrimaryRootWindowController()->shelf()->shelf_layout_manager();
   internal::StatusAreaWidget* widget =
       Shell::GetPrimaryRootWindowController()->shelf()->status_area_widget();
   SystemTray* tray = widget->system_tray();
 
-  // Test in bottom alignment. Bottom and right edges of the view should be
-  // aligned with the widget.
-  widget->SetShelfAlignment(SHELF_ALIGNMENT_BOTTOM);
+  // Test in bottom alignment.
+  manager->SetAlignment(SHELF_ALIGNMENT_BOTTOM);
   gfx::Rect window_bounds = widget->GetWindowBoundsInScreen();
   gfx::Rect tray_bounds = tray->GetBoundsInScreen();
-  EXPECT_EQ(window_bounds.bottom(),
-            tray_bounds.bottom() + kStatusTrayOffsetFromScreenEdgeForTest);
-  EXPECT_EQ(window_bounds.right(), tray_bounds.right());
+  EXPECT_TRUE(window_bounds.bottom() >= tray_bounds.bottom());
+  EXPECT_TRUE(window_bounds.right() >= tray_bounds.right());
+  EXPECT_TRUE(window_bounds.x() >= tray_bounds.x());
+  EXPECT_TRUE(window_bounds.y() >= tray_bounds.y());
 
-  // Test in the top alignment. Top and right edges should match.
-  widget->SetShelfAlignment(SHELF_ALIGNMENT_TOP);
+  // Test in the left alignment.
+  manager->SetAlignment(SHELF_ALIGNMENT_LEFT);
   window_bounds = widget->GetWindowBoundsInScreen();
   tray_bounds = tray->GetBoundsInScreen();
-  EXPECT_EQ(window_bounds.y(),
-            tray_bounds.y() - kStatusTrayOffsetFromScreenEdgeForTest);
-  EXPECT_EQ(window_bounds.right(), tray_bounds.right());
+  EXPECT_TRUE(window_bounds.bottom() >= tray_bounds.bottom());
+  EXPECT_TRUE(window_bounds.right() >= tray_bounds.right());
+  EXPECT_TRUE(window_bounds.x() >= tray_bounds.x());
+  EXPECT_TRUE(window_bounds.y() >= tray_bounds.y());
 
-  // Test in the left alignment. Left and bottom edges should match.
-  widget->SetShelfAlignment(SHELF_ALIGNMENT_LEFT);
+  // Test in the right alignment.
+  manager->SetAlignment(SHELF_ALIGNMENT_LEFT);
   window_bounds = widget->GetWindowBoundsInScreen();
   tray_bounds = tray->GetBoundsInScreen();
-  EXPECT_EQ(window_bounds.bottom(), tray_bounds.bottom());
-  EXPECT_EQ(window_bounds.x(),
-            tray_bounds.x() - kStatusTrayOffsetFromScreenEdgeForTest);
-
-  // Test in the right alignment. Right and bottom edges should match.
-  widget->SetShelfAlignment(SHELF_ALIGNMENT_LEFT);
-  window_bounds = widget->GetWindowBoundsInScreen();
-  tray_bounds = tray->GetBoundsInScreen();
-  EXPECT_EQ(window_bounds.bottom(), tray_bounds.bottom());
-  EXPECT_EQ(window_bounds.right(), tray_bounds.right());
+  EXPECT_TRUE(window_bounds.bottom() >= tray_bounds.bottom());
+  EXPECT_TRUE(window_bounds.right() >= tray_bounds.right());
+  EXPECT_TRUE(window_bounds.x() >= tray_bounds.x());
+  EXPECT_TRUE(window_bounds.y() >= tray_bounds.y());
 }
 
 }  // namespace test
