10180bb457c4c96aa7d5500e65d530d35312c58d
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-19 10:31:21 +0000

    Remove dependency on Skia from chromoting client.
    Now DesktopRegion, DesktopRect and DesktopSize are used instead of
    corresponding skia types.
    
    TBR=reed@google.com (for _moved_ skia dependency)
    
    Review URL: https://chromiumcodereview.appspot.com/23440046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224101 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/client/frame_consumer_proxy.cc
+++ remoting/client/frame_consumer_proxy.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/single_thread_task_runner.h"
 #include "third_party/webrtc/modules/desktop_capture/desktop_frame.h"
+#include "third_party/webrtc/modules/desktop_capture/desktop_geometry.h"
+#include "third_party/webrtc/modules/desktop_capture/desktop_region.h"
 
 namespace remoting {
 
@@ -17,10 +19,10 @@ FrameConsumerProxy::FrameConsumerProxy(
     : task_runner_(task_runner) {
 }
 
-void FrameConsumerProxy::ApplyBuffer(const SkISize& view_size,
-                                     const SkIRect& clip_area,
+void FrameConsumerProxy::ApplyBuffer(const webrtc::DesktopSize& view_size,
+                                     const webrtc::DesktopRect& clip_area,
                                      webrtc::DesktopFrame* buffer,
-                                     const SkRegion& region) {
+                                     const webrtc::DesktopRegion& region) {
   if (!task_runner_->BelongsToCurrentThread()) {
     task_runner_->PostTask(FROM_HERE, base::Bind(
         &FrameConsumerProxy::ApplyBuffer, this,
@@ -43,8 +45,9 @@ void FrameConsumerProxy::ReturnBuffer(webrtc::DesktopFrame* buffer) {
     frame_consumer_->ReturnBuffer(buffer);
 }
 
-void FrameConsumerProxy::SetSourceSize(const SkISize& source_size,
-                                       const SkIPoint& source_dpi) {
+void FrameConsumerProxy::SetSourceSize(
+    const webrtc::DesktopSize& source_size,
+    const webrtc::DesktopVector& source_dpi) {
   if (!task_runner_->BelongsToCurrentThread()) {
     task_runner_->PostTask(FROM_HERE, base::Bind(
         &FrameConsumerProxy::SetSourceSize, this, source_size, source_dpi));
--- remoting/protocol/input_event_tracker.h
+++ remoting/protocol/input_event_tracker.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
 #include "remoting/protocol/input_stub.h"
-#include "third_party/skia/include/core/SkPoint.h"
+#include "third_party/webrtc/modules/desktop_capture/desktop_geometry.h"
 
 namespace remoting {
 namespace protocol {
@@ -43,7 +43,7 @@ class InputEventTracker : public InputStub {
 
   std::set<uint32> pressed_keys_;
 
-  SkIPoint mouse_pos_;
+  webrtc::DesktopVector mouse_pos_;
   uint32 mouse_button_state_;
 
   DISALLOW_COPY_AND_ASSIGN(InputEventTracker);
