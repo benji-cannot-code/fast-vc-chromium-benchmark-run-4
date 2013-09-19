5d246649377c07493c655b024f8362aa620a59b1
pwestin@google.com <pwestin@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-19 04:53:14 +0000

    Removed unused code and improved naming
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/23431031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224032 0039d316-1c4b-4281-b951-d872f2087c98

--- media/cast/rtp_common/mock_rtp_payload_feedback.h
+++ media/cast/rtp_common/mock_rtp_payload_feedback.h
@@ -16,8 +16,6 @@ class MockRtpPayloadFeedback : public RtpPayloadFeedback {
  public:
   MOCK_METHOD1(CastFeedback,
                void(const RtcpCastMessage& cast_feedback));
-
-  MOCK_METHOD0(RequestKeyFrame, void());
 };
 
 }  // namespace cast
