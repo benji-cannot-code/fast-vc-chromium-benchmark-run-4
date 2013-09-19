54c34510e5f2f053b4fc34f72ed877fdfa615a05
rtenneti@chromium.org <rtenneti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-19 17:47:38 +0000

    Check for delegate before calling it's HasSendHeadersComplete method.
    BUG=294870
    R=rch@chromium.org
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/24210007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224157 0039d316-1c4b-4281-b951-d872f2087c98

--- net/quic/quic_reliable_client_stream.cc
+++ net/quic/quic_reliable_client_stream.cc
@@ -57,7 +57,7 @@ void QuicReliableClientStream::OnCanWrite() {
 }
 
 QuicPriority QuicReliableClientStream::EffectivePriority() const {
-  if (delegate_->HasSendHeadersComplete()) {
+  if (delegate_ && delegate_->HasSendHeadersComplete()) {
     return ReliableQuicStream::EffectivePriority();
   }
   return kHighestPriority;
