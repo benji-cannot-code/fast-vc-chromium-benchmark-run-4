78e2622c603f4256116cbda437a5e5d0b84626ad
dongseong.hwang@intel.com <dongseong.hwang@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-19 20:00:47 +0000

    Clarify the process title of SandboxIPC process.
    Currently, SandboxIPC process has the same process title to browser process.
    To distinguish SandboxIPC process, this CL introduces sandbox-ipc switch and
    updates the process title of SandboxIPC process.
    
    BUG=N/A
    
    Review URL: https://chromiumcodereview.appspot.com/23707032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224194 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_sandbox_host_linux.cc
+++ content/browser/renderer_host/render_sandbox_host_linux.cc
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/child/webkitplatformsupport_impl.h"
 #include "content/common/font_config_ipc_linux.h"
 #include "content/common/sandbox_linux.h"
+#include "content/common/set_process_title.h"
+#include "content/public/common/content_switches.h"
 #include "skia/ext/skia_utils_base.h"
 #include "third_party/WebKit/public/web/WebKit.h"
 #include "third_party/WebKit/public/web/linux/WebFontInfo.h"
@@ -72,6 +74,15 @@ class SandboxIPCProcess  {
     // positioning, so we pass the current setting through to WebKit.
     WebFontInfo::setSubpixelPositioning(
         gfx::GetDefaultWebkitSubpixelPositioning());
+
+    CommandLine& command_line = *CommandLine::ForCurrentProcess();
+    command_line.AppendSwitchASCII(switches::kProcessType,
+                                   switches::kSandboxIPCProcess);
+
+    // Update the process title. The argv was already cached by the call to
+    // SetProcessTitleFromCommandLine in content_main_runner.cc, so we can pass
+    // NULL here (we don't have the original argv at this point).
+    SetProcessTitleFromCommandLine(NULL);
   }
 
   ~SandboxIPCProcess();
