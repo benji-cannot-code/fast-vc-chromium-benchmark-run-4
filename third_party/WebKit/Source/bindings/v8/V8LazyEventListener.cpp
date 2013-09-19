7cfd1403d9519cd3d995bd93e16ab22ed0e43630
ch.dumez@sisa.samsung.com <ch.dumez@sisa.samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-19 07:15:48 +0000

    Pass isolate to V8LazyEventListener constructor
    Pass isolate to V8LazyEventListener constructor to avoid call to
    v8::Isolate::GetCurrent().
    
    R=haraken
    BUG=263412
    
    Review URL: https://chromiumcodereview.appspot.com/23885006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158012 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/V8LazyEventListener.cpp
+++ third_party/WebKit/Source/bindings/v8/V8LazyEventListener.cpp
@@ -53,8 +53,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-V8LazyEventListener::V8LazyEventListener(const AtomicString& functionName, const AtomicString& eventParameterName, const String& code, const String sourceURL, const TextPosition& position, Node* node)
-    : V8AbstractEventListener(true, mainThreadNormalWorld(), v8::Isolate::GetCurrent()) // FIXME Remove GetCurrent()
+V8LazyEventListener::V8LazyEventListener(const AtomicString& functionName, const AtomicString& eventParameterName, const String& code, const String sourceURL, const TextPosition& position, Node* node, v8::Isolate* isolate)
+    : V8AbstractEventListener(true, mainThreadNormalWorld(), isolate)
     , m_functionName(functionName)
     , m_eventParameterName(eventParameterName)
     , m_code(code)
