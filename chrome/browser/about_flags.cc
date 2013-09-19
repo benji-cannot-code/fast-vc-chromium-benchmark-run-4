c3954553ab3c4b225419466f536a52e4aa7fd80f
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-19 18:00:55 +0000

    Revert 223894 "Revert "Enable device discovery notifications.""
    Re-enable device discovery. It should not regress perf after r224052.
    
    > Revert "Enable device discovery notifications."
    > 
    > This reverts commit 15e19ede41054676329caafa49f0b29065e5f1e3. This commit is
    > being reverted since it causes a significant increase in memory usage.
    > 
    > TBR=vitalybuka
    > BUG=292754
    > 
    > Review URL: https://codereview.chromium.org/23536066
    
    TBR=teravest@chromium.org
    
    Review URL: https://codereview.chromium.org/24191003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224162 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1517,6 +1517,7 @@ const Experiment kExperiments[] = {
     kOsAll,
     SINGLE_VALUE_TYPE(switches::kSyncfsEnableDirectoryOperation),
   },
+#if defined(ENABLE_MDNS)
   {
     "disable-device-discovery",
     IDS_FLAGS_DISABLE_DEVICE_DISCOVERY_NAME,
@@ -1524,6 +1525,14 @@ const Experiment kExperiments[] = {
     kOsWin | kOsLinux | kOsCrOS,
     SINGLE_VALUE_TYPE(switches::kDisableDeviceDiscovery)
   },
+  {
+    "disable-device-discovery-notifications",
+    IDS_FLAGS_DISABLE_DEVICE_DISCOVERY_NOTIFICATIONS_NAME,
+    IDS_FLAGS_DISABLE_DEVICE_DISCOVERY_NOTIFICATIONS_DESCRIPTION,
+    kOsWin | kOsLinux | kOsCrOS,
+    SINGLE_VALUE_TYPE(switches::kDisableDeviceDiscoveryNotifications)
+  },
+#endif  // ENABLE_MDNS
 #if defined(OS_MACOSX)
   {
     "enable-app-shims",
