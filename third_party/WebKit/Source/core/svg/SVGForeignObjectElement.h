4b07ade86c81ccc25902c5dd573c88af74939aa8
gyuyoung.kim@samsung.com <gyuyoung.kim@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-19 11:52:59 +0000

    Add toSVGForeignObjectElement(), and use it.
    This function has type checking assertions for helping to find bad-cast bugs.
    
    BUG=N/A
    
    Review URL: https://chromiumcodereview.appspot.com/24237003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158029 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGForeignObjectElement.h
+++ third_party/WebKit/Source/core/svg/SVGForeignObjectElement.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SVGForeignObjectElement_h
 #define SVGForeignObjectElement_h
 
+#include "SVGNames.h"
 #include "core/svg/SVGAnimatedBoolean.h"
 #include "core/svg/SVGAnimatedLength.h"
 #include "core/svg/SVGExternalResourcesRequired.h"
@@ -57,6 +58,12 @@ private:
         DECLARE_ANIMATED_BOOLEAN(ExternalResourcesRequired, externalResourcesRequired)
     END_DECLARE_ANIMATED_PROPERTIES
 };
+
+inline SVGForeignObjectElement* toSVGForeignObjectElement(Node* node)
+{
+    ASSERT_WITH_SECURITY_IMPLICATION(!node || node->hasTagName(SVGNames::foreignObjectTag));
+    return static_cast<SVGForeignObjectElement*>(node);
+}
 
 } // namespace WebCore
 
