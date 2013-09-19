5a0d32ccfba5e90cc95a0174559607df5c32a37f
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-19 11:21:12 +0000

    Add a fake GAIA implementation for use with the test server.
    This is for the benefit of integration tests that need the browser to
    interface with GAIA and friends, such as for OAuth2 token minting.
    
    BUG=chromium:243889
    R=joi@chromium.org, xiyuan@chromium.org, zelidrag@chromium.org
    
    Review URL: https://codereview.chromium.org/24172005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224108 0039d316-1c4b-4281-b951-d872f2087c98

--- google_apis/gaia/gaia_urls.cc
+++ google_apis/gaia/gaia_urls.cc
@@ -19,6 +19,7 @@ const char kDefaultGoogleApisBaseUrl[] = "https://www.googleapis.com";
 // API calls from accounts.google.com
 const char kClientLoginUrlSuffix[] = "ClientLogin";
 const char kServiceLoginUrlSuffix[] = "ServiceLogin";
+const char kServiceLoginAuthUrlSuffix[] = "ServiceLoginAuth";
 const char kServiceLogoutUrlSuffix[] = "Logout";
 const char kIssueAuthTokenUrlSuffix[] = "IssueAuthToken";
 const char kGetUserInfoUrlSuffix[] = "GetUserInfo";
@@ -93,6 +94,7 @@ GaiaUrls::GaiaUrls() {
   // URLs from accounts.google.com.
   client_login_url_ = gaia_url_.Resolve(kClientLoginUrlSuffix);
   service_login_url_ = gaia_url_.Resolve(kServiceLoginUrlSuffix);
+  service_login_auth_url_ = gaia_url_.Resolve(kServiceLoginAuthUrlSuffix);
   service_logout_url_ = gaia_url_.Resolve(kServiceLogoutUrlSuffix);
   issue_auth_token_url_ = gaia_url_.Resolve(kIssueAuthTokenUrlSuffix);
   get_user_info_url_ = gaia_url_.Resolve(kGetUserInfoUrlSuffix);
@@ -148,6 +150,10 @@ const GURL& GaiaUrls::client_login_url() const {
 
 const GURL& GaiaUrls::service_login_url() const {
   return service_login_url_;
+}
+
+const GURL& GaiaUrls::service_login_auth_url() const {
+  return service_login_auth_url_;
 }
 
 const GURL& GaiaUrls::service_logout_url() const {
