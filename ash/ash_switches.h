2b6b754d38219532be4088689f8087ba8d9df1d2
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-19 20:03:02 +0000

    Try to enable software mirror mode in UI message loop after Init() This wasn't necessary because chrome used to get extra event. We know ignore redundant events and may not process any display change event, so we need to explicitly run extra task to see if we need to create mirror window.
    BUG=293015
    TEST=see bug for manual test. Also covered by unit test.
    
    Review URL: https://chromiumcodereview.appspot.com/23620060
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224197 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/ash_switches.h
+++ ash/ash_switches.h
@@ -53,6 +53,7 @@ ASH_EXPORT extern const char kAshEnableMemoryMonitor[];
 #endif
 ASH_EXPORT extern const char kAshEnableImmersiveFullscreen[];
 ASH_EXPORT extern const char kAshEnableOak[];
+ASH_EXPORT extern const char kAshEnableSoftwareMirroring[];
 ASH_EXPORT extern const char kAshEnableStickyEdges[];
 ASH_EXPORT extern const char kAshEnableTrayDragging[];
 ASH_EXPORT extern const char kAshForceMirrorMode[];
