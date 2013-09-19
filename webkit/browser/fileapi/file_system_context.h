aca08a226d1002ca3227ead5a155e9d06d6b47e3
nhiroki@chromium.org <nhiroki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-19 05:04:11 +0000

    Revert 223635 "SyncFS: Support resolveLocalFileSystemURL on Sync..."
    > SyncFS: Support resolveLocalFileSystemURL on SyncFileSystem
    > 
    > window.resolveLocalFileSystemURL is originally defined only for FileSystem API,
    > but it'd be nice if it works on SyncFileSystem in a similar way. For that this
    > change adds new IPC messages and interfaces.
    > 
    > Blink side change depends on this:
    > https://codereview.chromium.org/23537011/
    > 
    > 
    > BUG=177137
    > TEST=manual (Get FileSystemURL for a file on SyncFS, and then resolve it)
    > TEST=unit_tests
    > TEST=run_webkit_tests.sh http/tests/inspector/filesystem/\* 
    > TEST=run_webkit_tests.sh http/tests/inspector/filesystem/\*
    > 
    > Review URL: https://chromiumcodereview.appspot.com/23856002
    
    BUG=294502
    TBR=nhiroki@chromium.org
    
    Review URL: https://codereview.chromium.org/23558016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224038 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/browser/fileapi/file_system_context.h
+++ webkit/browser/fileapi/file_system_context.h
@@ -50,8 +50,8 @@ class CopyOrMoveFileValidatorFactory;
 class ExternalFileSystemBackend;
 class ExternalMountPoints;
 class FileStreamWriter;
-class FileSystemBackend;
 class FileSystemFileUtil;
+class FileSystemBackend;
 class FileSystemOperation;
 class FileSystemOperationRunner;
 class FileSystemOptions;
@@ -62,7 +62,6 @@ class MountPoints;
 class SandboxFileSystemBackend;
 
 struct DefaultContextDeleter;
-struct FileSystemInfo;
 
 // This class keeps and provides a file system context for FileSystem API.
 // An instance of this class is created and owned by profile.
@@ -156,12 +155,6 @@ class WEBKIT_STORAGE_BROWSER_EXPORT FileSystemContext
                               const std::string& name,
                               const GURL& root)> OpenFileSystemCallback;
 
-  // Used for ResolveURL.
-  typedef base::Callback<void(base::PlatformFileError result,
-                              const FileSystemInfo& info,
-                              const base::FilePath& file_path,
-                              bool is_directory)> ResolveURLCallback;
-
   // Used for DeleteFileSystem.
   typedef base::Callback<void(base::PlatformFileError result)>
       DeleteFileSystemCallback;
@@ -177,15 +170,8 @@ class WEBKIT_STORAGE_BROWSER_EXPORT FileSystemContext
       OpenFileSystemMode mode,
       const OpenFileSystemCallback& callback);
 
-  // Opens the filesystem for the given |url| as read-only, and then checks the
-  // existence of the file entry referred by the URL. This should be called on
-  // the IO thread.
-  void ResolveURL(
-      const FileSystemURL& url,
-      const ResolveURLCallback& callback);
-
   // Deletes the filesystem for the given |origin_url| and |type|. This should
-  // be called on the IO thread.
+  // be called on the IO Thread.
   void DeleteFileSystem(
       const GURL& origin_url,
       FileSystemType type,
@@ -288,13 +274,6 @@ class WEBKIT_STORAGE_BROWSER_EXPORT FileSystemContext
   // For initial backend_map construction. This must be called only from
   // the constructor.
   void RegisterBackend(FileSystemBackend* backend);
-
-  void DidOpenFileSystemForResolveURL(
-      const FileSystemURL& url,
-      const ResolveURLCallback& callback,
-      const GURL& filesystem_root,
-      const std::string& filesystem_name,
-      base::PlatformFileError error);
 
   // Returns a FileSystemBackend, used only by test code.
   SandboxFileSystemBackend* sandbox_backend() const {
--- webkit/common/fileapi/file_system_util.cc
+++ webkit/common/fileapi/file_system_util.cc
@@ -297,8 +297,6 @@ WebKit::WebFileError PlatformFileErrorToWebFileError(
       return WebKit::WebFileErrorSecurity;
     case base::PLATFORM_FILE_ERROR_NO_SPACE:
       return WebKit::WebFileErrorQuotaExceeded;
-    case base::PLATFORM_FILE_ERROR_INVALID_URL:
-      return WebKit::WebFileErrorEncoding;
     default:
       return WebKit::WebFileErrorInvalidModification;
   }
