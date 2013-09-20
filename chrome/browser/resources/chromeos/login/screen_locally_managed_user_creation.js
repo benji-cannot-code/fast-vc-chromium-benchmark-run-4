727cb8bbf65b6e61534a8bc95aeccf58c82ccfb8
antrim@chromium.org <antrim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-20 15:30:16 +0000

    Make sure supervised user name does not contain any html
    BUG=290298
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/24246012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224385 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/login/screen_locally_managed_user_creation.js
+++ chrome/browser/resources/chromeos/login/screen_locally_managed_user_creation.js
@@ -581,6 +581,7 @@ login.createScreen('LocallyManagedUserCreationScreen',
             12, 4);
         this.setButtonDisabledStatus('next', true);
       }
+      this.disabled = false;
     },
 
     /**
--- chrome/browser/ui/webui/chromeos/login/locally_managed_user_creation_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/locally_managed_user_creation_screen_handler.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "google_apis/gaia/gaia_auth_util.h"
 #include "grit/generated_resources.h"
 #include "net/base/data_url.h"
+#include "net/base/escape.h"
 #include "ui/base/l10n/l10n_util.h"
 
 namespace {
@@ -245,6 +246,10 @@ void LocallyManagedUserCreationScreenHandler::HandleCheckLocallyManagedUserName(
     CallJS("managedUserNameError", name,
            l10n_util::GetStringUTF16(
                IDS_CREATE_LOCALLY_MANAGED_USER_CREATE_USERNAME_ALREADY_EXISTS));
+  } else if (net::EscapeForHTML(name) != name) {
+    CallJS("managedUserNameError", name,
+           l10n_util::GetStringUTF16(
+               IDS_CREATE_LOCALLY_MANAGED_USER_CREATE_ILLEGAL_USERNAME));
   } else {
     CallJS("managedUserNameOk", name);
   }
@@ -263,8 +268,13 @@ void LocallyManagedUserCreationScreenHandler::HandleCreateManagedUser(
                new_user_name));
     return;
   }
+  if (net::EscapeForHTML(new_user_name) != new_user_name) {
+    CallJS("managedUserNameError", new_user_name,
+           l10n_util::GetStringUTF16(
+               IDS_CREATE_LOCALLY_MANAGED_USER_CREATE_ILLEGAL_USERNAME));
+    return;
+  }
 
-  // TODO(antrim): Any other password checks here?
   if (new_user_password.length() == 0) {
     CallJS("showPasswordError",
            l10n_util::GetStringUTF16(
