d2e2abf5926790432323a9d4f05615e021f3cd72
earthdok@chromium.org <earthdok@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-20 04:34:20 +0000

    Fix test-only leak in HttpStreamParser.ShouldMergeRequestHeadersAndBody_FileBody.
    BUG=294807,290897,294797
    R=rsleevi@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/23707054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224286 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_stream_parser_unittest.cc
+++ net/http/http_stream_parser_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/memory/ref_counted.h"
+#include "base/run_loop.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/stringprintf.h"
 #include "net/base/io_buffer.h"
@@ -117,29 +118,34 @@ TEST(HttpStreamParser, ShouldMergeRequestHeadersAndBody_ChunkedBody) {
 }
 
 TEST(HttpStreamParser, ShouldMergeRequestHeadersAndBody_FileBody) {
-  ScopedVector<UploadElementReader> element_readers;
+  {
+    ScopedVector<UploadElementReader> element_readers;
 
-  // Create an empty temporary file.
-  base::ScopedTempDir temp_dir;
-  ASSERT_TRUE(temp_dir.CreateUniqueTempDir());
-  base::FilePath temp_file_path;
-  ASSERT_TRUE(file_util::CreateTemporaryFileInDir(temp_dir.path(),
-                                                  &temp_file_path));
+    // Create an empty temporary file.
+    base::ScopedTempDir temp_dir;
+    ASSERT_TRUE(temp_dir.CreateUniqueTempDir());
+    base::FilePath temp_file_path;
+    ASSERT_TRUE(file_util::CreateTemporaryFileInDir(temp_dir.path(),
+                                                    &temp_file_path));
 
-  element_readers.push_back(
-      new UploadFileElementReader(base::MessageLoopProxy::current().get(),
-                                  temp_file_path,
-                                  0,
-                                  0,
-                                  base::Time()));
+    element_readers.push_back(
+        new UploadFileElementReader(base::MessageLoopProxy::current().get(),
+                                    temp_file_path,
+                                    0,
+                                    0,
+                                    base::Time()));
 
-  scoped_ptr<UploadDataStream> body(new UploadDataStream(&element_readers, 0));
-  TestCompletionCallback callback;
-  ASSERT_EQ(ERR_IO_PENDING, body->Init(callback.callback()));
-  ASSERT_EQ(OK, callback.WaitForResult());
-  // Shouldn't be merged if upload data carries a file, as it's not in-memory.
-  ASSERT_FALSE(HttpStreamParser::ShouldMergeRequestHeadersAndBody(
-      "some header", body.get()));
+    scoped_ptr<UploadDataStream> body(
+        new UploadDataStream(&element_readers, 0));
+    TestCompletionCallback callback;
+    ASSERT_EQ(ERR_IO_PENDING, body->Init(callback.callback()));
+    ASSERT_EQ(OK, callback.WaitForResult());
+    // Shouldn't be merged if upload data carries a file, as it's not in-memory.
+    ASSERT_FALSE(HttpStreamParser::ShouldMergeRequestHeadersAndBody(
+        "some header", body.get()));
+  }
+  // UploadFileElementReaders may post clean-up tasks on destruction.
+  base::RunLoop().RunUntilIdle();
 }
 
 TEST(HttpStreamParser, ShouldMergeRequestHeadersAndBody_SmallBodyInMemory) {
