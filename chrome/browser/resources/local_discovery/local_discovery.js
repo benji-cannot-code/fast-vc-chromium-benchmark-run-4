8972009d16eef66f9f4766ebd1ff754b81428c2e
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-20 23:27:54 +0000

    Replaced UMA counters with UMA histogram.
    BUG=295476
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/24246015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224520 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/local_discovery/local_discovery.js
+++ chrome/browser/resources/local_discovery/local_discovery.js
@@ -22,6 +22,21 @@ cr.define('local_discovery', function() {
    */
   var PRINTER_MANAGEMENT_PAGE_PREFIX = '#printers/';
 
+  // Histogram buckets for UMA tracking.
+  /** @const */ var DEVICES_PAGE_EVENTS = {
+    OPENED: 0,
+    LOG_IN_STARTED_FROM_REGISTER_PROMO: 1,
+    LOG_IN_STARTED_FROM_DEVICE_LIST_PROMO: 2,
+    ADD_PRINTER_CLICKED: 3,
+    REGISTER_CLICKED: 4,
+    REGISTER_CONFIRMED: 5,
+    REGISTER_SUCCESS: 6,
+    REGISTER_CANCEL: 7,
+    REGISTER_FAILURE: 8,
+    MANAGE_CLICKED: 9,
+    MAX_EVENT: 10,
+  };
+
   /**
    * Map of service names to corresponding service objects.
    * @type {Object.<string,Service>}
@@ -101,7 +116,7 @@ cr.define('local_discovery', function() {
      * Register the device.
      */
     register: function() {
-      recordUmaAction('DevicesPage_RegisterConfirmed');
+      recordUmaEvent(DEVICES_PAGE_EVENTS.REGISTER_CONFIRMED);
       chrome.send('registerDevice', [this.info.service_name]);
       setRegisterPage('register-page-adding1');
     },
@@ -109,7 +124,7 @@ cr.define('local_discovery', function() {
      * Show registrtation UI for device.
      */
     showRegister: function() {
-      recordUmaAction('DevicesPage_RegisterClicked');
+      recordUmaEvent(DEVICES_PAGE_EVENTS.REGISTER_CLICKED);
       $('register-message').textContent = loadTimeData.getStringF(
         'registerConfirmMessage',
         this.info.human_readable_name);
@@ -204,7 +219,7 @@ cr.define('local_discovery', function() {
    * Show the register overlay.
    */
   function showRegisterOverlay() {
-    recordUmaAction('DevicesPage_AddPrintersClicked');
+    recordUmaEvent(DEVICES_PAGE_EVENTS.ADD_PRINTER_CLICKED);
 
     var registerOverlay = $('register-overlay');
     registerOverlay.classList.add('showing');
@@ -237,7 +252,7 @@ cr.define('local_discovery', function() {
    */
   function onRegistrationFailed() {
     setRegisterPage('register-page-error');
-    recordUmaAction('DevicesPage_RegisterFailure');
+    recordUmaEvent(DEVICES_PAGE_EVENTS.REGISTER_FAILURE);
   }
 
   /**
@@ -344,7 +359,7 @@ cr.define('local_discovery', function() {
   function onRegistrationSuccess() {
     hideRegisterOverlay();
     requestPrinterList();
-    recordUmaAction('DevicesPage_RegisterSuccess');
+    recordUmaEvent(DEVICES_PAGE_EVENTS.REGISTER_SUCCESS);
   }
 
   /**
@@ -386,17 +401,19 @@ cr.define('local_discovery', function() {
    * @param {string} device_id ID of device.
    */
   function manageCloudDevice(device_id) {
-    recordUmaAction('DevicesPage_ManageClicked');
+    recordUmaEvent(DEVICES_PAGE_EVENTS.MANAGE_CLICKED);
     chrome.send('openCloudPrintURL',
                 [PRINTER_MANAGEMENT_PAGE_PREFIX + device_id]);
   }
 
   /**
-   * Record an action in UMA.
-   * @param {string} actionDesc The name of the action to be logged.
-   */
-  function recordUmaAction(actionDesc) {
-    chrome.send('metricsHandler:recordAction', [actionDesc]);
+  * Record an event in the UMA histogram.
+  * @param {number} eventId The id of the event to be recorded.
+  * @private
+  */
+  function recordUmaEvent(eventId) {
+    chrome.send('metricsHandler:recordInHistogram',
+      ['LocalDiscovery.DevicesPage', eventId, DEVICES_PAGE_EVENTS.MAX_EVENT]);
   }
 
   /**
@@ -405,7 +422,7 @@ cr.define('local_discovery', function() {
   function cancelRegistration() {
     hideRegisterOverlay();
     chrome.send('cancelRegistration');
-    recordUmaAction('DevicesPage_RegisterCancel');
+    recordUmaEvent(DEVICES_PAGE_EVENTS.REGISTER_CANCEL);
   }
 
   /**
@@ -444,12 +461,12 @@ cr.define('local_discovery', function() {
   }
 
   function registerLoginButtonClicked() {
-    recordUmaAction('DevicesPage_LogInStartedFromRegisterPromo');
+    recordUmaEvent(DEVICES_PAGE_EVENTS.LOG_IN_STARTED_FROM_REGISTER_PROMO);
     openSignInPage();
   }
 
   function cloudDevicesLoginButtonClicked() {
-    recordUmaAction('DevicesPage_LogInStartedFromDeviceListPromo');
+    recordUmaEvent(DEVICES_PAGE_EVENTS.LOG_IN_STARTED_FROM_DEVICE_LIST_PROMO);
     openSignInPage();
   }
 
@@ -487,7 +504,7 @@ cr.define('local_discovery', function() {
     focusManager.initialize();
 
     chrome.send('start');
-    recordUmaAction('DevicesPage_Opened');
+    recordUmaEvent(DEVICES_PAGE_EVENTS.OPENED);
   });
 
   return {
