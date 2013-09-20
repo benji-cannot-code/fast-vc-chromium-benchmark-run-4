d8b7d9876093a494fe1e77f4e8f48a37ec02c7ab
vivek.vg@samsung.com <vivek.vg@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-20 07:00:38 +0000

    Clear the usage of useBrowserOverlays from WebFrame/Impl and ChromePrintContext
    As per https://codereview.chromium.org/11953010/, chromium is no longer using the useBrowserOverlays.
    Also the bug https://bugs.webkit.org/show_bug.cgi?id=107718 marks the removal of the same.
    
    Review URL: https://chromiumcodereview.appspot.com/24304003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158087 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebFrameImpl.cpp
+++ third_party/WebKit/Source/web/WebFrameImpl.cpp
@@ -404,11 +404,6 @@ public:
         return PrintContext::pageCount();
     }
 
-    virtual bool shouldUseBrowserOverlays() const
-    {
-        return true;
-    }
-
 private:
     // Set when printing.
     float m_printedPageWidth;
@@ -459,11 +454,6 @@ public:
     {
         m_plugin->printPage(pageNumber, &context);
         return 1.0;
-    }
-
-    virtual bool shouldUseBrowserOverlays() const
-    {
-        return false;
     }
 
 private:
@@ -1380,7 +1370,7 @@ VisiblePosition WebFrameImpl::visiblePositionForWindowPoint(const WebPoint& poin
     return VisiblePosition();
 }
 
-int WebFrameImpl::printBegin(const WebPrintParams& printParams, const WebNode& constrainToNode, bool* useBrowserOverlays)
+int WebFrameImpl::printBegin(const WebPrintParams& printParams, const WebNode& constrainToNode)
 {
     ASSERT(!frame()->document()->isFrameSet());
     WebPluginContainerImpl* pluginContainer = 0;
@@ -1404,8 +1394,6 @@ int WebFrameImpl::printBegin(const WebPrintParams& printParams, const WebNode& c
     // We ignore the overlays calculation for now since they are generated in the
     // browser. pageHeight is actually an output parameter.
     m_printContext->computePageRects(rect, 0, 0, 1.0, pageHeight);
-    if (useBrowserOverlays)
-        *useBrowserOverlays = m_printContext->shouldUseBrowserOverlays();
 
     return m_printContext->pageCount();
 }
