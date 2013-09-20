29faa7893814c83dd617c19870309e2a83c7a5a9
andrei.prv@gmail.com <andrei.prv@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-20 12:35:12 +0000

    Implement CSS luminance masking using skia SkLumaMaskXfermode class.
    BUG=289420
    
    Review URL: https://chromiumcodereview.appspot.com/23947005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158106 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/graphics/GraphicsContextState.h
+++ third_party/WebKit/Source/core/platform/graphics/GraphicsContextState.h
@@ -66,6 +66,7 @@ private:
         , m_shouldAntialias(true)
         , m_shouldSmoothFonts(true)
         , m_shouldClampToSourceRect(true)
+        , m_drawLuminanceMask(false)
     {
     }
 
@@ -86,6 +87,7 @@ private:
         , m_shouldAntialias(other.m_shouldAntialias)
         , m_shouldSmoothFonts(other.m_shouldSmoothFonts)
         , m_shouldClampToSourceRect(other.m_shouldClampToSourceRect)
+        , m_drawLuminanceMask(other.m_drawLuminanceMask)
     {
     }
 
@@ -139,6 +141,7 @@ private:
     bool m_shouldAntialias : 1;
     bool m_shouldSmoothFonts : 1;
     bool m_shouldClampToSourceRect : 1;
+    bool m_drawLuminanceMask : 1;
 };
 
 } // namespace WebCore
--- third_party/WebKit/Source/core/platform/graphics/skia/NativeImageSkia.cpp
+++ third_party/WebKit/Source/core/platform/graphics/skia/NativeImageSkia.cpp
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkPaint.h"
 #include "third_party/skia/include/core/SkScalar.h"
 #include "third_party/skia/include/core/SkShader.h"
+#include "third_party/skia/include/effects/SkLumaXfermode.h"
 
 #include <limits>
 #include <math.h>
@@ -325,7 +326,11 @@ void NativeImageSkia::draw(GraphicsContext* context, const SkRect& srcRect, cons
 {
     TRACE_EVENT0("skia", "NativeImageSkia::draw");
     SkPaint paint;
-    paint.setXfermode(compOp.get());
+    if (context->drawLuminanceMask()) {
+        paint.setXfermode(SkLumaMaskXfermode::Create(SkXfermode::kSrcOver_Mode));
+    } else {
+        paint.setXfermode(compOp.get());
+    }
     paint.setAlpha(context->getNormalizedAlpha());
     paint.setLooper(context->drawLooper());
     // only antialias if we're rotated or skewed
@@ -462,7 +467,11 @@ void NativeImageSkia::drawPattern(
 
     SkPaint paint;
     paint.setShader(shader.get());
-    paint.setXfermode(WebCoreCompositeToSkiaComposite(compositeOp, blendMode).get());
+    if (context->drawLuminanceMask()) {
+        paint.setXfermode(SkLumaMaskXfermode::Create(SkXfermode::kSrcOver_Mode));
+    } else {
+        paint.setXfermode(WebCoreCompositeToSkiaComposite(compositeOp, blendMode).get());
+    }
 
     paint.setFilterBitmap(resampling == LinearResampling);
     if (useBicubicFilter)
