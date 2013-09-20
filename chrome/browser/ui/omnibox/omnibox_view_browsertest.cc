eeeb38ce186dca03dba8dedc1bc29518b1910a60
mkwst@chromium.org <mkwst@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-20 11:23:10 +0000

    Disable OmniboxViewTest.Escape on Windows.
    This started failing on XP in the range between r224289 and r224301[1].
    
    [1]: http://build.chromium.org/f/chromium/perf/dashboard/ui/changelog.html?url=/trunk/src&range=224289:224301&mode=html
    
    BUG=158913
    TBR=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/23714011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224356 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/omnibox/omnibox_view_browsertest.cc
+++ chrome/browser/ui/omnibox/omnibox_view_browsertest.cc
@@ -1287,7 +1287,7 @@ IN_PROC_BROWSER_TEST_F(OmniboxViewTest, MAYBE_BackspaceInKeywordMode) {
 }
 
 // http://crbug.com/158913
-#if defined(OS_CHROMEOS)
+#if defined(OS_CHROMEOS) || defined(OS_WIN)
 #define MAYBE_Escape DISABLED_Escape
 #else
 #define MAYBE_Escape Escape
