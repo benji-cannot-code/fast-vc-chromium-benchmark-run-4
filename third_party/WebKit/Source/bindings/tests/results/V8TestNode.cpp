dd202d30dded826485f85cdef4361c4ceac86264
mkwst@chromium.org <mkwst@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-20 09:23:52 +0000

    Improve generated TypeError exception messages in bindings.
    "DOM object constructor cannot be called as a function" now reads "Failed
    to construct '[TYPE GOES HERE]': Please use the 'new' operator, this DOM
    object constructor cannot be called as a function."
    
    BUG=270033
    
    Review URL: https://chromiumcodereview.appspot.com/23653054
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158095 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/tests/results/V8TestNode.cpp
+++ third_party/WebKit/Source/bindings/tests/results/V8TestNode.cpp
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "RuntimeEnabledFeatures.h"
 #include "V8Node.h"
+#include "bindings/v8/ExceptionMessages.h"
 #include "bindings/v8/ScriptController.h"
 #include "bindings/v8/V8Binding.h"
 #include "bindings/v8/V8DOMConfiguration.h"
@@ -78,7 +79,7 @@ void V8TestNode::constructorCallback(const v8::FunctionCallbackInfo<v8::Value>& 
 {
     TRACE_EVENT_SCOPED_SAMPLING_STATE("Blink", "DOMConstructor");
     if (!args.IsConstructCall()) {
-        throwTypeError("DOM object constructor cannot be called as a function.", args.GetIsolate());
+        throwTypeError(ExceptionMessages::failedToConstruct("TestNode", "Please use the 'new' operator, this DOM object constructor cannot be called as a function."), args.GetIsolate());
         return;
     }
 
