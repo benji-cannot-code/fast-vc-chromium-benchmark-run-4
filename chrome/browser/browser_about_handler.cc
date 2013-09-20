f906bb7d6c333208199db2e043913db4d6ac649d
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-20 09:00:17 +0000

    Cleanup unused includes in browser_about_handler.cc. Remove AboutTcmallocOutputs declaration in browser_about_handler.h because that has moevd to content/browser/tcmalloc_internals_request_job.h.
    Review URL: https://chromiumcodereview.appspot.com/24250002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224342 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_about_handler.cc
+++ chrome/browser/browser_about_handler.cc
@@ -5,12 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/browser_about_handler.h"
 
-#include <algorithm>
 #include <string>
 
-#include "base/command_line.h"
 #include "base/logging.h"
-#include "base/memory/singleton.h"
 #include "base/strings/string_util.h"
 #include "chrome/browser/lifetime/application_lifetime.h"
 #include "chrome/browser/ui/browser_dialogs.h"
