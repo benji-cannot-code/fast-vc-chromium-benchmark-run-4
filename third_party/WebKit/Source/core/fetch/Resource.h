8ea2c4375cbe46b87fc3ef8801f1c97a802f4473
pliard@chromium.org <pliard@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-20 12:05:04 +0000

    Revert "Add histograms to ResourceFetcher to measure memory cache hit rate."
    This caused a PLT regression.
    
    TBR=abarth@chromium.org
    BUG=294382
    
    Review URL: https://chromiumcodereview.appspot.com/23850007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158103 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/fetch/Resource.h
+++ third_party/WebKit/Source/core/fetch/Resource.h
@@ -71,8 +71,7 @@ public:
         LinkSubresource,
         TextTrack,
         Shader,
-        ImportResource,
-        NumberOfTypes,
+        ImportResource
     };
 
     enum Status {
--- third_party/WebKit/Source/core/fetch/ResourceFetcher.cpp
+++ third_party/WebKit/Source/core/fetch/ResourceFetcher.cpp
@@ -60,7 +60,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/page/Performance.h"
 #include "core/page/ResourceTimingInfo.h"
 #include "core/page/Settings.h"
-#include "core/platform/HistogramSupport.h"
 #include "core/platform/Logging.h"
 #include "core/platform/chromium/TraceEvent.h"
 #include "public/platform/Platform.h"
@@ -74,16 +73,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-namespace {
-
-enum ActionUponResourceRequest {
-    LoadResource,
-    RevalidateResource,
-    UseResourceFromCache,
-    NumberOfResourceRequestActions,
-};
-
-Resource* createResource(Resource::Type type, const ResourceRequest& request, const String& charset)
+static Resource* createResource(Resource::Type type, const ResourceRequest& request, const String& charset)
 {
     switch (type) {
     case Resource::Image:
@@ -111,15 +101,13 @@ Resource* createResource(Resource::Type type, const ResourceRequest& request, co
         return new ShaderResource(request);
     case Resource::ImportResource:
         return new RawResource(request, type);
-    case Resource::NumberOfTypes:
-        ASSERT_NOT_REACHED();
     }
 
     ASSERT_NOT_REACHED();
     return 0;
 }
 
-ResourceLoadPriority loadPriority(Resource::Type type, const FetchRequest& request)
+static ResourceLoadPriority loadPriority(Resource::Type type, const FetchRequest& request)
 {
     if (request.priority() != ResourceLoadPriorityUnresolved)
         return request.priority();
@@ -149,14 +137,12 @@ ResourceLoadPriority loadPriority(Resource::Type type, const FetchRequest& reque
         return ResourceLoadPriorityLow;
     case Resource::Shader:
         return ResourceLoadPriorityMedium;
-    case Resource::NumberOfTypes:
-        ASSERT_NOT_REACHED();
     }
     ASSERT_NOT_REACHED();
     return ResourceLoadPriorityUnresolved;
 }
 
-Resource* resourceFromDataURIRequest(const ResourceRequest& request)
+static Resource* resourceFromDataURIRequest(const ResourceRequest& request)
 {
     const KURL& url = request.url();
     ASSERT(url.protocolIsData());
@@ -176,8 +162,6 @@ Resource* resourceFromDataURIRequest(const ResourceRequest& request)
     resource->finish();
     return resource;
 }
-
-} // namespace
 
 ResourceFetcher::ResourceFetcher(DocumentLoader* documentLoader)
     : m_document(0)
@@ -369,8 +353,6 @@ bool ResourceFetcher::checkInsecureContent(Resource::Type type, const KURL& url,
             // These cannot affect the current document.
             treatment = TreatAsAlwaysAllowedContent;
             break;
-        case Resource::NumberOfTypes:
-            ASSERT_NOT_REACHED();
         }
     }
     if (treatment == TreatAsActiveContent) {
@@ -431,8 +413,6 @@ bool ResourceFetcher::canRequest(Resource::Type type, const KURL& url, const Res
             return false;
         }
         break;
-    case Resource::NumberOfTypes:
-        ASSERT_NOT_REACHED();
     }
 
     switch (type) {
@@ -480,8 +460,6 @@ bool ResourceFetcher::canRequest(Resource::Type type, const KURL& url, const Res
         if (!shouldBypassMainWorldContentSecurityPolicy && !m_document->contentSecurityPolicy()->allowMediaFromSource(url))
             return false;
         break;
-    case Resource::NumberOfTypes:
-        ASSERT_NOT_REACHED();
     }
 
     // Last of all, check for insecure content. We do this last so that when
@@ -561,27 +539,13 @@ ResourcePtr<Resource> ResourceFetcher::requestResource(Resource::Type type, Fetc
         // Fall through
     case Load:
         resource = loadResource(type, request, request.charset());
-        HistogramSupport::histogramEnumeration(
-            "WebCore.ResourceFetcher.ActionUponResourceRequest", LoadResource,
-            NumberOfResourceRequestActions);
         break;
     case Revalidate:
         resource = revalidateResource(request, resource.get());
-        HistogramSupport::histogramEnumeration(
-            "WebCore.ResourceFetcher.ActionUponResourceRequest", RevalidateResource,
-            NumberOfResourceRequestActions);
         break;
     case Use:
         resource->updateForAccess();
         notifyLoadedFromMemoryCache(resource.get());
-        HistogramSupport::histogramEnumeration(
-            "WebCore.ResourceFetcher.ActionUponResourceRequest", UseResourceFromCache,
-            NumberOfResourceRequestActions);
-        HistogramSupport::histogramEnumeration(
-            "WebCore.ResourceFetcher.ResourceHasClientUponCacheHit", resource->hasClients(), 2);
-        HistogramSupport::histogramEnumeration(
-            "WebCore.ResourceFetcher.ResourceTypeUponCacheHit", resource->type(),
-            Resource::NumberOfTypes);
         break;
     }
 
