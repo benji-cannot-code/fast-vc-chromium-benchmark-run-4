caa821f10283a9d9cd65191277013d50dcee654c
gab@chromium.org <gab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-20 19:39:13 +0000

    Re-enable blacklist check for force_compositing_mode..!
    Oops, I made a HUGE mistake and somehow removed that check in https://chromiumcodereview.appspot.com/23874016/ (at the time IIRC I refactored this because I thought it was redudant with the blacklist check in CanDoAcceleratedCompositing... it clearly isn't since it's not checking the same blacklist item... no idea how that slipped through... my bad).
    
    
    BUG=286540
    TBR=zmo@chromium.org
    NOTRY=True
    
    Review URL: https://chromiumcodereview.appspot.com/24152012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224456 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/gpu/compositor_util.cc
+++ content/browser/gpu/compositor_util.cc
@@ -44,6 +44,11 @@ bool CanDoAcceleratedCompositing() {
   return true;
 }
 
+bool IsForceCompositingModeBlacklisted() {
+  return GpuDataManager::GetInstance()->IsFeatureBlacklisted(
+      gpu::GPU_FEATURE_TYPE_FORCE_COMPOSITING_MODE);
+}
+
 }  // namespace
 
 bool IsThreadedCompositingEnabled() {
@@ -62,7 +67,7 @@ bool IsThreadedCompositingEnabled() {
     return true;
   }
 
-  if (!CanDoAcceleratedCompositing())
+  if (!CanDoAcceleratedCompositing() || IsForceCompositingModeBlacklisted())
     return false;
 
   base::FieldTrial* trial =
@@ -84,7 +89,7 @@ bool IsForceCompositingModeEnabled() {
   else if (command_line.HasSwitch(switches::kForceCompositingMode))
     return true;
 
-  if (!CanDoAcceleratedCompositing())
+  if (!CanDoAcceleratedCompositing() || IsForceCompositingModeBlacklisted())
     return false;
 
   // Hardcode some platforms to use FCM, this has to be done here instead of via
