0226427b42e825310d6ad823b0a9748246543e82
scheib@chromium.org <scheib@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-20 23:03:17 +0000

    Update the App restored window frame upon move and resize on Mac.
    Previous change https://src.chromium.org/viewvc/chrome?revision=219648&view=revision added support for non resiazable windows to become fullscreen. That change no longer reports the current window's frame dimensions for it's GetRestoredBounds(), but instead returns a cached state which is updated with UpdateRestoredBounds(). This update method needs to be called when the window is moved or resized as well.
    
    BUG=294938
    
    Review URL: https://chromiumcodereview.appspot.com/24237008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224512 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/apps/native_app_window_cocoa.mm
+++ chrome/browser/ui/cocoa/apps/native_app_window_cocoa.mm
@@ -852,10 +852,12 @@ void NativeAppWindowCocoa::WindowDidResize() {
   else if (NSEqualPoints(frame.origin, screen.origin))
     is_maximized_ = true;
 
+  UpdateRestoredBounds();
   shell_window_->OnNativeWindowChanged();
 }
 
 void NativeAppWindowCocoa::WindowDidMove() {
+  UpdateRestoredBounds();
   shell_window_->OnNativeWindowChanged();
 }
 
