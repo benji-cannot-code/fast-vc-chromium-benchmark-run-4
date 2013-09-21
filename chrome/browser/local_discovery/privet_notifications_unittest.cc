62ea09eb53c6289159bd6b1f263dadb3ed2b1404
noamsml@chromium.org <noamsml@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-21 16:53:13 +0000

    Update privet notifications to not be printer specific
    Instead of a specific notification per printer, show the generic messages "new
    printer on your network" and "new printers on your network".  Show only one
    notification globally.
    
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/24335002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224602 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/local_discovery/privet_notifications_unittest.cc
+++ chrome/browser/local_discovery/privet_notifications_unittest.cc
@@ -26,10 +26,8 @@ const char kExampleDeviceID[] = "__test__id";
 class MockPrivetNotificationsListenerDeleagate
     : public PrivetNotificationsListener::Delegate {
  public:
-  MOCK_METHOD3(PrivetNotify, void(const std::string& device_name,
-                                  const std::string& human_readable_name,
-                                  const std::string& description));
-  MOCK_METHOD1(PrivetRemoveNotification, void(const std::string& device_name));
+  MOCK_METHOD2(PrivetNotify, void(bool multiple, bool added));
+  MOCK_METHOD0(PrivetRemoveNotification, void());
 };
 
 class MockPrivetInfoOperation : public PrivetInfoOperation {
@@ -179,9 +177,8 @@ TEST_F(PrivetNotificationsListenerTest, DisappearReappearTest) {
   ExpectInfoOperation();
 
   EXPECT_CALL(mock_delegate_, PrivetNotify(
-      kExampleDeviceName,
-      kExampleDeviceHumanName,
-      kExampleDeviceDescription));
+      false,
+      true));
 
   notification_listener_->DeviceChanged(
       true,
@@ -195,8 +192,7 @@ TEST_F(PrivetNotificationsListenerTest, DisappearReappearTest) {
   info_operation_->delegate()->OnPrivetInfoDone(info_operation_,
                                                 200, &value);
 
-  EXPECT_CALL(mock_delegate_, PrivetRemoveNotification(
-      kExampleDeviceName));
+  EXPECT_CALL(mock_delegate_, PrivetRemoveNotification());
 
   notification_listener_->DeviceRemoved(
       kExampleDeviceName);
@@ -218,9 +214,8 @@ TEST_F(PrivetNotificationsListenerTest, RegisterTest) {
   ExpectInfoOperation();
 
   EXPECT_CALL(mock_delegate_, PrivetNotify(
-      kExampleDeviceName,
-      kExampleDeviceHumanName,
-      kExampleDeviceDescription));
+      false,
+      true));
 
   notification_listener_->DeviceChanged(
       true,
@@ -234,8 +229,7 @@ TEST_F(PrivetNotificationsListenerTest, RegisterTest) {
   info_operation_->delegate()->OnPrivetInfoDone(info_operation_,
                                                 200, &value);
 
-  EXPECT_CALL(mock_delegate_, PrivetRemoveNotification(
-      kExampleDeviceName));
+  EXPECT_CALL(mock_delegate_, PrivetRemoveNotification());
 
   description_.id = kExampleDeviceID;
 
