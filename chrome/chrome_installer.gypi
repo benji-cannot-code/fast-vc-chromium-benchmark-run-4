981f3fa468419bf129ced64a28138ef6a56da6c1
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-21 16:44:25 +0000

    Invoke the CF migration path when applying a CF multi update.
    Moves MigrateChromeFrame (without changing its logic) from setup_main.cc
    into cf_migration.cc.
    
    Contributed by robertshield@chromium.org (originally from
    https://codereview.chromium.org/22632009/).
    
    BUG=265952
    TEST=A multi Chrome Frame install receiving an update will change to a SxS Chrome Frame install. And will still work.
    
    Review URL: https://chromiumcodereview.appspot.com/24287006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224600 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/chrome_installer.gypi
+++ chrome/chrome_installer.gypi
@@ -271,6 +271,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'installer/mini_installer/chrome.release',
             'installer/setup/archive_patch_helper.cc',
             'installer/setup/archive_patch_helper.h',
+            'installer/setup/cf_migration.cc',
+            'installer/setup/cf_migration.h',
             'installer/setup/chrome_frame_quick_enable.cc',
             'installer/setup/chrome_frame_quick_enable.h',
             'installer/setup/chrome_frame_ready_mode.cc',
--- chrome/installer/setup/setup_main.h
+++ chrome/installer/setup/setup_main.h
@@ -13,20 +13,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class CommandLine;
 
-namespace installer {
+namespace base {
+class FilePath;
+}  // namespace base
 
+namespace installer {
 class InstallationState;
 class InstallerState;
 class MasterPreferences;
 
 // Helper function that performs the installation of a set of products.
-installer::InstallStatus InstallProductsHelper(
-    const installer::InstallationState& original_state,
-    const CommandLine& cmd_line,
-    const installer::MasterPreferences& prefs,
-    const installer::InstallerState& installer_state,
-    installer::ArchiveType* archive_type,
-    bool* delegated_to_existing);
+// |installer_directory|, if non-NULL, is populated with the path to the
+// directory containing the newly installed setup.exe. |archive_type| is
+// populated with the type of archive found. |delegated_to_existing| is set to
+// |true| if installation was delegated to a pre-existing higher version.
+InstallStatus InstallProductsHelper(const InstallationState& original_state,
+                                    const CommandLine& cmd_line,
+                                    const MasterPreferences& prefs,
+                                    const InstallerState& installer_state,
+                                    base::FilePath* installer_directory,
+                                    ArchiveType* archive_type,
+                                    bool* delegated_to_existing);
 
 }  // namespace installer
 
