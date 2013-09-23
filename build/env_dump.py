695b2df67f29fef05242c75b96d5abdf18f8b2a4
sivachandra@chromium.org <sivachandra@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-23 23:08:09 +0000

    Treat options to the envsetup script as args.
    NOTRY=True
    
    Review URL: https://chromiumcodereview.appspot.com/24350010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224822 0039d316-1c4b-4281-b951-d872f2087c98

--- build/env_dump.py
+++ build/env_dump.py
@@ -23,6 +23,7 @@ def main():
       '-d', '--dump-mode', action='store_true',
       help='Dump the environment to sys.stdout and exit immediately.')
 
+  parser.disable_interspersed_args()
   options, args = parser.parse_args()
   if options.dump_mode:
     if args or options.output_json:
