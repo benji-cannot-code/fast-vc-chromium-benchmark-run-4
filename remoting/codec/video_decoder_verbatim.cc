dfcdeb41a7d6d2359e44ad3be26287d1048c5f4b
lambroslambrou@chromium.org <lambroslambrou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-23 18:53:52 +0000

    Cleanup VideoDecoder interface.
    This removes some un-needed methods and the DecodeResult enumeration.
    
    Review URL: https://chromiumcodereview.appspot.com/23702056
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224762 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/codec/video_decoder_verbatim.cc
+++ remoting/codec/video_decoder_verbatim.cc
@@ -10,19 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace remoting {
 
-namespace {
-// Both input and output data are assumed to be RGBA32.
-const int kBytesPerPixel = 4;
-}  // namespace
-
 VideoDecoderVerbatim::VideoDecoderVerbatim()
     : screen_size_(SkISize::Make(0, 0)) {}
 
 VideoDecoderVerbatim::~VideoDecoderVerbatim() {}
-
-bool VideoDecoderVerbatim::IsReadyForData() {
-  return true;
-}
 
 void VideoDecoderVerbatim::Initialize(const SkISize& screen_size) {
   updated_region_.setEmpty();
@@ -32,19 +23,18 @@ void VideoDecoderVerbatim::Initialize(const SkISize& screen_size) {
   // Allocate the screen buffer, if necessary.
   if (!screen_size_.isEmpty()) {
     screen_buffer_.reset(
-        new uint8
-            [screen_size_.width() * screen_size_.height() * kBytesPerPixel]);
+        new uint8[screen_size_.width() * screen_size_.height() *
+                  kBytesPerPixel]);
   }
 }
 
-VideoDecoder::DecodeResult VideoDecoderVerbatim::DecodePacket(
-    const VideoPacket* packet) {
+bool VideoDecoderVerbatim::DecodePacket(const VideoPacket& packet) {
   SkRegion region;
 
-  const char* in = packet->data().data();
+  const char* in = packet.data().data();
   int stride = kBytesPerPixel * screen_size_.width();
-  for (int i = 0; i < packet->dirty_rects_size(); ++i) {
-    Rect proto_rect = packet->dirty_rects(i);
+  for (int i = 0; i < packet.dirty_rects_size(); ++i) {
+    Rect proto_rect = packet.dirty_rects(i);
     SkIRect rect = SkIRect::MakeXYWH(proto_rect.x(),
                                      proto_rect.y(),
                                      proto_rect.width(),
@@ -53,16 +43,16 @@ VideoDecoder::DecodeResult VideoDecoderVerbatim::DecodePacket(
 
     if (!SkIRect::MakeSize(screen_size_).contains(rect)) {
       LOG(ERROR) << "Invalid packet received";
-      return DECODE_ERROR;
+      return false;
     }
 
     int rect_row_size = kBytesPerPixel * rect.width();
     uint8_t* out = screen_buffer_.get() + rect.y() * stride +
                    rect.x() * kBytesPerPixel;
     for (int y = rect.y(); y < rect.y() + rect.height(); ++y) {
-      if (in + rect_row_size > packet->data().data() + packet->data().size()) {
+      if (in + rect_row_size > packet.data().data() + packet.data().size()) {
         LOG(ERROR) << "Invalid packet received";
-        return DECODE_ERROR;
+        return false;
       }
       memcpy(out, in, rect_row_size);
       in += rect_row_size;
@@ -70,18 +60,14 @@ VideoDecoder::DecodeResult VideoDecoderVerbatim::DecodePacket(
     }
   }
 
-  if (in != packet->data().data() + packet->data().size()) {
+  if (in != packet.data().data() + packet.data().size()) {
     LOG(ERROR) << "Invalid packet received";
-    return DECODE_ERROR;
+    return false;
   }
 
   updated_region_.op(region, SkRegion::kUnion_Op);
 
-  return DECODE_DONE;
-}
-
-VideoPacketFormat::Encoding VideoDecoderVerbatim::Encoding() {
-  return VideoPacketFormat::ENCODING_VERBATIM;
+  return true;
 }
 
 void VideoDecoderVerbatim::Invalidate(const SkISize& view_size,
