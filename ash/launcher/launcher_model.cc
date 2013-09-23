4a49dfb4622f20ed422397747996c22fb66d01e0
skuhne@chromium.org <skuhne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-23 23:20:52 +0000

    Make the app list item position syncable (and movable)
    I know how much you wanted this jamescook - so I have spent over 12 hours today (saturday) to get this CL done (you owe me a beer ;) !).
    
    Please have a close look at the CL and lets hurry and cross our fingers that we can land it on Monday!
    
    What this does:
    It allows the user to change the app list (there were a few things missing, like the wight and so) + it stores the location in the pinned prefs list. Note that this is backwards compatible since all prefs get correctly stored. The placeholder for the app list will be ignored by pre M31 builds.
    
    One problem could arise though if you have an M30 and a M31 session at the same time running and you constantly change the layout on both systems. In that case the M31 system's app list would fall back to the first location, but I think that this is a rather rare case.
    
    BUG=295998
    TEST=unittests (and also checked visually)
    R=jamescook@chromium.org
    
    Review URL: https://codereview.chromium.org/23441079
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224832 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/launcher/launcher_model.cc
+++ ash/launcher/launcher_model.cc
@@ -18,6 +18,8 @@ int LauncherItemTypeToWeight(LauncherItemType type) {
   if (ash::switches::UseAlternateShelfLayout()) {
     switch (type) {
       case TYPE_APP_LIST:
+        // TODO(skuhne): If the app list item becomes movable again, this need
+        // to be a fallthrough.
         return 0;
       case TYPE_BROWSER_SHORTCUT:
       case TYPE_APP_SHORTCUT:
