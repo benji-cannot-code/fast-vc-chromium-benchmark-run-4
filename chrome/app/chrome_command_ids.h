6e830dab42580112d9f3425ef6f3bfd1bda1edd4
jackhou@chromium.org <jackhou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-23 07:27:24 +0000

    Add remaining items to Chrome App menus. (Mac)
    These are disabled as they are not yet implemented. They are being added
    now primarily for aesthetic reasons, on advice from UX.
    
    Menu items added:
    <app> -> About <app>
    File -> New
    File -> Open File
    
    Menu items removed:
    Edit -> Find
    
    This also adds a new resource string for "New", and NSTags to various
    NSMenuItems in MainMenu.xib.
    
    BUG=168080, 276052
    
    Review URL: https://chromiumcodereview.appspot.com/24153009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224687 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/chrome_command_ids.h
+++ chrome/app/chrome_command_ids.h
@@ -139,6 +139,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IDC_FIND                        37000
 #define IDC_FIND_NEXT                   37001
 #define IDC_FIND_PREVIOUS               37002
+#define IDC_FIND_MENU                   37100
 
 // Zoom
 #define IDC_ZOOM_MENU                   38000
