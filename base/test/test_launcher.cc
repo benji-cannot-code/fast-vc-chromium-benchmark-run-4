b0da0b027baa6331e9eca42766e84974c1fd433b
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-23 23:49:04 +0000

    GTTF: instantiate ThreadChecker early in the test launcher
    This makes sure it's also available during destructions.
    
    While the immediate problem happened only with ParallelTestLauncher,
    I'm proactively fixing similar pattern in other places.
    
    BUG=236893
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/24398002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224838 0039d316-1c4b-4281-b951-d872f2087c98

--- base/test/test_launcher.cc
+++ base/test/test_launcher.cc
@@ -220,6 +220,8 @@ class ResultsPrinter {
   void PrintTestsByStatus(TestResult::Status status,
                           const std::string& description);
 
+  ThreadChecker thread_checker_;
+
   // Test results grouped by test case name.
   typedef std::map<std::string, std::vector<TestResult> > ResultsMap;
   ResultsMap results_;
@@ -237,8 +239,6 @@ class ResultsPrinter {
   FILE* out_;
 
   RunTestsCallback callback_;
-
-  ThreadChecker thread_checker_;
 
   WeakPtrFactory<ResultsPrinter> weak_ptr_;
 
