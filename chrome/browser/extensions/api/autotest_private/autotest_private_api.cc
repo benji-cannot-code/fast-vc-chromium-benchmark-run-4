dc34877e28aaa4a206277d0d3d1ce942c8f3b7f7
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-23 06:38:58 +0000

    autotestPrivate.lockScreen implementation.
    BUG=233834
    TEST=manual
    
    Review URL: https://chromiumcodereview.appspot.com/24276002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224684 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/autotest_private/autotest_private_api.cc
+++ chrome/browser/extensions/api/autotest_private/autotest_private_api.cc
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_CHROMEOS)
 #include "chrome/browser/chromeos/login/screen_locker.h"
 #include "chrome/browser/chromeos/login/user_manager.h"
+#include "chromeos/dbus/dbus_thread_manager.h"
+#include "chromeos/dbus/session_manager_client.h"
 #endif
 
 namespace extensions {
@@ -94,6 +96,15 @@ bool AutotestPrivateLoginStatusFunction::RunImpl() {
 #endif
 
   SetResult(result);
+  return true;
+}
+
+bool AutotestPrivateLockScreenFunction::RunImpl() {
+  DVLOG(1) << "AutotestPrivateLockScreenFunction";
+#if defined(OS_CHROMEOS)
+  chromeos::DBusThreadManager::Get()->GetSessionManagerClient()->
+      RequestLockScreen();
+#endif
   return true;
 }
 
