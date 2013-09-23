d005cc8c556c5bf33b0f252e0c821e175e0d057e
brianderson@chromium.org <brianderson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-23 07:28:22 +0000

    about:flags: Add deadline scheduling enable/disable option
    BUG=243461
    
    Review URL: https://chromiumcodereview.appspot.com/24239012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224688 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/media_switches.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/ui_base_switches.h"
+#include "ui/compositor/compositor_switches.h"
 #include "ui/gfx/switches.h"
 #include "ui/gl/gl_switches.h"
 #include "ui/keyboard/keyboard_switches.h"
@@ -204,6 +205,22 @@ const Experiment::Choice kImplSidePaintingChoices[] = {
     cc::switches::kEnableImplSidePainting, ""},
   { IDS_GENERIC_EXPERIMENT_CHOICE_DISABLED,
     cc::switches::kDisableImplSidePainting, ""}
+};
+
+const Experiment::Choice kDeadlineSchedulingChoices[] = {
+  { IDS_GENERIC_EXPERIMENT_CHOICE_DEFAULT, "", "" },
+  { IDS_GENERIC_EXPERIMENT_CHOICE_ENABLED,
+    switches::kEnableDeadlineScheduling, ""},
+  { IDS_GENERIC_EXPERIMENT_CHOICE_DISABLED,
+    switches::kDisableDeadlineScheduling, ""}
+};
+
+const Experiment::Choice kUIDeadlineSchedulingChoices[] = {
+  { IDS_GENERIC_EXPERIMENT_CHOICE_DEFAULT, "", "" },
+  { IDS_GENERIC_EXPERIMENT_CHOICE_ENABLED,
+    switches::kUIEnableDeadlineScheduling, ""},
+  { IDS_GENERIC_EXPERIMENT_CHOICE_DISABLED,
+    switches::kUIDisableDeadlineScheduling, ""}
 };
 
 const Experiment::Choice kLCDTextChoices[] = {
@@ -1430,6 +1447,23 @@ const Experiment kExperiments[] = {
     IDS_FLAGS_IMPL_SIDE_PAINTING_DESCRIPTION,
     kOsAndroid | kOsLinux | kOsCrOS,
     MULTI_VALUE_TYPE(kImplSidePaintingChoices)
+  },
+  {
+    "deadline-scheduling",
+    IDS_FLAGS_DEADLINE_SCHEDULING_NAME,
+    IDS_FLAGS_DEADLINE_SCHEDULING_DESCRIPTION,
+    kOsMac | kOsWin | kOsLinux | kOsCrOS | kOsAndroid,
+    MULTI_VALUE_TYPE(kDeadlineSchedulingChoices)
+  },
+  {
+    "ui-deadline-scheduling",
+    IDS_FLAGS_UI_DEADLINE_SCHEDULING_NAME,
+    IDS_FLAGS_UI_DEADLINE_SCHEDULING_DESCRIPTION,
+#ifdef USE_AURA
+    kOsWin | kOsLinux |
+#endif
+    kOsCrOS,
+    MULTI_VALUE_TYPE(kUIDeadlineSchedulingChoices)
   },
   {
     "lcd-text-aa",
