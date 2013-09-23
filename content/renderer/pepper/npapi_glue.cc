741a5f33ae527152f7bc984fb21d23fad3a70c0e
teravest@chromium.org <teravest@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-23 16:30:26 +0000

    Pepper: Check WebFrame not NULL in NPObjectToPPVar
    It appears that WebFrame is sometimes NULL inside content::NPObjectToPPVar. We
    should fail gracefully when this happens.
    
    BUG=275791
    
    Review URL: https://chromiumcodereview.appspot.com/24049003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224726 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/pepper/npapi_glue.cc
+++ content/renderer/pepper/npapi_glue.cc
@@ -31,6 +31,7 @@ using ppapi::StringVar;
 using ppapi::Var;
 using WebKit::WebArrayBuffer;
 using WebKit::WebBindings;
+using WebKit::WebFrame;
 using WebKit::WebPluginContainer;
 
 namespace content {
@@ -179,9 +180,12 @@ PP_Var NPObjectToPPVar(PepperPluginInstanceImpl* instance, NPObject* object) {
   // the DOM (but the PluginInstance is not destroyed yet).
   if (!container)
     return PP_MakeUndefined();
+  WebFrame* frame = container->element().document().frame();
+  if (!frame)
+    return PP_MakeUndefined();
+
   v8::HandleScope scope(instance->GetIsolate());
-  v8::Local<v8::Context> context =
-      container->element().document().frame()->mainWorldScriptContext();
+  v8::Local<v8::Context> context = frame->mainWorldScriptContext();
   return NPObjectToPPVarImpl(instance, object, context);
 }
 
