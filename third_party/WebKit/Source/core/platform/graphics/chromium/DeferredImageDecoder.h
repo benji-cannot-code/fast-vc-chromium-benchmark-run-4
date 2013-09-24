a863a4bbf261dec20eccd1d584106c00a96c5b78
wangxianzhu@chromium.org <wangxianzhu@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-24 03:11:16 +0000

    Expose ImageDecoder::decodedSize for DeferredImageDecoder
    Added ImageDecoder::decodedSize to return the decoded size which is
    normally the actuall image size but may be smaller because of
    maxDecodedSize. DeferredImageDecoder uses it as a part of key
    (scaledSize) in the decoding store to store and find a specific
    decoder.
    
    BUG=276666
    TEST=DeferredImageDecoder.decodedSize and updated JPEGImageDecoderTest.*.
    
    Review URL: https://chromiumcodereview.appspot.com/23551014
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158233 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/graphics/chromium/DeferredImageDecoder.h
+++ third_party/WebKit/Source/core/platform/graphics/chromium/DeferredImageDecoder.h
@@ -72,6 +72,9 @@ public:
     ImageOrientation orientation() const;
     bool hotSpot(IntPoint&) const;
 
+    // For testing.
+    ImageFrameGenerator* frameGenerator() { return m_frameGenerator.get(); }
+
 private:
     explicit DeferredImageDecoder(PassOwnPtr<ImageDecoder> actualDecoder);
     void prepareLazyDecodedFrames();
