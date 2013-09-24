72e21a6866ecc7f25e0bec0422e1a6eb4f02884d
tdresser@chromium.org <tdresser@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-24 23:51:52 +0000

    Enable tap_suppression_controller_unittest.cc.
    BUG=293468
    TEST=TapSuppressionControllerTest.*
    
    Review URL: https://chromiumcodereview.appspot.com/24158004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225103 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/input/tap_suppression_controller_unittest.cc
+++ content/browser/renderer_host/input/tap_suppression_controller_unittest.cc
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/memory/scoped_ptr.h"
-#include "content/browser/renderer_host/tap_suppression_controller.h"
-#include "content/browser/renderer_host/tap_suppression_controller_client.h"
+#include "content/browser/renderer_host/input/tap_suppression_controller.h"
+#include "content/browser/renderer_host/input/tap_suppression_controller_client.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 using base::TimeDelta;
