a66d33297ac14588a0ad792e2b892bbf8da64610
garykac@chromium.org <garykac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-24 15:58:07 +0000

    Whitelist test apps for experimental APIs and input-region
    BUG=196651
    
    Review URL: https://chromiumcodereview.appspot.com/24267015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224997 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/app_current_window_internal/app_current_window_internal_api.cc
+++ chrome/browser/extensions/api/app_current_window_internal/app_current_window_internal_api.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/extensions/api/app_current_window_internal.h"
 #include "chrome/common/extensions/api/app_window.h"
 #include "chrome/common/extensions/features/feature_channel.h"
+#include "chrome/common/extensions/features/simple_feature.h"
 #include "extensions/common/switches.h"
 #include "third_party/skia/include/core/SkRegion.h"
 
@@ -152,7 +153,16 @@ bool AppCurrentWindowInternalSetIconFunction::RunWithWindow(
 
 bool AppCurrentWindowInternalSetInputRegionFunction::RunWithWindow(
     ShellWindow* window) {
-  if (GetCurrentChannel() > chrome::VersionInfo::CHANNEL_DEV) {
+
+  const char* whitelist[] = {
+    "2775E568AC98F9578791F1EAB65A1BF5F8CEF414",
+    "4AA3C5D69A4AECBD236CAD7884502209F0F5C169"
+  };
+  if (GetCurrentChannel() > chrome::VersionInfo::CHANNEL_DEV &&
+      !SimpleFeature::IsIdInWhitelist(
+          GetExtension()->id(),
+          std::set<std::string>(whitelist,
+                                whitelist + arraysize(whitelist)))) {
     error_ = kDevChannelOnly;
     return false;
   }
