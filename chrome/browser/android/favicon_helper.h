9d3a7c8800a756910955bc4aa0b36412d995ba7d
apiccion@chromium.org <apiccion@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-24 03:04:42 +0000

    Java method to get synced session favicons. Added a method to access favicons in synced session storage (e.g. other devices).
    BUG=247382
    NOTRY=True
    
    Review URL: https://chromiumcodereview.appspot.com/23441008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224867 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/android/favicon_helper.h
+++ chrome/browser/android/favicon_helper.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <jni.h>
 
+#include "base/android/scoped_java_ref.h"
 #include "base/memory/scoped_ptr.h"
 #include "chrome/common/cancelable_task_tracker.h"
 
@@ -15,11 +16,20 @@ class FaviconHelper {
  public:
   FaviconHelper();
   void Destroy(JNIEnv* env, jobject obj);
-  jboolean GetFaviconImageForURL(JNIEnv* env, jobject obj, jobject jprofile,
-                                 jstring page_url, jint icon_types,
-                                 jint desired_size_in_dip,
-                                 jobject java_favicon_image_callback);
+  jboolean GetLocalFaviconImageForURL(JNIEnv* env,
+                                      jobject obj,
+                                      jobject j_profile,
+                                      jstring j_page_url,
+                                      jint j_icon_types,
+                                      jint j_desired_size_in_dip,
+                                      jobject j_favicon_image_callback);
+  base::android::ScopedJavaLocalRef<jobject> GetSyncedFaviconImageForURL(
+      JNIEnv* env,
+      jobject obj,
+      jobject jprofile,
+      jstring j_page_url);
   static bool RegisterFaviconHelper(JNIEnv* env);
+
  private:
   scoped_ptr<CancelableTaskTracker> cancelable_task_tracker_;
 
