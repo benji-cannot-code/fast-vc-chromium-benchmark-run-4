2d8b8047f5efb17b0bafd5b097becb8c68bd16c9
asvitkine@chromium.org <asvitkine@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-24 18:03:19 +0000

    [Mac] Fix rubberband overhang appearing after a content size change.
    This was caused by r152374, which changed the wheel phase end event to
    not always call snapRubberBand(). Which resulted in some state persisting
    even after the scroll, such as m_inScrollGesture being true, causing issues
    later.
    
    For example, it would cause ScrollView::updateScrollbars() to later believe
    rubber banding was in progress and not reset the scroll position on a
    content size change (leading to this bug).
    
    This CL changes the code to always call snapRubberBand() on a phase end
    event, like before r152374.
    
    Additionally, it makes snapRubberBand() to not start a timer to revert the
    state if there's no overhang or momentum scroll, which removes a race (i.e.
    a resize before the timer fires) and makes the test more straight forward.
    
    BUG=290590
    TEST=See bug.
    
    Review URL: https://chromiumcodereview.appspot.com/24284003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158284 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/testing/runner/EventSender.cpp
+++ third_party/WebKit/Source/testing/runner/EventSender.cpp
@@ -279,6 +279,7 @@ EventSender::EventSender(TestInterfaces* interfaces)
     bindMethod("mouseScrollBy", &EventSender::mouseScrollBy);
     bindMethod("mouseUp", &EventSender::mouseUp);
     bindMethod("mouseDragBegin", &EventSender::mouseDragBegin);
+    bindMethod("mouseDragEnd", &EventSender::mouseDragEnd);
     bindMethod("releaseTouchPoint", &EventSender::releaseTouchPoint);
     bindMethod("scheduleAsynchronousClick", &EventSender::scheduleAsynchronousClick);
     bindMethod("scheduleAsynchronousKeyDown", &EventSender::scheduleAsynchronousKeyDown);
@@ -1092,6 +1093,15 @@ void EventSender::mouseDragBegin(const CppArgumentList& arguments, CppVariant* r
     WebMouseWheelEvent event;
     initMouseEvent(WebInputEvent::MouseWheel, WebMouseEvent::ButtonNone, lastMousePos, &event, getCurrentEventTimeSec(m_delegate));
     event.phase = WebMouseWheelEvent::PhaseBegan;
+    event.hasPreciseScrollingDeltas = true;
+    webview()->handleInputEvent(event);
+}
+
+void EventSender::mouseDragEnd(const CppArgumentList& arguments, CppVariant* result)
+{
+    WebMouseWheelEvent event;
+    initMouseEvent(WebInputEvent::MouseWheel, WebMouseEvent::ButtonNone, lastMousePos, &event, getCurrentEventTimeSec(m_delegate));
+    event.phase = WebMouseWheelEvent::PhaseEnded;
     event.hasPreciseScrollingDeltas = true;
     webview()->handleInputEvent(event);
 }
