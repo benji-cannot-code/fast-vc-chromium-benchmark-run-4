57ec40b5222d2bf99f0083c334d47067f89dffc0
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-24 17:00:14 +0000

    GTTF: add independent test timeout for the test launcher
    Otherwise when increasing action timeout some tests actually start taking
    longer (they probably sleep).
    
    Also change test status to timeout for tests in a batch that completed but
    took longer than the timeout so that the outcome is not dependent on test
    order and similar factors.
    
    BUG=236893
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/24255017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225013 0039d316-1c4b-4281-b951-d872f2087c98

--- base/test/test_timeouts.h
+++ base/test/test_timeouts.h
@@ -45,6 +45,13 @@ class TestTimeouts {
     return base::TimeDelta::FromMilliseconds(large_test_timeout_ms_);
   }
 
+  // Timeout for a single test launched used built-in test launcher.
+  // Do not use outside of the test launcher.
+  static base::TimeDelta test_launcher_timeout() {
+    DCHECK(initialized_);
+    return base::TimeDelta::FromMilliseconds(test_launcher_timeout_ms_);
+  }
+
  private:
   static bool initialized_;
 
@@ -52,6 +59,7 @@ class TestTimeouts {
   static int action_timeout_ms_;
   static int action_max_timeout_ms_;
   static int large_test_timeout_ms_;
+  static int test_launcher_timeout_ms_;
 
   DISALLOW_IMPLICIT_CONSTRUCTORS(TestTimeouts);
 };
