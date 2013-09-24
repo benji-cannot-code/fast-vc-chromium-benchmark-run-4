ef7c9fbabf7669a65f93c2cbd4f2aff9ec806864
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-24 22:35:55 +0000

    Disable ExternallyConnectableMessagingTest.BackgroundPageClosesOnMessageReceipt test.
    http://test-results.appspot.com/dashboards/flakiness_dashboard.html#group=%40ToT%20Chromium&testType=browser_tests&tests=ExternallyConnectableMessagingTest.BackgroundPageClosesOnMessageReceipt
    
    TBR=juanlang@google.com
    BUG=297866
    TEST=none
    
    Review URL: https://codereview.chromium.org/24453003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225091 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_messages_apitest.cc
+++ chrome/browser/extensions/extension_messages_apitest.cc
@@ -377,8 +377,9 @@ IN_PROC_BROWSER_TEST_F(ExternallyConnectableMessagingTest,
   EXPECT_FALSE(AreAnyNonWebApisDefined());
 }
 
+// See http://crbug.com/297866
 IN_PROC_BROWSER_TEST_F(ExternallyConnectableMessagingTest,
-                       BackgroundPageClosesOnMessageReceipt) {
+                       DISABLED_BackgroundPageClosesOnMessageReceipt) {
   InitializeTestServer();
 
   // Install the web connectable extension.
