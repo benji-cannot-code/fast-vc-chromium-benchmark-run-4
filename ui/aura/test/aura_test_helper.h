ae1b84d43aab809b61d7ea363ba9763162badfdd
dnicoara@chromium.org <dnicoara@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-24 06:38:52 +0000

    Update SurfaceFactoryOzone namespace in aura tests helper.
    This change was missed when the OZONE files were moved to gfx.
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/24303007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224922 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/test/aura_test_helper.h
+++ ui/aura/test/aura_test_helper.h
@@ -13,10 +13,13 @@ namespace base {
 class MessageLoopForUI;
 }
 
+namespace gfx {
+class SurfaceFactoryOzone;
+}
+
 namespace ui {
 class InputMethod;
 class ScopedAnimationDurationScaleMode;
-class SurfaceFactoryOzone;
 }
 
 namespace aura {
@@ -67,7 +70,7 @@ class AuraTestHelper {
   scoped_ptr<ui::ScopedAnimationDurationScaleMode> zero_duration_mode_;
 
 #if defined(USE_OZONE)
-  scoped_ptr<ui::SurfaceFactoryOzone> surface_factory_;
+  scoped_ptr<gfx::SurfaceFactoryOzone> surface_factory_;
 #endif
 
   DISALLOW_COPY_AND_ASSIGN(AuraTestHelper);
