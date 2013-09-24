7e4ed9c4abd08db11a5913baab644cef14bb6b82
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-24 06:40:23 +0000

    drive: Remove FileCache::IterateOnUIThread
    Remove FileCache::IterateOnUIThread().
    Rewrite user code (SyncClient and DebugInfoCollector).
    
    BUG=231222
    TEST=unit_tests
    R=hidehiko@chromium.org
    
    Review URL: https://codereview.chromium.org/24285005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224924 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/file_cache.cc
+++ chrome/browser/chromeos/drive/file_cache.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/drive/drive.pb.h"
 #include "chrome/browser/chromeos/drive/file_system_util.h"
 #include "chrome/browser/chromeos/drive/resource_metadata_storage.h"
-#include "chrome/browser/google_apis/task_util.h"
 #include "chromeos/chromeos_constants.h"
 #include "content/public/browser/browser_thread.h"
 
@@ -81,15 +80,6 @@ void RunGetCacheEntryCallback(const GetCacheEntryCallback& callback,
   callback.Run(success, *cache_entry);
 }
 
-// Calls |iteration_callback| with each entry in |cache|.
-void IterateCache(FileCache* cache,
-                  const CacheIterateCallback& iteration_callback) {
-  scoped_ptr<FileCache::Iterator> it = cache->GetIterator();
-  for (; !it->IsAtEnd(); it->Advance())
-    iteration_callback.Run(it->GetID(), it->GetValue());
-  DCHECK(!it->HasError());
-}
-
 }  // namespace
 
 FileCache::FileCache(ResourceMetadataStorage* storage,
@@ -146,21 +136,6 @@ bool FileCache::GetCacheEntry(const std::string& id, FileCacheEntry* entry) {
   DCHECK(entry);
   AssertOnSequencedWorkerPool();
   return storage_->GetCacheEntry(id, entry);
-}
-
-void FileCache::IterateOnUIThread(
-    const CacheIterateCallback& iteration_callback,
-    const base::Closure& completion_callback) {
-  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
-  DCHECK(!iteration_callback.is_null());
-  DCHECK(!completion_callback.is_null());
-
-  blocking_task_runner_->PostTaskAndReply(
-      FROM_HERE,
-      base::Bind(&IterateCache,
-                 base::Unretained(this),
-                 google_apis::CreateRelayCallback(iteration_callback)),
-      completion_callback);
 }
 
 scoped_ptr<FileCache::Iterator> FileCache::GetIterator() {
