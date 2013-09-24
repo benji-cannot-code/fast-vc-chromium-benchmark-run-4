53e23ed842ef18861301d5105f0efb58909af3a2
dominicc@chromium.org <dominicc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-24 22:24:49 +0000

    Update Webview to new Custom Elements callback names.
    These will imminently be called entered/leftViewCallback, not
    entered/leftDocumentCallback.
    
    BUG=234509
    
    Review URL: https://chromiumcodereview.appspot.com/23638010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225086 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/resources/extensions/web_view.js
+++ chrome/renderer/resources/extensions/web_view.js
@@ -869,8 +869,8 @@ function registerBrowserPluginElement() {
                                                          prototype: proto});
 
   delete proto.createdCallback;
-  delete proto.enteredDocumentCallback;
-  delete proto.leftDocumentCallback;
+  delete proto.enteredViewCallback;
+  delete proto.leftViewCallback;
   delete proto.attributeChangedCallback;
 }
 
@@ -940,8 +940,8 @@ function registerWebViewElement() {
   // Delete the callbacks so developers cannot call them and produce unexpected
   // behavior.
   delete proto.createdCallback;
-  delete proto.enteredDocumentCallback;
-  delete proto.leftDocumentCallback;
+  delete proto.enteredViewCallback;
+  delete proto.leftViewCallback;
   delete proto.attributeChangedCallback;
 }
 
