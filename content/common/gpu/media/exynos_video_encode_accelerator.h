1e8fcacba508cb03fbf2746354d62b443937c3ce
posciak@chromium.org <posciak@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-24 10:02:04 +0000

    Add HW VP8 encode support for WebRTC.
    Add HW VP8 video encode support to EVEA and RTCVideoEncoder.
    
    This is currently disabled and behind the --enable-webrtc-hw-vp8-encoding flag.
    
    Also extract controls' setup in EVEA to a separate method.
    
    BUG=266632
    TEST=apprtc, eveatest
    
    Review URL: https://chromiumcodereview.appspot.com/24259006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224968 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/media/exynos_video_encode_accelerator.h
+++ content/common/gpu/media/exynos_video_encode_accelerator.h
@@ -177,6 +177,7 @@ class ExynosVideoEncodeAccelerator : public media::VideoEncodeAccelerator {
   bool CreateGscInputBuffers();
   bool CreateGscOutputBuffers();
   bool SetMfcFormats();
+  bool InitMfcControls();
   bool CreateMfcInputBuffers();
   bool CreateMfcOutputBuffers();
 
