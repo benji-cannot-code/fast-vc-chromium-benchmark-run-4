e6c58d55a1c85b50db3a8790227f32bd115837da
dominikg@chromium.org <dominikg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-24 06:50:48 +0000

    Telemetry: temporarily stop using gesture marker in smoothness measurement.
    Wait until CL 23532057 has been rolled to the builds.
    
    TBR=nduca@chromium.org
    
    BUG=295849
    
    Review URL: https://chromiumcodereview.appspot.com/24252016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224928 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/measurements/smoothness.py
+++ tools/perf/measurements/smoothness.py
@@ -2,6 +2,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright (c) 2013 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+import logging
+
 from metrics import loading
 from metrics import smoothness
 from metrics.gpu_rendering_stats import GpuRenderingStats
@@ -85,8 +87,15 @@ class Smoothness(page_measurement.PageMeasurement):
     timeline = self._trace_result.AsTimelineModel()
     smoothness_marker = self.FindTimelineMarker(timeline,
         smoothness.TIMELINE_MARKER)
-    gesture_marker = self.FindTimelineMarker(timeline,
-        smoothness.SYNTHETIC_GESTURE_MARKER)
+    # TODO(dominikg): remove try..except once CL 23532057 has been rolled to the
+    # reference builds?
+    try:
+      gesture_marker = self.FindTimelineMarker(timeline,
+          smoothness.SYNTHETIC_GESTURE_MARKER)
+    except MissingTimelineMarker:
+      logging.warning(
+        'No gesture marker found in timeline; using smoothness marker instead.')
+      gesture_marker = smoothness_marker
     benchmark_stats = GpuRenderingStats(smoothness_marker,
                                         gesture_marker,
                                         rendering_stats_deltas,
