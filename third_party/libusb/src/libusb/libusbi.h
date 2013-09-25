9c6bfcfec69728299c6f0a5ff9e05319b48b6115
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-25 01:29:57 +0000

    Revert 225105 "Roll libusbx to 1.0.17"
    Linux ChromiumOS Full: check_licenses failed
    
    'third_party/libusb/src/libusb/version_nano.h' has non-whitelisted license
    'UNKNOWN'
    
    FAILED
    
    http://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Full/builds/25811
    
    > Roll libusbx to 1.0.17
    >
    > R=meacer@chromium.org
    >
    > Review URL: https://codereview.chromium.org/24227008
    
    TBR=ikarienator@chromium.org
    
    Review URL: https://codereview.chromium.org/24452005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225120 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/libusb/src/libusb/libusbi.h
+++ third_party/libusb/src/libusb/libusbi.h
@@ -24,8 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 
-#include <stdlib.h>
-
 #include <stddef.h>
 #include <stdint.h>
 #include <time.h>
@@ -436,7 +434,7 @@ void usbi_connect_device (struct libusb_device *dev);
 void usbi_disconnect_device (struct libusb_device *dev);
 
 /* Internal abstraction for poll (needs struct usbi_transfer on Windows) */
-#if defined(OS_LINUX) || defined(OS_DARWIN) || defined(OS_OPENBSD) || defined(OS_NETBSD)
+#if defined(OS_LINUX) || defined(OS_DARWIN) || defined(OS_OPENBSD)
 #include <unistd.h>
 #include "os/poll_posix.h"
 #elif defined(OS_WINDOWS) || defined(OS_WINCE)
@@ -998,7 +996,6 @@ extern const struct usbi_os_backend * const usbi_backend;
 extern const struct usbi_os_backend linux_usbfs_backend;
 extern const struct usbi_os_backend darwin_backend;
 extern const struct usbi_os_backend openbsd_backend;
-extern const struct usbi_os_backend netbsd_backend;
 extern const struct usbi_os_backend windows_backend;
 extern const struct usbi_os_backend wince_backend;
 
