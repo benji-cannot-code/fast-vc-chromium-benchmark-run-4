0e8cefc8bfc91edf4ead8bb86eb6ed806ae1f41e
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-25 09:00:02 +0000

    Fix copy pasta in PlatformExport.h
    R=jamesr
    BUG=297477
    
    Review URL: https://chromiumcodereview.appspot.com/24520002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158312 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/PlatformExport.h
+++ third_party/WebKit/Source/platform/PlatformExport.h
@@ -33,8 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef PlatformExport_h
 #define PlatformExport_h
 
-#if !defined(WEBORIGIN_IMPLEMENTATION)
-#define WEBORIGIN_IMPLEMENTATION 0
+#if !defined(PLATFORM_IMPLEMENTATION)
+#define PLATFORM_IMPLEMENTATION 0
 #endif
 
 #if defined(COMPONENT_BUILD)
