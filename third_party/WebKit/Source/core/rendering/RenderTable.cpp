f751a7713df6395c63d3e2d337944112c9398acf
tasak@google.com <tasak@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-25 06:06:07 +0000

    RenderTable should clip rounded rect in the same way as RenderBox does.
    RenderBox::paintBoxDecorations renders background and border into a transparency layer and then clip that in one go.
    So RenderTable should do the same way for border-radius.
    
    TESTS=fast/table/border-radius-with-image.html
    BUG=243774
    
    Review URL: https://chromiumcodereview.appspot.com/18541007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158295 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderTable.cpp
+++ third_party/WebKit/Source/core/rendering/RenderTable.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/Document.h"
 #include "core/html/HTMLTableElement.h"
 #include "core/page/FrameView.h"
+#include "core/platform/graphics/GraphicsContextStateSaver.h"
 #include "core/rendering/AutoTableLayout.h"
 #include "core/rendering/FixedTableLayout.h"
 #include "core/rendering/HitTestResult.h"
@@ -719,14 +720,15 @@ void RenderTable::paintBoxDecorations(PaintInfo& paintInfo, const LayoutPoint& p
 
     LayoutRect rect(paintOffset, size());
     subtractCaptionRect(rect);
+    paintBoxDecorationsWithRect(paintInfo, paintOffset, rect);
+}
 
-    BackgroundBleedAvoidance bleedAvoidance = determineBackgroundBleedAvoidance(paintInfo.context);
-    if (!boxShadowShouldBeAppliedToBackground(bleedAvoidance))
-        paintBoxShadow(paintInfo, rect, style(), Normal);
+void RenderTable::paintBackgroundWithBorderAndBoxShadow(PaintInfo& paintInfo, const LayoutRect& rect, BackgroundBleedAvoidance bleedAvoidance)
+{
     paintBackground(paintInfo, rect, bleedAvoidance);
     paintBoxShadow(paintInfo, rect, style(), Inset);
 
-    if (style()->hasBorder() && !collapseBorders())
+    if (bleedAvoidance != BackgroundBleedBackgroundOverBorder && !style()->hasAppearance() && style()->hasBorder() && !collapseBorders())
         paintBorder(paintInfo, rect, style());
 }
 
