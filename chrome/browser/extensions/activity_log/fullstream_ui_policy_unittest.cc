1ed756484d3c64268371e66a41698f74ecf2b2fe
mvrable@chromium.org <mvrable@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-25 00:43:49 +0000

    [Activity Log] Use TestTimeouts instead of fixed timeouts in unit tests
    Rather than hard-coding timeout values in the activity log unit tests,
    make use of the TestTimeouts class which will automatically provide
    longer timeouts when running under environments like Valgrind or ASAN.
    
    Review URL: https://chromiumcodereview.appspot.com/24386002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225113 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/activity_log/fullstream_ui_policy_unittest.cc
+++ chrome/browser/extensions/activity_log/fullstream_ui_policy_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/test/simple_test_clock.h"
+#include "base/test/test_timeouts.h"
 #include "chrome/browser/extensions/activity_log/activity_log.h"
 #include "chrome/browser/extensions/activity_log/fullstream_ui_policy.h"
 #include "chrome/browser/extensions/extension_service.h"
@@ -98,12 +99,12 @@ class FullStreamUIPolicyTest : public testing::Test {
                    checker,
                    base::MessageLoop::current()->QuitClosure()));
 
-    // Set up a timeout that will trigger after 8 seconds; if we haven't
-    // received any results by then assume that the test is broken.
+    // Set up a timeout for receiving results; if we haven't received anything
+    // when the timeout triggers then assume that the test is broken.
     base::CancelableClosure timeout(
         base::Bind(&FullStreamUIPolicyTest::TimeoutCallback));
     base::MessageLoop::current()->PostDelayedTask(
-        FROM_HERE, timeout.callback(), base::TimeDelta::FromSeconds(8));
+        FROM_HERE, timeout.callback(), TestTimeouts::action_timeout());
 
     // Wait for results; either the checker or the timeout callbacks should
     // cause the main loop to exit.
