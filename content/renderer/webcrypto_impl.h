55c7b0a0530821219677c1a1029a8ea50a8de891
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-26 06:47:14 +0000

    Refactor webcrypto unittests.
    Extracts some helper functions which will be re-used for AES-CBC tests. Notably, importing of secret keys and comparison of arraybuffer against hex string.
    
    BUG=245025
    
    Review URL: https://codereview.chromium.org/24237013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225365 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/webcrypto_impl.h
+++ content/renderer/webcrypto_impl.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
-#include "base/gtest_prod_util.h"
 #include "base/memory/scoped_ptr.h"
 #include "content/common/content_export.h"
 #include "third_party/WebKit/public/platform/WebCrypto.h"
@@ -41,8 +40,7 @@ class CONTENT_EXPORT WebCryptoImpl
       WebKit::WebCryptoResult result);
 
  protected:
-  FRIEND_TEST_ALL_PREFIXES(WebCryptoImplTest, DigestSampleSets);
-  FRIEND_TEST_ALL_PREFIXES(WebCryptoImplTest, HMACSampleSets);
+  friend class WebCryptoImplTest;
 
   void Init();
 
