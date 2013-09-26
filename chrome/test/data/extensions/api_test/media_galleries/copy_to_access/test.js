e78b635ec65a29436455cdfda9df419d61e90582
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-26 08:22:17 +0000

    Simplify the MediaGalleriesPlatformAppBrowserTest copyTo tests.
    Review URL: https://codereview.chromium.org/24356005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225393 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/api_test/media_galleries/copy_to_access/test.js
+++ chrome/test/data/extensions/api_test/media_galleries/copy_to_access/test.js
@@ -3,8 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+CreateDummyWindowToPreventSleep();
+
 chrome.test.runTests([
-  createTest(validCase, getOnwriteendCallbackExpectSuccess),
-  createTest(invalidCase, getOnwriteendCallbackExpectFailure)
+  function validFileCopyToShouldSucceed() {
+    runCopyToTest(validWEBPImageCase, true /* expect success */);
+  },
+  function invalidFileCopyToShouldFail() {
+    runCopyToTest(invalidWEBPImageCase, false /* expect failure */);
+  },
 ]);
 
