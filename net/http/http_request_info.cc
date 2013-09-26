df9b72fdcb59e44c253e429971c058fe85bbb168
eustas@chromium.org <eustas@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-26 21:57:36 +0000

    Remove unused request_id member from HttpRequestInfo.
    EOM
    
    BUG=
    
    Review URL: https://codereview.chromium.org/24743002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225583 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_request_info.cc
+++ net/http/http_request_info.cc
@@ -11,7 +11,6 @@ HttpRequestInfo::HttpRequestInfo()
     : upload_data_stream(NULL),
       load_flags(0),
       motivation(NORMAL_MOTIVATION),
-      request_id(0),
       privacy_mode(kPrivacyModeDisabled) {
 }
 
