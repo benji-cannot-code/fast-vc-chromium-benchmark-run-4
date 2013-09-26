834b92b241fa773e18fe3623bf55694ace2dca17
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-26 23:20:55 +0000

    Greatly trim down what HistoryController::restoreDocumentState does.
    It has a lot of complex rules that we don't use. We just want to restore form
    state if there's form state saved and it's a back/forward navigation.
    
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/24787004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158404 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/FrameLoader.cpp
+++ third_party/WebKit/Source/core/loader/FrameLoader.cpp
@@ -438,7 +438,6 @@ void FrameLoader::checkCompleted()
 
     // OK, completed.
     m_isComplete = true;
-    m_requestedHistoryItem = 0;
     m_frame->document()->setReadyState(Document::Complete);
     if (m_frame->document()->loadEventStillNeeded())
         m_frame->document()->implicitClose();
@@ -1546,7 +1545,6 @@ Frame* FrameLoader::findFrameForNavigation(const AtomicString& name, Document* a
 
 void FrameLoader::loadHistoryItem(HistoryItem* item)
 {
-    m_requestedHistoryItem = item;
     HistoryItem* currentItem = history()->currentItem();
 
     if (currentItem && item->shouldDoSameDocumentNavigationTo(currentItem)) {
