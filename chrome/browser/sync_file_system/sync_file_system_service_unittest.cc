7762be64aeef320c375a322629a80a99dd9f64d1
timurrrr@chromium.org <timurrrr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-26 08:54:02 +0000

    Exclude flaky tests BUG=295418,298777,298771,237710 TBR=eugenis NOTRY=true
    Review URL: https://codereview.chromium.org/24447004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225408 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/sync_file_system_service_unittest.cc
+++ chrome/browser/sync_file_system/sync_file_system_service_unittest.cc
@@ -305,7 +305,8 @@ TEST_F(SyncFileSystemServiceTest, InitializeForAppWithError) {
       SYNC_STATUS_FAILED);
 }
 
-TEST_F(SyncFileSystemServiceTest, SimpleLocalSyncFlow) {
+// Flaky.  http://crbug.com/237710
+TEST_F(SyncFileSystemServiceTest, DISABLED_SimpleLocalSyncFlow) {
   InitializeApp();
 
   StrictMock<MockSyncStatusObserver> status_observer;
