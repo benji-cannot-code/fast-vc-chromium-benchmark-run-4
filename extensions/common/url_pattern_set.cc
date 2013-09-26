cfb1a537137bebea2b5f4eb553d55b9b2e46e613
limasdf@gmail.com <limasdf@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-26 06:32:15 +0000

    [CleanUp] Use base::STLSetDifference() in place of std::set_difference()
    BUG=254066
    TEST=compile
    
    Review URL: https://codereview.chromium.org/23532084
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225358 0039d316-1c4b-4281-b951-d872f2087c98

--- extensions/common/url_pattern_set.cc
+++ extensions/common/url_pattern_set.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/memory/linked_ptr.h"
+#include "base/stl_util.h"
 #include "base/values.h"
 #include "content/public/common/url_constants.h"
 #include "extensions/common/error_utils.h"
@@ -29,10 +30,8 @@ void URLPatternSet::CreateDifference(const URLPatternSet& set1,
                                      const URLPatternSet& set2,
                                      URLPatternSet* out) {
   out->ClearPatterns();
-  std::set_difference(set1.patterns_.begin(), set1.patterns_.end(),
-                      set2.patterns_.begin(), set2.patterns_.end(),
-                      std::inserter<std::set<URLPattern> >(
-                          out->patterns_, out->patterns_.begin()));
+  out->patterns_ = base::STLSetDifference<std::set<URLPattern> >(
+      set1.patterns_, set2.patterns_);
 }
 
 // static
