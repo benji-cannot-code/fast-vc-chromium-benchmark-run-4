3be0bd110f8747399ddb1566acf398d06fbca486
caitkp@chromium.org <caitkp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-26 19:20:18 +0000

    Rename CallbackRegistry to CallbackList
    TBR=darin@chromium.org
    R=ajwong@chromium.org, darin@chromium.org
    
    Review URL: https://codereview.chromium.org/24648002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225534 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/settings/cros_settings.h
+++ chrome/browser/chromeos/settings/cros_settings.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/callback_forward.h"
-#include "base/callback_registry.h"
+#include "base/callback_list.h"
 #include "base/containers/hash_tables.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/threading/non_thread_safe.h"
@@ -95,7 +95,7 @@ class CrosSettings : public base::NonThreadSafe {
   bool RemoveSettingsProvider(CrosSettingsProvider* provider);
 
   // Add an observer Callback for changes for the given |path|.
-  typedef base::CallbackRegistry<void(void)>::Subscription ObserverSubscription;
+  typedef base::CallbackList<void(void)>::Subscription ObserverSubscription;
   scoped_ptr<ObserverSubscription> AddSettingsObserver(
       const std::string& path,
       const base::Closure& callback);
@@ -114,7 +114,7 @@ class CrosSettings : public base::NonThreadSafe {
 
   // A map from settings names to a list of observers. Observers get fired in
   // the order they are added.
-  typedef base::hash_map<std::string, base::CallbackRegistry<void(void)>*>
+  typedef base::hash_map<std::string, base::CallbackList<void(void)>*>
       SettingsObserverMap;
   SettingsObserverMap settings_observers_;
 
