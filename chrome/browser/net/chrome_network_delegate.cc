e71556b560ae45fbd120a31d6dc8e26ccad11c6b
petewil@chromium.org <petewil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-26 21:19:13 +0000

    Revert 225425 "Client Hints"
    > Client Hints
    > 
    > Client Hints can be used as input to proactive content negotiation;
    > just as the Accept header allowed clients to indicate what formats
    > they prefer, Client Hints allow clients to indicate a list of device
    > and agent specific preferences.
    > 
    > See:
    > https://github.com/igrigorik/http-client-hints/blob/draft2/draft-grigorik-http-client-hints-01.txt
    > 
    > Patched from https://codereview.chromium.org/23654014/, which was
    > patched from https://codereview.chromium.org/11970002
    > 
    > BUG=170388
    > CONTRIBUTOR=yoav@yoav.ws
    > 
    > Review URL: https://codereview.chromium.org/24451003
    
    TBR=bengr@chromium.org
    
    Review URL: https://codereview.chromium.org/24824003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225568 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/chrome_network_delegate.cc
+++ chrome/browser/net/chrome_network_delegate.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_system.h"
 #include "chrome/browser/google/google_util.h"
 #include "chrome/browser/net/chrome_network_data_saving_metrics.h"
-#include "chrome/browser/net/client_hints.h"
 #include "chrome/browser/net/connect_interceptor.h"
 #include "chrome/browser/net/load_time_stats.h"
 #include "chrome/browser/performance_monitor/performance_monitor.h"
@@ -336,11 +335,6 @@ void ChromeNetworkDelegate::set_predictor(
       new chrome_browser_net::ConnectInterceptor(predictor));
 }
 
-void ChromeNetworkDelegate::SetEnableClientHints() {
-  client_hints_.reset(new ClientHints());
-  client_hints_->Init();
-}
-
 // static
 void ChromeNetworkDelegate::NeverThrottleRequests() {
   g_never_throttle_requests_ = true;
@@ -424,12 +418,6 @@ int ChromeNetworkDelegate::OnBeforeURLRequest(
     request->SetReferrer(std::string());
   if (enable_do_not_track_ && enable_do_not_track_->GetValue())
     request->SetExtraRequestHeaderByName(kDNTHeader, "1", true /* override */);
-
-  if (client_hints_) {
-    request->SetExtraRequestHeaderByName(
-        ClientHints::kDevicePixelRatioHeader,
-        client_hints_->GetDevicePixelRatioHeader(), true);
-  }
 
   bool force_safe_search = force_google_safe_search_ &&
                            force_google_safe_search_->GetValue();
