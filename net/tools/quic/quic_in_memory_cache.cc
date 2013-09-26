8acbb1b73e9aa17f614730a26597eac3360cdf02
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-26 03:46:44 +0000

    Factor out ServerThread from the QUIC EndToEndTest in order to reuse in a Chromium test.
    Merge internal change: 53205265
    
    Review URL: https://codereview.chromium.org/24493003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225309 0039d316-1c4b-4281-b951-d872f2087c98

--- net/tools/quic/quic_in_memory_cache.cc
+++ net/tools/quic/quic_in_memory_cache.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_util.h"
 #include "base/files/file_enumerator.h"
 #include "base/stl_util.h"
+#include "base/strings/string_number_conversions.h"
 
 using base::FilePath;
 using base::StringPiece;
@@ -92,6 +93,36 @@ const QuicInMemoryCache::Response* QuicInMemoryCache::GetResponse(
     return NULL;
   }
   return it->second;
+}
+
+void QuicInMemoryCache::AddOrVerifyResponse(StringPiece method,
+                                            StringPiece path,
+                                            StringPiece version,
+                                            StringPiece response_code,
+                                            StringPiece response_detail,
+                                            StringPiece body) {
+  BalsaHeaders request_headers, response_headers;
+  request_headers.SetRequestFirstlineFromStringPieces(method,
+                                                      path,
+                                                      version);
+  response_headers.SetRequestFirstlineFromStringPieces(version,
+                                                       response_code,
+                                                       response_detail);
+  response_headers.AppendHeader("content-length",
+                                base::IntToString(body.length()));
+
+  // Check if response already exists and matches.
+  const QuicInMemoryCache::Response* cached_response =
+      GetResponse(request_headers);
+  if (cached_response == NULL) {
+    AddResponse(request_headers, response_headers, body);
+    return;
+  }
+  string cached_response_headers_str, response_headers_str;
+  cached_response->headers().DumpToString(&cached_response_headers_str);
+  response_headers.DumpToString(&response_headers_str);
+  CHECK_EQ(cached_response_headers_str, response_headers_str);
+  CHECK_EQ(cached_response->body(), body);
 }
 
 void QuicInMemoryCache::AddResponse(const BalsaHeaders& request_headers,
