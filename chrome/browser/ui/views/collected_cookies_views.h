a2f0f9762d7321f7b3f90ed824c6f3c5f75b4966
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-26 07:56:12 +0000

    Let the Views collected cookies dialog width be clamped.
    Override this dialog's GetMinimumSize() value.
    Use 0 width and continue using the preferred height.
    (lets UpdateWebContentsModalDialogPosition clamp width)
    
    Before pic (clipped dialog): http://crbug.com/295870#c1
    After pic (shrunk dialog): http://crbug.com/295870#c5
    
    BUG=295870
    TEST=The collected cookie dialog shrinks reasonably with the browser window.
    R=rfevang@chromium.org,sky@chromium.org
    
    Review URL: https://codereview.chromium.org/24329003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225385 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/collected_cookies_views.h
+++ chrome/browser/ui/views/collected_cookies_views.h
@@ -66,6 +66,7 @@ class CollectedCookiesViews : public views::DialogDelegateView,
   virtual void OnTreeViewSelectionChanged(views::TreeView* tree_view) OVERRIDE;
 
   // views::View:
+  virtual gfx::Size GetMinimumSize() OVERRIDE;
   virtual void ViewHierarchyChanged(
       const ViewHierarchyChangedDetails& details) OVERRIDE;
 
