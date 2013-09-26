906ad0b9110b27ece07760fef84e5ab14420e8d1
hidehiko@chromium.org <hidehiko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-26 17:32:56 +0000

    Support CopyOrMoveOption on Drive File System.
    This CL starts to support CopyOrMoveOption on DriveFileSystem's AsyncFileUtil.
    This will be used for Files.app's copyTo.
    
    BUG=282107
    TEST=Ran unit_tests
    
    Review URL: https://codereview.chromium.org/24532002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225488 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/fileapi_worker.h
+++ chrome/browser/chromeos/drive/fileapi_worker.h
@@ -92,6 +92,7 @@ void GetFileInfo(const base::FilePath& file_path,
 // Called from FileSystemProxy::Copy().
 void Copy(const base::FilePath& src_file_path,
           const base::FilePath& dest_file_path,
+          bool preserve_last_modified,
           const StatusCallback& callback,
           FileSystemInterface* file_system);
 
@@ -99,6 +100,7 @@ void Copy(const base::FilePath& src_file_path,
 // Called from FileSystemProxy::Move().
 void Move(const base::FilePath& src_file_path,
           const base::FilePath& dest_file_path,
+          bool preserve_last_modified,
           const StatusCallback& callback,
           FileSystemInterface* file_system);
 
