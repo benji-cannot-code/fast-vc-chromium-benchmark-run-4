3b619a8803c516606902dd978c04a4de9914145f
bryaneyler@google.com <bryaneyler@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-26 22:28:10 +0000

    Implement verify() for HMAC using NSS
    BUG=245025
    
    Review URL: https://codereview.chromium.org/24616003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225589 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/webcrypto_impl.h
+++ content/renderer/webcrypto_impl.h
@@ -38,6 +38,14 @@ class CONTENT_EXPORT WebCryptoImpl
       const unsigned char* data,
       unsigned data_size,
       WebKit::WebCryptoResult result);
+  virtual void verifySignature(
+      const WebKit::WebCryptoAlgorithm& algorithm,
+      const WebKit::WebCryptoKey& key,
+      const unsigned char* signature,
+      unsigned signature_size,
+      const unsigned char* data,
+      unsigned data_size,
+      WebKit::WebCryptoResult result);
 
  protected:
   friend class WebCryptoImplTest;
@@ -63,6 +71,14 @@ class CONTENT_EXPORT WebCryptoImpl
       const unsigned char* data,
       unsigned data_size,
       WebKit::WebArrayBuffer* buffer);
+  bool VerifySignatureInternal(
+      const WebKit::WebCryptoAlgorithm& algorithm,
+      const WebKit::WebCryptoKey& key,
+      const unsigned char* signature,
+      unsigned signature_size,
+      const unsigned char* data,
+      unsigned data_size,
+      bool* signature_match);
 
  private:
   DISALLOW_COPY_AND_ASSIGN(WebCryptoImpl);
