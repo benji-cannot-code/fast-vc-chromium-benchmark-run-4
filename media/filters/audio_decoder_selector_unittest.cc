733a29cbbc38b4b15583970482fa5e75c8aa047e
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-26 10:05:59 +0000

    Add AudioDecoderSelector::Abort().
    This new functions isn't used by AudioRendererImpl yet.
    AudioRendererImpl::Stop() doesn't wait for AudioDecoderSelector to finish.
    But it's still good to add this function for completeness and be future ready.
    
    BUG=222054
    TEST=Added unittests.
    
    Review URL: https://codereview.chromium.org/24495002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225434 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/audio_decoder_selector_unittest.cc
+++ media/filters/audio_decoder_selector_unittest.cc
@@ -26,6 +26,10 @@ class AudioDecoderSelectorTest : public ::testing::Test {
  public:
   enum DecryptorCapability {
     kNoDecryptor,
+    // Used to test Abort() during DecryptingAudioDecoder::Initialize() and
+    // DecryptingDemuxerStream::Initialize(). We don't need this for normal
+    // AudioDecoders since we use MockAudioDecoder.
+    kHoldSetDecryptor,
     kDecryptOnly,
     kDecryptAndDecode
   };
@@ -68,11 +72,14 @@ class AudioDecoderSelectorTest : public ::testing::Test {
   void InitializeDecoderSelector(DecryptorCapability decryptor_capability,
                                  int num_decoders) {
     SetDecryptorReadyCB set_decryptor_ready_cb;
+    if (decryptor_capability != kNoDecryptor) {
+      set_decryptor_ready_cb =
+          base::Bind(&AudioDecoderSelectorTest::SetDecryptorReadyCallback,
+                     base::Unretained(this));
+    }
+
     if (decryptor_capability == kDecryptOnly ||
         decryptor_capability == kDecryptAndDecode) {
-      set_decryptor_ready_cb = base::Bind(
-          &AudioDecoderSelectorTest::SetDecryptorReadyCallback,
-          base::Unretained(this));
 
       EXPECT_CALL(*this, SetDecryptorReadyCallback(_))
           .WillRepeatedly(RunCallback<0>(decryptor_.get()));
@@ -84,6 +91,9 @@ class AudioDecoderSelectorTest : public ::testing::Test {
         EXPECT_CALL(*decryptor_, InitializeAudioDecoder(_, _))
             .WillRepeatedly(RunCallback<1>(true));
       }
+    } else if (decryptor_capability == kHoldSetDecryptor) {
+      // Set DecryptorReadyCB but the callback is never fired.
+      EXPECT_CALL(*this, SetDecryptorReadyCallback(_));
     }
 
     DCHECK_GE(all_decoders_.size(), static_cast<size_t>(num_decoders));
@@ -103,6 +113,14 @@ class AudioDecoderSelectorTest : public ::testing::Test {
                    base::Unretained(this)),
         base::Bind(&AudioDecoderSelectorTest::MockOnDecoderSelected,
                    base::Unretained(this)));
+    message_loop_.RunUntilIdle();
+  }
+
+  void SelectDecoderAndAbort() {
+    SelectDecoder();
+
+    EXPECT_CALL(*this, OnDecoderSelected(IsNull(), IsNull()));
+    decoder_selector_->Abort();
     message_loop_.RunUntilIdle();
   }
 
@@ -145,6 +163,16 @@ TEST_F(AudioDecoderSelectorTest, ClearStream_NoDecryptor_OneClearDecoder) {
   SelectDecoder();
 }
 
+TEST_F(AudioDecoderSelectorTest,
+       Abort_ClearStream_NoDecryptor_OneClearDecoder) {
+  UseClearStream();
+  InitializeDecoderSelector(kNoDecryptor, 1);
+
+  EXPECT_CALL(*decoder_1_, Initialize(_, _, _));
+
+  SelectDecoderAndAbort();
+}
+
 // The stream is not encrypted and we have multiple clear decoders. The first
 // decoder that can decode the input stream will be selected.
 TEST_F(AudioDecoderSelectorTest, ClearStream_NoDecryptor_MultipleClearDecoder) {
@@ -160,6 +188,18 @@ TEST_F(AudioDecoderSelectorTest, ClearStream_NoDecryptor_MultipleClearDecoder) {
   SelectDecoder();
 }
 
+TEST_F(AudioDecoderSelectorTest,
+       Abort_ClearStream_NoDecryptor_MultipleClearDecoder) {
+  UseClearStream();
+  InitializeDecoderSelector(kNoDecryptor, 2);
+
+  EXPECT_CALL(*decoder_1_, Initialize(_, _, _))
+      .WillOnce(RunCallback<1>(DECODER_ERROR_NOT_SUPPORTED));
+  EXPECT_CALL(*decoder_2_, Initialize(_, _, _));
+
+  SelectDecoderAndAbort();
+}
+
 // There is a decryptor but the stream is not encrypted. The decoder will be
 // selected.
 TEST_F(AudioDecoderSelectorTest, ClearStream_HasDecryptor) {
@@ -171,6 +211,15 @@ TEST_F(AudioDecoderSelectorTest, ClearStream_HasDecryptor) {
   EXPECT_CALL(*this, OnDecoderSelected(decoder_1_, IsNull()));
 
   SelectDecoder();
+}
+
+TEST_F(AudioDecoderSelectorTest, Abort_ClearStream_HasDecryptor) {
+  UseClearStream();
+  InitializeDecoderSelector(kDecryptOnly, 1);
+
+  EXPECT_CALL(*decoder_1_, Initialize(_, _, _));
+
+  SelectDecoderAndAbort();
 }
 
 // The stream is encrypted and there's no decryptor. No decoder can be selected.
@@ -194,6 +243,14 @@ TEST_F(AudioDecoderSelectorTest, EncryptedStream_DecryptOnly_NoClearDecoder) {
   SelectDecoder();
 }
 
+TEST_F(AudioDecoderSelectorTest,
+       Abort_EncryptedStream_DecryptOnly_NoClearDecoder) {
+  UseEncryptedStream();
+  InitializeDecoderSelector(kHoldSetDecryptor, 0);
+
+  SelectDecoderAndAbort();
+}
+
 // Decryptor can do decryption-only and there's a decoder available. The decoder
 // will be selected and a DecryptingDemuxerStream will be created.
 TEST_F(AudioDecoderSelectorTest, EncryptedStream_DecryptOnly_OneClearDecoder) {
@@ -205,6 +262,16 @@ TEST_F(AudioDecoderSelectorTest, EncryptedStream_DecryptOnly_OneClearDecoder) {
   EXPECT_CALL(*this, OnDecoderSelected(decoder_1_, NotNull()));
 
   SelectDecoder();
+}
+
+TEST_F(AudioDecoderSelectorTest,
+       Abort_EncryptedStream_DecryptOnly_OneClearDecoder) {
+  UseEncryptedStream();
+  InitializeDecoderSelector(kDecryptOnly, 1);
+
+  EXPECT_CALL(*decoder_1_, Initialize(_, _, _));
+
+  SelectDecoderAndAbort();
 }
 
 // Decryptor can only do decryption and there are multiple decoders available.
@@ -224,6 +291,18 @@ TEST_F(AudioDecoderSelectorTest,
   SelectDecoder();
 }
 
+TEST_F(AudioDecoderSelectorTest,
+       Abort_EncryptedStream_DecryptOnly_MultipleClearDecoder) {
+  UseEncryptedStream();
+  InitializeDecoderSelector(kDecryptOnly, 2);
+
+  EXPECT_CALL(*decoder_1_, Initialize(_, _, _))
+      .WillOnce(RunCallback<1>(DECODER_ERROR_NOT_SUPPORTED));
+  EXPECT_CALL(*decoder_2_, Initialize(_, _, _));
+
+  SelectDecoderAndAbort();
+}
+
 // Decryptor can do decryption and decoding. A DecryptingAudioDecoder will be
 // created and selected. The clear decoders should not be touched at all.
 // No DecryptingDemuxerStream should to be created.
@@ -234,6 +313,13 @@ TEST_F(AudioDecoderSelectorTest, EncryptedStream_DecryptAndDecode) {
   EXPECT_CALL(*this, OnDecoderSelected(NotNull(), IsNull()));
 
   SelectDecoder();
+}
+
+TEST_F(AudioDecoderSelectorTest, Abort_EncryptedStream_DecryptAndDecode) {
+  UseEncryptedStream();
+  InitializeDecoderSelector(kHoldSetDecryptor, 1);
+
+  SelectDecoderAndAbort();
 }
 
 }  // namespace media
