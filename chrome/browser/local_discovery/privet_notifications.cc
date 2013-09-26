bbace59dff071a74b9dfa719b552d53ac840b66b
noamsml@chromium.org <noamsml@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-26 23:31:16 +0000

    Show created browser window when privet notification is clicked
    Show browser created by FindOrCreatedTabbedBrowser when the privet notification
    is clicked. Otherwise, it may stay hidden.
    
    BUG=299116
    
    Review URL: https://codereview.chromium.org/24805002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225604 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/local_discovery/privet_notifications.cc
+++ chrome/browser/local_discovery/privet_notifications.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
+#include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/host_desktop.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/webui/local_discovery/local_discovery_ui_handler.h"
@@ -376,18 +377,11 @@ void PrivetNotificationDelegate::ButtonClick(int button_index) {
 void PrivetNotificationDelegate::OpenTab(const GURL& url) {
   Profile* profile_obj = Profile::FromBrowserContext(profile_);
 
-  Browser* browser = FindOrCreateTabbedBrowser(profile_obj,
-                                               chrome::GetActiveDesktop());
-  content::WebContents::CreateParams create_params(profile_obj);
-
-  scoped_ptr<content::WebContents> contents(
-      content::WebContents::Create(create_params));
-  contents->GetController().LoadURL(url,
-                                    content::Referrer(),
-                                    content::PAGE_TRANSITION_AUTO_TOPLEVEL,
-                                    "");
-
-  browser->tab_strip_model()->AppendWebContents(contents.release(), true);
+  chrome::NavigateParams params(profile_obj,
+                              url,
+                              content::PAGE_TRANSITION_AUTO_TOPLEVEL);
+  params.disposition = NEW_FOREGROUND_TAB;
+  chrome::Navigate(&params);
 }
 
 void PrivetNotificationDelegate::DisableNotifications() {
