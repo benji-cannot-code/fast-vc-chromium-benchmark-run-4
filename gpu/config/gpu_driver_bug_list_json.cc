e109ae95376a8ad912588431c58d2b90b8d8cedb
skyostil@chromium.org <skyostil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-26 03:27:31 +0000

    Re-enable framebuffer discarding on new ARM drivers
    Re-enable framebuffer discarding on new ARM drivers now that the
    flickering bug has been fixed.
    
    BUG=290876
    
    Review URL: https://codereview.chromium.org/24169007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225306 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/config/gpu_driver_bug_list_json.cc
+++ gpu/config/gpu_driver_bug_list_json.cc
@@ -20,7 +20,7 @@ const char kGpuDriverBugListJson[] = LONG_STRING_CONST(
 {
   "name": "gpu driver bug list",
   // Please update the version number whenever you change this file.
-  "version": "2.18",
+  "version": "2.19",
   "entries": [
     {
       "id": 1,
@@ -548,9 +548,13 @@ const char kGpuDriverBugListJson[] = LONG_STRING_CONST(
     {
       "id": 40,
       "cr_bugs": [290876],
-      "description": "Framebuffer discarding causes flickering on ARM",
+      "description": "Framebuffer discarding causes flickering on old ARM drivers",
       "os": {
-        "type": "android"
+        "type": "android",
+        "version": {
+          "op": "<",
+          "value": "4.4"
+        }
       },
       "gl_vendor": {
         "op": "beginwith",
