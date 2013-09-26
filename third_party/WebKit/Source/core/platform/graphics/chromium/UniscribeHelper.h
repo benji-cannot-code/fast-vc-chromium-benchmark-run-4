198ebe15beaf88842c8e0a702b470003a796bb01
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-26 05:58:32 +0000

    Merge FontPlatformData::setupPaint and setupPaintForFont on Win
    We currently have two different setupPaint methods for the font
    graphics code on windows, one used for drawing (setupPaintForFont)
    and one used for metrics for the non-gdi code path (setupPaint).
    
    This is silly and error prone, merge the two into
    FontPlatformData::setupPaint.
    
    BUG=249099
    
    Review URL: https://chromiumcodereview.appspot.com/24422003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158352 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/graphics/chromium/UniscribeHelper.h
+++ third_party/WebKit/Source/core/platform/graphics/chromium/UniscribeHelper.h
@@ -47,6 +47,7 @@ namespace WebCore {
 
 class FloatRect;
 class FontFeatureSettings;
+class FontPlatformData;
 class GraphicsContext;
 
 const unsigned cUniscribeHelperStackRuns = 8;
@@ -198,10 +199,9 @@ public:
     // be pre-set.
     //
     // The y position is the upper left corner, NOT the baseline.
-    void draw(GraphicsContext* graphicsContext, HDC dc,
-              int x, int y,
-              const FloatRect& textRect,
-              int from, int to);
+    void draw(GraphicsContext*, const FontPlatformData&, HDC,
+        int x, int y, const FloatRect& textRect,
+        int from, int to);
 
     // Returns the first glyph assigned to the character at the given offset.
     // This function is used to retrieve glyph information when Uniscribe is
