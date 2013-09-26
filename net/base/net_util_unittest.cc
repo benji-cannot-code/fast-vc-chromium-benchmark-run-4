532fd582fc0484c08f52517e0494dcf3ca2a6223
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-26 03:14:16 +0000

    Added NetworkInterface::network_prefix.
    BUG=295997
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/23726043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225303 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/net_util_unittest.cc
+++ net/base/net_util_unittest.cc
@@ -3292,7 +3292,6 @@ TEST(NetUtilTest, IsLocalhost) {
 TEST(NetUtilTest, GetNetworkList) {
   NetworkInterfaceList list;
   ASSERT_TRUE(GetNetworkList(&list));
-
   for (NetworkInterfaceList::iterator it = list.begin();
        it != list.end(); ++it) {
     // Verify that the name is not empty.
@@ -3310,6 +3309,8 @@ TEST(NetUtilTest, GetNetworkList) {
       }
     }
     EXPECT_FALSE(all_zeroes);
+    EXPECT_GE(it->network_prefix, 1u);
+    EXPECT_LE(it->network_prefix, it->address.size() * 8);
   }
 }
 
