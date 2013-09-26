74deeb4d62ec87a3f3972065bbce3269b636826c
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-26 20:03:35 +0000

    Rename "disable-legacy-encrytped-media" flag to "disable-prefixed-encrypted-media".
    The new name better reflects what this flag is doing.
    
    TBR=jochen@chromium.org,davemoore@chromium.org
    
    Review URL: https://codereview.chromium.org/24228004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225540 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -916,7 +916,7 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kEnableDeadlineScheduling,
     switches::kEnableDelegatedRenderer,
     switches::kEnableEncryptedMedia,
-    switches::kDisableLegacyEncryptedMedia,
+    switches::kDisablePrefixedEncryptedMedia,
     switches::kOverrideEncryptedMediaCanPlayType,
 #if defined(OS_ANDROID)
     switches::kMediaDrmEnableNonCompositing,
