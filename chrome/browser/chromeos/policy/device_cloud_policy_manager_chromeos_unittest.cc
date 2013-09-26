dba9b015bc0ba7bbb8793b5f37f5429f4ffb0e0c
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-26 05:19:41 +0000

    cryptohome: Remove unnecessary include of mock_cryptohome_library.h
    BUG=298605
    TEST=none
    R=hashimoto@chromium.org
    
    Review URL: https://codereview.chromium.org/24618004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225341 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/policy/device_cloud_policy_manager_chromeos_unittest.cc
+++ chrome/browser/chromeos/policy/device_cloud_policy_manager_chromeos_unittest.cc
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/prefs/browser_prefs.h"
 #include "chrome/test/base/testing_browser_process.h"
 #include "chromeos/cryptohome/cryptohome_library.h"
-#include "chromeos/cryptohome/mock_cryptohome_library.h"
 #include "chromeos/dbus/cryptohome_client_stub.h"
 #include "chromeos/dbus/dbus_client_implementation_type.h"
 #include "google_apis/gaia/gaia_oauth_client.h"
