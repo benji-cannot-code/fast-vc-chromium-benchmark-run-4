a9b5d925e89cdfae80d4e3e1f9f45f735bc16738
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-26 18:12:13 +0000

    Fixes possible use after free when deleting menu during execution
    The menu code returns an enum indicating if the menu was deleted while
    running. Currently the code would return success if deletion happened
    while notifying the delegate of the item that was selected. It needs
    to return the item was deleted in this case too.
    
    BUG=292980
    TEST=none
    R=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/24614002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225507 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/menu/menu_runner.cc
+++ ui/views/controls/menu/menu_runner.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <set>
 
+#include "base/memory/weak_ptr.h"
 #include "base/metrics/histogram.h"
 #include "ui/base/models/menu_model.h"
 #include "ui/views/controls/button/menu_button.h"
@@ -126,6 +127,9 @@ class MenuRunnerImpl : public internal::MenuControllerDelegate {
   // The timestamp of the event which closed the menu - or 0.
   base::TimeDelta closing_event_time_;
 
+  // Used to detect deletion of |this| when notifying delegate of success.
+  base::WeakPtrFactory<MenuRunnerImpl> weak_factory_;
+
   DISALLOW_COPY_AND_ASSIGN(MenuRunnerImpl);
 };
 
@@ -136,7 +140,8 @@ MenuRunnerImpl::MenuRunnerImpl(MenuItemView* menu)
       for_drop_(false),
       controller_(NULL),
       owns_controller_(false),
-      closing_event_time_(base::TimeDelta()) {
+      closing_event_time_(base::TimeDelta()),
+      weak_factory_(this) {
 }
 
 void MenuRunnerImpl::Release() {
@@ -284,8 +289,12 @@ MenuRunner::RunResult MenuRunnerImpl::MenuDone(MenuItemView* result,
   }
   running_ = false;
   if (result && menu_->GetDelegate()) {
+    // Executing the command may also delete this.
+    base::WeakPtr<MenuRunnerImpl> ref(weak_factory_.GetWeakPtr());
     menu_->GetDelegate()->ExecuteCommand(result->GetCommand(),
                                          mouse_event_flags);
+    if (!ref)
+      return MenuRunner::MENU_DELETED;
   }
   return MenuRunner::NORMAL_EXIT;
 }
