835b09130437aca7688a64f2c3ad96577344dff1
ohrn@opera.com <ohrn@opera.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-26 19:19:50 +0000

    Fix missing SYS_mmap when building Android on x86.
    Review URL: https://codereview.chromium.org/24560002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225533 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/tcmalloc/chromium/src/malloc_hook_mmap_linux.h
+++ third_party/tcmalloc/chromium/src/malloc_hook_mmap_linux.h
@@ -55,6 +55,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // SYS_mmap2, SYS_munmap, SYS_mremap and __off64_t are not defined in Android.
 #if defined(__ANDROID__)
+#if defined(__NR_mmap) && !defined(SYS_mmap)
+#define SYS_mmap __NR_mmap
+#endif
 #ifndef SYS_mmap2
 #define SYS_mmap2 __NR_mmap2
 #endif
