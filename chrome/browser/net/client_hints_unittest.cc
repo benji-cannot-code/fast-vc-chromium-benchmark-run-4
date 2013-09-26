a97da997e21546ca21502406204680c818cd97ad
petewil@chromium.org <petewil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-26 20:25:55 +0000

    Revert 225538 "Leaking locale changes is a Very Bad Idea."
    > Leaking locale changes is a Very Bad Idea.
    > 
    > BUG=298893,298900
    > TEST=tests stop failing
    > R=cbentzel@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/24809002
    
    TBR=avi@chromium.org
    
    Review URL: https://codereview.chromium.org/24815002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225546 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/client_hints_unittest.cc
+++ chrome/browser/net/client_hints_unittest.cc
@@ -5,8 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/net/client_hints.h"
 
+#include <locale.h>
+
 #include "base/logging.h"
-#include "base/test/scoped_locale.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 class ClientHintsTest : public testing::Test {
@@ -26,7 +27,7 @@ TEST_F(ClientHintsTest, HintsWellFormatted) {
 }
 
 TEST_F(ClientHintsTest, HintsWellFormattedWithNonEnLocale) {
-  base::ScopedLocale locale("fr_FR.UTF-8");
+  setlocale(LC_ALL, "fr_FR.UTF-8");
   UpdateScreenInfo(1.567f);
   std::string hint = client_hints_.GetDevicePixelRatioHeader();
   EXPECT_EQ("1.57", hint);
