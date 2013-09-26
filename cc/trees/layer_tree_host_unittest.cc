ba559b4b94776e34f7d4d49852dc7619fe7369fc
alokp@chromium.org <alokp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-26 20:28:00 +0000

    Remove LTH test dependency on surface caching.
    Missed updating one call-site in r225515. The test did not fail because the expected return value was FALSE, which would be returned if surface caching is disabled.
    
    TBR=danakj@chromium.org
    
    Review URL: https://codereview.chromium.org/24786003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225547 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/trees/layer_tree_host_unittest.cc
+++ cc/trees/layer_tree_host_unittest.cc
@@ -1676,9 +1676,9 @@ class LayerTreeHostTestSurfaceNotAllocatedForLayersOutsideMemoryLimit
         host_impl->SetDiscardBackBufferWhenNotVisible(true);
         break;
       case 1:
-        EXPECT_FALSE(renderer->HaveCachedResourcesForRenderPassId(
+        EXPECT_FALSE(renderer->HasAllocatedResourcesForTesting(
             surface1_render_pass_id));
-        EXPECT_FALSE(renderer->HaveCachedResourcesForRenderPassId(
+        EXPECT_FALSE(renderer->HasAllocatedResourcesForTesting(
             surface2_render_pass_id));
 
         EndTest();
