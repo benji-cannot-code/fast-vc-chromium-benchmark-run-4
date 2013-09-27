ab1493454d9bd3a4fa322755a543c4720ed4ca23
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-27 06:41:39 +0000

    Cleanup: Move imageTitle() from LocalizedStrings to ImageDocument.
    Our implmenetation of imageTitle() is locale-independent.
    
    BUG=298020
    TEST=none; no behavior changes.
    
    Review URL: https://chromiumcodereview.appspot.com/24878005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158421 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/LocalizedStrings.h
+++ third_party/WebKit/Source/core/platform/LocalizedStrings.h
@@ -33,8 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-    class IntSize;
-
     // FIXME: Use Locale::queryString instead of the following functions.
 
     String searchableIndexIntroduction();
@@ -74,8 +72,6 @@ namespace WebCore {
 
     String missingPluginText();
     String blockedPluginByContentSecurityPolicyText();
-
-    String imageTitle(const String& filename, const IntSize& size);
 
     String localizedMediaControlElementString(const String&);
     String localizedMediaControlElementHelpText(const String&);
