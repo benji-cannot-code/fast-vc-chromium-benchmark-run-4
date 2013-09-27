05aadfc1927a022ad3f7675bf7db33c4c6ee12b1
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-27 05:16:03 +0000

    [webcrypto] Remove the mock implementation of WebCrypto.
    Instead let the layout tests run with WebCryptoImpl.
    
    R=jamesr@chromium.org, jochen@chromium.org
    TBR=joi
    BUG=245025
    
    Review URL: https://codereview.chromium.org/24702003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225665 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/renderer/content_renderer_client.cc
+++ content/public/renderer/content_renderer_client.cc
@@ -80,10 +80,6 @@ WebKit::WebSpeechSynthesizer* ContentRendererClient::OverrideSpeechSynthesizer(
   return NULL;
 }
 
-WebKit::WebCrypto* ContentRendererClient::OverrideWebCrypto() {
-  return NULL;
-}
-
 bool ContentRendererClient::RunIdleHandlerWhenWidgetsHidden() {
   return true;
 }
