d56cab5fda0a89bb3be2742ed8198a0fce90e12c
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-27 19:19:10 +0000

    Makes destruction of DesktopNativeWidgetAura saner
    Prior to this patch DesktopNativeWidgetAura destruction keyed off
    destruction of the window_ (not the rootwindow). This meant that by
    the time we destroyed the Widget the RootWindow was still live. This
    meant any children of the rootwindow, such as child widgets, were
    alive as well. The general expectation is that by the time a Widget is
    destroyed and child Widgets have also been destroyed.
    
    This patch changes destruction so that we key off the
    AcceleratedWidget going away. When that happens we destroy the
    RootWindow. By doing this we ensure child Widgets have also been
    destroyed.
    
    BUG=288953
    TEST=covered by test
    R=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/24515004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225755 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/frame/desktop_browser_frame_aura.cc
+++ chrome/browser/ui/views/frame/desktop_browser_frame_aura.cc
@@ -46,6 +46,11 @@ DesktopBrowserFrameAura::~DesktopBrowserFrameAura() {
 ///////////////////////////////////////////////////////////////////////////////
 // DesktopBrowserFrameAura, views::DesktopNativeWidgetAura overrides:
 
+void DesktopBrowserFrameAura::OnHostClosed() {
+  aura::client::SetVisibilityClient(GetNativeView()->GetRootWindow(), NULL);
+  DesktopNativeWidgetAura::OnHostClosed();
+}
+
 void DesktopBrowserFrameAura::InitNativeWidget(
     const views::Widget::InitParams& params) {
   browser_desktop_root_window_host_ =
@@ -70,11 +75,6 @@ void DesktopBrowserFrameAura::InitNativeWidget(
                                     visibility_controller_.get());
   views::corewm::SetChildWindowVisibilityChangesAnimated(
       GetNativeView()->GetRootWindow());
-}
-
-void DesktopBrowserFrameAura::OnWindowDestroying() {
-  aura::client::SetVisibilityClient(GetNativeView()->GetRootWindow(), NULL);
-  DesktopNativeWidgetAura::OnWindowDestroying();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
