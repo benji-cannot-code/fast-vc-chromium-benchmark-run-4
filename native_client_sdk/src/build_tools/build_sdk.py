a7f0a8e50ab3950749a97ab194e9a9f5882f0009
jvoung@chromium.org <jvoung@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-27 21:57:59 +0000

    [NaCl SDK] Remove pnacl.h from the sdk.
    The __builtin_nacl_target_arch that it defines is not
    part of the PNaCl bitcode ABI.  For non-pnacl cases, it has
    #error when !defined(__pnacl__) so doesn't make sense to
    have there either.
    
    It is only used in the private build of pnacl-llc.
    
    BUG=https://code.google.com/p/nativeclient/issues/detail?id=3525
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/25039005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225793 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/build_tools/build_sdk.py
+++ native_client_sdk/src/build_tools/build_sdk.py
@@ -288,7 +288,6 @@ NACL_HEADER_MAP = {
       ('src/untrusted/nacl/nacl_dyncode.h', 'nacl/'),
       ('src/untrusted/nacl/nacl_startup.h', 'nacl/'),
       ('src/untrusted/nacl/nacl_thread.h', 'nacl/'),
-      ('src/untrusted/nacl/pnacl.h', ''),
       ('src/untrusted/pthread/pthread.h', ''),
       ('src/untrusted/pthread/semaphore.h', ''),
       ('src/untrusted/valgrind/dynamic_annotations.h', 'nacl/'),
@@ -302,7 +301,6 @@ NACL_HEADER_MAP = {
       ('src/untrusted/nacl/nacl_dyncode.h', 'nacl/'),
       ('src/untrusted/nacl/nacl_startup.h', 'nacl/'),
       ('src/untrusted/nacl/nacl_thread.h', 'nacl/'),
-      ('src/untrusted/nacl/pnacl.h', ''),
       ('src/untrusted/valgrind/dynamic_annotations.h', 'nacl/'),
   ],
   'host': []
