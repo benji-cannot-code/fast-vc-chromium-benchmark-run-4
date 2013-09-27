96542d6480c41a11fa9a9e56fe1755390fa8dfdb
dzhioev@chromium.org <dzhioev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-27 17:20:31 +0000

    Added flag enabling new first-run UI to about:flags.
    BUG=269294
    NOTRY=true
    TBR=nkostylev
    
    Review URL: https://codereview.chromium.org/25099002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225723 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1772,6 +1772,15 @@ const Experiment kExperiments[] = {
     SINGLE_VALUE_TYPE(switches::kEnableCast)
   },
 #endif
+#if defined(OS_CHROMEOS)
+  {
+    "enable-first-run-ui",
+    IDS_FLAGS_ENABLE_FIRST_RUN_UI_NAME,
+    IDS_FLAGS_ENABLE_FIRST_RUN_UI_DESCRIPTION,
+    kOsCrOS,
+    SINGLE_VALUE_TYPE(chromeos::switches::kEnableFirstRunUI)
+  },
+#endif
 };
 
 const Experiment* experiments = kExperiments;
