7878851618c4ac6a07396262e2ea2fb8d0f25bd1
ajm@chromium.org <ajm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-27 00:19:30 +0000

    Add experimental AGC to default audio constraints.
    TESTED=Experimental AGC was enabled in apprtc.
    
    Review URL: https://codereview.chromium.org/24516002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225614 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/media_stream_dependency_factory.cc
+++ content/renderer/media/media_stream_dependency_factory.cc
@@ -70,6 +70,8 @@ struct {
 #endif
   { webrtc::MediaConstraintsInterface::kAutoGainControl,
     webrtc::MediaConstraintsInterface::kValueTrue },
+  { webrtc::MediaConstraintsInterface::kExperimentalAutoGainControl,
+    webrtc::MediaConstraintsInterface::kValueTrue },
   { webrtc::MediaConstraintsInterface::kNoiseSuppression,
     webrtc::MediaConstraintsInterface::kValueTrue },
   { webrtc::MediaConstraintsInterface::kHighpassFilter,
