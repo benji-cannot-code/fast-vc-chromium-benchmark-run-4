27a858680569938bd416e9868826c95779e4242a
tedchoc@chromium.org <tedchoc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-27 00:49:06 +0000

    Fix the location provider activity status checking.
    There were a couple holes where activity status changes could cause the location
    provider to be in an unexpected state.
    
    BUG=297832
    
    Review URL: https://codereview.chromium.org/24509008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225620 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/LocationProvider.java
+++ content/public/android/java/src/org/chromium/content/browser/LocationProvider.java
@@ -54,7 +54,7 @@ class LocationProvider {
         @Override
         public void onActivityStateChange(int state) {
             if (state == ActivityStatus.PAUSED) {
-                mShouldRunAfterActivityResume = mIsRunning;
+                mShouldRunAfterActivityResume |= mIsRunning;
                 unregisterFromLocationUpdates();
             } else if (state == ActivityStatus.RESUMED) {
                 assert !mIsRunning;
@@ -75,10 +75,7 @@ class LocationProvider {
             }
             mIsGpsEnabled = gpsEnabled;
 
-            int activityState = ActivityStatus.getState();
-            if (activityState == ActivityStatus.PAUSED
-                    || activityState == ActivityStatus.STOPPED
-                    || activityState == ActivityStatus.DESTROYED) {
+            if (ActivityStatus.getState() != ActivityStatus.RESUMED) {
                 mShouldRunAfterActivityResume = true;
             } else {
                 unregisterFromLocationUpdates();
