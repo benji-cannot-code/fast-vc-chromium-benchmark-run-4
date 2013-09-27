0b6e824d4e6069467b5c1370d918579bced0ee32
aelias@chromium.org <aelias@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-27 02:46:48 +0000

    Move tile-free-software render surface disabling to UpdateDrawProperties.
    Currently when DrawProperties decides a subtree needs a render surface,
    Android WebView software mode just drops it at CalculateRenderPasses
    time and draws nothing.  If we instead suppress the render surface
    decision at an earlier stage, we're more likely to get a halfway
    reasonable result.
    
    The motivating example was a page setting preserve-3d where the render
    surface didn't affect the output result anyway.
    
    New test LayerTreeHostCommonTest.CanRenderToSeparateSurface.
    
    NOTRY=true
    BUG=297299
    
    Review URL: https://codereview.chromium.org/24280009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225637 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/trees/layer_tree_host_common.h
+++ cc/trees/layer_tree_host_common.h
@@ -40,6 +40,7 @@ class CC_EXPORT LayerTreeHostCommon {
                         LayerType* page_scale_application_layer,
                         int max_texture_size,
                         bool can_use_lcd_text,
+                        bool can_render_to_separate_surface,
                         bool can_adjust_raster_scales,
                         RenderSurfaceLayerListType* render_surface_layer_list)
         : root_layer(root_layer),
@@ -50,6 +51,7 @@ class CC_EXPORT LayerTreeHostCommon {
           page_scale_application_layer(page_scale_application_layer),
           max_texture_size(max_texture_size),
           can_use_lcd_text(can_use_lcd_text),
+          can_render_to_separate_surface(can_render_to_separate_surface),
           can_adjust_raster_scales(can_adjust_raster_scales),
           render_surface_layer_list(render_surface_layer_list) {}
 
@@ -61,6 +63,7 @@ class CC_EXPORT LayerTreeHostCommon {
     LayerType* page_scale_application_layer;
     int max_texture_size;
     bool can_use_lcd_text;
+    bool can_render_to_separate_surface;
     bool can_adjust_raster_scales;
     RenderSurfaceLayerListType* render_surface_layer_list;
   };
@@ -221,6 +224,7 @@ LayerTreeHostCommon::CalcDrawPropsInputsForTesting<LayerType,
           NULL,
           std::numeric_limits<int>::max() / 2,
           false,
+          true,
           false,
           render_surface_layer_list) {
   DCHECK(root_layer);
@@ -243,6 +247,7 @@ LayerTreeHostCommon::CalcDrawPropsInputsForTesting<LayerType,
           NULL,
           std::numeric_limits<int>::max() / 2,
           false,
+          true,
           false,
           render_surface_layer_list) {
   DCHECK(root_layer);
