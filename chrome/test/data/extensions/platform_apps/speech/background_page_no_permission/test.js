df6481d123944485b43c991dab88b5be83b0cf36
lazyboy@chromium.org <lazyboy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-29 00:08:31 +0000

    Allow webkitSpeechRecognition work from an app's background page.
    The manifest would already decide whether or not to allow the request (via
        audioCapture permission).
    Note that I'm enabling it only for speech coming through JS API. Speech
    coming through <input> element are still discarded since the showing speech
    bubble is not trivial as is, especially from popups.
    
    BUG=291140
    Test=From a chrome v2 packaged app with audioCapture permission, speech
    api (webkitSpeechRecognition) should work from the app's background page.
    Added browser_test that makes sure onerror ("not-allowed") doesn't get
    called as it used to.
    
    Review URL: https://codereview.chromium.org/24292004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225866 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/platform_apps/speech/background_page_no_permission/test.js
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.test.runTests([
+    function testWebkitSpeechRecognition() {
+      var r = new webkitSpeechRecognition();
+      r.onerror = function(e) {  // Expect to fail.
+        chrome.test.assertEq('not-allowed', e.error);
+        chrome.test.succeed();
+      };
+      r.onstart = function() {
+        chrome.test.fail();
+      };
+      r.start();
+    }
+  ]);
+});
