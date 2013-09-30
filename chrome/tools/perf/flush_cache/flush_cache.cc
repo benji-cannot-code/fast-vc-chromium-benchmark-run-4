6f626c674f227c59bc29f46ef9a3278123d637f3
limasdf@gmail.com <limasdf@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-30 12:12:51 +0000

    Remove deprecated usage of FromWStringHack() from importer and flush_cache.
    BUG=76112
    TEST=compiles, no functional changes.
    R=thestig, isherman
    
    Review URL: https://codereview.chromium.org/25174002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225972 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/tools/perf/flush_cache/flush_cache.cc
+++ chrome/tools/perf/flush_cache/flush_cache.cc
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/process/memory.h"
-#include "base/strings/string_piece.h"
-#include "base/strings/sys_string_conversions.h"
 #include "base/test/test_file_util.h"
 
 int main(int argc, const char* argv[]) {
@@ -21,8 +19,7 @@ int main(int argc, const char* argv[]) {
   }
 
   for (int i = 1; i < argc; ++i) {
-    std::wstring filename = base::SysNativeMBToWide(argv[i]);
-    base::FilePath path = base::FilePath::FromWStringHack(filename);
+    base::FilePath path = base::FilePath::FromUTF8Unsafe(argv[i]);
     if (!file_util::EvictFileFromSystemCache(path)) {
       fprintf(stderr, "Failed to evict %s from cache -- is it a directory?\n",
               argv[i]);
