9476cc7057144eea695fc0a49bbd29e937b82474
tmdiep@chromium.org <tmdiep@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-30 02:27:38 +0000

    Ensure event pages are unloaded when onSuspend calls an API function
    Extensions and apps which call API functions with callbacks such as
    chrome.storage.local.set() and chrome.storage.local.get() in the
    onSuspend event handler prevents the event page from ever being
    unloaded.
    
    The API function finishes between ExtensionProcessManager::OnSuspendAck()
    and ExtensionProcessManager::CloseLazyBackgroundPageNow(). The destructor
    of UIThreadExtensionFunction results in DecrementLazyKeepaliveCount()
    being called in between these two functions, which increments
    close_sequence_id. This causes CloseLazyBackgroundPageNow() to drop the
    delayed close.
    
    API functions which finish before OnSuspendAck() will not cause this
    problem.
    
    Fix:
    DecrementLazyKeepAliveCount() should check the is_closing flag before
    incrementing close_sequence_id.
    
    BUG=296834
    TEST=browser_tests (AppEventPageTest*:LazyBackgroundPageApiTest*)
    See bug for how to reproduce and verify issue.
    
    Review URL: https://codereview.chromium.org/24712003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225906 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/platform_apps/event_page/suspend_simple/main.js
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.app.runtime.onLaunched.addListener(function() {
+
+  var screenWidth = screen.availWidth;
+  var screenHeight = screen.availHeight;
+  var width = 500;
+  var height = 300;
+
+  chrome.app.window.create('index.html', {
+    bounds: {
+      width: width,
+      height: height,
+      left: Math.round((screenWidth-width)/2),
+      top: Math.round((screenHeight-height)/2)
+    }
+  });
+});
+
+chrome.runtime.onSuspend.addListener(function() {
+  var now = new Date();
+  console.log("The current time is: " + now.toLocaleString());
+});
