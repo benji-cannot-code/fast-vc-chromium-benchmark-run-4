499f1a8abc0d2d7636dddfc387f6d57dafd18cea
fsamuel@chromium.org <fsamuel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-30 23:43:57 +0000

    <webview>: Update WebviewOverrideUserAgentFunction to inherit from common class
    WebviewExtensionFunction was recently added to move the WebViewGuest security
    check to a common base class.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/25073003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226082 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/webview/webview_api.cc
+++ chrome/browser/extensions/api/webview/webview_api.cc
@@ -232,10 +232,6 @@ WebviewReloadFunction::~WebviewReloadFunction() {
 
 bool WebviewReloadFunction::RunImplSafe(WebViewGuest* guest) {
   content::RecordAction(content::UserMetricsAction("WebView.Reload"));
-  scoped_ptr<webview::Reload::Params> params(
-      webview::Reload::Params::Create(*args_));
-  EXTENSION_FUNCTION_VALIDATE(params.get());
-
   guest->Reload();
   return true;
 }
@@ -264,19 +260,12 @@ WebviewOverrideUserAgentFunction::WebviewOverrideUserAgentFunction() {
 WebviewOverrideUserAgentFunction::~WebviewOverrideUserAgentFunction() {
 }
 
-bool WebviewOverrideUserAgentFunction::RunImpl() {
+bool WebviewOverrideUserAgentFunction::RunImplSafe(WebViewGuest* guest) {
   scoped_ptr<extensions::api::webview::OverrideUserAgent::Params> params(
       extensions::api::webview::OverrideUserAgent::Params::Create(*args_));
   EXTENSION_FUNCTION_VALIDATE(params.get());
-  int instance_id = params->instance_id;
-  std::string user_agent_override = params->user_agent_override;
 
-  WebViewGuest* guest = WebViewGuest::From(
-      render_view_host()->GetProcess()->GetID(), instance_id);
-  if (!guest)
-    return false;
-
-  guest->SetUserAgentOverride(user_agent_override);
+  guest->SetUserAgentOverride(params->user_agent_override);
   return true;
 }
 
@@ -288,10 +277,6 @@ WebviewStopFunction::~WebviewStopFunction() {
 
 bool WebviewStopFunction::RunImplSafe(WebViewGuest* guest) {
   content::RecordAction(content::UserMetricsAction("WebView.Stop"));
-  scoped_ptr<webview::Stop::Params> params(
-      webview::Stop::Params::Create(*args_));
-  EXTENSION_FUNCTION_VALIDATE(params.get());
-
   guest->Stop();
   return true;
 }
@@ -304,10 +289,6 @@ WebviewTerminateFunction::~WebviewTerminateFunction() {
 
 bool WebviewTerminateFunction::RunImplSafe(WebViewGuest* guest) {
   content::RecordAction(content::UserMetricsAction("WebView.Terminate"));
-  scoped_ptr<webview::Terminate::Params> params(
-      webview::Terminate::Params::Create(*args_));
-  EXTENSION_FUNCTION_VALIDATE(params.get());
-
   guest->Terminate();
   return true;
 }
--- chrome/browser/extensions/api/webview/webview_api.h
+++ chrome/browser/extensions/api/webview/webview_api.h
@@ -165,7 +165,7 @@ class WebviewSetPermissionFunction : public WebviewExtensionFunction {
   DISALLOW_COPY_AND_ASSIGN(WebviewSetPermissionFunction);
 };
 
-class WebviewOverrideUserAgentFunction: public AsyncExtensionFunction {
+class WebviewOverrideUserAgentFunction: public WebviewExtensionFunction {
  public:
   DECLARE_EXTENSION_FUNCTION("webview.overrideUserAgent",
                              WEBVIEW_OVERRIDEUSERAGENT);
@@ -175,10 +175,10 @@ class WebviewOverrideUserAgentFunction: public AsyncExtensionFunction {
  protected:
   virtual ~WebviewOverrideUserAgentFunction();
 
-  // ExtensionFunction implementation.
-  virtual bool RunImpl() OVERRIDE;
-
  private:
+  // WebviewExtensionFunction implementation.
+  virtual bool RunImplSafe(WebViewGuest* guest) OVERRIDE;
+
   DISALLOW_COPY_AND_ASSIGN(WebviewOverrideUserAgentFunction);
 };
 
