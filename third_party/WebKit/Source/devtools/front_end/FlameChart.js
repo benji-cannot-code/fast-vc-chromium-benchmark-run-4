7ecd9d5ba9432cd7d7a847159b2e35ead18169b5
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-30 14:49:09 +0000

    DevTools: REGRESSION: FlameChart doesn't highlight the bar under mouse pointer.
    Regressed at r157451.
    highlighted index is absolute.
    So we should compare entries by value.
    
    BUG=none
    R=yurys@chromium.org
    
    Review URL: https://codereview.chromium.org/25261002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158538 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/FlameChart.js
+++ third_party/WebKit/Source/devtools/front_end/FlameChart.js
@@ -662,6 +662,7 @@ WebInspector.FlameChart.prototype = {
         var visibleTimeLeft = this._timeWindowLeft - this._paddingLeftTime;
         var timeWindowRight = this._timeWindowRight;
         var lastUsedColor = "";
+        var highlightedEntry = this._timelineData.entries[this._highlightedEntryIndex];
 
         function forEachEntry(flameChart, callback)
         {
@@ -679,7 +680,7 @@ WebInspector.FlameChart.prototype = {
                         continue;
                     flameChart._entryToAnchorBox(entry, anchorBox);
 
-                    callback(flameChart, context, entry, anchorBox, flameChart._highlightedEntryIndex === i);
+                    callback(flameChart, context, entry, anchorBox, highlightedEntry === entry);
                 }
             }
         }
