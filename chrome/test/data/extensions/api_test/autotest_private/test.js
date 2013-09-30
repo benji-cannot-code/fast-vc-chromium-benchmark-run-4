ae26dfe5b495dbee71b81ff297e1a23af2a8fc21
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-30 22:31:28 +0000

    Add missing test for autotestPrivate.lockScreen
    BUG=233834
    TEST=browser test
    
    Review URL: https://codereview.chromium.org/25108011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226072 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/api_test/autotest_private/test.js
+++ chrome/test/data/extensions/api_test/autotest_private/test.js
@@ -18,6 +18,10 @@ chrome.test.runTests([
     chrome.autotestPrivate.shutdown(true);
     chrome.test.succeed();
   },
+  function lockScreen() {
+    chrome.autotestPrivate.lockScreen();
+    chrome.test.succeed();
+  },
   function simulateAsanMemoryBug() {
     chrome.autotestPrivate.simulateAsanMemoryBug();
     chrome.test.succeed();
