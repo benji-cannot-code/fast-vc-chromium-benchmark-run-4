decff0f99468dfad6a1337fc21361bf611c38c0c
acolwell@chromium.org <acolwell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-30 18:02:37 +0000

    Revert 225992 "Add unit tests for AppListServiceImpl."
    This change appears to be causing the FastShutdown test in unit_tests to fail
    on the Linux Clang (dbg) bot.
    
    > Add unit tests for AppListServiceImpl.
    > 
    > TBR=sail@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/24707002
    
    TBR=koz@chromium.org
    
    Review URL: https://codereview.chromium.org/25269004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226010 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/app_list/keep_alive_service.h
+++ chrome/browser/ui/views/app_list/keep_alive_service.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_APP_LIST_KEEP_ALIVE_SERVICE_H_
-#define CHROME_BROWSER_UI_APP_LIST_KEEP_ALIVE_SERVICE_H_
+#ifndef CHROME_BROWSER_UI_VIEWS_APP_LIST_KEEP_ALIVE_SERVICE_H_
+#define CHROME_BROWSER_UI_VIEWS_APP_LIST_KEEP_ALIVE_SERVICE_H_
 
 // An interface to manipulate browser keepalive. Subclasses should ensure that
 // keepalive is cleared when the object is destructed.
@@ -15,4 +15,4 @@ class KeepAliveService {
   virtual void FreeKeepAlive() = 0;
 };
 
-#endif  // CHROME_BROWSER_UI_APP_LIST_KEEP_ALIVE_SERVICE_H_
+#endif  // CHROME_BROWSER_UI_VIEWS_APP_LIST_KEEP_ALIVE_SERVICE_H_
--- chrome/browser/ui/views/app_list/keep_alive_service_impl.h
+++ chrome/browser/ui/views/app_list/keep_alive_service_impl.h
@@ -3,11 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_APP_LIST_KEEP_ALIVE_SERVICE_IMPL_H_
-#define CHROME_BROWSER_UI_APP_LIST_KEEP_ALIVE_SERVICE_IMPL_H_
+#ifndef CHROME_BROWSER_UI_VIEWS_APP_LIST_KEEP_ALIVE_SERVICE_IMPL_H_
+#define CHROME_BROWSER_UI_VIEWS_APP_LIST_KEEP_ALIVE_SERVICE_IMPL_H_
 
 #include "base/memory/scoped_ptr.h"
-#include "chrome/browser/ui/app_list/keep_alive_service.h"
+#include "chrome/browser/ui/views/app_list/keep_alive_service.h"
 
 class ScopedKeepAlive {
  public:
@@ -36,4 +36,4 @@ class KeepAliveServiceImpl : public KeepAliveService {
 
 
 
-#endif  // CHROME_BROWSER_UI_APP_LIST_KEEP_ALIVE_SERVICE_IMPL_H_
+#endif  // CHROME_BROWSER_UI_VIEWS_APP_LIST_KEEP_ALIVE_SERVICE_IMPL_H_
