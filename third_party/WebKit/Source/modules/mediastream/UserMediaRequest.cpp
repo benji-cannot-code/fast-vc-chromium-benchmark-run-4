b9ccaa50343b7fc7d86de2f959d205dcd9cf617f
tommyw@chromium.org <tommyw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-30 09:39:04 +0000

    MediaStream API: Updating NavigatorUserMediaError Updating NavigatorUserMediaError both to reflect the latest spec but also to make it a bit cleaner.
    This patch has been committed as r157697 and rolled out as r157752 due to
    failing browser_tests. The tests have now been updated to accept both the
    new and the old error name.
    
    BUG=294145
    
    Review URL: https://chromiumcodereview.appspot.com/24541002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158527 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/mediastream/UserMediaRequest.cpp
+++ third_party/WebKit/Source/modules/mediastream/UserMediaRequest.cpp
@@ -155,7 +155,7 @@ void UserMediaRequest::fail(const String& description)
         return;
 
     if (m_errorCallback) {
-        RefPtr<NavigatorUserMediaError> error = NavigatorUserMediaError::create("PERMISSION_DENIED", description, String());
+        RefPtr<NavigatorUserMediaError> error = NavigatorUserMediaError::create(NavigatorUserMediaError::NamePermissionDenied, description, String());
         m_errorCallback->handleEvent(error.get());
     }
 }
@@ -167,7 +167,7 @@ void UserMediaRequest::failConstraint(const String& constraintName, const String
         return;
 
     if (m_errorCallback) {
-        RefPtr<NavigatorUserMediaError> error = NavigatorUserMediaError::create("CONSTRAINT_NOT_SATISFIED", description, constraintName);
+        RefPtr<NavigatorUserMediaError> error = NavigatorUserMediaError::create(NavigatorUserMediaError::NameConstraintNotSatisfied, description, constraintName);
         m_errorCallback->handleEvent(error.get());
     }
 }
