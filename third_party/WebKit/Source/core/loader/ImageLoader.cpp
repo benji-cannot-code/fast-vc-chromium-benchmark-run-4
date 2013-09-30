d88c934a4db63c97c315c1c7413560b434bc11b6
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-30 07:15:58 +0000

    ImageLoader shouldn't call confusingAndOftenMisusedAttached
    What the ImageLoader really wants to know is whether it's appropriate to
    fire events on its associated image elements. That's safe when the document is
    active in a frame, which we can check more directly by asking whether the
    document has an associated frame.
    
    R=esprehn
    BUG=299011
    
    Review URL: https://chromiumcodereview.appspot.com/24972002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158517 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/ImageLoader.cpp
+++ third_party/WebKit/Source/core/loader/ImageLoader.cpp
@@ -378,7 +378,7 @@ void ImageLoader::dispatchPendingBeforeLoadEvent()
         return;
     if (!m_image)
         return;
-    if (!m_element->document().confusingAndOftenMisusedAttached())
+    if (!m_element->document().frame())
         return;
     m_hasPendingBeforeLoadEvent = false;
     if (m_element->dispatchBeforeLoadEvent(m_image->url().string())) {
@@ -408,7 +408,7 @@ void ImageLoader::dispatchPendingLoadEvent()
     if (!m_image)
         return;
     m_hasPendingLoadEvent = false;
-    if (element()->document().confusingAndOftenMisusedAttached())
+    if (element()->document().frame())
         dispatchLoadEvent();
 
     // Only consider updating the protection ref-count of the Element immediately before returning
@@ -421,7 +421,7 @@ void ImageLoader::dispatchPendingErrorEvent()
     if (!m_hasPendingErrorEvent)
         return;
     m_hasPendingErrorEvent = false;
-    if (element()->document().confusingAndOftenMisusedAttached())
+    if (element()->document().frame())
         element()->dispatchEvent(Event::create(eventNames().errorEvent));
 
     // Only consider updating the protection ref-count of the Element immediately before returning
