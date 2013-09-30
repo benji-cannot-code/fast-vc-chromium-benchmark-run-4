e782b26740a6a59da4e0b5bebcac69c858359cb3
ajwong@chromium.org <ajwong@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-30 22:20:59 +0000

    Remove unnecessary static variable that entangles TEST_F state.
    BUG=none
    
    Review URL: https://codereview.chromium.org/24642004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226068 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/test/mock_render_process_host.cc
+++ content/public/test/mock_render_process_host.cc
@@ -27,6 +27,7 @@ MockRenderProcessHost::MockRenderProcessHost(
           factory_(NULL),
           id_(ChildProcessHostImpl::GenerateChildProcessUniqueId()),
           browser_context_(browser_context),
+          prev_routing_id_(0),
           fast_shutdown_started_(false) {
   // Child process security operations can't be unit tested unless we add
   // ourselves as an existing child process.
@@ -52,8 +53,7 @@ bool MockRenderProcessHost::Init() {
 }
 
 int MockRenderProcessHost::GetNextRoutingID() {
-  static int prev_routing_id = 0;
-  return ++prev_routing_id;
+  return ++prev_routing_id_;
 }
 
 void MockRenderProcessHost::AddRoute(
