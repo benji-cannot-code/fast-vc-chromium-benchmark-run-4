de855ae6ddaeeeeb6778783f6e9ab73893de9876
sadrul@chromium.org <sadrul@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-30 03:37:02 +0000

    webkit-app-region: Make sure the bounds is properly transformed.
    The draggable-regions reported by a Document can be incorrect if the element that
    has -webkit-app-region set also has transforms (e.g. rotate, scale). Make sure the
    entire bounds of the element is transformed instead of just the origin to fix this.
    
    BUG=none
    R=hartmanng@chromium.org, vollick@chromium.org
    
    Review URL: https://codereview.chromium.org/25086003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158510 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/testing/Internals.h
+++ third_party/WebKit/Source/core/testing/Internals.h
@@ -277,6 +277,9 @@ public:
     void startTrackingRepaints(Document*, ExceptionState&);
     void stopTrackingRepaints(Document*, ExceptionState&);
 
+    PassRefPtr<ClientRectList> draggableRegions(Document*, ExceptionState&);
+    PassRefPtr<ClientRectList> nonDraggableRegions(Document*, ExceptionState&);
+
     PassRefPtr<ArrayBuffer> serializeObject(PassRefPtr<SerializedScriptValue>) const;
     PassRefPtr<SerializedScriptValue> deserializeBuffer(PassRefPtr<ArrayBuffer>) const;
 
@@ -302,6 +305,7 @@ private:
     Document* contextDocument() const;
     Frame* frame() const;
     Vector<String> iconURLs(Document*, int iconTypesMask) const;
+    PassRefPtr<ClientRectList> annotatedRegions(Document*, bool draggable, ExceptionState&);
 
     DocumentMarker* markerAt(Node*, const String& markerType, unsigned index, ExceptionState&);
     RefPtr<DOMWindow> m_frontendWindow;
