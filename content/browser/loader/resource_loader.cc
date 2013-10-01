5e1ec3ff63444cec8ebc2449c50d56fa3834f6e5
creis@chromium.org <creis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-01 20:52:26 +0000

    Revert 226284 "Move TransferNavigationResourceThrottle into Cros..."
    Memory errors in TransferNavigation unit tests.
    
    > Move TransferNavigationResourceThrottle into CrossSiteResourceHandler.
    > 
    > We now transfer requests to a new process when they are ready to commit, rather than each time a redirect occurs.  This simplifies the ResourceDispatcherHost logic, and it prepares for a future CL to intercept all navigations in the browser process.
    > 
    > BUG=238331
    > TEST=No more than one process swap for repeated redirects.
    > 
    > Review URL: https://codereview.chromium.org/15476003
    
    TBR=creis@chromium.org
    
    Review URL: https://codereview.chromium.org/25372005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226293 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/loader/resource_loader.cc
+++ content/browser/loader/resource_loader.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram.h"
 #include "base/time/time.h"
 #include "content/browser/child_process_security_policy_impl.h"
-#include "content/browser/loader/cross_site_resource_handler.h"
 #include "content/browser/loader/resource_loader_delegate.h"
 #include "content/browser/loader/resource_request_info_impl.h"
 #include "content/browser/ssl/ssl_client_auth_handler.h"
@@ -167,10 +166,10 @@ void ResourceLoader::MarkAsTransferring(const GURL& target_url) {
 }
 
 void ResourceLoader::CompleteTransfer() {
-  DCHECK_EQ(DEFERRED_READ, deferred_stage_);
+  DCHECK_EQ(DEFERRED_REDIRECT, deferred_stage_);
 
   is_transferring_ = false;
-  GetRequestInfo()->cross_site_handler()->ResumeResponse();
+  Resume();
 }
 
 ResourceRequestInfoImpl* ResourceLoader::GetRequestInfo() {
--- content/content_browser.gypi
+++ content/content_browser.gypi
@@ -691,6 +691,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     'browser/loader/sync_resource_handler.h',
     'browser/loader/throttling_resource_handler.cc',
     'browser/loader/throttling_resource_handler.h',
+    'browser/loader/transfer_navigation_resource_throttle.cc',
+    'browser/loader/transfer_navigation_resource_throttle.h',
     'browser/loader/upload_data_stream_builder.cc',
     'browser/loader/upload_data_stream_builder.h',
     'browser/mach_broker_mac.h',
