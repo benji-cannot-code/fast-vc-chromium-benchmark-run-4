5129af61190695fd68dd831c87acd4a4552243d7
alecflett@chromium.org <alecflett@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-01 04:07:08 +0000

    Wrap ScriptPromiseResolver resolution with DOMRequestState
    When resolving a Promise from a non-script context, you need to reuse the
    V8 context that was used when the resolver was created.
    BUG=
    
    Review URL: https://codereview.chromium.org/25015004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158586 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/serviceworkers/CallbackPromiseAdapter.h
+++ third_party/WebKit/Source/modules/serviceworkers/CallbackPromiseAdapter.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CallbackPromiseAdapter_h
 #define CallbackPromiseAdapter_h
 
+#include "bindings/v8/DOMRequestState.h"
 #include "public/platform/WebCallbacks.h"
 
 namespace WebCore {
@@ -39,8 +40,9 @@ namespace WebCore {
 // FIXME: this class can be generalized
 class CallbackPromiseAdapter : public WebKit::WebCallbacks<WebKit::WebServiceWorker, WebKit::WebServiceWorker> {
 public:
-    explicit CallbackPromiseAdapter(PassRefPtr<ScriptPromiseResolver> resolver)
+    explicit CallbackPromiseAdapter(PassRefPtr<ScriptPromiseResolver> resolver, ScriptExecutionContext* context)
         : m_resolver(resolver)
+        , m_requestState(context)
     {
     }
     virtual ~CallbackPromiseAdapter() { }
@@ -48,15 +50,18 @@ public:
     virtual void onSuccess(WebKit::WebServiceWorker* worker) OVERRIDE
     {
         // FIXME: When the same worker is "registered" twice, we should return the same object.
+        DOMRequestState::Scope scope(m_requestState);
         m_resolver->resolve(ServiceWorker::create(adoptPtr(worker)));
     }
     void onError(WebKit::WebServiceWorker* worker) OVERRIDE
     {
         // FIXME: need to propagate some kind of reason for failure.
+        DOMRequestState::Scope scope(m_requestState);
         m_resolver->reject(ServiceWorker::create(adoptPtr(worker)));
     }
 private:
     RefPtr<ScriptPromiseResolver> m_resolver;
+    DOMRequestState m_requestState;
 };
 
 } // namespace WebCore
