34a534d91af2bf47a3112ae6155d5a72f87b0746
beaufort.francois@gmail.com <beaufort.francois@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-01 12:48:19 +0000

    Expose apps Chrome URL hosts.
    chrome://apps will be displayed in chrome://chrome-urls and be suggested in
    omnibox autocomplete.
    
    BUG=301504
    R=pkasting@chromium.org
    
    Review URL: https://codereview.chromium.org/25228002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226210 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/url_constants.cc
+++ chrome/common/url_constants.cc
@@ -519,6 +519,7 @@ const char kNotificationsHelpURL[] =
 // Add hosts here to be included in chrome://chrome-urls (about:about).
 // These hosts will also be suggested by BuiltinProvider.
 const char* const kChromeHostURLs[] = {
+  kChromeUIAppLauncherPageHost,
   kChromeUICacheHost,
   kChromeUIChromeURLsHost,
   kChromeUICrashesHost,
