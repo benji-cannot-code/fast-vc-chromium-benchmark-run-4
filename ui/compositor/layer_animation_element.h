1b4711ceed42beb19e15e91925cfe232bf2a6ced
varkha@chromium.org <varkha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-01 02:30:20 +0000

    Protect reentrant animations when IMMEDIATELY_ANIMATE_TO_NEW_TARGET is used.
    BUG=297974
    TEST=TBD
    
    Review URL: https://codereview.chromium.org/25107002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226111 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/compositor/layer_animation_element.h
+++ ui/compositor/layer_animation_element.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <set>
 
+#include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "cc/animation/animation.h"
 #include "cc/animation/animation_events.h"
@@ -231,6 +232,8 @@ class COMPOSITOR_EXPORT LayerAnimationElement {
   int animation_group_id_;
 
   double last_progressed_fraction_;
+
+  base::WeakPtrFactory<LayerAnimationElement> weak_ptr_factory_;
 
   DISALLOW_ASSIGN(LayerAnimationElement);
 };
