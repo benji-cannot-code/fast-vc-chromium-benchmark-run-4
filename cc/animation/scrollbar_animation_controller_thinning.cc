53169623b586d2e0bdd351acaaa1e5f6a25974ca
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-01 21:38:24 +0000

    Grow scrollbars while the mouse is over the track or thumb.
    For overlay scrollbars, when the mouse is over the scrollbar,
    prevent the animation from making the scrollbar smaller.
    
    BUG=274010
    R=enne@chromium.org
    
    Review URL: https://codereview.chromium.org/24782002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226308 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/animation/scrollbar_animation_controller_thinning.cc
+++ cc/animation/scrollbar_animation_controller_thinning.cc
@@ -35,6 +35,7 @@ ScrollbarAnimationControllerThinning::ScrollbarAnimationControllerThinning(
     : ScrollbarAnimationController(),
       scroll_layer_(scroll_layer),
       scroll_gesture_in_progress_(false),
+      mouse_is_over_scrollbar_(false),
       animation_delay_(animation_delay),
       animation_duration_(animation_duration),
       mouse_move_distance_to_trigger_animation_(100.f) {}
@@ -54,6 +55,10 @@ base::TimeDelta ScrollbarAnimationControllerThinning::DelayBeforeStart(
 }
 
 bool ScrollbarAnimationControllerThinning::Animate(base::TimeTicks now) {
+  if (mouse_is_over_scrollbar_) {
+    ApplyOpacityAndThumbThicknessScale(1, 1);
+    return false;
+  }
   float progress = AnimationProgressAtTime(now);
   float opacity = OpacityAtAnimationProgress(progress);
   float thumb_thickness_scale = ThumbThicknessScaleAtAnimationProgress(
@@ -76,6 +81,12 @@ void ScrollbarAnimationControllerThinning::DidScrollGestureEnd(
   scroll_gesture_in_progress_ = false;
 }
 
+void ScrollbarAnimationControllerThinning::DidMouseMoveOffScrollbar(
+    base::TimeTicks now) {
+  mouse_is_over_scrollbar_ = false;
+  DidScrollUpdate(now);
+}
+
 bool ScrollbarAnimationControllerThinning::DidScrollUpdate(
     base::TimeTicks now) {
   ApplyOpacityAndThumbThicknessScale(1, 1);
@@ -86,6 +97,11 @@ bool ScrollbarAnimationControllerThinning::DidScrollUpdate(
 
 bool ScrollbarAnimationControllerThinning::DidMouseMoveNear(
     base::TimeTicks now, float distance) {
+  if (distance == 0.0) {
+    mouse_is_over_scrollbar_ = true;
+    return false;
+  }
+
   if (distance < mouse_move_distance_to_trigger_animation_)
     return DidScrollUpdate(now);
 
