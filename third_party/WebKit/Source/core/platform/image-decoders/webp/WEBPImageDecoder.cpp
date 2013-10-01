5213e477c135f5084491535fed706cef7eb9aaef
urvang@google.com <urvang@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-01 08:06:38 +0000

    A crash fix that was happening in Debug build
    This was happening for a specific invalid (non-animated) image in the following scenario:
    - frameCount() is called with enough partial data to detect the error. So frameCount() returns 0.
    - Next frameBufferAtIndex() is called with the same data. This in turn calls frameCount() --> updateDemuxer(). But as m_haveAlreadyParsedThisData is true, updateDemuxer() returns true; and hence frameCount() returns 1. [Ideally, updateDemuxer() should have returned false].
    
    Added a unit test for the same.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/25345003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158606 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/image-decoders/webp/WEBPImageDecoder.cpp
+++ third_party/WebKit/Source/core/platform/image-decoders/webp/WEBPImageDecoder.cpp
@@ -191,6 +191,9 @@ float WEBPImageDecoder::frameDurationAtIndex(size_t index) const
 
 bool WEBPImageDecoder::updateDemuxer()
 {
+    if (failed())
+        return false;
+
     if (m_haveAlreadyParsedThisData)
         return true;
 
