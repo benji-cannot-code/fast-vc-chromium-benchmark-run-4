7b2a4a16cf8d4ba255d874331ea6cda73f9b2bca
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-01 15:07:37 +0000

    Remove unused functions in LocalizedStrings.
    There are no callsites of the following functions. We remove them.
     AXWebAreaText()
     AXLinkText()
     AXListMarkerText()
     AXImageMapText()
     AXHeadingText()
     AXFileUploadButtonText()
    
    BUG=298020
    TEST=none; no behavior changes.
    
    Review URL: https://codereview.chromium.org/25462002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158633 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebLocalizedString.h
+++ third_party/WebKit/public/platform/WebLocalizedString.h
@@ -41,12 +41,12 @@ struct WebLocalizedString {
         AXCheckedCheckBoxActionVerb,
         AXDateTimeFieldEmptyValueText,
         AXDayOfMonthFieldText,
-        AXHeadingText,
+        AXHeadingText, // Deprecated.
         AXHourFieldText,
-        AXImageMapText,
+        AXImageMapText, // Deprecated.
         AXLinkActionVerb,
-        AXLinkText,
-        AXListMarkerText,
+        AXLinkText, // Deprecated.
+        AXListMarkerText, // Deprecated.
         AXMediaAudioElement,
         AXMediaAudioElementHelp,
         AXMediaCurrentTimeDisplay,
@@ -93,7 +93,7 @@ struct WebLocalizedString {
         AXSecondFieldText,
         AXTextFieldActionVerb,
         AXUncheckedCheckBoxActionVerb,
-        AXWebAreaText,
+        AXWebAreaText, // Deprecated.
         AXWeekOfYearFieldText,
         AXYearFieldText,
         CalendarClear,
