a89fd34ad149b2a2ee0e2d152f1e78e7d21760ce
beaufort.francois@gmail.com <beaufort.francois@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-01 13:31:58 +0000

    [DocServer] Made the "Content Settings" Sample appear in chrome.contentSettings page
    BUG=299634
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/25014003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226221 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/examples/api/contentSettings/popup.js
+++ chrome/common/extensions/docs/examples/api/contentSettings/popup.js
@@ -11,6 +11,9 @@ function settingChanged() {
   var setting = this.value;
   var pattern = /^file:/.test(url) ? url : url.replace(/\/[^\/]*?$/, '/*');
   console.log(type+' setting for '+pattern+': '+setting);
+  // HACK: [type] is not recognised by the docserver's sample crawler, so
+  // mention an explicit
+  // type: chrome.contentSettings.cookies.set - See http://crbug.com/299634
   chrome.contentSettings[type].set({
         'primaryPattern': pattern,
         'setting': setting,
@@ -26,6 +29,9 @@ document.addEventListener('DOMContentLoaded', function () {
     var types = ['cookies', 'images', 'javascript', 'plugins', 'popups',
                  'notifications'];
     types.forEach(function(type) {
+      // HACK: [type] is not recognised by the docserver's sample crawler, so
+      // mention an explicit
+      // type: chrome.contentSettings.cookies.get - See http://crbug.com/299634
       chrome.contentSettings[type].get({
             'primaryUrl': url,
             'incognito': incognito
