862929a4c9a2b528ff94bb5904d9382bbffcc8ea
limasdf@gmail.com <limasdf@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-01 09:21:55 +0000

    Cleanup: Use std::string& instead of const char* in extension/api
    TEST=compile
    R=yoz
    BUG=
    
    Review URL: https://codereview.chromium.org/25308004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226193 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/bookmarks/bookmarks_api.cc
+++ chrome/browser/extensions/api/bookmarks/bookmarks_api.cc
@@ -156,7 +156,7 @@ BookmarkEventRouter::~BookmarkEventRouter() {
 }
 
 void BookmarkEventRouter::DispatchEvent(
-    const char* event_name,
+    const std::string& event_name,
     scoped_ptr<base::ListValue> event_args) {
   if (extensions::ExtensionSystem::Get(profile_)->event_router()) {
     extensions::ExtensionSystem::Get(profile_)->event_router()->BroadcastEvent(
