b5c1ebcba955dea13681f4bf0a9ea0f9a00ed304
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-01 01:57:44 +0000

    Remove sync resize event from FrameView::setFrameRect.
    This fixes a hang with resize events and seamless iframes that's
    showing up on codereview.chromium.org with the rietveld usability
    extension installed.
    
    r156320 added this in a patch that was supposed to just be a refactor.
    I'm not convinced we want this code. WebKit added it in
    http://trac.webkit.org/changeset/149287 and
    http://trac.webkit.org/changeset/150011 and updated it to work for
    frame flattening (but not seamless!) in
    http://trac.webkit.org/changeset/153640.
    
    I don't really understand the changes to fast/forms, but they're just
    undoing unexplained changes from r156320.
    
    BUG=282603
    
    Review URL: https://codereview.chromium.org/25092012
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158578 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/FrameView.cpp
+++ third_party/WebKit/Source/core/page/FrameView.cpp
@@ -430,8 +430,6 @@ void FrameView::setFrameRect(const IntRect& newRect)
         if (renderView->usesCompositing())
             renderView->compositor()->frameViewDidChangeSize();
     }
-
-    sendResizeEventIfNeeded();
 }
 
 bool FrameView::scheduleAnimation()
