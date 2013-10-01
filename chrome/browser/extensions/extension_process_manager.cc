2106f72c18749e478c01580ed1e74bc1f5b0871e
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-01 00:43:07 +0000

    Don't add pending tasks to the lazy background task queue if there is no host.
    BUG=297886
    R=mpcomplete@chromium.org
    
    Review URL: https://codereview.chromium.org/24476002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226093 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_process_manager.cc
+++ chrome/browser/extensions/extension_process_manager.cc
@@ -87,8 +87,8 @@ class IncognitoExtensionProcessManager : public ExtensionProcessManager {
       const GURL& url,
       Browser* browser,
       extensions::ViewType view_type) OVERRIDE;
-  virtual void CreateBackgroundHost(const Extension* extension,
-                                    const GURL& url) OVERRIDE;
+  virtual ExtensionHost* CreateBackgroundHost(const Extension* extension,
+                                              const GURL& url) OVERRIDE;
   virtual SiteInstance* GetSiteInstanceForURL(const GURL& url) OVERRIDE;
 
  private:
@@ -293,16 +293,16 @@ ExtensionHost* ExtensionProcessManager::CreateInfobarHost(
   return CreateViewHost(url, browser, extensions::VIEW_TYPE_EXTENSION_INFOBAR);
 }
 
-void ExtensionProcessManager::CreateBackgroundHost(
+ExtensionHost* ExtensionProcessManager::CreateBackgroundHost(
     const Extension* extension, const GURL& url) {
   // Hosted apps are taken care of from BackgroundContentsService. Ignore them
   // here.
   if (extension->is_hosted_app())
-    return;
+    return NULL;
 
   // Don't create multiple background hosts for an extension.
-  if (GetBackgroundHostForExtension(extension->id()))
-    return;
+  if (ExtensionHost* host = GetBackgroundHostForExtension(extension->id()))
+    return host;  // TODO(kalman): return NULL here? It might break things...
 
   ExtensionHost* host =
 #if defined(OS_MACOSX)
@@ -316,6 +316,7 @@ void ExtensionProcessManager::CreateBackgroundHost(
 
   host->CreateRenderViewSoon();
   OnExtensionHostCreated(host, true);
+  return host;
 }
 
 ExtensionHost* ExtensionProcessManager::GetBackgroundHostForExtension(
@@ -888,15 +889,16 @@ ExtensionHost* IncognitoExtensionProcessManager::CreateViewHost(
   }
 }
 
-void IncognitoExtensionProcessManager::CreateBackgroundHost(
+ExtensionHost* IncognitoExtensionProcessManager::CreateBackgroundHost(
     const Extension* extension, const GURL& url) {
   if (extensions::IncognitoInfo::IsSplitMode(extension)) {
     if (IsIncognitoEnabled(extension))
-      ExtensionProcessManager::CreateBackgroundHost(extension, url);
+      return ExtensionProcessManager::CreateBackgroundHost(extension, url);
   } else {
     // Do nothing. If an extension is spanning, then its original-profile
     // background page is shared with incognito, so we don't create another.
   }
+  return NULL;
 }
 
 SiteInstance* IncognitoExtensionProcessManager::GetSiteInstanceForURL(
