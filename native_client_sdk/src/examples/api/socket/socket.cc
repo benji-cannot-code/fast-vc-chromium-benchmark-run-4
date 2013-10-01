0eccd80e911b05d78ec163a30a022a5430955985
sbc@chromium.org <sbc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-01 15:57:59 +0000

    [NaCl SDK] Add example code for TCP listening socket.
    This change adds a simple echo server to the socket
    example, that is implemented using a TCP listening socket.
    
    R=binji@chromium.org, noelallen@google.com
    
    Review URL: https://codereview.chromium.org/24165009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226240 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/examples/api/socket/socket.cc
+++ native_client_sdk/src/examples/api/socket/socket.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string.h>
 #include <sstream>
 
+#include "echo_server.h"
+
 #include "ppapi/cpp/host_resolver.h"
 #include "ppapi/cpp/instance.h"
 #include "ppapi/cpp/module.h"
@@ -21,16 +23,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma warning(disable : 4355)
 #endif
 
-static const int s_buffer_size = 1024;
-
 class ExampleInstance : public pp::Instance {
  public:
   explicit ExampleInstance(PP_Instance instance)
     : pp::Instance(instance),
       callback_factory_(this),
-      send_outstanding_(false) {}
+      send_outstanding_(false),
+      echo_server_(NULL) {}
 
-  virtual ~ExampleInstance() {}
+  virtual ~ExampleInstance() {
+    delete echo_server_;
+  }
+
   virtual void HandleMessage(const pp::Var& var_message);
 
  private:
@@ -53,14 +57,17 @@ class ExampleInstance : public pp::Instance {
   pp::UDPSocket udp_socket_;
   pp::HostResolver resolver_;
   pp::NetAddress remote_host_;
-  char receive_buffer_[s_buffer_size];
+
+  char receive_buffer_[kBufferSize];
   bool send_outstanding_;
+  EchoServer* echo_server_;
 };
 
 #define MSG_CREATE_TCP 't'
 #define MSG_CREATE_UDP 'u'
 #define MSG_SEND 's'
 #define MSG_CLOSE 'c'
+#define MSG_LISTEN 'l'
 
 void ExampleInstance::HandleMessage(const pp::Var& var_message) {
   if (!var_message.is_string())
@@ -86,10 +93,22 @@ void ExampleInstance::HandleMessage(const pp::Var& var_message) {
       // The command 'c' requests to close without any argument like "c;"
       Close();
       break;
+    case MSG_LISTEN:
+      {
+        // The command 'l' starts a listening socket (server).
+        int port = atoi(message.substr(2).c_str());
+        echo_server_ = new EchoServer(this, port);
+        break;
+      }
     case MSG_SEND:
       // The command 't' requests to send a message as a text frame. The
       // message passed as an argument like "t;message".
       Send(message.substr(2));
+      break;
+    default:
+      std::ostringstream status;
+      status << "Unhandled message from JavaScript: " << message;
+      PostMessage(status.str());
       break;
   }
 }
@@ -156,7 +175,7 @@ void ExampleInstance::Connect(const std::string& host, bool tcp) {
     port = atoi(host.substr(pos+1).c_str());
   }
 
-  pp::CompletionCallback callback = \
+  pp::CompletionCallback callback =
       callback_factory_.NewCallback(&ExampleInstance::OnResolveCompletion);
   PP_HostResolver_Hint hint = { PP_NETADDRESS_FAMILY_UNSPECIFIED, 0 };
   resolver_.Resolve(hostname.c_str(), port, hint, callback);
@@ -173,7 +192,7 @@ void ExampleInstance::OnResolveCompletion(int32_t result) {
   PostMessage(std::string("Resolved: ") +
               addr.DescribeAsString(true).AsString());
 
-  pp::CompletionCallback callback = \
+  pp::CompletionCallback callback =
       callback_factory_.NewCallback(&ExampleInstance::OnConnectCompletion);
 
   if (IsUDP()) {
@@ -217,7 +236,7 @@ void ExampleInstance::Send(const std::string& message) {
 
   uint32_t size = message.size();
   const char* data = message.c_str();
-  pp::CompletionCallback callback = \
+  pp::CompletionCallback callback =
       callback_factory_.NewCallback(&ExampleInstance::OnSendCompletion);
   int32_t result;
   if (IsUDP())
@@ -241,16 +260,16 @@ void ExampleInstance::Send(const std::string& message) {
 }
 
 void ExampleInstance::Receive() {
-  memset(receive_buffer_, 0, s_buffer_size);
+  memset(receive_buffer_, 0, kBufferSize);
   if (IsUDP()) {
-    pp::CompletionCallbackWithOutput<pp::NetAddress> callback = \
+    pp::CompletionCallbackWithOutput<pp::NetAddress> callback =
         callback_factory_.NewCallbackWithOutput(
             &ExampleInstance::OnReceiveFromCompletion);
-    udp_socket_.RecvFrom(receive_buffer_, s_buffer_size-1, callback);
+    udp_socket_.RecvFrom(receive_buffer_, kBufferSize, callback);
   } else {
-    pp::CompletionCallback callback = \
+    pp::CompletionCallback callback =
         callback_factory_.NewCallback(&ExampleInstance::OnReceiveCompletion);
-    tcp_socket_.Read(receive_buffer_, s_buffer_size-1, callback);
+    tcp_socket_.Read(receive_buffer_, kBufferSize, callback);
   }
 }
 
@@ -286,7 +305,7 @@ void ExampleInstance::OnReceiveCompletion(int32_t result) {
     return;
   }
 
-  PostMessage(std::string("Received: ") + receive_buffer_);
+  PostMessage(std::string("Received: ") + std::string(receive_buffer_, result));
   Receive();
 }
 
