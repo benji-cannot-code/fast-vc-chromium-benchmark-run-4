e9b57e9227be66b4980d9acd954f1cafbc98a827
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-01 05:09:24 +0000

    ui/native_theme: Get rid of unnecessary views include.
    It seems menu_config.h is not actually used by common_theme.cc. Remove it.
    
    BUG=None
    TEST=checkdeps.py passes, native_theme links.
    R=ben@chromium.org
    TBR=ben
    
    Review URL: https://codereview.chromium.org/25021004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226149 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/native_theme/common_theme.cc
+++ ui/native_theme/common_theme.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/rect.h"
 #include "ui/gfx/skia_util.h"
 #include "ui/gfx/sys_color_change_listener.h"
-#include "ui/views/controls/menu/menu_config.h"
 
 namespace {
 
