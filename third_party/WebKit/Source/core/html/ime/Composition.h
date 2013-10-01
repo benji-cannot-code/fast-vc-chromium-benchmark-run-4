5deb01b0941e4a43f7d01ec0aed909c68c87fc44
kochi@chromium.org <kochi@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-01 15:56:00 +0000

    Implements Composition interface for IME API
    Spec: http://www.w3.org/TR/ime-api/#composition-interface
    This interface represents an ongoing IME composition. It provides attributes
    representing the text being composed by an IME and its associated information.
    
    The interface IDL is already committed at r155072.
    
    The original CL was submitted as r157593 but reverted at
    r157811, as it had a reference counting issue that may
    lead to a crash (use-after-free).
    
    This fixes the issue as suggested in the code review
    https://codereview.chromium.org/23872024/
    to use its own ref counting rather than using RefCounted<T>.
    
    BUG=238585
    TEST=layouttest fast/ime
    
    Review URL: https://codereview.chromium.org/25208002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158635 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/ime/Composition.h
+++ third_party/WebKit/Source/core/html/ime/Composition.h
@@ -33,36 +33,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define Composition_h
 
 #include "bindings/v8/ScriptWrappable.h"
-#include "wtf/PassRefPtr.h"
-#include "wtf/RefCounted.h"
-#include "wtf/RefPtr.h"
+#include "wtf/PassOwnPtr.h"
 #include "wtf/text/WTFString.h"
 
 namespace WebCore {
 
-class Composition : public RefCounted<Composition>, public ScriptWrappable {
+class InputMethodContext;
+
+class Composition : public ScriptWrappable {
 public:
-    static PassRefPtr<Composition> create();
+    static PassOwnPtr<Composition> create(InputMethodContext*);
     ~Composition();
 
-    String text() const { return m_text; }
-    void setText(const String& text) { m_text = text; }
+    void ref();
+    void deref();
 
-    int selectionStart() const { return m_selectionStart; }
-    void setSelectionStart(int selectionStart) { selectionStart = m_selectionStart; }
-
-    int selectionEnd() const { return m_selectionEnd; }
-    void setSelectionEnd(int selectionEnd) { selectionEnd = m_selectionEnd; }
-
+    String text() const;
+    int selectionStart() const;
+    int selectionEnd() const;
     const Vector<unsigned>& getSegments() const;
 
 private:
-    Composition();
+    explicit Composition(InputMethodContext*);
 
-    String m_text;
-    int m_selectionStart;
-    int m_selectionEnd;
-    Vector<unsigned> m_segments;
+    InputMethodContext* m_inputMethodContext;
 };
 
 } // namespace WebCore
--- third_party/WebKit/Source/core/html/ime/InputMethodContext.h
+++ third_party/WebKit/Source/core/html/ime/InputMethodContext.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define InputMethodContext_h
 
 #include "bindings/v8/ScriptWrappable.h"
+#include "core/editing/CompositionUnderline.h"
 #include "core/html/HTMLElement.h"
 #include "wtf/PassOwnPtr.h"
 #include "wtf/RefPtr.h"
@@ -41,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 class Composition;
+class InputMethodController;
 class Node;
 
 class InputMethodContext : public ScriptWrappable {
@@ -51,17 +53,27 @@ public:
     void ref() { m_element->ref(); }
     void deref() { m_element->deref(); }
 
-    Composition* composition() const;
+    Composition* composition();
     String locale() const;
     HTMLElement* target() const;
     void confirmComposition();
     void setCaretRectangle(Node* anchor, int x, int y, int w, int h);
     void setExclusionRectangle(Node* anchor, int x, int y, int w, int h);
 
+    String compositionText() const;
+    int selectionStart() const;
+    int selectionEnd() const;
+    const Vector<unsigned>& segments();
+
 private:
     InputMethodContext(HTMLElement*);
-    RefPtr<Composition> m_composition;
+    bool hasFocus() const;
+    CompositionUnderline selectedSegment() const;
+    InputMethodController& inputMethodController() const;
+
     HTMLElement* m_element;
+    OwnPtr<Composition> m_composition;
+    Vector<unsigned> m_segments;
 };
 
 } // namespace WebCore
