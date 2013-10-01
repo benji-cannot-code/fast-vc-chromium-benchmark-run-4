21c177eccc7715ba3848463755d368530aca7687
skuhne@chromium.org <skuhne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-01 02:58:02 +0000

    Making app_tab_helper_ multi profile aware
    This pitch is ridding the first of the single profile dependencies from the launcher.
    
    BUG=299165
    TEST=unittest
    
    Review URL: https://codereview.chromium.org/24998012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226115 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/ash/launcher/launcher_app_tab_helper.h
+++ chrome/browser/ui/ash/launcher/launcher_app_tab_helper.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_ASH_LAUNCHER_LAUNCHER_APP_TAB_HELPER_H_
 #define CHROME_BROWSER_UI_ASH_LAUNCHER_LAUNCHER_APP_TAB_HELPER_H_
 
-#include <map>
 #include <string>
 
 #include "chrome/browser/ui/ash/launcher/chrome_launcher_controller.h"
@@ -22,7 +21,8 @@ class LauncherAppTabHelper : public ChromeLauncherController::AppTabHelper {
 
   // AppTabHelper:
   virtual std::string GetAppID(content::WebContents* tab) OVERRIDE;
-  virtual bool IsValidID(const std::string& id) OVERRIDE;
+  virtual bool IsValidIDForCurrentUser(const std::string& id) OVERRIDE;
+  virtual void SetCurrentUser(Profile* profile) OVERRIDE;
 
  private:
   Profile* profile_;
