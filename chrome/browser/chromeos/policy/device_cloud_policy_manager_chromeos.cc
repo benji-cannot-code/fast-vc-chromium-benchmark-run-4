abcffd166cdf4f219f59096b3ee87692d7952c72
stevenjb@chromium.org <stevenjb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-02 20:30:28 +0000

    Reduce and delay calls to StatisticsProvider that might block
    BUG=302798
    R=xiyuan@chromium.org
    
    Review URL: https://codereview.chromium.org/25494004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226552 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/policy/device_cloud_policy_manager_chromeos.cc
+++ chrome/browser/chromeos/policy/device_cloud_policy_manager_chromeos.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/prefs/pref_registry_simple.h"
 #include "base/prefs/pref_service.h"
 #include "chrome/browser/chromeos/attestation/attestation_policy_observer.h"
+#include "chrome/browser/chromeos/login/startup_utils.h"
 #include "chrome/browser/chromeos/policy/device_cloud_policy_store_chromeos.h"
 #include "chrome/browser/chromeos/policy/enrollment_handler_chromeos.h"
 #include "chrome/browser/chromeos/policy/enterprise_install_attributes.h"
@@ -139,7 +140,8 @@ std::string DeviceCloudPolicyManagerChromeOS::GetDeviceRequisition() const {
   std::string requisition;
   const PrefService::Preference* pref = local_state_->FindPreference(
       prefs::kDeviceEnrollmentRequisition);
-  if (pref->IsDefaultValue()) {
+  if (pref->IsDefaultValue() && !chromeos::StartupUtils::IsOobeCompleted()) {
+    // OEM statistics are only loaded when OOBE is not completed.
     requisition =
         GetMachineStatistic(chromeos::system::kOemDeviceRequisitionKey);
   } else {
