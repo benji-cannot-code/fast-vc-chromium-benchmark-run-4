a8a8b8d51016cbea8f36359ab80f15abe254903c
oysteine@chromium.org <oysteine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-02 15:56:09 +0000

    Modified the PerformanceMonitor to not use test/ code for enumerating Chrome processes.
    Separated out basic performance metrics gathering in the PerformanceMonitor to run independently from the metrics/events database code. The former will now run even without the performance-monitor-gathering flag.
    
    Added an UMA event which triggers when the browser process never runs below X CPU utilization over a period of Y seconds.
    
    BUG=279660, 273320
    
    Review URL: https://codereview.chromium.org/23825004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226466 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_browser_main.cc
+++ chrome/browser/chrome_browser_main.cc
@@ -1576,10 +1576,7 @@ bool ChromeBrowserMainParts::MainMessageLoopRun(int* result_code) {
   base::RunLoop run_loop;
 #endif
 
-  if (CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kPerformanceMonitorGathering)) {
-    performance_monitor::PerformanceMonitor::GetInstance()->Start();
-  }
+  performance_monitor::PerformanceMonitor::GetInstance()->Start();
 
   run_loop.Run();
 
