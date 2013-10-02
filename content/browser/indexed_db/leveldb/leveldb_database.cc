ce6b9fba14ae5fe11e23c80c02944d6a13ab1e5a
jsbell@chromium.org <jsbell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-02 22:40:53 +0000

    IndexedDB: Don't fsync after transactions
    Syncing after transaction writes was an early shot in
    the dark at reducing data loss issues, but it appears
    that other sources of corruption are more prevalent and
    the performance impact on slower file systems is too great.
    
    BUG=299831
    
    Review URL: https://codereview.chromium.org/23542012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226587 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/indexed_db/leveldb/leveldb_database.cc
+++ content/browser/indexed_db/leveldb/leveldb_database.cc
@@ -31,6 +31,20 @@ using base::StringPiece;
 
 namespace content {
 
+// Forcing flushes to disk at the end of a transaction guarantees that the
+// data hit disk, but drastically impacts throughput when the filesystem is
+// busy with background compactions. Not syncing trades off reliability for
+// performance. Note that background compactions which move data from the
+// log to SSTs are still done with reliable writes.
+//
+// Sync writes are necessary on Windows for quota calculations; POSIX
+// calculates file sizes correctly even when not synced to disk.
+#if defined(OS_WIN)
+static const bool kSyncWrites = true;
+#else
+static const bool kSyncWrites = false;
+#endif
+
 static leveldb::Slice MakeSlice(const StringPiece& s) {
   return leveldb::Slice(s.begin(), s.size());
 }
@@ -329,7 +343,7 @@ scoped_ptr<LevelDBDatabase> LevelDBDatabase::OpenInMemory(
 
 bool LevelDBDatabase::Put(const StringPiece& key, std::string* value) {
   leveldb::WriteOptions write_options;
-  write_options.sync = true;
+  write_options.sync = kSyncWrites;
 
   const leveldb::Status s =
       db_->Put(write_options, MakeSlice(key), MakeSlice(*value));
@@ -341,7 +355,7 @@ bool LevelDBDatabase::Put(const StringPiece& key, std::string* value) {
 
 bool LevelDBDatabase::Remove(const StringPiece& key) {
   leveldb::WriteOptions write_options;
-  write_options.sync = true;
+  write_options.sync = kSyncWrites;
 
   const leveldb::Status s = db_->Delete(write_options, MakeSlice(key));
   if (s.ok())
@@ -376,7 +390,7 @@ bool LevelDBDatabase::Get(const StringPiece& key,
 
 bool LevelDBDatabase::Write(const LevelDBWriteBatch& write_batch) {
   leveldb::WriteOptions write_options;
-  write_options.sync = true;
+  write_options.sync = kSyncWrites;
 
   const leveldb::Status s =
       db_->Write(write_options, write_batch.write_batch_.get());
