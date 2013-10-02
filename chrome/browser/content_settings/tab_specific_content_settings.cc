1493be5b073b0906ef3270cae0ca792cb0c7f20b
gbillock@chromium.org <gbillock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-02 00:21:21 +0000

    Make sure we clear registerProtocolHandler settings on page navigation.
    BUG=298198
    
    Review URL: https://codereview.chromium.org/25084003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226360 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/content_settings/tab_specific_content_settings.cc
+++ chrome/browser/content_settings/tab_specific_content_settings.cc
@@ -710,6 +710,7 @@ void TabSpecificContentSettings::DidStartProvisionalLoadForFrame(
     ClearCookieSpecificContentSettings();
   ClearGeolocationContentSettings();
   ClearMIDIContentSettings();
+  ClearPendingProtocolHandler();
 }
 
 void TabSpecificContentSettings::AppCacheAccessed(const GURL& manifest_url,
