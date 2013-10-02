ab1436818d728e3ef7628041a5e423b767dd7ef9
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-02 21:22:43 +0000

    Move core/platform/midi to modules/webmidi
    There doesn't seem to be any reason for this code to be in the platform
    directory. We might as well move it to the webmidi module next to the code that
    uses it.
    
    R=eseidel
    BUG=297477
    
    Review URL: https://codereview.chromium.org/25722004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158753 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/webmidi/MIDIAccessor.cpp
+++ third_party/WebKit/Source/modules/webmidi/MIDIAccessor.cpp
@@ -30,9 +30,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
-#include "core/platform/midi/MIDIAccessor.h"
+#include "modules/webmidi/MIDIAccessor.h"
 
-#include "core/platform/midi/MIDIAccessorClient.h"
+#include "modules/webmidi/MIDIAccessorClient.h"
 #include "public/platform/Platform.h"
 #include "public/platform/WebMIDIAccessor.h"
 #include "wtf/text/WTFString.h"
