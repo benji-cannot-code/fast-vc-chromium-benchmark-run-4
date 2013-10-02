cdc90ef6aac7922ec55f4a33ee6551667959a700
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-02 21:45:15 +0000

    DevTools: Retain DevToolsAdbBridge::RefCountedAdbThread instance everywhere instead of DevToolsAdbBridge.
    BUG=None
    
    Review URL: https://codereview.chromium.org/23702049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226571 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/devtools/port_forwarding_controller.h
+++ chrome/browser/devtools/port_forwarding_controller.h
@@ -16,9 +16,7 @@ class PrefService;
 
 class PortForwardingController : public BrowserContextKeyedService {
  public:
-  PortForwardingController(
-      scoped_refptr<DevToolsAdbBridge> bridge,
-      PrefService* pref_service);
+  explicit PortForwardingController(PrefService* pref_service);
 
   virtual ~PortForwardingController();
 
@@ -53,7 +51,7 @@ class PortForwardingController : public BrowserContextKeyedService {
   class Connection;
   typedef std::map<std::string, Connection* > Registry;
 
-  scoped_refptr<DevToolsAdbBridge> bridge_;
+  scoped_refptr<DevToolsAdbBridge::RefCountedAdbThread> adb_thread_;
   PrefService* pref_service_;
   Registry registry_;
 
