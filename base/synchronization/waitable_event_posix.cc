ff756dadac2d85512203cdd5a98194f8a418b66a
piman@chromium.org <piman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-02 01:25:21 +0000

    Fix WaitableEvent and ConditionVariable::TimedWait to use monotonic time on non-Mac non-NaCl posix
    Both use a relative time, and it's important that this relative time is consistent with the wall clock when the system time gets adjusted (e.g. NTP, tlsdate, etc.). The monotonic clock, when available, has that property. Unfortunately, by default, pthread_cond_timedwait takes the system time which doesn't have that property.
    
    On Linux/Chrome OS, we use pthread_condattr_setclock which lets us use the monotonic clock. On Android, we use the non-portable pthread_cond_timedwait_monotonic_np which has the same effect.
    Unfortunately, neither is supported by NaCl.
    Mac can use the non-portable pthread_cond_timedwait_relative_np which uses a relative time.
    
    BUG=293736
    R=thakis@chromium.org
    
    Review URL: https://codereview.chromium.org/24158005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226378 0039d316-1c4b-4281-b951-d872f2087c98

--- base/synchronization/waitable_event_posix.cc
+++ base/synchronization/waitable_event_posix.cc
@@ -160,7 +160,7 @@ void WaitableEvent::Wait() {
 
 bool WaitableEvent::TimedWait(const TimeDelta& max_time) {
   base::ThreadRestrictions::AssertWaitAllowed();
-  const Time end_time(Time::Now() + max_time);
+  const TimeTicks end_time(TimeTicks::Now() + max_time);
   const bool finite_time = max_time.ToInternalValue() >= 0;
 
   kernel_->lock_.Acquire();
@@ -185,7 +185,7 @@ bool WaitableEvent::TimedWait(const TimeDelta& max_time) {
   // again before unlocking it.
 
   for (;;) {
-    const Time current_time(Time::Now());
+    const TimeTicks current_time(TimeTicks::Now());
 
     if (sw.fired() || (finite_time && current_time >= end_time)) {
       const bool return_value = sw.fired();
