cccb923c6c7d95aca8baf9b0832da70250987579
rune@opera.com <rune@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-02 11:18:30 +0000

    Media query malformed when 'or' is used as a media type.
    The Media Queries errata[1] dated 2013-06-27 includes 'or' in the list of
    reserved keywords that will result in a malformed media query instead of an
    unknown media type when used as a media type.
    
    [1] http://www.w3.org/Style/2012/REC-mediaqueries-20120619-errata.html
    
    BUG=288213
    
    Review URL: https://codereview.chromium.org/23477048
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158713 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/CSSParser-in.cpp
+++ third_party/WebKit/Source/core/css/CSSParser-in.cpp
@@ -10180,6 +10180,9 @@ inline void CSSParser::detectMediaQueryToken(int length)
         CASE("only") {
             m_token = MEDIA_ONLY;
         }
+        CASE("or") {
+            m_token = MEDIA_OR;
+        }
     }
 }
 
