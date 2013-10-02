67a5d0602b6179fc2d3d28eb8e25fdc66b303ebc
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-02 07:42:23 +0000

    Mojo: Optimize VerifyUserPointer().
    Gets rid of run-time division. In the new empty read test, goes from ~8.7
    million iterations/second to ~9.7 million iterations/second (and in perf makes
    VerifyUserPointer() go from ~10% to ~0.5%).
    
    R=darin
    
    Review URL: https://codereview.chromium.org/25323003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226422 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/system/memory.cc
+++ mojo/system/memory.cc
@@ -12,9 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace mojo {
 namespace system {
 
-bool VerifyUserPointer(const void* pointer, size_t count, size_t size_each) {
-  DCHECK_GT(size_each, 0u);
-  if (count > std::numeric_limits<size_t>::max() / size_each)
+template <size_t size>
+bool VerifyUserPointerForSize(const void* pointer, size_t count) {
+  if (count > std::numeric_limits<size_t>::max() / size)
     return false;
 
   // TODO(vtl): If running in kernel mode, do a full verification. For now, just
@@ -22,6 +22,10 @@ bool VerifyUserPointer(const void* pointer, size_t count, size_t size_each) {
   // implementation is also possible if this check is skipped.)
   return count == 0 || !!pointer;
 }
+
+// Explicitly instantiate the sizes we need. Add instantiations as needed.
+template bool VerifyUserPointerForSize<1>(const void*, size_t);
+template bool VerifyUserPointerForSize<4>(const void*, size_t);
 
 }  // namespace system
 }  // namespace mojo
