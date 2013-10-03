b6c3224a04a4e3359a9545181e43155e16859434
teravest@chromium.org <teravest@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-03 03:34:58 +0000

    NaCl: FileDownloader now uses FileIO_Private.
    This changes the implementation of FileDownloader to use FileIO_Private instead
    of FileIOTrusted. This allows us to get rid of the FileIOTrusted interface.
    
    BUG=246396
    
    Review URL: https://codereview.chromium.org/25258004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226677 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/native_client/src/trusted/plugin/file_downloader.h
+++ ppapi/native_client/src/trusted/plugin/file_downloader.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "native_client/src/include/nacl_string.h"
 #include "native_client/src/trusted/validator/nacl_file_info.h"
 #include "ppapi/c/private/pp_file_handle.h"
-#include "ppapi/c/trusted/ppb_file_io_trusted.h"
+#include "ppapi/c/private/ppb_file_io_private.h"
 #include "ppapi/c/trusted/ppb_url_loader_trusted.h"
 #include "ppapi/cpp/file_io.h"
 #include "ppapi/cpp/instance.h"
@@ -53,7 +53,7 @@ class FileDownloader {
         file_open_notify_callback_(pp::BlockUntilComplete()),
         stream_finish_callback_(pp::BlockUntilComplete()),
         file_handle_(PP_kInvalidFileHandle),
-        file_io_trusted_interface_(NULL),
+        file_io_private_interface_(NULL),
         url_loader_trusted_interface_(NULL),
         open_time_(-1),
         mode_(DOWNLOAD_NONE),
@@ -172,6 +172,8 @@ class FileDownloader {
   void URLReadBodyNotify(int32_t pp_error);
   void StreamFinishNotify(int32_t pp_error);
 
+  void GotFileHandleNotify(int32_t pp_error, PP_FileHandle handle);
+
   Plugin* instance_;
   nacl::string url_to_open_;
   nacl::string url_;
@@ -181,7 +183,7 @@ class FileDownloader {
   pp::FileIO file_reader_;
   PP_FileHandle file_handle_;
   struct NaClFileToken file_token_;
-  const PPB_FileIOTrusted* file_io_trusted_interface_;
+  const PPB_FileIO_Private* file_io_private_interface_;
   const PPB_URLLoaderTrusted* url_loader_trusted_interface_;
   pp::URLLoader url_loader_;
   pp::CompletionCallbackFactory<FileDownloader> callback_factory_;
@@ -194,6 +196,7 @@ class FileDownloader {
   std::deque<char> buffer_;
   UrlSchemeType url_scheme_;
   StreamCallbackSource* data_stream_callback_source_;
+  NaClFileInfo cached_file_info_;
 };
 }  // namespace plugin;
 #endif  // NATIVE_CLIENT_SRC_TRUSTED_PLUGIN_FILE_DOWNLOADER_H_
