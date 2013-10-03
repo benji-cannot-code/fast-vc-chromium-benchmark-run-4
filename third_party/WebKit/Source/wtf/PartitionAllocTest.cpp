b83e7389d7bdfe0075c9de98b30c943f2e9886ea
cevans@chromium.org <cevans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-03 07:09:02 +0000

    Optimize test for pointer being in a partition on 32-bit.
    We use a small (8KB) global bitmap.
    
    R=tsepez@chromium.org
    
    Review URL: https://codereview.chromium.org/25640003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158777 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/PartitionAllocTest.cpp
+++ third_party/WebKit/Source/wtf/PartitionAllocTest.cpp
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/PartitionAlloc.h"
 
 #include "wtf/OwnArrayPtr.h"
-#include "wtf/PageAllocator.h"
 #include <gtest/gtest.h>
 #include <stdlib.h>
 #include <string.h>
