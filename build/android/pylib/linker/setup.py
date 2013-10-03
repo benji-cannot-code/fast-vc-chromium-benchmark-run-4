efa760528be153cd2609636a005414d538beda14
digit@chromium.org <digit@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-03 11:56:38 +0000

    Add new Android test runner command to handle linker tests.
    This adds a new test runner command to run the content linker
    tests on Android. This requires first the CL at
    https://chromiumcodereview.appspot.com/23717023/
    which defines a new build target named 'content_linker_test_apk'.
    
    To launch the linker tests, simply do:
    
      ninja -C out/Debug content_linker_tests_apk
      build/android/test_runner.py linker
    
    See test_case.py for details of what the test does.
    
    BUG=287739
    R=bulach@chromium.org, craigdh@chromium.org, frankf@chromium.org, yfriedman@chromium.org
    
    Review URL: https://codereview.chromium.org/25525003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226739 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ build/android/pylib/linker/setup.py
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Setup for linker tests."""
+
+import logging
+import os
+import sys
+import types
+
+import test_case
+import test_runner
+
+
+def Setup(options, devices):
+  """Creates a list of test cases and a runner factory.
+
+  Returns:
+    A tuple of (TestRunnerFactory, tests).
+  """
+
+  all_tests = [
+      test_case.LinkerTestCase('ForRegularDevice',
+                               is_low_memory=False),
+      test_case.LinkerTestCase('ForLowMemoryDevice',
+                               is_low_memory=True) ]
+
+  def TestRunnerFactory(device, shard_index):
+    return test_runner.LinkerTestRunner(
+        device, options.tool, options.push_deps,
+        options.cleanup_test_files)
+
+  return (TestRunnerFactory, all_tests)
