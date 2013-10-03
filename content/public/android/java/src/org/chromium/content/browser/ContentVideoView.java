3cab2b110d01ffe856a1d856442936c2ad8bc1dd
boliu@chromium.org <boliu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-03 21:51:07 +0000

    [Android] Prevent re-entrance into ContentVideoView.exitFullscreen
    For Android WebView, if app decides to call exitFullscreen from
    onDestroyContentVideoView, then re-entering into exitFullscreen and
    causing an infinite recursion.
    
    Fix by moving the onDestroyContentVideoView to after removeSurfaceView.
    The next time destroyContentVideoView in, onDestroyContentVideoView will
    not be called since mVideoSurfaceView is already null.
    
    BUG=
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/25899002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226859 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/ContentVideoView.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentVideoView.java
@@ -617,10 +617,12 @@ public class ContentVideoView
     @CalledByNative
     private void destroyContentVideoView(boolean nativeViewDestroyed) {
         if (mVideoSurfaceView != null) {
-            mClient.onDestroyContentVideoView();
             removeControls();
             removeSurfaceView();
             setVisibility(View.GONE);
+
+            // To prevent re-entrance, call this after removeSurfaceView.
+            mClient.onDestroyContentVideoView();
         }
         if (nativeViewDestroyed) {
             mNativeContentVideoView = 0;
