401b75c24096dd4229d29355f9adf6885b78397d
eseidel@chromium.org <eseidel@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-03 11:10:44 +0000

    Remove unused header triggering check-blink-deps violation
    BUG=297477
    
    Review URL: https://codereview.chromium.org/25820002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158801 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/clipboard/ClipboardUtilities.cpp
+++ third_party/WebKit/Source/platform/clipboard/ClipboardUtilities.cpp
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "platform/clipboard/ClipboardUtilities.h"
 
-#include "core/platform/Pasteboard.h"
 #include "weborigin/KURL.h"
 #include "wtf/text/StringBuilder.h"
 #include "wtf/text/WTFString.h"
