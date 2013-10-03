c6ca25662cca09a39294ac3c1e33b8fe4d44826a
rockot@chromium.org <rockot@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-03 18:39:30 +0000

    Support DOM events in the API reference template.
    This enables events in an API to be flagged as DOM events,
    giving them a separate schema, TOC section, and reference
    presentation.
    
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/25771002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226803 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/server2/api_data_source.py
+++ chrome/common/extensions/docs/server2/api_data_source.py
@@ -183,6 +183,7 @@ class _JSCModel(object):
       'types': self._GenerateTypes(self._namespace.types.values()),
       'functions': self._GenerateFunctions(self._namespace.functions),
       'events': self._GenerateEvents(self._namespace.events),
+      'domEvents': self._GenerateDomEvents(self._namespace.events),
       'properties': self._GenerateProperties(self._namespace.properties),
       'introList': self._GetIntroTableList(),
       'channelWarning': self._GetChannelWarning(),
@@ -257,7 +258,12 @@ class _JSCModel(object):
     return function_dict
 
   def _GenerateEvents(self, events):
-    return [self._GenerateEvent(e) for e in events.values()]
+    return [self._GenerateEvent(e) for e in events.values()
+            if not e.supports_dom]
+
+  def _GenerateDomEvents(self, events):
+    return [self._GenerateEvent(e) for e in events.values()
+            if e.supports_dom]
 
   def _GenerateEvent(self, event):
     event_dict = {
@@ -269,6 +275,7 @@ class _JSCModel(object):
       'actions': [self._GetLink(action) for action in event.actions],
       'supportsRules': event.supports_rules,
       'supportsListeners': event.supports_listeners,
+      'properties': [],
       'id': _CreateId(event, 'event')
     }
     if (event.parent is not None and
@@ -296,6 +303,11 @@ class _JSCModel(object):
         'callback': self._GenerateFunction(callback_object),
         'parameters': [callback_parameters]
       }
+    if event.supports_dom:
+      # Treat params as properties of the custom Event object associated with
+      # this DOM Event.
+      event_dict['properties'] += [self._GenerateProperty(param)
+                                   for param in event.params]
     return event_dict
 
   def _GenerateCallback(self, callback):
