2f26ef70f3f54cd006f731449718c0613124e7f6
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-03 06:56:33 +0000

    Use views::corewm::GetToplevelWindow instead of wm::GetActivatableWindow
    BUG=none
    
    Review URL: https://codereview.chromium.org/25731006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226698 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/video_detector.cc
+++ ash/wm/video_detector.cc
@@ -7,11 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/shell.h"
 #include "ash/wm/window_state.h"
-#include "ash/wm/window_util.h"
 #include "ui/aura/env.h"
 #include "ui/aura/root_window.h"
 #include "ui/aura/window.h"
 #include "ui/gfx/rect.h"
+#include "ui/views/corewm/window_util.h"
 
 namespace ash {
 
@@ -123,7 +123,7 @@ void VideoDetector::MaybeNotifyObservers(aura::Window* window,
   if (!window->GetBoundsInRootWindow().Intersects(root_bounds))
     return;
 
-  aura::Window* toplevel_window = wm::GetActivatableWindow(window);
+  aura::Window* toplevel_window = views::corewm::GetToplevelWindow(window);
   bool is_fullscreen = toplevel_window ?
       wm::GetWindowState(toplevel_window)->IsFullscreen() : false;
 
