db11958070f3920fd72b3814e9f1ee6d7170e660
ppi@chromium.org <ppi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-03 22:16:47 +0000

    Allow to request a lazy restore load in the NavigationController.
    On Desktop restore of a tab is equivalent to restore of its
    NavigationController. After the navigation state is restored, the controller is
    left with the |needs_reload_| flag on, which makes it load the content (using
    restore load cache policies) when the controller becomes active.
    
    On Android and ChromeOS the system out-of-memory killer kills the renderers of
    background tabs when under memory pressure. The tabs that lost their renderers
    need to be loaded when the user switches to them.
    
    While semantically this is tab restore and should be done using restore load
    cache policies, we currently bring the content back (on Android and ChromeOS)
    using NavigationController.Reload().
    
    This patch allows the consumers to request a restore load in
    NavigationController, allowing to use the same load code path for all restore
    loads on memory-constrained platforms.
    
    BUG=299865
    
    Review URL: https://codereview.chromium.org/25660011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226869 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/web_contents/navigation_controller_impl.cc
+++ content/browser/web_contents/navigation_controller_impl.cc
@@ -1417,6 +1417,10 @@ bool NavigationControllerImpl::NeedsReload() const {
   return needs_reload_;
 }
 
+void NavigationControllerImpl::SetNeedsReload() {
+  needs_reload_ = true;
+}
+
 void NavigationControllerImpl::RemoveEntryAtIndexInternal(int index) {
   DCHECK(index < GetEntryCount());
   DCHECK(index != last_committed_entry_index_);
