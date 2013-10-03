bbe1b3325fa8099d2d2bf5517c2710400652d510
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-03 15:25:04 +0000

    drive: Stop using ConvertToMap to implement directory load
    Stop using ConvertToMap to call ChangeListProcessor::RefreshDirectory.
    Make ChangeListProcessor::RefreshDirectory take ScopedVector<ChangeList> as its argument.
    
    BUG=275270
    TEST=unit_tests
    
    Review URL: https://codereview.chromium.org/25634004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226763 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/change_list_loader.cc
+++ chrome/browser/chromeos/drive/change_list_loader.cc
@@ -883,8 +883,6 @@ void ChangeListLoader::DoLoadDirectoryFromServerAfterLoad(
     return;
   }
 
-  ChangeListProcessor::ResourceEntryMap entry_map;
-  ChangeListProcessor::ConvertToMap(change_lists.Pass(), &entry_map, NULL);
   base::FilePath* directory_path = new base::FilePath;
   base::PostTaskAndReplyWithResult(
       blocking_task_runner_,
@@ -892,7 +890,7 @@ void ChangeListLoader::DoLoadDirectoryFromServerAfterLoad(
       base::Bind(&ChangeListProcessor::RefreshDirectory,
                  resource_metadata_,
                  directory_fetch_info,
-                 entry_map,
+                 base::Passed(&change_lists),
                  directory_path),
       base::Bind(&ChangeListLoader::DoLoadDirectoryFromServerAfterRefresh,
                  weak_ptr_factory_.GetWeakPtr(),
