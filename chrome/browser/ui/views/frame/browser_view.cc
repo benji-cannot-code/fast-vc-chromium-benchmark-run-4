b08d0fbe9e46dbcc173cd8679f24e5f1d74cb0f1
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-03 01:10:46 +0000

    A browwer window should be activatable unless the modal dialog is actually shown
    BUG=302002
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/25411006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226646 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -1476,7 +1476,8 @@ bool BrowserView::CanMaximize() const {
 }
 
 bool BrowserView::CanActivate() const {
-  if (!AppModalDialogQueue::GetInstance()->active_dialog())
+  if (!AppModalDialogQueue::GetInstance()->active_dialog() ||
+      !AppModalDialogQueue::GetInstance()->active_dialog()->native_dialog())
     return true;
 
 #if defined(USE_AURA) && defined(OS_CHROMEOS)
