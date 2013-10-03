e782dbe30b03044a5dc0e891c448bd100348d54f
noamsml@chromium.org <noamsml@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-03 10:23:08 +0000

    Send three spaced repeat query when the user opens the devices page
    Send three queries to the network when the user opens chrome://devices/,
    one zero seconds after the user opens the page, one 1 second, and one 2
    seconds after.
    
    BUG=303428
    TEST=Manual with wireshark
    
    Review URL: https://codereview.chromium.org/25585004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226723 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/local_discovery/local_discovery_ui_handler.h
+++ chrome/browser/ui/webui/local_discovery/local_discovery_ui_handler.h
@@ -159,6 +159,10 @@ class LocalDiscoveryUIHandler : public content::WebUIMessageHandler,
 
   void CheckUserLoggedIn();
 
+  void ScheduleQuery(int timeout_seconds);
+
+  void SendQuery(int next_timeout_seconds);
+
 #if defined(CLOUD_PRINT_CONNECTOR_UI_AVAILABLE)
   void StartCloudPrintConnector();
   void OnCloudPrintPrefsChanged();
@@ -205,6 +209,9 @@ class LocalDiscoveryUIHandler : public content::WebUIMessageHandler,
 
   // Announcement timeout for registration.
   base::CancelableCallback<void()> registration_announce_timeout_;
+
+  // Callback for requery.
+  base::CancelableCallback<void()> requery_callback_;
 
 #if defined(CLOUD_PRINT_CONNECTOR_UI_AVAILABLE)
   StringPrefMember cloud_print_connector_email_;
