eeb72e1c4475cd07e946f17674ee3632670af35e
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-03 07:46:49 +0000

    Delete unused Windows UA string logic
    We don't use this code to compute the UA string on Windows.
    
    R=adamk@chromium.org
    
    Review URL: https://codereview.chromium.org/25756004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158781 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/win/SystemInfo.h
+++ third_party/WebKit/Source/core/platform/win/SystemInfo.h
@@ -64,8 +64,6 @@ enum WindowsVersion {
 // and dwMinorVersion field values, respectively.
 WindowsVersion windowsVersion(int* major = 0, int* minor = 0);
 
-String windowsVersionForUAString();
-
 } // namespace WebCore
 
 #endif // SystemInfo_h
