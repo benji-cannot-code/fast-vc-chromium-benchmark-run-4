853d516704c376e976b95ca5d50e8012c9667323
miletus@chromium.org <miletus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-03 00:58:15 +0000

    Send touch cancel to renderer when scrolling starts
    We decide that Desktop Chrome should follow the Chrome on Android
    mode for touch behaviour during scrolling: send touch cancel to
    renderer when scrolling starts and then stop sending touch events
    to renderer.
    
    This CL implements the above model. When GestureScrollBegin is received,
    we inject a synthesized touch cancel event into the touch event queue
    and stop sending all the following touch events to renderer. When the
    touch cancel event is acked, we drop it before sending it to view so it
    won't interrupt normal gesture flow.
    
    BUG=240735
    TEST=Enable the flag #enable-no-touch-to-renderer-while-scrolling in chrome://flags
    1. go to www.rbyers.net/eventTest.html
    scroll the page, one should first see touch move events, then touch cancel events.
    2. go to www.rbyers.net/janky-touch-scroll.html
    scroll the page and one should see smooth scrolling once the scrolling starts.
    
    Review URL: https://codereview.chromium.org/23856016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226638 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/input/touch_event_queue_unittest.cc
+++ content/browser/renderer_host/input/touch_event_queue_unittest.cc
@@ -119,12 +119,12 @@ class TouchEventQueueTest : public testing::Test,
     return last_acked_event_state_;
   }
 
-  void set_no_touch_move_to_renderer(bool no_touch_move) {
-    queue_->set_no_touch_move_to_renderer(no_touch_move);
+  void set_no_touch_to_renderer(bool no_touch) {
+    queue_->no_touch_to_renderer_ = no_touch;
   }
 
-  bool no_touch_move_to_renderer() const {
-    return queue_->no_touch_move_to_renderer_;
+  bool no_touch_to_renderer() const {
+    return queue_->no_touch_to_renderer_;
   }
 
  private:
@@ -642,7 +642,7 @@ TEST_F(TouchEventQueueTest, NoTouchMove) {
   EXPECT_EQ(1U, GetAndResetAckedEventCount());
 
   // Touch move should not be sent to renderer (e.g., when scrolling starts).
-  set_no_touch_move_to_renderer(true);
+  set_no_touch_to_renderer(true);
   MoveTouchPoint(0, 30, 5);
   SendTouchEvent();
   EXPECT_EQ(0U, GetAndResetSentEventCount());
@@ -654,16 +654,14 @@ TEST_F(TouchEventQueueTest, NoTouchMove) {
   EXPECT_EQ(0U, GetAndResetSentEventCount());
   EXPECT_EQ(1U, GetAndResetAckedEventCount());
 
-  // Touch end should still be sent to renderer.
+  // Touch end should not be sent to renderer.
   ReleaseTouchPoint(0);
   SendTouchEvent();
-  EXPECT_EQ(1U, GetAndResetSentEventCount());
-  EXPECT_EQ(0U, GetAndResetAckedEventCount());
+  EXPECT_EQ(0U, GetAndResetSentEventCount());
+  EXPECT_EQ(1U, GetAndResetAckedEventCount());
 
   // Resume sending touch moves to renderer (e.g., when scrolling ends).
-  set_no_touch_move_to_renderer(false);
-  SendTouchEventACK(INPUT_EVENT_ACK_STATE_NOT_CONSUMED);
-  EXPECT_EQ(1U, GetAndResetAckedEventCount());
+  set_no_touch_to_renderer(false);
 
   // Now touch events should come through to renderer.
   PressTouchPoint(80, 10);
