06da2ceb3d7af1b813c8f00db503edb431401dc3
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-03 18:10:23 +0000

    linux_aura: Use rounded corners on windows.
    This asks the same views shaping system as Windows, and updates the
    xshape on resize, like Windows.
    
    BUG=249229
    R=sadrul@chromium.org
    
    Review URL: https://codereview.chromium.org/25812003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226794 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/desktop_aura/desktop_root_window_host_x11.cc
+++ ui/views/widget/desktop_aura/desktop_root_window_host_x11.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/x/device_data_manager.h"
 #include "ui/events/x/touch_factory_x11.h"
 #include "ui/gfx/insets.h"
+#include "ui/gfx/path.h"
 #include "ui/gfx/path_x11.h"
 #include "ui/native_theme/native_theme.h"
 #include "ui/views/corewm/compound_event_filter.h"
@@ -1037,6 +1038,27 @@ void DesktopRootWindowHostX11::DispatchMouseEvent(ui::MouseEvent* event) {
   }
 }
 
+void DesktopRootWindowHostX11::ResetWindowRegion() {
+  if (!IsMaximized()) {
+    gfx::Path window_mask;
+    views::Widget* widget = native_widget_delegate_->AsWidget();
+    if (widget->non_client_view()) {
+      widget->non_client_view()->GetWindowMask(bounds_.size(), &window_mask);
+      Region region = gfx::CreateRegionFromSkPath(window_mask);
+      XShapeCombineRegion(xdisplay_, xwindow_, ShapeBounding,
+                          0, 0, region, false);
+      XDestroyRegion(region);
+      return;
+    }
+  }
+
+  // If we didn't set the shape for any reason, reset the shaping information
+  // by ShapeSet-ing with our bounds rect.
+  XRectangle r = { 0, 0, bounds_.width(), bounds_.height() };
+  XShapeCombineRectangles(xdisplay_, xwindow_, ShapeBounding,
+                          0, 0, &r, 1, ShapeSet, YXBanded);
+}
+
 std::list<XID>& DesktopRootWindowHostX11::open_windows() {
   if (!open_windows_)
     open_windows_ = new std::list<XID>();
@@ -1124,6 +1146,7 @@ bool DesktopRootWindowHostX11::Dispatch(const base::NativeEvent& event) {
         root_window_host_delegate_->OnHostResized(bounds.size());
       if (origin_changed)
         root_window_host_delegate_->OnHostMoved(bounds_.origin());
+      ResetWindowRegion();
       break;
     }
     case GenericEvent: {
