c5877bf6458c02b54ad20ed4804f0cb80ca6002b
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-03 05:17:51 +0000

    rAc: phone number tooltip
    BUG=292870
    
    Review URL: https://codereview.chromium.org/24953002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226689 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/autofill/decorated_textfield.h
+++ chrome/browser/ui/views/autofill/decorated_textfield.h
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_AUTOFILL_DECORATED_TEXTFIELD_H_
 #define CHROME_BROWSER_UI_VIEWS_AUTOFILL_DECORATED_TEXTFIELD_H_
 
+#include "base/memory/scoped_ptr.h"
 #include "base/strings/string16.h"
 #include "ui/gfx/image/image.h"
+#include "ui/views/controls/textfield/textfield.h"
 #include "ui/views/controls/textfield/textfield.h"
 
 namespace views {
@@ -36,14 +38,23 @@ class DecoratedTextfield : public views::Textfield {
   // text.
   void SetIcon(const gfx::Image& icon);
 
+  // Sets a tooltip for this field. This will override the icon set with
+  // SetIcon(), if any, and will be overridden by future calls to SetIcon().
+  void SetTooltipIcon(const base::string16& text);
+
   // views::View implementation.
   virtual const char* GetClassName() const OVERRIDE;
   virtual gfx::Size GetPreferredSize() OVERRIDE;
-  virtual void PaintChildren(gfx::Canvas* canvas) OVERRIDE;
-  virtual void OnPaint(gfx::Canvas* canvas) OVERRIDE;
+  virtual void Layout() OVERRIDE;
+  virtual void OnFocus() OVERRIDE;
+  virtual void OnBlur() OVERRIDE;
 
  private:
   FRIEND_TEST_ALL_PREFIXES(DecoratedTextfieldTest, HeightMatchesButton);
+
+  // Called to update the layout after SetIcon or SetTooltipIcon has been
+  // called.
+  void IconChanged();
 
   // This number corresponds to the number of pixels in the images that
   // are used to draw a views button which are above or below the actual border.
@@ -54,8 +65,8 @@ class DecoratedTextfield : public views::Textfield {
   // We draw the border.
   views::FocusableBorder* border_;  // Weak.
 
-  // The icon that goes at the right side of the textfield.
-  gfx::Image icon_;
+  // The view that holds the icon at the end of the textfield.
+  scoped_ptr<views::ImageView> icon_view_;
 
   // Whether the text contents are "invalid" (i.e. should special markers be
   // shown to indicate invalidness).
