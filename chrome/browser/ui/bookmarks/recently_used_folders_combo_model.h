f24e9ffa28d4bd920ac5dcec04687263036ec9ab
tapted@chromium.org <tapted@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-03 03:38:44 +0000

    Revert 226618 "Makes RecentlyUsedFoldersComboModel clear interna..."
    Results in test failures under asan in linux_chromeos. Verified revert in
    a local asan build. It might be the tests that are wrong, but reverting
    in case there are more subtle problems not tested.
    
    Problematic tests:
     - unit_tests:BookmarkBubbleViewTest.SyncPromoNotSignedIn
     - unit_tests:BookmarkBubbleViewTest.SyncPromoSignedIn
     - interactive_ui_tests:StarViewTest.HideOnSecondClick
    
    > Makes RecentlyUsedFoldersComboModel clear internal structur when node deleted
    > 
    > Prior to this RecentlyUsedFoldersComboModel would not clear its
    > internal structures if a node was deleted, leading to a future
    > crash. This patch makes RecentlyUsedFoldersComboModel clear internal
    > structure and notify observers.
    > 
    > Having the BookmarkModel change while the bubble is showing is rare,
    > so the code doesn't attempt to do anything elegant.
    > 
    > BUG=294330
    > TEST=none
    > R=msw@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/25310003
    
    TBR=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/25833002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226678 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/bookmarks/recently_used_folders_combo_model.h
+++ chrome/browser/ui/bookmarks/recently_used_folders_combo_model.h
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
-#include "base/observer_list.h"
-#include "chrome/browser/bookmarks/bookmark_model_observer.h"
 #include "ui/base/models/combobox_model.h"
 
 class BookmarkModel;
@@ -21,9 +19,7 @@ class BookmarkNode;
 // list always contains the Bookmarks Bar, Other Bookmarks and the parent
 // folder. The list also contains an extra item that shows the text
 // "Choose Another Folder...".
-class RecentlyUsedFoldersComboModel
-    : public ui::ComboboxModel,
-      public BookmarkModelObserver {
+class RecentlyUsedFoldersComboModel : public ui::ComboboxModel {
  public:
   RecentlyUsedFoldersComboModel(BookmarkModel* model, const BookmarkNode* node);
   virtual ~RecentlyUsedFoldersComboModel();
@@ -33,36 +29,6 @@ class RecentlyUsedFoldersComboModel
   virtual string16 GetItemAt(int index) OVERRIDE;
   virtual bool IsItemSeparatorAt(int index) OVERRIDE;
   virtual int GetDefaultIndex() const OVERRIDE;
-  virtual void AddObserver(ui::ComboboxModelObserver* observer) OVERRIDE;
-  virtual void RemoveObserver(ui::ComboboxModelObserver* observer) OVERRIDE;
-
-  // Overriden from BaseBookmarkModelObserver:
-  virtual void Loaded(BookmarkModel* model, bool ids_reassigned) OVERRIDE;
-  virtual void BookmarkModelBeingDeleted(BookmarkModel* model) OVERRIDE;
-  virtual void BookmarkNodeMoved(BookmarkModel* model,
-                                 const BookmarkNode* old_parent,
-                                 int old_index,
-                                 const BookmarkNode* new_parent,
-                                 int new_index) OVERRIDE;
-  virtual void BookmarkNodeAdded(BookmarkModel* model,
-                                 const BookmarkNode* parent,
-                                 int index) OVERRIDE;
-  virtual void OnWillRemoveBookmarks(BookmarkModel* model,
-                                     const BookmarkNode* parent,
-                                     int old_index,
-                                     const BookmarkNode* node) OVERRIDE;
-  virtual void BookmarkNodeRemoved(BookmarkModel* model,
-                                   const BookmarkNode* parent,
-                                   int old_index,
-                                   const BookmarkNode* node) OVERRIDE;
-  virtual void BookmarkNodeChanged(BookmarkModel* model,
-                                   const BookmarkNode* node) OVERRIDE;
-  virtual void BookmarkNodeFaviconChanged(BookmarkModel* model,
-                                          const BookmarkNode* node) OVERRIDE;
-  virtual void BookmarkNodeChildrenReordered(
-      BookmarkModel* model,
-      const BookmarkNode* node) OVERRIDE;
-  virtual void BookmarkAllNodesRemoved(BookmarkModel* model) OVERRIDE;
 
   // If necessary this function moves |node| into the corresponding folder for
   // the given |selected_index|.
@@ -82,8 +48,6 @@ class RecentlyUsedFoldersComboModel
 
   // The index of the original parent folder.
   int node_parent_index_;
-
-  ObserverList<ui::ComboboxModelObserver> observers_;
 
   DISALLOW_COPY_AND_ASSIGN(RecentlyUsedFoldersComboModel);
 };
