16d19ad9be5c54169b4eb1592e32443e3f0e651f
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-03 11:32:02 +0000

    DevTools: display paint rectangles on Layers panel 3d view
    ... toggled with the existing settings panel checkbox.
    
    BUG=303130
    R=pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/25666005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158802 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorLayerTreeAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorLayerTreeAgent.cpp
@@ -57,10 +57,15 @@ namespace LayerTreeAgentState {
 static const char layerTreeAgentEnabled[] = "layerTreeAgentEnabled";
 };
 
+inline String idForLayer(GraphicsLayer* graphicsLayer)
+{
+    return String::number(graphicsLayer->platformLayer()->id());
+}
+
 static PassRefPtr<TypeBuilder::LayerTree::Layer> buildObjectForLayer(GraphicsLayer* graphicsLayer, int nodeId)
 {
     RefPtr<TypeBuilder::LayerTree::Layer> layerObject = TypeBuilder::LayerTree::Layer::create()
-        .setLayerId(String::number(graphicsLayer->platformLayer()->id()))
+        .setLayerId(idForLayer(graphicsLayer))
         .setOffsetX(graphicsLayer->position().x())
         .setOffsetY(graphicsLayer->position().y())
         .setWidth(graphicsLayer->size().width())
@@ -74,7 +79,7 @@ static PassRefPtr<TypeBuilder::LayerTree::Layer> buildObjectForLayer(GraphicsLay
     if (!parent)
         parent = graphicsLayer->replicatedLayer();
     if (parent)
-        layerObject->setParentLayerId(String::number(parent->platformLayer()->id()));
+        layerObject->setParentLayerId(idForLayer(parent));
     if (!graphicsLayer->contentsAreVisible())
         layerObject->setInvisible(true);
     const TransformationMatrix& transform = graphicsLayer->transform();
@@ -149,6 +154,22 @@ void InspectorLayerTreeAgent::disable(ErrorString*)
 void InspectorLayerTreeAgent::layerTreeDidChange()
 {
     m_frontend->layerTreeDidChange();
+}
+
+void InspectorLayerTreeAgent::didPaint(RenderObject* renderer, GraphicsContext*, const LayoutRect& rect)
+{
+    RenderLayer* renderLayer = toRenderLayerModelObject(renderer)->layer();
+    RenderLayerBacking* backing = renderLayer->backing();
+    // Should only happen for FrameView paints when compositing is off. Consider different instrumentation method for that.
+    if (!backing)
+        return;
+    GraphicsLayer* graphicsLayer = backing->graphicsLayer();
+    RefPtr<TypeBuilder::DOM::Rect> domRect = TypeBuilder::DOM::Rect::create()
+        .setX(rect.x())
+        .setY(rect.y())
+        .setWidth(rect.width())
+        .setHeight(rect.height());
+    m_frontend->layerPainted(idForLayer(graphicsLayer), domRect.release());
 }
 
 void InspectorLayerTreeAgent::getLayers(ErrorString* errorString, const int* nodeId, RefPtr<TypeBuilder::Array<TypeBuilder::LayerTree::Layer> >& layers)
