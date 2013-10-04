4b746335752adffa17b46654be653b95f652b472
flackr@chromium.org <flackr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-04 20:54:38 +0000

    Handle bounds changes on a window during overview.
    When a window bounds change happens during overview, the transform on that window needs to be recomputed for its new position (and size) and applied immediately to keep it within the expected location on the overview.
    
    BUG=302709
    TEST=WindowSelectorTest.BoundsChangeDuringOverview, WindowSelectorTest.BoundsChangeDuringCycleOnOtherDisplay
    TEST=Close all windows. Press Ctrl+N a few times, then Ctrl+M to minimize all but one window such that the one window automatically recenters. Enter overview. The window positions should be evenly spaced even though restoring the windows changed their bounds.
    
    Review URL: https://codereview.chromium.org/25444006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227085 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/overview/window_selector_item.cc
+++ ash/wm/overview/window_selector_item.cc
@@ -4,10 +4,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "ash/wm/overview/window_selector_item.h"
+#include "base/auto_reset.h"
+#include "ui/aura/window.h"
 
 namespace ash {
 
-WindowSelectorItem::WindowSelectorItem() {
+WindowSelectorItem::WindowSelectorItem()
+    : root_window_(NULL),
+      in_bounds_update_(false) {
 }
 
 WindowSelectorItem::~WindowSelectorItem() {
@@ -15,8 +19,20 @@ WindowSelectorItem::~WindowSelectorItem() {
 
 void WindowSelectorItem::SetBounds(aura::RootWindow* root_window,
                                    const gfx::Rect& target_bounds) {
+  if (in_bounds_update_)
+    return;
+  base::AutoReset<bool> auto_reset_in_bounds_update(&in_bounds_update_, true);
+  root_window_ = root_window;
   bounds_ = target_bounds;
-  SetItemBounds(root_window, target_bounds);
+  SetItemBounds(root_window, target_bounds, true);
+}
+
+void WindowSelectorItem::RecomputeWindowTransforms() {
+  if (in_bounds_update_ || bounds_.IsEmpty())
+    return;
+  DCHECK(root_window_);
+  base::AutoReset<bool> auto_reset_in_bounds_update(&in_bounds_update_, true);
+  SetItemBounds(root_window_, bounds_, false);
 }
 
 }  // namespace ash
