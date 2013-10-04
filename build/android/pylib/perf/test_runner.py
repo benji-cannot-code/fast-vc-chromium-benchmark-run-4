4eb96822bd040b55416db03f1c0b556f56a6afed
bulach@chromium.org <bulach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-04 18:14:27 +0000

    Relands: Android perf tests: kill adbd on the device after running a test.
    There's some data indicating that adbd on the device sometimes
    spontaneously restores its connection.
    After running a test, let's kill adbd and forwarder and wait for bit.
    
    This is a speculative change to try to make the bots healthier.
    Relands crrev.com/226762: will also kill the forwarder.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/25982005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227038 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/pylib/perf/test_runner.py
+++ build/android/pylib/perf/test_runner.py
@@ -51,7 +51,9 @@ import pickle
 import os
 import sys
 
+from pylib import android_commands
 from pylib import constants
+from pylib import forwarder
 from pylib.base import base_test_result
 from pylib.base import base_test_runner
 
@@ -153,6 +155,13 @@ class TestRunner(base_test_runner.BaseTestRunner):
         'cmd': cmd,
     }
     self._SaveResult(persisted_result)
+
+    try:
+      logging.warning('Unmapping device ports')
+      forwarder.Forwarder.UnmapAllDevicePorts(self.adb)
+      self.adb.KillAdbdDevice()
+    except Exception as e:
+      logging.error('Exception when tearing down device %s', e)
 
     return (output, result_type)
 
