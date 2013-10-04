1a65d242f139c0aa5708675cedca35c5edc58c8f
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-04 10:32:16 +0000

    Fix crash when we start dragging a input[type=range] thumb on the padding area.
    BUG=303772
    
    Review URL: https://codereview.chromium.org/25603005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158914 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/shadow/SliderThumbElement.cpp
+++ third_party/WebKit/Source/core/html/shadow/SliderThumbElement.cpp
@@ -245,6 +245,7 @@ Node* SliderThumbElement::focusDelegate()
 
 void SliderThumbElement::dragFrom(const LayoutPoint& point)
 {
+    RefPtr<SliderThumbElement> protector(this);
     setPositionFromPoint(point);
     startDragging();
 }
