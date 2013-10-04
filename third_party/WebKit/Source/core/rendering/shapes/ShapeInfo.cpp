ccefc97e78ba795dc0e09ff66848b3d2ffd2bfeb
hmuller@adobe.com <hmuller@adobe.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-04 19:17:53 +0000

    [CSS Shapes] Support the shape-image-threshold property
    Replace the hardwired 0 Shape::createShape() threshold parameter with the style's shapeImageThreshold() value (which has been clamped to [0,1]).
    
    BUG=303453
    
    Review URL: https://codereview.chromium.org/25968007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158953 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/shapes/ShapeInfo.cpp
+++ third_party/WebKit/Source/core/rendering/shapes/ShapeInfo.cpp
@@ -45,6 +45,7 @@ const Shape* ShapeInfo<RenderType>::computedShape() const
     WritingMode writingMode = m_renderer->style()->writingMode();
     Length margin = m_renderer->style()->shapeMargin();
     Length padding = m_renderer->style()->shapePadding();
+    float shapeImageThreshold = m_renderer->style()->shapeImageThreshold();
     const ShapeValue* shapeValue = this->shapeValue();
     ASSERT(shapeValue);
 
@@ -55,7 +56,7 @@ const Shape* ShapeInfo<RenderType>::computedShape() const
         break;
     case ShapeValue::Image:
         ASSERT(shapeValue->image());
-        m_shape = Shape::createShape(shapeValue->image(), 0, m_shapeLogicalSize, writingMode, margin, padding);
+        m_shape = Shape::createShape(shapeValue->image(), shapeImageThreshold, m_shapeLogicalSize, writingMode, margin, padding);
         break;
     default:
         ASSERT_NOT_REACHED();
