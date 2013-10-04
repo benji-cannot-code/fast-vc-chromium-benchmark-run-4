a5448c92a50e1aab0598c1e138ddb85fdfa1213e
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-04 03:33:23 +0000

    Revert 158860 "[blink] Add TextDecorationType enum"
    Broke Linux build: http://build.chromium.org/p/chromium.webkit/builders/Linux%20Tests/builds/31371/steps/compile/logs/stdio
    
    > [blink] Add TextDecorationType enum
    > 
    > This patch splits off a part of TextCheckingType enum into
    > TextDecorationType.
    > 
    > After this patch, TextCheckingType enum denotes the types of spellcheck
    > that Blink can request from the browser.
    > 
    > The browser uses the TextDecorationType enum to indicate the types of text
    > decorations that Blink needs to store in text. Some of these decorations
    > are visible (spelling and grammar). Other decorations are invisible (in
    > custom spellcheck dictionary).
    > 
    > BUG=302807
    > 
    > Review URL: https://codereview.chromium.org/25740003
    
    TBR=rouslan@chromium.org
    
    Review URL: https://codereview.chromium.org/25577007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158869 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/text/TextChecking.h
+++ third_party/WebKit/Source/core/platform/text/TextChecking.h
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef TextChecking_h
 #define TextChecking_h
 
-#include "core/platform/text/TextDecoration.h"
 #include "wtf/RefCounted.h"
 #include "wtf/Vector.h"
 #include "wtf/text/WTFString.h"
@@ -40,9 +39,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 enum TextCheckingType {
-    TextCheckingTypeNone     = 0,
-    TextCheckingTypeSpelling = 1 << 1,
-    TextCheckingTypeGrammar  = 1 << 2,
+    TextCheckingTypeNone                         = 0,
+    TextCheckingTypeSpelling                     = 1 << 1,
+    TextCheckingTypeGrammar                      = 1 << 2,
+    TextCheckingTypeInCustomSpellcheckDictionary = 1 << 3,
 };
 
 typedef unsigned TextCheckingTypeMask;
@@ -60,7 +60,7 @@ struct GrammarDetail {
 };
 
 struct TextCheckingResult {
-    TextDecorationType decoration;
+    TextCheckingType type;
     int location;
     int length;
     Vector<GrammarDetail> details;
