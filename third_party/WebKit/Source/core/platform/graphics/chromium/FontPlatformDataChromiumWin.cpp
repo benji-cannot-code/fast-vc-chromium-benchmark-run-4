893f28c4d501e0c9b8119be90a0acf92c983c9d7
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-04 05:06:26 +0000

    Mark HFONT FontPlatformData constructor as GDI only
    Add ENABLE(GDI_FONTS_ON_WINDOWS) ifdef around
    FontPlatformData(HFONT) constructor.
    
    BUG=251355
    R=eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/25891002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158872 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/graphics/chromium/FontPlatformDataChromiumWin.cpp
+++ third_party/WebKit/Source/core/platform/graphics/chromium/FontPlatformDataChromiumWin.cpp
@@ -194,6 +194,7 @@ FontPlatformData::FontPlatformData()
 {
 }
 
+#if ENABLE(GDI_FONTS_ON_WINDOWS)
 FontPlatformData::FontPlatformData(HFONT font, float size, FontOrientation orientation)
     : m_font(RefCountedHFONT::create(font))
     , m_textSize(size)
@@ -205,6 +206,7 @@ FontPlatformData::FontPlatformData(HFONT font, float size, FontOrientation orien
     , m_isHashTableDeletedValue(false)
 {
 }
+#endif
 
 // FIXME: this constructor is needed for SVG fonts but doesn't seem to do much
 FontPlatformData::FontPlatformData(float size, bool bold, bool oblique)
