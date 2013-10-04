7e543fc758ebdcb8d2447cd7597dbbcf872e409c
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-04 01:44:26 +0000

    Track successful restart attempts of Local Discovery.
    BUG=295476
    
    Review URL: https://codereview.chromium.org/25568005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226928 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/local_discovery/service_discovery_client_mdns.h
+++ chrome/browser/local_discovery/service_discovery_client_mdns.h
@@ -45,6 +45,7 @@ class ServiceDiscoveryClientMdns
   virtual ~ServiceDiscoveryClientMdns();
   void ScheduleStartNewClient();
   void StartNewClient();
+  void ReportSuccess();
 
   scoped_refptr<ServiceDiscoveryHostClient> host_client_;
   int restart_attempts_;
