7259b8983abc38a2e34bce940d142f37fb9984d9
mvujovic@adobe.com <mvujovic@adobe.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-04 21:49:38 +0000

    Factor TextureMap and Texture class out of resource_provider_unittest.cc and into TestWebGraphicsContext3D
    So that in a future patch, TestWebGraphicsContext3D will be able to store texture parameters.
    
    Review URL: https://codereview.chromium.org/23895005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227097 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/debug/test_web_graphics_context_3d.h
+++ cc/debug/test_web_graphics_context_3d.h
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/lock.h"
 #include "cc/base/cc_export.h"
 #include "cc/debug/fake_web_graphics_context_3d.h"
+#include "cc/debug/ordered_texture_map.h"
+#include "cc/debug/test_texture.h"
 #include "cc/output/context_provider.h"
 #include "third_party/khronos/GLES2/gl2.h"
 
@@ -74,6 +76,7 @@ class CC_EXPORT TestWebGraphicsContext3D : public FakeWebGraphicsContext3D {
   virtual WebKit::WebGLId createRenderbuffer();
   virtual WebKit::WebGLId createShader(WebKit::WGC3Denum);
   virtual WebKit::WebGLId createTexture();
+  virtual WebKit::WebGLId createExternalTexture();
 
   virtual void deleteBuffer(WebKit::WebGLId id);
   virtual void deleteFramebuffer(WebKit::WebGLId id);
@@ -211,6 +214,20 @@ class CC_EXPORT TestWebGraphicsContext3D : public FakeWebGraphicsContext3D {
   void SetMaxTransferBufferUsageBytes(size_t max_transfer_buffer_usage_bytes);
 
  protected:
+  struct TextureTargets {
+    TextureTargets();
+    ~TextureTargets();
+
+    void BindTexture(WebKit::WGC3Denum target, WebKit::WebGLId id);
+    void UnbindTexture(WebKit::WebGLId id);
+
+    WebKit::WebGLId BoundTexture(WebKit::WGC3Denum target);
+
+   private:
+    typedef base::hash_map<WebKit::WGC3Denum, WebKit::WebGLId> TargetTextureMap;
+    TargetTextureMap bound_textures_;
+  };
+
   struct Buffer {
     Buffer();
     ~Buffer();
@@ -241,9 +258,9 @@ class CC_EXPORT TestWebGraphicsContext3D : public FakeWebGraphicsContext3D {
     unsigned next_buffer_id;
     unsigned next_image_id;
     unsigned next_texture_id;
-    std::vector<WebKit::WebGLId> textures;
     base::ScopedPtrHashMap<unsigned, Buffer> buffers;
     base::ScopedPtrHashMap<unsigned, Image> images;
+    OrderedTextureMap textures;
 
    private:
     friend class base::RefCountedThreadSafe<Namespace>;
@@ -258,6 +275,7 @@ class CC_EXPORT TestWebGraphicsContext3D : public FakeWebGraphicsContext3D {
   void CallAllSyncPointCallbacks();
   void SwapBuffersComplete();
   void CreateNamespace();
+  WebKit::WebGLId BoundTextureId(WebKit::WGC3Denum target);
 
   unsigned context_id_;
   Attributes attributes_;
@@ -279,6 +297,7 @@ class CC_EXPORT TestWebGraphicsContext3D : public FakeWebGraphicsContext3D {
   int height_;
 
   unsigned bound_buffer_;
+  TextureTargets texture_targets_;
 
   scoped_refptr<Namespace> namespace_;
   static Namespace* shared_namespace_;
