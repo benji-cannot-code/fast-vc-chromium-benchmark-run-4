779c25d11c2fb830369d77f89cf72f80319e7f92
Tomasz Mikolajewski <mtomasz@chromium.org>
2013-10-04 17:22:07 +0900

    Initial upload of the Camera app.
    This patch uploads the Camera app, previously developed on:
    https://github.com/GoogleChrome/camera-app.
    
    BUG=294962
    TEST=run_all_tests.py
    
    Change-Id: Ib1e042b1f512f71b3d8cd772ddceff1dda806673

---
+++ chrome/browser/resources/chromeos/camera/src/js/effect.js
@@ -0,0 +1,55 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+/**
+ * Namespace for the Camera app.
+ */
+var camera = camera || {};
+
+/**
+ * @param {camera.Tracker} tracker Head tracker object.
+ * @constructor
+ */
+camera.Effect = function(tracker) {
+  /**
+   * @type {camera.Tracker}
+   * @private
+   */
+  this.tracker_ = tracker;
+};
+
+/**
+ * Randomizes the effect values.
+ */
+camera.Effect.prototype.randomize = function() {
+};
+
+/**
+ * Filters the passed frame with the effect.
+ * @param {HTMLCanvasElement} canvas Canvas element to be filtered.
+ */
+camera.Effect.prototype.filterFrame = function(canvas) {
+};
+
+/**
+ * Provides title of the effect.
+ * @return {string} Title.
+ */
+camera.Effect.prototype.getTitle = function() {
+  return '(Effect)';
+};
+
+/**
+ * Returns true if the effect is slow and preview should be done in lower
+ * resolution to keep acceptable FPS.
+ *
+ * @return {boolean} True if slow, false otherwise.
+ */
+camera.Effect.prototype.isSlow = function() {
+  return false;
+};
+
