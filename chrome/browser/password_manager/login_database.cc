50086665c25c0f1cc0541c2f122faf6cdcd80843
yfriedman@chromium.org <yfriedman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-04 12:00:16 +0000

    Add metrics for PSL usage.
    1) Record histogram of how often password form list contains an entry
    that came from matching PSL
    2) Add a user type selection when the user selects an entry from PSL.
    
    BUG=301511
    
    Review URL: https://codereview.chromium.org/25581004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226993 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/password_manager/login_database.cc
+++ chrome/browser/password_manager/login_database.cc
@@ -49,6 +49,16 @@ enum LoginTableColumns {
   COLUMN_FORM_DATA
 };
 
+// Enum used for histogram tracking PSL Domain triggering.
+// New entries should only be added to the end of the enum (before *_COUNT) so
+// as to not disrupt existing data.
+enum PSLDomainMatchMetric {
+  PSL_DOMAIN_MATCH_DISABLED = 0,
+  PSL_DOMAIN_MATCH_NONE,
+  PSL_DOMAIN_MATCH_FOUND,
+  PSL_DOMAIN_MATCH_COUNT
+};
+
 // Using the public suffix list for matching the origin is only needed for
 // websites that do not have a single hostname for entering credentials. It
 // would be better for their users if they did, but until then we help them find
@@ -475,6 +485,7 @@ bool LoginDatabase::GetLogins(const PasswordForm& form,
   sql::Statement s;
   const GURL signon_realm(form.signon_realm);
   std::string registered_domain = GetRegistryControlledDomain(signon_realm);
+  PSLDomainMatchMetric psl_domain_match_metric = PSL_DOMAIN_MATCH_NONE;
   if (public_suffix_domain_matching_ &&
       ShouldPSLDomainMatchingApply(registered_domain)) {
     // We are extending the original SQL query with one that includes more
@@ -506,6 +517,7 @@ bool LoginDatabase::GetLogins(const PasswordForm& form,
     s.BindString(0, form.signon_realm);
     s.BindString(1, regexp);
   } else {
+    psl_domain_match_metric = PSL_DOMAIN_MATCH_DISABLED;
     s.Assign(db_.GetCachedStatement(SQL_FROM_HERE, sql_query.c_str()));
     s.BindString(0, form.signon_realm);
   }
@@ -526,6 +538,7 @@ bool LoginDatabase::GetLogins(const PasswordForm& form,
         continue;
       }
       if (form.signon_realm != new_form->signon_realm) {
+        psl_domain_match_metric = PSL_DOMAIN_MATCH_FOUND;
         // This is not a perfect match, so we need to create a new valid result.
         // We do this by copying over origin, signon realm and action from the
         // observed form and setting the original signon realm to what we found
@@ -540,6 +553,9 @@ bool LoginDatabase::GetLogins(const PasswordForm& form,
     }
     forms->push_back(new_form.release());
   }
+  UMA_HISTOGRAM_ENUMERATION("PasswordManager.PslDomainMatchTriggering",
+                            psl_domain_match_metric,
+                            PSL_DOMAIN_MATCH_COUNT);
   return s.Succeeded();
 }
 
