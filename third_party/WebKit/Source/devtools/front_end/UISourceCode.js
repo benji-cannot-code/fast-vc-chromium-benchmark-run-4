a183ec9f8030649daed25de2e02b3b2734454d24
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-04 13:57:30 +0000

    DevTools: Get rid of styleFile on UISourceCode
    R=pfeldman
    
    Review URL: https://codereview.chromium.org/25879002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158931 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/UISourceCode.js
+++ third_party/WebKit/Source/devtools/front_end/UISourceCode.js
@@ -222,22 +222,6 @@ WebInspector.UISourceCode.prototype = {
     },
 
     /**
-     * @return {WebInspector.StyleFile}
-     */
-    styleFile: function()
-    {
-        return this._styleFile;
-    },
-
-    /**
-     * @param {WebInspector.StyleFile} styleFile
-     */
-    setStyleFile: function(styleFile)
-    {
-        this._styleFile = styleFile;
-    },
-
-    /**
      * @return {WebInspector.Project}
      */
     project: function()
