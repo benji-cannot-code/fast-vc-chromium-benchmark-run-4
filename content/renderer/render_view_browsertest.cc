14050e242bdba31849f8b1f18d8342c3a3860836
justinlin@chromium.org <justinlin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-04 15:50:18 +0000

    Disable RenderViewImplTest.GetCompositionCharacterBoundsTest for XP.
    BUG=304193
    TBR=haraken
    
    Review URL: https://codereview.chromium.org/25418003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227005 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_view_browsertest.cc
+++ content/renderer/render_view_browsertest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/shared_memory.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/win/windows_version.h"
 #include "content/common/ssl_status_serialization.h"
 #include "content/common/view_messages.h"
 #include "content/public/browser/native_web_keyboard_event.h"
@@ -1761,6 +1762,13 @@ TEST_F(RenderViewImplTest, TestBackForward) {
 
 #if defined(OS_MACOSX) || defined(OS_WIN) || defined(USE_AURA)
 TEST_F(RenderViewImplTest, GetCompositionCharacterBoundsTest) {
+
+#if defined(OS_WIN)
+  // http://crbug.com/304193
+  if (base::win::GetVersion() < base::win::VERSION_VISTA)
+    return;
+#endif
+
   LoadHTML("<textarea id=\"test\"></textarea>");
   ExecuteJavaScript("document.getElementById('test').focus();");
 
