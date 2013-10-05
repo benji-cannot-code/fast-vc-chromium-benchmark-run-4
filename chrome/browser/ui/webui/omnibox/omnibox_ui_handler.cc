d8bdff2c6cffe9dfd9a79e032e463a19ee66f9e6
jered@chromium.org <jered@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-05 00:17:11 +0000

    omnibox: Nuke the InstantExtended provider list.
    InstantExtended is launched by default, so it doesn't make sense to keep
    a "separate" provider list for it.
    
    BUG=252892
    TEST=none
    
    Review URL: https://codereview.chromium.org/25927002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227146 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/omnibox/omnibox_ui_handler.cc
+++ chrome/browser/ui/webui/omnibox/omnibox_ui_handler.cc
@@ -206,7 +206,5 @@ void OmniboxUIHandler::StartOmniboxQuery(const base::ListValue* input) {
 
 void OmniboxUIHandler::ResetController() {
   controller_.reset(new AutocompleteController(profile_, this,
-      chrome::IsInstantExtendedAPIEnabled() ?
-          AutocompleteClassifier::kInstantExtendedOmniboxProviders :
           AutocompleteClassifier::kDefaultOmniboxProviders));
 }
