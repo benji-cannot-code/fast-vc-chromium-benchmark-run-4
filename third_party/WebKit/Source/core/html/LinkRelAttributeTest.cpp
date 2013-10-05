eb43498d36cfa3bbe4d3acf66c1d9a85f2b3007a
tfarina@chromium.org <tfarina@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-05 19:34:48 +0000

    Move more tests near their implementations.
    BUG=None
    TEST=None, no functional changes.
    R=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/26127002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158992 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/LinkRelAttributeTest.cpp
+++ third_party/WebKit/Source/core/html/LinkRelAttributeTest.cpp
@@ -30,9 +30,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
+#include "core/html/LinkRelAttribute.h"
 
 #include <gtest/gtest.h>
-#include "core/html/LinkRelAttribute.h"
 
 using namespace WebCore;
 
--- third_party/WebKit/Source/core/platform/PODRedBlackTreeTest.cpp
+++ third_party/WebKit/Source/core/platform/PODRedBlackTreeTest.cpp
@@ -29,8 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "core/platform/PODRedBlackTree.h"
 
-#include "ArenaTestHelpers.h"
-#include "TreeTestHelpers.h"
+#include "core/tests/ArenaTestHelpers.h"
+#include "core/tests/TreeTestHelpers.h"
 #include "wtf/Vector.h"
 
 #include <gtest/gtest.h>
