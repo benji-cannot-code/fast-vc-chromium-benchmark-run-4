10edd9eeb15aa0e06a8f811acb859aaab7af64cd
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-05 05:41:54 +0000

    aura: Set max pending frames to the default instead of 1 with ubercomp.
    R=jbauman, piman
    BUG=286548
    
    Review URL: https://codereview.chromium.org/26069002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227195 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/gpu/delegated_compositor_output_surface.cc
+++ content/renderer/gpu/delegated_compositor_output_surface.cc
@@ -18,7 +18,6 @@ DelegatedCompositorOutputSurface::DelegatedCompositorOutputSurface(
                               software.Pass(),
                               true) {
   capabilities_.delegated_rendering = true;
-  capabilities_.max_frames_pending = 1;
 }
 
 }  // namespace content
