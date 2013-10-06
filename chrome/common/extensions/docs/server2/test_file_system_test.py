db10c69a66af403b97db1a2506391bad10b09ee9
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-06 23:41:00 +0000

    Docserver: Properly qualify sidebar paths so that _patch/12345667 links are maintained. Make the SidenavDataSource tests slightly more sensible.
    BUG=304302
    R=jyasskin@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/26080002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227232 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/server2/test_file_system_test.py
+++ chrome/common/extensions/docs/server2/test_file_system_test.py
@@ -6,8 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 from copy import deepcopy
 from file_system import FileNotFoundError, StatInfo
-from test_file_system import TestFileSystem
+from test_file_system import TestFileSystem, _MoveTo
 import unittest
+
 
 _TEST_DATA = {
   '404.html': '404.html contents',
@@ -24,10 +25,12 @@ _TEST_DATA = {
   }
 }
 
+
 def _Get(fn):
   '''Returns a function which calls Future.Get on the result of |fn|.
   '''
   return lambda *args: fn(*args).Get()
+
 
 class TestFileSystemTest(unittest.TestCase):
   def testEmptyFileSystem(self):
@@ -115,11 +118,12 @@ class TestFileSystemTest(unittest.TestCase):
 
   def testMoveTo(self):
     self.assertEqual({'foo': {'a': 'b', 'c': 'd'}},
-                    TestFileSystem.MoveTo('foo', {'a': 'b', 'c': 'd'}))
+                     _MoveTo('foo', {'a': 'b', 'c': 'd'}))
     self.assertEqual({'foo': {'bar': {'a': 'b', 'c': 'd'}}},
-                    TestFileSystem.MoveTo('foo/bar', {'a': 'b', 'c': 'd'}))
+                     _MoveTo('foo/bar', {'a': 'b', 'c': 'd'}))
     self.assertEqual({'foo': {'bar': {'baz': {'a': 'b'}}}},
-                    TestFileSystem.MoveTo('foo/bar/baz', {'a': 'b'}))
+                     _MoveTo('foo/bar/baz', {'a': 'b'}))
+
 
 if __name__ == '__main__':
   unittest.main()
