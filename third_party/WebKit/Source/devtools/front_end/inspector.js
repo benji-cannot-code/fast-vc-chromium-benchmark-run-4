e98b60a5b0b091843392ec2004eee67e7672ddaf
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-07 13:22:56 +0000

    DevTools: don't fail on opening devtools when there is no screencast available.
    R=apavlov@chromium.org
    
    Review URL: https://codereview.chromium.org/26246002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159027 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/inspector.js
+++ third_party/WebKit/Source/devtools/front_end/inspector.js
@@ -132,6 +132,8 @@ var WebInspector = {
      */
     _toggleScreencastButtonClicked: function(resizeWindow)
     {
+        if (!this._screencastAvailable())
+            return;
         this._toggleScreencastButton.toggled = !this._toggleScreencastButton.toggled;
         WebInspector.settings.screencastEnabled.set(this._toggleScreencastButton.toggled);
 
