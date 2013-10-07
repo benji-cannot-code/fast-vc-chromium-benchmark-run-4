0432446125d453010e0ef00a20c55a5bc8f75850
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-07 20:17:16 +0000

    Add inherited libs and lib_dirs to GN.
    The libs and lib_dirs are pushed up the tree until they reach a shared library or executable. Additionally, I added prefixes in the toolchain to specify these.
    
    This converts ldflags to be per-target rather than inherited which was previously used to implement this feature.
    
    Rename includes to include_dirs for consistency with lib_dirs.
    
    Remove the sill swap_in_* inline functions and just do foo().swap().
    
    BUG=
    R=scottmg@chromium.org
    
    Review URL: https://codereview.chromium.org/26074002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227328 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/gn/ninja_binary_target_writer.h
+++ tools/gn/ninja_binary_target_writer.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "tools/gn/ninja_target_writer.h"
+#include "tools/gn/toolchain.h"
 
 // Writes a .ninja file for a binary target type (an executable, a shared
 // library, or a static library).
@@ -22,11 +23,14 @@ class NinjaBinaryTargetWriter : public NinjaTargetWriter {
   void WriteCompilerVars();
   void WriteSources(std::vector<OutputFile>* object_files);
   void WriteLinkerStuff(const std::vector<OutputFile>& object_files);
+  void WriteLinkerFlags();
 
   // Writes the build line for linking the target. Includes newline.
   void WriteLinkCommand(const OutputFile& external_output_file,
                         const OutputFile& internal_output_file,
                         const std::vector<OutputFile>& object_files);
+
+  Toolchain::ToolType tool_type_;
 
   DISALLOW_COPY_AND_ASSIGN(NinjaBinaryTargetWriter);
 };
