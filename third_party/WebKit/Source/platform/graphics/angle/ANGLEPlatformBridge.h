f57b8b5f8d5868045837842c99eb7734c60d05f8
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-07 17:20:10 +0000

    Make check-blink-deps pass
    This CL cleans up our DEPS files so that check-blink-deps passes.
    
    I've also fixed a bug in PRESUBMIT.py that was preventing the DEPS check
    from actually running!
    
    R=eseidel@chromium.org, jochen@chromium.org
    
    Review URL: https://codereview.chromium.org/26232002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159032 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/angle/ANGLEPlatformBridge.h
+++ third_party/WebKit/Source/platform/graphics/angle/ANGLEPlatformBridge.h
@@ -27,11 +27,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ANGLEPlatformBridge_h
 #define ANGLEPlatformBridge_h
 
-#include "GLSLANG/ShaderLang.h"
-
 #include "platform/PlatformExport.h"
 #include "wtf/text/CString.h"
 #include "wtf/text/WTFString.h"
+#include <GLSLANG/ShaderLang.h>
 
 namespace WebCore {
 
