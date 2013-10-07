ee426ba1172d8c1331311f195b4124f1b8d38885
hirono@chromium.org <hirono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-07 08:50:05 +0000

    Originally the initialization of window button's is done in FileManager class. To keep FileManager class small, this CL moves the initialization to the UI class.
    BUG=267281
    TEST=manually
    R=mtomasz@chromium.org
    
    Review URL: https://codereview.chromium.org/25992002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227265 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/ui/file_manager_ui.js
+++ chrome/browser/resources/file_manager/js/ui/file_manager_ui.js
@@ -9,15 +9,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * The root of the file manager's view managing the DOM of Files.app.
  *
  * @param {HTMLElement} element Top level element of Files.app.
+ * @param {DialogType} dialogType Dialog type.
  * @constructor.
  */
-var FileManagerUI = function(element) {
+var FileManagerUI = function(element, dialogType) {
   /**
    * Top level element of Files.app.
    * @type {HTMLElement}
    * @private
    */
   this.element_ = element;
+
+  /**
+   * Dialog type.
+   * @type {DialogType}
+   * @private
+   */
+  this.dialogType_ = dialogType;
 
   /**
    * Error dialog.
@@ -65,7 +73,7 @@ var FileManagerUI = function(element) {
 };
 
 /**
- * Initialize the dialogs.
+ * Initializes the dialogs.
  */
 FileManagerUI.prototype.initDialogs = function() {
   // Initialize the dialog label.
@@ -84,4 +92,51 @@ FileManagerUI.prototype.initDialogs = function() {
       new cr.filebrowser.DefaultActionDialog(this.element_);
   this.suggestAppsDialog = new SuggestAppsDialog(
       this.element_, appState.suggestAppsDialogState || {});
+};
+
+/**
+ * Initializes the window buttons.
+ */
+FileManagerUI.prototype.initWindowButtons = function() {
+  // Do not maximize/close when running via chrome://files in a browser.
+  if (this.dialogType_ !== DialogType.FULL_PAGE ||
+      util.platform.runningInBrowser())
+    return;
+
+  // This is to prevent the buttons from stealing focus on mouse down.
+  var preventFocus = function(event) {
+    event.preventDefault();
+  };
+
+  var maximizeButton = this.element_.querySelector('#maximize-button');
+  maximizeButton.addEventListener('click', this.onMaximize_.bind(this));
+  maximizeButton.addEventListener('mousedown', preventFocus);
+
+  var closeButton = this.element_.querySelector('#close-button');
+  closeButton.addEventListener('click', this.onClose_.bind(this));
+  closeButton.addEventListener('mousedown', preventFocus);
+};
+
+/**
+ * Handles maximize button click.
+ * @private
+ */
+FileManagerUI.prototype.onMaximize_ = function() {
+  var appWindow = chrome.app.window.current();
+  if (appWindow.isMaximized())
+    appWindow.restore();
+  else
+    appWindow.maximize();
+};
+
+/**
+ * Hanldes close button click.
+ * @private
+ */
+FileManagerUI.prototype.onClose_ = function() {
+  // Do not close when running via chrome://files in a browser.
+  if (util.platform.runningInBrowser())
+    return;
+
+  window.close();
 };
