09c5f44c9def10ac02eeec11e6f8ee6fb6be58b6
yosin@chromium.org <yosin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-07 06:09:19 +0000

    Notify nodes removal to Range/Selection after dispatching blur and mutation event
    This patch changes notifying nodes removal to Range/Selection after dispatching blur and mutation event. In willRemoveChildren(), like willRemoveChild(); r115686 did same change, although it didn't change willRemoveChildren().
    
    The issue 295010, use-after-free, is caused by setting removed node to Selection in mutation event handler.
    
    BUG=295010
    TEST=LayoutTests/fast/dom/Range/range-created-during-remove-children.html, LayoutTests/editing/selection/selection-change-in-mutation-event-by-remove-children.html, LayoutTests/editing/selection/selection-change-in-blur-event-by-remove-children.html
    R=tkent@chromium.org
    
    Review URL: https://codereview.chromium.org/25389004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159007 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/ContainerNode.cpp
+++ third_party/WebKit/Source/core/dom/ContainerNode.cpp
@@ -403,8 +403,6 @@ static void willRemoveChildren(ContainerNode* container)
     NodeVector children;
     getChildNodes(container, children);
 
-    container->document().nodeChildrenWillBeRemoved(container);
-
     ChildListMutationScope mutation(container);
     for (NodeVector::const_iterator it = children.begin(); it != children.end(); it++) {
         Node* child = it->get();
@@ -546,6 +544,8 @@ void ContainerNode::removeChildren()
         // state of a child.
         document().removeFocusedElementOfSubtree(this, true);
     }
+
+    document().nodeChildrenWillBeRemoved(this);
 
     NodeVector removedChildren;
     {
