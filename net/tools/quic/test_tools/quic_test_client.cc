186d29283f052f5f8915b5b0d8d4d9c7bfa1e019
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-07 23:10:47 +0000

    Revert 227365 "Add better help information to quic_client and qu..."
    > Add better help information to quic_client and quic_server.
    > Also, modify the quic_client to print out the response contents.
    > 
    > Merge internal change: 53403247
    > 
    > Review URL: https://codereview.chromium.org/25043005
    
    TBR=rch@chromium.org
    
    Review URL: https://codereview.chromium.org/26382002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227373 0039d316-1c4b-4281-b951-d872f2087c98

--- net/tools/quic/test_tools/quic_test_client.cc
+++ net/tools/quic/test_tools/quic_test_client.cc
@@ -99,7 +99,7 @@ class QuicEpollClient : public QuicClient {
   QuicEpollClient(IPEndPoint server_address,
              const string& server_hostname,
              const QuicVersion version)
-      : Super(server_address, server_hostname, version, false) {
+      : Super(server_address, server_hostname, version) {
   }
 
   QuicEpollClient(IPEndPoint server_address,
