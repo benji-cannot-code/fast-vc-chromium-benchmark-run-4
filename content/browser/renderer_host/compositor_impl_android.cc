30be5f4c98755cabb2f670a9ca4843a325201798
sievers@chromium.org <sievers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-07 23:41:39 +0000

    Android: Remove unneeded Compositor::SetNeedsRedraw()
    Was used in the old WebView mode, not needed anymore.
    
    NOTRY=True
    
    Review URL: https://codereview.chromium.org/26374002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227387 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/compositor_impl_android.cc
+++ content/browser/renderer_host/compositor_impl_android.cc
@@ -170,11 +170,6 @@ CompositorImpl::~CompositorImpl() {
   SetSurface(NULL);
 }
 
-void CompositorImpl::SetNeedsRedraw() {
-  if (host_)
-    host_->SetNeedsRedraw();
-}
-
 void CompositorImpl::Composite() {
   if (host_)
     host_->Composite(base::TimeTicks::Now());
