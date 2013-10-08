7b914aee2d7c9981852eb0091343fb42d538ab4b
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-08 23:37:52 +0000

    Change InternalSettings to reset overriden default fonts
    Change the InternalSettings::Backup::restoreTo to reset the font
    family mappings as a part of the restore option.
    
    This fixes the flakiness that occurs when a pixel test with a matching
    script is executed after a test that overrides the default font
    family for that script.
    
    BUG=305018
    R=leviw@chromium.org
    
    Review URL: https://codereview.chromium.org/26122004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159167 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/Settings.cpp
+++ third_party/WebKit/Source/core/page/Settings.cpp
@@ -282,6 +282,17 @@ void Settings::setMediaTypeOverride(const String& mediaTypeOverride)
     m_page->setNeedsRecalcStyleInAllFrames();
 }
 
+void Settings::resetFontFamilies()
+{
+    m_standardFontFamilyMap.clear();
+    m_serifFontFamilyMap.clear();
+    m_fixedFontFamilyMap.clear();
+    m_sansSerifFontFamilyMap.clear();
+    m_cursiveFontFamilyMap.clear();
+    m_fantasyFontFamilyMap.clear();
+    m_pictographFontFamilyMap.clear();
+}
+
 void Settings::setLoadsImagesAutomatically(bool loadsImagesAutomatically)
 {
     m_loadsImagesAutomatically = loadsImagesAutomatically;
