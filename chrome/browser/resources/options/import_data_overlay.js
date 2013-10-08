0b43b5d844fc3bddac4ba489cf7f4f0b467bb4e8
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-08 01:20:43 +0000

    Fix regression where all checkboxes are disabled on "Import Bookmarks ..." dialog.
    This regression happened because of a typo committed in r224933, at line 235
    of this file. In that revision |state| parameter was renamed to |importing| in
    setImportingState() function for clarity, but not all references were updated,
    we have missed one when setting the disable state of the checkboxes.
    
    BUG=299508
    TEST=
    1.Launch chrome and navigate to chrome://settings.
    2.Click on 'Import bookmark and settings' button under 'Users' section.
    3.Select 'Mozilla firefox' from dropdown and click On 'Import button'.
    4.Now Click on 'Skip import' button.
    5.Repeat step 2 and Observe the checkboxes.
    They were "disabled", now this fixes the behavior to make them "enabled" again.
    
    R=dubroy@chromium.org
    
    Review URL: https://codereview.chromium.org/26123002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227419 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/import_data_overlay.js
+++ chrome/browser/resources/options/import_data_overlay.js
@@ -233,7 +233,7 @@ cr.define('options', function() {
     var checkboxes =
         document.querySelectorAll('#import-checkboxes input[type=checkbox]');
     for (var i = 0; i < checkboxes.length; i++)
-        checkboxes[i].setDisabled('Importing', state);
+        checkboxes[i].setDisabled('Importing', importing);
     if (!importing)
       ImportDataOverlay.getInstance().updateCheckboxes_();
     $('import-browsers').disabled = importing;
