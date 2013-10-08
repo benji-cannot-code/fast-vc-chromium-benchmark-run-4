72b093b6ca95ef2ce5d8ed06220b1cc81f70e9e5
hajimehoshi@chromium.org <hajimehoshi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-08 06:49:46 +0000

    Add ComboboxModel::IsItemEnabledAt
    This CL enables to switch the enabling/disabling state of menu item of a Combobox.
    
    Concretely, this feature is needed by new Translate UI. Actually, the Translate current infobar has this feature. For example, the Translate UI has the source language and the target language selectors as Comboboxes, and when English is selected as the source language, English in the target language selector should be disabled.
    
    BUG=276181
    
    Review URL: https://codereview.chromium.org/26219002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227476 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/models/combobox_model.cc
+++ ui/base/models/combobox_model.cc
@@ -15,4 +15,8 @@ int ComboboxModel::GetDefaultIndex() const {
   return 0;
 }
 
+bool ComboboxModel::IsItemEnabledAt(int index) const {
+  return true;
+}
+
 }  // namespace ui
--- ui/views/controls/combobox/combobox.cc
+++ ui/views/controls/combobox/combobox.cc
@@ -69,13 +69,14 @@ class InvalidBackground : public Background {
 };
 
 // Returns the next or previous valid index (depending on |increment|'s value).
-// Skips separator indices. Returns -1 if there is no valid adjacent index.
+// Skips separator or disabled indices. Returns -1 if there is no valid adjacent
+// index.
 int GetAdjacentIndex(ui::ComboboxModel* model, int increment, int index) {
   DCHECK(increment == -1 || increment == 1);
 
   index += increment;
   while (index >= 0 && index < model->GetItemCount()) {
-    if (!model->IsItemSeparatorAt(index))
+    if (!model->IsItemSeparatorAt(index) || !model->IsItemEnabledAt(index))
       return index;
     index += increment;
   }
@@ -153,15 +154,11 @@ bool Combobox::IsItemChecked(int id) const {
 }
 
 bool Combobox::IsCommandEnabled(int id) const {
-  return true;
+  return model()->IsItemEnabledAt(MenuCommandToIndex(id));
 }
 
 void Combobox::ExecuteCommand(int id) {
-  // (note that the id received is offset by kFirstMenuItemId)
-  // Revert menu ID offset to map back to combobox model.
-  id -= kFirstMenuItemId;
-  DCHECK_LT(id, model()->GetItemCount());
-  selected_index_ = id;
+  selected_index_ = MenuCommandToIndex(id);
   OnSelectionChanged();
 }
 
@@ -457,6 +454,14 @@ void Combobox::OnSelectionChanged() {
     listener_->OnSelectedIndexChanged(this);
   NotifyAccessibilityEvent(ui::AccessibilityTypes::EVENT_VALUE_CHANGED, false);
   SchedulePaint();
+}
+
+int Combobox::MenuCommandToIndex(int menu_command_id) const {
+  // (note that the id received is offset by kFirstMenuItemId)
+  // Revert menu ID offset to map back to combobox model.
+  int index = menu_command_id - kFirstMenuItemId;
+  DCHECK_LT(index, model()->GetItemCount());
+  return index;
 }
 
 }  // namespace views
