4adc74d5c91640bb3af3c3611fd1eca1ee64ea22
jennyz@chromium.org <jennyz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-08 20:43:01 +0000

    Fix the memory issue in cras_audio_client_impl.cc.
    BUG=298367
    TBR=stevenjb
    
    Review URL: https://codereview.chromium.org/26487005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227582 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/dbus/cras_audio_client_stub_impl.cc
+++ chromeos/dbus/cras_audio_client_stub_impl.cc
@@ -7,7 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromeos {
 
-CrasAudioClientStubImpl::CrasAudioClientStubImpl() {}
+CrasAudioClientStubImpl::CrasAudioClientStubImpl()
+    : active_input_node_id_(0),
+      active_output_node_id_(0) {
+}
 
 void CrasAudioClientStubImpl::Init(dbus::Bus* bus) {
   VLOG(1) << "CrasAudioClientStubImpl is created";
