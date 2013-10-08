0b560ff9e49c8067d827debaffba539b9932f421
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-08 02:19:04 +0000

    Minor window_sizer_ash cleanups  * remove unnecessary code  * move ash specific piece to ash/wm/window_positioner  * add separate if block for session_restore path.
    Next step: I'll move window_sizer_ash logic to ash/wm/window_positoner
    
    BUG=272460
    R=jamescook@chromium.org, sky@chromium.org, varkha@chromium.org
    
    Review URL: https://codereview.chromium.org/25453009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227432 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/window_sizer/window_sizer_ash_unittest.cc
+++ chrome/browser/ui/window_sizer/window_sizer_ash_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/test/ash_test_base.h"
 #include "ash/test/test_shell_delegate.h"
+#include "ash/wm/window_positioner.h"
 #include "ash/wm/window_resizer.h"
 #include "ash/wm/window_state.h"
 #include "base/compiler_specific.h"
@@ -757,7 +758,7 @@ TEST_F(WindowSizerAshTest, TestShowState) {
 
   // In smaller screen resolutions we default to maximized if there is no other
   // window visible.
-  int min_size = WindowSizer::GetForceMaximizedWidthLimit() / 2;
+  int min_size = ash::WindowPositioner::GetForceMaximizedWidthLimit() / 2;
   if (min_size > 0) {
     const gfx::Rect tiny_screen(0, 0, min_size, min_size);
     EXPECT_EQ(ui::SHOW_STATE_DEFAULT,
