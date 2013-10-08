cab6a9beab81ae8cc8a1af92e6d9e3c560d1be63
alancutter@chromium.org <alancutter@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-08 09:09:46 +0000

    Revert "Make compositingState explicit (re-land with bogus ASSERT removed)"
    This reverts commit 2a33ee04f3cb493d3081ef700fb12d6344c39cd4.
    
    This change is hitting
    "ASSERTION FAILED: layerChanged == compositingStateChanged"
    in 100+ debug tests.
    
    TBR=shawnsingh@chromium.org
    NOTRY=TRUE
    
    Review URL: https://codereview.chromium.org/26470002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159108 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderObject.h
+++ third_party/WebKit/Source/core/rendering/RenderObject.h
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/Position.h"
 #include "core/dom/StyleEngine.h"
 #include "core/fetch/ImageResourceClient.h"
-#include "core/rendering/CompositingState.h"
 #include "core/rendering/LayoutIndicator.h"
 #include "core/rendering/PaintPhase.h"
 #include "core/rendering/RenderObjectChildList.h"
@@ -700,7 +699,7 @@ public:
     virtual void addAnnotatedRegions(Vector<AnnotatedRegionValue>&);
     void collectAnnotatedRegions(Vector<AnnotatedRegionValue>&);
 
-    CompositingState compositingState() const;
+    bool isComposited() const;
 
     bool hitTest(const HitTestRequest&, HitTestResult&, const HitTestLocation& locationInContainer, const LayoutPoint& accumulatedOffset, HitTestFilter = HitTestAll);
     virtual void updateHitTestResult(HitTestResult&, const LayoutPoint&);
