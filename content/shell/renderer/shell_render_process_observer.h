ce7290a745f8154280180e887ddc9ce2a1c054ce
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-08 12:28:49 +0000

    Destroy the testing interfaces when shutting down WebKit
    When tearing down the interfaces, we call into V8 which is also shut
    down during WebKit shutdown.
    
    BUG=304324
    R=marja@chromium.org
    
    Review URL: https://codereview.chromium.org/26182003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227504 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/renderer/shell_render_process_observer.h
+++ content/shell/renderer/shell_render_process_observer.h
@@ -39,6 +39,7 @@ class ShellRenderProcessObserver : public RenderProcessObserver {
 
   // RenderProcessObserver implementation.
   virtual void WebKitInitialized() OVERRIDE;
+  virtual void OnRenderProcessShutdown() OVERRIDE;
   virtual bool OnControlMessageReceived(const IPC::Message& message) OVERRIDE;
 
   WebTestRunner::WebTestDelegate* test_delegate() const {
