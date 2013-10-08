220b9a7ffaf9d00b549c0108376ef1d2d7548b02
skuhne@chromium.org <skuhne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-08 18:40:46 +0000

    Adding flag for the "full multi profile mode" aka the Googler mode
    BUG=302518
    TEST=none
    
    Review URL: https://codereview.chromium.org/26489002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227554 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/ash_switches.h
+++ ash/ash_switches.h
@@ -45,6 +45,9 @@ ASH_EXPORT extern const char kAshEnableAdvancedGestures[];
 ASH_EXPORT extern const char kAshEnableAlternateFrameCaptionButtonStyle[];
 ASH_EXPORT extern const char kAshEnableBrightnessControl[];
 ASH_EXPORT extern const char kAshEnableDockedWindows[];
+#if defined(OS_CHROMEOS)
+ASH_EXPORT extern const char kAshEnableFullMultiProfileMode[];
+#endif
 ASH_EXPORT extern const char kAshEnableImmersiveFullscreen[];
 #if defined(OS_LINUX)
 ASH_EXPORT extern const char kAshEnableMemoryMonitor[];
@@ -87,6 +90,9 @@ ASH_EXPORT bool UseDragOffShelf();
 
 // Returns true if side shelf alignment is enabled.
 ASH_EXPORT bool ShowShelfAlignmentMenu();
+
+// Returns true if the full MultiProfile mode (M-31 version) is used.
+ASH_EXPORT bool UseFullMultiProfileMode();
 
 // Returns true if the MultiProfile shelf menu should be shown.
 ASH_EXPORT bool ShowMultiProfileShelfMenu();
