4bb1a54d783a73a32f8413942631863ee6ac2f2b
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-08 20:45:33 +0000

    Fix WeakPtrFactory member placement.
    WeakPtrFactory should always be declared last, see class comments on it.
    
    BUG=303818
    
    Review URL: https://codereview.chromium.org/26329003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227584 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/spellchecker/spellcheck_hunspell_dictionary.h
+++ chrome/browser/spellchecker/spellcheck_hunspell_dictionary.h
@@ -124,8 +124,6 @@ class SpellcheckHunspellDictionary
   // Used for downloading the dictionary file.
   scoped_ptr<net::URLFetcher> fetcher_;
 
-  base::WeakPtrFactory<SpellcheckHunspellDictionary> weak_ptr_factory_;
-
   SpellcheckService* spellcheck_service_;
 
   // Observers of Hunspell dictionary events.
@@ -136,6 +134,8 @@ class SpellcheckHunspellDictionary
 
   // Dictionary file path and descriptor.
   scoped_ptr<DictionaryFile> dictionary_file_;
+
+  base::WeakPtrFactory<SpellcheckHunspellDictionary> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(SpellcheckHunspellDictionary);
 };
