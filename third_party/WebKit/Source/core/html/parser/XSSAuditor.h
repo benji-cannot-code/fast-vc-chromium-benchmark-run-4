177c746b6bac14b10bdafc095def85fa8f0e752a
tsepez@chromium.org <tsepez@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-08 23:57:10 +0000

    XSSAuditor false positive on large script blocks.
    The snippet extraction logic is only valid for the start of a script block, therefore large scripts which may be passed to the auditor in multiple chunks must be processed in their entirety as was the first chunk.
    
    BUG=302797
    R=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/25726005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159172 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/parser/XSSAuditor.h
+++ third_party/WebKit/Source/core/html/parser/XSSAuditor.h
@@ -70,7 +70,9 @@ private:
 
     enum State {
         Uninitialized,
-        Initialized
+        FilteringTokens,
+        PermittingAdjacentCharacterTokens,
+        SuppressingAdjacentCharacterTokens
     };
 
     enum AttributeKind {
