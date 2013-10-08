191f09a3aa49f2848b17cfff0ac66487d589a8b8
qinmin@chromium.org <qinmin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-08 20:33:59 +0000

    Add a content switch to turn off unprefixed MediaSource API
    this is for ICS devices
    
    blink CL: https://codereview.chromium.org/26028003/
    
    BUG=304112
    
    Review URL: https://codereview.chromium.org/26015004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227577 0039d316-1c4b-4281-b951-d872f2087c98

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -25,6 +25,7 @@ static void SetRuntimeFeatureDefaultsForPlatform() {
   // in JellyBrean.
   if (base::android::BuildInfo::GetInstance()->sdk_int() < 16) {
     WebRuntimeFeatures::enableWebKitMediaSource(false);
+    WebRuntimeFeatures::enableMediaSource(false);
     WebRuntimeFeatures::enablePrefixedEncryptedMedia(false);
   }
 #endif  // !defined(GOOGLE_TV)
@@ -77,6 +78,9 @@ void SetRuntimeFeaturesDefaultsAndUpdateFromArgs(
 
   if (command_line.HasSwitch(switches::kDisableWebKitMediaSource))
     WebRuntimeFeatures::enableWebKitMediaSource(false);
+
+  if (command_line.HasSwitch(switches::kDisableUnprefixedMediaSource))
+    WebRuntimeFeatures::enableMediaSource(false);
 
 #if defined(OS_ANDROID)
   if (command_line.HasSwitch(switches::kDisableWebRTC)) {
