bbfea24e4865c05a2443e1dd154c959b009df066
ajuma@chromium.org <ajuma@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-08 04:03:02 +0000

    Define WebFilterAnimationCurve
    This defines WebFilterAnimationCurve, needed for sending
    filter animations from Blink to cc.
    
    BUG=181613
    
    Review URL: https://codereview.chromium.org/25345002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159082 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebCompositorSupport.h
+++ third_party/WebKit/public/platform/WebCompositorSupport.h
@@ -41,6 +41,7 @@ class WebContentLayer;
 class WebContentLayerClient;
 class WebExternalTextureLayer;
 class WebExternalTextureLayerClient;
+class WebFilterAnimationCurve;
 class WebFilterOperations;
 class WebFloatAnimationCurve;
 class WebGraphicsContext3D;
@@ -83,6 +84,8 @@ public:
     // Animation ----------------------------------------------------
 
     virtual WebAnimation* createAnimation(const WebAnimationCurve&, WebAnimation::TargetProperty, int animationId = 0) { return 0; }
+
+    virtual WebFilterAnimationCurve* createFilterAnimationCurve() { return 0; }
 
     virtual WebFloatAnimationCurve* createFloatAnimationCurve() { return 0; }
 
