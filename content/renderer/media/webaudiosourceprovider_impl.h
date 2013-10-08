c31a7a33a8901de8e71aca31e15748e274dab2d9
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-08 00:04:41 +0000

    content: Update the includes paths of WebAudioSourceProvider.h to its new location.
    This header file moved to public/platform/ in Blink code base.
    After this lands, the forwarding header in public/web/ will be removed from Blink.
    
    BUG=None
    TEST=None, no functional changes.
    R=fischman@chromium.org
    
    Review URL: https://codereview.chromium.org/26169002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227394 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/webaudiosourceprovider_impl.h
+++ content/renderer/media/webaudiosourceprovider_impl.h
@@ -9,8 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/lock.h"
 #include "content/common/content_export.h"
 #include "media/base/audio_renderer_sink.h"
+#include "third_party/WebKit/public/platform/WebAudioSourceProvider.h"
 #include "third_party/WebKit/public/platform/WebVector.h"
-#include "third_party/WebKit/public/web/WebAudioSourceProvider.h"
 
 namespace WebKit {
 class WebAudioSourceProviderClient;
--- content/renderer/media/webrtc_local_audio_source_provider.h
+++ content/renderer/media/webrtc_local_audio_source_provider.h
@@ -12,8 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "content/common/content_export.h"
 #include "media/base/audio_converter.h"
+#include "third_party/WebKit/public/platform/WebAudioSourceProvider.h"
 #include "third_party/WebKit/public/platform/WebVector.h"
-#include "third_party/WebKit/public/web/WebAudioSourceProvider.h"
 
 namespace media {
 class AudioBus;
