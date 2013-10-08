0fd9649581bc2bd7192dc6f6d92da4258fb0d1c9
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-08 18:21:50 +0000

    drive: Stop using resource ID to access local metadata from ChangeListProcessor
    Add a new member parent_resource_id_map_ to ChangeListProcessor.
    Rewrite ApplyEntryMap() and ApplyEntry().
    
    BUG=275270
    TEST=unit_tests
    
    Review URL: https://codereview.chromium.org/25430004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227547 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/change_list_processor.h
+++ chrome/browser/chromeos/drive/change_list_processor.h
@@ -117,6 +117,8 @@ class ChangeListProcessor {
  private:
   typedef std::map<std::string /* resource_id */, ResourceEntry>
       ResourceEntryMap;
+  typedef std::map<std::string /* resource_id */,
+                   std::string /* parent_resource_id*/> ParentResourceIdMap;
 
   // Applies the pre-processed metadata from entry_map_ onto the resource
   // metadata. If this is not delta update (i.e. |is_delta_update| is false),
@@ -135,6 +137,7 @@ class ChangeListProcessor {
   ResourceMetadata* resource_metadata_;  // Not owned.
 
   ResourceEntryMap entry_map_;
+  ParentResourceIdMap parent_resource_id_map_;
   std::set<base::FilePath> changed_dirs_;
 
   DISALLOW_COPY_AND_ASSIGN(ChangeListProcessor);
