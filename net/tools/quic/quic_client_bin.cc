5eefd41aeac9850252261b880ce952554cde5a24
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-08 19:00:49 +0000

    Add better help information to quic_client and quic_server. Also, modify the quic_client to print out the response contents.
    Merge internal change: 53403247
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=227365
    
    Review URL: https://codereview.chromium.org/25043005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227558 0039d316-1c4b-4281-b951-d872f2087c98

--- net/tools/quic/quic_client_bin.cc
+++ net/tools/quic/quic_client_bin.cc
@@ -3,11 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// A binary wrapper for QuicClient.  Connects to --hostname or --address on
-// --port and requests URLs specified on the command line.
+// A binary wrapper for QuicClient.  Connects to --hostname via --address
+// on --port and requests URLs specified on the command line.
 //
 // For example:
-//  quic_client --port=6122 /index.html /favicon.ico
+//  quic_client --address=127.0.0.1 --port=6122 --hostname=www.google.com
+//      http://www.google.com/index.html http://www.google.com/favicon.ico
+
+#include <iostream>
 
 #include "base/at_exit.h"
 #include "base/command_line.h"
@@ -24,6 +27,18 @@ std::string FLAGS_hostname = "localhost";
 int main(int argc, char *argv[]) {
   CommandLine::Init(argc, argv);
   CommandLine* line = CommandLine::ForCurrentProcess();
+  if (line->HasSwitch("h") || line->HasSwitch("help")) {
+    const char* help_str =
+        "Usage: quic_client [options]\n"
+        "\n"
+        "Options:\n"
+        "-h, --help                  show this help message and exit\n"
+        "--port=<port>               specify the port to connect to\n"
+        "--address=<address>         specify the IP address to connect to\n"
+        "--host=<host>               specify the SNI hostname to use\n";
+    std::cout << help_str;
+    exit(0);
+  }
   if (line->HasSwitch("port")) {
     int port;
     if (base::StringToInt(line->GetSwitchValueASCII("port"), &port)) {
@@ -45,8 +60,8 @@ int main(int argc, char *argv[]) {
   net::IPAddressNumber addr;
   CHECK(net::ParseIPLiteralToNumber(FLAGS_address, &addr));
   // TODO(rjshade): Set version on command line.
-  net::tools::QuicClient client(
-      net::IPEndPoint(addr, FLAGS_port), FLAGS_hostname, net::QuicVersionMax());
+  net::tools::QuicClient client(net::IPEndPoint(addr, FLAGS_port),
+                                FLAGS_hostname, net::QuicVersionMax(), true);
 
   client.Initialize();
 
