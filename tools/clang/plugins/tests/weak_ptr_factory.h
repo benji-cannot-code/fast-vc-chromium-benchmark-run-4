88d43f872fbe49a46b6a459050524f859e2555d5
dmichael@chromium.org <dmichael@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-08 16:47:18 +0000

    Add Clang plugin check for WeakPtrFactory member order
    Also consolidates some of the FindBadConstruct options in to a small struct for readability.
    
    The WeakPtrFactory member order is behind a flag, because
    there are many violations in Chromium right now. Also, there
    might need to be exceptions in the checker. For example,
    we might want to allow more than one WeakPtrFactory to refer
    to the outer class, as long as all such factories appear
    after all other members. That's TBD.
    
    BUG=303818
    R=erg@chromium.org
    
    Review URL: https://codereview.chromium.org/26303002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227528 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/clang/plugins/tests/weak_ptr_factory.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WEAK_PTR_FACTORY_H_
+#define WEAK_PTR_FACTORY_H_
+
+namespace base {
+
+template <typename T>
+class WeakPtrFactory {
+ public:
+  explicit WeakPtrFactory(T*) {}
+};
+
+}  // namespace base
+
+#endif  // WEAK_PTR_FACTORY_H_
