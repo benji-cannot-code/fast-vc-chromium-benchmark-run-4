5cbb22650dee9753b361c75399213f3e0fb48edb
tfarina@chromium.org <tfarina@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-09 14:39:46 +0000

    Remove WebAudioSourceProvider.h from public/web.
    This is a two side patch and the Chromium side has to be landed first.
    
    BUG=None
    TEST=None
    R=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/26168002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159232 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/web/WebAudioSourceProvider.h
+++
@@ -1,4 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// FIXME: Delete this file.
-// This file used to be in the wrong location.
-#include "public/platform/WebAudioSourceProvider.h"
