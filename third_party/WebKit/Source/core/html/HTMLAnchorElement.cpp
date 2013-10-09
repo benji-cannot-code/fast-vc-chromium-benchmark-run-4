f203fbf430879791acbffd59c32b07958fd9ccb3
davidben@chromium.org <davidben@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-09 00:01:02 +0000

    Schedule pings before frame loads in anchors.
    If scheduled afterwards, a prerender navigation may kill the renderer
    before the ping goes through.
    
    BUG=302816
    
    Review URL: https://codereview.chromium.org/26312002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159175 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLAnchorElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLAnchorElement.cpp
@@ -576,6 +576,10 @@ void HTMLAnchorElement::handleClick(Event* event)
     appendServerMapMousePosition(url, event);
     KURL completedURL = document().completeURL(url.toString());
 
+    // Schedule the ping before the frame load. Prerender in Chrome may kill the renderer as soon as the navigation is
+    // sent out.
+    sendPings(completedURL);
+
     ResourceRequest request(completedURL);
     if (prefetchEventHandler()->hasIssuedPreconnect())
         frame->loader()->client()->dispatchWillRequestAfterPreconnect(request);
@@ -594,8 +598,6 @@ void HTMLAnchorElement::handleClick(Event* event)
             frameRequest.setShouldSendReferrer(NeverSendReferrer);
         frame->loader()->load(frameRequest);
     }
-
-    sendPings(completedURL);
 }
 
 HTMLAnchorElement::EventType HTMLAnchorElement::eventType(Event* event)
