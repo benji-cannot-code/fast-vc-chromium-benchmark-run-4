1f204f87e94c651ef3d973d06b8aec20d94a85f8
arv@chromium.org <arv@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-09 16:26:10 +0000

    Setting host without port should keep old port
    This makes us compatible with Firefox, IE and the spec:
    
    http://url.spec.whatwg.org/#dom-url-host
    
    BUG=304946
    R=abarth
    
    Review URL: https://codereview.chromium.org/26506003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159239 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/dom/HTMLAnchorElement/script-tests/set-href-attribute-host.js
+++ third_party/WebKit/LayoutTests/fast/dom/HTMLAnchorElement/script-tests/set-href-attribute-host.js
@@ -8,6 +8,11 @@ a.href = "https://www.mydomain.com:8080/path/";
 a.host = "www.otherdomain.com:0";
 shouldBe("a.href", "'https://www.otherdomain.com:0/path/'");
 
+debug("Set host without port");
+a.href = "https://www.mydomain.com:8080/path/";
+a.host = "www.otherdomain.com";
+shouldBe("a.href", "'https://www.otherdomain.com:8080/path/'");
+
 // IE8 throws "The URL is invalid" exception.
 debug("Set host with '?' in it");
 try {
@@ -27,6 +32,11 @@ debug("Set default port");
 a.href = "https://www.mydomain.com:8080/path/";
 a.host = "www.otherdomain.com:443";
 shouldBe("a.href", "'https://www.otherdomain.com/path/'");
+
+debug("Set host with invalid port");
+a.href = "https://www.mydomain.com:8080/path/";
+a.host = "www.otherdomain.com:invalid";
+shouldBe("a.href", "'https://www.otherdomain.com:0/path/'");
 
 // Firefox 3.5.2 rejects a port that contains non-digits.
 debug("Set host with letters in port number");
@@ -70,7 +80,7 @@ debug("Set host containing slashes in it");
 try {
 a.href = "https://www.mydomain.com:8080/path/";
 a.host = "www.other\dom/ain.com";
-shouldBe("a.href", "'https://www.otherdom/ain.com/path/'");
+shouldBe("a.href", "'https://www.otherdom%2Fain.com:8080/path/'");
 } catch(e) {
 debug("Exception: " + e.description);
 }
