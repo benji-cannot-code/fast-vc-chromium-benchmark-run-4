6a0b50a60e0d245f2db0afe00898d5db036079e0
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-09 18:16:36 +0000

    Fix flakyness of MetricsService unit tests.
    MetricsService::InitializeMetricsState() has a DCHECK 
    that MetricsLog::GetIncrementalUptime() would always 
    return 0, which isn't the case for unit tests. (This is 
    because MetricsService may be created and destroyed 
    multiple times.) 
    
    This CL de-flakes the tests by moving the static 
    MetricsLog::GetIncrementalUptime to be a non-static 
    MetricsService::GetIncrementalUptime function (with 
    the last time stored as an instance variable). 
    
    BUG=290326
    TEST=Tests still pass even if you add a sleep() call 
    to MetricsServiceTest() constructor.
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/25559009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227767 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metrics/metrics_log.h
+++ chrome/browser/metrics/metrics_log.h
@@ -76,10 +76,6 @@ class MetricsLog : public MetricsLogBase {
 
   static void RegisterPrefs(PrefRegistrySimple* registry);
 
-  // Get the amount of uptime in seconds since this function was last called.
-  // This updates the cumulative uptime metric for uninstall as a side effect.
-  static int64 GetIncrementalUptime(PrefService* pref);
-
   // Get the current version of the application as a string.
   static std::string GetVersionString();
 
@@ -96,7 +92,8 @@ class MetricsLog : public MetricsLogBase {
   // dictionary giving the metrics for the profile.
   void RecordEnvironment(
       const std::vector<content::WebPluginInfo>& plugin_list,
-      const GoogleUpdateMetrics& google_update_metrics);
+      const GoogleUpdateMetrics& google_update_metrics,
+      base::TimeDelta incremental_uptime);
 
   // Records the current operating environment.  Takes the list of installed
   // plugins and Google Update statistics as parameters because those can't be
@@ -125,7 +122,8 @@ class MetricsLog : public MetricsLogBase {
   // installed plugins as a parameter because that can't be obtained
   // synchronously from the UI thread.
   void RecordIncrementalStabilityElements(
-      const std::vector<content::WebPluginInfo>& plugin_list);
+      const std::vector<content::WebPluginInfo>& plugin_list,
+      base::TimeDelta incremental_uptime);
 
  protected:
   // Exposed for the sake of mocking in test code.
@@ -154,6 +152,7 @@ class MetricsLog : public MetricsLogBase {
   // NOTE: Has the side-effect of clearing those counts.
   void WriteStabilityElement(
       const std::vector<content::WebPluginInfo>& plugin_list,
+      base::TimeDelta incremental_uptime,
       PrefService* pref);
 
   // Within stability group, write plugin crash stats.
@@ -168,7 +167,8 @@ class MetricsLog : public MetricsLogBase {
   // and can't be delayed until the user decides to restart chromium.
   // Delaying these stats would bias metrics away from happy long lived
   // chromium processes (ones that don't crash, and keep on running).
-  void WriteRealtimeStabilityAttributes(PrefService* pref);
+  void WriteRealtimeStabilityAttributes(PrefService* pref,
+                                        base::TimeDelta incremental_uptime);
 
   // Writes the list of installed plugins.
   void WritePluginList(const std::vector<content::WebPluginInfo>& plugin_list);
