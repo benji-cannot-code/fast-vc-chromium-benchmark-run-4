d5f5a20f694713c194c39a38af6e04df089de28b
mohsen@chromium.org <mohsen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-09 23:47:50 +0000

    Do not send mouse-move when target is destroyed due to synthesized mouse-exit in RootWindow
    When a RootWindow is going to dispatch a mouse-move to a child, if the
    target is different than the current mouse_moved_handler_, it sends a
    synthesized mouse-exit to to mouse_moved_handler_. The target might be
    destroyed because of this mouse-exit. If that's the case, the original
    mouse-move should not be sent to the destroyed target.
    
    BUG=304276
    
    Review URL: https://codereview.chromium.org/26499003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227835 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/root_window.cc
+++ ui/aura/root_window.cc
@@ -1131,6 +1131,7 @@ bool RootWindow::DispatchMouseEventToTarget(ui::MouseEvent* event,
             return false;
         } else {
           SetMouseMovedHandler(NULL, MOUSE_MOVED_HANDLER_SET_REASON_NULL);
+          return false;
         }
       }
       break;
