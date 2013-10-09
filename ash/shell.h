ccad43aeed6b43c98805d4c7ed38cbef4bffe620
tengs@chromium.org <tengs@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-09 17:57:20 +0000

    Add autoclick accessibility feature to ash.
    BUG=272401
    TEST=added unit tests
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=226895
    
    Review URL: https://codereview.chromium.org/24191002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227760 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/shell.h
+++ ash/shell.h
@@ -75,6 +75,7 @@ namespace ash {
 
 class AcceleratorController;
 class AshNativeCursorManager;
+class AutoclickController;
 class CapsLockDelegate;
 class DesktopBackgroundController;
 class DisplayController;
@@ -370,6 +371,11 @@ class ASH_EXPORT Shell
   PartialMagnificationController* partial_magnification_controller() {
     return partial_magnification_controller_.get();
   }
+
+  AutoclickController* autoclick_controller() {
+    return autoclick_controller_.get();
+  }
+
   aura::client::ActivationClient* activation_client() {
     return activation_client_;
   }
@@ -591,6 +597,7 @@ class ASH_EXPORT Shell
   scoped_ptr<HighContrastController> high_contrast_controller_;
   scoped_ptr<MagnificationController> magnification_controller_;
   scoped_ptr<PartialMagnificationController> partial_magnification_controller_;
+  scoped_ptr<AutoclickController> autoclick_controller_;
   scoped_ptr<aura::client::FocusClient> focus_client_;
   scoped_ptr<aura::client::UserActionClient> user_action_client_;
   aura::client::ActivationClient* activation_client_;
