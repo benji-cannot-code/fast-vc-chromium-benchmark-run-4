c079354a0098d6102c1a16e46ec463c3f14c5054
arv@chromium.org <arv@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-09 15:10:18 +0000

    DOMTokenList: Make sure we do not add the same token twice
    In case we do element.classList.add('a', 'a') we need to ensure that we
    do not add the same token twice.
    
    http://dom.spec.whatwg.org/#dom-domtokenlist-add
    
    BUG=232448
    R=tkent
    
    Review URL: https://codereview.chromium.org/25750002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159235 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/dom/HTMLElement/script-tests/class-list.js
+++ third_party/WebKit/LayoutTests/fast/dom/HTMLElement/script-tests/class-list.js
@@ -300,7 +300,7 @@ shouldBeEqualToString('element.className', 'a b c null d undefined 0 false');
 
 createElement('');
 element.classList.add('a', 'b', 'a');
-shouldBeEqualToString('element.className', 'a b a');
+shouldBeEqualToString('element.className', 'a b');
 
 createElement('');
 shouldThrowDOMException(function() {
