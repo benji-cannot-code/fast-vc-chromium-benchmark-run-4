d04c2e2da4ea17b31dfaf2830d9e38f392fce72f
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-09 06:19:56 +0000

    Remove unnecessary sandbox_file_system_backend.h dependency
    BUG=265769
    TEST=build
    
    Review URL: https://codereview.chromium.org/26479002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227682 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/local/canned_syncable_file_system.cc
+++ chrome/browser/sync_file_system/local/canned_syncable_file_system.cc
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/browser/fileapi/file_system_context.h"
 #include "webkit/browser/fileapi/file_system_operation_context.h"
 #include "webkit/browser/fileapi/file_system_operation_runner.h"
-#include "webkit/browser/fileapi/sandbox_file_system_backend.h"
 #include "webkit/browser/quota/mock_special_storage_policy.h"
 #include "webkit/browser/quota/quota_manager.h"
 #include "webkit/common/blob/shareable_file_reference.h"
