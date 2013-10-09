3392f93ca4779143cca429526c48968110b88755
dharcourt@chromium.org <dharcourt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-09 20:38:15 +0000

    Made the preview auto-cancel used for app print testing more realistic.
    Previously this auto-cancel was accomplished directly in C++, causing
    print preview dialog cancellations triggerred in tests to behave
    slightly differently from similar cancellations initiated by users.
    The most notable difference was that test cancellations happened
    immediately after the print preview dialog was brought up whereas
    user-initiated cancellations only happen on the next pass through the
    JavaScript event loop. Differences like these were not a problem for
    basic tests but would prevent some more advanced tests that are being
    developed from working.
    
    This change lists has auto-cancel work by sending a mouse click event to
    the dialog's cancel button, replicating almost exactly what happens for
    user-initiated cancellations.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/26564003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227799 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/print_preview/native_layer.js
+++ chrome/browser/resources/print_preview/native_layer.js
@@ -41,6 +41,7 @@ cr.define('print_preview', function() {
     global['printScalingDisabledForSourcePDF'] =
         this.onPrintScalingDisabledForSourcePDF_.bind(this);
     global['onDidGetAccessToken'] = this.onDidGetAccessToken_.bind(this);
+    global['autoCancelForTesting'] = this.autoCancelForTesting_.bind(this);
   };
 
   /**
@@ -579,6 +580,17 @@ cr.define('print_preview', function() {
      */
     onPrintScalingDisabledForSourcePDF_: function() {
       cr.dispatchSimpleEvent(this, NativeLayer.EventType.DISABLE_SCALING);
+    },
+
+    /**
+     * Simulates a user click on the print preview dialog cancel button. Used
+     * only for testing.
+     * @private
+     */
+    autoCancelForTesting_: function() {
+      var properties = {view: window, bubbles: true, cancelable: true};
+      var click = new MouseEvent('click', properties);
+      document.querySelector('#print-header .cancel').dispatchEvent(click);
     }
   };
 
