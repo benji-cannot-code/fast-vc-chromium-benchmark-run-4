9dd6dbd0000f82ec2d25ed4432f2ab691d287ac1
mek@chromium.org <mek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-09 19:12:33 +0000

    Revert 227772 "Clears the expired identities from the WebRTC ide..."
    This fails on ASAN tests:
    http://build.chromium.org/p/chromium.memory/builders/Linux%20ASAN%20Tests%20%282%29/builds/18341/steps/content_unittests/logs/stdio
    ==23976==ERROR: AddressSanitizer: heap-use-after-free on address 0x60b00002ae68 at pc 0x2281510 bp 0x7fff24edac90 sp 0x7fff24edac88
    READ of size 8 at 0x60b00002ae68 thread T0
        #0 0x228150f in content::WebRTCIdentityStoreBackend::FindIdentity(GURL const&, std::string const&, std::string const&, base::Callback<void (int, std::string const&, std::string const&)> const&) content/browser/media/webrtc_identity_store_backend.cc:225
        #1 0x228340f in content::WebRTCIdentityStoreBackend::OnLoaded(scoped_ptr<std::map<content::WebRTCIdentityStoreBackend::IdentityKey, content::WebRTCIdentityStoreBackend::Identity, std::less<content::WebRTCIdentityStoreBackend::IdentityKey>, std::allocator<std::pair<content::WebRTCIdentityStoreBackend::IdentityKey const, content::WebRTCIdentityStoreBackend::Identity> > >, base::DefaultDeleter<std::map<content::WebRTCIdentityStoreBackend::IdentityKey, content::WebRTCIdentityStoreBackend::Identity, std::less<content::WebRTCIdentityStoreBackend::IdentityKey>, std::allocator<std::pair<content::WebRTCIdentityStoreBackend::IdentityKey const, content::WebRTCIdentityStoreBackend::Identity> > > > >) content/browser/media/webrtc_identity_store_backend.cc:353
        #2 0x2289f53 in Run base/bind_internal.h:190
    ....
    
    > Clears the expired identities from the WebRTC identity cache.
    > We now check the creation time of an identity before returning for WebRTCIdentityStoreBackend::FindIdentity and remove the expired identity from the in-memory cache.
    > The expired identities are also removed from the on-disk database before it's loaded to the in-memory cache. 
    > The identities have 30-day expiration time by default for now. 
    > 
    > BUG=304398
    > 
    > Review URL: https://codereview.chromium.org/26082002
    
    TBR=jiayl@chromium.org
    
    Review URL: https://codereview.chromium.org/26567009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227776 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/media/webrtc_identity_store.h
+++ content/browser/media/webrtc_identity_store.h
@@ -84,7 +84,6 @@ class CONTENT_EXPORT WebRTCIdentityStore
   friend class base::RefCountedThreadSafe<WebRTCIdentityStore>;
   friend class WebRTCIdentityStoreTest;
 
-  void SetValidityPeriodForTesting(base::TimeDelta validity_period);
   void SetTaskRunnerForTesting(
       const scoped_refptr<base::TaskRunner>& task_runner);
 
@@ -100,12 +99,8 @@ class CONTENT_EXPORT WebRTCIdentityStore
   void PostRequestResult(WebRTCIdentityRequest* request,
                          const WebRTCIdentityRequestResult& result);
 
-  // The validity period of the certificates.
-  base::TimeDelta validity_period_;
-
   // The TaskRunner for doing work on a worker thread.
   scoped_refptr<base::TaskRunner> task_runner_;
-
   // Weak references of the in flight requests. Used to join identical external
   // requests.
   std::vector<WebRTCIdentityRequest*> in_flight_requests_;
