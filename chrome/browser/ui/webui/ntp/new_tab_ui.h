4535bea1a4a5faf1cdf0d0562e1f972884b41838
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-09 17:52:00 +0000

    Remove NOTIFICATION_WEB_CONTENTS_VISIBILITY_CHANGED from NewTabUI.
    BUG=170921
    TEST=everything still works
    
    Review URL: https://codereview.chromium.org/26471006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227758 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/ntp/new_tab_ui.h
+++ chrome/browser/ui/webui/ntp/new_tab_ui.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_registrar.h"
 #include "content/public/browser/url_data_source.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_ui_controller.h"
 
 class GURL;
@@ -31,6 +32,7 @@ class PrefRegistrySyncable;
 
 // The WebUIController used for the New Tab page.
 class NewTabUI : public content::WebUIController,
+                 public content::WebContentsObserver,
                  public content::NotificationObserver {
  public:
   explicit NewTabUI(content::WebUI* web_ui);
@@ -74,6 +76,9 @@ class NewTabUI : public content::WebUIController,
       content::RenderViewHost* render_view_host) OVERRIDE;
   virtual void RenderViewReused(
       content::RenderViewHost* render_view_host) OVERRIDE;
+
+  // WebContentsObserver implementation:
+  virtual void WasHidden() OVERRIDE;
 
   bool showing_sync_bubble() { return showing_sync_bubble_; }
   void set_showing_sync_bubble(bool showing) { showing_sync_bubble_ = showing; }
@@ -121,7 +126,7 @@ class NewTabUI : public content::WebUIController,
 
   // If |web_contents| has an NTP URL, emits the number of NTP mouseovers
   // associated with |web_contents|, to be logged in UMA histogram.
-  void EmitMouseoverCount(content::WebContents* web_contents);
+  void EmitMouseoverCount();
 
   void OnShowBookmarkBarChanged();
 
