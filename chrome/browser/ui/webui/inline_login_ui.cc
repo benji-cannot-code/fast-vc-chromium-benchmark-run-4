227c5ef17d882c1bf739e210ed9bdb9dc6b2d748
guohui@chromium.org <guohui@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-09 03:43:45 +0000

    Trigger inline signin within avatar bubble
    BUG=302037
    
    Review URL: https://codereview.chromium.org/26494002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227659 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/inline_login_ui.cc
+++ chrome/browser/ui/webui/inline_login_ui.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/values.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/extensions/tab_helper.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/signin_manager_cookie_helper.h"
 #include "chrome/browser/signin/signin_promo.h"
@@ -218,6 +219,10 @@ InlineLoginUI::InlineLoginUI(content::WebUI* web_ui)
   content::WebUIDataSource::Add(profile, CreateWebUIDataSource());
 
   web_ui->AddMessageHandler(new InlineLoginUIHandler(profile));
+  // Required for intercepting extension function calls when the page is loaded
+  // in a bubble (not a full tab, thus tab helpers are not registered
+  // automatically).
+  extensions::TabHelper::CreateForWebContents(web_ui->GetWebContents());
 }
 
 InlineLoginUI::~InlineLoginUI() {}
