65737399be38c538476c8093ab6badbf06f14a7a
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-09 08:15:21 +0000

    Revert 227675 "reland "views: change WrenchMenu to use each mode..."
    > reland "views: change WrenchMenu to use each model's command ID's when creating MenuItemView's"
    > 
    > the original cl (hhttps://codereview.chromium.org/25667005/) was reverted because of crash in bug 304756, this cl relands it with the fixes. 
    > 
    > this is in preparation for dynamic recent tabs model and menu: 
    > 
    > * WrenchMenu: 
    > - for each menu item, MenuItemView::GetCommand() used to be separate and different from MenuModel::GetCommandIdAt(model_index); they are the same now. 
    > - different range of command id's are reserved for recent tabs and bookmarks. 
    > - command id's of all items in the wrench menu and all its submenus can't clash, except for separator and IDC_SHOW_HISTORY, the latter of which is in both wrench menu and recent tabs submenu. 
    > 
    > * RecentTabsSubMenuModel: 
    > - make all command ids within range, use different ids among device name headers. 
    > 
    > * BookmarkMenuDelegate 
    > - makes sure only maximum number of menu items allowed is created. 
    > - add test. 
    > 
    > BUG=256750
    > TEST=wrench menu and all its submenus still work.
    > 
    > Review URL: https://codereview.chromium.org/26455004
    
    The new test has memory leaks. When the previous menu delegate is freed it doesn't
    free the menu it creates in Init, I suspect this normally is handled by the views
    system itself.
    
    Example of leaks:
    http://build.chromium.org/p/chromium.memory.fyi/builders/Chromium%20OS%20%28valgrind%29%286%29/builds/21625
    
    You can reproduce the leaks by building for chromeos and running under valgrind:
    http://www.chromium.org/developers/how-tos/using-valgrind
    
    TBR=kuan@chromium.org
    
    Review URL: https://codereview.chromium.org/26546004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227699 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/bookmarks/bookmark_menu_delegate.h
+++ chrome/browser/ui/views/bookmarks/bookmark_menu_delegate.h
@@ -54,8 +54,7 @@ class BookmarkMenuDelegate : public BaseBookmarkModelObserver,
   BookmarkMenuDelegate(Browser* browser,
                        content::PageNavigator* navigator,
                        views::Widget* parent,
-                       int first_menu_id,
-                       int max_menu_id);
+                       int first_menu_id);
   virtual ~BookmarkMenuDelegate();
 
   // Creates the menus from the model.
@@ -131,9 +130,6 @@ class BookmarkMenuDelegate : public BaseBookmarkModelObserver,
   virtual void DidRemoveBookmarks() OVERRIDE;
 
  private:
-  friend class BookmarkMenuDelegateTest;
-  FRIEND_TEST_ALL_PREFIXES(BookmarkMenuDelegateTest, MenuIdRange);
-
   typedef std::map<int, const BookmarkNode*> MenuIDToNodeMap;
   typedef std::map<const BookmarkNode*, views::MenuItemView*> NodeToMenuMap;
 
@@ -162,10 +158,6 @@ class BookmarkMenuDelegate : public BaseBookmarkModelObserver,
                  int start_child_index,
                  views::MenuItemView* menu,
                  int* next_menu_id);
-
-  // Returns true if |menu_id_| is outside the range of minimum and maximum menu
-  // ID's allowed.
-  bool IsOutsideMenuIdRange(int menu_id) const;
 
   Browser* browser_;
   Profile* profile_;
@@ -198,10 +190,6 @@ class BookmarkMenuDelegate : public BaseBookmarkModelObserver,
 
   // ID of the next menu item.
   int next_menu_id_;
-
-  // Minimum and maximum ID's to use for menu items.
-  const int min_menu_id_;
-  const int max_menu_id_;
 
   views::MenuDelegate* real_delegate_;
 
