01032c37acb4bfb09fc406b390920665a4b93f7c
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-09 00:54:37 +0000

    Move kGuestScheme into content namespace.
    BUG=None
    TEST=None, no functional changes.
    R=jam@chromium.org
    
    Review URL: https://codereview.chromium.org/26155002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227637 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/common/url_constants.cc
+++ content/public/common/url_constants.cc
@@ -19,11 +19,11 @@ const char kDataScheme[] = "data";
 const char kFileScheme[] = "file";
 const char kFileSystemScheme[] = "filesystem";
 const char kFtpScheme[] = "ftp";
-const char kGuestScheme[] = "chrome-guest";
 }  // namespace chrome
 
 namespace content {
 
+const char kGuestScheme[] = "chrome-guest";
 const char kHttpScheme[] = "http";
 const char kHttpsScheme[] = "https";
 const char kJavaScriptScheme[] = "javascript";
