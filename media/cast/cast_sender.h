0f9f69a011876691ae147c7c2324fe9fa5d93d0e
pwestin@google.com <pwestin@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-09 19:53:23 +0000

    Bug fixes and nit cleanup, bugs found while writing a new end 2 end test.
    BUG=
    
    Review URL: https://codereview.chromium.org/25048005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227781 0039d316-1c4b-4281-b951-d872f2087c98

--- media/cast/cast_sender.h
+++ media/cast/cast_sender.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/callback.h"
 #include "base/memory/ref_counted.h"
+#include "base/time/tick_clock.h"
 #include "base/time/time.h"
 #include "media/cast/cast_config.h"
 #include "media/cast/cast_environment.h"
