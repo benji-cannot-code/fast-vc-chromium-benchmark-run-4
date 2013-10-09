856cd9b1498303e0cd4067984d84097b17cf4135
cevans@chromium.org <cevans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-09 11:29:48 +0000

    Fix possible race condition in updating superpage bitmap.
    Review URL: https://codereview.chromium.org/26572005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159222 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/PageAllocator.cpp
+++ third_party/WebKit/Source/wtf/PageAllocator.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/PageAllocator.h"
 
 #include "wtf/CryptographicallyRandomNumber.h"
+#include "wtf/SpinLock.h"
 
 #if OS(POSIX)
 
@@ -169,6 +170,8 @@ char* getRandomSuperPageBase()
 #if CPU(32BIT)
 unsigned char SuperPageBitmap::s_bitmap[1 << (32 - kSuperPageShift - 3)];
 
+static int bitmapLock = 0;
+
 void SuperPageBitmap::registerSuperPage(void* ptr)
 {
     ASSERT(!isPointerInSuperPage(ptr));
@@ -177,7 +180,10 @@ void SuperPageBitmap::registerSuperPage(void* ptr)
     size_t byteIndex = raw >> 3;
     size_t bit = raw & 7;
     ASSERT(byteIndex < sizeof(s_bitmap));
+    // The read/modify/write is not guaranteed atomic, so take a lock.
+    spinLockLock(&bitmapLock);
     s_bitmap[byteIndex] |= (1 << bit);
+    spinLockUnlock(&bitmapLock);
 }
 
 void SuperPageBitmap::unregisterSuperPage(void* ptr)
@@ -188,7 +194,10 @@ void SuperPageBitmap::unregisterSuperPage(void* ptr)
     size_t byteIndex = raw >> 3;
     size_t bit = raw & 7;
     ASSERT(byteIndex < sizeof(s_bitmap));
+    // The read/modify/write is not guaranteed atomic, so take a lock.
+    spinLockLock(&bitmapLock);
     s_bitmap[byteIndex] &= ~(1 << bit);
+    spinLockUnlock(&bitmapLock);
 }
 #endif
 
