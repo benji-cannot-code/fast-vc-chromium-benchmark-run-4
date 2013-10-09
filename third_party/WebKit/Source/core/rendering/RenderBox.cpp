9af5309b5334252193694f25b0335178f1fad7f7
betravis@adobe.com <betravis@adobe.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-09 00:25:13 +0000

    Relayout when shape-outside is modified dynamically
    Mark the appropriate content for relayout when the shape-outside property
    changes values dynamically. Do this using the same mechanism in place
    for changing the float property from left to right. See
    RenderObject::styleWillChange().
    
    BUG=304916
    
    Review URL: https://codereview.chromium.org/26275006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159180 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderBox.cpp
+++ third_party/WebKit/Source/core/rendering/RenderBox.cpp
@@ -294,6 +294,7 @@ void RenderBox::updateShapeOutsideInfoAfterStyleChange(const ShapeValue* shapeOu
     } else {
         ShapeOutsideInfo::removeInfo(this);
     }
+    markShapeOutsideDependentsForLayout();
 }
 
 void RenderBox::updateGridPositionAfterStyleChange(const RenderStyle* oldStyle)
