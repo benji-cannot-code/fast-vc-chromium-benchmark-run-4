19007e81ea870189e209afb444c9ab4049d70a26
teravest@chromium.org <teravest@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-09 04:04:36 +0000

    Pepper: Style nits for QuotaFileIO.
    This cleans up some renderer-specific stuff to make it easier to read the code
    changes when moving QuotaFileIO to the browser.
    
    BUG=246396
    
    Review URL: https://codereview.chromium.org/26560003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227667 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/pepper/quota_file_io.cc
+++ content/renderer/pepper/quota_file_io.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_loop_proxy.h"
 #include "base/stl_util.h"
 #include "base/task_runner_util.h"
-#include "content/renderer/pepper/host_globals.h"
 
 using base::PlatformFile;
 using base::PlatformFileError;
