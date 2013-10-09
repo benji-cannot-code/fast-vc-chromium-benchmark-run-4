8f53441b614cd2affe75edb95db7e782273cca58
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-09 20:35:45 +0000

    Remove NOTIFICATION_WEB_CONTENTS_VISIBILITY_CHANGED from autofill.
    BUG=170921
    TEST=everything still works
    
    Review URL: https://codereview.chromium.org/26553002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227798 0039d316-1c4b-4281-b951-d872f2087c98

--- components/autofill/content/browser/autofill_driver_impl.cc
+++ components/autofill/content/browser/autofill_driver_impl.cc
@@ -14,9 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/common/autofill_switches.h"
 #include "content/public/browser/navigation_controller.h"
 #include "content/public/browser/navigation_details.h"
-#include "content/public/browser/notification_service.h"
-#include "content/public/browser/notification_source.h"
-#include "content/public/browser/notification_types.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/frame_navigate_params.h"
@@ -65,15 +62,6 @@ AutofillDriverImpl::AutofillDriverImpl(
   SetAutofillExternalDelegate(scoped_ptr<AutofillExternalDelegate>(
       new AutofillExternalDelegate(web_contents, autofill_manager_.get(),
                                    this)));
-
-  registrar_.Add(this,
-                 content::NOTIFICATION_WEB_CONTENTS_VISIBILITY_CHANGED,
-                 content::Source<content::WebContents>(web_contents));
-  registrar_.Add(
-      this,
-      content::NOTIFICATION_NAV_ENTRY_COMMITTED,
-      content::Source<content::NavigationController>(
-          &(web_contents->GetController())));
 }
 
 AutofillDriverImpl::~AutofillDriverImpl() {}
@@ -208,18 +196,14 @@ void AutofillDriverImpl::SetAutofillManager(
   autofill_manager_->SetExternalDelegate(autofill_external_delegate_.get());
 }
 
-void AutofillDriverImpl::Observe(
-    int type,
-    const content::NotificationSource& source,
-    const content::NotificationDetails& details) {
-  if (type == content::NOTIFICATION_WEB_CONTENTS_VISIBILITY_CHANGED) {
-    if (!*content::Details<bool>(details).ptr())
-      autofill_manager_->delegate()->HideAutofillPopup();
-  } else if (type == content::NOTIFICATION_NAV_ENTRY_COMMITTED) {
-    autofill_manager_->delegate()->HideAutofillPopup();
-  } else {
-    NOTREACHED();
-  }
+void AutofillDriverImpl::NavigationEntryCommitted(
+    const content::LoadCommittedDetails& load_details) {
+  autofill_manager_->delegate()->HideAutofillPopup();
 }
+
+void AutofillDriverImpl::WasHidden() {
+  autofill_manager_->delegate()->HideAutofillPopup();
+}
+
 
 }  // namespace autofill
