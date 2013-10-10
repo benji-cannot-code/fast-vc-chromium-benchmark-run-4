2a7b3a014577f7e398a02530a297c383e59fa548
ch.dumez@samsung.com <ch.dumez@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-10 16:18:17 +0000

    Remove several unused Node methods
    Remove several unused Node methods that merely call the corresponding implementation
    in Element:
    - hasLocalName()
    - hasAttributes()
    - attributes()
    
    R=eseidel
    BUG=
    
    Review URL: https://codereview.chromium.org/26678003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159344 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Node.h
+++ third_party/WebKit/Source/core/dom/Node.h
@@ -168,7 +168,6 @@ public:
     // DOM methods & attributes for Node
 
     bool hasTagName(const QualifiedName&) const;
-    bool hasLocalName(const AtomicString&) const;
     virtual String nodeName() const = 0;
     virtual String nodeValue() const;
     virtual void setNodeValue(const String&);
@@ -180,8 +179,6 @@ public:
     PassRefPtr<NodeList> childNodes();
     Node* firstChild() const;
     Node* lastChild() const;
-    bool hasAttributes() const;
-    NamedNodeMap* attributes() const;
 
     // ChildNode interface API
     Element* previousElementSibling() const;
