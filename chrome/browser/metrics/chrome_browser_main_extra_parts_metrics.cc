259706e50042abdb8ab0ae10a5ed928c76b64f75
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-10 22:09:24 +0000

    Adding UMA count for number of logical processors on Windows.
    I need to merge this into m30 or even m29 so trying to find a happy place to gather this.
    
    It can be found as Platform.LogicalCpuCount
    
    BUG=none
    TBR=sky
    TEST=none
    
    Review URL: https://codereview.chromium.org/25647002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228020 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.cc
+++ chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.cc
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/cpu.h"
 #include "base/metrics/histogram.h"
+#include "base/metrics/sparse_histogram.h"
+#include "base/sys_info.h"
 #include "base/threading/sequenced_worker_pool.h"
 #include "base/time/time.h"
 #include "chrome/browser/about_flags.h"
@@ -51,13 +53,15 @@ enum UMATouchEventsState {
   UMA_TOUCH_EVENTS_STATE_COUNT
 };
 
-void RecordIntelMicroArchitecture() {
+void RecordMicroArchitectureStats() {
 #if defined(ARCH_CPU_X86_FAMILY)
   base::CPU cpu;
   base::CPU::IntelMicroArchitecture arch = cpu.GetIntelMicroArchitecture();
   UMA_HISTOGRAM_ENUMERATION("Platform.IntelMaxMicroArchitecture", arch,
                             base::CPU::MAX_INTEL_MICRO_ARCHITECTURE);
 #endif  // defined(ARCH_CPU_X86_FAMILY)
+  UMA_HISTOGRAM_SPARSE_SLOWLY("Platform.LogicalCpuCount",
+                              base::SysInfo::NumberOfProcessors());
 }
 
 void RecordDefaultBrowserUMAStat() {
@@ -129,7 +133,7 @@ ChromeBrowserMainExtraPartsMetrics::~ChromeBrowserMainExtraPartsMetrics() {
 }
 
 void ChromeBrowserMainExtraPartsMetrics::PreProfileInit() {
-  RecordIntelMicroArchitecture();
+  RecordMicroArchitectureStats();
 }
 
 void ChromeBrowserMainExtraPartsMetrics::PreBrowserStart() {
