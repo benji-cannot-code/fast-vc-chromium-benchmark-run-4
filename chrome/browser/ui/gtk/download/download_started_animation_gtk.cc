efaef5abe8286cae1daac367ebe4ac47efaebede
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-10 23:25:17 +0000

    Remove NOTIFICATION_WEB_CONTENTS_VISIBILITY_CHANGED from download started animations.
    BUG=170921
    TEST=everything still works
    
    Review URL: https://codereview.chromium.org/26521003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228039 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/download/download_started_animation_gtk.cc
+++ chrome/browser/ui/gtk/download/download_started_animation_gtk.cc
@@ -10,12 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <gtk/gtk.h>
 
 #include "base/message_loop/message_loop.h"
-#include "content/public/browser/notification_details.h"
-#include "content/public/browser/notification_observer.h"
-#include "content/public/browser/notification_registrar.h"
-#include "content/public/browser/notification_source.h"
-#include "content/public/browser/notification_types.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_view.h"
 #include "grit/theme_resources.h"
 #include "ui/base/resource/resource_bundle.h"
@@ -34,7 +30,7 @@ const int kMoveTimeMs = 600;
 const int kFrameRateHz = 60;
 
 class DownloadStartedAnimationGtk : public gfx::LinearAnimation,
-                                    public content::NotificationObserver {
+                                    public content::WebContentsObserver {
  public:
   explicit DownloadStartedAnimationGtk(WebContents* web_contents);
 
@@ -52,10 +48,9 @@ class DownloadStartedAnimationGtk : public gfx::LinearAnimation,
   // Animation implementation.
   virtual void AnimateToState(double state) OVERRIDE;
 
-  // NotificationObserver
-  virtual void Observe(int type,
-                       const content::NotificationSource& source,
-                       const content::NotificationDetails& details) OVERRIDE;
+  // content::WebContentsObserver
+  virtual void WasHidden() OVERRIDE;
+  virtual void WebContentsDestroyed(WebContents* web_contents) OVERRIDE;
 
   // The top level window that floats over the browser and displays the
   // image.
@@ -65,9 +60,6 @@ class DownloadStartedAnimationGtk : public gfx::LinearAnimation,
   int width_;
   int height_;
 
-  // The content area holding us.
-  WebContents* web_contents_;
-
   // The content area at the start of the animation. We store this so that the
   // download shelf's resizing of the content area doesn't cause the animation
   // to move around. This means that once started, the animation won't move
@@ -75,17 +67,14 @@ class DownloadStartedAnimationGtk : public gfx::LinearAnimation,
   // much heartbreak.
   gfx::Rect web_contents_bounds_;
 
-  // A scoped container for notification registries.
-  content::NotificationRegistrar registrar_;
-
   DISALLOW_COPY_AND_ASSIGN(DownloadStartedAnimationGtk);
 };
 
 DownloadStartedAnimationGtk::DownloadStartedAnimationGtk(
     WebContents* web_contents)
     : gfx::LinearAnimation(kMoveTimeMs, kFrameRateHz, NULL),
-      popup_(NULL),
-      web_contents_(web_contents) {
+      content::WebContentsObserver(web_contents),
+      popup_(NULL) {
   static GdkPixbuf* kDownloadImage = NULL;
   if (!kDownloadImage) {
     ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
@@ -98,18 +87,9 @@ DownloadStartedAnimationGtk::DownloadStartedAnimationGtk(
 
   // If we're too small to show the download image, then don't bother -
   // the shelf will be enough.
-  web_contents_->GetView()->GetContainerBounds(&web_contents_bounds_);
+  web_contents->GetView()->GetContainerBounds(&web_contents_bounds_);
   if (web_contents_bounds_.height() < height_)
     return;
-
-  registrar_.Add(
-      this,
-      content::NOTIFICATION_WEB_CONTENTS_VISIBILITY_CHANGED,
-      content::Source<WebContents>(web_contents_));
-  registrar_.Add(
-      this,
-      content::NOTIFICATION_WEB_CONTENTS_DESTROYED,
-      content::Source<WebContents>(web_contents_));
 
   // TODO(estade): don't show up on the wrong virtual desktop.
 
@@ -139,7 +119,7 @@ DownloadStartedAnimationGtk::~DownloadStartedAnimationGtk() {
 }
 
 void DownloadStartedAnimationGtk::Reposition() {
-  if (!web_contents_)
+  if (!web_contents())
     return;
 
   DCHECK(popup_);
@@ -153,27 +133,18 @@ void DownloadStartedAnimationGtk::Reposition() {
 }
 
 void DownloadStartedAnimationGtk::Close() {
-  if (!web_contents_)
+  if (!web_contents())
     return;
 
   DCHECK(popup_);
 
-  registrar_.Remove(
-      this,
-      content::NOTIFICATION_WEB_CONTENTS_VISIBILITY_CHANGED,
-      content::Source<WebContents>(web_contents_));
-  registrar_.Remove(
-      this,
-      content::NOTIFICATION_WEB_CONTENTS_DESTROYED,
-      content::Source<WebContents>(web_contents_));
-
-  web_contents_ = NULL;
+  Observe(NULL);
   gtk_widget_destroy(popup_);
   base::MessageLoop::current()->DeleteSoon(FROM_HERE, this);
 }
 
 void DownloadStartedAnimationGtk::AnimateToState(double state) {
-  if (!web_contents_)
+  if (!web_contents())
     return;
 
   if (state >= 1.0) {
@@ -190,15 +161,12 @@ void DownloadStartedAnimationGtk::AnimateToState(double state) {
   }
 }
 
-void DownloadStartedAnimationGtk::Observe(
-    int type,
-    const content::NotificationSource& source,
-    const content::NotificationDetails& details) {
-  if (type == content::NOTIFICATION_WEB_CONTENTS_VISIBILITY_CHANGED) {
-    bool visible = *content::Details<bool>(details).ptr();
-    if (visible)
-      return;
-  }
+void DownloadStartedAnimationGtk::WasHidden() {
+  Close();
+}
+
+void DownloadStartedAnimationGtk::WebContentsDestroyed(
+    WebContents* web_contents) {
   Close();
 }
 
