e9ee0217d4e0485474f9e55987631b341a508a83
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-10 23:28:22 +0000

    FontPlatformDataHarfBuzz incorrectly shares HarfBuzzFace
    The copy construcors FontPlatformDataHarfBuzz incorrectly copies the
    m_harfBuzzFace pointer. As the copy constructs allows certain
    properties to be overridden this is incorrect.
    
    Furthermore HarfBuzzFace implements caching based on uniqueId so the
    HarfBuzzFace objects will be shared when safe to do so regardless.
    
    BUG=306224
    R=eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/26910003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159391 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/graphics/harfbuzz/FontPlatformDataHarfBuzz.cpp
+++ third_party/WebKit/Source/core/platform/graphics/harfbuzz/FontPlatformDataHarfBuzz.cpp
@@ -124,7 +124,7 @@ FontPlatformData::FontPlatformData(const FontPlatformData& src)
     , m_fakeItalic(src.m_fakeItalic)
     , m_orientation(src.m_orientation)
     , m_style(src.m_style)
-    , m_harfBuzzFace(src.m_harfBuzzFace)
+    , m_harfBuzzFace(0)
     , m_isHashTableDeletedValue(false)
 {
 }
@@ -150,7 +150,7 @@ FontPlatformData::FontPlatformData(const FontPlatformData& src, float textSize)
     , m_fakeBold(src.m_fakeBold)
     , m_fakeItalic(src.m_fakeItalic)
     , m_orientation(src.m_orientation)
-    , m_harfBuzzFace(src.m_harfBuzzFace)
+    , m_harfBuzzFace(0)
     , m_isHashTableDeletedValue(false)
 {
     querySystemForRenderStyle();
@@ -176,7 +176,7 @@ FontPlatformData& FontPlatformData::operator=(const FontPlatformData& src)
     m_textSize = src.m_textSize;
     m_fakeBold = src.m_fakeBold;
     m_fakeItalic = src.m_fakeItalic;
-    m_harfBuzzFace = src.m_harfBuzzFace;
+    m_harfBuzzFace = 0;
     m_orientation = src.m_orientation;
     m_style = src.m_style;
     m_emSizeInFontUnits = src.m_emSizeInFontUnits;
