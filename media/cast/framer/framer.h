1329ae34b3013e53a23cfd1860db6ae4ec69f6ed
pwestin@google.com <pwestin@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-10 13:42:56 +0000

    Cast: Cleaning up framer code; resolving TODOs
    // TODO(pwestin): we can deprecate the timeout argument.
    The timeout argument passed to 
    Framer::GetEncodedVideoFrame and Framer::GetEncodedAudioFrame is removed since is was not used.
    
    // TODO(pwestin): only release if we have a complete frame
    VideoReceiver::GetEncodedVideoFrame and AudioReceiver::PlayoutTimeout()
    Update logic such that a frame is only checked if it can be released if the incoming packet make a frame complete.
    
    Review URL: https://codereview.chromium.org/26686004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227923 0039d316-1c4b-4281-b951-d872f2087c98

--- media/cast/framer/framer.h
+++ media/cast/framer/framer.h
@@ -33,7 +33,9 @@ class Framer {
          int max_unacked_frames);
   ~Framer();
 
-  void InsertPacket(const uint8* payload_data,
+  // Return true when receiving the last packet in a frame, creating a
+  // complete frame.
+  bool InsertPacket(const uint8* payload_data,
                     size_t payload_size,
                     const RtpCastHeader& rtp_header);
 
@@ -41,13 +43,11 @@ class Framer {
   // frame.
   // Returns false if the frame does not exist or if the frame is not complete
   // within the given time frame.
-  bool GetEncodedVideoFrame(const base::TimeTicks& timeout,
-                            EncodedVideoFrame* video_frame,
+  bool GetEncodedVideoFrame(EncodedVideoFrame* video_frame,
                             uint32* rtp_timestamp,
                             bool* next_frame);
 
-  bool GetEncodedAudioFrame(const base::TimeTicks& timeout,
-                            EncodedAudioFrame* audio_frame,
+  bool GetEncodedAudioFrame(EncodedAudioFrame* audio_frame,
                             uint32* rtp_timestamp,
                             bool* next_frame);
 
@@ -59,10 +59,6 @@ class Framer {
   void SendCastMessage();
 
  private:
-  // Return true if we should wait.
-  bool WaitForNextFrame(const base::TimeTicks& timeout) const;
-
-  base::TickClock* const clock_;  // Not owned by this class.
   const bool decoder_faster_than_max_frame_rate_;
   FrameList frames_;
   FrameIdMap frame_id_map_;
