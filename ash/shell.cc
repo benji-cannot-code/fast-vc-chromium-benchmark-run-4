2338ebf82325f20e424230ea81a6226f984148ad
tengs@chromium.org <tengs@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-10 19:49:20 +0000

    Add autoclick accessibility feature to ash.
    BUG=272401
    TEST=added unit tests
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=226895
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=227760
    
    Review URL: https://codereview.chromium.org/24191002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227976 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/shell.cc
+++ ash/shell.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/accelerators/focus_manager_factory.h"
 #include "ash/accelerators/nested_dispatcher_controller.h"
 #include "ash/ash_switches.h"
+#include "ash/autoclick/autoclick_controller.h"
 #include "ash/caps_lock_delegate.h"
 #include "ash/desktop_background/desktop_background_controller.h"
 #include "ash/desktop_background/desktop_background_view.h"
@@ -810,6 +811,8 @@ void Shell::Init() {
 
   partial_magnification_controller_.reset(
       new PartialMagnificationController());
+
+  autoclick_controller_.reset(AutoclickController::CreateInstance());
 
   high_contrast_controller_.reset(new HighContrastController);
   video_detector_.reset(new VideoDetector);
