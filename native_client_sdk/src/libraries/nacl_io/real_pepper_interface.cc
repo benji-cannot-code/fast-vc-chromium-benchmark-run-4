e62459533e8d6a998d8a72e21affdc6ee79bd2d3
sbc@chromium.org <sbc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-10 21:12:42 +0000

    [NaCl SDK] nacl_io: fix handling of missing interfaces.
    Previously, for example, TCPInterface() would return
    a valid interface pointer, even when the TCP interface
    was missing.
    
    This meant that the checks we had in place for the
    precense of a given inteface were not working, and
    calls to methods on the interface would crash.
    
    R=noelallen@chromium.org
    
    Review URL: https://codereview.chromium.org/26888003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228000 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/libraries/nacl_io/real_pepper_interface.cc
+++ native_client_sdk/src/libraries/nacl_io/real_pepper_interface.cc
@@ -4,10 +4,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "nacl_io/real_pepper_interface.h"
+
 #include <assert.h>
 #include <stdio.h>
-
 #include <ppapi/c/pp_errors.h>
+
+#include "nacl_io/dbgprint.h"
 
 namespace nacl_io {
 
@@ -80,10 +82,15 @@ RealPepperInterface::RealPepperInterface(PP_Instance instance,
 #include "nacl_io/pepper/undef_macros.h"
 #include "nacl_io/pepper/define_empty_macros.h"
 #undef BEGIN_INTERFACE
-#define BEGIN_INTERFACE(BaseClass, PPInterface, InterfaceString) \
-    BaseClass##interface_ = new Real##BaseClass( \
-        static_cast<const PPInterface*>( \
-            get_browser_interface(InterfaceString)));
+#define BEGIN_INTERFACE(BaseClass, PPInterface, InterfaceString) { \
+    const PPInterface* iface = static_cast<const PPInterface*>( \
+        get_browser_interface(InterfaceString)); \
+    BaseClass##interface_ = NULL; \
+    if (iface) \
+      BaseClass##interface_ = new Real##BaseClass(iface); \
+    else \
+      dbgprintf("nacl_io: interface missing: %s\n", InterfaceString); \
+  }
 #include "nacl_io/pepper/all_interfaces.h"
 }
 
