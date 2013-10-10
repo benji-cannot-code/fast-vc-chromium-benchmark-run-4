0f8a19e8310dc94a4339a5109e072da3669b5128
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-10 21:04:28 +0000

    Docserver: Don't add two leading slashes in the samples data source for the placeholder extension icons. It was leading to paths like '//static' which was requesting the 'static' host. It should have been '/static'.
    R=jyasskin@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/26885002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227992 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/server2/samples_data_source.py
+++ chrome/common/extensions/docs/server2/samples_data_source.py
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import hashlib
 import json
 import logging
+import posixpath
 import re
 import traceback
 
@@ -148,7 +149,8 @@ class SamplesDataSource(object):
 
         manifest_data = self._GetDataFromManifest(sample_path, file_system)
         if manifest_data['icon'] is None:
-          icon_path = '%s/static/%s' % (self._base_path, DEFAULT_ICON_PATH)
+          icon_path = posixpath.join(
+              self._base_path, 'static', DEFAULT_ICON_PATH)
         else:
           icon_path = '%s/%s' % (icon_base, manifest_data['icon'])
         manifest_data.update({
