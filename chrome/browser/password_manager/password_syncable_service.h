92a5208a0f07cc05a384ee2e4c8ab2e3b66e3fe6
lipalani@chromium.org <lipalani@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-10 09:04:40 +0000

    Skeleton implementation. None of the methods are implemented yet. BUG=95758
    Review URL: https://codereview.chromium.org/25125002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227885 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/password_manager/password_syncable_service.h
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_PASSWORD_MANAGER_PASSWORD_SYNCABLE_SERVICE_H__
+#define CHROME_BROWSER_PASSWORD_MANAGER_PASSWORD_SYNCABLE_SERVICE_H__
+
+#include "base/memory/scoped_ptr.h"
+#include "sync/api/sync_change.h"
+#include "sync/api/sync_data.h"
+#include "sync/api/sync_error.h"
+#include "sync/api/syncable_service.h"
+
+namespace syncer {
+class SyncErrorFactory;
+}  // namespace syncer
+
+class PasswordStore;
+
+class PasswordSyncableService : public syncer::SyncableService {
+ public:
+  PasswordSyncableService();
+  virtual ~PasswordSyncableService();
+
+  // syncer::SyncableServiceImplementations
+  virtual syncer::SyncMergeResult MergeDataAndStartSyncing(
+      syncer::ModelType type,
+      const syncer::SyncDataList& initial_sync_data,
+      scoped_ptr<syncer::SyncChangeProcessor> sync_processor,
+      scoped_ptr<syncer::SyncErrorFactory> error_handler) OVERRIDE;
+  virtual void StopSyncing(syncer::ModelType type) OVERRIDE;
+  virtual syncer::SyncDataList GetAllSyncData(
+      syncer::ModelType type) const OVERRIDE;
+  virtual syncer::SyncError ProcessSyncChanges(
+      const tracked_objects::Location& from_here,
+      const syncer::SyncChangeList& change_list) OVERRIDE;
+
+ private:
+  scoped_ptr<syncer::SyncErrorFactory> sync_error_factory_;
+  scoped_ptr<syncer::SyncChangeProcessor> sync_processor_;
+};
+
+#endif  // CHROME_BROWSER_PASSWORD_MANAGER_PASSWORD_SYNCABLE_SERVICE_H__
+
--- chrome/chrome_browser.gypi
+++ chrome/chrome_browser.gypi
@@ -1388,6 +1388,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/password_manager/password_store_win.h',
         'browser/password_manager/password_store_x.cc',
         'browser/password_manager/password_store_x.h',
+        'browser/password_manager/password_syncable_service.cc',
+        'browser/password_manager/password_syncable_service.h',
         'browser/pepper_broker_infobar_delegate.cc',
         'browser/pepper_broker_infobar_delegate.h',
         'browser/pepper_flash_settings_manager.cc',
