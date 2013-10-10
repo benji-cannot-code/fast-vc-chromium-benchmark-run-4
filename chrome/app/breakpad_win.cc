9aba1f55427ca89505cbd8b2a0ae93aed73966df
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-10 14:30:32 +0000

    Remove child_process_logging.h include from breakpad_win.cc
    BUG=247431
    R=rsesek@chromium.org
    
    Review URL: https://codereview.chromium.org/26817003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227929 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/breakpad_win.cc
+++ chrome/app/breakpad_win.cc
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/win_util.h"
 #include "breakpad/src/client/windows/handler/exception_handler.h"
 #include "chrome/app/hard_error_handler_win.h"
-#include "chrome/common/child_process_logging.h"
 #include "chrome/common/chrome_constants.h"
 #include "components/breakpad/breakpad_client.h"
 #include "content/public/common/content_switches.h"
