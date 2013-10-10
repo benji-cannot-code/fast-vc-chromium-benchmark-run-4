7c988dcb7360f45623191be9a770fafe325ba1fe
srinivasa.ragavan.venkateswaran@intel.com <srinivasa.ragavan.venkateswaran@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-10 17:36:34 +0000

    Fix getComputedStyle background-position to output calc values
    BUG=260097
    Review URL: https://codereview.chromium.org/23651010
    
    Review URL: https://codereview.chromium.org/23651010
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159351 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/CSSComputedStyleDeclaration.cpp
+++ third_party/WebKit/Source/core/css/CSSComputedStyleDeclaration.cpp
@@ -565,7 +565,7 @@ static PassRefPtr<CSSPrimitiveValue> zoomAdjustedPixelValueForLength(const Lengt
 {
     if (length.isFixed())
         return zoomAdjustedPixelValue(length.value(), style);
-    return cssValuePool().createValue(length);
+    return cssValuePool().createValue(length, style);
 }
 
 static PassRefPtr<CSSValue> valueForReflection(const StyleReflection* reflection, const RenderStyle* style)
