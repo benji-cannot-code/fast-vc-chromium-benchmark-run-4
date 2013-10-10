2f0bedbd75534b42003d4e777fb2bf6051fe086d
robertshield@chromium.org <robertshield@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-10 02:25:01 +0000

    Remove file_util debugging cruft from crbug.com/108724
    BUG=108724
    TEST=NONE
    
    Review URL: https://codereview.chromium.org/24235010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227866 0039d316-1c4b-4281-b951-d872f2087c98

--- base/file_util.h
+++ base/file_util.h
@@ -41,8 +41,6 @@ namespace base {
 
 class Time;
 
-extern bool g_bug108724_debug;
-
 //-----------------------------------------------------------------------------
 // Functions that involve filesystem access or modification:
 
