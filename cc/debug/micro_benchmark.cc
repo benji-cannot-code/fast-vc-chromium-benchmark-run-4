83a796fa3d777a78a59306856771c949a6448635
vmpstr@chromium.org <vmpstr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-11 13:46:05 +0000

    cc: Add MicroBenchmarkController.
    This patch adds a MicroBenchmark base class along with
    the MicroBenchmarkController. The idea of these classes is to be
    able to run benchmarks on real trees. That is, we would be able to
    inject benchmarking code into regular operations on the tree.
    
    The plumbing is going into a separate patch, just so each piece
    can be inspected separately. The general idea is the following.
    
    - The controller would live on the LayerTreeHost.
    - Gpu benchmarking extension would get access to it and schedule
      whatever benchmark is specified from javascript. For this reason,
      we decided to keep std::string as a parameter to ScheduleRun,
      instead of an enum
    - The benchmark would then use DidUpdateLayers callback to run
      the benchmark.
    
    Note that there are a couple of empty implementation virtual functions,
    but together with the plumbing patch, it should hopefully make sense.
    
    R=enne@chromium.org,nduca@chromium.org
    
    Review URL: https://codereview.chromium.org/26585002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228177 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ cc/debug/micro_benchmark.cc
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "cc/debug/micro_benchmark.h"
+
+#include "base/callback.h"
+#include "base/memory/scoped_ptr.h"
+#include "base/values.h"
+
+namespace cc {
+
+MicroBenchmark::MicroBenchmark(const DoneCallback& callback)
+    : callback_(callback) {}
+
+MicroBenchmark::~MicroBenchmark() {}
+
+bool MicroBenchmark::IsDone() const {
+  return is_done_;
+}
+
+void MicroBenchmark::DidUpdateLayers(LayerTreeHost* host) {}
+
+void MicroBenchmark::NotifyDone(scoped_ptr<base::Value> result) {
+  callback_.Run(result.Pass());
+  is_done_ = true;
+}
+
+void MicroBenchmark::RunOnLayer(Layer* layer) {}
+
+void MicroBenchmark::RunOnLayer(PictureLayer* layer) {}
+
+}  // namespace cc
