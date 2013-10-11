23764dec263610a0817e64bcbbaf176cfd19759e
sbc@chromium.org <sbc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-11 19:11:50 +0000

    [NaCl SDK] nacl_io: reduce sleep time in event test.
    This test was sleeping for an entire seconds in three
    different places.
    
    The first sleep can be removed copletely and the latter
    two have been reduced to 50ms.  This makes iterating
    on these tests a lot faster.
    
    TEST=trybots
    R=noelallen@chromium.org
    
    Review URL: https://codereview.chromium.org/26877003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228216 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/tests/nacl_io_test/event_test.cc
+++ native_client_sdk/src/tests/nacl_io_test/event_test.cc
@@ -106,7 +106,7 @@ class EmitterTest : public ::testing::Test {
     waiting_++;
     EXPECT_EQ(0, listener.WaitOnEvent(POLLIN, -1));
     emitter_.ClearEvents_Locked(POLLIN);
-    signaled_ ++;
+    signaled_++;
     return NULL;
   }
 
@@ -124,9 +124,6 @@ TEST_F(EmitterTest, MultiThread) {
   for (int a=0; a <NUM_THREADS; a++)
     CreateThread();
 
-  sleep(1);
-  EXPECT_EQ(0, signaled_);
-
   {
     AUTO_LOCK(emitter_.GetLock());
 
@@ -134,10 +131,15 @@ TEST_F(EmitterTest, MultiThread) {
     while(waiting_ < NUM_THREADS)
       pthread_cond_wait(&multi_cond_, emitter_.GetLock().mutex());
 
+    ASSERT_EQ(0, signaled_);
+
     emitter_.RaiseEvents_Locked(POLLIN);
   }
 
-  sleep(1);
+  // sleep for 50 milliseconds
+  struct timespec sleeptime = { 0,  50 * 1000 * 1000 };
+  nanosleep(&sleeptime, NULL);
+
   EXPECT_EQ(1, signaled_);
 
   {
@@ -145,7 +147,7 @@ TEST_F(EmitterTest, MultiThread) {
     emitter_.RaiseEvents_Locked(POLLIN);
   }
 
-  sleep(1);
+  nanosleep(&sleeptime, NULL);
   EXPECT_EQ(2, signaled_);
 
   // Clean up remaining threads.
@@ -154,7 +156,6 @@ TEST_F(EmitterTest, MultiThread) {
     emitter_.RaiseEvents_Locked(POLLIN);
   }
 }
-
 
 TEST(PipeTest, Listener) {
   const char hello[] = "Hello World.";
