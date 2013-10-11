39c78bd33cf3e75da5493d4ab6f0f519f423b79d
arv@chromium.org <arv@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-11 21:18:11 +0000

    Revert "URLUtils: Use ImplementedAs in the idl"
    This reverts commit f72850317775b777610e5f299dfdbd3b47562966.
    
    Broke compilation
    
    BUG=None
    TBR=ojan@chromium.org
    
    Review URL: https://codereview.chromium.org/27069002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159477 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/DOMURLUtilsReadOnly.h
+++ third_party/WebKit/Source/core/dom/DOMURLUtilsReadOnly.h
@@ -41,6 +41,7 @@ public:
     virtual String input() const = 0;
 
     static String href(DOMURLUtilsReadOnly*);
+    static String toString(DOMURLUtilsReadOnly*);
 
     static String protocol(DOMURLUtilsReadOnly*);
     static String host(DOMURLUtilsReadOnly*);
