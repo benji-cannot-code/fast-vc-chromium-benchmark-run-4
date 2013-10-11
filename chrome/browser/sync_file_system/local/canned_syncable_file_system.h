4465595078c35c0ab810d787095fdc79c139e8b1
nhiroki@chromium.org <nhiroki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-11 04:53:27 +0000

    FileAPI: {Open,Delete}FileSystem should run on IO thread
    FileSystemContext::{Open,Delete}FileSystem should run on IO thread, but some
    tests violate the rule. This forces these tests to follow the rule.
    
    
    BUG=none
    TEST=unit_tests --gtest_filter=LocalFileSyncContextTest.*
    TEST=unit_tests --gtest_filter=SyncFileSystemServiceTest.*
    
    Review URL: https://codereview.chromium.org/25209002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228117 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/local/canned_syncable_file_system.h
+++ chrome/browser/sync_file_system/local/canned_syncable_file_system.h
@@ -56,6 +56,10 @@ class SyncFileSystemBackend;
 class CannedSyncableFileSystem
     : public LocalFileSyncStatus::Observer {
  public:
+  typedef base::Callback<void(const GURL& root,
+                              const std::string& name,
+                              base::PlatformFileError result)>
+      OpenFileSystemCallback;
   typedef base::Callback<void(base::PlatformFileError)> StatusCallback;
   typedef base::Callback<void(int64)> WriteCallback;
   typedef fileapi::FileSystemOperation::FileEntryList FileEntryList;
@@ -154,6 +158,7 @@ class CannedSyncableFileSystem
 
   // Operation methods body.
   // They can be also called directly if the caller is already on IO thread.
+  void DoOpenFileSystem(const OpenFileSystemCallback& callback);
   void DoCreateDirectory(const fileapi::FileSystemURL& url,
                          const StatusCallback& callback);
   void DoCreateFile(const fileapi::FileSystemURL& url,
@@ -203,7 +208,8 @@ class CannedSyncableFileSystem
   typedef ObserverListThreadSafe<LocalFileSyncStatus::Observer> ObserverList;
 
   // Callbacks.
-  void DidOpenFileSystem(const GURL& root,
+  void DidOpenFileSystem(base::SingleThreadTaskRunner* original_task_runner,
+                         const GURL& root,
                          const std::string& name,
                          base::PlatformFileError result);
   void DidInitializeFileSystemContext(sync_file_system::SyncStatusCode status);
@@ -226,7 +232,7 @@ class CannedSyncableFileSystem
 
   // Boolean flags mainly for helping debug.
   bool is_filesystem_set_up_;
-  bool is_filesystem_opened_;
+  bool is_filesystem_opened_;  // Should be accessed only on the IO thread.
 
   scoped_refptr<ObserverList> sync_status_observers_;
 
