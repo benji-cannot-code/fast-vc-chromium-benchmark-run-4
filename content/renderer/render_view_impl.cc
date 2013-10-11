f9d22aafb9c77ef3a529d40fe3d5d8f25c64d895
vollick@chromium.org <vollick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-11 15:22:17 +0000

    Add a flag for enabling/disabling the new accelerated scrolling path
    We now have two accelerated scrolling paths. One accelerates scrolling whenever
    it's safe to promote the overflow scrolling div to a stacking container /
    containing block and not break stacking or clipping.
    
    The new path, the 'universal' path, enables accelerated scrolling in those cases
    where the old path would not opt-in.
    
    Eventually, we'd like to eliminate the old path, but this will require layer
    squashing (the new path can result in many composited layers that ought to be
    combined -- i.e., a 'layer explosion'). Until these paths are mature, we need to
    be able to enable and disable them both, so we have two separate sets of flags.
    
    Once we've eliminated the old path, we can replace both with
    --[enable/disable]-accelerated-overflow-scroll (and eventually get rid of both
    sets of flags).
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/26809004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228187 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -603,6 +603,20 @@ static bool ShouldUseAcceleratedCompositingForOverflowScroll(
   return DeviceScaleEnsuresTextQuality(device_scale_factor);
 }
 
+static bool ShouldUseUniversalAcceleratedCompositingForOverflowScroll() {
+  const CommandLine& command_line = *CommandLine::ForCurrentProcess();
+
+  if (command_line.HasSwitch(
+          switches::kDisableUniversalAcceleratedOverflowScroll))
+    return false;
+
+  if (command_line.HasSwitch(
+          switches::kEnableUniversalAcceleratedOverflowScroll))
+    return true;
+
+  return false;
+}
+
 static bool ShouldUseAcceleratedCompositingForScrollableFrames(
     float device_scale_factor) {
   const CommandLine& command_line = *CommandLine::ForCurrentProcess();
@@ -924,6 +938,8 @@ void RenderViewImpl::Initialize(RenderViewImplParams* params) {
       ShouldUseFixedPositionCompositing(device_scale_factor_));
   webview()->settings()->setAcceleratedCompositingForOverflowScrollEnabled(
       ShouldUseAcceleratedCompositingForOverflowScroll(device_scale_factor_));
+  webview()->settings()->setCompositorDrivenAcceleratedScrollingEnabled(
+      ShouldUseUniversalAcceleratedCompositingForOverflowScroll());
   webview()->settings()->setAcceleratedCompositingForTransitionEnabled(
       ShouldUseTransitionCompositing(device_scale_factor_));
   webview()->settings()->setAcceleratedCompositingForFixedRootBackgroundEnabled(
