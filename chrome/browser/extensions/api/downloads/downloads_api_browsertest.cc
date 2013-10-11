1d5781bd7c6ad39c24005b7b88c4705d165ee3ca
abodenha@chromium.org <abodenha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-11 21:18:11 +0000

    Disable DownloadExtensionTests
    R=grt@chromium.org
    TBR=benjhayden@chromium.org
    NOTRY=true
    BUG=306144
    
    Review URL: https://codereview.chromium.org/26501006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228255 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/downloads/downloads_api_browsertest.cc
+++ chrome/browser/extensions/api/downloads/downloads_api_browsertest.cc
@@ -53,6 +53,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/browser/fileapi/file_system_operation_runner.h"
 #include "webkit/browser/fileapi/file_system_url.h"
 
+// Disable everything due to issue 306144
+#if 0
 using content::BrowserContext;
 using content::BrowserThread;
 using content::DownloadItem;
@@ -3611,3 +3613,4 @@ TEST(ExtensionDetermineDownloadFilenameInternal,
             warnings.begin()->warning_type());
   EXPECT_EQ("incumbent", warnings.begin()->extension_id());
 }
+#endif // Issue 306144
