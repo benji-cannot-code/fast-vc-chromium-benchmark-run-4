1470ab7f60789507807f907c45bf1803a57d5341
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-11 03:27:26 +0000

    Use GUID, instead of sequential ID, if notification ID is not provided
    BUG=306294
    TEST=existing tests
    
    Review URL: https://codereview.chromium.org/26905003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228093 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/notifications/notifications_api.cc
+++ chrome/browser/extensions/api/notifications/notifications_api.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/notifications/notifications_api.h"
 
 #include "base/callback.h"
+#include "base/guid.h"
+#include "base/rand_util.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
@@ -475,10 +477,6 @@ NotificationsApiFunction::MapApiTemplateTypeToType(
   }
 }
 
-const char kNotificationPrefix[] = "extensions.api.";
-
-static uint64 next_id_ = 0;
-
 NotificationsCreateFunction::NotificationsCreateFunction() {
 }
 
@@ -489,17 +487,18 @@ bool NotificationsCreateFunction::RunNotificationsApi() {
   params_ = api::notifications::Create::Params::Create(*args_);
   EXTENSION_FUNCTION_VALIDATE(params_.get());
 
-  // If the caller provided a notificationId, use that. Otherwise, generate
-  // one. Note that there's nothing stopping an app developer from passing in
-  // arbitrary "extension.api.999" notificationIds that will collide with
-  // future generated IDs. It doesn't seem necessary to try to prevent this; if
-  // developers want to hurt themselves, we'll let them.
   const std::string extension_id(extension_->id());
   std::string notification_id;
-  if (!params_->notification_id.empty())
+  if (!params_->notification_id.empty()) {
+    // If the caller provided a notificationId, use that.
     notification_id = params_->notification_id;
-  else
-    notification_id = kNotificationPrefix + base::Uint64ToString(next_id_++);
+  } else {
+    // Otherwise, use a randomly created GUID. In case that GenerateGUID returns
+    // the empty string, simply generate a random string.
+    notification_id = base::GenerateGUID();
+    if (notification_id.empty())
+      notification_id = base::RandBytesAsString(16);
+  }
 
   SetResult(new base::StringValue(notification_id));
 
