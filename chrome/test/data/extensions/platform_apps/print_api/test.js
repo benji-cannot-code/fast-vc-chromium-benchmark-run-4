1256fed481ed71f6ce8a7c316aa6e6fe7bf7cb55
dharcourt@chromium.org <dharcourt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-11 19:32:30 +0000

    The WindowDotPrintShouldBringUpPrintPreview browser test was generating false positives because the print preview dialog wasn't getting a chance to auto-cancel before the cancel was being verified. This was fixed by using a delegate to explicitly wait until the preview is ready instead of using a cancel verification as a proxy for that preview readyness verification.
    The ClosingWindowWhilePrintingShouldNotCrash browser tests was generating false negatives because the print preview dialog was being closed too quickly in the test. This was fixed by making the test's close better simulate a real user close, and by using a delegate to wait until the print preview is ready before closing.
    
    Review URL: https://codereview.chromium.org/26527003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228224 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/platform_apps/print_api/test.js
+++ chrome/test/data/extensions/platform_apps/print_api/test.js
@@ -5,18 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.test.getConfig(function(config) {
-    chrome.test.runTests([
-      function testWindowDotPrintApi() {
-        chrome.app.window.create('test.html', {}, chrome.test.callbackPass(
-            function(appWindow) {
-              appWindow.contentWindow.onload = chrome.test.callbackPass(
-                  function() {
-                    appWindow.contentWindow.print();
-                    if (config.customArg === 'close')
-                      appWindow.contentWindow.close();
-                  });
-            }));
-      }
-    ]);
+    chrome.app.window.create('test.html', {}, function(appWindow) {
+      appWindow.contentWindow.onload = function() {
+        appWindow.contentWindow.print();
+        chrome.test.notifyPass();
+      };
+    });
   });
 });
