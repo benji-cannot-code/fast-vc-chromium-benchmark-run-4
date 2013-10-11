94743689cbef7cc453304524c6c78b574129e50c
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-11 05:57:26 +0000

    app_list: Use existing item when installing an already installed app.
    BUG=306004
    
    Review URL: https://codereview.chromium.org/26901004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228124 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/app_list/extension_app_model_builder_unittest.cc
+++ chrome/browser/ui/app_list/extension_app_model_builder_unittest.cc
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_function_test_utils.h"
 #include "chrome/browser/extensions/extension_service_unittest.h"
 #include "chrome/browser/extensions/extension_sorting.h"
+#include "chrome/browser/extensions/install_tracker.h"
+#include "chrome/browser/extensions/install_tracker_factory.h"
 #include "chrome/common/extensions/extension_constants.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/test/base/testing_profile.h"
@@ -181,6 +183,21 @@ TEST_F(ExtensionAppModelBuilderTest, UninstallTerminatedApp) {
             GetModelContent(model_.get()));
 
   base::RunLoop().RunUntilIdle();
+}
+
+TEST_F(ExtensionAppModelBuilderTest, Reinstall) {
+  ExtensionAppModelBuilder builder(profile_.get(), model_.get(), NULL);
+  EXPECT_EQ(std::string("Packaged App 1,Packaged App 2,Hosted App"),
+            GetModelContent(model_.get()));
+
+  // Install kPackagedApp1Id again should not create a new entry.
+  extensions::InstallTracker* tracker =
+      extensions::InstallTrackerFactory::GetForProfile(profile_.get());
+  tracker->OnBeginExtensionInstall(
+      kPackagedApp1Id, "", gfx::ImageSkia(), true, true);
+
+  EXPECT_EQ(std::string("Packaged App 1,Packaged App 2,Hosted App"),
+            GetModelContent(model_.get()));
 }
 
 TEST_F(ExtensionAppModelBuilderTest, OrdinalPrefsChange) {
