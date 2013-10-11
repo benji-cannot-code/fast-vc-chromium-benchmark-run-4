4bdc3354efff8b34d56dae709df8f0c3fe394570
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-11 04:22:34 +0000

    Revert 227999 "Alternative workaround for mac kernel bug."
    > Alternative workaround for mac kernel bug.
    > 
    > BUG=298276
    > 
    > Review URL: https://codereview.chromium.org/25325002
    
    This change seemed to cause the Mac ASAN bot to timeout when running
    the ipc_tests.
    
    TBR=hubbe@chromium.org
    
    Review URL: https://codereview.chromium.org/26384003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228110 0039d316-1c4b-4281-b951-d872f2087c98

--- ipc/ipc_channel_reader.cc
+++ ipc/ipc_channel_reader.cc
@@ -39,15 +39,9 @@ bool ChannelReader::AsyncReadComplete(int bytes_read) {
   return DispatchInputData(input_buf_, bytes_read);
 }
 
-bool ChannelReader::IsInternalMessage(const Message& m) const {
-  return m.routing_id() == MSG_ROUTING_NONE &&
-      m.type() >= Channel::CLOSE_FD_MESSAGE_TYPE &&
-      m.type() <= Channel::HELLO_MESSAGE_TYPE;
-}
-
 bool ChannelReader::IsHelloMessage(const Message& m) const {
   return m.routing_id() == MSG_ROUTING_NONE &&
-      m.type() == Channel::HELLO_MESSAGE_TYPE;
+         m.type() == Channel::HELLO_MESSAGE_TYPE;
 }
 
 bool ChannelReader::DispatchInputData(const char* input_data,
@@ -91,8 +85,8 @@ bool ChannelReader::DispatchInputData(const char* input_data,
                    "line", IPC_MESSAGE_ID_LINE(m.type()));
 #endif
       m.TraceMessageEnd();
-      if (IsInternalMessage(m))
-        HandleInternalMessage(m);
+      if (IsHelloMessage(m))
+        HandleHelloMessage(m);
       else
         listener_->OnMessageReceived(m);
       p = message_tail;
