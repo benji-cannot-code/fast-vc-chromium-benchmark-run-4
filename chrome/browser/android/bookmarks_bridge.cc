8f5bae66b984ce2e2af4171a4c0e5e543a7a863e
aurimas@chromium.org <aurimas@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-11 00:20:37 +0000

    Upstreaming isEditBookmarksEnabled call.
    BUG=303312
    
    Review URL: https://codereview.chromium.org/26477003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228056 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/android/bookmarks_bridge.cc
+++ chrome/browser/android/bookmarks_bridge.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/bookmarks/bookmark_model_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_android.h"
+#include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/common/pref_names.h"
 #include "content/public/browser/browser_thread.h"
 #include "jni/BookmarksBridge_jni.h"
@@ -29,7 +30,6 @@ BookmarksBridge::BookmarksBridge(JNIEnv* env,
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
   Profile* profile = ProfileAndroid::FromProfileAndroid(j_profile);
   bookmark_model_ = BookmarkModelFactory::GetForProfile(profile);
-  pref_service_ = profile->GetPrefs();
 
   // Registers the notifications we are interested.
   bookmark_model_->AddObserver(this);
@@ -53,6 +53,15 @@ bool BookmarksBridge::RegisterBookmarksBridge(JNIEnv* env) {
 static jint Init(JNIEnv* env, jobject obj, jobject j_profile) {
   BookmarksBridge* delegate = new BookmarksBridge(env, obj, j_profile);
   return reinterpret_cast<jint>(delegate);
+}
+
+static bool IsEditBookmarksEnabled() {
+  return ProfileManager::GetLastUsedProfile()->GetPrefs()->GetBoolean(
+      prefs::kEditBookmarksEnabled);
+}
+
+static jboolean IsEditBookmarksEnabled(JNIEnv* env, jclass clazz) {
+  return IsEditBookmarksEnabled();
 }
 
 void BookmarksBridge::GetBookmarksForFolder(JNIEnv* env,
@@ -154,7 +163,7 @@ bool BookmarksBridge::IsEditable(const BookmarkNode* node) const {
   return node &&
          (node->type() == BookmarkNode::FOLDER ||
           node->type() == BookmarkNode::URL) &&
-         pref_service_->GetBoolean(prefs::kEditBookmarksEnabled);
+          IsEditBookmarksEnabled();
 }
 
 // ------------- Observer-related methods ------------- //
--- chrome/browser/android/bookmarks_bridge.h
+++ chrome/browser/android/bookmarks_bridge.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "chrome/browser/bookmarks/base_bookmark_model_observer.h"
 
-class PrefService;
 class Profile;
 
 // The delegate to fetch bookmarks information for the Android native
@@ -80,7 +79,6 @@ class BookmarksBridge : public BaseBookmarkModelObserver {
 
   JavaObjectWeakGlobalRef weak_java_ref_;
   BookmarkModel* bookmark_model_;  // weak
-  PrefService* pref_service_;  // weak
 
   DISALLOW_COPY_AND_ASSIGN(BookmarksBridge);
 };
