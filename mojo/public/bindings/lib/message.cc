ac5d9119f852a5f8b62506425ef183abd70e7571
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-11 10:52:31 +0000

    C++ bindings
    This CL includes code for a static library that supports C++ bindings to the Mojo message format. A sample interface with "generated" bindings is included in the sample/ folder along with sample_test.cc that shows how the generated code may be used.
    
    This is a WIP but seems worth snapshotting as is. See the TODO file for a list of near term improvements, most notably testing.
    
    Review URL: https://codereview.chromium.org/23913008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228153 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ mojo/public/bindings/lib/message.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "mojo/public/bindings/lib/message.h"
+
+#include <stdlib.h>
+
+namespace mojo {
+
+Message::Message()
+    : data(NULL) {
+}
+
+Message::~Message() {
+}
+
+}  // namespace mojo
