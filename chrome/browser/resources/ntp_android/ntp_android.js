ecc078db30216e25968db266d7310ccb2393dadf
arv@chromium.org <arv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-11 01:20:32 +0000

    Remove usage of deprecated DOM API
    Use defaultPrevented instead of returnValue
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/26924002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228073 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/ntp_android/ntp_android.js
+++ chrome/browser/resources/ntp_android/ntp_android.js
@@ -1184,9 +1184,7 @@ cr.define('ntp', function() {
    * @param {Object} evt User interface event that triggered the action.
    */
   function executePromoAction(evt) {
-    if (evt.preventDefault)
-      evt.preventDefault();
-    evt.returnValue = false;
+    evt.preventDefault();
     chrome.send('promoActionTriggered');
   }
 
