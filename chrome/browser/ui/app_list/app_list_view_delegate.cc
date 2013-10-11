a1ac9d10604c7d917a4eb85b6ad6e72b690c51a5
calamity@chromium.org <calamity@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-11 01:22:55 +0000

    Make app list profile selector reload on non-active user signin status change.
    The AppListViewDelegate only listens to signin events from the current app list
    profile. This CL makes it listen to all profiles which is necessary to update
    the signin details in the menu when a user that isn't the active app list user
    signs into chrome.
    
    BUG=305475
    
    Review URL: https://codereview.chromium.org/26653003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228075 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/app_list/app_list_view_delegate.cc
+++ chrome/browser/ui/app_list/app_list_view_delegate.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/extensions/extension_constants.h"
 #include "chrome/common/url_constants.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/notification_service.h"
 #include "content/public/browser/notification_source.h"
 #include "content/public/browser/page_navigator.h"
 #include "content/public/browser/user_metrics.h"
@@ -95,11 +96,11 @@ void AppListViewDelegate::RegisterForNotifications() {
   DCHECK(profile_);
 
   registrar_.Add(this, chrome::NOTIFICATION_GOOGLE_SIGNIN_SUCCESSFUL,
-                 content::Source<Profile>(profile_));
+                 content::NotificationService::AllSources());
   registrar_.Add(this, chrome::NOTIFICATION_GOOGLE_SIGNIN_FAILED,
-                 content::Source<Profile>(profile_));
+                 content::NotificationService::AllSources());
   registrar_.Add(this, chrome::NOTIFICATION_GOOGLE_SIGNED_OUT,
-                 content::Source<Profile>(profile_));
+                 content::NotificationService::AllSources());
 }
 
 void AppListViewDelegate::OnProfileChanged() {
