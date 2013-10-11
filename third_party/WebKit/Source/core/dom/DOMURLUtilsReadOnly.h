17846534fb49f23ed8caac2e68750c1e15efc837
arv@chromium.org <arv@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-11 20:42:24 +0000

    URLUtils: Use ImplementedAs in the idl
    This is so that we do not need to have a toString in the C++ code.
    
    BUG=None
    R=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/26960005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159476 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/DOMURLUtilsReadOnly.h
+++ third_party/WebKit/Source/core/dom/DOMURLUtilsReadOnly.h
@@ -41,7 +41,6 @@ public:
     virtual String input() const = 0;
 
     static String href(DOMURLUtilsReadOnly*);
-    static String toString(DOMURLUtilsReadOnly*);
 
     static String protocol(DOMURLUtilsReadOnly*);
     static String host(DOMURLUtilsReadOnly*);
