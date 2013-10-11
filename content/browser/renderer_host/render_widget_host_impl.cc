4f9e9ab20490858461d0850c35e326c09d34a423
zturner@chromium.org <zturner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-11 01:04:41 +0000

    Revert 228045 "Treat all coordinates to RWHVA::SetBounds() as sc..."
    > Treat all coordinates to RWHVA::SetBounds() as screen coordinates.
    > 
    > This is a speculative re-upload, in the hopes that random crashes we are seeing on the ChromeOS waterfall will have been fixed by Issue 268648 (issue is not re-producible anywhere but the waterfall).  If CrOS waterfall begins experiencing memory corruption errors, this change should be reverted.
    > 
    > BUG=176221
    > TBR=sky
    > 
    > Review URL: https://codereview.chromium.org/24901003
    
    TBR=zturner@chromium.org
    
    Review URL: https://codereview.chromium.org/26489008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228071 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_impl.cc
+++ content/browser/renderer_host/render_widget_host_impl.cc
@@ -1505,6 +1505,7 @@ void RenderWidgetHostImpl::OnUpdateScreenRectsAck() {
 }
 
 void RenderWidgetHostImpl::OnRequestMove(const gfx::Rect& pos) {
+  // Note that we ignore the position.
   if (view_) {
     view_->SetBounds(pos);
     Send(new ViewMsg_Move_ACK(routing_id_));
