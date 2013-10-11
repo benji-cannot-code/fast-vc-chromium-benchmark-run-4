3b2db7f7869969d019d45cd12b4004d087d4d756
arv@chromium.org <arv@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-11 17:59:49 +0000

    Implement URLUtilsReadOnly and let WorkerLocation use that
    This splits the implementation into DOMURLUtils and DOMURLUtilsReadOnly
    
    This way we are now sharing the implementation of the URL related
    getters.
    
    BUG=303152
    R=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/26839003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159464 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/DOMURLUtils.cpp
+++ third_party/WebKit/Source/core/dom/DOMURLUtils.cpp
@@ -34,22 +34,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-String DOMURLUtils::href(DOMURLUtils* impl)
-{
-    const KURL& url = impl->url();
-    if (url.isNull())
-        return impl->input();
-    return url.string();
-}
-
 void DOMURLUtils::setHref(DOMURLUtils* impl, const String& value)
 {
     impl->setInput(value);
-}
-
-String DOMURLUtils::toString(DOMURLUtils* impl)
-{
-    return href(impl);
 }
 
 String DOMURLUtils::origin(DOMURLUtils* impl)
@@ -60,11 +47,6 @@ String DOMURLUtils::origin(DOMURLUtils* impl)
 
     RefPtr<SecurityOrigin> origin = SecurityOrigin::create(url);
     return origin->toString();
-}
-
-String DOMURLUtils::protocol(DOMURLUtils* impl)
-{
-    return impl->url().protocol() + ":";
 }
 
 void DOMURLUtils::setProtocol(DOMURLUtils* impl, const String& value)
@@ -102,16 +84,6 @@ void DOMURLUtils::setPassword(DOMURLUtils* impl, const String& value)
         return;
     url.setPass(value);
     impl->setURL(url);
-}
-
-String DOMURLUtils::host(DOMURLUtils* impl)
-{
-    const KURL& url = impl->url();
-    if (url.hostEnd() == url.pathStart())
-        return url.host();
-    if (isDefaultPortForProtocol(url.port(), url.protocol()))
-        return url.host();
-    return url.host() + ":" + String::number(url.port());
 }
 
 // This function does not allow leading spaces before the port number.
@@ -160,11 +132,6 @@ void DOMURLUtils::setHost(DOMURLUtils* impl, const String& value)
     impl->setURL(url);
 }
 
-String DOMURLUtils::hostname(DOMURLUtils* impl)
-{
-    return impl->url().host();
-}
-
 void DOMURLUtils::setHostname(DOMURLUtils* impl, const String& value)
 {
     KURL url = impl->url();
@@ -186,15 +153,6 @@ void DOMURLUtils::setHostname(DOMURLUtils* impl, const String& value)
     impl->setURL(url);
 }
 
-String DOMURLUtils::port(DOMURLUtils* impl)
-{
-    const KURL& url = impl->url();
-    if (url.hasPort())
-        return String::number(url.port());
-
-    return emptyString();
-}
-
 void DOMURLUtils::setPort(DOMURLUtils* impl, const String& value)
 {
     KURL url = impl->url();
@@ -213,11 +171,6 @@ void DOMURLUtils::setPort(DOMURLUtils* impl, const String& value)
     impl->setURL(url);
 }
 
-String DOMURLUtils::pathname(DOMURLUtils* impl)
-{
-    return impl->url().path();
-}
-
 void DOMURLUtils::setPathname(DOMURLUtils* impl, const String& value)
 {
     KURL url = impl->url();
@@ -232,12 +185,6 @@ void DOMURLUtils::setPathname(DOMURLUtils* impl, const String& value)
     impl->setURL(url);
 }
 
-String DOMURLUtils::search(DOMURLUtils* impl)
-{
-    String query = impl->url().query();
-    return query.isEmpty() ? emptyString() : "?" + query;
-}
-
 void DOMURLUtils::setSearch(DOMURLUtils* impl, const String& value)
 {
     KURL url = impl->url();
@@ -249,14 +196,6 @@ void DOMURLUtils::setSearch(DOMURLUtils* impl, const String& value)
     url.setQuery(newSearch.replaceWithLiteral('#', "%23"));
 
     impl->setURL(url);
-}
-
-String DOMURLUtils::hash(DOMURLUtils* impl)
-{
-    String fragmentIdentifier = impl->url().fragmentIdentifier();
-    if (fragmentIdentifier.isEmpty())
-        return emptyString();
-    return AtomicString(String("#" + fragmentIdentifier));
 }
 
 void DOMURLUtils::setHash(DOMURLUtils* impl, const String& value)
