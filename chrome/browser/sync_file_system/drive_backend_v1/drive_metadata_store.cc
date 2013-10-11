ce8723fea6c2083541e0cd1d3604ffea39766ea8
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-11 06:48:44 +0000

    [SyncFS] Thread restriction fix on a error case of DriveMetadataStore
    TEST=n/a
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/26223003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228137 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_backend_v1/drive_metadata_store.cc
+++ chrome/browser/sync_file_system/drive_backend_v1/drive_metadata_store.cc
@@ -388,6 +388,7 @@ void DriveMetadataStore::DidInitialize(const InitializationCallback& callback,
   db_status_ = contents->status;
   if (db_status_ != SYNC_STATUS_OK) {
     callback.Run(db_status_, false);
+    file_task_runner_->DeleteSoon(FROM_HERE, contents.release());
     return;
   }
 
