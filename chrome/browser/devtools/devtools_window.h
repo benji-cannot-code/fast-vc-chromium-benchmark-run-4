ccecc809ff16e36e47033021de9182bcbed39281
lushnikov@chromium.org <lushnikov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-12 17:01:19 +0000

    DevTools: support beforeunload lifecycle for DevTools window
    This patch teaches DevTools window to send beforeunload events to
    its frontend. This events won't be send if the inspected webcontents
    got closed - additional work is needed to handle this case.
    
    BUG=289552
    TBR=pfeldman
    
    Review URL: https://codereview.chromium.org/23766027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228346 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/devtools/devtools_window.h
+++ chrome/browser/devtools/devtools_window.h
@@ -167,6 +167,9 @@ class DevToolsWindow : private content::NotificationObserver,
                               bool user_gesture,
                               bool* was_blocked) OVERRIDE;
   virtual void CloseContents(content::WebContents* source) OVERRIDE;
+  virtual void BeforeUnloadFired(content::WebContents* tab,
+                                 bool proceed,
+                                 bool* proceed_to_fire_unload) OVERRIDE;
   virtual bool PreHandleKeyboardEvent(
       content::WebContents* source,
       const content::NativeWebKeyboardEvent& event,
@@ -189,6 +192,7 @@ class DevToolsWindow : private content::NotificationObserver,
 
   // DevToolsEmbedderMessageDispatcher::Delegate overrides:
   virtual void ActivateWindow() OVERRIDE;
+  virtual void ActivateContents(content::WebContents* contents) OVERRIDE;
   virtual void CloseWindow() OVERRIDE;
   virtual void SetWindowBounds(int x, int y, int width, int height) OVERRIDE;
   virtual void MoveWindow(int x, int y) OVERRIDE;
@@ -233,7 +237,6 @@ class DevToolsWindow : private content::NotificationObserver,
   BrowserWindow* GetInspectedBrowserWindow();
   bool IsInspectedBrowserPopup();
   void UpdateFrontendDockSide();
-  void Hide();
   void ScheduleAction(DevToolsToggleAction action);
   void DoAction();
   void UpdateTheme();
