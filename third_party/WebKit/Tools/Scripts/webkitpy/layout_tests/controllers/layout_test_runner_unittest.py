872c997b4f35fa88764bc58e57a940057fda8e64
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-12 07:12:42 +0000

    For http tests, we still need to restrict to --max-locked-shards because otherwise they get flaky waiting on the http server.
    For virtual tests, we don't actually want to make them fully parallel in practice because each time we switch to/from a virtual test suite, we need to restart content_shell and that takes forever.
    
    On my machine, I got the following times:
    run-webkit-tests --no-retry == 2m56s
    run-webkit-tests --no-retry -f == from 6m30s down to 3m15s
    run-webkit-tests --no-retry -f --order=random == from 28m (!!) to 2m25s
    
    I have no idea why --order-random is faster, but I tried each one a couple times to be sure and the numbers were very close each time.
    
    Also, -f and --order=random do result in extra flakiness, so they still aren't quite ready for primetime, but this brings it a lot closer.
    
    Review URL: https://codereview.chromium.org/26878008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159504 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/controllers/layout_test_runner_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/controllers/layout_test_runner_unittest.py
@@ -170,7 +170,8 @@ class SharderTests(unittest.TestCase):
         "ietestcenter/Javascript/11.1.5_4-4-c-1.html",
         "dom/html/level2/html/HTMLAnchorElement06.html",
         "perf/object-keys.html",
-        "virtual/threaded/test.html",
+        "virtual/threaded/dir/test.html",
+        "virtual/threaded/fast/foo/test.html",
     ]
 
     def get_test_input(self, test_file):
@@ -204,7 +205,8 @@ class SharderTests(unittest.TestCase):
                 'http/tests/xmlhttprequest/supported-xml-content-types.html',
                 'perf/object-keys.html'])])
         self.assert_shards(unlocked,
-            [('virtual/threaded', ['virtual/threaded/test.html']),
+            [('virtual/threaded/dir', ['virtual/threaded/dir/test.html']),
+             ('virtual/threaded/fast/foo', ['virtual/threaded/fast/foo/test.html']),
              ('animations', ['animations/keyframes.html']),
              ('dom/html/level2/html', ['dom/html/level2/html/HTMLAnchorElement03.html',
                                       'dom/html/level2/html/HTMLAnchorElement06.html']),
@@ -212,20 +214,23 @@ class SharderTests(unittest.TestCase):
              ('ietestcenter/Javascript', ['ietestcenter/Javascript/11.1.5_4-4-c-1.html'])])
 
     def test_shard_every_file(self):
-        locked, unlocked = self.get_shards(num_workers=2, fully_parallel=True)
+        locked, unlocked = self.get_shards(num_workers=2, fully_parallel=True, max_locked_shards=2)
         self.assert_shards(locked,
-            [('.', ['http/tests/websocket/tests/unicode.htm']),
-             ('.', ['http/tests/security/view-source-no-refresh.html']),
-             ('.', ['http/tests/websocket/tests/websocket-protocol-ignored.html']),
-             ('.', ['http/tests/xmlhttprequest/supported-xml-content-types.html']),
-             ('.', ['perf/object-keys.html'])]),
+            [('locked_shard_1',
+              ['http/tests/websocket/tests/unicode.htm',
+               'http/tests/security/view-source-no-refresh.html',
+               'http/tests/websocket/tests/websocket-protocol-ignored.html']),
+             ('locked_shard_2',
+              ['http/tests/xmlhttprequest/supported-xml-content-types.html',
+               'perf/object-keys.html'])]),
         self.assert_shards(unlocked,
-            [('.', ['animations/keyframes.html']),
+            [('virtual/threaded/dir', ['virtual/threaded/dir/test.html']),
+             ('virtual/threaded/fast/foo', ['virtual/threaded/fast/foo/test.html']),
+             ('.', ['animations/keyframes.html']),
              ('.', ['fast/css/display-none-inline-style-change-crash.html']),
              ('.', ['dom/html/level2/html/HTMLAnchorElement03.html']),
              ('.', ['ietestcenter/Javascript/11.1.5_4-4-c-1.html']),
-             ('.', ['dom/html/level2/html/HTMLAnchorElement06.html']),
-             ('.', ['virtual/threaded/test.html'])])
+             ('.', ['dom/html/level2/html/HTMLAnchorElement06.html'])])
 
     def test_shard_in_two(self):
         locked, unlocked = self.get_shards(num_workers=1, fully_parallel=False)
@@ -243,7 +248,8 @@ class SharderTests(unittest.TestCase):
                'dom/html/level2/html/HTMLAnchorElement03.html',
                'ietestcenter/Javascript/11.1.5_4-4-c-1.html',
                'dom/html/level2/html/HTMLAnchorElement06.html',
-               'virtual/threaded/test.html'])])
+               'virtual/threaded/dir/test.html',
+               'virtual/threaded/fast/foo/test.html'])])
 
     def test_shard_in_two_has_no_locked_shards(self):
         locked, unlocked = self.get_shards(num_workers=1, fully_parallel=False,
