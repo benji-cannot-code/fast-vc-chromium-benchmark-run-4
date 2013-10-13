ad0e09f0c37dff99ab50b103ad96da20c93bb877
frankf@chromium.org <frankf@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-13 18:33:37 +0000

    [Telemetry] Build and install memconsumer app.
    BUG=None
    
    Review URL: https://codereview.chromium.org/26932003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228410 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/platform/profiler/oomkiller_profiler.py
+++ tools/telemetry/telemetry/core/platform/profiler/oomkiller_profiler.py
@@ -3,6 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import os
+
+from telemetry.core import util
 from telemetry.core.backends.chrome import android_browser_finder
 from telemetry.core.platform import profiler
 
@@ -27,6 +30,12 @@ class OOMKillerProfiler(profiler.Profiler):
         browser_backend, platform_backend, output_path, state)
     if not 'mem_consumer_launched' in state:
       state['mem_consumer_launched'] = True
+      mem_consumer_path = util.FindSupportBinary(
+          os.path.join('apks', 'MemConsumer.apk'),
+          executable=False)
+      assert mem_consumer_path, ('Could not find memconsumer app. Please build '
+                                 'memconsumer target.')
+      self._browser_backend.adb.Install(mem_consumer_path)
       self._browser_backend.adb.GoHome()
       self._platform_backend.LaunchApplication(
           'org.chromium.memconsumer/.MemConsumer',
