2dce09d09d61095180576628a71d0d207dac1990
skyostil@chromium.org <skyostil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-14 11:50:36 +0000

    telemetry: Enable GPU-specific test expectations on Android
    Make it possible to specify on a GPU vendor/device basis whether a
    telemetry test is expected to pass on Android. On desktop and CrOS we
    use the PCI device identifiers for this purpose, but they are not
    available on Android. Instead, this patch makes lets us match
    expectations against the GL_VENDOR and GL_RENDERER strings.
    
    BUG=306462
    
    Review URL: https://codereview.chromium.org/27009002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228446 0039d316-1c4b-4281-b951-d872f2087c98

--- content/test/gpu/gpu_tests/webgl_conformance_expectations.py
+++ content/test/gpu/gpu_tests/webgl_conformance_expectations.py
@@ -6,13 +6,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 from telemetry.page import test_expectations
 
 # Valid expectation conditions are:
-# win xp vista win7
-# mac leopard snowleopard lion mountainlion
-# linux chromeos android
-# nvidia amd intel
-# Specific gpu's can be listed as a tuple with vendor name and device ID.
-# Example: ('nvidia', 0x1234)
-# Device ID's must be paired with a gpu vendor.
+#
+# Operating systems:
+#     win, xp, vista, win7, mac, leopard, snowleopard, lion, mountainlion,
+#     linux, chromeos, android
+#
+# GPU vendors:
+#     amd, arm, broadcom, hisilicon, intel, imagination, nvidia, qualcomm,
+#     vivante
+#
+# Specific GPUs can be listed as a tuple with vendor name and device ID.
+# Examples: ('nvidia', 0x1234), ('arm', 'Mali-T604')
+# Device IDs must be paired with a GPU vendor.
 
 class WebGLConformanceExpectations(test_expectations.TestExpectations):
   def SetExpectations(self):
