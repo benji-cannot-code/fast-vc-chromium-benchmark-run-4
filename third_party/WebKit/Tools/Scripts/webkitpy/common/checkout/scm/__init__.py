8e3c1d72b95b5ca2c2c760b98096471c01a8fb54
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-14 20:38:55 +0000

    webkitpy: prune unused code in scm and make tests pass
    This patch removes a bunch of no-longer needed code from the scm modules in webkitpy.common.checkout.scm. This is safe because we no longer use any of this logic to push commits to the server (though we still support local commits), and so we can shrink the API for the base SCM interface substantially and delete a lot of tests.
    
    This patch also updates scm_unittest so that all of the remaining tests pass. The tests are still slow and not run by default in test-webkitpy.
    
    This patch depends on https://codereview.chromium.org/27055003/ .
    
    BUG=
    R=eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/27045007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159600 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/common/checkout/scm/__init__.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/checkout/scm/__init__.py
@@ -2,8 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Required for Python to search this directory for module files
 
 # We only export public API here.
-from .commitmessage import CommitMessage
 from .detection import SCMDetector
 from .git import Git, AmbiguousCommitError
-from .scm import SCM, AuthenticationError, CheckoutNeedsUpdate
+from .scm import SCM
 from .svn import SVN
