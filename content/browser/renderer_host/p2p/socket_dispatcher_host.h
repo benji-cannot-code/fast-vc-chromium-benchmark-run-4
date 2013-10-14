6119727c39fcd462b026ce5adfe4a1f1099c2b56
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-14 22:34:38 +0000

    Cleanup: Fix style errors and remove unused #includes in net/base/net_util.h, and fix missing #includes elsewhere as a result.
    Review URL: https://codereview.chromium.org/25183003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228546 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/p2p/socket_dispatcher_host.h
+++ content/browser/renderer_host/p2p/socket_dispatcher_host.h
@@ -7,6 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_BROWSER_RENDERER_HOST_P2P_SOCKET_DISPATCHER_HOST_H_
 
 #include <map>
+#include <set>
+#include <string>
+#include <vector>
 
 #include "content/browser/renderer_host/p2p/socket_host_throttler.h"
 #include "content/common/p2p_sockets.h"
