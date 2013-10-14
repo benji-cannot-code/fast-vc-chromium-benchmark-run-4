c9a51254b3ec8521dc45d61da798f6989fc2383e
pastarmovj@chromium.org <pastarmovj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-14 17:18:04 +0000

    Schedule an ephemeral profile for deletion once the last browser is closed.
    BUG=302461
    TEST=browser_tests
    TEST=Manual:Set the ephemeral cloud policy on a profile and make sure the profile is deleted when Chrome is closed.
    
    Review URL: https://codereview.chromium.org/25495003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228474 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_manager.cc
+++ chrome/browser/profiles/profile_manager.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/profile_sync_service.h"
 #include "chrome/browser/sync/profile_sync_service_factory.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_iterator.h"
 #include "chrome/browser/ui/sync/sync_promo_ui.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_paths_internal.h"
@@ -691,7 +692,23 @@ void ProfileManager::BrowserListObserver::OnBrowserAdded(
     Browser* browser) {}
 
 void ProfileManager::BrowserListObserver::OnBrowserRemoved(
-    Browser* browser) {}
+    Browser* browser) {
+  Profile* profile = browser->profile();
+  for (chrome::BrowserIterator it; !it.done(); it.Next()) {
+    if (it->profile()->GetOriginalProfile() == profile->GetOriginalProfile())
+      // Not the last window for this profile.
+      return;
+  }
+
+  // If the last browser of a profile that is scheduled for deletion is closed
+  // do that now.
+  base::FilePath path = profile->GetPath();
+  if (profile->GetPrefs()->GetBoolean(prefs::kForceEphemeralProfiles) &&
+      !IsProfileMarkedForDeletion(path)) {
+    g_browser_process->profile_manager()->ScheduleProfileForDeletion(
+        path, ProfileManager::CreateCallback());
+  }
+}
 
 void ProfileManager::BrowserListObserver::OnBrowserSetLastActive(
     Browser* browser) {
