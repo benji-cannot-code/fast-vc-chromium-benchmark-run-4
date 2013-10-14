291a47c2add7e41ece5697256c1e0ef4fedfe2d4
craigdh@chromium.org <craigdh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-14 19:06:42 +0000

    [ispy] Split I-Spy into an appengine frontend and client library.
    BUG=298500
    TEST=Included
    NOTRY=True
    
    Review URL: https://codereview.chromium.org/26030004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228499 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/functional/ispy/server/image_handler.py
+++ chrome/test/functional/ispy/server/image_handler.py
@@ -3,19 +3,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Request handler to display an image from ispy.googleplex.com."""
+"""Request handler to display an image from Google Cloud Storage."""
 
 import json
 import os
 import sys
 import webapp2
 
-sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir))
+from common import cloud_bucket
+from common import constants
 
-from tests.rendering_test_manager import cloud_bucket
-from tests.rendering_test_manager import cloud_bucket_impl
-
-import ispy_auth_constants
+import gs_bucket
 
 
 class ImageHandler(webapp2.RequestHandler):
@@ -31,9 +29,7 @@ class ImageHandler(webapp2.RequestHandler):
     if not file_path:
       self.error(404)
       return
-    bucket = cloud_bucket_impl.CloudBucketImpl(
-        ispy_auth_constants.KEY, ispy_auth_constants.SECRET,
-        ispy_auth_constants.BUCKET)
+    bucket = gs_bucket.GoogleCloudStorageBucket(constants.BUCKET)
     try:
       image = bucket.DownloadFile(file_path)
     except cloud_bucket.FileNotFoundError:
