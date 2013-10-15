5d2cda935a642b75e70ad855a99b3dc73bd4737b
Tomasz Mikolajewski <mtomasz@chromium.org>
2013-10-15 14:59:58 +0900

    Introduce model classes for the gallery.
    This patch separates the views/gallery.js file into an actual view
    and a model class. The model class will be used also by another view
    very soon.
    
    BUG=None
    TEST=run_all_tests.py
    
    Change-Id: I7aecf1fa107348839433047b6401701fa130f299

--- chrome/browser/resources/chromeos/camera/src/js/main.js
+++ chrome/browser/resources/chromeos/camera/src/js/main.js
@@ -20,7 +20,6 @@ camera.Camera = function() {
    * @private
    */
   this.context_ = new camera.Camera.Context(
-      this.onPictureTaken_.bind(this),
       this.onError_.bind(this),
       this.onErrorRecovered_.bind(this));
 
@@ -77,15 +76,13 @@ camera.Camera = function() {
 /**
  * Creates context for the views.
  *
- * @param {function(string)} onPictureTaken Callback to be called, when a
- *     picture is added.
  * @param {function(string, string, opt_string)} onError Callback to be called,
  *     when an error occurs. Arguments: identifier, first line, second line.
  * @param {function(string)} onErrorRecovered Callback to be called,
  *     when the error goes away. The argument is the error id.
  * @constructor
  */
-camera.Camera.Context = function(onPictureTaken, onError, onErrorRecovered) {
+camera.Camera.Context = function(onError, onErrorRecovered) {
   camera.View.Context.call(this);
 
   /**
@@ -97,11 +94,6 @@ camera.Camera.Context = function(onPictureTaken, onError, onErrorRecovered) {
    * @type {boolean}
    */
   this.hasError = false;
-
-  /**
-   * @type {function(string)}
-   */
-  this.onPictureTaken = onPictureTaken;
 
   /**
    * @type {function(string, string, string)}
@@ -118,7 +110,7 @@ camera.Camera.Context = function(onPictureTaken, onError, onErrorRecovered) {
 };
 
 camera.Camera.Context.prototype = {
-  __proto__: camera.View.Context.prototype,
+  __proto__: camera.View.Context.prototype
 };
 
 camera.Camera.prototype = {
@@ -228,15 +220,6 @@ camera.Camera.prototype.onMaximizeClicked_ = function() {
  */
 camera.Camera.prototype.onCloseClicked_ = function() {
   chrome.app.window.current().close();
-};
-
-/**
- * Adds a picture taken in the camera view to the gallery view.
- * @param {string} dataURL Picture data.
- * @private
- */
-camera.Camera.prototype.onPictureTaken_ = function(dataURL) {
-  this.galleryView_.addPicture(dataURL);
 };
 
 /**
