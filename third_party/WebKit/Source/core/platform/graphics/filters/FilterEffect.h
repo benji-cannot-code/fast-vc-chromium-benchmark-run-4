00c088f30a3122bf65ac4c18c19c3025a4f2aaa4
senorblanco@chromium.org <senorblanco@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-15 21:03:17 +0000

    Switch over to using the SkImageFilter::CropRect API for filter primitive subregion in Blink.
    BUG=229910
    R=schenney@chromium.org
    
    Review URL: https://codereview.chromium.org/27063002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159692 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/graphics/filters/FilterEffect.h
+++ third_party/WebKit/Source/core/platform/graphics/filters/FilterEffect.h
@@ -28,6 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/geometry/FloatRect.h"
 #include "platform/geometry/IntRect.h"
 
+#include "third_party/skia/include/core/SkImageFilter.h"
+
 #include "wtf/PassOwnPtr.h"
 #include "wtf/RefCounted.h"
 #include "wtf/RefPtr.h"
@@ -35,8 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/Vector.h"
 
 static const float kMaxFilterSize = 5000.0f;
-
-class SkImageFilter;
 
 namespace WebCore {
 
@@ -171,7 +171,7 @@ protected:
 
     // If a pre-multiplied image, check every pixel for validity and correct if necessary.
     void forceValidPreMultipliedPixels();
-    SkIRect getCropRect(const FloatSize& cropOffset) const;
+    SkImageFilter::CropRect getCropRect(const FloatSize& cropOffset) const;
 
 private:
     virtual void applySoftware() = 0;
