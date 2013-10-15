a7797e2793affb7262814fb0bf00edd9635c6000
nhiroki@chromium.org <nhiroki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-15 03:38:50 +0000

    SyncFS: WeakPtrFactory should be the last member in SyncEngine
    BUG=303818
    TEST=n/a (compile)
    
    Review URL: https://codereview.chromium.org/27107002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228609 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_backend/sync_engine.cc
+++ chrome/browser/sync_file_system/drive_backend/sync_engine.cc
@@ -31,8 +31,7 @@ SyncEngine::SyncEngine(
       drive_service_(drive_service.Pass()),
       notification_manager_(notification_manager),
       extension_service_(extension_service),
-      weak_ptr_factory_(this),
-      task_manager_(weak_ptr_factory_.GetWeakPtr()) {
+      weak_ptr_factory_(this) {
 }
 
 SyncEngine::~SyncEngine() {
@@ -40,13 +39,15 @@ SyncEngine::~SyncEngine() {
 }
 
 void SyncEngine::Initialize() {
-  task_manager_.Initialize(SYNC_STATUS_OK);
+  DCHECK(!task_manager_);
+  task_manager_.reset(new SyncTaskManager(weak_ptr_factory_.GetWeakPtr()));
+  task_manager_->Initialize(SYNC_STATUS_OK);
 
   SyncEngineInitializer* initializer =
       new SyncEngineInitializer(task_runner_.get(),
                                 drive_service_.get(),
                                 base_dir_.Append(kDatabaseName));
-  task_manager_.ScheduleSyncTask(
+  task_manager_->ScheduleSyncTask(
       scoped_ptr<SyncTask>(initializer),
       base::Bind(&SyncEngine::DidInitialize, weak_ptr_factory_.GetWeakPtr(),
                  initializer));
@@ -63,7 +64,7 @@ void SyncEngine::AddFileStatusObserver(FileStatusObserver* observer) {
 void SyncEngine::RegisterOrigin(
     const GURL& origin,
     const SyncStatusCallback& callback) {
-  task_manager_.ScheduleSyncTask(
+  task_manager_->ScheduleSyncTask(
       scoped_ptr<SyncTask>(new RegisterAppTask(this, origin.host())),
       callback);
 }
@@ -71,7 +72,7 @@ void SyncEngine::RegisterOrigin(
 void SyncEngine::EnableOrigin(
     const GURL& origin,
     const SyncStatusCallback& callback) {
-  task_manager_.ScheduleTask(
+  task_manager_->ScheduleTask(
       base::Bind(&SyncEngine::DoEnableApp,
                  weak_ptr_factory_.GetWeakPtr(),
                  origin.host()),
@@ -81,7 +82,7 @@ void SyncEngine::EnableOrigin(
 void SyncEngine::DisableOrigin(
     const GURL& origin,
     const SyncStatusCallback& callback) {
-  task_manager_.ScheduleTask(
+  task_manager_->ScheduleTask(
       base::Bind(&SyncEngine::DoDisableApp,
                  weak_ptr_factory_.GetWeakPtr(),
                  origin.host()),
@@ -92,7 +93,7 @@ void SyncEngine::UninstallOrigin(
     const GURL& origin,
     UninstallFlag flag,
     const SyncStatusCallback& callback) {
-  task_manager_.ScheduleSyncTask(
+  task_manager_->ScheduleSyncTask(
       scoped_ptr<SyncTask>(new UninstallAppTask(this, origin.host(), flag)),
       callback);
 }
@@ -100,7 +101,7 @@ void SyncEngine::UninstallOrigin(
 void SyncEngine::ProcessRemoteChange(
     const SyncFileCallback& callback) {
   RemoteToLocalSyncer* syncer = new RemoteToLocalSyncer;
-  task_manager_.ScheduleSyncTask(
+  task_manager_->ScheduleSyncTask(
       scoped_ptr<SyncTask>(syncer),
       base::Bind(&SyncEngine::DidProcessRemoteChange,
                  weak_ptr_factory_.GetWeakPtr(),
@@ -173,7 +174,7 @@ void SyncEngine::ApplyLocalChange(
     const fileapi::FileSystemURL& url,
     const SyncStatusCallback& callback) {
   LocalToRemoteSyncer* syncer = new LocalToRemoteSyncer;
-  task_manager_.ScheduleSyncTask(
+  task_manager_->ScheduleSyncTask(
       scoped_ptr<SyncTask>(syncer),
       base::Bind(&SyncEngine::DidApplyLocalChange,
                  weak_ptr_factory_.GetWeakPtr(),
