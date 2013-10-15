2dcc373bed7605772585fe1a24e66307cd75b1f3
bbudge@chromium.org <bbudge@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-15 21:55:13 +0000

    Fix minor issues in PPB_TrueTypeFont_Dev implementation.
    1) PepperTrueTypeFontWin::GetTable needs to match behavior on Mac and Linux,
    where we return success and copy 0 bytes if offset is off the end of a table.
    2) Remove useless code from the message filter used by PepperTrueTypeFontListHost.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/27317003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228775 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/c/dev/ppb_truetype_font_dev.h
+++ ppapi/c/dev/ppb_truetype_font_dev.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * found in the LICENSE file.
  */
 
-/* From dev/ppb_truetype_font_dev.idl modified Wed Apr 17 15:38:46 2013. */
+/* From dev/ppb_truetype_font_dev.idl modified Tue Oct 15 05:52:52 2013. */
 
 #ifndef PPAPI_C_DEV_PPB_TRUETYPE_FONT_DEV_H_
 #define PPAPI_C_DEV_PPB_TRUETYPE_FONT_DEV_H_
@@ -267,7 +267,8 @@ struct PPB_TrueTypeFont_Dev_0_1 {
    * @param[in] table A 4 byte value indicating which table to copy.
    * For example, 'glyf' will cause the outline table to be copied into the
    * output array. A zero tag value will cause the entire font to be copied.
-   * @param[in] offset The offset into the font table.
+   * @param[in] offset The offset into the font table. Passing an offset
+   * greater than or equal to the table size will succeed with 0 bytes copied.
    * @param[in] max_data_length The maximum number of bytes to transfer from
    * <code>offset</code>.
    * @param[in] output A <code>PP_ArrayOutput</code> to hold the font data.
