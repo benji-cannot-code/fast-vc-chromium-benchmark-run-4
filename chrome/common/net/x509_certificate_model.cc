9fcd17bfc730cd0e3c2cde36b5c60278ac6e5f36
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-15 17:13:38 +0000

    Add #include of <algorithm> for compilation on VS2013
    Needed for usage of std::max.
    
    R=thakis@chromium.org
    BUG=288948
    
    Review URL: https://codereview.chromium.org/26477008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228712 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/net/x509_certificate_model.cc
+++ chrome/common/net/x509_certificate_model.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <unicode/uidna.h>
 
+#include <algorithm>
+
 #include "base/strings/utf_string_conversions.h"
 #include "grit/generated_resources.h"
 #include "net/base/net_util.h"
