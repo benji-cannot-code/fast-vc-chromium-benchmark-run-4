af666792b404c7641b97e42f94b74abd0c94ce87
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-15 19:17:38 +0000

    Fixes for tcmalloc for VS2013
    - Add missing #include of <algorithm> for std::min/max
    - strtoll, strtoull, strotoq in runtime on >= VS2013.
    
    R=jar@chromium.org
    BUG=288948
    
    Review URL: https://codereview.chromium.org/27017003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228737 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/tcmalloc/chromium/src/windows/port.cc
+++ third_party/tcmalloc/chromium/src/windows/port.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <assert.h>
 #include <stdarg.h>    // for va_list, va_start, va_end
 #include <windows.h>
+#include <algorithm>
 #include "port.h"
 #include "base/logging.h"
 #include "base/spinlock.h"
