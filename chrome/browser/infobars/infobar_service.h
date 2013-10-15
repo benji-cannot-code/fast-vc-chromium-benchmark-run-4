8a379067fa657e04af5fdfe57f6defe0159f17e8
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-15 01:23:58 +0000

    Remove incoming notifications from InfobarService.
    BUG=170921
    TEST=everything still works
    
    Review URL: https://codereview.chromium.org/27240002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228585 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/infobars/infobar_service.h
+++ chrome/browser/infobars/infobar_service.h
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/memory/scoped_ptr.h"
-#include "content/public/browser/notification_observer.h"
-#include "content/public/browser/notification_registrar.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
 
@@ -19,7 +17,6 @@ class InfoBarDelegate;
 // Provides access to creating, removing and enumerating info bars
 // attached to a tab.
 class InfoBarService : public content::WebContentsObserver,
-                       public content::NotificationObserver,
                        public content::WebContentsUserData<InfoBarService> {
  public:
   // Changes whether infobars are enabled.  The default is true.
@@ -80,12 +77,11 @@ class InfoBarService : public content::WebContentsObserver,
 
   // content::WebContentsObserver:
   virtual void RenderProcessGone(base::TerminationStatus status) OVERRIDE;
+  virtual void NavigationEntryCommitted(
+      const content::LoadCommittedDetails& load_details) OVERRIDE;
+  virtual void WebContentsDestroyed(
+      content::WebContents* web_contents) OVERRIDE;
   virtual bool OnMessageReceived(const IPC::Message& message) OVERRIDE;
-
-  // content::NotificationObserver:
-  virtual void Observe(int type,
-                       const content::NotificationSource& source,
-                       const content::NotificationDetails& details) OVERRIDE;
 
   void RemoveInfoBarInternal(InfoBarDelegate* infobar, bool animate);
   void RemoveAllInfoBars(bool animate);
@@ -96,8 +92,6 @@ class InfoBarService : public content::WebContentsObserver,
 
   InfoBars infobars_;
   bool infobars_enabled_;
-
-  content::NotificationRegistrar registrar_;
 
   DISALLOW_COPY_AND_ASSIGN(InfoBarService);
 };
