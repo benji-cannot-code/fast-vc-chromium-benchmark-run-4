74701010d45044fc4276e8423283770094d76809
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-15 18:13:37 +0000

    Cherry pick r512 from upstream protobuf
    Fixes compilation on VS2013.
    
    TBR=cpu@chromium.org
    BUG=288498
    
    Review URL: https://codereview.chromium.org/27017004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228723 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/protobuf/src/google/protobuf/io/zero_copy_stream_impl_lite.cc
+++ third_party/protobuf/src/google/protobuf/io/zero_copy_stream_impl_lite.cc
@@ -37,6 +37,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <google/protobuf/stubs/common.h>
 #include <google/protobuf/stubs/stl_util.h>
 
+#include <algorithm>
+
 namespace google {
 namespace protobuf {
 namespace io {
