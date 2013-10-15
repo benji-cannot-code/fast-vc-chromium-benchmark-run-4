22674b23d3472009e57bf3dcf4f7efc6ccca5342
lushnikov@chromium.org <lushnikov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-15 15:35:36 +0000

    DevTools: support beforeunload lifecycle for DevTools window
    This patch teaches DevTools window to send beforeunload events to
    its frontend. This events won't be send if the inspected webcontents
    got closed - additional work is needed to handle this case.
    
    BUG=289552
    TBR=pfeldman
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=228346
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=228473
    
    Review URL: https://codereview.chromium.org/23766027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228681 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/devtools/devtools_window.cc
+++ chrome/browser/devtools/devtools_window.cc
@@ -2,7 +2,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
 #include "chrome/browser/devtools/devtools_window.h"
 
 #include <algorithm>
@@ -450,7 +449,7 @@ int DevToolsWindow::GetMinimizedHeight() {
 }
 
 void DevToolsWindow::InspectedContentsClosing() {
-  Hide();
+  web_contents_->GetRenderViewHost()->ClosePage();
 }
 
 content::RenderViewHost* DevToolsWindow::GetRenderViewHost() {
@@ -789,6 +788,25 @@ void DevToolsWindow::AddNewContents(content::WebContents* source,
 }
 
 void DevToolsWindow::CloseContents(content::WebContents* source) {
+  CHECK(IsDocked());
+  // Update dev tools to reflect removed dev tools window.
+  BrowserWindow* inspected_window = GetInspectedBrowserWindow();
+  if (inspected_window)
+    inspected_window->UpdateDevTools();
+  // In case of docked web_contents_, we own it so delete here.
+  delete web_contents_;
+
+  delete this;
+}
+
+void DevToolsWindow::BeforeUnloadFired(content::WebContents* tab,
+                                       bool proceed,
+                                       bool* proceed_to_fire_unload) {
+  if (proceed) {
+    content::DevToolsManager::GetInstance()->ClientHostClosing(
+        frontend_host_.get());
+  }
+  *proceed_to_fire_unload = proceed;
 }
 
 bool DevToolsWindow::PreHandleKeyboardEvent(
@@ -880,11 +898,18 @@ void DevToolsWindow::ActivateWindow() {
     browser_->window()->Activate();
 }
 
+void DevToolsWindow::ActivateContents(content::WebContents* contents) {
+  if (IsDocked()) {
+    content::WebContents* inspected_tab = this->GetInspectedWebContents();
+    inspected_tab->GetDelegate()->ActivateContents(inspected_tab);
+  } else {
+    browser_->window()->Activate();
+  }
+}
+
 void DevToolsWindow::CloseWindow() {
   DCHECK(IsDocked());
-  content::DevToolsManager::GetInstance()->ClientHostClosing(
-      frontend_host_.get());
-  Hide();
+  web_contents_->GetRenderViewHost()->FirePageBeforeUnload(false);
 }
 
 void DevToolsWindow::SetWindowBounds(int x, int y, int width, int height) {
@@ -1228,26 +1253,6 @@ void DevToolsWindow::UpdateFrontendDockSide() {
   base::FundamentalValue docked(IsDocked());
   CallClientFunction("InspectorFrontendAPI.setAttachedWindow", &docked, NULL,
                      NULL);
-}
-
-void DevToolsWindow::Hide() {
-  if (IsDocked()) {
-    // Update dev tools to reflect removed dev tools window.
-    BrowserWindow* inspected_window = GetInspectedBrowserWindow();
-    if (inspected_window)
-      inspected_window->UpdateDevTools();
-    // In case of docked web_contents_, we own it so delete here.
-    delete web_contents_;
-
-    delete this;
-  } else {
-    // First, initiate self-destruct to free all the registrars.
-    // Then close all tabs. Browser will take care of deleting web_contents_
-    // for us.
-    Browser* browser = browser_;
-    delete this;
-    browser->tab_strip_model()->CloseAllTabs();
-  }
 }
 
 void DevToolsWindow::ScheduleAction(DevToolsToggleAction action) {
