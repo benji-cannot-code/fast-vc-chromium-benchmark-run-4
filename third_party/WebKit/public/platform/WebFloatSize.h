79f175d8bb595b3c64871fc7fed5d4e6eac5cb91
scottmg@chromium.org <scottmg@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-15 19:32:31 +0000

    Fixes for compilation of Blink on VS2013
    - Disable some math functions that are now part of the C runtime
    - Add missing include of <algorithm> for std::max
    - Disable optimization of dtoa code on VS2013. Upstream bug filed and
      fixed for next release.
    
    NOTRY=true
    R=abarth@chromium.org
    BUG=288948
    
    Review URL: https://codereview.chromium.org/27009003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159686 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebFloatSize.h
+++ third_party/WebKit/public/platform/WebFloatSize.h
@@ -34,6 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "WebCommon.h"
 
+#include <algorithm>
+
 #if BLINK_IMPLEMENTATION
 #include "platform/geometry/FloatSize.h"
 #else
