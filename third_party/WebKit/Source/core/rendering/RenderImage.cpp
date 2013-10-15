290aa63d35963f909753dd88bd6521a072ed264e
yoav@yoav.ws <yoav@yoav.ws@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-15 01:38:44 +0000

    Use srcset's resource pixel density to determine intrinsic size
    According to the spec "When an img element has a current pixel density that is not 1.0,
    the element's image data must be treated as if its resolution, in device pixels per CSS pixels,
    was the current pixel density."
    
    I've added that support using the following changes:
    * HTMLSrcsetParser now returns the image candidate to HTMLImageElement.
    * HTMLImageElement passes the devicePixelRatio data to RenderImage, which stores it.
    * Bitmap images are scaled using the devicePixelRatio at RenderImageResource's intrinsicSize() and imageSize().
    * SVG images are scaled using the devicePixelRatio at RenderReplaced::computeAspectRatioInformationForRenderBox.
    * Canvas support added at CanvasRenderingContext2D::sizeFor.
    
    
    Other changes:
    * Removed useless dumpAsText from tests
    * Modified one of the tests to use testRunner.dumpResourceRequestCallbacks() instead of
      internals.isPreloaded(), since isPreloaded wasn't reporting what the test was intended to test.
    * Added some tests to TestExpectations because of bugs discovered during this CL work.
    
    BUG=299556
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=159544
    
    Review URL: https://codereview.chromium.org/25105004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159622 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderImage.cpp
+++ third_party/WebKit/Source/core/rendering/RenderImage.cpp
@@ -60,6 +60,7 @@ RenderImage::RenderImage(Element* element)
     , m_needsToSetSizeForAltText(false)
     , m_didIncrementVisuallyNonEmptyPixelCount(false)
     , m_isGeneratedContent(false)
+    , m_imageDevicePixelRatio(1.0f)
 {
     updateAltText();
 }
