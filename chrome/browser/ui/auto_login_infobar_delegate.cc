121ffd104bac14aee263fac29f49a84febdf80c1
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-15 02:53:41 +0000

    Remove NOTIFICATION_WEB_CONTENTS_DESTROYED from AutoLoginRedirector.
    BUG=170921
    TEST=everything still works
    
    Review URL: https://codereview.chromium.org/27019009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228601 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/auto_login_infobar_delegate.cc
+++ chrome/browser/ui/auto_login_infobar_delegate.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_types.h"
 #include "content/public/browser/page_navigator.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/browser/web_contents_observer.h"
 #include "content/public/common/referrer.h"
 #include "google_apis/gaia/gaia_constants.h"
 #include "google_apis/gaia/gaia_urls.h"
@@ -43,10 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/android/infobars/auto_login_infobar_delegate_android.h"
 #endif
 
-using content::NavigationController;
-using content::NotificationDetails;
-using content::NotificationSource;
-
 
 // AutoLoginRedirector --------------------------------------------------------
 
@@ -57,9 +54,9 @@ namespace {
 // tokens using the TokenService, gets the browser cookies with the TokenAuth
 // API, and finally redirects the user to the correct page.
 class AutoLoginRedirector : public UbertokenConsumer,
-                            public content::NotificationObserver {
+                            public content::WebContentsObserver {
  public:
-  AutoLoginRedirector(NavigationController* navigation_controller,
+  AutoLoginRedirector(content::WebContents* web_contents,
                       const std::string& args);
   virtual ~AutoLoginRedirector();
 
@@ -68,47 +65,35 @@ class AutoLoginRedirector : public UbertokenConsumer,
   virtual void OnUbertokenSuccess(const std::string& token) OVERRIDE;
   virtual void OnUbertokenFailure(const GoogleServiceAuthError& error) OVERRIDE;
 
-  // Implementation of content::NotificationObserver
-  virtual void Observe(int type,
-                       const content::NotificationSource& source,
-                       const content::NotificationDetails& details) OVERRIDE;
+  // Implementation of content::WebContentsObserver
+  virtual void WebContentsDestroyed(
+      content::WebContents* web_contents) OVERRIDE;
 
   // Redirect tab to MergeSession URL, logging the user in and navigating
   // to the desired page.
   void RedirectToMergeSession(const std::string& token);
 
-  NavigationController* navigation_controller_;
   const std::string args_;
   scoped_ptr<UbertokenFetcher> ubertoken_fetcher_;
-
-  // For listening to NavigationController destruction.
-  content::NotificationRegistrar registrar_;
 
   DISALLOW_COPY_AND_ASSIGN(AutoLoginRedirector);
 };
 
 AutoLoginRedirector::AutoLoginRedirector(
-    NavigationController* navigation_controller,
+    content::WebContents* web_contents,
     const std::string& args)
-    : navigation_controller_(navigation_controller),
+    : content::WebContentsObserver(web_contents),
       args_(args) {
   ubertoken_fetcher_.reset(new UbertokenFetcher(
-      Profile::FromBrowserContext(navigation_controller_->GetBrowserContext()),
-      this));
-  registrar_.Add(this,
-                 content::NOTIFICATION_WEB_CONTENTS_DESTROYED,
-                 content::Source<content::WebContents>(
-                     navigation_controller_->GetWebContents()));
+      Profile::FromBrowserContext(web_contents->GetBrowserContext()), this));
   ubertoken_fetcher_->StartFetchingToken();
 }
 
 AutoLoginRedirector::~AutoLoginRedirector() {
 }
 
-void AutoLoginRedirector::Observe(int type,
-                                  const NotificationSource& source,
-                                  const NotificationDetails& details) {
-  DCHECK(type == content::NOTIFICATION_WEB_CONTENTS_DESTROYED);
+void AutoLoginRedirector::WebContentsDestroyed(
+    content::WebContents* web_contents) {
   // The WebContents that started this has been destroyed. The request must be
   // cancelled and this object must be deleted.
   ubertoken_fetcher_.reset();
@@ -128,7 +113,7 @@ void AutoLoginRedirector::OnUbertokenFailure(
 
 void AutoLoginRedirector::RedirectToMergeSession(const std::string& token) {
   // TODO(rogerta): what is the correct page transition?
-  navigation_controller_->LoadURL(
+  web_contents()->GetController().LoadURL(
       GaiaUrls::GetInstance()->merge_session_url().Resolve(
           "?source=chrome&uberauth=" + token + "&" + args_),
       content::Referrer(), content::PAGE_TRANSITION_AUTO_BOOKMARK,
@@ -136,7 +121,6 @@ void AutoLoginRedirector::RedirectToMergeSession(const std::string& token) {
 }
 
 }  // namespace
-
 
 
 // AutoLoginInfoBarDelegate ---------------------------------------------------
@@ -201,8 +185,7 @@ string16 AutoLoginInfoBarDelegate::GetButtonLabel(
 
 bool AutoLoginInfoBarDelegate::Accept() {
   // AutoLoginRedirector deletes itself.
-  new AutoLoginRedirector(&web_contents()->GetController(),
-                          params_.header.args);
+  new AutoLoginRedirector(web_contents(), params_.header.args);
   RecordHistogramAction(ACCEPTED);
   button_pressed_ = true;
   return true;
@@ -217,9 +200,10 @@ bool AutoLoginInfoBarDelegate::Cancel() {
   return true;
 }
 
-void AutoLoginInfoBarDelegate::Observe(int type,
-                                       const NotificationSource& source,
-                                       const NotificationDetails& details) {
+void AutoLoginInfoBarDelegate::Observe(
+    int type,
+    const content::NotificationSource& source,
+    const content::NotificationDetails& details) {
   DCHECK_EQ(chrome::NOTIFICATION_GOOGLE_SIGNED_OUT, type);
   // owner() can be NULL when InfoBarService removes us. See
   // |InfoBarDelegate::clear_owner|.
