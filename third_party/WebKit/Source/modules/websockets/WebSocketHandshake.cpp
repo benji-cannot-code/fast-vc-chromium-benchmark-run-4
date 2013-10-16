42dc101ecdedcb5c6d3d195662f9d41470f7e3de
yhirano@chromium.org <yhirano@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-16 10:43:01 +0000

    Provide a white list to prevent the WebSocket handshake fail.
    The spec says that the Sec-WebSocket-Protocol header in a handshake
    response can't be null if the header in a request is not null.
    Some servers are not accustomed to the fail,
    so we provide an adhoc white-list for it tentatively.
    
    BUG=304319
    R=tyoshino
    
    Review URL: https://codereview.chromium.org/27465002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159754 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/websockets/WebSocketHandshake.cpp
+++ third_party/WebKit/Source/modules/websockets/WebSocketHandshake.cpp
@@ -59,6 +59,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+namespace {
+
+// FIXME: The spec says that the Sec-WebSocket-Protocol header in a handshake
+// response can't be null if the header in a request is not null.
+// Some servers are not accustomed to the shutdown,
+// so we provide an adhoc white-list for it tentatively.
+const char* const missingProtocolWhiteList[] = {
+    "ica.citrix.com",
+};
+
+} // namespace
+
 static const char randomCharacterInSecWebSocketKey[] = "!\"#$%&'()*+,-./:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
 
 static String resourceName(const KURL& url)
@@ -550,8 +562,20 @@ bool WebSocketHandshake::checkResponseHeaders()
             return false;
         }
     } else if (!m_clientProtocol.isEmpty()) {
-        m_failureReason = "Error during WebSocket handshake: Sec-WebSocket-Protocol mismatch";
-        return false;
+        // FIXME: Some servers are not accustomed to this failure, so we provide an adhoc white-list for it tentatively.
+        Vector<String> protocols;
+        m_clientProtocol.split(String(WebSocket::subProtocolSeperator()), protocols);
+        bool match = false;
+        for (size_t i = 0; i < protocols.size() && !match; ++i) {
+            for (size_t j = 0; j < WTF_ARRAY_LENGTH(missingProtocolWhiteList) && !match; ++j) {
+                if (protocols[i] == missingProtocolWhiteList[j])
+                    match = true;
+            }
+        }
+        if (!match) {
+            m_failureReason = "Error during WebSocket handshake: Sec-WebSocket-Protocol mismatch";
+            return false;
+        }
     }
     return true;
 }
