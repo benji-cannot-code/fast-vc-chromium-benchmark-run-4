35ea2429d346516179469d1f8b9f34baab0925ab
noel@chromium.org <noel@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-16 11:45:50 +0000

    Image decoders don't need to check for big-endian anymore
    Chromium is designed for little-endian machines, and these have BGRA
    pixel order (or RGBA on chrome Android), refer to:
    
    https://bugs.webkit.org/show_bug.cgi?id=59670#c10
    https://bugs.webkit.org/show_bug.cgi?id=72234
    https://bugs.webkit.org/show_bug.cgi?id=88613
    
    TEST=Covered by existing JPEG/WEBP layout tests.
    BUG=None
    
    Review URL: https://codereview.chromium.org/15989004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159760 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/image-decoders/webp/WEBPImageDecoder.cpp
+++ third_party/WebKit/Source/core/platform/image-decoders/webp/WEBPImageDecoder.cpp
@@ -30,19 +30,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "core/platform/image-decoders/webp/WEBPImageDecoder.h"
 
+#include "RuntimeEnabledFeatures.h"
 #include "core/platform/PlatformInstrumentation.h"
 
 #if USE(QCMSLIB)
 #include "qcms.h"
 #endif
 
-#include "RuntimeEnabledFeatures.h"
-
 #if CPU(BIG_ENDIAN) || CPU(MIDDLE_ENDIAN)
+#error Blink assumes a little-endian target.
+#endif
+
+#if SK_B32_SHIFT // Output little-endian RGBA pixels (Android).
 inline WEBP_CSP_MODE outputMode(bool hasAlpha) { return hasAlpha ? MODE_rgbA : MODE_RGBA; }
-#elif SK_B32_SHIFT
-inline WEBP_CSP_MODE outputMode(bool hasAlpha) { return hasAlpha ? MODE_rgbA : MODE_RGBA; }
-#else // LITTLE_ENDIAN, output BGRA pixels.
+#else // Output little-endian BGRA pixels.
 inline WEBP_CSP_MODE outputMode(bool hasAlpha) { return hasAlpha ? MODE_bgrA : MODE_BGRA; }
 #endif
 
