17ce7955caae2f2bb9374ba92b93f051b5f20395
simonhatch@chromium.org <simonhatch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-16 18:03:00 +0000

    Move the setup of the build system into the builder class. This is so the perf try bot script can just create a BisectPerformanceMetrics instance and start building/testing without any extra setup.
    BUG=
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/26883008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228947 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/bisect_utils.py
+++ tools/bisect_utils.py
@@ -376,12 +376,12 @@ def CopyAndSaveOriginalEnvironmentVars():
   ORIGINAL_ENV = os.environ.copy()
 
 
-def SetupAndroidBuildEnvironment(opts):
+def SetupAndroidBuildEnvironment(opts, path_to_src=None):
   """Sets up the android build environment.
 
   Args:
     opts: The options parsed from the command line through parse_args().
-    path_to_file: Path to the bisect script's directory.
+    path_to_src: Path to the src checkout.
 
   Returns:
     True if successful.
@@ -399,7 +399,7 @@ def SetupAndroidBuildEnvironment(opts):
   proc = subprocess.Popen(['bash', '-c', 'source %s && env' % path_to_file],
                            stdout=subprocess.PIPE,
                            stderr=subprocess.PIPE,
-                           cwd='src')
+                           cwd=path_to_src)
   (out, _) = proc.communicate()
 
   for line in out.splitlines():
@@ -413,7 +413,6 @@ def SetupPlatformBuildEnvironment(opts):
 
   Args:
     opts: The options parsed from the command line through parse_args().
-    path_to_file: Path to the bisect script's directory.
 
   Returns:
     True if successful.
