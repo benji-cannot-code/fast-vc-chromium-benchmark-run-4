123b63618c41d03ee0821a3a62317684369c1544
juanlang@google.com <juanlang@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-16 02:54:11 +0000

    Introduce a new attribute to externally_connectable, accepts_tls_channel_id.  When sites are allowed to connect to an extension, and specify include_tls_channel_id in their chrome.runtime.connect or chrome.runtime.sendMessage options, and the extension sets the accepts_tls_channel_id attribute, automatically annotate the TLS channel ID as an attribute of the MessageSender given to the onConnectExternal handler.
    BUG=259097
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=227722
    
    Review URL: https://codereview.chromium.org/23002007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228842 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/resources/extensions/extension_custom_bindings.js
+++ chrome/renderer/resources/extensions/extension_custom_bindings.js
@@ -89,7 +89,7 @@ binding.registerCustomHook(function(bindingsAPI, extensionId) {
 
   apiFunctions.setUpdateArgumentsPreValidate('sendRequest',
       $Function.bind(messaging.sendMessageUpdateArguments,
-                     null, 'sendRequest'));
+                     null, 'sendRequest', false /* hasOptionsArgument */));
 
   apiFunctions.setHandleRequest('sendRequest',
                                 function(targetId, request, responseCallback) {
--- chrome/renderer/resources/extensions/messaging.js
+++ chrome/renderer/resources/extensions/messaging.js
@@ -214,7 +214,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                              sourceTab,
                              sourceExtensionId,
                              targetExtensionId,
-                             sourceUrl) {
+                             sourceUrl,
+                             tlsChannelId) {
     // Only create a new Port if someone is actually listening for a connection.
     // In addition to being an optimization, this also fixes a bug where if 2
     // channels were opened to and from the same process, closing one would
@@ -237,6 +238,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       sender.url = sourceUrl;
     if (sourceTab)
       sender.tab = sourceTab;
+    if (tlsChannelId !== undefined)
+      sender.tlsChannelId = tlsChannelId;
 
     // Special case for sendRequest/onRequest and sendMessage/onMessage.
     if (channelName == kRequestChannel || channelName == kMessageChannel) {
@@ -333,9 +336,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     });
   };
 
-  function sendMessageUpdateArguments(functionName) {
-    var args = $Array.slice(arguments, 1);  // skip functionName
-    var alignedArgs = messagingUtils.alignSendMessageArguments(args);
+  function sendMessageUpdateArguments(functionName, hasOptionsArgument) {
+    // skip functionName and hasOptionsArgument
+    var args = $Array.slice(arguments, 2);
+    var alignedArgs = messagingUtils.alignSendMessageArguments(args,
+        hasOptionsArgument);
     if (!alignedArgs)
       throw new Error('Invalid arguments to ' + functionName + '.');
     return alignedArgs;
