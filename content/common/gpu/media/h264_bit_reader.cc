0323c1fabd6dabae54e8f7b2a59af5e8025a4153
chihchung@chromium.org <chihchung@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-16 10:38:56 +0000

    Exclude emulation prevention bytes in slice_param.slice_data_bit_offset
    See http://lists.freedesktop.org/archives/libva/2012-March/000793.html
    for the comment about the assumption of slice_data_bit_offset.
    
    BUG=chromium:301744
    TEST=play the problematic video in chrome.
    
    Review URL: https://codereview.chromium.org/27006002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228890 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/media/h264_bit_reader.cc
+++ content/common/gpu/media/h264_bit_reader.cc
@@ -10,7 +10,8 @@ namespace content {
 
 H264BitReader::H264BitReader()
     : data_(NULL), bytes_left_(0), curr_byte_(0),
-      num_remaining_bits_in_curr_byte_(0), prev_two_bytes_(0) {
+      num_remaining_bits_in_curr_byte_(0), prev_two_bytes_(0),
+      emulation_prevention_bytes_(0) {
 }
 
 H264BitReader::~H264BitReader() {}
@@ -26,6 +27,7 @@ bool H264BitReader::Initialize(const uint8* data, off_t size) {
   num_remaining_bits_in_curr_byte_ = 0;
   // Initially set to 0xffff to accept all initial two-byte sequences.
   prev_two_bytes_ = 0xffff;
+  emulation_prevention_bytes_ = 0;
 
   return true;
 }
@@ -40,6 +42,7 @@ bool H264BitReader::UpdateCurrByte() {
     // Detected 0x000003, skip last byte.
     ++data_;
     --bytes_left_;
+    ++emulation_prevention_bytes_;
     // Need another full three bytes before we can detect the sequence again.
     prev_two_bytes_ = 0xffff;
 
@@ -100,6 +103,11 @@ bool H264BitReader::HasMoreRBSPData() {
   // first available bit.
   return (curr_byte_ &
           ((1 << (num_remaining_bits_in_curr_byte_ - 1)) - 1)) != 0;
+}
+
+size_t H264BitReader::NumEmulationPreventionBytesRead()
+{
+  return emulation_prevention_bytes_;
 }
 
 }  // namespace content
