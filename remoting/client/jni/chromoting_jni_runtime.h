fda4a202d46b7a837cef3eddec885a118d4476f6
lambroslambrou@chromium.org <lambroslambrou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-16 11:25:41 +0000

    Hold video frame in Bitmap instead of keeping a ByteBuffer reference.
    Sometimes, the DesktopView was repainted whilst |JniInterface.sBuffer|
    no longer referred to valid frame data. This CL cleans things up by
    replacing the ByteBuffer with a Bitmap, and having JniFrameConsumer
    copy the completely-decoded data directly into the Java Bitmap.
    
    Review URL: https://codereview.chromium.org/24072012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228898 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/client/jni/chromoting_jni_runtime.h
+++ remoting/client/jni/chromoting_jni_runtime.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <jni.h>
 #include <string>
 
+#include "base/android/scoped_java_ref.h"
 #include "base/at_exit.h"
 #include "net/url_request/url_request_context_getter.h"
 #include "remoting/base/auto_thread.h"
@@ -80,8 +81,13 @@ class ChromotingJniRuntime {
                                 const std::string& id,
                                 const std::string& secret);
 
-  // Updates image dimensions and canvas memory space. Call on display thread.
-  void UpdateImageBuffer(int width, int height, jobject buffer);
+  // Creates a new Bitmap object to store a video frame.
+  base::android::ScopedJavaLocalRef<jobject> NewBitmap(
+      webrtc::DesktopSize size);
+
+  // Updates video frame bitmap. |bitmap| must be an instance of
+  // android.graphics.Bitmap. Call on the display thread.
+  void UpdateFrameBitmap(jobject bitmap);
 
   // Updates cursor shape. Call on display thread.
   void UpdateCursorShape(const protocol::CursorShapeInfo& cursor_shape);
