5b3d8d625b276e2c26dc574a4c0cd1bf953e50f5
ccameron@chromium.org <ccameron@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-16 23:33:52 +0000

    Revert 228993 "Disable tab capture perf tests on Aura."
    > Disable tab capture perf tests on Aura.
    > 
    > This was recently re-enabled, but is failing on the Windows (Aura) GPU
    > bots.
    > 
    > TBR=hubbe@chromium.org
    > BUG=308236
    > 
    > Review URL: https://codereview.chromium.org/27537006
    
    TBR=ccameron@chromium.org
    
    Review URL: https://codereview.chromium.org/27613002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@229002 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/tab_capture/tab_capture_performancetest.cc
+++ chrome/browser/extensions/api/tab_capture/tab_capture_performancetest.cc
@@ -232,15 +232,7 @@ class TabCapturePerformanceTest
 }  // namespace
 
 
-// This does not work on Aura GPU bots yet
-// http://crbug.com/308236
-#if defined(USE_AURA)
-#define MAYBE_TabCapturePerformanceTest DISABLED_TabCapturePerformanceTest
-#else
-#define MAYBE_TabCapturePerformanceTest TabCapturePerformanceTest
-#endif
-
-IN_PROC_BROWSER_TEST_P(MAYBE_TabCapturePerformanceTest, Performance) {
+IN_PROC_BROWSER_TEST_P(TabCapturePerformanceTest, Performance) {
   RunTest("TabCapturePerformance");
 }
 
@@ -259,7 +251,7 @@ INSTANTIATE_TEST_CASE_P(
         kTestThroughWebRTC | kDisableVsync,
         kTestThroughWebRTC | kDisableVsync | kUseGpu | kForceGpuComposited));
 
-#if defined(USE_AURA)
+#ifdef USE_AURA
 // TODO(hubbe):
 // These are temporary tests for the purpose of determining what the
 // appropriate scaling quality is. Once that has been determined,
