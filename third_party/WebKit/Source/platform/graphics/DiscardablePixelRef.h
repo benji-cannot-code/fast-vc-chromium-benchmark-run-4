437578275ae7e484a20035b6f85a57151b3f34c6
pilgrim@chromium.org <pilgrim@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-16 18:41:39 +0000

    Move DiscardablePixelRef.cpp/.h to platform/graphics/
    BUG=297477
    R=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/26433004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159800 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/DiscardablePixelRef.h
+++ third_party/WebKit/Source/platform/graphics/DiscardablePixelRef.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "SkBitmap.h"
 #include "SkPixelRef.h"
 
+#include "platform/PlatformExport.h"
 #include "public/platform/WebDiscardableMemory.h"
 #include "wtf/OwnPtr.h"
 #include "wtf/PassOwnPtr.h"
@@ -37,7 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 // Class for allocating the DiscardablePixelRef object.
-class DiscardablePixelRefAllocator : public SkBitmap::Allocator {
+class PLATFORM_EXPORT DiscardablePixelRefAllocator : public SkBitmap::Allocator {
     // SkBitmap::Allocator implementation. The discardable memory allocated
     // after this call is locked and will not be purged until next
     // onUnlockPixels().
@@ -45,7 +46,7 @@ class DiscardablePixelRefAllocator : public SkBitmap::Allocator {
 };
 
 // PixelRef object whose memory can be discarded when pixels are unlocked.
-class DiscardablePixelRef : public SkPixelRef {
+class PLATFORM_EXPORT DiscardablePixelRef : public SkPixelRef {
 public:
     DiscardablePixelRef(PassOwnPtr<SkMutex>);
     ~DiscardablePixelRef();
