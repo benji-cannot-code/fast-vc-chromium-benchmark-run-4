92696ba8c3dcd2e23eda80ba570c4d21d46aebfa
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-16 03:35:30 +0000

    Fix ViewEventTestBase for Win Aura, re-enable tests on Win.
    Move ChromeViewsDelegate usage to ViewEventTestBase.
    (previously just done in BookmarkBarViewEventTestBase)
    (needed to create DesktopNativeWidgetAura widgets)
    (otherwise NativeWidgetAura widgets DCHECK with no parent/context)
    Cleanup SetUp()/TearDown for correctness and clarity.
    Re-enable tests that pass locally with the above change:
    BookmarkBarViewTest20.ContextMenuExitTest (except non-Aura Win)
    ButtonDropDownDragTest.DragActivation
    MenuItemViewTest*.*
    MenuModelAdapterTest.RebuildMenu
    
    These <tests> reliably pass locally with this patch applied.
    interactive_ui_tests.exe --gtest_filter=<tests> --gtest_repeat=-1 --gtest_break_on_failure
    They only break on locking the computer or using the mouse.
    
    BUG=128724
    TEST=NONE
    R=pkasting@chromium.org,sky@chromium.org
    
    Review URL: https://codereview.chromium.org/26567014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228854 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/bookmarks/bookmark_bar_view_test.cc
+++ chrome/browser/ui/views/bookmarks/bookmark_bar_view_test.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/views/bookmarks/bookmark_bar_view.h"
-#include "chrome/browser/ui/views/chrome_views_delegate.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/test/base/interactive_test_utils.h"
 #include "chrome/test/base/scoped_testing_local_state.h"
@@ -167,7 +166,6 @@ class BookmarkBarViewEventTestBase : public ViewEventTestBase {
 
     tmp_parent.RemoveChildView(bb_view_.get());
 
-    views::ViewsDelegate::views_delegate = &views_delegate_;
     ViewEventTestBase::SetUp();
   }
 
@@ -186,7 +184,6 @@ class BookmarkBarViewEventTestBase : public ViewEventTestBase {
 
     ViewEventTestBase::TearDown();
     BookmarkBarView::DisableAnimationsForTesting(false);
-    views::ViewsDelegate::views_delegate = NULL;
   }
 
  protected:
@@ -244,7 +241,6 @@ class BookmarkBarViewEventTestBase : public ViewEventTestBase {
   scoped_ptr<TestingProfile> profile_;
   scoped_ptr<Browser> browser_;
   scoped_ptr<ScopedTestingLocalState> local_state_;
-  ChromeViewsDelegate views_delegate_;
 };
 
 // Clicks on first menu, makes sure button is depressed. Moves mouse to first
@@ -1605,10 +1601,6 @@ class BookmarkBarViewTest19 : public BookmarkBarViewEventTestBase {
 
 VIEW_TEST(BookmarkBarViewTest19, BookmarkBarViewTest19_SiblingMenu)
 
-#if !defined(OS_WIN)
-// Not ready for Win Aura. (NOTREACHED in chrome_views_delegate.cc
-// and desktop_root_window_host_win.cc.)
-
 // Verify that when clicking a mouse button outside a context menu,
 // the context menu is dismissed *and* the underlying view receives
 // the the mouse event (due to event reposting).
@@ -1707,9 +1699,13 @@ class BookmarkBarViewTest20 : public BookmarkBarViewEventTestBase {
   TestViewForMenuExit* test_view_;
 };
 
-VIEW_TEST(BookmarkBarViewTest20, ContextMenuExitTest)
+#if defined(OS_WIN) && !defined(USE_AURA)
+#define MAYBE_ContextMenuExitTest DISABLED_ContextMenuExitTest
+#else
+#define MAYBE_ContextMenuExitTest ContextMenuExitTest
+#endif
 
-#endif // !defined(OS_WIN)
+VIEW_TEST(BookmarkBarViewTest20, MAYBE_ContextMenuExitTest)
 
 // Tests context menu by way of opening a context menu for a empty folder menu.
 // The opened context menu should behave as it is from the folder button.
