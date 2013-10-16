a0dbfc3430ab0316cf8893403db1f64d1de91904
betravis@adobe.com <betravis@adobe.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-16 08:49:07 +0000

    Enable animation for the shape-outside property
    Add shape-outside to the list of animatable CSS properties, using the
    same interpolator as shape-inside.
    
    BUG=304916
    
    Review URL: https://codereview.chromium.org/26546007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159730 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/css/CSSAnimations.cpp
+++ third_party/WebKit/Source/core/animation/css/CSSAnimations.cpp
@@ -547,6 +547,7 @@ bool CSSAnimations::isAnimatableProperty(CSSPropertyID property)
     case CSSPropertyWebkitPerspectiveOriginX:
     case CSSPropertyWebkitPerspectiveOriginY:
     case CSSPropertyWebkitShapeInside:
+    case CSSPropertyWebkitShapeOutside:
     case CSSPropertyWebkitTextStrokeColor:
     case CSSPropertyWebkitTransform:
     case CSSPropertyWebkitTransformOriginX:
