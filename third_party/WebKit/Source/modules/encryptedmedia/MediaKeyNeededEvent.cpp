2908ef6a32008987332c2b52bc354a078fefa85d
tsepez@chromium.org <tsepez@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-16 03:19:42 +0000

    Missing ScriptWrappable::init() in MediaKeyNeededEvent.
    This path is previously uncovered by tests, since the test that ought to
    cover it fails to trigger the event for some reason. So create one explicitly
    in the test and add the initialization.
    
    BUG=307069
    R=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/26601009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159708 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/encryptedmedia/MediaKeyNeededEvent.cpp
+++ third_party/WebKit/Source/modules/encryptedmedia/MediaKeyNeededEvent.cpp
@@ -41,12 +41,14 @@ MediaKeyNeededEventInit::MediaKeyNeededEventInit()
 
 MediaKeyNeededEvent::MediaKeyNeededEvent()
 {
+    ScriptWrappable::init(this);
 }
 
 MediaKeyNeededEvent::MediaKeyNeededEvent(const AtomicString& type, const MediaKeyNeededEventInit& initializer)
     : Event(type, initializer)
     , m_initData(initializer.initData)
 {
+    ScriptWrappable::init(this);
 }
 
 MediaKeyNeededEvent::~MediaKeyNeededEvent()
