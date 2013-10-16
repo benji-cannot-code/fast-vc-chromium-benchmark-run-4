7c8191f0a9dcc0db6e8a418091ad27135a974f2b
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-16 03:05:30 +0000

    Make the OmniboxNavigationObserver update the ShortcutsProvider.
    This means the ShortcutsProvider will only store shortcuts to successful
    navigations, eliminating a major source of bad omnibox suggestions.
    
    BUG=151044
    TEST=Type "sughoidusgbpoiun.com" in omnibox, hit enter; retype "sugho" and ensure old bogus URL is not suggested
    R=brettw@chromium.org, sky@chromium.org
    
    Review URL: https://codereview.chromium.org/27057004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228846 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/omnibox/omnibox_navigation_observer.cc
+++ chrome/browser/ui/omnibox/omnibox_navigation_observer.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/omnibox/omnibox_navigation_observer.h"
 
+#include "chrome/browser/history/shortcuts_backend.h"
+#include "chrome/browser/history/shortcuts_backend_factory.h"
 #include "chrome/browser/infobars/infobar_service.h"
 #include "chrome/browser/intranet_redirect_detector.h"
 #include "chrome/browser/ui/omnibox/alternate_nav_infobar_delegate.h"
@@ -50,8 +52,14 @@ bool IsValidNavigation(const GURL& original_url, const GURL& final_url) {
 // OmniboxNavigationObserver --------------------------------------------------
 
 OmniboxNavigationObserver::OmniboxNavigationObserver(
+    Profile* profile,
+    const string16& text,
+    const AutocompleteMatch& match,
     const GURL& alternate_nav_url)
-    : alternate_nav_url_(alternate_nav_url),
+    : text_(text),
+      match_(match),
+      alternate_nav_url_(alternate_nav_url),
+      shortcuts_backend_(ShortcutsBackendFactory::GetForProfile(profile)),
       load_state_(LOAD_NOT_SEEN),
       fetch_state_(FETCH_NOT_COMPLETE) {
   if (alternate_nav_url_.is_valid()) {
@@ -67,6 +75,11 @@ OmniboxNavigationObserver::OmniboxNavigationObserver(
 }
 
 OmniboxNavigationObserver::~OmniboxNavigationObserver() {
+}
+
+void OmniboxNavigationObserver::OnSuccessfulNavigation() {
+  if (shortcuts_backend_)
+    shortcuts_backend_->OnOmniboxNavigation(text_, match_);
 }
 
 void OmniboxNavigationObserver::Observe(
@@ -89,6 +102,9 @@ void OmniboxNavigationObserver::Observe(
 void OmniboxNavigationObserver::NavigationEntryCommitted(
     const content::LoadCommittedDetails& load_details) {
   load_state_ = LOAD_COMMITTED;
+  if (ResponseCodeIndicatesSuccess(load_details.http_status_code) &&
+      IsValidNavigation(match_.destination_url, load_details.entry->GetURL()))
+    OnSuccessfulNavigation();
   if (!fetcher_ || (fetch_state_ != FETCH_NOT_COMPLETE))
     OnAllLoadingFinished();  // deletes |this|!
 }
@@ -112,6 +128,7 @@ void OmniboxNavigationObserver::NavigateToPendingEntry(
 
 void OmniboxNavigationObserver::OnURLFetchComplete(
     const net::URLFetcher* source) {
+  DCHECK_EQ(fetcher_.get(), source);
   const net::URLRequestStatus& status = source->GetStatus();
   int response_code = source->GetResponseCode();
   fetch_state_ =
--- chrome/browser/ui/omnibox/omnibox_navigation_observer.h
+++ chrome/browser/ui/omnibox/omnibox_navigation_observer.h
@@ -8,15 +8,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
+#include "chrome/browser/autocomplete/autocomplete_match.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "net/url_request/url_fetcher_delegate.h"
 #include "url/gurl.h"
 
-namespace content {
-class NavigationController;
+namespace history {
+class ShortcutsBackend;
 }
 
 namespace net {
@@ -34,7 +36,7 @@ class URLRequestStatus;
 //     displays an infobar once the search result has also loaded.  See
 //     AlternateNavInfoBarDelegate.
 // (2) Omnibox navigations that complete successfully are added to the
-//     Shortcuts backend.  TODO(pkasting): Not yet implemented.
+//     Shortcuts backend.
 //
 // TODO(pkasting): Probably NOTIFICATION_OMNIBOX_OPENED_URL should disappear and
 // everyone who listened to it should be triggered from this class instead.
@@ -53,10 +55,18 @@ class OmniboxNavigationObserver : public content::NotificationObserver,
     LOAD_COMMITTED,
   };
 
-  explicit OmniboxNavigationObserver(const GURL& alternate_nav_url);
+  OmniboxNavigationObserver(Profile* profile,
+                            const string16& text,
+                            const AutocompleteMatch& match,
+                            const GURL& alternate_nav_url);
   virtual ~OmniboxNavigationObserver();
 
   LoadState load_state() const { return load_state_; }
+
+  // Called directly by OmniboxEditModel when an extension-related navigation
+  // occurs.  Such navigations don't trigger an immediate NAV_ENTRY_PENDING and
+  // must be handled separately.
+  void OnSuccessfulNavigation();
 
  private:
   enum FetchState {
@@ -86,7 +96,11 @@ class OmniboxNavigationObserver : public content::NotificationObserver,
   // the alternate nav infobar if necessary, and deletes |this|.
   void OnAllLoadingFinished();
 
-  GURL alternate_nav_url_;
+  const string16 text_;
+  const AutocompleteMatch match_;
+  const GURL alternate_nav_url_;
+  scoped_refptr<history::ShortcutsBackend> shortcuts_backend_;  // May be NULL
+                                                                // in incognito.
   scoped_ptr<net::URLFetcher> fetcher_;
   LoadState load_state_;
   FetchState fetch_state_;
