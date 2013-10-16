9b3c6652c0e29d38d854abee7a55b80a20443b4e
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-16 19:10:12 +0000

    Revert 228857 "Refactor HistoryDataStore into DictionaryDataStor..."
    > Refactor HistoryDataStore into DictionaryDataStore+HistoryDataStore.
    > Separate out the part that writes the dictionary to a file store into a separate class, DictionaryDataStore.
    > 
    > R=xiyuan@chromium.org
    > BUG=None.
    > TEST=Unit tests pass, search cross-session functionality works correctly.
    > 
    > Review URL: https://codereview.chromium.org/26179007
    
    TBR=rkc@chromium.org
    
    Review URL: https://codereview.chromium.org/27031006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228962 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/app_list/search/history_data_store.h
+++ chrome/browser/ui/app_list/search/history_data_store.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/important_file_writer.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
-#include "chrome/browser/ui/app_list/search/common/dictionary_data_store.h"
 #include "chrome/browser/ui/app_list/search/history_data.h"
 
 namespace base {
@@ -30,16 +29,18 @@ class HistoryDataStoreTest;
 }
 
 // A simple json store to persist HistoryData.
-class HistoryDataStore : public base::RefCountedThreadSafe<HistoryDataStore> {
+class HistoryDataStore : public base::RefCountedThreadSafe<HistoryDataStore>,
+                         public base::ImportantFileWriter::DataSerializer {
  public:
   typedef base::Callback<void(scoped_ptr<HistoryData::Associations>)>
       OnLoadedCallback;
+  typedef base::Closure OnFlushedCallback;
 
   explicit HistoryDataStore(const base::FilePath& data_file);
 
   // Flushes pending writes. |on_flushed| is invoked when disk write is
   // finished.
-  void Flush(const DictionaryDataStore::OnFlushedCallback& on_flushed);
+  void Flush(const OnFlushedCallback& on_flushed);
 
   // Reads the persisted data from disk asynchronously. |on_read| is called
   // with the loaded and parsed data. If there is an error, |on_read| is called
@@ -65,10 +66,18 @@ class HistoryDataStore : public base::RefCountedThreadSafe<HistoryDataStore> {
   // Gets entry dictionary for given |query|. Creates one if necessary.
   base::DictionaryValue* GetEntryDict(const std::string& query);
 
-  void OnDictionaryLoadedCallback(OnLoadedCallback callback,
-                                  base::DictionaryValue* dict);
+  // Reads data from backing file.
+  scoped_ptr<HistoryData::Associations> LoadOnBlockingPool();
 
-  scoped_refptr<DictionaryDataStore> data_store_;
+  // ImportantFileWriter::DataSerializer overrides:
+  virtual bool SerializeData(std::string* data) OVERRIDE;
+
+  base::FilePath data_file_;
+  scoped_refptr<base::SequencedTaskRunner> file_task_runner_;
+  scoped_ptr<base::ImportantFileWriter> writer_;
+
+  // Cached json dictionary to serve read and incremental change calls.
+  scoped_ptr<base::DictionaryValue> cached_json_;
 
   DISALLOW_COPY_AND_ASSIGN(HistoryDataStore);
 };
--- chrome/browser/ui/app_list/search/history_data_store_unittest.cc
+++ chrome/browser/ui/app_list/search/history_data_store_unittest.cc
@@ -60,7 +60,7 @@ class HistoryDataStoreTest : public testing::Test {
   }
 
   void Flush() {
-    store_->Flush(DictionaryDataStore::OnFlushedCallback());
+    store_->Flush(HistoryDataStore::OnFlushedCallback());
   }
 
   void Load() {
