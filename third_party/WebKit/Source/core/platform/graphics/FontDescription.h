db1040764ff47efa02d163e7c1dedaed1a9f8ef7
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-16 08:38:03 +0000

    Use the same cache key logic for all font caches
    Introduce a new CacheKey class and a cacheKey method on FontDescription
    and use that instead of the three different keys in FontCache,
    CSSFontFaceSource and CSSSegmentedFontFace.
    
    This does not change any of the caching logic, only the computation
    of the cache key.
    
    TEST=Covered by existing tests.
    BUG=307324
    R=dglazkov@chromium.org,bungeman@chromium.org
    
    Review URL: https://codereview.chromium.org/27270003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159729 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/graphics/FontDescription.h
+++ third_party/WebKit/Source/core/platform/graphics/FontDescription.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define FontDescription_h
 
 #include "FontFamilyNames.h"
+#include "core/platform/graphics/FontCacheKey.h"
 #include "core/platform/graphics/FontOrientation.h"
 #include "core/platform/graphics/FontSmoothingMode.h"
 #include "core/platform/graphics/FontTraitsMask.h"
@@ -134,6 +135,9 @@ public:
     FontWidthVariant widthVariant() const { return static_cast<FontWidthVariant>(m_widthVariant); }
     FontFeatureSettings* featureSettings() const { return m_featureSettings.get(); }
     FontDescription makeNormalFeatureSettings() const;
+
+    float effectiveFontSize() const; // Returns either the computedSize or the computedPixelSize
+    FontCacheKey cacheKey(const AtomicString& familyName, FontTraitsMask desiredTraits = static_cast<FontTraitsMask>(0)) const;
 
     void setFamily(const FontFamily& family) { m_familyList = family; }
     void setComputedSize(float s) { m_computedSize = clampToFloat(s); }
