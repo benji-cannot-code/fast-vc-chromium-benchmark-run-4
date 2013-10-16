4d0607b3b989b15335235fcd7c342eb61bcafd7e
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-16 17:50:53 +0000

    Update Desktop Capture API to use 'tabs.Tab' instead of origin.
    When delegating access to desktop capture stream the API has origin
    parameter. Problem is that with that approach Chrome didn't know which
    tab the request is coming from, so it didn't assign dialog parent
    appropriately. Replaced the origin parameter with a parameter of
    tabs.Tab type, so now chrome knows which window should be parent
    for the dialog.
    
    Also converted interface defintion to JSON, becase IDL doesn't allow
    refererencing types from other namespaces.
    
    BUG=289781
    R=mpcomplete@chromium.org
    
    Review URL: https://codereview.chromium.org/27282002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228943 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/api/api.gyp
+++ chrome/common/extensions/api/api.gyp
@@ -43,7 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
               'context_menus.json',
               'cookies.json',
               'debugger.json',
-              'desktop_capture.idl',
+              'desktop_capture.json',
               'developer_private.idl',
               'dial.idl',
               'dns.idl',
--- chrome/test/data/extensions/api_test/desktop_capture_delegate/background.js
+++ chrome/test/data/extensions/api_test/desktop_capture_delegate/background.js
@@ -5,10 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 chrome.runtime.onMessageExternal.addListener(function(message, sender, reply) {
   if (message[0] == "getStream") {
-    origin = message[1];
     chrome.desktopCapture.chooseDesktopMedia(
         ["screen", "window"],
-        origin,
+        sender.tab,
         function(id) {
           reply({"id": id});
         });
