59c55bdbe750cc71c2004570d5b73f21607ce9e7
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-16 20:34:39 +0000

    Fix nativeMessaging example to use chrome.runtime namespace.
    Previously the example was calling chrome.extension.connectNative().
    
    BUG=
    R=mpcomplete@chromium.org
    
    Review URL: https://codereview.chromium.org/27556002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228976 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/examples/api/nativeMessaging/app/main.js
+++ chrome/common/extensions/docs/examples/api/nativeMessaging/app/main.js
@@ -49,7 +49,7 @@ function onDisconnected() {
 function connect() {
   var hostName = "com.google.chrome.example.echo";
   appendMessage("Connecting to native messaging host <b>" + hostName + "</b>")
-  port = chrome.extension.connectNative(hostName);
+  port = chrome.runtime.connectNative(hostName);
   port.onMessage.addListener(onNativeMessage);
   port.onDisconnect.addListener(onDisconnected);
   updateUiState();
