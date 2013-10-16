658c0318bd507ccf67bed700a26bf5f40181c0a0
dominicc@chromium.org <dominicc@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-16 02:00:14 +0000

    Add use counters to Attr members removed in DOM4.
    This is to gather evidence of what (if anything) can be deprecated and
    removed from Attr. The relevant spec is here:
    
    <http://dom.spec.whatwg.org/#interface-attr>
    
    BUG=305105
    R=eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/26463002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159704 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Attr.cpp
+++ third_party/WebKit/Source/core/dom/Attr.cpp
@@ -30,8 +30,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/v8/ExceptionStatePlaceholder.h"
 #include "core/dom/Element.h"
 #include "core/dom/ExceptionCode.h"
-#include "core/events/ScopedEventQueue.h"
 #include "core/dom/Text.h"
+#include "core/events/ScopedEventQueue.h"
+#include "core/page/UseCounter.h"
 #include "wtf/text/AtomicString.h"
 #include "wtf/text/StringBuilder.h"
 
@@ -93,6 +94,8 @@ void Attr::createTextChild()
 
 void Attr::setPrefix(const AtomicString& prefix, ExceptionState& es)
 {
+    UseCounter::count(document(), UseCounter::AttributeSetPrefix);
+
     checkSetPrefix(prefix, es);
     if (es.hadException())
         return;
