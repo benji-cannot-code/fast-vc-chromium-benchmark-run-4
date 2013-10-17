479e68e5d7d109ce5a563d2d6a2ada2702d3099a
hirono@chromium.org <hirono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-17 08:21:33 +0000

    Previously the initEssentialUI_ has lines
    * that can be done at the earlier steps.
    * that are no longer needed.
    * that should be done in the FileManagerUI class.
    
    This CL removed the above lines.
    
    BUG=267281
    TEST=manually
    R=mtomasz@chromium.org
    
    Review URL: https://codereview.chromium.org/27300004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@229076 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -16,6 +16,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 function FileManager() {
   this.initializeQueue_ = new AsyncUtil.Group();
+
+  /**
+   * Current list type.
+   * @type {ListType}
+   * @private
+   */
+  this.listType_ = null;
+
+  /**
+   * Whether to suppress the focus moving or not.
+   * This is used to filter out focusing by mouse.
+   * @type {boolean}
+   * @private
+   */
+  this.suppressFocus_ = false;
+
+  /**
+   * SelectionHandler.
+   * @type {SelectionHandler}
+   * @private
+   */
+  this.selectionHandler_ = null;
 }
 
 /**
@@ -162,8 +184,6 @@ var BOTTOM_MARGIN_FOR_PREVIEW_PANEL_PX = 52;
     // Get startup preferences.
     this.viewOptions_ = {};
     group.add(function(done) {
-      this.dialogType = this.params_.type || DialogType.FULL_PAGE;
-      this.startupPrefName_ = 'file-manager-' + this.dialogType;
       util.platform.getPreference(this.startupPrefName_, function(value) {
         // Load the global default options.
         try {
@@ -547,6 +567,7 @@ var BOTTOM_MARGIN_FOR_PREVIEW_PANEL_PX = 52;
 
   FileManager.prototype.initializeUI = function(dialogDom, callback) {
     this.dialogDom_ = dialogDom;
+    this.document_ = this.dialogDom_.ownerDocument;
 
     this.initializeQueue_.add(
         this.initEssentialUI_.bind(this),
@@ -581,6 +602,12 @@ var BOTTOM_MARGIN_FOR_PREVIEW_PANEL_PX = 52;
                      {};
       this.defaultPath = this.params_.defaultPath;
     }
+
+    // Initialize the member variables that depend this.params_.
+    this.dialogType = this.params_.type || DialogType.FULL_PAGE;
+    this.startupPrefName_ = 'file-manager-' + this.dialogType;
+    this.fileTypes_ = this.params_.typeList || [];
+
     callback();
   };
 
@@ -654,20 +681,7 @@ var BOTTOM_MARGIN_FOR_PREVIEW_PANEL_PX = 52;
    * @private
    */
   FileManager.prototype.initEssentialUI_ = function(callback) {
-    this.listType_ = null;
-
-    this.filesystemObserverId_ = null;
-    this.driveObserverId_ = null;
-
-    this.document_ = this.dialogDom_.ownerDocument;
-    this.dialogType = this.params_.type || DialogType.FULL_PAGE;
-    this.startupPrefName_ = 'file-manager-' + this.dialogType;
-
-    // Used to filter out focusing by mouse.
-    this.suppressFocus_ = false;
-
     // Optional list of file types.
-    this.fileTypes_ = this.params_.typeList || [];
     metrics.recordEnum('Create', this.dialogType,
         [DialogType.SELECT_FOLDER,
          DialogType.SELECT_UPLOAD_FOLDER,
@@ -676,34 +690,14 @@ var BOTTOM_MARGIN_FOR_PREVIEW_PANEL_PX = 52;
          DialogType.SELECT_OPEN_MULTI_FILE,
          DialogType.FULL_PAGE]);
 
-    this.selectionHandler_ = null;
-
+    // Create the metadata cache.
     this.metadataCache_ = MetadataCache.createFull();
 
-    this.hasFooterPanel_ =
-        this.dialogType == DialogType.SELECT_SAVEAS_FILE ||
-        this.dialogType == DialogType.SELECT_FOLDER;
-
-    // If the footer panel exists, the buttons are placed there. Otherwise,
-    // the buttons are on the preview panel.
-    var parentPanelOfButtons = this.dialogDom_.querySelector(
-        !this.hasFooterPanel_ ? '.preview-panel' : '.dialog-footer');
-    parentPanelOfButtons.classList.add('button-panel');
-    this.fileTypeSelector_ = parentPanelOfButtons.querySelector('.file-type');
-    this.okButton_ = parentPanelOfButtons.querySelector('.ok');
-    this.cancelButton_ = parentPanelOfButtons.querySelector('.cancel');
-
-    // Pre-populate the static localized strings.
-    i18nTemplate.process(this.document_, loadTimeData);
-
-    // Initialize the header.
-    this.dialogDom_.querySelector('#app-name').innerText =
-        chrome.runtime.getManifest().name;
-
-    this.initDialogType_();
-
     // Create the root view of FileManager.
-    this.ui_ = new FileManagerUI(this.dialogDom_);
+    this.ui_ = new FileManagerUI(this.dialogDom_, this.dialogType);
+    this.fileTypeSelector_ = this.ui_.fileTypeSelector;
+    this.okButton_ = this.ui_.okButton;
+    this.cancelButton_ = this.ui_.cancelButton;
 
     // Show the window as soon as the UI pre-initialization is done.
     if (this.dialogType == DialogType.FULL_PAGE &&
@@ -1574,50 +1568,6 @@ var BOTTOM_MARGIN_FOR_PREVIEW_PANEL_PX = 52;
         return;
       }
     }.bind(this));
-  };
-
-  /**
-   * Tweak the UI to become a particular kind of dialog, as determined by the
-   * dialog type parameter passed to the constructor.
-   *
-   * @private
-   */
-  FileManager.prototype.initDialogType_ = function() {
-    var defaultTitle;
-    var okLabel = str('OPEN_LABEL');
-
-    switch (this.dialogType) {
-      case DialogType.SELECT_FOLDER:
-        defaultTitle = str('SELECT_FOLDER_TITLE');
-        break;
-
-      case DialogType.SELECT_UPLOAD_FOLDER:
-        defaultTitle = str('SELECT_UPLOAD_FOLDER_TITLE');
-        okLabel = str('UPLOAD_LABEL');
-        break;
-
-      case DialogType.SELECT_OPEN_FILE:
-        defaultTitle = str('SELECT_OPEN_FILE_TITLE');
-        break;
-
-      case DialogType.SELECT_OPEN_MULTI_FILE:
-        defaultTitle = str('SELECT_OPEN_MULTI_FILE_TITLE');
-        break;
-
-      case DialogType.SELECT_SAVEAS_FILE:
-        defaultTitle = str('SELECT_SAVEAS_FILE_TITLE');
-        okLabel = str('SAVE_LABEL');
-        break;
-
-      case DialogType.FULL_PAGE:
-        break;
-
-      default:
-        throw new Error('Unknown dialog type: ' + this.dialogType);
-    }
-
-    this.okButton_.textContent = okLabel;
-    this.dialogDom_.setAttribute('type', this.dialogType);
   };
 
   /**
