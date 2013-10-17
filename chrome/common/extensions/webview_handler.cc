2aefe8d8a81671560176daaf78a482d9b1a50cfb
fsamuel@chromium.org <fsamuel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-17 15:33:21 +0000

    <webview>: Fix crash when navigating to invalid chrome-extension: URL
    BUG=308119
    Test=Open Browser app demo, navigation to an invalid chrome-extension: URL such as chrome-extension://abc123/guest.html. Chrome should not crash.
    
    Review URL: https://codereview.chromium.org/26332006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@229137 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/webview_handler.cc
+++ chrome/common/extensions/webview_handler.cc
@@ -22,9 +22,9 @@ namespace errors = extensions::manifest_errors;
 namespace {
 
 const WebviewInfo* GetResourcesInfo(
-    const Extension* extension) {
+    const Extension& extension) {
   return static_cast<WebviewInfo*>(
-      extension->GetManifestData(keys::kWebviewAccessibleResources));
+      extension.GetManifestData(keys::kWebviewAccessibleResources));
 }
 
 }  // namespace
@@ -40,7 +40,10 @@ bool WebviewInfo::IsResourceWebviewAccessible(
     const Extension* extension,
     const std::string& partition_id,
     const std::string& relative_path) {
-  const WebviewInfo* info = GetResourcesInfo(extension);
+  if (!extension)
+    return false;
+
+  const WebviewInfo* info = GetResourcesInfo(*extension);
   if (!info)
     return false;
 
@@ -56,12 +59,6 @@ bool WebviewInfo::IsResourceWebviewAccessible(
 
   return partition_is_privileged && extension->ResourceMatches(
       info->webview_accessible_resources_, relative_path);
-}
-
-// static
-bool WebviewInfo::HasWebviewAccessibleResources(const Extension* extension) {
-  const WebviewInfo* info = GetResourcesInfo(extension);
-  return info && info->webview_accessible_resources_.size() > 0;
 }
 
 WebviewHandler::WebviewHandler() {
