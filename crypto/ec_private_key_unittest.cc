eac4bf04bb77904f3abdc5ee7fb7ebd6dcbff18f
digit@chromium.org <digit@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-17 16:09:24 +0000

    openssl: Implement crypto::ECPrivateKey.
    BUG=306176
    R=rsleevi@chromium.org, wtc@chromium.org, agl@chromium.org
    
    Review URL: https://codereview.chromium.org/27195002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@229153 0039d316-1c4b-4281-b951-d872f2087c98

--- crypto/ec_private_key_unittest.cc
+++ crypto/ec_private_key_unittest.cc
@@ -10,16 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-#if defined(USE_OPENSSL)
-// Once ECPrivateKey is implemented for OpenSSL, remove this #if block.
-// TODO(mattm): When that happens, also add some exported keys from each to test
+// TODO(mattm): Add some exported keys from each to test
 // interop between NSS and OpenSSL.
-TEST(ECPrivateKeyUnitTest, OpenSSLStub) {
-  scoped_ptr<crypto::ECPrivateKey> keypair1(
-      crypto::ECPrivateKey::Create());
-  ASSERT_FALSE(keypair1.get());
-}
-#else
+
 // Generate random private keys. Export, then re-import. We should get
 // back the same exact public key, and the private key should have the same
 // value and elliptic curve params.
@@ -105,4 +98,3 @@ TEST(ECPrivateKeyUnitTest, BadPasswordTest) {
           password2, privkey1, pubkey1));
   ASSERT_FALSE(keypair2.get());
 }
-#endif  // !defined(USE_OPENSSL)
