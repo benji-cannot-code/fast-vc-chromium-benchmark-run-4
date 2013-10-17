0493944b01082895e824820a5e9d6b98b6891f8c
felt@chromium.org <felt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-17 10:53:56 +0000

    This removes the 'keep' option from the download notifications for known malware (dangerous host, URL, or content).
    Windows, ChromeOS: This removes the 'keep' option from the dropdown menu that hangs off the notification/warning. Only the 'learn more' option remains in the dropdown menu that hangs off the notification. One exception: mattm is doing a feedback experiment, and if that experiment is turned on there is also a plain 'discard' option in the shelf.
    
    GTK (Mac, Linux): This removes the 'keep' option from the main download notification itself, since there is no dropdown. 
    
    This does *not* change anything in chrome://downloads.
    
    BUG=305387
    
    Review URL: https://codereview.chromium.org/26261010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@229094 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/download/download_item_view.cc
+++ chrome/browser/ui/views/download/download_item_view.cc
@@ -1075,7 +1075,7 @@ void DownloadItemView::ClearWarningDialog() {
 
 void DownloadItemView::ShowWarningDialog() {
   DCHECK(mode_ != DANGEROUS_MODE && mode_ != MALICIOUS_MODE);
-  mode_ = ((model_.IsMalicious()) ? MALICIOUS_MODE : DANGEROUS_MODE);
+  mode_ = model_.MightBeMalicious() ? MALICIOUS_MODE : DANGEROUS_MODE;
 
   body_state_ = NORMAL;
   drop_down_state_ = NORMAL;
