adff58e8745a7cf8b08e2c2848456e772eca7532
yosin@chromium.org <yosin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-17 04:15:29 +0000

    Re-factor InputMethodController for setSelectionOffsets
    This patch moves implementation of Editor::setSelectionOffsets related member functions in WebViewImpl to InputMethodController to make Editor and WebViewImple smaller and improve modularity of InputMethodController for ease of maintenance.
    
    BUG=276777
    TEST=n/a; no behavior changes, webkit_unit_tests covers.
    R=kochi@chromium.org, tkent@chromium.org
    
    Review URL: https://codereview.chromium.org/26973003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159823 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/Editor.h
+++ third_party/WebKit/Source/core/editing/Editor.h
@@ -191,8 +191,6 @@ public:
     bool smartInsertDeleteEnabled();
     bool isSelectTrailingWhitespaceEnabled();
 
-    bool setSelectionOffsets(int selectionStart, int selectionEnd);
-
     bool preventRevealSelection() const { return m_preventRevealSelection; }
 
     void setStartNewKillRingSequence(bool);
