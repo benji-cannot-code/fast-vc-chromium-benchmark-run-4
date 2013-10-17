d0fc65a26370207f651ed59b656657d7c920999f
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-17 12:41:21 +0000

    [Cleanup] move root window controller init logic to root_window_controller.cc
     Remove obsolete code and comments for 165214
    
    BUG=273142,165214
    
    Review URL: https://codereview.chromium.org/26800007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@229106 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/display/display_controller.cc
+++ ash/display/display_controller.cc
@@ -298,7 +298,7 @@ void DisplayController::InitSecondaryDisplays() {
     const gfx::Display& display = display_manager->GetDisplayAt(i);
     if (primary_display_id != display.id()) {
       aura::RootWindow* root = AddRootWindowForDisplay(display);
-      Shell::GetInstance()->InitRootWindowForSecondaryDisplay(root);
+      internal::RootWindowController::CreateForSecondaryDisplay(root);
     }
   }
   UpdateHostWindowNames();
@@ -618,7 +618,7 @@ void DisplayController::OnDisplayAdded(const gfx::Display& display) {
       primary_display_id = display.id();
     DCHECK(!root_windows_.empty());
     aura::RootWindow* root = AddRootWindowForDisplay(display);
-    Shell::GetInstance()->InitRootWindowForSecondaryDisplay(root);
+    internal::RootWindowController::CreateForSecondaryDisplay(root);
   }
 }
 
