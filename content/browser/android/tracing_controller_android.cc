190f8ea9d3a584948b70ab41c9716d29eba43776
wangxianzhu@chromium.org <wangxianzhu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-17 19:12:17 +0000

    Use CategoryFilter::kDefaultCategoryFilterString instead of "*" as the default category
    NOTRY=true (passed presubmit and android try bots)
    R=skyostil@chromium.org, yfriedman@chromium.org
    
    Review URL: https://codereview.chromium.org/26358012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@229195 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/android/tracing_controller_android.cc
+++ content/browser/android/tracing_controller_android.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_android.h"
 #include "base/android/jni_string.h"
 #include "base/command_line.h"
+#include "base/debug/trace_event.h"
 #include "base/files/file_path.h"
 #include "base/logging.h"
 #include "content/browser/tracing/trace_subscriber_stdio.h"
@@ -89,6 +90,11 @@ void TracingControllerAndroid::OnTracingStopped() {
     Java_TracingControllerAndroid_onTracingStopped(env, obj.obj());
   }
   subscriber_.reset();
+}
+
+static jstring GetDefaultCategories(JNIEnv* env, jobject obj) {
+  return base::android::ConvertUTF8ToJavaString(env,
+      base::debug::CategoryFilter::kDefaultCategoryFilterString).Release();
 }
 
 bool RegisterTracingControllerAndroid(JNIEnv* env) {
