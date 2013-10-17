b0d6f16e4ae1e0a5fbc5822b9b1560fc8d09c036
DHNishi@gmail.com <DHNishi@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-17 19:28:46 +0000

    Add a deprecated annotation for api documentation.
    Example Image: http://i.imgur.com/CNGWOVS.png
    
    NOTRY=true
    BUG=239196
    
    Review URL: https://codereview.chromium.org/26231004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@229207 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/server2/api_data_source.py
+++ chrome/common/extensions/docs/server2/api_data_source.py
@@ -243,6 +243,9 @@ class _JSCModel(object):
       'returns': None,
       'id': _CreateId(function, 'method')
     }
+    if (function.deprecated is not None):
+      function_dict['deprecated'] = self._FormatDescription(
+          function.deprecated)
     if (function.parent is not None and
         not isinstance(function.parent, model.Namespace)):
       function_dict['parentName'] = function.parent.simple_name
