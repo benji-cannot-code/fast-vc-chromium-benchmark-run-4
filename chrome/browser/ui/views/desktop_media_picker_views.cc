031fdef2cbfabe50b05aa54160334f2218765fdc
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-17 16:21:33 +0000

    Fix DesktopMediaPickerViews to dismiss the dialog on doubleclick.
    BUG=297812
    
    Review URL: https://codereview.chromium.org/27549002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@229162 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/desktop_media_picker_views.cc
+++ chrome/browser/ui/views/desktop_media_picker_views.cc
@@ -114,6 +114,9 @@ class DesktopMediaListView : public views::View,
   // Called by DesktopMediaSourceView when selection has changed.
   void OnSelectionChanged();
 
+  // Called by DesktopMediaSourceView when a source has been double-clicked.
+  void OnDoubleClick();
+
   // Returns currently selected source.
   DesktopMediaSourceView* GetSelection();
 
@@ -150,6 +153,7 @@ class DesktopMediaPickerDialogView : public views::DialogDelegateView {
 
   // Called by DesktopMediaListView.
   void OnSelectionChanged();
+  void OnDoubleClick();
 
   // views::View overrides.
   virtual gfx::Size GetPreferredSize() OVERRIDE;
@@ -293,7 +297,12 @@ void DesktopMediaSourceView::OnFocus() {
 }
 
 bool DesktopMediaSourceView::OnMousePressed(const ui::MouseEvent& event) {
-  RequestFocus();
+  if (event.GetClickCount() == 1) {
+    RequestFocus();
+  } else if (event.GetClickCount() == 2) {
+    RequestFocus();
+    parent_->OnDoubleClick();
+  }
   return true;
 }
 
@@ -315,6 +324,10 @@ void DesktopMediaListView::StartUpdating(
 
 void DesktopMediaListView::OnSelectionChanged() {
   parent_->OnSelectionChanged();
+}
+
+void DesktopMediaListView::OnDoubleClick() {
+  parent_->OnDoubleClick();
 }
 
 DesktopMediaSourceView* DesktopMediaListView::GetSelection() {
@@ -549,6 +562,11 @@ void DesktopMediaPickerDialogView::DeleteDelegate() {
 
 void DesktopMediaPickerDialogView::OnSelectionChanged() {
   GetDialogClientView()->UpdateDialogButtons();
+}
+
+void DesktopMediaPickerDialogView::OnDoubleClick() {
+  // This will call Accept() and close the dialog.
+  GetDialogClientView()->AcceptWindow();
 }
 
 DesktopMediaPickerViews::DesktopMediaPickerViews()
