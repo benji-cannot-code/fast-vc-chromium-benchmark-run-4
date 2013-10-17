4a3b67fd9748b1304c934b104e08b8d3cf53d8f1
steveblock@chromium.org <steveblock@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-17 10:23:28 +0000

    Fix an optimization in AnimationControllerPrivate::clear()
    As noted in the comment, this method should return false if the animation is
    suspended, but this was broken in
    http://src.chromium.org/viewvc/blink?view=revision&revision=39059
    
    R=dstockwell
    
    Review URL: https://codereview.chromium.org/27516005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159853 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/animation/AnimationController.cpp
+++ third_party/WebKit/Source/core/frame/animation/AnimationController.cpp
@@ -81,7 +81,7 @@ bool AnimationControllerPrivate::clear(RenderObject* renderer)
     if (!animation)
         return false;
     animation->clearRenderer();
-    return animation->suspended();
+    return !animation->suspended();
 }
 
 void AnimationControllerPrivate::updateAnimations(double& timeToNextService, double& timeToNextEvent, SetNeedsStyleRecalc callSetNeedsStyleRecalc/* = DoNotCallSetNeedsStyleRecalc*/)
