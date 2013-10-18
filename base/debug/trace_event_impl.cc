c6f2fe490266426a6d6b7ef3a3c54c4f0304264e
haraken@chromium.org <haraken@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-18 08:39:02 +0000

    Revert 228889 "Avoid threading races on TraceSamplingThread's me..."
    > Avoid threading races on TraceSamplingThread's members.
    > 
    > See a TSAN log in the bug for more details.
    > 
    > BUG=305459
    > 
    > Review URL: https://codereview.chromium.org/26541005
    
    TBR=haraken@chromium.org
    
    Review URL: https://codereview.chromium.org/28623002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@229328 0039d316-1c4b-4281-b951-d872f2087c98

--- base/debug/trace_event_impl.cc
+++ base/debug/trace_event_impl.cc
@@ -830,7 +830,6 @@ class TraceSamplingThread : public PlatformThread::Delegate {
   static void ExtractCategoryAndName(const char* combined,
                                      const char** category,
                                      const char** name);
-  Lock lock_;
   std::vector<TraceBucketData> sample_buckets_;
   bool thread_running_;
   scoped_ptr<CancellationFlag> cancellation_flag_;
@@ -840,7 +839,6 @@ class TraceSamplingThread : public PlatformThread::Delegate {
 
 TraceSamplingThread::TraceSamplingThread()
     : thread_running_(false) {
-  AutoLock lock(lock_);
   cancellation_flag_.reset(new CancellationFlag);
 }
 
@@ -851,14 +849,10 @@ void TraceSamplingThread::ThreadMain() {
   PlatformThread::SetName("Sampling Thread");
   thread_running_ = true;
   const int kSamplingFrequencyMicroseconds = 1000;
-  while (true) {
+  while (!cancellation_flag_->IsSet()) {
     PlatformThread::Sleep(
         TimeDelta::FromMicroseconds(kSamplingFrequencyMicroseconds));
     GetSamples();
-
-    AutoLock lock(lock_);
-    if (cancellation_flag_->IsSet())
-      break;
     if (waitable_event_for_testing_.get())
       waitable_event_for_testing_->Signal();
   }
@@ -882,7 +876,6 @@ void TraceSamplingThread::DefaultSamplingCallback(
 }
 
 void TraceSamplingThread::GetSamples() {
-  AutoLock lock(lock_);
   for (size_t i = 0; i < sample_buckets_.size(); ++i) {
     TraceBucketData* bucket_data = &sample_buckets_[i];
     bucket_data->callback.Run(bucket_data);
@@ -893,7 +886,6 @@ void TraceSamplingThread::RegisterSampleBucket(
     TRACE_EVENT_API_ATOMIC_WORD* bucket,
     const char* const name,
     TraceSampleCallback callback) {
-  AutoLock lock(lock_);
   DCHECK(!thread_running_);
   sample_buckets_.push_back(TraceBucketData(bucket, name, callback));
 }
@@ -907,13 +899,11 @@ void TraceSamplingThread::ExtractCategoryAndName(const char* combined,
 }
 
 void TraceSamplingThread::Stop() {
-  AutoLock lock(lock_);
   cancellation_flag_->Set();
 }
 
 void TraceSamplingThread::InstallWaitableEventForSamplingTesting(
     WaitableEvent* waitable_event) {
-  AutoLock lock(lock_);
   waitable_event_for_testing_.reset(waitable_event);
 }
 
