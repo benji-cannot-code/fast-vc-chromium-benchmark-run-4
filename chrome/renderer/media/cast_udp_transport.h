1a1313fdb26d4ee8c4d5f3787f049ff37074ee65
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-18 10:30:24 +0000

    Interface and skeleton code for Cast Extensions API
    Adding the following classes that make up the Cast Extensions API:
    
    1. CastSendTransport
       Allows configuration of RTP and encoding of streams. Takes
       video/audio streams and connect to network transport.
    
    2. CastUdpTransport
       Interface for a UDP transport in Cast. This is used by
       CastSendTransport to connect to a peer.
    
    3. CastSession
       A hub of objects that make up the Cast services. Submits actual
       video and audio data to CastSender, which performs encoding and
       packetization. It takes the packetized data to a network transport.
    
    BUG=301920
    
    Review URL: https://codereview.chromium.org/27435002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@229348 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/renderer/media/cast_udp_transport.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_RENDERER_MEDIA_CAST_UDP_TRANSPORT_H_
+#define CHROME_RENDERER_MEDIA_CAST_UDP_TRANSPORT_H_
+
+#include "base/basictypes.h"
+#include "net/base/ip_endpoint.h"
+
+// This class represents an end point to which communication is done by
+// UDP. The interface does not allow direct access to a UDP socket but
+// represents a transport mechanism.
+class CastUdpTransport {
+ public:
+  CastUdpTransport();
+  ~CastUdpTransport();
+
+  // Begin the transport by specifying the remote IP address.
+  // The transport will use UDP.
+  void Start(const net::IPEndPoint& remote_address);
+
+  // Terminate the communication with the end point.
+  void Stop();
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(CastUdpTransport);
+};
+
+#endif  // CHROME_RENDERER_MEDIA_CAST_UDP_TRANSPORT_H_
