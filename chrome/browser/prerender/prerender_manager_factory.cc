a3e581069b57d3372f1b7a16f44a11c213fe9d4d
tburkard@chromium.org <tburkard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-18 20:49:53 +0000

    Defer checking whether unencrypted sync is enabled until we are well past profile initialization. R=dougw@chromium.org BUG=308813
    Review URL: https://codereview.chromium.org/29643002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@229468 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prerender/prerender_manager_factory.cc
+++ chrome/browser/prerender/prerender_manager_factory.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/prerender/prerender_manager.h"
 #include "chrome/browser/profiles/incognito_helpers.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/sync/profile_sync_service_factory.h"
 #include "components/browser_context_keyed_service/browser_context_dependency_manager.h"
 
 #if defined(OS_CHROMEOS)
@@ -49,6 +50,7 @@ PrerenderManagerFactory::PrerenderManagerFactory()
   // PrerenderLocalPredictor observers the history visit DB.
   DependsOn(HistoryServiceFactory::GetInstance());
   DependsOn(predictors::PredictorDatabaseFactory::GetInstance());
+  DependsOn(ProfileSyncServiceFactory::GetInstance());
 }
 
 PrerenderManagerFactory::~PrerenderManagerFactory() {
