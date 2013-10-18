b817addb2800bcad032cd58b74392e49d5dd0bdf
peria@chromium.org <peria@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-18 09:50:38 +0000

    Set uptime on x-axis in the graph of DMP visualizer. It is assumed that the first snapshot has been taken on booting Chrome.
    BUG=259206
    TEST=Manual
    
    Review URL: https://codereview.chromium.org/26867003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@229337 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/deep_memory_profiler/visualizer/static/profiler.js
+++ tools/deep_memory_profiler/visualizer/static/profiler.js
@@ -169,6 +169,7 @@ Profiler.prototype.accumulate_ = function(
   var remainderUnits = localUnits.slice(0);
   var model = {
     name: name || worldName + '-' + breakdownName,
+    time: snapshot.time,
     children: []
   };
 
