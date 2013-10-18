ae059cf75b19f2e63f268efeade5e10b26498100
Tomasz Mikolajewski <mtomasz@chromium.org>
2013-10-18 18:49:58 +0900

    Fix exporting by hiting Enter.
    This patch fixes a wrong method name called when pressing Enter in the
    browser view. This caused a JS error, instead of exporting.
    
    TEST=Tested manually. Open the browser view, then hit Enter.
    BUG=None
    
    Change-Id: Ie47d6aa657b9b55e235e3d970d93e11d041eb6bc

--- src/js/views/browser.js
+++ src/js/views/browser.js
@@ -368,7 +368,7 @@ camera.views.Browser.prototype.onKeyPressed = function(event) {
                 });
       break;
     case 'Enter':
-      this.exportPicture_();
+      this.exportSelection_();
       break;
     case 'U+001B':
       this.context_.onGalleryRequested();
