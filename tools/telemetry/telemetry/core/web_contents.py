55205f91350b57ce081e079202e9e6eec2f04d83
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-18 00:08:49 +0000

    [Telemetry] Raise default web contents timeout from 60->90s.
    We want to measure muzoboss in the tough_layout_cases page set, which
    legitimately takes longer than 60s to load on a Galaxy Nexus.
    
    BUG=308627
    
    Review URL: https://codereview.chromium.org/26206005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@229263 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/web_contents.py
+++ tools/telemetry/telemetry/core/web_contents.py
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-DEFAULT_WEB_CONTENTS_TIMEOUT = 60
+DEFAULT_WEB_CONTENTS_TIMEOUT = 90
 
 # TODO(achuith, dtu, nduca): Add unit tests specifically for WebContents,
 # independent of Tab.
