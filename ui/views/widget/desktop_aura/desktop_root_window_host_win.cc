ee5ec0166b396dcaa22e11a12fc4258985a2b70a
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-18 14:18:08 +0000

    Revert 229285 "Gets hiding animations to work again"
    > Gets hiding animations to work again
    > 
    > This broke when bounds changes was consolidated into
    > DesktopNativeWidgetAura::OnRootWindowHostResized. We can't resize the
    > child layers when this happens as otherwise we're going to force a
    > paint of the layers and thus the views. But at this point nothing is
    > valid and we don't want to paint anyway.
    > 
    > Also removing a call to Show() early on as it may trigger animations
    > that we don't want. A Show() at this time is before the creator has
    > had a chance to turn them off.
    > 
    > BUG=none
    > TEST=none
    > R=ananta@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/27043007
    
    TBR=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/29143002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@229410 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/desktop_aura/desktop_root_window_host_win.cc
+++ ui/views/widget/desktop_aura/desktop_root_window_host_win.cc
@@ -160,6 +160,7 @@ aura::RootWindow* DesktopRootWindowHostWin::Init(
                                                        GetHWND()));
   aura::client::SetDragDropClient(root_window_, drag_drop_client_.get());
 
+  focus_client_->FocusWindow(content_window_);
   root_window_->SetProperty(kContentWindowForRootWindow, content_window_);
 
   aura::client::SetAnimationHost(content_window_, this);
@@ -169,6 +170,10 @@ aura::RootWindow* DesktopRootWindowHostWin::Init(
       !views::corewm::WindowAnimationsDisabled(content_window_);
 
   return root_window_;
+}
+
+void DesktopRootWindowHostWin::InitFocus(aura::Window* window) {
+  focus_client_->FocusWindow(window);
 }
 
 scoped_ptr<corewm::Tooltip> DesktopRootWindowHostWin::CreateTooltip() {
@@ -386,10 +391,6 @@ void DesktopRootWindowHostWin::OnNativeWidgetFocus() {
 }
 
 void DesktopRootWindowHostWin::OnNativeWidgetBlur() {
-}
-
-bool DesktopRootWindowHostWin::IsAnimatingClosed() const {
-  return pending_close_;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
