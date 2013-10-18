7a6a6599c98e83d0297a6aea1bf12eb2198f5ab8
bungeman@chromium.org <bungeman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-18 18:24:17 +0000

    Fix non-aura build break in r229103.
    The change in r229103 missed adding a required header file.
    
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/28733006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@229444 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metro_utils/metro_chrome_win.cc
+++ chrome/browser/metro_utils/metro_chrome_win.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/path_service.h"
+#include "base/win/metro.h"
 #include "base/win/scoped_com_initializer.h"
 #include "base/win/scoped_comptr.h"
 #include "chrome/installer/util/browser_distribution.h"
