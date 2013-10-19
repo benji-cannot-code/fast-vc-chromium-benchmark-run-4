d2f8147dae25039f10d6752c341b92b6d2bca1e4
nbarth@chromium.org <nbarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-19 15:30:51 +0000

    IDL compiler: Cleanup [ActivityLogging]
    This:
    1. Removes unnecessary includes:
    bindings/v8/V8Binding.h  (always included)
    wtf/Vector.h  (not needed)
    ...from [ActivityLogging] functions.
    
    2. Add 'ForAllWorlds' suffix for clarity:
    Before:
    Access, AccessForIsolatedWorlds
    After:
    AccessForAllWorlds, AccessForIsolatedWorlds
    
    Docs:
    https://sites.google.com/a/chromium.org/dev/blink/webidl/blink-idl-extended-attributes#TOC-ActivityLogging-m-a-
    
    BUG=239771
    
    Review URL: https://codereview.chromium.org/27646002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160013 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/tests/results/V8TestObjectPython.cpp
+++ third_party/WebKit/Source/bindings/tests/results/V8TestObjectPython.cpp
@@ -61,7 +61,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/GetPtr.h"
 #include "wtf/RefPtr.h"
 #include "wtf/UnusedParam.h"
-#include "wtf/Vector.h"
 
 namespace WebCore {
 
