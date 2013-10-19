e50c3af9a66e84401bcb56dd1a4b24b8be975c27
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-19 18:23:41 +0000

    views: Cleanup stubs_aura.cc.
    Remove a lot of unused forward declarations and unused includes.
    
    BUG=None
    TEST=None
    TBR=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/30593002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@229606 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/stubs_aura.cc
+++ chrome/browser/ui/views/stubs_aura.cc
@@ -3,34 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/callback.h"
 #include "base/logging.h"
 #include "chrome/browser/external_protocol/external_protocol_handler.h"
-#include "ui/gfx/native_widget_types.h"
-
-#if !defined(OS_WIN)
-#include "chrome/browser/ui/certificate_dialogs.h"
-#endif
-
-#if defined(USE_NSS)
-#include "chrome/browser/ui/crypto_module_password_dialog.h"
-#endif
-
-class SSLClientAuthHandler;
-
-namespace content {
-class WebContents;
-}
-
-namespace net {
-class HttpNetworkSession;
-class SSLCertRequestInfo;
-class X509Certificate;
-}
-
-namespace views {
-class Widget;
-}
+#include "chrome/browser/ui/browser_dialogs.h"
 
 namespace chrome {
 
@@ -44,6 +19,8 @@ void ShowAboutIPCDialog() {
 #if !defined(OS_CHROMEOS) && !defined(OS_WIN)
 // static
 void ExternalProtocolHandler::RunExternalProtocolDialog(
-    const GURL& url, int render_process_host_id, int routing_id) {
+    const GURL& url,
+    int render_process_host_id,
+    int routing_id) {
 }
 #endif
