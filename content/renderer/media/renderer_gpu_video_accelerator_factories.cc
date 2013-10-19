b903cd621707c2c6e8d8750a7f853be28b359aca
sheu@chromium.org <sheu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-19 18:05:07 +0000

    Remove message loop parameter from RenderThreadImpl::GetGpuFactories()
    RenderThreadImpl::GetGpuFactories() makes the assumption that all the
    GpuVideoAcceleratorFactories retrieved from it can use the same 3D context.
    This is only true if all the factories use the same message loop for the
    3D context.  Ensure this by unconditionally using
    RenderThreadImpl::GetMediaThreadMessageLoopProxy() for that message loop.
    
    This change should be a functional no-op according to the current
    architecture.
    
    BUG=306333
    TEST=local build, run on CrOS snow, build on desktop Linux
    
    Review URL: https://codereview.chromium.org/27019011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@229600 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/renderer_gpu_video_accelerator_factories.cc
+++ content/renderer/media/renderer_gpu_video_accelerator_factories.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/gpu/client/context_provider_command_buffer.h"
 #include "content/common/gpu/client/gpu_channel_host.h"
 #include "content/common/gpu/client/webgraphicscontext3d_command_buffer_impl.h"
+#include "content/renderer/render_thread_impl.h"
 #include "gpu/command_buffer/client/gles2_implementation.h"
 #include "third_party/skia/include/core/SkPixelRef.h"
 
@@ -21,9 +22,9 @@ namespace content {
 RendererGpuVideoAcceleratorFactories::~RendererGpuVideoAcceleratorFactories() {}
 RendererGpuVideoAcceleratorFactories::RendererGpuVideoAcceleratorFactories(
     GpuChannelHost* gpu_channel_host,
-    const scoped_refptr<base::MessageLoopProxy>& message_loop,
     const scoped_refptr<ContextProviderCommandBuffer>& context_provider)
-    : message_loop_(message_loop),
+    : message_loop_(
+          RenderThreadImpl::current()->GetMediaThreadMessageLoopProxy()),
       gpu_channel_host_(gpu_channel_host),
       context_provider_(context_provider),
       thread_safe_sender_(ChildThread::current()->thread_safe_sender()),
