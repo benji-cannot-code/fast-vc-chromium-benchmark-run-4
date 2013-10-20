e78b4404a34664daf31d3dc4710174fcb7935346
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-20 06:49:12 +0000

    Revert r229671 "aura: Allow creating content_shell without views."
    http://build.chromium.org/p/chromium.webkit/builders/WebKit%20Win%20Builder%20%28deps%29/builds/74852/steps/compile/logs/stdio
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@229683 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/browser/shell.h
+++ content/shell/browser/shell.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_forward.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/strings/string_piece.h"
-#include "build/build_config.h"
 #include "content/public/browser/web_contents_delegate.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "ipc/ipc_channel.h"
@@ -40,10 +39,6 @@ class ViewsDelegate;
 
 class GURL;
 namespace content {
-
-#if defined(USE_AURA)
-class ShellAuraPlatformData;
-#endif
 
 class BrowserContext;
 class ShellDevToolsFrontend;
@@ -265,13 +260,9 @@ class Shell : public WebContentsDelegate,
 #if defined(OS_CHROMEOS)
   static shell::MinimalShell* minimal_shell_;
 #endif
-#if defined(TOOLKIT_VIEWS)
   static views::ViewsDelegate* views_delegate_;
 
   views::Widget* window_widget_;
-#else // defined(TOOLKIT_VIEWS)
-  scoped_ptr<ShellAuraPlatformData> platform_;
-#endif // defined(TOOLKIT_VIEWS)
 #elif defined(OS_MACOSX)
   int content_width_;
   int content_height_;
