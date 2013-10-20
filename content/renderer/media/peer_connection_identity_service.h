146531b958c6b42a9b7460d3b0a445ba216a15f6
jiayl@chromium.org <jiayl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-20 08:19:34 +0000

    Enable DTLS caching for Android.
    Review URL: https://codereview.chromium.org/29253003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@229686 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/peer_connection_identity_service.h
+++ content/renderer/media/peer_connection_identity_service.h
@@ -20,7 +20,7 @@ namespace content {
 class PeerConnectionIdentityService
     : public webrtc::DTLSIdentityServiceInterface {
  public:
-  static PeerConnectionIdentityService* Create(const GURL& origin);
+  explicit PeerConnectionIdentityService(const GURL& origin);
 
   virtual ~PeerConnectionIdentityService();
 
@@ -31,8 +31,6 @@ class PeerConnectionIdentityService
       webrtc::DTLSIdentityRequestObserver* observer) OVERRIDE;
 
  private:
-  explicit PeerConnectionIdentityService(const GURL& origin);
-
   void OnIdentityReady(const std::string& certificate,
                        const std::string& private_key);
   void OnRequestFailed(int error);
