a1fdc02c46b04bac92f4ac76cfa816e354141c0d
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-21 08:53:46 +0000

    Revert r229753 "Gesture recognizer must be a singleton accross all aura::RootWindows"
    Breaks windows bots on chromium.webkit.
    http://build.chromium.org/p/chromium.webkit/builders/WebKit%20Win%20Builder/builds/44634
    
    TBR=varunjain
    
    Review URL: https://codereview.chromium.org/30813003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@229761 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/events/gestures/gesture_recognizer_impl.h
+++ ui/events/gestures/gesture_recognizer_impl.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/event_constants.h"
 #include "ui/events/events_export.h"
 #include "ui/events/gestures/gesture_recognizer.h"
-#include "ui/events/gestures/gesture_sequence.h"
 #include "ui/gfx/point.h"
 
 namespace ui {
@@ -24,15 +23,12 @@ class GestureEventHelper;
 class GestureSequence;
 class TouchEvent;
 
-class EVENTS_EXPORT GestureRecognizerImpl : public GestureRecognizer,
-                                            public GestureSequenceDelegate {
+class EVENTS_EXPORT GestureRecognizerImpl : public GestureRecognizer {
  public:
   typedef std::map<int, GestureConsumer*> TouchIdToConsumerMap;
 
-  GestureRecognizerImpl();
+  explicit GestureRecognizerImpl(GestureEventHelper* helper);
   virtual ~GestureRecognizerImpl();
-
-  std::vector<GestureEventHelper*>& helpers() { return helpers_; }
 
   // Overridden from GestureRecognizer
   virtual GestureConsumer* GetTouchLockedTarget(TouchEvent* event) OVERRIDE;
@@ -46,7 +42,7 @@ class EVENTS_EXPORT GestureRecognizerImpl : public GestureRecognizer,
                                           gfx::Point* point) OVERRIDE;
 
  protected:
-  virtual GestureSequence* CreateSequence(GestureSequenceDelegate* delegate);
+  virtual GestureSequence* CreateSequence(GestureEventHelper* helper);
   virtual GestureSequence* GetGestureSequenceForConsumer(GestureConsumer* c);
 
  private:
@@ -59,15 +55,6 @@ class EVENTS_EXPORT GestureRecognizerImpl : public GestureRecognizer,
       ui::EventResult result,
       GestureConsumer* target) OVERRIDE;
   virtual void CleanupStateForConsumer(GestureConsumer* consumer) OVERRIDE;
-  virtual void AddGestureEventHelper(GestureEventHelper* helper) OVERRIDE;
-  virtual void RemoveGestureEventHelper(GestureEventHelper* helper) OVERRIDE;
-
-  // Overridden from ui::GestureSequenceDelegate.
-  virtual void DispatchLongPressGestureEvent(GestureEvent* event) OVERRIDE;
-
-  // Convenience method to find the GestureEventHelper that can dispatch events
-  // to a specific |consumer|.
-  GestureEventHelper* FindDispatchHelperForConsumer(GestureConsumer* consumer);
 
   std::map<GestureConsumer*, GestureSequence*> consumer_sequence_;
 
@@ -81,15 +68,10 @@ class EVENTS_EXPORT GestureRecognizerImpl : public GestureRecognizer,
   // Touches cancelled by touch capture are routed to the
   // gesture_consumer_ignorer_.
   scoped_ptr<GestureConsumer> gesture_consumer_ignorer_;
-
-  std::vector<GestureEventHelper*> helpers_;
+  GestureEventHelper* helper_;
 
   DISALLOW_COPY_AND_ASSIGN(GestureRecognizerImpl);
 };
-
-// Provided only for testing:
-EVENTS_EXPORT void SetGestureRecognizerForTesting(
-    GestureRecognizer* gesture_recognizer);
 
 }  // namespace ui
 
