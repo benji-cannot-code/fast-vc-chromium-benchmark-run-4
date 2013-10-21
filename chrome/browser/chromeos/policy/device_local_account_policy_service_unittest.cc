555fc61f71e901c8b1d41e5cf192a16029182952
bartfab@chromium.org <bartfab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-21 23:12:20 +0000

    Fix leak introduced by CL 27548004
    This CL fixes by spinning a task runner to flush out any remaining
    tasks.
    
    BUG=287802
    TEST=Manual
    R=jyasskin@chromium.org
    
    Review URL: https://codereview.chromium.org/33793003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@229952 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/policy/device_local_account_policy_service_unittest.cc
+++ chrome/browser/chromeos/policy/device_local_account_policy_service_unittest.cc
@@ -75,6 +75,7 @@ class DeviceLocalAccountPolicyServiceTestBase
   DeviceLocalAccountPolicyServiceTestBase();
 
   virtual void SetUp() OVERRIDE;
+  virtual void TearDown() OVERRIDE;
 
   void CreatePolicyService();
 
@@ -165,6 +166,11 @@ void DeviceLocalAccountPolicyServiceTestBase::SetUp() {
       true);
   device_local_account_policy_.policy_data().set_policy_type(
       dm_protocol::kChromePublicAccountPolicyType);
+}
+
+void DeviceLocalAccountPolicyServiceTestBase::TearDown() {
+  extension_cache_task_runner_->RunUntilIdle();
+  chromeos::DeviceSettingsTestBase::TearDown();
 }
 
 void DeviceLocalAccountPolicyServiceTestBase::CreatePolicyService() {
