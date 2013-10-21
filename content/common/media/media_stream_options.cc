c3fe6f707dc2d3735fdbabcb3e3829686f155a30
perkj@chromium.org <perkj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-21 11:41:17 +0000

    This cl is the first cl in order to be able to support MediaStreamTrack::Stop.
    It adds a new method StopStreamDevice to MediaStreamDispatcherHost and MediaStreamManager.
    
    It also change the behavior in that sense that only one session_id (used for using a device from the render process) is generated for a specific device_id, process_id and render_view_id combination. It means that multiple gum requests for the same device will have the same session id.
    
    It also means that that when StopStreamDevice is called - it will stop the (video) device from being used - even if it is being used in several MediaStreams.
    
    TEST= Open https://src.chromium.org/chrome/trunk/src/chrome/test/data/webrtc/manual/peerconnection-multi.html. Open the Java script console and type gLocalStream1.stop(). Notice that both video tags stop displaying video from the camera. Before this change - only the first video stopped.
    
    BUG=293304
    R=tsepez@chromium.org, wjia@chromium.org, xians@chromium.org
    
    Review URL: https://codereview.chromium.org/24070004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@229780 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/media/media_stream_options.cc
+++ content/common/media/media_stream_options.cc
@@ -34,15 +34,12 @@ StreamOptions::StreamOptions(MediaStreamType audio_type,
 const int StreamDeviceInfo::kNoId = -1;
 
 StreamDeviceInfo::StreamDeviceInfo()
-    : in_use(false),
-      session_id(kNoId) {}
+    : session_id(kNoId) {}
 
 StreamDeviceInfo::StreamDeviceInfo(MediaStreamType service_param,
                                    const std::string& name_param,
-                                   const std::string& device_param,
-                                   bool opened)
+                                   const std::string& device_param)
     : device(service_param, device_param, name_param),
-      in_use(opened),
       session_id(kNoId) {
 }
 
@@ -51,11 +48,9 @@ StreamDeviceInfo::StreamDeviceInfo(MediaStreamType service_param,
                                    const std::string& device_param,
                                    int sample_rate,
                                    int channel_layout,
-                                   int frames_per_buffer,
-                                   bool opened)
+                                   int frames_per_buffer)
     : device(service_param, device_param, name_param, sample_rate,
              channel_layout, frames_per_buffer),
-      in_use(opened),
       session_id(kNoId) {
 }
 
@@ -71,7 +66,6 @@ bool StreamDeviceInfo::IsEqual(const StreamDeviceInfo& first,
       first.device.id == second.device.id &&
       input_first.sample_rate == input_second.sample_rate &&
       input_first.channel_layout == input_second.channel_layout &&
-      first.in_use == second.in_use &&
       first.session_id == second.session_id;
 }
 
