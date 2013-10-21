d2196f0dd8e27e68c0bbf4f8a0b9a5fcaf6ddbe0
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-21 07:55:41 +0000

    [FileAPI] Fix dump tool build failure
    Review URL: https://codereview.chromium.org/31383003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@229754 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/browser/fileapi/dump_file_system.cc
+++ webkit/browser/fileapi/dump_file_system.cc
@@ -49,7 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 bool g_opt_long;
-fileapi::FileSystemType g_opt_fs_type = fileapi::kFileSystemTypePersistent;
+const char* g_opt_fs_type = "p";
 
 void ShowMessageAndExit(const std::string& msg) {
   fprintf(stderr, "%s\n", msg.c_str());
@@ -68,8 +68,7 @@ namespace fileapi {
 
 static void DumpDirectoryTree(const std::string& origin_name,
                               base::FilePath origin_dir) {
-  origin_dir = origin_dir.Append(
-      ObfuscatedFileUtil::GetDirectoryNameForType(g_opt_fs_type));
+  origin_dir = origin_dir.Append(g_opt_fs_type);
 
   printf("=== ORIGIN %s %s ===\n",
          origin_name.c_str(), FilePathToString(origin_dir).c_str());
@@ -174,11 +173,11 @@ int main(int argc, char* argv[]) {
       argc--;
       argv++;
     } else if (std::string(argv[1]) == "-t") {
-      g_opt_fs_type = fileapi::kFileSystemTypeTemporary;
+      g_opt_fs_type = "t";
       argc--;
       argv++;
     } else if (std::string(argv[1]) == "-s") {
-      g_opt_fs_type = fileapi::kFileSystemTypeSyncable;
+      g_opt_fs_type = "s";
       argc--;
       argv++;
     } else {
