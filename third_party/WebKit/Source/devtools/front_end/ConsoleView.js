11cc7deabc6f29e9836a6399860ce9a64f6cc940
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-21 15:49:52 +0000

    DevTools: Layout console view using flex.
    R=pfeldman@chromium.org, pfeldman
    
    Review URL: https://codereview.chromium.org/32753003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160092 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ConsoleView.js
+++ third_party/WebKit/Source/devtools/front_end/ConsoleView.js
@@ -37,8 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 WebInspector.ConsoleView = function(hideContextSelector)
 {
     WebInspector.View.call(this);
-
-    this.element.id = "console-view";
+    this.element.classList.add("fill", "vbox");
     this._visibleMessagesIndices = [];
     this._urlToMessageCount = {};
 
