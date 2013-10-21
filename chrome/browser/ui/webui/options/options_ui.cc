395ab040ade175a7e30bcecc7de0cf65cb3a4e8e
bartfab@chromium.org <bartfab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-21 12:00:17 +0000

    Settings page: Do not expect a reload when navigating between sub-pages
    OptionsUI informs the individual handlers whenever the setting page starts
    and finishes loading. This was not working correctly when the forward/back
    buttons were used to navigate between sub-pages: OptionsUI would inform
    the handlers that a load had started but no load is actually needed for
    this kind of navigation so that no end of load signal would be generated.
    
    This CL makes OptionsUI wait for the correct trigger so that it signals a
    start of load only when a load actually begins.
    
    BUG=301436
    TEST=New browser test
    
    Review URL: https://codereview.chromium.org/27042002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@229783 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options/options_ui.cc
+++ chrome/browser/ui/webui/options/options_ui.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <vector>
 
+#include "base/basictypes.h"
 #include "base/callback.h"
 #include "base/command_line.h"
 #include "base/memory/ref_counted_memory.h"
@@ -65,6 +66,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/webui/jstemplate_builder.h"
 #include "ui/base/webui/web_ui_util.h"
+#include "url/gurl.h"
 
 #if defined(OS_CHROMEOS)
 #include "chrome/browser/chromeos/system/pointer_device_observer.h"
@@ -236,6 +238,7 @@ void OptionsPageUIHandler::RegisterTitle(DictionaryValue* localized_strings,
 
 OptionsUI::OptionsUI(content::WebUI* web_ui)
     : WebUIController(web_ui),
+      WebContentsObserver(web_ui->GetWebContents()),
       initialized_handlers_(false) {
   DictionaryValue* localized_strings = new DictionaryValue();
   localized_strings->Set(OptionsPageUIHandler::kSettingsAppKey,
@@ -377,6 +380,21 @@ base::RefCountedMemory* OptionsUI::GetFaviconResourceBytes(
       LoadDataResourceBytesForScale(IDR_SETTINGS_FAVICON, scale_factor);
 }
 
+void OptionsUI::DidStartProvisionalLoadForFrame(
+    int64 frame_id,
+    int64 parent_frame_id,
+    bool is_main_frame,
+    const GURL& validated_url,
+    bool is_error_page,
+    bool is_iframe_srcdoc,
+    content::RenderViewHost* render_view_host) {
+  if (render_view_host == web_ui()->GetWebContents()->GetRenderViewHost() &&
+      validated_url.host() == chrome::kChromeUISettingsFrameHost) {
+    for (size_t i = 0; i < handlers_.size(); ++i)
+      handlers_[i]->PageLoadStarted();
+  }
+}
+
 void OptionsUI::InitializeHandlers() {
   Profile* profile = Profile::FromWebUI(web_ui());
   DCHECK(!profile->IsOffTheRecord() || profile->IsGuestSession());
@@ -404,18 +422,6 @@ void OptionsUI::InitializeHandlers() {
 
   web_ui()->CallJavascriptFunction(
       "BrowserOptions.notifyInitializationComplete");
-}
-
-void OptionsUI::RenderViewCreated(content::RenderViewHost* render_view_host) {
-  content::WebUIController::RenderViewCreated(render_view_host);
-  for (size_t i = 0; i < handlers_.size(); ++i)
-    handlers_[i]->PageLoadStarted();
-}
-
-void OptionsUI::RenderViewReused(content::RenderViewHost* render_view_host) {
-  content::WebUIController::RenderViewReused(render_view_host);
-  for (size_t i = 0; i < handlers_.size(); ++i)
-    handlers_[i]->PageLoadStarted();
 }
 
 void OptionsUI::AddOptionsPageUIHandler(DictionaryValue* localized_strings,
