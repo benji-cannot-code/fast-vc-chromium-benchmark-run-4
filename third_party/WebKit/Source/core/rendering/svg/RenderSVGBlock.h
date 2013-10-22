4f6a7f2cdd4745fca39b1b63749dc47a43da9dff
pdr@chromium.org <pdr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-22 22:30:36 +0000

    Simplify SVG's styleDidChange
    This is a merge of wkbug.com/123108 by Andreas Kling <akling@apple.com>.
    
    SVG's styleWillChange code can me moved into styleDidChange which both
    simplifies this code and removes a few virtuals. This patch also merges
    RenderSVGBlock::styleWillChange into RenderSVGBlock::styleDidChange
    which was not in the WebKit patch.
    
    No new tests, covered by existing tests.
    
    Review URL: https://codereview.chromium.org/32343009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160259 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/svg/RenderSVGBlock.h
+++ third_party/WebKit/Source/core/rendering/svg/RenderSVGBlock.h
@@ -46,7 +46,6 @@ private:
 
     virtual void absoluteRects(Vector<IntRect>&, const LayoutPoint& accumulatedOffset) const OVERRIDE FINAL;
 
-    virtual void styleWillChange(StyleDifference, const RenderStyle* newStyle) OVERRIDE FINAL;
     virtual void styleDidChange(StyleDifference, const RenderStyle* oldStyle) OVERRIDE FINAL;
 };
 
