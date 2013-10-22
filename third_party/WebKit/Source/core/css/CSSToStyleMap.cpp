16c7a7a735ce294c2bbf78074d4e933aec3fbf13
davve@opera.com <davve@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-22 19:24:25 +0000

    Unstick background position origins
    In the FillLayer class, reset the {x,y} origins each time {x,y}
    position is set and require the caller to later call
    setBackground{X,Y}Origin to override the default value. This fits
    better with the way 'initial' values are set from the StyleBuilder and
    avoids the problem of origins lingering in the FillLayer when setting
    the 2-value versions of background-position.
    
    Furthermore, split the flag m_backgroundOriginSet into one for each
    property so that something like 'background-position-x:initial' won't
    reset the position origin for both properties.
    
    Also, remove code for handling 'initial' for {x,y} position, 'initial'
    is already handled in the StyleBuilder further up the call chain.
    
    BUG=304721
    
    Review URL: https://codereview.chromium.org/35303002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160243 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/CSSToStyleMap.cpp
+++ third_party/WebKit/Source/core/css/CSSToStyleMap.cpp
@@ -228,11 +228,6 @@ void CSSToStyleMap::mapFillSize(CSSPropertyID, FillLayer* layer, CSSValue* value
 
 void CSSToStyleMap::mapFillXPosition(CSSPropertyID propertyID, FillLayer* layer, CSSValue* value) const
 {
-    if (value->isInitialValue()) {
-        layer->setXPosition(FillLayer::initialFillXPosition(layer->type()));
-        return;
-    }
-
     if (!value->isPrimitiveValue())
         return;
 
@@ -264,11 +259,6 @@ void CSSToStyleMap::mapFillXPosition(CSSPropertyID propertyID, FillLayer* layer,
 
 void CSSToStyleMap::mapFillYPosition(CSSPropertyID propertyID, FillLayer* layer, CSSValue* value) const
 {
-    if (value->isInitialValue()) {
-        layer->setYPosition(FillLayer::initialFillYPosition(layer->type()));
-        return;
-    }
-
     if (!value->isPrimitiveValue())
         return;
 
