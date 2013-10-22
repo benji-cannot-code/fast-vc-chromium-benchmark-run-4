22f57a472cca810189b00484ab7602afc803e4b3
hartmanng@chromium.org <hartmanng@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-22 00:28:46 +0000

    Fix flaky comp-scrolling layout tests.
    BUG=307594,307314
    
    Review URL: https://codereview.chromium.org/26540005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160109 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/testing/Internals.h
+++ third_party/WebKit/Source/core/testing/Internals.h
@@ -221,6 +221,7 @@ public:
 
     bool scrollsWithRespectTo(Element*, Element*, ExceptionState&);
     bool isUnclippedDescendant(Element*, ExceptionState&);
+    bool needsCompositedScrolling(Element*, ExceptionState&);
 
     void setNeedsCompositedScrolling(Element*, unsigned value, ExceptionState&);
 
