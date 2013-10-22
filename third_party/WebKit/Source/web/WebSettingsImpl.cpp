42d04636cda6a8f8e070e5515524eed4927ddd2c
sblom@microsoft.com <sblom@microsoft.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-22 08:43:36 +0000

    Add navigator.maxTouchPoints
    Intent to implement-and-ship: https://groups.google.com/a/chromium.org/forum/#!searchin/blink-dev/maxTouchPoints/blink-dev/ayzxdztUlOQ/rd-z_Jo3ocIJ
    
    navigator.maxTouchPoints is defined in the W3C Pointer Events
    standard draft:
    http://www.w3.org/TR/pointerevents/#widl-Navigator-maxTouchPoints
    
    BUG=248918
    
    Review URL: https://codereview.chromium.org/27750002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160153 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebSettingsImpl.cpp
+++ third_party/WebKit/Source/web/WebSettingsImpl.cpp
@@ -338,6 +338,11 @@ void WebSettingsImpl::setMainFrameClipsContent(bool enabled)
     m_settings->setMainFrameClipsContent(enabled);
 }
 
+void WebSettingsImpl::setMaxTouchPoints(int maxTouchPoints)
+{
+    m_settings->setMaxTouchPoints(maxTouchPoints);
+}
+
 void WebSettingsImpl::setEditableLinkBehaviorNeverLive()
 {
     // FIXME: If you ever need more behaviors than this, then we should probably
