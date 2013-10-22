314c6367c9a2f3e4478a36d3d522573df72a4932
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-22 22:43:58 +0000

    Use BaseShellDialog for print dialog on Windows
    This puts the print dialog on a background thread which is necessary so other
    top level windows can keep painting as Aura does the compositor swaps on the
    UI thread.
    
    R=sky@chromium.org,vitalybuka@chromium.org
    BUG=180997
    
    Review URL: https://codereview.chromium.org/27441003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@230235 0039d316-1c4b-4281-b951-d872f2087c98

--- printing/printing_context_win.h
+++ printing/printing_context_win.h
@@ -15,10 +15,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "printing/printing_context.h"
 #include "ui/gfx/native_widget_types.h"
+#include "ui/shell_dialogs/print_settings_dialog_win.h"
 
 namespace printing {
 
-class PRINTING_EXPORT PrintingContextWin : public PrintingContext {
+class PRINTING_EXPORT PrintingContextWin
+    : public PrintingContext,
+      public ui::PrintSettingsDialogWin::Observer {
  public:
   explicit PrintingContextWin(const std::string& app_locale);
   ~PrintingContextWin();
@@ -41,6 +44,10 @@ class PRINTING_EXPORT PrintingContextWin : public PrintingContext {
   virtual void Cancel() OVERRIDE;
   virtual void ReleaseContext() OVERRIDE;
   virtual gfx::NativeDrawingContext context() const OVERRIDE;
+
+  // PrintSettingsDialogWin::Observer implementation:
+  virtual void PrintSettingsConfirmed(PRINTDLGEX* dialog_options) OVERRIDE;
+  virtual void PrintSettingsCancelled(PRINTDLGEX* dialog_options) OVERRIDE;
 
 #if defined(UNIT_TEST) || defined(PRINTING_IMPLEMENTATION)
   // Sets a fake PrintDlgEx function pointer in tests.
@@ -88,6 +95,12 @@ class PRINTING_EXPORT PrintingContextWin : public PrintingContext {
   // Function pointer that defaults to PrintDlgEx. It can be changed using
   // SetPrintDialog() in tests.
   HRESULT (__stdcall *print_dialog_func_)(LPPRINTDLGEX);
+
+  // Where to notify when the dialog is closed.
+  PrintSettingsCallback callback_;
+
+  // Wrapper around native print dialog that runs it on a background thread.
+  scoped_refptr<ui::PrintSettingsDialogWin> print_settings_dialog_;
 
   DISALLOW_COPY_AND_ASSIGN(PrintingContextWin);
 };
