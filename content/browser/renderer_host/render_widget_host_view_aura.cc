59bbabb83f4c3f17b1cb191c5985f888df02d39d
hubbe@chromium.org <hubbe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-22 18:29:39 +0000

    re-enable mrt The problem was a bug in mesa, which has now been fixed in the chrome tree. crbug.com/308715 tracks the progress of upstreaming the mesa bug.
    BUG=260048
    
    Review URL: https://codereview.chromium.org/28553007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@230166 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_view_aura.cc
+++ content/browser/renderer_host/render_widget_host_view_aura.cc
@@ -2079,7 +2079,7 @@ void RenderWidgetHostViewAura::CopyFromCompositingSurfaceHasResultForVideo(
                                              video_frame->coded_size(),
                                              region_in_frame,
                                              true,
-                                             false));
+                                             true));
     yuv_readback_pipeline = rwhva->yuv_readback_pipeline_.get();
   }
 
