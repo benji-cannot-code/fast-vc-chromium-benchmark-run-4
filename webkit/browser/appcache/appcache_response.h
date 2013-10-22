fd507ddae47d29bc5f226403046f803473643596
michaeln@google.com <michaeln@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-22 23:08:20 +0000

    Changes to reinitialize the appcache system if/when DiskCache initialization fails.
    Older versions would attempt to delete the existing data on disk, and disable the appcache system until browser restart, and then have a clean slate. With these changes, we no longer wait for a browser restart get going again. Also fixes a bug with the older behavior, it was racy and could *fail* to delete the old data.
    
    BUG=258191,95101
    R=kinuko@chromium.org, rvargas@chromium.org
    
    Review URL: https://codereview.chromium.org/25955002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@230246 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/browser/appcache/appcache_response.h
+++ webkit/browser/appcache/appcache_response.h
@@ -22,7 +22,7 @@ class IOBuffer;
 
 namespace appcache {
 
-class AppCacheService;
+class AppCacheStorage;
 
 static const int kUnkownResponseDataSize = -1;
 
@@ -32,7 +32,7 @@ class WEBKIT_STORAGE_BROWSER_EXPORT AppCacheResponseInfo
     : public base::RefCounted<AppCacheResponseInfo> {
  public:
   // AppCacheResponseInfo takes ownership of the http_info.
-  AppCacheResponseInfo(AppCacheService* service, const GURL& manifest_url,
+  AppCacheResponseInfo(AppCacheStorage* storage, const GURL& manifest_url,
                        int64 response_id, net::HttpResponseInfo* http_info,
                        int64 response_data_size);
 
@@ -51,7 +51,7 @@ class WEBKIT_STORAGE_BROWSER_EXPORT AppCacheResponseInfo
   const int64 response_id_;
   const scoped_ptr<net::HttpResponseInfo> http_response_info_;
   const int64 response_data_size_;
-  const AppCacheService* service_;
+  AppCacheStorage* storage_;
 };
 
 // A refcounted wrapper for HttpResponseInfo so we can apply the
