a0fe687585337094c00bee479e5221c31aa33410
ellyjones@chromium.org <ellyjones@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-22 21:18:25 +0000

    android: Use new proxy from PROXY_CHANGE intent
    When we get a PROXY_CHANGE intent, the intent comes with the new proxy value;
    use this value instead of ignoring it and asking the system property store for
    the new proxy. This fixes a race condition where the system property store may
    not have the new proxy by the time we receive the intent.
    
    BUG=none yet
    TEST=trybot
    
    Review URL: https://codereview.chromium.org/26763005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@230208 0039d316-1c4b-4281-b951-d872f2087c98

--- net/proxy/proxy_config_service_android.cc
+++ net/proxy/proxy_config_service_android.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequenced_task_runner.h"
 #include "base/strings/string_tokenizer.h"
 #include "base/strings/string_util.h"
+#include "base/strings/stringprintf.h"
 #include "jni/ProxyChangeListener_jni.h"
 #include "net/base/host_port_pair.h"
 #include "net/proxy/proxy_config.h"
@@ -160,6 +161,16 @@ std::string GetJavaProperty(const std::string& property) {
       std::string() : ConvertJavaStringToUTF8(env, result.obj());
 }
 
+void CreateStaticProxyConfig(const std::string& host, int port,
+                             ProxyConfig* config) {
+  if (port != 0) {
+    std::string rules = base::StringPrintf("%s:%d", host.c_str(), port);
+    config->proxy_rules().ParseFromString(rules);
+  } else {
+    *config = ProxyConfig::CreateDirect();
+  }
+}
+
 }  // namespace
 
 class ProxyConfigServiceAndroid::Delegate
@@ -238,6 +249,17 @@ class ProxyConfigServiceAndroid::Delegate
             &Delegate::SetNewConfigOnNetworkThread, this, proxy_config));
   }
 
+  // Called on the JNI thread.
+  void ProxySettingsChangedTo(const std::string& host, int port) {
+    DCHECK(OnJNIThread());
+    ProxyConfig proxy_config;
+    CreateStaticProxyConfig(host, port, &proxy_config);
+    network_task_runner_->PostTask(
+        FROM_HERE,
+        base::Bind(
+            &Delegate::SetNewConfigOnNetworkThread, this, proxy_config));
+  }
+
  private:
   friend class base::RefCountedThreadSafe<Delegate>;
 
@@ -246,7 +268,13 @@ class ProxyConfigServiceAndroid::Delegate
     explicit JNIDelegateImpl(Delegate* delegate) : delegate_(delegate) {}
 
     // ProxyConfigServiceAndroid::JNIDelegate overrides.
-    virtual void ProxySettingsChanged(JNIEnv*, jobject) OVERRIDE {
+    virtual void ProxySettingsChangedTo(JNIEnv* env, jobject jself,
+                                      jstring jhost, jint jport) OVERRIDE {
+      std::string host = ConvertJavaStringToUTF8(env, jhost);
+      delegate_->ProxySettingsChangedTo(host, jport);
+    }
+
+    virtual void ProxySettingsChanged(JNIEnv* env, jobject self) OVERRIDE {
       delegate_->ProxySettingsChanged();
     }
 
