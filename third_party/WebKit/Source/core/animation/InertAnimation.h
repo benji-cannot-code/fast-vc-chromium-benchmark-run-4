a32735d2d3f7ba3d202e0bb2c080f9cb88cded1b
shans@chromium.org <shans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-22 08:41:47 +0000

    Remove boolean flag from updateChildrenAndEffects
    BUG=258896
    
    Review URL: https://codereview.chromium.org/32413004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160152 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/InertAnimation.h
+++ third_party/WebKit/Source/core/animation/InertAnimation.h
@@ -46,7 +46,7 @@ public:
     AnimationEffect* effect() const { return m_effect.get(); }
 
 protected:
-    virtual void updateChildrenAndEffects(bool) const OVERRIDE { };
+    virtual void updateChildrenAndEffects() const OVERRIDE { };
     virtual void willDetach() OVERRIDE { };
     virtual double calculateTimeToEffectChange(double inheritedTime, double activeTime, Phase) const OVERRIDE FINAL;
 
