3163dca6d2e604d5f4742692dadd3c0ab6bb7eab
robertshield@chromium.org <robertshield@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-22 04:15:51 +0000

    Use STLValueDeleter for maps in ProfileSyncServicePreferenceTest.
    These tests have maps with pointer values, prefer STLValueDeleter next
    to the definition of the map over STLDeleteValues at the end of the
    test to make it slightly harder to lop off the cleanup code.
    
    BUG=299290
    TEST=unit_test.exe
    
    Review URL: https://codereview.chromium.org/32363005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@230045 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/profile_sync_service_preference_unittest.cc
+++ chrome/browser/sync/profile_sync_service_preference_unittest.cc
@@ -366,6 +366,7 @@ TEST_F(ProfileSyncServicePreferenceTest, ModelAssociationCloudHasData) {
   }
 
   PreferenceValues cloud_data;
+  STLValueDeleter<PreferenceValues> cloud_data_deleter(&cloud_data);
   cloud_data[prefs::kHomePage] = Value::CreateStringValue(example_url1_);
   ListValue* urls_to_restore = new ListValue;
   urls_to_restore->Append(Value::CreateStringValue(example_url1_));
@@ -401,7 +402,6 @@ TEST_F(ProfileSyncServicePreferenceTest, ModelAssociationCloudHasData) {
   EXPECT_EQ(non_default_charset_value_, string_value);
   EXPECT_EQ(non_default_charset_value_,
             prefs_->GetString(prefs::kDefaultCharset));
-  STLDeleteValues(&cloud_data);
 }
 
 TEST_F(ProfileSyncServicePreferenceTest,
@@ -419,6 +419,7 @@ TEST_F(ProfileSyncServicePreferenceTest,
   }
 
   PreferenceValues cloud_data;
+  STLValueDeleter<PreferenceValues> cloud_data_deleter(&cloud_data);
   cloud_data[prefs::kHomePage] = Value::CreateStringValue(example_url1_);
   ListValue* urls_to_restore = new ListValue;
   urls_to_restore->Append(Value::CreateStringValue(example_url1_));
@@ -454,7 +455,6 @@ TEST_F(ProfileSyncServicePreferenceTest,
   ASSERT_TRUE(value.get());
   EXPECT_TRUE(GetPreferenceValue(prefs::kURLsToRestoreOnStartupOld).
               Equals(expected_urls.get()));
-  STLDeleteValues(&cloud_data);
 }
 
 TEST_F(ProfileSyncServicePreferenceTest,
@@ -472,6 +472,7 @@ TEST_F(ProfileSyncServicePreferenceTest,
   }
 
   PreferenceValues cloud_data;
+  STLValueDeleter<PreferenceValues> cloud_data_deleter(&cloud_data);
   cloud_data[prefs::kHomePage] = Value::CreateStringValue(example_url1_);
   ListValue* urls_to_restore = new ListValue;
   urls_to_restore->Append(Value::CreateStringValue(example_url1_));
@@ -509,7 +510,6 @@ TEST_F(ProfileSyncServicePreferenceTest,
   ASSERT_FALSE(value.get());
   EXPECT_TRUE(GetPreferenceValue(prefs::kURLsToRestoreOnStartupOld).
               Equals(expected_urls.get()));
-  STLDeleteValues(&cloud_data);
 }
 
 TEST_F(ProfileSyncServicePreferenceTest,
@@ -531,6 +531,7 @@ TEST_F(ProfileSyncServicePreferenceTest,
   }
 
   PreferenceValues cloud_data;
+  STLValueDeleter<PreferenceValues> cloud_data_deleter(&cloud_data);
   cloud_data[prefs::kHomePage] = Value::CreateStringValue(example_url1_);
 
   AddPreferenceEntriesHelper helper(this, cloud_data);
@@ -560,7 +561,6 @@ TEST_F(ProfileSyncServicePreferenceTest,
   ASSERT_FALSE(value.get());
   EXPECT_FALSE(GetPreferenceValue(prefs::kURLsToRestoreOnStartupOld).
                Equals(expected_urls.get()));
-  STLDeleteValues(&cloud_data);
 }
 
 TEST_F(ProfileSyncServicePreferenceTest, FailModelAssociation) {
@@ -713,10 +713,11 @@ TEST_F(ProfileSyncServicePreferenceTest, ManagedListPreferences) {
 
   // Set a cloud version.
   PreferenceValues cloud_data;
-  scoped_ptr<ListValue> urls_to_restore(new ListValue);
+  STLValueDeleter<PreferenceValues> cloud_data_deleter(&cloud_data);
+  ListValue* urls_to_restore = new ListValue;
   urls_to_restore->Append(Value::CreateStringValue(example_url1_));
   urls_to_restore->Append(Value::CreateStringValue(example_url2_));
-  cloud_data[prefs::kURLsToRestoreOnStartup] = urls_to_restore.get();
+  cloud_data[prefs::kURLsToRestoreOnStartup] = urls_to_restore;
 
   // Start sync and verify the synced value didn't get merged.
   AddPreferenceEntriesHelper helper(this, cloud_data);
