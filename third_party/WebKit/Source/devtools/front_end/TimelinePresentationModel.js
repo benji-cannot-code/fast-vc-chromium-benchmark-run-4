35636e9bb172e9471ebe2b2d4329326de0324161
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-22 06:17:10 +0000

    Revert r160130 "DevTools: Show displayName in frame call stack and popovers for functions."
    Breaks Win layout tests: http://test-results.appspot.com/dashboards/flakiness_dashboard.html#group=%40ToT%20Blink&showExpectations=true&tests=inspector%2Fdebugger%2Ffunction-display-name-call-stack.html
    
    TBR=aandrey
    BUG=
    
    Review URL: https://codereview.chromium.org/34463002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160139 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/TimelinePresentationModel.js
+++ third_party/WebKit/Source/devtools/front_end/TimelinePresentationModel.js
@@ -976,7 +976,7 @@ WebInspector.TimelinePresentationModel.Record.prototype = {
     },
 
     /**
-     * @return {?Array.<ConsoleAgent.CallFrame>}
+     * @return {Array.<DebuggerAgent.CallFrame>?}
      */
     get stackTrace()
     {
@@ -1327,9 +1327,6 @@ WebInspector.TimelinePresentationModel.Record.prototype = {
         return this._linkifier.linkifyLocation(url, lineNumber - 1, columnNumber, "timeline-details");
     },
 
-    /**
-     * @param {ConsoleAgent.CallFrame} callFrame
-     */
     _linkifyCallFrame: function(callFrame)
     {
         return this._linkifyLocation(callFrame.url, callFrame.lineNumber, callFrame.columnNumber);
