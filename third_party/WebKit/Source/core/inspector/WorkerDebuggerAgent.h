394d985b2adb8d7e82d61ab9b0fa6dae5eef3b80
ch.dumez@samsung.com <ch.dumez@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-22 13:20:46 +0000

    Move even more data from .data to .rodata section
    Move more data from .data to .rodata section so that they can be shared
    between different library instances on mobile / android.
    
    In particular, the following data was moved:
    - CSSGrammar's star variables
    - WorkerDebuggerAgent::debuggerTaskMode string
    - Ranges arrays in Font class
    
    This also gets rid of 1 relocation according to relinfo.
    
    R=abarth
    BUG=249746
    
    Review URL: https://codereview.chromium.org/28333004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160203 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/WorkerDebuggerAgent.h
+++ third_party/WebKit/Source/core/inspector/WorkerDebuggerAgent.h
@@ -47,7 +47,7 @@ public:
     static PassOwnPtr<WorkerDebuggerAgent> create(InstrumentingAgents*, InspectorCompositeState*, WorkerScriptDebugServer*, WorkerGlobalScope*, InjectedScriptManager*);
     virtual ~WorkerDebuggerAgent();
 
-    static const char* debuggerTaskMode;
+    static const char debuggerTaskMode[];
     static void interruptAndDispatchInspectorCommands(WorkerThread*);
 
 private:
