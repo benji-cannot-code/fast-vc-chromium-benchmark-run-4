f6de2562eb3e7eed03f6b841c66b9d234ab51e41
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-22 02:30:31 +0000

    Add missing #includes of <algorithm> in ppapi for std::min/max
    Required when building on VS2013.
    
    TBR=dmichael@chromium.org
    BUG=288948
    
    Review URL: https://codereview.chromium.org/32343005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@230018 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/examples/2d/scroll.cc
+++ ppapi/examples/2d/scroll.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <math.h>
 
+#include <algorithm>
+
 #include "ppapi/cpp/graphics_2d.h"
 #include "ppapi/cpp/image_data.h"
 #include "ppapi/cpp/instance.h"
