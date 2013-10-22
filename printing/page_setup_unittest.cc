677fe222e739e041a88a3dd21e254f0a58b51665
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-22 03:02:56 +0000

    Add missing #include of algorithm for std::max
    Needed for compilation on VS2013.
    
    TBR=vitalybuka@chromium.org
    BUG=288948
    
    Review URL: https://codereview.chromium.org/33923004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@230024 0039d316-1c4b-4281-b951-d872f2087c98

--- printing/page_setup_unittest.cc
+++ printing/page_setup_unittest.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdlib.h>
 #include <time.h>
 
+#include <algorithm>
+
 #include "testing/gtest/include/gtest/gtest.h"
 
 TEST(PageSetupTest, Random) {
