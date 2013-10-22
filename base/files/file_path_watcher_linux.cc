5dac7ea271833bd74b102a94be3db64ab7f9de5e
wangxianzhu@chromium.org <wangxianzhu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-22 02:16:58 +0000

    TraceLog::SetCurrentThreadBlocksMessageLoop()
    - When some thread doesn't respond the flush request in time, output
    the thread name, and suggest calling SetCurrentThreadBlocksMessageLoop
    from the thread.
    
    - For a thread which calls SetCurrentThreadBlocksMessageLoop, trace
    events from the thread will be added into the main buffer directly,
    without using the thread local buffer.
    
    TEST=TraceEventTestFixture.CurrentThreadBlocksMessageLoop
    BUG=trace-viewer:486
    
    Review URL: https://codereview.chromium.org/29803002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@230012 0039d316-1c4b-4281-b951-d872f2087c98

--- base/files/file_path_watcher_linux.cc
+++ base/files/file_path_watcher_linux.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/containers/hash_tables.h"
+#include "base/debug/trace_event.h"
 #include "base/file_util.h"
 #include "base/files/file_path.h"
 #include "base/lazy_instance.h"
@@ -158,6 +159,8 @@ void InotifyReaderCallback(InotifyReader* reader, int inotify_fd,
   CHECK_GT(FD_SETSIZE, inotify_fd);
   CHECK_LE(0, shutdown_fd);
   CHECK_GT(FD_SETSIZE, shutdown_fd);
+
+  base::debug::TraceLog::GetInstance()->SetCurrentThreadBlocksMessageLoop();
 
   while (true) {
     fd_set rfds;
