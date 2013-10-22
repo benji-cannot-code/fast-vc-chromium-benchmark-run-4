e9f754b0ac75ab00b05ee84df91898aae0ff8941
bokan@chromium.org <bokan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-22 17:25:34 +0000

    Converted link highlighting tests into reftests.
    BUG=
    
    Review URL: https://codereview.chromium.org/32703002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160226 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/compositing/gestures/resources/link-highlight-helper.js
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function createCompositedHighlight(node)
+{
+  return _createHighlight(node, "highlightDiv composited");
+}
+
+function createHighlight(node)
+{
+  return _createHighlight(node, "highlightDiv");
+}
+
+function _createHighlight(node, classes) {
+  var div = document.createElement('div');
+  div.setAttribute('id', 'highlight');
+  div.setAttribute('class', classes);
+  document.body.appendChild(div);
+
+  var offset = _getAbsoluteOffset(node);
+  var clientRect = node.getBoundingClientRect();
+  div.style.top = offset.top + "px";
+  div.style.left = offset.left + "px";
+  div.style.width = node.offsetWidth + "px";
+  div.style.height = node.offsetHeight + "px";
+
+  return div;
+}
+
+function _getAbsoluteOffset( elem )
+{
+    var offsetLeft = 0;
+    var offsetTop = 0;
+    do {
+      if ( !isNaN( elem.offsetLeft ) )
+          offsetLeft += elem.offsetLeft - elem.scrollLeft;
+
+      if ( !isNaN( elem.offsetTop ) )
+          offsetTop += elem.offsetTop - elem.scrollTop;
+
+    } while( elem = elem.offsetParent );
+    return { top : offsetTop,
+             left : offsetLeft };
+}
