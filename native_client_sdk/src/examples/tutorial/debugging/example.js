c661838901923780a4a86ab08b7b9bfa304f09d6
binji@chromium.org <binji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-22 18:48:15 +0000

    [NaCl SDK] Re-order the calls to createNaClModule and attachDefaultListeners.
    If attachDefaultListeners is called after createNaClModule, then there is a
    potential race condition -- if the module loads quickly enough, the listeners
    will not yet be attached when the 'load' message is dispatched.
    
    BUG=245910
    R=sbc@chromium.org
    
    Review URL: https://codereview.chromium.org/33423005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@230170 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/examples/tutorial/debugging/example.js
+++ native_client_sdk/src/examples/tutorial/debugging/example.js
@@ -7,8 +7,8 @@ var lastModuleError = '';
 var crashed = false;
 
 function domContentLoaded(name, tc, config, width, height) {
-  common.createNaClModule(name, tc, config, width, height);
   common.attachDefaultListeners();
+  common.createNaClModule(name, tc, config, width, height);
 
   updateStatus('Page Loaded');
 }
