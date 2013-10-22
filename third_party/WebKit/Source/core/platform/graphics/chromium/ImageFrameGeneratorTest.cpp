2474b44e33e2a7be40e9eee6ffc684a2f962e424
jochen@chromium.org <jochen@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-22 22:40:38 +0000

    Update platform/graphics tests to WebThread API
    BUG=301514
    R=hclam@chromium.org
    
    Review URL: https://codereview.chromium.org/35223002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160262 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/graphics/chromium/ImageFrameGeneratorTest.cpp
+++ third_party/WebKit/Source/core/platform/graphics/chromium/ImageFrameGeneratorTest.cpp
@@ -27,10 +27,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "core/platform/graphics/chromium/ImageFrameGenerator.h"
 
-#include "platform/SharedBuffer.h"
 #include "core/platform/graphics/chromium/ImageDecodingStore.h"
 #include "core/platform/graphics/chromium/test/MockImageDecoder.h"
-#include "wtf/Threading.h"
+#include "platform/SharedBuffer.h"
+#include "platform/Task.h"
+#include "public/platform/Platform.h"
+#include "public/platform/WebThread.h"
 #include <gtest/gtest.h>
 
 namespace WebCore {
@@ -304,9 +306,8 @@ TEST_F(ImageFrameGeneratorTest, incompleteDecodeAndScaleBecomesComplete)
     EXPECT_EQ(2, m_frameBufferRequestCount);
 }
 
-static void decodeThreadMain(void* arg)
+static void decodeThreadMain(ImageFrameGenerator* generator)
 {
-    ImageFrameGenerator* generator = reinterpret_cast<ImageFrameGenerator*>(arg);
     const ScaledImageFragment* tempImage = generator->decodeAndScale(fullSize());
     ImageDecodingStore::instance()->unlockCache(generator, tempImage);
 }
@@ -327,8 +328,9 @@ TEST_F(ImageFrameGeneratorTest, incompleteDecodeBecomesCompleteMultiThreaded)
     // Frame can now be decoded completely.
     setFrameStatus(ImageFrame::FrameComplete);
     addNewData();
-    ThreadIdentifier threadID = createThread(&decodeThreadMain, m_generator.get(), "DecodeThread");
-    waitForThreadCompletion(threadID);
+    OwnPtr<WebKit::WebThread> thread = adoptPtr(WebKit::Platform::current()->createThread("DecodeThread"));
+    thread->postTask(new Task(WTF::bind(&decodeThreadMain, m_generator.get())));
+    thread.clear();
 
     EXPECT_EQ(2, m_frameBufferRequestCount);
     EXPECT_EQ(1, m_decodersDestroyed);
