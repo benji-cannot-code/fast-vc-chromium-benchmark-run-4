9da978ae8fe59da19f66c4965d2ff4f5d86b4dcb
timvolodine@chromium.org <timvolodine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-23 18:05:51 +0000

    Remove unused Device Orientation IPC messages.
    Remove old Device Orientation IPC message definitions and leave
    only the ones required for the new implementation using shared
    memory.
    
    BUG=261165
    
    Review URL: https://codereview.chromium.org/28913002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@230468 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/device_orientation/device_motion_messages.h
+++ content/common/device_orientation/device_motion_messages.h
@@ -13,14 +13,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #define IPC_MESSAGE_START DeviceMotionMsgStart
 
-// Messages sent from the renderer to the browser.
-
-// Asks the browser process to start polling, and return a shared memory
-// handles that will hold the data from the hardware. See
-// device_motion_hardware_buffer.h for a description of how synchronization is
-// handled. The number of Starts should match the number of Stops.
+// Asks the browser process to activate Device Motion sensors if necessary.
 IPC_MESSAGE_CONTROL0(DeviceMotionHostMsg_StartPolling)
+
+// The browser process asynchronously returns the shared memory handle that will
+// hold the data from the hardware sensors.
+// See device_motion_hardware_buffer.h for a description of how
+// synchronization is handled.
 IPC_MESSAGE_CONTROL1(DeviceMotionMsg_DidStartPolling,
                      base::SharedMemoryHandle /* handle */)
 
+// Notifies the browser process that the renderer process is not using the
+// Device Motion data anymore. The number of Starts should match the number
+// of Stops.
 IPC_MESSAGE_CONTROL0(DeviceMotionHostMsg_StopPolling)
