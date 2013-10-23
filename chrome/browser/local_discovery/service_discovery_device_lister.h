f032313952a7ea619912b39ef1de2b50859551bc
justinlin@chromium.org <justinlin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-23 01:17:51 +0000

    Fix service discovery resolver not being cleaned up on DNS resolving failures. Fix mDNS discovery on mac not detecting when devices are shutdown ungracefully (unplugged). Add more logging and checks to chrome.mdns discovery.
    BUG=308354
    TEST=Plug, unplug, plug, unplug device and use test extension to check that discovery picks up changes on mac.
    
    Review URL: https://codereview.chromium.org/30843003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@230271 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/local_discovery/service_discovery_device_lister.h
+++ chrome/browser/local_discovery/service_discovery_device_lister.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/linked_ptr.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "chrome/common/local_discovery/service_discovery_client.h"
 
 namespace local_discovery {
@@ -45,6 +46,7 @@ class ServiceDiscoveryDeviceLister {
 
   void OnResolveComplete(
       bool added,
+      std::string service_name,
       ServiceResolver::RequestStatus status,
       const ServiceDescription& description);
 
@@ -57,6 +59,10 @@ class ServiceDiscoveryDeviceLister {
 
   scoped_ptr<ServiceWatcher> service_watcher_;
   ServiceResolverMap resolvers_;
+
+  base::WeakPtrFactory<ServiceDiscoveryDeviceLister> weak_factory_;
+
+  DISALLOW_COPY_AND_ASSIGN(ServiceDiscoveryDeviceLister);
 };
 
 }  // namespace local_discovery
