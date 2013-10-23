0361d7fd8809ed8e15489259b0e25ae2b6bcc540
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-23 04:51:04 +0000

    Move some members of HTMLPlugInImageElement to HTMLPlugInElement, part 2.
     - serviceType()
     - url()
     - m_loadedurl and loadedUrl()
     - loadedMimeType()
     - m_needsWidgetUpdate, needsWidgetUpdate(), and setNeedsWidgetUpdate()
     - updateWidget()
    
    and remove HTMLPlugInImageElement::setDisplayState(), which just called
    HTMLPlugInElement::setDisplayState().
    
    This is a part of a task to merge HTMLPlugInImageELement into HTMLPlugInElement.
    Only HTMLPlugInImageElement inherits from HTMLPlugInElement.
    
    BUG=301217
    
    Review URL: https://codereview.chromium.org/34903007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160296 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLPlugInImageElement.h
+++ third_party/WebKit/Source/core/html/HTMLPlugInImageElement.h
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/html/HTMLPlugInElement.h"
 
-#include "core/platform/MIMETypeFromURL.h"
 #include "core/rendering/style/RenderStyle.h"
 #include "wtf/OwnPtr.h"
 
@@ -36,11 +35,6 @@ class Image;
 class MouseEvent;
 class Widget;
 
-enum PluginCreationOption {
-    CreateAnyWidgetType,
-    CreateOnlyNonNetscapePlugins,
-};
-
 // Base class for HTMLObjectElement and HTMLEmbedElement
 class HTMLPlugInImageElement : public HTMLPlugInElement {
 public:
@@ -48,31 +42,10 @@ public:
 
     RenderEmbeddedObject* renderEmbeddedObject() const;
 
-    virtual void setDisplayState(DisplayState) OVERRIDE;
-
-    virtual void updateWidget(PluginCreationOption) = 0;
-
-    const String& serviceType() const { return m_serviceType; }
-    const String& url() const { return m_url; }
-    const KURL& loadedUrl() const { return m_loadedUrl; }
-
-    const String loadedMimeType() const
-    {
-        String mimeType = serviceType();
-        if (mimeType.isEmpty())
-            mimeType = mimeTypeFromURL(m_loadedUrl);
-        return mimeType;
-    }
-
-    // Public for FrameView::addWidgetToUpdate()
-    bool needsWidgetUpdate() const { return m_needsWidgetUpdate; }
-    void setNeedsWidgetUpdate(bool needsWidgetUpdate) { m_needsWidgetUpdate = needsWidgetUpdate; }
-
 protected:
     HTMLPlugInImageElement(const QualifiedName& tagName, Document&, bool createdByParser, PreferPlugInsForImagesOption);
 
     OwnPtr<HTMLImageLoader> m_imageLoader;
-    KURL m_loadedUrl;
 
     static void updateWidgetCallback(Node*);
     virtual void attach(const AttachContext& = AttachContext()) OVERRIDE;
@@ -103,7 +76,6 @@ private:
 
     virtual bool isPlugInImageElement() const OVERRIDE { return true; }
 
-    bool m_needsWidgetUpdate;
     bool m_createdDuringUserGesture;
 };
 
