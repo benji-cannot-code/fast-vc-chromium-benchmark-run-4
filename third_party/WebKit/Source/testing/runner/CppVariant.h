8320aedb98b585a66553466de97766eca47bb2a0
r.kasibhatla@samsung.com <r.kasibhatla@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-23 08:43:05 +0000

    Remove incorrect deriving of CppVariant from WebNonCopyable.
    CppVariant is supposed to be a copyable class and we override the default copy ctor and assignment operator for the same. It is wrongly derived from WebNonCopyable. Remove the incorrect inheritance.
    
    R=jochen
    
    BUG=None
    
    TEST=None; no behavior change;
    
    Review URL: https://codereview.chromium.org/32943007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160319 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/testing/runner/CppVariant.h
+++ third_party/WebKit/Source/testing/runner/CppVariant.h
@@ -43,14 +43,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CppVariant_h
 #define CppVariant_h
 
-#include "public/platform/WebNonCopyable.h"
 #include "public/web/WebBindings.h"
 #include <string>
 #include <vector>
 
 namespace WebTestRunner {
 
-class CppVariant : public NPVariant, public WebKit::WebNonCopyable {
+class CppVariant : public NPVariant {
 public:
     CppVariant();
     ~CppVariant();
