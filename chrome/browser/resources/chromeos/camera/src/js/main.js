7003f1470eedf658567da44be266e0bda06491f0
Tomasz Mikolajewski <mtomasz@chromium.org>
2013-10-23 16:48:54 +0900

    Implement the delete confirmation dialog.
    This patch introduces the dialog view as well with its first client -
    the delete confirmation dialog.
    
    TEST=Try to delete from either the browser or the gallery view.
    BUG=chromium:309899
    
    Change-Id: I9d07d257671c8464ee71e1f0b4b94eef2b5f0663

--- chrome/browser/resources/chromeos/camera/src/js/main.js
+++ chrome/browser/resources/chromeos/camera/src/js/main.js
@@ -56,6 +56,12 @@ camera.Camera = function() {
   this.browserView_ = new camera.views.Browser(this.context_, this.router_);
 
   /**
+   * @type {camera.views.Dialog}
+   * @private
+   */
+  this.dialogView_ = new camera.views.Dialog(this.context_, this.router_);
+
+  /**
    * @type {?number}
    * @private
    */
@@ -207,6 +213,9 @@ camera.Camera.prototype = {
   },
   get browserView() {
     return this.browserView_;
+  },
+  get dialogView() {
+    return this.dialogView_;
   }
 };
 
@@ -220,6 +229,7 @@ camera.Camera.prototype.start = function() {
   queue.run(this.cameraView_.initialize.bind(this.cameraView_));
   queue.run(this.galleryView_.initialize.bind(this.galleryView_));
   queue.run(this.browserView_.initialize.bind(this.browserView_));
+  queue.run(this.dialogView_.initialize.bind(this.dialogView_));
 
   // Display the camera view after initializing.
   queue.run(function(callback) {
@@ -249,6 +259,9 @@ camera.Camera.prototype.navigateById_ = function(
       break;
     case camera.Router.ViewIdentifier.BROWSER:
       this.viewsStack_.push(this.browserView_, opt_arguments, opt_callback);
+      break;
+    case camera.Router.ViewIdentifier.DIALOG:
+      this.viewsStack_.push(this.dialogView_, opt_arguments, opt_callback);
       break;
   }
 };
