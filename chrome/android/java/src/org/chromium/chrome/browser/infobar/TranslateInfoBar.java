cfb980ad454521c35987a42facec2765b5d61d77
miguelg@chromium.org <miguelg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-23 19:53:26 +0000

    [InfoBar] Do not try to keep a copy of the native pointer on the translate infobar
    BUG=310212
    
    We need to be using the one from the superclass which is the one that gets properly swapped in case of in place replacements.
    
    Review URL: https://codereview.chromium.org/37333002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@230494 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/android/java/src/org/chromium/chrome/browser/infobar/TranslateInfoBar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/infobar/TranslateInfoBar.java
@@ -50,7 +50,6 @@ public class TranslateInfoBar extends TwoButtonInfoBar implements SubPanelListen
     private TranslateSubPanel mSubPanel;
     private final boolean mShouldShowNeverBar;
     private final TranslateInfoBarDelegate mTranslateDelegate;
-    private final int mNativeInfoBarPtr;
 
     public TranslateInfoBar(int nativeInfoBarPtr, TranslateInfoBarDelegate delegate,
             int infoBarType, int sourceLanguageIndex, int targetLanguageIndex,
@@ -63,7 +62,6 @@ public class TranslateInfoBar extends TwoButtonInfoBar implements SubPanelListen
         mInfoBarType = infoBarType;
         mShouldShowNeverBar = shouldShowNeverBar;
         mOptionsPanelViewType = NO_PANEL;
-        mNativeInfoBarPtr = nativeInfoBarPtr;
         setNativeInfoBar(nativeInfoBarPtr);
     }
 
