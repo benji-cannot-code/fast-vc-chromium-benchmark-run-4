a90680d4815ac038de16f60af69d34880ac374f3
ricea@chromium.org <ricea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-23 08:15:10 +0000

    Add closing handshake timeout.
    If either the Close message or socket close fails to arrive within 4
    minutes, we should close the connection outselves.
    
    Also add tests for the closing handshake timeout (using a 1ms timeout).
    
    BUG=230756
    TEST=net_unittests --gtest_filter=WebSocketChannel*
    
    Review URL: https://codereview.chromium.org/31813007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@230367 0039d316-1c4b-4281-b951-d872f2087c98

--- net/websockets/websocket_channel.h
+++ net/websockets/websocket_channel.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/scoped_vector.h"
+#include "base/time/time.h"
+#include "base/timer/timer.h"
 #include "net/base/net_export.h"
 #include "net/websockets/websocket_frame.h"
 #include "net/websockets/websocket_stream.h"
@@ -93,6 +95,11 @@ class NET_EXPORT WebSocketChannel {
       const std::vector<std::string>& requested_protocols,
       const GURL& origin,
       const WebSocketStreamFactory& factory);
+
+  // The default timout for the closing handshake is a sensible value (see
+  // kClosingHandshakeTimeoutSeconds in websocket_channel.cc). However, we can
+  // set it to a very small value for testing purposes.
+  void SetClosingHandshakeTimeoutForTesting(base::TimeDelta delay);
 
  private:
   // The object passes through a linear progression of states from
@@ -210,6 +217,10 @@ class NET_EXPORT WebSocketChannel {
                   uint16* code,
                   std::string* reason);
 
+  // Called if the closing handshake times out. Closes the connection and
+  // informs the |event_interface_| if appropriate.
+  void CloseTimeout();
+
   // The URL of the remote server.
   GURL socket_url_;
 
@@ -248,6 +259,12 @@ class NET_EXPORT WebSocketChannel {
   // The current amount of quota that the renderer has available for sending
   // on this logical channel (quota units).
   int current_send_quota_;
+
+  // Timer for the closing handshake.
+  base::OneShotTimer<WebSocketChannel> timer_;
+
+  // Timeout for the closing handshake.
+  base::TimeDelta timeout_;
 
   // Storage for the status code and reason from the time the Close frame
   // arrives until the connection is closed and they are passed to
