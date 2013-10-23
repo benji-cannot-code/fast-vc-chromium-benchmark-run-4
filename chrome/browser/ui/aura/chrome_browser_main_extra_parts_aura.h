a4dbc09db8e5fabef5ec15da16742a911adda96a
kbr@chromium.org <kbr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-23 23:19:34 +0000

    Move Gtk initialization earlier in BrowserMain.
    In order to work around bugs in glib, it must be initialized before
    any threads are started in the process. If this is not done then the
    process will intermittently hang upon exit.
    
    BUG=309093
    R=erg@chromium.org, jorgelo@chromium.org, sky@chromium.org
    
    Review URL: https://codereview.chromium.org/34463007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@230547 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/aura/chrome_browser_main_extra_parts_aura.h
+++ chrome/browser/ui/aura/chrome_browser_main_extra_parts_aura.h
@@ -19,6 +19,7 @@ class ChromeBrowserMainExtraPartsAura : public ChromeBrowserMainExtraParts {
   virtual ~ChromeBrowserMainExtraPartsAura();
 
   // Overridden from ChromeBrowserMainExtraParts:
+  virtual void PreEarlyInitialization() OVERRIDE;
   virtual void ToolkitInitialized() OVERRIDE;
   virtual void PostMainMessageLoopStart() OVERRIDE;
   virtual void PostMainMessageLoopRun() OVERRIDE;
