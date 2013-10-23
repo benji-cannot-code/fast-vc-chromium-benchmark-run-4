e8b0305a88a31f6f11fcaab472fcbf65e71003a0
tapted@chromium.org <tapted@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-23 04:48:30 +0000

    [win8] Ignore non-Ash ShellWindows in Ash's ShellWindowLauncherController
    Currently if a packaged app is running in desktop mode, and it is
    launched again from the Ash Desktop on Windows 8, Ash will try to focus
    the currently running app on the (hidden) Windows Desktop.
    
    This change allows the Ash desktop to open its own copy of packaged apps
    that support being launched multiple times.
    
    BUG=288251
    TEST=Open a packaged app in desktop mode that supports multiple windows
    (e.g. 500px, "Hex Lines"). Leave it open, and open the same app in
    Windows 8/Ash mode. A new window should open in Ash. Note that
    "singleton" apps  that specify an id when creating windows (e.g. Google
    Keep) are not yet fixed by this.
    
    Review URL: https://codereview.chromium.org/28913003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@230322 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/ash/launcher/shell_window_launcher_controller.cc
+++ chrome/browser/ui/ash/launcher/shell_window_launcher_controller.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/ash/launcher/chrome_launcher_controller.h"
 #include "chrome/browser/ui/ash/launcher/shell_window_launcher_item_controller.h"
+#include "chrome/browser/ui/host_desktop.h"
 #include "ui/aura/client/activation_client.h"
 
 using apps::ShellWindow;
@@ -22,6 +23,11 @@ std::string GetAppLauncherId(ShellWindow* shell_window) {
   if (shell_window->window_type_is_panel())
     return base::StringPrintf("panel:%d", shell_window->session_id().id());
   return shell_window->extension()->id();
+}
+
+bool ControlsWindow(aura::Window* window) {
+  return chrome::GetHostDesktopTypeForNativeWindow(window) ==
+      chrome::HOST_DESKTOP_TYPE_ASH;
 }
 
 }  // namespace
@@ -56,6 +62,9 @@ ShellWindowLauncherController::~ShellWindowLauncherController() {
 void ShellWindowLauncherController::OnShellWindowAdded(
     ShellWindow* shell_window) {
   aura::Window* window = shell_window->GetNativeWindow();
+  if (!ControlsWindow(window))
+    return;
+
   // Get the app's launcher identifier and add an entry to the map.
   DCHECK(window_to_app_launcher_id_map_.find(window) ==
          window_to_app_launcher_id_map_.end());
@@ -98,6 +107,9 @@ void ShellWindowLauncherController::OnShellWindowAdded(
 
 void ShellWindowLauncherController::OnShellWindowIconChanged(
     ShellWindow* shell_window) {
+  if (!ControlsWindow(shell_window->GetNativeWindow()))
+    return;
+
   const std::string app_launcher_id = GetAppLauncherId(shell_window);
   AppControllerMap::iterator iter = app_controller_map_.find(app_launcher_id);
   if (iter == app_controller_map_.end())
@@ -118,6 +130,9 @@ void ShellWindowLauncherController::OnShellWindowRemoved(
 // destroys ShellWindow, so both |window| and the associated ShellWindow
 // are valid here.
 void ShellWindowLauncherController::OnWindowDestroying(aura::Window* window) {
+  if (!ControlsWindow(window))
+    return;
+
   WindowToAppLauncherIdMap::iterator iter1 =
       window_to_app_launcher_id_map_.find(window);
   DCHECK(iter1 != window_to_app_launcher_id_map_.end());
