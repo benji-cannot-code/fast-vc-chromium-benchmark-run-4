2d46c6425b9ceb9a7396fc6ede6458dac3bfa795
simonjam@chromium.org <simonjam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-24 02:44:11 +0000

    [Tracing] Add support for forward looking or backwards looking async steps.
    Renamed TRACE_EVENT_ASYNC_STEP to TRACE_EVENT_ASYNC_STEP_INTO and
    added TRACE_EVENT_ASYNC_STEP_PAST. These are mutually exclusive. Users
    must decide all of their steps are backwards or forwards looking for an
    async event.
    
    Updated callers to use the correct versions. Note that we had both!
    
    BUG=None
    TBR=nduca,mmenke,wjia,hclam
    
    Review URL: https://codereview.chromium.org/25536014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@230606 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/resources/pixel_buffer_raster_worker_pool.cc
+++ cc/resources/pixel_buffer_raster_worker_pool.cc
@@ -213,7 +213,7 @@ void PixelBufferRasterWorkerPool::ScheduleTasks(RasterTask::Queue* queue) {
   check_for_completed_raster_tasks_pending_ = false;
   ScheduleCheckForCompletedRasterTasks();
 
-  TRACE_EVENT_ASYNC_STEP1(
+  TRACE_EVENT_ASYNC_STEP_INTO1(
       "cc", "ScheduledTasks", this, StateName(),
       "state", TracedValue::FromValue(StateAsValue().release()));
 }
@@ -398,7 +398,7 @@ void PixelBufferRasterWorkerPool::CheckForCompletedRasterTasks() {
   if (PendingRasterTaskCount())
     ScheduleMoreTasks();
 
-  TRACE_EVENT_ASYNC_STEP1(
+  TRACE_EVENT_ASYNC_STEP_INTO1(
       "cc", "ScheduledTasks", this, StateName(),
       "state", TracedValue::FromValue(StateAsValue().release()));
 
