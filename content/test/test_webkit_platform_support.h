d92d0d24e347eb19317c891c14767be2056c9314
jamesr@chromium.org <jamesr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-24 20:29:13 +0000

    Trim more dead code from webkit/support/
    Review URL: https://codereview.chromium.org/23264025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@230794 0039d316-1c4b-4281-b951-d872f2087c98

--- content/test/test_webkit_platform_support.h
+++ content/test/test_webkit_platform_support.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/platform/WebUnitTestSupport.h"
 #include "webkit/child/webkitplatformsupport_child_impl.h"
 #include "webkit/glue/simple_webmimeregistry_impl.h"
-#include "webkit/glue/webfileutilities_impl.h"
 #include "webkit/renderer/compositor_bindings/web_compositor_support_impl.h"
 
 namespace WebKit {
@@ -32,11 +31,10 @@ class TestWebKitPlatformSupport
 
   virtual WebKit::WebMimeRegistry* mimeRegistry();
   virtual WebKit::WebClipboard* clipboard();
-  virtual WebKit::WebFileUtilities* fileUtilities();
-  virtual WebKit::WebIDBFactory* idbFactory();
 
   virtual WebKit::WebURLLoader* createURLLoader();
-  virtual WebKit::WebData loadResource(const char* name);
+  virtual WebKit::WebString defaultLocale();
+
   virtual WebKit::WebString queryLocalizedString(
       WebKit::WebLocalizedString::Name name);
   virtual WebKit::WebString queryLocalizedString(
@@ -46,7 +44,6 @@ class TestWebKitPlatformSupport
       WebKit::WebLocalizedString::Name name,
       const WebKit::WebString& value1,
       const WebKit::WebString& value2);
-  virtual WebKit::WebString defaultLocale();
 
 #if defined(OS_WIN) || defined(OS_MACOSX)
   void SetThemeEngine(WebKit::WebThemeEngine* engine);
@@ -57,10 +54,6 @@ class TestWebKitPlatformSupport
 
   WebURLLoaderMockFactory* url_loader_factory() {
     return url_loader_factory_.get();
-  }
-
-  const base::FilePath& file_system_root() const {
-    return file_system_root_.path();
   }
 
   virtual base::string16 GetLocalizedString(int message_id) OVERRIDE;
@@ -100,8 +93,6 @@ class TestWebKitPlatformSupport
  private:
   webkit_glue::SimpleWebMimeRegistryImpl mime_registry_;
   scoped_ptr<MockWebClipboardImpl> mock_clipboard_;
-  webkit_glue::WebFileUtilitiesImpl file_utilities_;
-  base::ScopedTempDir file_system_root_;
   scoped_ptr<WebURLLoaderMockFactory> url_loader_factory_;
   webkit::WebCompositorSupportImpl compositor_support_;
 
