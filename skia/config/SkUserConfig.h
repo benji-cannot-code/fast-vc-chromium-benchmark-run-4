7566ac2386527c9560a6b5e8858da9cfdd97a3d0
bungeman@chromium.org <bungeman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-24 21:58:01 +0000

    Enable reference adoption checks for Blink+skia in Debug builds.
    This is an alternate to Issue 29763002.
    
    R=junov@chromium.org
    
    Review URL: https://codereview.chromium.org/40973002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@230831 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -153,6 +153,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // ===== Begin Chrome-specific definitions =====
 
+#ifdef SK_DEBUG
+#define SK_REF_CNT_MIXIN_INCLUDE "sk_ref_cnt_ext_debug.h"
+#else
+#define SK_REF_CNT_MIXIN_INCLUDE "sk_ref_cnt_ext_release.h"
+#endif
+
 #define SK_SCALAR_IS_FLOAT
 #undef SK_SCALAR_IS_FIXED
 
