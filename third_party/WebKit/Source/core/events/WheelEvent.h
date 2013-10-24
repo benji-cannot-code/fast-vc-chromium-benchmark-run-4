7660de7acd4e1d2a1ce5862dc97226b822ceeb4c
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-24 02:47:43 +0000

    Generate casting functions for Event subclasses by DEFINE_TYPE_CASTS macro.
    This reduces source code size, and adds const/reference support.
    This CL introduces DEFINE_EVENT_TYPE_CASTS macro for cases that Event
    class has isTypeName() function.
    
    BUG=309516
    TEST=none; no behavior changes.
    
    Review URL: https://codereview.chromium.org/38663002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160408 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/events/WheelEvent.h
+++ third_party/WebKit/Source/core/events/WheelEvent.h
@@ -113,11 +113,7 @@ private:
     bool m_directionInvertedFromDevice;
 };
 
-inline WheelEvent* toWheelEvent(Event* event)
-{
-    ASSERT_WITH_SECURITY_IMPLICATION(!event || event->isWheelEvent());
-    return static_cast<WheelEvent*>(event);
-}
+DEFINE_EVENT_TYPE_CASTS(WheelEvent);
 
 class WheelEventDispatchMediator : public EventDispatchMediator {
 public:
