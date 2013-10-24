4fc8eb426e547a9495b123318a8310029b28337d
aandrey@chromium.org <aandrey@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-24 15:53:40 +0000

    DevTools: Object accessors query results are to be shown as preview or array entry.
    This is a follow-up to r160345, which made object accessors eval results
    display as a preview instead of array entry. We should support both.
    
    BUG=175898
    R=pfeldman,vsevik
    
    Review URL: https://codereview.chromium.org/38483006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160459 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ConsoleMessage.js
+++ third_party/WebKit/Source/devtools/front_end/ConsoleMessage.js
@@ -437,7 +437,7 @@ WebInspector.ConsoleMessageImpl.prototype = {
     {
         var property = propertyPath.peekLast();
         if (property.type === "accessor")
-            return this._formatAsAccessorProperty(object, propertyPath.select("name"));
+            return this._formatAsAccessorProperty(object, propertyPath.select("name"), false);
         return this._renderPropertyPreview(property.type, /** @type {string} */ (property.subtype), property.value);
     },
 
@@ -617,7 +617,7 @@ WebInspector.ConsoleMessageImpl.prototype = {
             if (isNaN(name))
                 continue;
             if (property.getter)
-                elements[name] = this._formatAsAccessorProperty(array, [name]);
+                elements[name] = this._formatAsAccessorProperty(array, [name], true);
             else if (property.value)
                 elements[name] = this._formatAsArrayEntry(property.value);
         }
@@ -667,9 +667,10 @@ WebInspector.ConsoleMessageImpl.prototype = {
     /**
      * @param {!WebInspector.RemoteObject} object
      * @param {!Array.<string>} propertyPath
+     * @param {boolean} isArrayEntry
      * @return {!Element}
      */
-    _formatAsAccessorProperty: function(object, propertyPath)
+    _formatAsAccessorProperty: function(object, propertyPath, isArrayEntry)
     {
         var rootElement = WebInspector.ObjectPropertyTreeElement.createRemoteObjectAccessorPropertySpan(object, propertyPath, onInvokeGetterClick.bind(this));
 
@@ -684,6 +685,8 @@ WebInspector.ConsoleMessageImpl.prototype = {
                 var element = rootElement.createChild("span", "error-message");
                 element.textContent = WebInspector.UIString("<exception>");
                 element.title = result.description;
+            } else if (isArrayEntry) {
+                rootElement.appendChild(this._formatAsArrayEntry(result));
             } else {
                 // Make a PropertyPreview from the RemoteObject similar to the backend logic.
                 const maxLength = 100;
