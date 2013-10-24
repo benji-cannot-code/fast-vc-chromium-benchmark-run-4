93150f7bbe45dd58bccf49782fea50a7f722969c
guohui@chromium.org <guohui@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-24 23:00:04 +0000

    Prefill email on Chrome reauth page
    BUG=
    
    Review URL: https://codereview.chromium.org/38233002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@230860 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/sync_setup_handler.cc
+++ chrome/browser/ui/webui/sync_setup_handler.cc
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/chromium_strings.h"
 #include "grit/generated_resources.h"
 #include "grit/locale_settings.h"
+#include "net/base/url_util.h"
 #include "ui/base/l10n/l10n_util.h"
 
 #if defined(OS_CHROMEOS)
@@ -505,11 +506,7 @@ void SyncSetupHandler::DisplayGaiaLoginInNewTabOrWindow() {
     UMA_HISTOGRAM_ENUMERATION("Signin.Reauth",
                               signin::HISTOGRAM_SHOWN,
                               signin::HISTOGRAM_MAX);
-    std::string fragment("Email=");
-    fragment += email;
-    GURL::Replacements replacements;
-    replacements.SetRefStr(fragment);
-    url = url.ReplaceComponents(replacements);
+    url = net::AppendQueryParameter(url, "Email", email);
   }
 
   browser->OpenURL(
