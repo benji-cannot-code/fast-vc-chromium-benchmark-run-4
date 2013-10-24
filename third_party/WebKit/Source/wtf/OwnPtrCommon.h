937e92ff6c506f005dec2af16f4bcc8d6f3a2ecc
mikhail.pozdnyakov@intel.com <mikhail.pozdnyakov@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-24 16:23:24 +0000

    Do not include RefCounted header files to OwnPtrCommon.h
    Do not include RefCounted header files to OwnPtrCommon.h - forward declaration should be used instead.
    
    Review URL: https://codereview.chromium.org/39903002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160461 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/OwnPtrCommon.h
+++ third_party/WebKit/Source/wtf/OwnPtrCommon.h
@@ -31,11 +31,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WTF_OwnPtrCommon_h
 
 #include "wtf/Assertions.h"
-#include "wtf/RefCounted.h"
-#include "wtf/ThreadSafeRefCounted.h"
 #include "wtf/TypeTraits.h"
 
 namespace WTF {
+
+class RefCountedBase;
+class ThreadSafeRefCountedBase;
 
 template<typename T>
 struct IsRefCounted {
