f78737b2298a9cb1876055864af52881b29bd856
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-24 01:53:21 +0000

    Media Galleries: Add missing include from r230593.
    R=rsleevi@chromium.org
    TBR=gbillock@chromium.org
    
    Review URL: https://codereview.chromium.org/39103002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@230598 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media_galleries/media_file_system_registry.h
+++ chrome/browser/media_galleries/media_file_system_registry.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/files/file_path.h"
 #include "base/memory/ref_counted.h"
+#include "base/memory/scoped_ptr.h"
 #include "chrome/browser/media_galleries/media_galleries_preferences.h"
 #include "chrome/browser/storage_monitor/removable_storage_observer.h"
 
