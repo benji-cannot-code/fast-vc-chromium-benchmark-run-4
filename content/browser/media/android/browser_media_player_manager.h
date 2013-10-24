867670055410d063c9ade1bd482a60e3a9b05a7e
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-24 19:29:38 +0000

    Make BrowserMediaPlayerManager a WebContentsObserver instead of a RenderViewHostObserver so that it can filter IPCs.
    This is part of the work in removing RenderViewHostObserver.
    
    BUG=306569
    R=qinmin@chromium.org
    
    Review URL: https://codereview.chromium.org/36323002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@230789 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/media/android/browser_media_player_manager.h
+++ content/browser/media/android/browser_media_player_manager.h
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "content/browser/android/content_video_view.h"
 #include "content/common/media/media_player_messages_enums_android.h"
-#include "content/public/browser/render_view_host_observer.h"
+#include "content/public/browser/web_contents_observer.h"
 #include "media/base/android/media_player_android.h"
 #include "media/base/android/media_player_manager.h"
 #include "ui/gfx/rect_f.h"
@@ -39,7 +39,7 @@ class WebContents;
 // MediaPlayerAndroid objects are converted to IPCs and then sent to the
 // render process.
 class CONTENT_EXPORT BrowserMediaPlayerManager
-    : public RenderViewHostObserver,
+    : public WebContentsObserver,
       public media::MediaPlayerManager {
  public:
   // Permits embedders to provide an extended version of the class.
@@ -51,7 +51,7 @@ class CONTENT_EXPORT BrowserMediaPlayerManager
 
   virtual ~BrowserMediaPlayerManager();
 
-  // RenderViewHostObserver overrides.
+  // WebContentsObserver overrides.
   virtual bool OnMessageReceived(const IPC::Message& message) OVERRIDE;
 
   // Fullscreen video playback controls.
