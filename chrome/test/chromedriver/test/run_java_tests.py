070680607a93834ab1c85c0849d77fdcc2bcbaf7
frankf@chromium.org <frankf@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-24 18:29:31 +0000

    [chromedriver] Capture server logs to a file.
    BUG=chromedriver:592
    R=kkania@chromium.org
    
    Review URL: https://codereview.chromium.org/39143002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@230774 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/chromedriver/test/run_java_tests.py
+++ chrome/test/chromedriver/test/run_java_tests.py
@@ -59,7 +59,7 @@ class TestResult(object):
 
 
 def _Run(java_tests_src_dir, test_filter,
-         chromedriver_path, chrome_path, android_package,
+         chromedriver_path, chrome_path, log_path, android_package,
          verbose, debug):
   """Run the WebDriver Java tests and return the test results.
 
@@ -69,6 +69,7 @@ def _Run(java_tests_src_dir, test_filter,
         as Google C++ Test format.
     chromedriver_path: path to ChromeDriver exe.
     chrome_path: path to Chrome exe.
+    log_path: path to server log.
     android_package: name of Chrome's Android package.
     verbose: whether the output should be verbose.
     debug: whether the tests should wait until attached by a debugger.
@@ -95,9 +96,11 @@ def _Run(java_tests_src_dir, test_filter,
 
   sys_props = ['selenium.browser=chrome',
                'webdriver.chrome.driver=' + os.path.abspath(chromedriver_path)]
-  if chrome_path is not None:
+  if chrome_path:
     sys_props += ['webdriver.chrome.binary=' + os.path.abspath(chrome_path)]
-  if android_package is not None:
+  if log_path:
+    sys_props += ['webdriver.chrome.logfile=' + log_path]
+  if android_package:
     sys_props += ['webdriver.chrome.android_package=' + android_package]
   if test_filter:
     # Test jar actually takes a regex. Convert from glob.
@@ -234,6 +237,9 @@ def main():
       '', '--chrome', type='string', default=None,
       help='Path to a build of the chrome binary')
   parser.add_option(
+      '', '--log-path',
+      help='Output verbose server logs to this file')
+  parser.add_option(
       '', '--chrome-version', default='HEAD',
       help='Version of chrome. Default is \'HEAD\'')
   parser.add_option(
@@ -303,6 +309,7 @@ def main():
           test_filter=filter,
           chromedriver_path=options.chromedriver,
           chrome_path=options.chrome,
+          log_path=options.log_path,
           android_package=options.android_package,
           verbose=options.verbose,
           debug=options.debug)
