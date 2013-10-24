4787ec0ee73615bf1e3c3df10ad8732eb9de4bbc
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-24 04:43:11 +0000

    Show the Chrome Frame turndown prompt daily.
    BUG=310806
    R=robertshield@chromium.org
    
    Review URL: https://codereview.chromium.org/37173006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@230642 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/turndown_prompt/turndown_prompt.cc
+++ chrome_frame/turndown_prompt/turndown_prompt.cc
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // Time between showings of the turndown prompt.
-const int kTurndownPromptReshowDeltaMinutes = 60 * 24 * 7;
+const int kTurndownPromptReshowDeltaMinutes = 60 * 24 * 1;
 
 void OnUninstallClicked(UrlLauncher* url_launcher);
 
