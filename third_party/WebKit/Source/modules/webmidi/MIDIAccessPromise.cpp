1e58c9a9a31e984a198e30f1ddfe04445c15f89c
toyoshim@chromium.org <toyoshim@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-24 10:06:44 +0000

    MIDIAccessPromise cleanup
    - Stop using {un}setPendingActivity to avoid counting mismatch
    - Implement ActiveDOMObject::stop()
    
    BUG=N/A
    TEST=Layouttests/webmidi
    
    Review URL: https://codereview.chromium.org/39233002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160436 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/webmidi/MIDIAccessPromise.cpp
+++ third_party/WebKit/Source/modules/webmidi/MIDIAccessPromise.cpp
@@ -59,12 +59,17 @@ MIDIAccessPromise::~MIDIAccessPromise()
     stop();
 }
 
-void MIDIAccessPromise::clear()
+bool MIDIAccessPromise::hasPendingActivity() const
 {
-    ASSERT(m_state == Invoked);
-    m_access.clear();
-    m_error.clear();
-    m_options.clear();
+    return !!m_access && m_state != Accepted && m_state != Rejected;
+}
+
+void MIDIAccessPromise::stop()
+{
+    m_state = Invoked;
+    if (!!m_access)
+        m_access->stop();
+    clear();
 }
 
 void MIDIAccessPromise::fulfill()
@@ -78,10 +83,7 @@ void MIDIAccessPromise::fulfill()
         } else {
             m_state = Accepted;
         }
-        unsetPendingActivity(this);
     }
-    m_successCallback.clear();
-    m_errorCallback.clear();
 }
 
 void MIDIAccessPromise::reject(PassRefPtr<DOMError> error)
@@ -95,19 +97,14 @@ void MIDIAccessPromise::reject(PassRefPtr<DOMError> error)
             m_state = Rejected;
             m_error = error;
         }
-        unsetPendingActivity(this);
     }
-    m_successCallback.clear();
-    m_errorCallback.clear();
 }
 
 void MIDIAccessPromise::then(PassRefPtr<MIDISuccessCallback> successCallback, PassRefPtr<MIDIErrorCallback> errorCallback)
 {
     // Lazily request access.
-    if (!m_access) {
-        setPendingActivity(this);
+    if (!m_access)
         m_access = MIDIAccess::create(executionContext(), this);
-    }
 
     switch (m_state) {
     case Accepted:
@@ -129,6 +126,16 @@ void MIDIAccessPromise::then(PassRefPtr<MIDISuccessCallback> successCallback, Pa
     default:
         ASSERT_NOT_REACHED();
     }
+}
+
+void MIDIAccessPromise::clear()
+{
+    ASSERT(m_state == Invoked);
+    m_access.clear();
+    m_error.clear();
+    m_options.clear();
+    m_successCallback.clear();
+    m_errorCallback.clear();
 }
 
 } // namespace WebCore
