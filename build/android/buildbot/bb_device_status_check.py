86dfccbc5fa8442a66986f8f3c85c6f86f122296
bulach@chromium.org <bulach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-25 16:32:43 +0000

    Android: kill all adb instances before trying to restart usb.
    On some bots, there are adb instances left over from previous runs.
    They don't play well with restarting the usb, and devices eventually "disappear".
    This tries to kill all adb instances before restarting usb.
    
    BUG=311058
    TBR=navabi@chromium.org
    
    Review URL: https://codereview.chromium.org/39833003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231035 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/buildbot/bb_device_status_check.py
+++ build/android/buildbot/bb_device_status_check.py
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import logging
 import optparse
 import os
+import psutil
+import signal
 import smtplib
 import subprocess
 import sys
@@ -242,6 +244,24 @@ def RestartUsb():
   return 0
 
 
+def KillAllAdb():
+  def GetAllAdb():
+    for p in psutil.process_iter():
+      if 'adb' in p.name or 'adb' in ' '.join(p.cmdline):
+        yield p
+
+  for sig in [signal.SIGTERM, signal.SIGQUIT, signal.SIGKILL]:
+    for p in GetAllAdb():
+      try:
+        print 'kill %d %d (%s [%s])' % (sig, p.pid, p.name,
+            ' '.join(p.cmdline))
+        p.send_signal(sig)
+      except psutil.error.NoSuchProcess:
+        pass
+  for p in GetAllAdb():
+    print 'Unable to kill %d (%s [%s])' % (p.pid, p.name, ' '.join(p.cmdline))
+
+
 def main():
   parser = optparse.OptionParser()
   parser.add_option('', '--out-dir',
@@ -258,6 +278,7 @@ def main():
     parser.error('Unknown options %s' % args)
 
   if options.restart_usb:
+    KillAllAdb()
     rc = RestartUsb()
     if rc:
       return 1
