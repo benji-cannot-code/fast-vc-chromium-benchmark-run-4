fcff511f4d085373e89bf769f13cf944c80205f5
kaznacheev@chromium.org <kaznacheev@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-25 15:40:49 +0000

    DevTools: Do not open newer frontend in an older Chrome
    BUG=None
    
    Review URL: https://codereview.chromium.org/43623002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160582 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/InspectorFrontendHostStub.js
+++ third_party/WebKit/Source/devtools/front_end/InspectorFrontendHostStub.js
@@ -199,8 +199,8 @@ WebInspector.InspectorFrontendHostStub.prototype = {
 
 InspectorFrontendHost = new WebInspector.InspectorFrontendHostStub();
 
-} else {
-    // Install message-based handlers with callbacks.
+} else if (InspectorFrontendHost.sendMessageToEmbedder) {
+  // Install message-based handlers with callbacks.
     var lastCallId = 0;
     InspectorFrontendHost._callbacks = [];
 
