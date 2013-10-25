65de019db214c746573c20a4ab9b3567091b5c93
Tomasz Mikolajewski <mtomasz@chromium.org>
2013-10-25 12:37:14 +0900

    Introduce a GalleryBase view.
    This patch cleans up the browser and the gallery view, which were sharing
    some methods and buttons. Since now, they both extend a general GalleryButton
    class.
    
    Along the way, the button logic has been cleaned, so each view keeps its own
    buttons, to avoid spaghetti.
    
    TEST=Check if the gallery and browser view work.
    BUG=chromium:311450
    
    Change-Id: I991de79ed2d1f5eea799ca441a9e2aa5ad8d8dd6

--- src/js/views/camera.js
+++ src/js/views/camera.js
@@ -256,8 +256,8 @@ camera.views.Camera = function(context, router) {
   document.querySelector('#take-picture').addEventListener(
       'click', this.takePicture_.bind(this));
 
-  document.querySelector('#toolbar .gallery-switch').addEventListener('click',
-      this.onGalleryClicked_.bind(this));
+  document.querySelector('#toolbar #album-enter').addEventListener('click',
+      this.onAlbumEnterClicked_.bind(this));
 
   // Load the shutter sound.
   this.shutterSound_.src = '../sounds/shutter.ogg';
@@ -395,12 +395,12 @@ camera.views.Camera.prototype.onLeave = function() {
 };
 
 /**
- * Handles clicking on the gallery button.
+ * Handles clicking on the album button.
  * @param {Event} event Mouse event
  * @private
  */
-camera.views.Camera.prototype.onGalleryClicked_ = function(event) {
-  this.router.navigate(camera.Router.ViewIdentifier.GALLERY);
+camera.views.Camera.prototype.onAlbumEnterClicked_ = function(event) {
+  this.router.navigate(camera.Router.ViewIdentifier.ALBUM);
 };
 
 /**
@@ -548,7 +548,7 @@ camera.views.Camera.prototype.chooseFileStream = function() {
 };
 
 /**
- * Takes the picture, saves and puts to the gallery with a nice animation.
+ * Takes the picture, saves and puts to the album with a nice animation.
  * @private
  */
 camera.views.Camera.prototype.takePicture_ = function() {
@@ -560,8 +560,8 @@ camera.views.Camera.prototype.takePicture_ = function() {
 
   // Show flashing animation with the shutter sound.
   document.body.classList.add('show-shutter');
-  var galleryButton = document.querySelector('#toolbar .gallery-switch');
-  camera.util.setAnimationClass(galleryButton, galleryButton, 'flash');
+  var albumButton = document.querySelector('#toolbar #album-enter');
+  camera.util.setAnimationClass(albumButton, albumButton, 'flash');
   setTimeout(function() {
     document.body.classList.remove('show-shutter');
   }.bind(this), 200);
@@ -589,7 +589,7 @@ camera.views.Camera.prototype.takePicture_ = function() {
     // Create the fly-away animation after two second.
     setTimeout(function() {
       var sourceRect = img.getBoundingClientRect();
-      var targetRect = galleryButton.getBoundingClientRect();
+      var targetRect = albumButton.getBoundingClientRect();
 
       var translateXValue = (targetRect.left + targetRect.right) / 2 -
           (sourceRect.left + sourceRect.right) / 2;
