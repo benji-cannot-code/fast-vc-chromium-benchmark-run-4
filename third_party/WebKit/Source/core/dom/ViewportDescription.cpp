08804cba28f1b68f0cc80f7ced62e3a02df3c8b4
seokju.kwon@gmail.com <seokju.kwon@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-25 11:17:16 +0000

    Clean up ViewportDescription.
    Get rid of unused 'includes' and forward declarations.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/39823002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160566 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/ViewportDescription.cpp
+++ third_party/WebKit/Source/core/dom/ViewportDescription.cpp
@@ -29,11 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "core/dom/ViewportDescription.h"
 
-#include "core/dom/Document.h"
-#include "core/page/Page.h"
-#include "core/page/Settings.h"
-#include "wtf/text/WTFString.h"
-
 using namespace std;
 
 namespace WebCore {
--- third_party/WebKit/Source/core/dom/ViewportDescription.h
+++ third_party/WebKit/Source/core/dom/ViewportDescription.h
@@ -32,11 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/page/PageScaleConstraints.h"
 #include "platform/Length.h"
 #include "platform/geometry/FloatSize.h"
-#include "wtf/Forward.h"
 
 namespace WebCore {
-
-class Document;
 
 struct ViewportDescription {
 
