9f83b14fa2c4f41bac240d6b0be3fd15ef983682
miu@chromium.org <miu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-25 23:34:37 +0000

    Re-land r230914 (tab tooltip strings) with fix for generated_resources.grd issue, per joi@ and tony@.
    It turns out adding strings to generated_resources.grd has caused us to reach our limit in the ID key space.  Per e-mail thread on chromium-dev@, we decided to make an ID bump in tools/gritsettings/resource_ids, to the starting point for webkit_strings.grd, in order to make room for more strings in generated_resources.grd.
    
    TBR=sky,tony
    
    Review URL: https://codereview.chromium.org/46043002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231149 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/tabs/tab_controller.h
+++ chrome/browser/ui/cocoa/tabs/tab_controller.h
@@ -70,6 +70,7 @@ class MenuDelegate;
 @property(assign, nonatomic) BOOL app;
 @property(assign, nonatomic) BOOL mini;
 @property(assign, nonatomic) BOOL pinned;
+@property(assign, nonatomic) NSString* toolTip;
 // Note that |-selected| will return YES if the controller is |-active|, too.
 // |-setSelected:| affects the selection, while |-setActive:| affects the key
 // status/focus of the content.
@@ -116,7 +117,6 @@ class MenuDelegate;
 @end
 
 @interface TabController(TestingAPI)
-- (NSString*)toolTip;
 - (int)iconCapacity;
 - (BOOL)shouldShowIcon;
 - (BOOL)shouldShowMediaIndicator;
