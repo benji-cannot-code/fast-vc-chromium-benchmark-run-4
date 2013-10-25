965b17555e27c0bffc1027b9010a69a6de17e4d4
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-25 13:53:24 +0000

    DevTools: fix upstream test data for dowstream tests.
    TBR=vsevik
    
    Review URL: https://codereview.chromium.org/44353002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160574 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/Tests.js
+++ third_party/WebKit/Source/devtools/front_end/Tests.js
@@ -187,7 +187,7 @@ TestSuite.prototype.runTest = function(testName)
 TestSuite.prototype.showPanel = function(panelName)
 {
     // Open Scripts panel.
-    var toolbar = document.getElementById("toolbar");
+    var toolbar = document.querySelector(".toolbar");
     var button = toolbar.getElementsByClassName(panelName)[0];
     button.click();
     this.assertEquals(WebInspector.panels[panelName], WebInspector.inspectorView.currentPanel());
