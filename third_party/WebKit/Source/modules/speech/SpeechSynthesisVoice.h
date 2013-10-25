e6b25383a468def3a629078a129d88f1db384dcc
ch.dumez@samsung.com <ch.dumez@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-25 20:17:47 +0000

    Remove SpeechSynthesisVoice's virtual destructor
    Remove SpeechSynthesisVoice's destructor as it was empty and marked as virtual.
    This avoids creating an unnecessary vtable as this class is never subclassed,
    nor does it have any virtual function.
    
    BUG=311561
    
    Review URL: https://codereview.chromium.org/45053002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160620 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/speech/SpeechSynthesisVoice.h
+++ third_party/WebKit/Source/modules/speech/SpeechSynthesisVoice.h
@@ -37,7 +37,6 @@ namespace WebCore {
 
 class SpeechSynthesisVoice : public ScriptWrappable, public RefCounted<SpeechSynthesisVoice> {
 public:
-    virtual ~SpeechSynthesisVoice() { }
     static PassRefPtr<SpeechSynthesisVoice> create(PassRefPtr<PlatformSpeechSynthesisVoice>);
 
     const String& voiceURI() const { return m_platformVoice->voiceURI(); }
