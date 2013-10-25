9f9a3cf6db8c95a8cba8a0ec14b6a7717230d396
lambroslambrou@chromium.org <lambroslambrou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-25 16:50:20 +0000

    Cursor-tracking in Android Chromoting client.
    This is a first step towards implementing the new gestures:
    * Single-finger swipe moves the cursor (and pans the image in the
    opposite direction).
    * Single-finger tap injects left-click.
    * Two-finger tap injects right-click.
    * Two-finger pinch to zoom (as before)
    * Three-finger tap to show the action-bar (as before)
    
    There are some regressions (to keep this CL simple) but I plan to
    address these in follow-up CLs:
    
    * No constraints on image-positioning or zoom-level.
    * Cannot do left-button dragging.
    * Cannot send middle-button press.
    * Removed minimum-scroll constraint, as that makes it difficult to
    control the cursor precisely.
    
    BUG=270348
    
    Review URL: https://codereview.chromium.org/26336004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231042 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/android/java/src/org/chromium/chromoting/Desktop.java
+++ remoting/android/java/src/org/chromium/chromoting/Desktop.java
@@ -43,7 +43,7 @@ public class Desktop extends Activity {
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
-        remoteHostDesktop.requestRecheckConstrainingDimension();
+        remoteHostDesktop.onScreenConfigurationChanged();
     }
 
     /** Called to initialize the action bar. */
