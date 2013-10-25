ad6fbce348de38697355ea8d58c54533bda3c52c
sbc@chromium.org <sbc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-25 23:35:38 +0000

    [NaCl SDK] Add lua demo to appengine site.
    I pushed an initial version of lua here:
    http://gsdview.appspot.com/gonacl/demos/continuous/231022/lua/
    
    But the static content is not yet pushed to appengine
    pending the ability to push without clobbering what is
    there.
    
    R=binji@chromium.org
    
    Review URL: https://codereview.chromium.org/45223002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231150 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ native_client_sdk/src/gonacl_appengine/static/pnacl-demo-lua/lua.js
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/*
+ * Copyright (c) 2013 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+NaClTerm.prefix = 'lua'
+NaClTerm.nmf = 'http://commondatastorage.googleapis.com/gonacl/demos/publish/231022/lua/lua.nmf'
