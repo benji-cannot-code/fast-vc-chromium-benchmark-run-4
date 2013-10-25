bbb53ba36d4ee1ded907cb4c8f6cc897698ed54f
dongseong.hwang@intel.com <dongseong.hwang@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-25 21:33:32 +0000

    Improve HTMLCanvasElement::convertLogicalToDevice().
    HTMLCanvasElement::convertLogicalToDevice() uses IntSize instead of FloatSize.
    
    Unused following methods are removed:
    1. FloatRect convertLogicalToDevice(const FloatRect&) const;
    2. FloatSize convertDeviceToLogical(const FloatSize&) const;
    
    Review URL: https://codereview.chromium.org/43873003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160627 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLCanvasElement.h
+++ third_party/WebKit/Source/core/html/HTMLCanvasElement.h
@@ -112,11 +112,6 @@ public:
     void makePresentationCopy();
     void clearPresentationCopy();
 
-    FloatRect convertLogicalToDevice(const FloatRect&) const;
-    FloatSize convertLogicalToDevice(const FloatSize&) const;
-
-    FloatSize convertDeviceToLogical(const FloatSize&) const;
-
     SecurityOrigin* securityOrigin() const;
     void setOriginTainted() { m_originClean = false; }
     bool originClean() const { return m_originClean; }
@@ -148,6 +143,7 @@ private:
     void clearImageBuffer();
 
     void setSurfaceSize(const IntSize&);
+    IntSize convertLogicalToDevice(const IntSize&) const;
 
     bool paintsIntoCanvasBuffer() const;
 
