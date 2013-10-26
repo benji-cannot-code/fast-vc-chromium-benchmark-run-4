7a973b40247d8c058ac6469485e73d9e1692c106
dmazzoni@chromium.org <dmazzoni@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-26 00:01:54 +0000

    Clean up accessibility layout tests!
    The end result here is 26 lines removed from TestExpectations,
    only two accessibility tests remain.
    
    This change removes unimplemented bindings from
    testing/runner/WebAXProxy (formerly AccessibilityUIElement),
    and deletes 15 tests that used those bindings. They were
    either hard to implement, poorly designed, or too Mac-centric.
    
    It also fixes one binding, and fixes a bunch of other tests
    that just needed small tweaks.
    
    (NOTE: many of these tests were deleted before this change
    could be landed. This change now adds them back.)
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/31223003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160643 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/testing/runner/WebAXObjectProxy.cpp
+++ third_party/WebKit/Source/testing/runner/WebAXObjectProxy.cpp
@@ -526,28 +526,17 @@ WebAXObjectProxy::WebAXObjectProxy(const WebAXObject& object, Factory* factory)
     //
 
     bindMethod("allAttributes", &WebAXObjectProxy::allAttributesCallback);
-    bindMethod("attributesOfLinkedUIElements", &WebAXObjectProxy::attributesOfLinkedUIElementsCallback);
-    bindMethod("attributesOfDocumentLinks", &WebAXObjectProxy::attributesOfDocumentLinksCallback);
     bindMethod("attributesOfChildren", &WebAXObjectProxy::attributesOfChildrenCallback);
     bindMethod("lineForIndex", &WebAXObjectProxy::lineForIndexCallback);
     bindMethod("boundsForRange", &WebAXObjectProxy::boundsForRangeCallback);
-    bindMethod("stringForRange", &WebAXObjectProxy::stringForRangeCallback);
     bindMethod("childAtIndex", &WebAXObjectProxy::childAtIndexCallback);
     bindMethod("elementAtPoint", &WebAXObjectProxy::elementAtPointCallback);
-    bindMethod("attributesOfColumnHeaders", &WebAXObjectProxy::attributesOfColumnHeadersCallback);
-    bindMethod("attributesOfRowHeaders", &WebAXObjectProxy::attributesOfRowHeadersCallback);
-    bindMethod("attributesOfColumns", &WebAXObjectProxy::attributesOfColumnsCallback);
-    bindMethod("attributesOfRows", &WebAXObjectProxy::attributesOfRowsCallback);
-    bindMethod("attributesOfVisibleCells", &WebAXObjectProxy::attributesOfVisibleCellsCallback);
-    bindMethod("attributesOfHeader", &WebAXObjectProxy::attributesOfHeaderCallback);
     bindMethod("tableHeader", &WebAXObjectProxy::tableHeaderCallback);
-    bindMethod("indexInTable", &WebAXObjectProxy::indexInTableCallback);
     bindMethod("rowIndexRange", &WebAXObjectProxy::rowIndexRangeCallback);
     bindMethod("columnIndexRange", &WebAXObjectProxy::columnIndexRangeCallback);
     bindMethod("cellForColumnAndRow", &WebAXObjectProxy::cellForColumnAndRowCallback);
     bindMethod("titleUIElement", &WebAXObjectProxy::titleUIElementCallback);
     bindMethod("setSelectedTextRange", &WebAXObjectProxy::setSelectedTextRangeCallback);
-    bindMethod("attributeValue", &WebAXObjectProxy::attributeValueCallback);
     bindMethod("isAttributeSettable", &WebAXObjectProxy::isAttributeSettableCallback);
     bindMethod("isPressActionSupported", &WebAXObjectProxy::isPressActionSupportedCallback);
     bindMethod("isIncrementActionSupported", &WebAXObjectProxy::isIncrementActionSupportedCallback);
@@ -813,16 +802,6 @@ void WebAXObjectProxy::allAttributesCallback(const CppArgumentList&, CppVariant*
     result->set(getAttributes(accessibilityObject()));
 }
 
-void WebAXObjectProxy::attributesOfLinkedUIElementsCallback(const CppArgumentList&, CppVariant* result)
-{
-    result->setNull();
-}
-
-void WebAXObjectProxy::attributesOfDocumentLinksCallback(const CppArgumentList&, CppVariant* result)
-{
-    result->setNull();
-}
-
 void WebAXObjectProxy::attributesOfChildrenCallback(const CppArgumentList& arguments, CppVariant* result)
 {
     AttributesCollector collector;
@@ -830,11 +809,6 @@ void WebAXObjectProxy::attributesOfChildrenCallback(const CppArgumentList& argum
     for (unsigned i = 0; i < size; ++i)
         collector.collectAttributes(accessibilityObject().childAt(i));
     result->set(collector.attributes());
-}
-
-void WebAXObjectProxy::parametrizedAttributeNamesCallback(const CppArgumentList&, CppVariant* result)
-{
-    result->setNull();
 }
 
 void WebAXObjectProxy::lineForIndexCallback(const CppArgumentList& arguments, CppVariant* result)
@@ -887,11 +861,6 @@ void WebAXObjectProxy::boundsForRangeCallback(const CppArgumentList& arguments, 
     result->set(string(buffer));
 }
 
-void WebAXObjectProxy::stringForRangeCallback(const CppArgumentList&, CppVariant* result)
-{
-    result->setNull();
-}
-
 void WebAXObjectProxy::childAtIndexCallback(const CppArgumentList& arguments, CppVariant* result)
 {
     if (!arguments.size() || !arguments[0].isNumber()) {
@@ -925,36 +894,6 @@ void WebAXObjectProxy::elementAtPointCallback(const CppArgumentList& arguments, 
     result->set(*(m_factory->getOrCreate(obj)->getAsCppVariant()));
 }
 
-void WebAXObjectProxy::attributesOfColumnHeadersCallback(const CppArgumentList&, CppVariant* result)
-{
-    result->setNull();
-}
-
-void WebAXObjectProxy::attributesOfRowHeadersCallback(const CppArgumentList&, CppVariant* result)
-{
-    result->setNull();
-}
-
-void WebAXObjectProxy::attributesOfColumnsCallback(const CppArgumentList&, CppVariant* result)
-{
-    result->setNull();
-}
-
-void WebAXObjectProxy::attributesOfRowsCallback(const CppArgumentList&, CppVariant* result)
-{
-    result->setNull();
-}
-
-void WebAXObjectProxy::attributesOfVisibleCellsCallback(const CppArgumentList&, CppVariant* result)
-{
-    result->setNull();
-}
-
-void WebAXObjectProxy::attributesOfHeaderCallback(const CppArgumentList&, CppVariant* result)
-{
-    result->setNull();
-}
-
 void WebAXObjectProxy::tableHeaderCallback(const CppArgumentList&, CppVariant* result)
 {
     WebAXObject obj = accessibilityObject().headerContainerObject();
@@ -964,11 +903,6 @@ void WebAXObjectProxy::tableHeaderCallback(const CppArgumentList&, CppVariant* r
     }
 
     result->set(*(m_factory->getOrCreate(obj)->getAsCppVariant()));
-}
-
-void WebAXObjectProxy::indexInTableCallback(const CppArgumentList&, CppVariant* result)
-{
-    result->setNull();
 }
 
 void WebAXObjectProxy::rowIndexRangeCallback(const CppArgumentList&, CppVariant* result)
@@ -1028,11 +962,6 @@ void WebAXObjectProxy::setSelectedTextRangeCallback(const CppArgumentList&argume
     accessibilityObject().setSelectedTextRange(selectionStart, selectionEnd);
 }
 
-void WebAXObjectProxy::attributeValueCallback(const CppArgumentList&, CppVariant* result)
-{
-    result->setNull();
-}
-
 void WebAXObjectProxy::isAttributeSettableCallback(const CppArgumentList& arguments, CppVariant* result)
 {
     if (arguments.size() < 1 && !arguments[0].isString()) {
@@ -1064,7 +993,10 @@ void WebAXObjectProxy::isDecrementActionSupportedCallback(const CppArgumentList&
 
 void WebAXObjectProxy::parentElementCallback(const CppArgumentList&, CppVariant* result)
 {
-    WebAXObjectProxy* parent = m_factory->getOrCreate(accessibilityObject().parentObject());
+    WebAXObject parentObject = accessibilityObject().parentObject();
+    while (parentObject.accessibilityIsIgnored())
+        parentObject = parentObject.parentObject();
+    WebAXObjectProxy* parent = m_factory->getOrCreate(parentObject);
     if (!parent) {
         result->setNull();
         return;
@@ -1204,7 +1136,6 @@ void WebAXObjectProxy::wordEndCallback(const CppArgumentList& arguments, CppVari
 
 void WebAXObjectProxy::fallbackCallback(const CppArgumentList &, CppVariant* result)
 {
-    // FIXME: Implement this.
     result->setNull();
 }
 
