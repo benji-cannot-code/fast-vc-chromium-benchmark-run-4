f2c8bdbf5c69e95480fed5fbd0da08fcb781e9e3
urvang@google.com <urvang@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-26 00:23:18 +0000

    WEBPImageDecoder: Fix for ASSERTION FAILED: width == frameRect.width()
    If all data of a frame is received, but WebPIUpdate returns VP8_STATUS_SUSPENDED, then it's an invalid image.
    
    BUG=310259
    R=pkasting@chromium.org
    
    Review URL: https://codereview.chromium.org/36363002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160644 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/image-decoders/webp/WEBPImageDecoder.cpp
+++ third_party/WebKit/Source/core/platform/image-decoders/webp/WEBPImageDecoder.cpp
@@ -140,6 +140,9 @@ ImageFrame* WEBPImageDecoder::frameBufferAtIndex(size_t index)
             PlatformInstrumentation::didDecodeImage();
             WebPDemuxReleaseIterator(&webpFrame);
 
+            if (failed())
+                return 0;
+
             // We need more data to continue decoding.
             if (m_frameBufferCache[frameIndex].status() != ImageFrame::FrameComplete)
                 break;
@@ -157,7 +160,8 @@ ImageFrame* WEBPImageDecoder::frameBufferAtIndex(size_t index)
     PlatformInstrumentation::willDecodeImage("WEBP");
     decode(reinterpret_cast<const uint8_t*>(m_data->data()), m_data->size(), false, index);
     PlatformInstrumentation::didDecodeImage();
-    return &frame;
+
+    return failed() ? 0 : &frame;
 }
 
 void WEBPImageDecoder::setData(SharedBuffer* data, bool allDataReceived)
@@ -529,8 +533,11 @@ bool WEBPImageDecoder::decode(const uint8_t* dataBytes, size_t dataSize, bool on
         clearDecoder();
         return true;
     case VP8_STATUS_SUSPENDED:
-        applyPostProcessing(frameIndex);
-        return false;
+        if (!isAllDataReceived() && !frameIsCompleteAtIndex(frameIndex)) {
+            applyPostProcessing(frameIndex);
+            return false;
+        }
+        // FALLTHROUGH
     default:
         clear();
         return setFailed();
