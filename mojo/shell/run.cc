0f7f1b3be9c349671a7d68bca3e7da3a9549d029
abarth@chromium.org <abarth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-26 12:44:33 +0000

    mojo_shell should start without crashing on Android
    This CL finishes the bootup process for mojo_shell on Android. We now succeed
    in starting all the threads we expect. Instead of crashing, we now error out
    because there's no --app command line argument. I've also wired up logging to
    the Android system log.
    
    In order to initialize |net| properly, I've introduced a direct dependency from
    mojo/shell to net. We're considering doing that in another CL, and I now think
    having that dependency will make things easier, at least in the near term.
    
    R=darin@chromium.org, aa@chromium.org
    
    Review URL: https://codereview.chromium.org/45383002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231201 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/shell/run.cc
+++ mojo/shell/run.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "mojo/shell/run.h"
+
 #include "base/command_line.h"
 #include "base/files/file_path.h"
 #include "base/logging.h"
@@ -20,10 +22,6 @@ namespace shell {
 
 void Run() {
   system::CoreImpl::Init();
-
-#if defined(OS_ANDROID)
-  return;  // TODO(abarth): Run more of RunShell on Android.
-#endif
 
   // TODO(abarth): Group these objects into a "context" object.
   TaskRunners task_runners(base::MessageLoop::current()->message_loop_proxy());
