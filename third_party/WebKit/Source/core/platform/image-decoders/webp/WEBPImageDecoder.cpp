3452bcc91773b98f1da8de48b4241edbf28bfa33
falken@chromium.org <falken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-27 03:04:15 +0000

    Revert 160644 "WEBPImageDecoder: Fix for ASSERTION FAILED: width..."
    Reverting because the Blink rolls are failing the CQ on
    android_dbg_triggered_tests on webkit_unit_tests for
    AnimatedWebPTests.truncatedInBetweenFrame
    
    > WEBPImageDecoder: Fix for ASSERTION FAILED: width == frameRect.width()
    > 
    > If all data of a frame is received, but WebPIUpdate returns VP8_STATUS_SUSPENDED, then it's an invalid image.
    > 
    > BUG=310259
    > R=pkasting@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/36363002
    
    TBR=urvang@google.com
    
    Review URL: https://codereview.chromium.org/47373002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160666 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/image-decoders/webp/WEBPImageDecoder.cpp
+++ third_party/WebKit/Source/core/platform/image-decoders/webp/WEBPImageDecoder.cpp
@@ -140,9 +140,6 @@ ImageFrame* WEBPImageDecoder::frameBufferAtIndex(size_t index)
             PlatformInstrumentation::didDecodeImage();
             WebPDemuxReleaseIterator(&webpFrame);
 
-            if (failed())
-                return 0;
-
             // We need more data to continue decoding.
             if (m_frameBufferCache[frameIndex].status() != ImageFrame::FrameComplete)
                 break;
@@ -160,8 +157,7 @@ ImageFrame* WEBPImageDecoder::frameBufferAtIndex(size_t index)
     PlatformInstrumentation::willDecodeImage("WEBP");
     decode(reinterpret_cast<const uint8_t*>(m_data->data()), m_data->size(), false, index);
     PlatformInstrumentation::didDecodeImage();
-
-    return failed() ? 0 : &frame;
+    return &frame;
 }
 
 void WEBPImageDecoder::setData(SharedBuffer* data, bool allDataReceived)
@@ -533,11 +529,8 @@ bool WEBPImageDecoder::decode(const uint8_t* dataBytes, size_t dataSize, bool on
         clearDecoder();
         return true;
     case VP8_STATUS_SUSPENDED:
-        if (!isAllDataReceived() && !frameIsCompleteAtIndex(frameIndex)) {
-            applyPostProcessing(frameIndex);
-            return false;
-        }
-        // FALLTHROUGH
+        applyPostProcessing(frameIndex);
+        return false;
     default:
         clear();
         return setFailed();
