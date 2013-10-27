2be96861e92abd7d0cb94d7fd6fc883408390ef7
ddorwin@chromium.org <ddorwin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-27 02:44:19 +0000

    Remove libc version checks from CDM tests.
    All of the official bots and developers should be upgraded to Precise now, so
    we don't need these checks in the tests.
    We continue to check in the production code.
    
    TEST=Tests continue to pass on official bots.
    
    Review URL: https://codereview.chromium.org/45633003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231237 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media/encrypted_media_istypesupported_browsertest.cc
+++ chrome/browser/media/encrypted_media_istypesupported_browsertest.cc
@@ -22,11 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "widevine_cdm_version.h"  // In SHARED_INTERMEDIATE_DIR.
 
-#if defined(WIDEVINE_CDM_AVAILABLE) && \
-    defined(OS_LINUX) && !defined(OS_CHROMEOS)
-#include <gnu/libc-version.h>
-#endif
-
 #if defined(USE_PROPRIETARY_CODECS)
 #define EXPECT_PROPRIETARY EXPECT_TRUE
 #else
@@ -44,13 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Expectations for Widevine.
 #if defined(WIDEVINE_CDM_AVAILABLE)
-#if defined(OS_LINUX) && !defined(OS_CHROMEOS)
-// TODO(ddorwin): Remove after bots switch to Precise.
-#define EXPECT_WV(a) \
-    EXPECT_EQ((std::string(gnu_get_libc_version()) != "2.11.1"), (a))
-#else
 #define EXPECT_WV EXPECT_TRUE
-#endif  // defined(OS_LINUX) && !defined(OS_CHROMEOS)
 
 #if defined(WIDEVINE_CDM_AVC1_SUPPORT_AVAILABLE)
 #define EXPECT_WVAVC1 EXPECT_TRUE
