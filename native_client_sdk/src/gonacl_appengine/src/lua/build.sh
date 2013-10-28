f7c9f2fc829114c549dde442bee4e5fa8c22dc34
sbc@chromium.org <sbc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-28 15:59:21 +0000

    [NaCl SDK] Fix buiding of gonacl lua demo
    TBR=binji
    
    Review URL: https://codereview.chromium.org/47883002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231335 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/gonacl_appengine/src/lua/build.sh
+++ native_client_sdk/src/gonacl_appengine/src/lua/build.sh
@@ -6,14 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 set -o nounset
 set -o errexit
+set -o xtrace
 
 SCRIPT_DIR="$(cd $(dirname $0) && pwd)"
-
 cd ${SCRIPT_DIR}
 
 OUT_DIR=out
-NACLPORTS_URL=https://chromium.googlesource.com/external/naclports.git
-NACLPORTS_REV=58a6ab9
+NACLPORTS_URL=http://naclports.googlecode.com/svn/trunk/src
+NACLPORTS_REV=939
 NACLPORTS_DIR=${OUT_DIR}/naclports
 
 if [ -z "${NACL_SDK_ROOT:-}" ]; then
@@ -63,10 +63,12 @@ if [ ! -d ${NACLPORTS_DIR} ]; then
   popd
 fi
 
-pushd ${NACLPORTS_DIR}/src
+pushd ${NACLPORTS_DIR}
 gclient sync -r ${NACLPORTS_REV}
+popd
 
 Banner Building lua
+pushd ${NACLPORTS_DIR}/src
 make NACL_ARCH=pnacl lua_ppapi
 popd
 
