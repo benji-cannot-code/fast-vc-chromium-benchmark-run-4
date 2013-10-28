19a24235cd545cc61f3edc27c6fa208e87b28c1f
aandrey@chromium.org <aandrey@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-28 11:25:15 +0000

    DevTools: Do not include thrown property descriptors in object preview.
    Minor clean-up.
    
    BUG=
    R=pfeldman
    
    Review URL: https://codereview.chromium.org/40093002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160715 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InjectedScriptSource.js
+++ third_party/WebKit/Source/core/inspector/InjectedScriptSource.js
@@ -409,9 +409,10 @@ InjectedScript.prototype = {
         var nameProcessed = { __proto__: null };
 
         /**
+         * @param {Object} o
          * @param {Array.<string>} names
          */
-        function process(names)
+        function process(o, names)
         {
             for (var i = 0; i < names.length; ++i) {
                 var name = names[i];
@@ -455,8 +456,8 @@ InjectedScript.prototype = {
 
         for (var o = object; this._isDefined(o); o = o.__proto__) {
             // First call Object.keys() to enforce ordering of the property descriptors.
-            process(Object.keys(/** @type {!Object} */ (o)));
-            process(Object.getOwnPropertyNames(/** @type {!Object} */ (o)));
+            process(o, Object.keys(/** @type {!Object} */ (o)));
+            process(o, Object.getOwnPropertyNames(/** @type {!Object} */ (o)));
 
             if (ownProperties) {
                 if (object.__proto__ && !accessorPropertiesOnly)
@@ -995,6 +996,10 @@ InjectedScript.RemoteObject.prototype = {
                 var descriptor = descriptors[i];
                 if (!descriptor)
                     continue;
+                if (descriptor.wasThrown) {
+                    preview.lossless = false;
+                    continue;
+                }
                 if (!descriptor.enumerable && !descriptor.isOwn)
                     continue;
 
