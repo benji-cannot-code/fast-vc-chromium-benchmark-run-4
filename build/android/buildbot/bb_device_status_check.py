0a07a7f8af7425e3c409d43150d70af6ec3dabf2
navabi@google.com <navabi@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-28 11:33:06 +0000

    Add sleep after restarting usb ports and before checking for devices.
    Need to wait a couple seconds after restarting usb before doing 'adb devices',
    or the devices do not show up.
    
    E.g. https://chromegw.corp.google.com/i/clank/builders/s3-sharded-official-perf-
    noflash-clankium/builds/3572/steps/device_status_check/logs/stdio
    
    BUG=299891
    TBR=ilevy@chromium.org
    
    Review URL: https://codereview.chromium.org/38483002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231310 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/buildbot/bb_device_status_check.py
+++ build/android/buildbot/bb_device_status_check.py
@@ -10,11 +10,12 @@ import logging
 import optparse
 import os
 import psutil
+import re
 import signal
 import smtplib
 import subprocess
 import sys
-import re
+import time
 import urllib
 
 import bb_annotations
@@ -112,6 +113,26 @@ def DeviceInfo(serial, options):
   return device_type, device_build, battery_level, full_report, errors, True
 
 
+def GetLastDevices(out_dir):
+  """Returns a list of devices that have been seen on the bot.
+
+  Args:
+    options: out_dir parameter of options argument is used as the base
+             directory to load and update the cache file.
+
+  Returns: List of device serial numbers that were on the bot.
+  """
+  devices_path = os.path.join(out_dir, '.last_devices')
+  devices = []
+  try:
+    with open(devices_path) as f:
+      devices = f.read().splitlines()
+  except IOError:
+    # Ignore error, file might not exist
+    pass
+  return devices
+
+
 def CheckForMissingDevices(options, adb_online_devs):
   """Uses file of previous online devices to detect broken phones.
 
@@ -128,17 +149,6 @@ def CheckForMissingDevices(options, adb_online_devs):
 
   out_dir = os.path.abspath(options.out_dir)
 
-  def ReadDeviceList(file_name):
-    devices_path = os.path.join(out_dir, file_name)
-    devices = []
-    try:
-      with open(devices_path) as f:
-        devices = f.read().splitlines()
-    except IOError:
-      # Ignore error, file might not exist
-      pass
-    return devices
-
   def WriteDeviceList(file_name, device_list):
     path = os.path.join(out_dir, file_name)
     if not os.path.exists(out_dir):
@@ -148,7 +158,7 @@ def CheckForMissingDevices(options, adb_online_devs):
       f.write('\n'.join(set(device_list)))
 
   last_devices_path = os.path.join(out_dir, '.last_devices')
-  last_devices = ReadDeviceList('.last_devices')
+  last_devices = GetLastDevices(out_dir)
   missing_devs = list(set(last_devices) - set(adb_online_devs))
 
   all_known_devices = list(set(adb_online_devs) | set(last_devices))
@@ -278,10 +288,20 @@ def main():
     parser.error('Unknown options %s' % args)
 
   if options.restart_usb:
-    KillAllAdb()
-    rc = RestartUsb()
-    if rc:
-      return 1
+    expected_devices = GetLastDevices(os.path.abspath(options.out_dir))
+    devices = android_commands.GetAttachedDevices()
+    # Only restart usb if devices are missing
+    if set(expected_devices) != set(devices):
+      KillAllAdb()
+      if RestartUsb():
+        return 1
+      retries = 5
+      while retries:
+        time.sleep(1)
+        devices = android_commands.GetAttachedDevices()
+        if set(expected_devices) == set(devices):
+          break
+        retries -= 1
 
   devices = android_commands.GetAttachedDevices()
   # TODO(navabi): Test to make sure this fails and then fix call
