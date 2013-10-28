24d1525efe93d1fb15393f236ed9e7bc3482d0aa
tomhudson@chromium.org <tomhudson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-28 12:17:10 +0000

    Remember value of IsImplSidePaintingEnabled
    Command line arguments are constant at runtime, but the function
    cc::switches::IsImplSidePaintingEnabled() does two searches through
    the list of arguments every time it's called, and it's called every
    time we record a SkPicture.
    
    With this patch, the function remembers the value determined by the
    first call and avoids repeating the searches on subsequent calls.
    
    BUG=311248
    R=nduca@chromium.org,skyostil@chromium.org
    
    Review URL: https://codereview.chromium.org/43463003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231314 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/base/switches.cc
+++ cc/base/switches.cc
@@ -163,7 +163,8 @@ bool IsLCDTextEnabled() {
 #endif
 }
 
-bool IsImplSidePaintingEnabled() {
+namespace {
+bool CheckImplSidePaintingStatus() {
   const CommandLine& command_line = *CommandLine::ForCurrentProcess();
 
   if (command_line.HasSwitch(cc::switches::kDisableImplSidePainting))
@@ -176,6 +177,12 @@ bool IsImplSidePaintingEnabled() {
 #else
   return false;
 #endif
+}
+}  // namespace
+
+bool IsImplSidePaintingEnabled() {
+  static bool enabled = CheckImplSidePaintingStatus();
+  return enabled;
 }
 
 bool IsMapImageEnabled() {
