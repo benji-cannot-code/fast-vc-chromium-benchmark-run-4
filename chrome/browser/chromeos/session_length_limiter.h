9d525faf4b35ad4ed74740a735716da00a4016e4
bartfab@chromium.org <bartfab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-28 15:38:09 +0000

    Add WaitForInitialUserActivity policy
    This CL adds a user policy that controls whether power management delays
    and the session length limit should only start running after initial user
    activity in a session. The SessionLengthLimiter is modified to obey the
    new policy. Power management will hooked up to the new policy in a
    follow-up CL.
    
    It would technically be possible to add separate policies that determine
    whether power management delays on the one hand and the session length
    limit on the other hand should wait for initial user activity. However,
    it does not make sense for one of these to start running on session start
    while the other waits for user activity. Therefore, a single policy is
    added for both.
    
    BUG=310176
    TEST=Updated unit and browser tests
    
    Review URL: https://codereview.chromium.org/35943002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231332 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/session_length_limiter.h
+++ chrome/browser/chromeos/session_length_limiter.h
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CHROMEOS_SESSION_LENGTH_LIMITER_H_
 #define CHROME_BROWSER_CHROMEOS_SESSION_LENGTH_LIMITER_H_
 
+#include "ash/wm/user_activity_observer.h"
 #include "base/basictypes.h"
+#include "base/compiler_specific.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/prefs/pref_change_registrar.h"
 #include "base/threading/thread_checker.h"
@@ -20,7 +22,7 @@ namespace chromeos {
 
 // Enforces a session length limit by terminating the session when the limit is
 // reached.
-class SessionLengthLimiter {
+class SessionLengthLimiter : public ash::UserActivityObserver {
  public:
   class Delegate {
    public:
@@ -34,10 +36,28 @@ class SessionLengthLimiter {
   static void RegisterPrefs(PrefRegistrySimple* registry);
 
   SessionLengthLimiter(Delegate* delegate, bool browser_restarted);
-  ~SessionLengthLimiter();
+  virtual ~SessionLengthLimiter();
+
+  // ash::UserActivityObserver:
+  virtual void OnUserActivity(const ui::Event* event) OVERRIDE;
 
  private:
-  void OnSessionLengthLimitChanged();
+  // Attempt to restore the session start time and the flag indicating user
+  // activity from local state. Return |true| if the restore is successful.
+  bool RestoreStateAfterCrash();
+
+  // Update the session start time if possible:
+  // * If instructed to wait for initial user activity, the session start time
+  //   advances every time this method is called as long as no user activity has
+  //   occurred yet. The time is not persisted in local state.
+  // * If instructed not to wait for initial user activity, the session start
+  //   time is set and persisted in local state the first time this method is
+  //   called.
+  // The pref indicating whether to wait for initial user activity may change at
+  // any time, switching between the two behaviors.
+  void UpdateSessionStartTime();
+
+  void UpdateLimit();
 
   base::ThreadChecker thread_checker_;
 
@@ -46,6 +66,7 @@ class SessionLengthLimiter {
 
   scoped_ptr<base::OneShotTimer<SessionLengthLimiter::Delegate> > timer_;
   base::TimeTicks session_start_time_;
+  bool user_activity_seen_;
 
   DISALLOW_COPY_AND_ASSIGN(SessionLengthLimiter);
 };
