ad848a7ff6231a09d77022c0dee4fe9a23a5b7b9
calamity@chromium.org <calamity@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-28 08:38:48 +0000

    Enable profile pinning by setting browser window relaunch details
    Make it possible to pin shortcuts by setting the relaunch data for each browser window.
    
    BUG=177490,107080,146647
    TEST=Create separate profiles and pin the taskbar icons. Clicking the pinned icons should relaunch chrome with the appropriate profile.
    
    TBR=cpu@chromium.org
    
    Review URL: https://codereview.chromium.org/14122006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231295 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/frame/browser_frame_win.h
+++ chrome/browser/ui/views/frame/browser_frame_win.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/native_widget_win.h"
 
 class BrowserView;
+class BrowserWindowPropertyManager;
 
 namespace views {
 class NativeMenuWin;
@@ -44,6 +45,7 @@ class BrowserFrameWin : public views::NativeWidgetWin,
   // Overridden from views::NativeWidgetWin:
   virtual int GetInitialShowState() const OVERRIDE;
   virtual bool GetClientAreaInsets(gfx::Insets* insets) const OVERRIDE;
+  virtual void HandleCreate() OVERRIDE;
   virtual void HandleFrameChanged() OVERRIDE;
   virtual bool PreHandleMSG(UINT message,
                             WPARAM w_param,
@@ -101,6 +103,8 @@ class BrowserFrameWin : public views::NativeWidgetWin,
   scoped_ptr<views::NativeMenuWin> system_menu_;
 
   MinimizeButtonMetrics minimize_button_metrics_;
+
+  scoped_ptr<BrowserWindowPropertyManager> browser_window_property_manager_;
 
   DISALLOW_COPY_AND_ASSIGN(BrowserFrameWin);
 };
