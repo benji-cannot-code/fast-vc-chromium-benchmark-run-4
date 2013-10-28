9ef8b6a26a57cff7ac7264cb4c1e42e7777b435d
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-28 15:36:26 +0000

    DevTools: Create PowerSaveBlocker only on Android
    BUG=311608
    
    Review URL: https://codereview.chromium.org/46933002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231331 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/devtools/render_view_devtools_agent_host.cc
+++ content/browser/devtools/render_view_devtools_agent_host.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/devtools/devtools_protocol_constants.h"
 #include "content/browser/devtools/devtools_tracing_handler.h"
 #include "content/browser/devtools/renderer_overrides_handler.h"
-#include "content/browser/power_save_blocker_impl.h"
 #include "content/browser/renderer_host/render_process_host_impl.h"
 #include "content/browser/renderer_host/render_view_host_impl.h"
 #include "content/browser/site_instance_impl.h"
@@ -26,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_widget_host_iterator.h"
 
 #if defined(OS_ANDROID)
+#include "content/browser/power_save_blocker_impl.h"
 #include "content/public/browser/render_widget_host_view.h"
 #endif
 
@@ -177,12 +177,12 @@ void RenderViewDevToolsAgentHost::OnClientAttached() {
   // ExtensionProcessManager no longer relies on this notification.
   DevToolsManagerImpl::GetInstance()->NotifyObservers(this, true);
 
+#if defined(OS_ANDROID)
   power_save_blocker_.reset(
       static_cast<PowerSaveBlockerImpl*>(
           PowerSaveBlocker::Create(
               PowerSaveBlocker::kPowerSaveBlockPreventDisplaySleep,
               "DevTools").release()));
-#if defined(OS_ANDROID)
   if (render_view_host_->GetView()) {
     power_save_blocker_.get()->
         InitDisplaySleepBlocker(render_view_host_->GetView()->GetNativeView());
@@ -191,7 +191,9 @@ void RenderViewDevToolsAgentHost::OnClientAttached() {
 }
 
 void RenderViewDevToolsAgentHost::OnClientDetached() {
+#if defined(OS_ANDROID)
   power_save_blocker_.reset();
+#endif
   overrides_handler_->OnClientDetached();
   ClientDetachedFromRenderer();
 }
--- content/browser/devtools/render_view_devtools_agent_host.h
+++ content/browser/devtools/render_view_devtools_agent_host.h
@@ -20,9 +20,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 class DevToolsTracingHandler;
-class PowerSaveBlockerImpl;
 class RendererOverridesHandler;
 class RenderViewHost;
+
+#if defined(OS_ANDROID)
+class PowerSaveBlockerImpl;
+#endif
 
 class CONTENT_EXPORT RenderViewDevToolsAgentHost
     : public IPCDevToolsAgentHost,
@@ -80,7 +83,9 @@ class CONTENT_EXPORT RenderViewDevToolsAgentHost
   RenderViewHost* render_view_host_;
   scoped_ptr<RendererOverridesHandler> overrides_handler_;
   scoped_ptr<DevToolsTracingHandler> tracing_handler_;
+#if defined(OS_ANDROID)
   scoped_ptr<PowerSaveBlockerImpl> power_save_blocker_;
+#endif
   std::string state_;
   NotificationRegistrar registrar_;
 
