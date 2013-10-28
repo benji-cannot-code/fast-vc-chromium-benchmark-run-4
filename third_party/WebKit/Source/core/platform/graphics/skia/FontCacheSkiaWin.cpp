687e49933b3766bda661eac726750e8666b2054a
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-28 15:50:02 +0000

    Change fontContainsCharacter to use SkTypeface::charsToGlyphs
    Change FontCacheSkiaWin::fontContainsCharacter to use the SkTypeface
    charsToGlyphs API instead of the SkPaint textToGlyphs version.
    
    The SkTypeface one is faster and is what we use in GlyphPageTreeNode
    as of r160612.
    
    R=reed@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/46333002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160727 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/graphics/skia/FontCacheSkiaWin.cpp
+++ third_party/WebKit/Source/core/platform/graphics/skia/FontCacheSkiaWin.cpp
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "RuntimeEnabledFeatures.h"
 #include "SkFontMgr.h"
+#include "SkTypeface.h"
 #include "SkTypeface_win.h"
 #include "core/platform/graphics/Font.h"
 #include "core/platform/graphics/SimpleFontData.h"
@@ -58,16 +59,10 @@ FontCache::FontCache()
     m_fontManager = adoptPtr(fontManager);
 }
 
-
-static bool fontContainsCharacter(const FontPlatformData* fontData, const wchar_t* family, UChar32 character)
+static bool fontContainsCharacter(const FontPlatformData* platformData, const wchar_t* family, UChar32 character)
 {
-    SkPaint paint;
-    fontData->setupPaint(&paint);
-    paint.setTextEncoding(SkPaint::kUTF32_TextEncoding);
-
-    uint16_t glyph;
-    paint.textToGlyphs(&character, sizeof(character), &glyph);
-    return glyph != 0;
+    SkTypeface* typeface = platformData->typeface();
+    return typeface->charsToGlyphs(&character, SkTypeface::kUTF32_Encoding, 0 /* glyph */, 1 /* length */) == 1;
 }
 
 // Given the desired base font, this will create a SimpleFontData for a specific
