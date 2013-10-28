0bba876dee15164c164b8c2f8811f16e1840d6b4
nbarth@chromium.org <nbarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-28 04:16:18 +0000

    IDL compiler: Methods with basic type return type
    Mostly already implemented by type handling.
    
    BUG=239771
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/47983002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160694 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/templates/methods.cpp
+++ third_party/WebKit/Source/bindings/templates/methods.cpp
@@ -4,8 +4,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 static void {{method.name}}Method(const v8::FunctionCallbackInfo<v8::Value>& args)
 {
     {{cpp_class_name}}* imp = {{v8_class_name}}::toNative(args.Holder());
-    imp->{{method.name}}();
+    {% if method.idl_type == 'void' %}
+    {{method.cpp_value}};
 
+    {% else %}
+    {{method.v8_set_return_value}};
+    {% endif %}
     return;
 }
 {% endmacro %}
