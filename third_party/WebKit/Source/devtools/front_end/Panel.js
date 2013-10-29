70478c40f7655c7c6a29437a8e0af5ba422d49ac
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-29 15:55:49 +0000

    DevTools: Remove canShowAnchorLocation from Panel and let panel decide whether to show itself or not.
    R=pfeldman@chromium.org, pfeldman
    
    Review URL: https://codereview.chromium.org/50683003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160840 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/Panel.js
+++ third_party/WebKit/Source/devtools/front_end/Panel.js
@@ -222,16 +222,9 @@ WebInspector.Panel.prototype = {
      * @param {Element} anchor
      * @return {boolean}
      */
-    canShowAnchorLocation: function(anchor)
-    {
-        return false;
-    },
-
-    /**
-     * @param {Element} anchor
-     */
     showAnchorLocation: function(anchor)
     {
+        return false;
     },
 
     elementsToRestoreScrollPositionsFor: function()
