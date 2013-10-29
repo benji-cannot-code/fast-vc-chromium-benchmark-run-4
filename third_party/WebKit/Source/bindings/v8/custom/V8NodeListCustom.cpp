883b82da931849c5cd380a1df7e6b39cb35de89c
kouhei@chromium.org <kouhei@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-29 07:05:25 +0000

    Reland: Implement new Blink IDL attribute [SetReference]
    SetReference is a new Blink IDL attribute which can be used to specify dependency from a wrapper to another. This is implemented by calling v8::Isolate::SetReference on minor GC prologues. This has a lower overhead compared to old technique using SetHiddenValueTo.
    
    This patch also modifies DOMDataStore::Current to support call outside any active context.
    
    BUG=307341
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=159861
    
    Review URL: https://codereview.chromium.org/26792002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160800 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/custom/V8NodeListCustom.cpp
+++ third_party/WebKit/Source/bindings/v8/custom/V8NodeListCustom.cpp
@@ -42,15 +42,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-void* V8NodeList::opaqueRootForGC(void* object, v8::Isolate* isolate)
+void V8NodeList::resolveWrapperReachability(void* object, const v8::Persistent<v8::Object>& wrapper, v8::Isolate* isolate)
 {
     NodeList* impl = static_cast<NodeList*>(object);
-    if (!impl->isLiveNodeList())
-        return object;
+    if (!impl->isLiveNodeList()) {
+        setObjectGroup(object, wrapper, isolate);
+        return;
+    }
     Node* owner = static_cast<LiveNodeList*>(impl)->ownerNode();
-    if (!owner)
-        return object;
-    return V8GCController::opaqueRootForGC(owner, isolate);
+    if (!owner) {
+        setObjectGroup(object, wrapper, isolate);
+        return;
+    }
+
+    setObjectGroup(V8GCController::opaqueRootForGC(owner, isolate), wrapper, isolate);
 }
 
 } // namespace WebCore
