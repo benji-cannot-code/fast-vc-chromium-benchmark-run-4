175463f6d12786b90b50d8233bb55fe487decf5b
miguelg@chromium.org <miguelg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-29 16:51:30 +0000

    [Translate]Add Uma stats for language changes in linux and android
    BUG=311245
    
    Review URL: https://codereview.chromium.org/48673002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231582 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/infobars/after_translate_infobar_gtk.cc
+++ chrome/browser/ui/gtk/infobars/after_translate_infobar_gtk.cc
@@ -7,9 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/message_loop/message_loop.h"
+#include "base/metrics/histogram.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/translate/translate_infobar_delegate.h"
 #include "chrome/browser/ui/gtk/gtk_util.h"
+#include "components/translate/common/translate_metrics.h"
 #include "grit/generated_resources.h"
 #include "ui/base/gtk/gtk_hig_constants.h"
 #include "ui/base/gtk/gtk_signal_registrar.h"
@@ -91,11 +93,15 @@ bool AfterTranslateInfoBar::ShowOptionsMenuButton() const {
 }
 
 void AfterTranslateInfoBar::SetOriginalLanguage(size_t language_index) {
+  UMA_HISTOGRAM_BOOLEAN(
+      translate::GetMetricsName(translate::UMA_MODIFY_ORIGINAL_LANG), true);
   GetDelegate()->set_original_language_index(language_index);
   GetDelegate()->Translate();
 }
 
 void AfterTranslateInfoBar::SetTargetLanguage(size_t language_index) {
+  UMA_HISTOGRAM_BOOLEAN(
+      translate::GetMetricsName(translate::UMA_MODIFY_TARGET_LANG), true);
   GetDelegate()->set_target_language_index(language_index);
   GetDelegate()->Translate();
 }
