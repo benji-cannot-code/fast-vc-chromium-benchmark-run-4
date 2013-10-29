4f517b80d5a9fdd469d80bd56a32f620ea81ef46
alph@chromium.org <alph@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-29 16:00:56 +0000

    Pass timestamp into trace event callback.
    Review URL: https://codereview.chromium.org/47563004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231574 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/devtools/devtools_agent.h
+++ content/renderer/devtools/devtools_agent.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/atomicops.h"
 #include "base/basictypes.h"
+#include "base/time/time.h"
 #include "content/public/common/console_message_level.h"
 #include "content/public/renderer/render_view_observer.h"
 #include "third_party/WebKit/public/web/WebDevToolsAgentClient.h"
@@ -54,8 +55,14 @@ class DevToolsAgent : public RenderViewObserver,
   virtual void clearBrowserCache();
   virtual void clearBrowserCookies();
   virtual void visitAllocatedObjects(AllocatedObjectVisitor* visitor);
+
+  typedef void (*TraceEventCallback)(
+      char phase, const unsigned char*, const char* name, unsigned long long id,
+      int numArgs, const char* const* argNames, const unsigned char* argTypes,
+      const unsigned long long* argValues,
+      unsigned char flags, double timestamp);
   virtual void setTraceEventCallback(TraceEventCallback cb);
-  virtual void setTraceEventCallback(TraceEventWithTimestampCallback cb);
+
   virtual void enableDeviceEmulation(
       const WebKit::WebSize& device_size,
       const WebKit::WebRect& view_rect, float device_scale_factor,
@@ -73,6 +80,7 @@ class DevToolsAgent : public RenderViewObserver,
   void OnSetupDevToolsClient();
 
   static void TraceEventCallbackWrapper(
+      base::TimeTicks timestamp,
       char phase,
       const unsigned char* category_group_enabled,
       const char* name,
@@ -86,8 +94,7 @@ class DevToolsAgent : public RenderViewObserver,
   bool is_attached_;
   bool is_devtools_client_;
 
-  static base::subtle::AtomicWord
-      /* TraceEventWithTimestampCallback */ event_callback_;
+  static base::subtle::AtomicWord /* TraceEventCallback */ event_callback_;
 
   DISALLOW_COPY_AND_ASSIGN(DevToolsAgent);
 };
