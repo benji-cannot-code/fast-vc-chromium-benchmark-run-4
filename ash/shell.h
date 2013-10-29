d50b09207ada0d802efc3937d553ea05ba2d1dcd
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-29 01:07:15 +0000

    Fix a crash reported in ASH reported in the Chrome SyzyASAN canary.
    The crash occurs while executing the DisplayManager::CreateMirrorWindowIfAny task because the DisplayManager
    instance was destroyed.
    
    I was able to reproduce this by putting executing this task with a small delay and then closing ASH.
    
    Fix is to use a WeakPtrFactory for the DisplayManager::CreateMirrorWindowIfAny task
    
    Fixes bug https://code.google.com/p/chromium/issues/detail?id=309767
    
    BUG=309767
    R=sky@chromium.org, sky
    
    Review URL: https://codereview.chromium.org/46463002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231467 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/shell.h
+++ ash/shell.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/gtest_prod_util.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
 #include "ui/aura/client/activation_change_observer.h"
 #include "ui/base/ui_base_types.h"
@@ -646,6 +647,8 @@ class ASH_EXPORT Shell
   scoped_ptr<views::corewm::InputMethodEventFilter> input_method_filter_;
 
   scoped_ptr<internal::DisplayManager> display_manager_;
+  scoped_ptr<base::WeakPtrFactory<internal::DisplayManager> >
+      weak_display_manager_factory_;
 
   scoped_ptr<internal::LocaleNotificationController>
       locale_notification_controller_;
