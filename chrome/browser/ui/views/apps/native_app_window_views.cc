06cfd3bfa36f579aa65e730a97199e78778ca8a8
rharrison@chromium.org <rharrison@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-29 19:52:49 +0000

    Adjust app windows to play nice with NVHA fast resize CL
    This CL changes the test in
    NativeAppWindowViews::ShouldDescendIntoChildForEventHandling from being
    child is equal to child contains. This is done since the NVHA CL,
    https://codereview.chromium.org/48113013, introduces a window between
    the top level and the child to do clipping, so the old condition will
    never be satisfied. This has been broken out into its own CL to reduce
    the scope of the NVHA CL.
    
    BUG=282463
    BUG=312201
    BUG=311895
    TEST=Confirmed that the window app sample works correctly with and
         without NVHA CL.
    
    Review URL: https://codereview.chromium.org/48113014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231619 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/apps/native_app_window_views.cc
+++ chrome/browser/ui/views/apps/native_app_window_views.cc
@@ -53,6 +53,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window.h"
 #endif
 
+#if defined(USE_AURA)
+#include "ui/aura/window.h"
+#endif
+
 using apps::ShellWindow;
 
 namespace {
@@ -652,7 +656,7 @@ bool NativeAppWindowViews::ShouldDescendIntoChildForEventHandling(
     gfx::NativeView child,
     const gfx::Point& location) {
 #if defined(USE_AURA)
-  if (child == web_view_->web_contents()->GetView()->GetNativeView()) {
+  if (child->Contains(web_view_->web_contents()->GetView()->GetNativeView())) {
     // Shell window should claim mouse events that fall within the draggable
     // region.
     return !draggable_region_.get() ||
