6125339d84b8201c79e2dd01331979c00475aed5
davidben@chromium.org <davidben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-29 21:57:55 +0000

    Mac: delay window raising until space transitions.
    When manually raising browser windows in response to a dock activation that
    triggered a space transition, isOnActiveSpace is wrong and we focus the wrong
    space's windows. Detect this by seeing if the key window is off-screen. If so,
    delay the raise until the space change notification.
    
    BUG=309656
    TEST=see bug
    
    Review URL: https://codereview.chromium.org/37253004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231647 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/app_controller_mac.h
+++ chrome/browser/app_controller_mac.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/observer_list.h"
 #include "base/prefs/pref_change_registrar.h"
+#include "base/time/time.h"
 #include "ui/base/work_area_watcher_observer.h"
 
 class AppControllerProfileObserver;
@@ -80,6 +81,10 @@ class WorkAreaWatcherObserver;
 
   // Indicates wheter an NSPopover is currently being shown.
   BOOL hasPopover_;
+
+  // If we are expecting a workspace change in response to a reopen
+  // event, the time we got the event. A null time otherwise.
+  base::TimeTicks reopenTime_;
 
   // Observers that listen to the work area changes.
   ObserverList<ui::WorkAreaWatcherObserver> workAreaChangeObservers_;
