d94081aad773d5c70f5c39f58972fff73f82e70a
nbarth@chromium.org <nbarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-29 10:02:39 +0000

    IDL compiler: rebase bindings tests following r160807
    Fix for new tests, since test output changed by:
    IDL compiler: remove empty statement from void methods
    https://codereview.chromium.org/49163006/
    
    BUG=239771
    NOTRY=true
    TBR=haraken
    
    Review URL: https://codereview.chromium.org/50553003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160820 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/tests/results/V8TestObjectPython.cpp
+++ third_party/WebKit/Source/bindings/tests/results/V8TestObjectPython.cpp
@@ -3828,7 +3828,6 @@ static void voidMethodOptionalStringArgMethod(const v8::FunctionCallbackInfo<v8:
     TestObjectPython* imp = V8TestObjectPython::toNative(args.Holder());
     if (UNLIKELY(args.Length() <= 0)) {
         imp->voidMethodOptionalStringArg();
-;
         return;
     }
     V8TRYCATCH_FOR_V8STRINGRESOURCE_VOID(V8StringResource<>, stringArg, args[0]);
@@ -3847,7 +3846,6 @@ static void voidMethodOptionalLongArgMethod(const v8::FunctionCallbackInfo<v8::V
     TestObjectPython* imp = V8TestObjectPython::toNative(args.Holder());
     if (UNLIKELY(args.Length() <= 0)) {
         imp->voidMethodOptionalLongArg();
-;
         return;
     }
     V8TRYCATCH_VOID(int, optionalLongArg, toInt32(args[0]));
@@ -3866,7 +3864,6 @@ static void stringMethodOptionalLongArgMethod(const v8::FunctionCallbackInfo<v8:
     TestObjectPython* imp = V8TestObjectPython::toNative(args.Holder());
     if (UNLIKELY(args.Length() <= 0)) {
         v8SetReturnValueString(args, imp->stringMethodOptionalLongArg(), args.GetIsolate());
-;
         return;
     }
     V8TRYCATCH_VOID(int, optionalLongArg, toInt32(args[0]));
@@ -3885,7 +3882,6 @@ static void testInterfaceEmptyMethodOptionalLongArgMethod(const v8::FunctionCall
     TestObjectPython* imp = V8TestObjectPython::toNative(args.Holder());
     if (UNLIKELY(args.Length() <= 0)) {
         v8SetReturnValue(args, imp->testInterfaceEmptyMethodOptionalLongArg(), args.Holder());
-;
         return;
     }
     V8TRYCATCH_VOID(int, optionalLongArg, toInt32(args[0]));
@@ -3904,7 +3900,6 @@ static void longMethodOptionalLongArgMethod(const v8::FunctionCallbackInfo<v8::V
     TestObjectPython* imp = V8TestObjectPython::toNative(args.Holder());
     if (UNLIKELY(args.Length() <= 0)) {
         v8SetReturnValueInt(args, imp->longMethodOptionalLongArg());
-;
         return;
     }
     V8TRYCATCH_VOID(int, optionalLongArg, toInt32(args[0]));
@@ -3928,7 +3923,6 @@ static void voidMethodLongArgOptionalLongArgMethod(const v8::FunctionCallbackInf
     V8TRYCATCH_VOID(int, longArg, toInt32(args[0]));
     if (UNLIKELY(args.Length() <= 1)) {
         imp->voidMethodLongArgOptionalLongArg(longArg);
-;
         return;
     }
     V8TRYCATCH_VOID(int, optionalLongArg, toInt32(args[1]));
@@ -3952,13 +3946,11 @@ static void voidMethodLongArgOptionalLongArgOptionalLongArgMethod(const v8::Func
     V8TRYCATCH_VOID(int, longArg, toInt32(args[0]));
     if (UNLIKELY(args.Length() <= 1)) {
         imp->voidMethodLongArgOptionalLongArgOptionalLongArg(longArg);
-;
         return;
     }
     V8TRYCATCH_VOID(int, optionalLongArg1, toInt32(args[1]));
     if (UNLIKELY(args.Length() <= 2)) {
         imp->voidMethodLongArgOptionalLongArgOptionalLongArg(longArg, optionalLongArg1);
-;
         return;
     }
     V8TRYCATCH_VOID(int, optionalLongArg2, toInt32(args[2]));
