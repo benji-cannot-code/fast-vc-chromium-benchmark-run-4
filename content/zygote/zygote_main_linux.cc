43d36704ce000fb1af6f351beec29c4fb0fd0905
rmcilroy@chromium.org <rmcilroy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-29 19:32:45 +0000

    Enable SysInfo::AmountOfPhysicalMemory to be called from within the Linux sandbox.
    Trigger caching of SysInfo::AmountOfPhysicalMemory in PreSandboxInit() to enable
    it to be called by the renderer process after the sandbox is sealed.
    
    BUG=312241
    
    Review URL: https://codereview.chromium.org/28833002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231613 0039d316-1c4b-4281-b951-d872f2087c98

--- content/zygote/zygote_main_linux.cc
+++ content/zygote/zygote_main_linux.cc
@@ -275,6 +275,7 @@ void PreloadPepperPlugins() {
 static void PreSandboxInit() {
   base::RandUint64();
 
+  base::SysInfo::AmountOfPhysicalMemory();
   base::SysInfo::MaxSharedMemorySize();
 
   // ICU DateFormat class (used in base/time_format.cc) needs to get the
