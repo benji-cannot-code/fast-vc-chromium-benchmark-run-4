d302914acd2c5e085259ecb59915fb3b36f0fda3
hmuller@adobe.com <hmuller@adobe.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-29 19:41:51 +0000

    [CSS Shapes] CORS-enabled fetch for shape image values - ShapeInfo.cpp cleanups
    > ShapeInfo<RenderType>::checkImageOrigin(const RenderType* renderer, const StyleImage* styleImage)
    
    It's simpler and more straightforward to just pass a Document& to this (essentially private) function, instead of a renderer. Similarly, the second parameter can be an ImageResource&.
    
    Currently a (static) copy of checkImageOrigin() is needlessly generated for each RenderType template instantiation. Replaced it with a global function called checkShapeImageOrigin().
    
    Replaced the following test logic in checkImageOrigin with a short-circuit return:
    
    > bool accessAllowed= styleImage->cachedImage()->isOriginClean(renderer->document().securityOrigin());
    >    if (!accessAllowed) {
    >       ...
    >    }
    
    
    BUG=312480
    
    Review URL: https://codereview.chromium.org/50973002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160856 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/shapes/ShapeInfo.cpp
+++ third_party/WebKit/Source/core/rendering/shapes/ShapeInfo.cpp
@@ -37,19 +37,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-template<class RenderType>
-bool ShapeInfo<RenderType>::checkImageOrigin(const RenderType* renderer, const StyleImage* styleImage)
+bool checkShapeImageOrigin(Document& document, ImageResource& imageResource)
 {
-    ASSERT(styleImage && styleImage->isImageResource() && styleImage->cachedImage() && styleImage->cachedImage()->image());
+    if (imageResource.isAccessAllowed(document.securityOrigin()))
+        return true;
 
-    bool accessAllowed = styleImage->cachedImage()->isAccessAllowed(renderer->document().securityOrigin());
-    if (!accessAllowed) {
-        const KURL& url = styleImage->cachedImage()->url();
-        String urlString = url.isNull() ? "''" : url.elidedString();
-        renderer->document().addConsoleMessage(SecurityMessageSource, ErrorMessageLevel, "Unsafe attempt to load URL " + urlString + ".");
-        return false;
-    }
-    return true;
+    const KURL& url = imageResource.url();
+    String urlString = url.isNull() ? "''" : url.elidedString();
+    document.addConsoleMessage(SecurityMessageSource, ErrorMessageLevel, "Unsafe attempt to load URL " + urlString + ".");
+
+    return false;
 }
 
 template<class RenderType>
