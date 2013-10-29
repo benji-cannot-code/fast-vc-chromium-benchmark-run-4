65b607be74f8712e98661d35267e90be23f940ad
sbc@chromium.org <sbc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-29 00:00:35 +0000

    [NaCl SDK] nacl_io: add support for SO_KEEPALIVE and FD_CLOEXEC
    These are useful for the civitweb port in naclports.
    
    R=binji@chromium.org
    
    Review URL: https://codereview.chromium.org/40553005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231450 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/libraries/nacl_io/mount_node_socket.h
+++ native_client_sdk/src/libraries/nacl_io/mount_node_socket.h
@@ -158,6 +158,8 @@ class MountNodeSocket : public MountNodeStream {
   PP_Resource remote_addr_;
   uint32_t socket_flags_;
   int last_errno_;
+  bool keep_alive_;
+  struct linger linger_;
 
   friend class KernelProxy;
   friend class MountStream;
