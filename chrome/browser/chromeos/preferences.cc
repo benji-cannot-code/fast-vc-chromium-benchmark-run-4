b1eeaba6a99bba4e2e9dc004d921d0985e282287
kinaba@chromium.org <kinaba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-30 09:04:27 +0000

    Revert 231699 "Remove kNaturalScrollDefault."
    Many tests started failing at ui::ShouldDefaultToNaturalScroll(), e.g.,
    http://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Tests%20%28dbg%29%281%29/builds/26125/steps/unit_tests/logs/TestFocusNotification
    
    > Remove kNaturalScrollDefault.
    > 
    > We have a runtime check - IsTouchDevicePresent, so just use that instead of the flag.
    > 
    > BUG=NONE
    > TEST=manual
    > 
    > Review URL: https://codereview.chromium.org/33913003
    
    TBR=achuith@chromium.org
    
    Review URL: https://codereview.chromium.org/52223002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231779 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/preferences.cc
+++ chrome/browser/chromeos/preferences.cc
@@ -117,7 +117,8 @@ void Preferences::RegisterProfilePrefs(
       user_prefs::PrefRegistrySyncable::UNSYNCABLE_PREF);
   registry->RegisterBooleanPref(
       prefs::kNaturalScroll,
-      ui::ShouldDefaultToNaturalScroll(),
+      CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kNaturalScrollDefault),
       user_prefs::PrefRegistrySyncable::SYNCABLE_PRIORITY_PREF);
   registry->RegisterBooleanPref(
       prefs::kPrimaryMouseButtonRight,
@@ -586,7 +587,8 @@ void Preferences::OnIsSyncingChanged() {
 
 void Preferences::ForceNaturalScrollDefault() {
   DVLOG(1) << "ForceNaturalScrollDefault";
-  if (ui::ShouldDefaultToNaturalScroll() &&
+  if (CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kNaturalScrollDefault) &&
       prefs_->IsSyncing() &&
       !prefs_->GetUserPrefValue(prefs::kNaturalScroll)) {
     DVLOG(1) << "Natural scroll forced to true";
--- chrome/browser/ui/views/ash/tab_scrubber_browsertest.cc
+++ chrome/browser/ui/views/ash/tab_scrubber_browsertest.cc
@@ -42,6 +42,9 @@ class TabScrubberTest : public InProcessBrowserTest,
   }
 
   virtual void SetUpCommandLine(CommandLine* command_line) OVERRIDE {
+#if defined(OS_CHROMEOS)
+    command_line->AppendSwitch(chromeos::switches::kNaturalScrollDefault);
+#endif
     command_line->AppendSwitch(switches::kOpenAsh);
   }
 
@@ -52,8 +55,6 @@ class TabScrubberTest : public InProcessBrowserTest,
     ash::Shell* shell = ash::Shell::GetInstance();
     shell->event_transformation_handler()->set_transformation_mode(
         ash::internal::EventTransformationHandler::TRANSFORM_NONE);
-
-    ui::SetNaturalScroll(true);
   }
 
   virtual void CleanUpOnMainThread() OVERRIDE {
