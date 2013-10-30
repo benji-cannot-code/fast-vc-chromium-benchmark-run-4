ce29c8a9d7a5a0f990e80b37edfef9a74e48b5fc
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-30 18:36:07 +0000

    [Telemetry] Record CPU usage in Session restore measurement
    * Record CPU usage from just before browser launch till after all restored tabs have loaded.
    
    BUG=312375
    NOTRY= true
    
    Review URL: https://codereview.chromium.org/47743010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231884 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/measurements/session_restore.py
+++ tools/perf/measurements/session_restore.py
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 from measurements import startup
+from metrics import cpu
 from metrics import startup_metric
 
 
@@ -18,6 +19,7 @@ class SessionRestore(startup.Startup):
   def __init__(self):
     super(SessionRestore, self).__init__()
     self.close_tabs_before_run = False
+    self._cpu_metric = None
 
   def CustomizeBrowserOptions(self, options):
     super(SessionRestore, self).CustomizeBrowserOptions(options)
@@ -43,11 +45,19 @@ class SessionRestore(startup.Startup):
       raise Exception("Invalid pageset: more than 1 WPR archive found.: " +
           ', '.join(wpr_archives.keys()))
 
+  def WillStartBrowser(self, browser):
+    self._cpu_metric = cpu.CpuMetric(browser)
+    self._cpu_metric.Start(None, None)
+
   def MeasurePage(self, page, tab, results):
     # Wait for all tabs to finish loading.
     for i in xrange(len(tab.browser.tabs)):
       t = tab.browser.tabs[i]
       t.WaitForDocumentReadyStateToBeComplete()
+
+    # Record CPU usage from browser start to when all pages have loaded.
+    self._cpu_metric.Stop(None, None)
+    self._cpu_metric.AddResults(tab, results, 'cpu_utilization')
 
     startup_metric.StartupMetric().AddResults(tab, results)
 
