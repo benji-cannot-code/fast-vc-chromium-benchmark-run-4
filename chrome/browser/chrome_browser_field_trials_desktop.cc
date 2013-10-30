b679bceaaba9300c1387a421105ead86e8a8ca8e
gab@chromium.org <gab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-30 23:13:14 +0000

    Statically enable TCM and get rid of compositing-mode-field-trials.
    This is based on top of https://codereview.chromium.org/27197009/
    
    BUG=302738, 233830, 306557
    
    Originally Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=229806
    Reverted: https://src.chromium.org/viewvc/chrome?view=rev&revision=229907
    
    Review URL: https://codereview.chromium.org/26389003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231949 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_browser_field_trials_desktop.cc
+++ chrome/browser/chrome_browser_field_trials_desktop.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "chrome/browser/auto_launch_trial.h"
 #include "chrome/browser/google/google_util.h"
-#include "chrome/browser/gpu/chrome_gpu_util.h"
 #include "chrome/browser/omnibox/omnibox_field_trial.h"
 #include "chrome/browser/prerender/prerender_field_trial.h"
 #include "chrome/browser/profiles/profiles_state.h"
@@ -131,7 +130,6 @@ void SetupDesktopFieldTrials(const CommandLine& parsed_command_line,
                              PrefService* local_state) {
   prerender::ConfigurePrefetchAndPrerender(parsed_command_line);
   AutoLaunchChromeFieldTrial();
-  gpu_util::InitializeCompositingFieldTrial();
   OmniboxFieldTrial::ActivateStaticTrials();
   SetupInfiniteCacheFieldTrial();
   DisableShowProfileSwitcherTrialIfNecessary();
