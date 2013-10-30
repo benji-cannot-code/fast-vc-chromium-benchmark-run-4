79c69619ee033b7f14756beaeab763392024303d
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-30 05:01:47 +0000

    [Files.app] Skip the suggest-apps dialog for some extension
    BUG=308274
    TEST=manually
    R=hirono@chromium.org
    
    Review URL: https://codereview.chromium.org/50213002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231758 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/foreground/js/file_tasks.js
+++ chrome/browser/resources/file_manager/foreground/js/file_tasks.js
@@ -135,6 +135,16 @@ FileTasks.knownExtensions_ = [
 ];
 
 /**
+ * The list of extensions to skip the suggest app dialog.
+ * @const
+ * @type {Array.<string>}
+ * @private
+ */
+FileTasks.EXTENSIONS_TO_SKIP_SUGGEST_APPS_ = Object.freeze([
+  'crdownload', 'dsc', 'inf',
+]);
+
+/**
  * Records trial of opening file grouped by extensions.
  *
  * @param {Array.<string>} urls The path to be opened.
@@ -329,7 +339,9 @@ FileTasks.prototype.executeDefaultInternal_ = function(urls, opt_callback) {
       return;
     }
 
-    if (!fm.isOnDrive() || !urls[0]) {
+    if (!fm.isOnDrive() ||
+        !urls[0] ||
+        FileTasks.EXTENSIONS_TO_SKIP_SUGGEST_APPS_.indexOf(extension) != -1) {
       showAlert();
       return;
     }
