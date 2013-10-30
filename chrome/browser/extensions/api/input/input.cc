b20f183134789f93053dcea1228de7b6bfe3f409
stevet@chromium.org <stevet@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-30 03:31:27 +0000

    Add a UserAction to track keyboard loads.
    This will give a user count in the Counts analysis.
    
    BUG=288192
    
    Review URL: https://codereview.chromium.org/48923004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231724 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/input/input.cc
+++ chrome/browser/extensions/api/input/input.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string16.h"
 #include "chrome/browser/extensions/extension_function_registry.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/user_metrics.h"
 #include "ui/events/event.h"
 #include "ui/keyboard/keyboard_controller.h"
 #include "ui/keyboard/keyboard_switches.h"
@@ -120,6 +121,8 @@ bool VirtualKeyboardPrivateKeyboardLoadedFunction::RunImpl() {
   DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
 
   keyboard::MarkKeyboardLoadFinished();
+
+  content::UserMetricsAction("VirtualKeyboardLoaded");
 
   return true;
 #endif
