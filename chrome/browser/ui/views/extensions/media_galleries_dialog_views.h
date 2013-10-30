bfbe92b3fa030ed40a79cd8e4c9da7c0da92fdce
gbillock@chromium.org <gbillock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-30 01:35:54 +0000

    [MediaGalleries] Add context menu for forgetting galleries. [views]
    Review URL: https://codereview.chromium.org/37553002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231703 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/extensions/media_galleries_dialog_views.h
+++ chrome/browser/ui/views/extensions/media_galleries_dialog_views.h
@@ -10,12 +10,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "chrome/browser/media_galleries/media_galleries_dialog_controller.h"
+#include "ui/views/context_menu_controller.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/window/dialog_delegate.h"
+
+namespace ui {
+class MenuModel;
+}
 
 namespace views {
 class Checkbox;
 class LabelButton;
+class MenuRunner;
 class Widget;
 }
 
@@ -23,6 +29,7 @@ class Widget;
 // upon construction.
 class MediaGalleriesDialogViews : public MediaGalleriesDialog,
                                   public views::ButtonListener,
+                                  public views::ContextMenuController,
                                   public views::DialogDelegate {
  public:
   explicit MediaGalleriesDialogViews(
@@ -52,6 +59,11 @@ class MediaGalleriesDialogViews : public MediaGalleriesDialog,
   virtual void ButtonPressed(views::Button* sender,
                              const ui::Event& event) OVERRIDE;
 
+  // views::ContextMenuController implementation:
+  virtual void ShowContextMenuForView(views::View* source,
+                                      const gfx::Point& point,
+                                      ui::MenuSourceType source_type) OVERRIDE;
+
  private:
   typedef std::map<MediaGalleryPrefId, views::Checkbox*> CheckboxMap;
   typedef std::map<views::Checkbox*, MediaGalleryPrefInfo> NewCheckboxMap;
@@ -64,6 +76,10 @@ class MediaGalleriesDialogViews : public MediaGalleriesDialog,
                           bool permitted,
                           views::View* container,
                           int trailing_vertical_space);
+
+  void ShowContextMenu(const gfx::Point& point,
+                       ui::MenuSourceType source_type,
+                       MediaGalleryPrefId id);
 
   MediaGalleriesDialogController* controller_;
 
@@ -89,6 +105,8 @@ class MediaGalleriesDialogViews : public MediaGalleriesDialog,
 
   // True if the user has pressed accept.
   bool accepted_;
+
+  scoped_ptr<views::MenuRunner> context_menu_runner_;
 
   DISALLOW_COPY_AND_ASSIGN(MediaGalleriesDialogViews);
 };
