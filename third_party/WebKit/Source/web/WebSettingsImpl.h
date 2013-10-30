26a91b21be96358232619fb9e5710c13da224bba
skobes@chromium.org <skobes@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-30 18:55:21 +0000

    Add Settings::deviceScaleAdjustment.
    This will be used to plumb the 1.05-1.3x fudge factor separately from the font
    scale factor.  It is applied to the autosizer's cluster multiplier only when
    the page does not set an explicit meta viewport or @viewport.
    
    BUG=252828
    
    Review URL: https://codereview.chromium.org/49913005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160957 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebSettingsImpl.h
+++ third_party/WebKit/Source/web/WebSettingsImpl.h
@@ -88,6 +88,7 @@ public:
     virtual void setDefaultVideoPosterURL(const WebString&);
     virtual void setDeferred2dCanvasEnabled(bool);
     virtual void setDeferredImageDecodingEnabled(bool);
+    virtual void setDeviceScaleAdjustment(float);
     virtual void setDeviceSupportsMouse(bool);
     virtual void setDeviceSupportsTouch(bool);
     virtual void setDoubleTapToZoomEnabled(bool);
