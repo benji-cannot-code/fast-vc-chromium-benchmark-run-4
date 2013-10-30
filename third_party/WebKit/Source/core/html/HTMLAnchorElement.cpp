2f99e0ad7b16ed1d0bc49b576384edca41b4d808
l.gombos@samsung.com <l.gombos@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-30 16:36:31 +0000

    Fix unused functions warnings on Linux.
    BUG=312287
    TBR=abarth
    
    Review URL: https://codereview.chromium.org/48523009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160945 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLAnchorElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLAnchorElement.cpp
@@ -134,15 +134,6 @@ HTMLAnchorElement::~HTMLAnchorElement()
     clearRootEditableElementForSelectionOnMouseDown();
 }
 
-// This function does not allow leading spaces before the port number.
-static unsigned parsePortFromStringPosition(const String& value, unsigned portStart, unsigned& portEnd)
-{
-    portEnd = portStart;
-    while (isASCIIDigit(value[portEnd]))
-        ++portEnd;
-    return value.substring(portStart, portEnd - portStart).toUInt();
-}
-
 bool HTMLAnchorElement::supportsFocus() const
 {
     if (rendererIsEditable())
--- third_party/WebKit/Source/weborigin/SecurityOriginTest.cpp
+++ third_party/WebKit/Source/weborigin/SecurityOriginTest.cpp
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "weborigin/KURL.h"
 #include "weborigin/SecurityOrigin.h"
-#include "wtf/testing/WTFTestHelpers.h"
 
 #include <gtest/gtest.h>
 
