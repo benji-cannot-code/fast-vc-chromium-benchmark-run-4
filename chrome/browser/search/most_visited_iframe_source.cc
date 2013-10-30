ec80a994b20113249e592981f679a03760f62d75
beaudoin@chromium.org <beaudoin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-30 17:28:32 +0000

    Adding UMA events to track when the user clicks on the Most Visited tiles.
    Events are used by the script computing time-to-long-dwell because they're available as a time-stamped stream of events server-side. We want to compute ttld starting from a click on a Most Visited.
    
    BUG=312752
    
    Review URL: https://codereview.chromium.org/49143002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231866 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/search/most_visited_iframe_source.cc
+++ chrome/browser/search/most_visited_iframe_source.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram.h"
 #include "base/strings/string_number_conversions.h"
 #include "chrome/common/url_constants.h"
+#include "content/public/browser/user_metrics.h"
 #include "grit/browser_resources.h"
 #include "net/base/url_util.h"
 #include "url/gurl.h"
@@ -68,9 +69,13 @@ void MostVisitedIframeSource::StartDataRequest(
     std::string str_position;
     int position;
     if (net::GetValueForKeyInQuery(url, "pos", &str_position) &&
-        base::StringToInt(str_position, &position))
+        base::StringToInt(str_position, &position)) {
       UMA_HISTOGRAM_ENUMERATION(kMostVisitedHistogramName, position,
                                 kNumMostVisited);
+      // Records the action. This will be available as a time-stamped stream
+      // server-side and can be used to compute time-to-long-dwell.
+      content::RecordAction(content::UserMetricsAction("MostVisited_Clicked"));
+    }
     callback.Run(NULL);
   } else {
     callback.Run(NULL);
