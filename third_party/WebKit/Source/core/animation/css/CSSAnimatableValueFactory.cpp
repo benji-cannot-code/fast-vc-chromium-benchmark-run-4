62d3ac485ef4884c70e3ac27ac017abd7b1e8e98
alancutter@chromium.org <alancutter@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-30 04:27:31 +0000

    Web Animations CSS: Fix release crash when animating 'none' images
    StyleImages on render styles are allowed to be null meaning the CSS
    keyword none was used as the value.
    This patch handles the null case and properly animates it as the none
    keyword.
    
    BUG=257591
    
    Review URL: https://codereview.chromium.org/46883006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160899 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/css/CSSAnimatableValueFactory.cpp
+++ third_party/WebKit/Source/core/animation/css/CSSAnimatableValueFactory.cpp
@@ -133,6 +133,13 @@ inline static PassRefPtr<AnimatableValue> createFromLengthSize(const LengthSize&
         createFromLength(lengthSize.height(), style));
 }
 
+inline static PassRefPtr<AnimatableValue> createFromStyleImage(StyleImage* image)
+{
+    if (image)
+        return AnimatableImage::create(image);
+    return AnimatableUnknown::create(CSSValueNone);
+}
+
 inline static PassRefPtr<AnimatableValue> createFromFillSize(const FillSize& fillSize, const RenderStyle* style)
 {
     switch (fillSize.type) {
@@ -157,11 +164,7 @@ inline static PassRefPtr<AnimatableValue> createFromFillLayers(const FillLayer* 
         if (property == CSSPropertyBackgroundImage || property == CSSPropertyWebkitMaskImage) {
             if (!fillLayer->isImageSet())
                 break;
-            StyleImage* image = fillLayer->image();
-            if (image)
-                values.append(AnimatableImage::create(image));
-            else
-                values.append(AnimatableUnknown::create(CSSValueNone));
+            values.append(createFromStyleImage(fillLayer->image()));
         } else if (property == CSSPropertyBackgroundPositionX || property == CSSPropertyWebkitMaskPositionX) {
             if (!fillLayer->isXPositionSet())
                 break;
@@ -231,7 +234,7 @@ PassRefPtr<AnimatableValue> CSSAnimatableValueFactory::create(CSSPropertyID prop
     case CSSPropertyBorderImageSlice:
         return createFromLengthBox(style->borderImageSlices(), style);
     case CSSPropertyBorderImageSource:
-        return AnimatableImage::create(style->borderImageSource());
+        return createFromStyleImage(style->borderImageSource());
     case CSSPropertyBorderImageWidth:
         return createFromLengthBox(style->borderImageWidth(), style);
     case CSSPropertyBorderLeftColor:
@@ -288,7 +291,7 @@ PassRefPtr<AnimatableValue> CSSAnimatableValueFactory::create(CSSPropertyID prop
     case CSSPropertyLightingColor:
         return createFromColor(property, style);
     case CSSPropertyListStyleImage:
-        return AnimatableImage::create(style->listStyleImage());
+        return createFromStyleImage(style->listStyleImage());
     case CSSPropertyLeft:
         return createFromLength(style->left(), style);
     case CSSPropertyLetterSpacing:
@@ -378,7 +381,7 @@ PassRefPtr<AnimatableValue> CSSAnimatableValueFactory::create(CSSPropertyID prop
     case CSSPropertyWebkitMaskBoxImageSlice:
         return createFromLengthBoxAndBool(style->maskBoxImageSlices(), style->maskBoxImageSlicesFill(), style);
     case CSSPropertyWebkitMaskBoxImageSource:
-        return AnimatableImage::create(style->maskBoxImageSource());
+        return createFromStyleImage(style->maskBoxImageSource());
     case CSSPropertyWebkitMaskBoxImageWidth:
         return createFromLengthBox(style->maskBoxImageWidth(), style);
     case CSSPropertyWebkitMaskImage:
