744010dac14d9a9aa1b05b55f23bce893f7662b6
k.wolanski@samsung.com <k.wolanski@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-30 17:49:37 +0000

    Enable correct behaviour for deleting regional indicator symbols on Linux OS
    Regional indicator symbols that are combined should behave as a single
    character when deleting.
    
    BUG=306948
    
    Review URL: https://codereview.chromium.org/27169003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160954 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderText.cpp
+++ third_party/WebKit/Source/core/rendering/RenderText.cpp
@@ -1654,7 +1654,7 @@ int RenderText::previousOffset(int current) const
     return result;
 }
 
-#if OS(MACOSX)
+#if OS(MACOSX) || OS(LINUX)
 
 #define HANGUL_CHOSEONG_START (0x1100)
 #define HANGUL_CHOSEONG_END (0x115F)
@@ -1696,7 +1696,7 @@ inline bool isRegionalIndicator(UChar32 c)
 
 int RenderText::previousOffsetForBackwardDeletion(int current) const
 {
-#if OS(MACOSX)
+#if OS(MACOSX) || OS(LINUX)
     ASSERT(m_text);
     StringImpl& text = *m_text.impl();
     UChar32 character;
@@ -1739,7 +1739,6 @@ int RenderText::previousOffsetForBackwardDeletion(int current) const
     character = text.characterStartingAt(current);
     if (((character >= HANGUL_CHOSEONG_START) && (character <= HANGUL_JONGSEONG_END)) || ((character >= HANGUL_SYLLABLE_START) && (character <= HANGUL_SYLLABLE_END))) {
         HangulState state;
-        HangulState initialState;
 
         if (character < HANGUL_JUNGSEONG_START)
             state = HangulStateL;
@@ -1749,8 +1748,6 @@ int RenderText::previousOffsetForBackwardDeletion(int current) const
             state = HangulStateT;
         else
             state = isHangulLVT(character) ? HangulStateLVT : HangulStateLV;
-
-        initialState = state;
 
         while (current > 0 && ((character = text.characterStartingAt(current - 1)) >= HANGUL_CHOSEONG_START) && (character <= HANGUL_SYLLABLE_END) && ((character <= HANGUL_JONGSEONG_END) || (character >= HANGUL_SYLLABLE_START))) {
             switch (state) {
@@ -1783,7 +1780,7 @@ int RenderText::previousOffsetForBackwardDeletion(int current) const
 
     return current;
 #else
-    // Platforms other than Mac delete by one code point.
+    // Platforms other than Mac or Linux delete by one code point.
     if (U16_IS_TRAIL(m_text[--current]))
         --current;
     if (current < 0)
