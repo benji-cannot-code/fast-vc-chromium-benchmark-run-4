da35a44f5d133acba938491fe7d4201a4ab5d84f
earthdok@chromium.org <earthdok@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-30 05:42:45 +0000

    Do not leak BookmarkStorage in tests which create their own Profiles.
    Pump the deferred task runner.
    
    BUG=312838
    R=erg@chromium.org,shess@chromium.org
    
    Review URL: https://codereview.chromium.org/51013002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231774 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_browsertest.cc
+++ chrome/browser/profiles/profile_browsertest.cc
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/profiles/chrome_version_service.h"
 #include "chrome/browser/profiles/profile_impl.h"
+#include "chrome/browser/profiles/startup_task_runner_service.h"
+#include "chrome/browser/profiles/startup_task_runner_service_factory.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_version_info.h"
 #include "chrome/common/pref_names.h"
@@ -74,6 +76,8 @@ IN_PROC_BROWSER_TEST_F(ProfileBrowserTest,
       temp_dir.path(), &delegate, Profile::CREATE_MODE_SYNCHRONOUS));
   ASSERT_TRUE(profile.get());
   CheckChromeVersion(profile.get(), true);
+  StartupTaskRunnerServiceFactory::GetForProfile(profile.get())->
+              StartDeferredTaskRunners();
 }
 
 // Test OnProfileCreate is called with is_new_profile set to false when
@@ -92,6 +96,8 @@ IN_PROC_BROWSER_TEST_F(ProfileBrowserTest,
       temp_dir.path(), &delegate, Profile::CREATE_MODE_SYNCHRONOUS));
   ASSERT_TRUE(profile.get());
   CheckChromeVersion(profile.get(), false);
+  StartupTaskRunnerServiceFactory::GetForProfile(profile.get())->
+              StartDeferredTaskRunners();
 }
 
 // Test OnProfileCreate is called with is_new_profile set to true when
@@ -108,6 +114,8 @@ IN_PROC_BROWSER_TEST_F(ProfileBrowserTest,
   scoped_ptr<Profile> profile(Profile::CreateProfile(
       temp_dir.path(), &delegate, Profile::CREATE_MODE_ASYNCHRONOUS));
   ASSERT_TRUE(profile.get());
+  StartupTaskRunnerServiceFactory::GetForProfile(profile.get())->
+              StartDeferredTaskRunners();
 
   // Wait for the profile to be created.
   content::WindowedNotificationObserver observer(
@@ -131,6 +139,8 @@ IN_PROC_BROWSER_TEST_F(ProfileBrowserTest,
   scoped_ptr<Profile> profile(Profile::CreateProfile(
       temp_dir.path(), &delegate, Profile::CREATE_MODE_ASYNCHRONOUS));
   ASSERT_TRUE(profile.get());
+  StartupTaskRunnerServiceFactory::GetForProfile(profile.get())->
+              StartDeferredTaskRunners();
 
   // Wait for the profile to be created.
   content::WindowedNotificationObserver observer(
@@ -155,6 +165,8 @@ IN_PROC_BROWSER_TEST_F(ProfileBrowserTest, DISABLED_ProfileReadmeCreated) {
   scoped_ptr<Profile> profile(Profile::CreateProfile(
       temp_dir.path(), &delegate, Profile::CREATE_MODE_ASYNCHRONOUS));
   ASSERT_TRUE(profile.get());
+  StartupTaskRunnerServiceFactory::GetForProfile(profile.get())->
+              StartDeferredTaskRunners();
 
   // Wait for the profile to be created.
   content::WindowedNotificationObserver observer(
@@ -183,6 +195,8 @@ IN_PROC_BROWSER_TEST_F(ProfileBrowserTest, ProfileDeletedBeforeReadmeCreated) {
   scoped_ptr<Profile> profile(Profile::CreateProfile(
       temp_dir.path(), &delegate, Profile::CREATE_MODE_SYNCHRONOUS));
   ASSERT_TRUE(profile.get());
+  StartupTaskRunnerServiceFactory::GetForProfile(profile.get())->
+              StartDeferredTaskRunners();
 
   // Delete the Profile instance and run pending tasks (this includes the task
   // for README creation).
@@ -209,6 +223,8 @@ IN_PROC_BROWSER_TEST_F(ProfileBrowserTest, MAYBE_ExitType) {
   scoped_ptr<Profile> profile(Profile::CreateProfile(
       temp_dir.path(), &delegate, Profile::CREATE_MODE_SYNCHRONOUS));
   ASSERT_TRUE(profile.get());
+  StartupTaskRunnerServiceFactory::GetForProfile(profile.get())->
+              StartDeferredTaskRunners();
 
   PrefService* prefs = profile->GetPrefs();
   // The initial state is crashed; store for later reference.
