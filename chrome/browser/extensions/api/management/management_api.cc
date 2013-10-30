82471705a28e37b9ae7efd79fbb28ede8c9e4f9e
earthdok@chromium.org <earthdok@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-30 09:53:11 +0000

    Do not leak a JSON Value from SafeManifestJSONParser::ReportResultFromUIThread.
    BUG=312792
    R=benwells@chromium.org
    
    Review URL: https://codereview.chromium.org/49223006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231792 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/management/management_api.cc
+++ chrome/browser/extensions/api/management/management_api.cc
@@ -349,7 +349,7 @@ class SafeManifestJSONParser : public UtilityProcessHostClient {
   void ReportResultFromUIThread() {
     CHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
     if (error_.empty() && parsed_manifest_.get())
-      client_->OnParseSuccess(parsed_manifest_.release());
+      client_->OnParseSuccess(parsed_manifest_.Pass());
     else
       client_->OnParseFailure(error_);
   }
@@ -388,8 +388,8 @@ bool ManagementGetPermissionWarningsByManifestFunction::RunImpl() {
 }
 
 void ManagementGetPermissionWarningsByManifestFunction::OnParseSuccess(
-    base::DictionaryValue* parsed_manifest) {
-  CHECK(parsed_manifest);
+    scoped_ptr<base::DictionaryValue> parsed_manifest) {
+  CHECK(parsed_manifest.get());
 
   scoped_refptr<Extension> extension = Extension::Create(
       base::FilePath(), Manifest::INVALID_LOCATION, *parsed_manifest,
