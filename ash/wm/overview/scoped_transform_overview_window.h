2307a445abae859eeb6c548a543e2b9da298defa
flackr@chromium.org <flackr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-30 00:49:07 +0000

    Copy window and its transient parents when moving to another display.
    BUG=306157
    TEST=WindowSelectorTest.CycleMultipleDisplaysCopiesWindows
    TEST=Open two browsers on different displays. Press Ctrl+O to show an open file dialog. Then Alt+Tab and hold Alt. The browser and the open file dialog should be visible in the overview on the other display.
    
    Review URL: https://codereview.chromium.org/41073006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231691 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/overview/scoped_transform_overview_window.h
+++ ash/wm/overview/scoped_transform_overview_window.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_WM_OVERVIEW_SCOPED_TRANSFORM_OVERVIEW_WINDOW_H_
 
 #include "base/compiler_specific.h"
-#include "base/memory/scoped_ptr.h"
+#include "base/memory/scoped_vector.h"
 #include "ui/gfx/rect.h"
 #include "ui/gfx/transform.h"
 
@@ -81,6 +81,11 @@ class ScopedTransformOverviewWindow {
   aura::Window* window() const { return window_; }
 
  private:
+  // Creates copies of |window| and all of its modal transient parents on the
+  // root window |target_root|.
+  void CopyWindowAndTransientParents(aura::Window* target_root,
+                                     aura::Window* window);
+
   // Applies the |transform| to the overview window and all of its transient
   // children using animations. If |animate| the transform is animated in,
   // otherwise it is applied immediately.
@@ -90,8 +95,8 @@ class ScopedTransformOverviewWindow {
   // A weak pointer to the real window in the overview.
   aura::Window* window_;
 
-  // A copy of the window used to transition the window to another root.
-  scoped_ptr<ScopedWindowCopy> window_copy_;
+  // Copies of the window and transient parents for a different root window.
+  ScopedVector<ScopedWindowCopy> window_copies_;
 
   // If true, the window was minimized and should be restored if the window
   // was not selected.
