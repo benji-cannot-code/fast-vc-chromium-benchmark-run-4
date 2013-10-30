de0e24f655bed3d5409c42b3e158917eab080932
tomhudson@chromium.org <tomhudson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-30 10:12:38 +0000

    Disable single-threaded GatherPixelRefs().
    GatherPixelRefs has fairly high overhead. When we're running with
    a single raster worker thread, we *think* the overhead isn't justified.
    
    If we can fold GatherPixelRefs into the Skia picture recording process
    we should get a huge cost savings and be able to reenable this stage
    under all conditions.
    
    This is a second approach, much simpler than crrev.com/51173002 but relies
    on an early out in the picture iterator to avoid divide-by-zero crashes.
    
    BUG=310803
    R=reveman@chromium.org
    
    Review URL: https://codereview.chromium.org/51253002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231799 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/resources/picture.cc
+++ cc/resources/picture.cc
@@ -87,7 +87,8 @@ scoped_refptr<Picture> Picture::Create(gfx::Rect layer_rect) {
 }
 
 Picture::Picture(gfx::Rect layer_rect)
-    : layer_rect_(layer_rect) {
+  : layer_rect_(layer_rect),
+    cell_size_(layer_rect.size()) {
   // Instead of recording a trace event for object creation here, we wait for
   // the picture to be recorded in Picture::Record.
 }
@@ -156,7 +157,8 @@ Picture::Picture(SkPicture* picture,
                  gfx::Rect opaque_rect) :
     layer_rect_(layer_rect),
     opaque_rect_(opaque_rect),
-    picture_(skia::AdoptRef(picture)) {
+    picture_(skia::AdoptRef(picture)),
+    cell_size_(layer_rect.size()) {
 }
 
 Picture::Picture(const skia::RefPtr<SkPicture>& picture,
@@ -166,7 +168,8 @@ Picture::Picture(const skia::RefPtr<SkPicture>& picture,
     layer_rect_(layer_rect),
     opaque_rect_(opaque_rect),
     picture_(picture),
-    pixel_refs_(pixel_refs) {
+    pixel_refs_(pixel_refs),
+    cell_size_(layer_rect.size()) {
 }
 
 Picture::~Picture() {
@@ -388,8 +391,9 @@ Picture::PixelRefIterator::PixelRefIterator(
       current_index_(0) {
   gfx::Rect layer_rect = picture->layer_rect_;
   gfx::Size cell_size = picture->cell_size_;
+  DCHECK(!cell_size.IsEmpty());
 
-  // Early out if the query rect doesn't intersect this picture
+  // Early out if the query rect doesn't intersect this picture.
   if (!query_rect.Intersects(layer_rect)) {
     min_point_ = gfx::Point(0, 0);
     max_point_ = gfx::Point(0, 0);
