7310082ab709bf42defac230c86f0dc6c6516476
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-30 10:12:15 +0000

    Update order of FieldTrial functions.
    Puts the static functions together and
    moves the constructor to be in the same
    order in the .cc file as in the .h file.
    
    No logic changes.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/51233003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231798 0039d316-1c4b-4281-b951-d872f2087c98

--- base/metrics/field_trial.cc
+++ base/metrics/field_trial.cc
@@ -73,26 +73,6 @@ int FieldTrialList::kNoExpirationYear = 0;
 //------------------------------------------------------------------------------
 // FieldTrial methods and members.
 
-FieldTrial::FieldTrial(const std::string& trial_name,
-                       const Probability total_probability,
-                       const std::string& default_group_name,
-                       double entropy_value)
-    : trial_name_(trial_name),
-      divisor_(total_probability),
-      default_group_name_(default_group_name),
-      random_(GetGroupBoundaryValue(total_probability, entropy_value)),
-      accumulated_group_probability_(0),
-      next_group_number_(kDefaultGroupNumber + 1),
-      group_(kNotFinalized),
-      enable_field_trial_(true),
-      forced_(false),
-      group_reported_(false),
-      trial_registered_(false) {
-  DCHECK_GT(total_probability, 0);
-  DCHECK(!trial_name_.empty());
-  DCHECK(!default_group_name_.empty());
-}
-
 FieldTrial::EntropyProvider::~EntropyProvider() {
 }
 
@@ -160,12 +140,6 @@ const std::string& FieldTrial::group_name() {
   return group_name_;
 }
 
-// static
-void FieldTrial::EnableBenchmarking() {
-  DCHECK_EQ(0u, FieldTrialList::GetFieldTrialCount());
-  enable_benchmarking_ = true;
-}
-
 void FieldTrial::SetForced() {
   // We might have been forced before (e.g., by CreateFieldTrial) and it's
   // first come first served, e.g., command line switch has precedence.
@@ -178,6 +152,12 @@ void FieldTrial::SetForced() {
 }
 
 // static
+void FieldTrial::EnableBenchmarking() {
+  DCHECK_EQ(0u, FieldTrialList::GetFieldTrialCount());
+  enable_benchmarking_ = true;
+}
+
+// static
 FieldTrial* FieldTrial::CreateSimulatedFieldTrial(
     const std::string& trial_name,
     Probability total_probability,
@@ -185,6 +165,26 @@ FieldTrial* FieldTrial::CreateSimulatedFieldTrial(
     double entropy_value) {
   return new FieldTrial(trial_name, total_probability, default_group_name,
                         entropy_value);
+}
+
+FieldTrial::FieldTrial(const std::string& trial_name,
+                       const Probability total_probability,
+                       const std::string& default_group_name,
+                       double entropy_value)
+    : trial_name_(trial_name),
+      divisor_(total_probability),
+      default_group_name_(default_group_name),
+      random_(GetGroupBoundaryValue(total_probability, entropy_value)),
+      accumulated_group_probability_(0),
+      next_group_number_(kDefaultGroupNumber + 1),
+      group_(kNotFinalized),
+      enable_field_trial_(true),
+      forced_(false),
+      group_reported_(false),
+      trial_registered_(false) {
+  DCHECK_GT(total_probability, 0);
+  DCHECK(!trial_name_.empty());
+  DCHECK(!default_group_name_.empty());
 }
 
 FieldTrial::~FieldTrial() {}
