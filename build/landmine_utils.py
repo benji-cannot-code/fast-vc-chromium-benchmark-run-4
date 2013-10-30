119548743dc72bf6f90df51f746b4c9240889984
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-30 00:04:06 +0000

    mac: Use ninja by default.
    With this change, `gclient runhooks` creates ninja files by default on Mac.
    To build, run `ninja -C out/Release chrome`. The output appears in out/
    instead of in xcodebuild/. (This doesn't happen if OS=ios, so it shouldn't
    affect chrome/ios development.)
    
    If you need to use xcode for some reason, you can
    `export GYP_GENERATORS=xcode` to force xcode project generation.
    If you do this, please inform me why.
    
    Bots should automatically pick up the binaries in the new location (out).
    If something does break, it should be safe to revert this change, and bots
    should then automatically go back to picking up binaries from the old
    location (xcodebuild).
    
    BUG=294387
    R=mark@chromium.org
    
    Review URL: https://codereview.chromium.org/31143002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231675 0039d316-1c4b-4281-b951-d872f2087c98

--- build/landmine_utils.py
+++ build/landmine_utils.py
@@ -110,6 +110,6 @@ def builder():
     elif IsLinux():
       return 'ninja'
     elif IsMac():
-      return 'xcode'
+      return 'ninja'
     else:
       assert False, 'Don\'t know what builder we\'re using!'
