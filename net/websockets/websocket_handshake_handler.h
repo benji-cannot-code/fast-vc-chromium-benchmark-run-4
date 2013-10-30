c3c29128f78792cdd479a5c7c08fcf15a431aa69
tyoshino@chromium.org <tyoshino@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-30 01:37:22 +0000

    Factor out Sec-WebSocket-Accept computing logic so that the new WebSocket handshake processor can reuse it.
    BUG=226931
    
    Review URL: https://codereview.chromium.org/48733008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231707 0039d316-1c4b-4281-b951-d872f2087c98

--- net/websockets/websocket_handshake_handler.h
+++ net/websockets/websocket_handshake_handler.h
@@ -24,6 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace net {
 
+void ComputeSecWebSocketAccept(const std::string& key,
+                               std::string* accept);
+
 class NET_EXPORT_PRIVATE WebSocketHandshakeRequestHandler {
  public:
   WebSocketHandshakeRequestHandler();
