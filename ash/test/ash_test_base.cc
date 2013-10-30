fdc87b9452fd68d22f87afa687965463f39d087c
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-30 15:33:18 +0000

    Always adjust the bounds to work area if the new window is the first window.  I think this is correct behavior.
    Cleanup: remove set_target_root_window() when adding new root window. This was old hack for test and no longer necessary.
    
    BUG=286391
    TEST=covered by unit test.
    
    Review URL: https://codereview.chromium.org/46183004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231840 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/test/ash_test_base.cc
+++ ash/test/ash_test_base.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/display/display_controller.h"
 #include "ash/screen_ash.h"
 #include "ash/shell.h"
+#include "ash/shell/toplevel_window.h"
 #include "ash/test/ash_test_helper.h"
 #include "ash/test/display_manager_test_api.h"
 #include "ash/test/test_session_state_delegate.h"
@@ -102,6 +103,11 @@ AshTestBase::~AshTestBase() {
 
 void AshTestBase::SetUp() {
   setup_called_ = true;
+
+  // Clears the saved state so that test doesn't use on the wrong
+  // default state.
+  shell::ToplevelWindow::ClearSavedStateForTest();
+
   // TODO(jamescook): Can we do this without changing command line?
   // Use the origin (1,1) so that it doesn't over
   // lap with the native mouse cursor.
