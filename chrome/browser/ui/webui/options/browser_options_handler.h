202de79db79149d267df6bbe2ce0ef74ec16cc6a
pam@chromium.org <pam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-30 17:56:50 +0000

    Add a pointer to the supervised user dashboard to the Settings page
    It's hard for managers to know where to manage their users. Add a link to the
    dashboard under the Users section on the Settngs page, for users who are
    managing one or more supervised users.
    
    BUG=298836
    
    Review URL: https://codereview.chromium.org/27715002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231873 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options/browser_options_handler.h
+++ chrome/browser/ui/webui/options/browser_options_handler.h
@@ -265,6 +265,9 @@ class BrowserOptionsHandler
   // Setup the proxy settings section UI.
   void SetupProxySettingsSection();
 
+  // Setup the UI specific to managing supervised users.
+  void SetupManagingSupervisedUsers();
+
 #if defined(OS_CHROMEOS)
   // Setup the accessibility features for ChromeOS.
   void SetupAccessibilityFeatures();
