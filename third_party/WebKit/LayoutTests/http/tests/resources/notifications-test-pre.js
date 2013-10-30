03fa753032a5533a8cb62c47b11ba85847f3ac71
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-30 22:57:06 +0000

    Get rid of js-test-post-async.
    This file doesn't serve any purpose. Getting rid of it gets
    us one step closer to getting rid of js-test-post.
    
    Review URL: https://codereview.chromium.org/51333009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160979 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/resources/notifications-test-pre.js
+++
@@ -1,9 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-var testURL = "http://127.0.0.1:8000";
-
-function testCompleted()
-{
-  var scriptElement = document.createElement("script");
-  scriptElement.src = "/resources/js-test-post-async.js";
-  document.body.appendChild(scriptElement);
-}
