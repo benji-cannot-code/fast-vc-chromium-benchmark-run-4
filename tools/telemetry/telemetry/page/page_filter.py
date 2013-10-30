77e32810c0a6a1e4956de08d8a377fbef3500459
ernstm@chromium.org <ernstm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-30 10:33:46 +0000

    telemetry: Fixed bug in page_filter.py
    R=kbr@chromium.org,dtu@chromium.org
    BUG=312866
    
    Review URL: https://codereview.chromium.org/51593002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231807 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/page/page_filter.py
+++ tools/telemetry/telemetry/page/page_filter.py
@@ -28,12 +28,12 @@ class PageFilter(object):
   def IsSelected(self, page):
     if self._page_exclude_regex and (
         self._page_exclude_regex.search(page.url) or
-        self._page_exclude_regex.search(page.name)):
+        (page.name and self._page_exclude_regex.search(page.name))):
       return False
     if self._page_regex:
       return (
           self._page_regex.search(page.url) or
-          self._page_regex.search(page.name))
+          (page.name and self._page_regex.search(page.name)))
     return True
 
   @staticmethod
