5dcaa16efe9a514592c0c32a7290229a211278f9
kinaba@chromium.org <kinaba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-30 17:42:34 +0000

    Chrome OS: fix to enable opening file dialogs simultaneously from multiple packaged apps.
    The existing implementation has identified the callers with their
    tab IDs, but apps window seems to have no associated tab ID.
    This CL changes the identifier to use the WebContents pointer directly.
    
    BUG=309964
    
    Review URL: https://codereview.chromium.org/48273002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231869 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/select_file_dialog_extension_unittest.cc
+++ chrome/browser/ui/views/select_file_dialog_extension_unittest.cc
@@ -11,7 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-const int32 kDefaultTabId = 0;
+const SelectFileDialogExtension::RoutingID kDefaultRoutingID =
+    SelectFileDialogExtension::RoutingID();
 
 }  // namespace
 
@@ -26,9 +27,9 @@ class SelectFileDialogExtensionTest : public testing::Test {
     SelectFileDialogExtension* dialog = new SelectFileDialogExtension(listener,
                                                                       NULL);
     // Simulate the dialog opening.
-    EXPECT_FALSE(SelectFileDialogExtension::PendingExists(kDefaultTabId));
-    dialog->AddPending(kDefaultTabId);
-    EXPECT_TRUE(SelectFileDialogExtension::PendingExists(kDefaultTabId));
+    EXPECT_FALSE(SelectFileDialogExtension::PendingExists(kDefaultRoutingID));
+    dialog->AddPending(kDefaultRoutingID);
+    EXPECT_TRUE(SelectFileDialogExtension::PendingExists(kDefaultRoutingID));
     return dialog;
   }
 
@@ -93,7 +94,8 @@ TEST_F(SelectFileDialogExtensionTest, FileSelected) {
       CreateDialog(listener.get());
   // Simulate selecting a file.
   ui::SelectedFileInfo info;
-  SelectFileDialogExtension::OnFileSelected(kDefaultTabId, info, kFileIndex);
+  SelectFileDialogExtension::OnFileSelected(kDefaultRoutingID, info,
+                                            kFileIndex);
   // Simulate closing the dialog so the listener gets invoked.
   dialog->ExtensionDialogClosing(NULL);
   EXPECT_TRUE(listener->selected());
@@ -105,7 +107,7 @@ TEST_F(SelectFileDialogExtensionTest, FileSelectionCanceled) {
   scoped_refptr<SelectFileDialogExtension> dialog =
       CreateDialog(listener.get());
   // Simulate cancelling the dialog.
-  SelectFileDialogExtension::OnFileSelectionCanceled(kDefaultTabId);
+  SelectFileDialogExtension::OnFileSelectionCanceled(kDefaultRoutingID);
   // Simulate closing the dialog so the listener gets invoked.
   dialog->ExtensionDialogClosing(NULL);
   EXPECT_FALSE(listener->selected());
@@ -117,7 +119,7 @@ TEST_F(SelectFileDialogExtensionTest, SelfDeleting) {
   // Ensure we don't crash or trip an Address Sanitizer warning about
   // use-after-free.
   ui::SelectedFileInfo file_info;
-  SelectFileDialogExtension::OnFileSelected(kDefaultTabId, file_info, 0);
+  SelectFileDialogExtension::OnFileSelected(kDefaultRoutingID, file_info, 0);
   // Simulate closing the dialog so the listener gets invoked.
   client->dialog()->ExtensionDialogClosing(NULL);
 }
