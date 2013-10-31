be11d73470300bb48303af16eb90e1e1ff61d709
peria@chromium.org <peria@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-31 01:25:15 +0000

    Show subs box on clicking breakable categories in dmprof visualizer.
    - For clicks on the graph, the box shows up on the cursor.
    - For clicks on the menu view, the box shows up below the menu box.
    - Clicks on non-breakable categories erase the box.
    - Clicks out of graph and menu-box do nothing.
    
    
    BUG=259206
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/39243003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231969 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/deep_memory_profiler/visualizer/services.py
+++ tools/deep_memory_profiler/visualizer/services.py
@@ -3,6 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+# This file is expected to be used under another directory to use,
+# so we disable checking import path of GAE tools from this directory.
+# pylint: disable=F0401,E0611,W0232
+
 import json
 
 from google.appengine.ext import blobstore
