c9a2a22352e8d9e701c6f292ffd289fe3e538da7
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-31 22:56:30 +0000

    Mojo: add generation of stubs and struct serialization
    BUG=None
    TEST=Will be added after all generated files are done
    R=darin@chromium.org, darin
    
    Review URL: https://codereview.chromium.org/54303006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232230 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ mojo/public/bindings/generators/module_struct_serialization.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright $YEAR The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef $HEADER_GUARD
+#define $HEADER_GUARD
+
+#include "mojo/public/bindings/lib/bindings_serialization.h"
+
+namespace $NAMESPACE {
+class $CLASS;
+}
+
+namespace mojo {
+namespace internal {
+
+template <>
+class ObjectTraits<$FULL_CLASS> {
+ public:
+  static size_t ComputeSizeOf(const $FULL_CLASS* bar);
+  static $FULL_CLASS* Clone(const $FULL_CLASS* bar, Buffer* buf);
+  static void EncodePointersAndHandles($FULL_CLASS* bar,
+                                       std::vector<mojo::Handle>* handles);
+  static bool DecodePointersAndHandles($FULL_CLASS* bar,
+                                       const mojo::Message& message);
+};
+
+}  // namespace internal
+}  // namespace mojo
+
+#endif  // $HEADER_GUARD
