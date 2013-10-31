9a00687e89c0b53095eaffe17975febb262b3bf1
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-31 22:29:56 +0000

    Docserver: Introduce an annotation to CompiledFileSystem's compilation function which indicates it only relies on that data from a single file, not multiple files. This allows implementing an optimisation where the file data doesn't need to be re-compiled if the file stat is the same (note that it's never re-read because CachingFileSystem already implements a similar optimisation).
    This also replace a couple of uses of CompiledFileSystem.Create with the
    appropriate CompileFileSystem.For* method.
    
    BUG=305280
    R=jyasskin@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/54693004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232228 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/server2/sidenav_data_source.py
+++ chrome/common/extensions/docs/server2/sidenav_data_source.py
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import copy
 import logging
 
+from compiled_file_system import SingleFile
 from data_source import DataSource
 from future import Gettable, Future
 from third_party.json_schema_compiler.json_parse import Parse
@@ -51,6 +52,7 @@ class SidenavDataSource(DataSource):
     self._server_instance = server_instance
     self._request = request
 
+  @SingleFile
   def _CreateSidenavDict(self, _, content):
     items = Parse(content)
     # Start at level 2, the top <ul> element is level 1.
