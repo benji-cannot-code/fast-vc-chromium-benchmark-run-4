e2ce863a14e52c7a2f899c190385f026557233ed
fs@opera.com <fs@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-31 11:17:54 +0000

    Use TextResourceDecoder for the WebVTTParser
    Simplifies BOM handling, signature detection and fixes handling
    of "short" (< 9 bytes) inputs.
    
    Also fixes the signature checker to allow a formfeed character to
    follow the signature itself.
    
    BUG=231460
    
    Review URL: https://codereview.chromium.org/54203003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161049 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/track/WebVTTParser.h
+++ third_party/WebKit/Source/core/html/track/WebVTTParser.h
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLNames.h"
 #include "RuntimeEnabledFeatures.h"
 #include "core/dom/DocumentFragment.h"
+#include "core/fetch/TextResourceDecoder.h"
 #include "core/html/track/TextTrackCue.h"
 #include "core/html/track/TextTrackRegion.h"
 #include "core/html/track/WebVTTTokenizer.h"
@@ -115,10 +116,10 @@ private:
     Document* m_document;
     ParseState m_state;
 
-    bool hasRequiredFileIdentifier();
+    bool hasRequiredFileIdentifier(const String& line);
     ParseState collectCueId(const String&);
     ParseState collectTimingsAndSettings(const String&);
-    ParseState collectCueText(const String&, unsigned length, unsigned);
+    ParseState collectCueText(const String&, bool);
     ParseState ignoreBadCue(const String&);
 
     void createNewCue();
@@ -128,15 +129,15 @@ private:
     void createNewRegion();
 
     void skipWhiteSpace(const String&, unsigned*);
-    static void skipLineTerminator(const char* data, unsigned length, unsigned*);
-    static String collectNextLine(const char* data, unsigned length, unsigned*);
+    static void skipLineTerminator(const String& data, unsigned*);
+    static String collectNextLine(const String& data, unsigned*);
 
     void constructTreeFromToken(Document&);
 
     String m_currentHeaderName;
     String m_currentHeaderValue;
 
-    Vector<char> m_identifierData;
+    RefPtr<TextResourceDecoder> m_decoder;
     String m_currentId;
     double m_currentStartTime;
     double m_currentEndTime;
