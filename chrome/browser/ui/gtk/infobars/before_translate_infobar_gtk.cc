a07636ccab5383d6c109bae7790a127e8a77ab09
hajimehoshi@chromium.org <hajimehoshi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-31 09:19:01 +0000

    Collect UMAs (Modify*Lang) into TranslateUIDelegate
    Renamed TranslateUIDelegate::Set*LanguageIndex to Update*LanguageIndex and have them include UMA callings.
    
    BUG=312720
    TEST=unit_tests --gtest_filter=TranslateBubbleViewTest.*
    
    Review URL: https://codereview.chromium.org/52213002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232057 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/infobars/before_translate_infobar_gtk.cc
+++ chrome/browser/ui/gtk/infobars/before_translate_infobar_gtk.cc
@@ -5,11 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/gtk/infobars/before_translate_infobar_gtk.h"
 
-#include "base/metrics/histogram.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/translate/translate_infobar_delegate.h"
 #include "chrome/browser/ui/gtk/gtk_util.h"
-#include "components/translate/common/translate_metrics.h"
 #include "grit/generated_resources.h"
 #include "ui/base/gtk/gtk_hig_constants.h"
 #include "ui/base/gtk/gtk_signal_registrar.h"
@@ -90,9 +88,7 @@ bool BeforeTranslateInfoBar::ShowOptionsMenuButton() const {
 }
 
 void BeforeTranslateInfoBar::OnLanguageModified(GtkWidget* sender) {
-  UMA_HISTOGRAM_BOOLEAN(
-      translate::GetMetricsName(translate::UMA_MODIFY_ORIGINAL_LANG), true);
-  GetDelegate()->set_original_language_index(
+  GetDelegate()->UpdateOriginalLanguageIndex(
       GetLanguageComboboxActiveId(GTK_COMBO_BOX(sender)));
 }
 
