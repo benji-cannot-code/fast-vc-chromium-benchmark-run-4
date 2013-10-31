6f75ebda81e1fe0bd3089d6ffa76cffc418bdd8d
apiccion@chromium.org <apiccion@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-31 00:38:42 +0000

    Fixed SyncStatusHelper not updating observers on changes.
    * SyncStatusHelper now updates observers whenever cached contents
      changes, previously only updated on content resolver notifications.
    
    BUG=299123
    
    Review URL: https://codereview.chromium.org/26116009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231960 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/test/android/javatests/src/org/chromium/sync/test/util/MockSyncContentResolverDelegate.java
+++ sync/test/android/javatests/src/org/chromium/sync/test/util/MockSyncContentResolverDelegate.java
@@ -32,16 +32,20 @@ import java.util.concurrent.TimeUnit;
  */
 public class MockSyncContentResolverDelegate implements SyncContentResolverDelegate {
 
-    private final Map<String, Boolean> mSyncAutomaticallyMap;
+    private final Set<String> mSyncAutomaticallySet;
+    private final Map<String, Boolean> mIsSyncableMap;
+    private final Object mSyncableMapLock = new Object();
 
     private final Set<AsyncSyncStatusObserver> mObservers;
 
     private boolean mMasterSyncAutomatically;
+    private boolean mDisableObserverNotifications;
 
     private Semaphore mPendingObserverCount;
 
     public MockSyncContentResolverDelegate() {
-        mSyncAutomaticallyMap = new HashMap<String, Boolean>();
+        mSyncAutomaticallySet = new HashSet<String>();
+        mIsSyncableMap = new HashMap<String, Boolean>();
         mObservers = new HashSet<AsyncSyncStatusObserver>();
     }
 
@@ -81,22 +85,25 @@ public class MockSyncContentResolverDelegate implements SyncContentResolverDeleg
     @Override
     public boolean getSyncAutomatically(Account account, String authority) {
         String key = createKey(account, authority);
-        synchronized (mSyncAutomaticallyMap) {
-            return mSyncAutomaticallyMap.containsKey(key) && mSyncAutomaticallyMap.get(key);
+        synchronized (mSyncableMapLock) {
+            return mSyncAutomaticallySet.contains(key);
         }
     }
 
     @Override
     public void setSyncAutomatically(Account account, String authority, boolean sync) {
         String key = createKey(account, authority);
-        synchronized (mSyncAutomaticallyMap) {
-            if (!mSyncAutomaticallyMap.containsKey(key)) {
+        synchronized (mSyncableMapLock) {
+            if (!mIsSyncableMap.containsKey(key) || !mIsSyncableMap.get(key)) {
                 throw new IllegalArgumentException("Account " + account +
                         " is not syncable for authority " + authority +
                         ". Can not set sync state to " + sync);
             }
-            if (mSyncAutomaticallyMap.get(key) == sync) return;
-            mSyncAutomaticallyMap.put(key, sync);
+            if (sync) {
+                mSyncAutomaticallySet.add(key);
+            } else if (mSyncAutomaticallySet.contains(key)) {
+                mSyncAutomaticallySet.remove(key);
+            }
         }
         notifyObservers();
     }
@@ -105,17 +112,22 @@ public class MockSyncContentResolverDelegate implements SyncContentResolverDeleg
     public void setIsSyncable(Account account, String authority, int syncable) {
         String key = createKey(account, authority);
 
-        synchronized (mSyncAutomaticallyMap) {
+        synchronized (mSyncableMapLock) {
             switch (syncable) {
                 case 0:
-                    if (!mSyncAutomaticallyMap.containsKey(key)) return;
+                    if (mSyncAutomaticallySet.contains(key)) {
+                        mSyncAutomaticallySet.remove(key);
+                    }
 
-                    mSyncAutomaticallyMap.remove(key);
+                    mIsSyncableMap.put(key, false);
                     break;
                 case 1:
-                    if (mSyncAutomaticallyMap.containsKey(key)) return;
-
-                    mSyncAutomaticallyMap.put(key, false);
+                    mIsSyncableMap.put(key, true);
+                    break;
+                case -1:
+                    if (mIsSyncableMap.containsKey(key)) {
+                        mIsSyncableMap.remove(key);
+                    }
                     break;
                 default:
                     throw new IllegalArgumentException("Unable to understand syncable argument: " +
@@ -127,12 +139,13 @@ public class MockSyncContentResolverDelegate implements SyncContentResolverDeleg
 
     @Override
     public int getIsSyncable(Account account, String authority) {
-        synchronized (mSyncAutomaticallyMap) {
-            final Boolean isSyncable = mSyncAutomaticallyMap.get(createKey(account, authority));
-            if (isSyncable == null) {
+        String key = createKey(account, authority);
+        synchronized (mSyncableMapLock) {
+            if (mIsSyncableMap.containsKey(key)) {
+                return mIsSyncableMap.containsKey(key) ? 1 : 0;
+            } else {
                 return -1;
             }
-            return isSyncable ? 1 : 0;
         }
     }
 
@@ -141,6 +154,7 @@ public class MockSyncContentResolverDelegate implements SyncContentResolverDeleg
     }
 
     private void notifyObservers() {
+        if (mDisableObserverNotifications) return;
         synchronized (mObservers) {
             mPendingObserverCount = new Semaphore(1 - mObservers.size());
             for (AsyncSyncStatusObserver observer : mObservers) {
@@ -159,6 +173,10 @@ public class MockSyncContentResolverDelegate implements SyncContentResolverDeleg
     public void waitForLastNotificationCompleted() throws InterruptedException {
         Assert.assertTrue("Timed out waiting for notifications to complete.",
                 mPendingObserverCount.tryAcquire(5, TimeUnit.SECONDS));
+    }
+
+    public void disableObserverNotifications() {
+        mDisableObserverNotifications = true;
     }
 
     private static class AsyncSyncStatusObserver {
