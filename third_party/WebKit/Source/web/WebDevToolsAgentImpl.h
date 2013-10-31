337848b032d56c3f7daa70fc09ae73da4b3a5f1e
alph@chromium.org <alph@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-31 13:10:14 +0000

    DevTools: Rename TraceEventWithTimestampCallback -> TraceEventCallback
    Review URL: https://codereview.chromium.org/51063004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161056 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebDevToolsAgentImpl.h
+++ third_party/WebKit/Source/web/WebDevToolsAgentImpl.h
@@ -108,7 +108,7 @@ public:
 
     virtual void getAllocatedObjects(HashSet<const void*>&);
     virtual void dumpUncountedAllocatedObjects(const HashMap<const void*, size_t>&);
-    virtual void setTraceEventCallback(TraceEventWithTimestampCallback);
+    virtual void setTraceEventCallback(TraceEventCallback);
 
     virtual void dispatchKeyEvent(const WebCore::PlatformKeyboardEvent&);
     virtual void dispatchMouseEvent(const WebCore::PlatformMouseEvent&);
