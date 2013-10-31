bf4d4505a7bab210a4304cd25ed2bc2669465a44
dominik.rottsches@intel.com <dominik.rottsches@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-31 09:16:56 +0000

    GetDisplayForScreen cleanup
    Doing the primary display check inside the GetDisplayForScreen function
    so that the other utility functions can stop guessing about whether
    the display they return is primary or not.
    
    BUG=
    R=Nico
    
    Review URL: https://codereview.chromium.org/47073006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232054 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/screen_mac.mm
+++ ui/gfx/screen_mac.mm
@@ -43,15 +43,16 @@ NSScreen* GetMatchingScreen(const gfx::Rect& match_rect) {
   return max_screen;
 }
 
-gfx::Display GetDisplayForScreen(NSScreen* screen, bool is_primary) {
+gfx::Display GetDisplayForScreen(NSScreen* screen) {
   NSRect frame = [screen frame];
   // TODO(oshima): Implement ID and Observer.
   gfx::Display display(0, gfx::Rect(NSRectToCGRect(frame)));
 
   NSRect visible_frame = [screen visibleFrame];
+  NSScreen* primary = [[NSScreen screens] objectAtIndex:0];
 
   // Convert work area's coordinate systems.
-  if (is_primary) {
+  if ([screen isEqual:primary]) {
     gfx::Rect work_area = gfx::Rect(NSRectToCGRect(visible_frame));
     work_area.set_y(frame.size.height - visible_frame.origin.y -
                     visible_frame.size.height);
@@ -143,7 +144,7 @@ class ScreenMac : public gfx::Screen {
     NSScreen* match_screen = [window screen];
     if (!match_screen)
       return GetPrimaryDisplay();
-    return GetDisplayForScreen(match_screen, false /* may not be primary */);
+    return GetDisplayForScreen(match_screen);
   }
 
   virtual gfx::Display GetDisplayNearestPoint(
@@ -155,7 +156,7 @@ class ScreenMac : public gfx::Screen {
     ns_point.y = NSMaxY([primary frame]) - ns_point.y;
     for (NSScreen* screen in screens) {
       if (NSMouseInRect(ns_point, [screen frame], NO))
-        return GetDisplayForScreen(screen, screen == primary);
+        return GetDisplayForScreen(screen);
     }
     return GetPrimaryDisplay();
   }
@@ -164,7 +165,7 @@ class ScreenMac : public gfx::Screen {
   virtual gfx::Display GetDisplayMatching(
       const gfx::Rect& match_rect) const OVERRIDE {
     NSScreen* match_screen = GetMatchingScreen(match_rect);
-    return GetDisplayForScreen(match_screen, false /* may not be primary */);
+    return GetDisplayForScreen(match_screen);
   }
 
   // Returns the primary display.
@@ -172,7 +173,7 @@ class ScreenMac : public gfx::Screen {
     // Primary display is defined as the display with the menubar,
     // which is always at index 0.
     NSScreen* primary = [[NSScreen screens] objectAtIndex:0];
-    gfx::Display display = GetDisplayForScreen(primary, true /* primary */);
+    gfx::Display display = GetDisplayForScreen(primary);
     return display;
   }
 
