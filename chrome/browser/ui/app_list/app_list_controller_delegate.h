95e189d6dfd0857e1c929b0f7b6ad6596e24c622
rpaquay@chromium.org <rpaquay@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-31 19:49:04 +0000

    Revert 232056 "Move ExtensionSystem references out of ExtensionA..."
    > Move ExtensionSystem references out of ExtensionAppModelBuilder.
    > 
    > BUG=310376
    > 
    > Review URL: https://codereview.chromium.org/52143002
    
    TBR=koz@chromium.org
    
    Review URL: https://codereview.chromium.org/49533004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232188 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/app_list/app_list_controller_delegate.h
+++ chrome/browser/ui/app_list/app_list_controller_delegate.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_prefs.h"
 #include "ui/gfx/native_widget_types.h"
 
-class ExtensionSet;
 class Profile;
 
 namespace base {
@@ -20,7 +19,6 @@ class FilePath;
 
 namespace extensions {
 class Extension;
-class InstallTracker;
 }
 
 namespace gfx {
@@ -137,10 +135,6 @@ class AppListControllerDelegate {
   // Returns true if the given extension is installed.
   bool IsExtensionInstalled(Profile* profile, const std::string& app_id);
 
-  extensions::InstallTracker* GetInstallTrackerFor(Profile* profile);
-
-  // Get the list of installed apps for the given profile.
-  void GetApps(Profile* profile, ExtensionSet* out_apps);
 };
 
 #endif  // CHROME_BROWSER_UI_APP_LIST_APP_LIST_CONTROLLER_DELEGATE_H_
