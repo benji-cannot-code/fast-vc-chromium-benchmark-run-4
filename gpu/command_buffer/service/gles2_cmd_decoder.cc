16aefcb8985552c473f63433e58d00b72f7fe70c
sievers@chromium.org <sievers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-31 18:08:01 +0000

    Don't call into driver for unsupported extensions.
    Check extension availability for glDiscardFramebufferEXT()
    and glBlitFramebufferEXT().
    
    Adds some tests for glDiscardFramebuffer() also.
    
    Review URL: https://codereview.chromium.org/46673007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232165 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/gles2_cmd_decoder.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder.cc
@@ -3893,6 +3893,13 @@ void GLES2DecoderImpl::DoDisableVertexAttribArray(GLuint index) {
 void GLES2DecoderImpl::DoDiscardFramebufferEXT(GLenum target,
                                                GLsizei numAttachments,
                                                const GLenum* attachments) {
+  if (!features().ext_discard_framebuffer) {
+    LOCAL_SET_GL_ERROR(GL_INVALID_OPERATION,
+                       "glDiscardFramebufferEXT",
+                       "function not available");
+    return;
+  }
+
   Framebuffer* framebuffer =
       GetFramebufferInfoForTarget(GL_FRAMEBUFFER);
 
@@ -4978,6 +4985,7 @@ void GLES2DecoderImpl::DoBlitFramebufferEXT(
     LOCAL_SET_GL_ERROR(
         GL_INVALID_OPERATION,
         "glBlitFramebufferEXT", "function not available");
+    return;
   }
 
   if (!CheckBoundFramebuffersValid("glBlitFramebufferEXT")) {
