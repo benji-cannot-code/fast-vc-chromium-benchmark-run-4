bf1b5f2aa771c0d1caffa7814bc84784c0e2a805
yukawa@chromium.org <yukawa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-31 09:48:26 +0000

    Add missing #includes of <algorithm> for std::min/std::max
    Required when building on VS2013.
    
    BUG=288948
    
    Review URL: https://codereview.chromium.org/52823005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232070 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/animation/tween.cc
+++ ui/gfx/animation/tween.cc
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <float.h>
 #endif
 
+#include <algorithm>
+
 #include "base/basictypes.h"
 #include "base/logging.h"
 #include "ui/gfx/safe_integer_conversions.h"
