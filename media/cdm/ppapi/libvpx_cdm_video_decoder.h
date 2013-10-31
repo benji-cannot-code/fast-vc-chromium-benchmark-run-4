46412bb08de1bf797bc82f2dde12ee05a1d4e155
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-31 18:06:50 +0000

    Revert 232092 "Revert 232043 "Update ClearKeyCdm to use CDM::Host and CDM::Host...""
    This relands r232043:
    
    Update ClearKeyCdm to use CDM::Host and CDM::Host::kVersion.
    
    R=ddorwin@chromium.org
    TBR=ddorwin@chromium.org, tommyw@chromium.org
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/54973002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232161 0039d316-1c4b-4281-b951-d872f2087c98

--- media/cdm/ppapi/libvpx_cdm_video_decoder.h
+++ media/cdm/ppapi/libvpx_cdm_video_decoder.h
@@ -18,7 +18,7 @@ namespace media {
 
 class LibvpxCdmVideoDecoder : public CdmVideoDecoder {
  public:
-  explicit LibvpxCdmVideoDecoder(cdm::Host* host);
+  explicit LibvpxCdmVideoDecoder(CdmHost* host);
   virtual ~LibvpxCdmVideoDecoder();
 
   // CdmVideoDecoder implementation.
@@ -43,7 +43,7 @@ class LibvpxCdmVideoDecoder : public CdmVideoDecoder {
 
   bool is_initialized_;
 
-  cdm::Host* const host_;
+  CdmHost* const host_;
 
   vpx_codec_ctx* vpx_codec_;
   vpx_image* vpx_image_;
--- media/media_cdm.gypi
+++ media/media_cdm.gypi
@@ -84,6 +84,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'cdm/ppapi/cdm_video_decoder.h',
         'cdm/ppapi/clear_key_cdm.cc',
         'cdm/ppapi/clear_key_cdm.h',
+        'cdm/ppapi/clear_key_cdm_common.h',
       ],
       # TODO(jschuh): crbug.com/167187 fix size_t to int truncations.
       'msvs_disabled_warnings': [ 4267, ],
