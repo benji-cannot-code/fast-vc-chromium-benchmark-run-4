8968b54141c7723d5842607cf4371f664d2b7e0e
tommyw@chromium.org <tommyw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-31 15:22:33 +0000

    Revert 231659 "Added flag to enable AVFoundation in Mac"
    Patch reverted by request of the author.
    
    > Added flag to enable AVFoundation in Mac
    > 
    > From the flag documentation:
    > " AVFoundation is available in versions 10.7 and onwards, and is to be used
    > http://crbug.com/288562 for both audio and video device monitoring and for
    > video capture. Being a dynamically loaded NSBundle and library, it hits the
    > Chrome startup time (http://crbug.com/311325 and http://crbug.com/311437);
    > until development is finished and the library load time issue is solved, the
    > usage of this library is hidden behind this flag. "
    > 
    > BUG=288562, 311325, 311437
    > 
    > Review URL: https://codereview.chromium.org/50213004
    
    TBR=mcasas@chromium.org
    
    Review URL: https://codereview.chromium.org/51863003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232121 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/media_switches.h
+++ media/base/media_switches.h
@@ -41,7 +41,6 @@ MEDIA_EXPORT extern const char kAlsaOutputDevice[];
 
 #if defined(OS_MACOSX)
 MEDIA_EXPORT extern const char kDisableMainThreadAudio[];
-MEDIA_EXPORT extern const char kEnableAVFoundation[];
 #endif
 
 #if defined(OS_WIN)
--- media/video/capture/mac/avfoundation_glue.mm
+++ media/video/capture/mac/avfoundation_glue.mm
@@ -7,14 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <dlfcn.h>
 
-#include "base/command_line.h"
 #include "base/mac/mac_util.h"
-#include "media/base/media_switches.h"
 
 bool AVFoundationGlue::IsAVFoundationSupported() {
-  const CommandLine* cmd_line = CommandLine::ForCurrentProcess();
-  return cmd_line->HasSwitch(switches::kEnableAVFoundation) &&
-      base::mac::IsOSLionOrLater() && [AVFoundationBundle() load];
+  return (base::mac::IsOSLionOrLater() && [AVFoundationBundle() load]);
 }
 
 NSBundle const* AVFoundationGlue::AVFoundationBundle() {
