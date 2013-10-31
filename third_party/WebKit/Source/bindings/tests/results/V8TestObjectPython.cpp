34c74c862150414d5a4891bdeb43a698448326d1
nbarth@chromium.org <nbarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-31 07:56:46 +0000

    IDL compiler: Use C++ type for local variable in [Clamp]
    In current compiler, [Clamp] uses a local variable with the IDL type
    (|unsigned short| or |unsigned long|), instead of the C++ type (in both
    these cases |unsigned|).
    This is then implicitly converted to |unsigned| on use.
    It's clearer (and likely safer and more correct) to use the internal C++
    type from the start, which this CL does.
    
    BUG=239771
    
    Review URL: https://codereview.chromium.org/54033002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161035 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/tests/results/V8TestObjectPython.cpp
+++ third_party/WebKit/Source/bindings/tests/results/V8TestObjectPython.cpp
@@ -4826,7 +4826,7 @@ static void voidMethodClampUnsignedShortArgMethod(const v8::FunctionCallbackInfo
         return;
     }
     TestObjectPython* imp = V8TestObjectPython::toNative(args.Holder());
-    unsigned short clampUnsignedShortArg = 0;
+    unsigned clampUnsignedShortArg = 0;
     V8TRYCATCH_VOID(double, clampUnsignedShortArgNativeValue, args[0]->NumberValue());
     if (!std::isnan(clampUnsignedShortArgNativeValue))
         clampUnsignedShortArg = clampTo<unsigned short>(clampUnsignedShortArgNativeValue);
@@ -4847,7 +4847,7 @@ static void voidMethodClampUnsignedLongArgMethod(const v8::FunctionCallbackInfo<
         return;
     }
     TestObjectPython* imp = V8TestObjectPython::toNative(args.Holder());
-    unsigned long clampUnsignedLongArg = 0;
+    unsigned clampUnsignedLongArg = 0;
     V8TRYCATCH_VOID(double, clampUnsignedLongArgNativeValue, args[0]->NumberValue());
     if (!std::isnan(clampUnsignedLongArgNativeValue))
         clampUnsignedLongArg = clampTo<unsigned long>(clampUnsignedLongArgNativeValue);
