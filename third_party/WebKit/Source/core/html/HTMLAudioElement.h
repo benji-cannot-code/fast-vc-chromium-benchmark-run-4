1181bbc1571bc19860557ea903d2617e79a79166
philipj@opera.com <philipj@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-31 16:42:40 +0000

    Add OVERRIDE where possible in HTMLMediaElement and children
    Some of the places where it wasn't added are in order, but some of
    them simply shouldn't be virtual and will be dealt with later.
    
    BUG=313702
    
    Review URL: https://codereview.chromium.org/51763009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161068 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLAudioElement.h
+++ third_party/WebKit/Source/core/html/HTMLAudioElement.h
@@ -42,7 +42,7 @@ public:
 private:
     HTMLAudioElement(const QualifiedName&, Document&, bool);
 
-    virtual bool isVideo() const { return false; }
+    virtual bool isVideo() const OVERRIDE { return false; }
 };
 
 } //namespace
