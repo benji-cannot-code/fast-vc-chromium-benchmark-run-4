8eeb1f8adab23f910f40c75c40a308ec9f7f33e2
dmazzoni@chromium.org <dmazzoni@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-01 17:59:40 +0000

    This adds support for the InlineTextBox role in the AX tree, and its attributes.
    Depends on this Blink change that adds InlineTextBox support.
    https://codereview.chromium.org/23983002/
    
    This change only adds support and makes minimal changes to
    allow tests to pass. Other changes will take advantage of the
    inline text boxes to compute boundsForRange, etc.
    
    BUG=98977
    
    Review URL: https://codereview.chromium.org/25943003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232429 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/accessibility/browser_accessibility_android.cc
+++ content/browser/accessibility/browser_accessibility_android.cc
@@ -25,7 +25,7 @@ bool BrowserAccessibilityAndroid::IsNative() const {
   return true;
 }
 
-bool BrowserAccessibilityAndroid::IsLeaf() const {
+bool BrowserAccessibilityAndroid::PlatformIsLeaf() const {
   if (child_count() == 0)
     return true;
 
@@ -53,7 +53,7 @@ bool BrowserAccessibilityAndroid::IsLeaf() const {
   if (HasOnlyStaticTextChildren())
     return true;
 
-  return false;
+  return BrowserAccessibility::PlatformIsLeaf();
 }
 
 bool BrowserAccessibilityAndroid::IsCheckable() const {
@@ -76,7 +76,7 @@ bool BrowserAccessibilityAndroid::IsChecked() const {
 }
 
 bool BrowserAccessibilityAndroid::IsClickable() const {
-  return (IsLeaf() && !GetText().empty());
+  return (PlatformIsLeaf() && !GetText().empty());
 }
 
 bool BrowserAccessibilityAndroid::IsEnabled() const {
@@ -186,8 +186,8 @@ string16 BrowserAccessibilityAndroid::GetText() const {
     text = base::UTF8ToUTF16(value());
 
   if (text.empty() && HasOnlyStaticTextChildren()) {
-    for (uint32 i = 0; i < child_count(); i++) {
-      BrowserAccessibility* child = GetChild(i);
+    for (uint32 i = 0; i < PlatformChildCount(); i++) {
+      BrowserAccessibility* child = PlatformGetChild(i);
       text += static_cast<BrowserAccessibilityAndroid*>(child)->GetText();
     }
   }
@@ -234,7 +234,7 @@ int BrowserAccessibilityAndroid::GetItemCount() const {
   switch(role()) {
     case WebKit::WebAXRoleList:
     case WebKit::WebAXRoleListBox:
-      count = child_count();
+      count = PlatformChildCount();
       break;
     case WebKit::WebAXRoleSlider:
     case WebKit::WebAXRoleProgressIndicator: {
@@ -340,8 +340,8 @@ int BrowserAccessibilityAndroid::GetEditableTextLength() const {
 }
 
 bool BrowserAccessibilityAndroid::HasFocusableChild() const {
-  for (uint32 i = 0; i < child_count(); i++) {
-    BrowserAccessibility* child = GetChild(i);
+  for (uint32 i = 0; i < PlatformChildCount(); i++) {
+    BrowserAccessibility* child = PlatformGetChild(i);
     if (child->HasState(WebKit::WebAXStateFocusable))
       return true;
     if (static_cast<BrowserAccessibilityAndroid*>(child)->HasFocusableChild())
@@ -351,8 +351,8 @@ bool BrowserAccessibilityAndroid::HasFocusableChild() const {
 }
 
 bool BrowserAccessibilityAndroid::HasOnlyStaticTextChildren() const {
-  for (uint32 i = 0; i < child_count(); i++) {
-    BrowserAccessibility* child = GetChild(i);
+  for (uint32 i = 0; i < PlatformChildCount(); i++) {
+    BrowserAccessibility* child = PlatformGetChild(i);
     if (child->role() != WebKit::WebAXRoleStaticText)
       return false;
   }
--- content/renderer/accessibility/renderer_accessibility_complete.cc
+++ content/renderer/accessibility/renderer_accessibility_complete.cc
@@ -37,6 +37,7 @@ RendererAccessibilityComplete::RendererAccessibilityComplete(
       last_scroll_offset_(gfx::Size()),
       ack_pending_(false) {
   WebAXObject::enableAccessibility();
+  WebAXObject::enableInlineTextBoxAccessibility();
 
   const WebDocument& document = GetMainDocument();
   if (!document.isNull()) {
