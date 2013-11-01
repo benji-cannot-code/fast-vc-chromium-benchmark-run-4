1ea4042d5f441d465c5d01a05bd40b81ea42efb4
teravest@chromium.org <teravest@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-01 19:32:57 +0000

    Pepper: Move FileIO host from renderer to browser.
    This change is large because it moves QuotaFileIO and PepperFileIOHost all at
    once to the browser process. Some code in the refactored PepperFileIOHost is
    moved from what's provided in FileAPIMessageFilter.
    
    Tested locally with Bastion, From Dust, and Angry Bots.
    
    TBR=jschuh
    BUG=246396
    
    Review URL: https://codereview.chromium.org/33053002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232440 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/pepper/pepper_file_ref_host.h
+++ content/browser/renderer_host/pepper/pepper_file_ref_host.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/browser/fileapi/file_system_url.h"
 
 namespace content {
-
 class PepperFileRefHost;
 
 // Internal and external filesystems have very different codepaths for
