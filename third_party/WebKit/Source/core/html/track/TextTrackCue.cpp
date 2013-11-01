2fa5c535cd78d857412003c5f91289de6755ef83
philipj@opera.com <philipj@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-01 16:02:46 +0000

    Remove virtual where not needed in TextTrackCue
    Remove TextTrackCue::cueType() since it always returned CueType::WebVTT
    and was never overriden. CueType::Generic was for TextTrackCueGeneric
    which was removed in r158977.
    
    Remove TextTrackCue::videoSizeDidChange(...) since it did nothing and
    was never overridden. This looks like unfinished logic for moving cues
    when the controls are shown and hidden, and could be revived in
    <http://crbug.com/314066>.
    
    BUG=313287
    
    Review URL: https://codereview.chromium.org/50903009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161172 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/track/TextTrackCue.cpp
+++ third_party/WebKit/Source/core/html/track/TextTrackCue.cpp
@@ -1209,9 +1209,6 @@ Document& TextTrackCue::document() const
 
 bool TextTrackCue::operator==(const TextTrackCue& cue) const
 {
-    if (cueType() != cue.cueType())
-        return false;
-
     if (m_endTime != cue.endTime())
         return false;
     if (m_startTime != cue.startTime())
