2d32188541f39339cb614fe1c6c2d80eae1a417e
rob.buis@samsung.com <rob.buis@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-01 03:00:38 +0000

    Remove rendering dependency in GraphicsContextAnnotation
    BUG=297477
    
    Review URL: https://codereview.chromium.org/55223002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161125 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderFrameSet.cpp
+++ third_party/WebKit/Source/core/rendering/RenderFrameSet.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/frame/Frame.h"
 #include "core/platform/Cursor.h"
 #include "core/platform/graphics/GraphicsContext.h"
+#include "core/rendering/GraphicsContextAnnotator.h"
 #include "core/rendering/PaintInfo.h"
 #include "core/rendering/RenderFrame.h"
 #include "core/rendering/RenderView.h"
--- third_party/WebKit/Source/core/rendering/RenderListMarker.cpp
+++ third_party/WebKit/Source/core/rendering/RenderListMarker.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/fetch/ImageResource.h"
 #include "core/platform/graphics/Font.h"
 #include "core/platform/graphics/GraphicsContextStateSaver.h"
+#include "core/rendering/GraphicsContextAnnotator.h"
 #include "core/rendering/RenderLayer.h"
 #include "core/rendering/RenderListItem.h"
 #include "core/rendering/RenderView.h"
