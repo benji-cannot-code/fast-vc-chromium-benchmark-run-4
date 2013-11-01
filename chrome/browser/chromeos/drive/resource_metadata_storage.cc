42c92049b9e0423621c62369ecd8a7f54d5b81f6
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-01 22:08:57 +0000

    drive: Record UMA histogram "Drive.MetadataDBInitResult" with more granularity
    To make it possible to track how often DB gets discarded in the real world.
    
    Add values to DriveMetadataDBInitStatus enum and use it to record "Drive.MetadataDBInitResult"
    
    BUG=277333
    
    Review URL: https://codereview.chromium.org/45933005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232478 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/resource_metadata_storage.cc
+++ chrome/browser/chromeos/drive/resource_metadata_storage.cc
@@ -31,6 +31,9 @@ enum DBInitStatus {
   DB_INIT_FAILED,
   DB_INIT_INCOMPATIBLE,
   DB_INIT_BROKEN,
+  DB_INIT_OPENED_EXISTING_DB,
+  DB_INIT_CREATED_NEW_DB,
+  DB_INIT_REPLACED_EXISTING_DB_WITH_NEW_DB,
   DB_INIT_MAX_VALUE,
 };
 
@@ -436,7 +439,7 @@ bool ResourceMetadataStorage::Initialize() {
                             open_existing_result,
                             DB_INIT_MAX_VALUE);
 
-  DBInitStatus init_result = DB_INIT_SUCCESS;
+  DBInitStatus init_result = DB_INIT_OPENED_EXISTING_DB;
 
   // Failed to open the existing DB, create new DB.
   if (!resource_map_) {
@@ -454,9 +457,12 @@ bool ResourceMetadataStorage::Initialize() {
     if (status.ok()) {
       resource_map_.reset(db);
 
-      if (!PutHeader(GetDefaultHeaderEntry()) ||  // Set up header.
-          !MoveIfPossible(preserved_resource_map_path,  // Trash the old DB.
-                          trashed_resource_map_path)) {
+      if (PutHeader(GetDefaultHeaderEntry()) &&  // Set up header.
+          MoveIfPossible(preserved_resource_map_path,  // Trash the old DB.
+                         trashed_resource_map_path)) {
+        init_result = open_existing_result == DB_INIT_NOT_FOUND ?
+            DB_INIT_CREATED_NEW_DB : DB_INIT_REPLACED_EXISTING_DB_WITH_NEW_DB;
+      } else {
         init_result = DB_INIT_FAILED;
         resource_map_.reset();
       }
