db517f8b0b485099a35e821fc2cfbbb29d714357
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-01 05:48:42 +0000

    cros: Restore Alt-minus window minimize shortcut
    We're going to address these shortcuts in M33.
    
    BUG=313784
    TEST=Both Ctrl-M and Alt-minus minimize an open browser window and restore a minimized browser window
    TBR=xiyuan@chromium.org for trivial change to c/b/resources/chromeos/
    
    Review URL: https://codereview.chromium.org/54273008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232306 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/keyboard_overlay_data.js
+++ chrome/browser/resources/chromeos/keyboard_overlay_data.js
@@ -15765,6 +15765,7 @@ var keyboardOverlayData = {
     '+<>ALT<>SHIFT': 'keyboardOverlayCenterWindow',
     '+<>CTRL': 'keyboardOverlayZoomIn',
     '+<>CTRL<>SHIFT': 'keyboardOverlayZoomScreenIn',
+    '-<>ALT': 'keyboardOverlayMinimizeWindow',
     '-<>CTRL': 'keyboardOverlayZoomOut',
     '-<>CTRL<>SHIFT': 'keyboardOverlayZoomScreenOut',
     '-<>SEARCH': 'keyboardOverlayF11',
