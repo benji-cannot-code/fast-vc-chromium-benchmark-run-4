benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit a6e217fecc1182d7a0781c0b55e7e2ef08a23ff9

Settings: Remove delayed-focus hacks.
InlineEditableItem.editing deferred focus on an INPUT element because
it can be called in a mousedown event handler, and the default handler set focus
on a parent LIST element later. This CL changes InlineEditableItem so that it
focuses on the INPUT element immediately, and avoid to focus on the
parent LIST element in such case.

Also, we can remove setTimeout() in List.handleElementBlur_. We can refer to
FocusEvent.relatedTarget about new focused element.

BUG=271437

Review URL: https://codereview.chromium.org/25605004

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227470 0039d316-1c4b-4281-b951-d872f2087c98

ui/webui/resources/js/cr/ui/list.js
@@ -314,7 +314,7 @@ cr.define('cr.ui', function() {
       this.selectionModel = new ListSelectionModel(length);
 
       this.addEventListener('dblclick', this.handleDoubleClick_);
-      this.addEventListener('mousedown', this.handlePointerDownUp_);
+      this.addEventListener('mousedown', this.handleMouseDown_);
       this.addEventListener('mouseup', this.handlePointerDownUp_);
       this.addEventListener('keydown', this.handleKeyDown);
       this.addEventListener('focus', this.handleElementFocus_, true);
@@ -495,6 +495,44 @@ cr.define('cr.ui', function() {
     },
 
     /**
+     * Try focusing on |eventTarget| or its ancestor under |root|.
+     * This is a helper for handleMouseDown_.
+     * @param {!Element} start An element which we start to try.
+     * @param {!Element} root An element which we finish to try.
+     * @return {boolean} True if we focused on an element successfully.
+     * @private
+     */
+    tryFocusOnAncestor_: function(start, root) {
+      for (var element = start; element && element != root;
+          element = element.parentElement) {
+        element.focus();
+        if (root.ownerDocument.activeElement == element)
+          return true;
+      }
+      return false;
+    },
+
+    /**
+     * Mousedown event handler.
+     * @param {MouseEvent} e The mouse event object.
+     * @private
+     */
+    handleMouseDown_: function(e) {
+      this.handlePointerDownUp_(e);
+      // If non-focusable area in a list item is clicked and the item still
+      // contains the focused element, the item did a special focus handling
+      // [1] and we should not focus on the list.
+      //
+      // [1] For example, clicking non-focusable area gives focus on the first
+      // form control in the item.
+      var listItem = this.getListItemAncestor(e.target);
+      if (listItem && !this.tryFocusOnAncestor_(e.target, listItem) &&
+          listItem.contains(listItem.ownerDocument.activeElement)) {
+        e.preventDefault();
+      }
+    },
+
+    /**
      * Called when an element in the list is focused. Marks the list as having
      * a focused element, and dispatches an event if it didn't have focus.
      * @param {Event} e The focus event.
@@ -513,18 +551,8 @@ cr.define('cr.ui', function() {
      * @private
      */
     handleElementBlur_: function(e) {
-      // When the blur event happens we do not know who is getting focus so we
-      // delay this a bit until we know if the new focus node is outside the
-      // list.
-      // We need 51 msec delay because InlineEditableList sets focus after
-      // 50 msec.
-      var list = this;
-      var doc = e.target.ownerDocument;
-      window.setTimeout(function() {
-        var activeElement = doc.activeElement;
-        if (!list.contains(activeElement))
-          list.hasElementFocus = false;
-      }, 51);
+      if (!this.contains(e.relatedTarget))
+        this.hasElementFocus = false;
     },
 
     /**

chrome/browser/resources/options/inline_editable_list.js
@@ -126,18 +126,13 @@ cr.define('options', function() {
         var focusElement = this.editClickTarget_ || this.initialFocusElement;
         this.editClickTarget_ = null;
 
-        // When this is called in response to the selectedChange event,
-        // the list grabs focus immediately afterwards. Thus we must delay
-        // our focus grab.
-        var self = this;
         if (focusElement) {
-          window.setTimeout(function() {
-            // Make sure we are still in edit mode by the time we execute.
-            if (self.editing) {
-              focusElement.focus();
-              focusElement.select();
-            }
-          }, 50);
+          focusElement.focus();
+          // select() doesn't work well in mousedown event handler.
+          setTimeout(function() {
+              if (focusElement.ownerDocument.activeElement == focusElement)
+                focusElement.select();
+          }, 0);
         }
       } else {
         if (!this.editCancelled_ && this.hasBeenEdited &&

commit 11e91d7ebbbba4b9242e721724436715820622e7

WebUI: Apply focus hack only if a list item gets selected.
This fixes two regressions by r227470 [1].

crbug.com/312611:
We didn't remove focus from inner elements by mousedown because the
list item contained the focused element. We don't need such behavior if the
list item is already selected.

crbug.com/313125:
MenuButton::hideMenu() sets focus on the menu button element. So
List::handleMouseDown thought a list item had a special focus handling,
and canceled the default behavior. This issue is also resolved by not
applying the hack for already-selected list item.

crbug.com/313611:
The focus hack should work only for left button.

[1] http://src.chromium.org/viewvc/chrome?view=rev&revision=227470

BUG=312611,313125,313611

Review URL: https://codereview.chromium.org/54053003

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232368 0039d316-1c4b-4281-b951-d872f2087c98

ui/webui/resources/js/cr/ui/list.js
@@ -1284,9 +1284,15 @@ cr.define('cr.ui', function() {
    * @param {MouseEvent} e The mouse event object.
    */
   function handleMouseDown(e) {
+    var listItem = this.getListItemAncestor(e.target);
+    var wasSelected = listItem && listItem.selected;
     this.handlePointerDownUp_(e);
 
-    if (e.defaultPrevented)
+    if (e.defaultPrevented || e.button != 0)
+      return;
+
+    // The following hack is required only if the listItem gets selected.
+    if (!listItem || wasSelected || !listItem.selected)
       return;
 
     // If non-focusable area in a list item is clicked and the item still
@@ -1295,8 +1301,7 @@ cr.define('cr.ui', function() {
     //
     // [1] For example, clicking non-focusable area gives focus on the first
     // form control in the item.
-    var listItem = this.getListItemAncestor(e.target);
-    if (listItem && !tryFocusOnAncestor(e.target, listItem) &&
+    if (!tryFocusOnAncestor(e.target, listItem) &&
         listItem.contains(listItem.ownerDocument.activeElement)) {
       e.preventDefault();
     }