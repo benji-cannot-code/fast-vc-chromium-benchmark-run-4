4a8a219cd0cb3b6e6fe07aec220adba2958a409b
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-01 16:58:53 +0000

    ash: Rename GetLauncherItemIndexForType to GetShelfItemIndexForType().
    - move launcher_model_util.* from launcher/ to shelf/
    - rename to shelf_model_util.*
    
    BUG=248353
    TEST=None, no functional changes
    R=harrym@chromium.org,jamescook@chromium.org
    
    Review URL: https://codereview.chromium.org/53513003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232415 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/shelf/shelf_model_util.cc
+++ ash/shelf/shelf_model_util.cc
@@ -3,14 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ash/launcher/launcher_model_util.h"
+#include "ash/shelf/shelf_model_util.h"
 
 #include "ash/launcher/launcher_model.h"
 
 namespace ash {
 
-int GetLauncherItemIndexForType(LauncherItemType type,
-                                const LauncherModel& launcher_model) {
+int GetShelfItemIndexForType(LauncherItemType type,
+                             const LauncherModel& launcher_model) {
   for (size_t i = 0; i < launcher_model.items().size(); i++) {
     if (launcher_model.items()[i].type == type)
       return i;
