97a8b56eee38f08353d5441c6aebbcff6088ad68
csharp@chromium.org <csharp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-01 19:59:04 +0000

    Disable flaky tests in BrailleDisplayPrivateApiTest.
    TBR=plundblad@chromium.org
    NOTRY=True
    BUG=314181
    
    Review URL: https://codereview.chromium.org/56473002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232451 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/braille_display_private/braille_display_private_apitest.cc
+++ chrome/browser/extensions/api/braille_display_private/braille_display_private_apitest.cc
@@ -178,7 +178,7 @@ class BrailleDisplayPrivateApiTest : public ExtensionApiTest {
   StubBrailleController stub_braille_controller_;
 };
 
-IN_PROC_BROWSER_TEST_F(BrailleDisplayPrivateApiTest, WriteDots) {
+IN_PROC_BROWSER_TEST_F(BrailleDisplayPrivateApiTest, DISABLED_WriteDots) {
   connection_data_.display_size = 11;
   ASSERT_TRUE(RunComponentExtensionTest("braille_display_private/write_dots"))
       << message_;
@@ -193,7 +193,7 @@ IN_PROC_BROWSER_TEST_F(BrailleDisplayPrivateApiTest, WriteDots) {
   }
 }
 
-IN_PROC_BROWSER_TEST_F(BrailleDisplayPrivateApiTest, KeyEvents) {
+IN_PROC_BROWSER_TEST_F(BrailleDisplayPrivateApiTest, DISABLED_KeyEvents) {
   connection_data_.display_size = 11;
   connection_data_.pending_keys.push_back(
       BRLAPI_KEY_TYPE_CMD | BRLAPI_KEY_CMD_LNUP);
