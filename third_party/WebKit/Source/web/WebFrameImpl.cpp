e43ac4db1ee3af3f6dc1a5345a50eec436ed0d84
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-01 17:38:01 +0000

    Add a unique frame id and save it on HistoryItem.
    This id is currently unused, but is a prerequisite for separating frame tree
    state from per-frame state in HistoryItem.
    
    BUG=312929
    
    Review URL: https://codereview.chromium.org/46783003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161180 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebFrameImpl.cpp
+++ third_party/WebKit/Source/web/WebFrameImpl.cpp
@@ -546,7 +546,7 @@ void WebFrameImpl::setName(const WebString& name)
 
 long long WebFrameImpl::embedderIdentifier() const
 {
-    return m_embedderIdentifier;
+    return m_frameInit->frameID();
 }
 
 WebVector<WebIconURL> WebFrameImpl::iconURLs(int iconTypesMask) const
@@ -2097,7 +2097,7 @@ WebFrameImpl* WebFrameImpl::create(WebFrameClient* client, long long embedderIde
 
 WebFrameImpl::WebFrameImpl(WebFrameClient* client, long long embedderIdentifier)
     : FrameDestructionObserver(0)
-    , m_frameLoaderClient(this)
+    , m_frameInit(WebFrameInit::create(this, embedderIdentifier))
     , m_client(client)
     , m_currentActiveMatchFrame(0)
     , m_activeMatchIndexInCurrentFrame(-1)
@@ -2112,7 +2112,6 @@ WebFrameImpl::WebFrameImpl(WebFrameClient* client, long long embedderIdentifier)
     , m_nextInvalidateAfter(0)
     , m_findMatchMarkersVersion(0)
     , m_findMatchRectsAreValid(false)
-    , m_embedderIdentifier(embedderIdentifier)
     , m_inSameDocumentHistoryLoad(false)
     , m_inputEventsScaleFactorForEmulation(1)
 {
@@ -2136,7 +2135,8 @@ void WebFrameImpl::setWebCoreFrame(WebCore::Frame* frame)
 
 void WebFrameImpl::initializeAsMainFrame(WebCore::Page* page)
 {
-    RefPtr<Frame> mainFrame = Frame::create(page, 0, &m_frameLoaderClient);
+    m_frameInit->setPage(page);
+    RefPtr<Frame> mainFrame = Frame::create(m_frameInit);
     setWebCoreFrame(mainFrame.get());
 
     // Add reference on behalf of FrameLoader. See comments in
@@ -2174,7 +2174,9 @@ PassRefPtr<Frame> WebFrameImpl::createChildFrame(const FrameLoadRequest& request
     // of this file for more info.
     webframe->ref();
 
-    RefPtr<Frame> childFrame = Frame::create(frame()->page(), ownerElement, &webframe->m_frameLoaderClient);
+    webframe->m_frameInit->setPage(frame()->page());
+    webframe->m_frameInit->setOwnerElement(ownerElement);
+    RefPtr<Frame> childFrame = Frame::create(webframe->m_frameInit);
     webframe->setWebCoreFrame(childFrame.get());
 
     childFrame->tree().setName(request.frameName());
