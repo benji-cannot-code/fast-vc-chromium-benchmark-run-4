2c42f09e37446828e2a0845ee680f96924467673
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-01 23:33:42 +0000

    Revert 232456 "Convert ExtensionProcessManager to BrowserContext..."
    > Convert ExtensionProcessManager to BrowserContext, part 1
    > 
    > Part of moving ExtensionProcessManager to src/extensions.
    > * Introduce more test coverage
    > * Convert most usage of Profile to BrowserContext
    > * Separate out "master" vs. incognito profiles/contexts
    > * Move switches into extensions/common/switches.h
    > 
    > BUG=313481
    > TEST=unit_tests ExtensionProcessManager* and browser_tests ExtensionProcessManager*
    > R=miket@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/52983004
    
    TBR=jamescook@chromium.org
    
    Review URL: https://codereview.chromium.org/47653004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232513 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/apps/app_browsertest_util.cc
+++ chrome/browser/apps/app_browsertest_util.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/apps/chrome_shell_window_delegate.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/extensions/application_launch.h"
+#include "chrome/common/chrome_switches.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/test_utils.h"
@@ -37,8 +38,8 @@ void PlatformAppBrowserTest::SetUpCommandLine(CommandLine* command_line) {
   ExtensionBrowserTest::SetUpCommandLine(command_line);
 
   // Make event pages get suspended quicker.
-  command_line->AppendSwitchASCII(switches::kEventPageIdleTime, "1");
-  command_line->AppendSwitchASCII(switches::kEventPageSuspendingTime, "1");
+  command_line->AppendSwitchASCII(::switches::kEventPageIdleTime, "1");
+  command_line->AppendSwitchASCII(::switches::kEventPageSuspendingTime, "1");
 }
 
 // static
