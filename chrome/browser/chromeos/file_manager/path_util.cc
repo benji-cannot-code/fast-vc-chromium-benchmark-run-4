73f26b662a8111f2b49dc095e4486b0b3cbc78bd
kinaba@chromium.org <kinaba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-01 01:54:19 +0000

    Add migration code for 'Downloads' path change for multi profile support.
    The actual change for the path is not happening yet,
    but it is possible to add the migration code first.
    
    BUG=309556
    
    Review URL: https://codereview.chromium.org/52193002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232272 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/file_manager/path_util.cc
+++ chrome/browser/chromeos/file_manager/path_util.cc
@@ -8,11 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/logging.h"
 #include "base/path_service.h"
+#include "chrome/browser/chromeos/drive/file_system_util.h"
 
 namespace file_manager {
 namespace util {
 
 const char kDownloadsFolderName[] = "Downloads";
+const base::FilePath::CharType kOldDownloadsFolderPath[] =
+    FILE_PATH_LITERAL("/home/chronos/user/Downloads");
 
 base::FilePath GetDownloadsFolderForProfile(Profile* profile) {
   // TODO(kinaba) crbug/309556: "Downloads" directory should be per-profile.
@@ -24,6 +27,29 @@ base::FilePath GetDownloadsFolderForProfile(Profile* profile) {
   CHECK(PathService::Get(base::DIR_HOME, &path));
   return path.AppendASCII(kDownloadsFolderName);
 }
+
+bool MigratePathFromOldFormat(Profile* profile,
+                              const base::FilePath& old_path,
+                              base::FilePath* new_path) {
+  // /special/drive/xxx => /special/drive/root/xxx
+  if (drive::util::NeedsNamespaceMigration(old_path)) {
+    *new_path = drive::util::ConvertToMyDriveNamespace(old_path);
+    return true;
+  }
+
+  // /home/chronos/user/Downloads/xxx => /home/chronos/u-hash/Downloads/xxx
+  const base::FilePath old_base(kOldDownloadsFolderPath);
+  base::FilePath relative;
+  if (old_path == old_base ||
+      old_base.AppendRelativePath(old_path, &relative)) {
+    const base::FilePath new_base = GetDownloadsFolderForProfile(profile);
+    *new_path = new_base.Append(relative);
+    return old_path != *new_path;
+  }
+
+  return false;
+}
+
 
 }  // namespace util
 }  // namespace file_manager
