230dde7215eebe1102ed6eb84f4487ebdda63217
dewittj@chromium.org <dewittj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-01 23:13:59 +0000

    Fix Mac ASAN compile error by including appropriate header.
    std::max was used but <algorithm> was not included.
    
    TBR=dewittj@chromium.org
    BUG=NONE
    
    Review URL: https://codereview.chromium.org/55913005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232510 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/message_center/cocoa/settings_entry_view.mm
+++ ui/message_center/cocoa/settings_entry_view.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ui/message_center/cocoa/settings_entry_view.h"
 
+#include <algorithm>
+
 #include "base/strings/sys_string_conversions.h"
 #include "grit/ui_resources.h"
 #include "skia/ext/skia_utils_mac.h"
