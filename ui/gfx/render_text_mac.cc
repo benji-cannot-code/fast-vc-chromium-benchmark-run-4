ca63c99a7f6633d8bf967786fab01272b29c3553
yukishiino@chromium.org <yukishiino@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-01 14:25:31 +0000

    Always aligns text at vertically center (Textfield, Label).
    Re-applies http://crrev.com/25039002 with only the change s/numeric_limits<int>::max()/INT_MAX/
    
    This CL makes Textfield and Label put the content text at vertically center respecting the cap height.  Also removes vertical alignment support (ALIGN_{TOP,BOTTOM,VCENTER}).
    
    BUG=146236, 264436
    TEST=Test manually + ui_unittests
    
    Review URL: https://codereview.chromium.org/54353002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232374 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/render_text_mac.cc
+++ ui/gfx/render_text_mac.cc
@@ -34,11 +34,6 @@ SizeF RenderTextMac::GetStringSizeF() {
   return string_size_;
 }
 
-int RenderTextMac::GetBaseline() {
-  EnsureLayout();
-  return common_baseline_;
-}
-
 SelectionModel RenderTextMac::FindCursorPosition(const Point& point) {
   // TODO(asvitkine): Implement this. http://crbug.com/131618
   return SelectionModel();
@@ -58,6 +53,11 @@ std::vector<RenderText::FontSpan> RenderTextMac::GetFontSpansForTesting() {
   }
 
   return spans;
+}
+
+int RenderTextMac::GetLayoutTextBaseline() {
+  EnsureLayout();
+  return common_baseline_;
 }
 
 SelectionModel RenderTextMac::AdjacentCharSelectionModel(
--- ui/gfx/render_text_win.cc
+++ ui/gfx/render_text_win.cc
@@ -494,11 +494,6 @@ Size RenderTextWin::GetStringSize() {
   return multiline_string_size_;
 }
 
-int RenderTextWin::GetBaseline() {
-  EnsureLayout();
-  return lines()[0].baseline;
-}
-
 SelectionModel RenderTextWin::FindCursorPosition(const Point& point) {
   if (text().empty())
     return SelectionModel();
@@ -540,6 +535,11 @@ std::vector<RenderText::FontSpan> RenderTextWin::GetFontSpansForTesting() {
   }
 
   return spans;
+}
+
+int RenderTextWin::GetLayoutTextBaseline() {
+  EnsureLayout();
+  return lines()[0].baseline;
 }
 
 SelectionModel RenderTextWin::AdjacentCharSelectionModel(
