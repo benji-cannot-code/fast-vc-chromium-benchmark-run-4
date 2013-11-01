2952f51625efd3e430be70d4613e5b2eafac8aa7
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-01 02:34:52 +0000

    Don't null the RenderView during document destruction
    Instead of nulling out the RenderView when we're going to
    destroy the Document, we explicitly check the document's
    lifecycle state for Stopping.
    
    I tried this once but it was reverted because recalcStyle()
    and updateLayout() were not updated to avoid rendering layout
    or recalcStyle when the render tree is being torn down.
    
    Review URL: https://codereview.chromium.org/54373002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161121 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -1013,7 +1013,7 @@ bool Document::cssCompositingEnabled() const
 
 PassRefPtr<DOMNamedFlowCollection> Document::webkitGetNamedFlows()
 {
-    if (!RuntimeEnabledFeatures::cssRegionsEnabled() || !renderView())
+    if (!RuntimeEnabledFeatures::cssRegionsEnabled() || !isActive())
         return 0;
 
     updateStyleIfNeeded();
@@ -1188,7 +1188,7 @@ String Document::suggestedMIMEType() const
 
 Element* Document::elementFromPoint(int x, int y) const
 {
-    if (!renderView())
+    if (!isActive())
         return 0;
 
     return TreeScope::elementFromPoint(x, y);
@@ -1196,7 +1196,7 @@ Element* Document::elementFromPoint(int x, int y) const
 
 PassRefPtr<Range> Document::caretRangeFromPoint(int x, int y)
 {
-    if (!renderView())
+    if (!isActive())
         return 0;
     LayoutPoint localPoint;
     RenderObject* renderer = rendererFromPoint(this, x, y, &localPoint);
@@ -1779,7 +1779,7 @@ void Document::updateLayout()
     updateStyleIfNeeded();
 
     // Only do a layout if changes have occurred that make it necessary.
-    if (frameView && renderView() && (frameView->layoutPending() || renderView()->needsLayout()))
+    if (frameView && isActive() && (frameView->layoutPending() || renderView()->needsLayout()))
         frameView->layout();
 
     if (frameView)
@@ -1986,20 +1986,13 @@ void Document::detach(const AttachContext& context)
     if (svgExtensions())
         accessSVGExtensions()->pauseAnimations();
 
-    RenderView* renderView = m_renderView;
+    m_renderView->setIsInWindow(false);
 
-    if (renderView)
-        renderView->setIsInWindow(false);
-
+    // FIXME: How can the frame be null here?
     if (m_frame) {
-        FrameView* view = m_frame->view();
-        if (view)
+        if (FrameView* view = m_frame->view())
             view->detachCustomScrollbars();
     }
-
-    // indicate destruction mode, i.e. confusingAndOftenMisusedAttached() but renderer == 0
-    setRenderer(0);
-    m_renderView = 0;
 
     m_hoverNode = 0;
     m_focusedElement = 0;
@@ -2012,9 +2005,6 @@ void Document::detach(const AttachContext& context)
     unscheduleStyleRecalc();
 
     clearStyleResolver();
-
-    if (renderView)
-        renderView->destroy();
 
     if (m_touchEventTargets && m_touchEventTargets->size() && parentDocument())
         parentDocument()->didRemoveEventTargetNode(this);
@@ -2071,7 +2061,7 @@ AXObjectCache* Document::existingAXObjectCache() const
 
     // If the renderer is gone then we are in the process of destruction.
     // This method will be called before m_frame = 0.
-    if (!topDocument()->renderView())
+    if (!topDocument()->isActive())
         return 0;
 
     return topDocument()->m_axObjectCache.get();
@@ -2089,7 +2079,7 @@ AXObjectCache* Document::axObjectCache() const
     Document* topDocument = this->topDocument();
 
     // If the document has already been detached, do not make a new axObjectCache.
-    if (!topDocument->renderView())
+    if (!topDocument->isActive())
         return 0;
 
     ASSERT(topDocument == this || !m_axObjectCache);
@@ -2973,15 +2963,13 @@ void Document::processReferrerPolicy(const String& policy)
 
 MouseEventWithHitTestResults Document::prepareMouseEvent(const HitTestRequest& request, const LayoutPoint& documentPoint, const PlatformMouseEvent& event)
 {
-    ASSERT(!renderView() || renderView()->isRenderView());
-
     // RenderView::hitTest causes a layout, and we don't want to hit that until the first
     // layout because until then, there is nothing shown on the screen - the user can't
     // have intentionally clicked on something belonging to this page. Furthermore,
     // mousemove events before the first layout should not lead to a premature layout()
     // happening, which could show a flash of white.
     // See also the similar code in EventHandler::hitTestResultAtPoint.
-    if (!renderView() || !view() || !view()->didFirstLayout())
+    if (!isActive() || !view() || !view()->didFirstLayout())
         return MouseEventWithHitTestResults(event, HitTestResult(LayoutPoint()));
 
     HitTestResult result(documentPoint);
--- third_party/WebKit/Source/core/dom/Document.h
+++ third_party/WebKit/Source/core/dom/Document.h
@@ -486,6 +486,7 @@ public:
     void prepareForDestruction();
 
     RenderView* renderView() const { return m_renderView; }
+    void clearRenderView() { m_renderView = 0; }
 
     AXObjectCache* existingAXObjectCache() const;
     AXObjectCache* axObjectCache() const;
@@ -997,6 +998,7 @@ public:
 
     DocumentLifecycleNotifier& lifecycleNotifier();
     bool isActive() const { return m_lifecyle.state() == DocumentLifecycle::Active; }
+    bool isStopping() const { return m_lifecyle.state() == DocumentLifecycle::Stopping; }
     bool isStopped() const { return m_lifecyle.state() == DocumentLifecycle::Stopped; }
 
     enum HttpRefreshType {
