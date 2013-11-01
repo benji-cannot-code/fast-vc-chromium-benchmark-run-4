4b960443f4240a3419921197df16e2993e0ae1c7
dubroy@chromium.org <dubroy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-01 01:55:13 +0000

    Add value for "infobar dismissed" to the "Save password" infobar histogram.
    BUG=NONE
    R=markusheintz@chromium.org
    
    Review URL: https://codereview.chromium.org/52383002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232273 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/password_manager/password_manager_delegate_impl.cc
+++ chrome/browser/password_manager/password_manager_delegate_impl.cc
@@ -56,7 +56,8 @@ class SavePasswordInfoBarDelegate : public ConfirmInfoBarDelegate {
   enum ResponseType {
     NO_RESPONSE = 0,
     REMEMBER_PASSWORD,
-    DONT_REMEMBER_PASSWORD,
+    NEVER_REMEMBER_PASSWORD,
+    INFOBAR_DISMISSED,
     NUM_RESPONSE_TYPES,
   };
 
@@ -72,6 +73,7 @@ class SavePasswordInfoBarDelegate : public ConfirmInfoBarDelegate {
   virtual string16 GetButtonLabel(InfoBarButton button) const OVERRIDE;
   virtual bool Accept() OVERRIDE;
   virtual bool Cancel() OVERRIDE;
+  virtual void InfoBarDismissed() OVERRIDE;
 
   virtual InfoBarAutomationType GetInfoBarAutomationType() const OVERRIDE;
 
@@ -183,8 +185,13 @@ bool SavePasswordInfoBarDelegate::Accept() {
 bool SavePasswordInfoBarDelegate::Cancel() {
   DCHECK(form_to_save_.get());
   form_to_save_->PermanentlyBlacklist();
-  infobar_response_ = DONT_REMEMBER_PASSWORD;
+  infobar_response_ = NEVER_REMEMBER_PASSWORD;
   return true;
+}
+
+void SavePasswordInfoBarDelegate::InfoBarDismissed() {
+  DCHECK(form_to_save_.get());
+  infobar_response_ = INFOBAR_DISMISSED;
 }
 
 InfoBarDelegate::InfoBarAutomationType
