fa8616d09f630282f983e6031c34916935468c1e
pilgrim@chromium.org <pilgrim@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-01 22:48:03 +0000

    Move RTCConfiguration to platform/
    BUG=297477
    
    Review URL: https://codereview.chromium.org/54623004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161208 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/exported/WebRTCConfiguration.cpp
+++ third_party/WebKit/Source/platform/exported/WebRTCConfiguration.cpp
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "public/platform/WebRTCConfiguration.h"
 
-#include "core/platform/mediastream/RTCConfiguration.h"
+#include "platform/mediastream/RTCConfiguration.h"
 #include "public/platform/WebString.h"
 #include "public/platform/WebURL.h"
 #include "public/platform/WebVector.h"
