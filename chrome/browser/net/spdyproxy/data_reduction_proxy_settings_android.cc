ae0bad19309e9bc951e8c154760b2b3c6119bb74
marq@chromium.org <marq@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-01 05:47:30 +0000

    Refactored tests to use mock classes instead of explicitly subclassing DataReductionProxySettings(Android). This required a bit of templating to avoid duplicating code.
    No new tests were added beyond verifying expected calls to LogProxyState().
    
    Review URL: https://codereview.chromium.org/47593003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232304 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/spdyproxy/data_reduction_proxy_settings_android.cc
+++ chrome/browser/net/spdyproxy/data_reduction_proxy_settings_android.cc
@@ -77,6 +77,8 @@ DataReductionProxySettingsAndroid::DataReductionProxySettingsAndroid(
     JNIEnv* env, jobject obj): DataReductionProxySettings() {
 }
 
+DataReductionProxySettingsAndroid::DataReductionProxySettingsAndroid() {}
+
 DataReductionProxySettingsAndroid::~DataReductionProxySettingsAndroid() {}
 
 void DataReductionProxySettingsAndroid::InitDataReductionProxySettings(
--- chrome/browser/net/spdyproxy/data_reduction_proxy_settings_android.h
+++ chrome/browser/net/spdyproxy/data_reduction_proxy_settings_android.h
@@ -26,6 +26,9 @@ using base::android::ScopedJavaLocalRef;
 class DataReductionProxySettingsAndroid : public DataReductionProxySettings {
  public:
   DataReductionProxySettingsAndroid(JNIEnv* env, jobject obj);
+  // Parameter-free constructor for C++ unit tests.
+  DataReductionProxySettingsAndroid();
+
   virtual ~DataReductionProxySettingsAndroid();
 
   void InitDataReductionProxySettings(JNIEnv* env, jobject obj);
