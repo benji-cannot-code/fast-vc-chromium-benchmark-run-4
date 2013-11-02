36a610da888a750285220d784f797c18d58e784e
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-02 03:38:37 +0000

    kiosk: Remove password-echo for keyboard driven oobe.
    BUG=314110
    
    Review URL: https://codereview.chromium.org/56223003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232571 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/chrome_views_delegate.cc
+++ chrome/browser/ui/views/chrome_views_delegate.cc
@@ -295,9 +295,7 @@ void ChromeViewsDelegate::OnBeforeWidgetInit(
 #endif
 }
 
-#if !defined(OS_CHROMEOS)
 base::TimeDelta
 ChromeViewsDelegate::GetDefaultTextfieldObscuredRevealDuration() {
   return base::TimeDelta();
 }
-#endif
