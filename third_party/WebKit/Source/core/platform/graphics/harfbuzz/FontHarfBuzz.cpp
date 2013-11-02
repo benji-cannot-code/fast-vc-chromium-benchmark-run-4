e8131e19b7d0d5d4cb19fe3a7dfe5f51b3d25e38
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-02 00:30:53 +0000

    Add GraphicsContext parameter to FontPlatformData::setupPaint
    Add a GraphicsContext parameter to FontPlatformDataHarfBuzz::setupPaint
    and change Font::drawGlyphs in FontHarfBuzz.cpp to pass the context
    object when calling setupPaint.
    
    This matches the windows code path and allows the setup call to check
    flags on the context, as required by the FontPlatformDataChromiumWin
    implementation to determine whether LCD text should be used.
    
    BUG=313423
    R=leviw@chromium.org
    
    Review URL: https://codereview.chromium.org/52383003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161211 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/graphics/harfbuzz/FontHarfBuzz.cpp
+++ third_party/WebKit/Source/core/platform/graphics/harfbuzz/FontHarfBuzz.cpp
@@ -104,7 +104,7 @@ void Font::drawGlyphs(GraphicsContext* gc, const SimpleFontData* font,
     if (textMode & TextModeFill) {
         SkPaint paint;
         gc->setupPaintForFilling(&paint);
-        font->platformData().setupPaint(&paint);
+        font->platformData().setupPaint(&paint, gc);
         gc->adjustTextRenderMode(&paint);
         paint.setTextEncoding(SkPaint::kGlyphID_TextEncoding);
 
@@ -126,7 +126,7 @@ void Font::drawGlyphs(GraphicsContext* gc, const SimpleFontData* font,
 
         SkPaint paint;
         gc->setupPaintForStroking(&paint);
-        font->platformData().setupPaint(&paint);
+        font->platformData().setupPaint(&paint, gc);
         gc->adjustTextRenderMode(&paint);
         paint.setTextEncoding(SkPaint::kGlyphID_TextEncoding);
 
