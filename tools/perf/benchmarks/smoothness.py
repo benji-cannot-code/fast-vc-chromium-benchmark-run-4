f7d95da9d1db97968194c994f3544644ec4e2e53
ernstm@chromium.org <ernstm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-02 05:41:14 +0000

    telemetry: temporarily disable smoothness bench.
    Temporarily disable smoothness benchmark on Linux and Android,
    until crbug.com/313753 is fixed.
    
    R=tonyg@chromium.org
    BUG=313753
    NOTRY=True
    
    Review URL: https://codereview.chromium.org/56243005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232589 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/benchmarks/smoothness.py
+++ tools/perf/benchmarks/smoothness.py
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright (c) 2013 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+import sys
 from telemetry import test
 
 from measurements import smoothness
@@ -11,6 +12,8 @@ class SmoothnessTop25(test.Test):
   """Measures rendering statistics while scrolling down the top 25 web pages.
 
   http://www.chromium.org/developers/design-documents/rendering-benchmarks"""
+  # TODO(ernstm): remove when crbug.com/313753 is fixed.
+  enabled = sys.platform != 'linux2'
   test = smoothness.Smoothness
   page_set = 'page_sets/top_25.json'
 
@@ -24,5 +27,7 @@ class SmoothnessKeyMobileSites(test.Test):
   """Measures rendering statistics while scrolling down the key mobile sites.
 
   http://www.chromium.org/developers/design-documents/rendering-benchmarks"""
+  # TODO(ernstm): remove when crbug.com/313753 is fixed.
+  enabled = sys.platform != 'linux2'
   test = smoothness.Smoothness
   page_set = 'page_sets/key_mobile_sites.json'
