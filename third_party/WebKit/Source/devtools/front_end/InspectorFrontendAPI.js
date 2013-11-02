a98bb39213d2a86a0bba3ee3017228e5e4f7301d
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-02 09:55:45 +0000

    Revert r48393002 "DevTools: Use docking mode button to control Screencast visibility and docking state" Breaks docking modes, screencast.
    git-svn-id: svn://svn.chromium.org/blink/trunk@161225 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/InspectorFrontendAPI.js
+++ third_party/WebKit/Source/devtools/front_end/InspectorFrontendAPI.js
@@ -109,8 +109,7 @@ var InspectorFrontendAPI = {
     // FIXME: remove this legacy support.
     setDockSide: function(side)
     {
-        if (WebInspector.dockController)
-            WebInspector.dockController.setDockSide(side);
+        WebInspector.dockController.setDockSide(side);
     },
 
     dispatchMessage: function(messageObject)
