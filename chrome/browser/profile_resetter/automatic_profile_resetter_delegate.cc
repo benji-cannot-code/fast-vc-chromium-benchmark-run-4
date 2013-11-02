bc923a497b5cf50410ead59a9c115eec5134dce1
dewittj@chromium.org <dewittj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-02 03:34:47 +0000

    Revert 232525 "Integrate module import verification with module ..."
    Broke unit_tests on XP Tests (dbg)(1) - imported module verification
    tests.
    
    > Integrate module import verification with module loading verification.
    > 
    > BUG=
    > 
    > Review URL: https://codereview.chromium.org/27507010
    
    TBR=erikwright@chromium.org
    
    Review URL: https://codereview.chromium.org/56213004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232566 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profile_resetter/automatic_profile_resetter_delegate.cc
+++ chrome/browser/profile_resetter/automatic_profile_resetter_delegate.cc
@@ -5,11 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/profile_resetter/automatic_profile_resetter_delegate.h"
 
-#include <string>
-
 #include "base/callback.h"
 #include "base/logging.h"
-#include "base/md5.h"
 #include "base/memory/scoped_vector.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
@@ -24,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_WIN)
 #include "chrome/browser/enumerate_modules_model_win.h"
+#include "chrome/browser/install_module_verifier_win.h"
 #endif
 
 namespace {
@@ -60,35 +58,6 @@ scoped_ptr<base::DictionaryValue> BuildSubTreeFromTemplateURL(
   tree->Set("alternate_urls", alternate_urls);
   return tree.Pass();
 }
-
-#if defined(OS_WIN)
-void ExtractLoadedModuleNameDigests(
-    const base::ListValue& module_list,
-    base::ListValue* module_name_digests) {
-  DCHECK(module_name_digests);
-
-  // EnumerateModulesModel produces a list of dictionaries.
-  // Each dictionary corresponds to a module and exposes a number of properties.
-  // We care only about 'type' and 'name'.
-  for (size_t i = 0; i < module_list.GetSize(); ++i) {
-    const base::DictionaryValue* module_dictionary = NULL;
-    if (!module_list.GetDictionary(i, &module_dictionary))
-      continue;
-    ModuleEnumerator::ModuleType module_type =
-        ModuleEnumerator::LOADED_MODULE;
-    if (!module_dictionary->GetInteger(
-            "type", reinterpret_cast<int*>(&module_type)) ||
-        module_type != ModuleEnumerator::LOADED_MODULE) {
-      continue;
-    }
-    std::string module_name;
-    if (!module_dictionary->GetString("name", &module_name))
-      continue;
-    StringToLowerASCII(&module_name);
-    module_name_digests->AppendString(base::MD5String(module_name));
-  }
-}
-#endif
 
 }  // namespace
 
@@ -155,11 +124,15 @@ void AutomaticProfileResetterDelegateImpl::
 scoped_ptr<base::ListValue> AutomaticProfileResetterDelegateImpl::
     GetLoadedModuleNameDigests() const {
   DCHECK(modules_have_been_enumerated_event_.is_signaled());
-  scoped_ptr<base::ListValue> result(new base::ListValue);
+  std::set<std::string> module_name_digests;
 #if defined(OS_WIN)
   if (module_list_)
-    ExtractLoadedModuleNameDigests(*module_list_, result.get());
+    ExtractLoadedModuleNameDigests(*module_list_, &module_name_digests);
 #endif
+  scoped_ptr<base::ListValue> result(new base::ListValue);
+  for (std::set<std::string>::const_iterator it = module_name_digests.begin();
+       it != module_name_digests.end(); ++it)
+    result->AppendString(*it);
   return result.Pass();
 }
 
