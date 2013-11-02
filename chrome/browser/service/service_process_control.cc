6d6ed3518f0b1d6a1feb919994ae1b7da0a831aa
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-02 13:49:59 +0000

    UMA for service process events.
    BUG=314284
    
    Review URL: https://codereview.chromium.org/56723002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232613 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/service/service_process_control.cc
+++ chrome/browser/service/service_process_control.cc
@@ -115,6 +115,9 @@ void ServiceProcessControl::Launch(const base::Closure& success_task,
     return;
   }
 
+  UMA_HISTOGRAM_ENUMERATION("CloudPrint.ServiceEvents", SERVICE_EVENT_LAUNCH,
+                            SERVICE_EVENT_MAX);
+
   // A service process should have a different mechanism for starting, but now
   // we start it as if it is a child process.
 
@@ -164,10 +167,14 @@ void ServiceProcessControl::Disconnect() {
 void ServiceProcessControl::OnProcessLaunched() {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
   if (launcher_->launched()) {
+    UMA_HISTOGRAM_ENUMERATION("CloudPrint.ServiceEvents",
+                              SERVICE_EVENT_LAUNCHED, SERVICE_EVENT_MAX);
     // After we have successfully created the service process we try to connect
     // to it. The launch task is transfered to a connect task.
     ConnectInternal();
   } else {
+    UMA_HISTOGRAM_ENUMERATION("CloudPrint.ServiceEvents",
+                              SERVICE_EVENT_LAUNCH_FAILED, SERVICE_EVENT_MAX);
     // If we don't have process handle that means launching the service process
     // has failed.
     RunConnectDoneTasks();
@@ -191,6 +198,9 @@ bool ServiceProcessControl::OnMessageReceived(const IPC::Message& message) {
 void ServiceProcessControl::OnChannelConnected(int32 peer_pid) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
 
+  UMA_HISTOGRAM_ENUMERATION("CloudPrint.ServiceEvents",
+                            SERVICE_EVENT_CHANNEL_CONNECTED, SERVICE_EVENT_MAX);
+
   // We just established a channel with the service process. Notify it if an
   // upgrade is available.
   if (UpgradeDetector::GetInstance()->notify_upgrade()) {
@@ -205,6 +215,10 @@ void ServiceProcessControl::OnChannelConnected(int32 peer_pid) {
 
 void ServiceProcessControl::OnChannelError() {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+
+  UMA_HISTOGRAM_ENUMERATION("CloudPrint.ServiceEvents",
+                            SERVICE_EVENT_CHANNEL_ERROR, SERVICE_EVENT_MAX);
+
   channel_.reset();
   RunConnectDoneTasks();
 }
@@ -229,6 +243,8 @@ void ServiceProcessControl::Observe(
 void ServiceProcessControl::OnCloudPrintProxyInfo(
     const cloud_print::CloudPrintProxyInfo& proxy_info) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+  UMA_HISTOGRAM_ENUMERATION("CloudPrint.ServiceEvents",
+                            SERVICE_EVENT_INFO_REPLY, SERVICE_EVENT_MAX);
   if (!cloud_print_info_callback_.is_null()) {
     cloud_print_info_callback_.Run(proxy_info);
     cloud_print_info_callback_.Reset();
@@ -237,6 +253,8 @@ void ServiceProcessControl::OnCloudPrintProxyInfo(
 
 void ServiceProcessControl::OnHistograms(
     const std::vector<std::string>& pickled_histograms) {
+  UMA_HISTOGRAM_ENUMERATION("CloudPrint.ServiceEvents",
+                            SERVICE_EVENT_HISTOGRAMS_REPLY, SERVICE_EVENT_MAX);
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
   base::HistogramDeltaSerialization::DeserializeAndAddSamples(
       pickled_histograms);
@@ -257,6 +275,8 @@ bool ServiceProcessControl::GetCloudPrintProxyInfo(
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
   DCHECK(!cloud_print_info_callback.is_null());
   cloud_print_info_callback_.Reset();
+  UMA_HISTOGRAM_ENUMERATION("CloudPrint.ServiceEvents",
+                            SERVICE_EVENT_INFO_REQUEST, SERVICE_EVENT_MAX);
   if (!Send(new ServiceMsg_GetCloudPrintProxyInfo()))
     return false;
   cloud_print_info_callback_ = cloud_print_info_callback;
@@ -274,6 +294,11 @@ bool ServiceProcessControl::GetHistograms(
   if (!CheckServiceProcessReady())
     return false;
   ConnectInternal();
+
+  UMA_HISTOGRAM_ENUMERATION("CloudPrint.ServiceEvents",
+                            SERVICE_EVENT_HISTOGRAMS_REQUEST,
+                            SERVICE_EVENT_MAX);
+
   if (!Send(new ServiceMsg_GetHistograms()))
     return false;
 
