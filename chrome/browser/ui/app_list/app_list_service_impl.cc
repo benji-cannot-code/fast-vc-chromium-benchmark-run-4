3830534c967fc8ad83d11339e19b35045a5d9b69
jackhou@chromium.org <jackhou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-02 02:02:48 +0000

    Create shortcuts when app launcher is enabled.
    This change mostly affects Mac. At the moment, if app shims are not
    enabled, syncing of v2 apps does not create shims for those apps.
    If app shims are then enabled by enabling the app launcher, these shims
    will not be created until the next restart of Chrome. This CL runs a
    OnceOffCreateShortcuts (the same method run on startup) when the app
    launcher is enabled.
    
    BUG=313106
    TEST=Start with a fresh install of Chrome.
    Sign in with an account that has v2 apps.
    Wait for the apps to sync.
    Enable the app launcher.
    App shims should be installed for synced apps.
    Launch an app, it should have an icon in the dock.
    
    Review URL: https://codereview.chromium.org/54383005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232553 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/app_list/app_list_service_impl.cc
+++ chrome/browser/ui/app_list/app_list_service_impl.cc
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/prefs/pref_service.h"
 #include "base/strings/string16.h"
 #include "base/time/time.h"
+#include "chrome/browser/apps/shortcut_manager.h"
+#include "chrome/browser/apps/shortcut_manager_factory.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/app_list/keep_alive_service.h"
@@ -262,6 +264,8 @@ void AppListServiceImpl::EnableAppList(Profile* initial_profile) {
 
   local_state_->SetBoolean(prefs::kAppLauncherHasBeenEnabled, true);
   CreateShortcut();
+  AppShortcutManagerFactory::GetForProfile(initial_profile)->
+      OnceOffCreateShortcuts();
 }
 
 void AppListServiceImpl::InvalidatePendingProfileLoads() {
