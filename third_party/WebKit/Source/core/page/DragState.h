285c3f6ae979ff39e790568c6da16ee7b9139b74
tfarina@chromium.org <tfarina@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-03 19:22:10 +0000

    Trivial clean ups to DragState.h
    - remove unnecessary includes (the forward declarations are enough).
    
    R=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/48423003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161236 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/DragState.h
+++ third_party/WebKit/Source/core/page/DragState.h
@@ -27,10 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef DragState_h
 #define DragState_h
 
-#include "core/dom/Clipboard.h"
-#include "core/dom/Node.h"
 #include "core/page/DragActions.h"
-#include "wtf/Forward.h"
 #include "wtf/Noncopyable.h"
 #include "wtf/RefPtr.h"
 
