47c2618d8c7cde22317f469014bc9f0d84effe78
hirono@chromium.org <hirono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-03 05:17:02 +0000

    Files.app: Updates the message of progress center.
    Previously the progress center was showing fake messages. This CL replace with
    the fake messages with the real string assets.
    
    Along the way, the CL removes unused string assets used by the butter bar.
    
    BUG=277432
    TEST=manually
    R=yoshiki@chromium.org
    
    Review URL: https://codereview.chromium.org/54093002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232665 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/foreground/js/butter_bar.js
+++ chrome/browser/resources/file_manager/foreground/js/butter_bar.js
@@ -250,11 +250,7 @@ ButterBar.prototype.showProgress_ = function(progress) {
     timeout: false
   };
 
-  var pendingItems = progress.numRemainingItems;
-  var type = this.transferType_();
-  var progressString = (pendingItems === 1) ?
-          strf(type + '_FILE_NAME', progress.processingEntry.name) :
-          strf(type + '_ITEMS_REMAINING', pendingItems);
+  var progressString = '';
 
   if (this.currentMode_ == ButterBar.Mode.COPY) {
     this.update_(progressString, options);
@@ -296,8 +292,7 @@ ButterBar.prototype.onCopyProgress_ = function(event) {
       break;
 
     case 'CANCELLED':
-      this.show(ButterBar.Mode.DELETE,
-                str(this.transferType_() + '_CANCELLED'));
+      this.show(ButterBar.Mode.DELETE, '');
       break;
 
     case 'ERROR':
@@ -342,11 +337,11 @@ ButterBar.prototype.onDelete_ = function(event) {
 
     case 'PROGRESS':
       this.totalDeleted_ += event.urls.length;
-      var title = strf('DELETED_MESSAGE_PLURAL', this.totalDeleted_);
+      var title = '';
       if (this.totalDeleted_ == 1) {
         var fullPath = util.extractFilePath(event.urls[0]);
         var fileName = PathUtil.split(fullPath).pop();
-        title = strf('DELETED_MESSAGE', fileName);
+        title = '';
       }
 
       if (this.currentMode_ == ButterBar.Mode.DELETE)
