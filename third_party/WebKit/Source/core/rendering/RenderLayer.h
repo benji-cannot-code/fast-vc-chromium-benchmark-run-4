e78dd41bb087e0e7806e7551e82f82a97cbe94aa
jchaffraix@chromium.org <jchaffraix@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-04 19:27:28 +0000

    Move more of the composited scrolling code
    While touching RenderLayerStackingNode,
    made a private function private for real.
    
    BUG=260899
    
    Review URL: https://codereview.chromium.org/56613002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161271 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderLayer.h
+++ third_party/WebKit/Source/core/rendering/RenderLayer.h
@@ -351,11 +351,10 @@ public:
     CompositedLayerMapping* compositedLayerMapping() const { return m_compositedLayerMapping.get(); }
     CompositedLayerMapping* ensureCompositedLayerMapping();
     void clearCompositedLayerMapping(bool layerBeingDestroyed = false);
-    bool adjustForForceCompositedScrollingMode(bool) const;
 
     bool hasCompositedMask() const;
     bool hasCompositedClippingMask() const;
-    bool needsCompositedScrolling() const;
+    bool needsCompositedScrolling() const { return m_scrollableArea && m_scrollableArea->needsCompositedScrolling(); }
 
     RenderLayer* scrollParent() const;
     RenderLayer* clipParent() const;
@@ -422,14 +421,6 @@ public:
 
     bool scrollsWithRespectTo(const RenderLayer*) const;
 
-    enum ForceNeedsCompositedScrollingMode {
-        DoNotForceCompositedScrolling = 0,
-        CompositedScrollingAlwaysOn = 1,
-        CompositedScrollingAlwaysOff = 2
-    };
-
-    void setForceNeedsCompositedScrolling(ForceNeedsCompositedScrollingMode);
-
     void addLayerHitTestRects(LayerHitTestRects&) const;
 
     // FIXME: This should probably return a ScrollableArea but a lot of internal methods are mistakenly exposed.
@@ -475,7 +466,6 @@ private:
 
     void updateOutOfFlowPositioned(const RenderStyle* oldStyle);
 
-    bool setNeedsCompositedScrolling(bool);
     void didUpdateNeedsCompositedScrolling();
 
     // Returns true if the position changed.
@@ -635,8 +625,6 @@ protected:
 
     unsigned m_isUnclippedDescendant : 1;
 
-    unsigned m_needsCompositedScrolling : 1;
-
     const unsigned m_isRootLayer : 1;
 
     unsigned m_usedTransparency : 1; // Tracks whether we need to close a transparent layer, i.e., whether
@@ -722,8 +710,6 @@ protected:
     };
 
     CompositingProperties m_compositingProperties;
-
-    ForceNeedsCompositedScrollingMode m_forceNeedsCompositedScrolling;
 
 private:
     enum CompositedScrollingHistogramBuckets {
