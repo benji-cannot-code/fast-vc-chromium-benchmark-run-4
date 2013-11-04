22711216ddc062401d2ea1720e4ba13be279ae8c
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-04 19:06:57 +0000

    Revert "Don't null the RenderView during document destruction"
    This reverts http://src.chromium.org/viewvc/blink/?view=revision&revision=161121
    
    This apparently caused a bunch of SVG perf regressions.
    
    TBR=sonnyrao,pdr
    BUG=314428
    
    Review URL: https://codereview.chromium.org/58193002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161268 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Document.h
+++ third_party/WebKit/Source/core/dom/Document.h
@@ -486,7 +486,6 @@ public:
     void prepareForDestruction();
 
     RenderView* renderView() const { return m_renderView; }
-    void clearRenderView() { m_renderView = 0; }
 
     AXObjectCache* existingAXObjectCache() const;
     AXObjectCache* axObjectCache() const;
@@ -1001,7 +1000,6 @@ public:
 
     DocumentLifecycleNotifier& lifecycleNotifier();
     bool isActive() const { return m_lifecyle.state() == DocumentLifecycle::Active; }
-    bool isStopping() const { return m_lifecyle.state() == DocumentLifecycle::Stopping; }
     bool isStopped() const { return m_lifecyle.state() == DocumentLifecycle::Stopped; }
 
     enum HttpRefreshType {
