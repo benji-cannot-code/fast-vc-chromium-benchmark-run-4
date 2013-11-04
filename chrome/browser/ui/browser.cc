be8f1c91d869cd86fe1807d5d239721599a4bacf
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-04 23:45:49 +0000

    Don't attempt to session restore an undocked devtools window in AURA.
    Attempting to restore this window does not work anyway as it does not have the associated web contents wired up.
    This is the same behavior as on non AURA.
    
    Fixes bug https://code.google.com/p/chromium/issues/detail?id=304138
    
    BUG=304138
    R=sky@chromium.org, sky
    
    Review URL: https://codereview.chromium.org/51943007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232838 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -641,7 +641,7 @@ void Browser::OnWindowClosing() {
       TabRestoreServiceFactory::GetForProfile(profile());
 
 #if defined(USE_AURA)
-  if (tab_restore_service && is_app())
+  if (tab_restore_service && is_app() && !is_devtools())
     tab_restore_service->BrowserClosing(tab_restore_service_delegate());
 #endif
 
