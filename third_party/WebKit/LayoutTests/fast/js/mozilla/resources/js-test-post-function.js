823c3740e2762b2ae62222e0d50e03d90f62df9d
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-04 22:40:47 +0000

    Delete all the js-test-post-* files and rename the fast/js/mozilla js-test-* files.
    The js-test-post-* files are not used. And the fast/js/mozilla copies
    have diverged greatly. Better to rename them to avoid any confusion.
    
    TBR=eae@chromium.org
    
    Review URL: https://codereview.chromium.org/48343006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161289 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/js/mozilla/resources/js-test-post-function.js
+++
@@ -1,6 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-function isSuccessfullyParsed()
-{
-    shouldBeTrue("successfullyParsed");
-    debug('<br><span class="pass">TEST COMPLETE</span>');
-}
