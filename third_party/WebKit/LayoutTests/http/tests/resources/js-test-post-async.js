bcc2fc6d4f36fc338fb9e19f664ca0b464ce771c
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-04 21:14:29 +0000

    Delete http/tests/resources/js-test-{pre|post}.js.
    Use the one in fast/js. This leaves us with only two copies of these files
    left (the others are in fast/js/mozilla and I don't especially see the
    need to get rid of that copy).
    
    TBR=eae@chromium.org
    
    Review URL: https://codereview.chromium.org/46393004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161284 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/resources/js-test-post-async.js
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-if (!errorMessage)
-    successfullyParsed = true;
-shouldBeTrue("successfullyParsed");
-debug('<br /><span class="pass">TEST COMPLETE</span>');
-
-if (window.testRunner)
-    testRunner.notifyDone();
