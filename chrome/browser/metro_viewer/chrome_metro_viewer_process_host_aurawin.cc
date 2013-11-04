15ba0277fd11827c2e9fef7813fef50482d380ed
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-04 23:39:58 +0000

    Restart in Desktop if Metro tries to start up without GPU
    This doesn't yet handle the case where the GPU becomes crashy. The
    user would need to close and re-open Chrome, currently. That case
    seems more complicated to handle because it's only down low that
    we know that, not up at the chrome:: level.
    
    This DCHECKs in https://code.google.com/p/chromium/codesearch#chromium/src/base/win/scoped_com_initializer.h&l=55
    in debug, but I believe that's an unrelated problem at HEAD. (i.e. all
    restarts are doing that currently.)
    
    R=cpu@chromium.org
    BUG=308736
    
    Review URL: https://codereview.chromium.org/54093007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232836 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metro_viewer/chrome_metro_viewer_process_host_aurawin.cc
+++ chrome/browser/metro_viewer/chrome_metro_viewer_process_host_aurawin.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/browser_process_platform_part_aurawin.h"
 #include "chrome/browser/chrome_notification_types.h"
+#include "chrome/browser/lifetime/application_lifetime.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/search_engines/util.h"
 #include "chrome/browser/ui/ash/ash_init.h"
@@ -22,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/common/env_vars.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/gpu_data_manager.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/page_navigator.h"
 #include "content/public/browser/web_contents.h"
@@ -95,6 +97,11 @@ void ChromeMetroViewerProcessHost::OnChannelConnected(int32 /*peer_pid*/) {
   // Set environment variable to let breakpad know that metro process was
   // connected.
   ::SetEnvironmentVariableA(env_vars::kMetroConnected, "1");
+
+  if (!content::GpuDataManager::GetInstance()->GpuAccessAllowed(NULL)) {
+    DLOG(INFO) << "No GPU access, attempting to restart in Desktop\n";
+    chrome::AttemptRestartToDesktopMode();
+  }
 }
 
 void ChromeMetroViewerProcessHost::OnSetTargetSurface(
