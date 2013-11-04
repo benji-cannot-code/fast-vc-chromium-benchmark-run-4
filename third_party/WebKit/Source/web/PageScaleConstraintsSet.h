aa834945dfced7d2cc5803b521a3b5852397f651
aelias@chromium.org <aelias@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-04 17:29:49 +0000

    Reset to initial page scale in additional scenarios.
    This patch causes page scale to reset to initial scale in two more
    cases:
    1) When the page-defined initial scale goes from -1 to a specific
    value, or changes value.  This fixes a race where if page scale was reset
    before the viewport tag was processed, then we would never go to
    initial scale.
    2) When the initial scale was set to a value less than the final minimum
    scale and the content width expands.  In this case, the content width
    was previously preventing us from going to the desired scale so we
    should try again now that it's expanded.
    
    Also no-op cleanup of some surrounding tests.
    
    NOTRY=true
    BUG= internal b/10935045, b/10838508
    
    Review URL: https://codereview.chromium.org/26251010
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161255 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/PageScaleConstraintsSet.h
+++ third_party/WebKit/Source/web/PageScaleConstraintsSet.h
@@ -74,6 +74,8 @@ public:
     bool constraintsDirty() const { return m_constraintsDirty; }
 
 private:
+    WebCore::PageScaleConstraints computeConstraintsStack() const;
+
     WebCore::PageScaleConstraints m_pageDefinedConstraints;
     WebCore::PageScaleConstraints m_userAgentConstraints;
     WebCore::PageScaleConstraints m_finalConstraints;
