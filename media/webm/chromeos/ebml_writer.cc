1e715857f9c79619ef34597aa9fa7009a1020572
johannkoenig@chromium.org <johannkoenig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-04 17:30:39 +0000

    Roll libvpx 228256:232686
    Pull latest version from upstream. This had to be broken up across several
    changes. The first has the commit message:
    https://codereview.chromium.org/55493002/
    
    Remove AVX code until we update the gyp files:
    https://codereview.chromium.org/48903015/
    
    libmkv has moved to third_party/libmkv:
    https://codereview.chromium.org/54373014/
    media/webm/chromeos/webm_encoder.cc and
    media/webm/chromeon/ebml_writer.cc pull from this location
    as well and needs to be updated.
    
    The last roll attempt failed because we introduced a new textrel.
    Fixed upstream and cherry-picked here:
    https://codereview.chromium.org/49023003
    
    R=fischman@chromium.org, tomfinegan@chromium.org, fischman, tomfinegan
    
    Review URL: https://codereview.chromium.org/57063005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232721 0039d316-1c4b-4281-b951-d872f2087c98

--- media/webm/chromeos/ebml_writer.cc
+++ media/webm/chromeos/ebml_writer.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/media_export.h"
 
 extern "C" {
-#include "third_party/libvpx/source/libvpx/libmkv/EbmlWriter.h"
+#include "third_party/libvpx/source/libvpx/third_party/libmkv/EbmlWriter.h"
 
 EbmlGlobal::EbmlGlobal() {
 }
