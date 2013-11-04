f836cb1c892e695e710329f2e1242799ea191ae8
kinaba@chromium.org <kinaba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-04 19:11:33 +0000

    Allow file:/// access to the Downloads folders in multi-profile sessions.
    We have been whitelisted 'file:///home/chronos/user/Downloads'
    on Chrome OS, but the third component may vary from "user"
    when multiple profiles are logged into a session.
    
    BUG=309556
    
    Review URL: https://codereview.chromium.org/54053004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232751 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/chrome_network_delegate.h
+++ chrome/browser/net/chrome_network_delegate.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
+#include "base/files/file_path.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/values.h"
@@ -69,6 +70,13 @@ class ChromeNetworkDelegate : public net::NetworkDelegate {
   // otherwise they will only be sent to the specified profile.
   void set_profile(void* profile) {
     profile_ = profile;
+  }
+
+  // |profile_path| is used to locate the "Downloads" folder on Chrome OS. If it
+  // is set, the location of the Downloads folder for the profile is added to
+  // the whitelist for accesses via file: scheme.
+  void set_profile_path(const base::FilePath& profile_path) {
+    profile_path_ = profile_path;
   }
 
   // If |cookie_settings| is NULL or not set, all cookies are enabled,
@@ -178,6 +186,7 @@ class ChromeNetworkDelegate : public net::NetworkDelegate {
 
   scoped_refptr<extensions::EventRouterForwarder> event_router_;
   void* profile_;
+  base::FilePath profile_path_;
   scoped_refptr<CookieSettings> cookie_settings_;
 
   scoped_refptr<ExtensionInfoMap> extension_info_map_;
