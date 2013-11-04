49ef7d953e76525d75a978bd7bffb29c4584dfca
girard@chromium.org <girard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-04 20:26:57 +0000

    Flag --force-device-scale-factor was being inconsistently referenced. Also, --high-dpi-support was not being used consistently. As a result, high dpi support in aura has been broken for some time.
    BUG=306268
    
    Review URL: https://codereview.chromium.org/52773006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232778 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/l10n/l10n_util_win.cc
+++ ui/base/l10n/l10n_util_win.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/windows_version.h"
 #include "grit/app_locale_settings.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/gfx/display.h"
 #include "ui/gfx/win/dpi.h"
 
 namespace {
@@ -152,7 +153,12 @@ bool NeedOverrideDefaultUIFont(string16* override_font_family,
 }
 
 void AdjustUIFont(LOGFONT* logfont) {
-  AdjustUIFontForDIP(gfx::GetDPIScale(), logfont);
+  double dpi_scale = gfx::GetDPIScale();
+  if (gfx::Display::HasForceDeviceScaleFactor()) {
+    // If the scale is forced, we don't need to adjust it here.
+    dpi_scale = 1.0;
+  }
+  AdjustUIFontForDIP(dpi_scale, logfont);
 }
 
 void AdjustUIFontForDIP(float dpi_scale, LOGFONT* logfont) {
--- ui/base/win/dpi_setup.cc
+++ ui/base/win/dpi_setup.cc
@@ -13,14 +13,7 @@ namespace ui {
 namespace win {
 
 void InitDeviceScaleFactor() {
-  float scale = 1.0f;
-  if (gfx::IsHighDPIEnabled()) {
-    scale = gfx::Display::HasForceDeviceScaleFactor() ?
-        gfx::Display::GetForcedDeviceScaleFactor() : gfx::GetDPIScale();
-    // Quantize to nearest supported scale factor.
-    scale = ui::GetImageScale(ui::GetSupportedScaleFactor(scale));
-  }
-  gfx::InitDeviceScaleFactor(scale);
+  gfx::InitDeviceScaleFactor(gfx::GetDPIScale());
 }
 
 }  // namespace win
