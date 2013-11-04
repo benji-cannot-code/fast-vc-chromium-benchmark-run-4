d0853a421e9389a61d3a1ccab95a73e067e202ae
perkj@chromium.org <perkj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-04 19:54:58 +0000

    Refactor MediaStreamManager to never output real device id. It now always output sourceId in the form of a HMAC. DeviceMessageFilter now don't need to create a source id. This also fix a bug that made the source ids useless unless you have called gum once. Note that the sourceIds are still just using the security origin as salt. Next step is to add a random number that is stored in the profile somehow.
    This cl also make sure all public APIs are asynchronous by making sure all requests are posted on the UI thread and handled later. Before this change - there were code paths that posted on the UI thread and others that did not that could potentially happen after a request was cancelled. This also make it easier to fail a request based on the input arguments.
    
    BUG=269139
    
    Review URL: https://codereview.chromium.org/34393006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232766 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/policy_browsertest.cc
+++ chrome/browser/policy/policy_browsertest.cc
@@ -2635,7 +2635,7 @@ class MediaStreamDevicesControllerBrowserTest
   }
 
   void FinishAudioTest() {
-    content::MediaStreamRequest request(0, 0, 0, std::string(),
+    content::MediaStreamRequest request(0, 0, 0,
                                         request_url_.GetOrigin(),
                                         content::MEDIA_DEVICE_ACCESS,
                                         std::string(), std::string(),
@@ -2654,7 +2654,7 @@ class MediaStreamDevicesControllerBrowserTest
   void FinishVideoTest() {
     // TODO(raymes): Test MEDIA_DEVICE_OPEN (Pepper) which grants both webcam
     // and microphone permissions at the same time.
-    content::MediaStreamRequest request(0, 0, 0, std::string(),
+    content::MediaStreamRequest request(0, 0, 0,
                                         request_url_.GetOrigin(),
                                         content::MEDIA_DEVICE_ACCESS,
                                         std::string(),
