15f7b6f81a6fb66e8ed0311a86f3ecbce9bd5fe4
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-05 04:26:16 +0000

    Reland: Convert ExtensionProcessManager to BrowserContext, part 1
    Reland of https://codereview.chromium.org/52983004/ which failed due to
    timeouts on Windows XP.
    
    Part of moving ExtensionProcessManager to src/extensions.
    * Introduce more test coverage
    * Convert most usage of Profile to BrowserContext
    * Separate out "master" vs. incognito profiles/contexts
    * Move switches into extensions/common/switches.h
    
    BUG=313481
    TEST=unit_tests ExtensionProcessManager* and browser_tests ExtensionProcessManager*
    TBR=miket@chromium.org
    
    Review URL: https://codereview.chromium.org/57813003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232904 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_context_menu_browsertest.cc
+++ chrome/browser/extensions/extension_context_menu_browsertest.cc
@@ -15,9 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/tab_contents/render_view_context_menu.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
-#include "chrome/common/chrome_switches.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "content/public/common/context_menu_params.h"
+#include "extensions/common/switches.h"
 #include "net/dns/mock_host_resolver.h"
 #include "ui/base/models/menu_model.h"
 
@@ -609,8 +609,10 @@ class ExtensionContextMenuBrowserLazyTest :
   virtual void SetUpCommandLine(CommandLine* command_line) OVERRIDE {
     ExtensionContextMenuBrowserTest::SetUpCommandLine(command_line);
     // Set shorter delays to prevent test timeouts.
-    command_line->AppendSwitchASCII(switches::kEventPageIdleTime, "0");
-    command_line->AppendSwitchASCII(switches::kEventPageSuspendingTime, "0");
+    command_line->AppendSwitchASCII(
+        extensions::switches::kEventPageIdleTime, "0");
+    command_line->AppendSwitchASCII(
+        extensions::switches::kEventPageSuspendingTime, "0");
   }
 };
 
