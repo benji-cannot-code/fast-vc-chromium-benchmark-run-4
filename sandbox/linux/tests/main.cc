856815433878cc2fa091b098600bef599730565d
jln@chromium.org <jln@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-05 18:56:13 +0000

    Linux: add basic unprivileged namespace support.
    The Credentials class now has basic support for unprivileged namespaces.
    
    BUG=312380
    R=jorgelo@chromium.org
    
    Review URL: https://codereview.chromium.org/54643010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233041 0039d316-1c4b-4281-b951-d872f2087c98

--- sandbox/linux/tests/main.cc
+++ sandbox/linux/tests/main.cc
@@ -3,9 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/at_exit.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 int main(int argc, char *argv[]) {
+  // The use of Callbacks requires an AtExitManager.
+  base::AtExitManager exit_manager;
   testing::InitGoogleTest(&argc, argv);
   // Always go through re-execution for death tests.
   // This makes gtest only marginally slower for us and has the
