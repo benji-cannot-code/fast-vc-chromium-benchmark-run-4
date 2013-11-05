4c9cf68f47e2fdf4c0e35080c691759b4a68bab7
Tomasz Mikolajewski <mtomasz@chromium.org>
2013-11-05 13:36:07 +0900

    Make the Camera app accessible.
    This patch makes the Camera app accessible by enabling spoken feedback
    on all of the important controls.
    
    TEST=Tested manually with ChromeVox on all of the views.
    BUG=chromium:314914
    
    Change-Id: I6bd107e8ee65da1a16a70284e439633091e7ea67
    Reviewed-on: https://chromium-review.googlesource.com/175710
    Reviewed-by: Tomasz Mikolajewski <mtomasz@chromium.org>
    Tested-by: Tomasz Mikolajewski <mtomasz@chromium.org>

--- chrome/browser/resources/chromeos/camera/src/js/main.js
+++ chrome/browser/resources/chromeos/camera/src/js/main.js
@@ -243,6 +243,7 @@ camera.Camera.prototype.start = function() {
     this.tooltipManager_.initialize();
     this.viewsStack_.push(this.cameraView_);
     camera.util.makeEmptyButtonsUnfocusable();
+    camera.util.setAriaAttributes();
     callback();
   }.bind(this));
 };
--- chrome/browser/resources/chromeos/camera/src/js/views/dialog.js
+++ chrome/browser/resources/chromeos/camera/src/js/views/dialog.js
@@ -94,7 +94,6 @@ camera.views.Dialog.prototype.onEnter = function(opt_arguments) {
   this.messageElement_.textContent = opt_arguments.message;
 
   document.body.classList.add('dialog');
-  this.positiveButton_.focus();
 };
 
 /**
@@ -103,6 +102,13 @@ camera.views.Dialog.prototype.onEnter = function(opt_arguments) {
  */
 camera.views.Dialog.prototype.onLeave = function() {
   document.body.classList.remove('dialog');
+};
+
+/**
+ * @override
+ */
+camera.views.Dialog.prototype.onActivate = function() {
+  document.querySelector('#dialog-popup').focus();
 };
 
 /**
@@ -136,6 +142,12 @@ camera.views.Dialog.prototype.onNegativeButtonClicked_ = function(event) {
  */
 camera.views.Dialog.prototype.onKeyPressed = function(event) {
   switch (camera.util.getShortcutIdentifier(event)) {
+    case 'Enter':
+      if (document.activeElement == this.negativeButton_)
+        break;
+      this.router.back({isPositive: true});
+      event.preventDefault();
+      break;
     case 'U+001B':  // Escape.
       this.router.back({isPositive: false});
       event.preventDefault();
