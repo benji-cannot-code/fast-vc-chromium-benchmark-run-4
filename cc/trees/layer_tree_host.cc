57bd2cf5548eaefb8cae93ac820e240024fadf46
dongseong.hwang@intel.com <dongseong.hwang@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-05 19:11:17 +0000

    Make tree id sequence in LayerTreeHost thread-safe.
    LayerTreeHost instances can exists in multiple threads. e.g. Aura with
    --single-process. So this CL makes the sequence thread-safe.
    
    In addition, this CL removes unused s_num_layer_tree_instances.
    
    Review URL: https://codereview.chromium.org/57713004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233047 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/trees/layer_tree_host.cc
+++ cc/trees/layer_tree_host.cc
@@ -12,10 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/command_line.h"
 #include "base/debug/trace_event.h"
+#include "base/lazy_instance.h"
 #include "base/message_loop/message_loop.h"
 #include "base/metrics/histogram.h"
 #include "base/stl_util.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/synchronization/lock.h"
 #include "cc/animation/animation_registrar.h"
 #include "cc/animation/layer_animation_controller.h"
 #include "cc/base/math_util.h"
@@ -42,7 +44,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/size_conversions.h"
 
 namespace {
-static int s_num_layer_tree_instances;
+static base::LazyInstance<base::Lock>::Leaky
+    s_next_tree_id_lock = LAZY_INSTANCE_INITIALIZER;
+
+inline int GetNextTreeId() {
+  static int s_next_tree_id = 1;
+  base::AutoLock lock(s_next_tree_id_lock.Get());
+  return s_next_tree_id++;
+}
 }
 
 namespace cc {
@@ -90,10 +99,6 @@ UIResourceRequest& UIResourceRequest::operator=(
 
 UIResourceRequest::~UIResourceRequest() {}
 
-bool LayerTreeHost::AnyLayerTreeHostInstanceExists() {
-  return s_num_layer_tree_instances > 0;
-}
-
 scoped_ptr<LayerTreeHost> LayerTreeHost::Create(
     LayerTreeHostClient* client,
     SharedBitmapManager* manager,
@@ -105,8 +110,6 @@ scoped_ptr<LayerTreeHost> LayerTreeHost::Create(
     return scoped_ptr<LayerTreeHost>();
   return layer_tree_host.Pass();
 }
-
-static int s_next_tree_id = 1;
 
 LayerTreeHost::LayerTreeHost(LayerTreeHostClient* client,
                              SharedBitmapManager* manager,
@@ -136,12 +139,11 @@ LayerTreeHost::LayerTreeHost(LayerTreeHostClient* client,
       partial_texture_update_requests_(0),
       in_paint_layer_contents_(false),
       total_frames_used_for_lcd_text_metrics_(0),
-      tree_id_(s_next_tree_id++),
+      tree_id_(GetNextTreeId()),
       next_commit_forces_redraw_(false),
       shared_bitmap_manager_(manager) {
   if (settings_.accelerated_animation_enabled)
     animation_registrar_ = AnimationRegistrar::Create();
-  s_num_layer_tree_instances++;
   rendering_stats_instrumentation_->set_record_rendering_stats(
       debug_state_.RecordRenderingStats());
 }
@@ -182,8 +184,6 @@ LayerTreeHost::~LayerTreeHost() {
     DCHECK(proxy_->IsMainThread());
     proxy_->Stop();
   }
-
-  s_num_layer_tree_instances--;
 
   if (root_layer_.get()) {
     // The layer tree must be destroyed before the layer tree host. We've
