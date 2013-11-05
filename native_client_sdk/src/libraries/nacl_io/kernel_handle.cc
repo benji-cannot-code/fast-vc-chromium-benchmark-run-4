897d011628fcd96fb6aa54079b30a9c5f806b15e
sbc@chromium.org <sbc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-05 21:42:42 +0000

    [NaCl SDK] nacl_io: fix permissions on accepted sockets.
    Accepted sockets were not being marks as writable (O_RDWR).
    Also, calls to send and recv where not checking open flags
    so this was not being picked up by our tests.  This fixes
    both issues and increases test coverage.
    
    R=binji@chromium.org
    
    Review URL: https://codereview.chromium.org/60623002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233091 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/libraries/nacl_io/kernel_handle.cc
+++ native_client_sdk/src/libraries/nacl_io/kernel_handle.cc
@@ -178,6 +178,8 @@ Error KernelHandle::Recv(void* buf, size_t len, int flags, int* out_len) {
   MountNodeSocket* sock = socket_node();
   if (!sock)
     return ENOTSOCK;
+  if (OpenMode() == O_WRONLY)
+    return EACCES;
 
   AUTO_LOCK(handle_lock_);
   return sock->Recv(handle_attr_, buf, len, flags, out_len);
@@ -192,6 +194,8 @@ Error KernelHandle::RecvFrom(void* buf,
   MountNodeSocket* sock = socket_node();
   if (!sock)
     return ENOTSOCK;
+  if (OpenMode() == O_WRONLY)
+    return EACCES;
 
   AUTO_LOCK(handle_lock_);
   return sock->RecvFrom(handle_attr_, buf, len, flags, src_addr, addrlen,
@@ -205,6 +209,8 @@ Error KernelHandle::Send(const void* buf,
   MountNodeSocket* sock = socket_node();
   if (!sock)
     return ENOTSOCK;
+  if (OpenMode() == O_RDONLY)
+    return EACCES;
 
   AUTO_LOCK(handle_lock_);
   return sock->Send(handle_attr_, buf, len, flags, out_len);
@@ -219,6 +225,8 @@ Error KernelHandle::SendTo(const void* buf,
   MountNodeSocket* sock = socket_node();
   if (!sock)
     return ENOTSOCK;
+  if (OpenMode() == O_RDONLY)
+    return EACCES;
 
   AUTO_LOCK(handle_lock_);
   return sock->SendTo(handle_attr_, buf, len, flags, dest_addr, addrlen,
