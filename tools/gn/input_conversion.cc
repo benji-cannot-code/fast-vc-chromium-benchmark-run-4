fac2555c2c98c42d34700d0386623ed5edb662a2
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-05 14:17:27 +0000

    Make an include path relative to src/
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/54983009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232977 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/gn/input_conversion.cc
+++ tools/gn/input_conversion.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "input_conversion.h"
+#include "tools/gn/input_conversion.h"
 
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
