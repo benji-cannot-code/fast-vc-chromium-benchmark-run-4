48ce1ec8e0740793405dd97945e3962391ff0c21
mnaganov@chromium.org <mnaganov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-05 17:28:59 +0000

    [Android WebView] Add a quirk to clobber initial scale in certain cases
    In the following cases, the viewport meta tag would take precedence over
    the value specified in setInitialPageScaleOverride:
    
    The value provided to setInitialPageScaleOverride is <= 1 / device pixel ratio AND
    
       - viewport meta has width=device-width OR
    
       - viewport meta has auto width, and the page would be displayed at 100% scale.
    
    BUG=313754
    
    Review URL: https://codereview.chromium.org/55073002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161355 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebSettingsImpl.cpp
+++ third_party/WebKit/Source/web/WebSettingsImpl.cpp
@@ -58,6 +58,7 @@ WebSettingsImpl::WebSettingsImpl(Settings* settings)
     , m_doubleTapToZoomEnabled(false)
     , m_supportDeprecatedTargetDensityDPI(false)
     , m_viewportMetaLayoutSizeQuirk(false)
+    , m_clobberUserAgentInitialScaleQuirk(false)
     , m_pinchOverlayScrollbarThickness(0)
 {
     ASSERT(settings);
@@ -211,6 +212,11 @@ void WebSettingsImpl::setIgnoreMainFrameOverflowHiddenQuirk(bool ignoreMainFrame
 void WebSettingsImpl::setReportScreenSizeInPhysicalPixelsQuirk(bool reportScreenSizeInPhysicalPixelsQuirk)
 {
     m_settings->setReportScreenSizeInPhysicalPixelsQuirk(reportScreenSizeInPhysicalPixelsQuirk);
+}
+
+void WebSettingsImpl::setClobberUserAgentInitialScaleQuirk(bool clobberUserAgentInitialScaleQuirk)
+{
+    m_clobberUserAgentInitialScaleQuirk = clobberUserAgentInitialScaleQuirk;
 }
 
 void WebSettingsImpl::setSupportsMultipleWindows(bool supportsMultipleWindows)
