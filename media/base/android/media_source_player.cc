3b3d3eebc85e322f381ab8c97e14487c627e63ad
wolenetz@chromium.org <wolenetz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-05 05:46:23 +0000

    Allow simultaneous audio and video config change
    When completing prefetch for multiple streams, if more than one stream has
    |kConfigChanged| next, then the calls to DecodeMoreAudio() and
    DecodeMoreVideo() need to allow possibility that
    |CONFIG_CHANGE_EVENT_PENDING| is already set by the other stream.
    Also includes a new unit test for this scenario.
    
    R=qinmin@chromium.org,acolwell@chromium.org,xhwang@chromium.org
    BUG=314170
    TEST=All media_unittests pass on Android with MediaCodecBridge available.
    
    Review URL: https://codereview.chromium.org/50433007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232920 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/android/media_source_player.cc
+++ media/base/android/media_source_player.cc
@@ -656,7 +656,16 @@ void MediaSourcePlayer::DecodeMoreAudio() {
 
   // Failed to start the next decode.
   // Wait for demuxer ready message.
+  DCHECK(!reconfig_audio_decoder_);
   reconfig_audio_decoder_ = true;
+
+  // Config change may have just been detected on the other stream. If so,
+  // don't send a duplicate demuxer config request.
+  if (IsEventPending(CONFIG_CHANGE_EVENT_PENDING)) {
+    DCHECK(reconfig_video_decoder_);
+    return;
+  }
+
   SetPendingEvent(CONFIG_CHANGE_EVENT_PENDING);
   ProcessPendingEvents();
 }
@@ -676,11 +685,20 @@ void MediaSourcePlayer::DecodeMoreVideo() {
 
   // Failed to start the next decode.
   // Wait for demuxer ready message.
-  reconfig_video_decoder_ = true;
 
   // After this detection of video config change, next video data received
   // will begin with I-frame.
   next_video_data_is_iframe_ = true;
+
+  DCHECK(!reconfig_video_decoder_);
+  reconfig_video_decoder_ = true;
+
+  // Config change may have just been detected on the other stream. If so,
+  // don't send a duplicate demuxer config request.
+  if (IsEventPending(CONFIG_CHANGE_EVENT_PENDING)) {
+    DCHECK(reconfig_audio_decoder_);
+    return;
+  }
 
   SetPendingEvent(CONFIG_CHANGE_EVENT_PENDING);
   ProcessPendingEvents();
