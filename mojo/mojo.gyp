f5e72bc4857661ab70e92f7c58662ba7cf62a679
abarth@chromium.org <abarth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-05 13:45:02 +0000

    Add a basic NativeViewportX11 for Mojo
    This CL adds a basic implementation of NativeViewport for X11. It puts a window
    on screen and initializes GL for that window.
    
    One disadvantage of this CL is that it requires use_aura to be set to 1 in
    GYP_DEFINES.
    
    R=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/48323005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232960 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/mojo.gyp
+++ mojo/mojo.gyp
@@ -177,6 +177,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'type': 'executable',
       'dependencies': [
         '../base/base.gyp:base',
+        '../ui/gl/gl.gyp:gl',
         '../url/url.gyp:url_lib',
         'mojo_shell_lib',
         'mojo_system',
--- mojo/shell/desktop/mojo_main.cc
+++ mojo/shell/desktop/mojo_main.cc
@@ -8,10 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/message_loop/message_loop.h"
 #include "mojo/shell/run.h"
+#include "ui/gl/gl_surface.h"
 
 int main(int argc, char** argv) {
   base::AtExitManager at_exit;
   CommandLine::Init(argc, argv);
+
+  gfx::GLSurface::InitializeOneOff();
 
   base::MessageLoop message_loop(base::MessageLoop::TYPE_UI);
   mojo::shell::Context context;
