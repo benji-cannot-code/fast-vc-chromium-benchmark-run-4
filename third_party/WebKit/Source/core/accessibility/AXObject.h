581de4818a9218c2fbfc27d68fe959688a3c8ee6
eseidel@chromium.org <eseidel@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-05 20:38:20 +0000

    Remove dead code, AXObject::page
    I was looking for Page usages to remove
    in preparation for more OOPI work, but this
    code seems completely dead.
    
    BUG=305811
    
    Review URL: https://codereview.chromium.org/59403002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161362 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/accessibility/AXObject.h
+++ third_party/WebKit/Source/core/accessibility/AXObject.h
@@ -50,7 +50,6 @@ class HTMLAreaElement;
 class IntPoint;
 class IntSize;
 class Node;
-class Page;
 class RenderObject;
 class RenderListItem;
 class ScrollableArea;
@@ -509,7 +508,6 @@ public:
     virtual Element* anchorElement() const { return 0; }
     virtual Element* actionElement() const { return 0; }
     virtual Widget* widgetForAttachmentView() const { return 0; }
-    Page* page() const;
     String language() const;
     bool hasAttribute(const QualifiedName&) const;
     const AtomicString& getAttribute(const QualifiedName&) const;
