404fb5fdde3c4c6ba51b85a8d62c23b7becc41d3
aandrey@chromium.org <aandrey@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-05 12:18:39 +0000

    DevTools: Send returning value of a call frame.
    Send the returning value to front-end if a call frame is at the return point.
    
    BUG=155730
    R=yurys@chromium.org, pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/50043005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161323 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/DebuggerScript.js
+++ third_party/WebKit/Source/bindings/v8/DebuggerScript.js
@@ -339,6 +339,9 @@ DebuggerScript._frameMirrorToJSCallFrame = function(frameMirror, callerFrame)
     // Get this object.
     var thisObject = frameMirror.details_.receiver();
 
+    var isAtReturn = !!frameMirror.details_.isAtReturn();
+    var returnValue = isAtReturn ? frameMirror.details_.returnValue() : undefined;
+
     var scopeChain = [];
     var scopeType = [];
     for (var i = 0; i < frameMirror.scopeCount(); i++) {
@@ -397,6 +400,8 @@ DebuggerScript._frameMirrorToJSCallFrame = function(frameMirror, callerFrame)
         "restart": restart,
         "setVariableValue": setVariableValue,
         "stepInPositions": stepInPositions,
+        "isAtReturn": isAtReturn,
+        "returnValue": returnValue,
         "frameMirror": frameMirror
     };
 }
