97fa2bc96bfee2d1aa8928b672479e028b46d370
acolwell@chromium.org <acolwell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-05 22:54:11 +0000

    Add missing references to kDisableUnprefixedMediaSource so the option in about:flags actually works on desktop.
    Adds missing references to the flag that was added in http://crrev.com/227577
    
    BUG=304112
    
    Review URL: https://codereview.chromium.org/54103004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233123 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -921,6 +921,7 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kDisableTouchDragDrop,
     switches::kDisableTouchEditing,
     switches::kDisableUniversalAcceleratedOverflowScroll,
+    switches::kDisableUnprefixedMediaSource,
     switches::kDisableVp8AlphaPlayback,
     switches::kDisableWebAudio,
     switches::kDisableWebKitMediaSource,
