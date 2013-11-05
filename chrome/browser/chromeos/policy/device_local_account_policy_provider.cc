226a7c7e141a9d5cd8f48b96db3f1d9725ec4ee3
bartfab@chromium.org <bartfab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-05 19:11:23 +0000

    Support policies referencing external data for device-local accounts
    This CL adds a DeviceLocalAccountExternalDataService that provides each
    device-local account with a DeviceLocalAccountExternalDataManager which
    fetches, caches and retrieves external policy data.
    
    BUG=256635
    TEST=New browser test
    R=derat@chromium.org, joaodasilva@chromium.org
    
    Review URL: https://codereview.chromium.org/25242002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233048 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/policy/device_local_account_policy_provider.cc
+++ chrome/browser/chromeos/policy/device_local_account_policy_provider.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/policy/device_local_account_policy_provider.h"
 
 #include "base/bind.h"
+#include "chrome/browser/chromeos/policy/device_local_account_external_data_manager.h"
 #include "chrome/browser/policy/cloud/cloud_policy_core.h"
 #include "chrome/browser/policy/cloud/cloud_policy_service.h"
 #include "chrome/browser/policy/policy_bundle.h"
@@ -76,6 +77,7 @@ void DeviceLocalAccountPolicyProvider::UpdateFromBroker() {
       // Copy policy from the broker.
       bundle->Get(PolicyNamespace(POLICY_DOMAIN_CHROME, std::string()))
           .CopyFrom(broker->core()->store()->policy_map());
+      external_data_manager_ = broker->external_data_manager();
     } else {
       // Wait for the refresh to finish.
       return;
--- chrome/browser/chromeos/policy/device_local_account_policy_provider.h
+++ chrome/browser/chromeos/policy/device_local_account_policy_provider.h
@@ -10,7 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
+#include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
+#include "chrome/browser/chromeos/policy/device_local_account_external_data_manager.h"
 #include "chrome/browser/chromeos/policy/device_local_account_policy_service.h"
 #include "chrome/browser/policy/configuration_policy_provider.h"
 
@@ -50,6 +52,8 @@ class DeviceLocalAccountPolicyProvider
   void UpdateFromBroker();
 
   const std::string user_id_;
+  scoped_refptr<DeviceLocalAccountExternalDataManager> external_data_manager_;
+
   DeviceLocalAccountPolicyService* service_;
 
   bool store_initialized_;
