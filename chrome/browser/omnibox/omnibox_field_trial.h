7738d8638568b98b3840744918a8013f5b91658d
hfung@chromium.org <hfung@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-05 14:21:54 +0000

    Don't demote top match for certain match types.
    BUG=
    
    Review URL: https://codereview.chromium.org/55413002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232979 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/omnibox/omnibox_field_trial.h
+++ chrome/browser/omnibox/omnibox_field_trial.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_OMNIBOX_OMNIBOX_FIELD_TRIAL_H_
 
 #include <map>
+#include <set>
 #include <string>
 #include <vector>
 
@@ -22,6 +23,9 @@ class OmniboxFieldTrial {
   // specified type should have their relevance score multiplied by the
   // given number.  Omitted types are assumed to have multipliers of 1.0.
   typedef std::map<AutocompleteMatchType::Type, float> DemotionMultipliers;
+
+  // A set of types that should not be demoted when they are the top match.
+  typedef std::set<AutocompleteMatchType::Type> UndemotableTopMatchTypes;
 
   // Creates the static field trial groups.
   // *** MUST NOT BE CALLED MORE THAN ONCE. ***
@@ -149,6 +153,11 @@ class OmniboxFieldTrial {
       AutocompleteInput::PageClassification current_page_classification,
       DemotionMultipliers* demotions_by_type);
 
+  // Get the set of types that should not be demoted if they are the top
+  // match.
+  static UndemotableTopMatchTypes GetUndemotableTopTypes(
+      AutocompleteInput::PageClassification current_page_classification);
+
   // ---------------------------------------------------------
   // For the ReorderForLegalDefaultMatch experiment that's part of the
   // bundled omnibox field trial.
@@ -169,6 +178,7 @@ class OmniboxFieldTrial {
   static const char kShortcutsScoringMaxRelevanceRule[];
   static const char kSearchHistoryRule[];
   static const char kDemoteByTypeRule[];
+  static const char kUndemotableTopTypeRule[];
   static const char kReorderForLegalDefaultMatchRule[];
   // Rule values.
   static const char kReorderForLegalDefaultMatchRuleEnabled[];
