242a6de8e4e137a7ce241dd6f2339bdb707e4a03
morrita@chromium.org <morrita@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-06 08:37:50 +0000

    Clear DOMWindowEventQueue in Document::detach()
    This is a speculative fix for bug 313227.
    Apparently DOMWindowEventQueue potentially survives after the lifetime of
    companion Document. This change ensures that the queue is closed and cleared
    when the companion Document is gone.
    
    Note that this kind of lifetime misalignment should be fixed once
    DOMWindow becomes an ExecutionContext subclass.
    
    BUG=313227
    R=abarth,haraken
    
    Review URL: https://codereview.chromium.org/58883003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161434 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -1987,6 +1987,10 @@ void Document::detach(const AttachContext& context)
     if (svgExtensions())
         accessSVGExtensions()->pauseAnimations();
 
+    // FIXME: This shouldn't be needed once DOMWindow becomes ExecutionContext.
+    if (m_domWindow)
+        m_domWindow->clearEventQueue();
+
     RenderView* renderView = m_renderView;
 
     if (renderView)
--- third_party/WebKit/Source/core/frame/DOMWindow.h
+++ third_party/WebKit/Source/core/frame/DOMWindow.h
@@ -327,6 +327,10 @@ enum PageshowEventPersistence {
         void dispatchWindowLoadEvent();
         void documentWasClosed();
         void statePopped(PassRefPtr<SerializedScriptValue>);
+
+        // FIXME: This shouldn't be public once DOMWindow becomes ExecutionContext.
+        void clearEventQueue();
+
     protected:
         DOMWindowLifecycleNotifier& lifecycleNotifier();
 
