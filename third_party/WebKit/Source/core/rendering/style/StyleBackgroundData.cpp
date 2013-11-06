71d71565f1ff0c46730c3f4a202d38f27c54f344
alancutter@chromium.org <alancutter@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-06 09:30:15 +0000

    Web Animations CSS: Fix crash when transitioning background or mask properties
    This patch ensures the first layer for background and mask declares that
    their values are set. This has no visible effect on the way fill layers
    work, this just ensures an assertion made by the Web Animations engine
    about fill layer properties is correct.
    
    The current animation implementation did not make use of the flags on a fill layer
    that indicate whether a value was set or not and will be agnostic to this change.
    In order to implement correct interpolation behaviour[1] for many fill layer
    properties these flags need to be used.
    The existing FillLayer implementation was not setting these flags correctly for
    the initial values, this patch fixes that.
    
    [1] http://www.w3.org/TR/css3-transitions/#animtype-repeatable-list
    
    Fixes crashing tests:
    virtual/web-animations-css/transitions/multiple-background-size-transitions.html
    virtual/web-animations-css/transitions/shorthand-transitions.html
    
    BUG=257591
    
    Review URL: https://codereview.chromium.org/38573005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161438 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/style/StyleBackgroundData.cpp
+++ third_party/WebKit/Source/core/rendering/style/StyleBackgroundData.cpp
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 StyleBackgroundData::StyleBackgroundData()
-    : m_background(BackgroundFillLayer)
+    : m_background(BackgroundFillLayer, true)
     , m_color(RenderStyle::initialBackgroundColor())
 {
 }
