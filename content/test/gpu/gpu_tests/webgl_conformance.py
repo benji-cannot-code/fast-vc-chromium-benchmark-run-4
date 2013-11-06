5dd6674599ce75cd974b998cb772faa8f2682e9b
bajones@chromium.org <bajones@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-06 18:20:49 +0000

    Revert 233243 "Align the interface of TestHarness defined in gpu..."
    Reverting because this patch caused the GPU bots to begin failing. The failures
    may actually be legitimate, but we should determine if that's the case and fix
    the failing tests before landing this patch again
    
    > Align the interface of TestHarness defined in gpu_tests/webgl_conformance.py with that in third_party/webgl_conformance.
    > 
    > BUG=314997
    > NOTRY=true
    > 
    > Review URL: https://codereview.chromium.org/58833003
    
    TBR=jun.a.jiang@intel.com
    
    Review URL: https://codereview.chromium.org/62573002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233299 0039d316-1c4b-4281-b951-d872f2087c98

--- content/test/gpu/gpu_tests/webgl_conformance.py
+++ content/test/gpu/gpu_tests/webgl_conformance.py
@@ -30,7 +30,7 @@ conformance_harness_script = r"""
     testHarness._originalLog.apply(window.console, [msg]);
   }
 
-  testHarness.reportResults = function(url, success, msg) {
+  testHarness.reportResults = function(success, msg) {
     testHarness._allTestSucceeded = testHarness._allTestSucceeded && !!success;
     if(!success) {
       testHarness._failures++;
@@ -39,7 +39,7 @@ conformance_harness_script = r"""
       }
     }
   };
-  testHarness.notifyFinished = function(url) {
+  testHarness.notifyFinished = function() {
     testHarness._finished = true;
   };
   testHarness.navigateToPage = function(src) {
