ce85dce0f3a0f90275509c033d1b22610f02aa5e
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-06 22:46:09 +0000

    Store prefs::kVariationsSeedDate on a variations server 304.
    This prevents an issue where the variations seed not changing
    in a while will result in studies not actually expiring.
    
    Also, removes an unused header include.
    
    BUG=315177
    TEST=New unit test.
    
    Review URL: https://codereview.chromium.org/59953005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233406 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metrics/variations/variations_service.cc
+++ chrome/browser/metrics/variations/variations_service.cc
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/variations/proto/variations_seed.pb.h"
 #include "components/variations/variations_seed_processor.h"
 #include "content/public/browser/browser_thread.h"
-#include "content/public/common/url_fetcher.h"
 #include "net/base/load_flags.h"
 #include "net/base/net_errors.h"
 #include "net/base/network_change_notifier.h"
@@ -399,6 +398,10 @@ void VariationsService::OnURLFetchComplete(const net::URLFetcher* source) {
     if (response_code == net::HTTP_NOT_MODIFIED) {
       UMA_HISTOGRAM_MEDIUM_TIMES("Variations.FetchNotModifiedLatency", latency);
       RecordLastFetchTime();
+      // Update the seed date value in local state (used for expiry check on
+      // next start up), since 304 is a successful response.
+      local_state_->SetInt64(prefs::kVariationsSeedDate,
+                             response_date.ToInternalValue());
     } else {
       UMA_HISTOGRAM_MEDIUM_TIMES("Variations.FetchOtherLatency", latency);
     }
