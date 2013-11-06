cc9ed554cd1056537772fb824971e139cbee2a1b
scottmg@chromium.org <scottmg@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-06 20:02:13 +0000

    Enable chromium_code=1 on Windows
    TBR=dglazkov@chromium.org
    R=thakis@chromium.org
    BUG=312287
    
    Review URL: https://codereview.chromium.org/60113005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161469 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/shadow/ShadowRoot.cpp
+++ third_party/WebKit/Source/core/dom/shadow/ShadowRoot.cpp
@@ -208,7 +208,7 @@ void ShadowRoot::setApplyAuthorStyles(bool value)
     if (isOrphan())
         return;
 
-    if (m_applyAuthorStyles == value)
+    if (applyAuthorStyles() == value)
         return;
 
     m_applyAuthorStyles = value;
@@ -234,7 +234,7 @@ void ShadowRoot::setResetStyleInheritance(bool value)
     if (isOrphan())
         return;
 
-    if (value == m_resetStyleInheritance)
+    if (value == resetStyleInheritance())
         return;
 
     m_resetStyleInheritance = value;
--- third_party/WebKit/Source/core/rendering/RenderLayerStackingNode.cpp
+++ third_party/WebKit/Source/core/rendering/RenderLayerStackingNode.cpp
@@ -439,7 +439,7 @@ bool RenderLayerStackingNode::shouldBeNormalFlowOnlyIgnoringCompositedScrolling(
 void RenderLayerStackingNode::updateIsNormalFlowOnly()
 {
     bool isNormalFlowOnly = shouldBeNormalFlowOnly();
-    if (isNormalFlowOnly == m_isNormalFlowOnly)
+    if (isNormalFlowOnly == this->isNormalFlowOnly())
         return;
 
     m_isNormalFlowOnly = isNormalFlowOnly;
@@ -621,7 +621,7 @@ bool RenderLayerStackingNode::descendantsAreContiguousInStackingOrder() const
 
 bool RenderLayerStackingNode::setNeedsToBeStackingContainer(bool needsToBeStackingContainer)
 {
-    if (m_needsToBeStackingContainer == needsToBeStackingContainer)
+    if (this->needsToBeStackingContainer() == needsToBeStackingContainer)
         return false;
 
     // Count the total number of RenderLayers which need to be stacking
