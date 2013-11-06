e166a7316549c89e71f05a6b422f1cc9936e1b0b
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-06 20:14:00 +0000

    fix second window not painting on aura while cert dialog is open
    In preference to https://codereview.chromium.org/61093002/.
    
    R=sky@chromium.org
    BUG=306295
    
    Review URL: https://codereview.chromium.org/59613009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233332 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/certificate_viewer_win.cc
+++ chrome/browser/ui/views/certificate_viewer_win.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma comment(lib, "cryptui.lib")
 
 #include "base/logging.h"
+#include "base/message_loop/message_loop.h"
 #include "net/cert/x509_certificate.h"
 
 #if defined(USE_AURA)
@@ -41,6 +42,11 @@ void ShowCertificateViewerImpl(content::WebContents* web_contents,
   view_info.rghStores = &cert_store;
   BOOL properties_changed;
 
+  // We must allow nested tasks to dispatch so that, e.g. gpu tasks are
+  // processed for painting. This allows a second window to continue painting
+  // while the the certificate dialog is open.
+  base::MessageLoop::ScopedNestableTaskAllower allow(
+      base::MessageLoop::current());
   // This next call blocks but keeps processing windows messages, making it
   // modal to the browser window.
   BOOL rv = ::CryptUIDlgViewCertificate(&view_info, &properties_changed);
