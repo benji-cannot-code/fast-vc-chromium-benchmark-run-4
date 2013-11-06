0d01a7bc6cfedffe1d8f50aedb2cd01406509500
Tomasz Mikolajewski <mtomasz@chromium.org>
2013-11-06 14:10:43 +0900

    Reuse a texture between effects.
    Instead of having a texture per each effect, we should share it between
    all effects, since it is the same.
    
    This improves performance on ARM devices. From around 12 to 16 fps when
    the ribbon is expanded.
    
    TEST=Tested manually on Yoshi.
    BUG=None
    
    Change-Id: I834740f3c0c9f3bb5800e399cfa1c643506c90d9
    Reviewed-on: https://chromium-review.googlesource.com/175890
    Reviewed-by: Daichi Hirono <hirono@chromium.org>
    Tested-by: Tomasz Mikolajewski <mtomasz@chromium.org>

--- src/js/processor.js
+++ src/js/processor.js
@@ -14,7 +14,7 @@ var camera = camera || {};
  * Creates a processor object, which takes the camera stream and processes it.
  * Flushes the result to a canvas.
  *
- * @param {Canvas|Video} input Canvas or Video with the input frame.
+ * @param {fx.Texture} input Texture with the input frame.
  * @param {Canvas} output Canvas with the output frame.
  * @param {fx.Canvas} fxCanvas Fx canvas to be used for processing.
  * @param {camera.Processor.Mode=} opt_mode Optional mode of the processor.
@@ -23,7 +23,7 @@ var camera = camera || {};
  */
 camera.Processor = function(input, output, fxCanvas, opt_mode) {
   /**
-   * @type {Canvas|Video}
+   * @type {fx.Texture}
    * @private
    */
   this.input_ = input;
@@ -45,12 +45,6 @@ camera.Processor = function(input, output, fxCanvas, opt_mode) {
    * @private
    */
   this.mode_ = opt_mode || camera.Processor.Mode.DEFAULT;
-
-  /**
-   * @type {fx.Texture}
-   * @private
-   */
-  this.texture_ = null;
 
   /**
    * @type {camera.Effect}
@@ -79,9 +73,6 @@ camera.Processor.prototype = {
   get effect() {
     return this.effect_;
   },
-  get input() {
-    return this.input_;
-  },
   get output() {
     return this.output_;
   }
@@ -92,13 +83,10 @@ camera.Processor.prototype = {
  * canvas.
  */
 camera.Processor.prototype.processFrame = function() {
-  var width = this.input_.videoWidth || this.input_.width;
-  var height = this.input_.videoHeight || this.input_.height;
+  var width = this.input_._.width;
+  var height = this.input_._.height;
   if (!width || !height)
     return;
-
-  if (!this.texture_)
-    this.texture_ = this.fxCanvas_.texture(this.input_);
 
   var textureWidth = null;
   var textureHeight = null;
@@ -114,8 +102,7 @@ camera.Processor.prototype.processFrame = function() {
       break;
   }
 
-  this.texture_.loadContentsOf(this.input_);
-  this.fxCanvas_.draw(this.texture_, textureWidth, textureHeight);
+  this.fxCanvas_.draw(this.input_, textureWidth, textureHeight);
   if (this.effect_)
     this.effect_.filterFrame(this.fxCanvas_);
   this.fxCanvas_.update();
--- src/js/tracker.js
+++ src/js/tracker.js
@@ -223,6 +223,14 @@ camera.Tracker.prototype = {
    */
   get fps() {
     return this.performanceMonitor_.fps;
+  },
+
+  /**
+   * Returns true if busy, false otherwise.
+   * @return {boolean}
+   */
+  get busy() {
+    return this.busy_;
   }
 };
 
