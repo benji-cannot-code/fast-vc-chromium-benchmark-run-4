5acb1048f467b2f247be3e84f45f3560ab0153d3
koz@chromium.org <koz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-06 05:43:01 +0000

    Add UMA for profile creation during startup.
    BUG=314526
    
    Review URL: https://codereview.chromium.org/57603002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233222 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_browser_main.cc
+++ chrome/browser/chrome_browser_main.cc
@@ -329,6 +329,7 @@ Profile* CreateProfile(const content::MainFunctionParams& parameters,
                        const base::FilePath& user_data_dir,
                        const CommandLine& parsed_command_line) {
   TRACE_EVENT0("startup", "ChromeBrowserMainParts::CreateProfile")
+  base::Time start = base::Time::Now();
   if (profiles::IsMultipleProfilesEnabled() &&
       parsed_command_line.HasSwitch(switches::kProfileDirectory)) {
     g_browser_process->local_state()->SetString(prefs::kProfileLastUsed,
@@ -352,8 +353,11 @@ Profile* CreateProfile(const content::MainFunctionParams& parameters,
   profile = g_browser_process->profile_manager()->GetProfile(
       profile_path);
 #endif
-  if (profile)
+  if (profile) {
+    UMA_HISTOGRAM_LONG_TIMES(
+        "Startup.CreateFirstProfile", base::Time::Now() - start);
     return profile;
+  }
 
 #if !defined(OS_WIN)
   // TODO(port): fix this.  See comments near the definition of
