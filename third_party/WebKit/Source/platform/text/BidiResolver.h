5fcfee007f90450dc228997e32373e811c3ec855
igor.o@sisa.samsung.com <igor.o@sisa.samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-06 02:13:20 +0000

    Currently Blink creates a fake run for a given isolated span during each call to createBidiRunsForLine. However in some cases the added fake run is not necessary.
    This patch uses the same logic in RenderBlockFlow::appendRunsObject to create
    fake runs just when they are really needed.
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=160495
    
    Review URL: https://codereview.chromium.org/39783004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161410 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/text/BidiResolver.h
+++ third_party/WebKit/Source/platform/text/BidiResolver.h
@@ -120,6 +120,7 @@ struct BidiCharacterRun {
         , m_start(start)
         , m_stop(stop)
     {
+        ASSERT(m_start <= m_stop);
         if (dir == WTF::Unicode::OtherNeutral)
             dir = context->dir();
 
