660a0c894687caf7a7390144dbd938fb0524a0d8
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-06 21:11:18 +0000

    Data type conversion between extensions API and Cast API
    Converts to and from Cast API types. Also fixed a couple issues to
    match the API between Cast and Extensions API.
    
    Cast Extensions API is now fully redirected to Cast native APIs. Minus
    the conversion of MediaStreamTrack.
    
    BUG=301920
    
    Review URL: https://codereview.chromium.org/47343005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233366 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/webrtc_cast_apitest.cc
+++ chrome/browser/extensions/webrtc_cast_apitest.cc
@@ -3,12 +3,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/command_line.h"
 #include "chrome/browser/extensions/extension_apitest.h"
+#include "chrome/common/chrome_switches.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace extensions {
 
 class WebrtcCastApiTest : public ExtensionApiTest {
+  virtual void SetUpCommandLine(CommandLine* command_line) OVERRIDE {
+    ExtensionApiTest::SetUpCommandLine(command_line);
+    command_line->AppendSwitchASCII(
+        switches::kWhitelistedExtensionID,
+        "ddchlicdkolnonkihahngkmmmjnjlkkf");
+  }
 };
 
 // Test running the test extension for Cast Mirroring API.
