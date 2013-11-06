c29e37b4c75ad8038ef45786f007b323d7c3f735
pastarmovj@google.com <pastarmovj@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-06 12:59:06 +0000

    Add a flag to skip robot token fetching for testing device enrollments.
    None of the testing server implementations are currently capable of minting
    robot OAuth tokens and testing of device enrollment was only possible against
    actual DMServer. With this flag it will be possible to successfully complete
    enrollment while still testing the whole enrollment handshake and only skipping
    the step where the robot token is fetched from GAIA.
    
    BUG=313811
    TEST=Enrollment against YAPS should succeed.
    R=joaodasilva@chromium.org
    
    Review URL: https://codereview.chromium.org/60113002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233262 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/policy/enrollment_handler_chromeos.cc
+++ chrome/browser/chromeos/policy/enrollment_handler_chromeos.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/policy/enrollment_handler_chromeos.h"
 
 #include "base/bind.h"
+#include "base/command_line.h"
 #include "base/logging.h"
 #include "base/message_loop/message_loop.h"
 #include "chrome/browser/browser_process.h"
@@ -15,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/policy/cloud/cloud_policy_constants.h"
 #include "chrome/browser/policy/proto/chromeos/chrome_device_policy.pb.h"
 #include "chrome/browser/policy/proto/cloud/device_management_backend.pb.h"
+#include "chromeos/chromeos_switches.h"
 #include "google_apis/gaia/gaia_urls.h"
 #include "net/http/http_status_code.h"
 
@@ -28,6 +30,11 @@ namespace {
 const int kLockRetryIntervalMs = 500;
 // Maximum time to retry InstallAttrs initialization before we give up.
 const int kLockRetryTimeoutMs = 10 * 60 * 1000;  // 10 minutes.
+
+// Testing token used when the enrollment-skip-robot-auth is set to skip talking
+// to GAIA for an actual token. This is needed to be able to run against the
+// testing DMServer implementations.
+const char kTestingRobotToken[] = "test-token";
 
 }  // namespace
 
@@ -187,6 +194,16 @@ void EnrollmentHandlerChromeOS::PolicyValidated(
     policy_ = validator->policy().Pass();
     username_ = validator->policy_data()->username();
     device_id_ = validator->policy_data()->device_id();
+
+    if (CommandLine::ForCurrentProcess()->HasSwitch(
+            chromeos::switches::kEnterpriseEnrollmentSkipRobotAuth)) {
+      // For test purposes we allow enrollment to succeed without proper robot
+      // account and use the provided value as a token.
+      refresh_token_ = kTestingRobotToken;
+      enrollment_step_ = STEP_LOCK_DEVICE,
+      StartLockDevice(username_, device_mode_, device_id_);
+      return;
+    }
 
     enrollment_step_ = STEP_ROBOT_AUTH_FETCH;
     client_->FetchRobotAuthCodes(auth_token_);
