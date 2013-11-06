7dafbc11ddcf4641489688ae5f8c93eafe1aabc0
jdduke@chromium.org <jdduke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-06 02:17:30 +0000

    [Android] Fix missing onSingleTapConfirmed calls in GestureDetector
    Eliminate the short period of time between the tap timeout and long
    press causing onSingleTapConfirmed not to be dispatched when apps
    expect.
    
    Note: This is a cherry-pick of the upstream Android fix for b/8124095.
    
    BUG=315124
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/59173006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233192 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/third_party/GestureDetector.java
+++ content/public/android/java/src/org/chromium/content/browser/third_party/GestureDetector.java
@@ -226,6 +226,7 @@ public class GestureDetector {
     private OnDoubleTapListener mDoubleTapListener;
 
     private boolean mStillDown;
+    private boolean mDeferConfirmSingleTap;
     private boolean mInLongPress;
     private boolean mAlwaysInTapRegion;
     private boolean mAlwaysInBiggerTapRegion;
@@ -282,8 +283,12 @@ public class GestureDetector {
                 
             case TAP:
                 // If the user's finger is still down, do not count it as a tap
-                if (mDoubleTapListener != null && !mStillDown) {
-                    mDoubleTapListener.onSingleTapConfirmed(mCurrentDownEvent);
+                if (mDoubleTapListener != null) {
+                    if (!mStillDown) {
+                        mDoubleTapListener.onSingleTapConfirmed(mCurrentDownEvent);
+                    } else {
+                        mDeferConfirmSingleTap = true;
+                    }
                 }
                 break;
 
@@ -556,7 +561,8 @@ public class GestureDetector {
             mAlwaysInBiggerTapRegion = true;
             mStillDown = true;
             mInLongPress = false;
-            
+            mDeferConfirmSingleTap = false;
+
             if (mIsLongpressEnabled) {
                 mHandler.removeMessages(LONG_PRESS);
                 mHandler.sendEmptyMessageAtTime(LONG_PRESS, mCurrentDownEvent.getDownTime()
@@ -609,6 +615,9 @@ public class GestureDetector {
                 mInLongPress = false;
             } else if (mAlwaysInTapRegion) {
                 handled = mListener.onSingleTapUp(ev);
+                if (mDeferConfirmSingleTap && mDoubleTapListener != null) {
+                    mDoubleTapListener.onSingleTapConfirmed(ev);
+                }
             } else {
 
                 // A fling must travel the minimum tap distance
@@ -635,6 +644,7 @@ public class GestureDetector {
                 mVelocityTracker = null;
             }
             mIsDoubleTapping = false;
+            mDeferConfirmSingleTap = false;
             mHandler.removeMessages(SHOW_PRESS);
             mHandler.removeMessages(LONG_PRESS);
             break;
@@ -662,6 +672,7 @@ public class GestureDetector {
         mStillDown = false;
         mAlwaysInTapRegion = false;
         mAlwaysInBiggerTapRegion = false;
+        mDeferConfirmSingleTap = false;
         if (mInLongPress) {
             mInLongPress = false;
         }
@@ -674,6 +685,7 @@ public class GestureDetector {
         mIsDoubleTapping = false;
         mAlwaysInTapRegion = false;
         mAlwaysInBiggerTapRegion = false;
+        mDeferConfirmSingleTap = false;
         if (mInLongPress) {
             mInLongPress = false;
         }
@@ -696,6 +708,7 @@ public class GestureDetector {
 
     private void dispatchLongPress() {
         mHandler.removeMessages(TAP);
+        mDeferConfirmSingleTap = false;
         mInLongPress = true;
         mListener.onLongPress(mCurrentDownEvent);
     }
