8e0026a3b4f5649bbef57d099ec0db5c1b6afd17
mohsen@chromium.org <mohsen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-06 01:13:45 +0000

    Enable touch for autofill popup view
    Previously, items in autofill popup couldn't be selected using touch.
    Now, gesture events are handled on autofill popup to enable touch.
    
    BUG=290429
    
    Review URL: https://codereview.chromium.org/44543002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233172 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/autofill/autofill_popup_view_cocoa.mm
+++ chrome/browser/ui/cocoa/autofill/autofill_popup_view_cocoa.mm
@@ -159,8 +159,8 @@ NSColor* SubtextColor() {
                                fromView:nil];
 
   if (NSPointInRect(location, [self bounds])) {
-    controller_->MouseClicked(static_cast<int>(location.x),
-                              static_cast<int>(location.y));
+    controller_->LineAcceptedAtPoint(static_cast<int>(location.x),
+                                     static_cast<int>(location.y));
   }
 }
 
@@ -172,8 +172,8 @@ NSColor* SubtextColor() {
   NSPoint location = [self convertPoint:[theEvent locationInWindow]
                                fromView:nil];
 
-  controller_->MouseHovered(static_cast<int>(location.x),
-                            static_cast<int>(location.y));
+  controller_->LineSelectedAtPoint(static_cast<int>(location.x),
+                                   static_cast<int>(location.y));
 }
 
 - (void)mouseDragged:(NSEvent*)theEvent {
@@ -185,7 +185,7 @@ NSColor* SubtextColor() {
   if (!controller_)
     return;
 
-  controller_->MouseExitedPopup();
+  controller_->SelectionCleared();
 }
 
 #pragma mark -
