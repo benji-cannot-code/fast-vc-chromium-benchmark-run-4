5511e151fea9472cae98cd6a0012b0439a9f5ef2
noms@chromium.org <noms@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-07 18:08:49 +0000

    Use the correct guest icon in the new avatar bubble.
    BUG=
    R=oshima@chromium.org, pkasting@chromium.org
    
    Review URL: https://codereview.chromium.org/59943005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233642 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -572,7 +572,7 @@ int BrowserView::GetOTRIconResourceID() const {
 }
 
 int BrowserView::GetGuestIconResourceID() const {
-  return IDR_GUEST_ICON;
+  return IDR_LOGIN_GUEST;
 }
 
 bool BrowserView::ShouldShowAvatar() const {
