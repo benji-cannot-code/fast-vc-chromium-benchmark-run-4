e9f875a15b65dfbb28be5dc6e0587d012e3434b3
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-07 21:53:03 +0000

    Support variation params for forcing_flag groups.
    BUG=315860
    TEST=New unit test.
    
    Review URL: https://codereview.chromium.org/62953004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233701 0039d316-1c4b-4281-b951-d872f2087c98

--- components/variations/variations_seed_processor_unittest.cc
+++ components/variations/variations_seed_processor_unittest.cc
@@ -74,7 +74,23 @@ bool IsFieldTrialActive(const std::string& trial_name) {
 
 }  // namespace
 
-TEST(VariationsSeedProcessorTest, CheckStudyChannel) {
+class VariationsSeedProcessorTest : public ::testing::Test {
+ public:
+  VariationsSeedProcessorTest() {
+  }
+
+  virtual ~VariationsSeedProcessorTest() {
+    // Ensure that the maps are cleared between tests, since they are stored as
+    // process singletons.
+    testing::ClearAllVariationIDs();
+    testing::ClearAllVariationParams();
+  }
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(VariationsSeedProcessorTest);
+};
+
+TEST_F(VariationsSeedProcessorTest, CheckStudyChannel) {
   VariationsSeedProcessor seed_processor;
 
   const Study_Channel channels[] = {
@@ -120,7 +136,7 @@ TEST(VariationsSeedProcessorTest, CheckStudyChannel) {
   }
 }
 
-TEST(VariationsSeedProcessorTest, CheckStudyLocale) {
+TEST_F(VariationsSeedProcessorTest, CheckStudyLocale) {
   VariationsSeedProcessor seed_processor;
 
   struct {
@@ -153,7 +169,7 @@ TEST(VariationsSeedProcessorTest, CheckStudyLocale) {
   }
 }
 
-TEST(VariationsSeedProcessorTest, CheckStudyPlatform) {
+TEST_F(VariationsSeedProcessorTest, CheckStudyPlatform) {
   VariationsSeedProcessor seed_processor;
 
   const Study_Platform platforms[] = {
@@ -205,7 +221,7 @@ TEST(VariationsSeedProcessorTest, CheckStudyPlatform) {
   }
 }
 
-TEST(VariationsSeedProcessorTest, CheckStudyStartDate) {
+TEST_F(VariationsSeedProcessorTest, CheckStudyStartDate) {
   VariationsSeedProcessor seed_processor;
 
   const base::Time now = base::Time::Now();
@@ -232,7 +248,7 @@ TEST(VariationsSeedProcessorTest, CheckStudyStartDate) {
   }
 }
 
-TEST(VariationsSeedProcessorTest, CheckStudyVersion) {
+TEST_F(VariationsSeedProcessorTest, CheckStudyVersion) {
   VariationsSeedProcessor seed_processor;
 
   const struct {
@@ -324,7 +340,7 @@ TEST(VariationsSeedProcessorTest, CheckStudyVersion) {
 }
 
 // Test that the group for kForcingFlag1 is forced.
-TEST(VariationsSeedProcessorTest, ForceGroupWithFlag1) {
+TEST_F(VariationsSeedProcessorTest, ForceGroupWithFlag1) {
   CommandLine::ForCurrentProcess()->AppendSwitch(kForcingFlag1);
 
   base::FieldTrialList field_trial_list(NULL);
@@ -337,7 +353,7 @@ TEST(VariationsSeedProcessorTest, ForceGroupWithFlag1) {
 }
 
 // Test that the group for kForcingFlag2 is forced.
-TEST(VariationsSeedProcessorTest, ForceGroupWithFlag2) {
+TEST_F(VariationsSeedProcessorTest, ForceGroupWithFlag2) {
   CommandLine::ForCurrentProcess()->AppendSwitch(kForcingFlag2);
 
   base::FieldTrialList field_trial_list(NULL);
@@ -349,7 +365,7 @@ TEST(VariationsSeedProcessorTest, ForceGroupWithFlag2) {
             base::FieldTrialList::FindFullName(kFlagStudyName));
 }
 
-TEST(VariationsSeedProcessorTest, ForceGroup_ChooseFirstGroupWithFlag) {
+TEST_F(VariationsSeedProcessorTest, ForceGroup_ChooseFirstGroupWithFlag) {
   // Add the flag to the command line arguments so the flag group is forced.
   CommandLine::ForCurrentProcess()->AppendSwitch(kForcingFlag1);
   CommandLine::ForCurrentProcess()->AppendSwitch(kForcingFlag2);
@@ -363,7 +379,7 @@ TEST(VariationsSeedProcessorTest, ForceGroup_ChooseFirstGroupWithFlag) {
             base::FieldTrialList::FindFullName(kFlagStudyName));
 }
 
-TEST(VariationsSeedProcessorTest, ForceGroup_DontChooseGroupWithFlag) {
+TEST_F(VariationsSeedProcessorTest, ForceGroup_DontChooseGroupWithFlag) {
   base::FieldTrialList field_trial_list(NULL);
 
   // The two flag groups are given high probability, which would normally make
@@ -375,7 +391,7 @@ TEST(VariationsSeedProcessorTest, ForceGroup_DontChooseGroupWithFlag) {
             base::FieldTrialList::FindFullName(kFlagStudyName));
 }
 
-TEST(VariationsSeedProcessorTest, IsStudyExpired) {
+TEST_F(VariationsSeedProcessorTest, IsStudyExpired) {
   VariationsSeedProcessor seed_processor;
 
   const base::Time now = base::Time::Now();
@@ -402,7 +418,8 @@ TEST(VariationsSeedProcessorTest, IsStudyExpired) {
   }
 }
 
-TEST(VariationsSeedProcessorTest, NonExpiredStudyPrioritizedOverExpiredStudy) {
+TEST_F(VariationsSeedProcessorTest,
+       NonExpiredStudyPrioritizedOverExpiredStudy) {
   VariationsSeedProcessor seed_processor;
 
   const std::string kTrialName = "A";
@@ -445,7 +462,7 @@ TEST(VariationsSeedProcessorTest, NonExpiredStudyPrioritizedOverExpiredStudy) {
   }
 }
 
-TEST(VariationsSeedProcessorTest, ValidateStudy) {
+TEST_F(VariationsSeedProcessorTest, ValidateStudy) {
   VariationsSeedProcessor seed_processor;
 
   Study study;
@@ -515,7 +532,7 @@ TEST(VariationsSeedProcessorTest, ValidateStudy) {
   EXPECT_FALSE(valid);
 }
 
-TEST(VariationsSeedProcessorTest, VariationParams) {
+TEST_F(VariationsSeedProcessorTest, VariationParams) {
   base::FieldTrialList field_trial_list(NULL);
   VariationsSeedProcessor seed_processor;
 
@@ -540,7 +557,21 @@ TEST(VariationsSeedProcessorTest, VariationParams) {
   EXPECT_EQ(std::string(), GetVariationParamValue("Study2", "x"));
 }
 
-TEST(VariationsSeedProcessorTest, StartsActive) {
+TEST_F(VariationsSeedProcessorTest, VariationParamsWithForcingFlag) {
+  Study study = CreateStudyWithFlagGroups(100, 0, 0);
+  ASSERT_EQ(kForcingFlag1, study.experiment(1).forcing_flag());
+  Study_Experiment_Param* param = study.mutable_experiment(1)->add_param();
+  param->set_name("x");
+  param->set_value("y");
+
+  CommandLine::ForCurrentProcess()->AppendSwitch(kForcingFlag1);
+  base::FieldTrialList field_trial_list(NULL);
+  VariationsSeedProcessor().CreateTrialFromStudy(study, false);
+  EXPECT_EQ(kFlagGroup1Name, base::FieldTrialList::FindFullName(study.name()));
+  EXPECT_EQ("y", GetVariationParamValue(study.name(), "x"));
+}
+
+TEST_F(VariationsSeedProcessorTest, StartsActive) {
   base::FieldTrialList field_trial_list(NULL);
 
   VariationsSeed seed;
