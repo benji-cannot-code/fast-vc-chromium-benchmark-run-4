2477dcd23acde593b741f7b1faecbd5a198644ce
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-07 00:10:10 +0000

    Add ::Leaky to the LazyInstance<SafeBrowsingServiceFactoryImpl> in safe_browsing_service.cc.  Hopefully this will fix the "pure virtual method called" errors seen in various ExtensionService tests that use safe browsing; both those reverted as part of bug 233290, and the ones that are still live.
    BUG=233290
    R=jyasskin@chromium.org
    TBR=shess@chromium.org
    
    Review URL: https://codereview.chromium.org/60833007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233426 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_service_unittest.cc
+++ chrome/browser/extensions/extension_service_unittest.cc
@@ -3390,9 +3390,7 @@ TEST_F(ExtensionServiceTest, SetUnsetBlacklistInPrefs) {
 
 #if defined(ENABLE_BLACKLIST_TESTS)
 // Tests trying to install a blacklisted extension.
-// Disabled due to http://crbug.com/315396
-TEST_F(ExtensionServiceTest,
-       DISABLED_BlacklistedExtensionWillNotInstall) {
+TEST_F(ExtensionServiceTest, BlacklistedExtensionWillNotInstall) {
   scoped_refptr<FakeSafeBrowsingDatabaseManager> blacklist_db(
       new FakeSafeBrowsingDatabaseManager(true));
   Blacklist::ScopedDatabaseManagerForTest scoped_blacklist_db(blacklist_db);
@@ -3447,9 +3445,7 @@ TEST_F(ExtensionServiceTest, UnloadBlacklistedExtensionPolicy) {
 #if defined(ENABLE_BLACKLIST_TESTS)
 // Tests that a blacklisted extension is eventually unloaded on startup, if it
 // wasn't already.
-// Disabled due to http://crbug.com/315396
-TEST_F(ExtensionServiceTest,
-       DISABLED_WillNotLoadBlacklistedExtensionsFromDirectory) {
+TEST_F(ExtensionServiceTest, WillNotLoadBlacklistedExtensionsFromDirectory) {
   scoped_refptr<FakeSafeBrowsingDatabaseManager> blacklist_db(
       new FakeSafeBrowsingDatabaseManager(true));
   Blacklist::ScopedDatabaseManagerForTest scoped_blacklist_db(blacklist_db);
