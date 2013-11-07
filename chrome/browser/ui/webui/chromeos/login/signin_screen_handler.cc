6f96f9aea51d0f14a7f775bc0a18a55d8ed88dab
alemate@chromium.org <alemate@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-07 16:21:21 +0000

    3 new bootperf events:   login-start-signin-screen   login-wait-for-signin-state-initialize   login-send-user-list
    and a simple interface to add bootperf events alike not modifying session-manager.
    
    BUG=257026
    
    TEST=manual
    
    Review URL: https://codereview.chromium.org/41163003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233623 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chromeos/login/signin_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/signin_screen_handler.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_shutdown.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/chromeos/app_mode/kiosk_app_manager.h"
+#include "chrome/browser/chromeos/boot_times_loader.h"
 #include "chrome/browser/chromeos/input_method/input_method_util.h"
 #include "chrome/browser/chromeos/kiosk_mode/kiosk_mode_settings.h"
 #include "chrome/browser/chromeos/login/hwid_checker.h"
@@ -1298,6 +1299,10 @@ void SigninScreenHandler::HandleRemoveUser(const std::string& email) {
 }
 
 void SigninScreenHandler::HandleShowAddUser(const base::ListValue* args) {
+  TRACE_EVENT_ASYNC_STEP_INTO0("ui",
+                               "ShowLoginWebUI",
+                               LoginDisplayHostImpl::kShowLoginWebUIid,
+                               "ShowAddUser");
   email_.clear();
   // |args| can be null if it's OOBE.
   if (args)
@@ -1396,6 +1401,11 @@ void SigninScreenHandler::FillUserDictionary(User* user,
 void SigninScreenHandler::SendUserList(bool animated) {
   if (!delegate_)
     return;
+  TRACE_EVENT_ASYNC_STEP_INTO0("ui",
+                               "ShowLoginWebUI",
+                               LoginDisplayHostImpl::kShowLoginWebUIid,
+                               "SendUserList");
+  BootTimesLoader::Get()->RecordCurrentStats("login-send-user-list");
 
   size_t max_non_owner_users = kMaxUsers - 1;
   size_t non_owner_count = 0;
