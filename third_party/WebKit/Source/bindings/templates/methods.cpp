efcddd52ac180ebe02578269e78cf83103cfe14e
nbarth@chromium.org <nbarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-07 08:06:05 +0000

    IDL compiler: [PerWorldBindings] methods :: basic
    This is a first CL for [PerWorldBindings].
    This is one of the most complex extended attributes,
    so I'll break it up over a few CLs.
    
    Also added some {% set %} statements to the templates to replace
    inline {% if %}...{% else %}...{% endif %} blocks, which are illegible,
    but still keep the logic in the template.
    
    BUG=239771
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/63633003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161510 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/templates/methods.cpp
+++ third_party/WebKit/Source/bindings/templates/methods.cpp
@@ -1,8 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 {##############################################################################}
-{% macro generate_method(method) %}
+{% macro generate_method(method, world_suffix) %}
 {% filter conditional(method.conditional_string) %}
-static void {{method.name}}Method(const v8::FunctionCallbackInfo<v8::Value>& info)
+static void {{method.name}}Method{{world_suffix}}(const v8::FunctionCallbackInfo<v8::Value>& info)
 {
     {% if method.number_of_required_arguments %}
     if (UNLIKELY(info.Length() < {{method.number_of_required_arguments}})) {
@@ -115,7 +115,7 @@ if (state.hadException()) {
 {##############################################################################}
 {% macro method_callback(method, world_suffix) %}
 {% filter conditional(method.conditional_string) %}
-static void {{method.name}}MethodCallback(const v8::FunctionCallbackInfo<v8::Value>& info)
+static void {{method.name}}MethodCallback{{world_suffix}}(const v8::FunctionCallbackInfo<v8::Value>& info)
 {
     TRACE_EVENT_SET_SAMPLING_STATE("Blink", "DOMMethod");
     {% if method.measure_as %}
@@ -136,7 +136,7 @@ static void {{method.name}}MethodCallback(const v8::FunctionCallbackInfo<v8::Val
     {% if method.is_custom %}
     {{v8_class_name}}::{{method.name}}MethodCustom(info);
     {% else %}
-    {{cpp_class_name}}V8Internal::{{method.name}}Method(info);
+    {{cpp_class_name}}V8Internal::{{method.name}}Method{{world_suffix}}(info);
     {% endif %}
     TRACE_EVENT_SET_SAMPLING_STATE("V8", "Execution");
 }
