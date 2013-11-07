65a2c474eea9024d257bb2370c2f298994350090
piman@chromium.org <piman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-07 08:12:15 +0000

    Don't send the renderer flags that it doesn't use.
    BUG=314909
    
    Review URL: https://codereview.chromium.org/57463011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233542 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -903,7 +903,6 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kDisableFiltersOverIPC,
     switches::kDisableFullScreen,
     switches::kDisableGeolocation,
-    switches::kDisableGLMultisampling,
     switches::kDisableGpu,
     switches::kDisableGpuCompositing,
     switches::kDisableGpuVsync,
