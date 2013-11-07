2104be7aad091e7fd98799abd30ccd04f282b567
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-07 21:52:30 +0000

    [Sync] Unrevert r233630
    Unreverts dump/import functionality for chrome://sync-internals, with fix for android.
    
    Original review at https://codereview.chromium.org/60693002
    
    BUG=309112
    TBR=nyquist@chromium.org
    
    Review URL: https://codereview.chromium.org/63603003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233698 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/system_logs/chrome_internal_log_source.cc
+++ chrome/browser/chromeos/system_logs/chrome_internal_log_source.cc
@@ -55,7 +55,7 @@ void ChromeInternalLogSource::PopulateSyncLogs(SystemLogsResponse* response) {
   scoped_ptr<base::DictionaryValue> sync_logs(
       sync_ui_util::ConstructAboutInformation(service));
 
-  // Remove credentials section.
+  // Remove identity section.
   base::ListValue* details = NULL;
   sync_logs->GetList(kDetailsKey, &details);
   if (!details)
@@ -66,7 +66,7 @@ void ChromeInternalLogSource::PopulateSyncLogs(SystemLogsResponse* response) {
     if ((*it)->GetAsDictionary(&dict)) {
       std::string title;
       dict->GetString("title", &title);
-      if (title == kCredentialsTitle) {
+      if (title == kIdentityTitle) {
         details->Erase(it, NULL);
         break;
       }
