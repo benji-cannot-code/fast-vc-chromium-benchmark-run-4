0b6abf035ded499a04b9a27458b48295c6dff32c
asanka@chromium.org <asanka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-07 01:27:51 +0000

    Prefer opening PDF downloads in the browser.
    PDFs in particular are safer to open in the browser. This patch changes
    the handling of downloads to open such files in the browser by default
    instead of the system handler for the file type. A "Open with system
    handler" menu item will be available so that users can still use the
    system application if needed.
    
    The determination that a file is safer to handle in the browser is done
    as follows:
    
    a) DownloadTargetDeterminer determines whether the MIME type
       corresponding to the target filename of the download is one which is
       handled by the renderer or one that is handled by a sandboxed pepper
       plugin. If so, then the file is considered safely handled by the
       browser.
    
    b) ChromeDownloadManagerDelegate determines whether opening in the
       browser is preferred for the file type assuming the browser is able
       to handle it safely. Currently this is true for .pdf files.
    
    Opening behavior for a download will default to opening in the browser
    if both results from a) and b) are true.
    
    BUG=148492
    
    TEST=(1) Make sure Chrome PDF Viewer is enabled in chrome://plugins.
         (2) Download a .pdf file.
         (3) Download shelf context menu should show "Open" and "Open with
             system handler" options.
         (4) "Open" as well as clicking on the shelf icon and clicking on
             the download in chrome://downloads should all result in opening
             the file within the browser.
         (5) "Open with system handler" should open the .pdf with the
             application that's set up as the default handler for .pdf files
             on the users' machine.
    
    Review URL: https://codereview.chromium.org/55063002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233460 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_item_model.cc
+++ chrome/browser/download/download_item_model.cc
@@ -13,7 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/supports_user_data.h"
 #include "base/time/time.h"
+#include "chrome/browser/download/chrome_download_manager_delegate.h"
 #include "chrome/browser/download/download_crx_util.h"
+#include "chrome/browser/download/download_service.h"
+#include "chrome/browser/download/download_service_factory.h"
+#include "chrome/browser/download/download_stats.h"
 #include "chrome/browser/safe_browsing/download_feedback_service.h"
 #include "content/public/browser/download_danger_type.h"
 #include "content/public/browser/download_interrupt_reasons.h"
@@ -53,6 +57,13 @@ class DownloadItemModelData : public base::SupportsUserData::Data {
     should_notify_ui_ = should_notify_ui;
   }
 
+  bool should_prefer_opening_in_browser() const {
+    return should_prefer_opening_in_browser_;
+  }
+  void set_should_prefer_opening_in_browser(bool preference) {
+    should_prefer_opening_in_browser_ = preference;
+  }
+
  private:
   DownloadItemModelData();
   virtual ~DownloadItemModelData() {}
@@ -66,6 +77,10 @@ class DownloadItemModelData : public base::SupportsUserData::Data {
   // Whether the UI should be notified when the download is ready to be
   // presented.
   bool should_notify_ui_;
+
+  // Whether the download should be opened in the browser vs. the system handler
+  // for the file type.
+  bool should_prefer_opening_in_browser_;
 };
 
 // static
@@ -91,7 +106,8 @@ DownloadItemModelData* DownloadItemModelData::GetOrCreate(
 
 DownloadItemModelData::DownloadItemModelData()
     : should_show_in_shelf_(true),
-      should_notify_ui_(false) {
+      should_notify_ui_(false),
+      should_prefer_opening_in_browser_(false) {
 }
 
 string16 InterruptReasonStatusMessage(int reason) {
@@ -533,6 +549,16 @@ void DownloadItemModel::SetShouldNotifyUI(bool should_notify) {
   data->set_should_notify_ui(should_notify);
 }
 
+bool DownloadItemModel::ShouldPreferOpeningInBrowser() const {
+  const DownloadItemModelData* data = DownloadItemModelData::Get(download_);
+  return data && data->should_prefer_opening_in_browser();
+}
+
+void DownloadItemModel::SetShouldPreferOpeningInBrowser(bool preference) {
+  DownloadItemModelData* data = DownloadItemModelData::GetOrCreate(download_);
+  data->set_should_prefer_opening_in_browser(preference);
+}
+
 string16 DownloadItemModel::GetProgressSizesString() const {
   string16 size_ratio;
   int64 size = GetCompletedBytes();
@@ -605,4 +631,19 @@ string16 DownloadItemModel::GetInProgressStatusString() const {
 
   // Instead of displaying "0 B" we say "Starting..."
   return l10n_util::GetStringUTF16(IDS_DOWNLOAD_STATUS_STARTING);
+}
+
+void DownloadItemModel::OpenUsingPlatformHandler() {
+  DownloadService* download_service =
+      DownloadServiceFactory::GetForBrowserContext(
+          download_->GetBrowserContext());
+  if (!download_service)
+    return;
+
+  ChromeDownloadManagerDelegate* delegate =
+      download_service->GetDownloadManagerDelegate();
+  if (!delegate)
+    return;
+  delegate->OpenDownloadUsingPlatformHandler(download_);
+  RecordDownloadOpenMethod(DOWNLOAD_OPEN_METHOD_USER_PLATFORM);
 }
