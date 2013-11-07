2b0fd5ef64241300da780842248b08f39cc542ad
tengs@chromium.org <tengs@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-07 06:18:12 +0000

    Add flow to enable Google Drive offline mode automatically on first run.
    How this works is that we create a WebContents in the background to a Drive
    endpoint that will take care of all the offline initialization for the app.
    We succeed if a background page is opened; otherwise, we fail if the page does
    not load or after a timeout.
    
    BUG=307302
    TEST=manual
    
    Review URL: https://codereview.chromium.org/55423004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233501 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/login_display_host_impl.cc
+++ chrome/browser/chromeos/login/login_display_host_impl.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/accessibility/accessibility_manager.h"
 #include "chrome/browser/chromeos/app_mode/kiosk_app_manager.h"
 #include "chrome/browser/chromeos/customization_document.h"
+#include "chrome/browser/chromeos/first_run/drive_first_run_controller.h"
 #include "chrome/browser/chromeos/first_run/first_run_controller.h"
 #include "chrome/browser/chromeos/input_method/input_method_util.h"
 #include "chrome/browser/chromeos/kiosk_mode/kiosk_mode_settings.h"
@@ -318,6 +319,16 @@ LoginDisplayHostImpl::~LoginDisplayHostImpl() {
     // FirstRunController manages its lifetime and destructs after tutorial
     // completion.
     (new FirstRunController())->Start();
+  }
+
+  // TODO(tengs): This should be refactored together with the first run UI.
+  // See crbug.com/314934.
+  if (CommandLine::ForCurrentProcess()->HasSwitch(
+      switches::kEnableDriveOfflineFirstRun)) {
+    if (UserManager::Get()->IsCurrentUserNew()) {
+      // DriveOptInController will delete itself when finished.
+      (new DriveFirstRunController())->EnableOfflineMode();
+    }
   }
 }
 
--- chrome/chrome_browser_chromeos.gypi
+++ chrome/chrome_browser_chromeos.gypi
@@ -367,6 +367,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/chromeos/fileapi/file_system_backend.cc',
         'browser/chromeos/fileapi/file_system_backend.h',
         'browser/chromeos/fileapi/file_system_backend_delegate.h',
+        'browser/chromeos/first_run/drive_first_run_controller.cc',
+        'browser/chromeos/first_run/drive_first_run_controller.h',
         'browser/chromeos/first_run/first_run_controller.cc',
         'browser/chromeos/first_run/first_run_controller.h',
         'browser/chromeos/first_run/first_run_view.cc',
