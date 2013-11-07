2da7ddd4ed7556e78cc2463d3e78879334131d1a
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-07 11:38:32 +0000

    base: Remove call to g_get_home_dir() from GetHomeDir() for Chrome OS
    This is in preparation for removing a dependency to GLib.
    
    On a Chrome device, home directory is always set to /home/chronos/user
    as hard-coded in GetHomeDir().
    On a linux workstation for testing, referencing $HOME is sufficient.
    
    BUG=240617
    
    Review URL: https://codereview.chromium.org/60083009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233576 0039d316-1c4b-4281-b951-d872f2087c98

--- base/file_util_posix.cc
+++ base/file_util_posix.cc
@@ -25,8 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_MACOSX)
 #include <AvailabilityMacros.h>
 #include "base/mac/foundation_util.h"
-#elif !defined(OS_ANDROID)
-#include <glib.h>
+#elif !defined(OS_ANDROID) && !defined(OS_CHROMEOS)
+#include <glib.h>  // for g_get_home_dir()
 #endif
 
 #include <fstream>
@@ -779,7 +779,7 @@ FilePath GetHomeDir() {
 
 #if defined(OS_ANDROID)
   DLOG(WARNING) << "OS_ANDROID: Home directory lookup not yet implemented.";
-#else
+#elif !defined(OS_CHROMEOS)
   // g_get_home_dir calls getpwent, which can fall through to LDAP calls.
   base::ThreadRestrictions::AssertIOAllowed();
 
