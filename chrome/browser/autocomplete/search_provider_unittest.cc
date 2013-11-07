4b0c1c6ef79a8c5006bf82f48ddf71f4eb78b4de
mpearson@chromium.org <mpearson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-07 08:14:26 +0000

    Omnibox: Fix Inline Autocompletion of Navsuggest in Forced Query Mode
    This moves the block of code that adds the "?" from one place to another.
    Its original location was wrong because it incremented
    |inline_autocomplete_offset| before passing it to FormatUrl().
    This could change how FormatUrl() would correct inline_autocomplete_offset
    when its value is near the end of the formatted string.
    This is wrong.
    
    In both locations, in effect we are adding the "?" prefix after FormatUrl()
    runs.  This means we should correct the inline_autocomplete_offset value
    to compensate for the extra character "?" only after FormatUrl() runs.
    
    This fixes the bug.
    
    I also added a test that would've caught this issue.
    
    BUG=313451
    
    Review URL: https://codereview.chromium.org/62153002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233544 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/search_provider_unittest.cc
+++ chrome/browser/autocomplete/search_provider_unittest.cc
@@ -2363,6 +2363,8 @@ TEST_F(SearchProviderTest, NavigationInline) {
                                "?www.abc.com", "c.com", true },
     { "?ab",             "http://www.abc.com",
                                "?www.abc.com", "c.com", true },
+    { "?abc.com",        "http://www.abc.com",
+                               "?www.abc.com", "", true },
   };
 
   for (size_t i = 0; i < ARRAYSIZE_UNSAFE(cases); i++) {
