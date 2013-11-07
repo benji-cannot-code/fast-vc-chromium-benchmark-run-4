2b12567591eb72f27380ddaf3762e184fc9f5e3e
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-07 08:35:26 +0000

    WebUI: Serve mime type in Content-Type header
    BUG=315513
    
    Review URL: https://codereview.chromium.org/61783002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233552 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/browser/url_data_source.cc
+++ content/public/browser/url_data_source.cc
@@ -53,4 +53,8 @@ bool URLDataSource::ShouldServiceRequest(const net::URLRequest* request) const {
   return false;
 }
 
+bool URLDataSource::ShouldServeMimeTypeAsContentTypeHeader() const {
+  return false;
+}
+
 }  // namespace content
