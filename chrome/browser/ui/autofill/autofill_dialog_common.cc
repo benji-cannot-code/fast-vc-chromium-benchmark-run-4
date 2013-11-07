2d7e2e30a14f0a58665202d72784973a67298557
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-07 00:19:46 +0000

    rAc: fix validation logic when autofill completion sets country
    BUG=314842
    R=dbeam@chromium.org
    
    Review URL: https://codereview.chromium.org/58683002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233430 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/autofill/autofill_dialog_common.cc
+++ chrome/browser/ui/autofill/autofill_dialog_common.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/autofill/autofill_dialog_common.h"
 
+#include "chrome/browser/browser_process.h"
+#include "components/autofill/core/browser/autofill_country.h"
 #include "components/autofill/core/browser/autofill_field.h"
 #include "components/autofill/core/browser/autofill_type.h"
 #include "grit/chromium_strings.h"
@@ -175,6 +177,15 @@ AutofillMetrics::DialogUiEvent DialogSectionToUiSelectionChangedEvent(
 
   NOTREACHED();
   return AutofillMetrics::NUM_DIALOG_UI_EVENTS;
+}
+
+string16 GetHardcodedValueForType(ServerFieldType type) {
+  if (AutofillType(type).GetStorableType() == ADDRESS_HOME_COUNTRY) {
+    AutofillCountry country("US", g_browser_process->GetApplicationLocale());
+    return country.name();
+  }
+
+  return string16();
 }
 
 }  // namespace common
--- chrome/browser/ui/autofill/data_model_wrapper.cc
+++ chrome/browser/ui/autofill/data_model_wrapper.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/ui/autofill/autofill_dialog_common.h"
 #include "chrome/browser/ui/autofill/autofill_dialog_models.h"
 #include "components/autofill/content/browser/wallet/full_wallet.h"
 #include "components/autofill/content/browser/wallet/wallet_address.h"
@@ -28,7 +29,10 @@ DataModelWrapper::~DataModelWrapper() {}
 
 void DataModelWrapper::FillInputs(DetailInputs* inputs) {
   for (size_t i = 0; i < inputs->size(); ++i) {
-    (*inputs)[i].initial_value = GetInfo(AutofillType((*inputs)[i].type));
+    DetailInput* input = &(*inputs)[i];
+    input->initial_value = common::GetHardcodedValueForType(input->type);
+    if (input->initial_value.empty())
+      input->initial_value = GetInfo(AutofillType(input->type));
   }
 }
 
