7b382c6d6dcf221caa66c8005d2bff5872fe9539
mlamouri@chromium.org <mlamouri@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-07 00:50:42 +0000

    Nit: Using GetWidget() here is pretty confusing given that all methods around directly use window_ and GetWidget() would simply return window_.
    BUG=None
    
    Review URL: https://codereview.chromium.org/58883002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233443 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/apps/native_app_window_views.cc
+++ chrome/browser/ui/views/apps/native_app_window_views.cc
@@ -287,7 +287,7 @@ void NativeAppWindowViews::OnShortcutInfoLoaded(
 }
 
 HWND NativeAppWindowViews::GetNativeAppWindowHWND() const {
-  return views::HWNDForWidget(GetWidget()->GetTopLevelWidget());
+  return views::HWNDForWidget(window_->GetTopLevelWidget());
 }
 #endif
 
@@ -477,7 +477,7 @@ void NativeAppWindowViews::Restore() {
 }
 
 void NativeAppWindowViews::SetBounds(const gfx::Rect& bounds) {
-  GetWidget()->SetBounds(bounds);
+  window_->SetBounds(bounds);
 }
 
 void NativeAppWindowViews::FlashFrame(bool flash) {
