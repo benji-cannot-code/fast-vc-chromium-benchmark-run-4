2fe11c564d7b44863b17d978fa03f9a88afc732b
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-08 11:42:24 +0000

    don't launch chrome.exe when running in metro test binary
    In ash_unittests, TestMetroViewerProcessHost launches the metro viewer,
    and the metro viewer is expected to connect back to it. By default
    though, it launches chrome because it normally needs to do that to
    have something to connect to.
    
    Normally, ash_unittests creates the pipe, so chrome.exe's pipe creation
    fails, and then chrome.exe shuts down when the viewer shuts down. If,
    however, chrome takes longer to start, then the ash_unittests test
    may finish, try to kill chrome.exes, then chrome.exe starts up and
    creates the host pipe, then the next ash_unittest test starts up, and
    fails because it will fail to create the pipe.
    
    To avoid this race, pass an argument to the viewer (normally "open",
    now in tests "test_open") that inhibits it from starting chrome.exe
    
    R=ananta@chromium.org
    TBR=sky@chromium.org
    BUG=303478,316012
    
    Review URL: https://codereview.chromium.org/65623002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233872 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_process_platform_part_aurawin.cc
+++ chrome/browser/browser_process_platform_part_aurawin.cc
@@ -39,7 +39,8 @@ void BrowserProcessPlatformPart::PlatformSpecificCommandLineProcessing(
       if (launch) {
         CHECK(metro_viewer_process_host_->LaunchViewerAndWaitForConnection(
             command_line.GetSwitchValueNative(
-                switches::kViewerLaunchViaAppId)));
+                switches::kViewerLaunchViaAppId),
+            L""));
       }
     }
   }
