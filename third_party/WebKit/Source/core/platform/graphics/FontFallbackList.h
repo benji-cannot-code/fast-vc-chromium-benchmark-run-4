fe43b74ea059a0c16fc90071dea40c1f15a040f2
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-08 03:30:05 +0000

    Tighten up FontFallbackList's relatonship with Font.
    FontFallbackList is effectively owned by Font, so it's a little weird
    that Font queries its fallback list using itself. In fact, it actually
    means FontDescription all the time. We should fix that.
    
    Also -- move getFontData from FontCache to FontFallbackList. It is
    not a good final resting place, but better than the extra plumbing
    we had before.
    
    R=eae
    BUG=315783
    
    Review URL: https://codereview.chromium.org/63173007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161579 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/graphics/FontFallbackList.h
+++ third_party/WebKit/Source/core/platform/graphics/FontFallbackList.h
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-class Font;
 class GlyphPageTreeNode;
 class GraphicsContext;
 class IntRect;
@@ -71,8 +70,13 @@ public:
     ~FontFallbackList() { releaseFontData(); }
     void invalidate(PassRefPtr<FontSelector>);
 
-    bool isFixedPitch(const Font* f) const { if (m_pitch == UnknownPitch) determinePitch(f); return m_pitch == FixedPitch; };
-    void determinePitch(const Font*) const;
+    bool isFixedPitch(const FontDescription& fontDescription) const
+    {
+        if (m_pitch == UnknownPitch)
+            determinePitch(fontDescription);
+        return m_pitch == FixedPitch;
+    }
+    void determinePitch(const FontDescription&) const;
 
     bool loadingCustomFonts() const;
 
@@ -86,16 +90,18 @@ public:
 private:
     FontFallbackList();
 
-    const SimpleFontData* primarySimpleFontData(const Font* f)
+    const SimpleFontData* primarySimpleFontData(const FontDescription& fontDescription)
     {
         ASSERT(isMainThread());
         if (!m_cachedPrimarySimpleFontData)
-            m_cachedPrimarySimpleFontData = primaryFontData(f)->fontDataForCharacter(' ');
+            m_cachedPrimarySimpleFontData = primaryFontData(fontDescription)->fontDataForCharacter(' ');
         return m_cachedPrimarySimpleFontData;
     }
 
-    const FontData* primaryFontData(const Font*) const;
-    const FontData* fontDataAt(const Font*, unsigned index) const;
+    PassRefPtr<FontData> getFontData(const FontDescription&, int& familyIndex) const;
+
+    const FontData* primaryFontData(const FontDescription&) const;
+    const FontData* fontDataAt(const FontDescription&, unsigned index) const;
 
     void setPlatformFont(const FontPlatformData&);
 
--- third_party/WebKit/Source/core/platform/graphics/chromium/FontCacheChromiumWin.cpp
+++ third_party/WebKit/Source/core/platform/graphics/chromium/FontCacheChromiumWin.cpp
@@ -522,7 +522,7 @@ PassRefPtr<SimpleFontData> FontCache::getFontDataForCharacter(const Font& font, 
 
 }
 
-PassRefPtr<SimpleFontData> FontCache::getSimilarFontPlatformData(const Font& font)
+PassRefPtr<SimpleFontData> FontCache::getSimilarFontPlatformData(const FontDescription&)
 {
     return 0;
 }
