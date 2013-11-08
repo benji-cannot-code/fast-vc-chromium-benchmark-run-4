0ebf1e99a198fc11e2cdb8ad8a8b201cbe1c89d5
jbroman@chromium.org <jbroman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-08 13:27:08 +0000

    Move writeSelection out of WebCore::Pasteboard into Webcore::Editor.
    core/platform/ should not need to know this much about the DOM.
    This removes Pasteboard.cpp's dependency on core/editing/.
    
    BUG=276565
    
    Review URL: https://codereview.chromium.org/27110002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161618 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/Editor.cpp
+++ third_party/WebKit/Source/core/editing/Editor.cpp
@@ -406,6 +406,13 @@ void Editor::pasteWithPasteboard(Pasteboard* pasteboard)
         pasteAsFragment(fragment, canSmartReplaceWithPasteboard(pasteboard), chosePlainText);
 }
 
+void Editor::writeSelectionToPasteboard(Pasteboard* pasteboard, Range* selectedRange, const String& plainText)
+{
+    String html = createMarkup(selectedRange, 0, AnnotateForInterchange, false, ResolveNonLocalURLs);
+    KURL url = selectedRange->startContainer()->document().url();
+    pasteboard->writeHTML(html, url, plainText, canSmartCopyOrDelete());
+}
+
 // Returns whether caller should continue with "the default processing", which is the same as
 // the event handler NOT setting the return value to false
 bool Editor::dispatchCPPEvent(const AtomicString &eventType, ClipboardAccessPolicy policy, PasteMode pasteMode)
@@ -900,7 +907,7 @@ void Editor::cut()
             Pasteboard::generalPasteboard()->writePlainText(plainText,
                 canSmartCopyOrDelete() ? Pasteboard::CanSmartReplace : Pasteboard::CannotSmartReplace);
         } else {
-            Pasteboard::generalPasteboard()->writeSelection(selection.get(), canSmartCopyOrDelete(), plainText);
+            writeSelectionToPasteboard(Pasteboard::generalPasteboard(), selection.get(), plainText);
         }
         deleteSelectionWithSmartDelete(canSmartCopyOrDelete());
     }
@@ -920,7 +927,7 @@ void Editor::copy()
         if (HTMLImageElement* imageElement = imageElementFromImageDocument(document))
             Pasteboard::generalPasteboard()->writeImage(imageElement, document->url(), document->title());
         else
-            Pasteboard::generalPasteboard()->writeSelection(selectedRange().get(), canSmartCopyOrDelete(), m_frame.selectedTextForClipboard());
+            writeSelectionToPasteboard(Pasteboard::generalPasteboard(), selectedRange().get(), m_frame.selectedTextForClipboard());
     }
 }
 
