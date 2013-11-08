e7164501ba23308a33697bff868fb40671f08d8a
fischman@chromium.org <fischman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-08 01:06:51 +0000

    Unbreak android release/clang build by eliminating an unused variable warning.
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/64643004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161574 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/audio/chromium/FFTFrameOpenMAXDLAndroid.cpp
+++ third_party/WebKit/Source/platform/audio/chromium/FFTFrameOpenMAXDLAndroid.cpp
@@ -39,7 +39,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+#if !ASSERT_DISABLED
 const unsigned kMaxFFTPow2Size = 15;
+#endif
 
 // Normal constructor: allocates for a given fftSize.
 FFTFrame::FFTFrame(unsigned fftSize)
