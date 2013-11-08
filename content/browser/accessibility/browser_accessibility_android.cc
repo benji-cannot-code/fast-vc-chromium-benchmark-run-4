benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 8eeb1f8adab23f910f40c75c40a308ec9f7f33e2

This adds support for the InlineTextBox role in the AX tree, and its attributes.
Depends on this Blink change that adds InlineTextBox support.
https://codereview.chromium.org/23983002/

This change only adds support and makes minimal changes to
allow tests to pass. Other changes will take advantage of the
inline text boxes to compute boundsForRange, etc.

BUG=98977

Review URL: https://codereview.chromium.org/25943003

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232429 0039d316-1c4b-4281-b951-d872f2087c98

content/browser/accessibility/browser_accessibility_android.cc
@@ -25,7 +25,7 @@ bool BrowserAccessibilityAndroid::IsNative() const {
   return true;
 }
 
-bool BrowserAccessibilityAndroid::IsLeaf() const {
+bool BrowserAccessibilityAndroid::PlatformIsLeaf() const {
   if (child_count() == 0)
     return true;
 
@@ -53,7 +53,7 @@ bool BrowserAccessibilityAndroid::IsLeaf() const {
   if (HasOnlyStaticTextChildren())
     return true;
 
-  return false;
+  return BrowserAccessibility::PlatformIsLeaf();
 }
 
 bool BrowserAccessibilityAndroid::IsCheckable() const {
@@ -76,7 +76,7 @@ bool BrowserAccessibilityAndroid::IsChecked() const {
 }
 
 bool BrowserAccessibilityAndroid::IsClickable() const {
-  return (IsLeaf() && !GetText().empty());
+  return (PlatformIsLeaf() && !GetText().empty());
 }
 
 bool BrowserAccessibilityAndroid::IsEnabled() const {
@@ -186,8 +186,8 @@ string16 BrowserAccessibilityAndroid::GetText() const {
     text = base::UTF8ToUTF16(value());
 
   if (text.empty() && HasOnlyStaticTextChildren()) {
-    for (uint32 i = 0; i < child_count(); i++) {
-      BrowserAccessibility* child = GetChild(i);
+    for (uint32 i = 0; i < PlatformChildCount(); i++) {
+      BrowserAccessibility* child = PlatformGetChild(i);
       text += static_cast<BrowserAccessibilityAndroid*>(child)->GetText();
     }
   }
@@ -234,7 +234,7 @@ int BrowserAccessibilityAndroid::GetItemCount() const {
   switch(role()) {
     case WebKit::WebAXRoleList:
     case WebKit::WebAXRoleListBox:
-      count = child_count();
+      count = PlatformChildCount();
       break;
     case WebKit::WebAXRoleSlider:
     case WebKit::WebAXRoleProgressIndicator: {
@@ -340,8 +340,8 @@ int BrowserAccessibilityAndroid::GetEditableTextLength() const {
 }
 
 bool BrowserAccessibilityAndroid::HasFocusableChild() const {
-  for (uint32 i = 0; i < child_count(); i++) {
-    BrowserAccessibility* child = GetChild(i);
+  for (uint32 i = 0; i < PlatformChildCount(); i++) {
+    BrowserAccessibility* child = PlatformGetChild(i);
     if (child->HasState(WebKit::WebAXStateFocusable))
       return true;
     if (static_cast<BrowserAccessibilityAndroid*>(child)->HasFocusableChild())
@@ -351,8 +351,8 @@ bool BrowserAccessibilityAndroid::HasFocusableChild() const {
 }
 
 bool BrowserAccessibilityAndroid::HasOnlyStaticTextChildren() const {
-  for (uint32 i = 0; i < child_count(); i++) {
-    BrowserAccessibility* child = GetChild(i);
+  for (uint32 i = 0; i < PlatformChildCount(); i++) {
+    BrowserAccessibility* child = PlatformGetChild(i);
     if (child->role() != WebKit::WebAXRoleStaticText)
       return false;
   }

content/browser/accessibility/accessibility_tree_formatter.cc
@@ -67,10 +67,8 @@ void AccessibilityTreeFormatter::RecursiveBuildAccessibilityTree(
 
   base::ListValue* children = new base::ListValue;
   dict->Set(kChildrenDictAttr, children);
-  if (!IncludeChildren(node))
-    return;
 
-  for (size_t i = 0; i < node.children().size(); ++i) {
+  for (size_t i = 0; i < node.PlatformChildCount(); ++i) {
     BrowserAccessibility* child_node = node.children()[i];
     base::DictionaryValue* child_dict = new base::DictionaryValue;
     children->Append(child_dict);
@@ -93,13 +91,6 @@ void AccessibilityTreeFormatter::RecursiveFormatAccessibilityTree(
     RecursiveFormatAccessibilityTree(*child_dict, contents, depth + 1);
   }
 }
-
-#if !defined(OS_ANDROID)
-bool AccessibilityTreeFormatter::IncludeChildren(
-    const BrowserAccessibility& node) {
-  return true;
-}
-#endif
 
 #if (!defined(OS_WIN) && !defined(OS_MACOSX) && !defined(OS_ANDROID) && \
      !defined(TOOLKIT_GTK))

content/browser/accessibility/accessibility_tree_formatter.h
@@ -114,11 +114,6 @@ class CONTENT_EXPORT AccessibilityTreeFormatter {
   void AddProperties(const BrowserAccessibility& node,
                      base::DictionaryValue* dict);
 
-  // Returns true by default; can be overridden by the platform to
-  // prune some children from the tree when they wouldn't be exposed
-  // natively on that platform.
-  virtual bool IncludeChildren(const BrowserAccessibility& node);
-
   string16 FormatCoordinates(const char* name,
                              const char* x_name,
                              const char* y_name,

content/browser/accessibility/accessibility_tree_formatter_android.cc
@@ -78,13 +78,6 @@ void AccessibilityTreeFormatter::AddProperties(
   dict->SetInteger("item_count", android_node->GetItemCount());
 }
 
-bool AccessibilityTreeFormatter::IncludeChildren(
-    const BrowserAccessibility& node) {
-  const BrowserAccessibilityAndroid* android_node =
-      static_cast<const BrowserAccessibilityAndroid*>(&node);
-  return !android_node->IsLeaf();
-}
-
 string16 AccessibilityTreeFormatter::ToString(const DictionaryValue& dict,
                                               const string16& indent) {
   string16 line;

content/browser/accessibility/browser_accessibility.cc
@@ -44,6 +44,14 @@ BrowserAccessibility::BrowserAccessibility()
 BrowserAccessibility::~BrowserAccessibility() {
 }
 
+bool BrowserAccessibility::PlatformIsLeaf() const {
+  return role_ == WebKit::WebAXRoleStaticText || child_count() == 0;
+}
+
+uint32 BrowserAccessibility::PlatformChildCount() const {
+  return PlatformIsLeaf() ? 0 : children_.size();
+}
+
 void BrowserAccessibility::DetachTree(
     std::vector<BrowserAccessibility*>* nodes) {
   nodes->push_back(this);
@@ -113,7 +121,8 @@ bool BrowserAccessibility::IsDescendantOf(
   return false;
 }
 
-BrowserAccessibility* BrowserAccessibility::GetChild(uint32 child_index) const {
+BrowserAccessibility* BrowserAccessibility::PlatformGetChild(
+    uint32 child_index) const {
   DCHECK(child_index < children_.size());
   return children_[child_index];
 }

content/browser/accessibility/browser_accessibility.h
@@ -90,8 +90,21 @@ class CONTENT_EXPORT BrowserAccessibility {
   // Returns the number of children of this object.
   uint32 child_count() const { return children_.size(); }
 
-  // Return a pointer to the child with the given index.
-  BrowserAccessibility* GetChild(uint32 child_index) const;
+  // Returns true if this is a leaf node on this platform, meaning any
+  // children should not be exposed to this platform's native accessibility
+  // layer. Each platform subclass should implement this itself.
+  // The definition of a leaf may vary depending on the platform,
+  // but a leaf node should never have children that are focusable or
+  // that might send notifications.
+  virtual bool PlatformIsLeaf() const;
+
+  // Returns the number of children of this object, or 0 if PlatformIsLeaf()
+  // returns true.
+  uint32 PlatformChildCount() const;
+
+  // Return a pointer to the child at the given index, or NULL for an
+  // invalid index. Returns NULL if PlatformIsLeaf() returns true.
+  BrowserAccessibility* PlatformGetChild(uint32 child_index) const;
 
   // Return the previous sibling of this object, or NULL if it's the first
   // child of its parent.

content/browser/accessibility/browser_accessibility_android.h
@@ -17,7 +17,7 @@ class BrowserAccessibilityAndroid : public BrowserAccessibility {
   virtual void PostInitialize() OVERRIDE;
   virtual bool IsNative() const OVERRIDE;
 
-  bool IsLeaf() const;
+  virtual bool PlatformIsLeaf() const OVERRIDE;
 
   bool IsCheckable() const;
   bool IsChecked() const;

content/browser/accessibility/browser_accessibility_cocoa.mm
@@ -354,13 +354,12 @@ NSDictionary* attributeToMethodNameMap = nil;
 // accessibility children of this object.
 - (NSArray*)children {
   if (!children_) {
-    children_.reset([[NSMutableArray alloc]
-        initWithCapacity:browserAccessibility_->child_count()] );
-    for (uint32 index = 0;
-         index < browserAccessibility_->child_count();
-         ++index) {
+    uint32 childCount = browserAccessibility_->PlatformChildCount();
+    children_.reset([[NSMutableArray alloc] initWithCapacity:childCount]);
+    for (uint32 index = 0; index < childCount; ++index) {
       BrowserAccessibilityCocoa* child =
-          browserAccessibility_->GetChild(index)->ToBrowserAccessibilityCocoa();
+          browserAccessibility_->PlatformGetChild(index)->
+              ToBrowserAccessibilityCocoa();
       if ([child isIgnored])
         [children_ addObjectsFromArray:[child children]];
       else
@@ -1052,9 +1051,9 @@ NSDictionary* attributeToMethodNameMap = nil;
       return nil;
     }
     for (size_t i = 0;
-         i < browserAccessibility_->child_count();
+         i < browserAccessibility_->PlatformChildCount();
          ++i) {
-      BrowserAccessibility* child = browserAccessibility_->GetChild(i);
+      BrowserAccessibility* child = browserAccessibility_->PlatformGetChild(i);
       if (child->role() != WebKit::WebAXRoleRow)
         continue;
       int rowIndex;
@@ -1067,9 +1066,9 @@ NSDictionary* attributeToMethodNameMap = nil;
       if (rowIndex > row)
         break;
       for (size_t j = 0;
-           j < child->child_count();
+           j < child->PlatformChildCount();
            ++j) {
-        BrowserAccessibility* cell = child->GetChild(j);
+        BrowserAccessibility* cell = child->PlatformGetChild(j);
         if (cell->role() != WebKit::WebAXRoleCell)
           continue;
         int colIndex;

commit 06c03eaacca14d742a849e2c551dd601a38832f1

Fix infinite recursion in Android accessibility.
Bug introduced by r232429.

BUG=314812,314376

Review URL: https://codereview.chromium.org/63873002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233833 0039d316-1c4b-4281-b951-d872f2087c98

content/browser/accessibility/browser_accessibility_android.cc
@@ -340,8 +340,10 @@ int BrowserAccessibilityAndroid::GetEditableTextLength() const {
 }
 
 bool BrowserAccessibilityAndroid::HasFocusableChild() const {
-  for (uint32 i = 0; i < PlatformChildCount(); i++) {
-    BrowserAccessibility* child = PlatformGetChild(i);
+  // This is called from PlatformIsLeaf, so don't call PlatformChildCount
+  // from within this!
+  for (uint32 i = 0; i < child_count(); i++) {
+    BrowserAccessibility* child = children()[i];
     if (child->HasState(blink::WebAXStateFocusable))
       return true;
     if (static_cast<BrowserAccessibilityAndroid*>(child)->HasFocusableChild())