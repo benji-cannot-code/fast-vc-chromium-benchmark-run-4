e8207c5b730b0d464aa2c939d38da1337559c989
dmazzoni@chromium.org <dmazzoni@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-08 08:49:43 +0000

    Make focus ring layer more flexible.
    See bug for context and screenshots. Instead of drawing
    a scaled image into the layer, draw a faded outline
    directly around the focused object in a layer that's
    on top of all other widgets.
    
    BUG=314889
    
    Review URL: https://codereview.chromium.org/57513005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233829 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/ui/focus_ring_layer.h
+++ chrome/browser/chromeos/ui/focus_ring_layer.h
@@ -9,13 +9,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/memory/scoped_ptr.h"
 #include "ui/compositor/layer_delegate.h"
+#include "ui/gfx/rect.h"
+
+namespace aura {
+class Window;
+}
 
 namespace ui {
 class Layer;
 }
 
 namespace views {
-class Painter;
 class View;
 }
 
@@ -27,6 +31,9 @@ class FocusRingLayer : public ui::LayerDelegate {
   FocusRingLayer();
   virtual ~FocusRingLayer();
 
+  // Create the layer and update its bounds and position in the hierarchy.
+  void Update();
+
   // Updates the focus ring layer for the view or clears it if |view| is NULL.
   void SetForView(views::View* view);
 
@@ -36,8 +43,17 @@ class FocusRingLayer : public ui::LayerDelegate {
   virtual void OnDeviceScaleFactorChanged(float device_scale_factor) OVERRIDE;
   virtual base::Closure PrepareForLayerBoundsChange() OVERRIDE;
 
+  // The window containing focus.
+  aura::Window* window_;
+
+  // The current root window containing the focused object.
+  aura::Window* root_window_;
+
+  // The bounding rectangle of the focused object, in |window_| coordinates.
+  gfx::Rect focus_ring_;
+
+  // The current layer.
   scoped_ptr<ui::Layer> layer_;
-  scoped_ptr<views::Painter> ring_painter_;
 
   DISALLOW_COPY_AND_ASSIGN(FocusRingLayer);
 };
