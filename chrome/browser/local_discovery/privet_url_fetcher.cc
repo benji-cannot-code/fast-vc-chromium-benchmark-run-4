bc0ec6fb6fb44e43693c42f93ba3f7ccbdfd1b4c
noamsml@chromium.org <noamsml@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-08 07:43:32 +0000

    Retry on low-level connection errors and on HTTP 503 errors in PrivetURLFetcher
    To avoid connection reset issues on long file upload, retry on low-level
    connection errors and HTTP 503 errors in PrivetURLFetcher.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/62753004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233816 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/local_discovery/privet_url_fetcher.cc
+++ chrome/browser/local_discovery/privet_url_fetcher.cc
@@ -20,6 +20,7 @@ namespace {
 const char kXPrivetTokenHeaderPrefix[] = "X-Privet-Token: ";
 const char kXPrivetEmptyToken[] = "\"\"";
 const int kPrivetMaxRetries = 20;
+const int kPrivetTimeoutOnError = 5;
 }
 
 void PrivetURLFetcher::Delegate::OnNeedPrivetToken(
@@ -91,8 +92,9 @@ void PrivetURLFetcher::SetUploadData(const std::string& upload_content_type,
 }
 
 void PrivetURLFetcher::OnURLFetchComplete(const net::URLFetcher* source) {
-  if (source->GetStatus().status() != net::URLRequestStatus::SUCCESS) {
-    delegate_->OnError(this, URL_FETCH_ERROR);
+  if (source->GetStatus().status() != net::URLRequestStatus::SUCCESS ||
+      source->GetResponseCode() == net::HTTP_SERVICE_UNAVAILABLE) {
+    ScheduleRetry(kPrivetTimeoutOnError);
     return;
   }
 
@@ -179,7 +181,7 @@ void PrivetURLFetcher::RefreshToken(const std::string& token) {
 
 bool PrivetURLFetcher::PrivetErrorTransient(const std::string& error) {
   return (error == kPrivetErrorDeviceBusy) ||
-         (error == kPrivetErrorPendingUserAction);
+      (error == kPrivetErrorPendingUserAction);
 }
 
 PrivetURLFetcherFactory::PrivetURLFetcherFactory(
