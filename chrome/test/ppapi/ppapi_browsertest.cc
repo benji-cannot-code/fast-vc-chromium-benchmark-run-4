fda501aa492c6f575d58f857d19f918c71842627
scheib@chromium.org <scheib@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-08 23:26:31 +0000

    Disabled ppapi browsertests for TCPSocket due to flakiness.
    BUG=316933
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@234034 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/ppapi/ppapi_browsertest.cc
+++ chrome/test/ppapi/ppapi_browsertest.cc
@@ -327,8 +327,8 @@ TEST_PPAPI_OUT_OF_PROCESS(MAYBE_OUT_BrowserFont)
 TEST_PPAPI_IN_PROCESS(Buffer)
 TEST_PPAPI_OUT_OF_PROCESS(Buffer)
 
-// TCPSocket tests.
-IN_PROC_BROWSER_TEST_F(OutOfProcessPPAPITest, TCPSocket) {
+// TCPSocket tests.  Disabled due to flakiness: http://crbug.com/316933.
+IN_PROC_BROWSER_TEST_F(OutOfProcessPPAPITest, DISABLED_TCPSocket) {
   RunTestViaHTTP(
       LIST_TEST(TCPSocket_Connect)
       LIST_TEST(TCPSocket_ReadWrite)
@@ -338,7 +338,7 @@ IN_PROC_BROWSER_TEST_F(OutOfProcessPPAPITest, TCPSocket) {
       LIST_TEST(TCPSocket_Interface_1_0)
   );
 }
-IN_PROC_BROWSER_TEST_F(PPAPINaClNewlibTest, TCPSocket) {
+IN_PROC_BROWSER_TEST_F(PPAPINaClNewlibTest, DISABLED_TCPSocket) {
   RunTestViaHTTP(
       LIST_TEST(TCPSocket_Connect)
       LIST_TEST(TCPSocket_ReadWrite)
@@ -348,7 +348,7 @@ IN_PROC_BROWSER_TEST_F(PPAPINaClNewlibTest, TCPSocket) {
       LIST_TEST(TCPSocket_Interface_1_0)
   );
 }
-IN_PROC_BROWSER_TEST_F(PPAPINaClGLibcTest, MAYBE_GLIBC(TCPSocket)) {
+IN_PROC_BROWSER_TEST_F(PPAPINaClGLibcTest, MAYBE_GLIBC(DISABLED_TCPSocket)) {
   RunTestViaHTTP(
       LIST_TEST(TCPSocket_Connect)
       LIST_TEST(TCPSocket_ReadWrite)
@@ -358,7 +358,7 @@ IN_PROC_BROWSER_TEST_F(PPAPINaClGLibcTest, MAYBE_GLIBC(TCPSocket)) {
       LIST_TEST(TCPSocket_Interface_1_0)
   );
 }
-IN_PROC_BROWSER_TEST_F(PPAPINaClPNaClTest, TCPSocket) {
+IN_PROC_BROWSER_TEST_F(PPAPINaClPNaClTest, DISABLED_TCPSocket) {
   RunTestViaHTTP(
       LIST_TEST(TCPSocket_Connect)
       LIST_TEST(TCPSocket_ReadWrite)
