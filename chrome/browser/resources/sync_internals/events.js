3351a7e9198ad83e9d95dd5f10c4ea123006eb64
lipalani@chromium.org <lipalani@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-08 06:14:29 +0000

    Use jstemplate to construct the events page in about:sync
    BUG=316305
    
    Review URL: https://codereview.chromium.org/64383003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233798 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/sync_internals/events.js
+++ chrome/browser/resources/sync_internals/events.js
@@ -5,54 +5,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 (function() {
 'use strict';
-var toggleDisplay = function() {
-  var detailsNode = this.parentNode.getElementsByClassName('details')[0];
-  if (detailsNode.classList.contains('hidden')) {
-    detailsNode.classList.remove('hidden');
+var toggleDisplay = function(event) {
+  var originatingButton = event.target;
+  var detailsNode = originatingButton.parentNode.getElementsByClassName(
+      'details')[0];
+
+  if (detailsNode.getAttribute('hidden') != null) {
+    detailsNode.removeAttribute('hidden');
   } else {
-    detailsNode.classList.add('hidden');
+    detailsNode.setAttribute('hidden', 'hidden');
   }
-}
-
-var makeLogEntryNode = function(entry) {
-  var timeNode = document.createElement('td');
-  timeNode.textContent = entry.date;
-
-  var submoduleNode = document.createElement('td');
-  submoduleNode.textContent = entry.submodule;
-
-  var eventNode = document.createElement('td');
-  eventNode.textContent = entry.event;
-
-  var details = document.createElement('pre');
-  details.textContent = JSON.stringify(entry.details, null, 2);
-  details.className = 'details';
-  details.classList.add('details');
-  details.classList.add('hidden');
-  var detailsToggleButton = document.createElement('button');
-  detailsToggleButton.addEventListener('click', toggleDisplay, false);
-  detailsToggleButton.textContent = 'Show/Hide Details';
-  var detailsNode = document.createElement('td');
-  detailsNode.appendChild(detailsToggleButton);
-  detailsNode.appendChild(details);
-
-  var node = document.createElement('tr');
-  node.appendChild(timeNode);
-  node.appendChild(submoduleNode);
-  node.appendChild(eventNode);
-  node.appendChild(detailsNode);
-
-  return node;
 }
 
 var syncEvents = $('sync-events');
 
 var entries = chrome.sync.log.entries;
-for (var i = 0; i < entries.length; ++i) {
-  syncEvents.appendChild(makeLogEntryNode(entries[i]));
+var displaySyncEvents = function() {
+  var eventTemplateContext = {
+    eventList: entries,
+  };
+  var context = new JsEvalContext(eventTemplateContext);
+  jstProcess(context, syncEvents);
 }
 
+syncEvents.addEventListener('click', toggleDisplay);
 chrome.sync.log.addEventListener('append', function(event) {
-  syncEvents.appendChild(makeLogEntryNode(event.detail));
+  displaySyncEvents();
 });
 })();
