d432cf00d873ba0d10b3536ba93a04cc0b463084
creis@chromium.org <creis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-08 08:26:55 +0000

    Give FrameTreeNodes a browser-global ID for use in navigation.
    This is the first step in removing renderer-specific frame IDs.
    
    BUG=314791
    TEST=No visible change.
    
    Review URL: https://codereview.chromium.org/63523004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233827 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/frame_host/frame_tree_node.cc
+++ content/browser/frame_host/frame_tree_node.cc
@@ -13,10 +13,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 const int64 FrameTreeNode::kInvalidFrameId = -1;
+int64 FrameTreeNode::next_frame_tree_node_id_ = 1;
 
-FrameTreeNode::FrameTreeNode(int64 frame_id, const std::string& name,
+FrameTreeNode::FrameTreeNode(int64 frame_id,
+                             const std::string& name,
                              scoped_ptr<RenderFrameHostImpl> render_frame_host)
-  : frame_id_(frame_id),
+  : frame_tree_node_id_(next_frame_tree_node_id_++),
+    frame_id_(frame_id),
     frame_name_(name),
     owns_render_frame_host_(true),
     render_frame_host_(render_frame_host.release()) {
@@ -31,11 +34,11 @@ void FrameTreeNode::AddChild(scoped_ptr<FrameTreeNode> child) {
   children_.push_back(child.release());
 }
 
-void FrameTreeNode::RemoveChild(int64 child_id) {
+void FrameTreeNode::RemoveChild(FrameTreeNode* child) {
   std::vector<FrameTreeNode*>::iterator iter;
 
   for (iter = children_.begin(); iter != children_.end(); ++iter) {
-    if ((*iter)->frame_id() == child_id)
+    if ((*iter) == child)
       break;
   }
 
