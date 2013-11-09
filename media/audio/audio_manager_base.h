20081df667d5db1791dbcb37720774fdc4494834
dalecurtis@chromium.org <dalecurtis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-09 05:04:54 +0000

    Remove unused lock from AudioManagerBase.
    It was a dance without an audience.  I've also made a small
    cleanup to ShutdownOnAudioThread().
    
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/67443002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@234107 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/audio_manager_base.h
+++ media/audio/audio_manager_base.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/scoped_vector.h"
 #include "base/observer_list.h"
-#include "base/synchronization/lock.h"
+#include "base/threading/thread.h"
 #include "media/audio/audio_manager.h"
 
 #include "media/audio/audio_output_dispatcher.h"
@@ -21,10 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_WIN)
 #include "base/win/scoped_com_initializer.h"
 #endif
-
-namespace base {
-class Thread;
-}
 
 namespace media {
 
@@ -185,8 +181,7 @@ class MEDIA_EXPORT AudioManagerBase : public AudioManager {
   ObserverList<AudioDeviceListener> output_listeners_;
 
   // Thread used to interact with audio streams created by this audio manager.
-  scoped_ptr<base::Thread> audio_thread_;
-  mutable base::Lock audio_thread_lock_;
+  base::Thread audio_thread_;
 
   // The message loop of the audio thread this object runs on. Used for internal
   // tasks which run on the audio thread even after Shutdown() has been started
