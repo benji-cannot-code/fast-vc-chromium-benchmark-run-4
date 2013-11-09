0979bbd6ce7e84c69fbf09803272ba0009526da0
pdr@chromium.org <pdr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-09 02:23:35 +0000

    Revert r161357: phpBB forum post autosizer.
    This patch reverts r161357 which autosized forum posts based on an
    explicit check for phpBB. In place of this check, we are working on
    three parallel efforts to fix this problem:
    1) A more general solution to autosizing forum posts
    2) A new autosizing implementation more closely matching Gecko
    3) Developer outreach.
    
    For more information, see the following blink-dev discussion:
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/gX9DE3SAohI
    
    BUG=
    
    Review URL: https://codereview.chromium.org/67253005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161673 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/TextAutosizer.h
+++ third_party/WebKit/Source/core/rendering/TextAutosizer.h
@@ -45,13 +45,6 @@ class TextAutosizer FINAL {
     WTF_MAKE_NONCOPYABLE(TextAutosizer);
 
 public:
-    enum ContentType {
-        Unknown,
-        Default,
-        VBulletin,
-        PhpBB
-    };
-
     static PassOwnPtr<TextAutosizer> create(Document* document) { return adoptPtr(new TextAutosizer(document)); }
 
     bool processSubtree(RenderObject* layoutRoot);
@@ -63,6 +56,12 @@ private:
     enum TraversalDirection {
         FirstToLast,
         LastToFirst
+    };
+
+    enum ContentType {
+        Unknown,
+        Default,
+        VBulletin
     };
 
     explicit TextAutosizer(Document*);
@@ -80,6 +79,8 @@ private:
 
     void setMultiplier(RenderObject*, float);
     void setMultiplierForList(RenderObject* renderer, float multiplier);
+
+    ContentType detectContentType();
 
     static bool isAutosizingContainer(const RenderObject*);
     static bool isNarrowDescendant(const RenderBlock*, TextAutosizingClusterInfo& parentClusterInfo);
