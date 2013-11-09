72c8ecfd54ab98041cbc27b61ab0b680e304247a
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-09 19:25:41 +0000

    linux_aura: Turn aura on on the main linux builders.
    This is essentially a revert of r227214. In the interim, we've enabled interactive_ui_tests, switched to openbox on the bots to theoretically fix browser_tests, and have a speculative nacl integration crash fix.
    
    BUG=125106,303342,304554,125106,304555,316716,316919
    
    Review URL: https://codereview.chromium.org/26622003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@234139 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_tabs_apitest.cc
+++ chrome/browser/extensions/extension_tabs_apitest.cc
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // defined(OS_LINUX) && !defined(USE_AURA)
 
 // http://crbug.com/145639
-#if defined(OS_CHROMEOS) || defined(OS_WIN)
+#if defined(OS_LINUX) || defined(OS_WIN)
 #define MAYBE_TabEvents DISABLED_TabEvents
 #else
 #define MAYBE_TabEvents TabEvents
