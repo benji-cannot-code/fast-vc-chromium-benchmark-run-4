9475f0c1b391d9aad9e19046e6bc182092a4a420
frankf@chromium.org <frankf@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-09 00:32:12 +0000

    Revert 234046 "Revert 233774 "Make WindowAndroid constructor tak..."
    There are other dependant CLs upstream/downstream. Will wait
    for actual fix.
    
    > Revert 233774 "Make WindowAndroid constructor takes context as p..."
    > 
    > It broke content_browsertests
    > 
    > > Make WindowAndroid constructor takes context as param.
    > > 
    > > BUG=309104
    > > TBR=yfriedman
    > > 
    > > Review URL: https://codereview.chromium.org/29303004
    > 
    > TBR=michaelbai@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/66503005
    
    TBR=frankf@chromium.org
    
    Review URL: https://codereview.chromium.org/66603006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@234047 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/android/shell_apk/src/org/chromium/content_shell_apk/ContentShellActivity.java
+++ content/shell/android/shell_apk/src/org/chromium/content_shell_apk/ContentShellActivity.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.content_shell_apk;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -26,6 +25,7 @@ import org.chromium.content.common.CommandLine;
 import org.chromium.content.common.ProcessInitException;
 import org.chromium.content_shell.Shell;
 import org.chromium.content_shell.ShellManager;
+import org.chromium.ui.ActivityWindowAndroid;
 import org.chromium.ui.WindowAndroid;
 
 /**
@@ -67,7 +67,7 @@ public class ContentShellActivity extends Activity {
 
         setContentView(R.layout.content_shell_activity);
         mShellManager = (ShellManager) findViewById(R.id.shell_container);
-        mWindowAndroid = new WindowAndroid(this);
+        mWindowAndroid = new ActivityWindowAndroid(this);
         mWindowAndroid.restoreInstanceState(savedInstanceState);
         mShellManager.setWindow(mWindowAndroid);
 
