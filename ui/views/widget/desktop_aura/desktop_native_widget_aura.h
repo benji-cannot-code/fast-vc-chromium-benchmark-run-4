372df5835bfc0281b3f3c3307d20a42a3c97e3e2
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-10 23:59:18 +0000

    PreTarget accelerator handler should only be for one FocusManager
    Previously, the PreTargetHandler would send the key event to the
    FocusManager associated with the widget at creation time. When
    there's a transient open, the events should only be sent to the
    FocusManager associated with that transient, not to the main
    one.
    
    BUG=304754, 305082, 316064
    TEST=with --disable-dwm-composition, tab, f11, ctrl-f in main page, dialogs, bubbles.
    R=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/62713004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@234187 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/desktop_aura/desktop_native_widget_aura.h
+++ ui/views/widget/desktop_aura/desktop_native_widget_aura.h
@@ -44,6 +44,7 @@ class DesktopDispatcherClient;
 class DesktopEventClient;
 class DesktopRootWindowHost;
 class DropHelper;
+class FocusManagerEventHandler;
 class TooltipManagerAura;
 class WindowReorderer;
 
@@ -225,6 +226,8 @@ class VIEWS_EXPORT DesktopNativeWidgetAura
                                      const gfx::Point& new_origin) OVERRIDE;
 
  private:
+  friend class FocusManagerEventHandler;
+
   // Installs the input method filter.
   void InstallInputMethodEventFilter();
 
@@ -268,6 +271,7 @@ class VIEWS_EXPORT DesktopNativeWidgetAura
   scoped_ptr<aura::client::DragDropClient> drag_drop_client_;
   scoped_ptr<aura::client::WindowTreeClient> window_tree_client_;
   scoped_ptr<DesktopEventClient> event_client_;
+  scoped_ptr<FocusManagerEventHandler> focus_manager_event_handler_;
 
   // Toplevel event filter which dispatches to other event filters.
   corewm::CompoundEventFilter* root_window_event_filter_;
