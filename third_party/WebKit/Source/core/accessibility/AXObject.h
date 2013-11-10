401b4fe0ee7f5b0504fe5936a48314c2b2889618
inferno@chromium.org <inferno@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-10 02:54:20 +0000

    Revert 161595 "Update the accessibility tree when a modal dialog..."
    Causing use-after-frees on ClusterFuzz.
    
    > Update the accessibility tree when a modal dialog is opened/closed.
    > 
    > When a node's inertness changes, we must notify the AX tree since
    > the node may need to be added or removed from the tree.
    > 
    > This patch does the following.
    > 1) When an modal dialog opens/closes, essentially we traverse the AX
    > tree and call notifyIfIgnoredValueChanged on each object. However, once
    > an object is found whose ignored value changed, it's not necessary to
    > descend into its subtree by virtue of the childrenCleared/addChild
    > mechanism.
    > 
    > An earlier attempt called notifyIfIgnoredValueChanged only on immediate
    > children of the HTMLBodyElement. This is insufficient because siblings
    > of <dialog> are roots of newly (or formerly) inert subtrees, and some
    > objects are not descendants of body due to renderer reparenting (e.g.
    > modal dialogs).
    > 
    > 2) Modifies AXObject::isInertOrAriaHidden to climb the AX tree until an
    > object with non-null node() is found, when evaluating inertness. This is
    > necessary because some objects such as one created with
    > MenuListPopupRole don't have a node, but should be considered inert.
    > 
    > The test for this patch is Chrome-side:
    > https://codereview.chromium.org/64273003
    > 
    > BUG=304779
    > 
    > Review URL: https://codereview.chromium.org/59663005
    
    TBR=falken@chromium.org
    
    Review URL: https://codereview.chromium.org/68003002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161694 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/accessibility/AXObject.h
+++ third_party/WebKit/Source/core/accessibility/AXObject.h
@@ -538,7 +538,7 @@ public:
     virtual void childrenChanged() { }
     virtual void handleActiveDescendantChanged() { }
     virtual void handleAriaExpandedChanged() { }
-    bool notifyIfIgnoredValueChanged();
+    void notifyIfIgnoredValueChanged();
     virtual void selectionChanged();
     virtual void textChanged() { }
     virtual void updateAccessibilityRole() { }
--- third_party/WebKit/Source/core/html/HTMLDialogElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLDialogElement.cpp
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/html/HTMLDialogElement.h"
 
 #include "bindings/v8/ExceptionState.h"
-#include "core/accessibility/AXObjectCache.h"
 #include "core/dom/ExceptionCode.h"
 #include "core/dom/NodeTraversal.h"
 #include "core/html/HTMLFormControlElement.h"
@@ -57,12 +56,6 @@ static void runAutofocus(HTMLDialogElement* dialog)
         else
             next = NodeTraversal::next(node, dialog);
     }
-}
-
-static void inertSubtreesChanged(Document& document)
-{
-    if (AXObjectCache* cache = document.existingAXObjectCache())
-        cache->handleInertSubtreesChanged();
 }
 
 HTMLDialogElement::HTMLDialogElement(const QualifiedName& tagName, Document& document)
@@ -94,11 +87,7 @@ void HTMLDialogElement::closeDialog(const String& returnValue)
     if (!fastHasAttribute(openAttr))
         return;
     setBooleanAttribute(openAttr, false);
-
-    HTMLDialogElement* activeModalDialog = document().activeModalDialog();
     document().removeFromTopLayer(this);
-    if (activeModalDialog == this)
-        inertSubtreesChanged(document());
 
     if (!returnValue.isNull())
         m_returnValue = returnValue;
@@ -129,7 +118,6 @@ void HTMLDialogElement::showModal(ExceptionState& es)
         return;
     }
     document().addToTopLayer(this);
-    inertSubtreesChanged(document());
     setBooleanAttribute(openAttr, true);
 
     runAutofocus(this);
