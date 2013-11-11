f0fc179a0ffced2bfa03a1c195d9a73e776440d9
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-11 16:32:10 +0000

    DevTools: disable highlight in inspected page while recording is in progress
    ... so that we minimize noise in the recording, as highlight causes compositing.
    
    BUG=317357
    R=vsevik@chromium.org
    
    Review URL: https://codereview.chromium.org/68133003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161730 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/TimelinePanel.js
+++ third_party/WebKit/Source/devtools/front_end/TimelinePanel.js
@@ -739,6 +739,11 @@ WebInspector.TimelinePanel.prototype = {
         this.toggleTimelineButton.toggled = true;
     },
 
+    _recordingInProgress: function()
+    {
+        return this.toggleTimelineButton.toggled;
+    },
+
     _onRecordingStopped: function()
     {
         this.toggleTimelineButton.title = WebInspector.UIString("Record");
@@ -1141,7 +1146,7 @@ WebInspector.TimelinePanel.prototype = {
     {
         var anchor = this._getPopoverAnchor(e.target);
 
-        if (anchor && anchor.row && anchor.row._record.highlightQuad)
+        if (anchor && anchor.row && anchor.row._record.highlightQuad && !this._recordingInProgress())
             this._highlightQuad(anchor.row._record.highlightQuad);
         else
             this._hideQuadHighlight();
