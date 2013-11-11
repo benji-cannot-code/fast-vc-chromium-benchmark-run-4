d991e395da116d943014668136a41b5ac61df726
rjwright@chromium.org <rjwright@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-11 08:00:30 +0000

    Move Web Animations methods out of StyleResolver.
    Move the methods styleForKeyframe, matchScopedKeyframesRule,
    GenerateTimingFunction, and resolveKeyframes out of StyleResolver
    and into CSSAnimations. Expose methods and types in StyleResolver
    as needed by the transplanted methods.
    
    Change is made in preparation for adding use-counter analytics to
    Web Animations, to avoid adding more WA-specific code to the Style
    Resolver.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/57593002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161706 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/css/CSSAnimations.h
+++ third_party/WebKit/Source/core/animation/css/CSSAnimations.h
@@ -49,6 +49,7 @@ struct CandidateTransition;
 class Element;
 class StylePropertyShorthand;
 class StyleResolver;
+class StyleRuleKeyframes;
 
 // Applied to scopes where an animation update will be added as pending and should then be applied (eg. Element style recalc).
 class CSSAnimationUpdateScope FINAL {
@@ -149,6 +150,11 @@ private:
 
 class CSSAnimations FINAL {
 public:
+    // FIXME: This method is only used here and in the legacy animations
+    // implementation. It should be made private or file-scope when the legacy
+    // engine is removed.
+    static const StyleRuleKeyframes* matchScopedKeyframesRule(StyleResolver*, const Element*, const StringImpl*);
+
     static bool isAnimatableProperty(CSSPropertyID);
     static const StylePropertyShorthand& animatableProperties();
     // FIXME: This should take a const ScopedStyleTree instead of a StyleResolver.
