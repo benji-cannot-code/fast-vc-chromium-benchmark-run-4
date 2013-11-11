de3bbb652cfdb76518bde0eb5813c3d4b151bb78
avi@chromium.org <avi@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-11 00:21:13 +0000

    Classes used outside their components need to be exported.
    This fixes the Mac 64 component build after r161693.
    
    BUG=297477
    
    Review URL: https://codereview.chromium.org/68223002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161701 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/mac/WebFontCache.h
+++ third_party/WebKit/Source/platform/mac/WebFontCache.h
@@ -25,11 +25,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#import <platform/PlatformExport.h>
 #import <AppKit/NSFontManager.h>
 #import <wtf/Vector.h>
 
 // This interface exists so that third party products (like Silk) can patch in to an Obj-C method to manipulate WebKit's font caching/substitution.
-@interface WebFontCache : NSObject
+PLATFORM_EXPORT @interface WebFontCache : NSObject
 + (NSFont *)fontWithFamily:(NSString *)desiredFamily traits:(NSFontTraitMask)desiredTraits weight:(int)desiredWeight size:(float)size;
 + (void)getTraits:(Vector<unsigned>&)traitsMasks inFamily:(NSString *)desiredFamily;
 
