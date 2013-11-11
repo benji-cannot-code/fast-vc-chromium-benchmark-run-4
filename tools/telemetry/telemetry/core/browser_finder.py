5c430c7979d6707bb98b0158399afed1dc3f04f8
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-11 23:28:49 +0000

    [Telemetry] Shortcut browser finding when an explicit --browser is given.
    Browser finding on cros and android takes a little bit of time. If we are
    given a --browser, there's no need to do it.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/67953003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@234314 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/browser_finder.py
+++ tools/telemetry/telemetry/core/browser_finder.py
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """Finds browsers that can be controlled by telemetry."""
 
 import logging
+import operator
 
 from telemetry.core.backends.webdriver import webdriver_desktop_browser_finder
 from telemetry.core.backends.chrome import android_browser_finder
@@ -19,7 +20,8 @@ BROWSER_FINDERS = [
   webdriver_desktop_browser_finder,
   ]
 
-ALL_BROWSER_TYPES = ','.join([bf.ALL_BROWSER_TYPES for bf in BROWSER_FINDERS])
+ALL_BROWSER_TYPES = reduce(operator.add,
+                           [bf.ALL_BROWSER_TYPES for bf in BROWSER_FINDERS])
 
 
 class BrowserTypeRequiredException(Exception):
@@ -61,6 +63,9 @@ def FindBrowser(options):
   browsers = []
   default_browsers = []
   for finder in BROWSER_FINDERS:
+    if (options.browser_type and options.browser_type != 'any' and
+        options.browser_type not in finder.ALL_BROWSER_TYPES):
+      continue
     curr_browsers = finder.FindAllAvailableBrowsers(options)
     new_default_browser = finder.SelectDefaultBrowser(curr_browsers)
     if new_default_browser:
@@ -89,7 +94,7 @@ def FindBrowser(options):
         '\n'.join(sorted(set([b.browser_type for b in browsers]))))
 
   if options.browser_type == 'any':
-    types = ALL_BROWSER_TYPES.split(',')
+    types = ALL_BROWSER_TYPES
     def CompareBrowsersOnTypePriority(x, y):
       x_idx = types.index(x.browser_type)
       y_idx = types.index(y.browser_type)
