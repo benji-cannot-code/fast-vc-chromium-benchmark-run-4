07bac69f012ecf3297aafbe7211c751fa024f5b8
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-11 17:30:35 +0000

    Add mojo_idl_generator.gypi NOT FOR REVIEW BUG= R=darin@chromium.org
    Review URL: https://codereview.chromium.org/53173008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@234233 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/public/bindings/sample/sample_test.cc
+++ mojo/public/bindings/sample/sample_test.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "mojo/public/bindings/sample/generated/sample_service.h"
+#include "mojom/sample_service.h"
 
 namespace sample {
 
