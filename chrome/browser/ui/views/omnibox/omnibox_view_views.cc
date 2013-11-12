c89207c6e759e4f8dcbb827f97559d87ecc57ec5
yukishiino@chromium.org <yukishiino@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-12 11:46:52 +0000

    Disable inline autocompletion for IME in Omnibox by default.
    This CL disables the feature by default due to http://crbug.com/310407 .
    Sill you can enable the feature by explicitly setting the flag to 'Enable'.
    
    The flag will be applied to Linux Aura, which is Views-based.
    
    BUG=310407
    TEST=Test manually
    TBR=pkasting@chromium.org
    
    Review URL: https://codereview.chromium.org/66503012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@234484 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/omnibox/omnibox_view_views.cc
+++ chrome/browser/ui/views/omnibox/omnibox_view_views.cc
@@ -117,8 +117,8 @@ ui::TextInputType DetermineTextInputType() {
 }
 
 bool IsOmniboxAutoCompletionForImeEnabled() {
-  return !CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kDisableOmniboxAutoCompletionForIme);
+  return CommandLine::ForCurrentProcess()->HasSwitch(
+      switches::kEnableOmniboxAutoCompletionForIme);
 }
 
 }  // namespace
