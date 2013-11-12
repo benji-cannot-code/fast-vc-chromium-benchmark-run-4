ebac71d6f7cf58b156bbd398cd563dd7da7e8132
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-12 06:05:49 +0000

    Move reattachWhitespaceSiblings() into recalcStyle instead of recalcChildStyle
    Instead of returning a boolean and then checking it inside the loop we can
    just move the call into Element::recalcStyle and Text::recalcTextStyle. This
    makes the loop simpler and deletes a bunch of code.
    
    R=eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/64633006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161760 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Text.cpp
+++ third_party/WebKit/Source/core/dom/Text.cpp
@@ -306,7 +306,7 @@ void Text::attach(const AttachContext& context)
     CharacterData::attach(context);
 }
 
-bool Text::recalcTextStyle(StyleRecalcChange change)
+void Text::recalcTextStyle(StyleRecalcChange change)
 {
     if (RenderText* renderer = toRenderText(this->renderer())) {
         if (change != NoChange || needsStyleRecalc())
@@ -316,9 +316,8 @@ bool Text::recalcTextStyle(StyleRecalcChange change)
         clearNeedsStyleRecalc();
     } else if (needsStyleRecalc() || needsWhitespaceRenderer()) {
         reattach();
-        return true;
+        reattachWhitespaceSiblings();
     }
-    return false;
 }
 
 // If a whitespace node had no renderer and goes through a recalcStyle it may
--- third_party/WebKit/Source/core/dom/Text.h
+++ third_party/WebKit/Source/core/dom/Text.h
@@ -49,7 +49,7 @@ public:
     String wholeText() const;
     PassRefPtr<Text> replaceWholeText(const String&);
 
-    bool recalcTextStyle(StyleRecalcChange);
+    void recalcTextStyle(StyleRecalcChange);
     bool textRendererIsNeeded(const NodeRenderingContext&);
     virtual RenderText* createTextRenderer(RenderStyle*);
     void updateTextRenderer(unsigned offsetOfReplacedData, unsigned lengthOfReplacedData, RecalcStyleBehavior = DoNotRecalcStyle);
