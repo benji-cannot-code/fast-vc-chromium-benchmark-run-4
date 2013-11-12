e2da6eb9fc00c00ae476982c2d29c64aae2fc0f7
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-12 21:46:12 +0000

    Revert "Revert "Post-process font-face rules in addRulesFromSheet.""
    This reverts https://src.chromium.org/viewvc/blink?view=rev&revision=161741.
    It didn't turn out to be the cause of the perf regression.
    
    TBR=esprehn@chromium.org
    BUG=311669
    
    Review URL: https://codereview.chromium.org/66333009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161850 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/resolver/StyleResolver.h
+++ third_party/WebKit/Source/core/css/resolver/StyleResolver.h
@@ -217,6 +217,7 @@ public:
     void finishAppendAuthorStyleSheets();
 
     TreeBoundaryCrossingRules& treeBoundaryCrossingRules() { return m_treeBoundaryCrossingRules; }
+    void addFontFaceRules(const Vector<StyleRuleFontFace*>&, const ContainerNode* scope = 0);
 
     SelectorFilter& selectorFilter() { return m_selectorFilter; }
 
