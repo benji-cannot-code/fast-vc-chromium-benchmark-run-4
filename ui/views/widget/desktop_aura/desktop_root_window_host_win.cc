2834bbac2cfbbdb2a630e06e61c00928958508bd
zturner@chromium.org <zturner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-12 02:54:54 +0000

    Get rid of the 1-pixel client inset hack.
    This patch attempts to fix various graphical glitches and corruption
    once and for all.  The root of all these issues is the 1-pixel bottom
    client inset hack, so this patch aims to replace that hack with a
    (hopefully) less onerous hack.
    
    Prior to this patch, there was a comment indicating that before
    changing the frame type we must hide the window (and then show the window
    after the frame type is changed) or else the client area will paint black.
    However, no code was present to actually do this.  The original comment also
    indicated that this was not necessary for chrome theme changes, but rather
    only WM_DWMCOMPOSITIONCHANGED messages, but testing showed that this is
    not the case, and it is in fact required for both cases.
    
    Additionally, after changing the frame type, it is required to call
    SetWindowPos() with SWP_FRAMECHANGED in order to notify windows of the
    change.
    
    BUG=312535,311405
    
    TEST=
      Classic -> Aero Test  (Win7 only)
        0. Shutdown Chrome
        1. Set a Windows 7 Basic Theme
        2. Start Chrome
        3. Change to Windows 7 Aero Theme
        4. Observe correct rendering of browser frame
      Aero -> Classic Test  (Win7 only)
        0. Shutdown Chrome
        1. Set a Windows 7 Aero Theme
        2. Start Chrome
        3. Change to Windows 7 Basic Theme
        4. Observe correct rendering of browser frame
      Chrome Theme Test  (Win7 w/ Aero Theme, Win8, Win8.1)
        1. Hotdog -> Settings -> Reset to Default Theme
        2. Close Chrome
        3. Start Chrome
        4. Install Brushed Theme
          https://chrome.google.com/webstore/detail/brushed/bfjgbcjfpbbfepcccpaffkjofcmglifg?hl=en
        5. Close Chrome
        6. Start Chrome
        7. Hotdog -> Settings -> Reset to Default Theme
        8. Observe correct rendering of browser frame
    
    Review URL: https://codereview.chromium.org/59043012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@234386 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/desktop_aura/desktop_root_window_host_win.cc
+++ ui/views/widget/desktop_aura/desktop_root_window_host_win.cc
@@ -750,6 +750,7 @@ void DesktopRootWindowHostWin::HandleClientSizeChanged(
 }
 
 void DesktopRootWindowHostWin::HandleFrameChanged() {
+  SetWindowTransparency();
   // Replace the frame and layout the contents.
   GetWidget()->non_client_view()->UpdateFrame(true);
 }
@@ -889,6 +890,7 @@ void DesktopRootWindowHostWin::SetWindowTransparency() {
   bool transparent = ShouldUseNativeFrame() && !IsFullscreen();
   root_window_->compositor()->SetHostHasTransparentBackground(transparent);
   root_window_->window()->SetTransparent(transparent);
+  content_window_->SetTransparent(transparent);
 }
 
 bool DesktopRootWindowHostWin::IsModalWindowActive() const {
