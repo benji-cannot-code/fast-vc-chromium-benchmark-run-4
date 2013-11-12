2c8ac08177677d6e7b4ae0befc542181da30efec
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-12 21:12:23 +0000

    fix
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@234635 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/root_window_host_x11.cc
+++ ui/aura/root_window_host_x11.cc
@@ -417,7 +417,7 @@ bool RootWindowHostX11::Dispatch(const base::NativeEvent& event) {
 
   switch (xev->type) {
     case EnterNotify: {
-      aura::Window* root_window = GetRootWindow();
+      aura::Window* root_window = GetRootWindow()->window();
       client::CursorClient* cursor_client =
           client::GetCursorClient(root_window);
       if (cursor_client) {
