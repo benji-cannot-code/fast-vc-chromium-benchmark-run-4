6dc115dea62972be1f8addade928c11829b7ac64
ch.dumez@samsung.com <ch.dumez@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-12 18:30:54 +0000

    Abstract away some of the complexity in CSSSelectorWatch by using HashCountedSet
    Abstract away some of the complexity in CSSSelectorWatch by using HashCountedSet data
    structure instead of handling the counting manually and use a HashMap.
    WTF's HashCountedSet exists exactly for this kind of use cases and it makes the code
    a bit simpler.
    
    R=eseidel
    
    Review URL: https://codereview.chromium.org/69543002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161838 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/CSSSelectorWatch.h
+++ third_party/WebKit/Source/core/dom/CSSSelectorWatch.h
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/css/StyleRule.h"
 #include "core/dom/DocumentSupplementable.h"
 #include "platform/Timer.h"
+#include "wtf/HashCountedSet.h"
 #include "wtf/HashSet.h"
 #include "wtf/RefPtr.h"
 #include "wtf/Vector.h"
@@ -64,7 +65,7 @@ private:
 
     // Maps a CSS selector string with a -webkit-callback property to the number
     // of matching RenderStyle objects in this document.
-    HashMap<String, int> m_matchingCallbackSelectors;
+    HashCountedSet<String> m_matchingCallbackSelectors;
     // Selectors are relative to m_matchingCallbackSelectors's contents at
     // the previous call to selectorMatchChanged.
     HashSet<String> m_addedSelectors;
