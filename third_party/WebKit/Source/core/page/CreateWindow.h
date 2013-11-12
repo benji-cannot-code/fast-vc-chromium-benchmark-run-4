81944a4977f34b35b3b6d275b49498545a4c01ae
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-12 19:37:10 +0000

    Move the logic for creating a new window as part of a "normal" load to CreateWindow.cpp. Hopefully this will enable us to merge more of that code with the window.open case in the future. The new window case now re-enters FrameLoader via its public api, instead of calling into the new FrameLoader's private loadWithNavigationAction().
    Also, remove the ResourceRequest parameter from loadWithNavigationAction(), since it's redundant with part of the NavigationAction parameter.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/58283002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161843 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/CreateWindow.h
+++ third_party/WebKit/Source/core/page/CreateWindow.h
@@ -29,14 +29,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CreateWindow_h
 
 #include "core/frame/DOMWindow.h"
+#include "core/loader/FrameLoaderTypes.h"
+#include "core/loader/NavigationPolicy.h"
 #include "wtf/text/WTFString.h"
 
 namespace WebCore {
 class Frame;
+struct FrameLoadRequest;
 struct WindowFeatures;
 
 Frame* createWindow(const String& urlString, const AtomicString& frameName, const WindowFeatures&,
     DOMWindow* activeWindow, Frame* firstFrame, Frame* openerFrame, DOMWindow::PrepareDialogFunction = 0, void* functionContext = 0);
+
+void createWindowForRequest(const FrameLoadRequest&, Frame* openerFrame, NavigationPolicy, ShouldSendReferrer);
 
 } // namespace WebCore
 
