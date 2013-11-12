8c5d0fd94790773e3c7e49a204bf0c0621bb0be9
atwilson@chromium.org <atwilson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-12 08:48:33 +0000

    Change UserPolicySigninService to not create SigninManager in Observe()
    Add unit test to SigninManager to test kSigninAllowed=false case.
    
    Update UserPolicySigninService to no longer fetch a SigninManager in its
    Observe() callback. Since SigninManager can generate a SIGNED_OUT notification
    during initialization, trying to access SigninManager from the callback can
    lead to a crash when we double-create SigninManager.
    
    BUG=316229
    R=joaodasilva@chromium.org
    
    Review URL: https://codereview.chromium.org/66553003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@234464 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/signin/signin_manager_unittest.cc
+++ chrome/browser/signin/signin_manager_unittest.cc
@@ -56,6 +56,7 @@ BrowserContextKeyedService* SigninManagerBuild(
   service = new SigninManager(
       scoped_ptr<SigninManagerDelegate>(
           new ChromeSigninManagerDelegate(profile)));
+  service->Initialize(profile, NULL);
   return service;
 }
 
@@ -238,7 +239,6 @@ class SigninManagerTest : public TokenServiceTestHarness {
 
 TEST_F(SigninManagerTest, SignInWithCredentials) {
   CreateSigninManagerAsService();
-  manager_->Initialize(profile(), NULL);
   EXPECT_TRUE(manager_->GetAuthenticatedUsername().empty());
 
   manager_->StartSignInWithCredentials(
@@ -259,7 +259,6 @@ TEST_F(SigninManagerTest, SignInWithCredentials) {
 
 TEST_F(SigninManagerTest, SignInWithCredentialsNonCanonicalEmail) {
   CreateSigninManagerAsService();
-  manager_->Initialize(profile(), NULL);
   EXPECT_TRUE(manager_->GetAuthenticatedUsername().empty());
 
   manager_->StartSignInWithCredentials(
@@ -273,7 +272,6 @@ TEST_F(SigninManagerTest, SignInWithCredentialsNonCanonicalEmail) {
 
 TEST_F(SigninManagerTest, SignInWithCredentialsWrongEmail) {
   CreateSigninManagerAsService();
-  manager_->Initialize(profile(), NULL);
   EXPECT_TRUE(manager_->GetAuthenticatedUsername().empty());
 
   // If the email address used to start the sign in does not match the
@@ -289,7 +287,6 @@ TEST_F(SigninManagerTest, SignInWithCredentialsWrongEmail) {
 
 TEST_F(SigninManagerTest, SignInWithCredentialsEmptyPasswordValidCookie) {
   CreateSigninManagerAsService();
-  manager_->Initialize(profile(), NULL);
   EXPECT_TRUE(manager_->GetAuthenticatedUsername().empty());
 
   // Set a valid LSID cookie in the test cookie store.
@@ -317,7 +314,6 @@ TEST_F(SigninManagerTest, SignInWithCredentialsEmptyPasswordValidCookie) {
 
 TEST_F(SigninManagerTest, SignInWithCredentialsEmptyPasswordNoValidCookie) {
   CreateSigninManagerAsService();
-  manager_->Initialize(profile(), NULL);
   EXPECT_TRUE(manager_->GetAuthenticatedUsername().empty());
 
   // Since the password is empty, will verify the gaia cookies first.
@@ -336,7 +332,6 @@ TEST_F(SigninManagerTest, SignInWithCredentialsEmptyPasswordNoValidCookie) {
 
 TEST_F(SigninManagerTest, SignInWithCredentialsEmptyPasswordInValidCookie) {
   CreateSigninManagerAsService();
-  manager_->Initialize(profile(), NULL);
   EXPECT_TRUE(manager_->GetAuthenticatedUsername().empty());
 
   // Set an invalid LSID cookie in the test cookie store.
@@ -365,7 +360,6 @@ TEST_F(SigninManagerTest, SignInWithCredentialsEmptyPasswordInValidCookie) {
 
 TEST_F(SigninManagerTest, SignInWithCredentialsCallbackComplete) {
   CreateSigninManagerAsService();
-  manager_->Initialize(profile(), NULL);
   EXPECT_TRUE(manager_->GetAuthenticatedUsername().empty());
 
   // Since the password is empty, must verify the gaia cookies first.
@@ -385,7 +379,6 @@ TEST_F(SigninManagerTest, SignInWithCredentialsCallbackComplete) {
 
 TEST_F(SigninManagerTest, SignInWithCredentialsCallbackCancel) {
   CreateSigninManagerAsService();
-  manager_->Initialize(profile(), NULL);
   EXPECT_TRUE(manager_->GetAuthenticatedUsername().empty());
 
   // Since the password is empty, must verify the gaia cookies first.
@@ -406,7 +399,6 @@ TEST_F(SigninManagerTest, SignInWithCredentialsCallbackCancel) {
 
 TEST_F(SigninManagerTest, SignOut) {
   CreateSigninManagerAsService();
-  manager_->Initialize(profile(), NULL);
   SigninManager::OAuthTokenFetchedCallback dummy;
   manager_->StartSignInWithCredentials("0", "user@gmail.com", "password",
                                        dummy);
@@ -424,7 +416,6 @@ TEST_F(SigninManagerTest, SignOut) {
 
 TEST_F(SigninManagerTest, SignOutMidConnect) {
   CreateSigninManagerAsService();
-  manager_->Initialize(profile(), NULL);
   SigninManager::OAuthTokenFetchedCallback dummy;
   manager_->StartSignInWithCredentials("0", "user@gmail.com", "password",
                                        dummy);
@@ -439,7 +430,6 @@ TEST_F(SigninManagerTest, SignOutMidConnect) {
 
 TEST_F(SigninManagerTest, SignOutWhileProhibited) {
   CreateSigninManagerAsService();
-  manager_->Initialize(profile(), NULL);
   EXPECT_TRUE(manager_->GetAuthenticatedUsername().empty());
 
   manager_->SetAuthenticatedUsername("user@gmail.com");
@@ -531,4 +521,11 @@ TEST_F(SigninManagerTest, ExternalSignIn) {
   EXPECT_EQ("external@example.com",
             profile()->GetPrefs()->GetString(prefs::kGoogleServicesUsername));
   EXPECT_EQ("external@example.com", manager_->GetAuthenticatedUsername());
+}
+
+TEST_F(SigninManagerTest, SigninNotAllowed) {
+  std::string user("user@google.com");
+  profile()->GetPrefs()->SetString(prefs::kGoogleServicesUsername, user);
+  profile()->GetPrefs()->SetBoolean(prefs::kSigninAllowed, false);
+  CreateSigninManagerAsService();
 }
