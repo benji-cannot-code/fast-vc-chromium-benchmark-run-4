8c62f8ba62f48846bf0eebf040fffe0bc354e88e
davidben@chromium.org <davidben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-12 20:56:55 +0000

    Abort prerenders which try to create two navigation entries.
    We do not currently patch up the history correctly. Let's try to fix this by
    just aborting prerenders which do this, rather than complicating the history
    merge function. UMA will tell us if this is a problem and if this case turns
    out to be important.
    
    This does not affect the following:
    - Calls to location.replace.
    - <meta http-equiv=refresh> redirects with a timeout under 1 second.
    - Code which sets location.href before the load event.
    
    BUG=305660
    TEST=PrerenderBrowserTest.PrerenderNewNavigationEntry
         PrerenderBrowserTest.PrerenderLocationReplaceNavigateToFirst
         PrerenderBrowserTest.PrerenderLocationReplaceNavigateToSecond
    
    Review URL: https://codereview.chromium.org/68213005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@234631 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prerender/prerender_final_status.cc
+++ chrome/browser/prerender/prerender_final_status.cc
@@ -59,6 +59,7 @@ const char* kFinalStatusNames[] = {
   "Page Being Captured",
   "Bad Deferred Redirect",
   "Navigation Uncommitted",
+  "New Navigation Entry",
   "Max",
 };
 COMPILE_ASSERT(arraysize(kFinalStatusNames) == FINAL_STATUS_MAX + 1,
