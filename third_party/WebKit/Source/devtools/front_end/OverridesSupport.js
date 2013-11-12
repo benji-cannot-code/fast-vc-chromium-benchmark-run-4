02f55bf474dad2fad574a3528a573dca2c9fa592
dgozman@chromium.org <dgozman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-12 18:16:05 +0000

    DevTools: show message informing about required reload for some device emulation features.
    BUG=316608
    
    Review URL: https://codereview.chromium.org/66683003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161836 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/OverridesSupport.js
+++ third_party/WebKit/Source/devtools/front_end/OverridesSupport.js
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 WebInspector.OverridesSupport = function()
 {
     WebInspector.resourceTreeModel.addEventListener(WebInspector.ResourceTreeModel.EventTypes.MainFrameNavigated, this._deviceMetricsChanged.bind(this), this);
+    this._deviceMetricsOverrideEnabled = false;
 
     WebInspector.settings.overrideUserAgent.addChangeListener(this._userAgentChanged, this);
     WebInspector.settings.userAgent.addChangeListener(this._userAgentChanged, this);
@@ -479,7 +480,11 @@ WebInspector.OverridesSupport.prototype = {
                 this._updateWarningMessage(WebInspector.UIString("Screen emulation is not available on this page."));
                 return;
             }
-            this._updateWarningMessage("");
+
+            var enabled = !!(dipWidth && dipHeight);
+            this._updateWarningMessage(this._deviceMetricsOverrideEnabled !== enabled ?
+                WebInspector.UIString("You might need to reload the page for proper user agent spoofing and viewport rendering.") : "");
+            this._deviceMetricsOverrideEnabled = enabled;
         }
         this._revealOverridesTabIfNeeded();
     },
