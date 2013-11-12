3d16115bda5b383dd8749b94502e11059c87243b
teravest@chromium.org <teravest@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-12 22:56:08 +0000

    Pepper: Tighten GetLiveModule in PluginRegistry.
    On PepperPluginInstanceImpl::Delete(), we send a synchronous DidDestroy message
    to the plugin.  The plugin can exit() in response to this message.
    
    While the renderer is waiting for a response (and holding a reference to
    PepperPluginInstanceImpl on the stack), another plugin for the page could
    create an instance of a plugin using the same path as the deleted instance.
    
    This can cause a PluginModule to be reused on the host for a plugin which is no
    longer live. In this case, sending DidCreate will fail.
    
    Alternatively, the lifetime of PluginModule could be cleaned up, but there are
    many uses of PepperPluginInstanceImpl::module() throughout the codebase. I'm
    happy to look at seeing if that can be cleaned up in another change.
    
    BUG=
    R=dmichael@chromium.org, teravest@google.com
    
    Review URL: https://codereview.chromium.org/69363002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@234659 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/pepper/pepper_plugin_registry.cc
+++ content/renderer/pepper/pepper_plugin_registry.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "content/common/pepper_plugin_list.h"
+#include "content/renderer/pepper/pepper_plugin_instance_impl.h"
 #include "content/renderer/pepper/plugin_module.h"
 #include "ppapi/shared_impl/ppapi_permissions.h"
 
@@ -44,10 +45,29 @@ const PepperPluginInfo* PepperPluginRegistry::GetInfoForPlugin(
 }
 
 PluginModule* PepperPluginRegistry::GetLiveModule(const base::FilePath& path) {
-  NonOwningModuleMap::iterator it = live_modules_.find(path);
-  if (it == live_modules_.end())
+  NonOwningModuleMap::iterator module_iter = live_modules_.find(path);
+  if (module_iter == live_modules_.end())
     return NULL;
-  return it->second;
+
+  // Check the instances for the module to see if they've all been Delete()d.
+  // We don't want to return a PluginModule in that case, since the plugin may
+  // have exited already.
+  const PluginModule::PluginInstanceSet& instance_set =
+      module_iter->second->GetAllInstances();
+
+  // If instance_set is empty, InstanceCreated() hasn't been called yet, so
+  // it's safe to return the PluginModule.
+  if (instance_set.empty())
+    return module_iter->second;
+
+  PluginModule::PluginInstanceSet::const_iterator instance_iter =
+      instance_set.begin();
+  while (instance_iter != instance_set.end()) {
+    if (!(*instance_iter)->is_deleted())
+      return module_iter->second;
+    ++instance_iter;
+  }
+  return NULL;
 }
 
 void PepperPluginRegistry::AddLiveModule(const base::FilePath& path,
