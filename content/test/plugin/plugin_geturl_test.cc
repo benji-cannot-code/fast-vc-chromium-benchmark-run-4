7c1344ad550826e094c935d5912b11ad680efae9
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-12 23:48:34 +0000

    Add regression test for 301510
    BUG=301510
    R=ananta@chromium.org
    
    Review URL: https://codereview.chromium.org/62203013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@234664 0039d316-1c4b-4281-b951-d872f2087c98

--- content/test/plugin/plugin_geturl_test.cc
+++ content/test/plugin/plugin_geturl_test.cc
@@ -30,8 +30,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define REDIRECT_SRC_URL \
     "http://mock.http/npapi/plugin_read_page_redirect_src.html"
 
-// The notification id for the redirect notification url.
-#define REDIRECT_SRC_URL_NOTIFICATION_ID 4
+// The notification id for the redirect notification url that we will cancel.
+#define REDIRECT_SRC_URL_NOTIFICATION_CANCEL_ID 4
+
+// The notification id for the redirect notification url that we will accept.
+#define REDIRECT_SRC_URL_NOTIFICATION_ALLOW_ID 5
 
 // The identifier for the bogus url stream.
 #define BOGUS_URL_STREAM_ID 3
@@ -49,7 +52,8 @@ PluginGetURLTest::PluginGetURLTest(NPP id, NPNetscapeFuncs *host_functions)
     expect_404_response_(false),
     npn_evaluate_context_(false),
     handle_url_redirects_(false),
-    received_url_redirect_notification_(false),
+    received_url_redirect_cancel_notification_(false),
+    received_url_redirect_allow_notification_(false),
     check_cookies_(false) {
 }
 
@@ -115,7 +119,7 @@ NPError PluginGetURLTest::SetWindow(NPWindow* pNPWindow) {
     } else if (handle_url_redirects_) {
       HostFunctions()->geturlnotify(
           id(), REDIRECT_SRC_URL, NULL,
-          reinterpret_cast<void*>(REDIRECT_SRC_URL_NOTIFICATION_ID));
+          reinterpret_cast<void*>(REDIRECT_SRC_URL_NOTIFICATION_CANCEL_ID));
       return NPERR_NO_ERROR;
     } else if (check_cookies_) {
       HostFunctions()->geturlnotify(
@@ -218,8 +222,10 @@ NPError PluginGetURLTest::NewStream(NPMIMEType type, NPStream* stream,
     case BOGUS_URL_STREAM_ID:
       SetError("Unexpected NewStream for BOGUS_URL");
       break;
-    case REDIRECT_SRC_URL_NOTIFICATION_ID:
+    case REDIRECT_SRC_URL_NOTIFICATION_CANCEL_ID:
       SetError("Should not redirect to URL when plugin denied it.");
+      break;
+    case REDIRECT_SRC_URL_NOTIFICATION_ALLOW_ID:
       break;
     default:
       SetError("Unexpected NewStream callback");
@@ -295,6 +301,8 @@ int32 PluginGetURLTest::Write(NPStream *stream, int32 offset, int32 len,
     case BOGUS_URL_STREAM_ID:
       SetError("Unexpected write callback for BOGUS_URL");
       break;
+    case REDIRECT_SRC_URL_NOTIFICATION_ALLOW_ID:
+      break;
     default:
       SetError("Unexpected write callback");
       break;
@@ -340,6 +348,8 @@ NPError PluginGetURLTest::DestroyStream(NPStream *stream, NPError reason) {
   switch (stream_id) {
     case SELF_URL_STREAM_ID:
       // don't care
+      break;
+    case REDIRECT_SRC_URL_NOTIFICATION_ALLOW_ID:
       break;
     case FETCHED_URL_STREAM_ID:
       {
@@ -415,9 +425,21 @@ void PluginGetURLTest::URLNotify(const char* url, NPReason reason, void* data) {
       }
       tests_in_progress_--;
       break;
-    case REDIRECT_SRC_URL_NOTIFICATION_ID: {
-      if (!received_url_redirect_notification_) {
-        SetError("Failed to receive URLRedirect notification");
+    case REDIRECT_SRC_URL_NOTIFICATION_CANCEL_ID: {
+      if (!received_url_redirect_cancel_notification_) {
+        SetError("Failed to receive URLRedirect notification for cancel");
+      }
+      if (reason != NPRES_NETWORK_ERR)  {
+        SetError("Redirected URL didn't get canceled");
+      }
+      break;
+    }
+    case REDIRECT_SRC_URL_NOTIFICATION_ALLOW_ID: {
+      if (!received_url_redirect_allow_notification_) {
+        SetError("Failed to receive URLRedirect notification for allow");
+      }
+      if (reason != NPRES_DONE) {
+        SetError("Redirected URL didn't complete successfully");
       }
       tests_in_progress_--;
       break;
@@ -434,10 +456,22 @@ void PluginGetURLTest::URLNotify(const char* url, NPReason reason, void* data) {
 void PluginGetURLTest::URLRedirectNotify(const char* url,
                                          int32_t status,
                                          void* notify_data) {
-  if (!base::strcasecmp(url, "http://mock.http/npapi/plugin_read_page.html")) {
-    received_url_redirect_notification_ = true;
-    // Disallow redirect notification.
-    HostFunctions()->urlredirectresponse(id(), notify_data, false);
+  unsigned long stream_id = reinterpret_cast<unsigned long>(notify_data);
+  if (stream_id == REDIRECT_SRC_URL_NOTIFICATION_CANCEL_ID) {
+    if (!base::strcasecmp(url,
+                          "http://mock.http/npapi/plugin_read_page.html")) {
+      received_url_redirect_cancel_notification_ = true;
+      // Disallow redirect notification.
+      HostFunctions()->urlredirectresponse(id(), notify_data, false);
+
+      // Now start a request that we will allow to redirect.
+      HostFunctions()->geturlnotify(
+        id(), REDIRECT_SRC_URL, NULL,
+        reinterpret_cast<void*>(REDIRECT_SRC_URL_NOTIFICATION_ALLOW_ID));
+    }
+  } else if (stream_id == REDIRECT_SRC_URL_NOTIFICATION_ALLOW_ID) {
+    received_url_redirect_allow_notification_ = true;
+    HostFunctions()->urlredirectresponse(id(), notify_data, true);
   }
 }
 
