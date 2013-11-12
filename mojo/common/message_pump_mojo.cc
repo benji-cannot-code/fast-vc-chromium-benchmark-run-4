e15011fb546e1756f902be59daebed6a9e023bcc
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-12 16:51:09 +0000

    Add base/time include.
    R=darin@chromium.org
    TBR=darin@chromium.org
    BUG=none
    
    Review URL: https://codereview.chromium.org/68763006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@234542 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/common/message_pump_mojo.cc
+++ mojo/common/message_pump_mojo.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/logging.h"
+#include "base/time/time.h"
 #include "mojo/common/message_pump_mojo_handler.h"
 #include "mojo/common/scoped_message_pipe.h"
 
