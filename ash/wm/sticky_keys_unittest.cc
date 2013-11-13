24470c1ff4b7f457ca292288505e46af286f85ad
tengs@chromium.org <tengs@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-13 06:01:59 +0000

    Fix crash when shortcut destroys the target that the modifier up event is sent.
    BUG=317831
    TEST=new test case
    
    Review URL: https://codereview.chromium.org/68743003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@234769 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/sticky_keys_unittest.cc
+++ ash/wm/sticky_keys_unittest.cc
@@ -9,45 +9,54 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #undef None
 #undef Bool
 
+#include "ash/shell.h"
 #include "ash/test/ash_test_base.h"
+#include "base/bind.h"
+#include "base/callback.h"
 #include "base/memory/scoped_vector.h"
+#include "ui/aura/window.h"
+#include "ui/events/event_handler.h"
 #include "ui/events/x/events_x_utils.h"
 
 namespace ash {
-
-// A stub implementation of EventTarget.
-class StubEventTarget : public ui::EventTarget {
- public:
-  StubEventTarget() {}
-  virtual ~StubEventTarget() {}
-
-  virtual bool CanAcceptEvent(const ui::Event& event) OVERRIDE {
-    return true;
-  }
-
-  virtual EventTarget* GetParentTarget() OVERRIDE {
-    return NULL;
-  }
-
- private:
-  DISALLOW_COPY_AND_ASSIGN(StubEventTarget);
-};
 
 // A testable and StickyKeysHandler.
 class MockStickyKeysHandlerDelegate :
     public StickyKeysHandler::StickyKeysHandlerDelegate {
  public:
-  MockStickyKeysHandlerDelegate() {}
+  class Delegate {
+   public:
+    virtual aura::Window* GetExpectedTarget() = 0;
+    virtual void OnShortcutPressed() = 0;
+
+   protected:
+    virtual ~Delegate() {}
+  };
+
+  MockStickyKeysHandlerDelegate(Delegate* delegate) : delegate_(delegate) {}
+
   virtual ~MockStickyKeysHandlerDelegate() {}
 
   // StickyKeysHandler override.
   virtual void DispatchKeyEvent(ui::KeyEvent* event,
                                 aura::Window* target) OVERRIDE {
+    ASSERT_EQ(delegate_->GetExpectedTarget(), target);
+
+    // Detect a special shortcut when it is dispatched. This shortcut will
+    // not be hit in the LOCKED state as this case does not involve the
+    // delegate.
+    if (event->type() == ui::ET_KEY_PRESSED &&
+        event->key_code() == ui::VKEY_J &&
+        event->flags() | ui::EF_CONTROL_DOWN) {
+      delegate_->OnShortcutPressed();
+    }
+
     events_.push_back(event->Copy());
   }
 
   virtual void DispatchMouseEvent(ui::MouseEvent* event,
                                   aura::Window* target) OVERRIDE {
+    ASSERT_EQ(delegate_->GetExpectedTarget(), target);
     events_.push_back(new ui::MouseEvent(event->native_event()));
   }
 
@@ -63,20 +72,41 @@ class MockStickyKeysHandlerDelegate :
 
  private:
   ScopedVector<ui::Event> events_;
+  Delegate* delegate_;
 
   DISALLOW_COPY_AND_ASSIGN(MockStickyKeysHandlerDelegate);
 };
 
-class StickyKeysTest : public test::AshTestBase {
+class StickyKeysTest : public test::AshTestBase,
+                       public MockStickyKeysHandlerDelegate::Delegate {
  protected:
+  StickyKeysTest()
+      : target_(NULL),
+        root_window_(NULL) {}
+
   virtual void SetUp() OVERRIDE {
-    target_.reset(new StubEventTarget());
     test::AshTestBase::SetUp();
+
+    // |target_| owned by root window of shell. It is still safe to delete
+    // it ourselves.
+    target_ = CreateTestWindowInShellWithId(0);
+    root_window_ = target_->GetRootWindow();
   }
 
   virtual void TearDown() OVERRIDE {
     test::AshTestBase::TearDown();
-    target_.reset();
+  }
+
+  // Overridden from MockStickyKeysHandlerDelegate::Delegate:
+  virtual aura::Window* GetExpectedTarget() OVERRIDE {
+    return target_ ? target_ : root_window_;
+  }
+
+  virtual void OnShortcutPressed() OVERRIDE {
+    if (target_) {
+      delete target_;
+      target_ = NULL;
+    }
   }
 
   // Generates keyboard event.
@@ -90,7 +120,7 @@ class StickyKeysTest : public test::AshTestBase {
     xevs_.push_back(xev);
     ui::KeyEvent* event =  new ui::KeyEvent(xev, false);
     ui::Event::DispatcherApi dispatcher(event);
-    dispatcher.set_target(target_.get());
+    dispatcher.set_target(target_);
     return event;
   }
 
@@ -103,7 +133,7 @@ class StickyKeysTest : public test::AshTestBase {
     xevs_.push_back(xev);
     ui::MouseEvent* event = new ui::MouseEvent(xev);
     ui::Event::DispatcherApi dispatcher(event);
-    dispatcher.set_target(target_.get());
+    dispatcher.set_target(target_);
     return event;
   }
 
@@ -114,19 +144,24 @@ class StickyKeysTest : public test::AshTestBase {
     xevs_.push_back(xev);
     ui::MouseWheelEvent* event = new ui::MouseWheelEvent(xev);
     ui::Event::DispatcherApi dispatcher(event);
-    dispatcher.set_target(target_.get());
+    dispatcher.set_target(target_);
     return event;
   }
 
+  aura::Window* target() { return target_; }
+
  private:
-  scoped_ptr<StubEventTarget> target_;
+  // Owned by root window of shell, but we can still delete |target_| safely.
+  aura::Window* target_;
+  // The root window of |target_|. Not owned.
+  aura::Window* root_window_;
   ScopedVector<XEvent> xevs_;
 };
 
 TEST_F(StickyKeysTest, BasicOneshotScenarioTest) {
   scoped_ptr<ui::KeyEvent> ev;
   MockStickyKeysHandlerDelegate* mock_delegate =
-      new MockStickyKeysHandlerDelegate();
+      new MockStickyKeysHandlerDelegate(this);
   StickyKeysHandler sticky_key(ui::EF_SHIFT_DOWN, mock_delegate);
 
   EXPECT_EQ(StickyKeysHandler::DISABLED, sticky_key.current_state());
@@ -173,7 +208,7 @@ TEST_F(StickyKeysTest, BasicOneshotScenarioTest) {
 TEST_F(StickyKeysTest, BasicLockedScenarioTest) {
   scoped_ptr<ui::KeyEvent> ev;
   MockStickyKeysHandlerDelegate* mock_delegate =
-      new MockStickyKeysHandlerDelegate();
+      new MockStickyKeysHandlerDelegate(this);
   StickyKeysHandler sticky_key(ui::EF_SHIFT_DOWN, mock_delegate);
 
   EXPECT_EQ(StickyKeysHandler::DISABLED, sticky_key.current_state());
@@ -230,7 +265,7 @@ TEST_F(StickyKeysTest, BasicLockedScenarioTest) {
 TEST_F(StickyKeysTest, NonTargetModifierTest) {
   scoped_ptr<ui::KeyEvent> ev;
   MockStickyKeysHandlerDelegate* mock_delegate =
-      new MockStickyKeysHandlerDelegate();
+      new MockStickyKeysHandlerDelegate(this);
   StickyKeysHandler sticky_key(ui::EF_SHIFT_DOWN, mock_delegate);
 
   EXPECT_EQ(StickyKeysHandler::DISABLED, sticky_key.current_state());
@@ -279,7 +314,7 @@ TEST_F(StickyKeysTest, NormalShortcutTest) {
   // Sticky keys should not be enabled if we perform a normal shortcut.
   scoped_ptr<ui::KeyEvent> ev;
   MockStickyKeysHandlerDelegate* mock_delegate =
-      new MockStickyKeysHandlerDelegate();
+      new MockStickyKeysHandlerDelegate(this);
   StickyKeysHandler sticky_key(ui::EF_CONTROL_DOWN, mock_delegate);
 
   EXPECT_EQ(StickyKeysHandler::DISABLED, sticky_key.current_state());
@@ -303,7 +338,7 @@ TEST_F(StickyKeysTest, MouseEventOneshot) {
   scoped_ptr<ui::MouseEvent> ev;
   scoped_ptr<ui::KeyEvent> kev;
   MockStickyKeysHandlerDelegate* mock_delegate =
-      new MockStickyKeysHandlerDelegate();
+      new MockStickyKeysHandlerDelegate(this);
   StickyKeysHandler sticky_key(ui::EF_CONTROL_DOWN, mock_delegate);
 
   EXPECT_EQ(StickyKeysHandler::DISABLED, sticky_key.current_state());
@@ -347,7 +382,7 @@ TEST_F(StickyKeysTest, MouseEventLocked) {
   scoped_ptr<ui::MouseEvent> ev;
   scoped_ptr<ui::KeyEvent> kev;
   MockStickyKeysHandlerDelegate* mock_delegate =
-      new MockStickyKeysHandlerDelegate();
+      new MockStickyKeysHandlerDelegate(this);
   StickyKeysHandler sticky_key(ui::EF_CONTROL_DOWN, mock_delegate);
 
   EXPECT_EQ(StickyKeysHandler::DISABLED, sticky_key.current_state());
@@ -395,6 +430,29 @@ TEST_F(StickyKeysTest, MouseEventLocked) {
   kev.reset(GenerateKey(false, ui::VKEY_N));
   sticky_key.HandleKeyEvent(kev.get());
   EXPECT_EQ(StickyKeysHandler::LOCKED, sticky_key.current_state());
+}
+
+TEST_F(StickyKeysTest, EventTargetDestroyed) {
+  scoped_ptr<ui::KeyEvent> ev;
+  MockStickyKeysHandlerDelegate* mock_delegate =
+      new MockStickyKeysHandlerDelegate(this);
+  StickyKeysHandler sticky_key(ui::EF_CONTROL_DOWN, mock_delegate);
+
+  target()->Focus();
+
+  // Go into ENABLED state.
+  EXPECT_EQ(StickyKeysHandler::DISABLED, sticky_key.current_state());
+  ev.reset(GenerateKey(true, ui::VKEY_CONTROL));
+  sticky_key.HandleKeyEvent(ev.get());
+  ev.reset(GenerateKey(false, ui::VKEY_CONTROL));
+  sticky_key.HandleKeyEvent(ev.get());
+  EXPECT_EQ(StickyKeysHandler::ENABLED, sticky_key.current_state());
+
+  // CTRL+J is a special shortcut that will destroy the event target.
+  ev.reset(GenerateKey(true, ui::VKEY_J));
+  sticky_key.HandleKeyEvent(ev.get());
+  EXPECT_EQ(StickyKeysHandler::DISABLED, sticky_key.current_state());
+  EXPECT_FALSE(target());
 }
 
 }  // namespace ash
--- ui/aura/window_tracker.h
+++ ui/aura/window_tracker.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef UI_AURA_WINDOW_LIFE_TRACKER_H_
-#define UI_AURA_WINDOW_LIFE_TRACKER_H_
+#ifndef UI_AURA_WINDOW_TRACKER_H_
+#define UI_AURA_WINDOW_TRACKER_H_
 
 #include <set>
 
@@ -48,4 +48,4 @@ class AURA_EXPORT WindowTracker : public WindowObserver {
 
 }  // namespace aura
 
-#endif  // UI_AURA_WINDOW_LIFE_TRACKER_H_
+#endif  // UI_AURA_WINDOW_TRACKER_H_
