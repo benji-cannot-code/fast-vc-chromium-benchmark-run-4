d9d9d084f517a036fd04639e698f36aecaa25c23
aandrey@chromium.org <aandrey@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-13 14:51:53 +0000

    DevTools: Fix disabling all pauses on reload.
    BUG=115105, 286081
    R=yurys@chromium.org,johnjbarton@chromium.org,pfeldman
    
    Review URL: https://codereview.chromium.org/60203007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161920 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/inspector/debugger-test.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/debugger-test.js
@@ -84,6 +84,11 @@ InspectorTest.runTestFunctionAndWaitUntilPaused = function(callback)
     InspectorTest.waitUntilPaused(callback);
 };
 
+InspectorTest.waitUntilPausedNextTime = function(callback)
+{
+    InspectorTest._waitUntilPausedCallback = InspectorTest.safeWrap(callback);
+};
+
 InspectorTest.waitUntilPaused = function(callback)
 {
     callback = InspectorTest.safeWrap(callback);
@@ -92,6 +97,11 @@ InspectorTest.waitUntilPaused = function(callback)
         callback.apply(callback, InspectorTest._pausedScriptArguments);
     else
         InspectorTest._waitUntilPausedCallback = callback;
+};
+
+InspectorTest.waitUntilResumedNextTime = function(callback)
+{
+    InspectorTest._waitUntilResumedCallback = InspectorTest.safeWrap(callback);
 };
 
 InspectorTest.waitUntilResumed = function(callback)
