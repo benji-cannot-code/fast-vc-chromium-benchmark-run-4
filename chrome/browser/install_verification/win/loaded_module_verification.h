3c009659e5520e744021a552b341f24764d321e4
erikwright@chromium.org <erikwright@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-13 22:44:18 +0000

    Refactor module loading verification.
    BUG=
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=232525
    
    Review URL: https://codereview.chromium.org/27507010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@234936 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/install_verification/win/loaded_module_verification.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_INSTALL_VERIFICATION_WIN_LOADED_MODULE_VERIFICATION_H_
+#define CHROME_BROWSER_INSTALL_VERIFICATION_WIN_LOADED_MODULE_VERIFICATION_H_
+
+#include <set>
+
+#include "chrome/browser/install_verification/win/module_ids.h"
+#include "chrome/browser/install_verification/win/module_verification_common.h"
+
+struct ModuleInfo;
+
+// Verifies the provided |loaded_modules| info against the expected
+// |module_ids|. Reports results to |delegate|.
+void VerifyLoadedModules(const std::set<ModuleInfo>& loaded_modules,
+                         const ModuleIDs& module_ids,
+                         ModuleVerificationDelegate* delegate);
+
+#endif  // CHROME_BROWSER_INSTALL_VERIFICATION_WIN_LOADED_MODULE_VERIFICATION_H_
