3c67fe64bc23e4a6f94d47b485b4fe595a7b76ea
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-13 06:55:16 +0000

    Process host rules in a post-processing step.
    This is another step in making it so that RuleSets
    don't need to know about scoping nodes or StyleResolvers.
    
    BUG=311669
    
    Review URL: https://codereview.chromium.org/68173007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161877 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/RuleSet.h
+++ third_party/WebKit/Source/core/css/RuleSet.h
@@ -128,6 +128,7 @@ public:
     const Vector<StyleRuleViewport*>& viewportRules() const { ASSERT(!m_pendingRules); return m_viewportRules; }
     const Vector<StyleRuleFontFace*>& fontFaceRules() const { return m_fontFaceRules; }
     const Vector<StyleRuleKeyframes*>& keyframesRules() const { return m_keyframesRules; }
+    const Vector<StyleRuleHost*>& hostRules() const { return m_hostRules; }
 
     unsigned ruleCount() const { return m_ruleCount; }
 
@@ -162,6 +163,7 @@ private:
     void addViewportRule(StyleRuleViewport*);
     void addFontFaceRule(StyleRuleFontFace*);
     void addKeyframesRule(StyleRuleKeyframes*);
+    void addHostRule(StyleRuleHost*);
     void addRegionRule(StyleRuleRegion*, bool hasDocumentSecurityOrigin);
 
     void addChildRules(const Vector<RefPtr<StyleRuleBase> >&, const MediaQueryEvaluator& medium, StyleResolver*, const ContainerNode* scope, bool hasDocumentSecurityOrigin, AddRuleFlags);
@@ -197,6 +199,7 @@ private:
     Vector<StyleRuleViewport*> m_viewportRules;
     Vector<StyleRuleFontFace*> m_fontFaceRules;
     Vector<StyleRuleKeyframes*> m_keyframesRules;
+    Vector<StyleRuleHost*> m_hostRules;
 
     unsigned m_ruleCount;
     OwnPtr<PendingRuleMaps> m_pendingRules;
--- third_party/WebKit/Source/core/css/resolver/ScopedStyleResolver.cpp
+++ third_party/WebKit/Source/core/css/resolver/ScopedStyleResolver.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/css/RuleFeature.h"
 #include "core/css/RuleSet.h"
 #include "core/css/StyleRule.h"
+#include "core/css/StyleSheetContents.h"
 #include "core/css/resolver/StyleResolver.h" // For MatchRequest.
 #include "core/css/resolver/ViewportStyleResolver.h"
 #include "core/dom/Document.h"
@@ -252,7 +253,7 @@ void ScopedStyleResolver::addRulesFromSheet(StyleSheetContents* sheet, const Med
     if (!m_authorStyle)
         m_authorStyle = RuleSet::create();
     m_authorStyle->addRulesFromSheet(sheet, medium, resolver, &m_scopingNode);
-    resolver->processScopedRules(*m_authorStyle, &m_scopingNode);
+    resolver->processScopedRules(*m_authorStyle, sheet->baseURL(), &m_scopingNode);
 }
 
 inline RuleSet* ScopedStyleResolver::ensureAtHostRuleSetFor(const ShadowRoot* shadowRoot)
