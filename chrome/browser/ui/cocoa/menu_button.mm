ef1e0454b6c186e2024174e0c6aad505167740a3
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-13 03:05:13 +0000

    [rAC, OSX] Fix menu button placement.
    MenuButton was placing the popup menu too far away from the button for
    buttons with sizes different from toolbar buttons. Experimentation
    yields that it is placed correctly for a button height of 19px and
    needs to be placed differently based on button height.
    
    BUG=308180
    R=avi@chromium.org
    
    Review URL: https://codereview.chromium.org/66723010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@234721 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/menu_button.mm
+++ chrome/browser/ui/cocoa/menu_button.mm
@@ -133,7 +133,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // should. I don't understand it.
   NSRect frame = [self menuRect];
   frame.origin.x -= 2.0;
-  frame.size.height += 10.0;
+  frame.size.height -= 19.0 - NSHeight(frame);
 
   // Make our pop-up button cell and set things up. This is, as of 10.5, the
   // official Apple-recommended hack. Later, perhaps |-[NSMenu
