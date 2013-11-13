f5e09a8a7c1e30f0973441a3fafe560bc1aa1eaa
hajimehoshi@chromium.org <hajimehoshi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-13 05:07:13 +0000

    Copy the implementations of TranslateUIDelegate to TransalteInfobarDelegate
    TranslateInfobarDelegate won't own a WebContents by https://codereview.chromium.org/22694006/, while TranslateUIDelegate's implementation relies on the existence of a WebContents. Thus, I copied a part of implementations of TranslateUIDelegate to TranslteInfobarDelegate.
    
    BUG=62154
    TEST=browser_test --gtest_filter=TranslateManagerBrowserTest.*
    
    Review URL: https://codereview.chromium.org/64823005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@234759 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/translate/translate_manager_browsertest.cc
+++ chrome/browser/translate/translate_manager_browsertest.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/translate/translate_prefs.h"
 #include "chrome/browser/translate/translate_script.h"
 #include "chrome/browser/translate/translate_tab_helper.h"
+#include "chrome/browser/translate/translate_ui_delegate.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/translate/translate_bubble_factory.h"
