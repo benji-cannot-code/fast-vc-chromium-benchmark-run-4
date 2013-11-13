de29d5cc8d57c155011a2560dace191ce5951c8e
earthdok@chromium.org <earthdok@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-13 19:18:59 +0000

    Include <unistd.h> in base/process/internal_linux.h.
    When building with libc++, this causes a build error due to missing definition
    for pid_t.
    
    BUG=318770
    R=thakis@chromium.org
    
    Review URL: https://codereview.chromium.org/69763003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@234872 0039d316-1c4b-4281-b951-d872f2087c98

--- base/process/internal_linux.h
+++ base/process/internal_linux.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_PROCESS_LINUX_INTERNAL_H_
 #define BASE_PROCESS_LINUX_INTERNAL_H_
 
+#include <unistd.h>
+
 #include "base/files/file_path.h"
 
 namespace base {
