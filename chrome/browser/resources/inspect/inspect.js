582824fe9a976efbd8f87491295a423da0488f01
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-13 09:32:21 +0000

    DevTools: Fix exception in inspect.js when saving port forwarding settings
    TBR=pfeldman
    BUG=318494
    
    Review URL: https://codereview.chromium.org/70513006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@234785 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/inspect/inspect.js
+++ chrome/browser/resources/inspect/inspect.js
@@ -592,7 +592,7 @@ function unsetModal(dialog) {
   }
 
   if (window.holdDevices) {
-    populateDeviceLists(window.holdDevices);
+    populateRemoteTargets(window.holdDevices);
     delete window.holdDevices;
   }
 
