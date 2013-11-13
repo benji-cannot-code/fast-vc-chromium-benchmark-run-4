5c88d400787f0e8b1994f90fd873e01bede87d14
serya@chromium.org <serya@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-13 13:06:41 +0000

    Revert 161813 "Fix error recovery in CSS source data extraction."
    BUG=318678
    
    > Fix error recovery in CSS source data extraction.
    > 
    > An invalid selector of the last rule inside a @media rule would prevent correct source data extraction.
    > 
    > BUG=317499
    > 
    > Review URL: https://codereview.chromium.org/68373004
    
    TBR=serya@chromium.org
    
    Review URL: https://codereview.chromium.org/71523002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161917 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/CSSParser-in.cpp
+++ third_party/WebKit/Source/core/css/CSSParser-in.cpp
@@ -11565,6 +11565,7 @@ void CSSParser::startRuleHeader(CSSRuleSourceData::Type ruleType)
 
 void CSSParser::endRuleHeader()
 {
+    m_ruleHeaderType = CSSRuleSourceData::UNKNOWN_RULE;
     if (m_sourceDataHandler)
         m_sourceDataHandler->endRuleHeader(safeUserStringTokenOffset());
 }
@@ -11589,15 +11590,8 @@ void CSSParser::startRuleBody()
 
 void CSSParser::endRuleBody(bool discard)
 {
-    m_ruleHeaderType = CSSRuleSourceData::UNKNOWN_RULE;
     if (m_sourceDataHandler)
         m_sourceDataHandler->endRuleBody(safeUserStringTokenOffset(), discard);
-}
-
-void CSSParser::endInvalidRule()
-{
-    if (m_ruleHeaderType != CSSRuleSourceData::UNKNOWN_RULE)
-        endRuleBody(true);
 }
 
 void CSSParser::startProperty()
