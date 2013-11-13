2e91d1abcdf79c1bf4e480312ecd48d17fa361c4
apiccion@chromium.org <apiccion@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-13 00:57:39 +0000

    Pages from other devices open in current tab.
    * Opening a foreign session or recently closed now takes over the tab 
    (nuking its navigation history)
    * Opening a chrome-to-mobile page now loads in the current tab.
    
    BUG=257102
    
    Review URL: https://codereview.chromium.org/36473002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@234684 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/android/recently_closed_tabs_bridge.cc
+++ chrome/browser/android/recently_closed_tabs_bridge.cc
@@ -114,7 +114,7 @@ jboolean RecentlyClosedTabsBridge::OpenRecentlyClosedTab(JNIEnv* env,
 
   SessionRestore::RestoreForeignSessionTab(web_contents,
                                            session_tab,
-                                           NEW_FOREGROUND_TAB);
+                                           CURRENT_TAB);
   return true;
 }
 
