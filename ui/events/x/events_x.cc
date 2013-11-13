b183b1fbc3d5e55bba75f223ff5cfa8b76b108a6
miletus@chromium.org <miletus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-13 19:18:38 +0000

    Correct X event CTM calculation for multi-monitor case
    When there are multi-monitors, X will scale the touch event's
    coordinate from device coordinate to screen coordinate. We then
    in Chrome compute a CTM that will scale/translate the X events
    to the correct RootWindow's bounds. The scaling in X are done
    in floating points but when finally converted to ui::TouchEvent's
    location as integers, there could be +-1 happening which could
    make the touch events appear as out of bound of any RootWindow
    and thus get lost. One symptom is that when move fingers to the
    upper edge of external monitor and lift, the touch release event
    could be lost and the finger seems get stuck on the screen.
     
    We have a small fix in calculating CTM X/Y scale/offset by using
    framebuffer_width/height - 1 instead of framebuffer_width/height.
    
    Also when converting X event's coordinate to ui::TouchEvent's
    location, use rounding instead of static_cast<int>(double),
    which seems more correct.
    
    BUG=315942
    TEST=Connect Pixel to external touch monitor, turn on HUD projection
         by Ctrl + Shift + 9. Move fingers on external monitor to edges
         and lift. Make sure no fingers get stuck at the edges.
    
    Review URL: https://codereview.chromium.org/69653002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@234871 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/events/x/events_x.cc
+++ ui/events/x/events_x.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/events/event_constants.h"
 
+#include <cmath>
 #include <string.h>
 #include <X11/extensions/XInput.h>
 #include <X11/extensions/XInput2.h>
@@ -423,8 +424,8 @@ gfx::Point EventLocationFromNative(const base::NativeEvent& native_event) {
     case GenericEvent: {
       XIDeviceEvent* xievent =
           static_cast<XIDeviceEvent*>(native_event->xcookie.data);
-      return gfx::Point(static_cast<int>(xievent->event_x),
-                        static_cast<int>(xievent->event_y));
+      return gfx::Point(round(xievent->event_x),
+                        round(xievent->event_y));
     }
   }
   return gfx::Point();
