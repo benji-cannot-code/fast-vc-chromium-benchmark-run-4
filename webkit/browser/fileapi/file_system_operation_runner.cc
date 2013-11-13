8138731ed32b5f07448f21c7d5fdfdd800884918
teravest@chromium.org <teravest@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-13 20:09:25 +0000

    Pepper: Move FileIO host from renderer to browser.
    This change is large because it moves QuotaFileIO and PepperFileIOHost all at
    once to the browser process. Some code in the refactored PepperFileIOHost is
    moved from what's provided in FileAPIMessageFilter.
    
    Tested locally with Bastion, From Dust, and Angry Bots.
    
    TBR=jschuh
    BUG=246396
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=232440
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=232547
    
    Review URL: https://codereview.chromium.org/33053002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@234883 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/browser/fileapi/file_system_operation_runner.cc
+++ webkit/browser/fileapi/file_system_operation_runner.cc
@@ -340,7 +340,6 @@ OperationID FileSystemOperationRunner::TouchFile(
 OperationID FileSystemOperationRunner::OpenFile(
     const FileSystemURL& url,
     int file_flags,
-    base::ProcessHandle peer_handle,
     const OpenFileCallback& callback) {
   base::PlatformFileError error = base::PLATFORM_FILE_OK;
   FileSystemOperation* operation =
@@ -349,7 +348,7 @@ OperationID FileSystemOperationRunner::OpenFile(
   OperationHandle handle = BeginOperation(operation, scope.AsWeakPtr());
   if (!operation) {
     DidOpenFile(handle, callback, error, base::kInvalidPlatformFileValue,
-                base::Closure(), base::ProcessHandle());
+                base::Closure());
     return handle.id;
   }
   if (file_flags &
@@ -363,7 +362,7 @@ OperationID FileSystemOperationRunner::OpenFile(
     PrepareForRead(handle.id, url);
   }
   operation->OpenFile(
-      url, file_flags, peer_handle,
+      url, file_flags,
       base::Bind(&FileSystemOperationRunner::DidOpenFile, AsWeakPtr(),
                  handle, callback));
   return handle.id;
@@ -579,17 +578,16 @@ void FileSystemOperationRunner::DidOpenFile(
     const OpenFileCallback& callback,
     base::PlatformFileError rv,
     base::PlatformFile file,
-    const base::Closure& on_close_callback,
-    base::ProcessHandle peer_handle) {
+    const base::Closure& on_close_callback) {
   if (handle.scope) {
     finished_operations_.insert(handle.id);
     base::MessageLoopProxy::current()->PostTask(
         FROM_HERE, base::Bind(&FileSystemOperationRunner::DidOpenFile,
                               AsWeakPtr(), handle, callback, rv, file,
-                              on_close_callback, peer_handle));
+                              on_close_callback));
     return;
   }
-  callback.Run(rv, file, on_close_callback, peer_handle);
+  callback.Run(rv, file, on_close_callback);
   FinishOperation(handle.id);
 }
 
