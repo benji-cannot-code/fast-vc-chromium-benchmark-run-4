adb506acf5a27765c62d6ac8f82c11655ffa7170
hayato@chromium.org <hayato@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-13 12:33:14 +0000

    Revert 161702 "[Shadow DOM]: Empty shadow insertion points shoul..."
    This reverts commit 1d30145f59d2316df295abec1001e3d49f7c555c.
    
    > [Shadow DOM]: Empty shadow insertion points should behave like <shadow><content *leftover*></content></shadow>
    >
    > This is a follow up patch for r161137.
    > We've changed the spec so that we keep a backward compatibility for empty shadow insertion points.
    >
    > The spec side issue is https://www.w3.org/Bugs/Public/show_bug.cgi?id=23767
    >
    > BUG=316513
    >
    > Review URL: https://codereview.chromium.org/59903015
    
    TBR=hayato@chromium.org
    BUG=318057
    
    Review URL: https://codereview.chromium.org/70573003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161914 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/shadow/ElementShadow.cpp
+++ third_party/WebKit/Source/core/dom/shadow/ElementShadow.cpp
@@ -41,34 +41,26 @@ namespace WebCore {
 
 class DistributionPool {
 public:
-    explicit DistributionPool(const ContainerNode&);
-    void clear();
+    explicit DistributionPool(const ContainerNode*);
     ~DistributionPool();
     void distributeTo(InsertionPoint*, ElementShadow*);
-    void populateChildren(const ContainerNode&);
 
 private:
+    void populateChildren(const ContainerNode*);
     void detachNonDistributedNodes();
     Vector<Node*, 32> m_nodes;
     Vector<bool, 32> m_distributed;
 };
 
-inline DistributionPool::DistributionPool(const ContainerNode& parent)
+inline DistributionPool::DistributionPool(const ContainerNode* parent)
 {
-    populateChildren(parent);
+    if (parent)
+        populateChildren(parent);
 }
 
-inline void DistributionPool::clear()
+inline void DistributionPool::populateChildren(const ContainerNode* parent)
 {
-    detachNonDistributedNodes();
-    m_nodes.clear();
-    m_distributed.clear();
-}
-
-inline void DistributionPool::populateChildren(const ContainerNode& parent)
-{
-    clear();
-    for (Node* child = parent.firstChild(); child; child = child->nextSibling()) {
+    for (Node* child = parent->firstChild(); child; child = child->nextSibling()) {
         if (isActiveInsertionPoint(*child)) {
             InsertionPoint* insertionPoint = toInsertionPoint(child);
             for (size_t i = 0; i < insertionPoint->size(); ++i)
@@ -267,12 +259,13 @@ const DestinationInsertionPoints* ElementShadow::destinationInsertionPointsFor(c
 
 void ElementShadow::distribute()
 {
-    Vector<HTMLShadowElement*, 32> shadowInsertionPoints;
-    DistributionPool pool(*host());
-
     host()->setNeedsStyleRecalc();
+    const ContainerNode* poolContainer = host();
+    Vector<HTMLShadowElement*, 32> shadowInsertionPoints;
 
     for (ShadowRoot* root = youngestShadowRoot(); root; root = root->olderShadowRoot()) {
+        DistributionPool pool(poolContainer);
+
         HTMLShadowElement* shadowInsertionPoint = 0;
         const Vector<RefPtr<InsertionPoint> >& insertionPoints = root->descendantInsertionPoints();
         for (size_t i = 0; i < insertionPoints.size(); ++i) {
@@ -288,13 +281,9 @@ void ElementShadow::distribute()
                     shadow->setNeedsDistributionRecalc();
             }
         }
-        if (shadowInsertionPoint) {
+        if (shadowInsertionPoint)
             shadowInsertionPoints.append(shadowInsertionPoint);
-            if (shadowInsertionPoint->hasChildNodes())
-                pool.populateChildren(*shadowInsertionPoint);
-        } else if (!root->isOldest()) {
-            pool.clear();
-        }
+        poolContainer = shadowInsertionPoint;
     }
 
     for (size_t i = shadowInsertionPoints.size(); i > 0; --i) {
@@ -307,8 +296,7 @@ void ElementShadow::distribute()
             distributeNodeChildrenTo(shadowInsertionPoint, root->olderShadowRoot());
             root->olderShadowRoot()->setShadowInsertionPointOfYoungerShadowRoot(shadowInsertionPoint);
         } else if (root->isOldest()) {
-            if (shadowInsertionPoint->hasChildNodes())
-                pool.populateChildren(*shadowInsertionPoint);
+            DistributionPool pool(shadowInsertionPoint);
             pool.distributeTo(shadowInsertionPoint, this);
         }
         if (ElementShadow* shadow = shadowWhereNodeCanBeDistributed(*shadowInsertionPoint))
@@ -324,7 +312,7 @@ void ElementShadow::didDistributeNode(const Node* node, InsertionPoint* insertio
 
 void ElementShadow::distributeNodeChildrenTo(InsertionPoint* insertionPoint, ContainerNode* containerNode)
 {
-    DistributionPool pool(*containerNode);
+    DistributionPool pool(containerNode);
     pool.distributeTo(insertionPoint, this);
 }
 
