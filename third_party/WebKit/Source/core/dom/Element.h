e49ab9ffadf75c435a4675675a26055573f40807
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-13 07:54:52 +0000

    Rework pseudoStyleCacheIsInvalid to be simpler and more efficient
    Simplify this function using iterators and passing the cache instead of
    the style (which required sanity ASSERTs). Passing the cache also makes
    the call site more clear.
    
    Review URL: https://codereview.chromium.org/69923002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161887 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Element.h
+++ third_party/WebKit/Source/core/dom/Element.h
@@ -78,6 +78,8 @@ enum SpellcheckAttributeState {
     SpellcheckAttributeDefault
 };
 
+typedef Vector<RefPtr<RenderStyle>, 4> PseudoStyleCache;
+
 class Element : public ContainerNode {
 public:
     static PassRefPtr<Element> create(const QualifiedName&, Document*);
@@ -618,7 +620,7 @@ private:
     virtual void formatForDebugger(char* buffer, unsigned length) const;
 #endif
 
-    bool pseudoStyleCacheIsInvalid(const RenderStyle* currentStyle, RenderStyle* newStyle);
+    bool pseudoStyleCacheIsInvalid(const PseudoStyleCache*, RenderStyle* newStyle);
 
     void cancelFocusAppearanceUpdate();
 
