6a8e93c86fd4cbfa6b895f004aca2fd3cf661800
dstockwell@chromium.org <dstockwell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-13 08:36:09 +0000

    Web Animations: Move servicing logic from FrameView to Document
    When running animations on the compositor we won't be receiving regular
    frame callbacks and will need to trigger servicing separately, for
    example to satisfy getComputedStyle.
    
    Review URL: https://codereview.chromium.org/71213002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161892 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Document.h
+++ third_party/WebKit/Source/core/dom/Document.h
@@ -631,6 +631,9 @@ public:
     void setCSSTarget(Element*);
     Element* cssTarget() const { return m_cssTarget; }
 
+    void serviceAnimations(double monotonicAnimationStartTime);
+    void dispatchAnimationEvents();
+
     void scheduleStyleRecalc();
     void unscheduleStyleRecalc();
     bool hasPendingStyleRecalc() const;
--- third_party/WebKit/Source/core/frame/FrameView.cpp
+++ third_party/WebKit/Source/core/frame/FrameView.cpp
@@ -31,8 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLNames.h"
 #include "RuntimeEnabledFeatures.h"
 #include "core/accessibility/AXObjectCache.h"
-#include "core/animation/AnimationClock.h"
-#include "core/animation/DocumentTimeline.h"
 #include "core/css/FontFaceSet.h"
 #include "core/css/resolver/StyleResolver.h"
 #include "core/dom/DocumentMarkerController.h"
@@ -2051,15 +2049,9 @@ void FrameView::serviceScriptedAnimations(double monotonicAnimationStartTime)
         frame->view()->serviceScrollAnimations();
         if (!RuntimeEnabledFeatures::webAnimationsCSSEnabled())
             frame->animation().serviceAnimations();
-        if (RuntimeEnabledFeatures::webAnimationsEnabled()) {
-            frame->document()->animationClock().updateTime(monotonicAnimationStartTime);
-            bool didTriggerStyleRecalc = frame->document()->timeline()->serviceAnimations();
-            didTriggerStyleRecalc |= frame->document()->transitionTimeline()->serviceAnimations();
-            if (!didTriggerStyleRecalc)
-                frame->document()->animationClock().unfreeze();
-            frame->document()->timeline()->dispatchEvents();
-            frame->document()->transitionTimeline()->dispatchEvents();
-        }
+
+        frame->document()->serviceAnimations(monotonicAnimationStartTime);
+        frame->document()->dispatchAnimationEvents();
     }
 
     Vector<RefPtr<Document> > documents;
