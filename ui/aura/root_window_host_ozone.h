652fcfd73b5c2be55133656c3707e043b8e719b9
spang@chromium.org <spang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-13 20:24:53 +0000

    Add missing insets.h include to root_window_host_ozone.h
    TBR=sadrul@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/71853003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@234890 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/root_window_host_ozone.h
+++ ui/aura/root_window_host_ozone.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_loop.h"
 #include "ui/aura/window_tree_host.h"
 #include "ui/events/ozone/event_factory_ozone.h"
+#include "ui/gfx/insets.h"
 #include "ui/gfx/rect.h"
 
 namespace aura {
