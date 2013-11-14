f322c2e2679d378035bba68e29ad0aa84568cd27
bartfab@chromium.org <bartfab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-14 14:14:19 +0000

    Clean up and simplify UserImageLoader
    This CL simplifies the UserImageLoader. A foreground_task_runner_ and a
    background_task_runner_ are set during construction, providing well-
    defined task runners for all tasks. There no longer is a need for locking
    as all callbacks from the ImageDecoders arrive via the same task runner.
    
    The CL also adds a method that processes image data already present in
    memory, in preparation for policy-set avatars and wallpapers.
    
    BUG=152957,152959,220418
    TEST=Manual
    
    Review URL: https://codereview.chromium.org/69293002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235136 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/user_image_manager_impl.h
+++ chrome/browser/chromeos/login/user_image_manager_impl.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/basictypes.h"
+#include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
