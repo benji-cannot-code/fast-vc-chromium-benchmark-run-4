949427b3c1c041118d0cbe2da5ebe489dd7b43e7
digit@chromium.org <digit@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-14 19:34:06 +0000

    android: forwader2: Simplify Forwarder implementation.
    This patch does several things to the Forwarder class used in the 'forwarder2'
    tool:
    
    - Add a Stop() method to gently ask the forwarder to shut-down. This stops
      the forwarder from listening from incoming data, but keeps it running
      until it could flush its buffer properly, to avoid packet loss.
    
    - Simplify / refactor the buffer management code inside a Forwarder
      instance, to make the state of its buffer, and their transitions,
      more explict.
    
    BUG=313809
    R=pliard@chromium.org
    
    Review URL: https://codereview.chromium.org/61793013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235213 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/android/forwarder2/forwarder.h
+++ tools/android/forwarder2/forwarder.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define TOOLS_ANDROID_FORWARDER2_FORWARDER_H_
 
 #include "base/memory/scoped_ptr.h"
-#include "base/threading/thread.h"
 
 namespace forwarder2 {
 
