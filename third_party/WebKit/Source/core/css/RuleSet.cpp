7e9f2ebe278888e88f53ca7b9ca078e218a85b7f
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-14 02:56:25 +0000

    Add some shrinkToFit calls missing in recent patches.
    Review URL: https://codereview.chromium.org/59883012
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161968 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/RuleSet.cpp
+++ third_party/WebKit/Source/core/css/RuleSet.cpp
@@ -463,6 +463,10 @@ void RuleSet::compactRules()
     m_pageRules.shrinkToFit();
     m_viewportRules.shrinkToFit();
     m_fontFaceRules.shrinkToFit();
+    m_keyframesRules.shrinkToFit();
+    m_hostRules.shrinkToFit();
+    m_treeBoundaryCrossingRules.shrinkToFit();
+    m_shadowDistributedRules.shrinkToFit();
 }
 
 } // namespace WebCore
