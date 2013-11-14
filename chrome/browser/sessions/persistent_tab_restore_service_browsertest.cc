bfc10c3ef5f112d88096c20c7e96a3d1b394912c
samarth@chromium.org <samarth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-14 03:20:37 +0000

    InstantExtended: update tests for Instant Extended (part 2).
    Update tests that were using the wrong test for checking the NTP URL. Once
    Instant Extended is turned on, IsNTPURL will not return true for
    "chrome://newtab", so the tests expecting that particular literal must be
    updated. (Ultimately, this should be cleaned up so there is no distinction
    between chrome://newtab and chrome-search://local-ntp.. but that will need to
    happen later.)
    
    (Note that this is split off from http://crrev.com/24733003.)
    
    BUG=297915
    
    Review URL: https://codereview.chromium.org/58903004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235029 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sessions/persistent_tab_restore_service_browsertest.cc
+++ chrome/browser/sessions/persistent_tab_restore_service_browsertest.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/sequenced_worker_pool.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/search/search.h"
 #include "chrome/browser/sessions/session_service.h"
 #include "chrome/browser/sessions/session_service_factory.h"
 #include "chrome/browser/sessions/session_types.h"
@@ -737,9 +736,9 @@ TEST_F(PersistentTabRestoreServiceTest, PruneEntries) {
   EXPECT_EQ(max_entries + 1, service_->entries().size());
   PruneEntries();
   EXPECT_EQ(max_entries, service_->entries().size());
-  EXPECT_TRUE(chrome::IsNTPURL(
-      static_cast<Tab*>(service_->entries().front())->
-          navigations[0].virtual_url(), profile()));
+  EXPECT_EQ(GURL(chrome::kChromeUINewTabURL),
+            static_cast<Tab*>(service_->entries().front())->
+            navigations[0].virtual_url());
 
   // Don't prune NTPs that have multiple navigations.
   // (Erase the last NTP first.)
@@ -753,9 +752,9 @@ TEST_F(PersistentTabRestoreServiceTest, PruneEntries) {
   EXPECT_EQ(max_entries, service_->entries().size());
   PruneEntries();
   EXPECT_EQ(max_entries, service_->entries().size());
-  EXPECT_TRUE(chrome::IsNTPURL(
-      static_cast<Tab*>(service_->entries().front())->
-          navigations[1].virtual_url(), profile()));
+  EXPECT_EQ(GURL(chrome::kChromeUINewTabURL),
+            static_cast<Tab*>(service_->entries().front())->
+            navigations[1].virtual_url());
 }
 
 // Regression test for crbug.com/106082
