1ae4bf7c453f717ec4cecfc5d6dd0fb8195a9aaf
eseidel@chromium.org <eseidel@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-14 02:51:12 +0000

    Remove RenderingConfiguration, it's dead code
    The parallel layout effort will wait for another day.
    This code never really ended up being used
    and so I'm removing it.
    
    Added in:
    https://chromiumcodereview.appspot.com/23866009
    
    R=abarth,ojan
    BUG=289678
    
    Review URL: https://codereview.chromium.org/59813006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161966 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/core.gypi
+++ third_party/WebKit/Source/core/core.gypi
@@ -1562,8 +1562,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'rendering/RenderWidget.cpp',
             'rendering/RenderWordBreak.cpp',
             'rendering/RenderWordBreak.h',
-            'rendering/RenderingConfiguration.cpp',
-            'rendering/RenderingConfiguration.h',
             'rendering/RootInlineBox.cpp',
             'rendering/ScrollBehavior.cpp',
             'rendering/SubtreeLayoutScope.cpp',
--- third_party/WebKit/Source/core/rendering/RenderView.h
+++ third_party/WebKit/Source/core/rendering/RenderView.h
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/rendering/LayoutIndicator.h"
 #include "core/rendering/LayoutState.h"
 #include "core/rendering/RenderBlockFlow.h"
-#include "core/rendering/RenderingConfiguration.h"
 #include "platform/PODFreeListArena.h"
 #include "wtf/OwnPtr.h"
 
@@ -100,14 +99,6 @@ public:
     IntRect selectionBounds(bool clipToVisibleContent = true) const;
     void selectionStartEnd(int& startPos, int& endPos) const;
     void repaintSelection() const;
-
-    void updateConfiguration();
-    const RenderingConfiguration& configuration()
-    {
-        // If we're not inLayout(), then the configuration might be out of date.
-        ASSERT(LayoutIndicator::inLayout());
-        return m_configuration;
-    }
 
     virtual void absoluteRects(Vector<IntRect>&, const LayoutPoint& accumulatedOffset) const;
     virtual void absoluteQuads(Vector<FloatQuad>&, bool* wasFixed) const;
@@ -285,9 +276,6 @@ private:
 
     RenderObject* m_selectionStart;
     RenderObject* m_selectionEnd;
-
-    // Please use the configuration() accessor instead of accessing this member directly.
-    RenderingConfiguration m_configuration;
 
     int m_selectionStartPos;
     int m_selectionEndPos;
