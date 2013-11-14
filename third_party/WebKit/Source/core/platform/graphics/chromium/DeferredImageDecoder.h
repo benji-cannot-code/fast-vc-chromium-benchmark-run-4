9c655111c2050707cee6d52626040f6601055254
halcanary@google.com <halcanary@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-14 04:52:41 +0000

    Remove DeferredImageDecoder::createResizedLazyDecodingBitmap
    Motivation:
        This code is not used.  Removing it will make it easier to make
        changes to Lazy SkPixelRefs in the future.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/71663002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161982 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/graphics/chromium/DeferredImageDecoder.h
+++ third_party/WebKit/Source/core/platform/graphics/chromium/DeferredImageDecoder.h
@@ -49,8 +49,6 @@ public:
 
     static bool isLazyDecoded(const SkBitmap&);
 
-    static SkBitmap createResizedLazyDecodingBitmap(const SkBitmap&, const SkISize& scaledSize, const SkIRect& scaledSubset);
-
     static void setEnabled(bool);
 
     String filenameExtension() const;
