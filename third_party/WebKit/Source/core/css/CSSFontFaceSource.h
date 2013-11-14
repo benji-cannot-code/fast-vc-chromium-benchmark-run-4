da0654002e2291739c365a0f83f63a2462e1f5bb
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-14 04:29:45 +0000

    Teach FontDescription about synthetics.
    We pass synthetic bold/italic bits separately now, which
    means that FontDescription doesn't actually describe the
    font data we desire. We should fix that.
    
    R=eae,ksakamoto
    BUG=315783
    
    Review URL: https://codereview.chromium.org/63523007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161977 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/CSSFontFaceSource.h
+++ third_party/WebKit/Source/core/css/CSSFontFaceSource.h
@@ -61,7 +61,7 @@ public:
     virtual void didStartFontLoad(FontResource*) OVERRIDE;
     virtual void fontLoaded(FontResource*);
 
-    PassRefPtr<SimpleFontData> getFontData(const FontDescription&, bool syntheticBold, bool syntheticItalic, CSSFontSelector*);
+    PassRefPtr<SimpleFontData> getFontData(const FontDescription&, CSSFontSelector*);
 
     void pruneTable();
 
