43febb137338b33c481bf9d5858434bbcf8be88b
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-14 20:53:08 +0000

    linux_aura: Call Env::CreateInstance() earlier in chrome_browser_main.cc.
    This needs to happen before we create a profile because profiles can
    bring up error message dialogs.
    
    BUG=317974
    R=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/72133002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235227 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_browser_main.cc
+++ chrome/browser/chrome_browser_main.cc
@@ -1266,6 +1266,12 @@ int ChromeBrowserMainParts::PreMainMessageLoopRunImpl() {
 #endif  // defined(OS_WIN)
   }
 
+#if defined(USE_AURA)
+  // Env creates the compositor. Aura widgets need the compositor to be created
+  // before they can be initialized by the browser.
+  aura::Env::CreateInstance();
+#endif
+
   // Profile creation ----------------------------------------------------------
 
   // Called before CreateProfile because creating the profile can trigger
@@ -1531,12 +1537,6 @@ int ChromeBrowserMainParts::PreMainMessageLoopRunImpl() {
 
   if (!parsed_command_line().HasSwitch(switches::kDisableComponentUpdate))
     RegisterComponentsForUpdate(parsed_command_line());
-
-#if defined(USE_AURA)
-  // Env creates the compositor. Aura widgets need the compositor to be created
-  // before they can be initialized by the browser.
-  aura::Env::CreateInstance();
-#endif
 
   if (browser_creator_->Start(parsed_command_line(), base::FilePath(),
                               profile_, last_opened_profiles, &result_code)) {
