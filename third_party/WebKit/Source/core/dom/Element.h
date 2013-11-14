6d065316671c26279ea4105d394e1117ec6ab890
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-14 10:19:48 +0000

    Revert "Rework pseudoStyleCacheIsInvalid to be simpler and more efficient"
    This reverts commit http://src.chromium.org/viewvc/blink/?view=revision&revision=161887
    
    This whole pseudo style cache system doesn't work like how I thought. It needs more investigation.
    
    BUG=319040
    TBR=inferno@chromium.org
    
    Review URL: https://codereview.chromium.org/61793020
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162004 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Element.h
+++ third_party/WebKit/Source/core/dom/Element.h
@@ -78,8 +78,6 @@ enum SpellcheckAttributeState {
     SpellcheckAttributeDefault
 };
 
-typedef Vector<RefPtr<RenderStyle>, 4> PseudoStyleCache;
-
 class Element : public ContainerNode {
 public:
     static PassRefPtr<Element> create(const QualifiedName&, Document*);
@@ -620,7 +618,7 @@ private:
     virtual void formatForDebugger(char* buffer, unsigned length) const;
 #endif
 
-    bool pseudoStyleCacheIsInvalid(const PseudoStyleCache*, RenderStyle* newStyle);
+    bool pseudoStyleCacheIsInvalid(const RenderStyle* currentStyle, RenderStyle* newStyle);
 
     void cancelFocusAppearanceUpdate();
 
