20bb9a9b945333e11fbac89cc76655296b17b4e6
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-14 17:08:36 +0000

    Mojo: Add a MOJO_ARRAYSIZE, corresponding to Chromium's arraysize.
    (Like the other things in the file, so that mojo/public doesn't have to
    depend on base.)
    
    R=darin@chromium.org, davemoore
    
    Review URL: https://codereview.chromium.org/70273009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235166 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/public/system/macros.h
+++ mojo/public/system/macros.h
@@ -6,10 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MOJO_PUBLIC_SYSTEM_MACROS_H_
 #define MOJO_PUBLIC_SYSTEM_MACROS_H_
 
+#include <stddef.h>
+
 #ifdef __cplusplus
 
-// Annotate a virtual method indicating it must be overriding a virtual
-// method in the parent class.
+// Annotate a virtual method indicating it must be overriding a virtual method
+// in the parent class.
 // Use like:
 //   virtual void foo() OVERRIDE;
 #if defined(_MSC_VER) || defined(__clang__)
@@ -18,16 +20,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define MOJO_OVERRIDE
 #endif
 
-// A macro to disallow the copy constructor and operator= functions
-// This should be used in the private: declarations for a class
+// A macro to disallow the copy constructor and operator= functions.
+// This should be used in the private: declarations for a class.
 #define MOJO_DISALLOW_COPY_AND_ASSIGN(TypeName) \
-  TypeName(const TypeName&);                    \
-  void operator=(const TypeName&)
+    TypeName(const TypeName&); \
+    void operator=(const TypeName&)
 
 // Used to assert things at compile time.
 namespace mojo { template <bool> struct CompileAssert {}; }
 #define MOJO_COMPILE_ASSERT(expr, msg) \
     typedef ::mojo::CompileAssert<(bool(expr))> msg[bool(expr) ? 1 : -1]
+
+// Use to calculate the number of elements in an array.
+// (See |arraysize()| in Chromium's base/basictypes.h for more details.)
+namespace mojo {
+template <typename T, size_t N>
+char (&ArraySizeHelper(T (&array)[N]))[N];
+#if !defined(_MSC_VER)
+template <typename T, size_t N>
+char (&ArraySizeHelper(const T (&array)[N]))[N];
+#endif
+}  // namespace mojo
+#define MOJO_ARRAYSIZE(array) (sizeof(::mojo::ArraySizeHelper(array)))
 
 #endif  // __cplusplus
 
