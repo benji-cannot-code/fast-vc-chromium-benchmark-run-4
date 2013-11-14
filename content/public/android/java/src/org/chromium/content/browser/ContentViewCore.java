bf60becafc0a74c36aceea2c4ebeee8709320740
michaelbai@chromium.org <michaelbai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-14 02:15:10 +0000

    Prevent media from being loading from network when AwSettings.setBlockNetworkLoads is true
    BUG=313463
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/52463004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235017 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -3222,6 +3222,11 @@ public class ContentViewCore
         return mContentViewClient.getContentVideoViewClient();
     }
 
+    @CalledByNative
+    private boolean shouldBlockMediaRequest(String url) {
+        return mContentViewClient.shouldBlockMediaRequest(url);
+    }
+
     private native void nativeOnJavaContentViewCoreDestroyed(int nativeContentViewCoreImpl);
 
     private native void nativeLoadUrl(
