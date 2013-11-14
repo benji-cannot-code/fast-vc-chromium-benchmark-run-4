487f41ca80de455b02dc754cdd7386ca71a88874
asanka@chromium.org <asanka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-14 03:21:16 +0000

    [Downloads] Use a WeakPtrFactory in ChromeDownloadManagerDelegate.
    ChromeDownloadManagerDelegate is reference counted, and can outlive its
    associated Profile/BrowserContext. Since it issues potentially longer
    lived tasks, those tasks could complete and issue completion callbacks
    after Profile shutdown. Use a WeakPtrFactory to invalidate those
    callbacks so that we don't accidentally touch objects that were owned
    by the defunct Profile.
    
    In http://crrev.com/69793006 ChromeDownloadManagerDelegate will become
    owned by DownloadService and will no longer require being reference
    counted.
    
    BUG=317913
    
    Review URL: https://codereview.chromium.org/59903021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235030 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/chrome_download_manager_delegate.cc
+++ chrome/browser/download/chrome_download_manager_delegate.cc
@@ -196,7 +196,8 @@ bool IsOpenInBrowserPreferreredForFile(const base::FilePath& path) {
 ChromeDownloadManagerDelegate::ChromeDownloadManagerDelegate(Profile* profile)
     : profile_(profile),
       next_download_id_(content::DownloadItem::kInvalidId),
-      download_prefs_(new DownloadPrefs(profile)) {
+      download_prefs_(new DownloadPrefs(profile)),
+      weak_ptr_factory_(this) {
 }
 
 ChromeDownloadManagerDelegate::~ChromeDownloadManagerDelegate() {
@@ -208,6 +209,7 @@ void ChromeDownloadManagerDelegate::SetDownloadManager(DownloadManager* dm) {
 
 void ChromeDownloadManagerDelegate::Shutdown() {
   download_prefs_.reset();
+  weak_ptr_factory_.InvalidateWeakPtrs();
 }
 
 void ChromeDownloadManagerDelegate::SetNextId(uint32 next_id) {
@@ -252,7 +254,7 @@ bool ChromeDownloadManagerDelegate::DetermineDownloadTarget(
     const content::DownloadTargetCallback& callback) {
   DownloadTargetDeterminer::CompletionCallback target_determined_callback =
       base::Bind(&ChromeDownloadManagerDelegate::OnDownloadTargetDetermined,
-                 this,
+                 weak_ptr_factory_.GetWeakPtr(),
                  download->GetId(),
                  callback);
   DownloadTargetDeterminer::Start(
@@ -311,7 +313,7 @@ bool ChromeDownloadManagerDelegate::IsDownloadReadyForCompletion(
           item,
           base::Bind(
               &ChromeDownloadManagerDelegate::CheckClientDownloadDone,
-              this,
+              weak_ptr_factory_.GetWeakPtr(),
               item->GetId()));
       return false;
     }
@@ -339,7 +341,7 @@ bool ChromeDownloadManagerDelegate::ShouldCompleteDownload(
     const base::Closure& user_complete_callback) {
   return IsDownloadReadyForCompletion(item, base::Bind(
       &ChromeDownloadManagerDelegate::ShouldCompleteDownloadInternal,
-      this, item->GetId(), user_complete_callback));
+      weak_ptr_factory_.GetWeakPtr(), item->GetId(), user_complete_callback));
 }
 
 bool ChromeDownloadManagerDelegate::ShouldOpenDownload(
--- chrome/browser/download/chrome_download_manager_delegate.h
+++ chrome/browser/download/chrome_download_manager_delegate.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/hash_tables.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "chrome/browser/download/download_path_reservation_tracker.h"
 #include "chrome/browser/download/download_target_determiner_delegate.h"
 #include "chrome/browser/download/download_target_info.h"
@@ -183,6 +184,8 @@ class ChromeDownloadManagerDelegate
   CrxInstallerMap crx_installers_;
 
   content::NotificationRegistrar registrar_;
+
+  base::WeakPtrFactory<ChromeDownloadManagerDelegate> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(ChromeDownloadManagerDelegate);
 };
