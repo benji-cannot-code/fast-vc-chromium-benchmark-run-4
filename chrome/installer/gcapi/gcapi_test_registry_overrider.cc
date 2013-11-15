79f358a929a82d725651fead068eb7a264da83c1
tommycli@chromium.org <tommycli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-15 04:48:37 +0000

    Revert "Revert 234367 "Base: Make RegistryOverrideManager support sharde...""
    This reverts commit 98ee7dd64463c42a076782c923330107e60b8fad.
    
    Reapply of https://codereview.chromium.org/57423008 now that a blocking issue has been fixed.
    
    BUG=314800
    TBR=grt,owners
    
    Review URL: https://codereview.chromium.org/68743005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235284 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/gcapi/gcapi_test_registry_overrider.cc
+++ chrome/installer/gcapi/gcapi_test_registry_overrider.cc
@@ -12,12 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 GCAPITestRegistryOverrider::GCAPITestRegistryOverrider() {
   // Override keys - this is undone during destruction.
-  string16 hkcu_override = base::StringPrintf(
-      L"hkcu_override\\%ls", ASCIIToWide(base::GenerateGUID()));
-  override_manager_.OverrideRegistry(HKEY_CURRENT_USER, hkcu_override);
-  string16 hklm_override = base::StringPrintf(
-      L"hklm_override\\%ls", ASCIIToWide(base::GenerateGUID()));
-  override_manager_.OverrideRegistry(HKEY_LOCAL_MACHINE, hklm_override);
+  override_manager_.OverrideRegistry(HKEY_CURRENT_USER, L"hkcu_override");
+  override_manager_.OverrideRegistry(HKEY_LOCAL_MACHINE, L"hklm_override");
 }
 
 GCAPITestRegistryOverrider::~GCAPITestRegistryOverrider() {
