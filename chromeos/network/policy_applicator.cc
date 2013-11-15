ebae07e725ab70ef81cd959810cc855d47ef122b
pneubeck@chromium.org <pneubeck@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-15 20:19:46 +0000

    ONC policy: Support SSID and Security change.
    If the SSID of a ONC policy is changed without changing the GUID, this lead to a partial policy application where everything except the SSID is changed.
    This happened because Shill doesn't allow changes of the SSID property after the network creation but the policy application code didn't respect that.
    
    With this change, it's ensured that the respective network configuration (profile entry), is deleted and rewritten on each SSID change.
    
    The same applies to the Security type of the network.
    
    BUG=319362
    R=bartfab@chromium.org
    
    Review URL: https://codereview.chromium.org/68213018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235410 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/network/policy_applicator.cc
+++ chromeos/network/policy_applicator.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/location.h"
 #include "base/logging.h"
+#include "base/memory/scoped_ptr.h"
 #include "base/stl_util.h"
 #include "base/values.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
@@ -168,22 +169,32 @@ void PolicyApplicator::GetEntryCallback(
       VLOG(1) << "Not updating existing managed configuration with guid "
               << new_guid << " because the policy didn't change.";
     } else {
-      // Delete the entry to ensure that no old configuration remains.
-      // Don't do this if a policy is reapplied (e.g. after reboot) or updated
-      // (i.e. the GUID didn't change), in order to keep implicit state of
-      // Shill like "connected successfully before".
-      if (old_guid == new_guid) {
+      const base::DictionaryValue* user_settings =
+          ui_data ? ui_data->user_settings() : NULL;
+      scoped_ptr<base::DictionaryValue> new_shill_properties =
+          policy_util::CreateShillConfiguration(
+              profile_, new_guid, new_policy, user_settings);
+      // A new policy has to be applied to this profile entry. In order to keep
+      // implicit state of Shill like "connected successfully before", keep the
+      // entry if a policy is reapplied (e.g. after reboot) or is updated.
+      // However, some Shill properties are used to identify the network and
+      // cannot be modified after initial configuration, so we have to delete
+      // the profile entry in these cases. Also, keeping Shill's state if the
+      // SSID changed might not be a good idea anyways. If the policy GUID
+      // changed, or there was no policy before, we delete the entry at first to
+      // ensure that no old configuration remains.
+      if (old_guid == new_guid &&
+          shill_property_util::DoIdentifyingPropertiesMatch(
+              *new_shill_properties, entry_properties)) {
         VLOG(1) << "Updating previously managed configuration with the "
                 << "updated policy " << new_guid << ".";
       } else {
+        VLOG(1) << "Deleting profile entry before writing new policy "
+                << new_guid << " because of identifying properties changed.";
         DeleteEntry(entry);
       }
 
-      const base::DictionaryValue* user_settings =
-          ui_data ? ui_data->user_settings() : NULL;
-
-      // Write the new configuration.
-      CreateAndWriteNewShillConfiguration(new_guid, *new_policy, user_settings);
+      WriteNewShillConfiguration(*new_shill_properties, *new_policy);
       remaining_policies_.erase(new_guid);
     }
   } else if (was_managed) {
@@ -197,7 +208,6 @@ void PolicyApplicator::GetEntryCallback(
   } else {
     // The entry wasn't managed and doesn't match any current policy. Global
     // network settings have to be applied.
-
     base::DictionaryValue shill_properties_to_update;
     GetPropertiesForUnmanagedEntry(entry_properties,
                                    &shill_properties_to_update);
@@ -220,10 +230,9 @@ void PolicyApplicator::DeleteEntry(const std::string& entry) {
       base::Bind(&LogErrorMessage, FROM_HERE));
 }
 
-void PolicyApplicator::CreateAndWriteNewShillConfiguration(
-    const std::string& guid,
-    const base::DictionaryValue& policy,
-    const base::DictionaryValue* user_settings) {
+void PolicyApplicator::WriteNewShillConfiguration(
+    const base::DictionaryValue& shill_dictionary,
+    const base::DictionaryValue& policy) {
   // Ethernet (non EAP) settings, like GUID or UIData, cannot be stored per
   // user. Abort in that case.
   std::string type;
@@ -240,10 +249,7 @@ void PolicyApplicator::CreateAndWriteNewShillConfiguration(
       return;
   }
 
-  scoped_ptr<base::DictionaryValue> shill_dictionary =
-      policy_util::CreateShillConfiguration(
-          profile_, guid, &policy, user_settings);
-  handler_->CreateConfigurationFromPolicy(*shill_dictionary);
+  handler_->CreateConfigurationFromPolicy(shill_dictionary);
 }
 
 void PolicyApplicator::GetPropertiesForUnmanagedEntry(
@@ -305,8 +311,9 @@ void PolicyApplicator::ApplyRemainingPolicies() {
     VLOG(1) << "Creating new configuration managed by policy " << *it
             << " in profile " << profile_.ToDebugString() << ".";
 
-    CreateAndWriteNewShillConfiguration(
-        *it, *policy, NULL /* no user settings */);
+    scoped_ptr<base::DictionaryValue> shill_dictionary =
+        policy_util::CreateShillConfiguration(profile_, *it, policy, NULL);
+    WriteNewShillConfiguration(*shill_dictionary, *policy);
   }
 }
 
