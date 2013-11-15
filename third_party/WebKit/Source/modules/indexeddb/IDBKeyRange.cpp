9fbb74928eed1aa56b408f47a059195ae0ccbe27
alecflett@chromium.org <alecflett@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-15 19:23:26 +0000

    Remove dead code from IDB (blink side)
    BUG=
    
    Review URL: https://codereview.chromium.org/73313002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162129 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/indexeddb/IDBKeyRange.cpp
+++ third_party/WebKit/Source/modules/indexeddb/IDBKeyRange.cpp
@@ -35,12 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-PassRefPtr<IDBKeyRange> IDBKeyRange::create(PassRefPtr<IDBKey> prpKey)
-{
-    RefPtr<IDBKey> key = prpKey;
-    return adoptRef(new IDBKeyRange(key, key, LowerBoundClosed, UpperBoundClosed));
-}
-
 PassRefPtr<IDBKeyRange> IDBKeyRange::fromScriptValue(ExecutionContext* context, const ScriptValue& value, ExceptionState& exceptionState)
 {
     DOMRequestState requestState(context);
@@ -148,16 +142,6 @@ PassRefPtr<IDBKeyRange> IDBKeyRange::bound(ExecutionContext* context, const Scri
     }
 
     return IDBKeyRange::create(lower, upper, lowerOpen ? LowerBoundOpen : LowerBoundClosed, upperOpen ? UpperBoundOpen : UpperBoundClosed);
-}
-
-bool IDBKeyRange::isOnlyKey() const
-{
-    if (m_lowerType != LowerBoundClosed || m_upperType != UpperBoundClosed)
-        return false;
-
-    ASSERT(m_lower);
-    ASSERT(m_upper);
-    return m_lower->isEqual(m_upper.get());
 }
 
 } // namespace WebCore
