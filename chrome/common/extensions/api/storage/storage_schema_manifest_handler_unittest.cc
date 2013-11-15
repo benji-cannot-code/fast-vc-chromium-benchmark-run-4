ee85deda6a97db55389d715e3b3001b388631ca1
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-15 16:50:00 +0000

    Fix StorageSchemaManifestHandlerTest.Validate on linux_redux.
    Some of these tests must be skipped when ENABLE_CONFIGURATION_POLICY is not set,
    since the storage schema isn't parsed on those builds.
    
    BUG=316972
    
    Review URL: https://codereview.chromium.org/61263009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235352 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/api/storage/storage_schema_manifest_handler_unittest.cc
+++ chrome/common/extensions/api/storage/storage_schema_manifest_handler_unittest.cc
@@ -92,6 +92,7 @@ TEST_F(StorageSchemaManifestHandlerTest, Validate) {
   permissions.AppendString("storage");
   manifest_.Set("permissions", permissions.DeepCopy());
 
+#if defined(ENABLE_CONFIGURATION_POLICY)
   // Absolute path.
   manifest_.SetString("storage.managed_schema", "/etc/passwd");
   EXPECT_FALSE(Validates(""));
@@ -137,6 +138,7 @@ TEST_F(StorageSchemaManifestHandlerTest, Validate) {
       "  \"type\": \"object\","
       "  \"additionalProperties\": {}"
       "}"));
+#endif
 
   // All good now.
   EXPECT_TRUE(Validates(
