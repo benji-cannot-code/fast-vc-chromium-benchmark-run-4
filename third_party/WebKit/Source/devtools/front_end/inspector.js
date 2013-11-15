4f6541ffb7bfface67d9f6e74a757d0b7d5e4440
lushnikov@chromium.org <lushnikov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-15 05:33:25 +0000

    DevTools: ability to click close button in docked frontend multiple times
    This patch makes close button in docked devtools frontend clickable
    multiple times which might be useful whenever beforeunload event handler
    will be added to frontend.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/72823002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162062 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/inspector.js
+++ third_party/WebKit/Source/devtools/front_end/inspector.js
@@ -237,8 +237,7 @@ var WebInspector = {
 }
 
 WebInspector.Events = {
-    InspectorLoaded: "InspectorLoaded",
-    InspectorClosing: "InspectorClosing"
+    InspectorLoaded: "InspectorLoaded"
 }
 
 {(function parseQueryParameters()
@@ -514,10 +513,6 @@ WebInspector.windowResize = function(event)
 
 WebInspector.close = function(event)
 {
-    if (this._isClosing)
-        return;
-    this._isClosing = true;
-    this.notifications.dispatchEventToListeners(WebInspector.Events.InspectorClosing);
     InspectorFrontendHost.closeWindow();
 }
 
