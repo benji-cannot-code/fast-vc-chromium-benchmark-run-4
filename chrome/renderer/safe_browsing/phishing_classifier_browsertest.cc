3402c4c56098c77af14dabfcb50edba125b33d78
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-15 20:56:36 +0000

    Disable PhishingClassifierTest.DisableDetection on Mac 10.8. No idea why it fails just there flakily.
    R=avi@chromium.org
    
    Review URL: https://codereview.chromium.org/74123002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235420 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/safe_browsing/phishing_classifier_browsertest.cc
+++ chrome/renderer/safe_browsing/phishing_classifier_browsertest.cc
@@ -29,6 +29,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "url/gurl.h"
 
+#if defined(OS_MACOSX)
+#include "base/mac/mac_util.h"
+#endif
+
 using ::testing::AllOf;
 using ::testing::Contains;
 using ::testing::Not;
@@ -270,6 +274,14 @@ IN_PROC_BROWSER_TEST_F(PhishingClassifierTest, TestClassification) {
 }
 
 IN_PROC_BROWSER_TEST_F(PhishingClassifierTest, DisableDetection) {
+#if defined(OS_MACOSX)
+  if (base::mac::IsOSMountainLionOrLater()) {
+    // This test flakes on 10.8 only for an unknown reason.
+    // http://crbug.com/316709
+    return;
+  }
+#endif
+
   // No scorer yet, so the classifier is not ready.
   EXPECT_FALSE(classifier_->is_ready());
 
