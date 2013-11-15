093c66881c0e59811eaacf2f9cf1c41d7dd7c95e
steveblock@chromium.org <steveblock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-15 07:22:55 +0000

    Allow Web Animations CSS flag to be turned off as well as on
    This is a first step towards switching to Web Animations as the default
    implementation of CSS animations and transitions. When that switch is made, a
    virtual test suite will be added to run the animations and transitions
    LayoutTests with the legacy animations engine. Those virtual suites will
    require this --disable-web-animations-css flag.
    
    Also pass an explicit value to WebRuntimeFeatures::enableWebAnimationsSVG(), to
    allow the default value to be removed from Blink.
    
    BUG=258896
    R=dstockwell@chromium.org, jam@chromium.org, dstockwell
    
    Review URL: https://codereview.chromium.org/61643025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235301 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -927,6 +927,7 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kDisableUniversalAcceleratedOverflowScroll,
     switches::kDisableUnprefixedMediaSource,
     switches::kDisableVp8AlphaPlayback,
+    switches::kDisableWebAnimationsCSS,
     switches::kDisableWebAudio,
     switches::kDisableWebKitMediaSource,
     switches::kDomAutomationController,
