4b7f8adac72c2946a7f8beded72eaf31e40fc8c3
shashishekhar@chromium.org <shashishekhar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-15 19:37:53 +0000

    Upstream PasswordManagerHandler for Android.
    Android uses its own version of password_manager_handler, upstream it.
    Both WebUI and Android now share common UI agnostic logic that
    interacts with PasswordManagerPresenter.
    
    BUG=248589
    
    Review URL: https://codereview.chromium.org/61643024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235402 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/android/chrome_jni_registrar.cc
+++ chrome/browser/android/chrome_jni_registrar.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/most_visited_sites.h"
 #include "chrome/browser/android/new_tab_page_prefs.h"
 #include "chrome/browser/android/omnibox/omnibox_prerender.h"
+#include "chrome/browser/android/password_ui_view_android.h"
 #include "chrome/browser/android/provider/chrome_browser_provider.h"
 #include "chrome/browser/android/recently_closed_tabs_bridge.h"
 #include "chrome/browser/android/shortcut_helper.h"
@@ -117,6 +118,8 @@ static base::android::RegistrationMethod kChromeRegisteredMethods[] = {
   { "NewTabPagePrefs",
     NewTabPagePrefs::RegisterNewTabPagePrefs },
   { "OmniboxPrerender", RegisterOmniboxPrerender },
+  { "PasswordUIViewAndroid",
+    PasswordUIViewAndroid::RegisterPasswordUIViewAndroid },
   { "PersonalDataManagerAndroid",
     autofill::PersonalDataManagerAndroid::Register },
   { "ProfileAndroid", ProfileAndroid::RegisterProfileAndroid },
