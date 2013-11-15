93b031514118dc87df9c2881d0564d3588b0f828
costan@gmail.com <costan@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-15 13:24:42 +0000

    Implement File constructor.
    The feature is behind the FileConstructor RuntimeEnabledFeature with
    status=experimental.
    
    BUG=164933
    
    Review URL: https://codereview.chromium.org/57483002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162116 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/custom/V8FileCustom.cpp
+++ third_party/WebKit/Source/bindings/v8/custom/V8FileCustom.cpp
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2012 Google Inc. All rights reserved.
+ * Copyright (C) 2013 Google Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -30,18 +30,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
-#include "V8Blob.h"
+#include "V8File.h"
 
+#include "RuntimeEnabledFeatures.h"
 #include "bindings/v8/custom/V8BlobCustomHelpers.h"
 #include "core/fileapi/BlobBuilder.h"
 
 namespace WebCore {
 
-void V8Blob::constructorCustom(const v8::FunctionCallbackInfo<v8::Value>& info)
+void V8File::constructorCustom(const v8::FunctionCallbackInfo<v8::Value>& info)
 {
-    if (!info.Length()) {
-        RefPtr<Blob> blob = Blob::create();
-        v8SetReturnValue(info, blob.release());
+    if (!RuntimeEnabledFeatures::fileConstructorEnabled()) {
+        throwTypeError("Illegal constructor", info.GetIsolate());
+        return;
+    }
+
+    if (info.Length() < 2) {
+        throwTypeError("File constructor requires at least two arguments", info.GetIsolate());
         return;
     }
 
@@ -51,20 +56,22 @@ void V8Blob::constructorCustom(const v8::FunctionCallbackInfo<v8::Value>& info)
     } else {
         const int sequenceArgumentIndex = 0;
         if (toV8Sequence(info[sequenceArgumentIndex], length, info.GetIsolate()).IsEmpty()) {
-            throwTypeError(ExceptionMessages::failedToConstruct("Blob", ExceptionMessages::notAnArrayTypeArgumentOrValue(sequenceArgumentIndex + 1)), info.GetIsolate());
+            throwTypeError(ExceptionMessages::failedToConstruct("File", ExceptionMessages::notAnArrayTypeArgumentOrValue(sequenceArgumentIndex + 1)), info.GetIsolate());
             return;
         }
     }
 
+    V8TRYCATCH_FOR_V8STRINGRESOURCE_VOID(V8StringResource<>, fileName, info[1]);
+
     String contentType;
     String endings = "transparent"; // default if no BlobPropertyBag is passed
-    if (info.Length() > 1) {
-        if (!info[1]->IsObject()) {
-            throwTypeError(ExceptionMessages::failedToConstruct("Blob", "The 2nd argument is not of type Object."), info.GetIsolate());
+    if (info.Length() > 2) {
+        if (!info[2]->IsObject()) {
+            throwTypeError(ExceptionMessages::failedToConstruct("File", "The 3rd argument is not of type Object."), info.GetIsolate());
             return;
         }
 
-        if (!V8BlobCustomHelpers::processBlobPropertyBag(info[1], "Blob", contentType, endings, info.GetIsolate()))
+        if (!V8BlobCustomHelpers::processBlobPropertyBag(info[2], "File", contentType, endings, info.GetIsolate()))
             return;
     }
 
@@ -73,8 +80,8 @@ void V8Blob::constructorCustom(const v8::FunctionCallbackInfo<v8::Value>& info)
     if (!V8BlobCustomHelpers::processBlobParts(blobParts, length, endings, blobBuilder, info.GetIsolate()))
         return;
 
-    RefPtr<Blob> blob = blobBuilder.createBlob(contentType);
-    v8SetReturnValue(info, blob.release());
+    RefPtr<File> file = blobBuilder.createFile(contentType, fileName, currentTime());
+    v8SetReturnValue(info, file.release());
 }
 
 } // namespace WebCore
