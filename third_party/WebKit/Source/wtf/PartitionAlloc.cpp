58043abae50e46cc3f477a5b3720ef405c1048f2
cevans@chromium.org <cevans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-16 01:43:51 +0000

    Make PartitionAlloc's max allocation ~2GB, to match Chromium's allocator.
    BUG=319393
    
    Review URL: https://codereview.chromium.org/73843002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162149 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/PartitionAlloc.cpp
+++ third_party/WebKit/Source/wtf/PartitionAlloc.cpp
@@ -449,6 +449,7 @@ void partitionFreeSlowPath(PartitionPageHeader* page)
 
 void* partitionReallocGeneric(PartitionRoot* root, void* ptr, size_t newSize)
 {
+    RELEASE_ASSERT(newSize <= QuantizedAllocation::kMaxUnquantizedAllocation);
 #if defined(MEMORY_TOOL_REPLACES_ALLOCATOR)
     return realloc(ptr, newSize);
 #else
