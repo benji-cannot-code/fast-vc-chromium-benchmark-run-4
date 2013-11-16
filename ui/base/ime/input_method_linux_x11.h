a6671c5b7a87e75f8e62140a18fe85ace4c35764
yukishiino@chromium.org <yukishiino@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-16 02:57:11 +0000

    Clean up InputMethodLinuxX11 following crrev.com/48203002
    http://crrev.com/48203002 makes it possible to make the implementation of InputMethodXxx family much simpler and cleaner.  Cleans InputMethodLinuxX11 following the CL.
    
    TEST=none
    
    Review URL: https://codereview.chromium.org/61903004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235504 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/ime/input_method_linux_x11.h
+++ ui/base/ime/input_method_linux_x11.h
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ime/input_method_base.h"
 #include "ui/base/ime/linux/linux_input_method_context.h"
 
-typedef struct _GtkIMContext GtkIMContext;
-
 namespace ui {
 
 // A ui::InputMethod implementation for a X11 event loop on GNU/Linux.
