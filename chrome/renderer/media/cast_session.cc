e8a5fec30898306dd74e7356b6a3ebc4da4d9e10
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-16 01:52:58 +0000

    Create and configure CastSender from chrome extensions API
    Implement default codec parameters and translate CastRtpCaps to
    AudioSenderConfig and VideoSenderConfig. This allows CastSender to be
    be created with default parameters.
    
    Adding an extensions test that exercise these code.
    
    BUG=301920
    
    Review URL: https://codereview.chromium.org/60733021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235494 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/media/cast_session.cc
+++ chrome/renderer/media/cast_session.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_loop_proxy.h"
 #include "chrome/renderer/media/cast_session_delegate.h"
 #include "content/public/renderer/render_thread.h"
+#include "media/cast/cast_config.h"
 
 CastSession::CastSession()
     : delegate_(new CastSessionDelegate()),
@@ -18,4 +19,18 @@ CastSession::CastSession()
 CastSession::~CastSession() {
   // We should always be able to delete the object on the IO thread.
   CHECK(io_message_loop_proxy_->DeleteSoon(FROM_HERE, delegate_.release()));
+}
+
+void CastSession::StartAudio(const media::cast::AudioSenderConfig& config) {
+  io_message_loop_proxy_->PostTask(FROM_HERE,
+      base::Bind(&CastSessionDelegate::StartAudio,
+                 base::Unretained(delegate_.get()),
+                 config));
+}
+
+void CastSession::StartVideo(const media::cast::VideoSenderConfig& config) {
+  io_message_loop_proxy_->PostTask(FROM_HERE,
+      base::Bind(&CastSessionDelegate::StartVideo,
+                 base::Unretained(delegate_.get()),
+                 config));
 }
--- chrome/renderer/media/cast_session.h
+++ chrome/renderer/media/cast_session.h
@@ -14,6 +14,13 @@ namespace base {
 class MessageLoopProxy;
 }  // namespace base
 
+namespace media {
+namespace cast {
+struct AudioSenderConfig;
+struct VideoSenderConfig;
+}  // namespace cast
+}  // namespace media
+
 class CastSessionDelegate;
 
 // This class represents a Cast session and allows the session to be
@@ -22,6 +29,10 @@ class CastSessionDelegate;
 class CastSession : public base::RefCounted<CastSession> {
  public:
   CastSession();
+
+  // Start encoding of audio and video using the provided configuration.
+  void StartAudio(const media::cast::AudioSenderConfig& config);
+  void StartVideo(const media::cast::VideoSenderConfig& config);
 
  private:
   friend class base::RefCounted<CastSession>;
