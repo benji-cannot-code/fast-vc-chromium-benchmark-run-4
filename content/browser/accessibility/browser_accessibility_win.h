c9aba1ba997b468b1e677249431189fa55a41bc3
dmazzoni@chromium.org <dmazzoni@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-17 16:45:09 +0000

    Implement Windows character extent and scrolling methods.
    This should be pretty straightforward - the logic is all
    implemented and tested in cross-platform code, this just
    hooks it up.
    
    BUG=98977
    
    Review URL: https://codereview.chromium.org/61963003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235536 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/accessibility/browser_accessibility_win.h
+++ content/browser/accessibility/browser_accessibility_win.h
@@ -446,6 +446,14 @@ BrowserAccessibilityWin
 
   CONTENT_EXPORT STDMETHODIMP get_caretOffset(LONG* offset);
 
+  CONTENT_EXPORT STDMETHODIMP get_characterExtents(
+      LONG offset,
+      enum IA2CoordinateType coord_type,
+      LONG* out_x,
+      LONG* out_y,
+      LONG* out_width,
+      LONG* out_height);
+
   CONTENT_EXPORT STDMETHODIMP get_nSelections(LONG* n_selections);
 
   CONTENT_EXPORT STDMETHODIMP get_selection(LONG selection_index,
@@ -512,14 +520,6 @@ BrowserAccessibilityWin
   CONTENT_EXPORT STDMETHODIMP get_attributes(LONG offset, LONG* start_offset,
                                              LONG* end_offset,
                                              BSTR* text_attributes) {
-    return E_NOTIMPL;
-  }
-  CONTENT_EXPORT STDMETHODIMP get_characterExtents(LONG offset,
-      enum IA2CoordinateType coord_type,
-      LONG* x,
-      LONG* y,
-      LONG* width,
-      LONG* height) {
     return E_NOTIMPL;
   }
 
@@ -683,27 +683,21 @@ BrowserAccessibilityWin
   CONTENT_EXPORT STDMETHODIMP get_clippedSubstringBounds(
       unsigned int start_index,
       unsigned int end_index,
-      int* x,
-      int* y,
-      int* width,
-      int* height) {
-    return E_NOTIMPL;
-  }
+      int* out_x,
+      int* out_y,
+      int* out_width,
+      int* out_height);
 
   CONTENT_EXPORT STDMETHODIMP get_unclippedSubstringBounds(
       unsigned int start_index,
       unsigned int end_index,
-      int* x,
-      int* y,
-      int* width,
-      int* height) {
-    return E_NOTIMPL;
-  }
+      int* out_x,
+      int* out_y,
+      int* out_width,
+      int* out_height);
 
   CONTENT_EXPORT STDMETHODIMP scrollToSubstring(unsigned int start_index,
-                                                unsigned int end_index)  {
-    return E_NOTIMPL;
-  }
+                                                unsigned int end_index);
 
   CONTENT_EXPORT STDMETHODIMP get_fontFamily(BSTR *font_family)  {
     return E_NOTIMPL;
