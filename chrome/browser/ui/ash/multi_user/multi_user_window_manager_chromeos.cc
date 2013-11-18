f456144f7a642e1608d12c7463a505045433cea9
skuhne@chromium.org <skuhne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-18 18:42:40 +0000

    Switching to the correct desktop when a system modal dialog gets shown
    Instead of blindly hiding a window, this patch will check if the owner of the window is a system modal container. If so, switch users instead. 
    
    BUG=318832
    TEST=unittest (and visual)
    
    Review URL: https://codereview.chromium.org/74363004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235773 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/ash/multi_user/multi_user_window_manager_chromeos.cc
+++ chrome/browser/ui/ash/multi_user/multi_user_window_manager_chromeos.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/session_state_delegate.h"
 #include "ash/shell.h"
 #include "ash/shell_delegate.h"
+#include "ash/shell_window_ids.h"
 #include "ash/wm/mru_window_tracker.h"
 #include "ash/wm/window_positioner.h"
 #include "ash/wm/window_state.h"
@@ -467,6 +468,31 @@ void MultiUserWindowManagerChromeOS::SetWindowVisibility(
     aura::Window* window, bool visible) {
   if (window->IsVisible() == visible)
     return;
+
+  // Hiding a system modal dialog should not be allowed. Instead we switch to
+  // the user which is showing the system modal window.
+  // Note that in some cases (e.g. unit test) windows might not have a root
+  // window.
+  if (!visible && window->GetRootWindow()) {
+    // Get the system modal container for the window's root window.
+    aura::Window* system_modal_container =
+        window->GetRootWindow()->GetChildById(
+            ash::internal::kShellWindowId_SystemModalContainer);
+    if (window->parent() == system_modal_container) {
+      // The window is system modal and we need to find the parent which owns
+      // it so that we can switch to the desktop accordingly.
+      std::string user_id = GetUserPresentingWindow(window);
+      if (user_id.empty()) {
+        aura::Window* owning_window = GetOwningWindowInTransientChain(window);
+        DCHECK(owning_window);
+        user_id = GetUserPresentingWindow(owning_window);
+        DCHECK(!user_id.empty());
+      }
+      ash::Shell::GetInstance()->session_state_delegate()->SwitchActiveUser(
+          user_id);
+      return;
+    }
+  }
 
   // To avoid that these commands are recorded as any other commands, we are
   // suppressing any window entry changes while this is going on.
