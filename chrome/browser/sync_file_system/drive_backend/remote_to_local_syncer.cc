80d1932f45e2f93ab8bbf0075b7e8725f0e2b7e1
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-18 00:34:17 +0000

    [Retry][SyncFS] Delete local file on rename and reorganize.
    * Handle remote rename and reorganize by local file deletion.
    * Delete tracker on rename and reorganize completion.
    
    BUG=240165
    TBR=nhiroki, kinuko
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/67723003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235582 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_backend/remote_to_local_syncer.cc
+++ chrome/browser/sync_file_system/drive_backend/remote_to_local_syncer.cc
@@ -487,8 +487,8 @@ void RemoteToLocalSyncer::HandleRename(
   DCHECK_NE(dirty_tracker_->synced_details().title(),
             remote_metadata_->details().title());
 
-  NOTIMPLEMENTED();
-  callback.Run(SYNC_STATUS_FAILED);
+  Prepare(base::Bind(&RemoteToLocalSyncer::DidPrepareForDeletion,
+                     weak_ptr_factory_.GetWeakPtr(), callback));
 }
 
 void RemoteToLocalSyncer::HandleReorganize(
@@ -596,6 +596,7 @@ void RemoteToLocalSyncer::SyncCompleted(const SyncStatusCallback& callback,
   DCHECK(dirty_tracker_);
   DCHECK(remote_metadata_);
   DCHECK(remote_metadata_->has_details());
+
   metadata_database()->UpdateTracker(dirty_tracker_->tracker_id(),
                                      remote_metadata_->details(),
                                      callback);
