df378c92e83e7db6f253985c01432410e864e196
lazyboy@chromium.org <lazyboy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-18 07:48:21 +0000

    Add UMA for <webview> APIs: a. ClearData, b. when Permission request is allowed.
    BUG=317084
    Test=None
    
    Review URL: https://codereview.chromium.org/69913002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235647 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/guestview/webview/plugin_permission_helper.cc
+++ chrome/browser/guestview/webview/plugin_permission_helper.cc
@@ -109,8 +109,4 @@ void PluginPermissionHelper::OnPermissionResponse(const std::string& identifier,
     host->Send(new ChromeViewMsg_LoadBlockedPlugins(
         host->GetRoutingID(), identifier));
   }
-
-  content::RecordAction(
-      allow ? content::UserMetricsAction("WebView.Guest.PluginLoadAllowed") :
-              content::UserMetricsAction("WebView.Guest.PluginLoadDenied"));
 }
