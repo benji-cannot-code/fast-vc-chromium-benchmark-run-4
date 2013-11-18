c1950f52d6c6543ee2db3ebcdbeba023ed32c38e
tasak@google.com <tasak@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-18 06:42:09 +0000

    [Refactoring] Remove include "core/inspector/InspectorInstrumentation.h" from SelectorChecker.h
    To make it possible to add include "StyleResolver.h" to StyleEngine.h,
    We need to remove the above include from SelectorChecker.h.
    
    BUG=305885
    TEST=No layout tests, just refactoring.
    
    Review URL: https://codereview.chromium.org/70063005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162180 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/SelectorChecker.h
+++ third_party/WebKit/Source/core/css/SelectorChecker.h
@@ -30,11 +30,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define SelectorChecker_h
 
 #include "core/css/CSSSelector.h"
-#include "core/inspector/InspectorInstrumentation.h"
+#include "core/dom/Element.h"
+#include "platform/scroll/ScrollTypes.h"
 
 namespace WebCore {
 
 class CSSSelector;
+class ContainerNode;
 class Element;
 class RenderScrollbar;
 class RenderStyle;
--- third_party/WebKit/Source/core/dom/Element.cpp
+++ third_party/WebKit/Source/core/dom/Element.cpp
@@ -89,6 +89,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/html/HTMLTableRowsCollection.h"
 #include "core/html/HTMLTemplateElement.h"
 #include "core/html/parser/HTMLParserIdioms.h"
+#include "core/inspector/InspectorInstrumentation.h"
 #include "core/page/FocusController.h"
 #include "core/page/Page.h"
 #include "core/page/PointerLockController.h"
