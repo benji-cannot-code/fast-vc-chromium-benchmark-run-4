fdf074d48fdfeca6ebbae2b5407b8166a999ce1f
kalyan.kondapally@intel.com <kalyan.kondapally@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-18 00:38:13 +0000

    Remove un-necessary include in DisplayChangeObserverTest.
    BUG=
    
    Review URL: https://codereview.chromium.org/68473002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235584 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/display/display_change_observer_chromeos_unittest.cc
+++ ash/display/display_change_observer_chromeos_unittest.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/display/display_info.h"
 #include "chromeos/display/output_configurator.h"
-#include "chromeos/display/output_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 using chromeos::OutputConfigurator;
