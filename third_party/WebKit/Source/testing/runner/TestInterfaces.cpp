06df9e16debc04cfa237d7da1661e7021fe108a6
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-18 02:43:38 +0000

    Fix Android build failure after r162169.
    TBR=dglazkov@chromium.org
    BUG=318980
    
    Review URL: https://codereview.chromium.org/74913002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162170 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/testing/runner/TestInterfaces.cpp
+++ third_party/WebKit/Source/testing/runner/TestInterfaces.cpp
@@ -197,7 +197,7 @@ const vector<WebTestProxyBase*>& TestInterfaces::windowList()
 
 WebThemeEngine* TestInterfaces::themeEngine()
 {
-#if defined(USE_DEFAULT_RENDER_THEME) || !(defined(WIN32) || defined(__APPLE__))
+#if defined(USE_DEFAULT_RENDER_THEME) || !(defined(WIN32) || defined(__APPLE__) || defined(ANDROID))
     if (!m_themeEngine.get())
         m_themeEngine.reset(new WebTestThemeEngineMock());
     return m_themeEngine.get();
@@ -209,6 +209,8 @@ WebThemeEngine* TestInterfaces::themeEngine()
     if (!m_themeEngine.get())
         m_themeEngine.reset(new WebTestThemeEngineMac());
     return m_themeEngine.get();
+#else
+    return 0;
 #endif
 }
 
