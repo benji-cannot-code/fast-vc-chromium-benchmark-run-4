2966b20f69e1adb05c1b4f05da558fd13420e53a
tmdiep@chromium.org <tmdiep@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-18 09:33:09 +0000

    Move InstallObserver::OnBeginExtensionInstall parameters to a struct
    The parameters of InstallObserver::OnBeginExtensionInstall() have been
    moved to a struct to allow additional parameters to be added easily
    without changing the signature of the function.
    
    BUG=None
    TEST=unit_tests, browser_tests
    
    Review URL: https://codereview.chromium.org/72463002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235681 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/install_tracker.h
+++ chrome/browser/extensions/install_tracker.h
@@ -15,10 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class Profile;
 
-namespace gfx {
-class ImageSkia;
-}
-
 namespace extensions {
 
 class ExtensionPrefs;
@@ -34,11 +30,7 @@ class InstallTracker : public BrowserContextKeyedService,
   void RemoveObserver(InstallObserver* observer);
 
   void OnBeginExtensionInstall(
-      const std::string& extension_id,
-      const std::string& extension_name,
-      const gfx::ImageSkia& installing_icon,
-      bool is_app,
-      bool is_platform_app);
+      const InstallObserver::ExtensionInstallParams& params);
   void OnDownloadProgress(const std::string& extension_id,
                           int percent_downloaded);
   void OnInstallFailure(const std::string& extension_id);
--- chrome/chrome_browser_extensions.gypi
+++ chrome/chrome_browser_extensions.gypi
@@ -808,6 +808,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/extensions/image_loader_factory.h',
         'browser/extensions/installed_loader.cc',
         'browser/extensions/installed_loader.h',
+        'browser/extensions/install_observer.cc',
         'browser/extensions/install_observer.h',
         'browser/extensions/install_signer.cc',
         'browser/extensions/install_signer.h',
