efd843c6af28e806f085d03cd3fd4297119ec10c
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-18 09:33:32 +0000

    [rAc OSX] Fix reversed use of top and bottom padding.
    The coordinate system for the window controller is flipped, but the code was
    treating it as though it was unflipped.
    
    BUG=315977
    TEST=slightly less padding at top of dialog
    R=groby@chromium.org
    
    Review URL: https://codereview.chromium.org/72513003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235682 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/autofill/autofill_dialog_cocoa.mm
+++ chrome/browser/ui/cocoa/autofill/autofill_dialog_cocoa.mm
@@ -456,7 +456,7 @@ void AutofillDialogCocoa::OnConstrainedWindowClosed(
   NSRect contentRect = NSZeroRect;
   contentRect.size = [self preferredSize];
   NSRect clientRect = contentRect;
-  clientRect.origin.y = chrome_style::kClientBottomPadding;
+  clientRect.origin.y = chrome_style::kTitleTopPadding;
   clientRect.size.height -= chrome_style::kTitleTopPadding +
                             chrome_style::kClientBottomPadding;
 
