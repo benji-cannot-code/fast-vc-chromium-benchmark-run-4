f7f96b1170c64f6c01e6db64690acec7ac2b3716
alecflett@chromium.org <alecflett@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-18 11:38:23 +0000

    Remove dead code from IDB (chromium side)
    BUG=
    TBR=jam@chromium.org
    
    Review URL: https://codereview.chromium.org/73153003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235708 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/indexed_db/indexed_db_dispatcher_host.h
+++ content/browser/indexed_db/indexed_db_dispatcher_host.h
@@ -42,8 +42,7 @@ struct IndexedDBDatabaseMetadata;
 class IndexedDBDispatcherHost : public BrowserMessageFilter {
  public:
   // Only call the constructor from the UI thread.
-  IndexedDBDispatcherHost(int ipc_process_id,
-                          IndexedDBContextImpl* indexed_db_context);
+  explicit IndexedDBDispatcherHost(IndexedDBContextImpl* indexed_db_context);
 
   static ::IndexedDBDatabaseMetadata ConvertMetadata(
       const content::IndexedDBDatabaseMetadata& metadata);
@@ -156,7 +155,6 @@ class IndexedDBDispatcherHost : public BrowserMessageFilter {
 
     void CloseAll();
     bool OnMessageReceived(const IPC::Message& message, bool* msg_is_ok);
-    void Send(IPC::Message* message);
 
     void OnCreateObjectStore(
         const IndexedDBHostMsg_DatabaseCreateObjectStore_Params& params);
@@ -165,9 +163,6 @@ class IndexedDBDispatcherHost : public BrowserMessageFilter {
                              int64 object_store_id);
     void OnCreateTransaction(
         const IndexedDBHostMsg_DatabaseCreateTransaction_Params&);
-    void OnOpen(int32 ipc_database_id,
-                int32 ipc_thread_id,
-                int32 ipc_callbacks_id);
     void OnClose(int32 ipc_database_id);
     void OnDestroyed(int32 ipc_database_id);
 
@@ -211,7 +206,6 @@ class IndexedDBDispatcherHost : public BrowserMessageFilter {
     ~CursorDispatcherHost();
 
     bool OnMessageReceived(const IPC::Message& message, bool* msg_is_ok);
-    void Send(IPC::Message* message);
 
     void OnAdvance(int32 ipc_object_store_id,
                    int32 ipc_thread_id,
@@ -240,9 +234,6 @@ class IndexedDBDispatcherHost : public BrowserMessageFilter {
   // Only access on IndexedDB thread.
   scoped_ptr<DatabaseDispatcherHost> database_dispatcher_host_;
   scoped_ptr<CursorDispatcherHost> cursor_dispatcher_host_;
-
-  // Used to dispatch messages to the correct view host.
-  int ipc_process_id_;
 
   DISALLOW_IMPLICIT_CONSTRUCTORS(IndexedDBDispatcherHost);
 };
