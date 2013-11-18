64eece0b2860a67510d502569127fba2b212a533
ajuma@chromium.org <ajuma@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-18 23:34:51 +0000

    Don't PushProperties every commit for animating layers
    This removes the need to PushProperties every commit during an animation.
    Instead, a single PushProperties is needed whenever there are animations
    waiting for deletion.
    
    This also removes LayerAnimationController::set_force_sync, which no
    longer had any non-test callers and wasn't actually needed by its test
    callers.
    
    BUG=259088
    
    Review URL: https://codereview.chromium.org/60083018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235845 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/animation/layer_animation_controller_unittest.cc
+++ cc/animation/layer_animation_controller_unittest.cc
@@ -294,9 +294,14 @@ TEST(LayerAnimationControllerTest, AnimationsAreDeleted) {
   controller->Animate(1.0);
   controller->UpdateState(true, NULL);
 
+  EXPECT_FALSE(dummy.animation_waiting_for_deletion());
+  EXPECT_FALSE(dummy_impl.animation_waiting_for_deletion());
+
   events.reset(new AnimationEventsVector);
   controller_impl->Animate(2.0);
   controller_impl->UpdateState(true, events.get());
+
+  EXPECT_TRUE(dummy_impl.animation_waiting_for_deletion());
 
   // There should be a Finished event for the animation.
   EXPECT_EQ(1u, events->size());
@@ -310,6 +315,7 @@ TEST(LayerAnimationControllerTest, AnimationsAreDeleted) {
 
   controller->Animate(3.0);
   controller->UpdateState(true, NULL);
+  EXPECT_TRUE(dummy.animation_waiting_for_deletion());
 
   controller->PushAnimationUpdatesTo(controller_impl.get());
 
@@ -1141,7 +1147,7 @@ TEST(LayerAnimationControllerTest, AbortAGroupedAnimation) {
   EXPECT_EQ(0.75f, dummy.opacity());
 }
 
-TEST(LayerAnimationControllerTest, ForceSyncWhenSynchronizedStartTimeNeeded) {
+TEST(LayerAnimationControllerTest, PushUpdatesWhenSynchronizedStartTimeNeeded) {
   FakeLayerAnimationValueObserver dummy_impl;
   scoped_refptr<LayerAnimationController> controller_impl(
       LayerAnimationController::Create(0));
@@ -1166,8 +1172,6 @@ TEST(LayerAnimationControllerTest, ForceSyncWhenSynchronizedStartTimeNeeded) {
   Animation* active_animation = controller->GetAnimation(0, Animation::Opacity);
   EXPECT_TRUE(active_animation);
   EXPECT_TRUE(active_animation->needs_synchronized_start_time());
-
-  controller->set_force_sync();
 
   controller->PushAnimationUpdatesTo(controller_impl.get());
 
@@ -1438,9 +1442,12 @@ TEST(LayerAnimationControllerTest, MainThreadAbortedAnimationGetsDeleted) {
   controller->AbortAnimations(Animation::Opacity);
   EXPECT_EQ(Animation::Aborted,
             controller->GetAnimation(Animation::Opacity)->run_state());
+  EXPECT_FALSE(dummy.animation_waiting_for_deletion());
+  EXPECT_FALSE(dummy_impl.animation_waiting_for_deletion());
 
   controller->Animate(1.0);
   controller->UpdateState(true, NULL);
+  EXPECT_TRUE(dummy.animation_waiting_for_deletion());
   EXPECT_EQ(Animation::WaitingForDeletion,
             controller->GetAnimation(Animation::Opacity)->run_state());
 
@@ -1469,10 +1476,13 @@ TEST(LayerAnimationControllerTest, ImplThreadAbortedAnimationGetsDeleted) {
   controller_impl->AbortAnimations(Animation::Opacity);
   EXPECT_EQ(Animation::Aborted,
             controller_impl->GetAnimation(Animation::Opacity)->run_state());
+  EXPECT_FALSE(dummy.animation_waiting_for_deletion());
+  EXPECT_FALSE(dummy_impl.animation_waiting_for_deletion());
 
   AnimationEventsVector events;
   controller_impl->Animate(1.0);
   controller_impl->UpdateState(true, &events);
+  EXPECT_TRUE(dummy_impl.animation_waiting_for_deletion());
   EXPECT_EQ(1u, events.size());
   EXPECT_EQ(AnimationEvent::Aborted, events[0].type);
   EXPECT_EQ(Animation::WaitingForDeletion,
@@ -1484,6 +1494,7 @@ TEST(LayerAnimationControllerTest, ImplThreadAbortedAnimationGetsDeleted) {
 
   controller->Animate(1.5);
   controller->UpdateState(true, NULL);
+  EXPECT_TRUE(dummy.animation_waiting_for_deletion());
   EXPECT_EQ(Animation::WaitingForDeletion,
             controller->GetAnimation(Animation::Opacity)->run_state());
 
--- cc/test/animation_test_common.cc
+++ cc/test/animation_test_common.cc
@@ -187,7 +187,8 @@ float FakeFloatTransition::GetValue(double time) const {
 }
 
 FakeLayerAnimationValueObserver::FakeLayerAnimationValueObserver()
-    : opacity_(0.0f) {}
+    : opacity_(0.0f),
+      animation_waiting_for_deletion_(false) {}
 
 FakeLayerAnimationValueObserver::~FakeLayerAnimationValueObserver() {}
 
@@ -203,6 +204,10 @@ void FakeLayerAnimationValueObserver::OnOpacityAnimated(float opacity) {
 void FakeLayerAnimationValueObserver::OnTransformAnimated(
     const gfx::Transform& transform) {
   transform_ = transform;
+}
+
+void FakeLayerAnimationValueObserver::OnAnimationWaitingForDeletion() {
+  animation_waiting_for_deletion_ = true;
 }
 
 bool FakeLayerAnimationValueObserver::IsActive() const {
