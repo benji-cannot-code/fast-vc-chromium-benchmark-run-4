cbf9922e76bbda04f1c666dbef92e4974ae4dbf7
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-18 08:17:42 +0000

    Added the ability to set the upoad file path
    Added the upload file path to PrivetURLFetcher and PrivetLocalPrintOperation.
    
    BUG=318373
    R=gene@chromium.org, vitalybuka@chromium.org
    
    Review URL: https://codereview.chromium.org/72033002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235658 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/local_discovery/privet_http_impl.h
+++ chrome/browser/local_discovery/privet_http_impl.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/callback.h"
+#include "base/file_util.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/local_discovery/privet_http.h"
 
@@ -157,6 +158,7 @@ class PrivetLocalPrintOperationImpl
   virtual void Start() OVERRIDE;
 
   virtual void SendData(const std::string& data) OVERRIDE;
+  virtual void SendDataFile(const base::FilePath& data) OVERRIDE;
 
   virtual void SetTicket(const std::string& ticket) OVERRIDE;
 
@@ -196,6 +198,8 @@ class PrivetLocalPrintOperationImpl
   void OnCreatejobResponse(bool has_error,
                            const base::DictionaryValue* value);
 
+  void SendDataInternal();
+
   PrivetHTTPClientImpl* privet_client_;
   PrivetLocalPrintOperation::Delegate* delegate_;
 
@@ -203,6 +207,7 @@ class PrivetLocalPrintOperationImpl
 
   std::string ticket_;
   std::string data_;
+  base::FilePath data_file_;
 
   bool use_pdf_;
   bool has_capabilities_;
