3a56b76c3247db750d9c903bf4ce5f82347c0213
tsepez@chromium.org <tsepez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-18 22:03:51 +0000

    The WebPublicSuffixList API allows embedders to tell Blink about what constitutes a top-level domain.  It is the source of truth that says that for google.co.uk, co.uk is the top-level domain, and not just .uk. Blink has, until now, been completely ignorant of this complication.
    This is the third part of a two-sided patch. The tests will be on the Blink side, landing as the fourth part of the two-sided patch (once this has landed on the chrome side).
    
    
    BUG=307407
    
    Review URL: https://codereview.chromium.org/60893002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235828 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/renderer_webkitplatformsupport_impl.cc
+++ content/renderer/renderer_webkitplatformsupport_impl.cc
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/renderer_clipboard_client.h"
 #include "content/renderer/webclipboard_impl.h"
 #include "content/renderer/webcrypto/webcrypto_impl.h"
+#include "content/renderer/webpublicsuffixlist_impl.h"
 #include "gpu/config/gpu_info.h"
 #include "ipc/ipc_sync_message_filter.h"
 #include "media/audio/audio_output_device.h"
@@ -803,6 +804,13 @@ void RendererWebKitPlatformSupportImpl::getPluginList(
     }
   }
 #endif
+}
+
+//------------------------------------------------------------------------------
+
+blink::WebPublicSuffixList*
+RendererWebKitPlatformSupportImpl::publicSuffixList() {
+  return &public_suffix_list_;
 }
 
 //------------------------------------------------------------------------------
