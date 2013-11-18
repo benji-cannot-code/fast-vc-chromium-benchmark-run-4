a3e8c294436a911368da91bb70862eaa0cdf9b3c
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-18 00:49:57 +0000

    autofill: remove obsolete switch
    BUG=315415
    
    Review URL: https://codereview.chromium.org/73283003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235589 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -1493,7 +1493,6 @@ void ChromeContentBrowserClient::AppendExtraCommandLineSwitches(
     static const char* const kSwitchNames[] = {
       autofill::switches::kDisableInteractiveAutocomplete,
       autofill::switches::kDisablePasswordGeneration,
-      autofill::switches::kEnableExperimentalFormFilling,
       autofill::switches::kEnableInteractiveAutocomplete,
       autofill::switches::kEnablePasswordGeneration,
       autofill::switches::kNoAutofillNecessaryForPasswordGeneration,
