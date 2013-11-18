d65f7d1543c1fcdeb1a9dfe1a11e93a599540470
ksakamoto@chromium.org <ksakamoto@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-18 06:41:49 +0000

    Initiate webfont download right after style recalc
    Currently WebFont download is queued during layout, and actual download is
    delayed until after layout. This patch makes CSSFontFace::willUseFontData()
    enqueue font download, which is called in style calc time. StyleResolver
    kicks off font downloads, along with other pending resources.
    
    UMA metrics (WebFont.Resource.StyleRecalcToDownloadLatency) shows
    this reduces webfont download latency 114ms at the 50 percentile.
    This patch also removes FontResource::FontResourceHistograms as we
    collected sufficient data.
    
    BUG=246492
    TEST=Covered by existing tests
    
    Review URL: https://codereview.chromium.org/63713002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162179 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/CSSFontFaceSource.cpp
+++ third_party/WebKit/Source/core/css/CSSFontFaceSource.cpp
@@ -268,8 +268,8 @@ bool CSSFontFaceSource::isLocalFontAvailable(const FontDescription& fontDescript
 
 void CSSFontFaceSource::willUseFontData()
 {
-    if (m_font)
-        m_font->willUseFontData();
+    if (m_face && m_font && m_font->stillNeedsLoad())
+        beginLoadingFontSoon();
 }
 
 void CSSFontFaceSource::beginLoadingFontSoon()
