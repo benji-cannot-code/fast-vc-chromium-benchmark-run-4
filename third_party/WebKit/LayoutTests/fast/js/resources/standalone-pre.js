b11e42ff1be7cde7b45775e78a0b39088a2fd21a
dongseong.hwang@intel.com <dongseong.hwang@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-18 19:46:52 +0000

    Clean up js-test-post.js remainders.
    r161289 deleted js-test-post.js, so this CL cleans up.
    
    Review URL: https://codereview.chromium.org/75493002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162227 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/js/resources/standalone-pre.js
+++ third_party/WebKit/LayoutTests/fast/js/resources/standalone-pre.js
@@ -158,7 +158,7 @@ function isSuccessfullyParsed()
     debug("\nTEST COMPLETE\n");
 }
 
-// It's possible for an async test to call finishJSTest() before js-test-post.js
+// It's possible for an async test to call finishJSTest() before standalone-post.js
 // has been parsed.
 function finishJSTest()
 {
