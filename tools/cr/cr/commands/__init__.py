caa7cf1149650836dd0103c30b0b2164b948a596
iancottrell@chromium.org <iancottrell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-18 16:37:13 +0000

    Adding the cr command base class
    BUG=316397
    
    Review URL: https://codereview.chromium.org/72043002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235741 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/cr/cr/commands/__init__.py
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""A package for all the built in commands.
+
+This package has all the standard commands built in to the cr tool.
+Most commands use actions to perform the real work.
+"""
