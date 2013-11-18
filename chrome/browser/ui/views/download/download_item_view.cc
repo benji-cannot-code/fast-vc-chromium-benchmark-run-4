4558bcdcae32fe3ec444bf7f6c82a929d6a6a0fe
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-18 11:58:03 +0000

    Safebrowsing aura linux_redux compile fixes.
    BUG=319217
    
    Review URL: https://codereview.chromium.org/73573002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235716 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/download/download_item_view.cc
+++ chrome/browser/ui/views/download/download_item_view.cc
@@ -871,6 +871,7 @@ void DownloadItemView::OpenDownload() {
 }
 
 bool DownloadItemView::BeginDownloadFeedback() {
+#if defined(FULL_SAFE_BROWSING)
   SafeBrowsingService* sb_service = g_browser_process->safe_browsing_service();
   if (!sb_service)
     return false;
@@ -887,6 +888,10 @@ bool DownloadItemView::BeginDownloadFeedback() {
       download());
   // WARNING: we are deleted at this point.  Don't access 'this'.
   return true;
+#else
+  NOTREACHED();
+  return false;
+#endif
 }
 
 void DownloadItemView::LoadIcon() {
@@ -1096,10 +1101,12 @@ void DownloadItemView::ClearWarningDialog() {
 void DownloadItemView::ShowWarningDialog() {
   DCHECK(mode_ != DANGEROUS_MODE && mode_ != MALICIOUS_MODE);
   time_download_warning_shown_ = base::Time::Now();
+#if defined(FULL_SAFE_BROWSING)
   if (model_.ShouldAllowDownloadFeedback()) {
     safe_browsing::DownloadFeedbackService::RecordEligibleDownloadShown(
         download()->GetDangerType());
   }
+#endif
   mode_ = model_.MightBeMalicious() ? MALICIOUS_MODE : DANGEROUS_MODE;
 
   body_state_ = NORMAL;
