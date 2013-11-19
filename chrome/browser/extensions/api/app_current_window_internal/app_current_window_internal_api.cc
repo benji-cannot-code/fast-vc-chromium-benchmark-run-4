eb6518ad65c889e5e2970963184ff940b279e97f
tmdiep@chromium.org <tmdiep@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-19 01:58:00 +0000

    Whitelist always-on-top app windows for first-party apps
    Some first-party apps will be whitelisted to use the alwaysOnTop
    property in M32 stable.
    
    BUG=320847, 319521
    TESTS=browser_tests (PlatformAppBrowserTest.WindowsApiAlwaysOnTop*)
    
    Review URL: https://codereview.chromium.org/75023002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235888 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/app_current_window_internal/app_current_window_internal_api.cc
+++ chrome/browser/extensions/api/app_current_window_internal/app_current_window_internal_api.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "apps/shell_window_registry.h"
 #include "apps/ui/native_app_window.h"
 #include "base/command_line.h"
+#include "chrome/browser/extensions/api/app_window/app_window_api.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/extensions/api/app_current_window_internal.h"
 #include "chrome/common/extensions/api/app_window.h"
@@ -278,7 +279,7 @@ bool AppCurrentWindowInternalSetInputRegionFunction::RunWithWindow(
 
 bool AppCurrentWindowInternalSetAlwaysOnTopFunction::RunWithWindow(
     ShellWindow* window) {
-  if (GetCurrentChannel() > chrome::VersionInfo::CHANNEL_DEV) {
+  if (!AppWindowCreateFunction::AllowAlwaysOnTopWindows(GetExtension()->id())) {
     error_ = kDevChannelOnly;
     return false;
   }
