95617385e400920d2c5ea219abe7bbaae7d63c97
iancottrell@chromium.org <iancottrell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-19 11:10:01 +0000

    Adding the client handling. Includes the fake packages it needs for plugin loading.
    BUG=316397
    
    Review URL: https://codereview.chromium.org/71773002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235979 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/cr/cr/auto/__init__.py
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""A root package for all the automatically loaded modules.
+
+There are no built in packages below this, it holds a set of packages that
+have their search paths modified in order to pick up plugins from directories
+that are not known until run-time.
+"""
