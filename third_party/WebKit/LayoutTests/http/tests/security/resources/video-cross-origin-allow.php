b8f7f1eacb8e020f432b40a6f022d09ddc2df37a
sigbjornf@opera.com <sigbjornf@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-19 10:59:54 +0000

    Verify that video.crossOrigin performs no preflighting.
    Verify that a video element CORS fetch doesn't generate a preflight
    request and loads the cross-origin resource ok.
    
    R=jochen,kbr
    BUG=173727
    
    Review URL: https://codereview.chromium.org/75153002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162293 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/security/resources/video-cross-origin-allow.php
+++ third_party/WebKit/LayoutTests/http/tests/security/resources/video-cross-origin-allow.php
@@ -1,6 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php
 
+if (isset($_GET['no-preflight']) && $_SERVER['REQUEST_METHOD'] == "OPTIONS") {
+    echo "FAIL";
+    exit;
+}
+
 header("Access-Control-Allow-Origin: *");
 
 if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") {
