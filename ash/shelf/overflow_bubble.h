943e2c2ed2f59f6124743c60039e2132c6ac42e3
simonhong@chromium.org <simonhong@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-19 05:05:48 +0000

    Reland - [ash] Enable dragging an item between shelf and overflow bubble
    Previous cl (https://codereview.chromium.org/61703002/) was reverted
    because win8-aura doesn't support multiple display.
    
    Description Copied from original cl:
    This is the first CL to implement drag and drop from overflow bubble to shelf.
    
    Currently ripped off item from overflow bubble cannot be dragged into shelf.
    But, Item should be dragged from overflow bubble to shelf to implement drag & drop.
    In this CL, re-insert bounds for shelf/overflow bubble is re-defined like below.
    - shelf -bounds that include visible items.
    - overflow bubble - overflow bubble bounds.
    
    * The method of ripping off is not changed (dragging an item to above only in bottom alignment)
    If ripping off is possible by dragging to down side, it could cause flickering because overflow bubble and shelf is located too closely.
    
    R=jamescook@chromium.org
    BUG=137141
    TEST=ash_unittest --gtest_filter=*.CheckDragInsertBoundsWithMultiMonitor
    
    Review URL: https://codereview.chromium.org/74853002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235935 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/shelf/overflow_bubble.h
+++ ash/shelf/overflow_bubble.h
@@ -23,6 +23,8 @@ namespace ash {
 
 namespace internal {
 
+class OverflowBubbleView;
+class ShelfLayoutManager;
 class ShelfView;
 
 // OverflowBubble displays the overflown launcher items in a bubble.
@@ -44,6 +46,9 @@ class OverflowBubble : public ui::EventHandler,
   bool IsShowing() const { return !!bubble_; }
   ShelfView* shelf_view() { return shelf_view_; }
 
+  // Returns overflow bubble bounds in screen coordinate.
+  gfx::Rect GetBubbleBounds();
+
  private:
   void ProcessPressedEvent(ui::LocatedEvent* event);
 
@@ -54,7 +59,7 @@ class OverflowBubble : public ui::EventHandler,
   // Overridden from views::WidgetObserver:
   virtual void OnWidgetDestroying(views::Widget* widget) OVERRIDE;
 
-  views::View* bubble_;  // Owned by views hierarchy.
+  OverflowBubbleView* bubble_;  // Owned by views hierarchy.
   views::View* anchor_;  // Owned by ShelfView.
   ShelfView* shelf_view_;  // Owned by |bubble_|.
 
