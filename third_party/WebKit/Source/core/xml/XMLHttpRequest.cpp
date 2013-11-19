01a1da013433cb8eac7e2c34b2b6fb974b28f600
sigbjornf@opera.com <sigbjornf@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-19 01:13:52 +0000

    Factor out delivery of throttled progress event.
    To prepare for follow-on changes to progress event handling, split out
    the delivery of single XHR progress events into a separate method.
    
    R=
    BUG=315488
    
    Review URL: https://codereview.chromium.org/75103006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162253 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/xml/XMLHttpRequest.cpp
+++ third_party/WebKit/Source/core/xml/XMLHttpRequest.cpp
@@ -966,6 +966,15 @@ void XMLHttpRequest::dispatchEventAndLoadEnd(const AtomicString& type)
     m_progressEventThrottle.dispatchEventAndLoadEnd(XMLHttpRequestProgressEvent::create(type));
 }
 
+void XMLHttpRequest::dispatchThrottledProgressEvent()
+{
+    long long expectedLength = m_response.expectedContentLength();
+    bool lengthComputable = expectedLength > 0 && m_receivedLength <= expectedLength;
+    unsigned long long total = lengthComputable ? expectedLength : 0;
+
+    m_progressEventThrottle.dispatchProgressEvent(lengthComputable, m_receivedLength, total);
+}
+
 void XMLHttpRequest::handleNetworkError()
 {
     LOG(Network, "XMLHttpRequest %p handleNetworkError()", this);
@@ -1299,13 +1308,8 @@ void XMLHttpRequest::didReceiveData(const char* data, int len)
 
     m_receivedLength += len;
 
-    if (m_async) {
-        long long expectedLength = m_response.expectedContentLength();
-        bool lengthComputable = expectedLength > 0 && m_receivedLength <= expectedLength;
-        unsigned long long total = lengthComputable ? expectedLength : 0;
-
-        m_progressEventThrottle.dispatchProgressEvent(lengthComputable, m_receivedLength, total);
-    }
+    if (m_async)
+        dispatchThrottledProgressEvent();
 
     if (m_state != LOADING) {
         changeState(LOADING);
