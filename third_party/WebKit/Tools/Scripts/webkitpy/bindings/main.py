e803c6d154b48129ce1866ad8591c30c4c3542bb
nbarth@chromium.org <nbarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-19 09:48:58 +0000

    IDL compiler: [CheckSecurity=Frame] interfaces (initial)
    This adds support for [CheckSecurity=Frame] interface,
    but not for [CheckSecurity] + [DoNotCheckSecurity], as in:
    [CheckSecurity=Frame] interface { [DoNotCheckSecurity] void foo(); };
    (i.e., DomainSafeFunctionGetter/Setter).
    
    BUG=239771
    
    Review URL: https://codereview.chromium.org/61523003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162286 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/bindings/main.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/bindings/main.py
@@ -43,7 +43,6 @@ TBR=(someone in Source/bindings/OWNERS or WATCHLISTS:bindings)
 
 # Python compiler is incomplete; skip IDLs with unimplemented features
 SKIP_PYTHON = set([
-    'TestCheckSecurityInterface.idl',
     'TestCustomAccessors.idl',
     'TestEvent.idl',
     'TestEventConstructor.idl',
