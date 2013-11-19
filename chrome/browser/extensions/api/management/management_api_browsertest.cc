5d879a162ffcfcac7a07a1e4e9b9a32663de7583
creis@chromium.org <creis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-19 09:50:27 +0000

    Prevent creating a swapped out RVH in the same SiteInstance as the current one.
    BUG=123007, 319613
    TEST=RenderViewHostManagerTest.CleanUpSwappedOutRVHOnProcessCrash
    
    Review URL: https://codereview.chromium.org/57433010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235953 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/management/management_api_browsertest.cc
+++ chrome/browser/extensions/api/management/management_api_browsertest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/common/url_constants.h"
+#include "content/public/test/browser_test_utils.h"
 #include "content/public/test/test_utils.h"
 
 namespace keys = extension_management_api_constants;
@@ -32,18 +33,12 @@ namespace extensions {
 class ExtensionManagementApiBrowserTest : public ExtensionBrowserTest {
  protected:
   bool CrashEnabledExtension(const std::string& extension_id) {
-    content::WindowedNotificationObserver extension_crash_observer(
-        chrome::NOTIFICATION_EXTENSION_PROCESS_TERMINATED,
-        content::NotificationService::AllSources());
     ExtensionHost* background_host =
         ExtensionSystem::Get(browser()->profile())->
             process_manager()->GetBackgroundHostForExtension(extension_id);
     if (!background_host)
       return false;
-    background_host->host_contents()->GetController().LoadURL(
-        GURL(content::kChromeUICrashURL), content::Referrer(),
-        content::PAGE_TRANSITION_LINK, std::string());
-    extension_crash_observer.Wait();
+    content::CrashTab(background_host->host_contents());
     return true;
   }
 };
@@ -266,10 +261,17 @@ IN_PROC_BROWSER_TEST_F(ExtensionManagementApiEscalationTest,
       switches::kAppsGalleryInstallAutoConfirmForTests, "cancel");
   SetEnabled(true, true, keys::kUserDidNotReEnableError);
 
-  // This should succeed when user accepts dialog.
+  // This should succeed when user accepts dialog.  We must wait for the process
+  // to connect *and* for the channel to finish initializing before trying to
+  // crash it.  (NOTIFICATION_RENDERER_PROCESS_CREATED does not wait for the
+  // latter and can cause KillProcess to fail on Windows.)
+  content::WindowedNotificationObserver observer(
+      chrome::NOTIFICATION_EXTENSION_HOST_CREATED,
+      content::NotificationService::AllSources());
   CommandLine::ForCurrentProcess()->AppendSwitchASCII(
       switches::kAppsGalleryInstallAutoConfirmForTests, "accept");
   SetEnabled(true, true, std::string());
+  observer.Wait();
 
   // Crash the extension. Mock a reload by disabling and then enabling. The
   // extension should be reloaded and enabled.
