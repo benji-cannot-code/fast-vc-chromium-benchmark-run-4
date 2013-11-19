58b2318921274968705008b1a1f3bdaa7d2c25f0
ccameron@chromium.org <ccameron@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-19 03:58:15 +0000

    Revert 235863 "Remove blacklisting of Mac OS 10.6 and 10.7."
    > Remove blacklisting of Mac OS 10.6 and 10.7.
    > 
    > Add blacklisting of VMWare driver on 10.7 because it hangs.
    > 
    > Disable compositing expectation test on 10.7 because it will
    > give different results depending on whether it is running on
    > a VM or real hardware.
    > 
    > Expand disabling of PhishingClassifierTest.DisableDetection because it
    > flakes on all Mac versions now.
    > 
    > TBR=vangelis
    > BUG=266186
    > 
    > Review URL: https://codereview.chromium.org/62333028
    
    TBR=ccameron@chromium.org
    
    Review URL: https://codereview.chromium.org/75513004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235903 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/safe_browsing/phishing_classifier_browsertest.cc
+++ chrome/renderer/safe_browsing/phishing_classifier_browsertest.cc
@@ -29,6 +29,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "url/gurl.h"
 
+#if defined(OS_MACOSX)
+#include "base/mac/mac_util.h"
+#endif
+
 using ::testing::AllOf;
 using ::testing::Contains;
 using ::testing::Not;
@@ -269,14 +273,15 @@ IN_PROC_BROWSER_TEST_F(PhishingClassifierTest, TestClassification) {
   EXPECT_EQ(PhishingClassifier::kInvalidScore, phishy_score);
 }
 
-// This test flakes on Mac with force compositing mode for an unknown reason.
-// http://crbug.com/316709
+IN_PROC_BROWSER_TEST_F(PhishingClassifierTest, DisableDetection) {
 #if defined(OS_MACOSX)
-#define MAYBE_DisableDetection DISABLED_DisableDetection
-#else
-#define MAYBE_DisableDetection DisableDetection
+  if (base::mac::IsOSMountainLionOrLater()) {
+    // This test flakes on 10.8 only for an unknown reason.
+    // http://crbug.com/316709
+    return;
+  }
 #endif
-IN_PROC_BROWSER_TEST_F(PhishingClassifierTest, MAYBE_DisableDetection) {
+
   // No scorer yet, so the classifier is not ready.
   EXPECT_FALSE(classifier_->is_ready());
 
