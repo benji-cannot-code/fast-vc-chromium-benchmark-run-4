cf62213f09d8811483b5c41b9dbf387b35a68dfd
dominicc@chromium.org <dominicc@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-19 08:34:39 +0000

    Make Custom Element layout tests run on an attached Android device.
    BUG=234509
    
    Review URL: https://codereview.chromium.org/65043012
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162284 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/dom/custom/test-harness-utils.js
+++ third_party/WebKit/LayoutTests/fast/dom/custom/test-harness-utils.js
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Creates a frame and invokes f when the frame is ready.
 window.withFrame = function (opt_url, f) {
-    var url = typeof opt_url == 'string' ? opt_url : 'data:text/html,';
+    var url = typeof opt_url == 'string' ? opt_url : 'about:blank';
     f = f || opt_url;
 
     var frame = document.createElement('iframe');
