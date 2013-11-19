cb144093b5fa12537edc02b7c99062cae51d57a2
sievers@chromium.org <sievers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-19 19:04:26 +0000

    Android: Reenable canvas anti-aliasing
    This reverts r213238 (and r227928 which moved things around).
    
    Choose quality (crbug.com/285066) over benchmark scores
    (crbug.com/164719).
    The long-term solution for both is hw msaa (crbug.com/314214).
    
    BUG=285066,164719
    TBR=jochen@chromium.org
    NOTRY=True
    
    Review URL: https://codereview.chromium.org/60083017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236024 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/android/chrome_startup_flags.cc
+++ chrome/browser/android/chrome_startup_flags.cc
@@ -49,6 +49,4 @@ void SetChromeSpecificCommandLineFlags() {
 #if defined(GOOGLE_TV)
   SetCommandLineSwitch(switches::kPpapiInProcess);
 #endif
-  // Disable <canvas> path antialiasing.
-  SetCommandLineSwitch(switches::kDisable2dCanvasAntialiasing);
 }
