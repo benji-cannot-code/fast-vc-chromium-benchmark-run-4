4f43e6941649fb31ba073ad6da60d3bc4e72a601
pasko@chromium.org <pasko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-19 18:41:37 +0000

    Cleanup: Move PreferredCacheSize() to cache_util.h
    BUG=173425
    
    Review URL: https://codereview.chromium.org/50373004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236022 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/simple/simple_backend_impl.cc
+++ net/disk_cache/simple/simple_backend_impl.cc
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/sequenced_worker_pool.h"
 #include "base/time/time.h"
 #include "net/base/net_errors.h"
-#include "net/disk_cache/backend_impl.h"
+#include "net/disk_cache/cache_util.h"
 #include "net/disk_cache/simple/simple_entry_format.h"
 #include "net/disk_cache/simple/simple_entry_impl.h"
 #include "net/disk_cache/simple/simple_histogram_macros.h"
@@ -56,9 +56,6 @@ namespace {
 const int kDefaultMaxWorkerThreads = 50;
 
 const char kThreadNamePrefix[] = "SimpleCache";
-
-// Cache size when all other size heuristics failed.
-const uint64 kDefaultCacheSize = 80 * 1024 * 1024;
 
 // Maximum fraction of the cache that one entry can consume.
 const int kMaxFileRatio = 8;
@@ -503,12 +500,7 @@ SimpleBackendImpl::DiskStatResult SimpleBackendImpl::InitCacheStructureOnDisk(
     DCHECK(mtime_result);
     if (!result.max_size) {
       int64 available = base::SysInfo::AmountOfFreeDiskSpace(path);
-      if (available < 0)
-        result.max_size = kDefaultCacheSize;
-      else
-        // TODO(pasko): Move PreferedCacheSize() to cache_util.h. Also fix the
-        // spelling.
-        result.max_size = disk_cache::PreferedCacheSize(available);
+      result.max_size = disk_cache::PreferredCacheSize(available);
     }
     DCHECK(result.max_size);
   }
