b3635053d3934e6c26a748168b3a95f7c131d3ae
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-20 03:38:39 +0000

    Changes checkable HoverHighlightView to checkbox for a11y.
    BUG=320054
    R=stevenjb@chromium.org
    TEST=manually
    
    Review URL: https://codereview.chromium.org/76673002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236127 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/tray/hover_highlight_view.cc
+++ ash/system/tray/hover_highlight_view.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/tray/tray_constants.h"
 #include "ash/system/tray/view_click_listener.h"
 #include "grit/ui_resources.h"
+#include "ui/base/accessibility/accessible_view_state.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/canvas.h"
 #include "ui/views/controls/image_view.h"
@@ -33,7 +34,9 @@ HoverHighlightView::HoverHighlightView(ViewClickListener* listener)
       text_highlight_color_(0),
       text_default_color_(0),
       hover_(false),
-      expandable_(false) {
+      expandable_(false),
+      checkable_(false),
+      checked_(false) {
   set_notify_enter_exit_on_child(true);
 }
 
@@ -90,6 +93,8 @@ views::Label* HoverHighlightView::AddLabel(const base::string16& text,
 views::Label* HoverHighlightView::AddCheckableLabel(const base::string16& text,
                                                     gfx::Font::FontStyle style,
                                                     bool checked) {
+  checkable_ = true;
+  checked_ = checked;
   if (checked) {
     ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
     const gfx::ImageSkia* check =
@@ -129,6 +134,15 @@ bool HoverHighlightView::PerformAction(const ui::Event& event) {
     return false;
   listener_->OnViewClicked(this);
   return true;
+}
+
+void HoverHighlightView::GetAccessibleState(ui::AccessibleViewState* state) {
+  ActionableView::GetAccessibleState(state);
+
+  if (checkable_) {
+    state->role = ui::AccessibilityTypes::ROLE_CHECKBUTTON;
+    state->state = checked_ ? ui::AccessibilityTypes::STATE_CHECKED : 0;
+  }
 }
 
 gfx::Size HoverHighlightView::GetPreferredSize() {
