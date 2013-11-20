0c284405ad12cb95b2507b0a4aae4e34ec516d5e
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-20 03:24:53 +0000

    Fix alignment for bubbles with center-arrows.
    BUG=320910
    R=rsesek@chromium.org
    
    Review URL: https://codereview.chromium.org/75543008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236121 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/base_bubble_controller.mm
+++ chrome/browser/ui/cocoa/base_bubble_controller.mm
@@ -252,10 +252,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       NSSize offsets = NSMakeSize(info_bubble::kBubbleArrowXOffset +
                                   info_bubble::kBubbleArrowWidth / 2.0, 0);
       offsets = [[parentWindow_ contentView] convertSize:offsets toView:nil];
-      if ([bubble_ arrowLocation] == info_bubble::kTopRight) {
-        origin.x -= NSWidth([window frame]) - offsets.width;
-      } else {
-        origin.x -= offsets.width;
+      switch ([bubble_ arrowLocation]) {
+        case info_bubble::kTopRight:
+          origin.x -= NSWidth([window frame]) - offsets.width;
+          break;
+        case info_bubble::kTopLeft:
+          origin.x -= offsets.width;
+          break;
+        case info_bubble::kTopCenter:
+          origin.x -= NSWidth([window frame]) / 2.0;
+          break;
+        case info_bubble::kNoArrow:
+          NOTREACHED();
+          break;
       }
       break;
     }
