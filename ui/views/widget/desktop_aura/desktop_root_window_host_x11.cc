0102da18967dcd14350f3e8623ff82961bb6d7d5
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-20 20:38:36 +0000

    linux-aura: Fix dragging a single tab.
    Fix dragging a window with a single tab by dragging on the tabstrip.
    
    BUG=318147
    R=erg@chromium.org
    
    Review URL: https://codereview.chromium.org/76573004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236277 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/desktop_aura/desktop_root_window_host_x11.cc
+++ ui/views/widget/desktop_aura/desktop_root_window_host_x11.cc
@@ -113,6 +113,23 @@ const char* kAtomsToCache[] = {
   NULL
 };
 
+class ScopedCapturer {
+ public:
+  explicit ScopedCapturer(DesktopRootWindowHostX11* host)
+      : host_(host) {
+    host_->SetCapture();
+  }
+
+  ~ScopedCapturer() {
+    host_->ReleaseCapture();
+  }
+
+ private:
+  aura::RootWindowHost* host_;
+
+  DISALLOW_COPY_AND_ASSIGN(ScopedCapturer);
+};
+
 }  // namespace
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -551,7 +568,7 @@ Widget::MoveLoopResult DesktopRootWindowHostX11::RunMoveLoop(
     const gfx::Vector2d& drag_offset,
     Widget::MoveLoopSource source,
     Widget::MoveLoopEscapeBehavior escape_behavior) {
-  SetCapture();
+  ScopedCapturer capturer(this);
 
   aura::client::WindowMoveSource window_move_source =
       source == Widget::MOVE_LOOP_SOURCE_MOUSE ?
