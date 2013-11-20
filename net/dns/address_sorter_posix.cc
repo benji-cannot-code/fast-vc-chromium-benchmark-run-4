a6e4396f8d11511d53cd746b41a11403ec2044e1
szym@chromium.org <szym@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-20 09:08:19 +0000

    [net/dns] Fix check failure in AddressSorterPosix.
    If GetLocalAddress fails, |src| is empty so ToStringWithoutPort will
    CHECK. The actual intent is to log |dest|.
    
    BUG=321438
    
    Review URL: https://codereview.chromium.org/76913003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236213 0039d316-1c4b-4281-b951-d872f2087c98

--- net/dns/address_sorter_posix.cc
+++ net/dns/address_sorter_posix.cc
@@ -288,7 +288,7 @@ void AddressSorterPosix::Sort(const AddressList& list,
     rv = socket->GetLocalAddress(&src);
     if (rv != OK) {
       LOG(WARNING) << "Could not get local address for "
-                   << src.ToStringWithoutPort() << " reason " << rv;
+                   << dest.ToStringWithoutPort() << " reason " << rv;
       continue;
     }
 
