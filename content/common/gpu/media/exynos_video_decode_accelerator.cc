ac23e0c95d45e6920caf48f71da9510179b0271b
jorgelo@chromium.org <jorgelo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-20 02:40:53 +0000

    Move Exynos video decode acceleration warmup to sandboxing code.
    BUG=321209
    TEST=spring boots, video works, histogram shows video acceleration enabled.
    
    Review URL: https://codereview.chromium.org/76393003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236108 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/media/exynos_video_decode_accelerator.cc
+++ content/common/gpu/media/exynos_video_decode_accelerator.cc
@@ -58,7 +58,6 @@ namespace {
 #endif
 
 const char kExynosMfcDevice[] = "/dev/mfc-dec";
-const char kMaliDriver[] = "libmali.so";
 
 }  // anonymous namespace
 
@@ -495,15 +494,6 @@ void ExynosVideoDecodeAccelerator::Destroy() {
 }
 
 bool ExynosVideoDecodeAccelerator::CanDecodeOnIOThread() { return true; }
-
-// static
-void ExynosVideoDecodeAccelerator::PreSandboxInitialization() {
-  DVLOG(3) << "PreSandboxInitialization()";
-  dlerror();
-  if (!dlopen(kMaliDriver, RTLD_LAZY | RTLD_LOCAL)) {
-    DPLOG(ERROR) << "failed to dlopen()" << kMaliDriver << ": " << dlerror();
-  }
-}
 
 void ExynosVideoDecodeAccelerator::DecodeTask(
     const media::BitstreamBuffer& bitstream_buffer) {
