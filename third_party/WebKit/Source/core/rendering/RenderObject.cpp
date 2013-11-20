6cec86cca2f8a42966cc553088e8ba6a048f5922
mitica@adobe.com <mitica@adobe.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-20 13:25:30 +0000

    [CSS Blending] Fix isolation for the hardware path. For "isolation:isolate" with accelerated elements, an ASSERT was triggered in RenderLayerCompositor::subtreeReasonsForCompositing. This was caused by errorneously including |hasIsolation| to RenderObject::CreateGroup. Also, removing the comment from the method since it is misleading.
    BUG=229615
    
    Review URL: https://codereview.chromium.org/77723004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162360 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderObject.cpp
+++ third_party/WebKit/Source/core/rendering/RenderObject.cpp
@@ -2950,11 +2950,6 @@ bool RenderObject::hasBlendMode() const
     return RuntimeEnabledFeatures::cssCompositingEnabled() && style() && style()->hasBlendMode();
 }
 
-bool RenderObject::hasIsolation() const
-{
-    return RuntimeEnabledFeatures::cssCompositingEnabled() && style() && style()->hasIsolation();
-}
-
 static Color decorationColor(const RenderObject* object, RenderStyle* style)
 {
     Color result;
