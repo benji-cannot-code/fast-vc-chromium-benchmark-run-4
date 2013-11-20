e762446c533897491396935d5151a1967a6bb2f4
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-20 05:45:45 +0000

    [SyncFS] Implement local deletion handling on LocalToRemoteSyncer
    BUG=240165
    
    Review URL: https://codereview.chromium.org/76093002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236170 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_backend/local_to_remote_syncer.h
+++ chrome/browser/sync_file_system/drive_backend/local_to_remote_syncer.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_SYNC_FILE_SYSTEM_DRIVE_BACKEND_LOCAL_TO_REMOTE_SYNCER_H_
 
 #include "base/memory/weak_ptr.h"
+#include "chrome/browser/google_apis/gdata_errorcode.h"
 #include "chrome/browser/sync_file_system/file_change.h"
 #include "chrome/browser/sync_file_system/sync_callbacks.h"
 #include "chrome/browser/sync_file_system/sync_file_metadata.h"
@@ -41,6 +42,9 @@ class LocalToRemoteSyncer : public SyncTask {
   void HandleMissingRemoteFile(const SyncStatusCallback& callback);
   void HandleConflict(const SyncStatusCallback& callback);
   void HandleExistingRemoteFile(const SyncStatusCallback& callback);
+
+  void DidDeleteRemoteFile(const SyncStatusCallback& callback,
+                           google_apis::GDataErrorCode error);
 
   bool PopulateRemoteParentFolder();
 
