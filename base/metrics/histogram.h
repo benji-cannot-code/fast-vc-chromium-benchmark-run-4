172f68327a0308950543c140b30e0f8d4e22b8d7
etienneb@chromium.org <etienneb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-20 08:37:34 +0000

    Fix multiples typos while reading histogram. R=isherman@chromium.org BUG=
    Review URL: https://codereview.chromium.org/76543002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236206 0039d316-1c4b-4281-b951-d872f2087c98

--- base/metrics/histogram.h
+++ base/metrics/histogram.h
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // agree exactly in type, bucket size and range.
 
 // For Histogram and LinearHistogram, the maximum for a declared range should
-// always be larger (not equal) than minmal range. Zero and
+// always be larger (not equal) than minimal range. Zero and
 // HistogramBase::kSampleType_MAX are implicitly added as first and last ranges,
 // so the smallest legal bucket_count is 3. However CustomHistogram can have
 // bucket count as 2 (when you give a custom ranges vector containing only 1
@@ -97,7 +97,7 @@ class Lock;
 // process.
 
 // The following code is generally what a thread-safe static pointer
-// initializaion looks like for a histogram (after a macro is expanded).  This
+// initialization looks like for a histogram (after a macro is expanded).  This
 // sample is an expansion (with comments) of the code for
 // HISTOGRAM_CUSTOM_COUNTS().
 
@@ -108,7 +108,7 @@ class Lock;
     static base::subtle::AtomicWord atomic_histogram_pointer = 0;
 
     // Acquire_Load() ensures that we acquire visibility to the pointed-to data
-    // in the histogrom.
+    // in the histogram.
     base::Histogram* histogram_pointer(reinterpret_cast<base::Histogram*>(
         base::subtle::Acquire_Load(&atomic_histogram_pointer)));
 
@@ -422,7 +422,7 @@ class BASE_EXPORT Histogram : public HistogramBase {
   virtual int FindCorruption(const HistogramSamples& samples) const OVERRIDE;
 
   //----------------------------------------------------------------------------
-  // Accessors for factory constuction, serialization and testing.
+  // Accessors for factory construction, serialization and testing.
   //----------------------------------------------------------------------------
   Sample declared_min() const { return declared_min_; }
   Sample declared_max() const { return declared_max_; }
@@ -605,7 +605,7 @@ class BASE_EXPORT LinearHistogram : public Histogram {
   static HistogramBase* DeserializeInfoImpl(PickleIterator* iter);
 
   // For some ranges, we store a printable description of a bucket range.
-  // If there is no desciption, then GetAsciiBucketRange() uses parent class
+  // If there is no description, then GetAsciiBucketRange() uses parent class
   // to provide a description.
   typedef std::map<Sample, std::string> BucketDescriptionMap;
   BucketDescriptionMap bucket_description_;
