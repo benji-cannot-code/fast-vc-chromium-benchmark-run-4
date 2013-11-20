a5ed608cce73bf37ef66b8524c20a4eaf41dbdfb
jdduke@chromium.org <jdduke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-20 16:52:51 +0000

    [Android] Fix x86 compilation with multiply defined command_line.cc
    Multiple .cc files cannot be defined in a given compilation unit for x86 Android
    Android builds. Rename base/android's command_line.{h,cc} to
    command_line_android.{h,cc}.
    
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/77473002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236257 0039d316-1c4b-4281-b951-d872f2087c98

--- base/android/base_jni_registrar.cc
+++ base/android/base_jni_registrar.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/android/activity_status.h"
 #include "base/android/build_info.h"
-#include "base/android/command_line.h"
+#include "base/android/command_line_android.h"
 #include "base/android/content_uri_utils.h"
 #include "base/android/cpu_features.h"
 #include "base/android/important_file_writer_android.h"
--- content/app/android/library_loader_hooks.cc
+++ content/app/android/library_loader_hooks.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/app/android_library_loader_hooks.h"
 
 #include "base/android/base_jni_registrar.h"
-#include "base/android/command_line.h"
+#include "base/android/command_line_android.h"
 #include "base/android/jni_android.h"
 #include "base/android/jni_registrar.h"
 #include "base/android/jni_string.h"
