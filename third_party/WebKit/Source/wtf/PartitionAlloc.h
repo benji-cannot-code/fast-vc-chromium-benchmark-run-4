81ab6e35404cbd8d023afb5dcb7b31d322904b11
tsepez@chromium.org <tsepez@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-20 19:25:18 +0000

    Change PartitonAlloc max size constant to ~2GB.
    There is one additional constant beyond the work in https://src.chromium.org/viewvc/blink?view=rev&revision=162149 that needs to be incresed to get back to parity with TCMalloc.
    
    BUG=319393
    R=cevans@chromium.org,jochen@chromium.org
    
    Review URL: https://codereview.chromium.org/76573003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162394 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/PartitionAlloc.h
+++ third_party/WebKit/Source/wtf/PartitionAlloc.h
@@ -106,11 +106,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WTF {
 
-// Maximum size of a partition's mappings. 1GB. Note that the total amount of
+// Maximum size of a partition's mappings. 2047MB. Note that the total amount of
 // bytes allocatable at the API will be smaller. This is because things like
 // guard pages, metadata, page headers and wasted space come out of the total.
 // The 1GB is not necessarily contiguous in virtual address space.
-static const size_t kMaxPartitionSize = 1024 * 1024 * 1024;
+static const size_t kMaxPartitionSize = 2047u * 1024u * 1024u;
 // Allocation granularity of sizeof(void*) bytes.
 static const size_t kAllocationGranularity = sizeof(void*);
 static const size_t kAllocationGranularityMask = kAllocationGranularity - 1;
