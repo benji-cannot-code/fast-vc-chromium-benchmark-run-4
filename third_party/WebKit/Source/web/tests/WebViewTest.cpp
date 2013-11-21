87da39da9240a91dda7997416dd7c1bce69f321e
schenney@chromium.org <schenney@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-21 18:28:31 +0000

    Fix the Android compile.
    Be warned, fellow travellers, Android cares more about warnings than
    other platforms.
    
    TBR=
    BUG=
    
    Review URL: https://codereview.chromium.org/81273003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162483 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/tests/WebViewTest.cpp
+++ third_party/WebKit/Source/web/tests/WebViewTest.cpp
@@ -1290,7 +1290,6 @@ TEST_F(WebViewTest, ChooseValueFromDateTimeChooser)
     WebViewImpl* webViewImpl = toWebViewImpl(m_webViewHelper.initializeAndLoad(url, true, 0, &client));
 
     WebCore::Document* document = webViewImpl->mainFrameImpl()->frame()->document();
-    WebCore::Page* page = document->page();
 
     WebCore::HTMLInputElement* inputElement;
 
