3c0f7ebb42d991e1b6cb67422d3b595ab892a8eb
iancottrell@chromium.org <iancottrell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-21 13:57:12 +0000

    Adding linux support to cr.
    BUG=316397
    
    Review URL: https://codereview.chromium.org/79203002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236525 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/cr/cr/base/linux.py
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""The linux specific host and platform implementation module."""
+
+import os
+
+import cr
+
+
+class LinuxHost(cr.Host):
+  """The implementation of Host for linux."""
+
+  ACTIVE = cr.Config.From(
+      GOOGLE_CODE='/usr/local/google/code',
+  )
+
+  def __init__(self):
+    super(LinuxHost, self).__init__()
+
+  def Matches(self):
+    return cr.Platform.System() == 'Linux'
+
+
+class LinuxPlatform(cr.Platform):
+  """The implementation of Platform for the linux target."""
+
+  ACTIVE = cr.Config.From(
+      CR_BINARY=os.path.join('{CR_BUILD_DIR}', '{CR_BUILD_TARGET}'),
+      CHROME_DEVEL_SANDBOX='/usr/local/sbin/chrome-devel-sandbox',
+  )
+
+  @property
+  def enabled(self):
+    return cr.Platform.System() == 'Linux'
+
+  @property
+  def priority(self):
+    return 2
+
+  @property
+  def paths(self):
+    return ['{GOMA_DIR}']
