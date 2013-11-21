006df3b8d46b5f8ddd1d79d262432b3dbbd46eca
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-21 20:56:13 +0000

    linux_aura: Double clicking the titlebar should toggle maximized.
    This needs to be handled manually on Linux aura builds.
    
    BUG=317486
    
    Review URL: https://codereview.chromium.org/78393003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236592 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/desktop_aura/x11_window_event_filter.cc
+++ ui/views/widget/desktop_aura/x11_window_event_filter.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/event.h"
 #include "ui/events/event_utils.h"
 #include "ui/gfx/x/x11_types.h"
+#include "ui/views/widget/desktop_aura/desktop_root_window_host.h"
 #include "ui/views/widget/native_widget_aura.h"
 
 namespace {
@@ -60,11 +61,13 @@ const char* kAtomsToCache[] = {
 namespace views {
 
 X11WindowEventFilter::X11WindowEventFilter(
-    aura::RootWindow* root_window)
+    aura::RootWindow* root_window,
+    DesktopRootWindowHost* root_window_host)
     : xdisplay_(gfx::GetXDisplay()),
       xwindow_(root_window->host()->GetAcceleratedWidget()),
       x_root_window_(DefaultRootWindow(xdisplay_)),
       atom_cache_(xdisplay_, kAtomsToCache),
+      root_window_host_(root_window_host),
       is_active_(false) {
 }
 
@@ -100,6 +103,18 @@ void X11WindowEventFilter::OnMouseEvent(ui::MouseEvent* event) {
       target->delegate()->GetNonClientComponent(event->location());
   if (component == HTCLIENT)
     return;
+
+  if (event->flags() & ui::EF_IS_DOUBLE_CLICK && component == HTCAPTION) {
+    // Our event is a double click in the caption area. We are responsible for
+    // dispatching this as a minimize/maximize on X11 (Windows converts this to
+    // min/max events for us).
+    if (root_window_host_->IsMaximized())
+      root_window_host_->Restore();
+    else
+      root_window_host_->Maximize();
+    event->SetHandled();
+    return;
+  }
 
   // Get the |x_root_window_| location out of the native event.
   if (event->native_event()) {
