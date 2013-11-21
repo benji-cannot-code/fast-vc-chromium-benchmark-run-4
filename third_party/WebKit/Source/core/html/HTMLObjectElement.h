8e4ecf1a57e93db97e23d300c257d74f005fbec2
ch.dumez@samsung.com <ch.dumez@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-21 03:40:20 +0000

    Revert "Finding an <object> element by name only works it has no children other than <param> elements"
    This reverts commit 0e0a6fd6f7b0f1b34c474e785068eb5e59f6ceb3.
    It broke some websites, including http://map.d.co.il where the map no longer shows up.
    
    R=tkent
    BUG=319662
    
    Review URL: https://codereview.chromium.org/61213008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162435 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLObjectElement.h
+++ third_party/WebKit/Source/core/html/HTMLObjectElement.h
@@ -36,6 +36,8 @@ public:
     static PassRefPtr<HTMLObjectElement> create(Document&, HTMLFormElement*, bool createdByParser);
     virtual ~HTMLObjectElement();
 
+    bool isDocNamedItem() const { return m_docNamedItem; }
+
     const String& classId() const { return m_classId; }
 
     bool containsJavaApplet() const;
@@ -88,6 +90,7 @@ private:
     virtual void addSubresourceAttributeURLs(ListHashSet<KURL>&) const;
 
     virtual void updateWidget(PluginCreationOption);
+    void updateDocNamedItem();
 
     void reattachFallbackContent();
 
@@ -104,10 +107,11 @@ private:
     virtual void derefFormAssociatedElement() { deref(); }
     virtual HTMLFormElement* virtualForm() const;
 
-    virtual bool shouldRegisterAsNamedItem() const OVERRIDE { return true; }
-    virtual bool shouldRegisterAsExtraNamedItem() const OVERRIDE { return true; }
+    virtual bool shouldRegisterAsNamedItem() const OVERRIDE { return isDocNamedItem(); }
+    virtual bool shouldRegisterAsExtraNamedItem() const OVERRIDE { return isDocNamedItem(); }
 
     String m_classId;
+    bool m_docNamedItem : 1;
     bool m_useFallbackContent : 1;
 };
 
