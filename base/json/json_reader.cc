645bfc40ecee1421de9e08a912a2aa3d6fddf86a
gab@chromium.org <gab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-21 07:44:09 +0000

    Clarify the interface of JSONStringValueSerializer::Deserialize which can only return JSONParseError codes as an error_code.
    Also make sure JsonParseError codes do not go beyond JsonFileError codes which are used in conjunction for JSONFileValueSerializer.
    
    Review URL: https://codereview.chromium.org/71653002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236445 0039d316-1c4b-4281-b951-d872f2087c98

--- base/json/json_reader.cc
+++ base/json/json_reader.cc
@@ -10,6 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
+// Values 1000 and above are used by JSONFileValueSerializer::JsonFileError.
+COMPILE_ASSERT(JSONReader::JSON_PARSE_ERROR_COUNT < 1000,
+               json_reader_error_out_of_bounds);
+
 const char* JSONReader::kInvalidEscape =
     "Invalid escape sequence.";
 const char* JSONReader::kSyntaxError =
