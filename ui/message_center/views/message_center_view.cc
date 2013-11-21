fdf41a525c116663db9a0b839f5142496e44261c
dewittj@chromium.org <dewittj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-21 03:45:40 +0000

    Make animating the second notification in the message center work.
    This regressed in r234064.
    
    BUG=321771,312890
    
    Review URL: https://codereview.chromium.org/77043008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236390 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/message_center/views/message_center_view.cc
+++ ui/message_center/views/message_center_view.cc
@@ -523,7 +523,7 @@ void MessageListView::AnimateNotificationsAboveTarget() {
       break;
     }
   }
-  if (last_index > 0) {
+  if (last_index >= 0) {
     int between_items =
         kMarginBetweenItems - MessageView::GetShadowInsets().bottom();
     int bottom = (reposition_top_ > 0)
