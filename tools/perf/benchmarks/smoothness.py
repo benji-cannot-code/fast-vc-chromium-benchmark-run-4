4f14faa05d8118e94f63be5126fe3403d91c810f
ernstm@chromium.org <ernstm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-21 18:22:39 +0000

    telemetry: re-enable smoothness.key_mobile_sites
    This benchmark should work when crrev.com/68763011 has landed.
    
    R=tonyg@chromium.org
    BUG=
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/79883003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236550 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/benchmarks/smoothness.py
+++ tools/perf/benchmarks/smoothness.py
@@ -2,7 +2,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright (c) 2013 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-import sys
 from telemetry import test
 
 from measurements import smoothness
@@ -25,7 +24,5 @@ class SmoothnessKeyMobileSites(test.Test):
   """Measures rendering statistics while scrolling down the key mobile sites.
 
   http://www.chromium.org/developers/design-documents/rendering-benchmarks"""
-  # TODO(ernstm): remove when crbug.com/313753 is fixed.
-  enabled = sys.platform != 'linux2'
   test = smoothness.Smoothness
   page_set = 'page_sets/key_mobile_sites.json'
