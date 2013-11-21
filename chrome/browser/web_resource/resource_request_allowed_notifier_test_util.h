086723e293c6a7ca467a0aefdef6180db7fb1274
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-21 07:38:06 +0000

    VariationsService: Log reason why requests aren't allowed.
    Extends existing Variations.ResourceRequestsAllowed histogram to
    log the reason why requests weren't allowed.
    
    BUG=321677
    
    Review URL: https://codereview.chromium.org/78963002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236443 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/web_resource/resource_request_allowed_notifier_test_util.h
+++ chrome/browser/web_resource/resource_request_allowed_notifier_test_util.h
@@ -35,7 +35,7 @@ class TestRequestAllowedNotifier : public ResourceRequestAllowedNotifier {
   void NotifyObserver();
 
   // ResourceRequestAllowedNotifier overrides:
-  virtual bool ResourceRequestsAllowed() OVERRIDE;
+  virtual State GetResourceRequestsAllowedState() OVERRIDE;
   virtual EulaAcceptedNotifier* CreateEulaNotifier() OVERRIDE;
 
  private:
