8f141193ac9a49a30cafcc9b171014831e27bbc4
shrikant@chromium.org <shrikant@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-21 08:35:04 +0000

    Fixing unintended Metro chrome activation during shutdown.
    R=sky@chromium.org, tapted@chromium.org, cpu, sky
    TBR=tapted@chromium.org
    BUG=321738,315385
    
    Review URL: https://codereview.chromium.org/79273003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236457 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/frame/browser_frame_ashwin.cc
+++ chrome/browser/ui/views/frame/browser_frame_ashwin.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/message_loop/message_loop.h"
+#include "chrome/browser/browser_shutdown.h"
 #include "chrome/browser/metro_utils/metro_chrome_win.h"
 #include "ui/aura/remote_root_window_host_win.h"
 
@@ -24,9 +25,14 @@ void BrowserFrameAshWin::OnWindowFocused(aura::Window* gained_focus,
   if (GetNativeWindow() != gained_focus)
     return;
 
+  // TODO(shrikant): We need better way to handle chrome activation.
+  // There may be cases where focus events do not follow a user
+  // action to create or focus a window
+
   // If the activated window is in Metro mode, and the viewer process window is
   // not in the foreground, activate Metro Chrome.
-  if (!aura::RemoteRootWindowHostWin::Instance()->IsForegroundWindow()) {
+  if (!aura::RemoteRootWindowHostWin::Instance()->IsForegroundWindow() &&
+      !browser_shutdown::IsTryingToQuit()) {
     // PostTask because ActivateMetroChrome can not be called nested in another
     // ::SendMessage().
     base::MessageLoop::current()->PostTask(
