107c5af9a7e0cdfc2d1f5a4aaa3e7eb23908e404
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-21 12:24:54 +0000

    Make it possible to run the histograms XML validator from directories other than tools/metrics/histograms.
    Review URL: https://codereview.chromium.org/80433003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236508 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/metrics/histograms/validate_format.py
+++ tools/metrics/histograms/validate_format.py
@@ -6,11 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """Verifies that the histograms XML file is well-formatted."""
 
 import extract_histograms
+import os.path
 
 
 def main():
   # This will raise an exception if the file is not well-formatted.
-  histograms = extract_histograms.ExtractHistograms('histograms.xml')
+  xml_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                          'histograms.xml')
+  histograms = extract_histograms.ExtractHistograms(xml_file)
 
 
 if __name__ == '__main__':
