71d428b78a0fb382a99a17dc08f18b2129fe8ab2
maniscalco@chromium.org <maniscalco@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-21 20:55:44 +0000

    Don't clear debug info until after it has been sent to the server.
    Replaced DebugInfoGetter::GetAndClearDebugInfo with GetDebugInfo and ClearDebugInfo so we can clear the debug info only after we have successfully sent it to the server.
    
    Moved MockDebugInfoGetter into its own file so it can be used by other tests.
    
    BUG=319937
    
    Review URL: https://codereview.chromium.org/61213009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236591 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/sync_tests.gypi
+++ sync/sync_tests.gypi
@@ -57,6 +57,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'test/null_transaction_observer.cc',
         'test/null_transaction_observer.h',
         'test/sessions/test_scoped_session_event_listener.h',
+        'test/sessions/mock_debug_info_getter.h',
+        'test/sessions/mock_debug_info_getter.cc',
         'test/test_directory_backing_store.cc',
         'test/test_directory_backing_store.h',
         'test/test_transaction_observer.cc',
---
+++ sync/test/sessions/mock_debug_info_getter.cc
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "sync/test/sessions/mock_debug_info_getter.h"
+
+namespace syncer {
+namespace sessions {
+
+MockDebugInfoGetter::MockDebugInfoGetter() {
+}
+
+MockDebugInfoGetter::~MockDebugInfoGetter() {
+}
+
+void MockDebugInfoGetter::GetDebugInfo(sync_pb::DebugInfo* debug_info) {
+  debug_info->CopyFrom(debug_info_);
+}
+
+void MockDebugInfoGetter::ClearDebugInfo() {
+  debug_info_.Clear();
+}
+
+void MockDebugInfoGetter::AddDebugEvent() {
+  debug_info_.add_events();
+}
+
+}  // namespace sessions
+}  // namespace syncer
