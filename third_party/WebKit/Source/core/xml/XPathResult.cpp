a93201a49f4b00a76df4057b5e570ffe10e3a4c0
mkwst@chromium.org <mkwst@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-21 13:29:42 +0000

    Improve XPathResult exception messages.
    BUG=270033
    
    Review URL: https://codereview.chromium.org/77763005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162474 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/xml/XPathResult.cpp
+++ third_party/WebKit/Source/core/xml/XPathResult.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "core/xml/XPathResult.h"
 
+#include "bindings/v8/ExceptionMessages.h"
 #include "bindings/v8/ExceptionState.h"
 #include "core/dom/Document.h"
 #include "core/dom/ExceptionCode.h"
@@ -90,14 +91,14 @@ void XPathResult::convertTo(unsigned short type, ExceptionState& exceptionState)
         case ANY_UNORDERED_NODE_TYPE:
         case FIRST_ORDERED_NODE_TYPE: // This is correct - singleNodeValue() will take care of ordering.
             if (!m_value.isNodeSet()) {
-                exceptionState.throwUninformativeAndGenericTypeError();
+                exceptionState.throwTypeError(ExceptionMessages::failedToExecute("evaluate", "XPathExpression", "The result is not a node set, and therefore cannot be converted to the desired type."));
                 return;
             }
             m_resultType = type;
             break;
         case ORDERED_NODE_ITERATOR_TYPE:
             if (!m_value.isNodeSet()) {
-                exceptionState.throwUninformativeAndGenericTypeError();
+                exceptionState.throwTypeError(ExceptionMessages::failedToExecute("evaluate", "XPathExpression", "The result is not a node set, and therefore cannot be converted to the desired type."));
                 return;
             }
             m_nodeSet.sort();
@@ -105,7 +106,7 @@ void XPathResult::convertTo(unsigned short type, ExceptionState& exceptionState)
             break;
         case ORDERED_NODE_SNAPSHOT_TYPE:
             if (!m_value.isNodeSet()) {
-                exceptionState.throwUninformativeAndGenericTypeError();
+                exceptionState.throwTypeError(ExceptionMessages::failedToExecute("evaluate", "XPathExpression", "The result is not a node set, and therefore cannot be converted to the desired type."));
                 return;
             }
             m_value.toNodeSet().sort();
@@ -122,7 +123,7 @@ unsigned short XPathResult::resultType() const
 double XPathResult::numberValue(ExceptionState& exceptionState) const
 {
     if (resultType() != NUMBER_TYPE) {
-        exceptionState.throwUninformativeAndGenericTypeError();
+        exceptionState.throwTypeError(ExceptionMessages::failedToGet("numberValue", "XPathResult", "The result type is not a number."));
         return 0.0;
     }
     return m_value.toNumber();
@@ -131,7 +132,7 @@ double XPathResult::numberValue(ExceptionState& exceptionState) const
 String XPathResult::stringValue(ExceptionState& exceptionState) const
 {
     if (resultType() != STRING_TYPE) {
-        exceptionState.throwUninformativeAndGenericTypeError();
+        exceptionState.throwTypeError(ExceptionMessages::failedToGet("stringValue", "XPathResult", "The result type is not a string."));
         return String();
     }
     return m_value.toString();
@@ -140,7 +141,7 @@ String XPathResult::stringValue(ExceptionState& exceptionState) const
 bool XPathResult::booleanValue(ExceptionState& exceptionState) const
 {
     if (resultType() != BOOLEAN_TYPE) {
-        exceptionState.throwUninformativeAndGenericTypeError();
+        exceptionState.throwTypeError(ExceptionMessages::failedToGet("booleanValue", "XPathResult", "The result type is not a boolean."));
         return false;
     }
     return m_value.toBoolean();
@@ -149,7 +150,7 @@ bool XPathResult::booleanValue(ExceptionState& exceptionState) const
 Node* XPathResult::singleNodeValue(ExceptionState& exceptionState) const
 {
     if (resultType() != ANY_UNORDERED_NODE_TYPE && resultType() != FIRST_ORDERED_NODE_TYPE) {
-        exceptionState.throwUninformativeAndGenericTypeError();
+        exceptionState.throwTypeError(ExceptionMessages::failedToGet("singleNodeValue", "XPathResult", "The result type is not a single node."));
         return 0;
     }
 
@@ -172,7 +173,7 @@ bool XPathResult::invalidIteratorState() const
 unsigned long XPathResult::snapshotLength(ExceptionState& exceptionState) const
 {
     if (resultType() != UNORDERED_NODE_SNAPSHOT_TYPE && resultType() != ORDERED_NODE_SNAPSHOT_TYPE) {
-        exceptionState.throwUninformativeAndGenericTypeError();
+        exceptionState.throwTypeError(ExceptionMessages::failedToGet("snapshotLength", "XPathResult", "The result type is not a snapshot."));
         return 0;
     }
 
@@ -182,12 +183,12 @@ unsigned long XPathResult::snapshotLength(ExceptionState& exceptionState) const
 Node* XPathResult::iterateNext(ExceptionState& exceptionState)
 {
     if (resultType() != UNORDERED_NODE_ITERATOR_TYPE && resultType() != ORDERED_NODE_ITERATOR_TYPE) {
-        exceptionState.throwUninformativeAndGenericTypeError();
+        exceptionState.throwTypeError(ExceptionMessages::failedToExecute("iterateNext", "XPathResult", "The result type is not an iterator."));
         return 0;
     }
 
     if (invalidIteratorState()) {
-        exceptionState.throwUninformativeAndGenericDOMException(InvalidStateError);
+        exceptionState.throwDOMException(InvalidStateError, ExceptionMessages::failedToExecute("iterateNext", "XPathResult", "The document has mutated since the result was returned."));
         return 0;
     }
 
@@ -204,7 +205,7 @@ Node* XPathResult::iterateNext(ExceptionState& exceptionState)
 Node* XPathResult::snapshotItem(unsigned long index, ExceptionState& exceptionState)
 {
     if (resultType() != UNORDERED_NODE_SNAPSHOT_TYPE && resultType() != ORDERED_NODE_SNAPSHOT_TYPE) {
-        exceptionState.throwUninformativeAndGenericTypeError();
+        exceptionState.throwTypeError(ExceptionMessages::failedToExecute("iterateNext", "XPathResult", "The result type is not a snapshot."));
         return 0;
     }
 
