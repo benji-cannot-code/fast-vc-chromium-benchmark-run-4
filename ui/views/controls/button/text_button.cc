fcc73093f028d9f0f9683e1c560b6e7a84ceadcf
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-22 22:24:58 +0000

    Remove unnecessary Border subclasses where we can achieve the same functionality without subclassing.
    BUG=none
    TEST=none
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/81833007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236844 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/button/text_button.cc
+++ ui/views/controls/button/text_button.cc
@@ -75,6 +75,9 @@ TextButtonBorder::TextButtonBorder() {
 TextButtonBorder::~TextButtonBorder() {
 }
 
+void TextButtonBorder::Paint(const View& view, gfx::Canvas* canvas) {
+}
+
 gfx::Insets TextButtonBorder::GetInsets() const {
   return insets_;
 }
