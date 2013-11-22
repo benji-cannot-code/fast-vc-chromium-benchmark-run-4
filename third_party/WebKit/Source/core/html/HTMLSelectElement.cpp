eb0c88e7f3c6fad3d45609b754e6158d698b1687
sigbjornf@opera.com <sigbjornf@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-22 19:35:15 +0000

    Have the HTMLSelectElement setter throw TypeError.
    If an argument that fails to convert to 'HTMLOptionElement?' is passed
    to the option setter/remover on HTMLSelectElement, throw TypeError.
    
    This follows WebIDL and Gecko. Other implementations don't throw, but
    ignore such operations.
    
    R=
    BUG=270033
    
    Review URL: https://codereview.chromium.org/83743002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162559 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLSelectElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLSelectElement.cpp
@@ -1568,7 +1568,7 @@ void HTMLSelectElement::finishParsingChildren()
 bool HTMLSelectElement::anonymousIndexedSetter(unsigned index, PassRefPtr<HTMLOptionElement> value, ExceptionState& exceptionState)
 {
     if (!value) {
-        exceptionState.throwDOMException(TypeMismatchError, ExceptionMessages::failedToSet(String::number(index), "HTMLSelectElement", "The value provided was not an HTMLOptionElement."));
+        exceptionState.throwTypeError(ExceptionMessages::failedToSet(String::number(index), "HTMLSelectElement", "The value provided was not an HTMLOptionElement."));
         return false;
     }
     setOption(index, value.get(), exceptionState);
