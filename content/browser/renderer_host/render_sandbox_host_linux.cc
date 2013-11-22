85f4eab85134dc4fb3953598496da5c978091e7b
jln@chromium.org <jln@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-22 00:39:24 +0000

    Linux: initialize RenderSandboxHostLinux before threads are started.
    RenderSandboxHostLinux::Init() uses fork(). Therefore the process needs
    to only have one thread when this is called.
    
    Move RenderSandboxHostLinux early enough that on one thread exists.
    
    BUG=321388
    R=creis@chromium.org, kbr@chromium.org, oshima@chromium.org
    
    Review URL: https://codereview.chromium.org/77123002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236651 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_sandbox_host_linux.cc
+++ content/browser/renderer_host/render_sandbox_host_linux.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/posix/eintr_wrapper.h"
 #include "base/posix/unix_domain_socket_linux.h"
 #include "base/process/launch.h"
+#include "base/process/process_metrics.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "content/child/webkitplatformsupport_impl.h"
@@ -708,6 +709,8 @@ void RenderSandboxHostLinux::Init(const std::string& sandbox_path) {
   const int child_lifeline_fd = pipefds[0];
   childs_lifeline_fd_ = pipefds[1];
 
+  // We need to be monothreaded before we fork().
+  DCHECK_EQ(1, base::GetNumberOfThreads(base::GetCurrentProcessHandle()));
   pid_ = fork();
   if (pid_ == 0) {
     if (HANDLE_EINTR(close(fds[0])) < 0)
