4c1105d56e6e65217de351915822754d472aa045
robliao@chromium.org <robliao@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-22 07:51:19 +0000

    Track Dynamically Assigned Command IDs in Background Menu Generation
    Menu items require that Command IDs be unique. For a single profile,
    collisions don't happen. For multiple profiles, the profile menu
    as well as its submenus start at Command ID 0, confusing the menu
    system when it tries to identify or change and item.
    
    The fix here creates an vector to hold the extension indexes
    using the vector index as a Command ID. For profile menu labels,
    the extension index is -1.
    
    As a bonus, we now clean up the background submenus when multiple profiles exist. Previously, the submenus were leaking away.
    
    BUG=315164
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=236489
    
    Review URL: https://codereview.chromium.org/75973004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236717 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/background/background_mode_manager.h
+++ chrome/browser/background/background_mode_manager.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 
 #include "base/gtest_prod_util.h"
+#include "base/memory/scoped_vector.h"
 #include "base/prefs/pref_change_registrar.h"
 #include "chrome/browser/background/background_application_list_model.h"
 #include "chrome/browser/profiles/profile_info_cache_observer.h"
@@ -30,6 +31,8 @@ class StatusTray;
 namespace extensions {
 class Extension;
 }
+
+typedef std::vector<int> CommandIdExtensionVector;
 
 // BackgroundModeManager is responsible for switching Chrome into and out of
 // "background mode" and for providing UI for the user to exit Chrome when there
@@ -102,14 +105,16 @@ class BackgroundModeManager
                            ProfileInfoCacheObserver);
   FRIEND_TEST_ALL_PREFIXES(BackgroundModeManagerTest,
                            BackgroundMenuGeneration);
+  FRIEND_TEST_ALL_PREFIXES(BackgroundModeManagerTest,
+                           BackgroundMenuGenerationMultipleProfile);
   FRIEND_TEST_ALL_PREFIXES(BackgroundAppBrowserTest,
                            ReloadBackgroundApp);
 
   class BackgroundModeData : public StatusIconMenuModel::Delegate {
    public:
     explicit BackgroundModeData(
-        int command_id,
-        Profile* profile);
+        Profile* profile,
+        CommandIdExtensionVector* command_id_extension_vector);
     virtual ~BackgroundModeData();
 
     // The cached list of BackgroundApplications.
@@ -146,14 +151,17 @@ class BackgroundModeManager
                                           const BackgroundModeData* bmd2);
 
    private:
-    // Command id for the sub menu for this BackgroundModeData.
-    int command_id_;
-
     // Name associated with this profile which is used to label its submenu.
     string16 name_;
 
     // The profile associated with this background app data.
     Profile* profile_;
+
+    // Weak ref vector owned by BackgroundModeManager where the
+    // indices correspond to Command IDs and values correspond to
+    // extension indices. A value of -1 indicates no extension is associated
+    // with the index.
+    CommandIdExtensionVector* command_id_extension_vector_;
   };
 
   // Ideally we would want our BackgroundModeData to be scoped_ptrs,
@@ -293,6 +301,12 @@ class BackgroundModeManager
   // The profile-keyed data for this background mode manager. Keyed on profile.
   BackgroundModeInfoMap background_mode_data_;
 
+  // Contains the dynamic Command IDs for the entire background menu.
+  CommandIdExtensionVector command_id_extension_vector_;
+
+  // Maintains submenu lifetime for the multiple profile context menu.
+  ScopedVector<StatusIconMenuModel> submenus;
+
   // Reference to our status tray. If null, the platform doesn't support status
   // icons.
   StatusTray* status_tray_;
@@ -324,9 +338,6 @@ class BackgroundModeManager
 
   // Set to true when background mode is keeping Chrome alive.
   bool keeping_alive_;
-
-  // Provides a command id for each profile as they are created.
-  int current_command_id_;
 
   DISALLOW_COPY_AND_ASSIGN(BackgroundModeManager);
 };
