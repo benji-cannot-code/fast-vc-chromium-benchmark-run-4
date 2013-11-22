776f2fa6d3a058a7dd79eef849d5e3e38c8b881f
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-22 22:58:16 +0000

    ash: Add missing include to test from r236852.
    Add a missing base/threading/sequenced_worker_pool.h include
    to desktop_background_controller_unittest.cc.
    
    BUG=none
    TBR=brettw@chromium.org
    
    Review URL: https://codereview.chromium.org/83153007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236854 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/desktop_background/desktop_background_controller_unittest.cc
+++ ash/desktop_background/desktop_background_controller_unittest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/message_loop/message_loop.h"
+#include "base/threading/sequenced_worker_pool.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/test/test_browser_thread.h"
 #include "content/public/test/test_utils.h"
