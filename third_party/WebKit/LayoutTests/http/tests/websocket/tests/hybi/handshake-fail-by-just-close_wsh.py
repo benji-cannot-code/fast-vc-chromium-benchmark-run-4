0912a211d100e5b64e009dea1149e0b1f03e8435
tyoshino@chromium.org <tyoshino@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-22 08:06:28 +0000

    [WebSocket] Print error on JS console when connection is closed before receiving handshake response
    BUG=none
    
    Review URL: https://codereview.chromium.org/51763004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162523 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/websocket/tests/hybi/handshake-fail-by-just-close_wsh.py
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+from mod_pywebsocket import handshake
+
+
+def web_socket_do_extra_handshake(request):
+    # Prevents pywebsocket from sending its own handshake message.
+    raise handshake.AbortedByUserException('Abort the connection')
+
+
+def web_socket_transfer_data(request):
+    pass
