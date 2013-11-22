248dff0aeb1a6669663f4f0ca540fcc7775d7452
skuhne@chromium.org <skuhne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-22 21:18:15 +0000

    Fixing M32 crasher where closing of a windowed V1 app on a visiting desktop will cause crashes later on
    BUG=321374
    TEST=unit test (and checked that the crash does not happen anymore by hand)
    
    Review URL: https://codereview.chromium.org/83173007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236828 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/ash/launcher/multi_profile_browser_status_monitor.h
+++ chrome/browser/ui/ash/launcher/multi_profile_browser_status_monitor.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/ash/launcher/browser_status_monitor.h"
 
-// MultiProfileBrowserStatusMonitor uses mainly the BrowserStatusMonitormonitor
+// MultiProfileBrowserStatusMonitor uses mainly the BrowserStatusMonitor
 // with the addition that it creates and destroys launcher items for windowed
 // V1 apps - upon creation as well as upon user switch.
 class MultiProfileBrowserStatusMonitor : public BrowserStatusMonitor {
@@ -25,6 +25,12 @@ class MultiProfileBrowserStatusMonitor : public BrowserStatusMonitor {
  private:
   typedef std::vector<Browser*> AppList;
   AppList app_list_;
+
+  // Connect a V1 app to the launcher.
+  void ConnectV1AppToLauncher(Browser* browser);
+
+  // Disconnect a V1 app from the launcher.
+  void DisconnectV1AppFromLauncher(Browser* browser);
 
   // The launcher controller which is associated with this object.
   ChromeLauncherController* launcher_controller_;
