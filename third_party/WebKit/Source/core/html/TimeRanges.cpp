4a0e740f7520265f41e4191608f4ed570b070eb9
mkwst@chromium.org <mkwst@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-22 09:28:06 +0000

    Improve TimeRanges exception messages.
    BUG=270033
    
    Review URL: https://codereview.chromium.org/80633003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162529 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/TimeRanges.cpp
+++ third_party/WebKit/Source/core/html/TimeRanges.cpp
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "core/html/TimeRanges.h"
 
+#include "bindings/v8/ExceptionMessages.h"
 #include "bindings/v8/ExceptionState.h"
 #include "bindings/v8/ExceptionStatePlaceholder.h"
 #include "core/dom/ExceptionCode.h"
@@ -117,7 +118,7 @@ void TimeRanges::unionWith(const TimeRanges* other)
 double TimeRanges::start(unsigned index, ExceptionState& exceptionState) const
 {
     if (index >= length()) {
-        exceptionState.throwUninformativeAndGenericDOMException(IndexSizeError);
+        exceptionState.throwDOMException(IndexSizeError, ExceptionMessages::failedToExecute("start", "TimeRanges", "The index provided (" + String::number(index) + ") is not less than the object's length (" + String::number(length()) + ")."));
         return 0;
     }
     return m_ranges[index].m_start;
@@ -126,7 +127,7 @@ double TimeRanges::start(unsigned index, ExceptionState& exceptionState) const
 double TimeRanges::end(unsigned index, ExceptionState& exceptionState) const
 {
     if (index >= length()) {
-        exceptionState.throwUninformativeAndGenericDOMException(IndexSizeError);
+        exceptionState.throwDOMException(IndexSizeError, ExceptionMessages::failedToExecute("end", "TimeRanges", "The index provided (" + String::number(index) + ") is not less than the object's length (" + String::number(length()) + ")."));
         return 0;
     }
     return m_ranges[index].m_end;
