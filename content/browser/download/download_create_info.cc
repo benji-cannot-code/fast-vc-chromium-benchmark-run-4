40ec31e3bbcc2ae7dda2968978e0f9701fa1345d
asanka@chromium.org <asanka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-22 21:11:28 +0000

    [Downloads] Cleanup DownloadResourceHandler
    * Avoid duplicate assignments.
    * Don't calculate whether strong validators are present in an HTTP
      response. A spec compliant implementation of this determination is
      already present elsewhere.
    
    BUG=7648
    
    Review URL: https://codereview.chromium.org/66993008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236825 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/download/download_create_info.cc
+++ content/browser/download/download_create_info.cc
@@ -12,20 +12,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-DownloadCreateInfo::DownloadCreateInfo(
-    const base::Time& start_time,
-    int64 total_bytes,
-    const net::BoundNetLog& bound_net_log,
-    bool has_user_gesture,
-    PageTransition transition_type)
+DownloadCreateInfo::DownloadCreateInfo(const base::Time& start_time,
+                                       int64 total_bytes,
+                                       const net::BoundNetLog& bound_net_log,
+                                       bool has_user_gesture,
+                                       PageTransition transition_type,
+                                       scoped_ptr<DownloadSaveInfo> save_info)
     : start_time(start_time),
       total_bytes(total_bytes),
       download_id(DownloadItem::kInvalidId),
       has_user_gesture(has_user_gesture),
       transition_type(transition_type),
-      save_info(new DownloadSaveInfo()),
-      request_bound_net_log(bound_net_log) {
-}
+      save_info(save_info.Pass()),
+      request_bound_net_log(bound_net_log) {}
 
 DownloadCreateInfo::DownloadCreateInfo()
     : total_bytes(0),
