f71c42c65af6ebe479dfbf25e18640ac5334a42f
abodenha@chromium.org <abodenha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-22 19:37:21 +0000

    Add an accessibility alert for incorrect use of some accelerators.
    Some accelerators don't make sense if no windows exist on the desktop.
    ChromeVox users can end up in this state and not know it.
    If a window-needing accelerator is issued and there is no window we
    fire off an accessibiliy event to let the user know there is no
    window.
    
    This CL:
    Extends Ash's AcceleratorController to intercept these accelerators.
    Adds the concept of triggering an alert to AccessibilityDelegate.
    Adds handling in Chrome's AccessibilityDelegate to generate an alert.
    
    BUG=217571
    TEST=All automated tests pass. Activate ChromeVox and hit ctrl-m with
    no window open on Chrome OS. You should get an alert telling you to
    open a window.
    
    Review URL: https://codereview.chromium.org/77453013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236812 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/accelerators/accelerator_controller.cc
+++ ash/accelerators/accelerator_controller.cc
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/web_notification/web_notification_tray.h"
 #include "ash/touch/touch_hud_debug.h"
 #include "ash/volume_control_delegate.h"
+#include "ash/wm/mru_window_tracker.h"
 #include "ash/wm/overview/window_selector_controller.h"
 #include "ash/wm/partial_screenshot_view.h"
 #include "ash/wm/power_button_controller.h"
@@ -437,6 +438,8 @@ void AcceleratorController::Init() {
     nonrepeatable_actions_.insert(kNonrepeatableActions[i]);
   for (size_t i = 0; i < kActionsAllowedInAppModeLength; ++i)
     actions_allowed_in_app_mode_.insert(kActionsAllowedInAppMode[i]);
+  for (size_t i = 0; i < kActionsNeedingWindowLength; ++i)
+    actions_needing_window_.insert(kActionsNeedingWindow[i]);
 
   RegisterAccelerators(kAcceleratorData, kAcceleratorDataLength);
 
@@ -524,6 +527,12 @@ bool AcceleratorController::PerformAction(int action,
       actions_allowed_in_app_mode_.find(action) ==
       actions_allowed_in_app_mode_.end()) {
     return false;
+  }
+  if (MruWindowTracker::BuildWindowList(false).empty() &&
+      actions_needing_window_.find(action) != actions_needing_window_.end()) {
+    Shell::GetInstance()->accessibility_delegate()->TriggerAccessibilityAlert(
+        A11Y_ALERT_WINDOW_NEEDED);
+    return true;
   }
 
   const ui::KeyboardCode key_code = accelerator.key_code();
--- chrome/browser/ui/ash/chrome_shell_delegate_views.cc
+++ chrome/browser/ui/ash/chrome_shell_delegate_views.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/tray/default_system_tray_delegate.h"
 #include "ash/wm/window_util.h"
 #include "base/command_line.h"
+#include "chrome/browser/accessibility/accessibility_events.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/prefs/session_startup_pref.h"
 #include "chrome/browser/profiles/profile_manager.h"
@@ -121,6 +122,14 @@ class EmptyAccessibilityDelegate : public ash::AccessibilityDelegate {
   }
 
   virtual void SilenceSpokenFeedback() const OVERRIDE {
+  }
+
+  virtual void TriggerAccessibilityAlert(
+      ash::AccessibilityAlert alert) OVERRIDE {
+  }
+
+  virtual ash::AccessibilityAlert GetLastAccessibilityAlert() OVERRIDE {
+    return ash::A11Y_ALERT_NONE;
   }
 
  private:
