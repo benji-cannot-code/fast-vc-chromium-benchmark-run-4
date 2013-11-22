2750c3c90a379bf000172d2cc923f547856f46dd
igsolla@chromium.org <igsolla@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-22 21:05:46 +0000

    Disable a SyncTest correctly.
    This reverts https://codereview.chromium.org/34603003 and disables the test correctly.
    
    I have followed the example of https://codereview.chromium.org/42103005.
    
    You can see that the buildbot was trying to run the test today:
    http://build.chromium.org/p/chromium.linux/builders/Android%20Tests/builds/9870/steps/chromiumtestshell_instrumentation_tests/logs/stdio
    
    BUG=322562,309997
    
    Review URL: https://codereview.chromium.org/83173006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236824 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/android/javatests/src/org/chromium/chrome/browser/sync/SyncTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/sync/SyncTest.java
@@ -92,7 +92,7 @@ public class SyncTest extends ChromiumTestShellTestBase {
         assertTrue("Couldn't get about info.", gotInfo);
     }
 
-    @DisabledTest
+    @HostDrivenTest
     public void testAboutSyncPageDisplaysCurrentSyncStatus() throws InterruptedException {
         setupTestAccountAndSignInToSync(FOREIGN_SESSION_TEST_MACHINE_ID);
 
