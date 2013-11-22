d76b940cfd4869aa2f980e6947b38cb404511047
keishi@chromium.org <keishi@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-22 11:13:24 +0000

    Update WebTestProxy::createColorChooser to prevent hidden virtual function warning
    Updating WebTestProxy::createColorChooser to the method with the extra argument
    to avoid the "hides overloaded virtual function" warning.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/80933002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162536 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/testing/WebTestProxy.h
+++ third_party/WebKit/public/testing/WebTestProxy.h
@@ -85,6 +85,7 @@ class WebUserMediaClient;
 class WebValidationMessageClient;
 class WebView;
 class WebWidget;
+struct WebColorSuggestion;
 struct WebConsoleMessage;
 struct WebContextMenuData;
 struct WebFileChooserParams;
@@ -120,6 +121,7 @@ public:
     blink::WebSpellCheckClient *spellCheckClient() const;
     blink::WebValidationMessageClient* validationMessageClient();
     blink::WebColorChooser* createColorChooser(blink::WebColorChooserClient*, const blink::WebColor&);
+    blink::WebColorChooser* createColorChooser(blink::WebColorChooserClient*, const blink::WebColor&, const blink::WebVector<blink::WebColorSuggestion>& suggestions);
     bool runFileChooser(const blink::WebFileChooserParams&, blink::WebFileChooserCompletion*);
 
     std::string captureTree(bool debugRenderTree);
@@ -539,6 +541,10 @@ public:
     virtual blink::WebColorChooser* createColorChooser(blink::WebColorChooserClient* client, const blink::WebColor& color)
     {
         return WebTestProxyBase::createColorChooser(client, color);
+    }
+    virtual blink::WebColorChooser* createColorChooser(blink::WebColorChooserClient* client, const blink::WebColor& color, const blink::WebVector<blink::WebColorSuggestion>& suggestions)
+    {
+        return WebTestProxyBase::createColorChooser(client, color, suggestions);
     }
     virtual bool runFileChooser(const blink::WebFileChooserParams& params, blink::WebFileChooserCompletion* completion)
     {
