05253c61907a2e14626aa1a9200ffef1a0a77fef
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-23 20:58:14 +0000

    Add DesktopShapeTracker with implementation for Windows
    The new class will be used in Me2App mode to capture window shape.
    
    BUG=134209
    
    Review URL: https://codereview.chromium.org/83183007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236972 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ remoting/host/desktop_shape_tracker_mac.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "remoting/host/desktop_shape_tracker.h"
+
+#include "base/memory/scoped_ptr.h"
+#include "third_party/webrtc/modules/desktop_capture/desktop_capture_options.h"
+
+namespace remoting {
+
+scoped_ptr<DesktopShapeTracker> DesktopShapeTracker::Create(
+    webrtc::DesktopCaptureOptions options) {
+  return scoped_ptr<DesktopShapeTracker>();
+}
+
+}  // namespace remoting
