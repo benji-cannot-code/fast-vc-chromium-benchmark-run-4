6517ac7c282eae4ea5e3e664330f3b49904c6a29
sheu@chromium.org <sheu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-23 02:22:44 +0000

    Reorganize media::VideoCapture* types
    The purpose of this CL is to clean up the distinction between
    VideoCaptureFormat (which identifies the captured type of a frame),
    VideoCaptureParams (which identifies the requested format of a capture),
    and VideoCaptureCapability (which identifies the capture capabilities of a
    device).
    
    Notably:
    * VideoCaptureFormat::frame_size_type ->
      VideoCaptureParams::allow_resolution_change, as variable resolution
      capability is a per-session, not a per-frame property.
    * VideoCaptureCapability::color -> VideoCaptureFormat::pixel_format, as frame
      color format is a per-frame property.
      * As VideoCaptureParams holds a VideoCaptureFormat member, capture requests
        are able now to request a particular capture color format.
    
    BUG=269312
    TEST=local build, run unittests, chrome on CrOS snow, desktop Linux
    
    Review URL: https://codereview.chromium.org/68503005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236927 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/media/video_capture_messages.h
+++ content/common/media/video_capture_messages.h
@@ -15,12 +15,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IPC_MESSAGE_START VideoCaptureMsgStart
 
 IPC_ENUM_TRAITS(content::VideoCaptureState)
-IPC_ENUM_TRAITS_MAX_VALUE(media::VideoCaptureResolutionType,
-                          media::MaxVideoCaptureResolutionType - 1)
 
 IPC_STRUCT_TRAITS_BEGIN(media::VideoCaptureParams)
-  IPC_STRUCT_TRAITS_MEMBER(session_id)
   IPC_STRUCT_TRAITS_MEMBER(requested_format)
+  IPC_STRUCT_TRAITS_MEMBER(allow_resolution_change)
+IPC_STRUCT_TRAITS_END()
+
+IPC_STRUCT_TRAITS_BEGIN(media::VideoCaptureCapability)
+  IPC_STRUCT_TRAITS_MEMBER(supported_format)
 IPC_STRUCT_TRAITS_END()
 
 // TODO(nick): device_id in these messages is basically just a route_id. We
@@ -54,8 +56,9 @@ IPC_MESSAGE_CONTROL4(VideoCaptureMsg_BufferReady,
 
 // Start a video capture as |device_id|, a new id picked by the renderer
 // process. The session to be started is determined by |params.session_id|.
-IPC_MESSAGE_CONTROL2(VideoCaptureHostMsg_Start,
+IPC_MESSAGE_CONTROL3(VideoCaptureHostMsg_Start,
                      int /* device_id */,
+                     media::VideoCaptureSessionId, /* session_id */
                      media::VideoCaptureParams /* params */)
 
 // Pause the video capture specified by |device_id|.
--- media/video/capture/mac/video_capture_device_qtkit_mac.mm
+++ media/video/capture/mac/video_capture_device_qtkit_mac.mm
@@ -282,11 +282,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       addressToPass = adjustedAddress;
       frameSize = frameHeight * expectedBytesPerRow;
     }
-    media::VideoCaptureCapability captureCapability;
-    captureCapability.width = frameWidth;
-    captureCapability.height = frameHeight;
-    captureCapability.frame_rate = frameRate_;
-    captureCapability.color = media::PIXEL_FORMAT_UYVY;
+
+    media::VideoCaptureFormat captureFormat(gfx::Size(frameWidth, frameHeight),
+                                            frameRate_,
+                                            media::PIXEL_FORMAT_UYVY);
 
     // The aspect ratio dictionary is often missing, in which case we report
     // a pixel aspect ratio of 0:0.
@@ -306,7 +305,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     }
 
     // Deliver the captured video frame.
-    frameReceiver_->ReceiveFrame(addressToPass, frameSize, captureCapability,
+    frameReceiver_->ReceiveFrame(addressToPass, frameSize, captureFormat,
         aspectNumerator, aspectDenominator);
 
     CVPixelBufferUnlockBaseAddress(videoFrame, kLockFlags);
