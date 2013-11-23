d20da71d00d52e7964cc7452d942a5593802b183
horo@chromium.org <horo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-23 12:19:18 +0000

    Check the result of DocumentThreadableLoader::create in WorkerThreadableLoader::MainThreadBridge::mainThreadCreateLoader.
    DocumentThreadableLoader::create may return 0 when the document loader has been already changed.
    In the original code memory leak may happen when FileReader is runing in the worker thread and the user clicks the reload button because unsetPendingActivity will not be called.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/78393002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162571 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/files/workers/resources/worker-read-blob-async-crash.js
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+onmessage = function(event)
+{
+    var count = event.data.count;
+    var chunkSize = 1;
+    var chunk = new Uint8Array(chunkSize);
+    for (var j = 0; j < chunkSize; j++)
+        chunk[j] = i + j;
+    var blob = new Blob([chunk]);
+    for (var i = 0; i < count; i++) {
+        var reader = new FileReader();
+        reader.readAsArrayBuffer(blob);
+    }
+}
