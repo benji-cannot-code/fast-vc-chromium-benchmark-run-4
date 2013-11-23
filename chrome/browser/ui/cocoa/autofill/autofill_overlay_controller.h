1666af6fd017d591275ba066528aba97525534cb
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-23 01:48:09 +0000

    [rAc OSX] Simplify the overlay controller.
    * Remove code handling the case where the overlay doesn't include any text.
    * Remove remnants of code handling opacity transitions.
    
    BUG=none
    TEST=none
    R=groby@chromium.org
    
    Review URL: https://codereview.chromium.org/82933002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236916 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/autofill/autofill_overlay_controller.h
+++ chrome/browser/ui/cocoa/autofill/autofill_overlay_controller.h
@@ -21,10 +21,6 @@ struct DialogOverlayState;
 
 @interface AutofillOverlayController : NSViewController<AutofillLayout> {
  @private
-  // |childView_| contains all overlay UI elements. This is used to fade out
-  // UI elements first, before making the main view transparent to fade out the
-  // overlay shield.
-  base::scoped_nsobject<NSView> childView_;
   base::scoped_nsobject<NSImageView> imageView_;
   base::scoped_nsobject<AutofillMessageView> messageView_;
 
@@ -43,4 +39,3 @@ struct DialogOverlayState;
 @end
 
 #endif  // CHROME_BROWSER_UI_COCOA_AUTOFILL_AUTOFILL_OVERLAY_CONTROLLER_H_
-
