a39c36e714ee6674bb8754fa291c0010d1807bea
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-23 20:16:34 +0000

    [Build Fix] Add missing #include
    TBR=thestig@chromium.org
    
    Review URL: https://codereview.chromium.org/84533002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236956 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/extensions/extension_error_handler.cc
+++ chrome/browser/ui/webui/extensions/extension_error_handler.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/strings/string16.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/threading/sequenced_worker_pool.h"
 #include "base/values.h"
 #include "chrome/browser/devtools/devtools_window.h"
 #include "chrome/browser/extensions/extension_service.h"
--- chrome/browser/ui/webui/nacl_ui.cc
+++ chrome/browser/ui/webui/nacl_ui.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string16.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/threading/sequenced_worker_pool.h"
 #include "base/values.h"
 #include "chrome/browser/plugins/plugin_prefs.h"
 #include "chrome/browser/profiles/profile.h"
