b94ca9d439103ed9f837f19c1c32ac33154f5573
dtu@chromium.org <dtu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-24 06:24:15 +0000

    [telemetry] Fix type of of media metric.
    Missed one call in http://crrev.com/236951 :)
    This should make the perf bots green.
    
    BUG=None.
    TEST=None.
    TBR=shadi
    
    Review URL: https://codereview.chromium.org/84593002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236992 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/metrics/media.js
+++ tools/perf/metrics/media.js
@@ -97,7 +97,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         var actualDuration = loopTimer.stop();
         var idealDuration = metric.element.duration * loopCount;
         var avg_loop_time = (actualDuration - idealDuration) / loopCount;
-        metric.metrics['avg_loop_time'] = avg_loop_time.toFixed(3);
+        metric.metrics['avg_loop_time'] =
+            Math.round(avg_loop_time * 1000) / 1000;
         e.target.removeEventListener('endLoop', onEndLoop);
       };
     this.element.addEventListener('endLoop', onEndLoop);
