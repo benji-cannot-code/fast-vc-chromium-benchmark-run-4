f6b52809af7769528741db5577fa712acefc27da
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-24 16:14:13 +0000

    Disable image_decoding_benchmark on android (and linux).
    We don't have a general mechanism to differentiate them yet.
    
    BUG=323015
    TBR=nduca@chromium.org
    NOTRY=True
    
    Review URL: https://codereview.chromium.org/83293009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236995 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/benchmarks/image_decoding.py
+++ tools/perf/benchmarks/image_decoding.py
@@ -2,6 +2,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright (c) 2013 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+
+import sys
+
 from telemetry import test
 
 from measurements import image_decoding
@@ -11,3 +14,6 @@ class ImageDecodingToughImageCases(test.Test):
   test = image_decoding.ImageDecoding
   # TODO: Rename this page set to tough_image_cases.json
   page_set = 'page_sets/image_decoding_measurement.json'
+
+  # crbug.com/323015
+  enabled = not sys.platform.startswith('linux')
