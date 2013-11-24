887d648e5acdc4993bc71f3f01c5b19288971f7b
glenn@chromium.org <glenn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-24 23:08:19 +0000

    Move BufferedLineReader into html/track/vtt.
    BUG=322784
    
    Review URL: https://codereview.chromium.org/83343005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162586 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/track/vtt/BufferedLineReaderTest.cpp
+++ third_party/WebKit/Source/core/html/track/vtt/BufferedLineReaderTest.cpp
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
-#include "core/html/track/BufferedLineReader.h"
+#include "core/html/track/vtt/BufferedLineReader.h"
 
 #include "wtf/text/CString.h"
 #include "wtf/text/WTFString.h"
--- third_party/WebKit/Source/core/html/track/vtt/VTTParser.h
+++ third_party/WebKit/Source/core/html/track/vtt/VTTParser.h
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "RuntimeEnabledFeatures.h"
 #include "core/dom/DocumentFragment.h"
 #include "core/fetch/TextResourceDecoder.h"
-#include "core/html/track/BufferedLineReader.h"
+#include "core/html/track/vtt/BufferedLineReader.h"
 #include "core/html/track/vtt/VTTCue.h"
 #include "core/html/track/vtt/VTTRegion.h"
 #include "core/html/track/vtt/VTTTokenizer.h"
