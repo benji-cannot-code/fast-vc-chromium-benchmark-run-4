ad0bc7008c77e4c1239dcbff994126d6deca8106
grunell@chromium.org <grunell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-25 19:32:38 +0000

    Deleting pending URLFetchers in WebRtcLogUploader at shutdown.
    Review URL: https://codereview.chromium.org/68163005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237123 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media/webrtc_log_uploader.h
+++ chrome/browser/media/webrtc_log_uploader.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/memory/ref_counted.h"
 #include "base/platform_file.h"
+#include "base/threading/thread_checker.h"
 #include "chrome/browser/media/webrtc_logging_handler_host.h"
 #include "net/url_request/url_fetcher_delegate.h"
 
@@ -95,6 +96,12 @@ class WebRtcLogUploader : public net::URLFetcherDelegate {
                   uint32 log_buffer_length);
   void CompressLog(std::string* post_data, uint8* input, uint32 input_size);
   void ResizeForNextOutput(std::string* post_data, z_stream* stream);
+
+  void CreateAndStartURLFetcher(
+      scoped_refptr<net::URLRequestContextGetter> request_context,
+      const WebRtcLogUploadDoneData& upload_done_data,
+      scoped_ptr<std::string> post_data);
+
   void DecreaseLogCount();
 
   // Append information (time and report ID) about this uploaded log to a log
@@ -113,6 +120,12 @@ class WebRtcLogUploader : public net::URLFetcherDelegate {
                         const std::string& report_id,
                         const WebRtcLogUploadDoneData& upload_done_data);
 
+  // This is the UI thread for Chromium. Some other thread for tests.
+  base::ThreadChecker create_thread_checker_;
+
+  // This is the FILE thread for Chromium. Some other thread for tests.
+  base::ThreadChecker file_thread_checker_;
+
   int log_count_;
 
   // For testing purposes, see OverrideUploadWithBufferForTesting. Only accessed
@@ -121,6 +134,7 @@ class WebRtcLogUploader : public net::URLFetcherDelegate {
 
   typedef std::map<const net::URLFetcher*, WebRtcLogUploadDoneData>
       UploadDoneDataMap;
+  // Only accessed on the UI thread.
   UploadDoneDataMap upload_done_data_;
 
   DISALLOW_COPY_AND_ASSIGN(WebRtcLogUploader);
