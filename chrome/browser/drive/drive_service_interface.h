6a6309f1ae5eece83da81643ed8e5caf56685511
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-25 05:47:29 +0000

    [Drive] Update obsolete comment on DriveServiceInterface
    ContinueGetResourceList was split into GetRemaining{File,Change}List.
    
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/84743006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237031 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/drive/drive_service_interface.h
+++ chrome/browser/drive/drive_service_interface.h
@@ -89,7 +89,7 @@ class DriveServiceInterface {
   // completion.
   // If the list is too long, it may be paged. In such a case, a URL to fetch
   // remaining results will be included in the returned result. See also
-  // ContinueGetResourceList.
+  // GetRemainingFileList.
   //
   // |callback| must not be null.
   virtual google_apis::CancelCallback GetAllResourceList(
@@ -99,7 +99,7 @@ class DriveServiceInterface {
   // |callback| will be called upon completion.
   // If the list is too long, it may be paged. In such a case, a URL to fetch
   // remaining results will be included in the returned result. See also
-  // ContinueGetResourceList.
+  // GetRemainingFileList.
   //
   // |directory_resource_id| must not be empty.
   // |callback| must not be null.
@@ -111,7 +111,7 @@ class DriveServiceInterface {
   // resources. |callback| will be called upon completion.
   // If the list is too long, it may be paged. In such a case, a URL to fetch
   // remaining results will be included in the returned result. See also
-  // ContinueGetResourceList.
+  // GetRemainingFileList.
   //
   // |search_query| must not be empty.
   // |callback| must not be null.
@@ -125,7 +125,7 @@ class DriveServiceInterface {
   // the resources directly under the directory with |directory_resource_id|.
   // If the list is too long, it may be paged. In such a case, a URL to fetch
   // remaining results will be included in the returned result. See also
-  // ContinueGetResourceList.
+  // GetRemainingFileList.
   //
   // |title| must not be empty, and |callback| must not be null.
   virtual google_apis::CancelCallback SearchByTitle(
@@ -137,7 +137,7 @@ class DriveServiceInterface {
   // called upon completion.
   // If the list is too long, it may be paged. In such a case, a URL to fetch
   // remaining results will be included in the returned result. See also
-  // ContinueGetResourceList.
+  // GetRemainingChangeList.
   //
   // |callback| must not be null.
   virtual google_apis::CancelCallback GetChangeList(
