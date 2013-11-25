7dde282b37f78108417ed83ace69c93c2ef5c9ae
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-25 08:28:38 +0000

    [SyncFS] Implement LocalToRemoteSyncer::UploadNewFile
    BUG=240165
    
    Review URL: https://codereview.chromium.org/82713003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237045 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_backend/drive_backend_util.cc
+++ chrome/browser/sync_file_system/drive_backend/drive_backend_util.cc
@@ -15,10 +15,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/google_apis/gdata_wapi_parser.h"
 #include "chrome/browser/sync_file_system/drive_backend/drive_backend_constants.h"
 #include "chrome/browser/sync_file_system/drive_backend/metadata_database.pb.h"
+#include "net/base/mime_util.h"
 #include "third_party/leveldatabase/src/include/leveldb/write_batch.h"
 
 namespace sync_file_system {
 namespace drive_backend {
+
+namespace {
+
+const char kMimeTypeOctetStream[] = "application/octet-stream";
+
+}  // namespace
 
 void PutServiceMetadataToBatch(const ServiceMetadata& service_metadata,
                                leveldb::WriteBatch* batch) {
@@ -137,6 +144,15 @@ bool HasFileAsParent(const FileDetails& details, const std::string& file_id) {
       return true;
   }
   return false;
+}
+
+std::string GetMimeTypeFromTitle(const base::FilePath& title) {
+  base::FilePath::StringType extension = title.Extension();
+  std::string mime_type;
+  if (extension.empty() ||
+      !net::GetWellKnownMimeTypeFromExtension(extension.substr(1), &mime_type))
+    return kMimeTypeOctetStream;
+  return mime_type;
 }
 
 }  // namespace drive_backend
--- chrome/browser/sync_file_system/drive_backend_v1/api_util.cc
+++ chrome/browser/sync_file_system/drive_backend_v1/api_util.cc
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "extensions/common/constants.h"
 #include "extensions/common/extension.h"
-#include "net/base/mime_util.h"
 
 namespace sync_file_system {
 namespace drive_backend {
