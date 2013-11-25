fe755764b245646b6017933adf61f3b81274f998
yoichio@chromium.org <yoichio@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-25 03:41:12 +0000

    Remove a redundant type chromeos::ibus::Rect.
    BUG=N/A
    
    Review URL: https://codereview.chromium.org/80433002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237019 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/ime/chromeos/ibus_bridge.h
+++ ui/base/ime/chromeos/ibus_bridge.h
@@ -14,6 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ime/text_input_type.h"
 #include "ui/base/ui_export.h"
 
+namespace gfx {
+class Rect;
+}  // namespace gfx
+
 namespace chromeos {
 namespace input_method {
 class CandidateWindow;
@@ -128,8 +132,8 @@ class UI_EXPORT IBusPanelCandidateWindowHandlerInterface {
   virtual void HidePreeditText() = 0;
 
   // Called when the application changes its caret bounds.
-  virtual void SetCursorBounds(const ibus::Rect& cursor_bounds,
-                               const ibus::Rect& composition_head) = 0;
+  virtual void SetCursorBounds(const gfx::Rect& cursor_bounds,
+                               const gfx::Rect& composition_head) = 0;
 
   // Called when the text field's focus state is changed.
   // |is_focused| is true when the text field gains the focus.
