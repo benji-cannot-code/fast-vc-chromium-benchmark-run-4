ceacd1fef4fdf3db751d709e74f1e5d0cf6f53b9
horo@chromium.org <horo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-25 11:32:20 +0000

    Check the result of DocumentThreadableLoader::create in mainThreadCreateLoader.
    This CL is almost same as https://codereview.chromium.org/78393002 which is reverted in https://codereview.chromium.org/84883004/.
    In this CL I reduced the file read request count in the test case to avoid another crash bug (http://crbug.com/295428).
    
    BUG=
    
    Review URL: https://codereview.chromium.org/84693006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162616 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/files/workers/resources/worker-read-blob-async-crash.js
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+onmessage = function(event)
+{
+    var count = 100;
+    var chunk = new Uint8Array(1);
+    chunk[0] = 0;
+    var blob = new Blob([chunk]);
+    for (var i = 0; i < count; i++) {
+        var reader = new FileReader();
+        reader.readAsArrayBuffer(blob);
+    }
+    postMessage({});
+    for (var i = 0; i < count; i++) {
+        var reader = new FileReader();
+        reader.readAsArrayBuffer(blob);
+    }
+}
