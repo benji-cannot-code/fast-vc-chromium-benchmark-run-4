2beb4fcf62045538b4ca3537223dceb2bf98f161
hirono@chromium.org <hirono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-25 01:28:47 +0000

    Files.app: Add a semicolon to util.js.
    This CL fixes the code style.
    
    BUG=none
    TEST=none
    TBR=mtomasz@chromium.org
    
    Review URL: https://codereview.chromium.org/84923003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237015 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/common/js/util.js
+++ chrome/browser/resources/file_manager/common/js/util.js
@@ -457,7 +457,7 @@ util.deduplicatePath = function(dirEntry, relativePath, onSuccess, onError) {
 
     // Found a path that doesn't exist.
     onSuccess(trialPath);
-  }
+  };
 
   var numRetry = MAX_RETRY;
   var onResolved = function(entry) {
