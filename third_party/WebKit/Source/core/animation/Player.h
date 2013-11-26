1dd43c20a9cda1817d358e36972d03055c6bbace
dstockwell@chromium.org <dstockwell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-26 08:26:58 +0000

    Web Animations CSS: Avoid waiting for cancelled compositor animation
    When a renderer associated with a composited animation is destroyed, other
    animations waiting for a synchronized start may never be notified. We can
    detect this case and start such animations immediately. After this change
    CSSPendingAnimations now requests service on every frame until all pending
    animations have a chance to start.
    
    BUG=322272
    
    Review URL: https://codereview.chromium.org/83793017
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162678 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/Player.h
+++ third_party/WebKit/Source/core/animation/Player.h
@@ -78,13 +78,13 @@ public:
 
     bool maybeStartAnimationOnCompositor();
     void cancelAnimationOnCompositor();
+    bool hasActiveAnimationsOnCompositor();
 
 private:
     Player(DocumentTimeline&, TimedItem*);
     inline double pausedTimeDrift() const;
     inline double currentTimeBeforeDrift() const;
 
-    bool hasActiveAnimationsOnCompositor();
 
     void setPausedImpl(bool);
 
