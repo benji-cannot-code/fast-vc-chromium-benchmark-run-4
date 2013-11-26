1ffb3266f1e2fbde8cbd171454a1b45615b5f199
yosin@chromium.org <yosin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-26 07:17:09 +0000

    Use const Node* in HashMap of DocumentMarkerController
    This patch changes key type of HashMap used in DOcumentMarkerController to |const Node*| from |RefPtr<Node>|, which was introduced by r14748(http://src.chromium.org/viewvc/blink?view=revision&revision=14748) to avoid redundant ref counting. 
    
    This patch is a preparation of using |const Node&| parameter in DocumentMarkerController.
    
    BUG=n/a
    TEST=n/a; no behavior changes
    
    Review URL: https://codereview.chromium.org/82633002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162675 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -583,7 +583,7 @@ void Document::dispose()
     // removeDetachedChildren() can access FormController.
     m_formController.clear();
 
-    m_markers->detach();
+    m_markers->clear();
 
     m_cssCanvasElements.clear();
 
@@ -2054,6 +2054,7 @@ void Document::detach(const AttachContext& context)
 
 void Document::prepareForDestruction()
 {
+    m_markers->prepareForDestruction();
     disconnectDescendantFrames();
 
     // The process of disconnecting descendant frames could have already detached us.
@@ -3504,8 +3505,10 @@ void Document::nodeChildrenWillBeRemoved(ContainerNode* container)
             (*it)->nodeWillBeRemoved(*n);
     }
 
-    if (Frame* frame = this->frame()) {
-        for (Node* n = container->firstChild(); n; n = n->nextSibling()) {
+    Frame* frame = this->frame();
+    for (Node* n = container->firstChild(); n; n = n->nextSibling()) {
+        m_markers->nodeWillBeRemoved(*n);
+        if (frame) {
             frame->eventHandler().nodeWillBeRemoved(*n);
             frame->selection().nodeWillBeRemoved(*n);
             frame->page()->dragCaretController().nodeWillBeRemoved(*n);
@@ -3530,6 +3533,8 @@ void Document::nodeWillBeRemoved(Node& n)
         frame->selection().nodeWillBeRemoved(n);
         frame->page()->dragCaretController().nodeWillBeRemoved(n);
     }
+
+    m_markers->nodeWillBeRemoved(n);
 }
 
 void Document::didInsertText(Node* text, unsigned offset, unsigned length)
