43adf78b22ebc643a5fd9cf1a717626f6f15e800
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-26 05:50:42 +0000

    Enable HTML frame and Transparency by channel, not experimental flag.
    BUG=322834
    
    Review URL: https://codereview.chromium.org/84253004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237267 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/app_window/app_window_api.cc
+++ chrome/browser/extensions/api/app_window/app_window_api.cc
@@ -251,9 +251,7 @@ bool AppWindowCreateFunction::RunImpl() {
 
     if (options->frame.get()) {
       if (*options->frame == kHtmlFrameOption &&
-          (GetExtension()->HasAPIPermission(APIPermission::kExperimental) ||
-           CommandLine::ForCurrentProcess()->HasSwitch(
-               switches::kEnableExperimentalExtensionApis))) {
+          GetCurrentChannel() <= chrome::VersionInfo::CHANNEL_DEV) {
         create_params.frame = ShellWindow::FRAME_NONE;
         inject_html_titlebar = true;
       } else if (*options->frame == kNoneFrameOption) {
@@ -264,9 +262,7 @@ bool AppWindowCreateFunction::RunImpl() {
     }
 
     if (options->transparent_background.get() &&
-        (GetExtension()->HasAPIPermission(APIPermission::kExperimental) ||
-         CommandLine::ForCurrentProcess()->HasSwitch(
-             switches::kEnableExperimentalExtensionApis))) {
+        GetCurrentChannel() <= chrome::VersionInfo::CHANNEL_DEV) {
       create_params.transparent_background = *options->transparent_background;
     }
 
