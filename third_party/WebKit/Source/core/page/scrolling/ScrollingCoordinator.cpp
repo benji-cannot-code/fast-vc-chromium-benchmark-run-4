efe1c9702cb74c1fe0aec41ed2cc53a8e9500adc
vollick@chromium.org <vollick@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-26 03:01:37 +0000

    Ignore should-scroll-on-main-thread if main frame is not scrollable
    This change had been reverted previously because of LayoutTest flakiness. I had
    been concerned that this was because the code was actually incorrect, but it
    looks like the real problem is that we're not correctly resetting between
    layout tests inside Internals::resetToConsistentState. I've updated that fn
    and the tests are no longer flaky.
    
    This reverts commit 757a502a1b21193db4b84db6cff0703c112291ef.
    Review URL: https://codereview.chromium.org/76983003
    
    BUG=321358
    R=ojan@chromium.org
    
    Review URL: https://codereview.chromium.org/85763003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162663 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/scrolling/ScrollingCoordinator.cpp
+++ third_party/WebKit/Source/core/page/scrolling/ScrollingCoordinator.cpp
@@ -91,6 +91,8 @@ ScrollingCoordinator::ScrollingCoordinator(Page* page)
     : m_page(page)
     , m_scrollGestureRegionIsDirty(false)
     , m_touchEventTargetRectsAreDirty(false)
+    , m_wasFrameScrollable(false)
+    , m_lastMainThreadScrollingReasons(0)
 {
 }
 
@@ -151,6 +153,12 @@ void ScrollingCoordinator::updateAfterCompositingChange()
         updateTouchEventTargetRectsIfNeeded();
         m_touchEventTargetRectsAreDirty = false;
     }
+
+    FrameView* frameView = m_page->mainFrame()->view();
+    bool frameIsScrollable = frameView && frameView->isScrollable();
+    if (m_wasFrameScrollable != frameIsScrollable)
+        updateShouldUpdateScrollLayerPositionOnMainThread();
+    m_wasFrameScrollable = frameIsScrollable;
 
     const FrameTree& tree = m_page->mainFrame()->tree();
     for (const Frame* child = tree.firstChild(); child; child = child->tree().nextSibling()) {
@@ -494,6 +502,18 @@ void ScrollingCoordinator::updateTouchEventTargetRectsIfNeeded()
     setTouchEventTargetRects(touchEventTargetRects);
 }
 
+void ScrollingCoordinator::reset()
+{
+    m_horizontalScrollbars.clear();
+    m_verticalScrollbars.clear();
+    m_layersWithTouchRects.clear();
+    m_wasFrameScrollable = false;
+
+    // This is retained for testing.
+    m_lastMainThreadScrollingReasons = 0;
+    setShouldUpdateScrollLayerPositionOnMainThread(m_lastMainThreadScrollingReasons);
+}
+
 // Note that in principle this could be called more often than computeTouchEventTargetRects, for
 // example during a non-composited scroll (although that's not yet implemented - crbug.com/261307).
 void ScrollingCoordinator::setTouchEventTargetRects(const LayerHitTestRects& layerRects)
@@ -594,8 +614,10 @@ void ScrollingCoordinator::recomputeWheelEventHandlerCountForFrameView(FrameView
 
 void ScrollingCoordinator::setShouldUpdateScrollLayerPositionOnMainThread(MainThreadScrollingReasons reasons)
 {
-    if (WebLayer* scrollLayer = scrollingWebLayerForScrollableArea(m_page->mainFrame()->view()))
+    if (WebLayer* scrollLayer = scrollingWebLayerForScrollableArea(m_page->mainFrame()->view())) {
+        m_lastMainThreadScrollingReasons = reasons;
         scrollLayer->setShouldScrollOnMainThread(reasons);
+    }
 }
 
 void ScrollingCoordinator::pageDestroyed()
@@ -860,8 +882,11 @@ bool ScrollingCoordinator::hasVisibleSlowRepaintViewportConstrainedObjects(Frame
 
 MainThreadScrollingReasons ScrollingCoordinator::mainThreadScrollingReasons() const
 {
+    // The main thread scrolling reasons are applicable to scrolls of the main
+    // frame. If it does not exist or if it is not scrollable, there is no
+    // reason to force main thread scrolling.
     FrameView* frameView = m_page->mainFrame()->view();
-    if (!frameView)
+    if (!frameView || !frameView->isScrollable())
         return static_cast<MainThreadScrollingReasons>(0);
 
     MainThreadScrollingReasons mainThreadScrollingReasons = (MainThreadScrollingReasons)0;
@@ -897,7 +922,14 @@ String ScrollingCoordinator::mainThreadScrollingReasonsAsText(MainThreadScrollin
 
 String ScrollingCoordinator::mainThreadScrollingReasonsAsText() const
 {
-    return mainThreadScrollingReasonsAsText(mainThreadScrollingReasons());
+    return mainThreadScrollingReasonsAsText(m_lastMainThreadScrollingReasons);
+}
+
+bool ScrollingCoordinator::frameViewIsScrollableIsDirty() const
+{
+    FrameView* frameView = m_page->mainFrame()->view();
+    bool frameIsScrollable = frameView && frameView->isScrollable();
+    return frameIsScrollable != m_wasFrameScrollable;
 }
 
 } // namespace WebCore
