77b862f014f9d7c7c1c6a62dfc3c845951cb5682
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-26 10:49:52 +0000

    Print Preview: Set the print preview's tab title to the same as the initiator's.
    BUG=323162
    TEST=manual, see bug.
    
    Review URL: https://codereview.chromium.org/86963002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237305 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/print_preview/print_preview.js
+++ chrome/browser/resources/print_preview/print_preview.js
@@ -501,6 +501,7 @@ cr.define('print_preview', function() {
       this.destinationStore_.init(settings.systemDefaultDestinationId);
       this.appState_.setInitialized();
 
+      $('document-title').innerText = settings.documentTitle;
       setIsVisible($('system-dialog-link'),
                    !settings.hidePrintWithSystemDialogLink);
     },
