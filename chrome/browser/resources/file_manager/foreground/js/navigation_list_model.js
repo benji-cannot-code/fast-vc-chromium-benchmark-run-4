6446d48b257f1a821c2f734f3d9a82147d5ed3c6
hirono@chromium.org <hirono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-26 10:40:23 +0000

    Reland r236746: Enable volume manager to find volume by using any paths.
    The original patch is broken due to 2 reasons:
    
    * It overrides the current directory at initialization because of unexpected
      fallback.
    * Lack of null check for the return value of getVolumeInfo.
    
    In addition, this patch removes a validation for mount paths.
    This validation is useless, since it doesn't prevent any abusing, nor errors.
    
    Previously reverted CL: crrev.com/64753005
    
    BUG=302090, 322618
    TEST=manually, browser_tests
    
    Review URL: https://codereview.chromium.org/84653003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237302 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/foreground/js/navigation_list_model.js
+++ chrome/browser/resources/file_manager/foreground/js/navigation_list_model.js
@@ -151,10 +151,7 @@ function NavigationListModel(volumeManager, shortcutListModel) {
   this.shortcutList_ = [];
   for (var i = 0; i < this.shortcutListModel_.length; i++) {
     var shortcutPath = this.shortcutListModel_.item(i);
-    var mountPath = PathUtil.isDriveBasedPath(shortcutPath) ?
-        RootDirectory.DRIVE :
-        PathUtil.getRootPath(shortcutPath);
-    var volumeInfo = this.volumeManager_.getVolumeInfo(mountPath);
+    var volumeInfo = this.volumeManager_.getVolumeInfo(shortcutPath);
     var isMounted = volumeInfo && !volumeInfo.error;
     if (isMounted)
       this.shortcutList_.push(pathToModelItem(shortcutPath));
@@ -217,10 +214,7 @@ function NavigationListModel(volumeManager, shortcutListModel) {
       }
 
       // Check if the volume where the shortcutPath is is mounted or not.
-      var mountPath = PathUtil.isDriveBasedPath(shortcutPath) ?
-          RootDirectory.DRIVE :
-          PathUtil.getRootPath(shortcutPath);
-      var volumeInfo = this.volumeManager_.getVolumeInfo(mountPath);
+      var volumeInfo = this.volumeManager_.getVolumeInfo(shortcutPath);
       var isMounted = volumeInfo && !volumeInfo.error;
       if (cmp == 0) {
         // There exists an old NavigationModelItem instance.
@@ -243,10 +237,7 @@ function NavigationListModel(volumeManager, shortcutListModel) {
     // Add remaining (new) shortcuts if necessary.
     for (; modelIndex < this.shortcutListModel_.length; modelIndex++) {
       var shortcutPath = this.shortcutListModel_.item(modelIndex);
-      var mountPath = PathUtil.isDriveBasedPath(shortcutPath) ?
-          RootDirectory.DRIVE :
-          PathUtil.getRootPath(shortcutPath);
-      var volumeInfo = this.volumeManager_.getVolumeInfo(mountPath);
+      var volumeInfo = this.volumeManager_.getVolumeInfo(shortcutPath);
       var isMounted = volumeInfo && !volumeInfo.error;
       if (isMounted)
         newList.push(pathToModelItem(shortcutPath));
