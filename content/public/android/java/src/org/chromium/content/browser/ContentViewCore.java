ff08c50dd66cd796eba6e23656cf36e497e314ee
ppi@chromium.org <ppi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-26 14:14:23 +0000

    Android: move to CVC onShow() / onHide() for child process binding management.
    Existing process management logic adjusts child process bindings in response to
    ContentViewCore onAttachedToWindow() / onDetachedFromWindow() calls. This
    causes some inconveniences: views in the embedding application sometimes are
    detached for short-lived animations; but more importantly, we now have
    embedders that do not alter the view hierarchy at all, preventing any
    management of their renderer bindings
    
    This patch moves to onShow() / onHide() calls instead. These are called by the
    embedding applications independently from the view hierarchy changes.
    
    In order to keep the most recently used renderer strongly bound during the
    background period (as it was the case under onAttachedToWindow() /
    onDetachedFromWindow() calls), explicit logic is introduced in the
    BindingManager.
    
    Also, the delay for removing strong bindings is brought from 5s to 1s as we no
    longer face the interference of views detaching for short-lived animations (as
    it was the case when we were relying on onAttachedToWindow() /
    onDetachedFromWindow()).
    
    BUG=317963
    
    Review URL: https://codereview.chromium.org/74943006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237333 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -367,7 +367,7 @@ public class ContentViewCore
     // Native pointer to C++ ContentViewCoreImpl object which will be set by nativeInit().
     private long mNativeContentViewCore = 0;
 
-    private boolean mAttachedToWindow = false;
+    private boolean mInForeground = false;
 
     private ContentViewGestureHandler mContentViewGestureHandler;
     private GestureStateListener mGestureStateListener;
@@ -1517,6 +1517,17 @@ public class ContentViewCore
      * To be called when the ContentView is shown.
      */
     public void onShow() {
+        assert mNativeContentViewCore != 0;
+        if (!mInForeground) {
+            int pid = nativeGetCurrentRenderProcessId(mNativeContentViewCore);
+            ChildProcessLauncher.getBindingManager().bindAsHighPriority(pid);
+            // Normally the initial binding is removed in onRenderProcessSwap(), but it is possible
+            // to construct WebContents and spawn the renderer before passing it to ContentViewCore.
+            // In this case there will be no onRenderProcessSwap() call and the initial binding will
+            // be removed here.
+            ChildProcessLauncher.getBindingManager().removeInitialBinding(pid);
+        }
+        mInForeground = true;
         nativeOnShow(mNativeContentViewCore);
         setAccessibilityState(mAccessibilityManager.isEnabled());
     }
@@ -1525,6 +1536,12 @@ public class ContentViewCore
      * To be called when the ContentView is hidden.
      */
     public void onHide() {
+        assert mNativeContentViewCore != 0;
+        if (mInForeground) {
+            int pid = nativeGetCurrentRenderProcessId(mNativeContentViewCore);
+            ChildProcessLauncher.getBindingManager().unbindAsHighPriority(pid);
+        }
+        mInForeground = false;
         hidePopupDialog();
         setInjectedAccessibility(false);
         nativeOnHide(mNativeContentViewCore);
@@ -1584,16 +1601,6 @@ public class ContentViewCore
      */
     @SuppressWarnings("javadoc")
     public void onAttachedToWindow() {
-        mAttachedToWindow = true;
-        if (mNativeContentViewCore != 0) {
-            int pid = nativeGetCurrentRenderProcessId(mNativeContentViewCore);
-            ChildProcessLauncher.getBindingManager().bindAsHighPriority(pid);
-            // Normally the initial binding is removed in onRenderProcessSwap(), but it is possible
-            // to construct WebContents and spawn the renderer before passing it to ContentViewCore.
-            // In this case there will be no onRenderProcessSwap() call and the initial binding will
-            // be removed here.
-            ChildProcessLauncher.getBindingManager().removeInitialBinding(pid);
-        }
         setAccessibilityState(mAccessibilityManager.isEnabled());
     }
 
@@ -1602,11 +1609,6 @@ public class ContentViewCore
      */
     @SuppressWarnings("javadoc")
     public void onDetachedFromWindow() {
-        mAttachedToWindow = false;
-        if (mNativeContentViewCore != 0) {
-            int pid = nativeGetCurrentRenderProcessId(mNativeContentViewCore);
-            ChildProcessLauncher.getBindingManager().unbindAsHighPriority(pid);
-        }
         setInjectedAccessibility(false);
         hidePopupDialog();
         mZoomControlsDelegate.dismissZoomPicker();
@@ -2646,7 +2648,7 @@ public class ContentViewCore
     @SuppressWarnings("unused")
     @CalledByNative
     private void onRenderProcessSwap(int oldPid, int newPid) {
-        if (mAttachedToWindow && oldPid != newPid) {
+        if (mInForeground && oldPid != newPid) {
             ChildProcessLauncher.getBindingManager().unbindAsHighPriority(oldPid);
             ChildProcessLauncher.getBindingManager().bindAsHighPriority(newPid);
         }
