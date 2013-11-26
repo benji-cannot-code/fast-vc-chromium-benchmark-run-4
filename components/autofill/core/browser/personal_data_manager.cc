387802f7f4f282831abb3ea6683d77eeb786abfb
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-26 00:28:09 +0000

    autofill: Use timezone to guess user location.
    BUG=303368
    
    Review URL: https://codereview.chromium.org/70973002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237196 0039d316-1c4b-4281-b951-d872f2087c98

--- components/autofill/core/browser/personal_data_manager.cc
+++ components/autofill/core/browser/personal_data_manager.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <functional>
 #include <iterator>
 
+#include "base/i18n/timezone.h"
 #include "base/logging.h"
 #include "base/memory/ref_counted.h"
 #include "base/prefs/pref_service.h"
@@ -758,8 +759,11 @@ const std::string& PersonalDataManager::GetDefaultCountryCodeForNewAddress()
   if (default_country_code_.empty())
     default_country_code_ = MostCommonCountryCodeFromProfiles();
 
-  // If the profiles don't help, guess based on locale.
-  // TODO(estade): prefer to use the timezone instead.
+  // Failing that, guess based on system timezone.
+  if (default_country_code_.empty())
+    default_country_code_ = base::CountryCodeForCurrentTimezone();
+
+  // Failing that, guess based on locale.
   if (default_country_code_.empty())
     default_country_code_ = AutofillCountry::CountryCodeForLocale(app_locale());
 
