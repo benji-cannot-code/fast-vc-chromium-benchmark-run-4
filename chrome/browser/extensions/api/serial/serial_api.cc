c590afffe161ba1e06180478a7d52b9cd0044480
rockot@chromium.org <rockot@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-26 03:51:24 +0000

    Clarify serial control signals documentation
    Review URL: https://codereview.chromium.org/84123004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237249 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/serial/serial_api.cc
+++ chrome/browser/extensions/api/serial/serial_api.cc
@@ -382,7 +382,7 @@ void SerialGetControlSignalsFunction::Work() {
     return;
   }
 
-  serial::ControlSignals signals;
+  serial::DeviceControlSignals signals;
   if (!connection->GetControlSignals(&signals)) {
     error_ = kErrorGetControlSignalsFailed;
     return;
