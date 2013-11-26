91f348d1b31515b43d707e65107da1c3104325f6
eranm@google.com <eranm@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-26 18:09:45 +0000

    Certificate Transparency: Address review comments from issue 67513008 Comment and indentation fixes, no functional changes.
    BUG=309578
    
    Review URL: https://codereview.chromium.org/87923002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237355 0039d316-1c4b-4281-b951-d872f2087c98

--- net/cert/ct_serialization.cc
+++ net/cert/ct_serialization.cc
@@ -317,7 +317,8 @@ bool DecodeSCTList(base::StringPiece* input,
   return true;
 }
 
-bool DecodeSignedCertificateTimestamp(base::StringPiece* input,
+bool DecodeSignedCertificateTimestamp(
+    base::StringPiece* input,
     scoped_refptr<SignedCertificateTimestamp>* output) {
   scoped_refptr<SignedCertificateTimestamp> result(
       new SignedCertificateTimestamp());
