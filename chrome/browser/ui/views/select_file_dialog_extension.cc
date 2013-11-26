36f34ad427b8a24fe2b6d91196fbeebaeff3075b
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-26 00:39:28 +0000

    Revert 234870 "[Files.app] Set the minimum size of file dialog a..."
    > [Files.app] Set the minimum size of file dialog as 320px x 240px
    > 
    > BUG=310818
    > TEST=manually tested
    > 
    > Review URL: https://codereview.chromium.org/70673003
    
    TBR=yoshiki@chromium.org
    
    Review URL: https://codereview.chromium.org/85643004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237198 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/select_file_dialog_extension.cc
+++ chrome/browser/ui/views/select_file_dialog_extension.cc
@@ -44,8 +44,6 @@ namespace {
 
 const int kFileManagerWidth = 972;  // pixels
 const int kFileManagerHeight = 640;  // pixels
-const int kFileManagerMinWidth = 320;  // pixels
-const int kFileManagerMinHeight = 240;  // pixels
 
 // Holds references to file manager dialogs that have callbacks pending
 // to their listeners.
@@ -373,8 +371,8 @@ void SelectFileDialogExtension::SelectFileImpl(
   ExtensionDialog* dialog = ExtensionDialog::Show(file_manager_url,
       base_window, profile_, web_contents,
       kFileManagerWidth, kFileManagerHeight,
-      kFileManagerMinWidth,
-      kFileManagerMinHeight,
+      kFileManagerWidth,
+      kFileManagerHeight,
 #if defined(USE_AURA)
       file_manager::util::GetSelectFileDialogTitle(type),
 #else
