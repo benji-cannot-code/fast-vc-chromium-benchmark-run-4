89a176cde16ca6d37b2fbe8df24edb2ca7e50b43
xians@chromium.org <xians@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-26 14:27:36 +0000

    Revert 237311 "Added an "enable-audio-processor" flag and WebRtc..."
    > Added an "enable-audio-processor" flag and WebRtcAudioProcessor class.
    > 
    > 
    > This CL is a break-down CL from https://codereview.chromium.org/37793005. As the first step to move WebRtc APM to Chrome, it adds a enable-audio-processor command line flag, the WebRtcAudioProcessor class and its unittest.
    > WebRtcAudioProcessor is not hooked up the any of the code in Chrome yet, but it will be exercised and tested by its unittest.
    > 
    > 
    > BUG=264611
    > TEST=content_unittest
    > 
    > Review URL: https://codereview.chromium.org/54383003
    
    TBR=xians@chromium.org
    
    Review URL: https://codereview.chromium.org/88283003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237334 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/chrome_restart_request.cc
+++ chrome/browser/chromeos/login/chrome_restart_request.cc
@@ -49,7 +49,7 @@ namespace chromeos {
 
 namespace {
 
-// Increase logging level for Guest mode to avoid INFO messages in logs.
+// Increase logging level for Guest mode to avoid LOG(INFO) messages in logs.
 const char kGuestModeLoggingLevel[] = "1";
 
 // Format of command line switch.
@@ -148,7 +148,6 @@ std::string DeriveCommandLine(const GURL& start_url,
 #if defined(ENABLE_WEBRTC)
       ::switches::kDisableWebRtcHWDecoding,
       ::switches::kDisableWebRtcHWEncoding,
-      ::switches::kEnableAudioTrackProcessing,
       ::switches::kEnableWebRtcHWVp8Encoding,
 #endif
       ash::switches::kAshDefaultWallpaperLarge,
