736dc4e422995e13c14d5f331e454671e13749e2
seokju.kwon@gmail.com <seokju.kwon@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-26 18:48:12 +0000

    Remove leftover code after r162228
    This CL removes unused include as 'Debug code' was removed in r162228.
    
    Review URL: https://codereview.chromium.org/87943002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162702 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/history/HistoryItem.cpp
+++ third_party/WebKit/Source/core/history/HistoryItem.cpp
@@ -33,10 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/CurrentTime.h"
 #include "wtf/text/CString.h"
 
-#ifndef NDEBUG
-#include <stdio.h>
-#endif
-
 namespace WebCore {
 
 static long long generateSequenceNumber()
