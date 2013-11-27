650112c23b36a970398f304f7ef3da4cd08ced52
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-27 00:44:47 +0000

    Exclude mojo/examples from production related presubmit checks
    R=joi@chromium.org
    
    Review URL: https://codereview.chromium.org/87223002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237451 0039d316-1c4b-4281-b951-d872f2087c98

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -46,6 +46,8 @@ _TEST_CODE_EXCLUDED_PATHS = (
     r'content[/\\]shell[/\\].*',
     # At request of folks maintaining this folder.
     r'chrome[/\\]browser[/\\]automation[/\\].*',
+    # Non-production example code.
+    r'mojo[/\\]examples[/\\].*',
 )
 
 _TEST_ONLY_WARNING = (
