674da8d458cb4b4b997a27dc9ced09844d58504c
ch.dumez@samsung.com <ch.dumez@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-27 01:48:35 +0000

    Pass AtomicStrings as 2 first arguments to V8LazyEventListener::create()
    Pass AtomicStrings as 2 first arguments to V8LazyEventListener::create() in
    ScriptEventListener as this is what this factory method expects. This avoids
    uselessly constructing Strings and then converting them to AtomicStrings.
    
    R=abarth
    BUG=323739
    
    Review URL: https://codereview.chromium.org/89993002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162718 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/ScriptEventListener.cpp
+++ third_party/WebKit/Source/bindings/v8/ScriptEventListener.cpp
@@ -45,10 +45,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-static const String& eventParameterName(bool isSVGEvent)
+static const AtomicString& eventParameterName(bool isSVGEvent)
 {
-    DEFINE_STATIC_LOCAL(const String, eventString, ("event"));
-    DEFINE_STATIC_LOCAL(const String, evtString, ("evt"));
+    DEFINE_STATIC_LOCAL(const AtomicString, eventString, ("event"));
+    DEFINE_STATIC_LOCAL(const AtomicString, evtString, ("evt"));
     return isSVGEvent ? evtString : eventString;
 }
 
@@ -74,7 +74,7 @@ PassRefPtr<V8LazyEventListener> createAttributeEventListener(Node* node, const Q
         isolate = v8::Isolate::GetCurrent();
     }
 
-    return V8LazyEventListener::create(name.localName().string(), eventParameterName(node->isSVGElement()), value, sourceURL, position, node, isolate);
+    return V8LazyEventListener::create(name.localName(), eventParameterName(node->isSVGElement()), value, sourceURL, position, node, isolate);
 }
 
 PassRefPtr<V8LazyEventListener> createAttributeEventListener(Frame* frame, const QualifiedName& name, const AtomicString& value)
@@ -92,7 +92,7 @@ PassRefPtr<V8LazyEventListener> createAttributeEventListener(Frame* frame, const
     TextPosition position = scriptController.eventHandlerPosition();
     String sourceURL = frame->document()->url().string();
 
-    return V8LazyEventListener::create(name.localName().string(), eventParameterName(frame->document()->isSVGDocument()), value, sourceURL, position, 0, toIsolate(frame));
+    return V8LazyEventListener::create(name.localName(), eventParameterName(frame->document()->isSVGDocument()), value, sourceURL, position, 0, toIsolate(frame));
 }
 
 String eventListenerHandlerBody(Document* document, EventListener* listener)
