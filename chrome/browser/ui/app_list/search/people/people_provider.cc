c9e72cd3139c02e8f0e8c006e63a6db3c020d04c
rkc@chromium.org <rkc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-27 08:03:03 +0000

    Record user actions for People search.
    R=xiyuan@chromium.org
    BUG=317976
    
    Review URL: https://codereview.chromium.org/89643002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237534 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/app_list/search/people/people_provider.cc
+++ chrome/browser/ui/app_list/search/people/people_provider.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/callback.h"
-#include "base/metrics/field_trial.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
