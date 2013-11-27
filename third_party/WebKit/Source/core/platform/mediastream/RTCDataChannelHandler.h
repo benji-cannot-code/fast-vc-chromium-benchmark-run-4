13fac4befa765b19cd7b260a0b5aa53fe0e5ec01
l.gombos@samsung.com <l.gombos@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-27 16:19:47 +0000

    No need for a header to include itself.
    Review URL: https://codereview.chromium.org/90743002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162769 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/mediastream/RTCDataChannelHandler.h
+++ third_party/WebKit/Source/core/platform/mediastream/RTCDataChannelHandler.h
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef RTCDataChannelHandler_h
 #define RTCDataChannelHandler_h
 
-#include "core/platform/mediastream/RTCDataChannelHandler.h"
 #include "core/platform/mediastream/RTCDataChannelHandlerClient.h"
 #include "public/platform/WebRTCDataChannelHandler.h"
 #include "public/platform/WebRTCDataChannelHandlerClient.h"
--- third_party/WebKit/Source/core/platform/mediastream/RTCPeerConnectionHandler.h
+++ third_party/WebKit/Source/core/platform/mediastream/RTCPeerConnectionHandler.h
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define RTCPeerConnectionHandler_h
 
 #include "core/platform/mediastream/MediaStreamDescriptor.h"
-#include "core/platform/mediastream/RTCPeerConnectionHandler.h"
 #include "public/platform/WebRTCPeerConnectionHandler.h"
 #include "public/platform/WebRTCPeerConnectionHandlerClient.h"
 #include "wtf/OwnPtr.h"
