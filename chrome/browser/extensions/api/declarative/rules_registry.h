a66dfad8ad9bca65f416ade23fd58a2c705f6bfa
vabr@chromium.org <vabr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-27 10:56:02 +0000

    Make RulesRegistry::process_changed_rules_requested_ a map
    process_changed_rules_requested_ stores the state of progress on writing a set of rules to the rules store.
    For each extension there are separate write tasks. Currently, process_changed_rules_requested_ is share among
    all such writes, which is wrong.
    
    This CL makes process_changed_rules_requested_ a map from extension IDs to the state flags.
    
    BUG=320645
    
    Review URL: https://codereview.chromium.org/79433002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237555 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/declarative/rules_registry.h
+++ chrome/browser/extensions/api/declarative/rules_registry.h
@@ -196,6 +196,7 @@ class RulesRegistry : public base::RefCountedThreadSafe<RulesRegistry> {
     // to schedule one.
     NOT_SCHEDULED_FOR_PROCESSING
   };
+  typedef std::map<ExtensionId, ProcessChangedRulesState> ProcessStateMap;
 
   base::WeakPtr<RulesRegistry> GetWeakPtr() {
     DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
@@ -205,7 +206,8 @@ class RulesRegistry : public base::RefCountedThreadSafe<RulesRegistry> {
   // Common processing after extension's rules have changed.
   void ProcessChangedRules(const std::string& extension_id);
 
-  // Calls ProcessChangedRules if |process_changed_rules_requested_| ==
+  // Calls ProcessChangedRules if
+  // |process_changed_rules_requested_(extension_id)| ==
   // NOT_SCHEDULED_FOR_PROCESSING.
   void MaybeProcessChangedRules(const std::string& extension_id);
 
@@ -247,7 +249,7 @@ class RulesRegistry : public base::RefCountedThreadSafe<RulesRegistry> {
   // instance.
   base::WeakPtr<RulesCacheDelegate> cache_delegate_;
 
-  ProcessChangedRulesState process_changed_rules_requested_;
+  ProcessStateMap process_changed_rules_requested_;
 
   // Returns whether any existing rule is registered with identifier |rule_id|
   // for extension |extension_id|.
