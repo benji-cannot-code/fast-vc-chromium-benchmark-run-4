31060d08320423dd5073a6996af5f51d05bbe6bd
davidben@chromium.org <davidben@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-27 23:45:18 +0000

    Remove WEBKIT_HAS_TARGET_PING #define.
    Chromium has removed its dependency on the #define.
    
    BUG=302816
    TEST=no behavior change.
    
    Review URL: https://codereview.chromium.org/85003004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162780 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebURLRequest.h
+++ third_party/WebKit/public/platform/WebURLRequest.h
@@ -39,9 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore { class ResourceRequest; }
 #endif
 
-// TODO(davidben): Remove #define once Chromium side has updated.
-#define WEBKIT_HAS_TARGET_IS_PING
-
 namespace blink {
 
 class WebCString;
