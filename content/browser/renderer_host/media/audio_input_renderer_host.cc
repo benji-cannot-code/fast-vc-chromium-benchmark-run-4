7fda2fd7e92499b81d85f2fecb36de1e12a3af1a
dalecurtis@chromium.org <dalecurtis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-27 04:20:41 +0000

    Plumb audio logging for AudioInputRendererHost.  Fix naming.
    Adds AudioLog support to AudioInputRendererHost.  Very similar to
    how AudioLog works for output, except input hosts have no concept
    of "stop", only "created", "started", and "closed."
    
    While testing I noticed that output_channels doesn't make sense for
    input, so I've renamed the display names to remove the "output_"
    prefix.  The new names are inline with the member functions inside
    of AudioParamaters.
    
    BUG=260005
    TEST=Use WebRTC, observe input controller is shown on internals.
    
    Review URL: https://codereview.chromium.org/89653002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237509 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/media/audio_input_renderer_host.cc
+++ content/browser/renderer_host/media/audio_input_renderer_host.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/shared_memory.h"
 #include "base/metrics/histogram.h"
 #include "base/process/process.h"
+#include "content/browser/media/media_internals.h"
 #include "content/browser/renderer_host/media/audio_input_device_manager.h"
 #include "content/browser/renderer_host/media/audio_input_sync_writer.h"
 #include "content/browser/renderer_host/media/media_stream_manager.h"
@@ -57,7 +58,9 @@ AudioInputRendererHost::AudioInputRendererHost(
     : audio_manager_(audio_manager),
       media_stream_manager_(media_stream_manager),
       audio_mirroring_manager_(audio_mirroring_manager),
-      user_input_monitor_(user_input_monitor) {}
+      user_input_monitor_(user_input_monitor),
+      audio_log_(MediaInternals::GetInstance()->CreateAudioLog(
+          media::AudioLogFactory::AUDIO_INPUT_CONTROLLER)) {}
 
 AudioInputRendererHost::~AudioInputRendererHost() {
   DCHECK(audio_entries_.empty());
@@ -179,6 +182,7 @@ void AudioInputRendererHost::DoHandleError(
   if (!entry)
     return;
 
+  audio_log_->OnError(entry->stream_id);
   DeleteEntryOnError(entry);
 }
 
@@ -305,6 +309,8 @@ void AudioInputRendererHost::OnCreateStream(
   // to the map.
   entry->stream_id = stream_id;
   audio_entries_.insert(std::make_pair(stream_id, entry.release()));
+
+  audio_log_->OnCreated(stream_id, audio_params, device_id, std::string());
 }
 
 void AudioInputRendererHost::OnRecordStream(int stream_id) {
@@ -317,6 +323,7 @@ void AudioInputRendererHost::OnRecordStream(int stream_id) {
   }
 
   entry->controller->Record();
+  audio_log_->OnStarted(stream_id);
 }
 
 void AudioInputRendererHost::OnCloseStream(int stream_id) {
@@ -338,6 +345,7 @@ void AudioInputRendererHost::OnSetVolume(int stream_id, double volume) {
   }
 
   entry->controller->SetVolume(volume);
+  audio_log_->OnSetVolume(stream_id, volume);
 }
 
 void AudioInputRendererHost::SendErrorMessage(int stream_id) {
@@ -361,6 +369,7 @@ void AudioInputRendererHost::CloseAndDeleteStream(AudioEntry* entry) {
     entry->controller->Close(base::Bind(&AudioInputRendererHost::DeleteEntry,
                                         this, entry));
     entry->pending_close = true;
+    audio_log_->OnClosed(entry->stream_id);
   }
 }
 
--- content/browser/renderer_host/media/audio_input_renderer_host.h
+++ content/browser/renderer_host/media/audio_input_renderer_host.h
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "media/audio/audio_input_controller.h"
 #include "media/audio/audio_io.h"
+#include "media/audio/audio_logging.h"
 #include "media/audio/simple_sources.h"
 
 namespace media {
@@ -160,6 +161,8 @@ class CONTENT_EXPORT AudioInputRendererHost
 
   // Raw pointer of the UserInputMonitor.
   media::UserInputMonitor* user_input_monitor_;
+
+  scoped_ptr<media::AudioLog> audio_log_;
 
   DISALLOW_COPY_AND_ASSIGN(AudioInputRendererHost);
 };
