b5916fdce504e93e8314aadf771abac75dcda0b0
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-27 23:06:16 +0000

    [chromedriver] Avoid exception by using iswcntrl instead of std::iscntrl on windows. BUG=chromedriver:405 NOTRY=true
    Review URL: https://codereview.chromium.org/84333002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237639 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/chromedriver/keycode_text_conversion_win.cc
+++ chrome/test/chromedriver/keycode_text_conversion_win.cc
@@ -5,9 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/test/chromedriver/keycode_text_conversion.h"
 
+#include <stdlib.h>
 #include <windows.h>
-
-#include <cctype>
 
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/test/chromedriver/chrome/ui_events.h"
@@ -27,9 +26,9 @@ bool ConvertKeyCodeToText(
     keyboard_state[VK_MENU] |= 0x80;
   wchar_t chars[5];
   int code = ::ToUnicode(key_code, scan_code, keyboard_state, chars, 4, 0);
-  // |ToUnicode| converts some non-text key codes like F1 to various ASCII
+  // |ToUnicode| converts some non-text key codes like F1 to various
   // control chars. Filter those out.
-  if (code <= 0 || (code == 1 && std::iscntrl(chars[0])))
+  if (code <= 0 || (code == 1 && iswcntrl(chars[0])))
     *text = std::string();
   else
     WideToUTF8(chars, code, text);
