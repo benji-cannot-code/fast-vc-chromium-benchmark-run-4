c8e147a2d9d83a855df3559022e633d40aa6cd37
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-27 01:58:02 +0000

    Configure QUIC max packet size via a Field Trial or command line arg. When configured via field trial we well extract the packet size from the name of the field trial group.
    Review URL: https://codereview.chromium.org/84273002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237481 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_network_session.cc
+++ net/http/http_network_session.cc
@@ -83,6 +83,7 @@ HttpNetworkSession::Params::Params()
       enable_quic_https(false),
       quic_clock(NULL),
       quic_random(NULL),
+      quic_max_packet_length(kDefaultMaxPacketSize),
       enable_user_alternate_protocol_ports(false),
       quic_crypto_client_stream_factory(NULL) {
 }
@@ -112,7 +113,8 @@ HttpNetworkSession::HttpNetworkSession(const Params& params)
                            params.quic_random ? params.quic_random :
                                QuicRandom::GetInstance(),
                            params.quic_clock ? params. quic_clock :
-                               new QuicClock()),
+                               new QuicClock(),
+                           params.quic_max_packet_length),
       spdy_session_pool_(params.host_resolver,
                          params.ssl_config_service,
                          params.http_server_properties,
--- net/quic/quic_stream_factory.cc
+++ net/quic/quic_stream_factory.cc
@@ -247,7 +247,8 @@ QuicStreamFactory::QuicStreamFactory(
     base::WeakPtr<HttpServerProperties> http_server_properties,
     QuicCryptoClientStreamFactory* quic_crypto_client_stream_factory,
     QuicRandom* random_generator,
-    QuicClock* clock)
+    QuicClock* clock,
+    size_t max_packet_length)
     : require_confirmation_(true),
       host_resolver_(host_resolver),
       client_socket_factory_(client_socket_factory),
@@ -255,6 +256,7 @@ QuicStreamFactory::QuicStreamFactory(
       quic_crypto_client_stream_factory_(quic_crypto_client_stream_factory),
       random_generator_(random_generator),
       clock_(clock),
+      max_packet_length_(max_packet_length),
       weak_factory_(this) {
   config_.SetDefaults();
   config_.set_idle_connection_state_lifetime(
@@ -476,6 +478,7 @@ QuicClientSession* QuicStreamFactory::CreateSession(
                                                   writer.get(), false,
                                                   QuicSupportedVersions());
   writer->SetConnection(connection);
+  connection->options()->max_packet_length = max_packet_length_;
 
   QuicCryptoClientConfig* crypto_config =
       GetOrCreateCryptoConfig(host_port_proxy_pair);
