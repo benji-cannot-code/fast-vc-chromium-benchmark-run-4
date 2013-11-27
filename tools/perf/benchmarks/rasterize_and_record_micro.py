1d8e5e9b5364dc45fe6aafa8aaf546188755dc1b
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-27 04:21:52 +0000

    [Telemetry] Disable rasterize_and_record_micro.
    BUG=323797
    TBR=isherman@chromium.org,vmpstr@chromium.org
    NOTRY=True
    
    Review URL: https://codereview.chromium.org/90513002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237510 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/benchmarks/rasterize_and_record_micro.py
+++ tools/perf/benchmarks/rasterize_and_record_micro.py
@@ -2,6 +2,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright 2013 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+import sys
+
 from telemetry import test
 
 from measurements import rasterize_and_record_micro
@@ -11,6 +13,7 @@ class RasterizeAndRecordMicroTop25(test.Test):
   """Measures rasterize and record performance on the top 25 web pages.
 
   http://www.chromium.org/developers/design-documents/rendering-benchmarks"""
+  enabled = not sys.platform.startswith('linux')
   test = rasterize_and_record_micro.RasterizeAndRecordMicro
   page_set = 'page_sets/top_25.json'
 
