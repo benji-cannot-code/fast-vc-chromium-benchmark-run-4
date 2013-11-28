9a27f5de7ab26883f02dfc47cb0389a8a40d2139
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-28 08:20:27 +0000

    Remove dependency from ExtensionService to app mode utils.
    This is done by introducing a function on the generic extensions browser
    client to determine if the system is running in forced app mode.
    
    BUG=298537
    
    Review URL: https://codereview.chromium.org/93783002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237742 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/chrome_extensions_browser_client.cc
+++ chrome/browser/extensions/chrome_extensions_browser_client.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/version.h"
+#include "chrome/browser/app_mode/app_mode_utils.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/extensions/chrome_app_sorting.h"
 #include "chrome/browser/extensions/extension_prefs.h"
@@ -117,6 +118,10 @@ bool ChromeExtensionsBrowserClient::DidVersionUpdate(
 
 scoped_ptr<AppSorting> ChromeExtensionsBrowserClient::CreateAppSorting() {
   return scoped_ptr<AppSorting>(new ChromeAppSorting()).Pass();
+}
+
+bool ChromeExtensionsBrowserClient::IsRunningInForcedAppMode() {
+  return chrome::IsRunningInForcedAppMode();
 }
 
 // static
