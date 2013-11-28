bfdb7d8e8e602add553af8278581faa9a7badc91
jvoung@chromium.org <jvoung@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-28 05:59:44 +0000

    Use posix_memalign for OS_NACL instead of memalign, remove TODO for docs.
    We finally added it to the nacl newlib headers. Besides the
    headers there used to be a bug in the posix_memalign
    implementation which prevented it from being useful. That's also fixed.
    
    BUG=138579
    
    Review URL: https://codereview.chromium.org/89703005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237695 0039d316-1c4b-4281-b951-d872f2087c98

--- base/memory/aligned_memory.cc
+++ base/memory/aligned_memory.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 
-#if defined(OS_ANDROID) || defined(OS_NACL)
+#if defined(OS_ANDROID)
 #include <malloc.h>
 #endif
 
@@ -20,13 +20,12 @@ void* AlignedAlloc(size_t size, size_t alignment) {
   void* ptr = NULL;
 #if defined(COMPILER_MSVC)
   ptr = _aligned_malloc(size, alignment);
-// Both Android and NaCl technically support posix_memalign(), but do not expose
-// it in the current version of the library headers used by Chrome.  Luckily,
-// memalign() on both platforms returns pointers which can safely be used with
-// free(), so we can use it instead.  Issues filed with each project for docs:
+// Android technically supports posix_memalign(), but does not expose it in
+// the current version of the library headers used by Chrome.  Luckily,
+// memalign() on Android returns pointers which can safely be used with
+// free(), so we can use it instead.  Issue filed to document this:
 // http://code.google.com/p/android/issues/detail?id=35391
-// http://code.google.com/p/chromium/issues/detail?id=138579
-#elif defined(OS_ANDROID) || defined(OS_NACL)
+#elif defined(OS_ANDROID)
   ptr = memalign(alignment, size);
 #else
   if (posix_memalign(&ptr, alignment, size))
