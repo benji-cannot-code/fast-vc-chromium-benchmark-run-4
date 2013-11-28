a3f95377e4d31f055512047d4a43e19ebdc95bd2
engedy@chromium.org <engedy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-28 16:45:59 +0000

    Add a new JTL instruction that extracts effective SLD of a URL.
    BUG=298036
    
    Review URL: https://codereview.chromium.org/85693004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237803 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profile_resetter/jtl_instructions.h
+++ chrome/browser/profile_resetter/jtl_instructions.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     std::string(1, '\x13') + name + value + default_value
 #define OP_STORE_NODE_BOOL(name) std::string(1, '\x14') + name
 #define OP_STORE_NODE_HASH(name) std::string(1, '\x15') + name
+#define OP_STORE_NODE_EFFECTIVE_SLD_HASH(name) std::string(1, '\x16') + name
 #define OP_COMPARE_NODE_BOOL(value) std::string(1, '\x20') + value
 #define OP_COMPARE_NODE_HASH(value) std::string(1, '\x21') + value
 #define OP_COMPARE_NODE_HASH_NOT(value) std::string(1, '\x22') + value
