39e857844e5d41a07342867a47cead2be90ec728
ch.dumez@samsung.com <ch.dumez@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-28 01:31:02 +0000

    Have CSSFontFeatureValue store the tag as an AtomicString instead of a String
    Have CSSFontFeatureValue store the tag as an AtomicString instead of a String.
    CSSFontFeatureValue objects are constructed in:
    - CSSParser::parseFontFeatureTag() where the tag is a CSSParserString that can
    be easily be used to construct either a String or an AtomicString
    - CSSComputedStyleDeclaration::getPropertyCSSValue() where it is constructed
    from a FontFeature. FontFeature stores the tag as an AtomicString.
    
    CSSFontFeatureValue::tag() getter is used by:
    - FontBuilder::setFeatureSettingsValue() to construct a FontFeature object,
    which requires the tag as a AtomicString and thus required us to convert
    the tag from String to AtomicString.
    - CSSFontFeatureValue::customCSSText() which uses StringBuilder::append(), so
    it does not matter if the tag is a String or AtomicString.
    - CSSFontFeatureValue::equals() for comparing the tags of 2 CSSFontFeatureValue
    objects. In this case, keeping the tag as AtomicString would be more efficient.
    
    Therefore, by having CSSFontFeatureValue store the tag as an AtomicString, we
    gain consistency with FontFeature, which is important as we need 2-way conversion
    from CSSFontFeatureValue <-> FontFeature. Also, it makes
    CSSFontFeatureValue::equals() faster.
    
    R=eseidel
    BUG=323739
    
    Review URL: https://codereview.chromium.org/92493003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162789 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/CSSFontFeatureValue.cpp
+++ third_party/WebKit/Source/core/css/CSSFontFeatureValue.cpp
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-CSSFontFeatureValue::CSSFontFeatureValue(const String& tag, int value)
+CSSFontFeatureValue::CSSFontFeatureValue(const AtomicString& tag, int value)
     : CSSValue(FontFeatureClass)
     , m_tag(tag)
     , m_value(value)
--- third_party/WebKit/Source/core/css/CSSParser-in.cpp
+++ third_party/WebKit/Source/core/css/CSSParser-in.cpp
@@ -9243,7 +9243,7 @@ bool CSSParser::parseFontFeatureTag(CSSValueList* settings)
             return false;
     }
 
-    String tag = value->string;
+    AtomicString tag = value->string;
     int tagValue = 1;
     // Feature tag values could follow: <integer> | on | off
     value = m_valueList->next();
