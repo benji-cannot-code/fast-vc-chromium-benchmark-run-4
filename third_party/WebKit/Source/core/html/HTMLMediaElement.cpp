9dd611ed6934543fca10855bb2d4b83faa2a8838
acolwell@chromium.org <acolwell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-28 01:43:11 +0000

    Remove MediaPlayerClient::mediaPlayerEngineUpdated().
    This method is not used anywhere so it should be removed.
    
    Review URL: https://codereview.chromium.org/92103004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162791 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLMediaElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLMediaElement.cpp
@@ -3141,13 +3141,6 @@ void HTMLMediaElement::mediaPlayerSizeChanged()
         renderer()->updateFromElement();
 }
 
-void HTMLMediaElement::mediaPlayerEngineUpdated()
-{
-    LOG(Media, "HTMLMediaElement::mediaPlayerEngineUpdated");
-    if (renderer())
-        renderer()->updateFromElement();
-}
-
 PassRefPtr<TimeRanges> HTMLMediaElement::buffered() const
 {
     if (!m_player)
