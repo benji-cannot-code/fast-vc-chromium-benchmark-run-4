51b2facf180103be3943b08cf052baef89931b1a
dalecurtis@chromium.org <dalecurtis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-28 05:51:57 +0000

    Don't start WASAPI render thread until setup completes.
    Creating the render thread before setting up the |audio_client_|
    may lead to race conditions when repeatedly starting and stopping
    a stream.
    
    With this patch, nothing touches |audio_client_| until IAudioClient
    triggers the render event for data.
    
    As a bonus, it fixes a destruction issue where |audio_clock| was
    being destructed on the wrong thread (render vs audio), see docs:
    
    http://msdn.microsoft.com/en-us/library/windows/desktop/dd370873.aspx
    
    BUG=310838
    TEST=none
    
    Review URL: https://codereview.chromium.org/89663004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237694 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/win/audio_low_latency_output_win.h
+++ media/audio/win/audio_low_latency_output_win.h
@@ -158,7 +158,7 @@ class MEDIA_EXPORT WASAPIAudioOutputStream :
   // Checks available amount of space in the endpoint buffer and reads
   // data from the client to fill up the buffer without causing audio
   // glitches.
-  bool RenderAudioFromSource(IAudioClock* audio_clock, UINT64 device_frequency);
+  bool RenderAudioFromSource(UINT64 device_frequency);
 
   // Called when the device will be opened in exclusive mode and use the
   // application specified format.
@@ -207,9 +207,6 @@ class MEDIA_EXPORT WASAPIAudioOutputStream :
   // Size in bytes of each audio packet.
   size_t packet_size_bytes_;
 
-  // Size in milliseconds of each audio packet.
-  float packet_size_ms_;
-
   // Length of the audio endpoint buffer.
   uint32 endpoint_buffer_size_frames_;
 
@@ -230,9 +227,6 @@ class MEDIA_EXPORT WASAPIAudioOutputStream :
   // Pointer to the client that will deliver audio samples to be played out.
   AudioSourceCallback* source_;
 
-  // An IMMDeviceEnumerator interface which represents a device enumerator.
-  base::win::ScopedComPtr<IMMDeviceEnumerator> device_enumerator_;
-
   // An IAudioClient interface which enables a client to create and initialize
   // an audio stream between an audio application and the audio engine.
   base::win::ScopedComPtr<IAudioClient> audio_client_;
@@ -250,6 +244,8 @@ class MEDIA_EXPORT WASAPIAudioOutputStream :
 
   // Container for retrieving data from AudioSourceCallback::OnMoreData().
   scoped_ptr<AudioBus> audio_bus_;
+
+  base::win::ScopedComPtr<IAudioClock> audio_clock_;
 
   DISALLOW_COPY_AND_ASSIGN(WASAPIAudioOutputStream);
 };
