22ab886d8c81c71f4a16c256b3e6a1c4e550bcf7
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-28 16:17:12 +0000

    Prepare for using both backends in single SyncFileSystemService, part 2
    - Changes (fixes) GetSyncFileSystemDir() behavior to return non-dev dir if V2 is globally enabled
    - Adds '--syncfs-v2-whitelist' command line to allow specifying whitelisted app IDs at run-time, mainly for testing
    - Moves SyncEngine creation code from SyncFileSystemServiceFactory to RemoteFileSyncService::CreateForBrowserContext(version)
    - Changes the database name for v2 ("DriveMetadata" -> "DriveMetadata_v2") so that v1 and v2 db can coexist
    
    Almost no functional changes yet, except for GetSyncFileSystemDir() behavior fix
    
    BUG=324215
    R=tzik
    
    Review URL: https://codereview.chromium.org/94263002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237799 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/remote_file_sync_service.h
+++ chrome/browser/sync_file_system/remote_file_sync_service.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_SYNC_FILE_SYSTEM_REMOTE_FILE_SYNC_SERVICE_H_
 
 #include <map>
+#include <set>
 #include <string>
 #include <vector>
 
@@ -16,10 +17,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync_file_system/sync_file_metadata.h"
 #include "webkit/browser/fileapi/file_system_url.h"
 
+class BrowserContextKeyedServiceFactory;
 class GURL;
 
 namespace base {
 class ListValue;
+}
+
+namespace content {
+class BrowserContext;
 }
 
 namespace webkit_blob {
@@ -66,6 +72,11 @@ enum RemoteServiceState {
 // Owned by SyncFileSystemService.
 class RemoteFileSyncService {
  public:
+  enum BackendVersion {
+    V1,
+    V2,
+  };
+
   class Observer {
    public:
     Observer() {}
@@ -107,6 +118,18 @@ class RemoteFileSyncService {
   typedef base::Callback<void(SyncStatusCode status,
                               webkit_blob::ScopedFile downloaded)>
       DownloadVersionCallback;
+
+  // Creates an initialized RemoteFileSyncService for backend |version|
+  // for |context|.
+  static scoped_ptr<RemoteFileSyncService> CreateForBrowserContext(
+      BackendVersion version,
+      content::BrowserContext* context);
+
+  // Returns BrowserContextKeyedServiceFactory's an instance of
+  // RemoteFileSyncService for backend |version| depends on.
+  static void AppendDependsOnFactories(
+      BackendVersion version,
+      std::set<BrowserContextKeyedServiceFactory*>* factories);
 
   RemoteFileSyncService() {}
   virtual ~RemoteFileSyncService() {}
--- chrome/chrome_browser.gypi
+++ chrome/chrome_browser.gypi
@@ -2452,6 +2452,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/sync_file_system/logger.cc',
         'browser/sync_file_system/logger.h',
         'browser/sync_file_system/remote_change_processor.h',
+        'browser/sync_file_system/remote_file_sync_service.cc',
         'browser/sync_file_system/remote_file_sync_service.h',
         'browser/sync_file_system/sync_action.h',
         'browser/sync_file_system/sync_callbacks.h',
