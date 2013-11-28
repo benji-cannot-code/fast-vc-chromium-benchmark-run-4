8b0f54b15d3d3c0a61344d3288bae0e05ee62c6e
dslomov@chromium.org <dslomov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-28 15:48:56 +0000

    Remove all deprecated API usage from bindings/v8/custom
    R=haraken@chromium.org, jochen@chromium.org, mkwst@chromium.org
    BUG=324225
    
    Review URL: https://codereview.chromium.org/94993005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162850 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/custom/V8GeolocationCustom.cpp
+++ third_party/WebKit/Source/bindings/v8/custom/V8GeolocationCustom.cpp
@@ -39,7 +39,7 @@ using namespace WTF;
 
 namespace WebCore {
 
-static PassRefPtr<PositionOptions> createPositionOptions(v8::Local<v8::Value> value, bool& succeeded)
+static PassRefPtr<PositionOptions> createPositionOptions(v8::Local<v8::Value> value, v8::Isolate* isolate, bool& succeeded)
 {
     succeeded = true;
 
@@ -59,7 +59,7 @@ static PassRefPtr<PositionOptions> createPositionOptions(v8::Local<v8::Value> va
     // - If the getter or the property's valueOf method throws an exception, we
     //   quit so as not to risk overwriting the exception.
     // - If the value is absent or undefined, we don't override the default.
-    v8::Local<v8::Value> enableHighAccuracyValue = object->Get(v8::String::NewSymbol("enableHighAccuracy"));
+    v8::Local<v8::Value> enableHighAccuracyValue = object->Get(v8Symbol("enableHighAccuracy", isolate));
     if (enableHighAccuracyValue.IsEmpty()) {
         succeeded = false;
         return 0;
@@ -73,7 +73,7 @@ static PassRefPtr<PositionOptions> createPositionOptions(v8::Local<v8::Value> va
         options->setEnableHighAccuracy(enableHighAccuracyBoolean->Value());
     }
 
-    v8::Local<v8::Value> timeoutValue = object->Get(v8::String::NewSymbol("timeout"));
+    v8::Local<v8::Value> timeoutValue = object->Get(v8Symbol("timeout", isolate));
     if (timeoutValue.IsEmpty()) {
         succeeded = false;
         return 0;
@@ -97,7 +97,7 @@ static PassRefPtr<PositionOptions> createPositionOptions(v8::Local<v8::Value> va
         }
     }
 
-    v8::Local<v8::Value> maximumAgeValue = object->Get(v8::String::NewSymbol("maximumAge"));
+    v8::Local<v8::Value> maximumAgeValue = object->Get(v8Symbol("maximumAge", isolate));
     if (maximumAgeValue.IsEmpty()) {
         succeeded = false;
         return 0;
@@ -140,7 +140,7 @@ void V8Geolocation::getCurrentPositionMethodCustom(const v8::FunctionCallbackInf
     if (!succeeded)
         return;
 
-    RefPtr<PositionOptions> positionOptions = createPositionOptions(info[2], succeeded);
+    RefPtr<PositionOptions> positionOptions = createPositionOptions(info[2], info.GetIsolate(), succeeded);
     if (!succeeded)
         return;
     ASSERT(positionOptions);
@@ -163,7 +163,7 @@ void V8Geolocation::watchPositionMethodCustom(const v8::FunctionCallbackInfo<v8:
     if (!succeeded)
         return;
 
-    RefPtr<PositionOptions> positionOptions = createPositionOptions(info[2], succeeded);
+    RefPtr<PositionOptions> positionOptions = createPositionOptions(info[2], info.GetIsolate(), succeeded);
     if (!succeeded)
         return;
     ASSERT(positionOptions);
