276f392260984f90ef78d2ea872ce24a24733bf0
ulan@chromium.org <ulan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-28 12:43:23 +0000

    Remove calls to deprecated V8 APIs in V8ValueCache.cpp
    BUG=324225
    R=haraken@chromium.org
    
    Review URL: https://codereview.chromium.org/94893002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162834 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/V8ValueCache.cpp
+++ third_party/WebKit/Source/bindings/v8/V8ValueCache.cpp
@@ -33,18 +33,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-static v8::Local<v8::String> makeExternalString(const String& string)
+static v8::Local<v8::String> makeExternalString(const String& string, v8::Isolate* isolate)
 {
     if (string.is8Bit()) {
         WebCoreStringResource8* stringResource = new WebCoreStringResource8(string);
-        v8::Local<v8::String> newString = v8::String::NewExternal(stringResource);
+        v8::Local<v8::String> newString = v8::String::NewExternal(isolate, stringResource);
         if (newString.IsEmpty())
             delete stringResource;
         return newString;
     }
 
     WebCoreStringResource16* stringResource = new WebCoreStringResource16(string);
-    v8::Local<v8::String> newString = v8::String::NewExternal(stringResource);
+    v8::Local<v8::String> newString = v8::String::NewExternal(isolate, stringResource);
     if (newString.IsEmpty())
         delete stringResource;
     return newString;
@@ -88,7 +88,7 @@ v8::Local<v8::String> StringCache::createStringAndInsertIntoCache(StringImpl* st
     ASSERT(!m_stringCache.contains(stringImpl));
     ASSERT(stringImpl->length());
 
-    v8::Local<v8::String> newString = makeExternalString(String(stringImpl));
+    v8::Local<v8::String> newString = makeExternalString(String(stringImpl), isolate);
     if (newString.IsEmpty())
         return newString;
 
@@ -112,7 +112,7 @@ void StringCache::makeWeakCallback(v8::Isolate* isolate, v8::Persistent<v8::Stri
     ASSERT(stringCache->m_stringCache.contains(stringImpl));
     stringCache->m_stringCache.remove(stringImpl);
     stringImpl->deref();
-    wrapper->Dispose();
+    wrapper->Reset();
 }
 
 } // namespace WebCore
