2f398d4a09a6bee7a23c36bd22ad2afbe3b9630f
joth@chromium.org <joth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-28 07:37:06 +0000

    Fix file:// URIs passed in the file upload callback
    Follow-up to http://crrev.com/r236192 - we can now handle content:
    and absolute paths being passed back from the app in response to
    the showFileChooser callback (as FilePath natively handles both those
    formats). This completes the hookup by parsing the path out of
    file: schemes (e.g. file:///foo/bar => /foo/bar)
    
    BUG=Internal b/6930981
    
    Review URL: https://codereview.chromium.org/93163003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237737 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/native/aw_web_contents_delegate.cc
+++ android_webview/native/aw_web_contents_delegate.cc
@@ -200,8 +200,11 @@ static void FilesSelectedInChooser(
   std::vector<ui::SelectedFileInfo> files;
   files.reserve(file_path_str.size());
   for (size_t i = 0; i < file_path_str.size(); ++i) {
-    files.push_back(ui::SelectedFileInfo(base::FilePath(file_path_str[i]),
-                                         base::FilePath()));
+    GURL url(file_path_str[i]);
+    if (!url.is_valid())
+      continue;
+    base::FilePath path(url.SchemeIsFile() ? url.path() : file_path_str[i]);
+    files.push_back(ui::SelectedFileInfo(path, base::FilePath()));
   }
   FileChooserParams::Mode mode;
   if (mode_flags & kFileChooserModeOpenFolder) {
@@ -211,8 +214,8 @@ static void FilesSelectedInChooser(
   } else {
     mode = FileChooserParams::Open;
   }
-  LOG(INFO) << "File Chooser result: mode = " << mode
-            << ", file paths = " << JoinString(file_path_str, ":");
+  DVLOG(0) << "File Chooser result: mode = " << mode
+           << ", file paths = " << JoinString(file_path_str, ":");
   rvh->FilesSelectedInChooser(files, mode);
 }
 
