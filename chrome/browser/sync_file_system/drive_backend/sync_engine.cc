90effabbe264818eb59990d0cbfbed0717a1bfd8
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-29 03:57:46 +0000

    [SyncFS] Add TODOs for manual conflict resolution functions
    These function can be called in current code, but not meaningful until
    we start supporting manual conflict resolution.
    
    BUG=240165
    NOTRY=true
    R=kinuko@chromium.org, nhiroki@chromium.org
    
    Review URL: https://codereview.chromium.org/94193003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237870 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_backend/sync_engine.cc
+++ chrome/browser/sync_file_system/drive_backend/sync_engine.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync_file_system/syncable_file_system_util.h"
 #include "content/public/browser/browser_thread.h"
 #include "extensions/common/extension.h"
+#include "webkit/common/blob/scoped_file.h"
 
 namespace sync_file_system {
 namespace drive_backend {
@@ -229,7 +230,7 @@ LocalChangeProcessor* SyncEngine::GetLocalChangeProcessor() {
 }
 
 bool SyncEngine::IsConflicting(const fileapi::FileSystemURL& url) {
-  NOTIMPLEMENTED();
+  // TODO(tzik): Implement this before we support manual conflict resolution.
   return false;
 }
 
@@ -290,14 +291,16 @@ SyncEngine::GetConflictResolutionPolicy() const {
 void SyncEngine::GetRemoteVersions(
     const fileapi::FileSystemURL& url,
     const RemoteVersionsCallback& callback) {
-  NOTIMPLEMENTED();
+  // TODO(tzik): Implement this before we support manual conflict resolution.
+  callback.Run(SYNC_STATUS_FAILED, std::vector<Version>());
 }
 
 void SyncEngine::DownloadRemoteVersion(
     const fileapi::FileSystemURL& url,
     const std::string& version_id,
     const DownloadVersionCallback& callback) {
-  NOTIMPLEMENTED();
+  // TODO(tzik): Implement this before we support manual conflict resolution.
+  callback.Run(SYNC_STATUS_FAILED, webkit_blob::ScopedFile());
 }
 
 void SyncEngine::ApplyLocalChange(
@@ -420,6 +423,8 @@ void SyncEngine::DidProcessRemoteChange(RemoteToLocalSyncer* syncer,
                                           syncer->sync_action(),
                                           SYNC_DIRECTION_REMOTE_TO_LOCAL));
   }
+
+  callback.Run(status, syncer->url());
 }
 
 void SyncEngine::DidApplyLocalChange(LocalToRemoteSyncer* syncer,
