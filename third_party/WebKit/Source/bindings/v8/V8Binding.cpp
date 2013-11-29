33adbbd9c91a637463c4d5a349a3730140ae4891
dcarney@chromium.org <dcarney@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-29 12:16:36 +0000

    Remove random deprecated v8 api usages from blink
    BUG=324225
    R=jochen@chromium.org
    
    Review URL: https://codereview.chromium.org/96863002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162899 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/V8Binding.cpp
+++ third_party/WebKit/Source/bindings/v8/V8Binding.cpp
@@ -602,7 +602,7 @@ DOMWrapperWorld* isolatedWorldForIsolate(v8::Isolate* isolate)
         return 0;
     if (!DOMWrapperWorld::isolatedWorldsExist())
         return 0;
-    ASSERT(v8::Context::InContext());
+    ASSERT(isolate->InContext());
     return DOMWrapperWorld::isolatedWorld(isolate->GetCurrentContext());
 }
 
