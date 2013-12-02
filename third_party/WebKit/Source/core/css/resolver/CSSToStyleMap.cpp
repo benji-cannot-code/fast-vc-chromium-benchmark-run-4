abe2040047740137607c9adb03badddefd85f1b3
timloh@chromium.org <timloh@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-02 02:17:18 +0000

    Move CSSToStyleMap to from core/css to core/css/resolver
    Trivial clean-up; CSSToStyleMap is only used during style resolution, so
    move it to the resolver directory.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/97993002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162939 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/resolver/CSSToStyleMap.cpp
+++ third_party/WebKit/Source/core/css/resolver/CSSToStyleMap.cpp
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
-#include "core/css/CSSToStyleMap.h"
+#include "core/css/resolver/CSSToStyleMap.h"
 
 #include "CSSValueKeywords.h"
 #include "core/css/CSSBorderImageSliceValue.h"
--- third_party/WebKit/Source/core/css/resolver/StyleResolverState.h
+++ third_party/WebKit/Source/core/css/resolver/StyleResolverState.h
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/animation/css/CSSAnimations.h"
 #include "core/css/CSSSVGDocumentValue.h"
 #include "core/css/CSSToLengthConversionData.h"
-#include "core/css/CSSToStyleMap.h"
+#include "core/css/resolver/CSSToStyleMap.h"
 #include "core/css/resolver/ElementResolveContext.h"
 #include "core/css/resolver/ElementStyleResources.h"
 #include "core/css/resolver/FontBuilder.h"
