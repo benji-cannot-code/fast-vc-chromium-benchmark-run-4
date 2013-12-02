af5480b58434d3fa96ddfa1d0624c414a1e25567
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-02 08:19:17 +0000

    Revert 238049 "[Tests] Disable AwQuotaManagerBridgeTest.testDele..."
    > [Tests] Disable AwQuotaManagerBridgeTest.testDeleteAllWithAppCache
    > 
    > Disabling flaky android test.
    > 
    > BUG=324714
    > TBR=boliu@chromium.org, benm@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/98463003
    
    TBR=tzik@chromium.org
    
    Review URL: https://codereview.chromium.org/98093004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238052 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/javatests/src/org/chromium/android_webview/test/AwQuotaManagerBridgeTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwQuotaManagerBridgeTest.java
@@ -160,12 +160,8 @@ public class AwQuotaManagerBridgeTest extends AwTestBase {
               "window.applicationCache.update();");
     }
 
-    /*
     @LargeTest
     @Feature({"AndroidWebView", "WebStore"})
-    http://crbug.com/324714
-    */
-    @DisabledTest
     public void testDeleteAllWithAppCache() throws Exception {
         long currentUsage = getUsageForOrigin(mOrigin);
         assertEquals(0, currentUsage);
