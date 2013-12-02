6ccbece57b187f0b121dbc43cf1fd670da02bd9d
pmeenan@chromium.org <pmeenan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-02 20:22:11 +0000

    Changed the background tokenizer to keep track of the pending tokens and use that as a limit for how far ahead it can parse instead of the rough proxy of checkpoint counts that it was using before.
    This solves all of the issues I have seen where the preload parser was not seeing resources in the html because of a lot of script tags (with each script tag causing a checkpoint).  It also more directly ties the limit to actual memory use which was the original intent.
    
    BUG=284927
    
    Review URL: https://codereview.chromium.org/68133019
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163023 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/parser/BackgroundHTMLInputStream.cpp
+++ third_party/WebKit/Source/core/html/parser/BackgroundHTMLInputStream.cpp
@@ -32,6 +32,7 @@ namespace WebCore {
 BackgroundHTMLInputStream::BackgroundHTMLInputStream()
     : m_firstValidCheckpointIndex(0)
     , m_firstValidSegmentIndex(0)
+    , m_totalCheckpointTokenCount(0)
 {
 }
 
@@ -46,10 +47,11 @@ void BackgroundHTMLInputStream::close()
     m_current.close();
 }
 
-HTMLInputCheckpoint BackgroundHTMLInputStream::createCheckpoint()
+HTMLInputCheckpoint BackgroundHTMLInputStream::createCheckpoint(size_t tokensExtractedSincePreviousCheckpoint)
 {
     HTMLInputCheckpoint checkpoint = m_checkpoints.size();
-    m_checkpoints.append(Checkpoint(m_current, m_segments.size()));
+    m_checkpoints.append(Checkpoint(m_current, m_segments.size(), tokensExtractedSincePreviousCheckpoint));
+    m_totalCheckpointTokenCount += tokensExtractedSincePreviousCheckpoint;
     return checkpoint;
 }
 
@@ -71,6 +73,8 @@ void BackgroundHTMLInputStream::invalidateCheckpointsBefore(HTMLInputCheckpoint 
     for (size_t i = m_firstValidCheckpointIndex; i < newFirstValidCheckpointIndex; ++i)
         m_checkpoints[i].clear();
     m_firstValidCheckpointIndex = newFirstValidCheckpointIndex;
+
+    updateTotalCheckpointTokenCount();
 }
 
 void BackgroundHTMLInputStream::rewindTo(HTMLInputCheckpoint checkpointIndex, const String& unparsedInput)
@@ -100,6 +104,16 @@ void BackgroundHTMLInputStream::rewindTo(HTMLInputCheckpoint checkpointIndex, co
     m_checkpoints.clear();
     m_firstValidCheckpointIndex = 0;
     m_firstValidSegmentIndex = 0;
+
+    updateTotalCheckpointTokenCount();
+}
+
+void BackgroundHTMLInputStream::updateTotalCheckpointTokenCount()
+{
+    m_totalCheckpointTokenCount = 0;
+    size_t lastCheckpointIndex = m_checkpoints.size();
+    for (size_t i = 0; i < lastCheckpointIndex; ++i)
+        m_totalCheckpointTokenCount += m_checkpoints[i].tokensExtractedSincePreviousCheckpoint;
 }
 
 }
