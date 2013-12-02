2f4239f671c9ba51aa075a7f2780c7d103879a0c
alexis.menard@intel.com <alexis.menard@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-02 19:57:14 +0000

    Make const the interface returned by ppb_nacl_private_impl.
    After r238083 it was modified to be non-const by mistake and
    it should stay const.
    
    This is part of an effort to componentize NaCl code.
    
    BUG=244791
    
    Review URL: https://codereview.chromium.org/99153003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238160 0039d316-1c4b-4281-b951-d872f2087c98

--- components/nacl/renderer/ppb_nacl_private_impl.cc
+++ components/nacl/renderer/ppb_nacl_private_impl.cc
@@ -410,7 +410,7 @@ void SetReadOnlyProperty(PP_Instance instance,
   plugin_instance->SetEmbedProperty(key, value);
 }
 
-PPB_NaCl_Private nacl_interface = {
+const PPB_NaCl_Private nacl_interface = {
   &LaunchSelLdr,
   &StartPpapiProxy,
   &UrandomFD,
@@ -430,7 +430,7 @@ PPB_NaCl_Private nacl_interface = {
 
 namespace nacl {
 
-PPB_NaCl_Private* GetNaClPrivateInterface() {
+const PPB_NaCl_Private* GetNaClPrivateInterface() {
   return &nacl_interface;
 }
 
