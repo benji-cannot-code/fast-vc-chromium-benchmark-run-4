ecbbc92e877347c0f902898cb8dc8befae5532e2
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-02 01:01:01 +0000

    Extract unit tests from the mixed bag process_util_unittest.cc into proper files.
    It is a first step of multiple CLs.
    
    Rename process_metrics_unittests.cc to process_metrics_unittest.cc to be
    consistent with the rest of the project.
    
    Rename process_util_unittest_ios.cc to process_metrics_unittest_ios.cc since it
    is just about process_metrics.h.
    
    Extract a few process_metrics.h specific unit tests from
    process_util_unittest.cc into process_metrics_unittest.cc.
    
    R=thakis@chromium.org
    BUG=324546
    
    Review URL: https://codereview.chromium.org/78893002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238009 0039d316-1c4b-4281-b951-d872f2087c98

--- base/process/process_metrics_unittest_ios.cc
+++ base/process/process_metrics_unittest_ios.cc
@@ -1,13 +1,14 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright 2013 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/memory/scoped_ptr.h"
 #include "base/process/process_metrics.h"
+
+#include "base/memory/scoped_ptr.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-TEST(ProcessUtilTestIos, Memory) {
+TEST(ProcessMetricsTestIos, Memory) {
   scoped_ptr<base::ProcessMetrics> process_metrics(
       base::ProcessMetrics::CreateProcessMetrics(
           base::GetCurrentProcessHandle()));
