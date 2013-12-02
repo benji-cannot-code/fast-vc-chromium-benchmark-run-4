c3ac06e12614d23b6c387b11f3231149de889533
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-02 23:24:19 +0000

    Initialize ICU early in npchrome_frame.dll's lifetime.
    This fixes a crash when navigating to an URL containing an
    internationalized domain name.
    
    BUG=324855
    R=robertshield@chromium.org
    
    Review URL: https://codereview.chromium.org/99623003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238226 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/chrome_tab.cc
+++ chrome_frame/chrome_tab.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/file_util.h"
 #include "base/file_version_info.h"
+#include "base/i18n/icu_util.h"
 #include "base/logging.h"
 #include "base/logging_win.h"
 #include "base/path_service.h"
@@ -108,7 +109,9 @@ class ChromeTabModule : public CAtlDllModuleT<ChromeTabModule> {
  public:
   typedef CAtlDllModuleT<ChromeTabModule> ParentClass;
 
-  ChromeTabModule() : do_system_registration_(true), crash_reporting_(NULL) {}
+  ChromeTabModule() : do_system_registration_(true),
+                      crash_reporting_(NULL),
+                      icu_initialized_(false) {}
 
   DECLARE_LIBID(LIBID_ChromeTabLib)
   DECLARE_REGISTRY_APPID_RESOURCEID(IDR_CHROMETAB,
@@ -200,13 +203,20 @@ class ChromeTabModule : public CAtlDllModuleT<ChromeTabModule> {
 
   // The module is "locked" when an object takes a reference on it. The first
   // time it is locked, take a reference on crash reporting to bind its lifetime
-  // to the module.
+  // to the module and initialize ICU.
   virtual LONG Lock() throw() {
     LONG result = ParentClass::Lock();
     if (result == 1) {
       DCHECK_EQ(crash_reporting_,
                 static_cast<chrome_frame::ScopedCrashReporting*>(NULL));
       crash_reporting_ = new chrome_frame::ScopedCrashReporting();
+
+      // Initialize ICU if this is the first time the module has been locked.
+      if (!icu_initialized_) {
+        icu_initialized_ = true;
+        // Best-effort since something is better than nothing here.
+        ignore_result(base::i18n::InitializeICU());
+      }
     }
     return result;
   }
@@ -234,6 +244,10 @@ class ChromeTabModule : public CAtlDllModuleT<ChromeTabModule> {
   // reporting to shut down at exit, which would lead to problems with the
   // loader lock.
   chrome_frame::ScopedCrashReporting* crash_reporting_;
+
+  // Initially false, this is flipped to true to indicate that ICU has been
+  // initialized for the module.
+  bool icu_initialized_;
 };
 
 ChromeTabModule _AtlModule;
