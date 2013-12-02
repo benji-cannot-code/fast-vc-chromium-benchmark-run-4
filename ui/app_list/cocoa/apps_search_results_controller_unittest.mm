aeff720b7de3b96b2a2fdf30afe4ef6524e684c9
tapted@chromium.org <tapted@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-02 12:17:07 +0000

    Fix flaky AppsSearchResultsControllerTest.UninstallReperformsSearch
    Also fix the typo "reDperforms".
    
    Since the asynchronous event that the test was waiting for is a Cocoa
    event, the QuitClosure was only flakily ensuring that the event was
    before it in the message queue. Using the same queue ensures the
    messages are properly ordered.
    
    BUG=308828
    
    Review URL: https://codereview.chromium.org/98323002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238073 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/app_list/cocoa/apps_search_results_controller_unittest.mm
+++ ui/app_list/cocoa/apps_search_results_controller_unittest.mm
@@ -28,6 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(readonly, nonatomic) app_list::SearchResult* lastOpenedResult;
 @property(readonly, nonatomic) int redoSearchCount;
 
+- (void)quitMessageLoop;
+
 @end
 
 @implementation TestAppsSearchResultsDelegate
@@ -45,6 +47,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)redoSearch {
   ++redoSearchCount_;
+}
+
+- (void)quitMessageLoop {
+  base::MessageLoop::current()->QuitNow();
 }
 
 @end
@@ -296,12 +302,13 @@ TEST_F(AppsSearchResultsControllerTest, ContextMenus) {
 }
 
 // Test that observing a search result item uninstall performs the search again.
-// Disabled due to failure on 10.6 http://crbug.com/308828 - 10.7 is OK.
-TEST_F(AppsSearchResultsControllerTest, DISABLED_UninstallRedperformsSearch) {
+TEST_F(AppsSearchResultsControllerTest, UninstallReperformsSearch) {
   base::MessageLoopForUI message_loop;
   EXPECT_EQ(0, [delegate_ redoSearchCount]);
   ModelResultAt(0)->NotifyItemUninstalled();
-  message_loop.PostTask(FROM_HERE, base::MessageLoop::QuitClosure());
+  [delegate_ performSelector:@selector(quitMessageLoop)
+                  withObject:nil
+                  afterDelay:0];
   message_loop.Run();
   EXPECT_EQ(1, [delegate_ redoSearchCount]);
 }
