28398d1d2f2c5de0ce3c679cb3bb9c84df2577e3
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-02 18:51:43 +0000

    Mojo: Create a PlatformChannel abstraction.
    PlatformChannel abstracts the *creation* of a (bidirectional) "OS
    pipe"/"channel", and will (soon) also provide a platform-independent way
    of passing such a channel to a (new) child process.
    
    It intentionally does not abstract away the *use* of such a channel,
    which will still require using native primitives (via a native OS
    handle, thinly wrapped by the existing PlatformChannelHandle).
    
    Note that though channels are bidirectional and are functionally
    symmetric, there are nonetheless "server" and "client" ends. This is to
    accomodate setups where there is such a distinction at the OS level (and
    in particular, setups where channels are named OS objects).
    
    R=darin@chromium.org, darin
    
    Review URL: https://codereview.chromium.org/92563002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238141 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ mojo/system/platform_channel.cc
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "mojo/system/platform_channel.h"
+
+#include "base/logging.h"
+
+namespace mojo {
+namespace system {
+
+PlatformChannel::~PlatformChannel() {
+  // Implementations must close the handle if necessary (e.g., if no one else
+  // has taken ownership).
+  DCHECK(!is_valid());
+}
+
+PlatformChannelHandle PlatformChannel::PassHandle() {
+  DCHECK(is_valid());
+  PlatformChannelHandle rv = handle_;
+  handle_ = PlatformChannelHandle();
+  return rv;
+}
+
+PlatformChannel::PlatformChannel() {
+}
+
+PlatformServerChannel::PlatformServerChannel(const std::string& name)
+    : name_(name) {
+  DCHECK(!name_.empty());
+}
+
+// Static factory method.
+// static
+scoped_ptr<PlatformClientChannel> PlatformClientChannel::CreateFromHandle(
+      const PlatformChannelHandle& handle) {
+  scoped_ptr<PlatformClientChannel> rv(new PlatformClientChannel());
+  *rv->mutable_handle() = handle;
+  return rv.Pass();
+}
+
+}  // namespace system
+}  // namespace mojo
--- mojo/system/raw_channel_posix.cc
+++ mojo/system/raw_channel_posix.cc
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace mojo {
 namespace system {
 
+namespace {
+
 const size_t kReadSize = 4096;
 
 class RawChannelPosix : public RawChannel,
@@ -392,6 +394,8 @@ void RawChannelPosix::CancelPendingWritesNoLock() {
   }
   write_message_queue_.clear();
 }
+
+}  // namespace
 
 // -----------------------------------------------------------------------------
 
