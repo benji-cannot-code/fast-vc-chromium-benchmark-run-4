9cde035b4b8d06158aa028d52fc92a0075480fdf
betravis@adobe.com <betravis@adobe.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-02 19:02:23 +0000

    Shape-inside should default to 'auto'
    As part of the split between shape-outside and shape-inside, the default
    value of shape-inside should be 'auto' rather than 'outside-shape'.
    
    BUG=322165
    
    Review URL: https://codereview.chromium.org/92583002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163017 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/style/RenderStyle.h
+++ third_party/WebKit/Source/core/rendering/style/RenderStyle.h
@@ -1423,7 +1423,7 @@ public:
     }
     ShapeValue* shapeOutside() const { return rareNonInheritedData->m_shapeOutside.get(); }
 
-    static ShapeValue* initialShapeInside();
+    static ShapeValue* initialShapeInside() { return 0; }
     static ShapeValue* initialShapeOutside() { return 0; }
 
     void setClipPath(PassRefPtr<ClipPathOperation> operation)
