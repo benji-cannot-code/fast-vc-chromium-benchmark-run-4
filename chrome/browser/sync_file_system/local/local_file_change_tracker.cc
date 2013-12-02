391c717e68d315492e7754effcdd8137341b92f6
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-02 10:18:20 +0000

    Implement LocalFileChangeTracker::ResetForFileSystem for remote root deletion
    When a DELETE change for the root file system comes for a remote sync, I'm planning to call:
    1. DeleteFileSystem to delete the entire filesystem data
    2. Call this to reset all changes for the filesystem, and
    3. OpenFileSystem to reopen the filesystem
    
    BUG=324697
    TEST=LocalFileChangeTrackerTest.ResetForFileSystem
    R=nhiroki@chromium.org, tzik@chromium.org
    
    Review URL: https://codereview.chromium.org/98493002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238060 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/local/local_file_change_tracker.cc
+++ chrome/browser/sync_file_system/local/local_file_change_tracker.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync_file_system/local/local_file_sync_status.h"
 #include "chrome/browser/sync_file_system/syncable_file_system_util.h"
 #include "third_party/leveldatabase/src/include/leveldb/db.h"
+#include "third_party/leveldatabase/src/include/leveldb/write_batch.h"
 #include "webkit/browser/fileapi/file_system_context.h"
 #include "webkit/browser/fileapi/file_system_file_util.h"
 #include "webkit/browser/fileapi/file_system_operation_context.h"
@@ -43,6 +44,7 @@ class LocalFileChangeTracker::TrackerDB {
   SyncStatusCode ClearDirty(const std::string& url);
   SyncStatusCode GetDirtyEntries(
       std::queue<FileSystemURL>* dirty_files);
+  SyncStatusCode WriteBatch(scoped_ptr<leveldb::WriteBatch> batch);
 
  private:
   enum RecoveryOption {
@@ -204,6 +206,37 @@ SyncStatusCode LocalFileChangeTracker::Initialize(
   if (status == SYNC_STATUS_OK)
     initialized_ = true;
   return status;
+}
+
+void LocalFileChangeTracker::ResetForFileSystem(
+    const GURL& origin,
+    fileapi::FileSystemType type) {
+  DCHECK(file_task_runner_->RunsTasksOnCurrentThread());
+  scoped_ptr<leveldb::WriteBatch> batch(new leveldb::WriteBatch);
+  for (FileChangeMap::iterator iter = changes_.begin();
+       iter != changes_.end();) {
+    fileapi::FileSystemURL url = iter->first;
+    if (url.origin() != origin || url.type() != type) {
+      ++iter;
+      continue;
+    }
+    mirror_changes_.erase(url);
+    change_seqs_.erase(iter->second.change_seq);
+    changes_.erase(iter++);
+
+    std::string serialized_url;
+    const bool should_success =
+        SerializeSyncableFileSystemURL(url, &serialized_url);
+    if (!should_success) {
+      NOTREACHED() << "Failed to serialize: " << url.DebugString();
+      continue;
+    }
+    batch->Delete(serialized_url);
+  }
+  // Fail to apply batch to database wouldn't have critical effect, they'll be
+  // just marked deleted on next relaunch.
+  tracker_db_->WriteBatch(batch.Pass());
+  UpdateNumChanges();
 }
 
 void LocalFileChangeTracker::UpdateNumChanges() {
@@ -468,6 +501,21 @@ SyncStatusCode LocalFileChangeTracker::TrackerDB::GetDirtyEntries(
     }
     dirty_files->push(url);
     iter->Next();
+  }
+  return SYNC_STATUS_OK;
+}
+
+SyncStatusCode LocalFileChangeTracker::TrackerDB::WriteBatch(
+    scoped_ptr<leveldb::WriteBatch> batch) {
+  if (db_status_ != SYNC_STATUS_OK)
+    return db_status_;
+
+  leveldb::Status status = db_->Write(leveldb::WriteOptions(), batch.get());
+  if (!status.ok() && !status.IsNotFound()) {
+    HandleError(FROM_HERE, status);
+    db_status_ = LevelDBStatusToSyncStatusCode(status);
+    db_.reset();
+    return db_status_;
   }
   return SYNC_STATUS_OK;
 }
