5f406e0f3cf52428419a5883dbd4f6aada3bd21f
mtomasz@chromium.org <mtomasz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-02 10:15:56 +0000

    Limit usage of full paths during initialization of Files app.
    This patch being a part of big full path -> Entry migration effords, converts the default path to an entry as soon as possible, and makes the file_manager.js call the directory model with Entry objects instead.
    
    In general this CL:
    - Merges directory_model.js/setupPath() with file_manager.js/setupCurrentDirectory_()
    - Converts defaultPath to Entry as soon as the volume manager is available.
    - Opens the directory and sets the selection using Entry objects only.
    
    TEST=Tested manually with standalone, and save-as dialog. Also, partly covered by browser tests.
    BUG=323753
    
    Review URL: https://codereview.chromium.org/90773002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238057 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/foreground/js/navigation_list_model.js
+++ chrome/browser/resources/file_manager/foreground/js/navigation_list_model.js
@@ -37,6 +37,7 @@ NavigationModelItem.prototype.getCachedEntry = function() {
 };
 
 /**
+ * TODO(mtomasz): Use Entry instead of path.
  * @param {VolumeManagerWrapper} volumeManager VolumeManagerWrapper instance.
  * @param {string} path Path.
  * @param {DirectoryEntry} entry Entry. Can be null.
@@ -51,7 +52,7 @@ NavigationModelItem.create = function(
   // If the given entry is null, try to resolve path to get an entry.
   if (!entry) {
     item.resolvingQueue_.run(function(continueCallback) {
-      volumeManager.resolvePath(
+      volumeManager.resolveAbsolutePath(
           path,
           function(entry) {
             if (entry.isDirectory)
