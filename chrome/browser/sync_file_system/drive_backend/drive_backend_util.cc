a18cdaf456e4a4d4ce0e2920846ff40ab37af142
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-02 04:02:17 +0000

    Pass blocking pool runner to CreateTemporaryFile in SyncFS v2 drive_backend
    MessageLoopProxy::current() could be null otherwise, which hits assertion
    
    BUG=240165
    R=tzik@chromium.org
    
    Review URL: https://codereview.chromium.org/98063002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238021 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_backend/drive_backend_util.cc
+++ chrome/browser/sync_file_system/drive_backend/drive_backend_util.cc
@@ -125,7 +125,8 @@ scoped_ptr<FileMetadata> CreateDeletedFileMetadata(
   return file.Pass();
 }
 
-webkit_blob::ScopedFile CreateTemporaryFile() {
+webkit_blob::ScopedFile CreateTemporaryFile(
+    base::TaskRunner* file_task_runner) {
   base::FilePath temp_file_path;
   if (!file_util::CreateTemporaryFile(&temp_file_path))
     return webkit_blob::ScopedFile();
@@ -133,7 +134,7 @@ webkit_blob::ScopedFile CreateTemporaryFile() {
   return webkit_blob::ScopedFile(
       temp_file_path,
       webkit_blob::ScopedFile::DELETE_ON_SCOPE_OUT,
-      base::MessageLoopProxy::current().get());
+      file_task_runner);
 }
 
 std::string FileKindToString(FileKind file_kind) {
