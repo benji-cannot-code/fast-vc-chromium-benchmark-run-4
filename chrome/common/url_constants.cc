0ffe19c5ce5b12bf412d7d331cd8652a3dccf269
sammc@chromium.org <sammc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-03 03:27:42 +0000

    Add chrome://quit to shut down Chrome, including packaged apps.
    Now that packaged apps are not quit when using wrench quit, there is no
    way to completely shut down Chrome. While this is normally a good thing
    as apps are intended to be separate from Chrome, for developers and
    other power users, the ability to completely shut down Chrome can be a
    useful function. This also blacklists chrome://quit and chrome://restart
    from being saved to the user's session to prevent quit or restart loops.
    
    BUG=317414
    
    Review URL: https://codereview.chromium.org/84893002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238290 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/url_constants.cc
+++ chrome/common/url_constants.cc
@@ -75,6 +75,7 @@ const char kChromeUIProfileSigninConfirmationURL[] =
     "chrome://profile-signin-confirmation/";
 const char kChromeUIUserManagerURL[] = "chrome://user-manager/";
 const char kChromeUIPrintURL[] = "chrome://print/";
+const char kChromeUIQuitURL[] = "chrome://quit/";
 const char kChromeUIRestartURL[] = "chrome://restart/";
 const char kChromeUISettingsURL[] = "chrome://settings/";
 const char kChromeUISettingsFrameURL[] = "chrome://settings-frame/";
@@ -204,6 +205,7 @@ const char kChromeUIPredictorsHost[] = "predictors";
 const char kChromeUIPrintHost[] = "print";
 const char kChromeUIProfilerHost[] = "profiler";
 const char kChromeUIQuotaInternalsHost[] = "quota-internals";
+const char kChromeUIQuitHost[] = "quit";
 const char kChromeUIRestartHost[] = "restart";
 const char kChromeUISettingsHost[] = "settings";
 const char kChromeUISettingsFrameHost[] = "settings-frame";
@@ -628,6 +630,7 @@ const char* const kChromeDebugURLs[] = {
   content::kChromeUIGpuHangURL,
   content::kChromeUIPpapiFlashCrashURL,
   content::kChromeUIPpapiFlashHangURL,
+  chrome::kChromeUIQuitURL,
   chrome::kChromeUIRestartURL
 };
 const int kNumberOfChromeDebugURLs =
