7675ae7330a06299a5b97e56a0f0d47df1952389
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-03 12:10:22 +0000

    [rAC, OSX] Allow controller to validate data.
    This implements UpdateForErrors, which allows the
    AutofillDialogController to initiate form validation. Necessary so
    fields can be validated on e.g. instrument change.
    
    BUG=277319
    TEST=Use Wallet, use incomplete instrument, see error markers.
    
    Review URL: https://codereview.chromium.org/100203003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238364 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/autofill/autofill_dialog_cocoa.mm
+++ chrome/browser/ui/cocoa/autofill/autofill_dialog_cocoa.mm
@@ -84,6 +84,7 @@ void AutofillDialogCocoa::UpdateDetailArea() {
 }
 
 void AutofillDialogCocoa::UpdateForErrors() {
+  [sheet_delegate_ updateForErrors];
 }
 
 void AutofillDialogCocoa::UpdateNotificationArea() {
--- chrome/browser/ui/cocoa/autofill/autofill_dialog_window_controller.mm
+++ chrome/browser/ui/cocoa/autofill/autofill_dialog_window_controller.mm
@@ -369,6 +369,10 @@ const CGFloat kMinimumContentsHeight = 101;
   [mainContainer_ updateSaveInChrome];
 }
 
+- (void)updateForErrors {
+  [mainContainer_ validate];
+}
+
 - (content::NavigationController*)showSignIn {
   [self updateSignInSizeConstraints];
   [signInContainer_ loadSignInPage];
