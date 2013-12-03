4c7cc7e83d708bdb63ecafaae77524a1ea4993eb
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-03 02:33:06 +0000

    Define ARCH_CPU_LITTLE_ENDIAN for PNaCl
    PNaCl bytecode is little-endian, but build_config.h didn't define
    ARCH_CPU_LITTLE_ENDIAN.
    
    BUG=276739
    R=sehr@chromium.org
    
    Review URL: https://codereview.chromium.org/69763004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238276 0039d316-1c4b-4281-b951-d872f2087c98

--- build/build_config.h
+++ build/build_config.h
@@ -109,6 +109,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ARCH_CPU_LITTLE_ENDIAN 1
 #elif defined(__pnacl__)
 #define ARCH_CPU_32_BITS 1
+#define ARCH_CPU_LITTLE_ENDIAN 1
 #elif defined(__MIPSEL__)
 #define ARCH_CPU_MIPS_FAMILY 1
 #define ARCH_CPU_MIPSEL 1
