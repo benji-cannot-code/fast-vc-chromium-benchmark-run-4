c9cf1045b4401494a77ea52844ec3aa7c8f2caba
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-03 11:40:28 +0000

    Remove unused ColoredWindowController/DesktopBackgroundFadeController
    BUG=none
    TBR=antrim@chromium.org
    
    Review URL: https://codereview.chromium.org/99783005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238353 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/session_state_animator.h
+++ ash/wm/session_state_animator.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_WM_SESSION_STATE_ANIMATOR_H_
 
 #include "ash/ash_export.h"
-#include "ash/wm/workspace/colored_window_controller.h"
 #include "base/basictypes.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/timer/timer.h"
@@ -141,12 +140,6 @@ class ASH_EXPORT SessionStateAnimator {
   static void GetContainers(int container_mask,
                             aura::Window::Windows* containers);
 
-  // Create |foreground_| layer if it doesn't already exist, but makes it
-  // completely transparent.
-  void CreateForeground();
-  // Destroy |foreground_| when it is not needed anymore.
-  void DropForeground();
-
   // Apply animation |type| to all containers included in |container_mask| with
   // specified |speed|.
   void StartAnimation(int container_mask,
@@ -178,10 +171,6 @@ class ASH_EXPORT SessionStateAnimator {
                              AnimationType type,
                              AnimationSpeed speed,
                              ui::LayerAnimationObserver* observer);
-
-  // White foreground that is used during shutdown animation to "fade
-  // everything into white".
-  scoped_ptr<ColoredWindowController> foreground_;
 
   DISALLOW_COPY_AND_ASSIGN(SessionStateAnimator);
 };
