8e3d6dad30f75dc58c0bd41bbc56de48bed91e8b
fs@opera.com <fs@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-03 16:55:17 +0000

    Add new helper VTTParser::collectDigitsToInt
    Since most uses of VTTParser::collectDigits is followed by a call to
    String::toInt on the resulting string, add this new helper with the
    aim of later removing the temporary string construction.
    Handle overflow in the string-to-int conversion by means of saturation.
    (The latter is the intended change of semantics.)
    
    Replace uses of collectDigits with collectDigitsToInt where applicable.
    After doing this, there remain no callers to collectDigits outside of
    VTTParser, so make the function static.
    
    Also move VTTParser::skipWhiteSpace "textually" to group it with the other
    'scanning' functions, and make it static since it does not depend on any
    VTTParser state.
    
    BUG=305317
    
    Review URL: https://codereview.chromium.org/101513002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163098 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/track/vtt/VTTParser.h
+++ third_party/WebKit/Source/core/html/track/vtt/VTTParser.h
@@ -94,7 +94,7 @@ public:
         // U+0020 SPACE characters or U+0009 CHARACTER TABULATION (tab) characters.
         return c == ' ' || c == '\t';
     }
-    static String collectDigits(const String&, unsigned*);
+    static unsigned collectDigitsToInt(const String& input, unsigned* position, int& number);
     static String collectWord(const String&, unsigned*);
     static double collectTimeStamp(const String&, unsigned*);
 
@@ -134,7 +134,8 @@ private:
     void collectMetadataHeader(const String&);
     void createNewRegion(const String& headerValue);
 
-    void skipWhiteSpace(const String&, unsigned*);
+    static String collectDigits(const String&, unsigned*);
+    static void skipWhiteSpace(const String&, unsigned*);
 
     BufferedLineReader m_lineReader;
     OwnPtr<TextResourceDecoder> m_decoder;
