0e6e9fdef54fdda0389a6576bdc03dc22a6b05f7
hirono@chromium.org <hirono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-03 10:19:23 +0000

    Files.app: Add a unit test of ProgressCenterHandler class.
    This CL adds a unit test of ProgressCenterHanler class for copy operatons.
    
    BUG=324679
    TEST=file_manager_jstest
    R=yoshiki@chromium.org
    
    Review URL: https://codereview.chromium.org/98363003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238332 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/file_manager/unit_tests/mocks/mock_file_operation_manager.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+/**
+ * Mock class of FileOperationManager.
+ * @constructor
+ */
+function MockFileOperationManager() {
+  cr.EventTarget.call(this);
+
+  /**
+   * Event to be dispatched when requestTaskCancel is called.
+   * @type {Event}
+   */
+  this.cancelEvent = null;
+}
+
+MockFileOperationManager.prototype = {
+  __proto__: cr.EventTarget.prototype
+};
+
+/**
+ * Dispatches a pre-specified cancel event.
+ */
+MockFileOperationManager.prototype.requestTaskCancel = function() {
+  this.dispatchEvent(this.cancelEvent);
+};
