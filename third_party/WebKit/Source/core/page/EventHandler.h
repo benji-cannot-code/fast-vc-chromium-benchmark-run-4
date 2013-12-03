379e87162c2ea78b3eb6f4b8562a056df583d817
zeeshanq@chromium.org <zeeshanq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-03 01:48:56 +0000

    Defer clearing of active state for a minimum time after ShowPress.
    BUG=312798
    
    Review URL: https://codereview.chromium.org/69903002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163047 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/EventHandler.h
+++ third_party/WebKit/Source/core/page/EventHandler.h
@@ -177,6 +177,8 @@ public:
 
     bool useHandCursor(Node*, bool isOverLink, bool shiftKey);
 
+    void notifyElementActivated();
+
 private:
     static DragState& dragState();
 
@@ -199,7 +201,7 @@ private:
 
     bool handlePasteGlobalSelection(const PlatformMouseEvent&);
 
-    bool handleGestureTap(const PlatformGestureEvent&);
+    bool handleGestureTap(const PlatformGestureEvent&, const IntPoint& adjustedPoint);
     bool handleGestureLongPress(const PlatformGestureEvent&, const IntPoint& adjustedPoint);
     bool handleGestureLongTap(const PlatformGestureEvent&, const IntPoint& adjustedPoint);
     bool handleGestureTwoFingerTap(const PlatformGestureEvent&, const IntPoint& adjustedPoint);
@@ -214,6 +216,7 @@ private:
 
     void hoverTimerFired(Timer<EventHandler>*);
     void cursorUpdateTimerFired(Timer<EventHandler>*);
+    void activeIntervalTimerFired(Timer<EventHandler>*);
 
     bool logicalScrollOverflow(ScrollLogicalDirection, ScrollGranularity, Node* startingNode = 0);
 
@@ -377,6 +380,11 @@ private:
     OwnPtr<IntPoint> m_lastSyntheticPinchAnchorDip;
     OwnPtr<IntPoint> m_lastSyntheticPanLocation;
     float m_syntheticPageScaleFactor;
+
+    Timer<EventHandler> m_activeIntervalTimer;
+    double m_lastShowPressTimestamp;
+    bool m_shouldKeepActiveForMinInterval;
+    RefPtr<Element> m_lastDeferredTapElement;
 };
 
 } // namespace WebCore
