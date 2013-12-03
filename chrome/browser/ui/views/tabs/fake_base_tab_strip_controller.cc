bde8da900fb9938267f4e238fb245226045eab62
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-03 01:17:28 +0000

    Revert 238253 "Fix dragging supported files to tab strip."
    Probably due to submit order conflict this seems to have broken
    Linux chromeos build (compile):
    http://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Builder/builds/60931
    
    > Fix dragging supported files to tab strip.
    > 
    > Use the inferred MIME type of download items, rather than the server value,
    > when deciding if the tab strip can allow a drop. Get the MIME type
    > asynchronously so we don't crash (IO must be allowed in the call to
    > GetMimeTypeFromFile).
    > 
    > Also check the profile's plugins when determining whether a MIME type is
    > supported.
    > 
    > Regardless of whether a file is supported, the dragging operation should
    > still be allowed so that spring-loaded tabs work with file drags.
    > 
    > BUG=274288
    > R=asanka@chromium.org, sky@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/68133020
    
    TBR=michaelpg@chromium.org
    
    Review URL: https://codereview.chromium.org/100773002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238260 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/tabs/fake_base_tab_strip_controller.cc
+++ chrome/browser/ui/views/tabs/fake_base_tab_strip_controller.cc
@@ -125,7 +125,3 @@ void FakeBaseTabStripController::OnStartedDraggingTabs() {
 
 void FakeBaseTabStripController::OnStoppedDraggingTabs() {
 }
-
-void FakeBaseTabStripController::CheckFileSupported(const GURL& url) {
-  tab_strip_->FileSupported(url, true);
-}
