5f145adb89484a8729ef8a283d13d3e228b49535
Tomasz Mikolajewski <mtomasz@chromium.org>
2013-12-03 11:20:38 +0900

    Fix exporting images.
    This patch fixes a regression in saving files. The file name was being passed
    instead of the Entry. Along the way, error handling has been fixed.
    
    BUG=chromium:325022
    TEST=Tested manually by trying to save a file.
    
    Change-Id: I3345f93508c99980811da371b404d73b8f1a1759
    Reviewed-on: https://chromium-review.googlesource.com/178494
    Reviewed-by: Daichi Hirono <hirono@chromium.org>
    Tested-by: Tomasz Mikolajewski <mtomasz@chromium.org>

--- chrome/browser/resources/chromeos/camera/src/js/views/browser.js
+++ chrome/browser/resources/chromeos/camera/src/js/views/browser.js
@@ -290,7 +290,7 @@ camera.views.Browser.prototype.exportSelection_ = function() {
     mimeTypes: ['image/jpeg']
   }];
 
-  var fileName = this.currentPicture().picture.imageEntry.name;
+  var picture = this.currentPicture().picture;
 
   var onError = function() {
     // TODO(mtomasz): Check if it works.
@@ -301,15 +301,16 @@ camera.views.Browser.prototype.exportSelection_ = function() {
 
   chrome.fileSystem.chooseEntry({
     type: 'saveFile',
-    suggestedName: fileName,
+    suggestedName: picture.imageEntry.name,
     accepts: accepts
   }, function(fileEntry) {
       if (!fileEntry)
         return;
-      this.model.exportPicture(fileName,
-                fileEntry,
-                function() {},
-                onError);
+      this.model.exportPicture(
+          picture,
+          fileEntry,
+          function() {},
+          onError);
   }.bind(this));
 };
 
