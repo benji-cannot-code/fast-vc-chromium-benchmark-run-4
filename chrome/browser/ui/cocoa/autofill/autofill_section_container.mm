0fa1e336d7094a4b9dd59d1a5fdb3e54f349f6e3
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-03 00:30:11 +0000

    [rAC, OSX] Update 'enabled' status on edits.
    When a field is modified, the dialog must update the 'editable' status
    for all fields in the section.
    
    BUG=321331
    TEST=See bug. CVC should now be editable after editing expiration.
    R=isherman@chromium.org
    
    Review URL: https://codereview.chromium.org/88943003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238242 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/autofill/autofill_section_container.mm
+++ chrome/browser/ui/cocoa/autofill/autofill_section_container.mm
@@ -118,6 +118,9 @@ bool CompareInputRows(const autofill::DetailInput* input1,
 // Refresh all field icons based on |delegate_| status.
 - (void)updateFieldIcons;
 
+// Refresh the enabled/disabled state of all input fields.
+- (void)updateEditability;
+
 @end
 
 @implementation AutofillSectionContainer
@@ -283,6 +286,7 @@ bool CompareInputRows(const autofill::DetailInput* input1,
   delegate_->FocusMoved();
   [validationDelegate_ hideErrorBubble];
   [self validateFor:autofill::VALIDATE_EDIT];
+  [self updateEditability];
 }
 
 - (void)updateSuggestionState {
@@ -379,7 +383,10 @@ bool CompareInputRows(const autofill::DetailInput* input1,
 
 - (void)addInputsToArray:(NSMutableArray*)array {
   [array addObjectsFromArray:[inputs_ subviews]];
-  [array addObject:[suggestContainer_ inputField]];
+
+  // Only credit card sections can have a suggestion input.
+  if ([self isCreditCardSection])
+    [array addObject:[suggestContainer_ inputField]];
 }
 
 #pragma mark Internal API for AutofillSectionContainer.
@@ -435,6 +442,7 @@ bool CompareInputRows(const autofill::DetailInput* input1,
   // Update the icon if necessary.
   if (delegate_->FieldControlsIcons(type))
     [self updateFieldIcons];
+  [self updateEditability];
 }
 
 - (autofill::ServerFieldType)fieldTypeForControl:(NSControl*)control {
@@ -487,16 +495,13 @@ bool CompareInputRows(const autofill::DetailInput* input1,
     NSControl<AutofillInputField>* field = [inputs_ viewWithTag:iter->type];
     DCHECK(field);
 
-    // TODO(groby): We need to account for the fact editability state can change
-    // after any input in the same section is edited by the user.
-    [field setEnabled:delegate_->InputIsEditable(*iter, section_)];
-
     if (shouldClobber || [field isDefault]) {
       [field setFieldValue:base::SysUTF16ToNSString(iter->initial_value)];
     }
     if (shouldClobber)
       [field setValidityMessage:@""];
   }
+  [self updateEditability];
   [self updateFieldIcons];
   [self modelChanged];
 }
@@ -622,6 +627,17 @@ bool CompareInputRows(const autofill::DetailInput* input1,
     AutofillTextField* textfield = base::mac::ObjCCastStrict<AutofillTextField>(
         [inputs_ viewWithTag:iter->first]);
     [[textfield cell] setIcon:iter->second.ToNSImage()];
+  }
+}
+
+- (void)updateEditability {
+
+  base::scoped_nsobject<NSMutableArray> controls([[NSMutableArray alloc] init]);
+  [self addInputsToArray:controls];
+  for (NSControl<AutofillInputField>* control in controls.get()) {
+    autofill::ServerFieldType type = [self fieldTypeForControl:control];
+    const autofill::DetailInput* input = [self detailInputForType:type];
+    [control setEnabled:delegate_->InputIsEditable(*input, section_)];
   }
 }
 
