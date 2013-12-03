95020d27dcf0633ec4b99050c9732276e4e446a6
komatsu@chromium.org <komatsu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-03 11:29:41 +0000

    Mode Indicator using BubbleDelegateView.
    BUG=321520
    
    Review URL: https://codereview.chromium.org/98703003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238349 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/input_method/candidate_window_controller_impl.cc
+++ chrome/browser/chromeos/input_method/candidate_window_controller_impl.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/input_method/delayable_widget.h"
 #include "chrome/browser/chromeos/input_method/infolist_window_view.h"
 #include "chrome/browser/chromeos/input_method/mode_indicator_controller.h"
-#include "chrome/browser/chromeos/input_method/mode_indicator_widget.h"
 #include "ui/views/widget/widget.h"
 
 
@@ -84,7 +83,7 @@ void CandidateWindowControllerImpl::CreateView() {
 
   // Create the mode indicator controller.
   mode_indicator_controller_.reset(
-      new ModeIndicatorController(new ModeIndicatorWidget));
+      new ModeIndicatorController(InputMethodManager::Get()));
 }
 
 void CandidateWindowControllerImpl::Hide() {
