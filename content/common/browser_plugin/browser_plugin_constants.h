b605fab4b146e3948bdb9fd219377e7f97f183c6
fsamuel@chromium.org <fsamuel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-03 18:09:19 +0000

    <webview>: Expose transparency API
    BUG=157628
    
    Review URL: https://codereview.chromium.org/95693002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238428 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/browser_plugin/browser_plugin_constants.h
+++ content/common/browser_plugin/browser_plugin_constants.h
@@ -32,6 +32,7 @@ extern const char kEventInternalInstanceIDAllocated[];
 extern const char kEventInternalTrackedObjectGone[];
 
 // Attributes.
+extern const char kAttributeAllowTransparency[];
 extern const char kAttributeApi[];
 extern const char kAttributeAutoSize[];
 extern const char kAttributeContentWindow[];
