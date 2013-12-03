c75fe177b9747906da74f29c804ed8ec4b560ace
pneubeck@chromium.org <pneubeck@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-03 09:56:07 +0000

    Move DBusClient stub implementations into separate files.
    This allows to create these stubs directly without routing through the static Create() function in the client's interface.
    
    BUG=275286
    R=satorux@chromium.org
    
    Review URL: https://codereview.chromium.org/91373004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238329 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/dbus/fake_cros_disks_client.cc
+++ chromeos/dbus/fake_cros_disks_client.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/fake_cros_disks_client.h"
 
 #include "base/bind.h"
+#include "base/logging.h"
 #include "base/message_loop/message_loop.h"
 
 namespace chromeos {
--- chromeos/dbus/introspectable_client.h
+++ chromeos/dbus/introspectable_client.h
@@ -46,7 +46,7 @@ class CHROMEOS_EXPORT IntrospectableClient : public DBusClient {
       const std::string& xml_data);
 
   // Creates the instance
-  static IntrospectableClient* Create(DBusClientImplementationType type);
+  static IntrospectableClient* Create();
 
  protected:
   IntrospectableClient();
