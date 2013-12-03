4171d4306cbff7d8ff3ffe1629ce2fa8ffca5445
rlarocque@chromium.org <rlarocque@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-03 11:28:49 +0000

    Clean up TestProfileSyncService and related tests
    This CL refactors many of the tests in profile_sync_service_unittest.cc.
    It continues the refactoring work begun in r233533, r235661, and
    r235854.
    
    The JsController tests have been deleted.  There is not much point in
    testing the JsController here; it can be more easily tested on its own
    in sync_js_controller_uniittest.cc.  The SyncJsController unit tests
    have been updated so they now cover the few cases that were formerly
    only exercised in the ProfileSyncService unit tests.
    
    It converts all remaining uncoverted tests from relying on the
    TestProfileSyncService to using a real ProfileSyncService with an
    injected backend.  The injected backend makes it easier to create the
    scenarios we want to test.  We can inject a specially crafted SBH rather
    than fiddling with "synchronous init" and "fail initial download" flags.
    
    Since the TestProfileSyncService no longer needs to support the wide
    variety of test scenarios required by the tests in
    profile_sync_service_unittest.cc, we can greatly simplify its
    implementation.  Many of its parameters and associated code have been
    removed.
    
    BUG=140354,312994
    
    Review URL: https://codereview.chromium.org/67683005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238348 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/profile_sync_service_session_unittest.cc
+++ chrome/browser/sync/profile_sync_service_session_unittest.cc
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/invalidation/invalidation_service_factory.h"
 #include "chrome/browser/sessions/session_tab_helper.h"
+#include "chrome/browser/signin/profile_oauth2_token_service.h"
+#include "chrome/browser/signin/profile_oauth2_token_service_factory.h"
 #include "chrome/browser/signin/signin_manager.h"
 #include "chrome/browser/signin/signin_manager_factory.h"
 #include "chrome/browser/sync/abstract_profile_sync_service_test.h"
@@ -85,14 +87,12 @@ class FakeProfileSyncService : public TestProfileSyncService {
       Profile* profile,
       SigninManagerBase* signin,
       ProfileOAuth2TokenService* oauth2_token_service,
-      ProfileSyncService::StartBehavior behavior,
-      bool synchronous_backend_initialization)
+      ProfileSyncService::StartBehavior behavior)
       : TestProfileSyncService(factory,
                                profile,
                                signin,
                                oauth2_token_service,
-                               behavior,
-                               synchronous_backend_initialization) {}
+                               behavior) {}
   virtual ~FakeProfileSyncService() {}
 
   virtual scoped_ptr<DeviceInfo> GetLocalDeviceInfo() const OVERRIDE {
@@ -199,8 +199,7 @@ class ProfileSyncServiceSessionTest
         profile(),
         signin,
         oauth2_token_service,
-        ProfileSyncService::AUTO_START,
-        false));
+        ProfileSyncService::AUTO_START));
     sync_service_->set_backend_init_callback(callback);
 
     // Register the session data type.
