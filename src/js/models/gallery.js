b8222da20c123352b49472499b2e803e118d3d8f
Tomasz Mikolajewski <mtomasz@chromium.org>
2013-12-03 11:20:38 +0900

    Fix exporting images.
    This patch fixes a regression in saving files. The file name was being passed
    instead of the Entry. Along the way, error handling has been fixed.
    
    BUG=chromium:325022
    TEST=Tested manually by trying to save a file.
    
    Change-Id: I3345f93508c99980811da371b404d73b8f1a1759
    Reviewed-on: https://chromium-review.googlesource.com/178494
    Reviewed-by: Daichi Hirono <hirono@chromium.org>
    Tested-by: Tomasz Mikolajewski <mtomasz@chromium.org>

--- src/js/models/gallery.js
+++ src/js/models/gallery.js
@@ -361,8 +361,8 @@ camera.models.Gallery.prototype.exportPicture = function(
     picture, fileEntry, onSuccess, onFailure) {
   var onDataLoaded = function(data) {
     fileEntry.createWriter(function(fileWriter) {
-      fileWriter.onwrite = function() {};
-      fileWriter.onerror = onError;
+      fileWriter.onwriteend = onSuccess;
+      fileWriter.onerror = onFailure;
       // Blob has to be a Blob instance to be saved.
       var array = new Uint8Array(data.length);
       for (var index = 0; index < data.length; index++) {
@@ -370,7 +370,6 @@ camera.models.Gallery.prototype.exportPicture = function(
       }
       var blob = new Blob([array], {type: 'image/jpeg'});
       fileWriter.write(blob);
-      onSuccess();
     }.bind(this),
     onFailure);
   }.bind(this);
@@ -382,7 +381,7 @@ camera.models.Gallery.prototype.exportPicture = function(
     };
     reader.onerror = onFailure;
     reader.readAsBinaryString(file);
-  }.bind(this));
+  }.bind(this), onFailure);
 };
 
 /**
