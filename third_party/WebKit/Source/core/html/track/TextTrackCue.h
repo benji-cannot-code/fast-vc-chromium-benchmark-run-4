f41030aebbaeb5592812a2e090105d0bcf951ad5
philipj@opera.com <philipj@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-03 21:31:06 +0000

    Remove TextTrackCue::isVTTCue
    It is unused, as virtual functions are used instead of casting.
    
    BUG=270340
    
    Review URL: https://codereview.chromium.org/102003002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163113 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/track/TextTrackCue.h
+++ third_party/WebKit/Source/core/html/track/TextTrackCue.h
@@ -109,8 +109,6 @@ public:
     DEFINE_ATTRIBUTE_EVENT_LISTENER(enter);
     DEFINE_ATTRIBUTE_EVENT_LISTENER(exit);
 
-    virtual bool isVTTCue() const { return false; }
-
     virtual String toString() const;
 
 protected:
