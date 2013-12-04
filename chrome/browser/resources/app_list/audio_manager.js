df2c3f86750f8195b0fb76e1b03bdda39b752dff
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-04 20:33:47 +0000

    Removes the dependency between AudioManager and PluginManager.
    Now AudioManager can listen the audio without hotword recognition.
    The mean volume level would be useful to the speech UI.
    
    BUG=313904
    R=xiyuan@chromium.org
    TEST=manually
    
    Review URL: https://codereview.chromium.org/92863004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238753 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/app_list/audio_manager.js
+++ chrome/browser/resources/app_list/audio_manager.js
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 /**
- * @fileoverview The manager of audio streams and interaction with the plugin.
+ * @fileoverview The manager of audio streams.
  */
 
 cr.define('speech', function() {
@@ -16,38 +16,21 @@ cr.define('speech', function() {
    * @enum {number}
    */
   var AudioState = {
-    UNINITIALIZED: 0,
-    READY: 1,
-    RECOGNIZING: 2
+    STOPPED: 0,
+    LISTENING: 1
   };
 
   /**
    * @constructor
+   * @extends {cr.EventTarget}
    */
-  function AudioManager(onReady, onRecognizing, onRecognized) {
-    this.state = AudioState.UNINITIALIZED;
-    if (!speech.isPluginAvailable())
-      return;
-    this.onReady_ = onReady;
-    this.onRecognizing_ = onRecognizing;
-    this.pluginManager_ = new speech.PluginManager(
-        this.onPluginReady_.bind(this), onRecognized);
+  function AudioManager() {
     this.audioContext_ = new window.webkitAudioContext();
     this.audioProc_ = null;
-    this.pluginManager_.scheduleInitialize(
-        this.audioContext_.sampleRate,
-        'chrome://app-list/okgoogle_hotword.config');
+    this.state = AudioState.STOPPED;
   };
 
-  /**
-   * Called when the plugin is ready.
-   *
-   * @private
-   */
-  AudioManager.prototype.onPluginReady_ = function() {
-    this.state = AudioState.READY;
-    this.onReady_();
-  };
+  AudioManager.prototype.__proto__ = cr.EventTarget.prototype;
 
   /**
    * Called when the audio data arrives.
@@ -60,7 +43,9 @@ cr.define('speech', function() {
     var intData = new Int16Array(data.length);
     for (var i = 0; i < data.length; ++i)
       intData[i] = Math.round(data[i] * 32767);
-    this.pluginManager_.sendAudioData(intData.buffer);
+    var event = new Event('audio');
+    event.data = intData;
+    this.dispatchEvent(event);
   };
 
   /**
@@ -77,27 +62,27 @@ cr.define('speech', function() {
 
     audioIn.connect(this.audioProc_);
     this.audioProc_.connect(this.audioContext_.destination);
-    this.state = AudioState.RECOGNIZING;
-    this.onRecognizing_();
+    this.state = AudioState.LISTENING;
   };
 
   /**
-   * Starts the audio recognition with the plugin.
+   * Returns the sampling rate of the current audio context.
+   * @return {number} The sampling rate.
+   */
+  AudioManager.prototype.getSampleRate = function() {
+    return this.audioContext_.sampleRate;
+  };
+
+  /**
+   * Starts the audio processing.
    */
   AudioManager.prototype.start = function() {
-    // Not yet initialized.
-    if (this.state != AudioState.READY)
+    if (this.state == AudioState.LISTENING)
       return;
-    if (this.pluginManager_.state < speech.PluginState.READY)
-      return;
-
-    if (this.pluginManager_.state == speech.PluginState.READY)
-      this.pluginManager_.startRecognizer();
 
     if (this.audioProc_) {
       this.audioProc_.connect(this.audioContext_.destination);
-      this.state = AudioState.RECOGNIZING;
-      this.onRecognizing_();
+      this.state = AudioState.LISTENING;
       return;
     }
 
@@ -108,17 +93,17 @@ cr.define('speech', function() {
   };
 
   /**
-   * Stops the audio recognition.
+   * Stops the audio processing.
    */
   AudioManager.prototype.stop = function() {
-    if (this.state <= AudioState.READY)
+    if (this.state != AudioState.LISTENING)
       return;
     this.audioProc_.disconnect();
-    this.pluginManager_.stopRecognizer();
-    this.state = AudioState.READY;
+    this.state = AudioState.STOPPED;
   };
 
   return {
-    AudioManager: AudioManager
+    AudioManager: AudioManager,
+    AudioState: AudioState
   };
 });
