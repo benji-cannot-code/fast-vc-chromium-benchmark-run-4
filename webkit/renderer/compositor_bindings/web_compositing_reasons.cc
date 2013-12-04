38c59a688e69ca636ee11536d0f828a8ea2779f0
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-04 19:13:23 +0000

    Revert of https://codereview.chromium.org/66263003/ Reason for revert: Depends on blink roll that requires revert
    TBR=enne@chromium.org,rosca@adobe.com
    NOTREECHECKS=true
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/105333002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238733 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/renderer/compositor_bindings/web_compositing_reasons.cc
+++ webkit/renderer/compositor_bindings/web_compositing_reasons.cc
@@ -146,7 +146,3 @@ COMPILE_ASSERT_MATCHING_UINT64(
 COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonOutOfFlowClipping,
     blink::CompositingReasonOutOfFlowClipping);
-
-COMPILE_ASSERT_MATCHING_UINT64(
-    cc::kCompositingReasonIsolateCompositedDescendants,
-    blink::CompositingReasonIsolateCompositedDescendants);
