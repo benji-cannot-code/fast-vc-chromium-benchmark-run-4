6563492b6a8a743aa3172a0fbce05512756c1394
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-04 08:31:08 +0000

    Fix the Views hung renderer dialog placement.
    The Aura-specific code was old and incorrect (at least on Windows).
    The dialog should be initially placed like content/browser modal dialogs.
    See before/after pictures at http://crbug.com/325323
    
    BUG=325323
    TEST=about:hang shows the hung renderer dialog in a reasonable place (similar to the edit bookmark dialog, etc.).
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/86683002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238634 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/hung_renderer_view.cc
+++ chrome/browser/ui/views/hung_renderer_view.cc
@@ -15,10 +15,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/favicon/favicon_tab_helper.h"
 #include "chrome/browser/platform_util.h"
 #include "chrome/browser/ui/browser_dialogs.h"
+#include "chrome/browser/ui/browser_finder.h"
+#include "chrome/browser/ui/chrome_web_modal_dialog_manager_delegate.h"
 #include "chrome/browser/ui/tab_contents/core_tab_helper.h"
 #include "chrome/browser/ui/tab_contents/tab_contents_iterator.h"
+#include "chrome/browser/ui/views/constrained_window_views.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/logging_chrome.h"
+#include "components/web_modal/web_contents_modal_dialog_host.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/web_contents.h"
@@ -178,10 +182,6 @@ void HungPagesTableModel::WebContentsObserverImpl::WebContentsDestroyed(
 // static
 gfx::ImageSkia* HungRendererDialogView::frozen_icon_ = NULL;
 
-// The distance in pixels from the top of the relevant contents to place the
-// warning window.
-static const int kOverlayContentsOffsetY = 50;
-
 // The dimensions of the hung pages list table view, in pixels.
 static const int kTableViewWidth = 300;
 static const int kTableViewHeight = 100;
@@ -247,14 +247,18 @@ void HungRendererDialogView::ShowForWebContents(WebContents* contents) {
     return;
 
   if (!GetWidget()->IsActive()) {
-    gfx::Rect bounds = GetDisplayBounds(contents);
+    // Place the dialog over content's browser window, similar to modal dialogs.
+    Browser* browser = chrome::FindBrowserWithWebContents(contents);
+    if (browser) {
+      ChromeWebModalDialogManagerDelegate* manager = browser;
+      UpdateBrowserModalDialogPosition(
+          GetWidget(), manager->GetWebContentsModalDialogHost());
+    }
 
     gfx::NativeView frame_view =
         platform_util::GetTopLevel(contents->GetView()->GetNativeView());
-
     views::Widget* insert_after =
         views::Widget::GetWidgetForNativeView(frame_view);
-    GetWidget()->SetBoundsConstrained(bounds);
     if (insert_after)
       GetWidget()->StackAboveWidget(insert_after);
 
@@ -420,26 +424,6 @@ void HungRendererDialogView::Init() {
                   views::GridLayout::FILL, kTableViewWidth, kTableViewHeight);
 
   initialized_ = true;
-}
-
-gfx::Rect HungRendererDialogView::GetDisplayBounds(
-    WebContents* contents) {
-#if defined(USE_AURA)
-  gfx::Rect contents_bounds(
-      contents->GetView()->GetNativeView()->GetBoundsInRootWindow());
-#elif defined(OS_WIN)
-  HWND contents_hwnd = contents->GetView()->GetNativeView();
-  RECT contents_bounds_rect;
-  GetWindowRect(contents_hwnd, &contents_bounds_rect);
-  gfx::Rect contents_bounds(contents_bounds_rect);
-#endif
-  gfx::Rect window_bounds = GetWidget()->GetWindowBoundsInScreen();
-
-  int window_x = contents_bounds.x() +
-      (contents_bounds.width() - window_bounds.width()) / 2;
-  int window_y = contents_bounds.y() + kOverlayContentsOffsetY;
-  return gfx::Rect(window_x, window_y, window_bounds.width(),
-                   window_bounds.height());
 }
 
 // static
