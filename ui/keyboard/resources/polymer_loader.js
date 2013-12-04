4862ac1b9a80bf7dc1d2a8420e11cf1f54438815
bshe@chromium.org <bshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-04 23:51:33 +0000

    Uprev polymer library to 2013-11-26
    R=kevers
    BUG=NONE
    
    Review URL: https://codereview.chromium.org/105403002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238820 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ ui/keyboard/resources/polymer_loader.js
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This is necessary because polymer currently doesn't handle the configuration
+// where native Custom Elements are available but native Shadow DOM is not.
+// TODO(bshe): remove this line when polymer supports the configuration.
+document.register = undefined;
+
+<include src="../../../third_party/polymer/platform.js"></include>
+<include src="../../../third_party/polymer/polymer.js"></include>
