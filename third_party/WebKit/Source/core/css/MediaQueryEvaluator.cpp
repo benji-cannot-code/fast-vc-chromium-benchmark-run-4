f3644748b23e232628aa5419bc4c893b405751e4
kenneth.r.christiansen@intel.com <kenneth.r.christiansen@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-04 15:42:25 +0000

    Update some of the MQ comments, add issue link
    Simple updates to the comments refering to issue and spec.
    
    At the same time, fix the style of multiple comments.
    
    Review URL: https://codereview.chromium.org/98763006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163177 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/MediaQueryEvaluator.cpp
+++ third_party/WebKit/Source/core/css/MediaQueryEvaluator.cpp
@@ -90,7 +90,7 @@ MediaQueryEvaluator::MediaQueryEvaluator(const AtomicString& acceptedMediaType, 
     : m_mediaType(acceptedMediaType)
     , m_frame(frame)
     , m_style(style)
-    , m_expResult(false) // doesn't matter when we have m_frame and m_style
+    , m_expResult(false) // Doesn't matter when we have m_frame and m_style.
 {
 }
 
@@ -126,7 +126,7 @@ bool MediaQueryEvaluator::eval(const MediaQuerySet* querySet, MediaQueryResultLi
 
     const Vector<OwnPtr<MediaQuery> >& queries = querySet->queryVector();
     if (!queries.size())
-        return true; // empty query list evaluates to true
+        return true; // Empty query list evaluates to true.
 
     // Iterate over queries, stop if any of them eval to true (OR semantics).
     bool result = false;
@@ -205,6 +205,7 @@ static bool colorIndexMediaFeatureEval(CSSValue* value, RenderStyle*, Frame*, Me
     if (!value)
         return false;
 
+    // Acording to spec, if the device does not use a color lookup table, the value is zero.
     float number;
     return numberValue(value, number) && compareValue(0, static_cast<int>(number), op);
 }
@@ -251,7 +252,7 @@ static bool aspectRatioMediaFeatureEval(CSSValue* value, RenderStyle*, Frame* fr
     }
 
     // ({,min-,max-}aspect-ratio)
-    // assume if we have a device, its aspect ratio is non-zero
+    // assume if we have a device, its aspect ratio is non-zero.
     return true;
 }
 
@@ -263,13 +264,14 @@ static bool deviceAspectRatioMediaFeatureEval(CSSValue* value, RenderStyle*, Fra
     }
 
     // ({,min-,max-}device-aspect-ratio)
-    // assume if we have a device, its aspect ratio is non-zero
+    // assume if we have a device, its aspect ratio is non-zero.
     return true;
 }
 
 static bool evalResolution(CSSValue* value, Frame* frame, MediaFeaturePrefix op)
 {
-    // FIXME: Possibly handle other media types than 'screen' and 'print'.
+    // According to MQ4, only 'screen', 'print' and 'speech' may match.
+    // FIXME: What should speech match? https://www.w3.org/Style/CSS/Tracker/issues/348
     float actualResolution = 0;
 
     // This checks the actual media type applied to the document, and we know
@@ -347,8 +349,8 @@ static bool computeLength(CSSValue* value, bool strict, RenderStyle* initialStyl
     }
 
     if (primitiveValue->isLength()) {
-        // Relative (like EM) and root relative (like REM) units are always resolved against the initial values
-        // for media queries, hence the two initialStyle parameters.
+        // Relative (like EM) and root relative (like REM) units are always resolved against
+        // the initial values for media queries, hence the two initialStyle parameters.
         result = primitiveValue->computeLength<int>(CSSToLengthConversionData(initialStyle, initialStyle, 1.0 /* zoom */, true /* computingFontSize */));
         return true;
     }
@@ -418,7 +420,7 @@ static bool widthMediaFeatureEval(CSSValue* value, RenderStyle* style, Frame* fr
     return width;
 }
 
-// rest of the functions are trampolines which set the prefix according to the media feature expression used
+// Rest of the functions are trampolines which set the prefix according to the media feature expression used.
 
 static bool minColorMediaFeatureEval(CSSValue* value, RenderStyle* style, Frame* frame, MediaFeaturePrefix)
 {
@@ -649,7 +651,7 @@ static bool pointerMediaFeatureEval(CSSValue* value, RenderStyle*, Frame* frame,
 
 static bool scanMediaFeatureEval(CSSValue* value, RenderStyle*, Frame* frame, MediaFeaturePrefix)
 {
-    // Scan only applies to tv media.
+    // Scan only applies to 'tv' media.
     if (!equalIgnoringCase(frame->view()->mediaType(), "tv"))
         return false;
 
@@ -683,9 +685,8 @@ bool MediaQueryEvaluator::eval(const MediaQueryExp* expr) const
     if (!gFunctionMap)
         createFunctionMap();
 
-    // call the media feature evaluation function. Assume no prefix
-    // and let trampoline functions override the prefix if prefix is
-    // used
+    // Call the media feature evaluation function. Assume no prefix and let
+    // trampoline functions override the prefix if prefix is used.
     EvalFunc func = gFunctionMap->get(expr->mediaFeature().impl());
     if (func)
         return func(expr->value(), m_style.get(), m_frame, NoPrefix);
