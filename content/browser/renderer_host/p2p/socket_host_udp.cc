5f4a2fe20923da29b7895f30771ada88e41a9cbe
mallinath@chromium.org <mallinath@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-04 20:46:52 +0000

    Sockets used for p2p in chrome browser process is left with default socket send/recveive buffer sizes. This is good enough for POSIX platforms. But on Windows the default value is much lower.
    This CL is trying to change the default send/recv buffer size to 128 and 64K
    after socket is created.
    
    R=sergeyu@chromium.org
    
    Review URL: https://codereview.chromium.org/104683003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238755 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/p2p/socket_host_udp.cc
+++ content/browser/renderer_host/p2p/socket_host_udp.cc
@@ -19,6 +19,8 @@ namespace {
 
 // UDP packets cannot be bigger than 64k.
 const int kReadBufferSize = 65536;
+// Socket receive buffer size.
+const int kRecvSocketBufferSize = 65536;  // 64K
 
 // Defines set of transient errors. These errors are ignored when we get them
 // from sendto() or recvfrom() calls.
@@ -86,6 +88,12 @@ bool P2PSocketHostUdp::Init(const net::IPEndPoint& local_address,
     LOG(ERROR) << "bind() failed: " << result;
     OnError();
     return false;
+  }
+
+  // Setting recv socket buffer size.
+  if (!socket_->SetReceiveBufferSize(kRecvSocketBufferSize)) {
+    LOG(WARNING) << "Failed to set socket receive buffer size to "
+                 << kRecvSocketBufferSize;
   }
 
   net::IPEndPoint address;
