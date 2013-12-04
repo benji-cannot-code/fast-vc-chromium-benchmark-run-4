9f89c1507683c5d141eab9363222dacedd95e610
rmsousa@chromium.org <rmsousa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-04 08:32:54 +0000

    Separate ConfigWatcher interface from ConfigFileWatcher implementation.
    BUG=
    
    Review URL: https://codereview.chromium.org/92693005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238636 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/daemon_process.cc
+++ remoting/host/daemon_process.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "remoting/host/daemon_process.h"
 
+#include <algorithm>
+#include <string>
+
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/command_line.h"
@@ -14,8 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/single_thread_task_runner.h"
 #include "net/base/net_util.h"
 #include "remoting/base/auto_thread_task_runner.h"
+#include "remoting/base/url_request_context.h"
 #include "remoting/host/branding.h"
 #include "remoting/host/chromoting_messages.h"
+#include "remoting/host/config_file_watcher.h"
 #include "remoting/host/desktop_session.h"
 #include "remoting/host/host_event_logger.h"
 #include "remoting/host/host_status_observer.h"
@@ -255,20 +260,17 @@ void DaemonProcess::CrashNetworkProcess(
 void DaemonProcess::Initialize() {
   DCHECK(caller_task_runner()->BelongsToCurrentThread());
 
+  const CommandLine* command_line = CommandLine::ForCurrentProcess();
   // Get the name of the host configuration file.
   base::FilePath default_config_dir = remoting::GetConfigDir();
-  base::FilePath config_path = default_config_dir.Append(kDefaultHostConfigFile);
-  const CommandLine* command_line = CommandLine::ForCurrentProcess();
+  base::FilePath config_path = default_config_dir.Append(
+      kDefaultHostConfigFile);
   if (command_line->HasSwitch(kHostConfigSwitchName)) {
     config_path = command_line->GetSwitchValuePath(kHostConfigSwitchName);
   }
-
-  // Start watching the host configuration file.
-  config_watcher_.reset(new ConfigFileWatcher(caller_task_runner(),
-                                              io_task_runner(),
-                                              this));
-  config_watcher_->Watch(config_path);
-
+  config_watcher_.reset(new ConfigFileWatcher(
+      caller_task_runner(), io_task_runner(), config_path));
+  config_watcher_->Watch(this);
   host_event_logger_ =
       HostEventLogger::Create(weak_factory_.GetWeakPtr(), kApplicationName);
 
