149c2902d24f41d637999efd97d7fd2b9e9e9ccc
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-04 03:48:19 +0000

    Build fix attempt: add include file to windowed_install_dialog_controller_browsertest.mm
    BUG=none
    TBR=tapted
    
    Review URL: https://codereview.chromium.org/103643002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238551 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/extensions/windowed_install_dialog_controller_browsertest.mm
+++ chrome/browser/ui/cocoa/extensions/windowed_install_dialog_controller_browsertest.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/ui/cocoa/extensions/windowed_install_dialog_controller.h"
 
 #include "base/run_loop.h"
+#include "base/threading/sequenced_worker_pool.h"
 #include "chrome/browser/ui/browser.h"
 #import "chrome/browser/ui/cocoa/extensions/extension_install_prompt_test_utils.h"
 #import "chrome/browser/ui/cocoa/extensions/extension_install_view_controller.h"
