6352898e60da0b2c0e9ed402875a26ae02f57c4a
kevers@chromium.org <kevers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-04 17:36:50 +0000

    Add code field speced for DOM KeyEvent into virtual key events from the soft-keyboard. BUG=324899
    Review URL: https://codereview.chromium.org/99973002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238716 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/keyboard/keyboard_util.cc
+++ ui/keyboard/keyboard_util.cc
@@ -111,6 +111,7 @@ bool MoveCursor(int swipe_direction,
 bool SendKeyEvent(const std::string type,
                   int key_value,
                   int key_code,
+                  std::string key_name,
                   int modifiers,
                   aura::WindowEventDispatcher* dispatcher) {
   ui::EventType event_type = ui::ET_UNKNOWN;
@@ -154,7 +155,7 @@ bool SendKeyEvent(const std::string type,
       }
     }
 
-    ui::KeyEvent event(event_type, code, modifiers, false);
+    ui::KeyEvent event(event_type, code, key_name, modifiers, false);
     dispatcher->AsRootWindowHostDelegate()->OnHostKeyEvent(&event);
   }
   return true;
