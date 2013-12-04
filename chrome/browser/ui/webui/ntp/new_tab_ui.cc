c245786eef0e1d4f0881a1ba558b3ed10f5cc25b
beaudoin@chromium.org <beaudoin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-04 22:15:23 +0000

    Allow empty iframe tiles in the NTP and log them to UMA.
    These empty iframes allow controlling the visual of the tiles externally, from the page itself, rather than within the iframe.
    
    BUG=323186
    
    Review URL: https://codereview.chromium.org/85953003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238795 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/ntp/new_tab_ui.cc
+++ chrome/browser/ui/webui/ntp/new_tab_ui.cc
@@ -222,7 +222,7 @@ void NewTabUI::RenderViewReused(RenderViewHost* render_view_host) {
 }
 
 void NewTabUI::WasHidden() {
-  EmitMouseoverCount();
+  EmitNtpStatistics();
 }
 
 void NewTabUI::Observe(int type,
@@ -238,9 +238,9 @@ void NewTabUI::Observe(int type,
   }
 }
 
-void NewTabUI::EmitMouseoverCount() {
+void NewTabUI::EmitNtpStatistics() {
   NTPUserDataLogger::GetOrCreateFromWebContents(
-      web_contents())->EmitMouseoverCount();
+      web_contents())->EmitNtpStatistics();
 }
 
 void NewTabUI::OnShowBookmarkBarChanged() {
