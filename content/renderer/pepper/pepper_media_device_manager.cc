31c48148e098c4bc6efee380db0ab54bdfa37558
perkj@chromium.org <perkj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-04 06:03:08 +0000

    Fix build when build flag enable_webrtc is disabled. This fixes a build regression introduced by https://codereview.chromium.org/72603004/ where the EnumerationFailed callback was removed.
    TEST= change common.gypi 'enable_webrtc%': 0, and compile.
    
    Review URL: https://codereview.chromium.org/102393002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238593 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/pepper/pepper_media_device_manager.cc
+++ content/renderer/pepper/pepper_media_device_manager.cc
@@ -60,9 +60,10 @@ int PepperMediaDeviceManager::EnumerateDevices(
   base::MessageLoop::current()->PostTask(
       FROM_HERE,
       base::Bind(
-          &PepperMediaDeviceManager::OnDevicesEnumerationFailed,
+          &PepperMediaDeviceManager::OnDevicesEnumerated,
           AsWeakPtr(),
-          request_id));
+          request_id,
+          StreamDeviceInfoArray()));
 #endif
 
   return request_id;
