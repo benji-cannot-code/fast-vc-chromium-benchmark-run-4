a67075b766119f0c7aa45a5c0b7023a4c8458cc0
nbarth@chromium.org <nbarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-04 10:18:28 +0000

    IDL compiler: remove [MasqueradesAsUndefined]
    This removes the [MasqueradesAsUndefined] extended attribute, instead
    including a special case for the one interface that uses it.
    (HTMLAllCollection, for document.all)
    
    Response to request of haraken@:
    https://codereview.chromium.org/103753002/#msg2
    
    * No changes to generated code.
    * Removed from IDLExtendedAttributes.txt
    * Docs updated to remove mention.
    https://sites.google.com/a/chromium.org/dev/blink/webidl/blink-idl-extended-attributes
    
    BUG=239771
    
    Review URL: https://codereview.chromium.org/103813002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163163 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/scripts/code_generator_v8.pm
+++ third_party/WebKit/Source/bindings/scripts/code_generator_v8.pm
@@ -4163,12 +4163,13 @@ sub GenerateImplementationLegacyCall
     return $code;
 }
 
-sub GenerateImplementationMasqueradesAsUndefined
+sub GenerateImplementationMarkAsUndetectable
 {
     my $interface = shift;
     my $code = "";
 
-    if ($interface->extendedAttributes->{"MasqueradesAsUndefined"})
+    # Needed for legacy support of document.all
+    if ($interface->name eq "HTMLAllCollection")
     {
         $code .= "    functionTemplate->InstanceTemplate()->MarkAsUndetectable();\n";
     }
@@ -4610,7 +4611,7 @@ END
     $code .= GenerateImplementationIndexedPropertyAccessors($interface);
     $code .= GenerateImplementationNamedPropertyAccessors($interface);
     $code .= GenerateImplementationLegacyCall($interface);
-    $code .= GenerateImplementationMasqueradesAsUndefined($interface);
+    $code .= GenerateImplementationMarkAsUndetectable($interface);
 
     # Define operations.
     my $total_functions = 0;
