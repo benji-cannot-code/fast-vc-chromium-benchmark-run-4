eb736efb35ef72e6a90f4a6e688294531e15c431
seokju.kwon@gmail.com <seokju.kwon@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-04 12:39:38 +0000

    Remove inspectedPage() and inspectorClient() from InspectorController.
    These public methods are never called, so remove them.
    
    BUG=none.
    
    Review URL: https://codereview.chromium.org/103303002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163169 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorController.h
+++ third_party/WebKit/Source/core/inspector/InspectorController.h
@@ -75,8 +75,6 @@ public:
     static PassOwnPtr<InspectorController> create(Page*, InspectorClient*);
     void inspectedPageDestroyed();
 
-    Page* inspectedPage() const;
-
     void setInspectorFrontendClient(PassOwnPtr<InspectorFrontendClient>);
     void didClearWindowObjectInWorld(Frame*, DOMWrapperWorld*);
     void setInjectedScriptForOrigin(const String& origin, const String& source);
@@ -111,8 +109,6 @@ public:
     void resume();
 
     void setResourcesDataSizeLimitsFromInternals(int maximumResourcesContentSize, int maximumSingleResourceContentSize);
-
-    InspectorClient* inspectorClient() const { return m_inspectorClient; }
 
     void willProcessTask();
     void didProcessTask();
