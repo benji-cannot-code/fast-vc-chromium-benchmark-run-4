bd9a6964e57797802c1484e6bea335c991002a69
yhirano@chromium.org <yhirano@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-05 10:58:03 +0000

    Support SPDY3 on Flip Server.
    Flip Server now roughly supprots SPDY3 and SPDY4a2.
    Some control frames are currently not supported yet.
    
    BUG=267354
    R=rch@chromium.org
    
    Review URL: https://codereview.chromium.org/90623002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238930 0039d316-1c4b-4281-b951-d872f2087c98

--- net/tools/flip_server/sm_connection.cc
+++ net/tools/flip_server/sm_connection.cc
@@ -310,7 +310,8 @@ void SMConnection::HandleEvents() {
 }
 
 // Decide if SPDY was negotiated.
-bool SMConnection::WasSpdyNegotiated() {
+bool SMConnection::WasSpdyNegotiated(SpdyMajorVersion* version_negotiated) {
+  *version_negotiated = SPDY3;
   if (force_spdy())
     return true;
 
@@ -324,8 +325,20 @@ bool SMConnection::WasSpdyNegotiated() {
       VLOG(1) << log_prefix_ << ACCEPTOR_CLIENT_IDENT
               << "NPN protocol detected: " << npn_proto_str;
       if (!strncmp(reinterpret_cast<const char*>(npn_proto),
-                   "spdy/2", npn_proto_len))
+                   "spdy/2", npn_proto_len)) {
+        *version_negotiated = SPDY2;
         return true;
+      }
+      if (!strncmp(reinterpret_cast<const char*>(npn_proto),
+                   "spdy/3", npn_proto_len)) {
+        *version_negotiated = SPDY3;
+        return true;
+      }
+      if (!strncmp(reinterpret_cast<const char*>(npn_proto),
+                   "spdy/4a2", npn_proto_len)) {
+        *version_negotiated = SPDY4;
+        return true;
+      }
     }
   }
 
@@ -336,7 +349,8 @@ bool SMConnection::SetupProtocolInterfaces() {
   DCHECK(!protocol_detected_);
   protocol_detected_ = true;
 
-  bool spdy_negotiated = WasSpdyNegotiated();
+  SpdyMajorVersion version;
+  bool spdy_negotiated = WasSpdyNegotiated(&version);
   bool using_ssl = ssl_ != NULL;
 
   if (using_ssl)
@@ -395,7 +409,7 @@ bool SMConnection::SetupProtocolInterfaces() {
                                           epoll_server_,
                                           memory_cache_,
                                           acceptor_,
-                                          SPDY2);
+                                          version);
         sm_interface_ = sm_spdy_interface_;
       }
       break;
