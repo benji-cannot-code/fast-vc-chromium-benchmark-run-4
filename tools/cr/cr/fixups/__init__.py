871f49dc93f6c6c6f924fd1c62421eea39321549
iancottrell@chromium.org <iancottrell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-05 12:59:48 +0000

    [cr tool] Adding architecture fixups
    BUG=316397
    
    Review URL: https://codereview.chromium.org/102043002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238959 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/cr/cr/fixups/__init__.py
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""A package for all the version fixups.
+
+All the code in this package is there to fix up older output directories and
+clients to a form that works with the current version of cr.
+"""
