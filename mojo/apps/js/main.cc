6c46a85fd6b17082fb2ee1efa06855810b43a75c
abarth@chromium.org <abarth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-05 00:44:28 +0000

    Update mojo.js to use native_viewport
    This CL moves mojo.js from using hello_world_service to using native_viewport.
    Now mojo.js creates a viewport, but we're still not able to draw GL becaues we
    don't have WebGL bindings.
    
    R=aa@chromium.org
    BUG=324643
    
    Review URL: https://codereview.chromium.org/98423005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238834 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/apps/js/main.cc
+++ mojo/apps/js/main.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gin/public/isolate_holder.h"
 #include "mojo/apps/js/mojo_runner_delegate.h"
 #include "mojo/common/bindings_support_impl.h"
+#include "mojo/public/gles2/gles2.h"
 #include "mojo/public/system/core_cpp.h"
 #include "mojo/public/system/macros.h"
 
@@ -40,9 +41,11 @@ void Start(MojoHandle pipe, const std::string& module) {
 extern "C" MOJO_APPS_JS_EXPORT MojoResult CDECL MojoMain(MojoHandle pipe) {
   mojo::common::BindingsSupportImpl bindings_support;
   mojo::BindingsSupport::Set(&bindings_support);
+  MojoGLES2Initialize();
 
   mojo::apps::Start(pipe, "mojo/apps/js/main");
 
+  MojoGLES2Terminate();
   mojo::BindingsSupport::Set(NULL);
   return MOJO_RESULT_OK;
 }
--- mojo/mojo_apps.gypi
+++ mojo/mojo_apps.gypi
@@ -7,15 +7,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'dependencies': [
         '../base/base.gyp:base',
         '../gin/gin.gyp:gin',
-        'hello_world_service',
         'mojo_common_lib',
+        'mojo_gles2',
+        'mojo_gles2_bindings',
+        'mojo_native_viewport_bindings',
         'mojo_system',
       ],
       'export_dependent_settings': [
         '../base/base.gyp:base',
         '../gin/gin.gyp:gin',
-        'hello_world_service',
         'mojo_common_lib',
+        'mojo_gles2',
+        'mojo_gles2_bindings',
+        'mojo_native_viewport_bindings',
         'mojo_system',
       ],
       'sources': [
