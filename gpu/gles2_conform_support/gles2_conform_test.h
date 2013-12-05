6f77459fea4bfe975255c4456c180f511112714b
zmo@chromium.org <zmo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-05 21:41:09 +0000

    Move src/gpu/gles2_conform_test out of src-internal
    We directly check them into src/gpu/gles2_conform_support
    
    BUG=325536
    TEST=gpu builders
    R=kbr@chromium.org, piman@chromium.org
    
    Review URL: https://codereview.chromium.org/105733002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239037 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ gpu/gles2_conform_support/gles2_conform_test.h
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef GPU_GLES2_CONFORM_SUPPORT_GLES2_CONFORM_TEST_H
+#define GPU_GLES2_CONFORM_SUPPORT_GLES2_CONFORM_TEST_H
+
+bool RunGLES2ConformTest(const char* path);
+
+#endif  // GPU_GLES2_CONFORM_SUPPORT_GLES2_CONFORM_TEST_H
+
