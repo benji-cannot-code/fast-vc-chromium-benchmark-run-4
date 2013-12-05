9284802e42ca4ce795e304e240177406f7476f70
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-05 11:33:06 +0000

    Timeline: make TimelinePresentationModel._linkifyScriptLocation() never return null
    _linkifyScriptLocation() could previously return null if the details were not specified.
    The result could be accidently misused, so make details mandatory and use _linkifyLocation()
    when there's no default details.
    
    R=apavlov@chromium.org
    
    Review URL: https://codereview.chromium.org/106183003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163252 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/TimelinePresentationModel.js
+++ third_party/WebKit/Source/devtools/front_end/TimelinePresentationModel.js
@@ -1158,8 +1158,9 @@ WebInspector.TimelinePresentationModel.Record.prototype = {
                 callSiteStackTraceLabel = WebInspector.UIString("Animation frame requested");
                 contentHelper.appendTextRow(WebInspector.UIString("Callback ID"), this.data["id"]);
                 break;
-            case recordTypes.FunctionCall:
-                contentHelper.appendElementRow(WebInspector.UIString("Location"), this._linkifyScriptLocation());
+        case recordTypes.FunctionCall:
+                if (this.scriptName)
+                    contentHelper.appendElementRow(WebInspector.UIString("Location"), this._linkifyLocation(this.scriptName, this.scriptLine, 0));
                 break;
             case recordTypes.ScheduleResourceRequest:
             case recordTypes.ResourceSendRequest:
@@ -1257,7 +1258,7 @@ WebInspector.TimelinePresentationModel.Record.prototype = {
             contentHelper.appendElementRow(relatedNodeLabel || WebInspector.UIString("Related node"), this._createNodeAnchor(this._relatedNode));
 
         if (this.scriptName && this.type !== recordTypes.FunctionCall)
-            contentHelper.appendElementRow(WebInspector.UIString("Function Call"), this._linkifyScriptLocation());
+            contentHelper.appendElementRow(WebInspector.UIString("Function Call"), this._linkifyLocation(this.scriptName, this.scriptLine, 0));
 
         if (this.usedHeapSize) {
             if (this.usedHeapSizeDelta) {
@@ -1345,7 +1346,8 @@ WebInspector.TimelinePresentationModel.Record.prototype = {
             details = this._linkifyScriptLocation(this.data["timerId"]);
             break;
         case WebInspector.TimelineModel.RecordType.FunctionCall:
-            details = this._linkifyScriptLocation();
+            if (this.scriptName)
+                details = this._linkifyLocation(this.scriptName, this.scriptLine, 0);
             break;
         case WebInspector.TimelineModel.RecordType.FireAnimationFrame:
             details = this._linkifyScriptLocation(this.data["id"]);
@@ -1390,7 +1392,7 @@ WebInspector.TimelinePresentationModel.Record.prototype = {
             details = this.data["message"];
             break;
         default:
-            details = this._linkifyScriptLocation() || this._linkifyTopCallFrame() || null;
+            details = this.scriptName ? this._linkifyLocation(this.scriptName, this.scriptLine, 0) : (this._linkifyTopCallFrame() || null);
             break;
         }
 
@@ -1437,15 +1439,12 @@ WebInspector.TimelinePresentationModel.Record.prototype = {
     },
 
     /**
-     * @param {*=} defaultValue
+     * @param {*} defaultValue
      * @return {Element|string}
      */
     _linkifyScriptLocation: function(defaultValue)
     {
-        if (this.scriptName)
-            return this._linkifyLocation(this.scriptName, this.scriptLine, 0);
-        else
-            return defaultValue ? "" + defaultValue : null;
+        return this.scriptName ? this._linkifyLocation(this.scriptName, this.scriptLine, 0) : "" + defaultValue;
     },
 
     calculateAggregatedStats: function()
