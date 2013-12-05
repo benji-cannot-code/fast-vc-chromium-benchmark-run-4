f8c1705341e93b38be108939490a4117c57ba37e
noamsml@chromium.org <noamsml@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-05 22:23:52 +0000

    For Privet printing, cache the capabilities and use them to generate the preview
    Use cached capabilities to generate the preview for privet printing until
    printer is found.
    
    BUG=325983
    
    Review URL: https://codereview.chromium.org/102693006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239052 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/print_preview/data/app_state.js
+++ chrome/browser/resources/print_preview/data/app_state.js
@@ -37,6 +37,7 @@ cr.define('print_preview', function() {
     VERSION: 'version',
     SELECTED_DESTINATION_ID: 'selectedDestinationId',
     SELECTED_DESTINATION_ORIGIN: 'selectedDestinationOrigin',
+    SELECTED_DESTINATION_CAPABILITIES: 'selectedDestinationCapabilities',
     IS_SELECTED_DESTINATION_LOCAL: 'isSelectedDestinationLocal',  // Deprecated
     IS_GCP_PROMO_DISMISSED: 'isGcpPromoDismissed',
     MARGINS_TYPE: 'marginsType',
@@ -75,6 +76,11 @@ cr.define('print_preview', function() {
     /** @return {?string} Origin of the selected destination. */
     get selectedDestinationOrigin() {
       return this.state_[AppState.Field.SELECTED_DESTINATION_ORIGIN];
+    },
+
+    /** @return {?print_preview.Cdd} CDD of the selected destination. */
+    get selectedDestinationCapabilities() {
+      return this.state_[AppState.Field.SELECTED_DESTINATION_CAPBILITIES];
     },
 
     /** @return {boolean} Whether the GCP promotion has been dismissed. */
@@ -162,6 +168,8 @@ cr.define('print_preview', function() {
         return;
       this.state_[AppState.Field.SELECTED_DESTINATION_ID] = dest.id;
       this.state_[AppState.Field.SELECTED_DESTINATION_ORIGIN] = dest.origin;
+      this.state_[AppState.Field.SELECTED_DESTINATION_CAPBILITIES] =
+          dest.capabilities;
       this.persist_();
     },
 
