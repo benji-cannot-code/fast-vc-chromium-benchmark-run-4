6f1773722888b45076eefe32ff64ed8efc25ac63
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-05 11:46:27 +0000

    [Files.app] Clean-up the code around checking file-extension
    - Fix inconsistent expressions of file-extension (with-dot or without-dot)
    - Use PathUtil methods instead of self-implementation
    
    BUG=none
    TEST=manually
    
    Review URL: https://codereview.chromium.org/101183002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238950 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/foreground/js/file_tasks.js
+++ chrome/browser/resources/file_manager/foreground/js/file_tasks.js
@@ -59,13 +59,18 @@ FileTasks.WEB_STORE_HANDLER_BASE_URL =
  * Returns URL of the Chrome Web Store which show apps supporting the given
  * file-extension and mime-type.
  *
- * @param {string} extension Extension of the file.
+ * @param {string} extension Extension of the file (with the first dot).
  * @param {string} mimeType Mime type of the file.
  * @return {string} URL
  */
 FileTasks.createWebStoreLink = function(extension, mimeType) {
   if (!extension)
     return FileTasks.CHROME_WEB_STORE_URL;
+
+  if (extension[0] === '.')
+    extension = extension.substr(1);
+  else
+    console.warn('Please pass an extension with a dot to createWebStoreLink.');
 
   var url = FileTasks.WEB_STORE_HANDLER_BASE_URL;
   url += '?_fe=' + extension.toLowerCase().replace(/[^\w]/g, '');
@@ -145,7 +150,7 @@ FileTasks.knownExtensions_ = [
  * @type {Array.<string>}
  */
 FileTasks.EXECUTABLE_EXTENSIONS = Object.freeze([
-  '.exe', '.lnk', '.deb', '.dmg', 'jar', 'msi',
+  '.exe', '.lnk', '.deb', '.dmg', '.jar', '.msi',
 ]);
 
 /**
@@ -155,7 +160,7 @@ FileTasks.EXECUTABLE_EXTENSIONS = Object.freeze([
  * @private
  */
 FileTasks.EXTENSIONS_TO_SKIP_SUGGEST_APPS_ = Object.freeze([
-  'crdownload', 'dsc', 'inf',
+  '.crdownload', '.dsc', '.inf',
 ]);
 
 /**
@@ -325,19 +330,17 @@ FileTasks.prototype.executeDefaultInternal_ = function(entries, opt_callback) {
     return;
 
   var filename = entries[0].name;
-  var extension = filename.lastIndexOf('.') !== -1 ?
-      filename.substr(filename.lastIndexOf('.') + 1) : '';
+  var extension = PathUtil.splitExtension(filename)[1];
   var mimeType = this.mimeTypes_[0];
 
   var showAlert = function() {
-    var messageString =
-        extension === 'exe' ? 'NO_ACTION_FOR_EXECUTABLE' :
-                              'NO_ACTION_FOR_FILE';
+    var messageStringId =
+        extension === '.exe' ? 'NO_ACTION_FOR_EXECUTABLE' :
+                               'NO_ACTION_FOR_FILE';
     var webStoreUrl = FileTasks.createWebStoreLink(extension, mimeType);
-    var text = loadTimeData.getStringF(
-        messageString,
-        webStoreUrl,
-        FileTasks.NO_ACTION_FOR_FILE_URL);
+    var text = strf(messageStringId,
+                    webStoreUrl,
+                    FileTasks.NO_ACTION_FOR_FILE_URL);
     this.fileManager_.alert.showHtml(filename, text, function() {});
     callback(false, entries);
   }.bind(this);
