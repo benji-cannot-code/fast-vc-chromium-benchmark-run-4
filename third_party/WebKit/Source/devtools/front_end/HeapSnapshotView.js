6e454fb5e16b158ebfeec61153417460684373f1
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-05 14:41:28 +0000

    Get rid of _profilesIdMap
    Use linear search instead. This way we can postpone assigning profile uid until its header is received from the backend.
    
    BUG=324769
    R=loislo@chromium.org
    
    Review URL: https://codereview.chromium.org/100833004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163263 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/HeapSnapshotView.js
+++ third_party/WebKit/Source/devtools/front_end/HeapSnapshotView.js
@@ -901,7 +901,7 @@ WebInspector.HeapSnapshotProfileType.prototype = {
      */
     addHeapSnapshotChunk: function(uid, chunk)
     {
-        var profile = this._profilesIdMap[this._makeKey(uid)];
+        var profile = this.getProfile(uid);
         if (profile)
             profile.transferChunk(chunk);
     },
--- third_party/WebKit/Source/devtools/front_end/ProfilesPanel.js
+++ third_party/WebKit/Source/devtools/front_end/ProfilesPanel.js
@@ -36,7 +36,6 @@ WebInspector.ProfileType = function(id, name)
     this._name = name;
     /** @type {!Array.<!WebInspector.ProfileHeader>} */
     this._profiles = [];
-    this._profilesIdMap = {};
     /** @type {WebInspector.SidebarSectionTreeElement} */
     this.treeElement = null;
 }
@@ -141,7 +140,12 @@ WebInspector.ProfileType.prototype = {
      */
     getProfile: function(uid)
     {
-        return this._profilesIdMap[this._makeKey(uid)];
+
+        for (var i = 0; i < this._profiles.length; ++i) {
+            if (this._profiles[i].uid === uid)
+                return this._profiles[i];
+        }
+        return null;
     },
 
     // Must be implemented by subclasses.
@@ -155,23 +159,11 @@ WebInspector.ProfileType.prototype = {
     },
 
     /**
-     * @nosideeffects
-     * @param {number} id
-     * @return {string}
-     */
-    _makeKey: function(id)
-    {
-        return id + '/' + escape(this.id);
-    },
-
-    /**
      * @param {!WebInspector.ProfileHeader} profile
      */
     addProfile: function(profile)
     {
         this._profiles.push(profile);
-        // FIXME: uid only based key should be enough.
-        this._profilesIdMap[this._makeKey(profile.uid)] = profile;
         this.dispatchEventToListeners(WebInspector.ProfileType.Events.AddProfileHeader, profile);
     },
 
@@ -186,7 +178,6 @@ WebInspector.ProfileType.prototype = {
                 break;
             }
         }
-        delete this._profilesIdMap[this._makeKey(profile.uid)];
     },
 
     /**
@@ -217,7 +208,6 @@ WebInspector.ProfileType.prototype = {
         }
         this.treeElement.removeChildren();
         this._profiles = [];
-        this._profilesIdMap = {};
     },
 
     __proto__: WebInspector.Object.prototype
