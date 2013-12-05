f2fbe308b8aede913ca3b96eb2150f3ae48cff14
steveblock@chromium.org <steveblock@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-05 08:29:09 +0000

    Web Animations: Clarify logic for applying animated properties with matched properties cache
    This change clarifies the logic in StyleResolver::applyMatchedProperties() to
    make clear the three cases in which we need to apply animated properties when
    using the matched properties cache. Note that
    StyleResolver::applyAnimatedProperties() already null-checks the animating
    element. This will simplify an upcoming change for pseudo elements in
    https://codereview.chromium.org/76033002
    
    Also rename some method parameters from 'e' to 'element' for clarity.
    
    No functional change.
    
    R=timloh@chromium.org
    
    Review URL: https://codereview.chromium.org/99333008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163230 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/resolver/StyleResolver.cpp
+++ third_party/WebKit/Source/core/css/resolver/StyleResolver.cpp
@@ -747,15 +747,15 @@ PassRefPtr<RenderStyle> StyleResolver::styleForElement(Element* element, RenderS
     return state.takeStyle();
 }
 
-PassRefPtr<RenderStyle> StyleResolver::styleForKeyframe(Element* e, const RenderStyle& elementStyle, const StyleKeyframe* keyframe, const AtomicString& animationName)
+PassRefPtr<RenderStyle> StyleResolver::styleForKeyframe(Element* element, const RenderStyle& elementStyle, const StyleKeyframe* keyframe, const AtomicString& animationName)
 {
     ASSERT(document().frame());
     ASSERT(documentSettings());
     ASSERT(!hasPendingAuthorStyleSheets());
 
-    if (e == document().documentElement())
+    if (element == document().documentElement())
         resetDirectionAndWritingModeOnDocument(document());
-    StyleResolverState state(document(), e);
+    StyleResolverState state(document(), element);
 
     MatchResult result;
     if (keyframe->properties())
@@ -875,15 +875,15 @@ void StyleResolver::keyframeStylesForAnimation(Element* e, const RenderStyle& el
     }
 }
 
-PassRefPtr<RenderStyle> StyleResolver::pseudoStyleForElement(Element* e, const PseudoStyleRequest& pseudoStyleRequest, RenderStyle* parentStyle)
+PassRefPtr<RenderStyle> StyleResolver::pseudoStyleForElement(Element* element, const PseudoStyleRequest& pseudoStyleRequest, RenderStyle* parentStyle)
 {
     ASSERT(document().frame());
     ASSERT(documentSettings());
     ASSERT(parentStyle);
-    if (!e)
+    if (!element)
         return 0;
 
-    StyleResolverState state(document(), e, parentStyle);
+    StyleResolverState state(document(), element, parentStyle);
 
     if (pseudoStyleRequest.allowsInheritance(state.parentStyle())) {
         state.setStyle(RenderStyle::create());
@@ -911,7 +911,7 @@ PassRefPtr<RenderStyle> StyleResolver::pseudoStyleForElement(Element* e, const P
 
         state.style()->setStyleType(pseudoStyleRequest.pseudoId);
 
-        applyMatchedProperties(state, collector.matchedResult(), e->pseudoElement(pseudoStyleRequest.pseudoId));
+        applyMatchedProperties(state, collector.matchedResult(), element->pseudoElement(pseudoStyleRequest.pseudoId));
 
         addContentAttrValuesToFeatures(state.contentAttrValues(), m_features);
     }
@@ -924,7 +924,7 @@ PassRefPtr<RenderStyle> StyleResolver::pseudoStyleForElement(Element* e, const P
 
     didAccess();
 
-    if (PseudoElement* pseudoElement = e->pseudoElement(pseudoStyleRequest.pseudoId))
+    if (PseudoElement* pseudoElement = element->pseudoElement(pseudoStyleRequest.pseudoId))
         setAnimationUpdateIfNeeded(state, *pseudoElement);
 
     // Now return the style.
@@ -1293,8 +1293,8 @@ void StyleResolver::applyMatchedProperties(StyleResolverState& state, const Matc
             // Unfortunately the link status is treated like an inherited property. We need to explicitly restore it.
             state.style()->setInsideLink(linkStatus);
 
-            if (RuntimeEnabledFeatures::webAnimationsCSSEnabled() && animatingElement
-                && (animatingElement->hasActiveAnimations()
+            if (RuntimeEnabledFeatures::webAnimationsCSSEnabled()
+                && ((animatingElement && animatingElement->hasActiveAnimations())
                     || (state.style()->transitions() && !state.style()->transitions()->isEmpty())
                     || (state.style()->animations() && !state.style()->animations()->isEmpty())))
                 applyAnimatedProperties(state, animatingElement);
