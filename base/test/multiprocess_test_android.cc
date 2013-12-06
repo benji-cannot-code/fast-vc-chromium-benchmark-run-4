021a2386ddcc6d969d1f91268a3af236c3b32091
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-06 21:23:35 +0000

    Add a MultiProcessTest::SpawnChildWithOptions().
    Replace SpawnChildImpl() with SpawnChildWithOptions(). Leave the
    now-superfluous POSIX-only SpawnChild() that takes a
    FileHandleMappingVector for now.
    
    R=brettw@chromium.org, brettw
    
    Review URL: https://codereview.chromium.org/104803007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239258 0039d316-1c4b-4281-b951-d872f2087c98

--- base/test/multiprocess_test_android.cc
+++ base/test/multiprocess_test_android.cc
@@ -13,14 +13,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
-// A very basic implementation for android. On Android tests can run in an APK
+// A very basic implementation for Android. On Android tests can run in an APK
 // and we don't have an executable to exec*. This implementation does the bare
 // minimum to execute the method specified by procname (in the child process).
+//  - All options except |fds_to_remap| are ignored.
 //  - |debug_on_start| is ignored.
-ProcessHandle MultiProcessTest::SpawnChildImpl(
+ProcessHandle MultiProcessTest::SpawnChildWithOptions(
     const std::string& procname,
-    const FileHandleMappingVector& fds_to_remap,
+    const LaunchOptions& options,
     bool debug_on_start) {
+  // TODO(vtl): The FD-remapping done below is wrong in the presence of cycles
+  // (e.g., fd1 -> fd2, fd2 -> fd1). crbug.com/326576
+  FileHandleMappingVector empty;
+  const FileHandleMappingVector* fds_to_remap =
+      options.fds_to_remap ? options.fds_to_remap : &empty;
+
   pid_t pid = fork();
 
   if (pid < 0) {
@@ -33,8 +40,8 @@ ProcessHandle MultiProcessTest::SpawnChildImpl(
   }
   // Child process.
   std::hash_set<int> fds_to_keep_open;
-  for (FileHandleMappingVector::const_iterator it = fds_to_remap.begin();
-       it != fds_to_remap.end(); ++it) {
+  for (FileHandleMappingVector::const_iterator it = fds_to_remap->begin();
+       it != fds_to_remap->end(); ++it) {
     fds_to_keep_open.insert(it->first);
   }
   // Keep stdin, stdout and stderr open since this is not meant to spawn a
@@ -45,8 +52,8 @@ ProcessHandle MultiProcessTest::SpawnChildImpl(
       close(fd);
     }
   }
-  for (FileHandleMappingVector::const_iterator it = fds_to_remap.begin();
-       it != fds_to_remap.end(); ++it) {
+  for (FileHandleMappingVector::const_iterator it = fds_to_remap->begin();
+       it != fds_to_remap->end(); ++it) {
     int old_fd = it->first;
     int new_fd = it->second;
     if (dup2(old_fd, new_fd) < 0) {
