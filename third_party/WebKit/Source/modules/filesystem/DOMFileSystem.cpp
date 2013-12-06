5068636bfbc5345ff2f9ef34909b8ee6fa64c58a
tommycli@chromium.org <tommycli@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-06 05:42:14 +0000

    FileAPI Blink: Enable FileSystemURL-backed Blobs by removing an ASSERT.
    I'm doing this to support streaming MTP blobs. This should also allow remote Blobs without snapshot files in general though.
    
    Implementation plan: https://docs.google.com/a/chromium.org/document/d/16YJVUVFRc8z-q68KwoXP2s9aUIDIOSNfNPuGk83zotQ/edit?usp=sharing
    
    BUG=318450,110119
    
    Review URL: https://codereview.chromium.org/106893002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163303 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/filesystem/DOMFileSystem.cpp
+++ third_party/WebKit/Source/modules/filesystem/DOMFileSystem.cpp
@@ -157,7 +157,6 @@ public:
 
     virtual void didCreateSnapshotFile(const FileMetadata& metadata, PassRefPtr<BlobDataHandle> snapshot)
     {
-        ASSERT(!metadata.platformPath.isEmpty());
         if (!m_successCallback)
             return;
 
