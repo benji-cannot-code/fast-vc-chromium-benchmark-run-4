f81c589c8541c16caac1e8d143eee62c5b26fd76
pavely@chromium.org <pavely@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-06 12:45:18 +0000

    Refactor PushClientChannel
    Move code responsible for working with invalidations library out of
    PushClientChannel into SyncNetworkChannel. PushClientChannel only has
    code related to PushClient.
    
    Create PushClient independent SyncNetworkChannel::Observer interface and
    make SyncInvalidationListener observe it instead of PushClientObserver.
    
    Move ownership of PushClientChannel from SyncSystemResources to
    SyncInvalidationListener.
    
    Tests for PushClientChannel are still in push_client_channel_unittest.cc.
    I'll split those in subsequent patch.
    
    BUG=325020
    
    Review URL: https://codereview.chromium.org/103223004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239176 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/notifier/push_client_channel.h
+++ sync/notifier/push_client_channel.h
@@ -7,14 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define SYNC_NOTIFIER_PUSH_CLIENT_CHANNEL_H_
 
 #include <string>
-#include <vector>
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
 #include "base/memory/scoped_ptr.h"
-#include "google/cacheinvalidation/include/system-resources.h"
 #include "jingle/notifier/listener/push_client_observer.h"
 #include "sync/base/sync_export.h"
+#include "sync/notifier/sync_system_resources.h"
 
 namespace notifier {
 class PushClient;
@@ -25,7 +24,7 @@ namespace syncer {
 // A PushClientChannel is an implementation of NetworkChannel that
 // routes messages through a PushClient.
 class SYNC_EXPORT_PRIVATE PushClientChannel
-    : public NON_EXPORTED_BASE(invalidation::NetworkChannel),
+    : public SyncNetworkChannel,
       public NON_EXPORTED_BASE(notifier::PushClientObserver) {
  public:
   // |push_client| is guaranteed to be destroyed only when this object
@@ -39,14 +38,8 @@ class SYNC_EXPORT_PRIVATE PushClientChannel
   // credentials.
   void UpdateCredentials(const std::string& email, const std::string& token);
 
-  // invalidation::NetworkChannel implementation.
-  virtual void SendMessage(const std::string& outgoing_message) OVERRIDE;
-  virtual void SetMessageReceiver(
-      invalidation::MessageCallback* incoming_receiver) OVERRIDE;
-  virtual void AddNetworkStatusReceiver(
-      invalidation::NetworkStatusCallback* network_status_receiver) OVERRIDE;
-  virtual void SetSystemResources(
-      invalidation::SystemResources* resources) OVERRIDE;
+  // SyncNetworkChannel implementation.
+  virtual void SendEncodedMessage(const std::string& encoded_message) OVERRIDE;
 
   // notifier::PushClient::Observer implementation.
   virtual void OnNotificationsEnabled() OVERRIDE;
@@ -55,47 +48,8 @@ class SYNC_EXPORT_PRIVATE PushClientChannel
   virtual void OnIncomingNotification(
       const notifier::Notification& notification) OVERRIDE;
 
-  const std::string& GetServiceContextForTest() const;
-
-  int64 GetSchedulingHashForTest() const;
-
-  static notifier::Notification EncodeMessageForTest(
-      const std::string& message,
-      const std::string& service_context,
-      int64 scheduling_hash);
-
-  static bool DecodeMessageForTest(
-      const notifier::Notification& notification,
-      std::string* message,
-      std::string* service_context,
-      int64* scheduling_hash);
-
  private:
-  typedef std::vector<invalidation::NetworkStatusCallback*>
-      NetworkStatusReceiverList;
-
-  static notifier::Notification EncodeMessage(
-      const std::string& message,
-      const std::string& service_context,
-      int64 scheduling_hash);
-
-  static bool DecodeMessage(
-      const notifier::Notification& notification,
-      std::string* message,
-      std::string* service_context,
-      int64* scheduling_hash);
-
   scoped_ptr<notifier::PushClient> push_client_;
-  scoped_ptr<invalidation::MessageCallback> incoming_receiver_;
-  NetworkStatusReceiverList network_status_receivers_;
-
-  bool notifications_enabled_;
-
-  // Service context.
-  std::string service_context_;
-
-  // Scheduling hash.
-  int64 scheduling_hash_;
 
   DISALLOW_COPY_AND_ASSIGN(PushClientChannel);
 };
