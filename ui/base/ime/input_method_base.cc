1f91636c19cab974fca02d114dfb7553a3189d8f
kochi@chromium.org <kochi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-06 17:49:01 +0000

    Implement IMM32 version of candidate window events
    The blink part is already done in:
    https://src.chromium.org/viewvc/blink?revision=161139&view=revision
    IPCs are implemented in:
    https://src.chromium.org/viewvc/chrome?revision=238282&view=revision
    
    This CL enables IMM32-based Chromium (i.e. Windows except Metro mode)
    to generate candidate window events.
    
    BUG=238585
    TEST=ui_unittests.exe --gtest_filter="InputMethodBaseTest.*"
    
    Review URL: https://codereview.chromium.org/104023010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239238 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/ime/input_method_base.cc
+++ ui/base/ime/input_method_base.cc
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/base/ime/input_method_base.h"
 
+#include "base/bind.h"
 #include "base/logging.h"
+#include "base/message_loop/message_loop.h"
 #include "ui/base/ime/input_method_delegate.h"
 #include "ui/base/ime/input_method_observer.h"
 #include "ui/base/ime/text_input_client.h"
@@ -129,6 +131,40 @@ void InputMethodBase::SetFocusedTextInputClientInternal(
   text_input_client_ = client;  // NULL allowed.
   OnDidChangeFocusedClient(old, client);
   NotifyTextInputStateChanged(text_input_client_);
+}
+
+void InputMethodBase::OnCandidateWindowShown() {
+  base::MessageLoop::current()->PostTask(
+      FROM_HERE,
+      base::Bind(&InputMethodBase::CandidateWindowShownCallback, AsWeakPtr()));
+}
+
+void InputMethodBase::OnCandidateWindowUpdated() {
+  base::MessageLoop::current()->PostTask(
+      FROM_HERE,
+      base::Bind(&InputMethodBase::CandidateWindowUpdatedCallback,
+                 AsWeakPtr()));
+}
+
+void InputMethodBase::OnCandidateWindowHidden() {
+  base::MessageLoop::current()->PostTask(
+      FROM_HERE,
+      base::Bind(&InputMethodBase::CandidateWindowHiddenCallback, AsWeakPtr()));
+}
+
+void InputMethodBase::CandidateWindowShownCallback() {
+  if (text_input_client_)
+    text_input_client_->OnCandidateWindowShown();
+}
+
+void InputMethodBase::CandidateWindowUpdatedCallback() {
+  if (text_input_client_)
+    text_input_client_->OnCandidateWindowUpdated();
+}
+
+void InputMethodBase::CandidateWindowHiddenCallback() {
+  if (text_input_client_)
+    text_input_client_->OnCandidateWindowHidden();
 }
 
 }  // namespace ui
