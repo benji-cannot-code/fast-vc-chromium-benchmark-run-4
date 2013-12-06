fefadecefe17956324e2707b2e8b5d4da1b16396
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-06 06:22:26 +0000

    Revert 239112 "Send IPCs from RenderFrameHost to WebContents to ..."
    > Send IPCs from RenderFrameHost to WebContents to match what we do with RenderViewHost. This has two purposes:
    > -IPCs that go straight to WebContentsObserver methods don't have to touch render_frame_host_impl.cc, render_frame_host_impl.h, and render_frame_host_delegate.h
    > -IPC's sent with the RenderFrame's routing id now go to WebContentsObservers
    > 
    > We can now dispatch FrameHostMsg_PepperPluginHung in WebContentsImpl like it used to. Also change FrameHostMsg_PluginCrashed to use the RenderFrame's routing ID.
    > 
    > BUG=304341
    > R=nasko@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/107413003
    
    TBR=jam@chromium.org
    
    Review URL: https://codereview.chromium.org/107893002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239125 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/frame_messages.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/render_process_host.h"
-#include "content/public/browser/user_metrics.h"
 #include "url/gurl.h"
 
 namespace content {
@@ -67,23 +66,14 @@ bool RenderFrameHostImpl::Send(IPC::Message* message) {
 }
 
 bool RenderFrameHostImpl::OnMessageReceived(const IPC::Message &msg) {
-  if (delegate_->OnMessageReceived(this, msg))
-    return true;
-
   bool handled = true;
   bool msg_is_ok = true;
   IPC_BEGIN_MESSAGE_MAP_EX(RenderFrameHostImpl, msg, msg_is_ok)
     IPC_MESSAGE_HANDLER(FrameHostMsg_Detach, OnDetach)
     IPC_MESSAGE_HANDLER(FrameHostMsg_DidStartProvisionalLoadForFrame,
                         OnDidStartProvisionalLoadForFrame)
+    IPC_MESSAGE_HANDLER(FrameHostMsg_PepperPluginHung, OnPepperPluginHung)
   IPC_END_MESSAGE_MAP_EX()
-
-  if (!msg_is_ok) {
-    // The message had a handler, but its de-serialization failed.
-    // Kill the renderer.
-    RecordAction(UserMetricsAction("BadMessageTerminate_RFH"));
-    GetProcess()->ReceivedBadMessage();
-  }
 
   return handled;
 }
@@ -117,6 +107,12 @@ void RenderFrameHostImpl::OnDidStartProvisionalLoadForFrame(
     const GURL& url) {
   render_view_host_->OnDidStartProvisionalLoadForFrame(
       frame_id, parent_frame_id, is_main_frame, url);
+}
+
+void RenderFrameHostImpl::OnPepperPluginHung(int plugin_child_id,
+                                             const base::FilePath& path,
+                                             bool is_hung) {
+  delegate_->PepperPluginHung(plugin_child_id, path, is_hung);
 }
 
 }  // namespace content
--- content/content_browser.gypi
+++ content/content_browser.gypi
@@ -521,7 +521,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     'browser/frame_host/navigator.cc',
     'browser/frame_host/navigator.h',
     'browser/frame_host/navigator_delegate.h',
-    'browser/frame_host/render_frame_host_delegate.cc',
     'browser/frame_host/render_frame_host_delegate.h',
     'browser/frame_host/render_frame_host_factory.cc',
     'browser/frame_host/render_frame_host_factory.h',
