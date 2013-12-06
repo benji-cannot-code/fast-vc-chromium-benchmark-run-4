3723da39714a2a5cb6e265352d5879af52af594c
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-06 14:34:35 +0000

    drive: Stop firing OnDirectoryChangedByOperation for the same directory from MoveOperation
    BUG=None
    TEST=unit_tests
    
    Review URL: https://codereview.chromium.org/107953002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239213 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/file_system/move_operation.h
+++ chrome/browser/chromeos/drive/file_system/move_operation.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CHROMEOS_DRIVE_FILE_SYSTEM_MOVE_OPERATION_H_
 #define CHROME_BROWSER_CHROMEOS_DRIVE_FILE_SYSTEM_MOVE_OPERATION_H_
 
+#include <set>
+
 #include "base/basictypes.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
@@ -56,11 +58,11 @@ class MoveOperation {
 
  private:
   // Part of Move(). Called after updating the local state.
-  void MoveAfterUpdateLocalState(const base::FilePath& src_file_path,
-                                 const base::FilePath& dest_file_path,
-                                 const FileOperationCallback& callback,
-                                 const std::string* local_id,
-                                 FileError error);
+  void MoveAfterUpdateLocalState(
+      const FileOperationCallback& callback,
+      const std::set<base::FilePath>* changed_directories,
+      const std::string* local_id,
+      FileError error);
 
   scoped_refptr<base::SequencedTaskRunner> blocking_task_runner_;
   OperationObserver* observer_;
