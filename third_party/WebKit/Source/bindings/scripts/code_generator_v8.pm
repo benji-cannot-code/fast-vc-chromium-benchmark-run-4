99c588867bc148f4f650f56e5705fdd37a4d711a
nbarth@chromium.org <nbarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-06 13:47:58 +0000

    IDL compiler: cleanup [ConstructorTemplate] => [EventConstructor]
    Cleanup, no changes to generated code.
    
    Only [ConstructorTemplate=Event] is used in actual IDLs;
    this renames it to clearer [EventConstructor],
    and deletes code and test case for unused [ConstructorTemplate=TypedDef].
    
    Also a bit of renaming and cleanup of test cases.
    
    Docs updated:
    https://sites.google.com/a/chromium.org/dev/blink/webidl/blink-idl-extended-attributes#TOC-EventConstructor-i-
    
    Review URL: https://codereview.chromium.org/107753002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163337 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/scripts/code_generator_v8.pm
+++ third_party/WebKit/Source/bindings/scripts/code_generator_v8.pm
@@ -709,7 +709,7 @@ sub GenerateHeader
         }
     }
 
-    $header{nameSpaceWebCore}->addHeader("\nclass Dictionary;") if IsConstructorTemplate($interface, "Event");
+    $header{nameSpaceWebCore}->addHeader("\nclass Dictionary;") if $interface->extendedAttributes->{"EventConstructor"};
 
     my $nativeType = GetNativeTypeForConversions($interface);
     if ($interface->extendedAttributes->{"NamedConstructor"}) {
@@ -1026,7 +1026,7 @@ inline void v8SetReturnValueFast(const CallbackInfo& callbackInfo, PassRefPtr<${
 
 END
 
-    if (IsConstructorTemplate($interface, "Event")) {
+    if ($interface->extendedAttributes->{"EventConstructor"}) {
         $header{nameSpaceWebCore}->add("bool fill${implClassName}Init(${implClassName}Init&, const Dictionary&, ExceptionState&, const String& = \"\");\n\n");
     }
 }
@@ -1148,7 +1148,7 @@ sub IsConstructable
 {
     my $interface = shift;
 
-    return $interface->extendedAttributes->{"CustomConstructor"} || $interface->extendedAttributes->{"Constructor"} || $interface->extendedAttributes->{"ConstructorTemplate"};
+    return $interface->extendedAttributes->{"CustomConstructor"} || $interface->extendedAttributes->{"Constructor"} || $interface->extendedAttributes->{"EventConstructor"};
 }
 
 sub HasCustomConstructor
@@ -2403,17 +2403,6 @@ END
 
     $code .= GenerateArgumentsCountCheck($function, $interface);
 
-    if ($name eq "set" and IsConstructorTemplate($interface, "TypedArray")) {
-        AddToImplIncludes("bindings/v8/custom/V8ArrayBufferViewCustom.h");
-        $code .= <<END;
-    setWebGLArrayHelper<$implClassName, ${v8ClassName}>(info);
-}
-
-END
-        $implementation{nameSpaceInternal}->add($code);
-        return;
-    }
-
     if ($svgNativeType) {
         my $nativeClassName = GetNativeType($interfaceName);
         if ($interfaceName =~ /List$/) {
@@ -2820,7 +2809,7 @@ sub GetInterfaceLength
     my $interface = shift;
 
     my $leastConstructorLength = 0;
-    if (IsConstructorTemplate($interface, "Event") || IsConstructorTemplate($interface, "TypedArray")) {
+    if ($interface->extendedAttributes->{"EventConstructor"}) {
         $leastConstructorLength = 1;
     } elsif ($interface->extendedAttributes->{"Constructor"} || $interface->extendedAttributes->{"CustomConstructor"}) {
         my @constructors = @{$interface->constructors};
@@ -4317,21 +4306,6 @@ END
         GenerateSecurityCheckFunctions($interface);
     }
 
-    if (IsConstructorTemplate($interface, "TypedArray")) {
-        my ($nativeType, $arrayType) = GetNativeTypeOfTypedArray($interface);
-        $implementation{nameSpaceWebCore}->add(<<END);
-v8::Handle<v8::Object> wrap($implClassName* impl, v8::Handle<v8::Object> creationContext, v8::Isolate* isolate)
-{
-    ASSERT(impl);
-    v8::Handle<v8::Object> wrapper = ${v8ClassName}::createWrapper(impl, creationContext, isolate);
-    if (!wrapper.IsEmpty())
-        wrapper->SetIndexedPropertiesToExternalArrayData(impl->baseAddress(), $arrayType, impl->length());
-    return wrapper;
-}
-
-END
-    }
-
     my @perContextEnabledFunctions;
     my @normalFunctions;
     my $needsDomainSafeFunctionSetter = 0;
@@ -4471,7 +4445,7 @@ END
             GenerateNamedConstructor(@{$interface->constructors}[0], $interface);
         } elsif ($interface->extendedAttributes->{"Constructor"}) {
             GenerateConstructor($interface);
-        } elsif (IsConstructorTemplate($interface, "Event")) {
+        } elsif ($interface->extendedAttributes->{"EventConstructor"}) {
             GenerateEventConstructor($interface);
         }
     }
@@ -5994,14 +5968,6 @@ sub FindSuperMethod
         }
     });
     return $indexer;
-}
-
-sub IsConstructorTemplate
-{
-    my $interface = shift;
-    my $template = shift;
-
-    return $interface->extendedAttributes->{"ConstructorTemplate"} && $interface->extendedAttributes->{"ConstructorTemplate"} eq $template;
 }
 
 sub IsPrimitiveType
