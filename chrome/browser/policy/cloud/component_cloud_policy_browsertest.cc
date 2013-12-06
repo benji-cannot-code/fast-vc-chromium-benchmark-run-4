15f3b5ec331f66601680878289a095640a5de270
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-06 13:53:02 +0000

    Disable cloud policy for extensions by default.
    The flag will be flipped back once the server is ready. For now it'll be
    disabled on cloud policy. Note that policy for extensions is still loaded from
    the platform policy providers.
    
    BUG=108992
    
    Review URL: https://codereview.chromium.org/107063002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239198 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/cloud/component_cloud_policy_browsertest.cc
+++ chrome/browser/policy/cloud/component_cloud_policy_browsertest.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
+#include "components/policy/core/common/policy_switches.h"
 #include "extensions/common/extension.h"
 #include "net/url_request/url_request_context_getter.h"
 #include "policy/proto/cloud_policy.pb.h"
@@ -119,6 +120,7 @@ class ComponentCloudPolicyTest : public ExtensionBrowserTest {
     std::string url = test_server_.GetServiceURL().spec();
     CommandLine* command_line = CommandLine::ForCurrentProcess();
     command_line->AppendSwitchASCII(::switches::kDeviceManagementUrl, url);
+    command_line->AppendSwitch(switches::kEnableComponentCloudPolicy);
 
     ExtensionBrowserTest::SetUpInProcessBrowserTestFixture();
   }
