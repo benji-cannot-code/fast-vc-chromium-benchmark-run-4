e8f259898dc8fe6af2ae7cf80764df2f38aefed1
noamsml@chromium.org <noamsml@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-07 03:44:13 +0000

    Show name of pending last-selected printer next to throbber while printer loads
    Instead of showing a bare throbber while loading a local printer, show throbber
    next to name of the printer being loaded, so the user knows this is the pending
    printer.
    
    BUG=325983
    
    Review URL: https://codereview.chromium.org/105943002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239308 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/print_preview/data/app_state.js
+++ chrome/browser/resources/print_preview/data/app_state.js
@@ -38,6 +38,7 @@ cr.define('print_preview', function() {
     SELECTED_DESTINATION_ID: 'selectedDestinationId',
     SELECTED_DESTINATION_ORIGIN: 'selectedDestinationOrigin',
     SELECTED_DESTINATION_CAPABILITIES: 'selectedDestinationCapabilities',
+    SELECTED_DESTINATION_NAME: 'selectedDestinationName',
     IS_SELECTED_DESTINATION_LOCAL: 'isSelectedDestinationLocal',  // Deprecated
     IS_GCP_PROMO_DISMISSED: 'isGcpPromoDismissed',
     MARGINS_TYPE: 'marginsType',
@@ -81,6 +82,11 @@ cr.define('print_preview', function() {
     /** @return {?print_preview.Cdd} CDD of the selected destination. */
     get selectedDestinationCapabilities() {
       return this.state_[AppState.Field.SELECTED_DESTINATION_CAPBILITIES];
+    },
+
+    /** @return {?string} Name of the selected destination. */
+    get selectedDestinationName() {
+      return this.state_[AppState.Field.SELECTED_DESTINATION_NAME];
     },
 
     /** @return {boolean} Whether the GCP promotion has been dismissed. */
@@ -170,6 +176,7 @@ cr.define('print_preview', function() {
       this.state_[AppState.Field.SELECTED_DESTINATION_ORIGIN] = dest.origin;
       this.state_[AppState.Field.SELECTED_DESTINATION_CAPBILITIES] =
           dest.capabilities;
+      this.state_[AppState.Field.SELECTED_DESTINATION_NAME] = dest.displayName;
       this.persist_();
     },
 
