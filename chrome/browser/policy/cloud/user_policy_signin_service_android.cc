32c30da23306b41c0f6e4820b28186f72d21518f
msarda@chromium.org <msarda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-07 13:20:08 +0000

    Add MutableProfileOAuth2TokenService class placeholder.
    This CL adds a empty MutableProfileOAuth2TokenService class. Future CLs
    will move desktop specific logic that allow the users of PO2TS to update
    the refresh tokens from OAuth2TokenService and ProfileOAUth2TokenService
    to MutableProfileOAuth2TokenService.
    
    TEST=NONE (no user visible change)
    BUG=320625
    
    Review URL: https://codereview.chromium.org/102313004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239337 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/cloud/user_policy_signin_service_android.cc
+++ chrome/browser/policy/cloud/user_policy_signin_service_android.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/policy/cloud/user_cloud_policy_manager.h"
 #include "chrome/browser/policy/proto/cloud/device_management_backend.pb.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/signin/profile_oauth2_token_service.h"
 #include "chrome/browser/signin/profile_oauth2_token_service_factory.h"
 #include "chrome/browser/signin/signin_manager.h"
 #include "chrome/common/pref_names.h"
--- chrome/browser/signin/profile_oauth2_token_service_factory.cc
+++ chrome/browser/signin/profile_oauth2_token_service_factory.cc
@@ -11,6 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/webdata/web_data_service_factory.h"
 #include "components/browser_context_keyed_service/browser_context_dependency_manager.h"
 
+#if defined(OS_ANDROID)
+#include "chrome/browser/signin/android_profile_oauth2_token_service.h"
+#else
+#include "chrome/browser/signin/mutable_profile_oauth2_token_service.h"
+#endif
+
 ProfileOAuth2TokenServiceFactory::ProfileOAuth2TokenServiceFactory()
     : BrowserContextKeyedServiceFactory(
         "ProfileOAuth2TokenService",
@@ -22,21 +28,19 @@ ProfileOAuth2TokenServiceFactory::ProfileOAuth2TokenServiceFactory()
 ProfileOAuth2TokenServiceFactory::~ProfileOAuth2TokenServiceFactory() {
 }
 
-#if defined(OS_ANDROID)
-// static
-AndroidProfileOAuth2TokenService*
-    ProfileOAuth2TokenServiceFactory::GetForProfile(Profile* profile) {
-  return static_cast<AndroidProfileOAuth2TokenService*>(
-      GetInstance()->GetServiceForBrowserContext(profile, true));
-}
-#else
-// static
-ProfileOAuth2TokenService* ProfileOAuth2TokenServiceFactory::GetForProfile(
-    Profile* profile) {
+ProfileOAuth2TokenService*
+ProfileOAuth2TokenServiceFactory::GetForProfile(Profile* profile) {
   return static_cast<ProfileOAuth2TokenService*>(
       GetInstance()->GetServiceForBrowserContext(profile, true));
 }
-#endif  // defined(OS_ANDROID)
+
+// static
+ProfileOAuth2TokenServiceFactory::PlatformSpecificOAuth2TokenService*
+ProfileOAuth2TokenServiceFactory::GetPlatformSpecificForProfile(
+    Profile* profile) {
+  return static_cast<PlatformSpecificOAuth2TokenService*>(
+      GetForProfile(profile));
+}
 
 // static
 ProfileOAuth2TokenServiceFactory*
@@ -48,12 +52,8 @@ BrowserContextKeyedService*
 ProfileOAuth2TokenServiceFactory::BuildServiceInstanceFor(
     content::BrowserContext* context) const {
   Profile* profile = static_cast<Profile*>(context);
-  ProfileOAuth2TokenService* service;
-#if defined(OS_ANDROID)
-  service = new AndroidProfileOAuth2TokenService();
-#else
-  service = new ProfileOAuth2TokenService();
-#endif
+  PlatformSpecificOAuth2TokenService* service =
+      new PlatformSpecificOAuth2TokenService();
   service->Initialize(profile);
   return service;
 }
