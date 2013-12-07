238173a63994b915f8522426941bfd17402148a1
jyasskin@chromium.org <jyasskin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-07 03:58:32 +0000

    Use TestExtensionEnvironment in RulesRegistryWithCacheTest.
    This simplifies the test code.
    
    Review URL: https://codereview.chromium.org/106163010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239315 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/test_extension_environment.h
+++ chrome/browser/extensions/test_extension_environment.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/scoped_ptr.h"
 #include "base/message_loop/message_loop.h"
-#include "content/public/test/test_browser_thread.h"
+#include "content/public/test/test_browser_thread_bundle.h"
 
 #if defined(OS_CHROMEOS)
 #include "chrome/browser/chromeos/login/user_manager.h"
@@ -34,6 +34,8 @@ class WebContents;
 namespace extensions {
 
 class Extension;
+class ExtensionPrefs;
+class TestExtensionSystem;
 
 // This class provides a minimal environment in which to create
 // extensions and tabs for extension-related unittests.
@@ -44,9 +46,16 @@ class TestExtensionEnvironment {
 
   TestingProfile* profile() const;
 
+  // Returns the TestExtensionSystem created by the TestingProfile.
+  TestExtensionSystem* GetExtensionSystem();
+
   // Returns an ExtensionService created (and owned) by the
   // TestExtensionSystem created by the TestingProfile.
   ExtensionService* GetExtensionService();
+
+  // Returns ExtensionPrefs created (and owned) by the
+  // TestExtensionSystem created by the TestingProfile.
+  ExtensionPrefs* GetExtensionPrefs();
 
   // Creates an Extension and registers it with the ExtensionService.
   // The Extension has a default manifest of {name: "Extension",
@@ -58,14 +67,7 @@ class TestExtensionEnvironment {
   scoped_ptr<content::WebContents> MakeTab() const;
 
  private:
-  base::MessageLoopForUI loop_;
-  content::TestBrowserThread ui_thread_;
-  content::TestBrowserThread file_thread_;
-  content::TestBrowserThread file_blocking_thread_;
-  content::TestBrowserThread io_thread_;
-  // We may need to add the rest of the browser threads here.  This is
-  // likely to be indicated by memory leaks in which the object was
-  // expected to be freed by a DeleteSoon() call.
+  content::TestBrowserThreadBundle thread_bundle_;
 
 #if defined(OS_CHROMEOS)
   chromeos::ScopedTestDeviceSettingsService test_device_settings_service_;
@@ -78,6 +80,7 @@ class TestExtensionEnvironment {
 #endif
   scoped_ptr<TestingProfile> profile_;
   ExtensionService* extension_service_;
+  ExtensionPrefs* extension_prefs_;
 };
 
 }  // namespace extensions
