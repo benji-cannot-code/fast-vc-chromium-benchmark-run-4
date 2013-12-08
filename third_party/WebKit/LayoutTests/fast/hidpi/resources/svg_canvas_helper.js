7173c5ffa4ee75382d28f85cbf34f7d364f23608
yoav@yoav.ws <yoav@yoav.ws@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-08 09:19:00 +0000

    I've replaced srcset's canvas tests from pixel tests to ref tests & bullet-proofed the ref tests to fail in case that the images are missing.
    I've also deleted tons of platform specific expected results in the process.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/109613002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163379 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/hidpi/resources/svg_canvas_helper.js
+++ third_party/WebKit/LayoutTests/fast/hidpi/resources/svg_canvas_helper.js
@@ -1,21 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-function printSizes(imageId, outputId) {
-  var image = document.getElementById(imageId);
-  image.addEventListener('load', function() {
-    var output = document.getElementById(outputId);
-    output.innerHTML = 'image stats: '
-        + 'size(' + image.width + ',' + image.height + '), '
-        + 'naturalSize(' + image.naturalWidth + ',' + image.naturalHeight + ')<br/>'
-        + 'image codes: '
-        + image.outerHTML.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-  });
-}
-
 function drawCanvas(imageId, canvasId, canvasWidth, canvasHeight) {
   var image = document.getElementById(imageId);
   var canvas = document.getElementById(canvasId);
   canvas.width = canvasWidth;
   canvas.height = canvasHeight;
   var context = canvas.getContext('2d');
-  context.drawImage(image, 0, 0);
+  context.drawImage(image, 0, 0, canvasWidth, canvasHeight);
 }
