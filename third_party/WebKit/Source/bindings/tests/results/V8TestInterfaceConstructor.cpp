4dd5d48f8bffc4b77b0d6055c9eb545f901f30eb
nbarth@chromium.org <nbarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-09 01:45:36 +0000

    IDL compiler: [MeasureAs] interfaces
    BUG=239771
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/103963003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163385 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/tests/results/V8TestInterfaceConstructor.cpp
+++ third_party/WebKit/Source/bindings/tests/results/V8TestInterfaceConstructor.cpp
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/v8/V8ObjectConstructor.h"
 #include "core/dom/ContextFeatures.h"
 #include "core/dom/Document.h"
+#include "core/frame/UseCounter.h"
 #include "platform/TraceEvent.h"
 #include "wtf/UnusedParam.h"
 
@@ -91,6 +92,7 @@ static void constructor(const v8::FunctionCallbackInfo<v8::Value>& info)
 void V8TestInterfaceConstructor::constructorCallback(const v8::FunctionCallbackInfo<v8::Value>& info)
 {
     TRACE_EVENT_SCOPED_SAMPLING_STATE("Blink", "DOMConstructor");
+    UseCounter::count(activeDOMWindow(), UseCounter::TestFeature);
     if (!info.IsConstructCall()) {
         throwTypeError(ExceptionMessages::failedToConstruct("TestInterfaceConstructor", "Please use the 'new' operator, this DOM object constructor cannot be called as a function."), info.GetIsolate());
         return;
