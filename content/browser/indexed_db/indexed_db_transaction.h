40251f203f0886a08a30427d3751a566b480b016
jsbell@chromium.org <jsbell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-09 23:41:09 +0000

    Revert 239543 "IndexedDB: Timeout transactions if front-end is u..."
    > IndexedDB: Timeout transactions if front-end is unresponsive
    > 
    > Introduce a fixed time-out that limits how long a
    > transaction will wait after it handles the last request
    > before it gives up and aborts. This is to prevent "wedged"
    > renderers (not crashed but not turning the event loop, etc)
    > from holding up the transaction queue since they never
    > issue a another request or ask to commit.
    > 
    > (TBR for new file entry in gypi)
    > 
    > BUG=318992
    > R=dgrogam
    > TBR=jam
    > 
    > Review URL: https://codereview.chromium.org/67173010
    
    TBR=jsbell@chromium.org
    
    Review URL: https://codereview.chromium.org/101323007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239604 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/indexed_db/indexed_db_transaction.h
+++ content/browser/indexed_db/indexed_db_transaction.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/time/time.h"
-#include "base/timer/timer.h"
 #include "content/browser/indexed_db/indexed_db_backing_store.h"
 #include "content/browser/indexed_db/indexed_db_database.h"
 #include "content/browser/indexed_db/indexed_db_database_error.h"
@@ -24,18 +23,15 @@ namespace content {
 class IndexedDBCursor;
 class IndexedDBDatabaseCallbacks;
 
-class CONTENT_EXPORT IndexedDBTransaction
-    : public NON_EXPORTED_BASE(base::RefCounted<IndexedDBTransaction>) {
+class IndexedDBTransaction : public base::RefCounted<IndexedDBTransaction> {
  public:
   typedef base::Callback<void(IndexedDBTransaction*)> Operation;
 
-  IndexedDBTransaction(
-      int64 id,
-      scoped_refptr<IndexedDBDatabaseCallbacks> callbacks,
-      const std::set<int64>& object_store_ids,
-      indexed_db::TransactionMode,
-      IndexedDBDatabase* db,
-      IndexedDBBackingStore::Transaction* backing_store_transaction);
+  IndexedDBTransaction(int64 id,
+                       scoped_refptr<IndexedDBDatabaseCallbacks> callbacks,
+                       const std::set<int64>& object_store_ids,
+                       indexed_db::TransactionMode,
+                       IndexedDBDatabase* db);
 
   virtual void Abort();
   void Commit();
@@ -60,7 +56,7 @@ class CONTENT_EXPORT IndexedDBTransaction
     DCHECK_GE(pending_preemptive_events_, 0);
   }
   IndexedDBBackingStore::Transaction* BackingStoreTransaction() {
-    return transaction_.get();
+    return &transaction_;
   }
   int64 id() const { return id_; }
 
@@ -74,7 +70,6 @@ class CONTENT_EXPORT IndexedDBTransaction
   };
 
   State state() const { return state_; }
-  bool IsTimeoutTimerRunning() const { return timeout_timer_.IsRunning(); }
 
   struct Diagnostics {
     base::Time creation_time;
@@ -96,7 +91,6 @@ class CONTENT_EXPORT IndexedDBTransaction
 
   void ProcessTaskQueue();
   void CloseOpenCursors();
-  void Timeout();
 
   const int64 id_;
   const std::set<int64> object_store_ids_;
@@ -138,19 +132,13 @@ class CONTENT_EXPORT IndexedDBTransaction
   TaskQueue preemptive_task_queue_;
   TaskStack abort_task_stack_;
 
-  scoped_ptr<IndexedDBBackingStore::Transaction> transaction_;
+  IndexedDBBackingStore::Transaction transaction_;
   bool backing_store_transaction_begun_;
 
   bool should_process_queue_;
   int pending_preemptive_events_;
 
   std::set<IndexedDBCursor*> open_cursors_;
-
-  // This timer is started after requests have been processed. If no subsequent
-  // requests are processed before the timer fires, assume the script is
-  // unresponsive and abort to unblock the transaction queue.
-  base::OneShotTimer<IndexedDBTransaction> timeout_timer_;
-
   Diagnostics diagnostics_;
 };
 
