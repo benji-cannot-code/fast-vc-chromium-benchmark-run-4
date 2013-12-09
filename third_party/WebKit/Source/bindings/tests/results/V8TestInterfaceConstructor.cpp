b988e0fe631fa62d209180a085df1880f960865c
nbarth@chromium.org <nbarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-09 01:36:28 +0000

    IDL compiler: [RaisesException=Constructor]
    BUG=239771
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/105753004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163384 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/tests/results/V8TestInterfaceConstructor.cpp
+++ third_party/WebKit/Source/bindings/tests/results/V8TestInterfaceConstructor.cpp
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "RuntimeEnabledFeatures.h"
 #include "bindings/v8/ExceptionMessages.h"
+#include "bindings/v8/ExceptionState.h"
 #include "bindings/v8/V8Binding.h"
 #include "bindings/v8/V8DOMConfiguration.h"
 #include "bindings/v8/V8DOMWrapper.h"
@@ -75,8 +76,11 @@ template <typename T> void V8_USE(T) { }
 
 static void constructor(const v8::FunctionCallbackInfo<v8::Value>& info)
 {
-    RefPtr<TestInterfaceConstructor> impl = TestInterfaceConstructor::create();
+    ExceptionState exceptionState(info.Holder(), info.GetIsolate());
+    RefPtr<TestInterfaceConstructor> impl = TestInterfaceConstructor::create(exceptionState);
     v8::Handle<v8::Object> wrapper = info.Holder();
+    if (exceptionState.throwIfNeeded())
+        return;
 
     V8DOMWrapper::associateObjectWithWrapper<V8TestInterfaceConstructor>(impl.release(), &V8TestInterfaceConstructor::wrapperTypeInfo, wrapper, info.GetIsolate(), WrapperConfiguration::Dependent);
     v8SetReturnValue(info, wrapper);
