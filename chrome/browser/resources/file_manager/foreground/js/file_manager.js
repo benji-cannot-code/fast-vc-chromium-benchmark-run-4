cb17c4bb96ddd278f51223bf1e77aaf3064060d3
hirono@chromium.org <hirono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-09 09:58:57 +0000

    Files.app: Remove maybeScheduleCloseBackgroundPage_ from FileOperationManager.
    This check should be in Background class.  This CL updates the handler of file
    operation manager to request to close the Background class.
    
    BUG=325086
    TEST=close Files.app, see background page in the inspector.
    
    Review URL: https://codereview.chromium.org/107883002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239458 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/foreground/js/file_manager.js
+++ chrome/browser/resources/file_manager/foreground/js/file_manager.js
@@ -2314,7 +2314,7 @@ var BOTTOM_MARGIN_FOR_PREVIEW_PANEL_PX = 52;
     }
     window.closing = true;
     if (this.backgroundPage_ && util.platform.runningInBrowser())
-      this.backgroundPage_.maybeCloseBackgroundPage();
+      this.backgroundPage_.background.tryClose();
   };
 
   FileManager.prototype.initiateRename = function() {
