76af2d757cd5fc5e4e827ff8e953a4dacc05c038
dsinclair@chromium.org <dsinclair@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-09 21:58:27 +0000

    Split out repaint code from RenderBlockFlow layout.
    Currently the RenderBlockFlow layout code has a FIXME to extract some repaint
    logic into a helper function. This extraction is needed for the repaintAfterLayout
    work because we need to be able to call it when layout is complete.
    
    This CL pulls the code out into a method defined in RenderObject and overridden
    in RenderBlockFlow.
    
    BUG=310398
    
    Review URL: https://codereview.chromium.org/110543002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163460 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderBlockFlow.h
+++ third_party/WebKit/Source/core/rendering/RenderBlockFlow.h
@@ -228,6 +228,7 @@ private:
 
     virtual void moveAllChildrenIncludingFloatsTo(RenderBlock* toBlock, bool fullRemoveInsert) OVERRIDE;
     virtual void repaintOverhangingFloats(bool paintAllDescendants) OVERRIDE FINAL;
+    virtual void repaintOverflow() OVERRIDE;
     virtual void paintFloats(PaintInfo&, const LayoutPoint&, bool preservePhase = false) OVERRIDE FINAL;
     virtual void clipOutFloatingObjects(RenderBlock*, const PaintInfo*, const LayoutPoint&, const LayoutSize&) OVERRIDE;
     void clearFloats(EClear);
@@ -388,6 +389,9 @@ private:
     void createRenderNamedFlowFragmentIfNeeded();
 
     RenderBlockFlowRareData& ensureRareData();
+
+    LayoutUnit m_repaintLogicalTop;
+    LayoutUnit m_repaintLogicalBottom;
 
 protected:
     OwnPtr<RenderBlockFlowRareData> m_rareData;
--- third_party/WebKit/Source/core/rendering/RenderObject.cpp
+++ third_party/WebKit/Source/core/rendering/RenderObject.cpp
@@ -1585,6 +1585,10 @@ bool RenderObject::repaintAfterLayoutIfNeeded(const RenderLayerModelObject* repa
     return false;
 }
 
+void RenderObject::repaintOverflow()
+{
+}
+
 bool RenderObject::checkForRepaintDuringLayout() const
 {
     return !document().view()->needsFullRepaint() && !hasLayer() && everHadLayout();
