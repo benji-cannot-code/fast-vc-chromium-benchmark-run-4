f111a248c37be43622cd4c5443a08f2d70774e1b
stevenjb@chromium.org <stevenjb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-10 08:49:12 +0000

    ash_system_tray_delegate -> system_tray_delegate_chromeos
    BUG=311373
    
    Review URL: https://codereview.chromium.org/110933002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239730 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/ash/system_tray_delegate_chromeos.cc
+++ chrome/browser/ui/ash/system_tray_delegate_chromeos.cc
@@ -1,9 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright 2013 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/chromeos/system/ash_system_tray_delegate.h"
+#include "chrome/browser/ui/ash/system_tray_delegate_chromeos.h"
 
 #include <algorithm>
 #include <set>
@@ -440,7 +440,8 @@ class SystemTrayDelegate : public ash::SystemTrayDelegate,
             chromeos::UserManager::Get()->GetActiveUser()->email());
   }
 
-  virtual const base::string16 GetLocallyManagedUserManagerName() const OVERRIDE {
+  virtual const base::string16 GetLocallyManagedUserManagerName()
+      const OVERRIDE {
     if (GetUserLoginStatus() != ash::user::LOGGED_IN_LOCALLY_MANAGED)
       return base::string16();
     return UserManager::Get()->GetSupervisedUserManager()->
