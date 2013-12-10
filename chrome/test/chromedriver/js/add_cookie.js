12608c3fabd2d053b2428fc915ed2552fd81f9c3
chrisgao@chromium.org <chrisgao@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-10 23:15:47 +0000

    [chromedriver] Fix a regression: cookie domain getting prefixed with a ".".
    BUG=chromedriver:507
    
    Review URL: https://codereview.chromium.org/107273003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239906 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/chromedriver/js/add_cookie.js
+++ chrome/test/chromedriver/js/add_cookie.js
@@ -83,7 +83,8 @@ function addCookie(cookie) {
 
   var newCookie = name + '=' + value;
   newCookie += '; path=' + (path || '/');
-  newCookie += '; domain=' + (domain || document.domain);
+  if (domain)
+    newCookie += '; domain=' + domain;
   if (cookie['expiry']) {
     var expiredDate = new Date(cookie['expiry'] * 1000);
     newCookie += '; expires=' + expiredDate.toUTCString();
