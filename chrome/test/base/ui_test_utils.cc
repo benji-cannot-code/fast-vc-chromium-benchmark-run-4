2d3a07d2d8279b972d2e6f6ad5b0f3bf0fa79396
lushnikov@chromium.org <lushnikov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-10 22:19:48 +0000

    DevTools: fix flaky DevToolsBeforeUnloadTest
    This CL fixes flakiness in devtools beforeunload tests.
    It was caused by the fact that hang timeout for renderer during processing beforeunload events was set to only 1 second, which was not enough from time to time.
    
    Drive-by: ui_test_utils::WaitForAppModalDialog should check
    for any existing modal before installing an observer (otherwise, it might wait forever)
    
    BUG=323847,321673
    
    Review URL: https://codereview.chromium.org/108153003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239868 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/base/ui_test_utils.cc
+++ chrome/test/base/ui_test_utils.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/search_engines/template_url_service_test_util.h"
 #include "chrome/browser/thumbnails/render_widget_snapshot_taker.h"
 #include "chrome/browser/ui/app_modal_dialogs/app_modal_dialog.h"
+#include "chrome/browser/ui/app_modal_dialogs/app_modal_dialog_queue.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_finder.h"
@@ -322,6 +323,10 @@ bool GetRelativeBuildDirectory(base::FilePath* build_dir) {
 }
 
 AppModalDialog* WaitForAppModalDialog() {
+  AppModalDialogQueue* dialog_queue = AppModalDialogQueue::GetInstance();
+  if (dialog_queue->HasActiveDialog())
+    return dialog_queue->active_dialog();
+
   content::WindowedNotificationObserver observer(
       chrome::NOTIFICATION_APP_MODAL_DIALOG_SHOWN,
       content::NotificationService::AllSources());
