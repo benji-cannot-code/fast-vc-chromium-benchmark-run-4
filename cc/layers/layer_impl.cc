503903b5ea0f04bb0efd23b1415440259e4b8c53
vivek.vg@samsung.com <vivek.vg@samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-10 05:48:48 +0000

    Use bit fields inside cc to reduce memory usage.
    Using bitfields we get the memory usage as follows
    
    +-----+---------------------------+------------------------+---------+
    |.No..|.Class/Structure/File......|.Size.of.object.(bytes).|.Memory..|
    |.....|...........................+------------------------+.Reduced.|
    |.....|...........................|...Before..|...After....|.........|
    +-----+---------------------------+-----------+------------+---------+
    |..1..|.cc::Layer.................|......840..|.....824....|......16.|
    +-----+---------------------------+-----------+------------+---------+
    |..2..|.cc::LayerImpl.............|......880..|.....864....|......16.|
    +-----+---------------------------+-----------+------------+---------+
    |..3..|.cc::RenderSurfaceImpl.....|......408..|.....400....|.......8.|
    +-----+---------------------------+-----------+------------+---------+
    
    Review URL: https://codereview.chromium.org/102733006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239682 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/layers/layer_impl.cc
+++ cc/layers/layer_impl.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/rect_conversions.h"
 
 namespace cc {
-
 LayerImpl::LayerImpl(LayerTreeImpl* tree_impl, int id)
     : parent_(NULL),
       scroll_parent_(NULL),
@@ -48,14 +47,11 @@ LayerImpl::LayerImpl(LayerTreeImpl* tree_impl, int id)
       have_wheel_event_handlers_(false),
       user_scrollable_horizontal_(true),
       user_scrollable_vertical_(true),
-      background_color_(0),
       stacking_order_changed_(false),
       double_sided_(true),
       layer_property_changed_(false),
       masks_to_bounds_(false),
       contents_opaque_(false),
-      opacity_(1.0),
-      blend_mode_(SkXfermode::kSrcOver_Mode),
       is_root_for_isolated_group_(false),
       preserves_3d_(false),
       use_parent_backface_visibility_(false),
@@ -64,6 +60,9 @@ LayerImpl::LayerImpl(LayerTreeImpl* tree_impl, int id)
       hide_layer_and_subtree_(false),
       force_render_surface_(false),
       is_container_for_fixed_position_layers_(false),
+      background_color_(0),
+      opacity_(1.0),
+      blend_mode_(SkXfermode::kSrcOver_Mode),
       draw_depth_(0.f),
       compositing_reasons_(kCompositingReasonUnknown),
       current_draw_mode_(DRAW_MODE_NONE),
