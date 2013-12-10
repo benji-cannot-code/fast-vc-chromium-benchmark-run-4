2b669e72de0f3d0efdb939b5834afb72665452a4
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-10 14:33:53 +0000

    Revert 239778 "[Drive] Refine status code of FakeDriveService::R..."
    > [Drive] Refine status code of FakeDriveService::RemoveResourceFromDirectory
    > 
    > The server returns HTTP_NO_CONTENT rather than HTTP_SUCCESS.
    > 
    > BUG=327228
    > NOTRY=true
    > 
    > Review URL: https://codereview.chromium.org/110833003
    
    TBR=tzik@chromium.org
    
    Review URL: https://codereview.chromium.org/106433003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239781 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_backend/conflict_resolver.cc
+++ chrome/browser/sync_file_system/drive_backend/conflict_resolver.cc
@@ -110,9 +110,8 @@ void ConflictResolver::DetachFromNonPrimaryParents(
 
 void ConflictResolver::DidDetachFromParent(const SyncStatusCallback& callback,
                                            google_apis::GDataErrorCode error) {
-  SyncStatusCode status = GDataErrorCodeToSyncStatusCode(error);
-  if (status != SYNC_STATUS_OK) {
-    callback.Run(status);
+  if (error != google_apis::HTTP_SUCCESS) {
+    callback.Run(GDataErrorCodeToSyncStatusCode(error));
     return;
   }
 
