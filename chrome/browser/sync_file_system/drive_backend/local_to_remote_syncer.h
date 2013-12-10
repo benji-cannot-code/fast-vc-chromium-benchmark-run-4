5ed31200164d1e60c2c5e9761225025e2d3ca0f5
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-10 07:58:24 +0000

    [SyncFS] Split out folder creation sequence to separate class
    Preparation of app-root create-create conflict handling to share the implementation.
    
    BUG=240165
    R=kinuko@chromium.org, nhiroki@chromium.org
    
    Review URL: https://codereview.chromium.org/110113004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239711 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_backend/local_to_remote_syncer.h
+++ chrome/browser/sync_file_system/drive_backend/local_to_remote_syncer.h
@@ -33,6 +33,7 @@ class RemoteChangeProcessor;
 namespace drive_backend {
 
 class FileTracker;
+class FolderCreator;
 class MetadataDatabase;
 class SyncEngineContext;
 
@@ -88,16 +89,8 @@ class LocalToRemoteSyncer : public SyncTask {
 
   void CreateRemoteFolder(const SyncStatusCallback& callback);
   void DidCreateRemoteFolder(const SyncStatusCallback& callback,
-                             google_apis::GDataErrorCode error,
-                             scoped_ptr<google_apis::ResourceEntry> entry);
-  void DidListFolderForEnsureUniqueness(
-      const SyncStatusCallback& callback,
-      ScopedVector<google_apis::ResourceEntry> candidates,
-      google_apis::GDataErrorCode error,
-      scoped_ptr<google_apis::ResourceList> resource_list);
-  void DidUpdateDatabaseForCreateNewFolder(const SyncStatusCallback& callback,
-                                           const std::string& file_id,
-                                           SyncStatusCode status);
+                             const std::string& file_id,
+                             SyncStatusCode status);
   void DidDetachResourceForCreationConflict(const SyncStatusCallback& callback,
                                             google_apis::GDataErrorCode error);
 
@@ -116,6 +109,8 @@ class LocalToRemoteSyncer : public SyncTask {
   scoped_ptr<FileTracker> remote_file_tracker_;
   scoped_ptr<FileTracker> remote_parent_folder_tracker_;
   base::FilePath target_path_;
+
+  scoped_ptr<FolderCreator> folder_creator_;
 
   base::WeakPtrFactory<LocalToRemoteSyncer> weak_ptr_factory_;
 
