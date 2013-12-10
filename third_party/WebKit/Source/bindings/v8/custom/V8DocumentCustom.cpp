6ed1a3d482c35ce480dde91f786bffdf8daf635b
sigbjornf@opera.com <sigbjornf@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-10 12:49:57 +0000

    Have document.evaluate() report TypeError over incorrect resolvers.
    Switch from reporting TypeMismatchError to TypeError to align with
    other implementations (and WebIDL) when faced with resolver arguments
    that do not have a function type.
    
    To help format the error message about an incorrectly typed argument,
    introduced ExceptionMessages::incorrectArgumentType().
    
    R=
    BUG=270033
    
    Review URL: https://codereview.chromium.org/111533002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163533 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/custom/V8DocumentCustom.cpp
+++ third_party/WebKit/Source/bindings/v8/custom/V8DocumentCustom.cpp
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "V8WebGLRenderingContext.h"
 #include "V8XPathNSResolver.h"
 #include "V8XPathResult.h"
+#include "bindings/v8/ExceptionMessages.h"
 #include "bindings/v8/ExceptionState.h"
 #include "bindings/v8/ScriptController.h"
 #include "bindings/v8/V8Binding.h"
@@ -67,9 +68,11 @@ void V8Document::evaluateMethodCustom(const v8::FunctionCallbackInfo<v8::Value>&
     if (V8Node::hasInstance(info[1], info.GetIsolate(), worldType(info.GetIsolate())))
         contextNode = V8Node::toNative(v8::Handle<v8::Object>::Cast(info[1]));
 
-    RefPtr<XPathNSResolver> resolver = toXPathNSResolver(info[2], info.GetIsolate());
-    if (!resolver && !info[2]->IsNull() && !info[2]->IsUndefined()) {
-        setDOMException(TypeMismatchError, info.GetIsolate());
+    const int resolverArgumentIndex = 2;
+    RefPtr<XPathNSResolver> resolver = toXPathNSResolver(info[resolverArgumentIndex], info.GetIsolate());
+    if (!resolver && !isUndefinedOrNull(info[resolverArgumentIndex])) {
+        exceptionState.throwTypeError(ExceptionMessages::incorrectArgumentType(resolverArgumentIndex + 1, "is not a resolver function."));
+        exceptionState.throwIfNeeded();
         return;
     }
 
