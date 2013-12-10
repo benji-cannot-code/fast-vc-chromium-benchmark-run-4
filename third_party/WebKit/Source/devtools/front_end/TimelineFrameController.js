a6777440345d3531df0f42dd4ccba383e0de9754
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-10 15:26:49 +0000

    DevTools: Timeline: move overview control management out of TimelineOverviewPanel
    TimelinePanel manages presentation mode, so it has to assign proper Overview to TimelineOverviewPanel.
    
    BUG=
    R=pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/100483005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163539 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/TimelineFrameController.js
+++ third_party/WebKit/Source/devtools/front_end/TimelineFrameController.js
@@ -32,22 +32,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /**
  * @constructor
  * @param {!WebInspector.TimelineModel} model
- * @param {!WebInspector.TimelineOverviewPane} overviewPane
+ * @param {!WebInspector.TimelineFrameOverview} frameOverview
  * @param {!WebInspector.TimelinePresentationModel} presentationModel
  */
-WebInspector.TimelineFrameController = function(model, overviewPane, presentationModel)
+WebInspector.TimelineFrameController = function(model, frameOverview, presentationModel)
 {
     this._lastMainThreadFrame = null;
     this._lastBackgroundFrame = null;
     this._model = model;
-    this._overviewPane = overviewPane;
+    this._frameOverview = frameOverview;
     this._presentationModel = presentationModel;
     this._model.addEventListener(WebInspector.TimelineModel.Events.RecordAdded, this._onRecordAdded, this);
     this._model.addEventListener(WebInspector.TimelineModel.Events.RecordsCleared, this._onRecordsCleared, this);
 
+    this._frameOverview.reset();
     var records = model.records;
     for (var i = 0; i < records.length; ++i)
         this._addRecord(records[i]);
+    this._frameOverview.update();
 }
 
 WebInspector.TimelineFrameController.prototype = {
@@ -120,7 +122,7 @@ WebInspector.TimelineFrameController.prototype = {
         // Alternatively, we could compute CPU time as sum of all Program events.
         // This way it's a bit more flexible, as it works in case there's no program events.
         frame.cpuTime += frame.timeByCategory["other"];
-        this._overviewPane.addFrame(frame);
+        this._frameOverview.addFrame(frame);
         this._presentationModel.addFrame(frame);
     },
 
