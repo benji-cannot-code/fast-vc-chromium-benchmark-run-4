cf857a7d8704b202272223a3fe0c39220cb95812
tapted@chromium.org <tapted@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-10 10:02:22 +0000

    Remove an include of views/../textfield.h getting compiled on Mac.
    This is pulling in a bunch of views headers which don't seem to be
    required in this file. Noticed some bots reporting compile errors for
    mac trybots due to this.
    
    Appeared by accident in r185787 (code was only being removed from this
    file in that revision).
    
    BUG=None
    TBR=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/108913005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239747 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_keyevents_browsertest.cc
+++ chrome/browser/browser_keyevents_browsertest.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test_utils.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "ui/events/keycodes/keyboard_codes.h"
-#include "ui/views/controls/textfield/textfield.h"
 
 // TODO(kbr): remove: http://crbug.com/222296
 #if defined(OS_MACOSX)
