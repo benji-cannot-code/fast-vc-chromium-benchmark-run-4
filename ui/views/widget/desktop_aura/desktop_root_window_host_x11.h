c3ccfaa464da5e116d4ced1ce4d62f75a9ab4314
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-10 05:24:49 +0000

    Re-apply custom shape in DesktopRootWindowHostX11::ResetWindowShape.
    This prevents custom-shaped windows being restored to the default shape on e.g. focus changes.
    
    BUG=322361
    
    Review URL: https://codereview.chromium.org/109673002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239667 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/desktop_aura/desktop_root_window_host_x11.h
+++ ui/views/widget/desktop_aura/desktop_root_window_host_x11.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_VIEWS_WIDGET_DESKTOP_AURA_DESKTOP_ROOT_WINDOW_HOST_X11_H_
 #define UI_VIEWS_WIDGET_DESKTOP_AURA_DESKTOP_ROOT_WINDOW_HOST_X11_H_
 
+#include <X11/extensions/shape.h>
 #include <X11/Xlib.h>
 
 // Get rid of a macro from Xlib.h that conflicts with Aura's RootWindow class.
@@ -266,6 +267,9 @@ private:
   std::set<DesktopRootWindowHostX11*> window_children_;
 
   ObserverList<DesktopRootWindowHostObserverX11> observer_list_;
+
+  // Copy of custom window shape specified via SetShape(), if any.
+  ::Region custom_window_shape_;
 
   // The current root window host that has capture. While X11 has something
   // like Windows SetCapture()/ReleaseCapture(), it is entirely implicit and
