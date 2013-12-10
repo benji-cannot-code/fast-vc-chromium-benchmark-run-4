f0683c65c0a9d2014547235f93c016409636d432
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-10 07:10:35 +0000

    Don't check lfQuality in LOGFONT as it has no effect on rendering
    Remove the LOGFONT lfQuality check in computePaintTextFlags and instead
    base the textFlags entierly on the system ClearType settings (and
    whether a font is a web font). Skia will then use this information to
    pick the appropriate type of font smoothing to apply (if any).
    
    BUG=249099
    R=bungeman@chromium.org
    
    Review URL: https://codereview.chromium.org/99333013
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163507 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/fonts/win/FontPlatformDataWin.cpp
+++ third_party/WebKit/Source/platform/fonts/win/FontPlatformDataWin.cpp
@@ -86,7 +86,7 @@ void FontPlatformData::setupPaint(SkPaint* paint, GraphicsContext* context) cons
 // Lookup the current system settings for font smoothing.
 // We cache these values for performance, but if the browser has a way to be
 // notified when these change, we could re-query them at that time.
-static uint32_t getDefaultGDITextFlags()
+static uint32_t getSystemTextFlags()
 {
     static bool gInited;
     static uint32_t gFlags;
@@ -107,52 +107,29 @@ static uint32_t getDefaultGDITextFlags()
     return gFlags;
 }
 
-static bool isWebFont(const LOGFONT& lf)
+static bool isWebFont(const String& familyName)
 {
-    // web-fonts have artifical names constructed to always be
+    // Web-fonts have artifical names constructed to always be:
     // 1. 24 characters, followed by a '\0'
     // 2. the last two characters are '=='
-    return '=' == lf.lfFaceName[22] && '=' == lf.lfFaceName[23] && '\0' == lf.lfFaceName[24];
+    return familyName.length() == 24
+        && '=' == familyName[22] && '=' == familyName[23];
 }
 
-static int computePaintTextFlags(const LOGFONT& lf)
+int FontPlatformData::paintTextFlags() const
 {
-    int textFlags = 0;
-    switch (lf.lfQuality) {
-    case NONANTIALIASED_QUALITY:
-        textFlags = 0;
-        break;
-    case ANTIALIASED_QUALITY:
-        textFlags = SkPaint::kAntiAlias_Flag;
-        break;
-    case CLEARTYPE_QUALITY:
-        textFlags = (SkPaint::kAntiAlias_Flag | SkPaint::kLCDRenderText_Flag);
-        break;
-    default:
-        textFlags = getDefaultGDITextFlags();
-        break;
-    }
+    int textFlags = getSystemTextFlags();
 
-    // only allow features that SystemParametersInfo allows
-    textFlags &= getDefaultGDITextFlags();
-
-    /*
-     *  FontPlatformData(...) will read our logfont, and try to honor the the lfQuality
-     *  setting (computing the corresponding SkPaint flags for AA and LCD). However, it
-     *  will limit the quality based on its query of SPI_GETFONTSMOOTHING. This could mean
-     *  we end up drawing the text in BW, even though our lfQuality requested antialiasing.
-     *
-     *  Many web-fonts are so poorly hinted that they are terrible to read when drawn in BW.
-     *  In these cases, we have decided to FORCE these fonts to be drawn with at least grayscale AA,
-     *  even when the System (getDefaultGDITextFlags) tells us to draw only in BW.
-     */
-    if (isWebFont(lf) && !isRunningLayoutTest())
+    // Many web-fonts are so poorly hinted that they are terrible to read when drawn in BW.
+    // In these cases, we have decided to FORCE these fonts to be drawn with at least grayscale AA,
+    // even when the System (getSystemTextFlags) tells us to draw only in BW.
+    if (isWebFont(fontFamilyName()) && !isRunningLayoutTest())
         textFlags |= SkPaint::kAntiAlias_Flag;
     return textFlags;
 }
 
 #if !USE(HARFBUZZ)
-PassRefPtr<SkTypeface> CreateTypefaceFromHFont(HFONT hfont, int* size, int* paintTextFlags)
+PassRefPtr<SkTypeface> CreateTypefaceFromHFont(HFONT hfont, int* size)
 {
     LOGFONT info;
     GetObject(hfont, sizeof(info), &info);
@@ -162,8 +139,6 @@ PassRefPtr<SkTypeface> CreateTypefaceFromHFont(HFONT hfont, int* size, int* pain
             height = -height;
         *size = height;
     }
-    if (paintTextFlags)
-        *paintTextFlags = computePaintTextFlags(info);
     return adoptRef(SkCreateTypefaceFromLOGFONT(info));
 }
 #endif
@@ -174,7 +149,6 @@ FontPlatformData::FontPlatformData(WTF::HashTableDeletedValueType)
     , m_fakeItalic(false)
     , m_orientation(Horizontal)
     , m_typeface(adoptRef(SkTypeface::RefDefault()))
-    , m_paintTextFlags(0)
     , m_isHashTableDeletedValue(true)
     , m_useSubpixelPositioning(false)
 {
@@ -190,7 +164,6 @@ FontPlatformData::FontPlatformData()
     , m_fakeItalic(false)
     , m_orientation(Horizontal)
     , m_typeface(adoptRef(SkTypeface::RefDefault()))
-    , m_paintTextFlags(0)
     , m_isHashTableDeletedValue(false)
     , m_useSubpixelPositioning(false)
 {
@@ -208,7 +181,7 @@ FontPlatformData::FontPlatformData(HFONT font, float size, FontOrientation orien
     , m_fakeItalic(false)
     , m_orientation(orientation)
     , m_scriptCache(0)
-    , m_typeface(CreateTypefaceFromHFont(font, 0, &m_paintTextFlags))
+    , m_typeface(CreateTypefaceFromHFont(font, 0))
     , m_isHashTableDeletedValue(false)
     , m_useSubpixelPositioning(false)
 {
@@ -222,7 +195,6 @@ FontPlatformData::FontPlatformData(float size, bool bold, bool oblique)
     , m_fakeItalic(false)
     , m_orientation(Horizontal)
     , m_typeface(adoptRef(SkTypeface::RefDefault()))
-    , m_paintTextFlags(0)
     , m_isHashTableDeletedValue(false)
     , m_useSubpixelPositioning(false)
 {
@@ -238,7 +210,6 @@ FontPlatformData::FontPlatformData(const FontPlatformData& data)
     , m_fakeItalic(data.m_fakeItalic)
     , m_orientation(data.m_orientation)
     , m_typeface(data.m_typeface)
-    , m_paintTextFlags(data.m_paintTextFlags)
     , m_isHashTableDeletedValue(false)
     , m_useSubpixelPositioning(data.m_useSubpixelPositioning)
 {
@@ -254,7 +225,6 @@ FontPlatformData::FontPlatformData(const FontPlatformData& data, float textSize)
     , m_fakeItalic(data.m_fakeItalic)
     , m_orientation(data.m_orientation)
     , m_typeface(data.m_typeface)
-    , m_paintTextFlags(data.m_paintTextFlags)
     , m_isHashTableDeletedValue(false)
     , m_useSubpixelPositioning(data.m_useSubpixelPositioning)
 {
@@ -280,7 +250,6 @@ FontPlatformData::FontPlatformData(PassRefPtr<SkTypeface> tf, const char* family
     LOGFONT logFont;
     SkLOGFONTFromTypeface(m_typeface.get(), &logFont);
     logFont.lfHeight = -textSize;
-    m_paintTextFlags = computePaintTextFlags(logFont);
 
 #if !USE(HARFBUZZ)
     HFONT hFont = CreateFontIndirect(&logFont);
@@ -297,7 +266,6 @@ FontPlatformData& FontPlatformData::operator=(const FontPlatformData& data)
         m_fakeItalic = data.m_fakeItalic;
         m_orientation = data.m_orientation;
         m_typeface = data.m_typeface;
-        m_paintTextFlags = data.m_paintTextFlags;
 
 #if !USE(HARFBUZZ)
         m_font = data.m_font;
