eb13def4e72b45b44ffa5b0d6824e8f31302b8f5
tkent@chromium.org <tkent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-10 08:36:10 +0000

    Settings: Can't activate URL fields in 'Default search settings' section.
    This is a regression by r239172.
    
    Bug detail:
    These URL fields are input[disabled], which are not focusable. After r239172, a
    static version of a URL field gets focus by mousedown, and we try to move focus
    to the URL field later. But the URL field is not focusable and we loses
    focus. This behavior worked before r239172 because the owner <list> got focus by
    mousedown.
    
    Fix:
    We focus on the owner <list> explicity in such case. It's same as r239171 or
    earlier.
    Also, this CL fixes a disabled element issue in containsFocusableElement().
    This doesn't affect any behavior, and it just improves correctness.
    
    BUG=326863
    
    Review URL: https://codereview.chromium.org/109943002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239726 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/webui/resources/js/cr/ui/list.js
+++ ui/webui/resources/js/cr/ui/list.js
@@ -1315,7 +1315,7 @@ cr.define('cr.ui', function() {
   function containsFocusableElement(start, root) {
     for (var element = start; element && element != root;
         element = element.parentElement) {
-      if (element.tabIndex >= 0)
+      if (element.tabIndex >= 0 && !element.disabled)
         return true;
     }
     return false;
