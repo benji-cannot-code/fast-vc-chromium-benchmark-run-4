562d28da2aaa08d871a1d7a798db1cc03ea430a8
sgurun@chromium.org <sgurun@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-10 04:33:53 +0000

    Rather than using a throttle, we are now using HandleNavigation callback for implementing ShouldOverrideUrlLoading for compatibility with old Webview.
    BUG=308257
    
    Review URL: https://codereview.chromium.org/24228003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239646 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/java/src/org/chromium/android_webview/AwContentsClientBridge.java
+++ android_webview/java/src/org/chromium/android_webview/AwContentsClientBridge.java
@@ -92,6 +92,11 @@ public class AwContentsClientBridge {
         mClient.handleJsBeforeUnload(url, message, handler);
     }
 
+    @CalledByNative
+    private boolean shouldOverrideUrlLoading(String url) {
+        return mClient.shouldOverrideUrlLoading(url);
+    }
+
     void confirmJsResult(int id, String prompt) {
         if (mNativeContentsClientBridge == 0) return;
         nativeConfirmJsResult(mNativeContentsClientBridge, id, prompt);
