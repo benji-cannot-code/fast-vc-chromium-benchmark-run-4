c9a6ce312c15d782ac30d98419423db6d675d03c
aandrey@chromium.org <aandrey@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-10 18:42:37 +0000

    DevTools: Track async stacks on the corresponding execution context.
    We should track async stacks separately for each execution context,
    so that async event IDs (like timer ID) from different execution
    contexts (e.g. iframes) should not mess with each other.
    
    BUG=272416
    R=yurys, pfeldman
    
    Review URL: https://codereview.chromium.org/111653002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163553 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/AsyncCallStackTracker.h
+++ third_party/WebKit/Source/core/inspector/AsyncCallStackTracker.h
@@ -42,6 +42,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+class ExecutionContext;
+
 class AsyncCallStackTracker {
     WTF_MAKE_NONCOPYABLE(AsyncCallStackTracker);
 public:
@@ -72,13 +74,13 @@ public:
     void setAsyncCallStackDepth(int);
     const AsyncCallChain* currentAsyncCallChain() const;
 
-    void didInstallTimer(int timerId, bool singleShot, const ScriptValue& callFrames);
-    void didRemoveTimer(int timerId);
-    void willFireTimer(int timerId);
+    void didInstallTimer(ExecutionContext*, int timerId, bool singleShot, const ScriptValue& callFrames);
+    void didRemoveTimer(ExecutionContext*, int timerId);
+    void willFireTimer(ExecutionContext*, int timerId);
 
-    void didRequestAnimationFrame(int callbackId, const ScriptValue& callFrames);
-    void didCancelAnimationFrame(int callbackId);
-    void willFireAnimationFrame(int callbackId);
+    void didRequestAnimationFrame(ExecutionContext*, int callbackId, const ScriptValue& callFrames);
+    void didCancelAnimationFrame(ExecutionContext*, int callbackId);
+    void willFireAnimationFrame(ExecutionContext*, int callbackId);
 
     void didFireAsyncCall();
     void clear();
@@ -88,11 +90,13 @@ private:
     static void ensureMaxAsyncCallChainDepth(AsyncCallChain*, unsigned);
     static bool validateCallFrames(const ScriptValue& callFrames);
 
+    class ExecutionContextData;
+    void contextDestroyed(ExecutionContext*);
+    ExecutionContextData* createContextDataIfNeeded(ExecutionContext*);
+
     unsigned m_maxAsyncCallStackDepth;
     RefPtr<AsyncCallChain> m_currentAsyncCallChain;
-    HashSet<int> m_intervalTimerIds;
-    HashMap<int, RefPtr<AsyncCallChain> > m_timerCallChains;
-    HashMap<int, RefPtr<AsyncCallChain> > m_animationFrameCallChains;
+    HashMap<ExecutionContext*, ExecutionContextData*> m_executionContextDataMap;
 };
 
 } // namespace WebCore
