63d4c55329e0beee3bf136afc8223e830287af84
ygorshenin@chromium.org <ygorshenin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-10 20:53:29 +0000

    [Multi-Profiles] Fixed image selection for two users in multi-profile mode.
    BUG=313303
    TEST=manual tests on Linux ChromeOS build
    
    Review URL: https://codereview.chromium.org/108173004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239841 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options/chromeos/change_picture_options_handler.h
+++ chrome/browser/ui/webui/options/chromeos/change_picture_options_handler.h
@@ -20,6 +20,9 @@ class ListValue;
 }
 
 namespace chromeos {
+
+class User;
+
 namespace options {
 
 // ChromeOS user image options page UI handler.
@@ -105,6 +108,10 @@ class ChangePictureOptionsHandler : public ::options::OptionsPageUIHandler,
   virtual void OnImageDecoded(const ImageDecoder* decoder,
                               const SkBitmap& decoded_image) OVERRIDE;
   virtual void OnDecodeImageFailed(const ImageDecoder* decoder) OVERRIDE;
+
+  // Returns user related to current WebUI. If this user doesn't exist,
+  // returns active user.
+  User* GetUser() const;
 
   scoped_refptr<ui::SelectFileDialog> select_file_dialog_;
 
