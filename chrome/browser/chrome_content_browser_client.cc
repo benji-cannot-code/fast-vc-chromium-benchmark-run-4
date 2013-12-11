340b455c396bb384be9f0852df58a5bb5095acf4
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-11 23:43:31 +0000

    Remove some dead code in ChromeRenderViewObserver related to a method on WebPermissionClient that doesn't exist anymore.
    BUG=304341
    R=nasko@chromium.org
    
    Review URL: https://codereview.chromium.org/113063006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240215 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -1504,9 +1504,6 @@ void ChromeContentBrowserClient::AppendExtraCommandLineSwitches(
 #endif
     }
 
-    if (message_center::IsRichNotificationEnabled())
-      command_line->AppendSwitch(switches::kDisableHTMLNotifications);
-
     // Please keep this in alphabetical order.
     static const char* const kSwitchNames[] = {
       autofill::switches::kDisableInteractiveAutocomplete,
