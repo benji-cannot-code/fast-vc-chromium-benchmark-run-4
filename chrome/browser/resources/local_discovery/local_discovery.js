a2b829812596fdcc977473cb94264d68cc942987
noamsml@chromium.org <noamsml@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-11 01:38:53 +0000

    Make print preview register promo go directly to overlay in devices page
    Link the register button in the print preview register promo straight to the
    register overlay for the device. Also close the devices page once registration
    is successfully finished.
    
    BUG=326718
    
    Review URL: https://codereview.chromium.org/99793003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239938 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/local_discovery/local_discovery.js
+++ chrome/browser/resources/local_discovery/local_discovery.js
@@ -52,6 +52,12 @@ cr.define('local_discovery', function() {
   var isUserLoggedIn = true;
 
   /**
+   * Whether or not the path-based dialog has been shown.
+   * @type bool
+   */
+  var dialogFromPathHasBeenShown = false;
+
+  /**
    * Focus manager for page.
    */
   var focusManager = null;
@@ -301,6 +307,11 @@ cr.define('local_discovery', function() {
         devices[name] = new Device(info, isUserLoggedIn);
         devices[name].renderDevice();
       }
+
+      if (name == getOverlayIDFromPath() && !dialogFromPathHasBeenShown) {
+        dialogFromPathHasBeenShown = true;
+        devices[name].showRegister();
+      }
     } else {
       if (devices.hasOwnProperty(name)) {
         devices[name].removeDevice();
@@ -389,6 +400,11 @@ cr.define('local_discovery', function() {
    */
   function onRegistrationSuccess(device_data) {
     hideRegisterOverlay();
+
+    if (device_data.service_name == getOverlayIDFromPath()) {
+      window.close();
+    }
+
     var deviceDOM = createCloudDeviceDOM(device_data);
     $('cloud-devices').insertBefore(deviceDOM, $('cloud-devices').firstChild);
     recordUmaEvent(DEVICES_PAGE_EVENTS.REGISTER_SUCCESS);
@@ -480,6 +496,7 @@ cr.define('local_discovery', function() {
       $('cloud-devices-loading').hidden = true;
       $('cloud-devices-unavailable').hidden = true;
       clearElement($('cloud-devices'));
+      hideRegisterOverlay();
     }
 
     updateUIToReflectState();
@@ -544,6 +561,12 @@ cr.define('local_discovery', function() {
      }
   }
 
+  function getOverlayIDFromPath() {
+    if (document.location.pathname == '/register') {
+      var params = parseQueryParams(document.location);
+      return params['id'] || null;
+    }
+  }
 
   document.addEventListener('DOMContentLoaded', function() {
     cr.ui.overlay.setupOverlay($('overlay'));
@@ -572,7 +595,6 @@ cr.define('local_discovery', function() {
 
     updateVisibility();
     document.addEventListener('visibilitychange', updateVisibility, false);
-
 
     focusManager = new LocalDiscoveryFocusManager();
     focusManager.initialize();
