c7ca0a881f6b716267819dd3c6b443e1f70beeff
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-11 02:50:18 +0000

    Add some code to make views_examples_exe more interesting.
    At least now it will stay alive, until you get tired and hit Ctrl+C :)
    
    BUG=119759,139313,141187
    TEST=views_examples_exe runs and does not crash.
    R=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/110893002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239978 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/examples/examples_main.cc
+++ ui/views/examples/examples_main.cc
@@ -3,6 +3,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/at_exit.h"
+#include "base/command_line.h"
+#include "base/message_loop/message_loop.h"
+#include "base/run_loop.h"
+
 int main(int argc, char** argv) {
+  base::AtExitManager at_exit;
+  CommandLine::Init(argc, argv);
+
+  base::MessageLoop message_loop(base::MessageLoop::TYPE_UI);
+
+  base::RunLoop().Run();
+
   return 0;
 }
--- ui/views/views.gyp
+++ ui/views/views.gyp
@@ -940,6 +940,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     {
       'target_name': 'views_examples_exe',
       'type': 'executable',
+      'dependencies': [
+        '../../base/base.gyp:base',
+      ],
       'sources': [
         'examples/examples_main.cc',
       ],
