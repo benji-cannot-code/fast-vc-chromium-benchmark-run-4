d186b89803886a98f25f4f38c1ce7fa1fd5bdd6d
guohui@chromium.org <guohui@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-11 01:50:04 +0000

    Fix the crash when closing multiple tabs with gaia auth extension
    BUG=315366, 313281
    
    Review URL: https://codereview.chromium.org/105383003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239947 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/signin/gaia_auth_extension_loader.cc
+++ chrome/browser/extensions/signin/gaia_auth_extension_loader.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/extensions/scoped_gaia_auth_extension.h"
+#include "chrome/browser/extensions/signin/gaia_auth_extension_loader.h"
 
 #include "base/command_line.h"
 #include "chrome/browser/extensions/component_loader.h"
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_switches.h"
+#include "content/public/browser/browser_thread.h"
 #include "grit/browser_resources.h"
 
 #if defined(OS_CHROMEOS)
@@ -19,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/chromeos_constants.h"
 #include "chromeos/chromeos_switches.h"
 #endif
+
+using content::BrowserThread;
 
 namespace {
 
@@ -30,6 +33,8 @@ extensions::ComponentLoader* GetComponentLoader(Profile* profile) {
 }
 
 void LoadGaiaAuthExtension(Profile* profile) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+
   extensions::ComponentLoader* component_loader = GetComponentLoader(profile);
   const CommandLine* command_line = CommandLine::ForCurrentProcess();
   if (command_line->HasSwitch(switches::kAuthExtensionPath)) {
@@ -46,7 +51,7 @@ void LoadGaiaAuthExtension(Profile* profile) {
     manifest_resource_id = IDR_GAIA_AUTH_KEYBOARD_MANIFEST;
   else if (command_line->HasSwitch(chromeos::switches::kEnableSamlSignin))
     manifest_resource_id = IDR_GAIA_AUTH_SAML_MANIFEST;
-#elif !defined(OS_ANDROID)
+#else
   if (command_line->HasSwitch(switches::kEnableInlineSignin))
     manifest_resource_id = IDR_GAIA_AUTH_INLINE_MANIFEST;
 #endif
@@ -56,17 +61,51 @@ void LoadGaiaAuthExtension(Profile* profile) {
 }
 
 void UnloadGaiaAuthExtension(Profile* profile) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+
   const char kGaiaAuthId[] = "mfffpogegjflfpflabcdkioaeobkgjik";
   GetComponentLoader(profile)->Remove(kGaiaAuthId);
 }
 
 }  // namespace
 
-ScopedGaiaAuthExtension::ScopedGaiaAuthExtension(Profile* profile)
-    : profile_(profile) {
-  LoadGaiaAuthExtension(profile_);
+namespace extensions {
+
+GaiaAuthExtensionLoader::GaiaAuthExtensionLoader(Profile* profile)
+    : profile_(profile), load_count_(0) {}
+
+GaiaAuthExtensionLoader::~GaiaAuthExtensionLoader() {
+  if (load_count_ > 0) {
+    UnloadGaiaAuthExtension(profile_);
+    load_count_ = 0;
+  }
 }
 
-ScopedGaiaAuthExtension::~ScopedGaiaAuthExtension() {
-  UnloadGaiaAuthExtension(profile_);
+void GaiaAuthExtensionLoader::LoadIfNeeded() {
+  if (load_count_ == 0)
+    LoadGaiaAuthExtension(profile_);
+  ++load_count_;
 }
+
+void GaiaAuthExtensionLoader::UnloadIfNeeded() {
+  --load_count_;
+  if (load_count_ == 0)
+    UnloadGaiaAuthExtension(profile_);
+}
+
+// static
+GaiaAuthExtensionLoader* GaiaAuthExtensionLoader::Get(Profile* profile) {
+  return ProfileKeyedAPIFactory<GaiaAuthExtensionLoader>::GetForProfile(
+      profile);
+}
+
+static base::LazyInstance<ProfileKeyedAPIFactory<GaiaAuthExtensionLoader> >
+g_factory = LAZY_INSTANCE_INITIALIZER;
+
+// static
+ProfileKeyedAPIFactory<GaiaAuthExtensionLoader>*
+GaiaAuthExtensionLoader::GetFactoryInstance() {
+  return &g_factory.Get();
+}
+
+} // namespace extensions
---
+++ chrome/browser/extensions/signin/scoped_gaia_auth_extension.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/extensions/signin/scoped_gaia_auth_extension.h"
+
+#include "chrome/browser/extensions/signin/gaia_auth_extension_loader.h"
+
+ScopedGaiaAuthExtension::ScopedGaiaAuthExtension(Profile* profile)
+    : profile_(profile) {
+  extensions::GaiaAuthExtensionLoader* loader =
+      extensions::GaiaAuthExtensionLoader::Get(profile_);
+  if (loader)
+    loader->LoadIfNeeded();
+}
+
+ScopedGaiaAuthExtension::~ScopedGaiaAuthExtension() {
+  extensions::GaiaAuthExtensionLoader* loader =
+      extensions::GaiaAuthExtensionLoader::Get(profile_);
+  if (loader)
+    loader->UnloadIfNeeded();
+}
