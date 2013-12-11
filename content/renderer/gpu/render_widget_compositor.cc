92ed036fb11ba7914915dbbd1852397b84a037df
trchen@chromium.org <trchen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-11 04:11:29 +0000

    Unifies LayerTreeHost::SetNeedsUpdateLayers and SetNeedsAnimate
    [2/2] Unifies LayerTreeHost::SetNeedsUpdateLayers and SetNeedsAnimate
    
    They basically do the same thing except that SetNeedsAnimate makes the next
    commit non-cancellable. However there is really no reason why SetNeedsAnimate
    need to enforce a commit even if no tiles are updated and no layer properties
    changed.
    
    SetNeedsAnimate is thus merged into SetNeedsUpdateLayers. The proper use of
    it is when there are potential layout/tile changes, we can use it to defer
    calculation until the next frame. A commit will be scheduled but can be
    cancelled if no updates are needed after calculation.
    
    This part of the patch changes code behavior slightly.
    SingleThreadProxy::SetNeedsUpdateLayers was originally implemented as
    RenderWidget::ScheduleComposite but now it is RenderWidget::ScheduleAnimation.
    ThreadProxy::SetNeedsAnimate was non-cancellable but is now cancellable.
    
    [1/2] Cleanup RenderWidget::scheduleComposite/scheduleAnimation
    
    scheduleComposite has been renamed to ScheduleComposite as it is no longer
    a part of WebWidgetClient API.
    
    scheduleAnimation has been renamed to ScheduleAnimation. The semantics is to
    schedule a composite and also (potentially) animating WebWidget.
    
    A new WebWidgetClient API scheduleUpdate has been added, to replace the old
    scheduleAnimation. The semantics is to notify the embedder that something in
    the WebWidget may change in 0 seconds. (i.e. it is allowed to be called
    during a redraw, in such case another redraw will be scheduled after frame
    delay.
    
    This part of the patch should not change code behavior.
    
    BUG=316929
    R=danakj,jamesr,piman
    
    Review URL: https://codereview.chromium.org/68893031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240008 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/gpu/render_widget_compositor.cc
+++ content/renderer/gpu/render_widget_compositor.cc
@@ -360,8 +360,8 @@ void RenderWidgetCompositor::SetNeedsRedrawRect(gfx::Rect damage_rect) {
 }
 
 void RenderWidgetCompositor::SetNeedsForcedRedraw() {
-  layer_tree_host_->SetNextCommitForcesRedraw();
-  setNeedsAnimate();
+  layer_tree_host_->set_next_commit_forces_redraw();
+  setNeedsUpdateLayers();
 }
 
 scoped_ptr<cc::SwapPromiseMonitor>
@@ -480,12 +480,19 @@ void RenderWidgetCompositor::startPageScaleAnimation(
       duration);
 }
 
+// Renamed. Staged for removal.
 void RenderWidgetCompositor::setNeedsAnimate() {
-  layer_tree_host_->SetNeedsAnimate();
+  setNeedsUpdateLayers();
 }
 
+void RenderWidgetCompositor::setNeedsUpdateLayers() {
+  layer_tree_host_->SetNeedsUpdateLayers();
+}
+
+// Unused. Staged for removal.
 bool RenderWidgetCompositor::commitRequested() const {
-  return layer_tree_host_->CommitRequested();
+  NOTREACHED();
+  return false;
 }
 
 void RenderWidgetCompositor::didStopFlinging() {
@@ -623,11 +630,14 @@ RenderWidgetCompositor::OffscreenContextProvider() {
 
 void RenderWidgetCompositor::ScheduleComposite() {
   if (!suppress_schedule_composite_)
-    widget_->scheduleComposite();
+    widget_->ScheduleComposite();
 }
 
 void RenderWidgetCompositor::ScheduleAnimation() {
-  widget_->scheduleAnimation();
+  widget_->ScheduleAnimation();
+  // ScheduleAnimation alone doesn't trigger a redraw.
+  // ScheduleComposite here to send an artifical invalidation.
+  ScheduleComposite();
 }
 
 void RenderWidgetCompositor::DidPostSwapBuffers() {
