95529b738159e3abce09d1ceca0e407c24c0aef1
horo@chromium.org <horo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-11 14:19:40 +0000

    Split blink::WebSharedWorker interface into two in blink side (phase:1/3)
    This is one of cleanup patches.
    
    Goal:
    Split blink::WebSharedWorker interface into two interfaces(WebSharedWorker, WebSharedWorkerConnector)
    WebSharedWorkerConnector will be used in parent-context side.
    WebSharedWorker will be used in worker-context side
    
    
    Phase 1: in blink (this patch)
    - Introduce blink::WebSharedWorkerConnector
    - Make blink::WebSharedWorker to be a subclass of blink::WebSharedWorkerConnector
    - Move isStarted(), startWorkerContext() and ConnectListener and connect() from blink::WebSharedWorker to blink::WebSharedWorkerConnector.
    - Set default value of connect()'s 2nd argument(ConnectListener) to 0.
    - Add blink:WebSharedWorkerRepositoryClient::createSharedWorkerConnector() which calls createSharedWorker()
    
    Phase 2: in Chrome
    - Change content::WebSharedWorkerProxy to be a subclass of blink::WebSharedWorkerConnector, which is currently a subclass of blink::WebSharedWorker.
    - Remove content::WebSharedWorkerProxy::terminateWorkerContext() and content::WebSharedWorkerProxy::clientDestroyed().
    - Remove "WebSharedWorker* SharedWorkerRepository::createSharedWorker()"
    - Introduce "WebSharedWorkerConnector* SharedWorkerRepository::createSharedWorkerConnector()"
    
    Phase 3: in blink
    - Make blink::WebSharedWorker independent from blink::WebSharedWorkerConnector.
    - Remove blink::WebSharedWorkerImpl::isStarted()
    - Add startWorkerContext() and connect() to blink::WebSharedWorker.
    - Remove blink:WebSharedWorkerRepositoryClient::createSharedWorker()
    - Remove default value of blink::WebSharedWorkerConnector::connect()'s 2nd argument.
    
    
    BUG=327600
    
    Review URL: https://codereview.chromium.org/104943005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163707 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/web/WebSharedWorkerRepositoryClient.h
+++ third_party/WebKit/public/web/WebSharedWorkerRepositoryClient.h
@@ -32,10 +32,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WebSharedWorkerRepositoryClient_h
 #define WebSharedWorkerRepositoryClient_h
 
+#include "WebSharedWorker.h"
+
 namespace blink {
 
 class WebString;
-class WebSharedWorker;
 class WebURL;
 
 class WebSharedWorkerRepositoryClient {
@@ -44,7 +45,14 @@ public:
     typedef unsigned long long DocumentID;
 
     // Creates a new shared worker. This may return null.
+    // FIXME(horo):Remove this method.
     virtual WebSharedWorker* createSharedWorker(const WebURL&, const WebString&, DocumentID) { return 0; }
+
+    // Creates a new shared worker connector. This may return null.
+    virtual WebSharedWorkerConnector* createSharedWorkerConnector(const WebURL& url, const WebString& name, DocumentID id)
+    {
+        return createSharedWorker(url, name, id);
+    }
 
     // Invoked when a document has been detached. DocumentID can be re-used after documentDetached() is invoked.
     virtual void documentDetached(DocumentID) { }
