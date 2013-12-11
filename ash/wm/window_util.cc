e06869bd713155b6ade506847d35bfa96cb8d3df
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-11 19:28:17 +0000

    Do not send the OnWindowShowTypeChanged() notification if the window show type has not changed
    BUG=None
    TEST=None
    
    Review URL: https://codereview.chromium.org/99303006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240151 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/window_util.cc
+++ ash/wm/window_util.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "ash/ash_constants.h"
+#include "ash/screen_ash.h"
 #include "ash/shell.h"
 #include "ash/wm/window_properties.h"
 #include "ash/wm/window_state.h"
@@ -63,12 +64,19 @@ void CenterWindow(aura::Window* window) {
   const gfx::Display display =
       Shell::GetScreen()->GetDisplayNearestWindow(window);
   gfx::Rect center = display.work_area();
-  gfx::Size size = window_state->HasRestoreBounds() ?
-      window_state->GetRestoreBoundsInScreen().size() :
-      window->bounds().size();
-  center.ClampToCenteredSize(size);
-  window_state->SetRestoreBoundsInScreen(center);
-  window_state->Restore();
+  gfx::Size size = window->bounds().size();
+  if (window_state->IsSnapped()) {
+    if (window_state->HasRestoreBounds())
+      size = window_state->GetRestoreBoundsInScreen().size();
+    center.ClampToCenteredSize(size);
+    window_state->SetRestoreBoundsInScreen(center);
+    window_state->Restore();
+  } else {
+    center = ScreenAsh::ConvertRectFromScreen(window->parent(),
+        center);
+    center.ClampToCenteredSize(size);
+    window->SetBounds(center);
+  }
 }
 
 void AdjustBoundsToEnsureMinimumWindowVisibility(const gfx::Rect& visible_area,
