a70a8a91ccbf1229b2d83823da6aff94c8551c78
shadi@chromium.org <shadi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-11 02:09:26 +0000

    Revert 239236 "Disable media.tough_media_cases on Win8"
    The CL did not actually disable the tests.
    
    > Disable media.tough_media_cases on Win8
    > 
    > BUG=326384
    > NOTRY=true
    > 
    > Review URL: https://codereview.chromium.org/108583002
    
    TBR=shadi@chromium.org
    
    Review URL: https://codereview.chromium.org/111953006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239957 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/benchmarks/media.py
+++ tools/perf/benchmarks/media.py
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import platform
 import sys
 
 from measurements import media
@@ -12,9 +11,6 @@ from telemetry import test
 class Media(test.Test):
   """Obtains media metrics for key user scenarios."""
   test = media.Media
-  # Disable on Win8 crbug.com/326384
-  win8 = sys.platform.startswith('win') and '2008' in platform.release()
-  enabled = not win8
   page_set = 'page_sets/tough_video_cases.json'
 
 class MediaNetworkSimulation(test.Test):
