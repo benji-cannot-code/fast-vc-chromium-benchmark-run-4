b20e3879c500b7f277d515eb5ae93aa9c191c16d
eroman@chromium.org <eroman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-11 09:26:27 +0000

    [webcrypto] Refactor some of the algorithm parameter names.
     (1) Add a suffix of either "Bits" or "Bytes" to all lengths to help with misuse. [*]
    
     (2) Change how optional parameters are retrieved. Currently they require a getter, to prevent misuse [*]. However this is inconvenient, especially for buffers. Naming them "optional" should be enough to guide proper usage.
    
    [*] There was a bug attempting to do this caught in a codereview.
    
    BUG=245025
    
    Review URL: https://codereview.chromium.org/109393003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163691 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/crypto/AesCtrParams.cpp
+++ third_party/WebKit/Source/modules/crypto/AesCtrParams.cpp
@@ -47,7 +47,7 @@ Uint8Array* AesCtrParams::counter()
 
 unsigned char AesCtrParams::length()
 {
-    return m_algorithm.aesCtrParams()->length();
+    return m_algorithm.aesCtrParams()->lengthBits();
 }
 
 AesCtrParams::AesCtrParams(const blink::WebCryptoAlgorithm& algorithm)
