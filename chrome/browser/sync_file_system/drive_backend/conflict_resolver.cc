aa7aa43036a3dd3bb60944bcdde5f3b706912308
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-11 13:48:14 +0000

    [SyncFS] Fix error handling on LocalToRemoteSyncer
    BUG=240165
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/99273003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240102 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_backend/conflict_resolver.cc
+++ chrome/browser/sync_file_system/drive_backend/conflict_resolver.cc
@@ -212,9 +212,9 @@ void ConflictResolver::DidRemoveFile(const SyncStatusCallback& callback,
     return;
   }
 
-  if (error != google_apis::HTTP_SUCCESS &&
-      error != google_apis::HTTP_NOT_FOUND) {
-    callback.Run(GDataErrorCodeToSyncStatusCode(error));
+  SyncStatusCode status = GDataErrorCodeToSyncStatusCode(error);
+  if (status != SYNC_STATUS_OK && error != google_apis::HTTP_NOT_FOUND) {
+    callback.Run(status);
     return;
   }
 
