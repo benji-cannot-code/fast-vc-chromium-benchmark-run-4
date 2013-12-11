be4c4239ff51ea509c3103b1e35ecf25c5b437bc
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-11 21:53:34 +0000

    GTTF: Make new test launcher automatically use single process mode when debugged.
    This also applies on Valgrind.
    
    Use --test-launcher-debug-launcher to override auto-detection and debug
    the launcher itself.
    
    BUG=236893
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/101223009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240186 0039d316-1c4b-4281-b951-d872f2087c98

--- base/test/launcher/unit_test_launcher.cc
+++ base/test/launcher/unit_test_launcher.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_helpers.h"
 #include "base/command_line.h"
 #include "base/compiler_specific.h"
+#include "base/debug/debugger.h"
 #include "base/file_util.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/format_macros.h"
@@ -21,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/launcher/test_launcher.h"
 #include "base/test/test_switches.h"
 #include "base/test/test_timeouts.h"
+#include "base/third_party/dynamic_annotations/dynamic_annotations.h"
 #include "base/threading/thread_checker.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -61,6 +63,10 @@ void PrintUsage() {
           " Other flags:\n"
           "  --test-launcher-batch-limit=N\n"
           "    Sets the limit of test batch to run in a single process to N.\n"
+          "\n"
+          "  --test-launcher-debug-launcher\n"
+          "    Disables autodetection of debuggers and similar tools,\n"
+          "    making it possible to use them to debug launcher itself.\n"
           "\n"
           "  --test-launcher-retry-limit=N\n"
           "    Sets the limit of test retries on failures to N.\n"
@@ -464,12 +470,39 @@ int LaunchUnitTestsInternal(int argc,
                             const RunTestSuiteCallback& run_test_suite,
                             int default_jobs) {
   CommandLine::Init(argc, argv);
+
 #if defined(OS_ANDROID)
   // We can't easily fork on Android, just run the test suite directly.
   return run_test_suite.Run();
 #else
+  bool force_single_process = false;
+  if (CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kTestLauncherDebugLauncher)) {
+    fprintf(stdout, "Forcing test launcher debugging mode.\n");
+    fflush(stdout);
+  } else {
+    if (base::debug::BeingDebugged()) {
+      fprintf(stdout,
+              "Debugger detected, switching to single process mode.\n"
+              "Pass --test-launcher-debug-launcher to debug the launcher "
+              "itself.\n");
+      fflush(stdout);
+      force_single_process = true;
+    }
+
+    if (RunningOnValgrind()) {
+      fprintf(stdout,
+              "Valgrind detected, switching to single process mode.\n"
+              "Pass --test-launcher-debug-launcher to valgrind the launcher "
+              "itself.\n");
+      fflush(stdout);
+      force_single_process = true;
+    }
+  }
+
   if (CommandLine::ForCurrentProcess()->HasSwitch(kGTestHelpFlag) ||
-      CommandLine::ForCurrentProcess()->HasSwitch(kSingleProcessTestsFlag)) {
+      CommandLine::ForCurrentProcess()->HasSwitch(kSingleProcessTestsFlag) ||
+      force_single_process) {
     return run_test_suite.Run();
   }
 #endif
