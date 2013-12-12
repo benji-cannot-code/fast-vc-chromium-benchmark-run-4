4b6655b1d6a50b0890a756b09c2406b85d386617
morrita@chromium.org <morrita@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-12 07:31:10 +0000

    Make import-insert.html less flaky
    The test used zero-timeout retry. This change replaces it
    with exponential backoff.
    
    TEST=import-insert.html
    BUG=none
    R=tkent,haraken
    
    Review URL: https://codereview.chromium.org/99423009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163755 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/html/imports/resources/import-helpers.js
+++ third_party/WebKit/LayoutTests/fast/html/imports/resources/import-helpers.js
@@ -15,7 +15,8 @@ function waitAndTest(tests)
 
     function runSingleTest(options)
     {
-        var ntries = 10;
+        var interval = 1;
+        var ntries = 8;
         function checkWhenReady()
         {
             if (--ntries < 0) {
@@ -23,8 +24,9 @@ function waitAndTest(tests)
                 return finishJSTest();
             }
 
+            interval *= 2;
             if (!options.ready())
-                return setTimeout(checkWhenReady, 0);
+                return setTimeout(checkWhenReady, interval);
 
             options.test();
             return runNext();
