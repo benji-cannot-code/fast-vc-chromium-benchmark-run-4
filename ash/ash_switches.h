ab517043c831bf06a59efa4012de6ce460841a94
varkha@chromium.org <varkha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-12 23:31:06 +0000

    Disables docked windows by default in M33
    BUG=None
    TEST=Visual
    1. in chrome://flags verify that ash-disable-docked-windows is absent.
    2. in chrome://flags verify that ash-enable-docked-windows is present.
    3. When running chrome without ash-enable-docked-windows it should not be possible to dock a window at a screen edge.
    TEST=ash_unittests --gtest_filter=*Dock*
    
    Review URL: https://codereview.chromium.org/109663002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240475 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/ash_switches.h
+++ ash/ash_switches.h
@@ -33,7 +33,6 @@ ASH_EXPORT extern const char kAshDisableAudioDeviceMenu[];
 ASH_EXPORT extern const char kAshDisableAlternateFrameCaptionButtonStyle[];
 ASH_EXPORT extern const char kAshDisableAutoMaximizing[];
 ASH_EXPORT extern const char kAshDisableDisplayChangeLimiter[];
-ASH_EXPORT extern const char kAshDisableDockedWindows[];
 ASH_EXPORT extern const char kAshDisableDragOffShelf[];
 ASH_EXPORT extern const char kAshDisableOverviewMode[];
 ASH_EXPORT extern const char kAshDisableDragAndDropAppListToLauncher[];
@@ -44,6 +43,7 @@ ASH_EXPORT extern const char kAshEnableAudioDeviceMenu[];
 ASH_EXPORT extern const char kAshEnableAdvancedGestures[];
 ASH_EXPORT extern const char kAshEnableAlternateFrameCaptionButtonStyle[];
 ASH_EXPORT extern const char kAshEnableBrightnessControl[];
+ASH_EXPORT extern const char kAshEnableDockedWindows[];
 ASH_EXPORT extern const char kAshEnableImmersiveFullscreenForAllWindows[];
 #if defined(OS_CHROMEOS)
 ASH_EXPORT extern const char kAshEnableFullMultiProfileMode[];
