2cc66808781b66ca10cb8b5f28b12f373c3fa244
nbarth@chromium.org <nbarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-12 07:21:11 +0000

    Delete 2 redundant bindings tests: TestExtendedEvent.idl and TestEvent.idl
    TestExtendedEvent.idl and TestEvent.idl are superseded by the much more
    comprehensive TestInterfaceEventConstructor.idl, and do not test anything
    separate, so delete.
    
    NOTRY=true
    TBR=haraken
    
    Review URL: https://codereview.chromium.org/106183010
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163752 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/bindings/main.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/bindings/main.py
@@ -44,10 +44,8 @@ TBR=(someone in Source/bindings/OWNERS or WATCHLISTS:bindings)
 # Python compiler is incomplete; skip IDLs with unimplemented features
 SKIP_PYTHON = set([
     'TestCustomAccessors.idl',
-    'TestEvent.idl',
     'TestEventTarget.idl',
     'TestException.idl',
-    'TestExtendedEvent.idl',
     'TestImplements.idl',
     'TestInterface.idl',
     'TestInterfaceImplementedAs.idl',
