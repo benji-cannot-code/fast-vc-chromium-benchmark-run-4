1b80911b3a1c56b6e748a8e84e96efa105eb5cde
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-12 23:57:59 +0000

    Rename GetTestWalletItems(<1 param>) to avoid polymorphism in wallet_test_util.cc.
    There was another GetTestWalletItems(<4 params>) in the same file and this is
    banned by the Google C++ style guide and was just confusing.
    
    R=isherman@chromium.org
    BUG=none
    TEST=compile
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/114393003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240490 0039d316-1c4b-4281-b951-d872f2087c98

--- components/autofill/content/browser/wallet/wallet_test_util.cc
+++ components/autofill/content/browser/wallet/wallet_test_util.cc
@@ -238,7 +238,7 @@ scoped_ptr<Address> GetTestNonDefaultShippingAddress() {
   return address.Pass();
 }
 
-scoped_ptr<WalletItems> GetTestWalletItems(
+scoped_ptr<WalletItems> GetTestWalletItemsWithDetails(
     const std::vector<RequiredAction>& required_actions,
     const std::string& default_instrument_id,
     const std::string& default_address_id,
@@ -255,10 +255,10 @@ scoped_ptr<WalletItems> GetTestWalletItemsWithRequiredAction(
     RequiredAction action) {
   std::vector<RequiredAction> required_actions(1, action);
   scoped_ptr<WalletItems> items =
-      GetTestWalletItems(required_actions,
-                         "default_instrument_id",
-                         "default_address_id",
-                         AMEX_ALLOWED);
+      GetTestWalletItemsWithDetails(required_actions,
+                                    "default_instrument_id",
+                                    "default_address_id",
+                                    AMEX_ALLOWED);
 
   if (action != GAIA_AUTH)
     items->AddAccount(GetTestGaiaAccount());
@@ -275,10 +275,10 @@ scoped_ptr<WalletItems> GetTestWalletItems(AmexPermission amex_permission) {
 scoped_ptr<WalletItems> GetTestWalletItemsWithUsers(
     const std::vector<std::string>& users, size_t active_index) {
   scoped_ptr<WalletItems> items =
-      GetTestWalletItems(std::vector<RequiredAction>(),
-                         "default_instrument_id",
-                         "default_address_id",
-                         AMEX_ALLOWED);
+      GetTestWalletItemsWithDetails(std::vector<RequiredAction>(),
+                                    "default_instrument_id",
+                                    "default_address_id",
+                                    AMEX_ALLOWED);
   for (size_t i = 0; i < users.size(); ++i) {
     scoped_ptr<GaiaAccount> account(GaiaAccount::CreateForTesting(
         users[i], "obfuscated_id", i, i == active_index));
@@ -292,10 +292,10 @@ scoped_ptr<WalletItems> GetTestWalletItemsWithDefaultIds(
     const std::string& default_address_id,
     AmexPermission amex_permission) {
   scoped_ptr<WalletItems> items =
-      GetTestWalletItems(std::vector<RequiredAction>(),
-                         default_instrument_id,
-                         default_address_id,
-                         amex_permission);
+      GetTestWalletItemsWithDetails(std::vector<RequiredAction>(),
+                                    default_instrument_id,
+                                    default_address_id,
+                                    amex_permission);
   items->AddAccount(GetTestGaiaAccount());
   return items.Pass();
 }
