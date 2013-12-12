3f1e7b44c0c54f8f1cbd7a8c73bdfc9a8ba3b056
zmo@chromium.org <zmo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-12 20:45:11 +0000

    Use std::vector instead of base::List for log messages in GpuDataManagerImplPrivate
    This is to verify if the crashes in ~GpuDataManagerImplPrivate() in chromeos is indeep a racing in log message handling.
    
    If after this CL, there are no new crashes in ~GpuDataManagerImplPrivate() that are related with log_messages_, then the bug is in DictionaryValue() instead.
    
    BUG=327463
    TEST=
    B=kbr@chromium.org
    R=kbr@chromium.org
    
    Review URL: https://codereview.chromium.org/101513006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240400 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/gpu/gpu_data_manager_impl_private.h
+++ content/browser/gpu/gpu_data_manager_impl_private.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <set>
 #include <string>
+#include <vector>
 
 #include "base/memory/ref_counted.h"
 #include "base/memory/singleton.h"
@@ -164,6 +165,19 @@ class CONTENT_EXPORT GpuDataManagerImplPrivate {
   typedef ObserverListThreadSafe<GpuDataManagerObserver>
       GpuDataManagerObserverList;
 
+  struct LogMessage {
+    int level;
+    std::string header;
+    std::string message;
+
+    LogMessage(int _level,
+               const std::string& _header,
+               const std::string& _message)
+        : level(_level),
+          header(_header),
+          message(_message) { }
+  };
+
   explicit GpuDataManagerImplPrivate(GpuDataManagerImpl* owner);
 
   void InitializeImpl(const std::string& gpu_blacklist_json,
@@ -212,7 +226,7 @@ class CONTENT_EXPORT GpuDataManagerImplPrivate {
 
   const scoped_refptr<GpuDataManagerObserverList> observer_list_;
 
-  base::ListValue log_messages_;
+  std::vector<LogMessage> log_messages_;
 
   bool use_swiftshader_;
 
