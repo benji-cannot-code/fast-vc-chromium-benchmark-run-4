34eed26b37461787c45fb02350266c83194cec2c
yukawa@chromium.org <yukawa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-12 23:30:11 +0000

    Rename TSFTextStore to TextStore
    This CL does nothing except for renaming TSFTextStore to TextStore.
    
    The TSF prefix is considered to be redundant because TSF (Text Services Framework) is the only available API to support IMEs under Metro mode.
    
    BUG=326912
    
    Review URL: https://codereview.chromium.org/103383004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240474 0039d316-1c4b-4281-b951-d872f2087c98

--- win8/metro_driver/ime/text_service.cc
+++ win8/metro_driver/ime/text_service.cc
@@ -88,7 +88,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace metro_driver {
 namespace {
-typedef TSFTextStore TextStore;
 
 // Japanese IME expects the default value of this compartment is
 // TF_SENTENCEMODE_PHRASEPREDICT to emulate IMM32 behavior. This value is
