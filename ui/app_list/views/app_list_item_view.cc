a53e1581ff04b4cab34f76556b8994c7d88168bf
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-12 19:28:36 +0000

    app_list: UpdateDragFromItem should have a valid drag_view_.
    BUG=326368
    
    Review URL: https://codereview.chromium.org/108873007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240383 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/app_list/views/app_list_item_view.cc
+++ ui/app_list/views/app_list_item_view.cc
@@ -408,7 +408,8 @@ void AppListItemView::OnMouseCaptureLost() {
 
 bool AppListItemView::OnMouseDragged(const ui::MouseEvent& event) {
   CustomButton::OnMouseDragged(event);
-  apps_grid_view_->UpdateDragFromItem(AppsGridView::MOUSE, event);
+  if (apps_grid_view_->IsDraggedView(this))
+    apps_grid_view_->UpdateDragFromItem(AppsGridView::MOUSE, event);
 
   // Shows dragging UI when it's confirmed without waiting for the timer.
   if (ui_state_ != UI_STATE_DRAGGING &&
@@ -429,7 +430,7 @@ void AppListItemView::OnGestureEvent(ui::GestureEvent* event) {
       }
       break;
     case ui::ET_GESTURE_SCROLL_UPDATE:
-      if (touch_dragging_) {
+      if (touch_dragging_ && apps_grid_view_->IsDraggedView(this)) {
         apps_grid_view_->UpdateDragFromItem(AppsGridView::TOUCH, *event);
         event->SetHandled();
       }
