81e2333ef9c56152c43ab30c2ca9735e1279b773
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-12 13:26:22 +0000

    linux_aura: Reenable download browsertests.
    BUG=163931
    
    Review URL: https://codereview.chromium.org/99443007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240296 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/save_page_browsertest.cc
+++ chrome/browser/download/save_page_browsertest.cc
@@ -378,8 +378,7 @@ SavePageBrowserTest::~SavePageBrowserTest() {
 }
 
 // Disabled on Windows due to flakiness. http://crbug.com/162323
-// TODO(linux_aura) http://crbug.com/163931
-#if defined(OS_WIN) || (defined(OS_LINUX) && !defined(OS_CHROMEOS) && defined(USE_AURA))
+#if defined(OS_WIN)
 #define MAYBE_SaveHTMLOnly DISABLED_SaveHTMLOnly
 #else
 #define MAYBE_SaveHTMLOnly SaveHTMLOnly
@@ -410,7 +409,6 @@ IN_PROC_BROWSER_TEST_F(SavePageBrowserTest, MAYBE_SaveHTMLOnly) {
 }
 
 // http://crbug.com/162323
-// http://crbug.com/163931
 IN_PROC_BROWSER_TEST_F(SavePageBrowserTest, DISABLED_SaveHTMLOnlyCancel) {
   GURL url = NavigateToMockURL("a");
   DownloadManager* manager(GetDownloadManager());
@@ -495,8 +493,7 @@ IN_PROC_BROWSER_TEST_F(SavePageBrowserTest, SaveHTMLOnlyTabDestroy) {
 }
 
 // Disabled on Windows due to flakiness. http://crbug.com/162323
-// TODO(linux_aura) http://crbug.com/163931
-#if defined(OS_WIN) || (defined(OS_LINUX) && !defined(OS_CHROMEOS) && defined(USE_AURA))
+#if defined(OS_WIN)
 #define MAYBE_SaveViewSourceHTMLOnly DISABLED_SaveViewSourceHTMLOnly
 #else
 #define MAYBE_SaveViewSourceHTMLOnly SaveViewSourceHTMLOnly
@@ -535,8 +532,7 @@ IN_PROC_BROWSER_TEST_F(SavePageBrowserTest, MAYBE_SaveViewSourceHTMLOnly) {
 }
 
 // Disabled on Windows due to flakiness. http://crbug.com/162323
-// TODO(linux_aura) http://crbug.com/163931
-#if defined(OS_WIN) || (defined(OS_LINUX) && !defined(OS_CHROMEOS) && defined(USE_AURA))
+#if defined(OS_WIN)
 #define MAYBE_SaveCompleteHTML DISABLED_SaveCompleteHTML
 #else
 #define MAYBE_SaveCompleteHTML SaveCompleteHTML
@@ -578,8 +574,7 @@ IN_PROC_BROWSER_TEST_F(SavePageBrowserTest, MAYBE_SaveCompleteHTML) {
 // Invoke a save page during the initial navigation.
 // (Regression test for http://crbug.com/156538).
 // Disabled on Windows due to flakiness. http://crbug.com/162323
-// TODO(linux_aura) http://crbug.com/163931
-#if defined(OS_WIN) || (defined(OS_LINUX) && !defined(OS_CHROMEOS) && defined(USE_AURA))
+#if defined(OS_WIN)
 #define MAYBE_SaveDuringInitialNavigationIncognito DISABLED_SaveDuringInitialNavigationIncognito
 #else
 #define MAYBE_SaveDuringInitialNavigationIncognito SaveDuringInitialNavigationIncognito
@@ -630,8 +625,7 @@ IN_PROC_BROWSER_TEST_F(SavePageBrowserTest, NoSave) {
 }
 
 // Disabled on Windows due to flakiness. http://crbug.com/162323
-// TODO(linux_aura) http://crbug.com/163931
-#if defined(OS_WIN) || (defined(OS_LINUX) && !defined(OS_CHROMEOS) && defined(USE_AURA))
+#if defined(OS_WIN)
 #define MAYBE_FileNameFromPageTitle DISABLED_FileNameFromPageTitle
 #else
 #define MAYBE_FileNameFromPageTitle FileNameFromPageTitle
@@ -674,8 +668,7 @@ IN_PROC_BROWSER_TEST_F(SavePageBrowserTest, MAYBE_FileNameFromPageTitle) {
 }
 
 // Disabled on Windows due to flakiness. http://crbug.com/162323
-// TODO(linux_aura) http://crbug.com/163931
-#if defined(OS_WIN) || (defined(OS_LINUX) && !defined(OS_CHROMEOS) && defined(USE_AURA))
+#if defined(OS_WIN)
 #define MAYBE_RemoveFromList DISABLED_RemoveFromList
 #else
 #define MAYBE_RemoveFromList RemoveFromList
