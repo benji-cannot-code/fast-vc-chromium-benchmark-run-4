5c6206487c4b1da476f93e593490cf9cd1162ec5
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-12 23:47:45 +0000

    Replace string16 with base::string16 in gpu, media, and remoting.
    R=piman@chromium.org, scherkus@chromium.org, sergeyu@chromium.org
    TBR=piman, scherkus, wez
    
    Review URL: https://codereview.chromium.org/114613002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240484 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/disconnect_window_mac.h
+++ remoting/host/disconnect_window_mac.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @interface DisconnectWindowController : NSWindowController {
  @private
   base::Closure disconnect_callback_;
-  string16 username_;
+  base::string16 username_;
   IBOutlet NSTextField* connectedToField_;
   IBOutlet NSButton* disconnectButton_;
 }
