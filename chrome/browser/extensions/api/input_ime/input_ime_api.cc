06ee6f80a7cc5879e14586a04994ca8e5fff71ad
komatsu@chromium.org <komatsu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-12 17:27:40 +0000

    Add input.ime.sendKeyEvents.
    This patch adds input.ime.sendKeyEvents to IME JS API.
    The actual implementation of the function will be added in the next patch.
    
    BUG=326931
    
    Review URL: https://codereview.chromium.org/110083002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240351 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/input_ime/input_ime_api.cc
+++ chrome/browser/extensions/api/input_ime/input_ime_api.cc
@@ -23,6 +23,7 @@ namespace KeyEventHandled = extensions::api::input_ime::KeyEventHandled;
 namespace DeleteSurroundingText =
     extensions::api::input_ime::DeleteSurroundingText;
 namespace UpdateMenuItems = extensions::api::input_ime::UpdateMenuItems;
+namespace SendKeyEvents = extensions::api::input_ime::SendKeyEvents;
 namespace SetMenuItems = extensions::api::input_ime::SetMenuItems;
 namespace SetCursorPosition = extensions::api::input_ime::SetCursorPosition;
 namespace SetCandidates = extensions::api::input_ime::SetCandidates;
@@ -497,6 +498,11 @@ bool InputImeCommitTextFunction::RunImpl() {
 
   SetResult(new base::FundamentalValue(
       engine->CommitText(params.context_id, params.text.c_str(), &error_)));
+  return true;
+}
+
+bool InputImeSendKeyEventsFunction::RunImpl() {
+  // TODO(komatsu): Implement here.
   return true;
 }
 
