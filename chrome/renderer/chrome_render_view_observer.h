ccaefd94de7a409c02f5340681412c86b4c535f2
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-12 18:21:48 +0000

    Move WebPermissionContent implementation to ContentSettingsObserver instead of routing the callbacks from ChromeRenderViewObserver.
    BUG=304341
    R=jochen@chromium.org
    
    Review URL: https://codereview.chromium.org/108873009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240361 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/chrome_render_view_observer.h
+++ chrome/renderer/chrome_render_view_observer.h
@@ -16,8 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/timer/timer.h"
 #include "content/public/common/top_controls_state.h"
 #include "content/public/renderer/render_view_observer.h"
-#include "extensions/common/permissions/api_permission.h"
-#include "third_party/WebKit/public/web/WebPermissionClient.h"
 #include "ui/gfx/size.h"
 #include "url/gurl.h"
 
@@ -28,11 +26,6 @@ class SkBitmap;
 class TranslateHelper;
 class WebViewColorOverlay;
 class WebViewAnimatingOverlay;
-
-namespace extensions {
-class Dispatcher;
-class Extension;
-}
 
 namespace blink {
 class WebView;
@@ -45,15 +38,12 @@ class PhishingClassifierDelegate;
 
 // This class holds the Chrome specific parts of RenderView, and has the same
 // lifetime.
-class ChromeRenderViewObserver : public content::RenderViewObserver,
-                                 public blink::WebPermissionClient {
+class ChromeRenderViewObserver : public content::RenderViewObserver {
  public:
   // translate_helper can be NULL.
   ChromeRenderViewObserver(
       content::RenderView* render_view,
-      ContentSettingsObserver* content_settings,
-      ChromeRenderProcessObserver* chrome_render_process_observer,
-      extensions::Dispatcher* extension_dispatcher);
+      ChromeRenderProcessObserver* chrome_render_process_observer);
   virtual ~ChromeRenderViewObserver();
 
  private:
@@ -78,55 +68,6 @@ class ChromeRenderViewObserver : public content::RenderViewObserver,
                                            const base::string16& stack_trace,
                                            int32 line_number,
                                            int32 severity_level) OVERRIDE;
-
-  // blink::WebPermissionClient implementation.
-  virtual bool allowDatabase(blink::WebFrame* frame,
-                             const blink::WebString& name,
-                             const blink::WebString& display_name,
-                             unsigned long estimated_size);
-  virtual bool allowFileSystem(blink::WebFrame* frame);
-  virtual bool allowImage(blink::WebFrame* frame,
-                          bool enabled_per_settings,
-                          const blink::WebURL& image_url);
-  virtual bool allowIndexedDB(blink::WebFrame* frame,
-                              const blink::WebString& name,
-                              const blink::WebSecurityOrigin& origin);
-  virtual bool allowPlugins(blink::WebFrame* frame,
-                            bool enabled_per_settings);
-  virtual bool allowScript(blink::WebFrame* frame,
-                           bool enabled_per_settings);
-  virtual bool allowScriptFromSource(blink::WebFrame* frame,
-                                     bool enabled_per_settings,
-                                     const blink::WebURL& script_url);
-  virtual bool allowStorage(blink::WebFrame* frame, bool local);
-  virtual bool allowReadFromClipboard(blink::WebFrame* frame,
-                                      bool default_value);
-  virtual bool allowWriteToClipboard(blink::WebFrame* frame,
-                                     bool default_value);
-#if defined(WEBPERMISSIONCLIENT_USES_FRAME_FOR_ALL_METHODS)
-  virtual bool allowWebComponents(blink::WebFrame* frame, bool);
-  virtual bool allowMutationEvents(blink::WebFrame* frame,
-                                   bool default_value);
-  virtual bool allowPushState(blink::WebFrame* frame);
-#else
-  virtual bool allowWebComponents(const blink::WebDocument&, bool);
-  virtual bool allowMutationEvents(const blink::WebDocument&,
-                                   bool default_value);
-  virtual bool allowPushState(const blink::WebDocument&);
-#endif
-  virtual bool allowWebGLDebugRendererInfo(blink::WebFrame* frame);
-  virtual void didNotAllowPlugins(blink::WebFrame* frame);
-  virtual void didNotAllowScript(blink::WebFrame* frame);
-  virtual bool allowDisplayingInsecureContent(
-      blink::WebFrame* frame,
-      bool allowed_per_settings,
-      const blink::WebSecurityOrigin& context,
-      const blink::WebURL& url);
-  virtual bool allowRunningInsecureContent(
-      blink::WebFrame* frame,
-      bool allowed_per_settings,
-      const blink::WebSecurityOrigin& context,
-      const blink::WebURL& url);
   virtual void Navigate(const GURL& url) OVERRIDE;
 
   void OnWebUIJavaScript(const base::string16& frame_xpath,
@@ -137,14 +78,11 @@ class ChromeRenderViewObserver : public content::RenderViewObserver,
                                        const std::string& origin,
                                        const std::string& target);
   void OnJavaScriptStressTestControl(int cmd, int param);
-  void OnSetAllowDisplayingInsecureContent(bool allow);
-  void OnSetAllowRunningInsecureContent(bool allow);
   void OnSetClientSidePhishingDetection(bool enable_phishing_detection);
   void OnSetVisuallyDeemphasized(bool deemphasized);
   void OnRequestThumbnailForContextNode(int thumbnail_min_area_pixels,
                                         gfx::Size thumbnail_max_size_pixels);
   void OnGetFPS();
-  void OnNPAPINotSupported();
 #if defined(OS_ANDROID)
   void OnUpdateTopControlsState(content::TopControlsState constraints,
                                 content::TopControlsState current,
@@ -170,11 +108,6 @@ class ChromeRenderViewObserver : public content::RenderViewObserver,
   // Determines if a host is in the strict security host set.
   bool IsStrictSecurityHost(const std::string& host);
 
-  // If |origin| corresponds to an installed extension, returns that extension.
-  // Otherwise returns NULL.
-  const extensions::Extension* GetExtension(
-      const blink::WebSecurityOrigin& origin) const;
-
   // Checks if a page contains <meta http-equiv="refresh" ...> tag.
   bool HasRefreshMetaTag(blink::WebFrame* frame);
 
@@ -183,10 +116,8 @@ class ChromeRenderViewObserver : public content::RenderViewObserver,
 
   // Owned by ChromeContentRendererClient and outlive us.
   ChromeRenderProcessObserver* chrome_render_process_observer_;
-  extensions::Dispatcher* extension_dispatcher_;
 
   // Have the same lifetime as us.
-  ContentSettingsObserver* content_settings_;
   TranslateHelper* translate_helper_;
   safe_browsing::PhishingClassifierDelegate* phishing_classifier_;
 
@@ -197,10 +128,6 @@ class ChromeRenderViewObserver : public content::RenderViewObserver,
   // page id to decide whether to reindex in certain cases like history
   // replacement.
   GURL last_indexed_url_;
-
-  // Insecure content may be permitted for the duration of this render view.
-  bool allow_displaying_insecure_content_;
-  bool allow_running_insecure_content_;
 
   // External host exposed through automation controller.
   scoped_ptr<ExternalHostBindings> external_host_bindings_;
--- chrome/renderer/content_settings_observer_browsertest.cc
+++ chrome/renderer/content_settings_observer_browsertest.cc
@@ -35,7 +35,7 @@ class MockContentSettingsObserver : public ContentSettingsObserver {
 
 MockContentSettingsObserver::MockContentSettingsObserver(
     content::RenderView* render_view)
-    : ContentSettingsObserver(render_view),
+    : ContentSettingsObserver(render_view, NULL),
       image_url_("http://www.foo.com/image.jpg"),
       image_origin_("http://www.foo.com") {
 }
@@ -75,11 +75,11 @@ TEST_F(ChromeRenderViewTest, DISABLED_AllowDOMStorage) {
           OnAllowDOMStorage(_, _, _, _, _)).WillByDefault(DeleteArg<4>());
   EXPECT_CALL(observer,
               OnAllowDOMStorage(_, _, _, _, _));
-  observer.AllowStorage(view_->GetWebView()->focusedFrame(), true);
+  observer.allowStorage(view_->GetWebView()->focusedFrame(), true);
 
   // Accessing localStorage from the same origin again shouldn't result in a
   // new IPC.
-  observer.AllowStorage(view_->GetWebView()->focusedFrame(), true);
+  observer.allowStorage(view_->GetWebView()->focusedFrame(), true);
   ::testing::Mock::VerifyAndClearExpectations(&observer);
 }
 
@@ -190,7 +190,7 @@ TEST_F(ChromeRenderViewTest, ImagesBlockedByDefault) {
   observer->SetContentSettingRules(&content_setting_rules);
   EXPECT_CALL(mock_observer,
               OnContentBlocked(CONTENT_SETTINGS_TYPE_IMAGES));
-  EXPECT_FALSE(observer->AllowImage(GetMainFrame(),
+  EXPECT_FALSE(observer->allowImage(GetMainFrame(),
                                     true, mock_observer.image_url_));
   ::testing::Mock::VerifyAndClearExpectations(&observer);
 
@@ -207,7 +207,7 @@ TEST_F(ChromeRenderViewTest, ImagesBlockedByDefault) {
   EXPECT_CALL(
       mock_observer,
       OnContentBlocked(CONTENT_SETTINGS_TYPE_IMAGES)).Times(0);
-  EXPECT_TRUE(observer->AllowImage(GetMainFrame(), true,
+  EXPECT_TRUE(observer->allowImage(GetMainFrame(), true,
                                    mock_observer.image_url_));
   ::testing::Mock::VerifyAndClearExpectations(&observer);
 }
@@ -234,7 +234,7 @@ TEST_F(ChromeRenderViewTest, ImagesAllowedByDefault) {
   EXPECT_CALL(
       mock_observer,
       OnContentBlocked(CONTENT_SETTINGS_TYPE_IMAGES)).Times(0);
-  EXPECT_TRUE(observer->AllowImage(GetMainFrame(), true,
+  EXPECT_TRUE(observer->allowImage(GetMainFrame(), true,
                                    mock_observer.image_url_));
   ::testing::Mock::VerifyAndClearExpectations(&observer);
 
@@ -249,7 +249,7 @@ TEST_F(ChromeRenderViewTest, ImagesAllowedByDefault) {
           false));
   EXPECT_CALL(mock_observer,
               OnContentBlocked(CONTENT_SETTINGS_TYPE_IMAGES));
-  EXPECT_FALSE(observer->AllowImage(GetMainFrame(),
+  EXPECT_FALSE(observer->allowImage(GetMainFrame(),
                                     true, mock_observer.image_url_));
   ::testing::Mock::VerifyAndClearExpectations(&observer);
 }
@@ -373,7 +373,7 @@ TEST_F(ChromeRenderViewTest, ContentSettingsInterstitialPages) {
   EXPECT_CALL(
       mock_observer,
       OnContentBlocked(CONTENT_SETTINGS_TYPE_IMAGES)).Times(0);
-  EXPECT_TRUE(observer->AllowImage(GetMainFrame(), true,
+  EXPECT_TRUE(observer->allowImage(GetMainFrame(), true,
                                    mock_observer.image_url_));
   ::testing::Mock::VerifyAndClearExpectations(&observer);
 }
