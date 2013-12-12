38c4f4ab653725e2c5aea90c5a77c8324cbd77df
miletus@chromium.org <miletus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-12 13:20:36 +0000

    Make LatencyInfo trace show input event name
    It is useful to know the event type of the LatencyInfo when inspecting trace.
    
    BUG=246034
    TEST=When tracing input event latency, one can see the event type of the LatencyInfo trace.
    
    Review URL: https://codereview.chromium.org/103863008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240295 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_impl.cc
+++ content/browser/renderer_host/render_widget_host_impl.cc
@@ -928,9 +928,8 @@ void RenderWidgetHostImpl::EnableFullAccessibilityMode() {
 }
 
 void RenderWidgetHostImpl::ForwardMouseEvent(const WebMouseEvent& mouse_event) {
-  ForwardMouseEventWithLatencyInfo(
-      MouseEventWithLatencyInfo(mouse_event,
-                                CreateRWHLatencyInfoIfNotExist(NULL)));
+  ForwardMouseEventWithLatencyInfo(MouseEventWithLatencyInfo(
+      mouse_event, CreateRWHLatencyInfoIfNotExist(NULL, mouse_event.type)));
 }
 
 void RenderWidgetHostImpl::ForwardMouseEventWithLatencyInfo(
@@ -954,9 +953,8 @@ void RenderWidgetHostImpl::OnPointerEventActivate() {
 
 void RenderWidgetHostImpl::ForwardWheelEvent(
     const WebMouseWheelEvent& wheel_event) {
-  ForwardWheelEventWithLatencyInfo(
-      MouseWheelEventWithLatencyInfo(wheel_event,
-                                     CreateRWHLatencyInfoIfNotExist(NULL)));
+  ForwardWheelEventWithLatencyInfo(MouseWheelEventWithLatencyInfo(
+      wheel_event, CreateRWHLatencyInfoIfNotExist(NULL, wheel_event.type)));
 }
 
 void RenderWidgetHostImpl::ForwardWheelEventWithLatencyInfo(
@@ -984,7 +982,8 @@ void RenderWidgetHostImpl::ForwardGestureEventWithLatencyInfo(
   if (IgnoreInputEvents())
     return;
 
-  ui::LatencyInfo latency_info = CreateRWHLatencyInfoIfNotExist(&ui_latency);
+  ui::LatencyInfo latency_info =
+      CreateRWHLatencyInfoIfNotExist(&ui_latency, gesture_event.type);
 
   if (gesture_event.type == blink::WebInputEvent::GestureScrollUpdate) {
     latency_info.AddLatencyNumber(
@@ -1021,7 +1020,8 @@ void RenderWidgetHostImpl::ForwardTouchEventWithLatencyInfo(
   // Always forward TouchEvents for touch stream consistency. They will be
   // ignored if appropriate in FilterInputEvent().
 
-  ui::LatencyInfo latency_info = CreateRWHLatencyInfoIfNotExist(&ui_latency);
+  ui::LatencyInfo latency_info =
+      CreateRWHLatencyInfoIfNotExist(&ui_latency, touch_event.type);
   TouchEventWithLatencyInfo touch_with_latency(touch_event, latency_info);
   input_router_->SendTouchEvent(touch_with_latency);
 }
@@ -1087,9 +1087,10 @@ void RenderWidgetHostImpl::ForwardKeyboardEvent(
       suppress_next_char_events_ = false;
   }
 
-  input_router_->SendKeyboardEvent(key_event,
-                                   CreateRWHLatencyInfoIfNotExist(NULL),
-                                   is_shortcut);
+  input_router_->SendKeyboardEvent(
+      key_event,
+      CreateRWHLatencyInfoIfNotExist(NULL, key_event.type),
+      is_shortcut);
 }
 
 void RenderWidgetHostImpl::SendCursorVisibilityState(bool is_visible) {
@@ -1106,7 +1107,7 @@ void RenderWidgetHostImpl::DisableResizeAckCheckForTesting() {
 }
 
 ui::LatencyInfo RenderWidgetHostImpl::CreateRWHLatencyInfoIfNotExist(
-    const ui::LatencyInfo* original) {
+    const ui::LatencyInfo* original, WebInputEvent::Type type) {
   ui::LatencyInfo info;
   if (original)
     info = *original;
@@ -1118,6 +1119,7 @@ ui::LatencyInfo RenderWidgetHostImpl::CreateRWHLatencyInfoIfNotExist(
     info.AddLatencyNumber(ui::INPUT_EVENT_LATENCY_BEGIN_RWH_COMPONENT,
                           GetLatencyComponentId(),
                           ++last_input_number_);
+    info.TraceEventType(WebInputEventTraits::GetName(type));
   }
   return info;
 }
