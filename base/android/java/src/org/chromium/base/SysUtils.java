fc682b079594a2707e21f7c8e89bf84250239281
cramya@chromium.org <cramya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-12 23:57:22 +0000

    Enable svelte UX on low end devices for new installs or after clearing data.
    Clank CL https://chrome-internal-review.googlesource.com/#/c/45530/
    
    BUG=310844
    
    Review URL: https://codereview.chromium.org/108953006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240489 0039d316-1c4b-4281-b951-d872f2087c98

--- base/android/java/src/org/chromium/base/SysUtils.java
+++ base/android/java/src/org/chromium/base/SysUtils.java
@@ -99,8 +99,14 @@ public class SysUtils {
         if (sLowEndDevice == null) {
             sLowEndDevice = detectLowEndDevice();
         }
-
         return sLowEndDevice.booleanValue();
+    }
+
+    /**
+     * @return Whether isLowEndDevice() has ever been called.
+     */
+    public static boolean isLowEndStateInitialized() {
+        return (sLowEndDevice != null);
     }
 
     private static boolean detectLowEndDevice() {
