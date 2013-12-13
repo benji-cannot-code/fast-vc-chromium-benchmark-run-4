0638a27e451cd3720b14c1458d8e799879af8486
bcwhite@chromium.org <bcwhite@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-13 17:23:46 +0000

    Encrypt all stored cookies on selected operating systems.
    As part of the goal of protecting private user information, this encrypts the cookie values on operating systems with user-specific crypto APIs and that do not otherwise protect this data.
    
    Performance tests indicate a penalty of about 1ms per cookie  (regardless of size) on a Mac and 0.1ms to 0.7ms (depending on the size) under Windows.  This will be higher on older hardware but still insignificant.
    
    Encrypted data is binary (with an overhead of 128 bytes on Windows) and binary data must be stored in a BLOB so only one of two fields ("value" or "encrypted_value") will have data with the other being empty.  Both values, however, need to be read & written when accessing a cookie because they are marked "non null").
    
    BUG=313323
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=234855
    
    Review URL: https://codereview.chromium.org/24734007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240684 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ content/public/browser/cookie_crypto_delegate.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_PUBLIC_BROWSER_COOKIE_CRYPTO_DELEGATE_H_
+#define CONTENT_PUBLIC_BROWSER_COOKIE_CRYPTO_DELEGATE_H_
+
+namespace content {
+
+// Implements encryption and decryption for the persistent cookie store.
+class CookieCryptoDelegate {
+ public:
+  virtual ~CookieCryptoDelegate() {}
+  virtual bool EncryptString(const std::string& plaintext,
+                             std::string* ciphertext) = 0;
+  virtual bool DecryptString(const std::string& ciphertext,
+                             std::string* plaintext) = 0;
+};
+
+}  // namespace content
+
+#endif  // CONTENT_PUBLIC_BROWSER_COOKIE_CRYPTO_DELEGATE_H_
