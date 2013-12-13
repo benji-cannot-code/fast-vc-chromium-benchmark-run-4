2287ad32663965000e42d46b8707c56c4a62bd41
gbillock@chromium.org <gbillock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-13 20:04:37 +0000

    [SiteChip] Don't show bubble in response to location bar LocationIconView in experiment.
    Also restricts experiment-on location bar icon to be the page icon.
    
    R=msw@chromium.org
    BUG=315944
    
    Review URL: https://codereview.chromium.org/114723004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240722 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/location_bar/location_icon_view.cc
+++ chrome/browser/ui/views/location_bar/location_icon_view.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/location_bar/location_icon_view.h"
 
 #include "base/strings/utf_string_conversions.h"
+#include "chrome/browser/search/search.h"
 #include "chrome/browser/ui/views/location_bar/location_bar_view.h"
 #include "grit/generated_resources.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -26,11 +27,13 @@ bool LocationIconView::OnMousePressed(const ui::MouseEvent& event) {
 }
 
 void LocationIconView::OnMouseReleased(const ui::MouseEvent& event) {
-  page_info_helper_.ProcessEvent(event);
+  if (chrome::ShouldDisplayOriginChip())
+    page_info_helper_.ProcessEvent(event);
 }
 
 void LocationIconView::OnGestureEvent(ui::GestureEvent* event) {
-  if (event->type() == ui::ET_GESTURE_TAP) {
+  if (chrome::ShouldDisplayOriginChip() &&
+      (event->type() == ui::ET_GESTURE_TAP)) {
     page_info_helper_.ProcessEvent(*event);
     event->SetHandled();
   }
