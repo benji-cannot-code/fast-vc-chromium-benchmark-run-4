06d1e6b3cccdd8b0604aeaf9a1ae929dff75044c
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-13 13:00:15 +0000

    Revert 240566 "[rAC, OSX] Force better initial size for sign-in"
    Broke AutofillDialogControllerTest.SignInWebViewOpensLinksInNewTab
    
    ASSERTION FAILED: !enable || !minSize.isEmpty()
      ../../third_party/WebKit/Source/core/frame/FrameView.cpp(3065) : void WebCore::FrameView::enableAutoSizeMode(bool, const WebCore::IntSize &, const WebCore::IntSize &)
    
    > [rAC, OSX] Force better initial size for sign-in
    > 
    > Open the sign-in dialog with an initial size equal to the current
    > dialog. This avoids one unnecessary resize until the content is loaded
    > and makes the visual experience less jarring.
    > 
    > BUG=325518
    > R=isherman@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/103853004
    
    TBR=groby@chromium.org
    
    Review URL: https://codereview.chromium.org/113913008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240603 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/autofill/autofill_sign_in_container.mm
+++ chrome/browser/ui/cocoa/autofill/autofill_sign_in_container.mm
@@ -38,18 +38,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)loadSignInPage {
   DCHECK(webContents_.get());
 
-  // Ensure initial minimum size doesn't cause resize.
-  NSSize initialMinSize = [[self view] frame].size;
-
-  // Ensure |maxSize_| is bigger than |initialMinSize|.
-  maxSize_.height = std::max(maxSize_.height, initialMinSize.height);
-  maxSize_.width = std::max(maxSize_.width, initialMinSize.width);
+  // Prevent accidentaly empty |maxSize_|.
+  if (NSEqualSizes(NSMakeSize(0, 0), maxSize_)) {
+    maxSize_ = [[[self view] window] frame].size;
+    maxSize_.height -= chrome_style::kClientBottomPadding;
+  }
 
   signInDelegate_.reset(
       new autofill::AutofillDialogSignInDelegate(
           dialog_, webContents_.get(),
           dialog_->delegate()->GetWebContents()->GetDelegate(),
-          gfx::Size(NSSizeToCGSize(initialMinSize)),
+          gfx::Size(NSSizeToCGSize(minSize_)),
           gfx::Size(NSSizeToCGSize(maxSize_))));
   webContents_->GetController().LoadURL(
       dialog_->delegate()->SignInUrl(),
