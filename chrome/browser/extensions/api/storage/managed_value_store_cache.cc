838575d8b65597956168602d11ff4311c86437a3
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-13 20:19:01 +0000

    Make the "storage" permission optional to use the "storage.managed" API.
    Extensions can read configuration policy from the chrome.storage.managed API.
    This requires declaring a "storage.managed_schema" manifest key. This change
    automatically inserts the "storage" permission for extensions that declare
    that key in the manifest.
    
    BUG=108992
    
    Review URL: https://codereview.chromium.org/101483005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240729 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/storage/managed_value_store_cache.cc
+++ chrome/browser/extensions/api/storage/managed_value_store_cache.cc
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/manifest.h"
 #include "extensions/common/manifest_constants.h"
 #include "extensions/common/one_shot_event.h"
-#include "extensions/common/permissions/api_permission.h"
 
 using content::BrowserThread;
 
@@ -196,12 +195,8 @@ void ManagedValueStoreCache::ExtensionTracker::LoadSchemas(
     std::string schema_file;
     if (!(*it)->manifest()->GetString(
             manifest_keys::kStorageManagedSchema, &schema_file)) {
-      // TODO(joaodasilva): Remove this. http://crbug.com/240704
-      if ((*it)->HasAPIPermission(APIPermission::kStorage)) {
-        (*components)[(*it)->id()] = policy::Schema();
-      } else {
-        NOTREACHED();
-      }
+      // TODO(joaodasilva): Remove this. http://crbug.com/325349
+      (*components)[(*it)->id()] = policy::Schema();
       continue;
     }
     // The extension should have been validated, so assume the schema exists
--- chrome/common/extensions/api/storage/storage_schema_manifest_handler.cc
+++ chrome/common/extensions/api/storage/storage_schema_manifest_handler.cc
@@ -19,6 +19,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/manifest.h"
 #include "extensions/common/manifest_constants.h"
 #include "extensions/common/permissions/api_permission.h"
+#include "extensions/common/permissions/api_permission_set.h"
+#include "extensions/common/permissions/permissions_data.h"
+#include "extensions/common/permissions/permissions_info.h"
 
 #if defined(ENABLE_CONFIGURATION_POLICY)
 #include "components/policy/core/common/schema.h"
@@ -37,11 +40,6 @@ StorageSchemaManifestHandler::~StorageSchemaManifestHandler() {}
 policy::Schema StorageSchemaManifestHandler::GetSchema(
     const Extension* extension,
     std::string* error) {
-  if (!extension->HasAPIPermission(APIPermission::kStorage)) {
-    *error = base::StringPrintf("The storage permission is required to use %s",
-                                kStorageManagedSchema);
-    return policy::Schema();
-  }
   std::string path;
   extension->manifest()->GetString(kStorageManagedSchema, &path);
   base::FilePath file = base::FilePath::FromUTF8Unsafe(path);
@@ -73,6 +71,13 @@ bool StorageSchemaManifestHandler::Parse(Extension* extension,
         base::StringPrintf("%s must be a string", kStorageManagedSchema));
     return false;
   }
+
+  // If an extension declares the "storage.managed_schema" key then it gets
+  // the "storage" permission implicitly.
+  APIPermissionSet* permission_set =
+      PermissionsData::GetInitialAPIPermissions(extension);
+  permission_set->insert(APIPermission::kStorage);
+
   return true;
 }
 
