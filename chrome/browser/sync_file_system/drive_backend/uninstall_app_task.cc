136eefe0ea3eaf1f543259de372788e02433fe33
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-13 08:41:22 +0000

    [SyncFS] Refine network error handling
    BUG=240165
    
    Review URL: https://codereview.chromium.org/99383004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240549 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_backend/uninstall_app_task.cc
+++ chrome/browser/sync_file_system/drive_backend/uninstall_app_task.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync_file_system/drive_backend/metadata_database.pb.h"
 #include "chrome/browser/sync_file_system/drive_backend/sync_engine_context.h"
 #include "chrome/browser/sync_file_system/drive_backend/tracker_set.h"
+#include "chrome/browser/sync_file_system/drive_backend_v1/drive_file_sync_util.h"
 #include "chrome/browser/sync_file_system/syncable_file_system_util.h"
 #include "google_apis/drive/drive_api_parser.h"
 #include "google_apis/drive/gdata_wapi_parser.h"
@@ -71,7 +72,8 @@ void UninstallAppTask::Run(const SyncStatusCallback& callback) {
 void UninstallAppTask::DidDeleteAppRoot(const SyncStatusCallback& callback,
                                         int64 change_id,
                                         google_apis::GDataErrorCode error) {
-  if (error != google_apis::HTTP_SUCCESS &&
+  SyncStatusCode status = GDataErrorCodeToSyncStatusCode(error);
+  if (status != SYNC_STATUS_OK &&
       error != google_apis::HTTP_NOT_FOUND) {
     callback.Run(SYNC_STATUS_FAILED);
     return;
