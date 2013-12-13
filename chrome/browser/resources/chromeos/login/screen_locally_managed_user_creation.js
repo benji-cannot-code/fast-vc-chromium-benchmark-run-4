946192aa2f7405f90285d5ac11a6d1ad6c561856
merkulova@chromium.org <merkulova@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-13 13:46:34 +0000

    Tab functionality fixed in supervised user creation screen.
    BUG=315801
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/110633005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240621 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/login/screen_locally_managed_user_creation.js
+++ chrome/browser/resources/chromeos/login/screen_locally_managed_user_creation.js
@@ -991,11 +991,15 @@ login.createScreen('LocallyManagedUserCreationScreen',
         this.setButtonDisabledStatus('import', !passwordOk);
         return passwordOk;
       }
+      var imageGrid = this.getScreenElement('image-grid');
+      var imageChosen = !(imageGrid.selectionType == 'camera' &&
+                          imageGrid.cameraLive);
       var canProceed =
           passwordOk &&
           (userName.length > 0) &&
-           this.lastVerifiedName_ &&
-           (userName == this.lastVerifiedName_);
+          this.lastVerifiedName_ &&
+          (userName == this.lastVerifiedName_) &&
+          imageChosen;
 
       this.setButtonDisabledStatus('next', !canProceed);
       return canProceed;
@@ -1406,7 +1410,11 @@ login.createScreen('LocallyManagedUserCreationScreen',
      */
     handleSelect_: function(e) {
       var imageGrid = this.getScreenElement('image-grid');
-      if (!(imageGrid.selectionType == 'camera' && imageGrid.cameraLive)) {
+      this.updateNextButtonForUser_();
+
+      $('managed-user-creation-flip-photo').tabIndex =
+          (imageGrid.selectionType == 'camera') ? 0 : -1;
+      if (!imageGrid.cameraLive || imageGrid.selectionType != 'camera') {
         this.context_.selectedImageUrl = imageGrid.selectedItemUrl;
         chrome.send('supervisedUserSelectImage',
                     [imageGrid.selectedItemUrl, imageGrid.selectionType]);
