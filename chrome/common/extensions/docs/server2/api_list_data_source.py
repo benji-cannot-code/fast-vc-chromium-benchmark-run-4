ff30a63e848b34994a144c8cecd53510916b4fa9
DHNishi@gmail.com <DHNishi@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-13 21:54:25 +0000

    Docserver: Fix a regression where the new API Categorizer was causing many APIs were not private to be categorized as private.
    BUG=304839
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/113233003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240763 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/server2/api_list_data_source.py
+++ chrome/common/extensions/docs/server2/api_list_data_source.py
@@ -55,6 +55,8 @@ class APIListDataSource(object):
         experimental_apis = []
         all_apis = []
         for api_name, api_model in self._api_models.IterModels():
+          if not self._api_categorizer.IsDocumented(platform, api_name):
+            continue
           api = {
             'name': api_name,
             'description': api_model.description,
