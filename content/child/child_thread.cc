6a2c5558b90b7c745bd0ddad2488431296466d34
earthdok@chromium.org <earthdok@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-13 20:46:13 +0000

    Call the LeakSanitizer hook in SuicideOnChannelErrorFilter::OnChannelError().
    Avoid reporting shutdown-only leaks in render process and the like.
    
    BUG=307043
    R=jochen@chromium.org
    
    Review URL: https://codereview.chromium.org/105673007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240740 0039d316-1c4b-4281-b951-d872f2087c98

--- content/child/child_thread.cc
+++ content/child/child_thread.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/allocator_extension.h"
 #include "base/base_switches.h"
 #include "base/command_line.h"
+#include "base/debug/leak_annotations.h"
 #include "base/lazy_instance.h"
 #include "base/message_loop/message_loop.h"
 #include "base/process/kill.h"
@@ -90,6 +91,11 @@ class SuicideOnChannelErrorFilter : public IPC::ChannelProxy::MessageFilter {
       // that write profile data to disk (which happens under profile collection
       // mode).
       alarm(60);
+#if defined(LEAK_SANITIZER)
+      // Invoke LeakSanitizer early to avoid detecting shutdown-only leaks. If
+      // leaks are found, the process will exit here.
+      __lsan_do_leak_check();
+#endif
     } else {
       _exit(0);
     }
