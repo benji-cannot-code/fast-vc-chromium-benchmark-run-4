8dda9abf8edbb3e414dca4414f16572aed0001f9
binji@chromium.org <binji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-13 23:34:25 +0000

    [NaCl SDK] nacl_io: Rename Mount -> Filesystem, MountNode -> Node.
    (This basically renames everything...)
    
    BUG=none
    R=noelallen@chromium.org, sbc@chromium.org
    
    Review URL: https://codereview.chromium.org/113123003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240799 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ native_client_sdk/src/libraries/nacl_io/char_node.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef LIBRARIES_NACL_IO_CHAR_NODE_H_
+#define LIBRARIES_NACL_IO_CHAR_NODE_H_
+
+#include "nacl_io/node.h"
+
+namespace nacl_io {
+
+class CharNode : public Node {
+ public:
+  explicit CharNode(Filesystem* filesystem) : Node(filesystem) {
+    SetType(S_IFCHR);
+  }
+};
+
+}
+
+#endif  // LIBRARIES_NACL_IO_CHAR_NODE_H_
