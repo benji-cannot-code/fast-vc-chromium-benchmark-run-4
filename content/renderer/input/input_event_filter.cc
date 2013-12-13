d489842958d9dc99d47045aeafb0af4a01a6a515
jdduke@chromium.org <jdduke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-13 17:24:55 +0000

    Stop sending input event ack messages for ignored ack types
    A subset of input event types always ignore their ack messages responses from
    the renderer. Stop sending ack messages for those types.
    
    BUG=302852
    
    Review URL: https://codereview.chromium.org/109433014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240686 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/input/input_event_filter.cc
+++ content/renderer/input/input_event_filter.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/message_loop/message_loop_proxy.h"
 #include "cc/input/input_handler.h"
+#include "content/common/input/web_input_event_traits.h"
 #include "content/common/input_messages.h"
 #include "content/common/view_messages.h"
 #include "content/public/common/content_switches.h"
@@ -146,7 +147,8 @@ void InputEventFilter::ForwardToHandler(const IPC::Message& message) {
     return;
   }
 
-  SendACK(event->type, ack, latency_info, routing_id);
+  if (!WebInputEventTraits::IgnoresAckDisposition(event->type))
+    SendACK(event->type, ack, latency_info, routing_id);
 }
 
 void InputEventFilter::SendACK(blink::WebInputEvent::Type type,
