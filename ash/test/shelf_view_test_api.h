7a07f09bdf046eec4ec0b1666e4971fffb2fc7a3
simonhong@chromium.org <simonhong@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-13 14:42:50 +0000

    [ash] Can't rip off an item from left positioned shelf in secondary monitor
    Root window coordinated point is passed to CalculateShelfDistance().
    
    R=skuhne@chromium.org, jamescook@chromium.org
    BUG=328226
    TEST=ash_unittests --gtest_filter=ShelfViewTest.CheckRipOffFromLeftShelfAlignmentWithMultiMonitor
    
    Review URL: https://codereview.chromium.org/113743002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240646 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/test/shelf_view_test_api.h
+++ ash/test/shelf_view_test_api.h
@@ -85,7 +85,7 @@ class ShelfViewTestAPI {
   gfx::Rect GetBoundsForDragInsertInScreen();
 
   // Returns true if item is ripped off.
-  bool IsDraggingShelfItem();
+  bool IsRippedOffFromShelf();
 
   // Returns true if an item is ripped off and entered into shelf.
   bool DraggedItemFromOverflowToShelf();
