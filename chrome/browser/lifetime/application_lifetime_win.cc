19c1daaf9e2311ff42e142f56cb0459c3f255220
zturner@chromium.org <zturner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-13 20:21:24 +0000

    Show the download cancellation confirmation on relaunch to desktop.
    The issue is that previously, in response to the menu click
    handler, we were attempting to show the Confirmation dialog
    (which is asynchronous), and then sending a message to the
    metro viewer to activate the desktop.  This notification
    would terminate the metro viewer process, so the dialog
    would never be displayed.
    
    The fix is to only send the message to the metro viewer
    after we determine that it's safe to close (either because
    the user clicked yes on the cancel dialog, or because there
    were no pending downloads to begin with).
    
    It was necessary for me to remove a DCHECK from the
    StartPageService::ProfileDestroyObserver.  The service's
    profile member was non NULL and the the one being passed in
    as the notification details was null.  I suspect I will need
    to fix this, but I spent a while looking over the code and
    it was not obvious to me how this shutdown flow works, or
    even if this was a valid DCHECK(), so I would like to get
    some input from apps people on this point.
    
    BUG=315924
    R=ananta@chromium.org, jschuh@chromium.org, sky@chromium.org, tapted@chromium.org
    
    Review URL: https://codereview.chromium.org/108773002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240730 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/lifetime/application_lifetime_win.cc
+++ chrome/browser/lifetime/application_lifetime_win.cc
@@ -8,8 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/prefs/pref_service.h"
 #include "base/win/metro.h"
+#include "base/win/windows_version.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/first_run/upgrade_util.h"
+#include "chrome/browser/ui/browser_finder.h"
 #include "chrome/common/pref_names.h"
 #include "content/public/browser/browser_thread.h"
 #include "ui/views/widget/widget.h"
@@ -63,12 +65,6 @@ void AttemptRestartWithModeSwitch() {
 }
 
 #if defined(USE_AURA)
-void ActivateDesktopHelperReply() {
-  AttemptRestart();
-}
-
-void ActivateDesktopIgnore() {}
-
 void ActivateDesktopHelper(AshExecutionStatus ash_execution_status) {
   scoped_ptr<base::Environment> env(base::Environment::Create());
   std::string version_str;
@@ -91,15 +87,10 @@ void ActivateDesktopHelper(AshExecutionStatus ash_execution_status) {
 
   path = path.Append(installer::kDelegateExecuteExe);
 
-  bool ash_exit = ash_execution_status == ASH_TERMINATE;
   // Actually launching the process needs to happen in the metro viewer,
   // otherwise it won't automatically transition to desktop.  So we have
   // to send an IPC to the viewer to do the ShellExecute.
-  aura::HandleActivateDesktop(
-      path,
-      ash_exit,
-      ash_exit ? base::Bind(ActivateDesktopHelperReply) :
-          base::Bind(ActivateDesktopIgnore));
+  aura::HandleActivateDesktop(path, ash_execution_status == ASH_TERMINATE);
 }
 #endif
 
@@ -108,16 +99,7 @@ void AttemptRestartToDesktopMode() {
   prefs->SetString(prefs::kRelaunchMode,
                    upgrade_util::kRelaunchModeDesktop);
 
-#if defined(USE_AURA)
-
-  // We need to PostTask as there is some IO involved.
-  content::BrowserThread::PostTask(
-      content::BrowserThread::PROCESS_LAUNCHER, FROM_HERE,
-      base::Bind(&ActivateDesktopHelper, ASH_TERMINATE));
-
-#else
   AttemptRestart();
-#endif
 }
 
 void AttemptRestartToMetroMode() {
