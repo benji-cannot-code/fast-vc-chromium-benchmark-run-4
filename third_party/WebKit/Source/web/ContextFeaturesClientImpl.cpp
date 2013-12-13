7c98d401d56d9e33ce08a2df871924b6b90cee06
jam@chromium.org <jam@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-13 17:11:08 +0000

    Followup from r163843. I missed a line when relanding the change.
    BUG=304341
    R=jochen@chromium.org
    
    Review URL: https://codereview.chromium.org/115073002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163879 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/ContextFeaturesClientImpl.cpp
+++ third_party/WebKit/Source/web/ContextFeaturesClientImpl.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ContextFeaturesClientImpl.h"
 
 #include "WebDocument.h"
+#include "WebFrameImpl.h"
 #include "WebPermissionClient.h"
 #include "core/dom/Document.h"
 #include "platform/weborigin/SecurityOrigin.h"
