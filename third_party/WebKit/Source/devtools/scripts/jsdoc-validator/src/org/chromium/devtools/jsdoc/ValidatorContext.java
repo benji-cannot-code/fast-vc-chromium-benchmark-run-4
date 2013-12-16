3dbb72d65de6985d92d39096164687317b2786ef
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-16 11:17:23 +0000

    DevTools: [JsDocValidator] Make sure messages are always sorted in the output
    Messages were sorted in the order of node post-visits, which occurs inside-out,
    so for double-nested functions the message order was reversed.
    
    R=aandrey, eustas, sergeyv, vsevik
    
    Review URL: https://codereview.chromium.org/103753005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163953 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/scripts/jsdoc-validator/src/org/chromium/devtools/jsdoc/ValidatorContext.java
+++ third_party/WebKit/Source/devtools/scripts/jsdoc-validator/src/org/chromium/devtools/jsdoc/ValidatorContext.java
@@ -1,19 +1,33 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.devtools.jsdoc;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 public class ValidatorContext {
+
+    private static final Comparator<MessageRecord> MESSAGE_RECORD_COMPARATOR =
+            new Comparator<MessageRecord>() {
+                @Override
+                public int compare(MessageRecord left, MessageRecord right) {
+                    return left.position - right.position;
+                }
+            };
 
     public final ScriptText scriptText;
     public final String scriptFileName;
-    private final StringBuilder validationResult = new StringBuilder();
+    private final SortedSet<MessageRecord> validationResult =
+            new TreeSet<>(MESSAGE_RECORD_COMPARATOR);
 
     public ValidatorContext(ScriptText scriptText, String scriptFileName) {
         this.scriptText = scriptText;
         this.scriptFileName = scriptFileName;
     }
 
-    public String getValidationResult() {
-        return validationResult.toString();
+    public SortedSet<MessageRecord> getValidationResult() {
+        return Collections.unmodifiableSortedSet(validationResult);
     }
 
     public void reportError(int lineNumber, int absoluteOffset, String errorMessage) {
@@ -23,12 +37,23 @@ public class ValidatorContext {
             positionMarker.append(' ');
         }
         positionMarker.append('^');
-        validationResult.append(String.format("%s%s:%d: ERROR - %s\n%s\n%s\n",
-                validationResult.length() > 0 ? "\n" : "",
-                scriptFileName,
-                lineNumber,
-                errorMessage,
-                scriptText.getLineTextAt(absoluteOffset),
-                positionMarker.toString()));
+        validationResult.add(new MessageRecord(
+                absoluteOffset,
+                String.format("%s:%d: ERROR - %s\n%s\n%s\n",
+                        scriptFileName,
+                        lineNumber,
+                        errorMessage,
+                        scriptText.getLineTextAt(absoluteOffset),
+                        positionMarker.toString())));
+    }
+
+    public static class MessageRecord {
+        public final int position;
+        public final String text;
+
+        public MessageRecord(int position, String text) {
+            this.position = position;
+            this.text = text;
+        }
     }
 }
