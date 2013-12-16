a42e23297053493b268067ce8d02cbbde25bf7ba
nbarth@chromium.org <nbarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-16 04:41:31 +0000

    IDL compiler: [CustomConstructor]
    Initial CL.
    (Also a few FIXMEs in the parser, where we can simplify it post-Perl.)
    
    Docs:
    https://sites.google.com/a/chromium.org/dev/blink/webidl/blink-idl-extended-attributes#TOC-CustomConstructor-i-a-
    
    BUG=239771
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/116113002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163941 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/templates/interface.cpp
+++ third_party/WebKit/Source/bindings/templates/interface.cpp
@@ -326,7 +326,7 @@ bool initialize{{cpp_class}}({{cpp_class}}Init& eventInit, const Dictionary& opt
 
 {##############################################################################}
 {% block constructor_callback %}
-{% if constructors or has_event_constructor %}
+{% if constructors or has_custom_constructor or has_event_constructor %}
 void {{v8_class}}::constructorCallback(const v8::FunctionCallbackInfo<v8::Value>& info)
 {
     TRACE_EVENT_SCOPED_SAMPLING_STATE("Blink", "DOMConstructor");
@@ -343,7 +343,11 @@ void {{v8_class}}::constructorCallback(const v8::FunctionCallbackInfo<v8::Value>
         return;
     }
 
+    {% if has_custom_constructor %}
+    {{v8_class}}::constructorCustom(info);
+    {% else %}
     {{cpp_class}}V8Internal::constructor(info);
+    {% endif %}
 }
 
 {% endif %}
@@ -388,7 +392,7 @@ static v8::Handle<v8::FunctionTemplate> Configure{{v8_class}}Template(v8::Handle
         isolate, currentWorldType);
     {% endfilter %}
 
-    {% if constructors or has_event_constructor %}
+    {% if constructors or has_custom_constructor or has_event_constructor %}
     functionTemplate->SetCallHandler({{v8_class}}::constructorCallback);
     functionTemplate->SetLength({{interface_length}});
     {% endif %}
