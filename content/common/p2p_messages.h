aa7bd711c062e0648d0584d1411a3e1a6343e4ab
mallinath@chromium.org <mallinath@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-16 13:51:39 +0000

    Pass packet received timestamp in microseconds from browser process to render process. This timestamp will be used by the webrtc for remote bandwidth estimation.
    Currently webrtc does this in render proces. Due to sandboxing of chrome
    render process will have delay when it calculates from the actual receive value.
    
    R=sergeyu@chromium.org, cdn@chromium.org, joi@chromium.org, sergeyu@chromuim.org
    TBR=cdn@chromium.org, joi@chromium.org
    
    Review URL: https://codereview.chromium.org/104783004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240878 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/p2p_messages.h
+++ content/common/p2p_messages.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // IPC messages for the P2P Transport API.
 // Multiply-included message file, hence no include guard.
 
+#include "base/time/time.h"
 #include "content/common/content_export.h"
 #include "content/public/common/p2p_socket_type.h"
 #include "ipc/ipc_message_macros.h"
@@ -47,10 +48,11 @@ IPC_MESSAGE_CONTROL2(P2PMsg_OnIncomingTcpConnection,
                      int /* socket_id */,
                      net::IPEndPoint /* socket_address */)
 
-IPC_MESSAGE_CONTROL3(P2PMsg_OnDataReceived,
+IPC_MESSAGE_CONTROL4(P2PMsg_OnDataReceived,
                      int /* socket_id */,
                      net::IPEndPoint /* socket_address */,
-                     std::vector<char> /* data */)
+                     std::vector<char> /* data */,
+                     base::TimeTicks /* timestamp */ )
 
 // P2P Socket messages sent from the renderer to the browser.
 
