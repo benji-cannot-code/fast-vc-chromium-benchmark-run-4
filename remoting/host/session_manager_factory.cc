0bb6a10dfa9f21585c4af233df352805d3588766
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-16 18:08:39 +0000

    Fix LibjingleTransportFactory to refresh STUN/Relay.
    Previously chromoting host would get relay info only when started. Relay
    token expires within several hours, which means relay session creation would
    always fail from the host running for longer than several hours, and that
    makes connection impossible in some configurations. Moved code that
    gets Jingle info to LibjingleTransportFactory and it's responsible for
    refreshing relay token every hour.
    
    BUG=318897
    R=rmsousa@chromium.org
    
    Review URL: https://codereview.chromium.org/98173006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240926 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/session_manager_factory.cc
+++ remoting/host/session_manager_factory.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace remoting {
 
 scoped_ptr<protocol::SessionManager> CreateHostSessionManager(
+    SignalStrategy* signal_strategy,
     const NetworkSettings& network_settings,
     const scoped_refptr<net::URLRequestContextGetter>&
         url_request_context_getter) {
@@ -23,22 +24,14 @@ scoped_ptr<protocol::SessionManager> CreateHostSessionManager(
       ChromiumPortAllocator::Create(url_request_context_getter,
           network_settings));
 
-  bool incoming_only = network_settings.nat_traversal_mode ==
-      NetworkSettings::NAT_TRAVERSAL_DISABLED;
-
   scoped_ptr<protocol::TransportFactory> transport_factory(
       new protocol::LibjingleTransportFactory(
+          signal_strategy,
           port_allocator.PassAs<cricket::HttpPortAllocatorBase>(),
-          incoming_only));
-
-  // Use the Jingle protocol for channel-negotiation signalling between
-  // peer TransportFactories.
-  bool fetch_stun_relay_info = network_settings.nat_traversal_mode ==
-      NetworkSettings::NAT_TRAVERSAL_ENABLED;
+          network_settings));
 
   scoped_ptr<protocol::JingleSessionManager> session_manager(
-      new protocol::JingleSessionManager(
-          transport_factory.Pass(), fetch_stun_relay_info));
+      new protocol::JingleSessionManager(transport_factory.Pass()));
   return session_manager.PassAs<protocol::SessionManager>();
 }
 
--- remoting/protocol/jingle_session_manager.h
+++ remoting/protocol/jingle_session_manager.h
@@ -31,7 +31,6 @@ class SocketAddress;
 namespace remoting {
 
 class IqSender;
-class JingleInfoRequest;
 
 namespace protocol {
 
@@ -45,15 +44,7 @@ class TransportFactory;
 class JingleSessionManager : public SessionManager,
                              public SignalStrategy::Listener {
  public:
-  // When |fetch_stun_relay_config| is set to true then
-  // JingleSessionManager will also try to query configuration of STUN
-  // and Relay servers from the signaling server.
-  //
-  // TODO(sergeyu): Move NAT-traversal config fetching to a separate
-  // class.
-  explicit JingleSessionManager(
-      scoped_ptr<TransportFactory> transport_factory,
-      bool fetch_stun_relay_config);
+  explicit JingleSessionManager(scoped_ptr<TransportFactory> transport_factory);
   virtual ~JingleSessionManager();
 
   // SessionManager interface.
@@ -99,8 +90,6 @@ class JingleSessionManager : public SessionManager,
   SessionManager::Listener* listener_;
 
   bool ready_;
-
-  scoped_ptr<JingleInfoRequest> jingle_info_request_;
 
   SessionsMap sessions_;
 
