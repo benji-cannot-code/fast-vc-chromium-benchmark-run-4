278dec3a56723414c975c06e525485e2587ff586
wfh@chromium.org <wfh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-16 18:53:16 +0000

    Add histograms for whether the OS password is blank or not
    BUG=303113
    
    Review URL: https://codereview.chromium.org/114893004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240950 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/password_manager/password_manager.cc
+++ chrome/browser/password_manager/password_manager.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/password_manager/password_form_manager.h"
 #include "chrome/browser/password_manager/password_manager_delegate.h"
 #include "chrome/browser/password_manager/password_manager_metrics_util.h"
+#include "chrome/browser/password_manager/password_manager_util.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/passwords/manage_passwords_bubble_ui_controller.h"
 #include "chrome/common/chrome_switches.h"
@@ -22,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "components/autofill/content/common/autofill_messages.h"
 #include "components/user_prefs/pref_registry_syncable.h"
+#include "content/public/browser/browser_thread.h"
 #include "content/public/browser/navigation_details.h"
 #include "content/public/browser/user_metrics.h"
 #include "content/public/browser/web_contents.h"
@@ -30,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using autofill::PasswordForm;
 using autofill::PasswordFormMap;
+using content::BrowserThread;
 using content::UserMetricsAction;
 using content::WebContents;
 
@@ -40,6 +43,15 @@ namespace {
 const char kSpdyProxyRealm[] = "/SpdyProxy";
 const char kOtherPossibleUsernamesExperiment[] =
     "PasswordManagerOtherPossibleUsernames";
+
+void ReportOsPassword() {
+  password_manager_util::OsPasswordStatus status =
+      password_manager_util::GetOsPasswordStatus();
+
+  UMA_HISTOGRAM_ENUMERATION("PasswordManager.OsPasswordStatus",
+                            status,
+                            password_manager_util::MAX_PASSWORD_STATUS);
+}
 
 // This routine is called when PasswordManagers are constructed.
 //
@@ -56,6 +68,13 @@ void ReportMetrics(bool password_manager_enabled) {
   if (ran_once)
     return;
   ran_once = true;
+
+  // Avoid checking OS password until later on in browser startup
+  // since it calls a few Windows APIs.
+  BrowserThread::PostDelayedTask(BrowserThread::UI,
+                                 FROM_HERE,
+                                 base::Bind(&ReportOsPassword),
+                                 base::TimeDelta::FromSeconds(10));
 
   UMA_HISTOGRAM_BOOLEAN("PasswordManager.Enabled", password_manager_enabled);
 }
--- chrome/chrome_browser.gypi
+++ chrome/chrome_browser.gypi
@@ -1382,6 +1382,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/password_manager/password_manager_metrics_util.cc',
         'browser/password_manager/password_manager_metrics_util.h',
         'browser/password_manager/password_manager_util.h',
+        'browser/password_manager/password_manager_util_android.cc',
         'browser/password_manager/password_manager_util_mac.mm',
         'browser/password_manager/password_manager_util_linux.cc',
         'browser/password_manager/password_manager_util_win.cc',
