3f53dc694e1b3161e57fe08743a435a13c483fc8
mcasas@chromium.org <mcasas@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-16 23:02:50 +0000

    AVFoundation: Allow exceptions when checking if max frame rate setting is supported.
    Addressing some sporadic crashes, see http://crbug.com/327532, with 
    "Selector not found" when trying to query the AVCaptureConnection if 
    it supports setting max video frame rate, at [1].
    
    Beyond that, this problem started affecting all non-HD cameras in Mac
    Os X < 10.9 -- see log in http://crbug.com/328096--.
    
    This CL checks if the selector is recognised before attemping to use it.
    
    [1] https://code.google.com/p/chromium/codesearch#chromium/src/media/video/capture/mac/video_capture_device_avfoundation_mac.mm&q=isvideomaxframe&sq=package:chromium&type=cs&l=174
    BUG=288562, 327532, 328096
    
    Review URL: https://codereview.chromium.org/99353005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241056 0039d316-1c4b-4281-b951-d872f2087c98

--- media/video/capture/mac/video_capture_device_avfoundation_mac.mm
+++ media/video/capture/mac/video_capture_device_avfoundation_mac.mm
@@ -165,11 +165,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   CrAVCaptureConnection* captureConnection = [captureVideoDataOutput_
       connectionWithMediaType:AVFoundationGlue::AVMediaTypeVideo()];
-  if ([captureConnection isVideoMinFrameDurationSupported]) {
+  // TODO(mcasas): Check selector existence, related to bugs
+  // http://crbug.com/327532 and http://crbug.com/328096.
+  if ([captureConnection
+           respondsToSelector:@selector(isVideoMinFrameDurationSupported)] &&
+      [captureConnection isVideoMinFrameDurationSupported]) {
     [captureConnection setVideoMinFrameDuration:
         CoreMediaGlue::CMTimeMake(1, frameRate)];
   }
-  if ([captureConnection isVideoMaxFrameDurationSupported]) {
+  if ([captureConnection
+           respondsToSelector:@selector(isVideoMaxFrameDurationSupported)] &&
+      [captureConnection isVideoMaxFrameDurationSupported]) {
     [captureConnection setVideoMaxFrameDuration:
         CoreMediaGlue::CMTimeMake(1, frameRate)];
   }
