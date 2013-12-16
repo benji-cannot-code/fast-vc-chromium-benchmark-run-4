346998bdc7a3a6b32446fecd12e3aef4ad12f657
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-16 20:39:30 +0000

    Mojo: "Finish" local (in-process) implementation of data pipes.
    Still to do:
    - Write tests.
    - Fix bug in MojoReadData()/MojoWriteData() (see data_pipe.cc).
    - Write comments in core.h.
    
    R=darin@chromium.org
    
    Review URL: https://codereview.chromium.org/113603003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241015 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/mojo.gyp
+++ mojo/mojo.gyp
@@ -88,6 +88,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'system/dispatcher.cc',
         'system/dispatcher.h',
         'system/limits.h',
+        'system/local_data_pipe.cc',
+        'system/local_data_pipe.h',
         'system/local_message_pipe_endpoint.cc',
         'system/local_message_pipe_endpoint.h',
         'system/memory.cc',
--- mojo/system/limits.h
+++ mojo/system/limits.h
@@ -3,8 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// TODO(vtl): Rename this file to constants.h.
+
 #ifndef MOJO_SYSTEM_LIMITS_H_
 #define MOJO_SYSTEM_LIMITS_H_
+
+#include <stddef.h>
 
 namespace mojo {
 namespace system {
@@ -19,6 +23,19 @@ const size_t kMaxWaitManyNumHandles = kMaxHandleTableSize;
 const size_t kMaxMessageNumBytes = 4 * 1024 * 1024;
 
 const size_t kMaxMessageNumHandles = 10000;
+
+// Maximum capacity of a data pipe, in bytes. This value must fit into a
+// |uint32_t|.
+// WARNING: If you bump it closer to 2^32, you must audit all the code to check
+// that we don't overflow (2^31 would definitely be risky; up to 2^30 is
+// probably okay).
+const size_t kMaxDataPipeCapacityBytes = 256 * 1024 * 1024;  // 256 MB.
+
+const size_t kDefaultDataPipeCapacityBytes = 1024 * 1024;  // 1 MB.
+
+// Alignment for the "start" of the data buffer used by data pipes. (The
+// alignment of elements will depend on this and the element size.)
+const size_t kDataPipeBufferAlignmentBytes = 16;
 
 }  // namespace system
 }  // namespace mojo
