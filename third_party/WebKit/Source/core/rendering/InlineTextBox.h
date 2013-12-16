1be5293bc701731e3c281df47687dd47590b1c2e
yuki.sekiguchi@access-company.com <yuki.sekiguchi@access-company.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-16 03:32:42 +0000

    text-overflow: ellipsis should support vertical writing mode.
    Ellipsis layout logic only considers horizontal writing mode. Therefore,
    this patch replaced most layout code with logicalXXX().
    
    EllipsisBox cannot draw/hit-test vertical text. This patch is mostly
    copied drawing/hit-testing vertical text logic from InlineTextBox.
    
    Currently -webkit-line-clamp doesn't support vertical writing
    mode. Added FIXME to line-clamp logic.
    
    Changed InlineBox::width() and height() to use virtualLogicalHeight().
    This allows EllipsisBox use InlineBox's method like
    locationIncludingFlipping().
    
    Made rotation() static method of InlineTextBox to use it from EllipsisBox.
    
    Since -webkit-line-clamp will be removed, this didn't implement
    -webkit-line-clamp in vertical writing mode.
    
    BUG=318035
    
    Review URL: https://codereview.chromium.org/64833003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163936 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/InlineTextBox.h
+++ third_party/WebKit/Source/core/rendering/InlineTextBox.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/rendering/InlineBox.h"
 #include "core/rendering/RenderText.h" // so textRenderer() can be inline
+#include "platform/graphics/GraphicsContext.h"
 #include "platform/text/TextRun.h"
 #include "wtf/Forward.h"
 
@@ -98,6 +99,8 @@ public:
     virtual const char* boxName() const;
 #endif
 
+    enum RotationDirection { Counterclockwise, Clockwise };
+    static AffineTransform rotation(const FloatRect& boxRect, RotationDirection);
 private:
     LayoutUnit selectionTop();
     LayoutUnit selectionBottom();
@@ -199,6 +202,12 @@ inline RenderText* InlineTextBox::textRenderer() const
 }
 
 void alignSelectionRectToDevicePixels(FloatRect&);
+
+inline AffineTransform InlineTextBox::rotation(const FloatRect& boxRect, RotationDirection rotationDirection)
+{
+    return rotationDirection == Clockwise ? AffineTransform(0, 1, -1, 0, boxRect.x() + boxRect.maxY(), boxRect.maxY() - boxRect.x())
+        : AffineTransform(0, -1, 1, 0, boxRect.x() - boxRect.maxY(), boxRect.x() + boxRect.maxY());
+}
 
 } // namespace WebCore
 
