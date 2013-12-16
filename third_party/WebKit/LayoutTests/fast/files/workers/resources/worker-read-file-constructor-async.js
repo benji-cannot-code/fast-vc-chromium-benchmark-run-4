3f854203eeefd58c315366bd4a6cab9728267118
sigbjornf@opera.com <sigbjornf@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-16 13:40:37 +0000

    Make cloning of File objects more useful.
    Make the structured cloning of File objects complete, extending their
    wire format to also include:
    
     * name
     * relative path
     * size
     * last modification date.
    
    As this changes the format, bump the serialization version number up
    to 4. The deserializer is backwards compatible, including for File
    objects; verify this to be so through tests.
    
    Files constructed via the File constructor weren't correctly cloned
    as-was; added a Worker test to have some coverage.
    
    R=dslomov@chromium.org,jochen@chromium.org
    BUG=171905
    TEST=fast/storage/serialized-script-value
    TEST=fast/files/workers/worker-read-file-constructor-async
    
    Review URL: https://codereview.chromium.org/100023005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163956 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/files/workers/resources/worker-read-file-constructor-async.js
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+importScripts("../../resources/read-common.js")
+
+function log(message)
+{
+    postMessage(message);
+}
+
+function readFiles(index, files)
+{
+    if (index >= files.length) {
+        log("DONE");
+        return;
+    }
+
+    log("Reading: '" + files[index].name + "'");
+    log("Last modified: '" + (new Date(files[index].lastModified)).toUTCString() + "'");
+
+    var reader = new FileReader();
+    var isText = files[index].type.indexOf("text") > -1;
+    reader.onload = function (e) {
+        if (isText) {
+            log("Contents: '" + reader.result + "'");
+            log("Length: " + reader.result.length);
+        } else
+            log("Length: " + reader.result.byteLength);
+        readFiles(index + 1, files);
+    };
+    if (isText)
+        reader.readAsText(files[index]);
+    else
+        reader.readAsArrayBuffer(files[index])
+}
+
+onmessage = function (e) {
+    log('Received files in worker');
+    readFiles(0, e.data);
+};
