12fa160f18b92c4d70807ca7f33fe8fdc7df8e33
aruslan@chromium.org <aruslan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-16 21:31:25 +0000

    [rAc Android] Destroy the glue on Hide.
    BUG=328205
    
    Review URL: https://codereview.chromium.org/99133018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241029 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/android/autofill/autofill_dialog_controller_android.cc
+++ chrome/browser/ui/android/autofill/autofill_dialog_controller_android.cc
@@ -320,8 +320,7 @@ void AutofillDialogControllerAndroid::Show() {
 }
 
 void AutofillDialogControllerAndroid::Hide() {
-  // TODO(aruslan): make the view actually hide. http://crbug.com/328205
-  NOTIMPLEMENTED();
+  delete this;
 }
 
 void AutofillDialogControllerAndroid::TabActivated() {}
