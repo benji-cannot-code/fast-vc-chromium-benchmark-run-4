54221b4092abb8915c55cafaba0e8b1a592a0e7c
dimich@chromium.org <dimich@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-17 12:46:04 +0000

    Move common functionality to a base class MessageView. This includes background_view and ::Layout().
    BUG=320827
    
    Review URL: https://codereview.chromium.org/114813004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241267 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/message_center/views/message_view.h
+++ ui/message_center/views/message_view.h
@@ -42,7 +42,8 @@ const int kPaddingHorizontal = 18;
 const int kWebNotificationButtonWidth = 32;
 const int kWebNotificationIconSize = 40;
 
-// An abstract class that forms the basis of a view for a notification entry.
+// An base class for a notification entry. Contains background, close button
+// and other elements shared by derived notification views.
 class MESSAGE_CENTER_EXPORT MessageView : public views::SlideOutView,
                                           public views::ButtonListener {
  public:
@@ -71,6 +72,7 @@ class MESSAGE_CENTER_EXPORT MessageView : public views::SlideOutView,
   virtual void OnPaint(gfx::Canvas* canvas) OVERRIDE;
   virtual void OnFocus() OVERRIDE;
   virtual void OnBlur() OVERRIDE;
+  virtual void Layout() OVERRIDE;
 
   // Overridden from ui::EventHandler:
   virtual void OnGestureEvent(ui::GestureEvent* event) OVERRIDE;
@@ -95,6 +97,7 @@ class MESSAGE_CENTER_EXPORT MessageView : public views::SlideOutView,
   std::string notification_id_;
   NotifierId notifier_id_;
   scoped_ptr<MessageViewContextMenuController> context_menu_controller_;
+  views::View* background_view_;  // Owned by views hierarchy.
   scoped_ptr<views::ImageButton> close_button_;
   views::ScrollView* scroller_;
 
--- ui/message_center/views/notification_view.cc
+++ ui/message_center/views/notification_view.cc
@@ -55,12 +55,6 @@ const int kExpandIconBottomPadding = 8;
 const int kExpandIconRightPadding = 11;
 
 // static
-views::Background* MakeBackground(
-    SkColor color = message_center::kNotificationBackgroundColor) {
-  return views::Background::CreateSolidBackground(color);
-}
-
-// static
 views::Border* MakeEmptyBorder(int top, int left, int bottom, int right) {
   return views::Border::CreateEmptyBorder(top, left, bottom, right);
 }
@@ -311,11 +305,6 @@ NotificationView::NotificationView(MessageCenterController* controller,
       clickable_(notification.clickable()),
       is_expanded_(expanded) {
   std::vector<string16> accessible_lines;
-
-  // Create the opaque background that's above the view's shadow.
-  background_view_ = new views::View();
-  background_view_->set_background(MakeBackground());
-
   // Create the top_view_, which collects into a vertical box all content
   // at the top of the notification (to the right of the icon) except for the
   // close button.
@@ -419,7 +408,8 @@ NotificationView::NotificationView(MessageCenterController* controller,
     icon_view_ = new ProportionalImageView(icon);
   }
 
-  icon_view_->set_background(MakeBackground(kIconBackgroundColor));
+  icon_view_->set_background(
+      views::Background::CreateSolidBackground(kIconBackgroundColor));
 
   // Create the bottom_view_, which collects into a vertical box all content
   // below the notification icon except for the expand button.
@@ -466,7 +456,6 @@ NotificationView::NotificationView(MessageCenterController* controller,
   // for proper layout logic and it also allows the close and expand buttons to
   // overlap the content as needed to provide large enough click and touch areas
   // (<http://crbug.com/168822> and <http://crbug.com/168856>).
-  AddChildView(background_view_);
   AddChildView(top_view_);
   AddChildView(icon_view_);
   AddChildView(bottom_view_);
@@ -515,6 +504,7 @@ int NotificationView::GetHeightForWidth(int width) {
 }
 
 void NotificationView::Layout() {
+  MessageView::Layout();
   gfx::Insets insets = GetInsets();
   int content_width = width() - insets.width();
   int content_right = width() - insets.right();
@@ -522,10 +512,6 @@ void NotificationView::Layout() {
   // Before any resizing, set or adjust the number of message lines.
   if (message_view_)
     message_view_->SetLineLimit(GetMessageLineLimit(width()));
-
-  // Background.
-  background_view_->SetBounds(insets.left(), insets.top(),
-                              content_width, height() - insets.height());
 
   // Top views.
   int top_height = top_view_->GetHeightForWidth(content_width);
@@ -539,11 +525,6 @@ void NotificationView::Layout() {
   int bottom_height = bottom_view_->GetHeightForWidth(content_width);
   bottom_view_->SetBounds(insets.left(), bottom_y,
                           content_width, bottom_height);
-
-  // Close button.
-  gfx::Size close_size(close_button()->GetPreferredSize());
-  close_button()->SetBounds(content_right - close_size.width(), insets.top(),
-                            close_size.width(), close_size.height());
 
   // Expand button.
   gfx::Size expand_size(expand_button_->GetPreferredSize());
