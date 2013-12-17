67804516af8419d13fcdabb8976b77c8f3128182
kiyun@chromium.org <kiyun@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-17 08:14:18 +0000

    Revert "Disk cache: Increase the maximum number of threads on the cache pool to 20."
    reverts r239384
    
    This reverts commit fefa1666597f12a7911caf93bf8983e83ba39c80.
    
    > Disk cache: Increase the maximum number of threads on the cache pool to 20.
    >
    > Speculative fix to see if there's any perf change.
    >
    > BUG=325311
    > TEST=none
    > R=willchan@chromium.org
    >
    > Review URL: https://codereview.chromium.org/109363002
    
    We did not see an improvement.
    
    BUG=325311
    
    Review URL: https://codereview.chromium.org/113873003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241207 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/file_posix.cc
+++ net/disk_cache/file_posix.cc
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // The maximum number of threads for this pool.
-const int kMaxThreads = 20;
+const int kMaxThreads = 5;
 
 class FileWorkerPool : public base::SequencedWorkerPool {
  public:
