34f56afd04fa5e4a6a44200813a79ab63159d8ad
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-17 07:23:10 +0000

    [rAC, OSX] Highlight errors on section change.
    Errors weren't properly highlighted on section change, due to the fact
    that the data to be validated (suggestion section vs. input section)
    was picked based on visibility - but to avoid flicker, visibility is
    only updated during performLayout.
    
    New code queries flag for delayed visibility change instead.
    
    BUG=312307
    TEST=see instructions on bug
    R=isherman@chromium.org
    
    Review URL: https://codereview.chromium.org/108043008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241183 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/autofill/autofill_section_container.mm
+++ chrome/browser/ui/cocoa/autofill/autofill_section_container.mm
@@ -375,7 +375,7 @@ bool ShouldOverwriteComboboxes(autofill::DialogSection section,
 
 - (BOOL)validateFor:(autofill::ValidationType)validationType {
   NSArray* fields = nil;
-  if (![inputs_ isHidden]) {
+  if (!showSuggestions_) {
     fields = [inputs_ subviews];
   } else if ([self isCreditCardSection]) {
     if (![[suggestContainer_ inputField] isHidden])
