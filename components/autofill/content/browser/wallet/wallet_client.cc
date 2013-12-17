f23613313e5807c7f3d865c41b76a82386c514bd
scr@chromium.org <scr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-17 08:39:52 +0000

    Address a TODO to change some VLOGs to DVLOG after dogfood.
    BUG=261871
    
    Review URL: https://codereview.chromium.org/114973005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241221 0039d316-1c4b-4281-b951-d872f2087c98

--- components/autofill/content/browser/wallet/wallet_client.cc
+++ components/autofill/content/browser/wallet/wallet_client.cc
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_fetcher.h"
 #include "net/url_request/url_request_context_getter.h"
 
-// TODO(ahutter): Change all VLOGs to DVLOGs after dogfood.
 namespace autofill {
 namespace wallet {
 
@@ -535,7 +534,7 @@ void WalletClient::MakeWalletRequest(const GURL& url,
   request_.reset(net::URLFetcher::Create(
       0, url, net::URLFetcher::POST, this));
   request_->SetRequestContext(context_getter_.get());
-  VLOG(1) << "Making request to " << url << " with post_body=" << post_body;
+  DVLOG(1) << "Making request to " << url << " with post_body=" << post_body;
   request_->SetUploadData(mime_type, post_body);
   request_->AddExtraRequestHeader("Authorization: GoogleLogin auth=" +
                                   delegate_->GetWalletCookieValue());
@@ -558,7 +557,7 @@ void WalletClient::OnURLFetchComplete(
       base::Time::Now() - request_started_timestamp_);
 
   DCHECK_EQ(source, request_.get());
-  VLOG(1) << "Got response from " << source->GetOriginalURL();
+  DVLOG(1) << "Got response from " << source->GetOriginalURL();
 
   // |request_|, which is aliased to |source|, might continue to be used in this
   // |method, but should be freed once control leaves the method.
@@ -574,7 +573,7 @@ void WalletClient::OnURLFetchComplete(
 
   std::string data;
   source->GetResponseAsString(&data);
-  VLOG(1) << "Response body: " << data;
+  DVLOG(1) << "Response body: " << data;
 
   scoped_ptr<base::DictionaryValue> response_dict;
 
@@ -774,7 +773,7 @@ void WalletClient::HandleWalletError(WalletClient::ErrorType error_type) {
       break;
   }
 
-  VLOG(1) << "Wallet encountered a " << error_message;
+  DVLOG(1) << "Wallet encountered a " << error_message;
 
   delegate_->OnWalletError(error_type);
   delegate_->GetMetricLogger().LogWalletErrorMetric(
