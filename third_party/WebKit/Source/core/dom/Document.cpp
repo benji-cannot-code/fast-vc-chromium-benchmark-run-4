d9c00d41a9d269e25facbf7f85d77ab3f42654f2
mikelawther@chromium.org <mikelawther@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-17 11:00:48 +0000

    Ensure the current frame is not lost during testing of whether a style update is needed.
    BUG=315889
    
    Review URL: https://codereview.chromium.org/109263007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164006 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -1735,6 +1735,7 @@ void Document::updateStyleIfNeeded()
     if (!needsStyleRecalc() && !childNeedsStyleRecalc() && !childNeedsDistributionRecalc())
         return;
 
+    RefPtr<Frame> holder(m_frame);
     AnimationUpdateBlock animationUpdateBlock(m_frame ? &m_frame->animation() : 0);
     recalcStyle(NoChange);
     DocumentAnimations::serviceAfterStyleRecalc(*this);
