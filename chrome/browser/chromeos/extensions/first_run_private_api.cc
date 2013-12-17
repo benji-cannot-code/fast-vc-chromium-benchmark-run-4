f89b95906b1ccef781735fd629c599c24af58a15
dzhioev@chromium.org <dzhioev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-17 17:47:08 +0000

    Added syncable priority pref for ChromeOS first-run UI.
    First-run UI will be shown for user only once.
    Also changed place where we decide to show or not to show first-run tutorial
    for user. Now decision made in the same place, where we made decision for
    "Tips & Tricks" app.
    
    BUG=269739,323261
    
    Review URL: https://codereview.chromium.org/116543002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241315 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/extensions/first_run_private_api.cc
+++ chrome/browser/chromeos/extensions/first_run_private_api.cc
@@ -45,6 +45,6 @@ bool FirstRunPrivateGetLocalizedStringsFunction::RunImpl() {
 }
 
 bool FirstRunPrivateLaunchTutorialFunction::RunImpl() {
-  chromeos::LaunchFirstRunTutorial();
+  chromeos::first_run::LaunchTutorial();
   return true;
 }
--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -275,6 +275,7 @@ extern const char kRAConsentFirstTime[];
 extern const char kMultiProfileNotificationDismissed[];
 extern const char kMultiProfileUserBehavior[];
 extern const char kHighQualitySpeechSynthesisLanguages[];
+extern const char kFirstRunTutorialShown[];
 #endif  // defined(OS_CHROMEOS)
 extern const char kIpcDisabledMessages[];
 extern const char kShowHomeButton[];
