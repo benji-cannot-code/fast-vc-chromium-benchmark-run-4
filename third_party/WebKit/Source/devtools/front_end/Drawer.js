benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 8b02584a1b458365068977dc47dfd2c5dda4480c

DevTools: Resize panel after hiding drawer.
This fixes a bug when a white strip is shown in the bottom of the editor after closing the drawer.

BUG=328836

Review URL: https://codereview.chromium.org/116393002

git-svn-id: svn://svn.chromium.org/blink/trunk@163968 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/devtools/front_end/Drawer.js
@@ -214,8 +214,7 @@ WebInspector.Drawer.prototype = {
          */
         function animationCallback(finished)
         {
-            if (this._inspectorView.currentPanel())
-                this._inspectorView.currentPanel().doResize();
+            this._inspectorView.currentPanel().doResize();
             if (!finished)
                 return;
             this._updateTabStrip();
@@ -268,13 +267,15 @@ WebInspector.Drawer.prototype = {
          */
         function animationCallback(finished)
         {
-            if (this._inspectorView.currentPanel())
-                this._inspectorView.currentPanel().doResize();
-            if (!finished)
+            var panel = this._inspectorView.currentPanel();
+            if (!finished) {
+                panel.doResize();
                 return;
+            }
             this._tabbedPane.detach();
             this._drawerContentsElement.removeChildren();
             document.body.classList.remove("drawer-visible");
+            panel.doResize();
             delete this._currentAnimation;
             delete this._isHiding;
         }

commit 151f0a58cabe4fa245bbcfb6aa261ebec2dba01a

[DevTools] Fix "emulate device on load" behavior broken in r163968.
BUG=none

Review URL: https://codereview.chromium.org/102743003

git-svn-id: svn://svn.chromium.org/blink/trunk@164018 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/devtools/front_end/Drawer.js
@@ -214,7 +214,8 @@ WebInspector.Drawer.prototype = {
          */
         function animationCallback(finished)
         {
-            this._inspectorView.currentPanel().doResize();
+            if (this._inspectorView.currentPanel())
+                this._inspectorView.currentPanel().doResize();
             if (!finished)
                 return;
             this._updateTabStrip();