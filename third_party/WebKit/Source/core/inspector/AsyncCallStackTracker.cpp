98048332fa32cd2af721610fe61bbb57878a8d0d
aandrey@chromium.org <aandrey@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-17 13:00:50 +0000

    DevTools: Do not delete self in LifecycleObserver.contextDestroyed() callback in AsyncCallStackTracker.
    We can not delete self in the contextDestroyed() callback due to design of the LifecycleObserver. Instead defer deleting destroyed context data.
    
    Drive-by: Get rid of memcpy using HashMap.swap().
    
    BUG=328366
    R=yurys@chromium.org, pfeldman@chromium.org, yurys
    
    Review URL: https://codereview.chromium.org/99053005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164012 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/AsyncCallStackTracker.cpp
+++ third_party/WebKit/Source/core/inspector/AsyncCallStackTracker.cpp
@@ -48,8 +48,11 @@ public:
 
     virtual void contextDestroyed() OVERRIDE
     {
-        m_tracker->contextDestroyed(executionContext());
+        ASSERT(executionContext());
+        ExecutionContextData* self = m_tracker->m_executionContextDataMap.take(executionContext());
+        ASSERT(self == this);
         ContextLifecycleObserver::contextDestroyed();
+        delete self;
     }
 
 private:
@@ -194,13 +197,6 @@ bool AsyncCallStackTracker::validateCallFrames(const ScriptValue& callFrames)
     return !callFrames.hasNoValue();
 }
 
-void AsyncCallStackTracker::contextDestroyed(ExecutionContext* context)
-{
-    ASSERT(context);
-    if (ExecutionContextData* data = m_executionContextDataMap.take(context))
-        delete data;
-}
-
 AsyncCallStackTracker::ExecutionContextData* AsyncCallStackTracker::createContextDataIfNeeded(ExecutionContext* context)
 {
     ExecutionContextData* data = m_executionContextDataMap.get(context);
@@ -214,11 +210,10 @@ AsyncCallStackTracker::ExecutionContextData* AsyncCallStackTracker::createContex
 void AsyncCallStackTracker::clear()
 {
     m_currentAsyncCallChain = 0;
-    Vector<ExecutionContextData*> contextsData;
-    copyValuesToVector(m_executionContextDataMap, contextsData);
-    m_executionContextDataMap.clear();
-    for (Vector<ExecutionContextData*>::const_iterator it = contextsData.begin(); it != contextsData.end(); ++it)
-        delete *it;
+    ExecutionContextDataMap copy;
+    m_executionContextDataMap.swap(copy);
+    for (ExecutionContextDataMap::const_iterator it = copy.begin(); it != copy.end(); ++it)
+        delete it->value;
 }
 
 } // namespace WebCore
