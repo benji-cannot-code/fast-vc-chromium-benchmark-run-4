63e0993244511155b6f50600ad724eae8269ffc5
joi@chromium.org <joi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-17 10:28:43 +0000

    Only include the Hangout Services extension in Google Chrome builds.
    NOTRY=true
    BUG=291271
    
    Review URL: https://codereview.chromium.org/101323010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241248 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/component_loader.cc
+++ chrome/browser/extensions/component_loader.cc
@@ -289,8 +289,10 @@ void ComponentLoader::AddFileManagerExtension() {
 }
 
 void ComponentLoader::AddHangoutServicesExtension() {
+#if defined(GOOGLE_CHROME_BUILD)
   Add(IDR_HANGOUT_SERVICES_MANIFEST,
       base::FilePath(FILE_PATH_LITERAL("hangout_services")));
+#endif
 }
 
 void ComponentLoader::AddImageLoaderExtension() {
