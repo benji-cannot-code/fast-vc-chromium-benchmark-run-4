bf72da769cef97dfc7bf8352e040591a45b1b512
jansson@chromium.org <jansson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-17 14:42:05 +0000

    Removed getDevices from window.onload and added a checkbox letting the user choose if it should be run when the page loads or not.
    This is for making it easier testing using sourceID constraints
    before getDevices has been called upon.
    
    Also removed the audio and video prefix labels in getDevices() when
    enumerating devices in order to show more of the sourceID string in
    the device dropdown menus. Renamed the refresh-devices button to get-devices.
    
    Noticed a minor error in failTest (sentToTest should be sendToTest) and changed it to returnToTest so it would
    better accommodate peerconnection.html needs. Ran WebrtcBrowser_tests to make sure it did not break anything.
    
    BUG=313192
    NOTRY=TRUE
    TEST= WebrtcBrowserTest.MANUAL* and manually setup p2p call, request getUserMedia with/without sourceID etc. 
    
    Steps to clear LocalStorage for testing:
    Delete cookies and other site and plug-in data entry in the Clear browsing data dialog
    (chrome://chrome/settings/clearBrowserData
    
    Review URL: https://codereview.chromium.org/111543003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241282 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/webrtc/test_functions.js
+++ chrome/test/data/webrtc/test_functions.js
@@ -85,7 +85,7 @@ function sendToTest(message) {
  */
 function failTest(reason) {
   console.error(reason);
-  sendToTest('Test failed: ' + reason);
+  returnToTest('Test failed: ' + reason);
   return new Error(reason);
 }
 
