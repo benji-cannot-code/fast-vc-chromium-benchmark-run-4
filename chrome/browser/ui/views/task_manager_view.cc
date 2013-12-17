8090456f10fcb8620c338cd35de25604d266711e
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-17 11:53:09 +0000

    Linux Aura: Hook up ctrl+w to the task manager.
    BUG=318507
    TEST=Open the task manager, pressing ctrl+w should close it.
    
    Review URL: https://codereview.chromium.org/108953007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241257 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/task_manager_view.cc
+++ chrome/browser/ui/views/task_manager_view.cc
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/models/simple_menu_model.h"
 #include "ui/base/models/table_model.h"
 #include "ui/base/models/table_model_observer.h"
+#include "ui/events/event_constants.h"
+#include "ui/events/keycodes/keyboard_codes.h"
 #include "ui/gfx/canvas.h"
 #include "ui/views/context_menu_controller.h"
 #include "ui/views/controls/button/label_button.h"
@@ -163,6 +165,7 @@ class TaskManagerView : public views::ButtonListener,
   // views::View:
   virtual void Layout() OVERRIDE;
   virtual gfx::Size GetPreferredSize() OVERRIDE;
+  virtual bool AcceleratorPressed(const ui::Accelerator& accelerator) OVERRIDE;
   virtual void ViewHierarchyChanged(
       const ViewHierarchyChangedDetails& details) OVERRIDE;
 
@@ -382,6 +385,9 @@ void TaskManagerView::Init() {
 
   // Makes sure our state is consistent.
   OnSelectionChanged();
+
+  ui::Accelerator ctrl_w(ui::VKEY_W, ui::EF_CONTROL_DOWN);
+  AddAccelerator(ctrl_w);
 }
 
 void TaskManagerView::UpdateStatsCounters() {
@@ -466,6 +472,13 @@ void TaskManagerView::Layout() {
 
 gfx::Size TaskManagerView::GetPreferredSize() {
   return gfx::Size(460, 270);
+}
+
+bool TaskManagerView::AcceleratorPressed(const ui::Accelerator& accelerator) {
+  DCHECK_EQ(ui::VKEY_W, accelerator.key_code());
+  DCHECK_EQ(ui::EF_CONTROL_DOWN, accelerator.modifiers());
+  GetWidget()->Close();
+  return true;
 }
 
 // static
