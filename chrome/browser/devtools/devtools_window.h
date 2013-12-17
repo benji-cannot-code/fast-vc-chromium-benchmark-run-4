2035f76bf58b25f30f7a60793bd8cbfeefd193c4
dgozman@chromium.org <dgozman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-17 08:47:33 +0000

    Place DevTools WebContents underneath inspected WebContents.
    DevTools window: split width and height are gone, and instead
    contents insets are introduced. These insets are used for layout
    in browser window. Having insets instead of rect allows for faster
    regular-case resize occuring immediately in layout:
    we keep DevTools of the same size and resize the page contents.
    
    Views: removed SplitView and ContentsContainer, but instead added
    ContentsLayoutManager which layouts DevTools and page contents taking
    insets into account. It also supports active top margin, moved from
    ContentsContainer.
    
    Mac: removed NSSplitView, but instead added DevToolsContainerView
    which resizes subviews manually taking page insets into account.
    
    Gtk: removed both contents_vsplit and contents_hsplit, but instead
    added devtools_floating_container which positions page contents
    taking insets into account.
    
    Next step will remove dock side knowledge from the browser, and leave it
    on frontend only.
    
    BUG=318751
    
    Review URL: https://codereview.chromium.org/63173016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241223 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/devtools/devtools_window.h
+++ chrome/browser/devtools/devtools_window.h
@@ -22,6 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
 #include "content/public/browser/web_contents_delegate.h"
+#include "ui/gfx/insets.h"
+#include "ui/gfx/size.h"
 
 class Browser;
 class BrowserWindow;
@@ -97,8 +99,6 @@ class DevToolsWindow : private content::NotificationObserver,
   static void InspectElement(
       content::RenderViewHost* inspected_rvh, int x, int y);
 
-  static int GetMinimumWidth();
-  static int GetMinimumHeight();
   static int GetMinimizedHeight();
 
   // content::DevToolsFrontendHostDelegate:
@@ -111,23 +111,14 @@ class DevToolsWindow : private content::NotificationObserver,
   content::RenderViewHost* GetRenderViewHost();
   content::DevToolsClientHost* GetDevToolsClientHostForTest();
 
-  // Returns preferred devtools window width for given |container_width|. It
-  // tries to use the saved window width, or, if none exists, 1/3 of the
-  // container width, then clamps to try and ensure both devtools and content
-  // are at least somewhat visible.
-  // Called only for the case when devtools window is docked to the side.
-  int GetWidth(int container_width);
+  // Inspected WebContents is placed over DevTools WebContents in docked mode.
+  // The following methods return the insets of inspected WebContents
+  // relative to DevTools WebContents.
+  gfx::Insets GetContentsInsets() const;
 
-  // Returns preferred devtools window height for given |container_height|.
-  // Uses the same logic as GetWidth.
-  // Called only for the case when devtools window is docked to bottom.
-  int GetHeight(int container_height);
-
-  // Stores preferred devtools window width for this instance.
-  void SetWidth(int width);
-
-  // Stores preferred devtools window height for this instance.
-  void SetHeight(int height);
+  // Minimum size of the docked DevTools WebContents. This includes
+  // the overlaying inspected WebContents size.
+  gfx::Size GetMinimumSize() const;
 
   void Show(const DevToolsToggleAction& action);
 
@@ -209,6 +200,7 @@ class DevToolsWindow : private content::NotificationObserver,
 
  private:
   friend class DevToolsControllerTest;
+  friend class BrowserWindowControllerTest;
 
   DevToolsWindow(Profile* profile,
                  const GURL& frontend_url,
@@ -280,7 +272,8 @@ class DevToolsWindow : private content::NotificationObserver,
   virtual void ActivateWindow() OVERRIDE;
   virtual void ActivateContents(content::WebContents* contents) OVERRIDE;
   virtual void CloseWindow() OVERRIDE;
-  virtual void SetWindowBounds(int x, int y, int width, int height) OVERRIDE;
+  virtual void SetContentsInsets(
+      int left, int top, int right, int bottom) OVERRIDE;
   virtual void MoveWindow(int x, int y) OVERRIDE;
   virtual void SetDockSide(const std::string& side) OVERRIDE;
   virtual void OpenInNewTab(const std::string& url) OVERRIDE;
@@ -348,6 +341,7 @@ class DevToolsWindow : private content::NotificationObserver,
   Profile* profile_;
   content::WebContents* web_contents_;
   Browser* browser_;
+  // TODO(dgozman): move dock side knowledge entirely to frontend.
   DevToolsDockSide dock_side_;
   bool is_loaded_;
   DevToolsToggleAction action_on_load_;
@@ -360,8 +354,7 @@ class DevToolsWindow : private content::NotificationObserver,
       scoped_refptr<DevToolsFileSystemIndexer::FileSystemIndexingJob> >
       IndexingJobsMap;
   IndexingJobsMap indexing_jobs_;
-  int width_;
-  int height_;
+  gfx::Insets contents_insets_;
   DevToolsDockSide dock_side_before_minimized_;
   // True if we're in the process of handling a beforeunload event originating
   // from the inspected webcontents, see InterceptPageBeforeUnload for details.
--- chrome/browser/ui/views/frame/browser_view_interactive_uitest.cc
+++ chrome/browser/ui/views/frame/browser_view_interactive_uitest.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/search/instant_test_utils.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/views/bookmarks/bookmark_bar_view.h"
-#include "chrome/browser/ui/views/frame/contents_container.h"
 #include "chrome/browser/ui/views/frame/immersive_mode_controller.h"
 #include "chrome/browser/ui/views/frame/top_container_view.h"
 #include "chrome/browser/ui/views/location_bar/location_bar_view.h"
