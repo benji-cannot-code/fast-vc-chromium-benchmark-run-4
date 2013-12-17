49a890dbb155a825e8f56dfc7168416949251798
epenner@chromium.org <epenner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-17 07:30:08 +0000

    Telemetry: Add fast-path benchmark.
    Adding a benchmark to measure fast-path performance. This just
    filters a couple accelerated scrolling pages.
    
    BUG=321439
    
    Review URL: https://codereview.chromium.org/109203005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241184 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/perf/benchmarks/fast_path.py
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+from telemetry import test
+
+from measurements import smoothness
+
+class FastPathCases(test.Test):
+  """Measures timeline metrics while scrolling down fast-path mobile sites.
+  http://www.chromium.org/developers/design-documents/rendering-benchmarks"""
+  test = smoothness.Smoothness
+  page_set = 'page_sets/key_mobile_sites.json'
+  options = {'metric': 'timeline', 'page_label_filter' : 'fastpath'}
