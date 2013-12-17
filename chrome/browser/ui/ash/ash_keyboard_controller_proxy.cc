bfb5d455f760942c7f4dd71e608a8d75c9fa6818
bshe@chromium.org <bshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-17 03:21:17 +0000

    Polish VK show/hide animation
    This CL did:
    1. add opacity animation for VK when show/hide
    2. reduce animation time to 100 ms
    
    
    BUG=327462
    
    Review URL: https://codereview.chromium.org/114803004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241130 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/ash/ash_keyboard_controller_proxy.cc
+++ chrome/browser/ui/ash/ash_keyboard_controller_proxy.cc
@@ -40,7 +40,7 @@ namespace {
 const char* kVirtualKeyboardExtensionID = "mppnpdlheglhdfmldimlhpnegondlapf";
 
 // The virtual keyboard show/hide animation duration.
-const int kAnimationDurationMs = 1000;
+const int kAnimationDurationMs = 100;
 
 Context::Type TextInputTypeToGeneratedInputTypeEnum(ui::TextInputType type) {
   switch (type) {
@@ -174,6 +174,7 @@ void AshKeyboardControllerProxy::ShowKeyboardContainer(
     settings.SetTransitionDuration(
         base::TimeDelta::FromMilliseconds(kAnimationDurationMs));
     container->SetTransform(gfx::Transform());
+    container->layer()->SetOpacity(1.0);
   }
 
   // TODO(bshe): Add animation observer and do the workspace resizing after
@@ -201,6 +202,7 @@ void AshKeyboardControllerProxy::HideKeyboardContainer(
   gfx::Transform transform;
   transform.Translate(0, GetKeyboardWindow()->bounds().height());
   container->SetTransform(transform);
+  container->layer()->SetOpacity(0.0);
   KeyboardControllerProxy::HideKeyboardContainer(container);
 }
 
