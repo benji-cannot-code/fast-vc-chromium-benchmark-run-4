d63e0e57a7d48b1ad6e599138289faee4602e97e
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-17 02:14:28 +0000

    [SyncFS] Add NULL check to SyncFileSystemService::DidInitializeFileSystemForDump
    BUG=240165
    R=nhiroki@chromium.org
    
    Review URL: https://codereview.chromium.org/111963006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241107 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/sync_file_system_service.cc
+++ chrome/browser/sync_file_system/sync_file_system_service.cc
@@ -506,6 +506,11 @@ void SyncFileSystemService::DidInitializeFileSystemForDump(
 
   base::ListValue* files =
       GetRemoteService(origin)->DumpFiles(origin).release();
+  if (!files) {
+    callback.Run(new base::ListValue);
+    return;
+  }
+
   if (!files->GetSize()) {
     callback.Run(files);
     return;
