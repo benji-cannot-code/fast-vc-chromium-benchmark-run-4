b1a43eec4a79c5a51ef3b8faddde41d8d143448b
noel@chromium.org <noel@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-17 01:12:58 +0000

    Make qcmsOutputDeviceProfile() thread-safe
    Chrome may now use multiple image decode threads in the renderer and they
    can call qcmsOutputDeviceProfile(), which is not thread-safe. Use a mutex
    to quard the "once-only" qcms monitor profile initialization code.
    
    This change depends on bug 327484 and the fix for that has been submitted
    in https://src.chromium.org/viewvc/chrome?view=rev&revision=240120, which
    updated chromium with the upstream, thread-safe libqcms patch.
    
    Some might find the comments in the upstream libqcms patch confusing when
    considering if profiles can be used on different threads so, to elaborate
    here, we compute pre-cache data for the monitor profile when we create it
    and that creation is now guarded by a mutex in this change.
    
    The pre-cache data is ref-counted, and the ref-count is never less than 1
    because the pre-cache is stored inside the static monitor profile pointer
    |outputDeviceProfile| which is never deleted.
    
    The monitor profile and its pre-cache data are only read, after creation,
    and the pre-cache is thread-safe, ref-counted if used. Hence, the monitor
    profile |outputDeviceProfile| can be safely used from any thread.
    
    BUG=314012,327484,327944
    
    Review URL: https://codereview.chromium.org/108873010
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163987 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/image-decoders/ImageDecoder.h
+++ third_party/WebKit/Source/platform/image-decoders/ImageDecoder.h
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "public/platform/Platform.h"
 #include "wtf/Assertions.h"
 #include "wtf/RefPtr.h"
+#include "wtf/Threading.h"
 #include "wtf/text/WTFString.h"
 #include "wtf/Vector.h"
 
@@ -171,13 +172,11 @@ public:
     }
 
 #if USE(QCMSLIB)
-    static qcms_profile* qcmsOutputDeviceProfile()
-    {
-        static qcms_profile* outputDeviceProfile = 0;
-
-        static bool qcmsInitialized = false;
-        if (!qcmsInitialized) {
-            qcmsInitialized = true;
+    class OutputDeviceProfile {
+    public:
+        OutputDeviceProfile()
+            : m_outputDeviceProfile(0)
+        {
             // FIXME: Add optional ICCv4 support.
 #if OS(MACOSX)
             RetainPtr<CGColorSpaceRef> monitorColorSpace(AdoptCF, CGDisplayCopyColorSpace(CGMainDisplayID()));
@@ -185,25 +184,36 @@ public:
             if (iccProfile) {
                 size_t length = CFDataGetLength(iccProfile);
                 const unsigned char* systemProfile = CFDataGetBytePtr(iccProfile);
-                outputDeviceProfile = qcms_profile_from_memory(systemProfile, length);
+                m_outputDeviceProfile = qcms_profile_from_memory(systemProfile, length);
             }
 #else
             // FIXME: add support for multiple monitors.
             ColorProfile profile;
             screenColorProfile(profile);
             if (!profile.isEmpty())
-                outputDeviceProfile = qcms_profile_from_memory(profile.data(), profile.size());
+                m_outputDeviceProfile = qcms_profile_from_memory(profile.data(), profile.size());
 #endif
-            if (outputDeviceProfile && qcms_profile_is_bogus(outputDeviceProfile)) {
-                qcms_profile_release(outputDeviceProfile);
-                outputDeviceProfile = 0;
+            if (m_outputDeviceProfile && qcms_profile_is_bogus(m_outputDeviceProfile)) {
+                qcms_profile_release(m_outputDeviceProfile);
+                m_outputDeviceProfile = 0;
             }
-            if (!outputDeviceProfile)
-                outputDeviceProfile = qcms_profile_sRGB();
-            if (outputDeviceProfile)
-                qcms_profile_precache_output_transform(outputDeviceProfile);
+            if (!m_outputDeviceProfile)
+                m_outputDeviceProfile = qcms_profile_sRGB();
+            if (m_outputDeviceProfile)
+                qcms_profile_precache_output_transform(m_outputDeviceProfile);
         }
-        return outputDeviceProfile;
+
+        qcms_profile* profile() const { return m_outputDeviceProfile; }
+
+    private:
+        qcms_profile* m_outputDeviceProfile;
+    };
+
+    static qcms_profile* qcmsOutputDeviceProfile()
+    {
+        AtomicallyInitializedStatic(OutputDeviceProfile*, outputDeviceProfile = new OutputDeviceProfile());
+
+        return outputDeviceProfile->profile();
     }
 #endif
 
