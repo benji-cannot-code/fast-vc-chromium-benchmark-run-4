8be8d3582f02c1377701c1a02c1822d887f4bd6f
jyasskin@chromium.org <jyasskin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-18 04:53:46 +0000

    Pass a Feature* to IsAnyFeatureAvailableToContext instead of a string name.
    This makes it clear that we were never passing non-feature function names, which
    lets us simplify the test.
    
    BUG=305000
    
    Review URL: https://codereview.chromium.org/109683007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241490 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/extensions/dispatcher.cc
+++ chrome/renderer/extensions/dispatcher.cc
@@ -756,7 +756,7 @@ void Dispatcher::AddOrRemoveBindingsForContext(ChromeV8Context* context) {
         bool parent_feature_available = false;
         for (Feature* parent = api_feature_provider->GetParent(feature);
              parent != NULL; parent = api_feature_provider->GetParent(parent)) {
-          if (context->IsAnyFeatureAvailableToContext(parent->name())) {
+          if (context->IsAnyFeatureAvailableToContext(*parent)) {
             parent_feature_available = true;
             break;
           }
@@ -764,7 +764,7 @@ void Dispatcher::AddOrRemoveBindingsForContext(ChromeV8Context* context) {
         if (parent_feature_available)
           continue;
 
-        if (context->IsAnyFeatureAvailableToContext(api_name))
+        if (context->IsAnyFeatureAvailableToContext(*feature))
           RegisterBinding(api_name, context);
       }
       break;
