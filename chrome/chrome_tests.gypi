abdd3fd221fe134e19e7ae3cb0edf9f45b78d445
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-18 19:37:04 +0000

    [webNavigation] only observe redirects for frames
    Added a test that e.g. a redirects for a download does not trigger the
    DCHECK that we know about the frame.
    
    The if statement in web_navigation_api.cc used to be in place but was
    accidentally removed in r241514
    
    BUG=329456
    TEST=WebNavigationApiTest.Download
    R=battre@chromium.org
    
    Review URL: https://codereview.chromium.org/98903003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241613 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/chrome_tests.gypi
+++ chrome/chrome_tests.gypi
@@ -1080,6 +1080,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/devtools/devtools_sanity_browsertest.cc',
         'browser/do_not_track_browsertest.cc',
         'browser/download/download_browsertest.cc',
+        'browser/download/download_browsertest.h',
         'browser/download/download_danger_prompt_browsertest.cc',
         'browser/download/download_started_animation_browsertest.cc',
         'browser/download/save_page_browsertest.cc',
---
+++ chrome/test/data/extensions/api_test/webnavigation/download/a.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+onload = function() {
+  var port = location.search.substr(1);
+  var redirect =
+      "http://127.0.0.1:" + port + "/server-redirect";
+  var target =
+      "http://127.0.0.1:" + port + "/not-found";
+
+  var link = document.createElement("a");
+  link.href = redirect + "?" + target;
+  link.download = "somefile.txt";
+  document.body.appendChild(link);
+  link.click();
+};
