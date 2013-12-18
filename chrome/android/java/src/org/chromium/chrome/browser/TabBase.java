0d2a89467c777f36d82b95b4d61ed82df1c5dcbb
apiccion@chromium.org <apiccion@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-18 04:55:17 +0000

    Changes to enable HTML5 autoplay for voice search.
    * Added methods in ContentViewCore to enable/disable html5 autoplay restrictions.
    
    BUG=323264
    
    Review URL: https://codereview.chromium.org/86443002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241491 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/android/java/src/org/chromium/chrome/browser/TabBase.java
+++ chrome/android/java/src/org/chromium/chrome/browser/TabBase.java
@@ -100,6 +100,7 @@ public abstract class TabBase implements NavigationClient {
     // Content layer Observers and Delegates
     private ContentViewClient mContentViewClient;
     private WebContentsObserverAndroid mWebContentsObserver;
+    private VoiceSearchTabHelper mVoiceSearchTabHelper;
     private TabBaseChromeWebContentsDelegateAndroid mWebContentsDelegate;
 
     /**
@@ -625,6 +626,7 @@ public abstract class TabBase implements NavigationClient {
         mContentViewCore = mContentView.getContentViewCore();
         mWebContentsDelegate = createWebContentsDelegate();
         mWebContentsObserver = new TabBaseWebContentsObserverAndroid(mContentViewCore);
+        mVoiceSearchTabHelper = new VoiceSearchTabHelper(mContentViewCore);
 
         if (mContentViewClient != null) mContentViewCore.setContentViewClient(mContentViewClient);
 
@@ -730,6 +732,7 @@ public abstract class TabBase implements NavigationClient {
         mContentViewCore = null;
         mWebContentsDelegate = null;
         mWebContentsObserver = null;
+        mVoiceSearchTabHelper = null;
 
         assert mNativeTabAndroid != 0;
         nativeDestroyWebContents(mNativeTabAndroid, deleteNativeWebContents);
