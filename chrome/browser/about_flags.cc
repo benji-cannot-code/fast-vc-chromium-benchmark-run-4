3c2ae0ab7b938d2f2bc5ca3c63ec5cec339f63a6
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-18 08:17:16 +0000

    Web MIDI: minimum support for Linux and Chrome OS
    This change enables Web MIDI API on Linux and Chrome OS.
    Only Sending functionality works with this first experimental change.
    
    BUG=303601
    
    Review URL: https://codereview.chromium.org/111443005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241524 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1710,7 +1710,7 @@ const Experiment kExperiments[] = {
     "enable-web-midi",
     IDS_FLAGS_ENABLE_WEB_MIDI_NAME,
     IDS_FLAGS_ENABLE_WEB_MIDI_DESCRIPTION,
-    kOsMac | kOsWin,
+    kOsMac | kOsWin | kOsLinux | kOsCrOS,
     SINGLE_VALUE_TYPE(switches::kEnableWebMIDI)
   },
   {
