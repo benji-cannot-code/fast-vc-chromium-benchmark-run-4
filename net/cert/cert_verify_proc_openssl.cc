361f0fb70f1645edf448e4a86c73db99acbf388e
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-18 01:57:48 +0000

    Set verify_result->is_issued_by_known_root correctly in CertVerifyProcOpenSSL.
    Right now CertVerifyProcOpenSSL uses only the default root certs and
    the test root certs. So if a cert is not issued by a test root, consider
    it as being issued by a known root.
    
    R=rsleevi@chromium.org
    BUG=312754
    TEST=net_unittests
    
    Review URL: https://codereview.chromium.org/115393005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241444 0039d316-1c4b-4281-b951-d872f2087c98

--- net/cert/cert_verify_proc_openssl.cc
+++ net/cert/cert_verify_proc_openssl.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cert/cert_status_flags.h"
 #include "net/cert/cert_verifier.h"
 #include "net/cert/cert_verify_result.h"
+#include "net/cert/test_root_certs.h"
 #include "net/cert/x509_certificate.h"
 
 namespace net {
@@ -122,9 +123,34 @@ void GetCertChainInfo(X509_STORE_CTX* store_ctx,
     }
   }
 
+  // Set verify_result->verified_cert and
+  // verify_result->is_issued_by_known_root.
   if (verified_cert) {
     verify_result->verified_cert =
         X509Certificate::CreateFromHandle(verified_cert, verified_chain);
+
+    // For OpenSSL builds, only certificates used for unit tests are treated
+    // as not issued by known roots. The only way to determine whether a
+    // certificate is issued by a known root using OpenSSL is to examine
+    // distro-and-release specific hardcoded lists.
+    verify_result->is_issued_by_known_root = true;
+    if (TestRootCerts::HasInstance()) {
+      X509* root = NULL;
+      if (verified_chain.empty()) {
+        root = verified_cert;
+      } else {
+        root = verified_chain.back();
+      }
+      const CertificateList& temporary_roots =
+          TestRootCerts::GetInstance()->temporary_roots();
+      for (size_t i = 0; i < temporary_roots.size(); ++i) {
+        if (X509Certificate::IsSameOSCert(
+                root, temporary_roots[i]->os_cert_handle())) {
+          verify_result->is_issued_by_known_root = false;
+          break;
+        }
+      }
+    }
   }
 }
 
@@ -214,14 +240,6 @@ int CertVerifyProcOpenSSL::VerifyInternal(
   AppendPublicKeyHashes(ctx.get(), &verify_result->public_key_hashes);
   if (IsCertStatusError(verify_result->cert_status))
     return MapCertStatusToNetError(verify_result->cert_status);
-
-  // Currently we only ues OpenSSL's default root CA paths, so treat all
-  // correctly verified certs as being from a known root.
-  // TODO(joth): if the motivations described in
-  // http://src.chromium.org/viewvc/chrome?view=rev&revision=80778 become an
-  // issue on OpenSSL builds, we will need to embed a hardcoded list of well
-  // known root CAs, as per the _mac and _win versions.
-  verify_result->is_issued_by_known_root = true;
 
   return OK;
 }
--- net/cert/test_root_certs.h
+++ net/cert/test_root_certs.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(USE_NSS) || defined(OS_IOS)
 #include <list>
+#elif defined(USE_OPENSSL) && !defined(OS_ANDROID)
+#include <vector>
 #elif defined(OS_WIN)
 #include <windows.h>
 #include <wincrypt.h>
@@ -69,7 +71,9 @@ class NET_EXPORT_PRIVATE TestRootCerts {
   // be trusted. By default, this is true, indicating that the TestRootCerts
   // are used in addition to OS trust store.
   void SetAllowSystemTrust(bool allow_system_trust);
-
+#elif defined(USE_OPENSSL) && !defined(OS_ANDROID)
+  const std::vector<scoped_refptr<X509Certificate> >&
+      temporary_roots() const { return temporary_roots_; }
 #elif defined(OS_WIN)
   HCERTSTORE temporary_roots() const { return temporary_roots_; }
 
@@ -94,6 +98,8 @@ class NET_EXPORT_PRIVATE TestRootCerts {
   // settings, in order to restore them when Clear() is called.
   class TrustEntry;
   std::list<TrustEntry*> trust_cache_;
+#elif defined(USE_OPENSSL) && !defined(OS_ANDROID)
+  std::vector<scoped_refptr<X509Certificate> > temporary_roots_;
 #elif defined(OS_WIN)
   HCERTSTORE temporary_roots_;
 #elif defined(OS_MACOSX)
@@ -101,7 +107,7 @@ class NET_EXPORT_PRIVATE TestRootCerts {
   bool allow_system_trust_;
 #endif
 
-#if defined(OS_WIN) || defined(USE_OPENSSL)
+#if defined(OS_WIN) || defined(OS_ANDROID)
   // True if there are no temporarily trusted root certificates.
   bool empty_;
 #endif
