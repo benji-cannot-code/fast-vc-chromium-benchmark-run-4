28aebf552216151afcd271d0425f3e78eee60247
shuchen@chromium.org <shuchen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-18 14:20:28 +0000

    [IME API] Adds input.ime.hideInputView API.
    BUG=316524
    TEST=Locally verified on sandbox.
    
    Review URL: https://codereview.chromium.org/108723004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241566 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/input_method/input_method_engine_interface.h
+++ chrome/browser/chromeos/input_method/input_method_engine_interface.h
@@ -239,6 +239,9 @@ class InputMethodEngineInterface {
                                      int offset,
                                      size_t number_of_chars,
                                      std::string* error) = 0;
+
+  // Hides the input view window (from API call).
+  virtual void HideInputView() = 0;
 };
 
 }  // namespace chromeos
