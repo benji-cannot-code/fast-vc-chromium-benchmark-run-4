648a9014b97747ae854d5c420f787fbda6aca891
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-18 11:24:08 +0000

    Log more info in file_util::WriteFile for debugging.
    This is intended to be a temporary change.
    
    TBR=willchan
    
    BUG=329478
    
    Review URL: https://codereview.chromium.org/100033005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241538 0039d316-1c4b-4281-b951-d872f2087c98

--- base/file_util_win.cc
+++ base/file_util_win.cc
@@ -578,9 +578,10 @@ int WriteFile(const FilePath& filename, const char* data, int size) {
                                           CREATE_ALWAYS,
                                           0,
                                           NULL));
+  // TODO(phajdan.jr): Restore previous log levels, http://crbug.com/329478 .
   if (!file) {
-    DLOG_GETLASTERROR(WARNING) << "CreateFile failed for path "
-                               << filename.value();
+    LOG_GETLASTERROR(ERROR) << "CreateFile failed for path "
+                            << filename.value();
     return -1;
   }
 
@@ -591,12 +592,12 @@ int WriteFile(const FilePath& filename, const char* data, int size) {
 
   if (!result) {
     // WriteFile failed.
-    DLOG_GETLASTERROR(WARNING) << "writing file " << filename.value()
-                               << " failed";
+    LOG_GETLASTERROR(ERROR) << "writing file " << filename.value()
+                            << " failed";
   } else {
     // Didn't write all the bytes.
-    DLOG(WARNING) << "wrote" << written << " bytes to "
-                  << filename.value() << " expected " << size;
+    LOG(ERROR) << "wrote" << written << " bytes to "
+               << filename.value() << " expected " << size;
   }
   return -1;
 }
