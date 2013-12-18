bfcb218c4308002af3d4ee95bc3ba35396dda345
mlamouri@chromium.org <mlamouri@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-18 15:24:41 +0000

    Focus/Blur content of NativeAppWindowCocoa at (de)activation.
    Using RestoreFocus()/StoreFocus(). It also prevents the
    window to be focused when created inactive.
    
    TESTS=cl 102293002 and cl 101323005 have (disabled) tests for this.
    
    BUG=326987
    
    Review URL: https://codereview.chromium.org/108003007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241572 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/apps/native_app_window_cocoa.mm
+++ chrome/browser/ui/cocoa/apps/native_app_window_cocoa.mm
@@ -547,7 +547,7 @@ void NativeAppWindowCocoa::Show() {
   }
 
   [window_controller_ showWindow:nil];
-  [window() makeKeyAndOrderFront:window_controller_];
+  Activate();
 }
 
 void NativeAppWindowCocoa::ShowInactive() {
@@ -822,10 +822,9 @@ bool NativeAppWindowCocoa::IsAlwaysOnTop() const {
   return [window() level] == AlwaysOnTopWindowLevel();
 }
 
-void NativeAppWindowCocoa::RenderViewHostChanged(
-    content::RenderViewHost* old_host,
-    content::RenderViewHost* new_host) {
-  web_contents()->GetView()->Focus();
+void NativeAppWindowCocoa::RenderViewCreated(content::RenderViewHost* rvh) {
+  if (IsActive())
+    web_contents()->GetView()->RestoreFocus();
 }
 
 bool NativeAppWindowCocoa::IsFrameless() const {
@@ -888,6 +887,8 @@ void NativeAppWindowCocoa::WindowDidBecomeKey() {
   if (rwhv)
     rwhv->SetActive(true);
   shell_window_->OnNativeWindowActivated();
+
+  web_contents()->GetView()->RestoreFocus();
 }
 
 void NativeAppWindowCocoa::WindowDidResignKey() {
@@ -897,6 +898,8 @@ void NativeAppWindowCocoa::WindowDidResignKey() {
   // lose key window status.
   if ([NSApp isActive] && ([NSApp keyWindow] == window()))
     return;
+
+  web_contents()->GetView()->StoreFocus();
 
   content::RenderWidgetHostView* rwhv =
       web_contents()->GetRenderWidgetHostView();
