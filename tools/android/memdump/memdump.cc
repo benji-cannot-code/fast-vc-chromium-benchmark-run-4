8095e0d1b8ab36045eaf4f7dd2f2dcde7b129a83
hyunki.baik@samsung.com <hyunki.baik@samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-18 11:48:31 +0000

    Remove unnecessary #include
    BUG=none
    
    Review URL: https://codereview.chromium.org/113543004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241541 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/android/memdump/memdump.cc
+++ tools/android/memdump/memdump.cc
@@ -24,9 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/hash_tables.h"
 #include "base/file_util.h"
 #include "base/logging.h"
-#include "base/memory/scoped_ptr.h"
 #include "base/strings/string_number_conversions.h"
-#include "base/strings/string_piece.h"
 #include "base/strings/string_split.h"
 #include "base/strings/stringprintf.h"
 
