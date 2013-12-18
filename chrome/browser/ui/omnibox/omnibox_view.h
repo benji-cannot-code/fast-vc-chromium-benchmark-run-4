e7bad128142b404a24588a893ed28e8738901648
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-18 01:03:39 +0000

    Remove unused OmniboxView::OnPerformDrop and related code.
    These functions are not currently used nor necessary.
    
    BUG=131660
    TEST=No omnibox or toolbar drag-and-drop regressions.
    R=pkasting@chromium.org
    
    Review URL: https://codereview.chromium.org/116573009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241433 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/omnibox/omnibox_view.h
+++ chrome/browser/ui/omnibox/omnibox_view.h
@@ -34,21 +34,6 @@ namespace content {
 class WebContents;
 }
 
-#if defined(TOOLKIT_VIEWS)
-// TODO(beng): Move all views-related code to a views-specific sub-interface.
-namespace gfx {
-class Font;
-}
-
-namespace views {
-class View;
-}
-
-namespace ui {
-class DropTargetEvent;
-}
-#endif
-
 class OmniboxView {
  public:
   virtual ~OmniboxView();
@@ -231,11 +216,6 @@ class OmniboxView {
   // corpus (e.g. Images) rather than start a new Web search.  This method will
   // only ever return true on mobile ports.
   virtual bool IsIndicatingQueryRefinement() const;
-
-#if defined(TOOLKIT_VIEWS)
-  // Performs the drop of a drag and drop operation on the view.
-  virtual int OnPerformDrop(const ui::DropTargetEvent& event) = 0;
-#endif
 
   // Returns |text| with any leading javascript schemas stripped.
   static base::string16 StripJavascriptSchemas(const base::string16& text);
--- chrome/browser/ui/views/omnibox/omnibox_view_views.cc
+++ chrome/browser/ui/views/omnibox/omnibox_view_views.cc
@@ -688,11 +688,6 @@ bool OmniboxViewViews::IsImeShowingPopup() const {
 #endif
 }
 
-int OmniboxViewViews::OnPerformDrop(const ui::DropTargetEvent& event) {
-  NOTIMPLEMENTED();
-  return ui::DragDropTypes::DRAG_NONE;
-}
-
 ////////////////////////////////////////////////////////////////////////////////
 // OmniboxViewViews, views::TextfieldController implementation:
 
