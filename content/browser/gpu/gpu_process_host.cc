58171e348969a2250ef0125e063d7b55b4c963ef
jorgelo@chromium.org <jorgelo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-18 04:20:41 +0000

    Add a flag to make GPU sandbox failures nonfatal.
    BUG=327824
    TEST=chromeos=1 compiles, loads.
    
    Review URL: https://codereview.chromium.org/117463002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241480 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -49,6 +49,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/surface/accelerated_surface_win.h"
 #endif
 
+#if defined(OS_CHROMEOS)
+#include "chromeos/chromeos_switches.h"
+#endif
+
 #if defined(USE_OZONE)
 #include "ui/ozone/ozone_switches.h"
 #endif
@@ -1133,6 +1137,9 @@ bool GpuProcessHost::LaunchGpuProcess(const std::string& channel_id) {
     switches::kVModule,
 #if defined(OS_MACOSX)
     switches::kEnableSandboxLogging,
+#endif
+#if defined(OS_CHROMEOS)
+    chromeos::switches::kGpuSandboxFailuresNonfatal,
 #endif
 #if defined(USE_AURA)
     switches::kUIPrioritizeInGpuProcess,
