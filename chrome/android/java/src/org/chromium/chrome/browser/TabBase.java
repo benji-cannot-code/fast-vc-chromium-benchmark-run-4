172c51076f75dd0bf613877d40e03f9583d16736
apiccion@chromium.org <apiccion@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-19 21:42:42 +0000

    Revert of https://codereview.chromium.org/86443002/ Reason for revert: Breaks Android Performance Bots http://build.chromium.org/p/chromium.perf/builders/Android%20Nexus10%20Perf/builds/17
    TBR=dtrainor@chromium.org,tedchoc@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/98413005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241950 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/android/java/src/org/chromium/chrome/browser/TabBase.java
+++ chrome/android/java/src/org/chromium/chrome/browser/TabBase.java
@@ -100,7 +100,6 @@ public abstract class TabBase implements NavigationClient {
     // Content layer Observers and Delegates
     private ContentViewClient mContentViewClient;
     private WebContentsObserverAndroid mWebContentsObserver;
-    private VoiceSearchTabHelper mVoiceSearchTabHelper;
     private TabBaseChromeWebContentsDelegateAndroid mWebContentsDelegate;
 
     /**
@@ -626,7 +625,6 @@ public abstract class TabBase implements NavigationClient {
         mContentViewCore = mContentView.getContentViewCore();
         mWebContentsDelegate = createWebContentsDelegate();
         mWebContentsObserver = new TabBaseWebContentsObserverAndroid(mContentViewCore);
-        mVoiceSearchTabHelper = new VoiceSearchTabHelper(mContentViewCore);
 
         if (mContentViewClient != null) mContentViewCore.setContentViewClient(mContentViewClient);
 
@@ -732,7 +730,6 @@ public abstract class TabBase implements NavigationClient {
         mContentViewCore = null;
         mWebContentsDelegate = null;
         mWebContentsObserver = null;
-        mVoiceSearchTabHelper = null;
 
         assert mNativeTabAndroid != 0;
         nativeDestroyWebContents(mNativeTabAndroid, deleteNativeWebContents);
