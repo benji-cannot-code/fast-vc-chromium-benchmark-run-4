0924f77b5a64753fd948d37f8142c5b1aef5b738
bokan@chromium.org <bokan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-19 18:59:29 +0000

    Renamed the delta/multiplier parameter in scroll methods to be consistent.
    This parameter was called a 'delta' in EventHandler, then a 'multiplier'
    in the scroll methods of these classes. 'delta' seems clearer to me so
    I've renamed the parameter to 'delta' throughout.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/118453002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164192 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/mac/ScrollAnimatorMac.h
+++ third_party/WebKit/Source/platform/mac/ScrollAnimatorMac.h
@@ -86,7 +86,7 @@ private:
     Timer<ScrollAnimatorMac> m_sendContentAreaScrolledTimer;
     FloatSize m_contentAreaScrolledTimerScrollDelta;
 
-    virtual bool scroll(ScrollbarOrientation, ScrollGranularity, float step, float multiplier);
+    virtual bool scroll(ScrollbarOrientation, ScrollGranularity, float step, float delta);
     virtual void scrollToOffsetWithoutAnimation(const FloatPoint&);
 
 #if USE(RUBBER_BANDING)
