190fa0b22ef83a0f681e2235eb4b2745a9d9cc55
jsbell@chromium.org <jsbell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-19 01:33:59 +0000

    IndexedDB: Minor clean up of transaction/database calls
    The call graph between backend IDB objects are a bit hairy. This gives
    it a bit of a trim.
    
    R=dgrogan@chromium.org
    
    Review URL: https://codereview.chromium.org/112293002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241745 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/indexed_db/indexed_db_transaction.cc
+++ content/browser/indexed_db/indexed_db_transaction.cc
@@ -174,12 +174,11 @@ void IndexedDBTransaction::Abort(const IndexedDBDatabaseError& error) {
 #ifndef NDEBUG
   DCHECK(!database_->transaction_coordinator().IsActive(this));
 #endif
-  database_->TransactionFinished(this);
 
   if (callbacks_.get())
     callbacks_->OnAbort(id_, error);
 
-  database_->TransactionFinishedAndAbortFired(this);
+  database_->TransactionFinished(this, false);
 
   database_ = NULL;
 }
@@ -253,12 +252,11 @@ void IndexedDBTransaction::Commit() {
   // front-end is notified, as the transaction completion unblocks
   // operations like closing connections.
   database_->transaction_coordinator().DidFinishTransaction(this);
-  database_->TransactionFinished(this);
 
   if (committed) {
     abort_task_stack_.clear();
     callbacks_->OnComplete(id_);
-    database_->TransactionFinishedAndCompleteFired(this);
+    database_->TransactionFinished(this, true);
   } else {
     while (!abort_task_stack_.empty())
       abort_task_stack_.pop().Run(0);
@@ -267,7 +265,7 @@ void IndexedDBTransaction::Commit() {
         id_,
         IndexedDBDatabaseError(blink::WebIDBDatabaseExceptionUnknownError,
                                "Internal error committing transaction."));
-    database_->TransactionFinishedAndAbortFired(this);
+    database_->TransactionFinished(this, false);
     database_->TransactionCommitFailed();
   }
 
