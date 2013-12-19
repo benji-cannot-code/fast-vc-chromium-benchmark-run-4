8aa5eef65f4444d4f267e6f916072aaa4e5099c0
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-19 09:59:56 +0000

    GTTF: add an option to pass test filter using a file
    This is useful when --gtest_filter on the command line would exceed
    command line length limits.
    
    This will be used with chromium_trybot recipe to pass a list of tests
    to retry after deapplying the patch.
    
    BUG=236893, 44497
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/117113003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241833 0039d316-1c4b-4281-b951-d872f2087c98

--- base/test/test_switches.h
+++ base/test/test_switches.h
@@ -14,6 +14,7 @@ extern const char kTestLargeTimeout[];
 extern const char kTestLauncherBatchLimit[];
 extern const char kTestLauncherBotMode[];
 extern const char kTestLauncherDebugLauncher[];
+extern const char kTestLauncherFilterFile[];
 extern const char kTestLauncherJobs[];
 extern const char kTestLauncherOutput[];
 extern const char kTestLauncherRetryLimit[];
