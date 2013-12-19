4db1a39581e635ad63a82d5c576bbbd2bcd5974d
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-19 08:24:48 +0000

    Simplify the insides of DocumentSharedObjectPool and reduce memory usage
    Instead of storing an OwnPtr to an object that has a pointer to the
    SharableElementData as well as a pointer into the SharableElementData
    and the length we can just store a RefPtr to the SharableElementData.
    
    This also reduces the memory usage of the pool by 2 pointers per entry.
    
    In making this change I also realized that the way the cache currently works
    when two elements with attributes that hash to the same value collide we
    just don't share. I think we need fix this with a HashTranslator in the future.
    
    Review URL: https://codereview.chromium.org/102303004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164152 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/DocumentSharedObjectPool.h
+++ third_party/WebKit/Source/core/dom/DocumentSharedObjectPool.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "wtf/HashMap.h"
 #include "wtf/PassOwnPtr.h"
+#include "wtf/RefPtr.h"
 #include "wtf/text/StringHash.h"
 
 namespace WebCore {
@@ -48,7 +49,7 @@ public:
 private:
     DocumentSharedObjectPool();
 
-    typedef HashMap<unsigned, OwnPtr<ShareableElementDataCacheEntry>, AlreadyHashed> ShareableElementDataCache;
+    typedef HashMap<unsigned, RefPtr<ShareableElementData>, AlreadyHashed> ShareableElementDataCache;
     ShareableElementDataCache m_shareableElementDataCache;
 };
 
