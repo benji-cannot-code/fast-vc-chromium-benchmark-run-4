3c6d914169172dd465051f68e466166565f33d13
zoltan@adobe.com <zoltan@adobe.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-19 17:58:56 +0000

    Move LineWidth.{h,cpp} into rendering/line
    We introduced the 'line' subdirectory in rendering earlier. LineWidth should be in this directory.
    
    Review URL: https://codereview.chromium.org/113033003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164191 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/line/BreakingContextInlineHeaders.h
+++ third_party/WebKit/Source/core/rendering/line/BreakingContextInlineHeaders.h
@@ -27,11 +27,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/rendering/InlineIterator.h"
 #include "core/rendering/InlineTextBox.h"
-#include "core/rendering/LineWidth.h"
 #include "core/rendering/RenderCombineText.h"
 #include "core/rendering/RenderInline.h"
 #include "core/rendering/break_lines.h"
 #include "core/rendering/line/LineInfo.h"
+#include "core/rendering/line/LineWidth.h"
 #include "core/rendering/shapes/ShapeInsideInfo.h"
 #include "core/rendering/svg/RenderSVGInlineText.h"
 
--- third_party/WebKit/Source/core/rendering/line/LineWidth.cpp
+++ third_party/WebKit/Source/core/rendering/line/LineWidth.cpp
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
-#include "core/rendering/LineWidth.h"
+#include "core/rendering/line/LineWidth.h"
 
 #include "core/rendering/RenderBlock.h"
 #include "core/rendering/RenderRubyRun.h"
