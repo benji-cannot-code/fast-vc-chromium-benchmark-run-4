f5c5219798bc903f35109170c369812713932e0a
lambroslambrou@chromium.org <lambroslambrou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-19 03:20:53 +0000

    Remove |view_size_| from JniFrameConsumer
    It isn't needed, since it always matches the size of the allocated Java
    bitmap.
    
    Review URL: https://codereview.chromium.org/93873016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241777 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/client/jni/jni_frame_consumer.h
+++ remoting/client/jni/jni_frame_consumer.h
@@ -51,9 +51,9 @@ class JniFrameConsumer : public FrameConsumer {
   virtual PixelFormat GetPixelFormat() OVERRIDE;
 
  private:
-  // Allocates a new buffer of |view_size_|, informs Java about it, and tells
+  // Allocates a new buffer of |source_size|, informs Java about it, and tells
   // the producer to draw onto it.
-  void AllocateBuffer();
+  void AllocateBuffer(const webrtc::DesktopSize& source_size);
 
   // Frees a frame buffer previously allocated by AllocateBuffer.
   void FreeBuffer(webrtc::DesktopFrame* buffer);
@@ -67,7 +67,6 @@ class JniFrameConsumer : public FrameConsumer {
   scoped_refptr<ChromotingJniInstance> jni_instance_;
 
   FrameProducer* frame_producer_;
-  webrtc::DesktopSize view_size_;
   webrtc::DesktopRect clip_area_;
 
   // List of allocated image buffers.
