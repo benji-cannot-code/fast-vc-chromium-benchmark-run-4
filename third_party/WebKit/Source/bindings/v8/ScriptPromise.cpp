5cc562362a5dc248df29fd21da4068f1877583ba
svenpanne@chromium.org <svenpanne@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-19 13:26:00 +0000

    Do not use (mostly) deprecated Object::New function.
    R=dcarney@chromium.org
    BUG=324225
    
    Review URL: https://codereview.chromium.org/111573006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164175 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/ScriptPromise.cpp
+++ third_party/WebKit/Source/bindings/v8/ScriptPromise.cpp
@@ -46,7 +46,7 @@ ScriptPromise ScriptPromise::createPending(ExecutionContext* context)
     v8::Isolate* isolate = toIsolate(context);
     ASSERT(isolate->InContext());
     v8::Handle<v8::Context> v8Context = toV8Context(context, DOMWrapperWorld::current());
-    v8::Handle<v8::Object> creationContext = v8Context.IsEmpty() ? v8::Object::New() : v8Context->Global();
+    v8::Handle<v8::Object> creationContext = v8Context.IsEmpty() ? v8::Object::New(isolate) : v8Context->Global();
     v8::Handle<v8::Object> promise = V8PromiseCustom::createPromise(creationContext, isolate);
     return ScriptPromise(promise, isolate);
 }
@@ -55,7 +55,7 @@ ScriptPromise ScriptPromise::createPending()
 {
     v8::Isolate* isolate = v8::Isolate::GetCurrent();
     ASSERT(isolate->InContext());
-    v8::Handle<v8::Object> promise = V8PromiseCustom::createPromise(v8::Object::New(), isolate);
+    v8::Handle<v8::Object> promise = V8PromiseCustom::createPromise(v8::Object::New(isolate), isolate);
     return ScriptPromise(promise, isolate);
 }
 
