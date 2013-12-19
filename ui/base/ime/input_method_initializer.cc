84eaee1eb08fb8cbc3edff0e29d869d4451ba9a6
dnicoara@chromium.org <dnicoara@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-19 19:30:22 +0000

    Do not create an InputMethodContextFactory for Ozone in input_method_initializer.cc
    Ozone creates its own IMCF when the Ozone platform is initialized, causing
    the CHECK in InitializeInputMethodForTesting to fail since 2 IMCF have been
    created and the factory is now pointing to an unexpected value.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/118513002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241915 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/ime/input_method_initializer.cc
+++ ui/base/ime/input_method_initializer.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_CHROMEOS)
 #include "ui/base/ime/chromeos/ibus_bridge.h"
-#elif defined(USE_AURA) && defined(OS_LINUX)
+#elif defined(USE_AURA) && defined(OS_LINUX) && !defined(USE_OZONE)
 #include "ui/base/ime/input_method_auralinux.h"
 #include "ui/base/ime/linux/fake_input_method_context_factory.h"
 #elif defined(OS_WIN)
@@ -18,7 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-#if !defined(OS_CHROMEOS) && defined(USE_AURA) && defined(OS_LINUX)
+#if !defined(OS_CHROMEOS) && defined(USE_AURA) && defined(OS_LINUX) && \
+    !defined(USE_OZONE)
 const ui::LinuxInputMethodContextFactory* g_linux_input_method_context_factory;
 #endif
 
@@ -29,7 +30,7 @@ namespace ui {
 void InitializeInputMethod() {
 #if defined(OS_CHROMEOS)
   chromeos::IBusBridge::Initialize();
-#elif defined(USE_AURA) && defined(OS_LINUX)
+#elif defined(USE_AURA) && defined(OS_LINUX) && !defined(USE_OZONE)
   InputMethodAuraLinux::Initialize();
 #elif defined(OS_WIN)
   if (base::win::IsTSFAwareRequired())
@@ -50,7 +51,7 @@ void ShutdownInputMethod() {
 void InitializeInputMethodForTesting() {
 #if defined(OS_CHROMEOS)
   chromeos::IBusBridge::Initialize();
-#elif defined(USE_AURA) && defined(OS_LINUX)
+#elif defined(USE_AURA) && defined(OS_LINUX) && !defined(USE_OZONE)
   if (!g_linux_input_method_context_factory)
     g_linux_input_method_context_factory = new FakeInputMethodContextFactory();
   const LinuxInputMethodContextFactory* factory =
@@ -72,7 +73,7 @@ void InitializeInputMethodForTesting() {
 void ShutdownInputMethodForTesting() {
 #if defined(OS_CHROMEOS)
   chromeos::IBusBridge::Shutdown();
-#elif defined(USE_AURA) && defined(OS_LINUX)
+#elif defined(USE_AURA) && defined(OS_LINUX) && !defined(USE_OZONE)
   const LinuxInputMethodContextFactory* factory =
       LinuxInputMethodContextFactory::instance();
   CHECK(!factory || factory == g_linux_input_method_context_factory)
