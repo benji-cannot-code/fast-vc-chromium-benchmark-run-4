894bae316dfa7bdb1412dee1788910851dc3756f
imcheng@chromium.org <imcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-20 13:14:07 +0000

    "Reland" of r241209.
    DialServiceImpl now binds to all ipv4 interfaces instead of just the first ipv4 found. Dedup is done using address family + interface index.
    It now also checks if there are any other open sockets when notifying observers on error and sends a different error code (DIAL_SERVICE_NO_INTERFACES instead of the usual DIAL_SERVICE_SOCKET_ERROR) if there are no other open sockets. The error codes are treated the same upstream currently. Later we should make sure DIAL_SERVICE_SOCKET_ERROR doesn't clear the device list.
    
    Added unit tests for multiple interfaces and open sockets checking.
    
    TEST=unit tests, did some manual testing at the lab.
    
    Review URL: https://codereview.chromium.org/117853004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@242090 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/dial/dial_service.h
+++ chrome/browser/extensions/api/dial/dial_service.h
@@ -127,8 +127,8 @@ class DialServiceImpl : public DialService,
     // Creates a socket using |net_log| and |net_log_source| and binds it to
     // |bind_ip_address|.
     bool CreateAndBindSocket(const net::IPAddressNumber& bind_ip_address,
-                    net::NetLog* net_log,
-                    net::NetLog::Source net_log_source);
+                             net::NetLog* net_log,
+                             net::NetLog::Source net_log_source);
 
     // Sends a single discovery request |send_buffer| to |send_address|
     // over the socket.
@@ -196,6 +196,7 @@ class DialServiceImpl : public DialService,
     // Marks whether there is an active read callback.
     bool is_reading_;
 
+    FRIEND_TEST_ALL_PREFIXES(DialServiceTest, TestNotifyOnError);
     FRIEND_TEST_ALL_PREFIXES(DialServiceTest, TestOnDeviceDiscovered);
     FRIEND_TEST_ALL_PREFIXES(DialServiceTest, TestOnDiscoveryRequest);
     FRIEND_TEST_ALL_PREFIXES(DialServiceTest, TestResponseParsing);
@@ -205,11 +206,14 @@ class DialServiceImpl : public DialService,
   // Starts the control flow for one discovery cycle.
   void StartDiscovery();
 
-  // Send the network list to IO thread.
+  // For each network interface in |list|, finds all unqiue IPv4 network
+  // interfaces and call |DiscoverOnAddresses()| with their IP addresses.
   void SendNetworkList(const net::NetworkInterfaceList& list);
 
   // Calls |BindAndAddSocket()| for each address in |ip_addresses|, calls
   // |SendOneRequest()|, and start the timer to finish discovery if needed.
+  // The (Address family, interface index) of each address in |ip_addresses|
+  // must be unique. If |ip_address| is empty, calls |FinishDiscovery()|.
   void DiscoverOnAddresses(
       const std::vector<net::IPAddressNumber>& ip_addresses);
 
@@ -286,6 +290,8 @@ class DialServiceImpl : public DialService,
 
   friend class DialServiceTest;
   FRIEND_TEST_ALL_PREFIXES(DialServiceTest, TestSendMultipleRequests);
+  FRIEND_TEST_ALL_PREFIXES(DialServiceTest, TestMultipleNetworkInterfaces);
+  FRIEND_TEST_ALL_PREFIXES(DialServiceTest, TestNotifyOnError);
   FRIEND_TEST_ALL_PREFIXES(DialServiceTest, TestOnDeviceDiscovered);
   FRIEND_TEST_ALL_PREFIXES(DialServiceTest, TestOnDiscoveryFinished);
   FRIEND_TEST_ALL_PREFIXES(DialServiceTest, TestOnDiscoveryRequest);
