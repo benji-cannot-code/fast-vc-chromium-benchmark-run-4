0c5958fe27316f8995ce107876a472113ae2ccf0
mkwst@chromium.org <mkwst@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-20 15:07:59 +0000

    Improve Element exception messages.
    BUG=270033
    
    Review URL: https://codereview.chromium.org/108093006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164238 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/dom/script-tests/setAttributeNS-prefix-and-null-namespace.js
+++ third_party/WebKit/LayoutTests/fast/dom/script-tests/setAttributeNS-prefix-and-null-namespace.js
@@ -1,4 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 description("Test that calling setAttributeNS() with a prefixed qualifiedName and null NS throws NAMESPACE_ERR.");
 
-shouldThrow("document.createElement('test').setAttributeNS(null, 'foo:bar', 'baz')", "'NamespaceError: An attempt was made to create or change an object in a way which is incorrect with regard to namespaces.'");
+shouldThrow("document.createElement('test').setAttributeNS(null, 'foo:bar', 'baz')", '"NamespaceError: Failed to execute \'setAttributeNS\' on \'Element\': \'\' is an invalid namespace for attributes."');
