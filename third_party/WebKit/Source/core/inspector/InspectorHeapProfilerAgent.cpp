86ffc3c2ff90a7d11d5444fb1a60ecc0fb88b254
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-20 09:52:52 +0000

    Use HeapProfiler::FindObjectById when searching for node
    v8::HeapGraphNode::GetHeapValue is going to be deprecated soon. This change switches heap profiler implementation in Blink to FindObjectById which doesn't depend on a particular heap snapshot.
    
    BUG=324769
    
    Review URL: https://codereview.chromium.org/104313002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164224 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorHeapProfilerAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorHeapProfilerAgent.cpp
@@ -218,6 +218,7 @@ void InspectorHeapProfilerAgent::enable(ErrorString*)
 void InspectorHeapProfilerAgent::disable(ErrorString* error)
 {
     stopTrackingHeapObjectsInternal();
+    ScriptProfiler::clearHeapObjectIds();
     m_state->setBoolean(HeapProfilerAgentState::heapProfilerEnabled, false);
 }
 
