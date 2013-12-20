7f511f3161d7aca07d69008514b157f633fba0de
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-20 19:13:38 +0000

    Added merge session request throttle for XHR requests.
    BUG=308318,309131,308681
    TEST=manual, added MergeSessionTest.MergeSessionThrottle
    
    Review URL: https://codereview.chromium.org/118733002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@242135 0039d316-1c4b-4281-b951-d872f2087c98

--- google_apis/gaia/fake_gaia.h
+++ google_apis/gaia/fake_gaia.h
@@ -74,7 +74,7 @@ class FakeGaia {
   };
 
   FakeGaia();
-  ~FakeGaia();
+  virtual ~FakeGaia();
 
   // Sets the initial value of tokens and cookies.
   void SetMergeSessionParams(const MergeSessionParams& params);
@@ -106,6 +106,11 @@ class FakeGaia {
   static bool GetQueryParameter(const std::string& query,
                                 const std::string& key,
                                 std::string* value);
+ protected:
+  // HTTP handler for /MergeSession.
+  virtual void HandleMergeSession(
+      const net::test_server::HttpRequest& request,
+      net::test_server::BasicHttpResponse* http_response);
 
  private:
   typedef std::multimap<std::string, AccessTokenInfo> AccessTokenInfoMap;
@@ -131,8 +136,6 @@ class FakeGaia {
                         net::test_server::BasicHttpResponse* http_response);
   void HandleSSO(const net::test_server::HttpRequest& request,
                  net::test_server::BasicHttpResponse* http_response);
-  void HandleMergeSession(const net::test_server::HttpRequest& request,
-                          net::test_server::BasicHttpResponse* http_response);
   void HandleServiceLoginAuth(
       const net::test_server::HttpRequest& request,
       net::test_server::BasicHttpResponse* http_response);
