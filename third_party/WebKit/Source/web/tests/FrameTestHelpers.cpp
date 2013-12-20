d1794234610a6a40ecc8cabaa411a00028e0bd91
dcheng@chromium.org <dcheng@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-20 07:16:25 +0000

    Convert remaining uses of WebView::initializeMainFrame to setMainFrame.
    This is the Blink-side change, which also removes the interfaces, since
    Chrome code no longer uses this method. This patch also fixes a memory
    leak introduced in the unit tests by r164135 and adds a warning note to
    WebView::createChildFrame() to hopefully prevent this from happening
    again.
    
    BUG=329266
    R=eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/112023012
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164214 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/tests/FrameTestHelpers.cpp
+++ third_party/WebKit/Source/web/tests/FrameTestHelpers.cpp
@@ -97,8 +97,7 @@ void runPendingTasks()
 }
 
 WebViewHelper::WebViewHelper()
-    : m_mainFrame(0)
-    , m_webView(0)
+    : m_webView(0)
 {
 }
 
@@ -124,8 +123,7 @@ WebViewImpl* WebViewHelper::initialize(bool enableJavascript, WebFrameClient* we
         m_webView->settings()->setForceCompositingMode(true);
     }
 
-    m_mainFrame = WebFrameImpl::create(webFrameClient);
-    m_webView->setMainFrame(m_mainFrame);
+    m_webView->setMainFrame(WebFrameImpl::create(webFrameClient));
 
     return m_webView;
 }
@@ -146,16 +144,18 @@ void WebViewHelper::reset()
         m_webView->close();
         m_webView = 0;
     }
-    if (m_mainFrame) {
-        m_mainFrame->close();
-        m_mainFrame = 0;
-    }
 }
 
 WebFrame* TestWebFrameClient::createChildFrame(WebFrame* parent, const WebString& frameName)
 {
     return WebFrame::create(this);
 }
+
+void TestWebFrameClient::frameDetached(WebFrame* frame)
+{
+    frame->close();
+}
+
 
 } // namespace FrameTestHelpers
 } // namespace blink
