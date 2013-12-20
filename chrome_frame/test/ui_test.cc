7afd73ef888a741783e7d05b6097e931665bc68d
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-20 17:35:57 +0000

    Add base:: to string16 in chrome_frame/.
    R=grt@chromium.org
    BUG=329295
    
    Review URL: https://codereview.chromium.org/113143006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@242115 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/test/ui_test.cc
+++ chrome_frame/test/ui_test.cc
@@ -485,7 +485,7 @@ class ContextMenuTest : public MockIEEventSinkTest, public testing::Test {
     InSequence expect_in_sequence_for_scope;
 
     // Open 'Save As' dialog.
-    string16 initial_url(GetTestUrl(L"save_as_context_menu.html"));
+    base::string16 initial_url(GetTestUrl(L"save_as_context_menu.html"));
     const char* kSaveDlgCaption = "Save As";
     EXPECT_CALL(acc_observer_,
                 OnAccDocLoad(TabContentsTitleEq(initial_url,
@@ -533,7 +533,7 @@ TEST_F(ContextMenuTest, CFReload) {
   server_mock_.ExpectAndServeAnyRequests(CFInvocation::MetaTag());
   InSequence expect_in_sequence_for_scope;
 
-  string16 initial_url(GetSimplePageUrl());
+  base::string16 initial_url(GetSimplePageUrl());
   EXPECT_CALL(acc_observer_,
               OnAccDocLoad(TabContentsTitleEq(initial_url,
                                               GetSimplePageTitle())))
@@ -560,7 +560,7 @@ TEST_F(ContextMenuTest, CFViewSource) {
   MockIEEventSink view_source_mock;
   view_source_mock.ExpectAnyNavigations();
   InSequence expect_in_sequence_for_scope;
-  string16 initial_url(GetSimplePageUrl());
+  base::string16 initial_url(GetSimplePageUrl());
 
   // View the page source.
   EXPECT_CALL(acc_observer_,
@@ -598,7 +598,7 @@ TEST_F(ContextMenuTest, DISABLED_CFPageInfo) {
   server_mock_.ExpectAndServeAnyRequests(CFInvocation::MetaTag());
   MockWindowObserver win_observer_mock;
   InSequence expect_in_sequence_for_scope;
-  string16 initial_url(GetSimplePageUrl());
+  base::string16 initial_url(GetSimplePageUrl());
 
   // View page information.
   EXPECT_CALL(acc_observer_,
@@ -631,7 +631,7 @@ TEST_F(ContextMenuTest, CFInspector) {
   // Devtools begins life with "Untitled" caption and it changes
   // later to the 'Developer Tools - <url> form.
   const char* kPageInfoCaptionPattern = "Untitled*";
-  string16 initial_url(GetSimplePageUrl());
+  base::string16 initial_url(GetSimplePageUrl());
   EXPECT_CALL(acc_observer_,
               OnAccDocLoad(TabContentsTitleEq(initial_url,
                                               GetSimplePageTitle())))
@@ -689,7 +689,7 @@ TEST_F(ContextMenuTest, CFAboutVersionLoads) {
   MockIEEventSink new_window_mock;
   new_window_mock.ExpectAnyNavigations();
   InSequence expect_in_sequence_for_scope;
-  string16 initial_url(GetSimplePageUrl());
+  base::string16 initial_url(GetSimplePageUrl());
 
   EXPECT_CALL(acc_observer_,
               OnAccDocLoad(TabContentsTitleEq(initial_url,
@@ -718,7 +718,7 @@ TEST_F(ContextMenuTest, CFAboutVersionLoads) {
 TEST_F(ContextMenuTest, IEOpen) {
   server_mock_.ExpectAndServeAnyRequests(CFInvocation::None());
   InSequence expect_in_sequence_for_scope;
-  string16 initial_url(GetLinkPageUrl());
+  base::string16 initial_url(GetLinkPageUrl());
 
   // Open the link through the context menu.
   EXPECT_CALL(acc_observer_,
@@ -745,7 +745,7 @@ TEST_F(ContextMenuTest, IEOpenInNewWindow) {
   MockIEEventSink new_window_mock;
   new_window_mock.ExpectAnyNavigations();
   InSequence expect_in_sequence_for_scope;
-  string16 initial_url(GetLinkPageUrl());
+  base::string16 initial_url(GetLinkPageUrl());
 
   // Open the link in a new window.
   EXPECT_CALL(acc_observer_,
@@ -808,7 +808,7 @@ TEST_F(ContextMenuTest, DISABLED_CFOpenLinkInNewWindow) {
   server_mock_.ExpectAndServeAnyRequests(CFInvocation::MetaTag());
   MockIEEventSink new_window_mock;
   new_window_mock.ExpectAnyNavigations();
-  string16 initial_url(GetLinkPageUrl());
+  base::string16 initial_url(GetLinkPageUrl());
 
   // Invoke 'Open link in new window' context menu item.
   EXPECT_CALL(acc_observer_,
@@ -831,7 +831,7 @@ TEST_F(ContextMenuTest, DISABLED_CFOpenLinkInNewWindow) {
 // Test CF link context menu - Copy link address.
 TEST_F(ContextMenuTest, CFCopyLinkAddress) {
   server_mock_.ExpectAndServeAnyRequests(CFInvocation::MetaTag());
-  string16 initial_url(GetLinkPageUrl());
+  base::string16 initial_url(GetLinkPageUrl());
 
   // Invoke 'Copy link address' context menu item.
   EXPECT_CALL(acc_observer_,
--- chrome_frame/test_utils.h
+++ chrome_frame/test_utils.h
@@ -72,7 +72,7 @@ class ScopedChromeFrameRegistrar {
   // The string "--call-registration-entrypoint".
   static const wchar_t kCallRegistrationEntrypointSwitch[];
 
-  static void DoRegistration(const string16& path,
+  static void DoRegistration(const base::string16& path,
                              RegistrationType registration_type,
                              RegistrationOperation registration_operation);
 
