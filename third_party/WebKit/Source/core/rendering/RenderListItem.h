8372b44863c896c5ea55a518663b335e5c3f8ee8
leviw@chromium.org <leviw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-20 04:09:12 +0000

    Crash reparenting a floating inline list item without a list
    When a list item isn't in a list, without adjusting the style we instruct
    the renderer to behave as if its marker is inside. Floats are layed out
    late in line layout, and if the list item is moved and the marker already
    created, updateMarkerLocation would mark the chain through its containing
    block as needing layout towards the end of that block's layout, and
    therefor with stale pointers.
    
    There's no reason to defer calling updateMarkerLocation in this case.
    This change gets the marker's renderer properly positioned during
    attach.
    
    BUG=301196
    
    Review URL: https://codereview.chromium.org/75683002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164210 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderListItem.h
+++ third_party/WebKit/Source/core/rendering/RenderListItem.h
@@ -50,6 +50,7 @@ public:
     String markerTextWithSuffix() const;
 
     void updateListMarkerNumbers();
+    void updateMarkerLocation();
 
     static void updateItemValuesForOrderedList(const HTMLOListElement*);
     static unsigned itemCountForOrderedList(const HTMLOListElement*);
@@ -77,7 +78,6 @@ private:
 
     virtual void addOverflowFromChildren();
 
-    void updateMarkerLocation();
     inline int calcValue() const;
     void updateValueNow() const;
     void explicitValueChanged();
