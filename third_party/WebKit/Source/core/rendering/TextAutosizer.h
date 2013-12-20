7e9b90bb9713b79eca40918275120184124173dd
timvolodine@chromium.org <timvolodine@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-20 16:32:13 +0000

    Text Autosizer: ancestry-based hashing approach.
    Initial implementation of the ancestry-based hashing approach
    for autosizing repeating patterns, like forum comments.
    
    For each cluster we compute a hash value taking into account
    ancestor tag names and style properties. These hashes are stored
    in a HashSet and used to consistently autosize clusters with the
    same hash value.
    
    To improve efficiency the hashes of the ancestors are cached,
    so they need to be computed only once per call to processSubtree().
    
    Testing on forum sites, e.g.
    - http://forum.vudu.com/showthread.php?t=202551 (vBulletin)
    - https://www.phpbb.com/community/viewtopic.php?f=46&t=2203461 (phpBB)
    - http://bitcointalk.org/ (SMF)
    shows good results, i.e. the comments are autosized consistently.
    
    BUG=135947,312880
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/65643011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164240 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/TextAutosizer.h
+++ third_party/WebKit/Source/core/rendering/TextAutosizer.h
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "HTMLNames.h"
 #include "platform/text/WritingMode.h"
+#include "wtf/HashMap.h"
+#include "wtf/HashSet.h"
 #include "wtf/Noncopyable.h"
 #include "wtf/PassOwnPtr.h"
 
@@ -72,6 +74,8 @@ private:
     void setMultiplier(RenderObject*, float);
     void setMultiplierForList(RenderObject* renderer, float multiplier);
 
+    unsigned getCachedHash(const RenderObject* renderer, bool putInCacheIfAbsent);
+
     static bool isAutosizingContainer(const RenderObject*);
     static bool isNarrowDescendant(const RenderBlock*, TextAutosizingClusterInfo& parentClusterInfo);
     static bool isWiderDescendant(const RenderBlock*, const TextAutosizingClusterInfo& parentClusterInfo);
@@ -85,6 +89,8 @@ private:
     static bool clusterShouldBeAutosized(TextAutosizingClusterInfo&, float blockWidth);
     static bool compositeClusterShouldBeAutosized(Vector<TextAutosizingClusterInfo>&, float blockWidth);
     static void measureDescendantTextWidth(const RenderBlock* container, TextAutosizingClusterInfo&, float minTextWidth, float& textWidth);
+    unsigned computeCompositeClusterHash(Vector<TextAutosizingClusterInfo>&);
+    float computeMultiplier(Vector<TextAutosizingClusterInfo>&, const TextAutosizingWindowInfo&, float textWidth);
 
     // Use to traverse the tree of descendants, excluding descendants of containers (but returning the containers themselves).
     static RenderObject* nextInPreOrderSkippingDescendantsOfContainers(const RenderObject*, const RenderObject* stayWithin);
@@ -101,6 +107,9 @@ private:
     static void getNarrowDescendantsGroupedByWidth(const TextAutosizingClusterInfo& parentClusterInfo, Vector<Vector<TextAutosizingClusterInfo> >&);
 
     Document* m_document;
+
+    WTF::HashSet<unsigned> m_autosizedClusterHashes;
+    WTF::HashMap<const RenderObject*, unsigned> m_hashCache;
 };
 
 } // namespace WebCore
