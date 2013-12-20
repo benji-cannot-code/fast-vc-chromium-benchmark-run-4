27da8246520da0b09725166e4f3e31058a3aabac
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-20 17:47:30 +0000

    DevTools: Make hash case-insensitive in chrome://inspect
    BUG=330112
    
    Review URL: https://codereview.chromium.org/108843003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@242122 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/inspect/inspect.js
+++ chrome/browser/resources/inspect/inspect.js
@@ -41,7 +41,7 @@ function onload() {
 }
 
 function onHashChange() {
-  var hash = window.location.hash.slice(1);
+  var hash = window.location.hash.slice(1).toLowerCase();
   if (!selectTab(hash))
     selectTab('devices');
 }
