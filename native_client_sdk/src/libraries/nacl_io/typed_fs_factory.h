38484bd8cba12b3e8bba62660bdc12c63b2dd09d
binji@chromium.org <binji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-20 01:01:17 +0000

    [NaCl SDK] nacl_io: Rename Mount -> Filesystem, MountNode -> Node.
    (This basically renames everything...)
    
    This change was originally landed at r240799, but was reverted speculatively. See https://codereview.chromium.org/113123003/
    
    BUG=none
    TBR=noelallen@chromium.org, sbc@chromium.org
    
    Review URL: https://codereview.chromium.org/111143003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@242002 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ native_client_sdk/src/libraries/nacl_io/typed_fs_factory.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef LIBRARIES_NACL_IO_TYPED_FS_FACTORY_H_
+#define LIBRARIES_NACL_IO_TYPED_FS_FACTORY_H_
+
+#include "nacl_io/filesystem.h"
+#include "nacl_io/fs_factory.h"
+
+namespace nacl_io {
+
+template <typename T>
+class TypedFsFactory : public FsFactory {
+ public:
+  virtual Error CreateFilesystem(const FsInitArgs& args,
+                                 ScopedFilesystem* out_fs) {
+    sdk_util::ScopedRef<T> fs(new T());
+    Error error = fs->Init(args);
+    if (error)
+      return error;
+
+    *out_fs = fs;
+    return 0;
+  }
+};
+
+}  // namespace nacl_io
+
+#endif  // LIBRARIES_NACL_IO_TYPED_FS_FACTORY_H_
