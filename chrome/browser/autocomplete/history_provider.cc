0d932d27757577246a80fe49c3037c27d26c14a4
mpearson@chromium.org <mpearson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-20 07:27:54 +0000

    Omnibox: Bug Fixes for Shortcuts Inlining
    Precisely, fixes up URL-like inputs before comparing to the shortcuts
    fill_into_edit in order to better determine inlineability.
    
    This is especially useful for inputs such as about: which need to have
    their scheme fixed up to be recognized as comparable to chrome://
    shortcuts.
    
    BUG=328220
    
    Review URL: https://codereview.chromium.org/108643003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@242029 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/history_provider.cc
+++ chrome/browser/autocomplete/history_provider.cc
@@ -41,29 +41,6 @@ void HistoryProvider::DeleteMatch(const AutocompleteMatch& match) {
   DeleteMatchFromMatches(match);
 }
 
-HistoryProvider::~HistoryProvider() {}
-
-void HistoryProvider::DeleteMatchFromMatches(const AutocompleteMatch& match) {
-  bool found = false;
-  for (ACMatches::iterator i(matches_.begin()); i != matches_.end(); ++i) {
-    if (i->destination_url == match.destination_url && i->type == match.type) {
-      found = true;
-      if (i->is_history_what_you_typed_match || i->starred) {
-        // We can't get rid of What-You-Typed or Bookmarked matches,
-        // but we can make them look like they have no backing data.
-        i->deletable = false;
-        i->description.clear();
-        i->description_class.clear();
-      } else {
-        matches_.erase(i);
-      }
-      break;
-    }
-  }
-  DCHECK(found) << "Asked to delete a URL that isn't in our set of matches";
-  listener_->OnProviderUpdate(true);
-}
-
 // static
 bool HistoryProvider::FixupUserInput(AutocompleteInput* input) {
   const base::string16& input_text = input->text();
@@ -135,6 +112,29 @@ bool HistoryProvider::FixupUserInput(AutocompleteInput* input) {
   return !output.empty();
 }
 
+HistoryProvider::~HistoryProvider() {}
+
+void HistoryProvider::DeleteMatchFromMatches(const AutocompleteMatch& match) {
+  bool found = false;
+  for (ACMatches::iterator i(matches_.begin()); i != matches_.end(); ++i) {
+    if (i->destination_url == match.destination_url && i->type == match.type) {
+      found = true;
+      if (i->is_history_what_you_typed_match || i->starred) {
+        // We can't get rid of What-You-Typed or Bookmarked matches,
+        // but we can make them look like they have no backing data.
+        i->deletable = false;
+        i->description.clear();
+        i->description_class.clear();
+      } else {
+        matches_.erase(i);
+      }
+      break;
+    }
+  }
+  DCHECK(found) << "Asked to delete a URL that isn't in our set of matches";
+  listener_->OnProviderUpdate(true);
+}
+
 // static
 size_t HistoryProvider::TrimHttpPrefix(base::string16* url) {
   // Find any "http:".
@@ -192,4 +192,3 @@ ACMatchClassifications HistoryProvider::SpansFromTermMatch(
 
   return spans;
 }
-
