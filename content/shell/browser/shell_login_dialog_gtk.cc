8ccf5b19ba0297cb405c0faf188788509f2e8f80
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-21 17:53:58 +0000

    Replace the GetAssociatedRenderView call with GetAssociatedRenderFrame in ShellLoginDialog.
    BUG=304341
    R=nasko@chromium.org
    
    Review URL: https://codereview.chromium.org/108333008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@242283 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/browser/shell_login_dialog_gtk.cc
+++ content/shell/browser/shell_login_dialog_gtk.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string16.h"
 #include "base/strings/utf_string_conversions.h"
 #include "content/public/browser/browser_thread.h"
-#include "content/public/browser/render_view_host.h"
+#include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/resource_dispatcher_host.h"
 #include "content/public/browser/resource_request_info.h"
 #include "content/public/browser/web_contents.h"
@@ -24,17 +24,17 @@ void ShellLoginDialog::PlatformCreateDialog(const base::string16& message) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
 
   int render_process_id;
-  int render_view_id;
-  if (!ResourceRequestInfo::ForRequest(request_)->GetAssociatedRenderView(
-          &render_process_id,  &render_view_id)) {
+  int render_frame_id;
+  if (!ResourceRequestInfo::ForRequest(request_)->GetAssociatedRenderFrame(
+          &render_process_id,  &render_frame_id)) {
     NOTREACHED();
   }
 
   WebContents* web_contents = NULL;
-  RenderViewHost* render_view_host =
-      RenderViewHost::FromID(render_process_id, render_view_id);
-  if (render_view_host)
-    web_contents = WebContents::FromRenderViewHost(render_view_host);
+  RenderFrameHost* render_frame_host =
+      RenderFrameHost::FromID(render_process_id, render_frame_id);
+  if (render_frame_host)
+    web_contents = WebContents::FromRenderFrameHost(render_frame_host);
   DCHECK(web_contents);
 
   gfx::NativeWindow parent_window =
