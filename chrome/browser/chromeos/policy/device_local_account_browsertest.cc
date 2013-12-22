8bf82e796fa4c4102d4c1f3aa79846f85c371e0c
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-22 19:09:54 +0000

    Disable a bunch of policy-related tests that started failing a while ago.
    I tried reverting several changes first (see BUG), but didn't find the right
    one.  The consistent failures suggest that this feature is broken, so this
    needs to be investigated by someone familiar with this code.
    
    BUG=330454
    TBR=skuhne@chromium.org,mnissler@chromium.org
    
    Review URL: https://codereview.chromium.org/120563002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@242326 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/policy/device_local_account_browsertest.cc
+++ chrome/browser/chromeos/policy/device_local_account_browsertest.cc
@@ -431,7 +431,8 @@ static bool IsKnownUser(const std::string& account_id) {
   return chromeos::UserManager::Get()->IsKnownUser(account_id);
 }
 
-IN_PROC_BROWSER_TEST_F(DeviceLocalAccountTest, LoginScreen) {
+// http://crbug.com/330454
+IN_PROC_BROWSER_TEST_F(DeviceLocalAccountTest, DISABLED_LoginScreen) {
   AddPublicSessionToDevicePolicy(kAccountId1);
   AddPublicSessionToDevicePolicy(kAccountId2);
 
@@ -456,7 +457,8 @@ static bool DisplayNameMatches(const std::string& account_id,
   return true;
 }
 
-IN_PROC_BROWSER_TEST_F(DeviceLocalAccountTest, DisplayName) {
+// http://crbug.com/330454
+IN_PROC_BROWSER_TEST_F(DeviceLocalAccountTest, DISABLED_DisplayName) {
   UploadAndInstallDeviceLocalAccountPolicy();
   AddPublicSessionToDevicePolicy(kAccountId1);
 
@@ -465,7 +467,8 @@ IN_PROC_BROWSER_TEST_F(DeviceLocalAccountTest, DisplayName) {
       base::Bind(&DisplayNameMatches, user_id_1_, kDisplayName)).Wait();
 }
 
-IN_PROC_BROWSER_TEST_F(DeviceLocalAccountTest, PolicyDownload) {
+// http://crbug.com/330454
+IN_PROC_BROWSER_TEST_F(DeviceLocalAccountTest, DISABLED_PolicyDownload) {
   UploadDeviceLocalAccountPolicy();
   AddPublicSessionToDevicePolicy(kAccountId1);
 
@@ -524,7 +527,8 @@ static bool IsSessionStarted() {
   return chromeos::UserManager::Get()->IsSessionStarted();
 }
 
-IN_PROC_BROWSER_TEST_F(DeviceLocalAccountTest, StartSession) {
+// http://crbug.com/330454
+IN_PROC_BROWSER_TEST_F(DeviceLocalAccountTest, DISABLED_StartSession) {
   // Specify startup pages.
   device_local_account_policy_.payload().mutable_restoreonstartup()->set_value(
       SessionStartupPref::kPrefValueURLs);
@@ -582,7 +586,8 @@ IN_PROC_BROWSER_TEST_F(DeviceLocalAccountTest, StartSession) {
   }
 }
 
-IN_PROC_BROWSER_TEST_F(DeviceLocalAccountTest, FullscreenDisallowed) {
+// http://crbug.com/330454
+IN_PROC_BROWSER_TEST_F(DeviceLocalAccountTest, DISABLED_FullscreenDisallowed) {
   UploadAndInstallDeviceLocalAccountPolicy();
   AddPublicSessionToDevicePolicy(kAccountId1);
 
@@ -637,7 +642,8 @@ IN_PROC_BROWSER_TEST_F(DeviceLocalAccountTest, FullscreenDisallowed) {
   EXPECT_FALSE(browser_window->IsFullscreen());
 }
 
-IN_PROC_BROWSER_TEST_F(DeviceLocalAccountTest, ExtensionsUncached) {
+// http://crbug.com/330454
+IN_PROC_BROWSER_TEST_F(DeviceLocalAccountTest, DISABLED_ExtensionsUncached) {
   // Make it possible to force-install a hosted app and an extension.
   ASSERT_TRUE(embedded_test_server()->InitializeAndWaitUntilReady());
   TestingUpdateManifestProvider testing_update_manifest_provider(
@@ -735,7 +741,8 @@ IN_PROC_BROWSER_TEST_F(DeviceLocalAccountTest, ExtensionsUncached) {
       kAccountId1, kGoodExtensionID, kGoodExtensionVersion)));
 }
 
-IN_PROC_BROWSER_TEST_F(DeviceLocalAccountTest, ExtensionsCached) {
+// http://crbug.com/330454
+IN_PROC_BROWSER_TEST_F(DeviceLocalAccountTest, DISABLED_ExtensionsCached) {
   ASSERT_TRUE(embedded_test_server()->InitializeAndWaitUntilReady());
 
   // Pre-populate the device local account's extension cache with a hosted app
@@ -828,7 +835,8 @@ IN_PROC_BROWSER_TEST_F(DeviceLocalAccountTest, ExtensionsCached) {
   EXPECT_FALSE(PathExists(cached_extension));
 }
 
-IN_PROC_BROWSER_TEST_F(DeviceLocalAccountTest, ExternalData) {
+// http://crbug.com/330454
+IN_PROC_BROWSER_TEST_F(DeviceLocalAccountTest, DISABLED_ExternalData) {
   // chromeos::UserImageManagerImpl requests an external data fetch whenever the
   // key::kUserAvatarImage policy is set. Since this test wants to verify that
   // the underlying policy subsystem will start a fetch without this request as
@@ -953,7 +961,8 @@ IN_PROC_BROWSER_TEST_F(DeviceLocalAccountTest, ExternalData) {
   EXPECT_EQ(kExternalData, *fetched_external_data);
 }
 
-IN_PROC_BROWSER_TEST_F(DeviceLocalAccountTest, UserAvatarImage) {
+// http://crbug.com/330454
+IN_PROC_BROWSER_TEST_F(DeviceLocalAccountTest, DISABLED_UserAvatarImage) {
   ASSERT_TRUE(embedded_test_server()->InitializeAndWaitUntilReady());
 
   UploadDeviceLocalAccountPolicy();
@@ -1035,7 +1044,8 @@ class TermsOfServiceTest : public DeviceLocalAccountTest,
                            public testing::WithParamInterface<bool> {
 };
 
-IN_PROC_BROWSER_TEST_P(TermsOfServiceTest, TermsOfServiceScreen) {
+// http://crbug.com/330454
+IN_PROC_BROWSER_TEST_P(TermsOfServiceTest, DISABLED_TermsOfServiceScreen) {
   // Specify Terms of Service URL.
   ASSERT_TRUE(embedded_test_server()->InitializeAndWaitUntilReady());
   device_local_account_policy_.payload().mutable_termsofserviceurl()->set_value(
