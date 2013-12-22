4c950fdd728d06eb8d256892c232ac70888871a2
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-22 23:21:55 +0000

    Fix memory leak in TransientWindowStackingClientTest caused by r242126.
    http://src.chromium.org/viewvc/chrome/trunk/src/ui/aura/client/window_stacking_client.cc?r1=241867&r2=242126
    changed SetWindowStackingClient() to no longer delete the old client, but it
    didn't update these tests which relied on that behavior. Explicitly delete this
    object in the test.
    
    BUG=330382
    TBR=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/120503004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@242330 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/corewm/transient_window_stacking_client_unittest.cc
+++ ui/views/corewm/transient_window_stacking_client_unittest.cc
@@ -27,6 +27,7 @@ class TransientWindowStackingClientTest : public aura::test::AuraTestBase {
   }
 
   virtual void TearDown() OVERRIDE {
+    delete aura::client::GetWindowStackingClient();
     aura::client::SetWindowStackingClient(NULL);
     AuraTestBase::TearDown();
   }
