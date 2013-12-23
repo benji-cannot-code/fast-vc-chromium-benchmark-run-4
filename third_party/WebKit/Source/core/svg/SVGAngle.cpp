64c44483a868d8d88c12f9bd72cff6cbcf7c73c2
jbroman@chromium.org <jbroman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-23 02:13:58 +0000

    Improve exception messages for SVGAngle and SVGLength.
    Also add some additional tests for SVGLength to capture one of the affected exception messages.
    
    BUG=270033
    
    Review URL: https://codereview.chromium.org/106543007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164277 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGAngle.cpp
+++ third_party/WebKit/Source/core/svg/SVGAngle.cpp
@@ -164,7 +164,7 @@ void SVGAngle::setValueAsString(const String& value, ExceptionState& exceptionSt
     bool success = value.is8Bit() ? parseValue<LChar>(value, valueInSpecifiedUnits, unitType)
                                   : parseValue<UChar>(value, valueInSpecifiedUnits, unitType);
     if (!success) {
-        exceptionState.throwUninformativeAndGenericDOMException(SyntaxError);
+        exceptionState.throwDOMException(SyntaxError, "The value provided ('" + value + "') is invalid.");
         return;
     }
 
@@ -175,7 +175,7 @@ void SVGAngle::setValueAsString(const String& value, ExceptionState& exceptionSt
 void SVGAngle::newValueSpecifiedUnits(unsigned short unitType, float valueInSpecifiedUnits, ExceptionState& exceptionState)
 {
     if (unitType == SVG_ANGLETYPE_UNKNOWN || unitType > SVG_ANGLETYPE_GRAD) {
-        exceptionState.throwUninformativeAndGenericDOMException(NotSupportedError);
+        exceptionState.throwDOMException(NotSupportedError, "Cannot set value with unknown or invalid units (" + String::number(unitType) + ").");
         return;
     }
 
@@ -187,8 +187,12 @@ void SVGAngle::newValueSpecifiedUnits(unsigned short unitType, float valueInSpec
 
 void SVGAngle::convertToSpecifiedUnits(unsigned short unitType, ExceptionState& exceptionState)
 {
-    if (unitType == SVG_ANGLETYPE_UNKNOWN || m_unitType == SVG_ANGLETYPE_UNKNOWN || unitType > SVG_ANGLETYPE_GRAD) {
-        exceptionState.throwUninformativeAndGenericDOMException(NotSupportedError);
+    if (unitType == SVG_ANGLETYPE_UNKNOWN || unitType > SVG_ANGLETYPE_GRAD) {
+        exceptionState.throwDOMException(NotSupportedError, "Cannot convert to unknown or invalid units (" + String::number(unitType) + ").");
+        return;
+    }
+    if (m_unitType == SVG_ANGLETYPE_UNKNOWN) {
+        exceptionState.throwDOMException(NotSupportedError, "Cannot convert from unknown or invalid units.");
         return;
     }
 
--- third_party/WebKit/Source/core/svg/SVGLength.cpp
+++ third_party/WebKit/Source/core/svg/SVGLength.cpp
@@ -258,7 +258,7 @@ void SVGLength::setValueAsString(const String& string, ExceptionState& exception
         parseValueInternal<UChar>(string, convertedNumber, type);
 
     if (!success) {
-        exceptionState.throwUninformativeAndGenericDOMException(SyntaxError);
+        exceptionState.throwDOMException(SyntaxError, "The value provided ('" + string + "') is invalid.");
         return;
     }
 
@@ -274,7 +274,7 @@ String SVGLength::valueAsString() const
 void SVGLength::newValueSpecifiedUnits(unsigned short type, float value, ExceptionState& exceptionState)
 {
     if (type == LengthTypeUnknown || type > LengthTypePC) {
-        exceptionState.throwUninformativeAndGenericDOMException(NotSupportedError);
+        exceptionState.throwDOMException(NotSupportedError, "Cannot set value with unknown or invalid units (" + String::number(type) + ").");
         return;
     }
 
@@ -285,7 +285,7 @@ void SVGLength::newValueSpecifiedUnits(unsigned short type, float value, Excepti
 void SVGLength::convertToSpecifiedUnits(unsigned short type, const SVGLengthContext& context, ExceptionState& exceptionState)
 {
     if (type == LengthTypeUnknown || type > LengthTypePC) {
-        exceptionState.throwUninformativeAndGenericDOMException(NotSupportedError);
+        exceptionState.throwDOMException(NotSupportedError, "Cannot convert to unknown or invalid units (" + String::number(type) + ").");
         return;
     }
 
