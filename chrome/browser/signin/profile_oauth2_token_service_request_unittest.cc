48cdb2fd98396f04dcc5786f76974690b571d35e
ckocagil@chromium.org <ckocagil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-23 13:08:25 +0000

    Revert of https://codereview.chromium.org/120653002/ Reason for revert: Re-land. The speculative revert didn't fix the cros bot.
    TBR=acleung@chromium.org,guohui@chromium.org,atwilson@chromium.org,rogerta@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=320351
    
    Review URL: https://codereview.chromium.org/104663011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@242376 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/signin/profile_oauth2_token_service_request_unittest.cc
+++ chrome/browser/signin/profile_oauth2_token_service_request_unittest.cc
@@ -105,9 +105,8 @@ TEST_F(ProfileOAuth2TokenServiceRequestTest,
   EXPECT_EQ(1, consumer_.number_of_errors_);
 }
 
-TEST_F(ProfileOAuth2TokenServiceRequestTest,
-       Success) {
-  oauth2_service_->IssueRefreshToken(kRefreshToken);
+TEST_F(ProfileOAuth2TokenServiceRequestTest, Success) {
+  oauth2_service_->UpdateCredentials(kAccountId, kRefreshToken);
   scoped_ptr<ProfileOAuth2TokenServiceRequest> request(
       ProfileOAuth2TokenServiceRequest::CreateAndStart(
           profile_.get(),
@@ -124,7 +123,7 @@ TEST_F(ProfileOAuth2TokenServiceRequestTest,
 
 TEST_F(ProfileOAuth2TokenServiceRequestTest,
        RequestDeletionBeforeServiceComplete) {
-  oauth2_service_->IssueRefreshToken(kRefreshToken);
+  oauth2_service_->UpdateCredentials(kAccountId, kRefreshToken);
   scoped_ptr<ProfileOAuth2TokenServiceRequest> request(
       ProfileOAuth2TokenServiceRequest::CreateAndStart(
           profile_.get(),
@@ -141,7 +140,7 @@ TEST_F(ProfileOAuth2TokenServiceRequestTest,
 
 TEST_F(ProfileOAuth2TokenServiceRequestTest,
        RequestDeletionAfterServiceComplete) {
-  oauth2_service_->IssueRefreshToken(kRefreshToken);
+  oauth2_service_->UpdateCredentials(kAccountId, kRefreshToken);
   scoped_ptr<ProfileOAuth2TokenServiceRequest> request(
       ProfileOAuth2TokenServiceRequest::CreateAndStart(
           profile_.get(),
