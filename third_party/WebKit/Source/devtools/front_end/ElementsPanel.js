d360b29e6cd549269e3b1cdbb4806c72e7a6e803
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-23 16:14:36 +0000

    DevTools: [Elements] Make the sidebar panes tabbed pane header a resizer
    Now that the chevron issue is universally fixed in r164293,
    we can roll in the reverted part of r163547
    
    R=aandrey, eustas, vsevik
    
    Review URL: https://codereview.chromium.org/115723004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164296 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ElementsPanel.js
+++ third_party/WebKit/Source/devtools/front_end/ElementsPanel.js
@@ -1221,8 +1221,10 @@ WebInspector.ElementsPanel.prototype = {
         if (this.sidebarPaneView && vertically === !this.splitView.isVertical())
             return;
 
-        if (this.sidebarPaneView)
+        if (this.sidebarPaneView) {
             this.sidebarPaneView.detach();
+            this.splitView.uninstallResizer(this.sidebarPaneView.headerElement());
+        }
 
         this.splitView.setVertical(!vertically);
 
@@ -1264,6 +1266,7 @@ WebInspector.ElementsPanel.prototype = {
         this.sidebarPaneView = new WebInspector.SidebarTabbedPane();
 
         if (vertically) {
+            this.splitView.installResizer(this.sidebarPaneView.headerElement());
             this.sidebarPanes.metrics.show(computedPane.bodyElement, this.sidebarPanes.computedStyle.element);
             this.sidebarPanes.metrics.setExpandCallback(expandComputed);
 
