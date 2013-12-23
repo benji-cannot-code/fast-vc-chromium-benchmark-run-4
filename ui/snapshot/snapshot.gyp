eff646d0e8b0e8d8977810c810402832d4091e75
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-23 06:16:02 +0000

    Add AshDesktopMediaList and enable Desktop Capture API on Chrome OS.
    The new AshDesktopMediaList gets list of windows/screens for the window
    picker dialog which allows to enable Desktop Capture API on Chrome OS.
    
    BUG=295102
    R=ben@chromium.org, hshi@chromium.org
    
    Review URL: https://codereview.chromium.org/100833002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@242361 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/snapshot/snapshot.gyp
+++ ui/snapshot/snapshot.gyp
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'conditions': [
         ['use_aura==1', {
           'dependencies': [
+            '../../cc/cc.gyp:cc',
             '../aura/aura.gyp:aura',
             '../compositor/compositor.gyp:compositor',
           ],
--- ui/snapshot/snapshot_win.cc
+++ ui/snapshot/snapshot_win.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/snapshot/snapshot_win.h"
 
+#include "base/callback.h"
 #include "base/win/scoped_gdi_object.h"
 #include "base/win/scoped_hdc.h"
 #include "base/win/scoped_select_object.h"
@@ -104,6 +105,7 @@ bool GrabHwndSnapshot(HWND window_handle,
 }  // namespace internal
 
 #if !defined(USE_AURA)
+
 bool GrabViewSnapshot(gfx::NativeView view_handle,
                       std::vector<unsigned char>* png_representation,
                       const gfx::Rect& snapshot_bounds) {
@@ -117,6 +119,16 @@ bool GrabWindowSnapshot(gfx::NativeWindow window_handle,
   return internal::GrabHwndSnapshot(window_handle, snapshot_bounds,
                                     png_representation);
 }
+
+SNAPSHOT_EXPORT void GrapWindowSnapshotAsync(
+    gfx::NativeWindow window,
+    const gfx::Rect& snapshot_bounds,
+    const gfx::Size& target_size,
+    scoped_refptr<base::TaskRunner> background_task_runner,
+    GrapWindowSnapshotAsyncCallback callback) {
+  NOTIMPLEMENTED();
+}
+
 #endif  // !defined(USE_AURA)
 
 }  // namespace ui
