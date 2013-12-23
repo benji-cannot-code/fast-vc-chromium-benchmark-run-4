8e92e35a5bbbe026cabd46c33bff9428584bde6a
ppi@chromium.org <ppi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-23 17:18:52 +0000

    Address checkstyle errors in ContentViewCore.
    TBR=aurimas@chromium.org, yfriedman@chromium.org
    BUG=none
    
    Review URL: https://codereview.chromium.org/106443004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@242383 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -459,6 +459,7 @@ public class ContentViewCore
 
     private ViewAndroid mViewAndroid;
 
+    /** ActionAfterDoubleTap defined in tools/metrics/histograms/histograms.xml. */
     public static class UMAActionAfterDoubleTap {
         public static final int NAVIGATE_BACK = 0;
         public static final int NAVIGATE_STOP = 1;
@@ -466,6 +467,7 @@ public class ContentViewCore
         public static final int COUNT = 3;
     }
 
+    /** TapDelayType defined in tools/metrics/histograms/histograms.xml. */
     public static class UMASingleTapType {
         public static final int DELAYED_TAP = 0;
         public static final int UNDELAYED_TAP = 1;
@@ -1488,6 +1490,7 @@ public class ContentViewCore
         }
     }
 
+    /** Callback interface for evaluateJavaScript(). */
     public interface JavaScriptCallback {
         void handleJavaScriptResult(String jsonResult);
     }
@@ -3038,7 +3041,9 @@ public class ContentViewCore
 
             return Settings.Secure.getInt(contentResolver, accessibilityScriptInjection, 0) == 1;
         } catch (NoSuchFieldException e) {
+            // Do nothing, default to false.
         } catch (IllegalAccessException e) {
+            // Do nothing, default to false.
         }
         return false;
     }
