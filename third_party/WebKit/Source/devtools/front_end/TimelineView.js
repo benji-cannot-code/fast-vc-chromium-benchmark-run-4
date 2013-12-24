29859c054d41645e735365ee08d833718fa883ee
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-24 13:37:45 +0000

    Timeline: followup fix for r164289. Replace WebInspector.Panel.* calls with WebInspector.View.*
    BUG=
    R=caseq@chromium.org
    
    Review URL: https://codereview.chromium.org/112943004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164315 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/TimelineView.js
+++ third_party/WebKit/Source/devtools/front_end/TimelineView.js
@@ -564,7 +564,7 @@ WebInspector.TimelineView.prototype = {
 
     wasShown: function()
     {
-        WebInspector.Panel.prototype.wasShown.call(this);
+        WebInspector.View.prototype.wasShown.call(this);
         if (!WebInspector.TimelinePanel._categoryStylesInitialized) {
             WebInspector.TimelinePanel._categoryStylesInitialized = true;
             this._injectCategoryStyles();
@@ -576,7 +576,7 @@ WebInspector.TimelineView.prototype = {
     willHide: function()
     {
         this._closeRecordDetails();
-        WebInspector.Panel.prototype.willHide.call(this);
+        WebInspector.View.prototype.willHide.call(this);
     },
 
     _onScroll: function(event)
@@ -1322,7 +1322,7 @@ WebInspector.TimelineView.prototype = {
         this._updateSearchHighlight(true, shouldJump);
     },
 
-    __proto__: WebInspector.Panel.prototype
+    __proto__: WebInspector.View.prototype
 }
 
 /**
