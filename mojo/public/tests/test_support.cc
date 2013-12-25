bd3d952b736e026778cb808dd4b7515c4b6fd689
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-25 07:50:56 +0000

    Mojo: Add MessagePipe to core_cpp.h for convenience
    Review URL: https://codereview.chromium.org/119563003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@242484 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/public/tests/test_support.cc
+++ mojo/public/tests/test_support.cc
@@ -11,13 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace mojo {
 namespace test {
 
-MessagePipe::MessagePipe() {
-  CreateMessagePipe(&handle_0, &handle_1);
-}
-
-MessagePipe::~MessagePipe() {
-}
-
 bool WriteTextMessage(MessagePipeHandle handle, const std::string& text) {
   MojoResult rv = WriteMessageRaw(handle,
                                   text.data(),
--- mojo/public/tests/test_support.h
+++ mojo/public/tests/test_support.h
@@ -14,16 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace mojo {
 namespace test {
 
-struct MessagePipe {
-  MessagePipe();
-  ~MessagePipe();
-
-  ScopedMessagePipeHandle handle_0;
-  ScopedMessagePipeHandle handle_1;
-
-  DISALLOW_COPY_AND_ASSIGN(MessagePipe);
-};
-
 bool WriteTextMessage(MessagePipeHandle handle, const std::string& text);
 bool ReadTextMessage(MessagePipeHandle handle, std::string* text);
 
