84a76cec44f3f5b7bdff13065935c71c379e0a65
simonhong@chromium.org <simonhong@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-26 15:08:11 +0000

    [Cleanup] Remove unnecessary including GraphicsContext.h in GraphicsLayer.cpp
    We don't need to include GraphicsContext.h in GraphicsLayer.cpp because
    no one references the api of GraphicsContext.
    
    R=esprehn@chromium.org
    BUG=NONE
    TEST=Compiles
    
    Review URL: https://codereview.chromium.org/119673003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164345 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/GraphicsLayer.cpp
+++ third_party/WebKit/Source/platform/graphics/GraphicsLayer.cpp
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "SkMatrix44.h"
 #include "platform/geometry/FloatRect.h"
 #include "platform/geometry/LayoutRect.h"
-#include "platform/graphics/GraphicsContext.h"
 #include "platform/graphics/GraphicsLayerFactory.h"
 #include "platform/graphics/filters/SkiaImageFilterBuilder.h"
 #include "platform/graphics/skia/NativeImageSkia.h"
