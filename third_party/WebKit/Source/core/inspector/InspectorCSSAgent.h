c824a85bded3147027c2b51cf872a9f29a946c07
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-26 14:35:20 +0000

    DevTools: Get rid of style sheets appender in css agent.
    Reuse collectAllStyleSheets instead.
    
    Review URL: https://codereview.chromium.org/104903004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164344 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorCSSAgent.h
+++ third_party/WebKit/Source/core/inspector/InspectorCSSAgent.h
@@ -174,9 +174,10 @@ private:
     Element* elementForId(ErrorString*, int nodeId);
     int documentNodeWithRequestedFlowsId(Document*);
     void collectAllStyleSheets(Vector<InspectorStyleSheet*>&);
-    void collectStyleSheets(CSSStyleSheet*, Vector<InspectorStyleSheet*>&);
+    void collectAllDocumentStyleSheets(Document*, Vector<CSSStyleSheet*>&);
+    void collectStyleSheets(CSSStyleSheet*, Vector<CSSStyleSheet*>&);
 
-    void updateActiveStyleSheets(Document*, const Vector<RefPtr<StyleSheet> >& newSheets);
+    void updateActiveStyleSheets(Document*, const Vector<CSSStyleSheet*>&);
 
     void collectPlatformFontsForRenderer(RenderText*, HashCountedSet<String>*);
 
