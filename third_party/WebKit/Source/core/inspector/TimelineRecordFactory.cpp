77a6f2a8aad0b43e99586495d77fada55e86532b
alph@chromium.org <alph@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-26 09:33:13 +0000

    DevTools: Replace typedef with using for TimelineEvent
    Review URL: https://codereview.chromium.org/103213014
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164337 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/TimelineRecordFactory.cpp
+++ third_party/WebKit/Source/core/inspector/TimelineRecordFactory.cpp
@@ -43,7 +43,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-PassRefPtr<TimelineRecordFactory::TimelineEvent> TimelineRecordFactory::createGenericRecord(double startTime, int maxCallStackDepth, const String& type, PassRefPtr<JSONObject> data)
+using TypeBuilder::Timeline::TimelineEvent;
+
+PassRefPtr<TimelineEvent> TimelineRecordFactory::createGenericRecord(double startTime, int maxCallStackDepth, const String& type, PassRefPtr<JSONObject> data)
 {
     ASSERT(data.get());
     RefPtr<TimelineEvent> record = TimelineEvent::create()
@@ -58,7 +60,7 @@ PassRefPtr<TimelineRecordFactory::TimelineEvent> TimelineRecordFactory::createGe
     return record.release();
 }
 
-PassRefPtr<TimelineRecordFactory::TimelineEvent> TimelineRecordFactory::createBackgroundRecord(double startTime, const String& threadName, const String& type, PassRefPtr<JSONObject> data)
+PassRefPtr<TimelineEvent> TimelineRecordFactory::createBackgroundRecord(double startTime, const String& threadName, const String& type, PassRefPtr<JSONObject> data)
 {
     ASSERT(data.get());
     RefPtr<TimelineEvent> record = TimelineEvent::create()
