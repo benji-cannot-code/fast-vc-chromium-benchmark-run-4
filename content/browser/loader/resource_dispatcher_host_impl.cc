benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit e9765108243e3e83654b4f5bf3e188c7a5800d65

Replace the GetAssociatedRenderView call with GetAssociatedRenderFrame in ResourceDispatcherHostImpl.
BUG=304341
R=nasko@chromium.org

Review URL: https://codereview.chromium.org/120623002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@242533 0039d316-1c4b-4281-b951-d872f2087c98

content/browser/loader/resource_dispatcher_host_impl.cc
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/streams/stream_context.h"
 #include "content/browser/streams/stream_registry.h"
 #include "content/browser/worker_host/worker_service_impl.h"
+#include "content/browser/web_contents/web_contents_impl.h"
 #include "content/common/resource_messages.h"
 #include "content/common/ssl_status_serialization.h"
 #include "content/common/view_messages.h"
@@ -275,27 +276,30 @@ int GetCertID(net::URLRequest* request, int child_id) {
 }
 
 void NotifyRedirectOnUI(int render_process_id,
-                        int render_view_id,
+                        int render_frame_host,
                         scoped_ptr<ResourceRedirectDetails> details) {
-  RenderViewHostImpl* host =
-      RenderViewHostImpl::FromID(render_process_id, render_view_id);
+  RenderFrameHostImpl* host =
+      RenderFrameHostImpl::FromID(render_process_id, render_frame_host);
   if (!host)
     return;
 
-  RenderViewHostDelegate* delegate = host->GetDelegate();
-  delegate->DidGetRedirectForResourceRequest(host, *details.get());
+  WebContentsImpl* web_contents =
+      static_cast<WebContentsImpl*>(WebContents::FromRenderFrameHost(host));
+  web_contents->DidGetRedirectForResourceRequest(
+      host->render_view_host(), *details.get());
 }
 
 void NotifyResponseOnUI(int render_process_id,
-                        int render_view_id,
+                        int render_frame_host,
                         scoped_ptr<ResourceRequestDetails> details) {
-  RenderViewHostImpl* host =
-      RenderViewHostImpl::FromID(render_process_id, render_view_id);
+  RenderFrameHostImpl* host =
+      RenderFrameHostImpl::FromID(render_process_id, render_frame_host);
   if (!host)
     return;
 
-  RenderViewHostDelegate* delegate = host->GetDelegate();
-  delegate->DidGetResourceResponseStart(*details.get());
+  WebContentsImpl* web_contents =
+      static_cast<WebContentsImpl*>(WebContents::FromRenderFrameHost(host));
+  web_contents->DidGetResourceResponseStart(*details.get());
 }
 
 }  // namespace
@@ -698,8 +702,8 @@ void ResourceDispatcherHostImpl::DidReceiveRedirect(ResourceLoader* loader,
                                                     const GURL& new_url) {
   ResourceRequestInfoImpl* info = loader->GetRequestInfo();
 
-  int render_process_id, render_view_id;
-  if (!info->GetAssociatedRenderView(&render_process_id, &render_view_id))
+  int render_process_id, render_frame_host;
+  if (!info->GetAssociatedRenderFrame(&render_process_id, &render_frame_host))
     return;
 
   // Notify the observers on the UI thread.
@@ -711,7 +715,7 @@ void ResourceDispatcherHostImpl::DidReceiveRedirect(ResourceLoader* loader,
       BrowserThread::UI, FROM_HERE,
       base::Bind(
           &NotifyRedirectOnUI,
-          render_process_id, render_view_id, base::Passed(&detail)));
+          render_process_id, render_frame_host, base::Passed(&detail)));
 }
 
 void ResourceDispatcherHostImpl::DidReceiveResponse(ResourceLoader* loader) {
@@ -732,8 +736,8 @@ void ResourceDispatcherHostImpl::DidReceiveResponse(ResourceLoader* loader) {
            info->detachable_handler()->is_detached());
   }
 
-  int render_process_id, render_view_id;
-  if (!info->GetAssociatedRenderView(&render_process_id, &render_view_id))
+  int render_process_id, render_frame_host;
+  if (!info->GetAssociatedRenderFrame(&render_process_id, &render_frame_host))
     return;
 
   // Notify the observers on the UI thread.
@@ -744,7 +748,7 @@ void ResourceDispatcherHostImpl::DidReceiveResponse(ResourceLoader* loader) {
       BrowserThread::UI, FROM_HERE,
       base::Bind(
           &NotifyResponseOnUI,
-          render_process_id, render_view_id, base::Passed(&detail)));
+          render_process_id, render_frame_host, base::Passed(&detail)));
 }
 
 void ResourceDispatcherHostImpl::DidFinishLoading(ResourceLoader* loader) {

content/browser/renderer_host/render_view_host_delegate.h
@@ -62,8 +62,6 @@ struct GlobalRequestID;
 struct NativeWebKeyboardEvent;
 struct Referrer;
 struct RendererPreferences;
-struct ResourceRedirectDetails;
-struct ResourceRequestDetails;
 
 //
 // RenderViewHostDelegate
@@ -167,15 +165,6 @@ class CONTENT_EXPORT RenderViewHostDelegate {
   virtual void DidFailProvisionalLoadWithError(
       RenderViewHost* render_view_host,
       const ViewHostMsg_DidFailProvisionalLoadWithError_Params& params) {}
-
-  // A response has been received for a resource request.
-  virtual void DidGetResourceResponseStart(
-      const ResourceRequestDetails& details) {}
-
-  // A redirect was received while requesting a resource.
-  virtual void DidGetRedirectForResourceRequest(
-      RenderViewHost* render_view_host,
-      const ResourceRedirectDetails& details) {}
 
   // The RenderView was navigated to a different page.
   virtual void DidNavigate(RenderViewHost* render_view_host,

content/browser/web_contents/web_contents_impl.cc
@@ -1965,6 +1965,37 @@ void WebContentsImpl::DragSourceMovedTo(int client_x, int client_y,
                                                screen_x, screen_y);
 }
 
+void WebContentsImpl::DidGetResourceResponseStart(
+  const ResourceRequestDetails& details) {
+  controller_.ssl_manager()->DidStartResourceResponse(details);
+
+  FOR_EACH_OBSERVER(WebContentsObserver, observers_,
+                    DidGetResourceResponseStart(details));
+
+  // TODO(avi): Remove. http://crbug.com/170921
+  NotificationService::current()->Notify(
+      NOTIFICATION_RESOURCE_RESPONSE_STARTED,
+      Source<WebContents>(this),
+      Details<const ResourceRequestDetails>(&details));
+}
+
+void WebContentsImpl::DidGetRedirectForResourceRequest(
+  RenderViewHost* render_view_host,
+  const ResourceRedirectDetails& details) {
+  controller_.ssl_manager()->DidReceiveResourceRedirect(details);
+
+  FOR_EACH_OBSERVER(
+      WebContentsObserver,
+      observers_,
+      DidGetRedirectForResourceRequest(render_view_host, details));
+
+  // TODO(avi): Remove. http://crbug.com/170921
+  NotificationService::current()->Notify(
+      NOTIFICATION_RESOURCE_RECEIVED_REDIRECT,
+      Source<WebContents>(this),
+      Details<const ResourceRedirectDetails>(&details));
+}
+
 void WebContentsImpl::SystemDragEnded() {
   if (GetRenderViewHost())
     GetRenderViewHostImpl()->DragSourceSystemDragEnded();
@@ -2934,37 +2965,6 @@ void WebContentsImpl::RenderViewDeleted(RenderViewHost* rvh) {
   ClearPowerSaveBlockers(rvh);
   GetRenderManager()->RenderViewDeleted(rvh);
   FOR_EACH_OBSERVER(WebContentsObserver, observers_, RenderViewDeleted(rvh));
-}
-
-void WebContentsImpl::DidGetResourceResponseStart(
-  const ResourceRequestDetails& details) {
-  controller_.ssl_manager()->DidStartResourceResponse(details);
-
-  FOR_EACH_OBSERVER(WebContentsObserver, observers_,
-                    DidGetResourceResponseStart(details));
-
-  // TODO(avi): Remove. http://crbug.com/170921
-  NotificationService::current()->Notify(
-      NOTIFICATION_RESOURCE_RESPONSE_STARTED,
-      Source<WebContents>(this),
-      Details<const ResourceRequestDetails>(&details));
-}
-
-void WebContentsImpl::DidGetRedirectForResourceRequest(
-  RenderViewHost* render_view_host,
-  const ResourceRedirectDetails& details) {
-  controller_.ssl_manager()->DidReceiveResourceRedirect(details);
-
-  FOR_EACH_OBSERVER(
-      WebContentsObserver,
-      observers_,
-      DidGetRedirectForResourceRequest(render_view_host, details));
-
-  // TODO(avi): Remove. http://crbug.com/170921
-  NotificationService::current()->Notify(
-      NOTIFICATION_RESOURCE_RECEIVED_REDIRECT,
-      Source<WebContents>(this),
-      Details<const ResourceRedirectDetails>(&details));
 }
 
 void WebContentsImpl::DidNavigate(

content/browser/web_contents/web_contents_impl.h
@@ -69,6 +69,8 @@ class WebContentsViewDelegate;
 struct ColorSuggestion;
 struct FaviconURL;
 struct LoadNotificationDetails;
+struct ResourceRedirectDetails;
+struct ResourceRequestDetails;
 
 // Factory function for the implementations that content knows about. Takes
 // ownership of |delegate|.
@@ -166,6 +168,15 @@ class CONTENT_EXPORT WebContentsImpl
   // a Drag Source Move.
   void DragSourceMovedTo(int client_x, int client_y,
                          int screen_x, int screen_y);
+
+  // A response has been received for a resource request.
+  void DidGetResourceResponseStart(
+      const ResourceRequestDetails& details);
+
+  // A redirect was received while requesting a resource.
+  void DidGetRedirectForResourceRequest(
+      RenderViewHost* render_view_host,
+      const ResourceRedirectDetails& details);
 
   // WebContents ------------------------------------------------------
   virtual WebContentsDelegate* GetDelegate() OVERRIDE;
@@ -319,11 +330,6 @@ class CONTENT_EXPORT WebContentsImpl
       RenderViewHost* render_view_host,
       const ViewHostMsg_DidFailProvisionalLoadWithError_Params& params)
           OVERRIDE;
-  virtual void DidGetResourceResponseStart(
-      const ResourceRequestDetails& details) OVERRIDE;
-  virtual void DidGetRedirectForResourceRequest(
-      RenderViewHost* render_view_host,
-      const ResourceRedirectDetails& details) OVERRIDE;
   virtual void DidNavigate(
       RenderViewHost* render_view_host,
       const ViewHostMsg_FrameNavigate_Params& params) OVERRIDE;

commit 690f7ab417bbee7123d8ce84306daa52b012dc15

Fix chromium.content.browser.InterstitialPageTest#testCloseInterstitial on android bots. This was a regression from r242533.
BUG=304341
R=thakis@chromium.org

Review URL: https://codereview.chromium.org/110443011

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@242543 0039d316-1c4b-4281-b951-d872f2087c98

content/browser/loader/resource_dispatcher_host_impl.cc
@@ -285,6 +285,8 @@ void NotifyRedirectOnUI(int render_process_id,
 
   WebContentsImpl* web_contents =
       static_cast<WebContentsImpl*>(WebContents::FromRenderFrameHost(host));
+  if (!web_contents)
+    return;
   web_contents->DidGetRedirectForResourceRequest(
       host->render_view_host(), *details.get());
 }
@@ -299,6 +301,8 @@ void NotifyResponseOnUI(int render_process_id,
 
   WebContentsImpl* web_contents =
       static_cast<WebContentsImpl*>(WebContents::FromRenderFrameHost(host));
+  if (!web_contents)
+    return;
   web_contents->DidGetResourceResponseStart(*details.get());
 }
 