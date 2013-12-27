0fd8c7aeb28960a7e29d2403cba427b7ccad5edd
jbroman@chromium.org <jbroman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-27 19:02:34 +0000

    Move remaining KeyCode files to platform/
    This is a reland of r164107 (which was reverted due to an Android build error).
    https://codereview.chromium.org/111403004
    
    There was a missing #include in KeyCodeConversionAndroid.cpp which prevented a function from being exported from the platform component.
    
    BUG=297477,329743
    
    Review URL: https://codereview.chromium.org/106103009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164363 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/KeyCodeConversionAndroid.cpp
+++ third_party/WebKit/Source/platform/KeyCodeConversionAndroid.cpp
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
+#include "platform/KeyCodeConversion.h"
+
 #include "platform/KeyboardCodes.h"
 
 #include <android/keycodes.h>
--- third_party/WebKit/Source/web/WebInputEventFactoryAndroid.cpp
+++ third_party/WebKit/Source/web/WebInputEventFactoryAndroid.cpp
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebInputEventFactory.h"
 
 #include "WebInputEvent.h"
-#include "core/platform/chromium/KeyCodeConversion.h"
+#include "platform/KeyCodeConversion.h"
 #include "platform/KeyboardCodes.h"
 #include "wtf/Assertions.h"
 
