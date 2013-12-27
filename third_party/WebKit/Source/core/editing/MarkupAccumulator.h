0c105a7d32f2188f80efdee2e6ad1c440ab946f4
costan@gmail.com <costan@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-27 06:27:38 +0000

    Make MarkupAccumulator non-copyable.
    BUG=
    
    Review URL: https://codereview.chromium.org/116283003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164353 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/MarkupAccumulator.h
+++ third_party/WebKit/Source/core/editing/MarkupAccumulator.h
@@ -64,8 +64,9 @@ struct EntityDescription {
     EntityMask mask;
 };
 
-// FIXME: Noncopyable?
 class MarkupAccumulator {
+    WTF_MAKE_NONCOPYABLE(MarkupAccumulator);
+
 public:
     MarkupAccumulator(Vector<Node*>*, EAbsoluteURLs, const Range* = 0);
     virtual ~MarkupAccumulator();
