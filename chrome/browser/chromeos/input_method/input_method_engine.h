eb347c7c06daaeee9cceef1019c894f146f0891e
komatsu@chromium.org <komatsu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-27 10:08:39 +0000

    Delete redundant functions for auxiliary text.
    This patch unifies:
    * IBusPanelCandidateWindowHandlerInterface::UpdateAuxiliaryText into UpdateLookupTable
    * InputMethodEngineInterface::SetCandidateWindowAuxText into SetCandidateWindowProperty
    * InputMethodEngineInterface::SetCandidateWindowAuxTextVisible into SetCandidateWindowProperty
    
    
    BUG=N/A
    
    Review URL: https://codereview.chromium.org/111333004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@242610 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/input_method/input_method_engine.h
+++ chrome/browser/chromeos/input_method/input_method_engine.h
@@ -62,8 +62,6 @@ class InputMethodEngine : public InputMethodEngineInterface {
       const CandidateWindowProperty& property) OVERRIDE;
   virtual bool SetCandidateWindowVisible(bool visible,
                                          std::string* error) OVERRIDE;
-  virtual void SetCandidateWindowAuxText(const char* text) OVERRIDE;
-  virtual void SetCandidateWindowAuxTextVisible(bool visible) OVERRIDE;
   virtual bool SetCandidates(int context_id,
                              const std::vector<Candidate>& candidates,
                              std::string* error) OVERRIDE;
@@ -116,10 +114,6 @@ class InputMethodEngine : public InputMethodEngineInterface {
 
   // This IME ID in ibus.
   std::string ibus_id_;
-
-  // The current auxialy text and it's visiblity.
-  std::string aux_text_;
-  bool aux_text_visible_;
 
   // Pointer to the object recieving events for this IME.
   InputMethodEngineInterface::Observer* observer_;
