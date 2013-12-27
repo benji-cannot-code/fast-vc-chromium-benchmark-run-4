b2a04cf93750c6433116fba1ababeb0f03f9604f
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-27 00:13:43 +0000

    Add AshDesktopMediaList and enable Desktop Capture API on Chrome OS.
    The new AshDesktopMediaList gets list of windows/screens for the window
    picker dialog which allows to enable Desktop Capture API on Chrome OS.
    
    BUG=295102
    R=ben@chromium.org, hshi@chromium.org
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=242361
    
    Review URL: https://codereview.chromium.org/100833002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@242578 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/snapshot/snapshot_android.cc
+++ ui/snapshot/snapshot_android.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/snapshot/snapshot.h"
 
+#include "base/callback.h"
 #include "ui/base/android/view_android.h"
 #include "ui/base/android/window_android.h"
 #include "ui/gfx/display.h"
@@ -31,6 +32,15 @@ bool GrabWindowSnapshot(gfx::NativeWindow window,
   return window->GrabSnapshot(
       scaled_bounds.x(), scaled_bounds.y(), scaled_bounds.width(),
       scaled_bounds.height(), png_representation);
+}
+
+SNAPSHOT_EXPORT void GrapWindowSnapshotAsync(
+    gfx::NativeWindow window,
+    const gfx::Rect& snapshot_bounds,
+    const gfx::Size& target_size,
+    scoped_refptr<base::TaskRunner> background_task_runner,
+    GrapWindowSnapshotAsyncCallback callback) {
+  NOTIMPLEMENTED();
 }
 
 }  // namespace ui
--- ui/snapshot/snapshot_gtk.cc
+++ ui/snapshot/snapshot_gtk.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <gdk/gdkx.h>
 #include <gtk/gtk.h>
 
+#include "base/callback.h"
 #include "base/logging.h"
 #include "ui/base/x/x11_util.h"
 #include "ui/gfx/rect.h"
@@ -81,6 +82,15 @@ bool GrabWindowSnapshot(gfx::NativeWindow window_handle,
                         const gfx::Rect& snapshot_bounds) {
   return GrabViewSnapshot(GTK_WIDGET(window_handle), png_representation,
       snapshot_bounds);
+}
+
+SNAPSHOT_EXPORT void GrapWindowSnapshotAsync(
+    gfx::NativeWindow window,
+    const gfx::Rect& snapshot_bounds,
+    const gfx::Size& target_size,
+    scoped_refptr<base::TaskRunner> background_task_runner,
+    GrapWindowSnapshotAsyncCallback callback) {
+  NOTIMPLEMENTED();
 }
 
 }  // namespace ui
