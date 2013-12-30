57411d3369fc903793522a3cb602cd404851f1ab
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-30 23:43:41 +0000

    Use drawBidiText for RenderImage Alt Text
    Change RenderImage::paintReplaced to use context::drawBidiText instead
    of context::drawText. This negates the need for the changes in r163930
    to RenderBlockFlow::constructTextRunInternal and thus reverts that
    change. Calling drawBidiText instead ensures that the directionality is
    respected through the string.
    
    BUG=326921
    TEST=fast/text/bidi-img-alt-text.html
    R=eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/93713003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164388 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderBlockFlow.cpp
+++ third_party/WebKit/Source/core/rendering/RenderBlockFlow.cpp
@@ -43,8 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/rendering/RenderView.h"
 #include "core/rendering/line/LineWidth.h"
 #include "core/rendering/svg/SVGTextRunRenderingContext.h"
-#include "platform/text/BidiResolver.h"
-#include "platform/text/TextRunIterator.h"
 
 using namespace std;
 
@@ -2508,16 +2506,6 @@ static inline TextRun constructTextRunInternal(RenderObject* context, const Font
     }
 
     TextRun run(characters, length, 0, 0, expansion, textDirection, directionalOverride);
-    if (!directionalOverride) {
-        BidiResolver<TextRunIterator, BidiCharacterRun> bidiResolver;
-        bidiResolver.setStatus(BidiStatus(run.direction(), run.directionalOverride()));
-        bidiResolver.setPositionIgnoringNestedIsolates(TextRunIterator(&run, 0));
-        bool hasStrongDirectionality;
-        TextDirection direction = bidiResolver.determineParagraphDirectionality(&hasStrongDirectionality);
-        if (hasStrongDirectionality)
-            run.setDirection(direction);
-    }
-
     if (textRunNeedsRenderingContext(font))
         run.setRenderingContext(SVGTextRunRenderingContext::create(context));
 
