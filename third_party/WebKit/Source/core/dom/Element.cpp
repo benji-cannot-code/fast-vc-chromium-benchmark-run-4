fb40efeccb419e5b97d0e097c7b846a0f223d416
ykyyip@chromium.org <ykyyip@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-30 19:02:53 +0000

    Don't force recalc if there are no shadow boundary crossing rules
    Review URL: https://codereview.chromium.org/119693002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164382 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Element.cpp
+++ third_party/WebKit/Source/core/dom/Element.cpp
@@ -1622,7 +1622,7 @@ StyleRecalcChange Element::recalcOwnStyle(StyleRecalcChange change)
     }
 
     if (styleChangeType() >= SubtreeStyleChange)
-        return Force;
+        return document().ensureStyleResolver().treeBoundaryCrossingRules().isEmpty() ? Inherit : Force;
 
     return max(localChange, change);
 }
