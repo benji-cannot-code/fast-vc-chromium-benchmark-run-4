af69bb62df32964f02e3a657672e3e32fa18a930
rbyers@chromium.org <rbyers@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-31 15:42:12 +0000

    Move CSSTouchAction feature to experimental
    touch-action support is now sufficiently far along to enable polyfills (such as
    in polymer) to take advantage of it.  Make it available behind the
    --enable-experimental-web-platform-features flag.
    
    Also split out touch-action-delay support into it's own RuntimeEnabledFeature
    and leave it in the 'test' state.  I'm planning on shipping touch-action
    (crbug.com/241964) in advance of touch-action-delay (crbug.com/329559).
    
    BUG=241964
    
    Review URL: https://codereview.chromium.org/108673003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164399 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/parser/BisonCSSParser-in.cpp
+++ third_party/WebKit/Source/core/css/parser/BisonCSSParser-in.cpp
@@ -624,7 +624,7 @@ static inline bool isValidKeywordPropertyAndValue(CSSPropertyID propertyId, int 
             return true;
         break;
     case CSSPropertyTouchActionDelay: // none | script
-        if (RuntimeEnabledFeatures::cssTouchActionEnabled() && (valueID == CSSValueScript || valueID == CSSValueNone))
+        if (RuntimeEnabledFeatures::cssTouchActionDelayEnabled() && (valueID == CSSValueScript || valueID == CSSValueNone))
             return true;
         break;
     case CSSPropertyVisibility: // visible | hidden | collapse | inherit
