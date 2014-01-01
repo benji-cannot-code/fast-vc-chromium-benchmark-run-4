c3a7e4697326706144b2b148222f7d61abc866dc
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-01 02:35:39 +0000

    Remove special casing for MutationObserver from V8GCController
    This CL moves the logic from V8GCController to V8MutationObserverCustom.cpp
    
    Review URL: https://codereview.chromium.org/116443010
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164406 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/custom/V8MutationObserverCustom.cpp
+++ third_party/WebKit/Source/bindings/v8/custom/V8MutationObserverCustom.cpp
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/v8/ExceptionState.h"
 #include "bindings/v8/V8Binding.h"
 #include "bindings/v8/V8DOMWrapper.h"
+#include "bindings/v8/V8GCController.h"
 #include "bindings/v8/V8MutationCallback.h"
 #include "bindings/v8/V8Utilities.h"
 #include "core/dom/MutationObserver.h"
@@ -66,6 +67,16 @@ void V8MutationObserver::constructorCustom(const v8::FunctionCallbackInfo<v8::Va
 
     V8DOMWrapper::associateObjectWithWrapper<V8MutationObserver>(observer.release(), &wrapperTypeInfo, wrapper, info.GetIsolate(), WrapperConfiguration::Dependent);
     info.GetReturnValue().Set(wrapper);
+}
+
+void V8MutationObserver::visitDOMWrapper(void* object, const v8::Persistent<v8::Object>& wrapper, v8::Isolate* isolate)
+{
+    MutationObserver* observer = static_cast<MutationObserver*>(object);
+    HashSet<Node*> observedNodes = observer->getObservedNodes();
+    for (HashSet<Node*>::iterator it = observedNodes.begin(); it != observedNodes.end(); ++it) {
+        v8::UniqueId id(reinterpret_cast<intptr_t>(V8GCController::opaqueRootForGC(*it, isolate)));
+        isolate->SetReferenceFromGroup(id, wrapper);
+    }
 }
 
 } // namespace WebCore
