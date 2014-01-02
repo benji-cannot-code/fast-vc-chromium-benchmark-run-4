afb046a81fae0e255dd0f4b9bd57897077d1f55d
senorblanco@chromium.org <senorblanco@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-02 20:31:00 +0000

    Add SourceAlpha support to accelerated CSS filter effects (aka Skia SkImageFilter DAG) path.
    BUG=331362
    
    Review URL: https://codereview.chromium.org/123293002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164424 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/filters/SourceAlpha.cpp
+++ third_party/WebKit/Source/platform/graphics/filters/SourceAlpha.cpp
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/graphics/GraphicsContext.h"
 #include "platform/graphics/filters/Filter.h"
 #include "platform/text/TextStream.h"
+#include "third_party/skia/include/effects/SkColorFilterImageFilter.h"
+#include "third_party/skia/include/effects/SkColorMatrixFilter.h"
 #include "wtf/StdLibExtras.h"
 #include "wtf/text/WTFString.h"
 
@@ -63,6 +65,18 @@ void SourceAlpha::applySoftware()
     GraphicsContext* filterContext = resultImage->context();
     filterContext->fillRect(imageRect, Color::black);
     filterContext->drawImageBuffer(filter->sourceImage(), IntPoint(), CompositeDestinationIn);
+}
+
+PassRefPtr<SkImageFilter> SourceAlpha::createImageFilter(SkiaImageFilterBuilder* builder)
+{
+    SkScalar matrix[20] = {
+        0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0,
+        0, 0, 0, SK_Scalar1, 0
+    };
+    RefPtr<SkColorFilter> colorFilter(adoptRef(new SkColorMatrixFilter(matrix)));
+    return adoptRef(SkColorFilterImageFilter::Create(colorFilter.get()));
 }
 
 TextStream& SourceAlpha::externalRepresentation(TextStream& ts, int indent) const
