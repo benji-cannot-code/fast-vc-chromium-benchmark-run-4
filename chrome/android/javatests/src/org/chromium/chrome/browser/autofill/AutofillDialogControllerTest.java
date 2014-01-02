f2dade094c5e3f5419883d5680e9c820b96376dc
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-02 23:48:06 +0000

    rAc: bring Android and Desktop into better alignment re: security
    1) don't allow the dialog on insecure sites just because it's not asking for CC info (other PII is security-sensitive as well)
    2) manage security-based rejection in the same place (should make the override flag work on Android)
    
    BUG=272512
    
    Review URL: https://codereview.chromium.org/120813003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@242843 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/android/javatests/src/org/chromium/chrome/browser/autofill/AutofillDialogControllerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/autofill/AutofillDialogControllerTest.java
@@ -152,7 +152,6 @@ public class AutofillDialogControllerTest extends ChromiumTestShellTestBase {
         clearAppData();
         CommandLine.init(new String[]{});
         CommandLine.getInstance().appendSwitch(SWITCH_REDUCE_SECURITY_FOR_TESTING);
-        AutofillDialogControllerAndroid.allowInsecureDialogsForTesting();
     }
 
     @MediumTest
