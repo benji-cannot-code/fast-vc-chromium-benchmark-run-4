8e59d4de4f127430e070ef20bd9aaed3a20f4c83
reed@google.com <reed@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-02 22:43:55 +0000

    remove SK_SUPPORT_LEGACY_PIXELREF_CONSTRUCTOR now that call-sites have been updated
    BUG=
    
    Review URL: https://codereview.chromium.org/123233002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@242819 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/ext/lazy_pixel_ref.cc
+++ skia/ext/lazy_pixel_ref.cc
@@ -7,12 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace skia {
 
-#ifdef SK_SUPPORT_LEGACY_PIXELREF_CONSTRUCTOR
-// DEPRECATED -- will remove after blink updates to pass info
-LazyPixelRef::LazyPixelRef() {
-}
-#endif
-
 LazyPixelRef::LazyPixelRef(const SkImageInfo& info) : SkPixelRef(info) {
 }
 
