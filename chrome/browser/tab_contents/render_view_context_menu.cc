1584267d8fa5da4d37dd52adfe5a16c880153b32
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-02 21:42:42 +0000

    Remove kChromeFrame checks in the browser process.
    R=grt@chromium.org
    
    Review URL: https://codereview.chromium.org/122383003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@242804 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/render_view_context_menu.cc
+++ chrome/browser/tab_contents/render_view_context_menu.cc
@@ -404,7 +404,6 @@ RenderViewContextMenu::RenderViewContextMenu(
       menu_model_(this),
       extension_items_(profile_, this, &menu_model_,
                     base::Bind(MenuItemMatchesParams, params_)),
-      external_(false),
       speech_input_submenu_model_(this),
       protocol_handler_submenu_model_(this),
       protocol_handler_registry_(
@@ -847,10 +846,8 @@ void RenderViewContextMenu::AppendLinkItems() {
       AppendProtocolHandlerSubMenu();
     }
 
-    if (!external_) {
-      menu_model_.AddItemWithStringId(IDC_CONTENT_CONTEXT_OPENLINKOFFTHERECORD,
-                                      IDS_CONTENT_CONTEXT_OPENLINKOFFTHERECORD);
-    }
+    menu_model_.AddItemWithStringId(IDC_CONTENT_CONTEXT_OPENLINKOFFTHERECORD,
+                                    IDS_CONTENT_CONTEXT_OPENLINKOFFTHERECORD);
     menu_model_.AddItemWithStringId(IDC_CONTENT_CONTEXT_SAVELINKAS,
                                     IDS_CONTENT_CONTEXT_SAVELINKAS);
   }
@@ -969,13 +966,8 @@ void RenderViewContextMenu::AppendPageItems() {
 
   menu_model_.AddItemWithStringId(IDC_VIEW_SOURCE,
                                   IDS_CONTENT_CONTEXT_VIEWPAGESOURCE);
-  // Only add View Page Info if there's a browser.  This is a temporary thing
-  // while View Page Info crashes Chrome Frame; see http://crbug.com/120901.
-  // TODO(grt) Remove this once page info is back for Chrome Frame.
-  if (!external_) {
-    menu_model_.AddItemWithStringId(IDC_CONTENT_CONTEXT_VIEWPAGEINFO,
-                                    IDS_CONTENT_CONTEXT_VIEWPAGEINFO);
-  }
+  menu_model_.AddItemWithStringId(IDC_CONTENT_CONTEXT_VIEWPAGEINFO,
+                                  IDS_CONTENT_CONTEXT_VIEWPAGEINFO);
 }
 
 void RenderViewContextMenu::AppendFrameItems() {
@@ -987,13 +979,8 @@ void RenderViewContextMenu::AppendFrameItems() {
   //   IDS_CONTENT_CONTEXT_PRINTFRAME
   menu_model_.AddItemWithStringId(IDC_CONTENT_CONTEXT_VIEWFRAMESOURCE,
                                   IDS_CONTENT_CONTEXT_VIEWFRAMESOURCE);
-  // Only add View Frame Info if there's a browser.  This is a temporary thing
-  // while View Frame Info crashes Chrome Frame; see http://crbug.com/120901.
-  // TODO(grt) Remove this once frame info is back for Chrome Frame.
-  if (!external_) {
-    menu_model_.AddItemWithStringId(IDC_CONTENT_CONTEXT_VIEWFRAMEINFO,
-                                    IDS_CONTENT_CONTEXT_VIEWFRAMEINFO);
-  }
+  menu_model_.AddItemWithStringId(IDC_CONTENT_CONTEXT_VIEWFRAMEINFO,
+                                  IDS_CONTENT_CONTEXT_VIEWFRAMEINFO);
 }
 
 void RenderViewContextMenu::AppendCopyItem() {
@@ -1734,9 +1721,7 @@ void RenderViewContextMenu::ExecuteCommand(int id, int event_flags) {
       break;
 
     case IDC_RELOAD:
-      // Prevent the modal "Resubmit form post" dialog from appearing in the
-      // context of an external context menu.
-      source_web_contents_->GetController().Reload(!external_);
+      source_web_contents_->GetController().Reload(true);
       break;
 
     case IDC_CONTENT_CONTEXT_RELOAD_PACKAGED_APP: {
--- chrome/browser/ui/views/tab_contents/render_view_context_menu_win.cc
+++ chrome/browser/ui/views/tab_contents/render_view_context_menu_win.cc
@@ -77,7 +77,3 @@ void RenderViewContextMenuWin::ExecuteCommand(int command_id,
   }
   RenderViewContextMenu::ExecuteCommand(command_id, event_flags);
 }
-
-void RenderViewContextMenuWin::SetExternal() {
-  external_ = true;
-}
