fba3b1c6422c577ade4632dbed8700ab25eff1a0
jdduke@chromium.org <jdduke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-03 22:14:40 +0000

    [Android] Fix lint issues with ScaleGestureDetector.setQuickScaleEnabled
    Usage of ScaleGestureDetector.setQuickScaleEnabled is guarded by an API build
    check, but the linter still complains.  Annotate the usage to suppress the
    lint error.
    
    BUG=327768
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/123293004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@242938 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/ZoomManager.java
+++ content/public/android/java/src/org/chromium/content/browser/ZoomManager.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.content.browser;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
 import android.util.Log;
@@ -107,11 +108,11 @@ class ZoomManager {
         mContentViewCore = contentViewCore;
         mMultiTouchListener = new ScaleGestureListener();
         mMultiTouchDetector = new ScaleGestureDetector(context, mMultiTouchListener);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            // TODO(jdduke): Enable this and remove the custom double-tap drag
-            // zoom code from ContentViewGestureHandler, crbug/331092.
-            mMultiTouchDetector.setQuickScaleEnabled(false);
-        }
+
+        // ScaleGestureDetector's "QuickScale" feature was introduced in KitKat.
+        // As ContentViewGestureHandler already implements this feature,
+        // explicitly disable it to prevent double-handling of the gesture.
+        disableQuickScale(mMultiTouchDetector);
     }
 
     boolean isScaleGestureDetectionInProgress() {
@@ -158,4 +159,10 @@ class ZoomManager {
     void updateMultiTouchSupport(boolean supportsMultiTouchZoom) {
         mMultiTouchListener.setPermanentlyIgnoreDetectorEvents(!supportsMultiTouchZoom);
     }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    private static void disableQuickScale(ScaleGestureDetector scaleGestureDetector) {
+       if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) return;
+       scaleGestureDetector.setQuickScaleEnabled(false);
+     }
 }
