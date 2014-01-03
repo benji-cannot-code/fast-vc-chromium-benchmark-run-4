502ff47697ffa1649d6fee59289064b6075f041c
erikchen@chromium.org <erikchen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-03 19:43:35 +0000

    mac: Fix a history swiper bug where the shield wouldn't disappear.
    There were 3 separate problems.
    1. Retain bug :(
    2. historyOverlay was removing the view from the superview during dealloc,
    which is atypical. If the view didn't exist, historyOverlay was actually
    creating the view, and sending a callback to the delegate.
    3. We were not specifying an NSAnimationContext grouping/duration, which
    non-deterministically would use a 0-duration animation, which actually
    prevented the animation and callback from triggering. As a result, the
    shield would never disappear.
    
    On a side note: unit_tests did not work when run with
    --gtest-filter=HistoryOverlay* because we did not specify an NSAnimationContext
    with a non-zero duration. 0-duration animation contexts cause animations to not
    fire, which meant that the delegate callback never went through.
    
    BUG=330312
    TEST=manual
    
    Review URL: https://codereview.chromium.org/107343008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@242911 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/chrome_render_widget_host_view_mac_history_swiper.mm
+++ chrome/browser/renderer_host/chrome_render_widget_host_view_mac_history_swiper.mm
@@ -247,7 +247,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                      ? kHistoryOverlayModeForward
                      : kHistoryOverlayModeBack];
   [historyOverlay showPanelForView:[delegate_ viewThatWantsHistoryOverlay]];
-  historyOverlay_ = [historyOverlay retain];
+  historyOverlay_ = historyOverlay;
 
   // Record whether the user was swiping forwards or backwards.
   historySwipeDirection_ = direction;
