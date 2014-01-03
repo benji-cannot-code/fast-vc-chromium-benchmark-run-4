15b8cdd27a88a0beb923b20c61f2fce4ada30c5a
jochen@chromium.org <jochen@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-03 14:19:10 +0000

    Remove usage of deprecated Object::New and Integer::New calls from tests
    BUG=none
    TBR=dcarney@chromium.org
    
    Review URL: https://codereview.chromium.org/93603007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164450 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/ScriptPromiseResolverTest.cpp
+++ third_party/WebKit/Source/bindings/v8/ScriptPromiseResolverTest.cpp
@@ -109,7 +109,7 @@ TEST_F(ScriptPromiseResolverTest, resolve)
     EXPECT_EQ(V8PromiseCustom::Pending, state());
     EXPECT_TRUE(result()->IsUndefined());
 
-    m_resolver->resolve(ScriptValue(v8::Integer::New(3, m_isolate), m_isolate));
+    m_resolver->resolve(ScriptValue(v8::Integer::New(m_isolate, 3), m_isolate));
 
     EXPECT_FALSE(m_resolver->isPending());
     EXPECT_EQ(V8PromiseCustom::Fulfilled, state());
@@ -123,7 +123,7 @@ TEST_F(ScriptPromiseResolverTest, reject)
     EXPECT_EQ(V8PromiseCustom::Pending, state());
     EXPECT_TRUE(result()->IsUndefined());
 
-    m_resolver->reject(ScriptValue(v8::Integer::New(3, m_isolate), m_isolate));
+    m_resolver->reject(ScriptValue(v8::Integer::New(m_isolate, 3), m_isolate));
 
     EXPECT_FALSE(m_resolver->isPending());
     EXPECT_EQ(V8PromiseCustom::Rejected, state());
@@ -137,14 +137,14 @@ TEST_F(ScriptPromiseResolverTest, resolveOverResolve)
     EXPECT_EQ(V8PromiseCustom::Pending, state());
     EXPECT_TRUE(result()->IsUndefined());
 
-    m_resolver->resolve(ScriptValue(v8::Integer::New(3, m_isolate), m_isolate));
+    m_resolver->resolve(ScriptValue(v8::Integer::New(m_isolate, 3), m_isolate));
 
     EXPECT_FALSE(m_resolver->isPending());
     EXPECT_EQ(V8PromiseCustom::Fulfilled, state());
     ASSERT_TRUE(result()->IsNumber());
     EXPECT_EQ(3, result().As<v8::Integer>()->Value());
 
-    m_resolver->resolve(ScriptValue(v8::Integer::New(4, m_isolate), m_isolate));
+    m_resolver->resolve(ScriptValue(v8::Integer::New(m_isolate, 4), m_isolate));
     EXPECT_FALSE(m_resolver->isPending());
     EXPECT_EQ(V8PromiseCustom::Fulfilled, state());
     ASSERT_TRUE(result()->IsNumber());
@@ -157,14 +157,14 @@ TEST_F(ScriptPromiseResolverTest, rejectOverResolve)
     EXPECT_EQ(V8PromiseCustom::Pending, state());
     EXPECT_TRUE(result()->IsUndefined());
 
-    m_resolver->resolve(ScriptValue(v8::Integer::New(3, m_isolate), m_isolate));
+    m_resolver->resolve(ScriptValue(v8::Integer::New(m_isolate, 3), m_isolate));
 
     EXPECT_FALSE(m_resolver->isPending());
     EXPECT_EQ(V8PromiseCustom::Fulfilled, state());
     ASSERT_TRUE(result()->IsNumber());
     EXPECT_EQ(3, result().As<v8::Integer>()->Value());
 
-    m_resolver->reject(ScriptValue(v8::Integer::New(4, m_isolate), m_isolate));
+    m_resolver->reject(ScriptValue(v8::Integer::New(m_isolate, 4), m_isolate));
     EXPECT_FALSE(m_resolver->isPending());
     EXPECT_EQ(V8PromiseCustom::Fulfilled, state());
     ASSERT_TRUE(result()->IsNumber());
