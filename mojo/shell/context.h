85a644bdcb499a6e513e621e64e751f81b0df972
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-03 22:50:15 +0000

    Add test for ServiceManager
    I also separated out the dynamic service loader from the ServiceManager file, to remove the dependency of the ServiceManager on the shell's context. This made testing cleaner.
    
    BUG=None
    R=viettrungluu@chromium.org, viettrungluu
    
    Review URL: https://codereview.chromium.org/122173004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@242952 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/shell/context.h
+++ mojo/shell/context.h
@@ -19,6 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace mojo {
 namespace shell {
 
+class DynamicServiceLoader;
+
 class Context {
  public:
   Context();
@@ -27,7 +29,7 @@ class Context {
   TaskRunners* task_runners() { return &task_runners_; }
   Storage* storage() { return &storage_; }
   Loader* loader() { return &loader_; }
-  ServiceManager* service_manager() { return service_manager_.get(); }
+  ServiceManager* service_manager() { return &service_manager_; }
 
 #if defined(OS_ANDROID)
   jobject activity() const { return activity_.obj(); }
@@ -38,8 +40,9 @@ class Context {
   TaskRunners task_runners_;
   Storage storage_;
   Loader loader_;
+  ServiceManager service_manager_;
   common::BindingsSupportImpl bindings_support_impl_;
-  scoped_ptr<ServiceManager> service_manager_;
+  scoped_ptr<DynamicServiceLoader> dynamic_service_loader_;
 
 #if defined(OS_ANDROID)
   base::android::ScopedJavaGlobalRef<jobject> activity_;
