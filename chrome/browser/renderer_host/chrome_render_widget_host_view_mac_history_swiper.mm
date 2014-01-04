0e85c6b123354e12142f1a637a97e633b29bb1ff
erikchen@chromium.org <erikchen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-04 04:15:27 +0000

    mac: Fix a crash in the history swiper for magic mouse momentum events.
    The 'trackSwipeWithEvent:' api throws an exception if the event passed in is a
    momentum scroll event. We started using momentum scroll events in our logic for
    magic trackpads, since we're working directly with hardware touches. We failed
    to filter out momentum events when falling back to the 'trackSwipeWithEvent:'
    api for magic mouse events.
    
    BUG=331506
    
    Review URL: https://codereview.chromium.org/124313002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243003 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/chrome_render_widget_host_view_mac_history_swiper.mm
+++ chrome/browser/renderer_host/chrome_render_widget_host_view_mac_history_swiper.mm
@@ -296,6 +296,10 @@ static BOOL forceMagicMouse = NO;
 // We use an entirely different set of logic for magic mouse swipe events,
 // since we do not get NSTouch callbacks.
 - (BOOL)maybeHandleMagicMouseHistorySwiping:(NSEvent*)theEvent {
+  // The 'trackSwipeEventWithOptions:' api doesn't handle momentum events.
+  if ([theEvent phase] == NSEventPhaseNone)
+    return NO;
+
   mouseScrollDelta_.width += [theEvent scrollingDeltaX];
   mouseScrollDelta_.height += [theEvent scrollingDeltaY];
 
@@ -320,6 +324,12 @@ static BOOL forceMagicMouse = NO;
       return NO;
   }
 
+  [self initiateMagicMouseHistorySwipe:isRightScroll event:theEvent];
+  return YES;
+}
+
+- (void)initiateMagicMouseHistorySwipe:(BOOL)isRightScroll
+                                 event:(NSEvent*)event {
   // Released by the tracking handler once the gesture is complete.
   HistoryOverlayController* historyOverlay = [[HistoryOverlayController alloc]
       initForMode:isRightScroll ? kHistoryOverlayModeForward
@@ -349,7 +359,7 @@ static BOOL forceMagicMouse = NO;
   // direction after the initial swipe doesn't cause the shield to move
   // in the wrong direction.
   forceMagicMouse = YES;
-  [theEvent trackSwipeEventWithOptions:NSEventSwipeTrackingLockDirection
+  [event trackSwipeEventWithOptions:NSEventSwipeTrackingLockDirection
     dampenAmountThresholdMin:-1
     max:1
     usingHandler:^(CGFloat gestureAmount,
@@ -387,7 +397,6 @@ static BOOL forceMagicMouse = NO;
         if (isComplete)
           [historyOverlay release];
   }];
-  return YES;
 }
 
 // Checks if |theEvent| should trigger history swiping, and if so, does
@@ -506,5 +515,10 @@ static BOOL forceMagicMouse = NO;
   [self beginHistorySwipeInDirection:direction event:theEvent];
   return YES;
 }
+@end
 
+@implementation HistorySwiper (PrivateExposedForTesting)
++ (void)resetMagicMouseState {
+  forceMagicMouse = NO;
+}
 @end
