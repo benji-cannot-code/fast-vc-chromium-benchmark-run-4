a916d5a4bfac2e3945e380d9f5d543b476596734
markusheintz@chromium.org <markusheintz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-05 20:13:10 +0000

    Revert 241930 "This CL makes sure that the credentials that are ..."
    > This CL makes sure that the credentials that are displayed in the ManagePasswordsBubble are updated.
    > 
    > BUG=261628
    > 
    > Review URL: https://codereview.chromium.org/99813002
    
    TBR=npentrel@chromium.org
    
    Review URL: https://codereview.chromium.org/108343007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243044 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/password_manager/password_manager.cc
+++ chrome/browser/password_manager/password_manager.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/password_manager/password_manager_metrics_util.h"
 #include "chrome/browser/password_manager/password_manager_util.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/passwords/manage_passwords_bubble_ui_controller.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/chrome_version_info.h"
 #include "chrome/common/pref_names.h"
@@ -80,17 +81,6 @@ void ReportMetrics(bool password_manager_enabled) {
 }
 
 }  // namespace
-
-PasswordManager::PasswordObserver::PasswordObserver(
-    PasswordManager* password_manager)
-    : password_manager_(password_manager),
-      weak_factory_(this) {
-  password_manager_->SetPasswordObserver(weak_factory_.GetWeakPtr());
-}
-
-PasswordManager::PasswordObserver::~PasswordObserver() {
-  weak_factory_.InvalidateWeakPtrs();
-}
 
 // static
 void PasswordManager::RegisterProfilePrefs(
@@ -354,28 +344,6 @@ bool PasswordManager::ShouldShowSavePasswordInfoBar() const {
          !provisional_save_manager_->IsPendingCredentialsPublicSuffixMatch();
 }
 
-void PasswordManager::SetPasswordObserver(
-    base::WeakPtr<PasswordObserver> observer) {
-  password_observer_ = observer;
-}
-
-void PasswordManager::NotifyPasswordObserver(
-    PasswordObserver::BubbleNotification notification,
-    const autofill::PasswordFormMap& password_form_map,
-    const autofill::PasswordForm& password_form) const {
-  if (password_observer_) {
-    password_observer_->OnPasswordAction(notification, password_form_map,
-                                         password_form);
-  }
-}
-
-void PasswordManager::NotifyPasswordObserverUpdatePasswords(
-    const autofill::PasswordFormMap& password_form_map) const {
-  if (password_observer_) {
-    password_observer_->OnPasswordsUpdated(password_form_map);
-  }
-}
-
 void PasswordManager::OnPasswordFormsRendered(
     const std::vector<PasswordForm>& visible_forms) {
   if (!provisional_save_manager_.get())
@@ -402,9 +370,14 @@ void PasswordManager::OnPasswordFormsRendered(
   if (ShouldShowSavePasswordInfoBar()) {
     if (CommandLine::ForCurrentProcess()->HasSwitch(
           switches::kEnableSavePasswordBubble)) {
-      NotifyPasswordObserver(PasswordObserver::SAVE_NEW_PASSWORD,
-                             provisional_save_manager_->best_matches(),
-                             provisional_save_manager_->pending_credentials());
+      ManagePasswordsBubbleUIController* manage_passwords_bubble_ui_controller =
+          ManagePasswordsBubbleUIController::FromWebContents(web_contents());
+      if (manage_passwords_bubble_ui_controller) {
+        manage_passwords_bubble_ui_controller->OnPasswordSubmitted(
+            provisional_save_manager_.release());
+      } else {
+        provisional_save_manager_.reset();
+      }
     } else {
       delegate_->AddSavePasswordInfoBarIfPermitted(
           provisional_save_manager_.release());
@@ -486,26 +459,11 @@ void PasswordManager::Autofill(
       break;
   }
 
-  if (CommandLine::ForCurrentProcess()->HasSwitch(
+  ManagePasswordsBubbleUIController* manage_passwords_bubble_ui_controller =
+      ManagePasswordsBubbleUIController::FromWebContents(web_contents());
+  if (manage_passwords_bubble_ui_controller &&
+      CommandLine::ForCurrentProcess()->HasSwitch(
           switches::kEnableSavePasswordBubble)) {
-    NotifyPasswordObserver(PasswordObserver::AUTOFILL_PASSWORDS, best_matches,
-                           form_for_autofill);
+    manage_passwords_bubble_ui_controller->OnPasswordAutofilled(best_matches);
   }
-}
-
-void PasswordManager::UpdateBestMatches(
-    const autofill::PasswordForm& password_form) {
-  update_best_matches_password_form_manager_.reset(
-      new PasswordFormManager(delegate_->GetProfile(),
-                              this,
-                              web_contents(),
-                              password_form,
-                              true));
-  update_best_matches_password_form_manager_->
-      FetchMatchingLoginsFromPasswordStoreForBubble();
-}
-
-void PasswordManager::SendBestMatchesToManagePasswordsBubble(
-    const PasswordFormMap& best_matches) const {
-  NotifyPasswordObserverUpdatePasswords(best_matches);
 }
--- chrome/browser/ui/passwords/manage_passwords_bubble_model.h
+++ chrome/browser/ui/passwords/manage_passwords_bubble_model.h
@@ -24,6 +24,7 @@ class ManagePasswordsBubbleModel : public content::WebContentsObserver {
 
   enum ManagePasswordsBubbleState {
     PASSWORD_TO_BE_SAVED,
+    MANAGE_PASSWORDS_AFTER_SAVING,
     MANAGE_PASSWORDS
   };
 
@@ -40,10 +41,17 @@ class ManagePasswordsBubbleModel : public content::WebContentsObserver {
   // PasswordStore.
   void OnPasswordAction(autofill::PasswordForm password_form, bool remove);
 
+  // Called by the view code when the ManagePasswordItemView is destroyed and
+  // the user chose to delete the password.
+  // TODO(npentrel): Remove this once best_matches_ are newly made on bubble
+  // opening.
+  void DeleteFromBestMatches(autofill::PasswordForm password_form);
+
   ManagePasswordsBubbleState manage_passwords_bubble_state() {
     return manage_passwords_bubble_state_;
   }
 
+  bool password_submitted() { return password_submitted_; }
   const base::string16& title() { return title_; }
   const autofill::PasswordForm& pending_credentials() {
     return pending_credentials_;
@@ -58,6 +66,7 @@ class ManagePasswordsBubbleModel : public content::WebContentsObserver {
 
   content::WebContents* web_contents_;
   ManagePasswordsBubbleState manage_passwords_bubble_state_;
+  bool password_submitted_;
   base::string16 title_;
   autofill::PasswordForm pending_credentials_;
   autofill::PasswordFormMap best_matches_;
