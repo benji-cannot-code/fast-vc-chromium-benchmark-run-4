6b29e6590d893d7e6a56b4dfbf2f1b0f53f3b8ec
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-06 23:58:35 +0000

    Delete char32 from base/basictypes.h.
    (Convert its one use to use int32_t instead.)
    
    R=brettw@chromium.org
    TBR=rsleevi@chromium.org
    BUG=138542
    
    Review URL: https://codereview.chromium.org/125103006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243209 0039d316-1c4b-4281-b951-d872f2087c98

--- net/cert/x509_cert_types_mac.cc
+++ net/cert/x509_cert_types_mac.cc
@@ -1,9 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "net/cert/x509_cert_types.h"
+
+#include <stdint.h>
 
 #include <CoreServices/CoreServices.h>
 #include <Security/SecAsn1Coder.h>
@@ -131,7 +133,7 @@ bool UTF16BigEndianToUTF8(base::char16* chars, size_t length,
 
 // Converts big-endian UTF-32 to UTF-8 in a std::string.
 // Note: The byte-order flipping is done in place on the input buffer!
-bool UTF32BigEndianToUTF8(char32* chars, size_t length,
+bool UTF32BigEndianToUTF8(int32_t* chars, size_t length,
                           std::string* out_string) {
   for (size_t i = 0; i < length; ++i)
     chars[i] = EndianS32_BtoN(chars[i]);
@@ -251,8 +253,8 @@ bool CertPrincipal::ParseDistinguishedName(const void* ber_name_data,
         }
         case BER_TAG_PKIX_UNIVERSAL_STRING: {  // UTF-32, big-endian
           std::string value;
-          UTF32BigEndianToUTF8(reinterpret_cast<char32*>(pair->value.Data),
-                               pair->value.Length / sizeof(char32),
+          UTF32BigEndianToUTF8(reinterpret_cast<int32_t*>(pair->value.Data),
+                               pair->value.Length / sizeof(int32_t),
                                &value);
           AddTypeValuePair(pair->type, value, values);
           break;
