e79454770e7fe0809cc9561f42a994c967e241d4
shinyak@chromium.org <shinyak@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-06 19:47:11 +0000

    compile error fix for VS2013
    std::max and std::min requires algorithm header.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/117903005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243146 0039d316-1c4b-4281-b951-d872f2087c98

--- net/quic/congestion_control/inter_arrival_bitrate_ramp_up_test.cc
+++ net/quic/congestion_control/inter_arrival_bitrate_ramp_up_test.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <algorithm>
+
 #include "base/basictypes.h"
 #include "base/logging.h"
 #include "net/quic/congestion_control/inter_arrival_bitrate_ramp_up.h"
