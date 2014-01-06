79b827e4cc929b922609ee0cd413668516e0a439
dtrainor@chromium.org <dtrainor@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-06 18:24:12 +0000

    Turn on context menu tests
    The test harness flakiness was fixed with 115473002 and 114563002.  Turning these back on
    
    BUG=326769
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/106053009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243124 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ContextMenuTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ContextMenuTest.java
@@ -9,7 +9,6 @@ import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.os.Build;
-import android.test.FlakyTest;
 import android.test.suitebuilder.annotation.LargeTest;
 import android.view.ContextMenu;
 
@@ -42,9 +41,7 @@ public class ContextMenuTest extends ChromiumTestShellTestBase {
         assertWaitForPageScaleFactorMatch(0.5f);
     }
 
-    // http://crbug.com/326769
-    @FlakyTest
-    // @LargeTest
+    @LargeTest
     @Feature({"Browser", "Main"})
     public void testCopyLinkURL() throws InterruptedException, TimeoutException {
         TabBase tab = getActivity().getActiveTab();
@@ -55,9 +52,7 @@ public class ContextMenuTest extends ChromiumTestShellTestBase {
         assertStringContains("test_link.html", getClipboardText());
     }
 
-    // http://crbug.com/326769
-    @FlakyTest
-    // @LargeTest
+    @LargeTest
     @Feature({"Browser"})
     public void testCopyImageLinkCopiesLinkURL() throws InterruptedException, TimeoutException {
         TabBase tab = getActivity().getActiveTab();
@@ -68,9 +63,7 @@ public class ContextMenuTest extends ChromiumTestShellTestBase {
         assertStringContains("test_link.html", getClipboardText());
     }
 
-    // http://crbug.com/326769
-    @FlakyTest
-    // @LargeTest
+    @LargeTest
     @Feature({"Browser"})
     public void testCopyLinkTextSimple() throws InterruptedException, TimeoutException {
         TabBase tab = getActivity().getActiveTab();
@@ -82,9 +75,7 @@ public class ContextMenuTest extends ChromiumTestShellTestBase {
                 getClipboardText());
     }
 
-    // http://crbug.com/326769
-    @FlakyTest
-    // @LargeTest
+    @LargeTest
     @Feature({"Browser"})
     public void testCopyLinkTextComplex() throws InterruptedException, TimeoutException {
         TabBase tab = getActivity().getActiveTab();
@@ -96,9 +87,7 @@ public class ContextMenuTest extends ChromiumTestShellTestBase {
                 "This is pretty   extreme \n(newline). ", getClipboardText());
     }
 
-    // http://crbug.com/326769
-    @FlakyTest
-    // @LargeTest
+    @LargeTest
     @Feature({"Browser"})
     public void testCopyImageToClipboard() throws InterruptedException, TimeoutException {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) return;
@@ -114,9 +103,7 @@ public class ContextMenuTest extends ChromiumTestShellTestBase {
         assertEquals("Clipboard text is not correct", expectedUrl, getClipboardText());
     }
 
-    // http://crbug.com/326769
-    @FlakyTest
-    // @LargeTest
+    @LargeTest
     @Feature({"Browser"})
     public void testLongPressOnImage() throws InterruptedException, TimeoutException {
         final TabBase tab = getActivity().getActiveTab();
@@ -146,9 +133,7 @@ public class ContextMenuTest extends ChromiumTestShellTestBase {
         assertEquals("Failed to navigate to the image", expectedUrl, actualUrl.get());
     }
 
-    // http://crbug.com/326769
-    @FlakyTest
-    // @LargeTest
+    @LargeTest
     @Feature({"Browser"})
     public void testLongPressOnImageLink() throws InterruptedException, TimeoutException {
         final TabBase tab = getActivity().getActiveTab();
@@ -175,9 +160,7 @@ public class ContextMenuTest extends ChromiumTestShellTestBase {
         assertTrue("Navigated to the wrong page.", actualTitle.get().startsWith("test_image.png"));
     }
 
-    // http://crbug.com/326769
-    @FlakyTest
-    // @LargeTest
+    @LargeTest
     @Feature({"Browser"})
     public void testDismissContextMenuOnBack() throws InterruptedException, TimeoutException {
         TabBase tab = getActivity().getActiveTab();
@@ -197,9 +180,7 @@ public class ContextMenuTest extends ChromiumTestShellTestBase {
                 }));
     }
 
-    // http://crbug.com/326769
-    @FlakyTest
-    // @LargeTest
+    @LargeTest
     @Feature({"Browser"})
     public void testDismissContextMenuOnClick() throws InterruptedException, TimeoutException {
         TabBase tab = getActivity().getActiveTab();
