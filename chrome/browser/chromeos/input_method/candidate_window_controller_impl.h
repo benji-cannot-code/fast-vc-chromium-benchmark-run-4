093172bf003b4189f12cfda6c913bc7e238bb5cb
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-06 17:03:25 +0000

    Cleanup CandidateWindowView.
    - moves the implementation of CandidateView as a separate cc file.
    - CandidateView is now a button to reduce the click handling.
    - introduces canceling drag logic over CandidateViews
    - CandidateWindowView is now a BubbleDelegateView
    - HidableArea is removed. It's not used anymore.
    - removes complex GridLayout logic and use BoxLayout.
    
    BUG=325813
    R=komatsu@chromium.org
    TEST=unit_tests pass, manually check
    
    Review URL: https://codereview.chromium.org/121163003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243106 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/input_method/candidate_window_controller_impl.h
+++ chrome/browser/chromeos/input_method/candidate_window_controller_impl.h
@@ -56,8 +56,6 @@ class CandidateWindowControllerImpl
  private:
   // CandidateWindowView::Observer implementation.
   virtual void OnCandidateCommitted(int index) OVERRIDE;
-  virtual void OnCandidateWindowOpened() OVERRIDE;
-  virtual void OnCandidateWindowClosed() OVERRIDE;
 
   // views::WidgetObserver implementation.
   virtual void OnWidgetClosing(views::Widget* widget) OVERRIDE;
@@ -71,18 +69,16 @@ class CandidateWindowControllerImpl
                                  unsigned int cursor, bool visible) OVERRIDE;
   virtual void FocusStateChanged(bool is_focused) OVERRIDE;
 
-  // Creates the candidate window view.
-  void CreateView();
+  void InitCandidateWindowView();
 
   // The candidate window view.
   CandidateWindowView* candidate_window_view_;
 
-  // This is the outer frame of the candidate window view. The frame will
-  // own |candidate_window_|.
-  scoped_ptr<views::Widget> frame_;
-
   // This is the outer frame of the infolist window view. Owned by the widget.
   InfolistWindow* infolist_window_;
+
+  gfx::Rect cursor_bounds_;
+  gfx::Rect composition_head_;
 
   // This is the controller of the IME mode indicator.
   scoped_ptr<ModeIndicatorController> mode_indicator_controller_;
