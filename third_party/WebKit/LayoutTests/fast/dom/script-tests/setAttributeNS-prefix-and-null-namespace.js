f11d069a7e92406769db3b5f9832f8da7d730231
costan@gmail.com <costan@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-06 04:44:37 +0000

    createElementNS handles element name 'xmlns' correctly.
    This matches the behavior of Internet Explorer and Firefox, the DOM
    Level 3 Core specification, and the DOM4 specification.
    
    http://www.w3.org/TR/2004/REC-DOM-Level-3-Core-20040407/core.html#ID-DocCrElNS
    http://www.w3.org/TR/dom/#dom-document-createelementns
    http://w3c.github.io/dom/#dom-document-createelementns
    
    BUG=330946
    
    Review URL: https://codereview.chromium.org/122083002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164492 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/dom/script-tests/setAttributeNS-prefix-and-null-namespace.js
+++
@@ -1,4 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-description("Test that calling setAttributeNS() with a prefixed qualifiedName and null NS throws NAMESPACE_ERR.");
-
-shouldThrow("document.createElement('test').setAttributeNS(null, 'foo:bar', 'baz')", '"NamespaceError: Failed to execute \'setAttributeNS\' on \'Element\': \'\' is an invalid namespace for attributes."');
