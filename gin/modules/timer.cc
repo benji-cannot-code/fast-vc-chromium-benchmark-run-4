e9249745c620b5d4d9da0fda10549bda541369e3
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-06 22:23:26 +0000

    Added a test for destroying an isolate while a timer is outstanding.
    Cleaned up a few of the existing tests.
    
    BUG=
    R=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/100143006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243185 0039d316-1c4b-4281-b951-d872f2087c98

--- gin/modules/timer.cc
+++ gin/modules/timer.cc
@@ -19,7 +19,7 @@ v8::Handle<v8::String> GetHiddenPropertyName(v8::Isolate* isolate) {
 
 }  // namespace
 
-// Timer
+// Timer =======================================================================
 
 gin::WrapperInfo Timer::kWrapperInfo = { gin::kEmbedderNativeGin };
 
@@ -31,6 +31,8 @@ Handle<Timer> Timer::Create(TimerType type, v8::Isolate* isolate, int delay_ms,
 }
 
 ObjectTemplateBuilder Timer::GetObjectTemplateBuilder(v8::Isolate* isolate) {
+  // We use Unretained() here because we directly own timer_, so we know it will
+  // be alive when these methods are called.
   return Wrappable<Timer>::GetObjectTemplateBuilder(isolate)
       .SetMethod("cancel",
                  base::Bind(&base::Timer::Stop, base::Unretained(&timer_)))
@@ -54,8 +56,13 @@ Timer::~Timer() {
 }
 
 void Timer::OnTimerFired() {
-  if (!runner_)
+  // This can happen in spite of the weak callback because it is possible for
+  // a gin::Handle<> to keep this object alive past when the isolate it is part
+  // of is destroyed.
+  if (!runner_.get()) {
     return;
+  }
+
   Runner::Scope scope(runner_.get());
   v8::Handle<v8::Function> function = v8::Handle<v8::Function>::Cast(
       GetWrapper(runner_->isolate())->GetHiddenValue(
@@ -64,7 +71,7 @@ void Timer::OnTimerFired() {
 }
 
 
-// TimerModule
+// TimerModule =================================================================
 
 WrapperInfo TimerModule::kWrapperInfo = { kEmbedderNativeGin };
 
