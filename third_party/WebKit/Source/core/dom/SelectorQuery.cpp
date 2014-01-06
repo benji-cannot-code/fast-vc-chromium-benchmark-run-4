33af69b49032ef2fcfbdd5d4107b56836bcadf74
ch.dumez@samsung.com <ch.dumez@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-06 04:23:53 +0000

    Regression(r164102): CSS Selector queries on duplicate ids match non-descendant elements
    After r164102, CSS Selector queries on duplicate ids match non-descendant
    elements. It returns ALL elements with the given identifier, without checking
    that these are descendant of the scope Element.
    
    This CL adds the missing scope check.
    
    BUG=331692
    TEST=fast/dom/SelectorAPI/duplicate-id-scope.html
    R=esprehn, abarth
    
    Review URL: https://codereview.chromium.org/93863004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164484 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/SelectorQuery.cpp
+++ third_party/WebKit/Source/core/dom/SelectorQuery.cpp
@@ -338,6 +338,8 @@ void SelectorDataList::executeQueryAll(Node& rootNode, Vector<RefPtr<Node> >& ma
             for (size_t i = 0; i < count; ++i) {
                 Element* element = elements[i];
                 ASSERT(element);
+                if (!(isTreeScopeRoot(rootNode) || element->isDescendantOf(&rootNode)))
+                    continue;
                 if (selectorMatches(selector, *element, rootNode))
                     matchedElements.append(element);
             }
@@ -464,6 +466,8 @@ Element* SelectorDataList::executeQueryFirst(Node& rootNode) const
             for (size_t i = 0; i < count; ++i) {
                 Element* element = elements[i];
                 ASSERT(element);
+                if (!(isTreeScopeRoot(rootNode) || element->isDescendantOf(&rootNode)))
+                    continue;
                 if (selectorMatches(selector, *element, rootNode))
                     return element;
             }
