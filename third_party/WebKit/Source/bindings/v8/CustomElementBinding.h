0a4619afb4326621f1b3117c2f2be9aca557f616
kouhei@chromium.org <kouhei@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-06 05:23:19 +0000

    CustomElementBinding should not use UnsafePersistent
    BUG=None
    
    Review URL: https://codereview.chromium.org/120423003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164494 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/CustomElementBinding.h
+++ third_party/WebKit/Source/bindings/v8/CustomElementBinding.h
@@ -32,9 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CustomElementBinding_h
 #define CustomElementBinding_h
 
-#include "bindings/v8/UnsafePersistent.h"
+#include "bindings/v8/ScopedPersistent.h"
 #include "wtf/PassOwnPtr.h"
-#include <v8.h>
 
 namespace WebCore {
 
@@ -44,7 +43,7 @@ class CustomElementBinding {
 public:
     static PassOwnPtr<CustomElementBinding> create(v8::Isolate*, v8::Handle<v8::Object> prototype, const WrapperTypeInfo*);
 
-    ~CustomElementBinding() { m_prototype.dispose(); }
+    ~CustomElementBinding() { }
 
     v8::Handle<v8::Object> prototype() { return m_prototype.newLocal(m_isolate); }
     const WrapperTypeInfo* wrapperType() { return m_wrapperType; }
@@ -53,7 +52,7 @@ private:
     CustomElementBinding(v8::Isolate*, v8::Handle<v8::Object> prototype, const WrapperTypeInfo*);
 
     v8::Isolate* m_isolate;
-    UnsafePersistent<v8::Object> m_prototype;
+    ScopedPersistent<v8::Object> m_prototype;
     const WrapperTypeInfo* m_wrapperType;
 };
 
