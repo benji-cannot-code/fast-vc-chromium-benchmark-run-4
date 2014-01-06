cf53af8eaa88ab3898e14bad25ca6fad4f24f972
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-06 23:52:55 +0000

    WalletClient: don't bother queuing requests; we don't make multiple requests concurrently anyway
    BUG=327911
    
    Review URL: https://codereview.chromium.org/123733002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243207 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/autofill/autofill_dialog_controller_impl.cc
+++ chrome/browser/ui/autofill/autofill_dialog_controller_impl.cc
@@ -884,7 +884,7 @@ void AutofillDialogControllerImpl::GetWalletItems() {
 
   wallet_items_requested_ = true;
   wallet::WalletClient* wallet_client = GetWalletClient();
-  wallet_client->CancelRequests();
+  wallet_client->CancelRequest();
 
   previously_selected_instrument_id_.clear();
   previously_selected_shipping_address_id_.clear();
@@ -2430,7 +2430,7 @@ void AutofillDialogControllerImpl::AccountChoiceChanged() {
   wallet::WalletClient* client = GetWalletClient();
 
   if (is_submitting_)
-    client->CancelRequests();
+    client->CancelRequest();
 
   SetIsSubmitting(false);
 
@@ -2634,7 +2634,7 @@ bool AutofillDialogControllerImpl::IsManuallyEditingSection(
 
 void AutofillDialogControllerImpl::OnWalletSigninError() {
   account_chooser_model_->SetHadWalletSigninError();
-  GetWalletClient()->CancelRequests();
+  GetWalletClient()->CancelRequest();
   LogDialogLatencyToShow();
 }
 
@@ -2643,7 +2643,7 @@ void AutofillDialogControllerImpl::DisableWallet(
   signin_helper_.reset();
   wallet_items_.reset();
   wallet_errors_.clear();
-  GetWalletClient()->CancelRequests();
+  GetWalletClient()->CancelRequest();
   SetIsSubmitting(false);
   wallet_error_notification_ = GetWalletError(error_type);
   account_chooser_model_->SetHadWalletError();
