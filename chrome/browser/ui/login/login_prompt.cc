a60978d79626f834a2370b40476f8b6ec35bef61
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-06 22:22:02 +0000

    Remove calls of PrerenderTracker::TryCancel and TryCancelOnIOThread in ChromeResourceDispatcherHostDelegate. These short-circuited cancelling the prerendering on the IO thread. Instead, I moved cancelling the prerender on the UI thread since both of these calls end up going to the UI thread right after.
    This also removes the last calls of ResourceRequestInfo::GetAssociatedRenderView, which I will remove in a followup.
    
    BUG=304341
    R=davidben@chromium.org
    
    Review URL: https://codereview.chromium.org/124113003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243184 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/login/login_prompt.cc
+++ chrome/browser/ui/login/login_prompt.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/lock.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/password_manager/password_manager.h"
+#include "chrome/browser/prerender/prerender_contents.h"
 #include "chrome/browser/tab_contents/tab_util.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/notification_registrar.h"
@@ -425,6 +426,13 @@ void LoginDialogCallback(const GURL& request_url,
     // not hosted by a tab (e.g. an extension). Cancel just in case
     // (cancelling twice is a no-op).
     handler->CancelAuth();
+    return;
+  }
+
+  prerender::PrerenderContents* prerender_contents =
+      prerender::PrerenderContents::FromWebContents(parent_contents);
+  if (prerender_contents) {
+    prerender_contents->Destroy(prerender::FINAL_STATUS_AUTH_NEEDED);
     return;
   }
 
--- content/browser/loader/resource_loader_delegate.h
+++ content/browser/loader/resource_loader_delegate.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace net {
 class AuthChallengeInfo;
-class SSLCertRequestInfo;
 }
 
 namespace content {
@@ -22,12 +21,6 @@ class CONTENT_EXPORT ResourceLoaderDelegate {
   virtual ResourceDispatcherHostLoginDelegate* CreateLoginDelegate(
       ResourceLoader* loader,
       net::AuthChallengeInfo* auth_info) = 0;
-
-  virtual bool AcceptAuthRequest(ResourceLoader* loader,
-                                 net::AuthChallengeInfo* auth_info) = 0;
-  virtual bool AcceptSSLClientCertificateRequest(
-      ResourceLoader* loader,
-      net::SSLCertRequestInfo* cert_info) = 0;
 
   virtual bool HandleExternalProtocol(ResourceLoader* loader,
                                       const GURL& url) = 0;
