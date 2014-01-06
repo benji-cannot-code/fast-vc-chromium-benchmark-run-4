0200fd6d81a4c9251b1ff3b6ac05503e5afd5ccb
craigdh@chromium.org <craigdh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-06 19:31:25 +0000

    [I-Spy] Add support for rebaselining expectations from the web UI.
    BUG=318865
    TEST=unittests included
    NOTRY=True
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=240226
    
    Review URL: https://codereview.chromium.org/106523003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243140 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/functional/ispy/client/dom.py
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+def GetScriptToWaitForUnchangingDOM():
+  """Gets Javascript that waits until the DOM is stable for 5 seconds.
+
+  Times out if the DOM is not stable within 30 seconds.
+
+  Returns:
+    Javascript as a string.
+  """
+  return """
+    var target = document.body;
+    var callback = arguments[arguments.length - 1]
+
+    var timeout_id = setTimeout(function() {
+      callback()
+    }, 5000);
+
+    var observer = new MutationObserver(function(mutations) {
+      clearTimeout(timeout_id);
+      timeout_id = setTimeout(function() {
+        callback();
+      }, 5000);
+    }).observe(target, {attributes: true, childList: true,
+      characterData: true, subtree: true});
+  """
