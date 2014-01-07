21d2186bb87a72225abba22e55f9d7640fd5a576
eranm@google.com <eranm@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-07 12:55:01 +0000

    Certificate Transparency: Export a missing symbol.
    The SignedCertificateTimestampAndStatus should be available for the
    SCT viewer UI.
    
    BUG=309578
    
    Review URL: https://codereview.chromium.org/99553007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243281 0039d316-1c4b-4281-b951-d872f2087c98

--- net/ssl/signed_certificate_timestamp_and_status.h
+++ net/ssl/signed_certificate_timestamp_and_status.h
@@ -9,15 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/memory/ref_counted.h"
+#include "net/base/net_export.h"
 #include "net/cert/sct_status_flags.h"
+#include "net/cert/signed_certificate_timestamp.h"
 
 namespace net {
 
-namespace ct {
-struct SignedCertificateTimestamp;
-}
-
-struct SignedCertificateTimestampAndStatus {
+struct NET_EXPORT SignedCertificateTimestampAndStatus {
   SignedCertificateTimestampAndStatus(
       const scoped_refptr<ct::SignedCertificateTimestamp>& sct,
       ct::SCTVerifyStatus status);
