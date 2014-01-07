b5025c7a9591270a6a5e9c966367d9df7bfaec58
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-07 10:52:13 +0000

    Update type definitions in ppapi/lib/gl/include/KHR/khrplatform.h
    This fixes building PPAPI examples on Win 64 after (I believe) r242740.
    
    See also: third_party/khronos/KHR/khrplatform.h
    
    BUG=326382
    R=bajones@chromium.org, bbudge@chromium.org
    
    Review URL: https://codereview.chromium.org/125013002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243248 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/lib/gl/include/KHR/khrplatform.h
+++ ppapi/lib/gl/include/KHR/khrplatform.h
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /* Khronos platform-specific types and definitions.
  *
- * $Revision: 9356 $ on $Date: 2009-10-21 02:52:25 -0700 (Wed, 21 Oct 2009) $
+ * $Revision: 23298 $ on $Date: 2013-09-30 17:07:13 -0700 (Mon, 30 Sep 2013) $
  *
  * Adopters may modify this file to suit their platform. Adopters are
  * encouraged to submit platform specific modifications to the Khronos
@@ -232,10 +232,23 @@ typedef signed   char          khronos_int8_t;
 typedef unsigned char          khronos_uint8_t;
 typedef signed   short int     khronos_int16_t;
 typedef unsigned short int     khronos_uint16_t;
+
+/*
+ * Types that differ between LLP64 and LP64 architectures - in LLP64,
+ * pointers are 64 bits, but 'long' is still 32 bits. Win64 appears
+ * to be the only LLP64 architecture in current use.
+ */
+#ifdef _WIN64
+typedef signed   long long int khronos_intptr_t;
+typedef unsigned long long int khronos_uintptr_t;
+typedef signed   long long int khronos_ssize_t;
+typedef unsigned long long int khronos_usize_t;
+#else
 typedef signed   long  int     khronos_intptr_t;
 typedef unsigned long  int     khronos_uintptr_t;
 typedef signed   long  int     khronos_ssize_t;
 typedef unsigned long  int     khronos_usize_t;
+#endif
 
 #if KHRONOS_SUPPORT_FLOAT
 /*
