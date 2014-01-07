9fd3b3ba4888b459f6b1d98055b54ac81f022c59
l.gombos@samsung.com <l.gombos@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-07 19:44:36 +0000

    OS(MACOSX) and USE(WEBAUDIO_FFMPEG) are mutually exclusive.
    Simplify platform/audio to remove unnecessary tests.
    
    Review URL: https://codereview.chromium.org/110013008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164605 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/audio/ffmpeg/FFTFrameFFMPEG.cpp
+++ third_party/WebKit/Source/platform/audio/ffmpeg/FFTFrameFFMPEG.cpp
@@ -266,6 +266,6 @@ RDFTContext* FFTFrame::contextForSize(unsigned fftSize, int trans)
 
 } // namespace WebCore
 
-#endif // !OS(MACOSX) && USE(WEBAUDIO_FFMPEG)
+#endif // USE(WEBAUDIO_FFMPEG)
 
 #endif // ENABLE(WEB_AUDIO)
