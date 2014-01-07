e3ca68cba2cc53b761fa3fef8597c46caa4fc0ee
guohui@chromium.org <guohui@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-07 22:17:09 +0000

    Fix the advanced link on signin bubble
    BUG=331184
    
    Review URL: https://codereview.chromium.org/123263004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243405 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/sync/one_click_signin_bubble_links_delegate.cc
+++ chrome/browser/ui/sync/one_click_signin_bubble_links_delegate.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_navigator.h"
+#include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/common/url_constants.h"
 
 OneClickSigninBubbleLinksDelegate::OneClickSigninBubbleLinksDelegate(
@@ -24,9 +25,5 @@ void OneClickSigninBubbleLinksDelegate::OnLearnMoreLinkClicked(
 }
 
 void OneClickSigninBubbleLinksDelegate::OnAdvancedLinkClicked() {
-  chrome::NavigateParams params(browser_,
-                                GURL(chrome::kChromeUISettingsURL),
-                                content::PAGE_TRANSITION_LINK);
-  params.disposition = CURRENT_TAB;
-  chrome::Navigate(&params);
+  chrome::ShowSettingsSubPage(browser_, chrome::kSyncSetupSubPage);
 }
