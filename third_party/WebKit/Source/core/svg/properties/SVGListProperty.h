230b4934c72bcccaaffed8cb50d900db411abe2c
mkwst@chromium.org <mkwst@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-07 13:00:20 +0000

    Drop 'throwUninformativeAndGenericTypeError' usage from SVG.
    BUG=270033
    
    Review URL: https://codereview.chromium.org/104713010
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164573 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/properties/SVGListProperty.h
+++ third_party/WebKit/Source/core/svg/properties/SVGListProperty.h
@@ -148,7 +148,7 @@ public:
 
         // Not specified, but FF/Opera do it this way, and it's just sane.
         if (!passNewItem) {
-            exceptionState.throwUninformativeAndGenericTypeError();
+            exceptionState.throwTypeError("The PassListItemType provided is invalid.");
             return 0;
         }
 
@@ -242,7 +242,7 @@ public:
 
         // Not specified, but FF/Opera do it this way, and it's just sane.
         if (!passNewItem) {
-            exceptionState.throwUninformativeAndGenericTypeError();
+            exceptionState.throwTypeError("The PassListItemType provided is invalid.");
             return 0;
         }
 
@@ -315,7 +315,7 @@ public:
 
         // Not specified, but FF/Opera do it this way, and it's just sane.
         if (!passNewItem) {
-            exceptionState.throwUninformativeAndGenericTypeError();
+            exceptionState.throwTypeError("The PassListItemType provided is invalid.");
             return 0;
         }
 
@@ -418,7 +418,7 @@ public:
 
         // Not specified, but FF/Opera do it this way, and it's just sane.
         if (!passNewItem) {
-            exceptionState.throwUninformativeAndGenericTypeError();
+            exceptionState.throwTypeError("The PassListItemType provided is invalid.");
             return 0;
         }
 
