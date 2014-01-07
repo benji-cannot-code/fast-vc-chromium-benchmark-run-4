a9bd1686b566b5c7862f2ffecbfc10ba265c4852
horo@chromium.org <horo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-07 13:09:18 +0000

    Move the worker script loading code to the worker process (phase:1/5)
    - Add contentSecurityPolicy and WebContentSecurityPolicyType to the arguments of WebSharedWorkerRepositoryClient::createSharedWorkerConnector.
    - Add workerScriptLoaded(), workerScriptLoadFailed(), and selectAppChacheID() to WebSharedWorkerClient.
    
    This is step 1 of moving the worker script loading code from the renderer process to the worker process.
    See: http://crbug.com/329786
    
    BUG=329786
    
    Review URL: https://codereview.chromium.org/102243010
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164576 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/web/WebSharedWorkerRepositoryClient.h
+++ third_party/WebKit/public/web/WebSharedWorkerRepositoryClient.h
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+enum WebContentSecurityPolicyType;
 class WebString;
 class WebURL;
 
@@ -45,7 +46,12 @@ public:
     typedef unsigned long long DocumentID;
 
     // Creates a new shared worker connector. This may return null.
+    // FIXME(horo): Remove createSharedWorkerConnector of 3 arguments.
     virtual WebSharedWorkerConnector* createSharedWorkerConnector(const WebURL& url, const WebString& name, DocumentID id) { return 0; }
+    virtual WebSharedWorkerConnector* createSharedWorkerConnector(const WebURL& url, const WebString& name, DocumentID id, const WebString& contentSecurityPolicy, WebContentSecurityPolicyType)
+    {
+        return createSharedWorkerConnector(url, name, id);
+    }
 
     // Invoked when a document has been detached. DocumentID can be re-used after documentDetached() is invoked.
     virtual void documentDetached(DocumentID) { }
