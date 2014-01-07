ec2668aabbdbf30eb12aa21bdeb02fa0f7195c75
sgurun@chromium.org <sgurun@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-07 22:04:40 +0000

    Do not use CalledByNativeUnchecked
    The preferred method is to handle exceptions in java code.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/109023011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243398 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/native/android_protocol_handler.cc
+++ android_webview/native/android_protocol_handler.cc
@@ -147,8 +147,7 @@ AndroidStreamReaderURLRequestJobDelegateImpl::OpenInputStream(
           GetResourceContext(env).obj(),
           jurl.obj());
 
-  // Check and clear pending exceptions.
-  if (ClearException(env) || stream.is_null()) {
+  if (stream.is_null()) {
     DLOG(ERROR) << "Unable to open input stream for Android URL";
     return scoped_ptr<InputStream>();
   }
@@ -183,7 +182,7 @@ bool AndroidStreamReaderURLRequestJobDelegateImpl::GetMimeType(
           env,
           GetResourceContext(env).obj(),
           stream_impl->jobj(), url.obj());
-  if (ClearException(env) || returned_type.is_null())
+  if (returned_type.is_null())
     return false;
 
   *mime_type = base::android::ConvertJavaStringToUTF8(returned_type);
