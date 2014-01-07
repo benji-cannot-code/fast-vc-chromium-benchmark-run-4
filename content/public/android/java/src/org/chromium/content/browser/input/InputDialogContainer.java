982f840118470a8142351b9426d1c53ab0313864
keishi@chromium.org <keishi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-07 15:16:37 +0000

    Removing use of AlertDialog.Builder.setOnDismissListener from InputDialogContainer
    AlertDialog.Builder.setOnDismissListener exists only in API 17 and above.
    
    BUG=329327
    
    Review URL: https://codereview.chromium.org/117883002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243294 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/input/InputDialogContainer.java
+++ content/public/android/java/src/org/chromium/content/browser/input/InputDialogContainer.java
@@ -29,6 +29,9 @@ import java.util.GregorianCalendar;
 import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Opens the approprate date/time picker dialog for the given dialog type.
+ */
 public class InputDialogContainer {
 
     interface InputActionDelegate {
@@ -170,15 +173,6 @@ public class InputDialogContainer {
         mDialog = new AlertDialog.Builder(mContext)
             .setTitle(dialogTitleId)
             .setView(suggestionListView)
-            .setOnDismissListener(new DialogInterface.OnDismissListener() {
-                @Override
-                public void onDismiss(DialogInterface dialog) {
-                    if (mDialog == dialog && !mDialogAlreadyDismissed) {
-                        mDialogAlreadyDismissed = true;
-                        mInputActionDelegate.cancelDateTimeDialog();
-                    }
-                }
-            })
             .setNegativeButton(mContext.getText(android.R.string.cancel),
                 new DialogInterface.OnClickListener() {
                     @Override
@@ -188,6 +182,15 @@ public class InputDialogContainer {
                 })
             .create();
 
+        mDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                if (mDialog == dialog && !mDialogAlreadyDismissed) {
+                    mDialogAlreadyDismissed = true;
+                    mInputActionDelegate.cancelDateTimeDialog();
+                }
+            }
+        });
         mDialogAlreadyDismissed = false;
         mDialog.show();
     }
