45c102aef5d8242f694da1b474663f172f29c5be
adam.treat@samsung.com <adam.treat@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-07 22:47:13 +0000

    We should unify various forms of tree traversal in DOM
    Unifies tree-traversal methods in newer WTF::TreeNode class to reduce code
    copying and ensure that traversals are as fast as possible.  This is hopefully
    a first step towards unifying the data structures and traversal algorithms for
    Node/ContainerNode and RenderObject/RenderObjectChildList.
    
    The plan is to eventually merge NodeTraversal.h into TreeNode.h and replace all
    usage of the former with the latter.  This first step is more modest in scope,
    replacing only those methods that are templated and/or are copy pasted copies
    between the current user of TreeNode.h -- HTMLImport -- and NodeTraversal.h.
    
    BUG=331880
    
    Review URL: https://codereview.chromium.org/125503002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164625 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLImport.cpp
+++ third_party/WebKit/Source/core/html/HTMLImport.cpp
@@ -65,7 +65,7 @@ void HTMLImport::appendChild(HTMLImport* child)
 inline bool HTMLImport::isBlockedFromCreatingDocumentByPredecessors() const
 {
     ASSERT(isBlockedFromCreatingDocument());
-    HTMLImport* elder = previous();
+    HTMLImport* elder = previousSibling();
     return (elder && !elder->isDone());
 }
 
@@ -75,7 +75,7 @@ bool HTMLImport::isBlockedFromRunningScriptByPredecessors() const
     if (!parent)
         return false;
 
-    for (HTMLImport* sibling = parent->firstChild(); sibling; sibling = sibling->next()) {
+    for (HTMLImport* sibling = parent->firstChild(); sibling; sibling = sibling->nextSibling()) {
         if (sibling == this)
             break;
         if (sibling->isBlockingFollowersFromRunningScript())
@@ -148,7 +148,7 @@ bool HTMLImport::unblock(HTMLImport* import)
         return false;
     import->unblockFromCreatingDocument();
 
-    for (HTMLImport* child = import->firstChild(); child; child = child->next()) {
+    for (HTMLImport* child = import->firstChild(); child; child = child->nextSibling()) {
         if (!unblock(child))
             return false;
     }
@@ -160,7 +160,7 @@ bool HTMLImport::unblock(HTMLImport* import)
 
 void HTMLImport::block(HTMLImport* import)
 {
-    for (HTMLImport* child = import; child; child = traverseNext(child, import))
+    for (HTMLImport* child = import; child; child = traverseNext(*child, import))
         child->blockFromRunningScript();
 }
 
@@ -168,7 +168,7 @@ void HTMLImport::blockPredecessorsOf(HTMLImport* child)
 {
     ASSERT(child->parent() == this);
 
-    for (HTMLImport* sibling = lastChild(); sibling; sibling = sibling->previous()) {
+    for (HTMLImport* sibling = lastChild(); sibling; sibling = sibling->previousSibling()) {
         if (sibling == child)
             break;
         HTMLImport::block(sibling);
