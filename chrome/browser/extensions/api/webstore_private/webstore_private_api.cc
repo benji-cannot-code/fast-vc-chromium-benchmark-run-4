bfb4f7c106d81710f7b901ef218b5be6fd6a6213
tmdiep@chromium.org <tmdiep@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-07 10:05:45 +0000

    Fixed cached ephemeral apps preventing install of the app via webstore private api
    Fixed the following bug:
    If an ephemeral app is cached, attempting to install via the app's
    webstore detail page will fail. An error dialog will be displayed,
    stating that the install is already in progress.
    
    BUG=312460
    
    Review URL: https://codereview.chromium.org/93943006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243234 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/webstore_private/webstore_private_api.cc
+++ chrome/browser/extensions/api/webstore_private/webstore_private_api.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_prefs.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/extension_system.h"
+#include "chrome/browser/extensions/extension_util.h"
 #include "chrome/browser/extensions/webstore_installer.h"
 #include "chrome/browser/gpu/gpu_feature_checker.h"
 #include "chrome/browser/profiles/profile_manager.h"
@@ -300,9 +301,10 @@ bool WebstorePrivateBeginInstallWithManifest3Function::RunImpl() {
 
   ExtensionService* service =
       extensions::ExtensionSystem::Get(GetProfile())->extension_service();
-  if (service->GetInstalledExtension(params_->details.id) ||
-      !g_pending_installs.Get().InsertInstall(GetProfile(),
-                                              params_->details.id)) {
+  if (extension_util::IsExtensionInstalledPermanently(params_->details.id,
+                                                      service)
+      || !g_pending_installs.Get().InsertInstall(GetProfile(),
+                                                 params_->details.id)) {
     SetResultCode(ALREADY_INSTALLED);
     error_ = kAlreadyInstalledError;
     return false;
@@ -579,7 +581,7 @@ void WebstorePrivateCompleteInstallFunction::OnExtensionInstallSuccess(
   if (test_webstore_installer_delegate)
     test_webstore_installer_delegate->OnExtensionInstallSuccess(id);
 
-  LOG(INFO) << "Install success, sending response";
+  VLOG(1) << "Install success, sending response";
   g_pending_installs.Get().EraseInstall(GetProfile(), id);
   SendResponse(true);
 
@@ -599,7 +601,7 @@ void WebstorePrivateCompleteInstallFunction::OnExtensionInstallFailure(
   }
 
   error_ = error;
-  LOG(INFO) << "Install failed, sending response";
+  VLOG(1) << "Install failed, sending response";
   g_pending_installs.Get().EraseInstall(GetProfile(), id);
   SendResponse(false);
 
