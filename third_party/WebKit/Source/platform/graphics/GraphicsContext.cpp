16551d9748bbd7e9eddcea10dcf653e63aacae04
schenney@chromium.org <schenney@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-07 23:44:05 +0000

    Remove a FIXME related to SkDevice refactoring
    BUG=278148
    
    Review URL: https://codereview.chromium.org/126163002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164636 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/GraphicsContext.cpp
+++ third_party/WebKit/Source/platform/graphics/GraphicsContext.cpp
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkAnnotation.h"
 #include "third_party/skia/include/core/SkColorFilter.h"
 #include "third_party/skia/include/core/SkData.h"
+#include "third_party/skia/include/core/SkDevice.h"
 #include "third_party/skia/include/core/SkPicture.h"
 #include "third_party/skia/include/core/SkRRect.h"
 #include "third_party/skia/include/core/SkRefCnt.h"
--- third_party/WebKit/Source/platform/graphics/GraphicsContext.h
+++ third_party/WebKit/Source/platform/graphics/GraphicsContext.h
@@ -41,8 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/graphics/GraphicsContextState.h"
 #include "platform/graphics/skia/OpaqueRegionSkia.h"
 #include "platform/graphics/skia/SkiaUtils.h"
-// TODO(robertphillips): replace this include with "class SkBaseDevice;"
-#include "third_party/skia/include/core/SkDevice.h"
 #include "wtf/FastAllocBase.h"
 #include "wtf/Forward.h"
 #include "wtf/Noncopyable.h"
