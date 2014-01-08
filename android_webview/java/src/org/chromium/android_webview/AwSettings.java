58718d569a43fcaf02fce91124f82538b265026e
noamsml@chromium.org <noamsml@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-08 18:49:06 +0000

    Revert of https://codereview.chromium.org/115253004/ Reason for revert: http://crbug.com/332434
    TBR=benm@chromium.org,mnaganov@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=332089
    
    Review URL: https://codereview.chromium.org/128933002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243615 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/java/src/org/chromium/android_webview/AwSettings.java
+++ android_webview/java/src/org/chromium/android_webview/AwSettings.java
@@ -11,7 +11,6 @@ import android.os.Handler;
 import android.os.Message;
 import android.os.Process;
 import android.provider.Settings;
-import android.util.Log;
 import android.webkit.WebSettings;
 import android.webkit.WebSettings.PluginState;
 
@@ -28,8 +27,8 @@ import org.chromium.base.ThreadUtils;
  */
 @JNINamespace("android_webview")
 public class AwSettings {
-    // Do not use! Will be removed soon. See crbug.com/332089.
-    // Use android.webkit.WebSettings.LayoutAlgorithm instead.
+    // This enum corresponds to WebSettings.LayoutAlgorithm. We use our own to be
+    // able to extend it.
     public enum LayoutAlgorithm {
         NORMAL,
         SINGLE_COLUMN,
@@ -52,8 +51,7 @@ public class AwSettings {
     // Lock to protect all settings.
     private final Object mAwSettingsLock = new Object();
 
-    private WebSettings.LayoutAlgorithm mLayoutAlgorithm =
-            WebSettings.LayoutAlgorithm.NARROW_COLUMNS;
+    private LayoutAlgorithm mLayoutAlgorithm = LayoutAlgorithm.NARROW_COLUMNS;
     private int mTextSizePercent = 100;
     private String mStandardFontFamily = "sans-serif";
     private String mFixedFontFamily = "monospace";
@@ -184,9 +182,7 @@ public class AwSettings {
                     while (mIsUpdateWebkitPrefsMessagePending) {
                         mAwSettingsLock.wait();
                     }
-                } catch (InterruptedException e) {
-                    Log.w(TAG, "updateWebkitPreferencesLocked interrupted", e);
-                }
+                } catch (InterruptedException e) {}
             }
         }
     }
@@ -1031,21 +1027,10 @@ public class AwSettings {
         return mJavaScriptCanOpenWindowsAutomatically;
     }
 
-    // A temporary adapter to avoid breaking code from Android frameworks/webview. crbug.com/332089
-    public void setLayoutAlgorithm(AwSettings.LayoutAlgorithm l) {
-        final WebSettings.LayoutAlgorithm[] webViewValues = {
-            WebSettings.LayoutAlgorithm.NORMAL,
-            WebSettings.LayoutAlgorithm.SINGLE_COLUMN,
-            WebSettings.LayoutAlgorithm.NARROW_COLUMNS,
-            WebSettings.LayoutAlgorithm.TEXT_AUTOSIZING
-        };
-        setLayoutAlgorithm(webViewValues[l.ordinal()]);
-    }
-
     /**
      * See {@link android.webkit.WebSettings#setLayoutAlgorithm}.
      */
-    public void setLayoutAlgorithm(WebSettings.LayoutAlgorithm l) {
+    public void setLayoutAlgorithm(LayoutAlgorithm l) {
         synchronized (mAwSettingsLock) {
             if (mLayoutAlgorithm != l) {
                 mLayoutAlgorithm = l;
@@ -1057,7 +1042,7 @@ public class AwSettings {
     /**
      * See {@link android.webkit.WebSettings#getLayoutAlgorithm}.
      */
-    public WebSettings.LayoutAlgorithm getLayoutAlgorithm() {
+    public LayoutAlgorithm getLayoutAlgorithm() {
         synchronized (mAwSettingsLock) {
             return mLayoutAlgorithm;
         }
@@ -1071,7 +1056,7 @@ public class AwSettings {
      */
     @CalledByNative
     private boolean getTextAutosizingEnabledLocked() {
-        return mLayoutAlgorithm == WebSettings.LayoutAlgorithm.TEXT_AUTOSIZING;
+        return mLayoutAlgorithm == LayoutAlgorithm.TEXT_AUTOSIZING;
     }
 
     /**
