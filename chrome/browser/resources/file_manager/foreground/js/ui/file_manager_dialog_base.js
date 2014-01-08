92cdc3b89031e0975f540d1f52e9df1914cd9447
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-08 12:15:49 +0000

    [Files.app] Remove OK and Cancel buttons in task picker dialog
    BUG=223574
    TEST=manually tested
    
    Review URL: https://codereview.chromium.org/108163005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243526 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/foreground/js/ui/file_manager_dialog_base.js
+++ chrome/browser/resources/file_manager/foreground/js/ui/file_manager_dialog_base.js
@@ -104,6 +104,7 @@ FileManagerDialogBase.prototype.showTitleOnlyDialog = function(title) {
  *     dialog failed to show due to an existing dialog.
  */
 FileManagerDialogBase.prototype.showTitleAndTextDialog = function(title, text) {
+  this.buttons.style.display = 'none';
   return this.showImpl_(title, text, null, null, null);
 };
 
