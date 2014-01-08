29ca4e2bf56fa09cd4acccb252416ddd35ae0b88
rpaquay@chromium.org <rpaquay@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-08 18:36:46 +0000

    Add more device names to the list of known devices.
    BUG=331831
    
    Review URL: https://codereview.chromium.org/118633003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243606 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/music_manager_private/device_id_linux.cc
+++ chrome/browser/extensions/api/music_manager_private/device_id_linux.cc
@@ -16,7 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 const char kDiskByUuidDirectoryName[] = "/dev/disk/by-uuid";
-const char* kDeviceNames[] = { "sda1", "hda1", "dm-0" };
+const char* kDeviceNames[] = {
+  "sda1", "hda1", "dm-0", "xvda1", "sda2", "hda2", "dm-1", "xvda2",
+};
 
 // Map from device name to disk uuid
 typedef std::map<base::FilePath, base::FilePath> DiskEntries;
