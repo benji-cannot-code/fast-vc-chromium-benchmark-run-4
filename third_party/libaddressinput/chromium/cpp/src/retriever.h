9e6bda5459b85bc90764b5016121a8977844d48e
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-08 00:10:23 +0000

    [rAc - libaddressinput] slay a Helper class.
    For better memory ownership.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/115523011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243440 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/libaddressinput/chromium/cpp/src/retriever.h
+++ third_party/libaddressinput/chromium/cpp/src/retriever.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <libaddressinput/util/basictypes.h>
 #include <libaddressinput/util/scoped_ptr.h>
 
+#include <map>
 #include <string>
 
 #include "lookup_key_util.h"
@@ -51,12 +52,28 @@ class Retriever {
   // Retrieves the data for |key| and invokes the |retrieved| callback. Checks
   // for the data in storage first. If storage does not have the data for |key|,
   // then downloads the data and places it in storage.
-  void Retrieve(const std::string& key, scoped_ptr<Callback> retrieved) const;
+  void Retrieve(const std::string& key, scoped_ptr<Callback> retrieved);
 
  private:
+  // Callback for when a rule is retrieved from |storage_|.
+  void OnDataRetrievedFromStorage(bool success,
+                                  const std::string& key,
+                                  const std::string& data);
+
+  // Callback for when a rule is retrieved by |downloader_|.
+  void OnDownloaded(bool success,
+                    const std::string& url,
+                    const std::string& data);
+
+  // Looks up the callback for |key| in |requests_|, removes it from the map
+  // and returns it. Assumes that |key| is in fact in |requests_|.
+  scoped_ptr<Callback> GetCallbackForKey(const std::string& key);
+
   const LookupKeyUtil lookup_key_util_;
   scoped_ptr<const Downloader> downloader_;
   scoped_ptr<Storage> storage_;
+  // Holds pending requests. The callback pointers are owned.
+  std::map<std::string, Callback*> requests_;
 
   DISALLOW_COPY_AND_ASSIGN(Retriever);
 };
