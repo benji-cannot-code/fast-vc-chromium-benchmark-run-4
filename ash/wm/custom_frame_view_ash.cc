81bea4794fb794600662e74f0d5effbf3304875b
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-08 22:44:37 +0000

    Fix regression where the active window header was not used for v2 apps on CrOS
    BUG=331343
    TEST=Manual, see bug
    
    Review URL: https://codereview.chromium.org/99553008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243677 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/custom_frame_view_ash.cc
+++ ash/wm/custom_frame_view_ash.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/font.h"
 #include "ui/gfx/rect.h"
+#include "ui/gfx/rect_conversions.h"
 #include "ui/gfx/size.h"
 #include "ui/views/view.h"
 #include "ui/views/widget/native_widget_aura.h"
@@ -161,11 +162,6 @@ class CustomFrameViewAsh::HeaderView
   virtual void Layout() OVERRIDE;
   virtual void OnPaint(gfx::Canvas* canvas) OVERRIDE;
 
-  // Sets whether the header should be painted as active.
-  void set_paint_as_active(bool paint_as_active) {
-    paint_as_active_ = paint_as_active;
-  }
-
   HeaderPainter* header_painter() {
     return header_painter_.get();
   }
@@ -197,9 +193,6 @@ class CustomFrameViewAsh::HeaderView
   // Keeps track of whether |maximize_bubble_| is still alive.
   scoped_ptr<views::WidgetDeletionObserver> maximize_bubble_lifetime_observer_;
 
-  // Whether the header should be painted as active.
-  bool paint_as_active_;
-
   // The fraction of the header's height which is visible while in fullscreen.
   // This value is meaningless when not in fullscreen.
   double fullscreen_visible_fraction_;
@@ -212,7 +205,6 @@ CustomFrameViewAsh::HeaderView::HeaderView(views::Widget* frame)
       header_painter_(new ash::HeaderPainter),
       caption_button_container_(NULL),
       maximize_bubble_(NULL),
-      paint_as_active_(false),
       fullscreen_visible_fraction_(0) {
   // Unfortunately, there is no views::WidgetDelegate::CanMinimize(). Assume
   // that the window frame can be minimized if it can be maximized.
@@ -273,7 +265,7 @@ void CustomFrameViewAsh::HeaderView::OnPaint(gfx::Canvas* canvas) {
   int theme_image_id = 0;
   if (frame_->IsMaximized() || frame_->IsFullscreen())
     theme_image_id = IDR_AURA_WINDOW_HEADER_BASE_MINIMAL;
-  else if (paint_as_active_)
+  else if (frame_->non_client_view()->frame_view()->ShouldPaintAsActive())
     theme_image_id = IDR_AURA_WINDOW_HEADER_BASE_ACTIVE;
   else
     theme_image_id = IDR_AURA_WINDOW_HEADER_BASE_INACTIVE;
@@ -478,8 +470,9 @@ gfx::Size CustomFrameViewAsh::GetMaximumSize() {
 void CustomFrameViewAsh::SchedulePaintInRect(const gfx::Rect& r) {
   // The HeaderView is not a child of CustomFrameViewAsh. Redirect the paint to
   // HeaderView instead.
-  header_view_->set_paint_as_active(ShouldPaintAsActive());
-  header_view_->SchedulePaint();
+  gfx::RectF to_paint(r);
+  views::View::ConvertRectToTarget(this, header_view_, &to_paint);
+  header_view_->SchedulePaintInRect(gfx::ToEnclosingRect(to_paint));
 }
 
 bool CustomFrameViewAsh::HitTestRect(const gfx::Rect& rect) const {
--- ui/views/widget/native_widget_aura.cc
+++ ui/views/widget/native_widget_aura.cc
@@ -880,8 +880,6 @@ void NativeWidgetAura::OnWindowActivated(aura::Window* gained_active,
       GetWidget()->GetFocusManager()->StoreFocusedView(true);
   }
   delegate_->OnNativeWidgetActivationChanged(window_ == gained_active);
-  if (IsVisible() && GetWidget()->non_client_view())
-    GetWidget()->non_client_view()->SchedulePaint();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
