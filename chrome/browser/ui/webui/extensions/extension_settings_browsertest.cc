c85f4f6b6b1012183e4d81a510f06bc464039d96
skuhne@chromium.org <skuhne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-08 02:14:14 +0000

    Removing GitDefaultProfile occurences from the remaining WebUI areas (mobile & test)
    BUG=322682
    TEST=covered by existing unit tests
    
    Review URL: https://codereview.chromium.org/113653004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243468 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/extensions/extension_settings_browsertest.cc
+++ chrome/browser/ui/webui/extensions/extension_settings_browsertest.cc
@@ -36,8 +36,8 @@ ExtensionSettingsUIBrowserTest::~ExtensionSettingsUIBrowserTest() {}
 
 Profile* ExtensionSettingsUIBrowserTest::GetProfile() {
   if (!profile_) {
-    profile_ =
-        browser() ? browser()->profile() : ProfileManager::GetDefaultProfile();
+    profile_ = browser() ? browser()->profile() :
+                           ProfileManager::GetActiveUserProfile();
   }
   return profile_;
 }
