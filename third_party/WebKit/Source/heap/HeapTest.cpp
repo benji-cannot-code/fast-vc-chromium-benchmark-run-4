eac4a84710466af9961351e98b5c55bfe17d5526
wibling@chromium.org <wibling@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-08 14:29:54 +0000

    Add support for typed heaps on trunk.
    With oilpan we support segregating allocation into different heap pages based on the type. This change adds the remaining code to trunk to support this.
    There is a simple test which allocates two small objects and checks they are not on the same heap page. Given that the allocation is of small objects they would have been on the same page in the case where there is no segregation.
    
    I also added the isMarked method to the visitor and modified a test to use it. It was mainly to match what was supported for typed heaps.
    
    R=ager@chromium.org, cevans@chromium.org, erik.corry@gmail.com, haraken@chromium.org, vegorov@chromium.org, zerny@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/128103002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164684 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/heap/HeapTest.cpp
+++ third_party/WebKit/Source/heap/HeapTest.cpp
@@ -37,9 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <gtest/gtest.h>
 
-using namespace WebCore;
-
-namespace {
+namespace WebCore {
 
 class HeapAllocatedArray : public GarbageCollected<HeapAllocatedArray> {
     DECLARE_GC_INFO
@@ -213,7 +211,16 @@ public:
         return new ClassWithMember();
     }
 
-    void trace(Visitor* visitor) { visitor->trace(m_traceCounter); }
+    void trace(Visitor* visitor)
+    {
+        EXPECT_TRUE(visitor->isMarked(this));
+        if (!traceCount())
+            EXPECT_FALSE(visitor->isMarked(m_traceCounter));
+        else
+            EXPECT_TRUE(visitor->isMarked(m_traceCounter));
+
+        visitor->trace(m_traceCounter);
+    }
 
     int traceCount() { return m_traceCounter->traceCount(); }
 
@@ -245,6 +252,38 @@ TEST(HeapTest, SimplePersistent)
     Heap::collectGarbage(ThreadState::NoHeapPointersOnStack);
     EXPECT_EQ(1, classWithMember->traceCount());
     EXPECT_EQ(2, traceCounter->traceCount());
+
+    Heap::shutdown();
+}
+
+class TestTypedHeapClass : public GarbageCollected<TestTypedHeapClass> {
+    DECLARE_GC_INFO
+public:
+    static TestTypedHeapClass* create()
+    {
+        return new TestTypedHeapClass();
+    }
+
+    void trace(Visitor*) { }
+
+private:
+    TestTypedHeapClass() { }
+};
+
+DEFINE_GC_INFO(TestTypedHeapClass);
+
+TEST(HeapTest, TypedHeapSanity)
+{
+    // FIXME: init and shutdown should be called via Blink
+    // initialization in the test runner.
+    Heap::init(0);
+
+    // We use TraceCounter for allocating an object on the general heap.
+    Persistent<TraceCounter> generalHeapObject = TraceCounter::create();
+    Persistent<TestTypedHeapClass> typedHeapObject = TestTypedHeapClass::create();
+
+    EXPECT_NE(pageHeaderAddress(reinterpret_cast<Address>(typedHeapObject.raw())),
+        pageHeaderAddress(reinterpret_cast<Address>(generalHeapObject.raw())));
 
     Heap::shutdown();
 }
