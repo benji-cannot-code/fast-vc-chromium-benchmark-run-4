6af5ca2fb5684379b34bf19de9256fefd4ec26f3
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-08 00:28:38 +0000

    GN: Attach the scope provider to imported files.
    This enables imports to use the built-in programatic variables like root_build_dir.
    
    R=scottmg@chromium.org, scottmg
    
    Review URL: https://codereview.chromium.org/126033006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243444 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/gn/import_manager.cc
+++ tools/gn/import_manager.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util.h"
 #include "tools/gn/parse_tree.h"
 #include "tools/gn/scheduler.h"
+#include "tools/gn/scope_per_file_provider.h"
 
 namespace {
 
@@ -25,6 +26,7 @@ Scope* UncachedImport(const Settings* settings,
   CHECK(block);
 
   scoped_ptr<Scope> scope(new Scope(settings->base_config()));
+  ScopePerFileProvider per_file_provider(scope.get());
   scope->set_source_dir(file.GetDir());
   scope->SetProcessingImport();
   block->ExecuteBlockInScope(scope.get(), err);
