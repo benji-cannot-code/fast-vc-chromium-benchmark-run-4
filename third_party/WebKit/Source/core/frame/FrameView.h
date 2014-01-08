f020ed65185199a1762ce103825925ec35a9c536
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-08 00:46:36 +0000

    Revert "Perform layout when adding or removing custom scrollbars"
    This reverts r164352 and r164548. The former tried to rebaseline
    a reftest, which is not something that makes sense.
    
    TBR=ccameron@chromium.org,adamk@chromium.org
    
    Review URL: https://codereview.chromium.org/126943002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164640 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/FrameView.h
+++ third_party/WebKit/Source/core/frame/FrameView.h
@@ -437,7 +437,6 @@ private:
     void scrollPositionChanged();
 
     bool hasCustomScrollbars() const;
-    bool shouldUseCustomScrollbars(Element*& customScrollbarElement, Frame*& customScrollbarFrame);
 
     virtual void updateScrollCorner();
 
