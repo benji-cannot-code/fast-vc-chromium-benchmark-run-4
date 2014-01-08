e294be366804f7de66330a82e102dab0344a8a48
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-08 22:55:51 +0000

    Clean up ChildProcessHost unique id generation.
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/126033004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243683 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/signin/signin_manager.cc
+++ chrome/browser/signin/signin_manager.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/render_process_host.h"
+#include "content/public/common/child_process_host.h"
 #include "google_apis/gaia/gaia_auth_fetcher.h"
 #include "google_apis/gaia/gaia_auth_util.h"
 #include "google_apis/gaia/gaia_constants.h"
@@ -45,13 +46,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using namespace signin_internals_util;
 
 using content::BrowserThread;
+using content::ChildProcessHost;
 
 namespace {
 
 const char kGetInfoDisplayEmailKey[] = "displayEmail";
 const char kGetInfoEmailKey[] = "email";
-
-const int kInvalidProcessId = -1;
 
 const char kChromiumSyncService[] = "service=chromiumsync";
 
@@ -87,16 +87,17 @@ SigninManager::SigninManager(scoped_ptr<SigninManagerDelegate> delegate)
       had_two_factor_error_(false),
       type_(SIGNIN_TYPE_NONE),
       weak_pointer_factory_(this),
-      signin_process_id_(kInvalidProcessId),
+      signin_host_id_(ChildProcessHost::kInvalidUniqueID),
       delegate_(delegate.Pass()) {
 }
 
 void SigninManager::SetSigninProcess(int process_id) {
-  if (process_id == signin_process_id_)
+  if (process_id == signin_host_id_)
     return;
-  DLOG_IF(WARNING, signin_process_id_ != kInvalidProcessId) <<
-      "Replacing in-use signin process.";
-  signin_process_id_ = process_id;
+  DLOG_IF(WARNING,
+          signin_host_id_ != ChildProcessHost::kInvalidUniqueID)
+      << "Replacing in-use signin process.";
+  signin_host_id_ = process_id;
   const content::RenderProcessHost* process =
       content::RenderProcessHost::FromID(process_id);
   DCHECK(process);
@@ -106,15 +107,15 @@ void SigninManager::SetSigninProcess(int process_id) {
 }
 
 void SigninManager::ClearSigninProcess() {
-  signin_process_id_ = kInvalidProcessId;
+  signin_host_id_ = ChildProcessHost::kInvalidUniqueID;
 }
 
 bool SigninManager::IsSigninProcess(int process_id) const {
-  return process_id == signin_process_id_;
+  return process_id == signin_host_id_;
 }
 
 bool SigninManager::HasSigninProcess() const {
-  return signin_process_id_ != kInvalidProcessId;
+  return signin_host_id_ != ChildProcessHost::kInvalidUniqueID;
 }
 
 SigninManager::~SigninManager() {
@@ -607,14 +608,14 @@ void SigninManager::Observe(int type,
 
   // It's possible we're listening to a "stale" renderer because it was
   // replaced with a new process by process-per-site. In either case,
-  // stop listening to it, but only reset signin_process_id_ tracking
+  // stop listening to it, but only reset signin_host_id_ tracking
   // if this was from the current signin process.
   registrar_.Remove(this,
                     content::NOTIFICATION_RENDERER_PROCESS_TERMINATED,
                     source);
-  if (signin_process_id_ ==
+  if (signin_host_id_ ==
       content::Source<content::RenderProcessHost>(source)->GetID()) {
-    signin_process_id_ = kInvalidProcessId;
+    signin_host_id_ = ChildProcessHost::kInvalidUniqueID;
   }
 }
 
--- content/browser/dom_storage/dom_storage_namespace.cc
+++ content/browser/dom_storage/dom_storage_namespace.cc
@@ -17,8 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/dom_storage/dom_storage_context_impl.h"
 #include "content/browser/dom_storage/dom_storage_task_runner.h"
 #include "content/browser/dom_storage/session_storage_database.h"
-#include "content/common/child_process_host_impl.h"
 #include "content/common/dom_storage/dom_storage_types.h"
+#include "content/public/common/child_process_host.h"
 
 namespace content {
 
@@ -248,14 +248,14 @@ DOMStorageNamespace::GetAreaHolder(const GURL& origin) {
 }
 
 void DOMStorageNamespace::AddTransactionLogProcessId(int process_id) {
-  DCHECK(process_id != ChildProcessHostImpl::kInvalidChildProcessId);
+  DCHECK(process_id != ChildProcessHost::kInvalidUniqueID);
   DCHECK(transactions_.count(process_id) == 0);
   TransactionData* transaction_data = new TransactionData;
   transactions_[process_id] = transaction_data;
 }
 
 void DOMStorageNamespace::RemoveTransactionLogProcessId(int process_id) {
-  DCHECK(process_id != ChildProcessHostImpl::kInvalidChildProcessId);
+  DCHECK(process_id != ChildProcessHost::kInvalidUniqueID);
   DCHECK(transactions_.count(process_id) == 1);
   delete transactions_[process_id];
   transactions_.erase(process_id);
@@ -354,7 +354,7 @@ SessionStorageNamespace::MergeResult DOMStorageNamespace::Merge(
 }
 
 bool DOMStorageNamespace::IsLoggingRenderer(int process_id) {
-  DCHECK(process_id != ChildProcessHostImpl::kInvalidChildProcessId);
+  DCHECK(process_id != ChildProcessHost::kInvalidUniqueID);
   if (transactions_.count(process_id) < 1)
     return false;
   return !transactions_[process_id]->max_log_size_exceeded;
