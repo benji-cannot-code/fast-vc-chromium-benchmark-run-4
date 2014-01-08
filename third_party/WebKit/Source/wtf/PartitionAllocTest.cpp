f25b5862d3789eca5624c232c6273b8b995f627f
cevans@chromium.org <cevans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-08 03:00:05 +0000

    PartitionAlloc: move seedPage and similar items to a global variable.
    (This is a part of re-landing the reviewed but reverted
    https://codereview.chromium.org/105503009/
    in incremental pieces.)
    
    TBR=tsepez@chromium.org
    BUG=327095
    
    Review URL: https://codereview.chromium.org/127323002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164654 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/PartitionAllocTest.cpp
+++ third_party/WebKit/Source/wtf/PartitionAllocTest.cpp
@@ -103,7 +103,7 @@ static WTF::PartitionPage* GetFullPage(size_t size)
     EXPECT_EQ(numSlots, static_cast<size_t>(bucket->activePagesHead->numAllocatedSlots));
     EXPECT_EQ(0, partitionPageFreelistHead(bucket->activePagesHead));
     EXPECT_TRUE(bucket->activePagesHead);
-    EXPECT_TRUE(bucket->activePagesHead != &allocator.root()->seedPage);
+    EXPECT_TRUE(bucket->activePagesHead != &WTF::PartitionRootGeneric::gSeedPage);
     return bucket->activePagesHead;
 }
 
@@ -125,7 +125,7 @@ TEST(WTF_PartitionAlloc, Basic)
 {
     TestSetup();
     WTF::PartitionBucket* bucket = &allocator.root()->buckets()[kTestBucketIndex];
-    WTF::PartitionPage* seedPage = &allocator.root()->seedPage;
+    WTF::PartitionPage* seedPage = &WTF::PartitionRootGeneric::gSeedPage;
 
     EXPECT_FALSE(bucket->freePagesHead);
     EXPECT_EQ(seedPage, bucket->activePagesHead);
