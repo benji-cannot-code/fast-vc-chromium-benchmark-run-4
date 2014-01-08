cd9c7df31c1bfb8be2353aa0b28ea78a16c59138
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-08 19:05:34 +0000

    Tweaks to make compiler happy
    For some reason stl for android has problems with these lines,
    tweaking for loop makes the compiler happy.
    
    BUG=none
    TEST=none
    R=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/127043004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243621 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/window.cc
+++ ui/aura/window.cc
@@ -1190,8 +1190,8 @@ void Window::StackChildLayerRelativeTo(Window* child,
 
   if (!target_layer) {
     if (direction == STACK_ABOVE) {
-      for (Layers::const_reverse_iterator i = layers.rbegin();
-           i != layers.rend(); ++i) {
+      for (Layers::const_reverse_iterator i = layers.rbegin(),
+               rend = layers.rend(); i != rend; ++i) {
         ancestor_layer->StackAtBottom(*i);
       }
     } else {
@@ -1202,8 +1202,8 @@ void Window::StackChildLayerRelativeTo(Window* child,
   }
 
   if (direction == STACK_ABOVE) {
-    for (Layers::const_reverse_iterator i = layers.rbegin();
-         i != layers.rend(); ++i) {
+    for (Layers::const_reverse_iterator i = layers.rbegin(),
+             rend = layers.rend(); i != rend; ++i) {
       ancestor_layer->StackAbove(*i, target_layer);
     }
   } else {
