a729a07ff45d994ade95ffabd769eca63364bf69
fs@opera.com <fs@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-08 17:30:41 +0000

    Support colors w/ alpha in SVG 'stop' elements and 'feFlood'
    When a color w/ alpha is specified (via rgba(...)) as stop-color on
    a SVG 'stop' element, or as flood-color (or equivalent) on 'feFlood',
    then modulate the alpha-channel with the stop-opacity or flood-opacity
    respectively to produce the final color to use.
    
    BUG=331516
    
    Review URL: https://codereview.chromium.org/126033003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164690 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGStopElement.cpp
+++ third_party/WebKit/Source/core/svg/SVGStopElement.cpp
@@ -116,7 +116,7 @@ Color SVGStopElement::stopColorIncludingOpacity() const
         return Color(Color::transparent, true); // Transparent black.
 
     const SVGRenderStyle* svgStyle = style->svgStyle();
-    return colorWithOverrideAlpha(svgStyle->stopColor().rgb(), svgStyle->stopOpacity());
+    return svgStyle->stopColor().combineWithAlpha(svgStyle->stopOpacity());
 }
 
 }
