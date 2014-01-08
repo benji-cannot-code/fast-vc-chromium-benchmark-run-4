70d4e9dd5a3078671f165fd3701cf9ef8403cf04
dpolukhin@chromium.org <dpolukhin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-08 22:45:00 +0000

    Test that Guest profile is Incognito
    Follow up test for https://codereview.chromium.org/101413011
    
    BUG=329498
    TEST=unit_tests
    
    Review URL: https://codereview.chromium.org/115073003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243678 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/settings/device_settings_service.cc
+++ chrome/browser/chromeos/settings/device_settings_service.cc
@@ -368,6 +368,8 @@ ScopedTestDeviceSettingsService::ScopedTestDeviceSettingsService() {
 }
 
 ScopedTestDeviceSettingsService::~ScopedTestDeviceSettingsService() {
+  // Clean pending operations.
+  DeviceSettingsService::Get()->UnsetSessionManager();
   DeviceSettingsService::Shutdown();
 }
 
