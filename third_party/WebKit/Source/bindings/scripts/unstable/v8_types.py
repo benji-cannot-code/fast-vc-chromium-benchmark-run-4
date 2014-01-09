d2bbeaf30e0528e9a7c7dfe64996b7baf579ea14
nbarth@chromium.org <nbarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-09 10:46:34 +0000

    IDL compiler: EventHandler attributes in Node interfaces
    Add test file for Node interfaces.
    Also simplification of the hard-coded DOM Node list;
    I should be able to eliminate this in Python thanks to interfaces_info
    (in followup).
    
    BUG=239771
    R=haraken
    
    Review URL: https://codereview.chromium.org/131013002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164761 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/scripts/unstable/v8_types.py
+++ third_party/WebKit/Source/bindings/scripts/unstable/v8_types.py
@@ -179,6 +179,7 @@ DOM_NODE_TYPES = set([
     'SVGDocument',
     'Text',
     'TestNode',
+    'TestInterfaceNode',
 ])
 NON_WRAPPER_TYPES = set([
     'CompareHow',
@@ -208,6 +209,7 @@ def constructor_type(idl_type):
 
 
 def is_dom_node_type(idl_type):
+    # FIXME: replace with checking inheritance
     return (idl_type in DOM_NODE_TYPES or
             (idl_type.startswith(('HTML', 'SVG')) and
              idl_type.endswith('Element')))
