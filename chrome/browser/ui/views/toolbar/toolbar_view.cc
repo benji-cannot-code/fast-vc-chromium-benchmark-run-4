c70e8b785353a20853c61b483409a67f38ec1748
calamity@chromium.org <calamity@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-09 00:12:37 +0000

    Fix truncated location bar on maximized streamlined hosted apps.
    Streamlined Hosted App windows were having their location bar truncated
    when maximized. This was due to using the popup-style location bar to
    get the omnibox to be read-only.
    
    This CL changes streamlined hosted apps to use the normal location bar
    and sets the omnibox to be read only at a deeper level.
    
    BUG=318607
    
    Review URL: https://codereview.chromium.org/110413007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243705 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/toolbar/toolbar_view.cc
+++ chrome/browser/ui/views/toolbar/toolbar_view.cc
@@ -104,11 +104,6 @@ int GetButtonSpacing() {
       ToolbarView::kStandardSpacing : 0;
 }
 
-bool IsStreamlinedHostedAppsEnabled() {
-  return CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kEnableStreamlinedHostedApps);
-}
-
 }  // namespace
 
 // static
@@ -137,7 +132,8 @@ ToolbarView::ToolbarView(Browser* browser)
 
   display_mode_ = DISPLAYMODE_LOCATION;
   if (browser->SupportsWindowFeature(Browser::FEATURE_TABSTRIP) ||
-      (browser->is_app() && IsStreamlinedHostedAppsEnabled()))
+      (browser->is_app() && CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kEnableStreamlinedHostedApps)))
     display_mode_ = DISPLAYMODE_NORMAL;
 
   registrar_.Add(this, chrome::NOTIFICATION_UPGRADE_RECOMMENDED,
@@ -194,8 +190,7 @@ void ToolbarView::Init() {
   location_bar_ = new LocationBarView(
       browser_, browser_->profile(),
       browser_->command_controller()->command_updater(), this,
-      display_mode_ == DISPLAYMODE_LOCATION ||
-          (browser_->is_app() && IsStreamlinedHostedAppsEnabled()));
+      display_mode_ == DISPLAYMODE_LOCATION);
 
   reload_ = new ReloadButton(location_bar_,
                              browser_->command_controller()->command_updater());
