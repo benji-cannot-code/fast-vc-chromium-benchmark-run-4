ff651b44dc55816b91abf1e2aeab86e67f2c3d75
dominicc@chromium.org <dominicc@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-09 02:40:59 +0000

    Remove Node.prefix so it's no longer visible to script.
    This is in line with DOM4 <http://dom.spec.whatwg.org/#node>
    
    prefix remains on Attr and Element, which is the only place it does
    something useful, although setting prefixes will probably be removed
    entirely.
    
    BUG=325922
    
    Review URL: https://codereview.chromium.org/126713004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164729 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Element.cpp
+++ third_party/WebKit/Source/core/dom/Element.cpp
@@ -74,6 +74,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/frame/ContentSecurityPolicy.h"
 #include "core/frame/Frame.h"
 #include "core/frame/FrameView.h"
+#include "core/frame/UseCounter.h"
 #include "core/html/ClassList.h"
 #include "core/html/HTMLCollection.h"
 #include "core/html/HTMLDocument.h"
@@ -1233,6 +1234,8 @@ String Element::nodeNamePreservingCase() const
 
 void Element::setPrefix(const AtomicString& prefix, ExceptionState& exceptionState)
 {
+    UseCounter::count(document(), UseCounter::ElementSetPrefix);
+
     checkSetPrefix(prefix, exceptionState);
     if (exceptionState.hadException())
         return;
