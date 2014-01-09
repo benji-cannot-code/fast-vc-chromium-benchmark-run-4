1156a80d8e74323e9ddb232e23226e9580edaa59
akuegel@chromium.org <akuegel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-09 12:01:48 +0000

    Add error handling for supervised user import flow.
    Add a check if the user tries to create a profile with the
    name of an already existing supervised user. In that case,
    the user is not allowed to create a new such supervised user
    profile, instead he can directly import the existing supervised
    user.
    A screenshot can be found in the bug description.
    
    BUG=282464
    TEST=browser_tests
    
    Review URL: https://codereview.chromium.org/125993002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243855 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/managed_user_import.js
+++ chrome/browser/resources/options/managed_user_import.js
@@ -76,7 +76,8 @@ cr.define('options', function() {
      * @override
      */
     didShowPage: function() {
-      chrome.send('requestManagedUserImportUpdate');
+      options.ManagedUserListData.requestExistingManagedUsers(
+          this.receiveExistingManagedUsers_, this.onSigninError_.bind(this));
 
       this.updateImportInProgress_(false);
       $('managed-user-import-error-bubble').hidden = true;
@@ -120,6 +121,7 @@ cr.define('options', function() {
       // 'createProfile' is handled by CreateProfileHandler.
       chrome.send('createProfile', [managedUser.name, avatarUrl,
                                     false, true, managedUser.id]);
+      options.ManagedUserListData.reloadExistingManagedUsers();
     },
 
     /**
@@ -158,8 +160,7 @@ cr.define('options', function() {
     },
 
     /**
-     * Adds all the existing |managedUsers| to the list. If |managedUsers|
-     * is undefined, then the list is cleared.
+     * Sets the data model of the managed user list to |managedUsers|.
      * @param {Array.<Object>} managedUsers An array of managed user objects.
      *     Each object is of the form:
      *       managedUser = {
@@ -172,11 +173,6 @@ cr.define('options', function() {
      * @private
      */
     receiveExistingManagedUsers_: function(managedUsers) {
-      if (!managedUsers) {
-        $('managed-user-list').dataModel = null;
-        return;
-      }
-
       managedUsers.sort(function(a, b) {
         return a.name.localeCompare(b.name);
       });
@@ -185,14 +181,15 @@ cr.define('options', function() {
       if (managedUsers.length == 0) {
         this.onError_(loadTimeData.getString('noExistingManagedUsers'));
         $('managed-user-import-ok').disabled = true;
+      } else {
+        // Hide the error bubble.
+        $('managed-user-import-error-bubble').hidden = true;
       }
     },
 
-    /**
-     * @private
-     */
-    hideErrorBubble_: function() {
-      $('managed-user-import-error-bubble').hidden = true;
+    onSigninError_: function() {
+      $('managed-user-list').dataModel = null;
+      this.onError_(loadTimeData.getString('managedUserImportSigninError'));
     },
 
     /**
@@ -221,10 +218,7 @@ cr.define('options', function() {
 
   // Forward public APIs to private implementations.
   [
-    'hideErrorBubble',
-    'onError',
     'onSuccess',
-    'receiveExistingManagedUsers',
   ].forEach(function(name) {
     ManagedUserImportOverlay[name] = function() {
       var instance = ManagedUserImportOverlay.getInstance();
