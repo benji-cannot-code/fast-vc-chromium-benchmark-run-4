1a381ea3ec7c6528ebc15d42c28d182baac54628
varkha@chromium.org <varkha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-09 20:24:23 +0000

    Do not show auto-hidden shelf when dragging a window
    BUG=292238
    TEST=ash_unittests --gtest_filter=*ShelfLayoutManagerTest*
    
    Review URL: https://codereview.chromium.org/23591049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243964 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/mru_window_tracker.cc
+++ ash/wm/mru_window_tracker.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/shell_window_ids.h"
 #include "ash/wm/window_cycle_list.h"
+#include "ash/wm/window_state.h"
 #include "ash/wm/window_util.h"
 #include "ash/wm/workspace_controller.h"
 #include "ui/aura/client/activation_client.h"
@@ -30,6 +31,19 @@ void AddTrackedWindows(aura::Window* root,
   aura::Window* container = Shell::GetContainer(root, container_id);
   const MruWindowTracker::WindowList& children(container->children());
   windows->insert(windows->end(), children.begin(), children.end());
+}
+
+// Adds windows being dragged in the docked container to |windows| list.
+void AddDraggedWindows(aura::Window* root,
+                       MruWindowTracker::WindowList* windows) {
+  aura::Window* container = Shell::GetContainer(
+      root, internal::kShellWindowId_DockedContainer);
+  const MruWindowTracker::WindowList& children = container->children();
+  for (MruWindowTracker::WindowList::const_iterator iter = children.begin();
+       iter != children.end(); ++iter) {
+    if (wm::GetWindowState(*iter)->is_dragged())
+      windows->insert(windows->end(), *iter);
+  }
 }
 
 // Returns true if |window| is a container whose windows can be cycled to.
@@ -73,6 +87,10 @@ MruWindowTracker::WindowList BuildWindowListInternal(
   // in the active root window becomes the front of the list.
   for (size_t i = 0; i < kSwitchableWindowContainerIdsLength; ++i)
     AddTrackedWindows(active_root, kSwitchableWindowContainerIds[i], &windows);
+
+  // Dragged windows are temporarily parented in docked container. Include them
+  // in the in tracked list.
+  AddDraggedWindows(active_root, &windows);
 
   // Removes unfocusable windows.
   MruWindowTracker::WindowList::iterator last =
