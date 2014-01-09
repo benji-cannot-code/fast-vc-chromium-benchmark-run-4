4447d16a64b87b01fcf39db4abdc7ea6800ccc03
davve@opera.com <davve@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-09 16:39:56 +0000

    Inhibit title updates when removing children before setting new value
    In <https://codereview.chromium.org/107513013> children to <title>
    were removed before setting the new title child. This caused one extra
    title change notification to the empty string before the new value was
    set.
    
    Avoid the extra notification by ignoring title updates when children
    of HTMLTitleElement change until the final appendChild with the new
    text node.
    
    Also make sure to add a child list mutation scope for the update so
    that both the removal and addition ends up in the same mutation. Add
    test for this.
    
    BUG=331351
    
    Review URL: https://codereview.chromium.org/128603002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164789 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -1306,13 +1306,10 @@ void Document::setTitle(const String& title)
         }
     }
 
-    if (m_titleElement)
-        m_titleElement->removeChildren();
-
-    updateTitle(title);
-
     if (m_titleElement && m_titleElement->hasTagName(titleTag))
         toHTMLTitleElement(m_titleElement)->setText(title);
+    else
+        updateTitle(title);
 }
 
 void Document::setTitleElement(const String& title, Element* titleElement)
--- third_party/WebKit/Source/core/html/HTMLTitleElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLTitleElement.cpp
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "HTMLNames.h"
 #include "bindings/v8/ExceptionStatePlaceholder.h"
+#include "core/dom/ChildListMutationScope.h"
 #include "core/dom/Document.h"
 #include "core/dom/Text.h"
 #include "core/rendering/style/RenderStyle.h"
@@ -38,6 +39,7 @@ using namespace HTMLNames;
 
 inline HTMLTitleElement::HTMLTitleElement(Document& document)
     : HTMLElement(titleTag, document)
+    , m_ignoreTitleUpdatesWhenChildrenChange(false)
 {
     setHasCustomStyleCallbacks();
     ScriptWrappable::init(this);
@@ -66,7 +68,7 @@ void HTMLTitleElement::removedFrom(ContainerNode* insertionPoint)
 void HTMLTitleElement::childrenChanged(bool changedByParser, Node* beforeChange, Node* afterChange, int childCountDelta)
 {
     HTMLElement::childrenChanged(changedByParser, beforeChange, afterChange, childCountDelta);
-    if (inDocument() && !isInShadowTree())
+    if (inDocument() && !isInShadowTree() && !m_ignoreTitleUpdatesWhenChildrenChange)
         document().setTitleElement(text(), this);
 }
 
@@ -85,22 +87,14 @@ String HTMLTitleElement::text() const
 void HTMLTitleElement::setText(const String &value)
 {
     RefPtr<Node> protectFromMutationEvents(this);
+    ChildListMutationScope mutation(*this);
 
-    int numChildren = childNodeCount();
+    // Avoid calling Document::setTitleElement() during intermediate steps.
+    m_ignoreTitleUpdatesWhenChildrenChange = true;
+    removeChildren();
+    m_ignoreTitleUpdatesWhenChildrenChange = false;
 
-    if (numChildren == 1 && firstChild()->isTextNode())
-        toText(firstChild())->setData(value);
-    else {
-        // We make a copy here because entity of "value" argument can be Document::m_title,
-        // which goes empty during removeChildren() invocation below,
-        // which causes HTMLTitleElement::childrenChanged(), which ends up in Document::setTitleElement().
-        String valueCopy(value);
-
-        if (numChildren > 0)
-            removeChildren();
-
-        appendChild(document().createTextNode(valueCopy.impl()), IGNORE_EXCEPTION);
-    }
+    appendChild(document().createTextNode(value.impl()), IGNORE_EXCEPTION);
 }
 
 }
