5d6a1ff65e5c22f9f704a15d597ee5c6d09dab53
skyostil@chromium.org <skyostil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-09 22:15:40 +0000

    telemetry: Use trace categories to configure synthetic delays
    BUG=307841
    TEST=tools/perf/run_benchmark scheduler.tough_scheduling_cases
    
    Review URL: https://codereview.chromium.org/103143007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243993 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/perf/benchmarks/scheduler.py
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from telemetry import test
+from measurements import smoothness
+
+
+class SchedulerToughSchedulingCases(test.Test):
+  """Measures rendering statistics while interacting with pages that have
+  challenging scheduling properties.
+
+  https://docs.google.com/a/chromium.org/document/d/
+      17yhE5Po9By0sCdM1yZT3LiUECaUr_94rQt9j-4tOQIM/view"""
+  test = smoothness.Smoothness
+  page_set = 'page_sets/tough_scheduling_cases.json'
+
