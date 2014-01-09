f61e29fb37835bd7c4e12aa88896f21ea7b563a7
tengs@chromium.org <tengs@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-09 22:27:42 +0000

    Unregister Bluetooth profile when the extension that added it is unloaded.
    BUG=332177
    TEST=manually and new unit test
    
    Review URL: https://codereview.chromium.org/128523003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244000 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/bluetooth/bluetooth_api.cc
+++ chrome/browser/extensions/api/bluetooth/bluetooth_api.cc
@@ -195,7 +195,8 @@ void BluetoothAddProfileFunction::OnProfileRegistered(
                  base::Unretained(GetEventRouter(GetProfile())),
                  extension_id(),
                  uuid_));
-  GetEventRouter(GetProfile())->AddProfile(uuid_, bluetooth_profile);
+  GetEventRouter(GetProfile())->AddProfile(
+      uuid_, extension_id(), bluetooth_profile);
   SendResponse(true);
 }
 
