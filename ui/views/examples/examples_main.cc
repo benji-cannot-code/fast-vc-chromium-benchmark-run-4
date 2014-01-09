2f6a58946f0b15503caaf1673c8c24a25a491f14
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-09 14:33:53 +0000

    Set up more pieces necessary to show the main Window for views_examples_exe.
    We need to initialize ResourceBundle otherwise we can't call
    views::CreateDesktopScreen().
    
    We will also need a gfx::Screen, otherwise we won't have any surface to
    show any thing.
    
    BUG=119759,139313,141187
    TEST=views_examples_exe runs and does not crash.
    R=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/108513015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243898 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/examples/examples_main.cc
+++ ui/views/examples/examples_main.cc
@@ -5,16 +5,39 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/at_exit.h"
 #include "base/command_line.h"
+#include "base/files/file_path.h"
 #include "base/message_loop/message_loop.h"
+#include "base/path_service.h"
 #include "base/run_loop.h"
+#include "ui/base/resource/resource_bundle.h"
+#include "ui/gfx/screen.h"
+
+#if !defined(OS_CHROMEOS)
+#include "ui/views/widget/desktop_aura/desktop_screen.h"
+#endif
 
 int main(int argc, char** argv) {
-  base::AtExitManager at_exit;
   CommandLine::Init(argc, argv);
 
+  base::AtExitManager at_exit;
   base::MessageLoopForUI message_loop;
 
+  base::FilePath pak_dir;
+  PathService::Get(base::DIR_MODULE, &pak_dir);
+
+  base::FilePath pak_file;
+  pak_file = pak_dir.Append(FILE_PATH_LITERAL("ui_test.pak"));
+
+  ui::ResourceBundle::InitSharedInstanceWithPakPath(pak_file);
+
+#if !defined(OS_CHROMEOS)
+  scoped_ptr<gfx::Screen> desktop_screen(views::CreateDesktopScreen());
+  gfx::Screen::SetScreenInstance(gfx::SCREEN_TYPE_NATIVE, desktop_screen.get());
+#endif
+
   base::RunLoop().Run();
+
+  ui::ResourceBundle::CleanupSharedInstance();
 
   return 0;
 }
--- ui/views/views.gyp
+++ ui/views/views.gyp
@@ -962,6 +962,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'type': 'executable',
       'dependencies': [
         '../../base/base.gyp:base',
+        '../gfx/gfx.gyp:gfx',
+        '../resources/ui_resources.gyp:ui_test_pak',
+        '../ui.gyp:ui',
+        'views',
       ],
       'sources': [
         'examples/examples_main.cc',
