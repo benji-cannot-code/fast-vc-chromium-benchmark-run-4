5bc9f96d3fcfc6efcd3662e8e9105bb4649b0657
noamsml@google.com <noamsml@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-09 22:56:35 +0000

    Revert 243980 "Docserver: Support markdown for HTML content."
    > Docserver: Support markdown for HTML content.
    > 
    > R=kalman@chromium.org
    > BUG=293681
    > 
    > Review URL: https://codereview.chromium.org/93743005
    
    TBR=hukun@chromium.org
    
    Review URL: https://codereview.chromium.org/132753002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244010 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/server2/content_provider.py
+++ chrome/common/extensions/docs/server2/content_provider.py
@@ -4,15 +4,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 import mimetypes
-import posixpath
+import os
 
 from compiled_file_system import SingleFile
 from directory_zipper import DirectoryZipper
 from docs_server_utils import ToUnicode
-from file_system import FileNotFoundError
 from future import Gettable, Future
 from third_party.handlebar import Handlebar
-from third_party.markdown import markdown
 
 
 class ContentAndType(object):
@@ -58,15 +56,7 @@ class ContentProvider(object):
   def _CompileContent(self, path, text):
     assert text is not None, path
     mimetype = mimetypes.guess_type(path)[0]
-    if posixpath.splitext(path)[1] == '.md':
-      # See http://pythonhosted.org/Markdown/extensions
-      # for details on "extensions=".
-      content = markdown(ToUnicode(text),
-                         extensions=('extra', 'headerid', 'sane_lists'))
-      if self._supports_templates:
-        content = Handlebar(content, name=path)
-      mimetype = 'text/html'
-    elif mimetype is None:
+    if mimetype is None:
       content = text
       mimetype = 'text/plain'
     elif mimetype == 'text/html':
@@ -80,24 +70,9 @@ class ContentProvider(object):
       content = text
     return ContentAndType(content, mimetype)
 
-  def _MaybeMarkdown(self, path):
-    if posixpath.splitext(path)[1] != '.html':
-      return path
-
-    dirname, file_name = posixpath.split(path)
-    if dirname != '':
-      dirname = dirname + '/'
-    file_list = self.file_system.ReadSingle(dirname).Get()
-    if file_name in file_list:
-      return path
-
-    if posixpath.splitext(file_name)[0] + '.md' in file_list:
-      return posixpath.splitext(path)[0] + '.md'
-    return path
-
   def GetContentAndType(self, path):
     path = path.lstrip('/')
-    base, ext = posixpath.splitext(path)
+    base, ext = os.path.splitext(path)
 
     # Check for a zip file first, if zip is enabled.
     if self._directory_zipper and ext == '.zip':
@@ -105,7 +80,7 @@ class ContentProvider(object):
       return Future(delegate=Gettable(
           lambda: ContentAndType(zip_future.Get(), 'application/zip')))
 
-    return self._content_cache.GetFromFile(self._MaybeMarkdown(path))
+    return self._content_cache.GetFromFile(path)
 
   def Cron(self):
     # Running Refresh() on the file system is enough to pull GitHub content,
