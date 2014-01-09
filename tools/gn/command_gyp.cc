3da8186756705998a2958cf77717aae30913c9e1
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-09 14:10:07 +0000

    Make use of ElapsedTimer API for the 'gen' command.
    BUG=None
    TEST=run ./out/Default/gn, it should reporte the elapsed time without no
    regressions.
    R=brettw@chromium.org
    
    Review URL: https://codereview.chromium.org/124803002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243888 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/gn/command_gyp.cc
+++ tools/gn/command_gyp.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/environment.h"
 #include "base/strings/string_number_conversions.h"
-#include "base/time/time.h"
+#include "base/timer/elapsed_timer.h"
 #include "tools/gn/build_settings.h"
 #include "tools/gn/commands.h"
 #include "tools/gn/err.h"
@@ -327,9 +327,7 @@ const char kGyp_Help[] =
     "  }\n";
 
 int RunGyp(const std::vector<std::string>& args) {
-  const CommandLine* cmdline = CommandLine::ForCurrentProcess();
-
-  base::TimeTicks begin_time = base::TimeTicks::Now();
+  base::ElapsedTimer timer;
 
   // Deliberately leaked to avoid expensive process teardown.
   Setup* setup_debug = new Setup;
@@ -408,9 +406,9 @@ int RunGyp(const std::vector<std::string>& args) {
     return 1;
   }
 
-  // Timing info.
-  base::TimeTicks end_time = base::TimeTicks::Now();
-  if (!cmdline->HasSwitch(kSwitchQuiet)) {
+  base::TimeDelta elapsed_time = timer.Elapsed();
+
+  if (!CommandLine::ForCurrentProcess()->HasSwitch(kSwitchQuiet)) {
     OutputString("Done. ", DECORATION_GREEN);
 
     std::string stats = "Wrote " +
@@ -419,7 +417,7 @@ int RunGyp(const std::vector<std::string>& args) {
         base::IntToString(
             setup_debug->scheduler().input_file_manager()->GetInputFileCount())
         + " GN files in " +
-        base::IntToString((end_time - begin_time).InMilliseconds()) + "ms\n";
+        base::IntToString(elapsed_time.InMilliseconds()) + "ms\n";
 
     OutputString(stats);
   }
