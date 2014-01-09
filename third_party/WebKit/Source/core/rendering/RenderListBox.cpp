4c27405a198539ef2ca2ade5af6657fb4cb27510
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-09 18:54:57 +0000

    Listbox does not take text direction into account when sizing
    The listbox code currently does not take the directionality of the text
    into account when determining the size for the element. This can cause
    the size to be off by a character or more thus truncating the text for
    the longest option.
    
    BUG=332196
    TEST=fast/text/international/listbox-width-rtl.html
    R=leviw@chromium.org
    
    Review URL: https://codereview.chromium.org/126763002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164794 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderListBox.cpp
+++ third_party/WebKit/Source/core/rendering/RenderListBox.cpp
@@ -58,6 +58,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/fonts/FontCache.h"
 #include "platform/graphics/GraphicsContext.h"
 #include "platform/scroll/Scrollbar.h"
+#include "platform/text/BidiTextRun.h"
 
 using namespace std;
 
@@ -141,8 +142,12 @@ void RenderListBox::updateFromElement()
 
             if (!text.isEmpty()) {
                 applyTextTransform(style(), text, ' ');
-                // FIXME: Why is this always LTR? Can't text direction affect the width?
+
+                bool hasStrongDirectionality;
+                TextDirection direction = determineDirectionality(text, hasStrongDirectionality);
                 TextRun textRun = constructTextRun(this, itemFont, text, style(), TextRun::AllowTrailingExpansion);
+                if (hasStrongDirectionality)
+                    textRun.setDirection(direction);
                 textRun.disableRoundingHacks();
                 float textWidth = itemFont.width(textRun);
                 width = max(width, textWidth);
