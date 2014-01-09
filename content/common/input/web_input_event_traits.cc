65a799d6c775caa0349bdf79c490265dbc73ccfd
tdresser@chromium.org <tdresser@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-09 00:48:02 +0000

    Make tap gesture ignore its ack disposition.
    BUG=302852,275611
    TEST=ImmediateInputRouterTest.GestureTypesIgnoringAck
    
    Review URL: https://codereview.chromium.org/84653006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243713 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/input/web_input_event_traits.cc
+++ content/common/input/web_input_event_traits.cc
@@ -290,6 +290,7 @@ bool WebInputEventTraits::IgnoresAckDisposition(
     case WebInputEvent::GestureTapDown:
     case WebInputEvent::GestureShowPress:
     case WebInputEvent::GestureTapCancel:
+    case WebInputEvent::GestureTap:
     case WebInputEvent::GesturePinchBegin:
     case WebInputEvent::GesturePinchEnd:
     case WebInputEvent::GestureScrollBegin:
