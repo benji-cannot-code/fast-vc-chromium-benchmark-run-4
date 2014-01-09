a21c20e5dc14cc9b43186a9f9854a2cbcada12ab
jamiewalch@chromium.org <jamiewalch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-09 19:10:23 +0000

    Show connection type in web-app.
    This is mostly boilerplate to get the OnSessionRouteChange notification up to the web-app. The web-app then includes it in the stats panel. In the common case where all channels have the same type, it displays it as "connection: <type>"; if the types differ they are displayed individually.
    
    BUG=134528
    
    Review URL: https://codereview.chromium.org/126123002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243947 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/protocol/connection_to_host.cc
+++ remoting/protocol/connection_to_host.cc
@@ -213,8 +213,7 @@ void ConnectionToHost::OnSessionStateChange(
 
 void ConnectionToHost::OnSessionRouteChange(const std::string& channel_name,
                                             const TransportRoute& route) {
-  VLOG(0) << "Using " << TransportRoute::GetTypeString(route.type)
-          << " connection for " << channel_name << " channel";
+  event_callback_->OnRouteChanged(channel_name, route);
 }
 
 void ConnectionToHost::OnSessionChannelReady(const std::string& channel_name,
