48cb8315861577c939f9954c2161a7d193919a40
jiayl@chromium.org <jiayl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-09 12:54:24 +0000

    Remove the RTP recording related code from webrtc-internals. The code is dead and we have no plan to implement RTP recording in the near term. So removing to avoid confusion.
    BUG=
    
    Review URL: https://codereview.chromium.org/129533003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243876 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/media/webrtc_internals_message_handler.cc
+++ content/browser/media/webrtc_internals_message_handler.cc
@@ -28,13 +28,6 @@ void WebRTCInternalsMessageHandler::RegisterMessages() {
       base::Bind(&WebRTCInternalsMessageHandler::OnGetAllStats,
                  base::Unretained(this)));
 
-  web_ui()->RegisterMessageCallback("startRtpRecording",
-      base::Bind(&WebRTCInternalsMessageHandler::OnStartRtpRecording,
-                 base::Unretained(this)));
-
-  web_ui()->RegisterMessageCallback("stopRtpRecording",
-      base::Bind(&WebRTCInternalsMessageHandler::OnStopRtpRecording,
-                 base::Unretained(this)));
   web_ui()->RegisterMessageCallback("enableAecRecording",
       base::Bind(&WebRTCInternalsMessageHandler::OnSetAecRecordingEnabled,
                  base::Unretained(this), true));
@@ -55,16 +48,6 @@ void WebRTCInternalsMessageHandler::OnGetAllStats(
        !i.IsAtEnd(); i.Advance()) {
     i.GetCurrentValue()->Send(new PeerConnectionTracker_GetAllStats());
   }
-}
-
-void WebRTCInternalsMessageHandler::OnStartRtpRecording(
-    const base::ListValue* /* unused_list */) {
-  WebRTCInternals::GetInstance()->StartRtpRecording();
-}
-
-void WebRTCInternalsMessageHandler::OnStopRtpRecording(
-    const base::ListValue* /* unused_list */) {
-  WebRTCInternals::GetInstance()->StopRtpRecording();
 }
 
 void WebRTCInternalsMessageHandler::OnSetAecRecordingEnabled(
--- content/browser/resources/media/dump_creator.js
+++ content/browser/resources/media/dump_creator.js
@@ -5,9 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 
 /**
- * Provides the UI to start and stop RTP recording, forwards the start/stop
- * commands to Chrome, and updates the UI based on dump updates. Also provides
- * creating a file containing all PeerConnection updates and stats.
+ * Provides the UI for dump creation.
  */
 var DumpCreator = (function() {
   /**
@@ -16,30 +14,6 @@ var DumpCreator = (function() {
    * @constructor
    */
   function DumpCreator(containerElement) {
-    /**
-     * True if the RTP packets are being recorded.
-     * @type {bool}
-     * @private
-     */
-    this.recording_ = false;
-
-    /**
-     * @type {!Object.<string>}
-     * @private
-     * @const
-     */
-    this.StatusStrings_ = {
-      NOT_STARTED: 'not started.',
-      RECORDING: 'recording...',
-    },
-
-    /**
-     * The status of dump creation.
-     * @type {string}
-     * @private
-     */
-    this.status_ = this.StatusStrings_.NOT_STARTED;
-
     /**
      * The root element of the dump creation UI.
      * @type {Element}
@@ -55,8 +29,7 @@ var DumpCreator = (function() {
     var content = document.createElement('div');
     this.root_.appendChild(content);
 
-    content.innerHTML = '<button disabled></button> Status: <span></span>' +
-        '<div><a><button>' +
+    content.innerHTML = '<div><a><button>' +
         'Download the PeerConnection updates and stats data' +
         '</button></a></div>' +
         '<p><label><input type=checkbox>' +
@@ -68,14 +41,10 @@ var DumpCreator = (function() {
         ' calls. When the box is unchecked or this page is closed, this' +
         ' recording functionality will be disabled.</p>';
 
-    content.getElementsByTagName('button')[0].addEventListener(
-        'click', this.onRtpToggled_.bind(this));
     content.getElementsByTagName('a')[0].addEventListener(
         'click', this.onDownloadData_.bind(this));
     content.getElementsByTagName('input')[0].addEventListener(
         'click', this.onAecRecordingChanged_.bind(this));
-
-    this.updateDisplay_();
   }
 
   DumpCreator.prototype = {
@@ -99,24 +68,6 @@ var DumpCreator = (function() {
     },
 
     /**
-     * Handles the event of toggling the rtp recording state.
-     *
-     * @private
-     */
-    onRtpToggled_: function() {
-      if (this.recording_) {
-        this.recording_ = false;
-        this.status_ = this.StatusStrings_.NOT_STARTED;
-        chrome.send('stopRtpRecording');
-      } else {
-        this.recording_ = true;
-        this.status_ = this.StatusStrings_.RECORDING;
-        chrome.send('startRtpRecording');
-      }
-      this.updateDisplay_();
-    },
-
-    /**
      * Handles the event of toggling the AEC recording state.
      *
      * @private
@@ -127,34 +78,6 @@ var DumpCreator = (function() {
         chrome.send('enableAecRecording');
       } else {
         chrome.send('disableAecRecording');
-      }
-    },
-
-    /**
-     * Updates the UI based on the recording status.
-     *
-     * @private
-     */
-    updateDisplay_: function() {
-      if (this.recording_) {
-        this.root_.getElementsByTagName('button')[0].textContent =
-            'Stop Recording RTP Packets';
-      } else {
-        this.root_.getElementsByTagName('button')[0].textContent =
-            'Start Recording RTP Packets';
-      }
-
-      this.root_.getElementsByTagName('span')[0].textContent = this.status_;
-    },
-
-    /**
-     * Set the status to the content of the update.
-     * @param {!Object} update
-     */
-    onUpdate: function(update) {
-      if (this.recording_) {
-        this.status_ = JSON.stringify(update);
-        this.updateDisplay_();
       }
     },
   };
