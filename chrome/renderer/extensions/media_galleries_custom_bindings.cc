cca0bdbd75b6ed36a6761a0acc37403f42853bbc
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-09 01:13:11 +0000

    Cleanup: Move MediaGalleriesCustomBindings::GetMediaFileSystemObject() into an anonymous namespace.
    Review URL: https://codereview.chromium.org/129573002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243723 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/extensions/media_galleries_custom_bindings.cc
+++ chrome/renderer/extensions/media_galleries_custom_bindings.cc
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "base/files/file_path.h"
-#include "base/strings/stringprintf.h"
 #include "chrome/common/extensions/extension_constants.h"
 #include "third_party/WebKit/public/web/WebDocument.h"
 #include "third_party/WebKit/public/web/WebFrame.h"
@@ -17,17 +15,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace extensions {
 
-MediaGalleriesCustomBindings::MediaGalleriesCustomBindings(
-    Dispatcher* dispatcher, ChromeV8Context* context)
-    : ChromeV8Extension(dispatcher, context) {
-  RouteFunction(
-      "GetMediaFileSystemObject",
-      base::Bind(&MediaGalleriesCustomBindings::GetMediaFileSystemObject,
-                 base::Unretained(this)));
-}
+namespace {
 
-void MediaGalleriesCustomBindings::GetMediaFileSystemObject(
-    const v8::FunctionCallbackInfo<v8::Value>& args) {
+// FileSystemObject GetMediaFileSystem(string file_system_url): construct
+// a file system object from a file system url.
+void GetMediaFileSystemObject(const v8::FunctionCallbackInfo<v8::Value>& args) {
   if (args.Length() != 1) {
     NOTREACHED();
     return;
@@ -53,6 +45,15 @@ void MediaGalleriesCustomBindings::GetMediaFileSystemObject(
       webframe->createFileSystem(blink::WebFileSystemTypeIsolated,
                                  blink::WebString::fromUTF8(fs_name),
                                  blink::WebString::fromUTF8(root_url)));
+}
+
+}  // namespace
+
+MediaGalleriesCustomBindings::MediaGalleriesCustomBindings(
+    Dispatcher* dispatcher, ChromeV8Context* context)
+    : ChromeV8Extension(dispatcher, context) {
+  RouteFunction("GetMediaFileSystemObject",
+                base::Bind(&GetMediaFileSystemObject));
 }
 
 }  // namespace extensions
