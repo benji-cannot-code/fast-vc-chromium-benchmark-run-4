b08344108060bce639d07d866c87c8d67d1cbd83
costan@gmail.com <costan@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-09 08:32:10 +0000

    Feature detection-friendly restrictions for packaged apps.
    Packaged apps do not have access to some features of the Web platform
    that are deprecated, or that do not make sense in the context of an
    app. Currently, accessing most of those features throws an error.
    
    This change makes thse features behave more like missing featurs on the
    Web platform. Whenever possible, accessing restricted features returns
    undefined and logs an error to the browser console. Features accessible
    via non-configurable ES5 properties (e.g. window.alert, document.write)
    either become no-ops or throw errors, on a case-by-case basis.
    
    Deprecated events have their "on*" properties return undefined (as
    opposed to null, for events with no handlers) to facilitate feature
    detection. Using addEventListener for these events used to throw an
    error, and now logs an error to the console and turns into a no-op, to
    mimic the behavior for undefined events as well as possible.
    
    This change aims to make JavaScript libraries written for browsers work
    in packaged apps as seamlessly as possible, while still providing useful
    warnings for developers. This creates a couple of warts: (1) properties
    are not removed, so the "in" operator will still see them, and (2)
    dispatchEvent will never deliver deprecated events, but it normally
    delives un-implemented events.
    
    BUG=330487
    
    Review URL: https://codereview.chromium.org/120733003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243817 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/platform_apps/restrictions/sandboxed_iframe.js
+++ chrome/test/data/extensions/platform_apps/restrictions/sandboxed_iframe.js
@@ -4,17 +4,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 window.onload = function() {
-  try {
-    window.alert('should throw');
+  window.onunload = function() {
     window.parent.postMessage({'success': false,
-                               'reason' : 'should have thrown'},
+                               'reason' : 'unload handler works'},
                               '*');
-  } catch(e) {
-    var message = e.message || e;
-    var succ = message.indexOf('is not available in packaged apps') != -1;
-    window.parent.postMessage({'success': succ,
-                               'reason' : 'got wrong error: ' + message},
+  };
+  if (typeof(window.unload) !== 'undefined') {
+    window.parent.postMessage({'success': false,
+                               'reason' : 'unload is not undefined'},
                               '*');
-
   }
+  window.dispatchEvent(new Event('unload'));
+  window.parent.postMessage({'success': true}, '*');
 };
