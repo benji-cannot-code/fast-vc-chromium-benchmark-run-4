a50702530a4cbcc53ad9bacd140f751b0175d6f5
hamaji@chromium.org <hamaji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-09 04:39:48 +0000

    Implement lstat by stat in nacl_io
    As nacl_io does not support
    
    TEST=./build_tools/build_sdk.py
    TEST=./build_tools/test_sdk.py
    
    Review URL: https://codereview.chromium.org/125693002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243771 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/libraries/nacl_io/kernel_proxy.cc
+++ native_client_sdk/src/libraries/nacl_io/kernel_proxy.cc
@@ -635,8 +635,7 @@ int KernelProxy::truncate(const char* path, off_t len) {
 }
 
 int KernelProxy::lstat(const char* path, struct stat* buf) {
-  errno = ENOSYS;
-  return -1;
+  return stat(path, buf);
 }
 
 int KernelProxy::rename(const char* path, const char* newpath) {
