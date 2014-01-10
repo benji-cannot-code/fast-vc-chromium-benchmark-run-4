b5d4b1d7ad0859615750a7b1659f19bb468737bb
vmiura@chromium.org <vmiura@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-10 03:18:08 +0000

    Make correct GL context current during gpu_tracer processing
    The gpu_tracer reads back GL timer queries as a delayed task.
    During processing the associated GL context wasn't made current, so it
    was possible to query the wrong context, get incorrect timestamps, or
    cause GL errors.
    
    BUG=331161
    
    Review URL: https://codereview.chromium.org/130313002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244068 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/gpu_tracer.h
+++ gpu/command_buffer/service/gpu_tracer.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/threading/thread.h"
+#include "gpu/command_buffer/service/gles2_cmd_decoder.h"
 #include "gpu/gpu_export.h"
 #include "ui/gl/gl_bindings.h"
 
@@ -22,10 +23,10 @@ namespace gles2 {
 // Traces GPU Commands.
 class GPUTracer {
  public:
-  static scoped_ptr<GPUTracer> Create();
+  static scoped_ptr<GPUTracer> Create(gles2::GLES2Decoder* decoder);
 
-  GPUTracer() {}
-  virtual ~GPUTracer() {}
+  explicit GPUTracer(gles2::GLES2Decoder* decoder);
+  virtual ~GPUTracer();
 
   // Begin a trace.
   virtual bool Begin(const std::string& name) = 0;
@@ -36,6 +37,8 @@ class GPUTracer {
   // Retrieve the name of the current open trace.
   // Returns empty string if no current open trace.
   virtual const std::string& CurrentName() const = 0;
+
+  gles2::GLES2Decoder* decoder_;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(GPUTracer);
