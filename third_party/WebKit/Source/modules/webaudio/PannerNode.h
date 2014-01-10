9791695591869eef7915ba4de10caa1b8d01e543
rtoy@google.com <rtoy@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-10 01:42:27 +0000

    Handle loops in audio graph better for PannerNodes
    When a PannerNode searches the graph for audio sources connected to
    the panner, prevent feedback loops from causing infinite loops.
    Create a hashmap to keep track of all visited nodes so we don't visit
    them more than once.
    
    A test is also added to ensure we don't infloop.
    
    BUG=331446
    
    Review URL: https://codereview.chromium.org/130003002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164822 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/webaudio/PannerNode.h
+++ third_party/WebKit/Source/modules/webaudio/PannerNode.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "modules/webaudio/AudioNode.h"
 #include "modules/webaudio/AudioParam.h"
 #include "platform/geometry/FloatPoint3D.h"
+#include "wtf/HashMap.h"
 #include "wtf/OwnPtr.h"
 
 namespace WebCore {
@@ -138,7 +139,7 @@ private:
 
     // Notifies any AudioBufferSourceNodes connected to us either directly or indirectly about our existence.
     // This is in order to handle the pitch change necessary for the doppler shift.
-    void notifyAudioSourcesConnectedToNode(AudioNode*);
+    void notifyAudioSourcesConnectedToNode(AudioNode*, HashMap<AudioNode*, bool> &visitedNodes);
 
     OwnPtr<Panner> m_panner;
     unsigned m_panningModel;
