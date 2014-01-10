0482f622097791660a4f7e38f43fddb35b76df4c
tdresser@chromium.org <tdresser@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-10 19:52:57 +0000

    Target touches to the correct display.
    ui::TouchEvents now know the id of their source device.
    The target of a touch can't be effected by a touch on another display.
    
    Relanding https://codereview.chromium.org/103173004/, after it was
    reverted https://codereview.chromium.org/103173004. When ensuring that
    the GR wasn't reused between tests, I had neglected to delete the old
    GR.
    
    BUG=315651
    TEST=GestureRecognizerTest.GestureEventTouchLockIgnoresOtherScreens
    
    Review URL: https://codereview.chromium.org/132123004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244220 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/events/gestures/gesture_point.cc
+++ ui/events/gestures/gesture_point.cc
@@ -25,7 +25,8 @@ GesturePoint::GesturePoint()
       velocity_calculator_(
           GestureConfiguration::points_buffered_for_velocity()),
       point_id_(-1),
-      touch_id_(-1) {
+      touch_id_(-1),
+      source_device_id_(-1) {
 }
 
 GesturePoint::~GesturePoint() {}
@@ -35,6 +36,7 @@ void GesturePoint::Reset() {
   ResetVelocity();
   point_id_ = -1;
   clear_enclosing_rectangle();
+  source_device_id_ = -1;
 }
 
 void GesturePoint::ResetVelocity() {
