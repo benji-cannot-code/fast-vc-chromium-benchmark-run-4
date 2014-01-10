d543273ef7609d09991cab28d9b208cee6afaf92
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-10 21:00:49 +0000

    Removed unused function.
    This function has no implementation. It's a bummer the compiler
    doesn't complain about this.
    
    Review URL: https://codereview.chromium.org/134563002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164904 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Document.h
+++ third_party/WebKit/Source/core/dom/Document.h
@@ -1088,7 +1088,6 @@ private:
     void addMutationEventListenerTypeIfEnabled(ListenerType);
 
     void didAssociateFormControlsTimerFired(Timer<Document>*);
-    void styleResolverThrowawayTimerFired(Timer<Document>*);
 
     void processHttpEquivDefaultStyle(const AtomicString& content);
     void processHttpEquivRefresh(const AtomicString& content);
