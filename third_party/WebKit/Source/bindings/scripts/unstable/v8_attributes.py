a31c1e9d66db82c56e1654df4f97b41072ae5d5d
nbarth@chromium.org <nbarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-10 06:25:29 +0000

    IDL compiler: Rename special case: EventTarget => EventHandler
    A type that is a special case for is_keep_alive_for_gc was renamed.
    Didn't catch this earlier b/c test was broken (missing readonly).
    Fixed.
    
    TBR=haraken
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/133173002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164848 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/scripts/unstable/v8_attributes.py
+++ third_party/WebKit/Source/bindings/scripts/unstable/v8_attributes.py
@@ -231,7 +231,7 @@ def is_keep_alive_for_gc(interface, attribute):
              # A self-reference is unnecessary.
              attribute.name == 'self' or
              # FIXME: Remove these hard-coded hacks.
-             idl_type in ['EventHandler', 'Window'] or
+             idl_type in ['EventTarget', 'Window'] or
              idl_type.startswith('HTML'))))
 
 
