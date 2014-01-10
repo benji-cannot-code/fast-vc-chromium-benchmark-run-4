d2cba13b0768a6d2c635fc21ec7bb7a22d8b2d7b
bshe@chromium.org <bshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-10 20:05:41 +0000

    Add unit test for lock and unlock virtual keyboard feature
    BUG=326170
    
    Review URL: https://codereview.chromium.org/98073012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244225 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/chromeos/virtual_keyboard/control_keys_test.js
+++ chrome/test/data/chromeos/virtual_keyboard/control_keys_test.js
@@ -320,6 +320,7 @@ function testHideKeyboard(testDoneCallback) {
     assertTrue(!!hideKey, 'Unable to find key');
 
     chrome.virtualKeyboardPrivate.hideKeyboard.addExpectation();
+    chrome.virtualKeyboardPrivate.lockKeyboard.addExpectation(false);
 
     hideKey.down();
     hideKey.up();
