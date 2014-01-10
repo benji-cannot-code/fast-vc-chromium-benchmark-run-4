41148b3f96233b6f104e75b44a21cd0f4989ff38
mtomasz@chromium.org <mtomasz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-10 08:28:47 +0000

    Migrate from fullPath to Entry in the mosaic view.
    This patch removes the last instance of fullPath in the mosaic view and replaces it with use of the volume manager to extract the location information.
    
    TEST=Tested manually.
    BUG=320967
    
    Review URL: https://codereview.chromium.org/130383003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244126 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/foreground/js/photo/gallery.js
+++ chrome/browser/resources/file_manager/foreground/js/photo/gallery.js
@@ -220,6 +220,7 @@ Gallery.prototype.initDom_ = function() {
                                     this.dataModel_,
                                     this.selectionModel_,
                                     this.metadataCache_,
+                                    this.volumeManager_,
                                     this.toggleMode_.bind(this, null));
 
   this.slideMode_ = new SlideMode(this.container_,
