565439ea6c1a42c9a7db929aa8bea1fe6dc5ce26
johnme@chromium.org <johnme@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-10 17:16:20 +0000

    Make cr build run goma_ctl.py ensure_start before building.
    Review URL: https://codereview.chromium.org/131543004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244185 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/cr/cr/base/host.py
+++ tools/cr/cr/base/host.py
@@ -42,8 +42,8 @@ class Host(cr.Plugin, cr.Plugin.Type):
         return host
 
   def _Execute(self, context, command,
-               shell=False, capture=False, ignore_dry_run=False,
-               return_status=False):
+               shell=False, capture=False, silent=False,
+               ignore_dry_run=False, return_status=False):
     """This is the only method that launches external programs.
 
     It is a thin wrapper around subprocess.Popen that handles cr specific
@@ -79,6 +79,8 @@ class Host(cr.Plugin, cr.Plugin.Type):
       out = None
       if capture:
         out = subprocess.PIPE
+      elif silent:
+        out = open(os.devnull, "w")
       try:
         p = subprocess.Popen(
             command, shell=shell,
@@ -98,6 +100,9 @@ class Host(cr.Plugin, cr.Plugin.Type):
         p.terminate()
         p.wait()
         exit(1)
+      finally:
+        if silent:
+          out.close()
       if return_status:
         return p.returncode
       if p.returncode != 0:
@@ -114,6 +119,10 @@ class Host(cr.Plugin, cr.Plugin.Type):
   @cr.Plugin.activemethod
   def Execute(self, context, *command):
     return self._Execute(context, command, shell=False)
+
+  @cr.Plugin.activemethod
+  def ExecuteSilently(self, context, *command):
+    return self._Execute(context, command, shell=False, silent=True)
 
   @cr.Plugin.activemethod
   def CaptureShell(self, context, *command):
