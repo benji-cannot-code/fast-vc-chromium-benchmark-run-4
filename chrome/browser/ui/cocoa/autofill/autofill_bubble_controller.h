844bbddff80467e0b6f2513d067f2e832484858b
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-10 20:17:42 +0000

    [rAC] Do not re-create bubble window unnecessarily
    Prevent error bubbles from being recreated every single time a
    validation operation on the current field succeeds. Only destroy
    bubble if the anchor field changes.
    
    BUG=331147
    R=isherman@chromium.org
    
    Review URL: https://codereview.chromium.org/123703002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244228 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/autofill/autofill_bubble_controller.h
+++ chrome/browser/ui/cocoa/autofill/autofill_bubble_controller.h
@@ -35,6 +35,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Maximum width that the bubble will occupy, regardless of message size.
 - (CGFloat)maxWidth;
 
+// Update the current text with |message|.
+- (void)setMessage:(NSString*)message;
+
 @end
 
 
