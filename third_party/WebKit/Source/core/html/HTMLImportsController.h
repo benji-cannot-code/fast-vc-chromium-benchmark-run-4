f2b8a14f71f5ab5605a51b29abfe55e7b227881b
morrita@chromium.org <morrita@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-10 08:14:45 +0000

    [import] Refactoring: Get rid of HTMLImport::isProcessing()
    This change eliminates confusing isProcessing() and
    exposes ownsLoader() instead. It clarify the logic in
    HTMLImport::isBlockingFollowersFromRunningScript().
    
    BUG=none
    TEST=none
    R=dglazkov,dominicc
    
    Review URL: https://codereview.chromium.org/131043003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164854 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLImportsController.h
+++ third_party/WebKit/Source/core/html/HTMLImportsController.h
@@ -62,7 +62,6 @@ public:
     virtual Document* document() const OVERRIDE;
     virtual void wasDetachedFromDocument() OVERRIDE;
     virtual void didFinishParsing() OVERRIDE;
-    virtual bool isProcessing() const OVERRIDE;
     virtual bool isDone() const OVERRIDE;
 
     // HTMLImportRoot
