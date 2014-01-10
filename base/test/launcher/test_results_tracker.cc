7d94dc1e0da18e0d5a904077bdc589fed33fdcff
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-10 11:16:54 +0000

    Only print summary of all test iterations if there is more than one.
    Also print a clear "SUCCESS" message on success.
    
    BUG=236893
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/131663002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244147 0039d316-1c4b-4281-b951-d872f2087c98

--- base/test/launcher/test_results_tracker.cc
+++ base/test/launcher/test_results_tracker.cc
@@ -230,7 +230,7 @@ void TestResultsTracker::PrintSummaryOfAllIterations() const {
     }
   }
 
-  fprintf(stdout, "Summary of all itest iterations:\n");
+  fprintf(stdout, "Summary of all test iterations:\n");
   fflush(stdout);
 
   PrintTests(tests_by_status[TestResult::TEST_FAILURE].begin(),
