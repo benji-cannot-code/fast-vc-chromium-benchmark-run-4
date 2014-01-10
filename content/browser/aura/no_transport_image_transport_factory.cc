c1476f6e5daa2172cdf62793a03d700c49bb1da8
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-10 00:06:45 +0000

    aura: Remove sync point methods from ImageTransportFactory api.
    These methods were added to the GLHelper recently, so we don't need
    them in both places. They're actually just used by deprecated code
    we'll remove very soon, but this removes them from the API.
    
    R=piman@chromium.org
    BUG=332998
    
    Review URL: https://codereview.chromium.org/132473004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244025 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/aura/no_transport_image_transport_factory.cc
+++ content/browser/aura/no_transport_image_transport_factory.cc
@@ -84,10 +84,6 @@ GLHelper* NoTransportImageTransportFactory::GetGLHelper() {
   return gl_helper_.get();
 }
 
-uint32 NoTransportImageTransportFactory::InsertSyncPoint() { return 0; }
-
-void NoTransportImageTransportFactory::WaitSyncPoint(uint32 sync_point) {}
-
 // We don't generate lost context events, so we don't need to keep track of
 // observers
 void NoTransportImageTransportFactory::AddObserver(
