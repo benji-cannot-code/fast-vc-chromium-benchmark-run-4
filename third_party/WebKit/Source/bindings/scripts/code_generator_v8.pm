b7d3335c6bf668212319e0b72bda22629c5d040e
nbarth@chromium.org <nbarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-10 11:56:16 +0000

    IDL compiler: Document interfaces
    This is the last inherited interface!
    Straightforward, but affects several places in the code.
    
    BUG=239771
    R=haraken
    
    Review URL: https://codereview.chromium.org/132223006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164868 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/scripts/code_generator_v8.pm
+++ third_party/WebKit/Source/bindings/scripts/code_generator_v8.pm
@@ -220,6 +220,7 @@ my %domNodeTypes = ("Attr" => 1,
                     "SVGDocument" => 1,
                     "Text" => 1,
                     "TestNode" => 1,
+                    "TestInterfaceDocument" => 1,
                     "TestInterfaceNode" => 1,
                    );
 
@@ -919,7 +920,6 @@ public:
 };
 END
 
-    my $customWrap = NeedsSpecialWrap($interface);
     if ($customToV8) {
         $header{nameSpaceWebCore}->add(<<END);
 
@@ -945,7 +945,7 @@ inline void v8SetReturnValueFast(const CallbackInfo& callbackInfo, ${nativeType}
 }
 END
     } else {
-        if ($customWrap) {
+        if (NeedsSpecialWrap($interface)) {
             $header{nameSpaceWebCore}->add(<<END);
 
 v8::Handle<v8::Object> wrap(${nativeType}* impl, v8::Handle<v8::Object> creationContext, v8::Isolate*);
