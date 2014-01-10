792c5f4eae96ed5972f0d6f835c47505a618a741
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-10 18:41:19 +0000

    [Files.app] Change sort timing during metadata fetch
    This patch is a reland of r243816, fixing the tiny typo.
    
    Previously, sort was not started until all the metadata is fetched. When fetching took a time, the user saw unsorted list for a while. With this patch, sort is happened just after every updates. User can see sorted list every time.
    
    BUG=327479
    TEST=manually tested
    
    Review URL: https://codereview.chromium.org/132683004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244205 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/foreground/js/file_manager.js
+++ chrome/browser/resources/file_manager/foreground/js/file_manager.js
@@ -2598,7 +2598,6 @@ var BOTTOM_MARGIN_FOR_PREVIEW_PANEL_PX = 52;
     if (this.commandHandler)
       this.commandHandler.updateAvailability();
     this.hideSpinnerLater_();
-    this.refreshCurrentDirectoryMetadata_();
 
     if (this.scanUpdatedTimer_) {
       clearTimeout(this.scanUpdatedTimer_);
@@ -2690,7 +2689,6 @@ var BOTTOM_MARGIN_FOR_PREVIEW_PANEL_PX = 52;
    * @private
    */
   FileManager.prototype.onRescanCompleted_ = function() {
-    this.refreshCurrentDirectoryMetadata_();
     this.selectionHandler_.onFileSelectionChanged();
   };
 
