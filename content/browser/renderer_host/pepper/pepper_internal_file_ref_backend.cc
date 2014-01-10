567ce77fc79349454966202c005cdab83ad98954
nhiroki@chromium.org <nhiroki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-10 11:34:12 +0000

    [Retry] PPAPI: Add new PPB_FileRef.MakeDirectory to support exclusive operation
    Original Review: https://codereview.chromium.org/113363004/
    
    Current PPB_FileRef.MakeDirectory returns PP_OK if a directory exists on
    the given path. This makes it difficult to create POSIX compatible API on
    top of PPAPI.
    
    This change introduces new PPB_FileRef.MakeDirectory as dev channel API.
    That makes a new directory according to the given PP_MakeDirectoryFlags
    values. The flags provide exclusive operation option. If exclusive flag
    is specified and a directory exists on the given path, the function fails
    and returns PP_ERROR_FILEEXISTS.
    
    
    BUG=314879
    TEST=browser_tests
    TBR=dmichael@chromium.org,yzshen@chromium.org,tsepez@chromium.org
    
    Review URL: https://codereview.chromium.org/131403004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244148 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/pepper/pepper_internal_file_ref_backend.cc
+++ content/browser/renderer_host/pepper/pepper_internal_file_ref_backend.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/c/pp_file_info.h"
 #include "ppapi/c/pp_instance.h"
 #include "ppapi/c/pp_resource.h"
+#include "ppapi/c/ppb_file_ref.h"
 #include "ppapi/host/dispatch_host_message.h"
 #include "ppapi/host/ppapi_host.h"
 #include "ppapi/proxy/ppapi_messages.h"
@@ -93,14 +94,14 @@ void PepperInternalFileRefBackend::DidFinish(
 
 int32_t PepperInternalFileRefBackend::MakeDirectory(
     ppapi::host::ReplyMessageContext reply_context,
-    bool make_ancestors) {
+    int32_t make_directory_flags) {
   if (!GetFileSystemURL().is_valid())
     return PP_ERROR_FAILED;
 
   GetFileSystemContext()->operation_runner()->CreateDirectory(
       GetFileSystemURL(),
-      false,
-      make_ancestors,
+      !!(make_directory_flags & PP_MAKEDIRECTORYFLAG_EXCLUSIVE),
+      !!(make_directory_flags & PP_MAKEDIRECTORYFLAG_WITH_ANCESTORS),
       base::Bind(&PepperInternalFileRefBackend::DidFinish,
                  weak_factory_.GetWeakPtr(),
                  reply_context,
