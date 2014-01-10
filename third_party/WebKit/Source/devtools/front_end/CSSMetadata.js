150d9dcdc1b063dcb2dbe72db41deafeb8f19dc6
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-10 10:18:50 +0000

    DevTools: Add "inset" and "none" values for "box-shadow" property in CSSMetadata
    R=aandrey, eustas, vsevik
    BUG=332355
    
    Review URL: https://codereview.chromium.org/131423002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164860 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/CSSMetadata.js
+++ third_party/WebKit/Source/devtools/front_end/CSSMetadata.js
@@ -215,6 +215,9 @@ WebInspector.CSSMetadata._propertyDataMap = {
     "border-left-width": { values: [
         "medium", "thick", "thin"
     ] },
+    "box-shadow": { values: [
+        "inset", "none"
+    ] },
     "-webkit-writing-mode": { values: [
         "lr", "rl", "tb", "lr-tb", "rl-tb", "tb-rl", "horizontal-tb", "vertical-rl", "vertical-lr", "horizontal-bt"
     ] },
@@ -656,7 +659,6 @@ WebInspector.CSSMetadata._propertyDataMap = {
     "border-left": { m: "background" },
     "border-radius": { m: "background" },
     "bottom": { m: "visuren" },
-    "box-shadow": { m: "background" },
     "color": { m: "color", a: "foreground" },
     "counter-increment": { m: "generate" },
     "counter-reset": { m: "generate" },
