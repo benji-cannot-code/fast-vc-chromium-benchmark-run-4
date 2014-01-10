d3702b1ff1e27425462cafba94ee4e405a08dc8d
mahesh.kk@samsung.com <mahesh.kk@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-10 06:13:21 +0000

    First set of changes to remove ENABLE_ORIENTATION_EVENT compile time flag. Adding OrientationEvent runtime flag and the equivalent WebRuntimeFeature API.
    BUG=181136, 331337
    
    Review URL: https://codereview.chromium.org/132203007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164842 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/web/WebRuntimeFeatures.h
+++ third_party/WebKit/public/web/WebRuntimeFeatures.h
@@ -111,6 +111,9 @@ public:
     BLINK_EXPORT static void enableNavigatorContentUtils(bool);
     BLINK_EXPORT static bool isNavigatorContentUtilsEnabled();
 
+    BLINK_EXPORT static void enableOrientationEvent(bool);
+    BLINK_EXPORT static bool isOrientationEventEnabled();
+
     BLINK_EXPORT static void enablePagePopup(bool);
     BLINK_EXPORT static bool isPagePopupEnabled();
 
