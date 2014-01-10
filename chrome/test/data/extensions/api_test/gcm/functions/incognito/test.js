6e98331595382660b477c046f6ed600e7a369524
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-10 04:54:09 +0000

    [GCM] Make GCM API not work in incognito mode
    BUG=284553
    TEST=new tests added
    
    Review URL: https://codereview.chromium.org/130473002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244085 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/api_test/gcm/functions/incognito/test.js
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.test.runTests([
+  function testIncognito() {
+    chrome.gcm.register(["Sender"], function(registrationId) {
+      chrome.test.assertEq(chrome.runtime.lastError != undefined,
+                           chrome.extension.inIncognitoContext);
+      chrome.test.succeed();
+    });
+  }
+]);
