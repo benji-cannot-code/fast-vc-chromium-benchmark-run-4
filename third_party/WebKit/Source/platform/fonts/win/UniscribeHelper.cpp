a7674cc55f9b22d9e57214ed1a6a2b5b4fa63a28
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-10 20:09:28 +0000

    Fix compilation of Uniscribe code path
    Even though the Uniscribe code path is no longer used in tip of tree it
    is still used by the current stable version of chrome. Until no
    shipping version uses it it should still be maintained.
    
    Fix a couple of compilation errors due to recent refactorings.
    
    R=dglazkov@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/134503002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164899 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/fonts/win/UniscribeHelper.cpp
+++ third_party/WebKit/Source/platform/fonts/win/UniscribeHelper.cpp
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "platform/fonts/Font.h"
 #include "platform/fonts/skia/SkiaFontWin.h"
-#include "platform/fonts/FontFallbackWin.h"
+#include "platform/fonts/win/FontFallbackWin.h"
 #include "platform/graphics/GraphicsContext.h"
 #include "platform/win/HWndDC.h"
 #include "third_party/skia/include/core/SkPoint.h"
