1f6cf0b9186aab3f7f7ed32c2601a76a44491baf
eroman@chromium.org <eroman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-10 22:25:24 +0000

    Remove an unused header include.
    Review URL: https://codereview.chromium.org/134523005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164909 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/custom/V8ArrayBufferCustom.cpp
+++ third_party/WebKit/Source/bindings/v8/custom/V8ArrayBufferCustom.cpp
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "bindings/v8/custom/V8ArrayBufferCustom.h"
 
-#include "bindings/v8/ScriptPromiseResolver.h"
 #include "bindings/v8/V8Binding.h"
 #include "wtf/ArrayBuffer.h"
 #include "wtf/StdLibExtras.h"
