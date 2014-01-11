41de1d776ab1c49f3c5de0a940a22312547d7f9d
tengs@chromium.org <tengs@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-11 22:25:19 +0000

    Fix minor style issues for sticky keys.
    TEST=unit tests pass
    
    Review URL: https://codereview.chromium.org/134533002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244363 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/sticky_keys/sticky_keys_controller.h
+++ ash/sticky_keys/sticky_keys_controller.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef ASH_WM_STICKY_KEYS_CONTROLLER_H_
-#define ASH_WM_STICKY_KEYS_CONTROLLER_H_
+#ifndef ASH_STICKY_KEYS_STICKY_KEYS_CONTROLLER_H_
+#define ASH_STICKY_KEYS_STICKY_KEYS_CONTROLLER_H_
 
 #include "ash/ash_export.h"
 #include "base/memory/scoped_ptr.h"
@@ -238,4 +238,4 @@ class ASH_EXPORT StickyKeysHandler {
 
 }  // namespace ash
 
-#endif  // ASH_WM_STICKY_KEYS_CONTROLLER_H_
+#endif  // ASH_STICKY_KEYS_STICKY_KEYS_CONTROLLER_H_
