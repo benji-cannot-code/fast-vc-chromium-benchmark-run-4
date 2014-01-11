17a5bd65010ffcc9aed2c378124f0e231594e185
spang@chromium.org <spang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-11 02:54:38 +0000

    Add fontconfig to linux DEPS for embedded builds (reland)
    We'll use this in-tree fontconfig when "embedded=1" is set in GYP_DEFINES.
    
    This was reverted pending license checker changes (landed as r127213003).
    
    Relanding with the following changes from the third_party HOWTO:
      - Added OWNERS file
      - Updated .gitignore
    
    BUG=318315
    
    Review URL: https://codereview.chromium.org/133903005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244318 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ third_party/fontconfig/include/src/fcftalias.h
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+extern __typeof (FcFreeTypeCharIndex) IA__FcFreeTypeCharIndex __attribute((visibility("hidden")));
+#define FcFreeTypeCharIndex IA__FcFreeTypeCharIndex
+extern __typeof (FcFreeTypeCharSetAndSpacing) IA__FcFreeTypeCharSetAndSpacing __attribute((visibility("hidden")));
+#define FcFreeTypeCharSetAndSpacing IA__FcFreeTypeCharSetAndSpacing
+extern __typeof (FcFreeTypeCharSet) IA__FcFreeTypeCharSet __attribute((visibility("hidden")));
+#define FcFreeTypeCharSet IA__FcFreeTypeCharSet
+extern __typeof (FcPatternGetFTFace) IA__FcPatternGetFTFace __attribute((visibility("hidden")));
+#define FcPatternGetFTFace IA__FcPatternGetFTFace
+extern __typeof (FcPatternAddFTFace) IA__FcPatternAddFTFace __attribute((visibility("hidden")));
+#define FcPatternAddFTFace IA__FcPatternAddFTFace
+extern __typeof (FcFreeTypeQueryFace) IA__FcFreeTypeQueryFace __attribute((visibility("hidden")));
+#define FcFreeTypeQueryFace IA__FcFreeTypeQueryFace
