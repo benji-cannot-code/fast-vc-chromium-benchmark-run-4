82b2045da2370b7a117d7c3f345a6ab9eca3c55f
jamesr@chromium.org <jamesr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-11 23:08:53 +0000

    Use ContextProvider subclass to provide WGC3D pointers
    Some cc::ContextProvider instances will also need to provide WebGraphicsContext3D*
    interfaces to the underlying GL context, but not all. This adds a subclass of ContextProvider
    in content:: to use for these users. The compositor itself doesn't need the WGC3D
    accessor so it just uses the base class.
    
    BUG=181120
    
    Review URL: https://codereview.chromium.org/126093010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244383 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/renderer_webkitplatformsupport_impl.cc
+++ content/renderer/renderer_webkitplatformsupport_impl.cc
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/renderer_clipboard_client.h"
 #include "content/renderer/webclipboard_impl.h"
 #include "content/renderer/webcrypto/webcrypto_impl.h"
+#include "content/renderer/webgraphicscontext3d_provider_impl.h"
 #include "content/renderer/webpublicsuffixlist_impl.h"
 #include "gpu/config/gpu_info.h"
 #include "ipc/ipc_sync_message_filter.h"
@@ -71,7 +72,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/web/WebRuntimeFeatures.h"
 #include "ui/gfx/color_profile.h"
 #include "url/gurl.h"
-#include "webkit/common/gpu/webgraphicscontext3d_provider_impl.h"
+#include "webkit/common/gpu/context_provider_web_context.h"
 #include "webkit/common/quota/quota_types.h"
 #include "webkit/glue/simple_webmimeregistry_impl.h"
 #include "webkit/glue/webfileutilities_impl.h"
@@ -969,11 +970,11 @@ RendererWebKitPlatformSupportImpl::createOffscreenGraphicsContext3D(
 
 blink::WebGraphicsContext3DProvider* RendererWebKitPlatformSupportImpl::
     createSharedOffscreenGraphicsContext3DProvider() {
-  scoped_refptr<cc::ContextProvider> provider =
+  scoped_refptr<webkit::gpu::ContextProviderWebContext> provider =
       RenderThreadImpl::current()->SharedMainThreadContextProvider();
   if (!provider)
     return NULL;
-  return new webkit::gpu::WebGraphicsContext3DProviderImpl(provider);
+  return new WebGraphicsContext3DProviderImpl(provider);
 }
 
 //------------------------------------------------------------------------------
