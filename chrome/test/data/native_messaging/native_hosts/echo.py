67bd44dc17dee27f6c8f171c50495072c2c8926f
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-12 18:21:05 +0000

    Pass correct --parent-window value to native messaging hosts
    --parent-window is used to pass HWND of the chrome window when launching
    native messaging hosts. It was brokend with Aura because
    
    BUG=330777
    
    Review URL: https://codereview.chromium.org/115683005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244445 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/native_messaging/native_hosts/echo.py
+++ chrome/test/data/native_messaging/native_hosts/echo.py
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # All this client does is echo the text it receives back at the extension.
 
 import os
+import platform
 import sys
 import struct
 
@@ -23,13 +24,26 @@ def WriteMessage(message):
 def Main():
   message_number = 0
 
+  caller_url = None
+  parent_window = None
+
   if len(sys.argv) < 2:
     sys.stderr.write("URL of the calling application is not specified.\n")
-    return;
+    return 1
+  # TODO(sergeyu): Use argparse module to parse the arguments (not available in
+  # Python 2.6).
   for arg in sys.argv[1:]:
-    if not arg.startswith('--'):
+    if arg.startswith('--'):
+      if arg.startswith('--parent-window='):
+        parent_window = long(arg[len('--parent-window='):])
+    elif caller_url == None:
       caller_url = arg
-      break
+
+  if platform.system() == 'Windows':
+    import win32gui
+    if parent_window and not win32gui.IsWindow(parent_window):
+      sys.stderr.write('Invalid --parent-window.\n')
+      return 1
 
   while 1:
     # Read the message type (first 4 bytes).
@@ -64,10 +78,10 @@ def Main():
 
     message_number += 1
 
-    if not WriteMessage(
-        '{{"id": {0}, "echo": {1}, "caller_url": "{2}"}}'.format(
-          message_number, text, caller_url).encode('utf-8')):
+    message = '{{"id": {0}, "echo": {1}, "caller_url": "{2}"}}'.format(
+          message_number, text, caller_url).encode('utf-8')
+    if not WriteMessage(message):
       break
 
 if __name__ == '__main__':
-  Main()
+  sys.exit(Main())
