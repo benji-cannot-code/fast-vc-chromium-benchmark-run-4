5dc666ac0318141f9b08f1f1e0b1204f9455de78
skuhne@chromium.org <skuhne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-12 02:39:27 +0000

    Don't allow windowed but not pinned applications to mix with shortcuts in the shelf launcher
    At the moment running windowed applications can be mixed with pinned applications - which is incorrect (checked with kuscher).
    
    This CL is a pre-CL for the order preservation of all running applications in multi profile mode (trying to keep it small).
    
    BUG=309029
    TEST=unit test
    
    Review URL: https://codereview.chromium.org/133743005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244416 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/shelf/shelf_model.cc
+++ ash/shelf/shelf_model.cc
@@ -23,8 +23,8 @@ int LauncherItemTypeToWeight(LauncherItemType type) {
         return 0;
       case TYPE_BROWSER_SHORTCUT:
       case TYPE_APP_SHORTCUT:
-      case TYPE_WINDOWED_APP:
         return 1;
+      case TYPE_WINDOWED_APP:
       case TYPE_PLATFORM_APP:
         return 2;
       case TYPE_DIALOG:
@@ -39,8 +39,8 @@ int LauncherItemTypeToWeight(LauncherItemType type) {
     switch (type) {
       case TYPE_BROWSER_SHORTCUT:
       case TYPE_APP_SHORTCUT:
-      case TYPE_WINDOWED_APP:
         return 0;
+      case TYPE_WINDOWED_APP:
       case TYPE_PLATFORM_APP:
         return 1;
       case TYPE_APP_LIST:
