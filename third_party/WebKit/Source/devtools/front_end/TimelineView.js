93d72d1a88546dd12295b5c8c35e5feee65d4bcd
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-13 14:03:43 +0000

    TimelinePanel: a fix for Frames mode overview. It kept old records.
    Frame mode overview has its own array of frames. We have to clear it
    when we start new record session.
    
    BUG=
    R=alph@chromium.org
    
    Review URL: https://codereview.chromium.org/136683002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164965 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/TimelineView.js
+++ third_party/WebKit/Source/devtools/front_end/TimelineView.js
@@ -574,6 +574,7 @@ WebInspector.TimelineView.prototype = {
         this._windowEndTime = Infinity;
 
         this._resetView();
+        this.overviewControl().reset();
         this._windowFilter.reset();
         this._invalidateAndScheduleRefresh(true, true);
     },
