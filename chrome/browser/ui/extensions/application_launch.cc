3826e8d9e9d04c8c7fe658d069cde1c239a13005
koz@chromium.org <koz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-13 06:58:53 +0000

    Add PerAppSettingsService and use it to remember what desktop apps should create windows on.
    BUG=316062
    TBR=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/111553008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244466 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/extensions/application_launch.cc
+++ chrome/browser/ui/extensions/application_launch.cc
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/app_mode/app_mode_utils.h"
+#include "chrome/browser/apps/per_app_settings_service.h"
+#include "chrome/browser/apps/per_app_settings_service_factory.h"
 #include "chrome/browser/extensions/extension_prefs.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/extension_system.h"
@@ -345,6 +347,10 @@ WebContents* OpenEnabledApplication(const AppLaunchParams& params) {
       "Extensions.AppLaunchContainer", params.container, 100);
 
   if (extension->is_platform_app()) {
+    // Remember what desktop the launch happened on so that when the app opens a
+    // window we can open them on the right desktop.
+    PerAppSettingsServiceFactory::GetForBrowserContext(profile)->
+        SetDesktopLastLaunchedFrom(extension->id(), params.desktop_type);
 #if !defined(OS_CHROMEOS)
     SigninManager* signin_manager =
         SigninManagerFactory::GetForProfile(profile);
