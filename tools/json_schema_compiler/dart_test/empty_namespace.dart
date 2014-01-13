fb2aaacd51ac317551bdb9f0cd686797127e8626
DHNishi@gmail.com <DHNishi@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-13 17:21:45 +0000

    Rebase the Dart Generator test files for 2014.
    The test files include the year 2013 in the heading, and causes test
    failures when a 2014 generated file is tested.
    
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/134463006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244525 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/json_schema_compiler/dart_test/empty_namespace.dart
+++ tools/json_schema_compiler/dart_test/empty_namespace.dart
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
