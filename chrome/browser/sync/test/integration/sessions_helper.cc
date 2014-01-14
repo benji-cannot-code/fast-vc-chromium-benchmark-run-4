03646e4d785b1b438ed4e7af9f636f3aab46f24a
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-14 00:39:09 +0000

    sync: a few more fixes for sessions V2
    Caught by integraton tests. A nice part of this change is that in the v2 world, we no longer need "QuitLoopForSubtleTesting" and "test_weak_factory" stuff in the production code; it lives exclusively in SessionsHelper.
    
    BUG=98892
    R=zea@chromium.org
    
    Review URL: https://codereview.chromium.org/137163002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244621 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/test/integration/sessions_helper.cc
+++ chrome/browser/sync/test/integration/sessions_helper.cc
@@ -7,6 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
+#include "base/bind.h"
+#include "base/command_line.h"
+#include "base/memory/weak_ptr.h"
 #include "base/stl_util.h"
 #include "base/test/test_timeouts.h"
 #include "base/time/time.h"
@@ -15,10 +18,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/open_tabs_ui_delegate.h"
 #include "chrome/browser/sync/profile_sync_service.h"
 #include "chrome/browser/sync/profile_sync_service_factory.h"
+#include "chrome/browser/sync/sessions2/notification_service_sessions_router.h"
+#include "chrome/browser/sync/sessions2/sessions_sync_manager.h"
 #include "chrome/browser/sync/test/integration/profile_sync_service_harness.h"
 #include "chrome/browser/sync/test/integration/sync_datatype_helper.h"
 #include "chrome/browser/sync/test/integration/sync_test.h"
 #include "chrome/browser/ui/singleton_tabs.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "url/gurl.h"
 
@@ -120,6 +126,43 @@ bool OpenMultipleTabs(int index, const std::vector<GURL>& urls) {
   return WaitForTabsToLoad(index, urls);
 }
 
+namespace {
+
+class TabEventHandler : public browser_sync::LocalSessionEventHandler {
+ public:
+  TabEventHandler() : weak_factory_(this) {
+    base::MessageLoop::current()->PostDelayedTask(
+        FROM_HERE,
+        base::Bind(&TabEventHandler::QuitLoop, weak_factory_.GetWeakPtr()),
+        TestTimeouts::action_max_timeout());
+  }
+
+  virtual void OnLocalTabModified(
+      browser_sync::SyncedTabDelegate* modified_tab) OVERRIDE {
+    // Unwind to ensure SessionsSyncManager has processed the event.
+    base::MessageLoop::current()->PostTask(
+        FROM_HERE,
+        base::Bind(&TabEventHandler::QuitLoop, weak_factory_.GetWeakPtr()));
+  }
+
+  virtual void OnFaviconPageUrlsUpdated(
+      const std::set<GURL>& updated_page_urls) OVERRIDE {
+    // Unwind to ensure SessionsSyncManager has processed the event.
+    base::MessageLoop::current()->PostTask(
+        FROM_HERE,
+        base::Bind(&TabEventHandler::QuitLoop, weak_factory_.GetWeakPtr()));
+  }
+
+ private:
+  void QuitLoop() {
+    base::MessageLoop::current()->Quit();
+  }
+
+  base::WeakPtrFactory<TabEventHandler> weak_factory_;
+};
+
+}  // namespace
+
 bool WaitForTabsToLoad(int index, const std::vector<GURL>& urls) {
   DVLOG(1) << "Waiting for session to propagate to associator.";
   base::TimeTicks start_time = base::TimeTicks::Now();
@@ -137,10 +180,20 @@ bool WaitForTabsToLoad(int index, const std::vector<GURL>& urls) {
         return false;
       }
       if (!found) {
-        ProfileSyncServiceFactory::GetInstance()->GetForProfile(
-            test()->GetProfile(index))->GetSessionModelAssociatorDeprecated()->
-            BlockUntilLocalChangeForTest(TestTimeouts::action_max_timeout());
-        content::RunMessageLoop();
+        if (!CommandLine::ForCurrentProcess()->HasSwitch(
+            switches::kEnableSyncSessionsV2)) {
+          ProfileSyncServiceFactory::GetForProfile(test()->GetProfile(index))->
+              GetSessionModelAssociatorDeprecated()->
+              BlockUntilLocalChangeForTest(TestTimeouts::action_max_timeout());
+          content::RunMessageLoop();
+        } else {
+          TabEventHandler handler;
+          browser_sync::NotificationServiceSessionsRouter router(
+              test()->GetProfile(index),
+              syncer::SyncableService::StartSyncFlare());
+          router.StartRoutingTo(&handler);
+          content::RunMessageLoop();
+        }
       }
     }
   }
