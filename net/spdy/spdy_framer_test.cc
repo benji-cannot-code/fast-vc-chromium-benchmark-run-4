eda26ebb8d047f87e0d0512186a09ea6a833366f
jgraettinger@chromium.org <jgraettinger@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-14 20:32:18 +0000

    SPDY 4: Remove CREDENTIAL from SPDY 4.
    This lands server change 54240110 by hkhalil.
    
    R=rch@chromium.org
    
    Review URL: https://codereview.chromium.org/137403004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244797 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_framer_test.cc
+++ net/spdy/spdy_framer_test.cc
@@ -880,6 +880,11 @@ TEST_P(SpdyFramerTest, PushPromiseWithPromisedStreamIdZero) {
 }
 
 TEST_P(SpdyFramerTest, CreateCredential) {
+  // CREDENTIAL only exists in SPDY 3.
+  if (!IsSpdy3()) {
+    return;
+  }
+
   SpdyFramer framer(spdy_version_);
 
   {
@@ -901,23 +906,6 @@ TEST_P(SpdyFramerTest, CreateCredential) {
       'a',  'l',  ' ',  'c',
       'e',  'r',  't',
     };
-    const unsigned char kV4FrameData[] = {
-      0x00, 0x3b, 0x0A, 0x00,
-      0x00, 0x00, 0x00, 0x00,
-      0x00, 0x03, 0x00, 0x00,
-      0x00, 0x05, 'p',  'r',
-      'o',  'o',  'f',  0x00,
-      0x00, 0x00, 0x06, 'a',
-      ' ',  'c',  'e',  'r',
-      't',  0x00, 0x00, 0x00,
-      0x0C, 'a',  'n',  'o',
-      't',  'h',  'e',  'r',
-      ' ',  'c',  'e',  'r',
-      't',  0x00, 0x00, 0x00,
-      0x0A, 'f',  'i',  'n',
-      'a',  'l',  ' ',  'c',
-      'e',  'r',  't',
-    };
     SpdyCredential credential;
     credential.slot = 3;
     credential.proof = "proof";
@@ -925,15 +913,16 @@ TEST_P(SpdyFramerTest, CreateCredential) {
     credential.certs.push_back("another cert");
     credential.certs.push_back("final cert");
     scoped_ptr<SpdyFrame> frame(framer.CreateCredentialFrame(credential));
-    if (IsSpdy4()) {
-      CompareFrame(kDescription, *frame, kV4FrameData, arraysize(kV4FrameData));
-    } else {
-      CompareFrame(kDescription, *frame, kV3FrameData, arraysize(kV3FrameData));
-    }
+    CompareFrame(kDescription, *frame, kV3FrameData, arraysize(kV3FrameData));
   }
 }
 
 TEST_P(SpdyFramerTest, ParseCredentialFrameData) {
+  // CREDENTIAL only exists in SPDY 3.
+  if (!IsSpdy3()) {
+    return;
+  }
+
   SpdyFramer framer(spdy_version_);
 
   {
@@ -954,38 +943,13 @@ TEST_P(SpdyFramerTest, ParseCredentialFrameData) {
       'a',  'l',  ' ',  'c',
       'e',  'r',  't',
     };
-    const unsigned char kV4FrameData[] = {
-      0x00, 0x37, 0x0A, 0x00,
-      0x00, 0x00, 0x00, 0x00,
-      0x00, 0x03, 0x00, 0x00,
-      0x00, 0x05, 'p',  'r',
-      'o',  'o',  'f',  0x00,
-      0x00, 0x00, 0x06, 'a',
-      ' ',  'c',  'e',  'r',
-      't',  0x00, 0x00, 0x00,
-      0x0C, 'a',  'n',  'o',
-      't',  'h',  'e',  'r',
-      ' ',  'c',  'e',  'r',
-      't',  0x00,  0x00, 0x00,
-      0x0A, 'f',  'i',  'n',
-      'a',  'l',  ' ',  'c',
-      'e',  'r',  't',
-    };
 
     SpdyCredential credential;
-    if (IsSpdy4()) {
-      EXPECT_TRUE(SpdyFramer::ParseCredentialData(
-          reinterpret_cast<const char*>(kV4FrameData) +
-              framer.GetControlFrameHeaderSize(),
-          arraysize(kV4FrameData) - framer.GetControlFrameHeaderSize(),
-          &credential));
-    } else {
-      EXPECT_TRUE(SpdyFramer::ParseCredentialData(
-          reinterpret_cast<const char*>(kV3FrameData) +
-              framer.GetControlFrameHeaderSize(),
-          arraysize(kV3FrameData) - framer.GetControlFrameHeaderSize(),
-          &credential));
-    }
+    EXPECT_TRUE(SpdyFramer::ParseCredentialData(
+        reinterpret_cast<const char*>(kV3FrameData) +
+            framer.GetControlFrameHeaderSize(),
+        arraysize(kV3FrameData) - framer.GetControlFrameHeaderSize(),
+        &credential));
     EXPECT_EQ(3u, credential.slot);
     EXPECT_EQ("proof", credential.proof);
     EXPECT_EQ("a cert", credential.certs.front());
@@ -3613,6 +3577,11 @@ TEST_P(SpdyFramerTest, ReadWindowUpdate) {
 }
 
 TEST_P(SpdyFramerTest, ReadCredentialFrame) {
+  // CREDENTIAL only exists in SPDY 3.
+  if (!IsSpdy3()) {
+    return;
+  }
+
   SpdyCredential credential;
   credential.slot = 3;
   credential.proof = "proof";
@@ -3640,6 +3609,11 @@ TEST_P(SpdyFramerTest, ReadCredentialFrame) {
 }
 
 TEST_P(SpdyFramerTest, ReadCredentialFrameOneByteAtATime) {
+  // CREDENTIAL only exists in SPDY 3.
+  if (!IsSpdy3()) {
+    return;
+  }
+
   SpdyCredential credential;
   credential.slot = 3;
   credential.proof = "proof";
@@ -3671,6 +3645,11 @@ TEST_P(SpdyFramerTest, ReadCredentialFrameOneByteAtATime) {
 }
 
 TEST_P(SpdyFramerTest, ReadCredentialFrameWithNoPayload) {
+  // CREDENTIAL only exists in SPDY 3.
+  if (!IsSpdy3()) {
+    return;
+  }
+
   SpdyCredential credential;
   credential.slot = 3;
   credential.proof = "proof";
@@ -3691,6 +3670,11 @@ TEST_P(SpdyFramerTest, ReadCredentialFrameWithNoPayload) {
 }
 
 TEST_P(SpdyFramerTest, ReadCredentialFrameWithCorruptProof) {
+  // CREDENTIAL only exists in SPDY 3.
+  if (!IsSpdy3()) {
+    return;
+  }
+
   SpdyCredential credential;
   credential.slot = 3;
   credential.proof = "proof";
@@ -3713,6 +3697,11 @@ TEST_P(SpdyFramerTest, ReadCredentialFrameWithCorruptProof) {
 }
 
 TEST_P(SpdyFramerTest, ReadCredentialFrameWithCorruptCertificate) {
+  // CREDENTIAL only exists in SPDY 3.
+  if (!IsSpdy3()) {
+    return;
+  }
+
   SpdyCredential credential;
   credential.slot = 3;
   credential.proof = "proof";
@@ -3736,6 +3725,11 @@ TEST_P(SpdyFramerTest, ReadCredentialFrameWithCorruptCertificate) {
 
 // Regression test for parsing issue found in b/8278897.
 TEST_P(SpdyFramerTest, ReadCredentialFrameFollowedByAnotherFrame) {
+  // CREDENTIAL only exists in SPDY 3.
+  if (!IsSpdy3()) {
+    return;
+  }
+
   SpdyCredential credential;
   credential.slot = 3;
   credential.proof = "proof";
@@ -4359,6 +4353,11 @@ TEST_P(SpdyFramerTest, PushPromiseFrameFlags) {
 }
 
 TEST_P(SpdyFramerTest, CredentialFrameFlags) {
+  // CREDENTIAL only exists in SPDY 3.
+  if (!IsSpdy3()) {
+    return;
+  }
+
   for (int flags = 0; flags < 256; ++flags) {
     SCOPED_TRACE(testing::Message() << "Flags " << flags);
 
