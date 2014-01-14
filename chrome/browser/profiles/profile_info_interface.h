bcdaad24d24ef0f3fded956c8d62a4041fe63a18
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-14 19:29:01 +0000

    Revert 244760 "Track when profiles are last used."
    Speculatively reverted due to consistent linux asan failure for
    StartupBrowserCreatorTest.ProfilesWithoutPagesNotLaunched.
    
    http://build.chromium.org/p/chromium.memory/buildstatus?builder=Linux%20ASAN%20Tests%20%282%29&number=21744
    
    > Track when profiles are last used.
    > 
    > This will allow use to ignore profiles unused for a certain length of time (likely 28 days) when calculating statistics on number of profiles, number of signed-in profiles, etc.
    > 
    > BUG=295817
    > 
    > Review URL: https://codereview.chromium.org/134863002
    
    TBR=bcwhite@chromium.org
    
    Review URL: https://codereview.chromium.org/136153005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244789 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_info_interface.h
+++ chrome/browser/profiles/profile_info_interface.h
@@ -9,10 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/strings/string16.h"
 
-namespace base {
-class Time;
-}
-
 namespace gfx {
 class Image;
 }
@@ -26,8 +22,6 @@ class ProfileInfoInterface {
 
   virtual size_t GetIndexOfProfileWithPath(
       const base::FilePath& profile_path) const = 0;
-
-  virtual base::Time GetProfileActiveTimeAtIndex(size_t index) const = 0;
 
   virtual base::string16 GetNameOfProfileAtIndex(size_t index) const = 0;
 
