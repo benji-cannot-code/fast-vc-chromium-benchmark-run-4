3bc476034280b44867d9f09159db43275a48db0d
alph@chromium.org <alph@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-14 16:08:14 +0000

    Revert of Create our own blink_heap run_all_tests target to avoid piggy-backing on wtf's. (https://codereview.chromium.org/138133002/)
    Reason for revert:
    Broke compile.
    
    Original issue's description:
    > Create our own blink_heap run_all_tests target to avoid piggy-backing on wtf's.
    > 
    > We used to use wtf's run_all_tests target to run out blink heap unittests. That causes a problem when we want to initialize our heap since we cannot call Heap::init/shutdown from wtf's RunAllTests since it cannot/should not depend on blink heap.
    > 
    > R=ager@chromium.org, erik.corry@gmail.com, haraken@chromium.org, vegorov@chromium.org, zerny@chromium.org
    > BUG=
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=165073
    
    TBR=ager@chromium.org,erik.corry@gmail.com,haraken@chromium.org,vegorov@chromium.org,zerny@chromium.org,wibling@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=
    
    Review URL: https://codereview.chromium.org/138443002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165077 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/Init.cpp
+++ third_party/WebKit/Source/core/Init.cpp
@@ -45,7 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "XMLNSNames.h"
 #include "XMLNames.h"
 #include "core/css/MediaFeatureNames.h"
-#include "heap/Heap.h"
 #include "platform/EventTracer.h"
 #include "platform/Partitions.h"
 #include "platform/PlatformThreadData.h"
@@ -80,7 +79,6 @@ void init()
     QualifiedName::init();
     Partitions::init();
     EventTracer::initialize();
-    Heap::init();
 
     // Ensure that the main thread's thread-local data is initialized before
     // starting any worker threads.
@@ -91,7 +89,6 @@ void init()
 
 void shutdown()
 {
-    Heap::shutdown();
     Partitions::shutdown();
 }
 
