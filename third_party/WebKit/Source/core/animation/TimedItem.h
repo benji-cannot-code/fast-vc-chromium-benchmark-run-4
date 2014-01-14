ee7afeb441065739863fffacaac9698b46c5b605
dstockwell@chromium.org <dstockwell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-14 05:09:01 +0000

    Web Animations CSS: Delay animation-start event until notification from compositor.
    This aligns the timing of start events under Web Animations with the legacy
    implementation. Prior to this patch Web Animations would dispatch the start
    event for a compositor animation before the start time was known.
    
    This patch moves the skipping of redundant timing updates from Player to
    TimedItem allowing updateInheritedTime to be aware of when a Player gains
    a start time.
    
    This opens up opportunities to further simplify updateInheritedTime, but they
    have not been attempted in this patch to ease merging this fix.
    
    BUG=333059
    
    Review URL: https://codereview.chromium.org/133153003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165030 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/TimedItem.h
+++ third_party/WebKit/Source/core/animation/TimedItem.h
@@ -95,6 +95,7 @@ protected:
     // updateChildrenAndEffects.
     // Returns whether style recalc was triggered.
     bool updateInheritedTime(double inheritedTime) const;
+    void invalidate() const { m_needsUpdate = true; };
 
 private:
     // Returns whether style recalc was triggered.
@@ -136,6 +137,8 @@ private:
         double timeToEffectChange;
     } m_calculated;
     mutable bool m_isFirstSample;
+    mutable bool m_needsUpdate;
+    mutable double m_lastUpdateTime;
 
     // FIXME: Should check the version and reinherit time if inconsistent.
     const CalculatedTiming& ensureCalculated() const { return m_calculated; }
