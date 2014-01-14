f3539be820d37e42a64f25c4585efa7c56ebca6e
habib.virji@samsung.com <habib.virji@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-14 11:56:35 +0000

    SliderThumb is responsible for sending onchange for range input type. It is updated to send change event in its defaultEventHandler only for mouse up event. Previously onchange was triggered for any position change
    R=tkent             
    BUG=155747
    TEST=Range toggle test updated to generate onchange only on mouseUp.
    
    Review URL: https://codereview.chromium.org/125973004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165061 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/shadow/SliderThumbElement.cpp
+++ third_party/WebKit/Source/core/html/shadow/SliderThumbElement.cpp
@@ -306,7 +306,6 @@ void SliderThumbElement::setPositionFromPoint(const LayoutPoint& point)
     input->setValueFromRenderer(valueString);
     if (renderer())
         renderer()->setNeedsLayout();
-    input->dispatchFormControlChangeEvent();
 }
 
 void SliderThumbElement::startDragging()
@@ -327,6 +326,7 @@ void SliderThumbElement::stopDragging()
     m_inDragMode = false;
     if (renderer())
         renderer()->setNeedsLayout();
+    hostInput()->dispatchFormControlChangeEvent();
 }
 
 void SliderThumbElement::defaultEventHandler(Event* event)
