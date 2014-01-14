53d43ac8a8942be4622c3afac5792ddc5adfd3aa
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-14 22:41:31 +0000

    Revert 244591 "Move refresh_tokens_ to MutableProfileOAuth2Token..."
    Top crasher in canary. See bug 334251.
    
    > Move refresh_tokens_ to MutableProfileOAuth2TokenService.
    > 
    > This CL moves refresh_tokens_ from ProfileOAuth2TokenService to
    > MutableProfileOAuth2TokenService. It also removes some unused private
    > methods from ProfileOAuth2TokenService.
    > 
    > R=rogerta
    > TBR=sky, rohitrao
    > BUG=320625
    > 
    > Review URL: https://codereview.chromium.org/114513003
    
    TBR=msarda@chromium.org
    
    Review URL: https://codereview.chromium.org/132993006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244810 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/browser/profile_chooser_controller.mm
+++ chrome/browser/ui/cocoa/browser/profile_chooser_controller.mm
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/profiles/profile_window.h"
 #include "chrome/browser/profiles/profiles_state.h"
-#include "chrome/browser/signin/mutable_profile_oauth2_token_service.h"
 #include "chrome/browser/signin/profile_oauth2_token_service.h"
 #include "chrome/browser/signin/profile_oauth2_token_service_factory.h"
 #include "chrome/browser/signin/signin_manager.h"
@@ -251,7 +250,7 @@ class ActiveProfileObserverBridge : public AvatarMenuObserver,
   DCHECK_GE([sender tag], 0);  // Should not be called for the primary account.
   DCHECK(ContainsKey(currentProfileAccounts_, [sender tag]));
   std::string account = currentProfileAccounts_[[sender tag]];
-  ProfileOAuth2TokenServiceFactory::GetPlatformSpecificForProfile(
+  ProfileOAuth2TokenServiceFactory::GetForProfile(
       browser_->profile())->RevokeCredentials(account);
 }
 
--- chrome/browser/ui/views/profile_chooser_view.cc
+++ chrome/browser/ui/views/profile_chooser_view.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/profiles/profile_window.h"
 #include "chrome/browser/profiles/profiles_state.h"
-#include "chrome/browser/signin/mutable_profile_oauth2_token_service.h"
 #include "chrome/browser/signin/profile_oauth2_token_service.h"
 #include "chrome/browser/signin/profile_oauth2_token_service_factory.h"
 #include "chrome/browser/signin/signin_manager.h"
@@ -557,9 +556,8 @@ void ProfileChooserView::OnMenuButtonClicked(views::View* source,
       current_profile_accounts_map_.find(source);
   DCHECK(match != current_profile_accounts_map_.end());
 
-  MutableProfileOAuth2TokenService* oauth2_token_service =
-      ProfileOAuth2TokenServiceFactory::GetPlatformSpecificForProfile(
-          browser_->profile());
+  ProfileOAuth2TokenService* oauth2_token_service =
+      ProfileOAuth2TokenServiceFactory::GetForProfile(browser_->profile());
   if (oauth2_token_service)
     oauth2_token_service->RevokeCredentials(match->second);
 }
