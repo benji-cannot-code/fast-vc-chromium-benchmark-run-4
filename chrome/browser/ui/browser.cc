33f694b1684556a86da89ef0c452b989ec1c1855
miu@chromium.org <miu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-14 00:52:30 +0000

    Add Embedded Flash Fullscreen switch to chrome://flags.
    ...for users who do not have access to a command-line, or wish the switch setting to be sticky across browser runs.
    
    BUG=290403,256870
    TBR=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/131193010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244625 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -1566,7 +1566,6 @@ void Browser::EnumerateDirectory(WebContents* web_contents,
 }
 
 bool Browser::EmbedsFullscreenWidget() const {
-  // TODO(miu): Make this feature switchable in about:flags?
   return CommandLine::ForCurrentProcess()->
       HasSwitch(switches::kEmbedFlashFullscreen);
 }
