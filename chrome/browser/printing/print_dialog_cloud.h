780484a131ef35fc592b324e1930e802b4ae8425
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-14 15:38:31 +0000

    Replace signin dialog with browser tab. This should resolve unexpected behaivour user click links of sign in window.
    BUG=332294
    
    Review URL: https://codereview.chromium.org/131193009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244721 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/printing/print_dialog_cloud.h
+++ chrome/browser/printing/print_dialog_cloud.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string16.h"
 #include "ui/gfx/native_widget_types.h"
 
+class Browser;
 class CommandLine;
 class Profile;
 
@@ -62,15 +63,10 @@ void CreatePrintDialogForBytes(content::BrowserContext* browser_context,
 bool CreatePrintDialogFromCommandLine(Profile* profile,
                                       const CommandLine& command_line);
 
-// Creates a dialog for signing into cloud print.
-// The dialog will call |callback| when complete.
-// Called on the UI thread. Even though this starts up a modal
-// dialog, it will return immediately. The dialog is handled asynchronously.
-// If non-NULL, |modal_parent| specifies a window that the print dialog is modal
-// to.
-void CreateCloudPrintSigninDialog(content::BrowserContext* browser_context,
-                                  gfx::NativeWindow modal_parent,
-                                  const base::Closure& callback);
+// Creates a tab with Google sinnin page.
+// Calls |callback| when complete.
+void CreateCloudPrintSigninTab(Browser* browser,
+                               const base::Closure& callback);
 
 }  // namespace print_dialog_cloud
 
