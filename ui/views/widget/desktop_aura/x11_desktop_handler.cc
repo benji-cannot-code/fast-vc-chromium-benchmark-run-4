4ef0a146b681245e98ee339f107dccd30a3954ab
pauljensen@chromium.org <pauljensen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-14 19:26:15 +0000

    Revert 244779 "Call XSetInputFocus after XRaiseWindow when activ..." Looks like it's causing DesktopScreenX11Test.GetWindowAtScreenPoint to fail.
    > Call XSetInputFocus after XRaiseWindow when activating.
    > 
    > XRaiseWindow does not reliably set the focus. GTK code is doing the same
    > thing, see gdk/x11/gdkwindow-x11.c.
    > 
    > BUG=None
    > 
    > Review URL: https://codereview.chromium.org/137533006
    
    TBR=mlamouri@chromium.org
    
    Review URL: https://codereview.chromium.org/137793005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244787 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/desktop_aura/x11_desktop_handler.cc
+++ ui/views/widget/desktop_aura/x11_desktop_handler.cc
@@ -95,7 +95,6 @@ void X11DesktopHandler::ActivateWindow(::Window window) {
                &xclient);
   } else {
     XRaiseWindow(xdisplay_, window);
-    XSetInputFocus(xdisplay_, window, RevertToParent, CurrentTime);
     OnActiveWindowChanged(window);
   }
 }
