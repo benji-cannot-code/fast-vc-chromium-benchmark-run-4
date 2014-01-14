8c9722adc4bb4bf224bf83543a1015586a611804
vapier@chromium.org <vapier@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-14 18:52:34 +0000

    python_arch: return "unknown" when file does not exist
    The `file` program follows POSIX as outlined here:
    http://pubs.opengroup.org/onlinepubs/9699919799/utilities/file.html
    
    Specifically, this part causes troubles:
    If the file named by the file operand does not exist, cannot be read, or
    the type of the file named by the file operand cannot be determined, this
    shall not be considered an error that affects the exit status.
    
    With older versions of `file` (that does not conform to POSIX), this
    script will exit 0 with "unknown" as its output when "$1" does not exist.
    But with recent releases (that conform to POSIX), this script will exit 1
    and can break the build.
    
    Have the script explicitly check for the existence of $1 and if it does
    not exist, then runt he unknown logic like normal.
    
    BUG=chromium:332547
    TEST=ran python_arch.sh on bogus files with new & old `file` programs
    
    Review URL: https://codereview.chromium.org/137433002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244778 0039d316-1c4b-4281-b951-d872f2087c98

--- build/linux/python_arch.sh
+++ build/linux/python_arch.sh
@@ -12,7 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #
 
 file_out=$(file --dereference "$1")
-if [ $? -ne 0 ]; then
+# The POSIX spec says that `file` should not exit(1) if the file does not
+# exist, so do our own -e check to catch things.
+if [ $? -ne 0 ] || [ ! -e "$1" ] ; then
   echo unknown
   exit 0
 fi
