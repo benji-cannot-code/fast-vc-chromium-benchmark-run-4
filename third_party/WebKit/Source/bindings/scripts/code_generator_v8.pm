dd29cfe510b46b4b349118998e6a40c528b1a7ac
nbarth@chromium.org <nbarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-14 08:03:43 +0000

    Remove dead code path (typed array interfaces)
    Typed arrays all have custom bindings, so code path that checks if an
    interface is a typed array (IsTypedArray($interfaceName)) is dead code.
    
    This removes it to avoid being misleading (and clarify rewrite);
    no changes to generated code.
    
    Obviously we want to remove these custom bindings, but the existing dead
    code isn't helpful.
    
    R=haraken
    
    Review URL: https://codereview.chromium.org/129793007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165039 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/scripts/code_generator_v8.pm
+++ third_party/WebKit/Source/bindings/scripts/code_generator_v8.pm
@@ -491,8 +491,6 @@ sub HeaderFilesForInterface
     my @includes = ();
     if (IsPrimitiveType($interfaceName) or IsEnumType($interfaceName) or IsCallbackFunctionType($interfaceName)) {
         # Not interface type, no header files
-    } elsif (IsTypedArrayType($interfaceName)) {
-        push(@includes, "wtf/${interfaceName}.h");
     } else {
         my $idlFilename = Cwd::abs_path(IDLFileForInterface($interfaceName)) or die("Could NOT find IDL file for interface \"$interfaceName\" $!\n");
         my $idlRelPath = File::Spec->abs2rel($idlFilename, $sourceRoot);
@@ -5145,13 +5143,6 @@ END
         return wrapper;
 
 END
-    if (IsTypedArrayType($interface->name)) {
-      AddToImplIncludes("bindings/v8/custom/V8ArrayBufferCustom.h");
-      $code .= <<END;
-    impl->buffer()->setDeallocationObserver(V8ArrayBufferDeallocationObserver::instanceTemplate());
-END
-    }
-
     if (InheritsInterface($interface, "AudioBuffer")) {
       AddToImplIncludes("modules/webaudio/AudioBuffer.h");
       $code .= <<END;
