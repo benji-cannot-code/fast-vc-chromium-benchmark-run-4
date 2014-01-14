c4250803e0c1656fbf4dec8ff749210f20340fc2
dominicc@chromium.org <dominicc@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-14 11:39:05 +0000

    Remove Attr.prefix setter.
    Attr.prefix is readonly in DOM4. Use counters indicate setting
    Attr.prefix isn't used at all.
    
    BUG=305105
    
    Review URL: https://codereview.chromium.org/137733003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165058 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/dom/script-tests/node-prefix-setter-namespace-exception.js
+++ third_party/WebKit/LayoutTests/fast/dom/script-tests/node-prefix-setter-namespace-exception.js
@@ -1,22 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 description("Test how Node.prefix setter raises NAMESPACE_ERR.");
 
-var href = document.createAttribute("href");
-href.value = "#";
-
-// Should not throw.
-href.prefix = null;
-
-// Per DOM3 Core spec, setting to empty is implementation dependent.
-// Firefox treats empty like null.
-href.prefix = "";
-shouldBe("href.prefix", "null");
-
-shouldThrow("document.createAttribute('attr').prefix = 'abc'");
-shouldThrow("document.createAttributeNS(null, 'attr').prefix = 'abc'");
 shouldThrow("document.createElementNS(null, 'attr').prefix = 'abc'");
-shouldThrow("document.createAttributeNS('foo', 'bar').prefix = 'xml'");
 shouldThrow("document.createElementNS('foo', 'bar').prefix = 'xml'");
-shouldThrow("document.createAttribute('attr').prefix = 'xmlns'");
-shouldThrow("document.createAttributeNS('foo', 'attr').prefix = 'xmlns'");
-shouldThrow("document.createAttribute('xmlns').prefix = 'foo'");
