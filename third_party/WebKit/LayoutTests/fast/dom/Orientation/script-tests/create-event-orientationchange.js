bbb3df1c5b54460565fa50ce904cfb58d81c7127
mahesh.kk@samsung.com <mahesh.kk@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-14 08:21:29 +0000

    Remove compile time flag ORIENTATION_EVENT and use runtime flag instead. Update layout test case to use runtime flag.
    Depends on 131913003 & 132203007
    
    BUG=181136, 331337
    
    Review URL: https://codereview.chromium.org/137143002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165042 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/dom/Orientation/script-tests/create-event-orientationchange.js
+++ third_party/WebKit/LayoutTests/fast/dom/Orientation/script-tests/create-event-orientationchange.js
@@ -1,17 +1,21 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-description('Tests that document.createEvent() works with orientationChange')
+description('Tests that document.createEvent() works with orientationChange if enabled')
 
-function handleOrientationChange()
+function handleTestResult()
 {
     document.getElementById('result').innerHTML = "PASS";
 }
 
-window.addEventListener('orientationchange', handleOrientationChange, false);
+if (window.internals && internals.runtimeFlags.orientationEventEnabled) {
+    window.addEventListener('orientationchange', handleTestResult, false);
 
-try {
-    var event = document.createEvent("OrientationEvent");
-    event.initEvent("orientationchange", false, false);
-    window.dispatchEvent(event);
-} catch(e) {
-    document.getElementById('result').innerHTML = "FAIL... orientationChange event doesn't appear to be enabled or implemented.";
+    try {
+        var event = document.createEvent("OrientationEvent");
+        event.initEvent("orientationchange", false, false);
+        window.dispatchEvent(event);
+    } catch(e) {
+        document.getElementById('result').innerHTML = "FAIL... orientationChange event doesn't appear to be enabled or implemented.";
+    }
+} else {
+    handleTestResult();
 }
--- third_party/WebKit/Source/build/features.gypi
+++ third_party/WebKit/Source/build/features.gypi
@@ -59,7 +59,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           'ENABLE_INPUT_SPEECH=0',
           'ENABLE_LEGACY_NOTIFICATIONS=0',
           'ENABLE_MEDIA_CAPTURE=1',
-          'ENABLE_ORIENTATION_EVENTS=1',
           'ENABLE_TOUCH_ICON_LOADING=1',
         ],
       }, { # OS!="android"
@@ -68,7 +67,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           'ENABLE_INPUT_MULTIPLE_FIELDS_UI=1',
           'ENABLE_LEGACY_NOTIFICATIONS=1',
           'ENABLE_MEDIA_CAPTURE=0',
-          'ENABLE_ORIENTATION_EVENTS=0',
           'ENABLE_WEB_AUDIO=1',
         ],
       }],
