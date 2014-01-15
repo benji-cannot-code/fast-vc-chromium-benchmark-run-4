cb2d12c463154cbc23f6d7043e6060e4eae653cb
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-15 23:19:25 +0000

    Revert 245010 "Inject GPUDataManager support into ash to abstrac..."
    > Inject GPUDataManager support into ash to abstract a content dependency.
    > 
    > http://crbug.com/332504
    > 
    > R=oshima@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/138223004
    
    TBR=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/137893010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@245018 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/ash_with_content_export.h
+++ ash/ash_with_content_export.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef ASH_CONTENT_SUPPORT_ASH_WITH_CONTENT_EXPORT_H_
-#define ASH_CONTENT_SUPPORT_ASH_WITH_CONTENT_EXPORT_H_
+#ifndef ASH_ASH_WITH_CONTENT_EXPORT_H_
+#define ASH_ASH_WITH_CONTENT_EXPORT_H_
 
 // Defines ASH_EXPORT so that functionality implemented by the Ash module can
 // be exported to consumers.
@@ -30,4 +30,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_WITH_CONTENT_EXPORT
 #endif
 
-#endif  // ASH_CONTENT_SUPPORT_ASH_WITH_CONTENT_EXPORT_H_
+#endif  // ASH_ASH_WITH_CONTENT_EXPORT_H_
--- ash/keyboard_overlay/keyboard_overlay_delegate.h
+++ ash/keyboard_overlay/keyboard_overlay_delegate.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_KEYBOARD_OVERLAY_KEYBOARD_OVERLAY_DELEGATE_H_
 #define ASH_KEYBOARD_OVERLAY_KEYBOARD_OVERLAY_DELEGATE_H_
 
-#include "ash/content_support/ash_with_content_export.h"
+#include "ash/ash_with_content_export.h"
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
 #include "base/gtest_prod_util.h"
