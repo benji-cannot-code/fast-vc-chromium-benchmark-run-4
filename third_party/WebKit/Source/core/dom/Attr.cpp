f61897dfeeb58fdfab7195988dcc8e9327ff7851
dominicc@chromium.org <dominicc@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-15 14:08:58 +0000

    Remove detritus from Attr.
    BUG=305105
    
    Review URL: https://codereview.chromium.org/139033002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165126 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Attr.cpp
+++ third_party/WebKit/Source/core/dom/Attr.cpp
@@ -24,14 +24,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "core/dom/Attr.h"
 
-#include "XMLNSNames.h"
 #include "bindings/v8/ExceptionState.h"
 #include "bindings/v8/ExceptionStatePlaceholder.h"
 #include "core/dom/Element.h"
-#include "core/dom/ExceptionCode.h"
 #include "core/dom/Text.h"
 #include "core/events/ScopedEventQueue.h"
-#include "core/frame/UseCounter.h"
 #include "wtf/text/AtomicString.h"
 #include "wtf/text/StringBuilder.h"
 
