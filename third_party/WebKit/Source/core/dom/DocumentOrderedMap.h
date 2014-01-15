453c337194e685358adeb100162736b1eff3100c
ch.dumez@samsung.com <ch.dumez@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-15 10:43:20 +0000

    Remove dead code from DocumentOrderedMap
    Remove dead code from DocumentOrderedMap:
    - clear() method is never used
    - checkConsistency() is declared but unimplemented
    - "wtf/HashCountedSet.h" include is no longer needed
    
    R=esprehn, adamk
    
    Review URL: https://codereview.chromium.org/138573003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165118 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/DocumentOrderedMap.h
+++ third_party/WebKit/Source/core/dom/DocumentOrderedMap.h
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef DocumentOrderedMap_h
 #define DocumentOrderedMap_h
 
-#include "wtf/HashCountedSet.h"
 #include "wtf/HashMap.h"
 #include "wtf/Vector.h"
 #include "wtf/text/StringImpl.h"
@@ -46,7 +45,6 @@ class DocumentOrderedMap {
 public:
     void add(StringImpl*, Element*);
     void remove(StringImpl*, Element*);
-    void clear();
 
     bool contains(StringImpl*) const;
     bool containsMultiple(StringImpl*) const;
@@ -56,8 +54,6 @@ public:
     Element* getElementByMapName(StringImpl*, const TreeScope*) const;
     Element* getElementByLowercasedMapName(StringImpl*, const TreeScope*) const;
     Element* getElementByLabelForAttribute(StringImpl*, const TreeScope*) const;
-
-    void checkConsistency() const;
 
 private:
     template<bool keyMatches(StringImpl*, Element*)> Element* get(StringImpl*, const TreeScope*) const;
