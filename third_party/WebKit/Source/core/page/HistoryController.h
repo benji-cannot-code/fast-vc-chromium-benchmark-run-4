36ef9a7b09d42a30a53f26030c15c1a59a4fffec
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-15 18:27:27 +0000

    When exporting a HistoryItem, always export the main frame's item.
    This was regressed in blink r162228, where it switched to exporting the item
    for whichever frame requested the HistoryItem.
    
    BUG=334293
    TEST=WebFrameTest.ExportHistoryItemFromChildFrame
    
    Review URL: https://codereview.chromium.org/133683007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165155 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/HistoryController.h
+++ third_party/WebKit/Source/core/page/HistoryController.h
@@ -145,9 +145,8 @@ public:
     void updateBackForwardListForFragmentScroll(Frame*, HistoryItem*);
     void updateForCommit(Frame*, HistoryItem*, HistoryCommitType);
 
-    PassRefPtr<HistoryItem> currentItemForExport(Frame*);
-    PassRefPtr<HistoryItem> previousItemForExport(Frame*);
-    PassRefPtr<HistoryItem> provisionalItemForExport(Frame*);
+    PassRefPtr<HistoryItem> currentItemForExport();
+    PassRefPtr<HistoryItem> previousItemForExport();
     HistoryItem* itemForNewChildFrame(Frame*) const;
 
     void setDefersLoading(bool);
