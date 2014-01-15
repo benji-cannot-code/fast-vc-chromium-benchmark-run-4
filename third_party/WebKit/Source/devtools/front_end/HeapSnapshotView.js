16faf50011c49010266c059d12f3669d7a4551b6
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-15 15:40:48 +0000

    Use mock temp file in profiler tests
    Mock temp file implementation is used in all inspector tests instead of the real implementation based on temporary filesystem which (unlike temp fs in regular DevTools window) has limited quota which is often exceeded in the tests.
    
    This should fix flakiness in the following tests:
    inspector/profiler/heap-snapshot-loader.html
    inspector/profiler/cpu-profiler-save-load.html
    
    Also a bug in BufferedTempFileWriter.close was fixed.
    
    TempFile.getFile was replaced with writeToOutputSteam which operates on types defined in DevTools and simplifies mocks.
    
    BUG=330617,333860
    R=alph@chromium.org
    
    Review URL: https://codereview.chromium.org/132993009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165136 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/HeapSnapshotView.js
+++ third_party/WebKit/Source/devtools/front_end/HeapSnapshotView.js
@@ -1302,6 +1302,8 @@ WebInspector.HeapProfileHeader.prototype = {
 
     _didCompleteSnapshotTransfer: function()
     {
+        if (!this._snapshotProxy)
+            return;
         this.sidebarElement.subtitle = Number.bytesToString(this._snapshotProxy.totalSize);
         this.sidebarElement.wait = false;
     },
@@ -1373,33 +1375,13 @@ WebInspector.HeapProfileHeader.prototype = {
         {
             if (!accepted)
                 return;
-            /**
-             * @param {!File} file
-             * @this {WebInspector.HeapProfileHeader}
-             */
-            function didGetFile(file)
-            {
-                if (!file) {
-                    reportTempFileError();
-                    return;
-                }
-
-                var delegate = new WebInspector.SaveSnapshotOutputStreamDelegate(this);
-                var reader = new WebInspector.ChunkedFileReader(file, 10*1000*1000, delegate)
-                reader.start(fileOutputStream);
-            }
-
-            function reportTempFileError()
-            {
+            if (this._failedToCreateTempFile) {
                 WebInspector.log("Failed to open temp file with heap snapshot",
                                  WebInspector.ConsoleMessage.MessageLevel.Error);
                 fileOutputStream.close();
-            }
-
-            if (this._failedToCreateTempFile) {
-                reportTempFileError();
             } else if (this._tempFile) {
-                this._tempFile.getFile(didGetFile.bind(this));
+                var delegate = new WebInspector.SaveSnapshotOutputStreamDelegate(this);
+                this._tempFile.writeToOutputSteam(fileOutputStream, delegate);
             } else {
                 this._onTempFileReady = onOpen.bind(this, accepted);
                 this._updateSaveProgress(0, 1);
