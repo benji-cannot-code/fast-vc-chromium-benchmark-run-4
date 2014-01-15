33b27462998973190902d0ba86cec2bda4019780
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-15 23:17:40 +0000

    Revert 245012 "Add include"
    > Add include
    > 
    > TBR=sky@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/134173003
    
    TBR=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/138733004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@245015 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/content_support/inject.cc
+++ ash/content_support/inject.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/content_support/gpu_support_impl.h"
 #include "ash/shell.h"
-#include "base/memory/scoped_ptr.h"
 
 namespace ash {
 
