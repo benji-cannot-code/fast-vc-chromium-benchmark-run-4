4b1a2d2a2cecc81f727342bd9af7abfb45ac6102
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-15 17:36:42 +0000

    new tab creation can also fail right after we log in.
    This is most likely a fallout of crrev.com/239532
    
    Timeout for SyncRequest is 60 sec, which is too long for the initial tab creation/navigation which could fail. 
    
    Also catch TabCrashExceptions for TabListBackend.New()
    
    BUG=331940
    TEST=manual
    
    Review URL: https://codereview.chromium.org/132193009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244920 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/backends/chrome/tab_list_backend.py
+++ tools/telemetry/telemetry/core/backends/chrome/tab_list_backend.py
@@ -26,7 +26,7 @@ class TabListBackend(object):
     assert self._browser_backend.supports_tab_control
 
     self._browser_backend.Request('new', timeout=timeout)
-    return self[-1]
+    return self.Get(-1, None, timeout=timeout)
 
   def DoesDebuggerUrlExist(self, debugger_url):
     return self._FindTabInfo(debugger_url) is not None
@@ -82,7 +82,7 @@ class TabListBackend(object):
     self._UpdateTabList()
     return len(self._tab_list)
 
-  def Get(self, index, ret):
+  def Get(self, index, ret, timeout=60):
     """Returns self[index] if it exists, or ret if index is out of bounds.
     """
     self._UpdateTabList()
@@ -95,7 +95,8 @@ class TabListBackend(object):
       backend = inspector_backend.InspectorBackend(
           self._browser_backend.browser,
           self._browser_backend,
-          debugger_url)
+          debugger_url,
+          timeout=timeout)
       tab_object = tab.Tab(backend)
       self._tab_dict[debugger_url] = tab_object
     return tab_object
