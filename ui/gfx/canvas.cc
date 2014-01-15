5a7e886ad45e8205f1f894e0441a75988438e19b
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-15 00:28:04 +0000

    Removes duplicated include.
    R=ben@chromium.org
    TBR=ben@chromium.org
    BUG=none
    
    Review URL: https://codereview.chromium.org/136793011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244816 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/canvas.cc
+++ ui/gfx/canvas.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "third_party/skia/include/effects/SkGradientShader.h"
-#include "ui/gfx/canvas.h"
 #include "ui/gfx/font_list.h"
 #include "ui/gfx/rect.h"
 #include "ui/gfx/size_conversions.h"
