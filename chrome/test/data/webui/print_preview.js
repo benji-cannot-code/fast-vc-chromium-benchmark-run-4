a631fe558404e9117e4f65d6190f338e36a8b815
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-15 23:47:49 +0000

    Revert 244086 "Reset only invalid custom margins." Fixed the issue but introduced regressions.
    > Reset only invalid custom margins.
    > 
    > BUG=332183
    > 
    > Review URL: https://codereview.chromium.org/131013005
    
    TBR=vitalybuka@chromium.org
    
    Review URL: https://codereview.chromium.org/140313002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@245020 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/webui/print_preview.js
+++ chrome/test/data/webui/print_preview.js
@@ -723,7 +723,7 @@ TEST_F('PrintPreviewWebUITest', 'TestPrinterChangeUpdatesPreview', function() {
 
   var previewGenerator = mock(print_preview.PreviewGenerator);
   printPreview.previewArea_.previewGenerator_ = previewGenerator.proxy();
-  previewGenerator.expects(atLeastOnce()).requestPreview();
+  previewGenerator.expects(exactly(6)).requestPreview();
 
   var barDestination;
   var destinations = printPreview.destinationStore_.destinations;
