bb44b22703031fcfef5cabf7edd803c356d4c45f
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-15 18:41:53 +0000

    keyboard: Make sure the event handlers are destroyed/removed correctly.
    BUG=none
    R=kevers@chromium.org
    
    Review URL: https://codereview.chromium.org/132423003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244942 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/keyboard/keyboard_controller_unittest.cc
+++ ui/keyboard/keyboard_controller_unittest.cc
@@ -208,6 +208,7 @@ class KeyboardControllerTest : public testing::Test {
   }
 
   virtual void TearDown() OVERRIDE {
+    controller_.reset();
     focus_controller_.reset();
     aura_test_helper_->TearDown();
   }
@@ -294,6 +295,7 @@ TEST_F(KeyboardControllerTest, ClickDoesNotFocusKeyboard) {
   generator.MoveMouseTo(gfx::Point());
   generator.ClickLeftButton();
   EXPECT_EQ("1 1", delegate.GetMouseButtonCountsAndReset());
+  keyboard_container->RemovePreTargetHandler(&observer);
 }
 
 TEST_F(KeyboardControllerTest, VisibilityChangeWithTextInputTypeChange) {
