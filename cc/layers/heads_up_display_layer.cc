9b6c91712bee8d1d3afa828a2d757a461e8f364c
vollick@chromium.org <vollick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-15 18:51:40 +0000

    Plumb debug name via debug info
    There used to be a separate channel for plumbing back a layer's debug name. The
    debug info object is more general, and we can just send the debug name through
    it. This patch depends on https://codereview.chromium.org/135473002/
    
    R=jamesr@chromium.org
    BUG=None
    
    Review URL: https://codereview.chromium.org/135493002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244948 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/layers/heads_up_display_layer.cc
+++ cc/layers/heads_up_display_layer.cc
@@ -56,8 +56,4 @@ scoped_ptr<LayerImpl> HeadsUpDisplayLayer::CreateLayerImpl(
       PassAs<LayerImpl>();
 }
 
-std::string HeadsUpDisplayLayer::DebugName() {
-  return std::string("Heads Up Display Layer");
-}
-
 }  // namespace cc
