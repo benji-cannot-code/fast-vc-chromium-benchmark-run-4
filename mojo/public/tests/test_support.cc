f3c00782cda3790ef4c3ee79747925e38cb56a7d
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-15 20:08:43 +0000

    Mojo: Remove some base/ dependencies from mojo/public/tests/.
    mojo/public/tests/ shouldn't depend on stuff outside of public/.
    
    (Still left to do: a couple dependencies in test_support.cc.)
    
    R=darin@chromium.org
    
    Review URL: https://codereview.chromium.org/139843002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244966 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/public/tests/test_support.cc
+++ mojo/public/tests/test_support.cc
@@ -59,7 +59,8 @@ bool ReadTextMessage(MessagePipeHandle handle, std::string* text) {
 }
 
 void IterateAndReportPerf(const char* test_name,
-                          base::Callback<void()> single_iteration) {
+                          PerfTestSingleIteration single_iteration,
+                          void* closure) {
   // TODO(vtl): These should be specifiable using command-line flags.
   static const size_t kGranularity = 100;
   static const double kPerftestTimeSeconds = 3.0;
@@ -69,7 +70,7 @@ void IterateAndReportPerf(const char* test_name,
   size_t iterations = 0;
   do {
     for (size_t i = 0; i < kGranularity; i++)
-      single_iteration.Run();
+      (*single_iteration)(closure);
     iterations += kGranularity;
 
     end_time = base::TimeTicks::HighResNow();
--- mojo/public/tests/test_support.h
+++ mojo/public/tests/test_support.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "base/callback.h"
 #include "mojo/public/system/core_cpp.h"
 
 namespace mojo {
@@ -20,8 +19,10 @@ bool ReadTextMessage(MessagePipeHandle handle, std::string* text);
 // Run |single_iteration| an appropriate number of times and report its
 // performance appropriately. (This actually runs |single_iteration| for a fixed
 // amount of time and reports the number of iterations per unit time.)
+typedef void (*PerfTestSingleIteration)(void* closure);
 void IterateAndReportPerf(const char* test_name,
-                          base::Callback<void()> single_iteration);
+                          PerfTestSingleIteration single_iteration,
+                          void* closure);
 
 MojoResult WriteEmptyMessage(const MessagePipeHandle& handle);
 MojoResult ReadEmptyMessage(const MessagePipeHandle& handle);
