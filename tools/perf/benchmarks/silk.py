49acc4a16f90ec7aa31112628c50d772274ad89b
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-15 05:46:39 +0000

    Timeline cleanups for silk measurement
    The main point of this is to add some basic features into silk measurement:
    - Split the silk measurement out to its own thing
    - Add detailed reporting of where time goes on the render thread
    
    But, in support of this, I added much-needed test coverage:
    - Move timeline options from construction time to properties to reduce amount of glue code
    - Add smoke test coverage for the timeline-based metrics
    - Add unit test of ThreadTimesTimelineMetric that uses pure timeline instead of full data
    - Split some functions out of ThreadTimesTimelineMetric loop allow more drop in features
    - Allow Timeline to be finalized outside of ImportEvents for use by unit tests that hand-construct timelines
    
    R=ernstm,tonyg
    BUG=332447,332442
    
    Review URL: https://codereview.chromium.org/111013003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244837 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/benchmarks/silk.py
+++ tools/perf/benchmarks/silk.py
@@ -1,16 +1,16 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-# Copyright 2013 The Chromium Authors. All rights reserved.
+# Copyright 2014 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 from telemetry import test
 
-from measurements import smoothness
-
+from measurements import thread_times
 
 class SilkKeySilkCases(test.Test):
-  """Measures timeline metrics while scrolling down the key_silk_cases pages.
+  """Measures timeline metrics while performing smoothness action on
+  key silk cases.
 
   http://www.chromium.org/developers/design-documents/rendering-benchmarks"""
-  test = smoothness.Smoothness
+  test = thread_times.ThreadTimes
   page_set = 'page_sets/key_silk_cases.json'
-  options = {'metric': 'timeline'}
+  options = {"report_silk_details": True}
