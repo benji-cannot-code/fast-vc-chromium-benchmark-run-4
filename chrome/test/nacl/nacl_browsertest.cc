8e6c6f32d2b7b42f98228eb8b0a8f6ed0e90b5d4
ncbray@chromium.org <ncbray@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-15 19:14:35 +0000

    Port NaCl mime handling test from nacl_integration to browser_tests.
    BUG=154400
    
    Review URL: https://codereview.chromium.org/135013002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244957 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/nacl/nacl_browsertest.cc
+++ chrome/test/nacl/nacl_browsertest.cc
@@ -12,7 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #define TELEMETRY 1
 
+#include "base/command_line.h"
 #include "base/environment.h"
+#include "base/path_service.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/test/nacl/nacl_browsertest_util.h"
 
 namespace {
@@ -275,6 +278,28 @@ IN_PROC_BROWSER_TEST_F(NaClBrowserTestNewlibStderrPM, RedirectFg1) {
 IN_PROC_BROWSER_TEST_F(NaClBrowserTestNewlibStderrPM, RedirectBg1) {
   RunNaClIntegrationTest(FILE_PATH_LITERAL(
       "pm_redir_test.html?stream=stderr&thread=bg&delay_us=1000000"));
+}
+
+class NaClBrowserTestNewlibExtension : public NaClBrowserTestNewlib {
+ public:
+  virtual void SetUpCommandLine(CommandLine* command_line) OVERRIDE {
+    NaClBrowserTestNewlib::SetUpCommandLine(command_line);
+    base::FilePath src_root;
+    ASSERT_TRUE(PathService::Get(base::DIR_SOURCE_ROOT, &src_root));
+
+    base::FilePath document_root;
+    ASSERT_TRUE(GetDocumentRoot(&document_root));
+
+    // Document root is relative to source root, and source root may not be CWD.
+    command_line->AppendSwitchPath(switches::kLoadExtension,
+                                   src_root.Append(document_root));
+  }
+};
+
+// TODO(ncbray) support glibc and PNaCl
+IN_PROC_BROWSER_TEST_F(NaClBrowserTestNewlibExtension, MimeHandler) {
+  RunNaClIntegrationTest(FILE_PATH_LITERAL(
+      "ppapi_extension_mime_handler.html"));
 }
 
 }  // namespace
