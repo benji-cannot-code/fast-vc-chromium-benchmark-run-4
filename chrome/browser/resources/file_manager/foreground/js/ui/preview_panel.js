567cc6efb8b433516a0d8aeccccfd722c923ab7f
hirono@chromium.org <hirono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-15 17:37:41 +0000

    Files.app: Update the preview panel when its visibility type is changed.
    Previously the preview panel is not updated when the its visibility type is
    changed. This CL adds a method call to update the panel.
    
    BUG=332646
    TEST=manually
    
    Review URL: https://codereview.chromium.org/133643002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244923 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/foreground/js/ui/preview_panel.js
+++ chrome/browser/resources/file_manager/foreground/js/ui/preview_panel.js
@@ -160,6 +160,9 @@ PreviewPanel.prototype = {
   set visibilityType(visibilityType) {
     this.visibilityType_ = visibilityType;
     this.updateVisibility_();
+    // Also update the preview area contents, because the update is surpressed
+    // while the visibility is hiding or hidden.
+    this.updatePreviewArea_();
   },
 
   get visible() {
@@ -183,8 +186,10 @@ PreviewPanel.prototype = {
 PreviewPanel.prototype.initialize = function() {
   this.element_.addEventListener('webkitTransitionEnd',
                                  this.onTransitionEnd_.bind(this));
-  this.updatePreviewArea_();
   this.updateVisibility_();
+  // Also update the preview area contents, because the update is surpressed
+  // while the visibility is hiding or hidden.
+  this.updatePreviewArea_();
 };
 
 /**
@@ -195,9 +200,7 @@ PreviewPanel.prototype.setSelection = function(selection) {
   this.sequence_++;
   this.selection_ = selection;
   this.updateVisibility_();
-  // If the previw panel is hiding, does not update the current view.
-  if (this.visible)
-    this.updatePreviewArea_();
+  this.updatePreviewArea_();
 };
 
 /**
@@ -249,6 +252,9 @@ PreviewPanel.prototype.updateVisibility_ = function() {
  * @private
  */
 PreviewPanel.prototype.updatePreviewArea_ = function(breadCrumbsVisible) {
+  // If the previw panel is hiding, does not update the current view.
+  if (!this.visible)
+    return;
   var selection = this.selection_;
 
   // Update thumbnails.
