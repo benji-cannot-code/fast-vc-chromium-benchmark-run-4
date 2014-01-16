4e3aa24ae74753ce31c1a76c3418370a36cde649
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-16 05:51:33 +0000

    Remove stale comment about Chrome Frame.
    BUG=316496
    NOTRY=true
    R=msw@chromium.org
    
    Review URL: https://codereview.chromium.org/138053005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@245153 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/url_constants.h
+++ chrome/common/url_constants.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chrome {
 
-// TODO(msw): Resolve chrome_frame dependency on these constants.
 extern const char kAboutComponentsURL[];
 extern const char kAboutPluginsURL[];
 extern const char kAboutVersionURL[];
