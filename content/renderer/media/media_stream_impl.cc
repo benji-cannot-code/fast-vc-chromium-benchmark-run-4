174ee15a873320afbb484fa86d66d066509dafd9
xians@chromium.org <xians@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-16 16:03:04 +0000

    Cleaned up the WebRtcAudioCapturer a bit.
    This CL cleaned up the capturer a bit by moving the initialize() method to private and do NOT allow re-initializing the capturer when a new getUserMedia is called.
    Instead, it creates a new capturer for each getUserMedia.
    
    BUG=333285
    TEST=content_unittests
    
    Review URL: https://codereview.chromium.org/133903004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@245205 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/media_stream_impl.cc
+++ content/renderer/media/media_stream_impl.cc
@@ -729,9 +729,8 @@ void MediaStreamImpl::StopLocalSource(
            << "{device_id = " << extra_data->device_info().device.id << "})";
 
   if (source.type() == blink::WebMediaStreamSource::TypeAudio) {
-    if (extra_data->GetAudioCapturer()) {
+    if (extra_data->GetAudioCapturer())
       extra_data->GetAudioCapturer()->Stop();
-    }
   }
 
   if (notify_dispatcher)
@@ -802,19 +801,13 @@ bool MediaStreamImpl::GetAuthorizedDeviceInfoForAudioRenderer(
     int* output_sample_rate,
     int* output_frames_per_buffer) {
   DCHECK(CalledOnValidThread());
-
   WebRtcAudioDeviceImpl* audio_device =
       dependency_factory_->GetWebRtcAudioDevice();
   if (!audio_device)
     return false;
 
-  if (!audio_device->GetDefaultCapturer())
-    return false;
-
-  return audio_device->GetDefaultCapturer()->GetPairedOutputParameters(
-      session_id,
-      output_sample_rate,
-      output_frames_per_buffer);
+  return audio_device->GetAuthorizedDeviceInfoForAudioRenderer(
+      session_id, output_sample_rate, output_frames_per_buffer);
 }
 
 MediaStreamSourceExtraData::MediaStreamSourceExtraData(
--- content/renderer/media/rtc_peer_connection_handler.cc
+++ content/renderer/media/rtc_peer_connection_handler.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "content/public/common/content_switches.h"
 #include "content/renderer/media/media_stream_dependency_factory.h"
+#include "content/renderer/media/media_stream_source_extra_data.h"
 #include "content/renderer/media/peer_connection_tracker.h"
 #include "content/renderer/media/remote_media_stream_impl.h"
 #include "content/renderer/media/rtc_data_channel_handler.h"
@@ -550,13 +551,16 @@ bool RTCPeerConnectionHandler::addStream(
         this, stream, PeerConnectionTracker::SOURCE_LOCAL);
 
   // A media stream is connected to a peer connection, enable the
-  // peer connection mode for the capturer.
-  WebRtcAudioDeviceImpl* audio_device =
-      dependency_factory_->GetWebRtcAudioDevice();
-  if (audio_device) {
-    WebRtcAudioCapturer* capturer = audio_device->GetDefaultCapturer();
-    if (capturer)
-      capturer->EnablePeerConnectionMode();
+  // peer connection mode for the sources.
+  blink::WebVector<blink::WebMediaStreamTrack> audio_tracks;
+  stream.audioTracks(audio_tracks);
+  for (size_t i = 0; i < audio_tracks.size(); ++i) {
+    const blink::WebMediaStreamSource& source = audio_tracks[i].source();
+    MediaStreamSourceExtraData* extra_data =
+        static_cast<MediaStreamSourceExtraData*>(source.extraData());
+    // |extra_data| is NULL if the track is a remote audio track.
+    if (extra_data && extra_data->GetAudioCapturer())
+      extra_data->GetAudioCapturer()->EnablePeerConnectionMode();
   }
 
   return AddStream(stream, &constraints);
