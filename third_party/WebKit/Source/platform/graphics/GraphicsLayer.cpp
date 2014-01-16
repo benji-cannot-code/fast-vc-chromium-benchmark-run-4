9660600772f8ab4832dd415d3fc3cdbaf39aa1fc
vollick@chromium.org <vollick@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-16 15:19:41 +0000

    Remove DebugName from the public API
    Now that cc is getting debug name info for layers via debug info, we can remove
    the related plumbing in blink. This depends on
    https://codereview.chromium.org/135493002/
    
    R=jamesr@chromium.org
    BUG=None
    
    Review URL: https://codereview.chromium.org/135503002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165227 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/GraphicsLayer.cpp
+++ third_party/WebKit/Source/platform/graphics/GraphicsLayer.cpp
@@ -508,11 +508,6 @@ GraphicsLayerDebugInfo& GraphicsLayer::debugInfo()
     return m_debugInfo;
 }
 
-blink::WebGraphicsLayerDebugInfo* GraphicsLayer::takeDebugInfo()
-{
-    return m_debugInfo.clone();
-}
-
 blink::WebGraphicsLayerDebugInfo* GraphicsLayer::takeDebugInfoFor(WebLayer* layer)
 {
     GraphicsLayerDebugInfo* clone = m_debugInfo.clone();
@@ -777,7 +772,7 @@ String GraphicsLayer::layerTreeAsText(LayerTreeFlags flags) const
     return ts.release();
 }
 
-blink::WebString GraphicsLayer::debugName(blink::WebLayer* webLayer)
+String GraphicsLayer::debugName(blink::WebLayer* webLayer) const
 {
     String name;
     if (!m_client)
