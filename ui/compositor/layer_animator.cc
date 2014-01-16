5b8961be6ca75769f73454ff804b5e11b070c304
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-16 00:22:55 +0000

    This CL - Introduces views::corewm::SetWindowShowAnimationDuration() and views::corewm::SetWindowHideAnimationDuration(). This allows the animation duration for showing and hiding the window to be different. - Fixes a memory leak in AddLayerAnimationsForRotate() - Fixes calls to views::corewm::SetWindowVisibilityAnimationType() and views::corewm::SetWindowVisibilityAnimationDuration(). In particular, the animation type and animation duration were not properly reset. So docking a window would permanently affect the duration of the show and hide animations. - Removes unneeded ScopedLayerAnimationSettings::LockTransitionDuration(). This API is not currently necessary so it is best for it to not exist at this point in time.
    BUG=323791
    TEST=None
    R=varkha, oshima
    TBR=vollick
    
    Review URL: https://codereview.chromium.org/105673008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@245031 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/compositor/layer_animator.cc
+++ ui/compositor/layer_animator.cc
@@ -56,7 +56,6 @@ gfx::AnimationContainer* GetAnimationContainer() {
 LayerAnimator::LayerAnimator(base::TimeDelta transition_duration)
     : delegate_(NULL),
       preemption_strategy_(IMMEDIATELY_SET_NEW_TARGET),
-      is_transition_duration_locked_(false),
       transition_duration_(transition_duration),
       tween_type_(gfx::Tween::LINEAR),
       is_started_(false),
@@ -119,10 +118,6 @@ ANIMATED_PROPERTY(bool, VISIBILITY, Visibility, bool, visibility);
 ANIMATED_PROPERTY(float, BRIGHTNESS, Brightness, float, brightness);
 ANIMATED_PROPERTY(float, GRAYSCALE, Grayscale, float, grayscale);
 ANIMATED_PROPERTY(SkColor, COLOR, Color, SkColor, color);
-
-base::TimeDelta LayerAnimator::GetTransitionDuration() const {
-  return transition_duration_;
-}
 
 void LayerAnimator::SetDelegate(LayerAnimationDelegate* delegate) {
   delegate_ = delegate;
@@ -804,10 +799,8 @@ void LayerAnimator::OnScheduled(LayerAnimationSequence* sequence) {
   sequence->OnScheduled();
 }
 
-void LayerAnimator::SetTransitionDuration(base::TimeDelta duration) {
-  if (is_transition_duration_locked_)
-    return;
-  transition_duration_ = duration;
+base::TimeDelta LayerAnimator::GetTransitionDuration() const {
+  return transition_duration_;
 }
 
 void LayerAnimator::ClearAnimationsInternal() {
