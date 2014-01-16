b7b51c301f7800ea7ce29587d2f0c6744e8e2015
jsbell@chromium.org <jsbell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-16 02:17:04 +0000

    IndexedDB: Prevent terminated worker from hanging connections
    IndexedDB database connection shutdown is normally blocked on waiting
    for transactions to finish. And transaction finishing requires an
    abort/onAbort message round-trip to the back-end. When a Worker
    context is stopped, the communication channel goes away before the
    response comes back, so the transaction never finishes and the
    database connection never closes.
    
    Fix by explicitly closing the connection when the execution context is
    stopped.
    
    R=alecflett@chromium.org,dgrogan@chromium.org,cmumford@chromium.org
    BUG=242115
    
    Review URL: https://codereview.chromium.org/138473004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165179 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/indexeddb/IDBTransaction.cpp
+++ third_party/WebKit/Source/modules/indexeddb/IDBTransaction.cpp
@@ -197,12 +197,13 @@ void IDBTransaction::abort(ExceptionState& exceptionState)
 
     m_state = Finishing;
 
-    if (!m_contextStopped) {
-        while (!m_requestList.isEmpty()) {
-            RefPtr<IDBRequest> request = *m_requestList.begin();
-            m_requestList.remove(request);
-            request->abort();
-        }
+    if (m_contextStopped)
+        return;
+
+    while (!m_requestList.isEmpty()) {
+        RefPtr<IDBRequest> request = *m_requestList.begin();
+        m_requestList.remove(request);
+        request->abort();
     }
 
     RefPtr<IDBTransaction> selfRef = this;
@@ -226,6 +227,12 @@ void IDBTransaction::unregisterRequest(IDBRequest* request)
 void IDBTransaction::onAbort(PassRefPtr<DOMError> prpError)
 {
     IDB_TRACE("IDBTransaction::onAbort");
+    if (m_contextStopped) {
+        RefPtr<IDBTransaction> protect(this);
+        m_database->transactionFinished(this);
+        return;
+    }
+
     RefPtr<DOMError> error = prpError;
     ASSERT(m_state != Finished);
 
@@ -262,6 +269,12 @@ void IDBTransaction::onAbort(PassRefPtr<DOMError> prpError)
 void IDBTransaction::onComplete()
 {
     IDB_TRACE("IDBTransaction::onComplete");
+    if (m_contextStopped) {
+        RefPtr<IDBTransaction> protect(this);
+        m_database->transactionFinished(this);
+        return;
+    }
+
     ASSERT(m_state != Finished);
     m_state = Finishing;
     m_objectStoreCleanupMap.clear();
