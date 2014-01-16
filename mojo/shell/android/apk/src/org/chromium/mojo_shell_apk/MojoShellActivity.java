d82b999ef30c67e8d3a58dce7628f26f92a48fa1
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-16 00:53:52 +0000

    Add a URL entry prompt
    R=sky@chromium.org
    BUG=none
    
    Review URL: https://codereview.chromium.org/140103004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@245040 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/shell/android/apk/src/org/chromium/mojo_shell_apk/MojoShellActivity.java
+++ mojo/shell/android/apk/src/org/chromium/mojo_shell_apk/MojoShellActivity.java
@@ -6,9 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.mojo_shell_apk;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
 
 import org.chromium.mojo_shell_apk.LibraryLoader;
 import org.chromium.mojo_shell_apk.MojoMain;
@@ -34,11 +37,31 @@ public class MojoShellActivity extends Activity {
         MojoMain.init(this);
 
         String appUrl = getUrlFromIntent(getIntent());
-        MojoMain.start(this, appUrl);
-        Log.i(TAG, "Mojo started: " + appUrl);
+        if (appUrl == null) {
+            Log.i(TAG, "No URL provided via intent, prompting user...");
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setTitle("Enter a URL");
+            alert.setMessage("Enter a URL");
+            final EditText input = new EditText(this);
+            alert.setView(input);
+            alert.setPositiveButton("Load", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int button) {
+                    String url = input.getText().toString();
+                    startWithURL(url);
+                }
+            });
+            alert.show();
+        } else {
+            startWithURL(appUrl);
+        }
     }
 
     private static String getUrlFromIntent(Intent intent) {
         return intent != null ? intent.getDataString() : null;
+    }
+
+    private void startWithURL(String url) {
+        MojoMain.start(this, url);
+        Log.i(TAG, "Mojo started: " + url);
     }
 }
