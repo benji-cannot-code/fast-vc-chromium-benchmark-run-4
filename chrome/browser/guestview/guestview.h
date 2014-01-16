06207974defef74fb3a631c1170930c016a44d1e
fsamuel@chromium.org <fsamuel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-16 19:24:04 +0000

    <webview>: Speculative crash fix
    This CL also does a bit of cleanup moving the WeakPtrFactory to the bottom of the member variables of a couple of BrowserPlugin/webview classes.
    
    BUG=334606
    
    Review URL: https://codereview.chromium.org/139603002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@245275 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/guestview/guestview.h
+++ chrome/browser/guestview/guestview.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <queue>
 
+#include "base/memory/weak_ptr.h"
 #include "base/values.h"
 #include "content/public/browser/browser_plugin_guest_delegate.h"
 #include "content/public/browser/web_contents.h"
@@ -132,6 +133,10 @@ class GuestView : public content::BrowserPluginGuestDelegate {
   // This is a queue of Events that are destined to be sent to the embedder once
   // the guest is attached to a particular embedder.
   std::queue<Event*> pending_events_;
+
+  // This is used to ensure pending tasks will not fire after this object is
+  // destroyed.
+  base::WeakPtrFactory<GuestView> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(GuestView);
 };
