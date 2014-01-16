75c369b7cd6195420fe2b639dd8ba980f2d7ab6b
mpcomplete@google.com <mpcomplete@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-16 00:14:18 +0000

    Break apart mojom_jinja_generator back into C++ and JS versions.
    I combined the 2 separate ones when I switched the C++ generator to jinja, but
    it turns out they don't actually share much code. The code they do share, I
    moved to the parent class.
    
    R=darin@chromium.org
    
    Review URL: https://codereview.chromium.org/137643002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@245030 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/public/bindings/generators/mojom_generator.py
+++ mojo/public/bindings/generators/mojom_generator.py
@@ -5,8 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 """Code shared by the various language-specific code generators."""
 
+import os
 import mojom
+import mojom_pack
 import re
+from functools import partial
 
 def GetStructFromMethod(interface, method):
   """Converts a method's parameters into the fields of a struct."""
@@ -14,6 +17,13 @@ def GetStructFromMethod(interface, method):
   struct = mojom.Struct(params_class)
   for param in method.parameters:
     struct.AddField(param.name, param.kind, param.ordinal)
+  struct.packed = mojom_pack.PackedStruct(struct)
+  return struct
+
+def GetStructInfo(exported, struct):
+  struct.packed = mojom_pack.PackedStruct(struct)
+  struct.bytes = mojom_pack.GetByteLayout(struct.packed)
+  struct.exported = exported
   return struct
 
 def IsObjectKind(kind):
@@ -30,9 +40,26 @@ def StudlyCapsToCamel(studly):
   return studly[0].lower() + studly[1:]
 
 class Generator(object):
-  # Pass |output_dir| to emit files to disk. Omit |output_dir| to echo all files
-  # to stdout.
+  # Pass |output_dir| to emit files to disk. Omit |output_dir| to echo all
+  # files to stdout.
   def __init__(self, module, header_dir, output_dir=None):
     self.module = module
     self.header_dir = header_dir
     self.output_dir = output_dir
+
+  def GetStructsFromMethods(self):
+    result = []
+    for interface in self.module.interfaces:
+      for method in interface.methods:
+        result.append(GetStructFromMethod(interface, method))
+    return map(partial(GetStructInfo, False), result)
+
+  def GetStructs(self):
+    return map(partial(GetStructInfo, True), self.module.structs)
+
+  def Write(self, contents, filename):
+    if self.output_dir is None:
+      print contents
+      return
+    with open(os.path.join(self.output_dir, filename), "w+") as f:
+      f.write(contents)
