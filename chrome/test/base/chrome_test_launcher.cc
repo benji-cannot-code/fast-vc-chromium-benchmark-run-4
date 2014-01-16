f6302a7a48967ac102c19c0a39045d08294e104c
gab@chromium.org <gab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-16 18:27:05 +0000

    Introduce content::TestLauncherDelegate.AdjustDefaultParallelJobs() to allow us to auto-disable parallelization for Ash browsertests.
    BUG=286205
    
    Review URL: https://codereview.chromium.org/132313006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@245261 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/base/chrome_test_launcher.cc
+++ chrome/test/base/chrome_test_launcher.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/test/base/chrome_test_suite.h"
+#include "chrome/test/base/test_switches.h"
 #include "content/public/app/content_main.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/test/test_utils.h"
@@ -118,6 +119,18 @@ class ChromeTestLauncherDelegate : public content::TestLauncherDelegate {
     NOTREACHED();
     return NULL;
 #endif
+  }
+
+  virtual void AdjustDefaultParallelJobs(int* default_jobs) OVERRIDE {
+#if defined(OS_WIN) && defined(USE_AURA)
+    if (CommandLine::ForCurrentProcess()->HasSwitch(
+            switches::kAshBrowserTests)) {
+      *default_jobs = 1;
+      fprintf(stdout,
+              "Disabling test parallelization for --ash-browsertests.\n");
+      fflush(stdout);
+    }
+#endif  // defined(OS_WIN) && defined(USE_AURA)
   }
 
  private:
