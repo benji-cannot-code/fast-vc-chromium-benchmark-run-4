e2e699f9b40aeaf9b7664dcf4c2382589593c2ee
ccameron@chromium.org <ccameron@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-16 05:12:59 +0000

    Add the UI compositor to the Mac build
    Move the bulk of the files in content/browser/aura to
    content/browser/compositor, since they're not aura-specific.
    
    Build all of those files in the Mac build, and add ui/compositor
    to the dependencies of the Mac build. These files will soon only
    be excluded from the Android build.
    
    BUG=314190
    
    Review URL: https://codereview.chromium.org/137893007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@245131 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_unittest.cc
+++ content/browser/renderer_host/render_widget_host_unittest.cc
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/screen.h"
 
 #if defined(USE_AURA)
-#include "content/browser/aura/image_transport_factory.h"
+#include "content/browser/compositor/image_transport_factory.h"
 #include "content/browser/renderer_host/render_widget_host_view_aura.h"
 #include "ui/aura/env.h"
 #include "ui/aura/test/test_screen.h"
