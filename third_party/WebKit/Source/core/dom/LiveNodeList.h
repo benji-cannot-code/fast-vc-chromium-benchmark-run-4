f05dbc118fe10dda0b07f617ae04a17888d47d6f
ch.dumez@samsung.com <ch.dumez@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-16 20:46:00 +0000

    Move isNameCacheValid member from LiveNodeListBase to HTMLCollection
    The isNameCacheValid member is only useful to HTMLCollection (not LiveNodeList),
    so move it and its corresponding getter / setter to the HTMLCollection class.
    There is no reason anymore to keep this member in the LiveNodeListBase class.
    
    R=eseidel, esprehn
    BUG=None
    
    Review URL: https://codereview.chromium.org/130753004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165248 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/LiveNodeList.h
+++ third_party/WebKit/Source/core/dom/LiveNodeList.h
@@ -58,7 +58,6 @@ public:
         , m_rootType(rootType)
         , m_invalidationType(invalidationType)
         , m_shouldOnlyIncludeDirectChildren(shouldOnlyIncludeDirectChildren)
-        , m_isNameCacheValid(false)
         , m_collectionType(collectionType)
         , m_overridesItemAfter(itemAfterOverrideType == OverridesItemAfter)
     {
@@ -122,10 +121,6 @@ protected:
     }
 
     ALWAYS_INLINE NodeListRootType rootType() const { return static_cast<NodeListRootType>(m_rootType); }
-
-    bool hasNameCache() const { return m_isNameCacheValid; }
-    void setHasNameCache() const { m_isNameCacheValid = true; }
-
     bool shouldOnlyIncludeDirectChildren() const { return m_shouldOnlyIncludeDirectChildren; }
 
 private:
@@ -145,10 +140,9 @@ private:
     const unsigned m_rootType : 2;
     const unsigned m_invalidationType : 4;
     const unsigned m_shouldOnlyIncludeDirectChildren : 1;
+    const unsigned m_collectionType : 5;
 
     // From HTMLCollection
-    mutable unsigned m_isNameCacheValid : 1;
-    const unsigned m_collectionType : 5;
     const unsigned m_overridesItemAfter : 1;
 };
 
