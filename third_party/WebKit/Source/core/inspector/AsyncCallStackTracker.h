d2405b546b7ab01ff822a2045213e72aab250e4a
aandrey@chromium.org <aandrey@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-16 14:54:14 +0000

    DevTools: Implement async call stacks for Promises.
    BUG=272416
    R=yurys, vsevik, yusukesuzuki, yhirano
    
    Review URL: https://codereview.chromium.org/131823003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165226 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/AsyncCallStackTracker.h
+++ third_party/WebKit/Source/core/inspector/AsyncCallStackTracker.h
@@ -45,6 +45,7 @@ namespace WebCore {
 class EventListener;
 class EventTarget;
 class ExecutionContext;
+class ExecutionContextTask;
 class MutationObserver;
 class XMLHttpRequest;
 
@@ -99,6 +100,9 @@ public:
     bool hasEnqueuedMutationRecord(ExecutionContext*, MutationObserver*);
     void didClearAllMutationRecords(ExecutionContext*, MutationObserver*);
     void willDeliverMutationRecords(ExecutionContext*, MutationObserver*);
+
+    void didPostPromiseTask(ExecutionContext*, ExecutionContextTask*, bool isResolved, const ScriptValue& callFrames);
+    void willPerformPromiseTask(ExecutionContext*, ExecutionContextTask*);
 
     void didFireAsyncCall();
     void clear();
