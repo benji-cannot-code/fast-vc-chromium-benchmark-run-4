792c1feba27b2d6c22795752b10e6de05c439723
haraken@chromium.org <haraken@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-16 16:12:56 +0000

    Implement TracingController::GetMonitoringStatus
    trace-viewer side CL is here: https://codereview.appspot.com/52710047
    
    Currently the monitoring mode doesn't work correctly when we reload about:tracing. This is because reloaded about:tracing fails in extracting the current monitoring status (See my comment in patch set 2).
    
    This CL implements TracingController::GetMonitoringStatus so that trace-viewer/src/about_tracing/profiling_view.js can get the current monitoring status when loading the profiling view.
    
    TEST=content_browsertests::EnableCaptureAndDisableMonitoring
    R=nduca@chromium.org
    
    Review URL: https://codereview.chromium.org/139313005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@245211 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/tracing/tracing_controller_impl.cc
+++ content/browser/tracing/tracing_controller_impl.cc
@@ -189,6 +189,7 @@ bool TracingControllerImpl::EnableRecording(
     TraceLog::GetInstance()->AddClockSyncMetadataEvent();
 #endif
 
+  options_ = options;
   int trace_options = (options & RECORD_CONTINUOUSLY) ?
       TraceLog::RECORD_CONTINUOUSLY : TraceLog::RECORD_UNTIL_FULL;
   if (options & ENABLE_SAMPLING) {
@@ -235,6 +236,7 @@ bool TracingControllerImpl::DisableRecording(
   if (!can_disable_recording())
     return false;
 
+  options_ = TracingController::Options();
   // Disable local trace early to avoid traces during end-tracing process from
   // interfering with the process.
   base::Closure on_disable_recording_done_callback =
@@ -299,6 +301,7 @@ bool TracingControllerImpl::EnableMonitoring(
   TraceLog::GetInstance()->AddClockSyncMetadataEvent();
 #endif
 
+  options_ = options;
   int trace_options = 0;
   if (options & ENABLE_SAMPLING)
     trace_options |= TraceLog::ENABLE_SAMPLING;
@@ -341,6 +344,7 @@ bool TracingControllerImpl::DisableMonitoring(
   if (!can_disable_monitoring())
     return false;
 
+  options_ = TracingController::Options();
   base::Closure on_disable_monitoring_done_callback =
       base::Bind(&TracingControllerImpl::OnDisableMonitoringDone,
                  base::Unretained(this), callback);
@@ -371,7 +375,10 @@ void TracingControllerImpl::GetMonitoringStatus(
     bool* out_enabled,
     std::string* out_category_filter,
     TracingController::Options* out_options) {
-  NOTIMPLEMENTED();
+  *out_enabled = is_monitoring_;
+  *out_category_filter =
+    TraceLog::GetInstance()->GetCurrentCategoryFilter().ToString();
+  *out_options = options_;
 }
 
 bool TracingControllerImpl::CaptureMonitoringSnapshot(
@@ -788,7 +795,6 @@ void TracingControllerImpl::RegisterTracingUI(TracingUI* tracing_ui)
 {
   DCHECK(tracing_uis_.find(tracing_ui) == tracing_uis_.end());
   tracing_uis_.insert(tracing_ui);
-  tracing_ui->OnMonitoringStateChanged(is_monitoring_);
 }
 
 void TracingControllerImpl::UnregisterTracingUI(TracingUI* tracing_ui)
