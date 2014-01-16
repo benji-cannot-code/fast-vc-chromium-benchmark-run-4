28e49624f8e2641a241ead4470345dc4a3abfd19
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-16 21:09:28 +0000

    Add UMA actions for launching the plugin loader utility process.
    BUG=171404
    
    Review URL: https://codereview.chromium.org/140793002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@245322 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/plugin_loader_posix.cc
+++ content/browser/plugin_loader_posix.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_loop.h"
 #include "base/message_loop/message_loop_proxy.h"
 #include "base/metrics/histogram.h"
+#include "base/metrics/user_metrics.h"
 #include "content/browser/utility_process_host_impl.h"
 #include "content/common/child_process_host_impl.h"
 #include "content/common/plugin_list.h"
@@ -46,6 +47,9 @@ bool PluginLoaderPosix::OnMessageReceived(const IPC::Message& message) {
 }
 
 void PluginLoaderPosix::OnProcessCrashed(int exit_code) {
+  RecordAction(
+      base::UserMetricsAction("PluginLoaderPosix.UtilityProcessCrashed"));
+
   if (next_load_index_ == canonical_list_.size()) {
     // How this case occurs is unknown. See crbug.com/111935.
     canonical_list_.clear();
@@ -100,6 +104,9 @@ void PluginLoaderPosix::LoadPluginsInternal() {
   // forking.
   if (MaybeRunPendingCallbacks())
     return;
+
+  RecordAction(
+      base::UserMetricsAction("PluginLoaderPosix.LaunchUtilityProcess"));
 
   if (load_start_time_.is_null())
     load_start_time_ = base::TimeTicks::Now();
