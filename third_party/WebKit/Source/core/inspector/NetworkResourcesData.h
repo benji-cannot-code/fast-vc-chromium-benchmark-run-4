0ac85a8248655ef6dbf0916327346f84083cf5bb
eustas@chromium.org <eustas@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-17 10:35:39 +0000

    Supply "replayXHR" with appropriate execution context.
    BUG=332700
    
    Review URL: https://codereview.chromium.org/134643004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165297 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/NetworkResourcesData.h
+++ third_party/WebKit/Source/core/inspector/NetworkResourcesData.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NetworkResourcesData_h
 #define NetworkResourcesData_h
 
+#include "core/dom/ContextLifecycleObserver.h"
 #include "core/fetch/TextResourceDecoder.h"
 #include "core/inspector/InspectorPageAgent.h"
 #include "platform/network/HTTPHeaderMap.h"
@@ -42,15 +43,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+class ExecutionContext;
 class Resource;
 class FormData;
 class ResourceResponse;
 class SharedBuffer;
 class TextResourceDecoder;
 
-class XHRReplayData : public RefCounted<XHRReplayData> {
+class XHRReplayData
+    : public RefCounted<XHRReplayData>
+    , public ContextLifecycleObserver {
 public:
-    static PassRefPtr<XHRReplayData> create(const AtomicString& method, const KURL&, bool async, PassRefPtr<FormData>, bool includeCredentials);
+    static PassRefPtr<XHRReplayData> create(ExecutionContext*, const AtomicString& method, const KURL&, bool async, PassRefPtr<FormData>, bool includeCredentials);
 
     void addHeader(const AtomicString& key, const AtomicString& value);
     const AtomicString& method() const { return m_method; }
@@ -61,7 +65,7 @@ public:
     bool includeCredentials() const { return m_includeCredentials; }
 
 private:
-    XHRReplayData(const AtomicString& method, const KURL&, bool async, PassRefPtr<FormData>, bool includeCredentials);
+    XHRReplayData(ExecutionContext*, const AtomicString& method, const KURL&, bool async, PassRefPtr<FormData>, bool includeCredentials);
 
     AtomicString m_method;
     KURL m_url;
