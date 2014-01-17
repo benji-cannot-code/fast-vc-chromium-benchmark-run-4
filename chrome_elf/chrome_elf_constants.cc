732d5492434a9864169af2234ad0feebd1d20312
caitkp@chromium.org <caitkp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-17 07:20:47 +0000

    Use an alternate mechanism for CreateFile calls in Chrome
    BUG=334379
    
    Review URL: https://codereview.chromium.org/138593004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@245464 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome_elf/chrome_elf_constants.cc
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome_elf/chrome_elf_constants.h"
+
+const wchar_t kUserDataDirName[] = L"User Data";
+
+#if defined(GOOGLE_CHROME_BUILD)
+const wchar_t kAppDataDirName[] = L"Google\\Chrome";
+#else
+const wchar_t kAppDataDirName[] = L"Chromium";
+#endif
---
+++ chrome_elf/chrome_elf_constants.h
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// A handful of resource-like constants related to the ChromeELF.
+
+#ifndef CHROME_ELF_CHROME_ELF_CONSTANTS_H_
+#define CHROME_ELF_CHROME_ELF_CONSTANTS_H_
+
+// directory names
+extern const wchar_t kAppDataDirName[];
+extern const wchar_t kUserDataDirName[];
+
+#endif  // CHROME_ELF_CHROME_ELF_CONSTANTS_H_
