b8a5d4ada078504eb346dc2b5575623d52ba9767
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-17 20:35:12 +0000

    Back out trunk r245361.
    Go back to KeystoneRegistration.framework 1.1.0.3659. The new registration
    framework 1.2.0.6782, when using -[KSRegistration
    checkForUpdateWasUserInitiated:YES], is not compatible with an older installed
    Keystone version such as the current 1.1.0.3973. This results in the About
    page showing "Update failed (error: 11)" and this message being printed to
    stderr:
    
    ksadmin: unrecognized option `--user-initiated'
    
    In this case, updating is not broken, and Keystone will continue to update
    Chrome in the background, but no indication of update availability will be
    made in Chrome’s about page.
    
    > Update KeystoneRegistration.framework to 1.2.0.6782.
    > 
    > The big change in this version is that an installation attempt initiated by
    > the application can show up as ondemand on the server. This will allow us to
    > provide throttled Mac downloads (such as a "5% push") with correct behavior.
    > 
    > BUG=35495
    > R=thakis@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/141193002
    
    BUG=35495
    TBR=mark@chromium.org
    
    Review URL: https://codereview.chromium.org/141583005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@245603 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/mac/keystone_glue_unittest.mm
+++ chrome/browser/mac/keystone_glue_unittest.mm
@@ -39,7 +39,7 @@ namespace ksr = keystone_registration;
 - (void)setActive {
 }
 
-- (void)checkForUpdateWasUserInitiated:(BOOL)userInitiated {
+- (void)checkForUpdate {
 }
 
 - (void)startUpdate {
@@ -56,7 +56,7 @@ namespace ksr = keystone_registration;
 
 // Send the notifications that a real KeystoneGlue object would send.
 
-- (void)checkForUpdateWasUserInitiated:(BOOL)userInitiated {
+- (void)checkForUpdate {
   NSNumber* yesNumber = [NSNumber numberWithBool:YES];
   NSString* statusKey = @"Status";
   NSDictionary* dictionary = [NSDictionary dictionaryWithObject:yesNumber
