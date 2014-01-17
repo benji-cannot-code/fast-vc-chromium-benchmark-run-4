3ac863f758ac71b81f9e72f8e7aa1d1d3e35d3a0
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-17 07:24:01 +0000

    [rAC, OSX] Adjust red outline for NSPopUpButtons
    Made red outline match NSPopUpButton pixel for pixel.
    
    R=rsesek@chromium.org
    BUG=325492
    
    Review URL: https://codereview.chromium.org/138943002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@245465 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/autofill/autofill_section_container.mm
+++ chrome/browser/ui/cocoa/autofill/autofill_section_container.mm
@@ -654,6 +654,7 @@ bool ShouldOverwriteComboboxes(autofill::DialogSection section,
     }
     [control setFieldValue:base::SysUTF16ToNSString(input.initial_value)];
     [control sizeToFit];
+    [control setFrame:NSIntegralRect([control frame])];
     [control setTag:input.type];
     [control setInputDelegate:self];
     // Hide away fields that cannot be edited.
