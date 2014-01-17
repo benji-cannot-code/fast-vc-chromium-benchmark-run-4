5db4b7f98f2f1d4c4c4904afe5250319648694e7
dzhioev@chromium.org <dzhioev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-17 18:22:11 +0000

    Prevented flakiness in FirstRunUIBrowserTest.FirstRunFlow test.
    BUG=335280
    TEST=browser_test --gtest_filter=FirstRunUIBrowserTest.FirstRunFlow
    
    Review URL: https://codereview.chromium.org/141803002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@245561 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/first_run/first_run_browsertest.cc
+++ chrome/browser/chromeos/first_run/first_run_browsertest.cc
@@ -36,6 +36,7 @@ class FirstRunUIBrowserTest : public InProcessBrowserTest,
   }
 
   virtual void OnStepShown(const std::string& step_name) OVERRIDE {
+    current_step_name_ = step_name;
     if (!on_step_shown_callback_.is_null())
       on_step_shown_callback_.Run();
     controller()->OnStepShown(step_name);
@@ -78,10 +79,10 @@ class FirstRunUIBrowserTest : public InProcessBrowserTest,
   }
 
   void WaitForStep(const std::string& step_name) {
-    if (GetCurrentStepName() == step_name)
+    if (current_step_name_ == step_name)
       return;
     WaitUntilCalled(&on_step_shown_callback_);
-    EXPECT_EQ(GetCurrentStepName(), step_name);
+    EXPECT_EQ(current_step_name_, step_name);
   }
 
   void AdvanceStep() {
@@ -103,11 +104,6 @@ class FirstRunUIBrowserTest : public InProcessBrowserTest,
     callback->Reset();
   }
 
-  std::string GetCurrentStepName() {
-    return js().GetString(
-        "cr.FirstRun.currentStep_ ? cr.FirstRun.currentStep_.getName() : ''");
-  }
-
   test::JSChecker& js() { return js_; }
 
   ash::FirstRunHelper* shell_helper() {
@@ -119,6 +115,7 @@ class FirstRunUIBrowserTest : public InProcessBrowserTest,
   }
 
  private:
+  std::string current_step_name_;
   bool initialized_;
   bool finalized_;
   base::Closure on_initialized_callback_;
@@ -127,8 +124,7 @@ class FirstRunUIBrowserTest : public InProcessBrowserTest,
   test::JSChecker js_;
 };
 
-// Disabled due to flakiness, see http://crbug.com/335280
-IN_PROC_BROWSER_TEST_F(FirstRunUIBrowserTest, DISABLED_FirstRunFlow) {
+IN_PROC_BROWSER_TEST_F(FirstRunUIBrowserTest, FirstRunFlow) {
   LaunchTutorial();
   WaitForInitialization();
   WaitForStep(first_run::kAppListStep);
