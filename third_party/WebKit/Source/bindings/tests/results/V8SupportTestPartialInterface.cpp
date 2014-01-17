500a30de7e13881d37317a7636223d8f6330bd24
nbarth@chromium.org <nbarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-17 08:26:39 +0000

    Split dependencies from built IDL files in GYP
    This statically separates:
    * actual target IDL files (for which .h/.cpp files are generated)
    from:
    * dependency IDL files (for which dummy files are generated)
    ...which significantly reduces the work done in generating bindings and
    allows us to remove lots of code and complexity (b/c these aren't distinguished).
    
    Improvements:
    * Reduces generated bindings by 9% (1348 => 1236)
    * Reduces build edges by 9% (678 => 618)
    * Reduces tests/results by 10% (61 => 55)
    * Removes --additionalIdlFiles parameter and code
    * Removes dummy file generation code
    
    Further simplification is possible:
    * compute_dependencies
      (currently has to remove dependencies, and treat main and support separately)
    * derived_sources_all_in_one
      Possibly can be eliminated or significantly simplified, as we now have
      the full list (it's just main_interface_idl_files)
    * idl_compiler.py & code_generator_v8.py, since guaranteed to have real IDL file
    * Perhaps remove the 'support_*' variables (used by testing)
    
    I'll do these in followup CLs.
    
    Review URL: https://codereview.chromium.org/136033011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165289 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/tests/results/V8SupportTestPartialInterface.cpp
+++
@@ -1,7 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-/*
-    This file is generated just to tell build scripts that V8SupportTestPartialInterface.h and
-    V8SupportTestPartialInterface.cpp are created for SupportTestPartialInterface.idl, and thus
-    prevent the build scripts from trying to generate V8SupportTestPartialInterface.h and
-    V8SupportTestPartialInterface.cpp at every build. This file must not be tried to compile.
-*/
--- third_party/WebKit/Source/bindings/tests/results/V8TestImplements.cpp
+++
@@ -1,7 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-/*
-    This file is generated just to tell build scripts that V8TestImplements.h and
-    V8TestImplements.cpp are created for TestImplements.idl, and thus
-    prevent the build scripts from trying to generate V8TestImplements.h and
-    V8TestImplements.cpp at every build. This file must not be tried to compile.
-*/
