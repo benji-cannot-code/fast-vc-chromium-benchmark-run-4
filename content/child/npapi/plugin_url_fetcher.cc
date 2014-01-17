3bc30b255a985be58248fea03ce2e81b066bd7a1
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-17 20:32:38 +0000

    Give QuickTime the plugin stream data in writeable memory as it modifies it.
    This broke after switching the fetching of plugin stream data to the plugin process because now the data from ResourceDispatcher is passed directly to the plugin and it's read-only.
    
    BUG=308466
    R=ananta@chromium.org
    
    Review URL: https://codereview.chromium.org/141333005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@245600 0039d316-1c4b-4281-b951-d872f2087c98

--- content/child/npapi/plugin_url_fetcher.cc
+++ content/child/npapi/plugin_url_fetcher.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/child/npapi/plugin_url_fetcher.h"
 
+#include "base/memory/scoped_ptr.h"
 #include "content/child/child_thread.h"
 #include "content/child/npapi/webplugin.h"
 #include "content/child/npapi/plugin_host.h"
@@ -81,7 +82,8 @@ PluginURLFetcher::PluginURLFetcher(PluginStreamUrl* plugin_stream,
                                    bool is_plugin_src_load,
                                    int origin_pid,
                                    int render_frame_id,
-                                   unsigned long resource_id)
+                                   unsigned long resource_id,
+                                   bool copy_stream_data)
     : plugin_stream_(plugin_stream),
       url_(url),
       first_party_for_cookies_(first_party_for_cookies),
@@ -90,6 +92,7 @@ PluginURLFetcher::PluginURLFetcher(PluginStreamUrl* plugin_stream,
       notify_redirects_(notify_redirects),
       is_plugin_src_load_(is_plugin_src_load),
       resource_id_(resource_id),
+      copy_stream_data_(copy_stream_data),
       data_offset_(0),
       pending_failure_notification_(false) {
   webkit_glue::ResourceLoaderBridge::RequestInfo request_info;
@@ -302,7 +305,17 @@ void PluginURLFetcher::OnReceivedData(const char* data,
   } else {
     int64 offset = data_offset_;
     data_offset_ += data_length;
-    plugin_stream_->DidReceiveData(data, data_length, offset);
+
+    if (copy_stream_data_) {
+      // QuickTime writes to this memory, and since we got it from
+      // ResourceDispatcher it's not mapped for write access in this process.
+      // http://crbug.com/308466.
+      scoped_ptr<char[]> data_copy(new char[data_length]);
+      memcpy(data_copy.get(), data, data_length);
+      plugin_stream_->DidReceiveData(data_copy.get(), data_length, offset);
+    } else {
+      plugin_stream_->DidReceiveData(data, data_length, offset);
+    }
     // DANGER: this instance may be deleted at this point.
   }
 }
--- content/child/npapi/plugin_url_fetcher.h
+++ content/child/npapi/plugin_url_fetcher.h
@@ -34,7 +34,8 @@ class PluginURLFetcher : public webkit_glue::ResourceLoaderBridge::Peer {
                    bool is_plugin_src_load,
                    int origin_pid,
                    int render_frame_id,
-                   unsigned long resource_id);
+                   unsigned long resource_id,
+                   bool copy_stream_data);
   virtual ~PluginURLFetcher();
 
   // Cancels the current request.
@@ -72,6 +73,7 @@ class PluginURLFetcher : public webkit_glue::ResourceLoaderBridge::Peer {
   bool notify_redirects_;
   bool is_plugin_src_load_;
   unsigned long resource_id_;
+  bool copy_stream_data_;
   int64 data_offset_;
   bool pending_failure_notification_;
 
