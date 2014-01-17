f52612a4d35b5b28af2bfe32662254401d7f4187
mtomasz@chromium.org <mtomasz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-17 10:41:02 +0000

    Play automatically when reloading the audio player with new songs.
    Because of introducing an async call during initialization, the new appstate was being changed. Playing a song adds a 'time' field to the appstate. Having a time field causes pausing the audio player once the initialization is finished.
    
    To avoid this, a copy is made, to avoid altering the input 'playlist' object.
    
    TEST=Tested manually. Partly browser tests.
    BUG=335422
    
    Review URL: https://codereview.chromium.org/141273004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@245493 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/foreground/js/media/audio_player.js
+++ chrome/browser/resources/file_manager/foreground/js/media/audio_player.js
@@ -41,7 +41,6 @@ function AudioPlayer(container) {
 
   this.audioControls_ = new FullWindowAudioControls(
       createChild(), this.advance_.bind(this), this.onError_.bind(this));
-
   this.audioControls_.attachMedia(createChild('', 'audio'));
 
   chrome.fileBrowserPrivate.getStrings(function(strings) {
@@ -75,7 +74,7 @@ AudioPlayer.load = function() {
 
   AudioPlayer.instance =
       new AudioPlayer(document.querySelector('.audio-player'));
-  reload();
+  AudioPlayer.instance.load(window.appState);
 };
 
 util.addPageLoadHandler(AudioPlayer.load);
@@ -92,11 +91,7 @@ function unload() {
  * Reload the player.
  */
 function reload() {
-  if (window.appState) {
-    util.saveAppState();
-    AudioPlayer.instance.load(window.appState);
-    return;
-  }
+  AudioPlayer.instance.load(window.appState);
 }
 
 /**
@@ -108,8 +103,9 @@ AudioPlayer.prototype.load = function(playlist) {
   this.audioControls_.pause();
   this.currentTrack_ = -1;
 
-  // Save the app state, in case of restart.
-  window.appState = playlist;
+  // Save the app state, in case of restart. Make a copy of the object, so the
+  // playlist member is not changed after entries are resolved.
+  window.appState = JSON.parse(JSON.stringify(playlist));
   util.saveAppState();
 
   util.URLsToEntries(playlist.items, function(entries) {
@@ -217,13 +213,9 @@ AudioPlayer.prototype.select_ = function(newTrack, opt_restoreState) {
 
   this.currentTrack_ = newTrack;
 
-  if (window.appState) {
-    window.appState.position = this.currentTrack_;
-    window.appState.time = 0;
-    util.saveAppState();
-  } else {
-    util.platform.setPreference(AudioPlayer.TRACK_KEY, this.currentTrack_);
-  }
+  window.appState.position = this.currentTrack_;
+  window.appState.time = 0;
+  util.saveAppState();
 
   this.scrollToCurrent_(false);
 
