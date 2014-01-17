bdb70846a2e01b53b0d35d11d0b14ad1412127e9
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-17 10:55:21 +0000

    DevTools: follow up to r165291, fix WorkerDevToolsSanityTest.InspectSharedWorker TBR=alph
    Review URL: https://codereview.chromium.org/141663002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165302 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/inspector.js
+++ third_party/WebKit/Source/devtools/front_end/inspector.js
@@ -58,23 +58,22 @@ var WebInspector = {
             );
         }
 
-        var audits = new WebInspector.AuditsPanelDescriptor();
+        if (!WebInspector.WorkerManager.isWorkerFrontend())
+            new WebInspector.AuditsPanelDescriptor();
 
-        if (!WebInspector.WorkerManager.isWorkerFrontend()) {
-            WebInspector.moduleManager.registerModule(
-                {
-                    name: "ConsolePanel",
-                    extensions: [
-                        {
-                            type: "@WebInspector.Panel",
-                            name: "console",
-                            title: "Console",
-                            className: "WebInspector.ConsolePanel"
-                        }
-                    ]
-                }
-            );
-        }
+        WebInspector.moduleManager.registerModule(
+            {
+                name: "ConsolePanel",
+                extensions: [
+                    {
+                        type: "@WebInspector.Panel",
+                        name: "console",
+                        title: "Console",
+                        className: "WebInspector.ConsolePanel"
+                    }
+                ]
+            }
+        );
 
         if (WebInspector.experimentsSettings.layersPanel.isEnabled() && !WebInspector.WorkerManager.isWorkerFrontend())
             new WebInspector.LayersPanelDescriptor();
