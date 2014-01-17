2f0472b39a808b0358d55e6df07682abb05976d0
dpranke@chromium.org <dpranke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-17 19:48:40 +0000

    Include the default implementation of WebThemeEngine.h in webthemeengine_impl_android.h
    Rather than using the (now-obsolete) version in platform/android. Once
    this lands, we can delete the android stub.
    
    R=jamesr@chromium.org
    BUG=none
    
    Review URL: https://codereview.chromium.org/120313003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@245589 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/child/webthemeengine_impl_android.h
+++ webkit/child/webthemeengine_impl_android.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WEBKIT_CHILD_WEBTHEMEENGINE_IMPL_ANDROID_H_
 #define WEBKIT_CHILD_WEBTHEMEENGINE_IMPL_ANDROID_H_
 
-#include "third_party/WebKit/public/platform/android/WebThemeEngine.h"
+#include "third_party/WebKit/public/platform/default/WebThemeEngine.h"
 
 namespace webkit_glue {
 
