c939d5d18b09f1d762f972a201d4451c80c4e5a6
vivek.vg@samsung.com <vivek.vg@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-17 15:52:41 +0000

    MakeQualifiedNames should generate lazily created arrays for attributes and tags.
    The templates MakeQualifiedNames.cpp.tmpl and MakeQualifiedNames.h.tmpl currently generate
    local static arrays for the tags and the attributes.
    
    Currently these arrays hold pointers to the global data and these arrays are declared as
    static. Due to this static declaration, the sections .rel.dyn and .data.rel.ro will
    allocate the bytes equal to the size of the array.
    
    Even though these functions may not be called upon, the binary would still be loaded with
    allocation of that much amount of data.
    
    Making these arrays as non-static and allocating them lazily, we are able to
    save about 4.2KB size in these sections.
    
    * With the patch, we save - 
        = 2880 bytes in .rel.dyn 
        = 1440 bytes in .data.rel.ro
    
    * Total size increases by -
        = 384 bytes
    
    Using the tool relinfo.pl from [1] we get:
    * About 360 relocations are avoided by the patch.
    * About 360 relatives are avoided by the patch.
    
    [1] http://www.akkadia.org/drepper/relinfo.pl
    
    BUG=249746
    
    Review URL: https://codereview.chromium.org/137783012
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165316 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/parser/HTMLTreeBuilder.cpp
+++ third_party/WebKit/Source/core/html/parser/HTMLTreeBuilder.cpp
@@ -570,8 +570,8 @@ static void adjustSVGTagNameCase(AtomicHTMLToken* token)
     static PrefixedNameToQualifiedNameMap* caseMap = 0;
     if (!caseMap) {
         caseMap = new PrefixedNameToQualifiedNameMap;
-        const QualifiedName* const* svgTags = SVGNames::getSVGTags();
-        mapLoweredLocalNameToName(caseMap, svgTags, SVGNames::SVGTagsCount);
+        OwnPtr<const QualifiedName*[]> svgTags = SVGNames::getSVGTags();
+        mapLoweredLocalNameToName(caseMap, svgTags.get(), SVGNames::SVGTagsCount);
     }
 
     const QualifiedName& casedName = caseMap->get(token->name());
@@ -580,14 +580,14 @@ static void adjustSVGTagNameCase(AtomicHTMLToken* token)
     token->setName(casedName.localName());
 }
 
-template<const QualifiedName* const* getAttrs(), unsigned length>
+template<PassOwnPtr<const QualifiedName*[]> getAttrs(), unsigned length>
 static void adjustAttributes(AtomicHTMLToken* token)
 {
     static PrefixedNameToQualifiedNameMap* caseMap = 0;
     if (!caseMap) {
         caseMap = new PrefixedNameToQualifiedNameMap;
-        const QualifiedName* const* attrs = getAttrs();
-        mapLoweredLocalNameToName(caseMap, attrs, length);
+        OwnPtr<const QualifiedName*[]> attrs = getAttrs();
+        mapLoweredLocalNameToName(caseMap, attrs.get(), length);
     }
 
     for (unsigned i = 0; i < token->attributes().size(); ++i) {
@@ -625,11 +625,11 @@ static void adjustForeignAttributes(AtomicHTMLToken* token)
     if (!map) {
         map = new PrefixedNameToQualifiedNameMap;
 
-        const QualifiedName* const* attrs = XLinkNames::getXLinkAttrs();
-        addNamesWithPrefix(map, xlinkAtom, attrs, XLinkNames::XLinkAttrsCount);
+        OwnPtr<const QualifiedName*[]> attrs = XLinkNames::getXLinkAttrs();
+        addNamesWithPrefix(map, xlinkAtom, attrs.get(), XLinkNames::XLinkAttrsCount);
 
-        attrs = XMLNames::getXMLAttrs();
-        addNamesWithPrefix(map, xmlAtom, attrs, XMLNames::XMLAttrsCount);
+        OwnPtr<const QualifiedName*[]> xmlAttrs = XMLNames::getXMLAttrs();
+        addNamesWithPrefix(map, xmlAtom, xmlAttrs.get(), XMLNames::XMLAttrsCount);
 
         map->add(WTF::xmlnsAtom, XMLNSNames::xmlnsAttr);
         map->add("xmlns:xlink", QualifiedName(xmlnsAtom, xlinkAtom, XMLNSNames::xmlnsNamespaceURI));
