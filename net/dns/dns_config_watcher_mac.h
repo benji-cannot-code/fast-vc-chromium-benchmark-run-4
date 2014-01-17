66e85283619b2165d773ca3704afba7591f67162
droger@chromium.org <droger@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-17 00:27:50 +0000

    Remove private system API calls in dns_config_service_posix on iOS
    BUG=335000
    
    Review URL: https://codereview.chromium.org/135043003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@245391 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ net/dns/dns_config_watcher_mac.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/callback_forward.h"
+#include "net/dns/dns_config_service_posix.h"
+#include "net/dns/notify_watcher_mac.h"
+
+namespace net {
+namespace internal {
+
+// Watches DNS configuration on Mac.
+class DnsConfigWatcher {
+ public:
+  bool Watch(const base::Callback<void(bool succeeded)>& callback);
+
+  // Returns an error if the DNS configuration is invalid.
+  // Returns CONFIG_PARSE_POSIX_OK otherwise.
+  static ConfigParsePosixResult CheckDnsConfig();
+
+ private:
+  NotifyWatcherMac watcher_;
+};
+
+}  // naespace internal
+}  // namespace net
