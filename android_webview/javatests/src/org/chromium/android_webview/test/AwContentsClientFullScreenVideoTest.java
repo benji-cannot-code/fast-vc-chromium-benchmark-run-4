f617e7f873e092be34a74428a3fdb9c197fba3b1
boliu@chromium.org <boliu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-17 22:13:47 +0000

    [Android WebView] Enable video tests
    These should be working now.
    
    BUG=238735
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/133613003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@245637 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/javatests/src/org/chromium/android_webview/test/AwContentsClientFullScreenVideoTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwContentsClientFullScreenVideoTest.java
@@ -5,10 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.android_webview.test;
 
+import android.test.suitebuilder.annotation.SmallTest;
 import android.view.KeyEvent;
 
 import org.chromium.android_webview.test.util.VideoTestWebServer;
-import org.chromium.base.test.util.DisabledTest;
+import org.chromium.base.test.util.Feature;
 import org.chromium.content.browser.test.util.TouchCommon;
 
 /**
@@ -16,13 +17,8 @@ import org.chromium.content.browser.test.util.TouchCommon;
  */
 public class AwContentsClientFullScreenVideoTest extends AwTestBase {
 
-    /**
-     * @Feature({"AndroidWebView"})
-     * @SmallTest
-     *
-     * http://crbug.com/238735
-     */
-    @DisabledTest
+    @Feature({"AndroidWebView"})
+    @SmallTest
     public void testOnShowAndHideCustomView() throws Throwable {
         FullScreenVideoTestAwContentsClient contentsClient =
                 new FullScreenVideoTestAwContentsClient(getActivity());
--- android_webview/javatests/src/org/chromium/android_webview/test/AwContentsClientGetVideoLoadingProgressViewTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwContentsClientGetVideoLoadingProgressViewTest.java
@@ -5,11 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.android_webview.test;
 
+import android.test.suitebuilder.annotation.SmallTest;
 import android.view.View;
 
 import org.chromium.android_webview.AwContents;
 import org.chromium.android_webview.test.util.VideoTestWebServer;
-import org.chromium.base.test.util.DisabledTest;
+import org.chromium.base.test.util.Feature;
 import org.chromium.content.browser.test.util.CallbackHelper;
 import org.chromium.content.browser.test.util.TouchCommon;
 
@@ -46,13 +47,8 @@ public class AwContentsClientGetVideoLoadingProgressViewTest extends AwTestBase
     }
 
 
-    /**
-     * @Feature({"AndroidWebView"})
-     * @SmallTest
-     *
-     * http://crbug.com/238735
-     */
-    @DisabledTest
+    @Feature({"AndroidWebView"})
+    @SmallTest
     public void testGetVideoLoadingProgressView() throws Throwable {
         TestAwContentsClient contentsClient =
                 new FullScreenVideoTestAwContentsClient(getActivity()) {
