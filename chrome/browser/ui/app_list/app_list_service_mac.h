e59c653e56e6df0a2e1422909fd891e8e89c2886
tapted@chromium.org <tapted@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-17 10:01:07 +0000

    Show the Mac app launcher window with a spinner while loading Chrome.
    Currently showing the app launcher window on a Chrome cold start is
    blocked by profile loading and other browser startup tasks, which can
    take a while.
    
    This CL allows the Mac app launcher window to be shown empty, with a
    progress spinner. Once the the profile has finished loading, the window
    is populated with a subtle icon fade-in animation.
    
    Screenshot at http://crbug.com/335433#c1
    
    BUG=178260, 335433
    
    Review URL: https://codereview.chromium.org/117683002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@245472 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/app_list/app_list_service_mac.h
+++ chrome/browser/ui/app_list/app_list_service_mac.h
@@ -50,6 +50,7 @@ class AppListServiceMac : public AppListServiceImpl,
                               NSPoint* start_origin);
 
   void ShowWindowNearDock();
+  void WindowAnimationDidEnd();
 
   // AppListService overrides:
   virtual void Init(Profile* initial_profile) OVERRIDE;
