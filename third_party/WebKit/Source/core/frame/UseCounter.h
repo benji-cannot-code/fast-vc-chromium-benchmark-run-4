dac79d0fe3dc3decdda4c56ddc95f29db929b1ba
philipj@opera.com <philipj@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-17 01:41:01 +0000

    Remove AudioBufferSourceNode.looping
    Intent to Remove:
    https://groups.google.com/a/chromium.org/d/msg/blink-dev/v1KTqntSFKU/UWoupLnxPawJ
    
    BUG=335126
    
    Review URL: https://codereview.chromium.org/140973002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165267 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -133,7 +133,7 @@ public:
         ContentSecurityPolicyWithBaseElement,
         PrefixedMediaAddKey,
         PrefixedMediaGenerateKeyRequest,
-        WebAudioLooping,
+        UnusedSlot10, // Removed, was AudioBufferSourceNode.looping.
         DocumentClear,
         PrefixedTransitionMediaFeature,
         SVGFontElement,
