151e3f89f7ee77038c2389d6754da4a201525956
dominicc@chromium.org <dominicc@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-17 00:36:45 +0000

    Add use counters for using Attrs as Nodes.
    DOM4 makes Attr no longer be a subtype of Node. This adds a use
    counter for passing Attr where Node is expected, or calling a method
    declared on Node with an Attr.
    
    BUG=305105
    
    Review URL: https://codereview.chromium.org/127723002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165262 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/tests/results/V8SupportTestInterface.cpp
+++ third_party/WebKit/Source/bindings/tests/results/V8SupportTestInterface.cpp
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "V8SupportTestInterface.h"
 
 #include "RuntimeEnabledFeatures.h"
+#include "V8Attr.h"
 #include "V8Node.h"
 #include "V8TestObject.h"
 #include "bindings/tests/idls/testing/SupportTestPartialInterface.h"
@@ -43,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/v8/V8ObjectConstructor.h"
 #include "core/dom/ContextFeatures.h"
 #include "core/dom/Document.h"
+#include "core/frame/UseCounter.h"
 #include "platform/TraceEvent.h"
 #include "wtf/GetPtr.h"
 #include "wtf/RefPtr.h"
@@ -224,6 +226,8 @@ static void supplementalNodeAttributeSetter(v8::Local<v8::Value> jsValue, const 
 static void supplementalNodeAttributeSetterCallback(v8::Local<v8::String>, v8::Local<v8::Value> jsValue, const v8::PropertyCallbackInfo<void>& info)
 {
     TRACE_EVENT_SET_SAMPLING_STATE("Blink", "DOMSetter");
+    if (V8DOMWrapper::isWrapperOfType(jsValue, &V8Attr::wrapperTypeInfo))
+        UseCounter::count(activeExecutionContext(), UseCounter::AttrUsedAsNodeParameter);
     SupportTestInterfaceV8Internal::supplementalNodeAttributeSetter(jsValue, info);
     TRACE_EVENT_SET_SAMPLING_STATE("V8", "V8Execution");
 }
@@ -259,6 +263,8 @@ static void Node13AttributeSetter(v8::Local<v8::Value> jsValue, const v8::Proper
 static void Node13AttributeSetterCallback(v8::Local<v8::String>, v8::Local<v8::Value> jsValue, const v8::PropertyCallbackInfo<void>& info)
 {
     TRACE_EVENT_SET_SAMPLING_STATE("Blink", "DOMSetter");
+    if (V8DOMWrapper::isWrapperOfType(jsValue, &V8Attr::wrapperTypeInfo))
+        UseCounter::count(activeExecutionContext(), UseCounter::AttrUsedAsNodeParameter);
     SupportTestInterfaceV8Internal::Node13AttributeSetter(jsValue, info);
     TRACE_EVENT_SET_SAMPLING_STATE("V8", "V8Execution");
 }
@@ -294,6 +300,8 @@ static void Node14AttributeSetter(v8::Local<v8::Value> jsValue, const v8::Proper
 static void Node14AttributeSetterCallback(v8::Local<v8::String>, v8::Local<v8::Value> jsValue, const v8::PropertyCallbackInfo<void>& info)
 {
     TRACE_EVENT_SET_SAMPLING_STATE("Blink", "DOMSetter");
+    if (V8DOMWrapper::isWrapperOfType(jsValue, &V8Attr::wrapperTypeInfo))
+        UseCounter::count(activeExecutionContext(), UseCounter::AttrUsedAsNodeParameter);
     SupportTestInterfaceV8Internal::Node14AttributeSetter(jsValue, info);
     TRACE_EVENT_SET_SAMPLING_STATE("V8", "V8Execution");
 }
--- third_party/WebKit/Source/bindings/tests/results/V8TestInterface.cpp
+++ third_party/WebKit/Source/bindings/tests/results/V8TestInterface.cpp
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "V8TestInterface.h"
 
 #include "RuntimeEnabledFeatures.h"
+#include "V8Attr.h"
 #include "V8Node.h"
 #include "V8NodeList.h"
 #include "V8ReferencedType.h"
@@ -49,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/v8/V8ObjectConstructor.h"
 #include "core/dom/ContextFeatures.h"
 #include "core/dom/Document.h"
+#include "core/frame/UseCounter.h"
 #include "platform/TraceEvent.h"
 #include "wtf/GetPtr.h"
 #include "wtf/RefPtr.h"
@@ -195,6 +197,8 @@ static void implementsNodeAttributeSetter(v8::Local<v8::Value> jsValue, const v8
 static void implementsNodeAttributeSetterCallback(v8::Local<v8::String>, v8::Local<v8::Value> jsValue, const v8::PropertyCallbackInfo<void>& info)
 {
     TRACE_EVENT_SET_SAMPLING_STATE("Blink", "DOMSetter");
+    if (V8DOMWrapper::isWrapperOfType(jsValue, &V8Attr::wrapperTypeInfo))
+        UseCounter::count(activeExecutionContext(), UseCounter::AttrUsedAsNodeParameter);
     TestInterfaceV8Internal::implementsNodeAttributeSetter(jsValue, info);
     TRACE_EVENT_SET_SAMPLING_STATE("V8", "V8Execution");
 }
@@ -250,6 +254,8 @@ static void Node23AttributeSetter(v8::Local<v8::Value> jsValue, const v8::Proper
 static void Node23AttributeSetterCallback(v8::Local<v8::String>, v8::Local<v8::Value> jsValue, const v8::PropertyCallbackInfo<void>& info)
 {
     TRACE_EVENT_SET_SAMPLING_STATE("Blink", "DOMSetter");
+    if (V8DOMWrapper::isWrapperOfType(jsValue, &V8Attr::wrapperTypeInfo))
+        UseCounter::count(activeExecutionContext(), UseCounter::AttrUsedAsNodeParameter);
     TestInterfaceV8Internal::Node23AttributeSetter(jsValue, info);
     TRACE_EVENT_SET_SAMPLING_STATE("V8", "V8Execution");
 }
@@ -277,6 +283,8 @@ static void Node24AttributeSetter(v8::Local<v8::Value> jsValue, const v8::Proper
 static void Node24AttributeSetterCallback(v8::Local<v8::String>, v8::Local<v8::Value> jsValue, const v8::PropertyCallbackInfo<void>& info)
 {
     TRACE_EVENT_SET_SAMPLING_STATE("Blink", "DOMSetter");
+    if (V8DOMWrapper::isWrapperOfType(jsValue, &V8Attr::wrapperTypeInfo))
+        UseCounter::count(activeExecutionContext(), UseCounter::AttrUsedAsNodeParameter);
     TestInterfaceV8Internal::Node24AttributeSetter(jsValue, info);
     TRACE_EVENT_SET_SAMPLING_STATE("V8", "V8Execution");
 }
@@ -430,6 +438,8 @@ static void supplementalNodeAttributeSetter(v8::Local<v8::Value> jsValue, const 
 static void supplementalNodeAttributeSetterCallback(v8::Local<v8::String>, v8::Local<v8::Value> jsValue, const v8::PropertyCallbackInfo<void>& info)
 {
     TRACE_EVENT_SET_SAMPLING_STATE("Blink", "DOMSetter");
+    if (V8DOMWrapper::isWrapperOfType(jsValue, &V8Attr::wrapperTypeInfo))
+        UseCounter::count(activeExecutionContext(), UseCounter::AttrUsedAsNodeParameter);
     TestInterfaceV8Internal::supplementalNodeAttributeSetter(jsValue, info);
     TRACE_EVENT_SET_SAMPLING_STATE("V8", "V8Execution");
 }
@@ -465,6 +475,8 @@ static void Node13AttributeSetter(v8::Local<v8::Value> jsValue, const v8::Proper
 static void Node13AttributeSetterCallback(v8::Local<v8::String>, v8::Local<v8::Value> jsValue, const v8::PropertyCallbackInfo<void>& info)
 {
     TRACE_EVENT_SET_SAMPLING_STATE("Blink", "DOMSetter");
+    if (V8DOMWrapper::isWrapperOfType(jsValue, &V8Attr::wrapperTypeInfo))
+        UseCounter::count(activeExecutionContext(), UseCounter::AttrUsedAsNodeParameter);
     TestInterfaceV8Internal::Node13AttributeSetter(jsValue, info);
     TRACE_EVENT_SET_SAMPLING_STATE("V8", "V8Execution");
 }
@@ -500,6 +512,8 @@ static void Node14AttributeSetter(v8::Local<v8::Value> jsValue, const v8::Proper
 static void Node14AttributeSetterCallback(v8::Local<v8::String>, v8::Local<v8::Value> jsValue, const v8::PropertyCallbackInfo<void>& info)
 {
     TRACE_EVENT_SET_SAMPLING_STATE("Blink", "DOMSetter");
+    if (V8DOMWrapper::isWrapperOfType(jsValue, &V8Attr::wrapperTypeInfo))
+        UseCounter::count(activeExecutionContext(), UseCounter::AttrUsedAsNodeParameter);
     TestInterfaceV8Internal::Node14AttributeSetter(jsValue, info);
     TRACE_EVENT_SET_SAMPLING_STATE("V8", "V8Execution");
 }
@@ -535,6 +549,8 @@ static void Node15AttributeSetter(v8::Local<v8::Value> jsValue, const v8::Proper
 static void Node15AttributeSetterCallback(v8::Local<v8::String>, v8::Local<v8::Value> jsValue, const v8::PropertyCallbackInfo<void>& info)
 {
     TRACE_EVENT_SET_SAMPLING_STATE("Blink", "DOMSetter");
+    if (V8DOMWrapper::isWrapperOfType(jsValue, &V8Attr::wrapperTypeInfo))
+        UseCounter::count(activeExecutionContext(), UseCounter::AttrUsedAsNodeParameter);
     TestInterfaceV8Internal::Node15AttributeSetter(jsValue, info);
     TRACE_EVENT_SET_SAMPLING_STATE("V8", "V8Execution");
 }
