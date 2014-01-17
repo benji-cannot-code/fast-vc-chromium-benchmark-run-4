f643d5a5ff2974f7d922114d4bf8456752d04a38
beaudoin@chromium.org <beaudoin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-17 19:25:04 +0000

    - Most visited iframe now uses postMessage() to signal the iframing page that the link has been displayed. - Refactor most_visited_thumbnail.js to eliminate client-side secondary thumbnail fallback. - Refactor UMA logging of NTP suggestions events.
    The new postMessage() signal is sent whenever the iframe has failed loading the thumbnails, or when the thumbnail has been successfully loaded. This way it's impossible for the iframing page to learn whether the user has a given URL in its TopSites. The iframing page can use that signal to know when it's possible to display a fallback visual under the iframe.
    
    The refactor significantly simplifies the iframe javascript by dropping support for multiple thumbnails, which can now be handled by the iframing page. UMA logging of NTP suggestions related statistics have also been both simplified and augmented. The patch also includes better unit tests for the UMA logging.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/102433009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@245579 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/search/iframe_source.cc
+++ chrome/browser/search/iframe_source.cc
@@ -10,7 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "chrome/browser/search/instant_io_context.h"
 #include "chrome/common/url_constants.h"
-#include "content/public/browser/render_view_host.h"
+#include "content/public/browser/navigation_entry.h"
+#include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
 #include "grit/browser_resources.h"
 #include "net/url_request/url_request.h"
@@ -52,17 +53,22 @@ bool IframeSource::ShouldDenyXFrameOptions() const {
 
 bool IframeSource::GetOrigin(
     int render_process_id,
-    int render_view_id,
+    int render_frame_id,
     std::string* origin) const {
-  content::RenderViewHost* rvh =
-      content::RenderViewHost::FromID(render_process_id, render_view_id);
-  if (rvh == NULL)
+  content::RenderFrameHost* rfh =
+      content::RenderFrameHost::FromID(render_process_id, render_frame_id);
+  if (rfh == NULL)
     return false;
   content::WebContents* contents =
-      content::WebContents::FromRenderViewHost(rvh);
+      content::WebContents::FromRenderFrameHost(rfh);
   if (contents == NULL)
     return false;
-  *origin = contents->GetURL().GetOrigin().spec();
+  const content::NavigationEntry* entry =
+      contents->GetController().GetVisibleEntry();
+  if (entry == NULL)
+    return false;
+
+  *origin = entry->GetURL().GetOrigin().spec();
   // Origin should not include a trailing slash. That is part of the path.
   base::TrimString(*origin, "/", origin);
   return true;
@@ -79,10 +85,10 @@ void IframeSource::SendResource(
 void IframeSource::SendJSWithOrigin(
     int resource_id,
     int render_process_id,
-    int render_view_id,
+    int render_frame_id,
     const content::URLDataSource::GotDataCallback& callback) {
   std::string origin;
-  if (!GetOrigin(render_process_id, render_view_id, &origin)) {
+  if (!GetOrigin(render_process_id, render_frame_id, &origin)) {
     callback.Run(NULL);
     return;
   }
