056e10e58fbfcf505d8633bec78d3939bb1ac9d8
dsinclair@chromium.org <dsinclair@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-18 00:12:21 +0000

    Defer renderer repaint when filters change.
    If we're currently inside a layout then we can defer the full repaint of the
    renderer if repaint-after-layout is enabled.
    
    BUG=310398
    
    Review URL: https://codereview.chromium.org/140773003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165349 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderLayer.cpp
+++ third_party/WebKit/Source/core/rendering/RenderLayer.cpp
@@ -3893,8 +3893,12 @@ void RenderLayer::updateOrRemoveFilterEffectRenderer()
 void RenderLayer::filterNeedsRepaint()
 {
     toElement(renderer()->node())->scheduleLayerUpdate();
-    if (renderer()->view())
-        renderer()->repaint();
+    if (renderer()->view()) {
+        if (RuntimeEnabledFeatures::repaintAfterLayoutEnabled() && renderer()->frameView()->isInLayout())
+            renderer()->setShouldDoFullRepaintAfterLayout(true);
+        else
+            renderer()->repaint();
+    }
 }
 
 void RenderLayer::addLayerHitTestRects(LayerHitTestRects& rects) const
