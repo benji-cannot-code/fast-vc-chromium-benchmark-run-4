75c5ea32d3075d95f2f5580020300217c1fc4e7a
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-18 00:02:35 +0000

    Remove indirection plumbing in CSSFontSelector.
    Now that fontFaceCache is a direct accessor, we no longer
    need wrappers around adding/removing items to/from it.
    
    R=ksakamoto@chromium.org,eae
    BUG=305885
    
    Review URL: https://codereview.chromium.org/141553003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165348 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/FontFaceSet.cpp
+++ third_party/WebKit/Source/core/css/FontFaceSet.cpp
@@ -262,7 +262,7 @@ bool FontFaceSet::has(FontFace* fontFace, ExceptionState& exceptionState) const
         return false;
     }
     flushPendingStyleChanges(document());
-    return document()->styleEngine()->fontSelector()->cssSegmentedFontFaceCache()->cssFontFaceList().contains(fontFace->cssFontFace());
+    return document()->styleEngine()->fontSelector()->fontFaceCache()->cssFontFaceList().contains(fontFace->cssFontFace());
 }
 
 void FontFaceSet::forEach(PassOwnPtr<FontFaceSetForEachCallback> callback, ScriptValue& thisArg) const
@@ -278,7 +278,7 @@ void FontFaceSet::forEach(PassOwnPtr<FontFaceSetForEachCallback> callback) const
 void FontFaceSet::forEachInternal(PassOwnPtr<FontFaceSetForEachCallback> callback, ScriptValue* thisArg) const
 {
     flushPendingStyleChanges(document());
-    const ListHashSet<RefPtr<CSSFontFace> >& faces = document()->styleEngine()->fontSelector()->cssSegmentedFontFaceCache()->cssFontFaceList();
+    const ListHashSet<RefPtr<CSSFontFace> >& faces = document()->styleEngine()->fontSelector()->fontFaceCache()->cssFontFaceList();
     Vector<RefPtr<FontFace> > fontFaces;
     fontFaces.reserveInitialCapacity(faces.size());
     for (ListHashSet<RefPtr<CSSFontFace> >::const_iterator it = faces.begin(); it != faces.end(); ++it)
@@ -296,7 +296,7 @@ void FontFaceSet::forEachInternal(PassOwnPtr<FontFaceSetForEachCallback> callbac
 unsigned long FontFaceSet::size() const
 {
     flushPendingStyleChanges(document());
-    return document()->styleEngine()->fontSelector()->cssSegmentedFontFaceCache()->cssFontFaceList().size();
+    return document()->styleEngine()->fontSelector()->fontFaceCache()->cssFontFaceList().size();
 }
 
 void FontFaceSet::fireDoneEventIfPossible()
@@ -351,7 +351,7 @@ Vector<RefPtr<FontFace> > FontFaceSet::match(const String& fontString, const Str
         return matchedFonts;
     }
 
-    CSSSegmentedFontFaceCache* cssSegmentedFontFaceCache = document()->styleEngine()->fontSelector()->cssSegmentedFontFaceCache();
+    CSSSegmentedFontFaceCache* cssSegmentedFontFaceCache = document()->styleEngine()->fontSelector()->fontFaceCache();
     for (const FontFamily* f = &font.family(); f; f = f->next()) {
         CSSSegmentedFontFace* face = cssSegmentedFontFaceCache->get(font.fontDescription(), f->family());
         if (face)
@@ -368,7 +368,7 @@ ScriptPromise FontFaceSet::load(const String& fontString, const String& text, Ex
         return ScriptPromise();
     }
 
-    CSSSegmentedFontFaceCache* cssSegmentedFontFaceCache = document()->styleEngine()->fontSelector()->cssSegmentedFontFaceCache();
+    CSSSegmentedFontFaceCache* cssSegmentedFontFaceCache = document()->styleEngine()->fontSelector()->fontFaceCache();
     ScriptPromise promise = ScriptPromise::createPending(executionContext());
     RefPtr<LoadFontPromiseResolver> resolver = LoadFontPromiseResolver::create(font.family(), promise, executionContext());
     for (const FontFamily* f = &font.family(); f; f = f->next()) {
@@ -390,7 +390,7 @@ bool FontFaceSet::check(const String& fontString, const String& text, ExceptionS
         return false;
     }
 
-    CSSSegmentedFontFaceCache* cssSegmentedFontFaceCache = document()->styleEngine()->fontSelector()->cssSegmentedFontFaceCache();
+    CSSSegmentedFontFaceCache* cssSegmentedFontFaceCache = document()->styleEngine()->fontSelector()->fontFaceCache();
     for (const FontFamily* f = &font.family(); f; f = f->next()) {
         CSSSegmentedFontFace* face = cssSegmentedFontFaceCache->get(font.fontDescription(), f->family());
         if (!face || !face->checkFont(nullToSpace(text)))
