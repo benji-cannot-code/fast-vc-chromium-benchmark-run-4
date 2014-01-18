11b3c51ba1cd117d6f706ffbfe7be580ad149e08
pdr@chromium.org <pdr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-18 00:55:32 +0000

    Revert "Cache SkTypeface in FontPlatformData on Mac"
    This reverts commit bc0998dd326f2ce6f32ee622f2f6eaf394ae3d5[1] due to
    an assertion on OSX.
    
    [1] https://src.chromium.org/viewvc/blink?view=rev&revision=165347
    
    BUG=335782
    TBR=leviw
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/140913012
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165352 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/fonts/FontPlatformData.h
+++ third_party/WebKit/Source/platform/fonts/FontPlatformData.h
@@ -57,7 +57,6 @@ typedef const struct __CTFont* CTFontRef;
 
 #if OS(MACOSX)
 #include "platform/fonts/mac/MemoryActivatedFont.h"
-#include "third_party/skia/include/core/SkTypeface.h"
 #endif
 
 #if OS(MACOSX)
@@ -105,7 +104,6 @@ public:
 #if OS(MACOSX)
     CGFontRef cgFont() const { return m_cgFont.get(); }
     CTFontRef ctFont() const;
-    SkTypeface* typeface() const;
 
     bool roundsGlyphAdvances() const;
     bool allowsLigatures() const;
@@ -201,7 +199,6 @@ private:
 
     RefPtr<MemoryActivatedFont> m_inMemoryFont;
     RefPtr<HarfBuzzFace> m_harfBuzzFace;
-    mutable RefPtr<SkTypeface> m_typeface;
 #endif
 
     bool m_isColorBitmapFont;
