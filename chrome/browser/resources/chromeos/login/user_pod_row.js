f3f7515f594dcd236a32a22138c8a3bcd2fb41ad
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-19 23:30:40 +0000

    Revert 245802 "WallpaperLoader should inform caller that load re..."
    Broke interactive_ui_tests on Linux ChromiumOS Tests:
    http://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Tests%20%28dbg%29%283%29/builds/32619/steps/interactive_ui_tests/logs/UseMigratedWallpaperInfo
    
    [26595:26595:0119/124449:30256177955:FATAL:wallpaper_manager.cc(236)] Check failed: BrowserThread::CurrentlyOn(BrowserThread::UI).
     [0x7f741f468504] base::debug::StackTrace::StackTrace()
     [0x7f741f4d6523] logging::LogMessage::~LogMessage()
     [0x00000324a0a8] chromeos::WallpaperManager::PendingWallpaper::OnWallpaperSet()
     [0x00000325bde6] base::internal::RunnableAdapter\u003C>::Run()
     [0x000003259599] base::internal::InvokeHelper\u003C>::MakeItSo()
     [0x00000325612b] base::internal::Invoker\u003C>::Run()
     [0x000000b25601] base::Callback\u003C>::Run()
     [0x00000325076d] chromeos::MovableOnDestroyCallback::~MovableOnDestroyCallback()
     [0x000003252f54] base::DefaultDeleter\u003C>::operator()()
     [0x000003250ba2] base::internal::scoped_ptr_impl\u003C>::~scoped_ptr_impl()
     [0x0000032507be] scoped_ptr\u003C>::~scoped_ptr()
     [0x000003250930] base::internal::PassedWrapper\u003C>::~PassedWrapper()
     [0x00000325edf2] base::internal::BindState\u003C>::~BindState()
     [0x00000325ee40] base::internal::BindState\u003C>::~BindState()
     [0x7f741f41db61] base::RefCountedThreadSafe\u003C>::DeleteInternal()
     [0x7f741f41d41b] base::DefaultRefCountedThreadSafeTraits\u003C>::Destruct()
     [0x7f741f41cc88] base::RefCountedThreadSafe\u003C>::Release()
     [0x7f741f457afb] scoped_refptr\u003C>::~scoped_refptr()
     [0x7f741f457a92] base::internal::CallbackBase::~CallbackBase()
     [0x00000321f3a4] base::Callback\u003C>::~Callback()
     [0x0000034445de] base::internal::BindState\u003C>::~BindState()
     [0x00000344461c] base::internal::BindState\u003C>::~BindState()
     [0x7f741f41db61] base::RefCountedThreadSafe\u003C>::DeleteInternal()
     [0x7f741f41d41b] base::DefaultRefCountedThreadSafeTraits\u003C>::Destruct()
     [0x7f741f41cc88] base::RefCountedThreadSafe\u003C>::Release()
     [0x7f741f457afb] scoped_refptr\u003C>::~scoped_refptr()
     [0x7f741f457a92] base::internal::CallbackBase::~CallbackBase()
     [0x7f741f43a338] base::Callback\u003C>::~Callback()
     [0x7f741f538342] base::PendingTask::~PendingTask()
     [0x7f741f4f6201] base::MessageLoop::DeletePendingTasks()
     [0x7f741f4f4788] base::MessageLoop::~MessageLoop()
     [0x7f74156f263c] base::MessageLoopForUI::~MessageLoopForUI()
     [0x7f74156f266e] base::MessageLoopForUI::~MessageLoopForUI()
     [0x7f74156efd5a] base::DefaultDeleter\u003C>::operator()()
     [0x7f74156ee9a8] base::internal::scoped_ptr_impl\u003C>::~scoped_ptr_impl()
     [0x7f74156ee524] scoped_ptr\u003C>::~scoped_ptr()
     [0x7f74156e8857] content::BrowserMainLoop::~BrowserMainLoop()
     [0x7f74156e888c] content::BrowserMainLoop::~BrowserMainLoop()
     [0x7f74156f35e6] base::DefaultDeleter\u003C>::operator()()
     [0x7f74156f36a7] base::internal::scoped_ptr_impl\u003C>::reset()
     [0x7f74156f3479] scoped_ptr\u003C>::reset()
     [0x7f74156f333c] content::BrowserMainRunnerImpl::Shutdown()
     [0x7f74156e6d46] content::BrowserMain()
     [0x00000376384c] content::BrowserTestBase::SetUp()
     [0x000001c5b554] InProcessBrowserTest::SetUp()
    
    BUG=333809
    TBR=alemate@chromium.org
    
    Review URL: https://codereview.chromium.org/142593003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@245842 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/login/user_pod_row.js
+++ chrome/browser/resources/chromeos/login/user_pod_row.js
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+<include src="wallpaper_loader.js"></include>
+
 /**
  * @fileoverview User pod row implementation.
  */
@@ -1026,6 +1028,9 @@ cr.define('login', function() {
     // Pod that was most recently focused, if any.
     lastFocusedPod_: undefined,
 
+    // Note: created only in decorate() !
+    wallpaperLoader_: undefined,
+
     // Pods whose initial images haven't been loaded yet.
     podsWithPendingImages_: [],
 
@@ -1039,6 +1044,7 @@ cr.define('login', function() {
         mousemove: [this.handleMouseMove_.bind(this), false],
         keydown: [this.handleKeyDown.bind(this), false]
       };
+      this.wallpaperLoader_ = new login.WallpaperLoader();
     },
 
     /**
@@ -1369,6 +1375,7 @@ cr.define('login', function() {
       }
       this.insideFocusPod_ = true;
 
+      this.wallpaperLoader_.reset();
       for (var i = 0, pod; pod = this.pods[i]; ++i) {
         if (!this.isSinglePod) {
           pod.isActionBoxMenuActive = false;
@@ -1391,8 +1398,9 @@ cr.define('login', function() {
         podToFocus.classList.remove('faded');
         podToFocus.classList.add('focused');
         podToFocus.reset(true);  // Reset and give focus.
-        // focusPod() automatically loads wallpaper
         chrome.send('focusPod', [podToFocus.user.username]);
+
+        this.wallpaperLoader_.scheduleLoad(podToFocus.user.username, opt_force);
         this.firstShown_ = false;
         this.lastFocusedPod_ = podToFocus;
       }
@@ -1416,7 +1424,16 @@ cr.define('login', function() {
      */
     loadLastWallpaper: function() {
       if (this.lastFocusedPod_)
-        chrome.send('loadWallpaper', [this.lastFocusedPod_.user.username]);
+        this.wallpaperLoader_.scheduleLoad(this.lastFocusedPod_.user.username,
+                                           true /* force */);
+    },
+
+    /**
+     * Handles 'onWallpaperLoaded' event. Recalculates statistics and
+     * [re]schedules next wallpaper load.
+     */
+    onWallpaperLoaded: function(username) {
+      this.wallpaperLoader_.onWallpaperLoaded(username);
     },
 
     /**
@@ -1685,7 +1702,8 @@ cr.define('login', function() {
             focusedPod.reset(true);
             // Notify screen that it is ready.
             screen.onShow();
-            chrome.send('loadWallpaper', [focusedPod.user.username]);
+            self.wallpaperLoader_.scheduleLoad(focusedPod.user.username,
+                                               true /* force */);
           }
         });
         // Guard timer for 1 second -- it would conver all possible animations.
