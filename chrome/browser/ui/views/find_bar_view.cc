benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 52e90975349587b87402359a22361188f9396f54

Reland Merge NativeTextfieldViews into views::Textfield.
Reverted in r244402 for Linux-Aura test failures.
I fixed Ctrl+Shift+Delete/Backspace behavior on Linux here.

Consolidate the split textfield code into Textfield.
No behavior changes (except STYLE_LOWERCASE GetText nit).
Use the underlying model's text and font_list storage.
Remove unused code; rename TextfieldViewsModel::text().
Revise gesture and command handling.

Skip NotifyAccessibilityEvent native behavior without widget.
(No HWND for Views not yet added to view hierarchy)

Make TouchEditable::GetNativeView const; avoids conflict.
Fix includes, forward decls, Font[List] usage, tests.

Ideas for followup cleanup and refactoring:
-Cleanup View overrides decl/def order.
-Refactor RemoveBorder, SetHorizontalMargins, etc.
-Try to use composition instead of inheritance.
-Remove textfield style ctor; refactor style code.
-Remove STYLE_LOWERCASE functionality (one user).
-Replace placeholder DrawStringRect with a Label view.
-Cleanup CreateTouchSelectionControllerAndNotifyIt, etc.
-Cleanup SetColor/SetTextColor, etc.

BUG=131660
TEST=No textfield behavior/appearance regressions.
TBR=sky@chromium.org

Review URL: https://codereview.chromium.org/135863002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244436 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/views/find_bar_view.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/theme_provider.h"
 #include "ui/events/event.h"
 #include "ui/gfx/canvas.h"
+#include "ui/views/border.h"
 #include "ui/views/controls/button/image_button.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/controls/textfield/textfield.h"
@@ -86,6 +87,10 @@ FindBarView::FindBarView(FindBarHost* host)
   find_text_->set_default_width_in_chars(kDefaultCharWidth);
   find_text_->SetController(this);
   find_text_->SetAccessibleName(l10n_util::GetStringUTF16(IDS_ACCNAME_FIND));
+  // The find bar textfield has a background image instead of a border.
+  const gfx::Insets insets = find_text_->GetInsets();
+  find_text_->set_border(
+      views::Border::CreateEmptyBorder(insets.top(), 0, insets.bottom(), 2));
 
   AddChildView(find_text_);
 
@@ -338,14 +343,6 @@ void FindBarView::Layout() {
       find_text_edge, find_previous_button_->y(),
       find_previous_button_->x() - find_text_edge,
       find_previous_button_->height());
-}
-
-void FindBarView::ViewHierarchyChanged(
-    const ViewHierarchyChangedDetails& details) {
-  if (details.is_add && details.child == this) {
-    find_text_->SetHorizontalMargins(0, 2);  // Left and Right margins.
-    find_text_->RemoveBorder();  // We draw our own border (a background image).
-  }
 }
 
 gfx::Size FindBarView::GetPreferredSize() {

chrome/browser/ui/views/autofill/autofill_dialog_views.cc
@@ -209,9 +209,9 @@ class SectionRowView : public views::View {
     int end_x = bounds.width();
     views::View* decorated = child_at(2);
     if (decorated->visible()) {
-      decorated->SizeToPreferredSize();
-      decorated->SetX(bounds.width() - decorated->bounds().width());
-      decorated->SetY(bounds.y());
+      const int preferred_width = decorated->GetPreferredSize().width();
+      decorated->SetBounds(bounds.width() - preferred_width, bounds.y(),
+                           preferred_width, bounds.height());
       end_x = decorated->bounds().x() - kAroundTextPadding;
     }
 

chrome/browser/ui/views/autofill/decorated_textfield.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/background.h"
 #include "ui/views/controls/button/label_button.h"
 #include "ui/views/controls/focusable_border.h"
-#include "ui/views/controls/textfield/native_textfield_views.h"
 #include "ui/views/controls/textfield/textfield_controller.h"
 
 namespace {
@@ -26,38 +25,28 @@ namespace autofill {
 // static
 const char DecoratedTextfield::kViewClassName[] = "autofill/DecoratedTextfield";
 
-const int DecoratedTextfield::kMagicInsetNumber = 6;
-
 DecoratedTextfield::DecoratedTextfield(
     const base::string16& default_value,
     const base::string16& placeholder,
     views::TextfieldController* controller)
-    : border_(new views::FocusableBorder()),
-      invalid_(false),
+    : invalid_(false),
       editable_(true) {
   UpdateBackground();
-
-  set_border(border_);
-  // Removes the border from |native_wrapper_|.
-  RemoveBorder();
+  UpdateBorder();
 
   set_placeholder_text(placeholder);
   SetText(default_value);
   SetController(controller);
-  SetHorizontalMargins(0, 0);
 }
 
 DecoratedTextfield::~DecoratedTextfield() {}
 
 void DecoratedTextfield::SetInvalid(bool invalid) {
-  invalid_ = invalid;
-  if (!editable_)
+  if (invalid_ == invalid)
     return;
 
-  if (invalid)
-    border_->SetColor(kWarningColor);
-  else
-    border_->UseDefaultColor();
+  invalid_ = invalid;
+  UpdateBorder();
   SchedulePaint();
 }
 
@@ -66,14 +55,7 @@ void DecoratedTextfield::SetEditable(bool editable) {
     return;
 
   editable_ = editable;
-  if (editable) {
-    SetInvalid(invalid_);
-    UseDefaultBackgroundColor();
-  } else {
-    border_->SetColor(SK_ColorTRANSPARENT);
-    SetBackgroundColor(SK_ColorTRANSPARENT);
-  }
-
+  UpdateBorder();
   UpdateBackground();
   SetEnabled(editable);
   IconChanged();
@@ -112,10 +94,7 @@ void DecoratedTextfield::SetTooltipIcon(const base::string16& text) {
 }
 
 base::string16 DecoratedTextfield::GetPlaceholderText() const {
-  if (!editable_)
-    return base::string16();
-
-  return views::Textfield::GetPlaceholderText();
+  return editable_ ? views::Textfield::GetPlaceholderText() : base::string16();
 }
 
 const char* DecoratedTextfield::GetClassName() const {
@@ -126,25 +105,14 @@ views::View* DecoratedTextfield::GetEventHandlerForRect(const gfx::Rect& rect) {
   views::View* handler = views::Textfield::GetEventHandlerForRect(rect);
   if (handler->GetClassName() == TooltipIcon::kViewClassName)
     return handler;
-  return textfield_view_;
-}
-
-void DecoratedTextfield::OnFocus() {
-  views::Textfield::OnFocus();
-  SchedulePaint();
-}
-
-void DecoratedTextfield::OnBlur() {
-  views::Textfield::OnBlur();
-  SchedulePaint();
+  return this;
 }
 
 gfx::Size DecoratedTextfield::GetPreferredSize() {
-  int w = views::Textfield::GetPreferredSize().width();
-  views::LabelButton button(NULL, base::string16());
-  button.SetStyle(views::Button::STYLE_BUTTON);
-  int h = button.GetPreferredSize().height();
-  return gfx::Size(w, h - kMagicInsetNumber);
+  static const int height =
+      views::LabelButton(NULL, base::string16()).GetPreferredSize().height();
+  const gfx::Size size = views::Textfield::GetPreferredSize();
+  return gfx::Size(size.width(), std::max(size.height(), height));
 }
 
 void DecoratedTextfield::Layout() {
@@ -158,29 +126,45 @@ void DecoratedTextfield::Layout() {
         bounds.right() - icon_size.width() - kTextfieldIconPadding;
     // Vertically centered.
     int y = bounds.y() + (bounds.height() - icon_size.height()) / 2;
-    icon_view_->SetBounds(x,
-                          y,
-                          icon_size.width(),
-                          icon_size.height());
+    icon_view_->SetBounds(x, y, icon_size.width(), icon_size.height());
   }
 }
 
 void DecoratedTextfield::UpdateBackground() {
+  if (editable_)
+    UseDefaultBackgroundColor();
+  else
+    SetBackgroundColor(SK_ColorTRANSPARENT);
   set_background(
       views::Background::CreateSolidBackground(GetBackgroundColor()));
 }
 
+void DecoratedTextfield::UpdateBorder() {
+  views::FocusableBorder* border = new views::FocusableBorder();
+  if (invalid_)
+    border->SetColor(kWarningColor);
+  else if (!editable_)
+    border->SetColor(SK_ColorTRANSPARENT);
+
+  const gfx::Insets insets = GetInsets();
+  int left = icon_view_ ?
+      icon_view_->GetPreferredSize().width() + 2 * kTextfieldIconPadding : 0;
+  int right = 0;
+  if (base::i18n::IsRTL())
+    std::swap(left, right);
+  border->SetInsets(insets.top(), insets.left() + left, insets.bottom(),
+                    insets.right() + right);
+  set_border(border);
+}
+
 void DecoratedTextfield::IconChanged() {
   // Don't show the icon if nothing else is showing.
-  icon_view_->SetVisible(editable_ || !text().empty());
+  const bool visible = editable_ || !text().empty();
+  if (icon_view_->visible() == visible)
+    return;
 
-  int icon_space = icon_view_ ?
-      icon_view_->GetPreferredSize().width() + 2 * kTextfieldIconPadding : 0;
-
-  bool is_rtl = base::i18n::IsRTL();
-  SetHorizontalMargins(is_rtl ? icon_space : 0, is_rtl ? 0 : icon_space);
-
-  Layout();
+  icon_view_->SetVisible(visible);
+  UpdateBorder();
   SchedulePaint();
 }
 

chrome/browser/ui/views/autofill/decorated_textfield.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/textfield/textfield.h"
 
 namespace views {
-class FocusableBorder;
+class ImageView;
 class TextfieldController;
 }
 
@@ -37,8 +37,7 @@ class DecoratedTextfield : public views::Textfield {
   void SetEditable(bool editable);
   bool editable() const { return editable_; }
 
-  // Sets the icon to be displayed inside the textfield at the end of the
-  // text.
+  // Sets the icon to display inside the textfield at the end of the text.
   void SetIcon(const gfx::Image& icon);
 
   // Sets a tooltip for this field. This will override the icon set with
@@ -53,28 +52,18 @@ class DecoratedTextfield : public views::Textfield {
   virtual gfx::Size GetPreferredSize() OVERRIDE;
   virtual void Layout() OVERRIDE;
   virtual views::View* GetEventHandlerForRect(const gfx::Rect& rect) OVERRIDE;
-  virtual void OnFocus() OVERRIDE;
-  virtual void OnBlur() OVERRIDE;
 
  private:
   FRIEND_TEST_ALL_PREFIXES(DecoratedTextfieldTest, HeightMatchesButton);
 
-  // Updates the background of |this| after it may have changed. This is
-  // necessary for the sake of the padding around the native textfield.
+  // Updates the background after its color may have changed.
   void UpdateBackground();
 
-  // Called to update the layout after SetIcon or SetTooltipIcon has been
-  // called.
+  // Updates the border after its color or insets may have changed.
+  void UpdateBorder();
+
+  // Called to update the layout after SetIcon or SetTooltipIcon was called.
   void IconChanged();
-
-  // This number corresponds to the number of pixels in the images that
-  // are used to draw a views button which are above or below the actual border.
-  // This number is encoded in the button assets themselves, so there's no other
-  // way to get it than to hardcode it here.
-  static const int kMagicInsetNumber;
-
-  // We draw the border.
-  views::FocusableBorder* border_;  // Weak.
 
   // The view that holds the icon at the end of the textfield.
   scoped_ptr<views::ImageView> icon_view_;

chrome/browser/ui/views/autofill/decorated_textfield_unittest.cc
@@ -1,24 +1,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#include "base/strings/utf_string_conversions.h"
-#include "chrome/browser/ui/views/autofill/decorated_textfield.h"
-#include "testing/gtest/include/gtest/gtest.h"
-#include "ui/views/controls/button/label_button.h"
-
-namespace autofill {
-
-TEST(DecoratedTextfieldTest, HeightMatchesButton) {
-  DecoratedTextfield textfield(base::ASCIIToUTF16("default"),
-                               base::ASCIIToUTF16("placeholder"),
-                               NULL);
-  views::LabelButton button(NULL, base::ASCIIToUTF16("anyoldtext"));;
-  button.SetStyle(views::Button::STYLE_BUTTON);
-  EXPECT_EQ(button.GetPreferredSize().height() -
-                DecoratedTextfield::kMagicInsetNumber,
-            textfield.GetPreferredSize().height());
-}
-
-}  // namespace autofill

chrome/browser/ui/views/cookie_info_view.cc
@@ -129,7 +129,7 @@ void CookieInfoView::AddLabelRow(int layout_id, views::GridLayout* layout,
 
   // Now that the Textfield is in the view hierarchy, it can be initialized.
   text_field->SetReadOnly(true);
-  text_field->RemoveBorder();
+  text_field->set_border(NULL);
   // Color these borderless text areas the same as the containing dialog.
   text_field->SetBackgroundColor(GetNativeTheme()->GetSystemColor(
       ui::NativeTheme::kColorId_DialogBackground));

chrome/browser/ui/views/dropdown_bar_host.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/animation/animation_delegate.h"
 #include "ui/gfx/native_widget_types.h"
 #include "ui/gfx/rect.h"
-#include "ui/views/controls/textfield/textfield.h"
 #include "ui/views/focus/focus_manager.h"
 
 class BrowserView;

chrome/browser/ui/views/find_bar_view.h
@@ -71,8 +71,6 @@ class FindBarView : public DropdownBarView,
   virtual void OnPaint(gfx::Canvas* canvas) OVERRIDE;
   virtual void Layout() OVERRIDE;
   virtual gfx::Size GetPreferredSize() OVERRIDE;
-  virtual void ViewHierarchyChanged(
-      const ViewHierarchyChangedDetails& details) OVERRIDE;
 
   // views::ButtonListener:
   virtual void ButtonPressed(views::Button* sender,

commit 09e83665f2a417fa422c16638983261d83a43582

Remove Views find bar textfield border.
The border is not needed for painting or sizing.
It also causes an ugly solid color border on CrOS.
Fixes a regression from r244436 and r244746.

BUG=335346
TEST=The find bar looks as expected on Views.
R=sky@chromium.org

Review URL: https://codereview.chromium.org/142193002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@245821 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/views/find_bar_view.cc
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/theme_provider.h"
 #include "ui/events/event.h"
 #include "ui/gfx/canvas.h"
-#include "ui/views/border.h"
 #include "ui/views/controls/button/image_button.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/controls/textfield/textfield.h"
@@ -88,10 +87,7 @@ FindBarView::FindBarView(FindBarHost* host)
   find_text_->set_controller(this);
   find_text_->SetAccessibleName(l10n_util::GetStringUTF16(IDS_ACCNAME_FIND));
   // The find bar textfield has a background image instead of a border.
-  const gfx::Insets insets = find_text_->GetInsets();
-  find_text_->set_border(
-      views::Border::CreateEmptyBorder(insets.top(), 0, insets.bottom(), 2));
-
+  find_text_->set_border(NULL);
   AddChildView(find_text_);
 
   match_count_text_ = new views::Label();