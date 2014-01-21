8d5c9da04887f95f37ebd19024903e164eab25d8
hayato@chromium.org <hayato@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-21 19:57:26 +0000

    Fix crash when focus-navigating to a shadow insertion point in the oldest shadow tree.
    Shadow insertion points in the oldest shadow tree should not be treated as a focus scope owner.
    
    The relevant spec is: http://w3c.github.io/webcomponents/spec/shadow/#focus-navigation
    
    BUG=336289
    
    Review URL: https://codereview.chromium.org/143643004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165476 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/FocusController.cpp
+++ third_party/WebKit/Source/core/page/FocusController.cpp
@@ -62,6 +62,11 @@ namespace WebCore {
 
 using namespace HTMLNames;
 
+static inline bool isShadowInsertionPointFocusScopeOwner(Node& node)
+{
+    return isActiveShadowInsertionPoint(node) && toHTMLShadowElement(node).olderShadowRoot();
+}
+
 // FIXME: Some of Node* return values and Node* arguments should be Element*.
 
 FocusNavigationScope::FocusNavigationScope(TreeScope* treeScope)
@@ -103,7 +108,7 @@ FocusNavigationScope FocusNavigationScope::ownedByNonFocusableFocusScopeOwner(No
     ASSERT(node);
     if (isShadowHost(node))
         return FocusNavigationScope::ownedByShadowHost(node);
-    ASSERT(isActiveShadowInsertionPoint(*node));
+    ASSERT(isShadowInsertionPointFocusScopeOwner(*node));
     return FocusNavigationScope::ownedByShadowInsertionPoint(toHTMLShadowElement(node));
 }
 
@@ -121,7 +126,7 @@ FocusNavigationScope FocusNavigationScope::ownedByIFrame(HTMLFrameOwnerElement* 
 
 FocusNavigationScope FocusNavigationScope::ownedByShadowInsertionPoint(HTMLShadowElement* shadowInsertionPoint)
 {
-    ASSERT(shadowInsertionPoint->isActive());
+    ASSERT(isShadowInsertionPointFocusScopeOwner(*shadowInsertionPoint));
     return FocusNavigationScope(shadowInsertionPoint->olderShadowRoot());
 }
 
@@ -197,7 +202,7 @@ static inline bool isKeyboardFocusableShadowHost(Node* node)
 static inline bool isNonFocusableFocusScopeOwner(Node* node)
 {
     ASSERT(node);
-    return isNonKeyboardFocusableShadowHost(node) || isActiveShadowInsertionPoint(*node);
+    return isNonKeyboardFocusableShadowHost(node) || isShadowInsertionPointFocusScopeOwner(*node);
 }
 
 static inline int adjustedTabIndex(Node* node)
