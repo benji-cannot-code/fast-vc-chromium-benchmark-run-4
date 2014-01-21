e3bd2a2e0b1f10bc4cb05df99342a8f832f2e03e
rob.buis@samsung.com <rob.buis@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-21 21:11:48 +0000

    Code problems detected by cppcheck
    Testing cppcheck with flags style and performance showed up these problems.
    
    Review URL: https://codereview.chromium.org/141273002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165481 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/FrameLoader.cpp
+++ third_party/WebKit/Source/core/loader/FrameLoader.cpp
@@ -1005,7 +1005,7 @@ void FrameLoader::detachChildren()
     for (Frame* child = m_frame->tree().lastChild(); child; child = child->tree().previousSibling())
         childrenToDetach.append(child);
     FrameVector::iterator end = childrenToDetach.end();
-    for (FrameVector::iterator it = childrenToDetach.begin(); it != end; it++)
+    for (FrameVector::iterator it = childrenToDetach.begin(); it != end; ++it)
         (*it)->loader().detachFromParent();
 }
 
--- third_party/WebKit/Source/platform/exported/WebHTTPLoadInfo.cpp
+++ third_party/WebKit/Source/platform/exported/WebHTTPLoadInfo.cpp
@@ -56,8 +56,8 @@ void WebHTTPLoadInfo::assign(const WebHTTPLoadInfo& r)
 }
 
 WebHTTPLoadInfo::WebHTTPLoadInfo(WTF::PassRefPtr<WebCore::ResourceLoadInfo> value)
+    : m_private(value)
 {
-    m_private = value;
 }
 
 WebHTTPLoadInfo::operator WTF::PassRefPtr<WebCore::ResourceLoadInfo>() const
