60e10b32a61d2eb6bbf3017d43c71a55f9c41237
yosin@chromium.org <yosin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-21 02:31:45 +0000

    Make document.elementFromPoint() to work with OPTION elements
    Since r159012, which makes document.caretRangeFromPoint() to work with :first-letter style by getting text offsets, document.elementFromPoint() uses RenderObject from HitTestResult rather than HitTestResult.innerNode(). This works fine for all cases except for node which doesn't have RenderObject, e.g. OPTION element.
    
    This patch restores behavior before r159012 for document.elementFromPoint().
    Note: :first-letter style doesn't affect result of document.elementFromPoint(), because it doesn't use text offsets.
    
    BUG=335010
    TEST=LayoutTests/fast/dom/Document/document-elementFromPoint-on-option-element.html
    
    Review URL: https://codereview.chromium.org/137313007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165426 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -1211,8 +1211,8 @@ PassRefPtr<Range> Document::caretRangeFromPoint(int x, int y)
 {
     if (!renderView())
         return 0;
-    LayoutPoint localPoint;
-    RenderObject* renderer = rendererFromPoint(this, x, y, &localPoint);
+    HitTestResult result = hitTestInDocument(this, x, y);
+    RenderObject* renderer = result.renderer();
     if (!renderer)
         return 0;
 
@@ -1224,7 +1224,7 @@ PassRefPtr<Range> Document::caretRangeFromPoint(int x, int y)
         return Range::create(*this, container, offset, container, offset);
     }
 
-    PositionWithAffinity positionWithAffinity = renderer->positionForPoint(localPoint);
+    PositionWithAffinity positionWithAffinity = renderer->positionForPoint(result.localPoint());
     if (positionWithAffinity.position().isNull())
         return 0;
 
