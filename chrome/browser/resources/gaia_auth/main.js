df15984936d2d681348786b01c3660ec03c3acde
guohui@chromium.org <guohui@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-21 19:08:32 +0000

    Append source param to embedded gaia URL
    as requested by haon@ for logging purpose
    
    BUG=
    
    Review URL: https://codereview.chromium.org/136793028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@246085 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/gaia_auth/main.js
+++ chrome/browser/resources/gaia_auth/main.js
@@ -44,6 +44,7 @@ Authenticator.prototype = {
   PARENT_PAGE: 'chrome://oobe/',
   SERVICE_ID: 'chromeoslogin',
   CONTINUE_URL: Authenticator.THIS_EXTENSION_ORIGIN + '/success.html',
+  CONSTRAINED_FLOW_SOURCE: 'chrome',
 
   initialize: function() {
     var params = getUrlSearchParams(location.search);
@@ -89,7 +90,8 @@ Authenticator.prototype = {
       url = appendParam(url, 'hl', this.inputLang_);
     if (this.inputEmail_)
       url = appendParam(url, 'Email', this.inputEmail_);
-
+    if (this.constrained_)
+      url = appendParam(url, 'source', this.CONSTRAINED_FLOW_SOURCE);
     return url;
   },
 
@@ -127,6 +129,11 @@ Authenticator.prototype = {
         // reference to the embedder.
         gaiaFrame.contentWindow.postMessage('', gaiaFrame.src);
       });
+      if (this.constrained_) {
+        var preventContextMenu = 'document.addEventListener("contextmenu", ' +
+                                 'function(e) {e.preventDefault();})';
+        gaiaFrame.executeScript({code: preventContextMenu});
+      }
     }
 
     this.loaded_ || this.onLoginUILoaded();
@@ -169,9 +176,6 @@ Authenticator.prototype = {
           'newwindow', this.onWebviewNewWindow_.bind(this, gaiaFrame));
     }
     if (this.constrained_) {
-      var preventContextMenu = 'document.addEventListener("contextmenu", ' +
-                               'function(e) {e.preventDefault();})';
-      gaiaFrame.executeScript({code: preventContextMenu});
       gaiaFrame.request.onCompleted.addListener(
           this.onWebviewRequestCompleted_.bind(this),
           {urls: ['<all_urls>'], types: ['main_frame']},
