480f1851acff0b20aa922624cf48c9edfbd718ce
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-21 22:17:28 +0000

    The new animations engine doesn't need to use StyleChangeFromRenderer
    The old animation engine needed to use this because the animation was
    updated inside RenderObject::setAnimatableStyle, but the new engine
    handles animation updates inside Element::styleForRenderer() and the
    StyleResolver so we don't need to force a style change if there's
    nothing that actually changed.
    
    This patch removes usage of this flag as the first step of getting rid
    of this flag entirely. Forcing a new RenderStyle through the styleWillChange
    and styleDidChange machinery when nothing actually changed is not something
    we should support.
    
    BUG=336552
    
    Review URL: https://codereview.chromium.org/141483009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165485 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Element.cpp
+++ third_party/WebKit/Source/core/dom/Element.cpp
@@ -1784,7 +1784,7 @@ void Element::setNeedsAnimationStyleRecalc()
     if (styleChangeType() != NoStyleChange)
         return;
 
-    setNeedsStyleRecalc(LocalStyleChange, StyleChangeFromRenderer);
+    setNeedsStyleRecalc(LocalStyleChange);
     setAnimationStyleChange(true);
 }
 
