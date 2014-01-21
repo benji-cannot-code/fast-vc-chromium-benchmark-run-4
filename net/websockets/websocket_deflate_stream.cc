f531ed2480e7dd168a0e9653506ef7d34d3f2904
ricea@chromium.org <ricea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-21 09:59:43 +0000

    Add window_bits to DeflateStream constructor
    Allow specifying the window size that is used for transmitting data,
    ie. client_max_window_size.
    
    Add tests WindowBits8 and WindowBits10 to verify that the parameter is
    effective.
    
    BUG=280910
    TEST=net_unittests --gtest_filter=WebSocketDeflateStream*
    
    Review URL: https://codereview.chromium.org/143823002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@246012 0039d316-1c4b-4281-b951-d872f2087c98

--- net/websockets/websocket_deflate_stream.cc
+++ net/websockets/websocket_deflate_stream.cc
@@ -37,6 +37,7 @@ const size_t kChunkSize = 4 * 1024;
 WebSocketDeflateStream::WebSocketDeflateStream(
     scoped_ptr<WebSocketStream> stream,
     WebSocketDeflater::ContextTakeOverMode mode,
+    int client_window_bits,
     scoped_ptr<WebSocketDeflatePredictor> predictor)
     : stream_(stream.Pass()),
       deflater_(mode),
@@ -47,7 +48,9 @@ WebSocketDeflateStream::WebSocketDeflateStream(
       current_writing_opcode_(WebSocketFrameHeader::kOpCodeText),
       predictor_(predictor.Pass()) {
   DCHECK(stream_);
-  deflater_.Initialize(kWindowBits);
+  DCHECK_GE(client_window_bits, 8);
+  DCHECK_LE(client_window_bits, 15);
+  deflater_.Initialize(client_window_bits);
   inflater_.Initialize(kWindowBits);
 }
 
