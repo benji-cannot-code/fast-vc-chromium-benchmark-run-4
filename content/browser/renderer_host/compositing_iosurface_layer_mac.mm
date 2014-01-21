dc92b8f0434de03f293ee26cd9c11a0eba20c63b
ccameron@chromium.org <ccameron@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-21 18:31:43 +0000

    Fix bug where GL fence objects were shared between contexts.
    These objects were allocated during DrawIOSurface when copying
    the IOSurface to a video frame.
    
    This pulls the copy to the video frame up to CompositorSwapBuffers,
    and performs the copy in a separate context. Using the separate
    context makes a call to glFlush before the copy necessary (to be sure
    that the copy gets the correct contents), and after, to kick off the async
    copy.
    
    Moving the copy up to the swap make it so that we won't send more
    frames just because of window damage.
    
    BUG=334742
    
    Review URL: https://codereview.chromium.org/137893022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@246077 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/compositing_iosurface_layer_mac.mm
+++ content/browser/renderer_host/compositing_iosurface_layer_mac.mm
@@ -126,7 +126,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         context_,
         window_rect,
         window_scale_factor,
-        renderWidgetHostView_->frame_subscriber(),
         false)) {
     renderWidgetHostView_->GotAcceleratedCompositingError();
   }
