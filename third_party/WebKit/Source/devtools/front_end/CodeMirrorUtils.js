a9179ebc1c306cec240cc8b0161b56647b7891b8
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-22 16:24:41 +0000

    DevTools: operate single set of tokenizer types, do not patch mods in workers.
    R=lushnikov@chromium.org
    
    Review URL: https://codereview.chromium.org/134733018
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165531 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/CodeMirrorUtils.js
+++ third_party/WebKit/Source/devtools/front_end/CodeMirrorUtils.js
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 WebInspector.CodeMirrorUtils = {
     /**
      * @param {string} mimeType
-     * @return {function(string, function(string, string, number, number))}
+     * @return {function(string, function(string, ?string, number, number))}
      */
     createTokenizer: function(mimeType)
     {
@@ -49,58 +49,5 @@ WebInspector.CodeMirrorUtils = {
             }
         }
         return tokenize;
-    },
-
-    /**
-     * @param {string} tokenType
-     * @return {?string}
-     */
-    convertTokenType: function(tokenType)
-    {
-        if (tokenType.startsWith("js-variable") || tokenType.startsWith("js-property") || tokenType === "js-def")
-            return "javascript-ident";
-        if (tokenType === "js-string-2")
-            return "javascript-regexp";
-        if (tokenType === "js-number" || tokenType === "js-comment" || tokenType === "js-string" || tokenType === "js-keyword")
-            return "javascript-" + tokenType.substring("js-".length);
-        if (tokenType === "css-number")
-            return "css-number";
-        return null;
-    },
-
-    /**
-     * @param {string} modeName
-     * @param {string} tokenPrefix
-     */
-    overrideModeWithPrefixedTokens: function(modeName, tokenPrefix)
-    {
-        var oldModeName = modeName + "-old";
-        if (CodeMirror.modes[oldModeName])
-            return;
-
-        CodeMirror.defineMode(oldModeName, CodeMirror.modes[modeName]);
-        CodeMirror.defineMode(modeName, modeConstructor);
-
-        function modeConstructor(config, parserConfig)
-        {
-            var innerConfig = {};
-            for (var i in parserConfig)
-                innerConfig[i] = parserConfig[i];
-            innerConfig.name = oldModeName;
-            var codeMirrorMode = CodeMirror.getMode(config, innerConfig);
-            codeMirrorMode.name = modeName;
-            codeMirrorMode.token = tokenOverride.bind(null, codeMirrorMode.token);
-            return codeMirrorMode;
-        }
-
-        function tokenOverride(superToken, stream, state)
-        {
-            var token = superToken(stream, state);
-            return token ? tokenPrefix + token : token;
-        }
     }
 }
-
-WebInspector.CodeMirrorUtils.overrideModeWithPrefixedTokens("css", "css-");
-WebInspector.CodeMirrorUtils.overrideModeWithPrefixedTokens("javascript", "js-");
-WebInspector.CodeMirrorUtils.overrideModeWithPrefixedTokens("xml", "xml-");
--- third_party/WebKit/Source/devtools/front_end/DOMSyntaxHighlighter.js
+++ third_party/WebKit/Source/devtools/front_end/DOMSyntaxHighlighter.js
@@ -62,7 +62,7 @@ WebInspector.DOMSyntaxHighlighter.prototype = {
 
         /**
          * @param {string} token
-         * @param {string} tokenType
+         * @param {?string} tokenType
          * @param {number} column
          * @param {number} newColumn
          * @this {WebInspector.DOMSyntaxHighlighter}
