c021c8ab407091b0016dc850fc8a47e314a6541b
miletus@chromium.org <miletus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-22 10:58:46 +0000

    Remove LatencyInfo::MergeWith()
    Now that we have removed LatencyInfo merging throughout chrome, we
    can remove the function LatencyInfo::MergeWith().
    
    Also remove |dump_to_trace| from LatencyInfo::AddLatencyNumberWithTimestamp().
    Previously |dump_to_trace| exists because when we merge two LatencyInfo by
    calling A.MergeWith(B), we would add B's begin/terminal component into A, which
    would accidentally cause TRACE_EVENT_ASYNC_BEGIN/END to be triggered on A. So
    that we have |dump_to_trace| to guard against this. Now that we removed MergeWith, we
    can also remove |dump_to_trace|.
    
    BUG=246034
    TEST=code still compiles.
    
    Review URL: https://codereview.chromium.org/141473002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@246288 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/events/event.cc
+++ ui/events/event.cc
@@ -446,8 +446,7 @@ TouchEvent::TouchEvent(const base::NativeEvent& native_event)
       0,
       0,
       base::TimeTicks::FromInternalValue(time_stamp().ToInternalValue()),
-      1,
-      true);
+      1);
 
 #if defined(USE_X11)
   XIDeviceEvent* xiev = static_cast<XIDeviceEvent*>(native_event->xcookie.data);
