d1f778773858ea188c6936448ed1ec7cbcb363ea
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-22 20:55:48 +0000

    Remove RenderImage::m_needsToSetSizeForAltText
    Remove another user of StyleChangeFromRenderer. This code doesn't look
    needed and doesn't make sense. The test that was landed with it passes
    even without the code today too.
    
    The code came from http://trac.webkit.org/changeset/72040 and the
    hasPendingStyleRecalc() check comes from
    http://trac.webkit.org/changeset/72135
    
    Checking for a pending recalc like this code did is very suspect, any
    number of things can cause a recalc, or schedule one later, so checking
    for a pending recalc inside the ::imageChanged callback doesn't make a
    lot of sense.
    
    Removing this code also lets us remove Document::hasPendingStyleRecalc().
    In doing so I also moved methods related to the style recalc machinery
    to private so people will not use them.
    
    BUG=336552
    R=ojan@chromium.org
    
    Review URL: https://codereview.chromium.org/129873009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165553 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Document.h
+++ third_party/WebKit/Source/core/dom/Document.h
@@ -634,10 +634,7 @@ public:
     Element* cssTarget() const { return m_cssTarget; }
 
     void scheduleStyleRecalc();
-    void unscheduleStyleRecalc();
-    bool hasPendingStyleRecalc() const;
     bool hasPendingForcedStyleRecalc() const;
-    void styleRecalcTimerFired(Timer<Document>*);
 
     void registerNodeList(LiveNodeListBase*);
     void unregisterNodeList(LiveNodeListBase*);
@@ -1046,6 +1043,9 @@ private:
     void updateDistributionIfNeeded();
 
     void updateUseShadowTrees();
+
+    void unscheduleStyleRecalc();
+    void styleRecalcTimerFired(Timer<Document>*);
 
     void detachParser();
 
