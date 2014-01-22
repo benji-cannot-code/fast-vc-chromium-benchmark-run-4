7f75d62e9d1d1ec73a8a5b0cc29eb7386df2610f
oysteine@chromium.org <oysteine@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-22 23:07:50 +0000

    The HTML parser should ignore incoming bytes when stopped, not just when detached, otherwise we might redundantly re-start the background parser.
    Also added a few asserts to catch any future causes of the linked crash.
    
    BUG=329603
    R=abarth@chromium.org,eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/133173003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165564 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/parser/HTMLDocumentParser.cpp
+++ third_party/WebKit/Source/core/html/parser/HTMLDocumentParser.cpp
@@ -660,6 +660,7 @@ void HTMLDocumentParser::insert(const SegmentedString& source)
 
 void HTMLDocumentParser::startBackgroundParser()
 {
+    ASSERT(!isStopped());
     ASSERT(shouldUseThreading());
     ASSERT(!m_haveBackgroundParser);
     m_haveBackgroundParser = true;
@@ -965,7 +966,7 @@ void HTMLDocumentParser::resumeScheduledTasks()
 
 void HTMLDocumentParser::appendBytes(const char* data, size_t length)
 {
-    if (!length || isDetached())
+    if (!length || isStopped())
         return;
 
     if (shouldUseThreading()) {
@@ -997,6 +998,7 @@ void HTMLDocumentParser::flush()
 
 void HTMLDocumentParser::setDecoder(PassOwnPtr<TextResourceDecoder> decoder)
 {
+    ASSERT(decoder);
     DecodedDataDocumentParser::setDecoder(decoder);
 
     if (m_haveBackgroundParser)
