588fa60d42a00f1d5ca0de50ea41ca40d34b5976
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-22 01:42:22 +0000

    Remove old about scheme URL constants.
    These are no longer needed after chrome frame's deletion.
    Use the equivalent chrome scheme URL constants instead.
    
    BUG=316496
    TEST=NONE
    R=pkasting@chromium.org
    TBR=bauerb@chromium.org
    
    Review URL: https://codereview.chromium.org/137793017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@246199 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/url_constants.h
+++ chrome/common/url_constants.h
@@ -16,10 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chrome {
 
-extern const char kAboutComponentsURL[];
-extern const char kAboutPluginsURL[];
-extern const char kAboutVersionURL[];
-
 // chrome: URLs (including schemes). Should be kept in sync with the
 // components below.
 extern const char kChromeUIAboutURL[];
