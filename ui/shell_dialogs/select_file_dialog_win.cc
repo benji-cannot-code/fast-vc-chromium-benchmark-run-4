3281b1385000b641382a9a7dfc538a781133a7f4
tommi@chromium.org <tommi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-23 18:23:31 +0000

    Fix NULL deref in SelectFileDialogImpl when a NULL filter is provided. According to the documentation, NULL is a valid value but the code wasn't consistently checking for it.
    BUG=337068
    TEST=See repro steps in the bug.
    
    Review URL: https://codereview.chromium.org/139313011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@246647 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/shell_dialogs/select_file_dialog_win.cc
+++ ui/shell_dialogs/select_file_dialog_win.cc
@@ -518,7 +518,8 @@ class SelectFileDialogImpl : public ui::SelectFileDialog,
 
   // Returns the filter to be used while displaying the open/save file dialog.
   // This is computed from the extensions for the file types being opened.
-  base::string16 GetFilterForFileTypes(const FileTypeInfo& file_types);
+  // |file_types| can be NULL in which case the returned filter will be empty.
+  base::string16 GetFilterForFileTypes(const FileTypeInfo* file_types);
 
   bool has_multiple_file_type_choices_;
 
@@ -555,7 +556,7 @@ void SelectFileDialogImpl::SelectFileImpl(
       aura::HandleSaveFile(
           base::UTF16ToWide(title),
           default_path,
-          GetFilterForFileTypes(*file_types),
+          GetFilterForFileTypes(file_types),
           file_type_index,
           default_extension,
           base::Bind(&ui::SelectFileDialog::Listener::FileSelected,
@@ -567,7 +568,7 @@ void SelectFileDialogImpl::SelectFileImpl(
       aura::HandleOpenFile(
           base::UTF16ToWide(title),
           default_path,
-          GetFilterForFileTypes(*file_types),
+          GetFilterForFileTypes(file_types),
           base::Bind(&ui::SelectFileDialog::Listener::FileSelected,
                      base::Unretained(listener_)),
           base::Bind(&ui::SelectFileDialog::Listener::FileSelectionCanceled,
@@ -577,7 +578,7 @@ void SelectFileDialogImpl::SelectFileImpl(
       aura::HandleOpenMultipleFiles(
           base::UTF16ToWide(title),
           default_path,
-          GetFilterForFileTypes(*file_types),
+          GetFilterForFileTypes(file_types),
           base::Bind(&ui::SelectFileDialog::Listener::MultiFilesSelected,
                      base::Unretained(listener_)),
           base::Bind(&ui::SelectFileDialog::Listener::FileSelectionCanceled,
@@ -638,7 +639,7 @@ void SelectFileDialogImpl::ListenerDestroyed() {
 
 void SelectFileDialogImpl::ExecuteSelectFile(
     const ExecuteSelectParams& params) {
-  base::string16 filter = GetFilterForFileTypes(params.file_types);
+  base::string16 filter = GetFilterForFileTypes(&params.file_types);
 
   base::FilePath path = params.default_path;
   bool success = false;
@@ -882,10 +883,13 @@ bool SelectFileDialogImpl::RunOpenMultiFileDialog(
 }
 
 base::string16 SelectFileDialogImpl::GetFilterForFileTypes(
-    const FileTypeInfo& file_types) {
+    const FileTypeInfo* file_types) {
+  if (!file_types)
+    return base::string16();
+
   std::vector<base::string16> exts;
-  for (size_t i = 0; i < file_types.extensions.size(); ++i) {
-    const std::vector<base::string16>& inner_exts = file_types.extensions[i];
+  for (size_t i = 0; i < file_types->extensions.size(); ++i) {
+    const std::vector<base::string16>& inner_exts = file_types->extensions[i];
     base::string16 ext_string;
     for (size_t j = 0; j < inner_exts.size(); ++j) {
       if (!ext_string.empty())
@@ -897,8 +901,8 @@ base::string16 SelectFileDialogImpl::GetFilterForFileTypes(
   }
   return FormatFilterForExtensions(
       exts,
-      file_types.extension_description_overrides,
-      file_types.include_all_files);
+      file_types->extension_description_overrides,
+      file_types->include_all_files);
 }
 
 }  // namespace
