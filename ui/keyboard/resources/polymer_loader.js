09149b2d0418d7f48a6fd85f1d21188bf8c63b9c
rsadam@chromium.org <rsadam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-23 18:29:13 +0000

    Uprev polymer to 0.1.3 (attempt #2)
    BUG=334022
    
    Review URL: https://codereview.chromium.org/145273004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@246651 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/keyboard/resources/polymer_loader.js
+++ ui/keyboard/resources/polymer_loader.js
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // This is necessary because polymer currently doesn't handle the configuration
 // where native Custom Elements are available but native Shadow DOM is not.
 // TODO(bshe): remove this line when polymer supports the configuration.
-document.register = undefined;
+document.registerElement = undefined;
 
 <include src="../../../third_party/polymer/platform/platform.js"></include>
 <include src="../../../third_party/polymer/polymer/polymer.js"></include>
