b91f7cf1230b5f2ec27cc0bf108b7e15be81c7ac
ericwilligers@chromium.org <ericwilligers@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-23 03:01:53 +0000

    Handle missing or invalid Keyframe offsets.
    The keyframes are normalized using the algorithm from
    section 6.16.4 'Normalizing the list of keyframes' of the Web Animations draft specification.
    
    BUG=327564
    
    Review URL: https://codereview.chromium.org/133003004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165599 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/KeyframeEffectModel.h
+++ third_party/WebKit/Source/core/animation/KeyframeEffectModel.h
@@ -45,6 +45,8 @@ namespace WebCore {
 
 typedef HashSet<CSSPropertyID> PropertySet;
 
+class KeyframeEffectModelTest;
+
 // Represents the keyframes set through the API.
 class Keyframe : public RefCounted<Keyframe> {
 public:
@@ -137,7 +139,8 @@ public:
 private:
     KeyframeEffectModel(const KeyframeVector& keyframes);
 
-    KeyframeVector normalizedKeyframes() const;
+    static KeyframeVector normalizedKeyframes(const KeyframeVector& keyframes);
+
     // Lazily computes the groups of property-specific keyframes.
     void ensureKeyframeGroups() const;
 
@@ -147,6 +150,8 @@ private:
     // property-specific lists.
     typedef HashMap<CSSPropertyID, OwnPtr<PropertySpecificKeyframeGroup> > KeyframeGroupMap;
     mutable OwnPtr<KeyframeGroupMap> m_keyframeGroups;
+
+    friend class KeyframeEffectModelTest;
 };
 
 DEFINE_TYPE_CASTS(KeyframeEffectModel, AnimationEffect, value, value->isKeyframeEffectModel(), value.isKeyframeEffectModel());
