2587a73de6c55d279ba689b0c39987666e27947e
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-23 02:08:39 +0000

    [GCM] Change GCMProfileService and GCM API functions to pass by const ref
    BUG=284553
    TEST=existing tests due to no new functionaility
    
    Review URL: https://codereview.chromium.org/139093006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@246485 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/gcm/gcm_api.h
+++ chrome/browser/extensions/api/gcm/gcm_api.h
@@ -53,9 +53,6 @@ class GcmRegisterFunction : public GcmApiFunction {
  private:
   void CompleteFunctionWithResult(const std::string& registration_id,
                                   gcm::GCMClient::Result result);
-
-  std::string cert_;
-  std::vector<std::string> sender_ids_;
 };
 
 class GcmSendFunction : public GcmApiFunction {
@@ -77,9 +74,6 @@ class GcmSendFunction : public GcmApiFunction {
   // Validates that message data do not carry invalid keys and fit into
   // allowable size limits.
   bool ValidateMessageData(const gcm::GCMClient::MessageData& data) const;
-
-  std::string destination_id_;
-  gcm::GCMClient::OutgoingMessage outgoing_message_;
 };
 
 class GcmJsEventRouter : public gcm::GCMEventRouter {
