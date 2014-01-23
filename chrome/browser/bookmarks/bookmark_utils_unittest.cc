benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 9be656687dcea78d6d0c635fe77f0104ad5499bc

chrome.bookmark.search exact URL/title query support
Allow queries for exact URL and title strings within
chrome.bookmark.search by accepting an object for the query parameter,
with the desired query predicates specified on the object.

API proposal document: https://docs.google.com/a/chromium.org/document/d/1XZHmtCBQNqaGUZLJPbSL7TXkP3AMhqyxwJk6ez5pMMs/edit

BUG=311282

Review URL: https://codereview.chromium.org/113073002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241203 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/bookmarks/bookmark_utils.cc
@@ -96,6 +96,9 @@ bool PruneInvisibleFolders(const BookmarkNode* node) {
 
 namespace bookmark_utils {
 
+QueryFields::QueryFields() {}
+QueryFields::~QueryFields() {}
+
 void CloneBookmarkNode(BookmarkModel* model,
                        const std::vector<BookmarkNodeData::Element>& elements,
                        const BookmarkNode* parent,
@@ -217,25 +220,41 @@ bool MoreRecentlyAdded(const BookmarkNode* n1, const BookmarkNode* n2) {
   return n1->date_added() > n2->date_added();
 }
 
-void GetBookmarksContainingText(BookmarkModel* model,
-                                const base::string16& text,
-                                size_t max_count,
-                                const std::string& languages,
-                                std::vector<const BookmarkNode*>* nodes) {
-  std::vector<base::string16> words;
+void GetBookmarksMatchingProperties(BookmarkModel* model,
+                                    const QueryFields& query,
+                                    size_t max_count,
+                                    const std::string& languages,
+                                    std::vector<const BookmarkNode*>* nodes) {
+  std::vector<base::string16> query_words;
   QueryParser parser;
-  parser.ParseQueryWords(base::i18n::ToLower(text), &words);
-  if (words.empty())
-    return;
+  if (query.word_phrase_query) {
+    parser.ParseQueryWords(base::i18n::ToLower(*query.word_phrase_query),
+                           &query_words);
+  }
 
   ui::TreeNodeIterator<const BookmarkNode> iterator(model->root_node());
   while (iterator.has_next()) {
     const BookmarkNode* node = iterator.Next();
-    if (DoesBookmarkContainWords(node, words, languages)) {
-      nodes->push_back(node);
-      if (nodes->size() == max_count)
-        return;
+    if (!query_words.empty() &&
+        !DoesBookmarkContainWords(node, query_words, languages)) {
+      continue;
     }
+    if (query.url) {
+      // Check against bare url spec and IDN-decoded url.
+      if (!node->is_url() ||
+          !(UTF8ToUTF16(node->url().spec()) == *query.url ||
+            net::FormatUrl(
+                node->url(), languages, net::kFormatUrlOmitNothing,
+                net::UnescapeRule::NORMAL, NULL, NULL, NULL) == *query.url)) {
+        continue;
+      }
+    }
+    if (query.title && node->GetTitle() != *query.title)
+      continue;
+
+    nodes->push_back(node);
+    if (nodes->size() == max_count)
+      return;
   }
 }
 

chrome/browser/bookmarks/bookmark_utils_unittest.cc
@@ -31,7 +31,7 @@ class BookmarkUtilsTest : public ::testing::Test {
   base::MessageLoopForUI loop;
 };
 
-TEST_F(BookmarkUtilsTest, GetBookmarksContainingText) {
+TEST_F(BookmarkUtilsTest, GetBookmarksMatchingPropertiesWordPhraseQuery) {
   BookmarkModel model(NULL);
   const BookmarkNode* node1 = model.AddURL(model.other_node(),
                                         0,
@@ -47,24 +47,153 @@ TEST_F(BookmarkUtilsTest, GetBookmarksContainingText) {
                                            ASCIIToUTF16("foo"));
 
   std::vector<const BookmarkNode*> nodes;
-  GetBookmarksContainingText(
-      &model, ASCIIToUTF16("foo"), 100, string(), &nodes);
+  QueryFields query;
+  query.word_phrase_query.reset(new string16);
+  *query.word_phrase_query = ASCIIToUTF16("foo");
+  GetBookmarksMatchingProperties(&model, query, 100, string(), &nodes);
   ASSERT_EQ(2U, nodes.size());
   EXPECT_TRUE(nodes[0] == folder1);
   EXPECT_TRUE(nodes[1] == node1);
   nodes.clear();
 
-  GetBookmarksContainingText(
-      &model, ASCIIToUTF16("cnn"), 100, string(), &nodes);
+  *query.word_phrase_query = ASCIIToUTF16("cnn");
+  GetBookmarksMatchingProperties(&model, query, 100, string(), &nodes);
   ASSERT_EQ(1U, nodes.size());
   EXPECT_TRUE(nodes[0] == node2);
   nodes.clear();
 
-  GetBookmarksContainingText(
-      &model, ASCIIToUTF16("foo bar"), 100, string(), &nodes);
+  *query.word_phrase_query = ASCIIToUTF16("foo bar");
+  GetBookmarksMatchingProperties(&model, query, 100, string(), &nodes);
   ASSERT_EQ(1U, nodes.size());
   EXPECT_TRUE(nodes[0] == node1);
   nodes.clear();
+}
+
+// Check exact matching against a URL query.
+TEST_F(BookmarkUtilsTest, GetBookmarksMatchingPropertiesUrl) {
+  BookmarkModel model(NULL);
+  const BookmarkNode* node1 = model.AddURL(model.other_node(),
+                                        0,
+                                        ASCIIToUTF16("Google"),
+                                        GURL("https://www.google.com/"));
+  model.AddURL(model.other_node(),
+               0,
+               ASCIIToUTF16("Google Calendar"),
+               GURL("https://www.google.com/calendar"));
+
+  model.AddFolder(model.other_node(),
+                  0,
+                  ASCIIToUTF16("Folder"));
+
+  std::vector<const BookmarkNode*> nodes;
+  QueryFields query;
+  query.url.reset(new string16);
+  *query.url = ASCIIToUTF16("https://www.google.com/");
+  GetBookmarksMatchingProperties(&model, query, 100, string(), &nodes);
+  ASSERT_EQ(1U, nodes.size());
+  EXPECT_TRUE(nodes[0] == node1);
+  nodes.clear();
+
+  *query.url = ASCIIToUTF16("calendar");
+  GetBookmarksMatchingProperties(&model, query, 100, string(), &nodes);
+  ASSERT_EQ(0U, nodes.size());
+  nodes.clear();
+
+  // Empty URL should not match folders.
+  *query.url = ASCIIToUTF16("");
+  GetBookmarksMatchingProperties(&model, query, 100, string(), &nodes);
+  ASSERT_EQ(0U, nodes.size());
+  nodes.clear();
+}
+
+// Check exact matching against a title query.
+TEST_F(BookmarkUtilsTest, GetBookmarksMatchingPropertiesTitle) {
+  BookmarkModel model(NULL);
+  const BookmarkNode* node1 = model.AddURL(model.other_node(),
+                                        0,
+                                        ASCIIToUTF16("Google"),
+                                        GURL("https://www.google.com/"));
+  model.AddURL(model.other_node(),
+               0,
+               ASCIIToUTF16("Google Calendar"),
+               GURL("https://www.google.com/calendar"));
+
+  const BookmarkNode* folder1 = model.AddFolder(model.other_node(),
+                                           0,
+                                           ASCIIToUTF16("Folder"));
+
+  std::vector<const BookmarkNode*> nodes;
+  QueryFields query;
+  query.title.reset(new string16);
+  *query.title = ASCIIToUTF16("Google");
+  GetBookmarksMatchingProperties(&model, query, 100, string(), &nodes);
+  ASSERT_EQ(1U, nodes.size());
+  EXPECT_TRUE(nodes[0] == node1);
+  nodes.clear();
+
+  *query.title = ASCIIToUTF16("Calendar");
+  GetBookmarksMatchingProperties(&model, query, 100, string(), &nodes);
+  ASSERT_EQ(0U, nodes.size());
+  nodes.clear();
+
+  // Title should match folders.
+  *query.title = ASCIIToUTF16("Folder");
+  GetBookmarksMatchingProperties(&model, query, 100, string(), &nodes);
+  ASSERT_EQ(1U, nodes.size());
+  EXPECT_TRUE(nodes[0] == folder1);
+  nodes.clear();
+}
+
+// Check matching against a query with multiple predicates.
+TEST_F(BookmarkUtilsTest, GetBookmarksMatchingPropertiesConjunction) {
+  BookmarkModel model(NULL);
+  const BookmarkNode* node1 = model.AddURL(model.other_node(),
+                                        0,
+                                        ASCIIToUTF16("Google"),
+                                        GURL("https://www.google.com/"));
+  model.AddURL(model.other_node(),
+               0,
+               ASCIIToUTF16("Google Calendar"),
+               GURL("https://www.google.com/calendar"));
+
+  model.AddFolder(model.other_node(),
+                  0,
+                  ASCIIToUTF16("Folder"));
+
+  std::vector<const BookmarkNode*> nodes;
+  QueryFields query;
+
+  // Test all fields matching.
+  query.word_phrase_query.reset(new string16(ASCIIToUTF16("www")));
+  query.url.reset(new string16(ASCIIToUTF16("https://www.google.com/")));
+  query.title.reset(new string16(ASCIIToUTF16("Google")));
+  GetBookmarksMatchingProperties(&model, query, 100, string(), &nodes);
+  ASSERT_EQ(1U, nodes.size());
+  EXPECT_TRUE(nodes[0] == node1);
+  nodes.clear();
+
+  scoped_ptr<string16>* fields[] = {
+    &query.word_phrase_query, &query.url, &query.title };
+
+  // Test two fields matching.
+  for (size_t i = 0; i < arraysize(fields); i++) {
+    scoped_ptr<string16> original_value(fields[i]->release());
+    GetBookmarksMatchingProperties(&model, query, 100, string(), &nodes);
+    ASSERT_EQ(1U, nodes.size());
+    EXPECT_TRUE(nodes[0] == node1);
+    nodes.clear();
+    fields[i]->reset(original_value.release());
+  }
+
+  // Test two fields matching with one non-matching field.
+  for (size_t i = 0; i < arraysize(fields); i++) {
+    scoped_ptr<string16> original_value(fields[i]->release());
+    fields[i]->reset(new string16(ASCIIToUTF16("fjdkslafjkldsa")));
+    GetBookmarksMatchingProperties(&model, query, 100, string(), &nodes);
+    ASSERT_EQ(0U, nodes.size());
+    nodes.clear();
+    fields[i]->reset(original_value.release());
+  }
 }
 
 TEST_F(BookmarkUtilsTest, CopyPaste) {

chrome/browser/bookmarks/bookmark_utils.h
@@ -23,6 +23,16 @@ class PrefRegistrySyncable;
 // that show bookmarks: bookmark manager, bookmark bar view ...
 namespace bookmark_utils {
 
+// Fields to use when finding matching bookmarks.
+struct QueryFields {
+  QueryFields();
+  ~QueryFields();
+
+  scoped_ptr<base::string16> word_phrase_query;
+  scoped_ptr<base::string16> url;
+  scoped_ptr<base::string16> title;
+};
+
 // Clones bookmark node, adding newly created nodes to |parent| starting at
 // |index_to_add_at|. If |reset_node_times| is true cloned bookmarks and
 // folders will receive new creation times and folder modification times
@@ -64,14 +74,15 @@ void GetMostRecentlyAddedEntries(BookmarkModel* model,
 // Returns true if |n1| was added more recently than |n2|.
 bool MoreRecentlyAdded(const BookmarkNode* n1, const BookmarkNode* n2);
 
-// Returns up to |max_count| bookmarks from |model| whose url or title contains
-// the text |text|.  |languages| is user's accept-language setting to decode
-// IDN.
-void GetBookmarksContainingText(BookmarkModel* model,
-                                const base::string16& text,
-                                size_t max_count,
-                                const std::string& languages,
-                                std::vector<const BookmarkNode*>* nodes);
+// Returns up to |max_count| bookmarks from |model| whose url or title contain
+// the text |query.word_phrase_query| and exactly match |query.url| and
+// |query.title|, for all of the preceding fields that are not NULL.
+// |languages| is user's accept-language setting to decode IDN.
+void GetBookmarksMatchingProperties(BookmarkModel* model,
+                                    const QueryFields& query,
+                                    size_t max_count,
+                                    const std::string& languages,
+                                    std::vector<const BookmarkNode*>* nodes);
 
 // Register user preferences for Bookmarks Bar.
 void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry);

chrome/browser/extensions/api/bookmarks/bookmarks_api.cc
@@ -439,12 +439,34 @@ bool BookmarksSearchFunction::RunImpl() {
   PrefService* prefs = user_prefs::UserPrefs::Get(GetProfile());
   std::string lang = prefs->GetString(prefs::kAcceptLanguages);
   std::vector<const BookmarkNode*> nodes;
-  bookmark_utils::GetBookmarksContainingText(
-      BookmarkModelFactory::GetForProfile(GetProfile()),
-      UTF8ToUTF16(params->query),
-      std::numeric_limits<int>::max(),
-      lang,
-      &nodes);
+  if (params->query.as_string) {
+    bookmark_utils::QueryFields query;
+    query.word_phrase_query.reset(
+        new string16(UTF8ToUTF16(*params->query.as_string)));
+    bookmark_utils::GetBookmarksMatchingProperties(
+        BookmarkModelFactory::GetForProfile(GetProfile()),
+        query,
+        std::numeric_limits<int>::max(),
+        lang,
+        &nodes);
+  } else {
+    DCHECK(params->query.as_object);
+    const bookmarks::Search::Params::Query::Object& object =
+        *params->query.as_object;
+    bookmark_utils::QueryFields query;
+    if (object.query)
+      query.word_phrase_query.reset(new string16(UTF8ToUTF16(*object.query)));
+    if (object.url)
+      query.url.reset(new string16(UTF8ToUTF16(*object.url)));
+    if (object.title)
+      query.title.reset(new string16(UTF8ToUTF16(*object.title)));
+    bookmark_utils::GetBookmarksMatchingProperties(
+        BookmarkModelFactory::GetForProfile(GetProfile()),
+        query,
+        std::numeric_limits<int>::max(),
+        lang,
+        &nodes);
+  }
 
   std::vector<linked_ptr<BookmarkTreeNode> > tree_nodes;
   for (std::vector<const BookmarkNode*>::iterator node_iter = nodes.begin();

commit 28f57148cce335b99c08ac1b28de694a012e40b3

Don't return bookmark results if an empty or space-only query is specified
Fixes a regression introduced with r241203.

BUG=335473

Review URL: https://codereview.chromium.org/141583009

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@246716 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/bookmarks/bookmark_utils.cc
@@ -264,6 +264,8 @@ void GetBookmarksMatchingProperties(BookmarkModel* model,
   if (query.word_phrase_query) {
     parser.ParseQueryWords(base::i18n::ToLower(*query.word_phrase_query),
                            &query_words);
+    if (query_words.empty())
+      return;
   }
 
   ui::TreeNodeIterator<const BookmarkNode> iterator(model->root_node());

chrome/browser/bookmarks/bookmark_utils_unittest.cc
@@ -48,6 +48,17 @@ TEST_F(BookmarkUtilsTest, GetBookmarksMatchingPropertiesWordPhraseQuery) {
   std::vector<const BookmarkNode*> nodes;
   QueryFields query;
   query.word_phrase_query.reset(new base::string16);
+  // No nodes are returned for empty string.
+  *query.word_phrase_query = ASCIIToUTF16("");
+  GetBookmarksMatchingProperties(&model, query, 100, string(), &nodes);
+  EXPECT_TRUE(nodes.empty());
+  nodes.clear();
+
+  // No nodes are returned for space-only string.
+  *query.word_phrase_query = ASCIIToUTF16("   ");
+  GetBookmarksMatchingProperties(&model, query, 100, string(), &nodes);
+  EXPECT_TRUE(nodes.empty());
+  nodes.clear();
 
   // Node "foo bar" and folder "foo" are returned in search results.
   *query.word_phrase_query = ASCIIToUTF16("foo");