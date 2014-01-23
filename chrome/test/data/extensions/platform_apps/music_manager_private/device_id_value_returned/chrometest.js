f8be6c0135f484d95b49ce804f8acd0b522de353
rpaquay@chromium.org <rpaquay@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-23 10:23:08 +0000

    Update MacOS implementation DeviceId::GetDeviceId.
    Append the MAC address (as string) to the existing DeviceId string
    to improve id "uniqueness".
    
    BUG=334348
    
    Review URL: https://codereview.chromium.org/141383002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@246562 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/platform_apps/music_manager_private/device_id_value_returned/chrometest.js
+++ chrome/test/data/extensions/platform_apps/music_manager_private/device_id_value_returned/chrometest.js
@@ -3,13 +3,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+var deviceIdMaybeUndefined;
+
 function runTests() {
   chrome.test.runTests([
     function test() {
       chrome.musicManagerPrivate.getDeviceId(function(id) {
-          console.log("Device ID=" + id);
-          chrome.test.assertEq("string", typeof id);
-          chrome.test.assertTrue(id.length >= 8);
+          console.log('Device ID=' + id);
+          if (!id) {
+            chrome.test.assertEq(true, deviceIdMaybeUndefined)
+          } else {
+            chrome.test.assertEq('string', typeof id);
+            chrome.test.assertTrue(id.length >= 8);
+          }
           chrome.test.succeed();
       });
     }
@@ -18,6 +24,9 @@ function runTests() {
 
 window.onload = function() {
   chrome.test.getConfig(function(config) {
+    console.log('customArg=' + config.customArg);
+    deviceIdMaybeUndefined =
+        (config.customArg === 'device_id_may_be_undefined');
     runTests();
   });
 }
