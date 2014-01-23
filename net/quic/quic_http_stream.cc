dbb2ac5dda58514473c7c64db91179bee2d72dc6
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-23 22:59:15 +0000

    Implement QuicHttpStream::GetTotalReceivedBytes.
    Review URL: https://codereview.chromium.org/135013003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@246710 0039d316-1c4b-4281-b951-d872f2087c98

--- net/quic/quic_http_stream.cc
+++ net/quic/quic_http_stream.cc
@@ -38,6 +38,7 @@ QuicHttpStream::QuicHttpStream(const base::WeakPtr<QuicClientSession>& session)
       response_status_(OK),
       response_headers_received_(false),
       read_buf_(new GrowableIOBuffer()),
+      closed_stream_received_bytes_(0),
       user_buffer_len_(0),
       weak_factory_(this) {
   DCHECK(session_);
@@ -207,6 +208,7 @@ int QuicHttpStream::ReadResponseBody(
 void QuicHttpStream::Close(bool not_reusable) {
   // Note: the not_reusable flag has no meaning for SPDY streams.
   if (stream_) {
+    closed_stream_received_bytes_ = stream_->stream_bytes_read();
     stream_->SetDelegate(NULL);
     stream_->Reset(QUIC_STREAM_CANCELLED);
     stream_ = NULL;
@@ -240,8 +242,11 @@ bool QuicHttpStream::IsConnectionReusable() const {
 }
 
 int64 QuicHttpStream::GetTotalReceivedBytes() const {
-  // TODO(eustas): Implement.
-  return 0;
+  if (stream_) {
+    return stream_->stream_bytes_read();
+  }
+
+  return closed_stream_received_bytes_;
 }
 
 bool QuicHttpStream::GetLoadTimingInfo(LoadTimingInfo* load_timing_info) const {
@@ -321,6 +326,7 @@ void QuicHttpStream::OnClose(QuicErrorCode error) {
     response_status_ = ERR_ABORTED;
   }
 
+  closed_stream_received_bytes_ = stream_->stream_bytes_read();
   stream_ = NULL;
   if (!callback_.is_null())
     DoCallback(response_status_);
