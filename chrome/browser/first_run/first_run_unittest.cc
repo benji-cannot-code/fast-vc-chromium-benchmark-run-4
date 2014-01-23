ea76328b5040108d1d7d044a107ca7da1c59f888
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-23 22:20:39 +0000

    Add plumbing for persisting a signature for the variations seed.
    In a later CL, I will add logic to actually use this signature
    for validating the integrity of the seed.
    
    BUG=336536
    
    Review URL: https://codereview.chromium.org/143803008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@246702 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/first_run/first_run_unittest.cc
+++ chrome/browser/first_run/first_run_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_util.h"
 #include "base/path_service.h"
 #include "base/test/scoped_path_override.h"
+#include "base/values.h"
 #include "chrome/browser/first_run/first_run.h"
 #include "chrome/browser/first_run/first_run_internal.h"
 #include "chrome/common/chrome_paths.h"
@@ -43,18 +44,36 @@ TEST_F(FirstRunTest, RemoveSentinel) {
 
 TEST_F(FirstRunTest, SetupMasterPrefsFromInstallPrefs_VariationsSeed) {
   installer::MasterPreferences install_prefs("{ \"variations_seed\":\"xyz\" }");
+  EXPECT_EQ(1U, install_prefs.master_dictionary().size());
 
   MasterPrefs out_prefs;
   internal::SetupMasterPrefsFromInstallPrefs(install_prefs, &out_prefs);
   EXPECT_EQ("xyz", out_prefs.variations_seed);
+  // Variations prefs should have been extracted (removed) from the dictionary.
+  EXPECT_TRUE(install_prefs.master_dictionary().empty());
 }
 
 TEST_F(FirstRunTest, SetupMasterPrefsFromInstallPrefs_NoVariationsSeed) {
   installer::MasterPreferences install_prefs("{ }");
+  EXPECT_TRUE(install_prefs.master_dictionary().empty());
 
   MasterPrefs out_prefs;
   internal::SetupMasterPrefsFromInstallPrefs(install_prefs, &out_prefs);
   EXPECT_TRUE(out_prefs.variations_seed.empty());
+  EXPECT_TRUE(out_prefs.variations_seed_signature.empty());
+}
+
+TEST_F(FirstRunTest, SetupMasterPrefsFromInstallPrefs_VariationsSeedSignature) {
+  installer::MasterPreferences install_prefs(
+      "{ \"variations_seed\":\"xyz\", \"variations_seed_signature\":\"abc\" }");
+  EXPECT_EQ(2U, install_prefs.master_dictionary().size());
+
+  MasterPrefs out_prefs;
+  internal::SetupMasterPrefsFromInstallPrefs(install_prefs, &out_prefs);
+  EXPECT_EQ("xyz", out_prefs.variations_seed);
+  EXPECT_EQ("abc", out_prefs.variations_seed_signature);
+  // Variations prefs should have been extracted (removed) from the dictionary.
+  EXPECT_TRUE(install_prefs.master_dictionary().empty());
 }
 
 }  // namespace first_run
--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -446,6 +446,7 @@ extern const char kVariationsRestrictParameter[];
 extern const char kVariationsSeed[];
 extern const char kVariationsSeedDate[];
 extern const char kVariationsSeedHash[];
+extern const char kVariationsSeedSignature[];
 
 extern const char kDeviceOpenNetworkConfiguration[];
 
