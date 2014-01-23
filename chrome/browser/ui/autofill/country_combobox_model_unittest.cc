c5b852f9635dcf1182ec40a89588f304b03a56f8
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-23 04:23:40 +0000

    rAc: hide countries with no address components.
    R=rouslan@chromium.org
    BUG=336882
    TEST=unit_tests, manual: invoke rAc and see no Iran
    
    Review URL: https://codereview.chromium.org/132233046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@246522 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/autofill/country_combobox_model_unittest.cc
+++ chrome/browser/ui/autofill/country_combobox_model_unittest.cc
@@ -5,8 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/autofill/country_combobox_model.h"
 
+#include "components/autofill/core/browser/autofill_country.h"
 #include "components/autofill/core/browser/test_personal_data_manager.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/libaddressinput/chromium/cpp/include/libaddressinput/address_ui.h"
+#include "third_party/libaddressinput/chromium/cpp/include/libaddressinput/address_ui_component.h"
 
 namespace autofill {
 
@@ -20,6 +23,21 @@ TEST(CountryComboboxModel, RespectsManagerDefaultCountry) {
 
   CountryComboboxModel model(manager);
   EXPECT_EQ(kTestCountry, model.GetDefaultCountryCode());
+}
+
+TEST(CountryComboboxModel, AllCountriesHaveComponents) {
+  TestPersonalDataManager manager;
+  CountryComboboxModel model(manager);
+
+  for (int i = 0; i < model.GetItemCount(); ++i) {
+    if (model.IsItemSeparatorAt(i))
+      continue;
+
+    std::string country_code = model.countries()[i]->country_code();
+    std::vector< ::i18n::addressinput::AddressUiComponent> components =
+        ::i18n::addressinput::BuildComponents(country_code);
+    EXPECT_FALSE(components.empty());
+  }
 }
 
 }  // namespace autofill
--- chrome/chrome_tests_unit.gypi
+++ chrome/chrome_tests_unit.gypi
@@ -2033,6 +2033,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             '../media/media.gyp:media_test_support',
             '../ppapi/ppapi_internal.gyp:ppapi_unittest_shared',
             '../third_party/leveldatabase/leveldatabase.gyp:leveldatabase',
+            '../third_party/libaddressinput/libaddressinput.gyp:libaddressinput',
             '../third_party/libjingle/libjingle.gyp:libjingle',
             '../third_party/libphonenumber/libphonenumber.gyp:libphonenumber',
             '../tools/json_schema_compiler/test/json_schema_compiler_tests.gyp:json_schema_compiler_tests',
@@ -2527,6 +2528,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           ],
         }],
         ['OS=="android"', {
+          '!dependencies': [
+            '../third_party/libaddressinput/libaddressinput.gyp:libaddressinput',
+          ],
           'sources!': [
             # Bookmark export/import are handled via the BookmarkColumns
             # ContentProvider.
