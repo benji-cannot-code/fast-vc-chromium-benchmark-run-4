b26f07c9c5644796b0e4cc1ee29f0a9bae73e9aa
aedla@chromium.org <aedla@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-23 23:54:02 +0000

    IPC fuzzer: move from /dev/urandom to a PRNG.
    Functions in base/rand_util.h read from /dev/urandom. Each function call does a system call.
    Mutating an ipcdump with 3000 messages does around 1000000 reads from /dev/urandom, which takes about a second on my machine. Moving to a PRNG instead.
    
    APP_PATH=... time tools/ipc_fuzzer/mutate/ipc_fuzzer_mut.py --input_dir=... --output_dir=... --no_of_files=10
    
    Previously:
    real    0m13.262s
    user    0m2.550s
    sys 0m10.000s
    
    Now, 79% faster:
    real    0m2.763s
    user    0m1.590s
    sys 0m0.630s
    
    BUG=260848
    
    Review URL: https://codereview.chromium.org/145133011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@246734 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/ipc_fuzzer/mutate/mutate.cc
+++ tools/ipc_fuzzer/mutate/mutate.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/pickle.h"
-#include "base/rand_util.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 #include "base/strings/string_number_conversions.h"
@@ -27,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "tools/ipc_fuzzer/message_lib/all_messages.h"
 #include "tools/ipc_fuzzer/message_lib/message_cracker.h"
 #include "tools/ipc_fuzzer/message_lib/message_file.h"
+#include "tools/ipc_fuzzer/mutate/rand_util.h"
 
 #if defined(OS_POSIX)
 #include <unistd.h>
@@ -63,12 +63,12 @@ class Fuzzer {
 
 template <typename T>
 void FuzzIntegralType(T* value, unsigned int frequency) {
-  if (base::RandInt(0, frequency) == 0) {
-    switch (base::RandInt(0, 3)) {
+  if (RandEvent(frequency)) {
+    switch (RandInRange(4)) {
       case 0: (*value) = 0; break;
       case 1: (*value)--; break;
       case 2: (*value)++; break;
-      case 3: (*value) = base::RandUint64(); break;
+      case 3: (*value) = RandU64(); break;
     }
   }
 }
@@ -76,8 +76,8 @@ void FuzzIntegralType(T* value, unsigned int frequency) {
 template <typename T>
 void FuzzStringType(T* value, unsigned int frequency,
                     const T& literal1, const T& literal2) {
-  if (base::RandInt(0, frequency) == 0) {
-    switch (base::RandInt(0, 4)) {
+  if (RandEvent(frequency)) {
+    switch (RandInRange(5)) {
       case 4: (*value) = (*value) + (*value);   // FALLTHROUGH
       case 3: (*value) = (*value) + (*value);   // FALLTHROUGH
       case 2: (*value) = (*value) + (*value); break;
@@ -96,7 +96,7 @@ class DefaultFuzzer : public Fuzzer {
   virtual ~DefaultFuzzer() {}
 
   virtual void FuzzBool(bool* value) OVERRIDE {
-    if (base::RandInt(0, frequency_) == 0)
+    if (RandEvent(frequency_))
       (*value) = !(*value);
   }
 
@@ -133,13 +133,13 @@ class DefaultFuzzer : public Fuzzer {
   }
 
   virtual void FuzzFloat(float* value) OVERRIDE {
-    if (base::RandInt(0, frequency_) == 0)
-      *value = base::RandDouble();
+    if (RandEvent(frequency_))
+      *value = RandDouble();
   }
 
   virtual void FuzzDouble(double* value) OVERRIDE {
-    if (base::RandInt(0, frequency_) == 0)
-      *value = base::RandDouble();
+    if (RandEvent(frequency_))
+      *value = RandDouble();
   }
 
   virtual void FuzzString(std::string* value) OVERRIDE {
@@ -153,7 +153,7 @@ class DefaultFuzzer : public Fuzzer {
   }
 
   virtual void FuzzData(char* data, int length) OVERRIDE {
-    if (base::RandInt(0, frequency_) == 0) {
+    if (RandEvent(frequency_)) {
       for (int i = 0; i < length; ++i) {
         FuzzIntegralType<char>(&data[i], frequency_);
       }
@@ -686,6 +686,8 @@ int MutateMain(int argc, char** argv) {
     type_set.insert(atoi(type_string_vector[i].c_str()));
   }
 
+  InitRand();
+
   Fuzzer* fuzzer = FuzzerFactory::Create(fuzzer_name, frequency);
   if (!fuzzer)
     return EXIT_FAILURE;
@@ -694,10 +696,8 @@ int MutateMain(int argc, char** argv) {
   PopulateFuzzFunctionMap(&fuzz_function_map);
 
   MessageVector message_vector;
-  if (!MessageFile::Read(
-          base::FilePath(input_file_name), &message_vector)) {
+  if (!MessageFile::Read(base::FilePath(input_file_name), &message_vector))
     return EXIT_FAILURE;
-  }
 
   for (size_t i = 0; i < message_vector.size(); ++i) {
     IPC::Message* msg = message_vector[i];
@@ -712,8 +712,10 @@ int MutateMain(int argc, char** argv) {
     }
   }
 
-  if (permute)
-    std::random_shuffle(message_vector.begin(), message_vector.end());
+  if (permute) {
+    std::random_shuffle(message_vector.begin(), message_vector.end(),
+                        RandInRange);
+  }
 
   if (!MessageFile::Write(base::FilePath(output_file_name), message_vector))
     return EXIT_FAILURE;
--- tools/ipc_fuzzer/mutate/mutate.gyp
+++ tools/ipc_fuzzer/mutate/mutate.gyp
@@ -17,10 +17,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         '../../../ipc/ipc.gyp:ipc',
         '../../../ppapi/ppapi_internal.gyp:ppapi_ipc',
         '../../../skia/skia.gyp:skia',
+        '../../../third_party/mt19937ar/mt19937ar.gyp:mt19937ar',
         '../message_lib/message_lib.gyp:ipc_message_lib',
       ],
       'sources': [
         'mutate.cc',
+        'rand_util.h',
+        'rand_util.cc',
       ],
       'include_dirs': [
         '../../..',
@@ -35,10 +38,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         '../../../ipc/ipc.gyp:ipc',
         '../../../ppapi/ppapi_internal.gyp:ppapi_ipc',
         '../../../skia/skia.gyp:skia',
+        '../../../third_party/mt19937ar/mt19937ar.gyp:mt19937ar',
         '../message_lib/message_lib.gyp:ipc_message_lib',
       ],
       'sources': [
         'generate.cc',
+        'rand_util.h',
+        'rand_util.cc',
       ],
       'include_dirs': [
         '../../..',
