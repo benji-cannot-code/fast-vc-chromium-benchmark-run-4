de953c54bbd7ea9da451b7a67b9b050997d31ac4
reveman@chromium.org <reveman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-23 16:05:39 +0000

    cc: Clang-format cc/resources/task_graph_runner.* code.
    Style-change only.
    
    BUG=
    TBR=vmpstr
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/143413010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@246603 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/resources/task_graph_runner.h
+++ cc/resources/task_graph_runner.h
@@ -48,7 +48,8 @@ class CC_EXPORT Task : public base::RefCountedThreadSafe<Task> {
 
 #if defined(COMPILER_GCC)
 namespace BASE_HASH_NAMESPACE {
-template <> struct hash<cc::internal::Task*> {
+template <>
+struct hash<cc::internal::Task*> {
   size_t operator()(cc::internal::Task* ptr) const {
     return hash<size_t>()(reinterpret_cast<size_t>(ptr));
   }
@@ -104,9 +105,7 @@ class CC_EXPORT NamespaceToken {
   NamespaceToken() : id_(0) {}
   ~NamespaceToken() {}
 
-  bool IsValid() const {
-    return id_ != 0;
-  }
+  bool IsValid() const { return id_ != 0; }
 
  private:
   friend class TaskGraphRunner;
@@ -141,8 +140,8 @@ class CC_EXPORT TaskGraphRunner : public base::DelegateSimpleThread::Delegate {
   void WaitForTasksToFinishRunning(NamespaceToken token);
 
   // Collect all completed tasks in |completed_tasks|.
-  void CollectCompletedTasks(
-      NamespaceToken token, Task::Vector* completed_tasks);
+  void CollectCompletedTasks(NamespaceToken token,
+                             Task::Vector* completed_tasks);
 
  private:
   struct TaskNamespace {
