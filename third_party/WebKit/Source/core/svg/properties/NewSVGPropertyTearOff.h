c4fd4caa7f74dd1bb824f9dc9aa0e1068daa7658
kouhei@chromium.org <kouhei@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-23 06:54:48 +0000

    [SVG] SVG*List.{initialize,reset} should detach its values.
    SVGProperty value can be a member item of a list. When a list item value is appended to other list, the list item is removed from the original list, and then appended to the other list.
    
    When an item was attached from previously cleared list, the item still contained old |ownerList| information which is invalid. This CL fixes the issue by clearing |ownerList| information from its values when list is cleared.
    
    This CL moves |ownerList| from values' TearOffs to values itself to enable access from SVG*List impl.
    
    This CL also encapsulates access to NewSVGPropertyListHelper<T>::m_values. After this change, the helper derived classes should also use public interface to access m_values. This is to ensure |ownerList| attach/detach operation is done correctly.
    
    TBR=pdr@chromium.org
    TEST=svg/dom/SVGLengthList-appendItemFromClearedList.html
    BUG=308818
    
    Review URL: https://codereview.chromium.org/144853002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165617 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/properties/NewSVGPropertyTearOff.h
+++ third_party/WebKit/Source/core/svg/properties/NewSVGPropertyTearOff.h
@@ -57,16 +57,6 @@ public:
         return m_propertyIsAnimVal == PropertyIsAnimVal;
     }
 
-    NewSVGPropertyTearOffBase* ownerList() const
-    {
-        return m_ownerList;
-    }
-
-    void setOwnerList(NewSVGPropertyTearOffBase* ownerList)
-    {
-        m_ownerList = ownerList;
-    }
-
     bool isReadOnlyProperty() const
     {
         return m_isReadOnlyProperty;
@@ -82,7 +72,7 @@ public:
         return isReadOnlyProperty() || isAnimVal();
     }
 
-    void commitChange();
+    virtual void commitChange();
 
     SVGElement* contextElement()
     {
@@ -99,7 +89,6 @@ public:
 protected:
     NewSVGPropertyTearOffBase(SVGElement* contextElement, PropertyIsAnimValType propertyIsAnimVal, const QualifiedName& attributeName = nullQName())
         : m_contextElement(contextElement)
-        , m_ownerList(0)
         , m_propertyIsAnimVal(propertyIsAnimVal)
         , m_isReadOnlyProperty(false)
         , m_attributeName(attributeName)
@@ -109,7 +98,6 @@ protected:
 private:
     // These references are kept alive from V8 wrapper to prevent reference cycles
     SVGElement* m_contextElement;
-    NewSVGPropertyTearOffBase* m_ownerList;
 
     PropertyIsAnimValType m_propertyIsAnimVal;
     bool m_isReadOnlyProperty;
