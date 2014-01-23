07e6f132e1be127af0f97373af8cc8e492580103
mallinath@chromium.org <mallinath@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-23 23:58:30 +0000

    Implement SetOption and GetOption methods defined in talk_base::AsyncPacketSocket.
    These two methods allow client to set socket options like DSCP, Send and Receive socket
    buffer sizes.
    
    R=sergeyu@chromium.org
    TBR=kenrb@chromium.org,joi@chromium.org
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=246612
    
    Review URL: https://codereview.chromium.org/127413002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@246735 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/p2p/ipc_socket_factory.cc
+++ content/renderer/p2p/ipc_socket_factory.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/renderer/p2p/ipc_socket_factory.h"
 
+#include <algorithm>
 #include <deque>
 
 #include "base/compiler_specific.h"
@@ -22,6 +23,8 @@ namespace content {
 
 namespace {
 
+const int kDefaultNonSetOptionValue = -1;
+
 bool IsTcpClientSocket(P2PSocketType type) {
   return (type == P2P_SOCKET_STUN_TCP_CLIENT) ||
          (type == P2P_SOCKET_TCP_CLIENT) ||
@@ -29,6 +32,29 @@ bool IsTcpClientSocket(P2PSocketType type) {
          (type == P2P_SOCKET_SSLTCP_CLIENT) ||
          (type == P2P_SOCKET_TLS_CLIENT) ||
          (type == P2P_SOCKET_STUN_TLS_CLIENT);
+}
+
+bool JingleSocketOptionToP2PSocketOption(talk_base::Socket::Option option,
+                                         P2PSocketOption* ipc_option) {
+  switch (option) {
+    case talk_base::Socket::OPT_RCVBUF:
+      *ipc_option = P2P_SOCKET_OPT_RCVBUF;
+      break;
+    case talk_base::Socket::OPT_SNDBUF:
+      *ipc_option = P2P_SOCKET_OPT_SNDBUF;
+      break;
+    case talk_base::Socket::OPT_DSCP:
+      *ipc_option = P2P_SOCKET_OPT_DSCP;
+      break;
+    case talk_base::Socket::OPT_DONTFRAGMENT:
+    case talk_base::Socket::OPT_NODELAY:
+    case talk_base::Socket::OPT_IPV6_V6ONLY:
+      return false;  // Not supported by the chrome sockets.
+    default:
+      NOTREACHED();
+      return false;
+  }
+  return true;
 }
 
 // TODO(miu): This needs tuning.  http://crbug.com/237960
@@ -58,8 +84,8 @@ class IpcPacketSocket : public talk_base::AsyncPacketSocket,
                      talk_base::DiffServCodePoint dscp) OVERRIDE;
   virtual int Close() OVERRIDE;
   virtual State GetState() const OVERRIDE;
-  virtual int GetOption(talk_base::Socket::Option opt, int* value) OVERRIDE;
-  virtual int SetOption(talk_base::Socket::Option opt, int value) OVERRIDE;
+  virtual int GetOption(talk_base::Socket::Option option, int* value) OVERRIDE;
+  virtual int SetOption(talk_base::Socket::Option option, int value) OVERRIDE;
   virtual int GetError() const OVERRIDE;
   virtual void SetError(int error) OVERRIDE;
 
@@ -91,6 +117,9 @@ class IpcPacketSocket : public talk_base::AsyncPacketSocket,
   void InitAcceptedTcp(P2PSocketClient* client,
                        const talk_base::SocketAddress& local_address,
                        const talk_base::SocketAddress& remote_address);
+
+  int DoSetOption(P2PSocketOption option, int value);
+
   P2PSocketType type_;
 
   // Message loop on which this socket was created and being used.
@@ -125,6 +154,7 @@ class IpcPacketSocket : public talk_base::AsyncPacketSocket,
 
   // Current error code. Valid when state_ == IS_ERROR.
   int error_;
+  int options_[P2P_SOCKET_OPT_MAX];
 
   DISALLOW_COPY_AND_ASSIGN(IpcPacketSocket);
 };
@@ -137,6 +167,8 @@ IpcPacketSocket::IpcPacketSocket()
       writable_signal_expected_(false),
       error_(0) {
   COMPILE_ASSERT(kMaximumInFlightBytes > 0, would_send_at_zero_rate);
+  std::fill_n(options_, static_cast<int> (P2P_SOCKET_OPT_MAX),
+              kDefaultNonSetOptionValue);
 }
 
 IpcPacketSocket::~IpcPacketSocket() {
@@ -307,14 +339,41 @@ talk_base::AsyncPacketSocket::State IpcPacketSocket::GetState() const {
   return STATE_CLOSED;
 }
 
-int IpcPacketSocket::GetOption(talk_base::Socket::Option opt, int* value) {
-  // We don't support socket options for IPC sockets.
-  return -1;
+int IpcPacketSocket::GetOption(talk_base::Socket::Option option, int* value) {
+  P2PSocketOption p2p_socket_option = P2P_SOCKET_OPT_MAX;
+  if (!JingleSocketOptionToP2PSocketOption(option, &p2p_socket_option)) {
+    // unsupported option.
+    return -1;
+  }
+
+  *value = options_[p2p_socket_option];
+  return 0;
 }
 
-int IpcPacketSocket::SetOption(talk_base::Socket::Option opt, int value) {
-  // We don't support socket options for IPC sockets.
-  return -1;
+int IpcPacketSocket::SetOption(talk_base::Socket::Option option, int value) {
+  DCHECK_EQ(base::MessageLoop::current(), message_loop_);
+
+  P2PSocketOption p2p_socket_option = P2P_SOCKET_OPT_MAX;
+  if (!JingleSocketOptionToP2PSocketOption(option, &p2p_socket_option)) {
+    // Option is not supported.
+    return -1;
+  }
+
+  options_[p2p_socket_option] = value;
+
+  if (state_ == IS_OPEN) {
+    // Options will be applied when state becomes IS_OPEN in OnOpen.
+    return DoSetOption(p2p_socket_option, value);
+  }
+  return 0;
+}
+
+int IpcPacketSocket::DoSetOption(P2PSocketOption option, int value) {
+  DCHECK_EQ(base::MessageLoop::current(), message_loop_);
+  DCHECK_EQ(state_, IS_OPEN);
+
+  client_->SetOption(option, value);
+  return 0;
 }
 
 int IpcPacketSocket::GetError() const {
@@ -339,6 +398,12 @@ void IpcPacketSocket::OnOpen(const net::IPEndPoint& address) {
 
   state_ = IS_OPEN;
   TraceSendThrottlingState();
+
+  // Set all pending options if any.
+  for (int i = 0; i < P2P_SOCKET_OPT_MAX; ++i) {
+    if (options_[i] != kDefaultNonSetOptionValue)
+      DoSetOption(static_cast<P2PSocketOption> (i), options_[i]);
+  }
 
   SignalAddressReady(this, local_address_);
   if (IsTcpClientSocket(type_))
