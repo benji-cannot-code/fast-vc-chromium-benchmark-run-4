6f69017e90f2faae1b9158d858e2006dbb4c7f85
kinuko@chromium.org <kinuko@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-23 00:46:07 +0000

    Forgotten follow-up cleanups in WebFileSystem.h
    Was meant to be removed soon, but has been forgotten
    
    BUG=257349
    
    Review URL: https://codereview.chromium.org/144633004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165579 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebFileSystem.h
+++ third_party/WebKit/public/platform/WebFileSystem.h
@@ -39,9 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-// FIXME: Remove this after the switch is over.
-#define NON_SELFDESTRUCT_WEBFILESYSTEMCALLBACKS
-
 class WebFileWriter;
 class WebFileWriterClient;
 
@@ -141,12 +138,6 @@ public:
     // WebFileSystemCallbacks::didReadDirectory() must be called when the operation is completed successfully.
     // WebFileSystemCallbacks::didFail() must be called otherwise.
     virtual void readDirectory(const WebURL& path, WebFileSystemCallbacks) { BLINK_ASSERT_NOT_REACHED(); }
-
-    // Creates a WebFileWriter that can be used to write to the given file.
-    // This is a fast, synchronous call, and should not stat the filesystem.
-    // FIXME: deprecate this after async version of createFileWriter is
-    // implemented.
-    virtual WebFileWriter* createFileWriter(const WebURL& path, WebFileWriterClient*) { BLINK_ASSERT_NOT_REACHED(); return 0; }
 
     // Creates a WebFileWriter that can be used to write to the given file.
     // WebFileSystemCallbacks::didCreateFileWriter() must be called with the created WebFileWriter when the operation is completed successfully.
