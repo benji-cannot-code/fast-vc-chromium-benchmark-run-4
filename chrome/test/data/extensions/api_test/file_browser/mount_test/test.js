acad7aebc09e676d74220a07ef48310690e11170
kinaba@chromium.org <kinaba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-23 03:04:01 +0000

    Files.app: Treat zip archives correctly as readonly.
    The mounted zip archives volume info does not come as the "Disk"
    structure that represents a physical device-ish stuff.
    We should specially treat the archive case by inspecting the
    mount type, as we were doing in JS side before the regression in M33.
    
    BUG=336355
    
    Review URL: https://codereview.chromium.org/132393006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@246499 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/api_test/file_browser/mount_test/test.js
+++ chrome/test/data/extensions/api_test/file_browser/mount_test/test.js
@@ -65,7 +65,7 @@ var expectedArchiveVolume = {
   mountPath: '/archive/archive_mount_path',
   sourcePath: 'archive_path',
   volumeType: 'archive',
-  isReadOnly: false,
+  isReadOnly: true,
   profile: {displayName: "", isCurrentProfile: true}
 };
 
