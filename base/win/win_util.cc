65eaabb6c7deb38d7b9f383cb1d6b7a93688e4f0
pastarmovj@chromium.org <pastarmovj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-23 15:40:20 +0000

    Add Windows utillity function to verify if a computer is part of a domain.
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/140553005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@246600 0039d316-1c4b-4281-b951-d872f2087c98

--- base/win/win_util.cc
+++ base/win/win_util.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/win_util.h"
 
 #include <aclapi.h>
+#include <lm.h>
 #include <shellapi.h>
 #include <shlobj.h>
 #include <shobjidl.h>  // Must be before propkey.
@@ -354,6 +355,16 @@ BOOL GetMonitorInfoWrapper(HMONITOR monitor, MONITORINFO* mi) {
   }
 #endif
   return ret;
+}
+
+bool IsEnrolledToDomain() {
+  LPWSTR domain;
+  NETSETUP_JOIN_STATUS join_status;
+  if(::NetGetJoinInformation(NULL, &domain, &join_status) != NERR_Success)
+    return false;
+  ::NetApiBufferFree(domain);
+
+  return join_status == ::NetSetupDomainName;
 }
 
 }  // namespace win
