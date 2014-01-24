298c198743614dc4c3b5b0969667664118cea661
dgozman@chromium.org <dgozman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-24 15:00:26 +0000

    [DevTools] No split resizers for undocked DevTools.
    BUG=none
    
    Review URL: https://codereview.chromium.org/138953006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165754 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/InspectorView.js
+++ third_party/WebKit/Source/devtools/front_end/InspectorView.js
@@ -421,6 +421,8 @@ WebInspector.InspectorView.prototype = {
             }
         } else {
             this._splitView.showOnlySecond();
+            this._splitView.uninstallResizer(this._tabbedPane.headerElement());
+            this._splitView.uninstallResizer(this._splitView.resizerElement());
         }
     },
 
