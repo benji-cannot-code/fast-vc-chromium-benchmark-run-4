d7a74452fd6770c68b05bdeb57ecb8ca81f2f755
ferjmoreno@gmail.com <ferjmoreno@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-24 20:46:52 +0000

    The default action (browse the webstore) in the Extension Action context menu should, as with other platforms, be disabled on OS X when there's no valid webstore link.
    BUG=331614
    R=sail@chromium.org
    
    Review URL: https://codereview.chromium.org/144293003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@246961 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/extensions/extension_action_context_menu_controller.mm
+++ chrome/browser/ui/cocoa/extensions/extension_action_context_menu_controller.mm
@@ -100,6 +100,8 @@ class AsyncUninstaller : public ExtensionUninstallDialog::Delegate {
                       action:@selector(onExtensionName:)
                keyEquivalent:@""];
   [item setTarget:self];
+  [item setEnabled:extensions::ManifestURL::GetHomepageURL(
+      extension_).is_valid()];
 
   // Separator.
   [menu addItem:[NSMenuItem separatorItem]];
