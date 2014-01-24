05f101002ec7c37cc77639975d4acef02b0e4b1d
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-24 23:13:28 +0000

    Make mojom_data_tests.py exist and work again.
    This undoes r230890 (which was an incorrect "completion" of another
    revert), and updates mojom_data.py and mojom_data_tests.py to work
    properly again.
    
    (This does NOT add tests for the newer things.)
    
    R=davemoore@chromium.org
    
    Review URL: https://codereview.chromium.org/137203015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247001 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/public/bindings/generators/mojom_data.py
+++ mojo/public/bindings/generators/mojom_data.py
@@ -67,7 +67,8 @@ def StructFromData(kinds, data):
   struct.spec = 'x:' + struct.name
   kinds[struct.spec] = struct
   struct.fields = map(lambda field: FieldFromData(kinds, field), data['fields'])
-  struct.enums = map(lambda enum: EnumFromData(kinds, enum), data['enums'])
+  if data.has_key('enums'):
+    struct.enums = map(lambda enum: EnumFromData(kinds, enum), data['enums'])
   return struct
 
 def FieldToData(field):
@@ -123,7 +124,7 @@ def MethodFromData(kinds, data):
   method.ordinal = data.get('ordinal')
   method.default = data.get('default')
   method.parameters = map(
-    lambda parameter: ParameterFromData(kinds, parameter), data['parameters'])
+      lambda parameter: ParameterFromData(kinds, parameter), data['parameters'])
   return method
 
 def InterfaceToData(interface):
@@ -136,10 +137,11 @@ def InterfaceToData(interface):
 def InterfaceFromData(kinds, data):
   interface = mojom.Interface()
   interface.name = data['name']
-  interface.peer = data['peer']
+  interface.peer = data['peer'] if data.has_key('peer') else None
   interface.methods = map(
-    lambda method: MethodFromData(kinds, method), data['methods'])
-  interface.enums = map(lambda enum: EnumFromData(kinds, enum), data['enums'])
+      lambda method: MethodFromData(kinds, method), data['methods'])
+  if data.has_key('enums'):
+    interface.enums = map(lambda enum: EnumFromData(kinds, enum), data['enums'])
   return interface
 
 def EnumFieldFromData(kinds, data):
@@ -152,7 +154,7 @@ def EnumFromData(kinds, data):
   enum = mojom.Enum()
   enum.name = data['name']
   enum.fields = map(
-    lambda field: EnumFieldFromData(kinds, field), data['fields'])
+      lambda field: EnumFieldFromData(kinds, field), data['fields'])
   return enum
 
 def ModuleToData(module):
@@ -172,11 +174,12 @@ def ModuleFromData(data):
   module.name = data['name']
   module.namespace = data['namespace']
   module.structs = map(
-    lambda struct: StructFromData(kinds, struct), data['structs'])
+      lambda struct: StructFromData(kinds, struct), data['structs'])
   module.interfaces = map(
-    lambda interface: InterfaceFromData(kinds, interface), data['interfaces'])
-  module.enums = map(
-    lambda enum: EnumFromData(kinds, enum), data['enums'])
+      lambda interface: InterfaceFromData(kinds, interface), data['interfaces'])
+  if data.has_key('enums'):
+    module.enums = map(
+        lambda enum: EnumFromData(kinds, enum), data['enums'])
   return module
 
 def OrderedModuleFromData(data):
