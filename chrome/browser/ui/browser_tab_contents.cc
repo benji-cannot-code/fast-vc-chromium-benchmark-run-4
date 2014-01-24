d34dc0792be68fb59d9e2ac646a1deca92251f18
gbillock@chromium.org <gbillock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-24 23:17:37 +0000

    [WebsiteSettings] Adjustments to the permissions bubble API following mocks update
    R=markusheintz@chromium.org
    
    Review URL: https://codereview.chromium.org/134733021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247003 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser_tab_contents.cc
+++ chrome/browser/ui/browser_tab_contents.cc
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/search_engines/search_engine_tab_helper.h"
 #include "chrome/browser/ui/sync/tab_contents_synced_tab_delegate.h"
 #include "chrome/browser/ui/tab_contents/core_tab_helper.h"
+#include "chrome/browser/ui/website_settings/permission_bubble_manager.h"
 #include "chrome/browser/ui/zoom/zoom_controller.h"
 #include "chrome/common/chrome_switches.h"
 #include "components/autofill/content/browser/autofill_driver_impl.h"
@@ -136,6 +137,7 @@ void BrowserTabContents::AttachTabHelpers(WebContents* web_contents) {
   HistoryTabHelper::CreateForWebContents(web_contents);
   HungPluginTabHelper::CreateForWebContents(web_contents);
   InfoBarService::CreateForWebContents(web_contents);
+  PermissionBubbleManager::CreateForWebContents(web_contents);
   NavigationMetricsRecorder::CreateForWebContents(web_contents);
   NavigationTimeHelper::CreateForWebContents(web_contents);
   PasswordGenerationManager::CreateForWebContents(web_contents);
