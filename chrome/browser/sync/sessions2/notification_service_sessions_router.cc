52d7bf7328bcf8c8949e81323a189db90c11833a
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-24 13:19:03 +0000

    sync: sessionsv2 - handle deletion of local session by remote client
    BUG=98892
    
    Review URL: https://codereview.chromium.org/145623004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@246869 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/sessions2/notification_service_sessions_router.cc
+++ chrome/browser/sync/sessions2/notification_service_sessions_router.cc
@@ -151,7 +151,6 @@ void NotificationServiceSessionsRouter::StartRoutingTo(
 
 void NotificationServiceSessionsRouter::Stop() {
   weak_ptr_factory_.InvalidateWeakPtrs();
-  registrar_.RemoveAll();
   handler_ = NULL;
 }
 
