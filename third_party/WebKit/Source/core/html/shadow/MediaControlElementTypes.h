791281044f41e77ae1cf045cd29f4b275e6e4031
scherkus@chromium.org <scherkus@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-24 23:02:39 +0000

    Remove media seek back, seek forward, rewind, and return to realtime buttons.
    Chromium's media controls implementation doesn't make use of these buttons.
    
    BUG=337015
    R=adamk@chromium.org, jamesr@chromium.org, pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/146013005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165788 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/shadow/MediaControlElementTypes.h
+++ third_party/WebKit/Source/core/html/shadow/MediaControlElementTypes.h
@@ -39,17 +39,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-// Must match WebKitSystemInterface.h
 enum MediaControlElementType {
     MediaEnterFullscreenButton = 0,
     MediaMuteButton,
     MediaPlayButton,
-    MediaSeekBackButton,
-    MediaSeekForwardButton,
     MediaSlider,
     MediaSliderThumb,
-    MediaRewindButton,
-    MediaReturnToRealtimeButton,
     MediaShowClosedCaptionsButton,
     MediaHideClosedCaptionsButton,
     MediaUnMuteButton,
