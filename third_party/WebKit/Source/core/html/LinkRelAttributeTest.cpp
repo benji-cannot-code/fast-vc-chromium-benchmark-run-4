benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 3fa61c507785e1ac0019b716400e213b30dbc6c4

Remove compile time flag TOUCH_ICON_LOADING and use runtime flag instead. Update layout test case to use runtime flag.
Depends on 132173002 and 132163002

BUG=332863

Review URL: https://codereview.chromium.org/132723002

git-svn-id: svn://svn.chromium.org/blink/trunk@165160 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/html/LinkRelAttribute.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "core/html/LinkRelAttribute.h"
 
+#include "RuntimeEnabledFeatures.h"
 #include "wtf/text/WTFString.h"
 
 namespace WebCore {
@@ -59,23 +60,22 @@ LinkRelAttribute::LinkRelAttribute(const String& rel)
     , m_isLinkPrerender(false)
     , m_isImport(false)
 {
-    if (equalIgnoringCase(rel, "stylesheet"))
+    if (equalIgnoringCase(rel, "stylesheet")) {
         m_isStyleSheet = true;
-    else if (equalIgnoringCase(rel, "icon") || equalIgnoringCase(rel, "shortcut icon"))
+    } else if (equalIgnoringCase(rel, "icon") || equalIgnoringCase(rel, "shortcut icon")) {
         m_iconType = Favicon;
-#if ENABLE(TOUCH_ICON_LOADING)
-    else if (equalIgnoringCase(rel, "apple-touch-icon"))
-        m_iconType = TouchIcon;
-    else if (equalIgnoringCase(rel, "apple-touch-icon-precomposed"))
-        m_iconType = TouchPrecomposedIcon;
-#endif
-    else if (equalIgnoringCase(rel, "dns-prefetch"))
+    } else if (equalIgnoringCase(rel, "dns-prefetch")) {
         m_isDNSPrefetch = true;
-    else if (equalIgnoringCase(rel, "alternate stylesheet") || equalIgnoringCase(rel, "stylesheet alternate")) {
+    } else if (equalIgnoringCase(rel, "alternate stylesheet") || equalIgnoringCase(rel, "stylesheet alternate")) {
         m_isStyleSheet = true;
         m_isAlternate = true;
     } else if (equalIgnoringCase(rel, "import")) {
         m_isImport = true;
+    } else if (RuntimeEnabledFeatures::touchIconLoadingEnabled()) {
+        if (equalIgnoringCase(rel, "apple-touch-icon"))
+            m_iconType = TouchIcon;
+        else if (equalIgnoringCase(rel, "apple-touch-icon-precomposed"))
+            m_iconType = TouchPrecomposedIcon;
     } else {
         // Tokenize the rel attribute and set bits based on specific keywords that we find.
         String relCopy = rel;
@@ -84,24 +84,24 @@ LinkRelAttribute::LinkRelAttribute(const String& rel)
         relCopy.split(' ', list);
         Vector<String>::const_iterator end = list.end();
         for (Vector<String>::const_iterator it = list.begin(); it != end; ++it) {
-            if (equalIgnoringCase(*it, "stylesheet"))
+            if (equalIgnoringCase(*it, "stylesheet")) {
                 m_isStyleSheet = true;
-            else if (equalIgnoringCase(*it, "alternate"))
+            } else if (equalIgnoringCase(*it, "alternate")) {
                 m_isAlternate = true;
-            else if (equalIgnoringCase(*it, "icon"))
+            } else if (equalIgnoringCase(*it, "icon")) {
                 m_iconType = Favicon;
-#if ENABLE(TOUCH_ICON_LOADING)
-            else if (equalIgnoringCase(*it, "apple-touch-icon"))
-                m_iconType = TouchIcon;
-            else if (equalIgnoringCase(*it, "apple-touch-icon-precomposed"))
-                m_iconType = TouchPrecomposedIcon;
-#endif
-            else if (equalIgnoringCase(*it, "prefetch"))
-              m_isLinkPrefetch = true;
-            else if (equalIgnoringCase(*it, "subresource"))
-              m_isLinkSubresource = true;
-            else if (equalIgnoringCase(*it, "prerender"))
-              m_isLinkPrerender = true;
+            } else if (equalIgnoringCase(*it, "prefetch")) {
+                m_isLinkPrefetch = true;
+            } else if (equalIgnoringCase(*it, "subresource")) {
+                m_isLinkSubresource = true;
+            } else if (equalIgnoringCase(*it, "prerender")) {
+                m_isLinkPrerender = true;
+            } else if (RuntimeEnabledFeatures::touchIconLoadingEnabled()) {
+                if (equalIgnoringCase(*it, "apple-touch-icon"))
+                    m_iconType = TouchIcon;
+                else if (equalIgnoringCase(*it, "apple-touch-icon-precomposed"))
+                    m_iconType = TouchPrecomposedIcon;
+            }
         }
     }
 }

third_party/WebKit/Source/core/html/LinkRelAttributeTest.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 #include "core/html/LinkRelAttribute.h"
+#include "RuntimeEnabledFeatures.h"
 
 #include "wtf/text/CString.h"
 #include <gtest/gtest.h>
@@ -61,13 +62,13 @@ TEST(CoreLinkRelAttribute, Constructor)
     testLinkRelAttribute("shortcut icon", false, Favicon, false, false, false, false);
     testLinkRelAttribute("sHoRtCuT iCoN", false, Favicon, false, false, false, false);
 
-#if ENABLE(TOUCH_ICON_LOADING)
-    testLinkRelAttribute("apple-touch-icon", false, TouchIcon, false, false, false, false);
-    testLinkRelAttribute("aPpLe-tOuCh-IcOn", false, TouchIcon, false, false, false, false);
+    if (RuntimeEnabledFeatures::touchIconLoadingEnabled()) {
+        testLinkRelAttribute("apple-touch-icon", false, TouchIcon, false, false, false, false);
+        testLinkRelAttribute("aPpLe-tOuCh-IcOn", false, TouchIcon, false, false, false, false);
 
-    testLinkRelAttribute("apple-touch-icon-precomposed", false, TouchPrecomposedIcon, false, false, false, false);
-    testLinkRelAttribute("aPpLe-tOuCh-IcOn-pReCoMpOsEd", false, TouchPrecomposedIcon, false, false, false, false);
-#endif
+        testLinkRelAttribute("apple-touch-icon-precomposed", false, TouchPrecomposedIcon, false, false, false, false);
+        testLinkRelAttribute("aPpLe-tOuCh-IcOn-pReCoMpOsEd", false, TouchPrecomposedIcon, false, false, false, false);
+    }
 
     testLinkRelAttribute("dns-prefetch", false, InvalidIcon, false, true, false, false);
     testLinkRelAttribute("dNs-pReFeTcH", false, InvalidIcon, false, true, false, false);

third_party/WebKit/LayoutTests/fast/dom/icon-url-list-apple-touch-expected.txt
@@ -2,6 +2,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 PASS successfullyParsed is true
 
 TEST COMPLETE
-Tests that all favicons and touch icons (if ENABLE(TOUCH_ICON_LOADING)) are in document.iconURLs()
-http://test.com/oldfavicon.ico
+Tests that all favicons and touch icons are in document.iconURLs()
+All icons are preset and in-order in document.iconURLs()
 

third_party/WebKit/LayoutTests/fast/dom/icon-url-list-apple-touch.html
@@ -11,16 +11,29 @@ function runTests() {
     if (window.testRunner)
         testRunner.dumpAsText();
 
-    debug('Tests that all favicons and touch icons (if ENABLE(TOUCH_ICON_LOADING)) are in document.iconURLs()');
+    debug('Tests that all favicons and touch icons are in document.iconURLs()');
 
     // Fetch the actual list of icon URLs.
     var actualURLs = window.internals.allIconURLs(document);
 
-    // Print out the URL list in the document to match it against the expected list.
-    // Note that the expected order is reverse to ensure that icons seen later
-    // take precedence as required by the spec.
-    for (var i = 0; i < actualURLs.length; i++)
-        debug(actualURLs[i]);
+    if (!actualURLs[0] || actualURLs[0] != "http://test.com/oldfavicon.ico") {
+        debug("Test failed.. not the right icon on first place in document.iconURLs()")
+        return
+    }
+
+    if (window.internals && window.internals.runtimeFlags.touchIconLoadingEnabled) {
+        if (!actualURLs[1] || actualURLs[1] != "http://test.com/i/touch.png") {
+            debug("Test failed.. apple touch icon missing or invalid.")
+            return
+        }
+
+        if (!actualURLs[2] || actualURLs[2] != "http://test.com/i/touch-precomposed.png") {
+            debug("Test failed.. apple touch icon missing or invalid.")
+            return
+        }
+    }
+
+    debug("All icons are preset and in-order in document.iconURLs()")
 }
 </script>
 </head>

third_party/WebKit/Source/build/features.gypi
@@ -45,8 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     # through GYP_DEFINES.
     'variables': {
       # Enables the Oilpan garbage-collection infrastructure.
-      'enable_oilpan%': 0,
-      'enable_touch_icon_loading%' : 0,
+      'enable_oilpan%': 0
     },
     'conditions': [
       ['use_concatenated_impulse_responses==1', {
@@ -58,8 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           'ENABLE_FAST_MOBILE_SCROLLING=1',
           'ENABLE_INPUT_SPEECH=0',
           'ENABLE_LEGACY_NOTIFICATIONS=0',
-          'ENABLE_MEDIA_CAPTURE=1',
-          'ENABLE_TOUCH_ICON_LOADING=1',
+          'ENABLE_MEDIA_CAPTURE=1'
         ],
       }, { # OS!="android"
         'feature_defines': [
@@ -67,7 +65,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           'ENABLE_INPUT_MULTIPLE_FIELDS_UI=1',
           'ENABLE_LEGACY_NOTIFICATIONS=1',
           'ENABLE_MEDIA_CAPTURE=0',
-          'ENABLE_WEB_AUDIO=1',
+          'ENABLE_WEB_AUDIO=1'
         ],
       }],
       # Mac OS X uses Accelerate.framework FFT by default instead of FFmpeg.

third_party/WebKit/Source/core/dom/Document.cpp
@@ -4356,10 +4356,8 @@ Vector<IconURL> Document::iconURLs(int iconTypesMask)
             continue;
         if (linkElement->href().isEmpty())
             continue;
-#if !ENABLE(TOUCH_ICON_LOADING)
-        if (linkElement->iconType() != Favicon)
+        if (!RuntimeEnabledFeatures::touchIconLoadingEnabled() && linkElement->iconType() != Favicon)
             continue;
-#endif
 
         IconURL newURL(linkElement->href(), linkElement->iconSizes(), linkElement->type(), linkElement->iconType());
         if (linkElement->iconType() == Favicon) {

third_party/WebKit/Source/core/dom/IconURL.h
@@ -36,12 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-#if ENABLE(TOUCH_ICON_LOADING)
-#define ICON_COUNT 3
-#else
-#define ICON_COUNT 1
-#endif
-
 enum IconType {
     InvalidIcon = 0,
     Favicon = 1,
@@ -75,8 +69,6 @@ struct IconURL {
 };
 
 bool operator==(const IconURL&, const IconURL&);
-
-typedef Vector<IconURL, ICON_COUNT> IconURLs;
 
 }
 

commit 44c59b8d68ee47450338dbf2be945b58daafc2ee

Fix a bug that <link rel=prerender> is not parsed on mobile.
This bug was introduced in r165160.
https://codereview.chromium.org/132723002/

Review URL: https://codereview.chromium.org/134833005

git-svn-id: svn://svn.chromium.org/blink/trunk@165734 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/html/LinkRelAttribute.cpp
@@ -71,11 +71,14 @@ LinkRelAttribute::LinkRelAttribute(const String& rel)
         m_isAlternate = true;
     } else if (equalIgnoringCase(rel, "import")) {
         m_isImport = true;
-    } else if (RuntimeEnabledFeatures::touchIconLoadingEnabled()) {
-        if (equalIgnoringCase(rel, "apple-touch-icon"))
+    } else if (equalIgnoringCase(rel, "apple-touch-icon")) {
+        if (RuntimeEnabledFeatures::touchIconLoadingEnabled()) {
             m_iconType = TouchIcon;
-        else if (equalIgnoringCase(rel, "apple-touch-icon-precomposed"))
+        }
+    } else if (equalIgnoringCase(rel, "apple-touch-icon-precomposed")) {
+        if (RuntimeEnabledFeatures::touchIconLoadingEnabled()) {
             m_iconType = TouchPrecomposedIcon;
+        }
     } else {
         // Tokenize the rel attribute and set bits based on specific keywords that we find.
         String relCopy = rel;
@@ -96,11 +99,14 @@ LinkRelAttribute::LinkRelAttribute(const String& rel)
                 m_isLinkSubresource = true;
             } else if (equalIgnoringCase(*it, "prerender")) {
                 m_isLinkPrerender = true;
-            } else if (RuntimeEnabledFeatures::touchIconLoadingEnabled()) {
-                if (equalIgnoringCase(*it, "apple-touch-icon"))
+            } else if (equalIgnoringCase(*it, "apple-touch-icon")) {
+                if (RuntimeEnabledFeatures::touchIconLoadingEnabled()) {
                     m_iconType = TouchIcon;
-                else if (equalIgnoringCase(*it, "apple-touch-icon-precomposed"))
+                }
+            } else if (equalIgnoringCase(*it, "apple-touch-icon-precomposed")) {
+                if (RuntimeEnabledFeatures::touchIconLoadingEnabled()) {
                     m_iconType = TouchPrecomposedIcon;
+                }
             }
         }
     }

third_party/WebKit/Source/core/html/LinkRelAttributeTest.cpp
@@ -40,6 +40,22 @@ using namespace WebCore;
 
 namespace {
 
+class LinkRelAttributeTest : public testing::Test {
+protected:
+    virtual void SetUp()
+    {
+        m_touchIconLoadingEnabled = RuntimeEnabledFeatures::touchIconLoadingEnabled();
+    }
+
+    virtual void TearDown()
+    {
+        RuntimeEnabledFeatures::setTouchIconLoadingEnabled(m_touchIconLoadingEnabled);
+    }
+
+private:
+    bool m_touchIconLoadingEnabled;
+};
+
 static inline void testLinkRelAttribute(String value, bool isStyleSheet, IconType iconType, bool isAlternate, bool isDNSPrefetch, bool isLinkSubresource, bool isLinkPrerender, bool isImport = false)
 {
     LinkRelAttribute linkRelAttribute(value);
@@ -52,8 +68,10 @@ static inline void testLinkRelAttribute(String value, bool isStyleSheet, IconTyp
     ASSERT_EQ(isImport, linkRelAttribute.isImport()) << value.utf8().data();
 }
 
-TEST(CoreLinkRelAttribute, Constructor)
+TEST_F(LinkRelAttributeTest, Constructor)
 {
+    RuntimeEnabledFeatures::setTouchIconLoadingEnabled(false);
+
     testLinkRelAttribute("stylesheet", true, InvalidIcon, false, false, false, false);
     testLinkRelAttribute("sTyLeShEeT", true, InvalidIcon, false, false, false, false);
 
@@ -62,16 +80,47 @@ TEST(CoreLinkRelAttribute, Constructor)
     testLinkRelAttribute("shortcut icon", false, Favicon, false, false, false, false);
     testLinkRelAttribute("sHoRtCuT iCoN", false, Favicon, false, false, false, false);
 
-    if (RuntimeEnabledFeatures::touchIconLoadingEnabled()) {
-        testLinkRelAttribute("apple-touch-icon", false, TouchIcon, false, false, false, false);
-        testLinkRelAttribute("aPpLe-tOuCh-IcOn", false, TouchIcon, false, false, false, false);
+    testLinkRelAttribute("dns-prefetch", false, InvalidIcon, false, true, false, false);
+    testLinkRelAttribute("dNs-pReFeTcH", false, InvalidIcon, false, true, false, false);
 
-        testLinkRelAttribute("apple-touch-icon-precomposed", false, TouchPrecomposedIcon, false, false, false, false);
-        testLinkRelAttribute("aPpLe-tOuCh-IcOn-pReCoMpOsEd", false, TouchPrecomposedIcon, false, false, false, false);
-    }
+    testLinkRelAttribute("apple-touch-icon", false, InvalidIcon, false, false, false, false);
+    testLinkRelAttribute("aPpLe-tOuCh-IcOn", false, InvalidIcon, false, false, false, false);
+    testLinkRelAttribute("apple-touch-icon-precomposed", false, InvalidIcon, false, false, false, false);
+    testLinkRelAttribute("aPpLe-tOuCh-IcOn-pReCoMpOsEd", false, InvalidIcon, false, false, false, false);
+
+    testLinkRelAttribute("alternate stylesheet", true, InvalidIcon, true, false, false, false);
+    testLinkRelAttribute("stylesheet alternate", true, InvalidIcon, true, false, false, false);
+    testLinkRelAttribute("aLtErNaTe sTyLeShEeT", true, InvalidIcon, true, false, false, false);
+    testLinkRelAttribute("sTyLeShEeT aLtErNaTe", true, InvalidIcon, true, false, false, false);
+
+    testLinkRelAttribute("stylesheet icon prerender aLtErNaTe", true, Favicon, true, false, false, true);
+    testLinkRelAttribute("alternate subresource", false, InvalidIcon, true, false, true, false);
+    testLinkRelAttribute("alternate icon stylesheet", true, Favicon, true, false, false, false);
+
+    testLinkRelAttribute("import", false, InvalidIcon, false, false, false, false, true);
+    // "import" is mutually exclusive and "stylesheet" wins when they conflict.
+    testLinkRelAttribute("stylesheet import", true, InvalidIcon, false, false, false, false, false);
+}
+
+TEST_F(LinkRelAttributeTest, ConstructorTouchIconLoadingEnabled)
+{
+    RuntimeEnabledFeatures::setTouchIconLoadingEnabled(true);
+
+    testLinkRelAttribute("stylesheet", true, InvalidIcon, false, false, false, false);
+    testLinkRelAttribute("sTyLeShEeT", true, InvalidIcon, false, false, false, false);
+
+    testLinkRelAttribute("icon", false, Favicon, false, false, false, false);
+    testLinkRelAttribute("iCoN", false, Favicon, false, false, false, false);
+    testLinkRelAttribute("shortcut icon", false, Favicon, false, false, false, false);
+    testLinkRelAttribute("sHoRtCuT iCoN", false, Favicon, false, false, false, false);
 
     testLinkRelAttribute("dns-prefetch", false, InvalidIcon, false, true, false, false);
     testLinkRelAttribute("dNs-pReFeTcH", false, InvalidIcon, false, true, false, false);
+
+    testLinkRelAttribute("apple-touch-icon", false, TouchIcon, false, false, false, false);
+    testLinkRelAttribute("aPpLe-tOuCh-IcOn", false, TouchIcon, false, false, false, false);
+    testLinkRelAttribute("apple-touch-icon-precomposed", false, TouchPrecomposedIcon, false, false, false, false);
+    testLinkRelAttribute("aPpLe-tOuCh-IcOn-pReCoMpOsEd", false, TouchPrecomposedIcon, false, false, false, false);
 
     testLinkRelAttribute("alternate stylesheet", true, InvalidIcon, true, false, false, false);
     testLinkRelAttribute("stylesheet alternate", true, InvalidIcon, true, false, false, false);