00bc48e30c137ebdc3519b8d6cb54b47ad1c3de4
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-24 14:09:24 +0000

    We've replaced SharedPersistent with ScopedPersistent or ScriptValue before, but a couple of SharedPersistents are remaining. So this CL replaces them.
    Ideally we want to remove SharedPersistent though.
    
    Review URL: https://codereview.chromium.org/146523002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165751 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/ScopedPersistent.h
+++ third_party/WebKit/Source/bindings/v8/ScopedPersistent.h
@@ -78,7 +78,7 @@ public:
         m_handle.Reset();
     }
 
-    bool operator==(const ScopedPersistent<T>& other)
+    bool operator==(const ScopedPersistent<T>& other) const
     {
         return m_handle == other.m_handle;
     }
--- third_party/WebKit/Source/bindings/v8/ScriptController.h
+++ third_party/WebKit/Source/bindings/v8/ScriptController.h
@@ -33,8 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ScriptController_h
 
 #include "bindings/v8/ScriptValue.h"
-#include "bindings/v8/SharedPersistent.h"
-
 #include "core/fetch/CrossOriginAccessControl.h"
 #include "wtf/Forward.h"
 #include "wtf/HashMap.h"
@@ -111,7 +109,7 @@ public:
     // Creates a property of the global object of a frame.
     void bindToWindowObject(Frame*, const String& key, NPObject*);
 
-    PassRefPtr<SharedPersistent<v8::Object> > createPluginWrapper(Widget*);
+    ScriptValue createPluginWrapper(Widget*);
 
     void enableEval();
     void disableEval(const String& errorMessage);
