62cc0c309b3cdc8139c54ac4fbbb48b955e3a3f8
eustas@chromium.org <eustas@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-24 13:50:52 +0000

    Don't use gradient for strips on network panel.
    BUG=335095
    
    Review URL: https://codereview.chromium.org/145643002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165747 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/NetworkPanel.js
+++ third_party/WebKit/Source/devtools/front_end/NetworkPanel.js
@@ -190,7 +190,7 @@ WebInspector.NetworkLogView.prototype = {
     {
         var columns = [];
         columns.push({
-            id: "name", 
+            id: "name",
             titleDOMFragment: this._makeHeaderFragment(WebInspector.UIString("Name"), WebInspector.UIString("Path")),
             title: WebInspector.UIString("Name"),
             sortable: true,
@@ -1130,6 +1130,11 @@ WebInspector.NetworkLogView.prototype = {
                 row.enableStyleClass("offscreen", !rowIsVisible);
                 row.rowIsVisible = rowIsVisible;
             }
+            var rowIsOdd = !!(unfilteredRowIndex & 1);
+            if (rowIsOdd !== row.rowIsOdd) {
+                row.enableStyleClass("odd", rowIsOdd);
+                row.rowIsOdd = rowIsOdd;
+            }
             unfilteredRowIndex++;
         }
     },
@@ -1726,7 +1731,7 @@ WebInspector.NetworkPanel.prototype = {
         this._networkLogView.searchCanceled();
     },
 
-    /** 
+    /**
      * @param {!WebInspector.ContextMenu} contextMenu
      * @param {!Object} target
      * @this {WebInspector.NetworkPanel}
