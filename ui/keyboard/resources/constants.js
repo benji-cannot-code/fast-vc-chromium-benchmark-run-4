2018de5be8bf6316be68ca657e7f17ed032583d0
rsadam@chromium.org <rsadam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-24 01:34:16 +0000

    Dynamically resizes the virtual keyboard while maintaining pixel perfect.
    BUG=301747, 290180, 332074, 329063
    
    Review URL: https://codereview.chromium.org/138503004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@246768 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/keyboard/resources/constants.js
+++ ui/keyboard/resources/constants.js
@@ -9,6 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 var ASPECT_RATIO = 4.5;
 
+var RowAlignment = {
+  STRETCH: "stretch",
+  LEFT: "left",
+  RIGHT: "right",
+  CENTER: "center"
+}
+
 /**
  * Ratio of key height and font size.
  * @type {number}
@@ -36,3 +43,22 @@ var SWIPE_DIRECTION = {
   DOWN: 0x8
 };
 
+/**
+ * The default weight of a key in the X direction.
+ * @type {number}
+ */
+var DEFAULT_KEY_WEIGHT_X = 100;
+
+/**
+ * The default weight of a key in the Y direction.
+ * @type {number}
+ */
+var DEFAULT_KEY_WEIGHT_Y = 60;
+
+/**
+ * The top padding on each key.
+ * @type {number}
+ */
+// TODO(rsadam): Remove this variable once figure out how to calculate this
+// number before the key is rendered.
+var KEY_PADDING_TOP = 2;
