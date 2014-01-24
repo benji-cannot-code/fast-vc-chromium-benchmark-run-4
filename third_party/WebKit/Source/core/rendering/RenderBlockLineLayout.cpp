fd7b0b7e531ec6d7e4abe977d285a74d69393dc0
pdr@chromium.org <pdr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-24 03:21:14 +0000

    [FastTextAutosizer] Use the deepest block that contains text for width
    This patch makes the cluster multiplier calculation use the width of
    the deepest block containing text for a cluster instead of the cluster
    root's width. For example:
    <div width="500"> <-- cluster root
      <div width="100">
        text
      </div>
    </div>
    Previously, the cluster would use a width of 500 but with this patch
    a width of 100 is used. This improves our rendering on the lca
    tests.
    
    Unfortunately, the width of child render objects is not available at
    style recalc time (when FastTextAutosizer::record is called). This
    patch refactors the cluster multiplier calculation to be lazy so that
    it is not calculated until after style recalc. computeMultiplier has
    been renamed clusterMultiplier to better describe this laziness.
    
    With this patch we pass 2 additional tests, and fail 1 that we
    erroneously passed before.
    
    NOTRY=true
    R=skobes
    B=302005
    
    Review URL: https://codereview.chromium.org/142933002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165687 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderBlockLineLayout.cpp
+++ third_party/WebKit/Source/core/rendering/RenderBlockLineLayout.cpp
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 
-#include "core/rendering/FastTextAutosizer.h"
 #include "core/rendering/LayoutRectRecorder.h"
 #include "core/rendering/RenderCounter.h"
 #include "core/rendering/RenderFlowThread.h"
