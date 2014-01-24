92692577228e5a8b0b97f938c182eb5704f3b2f7
stip@google.com <stip@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-24 06:48:37 +0000

    Disable testRasterizeAndRecord.
    TBR=tonyg@chromium.org
    NOTRY=true
    BUG=156398
    
    Review URL: https://codereview.chromium.org/146433002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@246798 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/measurements/rasterize_and_record_unittest.py
+++ tools/perf/measurements/rasterize_and_record_unittest.py
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 from measurements import rasterize_and_record
 from telemetry.core import wpr_modes
 from telemetry.page import page_measurement_unittest_base
-from telemetry.unittest import options_for_unittests
+from telemetry.unittest import DisabledTest, options_for_unittests
 
 
 class RasterizeAndRecordUnitTest(
@@ -24,6 +24,7 @@ class RasterizeAndRecordUnitTest(
     self._options.start_wait_time = 0.0
     self._options.stop_wait_time = 2.0
 
+  @DisabledTest
   def testRasterizeAndRecord(self):
     ps = self.CreatePageSetFromFileInUnittestDataDir('blank.html')
     measurement = rasterize_and_record.RasterizeAndRecord()
