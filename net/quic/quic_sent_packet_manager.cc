7ce38a89eeaea6a9030546356d8b69e1f0d93baf
rtenneti@chromium.org <rtenneti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-25 20:32:51 +0000

    QUIC - Moved FindOrNull and FindOrDie to a separate files.
    R=rch@chromium.org
    
    Review URL: https://codereview.chromium.org/140253014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247139 0039d316-1c4b-4281-b951-d872f2087c98

--- net/quic/quic_sent_packet_manager.cc
+++ net/quic/quic_sent_packet_manager.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util.h"
 #include "net/quic/congestion_control/pacing_sender.h"
 #include "net/quic/quic_ack_notifier_manager.h"
+#include "net/quic/quic_utils_chromium.h"
 
 using std::make_pair;
 using std::max;
