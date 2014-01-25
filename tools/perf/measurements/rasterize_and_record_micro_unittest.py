6572cfa509a3713e08c4579d90916a23db0b2342
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-25 01:34:02 +0000

    [Telemetry] Disabled testRasterizeAndRecordMicro.
    It, like testRasterizeAndRecord, is flaky on Windows.
    
    TBR=ernstm@chromium.org,kbr@chromium.org
    NOTRY=True
    BUG=
    
    Review URL: https://codereview.chromium.org/134163007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247054 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/measurements/rasterize_and_record_micro_unittest.py
+++ tools/perf/measurements/rasterize_and_record_micro_unittest.py
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 from measurements import rasterize_and_record_micro
 from telemetry.core import wpr_modes
 from telemetry.page import page_measurement_unittest_base
-from telemetry.unittest import options_for_unittests
+from telemetry.unittest import DisabledTest, options_for_unittests
 
 
 class RasterizeAndRecordMicroUnitTest(
@@ -24,6 +24,7 @@ class RasterizeAndRecordMicroUnitTest(
     self._options.record_repeat = 1
     self._options.start_wait_time = 0.0
 
+  @DisabledTest
   def testRasterizeAndRecordMicro(self):
     ps = self.CreatePageSetFromFileInUnittestDataDir('blank.html')
     measurement = rasterize_and_record_micro.RasterizeAndRecordMicro()
