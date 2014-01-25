a83f9d6971dc4975f24fcf0645a58b15180a3715
arv@chromium.org <arv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-25 07:42:01 +0000

    Use ES6 Promise in Bookmark manager instead of cr.Promise
    BUG=None
    
    Review URL: https://codereview.chromium.org/137143004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247091 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/bookmark_manager/js/main.js
+++ chrome/browser/resources/bookmark_manager/js/main.js
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /** @const */ var ListItem = cr.ui.ListItem;
 /** @const */ var Menu = cr.ui.Menu;
 /** @const */ var MenuButton = cr.ui.MenuButton;
-/** @const */ var Promise = cr.Promise;
 /** @const */ var Splitter = cr.ui.Splitter;
 /** @const */ var TreeItem = cr.ui.TreeItem;
 
@@ -315,6 +314,12 @@ function handleLoadForTree(e) {
   processHash();
 }
 
+/**
+ * Returns a promise for all the URLs in the {@code nodes} and the direct
+ * children of {@code nodes}.
+ * @param {!Array.<BookmarkTreeNode>} nodes .
+ * @return {!Promise.<Array.<string>>} .
+ */
 function getAllUrls(nodes) {
   var urls = [];
 
@@ -338,20 +343,13 @@ function getAllUrls(nodes) {
     if (bmm.isFolder(node))
       return bmm.loadSubtree(node.id);
     // Not a folder so we already have all the data we need.
-    return new Promise(node);
+    return Promise.resolve(node);
   });
 
-  var urlsPromise = new Promise();
-
-  var p = Promise.all.apply(null, promises);
-  p.addListener(function(nodes) {
-    nodes.forEach(function(node) {
-      addNodes(node);
-    });
-    urlsPromise.value = urls;
+  return Promise.all(promises).then(function(nodes) {
+    nodes.forEach(addNodes);
+    return urls;
   });
-
-  return urlsPromise;
 }
 
 /**
@@ -382,7 +380,7 @@ function getNodesForOpen(target) {
  * Returns a promise that will contain all URLs of all the selected bookmarks
  * and the nested bookmarks for use with the open commands.
  * @param {HTMLElement} target The target list or tree.
- * @return {Promise} .
+ * @return {Promise.<Array.<string>>} .
  */
 function getUrlsForOpenCommands(target) {
   return getAllUrls(getNodesForOpen(target));
@@ -419,7 +417,7 @@ function updateOpenCommand(e, command, singularId, pluralId, commandDisabled) {
     return;
   }
 
-  getUrlsForOpenCommands(e.target).addListener(function(urls) {
+  getUrlsForOpenCommands(e.target).then(function(urls) {
     var disabled = !urls.length;
     command.disabled = disabled;
     e.canExecute = !disabled;
@@ -818,7 +816,7 @@ function openBookmarks(kind, opt_eventTarget) {
   // the whole tree since we would like to minimize the amount of data sent.
 
   var urlsP = getUrlsForOpenCommands(opt_eventTarget);
-  urlsP.addListener(function(urls) {
+  urlsP.then(function(urls) {
     getLinkController().openUrls(urls, kind);
     chrome.bookmarkManagerPrivate.recordLaunch();
   });
