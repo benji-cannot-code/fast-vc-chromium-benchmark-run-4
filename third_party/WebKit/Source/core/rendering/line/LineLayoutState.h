f3cf128a1ead494cd854f7230e9dc25333160b4d
leviw@chromium.org <leviw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-25 00:19:25 +0000

    Remove unnecessary repaint from line layout and cleanup logic
    In the beforetime, it apparently made sense to use the cached repaint
    rect from RenderLayer directly within line layout code to ensure we
    got repainted. We now properly repaint ourself in this case without
    having to rely on reaching directly into our layer for the info.
    
    While I'm in the code, I'm moving the logic to mark ourself as
    needing layout to ensure we trigger a repaint of ourself to
    determineStartPosition (since it's responsible for marking the block
    for full layout to begin with), and moving the hasInlineChild
    optimization to LineLayoutState.
    
    Review URL: https://codereview.chromium.org/145083009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165800 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/line/LineLayoutState.h
+++ third_party/WebKit/Source/core/rendering/line/LineLayoutState.h
@@ -43,6 +43,7 @@ public:
         , m_endLineLogicalTop(0)
         , m_endLineMatched(false)
         , m_checkForFloatsFromLastLine(false)
+        , m_hasInlineChild(false)
         , m_isFullLayout(fullLayout)
         , m_repaintLogicalTop(repaintLogicalTop)
         , m_repaintLogicalBottom(repaintLogicalBottom)
@@ -74,6 +75,9 @@ public:
 
     bool checkForFloatsFromLastLine() const { return m_checkForFloatsFromLastLine; }
     void setCheckForFloatsFromLastLine(bool check) { m_checkForFloatsFromLastLine = check; }
+
+    bool hasInlineChild() const { return m_hasInlineChild; }
+    void setHasInlineChild(bool hasInlineChild) { m_hasInlineChild = hasInlineChild; }
 
     LineInfo& lineInfo() { return m_lineInfo; }
     const LineInfo& lineInfo() const { return m_lineInfo; }
@@ -107,6 +111,9 @@ private:
     LayoutUnit m_endLineLogicalTop;
     bool m_endLineMatched;
     bool m_checkForFloatsFromLastLine;
+    // Used as a performance optimization to avoid doing a full repaint when our floats
+    // change but we don't have any inline children.
+    bool m_hasInlineChild;
 
     bool m_isFullLayout;
 
