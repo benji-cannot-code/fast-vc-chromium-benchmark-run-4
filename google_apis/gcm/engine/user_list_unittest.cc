67b0f5c4cbd4f202934d060bbe458c46d94bea8f
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-25 09:23:57 +0000

    [GCM] Fix memory leaks
    Fixes bug where we forgot to clear a ReliablePacketInfo in MCSClient.
    Makes GCMStore explicitly pass ownership of the outgoing message memory it
    allocates.
    
    BUG=337560
    
    Review URL: https://codereview.chromium.org/147193003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247108 0039d316-1c4b-4281-b951-d872f2087c98

--- google_apis/gcm/engine/user_list_unittest.cc
+++ google_apis/gcm/engine/user_list_unittest.cc
@@ -73,7 +73,7 @@ class UserListTest : public testing::Test {
 
   void UpdateCallback(bool success);
 
-  void Initialize(UserList* user_list, const GCMStore::LoadResult& result);
+  void Initialize(UserList* user_list, scoped_ptr<GCMStore::LoadResult> result);
 
   void ResetLoop();
 
@@ -117,9 +117,9 @@ scoped_ptr<UserList> UserListTest::BuildUserList() {
 }
 
 void UserListTest::Initialize(UserList* user_list,
-                              const GCMStore::LoadResult& result) {
-  ASSERT_TRUE(result.success);
-  user_list->Initialize(result.serial_number_mappings);
+                              scoped_ptr<GCMStore::LoadResult> result) {
+  ASSERT_TRUE(result->success);
+  user_list->Initialize(result->serial_number_mappings);
   ResetLoop();
 }
 
