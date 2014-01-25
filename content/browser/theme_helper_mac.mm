924b6d95b2009b0af809db7444459e0ab46f798b
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-25 00:20:04 +0000

    Move preferred scrollbar style preference change listening from renderer to browser, 4 of 3.
    When --single-process, do not broker system notifications to renderers, since it
    will cause some content_browsertests to DCHECK.
    
    BUG=306348
    
    Review URL: https://codereview.chromium.org/145013018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247030 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/theme_helper_mac.mm
+++ content/browser/theme_helper_mac.mm
@@ -7,11 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Cocoa/Cocoa.h>
 
+#include "base/command_line.h"
 #include "content/common/view_messages.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/notification_types.h"
 #include "content/public/browser/render_process_host.h"
+#include "content/public/common/content_switches.h"
 
 // Declare notification names from the 10.7 SDK.
 #if !defined(MAC_OS_X_VERSION_10_7) || \
@@ -50,7 +52,10 @@ suspensionBehavior:NSNotificationSuspensionBehaviorDeliverImmediately];
            object:nil
 suspensionBehavior:NSNotificationSuspensionBehaviorCoalesce];
 
-  if ([NSScroller respondsToSelector:@selector(preferredScrollerStyle)]) {
+  // In single-process mode, renderers will catch these notifications
+  // themselves and listening for them here may trigger the DCHECK in Observe().
+  if ([NSScroller respondsToSelector:@selector(preferredScrollerStyle)] &&
+      !CommandLine::ForCurrentProcess()->HasSwitch(switches::kSingleProcess)) {
     [[NSNotificationCenter defaultCenter]
         addObserver:self
            selector:@selector(behaviorPrefsChanged:)
