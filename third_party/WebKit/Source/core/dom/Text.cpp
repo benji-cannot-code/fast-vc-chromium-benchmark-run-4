2da00300ef2d2ba790743973fbce97258231ad75
rob.buis@samsung.com <rob.buis@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-26 09:23:57 +0000

    Remove some childTypeAllowed implementations
    All these implementations override some parent class childTypeAllowed with the same code.
    
    Review URL: https://codereview.chromium.org/130263005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165819 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Text.cpp
+++ third_party/WebKit/Source/core/dom/Text.cpp
@@ -350,11 +350,6 @@ void Text::updateTextRenderer(unsigned offsetOfReplacedData, unsigned lengthOfRe
     textRenderer->setTextWithOffset(dataImpl(), offsetOfReplacedData, lengthOfReplacedData);
 }
 
-bool Text::childTypeAllowed(NodeType) const
-{
-    return false;
-}
-
 PassRefPtr<Text> Text::cloneWithData(const String& data)
 {
     return create(document(), data);
