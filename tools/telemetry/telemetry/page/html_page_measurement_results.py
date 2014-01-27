91cbb9fff0ff80201d6199b27557f3f9a1c046c7
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-27 02:48:29 +0000

    Re-Reland: [Telemetry] Properly handle retries after browser crash.
    Previously it was easy for the results to get into a bad state and assert in a
    variety of ways.
    
    This ensures we ignore the partial results in the case of a crash.
    
    This reland is tested with GPU functional tests. The main fix was to make all
    result types shallow copyable.
    
    BUG=
    TBR=tonyg@chromium.org
    
    Review URL: https://codereview.chromium.org/147853002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247184 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/page/html_page_measurement_results.py
+++ tools/telemetry/telemetry/page/html_page_measurement_results.py
@@ -8,6 +8,7 @@ import json
 import logging
 import os
 import re
+import sys
 
 from telemetry.core import util
 from telemetry.page import buildbot_page_measurement_results
@@ -31,12 +32,11 @@ class HtmlPageMeasurementResults(
     buildbot_page_measurement_results.BuildbotPageMeasurementResults):
   def __init__(self, output_stream, test_name, reset_results, upload_results,
       browser_type, results_label=None, trace_tag=''):
-    super(HtmlPageMeasurementResults, self).__init__(trace_tag)
-
-    self._output_stream = output_stream
+    super(HtmlPageMeasurementResults, self).__init__(sys.stdout, trace_tag)
     self._test_name = test_name
     self._reset_results = reset_results
     self._upload_results = upload_results
+    self._html_output_stream = output_stream
     self._existing_results = self._ReadExistingResults(output_stream)
     self._result = {
         'buildTime': self._GetBuildTime(),
@@ -79,9 +79,9 @@ class HtmlPageMeasurementResults(
     return json.loads(m.group(1))[:512]
 
   def _SaveResults(self, results):
-    self._output_stream.seek(0)
-    self._output_stream.write(results)
-    self._output_stream.truncate()
+    self._html_output_stream.seek(0)
+    self._html_output_stream.write(results)
+    self._html_output_stream.truncate()
 
   def _PrintPerfResult(self, measurement, trace, values, units,
                        result_type='default'):
@@ -116,7 +116,7 @@ class HtmlPageMeasurementResults(
     self._SaveResults(html)
 
     if self._upload_results:
-      file_path = os.path.abspath(self._output_stream.name)
+      file_path = os.path.abspath(self._html_output_stream.name)
       file_name = 'html-results/results-%s' % datetime.datetime.now().strftime(
           '%Y-%m-%d_%H-%M-%S')
       cloud_storage.Insert(cloud_storage.PUBLIC_BUCKET, file_name, file_path)
@@ -124,4 +124,5 @@ class HtmlPageMeasurementResults(
       print ('View online at '
           'http://storage.googleapis.com/chromium-telemetry/%s' % file_name)
     print
-    print 'View result at file://%s' % os.path.abspath(self._output_stream.name)
+    print 'View result at file://%s' % os.path.abspath(
+        self._html_output_stream.name)
