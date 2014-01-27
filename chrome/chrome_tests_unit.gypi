df678149f1ff92e3a1d9008add9877aab75e9f64
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-27 21:34:28 +0000

    Add support for user-level Native Messaging hosts installation.
    With this change Native Messaging hosts can be installed into user's
    directory without requiring admin permissions
    
    BUG=237873
    R=asvitkine@chromium.org, dconnelly@chromium.org, jschuh@chromium.org, kalman@chromium.org
    TBR=mattm@chromium.org
    
    Review URL: https://codereview.chromium.org/140583002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247299 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/chrome_tests_unit.gypi
+++ chrome/chrome_tests_unit.gypi
@@ -382,6 +382,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         ['OS=="android"', {
           'sources/': [
             ['exclude', '^browser/media_galleries/'],
+            ['exclude', '^browser/extensions/api/messaging/native_messaging_'],
           ],
         }],
         ['OS=="win"', {
--- chrome/common/extensions/docs/examples/api/nativeMessaging/host/uninstall_host.sh
+++ chrome/common/extensions/docs/examples/api/nativeMessaging/host/uninstall_host.sh
@@ -7,9 +7,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 set -e
 
 if [ $(uname -s) == 'Darwin' ]; then
-  TARGET_DIR='/Library/Google/Chrome/NativeMessagingHosts'
+  if [ "$(whoami)" == "root" ]; then
+    TARGET_DIR="/Library/Google/Chrome/NativeMessagingHosts"
+  else
+    TARGET_DIR=\
+        "$HOME/Library/Application Support/Google/Chrome/NativeMessagingHosts"
+  fi
 else
-  TARGET_DIR='/etc/opt/chrome/native-messaging-hosts'
+  if [ "$(whoami)" == "root" ]; then
+    TARGET_DIR="/etc/opt/chrome/native-messaging-hosts"
+  else
+    TARGET_DIR='$HOME/.config/google-chrome/NativeMessagingHosts'
+  fi
 fi
 
 HOST_NAME=com.google.chrome.example.echo
