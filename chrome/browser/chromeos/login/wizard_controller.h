3420838664190cc65bbc977b5f56758ac5aa506d
ygorshenin@chromium.org <ygorshenin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-27 13:21:24 +0000

    Audio is unmuted iff ChromeVox is enabled during OOBE.
    BUG=328187
    TEST=browser_tests:WizardControllerTest.Volume*
    
    Review URL: https://codereview.chromium.org/138903010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247223 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/wizard_controller.h
+++ chrome/browser/chromeos/login/wizard_controller.h
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/timer/timer.h"
 #include "chrome/browser/chromeos/login/screens/screen_observer.h"
 #include "chrome/browser/chromeos/login/screens/wizard_screen.h"
+#include "content/public/browser/notification_observer.h"
+#include "content/public/browser/notification_registrar.h"
 #include "ui/gfx/rect.h"
 #include "url/gurl.h"
 
@@ -48,7 +50,8 @@ class WrongHWIDScreen;
 
 // Class that manages control flow between wizard screens. Wizard controller
 // interacts with screen controllers to move the user between screens.
-class WizardController : public ScreenObserver {
+class WizardController : public ScreenObserver,
+                         public content::NotificationObserver {
  public:
   // Observes screen changes.
   class Observer {
@@ -154,6 +157,9 @@ class WizardController : public ScreenObserver {
   static const char kLocallyManagedUserCreationScreenName[];
   static const char kAppLaunchSplashScreenName[];
 
+  // Volume percent at which spoken feedback is still audible.
+  static const int kMinAudibleOutputVolumePercent;
+
  private:
   // Show specific screen.
   void ShowNetworkScreen();
@@ -221,6 +227,11 @@ class WizardController : public ScreenObserver {
   virtual ErrorScreen* GetErrorScreen() OVERRIDE;
   virtual void ShowErrorScreen() OVERRIDE;
   virtual void HideErrorScreen(WizardScreen* parent_screen) OVERRIDE;
+
+  // Overridden from content::NotificationObserver:
+  virtual void Observe(int type,
+                       const content::NotificationSource& source,
+                       const content::NotificationDetails& details) OVERRIDE;
 
   // Switches from one screen to another.
   void SetCurrentScreen(WizardScreen* screen);
@@ -333,6 +344,8 @@ class WizardController : public ScreenObserver {
   friend class WizardControllerBrokenLocalStateTest;
 
   base::WeakPtrFactory<WizardController> weak_factory_;
+
+  content::NotificationRegistrar registrar_;
 
   DISALLOW_COPY_AND_ASSIGN(WizardController);
 };
--- chrome/browser/chromeos/login/wizard_controller_browsertest.cc
+++ chrome/browser/chromeos/login/wizard_controller_browsertest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/browser_shutdown.h"
 #include "chrome/browser/chrome_notification_types.h"
+#include "chrome/browser/chromeos/accessibility/accessibility_manager.h"
 #include "chrome/browser/chromeos/base/locale_util.h"
 #include "chrome/browser/chromeos/login/enrollment/enrollment_screen.h"
 #include "chrome/browser/chromeos/login/enrollment/mock_enrollment_screen.h"
@@ -36,11 +37,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/webui_login_view.h"
 #include "chrome/browser/chromeos/login/wizard_controller.h"
 #include "chrome/browser/chromeos/login/wizard_in_process_browser_test.h"
+#include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/ui/webui/chromeos/login/oobe_ui.h"
 #include "chrome/browser/ui/webui/chromeos/login/signin_screen_handler.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/test/base/ui_test_utils.h"
+#include "chromeos/audio/cras_audio_handler.h"
 #include "chromeos/chromeos_switches.h"
 #include "chromeos/chromeos_test_utils.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
@@ -120,6 +123,21 @@ void RunSwitchLanguageTest(const std::string& locale,
   EXPECT_EQ(data.success, expect_success);
 }
 
+void SetUpCrasAndEnableChromeVox(int volume_percent, bool mute_on) {
+  AccessibilityManager* a11y = AccessibilityManager::Get();
+  CrasAudioHandler* cras = CrasAudioHandler::Get();
+
+  // Audio output is at |volume_percent| and |mute_on|. Spoken feedback
+  // is disabled.
+  cras->SetOutputVolumePercent(volume_percent);
+  cras->SetOutputMute(mute_on);
+  a11y->EnableSpokenFeedback(false, ash::A11Y_NOTIFICATION_NONE);
+
+  // Spoken feedback is enabled.
+  a11y->EnableSpokenFeedback(true, ash::A11Y_NOTIFICATION_NONE);
+  base::RunLoop().RunUntilIdle();
+}
+
 }  // namespace
 
 using ::testing::_;
@@ -152,6 +170,12 @@ class WizardControllerTest : public WizardInProcessBrowserTest {
   WizardControllerTest() : WizardInProcessBrowserTest(
       WizardController::kTestNoScreenName) {}
   virtual ~WizardControllerTest() {}
+
+  virtual void SetUpOnMainThread() OVERRIDE {
+    AccessibilityManager::Get()->
+        SetProfileForTest(ProfileHelper::GetSigninProfile());
+    WizardInProcessBrowserTest::SetUpOnMainThread();
+  }
 
  private:
   DISALLOW_COPY_AND_ASSIGN(WizardControllerTest);
@@ -186,6 +210,35 @@ IN_PROC_BROWSER_TEST_F(WizardControllerTest, SwitchLanguage) {
       base::UTF16ToWide(l10n_util::GetStringUTF16(IDS_NETWORK_SELECTION_TITLE));
 
   EXPECT_NE(fr_str, ar_str);
+}
+
+IN_PROC_BROWSER_TEST_F(WizardControllerTest, VolumeIsChangedForChromeVox) {
+  SetUpCrasAndEnableChromeVox(75 /* volume_percent */, true /* mute_on */);
+
+  // Check that output is unmuted now and at some level.
+  CrasAudioHandler* cras = CrasAudioHandler::Get();
+  ASSERT_FALSE(cras->IsOutputMuted());
+  ASSERT_EQ(WizardController::kMinAudibleOutputVolumePercent,
+            cras->GetOutputVolumePercent());
+}
+
+IN_PROC_BROWSER_TEST_F(WizardControllerTest, VolumeIsUnchangedForChromeVox) {
+  SetUpCrasAndEnableChromeVox(75 /* volume_percent */, false /* mute_on */);
+
+  // Check that output is unmuted now and at some level.
+  CrasAudioHandler* cras = CrasAudioHandler::Get();
+  ASSERT_FALSE(cras->IsOutputMuted());
+  ASSERT_EQ(75, cras->GetOutputVolumePercent());
+}
+
+IN_PROC_BROWSER_TEST_F(WizardControllerTest, VolumeIsAdjustedForChromeVox) {
+  SetUpCrasAndEnableChromeVox(5 /* volume_percent */, false /* mute_on */);
+
+  // Check that output is unmuted now and at some level.
+  CrasAudioHandler* cras = CrasAudioHandler::Get();
+  ASSERT_FALSE(cras->IsOutputMuted());
+  ASSERT_EQ(WizardController::kMinAudibleOutputVolumePercent,
+            cras->GetOutputVolumePercent());
 }
 
 class WizardControllerFlowTest : public WizardControllerTest {
