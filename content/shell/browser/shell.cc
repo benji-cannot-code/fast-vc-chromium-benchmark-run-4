129a27fed8269fa0a9843a1977739ee1f57f64d3
dpranke@chromium.org <dpranke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-27 19:32:26 +0000

    Re-land r246921: "Allow running content-shell headless with --dump-render-tree."
    This change reverts r247148 and re-lands r246921. It's unclear if there
    really is something wrong with this patch, and the crashes in
    run-webkit-tests were addressed in Blink r165816.
    
    TBR=jochen@chromium.org, sadrul@chromium.org
    BUG=316712
    
    Review URL: https://codereview.chromium.org/132903012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247278 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/browser/shell.cc
+++ content/shell/browser/shell.cc
@@ -30,10 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/shell/common/shell_messages.h"
 #include "content/shell/common/shell_switches.h"
 
-#if defined(USE_AURA) && !defined(TOOLKIT_VIEWS)
-#include "content/shell/browser/shell_aura.h"
-#endif
-
 namespace content {
 
 const int Shell::kDefaultTestWindowWidthDip = 800;
