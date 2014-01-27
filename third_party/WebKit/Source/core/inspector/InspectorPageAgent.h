2e06ec70db76e5fb161ee1ac24b8aebb364e3196
jochen@chromium.org <jochen@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-27 17:03:43 +0000

    Only notify the inspector code when the main world window object was cleared
    It just bails out early anyways.
    
    BUG=none
    R=pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/137883004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165863 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorPageAgent.h
+++ third_party/WebKit/Source/core/inspector/InspectorPageAgent.h
@@ -127,7 +127,7 @@ public:
     float overrideFontScaleFactor(float);
 
     // InspectorInstrumentation API
-    void didClearWindowObjectInWorld(Frame*, DOMWrapperWorld*);
+    void didClearWindowObjectInMainWorld(Frame*);
     void domContentLoadedEventFired(Frame*);
     void loadEventFired(Frame*);
     void didCommitLoad(Frame*, DocumentLoader*);
