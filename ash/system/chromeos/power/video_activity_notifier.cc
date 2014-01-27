f07008a148daa7c1e2da72342a6f612578e45a1e
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-27 22:28:44 +0000

    chromeos: Don't report video while screen is locked.
    Avoid reporting video activity to powerd while the screen is
    locked.
    
    BUG=338392
    
    Review URL: https://codereview.chromium.org/140973004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247324 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/chromeos/power/video_activity_notifier.cc
+++ ash/system/chromeos/power/video_activity_notifier.cc
@@ -20,15 +20,21 @@ const int kNotifyIntervalSec = 5;
 }  // namespace
 
 VideoActivityNotifier::VideoActivityNotifier(VideoDetector* detector)
-    : detector_(detector) {
+    : detector_(detector),
+      screen_is_locked_(false) {
   detector_->AddObserver(this);
+  ash::Shell::GetInstance()->AddShellObserver(this);
 }
 
 VideoActivityNotifier::~VideoActivityNotifier() {
+  ash::Shell::GetInstance()->RemoveShellObserver(this);
   detector_->RemoveObserver(this);
 }
 
 void VideoActivityNotifier::OnVideoDetected(bool is_fullscreen) {
+  if (screen_is_locked_)
+    return;
+
   base::TimeTicks now = base::TimeTicks::Now();
   if (last_notify_time_.is_null() ||
       (now - last_notify_time_).InSeconds() >= kNotifyIntervalSec) {
@@ -36,6 +42,10 @@ void VideoActivityNotifier::OnVideoDetected(bool is_fullscreen) {
         NotifyVideoActivity(is_fullscreen);
     last_notify_time_ = now;
   }
+}
+
+void VideoActivityNotifier::OnLockStateChanged(bool locked) {
+  screen_is_locked_ = locked;
 }
 
 }  // namespace internal
