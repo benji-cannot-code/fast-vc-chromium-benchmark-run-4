6f9d988acc60e86456124226f469bdf47c00fb36
dgozman@chromium.org <dgozman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-27 12:47:26 +0000

    [DevTools] Strenghten up backward compatibility for remote frontends.
    This change moves installing convenient API atop of
    InspectorFrontendHost.sendMessageToEmbedder to InspectorFrontendClientImpl
    to maintain backward-compatibility architecture.
    
    If we do the same in javascript, any compatibility stuff may be
    overridden in javascript later.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/136463005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165850 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/InspectorFrontendHostStub.js
+++ third_party/WebKit/Source/devtools/front_end/InspectorFrontendHostStub.js
@@ -29,25 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-/**
- * @param {string} methodName
- */
-function dispatchMethodByName(methodName)
-{
-    var callId = ++lastCallId;
-    var argsArray = Array.prototype.slice.call(arguments, 1);
-    var callback = argsArray[argsArray.length - 1];
-    if (typeof callback === "function") {
-        argsArray.pop();
-        InspectorFrontendHost._callbacks[callId] = callback;
-    }
-
-    var message = { "id": callId, "method": methodName };
-    if (argsArray.length)
-        message.params = argsArray;
-    InspectorFrontendHost.sendMessageToEmbedder(JSON.stringify(message));
-}
-
 if (!window.InspectorFrontendHost) {
 
 /**
@@ -129,14 +110,6 @@ WebInspector.InspectorFrontendHostStub.prototype = {
     },
 
     setInjectedScriptForOrigin: function(origin, script)
-    {
-    },
-
-    loaded: function()
-    {
-    },
-
-    localizedStringsURL: function()
     {
     },
 
@@ -255,40 +228,4 @@ WebInspector.InspectorFrontendHostStub.prototype = {
 
 InspectorFrontendHost = new WebInspector.InspectorFrontendHostStub();
 
-} else if (InspectorFrontendHost.sendMessageToEmbedder) {
-  // Install message-based handlers with callbacks.
-    var lastCallId = 0;
-    InspectorFrontendHost._callbacks = [];
-
-    /**
-     * @param {number} id
-     * @param {?string} error
-     */
-    InspectorFrontendHost.embedderMessageAck = function(id, error)
-    {
-        var callback = InspectorFrontendHost._callbacks[id];
-        delete InspectorFrontendHost._callbacks[id];
-        if (callback)
-            callback(error);
-    }
-
-    var methodList = [
-        "addFileSystem",
-        "append",
-        "bringToFront",
-        "closeWindow",
-        "indexPath",
-        "moveWindowBy",
-        "openInNewTab",
-        "removeFileSystem",
-        "requestFileSystems",
-        "save",
-        "searchInPath",
-        "setContentsInsets",
-        "setIsDocked",
-        "stopIndexing"
-    ];
-
-    for (var i = 0; i < methodList.length; ++i)
-        InspectorFrontendHost[methodList[i]] = dispatchMethodByName.bind(null, methodList[i]);
 }
