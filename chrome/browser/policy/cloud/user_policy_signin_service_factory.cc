5ba92c2d2315437828292661bec5eb6f3c728f9a
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-27 15:39:00 +0000

    Added a UserPolicySigninService for iOS.
    This is just a frontend to UserPolicySigninServiceBase that uses blocks
    instead of base::Callbacks, for ease of integration into the Chrome iOS UI.
    
    BUG=275292
    R=dconnelly@chromium.org
    
    Review URL: https://codereview.chromium.org/146833009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247241 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/cloud/user_policy_signin_service_factory.cc
+++ chrome/browser/policy/cloud/user_policy_signin_service_factory.cc
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_ANDROID)
 #include "chrome/browser/policy/cloud/user_policy_signin_service_android.h"
+#elif defined(OS_IOS)
+#include "chrome/browser/policy/cloud/user_policy_signin_service_ios.h"
 #else
 #include "chrome/browser/policy/cloud/user_policy_signin_service.h"
 #endif
