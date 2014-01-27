429016de367d2ae25e76d203279b5feefa8a1044
ccameron@chromium.org <ccameron@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-27 22:29:54 +0000

    Fix setPatternPhase to work with squashed layers.
    The computation for setPatternPhase does not work if the NSView renders
    into the CALayer of an ancestor of the NSView. Change the computation
    to look up through the NSView hierarchy for a view that has a layer, and
    compute the new phase using the coordinate system of that layer.
    
    This is updating a fix that was originally made in r206004 to support
    the squashed layers enabled in r247098.
    
    BUG=245900
    
    Review URL: https://codereview.chromium.org/148243002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247326 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/cocoa/nsgraphics_context_additions.mm
+++ ui/base/cocoa/nsgraphics_context_additions.mm
@@ -9,11 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)cr_setPatternPhase:(NSPoint)phase
                    forView:(NSView*)view {
-  if ([view layer]) {
+  NSView* ancestorWithLayer = view;
+  while (ancestorWithLayer && ![ancestorWithLayer layer])
+    ancestorWithLayer = [ancestorWithLayer superview];
+  if (ancestorWithLayer) {
     NSPoint bottomLeft = NSZeroPoint;
-    if ([view isFlipped])
-      bottomLeft.y = NSMaxY([view bounds]);
-    NSPoint offset = [view convertPoint:bottomLeft toView:nil];
+    if ([ancestorWithLayer isFlipped])
+      bottomLeft.y = NSMaxY([ancestorWithLayer bounds]);
+    NSPoint offset = [ancestorWithLayer convertPoint:bottomLeft toView:nil];
     phase.x -= offset.x;
     phase.y -= offset.y;
   }
