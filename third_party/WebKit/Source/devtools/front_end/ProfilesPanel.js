c2443e026ff346a2ec834f97e04a7c07690e81ed
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-28 14:36:14 +0000

    Restore record button state on profiles panel reset
    BUG=338652
    R=loislo@chromium.org
    
    Review URL: https://codereview.chromium.org/134753009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165937 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ProfilesPanel.js
+++ third_party/WebKit/Source/devtools/front_end/ProfilesPanel.js
@@ -221,6 +221,10 @@ WebInspector.ProfileType.prototype = {
         return this._profileBeingRecorded;
     },
 
+    profileBeingRecordedRemoved: function()
+    {
+    },
+
     _reset: function()
     {
         var profiles = this._profiles.slice(0);
@@ -237,8 +241,10 @@ WebInspector.ProfileType.prototype = {
     {
         this.dispatchEventToListeners(WebInspector.ProfileType.Events.RemoveProfileHeader, profile);
         profile.dispose();
-        if (this._profileBeingRecorded === profile)
+        if (this._profileBeingRecorded === profile) {
+            this.profileBeingRecordedRemoved();
             this._profileBeingRecorded = null;
+        }
     },
 
     __proto__: WebInspector.Object.prototype
