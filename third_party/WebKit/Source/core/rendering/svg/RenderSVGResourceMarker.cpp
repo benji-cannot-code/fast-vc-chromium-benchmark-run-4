1ba3c81aed438f410423b2644f18b36c6f1062d4
fmalita@chromium.org <fmalita@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-28 02:40:26 +0000

    [SVG] RenderSVGResourceMarker::layout() should not invalidate dependents
    r160730 updated the resource-dependent client invalidation logic to
    occur at mutation time, but it missed the layout() override in
    RenderSVGResourceMarker - which is still invoking
    removeAllClientsFromCache() and may leave nodes in an invalide state at
    the end of layout.
    
    The CL aligns RenderSVGResourceMarker's layout() implementation with
    the other SVG resources (note that SVGMarkerElement's attribute listener
    is already handling the invalidation).
    
    BUG=338461
    R=pdr@chromium.org,schenney@chromium.org
    
    Review URL: https://codereview.chromium.org/147903003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165901 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/svg/RenderSVGResourceMarker.cpp
+++ third_party/WebKit/Source/core/rendering/svg/RenderSVGResourceMarker.cpp
@@ -53,10 +53,6 @@ void RenderSVGResourceMarker::layout()
     LayoutRectRecorder recorder(*this);
     TemporaryChange<bool> inLayoutChange(m_isInLayout, true);
 
-    // Invalidate all resources if our layout changed.
-    if (everHadLayout() && selfNeedsLayout())
-        removeAllClientsFromCache();
-
     // RenderSVGHiddenContainer overwrites layout(). We need the
     // layouting of RenderSVGContainer for calculating  local
     // transformations and repaint.
