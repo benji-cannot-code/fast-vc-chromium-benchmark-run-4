1127b05b94be870a6abdd7af2dd3675fa8be0b6e
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-28 12:22:56 +0000

    Send HeapProfiler.enable only when profiles panel is shown
    Otherwise on cross-process navigation we get following console error:
    
    [22766:22766:0127/193003:ERROR:CONSOLE(237)] "Protocol Error: the message HeapProfiler.resetProfiles is for non-existing domain 'HeapProfiler'", source: chrome-devtools://devtools/bundled/InspectorBackend.js (237)
    
    BUG=None
    R=alph@chromium.org
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=165858
    
    Review URL: https://codereview.chromium.org/132903010
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165930 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorHeapProfilerAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorHeapProfilerAgent.cpp
@@ -111,7 +111,8 @@ void InspectorHeapProfilerAgent::clearFrontend()
 
 void InspectorHeapProfilerAgent::restore()
 {
-    resetFrontendProfiles();
+    if (m_state->getBoolean(HeapProfilerAgentState::heapProfilerEnabled))
+        resetFrontendProfiles();
 }
 
 void InspectorHeapProfilerAgent::collectGarbage(WebCore::ErrorString*)
