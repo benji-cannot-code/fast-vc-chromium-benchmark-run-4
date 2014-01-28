7d575ad465219215b01cb78dec1dc890921fa071
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-28 23:57:01 +0000

    Use thread.PushSlice instead of Begin/EndSice to reduce timeline flake
    Telemetry's thread logic can actually handle slices even when they're added to
    the thread in arbitarary timestamp order. The BeginSlice/EndSlice calls are
    merely a helper. Since the timeline stream coming out of inspector actually has
    begin and end timestamps, just create slices and push them to the thread: this
    avoids flake from ordering issues rather than adding a hard dependency on
    timeline for the timestamp ordering of its data stream.
    
    BUG=321529
    
    Review URL: https://codereview.chromium.org/137143013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247532 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/backends/chrome/inspector_timeline_unittest.py
+++ tools/telemetry/telemetry/core/backends/chrome/inspector_timeline_unittest.py
@@ -3,13 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import platform
-import sys
-import unittest
-
 from telemetry.core import util
 from telemetry.core.backends.chrome import inspector_timeline
-from telemetry.unittest import DisabledTest, tab_test_case
+from telemetry.unittest import tab_test_case
 
 
 class InspectorTimelineTabTest(tab_test_case.TabTestCase):
@@ -21,14 +17,7 @@ class InspectorTimelineTabTest(tab_test_case.TabTestCase):
       return bool(self._tab.EvaluateJavaScript('window.done'))
     util.WaitFor(_IsDone, 5)
 
-  # Flaky on XP+Vista: crbug.com/321529
-  @DisabledTest
   def testGotTimeline(self):
-    if sys.platform in ('win32', 'cygwin'):
-      if platform.win32_ver()[0] == 'XP':
-        raise unittest.SkipTest(
-            'Test flaky on Windows XP. http://crbug.com/321529')
-
     # While the timeline is recording, call window.webkitRequestAnimationFrame.
     # This will create a FireAnimationEvent, which can be checked below. See:
     # https://developer.mozilla.org/en/docs/Web/API/window.requestAnimationFrame
--- tools/telemetry/telemetry/core/timeline/inspector_importer.py
+++ tools/telemetry/telemetry/core/timeline/inspector_importer.py
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 '''Imports event data obtained from the inspector's timeline.'''
 
 from telemetry.core.timeline import importer
+
 import telemetry.core.timeline.thread as timeline_thread
+import telemetry.core.timeline.slice as tracing_slice
 
 class InspectorTimelineImporter(importer.TimelineImporter):
   def __init__(self, model, event_data):
@@ -33,7 +35,7 @@ class InspectorTimelineImporter(importer.TimelineImporter):
 
   @staticmethod
   def AddRawEventToThreadRecursive(thread, raw_inspector_event):
-    did_begin_slice = False
+    pending_slice = None
     if ('startTime' in raw_inspector_event and
         'type' in raw_inspector_event):
       args = {}
@@ -45,18 +47,21 @@ class InspectorTimelineImporter(importer.TimelineImporter):
         args = None
       start_time = raw_inspector_event['startTime']
       end_time = raw_inspector_event.get('endTime', start_time)
-      thread.BeginSlice('inspector',
-                        raw_inspector_event['type'],
-                        start_time,
-                        args=args)
-      did_begin_slice = True
+
+      pending_slice = tracing_slice.Slice(
+        thread, 'inspector',
+        raw_inspector_event['type'],
+        start_time,
+        thread_timestamp=None,
+        args=args)
 
     for child in raw_inspector_event.get('children', []):
       InspectorTimelineImporter.AddRawEventToThreadRecursive(
           thread, child)
 
-    if did_begin_slice:
-      thread.EndSlice(end_time)
+    if pending_slice:
+      pending_slice.duration = end_time - pending_slice.start
+      thread.PushSlice(pending_slice)
 
   @staticmethod
   def RawEventToTimelineEvent(raw_inspector_event):
