79b6a39887623af0a6a100776b35f038fde3ea38
yukishiino@chromium.org <yukishiino@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-28 23:47:56 +0000

    Clean-up: Changes DialogOverlayString::font to font_list of type FontList.
    As part of effort to support multiple fonts, this CL changes methods which are now taking gfx::Font so they will take gfx::FontList instead.
    
    See https://docs.google.com/a/chromium.org/document/d/1D_25fp9B8b9aZJORfAjDIFq61NWvUquZ5xmKH-VcC4k/view
    
    BUG=265485
    TEST=Run unit_tests, ui_unittests, views_unittests.
    
    Review URL: https://codereview.chromium.org/135593003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247527 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/autofill/autofill_dialog_types.h
+++ chrome/browser/ui/autofill/autofill_dialog_types.h
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/autofill_metrics.h"
 #include "components/autofill/core/browser/field_types.h"
 #include "third_party/skia/include/core/SkColor.h"
-#include "ui/gfx/font.h"
+#include "ui/gfx/font_list.h"
 #include "ui/gfx/image/image.h"
 #include "ui/gfx/range/range.h"
 #include "ui/gfx/text_constants.h"
@@ -174,8 +174,8 @@ struct DialogOverlayString {
   // Text content of the message.
   base::string16 text;
 
-  // Font to render the message's text in.
-  gfx::Font font;
+  // Font list to render the message's text in.
+  gfx::FontList font_list;
 };
 
 // A struct to describe a dialog overlay. If |image| is empty, no overlay should
