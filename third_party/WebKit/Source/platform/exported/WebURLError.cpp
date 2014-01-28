b59601a8b6a2c2160bc3dc4aad450908f425a970
rdsmith@chromium.org <rdsmith@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-28 23:02:35 +0000

    Added field to error structures to signal existence of a stale copy in the cache, for use by the navigation source.
    BUG=329620
    
    Review URL: https://codereview.chromium.org/138493002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165973 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/exported/WebURLError.cpp
+++ third_party/WebKit/Source/platform/exported/WebURLError.cpp
@@ -54,6 +54,7 @@ WebURLError& WebURLError::operator=(const ResourceError& error)
         reason = error.errorCode();
         unreachableURL = KURL(ParsedURLString, error.failingURL());
         isCancellation = error.isCancellation();
+        staleCopyInCache = error.staleCopyInCache();
         localizedDescription = error.localizedDescription();
     }
     return *this;
@@ -66,6 +67,7 @@ WebURLError::operator ResourceError() const
     CString spec = unreachableURL.spec();
     ResourceError resourceError = ResourceError(domain, reason, String::fromUTF8(spec.data(), spec.length()), localizedDescription);
     resourceError.setIsCancellation(isCancellation);
+    resourceError.setStaleCopyInCache(staleCopyInCache);
     return resourceError;
 }
 
