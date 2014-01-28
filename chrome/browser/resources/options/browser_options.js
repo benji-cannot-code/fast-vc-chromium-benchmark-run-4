38f81bca30bfc844f01db60334e84ba3e2f5c999
tengs@chromium.org <tengs@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-28 21:20:16 +0000

    Enable sticky keys by default for launch in M34.
    BUG=256946
    TEST=manually checked chrome://settings for sticky keys checkbox
    
    Review URL: https://codereview.chromium.org/148163005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247506 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/browser_options.js
+++ chrome/browser/resources/options/browser_options.js
@@ -458,9 +458,6 @@ cr.define('options', function() {
         Preferences.getInstance().addEventListener(
             $('accessibility-autoclick-check').getAttribute('pref'),
             updateDelayDropdown);
-
-        $('accessibility-sticky-keys').hidden =
-            !loadTimeData.getBoolean('enableStickyKeys');
       }
 
       // Display management section (CrOS only).
