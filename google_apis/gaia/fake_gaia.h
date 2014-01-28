1a6b87503687b629c05a55716c23d55703adf560
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-28 16:15:04 +0000

    Re-landing https://codereview.chromium.org/118733002:
    Added merge session request throttle for XHR requests.
    
    This is the second attempt which should also have fix for
    regression that the original CL from r242135 have caused
    (see http://crbug.com/333046 for details).
    
    BUG=308318,309131,308681,333046
    TEST=manual, added MergeSessionTest.MergeSessionThrottle
    R=japhet@chromium.org
    TBR=sky, xiyuan
    
    Review URL: https://codereview.chromium.org/146953003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247454 0039d316-1c4b-4281-b951-d872f2087c98

--- google_apis/gaia/fake_gaia.h
+++ google_apis/gaia/fake_gaia.h
@@ -78,7 +78,7 @@ class FakeGaia {
   };
 
   FakeGaia();
-  ~FakeGaia();
+  virtual ~FakeGaia();
 
   // Sets the initial value of tokens and cookies.
   void SetMergeSessionParams(const MergeSessionParams& params);
@@ -110,6 +110,11 @@ class FakeGaia {
   static bool GetQueryParameter(const std::string& query,
                                 const std::string& key,
                                 std::string* value);
+ protected:
+  // HTTP handler for /MergeSession.
+  virtual void HandleMergeSession(
+      const net::test_server::HttpRequest& request,
+      net::test_server::BasicHttpResponse* http_response);
 
  private:
   typedef std::multimap<std::string, AccessTokenInfo> AccessTokenInfoMap;
@@ -133,8 +138,6 @@ class FakeGaia {
                           net::test_server::BasicHttpResponse* http_response);
   void HandleOAuthLogin(const net::test_server::HttpRequest& request,
                         net::test_server::BasicHttpResponse* http_response);
-  void HandleMergeSession(const net::test_server::HttpRequest& request,
-                          net::test_server::BasicHttpResponse* http_response);
   void HandleServiceLoginAuth(
       const net::test_server::HttpRequest& request,
       net::test_server::BasicHttpResponse* http_response);
