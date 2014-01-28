0f218fa9afdce0022e6d474cb42c07856684683c
rogerta@chromium.org <rogerta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-28 23:47:25 +0000

    The google accounts connected to a profile should remain consistent with the accounts in the cookie jar.
    BUG=305249
    
    Review URL: https://codereview.chromium.org/134633007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247526 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/sync_setup_handler.cc
+++ chrome/browser/ui/webui/sync_setup_handler.cc
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_info_cache.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/profiles/profile_metrics.h"
+#include "chrome/browser/signin/profile_oauth2_token_service.h"
+#include "chrome/browser/signin/profile_oauth2_token_service_factory.h"
 #include "chrome/browser/signin/signin_global_error.h"
 #include "chrome/browser/signin/signin_manager_factory.h"
 #include "chrome/browser/signin/signin_promo.h"
@@ -31,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/profile_sync_service_factory.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_navigator.h"
+#include "chrome/browser/ui/singleton_tabs.h"
 #include "chrome/browser/ui/sync/signin_histogram.h"
 #include "chrome/browser/ui/webui/signin/login_ui_service.h"
 #include "chrome/browser/ui/webui/signin/login_ui_service_factory.h"
@@ -489,8 +492,6 @@ void SyncSetupHandler::DisplayGaiaLogin() {
 }
 
 void SyncSetupHandler::DisplayGaiaLoginInNewTabOrWindow() {
-  GURL url(signin::GetPromoURL(signin::SOURCE_SETTINGS,
-                               true));  // auto close after success.
   Browser* browser = chrome::FindBrowserWithWebContents(
       web_ui()->GetWebContents());
   if (!browser) {
@@ -502,18 +503,25 @@ void SyncSetupHandler::DisplayGaiaLoginInNewTabOrWindow() {
   // If the signin manager already has an authenticated username, this is a
   // re-auth scenario, and we need to ensure that the user signs in with the
   // same email address.
+  GURL url;
   std::string email = SigninManagerFactory::GetForProfile(
       browser->profile())->GetAuthenticatedUsername();
   if (!email.empty()) {
     UMA_HISTOGRAM_ENUMERATION("Signin.Reauth",
                               signin::HISTOGRAM_SHOWN,
                               signin::HISTOGRAM_MAX);
-    url = net::AppendQueryParameter(url, "Email", email);
+
+    SigninGlobalError* signin_error =
+        ProfileOAuth2TokenServiceFactory::GetForProfile(browser->profile())->
+            signin_global_error();
+    DCHECK(signin_error->HasMenuItem());
+    url = signin::GetReauthURL(browser->profile(),
+                               signin_error->GetAccountIdOfLastAuthError());
+  } else {
+    url = signin::GetPromoURL(signin::SOURCE_SETTINGS, true);
   }
 
-  browser->OpenURL(
-      content::OpenURLParams(url, content::Referrer(), SINGLETON_TAB,
-                             content::PAGE_TRANSITION_AUTO_BOOKMARK, false));
+  chrome::ShowSingletonTab(browser, url);
 }
 #endif
 
