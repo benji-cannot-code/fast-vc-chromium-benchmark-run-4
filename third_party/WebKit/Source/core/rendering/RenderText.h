89bc771a844001423604c95d9a492a2ca5e5b1c7
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-28 01:22:30 +0000

    Change RenderText::trimmedPrefWidths to specify text direction
    Change the signature for RenderText::trimmedPrefWidths to require a
    direction argument and update the RenderBlock callsite to specify the
    desired direction.
    
    BUG=333004
    R=leviw@chromium.org
    
    Review URL: https://codereview.chromium.org/148693002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165898 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderText.h
+++ third_party/WebKit/Source/core/rendering/RenderText.h
@@ -96,7 +96,8 @@ public:
         float& lastLineMinWidth, bool& hasBreakableEnd,
         bool& hasBreakableChar, bool& hasBreak,
         float& firstLineMaxWidth, float& lastLineMaxWidth,
-        float& minWidth, float& maxWidth, bool& stripFrontSpaces);
+        float& minWidth, float& maxWidth, bool& stripFrontSpaces,
+        TextDirection);
 
     virtual IntRect linesBoundingBox() const;
     LayoutRect linesVisualOverflowBoundingBox() const;
