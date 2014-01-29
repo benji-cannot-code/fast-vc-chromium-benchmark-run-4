e3e3b42c06bc7f3738baf2d11b71f0de0c44e600
yhirano@chromium.org <yhirano@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-29 09:27:41 +0000

    [WebMIDI] Introduce MidiManagerUsb
    Introduce MidiManagerUsb, a MIDIManager subclass for USB-MIDI.
    Android port will use this class.
    
    BUG=303596
    R=toyoshim@chromium.org
    
    Review URL: https://codereview.chromium.org/107163008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247665 0039d316-1c4b-4281-b951-d872f2087c98

--- media/midi/usb_midi_input_stream.h
+++ media/midi/usb_midi_input_stream.h
@@ -23,7 +23,7 @@ class UsbMidiDevice;
 // Section 4 "USB-MIDI Event Packets" for details.
 class MEDIA_EXPORT UsbMidiInputStream {
  public:
-  class Delegate {
+  class MEDIA_EXPORT Delegate {
    public:
     virtual ~Delegate() {}
     // This function is called when some data arrives to a USB-MIDI jack.
@@ -32,6 +32,17 @@ class MEDIA_EXPORT UsbMidiInputStream {
                                 const uint8* data,
                                 size_t size,
                                 double timestamp) = 0;
+  };
+
+  // This is public for testing.
+  struct JackUniqueKey {
+    JackUniqueKey(UsbMidiDevice* device, int endpoint_number, int cable_number);
+    bool operator==(const JackUniqueKey& that) const;
+    bool operator<(const JackUniqueKey& that) const;
+
+    UsbMidiDevice* device;
+    int endpoint_number;
+    int cable_number;
   };
 
   UsbMidiInputStream(const std::vector<UsbMidiJack>& jacks,
@@ -48,18 +59,10 @@ class MEDIA_EXPORT UsbMidiInputStream {
                       size_t size,
                       double timestamp);
 
+  std::vector<JackUniqueKey> RegisteredJackKeysForTesting() const;
+
  private:
   static const size_t kPacketSize = 4;
-  struct JackUniqueKey {
-    JackUniqueKey(UsbMidiDevice* device, int endpoint_number, int cable_number);
-    bool operator==(const JackUniqueKey& that) const;
-    bool operator<(const JackUniqueKey& that) const;
-
-    UsbMidiDevice* device;
-    int endpoint_number;
-    int cable_number;
-  };
-
   // Processes a USB-MIDI Event Packet.
   // The first |kPacketSize| bytes of |packet| must be accessible.
   void ProcessOnePacket(UsbMidiDevice* device,
