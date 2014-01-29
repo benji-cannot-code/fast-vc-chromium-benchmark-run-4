842de5343abad4ae837d4f9c6f905ea3e8e053f3
nbarth@chromium.org <nbarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-29 02:38:22 +0000

    IDL compiler: basic named property getters
    Template a bit long b/c have enumerator and query as well.
    
    Minor Perl CG changes to make order regular (callback after function),
    and make code order match order of arguments in SetNamedPropertyHandler.
    
    Some Python refactoring, since the contents of indexed and named operations
    is the same: only the templates need to differ,
    so we can share substantial code!
    
    BUG=239771
    R=haraken
    
    Review URL: https://codereview.chromium.org/148813002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165989 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/scripts/code_generator_v8.pm
+++ third_party/WebKit/Source/bindings/scripts/code_generator_v8.pm
@@ -3831,11 +3831,13 @@ sub GenerateImplementationNamedPropertyAccessors
     if ($namedEnumeratorFunction) {
         my $hasCustomNamedEnumerator = $namedGetterFunction->extendedAttributes->{"CustomEnumerateProperty"};
         if (!$hasCustomNamedEnumerator) {
-            GenerateImplementationNamedPropertyEnumerator($interface);
             GenerateImplementationNamedPropertyQuery($interface);
         }
-        GenerateImplementationNamedPropertyEnumeratorCallback($interface, $hasCustomNamedEnumerator);
         GenerateImplementationNamedPropertyQueryCallback($interface, $hasCustomNamedEnumerator);
+        if (!$hasCustomNamedEnumerator) {
+            GenerateImplementationNamedPropertyEnumerator($interface);
+        }
+        GenerateImplementationNamedPropertyEnumeratorCallback($interface, $hasCustomNamedEnumerator);
     }
 
     my $subCode = "";
