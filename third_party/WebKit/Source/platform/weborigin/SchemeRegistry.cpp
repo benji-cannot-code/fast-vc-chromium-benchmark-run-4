2bd250df9fae055c16948f41265bdaf29a953a80
yhirano@chromium.org <yhirano@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-29 05:24:07 +0000

    Revert 165922 "Block WebSocket connection to insecure server fro..."
    The CL breaks some WebSocket applications and we want more time to announce.
    
    > Block WebSocket connection to insecure server from secure page.
    > 
    > The W3C WebSocket API specifies that the browser must throw an SecurityError
    > exception if the requested URL is insecure and the origin is secure.
    > 
    > BUG=85271
    > R=tyoshino, abarth
    > 
    > Review URL: https://codereview.chromium.org/143763002
    
    TBR=yhirano@chromium.org
    
    Review URL: https://codereview.chromium.org/147923009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165999 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/weborigin/SchemeRegistry.cpp
+++ third_party/WebKit/Source/platform/weborigin/SchemeRegistry.cpp
@@ -56,7 +56,6 @@ static URLSchemesMap& secureSchemes()
         secureSchemes.add("https");
         secureSchemes.add("about");
         secureSchemes.add("data");
-        secureSchemes.add("wss");
     }
 
     return secureSchemes;
