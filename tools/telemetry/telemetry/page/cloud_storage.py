7db5dfb6dbac5cf8da1995f9007f7fd58025f988
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-29 16:11:53 +0000

    [Telemetry] Detect whether sudo is required for certain admin-commands.
    BUG=337048
    NOTRY=True
    
    Review URL: https://codereview.chromium.org/137143011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247692 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/page/cloud_storage.py
+++ tools/telemetry/telemetry/page/cloud_storage.py
@@ -55,6 +55,7 @@ class NotFoundError(CloudStorageError):
   pass
 
 
+# TODO(tonyg/dtu): Can this be replaced with distutils.spawn.find_executable()?
 def _FindExecutableInPath(relative_executable_path, *extra_search_paths):
   for path in list(extra_search_paths) + os.environ['PATH'].split(os.pathsep):
     executable_path = os.path.join(path, relative_executable_path)
