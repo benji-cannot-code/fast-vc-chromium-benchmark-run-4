46582a65eb472ce653b5a57e787a36929b4fffc0
Hokein.Wu@gmail.com <Hokein.Wu@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-29 05:02:20 +0000

    [App Shell] Enable Initialized Logging
    BUG=None
    TEST=manual
    
    Review URL: https://codereview.chromium.org/141143010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247615 0039d316-1c4b-4281-b951-d872f2087c98

--- apps/shell/shell_main_delegate.cc
+++ apps/shell/shell_main_delegate.cc
@@ -14,6 +14,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_main_runner.h"
 #include "ui/base/resource/resource_bundle.h"
 
+namespace {
+
+void InitLogging() {
+  base::FilePath log_filename;
+  PathService::Get(base::DIR_EXE, &log_filename);
+  log_filename = log_filename.AppendASCII("app_shell.log");
+  logging::LoggingSettings settings;
+  settings.logging_dest = logging::LOG_TO_ALL;
+  settings.log_file = log_filename.value().c_str();
+  settings.delete_old = logging::DELETE_OLD_LOG_FILE;
+  logging::InitLogging(settings);
+  logging::SetLogItems(true, true, true, true);
+}
+
+}  // namespace
+
 namespace apps {
 
 ShellMainDelegate::ShellMainDelegate() {
@@ -23,7 +39,7 @@ ShellMainDelegate::~ShellMainDelegate() {
 }
 
 bool ShellMainDelegate::BasicStartupComplete(int* exit_code) {
-  // TODO(jamescook): Initialize logging here.
+  InitLogging();
   content_client_.reset(new ShellContentClient);
   SetContentClient(content_client_.get());
   return false;
