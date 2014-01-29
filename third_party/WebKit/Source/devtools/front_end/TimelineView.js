fdc67d8e83b987677c40f825d0ef3bcc11889a01
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-29 14:16:59 +0000

    TimelinePanel: move conversion from OverviewGrid window to windowTimes to TimelineOverviewPane
    This patch reduces the dependency between TimelineView and _overviewConroller
    which is an instance of TimelineOverviewBase successor.
    
    BUG=326121
    R=caseq@chromium.org
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=165933
    
    Review URL: https://codereview.chromium.org/148173004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166021 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/TimelineView.js
+++ third_party/WebKit/Source/devtools/front_end/TimelineView.js
@@ -447,8 +447,7 @@ WebInspector.TimelineView.prototype = {
 
     _updateWindowBoundaries: function()
     {
-        var windowBoundaries = this.overviewControl().windowBoundaries(this._windowStartTime, this._windowEndTime);
-        this._panel.setWindow(windowBoundaries.left, windowBoundaries.right);
+        this._panel.setWindowTimes(this._windowStartTime, this._windowEndTime);
     },
 
     /**
@@ -460,12 +459,10 @@ WebInspector.TimelineView.prototype = {
     },
 
     /**
-     * @param {?Object} windowTimes
+     * @param {!Object} windowTimes
      */
     setWindowTimes: function(windowTimes)
     {
-        if (!windowTimes)
-            return;
         this._setWindowTimes(windowTimes.windowStartTime, windowTimes.windowEndTime);
     },
 
@@ -478,8 +475,7 @@ WebInspector.TimelineView.prototype = {
         this._windowStartTime = startTime;
         this._windowEndTime = endTime;
         this._windowFilter.setWindowTimes(startTime, endTime);
-        var windowBoundaries = this.overviewControl().windowBoundaries(startTime, endTime);
-        this._panel.setWindow(windowBoundaries.left, windowBoundaries.right);
+        this._panel.setWindowTimes(startTime, endTime);
     },
 
     _repopulateRecords: function()
@@ -752,15 +748,14 @@ WebInspector.TimelineView.prototype = {
     },
 
     /**
-     * @param {number} left
-     * @param {number} right
+     * @param {number} startTime
+     * @param {number} endTime
      */
-    windowChanged: function(left, right)
+    windowTimesChanged: function(startTime, endTime)
     {
-        var windowTimes = this.overviewControl().windowTimes(left, right);
-        this._windowStartTime = windowTimes.startTime;
-        this._windowEndTime = windowTimes.endTime;
-        this._windowFilter.setWindowTimes(windowTimes.startTime, windowTimes.endTime);
+        this._windowStartTime = startTime;
+        this._windowEndTime = endTime;
+        this._windowFilter.setWindowTimes(startTime, endTime);
         this._invalidateAndScheduleRefresh(false, true);
         this._selectRecord(null);
     },
@@ -898,8 +893,6 @@ WebInspector.TimelineView.prototype = {
             this._setWindowTimes(windowStartTime, recordsInWindow[Math.max(0, lastVisibleLine - 1)].endTime);
             recordsInWindow = this._presentationModel.filteredRecords();
             endIndex = Math.min(recordsInWindow.length, lastVisibleLine);
-        } else {
-            this._updateWindowBoundaries();
         }
 
         // Resize gaps first.
