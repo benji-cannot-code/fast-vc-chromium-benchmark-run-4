f0256bc4a89df2a6b428d04e209d9434f486e115
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-30 06:14:26 +0000

    Move V8Utilities::xxxHiddenDependency to V8Binding.h
    The goal is to remove V8Utilities.h
    
    This CL also does the following rename:
    
    createHiddenDependency => addHiddenValueToArray
    removeHiddenDependency => removeHiddenValueFromArray
    transferHiddenDependency => moveEventListenerToNewWrapper (since this is a method specific to event listeners)
    
    BUG=339037
    
    Review URL: https://codereview.chromium.org/147843003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166086 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/V8Binding.cpp
+++ third_party/WebKit/Source/bindings/v8/V8Binding.cpp
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "V8WorkerGlobalScope.h"
 #include "V8XPathNSResolver.h"
 #include "bindings/v8/ScriptController.h"
+#include "bindings/v8/V8AbstractEventListener.h"
 #include "bindings/v8/V8BindingMacros.h"
 #include "bindings/v8/V8NodeFilterCondition.h"
 #include "bindings/v8/V8ObjectConstructor.h"
@@ -696,6 +697,48 @@ v8::Local<v8::Value> getHiddenValueFromMainWorldWrapper(v8::Isolate* isolate, Sc
 {
     v8::Local<v8::Object> wrapper = wrappable->newLocalWrapper(isolate);
     return wrapper.IsEmpty() ? v8::Local<v8::Value>() : getHiddenValue(isolate, wrapper, key);
+}
+
+void addHiddenValueToArray(v8::Handle<v8::Object> object, v8::Local<v8::Value> value, int arrayIndex, v8::Isolate* isolate)
+{
+    v8::Local<v8::Value> arrayValue = object->GetInternalField(arrayIndex);
+    if (arrayValue->IsNull() || arrayValue->IsUndefined()) {
+        arrayValue = v8::Array::New(isolate);
+        object->SetInternalField(arrayIndex, arrayValue);
+    }
+
+    v8::Local<v8::Array> array = v8::Local<v8::Array>::Cast(arrayValue);
+    array->Set(v8::Integer::New(isolate, array->Length()), value);
+}
+
+void removeHiddenValueFromArray(v8::Handle<v8::Object> object, v8::Local<v8::Value> value, int arrayIndex, v8::Isolate* isolate)
+{
+    v8::Local<v8::Value> arrayValue = object->GetInternalField(arrayIndex);
+    if (!arrayValue->IsArray())
+        return;
+    v8::Local<v8::Array> array = v8::Local<v8::Array>::Cast(arrayValue);
+    for (int i = array->Length() - 1; i >= 0; --i) {
+        v8::Local<v8::Value> item = array->Get(v8::Integer::New(isolate, i));
+        if (item->StrictEquals(value)) {
+            array->Delete(i);
+            return;
+        }
+    }
+}
+
+void moveEventListenerToNewWrapper(v8::Handle<v8::Object> object, EventListener* oldValue, v8::Local<v8::Value> newValue, int arrayIndex, v8::Isolate* isolate)
+{
+    if (oldValue) {
+        V8AbstractEventListener* oldListener = V8AbstractEventListener::cast(oldValue);
+        if (oldListener) {
+            v8::Local<v8::Object> oldListenerObject = oldListener->getExistingListenerObject();
+            if (!oldListenerObject.IsEmpty())
+                removeHiddenValueFromArray(object, oldListenerObject, arrayIndex, isolate);
+        }
+    }
+    // Non-callable input is treated as null and ignored
+    if (newValue->IsFunction())
+        addHiddenValueToArray(object, newValue, arrayIndex, isolate);
 }
 
 static gin::IsolateHolder* mainIsolateHolder = 0;
