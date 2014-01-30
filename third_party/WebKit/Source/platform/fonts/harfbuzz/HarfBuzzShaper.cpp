1a89ef231981cc582c16c2b3fd9d6b32f0f78bce
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-30 06:26:41 +0000

    Rename collectHarfBuzzRuns to createHarfBuzzRuns
    Rename HarfBuzzShaper::collectHarfBuzzRuns to createHarfBuzzRuns to
    better represent what the method is doing.
    
    BUG=311372
    R=leviw@chromium.org
    
    Review URL: https://codereview.chromium.org/145973009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166091 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/fonts/harfbuzz/HarfBuzzShaper.cpp
+++ third_party/WebKit/Source/platform/fonts/harfbuzz/HarfBuzzShaper.cpp
@@ -539,7 +539,7 @@ void HarfBuzzShaper::setFontFeatures()
 
 bool HarfBuzzShaper::shape(GlyphBuffer* glyphBuffer)
 {
-    if (!collectHarfBuzzRuns())
+    if (!createHarfBuzzRuns())
         return false;
 
     m_totalWidth = 0;
@@ -587,7 +587,7 @@ static inline int handleMultipleUChar(
     return 0;
 }
 
-bool HarfBuzzShaper::collectHarfBuzzRuns()
+bool HarfBuzzShaper::createHarfBuzzRuns()
 {
     const UChar* normalizedBufferEnd = m_normalizedBuffer.get() + m_normalizedBufferLength;
     SurrogatePairAwareTextIterator iterator(m_normalizedBuffer.get(), 0, m_normalizedBufferLength, m_normalizedBufferLength);
