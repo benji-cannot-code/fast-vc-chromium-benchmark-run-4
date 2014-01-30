c3ca28d9767b9db90506011e7e57819707e80888
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-30 07:28:25 +0000

    [Telemetry] Improvements to OS X powermetrics integration
    * Make StartMonitoringPowerAsync() synchronous in terms of starting the powermetrics process by waiting for output file to be created before returning.
    * Handle parse errors in powermetrics output - empirically while I was able to quite easily get powermetrics into a state where it exited in the middle of writing a sample, I was completely unable to get it not write out at least one sample completely.
    * Modify unit test to start/stop powermetrics faster than it's refresh rate to try to trigger malformed output - test fails without rest of this patch, passes with this patch.
    
    BUG=338808
    
    Review URL: https://codereview.chromium.org/145353012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247871 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/platform/platform_backend_unittest.py
+++ tools/telemetry/telemetry/core/platform/platform_backend_unittest.py
@@ -17,5 +17,5 @@ class PlatformBackendTest(unittest.TestCase):
       logging.warning('Test not supported on this platform.')
       return
 
-    output = backend.MonitorPowerSync(100)
+    output = backend.MonitorPowerSync(1)
     self.assertTrue(output.has_key('power_samples_mw'))
