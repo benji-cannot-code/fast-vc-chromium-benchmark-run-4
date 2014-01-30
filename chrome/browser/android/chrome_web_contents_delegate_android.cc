784f5a5fb1bf585a05132bb53cf592c2ab0d58c3
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-30 23:42:59 +0000

    Unify desktop and Android tab helper instantiation.
    BUG=337600
    TEST=zero visible change
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=247676
    
    Reverted: https://src.chromium.org/viewvc/chrome?view=rev&revision=247682
    
    Review URL: https://codereview.chromium.org/144443003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@248073 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/android/chrome_web_contents_delegate_android.cc
+++ chrome/browser/android/chrome_web_contents_delegate_android.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/android/jni_android.h"
 #include "base/command_line.h"
-#include "chrome/browser/android/tab_android.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/file_select_helper.h"
 #include "chrome/browser/media/media_capture_devices_dispatcher.h"
@@ -21,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_navigator.h"
 #include "chrome/browser/ui/find_bar/find_notification_details.h"
 #include "chrome/browser/ui/find_bar/find_tab_helper.h"
+#include "chrome/browser/ui/tab_helpers.h"
 #include "chrome/common/chrome_switches.h"
 #include "content/public/browser/notification_details.h"
 #include "content/public/browser/notification_service.h"
@@ -306,7 +306,7 @@ void ChromeWebContentsDelegateAndroid::AddNewContents(
   // Can't create a new contents for the current tab - invalid case.
   DCHECK_NE(disposition, CURRENT_TAB);
 
-  TabAndroid::InitTabHelpers(new_contents);
+  TabHelpers::AttachTabHelpers(new_contents);
 
   JNIEnv* env = AttachCurrentThread();
   ScopedJavaLocalRef<jobject> obj = GetJavaDelegate(env);
