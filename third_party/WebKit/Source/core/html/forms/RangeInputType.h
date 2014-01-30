c6e86242316ce61dac9c488bb86a4eb632860afe
sigbjornf@opera.com <sigbjornf@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-30 09:34:59 +0000

    HTMLInputElement.valueAsNumber compliance (NaN handling.)
    Change the valueAsNumber setter to follow the spec more closely:
    
     http://www.whatwg.org/specs/web-apps/current-work/#dom-input-valueasnumber
    
     - throw TypeError instead of NotSupportedError if the new value is
       infinite.
     - if the input element type supports 'valueAsNumber', then no longer
       throw for NaN values, but consider this as setting the empty
       string.
     - better InvalidStateError error message when attempting to update
       'valueAsNumber'.
    
    This aligns with the spec as well as Gecko. And Trident (IE11) with
    respect to NaN handling.
    
    R=mkwst@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/149413004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166112 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/forms/RangeInputType.h
+++ third_party/WebKit/Source/core/html/forms/RangeInputType.h
@@ -49,7 +49,7 @@ private:
     virtual bool isRangeControl() const OVERRIDE;
     virtual const AtomicString& formControlType() const OVERRIDE;
     virtual double valueAsDouble() const OVERRIDE;
-    virtual void setValueAsDecimal(const Decimal&, TextFieldEventBehavior, ExceptionState&) const OVERRIDE;
+    virtual void setValueAsDouble(double, TextFieldEventBehavior, ExceptionState&) const OVERRIDE;
     virtual bool typeMismatchFor(const String&) const OVERRIDE;
     virtual bool supportsRequired() const OVERRIDE;
     virtual StepRange createStepRange(AnyStepHandling) const OVERRIDE;
