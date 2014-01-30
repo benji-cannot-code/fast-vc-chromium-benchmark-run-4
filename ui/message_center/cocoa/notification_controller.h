b4a7d3ac0c81a34783223e910d829e78a115f3c1
dewittj@chromium.org <dewittj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-30 07:37:24 +0000

    Adds a small icon to notifications, and connects it to synced notifications.
    The small icon is a 16x16 image in the bottom right of all notification
    templates.  Currently it is not exposed to the JS API, but instead is used
    only by internal API systems (starting with synced notifications.)
    
    BUG=284592
    
    Review URL: https://codereview.chromium.org/149433005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247875 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/message_center/cocoa/notification_controller.h
+++ ui/message_center/cocoa/notification_controller.h
@@ -38,6 +38,9 @@ MESSAGE_CENTER_EXPORT
   // The button that invokes |-close:|, in the upper-right corner.
   base::scoped_nsobject<HoverImageButton> closeButton_;
 
+  // The small icon associated with the notification, on the bottom right.
+  base::scoped_nsobject<NSImageView> smallImage_;
+
   // The large icon associated with the notification, on the left side.
   base::scoped_nsobject<NSImageView> icon_;
 
@@ -85,6 +88,7 @@ MESSAGE_CENTER_EXPORT
 @end
 
 @interface MCNotificationController (TestingInterface)
+- (NSImageView*)smallImageView;
 - (NSImageView*)iconView;
 @end
 
--- ui/message_center/views/message_view.h
+++ ui/message_center/views/message_view.h
@@ -20,6 +20,7 @@ class MenuModel;
 
 namespace views {
 class ImageButton;
+class ImageView;
 class Painter;
 class ScrollView;
 }
@@ -49,6 +50,7 @@ class MESSAGE_CENTER_EXPORT MessageView : public views::SlideOutView,
   MessageView(MessageViewController* controller,
               const std::string& notification_id,
               const NotifierId& notifier_id,
+              const gfx::ImageSkia& small_image,
               const base::string16& display_source);
   virtual ~MessageView();
 
@@ -91,6 +93,7 @@ class MESSAGE_CENTER_EXPORT MessageView : public views::SlideOutView,
   // Overridden from views::SlideOutView:
   virtual void OnSlideOut() OVERRIDE;
 
+  views::ImageView* small_image() { return small_image_view_.get(); }
   views::ImageButton* close_button() { return close_button_.get(); }
   views::ScrollView* scroller() { return scroller_; }
 
@@ -100,6 +103,7 @@ class MESSAGE_CENTER_EXPORT MessageView : public views::SlideOutView,
   NotifierId notifier_id_;
   views::View* background_view_;  // Owned by views hierarchy.
   scoped_ptr<views::ImageButton> close_button_;
+  scoped_ptr<views::ImageView> small_image_view_;
   views::ScrollView* scroller_;
 
   base::string16 accessible_name_;
