7e8480fb5582206cc1fd50508705c29b3002963a
tsepez@chromium.org <tsepez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-30 08:53:38 +0000

    Apply automatic enum range checking for appcache enums acros IPC.
    This is part of a long-running background task to remove the remaining
    uses of the unchecked IPC_ENUM_TRAITS() macro.
    
    BUG=246708
    
    Review URL: https://codereview.chromium.org/149073002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247905 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/common/appcache/appcache_interfaces.h
+++ webkit/common/appcache/appcache_interfaces.h
@@ -35,7 +35,8 @@ enum Status {
   CHECKING,
   DOWNLOADING,
   UPDATE_READY,
-  OBSOLETE
+  OBSOLETE,
+  STATUS_LAST = OBSOLETE
 };
 
 enum EventID {
@@ -46,7 +47,8 @@ enum EventID {
   PROGRESS_EVENT,
   UPDATE_READY_EVENT,
   CACHED_EVENT,
-  OBSOLETE_EVENT
+  OBSOLETE_EVENT,
+  EVENT_ID_LAST = OBSOLETE_EVENT
 };
 
 // Temporarily renumber them in wierd way, to help remove LOG_TIP from WebKit
