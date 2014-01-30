ca40e02f2af41cfdea56419c194765d9fbdc40a4
asargent@chromium.org <asargent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-30 13:55:42 +0000

    Change default mode of extension install verification
    Right now the default mode is to make requests against the server unless
    the experiment group says not to, and that may be contributing to some
    unexpected load we're seeing. So this CL changes it to not do that.
    
    It also adds some histograms to help measure the success rate and
    sources of requests to the server.
    
    [This is a resubmit of https://codereview.chromium.org/149353002 with a
    compile error fixed]
    
    BUG=335379
    TBR=finnur@chromium.org
    
    Review URL: https://codereview.chromium.org/150093002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247954 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/install_verifier.cc
+++ chrome/browser/extensions/install_verifier.cc
@@ -31,6 +31,11 @@ enum VerifyStatus {
   NONE = 0,   // Do not request install signatures, and do not enforce them.
   BOOTSTRAP,  // Request install signatures, but do not enforce them.
   ENFORCE,    // Request install signatures, and enforce them.
+
+  // This is used in histograms - do not remove or reorder entries above! Also
+  // the "MAX" item below should always be the last element.
+
+  VERIFY_STATUS_MAX
 };
 
 #if defined(GOOGLE_CHROME_BUILD)
@@ -50,7 +55,7 @@ VerifyStatus GetExperimentStatus() {
     return ENFORCE;
   }
 
-  VerifyStatus default_status = BOOTSTRAP;
+  VerifyStatus default_status = NONE;
 
   if (group == "Enforce")
     return ENFORCE;
@@ -142,6 +147,11 @@ bool InstallVerifier::NeedsVerification(const Extension& extension) {
 }
 
 void InstallVerifier::Init() {
+  UMA_HISTOGRAM_ENUMERATION("ExtensionInstallVerifier.ExperimentStatus",
+                            GetExperimentStatus(), VERIFY_STATUS_MAX);
+  UMA_HISTOGRAM_ENUMERATION("ExtensionInstallVerifier.ActualStatus",
+                            GetStatus(), VERIFY_STATUS_MAX);
+
   const base::DictionaryValue* pref = prefs_->GetInstallSignature();
   if (pref) {
     scoped_ptr<InstallSignature> signature_from_prefs =
