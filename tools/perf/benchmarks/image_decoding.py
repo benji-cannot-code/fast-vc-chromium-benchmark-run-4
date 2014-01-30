665750cda65a970c93158afb1fee1a61000f6213
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-30 00:17:44 +0000

    Reenable image_decoding
    BUG=323015
    TBR=tonyg
    NOTRY=True
    
    Review URL: https://codereview.chromium.org/149773002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247787 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/benchmarks/image_decoding.py
+++ tools/perf/benchmarks/image_decoding.py
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import sys
-
 from telemetry import test
 
 from measurements import image_decoding
@@ -14,6 +12,3 @@ class ImageDecodingToughImageCases(test.Test):
   test = image_decoding.ImageDecoding
   # TODO: Rename this page set to tough_image_cases.json
   page_set = 'page_sets/image_decoding_measurement.json'
-
-  # crbug.com/323015
-  enabled = not sys.platform.startswith('linux')
