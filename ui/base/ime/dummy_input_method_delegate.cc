c0dd409f4d2f45c7734e9b12b780bba4b013bc54
yukishiino@chromium.org <yukishiino@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-30 13:38:54 +0000

    Refactors ui::internal::InputMethodDelegate.
    Simplify and combine Dispatch{,Fabricated}KeyEventPostIME methods into one method.
    
    TEST=Run unit_tests, ui_unittests, views_unittests.
    TBR=pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/142043006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247947 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/ime/dummy_input_method_delegate.cc
+++ ui/base/ime/dummy_input_method_delegate.cc
@@ -12,14 +12,7 @@ DummyInputMethodDelegate::DummyInputMethodDelegate() {}
 DummyInputMethodDelegate::~DummyInputMethodDelegate() {}
 
 bool DummyInputMethodDelegate::DispatchKeyEventPostIME(
-    const base::NativeEvent& native_key_event) {
-  return true;
-}
-
-bool DummyInputMethodDelegate::DispatchFabricatedKeyEventPostIME(
-    ui::EventType type,
-    ui::KeyboardCode key_code,
-    int flags) {
+    const ui::KeyEvent& key_event) {
   return true;
 }
 
