d19cfbc18f185bba3efe5fb1b240f4e592017c89
rlp@chromium.org <rlp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-30 10:33:08 +0000

    [Hotword] Set up hotwording to be externally loaded component extension. Only load if the field trial is correct and if they use "en" language since it is only available for English at this time.
    BUG=325439, 289023
    
    Review URL: https://codereview.chromium.org/141013015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247923 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/external_component_loader.cc
+++ chrome/browser/extensions/external_component_loader.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/external_component_loader.h"
 
 #include "base/command_line.h"
+#include "base/i18n/case_conversion.h"
+#include "base/metrics/field_trial.h"
 #include "base/prefs/pref_change_registrar.h"
 #include "base/prefs/pref_service.h"
 #include "base/values.h"
@@ -17,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "components/user_prefs/pref_registry_syncable.h"
 #include "content/public/browser/browser_thread.h"
+#include "ui/base/l10n/l10n_util.h"
 
 namespace extensions {
 
@@ -58,6 +61,26 @@ void ExternalComponentLoader::StartLoading() {
   std::string appId = extension_misc::kInAppPaymentsSupportAppId;
   prefs_->SetString(appId + ".external_update_url",
                     extension_urls::GetWebstoreUpdateUrl().spec());
+
+  std::string group = base::FieldTrialList::FindFullName("VoiceTrigger");
+  if (!group.empty() && group != "Disabled") {
+    std::string locale =
+#if defined(OS_CHROMEOS)
+        // On ChromeOS locale is per-profile.
+        profile_->GetPrefs()->GetString(prefs::kApplicationLocale);
+#else
+        g_browser_process->GetApplicationLocale();
+#endif
+    // Only available for English now.
+    std::string normalized_locale = l10n_util::NormalizeLocale(locale);
+    if (!(strcmp(normalized_locale.c_str(), "en")) ||
+        !(strcmp(normalized_locale.c_str(), "en_us")) ||
+        !(strcmp(normalized_locale.c_str(), "en_US"))) {
+      std::string hotwordId = extension_misc::kHotwordExtensionId;
+      prefs_->SetString(hotwordId + ".external_update_url",
+                        extension_urls::GetWebstoreUpdateUrl().spec());
+    }
+  }
 
   if (CommandLine::ForCurrentProcess()->
           GetSwitchValueASCII(switches::kEnableEnhancedBookmarks) != "0") {
