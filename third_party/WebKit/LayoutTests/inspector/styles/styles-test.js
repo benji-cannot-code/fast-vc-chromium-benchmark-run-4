0ab2279e29da4b385ff724cb1bdcaeda15bfead5
lushnikov@chromium.org <lushnikov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-31 18:05:56 +0000

    DevTools: [CSS] remove getAllStylesheets method from protocol
    This patch removes getAllStylesheets method from protocol. Instead,
    clients should track styleSheetAdded/styleSheetRemoved events to
    maintain actual stylesheet set.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/148523012
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166228 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/inspector/styles/styles-test.js
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function initialize_StylesTests()
+{
+
+InspectorTest.waitForStylesheetsOnFrontend = function(styleSheetsCount, callback)
+{
+    function styleSheetComparator(a, b)
+    {
+        if (a.sourceURL < b.sourceURL)
+            return -1;
+        else if (a.sourceURL > b.sourceURL)
+            return 1;
+        return a.startLine - b.startLine || a.startColumn - b.startColumn;
+    }
+
+    var styleSheets = WebInspector.cssModel.allStyleSheets();
+    if (styleSheets.length >= styleSheetsCount) {
+        styleSheets.sort(styleSheetComparator);
+        callback(styleSheets);
+        return;
+    }
+
+    function onStyleSheetAdded()
+    {
+        var styleSheets = WebInspector.cssModel.allStyleSheets();
+        if (styleSheets.length < styleSheetsCount)
+            return;
+
+        WebInspector.cssModel.removeEventListener(WebInspector.CSSStyleModel.Events.StyleSheetAdded, onStyleSheetAdded, this);
+        styleSheets.sort(styleSheetComparator);
+        callback(null, styleSheets);
+    }
+
+    WebInspector.cssModel.addEventListener(WebInspector.CSSStyleModel.Events.StyleSheetAdded, onStyleSheetAdded, this);
+}
+
+}
