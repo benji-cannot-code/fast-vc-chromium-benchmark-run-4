bfad31e47846ae0770ff0fbff9903572a1b21eb6
habib.virji@samsung.com <habib.virji@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-31 20:16:45 +0000

    Handling radio group name as case insensitive
    Added CaseFoldingHash in the NameToGroupMap to handle group name case
    insensitive.
    (Replicates patch fixed in WebKit for this issue:
    https://bugs.webkit.org/attachment.cgi?id=187180)
    
    Note that some test cases in radio-group-name-case.html fails because
    CaseFoldingHash doesn't conform to the specification.
    
    R=tkent
    BUG=136105
    TEST=automated
    
    Review URL: https://codereview.chromium.org/149173007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166241 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/forms/CheckedRadioButtons.h
+++ third_party/WebKit/Source/core/html/forms/CheckedRadioButtons.h
@@ -46,7 +46,7 @@ public:
     bool isInRequiredGroup(HTMLInputElement*) const;
 
 private:
-    typedef HashMap<AtomicString, OwnPtr<RadioButtonGroup> > NameToGroupMap;
+    typedef HashMap<AtomicString, OwnPtr<RadioButtonGroup>, CaseFoldingHash> NameToGroupMap;
     OwnPtr<NameToGroupMap> m_nameToGroupMap;
 };
 
