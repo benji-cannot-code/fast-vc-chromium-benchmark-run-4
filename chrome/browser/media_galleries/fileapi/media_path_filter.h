658ab4d4c083eb78de552191601b046da1883763
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-31 08:32:10 +0000

    Media Galleries: Skip hidden files on scans.
    Also do not follow symlinks.
    
    BUG=161119
    
    Review URL: https://codereview.chromium.org/134183005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@248155 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media_galleries/fileapi/media_path_filter.h
+++ chrome/browser/media_galleries/fileapi/media_path_filter.h
@@ -18,6 +18,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // media filesystem.
 class MediaPathFilter {
  public:
+  // Used to skip hidden folders and files. Returns true if the file specified
+  // by |path| should be skipped.
+  static bool ShouldSkip(const base::FilePath& path);
+
   MediaPathFilter();
   ~MediaPathFilter();
 
