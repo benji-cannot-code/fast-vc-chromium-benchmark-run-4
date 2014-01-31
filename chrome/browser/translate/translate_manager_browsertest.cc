669e858ba8e3b7b0dad63c9000e37bb8591ced65
hajimehoshi@chromium.org <hajimehoshi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-31 06:33:08 +0000

    Enable the Translate bubble UX by default
    Remove the flag --enable-translate-new-ux and enable the bubble by default only for the aura environments.
    
    BUG=276181
    TEST=browser_tests --gtest_filter=TransalteManagerBrowserTest.*:TranslateBubbleViewBrowserTest.*:TranslateBrowserTest.*
    
    Review URL: https://codereview.chromium.org/148173003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@248142 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/translate/translate_manager_browsertest.cc
+++ chrome/browser/translate/translate_manager_browsertest.cc
@@ -241,6 +241,7 @@ class TranslateManagerBrowserTest : public ChromeRenderViewHostTestHarness,
     TranslateManager::GetInstance()->
         SetTranslateScriptExpirationDelay(60 * 60 * 1000);
     TranslateManager::GetInstance()->set_translate_max_reload_attemps(0);
+    TranslateManager::SetUseInfobar(true);
 
     ChromeRenderViewHostTestHarness::SetUp();
     InfoBarService::CreateForWebContents(web_contents());
@@ -1473,12 +1474,11 @@ TEST_F(TranslateManagerBrowserTest, DownloadsAndHistoryNotTranslated) {
       GURL(chrome::kChromeUIHistoryURL)));
 }
 
-#if !defined(OS_ANDROID) && !defined(OS_IOS)
+#if defined(USE_AURA)
 
 TEST_F(TranslateManagerBrowserTest, BubbleNormalTranslate) {
   // Prepare for the bubble
-  CommandLine* command_line = CommandLine::ForCurrentProcess();
-  command_line->AppendSwitch(switches::kEnableTranslateNewUX);
+  TranslateManager::SetUseInfobar(false);
   MockTranslateBubbleFactory* factory = new MockTranslateBubbleFactory;
   scoped_ptr<TranslateBubbleFactory> factory_ptr(factory);
   TranslateBubbleFactory::SetFactory(factory);
@@ -1519,8 +1519,7 @@ TEST_F(TranslateManagerBrowserTest, BubbleNormalTranslate) {
 
 TEST_F(TranslateManagerBrowserTest, BubbleTranslateScriptNotAvailable) {
   // Prepare for the bubble
-  CommandLine* command_line = CommandLine::ForCurrentProcess();
-  command_line->AppendSwitch(switches::kEnableTranslateNewUX);
+  TranslateManager::SetUseInfobar(false);
   MockTranslateBubbleFactory* factory = new MockTranslateBubbleFactory;
   scoped_ptr<TranslateBubbleFactory> factory_ptr(factory);
   TranslateBubbleFactory::SetFactory(factory);
@@ -1552,8 +1551,7 @@ TEST_F(TranslateManagerBrowserTest, BubbleTranslateScriptNotAvailable) {
 
 TEST_F(TranslateManagerBrowserTest, BubbleUnknownLanguage) {
   // Prepare for the bubble
-  CommandLine* command_line = CommandLine::ForCurrentProcess();
-  command_line->AppendSwitch(switches::kEnableTranslateNewUX);
+  TranslateManager::SetUseInfobar(false);
   MockTranslateBubbleFactory* factory = new MockTranslateBubbleFactory;
   scoped_ptr<TranslateBubbleFactory> factory_ptr(factory);
   TranslateBubbleFactory::SetFactory(factory);
@@ -1580,7 +1578,7 @@ TEST_F(TranslateManagerBrowserTest, BubbleUnknownLanguage) {
             bubble->GetViewState());
 }
 
-#endif  // !defined(OS_ANDROID) && !defined(OS_IOS)
+#endif  // defined(USE_AURA)
 
 // Test is flaky on Win http://crbug.com/166334
 #if defined(OS_WIN)
