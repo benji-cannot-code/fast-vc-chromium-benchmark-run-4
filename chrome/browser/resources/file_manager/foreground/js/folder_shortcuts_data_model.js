58b9d3401516b5177de19ca889e10ea78f1c6a97
mtomasz@chromium.org <mtomasz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-31 10:01:12 +0000

    Fix conversion between virtual paths and URLs.
    The URLs encode space as %20, while virtual paths should use a space character. This caused failures in resolving shortcuts created by new version of Chrome, on older chromebooks, what was causing removing these shortcuts.
    
    TEST=Tested manually the scenario above.
    BUG=336155
    
    Review URL: https://codereview.chromium.org/151233002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@248158 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/foreground/js/folder_shortcuts_data_model.js
+++ chrome/browser/resources/file_manager/foreground/js/folder_shortcuts_data_model.js
@@ -456,7 +456,7 @@ FolderShortcutsDataModel.prototype = {
     }
 
     var url = this.driveVolumeInfo_.root.toURL() +
-              path.substr(STORED_DRIVE_MOUNT_PATH.length);
+        encodeURIComponent(path.substr(STORED_DRIVE_MOUNT_PATH.length));
     return url;
   },
 
@@ -476,7 +476,8 @@ FolderShortcutsDataModel.prototype = {
       return null;
     }
 
-    var storedPath = STORED_DRIVE_MOUNT_PATH + url.substr(rootUrl.length);
+    var storedPath = decodeURIComponent(
+        STORED_DRIVE_MOUNT_PATH + url.substr(rootUrl.length));
     return storedPath;
   },
 };
