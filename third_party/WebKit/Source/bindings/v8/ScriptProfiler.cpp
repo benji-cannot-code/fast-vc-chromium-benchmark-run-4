53189644d608f3f9e007caad4b84546b3b79dd5f
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-31 12:27:47 +0000

    Remove isWrapperOfType and toInnerGlobalObject
    Instead of using isWrapperOfType and toInnerGlobalObject,
    we can simply use toDOMWindow.
    
    R=dcarney@chromium.org
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=166193
    
    Review URL: https://codereview.chromium.org/151203002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166204 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/ScriptProfiler.cpp
+++ third_party/WebKit/Source/bindings/v8/ScriptProfiler.cpp
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/v8/RetainedDOMInfo.h"
 #include "bindings/v8/ScriptObject.h"
 #include "bindings/v8/V8Binding.h"
-#include "bindings/v8/V8DOMWrapper.h"
 #include "bindings/v8/WrapperTypeInfo.h"
 #include "core/dom/Document.h"
 #include "core/inspector/BindingVisitors.h"
@@ -167,15 +166,12 @@ class GlobalObjectNameResolver FINAL : public v8::HeapProfiler::ObjectNameResolv
 public:
     virtual const char* GetName(v8::Handle<v8::Object> object) OVERRIDE
     {
-        if (V8DOMWrapper::isWrapperOfType(object, &V8Window::wrapperTypeInfo)) {
-            DOMWindow* window = V8Window::toNative(object);
-            if (window) {
-                CString url = window->document()->url().string().utf8();
-                m_strings.append(url);
-                return url.data();
-            }
-        }
-        return 0;
+        DOMWindow* window = toDOMWindow(object, v8::Isolate::GetCurrent());
+        if (!window)
+            return 0;
+        CString url = window->document()->url().string().utf8();
+        m_strings.append(url);
+        return url.data();
     }
 
 private:
