0bc8b00ea72e4d06f03e284cd552ded5ae1f2ae8
weitaosu@chromium.org <weitaosu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-31 09:36:59 +0000

    It2me native messaging host: webapp implementation.
    The webapp implementation of the it2me native messaging is based on the me2me couterpart. But I got rid of the per-message onDone/onError callbacks because unlike me2me, all requests to the it2me host are asynchronous: direct responses to all requests are meaningless. Only the asynchronous callbacks on host state change and nat policy update need to be processed.
    
    I verified that this is fully functional on Linux. Windows and Mac verification haven't been done and are planned after the linux work is completed.
    
    BUG=309844
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=248072
    
    Review URL: https://codereview.chromium.org/138503009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@248157 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/plugin/host_script_object.cc
+++ remoting/host/plugin/host_script_object.cc
@@ -509,6 +509,7 @@ bool HostNPScriptObject::Disconnect(const NPVariant* args,
   if (it2me_host_.get()) {
     it2me_host_->Disconnect();
     it2me_host_ = NULL;
+    host_context_.reset();
   }
 
   return true;
