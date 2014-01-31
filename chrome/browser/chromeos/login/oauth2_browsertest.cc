f04b0bad618b449b3b90ca8e5b540913823fa48a
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-31 01:22:49 +0000

    Disable PageThrottle and XHRThrottle in MergeSessionTest.
    These tests are failing. See bug.
    
    TBR=zelidrag@chromium.org
    BUG=339671
    
    Review URL: https://codereview.chromium.org/144523003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@248099 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/oauth2_browsertest.cc
+++ chrome/browser/chromeos/login/oauth2_browsertest.cc
@@ -676,7 +676,7 @@ Browser* FindOrCreateVisibleBrowser(Profile* profile) {
   return browser;
 }
 
-IN_PROC_BROWSER_TEST_F(MergeSessionTest, PageThrottle) {
+IN_PROC_BROWSER_TEST_F(MergeSessionTest, DISABLED_PageThrottle) {
   StartNewUserSession();
 
   // Try to open a page from google.com.
@@ -719,7 +719,7 @@ IN_PROC_BROWSER_TEST_F(MergeSessionTest, PageThrottle) {
   DVLOG(1) << "Loaded page at the end : " << title;
 }
 
-IN_PROC_BROWSER_TEST_F(MergeSessionTest, XHRThrottle) {
+IN_PROC_BROWSER_TEST_F(MergeSessionTest, DISABLED_XHRThrottle) {
   StartNewUserSession();
 
   // Wait until we get send merge session request.
