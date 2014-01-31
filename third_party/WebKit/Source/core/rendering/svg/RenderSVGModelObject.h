6ba366d815df23c7c73a56f73ecb5f50793935e4
fs@opera.com <fs@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-31 19:51:25 +0000

    Implement canHaveWhitespaceChildren on RenderSVG{Root,ModelObject}
    Make said method return false to signal that we don't care about
    whitespace-only Nodes in these contexts.
    This avoids creating large number of text RenderObjects
    (RenderSVGInlineTexts) and also avoids hitting the linear search
    at the bottom of Text::textRendererIsNeeded.
    
    R=pdr,schenney,fmalita
    BUG=339470,337607
    
    Review URL: https://codereview.chromium.org/136963005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166238 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/svg/RenderSVGModelObject.h
+++ third_party/WebKit/Source/core/rendering/svg/RenderSVGModelObject.h
@@ -51,6 +51,7 @@ public:
     explicit RenderSVGModelObject(SVGElement*);
 
     virtual bool isChildAllowed(RenderObject*, RenderStyle*) const OVERRIDE;
+    virtual bool canHaveWhitespaceChildren() const OVERRIDE { return false; }
 
     virtual LayoutRect clippedOverflowRectForRepaint(const RenderLayerModelObject* repaintContainer) const OVERRIDE;
     virtual void computeFloatRectForRepaint(const RenderLayerModelObject* repaintContainer, FloatRect&, bool fixed = false) const OVERRIDE FINAL;
