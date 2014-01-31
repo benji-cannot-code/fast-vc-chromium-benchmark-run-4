d2b8be35a2dc74b9377a6465221a95452245cd3d
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-31 06:00:00 +0000

    Move Document::m_inStyleRecalc into DocumentLifecycle
    This CL removes the m_inStyleRecalc bool from Document and adds that
    information to the DocumentLifecycle state machine. We eventually want to move
    style recalc, layout, compositing update, and paiting into the state machine.
    
    BUG=282720
    R=eseidel@chromium.org, vollick@chromium.org
    
    Review URL: https://codereview.chromium.org/150423004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166187 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -455,7 +455,6 @@ Document::Document(const DocumentInit& initializer, DocumentClassFlags documentC
     , m_readyState(Complete)
     , m_bParsing(false)
     , m_hasPendingStyleRecalc(false)
-    , m_inStyleRecalc(false)
     , m_gotoAnchorNeededAfterStylesheetsLoad(false)
     , m_containsValidityStyleRules(false)
     , m_updateFocusAppearanceRestoresSelection(false)
@@ -1607,7 +1606,7 @@ void Document::unscheduleStyleRecalc()
 
 bool Document::hasPendingForcedStyleRecalc() const
 {
-    return m_hasPendingStyleRecalc && !m_inStyleRecalc && styleChangeType() >= SubtreeStyleChange;
+    return m_hasPendingStyleRecalc && !inStyleRecalc() && styleChangeType() >= SubtreeStyleChange;
 }
 
 void Document::updateDistributionIfNeeded()
@@ -1715,7 +1714,7 @@ void Document::recalcStyle(StyleRecalcChange change)
     if (!isActive() || !view())
         return;
 
-    if (m_inStyleRecalc)
+    if (inStyleRecalc())
         return;
 
     TRACE_EVENT0("webkit", "Document::recalcStyle");
@@ -1744,7 +1743,7 @@ void Document::recalcStyle(StyleRecalcChange change)
     {
         PostAttachCallbacks::SuspendScope suspendPostAttachCallbacks;
         RenderWidget::UpdateSuspendScope suspendWidgetHierarchyUpdates;
-        TemporaryChange<bool> changeInStyleRecalc(m_inStyleRecalc, true);
+        m_lifecycle.advanceTo(DocumentLifecycle::InStyleRecalc);
 
         if (styleChangeType() >= SubtreeStyleChange)
             change = Force;
@@ -1792,6 +1791,9 @@ void Document::recalcStyle(StyleRecalcChange change)
             m_styleEngine->resetCSSFeatureFlags(resolver.ensureRuleFeatureSet());
             resolver.clearStyleSharingList();
         }
+
+        ASSERT(inStyleRecalc());
+        m_lifecycle.advanceTo(DocumentLifecycle::Clean);
     }
 
     InspectorInstrumentation::didRecalculateStyle(cookie);
@@ -2065,7 +2067,7 @@ void Document::attach(const AttachContext& context)
 
     ContainerNode::attach(context);
 
-    m_lifecycle.advanceTo(DocumentLifecycle::Active);
+    m_lifecycle.advanceTo(DocumentLifecycle::Clean);
 }
 
 void Document::detach(const AttachContext& context)
--- third_party/WebKit/Source/core/dom/DocumentLifecycle.h
+++ third_party/WebKit/Source/core/dom/DocumentLifecycle.h
@@ -42,7 +42,13 @@ public:
     enum State {
         Uninitialized,
         Inactive,
-        Active,
+
+        // When the document is active, it traverses these states.
+        Clean,
+        InStyleRecalc,
+
+        // Once the document starts shuting down, we cannot return
+        // to the style/layout/rendering states.
         Stopping,
         Stopped,
         Disposed,
@@ -51,6 +57,7 @@ public:
     DocumentLifecycle();
     ~DocumentLifecycle();
 
+    bool isActive() const { return m_state > Inactive && m_state < Stopping; }
     State state() const { return m_state; }
 
     void advanceTo(State);
