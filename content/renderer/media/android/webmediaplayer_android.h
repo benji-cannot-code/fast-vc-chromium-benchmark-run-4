adec04c6a9ec5b513cad7ec8f975a4cae6851849
fischman@chromium.org <fischman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-31 03:16:00 +0000

    WebMediaPlayer{Android,Impl}: use RenderFrameObserver instead of ML::DestructionObserver/RenderViewObserver.
    RenderThreadImpl is torn down before the main thread's MessageLoop is, and some
    of WebMediaPlayerAndroid's dependencies require the render thread to still be
    alive, so move its tear-down to earlier in the process.
    This is the Android version of r240125 which fixed the same bug in
    WebMediaPlayerImpl, but forgot that WebMediaPlayerAndroid is a fork.
    
    Since RenderViewObserver is apparently out of favor nowadays, use RenderFrameObserver.
    
    BUG=338393,304967,338481
    
    Review URL: https://codereview.chromium.org/137243005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@248115 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/android/webmediaplayer_android.h
+++ content/renderer/media/android/webmediaplayer_android.h
@@ -14,10 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
-#include "base/message_loop/message_loop.h"
 #include "base/time/time.h"
 #include "cc/layers/video_frame_provider.h"
 #include "content/common/media/media_player_messages_enums_android.h"
+#include "content/public/renderer/render_frame_observer.h"
 #include "content/renderer/media/android/media_info_loader.h"
 #include "content/renderer/media/android/media_source_delegate.h"
 #include "content/renderer/media/android/stream_texture_factory_android.h"
@@ -31,6 +31,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/platform/WebSize.h"
 #include "third_party/WebKit/public/platform/WebURL.h"
 #include "ui/gfx/rect_f.h"
+
+namespace base {
+class MessageLoopProxy;
+}
 
 namespace media {
 class MediaLog;
@@ -55,24 +59,22 @@ class RendererMediaPlayerManager;
 class WebMediaPlayerAndroid
     : public blink::WebMediaPlayer,
       public cc::VideoFrameProvider,
-      public base::MessageLoop::DestructionObserver,
+      public RenderFrameObserver,
       public base::SupportsWeakPtr<WebMediaPlayerAndroid> {
  public:
-  // Construct a WebMediaPlayerAndroid object. This class communicates
-  // with the MediaPlayerAndroid object in the browser process through
-  // |proxy|.
+  // Construct a WebMediaPlayerAndroid object. This class communicates with the
+  // MediaPlayerAndroid object in the browser process through |proxy|.
   // TODO(qinmin): |frame| argument is used to determine whether the current
   // player can enter fullscreen. This logic should probably be moved into
   // blink, so that enterFullscreen() will not be called if another video is
   // already in fullscreen.
-  WebMediaPlayerAndroid(
-      blink::WebFrame* frame,
-      blink::WebMediaPlayerClient* client,
-      base::WeakPtr<WebMediaPlayerDelegate> delegate,
-      RendererMediaPlayerManager* manager,
-      StreamTextureFactory* factory,
-      const scoped_refptr<base::MessageLoopProxy>& media_loop,
-      media::MediaLog* media_log);
+  WebMediaPlayerAndroid(blink::WebFrame* frame,
+                        blink::WebMediaPlayerClient* client,
+                        base::WeakPtr<WebMediaPlayerDelegate> delegate,
+                        RendererMediaPlayerManager* manager,
+                        StreamTextureFactory* factory,
+                        const scoped_refptr<base::MessageLoopProxy>& media_loop,
+                        media::MediaLog* media_log);
   virtual ~WebMediaPlayerAndroid();
 
   // blink::WebMediaPlayer implementation.
@@ -178,8 +180,8 @@ class WebMediaPlayerAndroid
   // However, the actual GlTexture is not released to keep the video screenshot.
   virtual void ReleaseMediaResources();
 
-  // Method inherited from DestructionObserver.
-  virtual void WillDestroyCurrentMessageLoop() OVERRIDE;
+  // RenderFrameObserver implementation.
+  virtual void OnDestruct() OVERRIDE;
 
   // Detach the player from its manager.
   void Detach();
