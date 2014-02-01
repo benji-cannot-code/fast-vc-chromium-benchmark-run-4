722ca26b42add4c26009b6ec2e96db5cfcc03da9
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-01 14:37:07 +0000

    Exposed A11Y profile properties via chromeOSInfoPrivate API.
    TEST=expanded ExtensionApiTest.ChromeOSInfoPrivateTest
    BUG=339945
    
    Review URL: https://codereview.chromium.org/138903029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@248359 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/extensions/info_private_apitest.cc
+++ chrome/browser/chromeos/extensions/info_private_apitest.cc
@@ -2,8 +2,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
+#include "base/prefs/pref_service.h"
 #include "chrome/browser/chromeos/settings/cros_settings.h"
 #include "chrome/browser/extensions/extension_apitest.h"
+#include "chrome/common/pref_names.h"
 #include "chromeos/settings/cros_settings_names.h"
 
 IN_PROC_BROWSER_TEST_F(ExtensionApiTest, ChromeOSInfoPrivateTest) {
@@ -13,5 +16,26 @@ IN_PROC_BROWSER_TEST_F(ExtensionApiTest, ChromeOSInfoPrivateTest) {
   chromeos::CrosSettings::Get()->Set(chromeos::kSystemTimezone,
                                      initial_timezone);
 
+  // Check that accessability settings are set to default values.
+  ASSERT_FALSE(profile()->GetPrefs()->GetBoolean(prefs::kLargeCursorEnabled));
+  ASSERT_FALSE(profile()->GetPrefs()->GetBoolean(prefs::kStickyKeysEnabled));
+  ASSERT_FALSE(profile()->GetPrefs()->GetBoolean(
+      prefs::kSpokenFeedbackEnabled));
+  ASSERT_FALSE(profile()->GetPrefs()->GetBoolean(prefs::kHighContrastEnabled));
+  ASSERT_FALSE(profile()->GetPrefs()->GetBoolean(
+      prefs::kScreenMagnifierEnabled));
+  ASSERT_FALSE(profile()->GetPrefs()->GetBoolean(
+      prefs::kAutoclickEnabled));
+
   ASSERT_TRUE(RunComponentExtensionTest("chromeos_info_private")) << message_;
+
+  // Check that accessability settings have been all flipped by the test.
+  ASSERT_TRUE(profile()->GetPrefs()->GetBoolean(prefs::kLargeCursorEnabled));
+  ASSERT_TRUE(profile()->GetPrefs()->GetBoolean(prefs::kStickyKeysEnabled));
+  ASSERT_TRUE(profile()->GetPrefs()->GetBoolean(prefs::kSpokenFeedbackEnabled));
+  ASSERT_TRUE(profile()->GetPrefs()->GetBoolean(prefs::kHighContrastEnabled));
+  ASSERT_TRUE(profile()->GetPrefs()->GetBoolean(
+      prefs::kScreenMagnifierEnabled));
+  ASSERT_TRUE(profile()->GetPrefs()->GetBoolean(
+      prefs::kAutoclickEnabled));
 }
