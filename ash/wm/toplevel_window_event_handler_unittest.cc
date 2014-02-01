ddc58fce53097bc3ac6a18de2e2447377d065f4f
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-01 14:29:40 +0000

    Cleanup * Rename set_always_restores_to_restore_bounds to unminimize_to_restore_bounds because that's what it really is for. * add desktop shortcut to toggle fullscreen
    BUG=318325
    
    Review URL: https://codereview.chromium.org/146963003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@248354 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/toplevel_window_event_handler_unittest.cc
+++ ash/wm/toplevel_window_event_handler_unittest.cc
@@ -480,7 +480,7 @@ TEST_F(ToplevelWindowEventHandlerTest, GestureDrag) {
   RunAllPendingInMessageLoop();
   EXPECT_NE(old_bounds.ToString(), target->bounds().ToString());
   EXPECT_TRUE(window_state->IsMinimized());
-  EXPECT_TRUE(window_state->always_restores_to_restore_bounds());
+  EXPECT_TRUE(window_state->unminimize_to_restore_bounds());
   EXPECT_EQ(old_bounds.ToString(),
             window_state->GetRestoreBoundsInScreen().ToString());
 }
@@ -534,7 +534,7 @@ TEST_F(ToplevelWindowEventHandlerTest, GestureDragToRestore) {
   RunAllPendingInMessageLoop();
   EXPECT_NE(old_bounds.ToString(), window->bounds().ToString());
   EXPECT_TRUE(window_state->IsMinimized());
-  EXPECT_TRUE(window_state->always_restores_to_restore_bounds());
+  EXPECT_TRUE(window_state->unminimize_to_restore_bounds());
   EXPECT_EQ(old_bounds.ToString(),
             window_state->GetRestoreBoundsInScreen().ToString());
 }
