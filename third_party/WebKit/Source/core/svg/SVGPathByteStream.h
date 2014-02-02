5a3017f70ed32209ed280df731ad440006f0b80f
cevans@chromium.org <cevans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-02 01:15:35 +0000

    Improve efficiency of converting SVG path strings to streams.
    Building up a vector byte-by-byte causes a lot of vector expansations (and
    therefore copies and spamming of the allocator). In this case, we can make a
    reasonable estimate of how much space we'll need and trim it at the end.
    
    BUG=335734
    
    Review URL: https://codereview.chromium.org/151933002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166266 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGPathByteStream.h
+++ third_party/WebKit/Source/core/svg/SVGPathByteStream.h
@@ -68,6 +68,8 @@ public:
             append(*it);
     }
     void clear() { m_data.clear(); }
+    void reserveInitialCapacity(size_t size) { m_data.reserveInitialCapacity(size); }
+    void shrinkToFit() { m_data.shrinkToFit(); }
     bool isEmpty() const { return !m_data.size(); }
     unsigned size() const { return m_data.size(); }
 
