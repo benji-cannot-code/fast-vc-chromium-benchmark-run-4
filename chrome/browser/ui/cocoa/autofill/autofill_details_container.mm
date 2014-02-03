067b4688763be7def4a21425fe1072b78d4a9fa5
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-03 22:48:31 +0000

    [rAC, OSX] Fix field alignment.
    This fixes an issue around field alignment when the dialog displays
    scroll bars. Previous scroll would not only display the currently
    active field, but also scroll the entire view 15 pixels to the side,
    thus truncating the left edge. Since the scroll view is meant to scroll
    only in y-direction, this simply forces the X coordinate to 0.
    
    BUG=332678
    R=estade@chromium.org
    
    Review URL: https://codereview.chromium.org/141813005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@248599 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/autofill/autofill_details_container.mm
+++ chrome/browser/ui/cocoa/autofill/autofill_details_container.mm
@@ -145,7 +145,7 @@ typedef BOOL (^FieldFilterBlock)(NSView<AutofillInputField>*);
     return;
 
   NSPoint scrollPoint = [clipView constrainScrollPoint:
-      NSMakePoint(NSMinX(fieldRect), NSMinY(fieldRect) - bottomPadding)];
+      NSMakePoint(0, NSMinY(fieldRect) - bottomPadding)];
   [clipView scrollToPoint:scrollPoint];
   [scrollView_ reflectScrolledClipView:clipView];
   [self updateErrorBubble];
