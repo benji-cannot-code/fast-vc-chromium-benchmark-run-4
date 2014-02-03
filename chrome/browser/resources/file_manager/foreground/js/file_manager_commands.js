7d93e4d5b4208b4441abc1dcbd19b0d0a5f65697
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-03 12:22:21 +0000

    file_manager: Drive is not readonly even when offline
    BUG=260538,260540
    
    Review URL: https://codereview.chromium.org/140043005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@248478 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/foreground/js/file_manager_commands.js
+++ chrome/browser/resources/file_manager/foreground/js/file_manager_commands.js
@@ -484,11 +484,8 @@ CommandHandler.COMMANDS_['delete'] = {
     fileManager.deleteSelection();
   },
   canExecute: function(event, fileManager) {
-    var allowDeletingWhileOffline =
-        fileManager.directoryModel.getCurrentRootType() === RootType.DRIVE;
     var selection = fileManager.getSelection();
-    event.canExecute = (!fileManager.isOnReadonlyDirectory() ||
-                        allowDeletingWhileOffline) &&
+    event.canExecute = !fileManager.isOnReadonlyDirectory() &&
                        selection &&
                        selection.totalCount > 0;
   }
@@ -522,14 +519,11 @@ CommandHandler.COMMANDS_['rename'] = {
     fileManager.initiateRename();
   },
   canExecute: function(event, fileManager) {
-    var allowRenamingWhileOffline =
-        fileManager.directoryModel.getCurrentRootType() === RootType.DRIVE;
     var selection = fileManager.getSelection();
-    event.canExecute =
-        !fileManager.isRenamingInProgress() &&
-        (!fileManager.isOnReadonlyDirectory() || allowRenamingWhileOffline) &&
-        selection &&
-        selection.totalCount == 1;
+    event.canExecute = !fileManager.isRenamingInProgress() &&
+                       !fileManager.isOnReadonlyDirectory() &&
+                       selection &&
+                       selection.totalCount == 1;
   }
 };
 
