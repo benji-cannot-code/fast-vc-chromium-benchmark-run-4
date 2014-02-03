af7c4d39c8243dfccd3030022faa8626a89cfc97
hans@chromium.org <hans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-03 21:28:53 +0000

    Don't use strong enums when compiling with Clang on Windows
    They are exposed at the ABI boundary (some functions take strong enums, and that goes
    into the mangling of the function name). Since we cannot yet compile all of Blink
    with Clang on Windows, let's turn this feature off for now.
    
    BUG=82385
    
    Review URL: https://codereview.chromium.org/142083007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166332 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/Compiler.h
+++ third_party/WebKit/Source/wtf/Compiler.h
@@ -77,7 +77,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WTF_COMPILER_SUPPORTS_CXX_NULLPTR 1
 #endif
 
-#if !COMPILER(CLANG)
+#if COMPILER(CLANG)
+/* Keep strong enums turned off when building with clang-cl: We cannot yet build all of Blink without fallback to cl.exe, and strong enums are exposed at ABI boundaries. */
+#undef WTF_COMPILER_SUPPORTS_CXX_STRONG_ENUMS
+#else
 #define WTF_COMPILER_SUPPORTS_CXX_OVERRIDE_CONTROL 1
 #define WTF_COMPILER_QUIRK_FINAL_IS_CALLED_SEALED 1
 #endif
