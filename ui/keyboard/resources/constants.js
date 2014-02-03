5b17e2cd5ab974200f7c0c8b1aed111f7d36f169
rsadam@chromium.org <rsadam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-03 21:59:50 +0000

    Aligns the Q A Z keys on the a11y keyboard.
    BUG=297132
    
    Review URL: https://codereview.chromium.org/150783002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@248582 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/keyboard/resources/constants.js
+++ ui/keyboard/resources/constants.js
@@ -7,20 +7,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Aspect ratio of keyboard.
  * @type {number}
  */
-var ASPECT_RATIO = 4.5;
+var ASPECT_RATIO = 3.5;
 
 var RowAlignment = {
   STRETCH: "stretch",
   LEFT: "left",
   RIGHT: "right",
-  CENTER: "center"
+  CENTER: "center",
+  // Assigns all extra weight to the right most key. TODO(rsadam): remove.
+  STRETCHRIGHT: "stretchright"
 }
 
 /**
  * Ratio of key height and font size.
  * @type {number}
  */
-var FONT_SIZE_RATIO = 3;
+var FONT_SIZE_RATIO = 2.5;
 
 /**
  * @type {enum}
@@ -62,7 +64,7 @@ var DEFAULT_KEY_WEIGHT_X = 100;
  * The default weight of a key in the Y direction.
  * @type {number}
  */
-var DEFAULT_KEY_WEIGHT_Y = 100;
+var DEFAULT_KEY_WEIGHT_Y = 70;
 
 /**
  * The top padding on each key.
