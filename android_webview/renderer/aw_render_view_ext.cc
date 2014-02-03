3c4d74ef6d4dc2a194a022b1306fa59e281f87a0
ch.dumez@samsung.com <ch.dumez@samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-03 23:40:23 +0000

    Update WebNode::getElementsByTagName() callers to use a WebNodeCollection
    Update WebNode::getElementsByTagName() callers to use a WebNodeCollection
    instead of a WebNodeList. The WebNode::getElementsByTagName() public API was
    updated in Blink r166263 to return a WebNodeCollection instead of a
    WebNodeList.
    
    Callers need to be updated so that we can get rid of the current workaround
    allowing a WebNodeList to be implicitly converted into a WebNodeCollection.
    
    BUG=235008
    
    Review URL: https://codereview.chromium.org/152133004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@248613 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/renderer/aw_render_view_ext.cc
+++ android_webview/renderer/aw_render_view_ext.cc
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/web/WebHitTestResult.h"
 #include "third_party/WebKit/public/web/WebImageCache.h"
 #include "third_party/WebKit/public/web/WebNode.h"
-#include "third_party/WebKit/public/web/WebNodeList.h"
+#include "third_party/WebKit/public/web/WebNodeCollection.h"
 #include "third_party/WebKit/public/web/WebSecurityOrigin.h"
 #include "third_party/WebKit/public/web/WebView.h"
 #include "url/url_canon.h"
@@ -59,10 +59,9 @@ blink::WebNode GetImgChild(const blink::WebNode& node) {
   // This implementation is incomplete (for example if is an area tag) but
   // matches the original WebViewClassic implementation.
 
-  blink::WebNodeList list = node.getElementsByTagName("img");
-  if (list.length() > 0)
-    return list.item(0);
-  return blink::WebNode();
+  blink::WebNodeCollection collection = node.getElementsByTagName("img");
+  DCHECK(!collection.isNull());
+  return collection.firstItem();
 }
 
 bool RemovePrefixAndAssignIfMatches(const base::StringPiece& prefix,
--- components/autofill/content/renderer/form_autofill_util.cc
+++ components/autofill/content/renderer/form_autofill_util.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/web/WebInputElement.h"
 #include "third_party/WebKit/public/web/WebLabelElement.h"
 #include "third_party/WebKit/public/web/WebNode.h"
+#include "third_party/WebKit/public/web/WebNodeCollection.h"
 #include "third_party/WebKit/public/web/WebNodeList.h"
 #include "third_party/WebKit/public/web/WebOptionElement.h"
 #include "third_party/WebKit/public/web/WebSelectElement.h"
@@ -43,6 +44,7 @@ using blink::WebFrame;
 using blink::WebInputElement;
 using blink::WebLabelElement;
 using blink::WebNode;
+using blink::WebNodeCollection;
 using blink::WebNodeList;
 using blink::WebOptionElement;
 using blink::WebSelectElement;
@@ -906,9 +908,11 @@ bool WebFormElementToFormData(
   // element's name as a key into the <name, FormFieldData> map to find the
   // previously created FormFieldData and set the FormFieldData's label to the
   // label.firstChild().nodeValue() of the label element.
-  WebNodeList labels = form_element.getElementsByTagName(kLabel);
-  for (unsigned i = 0; i < labels.length(); ++i) {
-    WebLabelElement label = labels.item(i).to<WebLabelElement>();
+  WebNodeCollection labels = form_element.getElementsByTagName(kLabel);
+  DCHECK(!labels.isNull());
+  for (WebNode item = labels.firstItem(); !item.isNull();
+       item = labels.nextItem()) {
+    WebLabelElement label = item.to<WebLabelElement>();
     WebFormControlElement field_element =
         label.correspondingControl().to<WebFormControlElement>();
 
