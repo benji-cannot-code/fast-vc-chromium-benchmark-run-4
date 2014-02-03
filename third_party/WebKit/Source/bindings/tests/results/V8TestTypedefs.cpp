30fa06453e1bedc30b6e40699fcaece02482bf34
nbarth@chromium.org <nbarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-03 09:38:40 +0000

    IDL compiler: always include GetPtr.h and RefPtr.h
    These headers are used by all meaningful IDL files
    (any interface type argument);
    the current logic is twisty and reflects implementation of Perl and Python
    (i.e., early exit from generation functions),
    so removing it simplifies behavior.
    
    (Test IDLs are v. small and simple, hence often don't have
    these yet, but real IDL files do.)
    
    TBR=haraken
    
    Review URL: https://codereview.chromium.org/145103007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166300 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/tests/results/V8TestTypedefs.cpp
+++ third_party/WebKit/Source/bindings/tests/results/V8TestTypedefs.cpp
@@ -44,6 +44,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/ContextFeatures.h"
 #include "core/dom/Document.h"
 #include "platform/TraceEvent.h"
+#include "wtf/GetPtr.h"
+#include "wtf/RefPtr.h"
 
 namespace WebCore {
 
