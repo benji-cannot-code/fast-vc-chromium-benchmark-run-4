dd401b5bde2f37289234e460372d9bd1a25f3c39
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-03 19:06:45 +0000

    Add GPU bots to the default set of try jobs in ui/gl/ and gpu/.
    R=kbr@chromium.org, piman@chromium.org, piman
    BUG=270918
    
    Review URL: https://codereview.chromium.org/151593002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@248541 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/PRESUBMIT.py
+++ cc/PRESUBMIT.py
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 """Top-level presubmit script for cc.
 
-See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts for
-details on the presubmit API built into gcl.
+See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
+for more details about the presubmit API built into depot_tools.
 """
 
 import re
@@ -228,8 +228,8 @@ def CheckChangeOnUpload(input_api, output_api):
 def GetPreferredTrySlaves(project, change):
   return [
     'linux_layout_rel',
-    'win_gpu',
     'linux_gpu',
     'mac_gpu',
     'mac_gpu_retina',
+    'win_gpu',
   ]
---
+++ gpu/PRESUBMIT.py
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright (c) 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Top-level presubmit script for gpu/.
+
+See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
+for more details about the presubmit API built into depot_tools.
+"""
+
+def GetPreferredTrySlaves(project, change):
+  return [
+    'linux_gpu',
+    'mac_gpu',
+    'mac_gpu_retina',
+    'win_gpu',
+  ]
