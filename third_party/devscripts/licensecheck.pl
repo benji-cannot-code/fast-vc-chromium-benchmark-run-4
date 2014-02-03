3b4c47eae0040d6fb9d2b68605b3107d33dd1d74
szym@chromium.org <szym@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-03 22:02:51 +0000

    Expand licensecheck.pl for public domain
    Accept "into the public domain" as well as "in the public domain"
    as indication of a public domain license.
    
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/151883008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@248584 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/devscripts/licensecheck.pl
+++ third_party/devscripts/licensecheck.pl
@@ -572,7 +572,7 @@ sub parselicense($) {
 	$license = "SGI Free Software License B $license";
     }
 
-    if ($licensetext =~ /in the public domain/i) {
+    if ($licensetext =~ /(in|into) the public domain/i) {
 	$license = "Public domain $license";
     }
 
