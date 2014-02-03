3ec5ba6bbfb59dd5ab3a01c586bcbfde11a9e746
yhirano@chromium.org <yhirano@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-03 03:32:34 +0000

    [WebSocket] Run LayoutTests for the new implementation.
    Turn on ExperimentalWebSocketEnabled runtime feature in LayoutTests.
    Currently all failing tests are skipped, we should fix them later.
    Tests for the old implementation run in virtual/stable/http/tests/websocket.
    
    BUG=339373
    R=ricea@chromium.org, tyoshino@chromium.org
    
    Review URL: https://codereview.chromium.org/150133003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166281 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/base.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/base.py
@@ -1582,6 +1582,9 @@ class Port(object):
             VirtualTestSuite('serviceworker',
                              'http/tests/serviceworker',
                              ['--enable-service-worker']),
+            VirtualTestSuite('stable',
+                             'http/tests/websocket',
+                             ['--stable-release-mode']),
         ]
 
     @memoized
