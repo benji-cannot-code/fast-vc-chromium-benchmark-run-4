68affd77b7171665b24e9c0198d144aa20282127
falken@chromium.org <falken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-03 14:21:42 +0000

    Retry "Replace RenderFullScreen with top layer"
    This patch makes the Fullscreen API use the top layer, as per the latest
    Fullscreen spec. This retry fixes some tests/expectations.
    
    Previously landed as r165904 and r165710
    Codereviews:
    https://codereview.chromium.org/134753003/
    https://codereview.chromium.org/139743005/
    
    BUG=240576, 246077
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=166114
    
    Review URL: https://codereview.chromium.org/142653003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166304 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLPlugInElement.h
+++ third_party/WebKit/Source/core/html/HTMLPlugInElement.h
@@ -134,6 +134,7 @@ private:
     bool m_needsWidgetUpdate;
     bool m_shouldPreferPlugInsForImages;
     DisplayState m_displayState;
+    RefPtr<Widget> m_protectWidgetDuringReattach;
 };
 
 DEFINE_NODE_TYPE_CASTS(HTMLPlugInElement, isPluginElement());
--- third_party/WebKit/Source/core/rendering/RenderVideo.cpp
+++ third_party/WebKit/Source/core/rendering/RenderVideo.cpp
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/frame/FrameView.h"
 #include "core/rendering/LayoutRectRecorder.h"
 #include "core/rendering/PaintInfo.h"
-#include "core/rendering/RenderFullScreen.h"
 #include "platform/graphics/media/MediaPlayer.h"
 #include "public/platform/WebLayer.h"
 
@@ -234,47 +233,6 @@ LayoutUnit RenderVideo::minimumReplacedHeight() const
 bool RenderVideo::supportsAcceleratedRendering() const
 {
     return !!mediaElement()->platformLayer();
-}
-
-static const RenderBlock* rendererPlaceholder(const RenderObject* renderer)
-{
-    RenderObject* parent = renderer->parent();
-    if (!parent)
-        return 0;
-
-    RenderFullScreen* fullScreen = parent->isRenderFullScreen() ? toRenderFullScreen(parent) : 0;
-    if (!fullScreen)
-        return 0;
-
-    return fullScreen->placeholder();
-}
-
-LayoutUnit RenderVideo::offsetLeft() const
-{
-    if (const RenderBlock* block = rendererPlaceholder(this))
-        return block->offsetLeft();
-    return RenderMedia::offsetLeft();
-}
-
-LayoutUnit RenderVideo::offsetTop() const
-{
-    if (const RenderBlock* block = rendererPlaceholder(this))
-        return block->offsetTop();
-    return RenderMedia::offsetTop();
-}
-
-LayoutUnit RenderVideo::offsetWidth() const
-{
-    if (const RenderBlock* block = rendererPlaceholder(this))
-        return block->offsetWidth();
-    return RenderMedia::offsetWidth();
-}
-
-LayoutUnit RenderVideo::offsetHeight() const
-{
-    if (const RenderBlock* block = rendererPlaceholder(this))
-        return block->offsetHeight();
-    return RenderMedia::offsetHeight();
 }
 
 } // namespace WebCore
