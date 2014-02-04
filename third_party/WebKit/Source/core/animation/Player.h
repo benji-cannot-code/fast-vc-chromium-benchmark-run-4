4f8416c90e9ec03fbe7eb71a42d7a3d87de13b28
timloh@chromium.org <timloh@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-04 09:48:11 +0000

    Web Animations: Add Player::reverse()
    Spec at http://dev.w3.org/fxtf/web-animations/#widl-Player-reverse-void.
    
    BUG=334926
    
    Review URL: https://codereview.chromium.org/148983003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166376 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/Player.h
+++ third_party/WebKit/Source/core/animation/Player.h
@@ -60,6 +60,7 @@ public:
     bool paused() const { return m_paused && !m_isPausedForTesting; }
     void pause();
     void play();
+    void reverse();
     bool finished() { return limited(currentTime()); }
 
     double playbackRate() const { return m_playbackRate; }
