06666152518d83a94710da4b27b11a83773bb056
rkc@chromium.org <rkc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-04 01:41:52 +0000

    Add sequenced task runner include to feedback report.
    R=msw@chromium.org, jam@chromium.org
    BUG=None.
    
    Review URL: https://codereview.chromium.org/132703003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@248652 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/feedback/feedback_report.cc
+++ chrome/browser/feedback/feedback_report.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/important_file_writer.h"
 #include "base/guid.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/threading/sequenced_worker_pool.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_thread.h"
 #include "net/base/directory_lister.h"
