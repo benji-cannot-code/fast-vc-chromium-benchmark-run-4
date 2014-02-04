44e25c62edf989c953223c27bb9489ad022e7e6e
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-04 00:45:35 +0000

    Cleanup: do not reuse the ColorChooserAura instances.
    WebContentsImpl now owns ColorChooser instances. We shouldn't
    reuse the instances.
    
    BUG=none
    R=keishi@chromium.org
    TEST=none
    
    Review URL: https://codereview.chromium.org/148203008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@248636 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/color_chooser_aura.cc
+++ chrome/browser/ui/views/color_chooser_aura.cc
@@ -13,8 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/color_chooser/color_chooser_view.h"
 #include "ui/views/widget/widget.h"
 
-ColorChooserAura* ColorChooserAura::current_color_chooser_ = NULL;
-
 ColorChooserAura::ColorChooserAura(content::WebContents* web_contents,
                                    SkColor initial_color)
     : web_contents_(web_contents) {
@@ -48,8 +46,6 @@ void ColorChooserAura::End() {
 }
 
 void ColorChooserAura::DidEndColorChooser() {
-  DCHECK(current_color_chooser_ == this);
-  current_color_chooser_ = NULL;
   if (web_contents_)
     web_contents_->DidEndColorChooser();
 }
@@ -62,11 +58,7 @@ void ColorChooserAura::SetSelectedColor(SkColor color) {
 // static
 ColorChooserAura* ColorChooserAura::Open(
     content::WebContents* web_contents, SkColor initial_color) {
-  if (current_color_chooser_)
-    current_color_chooser_->End();
-  DCHECK(!current_color_chooser_);
-  current_color_chooser_ = new ColorChooserAura(web_contents, initial_color);
-  return current_color_chooser_;
+  return new ColorChooserAura(web_contents, initial_color);
 }
 
 #if !defined(OS_WIN)
