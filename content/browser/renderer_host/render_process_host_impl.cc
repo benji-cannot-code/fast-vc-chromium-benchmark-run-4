05fb67dbcde9c422f83f03a2817564f5b32666fb
wfh@chromium.org <wfh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-04 01:44:59 +0000

    Remove STANDARD_RIGHTS_REQUIRED from DIB section
    BUG=307301
    
    Review URL: https://codereview.chromium.org/140123008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@248653 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1250,7 +1250,7 @@ TransportDIB* RenderProcessHostImpl::MapTransportDIB(
   // On Windows we need to duplicate the handle from the remote process
   HANDLE section;
   DuplicateHandle(GetHandle(), dib_id.handle, GetCurrentProcess(), &section,
-                  STANDARD_RIGHTS_REQUIRED | FILE_MAP_READ | FILE_MAP_WRITE,
+                  FILE_MAP_READ | FILE_MAP_WRITE,
                   FALSE, 0);
   return TransportDIB::Map(section);
 #elif defined(TOOLKIT_GTK)
