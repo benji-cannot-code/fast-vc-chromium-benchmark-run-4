8bf5f47d4f6d73cb4fdb0dfceb7bc2b578ae46dc
dewittj@chromium.org <dewittj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-04 05:05:48 +0000

    Fix regression causing text to block part of notifications' small image.
    On mac the text labels need to render an opaque background to get
    subpixel text antialiasing, but the label was too wide and blocks other
    UI elements.  This narrows the width of the notifications' content area.
    
    BUG=335794
    
    Review URL: https://codereview.chromium.org/149573007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@248679 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/message_center/cocoa/notification_controller.mm
+++ ui/message_center/cocoa/notification_controller.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ui/message_center/cocoa/notification_controller.h"
 
+#include <algorithm>
+
 #include "base/mac/foundation_util.h"
 #include "base/strings/string_util.h"
 #include "base/strings/sys_string_conversions.h"
@@ -268,19 +270,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self configureCloseButtonInFrame:rootFrame];
   [rootView addSubview:closeButton_];
 
+  // Create the small image.
   [self configureSmallImageInFrame:rootFrame];
   [[self view] addSubview:smallImage_];
 
+  NSRect contentFrame = [self currentContentRect];
+
   // Create the title.
-  [self configureTitleInFrame:rootFrame];
+  [self configureTitleInFrame:contentFrame];
   [rootView addSubview:title_];
 
   // Create the message body.
-  [self configureBodyInFrame:rootFrame];
+  [self configureBodyInFrame:contentFrame];
   [rootView addSubview:message_];
 
   // Create the context message body.
-  [self configureContextMessageInFrame:rootFrame];
+  [self configureContextMessageInFrame:contentFrame];
   [rootView addSubview:contextMessage_];
 
   // Populate the data.
@@ -696,20 +701,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [smallImage_ setAutoresizingMask:NSViewMinYMargin];
 }
 
-- (void)configureTitleInFrame:(NSRect)rootFrame {
-  NSRect frame = [self currentContentRect];
-  frame.size.height = 0;
-  title_.reset([self newLabelWithFrame:frame]);
+- (void)configureTitleInFrame:(NSRect)contentFrame {
+  contentFrame.size.height = 0;
+  title_.reset([self newLabelWithFrame:contentFrame]);
   [title_ setAutoresizingMask:NSViewMinYMargin];
   [title_ setTextColor:gfx::SkColorToCalibratedNSColor(
       message_center::kRegularTextColor)];
   [title_ setFont:[NSFont messageFontOfSize:message_center::kTitleFontSize]];
 }
 
-- (void)configureBodyInFrame:(NSRect)rootFrame {
-  NSRect frame = [self currentContentRect];
-  frame.size.height = 0;
-  message_.reset([self newLabelWithFrame:frame]);
+- (void)configureBodyInFrame:(NSRect)contentFrame {
+  contentFrame.size.height = 0;
+  message_.reset([self newLabelWithFrame:contentFrame]);
   [message_ setAutoresizingMask:NSViewMinYMargin];
   [message_ setTextColor:gfx::SkColorToCalibratedNSColor(
       message_center::kRegularTextColor)];
@@ -717,10 +720,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       [NSFont messageFontOfSize:message_center::kMessageFontSize]];
 }
 
-- (void)configureContextMessageInFrame:(NSRect)rootFrame {
-  NSRect frame = [self currentContentRect];
-  frame.size.height = 0;
-  contextMessage_.reset([self newLabelWithFrame:frame]);
+- (void)configureContextMessageInFrame:(NSRect)contentFrame {
+  contentFrame.size.height = 0;
+  contextMessage_.reset([self newLabelWithFrame:contentFrame]);
   [contextMessage_ setAutoresizingMask:NSViewMinYMargin];
   [contextMessage_ setTextColor:gfx::SkColorToCalibratedNSColor(
       message_center::kDimTextColor)];
@@ -747,12 +749,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (NSRect)currentContentRect {
   DCHECK(icon_);
   DCHECK(closeButton_);
+  DCHECK(smallImage_);
 
   NSRect iconFrame, contentFrame;
   NSDivideRect([[self view] bounds], &iconFrame, &contentFrame,
       NSWidth([icon_ frame]) + message_center::kIconToTextPadding,
       NSMinXEdge);
-  contentFrame.size.width -= NSWidth([closeButton_ frame]);
+  // The content area is between the icon on the left and the control area
+  // on the right.
+  int controlAreaWidth =
+      std::max(NSWidth([closeButton_ frame]), NSWidth([smallImage_ frame]));
+  contentFrame.size.width -=
+      2 * message_center::kSmallImagePadding + controlAreaWidth;
   return contentFrame;
 }
 
