d6baffb2b6d68323d8235e5d9a8a8c8b9c22821b
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-04 07:03:30 +0000

    x11/test: Use a custom-deleter for the XEvent in ScopedXI2Event.
    BUG=none
    R=sky@chromium.org
    
    Previously committed in r248512, and reverted in r248526 because it introduced leaks.
    
    Review URL: https://codereview.chromium.org/136153006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@248683 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/events/test/events_test_utils_x11.h
+++ ui/events/test/events_test_utils_x11.h
@@ -25,6 +25,10 @@ struct Valuator {
   double value;
 };
 
+struct XEventDeleter {
+  void operator()(XEvent* event);
+};
+
 class ScopedXI2Event {
  public:
   ScopedXI2Event();
@@ -74,7 +78,7 @@ class ScopedXI2Event {
 
   void SetUpValuators(const std::vector<Valuator>& valuators);
 
-  scoped_ptr<XEvent> event_;
+  scoped_ptr<XEvent, XEventDeleter> event_;
 
   DISALLOW_COPY_AND_ASSIGN(ScopedXI2Event);
 };
