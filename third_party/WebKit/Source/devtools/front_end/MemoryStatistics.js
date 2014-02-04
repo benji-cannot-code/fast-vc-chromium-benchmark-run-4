b489670163b791efd65fd62ca951e36958b326ce
alph@chromium.org <alph@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-04 16:32:25 +0000

    DevTools: Fix origin offset when clicking in timeline memory counters.
    R=loislo@chromium.org
    
    Review URL: https://codereview.chromium.org/138803008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166413 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/MemoryStatistics.js
+++ third_party/WebKit/Source/devtools/front_end/MemoryStatistics.js
@@ -302,7 +302,7 @@ WebInspector.MemoryStatistics.prototype = {
      */
     _onClick: function(event)
     {
-        var x = event.x - event.target.offsetParent.offsetLeft;
+        var x = event.x - this._canvasContainer.offsetParent.offsetLeft;
         var minDistance = Infinity;
         var bestTime;
         for (var i = 0; i < this._counterUI.length; ++i) {
