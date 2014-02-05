ce4c7e44335f0dfebfc3ffbb2bf404e5fc008d24
gavinp@chromium.org <gavinp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-05 06:28:06 +0000

    Parse blink platform log channel command line args in webkit unit tests.
    This should make debugging webkit unit tests a bit easier.
    
    R=jochen@chromium.org,japhet@chromium.org
    BUG=None
    
    Review URL: https://codereview.chromium.org/139783014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@248877 0039d316-1c4b-4281-b951-d872f2087c98

--- content/test/webkit_support.cc
+++ content/test/webkit_support.cc
@@ -5,9 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/test/webkit_support.h"
 
+#include <string>
+
+#include "base/command_line.h"
 #include "base/message_loop/message_loop.h"
 #include "base/path_service.h"
 #include "base/run_loop.h"
+#include "base/strings/string_tokenizer.h"
+#include "content/public/common/content_switches.h"
 #include "content/test/test_webkit_platform_support.h"
 #include "third_party/WebKit/public/web/WebCache.h"
 #include "third_party/WebKit/public/web/WebKit.h"
@@ -29,6 +34,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 namespace {
+
+void EnableBlinkPlatformLogChannels(const std::string& channels) {
+  if (channels.empty())
+    return;
+  base::StringTokenizer t(channels, ", ");
+  while (t.GetNext())
+    blink::enableLogChannel(t.token().c_str());
+}
+
+void ParseBlinkCommandLineArgumentsForUnitTests() {
+  const CommandLine& command_line = *CommandLine::ForCurrentProcess();
+  EnableBlinkPlatformLogChannels(
+      command_line.GetSwitchValueASCII(switches::kBlinkPlatformLogChannels));
+}
 
 class TestEnvironment {
  public:
@@ -73,6 +92,8 @@ TestEnvironment* test_environment;
 }  // namespace
 
 void SetUpTestEnvironmentForUnitTests() {
+  ParseBlinkCommandLineArgumentsForUnitTests();
+
   blink::WebRuntimeFeatures::enableStableFeatures(true);
   blink::WebRuntimeFeatures::enableExperimentalFeatures(true);
   blink::WebRuntimeFeatures::enableTestOnlyFeatures(true);
