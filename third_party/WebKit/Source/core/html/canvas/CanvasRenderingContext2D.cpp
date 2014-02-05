e4d7d4e9843f39379944f9927416fa2d5e93cde1
junov@chromium.org <junov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-05 19:08:47 +0000

    2D canvas: Changing exceptions thrown by drawImage to reflect changes to spec
    Cases where source rect or source image have 0 size should fail silently
    
    BUG=309020
    R=senorblanco@chromium.org, senorblanco
    
    Review URL: https://codereview.chromium.org/138133017
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166532 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/canvas/CanvasRenderingContext2D.cpp
+++ third_party/WebKit/Source/core/html/canvas/CanvasRenderingContext2D.cpp
@@ -1335,12 +1335,8 @@ void CanvasRenderingContext2D::drawImage(ImageBitmap* bitmap,
         || !std::isfinite(srcRect.x()) || !std::isfinite(srcRect.y()) || !std::isfinite(srcRect.width()) || !std::isfinite(srcRect.height()))
         return;
 
-    if (!dstRect.width() || !dstRect.height())
+    if (!dstRect.width() || !dstRect.height() || !srcRect.width() || !srcRect.height())
         return;
-    if (!srcRect.width() || !srcRect.height()) {
-        exceptionState.throwDOMException(IndexSizeError, String::format("The source %s is 0.", srcRect.width() ? "height" : "width"));
-        return;
-    }
 
     ASSERT(bitmap->height() && bitmap->width());
     FloatRect normalizedSrcRect = normalizeRect(srcRect);
@@ -1420,22 +1416,13 @@ void CanvasRenderingContext2D::drawImage(HTMLImageElement* image, const FloatRec
         return;
 
     LayoutSize size = sizeFor(image, ImageSizeBeforeDevicePixelRatio);
-    if (!size.width() || !size.height()) {
-        exceptionState.throwDOMException(InvalidStateError, String::format("The source %s is 0.", size.width() ? "height" : "width"));
-        return;
-    }
-
-    if (!dstRect.width() || !dstRect.height())
+    if (!size.width() || !size.height() || !dstRect.width() || !dstRect.height() || !srcRect.width() || !srcRect.height())
         return;
 
     FloatRect normalizedSrcRect = normalizeRect(srcRect);
     FloatRect normalizedDstRect = normalizeRect(dstRect);
 
     FloatRect imageRect = FloatRect(FloatPoint(), size);
-    if (!srcRect.width() || !srcRect.height()) {
-        exceptionState.throwDOMException(IndexSizeError, String::format("The source %s is 0.", srcRect.width() ? "height" : "width"));
-        return;
-    }
     if (!imageRect.intersects(normalizedSrcRect))
         return;
 
@@ -1482,15 +1469,8 @@ void CanvasRenderingContext2D::drawImage(HTMLCanvasElement* sourceCanvas, const 
 
     FloatRect srcCanvasRect = FloatRect(FloatPoint(), sourceCanvas->size());
 
-    if (!srcCanvasRect.width() || !srcCanvasRect.height()) {
-        exceptionState.throwDOMException(InvalidStateError, String::format("The source canvas %s is 0.", srcCanvasRect.width() ? "height" : "width"));
+    if (!srcCanvasRect.width() || !srcCanvasRect.height() || !srcRect.width() || !srcRect.height())
         return;
-    }
-
-    if (!srcRect.width() || !srcRect.height()) {
-        exceptionState.throwDOMException(IndexSizeError, String::format("The source %s is 0.", srcRect.width() ? "height" : "width"));
-        return;
-    }
 
     FloatRect normalizedSrcRect = normalizeRect(srcRect);
     FloatRect normalizedDstRect = normalizeRect(dstRect);
@@ -1584,12 +1564,10 @@ void CanvasRenderingContext2D::drawImage(HTMLVideoElement* video, const FloatRec
 
     if (video->readyState() == HTMLMediaElement::HAVE_NOTHING || video->readyState() == HTMLMediaElement::HAVE_METADATA)
         return;
+    if (!srcRect.width() || !srcRect.height())
+        return;
 
     FloatRect videoRect = FloatRect(FloatPoint(), sizeFor(video));
-    if (!srcRect.width() || !srcRect.height()) {
-        exceptionState.throwDOMException(IndexSizeError, String::format("The source %s is 0.", srcRect.width() ? "height" : "width"));
-        return;
-    }
 
     FloatRect normalizedSrcRect = normalizeRect(srcRect);
     FloatRect normalizedDstRect = normalizeRect(dstRect);
