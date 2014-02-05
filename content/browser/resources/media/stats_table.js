a4ce53e0da9d316af626071548b988e80b6c7f77
jiayl@chromium.org <jiayl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-05 06:41:35 +0000

    Adds URL to each peerconnection tab title on webrtc-internals. Also use bold font for the stats table of the active ICE connection for highlight.
    BUG=244648
    
    Review URL: https://codereview.chromium.org/68103011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@248882 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/resources/media/stats_table.js
+++ content/browser/resources/media/stats_table.js
@@ -84,6 +84,7 @@ var StatsTable = (function(ssrcInfoManager) {
         var container = this.ensureStatsTableContainer_(peerConnectionElement);
         var details = document.createElement('details');
         container.appendChild(details);
+
         var summary = document.createElement('summary');
         summary.textContent = report.id;
         details.appendChild(summary);
@@ -140,6 +141,10 @@ var StatsTable = (function(ssrcInfoManager) {
         trElement.innerHTML = '<td>' + rowName + '</td><td></td>';
       }
       trElement.cells[1].textContent = value;
+
+      // Highlights the table for the active connection.
+      if (rowName == 'googActiveConnection' && value == 'true')
+        statsTable.parentElement.classList.add('stats-table-active-connection');
     }
   };
 
