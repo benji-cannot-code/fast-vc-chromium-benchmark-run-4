9683d5f938a98b68c6c25f3b4fe5b72bcd8b895a
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-05 23:56:36 +0000

    Update WebFrameClient implementations to maintain frame tree.
    BUG=340002
    R=boliu@chromium.org, creis@chromium.org, nasko@chromium.org, thestig@chromium.org
    
    Review URL: https://codereview.chromium.org/148353003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249168 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -722,6 +722,7 @@ blink::WebFrame* RenderFrameImpl::createChildFrame(
                                                 child_frame_identifier);
   // TODO(nasko): Over-conservative check for debugging.
   CHECK(web_frame);
+  parent->appendChild(web_frame);
   child_render_frame->SetWebFrame(web_frame);
 
   return web_frame;
@@ -770,6 +771,9 @@ void RenderFrameImpl::frameDetached(blink::WebFrame* frame) {
   CHECK(it != g_frame_map.Get().end());
   CHECK_EQ(it->second, this);
   g_frame_map.Get().erase(it);
+
+  if (is_subframe)
+    frame->parent()->removeChild(frame);
 
   // |frame| is invalid after here.
   frame->close();
