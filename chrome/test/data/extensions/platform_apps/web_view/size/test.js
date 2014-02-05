5a0deb86ac1f0040d8a3a56966e710030e9da67d
fsamuel@chromium.org <fsamuel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-05 18:42:35 +0000

    <webview>: Remove WebViewTest.Size
    This is a very old test that is no longer very relevant to <webview>.
    
    BUG=329032
    
    Review URL: https://codereview.chromium.org/155103002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249061 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/platform_apps/web_view/size/test.js
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2013 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('main.html', {}, function () {});
-});
