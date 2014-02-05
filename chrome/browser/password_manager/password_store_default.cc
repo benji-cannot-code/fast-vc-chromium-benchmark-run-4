bc87e5ba3dde36e0479ace1eda264867f0a477b9
dubroy@chromium.org <dubroy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-05 11:10:35 +0000

    Eliminate BrowserThread usage from PasswordStoreDefault.
    As part of componentizing the password manager, we need to remove all
    references to content/ from PasswordStoreDefault.
    
    Depends on https://codereview.chromium.org/152683002/.
    
    BUG=335147
    R=gcasto@chromium.org
    
    Review URL: https://codereview.chromium.org/153943002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@248964 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/password_manager/password_store_default.cc
+++ chrome/browser/password_manager/password_store_default.cc
@@ -15,11 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/pref_names.h"
-#include "content/public/browser/browser_thread.h"
 #include "content/public/browser/notification_service.h"
 
 using autofill::PasswordForm;
-using content::BrowserThread;
 
 PasswordStoreDefault::PasswordStoreDefault(
     scoped_refptr<base::SingleThreadTaskRunner> main_thread_runner,
@@ -37,12 +35,12 @@ PasswordStoreDefault::~PasswordStoreDefault() {
 }
 
 void PasswordStoreDefault::ShutdownOnUIThread() {
-  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+  DCHECK(main_thread_runner_->BelongsToCurrentThread());
   profile_ = NULL;
 }
 
 void PasswordStoreDefault::ReportMetricsImpl() {
-  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::DB));
+  DCHECK(GetBackgroundTaskRunner()->BelongsToCurrentThread());
   login_db_->ReportMetrics();
 }
 
@@ -123,12 +121,12 @@ void PasswordStoreDefault::GetBlacklistLoginsImpl(
 
 bool PasswordStoreDefault::FillAutofillableLogins(
          std::vector<PasswordForm*>* forms) {
-  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::DB));
+  DCHECK(GetBackgroundTaskRunner()->BelongsToCurrentThread());
   return login_db_->GetAutofillableLogins(forms);
 }
 
 bool PasswordStoreDefault::FillBlacklistLogins(
          std::vector<PasswordForm*>* forms) {
-  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::DB));
+  DCHECK(GetBackgroundTaskRunner()->BelongsToCurrentThread());
   return login_db_->GetBlacklistLogins(forms);
 }
