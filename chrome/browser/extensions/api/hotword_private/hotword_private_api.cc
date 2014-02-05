d54954a30e8891f9a2794595aa20550dd0ba365e
rlp@chromium.org <rlp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-05 17:03:42 +0000

    [Hotword] Some small fixes to the hotword_private extension api.
    BUG=325439, 289023
    
    Review URL: https://codereview.chromium.org/154683002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249026 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/hotword_private/hotword_private_api.cc
+++ chrome/browser/extensions/api/hotword_private/hotword_private_api.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/lazy_instance.h"
 #include "base/prefs/pref_service.h"
-#include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search/hotword_service.h"
 #include "chrome/browser/search/hotword_service_factory.h"
@@ -88,7 +87,7 @@ bool HotwordPrivateGetStatusFunction::RunImpl() {
 
   PrefService* prefs = GetProfile()->GetPrefs();
   result.enabled =
-      prefs->GetBoolean(prefs::kHotwordSearchEnabled) && result.available;
+      prefs->GetBoolean(prefs::kHotwordSearchEnabled);
 
   SetResult(result.ToValue().release());
   return true;
