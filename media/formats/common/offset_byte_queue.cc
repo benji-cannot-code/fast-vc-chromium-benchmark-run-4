b09269276663ae46b1b2eefda799ea9865afa6b0
damienv@chromium.org <damienv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-05 04:10:02 +0000

    Move offset_byte_queue.* from media/mp4 to media/common.
    This is the first step to using OffsetByteQueue in the Mpeg2 TS stream parser.
    
    BUG=340426
    
    Review URL: https://codereview.chromium.org/153103003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@248841 0039d316-1c4b-4281-b951-d872f2087c98

--- media/formats/common/offset_byte_queue.cc
+++ media/formats/common/offset_byte_queue.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "media/formats/mp4/offset_byte_queue.h"
+#include "media/formats/common/offset_byte_queue.h"
 
 #include "base/basictypes.h"
 #include "base/logging.h"
--- media/formats/common/offset_byte_queue.h
+++ media/formats/common/offset_byte_queue.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef MEDIA_FORMATS_MP4_OFFSET_BYTE_QUEUE_H_
-#define MEDIA_FORMATS_MP4_OFFSET_BYTE_QUEUE_H_
+#ifndef MEDIA_FORMATS_COMMON_OFFSET_BYTE_QUEUE_H_
+#define MEDIA_FORMATS_COMMON_OFFSET_BYTE_QUEUE_H_
 
 #include "base/basictypes.h"
 #include "media/base/byte_queue.h"
@@ -64,4 +64,4 @@ class MEDIA_EXPORT OffsetByteQueue {
 
 }  // namespace media
 
-#endif  // MEDIA_FORMATS_MP4_MP4_STREAM_PARSER_H_
+#endif  // MEDIA_FORMATS_COMMON_OFFSET_BYTE_QUEUE_H_
