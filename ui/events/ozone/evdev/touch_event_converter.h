02f7cd7a9c2968c43c5a86b21ce544ac6badd657
spang@chromium.org <spang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-05 21:27:40 +0000

    evdev: Add absolute axis information to EventDeviceInfo
    Move the ioctls to EventDeviceInfo, where the rest of the device
    information is retrieved.
    
    Review URL: https://codereview.chromium.org/148213014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249109 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/events/ozone/evdev/touch_event_converter.h
+++ ui/events/ozone/evdev/touch_event_converter.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/event_constants.h"
 #include "ui/events/events_export.h"
 #include "ui/events/ozone/evdev/event_converter.h"
+#include "ui/events/ozone/evdev/event_device_info.h"
 
 namespace ui {
 
@@ -26,7 +27,9 @@ class EVENTS_EXPORT TouchEventConverterEvdev
   enum {
     MAX_FINGERS = 11
   };
-  TouchEventConverterEvdev(int fd, base::FilePath path);
+  TouchEventConverterEvdev(int fd,
+                           base::FilePath path,
+                           const EventDeviceInfo& info);
   virtual ~TouchEventConverterEvdev();
 
  private:
@@ -51,8 +54,12 @@ class EVENTS_EXPORT TouchEventConverterEvdev
   float x_scale_;
   float y_scale_;
 
-  // Maximum coordinate-values allowed for the events.
+  // Range for x-axis.
+  int x_min_;
   int x_max_;
+
+  // Range for y-axis.
+  int y_min_;
   int y_max_;
 
   // Touch point currently being updated from the /dev/input/event* stream.
