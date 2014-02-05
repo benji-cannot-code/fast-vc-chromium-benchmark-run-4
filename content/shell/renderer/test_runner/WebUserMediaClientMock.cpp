70254733fed68a78d76b36dc3b63938458320dc1
tommyw@chromium.org <tommyw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-05 10:38:55 +0000

    MediaStream API: Patch #2 of implementing navigator.getMediaDevices This patch just adds basic mock support so that getMediaDevices can be exercised in blink layout tests.
    BUG=338511
    
    Review URL: https://codereview.chromium.org/136123005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@248956 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/renderer/test_runner/WebUserMediaClientMock.cpp
+++ content/shell/renderer/test_runner/WebUserMediaClientMock.cpp
@@ -8,11 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/shell/renderer/test_runner/MockConstraints.h"
 #include "content/shell/renderer/test_runner/WebTestDelegate.h"
 #include "third_party/WebKit/public/platform/WebMediaConstraints.h"
+#include "third_party/WebKit/public/platform/WebMediaDeviceInfo.h"
 #include "third_party/WebKit/public/platform/WebMediaStream.h"
 #include "third_party/WebKit/public/platform/WebMediaStreamSource.h"
 #include "third_party/WebKit/public/platform/WebMediaStreamTrack.h"
 #include "third_party/WebKit/public/platform/WebVector.h"
 #include "third_party/WebKit/public/web/WebDocument.h"
+#include "third_party/WebKit/public/web/WebMediaDevicesRequest.h"
 #include "third_party/WebKit/public/web/WebMediaStreamRegistry.h"
 #include "third_party/WebKit/public/web/WebUserMediaRequest.h"
 
@@ -76,6 +78,25 @@ private:
     WebUserMediaRequest m_request;
 };
 
+class MediaDevicesRequestTask : public WebMethodTask<WebUserMediaClientMock> {
+public:
+    MediaDevicesRequestTask(WebUserMediaClientMock* object, const WebMediaDevicesRequest& request, const WebVector<WebMediaDeviceInfo>& result)
+        : WebMethodTask<WebUserMediaClientMock>(object)
+        , m_request(request)
+        , m_result(result)
+    {
+    }
+
+    virtual void runIfValid() OVERRIDE
+    {
+        m_request.requestSucceeded(m_result);
+    }
+
+private:
+    WebMediaDevicesRequest m_request;
+    WebVector<WebMediaDeviceInfo> m_result;
+};
+
 ////////////////////////////////
 
 class MockExtraData : public WebMediaStream::ExtraData {
@@ -136,6 +157,22 @@ void WebUserMediaClientMock::requestUserMedia(const WebUserMediaRequest& streamR
 }
 
 void WebUserMediaClientMock::cancelUserMediaRequest(const WebUserMediaRequest&)
+{
+}
+
+void WebUserMediaClientMock::requestMediaDevices(const WebMediaDevicesRequest& request)
+{
+    const size_t three = 3;
+    WebVector<WebMediaDeviceInfo> devices(three);
+
+    devices[0].initialize("device1", WebMediaDeviceInfo::MediaDeviceKindAudioInput, "Built-in microphone", "group1");
+    devices[1].initialize("device2", WebMediaDeviceInfo::MediaDeviceKindAudioOutput, "Built-in speakers", "group1");
+    devices[2].initialize("device3", WebMediaDeviceInfo::MediaDeviceKindVideoInput, "Build-in webcam", "group2");
+
+    m_delegate->postTask(new MediaDevicesRequestTask(this, request, devices));
+}
+
+void WebUserMediaClientMock::cancelMediaDevicesRequest(const WebMediaDevicesRequest&)
 {
 }
 
