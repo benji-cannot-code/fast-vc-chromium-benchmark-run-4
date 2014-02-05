739593b3972c3d45a23006ad1cbcac2ffff75ea7
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-05 09:01:02 +0000

    [SyncFS] Refactor MetadataDatabase
     * Add StoreFile{Metadata,Tracker}
     * Rename GetNextTrackerID to IncrementTrackerID
    
    BUG=331988
    
    Review URL: https://codereview.chromium.org/153663003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@248935 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_backend/metadata_database.h
+++ chrome/browser/sync_file_system/drive_backend/metadata_database.h
@@ -419,7 +419,7 @@ class MetadataDatabase {
   void EraseFileFromDatabase(const std::string& file_id,
                              leveldb::WriteBatch* batch);
 
-  int64 GetNextTrackerID(leveldb::WriteBatch* batch);
+  int64 IncrementTrackerID(leveldb::WriteBatch* batch);
 
   void RecursiveMarkTrackerAsDirty(int64 root_tracker_id,
                                    leveldb::WriteBatch* batch);
@@ -431,6 +431,8 @@ class MetadataDatabase {
   bool HasActiveTrackerForPath(int64 parent_tracker,
                                const std::string& title) const;
 
+  void RemoveUnneededTrackersForMissingFile(const std::string& file_id,
+                                            leveldb::WriteBatch* batch);
   void UpdateByFileMetadata(const tracked_objects::Location& from_where,
                             scoped_ptr<FileMetadata> file,
                             leveldb::WriteBatch* batch);
@@ -442,6 +444,9 @@ class MetadataDatabase {
 
   scoped_ptr<base::ListValue> DumpTrackers();
   scoped_ptr<base::ListValue> DumpMetadata();
+
+  void StoreFileMetadata(scoped_ptr<FileMetadata> file_metadata);
+  void StoreFileTracker(scoped_ptr<FileTracker> file_tracker);
 
   scoped_refptr<base::SequencedTaskRunner> task_runner_;
   base::FilePath database_path_;
