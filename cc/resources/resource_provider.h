1ca15eac101df01984496181ccc84a6a4f3a0222
dongseong.hwang@intel.com <dongseong.hwang@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-05 05:30:46 +0000

    cc: Use ResourceProvider::Resouce::type to check the type of the given resource.
    Currently, some code uses the type variable to check the type of
    the given resource, while other code uses the shared_bitmap,
    pixels or mailbox variable to check the same condition.
    
    This CL makes all code use the type variable for this purpose.
    
    In addition, this CL clarify texture_pool can be non-null only if
    the type is Texture and the origin is Internal.
    
    In addition, add several DCHECKs to increase readability.
    
    BUG=337519
    
    Review URL: https://codereview.chromium.org/135273008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@248861 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/resources/resource_provider.h
+++ cc/resources/resource_provider.h
@@ -364,10 +364,13 @@ class CC_EXPORT ResourceProvider {
 
  private:
   struct Resource {
+    enum Origin { Internal, External, Delegated };
+
     Resource();
     ~Resource();
     Resource(unsigned texture_id,
              const gfx::Size& size,
+             Origin origin,
              GLenum target,
              GLenum filter,
              GLenum texture_pool,
@@ -377,6 +380,7 @@ class CC_EXPORT ResourceProvider {
     Resource(uint8_t* pixels,
              SharedBitmap* bitmap,
              const gfx::Size& size,
+             Origin origin,
              GLenum filter,
              GLint wrap_mode);
 
@@ -394,11 +398,6 @@ class CC_EXPORT ResourceProvider {
     int imported_count;
     int exported_count;
     bool locked_for_write;
-    enum Origin {
-      Internal,
-      External,
-      Delegated,
-    };
     Origin origin;
     bool marked_for_deletion;
     bool pending_set_pixels;
