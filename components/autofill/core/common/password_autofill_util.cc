5c488328f84ccca28166ad480300b1497be528df
jww@chromium.org <jww@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-05 06:54:43 +0000

    Password manager now ignores autocomplete='off' by default; user may specify a flag that says otherwise.
    (Recommit after original commit reverted in https://codereview.chromium.org/130183003 due to unit test memory leak.)
    
    BUG=177288
    
    Review URL: https://codereview.chromium.org/146023002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@248885 0039d316-1c4b-4281-b951-d872f2087c98

--- components/autofill/core/common/password_autofill_util.cc
+++ components/autofill/core/common/password_autofill_util.cc
@@ -10,11 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 
-// We ignore autocomplete='off' if the user has specified the command line
-// feature to enable it.
+// We ignore autocomplete='off' unless the user has specified the command line
+// flag instructing otherwise.
 bool ShouldIgnoreAutocompleteOffForPasswordFields() {
-  return CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kEnableIgnoreAutocompleteOff);
+  return !CommandLine::ForCurrentProcess()->HasSwitch(
+      switches::kDisableIgnoreAutocompleteOff);
 }
 
 }  // namespace autofill
