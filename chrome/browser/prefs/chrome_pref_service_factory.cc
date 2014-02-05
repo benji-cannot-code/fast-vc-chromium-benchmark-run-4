a0205f4484a70792cd245a59f94a50031b817bbb
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-05 09:08:28 +0000

    Linux: Remove PrefService.FileSystemType histogram.
    The answer is < 1% networked file systems, ~5% "other".
    
    Review URL: https://codereview.chromium.org/134073004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@248937 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prefs/chrome_pref_service_factory.cc
+++ chrome/browser/prefs/chrome_pref_service_factory.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/compiler_specific.h"
 #include "base/debug/trace_event.h"
-#include "base/file_util.h"
 #include "base/files/file_path.h"
 #include "base/metrics/field_trial.h"
 #include "base/metrics/histogram.h"
@@ -216,19 +215,6 @@ void PrepareBuilder(
     scoped_ptr<PrefHashStore> pref_hash_store,
     const scoped_refptr<PrefStore>& extension_prefs,
     bool async) {
-#if defined(OS_LINUX)
-  // We'd like to see what fraction of our users have the preferences
-  // stored on a network file system, as we've had no end of troubles
-  // with NFS/AFS.
-  // TODO(evanm): remove this once we've collected state.
-  file_util::FileSystemType fstype;
-  if (file_util::GetFileSystemType(pref_filename.DirName(), &fstype)) {
-    UMA_HISTOGRAM_ENUMERATION("PrefService.FileSystemType",
-                              static_cast<int>(fstype),
-                              file_util::FILE_SYSTEM_TYPE_COUNT);
-  }
-#endif
-
 #if defined(ENABLE_CONFIGURATION_POLICY)
   using policy::ConfigurationPolicyPrefStore;
   factory->set_managed_prefs(
