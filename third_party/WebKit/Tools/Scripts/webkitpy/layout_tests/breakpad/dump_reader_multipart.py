4f6ba6767e57c4697bc5f0faf822119ee7d1a177
sigbjornf@opera.com <sigbjornf@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-05 17:26:16 +0000

    Update breakpad to refer to libtest_netscape_plugin.so
    libTestNetscapePlugIn.so has been renamed to libtest_netscape_plugin.so, sync.
    
    R=jochen@chromium.org
    BUG=341016
    
    Review URL: https://codereview.chromium.org/146913007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166515 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/breakpad/dump_reader_multipart.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/breakpad/dump_reader_multipart.py
@@ -160,7 +160,7 @@ class DumpReaderLinux(DumpReaderMultipart):
     """Linux breakpad dump reader."""
 
     def _binaries_to_symbolize(self):
-        return ['content_shell', 'libTestNetscapePlugIn.so', 'libffmpegsumo.so', 'libosmesa.so']
+        return ['content_shell', 'libtest_netscape_plugin.so', 'libffmpegsumo.so', 'libosmesa.so']
 
     def _file_extension(self):
         return 'dmp'
