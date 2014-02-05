b2f15b308fa698d951356514ed131e228568c09e
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-05 16:18:03 +0000

    [SyncFS] Clean up MetadataDatabase::PopulateInitialData
     * Rename PutFileToBatch to PutFileMetadataToBatch
     * Rename PutTrackerToBatch to PutFileTrackerToBatch
     * Split PopulateInitialData implementation to AttachSyncRoot and AttachInitialAppRoot
    
    BUG=331988
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/140433008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249018 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_backend/drive_backend_util.cc
+++ chrome/browser/sync_file_system/drive_backend/drive_backend_util.cc
@@ -30,7 +30,8 @@ void PutServiceMetadataToBatch(const ServiceMetadata& service_metadata,
   batch->Put(kServiceMetadataKey, value);
 }
 
-void PutFileToBatch(const FileMetadata& file, leveldb::WriteBatch* batch) {
+void PutFileMetadataToBatch(const FileMetadata& file,
+                            leveldb::WriteBatch* batch) {
   if (!batch)
     return;
 
@@ -40,7 +41,8 @@ void PutFileToBatch(const FileMetadata& file, leveldb::WriteBatch* batch) {
   batch->Put(kFileMetadataKeyPrefix + file.file_id(), value);
 }
 
-void PutTrackerToBatch(const FileTracker& tracker, leveldb::WriteBatch* batch) {
+void PutFileTrackerToBatch(const FileTracker& tracker,
+                           leveldb::WriteBatch* batch) {
   if (!batch)
     return;
 
