da6cc5dfd1b02af8f8d908c5516e6fa0fda4706b
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-05 22:10:26 +0000

    Move insertAdjacentElement/insertAdjacentText to Element.
    They were moved in https://src.chromium.org/viewvc/blink?revision=163529&view=revision
    to match http://domparsing.spec.whatwg.org/#insertadjacenthtml.
    
    The domparsing spec doesn't have insertAdjacentText/insertAdjacentElement.
    Unless we remove them, they really should function the same as
    insertAdjacentHTML.
    
    Review URL: https://codereview.chromium.org/147993006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166547 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLElement.h
+++ third_party/WebKit/Source/core/html/HTMLElement.h
@@ -51,9 +51,6 @@ public:
     void setInnerText(const String&, ExceptionState&);
     void setOuterText(const String&, ExceptionState&);
 
-    Element* insertAdjacentElement(const String& where, Element* newChild, ExceptionState&);
-    void insertAdjacentText(const String& where, const String& text, ExceptionState&);
-
     virtual bool hasCustomFocusLogic() const;
     virtual bool supportsFocus() const OVERRIDE;
 
