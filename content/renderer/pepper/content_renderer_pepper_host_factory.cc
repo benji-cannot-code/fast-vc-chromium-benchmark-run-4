200330ec80ce0a65d08b9b71a221011ee3976f94
dmichael@chromium.org <dmichael@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-05 18:47:47 +0000

    Don't create Pepper resources after the PepperPluginInstance's RenderFrame has gone away.
    BUG=338984
    R=bbudge@chromium.org
    
    Review URL: https://codereview.chromium.org/135503003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249063 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/pepper/content_renderer_pepper_host_factory.cc
+++ content/renderer/pepper/content_renderer_pepper_host_factory.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/pepper/pepper_file_ref_renderer_host.h"
 #include "content/renderer/pepper/pepper_file_system_host.h"
 #include "content/renderer/pepper/pepper_graphics_2d_host.h"
+#include "content/renderer/pepper/pepper_plugin_instance_impl.h"
 #include "content/renderer/pepper/pepper_truetype_font_host.h"
 #include "content/renderer/pepper/pepper_url_loader_host.h"
 #include "content/renderer/pepper/pepper_video_capture_host.h"
@@ -74,6 +75,11 @@ scoped_ptr<ResourceHost> ContentRendererPepperHostFactory::CreateResourceHost(
 
   // Make sure the plugin is giving us a valid instance for this resource.
   if (!host_->IsValidInstance(instance))
+    return scoped_ptr<ResourceHost>();
+
+  PepperPluginInstanceImpl* instance_impl =
+      host_->GetPluginInstanceImpl(instance);
+  if (!instance_impl->render_frame())
     return scoped_ptr<ResourceHost>();
 
   // Public interfaces.
