598fa4a13460f78a2740964cf2cbb13e532e51dc
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-06 01:30:30 +0000

    Add an if(frame) check to toIsolate(Frame*)
    - The crash happens in Youtube in a very weird path
    (https://code.google.com/p/chromium/issues/detail?id=340567). To reproduce the
    crash, we need to somehow call isURLAllowed() after the frame is gone.
    
    - Actually the crash is a regression caused by
    https://codereview.chromium.org/151283002/. Previously toIsolate(Frame*) was
    implemented so that it accepts a null frame, but
    https://codereview.chromium.org/151283002/ disallowed a null frame. This change
    caused the crash.
    
    - So this CL is going to remove all call sites that call toIsolate() with a null
    frame.
    
    For more details, see a crash log in the bug.
    
    BUG=340567
    
    Review URL: https://codereview.chromium.org/146083004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166562 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/ScriptController.cpp
+++ third_party/WebKit/Source/bindings/v8/ScriptController.cpp
@@ -83,6 +83,8 @@ namespace WebCore {
 
 bool ScriptController::canAccessFromCurrentOrigin(Frame *frame)
 {
+    if (!frame)
+        return false;
     v8::Isolate* isolate = toIsolate(frame);
     return !isolate->InContext() || BindingSecurity::shouldAllowAccessToFrame(isolate, frame);
 }
