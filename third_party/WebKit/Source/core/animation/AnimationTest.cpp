8de780c3c3dcbd4a79e190befd16b531b88d422a
pdr@chromium.org <pdr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-06 06:23:35 +0000

    Revert of Revert of Web Animations: Replace hasIterationDuration with a NaN duration value (https://codereview.chromium.org/144233009/)
    Reason for revert:
    Tim's original patch (https://src.chromium.org/viewvc/blink?view=rev&revision=166567) did not cause the timeouts we thought it did. I am relanding the patch that was incorrectly reverted.
    
    Original issue's description:
    > Revert of Web Animations: Replace hasIterationDuration with a NaN duration value (https://codereview.chromium.org/154333003/)
    > 
    > Reason for revert:
    > Several virtual/threaded/transitions just started timing out and I suspect this patch may be the cause. A sample is below:
    > http://test-results.appspot.com/dashboards/flakiness_dashboard.html#tests=virtual%2Fthreaded%2Ftransitions%2Ftransition-end-event-rendering.html
    > 
    > Original issue's description:
    > > Web Animations: Replace hasIterationDuration with a NaN duration value
    > > 
    > > Minor clean-up to the code; e.g. this makes writing tests a bit simpler.
    > > 
    > > BUG=335350
    > > 
    > > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=166567
    > 
    > TBR=dstockwell@chromium.org,timloh@chromium.org
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=335350
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=166575
    
    TBR=dstockwell@chromium.org,timloh@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=335350
    
    Review URL: https://codereview.chromium.org/142383004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166579 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/AnimationTest.cpp
+++ third_party/WebKit/Source/core/animation/AnimationTest.cpp
@@ -155,7 +155,6 @@ TEST_F(AnimationAnimationTest, CanSetDuration)
 
     RefPtr<Animation> animation = createAnimation(element.get(), jsKeyframes, duration);
 
-    EXPECT_TRUE(animation->specified().hasIterationDuration);
     EXPECT_EQ(duration, animation->specified().iterationDuration);
 }
 
@@ -170,8 +169,7 @@ TEST_F(AnimationAnimationTest, CanOmitSpecifiedDuration)
 
     RefPtr<Animation> animation = createAnimation(element.get(), jsKeyframes);
 
-    EXPECT_FALSE(animation->specified().hasIterationDuration);
-    EXPECT_EQ(0, animation->specified().iterationDuration);
+    EXPECT_TRUE(std::isnan(animation->specified().iterationDuration));
 }
 
 TEST_F(AnimationAnimationTest, ClipNegativeDurationToZero)
@@ -185,7 +183,6 @@ TEST_F(AnimationAnimationTest, ClipNegativeDurationToZero)
 
     RefPtr<Animation> animation = createAnimation(element.get(), jsKeyframes, -2);
 
-    EXPECT_TRUE(animation->specified().hasIterationDuration);
     EXPECT_EQ(0, animation->specified().iterationDuration);
 }
 
@@ -350,56 +347,39 @@ TEST_F(AnimationAnimationTest, TimingInputIterationDuration)
     v8::Context::Scope contextScope(context);
 
     Timing timing;
-    EXPECT_EQ(0, timing.iterationDuration);
-    EXPECT_FALSE(timing.hasIterationDuration);
+    EXPECT_TRUE(std::isnan(timing.iterationDuration));
 
     applyTimingInputNumber(timing, isolate, "duration", 1.1);
     EXPECT_EQ(1.1, timing.iterationDuration);
-    EXPECT_TRUE(timing.hasIterationDuration);
-    timing.hasIterationDuration = false;
-    timing.iterationDuration = 0;
+    timing.iterationDuration = std::numeric_limits<double>::quiet_NaN();
 
     applyTimingInputNumber(timing, isolate, "duration", -1);
-    EXPECT_EQ(0, timing.iterationDuration);
-    EXPECT_FALSE(timing.hasIterationDuration);
-    timing.hasIterationDuration = false;
-    timing.iterationDuration = 0;
+    EXPECT_TRUE(std::isnan(timing.iterationDuration));
+    timing.iterationDuration = std::numeric_limits<double>::quiet_NaN();
 
     applyTimingInputString(timing, isolate, "duration", "1");
     EXPECT_EQ(1, timing.iterationDuration);
-    EXPECT_TRUE(timing.hasIterationDuration);
-    timing.hasIterationDuration = false;
-    timing.iterationDuration = 0;
+    timing.iterationDuration = std::numeric_limits<double>::quiet_NaN();
 
     applyTimingInputString(timing, isolate, "duration", "Infinity");
     EXPECT_TRUE(std::isinf(timing.iterationDuration) && (timing.iterationDuration > 0));
-    EXPECT_TRUE(timing.hasIterationDuration);
-    timing.hasIterationDuration = false;
-    timing.iterationDuration = 0;
+    timing.iterationDuration = std::numeric_limits<double>::quiet_NaN();
 
     applyTimingInputString(timing, isolate, "duration", "-Infinity");
-    EXPECT_EQ(0, timing.iterationDuration);
-    EXPECT_FALSE(timing.hasIterationDuration);
-    timing.hasIterationDuration = false;
-    timing.iterationDuration = 0;
+    EXPECT_TRUE(std::isnan(timing.iterationDuration));
+    timing.iterationDuration = std::numeric_limits<double>::quiet_NaN();
 
     applyTimingInputString(timing, isolate, "duration", "NaN");
-    EXPECT_EQ(0, timing.iterationDuration);
-    EXPECT_FALSE(timing.hasIterationDuration);
-    timing.hasIterationDuration = false;
-    timing.iterationDuration = 0;
+    EXPECT_TRUE(std::isnan(timing.iterationDuration));
+    timing.iterationDuration = std::numeric_limits<double>::quiet_NaN();
 
     applyTimingInputString(timing, isolate, "duration", "auto");
-    EXPECT_EQ(0, timing.iterationDuration);
-    EXPECT_FALSE(timing.hasIterationDuration);
-    timing.hasIterationDuration = false;
-    timing.iterationDuration = 0;
+    EXPECT_TRUE(std::isnan(timing.iterationDuration));
+    timing.iterationDuration = std::numeric_limits<double>::quiet_NaN();
 
     applyTimingInputString(timing, isolate, "duration", "rubbish");
-    EXPECT_EQ(0, timing.iterationDuration);
-    EXPECT_FALSE(timing.hasIterationDuration);
-    timing.hasIterationDuration = false;
-    timing.iterationDuration = 0;
+    EXPECT_TRUE(std::isnan(timing.iterationDuration));
+    timing.iterationDuration = std::numeric_limits<double>::quiet_NaN();
 }
 
 TEST_F(AnimationAnimationTest, TimingInputPlaybackRate)
@@ -568,8 +548,7 @@ TEST_F(AnimationAnimationTest, TimingInputEmpty)
     EXPECT_EQ(controlTiming.fillMode, updatedTiming.fillMode);
     EXPECT_EQ(controlTiming.iterationStart, updatedTiming.iterationStart);
     EXPECT_EQ(controlTiming.iterationCount, updatedTiming.iterationCount);
-    EXPECT_EQ(controlTiming.iterationDuration, updatedTiming.iterationDuration);
-    EXPECT_EQ(controlTiming.hasIterationDuration, updatedTiming.hasIterationDuration);
+    EXPECT_TRUE(std::isnan(updatedTiming.iterationDuration));
     EXPECT_EQ(controlTiming.playbackRate, updatedTiming.playbackRate);
     EXPECT_EQ(controlTiming.direction, updatedTiming.direction);
     EXPECT_EQ(*controlTiming.timingFunction.get(), *updatedTiming.timingFunction.get());
