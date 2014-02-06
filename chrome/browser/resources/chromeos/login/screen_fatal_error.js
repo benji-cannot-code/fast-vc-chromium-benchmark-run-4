fc531a5d8db73d92cf0e23c247d9031c38406acf
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-06 12:02:00 +0000

    cros: Saml flow update CL follow up.
    Follow up change for r248893. This CL properly restores UI states for
    fatal error screen to be more future proof (e.g. when we decide to show
    account picker screen after it).
    
    BUG=330206
    
    Review URL: https://codereview.chromium.org/146683006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249375 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/login/screen_fatal_error.js
+++ chrome/browser/resources/chromeos/login/screen_fatal_error.js
@@ -19,10 +19,10 @@ login.createScreen('FatalErrorScreen', 'fatal-error', function() { return {
     callback_: null,
 
     /**
-     * Saved hidden status of 'progress-dots'.
-     * @type {boolean}
+     * Saved UI states to restore when this screen hides.
+     * @type {Object}
      */
-    savedProgressDotsHidden_: null,
+    savedUIStates_: {},
 
     /** @override */
     decorate: function() {
@@ -30,8 +30,24 @@ login.createScreen('FatalErrorScreen', 'fatal-error', function() { return {
           'click', this.onDismiss_.bind(this));
     },
 
+    /** @override */
     get defaultControl() {
       return $('fatal-error-dismiss-button');
+    },
+
+    /** @override */
+    onBeforeShow: function() {
+      this.savedUIStates_.progressDotHidden = $('progress-dots').hidden;
+      $('progress-dots').hidden = true;
+
+      this.savedUIStates_.headerHidden = Oobe.getInstance().headerHidden;
+      Oobe.getInstance().headerHidden = true;
+    },
+
+    /** @override */
+    onBeforeHide: function() {
+      $('progress-dots').hidden = this.savedUIStates_.progressDotHidden;
+      Oobe.getInstance().headerHidden = this.savedUIStates_.headerHidden;
     },
 
     /**
@@ -39,7 +55,6 @@ login.createScreen('FatalErrorScreen', 'fatal-error', function() { return {
      */
     onDismiss_: function() {
       this.callback_();
-      $('progress-dots').hidden = this.savedProgressDotsHidden_;
     },
 
     /**
@@ -48,13 +63,8 @@ login.createScreen('FatalErrorScreen', 'fatal-error', function() { return {
      *     screen is dismissed.
      */
     show: function(callback) {
-      Oobe.getInstance().headerHidden = true;
       this.callback_ = callback;
-
       Oobe.showScreen({id: SCREEN_FATAL_ERROR});
-
-      this.savedProgressDotsHidden_ = $('progress-dots').hidden;
-      $('progress-dots').hidden = true;
     }
   };
 });
