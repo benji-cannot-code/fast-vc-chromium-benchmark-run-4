cb6baf2998f13c6fb14db0dddd0ecc866ab6148c
joone.hur@intel.com <joone.hur@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-06 11:30:41 +0000

    Build fix when use_alsa=0.
    When use_alsa=0, there is a link error as follows:
    
    ../../media/audio/pulse/audio_manager_pulse.cc:81: error: undefined reference to 'media::AudioManagerAlsa::ShowLinuxAudioInputSettings()'
    collect2: error: ld returned 1 exit status
    ninja: build stopped: subcommand failed.
    
    This patch allows to add a guard to alsa related code to fix this link error.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/150573017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249361 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/pulse/audio_manager_pulse.cc
+++ media/audio/pulse/audio_manager_pulse.cc
@@ -11,7 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/nix/xdg_util.h"
 #include "base/stl_util.h"
+#if defined(USE_ALSA)
 #include "media/audio/alsa/audio_manager_alsa.h"
+#endif
 #include "media/audio/audio_parameters.h"
 #include "media/audio/pulse/pulse_input.h"
 #include "media/audio/pulse/pulse_output.h"
@@ -79,7 +81,9 @@ bool AudioManagerPulse::HasAudioInputDevices() {
 }
 
 void AudioManagerPulse::ShowAudioInputSettings() {
+#if defined(USE_ALSA)
   AudioManagerAlsa::ShowLinuxAudioInputSettings();
+#endif
 }
 
 void AudioManagerPulse::GetAudioDeviceNames(
