bb48f38f6c309e1ce50bf5a040672306f6ec4ddd
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-06 19:22:39 +0000

    rAc: enable i18n by default, allow it to be disabled via --disable-autofill-address-i18n.
    R=estade@chromium.org
    BUG=317839
    TEST=country combobox shows up without flags, tests pass
    
    Review URL: https://codereview.chromium.org/155263002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249469 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/autofill/autofill_dialog_i18n_input.cc
+++ chrome/browser/ui/autofill/autofill_dialog_i18n_input.cc
@@ -24,6 +24,8 @@ namespace i18ninput {
 
 namespace {
 
+static bool g_enabled_for_testing_ = false;
+
 using base::UTF16ToUTF8;
 using ::i18n::addressinput::AddressData;
 using ::i18n::addressinput::AddressField;
@@ -39,8 +41,15 @@ DetailInput::Length LengthFromHint(AddressUiComponent::LengthHint hint) {
 }  // namespace
 
 bool Enabled() {
+  if (g_enabled_for_testing_)
+    return true;
+
   CommandLine* command_line = CommandLine::ForCurrentProcess();
-  return command_line->HasSwitch(::switches::kEnableAutofillAddressI18n);
+  return !command_line->HasSwitch(::switches::kDisableAutofillAddressI18n);
+}
+
+void EnableForTesting() {
+  g_enabled_for_testing_ = true;
 }
 
 void BuildAddressInputs(common::AddressType address_type,
