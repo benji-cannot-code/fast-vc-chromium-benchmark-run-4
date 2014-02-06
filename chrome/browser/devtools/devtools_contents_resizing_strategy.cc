6fa9a4305f1e74e5113ad40507e9379ba584a46f
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-06 21:25:22 +0000

    Fix chrome/browser/devtools/devtools_contents_resizing_strategy.cc compilation on VS2013
    <algorithm> missing for std::min/max.
    
    NOTRY=true
    TBR=pfeldman@chromium.org
    BUG=288948
    
    Review URL: https://codereview.chromium.org/156753002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249501 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/devtools/devtools_contents_resizing_strategy.cc
+++ chrome/browser/devtools/devtools_contents_resizing_strategy.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/devtools/devtools_contents_resizing_strategy.h"
 
+#include <algorithm>
+
 DevToolsContentsResizingStrategy::DevToolsContentsResizingStrategy() {
 }
 
