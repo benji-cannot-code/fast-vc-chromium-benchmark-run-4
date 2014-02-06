100f593ba29f7c109edb733ecb0c736b8ba963bf
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-06 12:47:00 +0000

    DevTools: make all views layout boundaries.
    Review URL: https://codereview.chromium.org/152123003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166614 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/View.js
+++ third_party/WebKit/Source/devtools/front_end/View.js
@@ -54,6 +54,11 @@ WebInspector.View.prototype = {
         this._isRoot = true;
     },
 
+    makeLayoutBoundary: function()
+    {
+        this._isLayoutBoundary = true;
+    },
+
     /**
      * @return {?WebInspector.View}
      */
@@ -125,6 +130,7 @@ WebInspector.View.prototype = {
             return;
         this.storeScrollPositions();
 
+        this._discardCachedSize();
         this._callOnVisibleChildren(this._processWillHide);
         this._notify(this.willHide);
         this._isShowing = false;
@@ -142,8 +148,43 @@ WebInspector.View.prototype = {
             return;
         if (!this.isShowing())
             return;
+        this._discardCachedSize();
         this._notify(this.onResize);
         this._callOnVisibleChildren(this._processOnResize);
+    },
+
+    _cacheSize: function()
+    {
+        this._prepareCacheSize();
+        this._applyCacheSize();
+    },
+
+    _prepareCacheSize: function()
+    {
+        if (this._isLayoutBoundary) {
+            this._cachedOffsetWidth = this.element.offsetWidth;
+            this._cachedOffsetHeight = this.element.offsetHeight;
+        }
+        this._callOnVisibleChildren(this._prepareCacheSize);
+    },
+
+    _applyCacheSize: function()
+    {
+        if (this._isLayoutBoundary) {
+            this.element.style.setProperty("width", this._cachedOffsetWidth + "px");
+            this.element.style.setProperty("height", this._cachedOffsetHeight + "px");
+            delete this._cachedOffsetWidth;
+            delete this._cachedOffsetHeight;
+        }
+        this._callOnVisibleChildren(this._applyCacheSize);
+    },
+
+    _discardCachedSize: function()
+    {
+        if (!this._isLayoutBoundary)
+            return;
+        this.element.style.removeProperty("width");
+        this.element.style.removeProperty("height");
     },
 
     /**
@@ -214,8 +255,10 @@ WebInspector.View.prototype = {
                 WebInspector.View._originalAppendChild.call(parentElement, this.element);
         }
 
-        if (this._parentIsShowing())
+        if (this._parentIsShowing()) {
             this._processWasShown();
+            this._cacheSize();
+        }
     },
 
     /**
@@ -313,7 +356,9 @@ WebInspector.View.prototype = {
     {
         if (!this.isShowing())
             return;
+        this._discardCachedSize();
         this._callOnVisibleChildren(this._processOnResize);
+        this._cacheSize();
     },
 
     registerRequiredCSS: function(cssFile)
