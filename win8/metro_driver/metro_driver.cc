5bd8075392897052d46f98d19c36c175d34b8728
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-06 02:54:54 +0000

    Revert 248332
    This applies 
    https://codereview.chromium.org/142473002
    
    Which got initially commited as
    https://src.chromium.org/viewvc/chrome?view=rev&revision=247151
    
    
    > Revert 248331 "Revert 247156 "Revert 247151 "Cleanup of windows ..."
    > 
    > Basically reverting 247151 which still breaks the tree.
    > 
    > 
    > > Revert 247156 "Revert 247151 "Cleanup of windows launch code""
    > > 
    > > This re-enables 247151 because I can't repro it
    > > so it is possible it was a temporary NaCL insanity and definitely
    > > the x64 breakage was a flake as confirmed by try jobs.
    > > 
    > > 
    > > > Revert 247151 "Cleanup of windows launch code"
    > > > 
    > > > > Cleanup of windows launch code
    > > > > 
    > > > > The windows startup code was handling aura and non-aura cases which
    > > > > require different startup logic in metro (immersive mode). Since we
    > > > > don't support the non-Aura logic we can get rid of a lot of code.
    > > > > 
    > > > > Tbring gab for the installer constant addition.
    > > > > 
    > > > > R=ananta@chromium.org, scottmg@chromium.org
    > > > > TBR=gab
    > > > > BUG=none
    > > > > 
    > > > > Review URL: https://codereview.chromium.org/142473002
    > > > 
    > > > TBR=cpu@chromium.org
    > > > 
    > > > Review URL: https://codereview.chromium.org/140123006
    > > 
    > > TBR=cpu@chromium.org
    > > 
    > > Review URL: https://codereview.chromium.org/151683004
    > 
    > TBR=cpu@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/152033002
    
    TBR=cpu@chromium.org
    
    Review URL: https://codereview.chromium.org/143473007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249248 0039d316-1c4b-4281-b951-d872f2087c98

--- win8/metro_driver/metro_driver.cc
+++ win8/metro_driver/metro_driver.cc
@@ -16,10 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/scoped_comptr.h"
 #include "win8/metro_driver/winrt_utils.h"
 
-#if !defined(USE_AURA)
-#include "win8/metro_driver/chrome_app_view.h"
-#endif
-
 // TODO(siggi): Move this to GYP.
 #pragma comment(lib, "runtimeobject.lib")
 
@@ -60,7 +56,7 @@ base::AtExitManager at_exit;
 #endif
 
 extern "C" __declspec(dllexport)
-int InitMetro(LPTHREAD_START_ROUTINE thread_proc, void* context) {
+int InitMetro() {
   // Initialize the command line.
   CommandLine::Init(0, NULL);
   logging::LoggingSettings settings;
@@ -98,8 +94,7 @@ int InitMetro(LPTHREAD_START_ROUTINE thread_proc, void* context) {
   if (FAILED(hr))
     return 1;
 
-  auto view_factory = mswr::Make<ChromeAppViewFactory>(
-      core_app.Get(), thread_proc, context);
+  auto view_factory = mswr::Make<ChromeAppViewFactory>(core_app.Get());
   hr = core_app->Run(view_factory.Get());
   DVLOG(1) << "exiting InitMetro, hr=" << hr;
 
