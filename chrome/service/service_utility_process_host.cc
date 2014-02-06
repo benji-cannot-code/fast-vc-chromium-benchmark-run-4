b1f10615147f3b6df60198d54e949c13b68ad4c2
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-06 02:41:04 +0000

    Remove ServiceUtilityProcessHost::Client::OnGetPrinterCapsAndDefaultsFailed.
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/155133002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249238 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/service/service_utility_process_host.cc
+++ chrome/service/service_utility_process_host.cc
@@ -288,7 +288,7 @@ void ServiceUtilityProcessHost::OnGetPrinterCapsAndDefaultsSucceeded(
   waiting_for_reply_ = false;
   client_message_loop_proxy_->PostTask(
       FROM_HERE,
-      base::Bind(&Client::OnGetPrinterCapsAndDefaultsSucceeded, client_.get(),
+      base::Bind(&Client::OnGetPrinterCapsAndDefaults, client_.get(), true,
                  printer_name, caps_and_defaults));
 }
 
@@ -303,8 +303,8 @@ void ServiceUtilityProcessHost::OnGetPrinterCapsAndDefaultsFailed(
   waiting_for_reply_ = false;
   client_message_loop_proxy_->PostTask(
       FROM_HERE,
-      base::Bind(&Client::OnGetPrinterCapsAndDefaultsFailed, client_.get(),
-                 printer_name));
+      base::Bind(&Client::OnGetPrinterCapsAndDefaults, client_.get(), false,
+                 printer_name, printing::PrinterCapsAndDefaults()));
 }
 
 void ServiceUtilityProcessHost::Client::MetafileAvailable(
