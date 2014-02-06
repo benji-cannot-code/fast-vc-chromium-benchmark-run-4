e5b856797a853a35b6ac77746057b059ab95b00f
sbc@chromium.org <sbc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-06 00:37:57 +0000

    [NaCl SDK] Fix select.h in newlib-extras.
    Turns out it was full of errors if one tried to
    include it before anything else.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/150633003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249181 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/libraries/third_party/newlib-extras/sys/select.h
+++ native_client_sdk/src/libraries/third_party/newlib-extras/sys/select.h
@@ -2,14 +2,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef _SYS_SELECT_H
 #define _SYS_SELECT_H	1
 
+#include <sys/cdefs.h>
+#include <sys/time.h>
 #include <sys/types.h>
 #include <time.h>
 
 #include "sdk_util/macros.h"
 
 EXTERN_C_BEGIN
-
-struct timeval;
 
 int select (int __nfds, fd_set *__restrict __readfds,
             fd_set *__restrict __writefds,
