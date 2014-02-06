cb87e303c0878ec91231a90a0b976290848e55d2
jdduke@chromium.org <jdduke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-06 14:02:44 +0000

    [Android] Pass WebInputEvent by const ref when creating LatencyInfo
    A raw WebInputEvent should never be constructed, as it's an abstract base
    class masquerading as a POD struct.  Use a const ref WebInputEvent arg
    to prevent this.
    
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/155043002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249400 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_view_android.cc
+++ content/browser/renderer_host/render_widget_host_view_android.cc
@@ -117,7 +117,7 @@ bool UsingDelegatedRenderer() {
   return using_delegated_renderer;
 }
 
-ui::LatencyInfo CreateLatencyInfo(const blink::WebInputEvent event) {
+ui::LatencyInfo CreateLatencyInfo(const blink::WebInputEvent& event) {
   ui::LatencyInfo latency_info;
   // The latency number should only be added if the timestamp is valid.
   if (event.timeStampSeconds) {
