44f11cfbc6d2d4d32462f440168b15e213b79356
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-06 10:23:47 +0000

    Clicking on a non-link A element in a LABEL element should activate the associated control.
    'A' element without href attribute should not be an interactive content.
    
    BUG=339629
    TEST=automated
    
    Review URL: https://codereview.chromium.org/144243006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166595 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLAnchorElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLAnchorElement.cpp
@@ -693,7 +693,7 @@ void HTMLAnchorElement::PrefetchEventHandler::prefetch(blink::WebPreconnectMotiv
 
 bool HTMLAnchorElement::isInteractiveContent() const
 {
-    return true;
+    return isLink();
 }
 
 }
