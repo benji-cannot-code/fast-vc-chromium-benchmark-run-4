91a1055d86a4d911f2f4299f145c7f770ec859c5
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-06 13:50:41 +0000

    DevTools: Improve animated highlighting in the Network and Profiler panels
    R=eustas, loislo
    BUG=341146
    
    Review URL: https://codereview.chromium.org/142173003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166618 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/NetworkPanel.js
+++ third_party/WebKit/Source/devtools/front_end/NetworkPanel.js
@@ -1363,7 +1363,7 @@ WebInspector.NetworkLogView.prototype = {
 
     _highlightNode: function(node)
     {
-        node.element.classList.add("highlighted-row");
+        WebInspector.runCSSAnimationOnce(node.element, "highlighted-row");
         this._highlightedNode = node;
     },
 
@@ -1635,8 +1635,8 @@ WebInspector.NetworkPanel.prototype = {
         var request = this._requestByAnchor(anchor);
         if (!request)
             return false;
-        this.revealAndHighlightRequest(request)
         WebInspector.inspectorView.setCurrentPanel(this);
+        this.revealAndHighlightRequest(request);
         return true;
     },
 
