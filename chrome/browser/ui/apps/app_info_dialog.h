57c3c7272af951fbaa2653e0c320e6e6136df2d2
sashab@chromium.org <sashab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-06 11:09:58 +0000

    Add placeholder dialog to the app list context menu under a flag.
    Create a context menu item for shortcuts in the app list called
    'App Info'. When clicked, launch a placeholder dialog that
    displays the app's information. Will eventually display more
    information about the app.
    
    Introduces & requires the flag --enable-app-list-app-info.
    
    BUG=266739
    
    Review URL: https://codereview.chromium.org/146613005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249352 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/ui/apps/app_info_dialog.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_APPS_APP_INFO_DIALOG_H_
+#define CHROME_BROWSER_UI_APPS_APP_INFO_DIALOG_H_
+
+#include "base/callback.h"
+#include "ui/gfx/native_widget_types.h"
+
+class Profile;
+
+namespace extensions {
+class Extension;
+}
+
+// Shows the chrome app information dialog box.
+// |close_callback| may be null.
+void ShowChromeAppInfoDialog(gfx::NativeWindow parent_window,
+                             Profile* profile,
+                             const extensions::Extension* app,
+                             const base::Closure& close_callback);
+
+#endif  // CHROME_BROWSER_UI_APPS_APP_INFO_DIALOG_H_
---
+++ chrome/browser/ui/gtk/apps/app_info_dialog_gtk.cc
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/apps/app_info_dialog.h"
+
+void ShowChromeAppInfoDialog(gfx::NativeWindow parent_window,
+                             Profile* profile,
+                             const extensions::Extension* app,
+                             const base::Closure& close_callback) {
+  NOTIMPLEMENTED();
+}
