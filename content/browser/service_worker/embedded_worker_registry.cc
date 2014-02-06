9ef5fa6eac2b025e14d581aac65520237358ecec
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-06 01:34:28 +0000

    Implement ServiceWorkerVersion::DispatchInstallEvent()
    This doesn't hook any blink code yet, but sends InstallEvent to the
    child process and dispatches callback for InstallEventFinished response.
    
    This depends on: https://codereview.chromium.org/139923005/
    
    BUG=313530
    TEST=ServiceWorkerBrowserTest.Install
    
    Review URL: https://codereview.chromium.org/142863009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249201 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/service_worker/embedded_worker_registry.cc
+++ content/browser/service_worker/embedded_worker_registry.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/service_worker/embedded_worker_instance.h"
 #include "content/browser/service_worker/service_worker_context_core.h"
 #include "content/common/service_worker/embedded_worker_messages.h"
+#include "content/common/service_worker/service_worker_messages.h"
 #include "ipc/ipc_message.h"
 #include "ipc/ipc_sender.h"
 
@@ -75,10 +76,14 @@ void EmbeddedWorkerRegistry::OnSendMessageToBrowser(
     LOG(ERROR) << "Worker " << embedded_worker_id << " not registered";
     return;
   }
-  // TODO(kinuko): Filter out unexpected messages here and uncomment below
-  // when we actually define messages that are to be sent from child process
-  // to the browser via this channel. (We don't have any yet)
-  found->second->OnMessageReceived(request_id, message);
+  // Perform security check to filter out any unexpected (and non-test)
+  // messages. This must list up all message types that can go through here.
+  if (message.type() == ServiceWorkerHostMsg_InstallEventFinished::ID ||
+      IPC_MESSAGE_CLASS(message) == TestMsgStart) {
+    found->second->OnMessageReceived(request_id, message);
+    return;
+  }
+  NOTREACHED() << "Got unexpected message: " << message.type();
 }
 
 void EmbeddedWorkerRegistry::AddChildProcessSender(
