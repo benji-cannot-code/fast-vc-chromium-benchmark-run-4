8a60163b3d9d13e932048e93cbb02491f237a616
zmo@chromium.org <zmo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-06 18:41:18 +0000

    Hookup clear_uniforms_before_first_program_use workaround with implementation.
    Right now it's hardwired in command buffer.
    
    BUG=340516
    TEST=gpu_unittests, webgl conformance tests
    R=bajones@chromium.org, kbr@chromium.org
    
    Review URL: https://codereview.chromium.org/147593011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249460 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/program_manager.h
+++ gpu/command_buffer/service/program_manager.h
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace gpu {
 namespace gles2 {
 
-class FeatureInfo;
 class ProgramCache;
 class ProgramManager;
 class Shader;
@@ -158,7 +157,6 @@ class GPU_EXPORT Program : public base::RefCounted<Program> {
   bool Link(ShaderManager* manager,
             ShaderTranslator* vertex_translator,
             ShaderTranslator* fragment_shader,
-            FeatureInfo* feature_info,
             const ShaderCacheCallback& shader_callback);
 
   // Performs glValidateProgram and related activities.
@@ -338,6 +336,11 @@ class GPU_EXPORT Program : public base::RefCounted<Program> {
 // need to be shared by multiple GLES2Decoders.
 class GPU_EXPORT ProgramManager {
  public:
+  enum TranslatedShaderSourceType {
+    kANGLE,
+    kGL,  // GL or GLES
+  };
+
   explicit ProgramManager(ProgramCache* program_cache,
                           uint32 max_varying_vectors);
   ~ProgramManager();
@@ -377,9 +380,10 @@ class GPU_EXPORT ProgramManager {
 
   static int32 MakeFakeLocation(int32 index, int32 element);
 
-  void DoCompileShader(Shader* shader,
-                       ShaderTranslator* translator,
-                       FeatureInfo* feature_info);
+  void DoCompileShader(
+      Shader* shader,
+      ShaderTranslator* translator,
+      TranslatedShaderSourceType translated_shader_source_type);
 
   uint32 max_varying_vectors() const {
     return max_varying_vectors_;
@@ -404,8 +408,6 @@ class GPU_EXPORT ProgramManager {
   unsigned int program_count_;
 
   bool have_context_;
-
-  bool disable_workarounds_;
 
   // Used to clear uniforms.
   std::vector<uint8> zero_;
