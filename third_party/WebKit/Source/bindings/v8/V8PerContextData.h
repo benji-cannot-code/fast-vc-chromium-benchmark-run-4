3b4396808db5c4b0678301c26097e663b5001d30
pdr@chromium.org <pdr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-06 06:22:55 +0000

    Revert of Revert of Use ScopedPersistent for persistent handles in V8PerContextData (https://codereview.chromium.org/150283006/)
    Reason for revert:
    Haraken's original patch (https://src.chromium.org/viewvc/blink?view=rev&revision=166569) did not cause the timeouts we thought it did. I am relanding the patch that was incorrectly reverted.
    
    Original issue's description:
    > Revert of Use ScopedPersistent for persistent handles in V8PerContextData (https://codereview.chromium.org/149263005/)
    > 
    > Reason for revert:
    > This change is being blamed for many virtual/threaded/transitions timeouts on the bots. There is no obvious connection between these test timeouts and this patch, but this is the only change listed when the timeouts started to occur.
    > 
    > Some of the timeouts: http://goo.gl/6tX16t
    > 
    > Original issue's description:
    > > Use ScopedPersistent for persistent handles in V8PerContextData
    > > 
    > > Using ScopedPersistent is safer, since ScopedPersistent will be automaticallly
    > > destructed when the scope is exited.
    > > 
    > > (Another motivation is that I want to replace all raw persistent handles
    > > with ScopedPersistent/UnsafePersistent in order to verify that persistent
    > > handles don't leak among worlds.)
    > > 
    > > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=166569
    > 
    > TBR=jochen@chromium.org,dcarney@chromium.org,haraken@chromium.org
    > NOTREECHECKS=true
    > NOTRY=true
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=166577
    
    TBR=jochen@chromium.org,dcarney@chromium.org,haraken@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/137213006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166578 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/V8PerContextData.h
+++ third_party/WebKit/Source/bindings/v8/V8PerContextData.h
@@ -85,7 +85,7 @@ private:
         , m_perContextData(0)
         , m_isolatedWorld(0)
     {
-        m_context.SetWeak(this, &V8PerContextDataHolder::weakCallback);
+        m_context.setWeak(this, &V8PerContextDataHolder::weakCallback);
         context->SetAlignedPointerInEmbedderData(v8ContextPerContextDataIndex, this);
     }
 
@@ -94,11 +94,10 @@ private:
     static void weakCallback(const v8::WeakCallbackData<v8::Context, V8PerContextDataHolder>& data)
     {
         data.GetValue()->SetAlignedPointerInEmbedderData(v8ContextPerContextDataIndex, 0);
-        data.GetParameter()->m_context.Reset();
         delete data.GetParameter();
     }
 
-    v8::Persistent<v8::Context> m_context;
+    ScopedPersistent<v8::Context> m_context;
     V8PerContextData* m_perContextData;
     DOMWrapperWorld* m_isolatedWorld;
 };
@@ -110,10 +109,7 @@ public:
         return adoptPtr(new V8PerContextData(context));
     }
 
-    ~V8PerContextData()
-    {
-        dispose();
-    }
+    ~V8PerContextData();
 
     bool init();
 
@@ -169,8 +165,6 @@ private:
     {
     }
 
-    void dispose();
-
     v8::Local<v8::Object> createWrapperFromCacheSlowCase(const WrapperTypeInfo*);
     v8::Local<v8::Function> constructorForTypeSlowCase(const WrapperTypeInfo*);
 
@@ -188,7 +182,7 @@ private:
     // by the DOMActivityLoggerMap in DOMWrapperWorld.
     V8DOMActivityLogger* m_activityLogger;
     v8::Isolate* m_isolate;
-    v8::Persistent<v8::Context> m_context;
+    ScopedPersistent<v8::Context> m_context;
     ScopedPersistent<v8::Value> m_errorPrototype;
 
     typedef WTF::HashMap<CustomElementDefinition*, OwnPtr<CustomElementBinding> > CustomElementBindingMap;
