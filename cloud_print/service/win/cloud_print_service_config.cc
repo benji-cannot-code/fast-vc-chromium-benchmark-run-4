d630210ba7eac412ed2ba2749191713b943ad002
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-06 04:11:51 +0000

    Revert 249234 "Use MessagePumpDispatcher instead of MessageFilter" Failed non-Aura build.
    > Use MessagePumpDispatcher instead of MessageFilter
    > 
    > This is a preparation to remove base::MessageLoopForUI::MessageFilter.
    > 
    > BUG=330735, 319122
    > TEST=done by manually executing cloud_print_service_config.exe
    > 
    > Review URL: https://codereview.chromium.org/152033005
    
    TBR=yukawa@chromium.org
    
    Review URL: https://codereview.chromium.org/150283005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249268 0039d316-1c4b-4281-b951-d872f2087c98

--- cloud_print/service/win/cloud_print_service_config.cc
+++ cloud_print/service/win/cloud_print_service_config.cc
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/file_util.h"
 #include "base/message_loop/message_loop.h"
-#include "base/message_loop/message_pump_dispatcher.h"
-#include "base/run_loop.h"
 #include "base/strings/string16.h"
 #include "base/threading/thread.h"
 #include "chrome/common/chrome_constants.h"
@@ -32,19 +30,15 @@ class SetupDialog : public base::RefCounted<SetupDialog>,
                     public ATL::CDialogImpl<SetupDialog> {
  public:
   // Enables accelerators.
-  class Dispatcher : public base::MessagePumpDispatcher {
+  class MessageFilter : public base::MessageLoopForUI::MessageFilter {
    public:
-    explicit Dispatcher(SetupDialog* dialog) : dialog_(dialog) {}
-    virtual ~Dispatcher() {};
+    explicit MessageFilter(SetupDialog* dialog) : dialog_(dialog){}
+    virtual ~MessageFilter() {};
 
-    // MessagePumpDispatcher:
-    virtual bool Dispatch(const MSG& msg) OVERRIDE {
+    // MessageLoopForUI::MessageFilter
+    virtual bool ProcessMessage(const MSG& msg) OVERRIDE {
       MSG msg2 = msg;
-      if (!dialog_->IsDialogMessage(&msg2)) {
-        ::TranslateMessage(&msg);
-        ::DispatchMessage(&msg);
-      }
-      return true;
+      return dialog_->IsDialogMessage(&msg2) != FALSE;
     }
 
    private:
@@ -451,9 +445,10 @@ int WINAPI WinMain(__in  HINSTANCE hInstance,
   scoped_refptr<SetupDialog> dialog(new SetupDialog());
   dialog->Create(NULL);
   dialog->ShowWindow(SW_SHOW);
-  SetupDialog::Dispatcher dispatcher(dialog);
-  base::RunLoop run_loop;
-  run_loop.set_dispatcher(&dispatcher);
-  run_loop.Run();
+  scoped_ptr<SetupDialog::MessageFilter> filter(
+      new SetupDialog::MessageFilter(dialog));
+  loop.SetMessageFilter(filter.Pass());
+
+  loop.Run();
   return 0;
 }
