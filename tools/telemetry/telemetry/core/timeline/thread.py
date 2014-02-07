b800a56b47d22ccac8570d6e1fc24aafc6f8f83a
fmeawad@chromium.org <fmeawad@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-07 21:52:54 +0000

    [Telemetry] Port FlowEvents importer from trace-viewer to telemetry.
    BUG=
    
    Review URL: https://codereview.chromium.org/144313013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249803 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/timeline/thread.py
+++ tools/telemetry/telemetry/core/timeline/thread.py
@@ -18,6 +18,7 @@ class Thread(event_container.TimelineEventContainer):
     super(Thread, self).__init__('thread %s' % tid, parent=process)
     self.tid = tid
     self._async_slices = []
+    self._flow_events = []
     self._samples = []
     self._toplevel_slices = []
 
@@ -70,10 +71,15 @@ class Thread(event_container.TimelineEventContainer):
       if s.name == name:
         yield s
 
+  def IterAllFlowEvents(self):
+    for flow_event in self._flow_events:
+      yield flow_event
+
   def IterEventsInThisContainer(self):
     return itertools.chain(
       iter(self._newly_added_slices),
       self.IterAllAsyncSlices(),
+      self.IterAllFlowEvents(),
       self.IterAllSlices(),
       iter(self._samples)
       )
@@ -88,6 +94,9 @@ class Thread(event_container.TimelineEventContainer):
 
   def AddAsyncSlice(self, async_slice):
     self._async_slices.append(async_slice)
+
+  def AddFlowEvent(self, flow_event):
+    self._flow_events.append(flow_event)
 
   def BeginSlice(self, category, name, timestamp, thread_timestamp=None,
                  args=None):
