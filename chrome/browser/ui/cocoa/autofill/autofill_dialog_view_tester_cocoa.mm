6322bcb7a4b55526e545c2784b5ec2e8854b5142
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-07 22:59:24 +0000

    r247886 moved testing code to its own file and failed to remove it from the original file. This change completes the move.
    BUG=340391, 306200
    TEST=none; covered by existing tests
    
    Review URL: https://codereview.chromium.org/157343003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249834 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/autofill/autofill_dialog_view_tester_cocoa.mm
+++ chrome/browser/ui/cocoa/autofill/autofill_dialog_view_tester_cocoa.mm
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @implementation AutofillDialogWindowController (AutofillDialogViewTesterCocoa)
 
 - (void)setTextContents:(NSString*)text
-               forType:(autofill::ServerFieldType)type {
+                forType:(autofill::ServerFieldType)type {
   for (size_t i = autofill::SECTION_MIN; i <= autofill::SECTION_MAX; ++i) {
     autofill::DialogSection section = static_cast<autofill::DialogSection>(i);
     // TODO(groby): Need to find the section for an input directly - wasteful.
