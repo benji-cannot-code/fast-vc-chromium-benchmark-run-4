153891085866b25fc928480a7927c870571da585
blundell@chromium.org <blundell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-07 08:18:36 +0000

    Move ReportOsStatus() into ChromePasswordManagerDelegateImpl
    This utility function is //chrome-specific at the current time and hence
    doesn't belong in (what will be) the core code of the component.
    
    BUG=334655
    
    Review URL: https://codereview.chromium.org/151693006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249621 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/password_manager/password_manager.cc
+++ chrome/browser/password_manager/password_manager.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/password_manager/password_manager_delegate.h"
 #include "chrome/browser/password_manager/password_manager_driver.h"
 #include "chrome/browser/password_manager/password_manager_metrics_util.h"
-#include "chrome/browser/password_manager/password_manager_util.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/passwords/manage_passwords_bubble_ui_controller.h"
 #include "chrome/common/chrome_switches.h"
@@ -38,14 +37,6 @@ using content::WebContents;
 namespace {
 
 const char kSpdyProxyRealm[] = "/SpdyProxy";
-void ReportOsPassword() {
-  password_manager_util::OsPasswordStatus status =
-      password_manager_util::GetOsPasswordStatus();
-
-  UMA_HISTOGRAM_ENUMERATION("PasswordManager.OsPasswordStatus",
-                            status,
-                            password_manager_util::MAX_PASSWORD_STATUS);
-}
 
 // This routine is called when PasswordManagers are constructed.
 //
@@ -62,13 +53,6 @@ void ReportMetrics(bool password_manager_enabled) {
   if (ran_once)
     return;
   ran_once = true;
-
-  // Avoid checking OS password until later on in browser startup
-  // since it calls a few Windows APIs.
-  base::MessageLoopProxy::current()->PostDelayedTask(
-      FROM_HERE,
-      base::Bind(&ReportOsPassword),
-      base::TimeDelta::FromSeconds(10));
 
   UMA_HISTOGRAM_BOOLEAN("PasswordManager.Enabled", password_manager_enabled);
 }
--- chrome/browser/password_manager/password_manager_delegate_impl.cc
+++ chrome/browser/password_manager/password_manager_delegate_impl.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/password_manager/password_form_manager.h"
 #include "chrome/browser/password_manager/password_manager.h"
 #include "chrome/browser/password_manager/password_manager_metrics_util.h"
+#include "chrome/browser/password_manager/password_manager_util.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/passwords/manage_passwords_bubble_ui_controller.h"
 #include "chrome/browser/ui/sync/one_click_signin_helper.h"
@@ -29,6 +30,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/generated_resources.h"
 #include "grit/theme_resources.h"
 #include "ui/base/l10n/l10n_util.h"
+
+namespace {
+
+void ReportOsPassword() {
+  password_manager_util::OsPasswordStatus status =
+      password_manager_util::GetOsPasswordStatus();
+
+  UMA_HISTOGRAM_ENUMERATION("PasswordManager.OsPasswordStatus",
+                            status,
+                            password_manager_util::MAX_PASSWORD_STATUS);
+}
+
+}  // namespace
 
 // SavePasswordInfoBarDelegate ------------------------------------------------
 
@@ -212,7 +226,14 @@ DEFINE_WEB_CONTENTS_USER_DATA_KEY(PasswordManagerDelegateImpl);
 
 PasswordManagerDelegateImpl::PasswordManagerDelegateImpl(
     content::WebContents* web_contents)
-    : web_contents_(web_contents), driver_(web_contents, this) {}
+    : web_contents_(web_contents), driver_(web_contents, this) {
+  // Avoid checking OS password until later on in browser startup
+  // since it calls a few Windows APIs.
+  base::MessageLoopProxy::current()->PostDelayedTask(
+      FROM_HERE,
+      base::Bind(&ReportOsPassword),
+      base::TimeDelta::FromSeconds(10));
+}
 
 PasswordManagerDelegateImpl::~PasswordManagerDelegateImpl() {
 }
