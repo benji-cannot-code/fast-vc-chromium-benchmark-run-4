d3342259b85135671c1cabe0d69c1040494aa085
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-07 08:07:23 +0000

    Move performance module/ to oilpan
    Nothing complicated. This CL just moves performance module/ to oilpan
    using transition types.
    
    BUG=340522
    
    Review URL: https://codereview.chromium.org/157213002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166691 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/performance/WorkerGlobalScopePerformance.h
+++ third_party/WebKit/Source/modules/performance/WorkerGlobalScopePerformance.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WorkerGlobalScopePerformance_h
 
 #include "core/workers/WorkerSupplementable.h"
+#include "heap/Handle.h"
 
 namespace WebCore {
 
@@ -51,7 +52,7 @@ private:
     WorkerPerformance* performance();
     static const char* supplementName();
 
-    RefPtr<WorkerPerformance> m_performance;
+    RefPtrWillBePersistent<WorkerPerformance> m_performance;
 };
 
 } // namespace WebCore
--- third_party/WebKit/Source/modules/performance/WorkerPerformance.h
+++ third_party/WebKit/Source/modules/performance/WorkerPerformance.h
@@ -42,12 +42,15 @@ namespace WebCore {
 
 class ExecutionContext;
 
-class WorkerPerformance : public ScriptWrappable, public RefCounted<WorkerPerformance> {
+class WorkerPerformance : public RefCountedWillBeGarbageCollectedFinalized<WorkerPerformance>, public ScriptWrappable {
+    DECLARE_GC_INFO;
 public:
-    static PassRefPtr<WorkerPerformance> create() { return adoptRef(new WorkerPerformance()); }
+    static PassRefPtrWillBeRawPtr<WorkerPerformance> create() { return adoptRefWillBeNoop(new WorkerPerformance()); }
     ~WorkerPerformance();
 
     double now(ExecutionContext*) const;
+
+    void trace(Visitor*) { }
 
 private:
     WorkerPerformance();
