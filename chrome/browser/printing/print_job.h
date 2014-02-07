ca903814f5f1084ab534f853f6b5605cb8b3192b
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-07 00:33:17 +0000

    Don't run internal message loop from PrintJob::ControlledWorkerShutdown()
    If loops is running, and render is closed, the RenderViewHostImpl that called ControlledWorkerShutdown is going to be destroyed. So RenderViewHostImpl crashes immediately after returning from ControlledWorkerShutdown.
    
    Solution it to use delayed messages instead of nested loop.
    
    BUG=313274
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/136733005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249561 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/printing/print_job.h
+++ chrome/browser/printing/print_job.h
@@ -119,7 +119,7 @@ class PrintJob : public PrintJobWorkerOwner,
   // Called at shutdown when running a nested message loop.
   void Quit();
 
-  void HoldUntilStopIsCalled(const scoped_refptr<PrintJob>& job);
+  void HoldUntilStopIsCalled();
 
   content::NotificationRegistrar registrar_;
 
