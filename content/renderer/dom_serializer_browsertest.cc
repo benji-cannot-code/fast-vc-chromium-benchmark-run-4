4c3b0991ae0aae590f4638036c3d739ace2598a6
ch.dumez@samsung.com <ch.dumez@samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-07 18:49:02 +0000

    Update content/renderer code to use WebElementCollection instead of WebNodeCollection
    Update content/renderer code to use WebElementCollection instead of WebNodeCollection.
    Update safe_browsing code to use WebElementCollection instead of WebNodeCollection.
    After Blink r166411 and r166500, WebNodeCollection is renamed to
    WebElementCollection and its API returns WebElements instead of WebNodes.
    
    The new code is clearer and avoids unnecessary isElementNode() checks.
    
    Review URL: https://codereview.chromium.org/149913011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249736 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/dom_serializer_browsertest.cc
+++ content/renderer/dom_serializer_browsertest.cc
@@ -28,9 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/platform/WebVector.h"
 #include "third_party/WebKit/public/web/WebDocument.h"
 #include "third_party/WebKit/public/web/WebElement.h"
+#include "third_party/WebKit/public/web/WebElementCollection.h"
 #include "third_party/WebKit/public/web/WebFrame.h"
 #include "third_party/WebKit/public/web/WebNode.h"
-#include "third_party/WebKit/public/web/WebNodeCollection.h"
 #include "third_party/WebKit/public/web/WebNodeList.h"
 #include "third_party/WebKit/public/web/WebPageSerializer.h"
 #include "third_party/WebKit/public/web/WebPageSerializerClient.h"
@@ -40,13 +40,12 @@ using blink::WebCString;
 using blink::WebData;
 using blink::WebDocument;
 using blink::WebElement;
+using blink::WebElementCollection;
 using blink::WebFrame;
 using blink::WebNode;
-using blink::WebNodeCollection;
 using blink::WebNodeList;
 using blink::WebPageSerializer;
 using blink::WebPageSerializerClient;
-using blink::WebNode;
 using blink::WebString;
 using blink::WebURL;
 using blink::WebView;
@@ -74,13 +73,10 @@ WebFrame* FindSubFrameByURL(WebView* web_view, const GURL& url) {
     stack.pop_back();
     if (GURL(current_frame->document().url()) == url)
       return current_frame;
-    WebNodeCollection all = current_frame->document().all();
-    for (WebNode node = all.firstItem();
-         !node.isNull(); node = all.nextItem()) {
-      if (!node.isElementNode())
-        continue;
+    WebElementCollection all = current_frame->document().all();
+    for (WebElement element = all.firstItem();
+         !element.isNull(); element = all.nextItem()) {
       // Check frame tag and iframe tag
-      WebElement element = node.to<WebElement>();
       if (!element.hasTagName("frame") && !element.hasTagName("iframe"))
         continue;
       WebFrame* sub_frame = WebFrame::fromFrameOwnerElement(element);
@@ -615,13 +611,10 @@ class DomSerializerTests : public ContentBrowserTest,
     WebDocument doc = web_frame->document();
     ASSERT_TRUE(doc.isHTMLDocument());
     // Go through all descent nodes.
-    WebNodeCollection all = doc.all();
+    WebElementCollection all = doc.all();
     int original_base_tag_count = 0;
-    for (WebNode node = all.firstItem(); !node.isNull();
-         node = all.nextItem()) {
-      if (!node.isElementNode())
-        continue;
-      WebElement element = node.to<WebElement>();
+    for (WebElement element = all.firstItem(); !element.isNull();
+         element = all.nextItem()) {
       if (element.hasTagName("base")) {
         original_base_tag_count++;
       } else {
