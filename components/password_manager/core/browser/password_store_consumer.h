c3a659c4958bec66c43dcfeca941160f59ac82f8
earthdok@chromium.org <earthdok@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-07 19:53:20 +0000

    Fix IWYU violation in password_store_consumer.h
    std::vector was used without \#include <vector>. This broke libc++ builds.
    
    TBR=isherman@chromium.org
    BUG=318770
    
    Review URL: https://codereview.chromium.org/151483006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249748 0039d316-1c4b-4281-b951-d872f2087c98

--- components/password_manager/core/browser/password_store_consumer.h
+++ components/password_manager/core/browser/password_store_consumer.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_PASSWORD_STORE_CONSUMER_H_
 #define COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_PASSWORD_STORE_CONSUMER_H_
 
+#include <vector>
+
 #include "base/task/cancelable_task_tracker.h"
 
 namespace autofill {
