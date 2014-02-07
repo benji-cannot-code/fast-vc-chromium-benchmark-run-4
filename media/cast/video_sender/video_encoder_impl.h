e2880610145b04b8b5cdfd5f375e5920030878a7
piman@chromium.org <piman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-07 02:58:23 +0000

    Revert 249576 "Cast: Wiring up the asyc initialization with the ..."
    > Cast: Wiring up the asyc initialization with the video encoders.
    > 
    > This is a follow up cl to complete the async intialization.
    > 
    > Review URL: https://codereview.chromium.org/151103004
    
    Suspected cause of browser_tests/CastStreamingApiTest.Basics failure:
    http://build.chromium.org/p/chromium.mac/builders/Mac%2010.6%20Tests%20%28dbg%29%281%29/builds/46664/steps/browser_tests/logs/stdio
    
    TBR=pwestin@google.com
    
    Review URL: https://codereview.chromium.org/157053006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249592 0039d316-1c4b-4281-b951-d872f2087c98

--- media/cast/video_sender/video_encoder_impl.h
+++ media/cast/video_sender/video_encoder_impl.h
@@ -32,7 +32,6 @@ class VideoEncoderImpl : public VideoEncoder {
 
   VideoEncoderImpl(scoped_refptr<CastEnvironment> cast_environment,
                    const VideoSenderConfig& video_config,
-                   const CastInitializationCallback& initialization_callback,
                    uint8 max_unacked_frames);
 
   virtual ~VideoEncoderImpl();
@@ -59,7 +58,6 @@ class VideoEncoderImpl : public VideoEncoder {
  private:
   const VideoSenderConfig video_config_;
   scoped_refptr<CastEnvironment> cast_environment_;
-  const CastInitializationCallback initialization_callback_;
   CodecDynamicConfig dynamic_config_;
   bool skip_next_frame_;
   int skip_count_;
