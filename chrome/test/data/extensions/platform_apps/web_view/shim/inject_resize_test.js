1ecb94ff7f545d70857be9fecf4306915c095da0
fsamuel@chromium.org <fsamuel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-07 18:33:32 +0000

    Revert 249482 "Revert 249293 "Revert 248828 "<webview>: navigati..."
    > Revert 249293 "Revert 248828 "<webview>: navigating to WebStore ..."
    > 
    > > Revert 248828 "<webview>: navigating to WebStore should fire a l..."
    > > 
    > > > <webview>: navigating to WebStore should fire a loadabort instead of crashing.
    > > > 
    > > > All top-level navigations now get plumbed through: BrowserPluginGuest::LoadURLWithParams.
    > > > URL validation now happens there. If a URL is determined to be inappropriate for a <webview>,
    > > > a loadabort event will fire instead of crashing the <webview> guest content.
    > > > 
    > > > BUG=334531
    > > > Test=WebViewTest.Shim_TestNavigateToWebStore
    > > > 
    > > > Review URL: https://codereview.chromium.org/140073002
    > > 
    > > TBR=fsamuel@chromium.org
    > > 
    > > Review URL: https://codereview.chromium.org/152783004
    > 
    > TBR=hajimehoshi@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/151243004
    
    TBR=fsamuel@chromium.org
    
    Review URL: https://codereview.chromium.org/157693003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249730 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/platform_apps/web_view/shim/inject_resize_test.js
+++ chrome/test/data/extensions/platform_apps/web_view/shim/inject_resize_test.js
@@ -8,5 +8,5 @@ window.addEventListener('resize', function(e) {
     return;
   }
   var msg = ['resize', document.body.clientWidth, document.body.clientHeight];
-  embedder.channel.postMessage(JSON.stringify(msg), '*');
+  embedder.postMessage(JSON.stringify(msg), '*');
 });
