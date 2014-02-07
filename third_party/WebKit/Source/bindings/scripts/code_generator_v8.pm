1f2a156e744cb849f392c0a9943f79d7978a0982
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-07 05:27:38 +0000

    Rename [GarbageCollected] to [WillBeGarbageCollected]
    We need different IDL attributes for transition types and real types in oilpan.
    We will use [WillBeGarbageCollected] for transition types and
    [GarbageCollected] for real types (which will be implemented in the near future).
    
    Review URL: https://codereview.chromium.org/140663011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166680 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/scripts/code_generator_v8.pm
+++ third_party/WebKit/Source/bindings/scripts/code_generator_v8.pm
@@ -1069,7 +1069,7 @@ sub GetInternalFields
     my @customInternalFields = ();
     # If we have persistentHandleIndex, it should be at the first index of the custom
     # internal fileds.
-    if (IsGarbageCollectedType($interface->name)) {
+    if (IsWillBeGarbageCollectedType($interface->name)) {
         push(@customInternalFields, "persistentHandleIndex");
     }
     # Event listeners on DOM nodes are explicitly supported in the GC controller.
@@ -2931,7 +2931,7 @@ END
     }
 
     my $argumentString = join(", ", @beforeArgumentList, @argumentList, @afterArgumentList);
-    my $refPtrType = IsGarbageCollectedType($interfaceName) ? "RefPtrWillBeRawPtr<$implClassName>" : "RefPtr<$implClassName>";
+    my $refPtrType = IsWillBeGarbageCollectedType($interfaceName) ? "RefPtrWillBeRawPtr<$implClassName>" : "RefPtr<$implClassName>";
     $code .= "    $refPtrType impl = ${implClassName}::create(${argumentString});\n";
     $code .= "    v8::Handle<v8::Object> wrapper = info.Holder();\n";
 
@@ -4370,7 +4370,7 @@ END
 
     my $code = "const WrapperTypeInfo ${v8ClassName}::wrapperTypeInfo = { gin::kEmbedderBlink, ${v8ClassName}::domTemplate, $derefObject, $toActiveDOMObject, $toEventTarget, ";
     $code .= "$visitDOMWrapper, ${v8ClassName}::installPerContextEnabledMethods, $parentClassInfo, $WrapperTypePrototype, ";
-    $code .= IsGarbageCollectedType($interfaceName) ? "true" : "false";
+    $code .= IsWillBeGarbageCollectedType($interfaceName) ? "true" : "false";
     $code .=  " };\n";
     $implementation{nameSpaceWebCore}->addHeader($code);
 
@@ -5482,7 +5482,7 @@ sub GetNativeType
         my $implClassName = GetImplName($interface);
         if ($isParameter) {
             return "$implClassName*";
-        } elsif (IsGarbageCollectedType($interface->name)) {
+        } elsif (IsWillBeGarbageCollectedType($interface->name)) {
             return "RefPtrWillBeRawPtr<$implClassName>";
         } else {
             return "RefPtr<$implClassName>";
@@ -5925,7 +5925,7 @@ sub GetPassRefPtrType
     my $interface = shift;
     my $nativeType = GetNativeTypeForConversions($interface);
 
-    my $willBe = IsGarbageCollectedType($interface->name) ? "WillBeRawPtr" : "";
+    my $willBe = IsWillBeGarbageCollectedType($interface->name) ? "WillBeRawPtr" : "";
     my $extraSpace = $nativeType =~ />$/ ? " " : "";
     return "PassRefPtr${willBe}<${nativeType}${extraSpace}>";
 }
@@ -6033,12 +6033,12 @@ sub IsTypedArrayType
     return 0;
 }
 
-sub IsGarbageCollectedType
+sub IsWillBeGarbageCollectedType
 {
     my $interfaceName = shift;
     return 0 unless IsWrapperType($interfaceName);
     my $interface = ParseInterface($interfaceName);
-    return $interface->extendedAttributes->{"GarbageCollected"};
+    return $interface->extendedAttributes->{"WillBeGarbageCollected"};
 }
 
 sub IsRefPtrType
