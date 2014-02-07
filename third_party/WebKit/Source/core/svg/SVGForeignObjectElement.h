e62abc6c113aef9f2e24ece02edfcec6576df8cf
fs@opera.com <fs@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-07 20:06:50 +0000

    Remove SVGElement::childShouldCreateRenderer
    All implementation now return true, and the RenderObject::isChildAllowed
    mechanism is used instead.
    
    BUG=337607
    
    Review URL: https://codereview.chromium.org/157663002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166733 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGForeignObjectElement.h
+++ third_party/WebKit/Source/core/svg/SVGForeignObjectElement.h
@@ -46,7 +46,6 @@ private:
     virtual void svgAttributeChanged(const QualifiedName&) OVERRIDE;
 
     virtual bool rendererIsNeeded(const RenderStyle&) OVERRIDE;
-    virtual bool childShouldCreateRenderer(const Node& child) const OVERRIDE { return true; }
     virtual RenderObject* createRenderer(RenderStyle*) OVERRIDE;
 
     virtual bool selfHasRelativeLengths() const OVERRIDE;
--- third_party/WebKit/Source/core/svg/SVGTSpanElement.h
+++ third_party/WebKit/Source/core/svg/SVGTSpanElement.h
@@ -34,7 +34,6 @@ private:
     explicit SVGTSpanElement(Document&);
 
     virtual RenderObject* createRenderer(RenderStyle*) OVERRIDE;
-    virtual bool childShouldCreateRenderer(const Node& child) const OVERRIDE { return true; }
     virtual bool rendererIsNeeded(const RenderStyle&) OVERRIDE;
 };
 
