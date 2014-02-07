7fe37a3ad52ea1b7419018c6e1d8a0a3f1031627
andresantoso@chromium.org <andresantoso@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-07 08:19:22 +0000

    mac: Fix some bugs related to multiple tab selection, in preparation for implementing dragging of multiple tabs.
    1. Clicking a selected (but inactive) tab should activate it.
    2. Cmd-clicking a selected (but inactive) tab should deselect it.
    
    TEST=Click around tabs using modifier keys, behavior should be the same
    as before except for the 2 fixes above.
    
    BUG=10899
    
    Review URL: https://codereview.chromium.org/130773006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249622 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/tabs/tab_strip_controller.mm
+++ chrome/browser/ui/cocoa/tabs/tab_strip_controller.mm
@@ -810,7 +810,7 @@ NSImage* Overlay(NSImage* ground, NSImage* overlay, CGFloat alpha) {
       tabStripModel_->ExtendSelectionTo(index);
     } else if (modifiers & NSCommandKeyMask) {
       tabStripModel_->ToggleSelectionAt(index);
-    } else if (!tabStripModel_->IsTabSelected(index)) {
+    } else {
       tabStripModel_->ActivateTabAt(index, true);
     }
   }
@@ -1310,8 +1310,6 @@ NSImage* Overlay(NSImage* ground, NSImage* overlay, CGFloat alpha) {
                         atIndex:(NSInteger)modelIndex
                          reason:(int)reason {
   // Take closing tabs into account.
-  NSInteger activeIndex = [self indexFromModelIndex:modelIndex];
-
   if (oldContents) {
     int oldModelIndex =
         browser_->tab_strip_model()->GetIndexOfWebContents(oldContents);
@@ -1324,21 +1322,13 @@ NSImage* Overlay(NSImage* ground, NSImage* overlay, CGFloat alpha) {
     }
   }
 
-  // First get the vector of indices, which is allays sorted in ascending order.
-  ui::ListSelectionModel::SelectedIndices selection(
-      tabStripModel_->selection_model().selected_indices());
-  // Iterate through all of the tabs, selecting each as necessary.
-  ui::ListSelectionModel::SelectedIndices::iterator iter = selection.begin();
-  int i = 0;
-  for (TabController* current in tabArray_.get()) {
-    BOOL selected = iter != selection.end() &&
-        [self indexFromModelIndex:*iter] == i;
-    [current setSelected:selected];
-    [current setActive:i == activeIndex];
-    if (selected)
-      ++iter;
-    ++i;
-  }
+  NSUInteger activeIndex = [self indexFromModelIndex:modelIndex];
+
+  [tabArray_ enumerateObjectsUsingBlock:^(TabController* current,
+                                          NSUInteger index,
+                                          BOOL* stop) {
+      [current setActive:index == activeIndex];
+  }];
 
   // Tell the new tab contents it is about to become the selected tab. Here it
   // can do things like make sure the toolbar is up to date.
@@ -1357,6 +1347,23 @@ NSImage* Overlay(NSImage* ground, NSImage* overlay, CGFloat alpha) {
   if (newContents) {
     newContents->WasShown();
     newContents->GetView()->RestoreFocus();
+  }
+}
+
+- (void)tabSelectionChanged {
+  // First get the vector of indices, which is allays sorted in ascending order.
+  ui::ListSelectionModel::SelectedIndices selection(
+      tabStripModel_->selection_model().selected_indices());
+  // Iterate through all of the tabs, selecting each as necessary.
+  ui::ListSelectionModel::SelectedIndices::iterator iter = selection.begin();
+  int i = 0;
+  for (TabController* current in tabArray_.get()) {
+    BOOL selected = iter != selection.end() &&
+        [self indexFromModelIndex:*iter] == i;
+    [current setSelected:selected];
+    if (selected)
+      ++iter;
+    ++i;
   }
 }
 
