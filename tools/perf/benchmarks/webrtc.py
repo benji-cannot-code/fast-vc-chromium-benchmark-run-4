cbe0e577a266a3ba29604daf1ab945945f3b5f05
kjellander@chromium.org <kjellander@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-07 17:51:58 +0000

    Simple Telemetry benchmark for WebRTC
    Adds a benchmark that performs CPU and memory
    measurements on a simple page only showing the
    video stream from a local fake video device in 
    a video tag.
    
    BUG=333257
    TEST=Compiled Chrome in Release mode. Ran:
    tools/perf/run_benchmark --browser=release webrtc
    
    Review URL: https://codereview.chromium.org/133733003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249721 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/perf/benchmarks/webrtc.py
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+from measurements import webrtc
+from telemetry import test
+
+
+class WebRTC(test.Test):
+  """Obtains WebRTC metrics for local video playback."""
+  test = webrtc.WebRTC
+  page_set = 'page_sets/webrtc_cases.json'
