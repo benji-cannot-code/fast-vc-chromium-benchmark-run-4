415b2137e3e24ac962c31516ca61c58349cfbf67
ch.dumez@samsung.com <ch.dumez@samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-07 18:38:05 +0000

    Update components/autofill code to use WebElementCollection instead of WebNodeCollection
    Update components/autofill code to use WebElementCollection instead of WebNodeCollection.
    After Blink r166411 and r166500, WebNodeCollection is renamed to
    WebElementCollection and its API returns WebElements instead of WebNodes.
    
    Review URL: https://codereview.chromium.org/145393004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249732 0039d316-1c4b-4281-b951-d872f2087c98

--- components/autofill/content/renderer/autofill_agent.cc
+++ components/autofill/content/renderer/autofill_agent.cc
@@ -35,12 +35,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/platform/WebURLRequest.h"
 #include "third_party/WebKit/public/web/WebDataSource.h"
 #include "third_party/WebKit/public/web/WebDocument.h"
+#include "third_party/WebKit/public/web/WebElementCollection.h"
 #include "third_party/WebKit/public/web/WebFormControlElement.h"
 #include "third_party/WebKit/public/web/WebFormElement.h"
 #include "third_party/WebKit/public/web/WebFrame.h"
 #include "third_party/WebKit/public/web/WebInputEvent.h"
 #include "third_party/WebKit/public/web/WebNode.h"
-#include "third_party/WebKit/public/web/WebNodeCollection.h"
 #include "third_party/WebKit/public/web/WebOptionElement.h"
 #include "third_party/WebKit/public/web/WebView.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -53,7 +53,7 @@ using blink::WebFrame;
 using blink::WebInputElement;
 using blink::WebKeyboardEvent;
 using blink::WebNode;
-using blink::WebNodeCollection;
+using blink::WebElementCollection;
 using blink::WebOptionElement;
 using blink::WebString;
 
@@ -67,7 +67,7 @@ void GetDataListSuggestions(const blink::WebInputElement& element,
                             bool ignore_current_value,
                             std::vector<base::string16>* values,
                             std::vector<base::string16>* labels) {
-  WebNodeCollection options = element.dataListOptions();
+  WebElementCollection options = element.dataListOptions();
   if (options.isNull())
     return;
 
