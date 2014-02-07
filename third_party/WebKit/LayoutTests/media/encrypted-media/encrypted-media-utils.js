f82dbbcb2659e2b0f4a933e5a712ec175aecba8c
xhwang@chromium.org <xhwang@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-07 08:35:06 +0000

    Convert encrypted-media-needkey test to use W3C testharness.
    - Add encrypted-media-utils.js for commonly used utility function for encrypted media tests.
    - Also remove unnecessary variables in the encrypted-media-needkey test.
    
    BUG=338485
    TEST=This update an existing test to use W3C testharness.
    
    Review URL: https://codereview.chromium.org/134753006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166693 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/media/encrypted-media/encrypted-media-utils.js
@@ -0,0 +1,48 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function stringToUint8Array(str)
+{
+    var result = new Uint8Array(str.length);
+    for(var i = 0; i < str.length; i++) {
+        result[i] = str.charCodeAt(i);
+    }
+    return result;
+}
+
+// For Clear Key, MediaKeySession.update() takes a JSON Web Key (JWK) Set,
+// which contains a set of cryptographic keys represented by JSON. These helper
+// functions help wrap raw keys into a JWK set.
+// See:
+// https://dvcs.w3.org/hg/html-media/raw-file/tip/encrypted-media/encrypted-media.html#simple-decryption-clear-key
+// http://tools.ietf.org/html/draft-ietf-jose-json-web-key
+
+// Encodes data into base64 string without trailing '='.
+function base64Encode(data)
+{
+    var result = btoa(String.fromCharCode.apply(null, data));
+    return result.replace(/=+$/g, '');
+}
+
+// Creates a JWK from raw key ID and key.
+function createJWK(keyId, key)
+{
+    var jwk = "{\"kty\":\"oct\",\"kid\":\"";
+    jwk += base64Encode(keyId);
+    jwk += "\",\"k\":\"";
+    jwk += base64Encode(key);
+    jwk += "\"}";
+    return jwk;
+}
+
+// Creates a JWK Set from multiple JWKs.
+function createJWKSet()
+{
+    var jwkSet = "{\"keys\":[";
+    for (var i = 0; i < arguments.length; i++) {
+        if (i != 0)
+            jwkSet += ",";
+        jwkSet += arguments[i];
+    }
+    jwkSet += "]}";
+    return jwkSet;
+}
+
