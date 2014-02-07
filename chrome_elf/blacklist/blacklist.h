02c05d09b616caedfa1e15ddde46e37428da68d3
csharp@chromium.org <csharp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-07 20:23:34 +0000

    Add 64-bit support to browser blacklisting
    BUG=
    
    Review URL: https://codereview.chromium.org/101203010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249755 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_elf/blacklist/blacklist.h
+++ chrome_elf/blacklist/blacklist.h
@@ -6,6 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_ELF_BLACKLIST_BLACKLIST_H_
 #define CHROME_ELF_BLACKLIST_BLACKLIST_H_
 
+#if defined(_WIN64)
+#include "sandbox/win/src/sandbox_nt_types.h"
+#endif
+
 namespace blacklist {
 
 // Max size of the DLL blacklist.
@@ -37,6 +41,10 @@ enum BlacklistState {
   // Always keep this at the end.
   BLACKLIST_STATE_MAX,
 };
+
+#if defined(_WIN64)
+extern NtMapViewOfSectionFunction g_nt_map_view_of_section_func;
+#endif
 
 // Attempts to leave a beacon in the current user's registry hive.
 // If the blacklist beacon doesn't say it is enabled or there are any other
