0ccb8b615e709ec050429d28f328784d643d450e
jdonnelly@chromium.org <jdonnelly@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-07 00:23:33 +0000

    [OriginChip] Hide/show the origin chip as the Omnibox gains/loses focus.
    The ultimate implementation may be rather more complicated, but this simple approach covers nearly all of the currently specified behaviors and should be a good starting point for everyone to play around with.
    
    Also add a check for editing in progress when the browser view sets focus to the location bar so that Ctrl-l/Alt-d won't clobber any edits in progress.
    
    BUG=338563
    
    Review URL: https://codereview.chromium.org/143043004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249555 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/toolbar/toolbar_model.cc
+++ chrome/browser/ui/toolbar/toolbar_model.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 ToolbarModel::ToolbarModel()
     : input_in_progress_(false),
+      origin_chip_enabled_(true),
       url_replacement_enabled_(true) {
 }
 
