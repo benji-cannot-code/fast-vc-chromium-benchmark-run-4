e19c947fcf8ec9d8d195bcb16aab4c3976cafa94
erik.corry@gmail.com <erik.corry@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-07 13:24:04 +0000

    [oilpan] Fix OffHeapCollectionTraceTrait
    R=ager@chromium.org, haraken@chromium.org, keishi@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/157093004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166709 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/heap/Handle.h
+++ third_party/WebKit/Source/heap/Handle.h
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 template<typename T> class Member;
-template<typename T> struct OffHeapCollectionTraceTrait;
 
 class PersistentNode {
 public:
