341ea10fdfe57f2a7ab3ca5a3caad1527d39d5fa
yhirano@chromium.org <yhirano@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-07 13:49:23 +0000

    Show warning when connecting to insecure server from secure page.
    The W3C WebSocket API specifies that the browser must throw an SecurityError
    exception if the requested URL is insecure and the origin is secure.
    For backward compatibility, this CL shows a warning message and doesn't throw an exception.
    
    BUG=85271
    R=tyoshino, abarth
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=165922
    
    Review URL: https://codereview.chromium.org/143763002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166710 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/weborigin/SchemeRegistry.cpp
+++ third_party/WebKit/Source/platform/weborigin/SchemeRegistry.cpp
@@ -56,6 +56,7 @@ static URLSchemesMap& secureSchemes()
         secureSchemes.add("https");
         secureSchemes.add("about");
         secureSchemes.add("data");
+        secureSchemes.add("wss");
     }
 
     return secureSchemes;
