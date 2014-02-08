a66e80740d9c82544deb1c201f716311d199f38b
dfalcantara@chromium.org <dfalcantara@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-08 00:32:58 +0000

    Adds a new ContentSettingType for Android app banners
    These settings will store information about what Play
    Store apps a site has popped up a banner for.
    
    * The ContentSetting will store a DictionaryValue of
    <package name, is allowed> pairs.  If anything except a
    DictionaryValue is found in the setting, it is wiped out.
    
    * Once a banner is dismissed, it will be permanently blocked 
    from being shown again by storing it in the DictionaryValue
    for the site.
    
    * We limit the number of apps a site is allowed to show a
    banner for to just 3 to avoid situations where a site tries
    inundating a user with banners for different apps.
    
    * These settings are destroyed when the user chooses to
    clear their site data and are not meant to be directly
    visible from the UI.
    
    BUG=341556
    
    Review URL: https://codereview.chromium.org/132333023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249864 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/android/banners/app_banner_settings_helper.h
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ANDROID_BANNERS_APP_BANNER_SETTINGS_HELPER_H_
+#define CHROME_BROWSER_ANDROID_BANNERS_APP_BANNER_SETTINGS_HELPER_H_
+
+#include <string>
+
+#include "base/macros.h"
+
+namespace content {
+class WebContents;
+}  // namesapce content
+
+class GURL;
+
+// Utility class for reading and updating ContentSettings for app banners.
+class AppBannerSettingsHelper {
+ public:
+  // Checks if a URL is allowed to show a banner for the given package.
+  static bool IsAllowed(content::WebContents* web_contents,
+                        const GURL& origin_url,
+                        const std::string& package_name);
+
+  // Blocks a URL from showing a banner for the given package.
+  static void Block(content::WebContents* web_contents,
+                    const GURL& origin_url,
+                    const std::string& package_name);
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(AppBannerSettingsHelper);
+};
+
+#endif  // CHROME_BROWSER_ANDROID_BANNERS_APP_BANNER_SETTINGS_HELPER_H_
