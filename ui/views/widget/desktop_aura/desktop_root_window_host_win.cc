16678100d1aa043914c573b5362d2444e7d9028e
enne@chromium.org <enne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-08 03:17:48 +0000

    Remove NOTIMPLEMENTED noise in DesktopWindowTreeHostWin
    Tests pass, so it is clearly implemented enough.
    
    BUG=339891
    
    Review URL: https://codereview.chromium.org/141063007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249918 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/desktop_aura/desktop_root_window_host_win.cc
+++ ui/views/widget/desktop_aura/desktop_root_window_host_win.cc
@@ -772,13 +772,7 @@ void DesktopWindowTreeHostWin::HandleAccelerator(
 }
 
 void DesktopWindowTreeHostWin::HandleCreate() {
-  // TODO(beng): moar
-  NOTIMPLEMENTED();
-
   native_widget_delegate_->OnNativeWidgetCreated(true);
-
-  // 1. Window property association
-  // 2. MouseWheel.
 }
 
 void DesktopWindowTreeHostWin::HandleDestroying() {
