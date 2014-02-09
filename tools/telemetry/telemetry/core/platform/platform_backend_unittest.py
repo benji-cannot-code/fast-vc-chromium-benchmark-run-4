44b83a6ff323451116d95f33a2d16ca9a11d1960
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-09 22:50:10 +0000

    [Telemetry] Add idle process metrics on OS X > 10.8.4 .
    BUG=338712
    
    Review URL: https://codereview.chromium.org/143823019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250016 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/platform/platform_backend_unittest.py
+++ tools/telemetry/telemetry/core/platform/platform_backend_unittest.py
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 import logging
+import os
 import unittest
 
 from telemetry.core import platform
@@ -19,3 +20,13 @@ class PlatformBackendTest(unittest.TestCase):
 
     output = backend.MonitorPowerSync(1)
     self.assertTrue(output.has_key('power_samples_mw'))
+
+  def testGetCPUStats(self):
+    backend = platform.CreatePlatformBackendForCurrentOS()
+    cpu_stats = backend.GetCpuStats(os.getpid())
+
+    self.assertGreater(cpu_stats['CpuProcessTime'], 0)
+    if backend.CanMonitorPowerSync():
+      self.assertTrue(cpu_stats.has_key('interrupt_wakeup_count'))
+      self.assertTrue(cpu_stats.has_key('package_idle_exit_count'))
+
