2c5e08a1d1a9c0b457074def2e922723f7aa71c7
tmdiep@chromium.org <tmdiep@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-09 23:06:04 +0000

    Use the same code to serialize app window state for the renderer
    We send the properties of the app window to the renderer process at
    window creation time and when the native app window state changes.
    The code to serialize the state into a dictionary is duplicated
    across two different locations. They should just use the same code.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/157133003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250023 0039d316-1c4b-4281-b951-d872f2087c98

--- apps/shell_window.h
+++ apps/shell_window.h
@@ -25,6 +25,10 @@ class GURL;
 class Profile;
 class SkRegion;
 
+namespace base {
+class DictionaryValue;
+}
+
 namespace content {
 class WebContents;
 }
@@ -344,6 +348,10 @@ class ShellWindow : public content::NotificationObserver,
   // API. Note that the actual value of this property in the native app window
   // may be false if the bit is silently switched off for security reasons.
   bool IsAlwaysOnTop() const;
+
+  // Retrieve the current state of the app window as a dictionary, to pass to
+  // the renderer.
+  void GetSerializedState(base::DictionaryValue* properties) const;
 
  protected:
   virtual ~ShellWindow();
