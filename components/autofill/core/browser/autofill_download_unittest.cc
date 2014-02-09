d8a6e7fe0c792971f424088f7d17dbd3d78ba791
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-09 22:57:57 +0000

    Remove WebInputElement.h includes from some autofill core unittests.
    This also allow us to remove this entry from the DEPS file.
    
    BUG=140037
    TEST=unit_tests links
    TBR=joi@chromium.org,blundell@chromium.org
    
    Review URL: https://codereview.chromium.org/145123011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250019 0039d316-1c4b-4281-b951-d872f2087c98

--- components/autofill/core/browser/autofill_download_unittest.cc
+++ components/autofill/core/browser/autofill_download_unittest.cc
@@ -22,10 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request_status.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "third_party/WebKit/public/web/WebInputElement.h"
 
 using base::ASCIIToUTF16;
-using blink::WebInputElement;
 
 namespace autofill {
 
--- components/autofill/core/browser/form_structure_unittest.cc
+++ components/autofill/core/browser/form_structure_unittest.cc
@@ -12,11 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/common/form_data.h"
 #include "components/autofill/core/common/form_field_data.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "third_party/WebKit/public/web/WebInputElement.h"
 #include "url/gurl.h"
 
 using base::ASCIIToUTF16;
-using blink::WebInputElement;
 
 namespace autofill {
 namespace {
