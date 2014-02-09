bf5703b36c700af41c95e716d93a31d2c5a6227f
vollick@chromium.org <vollick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-09 03:58:39 +0000

    Revert 249982 "Omnibox: Pass Input Box Type to AutocompleteClass..."
    > Omnibox: Pass Input Box Type to AutocompleteClassifier
    > 
    > (where possible and reasonable)
    > 
    > This is a resubmission of https://codereview.chromium.org/138173007/
    > that should have fixed the gtk compile error.
    > 
    > 
    > Previously, the input box type (a.k.a. current page classification)
    > (search results page doing search term replacement, arbitrary URL,
    > new tab page omnibox, new tab page fakebox, etc.) was not passed to
    > AutocompleteClassifier. This adds plumbing to pass the variable to
    > the classifier in some cases. (Some other cases I decided were too
    > much of a hassle to fix, and not important.)
    > 
    > TBR=brettw,pkasting
    > brettw:
    > for changes to chrome/browser/tab_contents/render_view_context_menu.cc
    > pkasting:
    > because he already reviewed it
    > 
    > BUG=265678
    > 
    > Review URL: https://codereview.chromium.org/157033005
    
    TBR=mpearson@chromium.org
    
    Review URL: https://codereview.chromium.org/158433003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249984 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/gtk_util.cc
+++ chrome/browser/ui/gtk/gtk_util.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/autocomplete/autocomplete_classifier.h"
 #include "chrome/browser/autocomplete/autocomplete_classifier_factory.h"
-#include "chrome/browser/autocomplete/autocomplete_input.h"
 #include "chrome/browser/autocomplete/autocomplete_match.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
@@ -862,8 +861,7 @@ bool URLFromPrimarySelection(Profile* profile, GURL* url) {
   // a search query if necessary.
   AutocompleteMatch match;
   AutocompleteClassifierFactory::GetForProfile(profile)->Classify(
-      base::UTF8ToUTF16(selection_text), false, false,
-      AutocompleteInput::INVALID_SPEC, &match, NULL);
+      base::UTF8ToUTF16(selection_text), false, false, &match, NULL);
   g_free(selection_text);
   if (!match.destination_url.is_valid())
     return false;
