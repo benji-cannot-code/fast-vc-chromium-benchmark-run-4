57d70153bd4411e02db118e6bd687a07dab19e1c
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-10 23:04:29 +0000

    Cleanup: Fix comments for IPC messages that got renamed.
    Review URL: https://codereview.chromium.org/158303002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250231 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/render_messages.h
+++ chrome/common/render_messages.h
@@ -48,8 +48,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class SkBitmap;
 
 // Command values for the cmd parameter of the
-// ViewHost_JavaScriptStressTestControl message. For each command the parameter
-// passed has a different meaning:
+// ChromeViewMsg_JavaScriptStressTestControl message. For each command the
+// parameter passed has a different meaning:
 // For the command kJavaScriptStressTestSetStressRunType the parameter it the
 // type taken from the enumeration v8::Testing::StressType.
 // For the command kJavaScriptStressTestPrepareStressRun the parameter it the
