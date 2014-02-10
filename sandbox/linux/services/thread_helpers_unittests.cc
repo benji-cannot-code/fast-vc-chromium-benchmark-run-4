0ebc1ef5b554eab648247610df079df27dbd34cc
glider@chromium.org <glider@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-10 08:33:42 +0000

    Disable the failing ThreadHelpers.* tests under ThreadSanitizer for now.
    BUG=342305
    TBR=jln@chromium.org
    
    Review URL: https://codereview.chromium.org/158703002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250062 0039d316-1c4b-4281-b951-d872f2087c98

--- sandbox/linux/services/thread_helpers_unittests.cc
+++ sandbox/linux/services/thread_helpers_unittests.cc
@@ -51,7 +51,18 @@ class ScopedProcSelfTask {
   DISALLOW_COPY_AND_ASSIGN(ScopedProcSelfTask);
 };
 
-TEST(ThreadHelpers, IsSingleThreadedBasic) {
+#if defined(THREAD_SANITIZER)
+// These tests fail under ThreadSanitizer, see http://crbug.com/342305
+#define MAYBE_IsSingleThreadedBasic DISABLED_IsSingleThreadedBasic
+#define MAYBE_IsSingleThreadedIterated DISABLED_IsSingleThreadedIterated
+#define MAYBE_IsSingleThreadedStartAndStop DISABLED_IsSingleThreadedStartAndStop
+#else
+#define MAYBE_IsSingleThreadedBasic IsSingleThreadedBasic
+#define MAYBE_IsSingleThreadedIterated IsSingleThreadedIterated
+#define MAYBE_IsSingleThreadedStartAndStop IsSingleThreadedStartAndStop
+#endif
+
+TEST(ThreadHelpers, MAYBE_IsSingleThreadedBasic) {
   ScopedProcSelfTask task;
   ASSERT_TRUE(ThreadHelpers::IsSingleThreaded(task.fd()));
 
@@ -60,7 +71,7 @@ TEST(ThreadHelpers, IsSingleThreadedBasic) {
   ASSERT_FALSE(ThreadHelpers::IsSingleThreaded(task.fd()));
 }
 
-TEST(ThreadHelpers, IsSingleThreadedIterated) {
+TEST(ThreadHelpers, MAYBE_IsSingleThreadedIterated) {
   ScopedProcSelfTask task;
   ASSERT_TRUE(ThreadHelpers::IsSingleThreaded(task.fd()));
 
@@ -72,7 +83,7 @@ TEST(ThreadHelpers, IsSingleThreadedIterated) {
   }
 }
 
-TEST(ThreadHelpers, IsSingleThreadedStartAndStop) {
+TEST(ThreadHelpers, MAYBE_IsSingleThreadedStartAndStop) {
   ScopedProcSelfTask task;
   ASSERT_TRUE(ThreadHelpers::IsSingleThreaded(task.fd()));
 
