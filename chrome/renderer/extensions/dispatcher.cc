26870aad722292f4b785cc35f6dfa1e0f5a8283c
dominicc@chromium.org <dominicc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-10 14:39:53 +0000

    Don't explicitly enable embedder custom elements.
    They're enabled by default.
    
    BUG=274833
    
    Review URL: https://codereview.chromium.org/150133004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250097 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/extensions/dispatcher.cc
+++ chrome/renderer/extensions/dispatcher.cc
@@ -1394,7 +1394,6 @@ void Dispatcher::AddOrRemoveOriginPermissions(
 }
 
 void Dispatcher::EnableCustomElementWhiteList() {
-  blink::WebRuntimeFeatures::enableEmbedderCustomElements(true);
   blink::WebCustomElement::addEmbedderCustomElementName("webview");
   // TODO(fsamuel): Add <adview> to the whitelist once it has been converted
   // into a custom element.
