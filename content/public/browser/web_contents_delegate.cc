81c2ff8f6e71b7464664d39eeba73d0bc5cecd83
marshall@chromium.org <marshall@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-10 16:37:47 +0000

    Pass RenderFrameHost to WebContentsDelegate::HandleContextMenu. This is necessary in order to call RenderFrameHost::NotifyContextMenuClosed at a future time.
    BUG=304341
    
    Review URL: https://codereview.chromium.org/148843003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250116 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/browser/web_contents_delegate.cc
+++ content/public/browser/web_contents_delegate.cc
@@ -77,6 +77,7 @@ void WebContentsDelegate::CanDownload(
 }
 
 bool WebContentsDelegate::HandleContextMenu(
+    RenderFrameHost* render_frame_host,
     const content::ContextMenuParams& params) {
   return false;
 }
