476aec1d94bddfd1dcd3def2dbd494ccf760e761
sigbjornf@opera.com <sigbjornf@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-10 03:17:42 +0000

    Fix WorkerGlobalScopePerformance thread state mismatch asserts.
    A handful of worker tests (in webexposed/ and http/tests/w3c/webperf/)
    currently assert and fail.
    
    The Persistent reference to WorkerPerformance from
    WorkerGlobalScopePerformance requires that the worker thread looks up
    and uses the current thread state rather than that of the main thread.
    That state is associated with the Persistent object when created.
    
    R=
    BUG=340522
    
    Review URL: https://codereview.chromium.org/158513002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166791 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/performance/WorkerGlobalScopePerformance.h
+++ third_party/WebKit/Source/modules/performance/WorkerGlobalScopePerformance.h
@@ -34,10 +34,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/workers/WorkerSupplementable.h"
 #include "heap/Handle.h"
+#include "modules/performance/WorkerPerformance.h"
 
 namespace WebCore {
-
-class WorkerPerformance;
 
 class WorkerGlobalScopePerformance FINAL : public WorkerSupplement {
 public:
