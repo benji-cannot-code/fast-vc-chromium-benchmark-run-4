e1ad6a75ef6b9a713cc14fd26a441407d30d9f1f
bulach@chromium.org <bulach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-10 19:52:42 +0000

    Allows logging the field names used by autofill.
    BUG=341493
    
    Review URL: https://codereview.chromium.org/150503003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250182 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/android/java/src/org/chromium/ui/autofill/AutofillPopup.java
+++ ui/android/java/src/org/chromium/ui/autofill/AutofillPopup.java
@@ -57,6 +57,20 @@ public class AutofillPopup extends ListPopupWindow implements AdapterView.OnItem
     private List<AutofillSuggestion> mSuggestions;
 
     /**
+     * An interface that can be injected to log field names selected by
+     * the autofill.
+     */
+    public interface AutofillLogger {
+        public void logSuggestionSelected(String fieldName);
+    }
+
+    private static AutofillLogger sAutofillLogger = null;
+
+    public static void setAutofillLogger(AutofillLogger autofillLogger) {
+        sAutofillLogger = autofillLogger;
+    }
+
+    /**
      * An interface to handle the touch interaction with an AutofillPopup object.
      */
     public interface AutofillPopupDelegate {
@@ -220,8 +234,12 @@ public class AutofillPopup extends ListPopupWindow implements AdapterView.OnItem
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         AutofillListAdapter adapter = (AutofillListAdapter) parent.getAdapter();
-        int listIndex = mSuggestions.indexOf(adapter.getItem(position));
+        AutofillSuggestion selectedSuggestion = adapter.getItem(position);
+        int listIndex = mSuggestions.indexOf(selectedSuggestion);
         assert listIndex > -1;
+        if (sAutofillLogger != null) {
+            sAutofillLogger.logSuggestionSelected(selectedSuggestion.mLabel);
+        }
         mAutofillCallback.suggestionSelected(listIndex);
     }
 
