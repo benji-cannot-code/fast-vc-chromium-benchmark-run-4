db34b8bcfb5c02030bd101ce9ac79b794017964c
eustas@chromium.org <eustas@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-10 07:32:55 +0000

    Turn tri-state button to toggle button + optional checkbox.
    BUG=262401
    
    Review URL: https://codereview.chromium.org/151323005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166806 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/DebuggerModel.js
+++ third_party/WebKit/Source/devtools/front_end/DebuggerModel.js
@@ -46,8 +46,8 @@ WebInspector.DebuggerModel = function()
 
     this._breakpointsActive = true;
 
-    WebInspector.settings.pauseOnExceptionStateString = WebInspector.settings.createSetting("pauseOnExceptionStateString", WebInspector.DebuggerModel.PauseOnExceptionsState.DontPauseOnExceptions);
-    WebInspector.settings.pauseOnExceptionStateString.addChangeListener(this._pauseOnExceptionStateChanged, this);
+    WebInspector.settings.pauseOnExceptionEnabled.addChangeListener(this._pauseOnExceptionStateChanged, this);
+    WebInspector.settings.pauseOnCaughtException.addChangeListener(this._pauseOnExceptionStateChanged, this);
 
     WebInspector.settings.enableAsyncStackTraces.addChangeListener(this._asyncStackTracesStateChanged, this);
 
@@ -56,7 +56,11 @@ WebInspector.DebuggerModel = function()
     WebInspector.DebuggerModel.applySkipStackFrameSettings();
 }
 
-// Keep these in sync with WebCore::ScriptDebugServer
+/**
+ * Keep these in sync with WebCore::ScriptDebugServer
+ *
+ * @enum {string}
+ */
 WebInspector.DebuggerModel.PauseOnExceptionsState = {
     DontPauseOnExceptions : "none",
     PauseOnAllExceptions : "all",
@@ -161,7 +165,15 @@ WebInspector.DebuggerModel.prototype = {
 
     _pauseOnExceptionStateChanged: function()
     {
-        DebuggerAgent.setPauseOnExceptions(WebInspector.settings.pauseOnExceptionStateString.get());
+        var state;
+        if (!WebInspector.settings.pauseOnExceptionEnabled.get()) {
+            state = WebInspector.DebuggerModel.PauseOnExceptionsState.DontPauseOnExceptions;
+        } else if (WebInspector.settings.pauseOnCaughtException.get()) {
+            state = WebInspector.DebuggerModel.PauseOnExceptionsState.PauseOnAllExceptions;
+        } else {
+            state = WebInspector.DebuggerModel.PauseOnExceptionsState.PauseOnUncaughtExceptions;
+        }
+        DebuggerAgent.setPauseOnExceptions(state);
     },
 
     _asyncStackTracesStateChanged: function()
