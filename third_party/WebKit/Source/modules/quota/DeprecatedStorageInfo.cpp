a9a5320943b88af1858229d624ad20dcda586ccb
sigbjornf@opera.com <sigbjornf@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-10 11:08:46 +0000

    Move quota module to oilpan
    This moves the storage quota interfaces to oilpan, using transition
    types.
    
    Mostly regular, apart from one one interface (DeprecatedStorageQuota)
    being used from both a main world and worker context, requiring thread
    affinity defaults to be overridden for its class.
    
    R=haraken@chromium.org
    BUG=340522
    
    Review URL: https://codereview.chromium.org/158133003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166819 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/quota/DeprecatedStorageInfo.cpp
+++ third_party/WebKit/Source/modules/quota/DeprecatedStorageInfo.cpp
@@ -43,6 +43,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+DEFINE_GC_INFO(DeprecatedStorageInfo);
+
 DeprecatedStorageInfo::DeprecatedStorageInfo()
 {
     ScriptWrappable::init(this);
@@ -89,6 +91,12 @@ DeprecatedStorageQuota* DeprecatedStorageInfo::getStorageQuota(int storageType)
         return m_persistentStorage.get();
     }
     return 0;
+}
+
+void DeprecatedStorageInfo::trace(Visitor* visitor)
+{
+    visitor->trace(m_temporaryStorage);
+    visitor->trace(m_persistentStorage);
 }
 
 } // namespace WebCore
