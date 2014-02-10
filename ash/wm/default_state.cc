45104caed68b69b0e75976ad228e5e09af6a14fd
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-10 18:38:38 +0000

    Fix behavior when double clicking on borders and caption area of side maximized windows.
    This CL additionally marks WindowState::IsNormalShowState() as deprecated and adds WindowState::IsNormalShowType().
    
    BUG=340169
    TEST=WorkspaceEventHandlerTest.DoubleClickSingleAxisWhenSideSnapped
    
    Review URL: https://codereview.chromium.org/150093005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250158 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/default_state.cc
+++ ash/wm/default_state.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/wm/default_state.h"
 
+#include "ash/screen_util.h"
 #include "ash/shell.h"
 #include "ash/wm/coordinate_conversion.h"
 #include "ash/wm/window_state.h"
@@ -12,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window_delegate.h"
 #include "ui/gfx/display.h"
 #include "ui/gfx/rect.h"
-#include "ui/gfx/screen.h"
 
 namespace ash {
 namespace wm {
@@ -23,19 +23,17 @@ DefaultState::~DefaultState() {}
 void DefaultState::OnWMEvent(WindowState* window_state,
                              WMEvent event) {
   aura::Window* window = window_state->window();
-  gfx::Rect work_area = Shell::GetScreen()->GetDisplayNearestWindow(
-      window).work_area();
 
   switch (event) {
     case TOGGLE_MAXIMIZE_CAPTION:
-      if (window_state->HasRestoreBounds()) {
-        if (window_state->GetShowState() == ui::SHOW_STATE_NORMAL) {
-          window_state->SetAndClearRestoreBounds();
-        } else {
-          window_state->Restore();
-        }
-      } else if (window_state->CanMaximize()) {
-        window_state->Maximize();
+      if (window_state->IsFullscreen()) {
+        window_state->ToggleFullscreen();
+      } else if (window_state->IsMaximized()) {
+        window_state->Restore();
+      } else if (window_state->IsNormalShowType() ||
+                 window_state->IsSnapped()) {
+        if (window_state->CanMaximize())
+          window_state->Maximize();
       }
       break;
 
@@ -48,40 +46,69 @@ void DefaultState::OnWMEvent(WindowState* window_state,
         window_state->Maximize();
       break;
 
-    case TOGGLE_VERTICAL_MAXIMIZE:
-      if (window->delegate()->GetMaximumSize().height() != 0)
+    case TOGGLE_VERTICAL_MAXIMIZE: {
+      gfx::Rect work_area =
+          ScreenUtil::GetDisplayWorkAreaBoundsInParent(window);
+
+      // Maximize vertically if:
+      // - The window does not have a max height defined.
+      // - The window has the normal show type. Snapped windows are excluded
+      //   because they are already maximized vertically and reverting to the
+      //   restored bounds looks weird.
+      if (window->delegate()->GetMaximumSize().height() != 0 ||
+          !window_state->IsNormalShowType()) {
         return;
+      }
       if (window_state->HasRestoreBounds() &&
           (window->bounds().height() == work_area.height() &&
            window->bounds().y() == work_area.y())) {
         window_state->SetAndClearRestoreBounds();
       } else {
         window_state->SaveCurrentBoundsForRestore();
-        gfx::Point origin = window->bounds().origin();
-        wm::ConvertPointToScreen(window->parent(), &origin);
-        window_state->SetBoundsInScreen(gfx::Rect(origin.x(),
-                                                  work_area.y(),
-                                                  window->bounds().width(),
-                                                  work_area.height()));
+        window->SetBounds(gfx::Rect(window->bounds().x(),
+                                    work_area.y(),
+                                    window->bounds().width(),
+                                    work_area.height()));
       }
       break;
-
-    case TOGGLE_HORIZONTAL_MAXIMIZE:
+    }
+    case TOGGLE_HORIZONTAL_MAXIMIZE: {
+      // Maximize horizontally if:
+      // - The window does not have a max width defined.
+      // - The window is snapped or has the normal show type.
       if (window->delegate()->GetMaximumSize().width() != 0)
         return;
-      if (window_state->HasRestoreBounds() &&
+      if (!window_state->IsNormalShowType() && !window_state->IsSnapped())
+        return;
+
+      gfx::Rect work_area =
+          ScreenUtil::GetDisplayWorkAreaBoundsInParent(window);
+
+      if (window_state->IsNormalShowType() &&
+          window_state->HasRestoreBounds() &&
           (window->bounds().width() == work_area.width() &&
            window->bounds().x() == work_area.x())) {
         window_state->SetAndClearRestoreBounds();
       } else {
-        window_state->SaveCurrentBoundsForRestore();
-        gfx::Point origin = window->bounds().origin();
-        wm::ConvertPointToScreen(window->parent(), &origin);
-        window_state->SetBoundsInScreen(gfx::Rect(work_area.x(),
-                                                  origin.y(),
-                                                  work_area.width(),
-                                                  window->bounds().height()));
+        gfx::Rect new_bounds(work_area.x(),
+                             window->bounds().y(),
+                             work_area.width(),
+                             window->bounds().height());
+
+        gfx::Rect restore_bounds = window->bounds();
+        if (window_state->IsSnapped()) {
+          window_state->SetRestoreBoundsInParent(new_bounds);
+          window_state->Restore();
+
+          // The restore logic prevents a window from being restored to bounds
+          // which match the workspace bounds exactly so it is necessary to set
+          // the bounds again below.
+        }
+
+        window_state->SetRestoreBoundsInParent(restore_bounds);
+        window->SetBounds(new_bounds);
       }
+    }
   }
 };
 
