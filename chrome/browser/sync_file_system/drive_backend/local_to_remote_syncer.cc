347b58842f28af82fbe4b48fb556c14c71a73f0f
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-10 05:04:41 +0000

    [SyncFS] Force activate folder tracker on folder creation conflict case
    If a folder activation failed due to existing item, the folder is eventually deleted.
    This can be happen in some edge case.
    
    This CL should fix it.
    
    BUG=331988
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/148483009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250049 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_backend/local_to_remote_syncer.cc
+++ chrome/browser/sync_file_system/drive_backend/local_to_remote_syncer.cc
@@ -630,7 +630,7 @@ void LocalToRemoteSyncer::DidCreateRemoteFolder(
   }
 
   MetadataDatabase::ActivationStatus activation_status =
-      metadata_database()->TryNoSideEffectActivation(
+      metadata_database()->TryActivateTracker(
           remote_parent_folder_tracker_->tracker_id(),
           file_id, callback);
   switch (activation_status) {
@@ -646,9 +646,6 @@ void LocalToRemoteSyncer::DidCreateRemoteFolder(
           base::Bind(&LocalToRemoteSyncer::DidDetachResourceForCreationConflict,
                      weak_ptr_factory_.GetWeakPtr(),
                      callback));
-      return;
-    case MetadataDatabase::ACTIVATION_FAILED_SAME_PATH_TRACKER:
-      callback.Run(SYNC_STATUS_FILE_BUSY);
       return;
   }
 
