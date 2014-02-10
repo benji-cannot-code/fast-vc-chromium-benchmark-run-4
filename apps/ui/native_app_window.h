bc527ccee858bc2149fa319b23354bfebeb8bfc9
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-10 13:25:03 +0000

    Add ability for running packaged app to badge their taskbar icon.
    This is only implemented on Windows with this patch.
    
    BUG=311878
    
    Review URL: https://codereview.chromium.org/140883018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250086 0039d316-1c4b-4281-b951-d872f2087c98

--- apps/ui/native_app_window.h
+++ apps/ui/native_app_window.h
@@ -35,6 +35,9 @@ class NativeAppWindow : public ui::BaseWindow,
   // Called when the title of the window changes.
   virtual void UpdateWindowTitle() = 0;
 
+  // Called to update the badge icon.
+  virtual void UpdateBadgeIcon() = 0;
+
   // Called when the draggable regions are changed.
   virtual void UpdateDraggableRegions(
       const std::vector<extensions::DraggableRegion>& regions) = 0;
