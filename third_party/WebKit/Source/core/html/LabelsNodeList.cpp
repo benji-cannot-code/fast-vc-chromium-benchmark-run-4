81ce3d378d0bbbaf4a1af20be42154ba95cd1f51
ch.dumez@samsung.com <ch.dumez@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-10 23:01:47 +0000

    Use more consistent naming for caching methods in NodeListsNodeData class
    All the caching methods are now called addCache() / removeCache() / cached()
    and overloading is used. The naming used to be a bit confusing, especially
    for the addCacheWithAtomicName() method which was not taking an AtomicString
    in argument.
    
    R=eseidel, adamk
    
    Review URL: https://codereview.chromium.org/138343007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166869 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/LabelsNodeList.cpp
+++ third_party/WebKit/Source/core/html/LabelsNodeList.cpp
@@ -41,7 +41,7 @@ LabelsNodeList::LabelsNodeList(ContainerNode* ownerNode)
 
 LabelsNodeList::~LabelsNodeList()
 {
-    ownerNode()->nodeLists()->removeCacheWithAtomicName(this, LabelsNodeListType, starAtom);
+    ownerNode()->nodeLists()->removeCache(this, LabelsNodeListType);
 }
 
 bool LabelsNodeList::nodeMatches(const Element& testNode) const
