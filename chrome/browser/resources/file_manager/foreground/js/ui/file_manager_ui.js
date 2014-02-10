44687931df24526b7ad3a5cb0533c4d2d38e97ee
kinaba@chromium.org <kinaba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-10 07:45:06 +0000

    Fix spell miss in Files.app.
    BUG=none
    TEST=profile image appears in the top-left corner correctly.
    R=hirono@chromium.org
    
    Review URL: https://codereview.chromium.org/148183018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250061 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/foreground/js/ui/file_manager_ui.js
+++ chrome/browser/resources/file_manager/foreground/js/ui/file_manager_ui.js
@@ -102,7 +102,7 @@ var FileManagerUI = function(element, dialogType) {
   Object.seal(this);
 
   // Initialize the header.
-  this.updateProfileBatch();
+  this.updateProfileBadge();
   this.element_.querySelector('#app-name').innerText =
       chrome.runtime.getManifest().name;
 
@@ -192,9 +192,9 @@ FileManagerUI.prototype.initAdditionalUI = function() {
 };
 
 /**
- * Updates visibility and image of the profile batch.
+ * Updates visibility and image of the profile badge.
  */
-FileManagerUI.prototype.updateProfileBatch = function() {
+FileManagerUI.prototype.updateProfileBadge = function() {
   if (this.dialogType_ !== DialogType.FULL_PAGE)
     return;
 
@@ -210,10 +210,10 @@ FileManagerUI.prototype.updateProfileBatch = function() {
         }
       }
     }
-    var profileBatch = this.element_.querySelector('#profile-batch');
+    var profileBadge = this.element_.querySelector('#profile-badge');
     if (imageUri)
-      profileBatch.setAttribute('src', imageUri);
+      profileBadge.setAttribute('src', imageUri);
     else
-      profileBatch.removeAttribute('src');
+      profileBadge.removeAttribute('src');
   }.bind(this));
 };
