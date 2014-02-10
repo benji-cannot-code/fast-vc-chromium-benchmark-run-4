f48611971640d9f79a02f1f0325799b34541af45
tnagel@chromium.org <tnagel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-10 15:02:15 +0000

    Grammar fix in scoped_ptr.h comment
    BUG=none
    
    Review URL: https://codereview.chromium.org/158473002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250099 0039d316-1c4b-4281-b951-d872f2087c98

--- base/memory/scoped_ptr.h
+++ base/memory/scoped_ptr.h
@@ -3,10 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Scopers help you manage ownership of a pointer, helping you easily manage the
-// a pointer within a scope, and automatically destroying the pointer at the
-// end of a scope.  There are two main classes you will use, which correspond
-// to the operators new/delete and new[]/delete[].
+// Scopers help you manage ownership of a pointer, helping you easily manage a
+// pointer within a scope, and automatically destroying the pointer at the end
+// of a scope.  There are two main classes you will use, which correspond to the
+// operators new/delete and new[]/delete[].
 //
 // Example usage (scoped_ptr<T>):
 //   {
