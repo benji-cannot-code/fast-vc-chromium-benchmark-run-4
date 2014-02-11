6b069c74e285789297578ded46493a5d37e0ee30
alph@chromium.org <alph@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-11 11:28:49 +0000

    DevTools: Move class filter to the heap snapshot toolbar
    R=loislo@chromium.org
    
    Review URL: https://codereview.chromium.org/143623005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166913 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/StatusBarButton.js
+++ third_party/WebKit/Source/devtools/front_end/StatusBarButton.js
@@ -32,12 +32,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /**
  * @constructor
  * @extends {WebInspector.Object}
- * @param {!Element} element
+ * @param {string} elementType
  */
-WebInspector.StatusBarItem = function(element)
+WebInspector.StatusBarItem = function(elementType)
 {
-    this.element = element;
+    this.element = document.createElement(elementType);
     this._enabled = true;
+    this._visible = true;
 }
 
 WebInspector.StatusBarItem.prototype = {
@@ -60,6 +61,19 @@ WebInspector.StatusBarItem.prototype = {
         this.element.disabled = !this._enabled;
     },
 
+    get visible()
+    {
+        return this._visible;
+    },
+
+    set visible(x)
+    {
+        if (this._visible === x)
+            return;
+        this.element.enableStyleClass("hidden", !x);
+        this._visible = x;
+    },
+
     __proto__: WebInspector.Object.prototype
 }
 
@@ -71,7 +85,7 @@ WebInspector.StatusBarItem.prototype = {
  */
 WebInspector.StatusBarText = function(text, className)
 {
-    WebInspector.StatusBarItem.call(this, document.createElement("span"));
+    WebInspector.StatusBarItem.call(this, "span");
     this.element.className = "status-bar-item status-bar-text";
     if (className)
         this.element.classList.add(className);
@@ -90,6 +104,39 @@ WebInspector.StatusBarText.prototype = {
     __proto__: WebInspector.StatusBarItem.prototype
 }
 
+/**
+ * @constructor
+ * @extends {WebInspector.StatusBarItem}
+ * @param {string=} placeholder
+ * @param {number=} width
+ */
+WebInspector.StatusBarInput = function(placeholder, width)
+{
+    WebInspector.StatusBarItem.call(this, "input");
+    this.element.className = "status-bar-item";
+    this.element.addEventListener("input", this._onChangeCallback.bind(this), false);
+    if (width)
+        this.element.style.width = width + "px";
+    if (placeholder)
+        this.element.setAttribute("placeholder", placeholder);
+}
+
+WebInspector.StatusBarInput.prototype = {
+    /**
+     * @param {?function(string)} handler
+     */
+    setOnChangeHandler: function(handler)
+    {
+        this._onChangeHandler = handler;
+    },
+
+    _onChangeCallback: function()
+    {
+        this._onChangeHandler && this._onChangeHandler(this.element.value);
+    },
+
+    __proto__: WebInspector.StatusBarItem.prototype
+}
 
 /**
  * @constructor
@@ -100,7 +147,7 @@ WebInspector.StatusBarText.prototype = {
  */
 WebInspector.StatusBarButton = function(title, className, states)
 {
-    WebInspector.StatusBarItem.call(this, document.createElement("button"));
+    WebInspector.StatusBarItem.call(this, "button");
     this.element.className = className + " status-bar-item";
     this.element.addEventListener("click", this._clicked.bind(this), false);
 
@@ -123,7 +170,6 @@ WebInspector.StatusBarButton = function(title, className, states)
 
     this.title = title;
     this.className = className;
-    this._visible = true;
 }
 
 WebInspector.StatusBarButton.prototype = {
@@ -201,20 +247,6 @@ WebInspector.StatusBarButton.prototype = {
         if (this.states !== 2)
             throw("Only used toggled when there are 2 states, otherwise, use state");
         this.state = x;
-    },
-
-    get visible()
-    {
-        return this._visible;
-    },
-
-    set visible(x)
-    {
-        if (this._visible === x)
-            return;
-
-        this.element.enableStyleClass("hidden", !x);
-        this._visible = x;
     },
 
     makeLongClickEnabled: function()
@@ -399,7 +431,7 @@ WebInspector.StatusBarButton.prototype = {
  */
 WebInspector.StatusBarComboBox = function(changeHandler, className)
 {
-    WebInspector.StatusBarItem.call(this, document.createElement("span"));
+    WebInspector.StatusBarItem.call(this, "span");
     this.element.className = "status-bar-select-container";
 
     this._selectElement = this.element.createChild("select", "status-bar-item");
@@ -517,7 +549,7 @@ WebInspector.StatusBarComboBox.prototype = {
  */
 WebInspector.StatusBarCheckbox = function(title)
 {
-    WebInspector.StatusBarItem.call(this, document.createElement("label"));
+    WebInspector.StatusBarItem.call(this, "label");
     this.element.classList.add("status-bar-item", "checkbox");
     this._checkbox = this.element.createChild("input");
     this._checkbox.type = "checkbox";
