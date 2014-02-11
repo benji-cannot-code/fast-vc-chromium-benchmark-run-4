f01b1970c963cf10ddef953d088a2a0f2e73cf67
jbudorick@chromium.org <jbudorick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-11 23:21:15 +0000

    [Android] Lint pylib/base.
    BUG=168518
    
    Review URL: https://codereview.chromium.org/156603003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250545 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/pylib/base/base_test_result_unittest.py
+++ build/android/pylib/base/base_test_result_unittest.py
@@ -7,9 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import unittest
 
-from base_test_result import BaseTestResult
-from base_test_result import TestRunResults
-from base_test_result import ResultType
+from pylib.base.base_test_result import BaseTestResult
+from pylib.base.base_test_result import TestRunResults
+from pylib.base.base_test_result import ResultType
 
 
 class TestTestRunResults(unittest.TestCase):
