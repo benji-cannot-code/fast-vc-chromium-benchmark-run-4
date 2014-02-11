06632efbd0e936a24dab1be633129d49ddcce7a1
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-11 18:20:12 +0000

    [Telemetry] Create ForwarderFactory earlier on CrOS.
    BUG=
    TBR=tonyg@chromium.org
    NOTRY=True
    
    Review URL: https://codereview.chromium.org/144283006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250448 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/backends/chrome/cros_browser_backend.py
+++ tools/telemetry/telemetry/core/backends/chrome/cros_browser_backend.py
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import logging
 import os
 
+from telemetry import decorators
+
 from telemetry.core import exceptions
 from telemetry.core import forwarders
 from telemetry.core import util
@@ -214,7 +216,6 @@ class CrOSBrowserBackend(chrome_browser_backend.ChromeBrowserBackend):
             'array:string:"%s"' % ','.join(startup_args)]
     self._cri.RunCmdOnDevice(args)
 
-    self._forwarder_factory = cros_forwarder.CrOsForwarderFactory(self._cri)
     self._forwarder = self.forwarder_factory.Create(
         forwarders.PortPairs(
             http=forwarders.PortPair(util.GetUnreservedAvailableLocalPort(),
@@ -273,8 +274,9 @@ class CrOSBrowserBackend(chrome_browser_backend.ChromeBrowserBackend):
     self._cri = None
 
   @property
+  @decorators.Cache
   def forwarder_factory(self):
-    return self._forwarder_factory
+    return cros_forwarder.CrOsForwarderFactory(self._cri)
 
   def IsBrowserRunning(self):
     return bool(self.pid)
