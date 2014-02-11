1ac5238d81d33db4303ec7db1b7c623f32fe8231
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-11 02:22:41 +0000

    Replace '.' with '_' in service name.
    Review URL: https://codereview.chromium.org/159343002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250288 0039d316-1c4b-4281-b951-d872f2087c98

--- cloud_print/gcp20/prototype/printer.cc
+++ cloud_print/gcp20/prototype/printer.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "cloud_print/gcp20/prototype/printer.h"
 
+#include <algorithm>
 #include <limits.h>
 #include <stdio.h>
 #include <string>
@@ -845,6 +846,8 @@ bool Printer::StartDnsServer() {
   std::string service_name_prefix =
       command_line_reader::ReadServiceNamePrefix(net::IPAddressToString(ip) +
                                                  kServiceNamePrefixDefault);
+  std::replace(service_name_prefix .begin(), service_name_prefix .end(),
+               '.', '_');
 
   std::string service_domain_name =
       command_line_reader::ReadDomainName(
