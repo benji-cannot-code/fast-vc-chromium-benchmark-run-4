75f1140caa151844658b4ba72477287db557e126
tengs@chromium.org <tengs@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-11 20:06:34 +0000

    Revert of [Telemetry] Create ForwarderFactory earlier on CrOS. (https://codereview.chromium.org/144283006/)
    Reason for revert:
    See https://codereview.chromium.org/134863005.
    
    Original issue's description:
    > [Telemetry] Create ForwarderFactory earlier on CrOS.
    > 
    > BUG=
    > TBR=tonyg@chromium.org
    > NOTRY=True
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=250448
    
    TBR=tonyg@chromium.org,tonyg@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=
    
    Review URL: https://codereview.chromium.org/143293004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250483 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/backends/chrome/cros_browser_backend.py
+++ tools/telemetry/telemetry/core/backends/chrome/cros_browser_backend.py
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import logging
 import os
 
-from telemetry import decorators
-
 from telemetry.core import exceptions
 from telemetry.core import forwarders
 from telemetry.core import util
@@ -216,6 +214,7 @@ class CrOSBrowserBackend(chrome_browser_backend.ChromeBrowserBackend):
             'array:string:"%s"' % ','.join(startup_args)]
     self._cri.RunCmdOnDevice(args)
 
+    self._forwarder_factory = cros_forwarder.CrOsForwarderFactory(self._cri)
     self._forwarder = self.forwarder_factory.Create(
         forwarders.PortPairs(
             http=forwarders.PortPair(util.GetUnreservedAvailableLocalPort(),
@@ -274,9 +273,8 @@ class CrOSBrowserBackend(chrome_browser_backend.ChromeBrowserBackend):
     self._cri = None
 
   @property
-  @decorators.Cache
   def forwarder_factory(self):
-    return cros_forwarder.CrOsForwarderFactory(self._cri)
+    return self._forwarder_factory
 
   def IsBrowserRunning(self):
     return bool(self.pid)
