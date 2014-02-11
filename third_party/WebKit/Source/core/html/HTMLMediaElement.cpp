937ccebbfe9d95abf00c13221f0222f6fa7d19ea
philipj@opera.com <philipj@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-11 09:11:49 +0000

    Make MediaControls minimally virtual
    Only the functions which are overridden in MediaControlsAndroid need
    be virtual.
    
    updateStatusDisplay() didn't do anything, so it was removed.
    
    BUG=341813
    
    Review URL: https://codereview.chromium.org/141323005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166907 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLMediaElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLMediaElement.cpp
@@ -1505,9 +1505,6 @@ void HTMLMediaElement::setNetworkState(MediaPlayer::NetworkState state)
             changeNetworkStateFromLoadingToIdle();
         m_completelyLoaded = true;
     }
-
-    if (hasMediaControls())
-        mediaControls()->updateStatusDisplay();
 }
 
 void HTMLMediaElement::changeNetworkStateFromLoadingToIdle()
@@ -1631,10 +1628,8 @@ void HTMLMediaElement::setReadyState(MediaPlayer::ReadyState state)
 
     if (shouldUpdateDisplayState) {
         updateDisplayState();
-        if (hasMediaControls()) {
+        if (hasMediaControls())
             mediaControls()->refreshClosedCaptionsButtonVisibility();
-            mediaControls()->updateStatusDisplay();
-        }
     }
 
     updatePlayState();
