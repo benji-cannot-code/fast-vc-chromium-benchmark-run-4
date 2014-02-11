f179671fc58ea1898af2ac9dfff84728f0ad05cc
mikhal@chromium.org <mikhal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-11 21:09:24 +0000

    Cast: Refactoring Cast to Clang format
    This cl is pure style-targeted refactoring. 
    No functional changes were made. 
    
    BUG=339176
    
    Review URL: https://codereview.chromium.org/159413002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250503 0039d316-1c4b-4281-b951-d872f2087c98

--- media/cast/cast_sender_impl.h
+++ media/cast/cast_sender_impl.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/cast/video_sender/video_sender.h"
 
 namespace media {
-  class VideoFrame;
+class VideoFrame;
 }
 
 namespace media {
