a8a0ed97dfd2510dbcaa88a136f7eb4994e6c581
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-11 00:58:25 +0000

    [Telemetry] Allow --netsim to work without running WPR with sudo
    This is part of the effort to make Telemetry more bot and dev
    friendly by eliminating dependencies on sudo.
    
    With this change, we start the host WPR sever on ephemeral
    ports and use iptables on the device to forward the true
    ports to the WPR ports. It eliminates the need for all of
    WPR to run as sudo.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/134863005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250254 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/backends/adb_commands.py
+++ tools/telemetry/telemetry/core/backends/adb_commands.py
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright 2013 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+
 """Brings in Chrome Android's android_commands module, which itself is a
 thin(ish) wrapper around adb."""
 
@@ -22,7 +23,6 @@ util.AddDirToPythonPath(util.GetChromiumSrcDir(), 'build', 'android')
 try:
   from pylib import android_commands  # pylint: disable=F0401
   from pylib import constants  # pylint: disable=F0401
-  from pylib import forwarder  # pylint: disable=F0401
   from pylib import ports  # pylint: disable=F0401
   from pylib.utils import apk_helper  # pylint: disable=F0401
   from pylib.utils import test_environment  # pylint: disable=F0401
@@ -242,23 +242,3 @@ http://www.chromium.org/developers/telemetry/upload_to_cloud_storage
       shutil.copyfile(prebuilt_path, dest)
       os.chmod(dest, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
   return True
-
-
-class Forwarder(object):
-  def __init__(self, adb, *port_pairs):
-    self._adb = adb.Adb()
-    self._host_port = port_pairs[0].local_port
-
-    new_port_pairs = [(port_pair.local_port, port_pair.remote_port)
-                      for port_pair in port_pairs]
-
-    self._port_pairs = new_port_pairs
-    forwarder.Forwarder.Map(new_port_pairs, self._adb)
-
-  @property
-  def url(self):
-    return 'http://127.0.0.1:%i' % self._host_port
-
-  def Close(self):
-    for (device_port, _) in self._port_pairs:
-      forwarder.Forwarder.UnmapDevicePort(device_port, self._adb)
