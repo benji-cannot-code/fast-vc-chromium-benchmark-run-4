c8e3d4aa562fe1629821e56b8c12d5881f579274
falken@chromium.org <falken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-11 06:26:55 +0000

    <dialog>: Non-modals are no longer magically positioned
    As per spec change: http://html5.org/r/8457
    Now <dialog> is centered and promoted to the top layer only in showModal().
    
    Existing tests are modified to meet the change. In
    particular I revamped non-anchored-dialog-positioning.html
    not just for the new change but also to be more readable.
    
    BUG=332133
    
    Review URL: https://codereview.chromium.org/130183005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166900 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLDialogElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLDialogElement.cpp
@@ -92,7 +92,7 @@ static void inertSubtreesChanged(Document& document)
 
 HTMLDialogElement::HTMLDialogElement(Document& document)
     : HTMLElement(dialogTag, document)
-    , m_centeringMode(Uninitialized)
+    , m_centeringMode(NotCentered)
     , m_centeredPosition(0)
     , m_returnValue("")
 {
@@ -132,10 +132,10 @@ void HTMLDialogElement::closeDialog(const String& returnValue)
 
 void HTMLDialogElement::forceLayoutForCentering()
 {
-    m_centeringMode = Uninitialized;
+    m_centeringMode = NeedsCentering;
     document().updateLayoutIgnorePendingStylesheets();
-    if (m_centeringMode == Uninitialized)
-        m_centeringMode = NotCentered;
+    if (m_centeringMode == NeedsCentering)
+        setNotCentered();
 }
 
 void HTMLDialogElement::show()
@@ -143,7 +143,6 @@ void HTMLDialogElement::show()
     if (fastHasAttribute(openAttr))
         return;
     setBooleanAttribute(openAttr, true);
-    forceLayoutForCentering();
 }
 
 void HTMLDialogElement::showModal(ExceptionState& exceptionState)
@@ -168,16 +167,22 @@ void HTMLDialogElement::showModal(ExceptionState& exceptionState)
     setFocusForModalDialog(this);
 }
 
+void HTMLDialogElement::removedFrom(ContainerNode* insertionPoint)
+{
+    HTMLElement::removedFrom(insertionPoint);
+    setNotCentered();
+    // FIXME: We should call inertSubtreesChanged() here.
+}
+
 void HTMLDialogElement::setCentered(LayoutUnit centeredPosition)
 {
-    ASSERT(m_centeringMode == Uninitialized);
+    ASSERT(m_centeringMode == NeedsCentering);
     m_centeredPosition = centeredPosition;
     m_centeringMode = Centered;
 }
 
 void HTMLDialogElement::setNotCentered()
 {
-    ASSERT(m_centeringMode == Uninitialized);
     m_centeringMode = NotCentered;
 }
 
