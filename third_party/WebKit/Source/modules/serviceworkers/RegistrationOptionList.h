15c70a3bccc4a53bc9e271c746805b5a0d13c7ae
kinuko@chromium.org <kinuko@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-12 07:57:41 +0000

    Implement navigator.serviceWorker interface
    Replacing old navigator.registerServiceWorker
    
    BUG=285976
    TEST=http/tests/serviceworker/registration.html
    
    Review URL: https://codereview.chromium.org/157523005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166980 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/Source/modules/serviceworkers/RegistrationOptionList.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef RegistrationOptionList_h
+#define RegistrationOptionList_h
+
+#include "bindings/v8/Dictionary.h"
+
+namespace WebCore {
+
+struct RegistrationOptionList  {
+    explicit RegistrationOptionList(const Dictionary& options)
+        : scope("/*")
+    {
+        options.get("scope", scope);
+    }
+
+    String scope;
+};
+
+} // namespace WebCore
+
+#endif // RegistrationOptionList_h
