77f0fe32e66fcc99711716cd00c07a446a11aa4e
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-12 01:46:25 +0000

    Add field trial that records if multiprofiles is allowed for a ChromeOS session.
    (Note that this doesn't mean more than one profile was actually used.)
    
    BUG=218627
    
    Review URL: https://codereview.chromium.org/159423002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250600 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_browser_field_trials_desktop.cc
+++ chrome/browser/chrome_browser_field_trials_desktop.cc
@@ -29,6 +29,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/spdy/spdy_session.h"
 #include "ui/base/layout.h"
 
+#if defined(OS_CHROMEOS)
+#include "chrome/browser/chromeos/login/user_manager.h"
+#endif
+
 namespace chrome {
 
 namespace {
@@ -112,6 +116,16 @@ void SetupPreReadFieldTrial() {
   trial->group();
 }
 
+#if defined(OS_CHROMEOS)
+void SetupChromeOSMultiProfilesAllowedTrial() {
+  const char kTrialName[] = "ChromeOSMultiProfilesAllowed";
+  if (chromeos::UserManager::IsMultipleProfilesAllowed())
+    base::FieldTrialList::CreateFieldTrial(kTrialName, "allowed")->group();
+  else
+    base::FieldTrialList::CreateFieldTrial(kTrialName, "not_allowed")->group();
+}
+#endif  // defined(OS_CHROMEOS)
+
 }  // namespace
 
 void SetupDesktopFieldTrials(const CommandLine& parsed_command_line,
@@ -124,6 +138,9 @@ void SetupDesktopFieldTrials(const CommandLine& parsed_command_line,
   DisableShowProfileSwitcherTrialIfNecessary();
   SetupShowAppLauncherPromoFieldTrial(local_state);
   SetupPreReadFieldTrial();
+#if defined(OS_CHROMEOS)
+  SetupChromeOSMultiProfilesAllowedTrial();
+#endif
 }
 
 }  // namespace chrome
