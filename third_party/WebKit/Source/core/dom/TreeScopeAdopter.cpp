4abdd65c28c9afd9635fb7e18d49e6a2d12617df
ch.dumez@samsung.com <ch.dumez@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-12 07:58:48 +0000

    Stop handling the case where oldDocument and newDocument are the same in adoptDocument()
    Stop handling the case where oldDocument and newDocument are the same in
    NodeListsNodeData::adoptDocument(). Replace the if check by an assertion as all
    the callers already make sure to call this method only if the oldDocument and
    the newDocument are different.
    
    R=eseidel, abarth
    
    Review URL: https://codereview.chromium.org/159913004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166981 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/TreeScopeAdopter.cpp
+++ third_party/WebKit/Source/core/dom/TreeScopeAdopter.cpp
@@ -83,6 +83,7 @@ void TreeScopeAdopter::moveTreeToNewScope(Node& root) const
 
 void TreeScopeAdopter::moveTreeToNewDocument(Node& root, Document& oldDocument, Document& newDocument) const
 {
+    ASSERT(oldDocument != newDocument);
     for (Node* node = &root; node; node = NodeTraversal::next(*node, &root)) {
         moveNodeToNewDocument(*node, oldDocument, newDocument);
         for (ShadowRoot* shadow = node->youngestShadowRoot(); shadow; shadow = shadow->olderShadowRoot())
@@ -113,7 +114,7 @@ inline void TreeScopeAdopter::updateTreeScope(Node& node) const
 
 inline void TreeScopeAdopter::moveNodeToNewDocument(Node& node, Document& oldDocument, Document& newDocument) const
 {
-    ASSERT(!node.inDocument() || oldDocument != newDocument);
+    ASSERT(oldDocument != newDocument);
 
     if (node.hasRareData()) {
         NodeRareData* rareData = node.rareData();
