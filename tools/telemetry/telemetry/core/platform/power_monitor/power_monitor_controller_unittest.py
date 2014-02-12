9ffe0a3626def78b90bca56595edca41346cf67b
qsr@chromium.org <qsr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-12 14:03:32 +0000

    [Telemetry] Have android choose the best available source of power monitoring.
    This CL adds the monsoon power monitor for telemetry power measurement.
    It also updates the android implementation to choose the best power
    monitoring source available.
    
    R=jeremy@chromium.org,tonyg@chromium.org
    
    Review URL: https://codereview.chromium.org/159803002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250709 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/telemetry/telemetry/core/platform/power_monitor/power_monitor_controller_unittest.py
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import unittest
+
+from telemetry.core.platform.power_monitor import power_monitor_controller
+import telemetry.core.platform.power_monitor as power_monitor
+
+
+class PowerMonitorControllerTest(unittest.TestCase):
+  def testComposition(self):
+
+    class P1(power_monitor.PowerMonitor):
+      def StartMonitoringPowerAsync(self):
+        raise NotImplementedError()
+      def StopMonitoringPowerAsync(self):
+        raise NotImplementedError()
+
+    class P2(power_monitor.PowerMonitor):
+      def __init__(self, value):
+        super(P2, self).__init__()
+        self._value = value
+      def CanMonitorPowerAsync(self):
+        return True
+      def StartMonitoringPowerAsync(self):
+        pass
+      def StopMonitoringPowerAsync(self):
+        return self._value
+
+    controller = power_monitor_controller.PowerMonitorController(
+        [P1(), P2(1), P2(2)])
+    self.assertEqual(controller.CanMonitorPowerAsync(), True)
+    controller.StartMonitoringPowerAsync()
+    self.assertEqual(controller.StopMonitoringPowerAsync(), 1)
