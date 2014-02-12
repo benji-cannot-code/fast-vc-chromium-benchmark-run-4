f9d0a4fecad71fbba3fed24a94226258ec73b6a6
stevet@chromium.org <stevet@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-12 17:26:56 +0000

    Revert 250730 "Introduce ADTSStreamParser. Factorize MP3StreamPa..."
    Causing compile failure on mac: http://build.chromium.org/p/chromium.chrome/builders/Google%20Chrome%20Mac/builds/16420/steps/compile/logs/stdio
    
    > Introduce ADTSStreamParser. Factorize MP3StreamParser and tests.
    > 
    > Factorizes MP3StreamParser into MPEGAudioStreamParserBase for reuse
    > with a new ADTSStreamParser.  In the process, also factorizes the
    > MP3StreamParserTest into a new test helper StreamParserTest for
    > reuse with the ADTSStreamParserTest.
    > 
    > Introduces an adts_constants.{cc,h} to remove duplicate constants
    > across the AAC and ES ADTS parsers.  Fixes a channel layout bug in
    > the AAC parser.
    > 
    > sfx.adts test file will be landed manually in a separate change.
    > 
    > BUG=341648,340426
    > TEST=new unittests pass!
    > 
    > Review URL: https://codereview.chromium.org/120503006
    
    TBR=dalecurtis@chromium.org
    
    Review URL: https://codereview.chromium.org/160593002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250736 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1015,7 +1015,6 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kEnableAcceleratedOverflowScroll,
     switches::kEnableAcceleratedScrollableFrames,
     switches::kEnableAccessibilityLogging,
-    switches::kEnableADTSStreamParser,
     switches::kEnableBeginFrameScheduling,
     switches::kEnableBrowserPluginForAllViewTypes,
     switches::kEnableCompositedScrollingForFrames,
