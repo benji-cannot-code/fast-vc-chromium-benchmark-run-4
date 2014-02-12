884c423876687c5fcc7c5319a6e717fbabb0a5dc
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-12 17:31:30 +0000

    Implement events for cast.streaming.rtpStream API
    This change implements:
    cast.streaming.rtpStream.onStarted
    cast.streaming.rtpStream.onStopped
    cast.streaming.rtpStream.onError
    
    Above event handlers are now called when a stream starts, stops and
    encounters an error.
    
    CastStreamingApiTest is updated to connect these events for testing.
    
    BUG=301920
    
    Review URL: https://codereview.chromium.org/143243003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250740 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/extensions/chrome_v8_context.cc
+++ chrome/renderer/extensions/chrome_v8_context.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/renderer/v8_value_converter.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/extension_api.h"
+#include "extensions/common/extension_urls.h"
 #include "third_party/WebKit/public/web/WebFrame.h"
 #include "third_party/WebKit/public/web/WebScopedMicrotaskSuppression.h"
 #include "third_party/WebKit/public/web/WebView.h"
@@ -118,6 +119,17 @@ Feature::Availability ChromeV8Context::GetAvailability(
                                                         extension,
                                                         context_type_,
                                                         GetURL());
+}
+
+void ChromeV8Context::DispatchEvent(const char* event_name,
+                                    v8::Handle<v8::Array> args) const {
+  v8::HandleScope handle_scope(isolate());
+  v8::Context::Scope context_scope(v8_context());
+
+  v8::Handle<v8::Value> argv[] = {
+      v8::String::NewFromUtf8(isolate(), event_name), args};
+  module_system_->CallModuleMethod(
+      kEventBindings, "dispatchEvent", arraysize(argv), argv);
 }
 
 void ChromeV8Context::DispatchOnUnloadEvent() {
