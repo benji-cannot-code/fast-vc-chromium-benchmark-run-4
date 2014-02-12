7ecf86d69cf74a210e03aaa9db28f8f9dfdecf72
rdevlin.cronin@chromium.org <rdevlin.cronin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-12 00:53:31 +0000

    Fix a ComponentLoader error at chrome startup.
    ComponentLoader was trying to load the Virtual Keyboard even when the resources
    were not loaded.  Ensure this doesn't happen, so we don't throw an error.
    
    BUG=342055
    
    Review URL: https://codereview.chromium.org/136933006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250581 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/component_loader.cc
+++ chrome/browser/extensions/component_loader.cc
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
 
-#if defined(USE_AURA)
+#if defined(OS_CHROMEOS)
 #include "grit/keyboard_resources.h"
 #include "ui/keyboard/keyboard_util.h"
 #endif
@@ -133,9 +133,6 @@ void ComponentLoader::LoadAll() {
 
 base::DictionaryValue* ComponentLoader::ParseManifest(
     const std::string& manifest_contents) const {
-  if (manifest_contents.empty())
-    return NULL;
-
   JSONStringValueSerializer serializer(manifest_contents);
   scoped_ptr<base::Value> manifest(serializer.Deserialize(NULL, NULL));
 
@@ -383,7 +380,7 @@ void ComponentLoader::AddChromeApp() {
 }
 
 void ComponentLoader::AddKeyboardApp() {
-#if defined(USE_AURA)
+#if defined(OS_CHROMEOS)
   Add(IDR_KEYBOARD_MANIFEST, base::FilePath(FILE_PATH_LITERAL("keyboard")));
 #endif
 }
