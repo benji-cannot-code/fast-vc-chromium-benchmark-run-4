927475a31bfd851b6ca55ec53fdcec86f6e31d87
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-12 07:36:16 +0000

    Revert of Update of change event for datetime input type (https://codereview.chromium.org/128153002/)
    Reason for revert:
    INPUT element dispatches extra events.
    
    
    Original issue's description:
    > Update of change event for datetime input type
    > 
    > BaseMultipleFieldDateAndTime includes changes for sending change event when popup is closed, keyboard event and mouse event.
    > 
    > BaseChoosedOnlyDateAndTime includes changes for sending change event when popup is closed. Special case for input type datetime-local.
    > 
    > R=tkent
    > BUG=155747
    > TEST=Fixes currently broken test in bug331189
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=165455
    
    TBR=keishi@chromium.org,habib.virji@samsung.com
    NOTREECHECKS=true
    NOTRY=true
    BUG=155747
    
    Review URL: https://codereview.chromium.org/159893007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166970 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/forms/BaseMultipleFieldsDateAndTimeInputType.cpp
+++ third_party/WebKit/Source/core/html/forms/BaseMultipleFieldsDateAndTimeInputType.cpp
@@ -188,6 +188,7 @@ void BaseMultipleFieldsDateAndTimeInputType::editControlValueChanged()
         input->setValueInternal(newValue, DispatchNoEvent);
         input->setNeedsStyleRecalc(SubtreeStyleChange);
         input->dispatchFormControlInputEvent();
+        input->dispatchFormControlChangeEvent();
     }
     input->notifyFormStateChanged();
     input->updateClearButtonVisibility();
@@ -264,7 +265,6 @@ void BaseMultipleFieldsDateAndTimeInputType::pickerIndicatorChooseValue(const St
     unsigned end;
     if (date.parseDate(value, 0, end) && end == value.length())
         edit->setOnlyYearMonthDay(date);
-    element().dispatchFormControlChangeEvent();
 }
 
 void BaseMultipleFieldsDateAndTimeInputType::pickerIndicatorChooseValue(double value)
@@ -447,11 +447,6 @@ bool BaseMultipleFieldsDateAndTimeInputType::hasBadInput() const
 AtomicString BaseMultipleFieldsDateAndTimeInputType::localeIdentifier() const
 {
     return element().computeInheritedLanguage();
-}
-
-void BaseMultipleFieldsDateAndTimeInputType::editControlDidChangeValueByKeyboard()
-{
-    element().dispatchFormControlChangeEvent();
 }
 
 void BaseMultipleFieldsDateAndTimeInputType::minOrMaxAttributeChanged()
--- third_party/WebKit/Source/core/html/shadow/DateTimeEditElement.h
+++ third_party/WebKit/Source/core/html/shadow/DateTimeEditElement.h
@@ -60,7 +60,6 @@ public:
         virtual bool isEditControlOwnerDisabled() const = 0;
         virtual bool isEditControlOwnerReadOnly() const = 0;
         virtual AtomicString localeIdentifier() const = 0;
-        virtual void editControlDidChangeValueByKeyboard() = 0;
     };
 
     struct LayoutParameters {
@@ -146,7 +145,6 @@ private:
     virtual bool isFieldOwnerDisabled() const OVERRIDE;
     virtual bool isFieldOwnerReadOnly() const OVERRIDE;
     virtual AtomicString localeIdentifier() const OVERRIDE;
-    virtual void fieldDidChangeValueByKeyboard() OVERRIDE;
 
     Vector<DateTimeFieldElement*, maximumNumberOfFields> m_fields;
     EditControlOwner* m_editControlOwner;
