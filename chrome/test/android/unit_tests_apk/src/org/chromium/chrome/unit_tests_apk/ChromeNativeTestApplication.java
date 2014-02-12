6bd54b06b3893d8f253b492d5fa33d40bc114f7f
yfriedman@chromium.org <yfriedman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-12 04:05:36 +0000

    Implement client certificate support via SmartCard.
    Updates SSLClientCertificateRequest such that certificate can retrieved
    from Android store or external PKCS11 KeyStore
    
    NOTRY=true
    COLLABORATOR=ppi@chromium.org
    BUG=341500
    
    Review URL: https://codereview.chromium.org/147573005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250624 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/android/unit_tests_apk/src/org/chromium/chrome/unit_tests_apk/ChromeNativeTestApplication.java
+++ chrome/test/android/unit_tests_apk/src/org/chromium/chrome/unit_tests_apk/ChromeNativeTestApplication.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.unit_tests_apk;
 
 import org.chromium.chrome.browser.ChromiumApplication;
+import org.chromium.chrome.browser.PKCS11AuthenticationManager;
 
 /**
  * A stub implementation of the chrome application to be used in chrome unit_tests.
@@ -27,5 +28,10 @@ public class ChromeNativeTestApplication extends ChromiumApplication {
     @Override
     protected boolean areParentalControlsEnabled() {
         return false;
+    }
+
+    @Override
+    protected PKCS11AuthenticationManager getPKCS11AuthenticationManager() {
+        return null;
     }
 }
