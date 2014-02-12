da85d4abdf46b48a851429e974df0e27a83881ca
teravest@chromium.org <teravest@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-12 22:41:13 +0000

    Trusted plugin: Remove ScriptablePlugin.
    ScriptablePlugin no longer provides anything from the plugin element in the DOM
    since properties were moved in r237604.
    
    BUG=239656
    R=dmichael@chromium.org
    
    Review URL: https://codereview.chromium.org/160533002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250811 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/native_client/src/trusted/plugin/plugin.gypi
+++ ppapi/native_client/src/trusted/plugin/plugin.gypi
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'pnacl_options.cc',
       'pnacl_resources.cc',
       'pnacl_translate_thread.cc',
-      'scriptable_plugin.cc',
       'sel_ldr_launcher_chrome.cc',
       'service_runtime.cc',
       'srpc_client.cc',
--- ppapi/native_client/src/trusted/plugin/plugin.h
+++ ppapi/native_client/src/trusted/plugin/plugin.h
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "native_client/src/trusted/validator/nacl_file_info.h"
 
 #include "ppapi/c/private/ppb_nacl_private.h"
-#include "ppapi/cpp/private/instance_private.h"
+#include "ppapi/cpp/instance.h"
 #include "ppapi/cpp/private/uma_private.h"
 #include "ppapi/cpp/url_loader.h"
 #include "ppapi/cpp/var.h"
@@ -53,9 +53,8 @@ namespace plugin {
 
 class ErrorInfo;
 class Manifest;
-class ScriptablePlugin;
 
-class Plugin : public pp::InstancePrivate {
+class Plugin : public pp::Instance {
  public:
   // Factory method for creation.
   static Plugin* New(PP_Instance instance);
@@ -69,10 +68,6 @@ class Plugin : public pp::InstancePrivate {
 
   // Handles document load, when the plugin is a MIME type handler.
   virtual bool HandleDocumentLoad(const pp::URLLoader& url_loader);
-
-  // Returns a scriptable reference to this plugin element.
-  // Called by JavaScript document.getElementById(plugin_id).
-  virtual pp::Var GetInstanceObject();
 
   // ----- Plugin interface support.
 
@@ -263,11 +258,6 @@ class Plugin : public pp::InstancePrivate {
   // in this order, for the main nacl subprocess.
   void ShutDownSubprocesses();
 
-  ScriptablePlugin* scriptable_plugin() const { return scriptable_plugin_; }
-  void set_scriptable_plugin(ScriptablePlugin* scriptable_plugin) {
-    scriptable_plugin_ = scriptable_plugin;
-  }
-
   // Access the service runtime for the main NaCl subprocess.
   ServiceRuntime* main_service_runtime() const {
     return main_subprocess_.service_runtime();
@@ -391,8 +381,6 @@ class Plugin : public pp::InstancePrivate {
   void set_nacl_ready_state(ReadyState state);
 
   void SetExitStatusOnMainThread(int32_t pp_error, int exit_status);
-
-  ScriptablePlugin* scriptable_plugin_;
 
   int argc_;
   char** argn_;
