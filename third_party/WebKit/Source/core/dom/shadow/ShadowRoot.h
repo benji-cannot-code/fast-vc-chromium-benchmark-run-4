43e5e715efa3e5b613b920575c88ed386f5f68fb
tasak@google.com <tasak@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-12 23:22:40 +0000

    ^, ^^, and ::distributed declared in inert shadow tree should not be applicable.
    Tree-boundary crossing rules should consider whether their trees are active or not.
    If not active, the rules should not match any elements.
    
    BUG=341372
    TEST=fast/dom/shadow/style-with-cat.html,fast/dom/shadow/style-with-hat.html,fast/dom/shadow/content-pseudo-element-in-inert-shadow.html
    
    Review URL: https://codereview.chromium.org/156583003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167052 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/shadow/ShadowRoot.h
+++ third_party/WebKit/Source/core/dom/shadow/ShadowRoot.h
@@ -126,6 +126,7 @@ public:
     PassRefPtr<Node> cloneNode(ExceptionState& exceptionState) { return cloneNode(true, exceptionState); }
 
     StyleSheetList* styleSheets();
+    bool isActiveForStyling() const;
 
 private:
     ShadowRoot(Document&, ShadowRootType);
@@ -145,7 +146,6 @@ private:
 
     // FIXME: This shouldn't happen. https://bugs.webkit.org/show_bug.cgi?id=88834
     bool isOrphan() const { return !host(); }
-    bool isActive() const;
 
     ShadowRoot* m_prev;
     ShadowRoot* m_next;
