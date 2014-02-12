f2be38888e7fe0795d5edf7c9875d6ec327273a4
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-12 11:17:41 +0000

    Reland of allowing netsim to work without sudo.
    Revert "Revert of [Telemetry] Fix typo in https port number. (https://codereview.chromium.org/147303003/)"
    
    This reverts commit f48aabf5c95e0e956c1de77680b1ff158b6ad495.
    
    Revert "Revert of [Telemetry] Create ForwarderFactory earlier on CrOS. (https://codereview.chromium.org/144283006/)"
    
    This reverts commit edef1c25eb44529879f759b46719bfed1fdfe4ff.
    
    Revert "Revert of [Telemetry] Allow --netsim to work without running WPR with sudo (https://codereview.chromium.org/134863005/)"
    
    This reverts commit 34b0d296a39c131a6d21e86472ac3abc2a241768.
    
    BUG=
    TBR=tengs@chromium.org,craigdh@chromium.org
    
    Review URL: https://codereview.chromium.org/130903010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250684 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/backends/webdriver/webdriver_browser_backend.py
+++ tools/telemetry/telemetry/core/backends/webdriver/webdriver_browser_backend.py
@@ -3,9 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from telemetry.core import util
+from telemetry.core import forwarders
 from telemetry.core.backends import browser_backend
 from telemetry.core.backends.webdriver import webdriver_tab_list_backend
+
 
 class WebDriverBrowserBackend(browser_backend.BrowserBackend):
   """The webdriver-based backend for controlling a locally-executed browser
@@ -21,8 +22,10 @@ class WebDriverBrowserBackend(browser_backend.BrowserBackend):
 
     self._driver_creator = driver_creator
     self._driver = None
-    self.wpr_http_port_pair = util.PortPair(80, 80)
-    self.wpr_https_port_pair = util.PortPair(443, 443)
+    self.wpr_port_pairs = forwarders.PortPairs(
+        http=forwarders.PortPair(80, 80),
+        https=forwarders.PortPair(443, 443),
+        dns=forwarders.PortPair(53, 53))
 
   def Start(self):
     assert not self._driver
@@ -54,9 +57,6 @@ class WebDriverBrowserBackend(browser_backend.BrowserBackend):
     if self._driver:
       self._driver.quit()
       self._driver = None
-
-  def CreateForwarder(self, *port_pairs):
-    return browser_backend.DoNothingForwarder(*port_pairs)
 
   def IsBrowserRunning(self):
     # Assume the browser is running if not explicitly closed.
