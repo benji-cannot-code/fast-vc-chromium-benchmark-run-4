4382e4e85b3baa06abb256d557c307a2a643305b
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-12 22:52:41 +0000

    Fix ASSERTs on the ChromeOS login screen
    This CL suppresses a few more strict state machine asserts. These asserts are
    hit only on the Chrome OS login screen. For some reason, that environment is
    triggering code paths that don't seem to exist in other configurations. Ideally
    we'd figure out how to test this configuration at the Blink layer.
    
    R=ojan@chromium.org
    
    Review URL: https://codereview.chromium.org/161083003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167046 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderView.cpp
+++ third_party/WebKit/Source/core/rendering/RenderView.cpp
@@ -525,6 +525,9 @@ void RenderView::repaintRectangleInViewAndCompositedLayers(const LayoutRect& ur)
 
     repaintViewRectangle(ur);
 
+    // FIXME: We don't actually know how to hit these ASSERTS.
+    DisableCompositingQueryAsserts disabler;
+
     if (compositor()->inCompositingMode()) {
         IntRect repaintRect = pixelSnappedIntRect(ur);
         compositor()->repaintCompositedLayers(&repaintRect);
@@ -534,6 +537,9 @@ void RenderView::repaintRectangleInViewAndCompositedLayers(const LayoutRect& ur)
 void RenderView::repaintViewAndCompositedLayers()
 {
     repaint();
+
+    // The only way we know how to hit these ASSERTS below this point is via the Chromium OS login screen.
+    DisableCompositingQueryAsserts disabler;
 
     if (compositor()->inCompositingMode())
         compositor()->repaintCompositedLayers();
