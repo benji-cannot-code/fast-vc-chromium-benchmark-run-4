940e85d8caa354d2be31051dd01e88b782920a8c
noms@chromium.org <noms@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-12 23:07:10 +0000

    [Win] Unbreak the new avatar bubble size when using the embedded view.
    (the embedded view is wider than the fixed size of the bubble)
    
    BUG=334761
    
    Review URL: https://codereview.chromium.org/137193005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250825 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/profile_chooser_view.cc
+++ chrome/browser/ui/views/profile_chooser_view.cc
@@ -432,8 +432,6 @@ void ProfileChooserView::ShowView(BubbleViewMode view_to_display,
   RemoveAllChildViews(true);
   view_mode_ = view_to_display;
 
-  views::GridLayout* layout = CreateSingleColumnLayout(this, kFixedMenuWidth);
-
   if (view_to_display == GAIA_SIGNIN_VIEW ||
       view_to_display == GAIA_ADD_ACCOUNT_VIEW) {
     // Minimum size for embedded sign in pages as defined in Gaia.
@@ -447,6 +445,8 @@ void ProfileChooserView::ShowView(BubbleViewMode view_to_display,
     GURL url(signin::GetPromoURL(
         source, false /* auto_close */, true /* is_constrained */));
     web_view->LoadInitialURL(url);
+    views::GridLayout* layout =
+        CreateSingleColumnLayout(this, kMinGaiaViewWidth);
     layout->StartRow(1, 0);
     layout->AddView(web_view);
     layout->set_minimum_size(
@@ -457,6 +457,7 @@ void ProfileChooserView::ShowView(BubbleViewMode view_to_display,
     return;
   }
 
+  views::GridLayout* layout = CreateSingleColumnLayout(this, kFixedMenuWidth);
   // Separate items into active and alternatives.
   Indexes other_profiles;
   bool is_guest_view = true;
