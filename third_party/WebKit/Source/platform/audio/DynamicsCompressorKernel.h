a89745e458f1ecd43689539ba5bdda4c1c20c12e
rtoy@google.com <rtoy@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-12 02:10:18 +0000

    Remove emphasis/de-emphasis filters from DynamicsCompressor.
    The pre-emphasis and de-emphasis filters are specified in the WebAudio spec, and this causes problems for Chrome OS which is using WebAudio to test speakers.
    
    A discussion on the W3C audio list indicates a consensus to remove the pre-emphasis and de-emphasis filters since they were never specified anyway:
    
    http://lists.w3.org/Archives/Public/public-audio/2014JanMar/0010.html
    
    A simple layout test is added.  Before this CL, the output is compressed; with this CL, the output is no longer compressed.
    
    See also http://webaudiodemos.appspot.com/compressorfreq/ which demonstrates the compression that is happening.  With this CL, the sine wave remains at a constant amplitude.  Previously, the sine wave amplitude decreases as the frequency increases.
    
    BUG=340870
    
    Review URL: https://codereview.chromium.org/152333003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166956 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/audio/DynamicsCompressorKernel.h
+++ third_party/WebKit/Source/platform/audio/DynamicsCompressorKernel.h
@@ -46,7 +46,7 @@ public:
     void setNumberOfChannels(unsigned);
 
     // Performs stereo-linked compression.
-    void process(float* sourceChannels[],
+    void process(const float* sourceChannels[],
                  float* destinationChannels[],
                  unsigned numberOfChannels,
                  unsigned framesToProcess,
