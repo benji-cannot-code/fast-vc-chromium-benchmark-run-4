10714b2bcd0074f0b824af81ad140c410ada5452
aberent@chromium.org <aberent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-12 19:07:08 +0000

    [Android][Mojo] Switch to standard Chromium library loader
    Now that the library loader has been moved to base mojo shell can use
    it. Switch to using it.
    
    BUG=225101
    
    Review URL: https://codereview.chromium.org/159253002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250768 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/shell/android/library_loader.cc
+++ mojo/shell/android/library_loader.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/base_jni_registrar.h"
 #include "base/android/jni_android.h"
 #include "base/android/jni_registrar.h"
+#include "base/android/library_loader/library_loader_hooks.h"
 #include "base/logging.h"
 #include "mojo/services/native_viewport/native_viewport_android.h"
 #include "mojo/shell/android/mojo_main.h"
@@ -29,6 +30,9 @@ bool RegisterMojoJni(JNIEnv* env) {
 JNI_EXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
   base::android::InitVM(vm);
   JNIEnv* env = base::android::AttachCurrentThread();
+
+  if (!base::android::RegisterLibraryLoaderEntryHook(env))
+    return -1;
 
   if (!base::android::RegisterJni(env))
     return -1;
