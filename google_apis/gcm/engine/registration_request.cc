8028e525d99c2c8c3a2cb86d6c4183c50e7190d6
juyik@chromium.org <juyik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-12 01:36:41 +0000

    Add GCM registration status code metric collection.
    BUG=284553
    
    Review URL: https://codereview.chromium.org/139493011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250597 0039d316-1c4b-4281-b951-d872f2087c98

--- google_apis/gcm/engine/registration_request.cc
+++ google_apis/gcm/engine/registration_request.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/message_loop/message_loop.h"
+#include "base/metrics/histogram.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/values.h"
 #include "net/base/escape.h"
@@ -214,6 +215,8 @@ void RegistrationRequest::OnURLFetchComplete(const net::URLFetcher* source) {
         response.substr(error_pos + arraysize(kErrorPrefix) - 1);
     status = GetStatusFromError(error);
   }
+  UMA_HISTOGRAM_ENUMERATION("GCM.RegistrationRequestStatus", status,
+                            RegistrationRequest::STATUS_COUNT);
 
   if (ShouldRetryWithStatus(status)) {
     RetryWithBackoff(true);
