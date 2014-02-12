2f41642a0b557549b4000e74ae6f4469c7b86990
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-12 19:09:32 +0000

    Support LoadSession() in MediaKeys and PPP_ContentDecryptor_Private interface.
    - Add LoadSession() to all MediaKeys implementations.
    - Add LoadSession() support in PPP_ContentDecryptor_Private stack.
    - Move "LOAD_SESSION|" parsing code from CdmWrapper to ProxyDecryptor.
    - Also update "type" parameter in CreateSession() to "content_type".
    
    BUG=338831
    TEST=LoadSession tests in browser_tests still pass.
    R=dcheng@chromium.org, ddorwin@chromium.org, dmichael@chromium.org
    
    Review URL: https://codereview.chromium.org/131653003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250769 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/c/private/ppp_content_decryptor_private.h
+++ ppapi/c/private/ppp_content_decryptor_private.h
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 /* From private/ppp_content_decryptor_private.idl,
- *   modified Wed Jan  8 16:02:29 2014.
+ *   modified Mon Feb 10 13:23:32 2014.
  */
 
 #ifndef PPAPI_C_PRIVATE_PPP_CONTENT_DECRYPTOR_PRIVATE_H_
@@ -19,10 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/c/pp_var.h"
 #include "ppapi/c/private/pp_content_decryptor.h"
 
-#define PPP_CONTENTDECRYPTOR_PRIVATE_INTERFACE_0_10 \
-    "PPP_ContentDecryptor_Private;0.10"
+#define PPP_CONTENTDECRYPTOR_PRIVATE_INTERFACE_0_11 \
+    "PPP_ContentDecryptor_Private;0.11"
 #define PPP_CONTENTDECRYPTOR_PRIVATE_INTERFACE \
-    PPP_CONTENTDECRYPTOR_PRIVATE_INTERFACE_0_10
+    PPP_CONTENTDECRYPTOR_PRIVATE_INTERFACE_0_11
 
 /**
  * @file
@@ -43,7 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Decryption Module (CDM) for Encrypted Media Extensions:
  * http://www.w3.org/TR/encrypted-media/
  */
-struct PPP_ContentDecryptor_Private_0_10 {
+struct PPP_ContentDecryptor_Private_0_11 {
   /**
    * Initialize for the specified key system.
    *
@@ -52,19 +52,18 @@ struct PPP_ContentDecryptor_Private_0_10 {
    */
   void (*Initialize)(PP_Instance instance, struct PP_Var key_system);
   /**
-   * Creates a session. <code>type</code> contains the MIME type of
+   * Creates a session. <code>content_type</code> contains the MIME type of
    * <code>init_data</code>. <code>init_data</code> is a data buffer
    * containing data for use in generating the request.
    *
-   * Note: <code>CreateSession()</code> must create the session ID used in
-   * other methods on this interface. The session ID must be provided to the
-   * browser by the CDM via <code>SessionCreated()</code> on the
+   * Note: <code>CreateSession()</code> must create a web session ID and provide
+   * it to the browser via <code>SessionCreated()</code> on the
    * <code>PPB_ContentDecryptor_Private</code> interface.
    *
    * @param[in] session_id A reference for the session for which a session
    * should be generated.
    *
-   * @param[in] type A <code>PP_Var</code> of type
+   * @param[in] content_type A <code>PP_Var</code> of type
    * <code>PP_VARTYPE_STRING</code> containing the MIME type for init_data.
    *
    * @param[in] init_data A <code>PP_Var</code> of type
@@ -73,8 +72,25 @@ struct PPP_ContentDecryptor_Private_0_10 {
    */
   void (*CreateSession)(PP_Instance instance,
                         uint32_t session_id,
-                        struct PP_Var type,
+                        struct PP_Var content_type,
                         struct PP_Var init_data);
+  /**
+   * Loads a session whose web session ID is <code>web_session_id</code>.
+   *
+   * Note: After the session is successfully loaded, the CDM must call
+   * <code>SessionCreated()</code> with <code>web_session_id</code> on the
+   * <code>PPB_ContentDecryptor_Private</code> interface.
+   *
+   * @param[in] session_id A reference for the session for which a session
+   * should be loaded.
+   *
+   * @param[in] web_session_id A <code>PP_Var</code> of type
+   * <code>PP_VARTYPE_STRING</code> containing the web session ID of the session
+   * to load.
+   */
+  void (*LoadSession)(PP_Instance instance,
+                      uint32_t session_id,
+                      struct PP_Var web_session_id);
   /**
    * Provides a license or other message to the decryptor.
    *
@@ -229,7 +245,7 @@ struct PPP_ContentDecryptor_Private_0_10 {
       const struct PP_EncryptedBlockInfo* encrypted_block_info);
 };
 
-typedef struct PPP_ContentDecryptor_Private_0_10 PPP_ContentDecryptor_Private;
+typedef struct PPP_ContentDecryptor_Private_0_11 PPP_ContentDecryptor_Private;
 /**
  * @}
  */
