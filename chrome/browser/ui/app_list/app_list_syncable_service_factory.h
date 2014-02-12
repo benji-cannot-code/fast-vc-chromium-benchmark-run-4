9253ea0cc091c9b44da495919906440d53d0a9e7
tbarzic@chromium.org <tbarzic@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-12 02:22:01 +0000

    Avoid using GetOffTheRecordProfile in AppListSyncableServiceFactory
    AppListSyncableService may be created during the profile creation
    before the profile is completely initialized. Calling
    GetOffTheRecordProfile at that point is not safe.
    
    BUG=335426
    
    TEST=on Linux with --force-variation-ids=3311944 --force-fieldtrials=NewProfileManagement/Enabled/ and logged in:
    -go to Guest mode
    -lock the profile
    -> no crash
    
    Review URL: https://codereview.chromium.org/156593004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250615 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/app_list/app_list_syncable_service_factory.h
+++ chrome/browser/ui/app_list/app_list_syncable_service_factory.h
@@ -38,7 +38,6 @@ class AppListSyncableServiceFactory : public BrowserContextKeyedServiceFactory {
       user_prefs::PrefRegistrySyncable* registry) OVERRIDE;
   virtual content::BrowserContext* GetBrowserContextToUse(
       content::BrowserContext* context) const OVERRIDE;
-  virtual bool ServiceIsCreatedWithBrowserContext() const OVERRIDE;
 
   DISALLOW_COPY_AND_ASSIGN(AppListSyncableServiceFactory);
 };
