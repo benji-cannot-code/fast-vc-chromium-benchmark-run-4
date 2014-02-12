f4decb309fa73bc3657afb637dfef8f25c9d3d59
jbudorick@chromium.org <jbudorick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-12 23:05:36 +0000

    [Android] Lint pylib/host_driven.
    BUG=168518
    
    Review URL: https://codereview.chromium.org/159853010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250822 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/pylib/host_driven/test_info_collection.py
+++ build/android/pylib/host_driven/test_info_collection.py
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import logging
 import os
 
-import tests_annotations
+from pylib.host_driven import tests_annotations
 
 
 class TestInfo(object):
@@ -23,7 +23,8 @@ class TestInfo(object):
     self.set_up = set_up
     self.tear_down = tear_down
 
-  def _GetQualifiedName(self, runnable):
+  @staticmethod
+  def _GetQualifiedName(runnable):
     """Helper method to infer a runnable's name and module name.
 
     Many filters and lists presuppose a format of module_name.testMethodName.
@@ -103,7 +104,8 @@ class TestInfoCollection(object):
 
     return available_tests
 
-  def _AnnotationIncludesTest(self, test_info, annotation_filter_list):
+  @staticmethod
+  def _AnnotationIncludesTest(test_info, annotation_filter_list):
     """Checks whether a given test represented by test_info matches annotation.
 
     Args:
@@ -129,7 +131,8 @@ class TestInfoCollection(object):
         return True
     return False
 
-  def _NameFilterIncludesTest(self, test_info, name_filter):
+  @staticmethod
+  def _NameFilterIncludesTest(test_info, name_filter):
     """Checks whether a name filter matches a given test_info's method name.
 
     This is a case-sensitive, substring comparison: 'Foo' will match methods
