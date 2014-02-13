d5293d9ba9320276ba2c77092fe3f09298eb05ec
jinho.bang@samsung.com <jinho.bang@samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-13 14:18:11 +0000

    Switch from WebDocument::insertUserStyleSheet() to insertStyleSheet().
    A UserStyleLevel parameter is no longer used in insertUserStyleSheet()
    and insertUserStyleSheet() calls just insertStyleSheet() again.
    
    blink side: https://codereview.chromium.org/143203006
    
    Review URL: https://codereview.chromium.org/154113008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250982 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -4324,9 +4324,7 @@ void RenderViewImpl::OnCSSInsertRequest(const base::string16& frame_xpath,
   if (!frame)
     return;
 
-  frame->document().insertUserStyleSheet(
-      WebString::fromUTF8(css),
-      WebDocument::UserStyleAuthorLevel);
+  frame->document().insertStyleSheet(WebString::fromUTF8(css));
 }
 
 void RenderViewImpl::OnAllowBindings(int enabled_bindings_flags) {
