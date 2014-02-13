6a50ac478cc3942aebc676ef5723aadfb417add3
apatole@nvidia.com <apatole@nvidia.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-13 14:17:25 +0000

    Set thread names to be visible in debuggers
    Like linux, on android we can get the thread names to show up in the
    debugger by setting the process name for the LWP.
    
    This change sets the name of the calling thread using prctl.
    
    BUG=332881
    
    R=jam@chromium.org
    R=torne@chromium.org
    
    Review URL: https://codereview.chromium.org/144943004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250981 0039d316-1c4b-4281-b951-d872f2087c98

--- base/threading/platform_thread_android.cc
+++ base/threading/platform_thread_android.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/platform_thread.h"
 
 #include <errno.h>
+#include <sys/prctl.h>
 #include <sys/resource.h>
 
 #include "base/android/jni_android.h"
@@ -78,6 +79,18 @@ void PlatformThread::SetThreadPriority(PlatformThreadHandle handle,
 void PlatformThread::SetName(const char* name) {
   ThreadIdNameManager::GetInstance()->SetName(CurrentId(), name);
   tracked_objects::ThreadData::InitializeThreadContext(name);
+
+  // Like linux, on android we can get the thread names to show up in the
+  // debugger by setting the process name for the LWP.
+  // We don't want to do this for the main thread because that would rename
+  // the process, causing tools like killall to stop working.
+  if (PlatformThread::CurrentId() == getpid())
+    return;
+
+  // Set the name for the LWP (which gets truncated to 15 characters).
+  int err = prctl(PR_SET_NAME, name);
+  if (err < 0 && errno != EPERM)
+    DPLOG(ERROR) << "prctl(PR_SET_NAME)";
 }
 
 
