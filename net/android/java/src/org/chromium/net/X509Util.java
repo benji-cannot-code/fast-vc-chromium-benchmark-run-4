3dd5b0dbda1282ef0c593ace484f2ee5253c2821
yfriedman@chromium.org <yfriedman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-13 15:51:34 +0000

    Add function to support clearing SSL/certificate store.
    When smartcard auth is disabled we need to wipe the store and any open
    sockets. By issuing a NULL cert being added this happens.
    
    BUG=341500
    
    Review URL: https://codereview.chromium.org/161653002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251033 0039d316-1c4b-4281-b951-d872f2087c98

--- net/android/java/src/org/chromium/net/X509Util.java
+++ net/android/java/src/org/chromium/net/X509Util.java
@@ -292,6 +292,11 @@ public class X509Util {
         ensureInitialized();
     }
 
+    public static void notifyClientCertificatesChanged() {
+        Log.d(TAG, "ClientCertificatesChanged!");
+        nativeNotifyClientCertificatesChanged();
+    }
+
     /**
      * Convert a DER encoded certificate to an X509Certificate.
      */
@@ -439,6 +444,9 @@ public class X509Util {
     public static void setDisableNativeCodeForTest(boolean disabled) {
         sDisableNativeCodeForTest = disabled;
     }
+
+    private static native void nativeNotifyClientCertificatesChanged();
+
     /**
      * Notify the native net::CertDatabase instance that the system database has been updated.
      */
