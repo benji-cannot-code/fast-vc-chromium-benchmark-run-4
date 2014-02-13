6de22b324426577a389d417af802b48fb5239e12
tmdiep@chromium.org <tmdiep@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-13 15:43:02 +0000

    Move Windows-specific code into NativeAppWindowViewsWin
    NativeAppWindowViews has Windows-specific code in #ifdefs. Move this
    into derived class NativeAppWindowViewsWin.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/161753002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251029 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/apps/native_app_window_views_win.h
+++ chrome/browser/ui/views/apps/native_app_window_views_win.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_APPS_NATIVE_APP_WINDOW_VIEWS_WIN_H_
 #define CHROME_BROWSER_UI_VIEWS_APPS_NATIVE_APP_WINDOW_VIEWS_WIN_H_
 
+#include "chrome/browser/shell_integration.h"
 #include "chrome/browser/ui/views/apps/native_app_window_views.h"
 
 // Windows-specific parts of the views-backed native shell window implementation
@@ -17,13 +18,22 @@ class NativeAppWindowViewsWin : public NativeAppWindowViews {
  private:
   void ActivateParentDesktopIfNecessary();
 
+  void OnShortcutInfoLoaded(
+      const ShellIntegration::ShortcutInfo& shortcut_info);
+
+  HWND GetNativeAppWindowHWND() const;
+
   // Overridden from NativeAppWindowViews:
   virtual void OnBeforeWidgetInit(views::Widget::InitParams* init_params,
                                   views::Widget* widget) OVERRIDE;
+  virtual void InitializeDefaultWindow(
+      const apps::ShellWindow::CreateParams& create_params) OVERRIDE;
 
   // Overridden from ui::BaseWindow:
   virtual void Show() OVERRIDE;
   virtual void Activate() OVERRIDE;
+
+  base::WeakPtrFactory<NativeAppWindowViewsWin> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(NativeAppWindowViewsWin);
 };
