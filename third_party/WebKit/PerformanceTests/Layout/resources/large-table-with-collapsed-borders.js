6fe61a6accc435f56269c906785fe3f8d2368291
robhogan@gmail.com <robhogan@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-13 23:29:06 +0000

    Optimize RenderTable::colToEffCol() for tables without colspans
    colToEffCol() is very hot in tables with collapsed borders and
    it is a very expensive function which iterates through every
    column in the table up to the current one. But it only needs to
    do this when the table's list of columns contains one whose
    colspan exceeds the width of the table, as that is the only
    situation in which |ColumnStruct| will still have a span.
    (See RenderTableSection:addCell() where the span is
    redistributed among other columns in the table during a
    second pass of the table.)
    
    Create a fast path for colToEffCol() and effColToCol() that
    avoids iterating through every column unless the |m_columns|
    vector still has a |ColumnStruct| with a span. The slow
    path is extremely rare and probably not
    worth optimizing for, as it only applies to tables
    where a cell has a colspan that exceeds the total number of
    columns provided by other rows in the table.
    
    Review URL: https://codereview.chromium.org/154243002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167120 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/PerformanceTests/Layout/resources/large-table-with-collapsed-borders.js
@@ -0,0 +1,33 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(function() {
+    function createElement(tag, parent, className, id) {
+        var el = document.createElement(tag);
+        el.className = className;
+        if (id)
+            el.id = id;
+        parent.appendChild(el);
+        return el;
+    }
+
+    function createTable(width, height, colspan) {
+        var table = createElement("table", document.body, "table");
+        for (var y = 0; y < height; ++y) {
+            var tr = createElement("tr", table, "tr");
+            for (var x = 0; x < width; ++x) {
+                var td = createElement("td", tr, "td");
+                if (colspan > 0 && x==10 && y==0)
+                    table.rows[y].cells[x].colSpan = colspan;
+            }
+        }
+        return table;
+    }
+
+    function createTestFunction(width, height, colspan) {
+        return function() {
+            var table = createTable(width, height, colspan);
+            table.clientHeight;
+        }
+    }
+
+    window.createTableTestFunction = createTestFunction;
+})();
