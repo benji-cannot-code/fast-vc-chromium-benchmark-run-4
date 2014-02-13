b2e0d22fcf98d24ef401542c8145bcda285fd921
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-13 19:43:01 +0000

    Fix printing unittests after revert of r230235
    r251066 reverted the backgrounding of the print dialog.
    
    Before the revert
    
    http://src.chromium.org/viewvc/chrome/trunk/src/printing/printing_context_win_unittest.cc?r1=232466&r2=232611
    
    assumed they were going to be async, so added RunLoop. Now that the
    dialog is synchronous again, remove that code which is causing the
    MessageLoop to complain.
    
    TBR=vitalybuka@chromium.org
    R=asvitkine@chromium.org
    BUG=180997,342697
    
    Review URL: https://codereview.chromium.org/164483002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251102 0039d316-1c4b-4281-b951-d872f2087c98

--- printing/printing_context_win_unittest.cc
+++ printing/printing_context_win_unittest.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/memory/scoped_ptr.h"
-#include "base/run_loop.h"
 #include "printing/backend/printing_info_win.h"
 #include "printing/printing_test.h"
 #include "printing/printing_context.h"
@@ -24,7 +23,6 @@ class PrintingContextTest : public PrintingTest<testing::Test> {
  public:
   void PrintSettingsCallback(printing::PrintingContext::Result result) {
     result_ = result;
-    base::MessageLoop::current()->QuitWhenIdle();
   }
 
  protected:
@@ -162,7 +160,6 @@ TEST_F(PrintingContextTest, Base) {
 }
 
 TEST_F(PrintingContextTest, PrintAll) {
-  base::MessageLoopForUI loop;
   if (IsTestCaseDisabled())
     return;
 
@@ -172,9 +169,7 @@ TEST_F(PrintingContextTest, PrintAll) {
   context.AskUserForSettings(
       NULL, 123, false, base::Bind(&PrintingContextTest::PrintSettingsCallback,
                                    base::Unretained(this)));
-  base::RunLoop().Run();
   EXPECT_EQ(printing::PrintingContext::OK, result());
-
   printing::PrintSettings settings = context.settings();
   EXPECT_EQ(settings.ranges().size(), 0);
 }
