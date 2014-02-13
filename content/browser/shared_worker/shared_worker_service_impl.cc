9af95abb959ee82bdf8f13335415b468669670d4
horo@chromium.org <horo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-13 15:10:01 +0000

    Add an empty class for the embedded SharedWorker. (SharedWorkerServiceImpl)
    These class will be used when "enable-embedded-shared-worker" is set.
    
    BUG=327256
    
    Review URL: https://codereview.chromium.org/140333011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251012 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ content/browser/shared_worker/shared_worker_service_impl.cc
@@ -0,0 +1,51 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/shared_worker/shared_worker_service_impl.h"
+
+#include "content/common/worker_messages.h"
+#include "content/public/browser/browser_thread.h"
+#include "content/public/browser/worker_service_observer.h"
+
+namespace content {
+
+SharedWorkerServiceImpl* SharedWorkerServiceImpl::GetInstance() {
+  // TODO(horo): implement this.
+  NOTIMPLEMENTED();
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+  return Singleton<SharedWorkerServiceImpl>::get();
+}
+
+SharedWorkerServiceImpl::SharedWorkerServiceImpl() {
+  // TODO(horo): implement this.
+}
+
+SharedWorkerServiceImpl::~SharedWorkerServiceImpl() {
+  // TODO(horo): implement this.
+}
+
+
+bool SharedWorkerServiceImpl::TerminateWorker(int process_id, int route_id) {
+  // TODO(horo): implement this.
+  return false;
+}
+
+std::vector<WorkerService::WorkerInfo> SharedWorkerServiceImpl::GetWorkers() {
+  // TODO(horo): implement this.
+  std::vector<WorkerService::WorkerInfo> results;
+  return results;
+}
+
+void SharedWorkerServiceImpl::AddObserver(WorkerServiceObserver* observer) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+  observers_.AddObserver(observer);
+}
+
+void SharedWorkerServiceImpl::RemoveObserver(WorkerServiceObserver* observer) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+  observers_.RemoveObserver(observer);
+}
+
+}  // namespace content
--- content/content_browser.gypi
+++ content/content_browser.gypi
@@ -1129,6 +1129,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     'browser/service_worker/service_worker_storage.h',
     'browser/service_worker/service_worker_version.cc',
     'browser/service_worker/service_worker_version.h',
+    'browser/shared_worker/shared_worker_service_impl.cc',
+    'browser/shared_worker/shared_worker_service_impl.h',
     'browser/signed_certificate_timestamp_store_impl.cc',
     'browser/signed_certificate_timestamp_store_impl.h',
     'browser/site_instance_impl.cc',
