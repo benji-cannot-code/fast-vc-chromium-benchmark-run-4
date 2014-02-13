5719e0c111af5072032d5cf103e005c13ff5f028
dpranke@chromium.org <dpranke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-13 00:07:26 +0000

    Revert r250851 - "Update all the client code to use a single WebThemeEngine.h header."
    This changes depends on r166988, and that hasn't actually rolled yet :(.
    
    TBR=jamesr@chromium.org
    BUG=327471
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250856 0039d316-1c4b-4281-b951-d872f2087c98

--- content/child/webthemeengine_impl_android.h
+++ content/child/webthemeengine_impl_android.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_CHILD_WEBTHEMEENGINE_IMPL_ANDROID_H_
 #define CONTENT_CHILD_WEBTHEMEENGINE_IMPL_ANDROID_H_
 
-#include "third_party/WebKit/public/platform/WebThemeEngine.h"
+#include "third_party/WebKit/public/platform/default/WebThemeEngine.h"
 
 namespace content {
 
--- content/shell/renderer/test_runner/WebTestThemeEngineMock.h
+++ content/shell/renderer/test_runner/WebTestThemeEngineMock.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_SHELL_RENDERER_TEST_RUNNER_WEBTESTTHEMEENGINEMOCK_H_
 
 #include "third_party/WebKit/public/platform/WebRect.h"
-#include "third_party/WebKit/public/platform/WebThemeEngine.h"
+#include "third_party/WebKit/public/platform/default/WebThemeEngine.h"
 
 using blink::WebRect;
 using blink::WebThemeEngine;
