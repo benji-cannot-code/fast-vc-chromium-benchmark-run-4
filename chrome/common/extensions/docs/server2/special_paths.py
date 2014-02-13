eae3d0778c1584becf74ef49fe71ef3e9375c645
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-13 03:49:18 +0000

    Docserver: Make the .html extension unnecessary for content pages, for example, the Storage API docs are now hosted at extensions/storage not extensions/storage.html. Paths which include .html are redirected to remove the .html.
    BUG=294008
    R=yoz@chromium.org
    
    Review URL: https://codereview.chromium.org/148293018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250926 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/common/extensions/docs/server2/special_paths.py
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Paths with special significance in the docserver.
+#
+# This is a bit of a hack, all path configuration should be moved into
+# content_providers.json and the code which uses it updated.
+
+SITE_VERIFICATION_FILE = 'google2ed1af765c529f57.html'
