85f06df33e39dcab3b43ef99206baf0f1f47be25
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-13 18:55:25 +0000

    Revert 250828 "Add a UMA stat to track if the Browser blacklist ..."
    Speculative revert for failures here:
    
    http://build.chromium.org/p/chromium.win/builders/Win7%20Sync%20x64/builds/11201
    
    > Add a UMA stat to track if the Browser blacklist is Set on the Renderer
    > 
    > This shouldn't be happening, but we got some crash reports suggesting it
    > does. Unable to repo locally so this stat will verify it does occur and
    > then can be used to verify our fixes actually fix it.
    > 
    > BUG=329023
    > 
    > Review URL: https://codereview.chromium.org/140763008
    
    TBR=csharp@chromium.org
    
    Review URL: https://codereview.chromium.org/163633005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251095 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_elf/blacklist/blacklist.cc
+++ chrome_elf/blacklist/blacklist.cc
@@ -61,10 +61,6 @@ enum WOW64Status {
   WOW64_UNKNOWN,
 };
 
-// Record if the blacklist was successfully initialized so processes can easily
-// determine if the blacklist is enabled for them.
-bool g_blacklist_initialized = false;
-
 WOW64Status GetWOW64StatusForCurrentProcess() {
   typedef BOOL (WINAPI* IsWow64ProcessFunc)(HANDLE, PBOOL);
   IsWow64ProcessFunc is_wow64_process = reinterpret_cast<IsWow64ProcessFunc>(
@@ -277,10 +273,6 @@ int BlacklistSize() {
   return size;
 }
 
-bool IsBlacklistInitialized() {
-  return g_blacklist_initialized;
-}
-
 bool AddDllToBlacklist(const wchar_t* dll_name) {
   int blacklist_size = BlacklistSize();
   // We need to leave one space at the end for the null pointer.
@@ -381,9 +373,6 @@ bool Initialize(bool force) {
     thunk = new sandbox::ServiceResolverThunk(current_process, kRelaxed);
   }
 #endif
-
-  // Record that we have initialized the blacklist.
-  g_blacklist_initialized = true;
 
   BYTE* thunk_storage = reinterpret_cast<BYTE*>(&g_thunk_storage);
 
