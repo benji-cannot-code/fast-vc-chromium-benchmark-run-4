c3e982962d617e500a657c47503617ba5e5075d4
jennyz@chromium.org <jennyz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-13 21:29:07 +0000

    Use the new animation tween types to improve the animation effects for app list folder operation.
    BUG=342832
    
    Review URL: https://codereview.chromium.org/162363002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251128 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/app_list/app_list_constants.h
+++ ui/app_list/app_list_constants.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/app_list/app_list_export.h"
 #include "ui/base/resource/resource_bundle.h"
+#include "ui/gfx/animation/tween.h"
 
 namespace app_list {
 
@@ -37,6 +38,8 @@ APP_LIST_EXPORT extern const int kPageTransitionDurationInMs;
 APP_LIST_EXPORT extern const int kOverscrollPageTransitionDurationMs;
 APP_LIST_EXPORT extern const int kFolderTransitionInDurationMs;
 APP_LIST_EXPORT extern const int kFolderTransitionOutDurationMs;
+APP_LIST_EXPORT extern const gfx::Tween::Type kFolderFadeInTweenType;
+APP_LIST_EXPORT extern const gfx::Tween::Type kFolderFadeOutTweenType;
 
 APP_LIST_EXPORT extern const int kPreferredCols;
 APP_LIST_EXPORT extern const int kPreferredRows;
