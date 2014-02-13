76009bed628008a6a9b40f28ad4fc0180a39015e
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-13 17:25:00 +0000

    aura: Remove an unnecessary parameter from DispatcherClient::RunWithDispatcher.
    The |nestable_tasks_allowed| parameter was always being set to true from the
    callsites. So get rid of the variable, and use ScopedNestableTaskAllower in
    the RunWithDispatcher implementations.
    
    BUG=none
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/162633002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251072 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/accelerators/nested_dispatcher_controller_unittest.cc
+++ ash/accelerators/nested_dispatcher_controller_unittest.cc
@@ -110,8 +110,7 @@ TEST_F(NestedDispatcherTest, AssociatedWindowBelowLockScreen) {
   aura::Window* root_window = ash::Shell::GetPrimaryRootWindow();
   aura::client::GetDispatcherClient(root_window)->RunWithDispatcher(
       &inner_dispatcher,
-      associated_window.get(),
-      true /* nestable_tasks_allowed */);
+      associated_window.get());
   EXPECT_EQ(0, inner_dispatcher.num_key_events_dispatched());
   Shell::GetInstance()->session_state_delegate()->UnlockScreen();
 }
@@ -131,8 +130,7 @@ TEST_F(NestedDispatcherTest, AssociatedWindowAboveLockScreen) {
   aura::Window* root_window = ash::Shell::GetPrimaryRootWindow();
   aura::client::GetDispatcherClient(root_window)->RunWithDispatcher(
       &inner_dispatcher,
-      associated_window.get(),
-      true /* nestable_tasks_allowed */);
+      associated_window.get());
   EXPECT_EQ(1, inner_dispatcher.num_key_events_dispatched());
 }
 
@@ -150,8 +148,7 @@ TEST_F(NestedDispatcherTest, AcceleratorsHandled) {
   DispatchKeyReleaseA();
   aura::client::GetDispatcherClient(root_window)->RunWithDispatcher(
       &inner_dispatcher,
-      root_window,
-      true /* nestable_tasks_allowed */);
+      root_window);
   EXPECT_EQ(0, inner_dispatcher.num_key_events_dispatched());
   EXPECT_EQ(1, target.accelerator_pressed_count());
 }
