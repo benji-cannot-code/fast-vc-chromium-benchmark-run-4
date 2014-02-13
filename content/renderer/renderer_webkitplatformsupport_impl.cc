af071b14200fab9462bb821bd340b403de6fb0f7
ddorwin@chromium.org <ddorwin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-13 04:34:28 +0000

    Remove unused includes & using statements from blink::Platform implementations.
    Review URL: https://codereview.chromium.org/161873002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250945 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/renderer_webkitplatformsupport_impl.cc
+++ content/renderer/renderer_webkitplatformsupport_impl.cc
@@ -71,8 +71,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/platform/WebPluginListBuilder.h"
 #include "third_party/WebKit/public/platform/WebURL.h"
 #include "third_party/WebKit/public/platform/WebVector.h"
-#include "third_party/WebKit/public/web/WebFrame.h"
-#include "third_party/WebKit/public/web/WebRuntimeFeatures.h"
 #include "ui/gfx/color_profile.h"
 #include "url/gurl.h"
 #include "webkit/common/gpu/context_provider_web_context.h"
@@ -114,7 +112,6 @@ using blink::WebBlobRegistry;
 using blink::WebDatabaseObserver;
 using blink::WebFileInfo;
 using blink::WebFileSystem;
-using blink::WebFrame;
 using blink::WebGamepads;
 using blink::WebIDBFactory;
 using blink::WebMIDIAccessor;
--- webkit/child/webkitplatformsupport_impl.cc
+++ webkit/child/webkitplatformsupport_impl.cc
@@ -33,17 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/data_url.h"
 #include "net/base/mime_util.h"
 #include "net/base/net_errors.h"
-#include "third_party/WebKit/public/platform/WebCookie.h"
 #include "third_party/WebKit/public/platform/WebData.h"
-#include "third_party/WebKit/public/platform/WebDiscardableMemory.h"
-#include "third_party/WebKit/public/platform/WebGestureCurve.h"
-#include "third_party/WebKit/public/platform/WebPluginListBuilder.h"
-#include "third_party/WebKit/public/platform/WebScreenInfo.h"
 #include "third_party/WebKit/public/platform/WebString.h"
-#include "third_party/WebKit/public/platform/WebURL.h"
-#include "third_party/WebKit/public/platform/WebVector.h"
-#include "third_party/WebKit/public/web/WebFrameClient.h"
-#include "third_party/WebKit/public/web/WebInputEvent.h"
 #include "ui/base/layout.h"
 #include "webkit/child/webkit_child_helpers.h"
 #include "webkit/child/websocketstreamhandle_impl.h"
@@ -58,17 +49,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/tcmalloc/chromium/src/gperftools/heap-profiler.h"
 #endif
 
-using blink::WebAudioBus;
-using blink::WebCookie;
 using blink::WebData;
 using blink::WebLocalizedString;
-using blink::WebPluginListBuilder;
 using blink::WebString;
 using blink::WebSocketStreamHandle;
 using blink::WebURL;
 using blink::WebURLError;
 using blink::WebURLLoader;
-using blink::WebVector;
 
 namespace {
 
