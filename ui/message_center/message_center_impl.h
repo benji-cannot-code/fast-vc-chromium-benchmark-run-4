ba1534e86accf375246e601cf9d0e1af0c8e77d5
koss@chromium.org <koss@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-13 21:33:20 +0000

    Fix misspelling of notifications.
    BUG=NONE
    
    Review URL: https://codereview.chromium.org/161173004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251130 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/message_center/message_center_impl.h
+++ ui/message_center/message_center_impl.h
@@ -198,7 +198,7 @@ class MessageCenterImpl : public MessageCenter,
   struct NotificationCache {
     NotificationCache();
     ~NotificationCache();
-    void Rebuild(const NotificationList::Notifications& notificaitons);
+    void Rebuild(const NotificationList::Notifications& notifications);
     void RecountUnread();
 
     NotificationList::Notifications visible_notifications;
