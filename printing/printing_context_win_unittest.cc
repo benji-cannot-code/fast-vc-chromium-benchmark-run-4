23290d274342eda77fd60f5ac22b2e3c5023a922
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-14 18:36:17 +0000

    Use ScopedNestedTaskAllower instead of background thread for print dialog
    Tasks need to be processed for painting to occur in other browser
    windows while the modal dialog is up in Aura.
    
    R=vitalybuka@chromium.org
    BUG=342697,180997,343848
    
    Review URL: https://codereview.chromium.org/164303002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251373 0039d316-1c4b-4281-b951-d872f2087c98

--- printing/printing_context_win_unittest.cc
+++ printing/printing_context_win_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/message_loop/message_loop.h"
 #include "printing/backend/printing_info_win.h"
 #include "printing/printing_test.h"
 #include "printing/printing_context.h"
@@ -160,6 +161,7 @@ TEST_F(PrintingContextTest, Base) {
 }
 
 TEST_F(PrintingContextTest, PrintAll) {
+  base::MessageLoop message_loop;
   if (IsTestCaseDisabled())
     return;
 
