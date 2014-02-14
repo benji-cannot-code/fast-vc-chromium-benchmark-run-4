c97e0da97505b6be080b6b08d64e2cd65bdf11b2
mkwst@chromium.org <mkwst@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-14 12:54:12 +0000

    Improve HTMLMediaElement exception messages.
    This patch takes care of most of the exceptions in HTMLMediaElement. A
    few run through a helper function after being caused in other files; a
    future patch will route reasonable messages through to the point where
    they're thrown.
    
    BUG=270033
    
    Review URL: https://codereview.chromium.org/164193007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167189 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/ExceptionMessages.h
+++ third_party/WebKit/Source/bindings/v8/ExceptionMessages.h
@@ -40,6 +40,11 @@ class Decimal;
 
 class ExceptionMessages {
 public:
+    enum BoundType {
+        InclusiveBound,
+        ExclusiveBound,
+    };
+
     static String failedToConstruct(const String& type, const String& detail = String());
     static String failedToExecute(const String& method, const String& type, const String& detail = String());
     static String failedToGet(const String& property, const String& type, const String& detail);
@@ -61,6 +66,7 @@ public:
     static String readOnly(const char* detail = 0);
 
     static String indexExceedsMaximumBound(const char* name, unsigned given, unsigned bound);
+    static String indexOutsideRange(const char* name, double given, double lowerBound, BoundType lowerType, double upperBound, BoundType upperType);
 
 private:
     static String ordinalNumber(int number);
