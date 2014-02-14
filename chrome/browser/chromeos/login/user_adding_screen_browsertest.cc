379f77d658da4f8c6f1a1a089ddbdfb268f8dc21
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-14 17:09:14 +0000

    Revert 251352 "cros: Enable multiprofile for everyone."
    > cros: Enable multiprofile for everyone.
    >
    > BUG=338941
    > R=dpolukhin@chromium.org, nkostylev@chromium.org
    >
    > Review URL: https://codereview.chromium.org/146193005
    
    TBR=xiyuan@chromium.org
    
    Review URL: https://codereview.chromium.org/167173002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251357 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/user_adding_screen_browsertest.cc
+++ chrome/browser/chromeos/login/user_adding_screen_browsertest.cc
@@ -40,6 +40,7 @@ class UserAddingScreenTest : public LoginManagerTest,
   virtual void SetUpCommandLine(CommandLine* command_line) OVERRIDE {
     LoginManagerTest::SetUpCommandLine(command_line);
     command_line->AppendSwitch(::switches::kMultiProfiles);
+    command_line->AppendSwitch(switches::kForceMultiProfileInTests);
   }
 
   virtual void SetUpInProcessBrowserTestFixture() OVERRIDE {
--- chrome/browser/ui/webui/options/chromeos/accounts_options_browsertest.cc
+++ chrome/browser/ui/webui/options/chromeos/accounts_options_browsertest.cc
@@ -60,6 +60,7 @@ class AccountsOptionsTest : public LoginManagerTest {
   virtual void SetUpCommandLine(CommandLine* command_line) OVERRIDE {
     LoginManagerTest::SetUpCommandLine(command_line);
     command_line->AppendSwitch(::switches::kMultiProfiles);
+    command_line->AppendSwitch(switches::kForceMultiProfileInTests);
   }
 
  protected:
