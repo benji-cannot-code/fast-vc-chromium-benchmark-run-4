benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 60e538df6faf224369f67087ccc06d3b79ccff84

Explicitly provide path to C built-in includes in libc++ builds.
Temporary workaround for a goma bug.

BUG=337426
TBR=glider@chromium.org

Review URL: https://codereview.chromium.org/167113004

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251365 0039d316-1c4b-4281-b951-d872f2087c98

third_party/libc++/libc++.gyp
@@ -43,6 +43,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'include_dirs': [
         'trunk/include',
         '../libc++abi/trunk/include',
+        # TODO(earthdok): remove when http://crbug.com/337426 is fixed
+        '../llvm-build/Release+Asserts/lib/clang/3.5/include/'
       ],
       'cflags': [
         '-g', '-Os', '-fPIC',
@@ -71,6 +73,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'ldflags': [
               '-stdlib=libc++',
               '-L<(PRODUCT_DIR)/lib/',
+            ],
+            'include_dirs': [
+              # TODO(earthdok): remove when http://crbug.com/337426 is fixed
+              '../llvm-build/Release+Asserts/lib/clang/3.5/include/'
             ],
           }],
         ],

third_party/libc++abi/libc++abi.gyp
@@ -33,7 +33,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       ],
       'include_dirs': [
         'trunk/include',
-        '../libc++/trunk/include'
+        '../libc++/trunk/include',
+        # TODO(earthdok): remove when http://crbug.com/337426 is fixed
+        '../llvm-build/Release+Asserts/lib/clang/3.5/include/'
       ],
       'cflags': [
         '-g', '-O3', '-fPIC',
@@ -66,6 +68,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             ],
             'ldflags': [
               '-L<(PRODUCT_DIR)/lib/',
+            ],
+            'include_dirs': [
+              # TODO(earthdok): remove when http://crbug.com/337426 is fixed
+              '../llvm-build/Release+Asserts/lib/clang/3.5/include/'
             ],
           }],
         ],

commit 014eef7dd2a579b640ec56c86b1ed8c359df7b6f

Fix build error caused by r251365.
BUG=337426
TBR=glider@chromium.org

Review URL: https://codereview.chromium.org/166373005

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251388 0039d316-1c4b-4281-b951-d872f2087c98

third_party/libc++/libc++.gyp
@@ -66,6 +66,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           ['_type!="none"', {
             'include_dirs': [
               'trunk/include',
+              # TODO(earthdok): remove when http://crbug.com/337426 is fixed
+              '../llvm-build/Release+Asserts/lib/clang/3.5/include/'
             ],
             'cflags_cc': [
               '-nostdinc++',
@@ -73,10 +75,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'ldflags': [
               '-stdlib=libc++',
               '-L<(PRODUCT_DIR)/lib/',
-            ],
-            'include_dirs': [
-              # TODO(earthdok): remove when http://crbug.com/337426 is fixed
-              '../llvm-build/Release+Asserts/lib/clang/3.5/include/'
             ],
           }],
         ],

third_party/libc++abi/libc++abi.gyp
@@ -62,16 +62,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           ['_type!="none"', {
             'include_dirs': [
               'trunk/include',
+              # TODO(earthdok): remove when http://crbug.com/337426 is fixed
+              '../llvm-build/Release+Asserts/lib/clang/3.5/include/'
             ],
             'cflags_cc': [
               '-nostdinc++',
             ],
             'ldflags': [
               '-L<(PRODUCT_DIR)/lib/',
-            ],
-            'include_dirs': [
-              # TODO(earthdok): remove when http://crbug.com/337426 is fixed
-              '../llvm-build/Release+Asserts/lib/clang/3.5/include/'
             ],
           }],
         ],