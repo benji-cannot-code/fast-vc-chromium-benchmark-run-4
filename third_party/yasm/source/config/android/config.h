1bad957b298ff3a92e0ec110ceab018bf4466a56
torne@chromium.org <torne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-14 15:28:38 +0000

    android: disable NLS when building yasm.
    When crosscompiling for android on mac hosts, some machines don't have
    the NLS header files available. Rather than insist on another build
    dependency, just disable NLS when building yasm; we can live without
    localisation in a build-time tool.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/146833017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251333 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/yasm/source/config/android/config.h
+++ third_party/yasm/source/config/android/config.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CPP_PROG "gcc -E"
 
 /* */
-#define ENABLE_NLS 1
+/* #undef ENABLE_NLS */
 
 /* Define to 1 if you have the `abort' function. */
 #define HAVE_ABORT 1
