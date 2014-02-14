684d3ebdcfcd6dbb784e3c9e103dbcb343d891ea
adamk@chromium.org <adamk@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-14 00:46:10 +0000

    Move CSSPropertyAnimation.{h,cpp} to core/animation/css
    This allows the removal of the confusingly-named core/frame/animation directory.
    
    R=dstockwell@chromium.org
    BUG=334225
    
    Review URL: https://codereview.chromium.org/164903002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167128 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/css/CSSPropertyAnimation.cpp
+++ third_party/WebKit/Source/core/animation/css/CSSPropertyAnimation.cpp
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
-#include "core/frame/animation/CSSPropertyAnimation.h"
+#include "core/animation/css/CSSPropertyAnimation.h"
 
 #include <algorithm>
 #include "StylePropertyShorthand.h"
