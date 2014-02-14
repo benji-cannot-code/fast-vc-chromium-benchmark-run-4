065fd4e5b7a7968527ded8d4f3df7f45c6f80b31
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-14 04:27:31 +0000

    Log QUIC public reset client address mismatch to a histogram.
    Remove the client_address member from QuicCryptoNegotiatedParameters.
    Replace it by the client_address_ member of QuicConnectionLogger.
    
    Add kCADR support to CryptoHandshakeMessage::DebugStringInternal.
    
    R=jar@chromium.org,rch@chromium.org
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/159143007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251258 0039d316-1c4b-4281-b951-d872f2087c98

--- net/quic/crypto/crypto_server_test.cc
+++ net/quic/crypto/crypto_server_test.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/quic/crypto/crypto_utils.h"
 #include "net/quic/crypto/quic_crypto_server_config.h"
 #include "net/quic/crypto/quic_random.h"
+#include "net/quic/quic_socket_address_coder.h"
 #include "net/quic/quic_utils.h"
 #include "net/quic/test_tools/crypto_test_utils.h"
 #include "net/quic/test_tools/delayed_verify_strike_register_client.h"
@@ -127,6 +128,23 @@ class CryptoServerTest : public ::testing::Test {
     const char* error_substr_;
     bool* called_;
   };
+
+  void CheckServerHello(const CryptoHandshakeMessage& server_hello) {
+    const QuicTag* versions;
+    size_t num_versions;
+    server_hello.GetTaglist(kVER, &versions, &num_versions);
+    ASSERT_EQ(QuicSupportedVersions().size(), num_versions);
+    for (size_t i = 0; i < num_versions; ++i) {
+      EXPECT_EQ(QuicVersionToQuicTag(QuicSupportedVersions()[i]), versions[i]);
+    }
+
+    StringPiece address;
+    ASSERT_TRUE(server_hello.GetStringPiece(kCADR, &address));
+    QuicSocketAddressCoder decoder;
+    ASSERT_TRUE(decoder.Decode(address.data(), address.size()));
+    EXPECT_EQ(client_address_.address(), decoder.ip());
+    EXPECT_EQ(client_address_.port(), decoder.port());
+  }
 
   void ShouldSucceed(const CryptoHandshakeMessage& message) {
     bool called = false;
@@ -334,17 +352,12 @@ TEST_F(CryptoServerTest, ReplayProtection) {
   ShouldSucceed(msg);
   // The message should be accepted now.
   ASSERT_EQ(kSHLO, out_.tag());
+  CheckServerHello(out_);
 
   ShouldSucceed(msg);
   // The message should accepted twice when replay protection is off.
   ASSERT_EQ(kSHLO, out_.tag());
-  const QuicTag* versions;
-  size_t num_versions;
-  out_.GetTaglist(kVER, &versions, &num_versions);
-  ASSERT_EQ(QuicSupportedVersions().size(), num_versions);
-  for (size_t i = 0; i < num_versions; ++i) {
-    EXPECT_EQ(QuicVersionToQuicTag(QuicSupportedVersions()[i]), versions[i]);
-  }
+  CheckServerHello(out_);
 }
 
 TEST(CryptoServerConfigGenerationTest, Determinism) {
--- net/quic/quic_connection_logger.h
+++ net/quic/quic_connection_logger.h
@@ -6,12 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_QUIC_QUIC_CONNECTION_LOGGER_H_
 #define NET_QUIC_QUIC_CONNECTION_LOGGER_H_
 
+#include "net/base/ip_endpoint.h"
 #include "net/quic/quic_connection.h"
 #include "net/quic/quic_protocol.h"
 
 namespace net {
 
-class BoundNetLog;
 class CryptoHandshakeMessage;
 
 // This class is a debug visitor of a QuicConnection which logs
@@ -77,6 +77,8 @@ class NET_EXPORT_PRIVATE QuicConnectionLogger
   size_t num_truncated_acks_sent_;
   // Number of times a truncated ACK frame was received.
   size_t num_truncated_acks_received_;
+  // The kCADR value provided by the server in ServerHello.
+  IPEndPoint client_address_;
   DISALLOW_COPY_AND_ASSIGN(QuicConnectionLogger);
 };
 
