919c18deb48cde96adc7bb95da429ad40ab83fd0
vegorov@chromium.org <vegorov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-14 00:40:32 +0000

    Clear the safepoint marker only after we left the safepoint.
    We should not change any GC related parts of the ThreadState while we are inside the safepoint region because GC can be in progress and it can be accesing them. In this case SafePointBarrier will park the current thread until GC is over. It is safe to mutate ThreadState after we successfully left safepoint as this means that there is no GC in progress.
    
    BUG=342856
    
    Review URL: https://codereview.chromium.org/162633003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167127 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/heap/ThreadState.cpp
+++ third_party/WebKit/Source/heap/ThreadState.cpp
@@ -350,9 +350,8 @@ void ThreadState::visitStack(Visitor* visitor)
     // will read past start address.
     current = reinterpret_cast<Address*>(reinterpret_cast<intptr_t>(current) & ~(sizeof(Address) - 1));
 
-    for (; current < start; ++current) {
+    for (; current < start; ++current)
         Heap::checkAndMarkPointer(visitor, *current);
-    }
 
     for (Vector<Address>::iterator it = m_safePointStackCopy.begin(); it != m_safePointStackCopy.end(); ++it)
         Heap::checkAndMarkPointer(visitor, *it);
@@ -606,10 +605,10 @@ void ThreadState::leaveSafePoint()
 {
     checkThread();
     ASSERT(m_atSafePoint);
+    s_safePointBarrier->leaveSafePoint(this);
     m_atSafePoint = false;
     m_stackState = HeapPointersOnStack;
     clearSafePointScopeMarker();
-    s_safePointBarrier->leaveSafePoint(this);
 }
 
 void ThreadState::copyStackUntilSafePointScope()
