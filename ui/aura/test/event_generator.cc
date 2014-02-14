149a51beff73a4b8adaab67d4106c6f413bf7a7e
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-14 13:44:48 +0000

    Do not set the last mouse location from synthesized event.  The last mouse location should be the updated only by real event because it should point to the real mouse point and synthesized enter/leave may not contain the valid point.
    minor: remove unnecessary cast.
    
    BUG=None
    TEST=TBD.
    
    Review URL: https://codereview.chromium.org/143303006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251291 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/test/event_generator.cc
+++ ui/aura/test/event_generator.cc
@@ -609,7 +609,7 @@ void EventGenerator::DoDispatchEvent(ui::Event* event, bool async) {
     pending_events_.push_back(pending_event);
   } else {
     ui::EventDispatchDetails details = current_root_window_->OnEventFromSource(
-        static_cast<ui::KeyEvent*>(event));
+        event);
     CHECK(!details.dispatcher_destroyed);
   }
 }
