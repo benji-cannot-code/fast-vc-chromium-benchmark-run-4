a5a8a4a95853d1bef275ab8bbf46a65503987dab
mvanouwerkerk@chromium.org <mvanouwerkerk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-14 16:00:38 +0000

    Delete Geolocation command line switch and callers of enableGeolocation.
    The Geolocation API was launched a long time ago, and a mock is used in layout tests.
    There are no unit tests so nothing needs to be set for them.
    
    BUG=343592,36451
    
    Review URL: https://codereview.chromium.org/164163002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251340 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -983,7 +983,6 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kDisableFileSystem,
     switches::kDisableFiltersOverIPC,
     switches::kDisableFullScreen,
-    switches::kDisableGeolocation,
     switches::kDisableGpu,
     switches::kDisableGpuCompositing,
     switches::kDisableGpuVsync,
