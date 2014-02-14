7082d72577e196b0993d01f51c97eb272a572756
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-14 04:11:54 +0000

    Ensure that OnCreated() gets sent before OnBoundsChanged()
    BUG=None
    TEST=None
    
    Review URL: https://codereview.chromium.org/165383002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251253 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/services/native_viewport/native_viewport_service.cc
+++ mojo/services/native_viewport/native_viewport_service.cc
@@ -42,6 +42,7 @@ class NativeViewportImpl
         services::NativeViewport::Create(context(), this);
     native_viewport_->Init(bounds);
     client()->OnCreated();
+    OnBoundsChanged(bounds);
   }
 
   virtual void Show() MOJO_OVERRIDE {
