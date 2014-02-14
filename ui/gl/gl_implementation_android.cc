42de5118763882d6772d881f0e172e485f2f60d1
hyunki.baik@samsung.com <hyunki.baik@samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-14 13:35:10 +0000

    Add few LOG(ERROR)s when LoadLibrary fails
    added log messages when LoadLibrary fails for GLES/EGL libraries.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/160403002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251287 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gl/gl_implementation_android.cc
+++ ui/gl/gl_implementation_android.cc
@@ -58,10 +58,13 @@ bool InitializeStaticGLBindings(GLImplementation implementation) {
   switch (implementation) {
     case kGLImplementationEGLGLES2: {
       base::NativeLibrary gles_library = LoadLibrary("libGLESv2.so");
-      if (!gles_library)
+      if (!gles_library) {
+        LOG(ERROR) << "Failed to load libGLESv2.so.";
         return false;
+      }
       base::NativeLibrary egl_library = LoadLibrary("libEGL.so");
       if (!egl_library) {
+        LOG(ERROR) << "Failed to load libEGL.so.";
         base::UnloadNativeLibrary(gles_library);
         return false;
       }
@@ -121,6 +124,7 @@ bool InitializeDynamicGLBindings(GLImplementation implementation,
         InitializeDynamicGLBindingsGL(context);
       break;
     default:
+      NOTREACHED() << "InitializeDynamicGLBindings on Android";
       return false;
   }
 
