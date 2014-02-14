702029fecb0d36dcd09eeeebc34f1d99a10adeee
skobes@chromium.org <skobes@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-14 22:10:11 +0000

    Handle subtree layout roots.
    FTA previously assumed that the RenderView would be the first block to beginLayout, but FrameView::layout may begin at a different block in some cases.
    
    With this change FTA correctly detects the start and end of layout in such a case, and treats the subtree layout root as an independent cluster.
    
    BUG=332253
    
    Review URL: https://codereview.chromium.org/165583002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167225 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/FastTextAutosizer.cpp
+++ third_party/WebKit/Source/core/rendering/FastTextAutosizer.cpp
@@ -82,16 +82,24 @@ void FastTextAutosizer::destroy(const RenderBlock* block)
     m_fingerprintMapper.remove(block);
 }
 
+bool FastTextAutosizer::isLayoutRoot(const RenderBlock* block) const
+{
+    RenderObject* layoutRoot = m_document->view()->layoutRoot(true);
+    if (!layoutRoot)
+        layoutRoot = m_document->renderer();
+    return block == layoutRoot;
+}
+
 void FastTextAutosizer::beginLayout(RenderBlock* block)
 {
     ASSERT(enabled());
 #ifndef NDEBUG
     m_blocksThatHaveBegunLayout.add(block);
 #endif
-    ASSERT(m_clusterStack.isEmpty() == block->isRenderView());
+    ASSERT(m_clusterStack.isEmpty() == isLayoutRoot(block));
 
-    if (block->isRenderView()) {
-        prepareRenderViewInfo(toRenderView(block));
+    if (isLayoutRoot(block)) {
+        prepareRenderViewInfo();
     } else if (block == currentCluster()->m_root) {
         // Ignore beginLayout on the same block twice.
         // This can happen with paginated overflow.
@@ -125,7 +133,7 @@ void FastTextAutosizer::inflateListItem(RenderListItem* listItem, RenderListMark
 void FastTextAutosizer::endLayout(RenderBlock* block)
 {
     ASSERT(enabled());
-    if (block->isRenderView()) {
+    if (isLayoutRoot(block)) {
         m_superclusters.clear();
 #ifndef NDEBUG
         m_blocksThatHaveBegunLayout.clear();
@@ -135,7 +143,7 @@ void FastTextAutosizer::endLayout(RenderBlock* block)
     if (currentCluster()->m_root == block)
         m_clusterStack.removeLast();
 
-    ASSERT(m_clusterStack.isEmpty() == block->isRenderView());
+    ASSERT(m_clusterStack.isEmpty() == isLayoutRoot(block));
 }
 
 void FastTextAutosizer::inflate(RenderBlock* block)
@@ -162,8 +170,9 @@ bool FastTextAutosizer::enabled()
     return m_document->settings()->textAutosizingEnabled();
 }
 
-void FastTextAutosizer::prepareRenderViewInfo(RenderView* renderView)
+void FastTextAutosizer::prepareRenderViewInfo()
 {
+    RenderView* renderView = toRenderView(m_document->renderer());
     bool horizontalWritingMode = isHorizontalWritingMode(renderView->style()->writingMode());
 
     Frame* mainFrame = m_document->page()->mainFrame();
@@ -254,12 +263,14 @@ FastTextAutosizer::Cluster* FastTextAutosizer::maybeCreateCluster(const RenderBl
         return 0;
 
     Cluster* parentCluster = m_clusterStack.isEmpty() ? 0 : currentCluster();
-    ASSERT(parentCluster || block->isRenderView());
+    ASSERT(parentCluster || isLayoutRoot(block));
 
     // Create clusters to suppress / unsuppress autosizing based on containerShouldBeAutosized.
     bool containerCanAutosize = TextAutosizer::containerShouldBeAutosized(block);
     bool parentClusterCanAutosize = parentCluster && parentCluster->m_autosize;
-    bool createClusterThatMightAutosize = mightBeWiderOrNarrowerDescendant(block) || TextAutosizer::isIndependentDescendant(block);
+    bool createClusterThatMightAutosize = isLayoutRoot(block)
+        || mightBeWiderOrNarrowerDescendant(block)
+        || TextAutosizer::isIndependentDescendant(block);
 
     // If the container would not alter the m_autosize bit, it doesn't need to be a cluster.
     if (!createClusterThatMightAutosize && containerCanAutosize == parentClusterCanAutosize)
@@ -308,7 +319,12 @@ float FastTextAutosizer::clusterMultiplier(Cluster* cluster)
 {
     ASSERT(m_renderViewInfoPrepared);
     if (!cluster->m_multiplier) {
-        if (TextAutosizer::isIndependentDescendant(cluster->m_root) || isWiderDescendant(cluster) || isNarrowerDescendant(cluster)) {
+
+        if (isLayoutRoot(cluster->m_root)
+            || TextAutosizer::isIndependentDescendant(cluster->m_root)
+            || isWiderDescendant(cluster)
+            || isNarrowerDescendant(cluster)) {
+
             if (cluster->m_supercluster) {
                 cluster->m_multiplier = superclusterMultiplier(cluster->m_supercluster);
             } else if (clusterHasEnoughTextToAutosize(cluster)) {
--- third_party/WebKit/Source/core/rendering/FastTextAutosizer.h
+++ third_party/WebKit/Source/core/rendering/FastTextAutosizer.h
@@ -171,7 +171,7 @@ private:
     void endLayout(RenderBlock*);
     void inflate(RenderBlock*);
     bool enabled();
-    void prepareRenderViewInfo(RenderView*);
+    void prepareRenderViewInfo();
     bool isFingerprintingCandidate(const RenderBlock*);
     bool clusterHasEnoughTextToAutosize(Cluster*);
     bool clusterWouldHaveEnoughTextToAutosize(const RenderBlock*);
@@ -187,6 +187,7 @@ private:
     bool mightBeWiderOrNarrowerDescendant(const RenderBlock*);
     bool isWiderDescendant(Cluster*);
     bool isNarrowerDescendant(Cluster*);
+    bool isLayoutRoot(const RenderBlock*) const;
 
     Cluster* currentCluster() const;
 
