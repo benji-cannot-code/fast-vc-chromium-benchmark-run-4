d2fad74f3010972c9e7b36065ae7c994bd6db59e
gcasto@chromium.org <gcasto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-15 03:38:57 +0000

    [Password Generation] Update UI to match final mocks.
    Specifically
      - Change the text of the generation popup.
      - Add a suggestion label.
      - Show editing popup when password is focused after generation.
      - Display password when it is focused after generation.
      - Various other small UI cleanups.
    
    BUG=318977
    
    Review URL: https://codereview.chromium.org/147533005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251494 0039d316-1c4b-4281-b951-d872f2087c98

--- components/autofill/content/renderer/password_generation_agent.cc
+++ components/autofill/content/renderer/password_generation_agent.cc
@@ -283,7 +283,9 @@ void PasswordGenerationAgent::DetermineGenerationElement() {
 }
 
 void PasswordGenerationAgent::FocusedNodeChanged(const blink::WebNode& node) {
-  // TODO(gcasto): Re-hide generation_element text.
+  if (!generation_element_.isNull())
+    generation_element_.setShouldRevealPassword(false);
+
   if (node.isNull() || !node.isElementNode())
     return;
 
@@ -296,7 +298,7 @@ void PasswordGenerationAgent::FocusedNodeChanged(const blink::WebNode& node) {
     return;
 
   if (password_is_generated_) {
-    // TODO(gcasto): Make characters visible.
+    generation_element_.setShouldRevealPassword(true);
     ShowEditingPopup();
   }
 
@@ -320,8 +322,11 @@ bool PasswordGenerationAgent::TextDidChangeInTextField(
           password_generation::PASSWORD_DELETED);
     }
 
+    // Do not treat the password as generated.
     // TODO(gcasto): Set PasswordForm::type in the browser to TYPE_NORMAL.
     password_is_generated_ = false;
+    generation_element_.setShouldRevealPassword(false);
+
     // Offer generation again.
     ShowGenerationPopup();
   } else if (!password_is_generated_) {
@@ -360,8 +365,10 @@ void PasswordGenerationAgent::ShowEditingPopup() {
       GetScaledBoundingBox(render_view_->GetWebView()->pageScaleFactor(),
                            &generation_element_);
 
-  Send(new AutofillHostMsg_ShowPasswordEditingPopup(routing_id(),
-                                                    bounding_box_scaled));
+  Send(new AutofillHostMsg_ShowPasswordEditingPopup(
+      routing_id(),
+      bounding_box_scaled,
+      *possible_account_creation_form_));
 
   password_generation::LogPasswordGenerationEvent(
       password_generation::EDITING_POPUP_SHOWN);
