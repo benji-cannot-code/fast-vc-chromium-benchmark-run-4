94f7b9f386a7e0acef6ecea86d5508ca36242e4e
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-15 05:23:29 +0000

    Don't raise for GYP_GENERATORS=dump_dependency_json, don't warn when using gyp_env
    R=thakis@chromium.org
    
    Review URL: https://codereview.chromium.org/167943002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251536 0039d316-1c4b-4281-b951-d872f2087c98

--- build/landmines.py
+++ build/landmines.py
@@ -16,7 +16,6 @@ build is clobbered.
 """
 
 import difflib
-import gyp_helper
 import logging
 import optparse
 import os
@@ -114,7 +113,9 @@ def process_options():
 
 def main():
   landmine_scripts = process_options()
-  gyp_helper.apply_chromium_gyp_env()
+
+  if landmine_utils.builder() == 'dump_dependency_json':
+    return 0
 
   for target in ('Debug', 'Release', 'Debug_x64', 'Release_x64'):
     landmines = []
