c3a3511ecb842ad6828f3c33564458706743dc8c
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-15 08:46:18 +0000

    Web MIDI: enable important logs in release build
    To investigate a reason that Web MIDI goes not working recently in ChromeOS,
    this change enable some important log messages in release build.
    Also, this change make it impossible to disable MIDIManager for ChromeOS.
    
    BUG=335865
    
    Review URL: https://codereview.chromium.org/166033004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251564 0039d316-1c4b-4281-b951-d872f2087c98

--- media/midi/midi_manager_alsa.cc
+++ media/midi/midi_manager_alsa.cc
@@ -146,12 +146,12 @@ bool MidiManagerAlsa::Initialize() {
     snd_ctl_t* handle;
     int err = snd_ctl_open(&handle, id.c_str(), 0);
     if (err != 0) {
-      DLOG(ERROR) << "snd_ctl_open fails: " << snd_strerror(err);
+      LOG(ERROR) << "snd_ctl_open fails: " << snd_strerror(err);
       continue;
     }
     err = snd_ctl_card_info(handle, card);
     if (err != 0) {
-      DLOG(ERROR) << "snd_ctl_card_info fails: " << snd_strerror(err);
+      LOG(ERROR) << "snd_ctl_card_info fails: " << snd_strerror(err);
       snd_ctl_close(handle);
       continue;
     }
@@ -172,7 +172,7 @@ bool MidiManagerAlsa::Initialize() {
       scoped_refptr<MidiDeviceInfo> port = new MidiDeviceInfo(
           this, id, card, output ? midi_out : midi_in, device);
       if (!port->IsOpened()) {
-        DLOG(ERROR) << "MidiDeviceInfo open fails";
+        LOG(ERROR) << "MidiDeviceInfo open fails";
         continue;
       }
       if (input) {
@@ -188,7 +188,7 @@ bool MidiManagerAlsa::Initialize() {
   }
 
   if (pipe(pipe_fd_) < 0) {
-    DPLOG(ERROR) << "pipe() failed";
+    PLOG(ERROR) << "pipe() failed";
     return false;
   }
   event_thread_.Start();
@@ -267,7 +267,7 @@ void MidiManagerAlsa::EventReset() {
 
 void MidiManagerAlsa::EventLoop() {
   if (HANDLE_EINTR(poll(&poll_fds_[0], poll_fds_.size(), -1)) < 0) {
-    DPLOG(ERROR) << "Couldn't poll(). Stop to poll input MIDI devices.";
+    PLOG(ERROR) << "Couldn't poll(). Stop to poll input MIDI devices.";
     // TODO(toyoshim): Handle device disconnection, and try to reconnect?
     return;
   }
