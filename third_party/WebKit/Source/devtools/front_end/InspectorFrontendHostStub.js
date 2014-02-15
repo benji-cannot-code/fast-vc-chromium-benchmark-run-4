5d9181e4f45df91fd934626b3b455eb326c5e3bf
dgozman@chromium.org <dgozman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-15 18:41:24 +0000

    [DevTools] Handle zoom via InspectorFrontendHost.
    TBR=pfeldman@chromium.org
    BUG=343816
    
    Review URL: https://codereview.chromium.org/166713002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167241 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/InspectorFrontendHostStub.js
+++ third_party/WebKit/Source/devtools/front_end/InspectorFrontendHostStub.js
@@ -211,6 +211,18 @@ WebInspector.InspectorFrontendHostStub.prototype = {
         return 1;
     },
 
+    zoomIn: function()
+    {
+    },
+
+    zoomOut: function()
+    {
+    },
+
+    resetZoom: function()
+    {
+    },
+
     /**
      * @return {boolean}
      */
