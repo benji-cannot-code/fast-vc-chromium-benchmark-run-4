22a73fa0f190c36a11ddd39d549f0ad42881cde3
weitaosu@chromium.org <weitaosu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-15 08:20:58 +0000

    Clear the secrets reg key as well as the clients reg key in clearPairedClients.
    BUG=325567
    
    Review URL: https://codereview.chromium.org/164183009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251558 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/pairing_registry_delegate_win.cc
+++ remoting/host/pairing_registry_delegate_win.cc
@@ -150,6 +150,8 @@ bool PairingRegistryDelegateWin::DeleteAll() {
     return false;
   }
 
+  // Enumerate and delete the values in the privileged and unprivileged keys
+  // separately in case they get out of sync.
   bool success = true;
   DWORD count = unprivileged_.GetValueCount();
   while (count > 0) {
@@ -161,6 +163,18 @@ bool PairingRegistryDelegateWin::DeleteAll() {
 
     success = success && (result == ERROR_SUCCESS);
     count = unprivileged_.GetValueCount();
+  }
+
+  count = privileged_.GetValueCount();
+  while (count > 0) {
+    // presubmit: allow wstring
+    std::wstring value_name;
+    LONG result = privileged_.GetValueNameAt(0, &value_name);
+    if (result == ERROR_SUCCESS)
+      result = privileged_.DeleteValue(value_name.c_str());
+
+    success = success && (result == ERROR_SUCCESS);
+    count = privileged_.GetValueCount();
   }
 
   return success;
