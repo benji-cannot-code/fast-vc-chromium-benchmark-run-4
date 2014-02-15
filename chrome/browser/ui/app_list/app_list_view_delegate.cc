8f342a46b44accfe0cf4077c14cb6722d6b95909
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-15 03:22:27 +0000

    Shorter timeout to auto-launch from the voice search.
    BUG=None
    R=xiyuan@chromium.org
    TEST=manually
    
    Review URL: https://codereview.chromium.org/167413002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251492 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/app_list/app_list_view_delegate.cc
+++ chrome/browser/ui/app_list/app_list_view_delegate.cc
@@ -52,7 +52,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-const int kAutoLaunchDefaultTimeoutSec = 3;
+const int kAutoLaunchDefaultTimeoutMilliSec = 50;
 
 #if defined(OS_WIN)
 void CreateShortcutInWebAppDir(
@@ -333,8 +333,8 @@ void AppListViewDelegate::OnSpeechResult(const base::string16& result,
                                          bool is_final) {
   speech_ui_->SetSpeechResult(result, is_final);
   if (is_final) {
-    auto_launch_timeout_ = base::TimeDelta::FromSeconds(
-        kAutoLaunchDefaultTimeoutSec);
+    auto_launch_timeout_ = base::TimeDelta::FromMilliseconds(
+        kAutoLaunchDefaultTimeoutMilliSec);
     model_->search_box()->SetText(result);
   }
 }
