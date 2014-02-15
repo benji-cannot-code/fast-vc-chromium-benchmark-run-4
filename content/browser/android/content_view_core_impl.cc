74970b25e0cedeb4f01f47b0905610ed46cd890e
b.kelemen@samsung.com <b.kelemen@samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-15 00:33:23 +0000

    Partial revert of "Restart fullscreen video playback when switching back from background"
    This CL partially reverts r242570 and r243346.
    
    The functionality added in those patches has been removed while some refactoring part
    has been kept because those make sense and also because some code came later would have
    to be moved otherwise.
    
    The suspend/resume fullscreen video logic does not work well when the browser
    activity is destroyed when it is in the background. Unfortunately I could not
    find a way to make it work for that case yet so it's better remove it.
    
    BUG=323697
    
    Review URL: https://codereview.chromium.org/157783004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251455 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/android/content_view_core_impl.cc
+++ content/browser/android/content_view_core_impl.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "cc/layers/layer.h"
 #include "cc/output/begin_frame_args.h"
-#include "content/browser/android/content_video_view.h"
 #include "content/browser/android/gesture_event_type.h"
 #include "content/browser/android/interstitial_page_delegate_android.h"
 #include "content/browser/android/load_url_params.h"
@@ -417,7 +416,6 @@ void ContentViewCoreImpl::OnShow(JNIEnv* env, jobject obj) {
 
 void ContentViewCoreImpl::Show() {
   GetWebContents()->WasShown();
-  ResumeVideo();
 }
 
 void ContentViewCoreImpl::Hide() {
@@ -429,13 +427,6 @@ void ContentViewCoreImpl::PauseVideo() {
   RenderViewHost* host = web_contents_->GetRenderViewHost();
   if (host)
     host->Send(new ViewMsg_PauseVideo(host->GetRoutingID()));
-  if (ContentVideoView::GetInstance())
-    ContentVideoView::GetInstance()->SuspendFullscreen();
-}
-
-void ContentViewCoreImpl::ResumeVideo() {
-  if (ContentVideoView::GetInstance())
-    ContentVideoView::GetInstance()->ResumeFullscreenIfSuspended();
 }
 
 void ContentViewCoreImpl::PauseOrResumeGeolocation(bool should_pause) {
