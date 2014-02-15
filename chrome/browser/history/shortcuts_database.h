3a72a20b7fbbb848ee3aed227dd802039c0df74c
yfriedman@chromium.org <yfriedman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-15 01:37:58 +0000

    Revert 251442 "Migrate old Shortcuts DB data to conform to new t..."
    Broke tests: http://build.chromium.org/p/chromium.linux/builders/Android%20Tests%20%28dbg%29/builds/18614/steps/unit_tests/logs/stdio#failure4
    
    > Migrate old Shortcuts DB data to conform to new type values
    > 
    > All new suggestion types will be a Google Search URL with https. In some cases,
    > url may contain additional identifying parameter.
    > 
    > BUG=339270
    > TBR=brettw@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/134853004
    
    TBR=skanuj@chromium.org
    
    Review URL: https://codereview.chromium.org/163613012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251473 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/shortcuts_database.h
+++ chrome/browser/history/shortcuts_database.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string16.h"
 #include "chrome/browser/history/shortcuts_backend.h"
 #include "sql/connection.h"
-#include "sql/meta_table.h"
 #include "url/gurl.h"
 
 namespace history {
@@ -82,8 +81,6 @@ class ShortcutsDatabase : public base::RefCountedThreadSafe<ShortcutsDatabase> {
   // The sql database. Not valid until Init is called.
   sql::Connection db_;
   base::FilePath database_path_;
-
-  sql::MetaTable meta_table_;
 
   static const base::FilePath::CharType kShortcutsDatabaseName[];
 
