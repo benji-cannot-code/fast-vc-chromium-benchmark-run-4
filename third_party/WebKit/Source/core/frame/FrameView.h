e1c1207e325d125c47aee6d920edd1e334973c14
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-15 18:33:52 +0000

    Merge paintControlTints into updateControlTints and remove the Internals call.
    Exposing this for these tests is crazy since there's currently only one
    caller left of updateControlTints. So whatever code these tests were
    hitting can't be hit this way anymore.
    
    BUG=343758
    
    Review URL: https://codereview.chromium.org/166043007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167239 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/FrameView.h
+++ third_party/WebKit/Source/core/frame/FrameView.h
@@ -334,8 +334,6 @@ private:
     void applyOverflowToViewportAndSetRenderer(RenderObject*, ScrollbarMode& hMode, ScrollbarMode& vMode);
     void updateOverflowStatus(bool horizontalOverflow, bool verticalOverflow);
 
-    void paintControlTints();
-
     void updateCounters();
     void autoSizeIfEnabled();
     void forceLayoutParentViewIfNeeded();
