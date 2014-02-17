fcac0a5c0aa07aefd97d222567c12b347504a2fc
tommi@chromium.org <tommi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-17 12:09:53 +0000

    Allow individual volume control of remote webrtc audio tracks. This fixes an issue where remote audio tracks would all share the same volume state.  Although there's still a limitation in the audio stack, this brings us a step closer to spec compliance.
    BUG=343998
    TEST=I ran the currently disabled, yet functional WebRtc tests in content_unittests.  Also created my own test HTML page that renders the same media stream in multiple video tags and verified that individual volume control worked and that the volume could be boosted up to a max factor of 10.
    NOTRY=True
    
    Review URL: https://codereview.chromium.org/135393009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251663 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/webrtc_audio_device_unittest.cc
+++ content/renderer/media/webrtc_audio_device_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/strings/stringprintf.h"
 #include "base/test/test_timeouts.h"
+#include "content/renderer/media/mock_media_stream_dependency_factory.h"
 #include "content/renderer/media/webrtc/webrtc_local_audio_track_adapter.h"
 #include "content/renderer/media/webrtc_audio_capturer.h"
 #include "content/renderer/media/webrtc_audio_device_impl.h"
@@ -539,11 +540,14 @@ TEST_F(MAYBE_WebRTCAudioDeviceTest, MAYBE_StartPlayout) {
   EXPECT_EQ(0, external_media->RegisterExternalMediaProcessing(
       ch, webrtc::kPlaybackPerChannel, *media_process.get()));
 
+  scoped_refptr<webrtc::MediaStreamInterface> media_stream(
+      new talk_base::RefCountedObject<MockMediaStream>("label"));
+
   EXPECT_EQ(0, base->StartPlayout(ch));
   scoped_refptr<WebRtcAudioRenderer> renderer(
-      CreateDefaultWebRtcAudioRenderer(kRenderViewId));
+      CreateDefaultWebRtcAudioRenderer(kRenderViewId, media_stream));
   scoped_refptr<MediaStreamAudioRenderer> proxy(
-      renderer->CreateSharedAudioRendererProxy());
+      renderer->CreateSharedAudioRendererProxy(media_stream));
   EXPECT_TRUE(webrtc_audio_device->SetAudioRenderer(renderer.get()));
   proxy->Start();
   proxy->Play();
@@ -709,10 +713,12 @@ TEST_F(MAYBE_WebRTCAudioDeviceTest, MAYBE_PlayLocalFile) {
   int ch = base->CreateChannel();
   EXPECT_NE(-1, ch);
   EXPECT_EQ(0, base->StartPlayout(ch));
+  scoped_refptr<webrtc::MediaStreamInterface> media_stream(
+      new talk_base::RefCountedObject<MockMediaStream>("label"));
   scoped_refptr<WebRtcAudioRenderer> renderer(
-      CreateDefaultWebRtcAudioRenderer(kRenderViewId));
+      CreateDefaultWebRtcAudioRenderer(kRenderViewId, media_stream));
   scoped_refptr<MediaStreamAudioRenderer> proxy(
-      renderer->CreateSharedAudioRendererProxy());
+      renderer->CreateSharedAudioRendererProxy(media_stream));
   EXPECT_TRUE(webrtc_audio_device->SetAudioRenderer(renderer.get()));
   proxy->Start();
   proxy->Play();
@@ -815,10 +821,12 @@ TEST_F(MAYBE_WebRTCAudioDeviceTest, MAYBE_FullDuplexAudioWithAGC) {
   EXPECT_EQ(0, network->RegisterExternalTransport(ch, *transport.get()));
   EXPECT_EQ(0, base->StartPlayout(ch));
   EXPECT_EQ(0, base->StartSend(ch));
+  scoped_refptr<webrtc::MediaStreamInterface> media_stream(
+      new talk_base::RefCountedObject<MockMediaStream>("label"));
   scoped_refptr<WebRtcAudioRenderer> renderer(
-      CreateDefaultWebRtcAudioRenderer(kRenderViewId));
+      CreateDefaultWebRtcAudioRenderer(kRenderViewId, media_stream));
   scoped_refptr<MediaStreamAudioRenderer> proxy(
-      renderer->CreateSharedAudioRendererProxy());
+      renderer->CreateSharedAudioRendererProxy(media_stream));
   EXPECT_TRUE(webrtc_audio_device->SetAudioRenderer(renderer.get()));
   proxy->Start();
   proxy->Play();
@@ -922,11 +930,13 @@ TEST_F(MAYBE_WebRTCAudioDeviceTest, MAYBE_WebRtcPlayoutSetupTime) {
   scoped_ptr<MockWebRtcAudioRendererSource> renderer_source(
       new MockWebRtcAudioRendererSource(&event));
 
+  scoped_refptr<webrtc::MediaStreamInterface> media_stream(
+      new talk_base::RefCountedObject<MockMediaStream>("label"));
   scoped_refptr<WebRtcAudioRenderer> renderer(
-      CreateDefaultWebRtcAudioRenderer(kRenderViewId));
+      CreateDefaultWebRtcAudioRenderer(kRenderViewId, media_stream));
   renderer->Initialize(renderer_source.get());
   scoped_refptr<MediaStreamAudioRenderer> proxy(
-      renderer->CreateSharedAudioRendererProxy());
+      renderer->CreateSharedAudioRendererProxy(media_stream));
   proxy->Start();
 
   // Start the timer and playout.
--- content/test/webrtc_audio_device_test.h
+++ content/test/webrtc_audio_device_test.h
@@ -32,6 +32,7 @@ class URLRequestContext;
 }
 
 namespace webrtc {
+class MediaStreamInterface;
 class VoENetwork;
 }
 
@@ -134,7 +135,8 @@ class MAYBE_WebRTCAudioDeviceTest : public ::testing::Test,
   void SetAudioHardwareConfig(media::AudioHardwareConfig* hardware_config);
 
   scoped_refptr<WebRtcAudioRenderer> CreateDefaultWebRtcAudioRenderer(
-      int render_view_id);
+      int render_view_id,
+      const scoped_refptr<webrtc::MediaStreamInterface>& media_stream);
 
  protected:
   void InitializeIOThread(const char* thread_name);
