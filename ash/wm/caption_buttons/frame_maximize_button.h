5820f4e8d53038ace6a873f99e286cfd5de35a6e
glider@chromium.org <glider@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-17 09:32:51 +0000

    Revert 251589 "Fixes crashes wrt to the maximize button when:"
    > Fixes crashes wrt to the maximize button when:
    > - A user hovers the minimize button with the mouse then taps the snap left / snap right button
    > - When the MaximizeBubbleFrameState computed by GetMaximizeBubbleFrameState() changes while the maximize button help bubble is visible due to the work area bounds changing (See comment #6 in bug)
    > 
    > BUG=342867
    > TEST=FrameMaximizeButtonTest.MaximizeBubbleMouseHoverAndTouch
    > 
    > Review URL: https://codereview.chromium.org/166543004
    
    TBR=pkotwicz@chromium.org
    
    Review URL: https://codereview.chromium.org/169433002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251647 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/caption_buttons/frame_maximize_button.h
+++ ash/wm/caption_buttons/frame_maximize_button.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_export.h"
 #include "ash/wm/caption_buttons/caption_button_types.h"
 #include "ash/wm/caption_buttons/frame_caption_button.h"
-#include "ash/wm/window_state_observer.h"
 #include "ash/wm/workspace/snap_types.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/observer_list.h"
@@ -34,8 +33,7 @@ class MaximizeBubbleController;
 // Button used for the maximize control on the frame. Handles snapping logic.
 class ASH_EXPORT FrameMaximizeButton : public FrameCaptionButton,
                                        public views::WidgetObserver,
-                                       public aura::WindowObserver,
-                                       public wm::WindowStateObserver {
+                                       public aura::WindowObserver {
  public:
   FrameMaximizeButton(views::ButtonListener* listener,
                       views::Widget* frame);
@@ -69,10 +67,6 @@ class ASH_EXPORT FrameMaximizeButton : public FrameCaptionButton,
                                        const void* key,
                                        intptr_t old) OVERRIDE;
   virtual void OnWindowDestroying(aura::Window* window) OVERRIDE;
-
-  // wm::WindowStateObserver override:
-  virtual void OnPostWindowShowTypeChange(wm::WindowState* window_state,
-                                          wm::WindowShowType old_type) OVERRIDE;
 
   // WidgetObserver overrides:
   virtual void OnWidgetActivationChanged(views::Widget* widget,
