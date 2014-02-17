ccd4e7dbdd4dc12ec6a5a73fb344028f9abfaf8d
ch.dumez@samsung.com <ch.dumez@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-17 04:12:53 +0000

    Add initial Screen Orientation API support: IDL changes
    Add initial Screen Orientation API support: IDL changes:
    https://dvcs.w3.org/hg/screen-orientation/raw-file/tip/Overview.html
    
    This new API is implemented behind a "ScreenOrientation" runtime-enabled flag.
    The intent to implement was sent to blink-dev:
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/lgUq8K9IGlU
    
    This CL changes Screen to be an EventTarget. While the Screen Orientation API
    has been implemented behind a RuntimeEnabled flag, there is no way of having
    inheritance depend on the flag's value. The Web exposed effect of this is that
    people will now be able to dispatch custom events on the window.screen
    interface, which in itself is harmless.
    
    R=abarth
    BUG=162827
    TEST=screen_orientation/screenorientation-api.html
    
    Review URL: https://codereview.chromium.org/168763002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167259 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/Screen.h
+++ third_party/WebKit/Source/core/frame/Screen.h
@@ -32,7 +32,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define Screen_h
 
 #include "bindings/v8/ScriptWrappable.h"
+#include "core/events/EventTarget.h"
 #include "core/frame/DOMWindowProperty.h"
+#include "platform/Supplementable.h"
 #include "wtf/PassRefPtr.h"
 #include "wtf/RefCounted.h"
 
@@ -40,9 +42,10 @@ namespace WebCore {
 
     class Frame;
 
-    class Screen FINAL : public ScriptWrappable, public RefCounted<Screen>, public DOMWindowProperty {
+    class Screen FINAL : public ScriptWrappable, public RefCounted<Screen>, public EventTargetWithInlineData, public DOMWindowProperty, public Supplementable<Screen> {
+        REFCOUNTED_EVENT_TARGET(Screen);
     public:
-        static PassRefPtr<Screen> create(Frame *frame) { return adoptRef(new Screen(frame)); }
+        static PassRefPtr<Screen> create(Frame* frame) { return adoptRef(new Screen(frame)); }
 
         unsigned height() const;
         unsigned width() const;
@@ -52,6 +55,10 @@ namespace WebCore {
         int availTop() const;
         unsigned availHeight() const;
         unsigned availWidth() const;
+
+        // EventTarget.
+        virtual const AtomicString& interfaceName() const OVERRIDE;
+        virtual ExecutionContext* executionContext() const OVERRIDE;
 
     private:
         explicit Screen(Frame*);
