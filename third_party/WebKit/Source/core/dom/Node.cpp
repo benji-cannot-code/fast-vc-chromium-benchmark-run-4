ee22e5cd840732805e444657bfa7f35694cd9cf4
ch.dumez@samsung.com <ch.dumez@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-17 00:53:57 +0000

    Drop [LegacyImplementedInBaseClass] from ChildNode IDL interface
    Drop [LegacyImplementedInBaseClass] from ChildNode IDL interface and move the
    related API from Node to a new ChildNode class. All the callers of Node's
    previousElementSibling() / nextElementSibling() were already previously updated
    to use ElementTraversal instead for consistency.
    
    R=haraken, adamk
    BUG=344224
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/168673002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167249 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Node.cpp
+++ third_party/WebKit/Source/core/dom/Node.cpp
@@ -2550,16 +2550,6 @@ void Node::setCustomElementState(CustomElementState newState)
         setNeedsStyleRecalc(SubtreeStyleChange); // :unresolved has changed
 }
 
-Element* Node::previousElementSibling() const
-{
-    return ElementTraversal::previousSibling(*this);
-}
-
-Element* Node::nextElementSibling() const
-{
-    return ElementTraversal::nextSibling(*this);
-}
-
 } // namespace WebCore
 
 #ifndef NDEBUG
