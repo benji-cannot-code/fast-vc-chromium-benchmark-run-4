3dcdd2364a81ff5d74190b83c2b6a1a1f6afd1cd
mikhail.pozdnyakov@intel.com <mikhail.pozdnyakov@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-18 11:36:47 +0000

    Optimize StylePropertySet::findPropertyIndex() to improve CSS properties performance
    The Intel VTune Amplifier profiler pointed out the
    'StylePropertySet::findPropertyIndex()' method as a
    biggest hotspot when running CSS properties performance
    tests.
    
    The reason for the slowness was a significant amount
    of unneeded work that this method was doing, e.g.:
    - Checking whether 'PropertySet' is mutable on every
      cycle iteration.
    - Working with a proxy 'PropertyReference' object
      instead of direct access to the 'PropertySet'
      own data members.
    
    The proposed optimization gives a significant performance
    gain, for example the 'CSS/CSSPropertySetterGetter.html'
    performance test shows the following.
    
    Before the optimization applied:
    avg 3860.903172938448 runs/s
    median 3954.754079910417 runs/s
    stdev 337.5965136262694 runs/s
    min 3170.2197482668807 runs/s
    max 4218.408078283424 runs/s
    
    After the optimization applied:
    avg 4522.43958295528 runs/s
    median 4524.851984183224 runs/s
    stdev 14.069412192849006 runs/s
    min 4481.849781528119 runs/s
    max 4545.219771571736 runs/s
    
    Performance gain for the average value is approx. 17% (Linux desktop x64).
    
    Review URL: https://codereview.chromium.org/169453006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167325 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/StylePropertySet.h
+++ third_party/WebKit/Source/core/css/StylePropertySet.h
@@ -150,6 +150,7 @@ public:
 
     const CSSValue** valueArray() const;
     const StylePropertyMetadata* metadataArray() const;
+    int findPropertyIndex(CSSPropertyID) const;
 
     void* m_storage;
 
@@ -209,6 +210,7 @@ public:
     void parseDeclaration(const String& styleDeclaration, StyleSheetContents* contextStyleSheet);
 
     CSSStyleDeclaration* ensureCSSStyleDeclaration();
+    int findPropertyIndex(CSSPropertyID) const;
 
     Vector<CSSProperty, 4> m_propertyVector;
 
@@ -266,6 +268,13 @@ inline void StylePropertySet::deref()
         delete toMutableStylePropertySet(this);
     else
         delete toImmutableStylePropertySet(this);
+}
+
+inline int StylePropertySet::findPropertyIndex(CSSPropertyID propertyID) const
+{
+    if (m_isMutable)
+        return toMutableStylePropertySet(this)->findPropertyIndex(propertyID);
+    return toImmutableStylePropertySet(this)->findPropertyIndex(propertyID);
 }
 
 } // namespace WebCore
