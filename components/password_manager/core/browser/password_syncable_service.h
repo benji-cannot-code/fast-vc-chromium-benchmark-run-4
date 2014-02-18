58d13af8c3b64b61c161cd3d85c172c7f7e5d9ce
vabr@chromium.org <vabr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-18 20:19:05 +0000

    Componentize PasswordSyncableService
    This just moves the files and adds needed dependencies.
    
    BUG=344456
    TBR=atwilson@chromium.org,rvargas@chromium.org
    
    Review URL: https://codereview.chromium.org/170313003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251818 0039d316-1c4b-4281-b951-d872f2087c98

--- components/password_manager/core/browser/password_syncable_service.h
+++ components/password_manager/core/browser/password_syncable_service.h
@@ -1,10 +1,14 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2013 The Chromium Authors. All rights reserved.
+// Copyright 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_PASSWORD_MANAGER_PASSWORD_SYNCABLE_SERVICE_H__
-#define CHROME_BROWSER_PASSWORD_MANAGER_PASSWORD_SYNCABLE_SERVICE_H__
+#ifndef COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_PASSWORD_SYNCABLE_SERVICE_H__
+#define COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_PASSWORD_SYNCABLE_SERVICE_H__
+
+#if !defined(PASSWORD_MANAGER_ENABLE_SYNC)
+#error "Only build this file when sync is enabled in Password Manager."
+#endif
 
 #include <string>
 #include <vector>
@@ -114,4 +118,4 @@ void PasswordFromSpecifics(const sync_pb::PasswordSpecificsData& password,
 // |password| entry.
 std::string MakePasswordSyncTag(const sync_pb::PasswordSpecificsData& password);
 
-#endif  // CHROME_BROWSER_PASSWORD_MANAGER_PASSWORD_SYNCABLE_SERVICE_H__
+#endif  // COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_PASSWORD_SYNCABLE_SERVICE_H__
