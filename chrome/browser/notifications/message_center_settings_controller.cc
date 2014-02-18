17456c6aa23c34949e1e34659d9dd79d56bf4774
tmdiep@chromium.org <tmdiep@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-18 12:44:43 +0000

    Notification settings for ephemeral apps
    Only running ephemeral apps will appear in the notification settings
    pane. This avoids spamming the list with inactive cached ephemeral apps.
    
    The setting will also be persisted across cache evictions, so that
    notifications will still be disabled when the ephemeral app is
    re-cached at a later date.
    
    BUG=343453
    TEST=browser_tests (EphemeralAppBrowserTest.StickyNotificationSettings)
    
    Review URL: https://codereview.chromium.org/163953012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251754 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/notifications/message_center_settings_controller.cc
+++ chrome/browser/notifications/message_center_settings_controller.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/extensions/app_icon_loader_impl.h"
 #include "chrome/browser/extensions/extension_service.h"
+#include "chrome/browser/extensions/extension_util.h"
 #include "chrome/browser/favicon/favicon_service.h"
 #include "chrome/browser/favicon/favicon_service_factory.h"
 #include "chrome/browser/history/history_types.h"
@@ -227,6 +228,12 @@ void MessageCenterSettingsController::GetNotifierList(
     const extensions::Extension* extension = iter->get();
     if (!extension->HasAPIPermission(
             extensions::APIPermission::kNotification)) {
+      continue;
+    }
+
+    // Exclude cached ephemeral apps that are not currently running.
+    if (extension->is_ephemeral() &&
+        extensions::util::IsExtensionIdle(extension->id(), profile)) {
       continue;
     }
 
