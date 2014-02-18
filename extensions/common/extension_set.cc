a9a34115386ab4605434ca9d3db1e79d51616d00
rob@robwu.nl <rob@robwu.nl@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-18 20:08:45 +0000

    Optimize GetExtensionOrAppIDByURL
    When the function reaches this point in the code, then
    url.SchemeIs(kExtensionScheme) was false.
    So the condition will remain false in GetExtensionOrAppByURL,
    which makes this method equivalent to GetHostedAppByURL.
    Save some cycles by calling GetHostedAppByURL directly.
    
    Contributed by Rob Wu <rob@robwu.nl>
    
    BUG=
    
    Review URL: https://codereview.chromium.org/167493003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251811 0039d316-1c4b-4281-b951-d872f2087c98

--- extensions/common/extension_set.cc
+++ extensions/common/extension_set.cc
@@ -76,7 +76,7 @@ std::string ExtensionSet::GetExtensionOrAppIDByURL(const GURL& url) const {
   if (url.SchemeIs(kExtensionScheme))
     return url.host();
 
-  const Extension* extension = GetExtensionOrAppByURL(url);
+  const Extension* extension = GetHostedAppByURL(url);
   if (!extension)
     return std::string();
 
