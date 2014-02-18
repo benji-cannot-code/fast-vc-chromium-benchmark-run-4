678d66db437c0df20b2541fbcba5be898c52bead
dzhioev@chromium.org <dzhioev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-18 16:23:21 +0000

    CrOs first-run UI polished.
    * Added shadows for bubbles.
    * Make app window to set its size automatically.
    * Prevented buttons blinking on hover/click/focus.
    
    BUG=342397
    TEST=manually
    
    Review URL: https://codereview.chromium.org/169173007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251767 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/first_run/app/background.js
+++ chrome/browser/resources/chromeos/first_run/app/background.js
@@ -5,12 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('main.html', {
-    'bounds': {
-      'width': 800,
-      'height': 555
-    },
     'frame': 'none',
     'resizable': false,
+    'hidden': true,
   }, function(appWindow) {
     appWindow.contentWindow.appWindow = appWindow;
   });
--- chrome/browser/resources/chromeos/first_run/app/main.js
+++ chrome/browser/resources/chromeos/first_run/app/main.js
@@ -3,17 +3,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/**
- * Global wallpaperManager reference useful for poking at from the console.
- */
-var wallpaperManager;
-
 function init() {
+  var content = $('greeting');
   chrome.firstRunPrivate.getLocalizedStrings(function(strings) {
     loadTimeData.data = strings;
     i18nTemplate.process(document, loadTimeData);
+    // Resizing and centering app's window.
+    var bounds = {};
+    bounds.width = content.offsetWidth;
+    bounds.height = content.offsetHeight;
+    bounds.left = Math.round(0.5 * (window.screen.availWidth - bounds.width));
+    bounds.top = Math.round(0.5 * (window.screen.availHeight - bounds.height));
+    appWindow.setBounds(bounds);
+    appWindow.show();
   });
-  var content = $('greeting');
   var closeButton = content.getElementsByClassName('close-button')[0];
   // Make close unfocusable by mouse.
   closeButton.addEventListener('mousedown', function(e) {
