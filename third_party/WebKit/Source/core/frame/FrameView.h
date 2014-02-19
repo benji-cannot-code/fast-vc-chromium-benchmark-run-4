97efab4bebaa15819217bbe0e3e798ff02831ae8
dstockwell@chromium.org <dstockwell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-19 05:36:11 +0000

    Revert of Fix compositing chicken and egg problem in updateControlTints. (https://codereview.chromium.org/168193002/)
    Reason for revert:
    Appears to cause a number of virtual/threaded tests to time out on windows.
    
    See: http://test-results.appspot.com/dashboards/flakiness_dashboard.html#group=%40ToT%20Blink&tests=virtual%2Fthreaded%2Fanimations%2F3d%2Freplace-filling-transform.html
    
    Original issue's description:
    > Fix compositing chicken and egg problem in updateControlTints.
    > 
    > updateControlTints reads compositingState, so it need to call the
    > new big hammer method that updates layout and compositing state,
    > updateLayoutAndStyleForPainting.
    > 
    > Also, move the update layout call to be inside updateControlTints
    > instead of FocusController::setActive so that we only do it if
    > we're actually on an OS that supportsControlTints.
    > 
    > BUG=343758
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=167384
    
    TBR=esprehn@chromium.org,abarth@chromium.org,vollick@chromium.org,ojan@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=343758
    
    Review URL: https://codereview.chromium.org/170803010
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167400 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/FrameView.h
+++ third_party/WebKit/Source/core/frame/FrameView.h
@@ -221,7 +221,6 @@ public:
 
     static double currentFrameTimeStamp() { return s_currentFrameTimeStamp; }
 
-    void updateLayoutAndStyleForPainting();
     void updateLayoutAndStyleIfNeededRecursive();
 
     void incrementVisuallyNonEmptyCharacterCount(unsigned);
