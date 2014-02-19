1f188924d35c12cc6353d513098058abce2067a8
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-19 21:11:46 +0000

    Remove the --ui-max-frames-pending flag.
    ChromeOS always sets this value to 1. Other Aura platforms don't use
    the cc scheduler yet so they don't care about this value. Once they
    do, they'll want to use 1 as well. So set it to 1 always.
    
    R=piman@chromium.org, piman
    BUG=344094
    
    Review URL: https://codereview.chromium.org/168073002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252091 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/chrome_restart_request.cc
+++ chrome/browser/chromeos/login/chrome_restart_request.cc
@@ -135,7 +135,6 @@ std::string DeriveCommandLine(const GURL& start_url,
       ::switches::kUIDisableDeadlineScheduling,
       ::switches::kUIDisableThreadedCompositing,
       ::switches::kUIEnableDeadlineScheduling,
-      ::switches::kUIMaxFramesPending,
       ::switches::kUIPrioritizeInGpuProcess,
 #if defined(USE_CRAS)
       ::switches::kUseCras,
