bbda5530c6e7ed7b5b5e608ac4f4152cffd993ce
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-19 00:32:40 +0000

    cc: Remove the --enable-partial-swap flag.
    R=piman
    BUG=
    
    Review URL: https://codereview.chromium.org/167953003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251898 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1111,7 +1111,6 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     cc::switches::kEnableImplSidePainting,
     cc::switches::kEnableLCDText,
     cc::switches::kEnableMapImage,
-    cc::switches::kEnablePartialSwap,
     cc::switches::kEnablePerTilePainting,
     cc::switches::kEnablePinchVirtualViewport,
     cc::switches::kEnableTopControlsPositionCalculation,
