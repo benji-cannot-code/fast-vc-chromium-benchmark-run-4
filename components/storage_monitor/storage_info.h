3432de543ac054127b9f6b069eefae403768a733
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-19 06:01:50 +0000

    StorageMonitor: Make StorageInfo a class.
    Review URL: https://codereview.chromium.org/168293002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251957 0039d316-1c4b-4281-b951-d872f2087c98

--- components/storage_monitor/storage_info.h
+++ components/storage_monitor/storage_info.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/strings/string16.h"
 
-struct StorageInfo {
+class StorageInfo {
  public:
   enum Type {
     // A removable mass storage device with a DCIM directory.
