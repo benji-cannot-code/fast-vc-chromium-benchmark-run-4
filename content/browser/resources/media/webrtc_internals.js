c57b42f429cea552a20a7de8acac438bbee8d7bb
jiayl@chromium.org <jiayl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-19 01:56:32 +0000

    Requests stats on webrtc-internals as soon as the initial peer connection records are received. This will get rid of the 1 second delay from waiting for setInternal to fire.
    BUG=
    
    Review URL: https://codereview.chromium.org/171263002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251916 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/resources/media/webrtc_internals.js
+++ content/browser/resources/media/webrtc_internals.js
@@ -100,12 +100,16 @@ function initialize() {
   chrome.send('finishedDOMLoad');
 
   // Requests stats from all peer connections every second.
-  window.setInterval(function() {
-    if (Object.keys(peerConnectionDataStore).length > 0)
-      chrome.send('getAllStats');
-  }, 1000);
+  window.setInterval(requestStats, 1000);
 }
 document.addEventListener('DOMContentLoaded', initialize);
+
+
+/** Sends a request to the browser to get peer connection statistics. */
+function requestStats() {
+  if (Object.keys(peerConnectionDataStore).length > 0)
+    chrome.send('getAllStats');
+}
 
 
 /**
@@ -222,6 +226,7 @@ function updateAllPeerConnections(data) {
       addPeerConnectionUpdate(peerConnection, log[j]);
     }
   }
+  requestStats();
 }
 
 
