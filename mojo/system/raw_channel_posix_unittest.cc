44152b5616669464f5a5dabbcaaff522157572ec
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-19 23:59:23 +0000

    Mojo: Make MessageInTransit more opaque.
    Still more to do, but incremental change is less scary.
    
    R=yzshen@chromium.org
    
    Review URL: https://codereview.chromium.org/172953003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252128 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/system/raw_channel_posix_unittest.cc
+++ mojo/system/raw_channel_posix_unittest.cc
@@ -68,9 +68,8 @@ bool WriteTestMessageToHandle(const embedder::PlatformHandle& handle,
   MessageInTransit* message = MakeTestMessage(num_bytes);
 
   ssize_t write_size = HANDLE_EINTR(
-     write(handle.fd, message, message->size_with_header_and_padding()));
-  bool result = write_size ==
-      static_cast<ssize_t>(message->size_with_header_and_padding());
+     write(handle.fd, message->main_buffer(), message->main_buffer_size()));
+  bool result = write_size == static_cast<ssize_t>(message->main_buffer_size());
   message->Destroy();
   return result;
 }
@@ -160,7 +159,7 @@ class TestMessageReaderAndChecker {
         }
 
         // If we've read the whole message....
-        if (bytes_.size() >= message->size_with_header_and_padding()) {
+        if (bytes_.size() >= message->main_buffer_size()) {
           if (!CheckMessageData(message->data(), message->data_size())) {
             LOG(ERROR) << "Incorrect message data.";
             return false;
@@ -169,7 +168,7 @@ class TestMessageReaderAndChecker {
           // Erase message data.
           bytes_.erase(bytes_.begin(),
                        bytes_.begin() +
-                           message->size_with_header_and_padding());
+                           message->main_buffer_size());
           return true;
         }
       }
