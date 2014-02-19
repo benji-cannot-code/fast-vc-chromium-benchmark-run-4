4feae27da15d882cae7d68b95422567576cad5dd
raymes@chromium.org <raymes@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-19 08:13:07 +0000

    Implement viewporting for the out of process PDF plugin.
    BUG=303491
    
    Review URL: https://codereview.chromium.org/137663008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251993 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/pdf/index.js
+++ chrome/browser/resources/pdf/index.js
@@ -388,39 +388,39 @@ Polymer={},"function"==typeof window.Polymer&&(Polymer={}),function(a){function 
     });
   ;
 
-    Polymer('viewer-toolbar', {
-      fadingIn: false,
-      timerId: undefined,
-      ready: function() {
-        this.fadingInChanged();
-      },
-      fadeIn: function() {
-        this.fadingIn = true;
-      },
-      fadeOut: function() {
-        this.fadingIn = false;
-      },
-      fadingInChanged: function() {
-        if (this.fadingIn) {
-          this.style.opacity = 1;
-          if (this.timerId !== undefined) {
-            clearTimeout(this.timerId);
-            this.timerId = undefined;
-          }
-        } else {
-          if (this.timerId === undefined) {
-            this.timerId = setTimeout(
-              function() {
-                this.style.opacity = 0;
-                this.timerId = undefined;
-              }.bind(this), 3000);
-          }
+  Polymer('viewer-toolbar', {
+    fadingIn: false,
+    timerId: undefined,
+    ready: function() {
+      this.fadingInChanged();
+    },
+    fadeIn: function() {
+      this.fadingIn = true;
+    },
+    fadeOut: function() {
+      this.fadingIn = false;
+    },
+    fadingInChanged: function() {
+      if (this.fadingIn) {
+        this.style.opacity = 1;
+        if (this.timerId !== undefined) {
+          clearTimeout(this.timerId);
+          this.timerId = undefined;
+        }
+      } else {
+        if (this.timerId === undefined) {
+          this.timerId = setTimeout(
+            function() {
+              this.style.opacity = 0;
+              this.timerId = undefined;
+            }.bind(this), 3000);
         }
       }
-    });
-  ;
+    }
+  });
+;
 
-    (function() {
+  (function() {
     var dpi = '';
 
     Polymer('viewer-button', {
@@ -436,7 +436,7 @@ Polymer={},"function"==typeof window.Polymer&&(Polymer={}),function(a){function 
         if (this.src) {
           this.$.icon.style.backgroundImage =
               'url(' + this.getAttribute('assetpath') + 'img/' + dpi +
-                  'DPI/' + this.src + ')';
+              'DPI/' + this.src + ')';
         } else {
           this.$.icon.style.backgroundImage = '';
         }
@@ -448,5 +448,4 @@ Polymer={},"function"==typeof window.Polymer&&(Polymer={}),function(a){function 
           this.classList.remove('latchable');
       },
     });
-    })();
-  
+  })();
