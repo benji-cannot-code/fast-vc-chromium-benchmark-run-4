09c1738c02fd3b91e891e636c1bf6a80352e2fe3
earthdok@chromium.org <earthdok@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-19 08:38:48 +0000

    Revert 251959 "Refactor the TrayAudio code so that it can be use..."
    Reverted due to memory leaks:
    http://build.chromium.org/p/chromium.memory/builders/Linux%20Chromium%20OS%20ASAN%20Tests%20%281%29/builds/15922
    
    > Refactor the TrayAudio code so that it can be used by other platforms.
    > The motivation here is to introduce the volume slider tray UI into
    > Windows Ash.
    > 
    > This change does not actually add tray audio UI to other platforms,
    > only moves some non-chromeos specific things out of the chromeos
    > platform folders, so that we can hook in the native Audio APIs
    > of other platforms to the tray UI.
    > 
    > At a high level, this change does the following:
    > 
    > 1) Move tray_audio out of chromeos/ so that other platforms can
    > use the UI that it provides.
    > 2) Decouples TrayAudio from CrasAudioHandler by introducing a
    > new delegate, in a similar vein to what is done with all the other
    > handlers (e.g. the BluetoothHandler)
    > 3) Move the additional views (VolumeView, etc) to their own files
    > so they can be used by other platforms as well.
    > 
    > TEST=
    >  On CrOS physical device, verified that:
    >  1) Keyboard volume buttons function appropriately and display the
    >     volume view
    >  2) AudioDetailView still is accessible at the correct times
    >  3) Adjusting the volume (via the keyboard shortcuts or via the
    >     slider) correctly change the output volume.
    >  4) UI still behaves correctly (e.g. icons update as the volume
    >     changes, mute button works, etc).
    > 
    > BUG=227247
    > 
    > Review URL: https://codereview.chromium.org/163953007
    
    TBR=zturner@chromium.org
    
    Review URL: https://codereview.chromium.org/170843009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252009 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/tray/system_tray_notifier.h
+++ ash/system/tray/system_tray_notifier.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "ash/ash_export.h"
-#include "ash/system/audio/audio_observer.h"
 #include "ash/system/bluetooth/bluetooth_observer.h"
 #include "ash/system/chromeos/tray_tracing.h"
 #include "ash/system/date/clock_observer.h"
@@ -40,15 +39,12 @@ class NetworkStateNotifier;
 #endif
 
 class ASH_EXPORT SystemTrayNotifier {
- public:
+public:
   SystemTrayNotifier();
   ~SystemTrayNotifier();
 
   void AddAccessibilityObserver(AccessibilityObserver* observer);
   void RemoveAccessibilityObserver(AccessibilityObserver* observer);
-
-  void AddAudioObserver(AudioObserver* observer);
-  void RemoveAudioObserver(AudioObserver* observer);
 
   void AddBluetoothObserver(BluetoothObserver* observer);
   void RemoveBluetoothObserver(BluetoothObserver* observer);
@@ -99,11 +95,6 @@ class ASH_EXPORT SystemTrayNotifier {
 
   void NotifyAccessibilityModeChanged(
       AccessibilityNotificationVisibility notify);
-  void NotifyAudioOutputVolumeChanged();
-  void NotifyAudioOutputMuteChanged();
-  void NotifyAudioNodesChanged();
-  void NotifyAudioActiveOutputNodeChanged();
-  void NotifyAudioActiveInputNodeChanged();
   void NotifyTracingModeChanged(bool value);
   void NotifyRefreshBluetooth();
   void NotifyBluetoothDiscoveringChanged();
@@ -141,7 +132,6 @@ class ASH_EXPORT SystemTrayNotifier {
 
  private:
   ObserverList<AccessibilityObserver> accessibility_observers_;
-  ObserverList<AudioObserver> audio_observers_;
   ObserverList<BluetoothObserver> bluetooth_observers_;
   ObserverList<CapsLockObserver> caps_lock_observers_;
   ObserverList<ClockObserver> clock_observers_;
--- chrome/browser/ui/ash/system_tray_delegate_chromeos.cc
+++ chrome/browser/ui/ash/system_tray_delegate_chromeos.cc
@@ -315,9 +315,6 @@ void SystemTrayDelegateChromeOS::Initialize() {
 
   if (LoginState::IsInitialized())
     LoginState::Get()->AddObserver(this);
-
-  if (CrasAudioHandler::IsInitialized())
-    CrasAudioHandler::Get()->AddAudioObserver(this);
 }
 
 void SystemTrayDelegateChromeOS::Shutdown() {
@@ -373,10 +370,7 @@ SystemTrayDelegateChromeOS::~SystemTrayDelegateChromeOS() {
       ->RemoveSessionStateObserver(this);
   LoginState::Get()->RemoveObserver(this);
 
-  if (CrasAudioHandler::IsInitialized())
-    CrasAudioHandler::Get()->RemoveAudioObserver(this);
-
-// Stop observing Drive operations.
+  // Stop observing Drive operations.
   UnobserveDriveUpdates();
 
   policy::BrowserPolicyConnectorChromeOS* connector =
@@ -1215,33 +1209,6 @@ void SystemTrayDelegateChromeOS::InputMethodChanged(
 void SystemTrayDelegateChromeOS::InputMethodMenuItemChanged(
     ash::ime::InputMethodMenuManager* manager) {
   GetSystemTrayNotifier()->NotifyRefreshIME(false);
-}
-
-// Overridden from CrasAudioHandler::AudioObserver.
-void SystemTrayDelegateChromeOS::OnOutputVolumeChanged() {
-  GetSystemTrayNotifier()->NotifyAudioOutputVolumeChanged();
-}
-
-void SystemTrayDelegateChromeOS::OnOutputMuteChanged() {
-  GetSystemTrayNotifier()->NotifyAudioOutputMuteChanged();
-}
-
-void SystemTrayDelegateChromeOS::OnInputGainChanged() {
-}
-
-void SystemTrayDelegateChromeOS::OnInputMuteChanged() {
-}
-
-void SystemTrayDelegateChromeOS::OnAudioNodesChanged() {
-  GetSystemTrayNotifier()->NotifyAudioNodesChanged();
-}
-
-void SystemTrayDelegateChromeOS::OnActiveOutputNodeChanged() {
-  GetSystemTrayNotifier()->NotifyAudioActiveOutputNodeChanged();
-}
-
-void SystemTrayDelegateChromeOS::OnActiveInputNodeChanged() {
-  GetSystemTrayNotifier()->NotifyAudioActiveInputNodeChanged();
 }
 
 // drive::JobListObserver overrides.
