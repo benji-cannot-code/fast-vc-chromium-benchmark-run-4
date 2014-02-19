6c0d2ba46e9a5b5a6953cd54335ba8db375c8b13
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-19 11:40:27 +0000

    Remove WrapperTypeTraits
    WrapperTypeTraits is unnecessary.
    This CL replaces WrapperTypeTraits<X>::wrapperTypeInfo() with &V8X::wrapperTypeInfo,
    and removes WrapperTypeTraits completely.
    
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/172033002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167409 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/tests/results/V8TestSpecialOperationsNotEnumerable.h
+++ third_party/WebKit/Source/bindings/tests/results/V8TestSpecialOperationsNotEnumerable.h
@@ -70,12 +70,6 @@ private:
     static v8::Handle<v8::Object> createWrapper(PassRefPtr<TestSpecialOperationsNotEnumerable>, v8::Handle<v8::Object> creationContext, v8::Isolate*);
 };
 
-template<>
-class WrapperTypeTraits<TestSpecialOperationsNotEnumerable > {
-public:
-    static const WrapperTypeInfo* wrapperTypeInfo() { return &V8TestSpecialOperationsNotEnumerable::wrapperTypeInfo; }
-};
-
 inline v8::Handle<v8::Object> wrap(TestSpecialOperationsNotEnumerable* impl, v8::Handle<v8::Object> creationContext, v8::Isolate* isolate)
 {
     ASSERT(impl);
