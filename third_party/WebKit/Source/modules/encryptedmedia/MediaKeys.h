b822f529a6d29ff10c526148a9cdc420d5836488
jrummell@chromium.org <jrummell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-19 22:42:46 +0000

    Update MediaKeys and MediaKeySession lifetimes
    MediaKeys objects live as long as JavaScript or HTMLMediaElement has a
    reference to it.
    
    MediaKeySession objects live as long as JavaScript has a reference to it
    or MediaKeys still exists and !closed. So even if all JavaScript references
    to MediaKeySession are gone, the MediaKeySession object remain so that
    events from the CDM can still get passed to JavaScript. It will fire events
    until MediaKeys goes away (indicating that the CDM is no longer needed)
    or the session is closed by the CDM.
    
    Also update MediaKeys to know when the context is going away so that it can
    clean up.
    
    BUG=341567
    TEST=EME content tests pass
    
    Review URL: https://codereview.chromium.org/165213004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167433 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/encryptedmedia/MediaKeys.h
+++ third_party/WebKit/Source/modules/encryptedmedia/MediaKeys.h
@@ -28,6 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define MediaKeys_h
 
 #include "bindings/v8/ScriptWrappable.h"
+#include "core/dom/ContextLifecycleObserver.h"
+#include "core/dom/ExecutionContext.h"
 #include "core/events/EventTarget.h"
 #include "heap/Handle.h"
 #include "modules/encryptedmedia/MediaKeySession.h"
@@ -52,12 +54,10 @@ class ExceptionState;
 
 // References are held by JS and HTMLMediaElement.
 // The ContentDecryptionModule has the same lifetime as this object.
-// Maintains a reference to all MediaKeySessions created to ensure they live as
-// long as this object unless explicitly close()'d.
-class MediaKeys : public RefCountedWillBeGarbageCollectedFinalized<MediaKeys>, public ScriptWrappable {
+class MediaKeys : public RefCountedWillBeGarbageCollectedFinalized<MediaKeys>, public ContextLifecycleObserver, public ScriptWrappable {
     DECLARE_GC_INFO;
 public:
-    static PassRefPtrWillBeRawPtr<MediaKeys> create(const String& keySystem, ExceptionState&);
+    static PassRefPtrWillBeRawPtr<MediaKeys> create(ExecutionContext*, const String& keySystem, ExceptionState&);
     ~MediaKeys();
 
     PassRefPtrWillBeRawPtr<MediaKeySession> createSession(ExecutionContext*, const String& contentType, Uint8Array* initData, ExceptionState&);
@@ -70,11 +70,12 @@ public:
 
     void trace(Visitor*);
 
-protected:
-    MediaKeys(const String& keySystem, PassOwnPtr<ContentDecryptionModule>);
-    void initializeNewSessionTimerFired(Timer<MediaKeys>*);
+    // ContextLifecycleObserver
+    virtual void contextDestroyed() OVERRIDE;
 
-    Vector<RefPtr<MediaKeySession> > m_sessions;
+protected:
+    MediaKeys(ExecutionContext*, const String& keySystem, PassOwnPtr<ContentDecryptionModule>);
+    void initializeNewSessionTimerFired(Timer<MediaKeys>*);
 
     HTMLMediaElement* m_mediaElement;
     const String m_keySystem;
@@ -95,6 +96,8 @@ protected:
     };
     Deque<InitializeNewSessionData> m_pendingInitializeNewSessionData;
     Timer<MediaKeys> m_initializeNewSessionTimer;
+
+    WeakPtrFactory<MediaKeys> m_weakFactory;
 };
 
 }
