079320ec1c534825299163c622be6d6e2de483af
newt@chromium.org <newt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-19 08:03:37 +0000

    Remove pp_ifdef from resource files in chrome/browser and chrome/renderer.
    Generated using:
    > git grep -l pp_ifdef | xargs sed -i "s:pp_ifdef('\([^']\+\)'):\1:g"
    
    BUG=343235
    
    Review URL: https://codereview.chromium.org/171763004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251987 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/extensions/extensions.js
+++ chrome/browser/resources/extensions/extensions.js
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <include src="pack_extension_overlay.js"></include>
 <include src="extension_error_overlay.js"></include>
 
-<if expr="pp_ifdef('chromeos')">
+<if expr="chromeos">
 <include src="chromeos/kiosk_apps.js"></include>
 </if>
 
