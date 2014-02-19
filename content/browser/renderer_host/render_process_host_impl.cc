9d3b15a4bf31c66087ab7205975b8043787d93dd
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-19 18:51:43 +0000

    Revert of cc: Remove the --enable-partial-swap flag. (https://codereview.chromium.org/167953003/)
    Reason for revert:
    Should have only enabled this on ChromeOS. Enabled elsewhere by mistake.
    
    Original issue's description:
    > cc: Remove the --enable-partial-swap flag.
    > 
    > R=piman
    > BUG=
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=251898
    
    TBR=piman@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=
    
    Review URL: https://codereview.chromium.org/172763002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252038 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1111,6 +1111,7 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     cc::switches::kEnableImplSidePainting,
     cc::switches::kEnableLCDText,
     cc::switches::kEnableMapImage,
+    cc::switches::kEnablePartialSwap,
     cc::switches::kEnablePerTilePainting,
     cc::switches::kEnablePinchVirtualViewport,
     cc::switches::kEnableTopControlsPositionCalculation,
