a9619d7181a1d30d5a1592eb4b86396f69334b5c
dkrahn@chromium.org <dkrahn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-19 23:15:32 +0000

    Show the extension / app name for platform verification.
    The platform verification dialog communicates to the user the origin of
    the request.  Normally this is a web origin but for extensions or v2
    apps the web origin is ugly and the extension / app name is better.
    
    BUG=chromium:344723
    TEST=manual
    
    Review URL: https://codereview.chromium.org/171483003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252126 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/attestation/platform_verification_dialog.cc
+++ chrome/browser/chromeos/attestation/platform_verification_dialog.cc
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/web_modal/web_contents_modal_dialog_manager.h"
 #include "components/web_modal/web_contents_modal_dialog_manager_delegate.h"
 #include "content/public/browser/web_contents.h"
+#include "extensions/browser/extension_registry.h"
+#include "extensions/common/extension.h"
 #include "grit/generated_resources.h"
 #include "ui/aura/window.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -36,7 +38,13 @@ const int kDialogMaxWidthInPixel = 400;
 void PlatformVerificationDialog::ShowDialog(
     content::WebContents* web_contents,
     const PlatformVerificationFlow::Delegate::ConsentCallback& callback) {
-  std::string origin = web_contents->GetLastCommittedURL().GetOrigin().spec();
+  GURL url = web_contents->GetLastCommittedURL();
+  // In the case of an extension or hosted app, the origin of the request is
+  // best described by the extension / app name.
+  const extensions::Extension* extension =
+      extensions::ExtensionRegistry::Get(web_contents->GetBrowserContext())->
+          enabled_extensions().GetExtensionOrAppByURL(url);
+  std::string origin = extension ? extension->name() : url.GetOrigin().spec();
 
   PlatformVerificationDialog* dialog = new PlatformVerificationDialog(
       chrome::FindBrowserWithWebContents(web_contents),
