07e4fa1433b825e18909300809903c1d851aef22
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-20 18:25:13 +0000

    Always pass text direction to computePreferredLogicalWidths
    Update RenderText to require a text direction when calling RenderText::
    computePreferredLogicalWidths. This in turns requires minLogicalWidth
    and maxLogicalWidth to require a text direction as well but all
    callers already have the direction so passing it along is trivial.
    
    BUG=343535
    R=leviw@chromium.org
    
    Review URL: https://codereview.chromium.org/173053003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167524 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderText.cpp
+++ third_party/WebKit/Source/core/rendering/RenderText.cpp
@@ -787,7 +787,7 @@ void RenderText::trimmedPrefWidths(float leadWidth,
         stripFrontSpaces = false;
 
     if (m_hasTab || preferredLogicalWidthsDirty())
-        computePreferredLogicalWidths(leadWidth);
+        computePreferredLogicalWidths(leadWidth, direction);
 
     hasBreakableStart = !stripFrontSpaces && m_hasBreakableStart;
     hasBreakableEnd = m_hasBreakableEnd;
@@ -866,27 +866,27 @@ void RenderText::trimmedPrefWidths(float leadWidth,
     }
 }
 
-float RenderText::minLogicalWidth() const
+float RenderText::minLogicalWidth(TextDirection textDirection) const
 {
     if (preferredLogicalWidthsDirty())
-        const_cast<RenderText*>(this)->computePreferredLogicalWidths(0);
+        const_cast<RenderText*>(this)->computePreferredLogicalWidths(0, textDirection);
 
     return m_minWidth;
 }
 
-float RenderText::maxLogicalWidth() const
+float RenderText::maxLogicalWidth(TextDirection textDirection) const
 {
     if (preferredLogicalWidthsDirty())
-        const_cast<RenderText*>(this)->computePreferredLogicalWidths(0);
+        const_cast<RenderText*>(this)->computePreferredLogicalWidths(0, textDirection);
 
     return m_maxWidth;
 }
 
-void RenderText::computePreferredLogicalWidths(float leadWidth)
+void RenderText::computePreferredLogicalWidths(float leadWidth, TextDirection textDirection)
 {
     HashSet<const SimpleFontData*> fallbackFonts;
     GlyphOverflow glyphOverflow;
-    computePreferredLogicalWidths(leadWidth, fallbackFonts, glyphOverflow);
+    computePreferredLogicalWidths(leadWidth, fallbackFonts, glyphOverflow, textDirection);
     if (fallbackFonts.isEmpty() && !glyphOverflow.left && !glyphOverflow.right && !glyphOverflow.top && !glyphOverflow.bottom)
         m_knownToHaveNoOverflowAndNoFallbackFonts = true;
 }
@@ -1502,8 +1502,9 @@ float RenderText::width(unsigned from, unsigned len, const Font& f, float xPos, 
                         m_knownToHaveNoOverflowAndNoFallbackFonts = true;
                 }
                 w = m_maxWidth;
-            } else
-                w = maxLogicalWidth();
+            } else {
+                w = maxLogicalWidth(textDirection);
+            }
         } else {
             w = widthFromCache(f, from, len, xPos, textDirection, fallbackFonts, glyphOverflow);
         }
