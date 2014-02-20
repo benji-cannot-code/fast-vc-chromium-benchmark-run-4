f7c6286e7b2f54333934f0524fcbfed54c6442ba
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-20 23:34:40 +0000

    Create DeviceMediaID for screen w/ correct 'id' value
    When per-screen capture supported was added to WebRTC, the
    MediaCaptureDevicesDispatcher was incorrectly set to request "screen:0"
    rather than "screen" to capture the entire desktop, in effect.
    
    BUG=343926
    
    Review URL: https://codereview.chromium.org/171023007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252417 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media/media_capture_devices_dispatcher.cc
+++ chrome/browser/media/media_capture_devices_dispatcher.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/extension.h"
 #include "grit/generated_resources.h"
 #include "media/audio/audio_manager_base.h"
+#include "third_party/webrtc/modules/desktop_capture/desktop_capture_types.h"
 #include "ui/base/l10n/l10n_util.h"
 
 #if defined(OS_CHROMEOS)
@@ -412,7 +413,8 @@ void MediaCaptureDevicesDispatcher::ProcessScreenCaptureAccessRequest(
           ash::Shell::GetInstance()->GetPrimaryRootWindow());
 #else  // defined(OS_CHROMEOS)
       screen_id =
-          content::DesktopMediaID(content::DesktopMediaID::TYPE_SCREEN, 0);
+          content::DesktopMediaID(content::DesktopMediaID::TYPE_SCREEN,
+                                  webrtc::kFullDesktopScreenId);
 #endif  // !defined(OS_CHROMEOS)
 
       bool capture_audio =
