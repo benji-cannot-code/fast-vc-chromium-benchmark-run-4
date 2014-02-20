66d392abf9ad75c5d547133297f9f5803bf497c1
rkc@chromium.org <rkc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-20 23:10:20 +0000

    Add flag to disable demo mode.
    R=dpolukhin@chromium.org
    BUG=343727
    
    Review URL: https://codereview.chromium.org/168853003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252412 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chromeos/login/network_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/network_screen_handler.cc
@@ -276,6 +276,9 @@ void NetworkScreenHandler::OnSystemTimezoneChanged() {
 }
 
 void NetworkScreenHandler::StartIdleDetection() {
+  if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kDisableDemoMode))
+    return;
+
   if (!detector_.get()) {
     detector_.reset(
         new IdleDetector(base::Closure(),
