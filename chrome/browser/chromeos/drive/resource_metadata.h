4422da6a048788847c9b60300931bba2a255e79c
kinaba@chromium.org <kinaba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-20 18:06:34 +0000

    Remove the last ...OnUIThread method from drive::ResourceMetadata.
    BUG=231222
    
    Review URL: https://codereview.chromium.org/166293004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252312 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/resource_metadata.h
+++ chrome/browser/chromeos/drive/resource_metadata.h
@@ -10,10 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "base/callback_forward.h"
 #include "base/files/file_path.h"
 #include "base/memory/scoped_ptr.h"
-#include "base/memory/weak_ptr.h"
 #include "chrome/browser/chromeos/drive/file_errors.h"
 #include "chrome/browser/chromeos/drive/resource_metadata_storage.h"
 
@@ -25,18 +23,11 @@ namespace drive {
 
 typedef std::vector<ResourceEntry> ResourceEntryVector;
 
-// Used to get a resource entry from the file system.
-// If |error| is not FILE_ERROR_OK, |entry_info| is set to NULL.
-typedef base::Callback<void(FileError error,
-                            scoped_ptr<ResourceEntry> entry)>
-    GetResourceEntryCallback;
-
-typedef base::Callback<void(const ResourceEntry& entry)> IterateCallback;
-
 namespace internal {
 
 // Storage for Drive Metadata.
-// All methods must be run with |blocking_task_runner| unless otherwise noted.
+// All methods except the constructor and Destroy() function must be run with
+// |blocking_task_runner| unless otherwise noted.
 class ResourceMetadata {
  public:
   typedef ResourceMetadataStorage::Iterator Iterator;
@@ -72,13 +63,6 @@ class ResourceMetadata {
   // Finds an entry (a file or a directory) by |id|.
   FileError GetResourceEntryById(const std::string& id,
                                  ResourceEntry* out_entry);
-
-  // Finds an entry (a file or a directory) by |file_path|.
-  // |callback| must not be null.
-  // Must be called on the UI thread.
-  void GetResourceEntryByPathOnUIThread(
-      const base::FilePath& file_path,
-      const GetResourceEntryCallback& callback);
 
   // Synchronous version of GetResourceEntryByPathOnUIThread().
   FileError GetResourceEntryByPath(const base::FilePath& file_path,
@@ -140,10 +124,6 @@ class ResourceMetadata {
   scoped_refptr<base::SequencedTaskRunner> blocking_task_runner_;
 
   ResourceMetadataStorage* storage_;
-
-  // This should remain the last member so it'll be destroyed first and
-  // invalidate its weak pointers before other members are destroyed.
-  base::WeakPtrFactory<ResourceMetadata> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(ResourceMetadata);
 };
