27c9c92d46ed2984062bcb65ba1ad9a8ffdea319
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-20 18:26:37 +0000

    AudioPlayer: Cancel the scheduled advance when the pause button clicked.
    BUG=345147
    TEST=manually tested
    
    Review URL: https://codereview.chromium.org/172253002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252322 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/audio_player/elements/audio_player.js
+++ chrome/browser/resources/file_manager/audio_player/elements/audio_player.js
@@ -102,6 +102,8 @@ Polymer('audio-player', {
       }
     }
 
+    // When the new status is "stopped".
+    this.cancelAutoAdvance_();
     this.audioController.playing = false;
     this.audioElement.pause();
     this.currenttrackurl = '';
