e3e92b0ed673ec3f7f0b220d7b8bdf820f2a2ccf
ccameron@chromium.org <ccameron@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-20 15:37:02 +0000

    Do not share a GL context between CoreAnimation and async readback
    There is nothing in the documentation that suggests that this
    change is necessary -- nothing says that you can't use the GL
    context that CoreAnimation is using outside of CoreAnimation.
    So this patch is not well-informed, in that sense.
    
    That said, the crashes reported did not appear until I merged
    the two GL contexts. Also, locally, they went away as soon as I
    separated them back out.
    
    BUG=340385
    
    Review URL: https://codereview.chromium.org/164873006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252239 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/compositing_iosurface_layer_mac.mm
+++ content/browser/renderer_host/compositing_iosurface_layer_mac.mm
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (self = [super init]) {
     renderWidgetHostView_ = r;
     context_ = content::CompositingIOSurfaceContext::Get(
-        content::CompositingIOSurfaceContext::kOffscreenContextWindowNumber);
+        content::CompositingIOSurfaceContext::kCALayerContextWindowNumber);
     DCHECK(context_);
     needsDisplay_ = NO;
 
