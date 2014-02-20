1c5adf1e8e67563633fe5eea850f2d0a9b44b281
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-20 22:55:02 +0000

    Remove out of date comments in EllipsisBox
    Remove out of date comments relating to text directionality in
    EllipsisBox.cpp. As of r165174 RenderBlockFlow::constructTextRun will
    resolve the direction unless specified.
    
    TBR=dglazkov@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/174383002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167546 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/EllipsisBox.cpp
+++ third_party/WebKit/Source/core/rendering/EllipsisBox.cpp
@@ -81,7 +81,6 @@ void EllipsisBox::paint(PaintInfo& paintInfo, const LayoutPoint& paintOffset, La
         context->setDrawLooper(drawLooper);
     }
 
-    // FIXME: Why is this always LTR? Fix by passing correct text run flags below.
     TextRun textRun = RenderBlockFlow::constructTextRun(renderer(), font, m_str, style, TextRun::AllowTrailingExpansion);
     TextRunPaintInfo textRunPaintInfo(textRun);
     textRunPaintInfo.bounds = boxRect;
@@ -132,7 +131,6 @@ IntRect EllipsisBox::selectionRect()
 {
     RenderStyle* style = m_renderer->style(isFirstLineStyle());
     const Font& font = style->font();
-    // FIXME: Why is this always LTR? Fix by passing correct text run flags below.
     return enclosingIntRect(font.selectionRectForText(RenderBlockFlow::constructTextRun(renderer(), font, m_str, style, TextRun::AllowTrailingExpansion), IntPoint(logicalLeft(), logicalTop() + root()->selectionTopAdjustedForPrecedingBlock()), root()->selectionHeightAdjustedForPrecedingBlock()));
 }
 
@@ -157,7 +155,6 @@ void EllipsisBox::paintSelection(GraphicsContext* context, const FloatPoint& box
     FloatRect clipRect(localOrigin, FloatSize(m_logicalWidth, h));
     alignSelectionRectToDevicePixels(clipRect);
     context->clip(clipRect);
-    // FIXME: Why is this always LTR? Fix by passing correct text run flags below.
     context->drawHighlightForText(font, RenderBlockFlow::constructTextRun(renderer(), font, m_str, style, TextRun::AllowTrailingExpansion), localOrigin, h, c);
 }
 
