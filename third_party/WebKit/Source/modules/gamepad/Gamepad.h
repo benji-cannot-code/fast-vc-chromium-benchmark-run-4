f6811dffe73cf1a97523c88b12e0bf624fbece82
bajones@chromium.org <bajones@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-20 22:18:23 +0000

    Revert 167539 "Updating Blink-side Gamepad API to match latest spec"
    Compiler errors introduced on Blink waterfall
    
    > Updating Blink-side Gamepad API to match latest spec
    > 
    > BUG=344556
    > R=abarth@chromium.org, scottmg@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/170993002
    
    TBR=bajones@chromium.org
    
    Review URL: https://codereview.chromium.org/171423019
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167541 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/gamepad/Gamepad.h
+++ third_party/WebKit/Source/modules/gamepad/Gamepad.h
@@ -29,8 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "bindings/v8/ScriptWrappable.h"
 #include "heap/Handle.h"
-#include "modules/gamepad/GamepadButton.h"
-#include "public/platform/WebGamepad.h"
 #include "wtf/RefCounted.h"
 #include "wtf/Vector.h"
 #include "wtf/text/WTFString.h"
@@ -47,7 +45,6 @@ public:
     ~Gamepad();
 
     typedef Vector<float> FloatVector;
-    typedef Vector<GamepadButton> ButtonVector;
 
     const String& id() const { return m_id; }
     void id(const String& id) { m_id = id; }
@@ -55,20 +52,14 @@ public:
     unsigned index() const { return m_index; }
     void index(unsigned val) { m_index = val; }
 
-    bool connected() const { return m_connected; }
-    void connected(const bool val) { m_connected = val; }
-
     unsigned long long timestamp() const { return m_timestamp; }
     void timestamp(unsigned long long val) { m_timestamp = val; }
-
-    const String& mapping() const { return m_mapping; }
-    void mapping(const String& val) { m_mapping = val; }
 
     const FloatVector& axes() const { return m_axes; }
     void axes(unsigned count, float* data);
 
-    const GamepadButtonVector& buttons() const { return m_buttons; }
-    void buttons(unsigned count, blink::WebGamepadButton* data);
+    const FloatVector& buttons() const { return m_buttons; }
+    void buttons(unsigned count, float* data);
 
     void trace(Visitor*);
 
@@ -76,11 +67,9 @@ private:
     Gamepad();
     String m_id;
     unsigned m_index;
-    bool m_connected;
     unsigned long long m_timestamp;
-    String m_mapping;
     FloatVector m_axes;
-    GamepadButtonVector m_buttons;
+    FloatVector m_buttons;
 };
 
 } // namespace WebCore
