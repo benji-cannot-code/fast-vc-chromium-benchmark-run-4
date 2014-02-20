5bf50ec582b9a571aef661b6561353a5df24089f
zeeshanq@chromium.org <zeeshanq@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-20 02:28:03 +0000

    Support viewport in content_shell.
    Viewport preferences are overridden even with the --enable-viewport flag passed in.
    
    BUG=343993
    
    Review URL: https://codereview.chromium.org/166033008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252161 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/common/webkit_test_helpers.cc
+++ content/shell/common/webkit_test_helpers.cc
@@ -109,7 +109,6 @@ void ApplyLayoutTestDefaultPreferences(WebPreferences* prefs) {
 #if defined(OS_ANDROID)
   prefs->text_autosizing_enabled = false;
 #endif
-  prefs->viewport_enabled = false;
 }
 
 base::FilePath GetWebKitRootDirFilePath() {
