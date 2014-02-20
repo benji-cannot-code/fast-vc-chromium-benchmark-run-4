e89436c6c272d06d38bbe8400bb2f1dd2faadb61
kmadhusu@chromium.org <kmadhusu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-20 19:33:22 +0000

    Clean up: Remove omnibox input state details from SearchTabHelper.
    This CL,
    - Renames OmniboxCurrentPageDelegate::NotifySearchTabHelper() to OmniboxCurrentPageDelegate::OnInputStateChanged().
    - Renames SearchTabHelper::OmniboxEditModelChanged() to SearchTabHelper::OmniboxInputStateChanged().
    - Removes omnibox input state details from SearchTabHelper and modifies SearchTabHelper to use OmniboxEditModel::user_input_in_progress() instead.
    
    Follow up of cl/159713009.
    
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/161543002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252350 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/omnibox/omnibox_edit_model.cc
+++ chrome/browser/ui/omnibox/omnibox_edit_model.cc
@@ -516,7 +516,8 @@ void OmniboxEditModel::SetInputInProgress(bool in_progress) {
   controller_->GetToolbarModel()->set_input_in_progress(in_progress);
   controller_->Update(NULL);
 
-  delegate_->NotifySearchTabHelper(user_input_in_progress_, !in_revert_);
+  if (user_input_in_progress_ || !in_revert_)
+    delegate_->OnInputStateChanged();
 }
 
 void OmniboxEditModel::Revert() {
@@ -870,7 +871,9 @@ bool OmniboxEditModel::AcceptKeyword(EnteredKeywordModeMethod entered_method) {
 void OmniboxEditModel::AcceptTemporaryTextAsUserText() {
   InternalSetUserText(UserTextFromDisplayText(view_->GetText()));
   has_temporary_text_ = false;
-  delegate_->NotifySearchTabHelper(user_input_in_progress_, !in_revert_);
+
+  if (user_input_in_progress_ || !in_revert_)
+    delegate_->OnInputStateChanged();
 }
 
 void OmniboxEditModel::ClearKeyword(const base::string16& visible_text) {
@@ -928,7 +931,8 @@ void OmniboxEditModel::OnSetFocus(bool control_down) {
                                                 permanent_text_);
   }
 
-  delegate_->NotifySearchTabHelper(user_input_in_progress_, !in_revert_);
+  if (user_input_in_progress_ || !in_revert_)
+    delegate_->OnInputStateChanged();
 }
 
 void OmniboxEditModel::SetCaretVisibility(bool visible) {
@@ -949,7 +953,9 @@ void OmniboxEditModel::OnWillKillFocus(gfx::NativeView view_gaining_focus) {
 
   // TODO(jered): Rip this out along with StartZeroSuggest.
   autocomplete_controller()->StopZeroSuggest();
-  delegate_->NotifySearchTabHelper(user_input_in_progress_, !in_revert_);
+
+  if (user_input_in_progress_ || !in_revert_)
+    delegate_->OnInputStateChanged();
 }
 
 void OmniboxEditModel::OnKillFocus() {
