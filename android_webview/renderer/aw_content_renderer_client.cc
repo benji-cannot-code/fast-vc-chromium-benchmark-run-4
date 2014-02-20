d4c97e6cd27a94f94b01a47ebbf688324f61c40a
benm@chromium.org <benm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-20 19:07:22 +0000

    [AndroidWebView] Properly escape the "Web Page Unavailable" document.
    BUG=344887
    
    Review URL: https://codereview.chromium.org/172573002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252344 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/renderer/aw_content_renderer_client.cc
+++ android_webview/renderer/aw_content_renderer_client.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/renderer/render_frame.h"
 #include "content/public/renderer/render_thread.h"
 #include "content/public/renderer/render_view.h"
+#include "net/base/escape.h"
 #include "net/base/net_errors.h"
 #include "third_party/WebKit/public/platform/WebString.h"
 #include "third_party/WebKit/public/platform/WebURL.h"
@@ -171,7 +172,7 @@ void AwContentRendererClient::GetNavigationErrorStrings(
     }
 
     ReplaceSubstringsAfterOffset(&contents, 0, "%s",
-                                 error_url.possibly_invalid_spec());
+        net::EscapeForHTML(error_url.possibly_invalid_spec()));
     *error_html = contents;
   }
   if (error_description) {
