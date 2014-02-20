032033972fa69052bd16342486ed8158eb7113b9
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-20 12:15:22 +0000

    Drop DECLARE_GC_INFO from PerformanceResourceTiming
    We shouldn't add DECLARE_GC_INFO to a subclass of garbage-collected class.
    Otherwise, we will hit an assertion about type marker:
    https://codereview.chromium.org/170143002/#msg22
    
    This CL fixes a crash of test_resource_dedicated_worker.html in oilpan build bots.
    
    R=ager@chromium.org
    
    Review URL: https://codereview.chromium.org/173673002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167507 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/timing/PerformanceResourceTiming.cpp
+++ third_party/WebKit/Source/core/timing/PerformanceResourceTiming.cpp
@@ -42,8 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-DEFINE_GC_INFO(PerformanceResourceTiming);
-
 static double monotonicTimeToDocumentMilliseconds(Document* document, double seconds)
 {
     ASSERT(seconds >= 0.0);
