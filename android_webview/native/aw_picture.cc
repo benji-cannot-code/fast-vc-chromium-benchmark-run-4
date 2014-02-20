86922f296346cf0bead3cbee01839ab921a7b609
boliu@chromium.org <boliu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-20 19:33:57 +0000

    aw: Refactor and clean up software rendering code
    Move most of logic to JavaBrowserViewRendererHelper.
    Remove dead code.
    Remove bitmap caching since nothing in production should be hitting
    auxillary bitmap code.
    
    BUG=
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/166903008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252351 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/native/aw_picture.cc
+++ android_webview/native/aw_picture.cc
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "android_webview/native/aw_picture.h"
 
-#include "android_webview/browser/in_process_view_renderer.h"
 #include "android_webview/native/java_browser_view_renderer_helper.h"
+#include "base/bind.h"
 #include "jni/AwPicture_jni.h"
 #include "third_party/skia/include/core/SkPicture.h"
 
@@ -40,13 +40,13 @@ bool RenderPictureToCanvas(SkPicture* picture, SkCanvas* canvas) {
 
 void AwPicture::Draw(JNIEnv* env, jobject obj, jobject canvas,
                      jint left, jint top, jint right, jint bottom) {
-  bool ok = InProcessViewRenderer::RenderViaAuxilaryBitmapIfNeeded(
-      canvas,
-      JavaBrowserViewRendererHelper::GetInstance(),
-      gfx::Vector2d(),
-      gfx::Rect(left, top, right - left, bottom - top),
-      base::Bind(&RenderPictureToCanvas, base::Unretained(picture_.get())),
-      this);
+  bool ok = JavaBrowserViewRendererHelper::GetInstance()
+                ->RenderViaAuxilaryBitmapIfNeeded(
+                      canvas,
+                      gfx::Vector2d(),
+                      gfx::Rect(left, top, right - left, bottom - top),
+                      base::Bind(&RenderPictureToCanvas,
+                                 base::Unretained(picture_.get())));
   LOG_IF(ERROR, !ok) << "Couldn't draw picture";
 }
 
