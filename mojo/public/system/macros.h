8478ac4987137ec7a8b08c20d3f779db5ea5c1d6
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-21 18:03:42 +0000

    Mojo: Write some tests for public/system/macros.h.
    I really set out to also write no-compile tests, but they're currently totally
    disabled. :(
    
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/175343002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252593 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/public/system/macros.h
+++ mojo/public/system/macros.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Annotate a function indicating that the caller must examine the return value.
 // Use like:
 //   int foo() MOJO_WARN_UNUSED_RESULT;
+// Note that it can only be used on the prototype, and not the definition.
 #if defined(__GNUC__)
 #define MOJO_WARN_UNUSED_RESULT __attribute__((warn_unused_result))
 #else
