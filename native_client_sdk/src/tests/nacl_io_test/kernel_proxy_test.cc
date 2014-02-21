a776647071a445e70bcc0854ab0e0e5314fdb612
sbc@chromium.org <sbc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-21 20:59:29 +0000

    [NaCl SDK] nacl_io: Disallow nested calls to ki_init/ki_uninit.
    Except for test code which needs to be able to temporarily
    inject a test KernelProxy and then restore the previous
    one after its done.
    
    BUG=
    R=binji@chromium.org
    
    Review URL: https://codereview.chromium.org/134883004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252642 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/tests/nacl_io_test/kernel_proxy_test.cc
+++ native_client_sdk/src/tests/nacl_io_test/kernel_proxy_test.cc
@@ -56,7 +56,8 @@ class KernelProxyTest : public ::testing::Test {
   KernelProxyTest() {}
 
   void SetUp() {
-    ki_init(&kp_);
+    ASSERT_EQ(0, ki_push_state_for_testing());
+    ASSERT_EQ(0, ki_init(&kp_));
     // Unmount the passthrough FS and mount a memfs.
     EXPECT_EQ(0, kp_.umount("/"));
     EXPECT_EQ(0, kp_.mount("", "/", "memfs", 0, NULL));
@@ -560,7 +561,10 @@ class KernelProxyMountTest : public ::testing::Test {
  public:
   KernelProxyMountTest() {}
 
-  void SetUp() { ki_init(&kp_); }
+  void SetUp() {
+    ASSERT_EQ(0, ki_push_state_for_testing());
+    ASSERT_EQ(0, ki_init(&kp_));
+  }
 
   void TearDown() { ki_uninit(); }
 
@@ -654,7 +658,10 @@ class KernelProxyMMapTest : public ::testing::Test {
  public:
   KernelProxyMMapTest() {}
 
-  void SetUp() { ki_init(&kp_); }
+  void SetUp() {
+    ASSERT_EQ(0, ki_push_state_for_testing());
+    ASSERT_EQ(0, ki_init(&kp_));
+  }
 
   void TearDown() { ki_uninit(); }
 
@@ -729,7 +736,8 @@ class KernelProxyErrorTest : public ::testing::Test {
   KernelProxyErrorTest() {}
 
   void SetUp() {
-    ki_init(&kp_);
+    ASSERT_EQ(0, ki_push_state_for_testing());
+    ASSERT_EQ(0, ki_init(&kp_));
     // Unmount the passthrough FS and mount a testfs.
     EXPECT_EQ(0, kp_.umount("/"));
     EXPECT_EQ(0, kp_.mount("", "/", "testfs", 0, NULL));
--- native_client_sdk/src/tests/nacl_io_test/mock_kernel_proxy.cc
+++ native_client_sdk/src/tests/nacl_io_test/mock_kernel_proxy.cc
@@ -8,8 +8,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 MockKernelProxy::MockKernelProxy() {}
 
-MockKernelProxy::~MockKernelProxy() {
-  // Uninitialize the kernel proxy so wrapped functions passthrough to their
-  // unwrapped versions.
-  ki_uninit();
-}
+MockKernelProxy::~MockKernelProxy() {}
