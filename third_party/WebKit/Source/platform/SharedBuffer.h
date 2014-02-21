1cb9b83b939bbdf5a3dcb98cf391b773f275a2ef
pliard@chromium.org <pliard@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-21 20:44:17 +0000

    Remove unnecessary memcpy() in XMLDocumentParser.cpp.
    This also removes SharedBuffer::moveTo() given that XMLDocumentParser.cpp is
    the only client.
    
    SharedBuffer::moveTo() was initially introduced to save a memcpy() in
    ResourceFetcher. Unfortunately the code has been moved around and also changed
    quite significantly leading to the addition of an unnecessary copy constructor
    call defeating the purpose of the optimization in the first place.
    
    BUG=290193
    
    Review URL: https://codereview.chromium.org/152813002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167613 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/SharedBuffer.h
+++ third_party/WebKit/Source/platform/SharedBuffer.h
@@ -61,8 +61,6 @@ public:
     // for better performance.
     const char* data() const;
 
-    void moveTo(Vector<char>&);
-
     unsigned size() const;
 
     bool isEmpty() const { return !size(); }
