2d705d0ab4deb4b79dda162af64cda48c3316a6e
tmdiep@chromium.org <tmdiep@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-21 02:36:47 +0000

    Fixed memory leak in IncludeRunningEphemeralAppsInNotifiers browser test
    The ownership of the objects in the list returned by
    GetNotifierList() is transferred to the caller. The test 
    should have deleted the objects.
    
    BUG=307043
    
    Review URL: https://codereview.chromium.org/170203004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252470 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/apps/ephemeral_app_browsertest.cc
+++ chrome/browser/apps/ephemeral_app_browsertest.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/stl_util.h"
 #include "chrome/browser/apps/app_browsertest_util.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/extension_test_message_listener.h"
@@ -265,7 +266,7 @@ IN_PROC_BROWSER_TEST_F(EphemeralAppBrowserTest, StickyNotificationSettings) {
 // Verify that only running ephemeral apps will appear in the Notification
 // Settings UI. Inactive, cached ephemeral apps should not appear.
 IN_PROC_BROWSER_TEST_F(EphemeralAppBrowserTest,
-                       IncludeRunningEphmeralAppsInNotifiers) {
+                       IncludeRunningEphemeralAppsInNotifiers) {
   message_center::NotifierSettingsProvider* settings_provider =
       message_center::MessageCenter::Get()->GetNotifierSettingsProvider();
   // TODO(tmdiep): Remove once notifications settings are supported across
@@ -281,15 +282,17 @@ IN_PROC_BROWSER_TEST_F(EphemeralAppBrowserTest,
   // Since the ephemeral app is running, it should be included in the list
   // of notifiers to show in the UI.
   NotifierList notifiers;
+  STLElementDeleter<NotifierList> notifier_deleter(&notifiers);
+
   settings_provider->GetNotifierList(&notifiers);
   EXPECT_TRUE(IsNotifierInList(notifier_id, notifiers));
+  STLDeleteElements(&notifiers);
 
   // Close the ephemeral app.
   CloseApp(app->id());
 
   // Inactive ephemeral apps should not be included in the list of notifiers to
   // show in the UI.
-  notifiers.clear();
   settings_provider->GetNotifierList(&notifiers);
   EXPECT_FALSE(IsNotifierInList(notifier_id, notifiers));
 }
