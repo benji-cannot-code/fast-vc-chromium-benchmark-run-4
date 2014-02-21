78899f27245a2e70fd8560a8d40dbfacf86fda7e
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-21 19:47:21 +0000

    DevTools: fix flex style in inspect.css
    BUG=345587
    
    Review URL: https://codereview.chromium.org/173493003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252620 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/inspect/inspect.js
+++ chrome/browser/resources/inspect/inspect.js
@@ -388,7 +388,8 @@ function addFavicon(row, data) {
   var favicon = document.createElement('img');
   if (data['faviconUrl'])
     favicon.src = data['faviconUrl'];
-  row.insertBefore(favicon, row.firstChild);
+  var propertiesBox = row.querySelector('.properties-box');
+  propertiesBox.insertBefore(favicon, propertiesBox.firstChild);
 }
 
 function addWebViewDetails(row, data) {
@@ -477,16 +478,21 @@ function addWebViewThumbnail(row, webview, screenWidth, screenHeight) {
     screenRect.appendChild(viewRect);
   }
 
-  row.insertBefore(thumbnail, row.firstChild);
+  var propertiesBox = row.querySelector('.properties-box');
+  propertiesBox.insertBefore(thumbnail, propertiesBox.firstChild);
 }
 
 function addTargetToList(data, list, properties) {
   var row = document.createElement('div');
   row.className = 'row';
 
+  var propertiesBox = document.createElement('div');
+  propertiesBox.className = 'properties-box';
+  row.appendChild(propertiesBox);
+
   var subrowBox = document.createElement('div');
   subrowBox.className = 'subrow-box';
-  row.appendChild(subrowBox);
+  propertiesBox.appendChild(subrowBox);
 
   var subrow = document.createElement('div');
   subrow.className = 'subrow';
