307a28c79b8639a9094325195c28c9bec8c9fa17
ccameron@chromium.org <ccameron@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-21 11:21:07 +0000

    NSView cleanup for CoreAnimation
    Make devtools place the contents view as a child of the devtools view,
    so that the contents appears on top of the devtools.
    
    Add instances of ScopedNSDisableScreenUpdates while adjusting views,
    and replace some instances of [NSWindow disableScreenUpdatesUntilFlush]
    with this more explicit mechanism.
    
    BUG=245900
    
    Review URL: https://codereview.chromium.org/171973004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252537 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/tabs/tab_strip_controller.mm
+++ chrome/browser/ui/cocoa/tabs/tab_strip_controller.mm
@@ -73,6 +73,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/theme_provider.h"
 #include "ui/gfx/image/image.h"
+#include "ui/gfx/mac/scoped_ns_disable_screen_updates.h"
 
 using base::UserMetricsAction;
 using content::OpenURLParams;
@@ -615,9 +616,11 @@ NSImage* Overlay(NSImage* ground, NSImage* overlay, CGFloat alpha) {
   NSInteger index = [self indexFromModelIndex:modelIndex];
   TabContentsController* controller = [tabContentsArray_ objectAtIndex:index];
 
+  // Make sure we do not draw any transient arrangements of views.
+  gfx::ScopedNSDisableScreenUpdates ns_disabler;
   // Make sure that any layers that move are not animated to their new
   // positions.
-  ScopedCAActionDisabler disabler;
+  ScopedCAActionDisabler ca_disabler;
 
   // Resize the new view to fit the window. Calling |view| may lazily
   // instantiate the TabContentsController from the nib. Until we call
