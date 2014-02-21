1e281f9b0e922f48aa23d22ef662e7a783cf1b67
rvargas@chromium.org <rvargas@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-21 16:07:52 +0000

    Remove PlatformFile from courgette
    BUG=322664
    R=tommi@chromium.org
    
    Review URL: https://codereview.chromium.org/166273021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252570 0039d316-1c4b-4281-b951-d872f2087c98

--- courgette/memory_allocator.h
+++ courgette/memory_allocator.h
@@ -9,9 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/basictypes.h"
+#include "base/files/file.h"
 #include "base/files/file_path.h"
 #include "base/logging.h"
-#include "base/platform_file.h"
 
 #ifndef NDEBUG
 
@@ -57,30 +57,6 @@ typedef bool CheckBool;
 namespace courgette {
 
 #if defined(OS_WIN)
-
-// Manages a temporary file.  The file is created in the %TEMP% folder and
-// is deleted when the file handle is closed.
-// NOTE: Since the file will be used as backing for a memory allocation,
-// it will never be so big that size_t cannot represent its size.
-class TempFile {
- public:
-  TempFile();
-  ~TempFile();
-
-  bool Create();
-  void Close();
-  bool SetSize(size_t size);
-
-  // Returns true iff the temp file is currently open.
-  bool valid() const;
-
-  // Returns the handle of the temporary file or INVALID_HANDLE_VALUE if
-  // a temp file has not been created.
-  base::PlatformFile handle() const;
-
- protected:
-  base::PlatformFile file_;
-};
 
 // Manages a read/write virtual mapping of a physical file.
 class FileMapping {
@@ -131,7 +107,7 @@ class TempMapping {
   static TempMapping* GetMappingFromPtr(void* mem);
 
  protected:
-  TempFile file_;
+  base::File file_;
   FileMapping mapping_;
 };
 
