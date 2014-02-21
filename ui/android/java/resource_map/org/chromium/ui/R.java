d889a935a7db361357f3ee8ddffa74754366319b
jdduke@chromium.org <jdduke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-21 20:53:26 +0000

    [Android] Provide fallback scaling constants in ViewConfiguration
    Certain devices may lack the internal Android dimension lookups for
    MinScalingSpan and MinScalingTouchMajor.  Provide sensible default backups
    for this case.
    
    BUG=345602
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/175493002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252641 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/android/java/resource_map/org/chromium/ui/R.java
+++ ui/android/java/resource_map/org/chromium/ui/R.java
@@ -74,5 +74,7 @@ public final class R {
         public static int autofill_text_height;
         public static int autofill_text_divider_height;
         public static int color_button_height;
+        public static int config_minScalingSpan;
+        public static int config_minScalingTouchMajor;
     }
 }
