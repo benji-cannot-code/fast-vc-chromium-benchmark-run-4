20b5c638c9f4aa727a7d9d16124867f982611ec0
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-22 07:41:13 +0000

    Changed tags to match caps printers.
    Review URL: https://codereview.chromium.org/174433003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252754 0039d316-1c4b-4281-b951-d872f2087c98

--- printing/backend/print_backend_consts.cc
+++ printing/backend/print_backend_consts.cc
@@ -7,11 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "printing/backend/print_backend_consts.h"
 
-const char kLocationTagName[] = "location";
-const char kDriverNameTagName[] = "drivername";
-const char kDriverInfoTagName[] = "system_driverinfo";
-const char kCUPSPrintServerURL[] = "print_server_url";
 const char kCUPSBlocking[] = "cups_blocking";
 const char kCUPSEncryption[] = "cups_encryption";
-const char kValueTrue[] = "true";
+const char kCUPSPrintServerURL[] = "print_server_url";
+const char kDriverInfoTagName[] = "system_driverinfo";
+const char kDriverNameTagName[] = "printer-make-and-model";  // Match CUPS.
+const char kLocationTagName[] = "printer-location";  // Match CUPS.
 const char kValueFalse[] = "false";
+const char kValueTrue[] = "true";
