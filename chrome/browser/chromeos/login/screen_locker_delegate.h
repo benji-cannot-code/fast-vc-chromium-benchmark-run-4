021666396a9ae7c7df01d0486d5ea8e56385f76f
tengs@chromium.org <tengs@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-22 07:51:53 +0000

    Refactor user pods to use authType property for distinct authentication modes.
    The authType of the user pod determines the authentication method to sign in
    or unlock from the account picker screen. This refactoring aims to unify the
    current two modes the user pod can use, and make the user pod more
    extensible for other modes.
    
    For example, the user pod may currently authenticate using a password or online
    GAIA signin. This CL introduces a full-pod user click authType, and a PIN
    code authType will be implemented in the future (see bug). This CL also adds
    plumbing for the authType to be set by the ScreenLocker for the
    chrome.screenlockPrivate API.
    
    BUG=344179
    TEST=visual inspection
    
    Review URL: https://codereview.chromium.org/168813002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252762 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/screen_locker_delegate.h
+++ chrome/browser/chromeos/login/screen_locker_delegate.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_forward.h"
 #include "base/strings/string16.h"
 #include "chrome/browser/chromeos/login/help_app_launcher.h"
+#include "chrome/browser/chromeos/login/login_display.h"
 #include "ui/gfx/native_widget_types.h"
 
 class GURL;
@@ -52,6 +53,18 @@ class ScreenLockerDelegate {
   virtual void ShowUserPodButton(const std::string& username,
                                  const std::string& iconURL,
                                  const base::Closure& click_callback) = 0;
+
+  // Hides the user pod button for a user.
+  virtual void HideUserPodButton(const std::string& username) = 0;
+
+  // Set the authentication type to be used on the lock screen.
+  virtual void SetAuthType(const std::string& username,
+                           LoginDisplay::AuthType auth_type,
+                           const std::string& initial_value) = 0;
+
+  // Returns the authentication type used for |username|.
+  virtual LoginDisplay::AuthType GetAuthType(const std::string& username)
+      const = 0;
 
   // Disables all UI needed and shows error bubble with |message|.
   // If |sign_out_only| is true then all other input except "Sign Out"
--- chrome/browser/ui/webui/chromeos/login/signin_screen_handler.h
+++ chrome/browser/ui/webui/chromeos/login/signin_screen_handler.h
@@ -80,6 +80,12 @@ class LoginDisplayWebUIHandler {
   virtual void ShowUserPodButton(const std::string& username,
                                  const std::string& iconURL,
                                  const base::Closure& click_callback) = 0;
+  virtual void HideUserPodButton(const std::string& username) = 0;
+  virtual void SetAuthType(const std::string& username,
+                           LoginDisplay::AuthType auth_type,
+                           const std::string& initial_value) = 0;
+  virtual LoginDisplay::AuthType GetAuthType(const std::string& username)
+      const = 0;
   virtual void ShowError(int login_attempts,
                          const std::string& error_text,
                          const std::string& help_link_text,
@@ -280,6 +286,12 @@ class SigninScreenHandler
   virtual void ShowUserPodButton(const std::string& username,
                                  const std::string& iconURL,
                                  const base::Closure& click_callback) OVERRIDE;
+  virtual void HideUserPodButton(const std::string& username) OVERRIDE;
+  virtual void SetAuthType(const std::string& username,
+                           LoginDisplay::AuthType auth_type,
+                           const std::string& initial_value) OVERRIDE;
+  virtual LoginDisplay::AuthType GetAuthType(const std::string& username)
+      const OVERRIDE;
   virtual void ShowError(int login_attempts,
                          const std::string& error_text,
                          const std::string& help_link_text,
@@ -371,6 +383,7 @@ class SigninScreenHandler
   static void FillUserDictionary(User* user,
                                  bool is_owner,
                                  bool is_signin_to_add,
+                                 LoginDisplay::AuthType auth_type,
                                  base::DictionaryValue* user_dict);
 
   // Sends user list to account picker.
@@ -526,6 +539,10 @@ class SigninScreenHandler
 
   // Map of callbacks run when the custom button on a user pod is clicked.
   std::map<std::string, base::Closure> user_pod_button_callback_map_;
+
+  // Map of usernames to their current authentication type. If a user is not
+  // contained in the map, it is using the default authentication type.
+  std::map<std::string, LoginDisplay::AuthType> user_auth_type_map_;
 
   // Non-owning ptr.
   // TODO (ygorshenin@): remove this dependency.
