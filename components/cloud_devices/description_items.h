34b6cd7fa707a26768f996a50cb33814b0072b56
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-22 07:40:37 +0000

    Use to base::checked_cast.
    Review URL: https://codereview.chromium.org/171683020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252753 0039d316-1c4b-4281-b951-d872f2087c98

--- components/cloud_devices/description_items.h
+++ components/cloud_devices/description_items.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/logging.h"
-
+#include "base/numerics/safe_conversions.h"
 #include "components/cloud_devices/cloud_device_description.h"
 
 namespace base {
@@ -134,7 +134,7 @@ class SelectionCapability {
     if (is_default) {
       DCHECK_EQ(default_idx_, -1);
       // Point to the last element.
-      default_idx_ = static_cast<int>(size());
+      default_idx_ = base::checked_cast<int>(size());
     }
     options_.push_back(option);
   }
--- components/cloud_devices/description_items_inl.h
+++ components/cloud_devices/description_items_inl.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "base/numerics/safe_conversions.h"
 #include "components/cloud_devices/description_items.h"
 
 // Implementation of templates defined in header file.
@@ -84,7 +85,7 @@ bool SelectionCapability<Option, Traits>::IsValid() const {
     if (!Traits::IsValid(options_[i]))
       return false;
   }
-  return default_idx_ >= 0 && default_idx_ < static_cast<int>(size());
+  return default_idx_ >= 0 && default_idx_ < base::checked_cast<int>(size());
 }
 
 template <class Option, class Traits>
@@ -125,7 +126,7 @@ void SelectionCapability<Option, Traits>::SaveTo(
   for (size_t i = 0; i < options_.size(); ++i) {
     base::DictionaryValue* option_value = new base::DictionaryValue;
     options_list->Append(option_value);
-    if (static_cast<int>(i) == default_idx_)
+    if (base::checked_cast<int>(i) == default_idx_)
       option_value->SetBoolean(json::kKeyIsDefault, true);
     Traits::Save(options_[i], option_value);
   }
