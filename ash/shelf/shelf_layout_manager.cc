75c1bf9ea2954daf4d048f235730c9427353b2e4
abodenha@chromium.org <abodenha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-22 08:35:09 +0000

    Fixed incorrect resize of docked windows when locking screen.
    Switches updating of state_.is_screen_locked to push rather than pull.
    is_screen_locked is set when the lock animation starts and unset when
    OnLockStateChanged is called with locked unset.
    If is_screen_locked is set avoid generating notifications about inset
    changes.
    
    BUG=338377
    
    Review URL: https://codereview.chromium.org/175473003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252785 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/shelf/shelf_layout_manager.cc
+++ ash/shelf/shelf_layout_manager.cc
@@ -557,6 +557,7 @@ void ShelfLayoutManager::SetChildBounds(aura::Window* child,
 void ShelfLayoutManager::OnLockStateChanged(bool locked) {
   // Force the shelf to layout for alignment (bottom if locked, restore
   // the previous alignment otherwise).
+  state_.is_screen_locked = locked;
   shelf_->SetAlignment(locked ? SHELF_ALIGNMENT_BOTTOM : alignment_);
   UpdateVisibilityState();
   LayoutShelf();
@@ -591,8 +592,6 @@ void ShelfLayoutManager::SetState(ShelfVisibilityState visibility_state) {
   State state;
   state.visibility_state = visibility_state;
   state.auto_hide_state = CalculateAutoHideState(visibility_state);
-  state.is_screen_locked =
-      Shell::GetInstance()->session_state_delegate()->IsScreenLocked();
   state.window_state = workspace_controller_ ?
       workspace_controller_->GetWindowState() : WORKSPACE_WINDOW_STATE_DEFAULT;
 
@@ -722,8 +721,10 @@ void ShelfLayoutManager::UpdateBoundsAndOpacity(
       ScreenUtil::ConvertRectToScreen(
           shelf_->status_area_widget()->GetNativeView()->parent(),
           status_bounds));
-  Shell::GetInstance()->SetDisplayWorkAreaInsets(
-      root_window_, target_bounds.work_area_insets);
+  if (!state_.is_screen_locked) {
+    Shell::GetInstance()->SetDisplayWorkAreaInsets(
+        root_window_, target_bounds.work_area_insets);
+  }
 }
 
 void ShelfLayoutManager::StopAnimating() {
@@ -1148,6 +1149,9 @@ void ShelfLayoutManager::OnDockBoundsChanging(
 
 void ShelfLayoutManager::OnLockStateEvent(LockStateObserver::EventType event) {
   if (event == EVENT_LOCK_ANIMATION_STARTED) {
+    // Enter the screen locked state as the animation starts to prevent
+    // layout changes as the screen locks.
+    state_.is_screen_locked = true;
     // Hide the status area widget (using auto hide animation).
     base::AutoReset<ShelfVisibilityState> state(&state_.visibility_state,
                                                 SHELF_HIDDEN);
