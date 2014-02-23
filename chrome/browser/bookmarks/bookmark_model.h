288d6af6b0e661b5e352bdd007716570048af10f
tom.cassiotis@gmail.com <tom.cassiotis@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-23 00:33:50 +0000

    Eliminate Bookmarks dependence to bookmark_undo_utils.h.
    Introduced a mechanism through BookmarkModelObserver to inform the
    bookmark undo service to group a set of bookmark changes instead of an
    explicit call.
    
    This removes the dependence to bookmark_undo_utils that was recently
    introduced in
    https://src.chromium.org/viewvc/chrome?view=rev&revision=241973
    
    BUG=126092
    TEST=Added a unit_tests test to ensure that the BookmarkModelObserver is notified.
    
    Review URL: https://codereview.chromium.org/155913004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252832 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/bookmarks/bookmark_model.h
+++ chrome/browser/bookmarks/bookmark_model.h
@@ -35,6 +35,7 @@ class BookmarkModelObserver;
 class BookmarkStorage;
 struct BookmarkTitleMatch;
 class Profile;
+class ScopedGroupBookmarkActions;
 
 namespace base {
 class SequencedTaskRunner;
@@ -440,6 +441,7 @@ class BookmarkModel : public content::NotificationObserver,
   friend class BookmarkCodecTest;
   friend class BookmarkModelTest;
   friend class BookmarkStorage;
+  friend class ScopedGroupBookmarkActions;
 
   // Used to order BookmarkNodes by URL.
   class NodeURLComparator {
@@ -514,6 +516,11 @@ class BookmarkModel : public content::NotificationObserver,
 
   // If we're waiting on a favicon for node, the load request is canceled.
   void CancelPendingFaviconLoadRequests(BookmarkNode* node);
+
+  // Notifies the observers that a set of changes initiated by a single user
+  // action is about to happen and has completed.
+  void BeginGroupedChanges();
+  void EndGroupedChanges();
 
   // content::NotificationObserver:
   virtual void Observe(int type,
