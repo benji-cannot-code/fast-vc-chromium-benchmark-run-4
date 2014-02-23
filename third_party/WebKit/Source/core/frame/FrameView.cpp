342c8b5fe4c0326de4a29ade4527f268814d558b
dcheng@chromium.org <dcheng@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-23 10:43:34 +0000

    Disable compositing query asserts in FrameView::setTransparent.
    Followup to r167636.
    
    BUG=none
    TBR=ojan@chromium.org
    
    Review URL: https://codereview.chromium.org/176543002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167645 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/FrameView.cpp
+++ third_party/WebKit/Source/core/frame/FrameView.cpp
@@ -1893,6 +1893,7 @@ bool FrameView::isTransparent() const
 void FrameView::setTransparent(bool isTransparent)
 {
     m_isTransparent = isTransparent;
+    DisableCompositingQueryAsserts disabler;
     if (renderView() && renderView()->layer()->hasCompositedLayerMapping())
         renderView()->layer()->compositedLayerMapping()->updateContentsOpaque();
 }
