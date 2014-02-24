34121bed8cf768a38fc3882c19b0f58071ad3da3
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-24 21:29:49 +0000

    GN: Work on Linux build defines
    This makes the -D defines for the default desktop Linux build of re2 in GN match that of GYP.
    
    This moves the SSL configuration stuff into the root build directory. It's better to match the GYP build exactly for ease of landing even though this is a stupid dependency.
    
    Fixes the help text that still references the implicit "gen" command (I removed this).
    
    R=scottmg@chromium.org
    
    Review URL: https://codereview.chromium.org/170533003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252981 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/gn/command_help.cc
+++ tools/gn/command_help.cc
@@ -31,8 +31,6 @@ void PrintToplevelHelp() {
 
   OutputString(
       "\n"
-      "  When run with no arguments \"gn gen\" is assumed.\n"
-      "\n"
       "Common switches:\n");
   PrintShortHelp(
       "--args: Specifies build arguments overrides. "
