423c71d1606c3ca6a25982361bd5a8acefdf627a
abodenha@chromium.org <abodenha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-24 21:30:47 +0000

    Remove --ash-disable-auto-maximizing flag.
    BUG=340466
    
    Review URL: https://codereview.chromium.org/173883005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252984 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/window_positioner.cc
+++ ash/wm/window_positioner.cc
@@ -41,7 +41,6 @@ namespace {
 // equal to this width, the window will get opened in maximized mode. This value
 // can be reduced to a "tame" number if the feature is disabled.
 const int kForceMaximizeWidthLimit = 1366;
-const int kForceMaximizeWidthLimitDisabled = 640;
 
 // The time in milliseconds which should be used to visually move a window
 // through an automatic "intelligent" window management option.
@@ -202,13 +201,7 @@ aura::Window* GetReferenceWindow(const aura::Window* root_window,
 
 // static
 int WindowPositioner::GetForceMaximizedWidthLimit() {
-  static int maximum_limit = 0;
-  if (!maximum_limit) {
-    maximum_limit = CommandLine::ForCurrentProcess()->HasSwitch(
-        switches::kAshDisableAutoMaximizing) ?
-        kForceMaximizeWidthLimitDisabled : kForceMaximizeWidthLimit;
-  }
-  return maximum_limit;
+  return kForceMaximizeWidthLimit;
 }
 
 // static
