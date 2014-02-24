e023fcc515808e50b587f0a9d063bd573d81b596
tommyw@chromium.org <tommyw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-24 12:54:11 +0000

    MediaStream API: Update getUserMedia to match the spec Both the success and the fail callbacks are mandatory and may not be null. Also taking the opportunity to clean up the code and add proper exceptions where needed as promised.
    BUG=344874
    
    Review URL: https://codereview.chromium.org/173893009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167685 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/mediastream/UserMediaRequest.cpp
+++ third_party/WebKit/Source/modules/mediastream/UserMediaRequest.cpp
@@ -35,8 +35,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "modules/mediastream/UserMediaRequest.h"
 
 #include "bindings/v8/Dictionary.h"
+#include "bindings/v8/ExceptionMessages.h"
 #include "bindings/v8/ExceptionState.h"
 #include "core/dom/Document.h"
+#include "core/dom/ExceptionCode.h"
 #include "core/dom/SpaceSplitString.h"
 #include "modules/mediastream/MediaConstraintsImpl.h"
 #include "modules/mediastream/MediaStream.h"
@@ -74,8 +76,10 @@ PassRefPtr<UserMediaRequest> UserMediaRequest::create(ExecutionContext* context,
     if (exceptionState.hadException())
         return nullptr;
 
-    if (audio.isNull() && video.isNull())
+    if (audio.isNull() && video.isNull()) {
+        exceptionState.throwDOMException(SyntaxError, "At least one of audio and video must be requested");
         return nullptr;
+    }
 
     return adoptRef(new UserMediaRequest(context, controller, audio, video, successCallback, errorCallback));
 }
@@ -154,10 +158,8 @@ void UserMediaRequest::fail(const String& description)
     if (!executionContext())
         return;
 
-    if (m_errorCallback) {
-        RefPtr<NavigatorUserMediaError> error = NavigatorUserMediaError::create(NavigatorUserMediaError::NamePermissionDenied, description, String());
-        m_errorCallback->handleEvent(error.get());
-    }
+    RefPtr<NavigatorUserMediaError> error = NavigatorUserMediaError::create(NavigatorUserMediaError::NamePermissionDenied, description, String());
+    m_errorCallback->handleEvent(error.get());
 }
 
 void UserMediaRequest::failConstraint(const String& constraintName, const String& description)
@@ -166,10 +168,8 @@ void UserMediaRequest::failConstraint(const String& constraintName, const String
     if (!executionContext())
         return;
 
-    if (m_errorCallback) {
-        RefPtr<NavigatorUserMediaError> error = NavigatorUserMediaError::create(NavigatorUserMediaError::NameConstraintNotSatisfied, description, constraintName);
-        m_errorCallback->handleEvent(error.get());
-    }
+    RefPtr<NavigatorUserMediaError> error = NavigatorUserMediaError::create(NavigatorUserMediaError::NameConstraintNotSatisfied, description, constraintName);
+    m_errorCallback->handleEvent(error.get());
 }
 
 void UserMediaRequest::contextDestroyed()
