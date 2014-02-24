65d4c1b1391615c0cf2a1a3e363fdce9c3315770
creis@chromium.org <creis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-24 22:33:18 +0000

    Revert 251563 "Move browser initiated navigation from RenderView..."
    Speculatively revert to help diagnose bug 345757.
    Mainly useful to make it easier to revert r249676, which changes the
    lifetime of RenderViewHosts.
    
    > Move browser initiated navigation from RenderViewHost to RenderFrameHost.
    > 
    > BUG=304341
    > TBR=jochen@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/148083013
    
    TBR=nasko@chromium.org
    
    Review URL: https://codereview.chromium.org/177713006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253010 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_frame_impl.h
+++ content/renderer/render_frame_impl.h
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class TransportDIB;
 struct FrameMsg_BuffersSwapped_Params;
 struct FrameMsg_CompositorFrameSwapped_Params;
-struct FrameMsg_Navigate_Params;
 
 namespace blink {
 class WebInputEvent;
@@ -331,10 +330,6 @@ class CONTENT_EXPORT RenderFrameImpl
 
   // TODO(jam): move this to WebFrameClient
   virtual void showContextMenu(const blink::WebContextMenuData& data);
-
-  // TODO(nasko): Make all tests in RenderViewImplTest friends and then move
-  // this back to private member.
-  void OnNavigate(const FrameMsg_Navigate_Params& params);
 
  protected:
   RenderFrameImpl(RenderViewImpl* render_view, int32 routing_id);
