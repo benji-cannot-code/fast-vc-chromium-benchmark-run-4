e94ea9b4031f92e85d9fe7af9fd8ea100d35ebc7
hirono@chromium.org <hirono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-24 08:00:19 +0000

    Files.app: Split the test_case.js file.
    Currently the test cases of Files.app's browser test are contained in the single
    file. This CL split the huge file.
    
    BUG=none
    TEST=browser_tests
    R=mtomasz@chromium.org
    
    Review URL: https://codereview.chromium.org/168043005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252890 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/api_test/file_manager_browsertest/hide_search_box.js
@@ -0,0 +1,39 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+/**
+ * Tests hiding the search box.
+ */
+testcase.hideSearchBox = function() {
+  var appId;
+  StepsRunner.run([
+    // Set up File Manager.
+    function() {
+      setupAndWaitUntilReady(null, RootPath.DOWNLOADS, this.next);
+    },
+    // Resize the window.
+    function(inAppId, inFileListBefore) {
+      appId = inAppId;
+      callRemoteTestUtil('resizeWindow', appId, [100, 100], this.next);
+    },
+    // Wait for the style change.
+    function(result) {
+      chrome.test.assertTrue(result);
+      callRemoteTestUtil('waitForStyles',
+                         appId,
+                         [{
+                            query: '#search-box',
+                            styles: {visibility: 'hidden'}
+                          }],
+                         this.next);
+    },
+    // Check the styles
+    function() {
+      checkIfNoErrorsOccured(this.next);
+    }
+  ]);
+};
