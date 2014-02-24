64cbe14819b15b783b8120f7625a23566cef9b06
nbarth@chromium.org <nbarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-24 03:12:04 +0000

    IDL compiler: [ImplementedAs] in [SetWrapperReferenceTo]
    * use cpp_type instead of hard-coded raw pointer
    * also fix test cases so TestObject/TestObjectPython has *no* extended
      attributes on the interface (those go on TestInterface)
    
    BUG=239771
    TBR=haraken
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/176873002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167655 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/tests/results/V8TestInterfacePython.cpp
+++ third_party/WebKit/Source/bindings/tests/results/V8TestInterfacePython.cpp
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "V8Node.h"
 #include "V8TestImplementedAs.h"
 #include "V8TestInterfaceEmpty.h"
-#include "V8TestObjectPython.h"
 #include "bindings/tests/idls/TestImplements.h"
 #include "bindings/tests/idls/TestImplements2Implementation.h"
 #include "bindings/tests/idls/TestPartialInterfacePython.h"
@@ -894,11 +893,11 @@ void V8TestInterfacePython::visitDOMWrapper(void* object, const v8::Persistent<v
     TestInterfacePythonImplementation* impl = fromInternalPointer(object);
     v8::Local<v8::Object> creationContext = v8::Local<v8::Object>::New(isolate, wrapper);
     V8WrapperInstantiationScope scope(creationContext, isolate);
-    TestObjectPython* referencedName = impl->referencedName();
+    TestInterfacePythonImplementation* referencedName = impl->referencedName();
     if (referencedName) {
-        if (!DOMDataStore::containsWrapper<V8TestObjectPython>(referencedName, isolate))
+        if (!DOMDataStore::containsWrapper<V8TestInterfacePython>(referencedName, isolate))
             wrap(referencedName, creationContext, isolate);
-        DOMDataStore::setWrapperReference<V8TestObjectPython>(wrapper, referencedName, isolate);
+        DOMDataStore::setWrapperReference<V8TestInterfacePython>(wrapper, referencedName, isolate);
     }
     setObjectGroup(object, wrapper, isolate);
 }
