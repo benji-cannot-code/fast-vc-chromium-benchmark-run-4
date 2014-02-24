485375e105e5830a7fb3aace95a10b92b12ac19d
mahesh.kk@samsung.com <mahesh.kk@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-24 03:36:16 +0000

    Add WebDocument::focusedElement.
    WebDocument::focusedNode should be focusedElement.
    DOM, HTML, SVG specifications assume only elements can be focused.
    Blink exposes focusedNode api and returns WebNode casting from Element*.
    This change would make a cleaner API and expose focused
    WebElement which is more rich than WebNode.
    
    BUG=258763
    
    Review URL: https://codereview.chromium.org/173283003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167657 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/tests/WebFrameTest.cpp
+++ third_party/WebKit/Source/web/tests/WebFrameTest.cpp
@@ -2923,7 +2923,7 @@ TEST_F(WebFrameTest, FindInPage)
     WebRange range = frame->selectionRange();
     EXPECT_EQ(5, range.startOffset());
     EXPECT_EQ(9, range.endOffset());
-    EXPECT_TRUE(frame->document().focusedNode().isNull());
+    EXPECT_TRUE(frame->document().focusedElement().isNull());
 
     // Find in an <input> value.
     EXPECT_TRUE(frame->find(findIdentifier, WebString::fromUTF8("bar2"), options, false, 0));
@@ -2933,7 +2933,7 @@ TEST_F(WebFrameTest, FindInPage)
     ASSERT_FALSE(range.isNull());
     EXPECT_EQ(5, range.startOffset());
     EXPECT_EQ(9, range.endOffset());
-    EXPECT_EQ(WebString::fromUTF8("INPUT"), frame->document().focusedNode().nodeName());
+    EXPECT_EQ(WebString::fromUTF8("INPUT"), frame->document().focusedElement().tagName());
 
     // Find in a <textarea> content.
     EXPECT_TRUE(frame->find(findIdentifier, WebString::fromUTF8("bar3"), options, false, 0));
@@ -2943,7 +2943,7 @@ TEST_F(WebFrameTest, FindInPage)
     ASSERT_FALSE(range.isNull());
     EXPECT_EQ(5, range.startOffset());
     EXPECT_EQ(9, range.endOffset());
-    EXPECT_EQ(WebString::fromUTF8("TEXTAREA"), frame->document().focusedNode().nodeName());
+    EXPECT_EQ(WebString::fromUTF8("TEXTAREA"), frame->document().focusedElement().tagName());
 
     // Find in a contentEditable element.
     EXPECT_TRUE(frame->find(findIdentifier, WebString::fromUTF8("bar4"), options, false, 0));
@@ -2954,7 +2954,7 @@ TEST_F(WebFrameTest, FindInPage)
     EXPECT_EQ(0, range.startOffset());
     EXPECT_EQ(4, range.endOffset());
     // "bar4" is surrounded by <span>, but the focusable node should be the parent <div>.
-    EXPECT_EQ(WebString::fromUTF8("DIV"), frame->document().focusedNode().nodeName());
+    EXPECT_EQ(WebString::fromUTF8("DIV"), frame->document().focusedElement().tagName());
 
     // Find in <select> content.
     EXPECT_FALSE(frame->find(findIdentifier, WebString::fromUTF8("bar5"), options, false, 0));
