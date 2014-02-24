db00abf03d312180017ef8b7aec3cbed1e6ac917
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-24 18:30:27 +0000

    aura: Make aura_unittests opt into real GL NullDraw contexts.
    This breaks the dependency on the TestCompositor for
    this test suite. Once it's no longer in use by any test
    suite, we can remove it.
    
    R=sky@chromium.org,ben@chromium.org
    BUG=270918
    
    Review URL: https://codereview.chromium.org/172623003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252945 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/test/run_all_unittests.cc
+++ ui/aura/test/run_all_unittests.cc
@@ -6,9 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/test/launcher/unit_test_launcher.h"
 #include "base/test/test_suite.h"
+#include "ui/gl/gl_surface.h"
 
 int main(int argc, char** argv) {
   base::TestSuite test_suite(argc, argv);
+  gfx::GLSurface::InitializeOneOffForTests(true);
 
   return base::LaunchUnitTests(
       argc,
