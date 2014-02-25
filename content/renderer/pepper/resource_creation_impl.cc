f4445f811f6837377a4099227be98d320a5fff10
teravest@chromium.org <teravest@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-25 09:49:20 +0000

    Pepper: Remove in-process support for PPB_Audio.
    This is the first in a series of changes to refactor support for PPB_Audio. We
    don't need in-process support, so this gets the support for that out of the way
    in a change that can be easily reverted if necessary.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/170303005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253102 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/pepper/resource_creation_impl.cc
+++ content/renderer/pepper/resource_creation_impl.cc
@@ -38,9 +38,7 @@ PP_Resource ResourceCreationImpl::CreateAudio1_0(
     PP_Resource config_id,
     PPB_Audio_Callback_1_0 audio_callback,
     void* user_data) {
-  return PPB_Audio_Impl::Create(
-      instance, config_id, ppapi::AudioCallbackCombined(audio_callback),
-      user_data);
+  return 0;  // Not supported in-process.
 }
 
 PP_Resource ResourceCreationImpl::CreateAudio(
@@ -48,9 +46,7 @@ PP_Resource ResourceCreationImpl::CreateAudio(
     PP_Resource config_id,
     PPB_Audio_Callback audio_callback,
     void* user_data) {
-  return PPB_Audio_Impl::Create(
-      instance, config_id, ppapi::AudioCallbackCombined(audio_callback),
-      user_data);
+  return 0;  // Not supported in-process.
 }
 
 PP_Resource ResourceCreationImpl::CreateAudioConfig(
