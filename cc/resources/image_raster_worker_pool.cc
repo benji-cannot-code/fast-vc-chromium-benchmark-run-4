09ffc339a72110caecd4fc498c009d3caf33e50a
reveman@chromium.org <reveman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-25 09:59:09 +0000

    cc: Remove task references from RasterWorkerPool.
    This moves the responsibility to keep tasks alive while scheduled
    from the RasterWorkerPool to the client where unnecessary reference
    counting can be avoided. The result is a ~5x improvement in
    BuildRasterTaskQueue performance, which under some circumstances
    translate to almost 2x improvement in ScheduleTasks performance.
    
    BUG=269841
    TEST=cc_perftests --gtest_filter=RasterWorkerPool*
    
    Review URL: https://codereview.chromium.org/173953003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253109 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/resources/image_raster_worker_pool.cc
+++ cc/resources/image_raster_worker_pool.cc
@@ -87,8 +87,6 @@ void ImageRasterWorkerPool::ScheduleTasks(RasterTaskQueue* queue) {
                     kRasterFinishedTaskPriority,
                     queue->items.size());
 
-  raster_tasks_.Swap(queue);
-
   SetTaskGraph(&graph_);
 
   set_raster_finished_task(new_raster_finished_task);
