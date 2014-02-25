45da44589868239779819a9163c183ce945ed2dc
raymes@google.com <raymes@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-25 00:23:28 +0000

    Hook up the password screen for the out of process PDF plugin.
    This hooks up the password screen which is shown if the document is password protected.
    It responds to requests to passwords for the plugin and responds with the password
    text if it is accepted.
    
    BUG=303491
    R=arv@chromium.org, koz@chromium.org
    
    Review URL: https://codereview.chromium.org/165973003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253034 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/pdf/pdf.js
+++ chrome/browser/resources/pdf/pdf.js
@@ -34,6 +34,9 @@ var viewerErrorScreen;
 // The viewport object.
 var viewport;
 
+// The element displaying the password screen.
+var viewerPasswordScreen;
+
 // The document dimensions.
 var documentDimensions;
 
@@ -56,7 +59,18 @@ function updateProgress(progress) {
     viewerErrorScreen.style.visibility = 'visible';
     sizer.style.display = 'none';
     viewerToolbar.style.visibility = 'hidden';
+    if (viewerPasswordScreen.active) {
+      viewerPasswordScreen.deny();
+      viewerPasswordScreen.active = false;
+    }
   }
+}
+
+function onPasswordSubmitted(event) {
+  plugin.postMessage({
+    type: 'getPasswordComplete',
+    password: event.detail.password
+  });
 }
 
 // Called when a message is received from the plugin.
@@ -66,6 +80,11 @@ function handleMessage(message) {
       documentDimensions = message.data;
       viewport.setDocumentDimensions(documentDimensions);
       viewerToolbar.style.visibility = 'visible';
+      // If we received the document dimensions, the password was good so we can
+      // dismiss the password screen.
+      if (viewerPasswordScreen.active)
+        viewerPasswordScreen.accept();
+
       viewerPageIndicator.initialFadeIn();
       viewerToolbar.initialFadeIn();
       break;
@@ -75,6 +94,13 @@ function handleMessage(message) {
     case 'goToPage':
       viewport.goToPage(message.data.page);
       break;
+    case 'getPassword':
+      // If the password screen isn't up, put it up. Otherwise we're responding
+      // to an incorrect password so deny it.
+      if (!viewerPasswordScreen.active)
+        viewerPasswordScreen.active = true;
+      else
+        viewerPasswordScreen.deny();
   }
 }
 
@@ -114,6 +140,9 @@ function load() {
   viewerToolbar = $('toolbar');
   viewerPageIndicator = $('page-indicator');
   viewerProgressBar = $('progress-bar');
+  viewerPasswordScreen = $('password-screen');
+  viewerPasswordScreen.addEventListener('password-submitted',
+                                        onPasswordSubmitted);
   viewerErrorScreen = $('error-screen');
   viewerErrorScreen.text = 'Failed to load PDF document';
 
