ff42a9bd335fa72be4545e0901b5f410e53200d8
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-25 19:35:56 +0000

    Remove out of date comments from FontFallbackWin.h
    Remove old out of date comments that no longer applies from
    FontFallbackWin.h
    
    TBR=dglazkov@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/176813011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167816 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/fonts/win/FontFallbackWin.h
+++ third_party/WebKit/Source/platform/fonts/win/FontFallbackWin.h
@@ -29,11 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-// A collection of utilities for font handling.
-
-// FIXME: Move all methods to the files that have their callsites and remove this file.
-// *Utils files are not very WebKit-ty.
-
 #ifndef FontFallbackWin_h
 #define FontFallbackWin_h
 
