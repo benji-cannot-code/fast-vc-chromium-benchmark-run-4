5b232462a0988a497a49c3a9f17dfb715736f6e2
pilgrim@chromium.org <pilgrim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-25 02:03:03 +0000

    Move appcache_test_helper.* to content/public/test/
    BUG=338338
    TBR=darin@chromium.org
    
    Review URL: https://codereview.chromium.org/158323005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253052 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/appcache/chrome_appcache_service_unittest.cc
+++ content/browser/appcache/chrome_appcache_service_unittest.cc
@@ -12,16 +12,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/browser_thread_impl.h"
 #include "content/public/browser/resource_context.h"
 #include "content/public/test/test_browser_context.h"
+#include "content/test/appcache_test_helper.h"
 #include "net/url_request/url_request_context_getter.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "webkit/browser/appcache/appcache_database.h"
 #include "webkit/browser/appcache/appcache_storage_impl.h"
-#include "webkit/browser/appcache/appcache_test_helper.h"
 #include "webkit/browser/quota/mock_special_storage_policy.h"
 
 #include <set>
-
-using appcache::AppCacheTestHelper;
 
 namespace content {
 namespace {
--- content/test/appcache_test_helper.h
+++ content/test/appcache_test_helper.h
@@ -1,18 +1,20 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef WEBKIT_BROWSER_APPCACHE_APPCACHE_TEST_HELPER_H_
-#define WEBKIT_BROWSER_APPCACHE_APPCACHE_TEST_HELPER_H_
+#ifndef CONTENT_PUBLIC_TEST_APPCACHE_TEST_HELPER_H_
+#define CONTENT_PUBLIC_TEST_APPCACHE_TEST_HELPER_H_
 
 #include <set>
 
 #include "webkit/browser/appcache/appcache_storage.h"
 
 namespace appcache {
-
 class AppCacheService;
+}
+
+namespace content {
 
 // Helper class for inserting data into a ChromeAppCacheService and reading it
 // back.
@@ -20,10 +22,10 @@ class AppCacheTestHelper : public appcache::AppCacheStorage::Delegate {
  public:
   AppCacheTestHelper();
   virtual ~AppCacheTestHelper();
-  void AddGroupAndCache(AppCacheService* appcache_service,
+  void AddGroupAndCache(appcache::AppCacheService* appcache_service,
                         const GURL& manifest_url);
 
-  void GetOriginsWithCaches(AppCacheService* appcache_service,
+  void GetOriginsWithCaches(appcache::AppCacheService* appcache_service,
                             std::set<GURL>* origins);
  private:
   virtual void OnGroupAndNewestCacheStored(
@@ -42,6 +44,6 @@ class AppCacheTestHelper : public appcache::AppCacheStorage::Delegate {
   DISALLOW_COPY_AND_ASSIGN(AppCacheTestHelper);
 };
 
-}  // namespace appcache
+}  // namespace content
 
-#endif  // WEBKIT_BROWSER_APPCACHE_APPCACHE_TEST_HELPER_H_
+#endif  // CONTENT_PUBLIC_TEST_APPCACHE_TEST_HELPER_H_
