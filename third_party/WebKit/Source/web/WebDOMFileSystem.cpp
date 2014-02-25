4ad1a0ad35012466f7c5e3aae092c1f2a4a11383
kinuko@chromium.org <kinuko@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-25 09:03:28 +0000

    (Cleanup) Implement WebDOMFileSystem::create() etc to deprecate FileSystem-related methods in WebFrame
    We have several FileSystem-related scripting methods in WebFrame:
    - WebFrame::createFileSystem
    - WebFrame::createSerializableFileSystem
    - WebFrame::createFileEntry
    
    But they are totally unrelated to Frame and would be better be
    isolated into FileSystem-related interface.
    
    This patch adds euivalent methods to WebDOMFileSystem to deprecate them.
    
    BUG=none
    TEST=no functional changes
    
    Review URL: https://codereview.chromium.org/179373002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167757 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebDOMFileSystem.cpp
+++ third_party/WebKit/Source/web/WebDOMFileSystem.cpp
@@ -33,8 +33,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebDOMFileSystem.h"
 
 #include "V8DOMFileSystem.h"
+#include "V8DirectoryEntry.h"
+#include "V8FileEntry.h"
+#include "WebFrameImpl.h"
 #include "bindings/v8/WrapperTypeInfo.h"
+#include "core/dom/Document.h"
 #include "modules/filesystem/DOMFileSystem.h"
+#include "modules/filesystem/DirectoryEntry.h"
+#include "modules/filesystem/FileEntry.h"
 #include <v8.h>
 
 using namespace WebCore;
@@ -48,6 +54,20 @@ WebDOMFileSystem WebDOMFileSystem::fromV8Value(v8::Handle<v8::Value> value)
     v8::Handle<v8::Object> object = v8::Handle<v8::Object>::Cast(value);
     DOMFileSystem* domFileSystem = V8DOMFileSystem::toNative(object);
     ASSERT(domFileSystem);
+    return WebDOMFileSystem(domFileSystem);
+}
+
+WebDOMFileSystem WebDOMFileSystem::create(
+    WebFrame* frame,
+    WebFileSystemType type,
+    const WebString& name,
+    const WebURL& rootURL,
+    SerializableType serializableType)
+{
+    ASSERT(frame && toWebFrameImpl(frame)->frame());
+    RefPtr<DOMFileSystem> domFileSystem = DOMFileSystem::create(toWebFrameImpl(frame)->frame()->document(), name, static_cast<WebCore::FileSystemType>(type), rootURL);
+    if (serializableType == SerializableTypeSerializable)
+        domFileSystem->makeClonable();
     return WebDOMFileSystem(domFileSystem);
 }
 
@@ -89,6 +109,25 @@ WebURL WebDOMFileSystem::rootURL() const
 {
     ASSERT(m_private.get());
     return m_private->rootURL();
+}
+
+v8::Handle<v8::Value> WebDOMFileSystem::toV8Value()
+{
+    if (!m_private.get())
+        return v8::Handle<v8::Value>();
+    return toV8(m_private.get(), v8::Handle<v8::Object>(), toIsolate(m_private->executionContext()));
+}
+
+v8::Handle<v8::Value> WebDOMFileSystem::createV8Entry(
+    const WebString& path,
+    EntryType entryType)
+{
+    if (!m_private.get())
+        return v8::Handle<v8::Value>();
+    if (entryType == EntryTypeDirectory)
+        return toV8(DirectoryEntry::create(m_private.get(), path), v8::Handle<v8::Object>(), toIsolate(m_private->executionContext()));
+    ASSERT(entryType == EntryTypeFile);
+    return toV8(FileEntry::create(m_private.get(), path), v8::Handle<v8::Object>(), toIsolate(m_private->executionContext()));
 }
 
 WebDOMFileSystem::WebDOMFileSystem(const WTF::PassRefPtr<DOMFileSystem>& domFileSystem)
