6b619807508f383e6d9d7d42c9759615627ba4d7
adamk@chromium.org <adamk@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-25 15:05:43 +0000

    Make MutableStylePropertySet::m_propertyVector private
    R=ojan@chromium.org
    
    Review URL: https://codereview.chromium.org/179233002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167778 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/StylePropertySet.h
+++ third_party/WebKit/Source/core/css/StylePropertySet.h
@@ -212,8 +212,6 @@ public:
     CSSStyleDeclaration* ensureCSSStyleDeclaration();
     int findPropertyIndex(CSSPropertyID) const;
 
-    Vector<CSSProperty, 4> m_propertyVector;
-
 private:
     explicit MutableStylePropertySet(CSSParserMode);
     explicit MutableStylePropertySet(const StylePropertySet&);
@@ -224,6 +222,8 @@ private:
     OwnPtr<PropertySetCSSStyleDeclaration> m_cssomWrapper;
 
     friend class StylePropertySet;
+
+    Vector<CSSProperty, 4> m_propertyVector;
 };
 
 DEFINE_TYPE_CASTS(MutableStylePropertySet, StylePropertySet, set, set->isMutable(), set.isMutable());
