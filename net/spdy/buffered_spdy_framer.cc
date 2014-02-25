e1f643c2c0a1ee3c75b7e2ae2924e92dbd148350
jgraettinger@chromium.org <jgraettinger@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-25 05:42:05 +0000

    Implements the ACK flag for SPDY4/HTTP2 PING frames
    This lands server change 61378132 by birenroy.
    
    Also update SpdySession, BufferedSpdyFramer, etc to accept and pass
    the |is_ack| flag.
    
    BUG=345769
    
    Review URL: https://codereview.chromium.org/169283012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253088 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/buffered_spdy_framer.cc
+++ net/spdy/buffered_spdy_framer.cc
@@ -187,8 +187,8 @@ void BufferedSpdyFramer::OnSetting(SpdySettingsIds id,
   visitor_->OnSetting(id, flags, value);
 }
 
-void BufferedSpdyFramer::OnPing(SpdyPingId unique_id) {
-  visitor_->OnPing(unique_id);
+void BufferedSpdyFramer::OnPing(SpdyPingId unique_id, bool is_ack) {
+  visitor_->OnPing(unique_id, is_ack);
 }
 
 void BufferedSpdyFramer::OnRstStream(SpdyStreamId stream_id,
@@ -296,8 +296,10 @@ SpdyFrame* BufferedSpdyFramer::CreateSettings(
 }
 
 // TODO(jgraettinger): Eliminate uses of this method (prefer SpdyPingIR).
-SpdyFrame* BufferedSpdyFramer::CreatePingFrame(uint32 unique_id) const {
+SpdyFrame* BufferedSpdyFramer::CreatePingFrame(uint32 unique_id,
+                                               bool is_ack) const {
   SpdyPingIR ping_ir(unique_id);
+  ping_ir.set_is_ack(is_ack);
   return spdy_framer_.SerializePing(ping_ir);
 }
 
