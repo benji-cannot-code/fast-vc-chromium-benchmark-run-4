a05725428e5cbd926d36976bf33e32499c1dfc42
jdduke@chromium.org <jdduke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-25 13:53:32 +0000

    [Android] Expose scaling-related constants in ViewConfiguration (v2)
    This is a re-landing of the original code (r252401), with a proper fallback
    for devices that lack the hidden dimension constants.
    
    BUG=345602, 332418
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/178193002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253153 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/android/java/resource_map/org/chromium/ui/R.java
+++ ui/android/java/resource_map/org/chromium/ui/R.java
@@ -74,5 +74,7 @@ public final class R {
         public static int autofill_text_height;
         public static int autofill_text_divider_height;
         public static int color_button_height;
+        public static int config_min_scaling_span;
+        public static int config_min_scaling_touch_major;
     }
 }
--- ui/gfx/android/view_configuration.cc
+++ ui/gfx/android/view_configuration.cc
@@ -26,7 +26,9 @@ struct ViewConfigurationData {
         max_fling_velocity_in_pixels_s_(0),
         min_fling_velocity_in_pixels_s_(0),
         touch_slop_in_pixels_(0),
-        double_tap_slop_in_pixels_(0) {
+        double_tap_slop_in_pixels_(0),
+        min_scaling_span_in_pixels_(0),
+        min_scaling_touch_major_in_pixels_(0) {
     JNIEnv* env = AttachCurrentThread();
     j_view_configuration_helper_.Reset(
         Java_ViewConfigurationHelper_createWithListener(
@@ -44,7 +46,9 @@ struct ViewConfigurationData {
         Java_ViewConfigurationHelper_getScaledMaximumFlingVelocity(env, obj),
         Java_ViewConfigurationHelper_getScaledMinimumFlingVelocity(env, obj),
         Java_ViewConfigurationHelper_getScaledTouchSlop(env, obj),
-        Java_ViewConfigurationHelper_getScaledDoubleTapSlop(env, obj));
+        Java_ViewConfigurationHelper_getScaledDoubleTapSlop(env, obj),
+        Java_ViewConfigurationHelper_getScaledMinScalingSpan(env, obj),
+        Java_ViewConfigurationHelper_getScaledMinScalingTouchMajor(env, obj));
   }
 
   ~ViewConfigurationData() {}
@@ -52,12 +56,16 @@ struct ViewConfigurationData {
   void SynchronizedUpdate(int scaled_maximum_fling_velocity,
                           int scaled_minimum_fling_velocity,
                           int scaled_touch_slop,
-                          int scaled_double_tap_slop) {
+                          int scaled_double_tap_slop,
+                          int scaled_min_scaling_span,
+                          int scaled_min_scaling_touch_major) {
     base::AutoLock autolock(lock_);
     Update(scaled_maximum_fling_velocity,
            scaled_minimum_fling_velocity,
            scaled_touch_slop,
-           scaled_double_tap_slop);
+           scaled_double_tap_slop,
+           scaled_min_scaling_span,
+           scaled_min_scaling_touch_major);
   }
 
   int double_tap_timeout_in_ms() const { return double_tap_timeout_in_ms_; }
@@ -85,16 +93,30 @@ struct ViewConfigurationData {
     return double_tap_slop_in_pixels_;
   }
 
+  int min_scaling_span_in_pixels() {
+    base::AutoLock autolock(lock_);
+    return min_scaling_span_in_pixels_;
+  }
+
+  int min_scaling_touch_major_in_pixels() {
+    base::AutoLock autolock(lock_);
+    return min_scaling_touch_major_in_pixels_;
+  }
+
  private:
   void Update(int scaled_maximum_fling_velocity,
               int scaled_minimum_fling_velocity,
               int scaled_touch_slop,
-              int scaled_double_tap_slop) {
+              int scaled_double_tap_slop,
+              int scaled_min_scaling_span,
+              int scaled_min_scaling_touch_major) {
     DCHECK_LE(scaled_minimum_fling_velocity, scaled_maximum_fling_velocity);
     max_fling_velocity_in_pixels_s_ = scaled_maximum_fling_velocity;
     min_fling_velocity_in_pixels_s_ = scaled_minimum_fling_velocity;
     touch_slop_in_pixels_ = scaled_touch_slop;
     double_tap_slop_in_pixels_ = scaled_double_tap_slop;
+    min_scaling_span_in_pixels_ = scaled_min_scaling_span;
+    min_scaling_touch_major_in_pixels_ = scaled_min_scaling_touch_major;
   }
 
   base::Lock lock_;
@@ -113,6 +135,8 @@ struct ViewConfigurationData {
   int min_fling_velocity_in_pixels_s_;
   int touch_slop_in_pixels_;
   int double_tap_slop_in_pixels_;
+  int min_scaling_span_in_pixels_;
+  int min_scaling_touch_major_in_pixels_;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(ViewConfigurationData);
@@ -129,11 +153,15 @@ static void UpdateSharedViewConfiguration(JNIEnv* env,
                                           jint scaled_maximum_fling_velocity,
                                           jint scaled_minimum_fling_velocity,
                                           jint scaled_touch_slop,
-                                          jint scaled_double_tap_slop) {
+                                          jint scaled_double_tap_slop,
+                                          jint scaled_min_scaling_span,
+                                          jint scaled_min_scaling_touch_major) {
   g_view_configuration.Get().SynchronizedUpdate(scaled_maximum_fling_velocity,
                                                 scaled_minimum_fling_velocity,
                                                 scaled_touch_slop,
-                                                scaled_double_tap_slop);
+                                                scaled_double_tap_slop,
+                                                scaled_min_scaling_span,
+                                                scaled_min_scaling_touch_major);
 }
 
 int ViewConfiguration::GetDoubleTapTimeoutInMs() {
@@ -166,6 +194,14 @@ int ViewConfiguration::GetTouchSlopInPixels() {
 
 int ViewConfiguration::GetDoubleTapSlopInPixels() {
   return g_view_configuration.Get().double_tap_slop_in_pixels();
+}
+
+int ViewConfiguration::GetMinScalingSpanInPixels() {
+  return g_view_configuration.Get().min_scaling_span_in_pixels();
+}
+
+int ViewConfiguration::GetMinScalingTouchMajorInPixels() {
+  return g_view_configuration.Get().min_scaling_touch_major_in_pixels();
 }
 
 bool ViewConfiguration::RegisterViewConfiguration(JNIEnv* env) {
