2b57facbf2558ed445563c79b258967d4cb6a0ce
zerny@chromium.org <zerny@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-25 15:16:26 +0000

    Mark some classes STACK_ALLOCATED.
    Marking a class stack allocated allows an object to have pointers to GC managed objects without wrapping them in Member and defining a trace method to trace them. The Blink GC plugin for clang will identify the marked object as stack allocated and check it accordingly.
    
    Review URL: https://codereview.chromium.org/163593003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167781 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/resolver/StyleResolverState.h
+++ third_party/WebKit/Source/core/css/resolver/StyleResolverState.h
@@ -43,7 +43,7 @@ class FontDescription;
 class StyleRule;
 
 class StyleResolverState {
-DISALLOW_ALLOCATION();
+STACK_ALLOCATED();
 WTF_MAKE_NONCOPYABLE(StyleResolverState);
 public:
     StyleResolverState(Document&, Element*, RenderStyle* parentStyle = 0);
