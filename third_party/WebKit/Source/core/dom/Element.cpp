d449745d5be4796d1f9d548ca97966e8b85e2ff9
chrishtr@chromium.org <chrishtr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-25 19:38:38 +0000

    Fix crash in which elementData() might be null.
    The issue is that an SVG animation may animate the class attribute. SVG tracks attributes, including class, internally. But it still needs to track style recalc dependencies on class, so it hooks into the class change machinery in Element. In order to do so, however, elementData() mus be non-null, which requires calling invalidateSVGElement() while mutating class in the SVG code.
    
    BUG=345178
    
    Review URL: https://codereview.chromium.org/174293005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167817 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Element.cpp
+++ third_party/WebKit/Source/core/dom/Element.cpp
@@ -1088,6 +1088,7 @@ void Element::classAttributeChanged(const AtomicString& newClassString)
     StyleResolver* styleResolver = document().styleResolver();
     bool testShouldInvalidateStyle = inActiveDocument() && styleResolver && styleChangeType() < SubtreeStyleChange;
 
+    ASSERT(elementData());
     if (classStringHasClassName(newClassString)) {
         const bool shouldFoldCase = document().inQuirksMode();
         const SpaceSplitString oldClasses = elementData()->classNames();
