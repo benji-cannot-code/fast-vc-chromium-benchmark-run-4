6cf964078a103ba678c56a9f5f627f0c16de375a
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-25 21:14:39 +0000

    Mojo: Get rid of LocalMessagePipeEndpoint::MessageQueueEntry.
    Instead, add the ability to attach dispatchers directly to a
    MessageInTransit, and just use a queue of MessageInTransits.
    
    R=yzshen@chromium.org
    
    Review URL: https://codereview.chromium.org/177713007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253249 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/system/message_in_transit.h
+++ mojo/system/message_in_transit.h
@@ -8,7 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include <vector>
+
 #include "base/macros.h"
+#include "base/memory/scoped_ptr.h"
+#include "mojo/system/dispatcher.h"
 #include "mojo/system/system_impl_export.h"
 
 namespace mojo {
@@ -48,8 +52,9 @@ class MOJO_SYSTEM_IMPL_EXPORT MessageInTransit {
                    uint32_t num_bytes,
                    uint32_t num_handles,
                    const void* bytes);
-  // "Copy" constructor. The input |MessageInTransit| may own its buffer or not.
-  // The constructed |MessageInTransit| will own its buffer.
+  // "Copy" constructor. The input |MessageInTransit| may own its buffer or not;
+  // however, it must not have any dispatchers. The constructed
+  // |MessageInTransit| will own its buffer.
   MessageInTransit(OwnedBuffer,
                    const MessageInTransit& other);
 
@@ -79,6 +84,13 @@ class MOJO_SYSTEM_IMPL_EXPORT MessageInTransit {
   static bool GetNextMessageSize(const void* buffer,
                                  size_t buffer_size,
                                  size_t* next_message_size);
+
+  // Makes this message "own" the given set of dispatchers. The dispatchers must
+  // not be referenced from anywhere else (in particular, not from the handle
+  // table), i.e., each dispatcher must have a reference count of 1. This
+  // message must also own its main buffer and not already have dispatchers.
+  void SetDispatchers(
+      scoped_ptr<std::vector<scoped_refptr<Dispatcher> > > dispatchers);
 
   // Gets the "main" buffer for a |MessageInTransit|. A |MessageInTransit| can
   // be serialized by writing the main buffer. The returned pointer will be
@@ -121,6 +133,13 @@ class MOJO_SYSTEM_IMPL_EXPORT MessageInTransit {
     header()->destination_id = destination_id;
   }
 
+  // Gets the dispatchers attached to this message; this may return null if
+  // there are none. Note that the caller may mutate the set of dispatchers
+  // (e.g., take ownership of all the dispatchers, leaving the vector empty).
+  std::vector<scoped_refptr<Dispatcher> >* dispatchers() {
+    return dispatchers_.get();
+  }
+
   // TODO(vtl): Add whatever's necessary to transport handles.
 
   // Rounds |n| up to a multiple of |kMessageAlignment|.
@@ -158,6 +177,13 @@ class MOJO_SYSTEM_IMPL_EXPORT MessageInTransit {
   bool owns_main_buffer_;
   size_t main_buffer_size_;
   void* main_buffer_;
+
+  // Any dispatchers that may be attached to this message. This is only
+  // supported if this message owns its main buffer. These dispatchers should be
+  // "owned" by this message, i.e., have a ref count of exactly 1. (We allow a
+  // dispatcher entry to be null, in case it couldn't be duplicated for some
+  // reason.)
+  scoped_ptr<std::vector<scoped_refptr<Dispatcher> > > dispatchers_;
 
   DISALLOW_COPY_AND_ASSIGN(MessageInTransit);
 };
