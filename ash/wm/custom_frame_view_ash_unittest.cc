9aae33a30daf59882fa21b1cbbbab3bd0e28f350
mpearson@chromium.org <mpearson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-25 01:09:22 +0000

    Revert 253003 "This CL:"
    likely cause of this failure
    http://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Tests%20%282%29/builds/21812
    
    OpenSpecialTypes/FileManagerBrowserTest.Test/0
    OpenSpecialTypes/FileManagerBrowserTest.Test/1
    OpenSpecialTypes/FileManagerBrowserTest.Test/2
    
    > This CL:
    > - Makes app windows and browser windows use different assets for the header and window controls
    > - Removes the separator for the window controls
    > - Changes FrameCaptionButton so that the buttons use separate assets for the button background and the button icon.
    > 
    > BUG=297187
    > TEST=Visual by kuscher@
    > NOTRY=true
    > R=jamescook@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/148003003
    
    TBR=pkotwicz@chromium.org
    
    Review URL: https://codereview.chromium.org/178843005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253046 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/custom_frame_view_ash_unittest.cc
+++ ash/wm/custom_frame_view_ash_unittest.cc
@@ -62,7 +62,7 @@ TEST_F(CustomFrameViewAshTest, HeaderHeight) {
 
   ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
   gfx::ImageSkia* close_button =
-      rb.GetImageSkiaNamed(IDR_AURA_WINDOW_CONTROL_BACKGROUND_H);
+      rb.GetImageSkiaNamed(IDR_AURA_WINDOW_MAXIMIZED_CLOSE);
 
   // |kSeparatorSize| should match |kHeaderContentSeparatorSize| in
   // header_painter.cc
@@ -76,6 +76,7 @@ TEST_F(CustomFrameViewAshTest, HeaderHeight) {
             delegate->custom_frame_view()->GetHeaderView()->height());
 
   widget->Maximize();
+  close_button = rb.GetImageSkiaNamed(IDR_AURA_WINDOW_MAXIMIZED_CLOSE2);
   EXPECT_EQ(close_button->height() + kSeparatorSize,
             delegate->custom_frame_view()->GetHeaderView()->height());
 }
