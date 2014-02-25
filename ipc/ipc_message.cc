68eabeff5ea09a3770b61ba366d01848f0823b57
andrew@tullo.ch <andrew@tullo.ch@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-25 15:30:15 +0000

    Use StaticAtomicSequenceNumber instead of subtle::Atomic32 in IPCMessage.
    BUG=
    
    Review URL: https://codereview.chromium.org/177123004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253169 0039d316-1c4b-4281-b951-d872f2087c98

--- ipc/ipc_message.cc
+++ ipc/ipc_message.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ipc/ipc_message.h"
 
-#include "base/atomicops.h"
+#include "base/atomic_sequence_num.h"
 #include "base/logging.h"
 #include "build/build_config.h"
 
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-base::subtle::Atomic32 g_ref_num = 0;
+base::StaticAtomicSequenceNumber g_ref_num;
 
 // Create a reference number for identifying IPC messages in traces. The return
 // values has the reference number stored in the upper 24 bits, leaving the low
@@ -23,7 +23,7 @@ base::subtle::Atomic32 g_ref_num = 0;
 inline uint32 GetRefNumUpper24() {
   base::debug::TraceLog* trace_log = base::debug::TraceLog::GetInstance();
   int32 pid = trace_log ? trace_log->process_id() : 0;
-  int32 count = base::subtle::NoBarrier_AtomicIncrement(&g_ref_num, 1);
+  int32 count = g_ref_num.GetNext();
   // The 24 bit hash is composed of 14 bits of the count and 10 bits of the
   // Process ID. With the current trace event buffer cap, the 14-bit count did
   // not appear to wrap during a trace. Note that it is not a big deal if
