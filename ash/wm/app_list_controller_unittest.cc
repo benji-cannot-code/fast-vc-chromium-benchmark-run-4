504b5782221b32fb6a9ca8057dd2a5801f6cdac7
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-25 12:46:58 +0000

    aura: Make Window::HitTest() a private method.
    This is the first step towards removing Window::HitTest(), and related methods
    from WindowDelegate (and corresponding methods in views::WidgetDelegate). This
    is currently used only in a handful of places. This CL replaces those calls
    with equivalent code, and makes Window::HitTest() a private method, so that
    new code do not use it.
    
    BUG=318879
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/178493003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253138 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/app_list_controller_unittest.cc
+++ ash/wm/app_list_controller_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/test/test_shell_delegate.h"
 #include "ash/wm/window_util.h"
 #include "base/memory/scoped_ptr.h"
+#include "ui/aura/test/event_generator.h"
 #include "ui/aura/test/test_windows.h"
 #include "ui/aura/window.h"
 
@@ -41,6 +42,53 @@ TEST_F(AppListControllerTest, RemainVisibleWhenFocusingToApplistContainer) {
   wm::ActivateWindow(window.get());
 
   EXPECT_TRUE(Shell::GetInstance()->GetAppListTargetVisibility());
+}
+
+// Tests that clicking outside the app-list bubble closes it.
+TEST_F(AppListControllerTest, ClickOutsideBubbleClosesBubble) {
+  Shell* shell = Shell::GetInstance();
+  shell->ToggleAppList(NULL);
+
+  aura::Window* app_window = shell->GetAppListWindow();
+  ASSERT_TRUE(app_window);
+  aura::test::EventGenerator generator(shell->GetPrimaryRootWindow(),
+                                       app_window);
+  // Click on the bubble itself. The bubble should remain visible.
+  generator.ClickLeftButton();
+  EXPECT_TRUE(shell->GetAppListTargetVisibility());
+
+  // Click outside the bubble. This should close it.
+  gfx::Rect app_window_bounds = app_window->GetBoundsInRootWindow();
+  gfx::Point point_outside =
+      gfx::Point(app_window_bounds.right(), app_window_bounds.y()) +
+      gfx::Vector2d(10, 0);
+  EXPECT_TRUE(shell->GetPrimaryRootWindow()->bounds().Contains(point_outside));
+  generator.MoveMouseToInHost(point_outside);
+  generator.ClickLeftButton();
+  EXPECT_FALSE(shell->GetAppListTargetVisibility());
+}
+
+// Tests that clicking outside the app-list bubble closes it.
+TEST_F(AppListControllerTest, TapOutsideBubbleClosesBubble) {
+  Shell* shell = Shell::GetInstance();
+  shell->ToggleAppList(NULL);
+
+  aura::Window* app_window = shell->GetAppListWindow();
+  ASSERT_TRUE(app_window);
+  gfx::Rect app_window_bounds = app_window->GetBoundsInRootWindow();
+
+  aura::test::EventGenerator generator(shell->GetPrimaryRootWindow());
+  // Click on the bubble itself. The bubble should remain visible.
+  generator.GestureTapAt(app_window_bounds.CenterPoint());
+  EXPECT_TRUE(shell->GetAppListTargetVisibility());
+
+  // Click outside the bubble. This should close it.
+  gfx::Point point_outside =
+      gfx::Point(app_window_bounds.right(), app_window_bounds.y()) +
+      gfx::Vector2d(10, 0);
+  EXPECT_TRUE(shell->GetPrimaryRootWindow()->bounds().Contains(point_outside));
+  generator.GestureTapAt(point_outside);
+  EXPECT_FALSE(shell->GetAppListTargetVisibility());
 }
 
 }  // namespace ash
