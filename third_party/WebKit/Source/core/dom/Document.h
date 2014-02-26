aee9d0b5099aa79920fa25cf0792ef78be7ebe67
ch.dumez@samsung.com <ch.dumez@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-26 19:38:35 +0000

    Have Document::elementSheet() return a reference
    Have Document::elementSheet() return a reference instead of a pointer as it can
    never return null. This makes the calling code look safer.
    
    R=adamk
    
    Review URL: https://codereview.chromium.org/181663002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167921 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Document.h
+++ third_party/WebKit/Source/core/dom/Document.h
@@ -561,7 +561,7 @@ public:
     bool canNavigate(LocalFrame* targetFrame);
     LocalFrame* findUnsafeParentScrollPropagationBoundary();
 
-    CSSStyleSheet* elementSheet();
+    CSSStyleSheet& elementSheet();
 
     virtual PassRefPtr<DocumentParser> createParser();
     DocumentParser* parser() const { return m_parser.get(); }
