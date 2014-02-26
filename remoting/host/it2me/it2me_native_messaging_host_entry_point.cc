32b77e252afc7e7c19caae9bdf1b7800b79ee4e6
weitaosu@chromium.org <weitaosu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-26 14:41:03 +0000

    Move it2me native messaging host implementation to remoting_core on windows.
    This will reduce the size of remote_assistance_host.exe and thus the host download.
    The size of the debug binary went down from 22MB to 440KB.
    
    BUG=309844
    
    Review URL: https://codereview.chromium.org/178473002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253446 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ remoting/host/it2me/it2me_native_messaging_host_entry_point.cc
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "remoting/host/it2me/it2me_native_messaging_host_main.h"
+
+int main(int argc, char** argv) {
+  return remoting::It2MeNativeMessagingHostMain(argc, argv);
+}
