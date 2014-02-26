3e043bda0b86843ed29b1be84598e4d8857e4e2b
hirono@chromium.org <hirono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-26 07:05:14 +0000

    Files.app: Let onCopyProgress events report error names instead of obsoleted error codes.
    Previously onCopyProgress reports error code numbers to the
    FileOperationManager. FileOperationManager tends to refer a name property of the
    code, but it failes since the code is just a number.  In addition, the FileError
    code is obsoleted and being removed from Files.app.  Thus the CL replaces the
    code numbers with error names.
    
    BUG=277949
    TEST=manually
    
    Review URL: https://codereview.chromium.org/177183009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253382 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/background/js/file_operation_manager.js
+++ chrome/browser/resources/file_manager/background/js/file_operation_manager.js
@@ -206,7 +206,7 @@ fileOperationUtil.copyTo = function(
         case 'error':
           chrome.fileBrowserPrivate.onCopyProgress.removeListener(
               onCopyProgress);
-          errorCallback(util.createDOMError(status.error.name));
+          errorCallback(util.createDOMError(status.error));
           callback();
           break;
 
