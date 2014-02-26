19b6c1078db98e9ffc37afed1160e8ca3af4665e
calamity@chromium.org <calamity@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-26 18:32:44 +0000

    Add a flag icon to the experimental app launcher.
    To more clearly indicate when the app launcher is using its experimental UI,
    display a small flag icon in the bottom right corner of the app launcher.
    
    BUG=344442
    
    Review URL: https://codereview.chromium.org/173423002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253518 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/app_list/views/app_list_background.cc
+++ ui/app_list/views/app_list_background.cc
@@ -5,11 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/app_list/views/app_list_background.h"
 
+#include "base/command_line.h"
+#include "grit/ui_resources.h"
 #include "third_party/skia/include/core/SkPaint.h"
 #include "third_party/skia/include/core/SkPath.h"
 #include "ui/app_list/app_list_constants.h"
+#include "ui/app_list/app_list_switches.h"
 #include "ui/app_list/views/app_list_main_view.h"
 #include "ui/app_list/views/search_box_view.h"
+#include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/rect.h"
 #include "ui/gfx/skia_util.h"
@@ -74,6 +78,19 @@ void AppListBackground::Paint(gfx::Canvas* canvas,
 
   paint.setColor(kContentsBackgroundColor);
   canvas->DrawRect(contents_rect, paint);
+
+  // Draw a banner in the corner of the app list if it is the experimental app
+  // list.
+  if (CommandLine::ForCurrentProcess()->HasSwitch(
+          app_list::switches::kEnableExperimentalAppList)) {
+    const gfx::ImageSkia& experimental_icon =
+        *ui::ResourceBundle::GetSharedInstance().GetImageSkiaNamed(
+             IDR_APP_LIST_EXPERIMENTAL_ICON);
+    canvas->DrawImageInt(experimental_icon,
+                         contents_rect.right() - experimental_icon.width(),
+                         contents_rect.bottom() - experimental_icon.height());
+  }
+
   canvas->Restore();
 }
 
