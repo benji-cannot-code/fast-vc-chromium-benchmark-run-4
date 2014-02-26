70472fec0c0fc97be3d14d15b15fb7b51189d34f
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-26 02:44:00 +0000

    Mojo: Attach dispatchers to message earlier.
    In particular, ProxyMessagePipeEndpoint::EnqueueMessage() gets a message with
    dispatchers already attached. It will be up to the Channel to force them into a
    serialized state.
    
    Note that this is a bit less optimal (especially right now), since we means we
    have to create duplicate dispatchers even for things that will be serialized
    immediately. However, this structure makes things more flexible, since we can
    defer serialization if we want (e.g., we don't want Channel to send everything
    to RawChannel in a serialized state immediately).
    
    R=yzshen@chromium.org
    
    Review URL: https://codereview.chromium.org/177073015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253331 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/system/proxy_message_pipe_endpoint.h
+++ mojo/system/proxy_message_pipe_endpoint.h
@@ -48,9 +48,7 @@ class MOJO_SYSTEM_IMPL_EXPORT ProxyMessagePipeEndpoint
   // |MessagePipeEndpoint| implementation:
   virtual void Close() OVERRIDE;
   virtual void OnPeerClose() OVERRIDE;
-  virtual void EnqueueMessage(
-      scoped_ptr<MessageInTransit> message,
-      std::vector<DispatcherTransport>* transports) OVERRIDE;
+  virtual void EnqueueMessage(scoped_ptr<MessageInTransit> message) OVERRIDE;
   virtual void Attach(scoped_refptr<Channel> channel,
                       MessageInTransit::EndpointId local_id) OVERRIDE;
   virtual void Run(MessageInTransit::EndpointId remote_id) OVERRIDE;
@@ -64,11 +62,6 @@ class MOJO_SYSTEM_IMPL_EXPORT ProxyMessagePipeEndpoint
     return remote_id_ != MessageInTransit::kInvalidEndpointId;
   }
 
-  // "Attaches" |transports| (which must be non-null and nonempty) to |message|
-  // by "serializing" them in an appropriate way, and closes each dispatcher.
-  // (Note that this simply modifies |*message|, and doesn't take ownership.)
-  void AttachAndCloseDispatchers(MessageInTransit* message,
-                                 std::vector<DispatcherTransport>* transports);
   void EnqueueMessageInternal(scoped_ptr<MessageInTransit> message);
 
 #ifdef NDEBUG
