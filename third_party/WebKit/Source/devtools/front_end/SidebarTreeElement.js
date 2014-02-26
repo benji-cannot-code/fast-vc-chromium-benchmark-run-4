3ac0ffc7a3914862228bbb8b290c5bce391f93aa
alph@chromium.org <alph@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-26 15:28:35 +0000

    DevTools: Replace Apple styled spinner icon with Chromium one.
    Drive-by: replace animated PNG files with a CSS animation.
    
    R=yurys@chromium.org
    
    Review URL: https://codereview.chromium.org/181433002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167914 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/SidebarTreeElement.js
+++ third_party/WebKit/Source/devtools/front_end/SidebarTreeElement.js
@@ -54,10 +54,7 @@ WebInspector.SidebarSectionTreeElement.prototype = {
 
         this._smallChildren = x;
 
-        if (this._smallChildren)
-            this._childrenListNode.classList.add("small");
-        else
-            this._childrenListNode.classList.remove("small");
+        this._childrenListNode.classList.toggle("small", this._smallChildren);
     },
 
     onattach: function()
@@ -92,7 +89,7 @@ WebInspector.SidebarTreeElement = function(className, title, subtitle, represent
         this.disclosureButton.className = "disclosure-button";
     }
 
-    this.iconElement = document.createElementWithClass("img", "icon");
+    this.iconElement = document.createElementWithClass("div", "icon");
     this.statusElement = document.createElementWithClass("div", "status");
     this.titlesElement = document.createElementWithClass("div", "titles");
 
@@ -115,13 +112,8 @@ WebInspector.SidebarTreeElement.prototype = {
     set small(x)
     {
         this._small = x;
-
-        if (this._listItemNode) {
-            if (this._small)
-                this._listItemNode.classList.add("small");
-            else
-                this._listItemNode.classList.remove("small");
-        }
+        if (this._listItemNode)
+            this._listItemNode.classList.toggle("small", this._small);
     },
 
     get mainTitle()
@@ -148,10 +140,7 @@ WebInspector.SidebarTreeElement.prototype = {
 
     set wait(x)
     {
-        if (x)
-            this._listItemNode.classList.add("wait");
-        else
-            this._listItemNode.classList.remove("wait");
+        this._listItemNode.classList.toggle("wait", x);
     },
 
     refreshTitles: function()
