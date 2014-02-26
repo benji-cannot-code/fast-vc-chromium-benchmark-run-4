f33a077b106c9bb2a5d5438e100f44d33f905b02
ch.dumez@samsung.com <ch.dumez@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-26 23:12:29 +0000

    Have Document::timing() / Document::contextFeatures() return a reference
    Have Document::timing() / Document::contextFeatures() return a reference
    instead of a pointer as they can never return null. This makes the calling code
    looks safer.
    
    R=adamk
    
    Review URL: https://codereview.chromium.org/181703004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167938 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/timing/PerformanceTiming.cpp
+++ third_party/WebKit/Source/core/timing/PerformanceTiming.cpp
@@ -325,7 +325,7 @@ const DocumentTiming* PerformanceTiming::documentTiming() const
     if (!document)
         return 0;
 
-    return document->timing();
+    return &document->timing();
 }
 
 DocumentLoadTiming* PerformanceTiming::documentLoadTiming() const
