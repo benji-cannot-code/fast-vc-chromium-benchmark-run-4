9380fec2f599ffa4e8f8e50dccfe85d66a3fae62
b.kelemen@samsung.com <b.kelemen@samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-26 12:01:15 +0000

    Don't miss to call ContentView.onShow when lunching content shell and test shell
    In the onStart callback of the activity we do not have a view yet in case
    the activity lunched the first time or when it is coming back from background
    and it was terminated by the os. This CL makes sure we issue onShow when
    finishing initialization.
    
    BUG=345425
    
    Review URL: https://codereview.chromium.org/165993008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253417 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/android/testshell/java/src/org/chromium/chrome/testshell/TabManager.java
+++ chrome/android/testshell/java/src/org/chromium/chrome/testshell/TabManager.java
@@ -119,5 +119,6 @@ public class TabManager extends LinearLayout {
         mContentViewHolder.addView(mCurrentTab.getContentView());
         mContentViewRenderView.setCurrentContentView(mCurrentTab.getContentView());
         mCurrentTab.getContentView().requestFocus();
+        mCurrentTab.getContentView().onShow();
     }
 }
