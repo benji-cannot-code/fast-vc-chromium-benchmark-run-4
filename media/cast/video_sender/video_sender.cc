9896c73b490a1e843cbfe356979260e9cc2abbe8
imcheng@chromium.org <imcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-26 00:27:56 +0000

    Cast: Log kVideoAckReceived as a frame event in VideoSender.
    To achieve this, a mapping from (lowest 8 bits of frame id) to rtp timestamp is added to VideoSender. The map is populated when a frame is encoded, and the map is read when an ack is received. 
    The mapping is in the form of a fixed-size array. Since the map only holds 256 entries, there is a very rare chance that we might get a really old ack back (more than 256 frames ago, or about 8-9 seconds ago) which will result in a bad mapping.
    Since this should be a very rare occurrence and it is only used for logging purposes, this works fine for our purpose. We acknowledge its limitations.
    
    BUG=339590,343990
    
    Review URL: https://codereview.chromium.org/177183004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253299 0039d316-1c4b-4281-b951-d872f2087c98

--- media/cast/video_sender/video_sender.cc
+++ media/cast/video_sender/video_sender.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/cast/video_sender/video_sender.h"
 
+#include <cstring>
 #include <list>
 
 #include "base/bind.h"
@@ -153,6 +154,8 @@ VideoSender::VideoSender(
       CastEnvironment::MAIN, FROM_HERE,
       base::Bind(initialization_status, STATUS_INITIALIZED));
   cast_environment_->Logging()->AddRawEventSubscriber(&event_subscriber_);
+
+  memset(frame_id_to_rtp_timestamp_, 0, sizeof(frame_id_to_rtp_timestamp_));
 }
 
 VideoSender::~VideoSender() {
@@ -200,11 +203,14 @@ void VideoSender::SendEncodedVideoFrameMainThread(
             << static_cast<int>(encoded_frame->frame_id);
   }
 
-  cast_environment_->Logging()->InsertFrameEvent(
-      last_send_time_,
-      kVideoFrameEncoded,
-      encoded_frame->rtp_timestamp,
-      encoded_frame->frame_id);
+  uint32 frame_id = encoded_frame->frame_id;
+  cast_environment_->Logging()->InsertFrameEvent(last_send_time_,
+                                                 kVideoFrameEncoded,
+                                                 encoded_frame->rtp_timestamp,
+                                                 frame_id);
+
+  // Only use lowest 8 bits as key.
+  frame_id_to_rtp_timestamp_[frame_id & 0xff] = encoded_frame->rtp_timestamp;
 
   last_sent_frame_id_ = static_cast<int>(encoded_frame->frame_id);
   cast_environment_->PostTask(
@@ -438,8 +444,12 @@ void VideoSender::ReceivedAck(uint32 acked_frame_id) {
   DCHECK(cast_environment_->CurrentlyOn(CastEnvironment::MAIN));
   last_acked_frame_id_ = static_cast<int>(acked_frame_id);
   base::TimeTicks now = cast_environment_->Clock()->NowTicks();
-  cast_environment_->Logging()->InsertGenericEvent(
-      now, kVideoAckReceived, acked_frame_id);
+
+  RtpTimestamp rtp_timestamp =
+      frame_id_to_rtp_timestamp_[acked_frame_id & 0xff];
+  cast_environment_->Logging()->InsertFrameEvent(
+      now, kVideoAckReceived, rtp_timestamp, acked_frame_id);
+
   VLOG(1) << "ReceivedAck:" << static_cast<int>(acked_frame_id);
   last_acked_frame_id_ = acked_frame_id;
   UpdateFramesInFlight();
