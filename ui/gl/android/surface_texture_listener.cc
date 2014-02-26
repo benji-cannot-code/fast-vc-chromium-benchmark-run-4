cad6ca224dcfcac40459c32d54f15faa4b0efa19
hyunki.baik@samsung.com <hyunki.baik@samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-26 17:58:36 +0000

    Remove unnecessary #include
    BUG=none
    
    Review URL: https://codereview.chromium.org/179493003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253505 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gl/android/surface_texture_listener.cc
+++ ui/gl/android/surface_texture_listener.cc
@@ -6,10 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/android/surface_texture_listener.h"
 
 #include "base/location.h"
-#include "base/logging.h"
 #include "base/message_loop/message_loop_proxy.h"
 #include "jni/SurfaceTextureListener_jni.h"
-#include "ui/gl/android/surface_texture.h"
 
 namespace gfx {
 
