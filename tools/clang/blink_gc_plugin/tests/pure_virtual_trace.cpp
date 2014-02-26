84c2cdda797e10443d968c5b8938c0513f8c18c9
zerny@chromium.org <zerny@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-26 19:49:53 +0000

    Check tracing of fields and base classes in trace-method bodies.
    Errors are described at:
    http://www.chromium.org/developers/blink-gc-plugin-errors
    
    BUG=334149
    R=thakis@chromium.org, ager@chromium.org
    
    Review URL: https://codereview.chromium.org/179873005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253562 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/clang/blink_gc_plugin/tests/pure_virtual_trace.cpp
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "pure_virtual_trace.h"
+
+namespace WebCore {
+
+void B::trace(Visitor* visitor) {
+    visitor->trace(m_a);
+    // Is not required to trace base class A.
+}
+
+}
---
+++ tools/clang/blink_gc_plugin/tests/pure_virtual_trace.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PURE_VIRTUAL_BASE_H_
+#define PURE_VIRTUAL_BASE_H_
+
+#include "heap/stubs.h"
+
+namespace WebCore {
+
+class A : public GarbageCollected<A> {
+public:
+    virtual void trace(Visitor*) = 0;
+};
+
+class B : public A {
+public:
+    void trace(Visitor*);
+private:
+    Member<A> m_a;
+};
+
+}
+
+#endif
