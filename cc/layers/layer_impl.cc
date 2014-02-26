310ac15a3e240ef677a223f95daa13c730ce7cc6
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-26 02:48:25 +0000

    Fix frame viewer: LayerTypeAsString() can't be called from ~LayerImpl
    For details on the fix see:
    https://code.google.com/p/trace-viewer/source/detail?r=1165
    
    R=enne@chromium.org
    
    Review URL: https://codereview.chromium.org/179063003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253336 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/layers/layer_impl.cc
+++ cc/layers/layer_impl.cc
@@ -112,8 +112,7 @@ LayerImpl::~LayerImpl() {
     clip_parent_->RemoveClipChild(this);
 
   TRACE_EVENT_OBJECT_DELETED_WITH_ID(
-      TRACE_DISABLED_BY_DEFAULT("cc.debug"),
-      LayerTypeAsString(), this);
+      TRACE_DISABLED_BY_DEFAULT("cc.debug"), "cc::LayerImpl", this);
 }
 
 void LayerImpl::AddChild(scoped_ptr<LayerImpl> child) {
@@ -1401,7 +1400,10 @@ void LayerImpl::RemoveDependentNeedsPushProperties() {
 void LayerImpl::AsValueInto(base::DictionaryValue* state) const {
   TracedValue::MakeDictIntoImplicitSnapshotWithCategory(
       TRACE_DISABLED_BY_DEFAULT("cc.debug"),
-      state, LayerTypeAsString(), this);
+      state,
+      "cc::LayerImpl",
+      LayerTypeAsString(),
+      this);
   state->SetInteger("layer_id", id());
   state->Set("bounds", MathUtil::AsValue(bounds()).release());
   state->SetInteger("draws_content", DrawsContent());
