fa062c55afe024c56a46bf16eb2f44f452d8b037
ziran.sun@samsung.com <ziran.sun@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-26 22:12:14 +0000

    Add autofill preview support for <month> input fields
    Add condition handling for suggestedValue case in updateview() and preview functions.
    Updated test cases accordingly.
    
    BUG=335212
    R=isherman@chromium.org, tkent@chromium.org, jhawkins@chromium.org
    
    Review URL: https://codereview.chromium.org/130213005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167933 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/forms/BaseChooserOnlyDateAndTimeInputType.h
+++ third_party/WebKit/Source/core/html/forms/BaseChooserOnlyDateAndTimeInputType.h
@@ -41,7 +41,6 @@ protected:
     virtual ~BaseChooserOnlyDateAndTimeInputType();
 
 private:
-    void updateAppearance();
     void closeDateTimeChooser();
 
     // InputType functions:
@@ -53,6 +52,7 @@ private:
     virtual void handleKeypressEvent(KeyboardEvent*) OVERRIDE;
     virtual void handleKeyupEvent(KeyboardEvent*) OVERRIDE;
     virtual void accessKeyAction(bool sendMouseEvents) OVERRIDE;
+    virtual void updateView() OVERRIDE;
 
     // DateTimeChooserClient functions:
     virtual void didChooseValue(const String&) OVERRIDE;
