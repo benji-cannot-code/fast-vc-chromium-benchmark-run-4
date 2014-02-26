431a88f8cecee3a681ea9348a2e8cc29f8ad6050
tengs@chromium.org <tengs@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-26 14:41:50 +0000

    Add chrome.screenlockPrivate API functions to handle alternate auth types.
    This CL adds the hideButton, setAuthType and getAuthType functions and the
    onAuthAttempted event to the chrome.screenlockPrivate namespace. The
    implementation of this API for ChromeOS is done in a separate CL.
    
    BUG=344179
    TEST=new api test
    
    Review URL: https://codereview.chromium.org/168623003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253447 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/extensions/screenlock_private_api.h
+++ chrome/browser/chromeos/extensions/screenlock_private_api.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CHROMEOS_EXTENSIONS_SCREENLOCK_PRIVATE_API_H_
 #define CHROME_BROWSER_CHROMEOS_EXTENSIONS_SCREENLOCK_PRIVATE_API_H_
 
+#include "chrome/browser/chromeos/login/login_display.h"
 #include "chrome/browser/extensions/api/profile_keyed_api_factory.h"
 #include "chrome/browser/extensions/chrome_extension_function.h"
 #include "chromeos/dbus/session_manager_client.h"
@@ -56,7 +57,60 @@ class ScreenlockPrivateShowButtonFunction
  private:
   virtual ~ScreenlockPrivateShowButtonFunction();
   void OnImageLoaded(const gfx::Image& image);
-  DISALLOW_COPY_AND_ASSIGN(ScreenlockPrivateShowButtonFunction );
+  DISALLOW_COPY_AND_ASSIGN(ScreenlockPrivateShowButtonFunction);
+};
+
+class ScreenlockPrivateHideButtonFunction
+    : public ChromeAsyncExtensionFunction {
+ public:
+  DECLARE_EXTENSION_FUNCTION("screenlockPrivate.hideButton",
+                             SCREENLOCKPRIVATE_HIDEBUTTON)
+  ScreenlockPrivateHideButtonFunction();
+  virtual bool RunImpl() OVERRIDE;
+
+ private:
+  virtual ~ScreenlockPrivateHideButtonFunction();
+  void OnImageLoaded(const gfx::Image& image);
+  DISALLOW_COPY_AND_ASSIGN(ScreenlockPrivateHideButtonFunction);
+};
+
+class ScreenlockPrivateSetAuthTypeFunction
+    : public ChromeAsyncExtensionFunction {
+ public:
+  DECLARE_EXTENSION_FUNCTION("screenlockPrivate.setAuthType",
+                             SCREENLOCKPRIVATE_SETAUTHTYPE)
+  ScreenlockPrivateSetAuthTypeFunction();
+  virtual bool RunImpl() OVERRIDE;
+
+ private:
+  virtual ~ScreenlockPrivateSetAuthTypeFunction();
+  DISALLOW_COPY_AND_ASSIGN(ScreenlockPrivateSetAuthTypeFunction);
+};
+
+class ScreenlockPrivateGetAuthTypeFunction
+    : public ChromeAsyncExtensionFunction {
+ public:
+  DECLARE_EXTENSION_FUNCTION("screenlockPrivate.getAuthType",
+                             SCREENLOCKPRIVATE_GETAUTHTYPE)
+  ScreenlockPrivateGetAuthTypeFunction();
+  virtual bool RunImpl() OVERRIDE;
+
+ private:
+  virtual ~ScreenlockPrivateGetAuthTypeFunction();
+  DISALLOW_COPY_AND_ASSIGN(ScreenlockPrivateGetAuthTypeFunction);
+};
+
+class ScreenlockPrivateAcceptAuthAttemptFunction
+    : public ChromeAsyncExtensionFunction {
+ public:
+  DECLARE_EXTENSION_FUNCTION("screenlockPrivate.acceptAuthAttempt",
+                             SCREENLOCKPRIVATE_ACCEPTAUTHATTEMPT)
+  ScreenlockPrivateAcceptAuthAttemptFunction();
+  virtual bool RunImpl() OVERRIDE;
+
+ private:
+  virtual ~ScreenlockPrivateAcceptAuthAttemptFunction();
+  DISALLOW_COPY_AND_ASSIGN(ScreenlockPrivateAcceptAuthAttemptFunction);
 };
 
 class ScreenlockPrivateEventRouter
@@ -67,6 +121,9 @@ class ScreenlockPrivateEventRouter
   virtual ~ScreenlockPrivateEventRouter();
 
   void OnButtonClicked();
+
+  void OnAuthAttempted(chromeos::LoginDisplay::AuthType auth_type,
+                       const std::string& value);
 
   // ProfileKeyedAPI
   static extensions::ProfileKeyedAPIFactory<ScreenlockPrivateEventRouter>*
