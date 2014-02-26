f470fca28df63cb8fed8db4e2dcd801ca6727975
dcheng@chromium.org <dcheng@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-26 22:46:51 +0000

    Cleanup now that WebHelperPlugin no longer needs to be a WebWidget.
    BUG=346543
    R=abarth@chromium.org, ddorwin@chromium.org
    
    Review URL: https://codereview.chromium.org/181953002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167936 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebHelperPluginImpl.cpp
+++ third_party/WebKit/Source/web/WebHelperPluginImpl.cpp
@@ -103,10 +103,6 @@ void WebHelperPluginImpl::destroy()
     m_destructionTimer.startOneShot(0);
 }
 
-void WebHelperPluginImpl::initializeFrame(WebFrameClient*)
-{
-}
-
 WebPlugin* WebHelperPluginImpl::getPlugin()
 {
     ASSERT(m_pluginContainer);
