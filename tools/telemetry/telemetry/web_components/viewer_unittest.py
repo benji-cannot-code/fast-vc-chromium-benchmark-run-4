cd3167dc1cc0de73ba4ec34917a58e8dcef367d8
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-26 18:40:07 +0000

    Introduce telemetry.web_components and a simple component-based Results object
    The main result of this patch is that
      cd $TELEMETRY && python -m telemetry.web_components.results_viewer
    
    will produce an HTML file that has the same standalone-and-emailable properties as telemetry's existing HTML results object, but that is built out of tvcm components, trace-viewer's component model. Followup patches will expand out results_viewer.js with features to actually be a viable replacement for Telemetry's existing results.html.
    
    Importantly, with this checkin, telemetry's existing  ./run_tests will also run any javascript unittests and the presubmit will lint the same js and css files.
    
    
    BUG=340096
    
    Review URL: https://codereview.chromium.org/169283004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253521 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/telemetry/telemetry/web_components/viewer_unittest.py
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import unittest
+import StringIO
+
+from telemetry.web_components import viewer
+
+class SimpleViewer(viewer.Viewer):
+  def __init__(self):
+    super(SimpleViewer, self).__init__(
+      tvcm_module_name='telemetry.web_components.viewer_unittest_data',
+      js_class_name='telemetry.web_components.SimpleViewer',
+      data_binding_property='dataToView')
+
+  def WriteDataToFileAsJson(self, f):
+    f.write("1\n")
+
+class ViewerTests(unittest.TestCase):
+  def testForSmoke(self):
+    v = SimpleViewer()
+
+    f = StringIO.StringIO()
+    v.WriteViewerToFile(f)
---
+++ tools/telemetry/telemetry/web_components/web_components_project.py
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+
+
+from telemetry.core import util
+from tvcm import project as project_module
+
+
+class WebComponentsProject(project_module.Project):
+  telemetry_path = os.path.abspath(util.GetTelemetryDir())
+
+  trace_viewer_path = os.path.abspath(os.path.join(
+      util.GetChromiumSrcDir(),
+      'third_party', 'trace-viewer'))
+
+  def __init__(self):
+    super(WebComponentsProject, self).__init__(
+      [self.telemetry_path])
