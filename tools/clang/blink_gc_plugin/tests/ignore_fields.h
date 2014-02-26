3fdd42dc5a88eecd1f05d5d2b52e989db3b6ecc3
zerny@chromium.org <zerny@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-26 07:27:00 +0000

    Require trace methods on structures with fields that need tracing.
    This CL checks that trace methods are declared based on the class/struct fields.
    Errors are described at:
    http://www.chromium.org/developers/blink-gc-plugin-errors
    
    To avoid erroneous checks, the STACK_ALLOCATED macro can be used to
    mark a structure as only allocated on the stack, and the
    NO_TRACE_CHECKING macro can be used to ignore a field for the
    purpose of trace checking.
    
    BUG=334149
    R=haraken@chromium.org, thakis@chromium.org, vegorov@chromium.org, wibling@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/163603002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253389 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/clang/blink_gc_plugin/tests/ignore_fields.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IGNORE_FIELDS_H_
+#define IGNORE_FIELDS_H_
+
+#include "heap/stubs.h"
+
+namespace WebCore {
+
+class HeapObject : public GarbageCollected<HeapObject> { };
+
+class A {
+private:
+    NO_TRACE_CHECKING("http://crbug.com/12345")
+    HeapObject* m_obj;
+};
+
+}
+
+#endif
