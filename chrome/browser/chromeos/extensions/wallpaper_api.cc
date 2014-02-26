21dbd331a3121f26e390ae5bebf1cbec0a48f587
bshe@chromium.org <bshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-26 20:31:08 +0000

    Reland wallpaper API test
    The original CL was reverted here:
    codereview.chromium.org/167243002
    It failed on linux_chromeos debug build and the reason is we try to decode a 404 response web page
    as a jpeg image and it somehow cause a out-of-bounds error.
    If the response code is 404, we shouldn't try to decode the response.
    
    
    BUG=176183
    
    Review URL: https://codereview.chromium.org/167613003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253584 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/extensions/wallpaper_api.cc
+++ chrome/browser/chromeos/extensions/wallpaper_api.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/wallpaper_manager.h"
 #include "chrome/common/chrome_paths.h"
 #include "net/base/load_flags.h"
+#include "net/http/http_status_code.h"
 #include "net/url_request/url_fetcher.h"
 #include "net/url_request/url_fetcher_delegate.h"
 #include "url/gurl.h"
@@ -24,8 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using base::BinaryValue;
 using content::BrowserThread;
 
-typedef base::Callback<void(net::URLRequestStatus::Status, const std::string&)>
-    FetchCallback;
+typedef base::Callback<void(bool success, const std::string&)> FetchCallback;
 
 namespace set_wallpaper = extensions::api::wallpaper::SetWallpaper;
 
@@ -52,15 +52,21 @@ class WallpaperFetcher : public net::URLFetcherDelegate {
  private:
   // URLFetcherDelegate overrides:
   virtual void OnURLFetchComplete(const net::URLFetcher* source) OVERRIDE {
-    net::URLRequestStatus::Status status = source->GetStatus().status();
+    DCHECK(url_fetcher_.get() == source);
+
+    bool success = source->GetStatus().is_success() &&
+                   source->GetResponseCode() == net::HTTP_OK;
     std::string response;
-    if (status == net::URLRequestStatus::SUCCESS) {
-      url_fetcher_->GetResponseAsString(&response);
+    if (success) {
+      source->GetResponseAsString(&response);
     } else {
-      response = source->GetStatus().error();
+      response = base::StringPrintf(
+          "Downloading wallpaper %s failed. The response code is %d.",
+          source->GetOriginalURL().ExtractFileName().c_str(),
+          source->GetResponseCode());
     }
     url_fetcher_.reset();
-    callback_.Run(status, response);
+    callback_.Run(success, response);
   }
 
   void CancelPreviousFetch() {
@@ -68,7 +74,7 @@ class WallpaperFetcher : public net::URLFetcherDelegate {
       std::string error = base::StringPrintf(
           "Downloading wallpaper %s is canceled.",
           url_fetcher_->GetOriginalURL().ExtractFileName().c_str());
-      callback_.Run(net::URLRequestStatus::CANCELED, error);
+      callback_.Run(false, error);
       url_fetcher_.reset();
     }
   }
@@ -102,10 +108,9 @@ bool WallpaperSetWallpaperFunction::RunImpl() {
   } else {
     GURL wallpaper_url(*params_->details.url);
     if (wallpaper_url.is_valid()) {
-    g_wallpaper_fetcher.Get().FetchWallpaper(
-        wallpaper_url,
-        base::Bind(&WallpaperSetWallpaperFunction::OnWallpaperFetched,
-                   this));
+      g_wallpaper_fetcher.Get().FetchWallpaper(
+          wallpaper_url,
+          base::Bind(&WallpaperSetWallpaperFunction::OnWallpaperFetched, this));
     } else {
       SetError("URL is invalid.");
       SendResponse(false);
@@ -191,8 +196,9 @@ void WallpaperSetWallpaperFunction::ThumbnailGenerated(
 }
 
 void WallpaperSetWallpaperFunction::OnWallpaperFetched(
-    net::URLRequestStatus::Status status, const std::string& response) {
-  if (status == net::URLRequestStatus::SUCCESS) {
+    bool success,
+    const std::string& response) {
+  if (success) {
     params_->details.wallpaper_data.reset(new std::string(response));
     StartDecode(*params_->details.wallpaper_data);
   } else {
