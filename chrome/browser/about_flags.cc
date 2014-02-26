4980781a09b1396f01ad0bedc8210b3bdf0c907b
skuhne@chromium.org <skuhne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-26 05:34:34 +0000

    Adding hotkey behind a commandline to allow testing the TouchView maximizing mode.
    The flag and the hotkey is only temporary since there is no other way to test the TouchView functionality at this time.
    
    BUG=337563
    TEST=visual
    
    Review URL: https://codereview.chromium.org/179063002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253369 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1189,6 +1189,12 @@ const Experiment kExperiments[] = {
     kOsCrOS,
     SINGLE_VALUE_TYPE(ash::switches::kAshDisableDragAndDropAppListToLauncher),
   },
+  { "ash-enable-touch-view-testing",
+    IDS_FLAGS_ASH_ENABLE_TOUCH_VIEW_TESTING_NAME,
+    IDS_FLAGS_ASH_ENABLE_TOUCH_VIEW_TESTING_DESCRIPTION,
+    kOsCrOS,
+    SINGLE_VALUE_TYPE(ash::switches::kAshEnableTouchViewTesting),
+  },
 #if defined(OS_LINUX)
   { "ash-enable-memory-monitor",
       IDS_FLAGS_ENABLE_MEMORY_MONITOR_NAME,
