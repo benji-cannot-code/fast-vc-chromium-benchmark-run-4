fe2bc4b2165eaffc539cc4776f0a03f9feed85ce
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-26 22:10:30 +0000

    Revert 251238 "cc: Clean up iterator template to only take 1 par..."
    Speculative revert to see if this affects memory stomping behaviours.
    
    > cc: Clean up iterator template to only take 1 parameter.
    > 
    > LayerIterator takes 4 parameters. One is the FrontToBack ordering
    > but that is now the only option. The other 3 are for the type of layers,
    > render surfaces, and the render surface layer list. The latter two are
    > now already available as typedefs off of Layer and LayerImpl, so we
    > only need Layer/LayerImpl in the template arguments.
    > 
    > R=enne
    > BUG=YAK
    > 
    > Review URL: https://codereview.chromium.org/156603005
    
    TBR=danakj@chromium.org
    
    Review URL: https://codereview.chromium.org/181783004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253606 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/layers/render_surface_impl.h
+++ cc/layers/render_surface_impl.h
@@ -26,8 +26,6 @@ class DelegatedRendererLayerImpl;
 class QuadSink;
 class RenderPassSink;
 class LayerImpl;
-template <typename LayerType>
-class LayerIterator;
 
 struct AppendQuadsData;
 
@@ -177,7 +175,7 @@ class CC_EXPORT RenderSurfaceImpl {
   int target_render_surface_layer_index_history_;
   int current_layer_index_history_;
 
-  friend class LayerIterator<LayerImpl>;
+  friend struct LayerIteratorActions;
 
   DISALLOW_COPY_AND_ASSIGN(RenderSurfaceImpl);
 };
--- cc/trees/quad_culler_unittest.cc
+++ cc/trees/quad_culler_unittest.cc
@@ -40,7 +40,10 @@ class TestOcclusionTrackerImpl
   DISALLOW_COPY_AND_ASSIGN(TestOcclusionTrackerImpl);
 };
 
-typedef LayerIterator<LayerImpl> LayerIteratorType;
+typedef LayerIterator<LayerImpl,
+                      LayerImplList,
+                      RenderSurfaceImpl,
+                      LayerIteratorActions::FrontToBack> LayerIteratorType;
 
 class QuadCullerTest : public testing::Test {
  public:
