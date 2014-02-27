e7614ce6b6c510a0c96db93d2c48e2ed802277a9
mpawlowski@opera.com <mpawlowski@opera.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-27 13:55:06 +0000

    Update incomplete credentials in Login Database
    Incomplete credentials (missing username_element, password_element
    or submit_element) that might get added to the database via importing
    from other browsers should now be updated correctly when they're
    entered into the right form.
    
    BUG=344337
    
    Review URL: https://codereview.chromium.org/169173005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253791 0039d316-1c4b-4281-b951-d872f2087c98

--- components/password_manager/core/browser/password_store.h
+++ components/password_manager/core/browser/password_store.h
@@ -116,7 +116,7 @@ class PasswordStore : public base::RefCountedThreadSafe<PasswordStore> {
   virtual void AddLogin(const autofill::PasswordForm& form);
 
   // Updates the matching PasswordForm in the secure password store (async).
-  void UpdateLogin(const autofill::PasswordForm& form);
+  virtual void UpdateLogin(const autofill::PasswordForm& form);
 
   // Removes the matching PasswordForm from the secure password store (async).
   void RemoveLogin(const autofill::PasswordForm& form);
