4b830ba8c185a79d357b8851bc71f927a237d860
ksakamoto@chromium.org <ksakamoto@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-27 04:15:29 +0000

    Fix webfont loading problems when stylesheets changed while loading
    Stylesheets change causes FontFaceCache::clear(). If there are any
    ongoing font downloads, that may cause two problems:
    
    (1) The font download can be canceled, because CSSFontFaceSource, which
    is a FontResourceClient, is destroyed. To prevent this, this patch makes
    FontFaceSet keep a reference to FontFace while loading.
    
    (2) Text is not refreshed because CSSFontFace::fontLoaded() returns
    immediately. FontFaceCache::clear() reconstructs CSSFontFace objects,
    so m_sources may not point to the source it was loading before style
    changes. This patch changes CSSFontFace::fontLoaded() always calls
    CSSFontSelector::fontLoaded(), regardless its status.
    
    In the long term, we should try to avoid reconstructing FontFace on
    stylesheet changes.
    
    BUG=336476
    TEST=http/tests/webfont/fontface-style-change.html
    
    Review URL: https://codereview.chromium.org/177043002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167981 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/FontFaceSet.h
+++ third_party/WebKit/Source/core/css/FontFaceSet.h
@@ -127,8 +127,8 @@ private:
 
     bool inActiveDocumentContext() const;
     void forEachInternal(PassOwnPtr<FontFaceSetForEachCallback>, ScriptValue* thisArg) const;
-    void incrementLoadingCount();
-    void decrementLoadingCount();
+    void addToLoadingFonts(PassRefPtr<FontFace>);
+    void removeFromLoadingFonts(PassRefPtr<FontFace>);
     void fireLoadingEvent();
     void fireDoneEventIfPossible();
     bool resolveFontStyle(const String&, Font&);
@@ -137,7 +137,7 @@ private:
     const ListHashSet<RefPtr<FontFace> >& cssConnectedFontFaceList() const;
     bool isCSSConnectedFontFace(FontFace*) const;
 
-    unsigned m_loadingCount;
+    HashSet<RefPtr<FontFace> > m_loadingFonts;
     bool m_shouldFireLoadingEvent;
     Vector<OwnPtr<FontsReadyPromiseResolver> > m_readyResolvers;
     FontFaceArray m_loadedFonts;
