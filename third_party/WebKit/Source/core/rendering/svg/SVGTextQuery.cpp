9cbb98dfad7ab9e3c4f16c41d5423b18030fc9ff
fmalita@chromium.org <fmalita@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-27 15:31:14 +0000

    [SVG] Remove redundant identity checks
    Minor cleanup: AffineTransform::mapRect() is already optimized for the
    identity/translation case, no need to check explicitly.
    
    R=pdr@chromium.org,schenney@chromium.org
    
    Review URL: https://codereview.chromium.org/175763002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168031 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/svg/SVGTextQuery.cpp
+++ third_party/WebKit/Source/core/rendering/svg/SVGTextQuery.cpp
@@ -477,8 +477,6 @@ static inline void calculateGlyphBoundaries(SVGTextQuery::Data* queryData, const
 
     AffineTransform fragmentTransform;
     fragment.buildFragmentTransform(fragmentTransform, SVGTextFragment::TransformIgnoringTextLength);
-    if (fragmentTransform.isIdentity())
-        return;
 
     extent = fragmentTransform.mapRect(extent);
 }
