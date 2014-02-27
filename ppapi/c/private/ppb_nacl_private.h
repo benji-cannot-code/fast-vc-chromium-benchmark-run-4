266784009eea18965feb05624e27b55730b49a5a
jvoung@chromium.org <jvoung@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-27 19:20:57 +0000

    Update comment about NaCl UMA. Histogram entries are public.
    Moved to public repo at r252429.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/183293003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253897 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/c/private/ppb_nacl_private.h
+++ ppapi/c/private/ppb_nacl_private.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * found in the LICENSE file.
  */
 
-/* From private/ppb_nacl_private.idl modified Tue Feb 25 09:18:09 2014. */
+/* From private/ppb_nacl_private.idl modified Thu Feb 27 08:19:06 2014. */
 
 #ifndef PPAPI_C_PRIVATE_PPB_NACL_PRIVATE_H_
 #define PPAPI_C_PRIVATE_PPB_NACL_PRIVATE_H_
@@ -36,8 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /** NaCl-specific errors that should be reported to the user.
  *  These error codes are reported via UMA so, if you edit them:
  *   1) make sure you understand UMA first.
- *   2) update src/tools/histograms/histograms.xml in
- *   svn://svn.chromium.org/chrome-internal/trunk/src-internal
+ *   2) update src/tools/metrics/histograms/histograms.xml
  *  Values are explicitly specified to make sure they don't shift around when
  *  edited, and also to make reading about:histograms easier.
  */
