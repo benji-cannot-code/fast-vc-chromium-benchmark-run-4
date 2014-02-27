74dbaf4a0171b3d28ab8aed1773eb2da03c745aa
robliao@chromium.org <robliao@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-27 22:16:25 +0000

    Google Now Code Cleanup
    BUG=306613
    R=isherman@chromium.org, rgustafson@chromium.org, skare@chromium.org, vadimt@chromium.org
    
    Review URL: https://codereview.chromium.org/169743008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253940 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/google_now/utility.js
+++ chrome/browser/resources/google_now/utility.js
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *     otherwise, we generate an error. Chrome may unload event pages waiting
  *     for an event. When the event fires, Chrome will reload the event page. We
  *     don't require event listeners to fire because they are generally not
- *     predictable (like a location change event).
+ *     predictable (like a button clicked event).
  * (2) Task Manager (built with buildTaskManager() call) provides controlling
  *     mutually excluding chains of callbacks called tasks. Task Manager uses
  *     WrapperPlugins to add instrumentation code to 'wrapper' to determine
