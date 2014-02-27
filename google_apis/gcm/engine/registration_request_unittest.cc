b1cfc090d033b629c44aa1793486ff9c3dc94184
fgorski@chromium.org <fgorski@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-27 06:18:52 +0000

    [GCM] Enable Registration Request to give after 5 retries.
    When a Chrome app issues a registration, that operation might be looping endlessly, when an error is recoverable. If meanwhile app wants to issue another registration (perhaps with a different set of senders) it will always be receiving and error (asynchronous operation pending) without a chance to cancel.
    
    The update is going to allow the registraiton to fail, after a 6 attempts (1 initial attempt + 5 retries). That approach ensures that applications follow an exponential backoff policy and have a chance to issue a new registration request after a period of time.
    
    BUG=344010
    
    Review URL: https://codereview.chromium.org/180113005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253739 0039d316-1c4b-4281-b951-d872f2087c98

--- google_apis/gcm/engine/registration_request_unittest.cc
+++ google_apis/gcm/engine/registration_request_unittest.cc
@@ -19,10 +19,10 @@ namespace gcm {
 
 namespace {
 const uint64 kAndroidId = 42UL;
+const char kAppId[] = "TestAppId";
 const char kCert[] = "0DEADBEEF420";
 const char kDeveloperId[] = "Project1";
 const char kLoginHeader[] = "AidLogin";
-const char kAppId[] = "TestAppId";
 const uint64 kSecurityToken = 77UL;
 
 // Backoff policy for testing registration request.
@@ -68,8 +68,12 @@ class RegistrationRequestTest : public testing::Test {
   void SetResponseStatusAndString(net::HttpStatusCode status_code,
                                   const std::string& response_body);
   void CompleteFetch();
+  void set_max_retry_count(int max_retry_count) {
+    max_retry_count_ = max_retry_count;
+  }
 
  protected:
+  int max_retry_count_;
   RegistrationRequest::Status status_;
   std::string registration_id_;
   bool callback_called_;
@@ -81,7 +85,8 @@ class RegistrationRequestTest : public testing::Test {
 };
 
 RegistrationRequestTest::RegistrationRequestTest()
-    : status_(RegistrationRequest::SUCCESS),
+    : max_retry_count_(2),
+      status_(RegistrationRequest::SUCCESS),
       callback_called_(false),
       url_request_context_getter_(new net::TestURLRequestContextGetter(
           message_loop_.message_loop_proxy())) {}
@@ -111,6 +116,7 @@ void RegistrationRequestTest::CreateRequest(const std::string& sender_ids) {
       kDefaultBackoffPolicy,
       base::Bind(&RegistrationRequestTest::RegistrationCallback,
                  base::Unretained(this)),
+      max_retry_count_,
       url_request_context_getter_.get()));
 }
 
@@ -131,6 +137,19 @@ void RegistrationRequestTest::CompleteFetch() {
   net::TestURLFetcher* fetcher = url_fetcher_factory_.GetFetcherByID(0);
   ASSERT_TRUE(fetcher);
   fetcher->delegate()->OnURLFetchComplete(fetcher);
+}
+
+TEST_F(RegistrationRequestTest, RequestSuccessful) {
+  set_max_retry_count(0);
+  CreateRequest("sender1,sender2");
+  request_->Start();
+
+  SetResponseStatusAndString(net::HTTP_OK, "token=2501");
+  CompleteFetch();
+
+  EXPECT_TRUE(callback_called_);
+  EXPECT_EQ(RegistrationRequest::SUCCESS, status_);
+  EXPECT_EQ("2501", registration_id_);
 }
 
 TEST_F(RegistrationRequestTest, RequestDataPassedToFetcher) {
@@ -313,7 +332,7 @@ TEST_F(RegistrationRequestTest, ResponseInvalidSender) {
   EXPECT_EQ(std::string(), registration_id_);
 }
 
-TEST_F(RegistrationRequestTest, RequestNotSucessful) {
+TEST_F(RegistrationRequestTest, RequestNotSuccessful) {
   CreateRequest("sender1,sender2");
   request_->Start();
 
@@ -352,6 +371,41 @@ TEST_F(RegistrationRequestTest, ResponseHttpNotOk) {
   EXPECT_TRUE(callback_called_);
   EXPECT_EQ(RegistrationRequest::SUCCESS, status_);
   EXPECT_EQ("2501", registration_id_);
+}
+
+TEST_F(RegistrationRequestTest, MaximumAttemptsReachedWithZeroRetries) {
+  set_max_retry_count(0);
+  CreateRequest("sender1,sender2");
+  request_->Start();
+
+  SetResponseStatusAndString(net::HTTP_GATEWAY_TIMEOUT, "token=2501");
+  CompleteFetch();
+
+  EXPECT_TRUE(callback_called_);
+  EXPECT_EQ(RegistrationRequest::REACHED_MAX_RETRIES, status_);
+  EXPECT_EQ(std::string(), registration_id_);
+}
+
+TEST_F(RegistrationRequestTest, MaximumAttemptsReached) {
+  CreateRequest("sender1,sender2");
+  request_->Start();
+
+  SetResponseStatusAndString(net::HTTP_GATEWAY_TIMEOUT, "token=2501");
+  CompleteFetch();
+
+  EXPECT_FALSE(callback_called_);
+
+  SetResponseStatusAndString(net::HTTP_GATEWAY_TIMEOUT, "token=2501");
+  CompleteFetch();
+
+  EXPECT_FALSE(callback_called_);
+
+  SetResponseStatusAndString(net::HTTP_GATEWAY_TIMEOUT, "token=2501");
+  CompleteFetch();
+
+  EXPECT_TRUE(callback_called_);
+  EXPECT_EQ(RegistrationRequest::REACHED_MAX_RETRIES, status_);
+  EXPECT_EQ(std::string(), registration_id_);
 }
 
 }  // namespace gcm
