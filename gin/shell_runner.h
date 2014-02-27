db9a97a9da37abc74e0bf08a8fb210c8217302c4
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-27 19:49:39 +0000

    Fixes header guard of ShellRunner
    I forgot to change these when I renamed from DefaultRunner to
    ShellRunner.
    
    BUG=none
    TEST=none
    R=abarth@chromium.org
    TBR=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/179203005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253906 0039d316-1c4b-4281-b951-d872f2087c98

--- gin/shell_runner.h
+++ gin/shell_runner.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef GIN_DEFAULT_RUNNER_H_
-#define GIN_DEFAULT_RUNNER_H_
+#ifndef GIN_SHELL_RUNNER_H_
+#define GIN_SHELL_RUNNER_H_
 
 #include "gin/runner.h"
 
@@ -66,4 +66,4 @@ class GIN_EXPORT ShellRunner : public Runner {
 
 }  // namespace gin
 
-#endif  // GIN_DEFAULT_RUNNER_H_
+#endif  // GIN_SHELL_RUNNER_H_
