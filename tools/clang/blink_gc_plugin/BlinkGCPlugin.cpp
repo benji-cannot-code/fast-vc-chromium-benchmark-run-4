ab9d65e5c63073eb86189a21096cc9aeeb0bacda
zerny@chromium.org <zerny@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-27 17:18:21 +0000

    Cleanup Blink GC config and add support for persistent heap collections.
    BUG=334149
    R=ager@chromium.org
    
    Review URL: https://codereview.chromium.org/182703002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253850 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/clang/blink_gc_plugin/BlinkGCPlugin.cpp
+++ tools/clang/blink_gc_plugin/BlinkGCPlugin.cpp
@@ -480,8 +480,7 @@ class BlinkGCPluginAction : public PluginASTAction {
 
     for (size_t i = 0; i < args.size() && parsed; ++i) {
       if (args[i] == "enable-oilpan") {
-        // TODO: Remove this once all transition types are eliminated.
-        Config::set_oilpan_enabled(true);
+        // TODO: Remove this flag.
       } else {
         parsed = false;
         llvm::errs() << "Unknown blink-gc-plugin argument: " << args[i] << "\n";
@@ -496,8 +495,6 @@ class BlinkGCPluginAction : public PluginASTAction {
 };
 
 }  // namespace
-
-bool Config::oilpan_enabled_ = false;
 
 static FrontendPluginRegistry::Add<BlinkGCPluginAction> X(
     "blink-gc-plugin",
