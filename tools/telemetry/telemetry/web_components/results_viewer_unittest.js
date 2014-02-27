ef5143e621437364e7f55ce7261cbb7b582ff3d8
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-27 00:26:49 +0000

    [Reland] Introduce telemetry.web_components and a simple component-based Results object
    The main result of this patch is that
    cd $TELEMETRY && python -m telemetry.web_components.results_viewer
    
    will produce an HTML file that has the same standalone-and-emailable properties as telemetry's existing HTML results object, but that is built out of tvcm components, trace-viewer's component model. Followup patches will expand out results_viewer.js with features to actually be a viable replacement for Telemetry's existing results.html.
    
    Importantly, with this checkin, telemetry's existing ./run_tests will also run any javascript unittests and the presubmit will lint the same js and css files.
    
    BUG=340096
    TBR=tonyg,achuith
    
    Original review at: https://codereview.chromium.org/169283004/
    
    Review URL: https://codereview.chromium.org/178043008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253658 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/telemetry/telemetry/web_components/results_viewer_unittest.js
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+tvcm.require('telemetry.web_components.results_viewer');
+
+tvcm.unittest.testSuite('telemetry.web_components.results_viewer_unittest',
+    function() {
+      test('testBasic', function() {
+        var resultsViewer = new telemetry.web_components.ResultsViewer();
+        resultsViewer.dataToView = {hello: 'world', nice: ['to', 'see', 'you']};
+        this.addHTMLOutput(resultsViewer);
+      });
+    });
