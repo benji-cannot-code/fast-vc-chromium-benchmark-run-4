5e31513b349adf20a945d3367ef88538e25c9349
rileya@chromium.org <rileya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-27 20:13:23 +0000

    Break logic from VideoFrameStream into a generic templated DecoderStream.
    BUG=347270
    
    Review URL: https://codereview.chromium.org/130203003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253915 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/video_frame_stream_unittest.cc
+++ media/filters/video_frame_stream_unittest.cc
@@ -9,9 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/gmock_callback_support.h"
 #include "media/base/mock_filters.h"
 #include "media/base/test_helpers.h"
+#include "media/filters/decoder_stream.h"
 #include "media/filters/fake_demuxer_stream.h"
 #include "media/filters/fake_video_decoder.h"
-#include "media/filters/video_frame_stream.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 using ::testing::_;
--- media/filters/video_renderer_impl.h
+++ media/filters/video_renderer_impl.h
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/video_decoder.h"
 #include "media/base/video_frame.h"
 #include "media/base/video_renderer.h"
-#include "media/filters/video_frame_stream.h"
+#include "media/filters/decoder_stream.h"
 
 namespace base {
 class SingleThreadTaskRunner;
