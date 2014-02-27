42e0b99df37cfa5b13d288c5e343a1ab8a5b038b
rob.buis@samsung.com <rob.buis@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-27 14:46:45 +0000

    Remove unused SVG getter in SVGViewSpec.
    Remove unused SVG getter in SVGViewSpec.
    
    Review URL: https://codereview.chromium.org/182233003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168028 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGViewSpec.h
+++ third_party/WebKit/Source/core/svg/SVGViewSpec.h
@@ -50,7 +50,6 @@ public:
     String transformString() const;
     String viewTargetString() const { return m_viewTargetString; }
 
-    SVGElement* contextElement() const { return m_contextElement; }
     void detachContextElement();
 
     SVGTransformList* transform() { return m_transform ? m_transform->baseValue() : 0; }
