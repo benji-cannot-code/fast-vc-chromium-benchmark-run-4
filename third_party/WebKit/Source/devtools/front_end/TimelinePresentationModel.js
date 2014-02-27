9fd13d206e1eed259c201bf568082a2ddde4a10f
alph@chromium.org <alph@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-27 11:28:23 +0000

    DevTools: Fix typo in jsHeapSizeUsed
    TBR=pfeldman
    
    Review URL: https://codereview.chromium.org/177043011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168013 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/TimelinePresentationModel.js
+++ third_party/WebKit/Source/devtools/front_end/TimelinePresentationModel.js
@@ -995,7 +995,7 @@ WebInspector.TimelinePresentationModel.Record.prototype = {
      */
     get jsHeapSizeUsed()
     {
-        return this._record.counters ? this._record.counters.jsHeapSizeUsed | 0 : 0;
+        return this._record.counters ? this._record.counters.jsHeapSizeUsed || 0 : 0;
     },
 
     /**
