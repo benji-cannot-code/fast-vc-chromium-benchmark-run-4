843a22b89d5adf2b722744628b96d3fab97358be
psj@chromium.org <psj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-27 04:12:08 +0000

    Add gnubby auth handler to webapp
    BUG=134250
    
    Review URL: https://codereview.chromium.org/166273022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253711 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/webapp/client_plugin.js
+++ remoting/webapp/client_plugin.js
@@ -56,6 +56,8 @@ remoting.ClientPlugin = function(plugin, onExtensionMessage) {
   /** @param {!Array.<string>} capabilities The negotiated capabilities. */
   this.onSetCapabilitiesHandler = function (capabilities) {};
   this.fetchPinHandler = function (supportsPairing) {};
+  /** @param {string} data Remote gnubbyd data. */
+  this.onGnubbyAuthHandler = function(data) {};
 
   /** @type {remoting.MediaSourceRenderer} */
   this.mediaSourceRenderer_ = null;
@@ -293,9 +295,12 @@ remoting.ClientPlugin.prototype.handleMessageMethod_ = function(message) {
     this.onPairingComplete_(clientId, sharedSecret);
 
   } else if (message.method == 'extensionMessage') {
-    var extMsgType = getStringAttr(message, 'type');
-    var extMsgData = getStringAttr(message, 'data');
+    var extMsgType = getStringAttr(message.data, 'type');
+    var extMsgData = getStringAttr(message.data, 'data');
     switch (extMsgType) {
+      case 'gnubby-auth':
+        this.onGnubbyAuthHandler(extMsgData);
+        break;
       case 'test-echo-reply':
         console.log('Got echo reply: ' + extMsgData);
         break;
@@ -618,7 +623,7 @@ remoting.ClientPlugin.prototype.requestPairing =
  * Send an extension message to the host.
  *
  * @param {string} type The message type.
- * @param {Object} message The message payload.
+ * @param {string} message The message payload.
  */
 remoting.ClientPlugin.prototype.sendClientMessage =
     function(type, message) {
@@ -626,8 +631,8 @@ remoting.ClientPlugin.prototype.sendClientMessage =
     return;
   }
   this.plugin.postMessage(JSON.stringify(
-    { method: 'extensionMessage',
-      data: { type: type, data: JSON.stringify(message) } }));
+      { method: 'extensionMessage',
+        data: { type: type, data: message } }));
 
 };
 
