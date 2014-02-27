746a2cdb4de6c9d52d93a8413a32a1bdcb1e2080
pkasting@chromium.org <pkasting@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-27 00:49:59 +0000

    Revert 167914 "DevTools: Replace Apple styled spinner icon with ..."
    May have caused timeouts on layout test debug canaries.
    
    > DevTools: Replace Apple styled spinner icon with Chromium one.
    > 
    > Drive-by: replace animated PNG files with a CSS animation.
    > 
    > R=yurys@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/181433002
    
    TBR=alph@chromium.org
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167947 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/devtools.gypi
+++ third_party/WebKit/Source/devtools/devtools.gypi
@@ -441,6 +441,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'front_end/Images/sessionStorage.png',
             'front_end/Images/settingsListRemove.png',
             'front_end/Images/settingsListRemove_2x.png',
+            'front_end/Images/spinner.gif',
+            'front_end/Images/spinnerSelected.gif',
             'front_end/Images/statusbarButtonGlyphs.png',
             'front_end/Images/statusbarButtonGlyphs_2x.png',
             'front_end/Images/statusbarResizerHorizontal.png',
--- third_party/WebKit/Source/devtools/front_end/SidebarTreeElement.js
+++ third_party/WebKit/Source/devtools/front_end/SidebarTreeElement.js
@@ -54,7 +54,10 @@ WebInspector.SidebarSectionTreeElement.prototype = {
 
         this._smallChildren = x;
 
-        this._childrenListNode.classList.toggle("small", this._smallChildren);
+        if (this._smallChildren)
+            this._childrenListNode.classList.add("small");
+        else
+            this._childrenListNode.classList.remove("small");
     },
 
     onattach: function()
@@ -89,7 +92,7 @@ WebInspector.SidebarTreeElement = function(className, title, subtitle, represent
         this.disclosureButton.className = "disclosure-button";
     }
 
-    this.iconElement = document.createElementWithClass("div", "icon");
+    this.iconElement = document.createElementWithClass("img", "icon");
     this.statusElement = document.createElementWithClass("div", "status");
     this.titlesElement = document.createElementWithClass("div", "titles");
 
@@ -112,8 +115,13 @@ WebInspector.SidebarTreeElement.prototype = {
     set small(x)
     {
         this._small = x;
-        if (this._listItemNode)
-            this._listItemNode.classList.toggle("small", this._small);
+
+        if (this._listItemNode) {
+            if (this._small)
+                this._listItemNode.classList.add("small");
+            else
+                this._listItemNode.classList.remove("small");
+        }
     },
 
     get mainTitle()
@@ -140,7 +148,10 @@ WebInspector.SidebarTreeElement.prototype = {
 
     set wait(x)
     {
-        this._listItemNode.classList.toggle("wait", x);
+        if (x)
+            this._listItemNode.classList.add("wait");
+        else
+            this._listItemNode.classList.remove("wait");
     },
 
     refreshTitles: function()
