f14bc011cf434af4cdebd3e909646401a0fc29f0
atwilson@chromium.org <atwilson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-27 14:05:49 +0000

    Changed verification_key_hash to verification_key_id in NAC header
    BUG=347070
    
    Review URL: https://codereview.chromium.org/181643003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253797 0039d316-1c4b-4281-b951-d872f2087c98

--- components/policy/core/common/cloud/policy_header_service.cc
+++ components/policy/core/common/cloud/policy_header_service.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 const char kUserDMTokenKey[] = "user_dmtoken";
 const char kUserPolicyTokenKey[] = "user_policy_token";
-const char kVerificationKeyHashKey[] = "verification_key_hash";
+const char kVerificationKeyHashKey[] = "verification_key_id";
 }
 
 namespace policy {
