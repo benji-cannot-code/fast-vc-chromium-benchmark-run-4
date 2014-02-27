242e44b3fbc72280f0d72393215f8b44cc3817f2
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-27 02:33:09 +0000

    Sign Native Messaging binaries on mac
    The previous fix in r252903 (--deep parameter for codesign) didn't help
    presumably because the signing bot uses an older version of the
    codesign tool that doesn't support --deep. Updated do_signing.sh script
    to sign the binaries explicitly.
    
    BUG=345967
    R=jamiewalch@chromium.org
    
    Review URL: https://codereview.chromium.org/181673004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253690 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/installer/mac/do_signing.sh
+++ remoting/host/installer/mac/do_signing.sh
@@ -38,6 +38,8 @@ setup() {
 
   # Binaries to sign.
   ME2ME_HOST='PrivilegedHelperTools/org.chromium.chromoting.me2me_host.app'
+  ME2ME_NM_HOST="${ME2ME_HOST}/Contents/MacOS/native_messaging_host"
+  IT2ME_NM_HOST="${ME2ME_HOST}/Contents/MacOS/remote_assistance_host"
   UNINSTALLER="Applications/${HOST_UNINSTALLER_NAME}.app"
   PREFPANE='PreferencePanes/org.chromium.chromoting.prefPane'
 
@@ -117,7 +119,7 @@ sign() {
   fi
 
   echo Signing "${name}"
-  codesign -vv -s "${id}" --deep --keychain "${keychain}" "${name}"
+  codesign -vv -s "${id}" --keychain "${keychain}" "${name}"
   codesign -v "${name}"
 }
 
@@ -126,6 +128,8 @@ sign_binaries() {
   local keychain="${2}"
   local id="${3}"
 
+  sign "${input_dir}/${ME2ME_NM_HOST}" "${keychain}" "${id}"
+  sign "${input_dir}/${IT2ME_NM_HOST}" "${keychain}" "${id}"
   sign "${input_dir}/${ME2ME_HOST}" "${keychain}" "${id}"
   sign "${input_dir}/${UNINSTALLER}" "${keychain}" "${id}"
   sign "${input_dir}/${PREFPANE}" "${keychain}" "${id}"
