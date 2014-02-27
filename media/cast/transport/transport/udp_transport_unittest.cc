903944cd87dac1616d5ef0e98eb34b567615da4b
miu@chromium.org <miu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-27 03:29:24 +0000

    Clean up packet receive code in media::cast::transport::UdpTransport.
    Used iteration to avoid repeated posting of tasks to the IO message loop.
    
    BUG=342578
    R=hclam@chromium.org
    
    Review URL: https://codereview.chromium.org/180823002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253701 0039d316-1c4b-4281-b951-d872f2087c98

--- media/cast/transport/transport/udp_transport_unittest.cc
+++ media/cast/transport/transport/udp_transport_unittest.cc
@@ -48,6 +48,7 @@ class MockPacketReceiver {
 void SendPacket(UdpTransport* transport, Packet packet) {
   transport->SendPacket(packet);
 }
+
 static void UpdateCastTransportStatus(transport::CastTransportStatus status) {
   NOTREACHED();
 }
