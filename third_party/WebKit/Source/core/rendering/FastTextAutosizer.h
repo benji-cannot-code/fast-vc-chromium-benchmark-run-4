99e4867c0a358eea3e75c75271983d8adaa2ba4a
pdr@chromium.org <pdr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-27 01:08:54 +0000

    [FastTextAutosizer] Implement table autosizing logic
    This patch changes how tables are autosized in the fast text autosizer.
    Tables are shrink-wrapped and do not follow the block layout
    algorithm (see comment in RenderBox::computePreferredLogicalWidths). To
    work around this, RenderTable creates a FTA LayoutScope before its
    width is known, and we special-case the inflation step for tables.
    
    The big ideas in this patch:
    1) All tables become clusters.
    2) All table cells become clusters.
    3) A table cell cluster's multipler will be 1.0 if the table itself does
    not have enough text to autosize.
    4) The width we use for autosizing cells is based on the table's width.
    If it has fixed size, we use the table's width directly. Otherwise, we
    use the table's containing block's width.
    5) When laying out a table we compute the column widths based on the
    cell's preferred widths (before laying out the cells). Therefore, we
    need to pre-inflate the column widths before entering the main layout
    code for RenderTable. This logic is now handled in inflateTable, where
    we walk the table cells and inflate them as necessary.
    6) Once a table cell is laid out we cannot change its width. Therefore,
    descendants of table cells only inflate text up to the table's multiplier.
    
    Because we will be iterating on tables for a little while, the tests
    have been added as pixel tests.
    
    clusters-sufficient-width has been updated to reflect FTA's slightly
    different algorithm.
    
    NOTRY=true
    BUG=302005
    
    Review URL: https://codereview.chromium.org/167893005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167949 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/FastTextAutosizer.h
+++ third_party/WebKit/Source/core/rendering/FastTextAutosizer.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define FastTextAutosizer_h
 
 #include "core/rendering/RenderObject.h"
+#include "core/rendering/RenderTable.h"
 #include "core/rendering/TextAutosizer.h"
 #include "wtf/HashMap.h"
 #include "wtf/HashSet.h"
@@ -120,6 +121,7 @@ private:
             , m_multiplier(0)
             , m_textLength(-1)
             , m_supercluster(supercluster)
+            , m_hasTableAncestor(root->isTableCell() || (m_parent && m_parent->m_hasTableAncestor))
         {
         }
 
@@ -139,6 +141,7 @@ private:
         int m_textLength;
         // A set of blocks that are similar to this block.
         Supercluster* m_supercluster;
+        bool m_hasTableAncestor;
     };
 
     enum TextLeafSearch {
@@ -190,6 +193,7 @@ private:
 
     void beginLayout(RenderBlock*);
     void endLayout(RenderBlock*);
+    void inflateTable(RenderTable*);
     void inflate(RenderBlock*);
     bool enabled();
     void prepareRenderViewInfo();
@@ -205,6 +209,13 @@ private:
     const RenderBlock* deepestCommonAncestor(BlockSet&);
     float clusterMultiplier(Cluster*);
     float superclusterMultiplier(Supercluster*);
+    // A cluster's width provider is typically the deepest block containing all text.
+    // There are exceptions, such as tables and table cells which use the table itself for width.
+    const RenderBlock* clusterWidthProvider(const RenderBlock*);
+    // Typically this returns a block's computed width. In the case of tables layout, this
+    // width is not yet known so the fixed width is used if it's available, or the containing
+    // block's width otherwise.
+    float widthFromBlock(const RenderBlock*);
     float multiplierFromBlock(const RenderBlock*);
     void applyMultiplier(RenderObject*, float);
     bool mightBeWiderOrNarrowerDescendant(const RenderBlock*);
