3b43d4eb828ba9c7f5a721eb1f9763adf6526a75
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-27 03:17:20 +0000

    Make WebRTCInternals no longer listen for events that it doesn't care about.
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/176883004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253696 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/media/webrtc_internals.cc
+++ content/browser/media/webrtc_internals.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "content/browser/media/webrtc_internals_ui_observer.h"
 #include "content/public/browser/browser_thread.h"
-#include "content/public/browser/child_process_data.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/notification_types.h"
 #include "content/public/browser/render_process_host.h"
@@ -39,7 +38,6 @@ WebRTCInternals::WebRTCInternals()
     : aec_dump_enabled_(false) {
   registrar_.Add(this, NOTIFICATION_RENDERER_PROCESS_TERMINATED,
                  NotificationService::AllBrowserContextsAndSources());
-  BrowserChildProcessObserver::Add(this);
 // TODO(grunell): Shouldn't all the webrtc_internals* files be excluded from the
 // build if WebRTC is disabled?
 #if defined(ENABLE_WEBRTC)
@@ -63,7 +61,6 @@ WebRTCInternals::WebRTCInternals()
 }
 
 WebRTCInternals::~WebRTCInternals() {
-  BrowserChildProcessObserver::Remove(this);
 }
 
 WebRTCInternals* WebRTCInternals::GetInstance() {
@@ -274,12 +271,6 @@ void WebRTCInternals::SendUpdate(const string& command, base::Value* value) {
   FOR_EACH_OBSERVER(WebRTCInternalsUIObserver,
                     observers_,
                     OnUpdate(command, value));
-}
-
-void WebRTCInternals::BrowserChildProcessCrashed(
-    const ChildProcessData& data) {
-  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
-  OnRendererExit(data.id);
 }
 
 void WebRTCInternals::Observe(int type,
