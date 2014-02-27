7cf5b1a41a428c4aa126ec9d76210fa5db2d413a
mnaganov@chromium.org <mnaganov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-27 18:15:20 +0000

    [Android WebView] Disable AwContentsClientShouldInterceptRequestTest.testCalledForImage
    Seems to be flaky. Reproducible locally.
    
    BUG=347599
    NOTRY=true
    R=boliu@chromium.org
    
    Review URL: https://codereview.chromium.org/183503003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253866 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/javatests/src/org/chromium/android_webview/test/AwContentsClientShouldInterceptRequestTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwContentsClientShouldInterceptRequestTest.java
@@ -11,6 +11,7 @@ import android.util.Pair;
 import org.chromium.android_webview.AwContents;
 import org.chromium.android_webview.InterceptedRequestData;
 import org.chromium.android_webview.test.util.CommonResources;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.TestFileUtil;
 import org.chromium.content.browser.test.util.CallbackHelper;
@@ -436,8 +437,12 @@ public class AwContentsClientShouldInterceptRequestTest extends AwTestBase {
         assertEquals(0, mWebServer.getRequestCount("/" + CommonResources.ABOUT_FILENAME));
     }
 
-    @SmallTest
-    @Feature({"AndroidWebView"})
+    /*
+     @SmallTest
+     @Feature({"AndroidWebView"})
+     http://crbug.com/347599
+    */
+    @DisabledTest
     public void testCalledForImage() throws Throwable {
         final String imagePath = "/" + CommonResources.FAVICON_FILENAME;
         mWebServer.setResponseBase64(imagePath,
