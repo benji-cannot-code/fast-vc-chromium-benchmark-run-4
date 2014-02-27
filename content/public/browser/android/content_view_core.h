424d0a1a77e2d8093a220312e882cb5185a9ab3d
clholgat@chromium.org <clholgat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-27 06:15:58 +0000

    Adds support for capturing a sub rect of the compositing surface.
    Also removes the old PopulateBitmapWithContents API.
    
    BUG=345865
    
    Review URL: https://codereview.chromium.org/173023005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253736 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/browser/android/content_view_core.h
+++ content/public/browser/android/content_view_core.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "content/common/content_export.h"
 #include "content/public/browser/navigation_controller.h"
+#include "ui/gfx/rect.h"
 
 class SkBitmap;
 
@@ -58,6 +59,7 @@ class CONTENT_EXPORT ContentViewCore {
   virtual void GetScaledContentBitmap(
       float scale,
       jobject bitmap_config,
+      gfx::Rect src_rect,
       const base::Callback<void(bool, const SkBitmap&)>& result_callback) = 0;
   virtual float GetDpiScale() const = 0;
   virtual void PauseVideo() = 0;
