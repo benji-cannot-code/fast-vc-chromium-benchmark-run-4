7a8d12f41b51f3b08f3cdc85f46cceb96f992967
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-27 13:57:27 +0000

    Use browser_backend's browser which is set by SetBrowser.
    BUG=345875,347319
    TEST=manual
    
    Review URL: https://codereview.chromium.org/179763007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253793 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/backends/chrome/inspector_backend.py
+++ tools/telemetry/telemetry/core/backends/chrome/inspector_backend.py
@@ -29,8 +29,7 @@ class InspectorException(Exception):
 
 
 class InspectorBackend(object):
-  def __init__(self, browser, browser_backend, context, timeout=60):
-    self._browser = browser
+  def __init__(self, browser_backend, context, timeout=60):
     self._browser_backend = browser_backend
     self._context = context
     self._socket = None
@@ -78,7 +77,7 @@ class InspectorBackend(object):
 
   @property
   def browser(self):
-    return self._browser
+    return self._browser_backend.browser
 
   @property
   def url(self):
