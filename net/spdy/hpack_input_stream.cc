7767b3d586d0a47dda3645ce6d20729e8543af1c
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-28 02:24:32 +0000

    Add missing #include of <algorithm> for std::min
    (Required on VS2013)
    
    TBR=mattm@chromium.org
    BUG=309197
    
    Review URL: https://codereview.chromium.org/180153007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254026 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/hpack_input_stream.cc
+++ net/spdy/hpack_input_stream.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/spdy/hpack_input_stream.h"
 
+#include <algorithm>
+
 #include "base/basictypes.h"
 #include "base/logging.h"
 
