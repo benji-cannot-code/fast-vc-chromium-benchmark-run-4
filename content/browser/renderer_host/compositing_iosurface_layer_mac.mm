cd7c5c69ed50596b7822844ea422e5186ddbf419
ccameron@chromium.org <ccameron@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-28 23:42:17 +0000

    Remove context property that is never used
    We're seeing crashes in this. Removing it probably won't make it go 
    away, but might move them to something more actionable. 
    
    BUG=348158
    R=kbr
    
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254274 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/compositing_iosurface_layer_mac.mm
+++ content/browser/renderer_host/compositing_iosurface_layer_mac.mm
@@ -27,8 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation CompositingIOSurfaceLayer
 
-@synthesize context = context_;
-
 - (id)initWithRenderWidgetHostViewMac:(content::RenderWidgetHostViewMac*)r {
   if (self = [super init]) {
     renderWidgetHostView_ = r;
