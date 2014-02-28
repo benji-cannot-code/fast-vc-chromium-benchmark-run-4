fd25da08f45cf0c609cd4b76768aa2415ea95c24
tommycli@chromium.org <tommycli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-28 00:03:10 +0000

    MTP Streaming: Optimize block reading
    Previously, due to some unoptimal code, we were doing two mtp stats per block.
    This cuts it down to one mtp stat per block. Still not quite there, but working on it...
    
    BUG=110119
    TBR=thestig@chromium.org
    
    Review URL: https://codereview.chromium.org/180783005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253980 0039d316-1c4b-4281-b951-d872f2087c98

--- device/media_transfer_protocol/media_transfer_protocol_daemon_client.cc
+++ device/media_transfer_protocol/media_transfer_protocol_daemon_client.cc
@@ -20,6 +20,7 @@ namespace device {
 namespace {
 
 const char kInvalidResponseMsg[] = "Invalid Response: ";
+uint32 kMaxChunkSize = 1024*1024;  // D-Bus has message size limits.
 
 // The MediaTransferProtocolDaemonClient implementation.
 class MediaTransferProtocolDaemonClientImpl
@@ -141,6 +142,7 @@ class MediaTransferProtocolDaemonClientImpl
       uint32 bytes_to_read,
       const ReadFileCallback& callback,
       const ErrorCallback& error_callback) OVERRIDE {
+    DCHECK_LE(bytes_to_read, kMaxChunkSize);
     dbus::MethodCall method_call(mtpd::kMtpdInterface,
                                  mtpd::kReadFileChunkByPath);
     dbus::MessageWriter writer(&method_call);
@@ -163,6 +165,7 @@ class MediaTransferProtocolDaemonClientImpl
                                  uint32 bytes_to_read,
                                  const ReadFileCallback& callback,
                                  const ErrorCallback& error_callback) OVERRIDE {
+    DCHECK_LE(bytes_to_read, kMaxChunkSize);
     dbus::MethodCall method_call(mtpd::kMtpdInterface,
                                  mtpd::kReadFileChunkById);
     dbus::MessageWriter writer(&method_call);
