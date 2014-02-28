5ca4108b15a779e0d1bed27aa6ee3d8cf94caac8
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-28 12:41:13 +0000

    TimelineFlameChart: REGRESSION. one line fix after r168084.
    BUG=
    TBR=apavlov
    
    Review URL: https://codereview.chromium.org/184513002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168122 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/FlameChart.js
+++ third_party/WebKit/Source/devtools/front_end/FlameChart.js
@@ -363,7 +363,7 @@ WebInspector.FlameChart.ColorGenerator.prototype = {
     {
         var colorData = {index: this._currentColorIndex++, color: color};
         this._colors[id] = colorData;
-        this._colorIndexes[color.index] = colorData;
+        this._colorIndexes[colorData.index] = colorData;
     },
 
     /**
