f520d14f9f66fc0d6ddd983b6d9d0b5270923290
mpawlowski@opera.com <mpawlowski@opera.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-28 01:27:24 +0000

    Fix memory leak in components_unittests
    This is a trivial fix for a leak that fires on the bots
    
    BUG=
    
    Review URL: https://codereview.chromium.org/183273003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253998 0039d316-1c4b-4281-b951-d872f2087c98

--- components/password_manager/core/browser/login_database_unittest.cc
+++ components/password_manager/core/browser/login_database_unittest.cc
@@ -702,6 +702,7 @@ TEST_F(LoginDatabaseTest, UpdateIncompleteCredentials) {
   expected_form.password_value.clear();
 #endif  // OS_MACOSX
   EXPECT_EQ(expected_form, *result[0]);
+  ClearResults(&result);
 }
 
 #if defined(OS_POSIX)
