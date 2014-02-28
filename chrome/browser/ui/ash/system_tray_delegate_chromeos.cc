33b89c8e79a3fbcd8b9fd6d752c685ae2dbceb1e
ygorshenin@google.com <ygorshenin@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-28 10:23:12 +0000

    Added a special badge to wifi-networks behind a captive portal.
    BUG=240237
    TEST=manual tests on Lumpy
    R=oshima@chromium.org, stevenjb@chromium.org
    
    Review URL: https://codereview.chromium.org/175053003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254079 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/ash/system_tray_delegate_chromeos.cc
+++ chrome/browser/ui/ash/system_tray_delegate_chromeos.cc
@@ -66,6 +66,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/user.h"
 #include "chrome/browser/chromeos/login/user_adding_screen.h"
 #include "chrome/browser/chromeos/login/user_manager.h"
+#include "chrome/browser/chromeos/net/network_portal_detector.h"
 #include "chrome/browser/chromeos/options/network_config_view.h"
 #include "chrome/browser/chromeos/policy/browser_policy_connector_chromeos.h"
 #include "chrome/browser/chromeos/policy/device_cloud_policy_manager_chromeos.h"
@@ -894,6 +895,13 @@ int SystemTrayDelegateChromeOS::GetSystemTrayMenuWidth() {
 
 void SystemTrayDelegateChromeOS::ActiveUserWasChanged() {
   GetSystemTrayNotifier()->NotifyUserUpdate();
+}
+
+bool SystemTrayDelegateChromeOS::IsNetworkBehindCaptivePortal(
+    const std::string& service_path) const {
+  NetworkPortalDetector::CaptivePortalState state =
+      NetworkPortalDetector::Get()->GetCaptivePortalState(service_path);
+  return state.status == NetworkPortalDetector::CAPTIVE_PORTAL_STATUS_PORTAL;
 }
 
 ash::SystemTray* SystemTrayDelegateChromeOS::GetPrimarySystemTray() {
