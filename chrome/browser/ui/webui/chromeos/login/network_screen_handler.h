9f529752be2b1be99edf3980d1778992535533f5
rkc@chromium.org <rkc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-28 23:12:24 +0000

    Change demo mode derelict detection. This CL changes the demo mode derelict detection to now take the total time on OOBE instead of idle time on OOBE. This is a much more accurate representation of a derelict state than idle time since a machine can be not idle for 8 hours at a stretch and still be effectively derelict if it is still sitting at OOBE after 8 hours.
    R=xiyuan@chromium.org
    BUG=345436
    TEST=Use the --oobe-timer-interval flag to test the new functionality and verify it works correctly.
    
    Review URL: https://codereview.chromium.org/184033002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254267 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chromeos/login/network_screen_handler.h
+++ chrome/browser/ui/webui/chromeos/login/network_screen_handler.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/time/time.h"
+#include "base/timer/timer.h"
 #include "chrome/browser/chromeos/login/screens/network_screen_actor.h"
 #include "chrome/browser/chromeos/settings/cros_settings.h"
 #include "chrome/browser/ui/webui/chromeos/login/base_screen_handler.h"
@@ -77,10 +78,13 @@ class NetworkScreenHandler : public NetworkScreenActor,
   // Callback when the system timezone settings is changed.
   void OnSystemTimezoneChanged();
 
-  // Idle detection related methods.
+  // Demo mode detection methods.
   void StartIdleDetection();
+  void StartOobeTimer();
   void OnIdle();
+  void OnOobeTimerUpdate();
   void SetupTimeouts();
+  bool IsDerelict();
 
   // Returns available languages. Caller gets the ownership. Note, it does
   // depend on the current locale.
@@ -98,8 +102,8 @@ class NetworkScreenHandler : public NetworkScreenActor,
 
   bool is_continue_enabled_;
 
-  // Flag set if we believe this is an abandoned machine.
-  bool is_derelict_;
+  // Total time this machine has spent on OOBE.
+  base::TimeDelta time_on_oobe_;
 
   // Keeps whether screen should be shown right after initialization.
   bool show_on_init_;
@@ -111,10 +115,14 @@ class NetworkScreenHandler : public NetworkScreenActor,
 
   scoped_ptr<IdleDetector> detector_;
 
+  base::RepeatingTimer<NetworkScreenHandler> oobe_timer_;
+
   // Timeout to detect if the machine is in a derelict state.
   base::TimeDelta derelict_detection_timeout_;
   // Timeout before showing our demo up if the machine is in a derelict state.
   base::TimeDelta derelict_idle_timeout_;
+  // Time between updating our total time on oobe.
+  base::TimeDelta oobe_timer_update_interval_;
 
   base::WeakPtrFactory<NetworkScreenHandler> weak_ptr_factory_;
 
