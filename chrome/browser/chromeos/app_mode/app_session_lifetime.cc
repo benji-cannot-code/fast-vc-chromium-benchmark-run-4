268e0be891dbb2cf0014629a68e8b00fa1bef517
rkc@chromium.org <rkc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-28 21:38:25 +0000

    Skip update service and idle app notifications for demo app.
    R=xiyuan@chromium.org
    BUG=346570
    TEST=Tested that the idle app notification does not come up for the demo app.
    
    Review URL: https://codereview.chromium.org/181593008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254236 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/app_mode/app_session_lifetime.cc
+++ chrome/browser/chromeos/app_mode/app_session_lifetime.cc
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chromeos/app_mode/kiosk_app_update_service.h"
 #include "chrome/browser/chromeos/app_mode/kiosk_mode_idle_app_name_notification.h"
+#include "chrome/browser/chromeos/login/demo_mode/demo_app_launcher.h"
+#include "chrome/browser/chromeos/login/user_manager.h"
 #include "chrome/browser/chromeos/policy/browser_policy_connector_chromeos.h"
 #include "chrome/browser/lifetime/application_lifetime.h"
 #include "chrome/browser/profiles/profile.h"
@@ -113,6 +115,12 @@ void InitAppSession(Profile* profile, const std::string& app_id) {
 
   CHECK(browser_window_handler == NULL);
   browser_window_handler.Get();
+
+  // For a demo app, we don't need to either setup the update service or
+  // the idle app name notification.
+  if (DemoAppLauncher::IsDemoAppSession(
+      chromeos::UserManager::Get()->GetActiveUser()->email()))
+    return;
 
   // Set the app_id for the current instance of KioskAppUpdateService.
   KioskAppUpdateService* update_service =
