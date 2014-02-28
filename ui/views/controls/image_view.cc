ec8e6c6ee20d6705715778fa86c6abaa4e1d7d85
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-28 20:40:21 +0000

    Fixes bug in ImageView caching of pixels
    It needed to lock the pixels before accessing them.
    
    BUG=336156
    TEST=see bug
    R=pkotwicz@chromium.org
    
    Review URL: https://codereview.chromium.org/184763002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254213 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/image_view.cc
+++ ui/views/controls/image_view.cc
@@ -15,6 +15,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace views {
 
+namespace {
+
+// Returns the pixels for the bitmap in |image| at scale |image_scale|.
+void* GetBitmapPixels(const gfx::ImageSkia& img, float image_scale) {
+  DCHECK_NE(0.0f, image_scale);
+  const SkBitmap& bitmap = img.GetRepresentation(image_scale).sk_bitmap();
+  SkAutoLockPixels pixel_lock(bitmap);
+  return bitmap.getPixels();
+}
+
+}  // namespace
+
 ImageView::ImageView()
     : image_size_set_(false),
       horiz_alignment_(CENTER),
@@ -100,8 +112,7 @@ bool ImageView::IsImageEqual(const gfx::ImageSkia& img) const {
   // the backing store but also the pixels of the last image we painted.
   return image_.BackedBySameObjectAs(img) &&
       last_paint_scale_ != 0.0f &&
-      last_painted_bitmap_pixels_ ==
-      img.GetRepresentation(last_paint_scale_).sk_bitmap().getPixels();
+      last_painted_bitmap_pixels_ == GetBitmapPixels(img, last_paint_scale_);
 }
 
 gfx::Point ImageView::ComputeImageOrigin(const gfx::Size& image_size) const {
@@ -221,8 +232,7 @@ void ImageView::OnPaintImage(gfx::Canvas* canvas) {
   } else {
     canvas->DrawImageInt(image_, image_bounds.x(), image_bounds.y());
   }
-  last_painted_bitmap_pixels_ =
-      image_.GetRepresentation(last_paint_scale_).sk_bitmap().getPixels();
+  last_painted_bitmap_pixels_ = GetBitmapPixels(image_, last_paint_scale_);
 }
 
 }  // namespace views
