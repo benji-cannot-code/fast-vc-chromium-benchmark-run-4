f46bb8376c7619f0b028579e95ee60313cc37ef6
pkasting@chromium.org <pkasting@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-28 01:59:01 +0000

    Add an explicit virtual destructor to WebScrollbarBehavior.
    This fixes a clang warning on the Chrome side when trying to compile
    scoped_ptr<WebScrollbarBehavior> about "delete called on class with virtual
    functions but non-virtual destructor".
    
    BUG=337919
    TEST=none
    TBR=abarth
    
    Review URL: https://codereview.chromium.org/183833005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168076 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebScrollbarBehavior.h
+++ third_party/WebKit/public/platform/WebScrollbarBehavior.h
@@ -19,6 +19,7 @@ public:
         ButtonMiddle,
         ButtonRight
     };
+    virtual ~WebScrollbarBehavior() { }
     virtual bool shouldCenterOnThumb(Button, bool shiftKeyPressed, bool altKeyPressed) { return false; }
     virtual bool shouldSnapBackToDragOrigin(const WebPoint& eventPoint, const WebRect& scrollbarRect, bool isHorizontal) { return false; }
 };
