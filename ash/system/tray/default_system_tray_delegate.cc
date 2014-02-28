bca100a46de1c7c7970e659cfb6911f7f7bb734a
ygorshenin@google.com <ygorshenin@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-28 10:37:47 +0000

    Revert 254079 "Added a special badge to wifi-networks behind a c..."
    > Added a special badge to wifi-networks behind a captive portal.
    > 
    > BUG=240237
    > TEST=manual tests on Lumpy
    > R=oshima@chromium.org, stevenjb@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/175053003
    
    TBR=ygorshenin@google.com
    
    Review URL: https://codereview.chromium.org/180613003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254086 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/tray/default_system_tray_delegate.cc
+++ ash/system/tray/default_system_tray_delegate.cc
@@ -285,9 +285,4 @@ int DefaultSystemTrayDelegate::GetSystemTrayMenuWidth() {
 void DefaultSystemTrayDelegate::ActiveUserWasChanged() {
 }
 
-bool DefaultSystemTrayDelegate::IsNetworkBehindCaptivePortal(
-    const std::string& /* service_path */) const {
-  return false;
-}
-
 }  // namespace ash
--- ash/system/tray/system_tray_notifier.h
+++ ash/system/tray/system_tray_notifier.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_CHROMEOS)
 #include "ash/system/chromeos/enterprise/enterprise_domain_observer.h"
 #include "ash/system/chromeos/network/network_observer.h"
-#include "ash/system/chromeos/network/network_portal_detector_observer.h"
 #include "ash/system/chromeos/screen_security/screen_capture_observer.h"
 #include "ash/system/chromeos/screen_security/screen_share_observer.h"
 #endif
@@ -88,11 +87,6 @@ class ASH_EXPORT SystemTrayNotifier {
   void AddNetworkObserver(NetworkObserver* observer);
   void RemoveNetworkObserver(NetworkObserver* observer);
 
-  void AddNetworkPortalDetectorObserver(
-      NetworkPortalDetectorObserver* observer);
-  void RemoveNetworkPortalDetectorObserver(
-      NetworkPortalDetectorObserver* observer);
-
   void AddEnterpriseDomainObserver(EnterpriseDomainObserver* observer);
   void RemoveEnterpriseDomainObserver(EnterpriseDomainObserver* observer);
 
@@ -132,7 +126,6 @@ class ASH_EXPORT SystemTrayNotifier {
   void NotifyUserAddedToSession();
 #if defined(OS_CHROMEOS)
   void NotifyRequestToggleWifi();
-  void NotifyOnCaptivePortalDetected(const std::string& service_path);
   void NotifyEnterpriseDomainChanged();
   void NotifyScreenCaptureStart(const base::Closure& stop_callback,
                                 const base::string16& sharing_app_name);
@@ -162,8 +155,6 @@ class ASH_EXPORT SystemTrayNotifier {
   ObserverList<UserObserver> user_observers_;
 #if defined(OS_CHROMEOS)
   ObserverList<NetworkObserver> network_observers_;
-  ObserverList<NetworkPortalDetectorObserver>
-      network_portal_detector_observers_;
   ObserverList<EnterpriseDomainObserver> enterprise_domain_observers_;
   ObserverList<ScreenCaptureObserver> screen_capture_observers_;
   ObserverList<ScreenShareObserver> screen_share_observers_;
