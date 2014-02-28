cd961eefd048216d1393ed9458008610584d8f2d
dnicoara@chromium.org <dnicoara@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-28 18:35:55 +0000

    Rename Start/Stop in OutputConfigurator to better reflect their intent
    BUG=
    
    Review URL: https://codereview.chromium.org/181483004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254180 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/display/output_configurator_unittest.cc
+++ chromeos/display/output_configurator_unittest.cc
@@ -464,7 +464,7 @@ class OutputConfiguratorTest : public testing::Test {
     EXPECT_EQ(kNoActions, log_->GetActionsAndClear());
     configurator_.Init(false);
     EXPECT_EQ(kNoActions, log_->GetActionsAndClear());
-    configurator_.Start(0);
+    configurator_.ForceInitialConfigure(0);
     EXPECT_EQ(
         JoinActions(
             kGrab,
@@ -998,7 +998,7 @@ TEST_F(OutputConfiguratorTest, Headless) {
   EXPECT_EQ(kNoActions, log_->GetActionsAndClear());
   configurator_.Init(false);
   EXPECT_EQ(kNoActions, log_->GetActionsAndClear());
-  configurator_.Start(0);
+  configurator_.ForceInitialConfigure(0);
   EXPECT_EQ(JoinActions(kGrab, kInitXRandR, kForceDPMS, kUngrab, NULL),
             log_->GetActionsAndClear());
 
@@ -1034,7 +1034,7 @@ TEST_F(OutputConfiguratorTest, StartWithTwoOutputs) {
   EXPECT_EQ(kNoActions, log_->GetActionsAndClear());
 
   state_controller_.set_state(ui::OUTPUT_STATE_DUAL_MIRROR);
-  configurator_.Start(0);
+  configurator_.ForceInitialConfigure(0);
   EXPECT_EQ(
       JoinActions(
           kGrab,
@@ -1057,7 +1057,7 @@ TEST_F(OutputConfiguratorTest, InvalidOutputStates) {
   UpdateOutputs(0, false);
   EXPECT_EQ(kNoActions, log_->GetActionsAndClear());
   configurator_.Init(false);
-  configurator_.Start(0);
+  configurator_.ForceInitialConfigure(0);
   observer_.Reset();
   EXPECT_TRUE(configurator_.SetDisplayMode(ui::OUTPUT_STATE_HEADLESS));
   EXPECT_FALSE(configurator_.SetDisplayMode(ui::OUTPUT_STATE_SINGLE));
@@ -1091,7 +1091,7 @@ TEST_F(OutputConfiguratorTest, GetOutputStateForDisplaysWithoutId) {
   UpdateOutputs(2, false);
   configurator_.Init(false);
   state_controller_.set_state(ui::OUTPUT_STATE_DUAL_MIRROR);
-  configurator_.Start(0);
+  configurator_.ForceInitialConfigure(0);
   EXPECT_EQ(ui::OUTPUT_STATE_DUAL_EXTENDED, configurator_.output_state());
 }
 
@@ -1100,7 +1100,7 @@ TEST_F(OutputConfiguratorTest, GetOutputStateForDisplaysWithId) {
   UpdateOutputs(2, false);
   configurator_.Init(false);
   state_controller_.set_state(ui::OUTPUT_STATE_DUAL_MIRROR);
-  configurator_.Start(0);
+  configurator_.ForceInitialConfigure(0);
   EXPECT_EQ(ui::OUTPUT_STATE_DUAL_MIRROR, configurator_.output_state());
 }
 
@@ -1141,7 +1141,7 @@ TEST_F(OutputConfiguratorTest, PanelFitting) {
   UpdateOutputs(2, false);
   state_controller_.set_state(ui::OUTPUT_STATE_DUAL_MIRROR);
   configurator_.Init(true /* is_panel_fitting_enabled */);
-  configurator_.Start(0);
+  configurator_.ForceInitialConfigure(0);
   EXPECT_EQ(ui::OUTPUT_STATE_DUAL_MIRROR, configurator_.output_state());
   EXPECT_EQ(
       JoinActions(
@@ -1180,7 +1180,7 @@ TEST_F(OutputConfiguratorTest, PanelFitting) {
 
 TEST_F(OutputConfiguratorTest, OutputProtection) {
   configurator_.Init(false);
-  configurator_.Start(0);
+  configurator_.ForceInitialConfigure(0);
   EXPECT_NE(kNoActions, log_->GetActionsAndClear());
 
   OutputConfigurator::OutputProtectionClientId id =
@@ -1243,7 +1243,7 @@ TEST_F(OutputConfiguratorTest, OutputProtectionTwoClients) {
   EXPECT_NE(client1, client2);
 
   configurator_.Init(false);
-  configurator_.Start(0);
+  configurator_.ForceInitialConfigure(0);
   UpdateOutputs(2, true);
   EXPECT_NE(kNoActions, log_->GetActionsAndClear());
 
@@ -1291,7 +1291,7 @@ TEST_F(OutputConfiguratorTest, CTMForMultiScreens) {
   UpdateOutputs(2, false);
   configurator_.Init(false);
   state_controller_.set_state(ui::OUTPUT_STATE_DUAL_EXTENDED);
-  configurator_.Start(0);
+  configurator_.ForceInitialConfigure(0);
 
   const int kDualHeight =
       kSmallModeHeight + OutputConfigurator::kVerticalGap + kBigModeHeight;
