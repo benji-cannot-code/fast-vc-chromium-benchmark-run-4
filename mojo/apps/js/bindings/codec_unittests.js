6bd6a39428ebc5ddec785376bb772d7f4666dd3b
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-28 20:15:45 +0000

    Mojo: fix js bindings
    BUG=348080
    R=davemoore@chromium.org
    
    Review URL: https://codereview.chromium.org/183273007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254205 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/apps/js/bindings/codec_unittests.js
+++ mojo/apps/js/bindings/codec_unittests.js
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 define([
     "gin/test/expect",
     "mojo/apps/js/bindings/codec",
-    "mojom/sample_service",
+    "mojo/public/bindings/tests/sample_service.mojom",
   ], function(expect, codec, sample) {
   testBar();
   testFoo();
