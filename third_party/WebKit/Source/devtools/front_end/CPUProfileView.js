213a51cbe78f2d9376b00f8d8164006790af05a4
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-28 13:24:26 +0000

    FlameChart: reduce number of arrays in timelineData.
    On the next step I'd change the logic of color processing.
    
    BUG=
    R=pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/184353003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168127 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/CPUProfileView.js
+++ third_party/WebKit/Source/devtools/front_end/CPUProfileView.js
@@ -1049,6 +1049,7 @@ WebInspector.CPUProfileView.colorGenerator = function()
 /**
  * @constructor
  * @implements {WebInspector.FlameChartDataProvider}
+ * @param {!WebInspector.CPUProfileView} cpuProfileView
  */
 WebInspector.CPUFlameChartDataProvider = function(cpuProfileView)
 {
@@ -1058,6 +1059,14 @@ WebInspector.CPUFlameChartDataProvider = function(cpuProfileView)
 }
 
 WebInspector.CPUFlameChartDataProvider.prototype = {
+    /**
+     * @return {number}
+     */
+    barHeight: function()
+    {
+        return 15;
+    },
+
     /**
      * @param {number} startTime
      * @param {number} endTime
@@ -1224,7 +1233,6 @@ WebInspector.CPUFlameChartDataProvider.prototype = {
             entryColorIndexes[i] = color.index;
             entryLevels[i] = entry.depth;
             entryTotalTimes[i] = entry.duration;
-            entrySelfTimes[i] = entry.selfTime;
             entryOffsets[i] = entry.startTime;
             entryTitles[i] = entry.node.functionName;
             var reason = entry.node.deoptReason;
@@ -1234,16 +1242,16 @@ WebInspector.CPUFlameChartDataProvider.prototype = {
         this._maxStackDepth = Math.max(maxDepth, depth);
 
         this._timelineData = {
-            entryNodes: entryNodes,
             entryColorIndexes: entryColorIndexes,
             entryLevels: entryLevels,
             entryTotalTimes: entryTotalTimes,
-            entrySelfTimes: entrySelfTimes,
             entryOffsets: entryOffsets,
-            colorEntryIndexes: colorEntryIndexes,
-            entryTitles: entryTitles,
-            entryDeoptFlags: entryDeoptFlags
         };
+
+        this._entryTitles = entryTitles;
+        this._entryNodes = entryNodes;
+        this._entrySelfTimes = entrySelfTimes;
+        this._entryDeoptFlags = entryDeoptFlags;
 
         return /** @type {!WebInspector.FlameChart.TimelineData} */ (this._timelineData);
     },
@@ -1268,7 +1276,7 @@ WebInspector.CPUFlameChartDataProvider.prototype = {
     prepareHighlightedEntryInfo: function(entryIndex)
     {
         var timelineData = this._timelineData;
-        var node = timelineData.entryNodes[entryIndex];
+        var node = this._entryNodes[entryIndex];
         if (!node)
             return null;
 
@@ -1281,8 +1289,8 @@ WebInspector.CPUFlameChartDataProvider.prototype = {
             entryInfo.push(row);
         }
 
-        pushEntryInfoRow(WebInspector.UIString("Name"), timelineData.entryTitles[entryIndex]);
-        var selfTime = this._millisecondsToString(timelineData.entrySelfTimes[entryIndex]);
+        pushEntryInfoRow(WebInspector.UIString("Name"), this._entryTitles[entryIndex]);
+        var selfTime = this._millisecondsToString(this._entrySelfTimes[entryIndex]);
         var totalTime = this._millisecondsToString(timelineData.entryTotalTimes[entryIndex]);
         pushEntryInfoRow(WebInspector.UIString("Self time"), selfTime);
         pushEntryInfoRow(WebInspector.UIString("Total time"), totalTime);
@@ -1302,7 +1310,29 @@ WebInspector.CPUFlameChartDataProvider.prototype = {
      */
     canJumpToEntry: function(entryIndex)
     {
-        return this._timelineData.entryNodes[entryIndex].scriptId !== "0";
+        return this._entryNodes[entryIndex].scriptId !== "0";
+    },
+
+    /**
+     * @param {number} entryIndex
+     * @return {?string}
+     */
+    entryTitle: function(entryIndex)
+    {
+        return this._entryTitles[entryIndex];
+    },
+
+    /**
+     * @param {number} entryIndex
+     * @return {?string}
+     */
+    entryFont: function(entryIndex)
+    {
+        if (!this._font) {
+            this._font = (this.barHeight() - 4) + "px " + WebInspector.fontFamily();
+            this._boldFont = "bold " + this._font;
+        }
+        return this._entryDeoptFlags[entryIndex] ? this._boldFont : this._font;
     },
 
     /**
@@ -1311,7 +1341,6 @@ WebInspector.CPUFlameChartDataProvider.prototype = {
      */
     entryData: function(entryIndex)
     {
-        return this._timelineData.entryNodes[entryIndex];
+        return this._entryNodes[entryIndex];
     }
 }
-
