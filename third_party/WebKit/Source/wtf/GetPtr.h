76dae4d1aa268ade35adb539ac06a279a46bacdd
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-28 12:15:04 +0000

    Partially revert r168100
    In r168100, I mis-committed an unnecessary change to wtf/ to fix compile failure.
    (The wtf/ change became unnecessary because the compile failure was fixed
    in https://codereview.chromium.org/182733004/.)
    This CL reverts the wtf/ part of r168100.
    
    TBR=tkent
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/176883010
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168121 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/GetPtr.h
+++ third_party/WebKit/Source/wtf/GetPtr.h
@@ -36,11 +36,6 @@ namespace WTF {
         return &p;
     }
 
-    template <typename T> inline T* getPtr(RawPtr<T> p)
-    {
-        return p.get();
-    }
-
 } // namespace WTF
 
 #endif // WTF_GetPtr_h
