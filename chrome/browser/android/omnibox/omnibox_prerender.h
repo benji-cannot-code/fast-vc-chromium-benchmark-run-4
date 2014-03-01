2a96c18c4268eefa3869f7b78a86b30b88de7d50
yang.gu@intel.com <yang.gu@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-01 01:40:30 +0000

    Fix build issues in chrome/ for Android x64
    BUG=346626
    
    Review URL: https://codereview.chromium.org/179873015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254313 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/android/omnibox/omnibox_prerender.h
+++ chrome/browser/android/omnibox/omnibox_prerender.h
@@ -51,7 +51,7 @@ class OmniboxPrerender {
                       jobject obj,
                       jstring j_url,
                       jstring j_current_url,
-                      jint jsource_match,
+                      jlong jsource_match,
                       jobject j_profile_android,
                       jobject j_tab);
 
