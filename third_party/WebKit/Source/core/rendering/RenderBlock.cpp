541a2fbb569e34c19bd7c1a5edad9491c219381d
simonhatch@chromium.org <simonhatch@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-01 04:01:03 +0000

    Follow up on ojan's comments from https://codereview.chromium.org/131233003/
    Remove call to ResourceLoadPriorityOptimizer from RenderBlock Destructor. This isn't necessary since it's already called from the RenderObject destructor.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/148523010
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168236 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderBlock.cpp
+++ third_party/WebKit/Source/core/rendering/RenderBlock.cpp
@@ -218,7 +218,6 @@ static void appendImagesFromStyle(Vector<ImageResource*>& images, RenderStyle& b
 
 RenderBlock::~RenderBlock()
 {
-    ResourceLoadPriorityOptimizer::resourceLoadPriorityOptimizer()->removeRenderObject(this);
     if (hasColumns())
         gColumnInfoMap->take(this);
     if (gPercentHeightDescendantsMap)
