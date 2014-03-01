f589c2a7c95dc87df180cf229aa631bceef6c379
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-01 00:09:27 +0000

    Remove blank password UMA collection
    This seems to cause some XP machines and Vista machines with blank
    passwords to not work at all. This is because we call GetOsPasswordStatus()
    10 seconds after a webcontents is created.
    
    TBR=jam
    BUG=345916
    
    Review URL: https://codereview.chromium.org/184843004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254280 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/password_manager/chrome_password_manager_client.cc
+++ chrome/browser/password_manager/chrome_password_manager_client.cc
@@ -33,19 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/password_authentication_manager.h"
 #endif  // OS_ANDROID
 
-namespace {
-
-void ReportOsPassword() {
-  password_manager_util::OsPasswordStatus status =
-      password_manager_util::GetOsPasswordStatus();
-
-  UMA_HISTOGRAM_ENUMERATION("PasswordManager.OsPasswordStatus",
-                            status,
-                            password_manager_util::MAX_PASSWORD_STATUS);
-}
-
-}  // namespace
-
 DEFINE_WEB_CONTENTS_USER_DATA_KEY(ChromePasswordManagerClient);
 
 ChromePasswordManagerClient::ChromePasswordManagerClient(
@@ -54,12 +41,6 @@ ChromePasswordManagerClient::ChromePasswordManagerClient(
       driver_(web_contents, this),
       observer_(NULL),
       weak_factory_(this) {
-  // Avoid checking OS password until later on in browser startup
-  // since it calls a few Windows APIs.
-  base::MessageLoopProxy::current()->PostDelayedTask(
-      FROM_HERE,
-      base::Bind(&ReportOsPassword),
-      base::TimeDelta::FromSeconds(10));
 }
 
 ChromePasswordManagerClient::~ChromePasswordManagerClient() {}
