d0c8a605b99b61d2dd83df113c9172437c95be32
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-01 22:23:28 +0000

    telemetry: Fix EmptyTraceImporter
    The EmptyTraceImporter was actually broken by https://codereview.chromium.org/183683003/.
    This patches fixes it and improves test coverage.
    
    R=nduca@chromium.org, dtu@chromium.org, tonyg@chromium.org
    BUG=347802
    
    Review URL: https://codereview.chromium.org/184873007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254387 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/timeline/model_unittest.py
+++ tools/telemetry/telemetry/core/timeline/model_unittest.py
@@ -6,8 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import unittest
 
 from telemetry.core.timeline import model
+from telemetry.core.backends.chrome import tracing_timeline_data
 
 class TimelineModelUnittest(unittest.TestCase):
   def testEmptyImport(self):
-    model.TimelineModel([])
-    model.TimelineModel('')
+    model.TimelineModel(
+        tracing_timeline_data.TracingTimelineData([]))
+    model.TimelineModel(
+        tracing_timeline_data.TracingTimelineData(''))
