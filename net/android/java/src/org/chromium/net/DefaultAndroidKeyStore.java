e0f001b9a1894c496aa64221855c84b0deffc486
yang.gu@intel.com <yang.gu@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-02 20:21:12 +0000

    Fix build issues in net/ for Android x64
    BUG=346626
    
    Review URL: https://codereview.chromium.org/182933002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254412 0039d316-1c4b-4281-b951-d872f2087c98

--- net/android/java/src/org/chromium/net/DefaultAndroidKeyStore.java
+++ net/android/java/src/org/chromium/net/DefaultAndroidKeyStore.java
@@ -144,7 +144,7 @@ public class DefaultAndroidKeyStore implements AndroidKeyStore {
     }
 
     @Override
-    public int getOpenSSLHandleForPrivateKey(AndroidPrivateKey key) {
+    public long getOpenSSLHandleForPrivateKey(AndroidPrivateKey key) {
         PrivateKey javaKey = ((DefaultAndroidPrivateKey) key).getJavaKey();
         // Sanity checks
         if (javaKey == null) {
@@ -208,9 +208,9 @@ public class DefaultAndroidKeyStore implements AndroidKeyStore {
                 return 0;
             }
             getPkeyContext.setAccessible(true);
-            int evp_pkey = 0;
+            long evp_pkey = 0;
             try {
-                evp_pkey = (Integer) getPkeyContext.invoke(opensslKey);
+                evp_pkey = (Long) getPkeyContext.invoke(opensslKey);
             } finally {
                 getPkeyContext.setAccessible(false);
             }
