19536ec127b17d25e810d2efb6f25f5aa7a6b37a
ccameron@chromium.org <ccameron@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-02 00:37:27 +0000

    Try to diagnose crashes on 10.6
    Canary is crashing in some strange places on just 10.6. It looks like
    they are related to our (incredibly fragile) browser resize code, where
    we run the message loop inside the display callback. Temporarily don't
    do this on 10.6 to see if the crashes go away.
    
    TBR=kbr@chromium.org
    BUG=348328
    NOTRY=True
    
    Review URL: https://codereview.chromium.org/180273006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254391 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/compositing_iosurface_layer_mac.mm
+++ content/browser/renderer_host/compositing_iosurface_layer_mac.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <CoreFoundation/CoreFoundation.h>
 #include <OpenGL/gl.h>
 
+#include "base/mac/mac_util.h"
 #include "base/mac/sdk_forward_declarations.h"
 #include "content/browser/renderer_host/render_widget_host_impl.h"
 #include "content/browser/renderer_host/render_widget_host_view_mac.h"
@@ -92,6 +93,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)waitForResizedFrameInContext:(CGLContextObj)glContext {
+  // This appears to be causing crashes on 10.6. Temporarily disable the
+  // synchronized resize on 10.6 to verify that the crashes go away.
+  // TODO(ccameron): Remove this.
+  // http://crbug.com/348328
+  if (base::mac::IsOSSnowLeopard())
+    return;
+
   // Cache a copy of renderWidgetHostView_ because it may be reset if
   // a software frame is received in GetBackingStore.
   content::RenderWidgetHostViewMac* cached_view = renderWidgetHostView_;
