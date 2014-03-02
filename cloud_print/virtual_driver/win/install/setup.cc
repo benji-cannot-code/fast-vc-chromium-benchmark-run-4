738963ff2b372d36e0ef87aa89954f6568f86137
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-02 23:02:44 +0000

    Set system error code for Google Update.
    TBR=noamsml
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/183803007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254422 0039d316-1c4b-4281-b951-d872f2087c98

--- cloud_print/virtual_driver/win/install/setup.cc
+++ cloud_print/virtual_driver/win/install/setup.cc
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <comdef.h>
 #include <iomanip>
 #include <windows.h>
 #include <winspool.h>
@@ -272,8 +271,6 @@ HRESULT InstallDriver(const base::FilePath& install_path) {
   base::FilePath ui_help_path = temp_path.path().Append(kHelpName);
 
   if (!base::PathExists(xps_path)) {
-    SetGoogleUpdateError(kGoogleUpdateProductId,
-                         LoadLocalString(IDS_ERROR_NO_XPS));
     return HRESULT_FROM_WIN32(ERROR_BAD_DRIVER);
   }
 
@@ -538,17 +535,26 @@ int WINAPI WinMain(__in  HINSTANCE hInstance,
                    __in  HINSTANCE hPrevInstance,
                    __in  LPSTR lpCmdLine,
                    __in  int nCmdShow) {
+  using namespace cloud_print;
+
   base::AtExitManager at_exit_manager;
   CommandLine::Init(0, NULL);
-  HRESULT retval = cloud_print::ExecuteCommands();
 
-  VLOG(0) << _com_error(retval).ErrorMessage() << " HRESULT=0x" <<
-            std::setbase(16) << retval;
+  HRESULT retval = ExecuteCommands();
+
+  if (retval == HRESULT_FROM_WIN32(ERROR_BAD_DRIVER)) {
+    SetGoogleUpdateError(kGoogleUpdateProductId,
+                         LoadLocalString(IDS_ERROR_NO_XPS));
+  } else if (FAILED(retval)) {
+    SetGoogleUpdateError(kGoogleUpdateProductId, retval);
+  }
+
+  VLOG(0) << GetErrorMessage(retval)
+          << " HRESULT=0x" << std::setbase(16) << retval;
 
   // Installer is silent by default as required by Google Update.
   if (CommandLine::ForCurrentProcess()->HasSwitch("verbose")) {
-    cloud_print::DisplayWindowsMessage(NULL, retval,
-        cloud_print::LoadLocalString(IDS_DRIVER_NAME));
+    DisplayWindowsMessage(NULL, retval, LoadLocalString(IDS_DRIVER_NAME));
   }
   return retval;
 }
