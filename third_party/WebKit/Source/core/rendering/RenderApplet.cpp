9819f74e94ad05733aec2bc6dc3fb45de29debe2
mkwst@chromium.org <mkwst@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-03 07:43:03 +0000

    Add UseCounter for <applet> (a boy can dream, can't he?)
    R=jochen@chromium.org
    
    Review URL: https://codereview.chromium.org/184543002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168272 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderApplet.cpp
+++ third_party/WebKit/Source/core/rendering/RenderApplet.cpp
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "core/rendering/RenderApplet.h"
 
+#include "core/frame/UseCounter.h"
 #include "core/html/HTMLAppletElement.h"
 
 namespace WebCore {
@@ -31,6 +32,7 @@ RenderApplet::RenderApplet(HTMLAppletElement* applet)
     : RenderEmbeddedObject(applet)
 {
     setInline(true);
+    UseCounter::count(document(), UseCounter::HTMLAppletElement);
 }
 
 RenderApplet::~RenderApplet()
