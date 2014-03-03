097140bacc86339ce43bbec0fe3613378747394b
uekawa@chromium.org <uekawa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-03 08:08:06 +0000

    Rename IBusInputContextHandlerInterface to IMEInputContextHandlerInterface
    R=nona@chromium.org
    BUG=283612
    
    Review URL: https://codereview.chromium.org/184193004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254442 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/input_method/input_method_engine.cc
+++ chrome/browser/chromeos/input_method/input_method_engine.cc
@@ -47,7 +47,7 @@ namespace {
 void UpdateComposition(const CompositionText& composition_text,
                        uint32 cursor_pos,
                        bool is_visible) {
-  IBusInputContextHandlerInterface* input_context =
+  IMEInputContextHandlerInterface* input_context =
       IMEBridge::Get()->GetInputContextHandler();
   if (input_context)
     input_context->UpdateCompositionText(
@@ -423,7 +423,7 @@ bool InputMethodEngine::DeleteSurroundingText(int context_id,
 
   // TODO(nona): Return false if there is ongoing composition.
 
-  IBusInputContextHandlerInterface* input_context =
+  IMEInputContextHandlerInterface* input_context =
       IMEBridge::Get()->GetInputContextHandler();
   if (input_context)
     input_context->DeleteSurroundingText(offset, number_of_chars);
