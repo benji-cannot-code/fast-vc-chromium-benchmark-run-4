12189369ba0d2a818065b5ec8513e32e8b522368
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-03 15:51:33 +0000

    Fix a couple of header guards.
    BUG=none
    TEST=none
    R=darin@chromium.org
    TBR=darin@chromium.org
    
    Review URL: https://codereview.chromium.org/180633007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254467 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/web_ui_extension.h
+++ content/renderer/web_ui_extension.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_RENDERER_WEBUI_EXTENSION_H_
-#define CONTENT_RENDERER_WEBUI_EXTENSION_H_
+#ifndef CONTENT_RENDERER_WEB_UI_EXTENSION_H_
+#define CONTENT_RENDERER_WEB_UI_EXTENSION_H_
 
 #include <string>
 
@@ -33,4 +33,4 @@ class WebUIExtension {
 
 }  // namespace content
 
-#endif  // CONTENT_RENDERER_WEBUI_EXTENSION_H_
+#endif  // CONTENT_RENDERER_WEB_UI_EXTENSION_H_
--- content/renderer/web_ui_extension_data.h
+++ content/renderer/web_ui_extension_data.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_RENDERER_WEBUI_EXTENSION_DATA_H_
-#define CONTENT_RENDERER_WEBUI_EXTENSION_DATA_H_
+#ifndef CONTENT_RENDERER_WEB_UI_EXTENSION_DATA_H_
+#define CONTENT_RENDERER_WEB_UI_EXTENSION_DATA_H_
 
 #include <map>
 #include <string>
@@ -39,4 +39,4 @@ class WebUIExtensionData
 
 }  // namespace content
 
-#endif  // CONTENT_RENDERER_WEBUI_EXTENSION_DATA_H_
+#endif  // CONTENT_RENDERER_WEB_UI_EXTENSION_DATA_H_
