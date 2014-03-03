3497690e270cbe3f5bb3bf160f0ff2092241e2f7
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-03 08:09:11 +0000

    drive: Wire |has_more| parameter to DirectoryLoader
    Move ReadDirectory to DirectoryLoader.
    Remove FileSystem::LoadDirectoryIfNeeded.
    Later, DirectoryLoader::LoadDirectoryIfNeeded will be removed and ReadDirectoryCallback's has_more parameter will be used to return the directory contents progressively.
    
    BUG=340931
    TEST=unit_tests
    
    Review URL: https://codereview.chromium.org/179363011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254443 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/directory_loader.h
+++ chrome/browser/chromeos/drive/directory_loader.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_vector.h"
 #include "base/observer_list.h"
 #include "chrome/browser/chromeos/drive/file_errors.h"
+#include "chrome/browser/chromeos/drive/file_system_interface.h"
 #include "google_apis/drive/drive_common_callbacks.h"
 #include "google_apis/drive/gdata_errorcode.h"
 
@@ -60,15 +61,26 @@ class DirectoryLoader {
   void AddObserver(ChangeListLoaderObserver* observer);
   void RemoveObserver(ChangeListLoaderObserver* observer);
 
-  // Starts loading the directory contents if needed.
+  // Reads the directory contents.
   // |callback| must not be null.
-  void LoadDirectoryIfNeeded(const base::FilePath& directory_path,
-                             const FileOperationCallback& callback);
+  void ReadDirectory(const base::FilePath& directory_path,
+                     const ReadDirectoryCallback& callback);
 
  private:
   class FeedFetcher;
 
-  // Part of LoadDirectoryIfNeeded().
+  // Part of ReadDirectory().
+  void ReadDirectoryAfterLoad(const base::FilePath& directory_path,
+                              const ReadDirectoryCallback& callback,
+                              FileError error);
+  void ReadDirectoryAfterRead(const ReadDirectoryCallback& callback,
+                              scoped_ptr<ResourceEntryVector> entries,
+                              FileError error);
+
+  // Starts loading the directory contents if needed.
+  // |callback| must not be null.
+  void LoadDirectoryIfNeeded(const base::FilePath& directory_path,
+                             const FileOperationCallback& callback);
   void LoadDirectoryIfNeededAfterGetEntry(const base::FilePath& directory_path,
                                           const FileOperationCallback& callback,
                                           bool should_try_loading_parent,
