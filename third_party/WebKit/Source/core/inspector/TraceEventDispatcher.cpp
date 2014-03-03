a0ef5fc5203534e5d2e1ba010d3fa8445a8be329
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-03 06:16:32 +0000

    DevTools: specify trace event filter when installing trace callback
    BUG=
    R=pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/184043004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168265 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/TraceEventDispatcher.cpp
+++ third_party/WebKit/Source/core/inspector/TraceEventDispatcher.cpp
@@ -101,10 +101,12 @@ void TraceEventDispatcher::processBackgroundEvents()
 
 void TraceEventDispatcher::innerAddListener(const char* name, char phase, TraceEventTargetBase* instance, TraceEventHandlerMethod method, InspectorClient* client)
 {
+    static const char CategoryFilter[] = "devtools,webkit";
+
     ASSERT(isMainThread());
     MutexLocker locker(m_mutex);
     if (m_handlers.isEmpty())
-        client->setTraceEventCallback(dispatchEventOnAnyThread);
+        client->setTraceEventCallback(CategoryFilter, dispatchEventOnAnyThread);
     HandlersMap::iterator it = m_handlers.find(std::make_pair(name, phase));
     if (it == m_handlers.end())
         m_handlers.add(std::make_pair(name, phase), Vector<BoundTraceEventHandler>()).storedValue->value.append(BoundTraceEventHandler(instance, method));
@@ -134,7 +136,7 @@ void TraceEventDispatcher::removeAllListeners(TraceEventTargetBase* instance, In
         m_handlers.swap(remainingHandlers);
     }
     if (m_handlers.isEmpty())
-        client->setTraceEventCallback(0);
+        client->resetTraceEventCallback();
 }
 
 size_t TraceEventDispatcher::TraceEvent::findParameter(const char* name) const
