1c1dccc55d67f13585fa65d1e0d4e6dd52266abe
Tomasz Mikolajewski <mtomasz@chromium.org>
2014-03-03 13:02:24 +0900

    Speed up the multi shot to 500 ms between pictures.
    Before, it was 1s between pictures, which was reported as too slow. This patch
    changes the interval to 0.5s.
    
    TEST=Tested manually.
    BUG=chrome-os-partner:26262
    
    Change-Id: I211c499d7979d51813b7987a267f586c3089c95c
    Reviewed-on: https://chromium-review.googlesource.com/188486
    Reviewed-by: Daichi Hirono <hirono@chromium.org>
    Tested-by: Tomasz Mikolajewski <mtomasz@chromium.org>

--- src/js/views/camera.js
+++ src/js/views/camera.js
@@ -1094,7 +1094,7 @@ camera.views.Camera.prototype.takePicture_ = function() {
             // Reset the controls.
             this.resetTakePicture_();
           } else {
-            setTimeout(takePicture, 500);
+            setTimeout(takePicture, 250);
           }
         }.bind(this));
       }.bind(this);
