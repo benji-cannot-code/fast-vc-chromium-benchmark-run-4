bfec1fa38120aed6a747d36dac8553de07b8b5b2
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-03 03:45:44 +0000

    Simplify the way to initialize context
    (Background: This is a preparation CL for making DOMWrapperWorld::current() callable from anywhere.)
    
    Currently, when we initialize a context, we need to call the following things in a proper order.
    
    - V8PerContextDataHolder::install()
    - V8PerContextData::create()
    - V8PerContextData::init()
    - gin::ContextHolder()
    - gin::ContextHolder::SetContext()
    
    However, this is more complicated than necessary. This CL refactors code so that we just need to call V8PerContextData::create() when initializing a context.
    
    Specifically, this CL does the following refactoring:
    
    - Move gin::ContextHolder from V8WindowShell/WorkerScriptController to V8PerContextData.
    - Remove V8_STORE_PRIMORDIAL macro.
    - Move the contents of V8PerContextData::init() to V8PerContextData's constructor.
    
    BUG=341032
    
    Review URL: https://codereview.chromium.org/180773003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168260 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/V8WindowShell.h
+++ third_party/WebKit/Source/bindings/v8/V8WindowShell.h
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/v8/ScopedPersistent.h"
 #include "bindings/v8/V8PerContextData.h"
 #include "bindings/v8/WrapperTypeInfo.h"
-#include "gin/public/context_holder.h"
 #include "platform/weborigin/SecurityOrigin.h"
 #include "wtf/Forward.h"
 #include "wtf/HashMap.h"
@@ -59,7 +58,7 @@ class V8WindowShell {
 public:
     static PassOwnPtr<V8WindowShell> create(LocalFrame*, PassRefPtr<DOMWrapperWorld>, v8::Isolate*);
 
-    v8::Local<v8::Context> context() const { return m_contextHolder ? m_contextHolder->context() : v8::Local<v8::Context>(); }
+    v8::Local<v8::Context> context() const { return m_perContextData ? m_perContextData->context() : v8::Local<v8::Context>(); }
 
     // Update document object of the frame.
     void updateDocument();
@@ -71,7 +70,7 @@ public:
     // (e.g., after setting docoument.domain).
     void updateSecurityOrigin(SecurityOrigin*);
 
-    bool isContextInitialized() { return m_contextHolder; }
+    bool isContextInitialized() { return m_perContextData; }
     bool isGlobalInitialized() { return !m_global.isEmpty(); }
 
     bool initializeIfNeeded();
@@ -110,10 +109,7 @@ private:
     LocalFrame* m_frame;
     RefPtr<DOMWrapperWorld> m_world;
     v8::Isolate* m_isolate;
-
     OwnPtr<V8PerContextData> m_perContextData;
-
-    OwnPtr<gin::ContextHolder> m_contextHolder;
     ScopedPersistent<v8::Object> m_global;
     ScopedPersistent<v8::Object> m_document;
 };
