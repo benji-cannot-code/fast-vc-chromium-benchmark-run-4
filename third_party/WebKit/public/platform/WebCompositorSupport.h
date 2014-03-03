ca7bb24bd05b70f14b1daffa4fec53fe54154b18
cbiesinger@chromium.org <cbiesinger@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-03 16:38:37 +0000

    Revert 168245 "Drop background color optimization for composited..."
    Suspected of causing lots of browser_test failures on Mac:
    http://build.chromium.org/p/chromium.webkit/builders/Mac10.6%20Tests/builds/23338
    
    > Drop background color optimization for composited layers
    > 
    > Rationale
    > 1. GraphicsLayer deals with background-color layer as a contents layer, so a
    > background-color layer is in trouble in terms of the correct order with negative
    > z-order children.
    > 2. CompositedLayerMapping has the big hack to decide the given layer can be
    > background-color layer. Unfortunately, the big hack is incomplete and invasive.
    > 3. Hybrid Accelerated Rasterization will cover this optimization in more generic way.
    > 4. background-color layer covers very limited cases. A layer-promoted box
    > object must not have children, not have decoration, only have a background-color
    > property.
    > 
    > BUG=114658, 340691
    > TEST=compositing/background-color/background-color-drawn-over-child.html,
    > compositing/*
    > 
    > Review URL: https://codereview.chromium.org/178013003
    
    TBR=dongseong.hwang@intel.com
    
    Review URL: https://codereview.chromium.org/183763024
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168317 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebCompositorSupport.h
+++ third_party/WebKit/public/platform/WebCompositorSupport.h
@@ -75,7 +75,6 @@ public:
 
     virtual WebNinePatchLayer* createNinePatchLayer() { return 0; }
 
-    // TODO(dshwang): This isn't used anymore.
     virtual WebSolidColorLayer* createSolidColorLayer() { return 0; }
 
     // The ownership of the WebScrollbarThemeGeometry pointer is passed to Chromium.
