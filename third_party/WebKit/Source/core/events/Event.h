66b51e7001b72bb5470689f4bf3a438d017bf840
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-04 03:10:15 +0000

    Revert Event back (from RefCountedWillBeRefCountedGarbageCollected) to RefCounted
    Moving Event to oilpan's heap caused a lot of crashes/failures. So this CL reverts Event back to RefCounted.
    
    This CL is a revert for https://codereview.chromium.org/182063004/. However, this CL does not revert trace() methods in the original CL because there is no harm in having trace() methods in the code base. (If I revert trace() methods, I have to resolve a bunch of conflicts manually, which will be more dangerous than leaving trace() methods in the code base.)
    
    BUG=340522
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/185463005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168354 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/events/Event.h
+++ third_party/WebKit/Source/core/events/Event.h
@@ -45,7 +45,7 @@ struct EventInit {
     bool cancelable;
 };
 
-class Event : public RefCountedWillBeRefCountedGarbageCollected<Event>,  public ScriptWrappable {
+class Event : public RefCounted<Event>,  public ScriptWrappable {
 public:
     enum PhaseType {
         NONE                = 0,
