1d9af200196618525901b93a429b536a104789ef
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-04 19:37:05 +0000

    CPUFlameChart: remove unnecessary arrays and calculate colors on demand.
    BUG=
    R=pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/184303005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168402 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/CPUProfileView.js
+++ third_party/WebKit/Source/devtools/front_end/CPUProfileView.js
@@ -1132,15 +1132,13 @@ WebInspector.CPUFlameChartDataProvider.prototype = {
 
         /**
          * @constructor
-         * @param {string} color
          * @param {number} depth
          * @param {number} duration
          * @param {number} startTime
          * @param {!Object} node
          */
-        function ChartEntry(color, depth, duration, startTime, node)
+        function ChartEntry(depth, duration, startTime, node)
         {
-            this.color = color;
             this.depth = depth;
             this.duration = duration;
             this.startTime = startTime;
@@ -1193,8 +1191,7 @@ WebInspector.CPUFlameChartDataProvider.prototype = {
             var colorGenerator = this._colorGenerator;
             var color = "";
             while (node) {
-                color = colorGenerator.colorForID(node.functionName + ":" + node.url + ":" + node.lineNumber);
-                entries.push(new ChartEntry(color, depth, samplingInterval, sampleIndex * samplingInterval, node));
+                entries.push(new ChartEntry(depth, samplingInterval, sampleIndex * samplingInterval, node));
                 openIntervals.push({node: node, index: index});
                 ++index;
 
@@ -1204,25 +1201,19 @@ WebInspector.CPUFlameChartDataProvider.prototype = {
             entries[entries.length - 1].selfTime += samplingInterval;
         }
 
-        var entryColors = new Array(entries.length);
         var entryNodes = new Array(entries.length);
         var entryLevels = new Uint8Array(entries.length);
         var entryTotalTimes = new Float32Array(entries.length);
         var entrySelfTimes = new Float32Array(entries.length);
         var entryOffsets = new Float32Array(entries.length);
-        var entryTitles = new Array(entries.length);
-        var entryDeoptFlags = new Uint8Array(entries.length);
 
         for (var i = 0; i < entries.length; ++i) {
             var entry = entries[i];
             entryNodes[i] = entry.node;
-            entryColors[i] = entry.color;
             entryLevels[i] = entry.depth;
             entryTotalTimes[i] = entry.duration;
             entryOffsets[i] = entry.startTime;
-            entryTitles[i] = entry.node.functionName;
-            var reason = entry.node.deoptReason;
-            entryDeoptFlags[i] = (reason && reason !== "no reason");
+            entrySelfTimes[i] = entry.selfTime;
         }
 
         this._maxStackDepth = Math.max(maxDepth, depth);
@@ -1233,11 +1224,8 @@ WebInspector.CPUFlameChartDataProvider.prototype = {
             entryOffsets: entryOffsets,
         };
 
-        this._entryTitles = entryTitles;
         this._entryNodes = entryNodes;
         this._entrySelfTimes = entrySelfTimes;
-        this._entryDeoptFlags = entryDeoptFlags;
-        this._entryColors = entryColors;
 
         return /** @type {!WebInspector.FlameChart.TimelineData} */ (this._timelineData);
     },
@@ -1275,7 +1263,7 @@ WebInspector.CPUFlameChartDataProvider.prototype = {
             entryInfo.push(row);
         }
 
-        pushEntryInfoRow(WebInspector.UIString("Name"), this._entryTitles[entryIndex]);
+        pushEntryInfoRow(WebInspector.UIString("Name"), node.functionName);
         var selfTime = this._millisecondsToString(this._entrySelfTimes[entryIndex]);
         var totalTime = this._millisecondsToString(timelineData.entryTotalTimes[entryIndex]);
         pushEntryInfoRow(WebInspector.UIString("Self time"), selfTime);
@@ -1305,7 +1293,8 @@ WebInspector.CPUFlameChartDataProvider.prototype = {
      */
     entryTitle: function(entryIndex)
     {
-        return this._entryTitles[entryIndex];
+        var node = this._entryNodes[entryIndex];
+        return node.functionName;
     },
 
     /**
@@ -1318,7 +1307,9 @@ WebInspector.CPUFlameChartDataProvider.prototype = {
             this._font = (this.barHeight() - 4) + "px " + WebInspector.fontFamily();
             this._boldFont = "bold " + this._font;
         }
-        return this._entryDeoptFlags[entryIndex] ? this._boldFont : this._font;
+        var node = this._entryNodes[entryIndex];
+        var reason = node.deoptReason;
+        return (reason && reason !== "no reason") ? this._boldFont : this._font;
     },
 
     /**
@@ -1336,6 +1327,7 @@ WebInspector.CPUFlameChartDataProvider.prototype = {
      */
     entryColor: function(entryIndex)
     {
-        return this._entryColors[entryIndex];
+        var node = this._entryNodes[entryIndex];
+        return this._colorGenerator.colorForID(node.functionName + ":" + node.url + ":" + node.lineNumber);;
     },
 }
