067af1d018ac73792984623611c8e33e391a69b6
leng@chromium.org <leng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-04 01:51:02 +0000

    Adds classes to implement the permissions bubble on the Mac.
    PermissionBubbleCocoa is the Mac implementation of the platform-
    independent PermissionBubbleView.  It creates the Cocoa implementation
    when requested.
    
    PermissionBubbleController is the Cocoa implementation for the
    PermissionBubbleCocoa.  It is a UIViewController which displays the bubble,
    and closes itself.
    
    The PermissionBubbleCocoa object is owned by the BrowserWindowController,
    and provided to the current PermissionBubbleManager (tied to WebContents). 
    The PermissionBubbleManager then uses the implementation of
    PermissionBubbleView to display the UI when required.
    
    Also adds MockPermissionBubbleDelegate for testing, and tests for
    PermissionBubbleController.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/151593005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254635 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/ui/website_settings/mock_permission_bubble_request.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/website_settings/mock_permission_bubble_request.h"
+
+#include "base/strings/utf_string_conversions.h"
+
+MockPermissionBubbleRequest::MockPermissionBubbleRequest(
+    const std::string& text) {
+  SetText(base::UTF8ToUTF16(text));
+}
+
+MockPermissionBubbleRequest::~MockPermissionBubbleRequest() {}
+
+void MockPermissionBubbleRequest::SetText(const base::string16& text) {
+  ON_CALL(*this, GetMessageText()).WillByDefault(testing::Return(text));
+  ON_CALL(*this, GetMessageTextFragment()).WillByDefault(testing::Return(text));
+}
