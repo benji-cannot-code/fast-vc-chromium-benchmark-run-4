3683b0ac638e908d54d2fe250f3d9074e4a96828
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-04 03:30:38 +0000

    Revert 254629 "gpu: Allow overriding GPU driver workarounds via ..."
    Adds many static initializers to gpu_driver_bug_list.cc:
    
    http://build.chromium.org/p/chromium/builders/Linux%20x64/builds/62225/steps/sizes/logs/stdio
    # gpu_driver_bug_list.cc gpu::(anonymous namespace)::kFeatureList
    # gpu_driver_bug_list.cc gpu::(anonymous namespace)::kFeatureList+0x10
    # gpu_driver_bug_list.cc gpu::(anonymous namespace)::kFeatureList+0x100
    # gpu_driver_bug_list.cc gpu::(anonymous namespace)::kFeatureList+0x108
    ...
    # gpu_driver_bug_list.cc gpu::(anonymous namespace)::kFeatureList+0xf8
    # gpu_driver_bug_list.cc std::basic_string<char, std::char_traits<char>, std::allocator<char> >::basic_string(char const*, std::allocator<char> const&)@plt
    # gpu_driver_bug_list.cc __cxa_atexit@plt [registers a dtor to run at exit]
    # gpu_driver_bug_list.cc __init_array_end+0x5e0
    # gpu_driver_bug_list.cc __tcf_0
    
    > gpu: Allow overriding GPU driver workarounds via command line
    > 
    > This change allows you to enable GPU driver workarounds by providing command
    > line switches with the name of the workaround. These workarounds are used
    > instead of the workarounds loaded from the JSON data. The command line switch
    > will be propagated into the GPU process allowing these to be applied both with
    > in-process GPU and without.
    > 
    > BUG=347130
    > TEST=run content_gl_test or chromium with and without switches and observe
    > workarounds being applied.
    > 
    > Review URL: https://codereview.chromium.org/180243012
    
    TBR=robert.bradford@intel.com
    
    Review URL: https://codereview.chromium.org/183003010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254640 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/config/gpu_driver_bug_list.cc
+++ gpu/config/gpu_driver_bug_list.cc
@@ -6,10 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/config/gpu_driver_bug_list.h"
 
 #include "base/basictypes.h"
-#include "base/command_line.h"
 #include "base/logging.h"
 #include "gpu/config/gpu_driver_bug_workaround_type.h"
-#include "gpu/config/gpu_util.h"
 
 namespace gpu {
 
@@ -18,12 +16,6 @@ namespace {
 struct DriverBugInfo {
   int feature_type;
   std::string feature_name;
-};
-
-const DriverBugInfo kFeatureList[] = {
-#define GPU_OP(type, name) { type, #name },
-  GPU_DRIVER_BUG_WORKAROUNDS(GPU_OP)
-#undef GPU_OP
 };
 
 }  // namespace anonymous
@@ -39,6 +31,11 @@ GpuDriverBugList::~GpuDriverBugList() {
 GpuDriverBugList* GpuDriverBugList::Create() {
   GpuDriverBugList* list = new GpuDriverBugList();
 
+  const DriverBugInfo kFeatureList[] = {
+#define GPU_OP(type, name) { type, #name },
+    GPU_DRIVER_BUG_WORKAROUNDS(GPU_OP)
+#undef GPU_OP
+  };
   DCHECK_EQ(static_cast<int>(arraysize(kFeatureList)),
             NUMBER_OF_GPU_DRIVER_BUG_WORKAROUND_TYPES);
   for (int i = 0; i < NUMBER_OF_GPU_DRIVER_BUG_WORKAROUND_TYPES; ++i) {
@@ -57,17 +54,6 @@ std::string GpuDriverBugWorkaroundTypeToString(
     default:
       return "unknown";
   };
-}
-
-std::set<int> WorkaroundsFromCommandLine(CommandLine* command_line) {
-  std::set<int> workarounds;
-
-  for (int i = 0; i < NUMBER_OF_GPU_DRIVER_BUG_WORKAROUND_TYPES; i++) {
-    if (command_line->HasSwitch(kFeatureList[i].feature_name))
-      workarounds.insert(kFeatureList[i].feature_type);
-  }
-
-  return workarounds;
 }
 
 }  // namespace gpu
