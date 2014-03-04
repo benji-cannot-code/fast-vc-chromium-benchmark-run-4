8a614dbe6f0b6124816637432e45afdbbe3ed696
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-04 22:32:39 +0000

    options: fix advanced settings regression.
    R=ajuma@chromium.org
    BUG=348876
    
    Review URL: https://codereview.chromium.org/185193004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254857 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options/browser_options_browsertest.js
+++ chrome/browser/ui/webui/options/browser_options_browsertest.js
@@ -7,15 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * TestFixture for browser options WebUI testing.
  * @extends {testing.Test}
  * @constructor
- **/
+ */
 function BrowserOptionsWebUITest() {}
 
 BrowserOptionsWebUITest.prototype = {
   __proto__: testing.Test.prototype,
 
-  /**
-   * Browse to browser options.
-   **/
+  /** @override */
   browsePreload: 'chrome://chrome/settings/',
 };
 
@@ -29,4 +27,43 @@ GEN('#define MAYBE_testOpenBrowserOptions testOpenBrowserOptions');
 GEN('#endif  // defined(OS_MACOSX)');
 TEST_F('BrowserOptionsWebUITest', 'MAYBE_testOpenBrowserOptions', function() {
   assertEquals(this.browsePreload, document.location.href);
+});
+
+
+/**
+ * @extends {testing.Test}
+ * @constructor
+ */
+function BrowserOptionsFrameWebUITest() {}
+
+BrowserOptionsFrameWebUITest.prototype = {
+  __proto__: testing.Test.prototype,
+
+  /** @override */
+  browsePreload: 'chrome://settings-frame/settings',
+};
+
+TEST_F('BrowserOptionsFrameWebUITest', 'testAdvancedSettingsHiddenByDefault',
+    function() {
+  assertEquals(this.browsePreload, document.location.href);
+  expectTrue($('advanced-settings').hidden);
+});
+
+
+/**
+ * @extends {testing.Test}
+ * @constructor
+ */
+function AdvancedSettingsWebUITest() {}
+
+AdvancedSettingsWebUITest.prototype = {
+  __proto__: testing.Test.prototype,
+
+  /** @override */
+  browsePreload: 'chrome://settings-frame/autofill',
+};
+
+TEST_F('AdvancedSettingsWebUITest', 'testAdvancedSettingsShown', function() {
+  assertEquals(this.browsePreload, document.location.href);
+  expectFalse($('advanced-settings').hidden);
 });
