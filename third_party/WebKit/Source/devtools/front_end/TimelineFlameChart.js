d966007403dcd83c36a8ecb15b7ce02d410ba0d4
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-04 05:10:09 +0000

    FlameChart: convert colors magic into dataProvider.entryColor getter
    BUG=
    R=pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/185533004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168361 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/TimelineFlameChart.js
+++ third_party/WebKit/Source/devtools/front_end/TimelineFlameChart.js
@@ -129,7 +129,7 @@ WebInspector.TimelineFlameChartDataProvider.prototype = {
             this._model.forAllRecords(this._appendRecord.bind(this));
             this._zeroTime = this._model.minimumRecordTime();
         }
-        return this._timelineData;
+        return /** @type {!WebInspector.FlameChart.TimelineData} */(this._timelineData);
     },
 
     /**
@@ -164,13 +164,20 @@ WebInspector.TimelineFlameChartDataProvider.prototype = {
         this._totalTime = 1000;
         this._zeroTime = 0;
 
+        /**
+         * @type {?WebInspector.FlameChart.TimelineData}
+         */
         this._timelineData = {
             entryLevels: [],
             entryTotalTimes: [],
             entryOffsets: [],
-            colorEntryIndexes: []
         };
-        this._entryTitles = [];
+
+        /** @type {!Array.<string>} */
+        this._entryTitles =  [];
+
+        /** @type {!Array.<string>} */
+        this._entryColors = [];
     },
 
     /**
@@ -200,13 +207,7 @@ WebInspector.TimelineFlameChartDataProvider.prototype = {
         timelineData.entryOffsets[index] = record.startTime - startTime;
         timelineData.entryLevels[index] = depth - 1;
         timelineData.entryTotalTimes[index] = endTime - record.startTime;
-
-        var color = this._colorGenerator.colorForID(WebInspector.TimelineUIUtils.categoryForRecord(record).name);
-        var indexesForColor = timelineData.colorEntryIndexes[color.index];
-        if (!indexesForColor)
-            indexesForColor = timelineData.colorEntryIndexes[color.index] = [];
-        indexesForColor.push(index);
-
+        this._entryColors[index] = this._colorGenerator.colorForID(WebInspector.TimelineUIUtils.categoryForRecord(record).name);
         this._maxStackDepth = Math.max(this._maxStackDepth, depth + 1);
     },
 
@@ -235,6 +236,15 @@ WebInspector.TimelineFlameChartDataProvider.prototype = {
     entryData: function(entryIndex)
     {
         return null;
+    },
+
+    /**
+     * @param {number} entryIndex
+     * @return {!string}
+     */
+    entryColor: function(entryIndex)
+    {
+        return this._entryColors[entryIndex];
     }
 }
 
