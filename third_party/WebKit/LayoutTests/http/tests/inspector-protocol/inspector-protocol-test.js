5a4d90cabe7028881189371c0a6147e74a3f6535
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-04 08:10:47 +0000

    DevTools: Unify protocol test scripts naming with frontend tests.
    R=lushnikov@chromium.org, lushnikov, pfeldman
    
    Review URL: https://codereview.chromium.org/183663014
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168370 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/inspector-protocol/inspector-protocol-test.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector-protocol/inspector-protocol-test.js
@@ -24,6 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
+var initialize_InspectorTest = function() {
+
 InspectorFrontendAPI = {};
 
 InspectorTest = {};
@@ -166,7 +169,7 @@ InspectorTest.logObject = function(object, title)
 
 /**
 * Logs message directly to process stdout via alert function (hopefully followed by flush call).
-* This message should survive process crash or kill by timeout. 
+* This message should survive process crash or kill by timeout.
 * @param {string} message
 */
 InspectorTest.debugLog = function(message)
@@ -229,12 +232,94 @@ InspectorTest.importScript = function(scriptName)
     window.eval(xhr.responseText + "\n//@ sourceURL=" + scriptName);
 }
 
-window.addEventListener("message", function(event) {
-    try {
-        eval(event.data);
-    } catch (e) {
-        alert(e.stack);
-        InspectorTest.completeTest();
-        throw e;
+};
+
+var outputElement;
+
+/**
+ * Logs message to process stdout via alert (hopefully implemented with immediate flush).
+ * @param {string} text
+ */
+function debugLog(text)
+{
+    alert(text);
+}
+
+/**
+ * @param {string} text
+ */
+function log(text)
+{
+    if (!outputElement) {
+        var intermediate = document.createElement("div");
+        document.body.appendChild(intermediate);
+
+        var intermediate2 = document.createElement("div");
+        intermediate.appendChild(intermediate2);
+
+        outputElement = document.createElement("div");
+        outputElement.className = "output";
+        outputElement.id = "output";
+        outputElement.style.whiteSpace = "pre";
+        intermediate2.appendChild(outputElement);
     }
-});
+    outputElement.appendChild(document.createTextNode(text));
+    outputElement.appendChild(document.createElement("br"));
+}
+
+function closeTest()
+{
+    closeInspector();
+    testRunner.notifyDone();
+}
+
+function runTest()
+{
+    if (!window.testRunner) {
+        console.error("This test requires DumpRenderTree");
+        return;
+    }
+    testRunner.dumpAsText();
+    testRunner.waitUntilDone();
+    testRunner.setCanOpenWindows(true);
+
+    openInspector();
+}
+
+function closeInspector()
+{
+    window.internals.closeDummyInspectorFrontend();
+}
+
+function openInspector()
+{
+    var scriptTags = document.getElementsByTagName("script");
+    var scriptUrlBasePath = "";
+    for (var i = 0; i < scriptTags.length; ++i) {
+        var index = scriptTags[i].src.lastIndexOf("/inspector-protocol-test.js");
+        if (index > -1 ) {
+            scriptUrlBasePath = scriptTags[i].src.slice(0, index);
+            break;
+        }
+    }
+
+    var dummyFrontendURL = scriptUrlBasePath + "/resources/protocol-test.html";
+    var inspectorFrontend = window.internals.openDummyInspectorFrontend(dummyFrontendURL);
+    inspectorFrontend.addEventListener("load", function(event) {
+        // FIXME: rename this 'test' global field across all tests.
+        var testFunction = window.test;
+        if (typeof testFunction === "function") {
+            var initializers = "";
+            for (var symbol in window) {
+                if (!/^initialize_/.test(symbol) || typeof window[symbol] !== "function")
+                    continue;
+                initializers += "(" + window[symbol].toString() + ")();\n";
+            }
+            inspectorFrontend.postMessage(initializers + "(" + testFunction.toString() +")();", "*");
+            return;
+        }
+        // Kill waiting process if failed to send.
+        alert("Failed to send test function");
+        testRunner.notifyDone();
+    });
+}
