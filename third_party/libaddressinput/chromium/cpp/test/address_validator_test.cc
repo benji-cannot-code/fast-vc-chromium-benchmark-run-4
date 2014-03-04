10d3ca99b99ee3ce6c9ae35a85be841167fb8ca7
scr@chromium.org <scr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-04 17:52:01 +0000

    Changes to verify all addresses in examples from countryinfo.txt.
    BUG=
    
    Review URL: https://codereview.chromium.org/181593007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254781 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/libaddressinput/chromium/cpp/test/address_validator_test.cc
+++ third_party/libaddressinput/chromium/cpp/test/address_validator_test.cc
@@ -13,11 +13,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <libaddressinput/address_validator.h>
+#include "address_validator_test.h"
 
 #include <libaddressinput/address_data.h>
 #include <libaddressinput/address_field.h>
 #include <libaddressinput/address_problem.h>
+#include <libaddressinput/address_validator.h>
 #include <libaddressinput/downloader.h>
 #include <libaddressinput/load_rules_delegate.h>
 #include <libaddressinput/storage.h>
@@ -36,12 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace i18n {
 namespace addressinput {
-
-scoped_ptr<AddressValidator> BuildAddressValidatorForTesting(
-    const std::string& validation_data_url,
-    scoped_ptr<Downloader> downloader,
-    scoped_ptr<Storage> storage,
-    LoadRulesDelegate* load_rules_delegate);
 
 class AddressValidatorTest : public testing::TestWithParam<std::string>,
                              public LoadRulesDelegate {
--- third_party/libaddressinput/libaddressinput.gyp
+++ third_party/libaddressinput/libaddressinput.gyp
@@ -135,6 +135,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         '<(libaddressinput_dir)/cpp/test/address_ui_test.cc',
         '<(libaddressinput_dir)/cpp/test/address_validator_test.cc',
         '<(libaddressinput_dir)/cpp/test/country_rules_aggregator_test.cc',
+        '<(libaddressinput_dir)/cpp/test/countryinfo_example_addresses_test.cc',
         '<(libaddressinput_dir)/cpp/test/fake_downloader.cc',
         '<(libaddressinput_dir)/cpp/test/fake_downloader.h',
         '<(libaddressinput_dir)/cpp/test/fake_downloader_test.cc',
