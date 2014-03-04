67e0f92f626d05cd4ab7c7cc4b8ebbfed96a28c6
blundell@chromium.org <blundell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-04 11:54:10 +0000

    Move AppsListViewDelegate away from listening to Signin notifications.
    As part of eliminating Signin notifications in favor of observing the
    SigninManager, this CL moves AppsListViewDelegate from listening to Signin
    notifications to observing SigninManagers. As AppsListViewDelegate needs to get
    information on signin-related events from all SigninManagers,
    AppsListViewDelegate also observes SigninManager creation and shutdown via the
    SigninManagerFactory (following the pattern established by
    https://codereview.chromium.org/132743004/).
    
    BUG=333997
    
    Review URL: https://codereview.chromium.org/169393003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254747 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/app_list/app_list_view_delegate.h
+++ chrome/browser/ui/app_list/app_list_view_delegate.h
@@ -13,11 +13,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/observer_list.h"
+#include "base/scoped_observer.h"
 #include "chrome/browser/profiles/profile_info_cache_observer.h"
+#include "chrome/browser/signin/signin_manager_base.h"
+#include "chrome/browser/signin/signin_manager_factory.h"
 #include "chrome/browser/ui/app_list/chrome_signin_delegate.h"
 #include "chrome/browser/ui/app_list/start_page_observer.h"
-#include "content/public/browser/notification_observer.h"
-#include "content/public/browser/notification_registrar.h"
 #include "ui/app_list/app_list_view_delegate.h"
 
 class AppListControllerDelegate;
@@ -32,11 +33,6 @@ namespace base {
 class FilePath;
 }
 
-namespace content {
-class NotificationDetails;
-class NotificationSource;
-}
-
 namespace gfx {
 class ImageSkia;
 }
@@ -47,16 +43,15 @@ class AppSyncUIStateWatcher;
 
 class AppListViewDelegate : public app_list::AppListViewDelegate,
                             public app_list::StartPageObserver,
-                            public content::NotificationObserver,
-                            public ProfileInfoCacheObserver {
+                            public ProfileInfoCacheObserver,
+                            public SigninManagerBase::Observer,
+                            public SigninManagerFactory::Observer {
  public:
   AppListViewDelegate(Profile* profile,
                       AppListControllerDelegate* controller);
   virtual ~AppListViewDelegate();
 
  private:
-  // Registers the current profile for notifications.
-  void RegisterForNotifications();
   // Updates the app list's current profile and ProfileMenuItems.
   void OnProfileChanged();
 
@@ -104,10 +99,15 @@ class AppListViewDelegate : public app_list::AppListViewDelegate,
   virtual void OnSpeechRecognitionStateChanged(
       app_list::SpeechRecognitionState new_state) OVERRIDE;
 
-  // Overridden from content::NotificationObserver:
-  virtual void Observe(int type,
-                       const content::NotificationSource& source,
-                       const content::NotificationDetails& details) OVERRIDE;
+  // Overridden from SigninManagerFactory::Observer:
+  virtual void SigninManagerCreated(SigninManagerBase* manager) OVERRIDE;
+  virtual void SigninManagerShutdown(SigninManagerBase* manager) OVERRIDE;
+
+  // Overridden from SigninManagerBase::Observer:
+  virtual void GoogleSigninFailed(const GoogleServiceAuthError& error) OVERRIDE;
+  virtual void GoogleSigninSucceeded(const std::string& username,
+                                     const std::string& password) OVERRIDE;
+  virtual void GoogleSignedOut(const std::string& username) OVERRIDE;
 
   // Overridden from ProfileInfoCacheObserver:
   virtual void OnProfileAdded(const base::FilePath& profile_path) OVERRIDE;
@@ -133,13 +133,16 @@ class AppListViewDelegate : public app_list::AppListViewDelegate,
 
   Users users_;
 
-  content::NotificationRegistrar registrar_;
   ChromeSigninDelegate signin_delegate_;
 #if defined(USE_ASH)
   scoped_ptr<AppSyncUIStateWatcher> app_sync_ui_state_watcher_;
 #endif
 
   ObserverList<app_list::AppListViewDelegateObserver> observers_;
+
+  // Used to track the SigninManagers that this instance is observing so that
+  // this instance can be removed as an observer on its destruction.
+  ScopedObserver<SigninManagerBase, AppListViewDelegate> scoped_observer_;
 
   DISALLOW_COPY_AND_ASSIGN(AppListViewDelegate);
 };
