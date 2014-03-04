0a1998c9ff7688c513268df43dec49aa2610a4d0
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-04 23:48:05 +0000

    Don't setStyle the RenderView every style recalc
    Prior to this CL, we were setting the style of the RenderView on every style
    recalc because we were comparing the RenderView's overflow style with the wrong
    value. This CL corrects the bug.
    
    This CL doesn't have much effect yet, but it will become important in a future
    CL when we use style modification on RenderLayers to scope compositing updates.
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=168254
    
    Review URL: https://codereview.chromium.org/185263002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168412 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebViewImpl.cpp
+++ third_party/WebKit/Source/web/WebViewImpl.cpp
@@ -3367,6 +3367,8 @@ bool WebViewImpl::isTransparent() const
 
 void WebViewImpl::setBaseBackgroundColor(WebColor color)
 {
+    layout();
+
     if (m_baseBackgroundColor == color)
         return;
 
