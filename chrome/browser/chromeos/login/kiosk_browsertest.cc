cc0a9dedaa11dd320794119a89988379b7a31a98
gab@chromium.org <gab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-04 21:50:44 +0000

    Do not hardcode the Preferences filename in kiosk_browsertest.
    BUG=343692
    
    Review URL: https://codereview.chromium.org/185543007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254835 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/kiosk_browsertest.cc
+++ chrome/browser/chromeos/login/kiosk_browsertest.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/settings/device_oauth2_token_service_factory.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/extension_test_message_listener.h"
+#include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
@@ -473,8 +474,9 @@ class KioskTest : public OobeBaseTest {
     base::FilePath test_data_dir;
     ASSERT_TRUE(PathService::Get(chrome::DIR_TEST_DATA, &test_data_dir));
     test_data_dir = test_data_dir.AppendASCII(relative_app_profile_dir);
-    ASSERT_TRUE(base::CopyFile(test_data_dir.AppendASCII("Preferences"),
-                               app_profile_dir.AppendASCII("Preferences")));
+    ASSERT_TRUE(
+        base::CopyFile(test_data_dir.Append(chrome::kPreferencesFilename),
+                       app_profile_dir.Append(chrome::kPreferencesFilename)));
     ASSERT_TRUE(
         base::CopyDirectory(test_data_dir.AppendASCII("Extensions"),
                             app_profile_dir,
