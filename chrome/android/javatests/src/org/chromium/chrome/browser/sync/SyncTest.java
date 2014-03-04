5f1c32ba76ccfd1de754ca3b752bf898bd16e1e0
ricow@chromium.org <ricow@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-04 12:11:44 +0000

    Revert 254746 "Disable HostDriven_SyncTest.testDisableAndEnableSync"
    > Disable HostDriven_SyncTest.testDisableAndEnableSync
    > 
    > This is failing on the android debug builder
    > http://build.chromium.org/p/chromium.linux/buildstatus?builder=Android%20Tests%20%28dbg%29&number=18942
    > 
    > I am not able to figure out which cl caused this, but it started here:
    > http://build.chromium.org/p/chromium.linux/builders/Android%20Tests%20%28dbg%29/builds/18939
    > 
    > Filed issue 348951 to track this
    > 
    > Review URL: https://codereview.chromium.org/177903011
    
    TBR=ricow@chromium.org
    
    Review URL: https://codereview.chromium.org/178473028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254748 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/android/javatests/src/org/chromium/chrome/browser/sync/SyncTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/sync/SyncTest.java
@@ -136,8 +136,6 @@ public class SyncTest extends ChromiumTestShellTestBase {
     }
 
     @HostDrivenTest
-    /* http://crbug.com/348951 */
-    @DisabledTest
     public void testDisableAndEnableSync() throws InterruptedException {
         setupTestAccountAndSignInToSync(FOREIGN_SESSION_TEST_MACHINE_ID);
         Account account =
