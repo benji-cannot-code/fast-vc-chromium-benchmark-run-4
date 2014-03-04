cbf77ecc67f083eede35e24a4ba3000bd0ca92eb
jam@chromium.org <jam@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-04 15:46:42 +0000

    Remove temporary code now that Chrome updated after it got r168165.
    BUG=304341
    
    Review URL: https://codereview.chromium.org/184043011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168390 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/web/WebFrameClient.h
+++ third_party/WebKit/public/web/WebFrameClient.h
@@ -48,9 +48,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "public/platform/WebURLRequest.h"
 #include <v8.h>
 
-// FIXME(jam): remove after roll
-#define SHOW_CONTEXT_MENU_ON_WEB_FRAME_CLIENT
-
 namespace blink {
 
 class WebApplicationCacheHost;
