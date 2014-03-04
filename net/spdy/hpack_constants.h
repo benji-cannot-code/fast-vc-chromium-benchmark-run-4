bcdd2ec3c685f3984a95f48f07d5d1ae4f5aa83d
jgraettinger@chromium.org <jgraettinger@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-04 21:43:04 +0000

    Update HPACK Huffman code for '06 draft release
    This effectively just drops the separate response code. Tests have been
    updated to use the new request-code spec fixtures.
    
    This lands server change 61954666 by jgraettinger.
    
    BUG=339578
    
    Review URL: https://codereview.chromium.org/182283003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254829 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/hpack_constants.h
+++ net/spdy/hpack_constants.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_export.h"
 
 // All section references below are to
-// http://tools.ietf.org/html/draft-ietf-httpbis-header-compression-05
+// http://tools.ietf.org/html/draft-ietf-httpbis-header-compression-06
 
 namespace net {
 
@@ -49,11 +49,8 @@ const HpackPrefix kLiteralNoIndexOpcode = { 0x01, 2 };
 // (from 4.3.2).
 const HpackPrefix kLiteralIncrementalIndexOpcode = { 0x00, 2 };
 
-// Returns symbol code table from "Appendix C. Huffman Codes For Requests".
-NET_EXPORT_PRIVATE std::vector<HpackHuffmanSymbol> HpackRequestHuffmanCode();
-
-// Returns symbol code table from "Appendix D. Huffman Codes For Responses".
-NET_EXPORT_PRIVATE std::vector<HpackHuffmanSymbol> HpackResponseHuffmanCode();
+// Returns symbol code table from "Appendix C. Huffman Codes".
+NET_EXPORT_PRIVATE std::vector<HpackHuffmanSymbol> HpackHuffmanCode();
 
 }  // namespace net
 
