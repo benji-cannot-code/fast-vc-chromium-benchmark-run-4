2d9fe6cd5fe38239501fbba3456d3770f1e63358
ricow@chromium.org <ricow@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-04 15:58:17 +0000

    Disable ExtensionApiTest.Processes, it's flaky.
    BUG=346990
    TBR=bradnelson@google.com
    
    Review URL: https://codereview.chromium.org/180803009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254760 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/processes/processes_apitest.cc
+++ chrome/browser/extensions/api/processes/processes_apitest.cc
@@ -13,7 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window.h"
 #include "extensions/common/switches.h"
 
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Processes) {
+// Test is flaky: http://crbug.com/346990
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, DISABLED_Processes) {
   ASSERT_TRUE(RunExtensionTest("processes/api")) << message_;
 }
 
