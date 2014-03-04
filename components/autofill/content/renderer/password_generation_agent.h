3a3baef87682869373c4a660c1338bd645f3aebf
gcasto@chromium.org <gcasto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-04 05:53:20 +0000

    [Password Generation] Start removing old UI
    This code in particular has been buggy in the past and is now no longer
    used for its original purpose.
    
    BUG=295698,347777
    
    Review URL: https://codereview.chromium.org/184033003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254684 0039d316-1c4b-4281-b951-d872f2087c98

--- components/autofill/content/renderer/password_generation_agent.h
+++ components/autofill/content/renderer/password_generation_agent.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "content/public/renderer/render_view_observer.h"
 #include "third_party/WebKit/public/web/WebInputElement.h"
-#include "third_party/WebKit/public/web/WebPasswordGeneratorClient.h"
 #include "url/gurl.h"
 
 namespace blink {
@@ -29,8 +28,7 @@ struct PasswordForm;
 // This class is responsible for controlling communication for password
 // generation between the browser (which shows the popup and generates
 // passwords) and WebKit (shows the generation icon in the password field).
-class PasswordGenerationAgent : public content::RenderViewObserver,
-                                public blink::WebPasswordGeneratorClient {
+class PasswordGenerationAgent : public content::RenderViewObserver {
  public:
   explicit PasswordGenerationAgent(content::RenderView* render_view);
   virtual ~PasswordGenerationAgent();
@@ -55,9 +53,6 @@ class PasswordGenerationAgent : public content::RenderViewObserver,
   virtual void DidFinishDocumentLoad(blink::WebFrame* frame) OVERRIDE;
   virtual void DidFinishLoad(blink::WebFrame* frame) OVERRIDE;
   virtual void FocusedNodeChanged(const blink::WebNode& node) OVERRIDE;
-
-  // WebPasswordGeneratorClient:
-  virtual void openPasswordGenerator(blink::WebInputElement& element) OVERRIDE;
 
   // Message handlers.
   void OnFormNotBlacklisted(const PasswordForm& form);
