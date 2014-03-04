e6504b397face460772c74b4cbd90877f505497b
uekawa@chromium.org <uekawa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-04 08:14:20 +0000

    Rename Nacl Log module name containing ':'.
    NACLVERBOSITY doesn't like : in name, so change :: to _.
    
    BUG=None
    TESTED=setenv NACLVERBOSITY=1:Plugin_ServiceRuntime=5 ....
    
    Review URL: https://codereview.chromium.org/176863008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254716 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/native_client/src/trusted/plugin/service_runtime.cc
+++ ppapi/native_client/src/trusted/plugin/service_runtime.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * found in the LICENSE file.
  */
 
-#define NACL_LOG_MODULE_NAME "Plugin::ServiceRuntime"
+#define NACL_LOG_MODULE_NAME "Plugin_ServiceRuntime"
 
 #include "ppapi/native_client/src/trusted/plugin/service_runtime.h"
 
