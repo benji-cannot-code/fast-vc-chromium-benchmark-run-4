62eac9c4ac33f1061880574e65e67f1dc17fb901
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-04 05:43:54 +0000

    sync: turn on deferred startup.
    BUG=80194
    R=rlarocque@chromium.org
    
    Review URL: https://codereview.chromium.org/184103008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254678 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/startup_controller_unittest.cc
+++ chrome/browser/sync/startup_controller_unittest.cc
@@ -87,15 +87,6 @@ class StartupControllerTest : public testing::Test {
     started_ = true;
   }
 
-  void ForceDeferredStartup() {
-    if (!CommandLine::ForCurrentProcess()->
-        HasSwitch(switches::kSyncEnableDeferredStartup)) {
-      CommandLine::ForCurrentProcess()->
-          AppendSwitch(switches::kSyncEnableDeferredStartup);
-      controller_->Reset(syncer::UserTypes());
-    }
-  }
-
   bool started() const { return started_; }
   void clear_started() { started_ = false; }
   StartupController* controller() { return controller_.get(); }
@@ -128,8 +119,8 @@ TEST_F(StartupControllerTest, Basic) {
   controller()->TryStart();
   EXPECT_FALSE(started());
   token_service()->IssueRefreshTokenForUser(kTestUser, kTestToken);
-  const bool deferred_start = CommandLine::ForCurrentProcess()->
-      HasSwitch(switches::kSyncEnableDeferredStartup);
+  const bool deferred_start = !CommandLine::ForCurrentProcess()->
+      HasSwitch(switches::kSyncDisableDeferredStartup);
   controller()->TryStart();
   EXPECT_EQ(!deferred_start, started());
   std::string state(controller()->GetBackendInitializationStateString());
@@ -164,7 +155,6 @@ TEST_F(StartupControllerTest, Managed) {
 // Test that sync doesn't start until all conditions are met and a
 // data type triggers sync startup.
 TEST_F(StartupControllerTest, DataTypeTriggered) {
-  ForceDeferredStartup();
   sync_prefs()->SetSyncSetupCompleted();
   signin()->set_account(kTestUser);
   token_service()->IssueRefreshTokenForUser(kTestUser, kTestToken);
@@ -187,7 +177,6 @@ TEST_F(StartupControllerTest, DataTypeTriggered) {
 // Test that the fallback timer starts sync in the event all
 // conditions are met and no data type requests sync.
 TEST_F(StartupControllerTest, FallbackTimer) {
-  ForceDeferredStartup();
   sync_prefs()->SetSyncSetupCompleted();
   signin()->set_account(kTestUser);
   token_service()->IssueRefreshTokenForUser(kTestUser, kTestToken);
@@ -207,8 +196,6 @@ TEST_F(StartupControllerTest, NoDeferralWithoutSessionsSync) {
   types.Remove(syncer::MANAGED_USER_SETTINGS);
   sync_prefs()->SetKeepEverythingSynced(false);
   sync_prefs()->SetPreferredDataTypes(syncer::UserTypes(), types);
-  CommandLine::ForCurrentProcess()->AppendSwitch(
-      switches::kSyncEnableDeferredStartup);
   controller()->Reset(syncer::UserTypes());
   sync_prefs()->SetSyncSetupCompleted();
   signin()->set_account(kTestUser);
@@ -220,7 +207,6 @@ TEST_F(StartupControllerTest, NoDeferralWithoutSessionsSync) {
 // Sanity check that the fallback timer doesn't fire before startup
 // conditions are met.
 TEST_F(StartupControllerTest, FallbackTimerWaits) {
-  ForceDeferredStartup();
   controller()->TryStart();
   EXPECT_FALSE(started());
   base::RunLoop().RunUntilIdle();
@@ -255,8 +241,8 @@ TEST_F(StartupControllerTest, Reset) {
   controller()->Reset(syncer::UserTypes());
   base::RunLoop().RunUntilIdle();
   EXPECT_FALSE(started());
-  const bool deferred_start = CommandLine::ForCurrentProcess()->
-      HasSwitch(switches::kSyncEnableDeferredStartup);
+  const bool deferred_start = !CommandLine::ForCurrentProcess()->
+      HasSwitch(switches::kSyncDisableDeferredStartup);
   controller()->TryStart();
   EXPECT_EQ(!deferred_start, started());
   controller()->OnDataTypeRequestsSyncStartup(syncer::SESSIONS);
