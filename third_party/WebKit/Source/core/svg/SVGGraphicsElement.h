b8bba9dd298bcdf622aa9bffd7359a70c18f102e
fmalita@chromium.org <fmalita@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-05 14:11:07 +0000

    [SVG] Refactor getIntersectionList() and getEnclosureList()
    This CL moves most of the implementation out of RenderSVGModelObject and
    into SVGSVGElement (which is the only client). Some notes:
    
    * use the consolidated SVGGraphicsElement::computeCTM() (which is
      accumulating localCoordinateSpaceTransform()s and not
      localToParentTransform()s like the old getElementCTM() did <- root
      cause for the zooming bug)
    * only force a layout update once, at the entry point, instead of on each
      getElementCTM() call.
    * detect the stopping element on the fly, instead of an additional upfront
      ancestor crawl.
    * transforms are now correctly computed relative to the target <svg>
      element instead of the nearest viewport.
    
    BUG=155277
    R=pdr@chromium.org,schenney@chromium.org,fs@opera.com,rob.buis@samsung.com
    
    Review URL: https://codereview.chromium.org/185333004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168479 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGGraphicsElement.h
+++ third_party/WebKit/Source/core/svg/SVGGraphicsElement.h
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2004, 2005, 2008 Nikolas Zimmermann <zimmermann@kde.org>
  * Copyright (C) 2004, 2005, 2006, 2007 Rob Buis <buis@kde.org>
+ * Copyright (C) 2014 Google, Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -64,6 +65,9 @@ public:
 
     SVGAnimatedTransformList* transform() { return m_transform.get(); }
     const SVGAnimatedTransformList* transform() const { return m_transform.get(); }
+
+    AffineTransform computeCTM(SVGElement::CTMScope mode, SVGGraphicsElement::StyleUpdateStrategy,
+        const SVGGraphicsElement* ancestor = 0) const;
 
 protected:
     SVGGraphicsElement(const QualifiedName&, Document&, ConstructionType = CreateSVGElement);
--- third_party/WebKit/Source/core/svg/SVGSVGElement.h
+++ third_party/WebKit/Source/core/svg/SVGSVGElement.h
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2004, 2005, 2006 Nikolas Zimmermann <zimmermann@kde.org>
  * Copyright (C) 2004, 2005, 2006, 2007, 2010 Rob Buis <buis@kde.org>
+ * Copyright (C) 2014 Google, Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -152,12 +153,13 @@ private:
 
     void updateCurrentTranslate();
 
-    enum CollectIntersectionOrEnclosure {
-        CollectIntersectionList,
-        CollectEnclosureList
+    enum CheckIntersectionOrEnclosure {
+        CheckIntersection,
+        CheckEnclosure
     };
 
-    PassRefPtr<NodeList> collectIntersectionOrEnclosureList(const FloatRect&, SVGElement*, CollectIntersectionOrEnclosure) const;
+    bool checkIntersectionOrEnclosure(const SVGElement&, const FloatRect&, CheckIntersectionOrEnclosure) const;
+    PassRefPtr<NodeList> collectIntersectionOrEnclosureList(const FloatRect&, SVGElement*, CheckIntersectionOrEnclosure) const;
 
     RefPtr<SVGAnimatedLength> m_x;
     RefPtr<SVGAnimatedLength> m_y;
