af532ad78ae051100638788b1c961bb4cdb6ec02
kenrb@chromium.org <kenrb@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-05 17:54:31 +0000

    Add Frame and RemoteFrame classes.
    Creates RemoteFrame, which will represent in WebCore frames
    that are rendered outside of Blink, and Frame, a base class
    for both LocalFrame and RemoteFrame. The current division of
    members and methods between Frame and LocalFrame is not
    final, but provides a starting point for us to start
    converting LocalFrame to Frame in some parts of Blink.
    
    BUG=346764
    R=eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/183713002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168501 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/Source/core/frame/RemoteFrame.cpp
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "config.h"
+#include "core/frame/RemoteFrame.h"
+
+namespace WebCore {
+
+inline RemoteFrame::RemoteFrame(PassRefPtr<FrameInit> frameInit)
+    : Frame(frameInit)
+{
+}
+
+PassRefPtr<RemoteFrame> RemoteFrame::create(PassRefPtr<FrameInit> frameInit)
+{
+    RefPtr<RemoteFrame> frame = adoptRef(new RemoteFrame(frameInit));
+    return frame.release();
+}
+
+RemoteFrame::~RemoteFrame()
+{
+}
+
+} // namespace WebCore
