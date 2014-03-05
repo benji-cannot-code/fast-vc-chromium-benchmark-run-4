0ad609b12044f503e067f4d4dd6edbadeb6ff49b
horo@chromium.org <horo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-05 17:18:58 +0000

    Tells the crash of SharedWorker to RenderViewHosts.
    BUG=327256
    
    Review URL: https://codereview.chromium.org/187553002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@255068 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/shared_worker/shared_worker_host.cc
+++ content/browser/shared_worker/shared_worker_host.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/shared_worker/shared_worker_host.h"
 
+#include "content/browser/frame_host/render_frame_host_delegate.h"
+#include "content/browser/frame_host/render_frame_host_impl.h"
 #include "content/browser/message_port_service.h"
 #include "content/browser/shared_worker/shared_worker_instance.h"
 #include "content/browser/shared_worker/shared_worker_message_filter.h"
@@ -13,6 +15,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 
 namespace content {
+namespace {
+
+// Notifies RenderViewHost that one or more worker objects crashed.
+void WorkerCrashCallback(int render_process_unique_id, int render_frame_id) {
+  RenderFrameHostImpl* host =
+      RenderFrameHostImpl::FromID(render_process_unique_id, render_frame_id);
+  if (host)
+    host->delegate()->WorkerCrashed(host);
+}
+
+}  // namespace
 
 SharedWorkerHost::SharedWorkerHost(SharedWorkerInstance* instance)
     : instance_(instance),
@@ -23,6 +36,21 @@ SharedWorkerHost::SharedWorkerHost(SharedWorkerInstance* instance)
 
 SharedWorkerHost::~SharedWorkerHost() {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+  // If we crashed, tell the RenderViewHosts.
+  if (instance_ && !instance_->load_failed()) {
+    const WorkerDocumentSet::DocumentInfoSet& parents =
+        instance_->worker_document_set()->documents();
+    for (WorkerDocumentSet::DocumentInfoSet::const_iterator parent_iter =
+             parents.begin();
+         parent_iter != parents.end();
+         ++parent_iter) {
+      BrowserThread::PostTask(BrowserThread::UI,
+                              FROM_HERE,
+                              base::Bind(&WorkerCrashCallback,
+                                         parent_iter->render_process_id(),
+                                         parent_iter->render_frame_id()));
+    }
+  }
 }
 
 bool SharedWorkerHost::Send(IPC::Message* message) {
