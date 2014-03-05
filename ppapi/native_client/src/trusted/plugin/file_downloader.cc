3d286ed292dd5f7c7fdcda876a5fd21c9f89b3a5
teravest@chromium.org <teravest@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-05 21:50:43 +0000

    Pepper: Simplify FileDownloader::url().
    The current behavior of FileDownloader::url() is strange since the value
    changes after the initial GET request. I've found this a little awkward, and
    decided to make url() always return the url passed to FileDownloader, and
    full_url() return the extra url information from the GET request.
    
    BUG=
    R=dmichael@chromium.org
    
    Review URL: https://codereview.chromium.org/181933005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@255156 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/native_client/src/trusted/plugin/file_downloader.cc
+++ ppapi/native_client/src/trusted/plugin/file_downloader.cc
@@ -129,7 +129,6 @@ bool FileDownloader::Open(
   CHECK(instance_ != NULL);
   open_time_ = NaClGetTimeOfDayMicroseconds();
   status_code_ = -1;
-  url_to_open_ = url;
   url_ = url;
   file_open_notify_callback_ = callback;
   mode_ = mode;
@@ -217,7 +216,6 @@ void FileDownloader::OpenFast(const nacl::string& url,
   CHECK(instance_ != NULL);
   open_time_ = NaClGetTimeOfDayMicroseconds();
   status_code_ = NACL_HTTP_STATUS_OK;
-  url_to_open_ = url;
   url_ = url;
   mode_ = DOWNLOAD_NONE;
   if (not_streaming() && file_handle != PP_kInvalidFileHandle) {
@@ -265,7 +263,7 @@ bool FileDownloader::InitialResponseIsValid() {
         "FileDownloader::InitialResponseIsValid (url is not a string)\n"));
     return false;
   }
-  url_ = full_url.AsString();
+  full_url_ = full_url.AsString();
 
   // Note that URLs in the data-URI scheme produce different error
   // codes than other schemes.  This is because data-URI are really a
@@ -372,7 +370,7 @@ void FileDownloader::URLLoadFinishNotify(int32_t pp_error) {
     stream_finish_callback_.RunAndClear(PP_ERROR_FAILED);
     return;
   }
-  url_ = full_url.AsString();
+  full_url_ = full_url.AsString();
 
   // The file is now fully downloaded.
   pp::FileRef file(url_response_.GetBodyAsFileRef());
