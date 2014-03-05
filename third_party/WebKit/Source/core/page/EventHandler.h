d9e6729dbae2b721e078f6d979fbb04ebe5cbca2
gnanasekar.s@samsung.com <gnanasekar.s@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-05 07:48:56 +0000

    Add hittest mode for Touch-action which ignore inline elements and svg elements
    Intoducing new HitTestRequest::TouchAction hit test type, which will be used
    in Special hit testing for computing effective touch-action in eventhandler.
    
    BUG=247396, 319479
    
    Review URL: https://codereview.chromium.org/137123009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168443 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/EventHandler.h
+++ third_party/WebKit/Source/core/page/EventHandler.h
@@ -246,7 +246,7 @@ private:
     bool dispatchSyntheticTouchEventIfEnabled(const PlatformMouseEvent&);
 
     TouchAction intersectTouchAction(const TouchAction, const TouchAction);
-    TouchAction computeEffectiveTouchAction(const Node&);
+    TouchAction computeEffectiveTouchAction(const LayoutPoint&);
 
     bool handleMouseEventAsEmulatedGesture(const PlatformMouseEvent&);
     bool handleWheelEventAsEmulatedGesture(const PlatformWheelEvent&);
--- third_party/WebKit/Source/core/rendering/InlineBox.h
+++ third_party/WebKit/Source/core/rendering/InlineBox.h
@@ -272,7 +272,13 @@ public:
 
     int expansion() const { return m_bitfields.expansion(); }
 
-    bool visibleToHitTestRequest(const HitTestRequest& request) const { return renderer().visibleToHitTestRequest(request); }
+    bool visibleForTouchAction() const { return false; }
+    bool visibleToHitTestRequest(const HitTestRequest& request) const
+    {
+        if (request.touchAction() && !visibleForTouchAction())
+            return false;
+        return renderer().visibleToHitTestRequest(request);
+    }
 
     EVerticalAlign verticalAlign() const { return renderer().style(m_bitfields.firstLine())->verticalAlign(); }
 
