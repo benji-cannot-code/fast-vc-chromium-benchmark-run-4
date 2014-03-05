468c31dda2414785f039e74d13e397e1cc8a0933
grunell@chromium.org <grunell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-05 14:13:13 +0000

    Improve network interface name on Win for WebRTC logs.
    - Add |friendly_name| member to NetworkInterface struct. This will be the same as |name| on non-Windows. On Windows it will be FriendlyName in IP_ADAPTER_ADDRESSES.
    - Use |friendly_name| in WebRTC logs.
    - Add simple test in unit test for |friendly_name| not empty.
    
    TBR=justinlin@chromium.org (trivial update in dial_service_unittest.cc)
    
    BUG=347541
    
    Review URL: https://codereview.chromium.org/185533006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@255031 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/net_util_posix.cc
+++ net/base/net_util_posix.cc
@@ -88,7 +88,7 @@ bool GetNetworkList(NetworkInterfaceList* networks,
     CHECK(base::StringToUint(network_tokenizer.token(), &index));
 
     networks->push_back(
-        NetworkInterface(name, index, address, network_prefix));
+        NetworkInterface(name, name, index, address, network_prefix));
   }
   return true;
 #else
@@ -162,7 +162,7 @@ bool GetNetworkList(NetworkInterfaceList* networks,
       }
 
       networks->push_back(
-          NetworkInterface(name, if_nametoindex(name.c_str()),
+          NetworkInterface(name, name, if_nametoindex(name.c_str()),
                            address.address(), net_mask));
     }
   }
