9968b2adbfb9c6b9a239669b6c6909159db2a37e
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-05 16:18:41 +0000

    [SyncFS] Respect sync_enabled_ flag on sync status
    BUG=
    
    Review URL: https://codereview.chromium.org/187563003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@255053 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_backend/sync_engine.cc
+++ chrome/browser/sync_file_system/drive_backend/sync_engine.cc
@@ -226,6 +226,8 @@ bool SyncEngine::IsConflicting(const fileapi::FileSystemURL& url) {
 }
 
 RemoteServiceState SyncEngine::GetCurrentState() const {
+  if (!sync_enabled_)
+    return REMOTE_SERVICE_DISABLED;
   return service_state_;
 }
 
@@ -280,8 +282,7 @@ SyncStatusCode SyncEngine::SetConflictResolutionPolicy(
   return SYNC_STATUS_OK;
 }
 
-ConflictResolutionPolicy
-SyncEngine::GetConflictResolutionPolicy() const {
+ConflictResolutionPolicy SyncEngine::GetConflictResolutionPolicy() const {
   return conflict_resolution_policy_;
 }
 
