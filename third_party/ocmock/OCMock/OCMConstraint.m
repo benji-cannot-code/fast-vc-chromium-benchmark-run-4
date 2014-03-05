2509b1b5fa9e31e8eb51fe28356e46df12b2cf81
sdefresne@chromium.org <sdefresne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-05 11:31:31 +0000

    Add missing selector to mocked object to fix unit tests with Xcode 5
    Unit tests on iOS were broken with Xcode 5 due to the absence of the selector
    copyWithZone: from the mocked object. Add the missing selector.
    
    This patches in OCMock e8a9cc97936bfa8be97706c5092110603745e708, "Small changes
    for compatibility with Xcode 5 Preview."
    
    BUG=256608,256607
    TEST=None
    
    Review URL: https://codereview.chromium.org/186193003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@255011 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/ocmock/OCMock/OCMConstraint.m
+++ third_party/ocmock/OCMock/OCMConstraint.m
@@ -19,6 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 	return NO;
 }
 
+- (id)copyWithZone:(NSZone *)zone
+{
+	return [self retain];
+}
 
 + (id)constraintWithSelector:(SEL)aSelector onObject:(id)anObject
 {
