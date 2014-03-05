c01b3b03f93774cad1250f63d7725f76a1140ac4
leviw@chromium.org <leviw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-05 08:51:38 +0000

    Fix sub-pixel handling of transparency layers
    Transparency layers were implemented before composited layers
    were made to be fully sub-pixel aware. Blink r164561 was the
    sub-pixel fix for composited layers, which caused them to behave
    differently than transparency layers leading to off-by-one
    errors. This patch teaches transparency layers about sub-pixel.
    
    BUG=347521
    
    Review URL: https://codereview.chromium.org/180163008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168450 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderLayer.h
+++ third_party/WebKit/Source/core/rendering/RenderLayer.h
@@ -140,7 +140,7 @@ public:
 
     bool isTransparent() const;
     RenderLayer* transparentPaintingAncestor();
-    void beginTransparencyLayers(GraphicsContext*, const RenderLayer* rootLayer, const LayoutRect& paintDirtyRect, PaintBehavior);
+    void beginTransparencyLayers(GraphicsContext*, const RenderLayer* rootLayer, const LayoutRect& paintDirtyRect, const LayoutSize& subPixelAccumulation, PaintBehavior);
 
     bool isReflection() const { return renderer()->isReplica(); }
     RenderLayerReflectionInfo* reflectionInfo() { return m_reflectionInfo.get(); }
@@ -602,7 +602,7 @@ private:
     void updateOrRemoveFilterClients();
     void updateOrRemoveFilterEffectRenderer();
 
-    LayoutRect paintingExtent(const RenderLayer* rootLayer, const LayoutRect& paintDirtyRect, PaintBehavior);
+    LayoutRect paintingExtent(const RenderLayer* rootLayer, const LayoutRect& paintDirtyRect, const LayoutSize& subPixelAccumulation, PaintBehavior);
 
     RenderLayer* enclosingTransformedAncestor() const;
 
