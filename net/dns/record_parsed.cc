2c4e6b76689008fce0e80e322e01d57c0626d0a4
noamsml@chromium.org <noamsml@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-05 21:55:06 +0000

    [mDNS] Move logging from LOG(WARNING) to DVLOG(1) to not spam the console
    Since packets we don't understand are not actually that unexpected, turn
    LOG(WARNING) messages in the mDNS code to DVLOG(1).
    
    BUG=
    
    Review URL: https://codereview.chromium.org/177373013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@255157 0039d316-1c4b-4281-b951-d872f2087c98

--- net/dns/record_parsed.cc
+++ net/dns/record_parsed.cc
@@ -54,7 +54,7 @@ scoped_ptr<const RecordParsed> RecordParsed::CreateFrom(
       rdata = NsecRecordRdata::Create(record.rdata, *parser);
       break;
     default:
-      LOG(WARNING) << "Unknown RData type for recieved record: " << record.type;
+      DVLOG(1) << "Unknown RData type for recieved record: " << record.type;
       return scoped_ptr<const RecordParsed>();
   }
 
