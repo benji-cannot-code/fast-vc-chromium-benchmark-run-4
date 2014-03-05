6951d8da4ff6606f4e8e309a592444cca047fc99
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-05 01:46:35 +0000

    Fix leak in content::WebKitPlatformSupportChildImpl::waitMultipleEvents
    BUG=348630
    R=jam@chromium.org
    
    Review URL: https://codereview.chromium.org/178653005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254913 0039d316-1c4b-4281-b951-d872f2087c98

--- content/child/webkitplatformsupport_child_impl.cc
+++ content/child/webkitplatformsupport_child_impl.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/discardable_memory.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/stl_util.h"
 #include "base/synchronization/waitable_event.h"
 #include "content/child/fling_curve_configuration.h"
 #include "content/child/web_discardable_memory_impl.h"
@@ -111,10 +112,11 @@ blink::WebWaitableEvent* WebKitPlatformSupportChildImpl::createWaitableEvent() {
 
 blink::WebWaitableEvent* WebKitPlatformSupportChildImpl::waitMultipleEvents(
     const blink::WebVector<blink::WebWaitableEvent*>& web_events) {
-  base::WaitableEvent** events = new base::WaitableEvent*[web_events.size()];
+  std::vector<base::WaitableEvent*> events;
   for (size_t i = 0; i < web_events.size(); ++i)
-    events[i] = static_cast<WebWaitableEventImpl*>(web_events[i])->impl();
-  size_t idx = base::WaitableEvent::WaitMany(events, web_events.size());
+    events.push_back(static_cast<WebWaitableEventImpl*>(web_events[i])->impl());
+  size_t idx = base::WaitableEvent::WaitMany(
+      vector_as_array(&events), events.size());
   DCHECK_LT(idx, web_events.size());
   return web_events[idx];
 }
