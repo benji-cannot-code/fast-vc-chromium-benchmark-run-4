23ca7fc671deed3df9ea1efe47d7e28e5c0812f3
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-05 06:07:35 +0000

    Mojo: share code between SyncDispatcher and Connector
    R=piman@chromium.org
    
    Review URL: https://codereview.chromium.org/185723003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254952 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/public/bindings/lib/sync_dispatcher.cc
+++ mojo/public/bindings/lib/sync_dispatcher.cc
@@ -12,39 +12,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace mojo {
 
 bool WaitForMessageAndDispatch(MessagePipeHandle handle,
-                               mojo::MessageReceiver* receiver) {
-  uint32_t num_bytes = 0, num_handles = 0;
+                               MessageReceiver* receiver) {
   while (true) {
-    MojoResult rv = ReadMessageRaw(handle,
-                                   NULL,
-                                   &num_bytes,
-                                   NULL,
-                                   &num_handles,
-                                   MOJO_READ_MESSAGE_FLAG_NONE);
-    if (rv == MOJO_RESULT_RESOURCE_EXHAUSTED)
-      break;
-    if (rv != MOJO_RESULT_SHOULD_WAIT)
-      return false;
-    rv = Wait(handle, MOJO_WAIT_FLAG_READABLE, MOJO_DEADLINE_INDEFINITE);
+    bool result;
+    MojoResult rv = ReadAndDispatchMessage(handle, receiver, &result);
+    if (rv == MOJO_RESULT_OK)
+      return result;
+    if (rv == MOJO_RESULT_SHOULD_WAIT)
+      rv = Wait(handle, MOJO_WAIT_FLAG_READABLE, MOJO_DEADLINE_INDEFINITE);
     if (rv != MOJO_RESULT_OK)
       return false;
   }
-
-  Message message;
-  message.AllocUninitializedData(num_bytes);
-  message.mutable_handles()->resize(num_handles);
-
-  MojoResult rv = ReadMessageRaw(
-      handle,
-      message.mutable_data(),
-      &num_bytes,
-      message.mutable_handles()->empty() ? NULL :
-          reinterpret_cast<MojoHandle*>(&message.mutable_handles()->front()),
-      &num_handles,
-      MOJO_READ_MESSAGE_FLAG_NONE);
-  if (rv != MOJO_RESULT_OK)
-    return false;
-  return receiver->Accept(&message);
 }
 
 }  // namespace mojo
