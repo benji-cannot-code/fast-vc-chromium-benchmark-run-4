f2eddd47e0b64bef3b0094501cf2d43edf33e079
hshi@chromium.org <hshi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-05 18:39:52 +0000

    Make Chrome OS virtual keyboard to work with cursor compositing.
    Explicitly define container IDs for the parent container of the virtual keyboard,
    as well as the mouse cursor container.
    
    This is to ensure the correct ordering in which containers are stacked together.
    Specifically the virtual keyboard parent container should be above all non-lock screen,
    lock screen and lock screen related containers, but below the mouse cursor container,
    which sits immediately below the power animation container.
    
    BUG=347304
    TEST=trybots and manual verification.
    R=kevers@chromium.org, oshima@chromium.org
    
    Review URL: https://codereview.chromium.org/186673008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@255096 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/display/cursor_window_controller.cc
+++ ash/display/cursor_window_controller.cc
@@ -138,7 +138,7 @@ void CursorWindowController::SetDisplay(const gfx::Display& display) {
     return;
 
   SetContainer(GetRootWindowController(root_window)->GetContainer(
-      kShellWindowId_OverlayContainer));
+      kShellWindowId_MouseCursorContainer));
   SetBoundsInScreen(display.bounds());
 }
 
