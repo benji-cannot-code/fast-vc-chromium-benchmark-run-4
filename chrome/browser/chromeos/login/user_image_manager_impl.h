bdac5ec7c8c449a067a2d60e33f385bfd6cba65b
tnagel@chromium.org <tnagel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-05 09:58:46 +0000

    Cleanup: Remove unused cros_settings parameter to UserImageManagerImpl()
    BUG=none
    
    Review URL: https://codereview.chromium.org/181833009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254988 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/user_image_manager_impl.h
+++ chrome/browser/chromeos/login/user_image_manager_impl.h
@@ -34,7 +34,6 @@ class SequencedTaskRunner;
 
 namespace chromeos {
 
-class CrosSettings;
 class UserImageSyncObserver;
 class UserManager;
 
@@ -44,7 +43,6 @@ class UserImageManagerImpl
  public:
   // UserImageManager:
   UserImageManagerImpl(const std::string& user_id,
-                       CrosSettings* cros_settings,
                        UserManager* user_manager);
   virtual ~UserImageManagerImpl();
 
