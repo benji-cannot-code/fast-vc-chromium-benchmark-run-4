4c8efd6d3b4f88a91f1585491a4055689881bb26
blundell@google.com <blundell@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-05 09:41:44 +0000

    Move TiclInvalidationService away from listening to Signin notifications
    As part of eliminating Signin-related notifications, this CL moves
    TiclInvalidationService to observing the SigninManager.
    
    BUG=345616
    R=pavely@chromium.org, rlarocque@chromium.org
    
    Review URL: https://codereview.chromium.org/185263010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254978 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/invalidation/ticl_invalidation_service.cc
+++ chrome/browser/invalidation/ticl_invalidation_service.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/metrics/histogram.h"
-#include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/invalidation/gcm_network_channel_delegate_impl.h"
 #include "chrome/browser/invalidation/invalidation_logger.h"
 #include "chrome/browser/invalidation/invalidation_service_util.h"
@@ -16,8 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/signin/about_signin_internals_factory.h"
 #include "chrome/browser/signin/profile_oauth2_token_service.h"
 #include "chrome/browser/signin/profile_oauth2_token_service_factory.h"
-#include "chrome/browser/signin/signin_manager.h"
-#include "content/public/browser/notification_service.h"
 #include "google_apis/gaia/gaia_constants.h"
 #include "sync/notifier/gcm_network_channel_delegate.h"
 #include "sync/notifier/invalidation_util.h"
@@ -90,9 +87,10 @@ void TiclInvalidationService::Init() {
     StartInvalidator(PUSH_CLIENT_CHANNEL);
   }
 
-  notification_registrar_.Add(this,
-                              chrome::NOTIFICATION_GOOGLE_SIGNED_OUT,
-                              content::Source<Profile>(profile_));
+  SigninManagerBase* signin_manager =
+      SigninManagerFactory::GetForProfile(profile_);
+  signin_manager->AddObserver(this);
+
   oauth2_token_service_->AddObserver(this);
 }
 
@@ -164,12 +162,8 @@ InvalidationLogger* TiclInvalidationService::GetInvalidationLogger() {
   return &logger_;
 }
 
-void TiclInvalidationService::Observe(
-    int type,
-    const content::NotificationSource& source,
-    const content::NotificationDetails& details) {
+void TiclInvalidationService::GoogleSignedOut(const std::string& username) {
   DCHECK(CalledOnValidThread());
-  DCHECK_EQ(type, chrome::NOTIFICATION_GOOGLE_SIGNED_OUT);
   Logout();
 }
 
@@ -291,6 +285,9 @@ std::string TiclInvalidationService::GetOwnerName() const { return "TICL"; }
 
 void TiclInvalidationService::Shutdown() {
   DCHECK(CalledOnValidThread());
+  SigninManagerBase* signin_manager =
+      SigninManagerFactory::GetForProfile(profile_);
+  signin_manager->RemoveObserver(this);
   oauth2_token_service_->RemoveObserver(this);
   if (IsStarted()) {
     StopInvalidator();
