e3f096e826ba760cded75527a6bfb24aba1557de
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-05 16:47:54 +0000

    Mojo: Enable EmbedderTest on Windows.
    R=yzshen@chromium.org
    
    Review URL: https://codereview.chromium.org/177353014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@255063 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/system/embedder/embedder_unittest.cc
+++ mojo/system/embedder/embedder_unittest.cc
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/location.h"
 #include "base/logging.h"
-// TODO(vtl): Remove build_config.h include when fully implemented on Windows.
-#include "build/build_config.h"
 #include "mojo/public/system/core.h"
 #include "mojo/system/embedder/platform_channel_pair.h"
 #include "mojo/system/embedder/test_embedder.h"
@@ -33,8 +31,6 @@ void StoreChannelInfo(ChannelInfo** store_channel_info_here,
 TEST_F(EmbedderTest, ChannelsBasic) {
   Init();
 
-// TODO(vtl): |PlatformChannelPair| not implemented on Windows yet.
-#if !defined(OS_WIN)
   PlatformChannelPair channel_pair;
   ScopedPlatformHandle server_handle = channel_pair.PassServerHandle();
   ScopedPlatformHandle client_handle = channel_pair.PassClientHandle();
@@ -90,7 +86,6 @@ TEST_F(EmbedderTest, ChannelsBasic) {
                                 FROM_HERE,
                                 base::Bind(&DestroyChannelOnIOThread,
                                            client_channel_info));
-#endif  // !defined(OS_WIN)
 
   EXPECT_TRUE(test::Shutdown());
 }
