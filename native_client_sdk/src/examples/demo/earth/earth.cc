67cc0502c84f7f8d7c616b53d4fe2af14d036659
mseaborn@chromium.org <mseaborn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-06 14:17:38 +0000

    NaCl SDK: Remove more unused variables in examples
    PNaCl is being upgraded to Clang 3.4, which now warns about unused
    declarations inside anonymous namespaces.
    
    BUG=https://code.google.com/p/nativeclient/issues/detail?id=3757
    TEST=none
    R=binji@chromium.org
    
    Review URL: https://codereview.chromium.org/180953007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@255341 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/examples/demo/earth/earth.cc
+++ native_client_sdk/src/examples/demo/earth/earth.cc
@@ -33,7 +33,6 @@ using namespace sdk_util;  // For sdk_util::ThreadPool
 
 // Global properties used to setup Earth demo.
 namespace {
-const float kHugeZ = 1.0e38f;
 const float kPI = M_PI;
 const float kTwoPI = kPI * 2.0f;
 const float kOneOverPI = 1.0f / kPI;
@@ -46,7 +45,6 @@ const float kZoomMax = 50.0f;
 const float kWheelSpeed = 2.0f;
 const float kLightMin = 0.0f;
 const float kLightMax = 2.0f;
-const int kFrameTimeBufferSize = 512;
 
 // Timer helper for benchmarking.  Returns seconds elapsed since program start,
 // as a double.
@@ -148,8 +146,6 @@ inline const float AsFloat(const int i) {
 }
 
 const long int kOneAsInteger = AsInteger(1.0f);
-const float kScaleUp = float(0x00800000);
-const float kScaleDown = 1.0f / kScaleUp;
 
 inline float inline_quick_sqrt(float x) {
   int i;
