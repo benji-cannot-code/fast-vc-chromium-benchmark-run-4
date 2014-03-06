b2f7c468ed6d174bd8bf2b511b86e37257156b56
bolian@chromium.org <bolian@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-06 01:19:47 +0000

    Added support to listen on Network.responseReceived and expose that through tab.py
    BUG=337526
    
    Review URL: https://codereview.chromium.org/145923002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@255200 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/backends/chrome/inspector_timeline.py
+++ tools/telemetry/telemetry/core/backends/chrome/inspector_timeline.py
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 from telemetry.core.backends.chrome import inspector_timeline_data
-from telemetry.core.timeline import model
+from telemetry.core.backends.chrome import timeline_recorder
 
 
 class TabBackendException(Exception):
@@ -12,7 +12,7 @@ class TabBackendException(Exception):
   pass
 
 
-class InspectorTimeline(object):
+class InspectorTimeline(timeline_recorder.TimelineRecorder):
   """Implementation of dev tools timeline."""
 
   class Recorder(object):
@@ -35,13 +35,9 @@ class InspectorTimeline(object):
       self._tab.StopTimelineRecording()
 
   def __init__(self, inspector_backend):
+    super(InspectorTimeline, self).__init__()
     self._inspector_backend = inspector_backend
     self._is_recording = False
-    self._timeline_model = None
-
-  @property
-  def timeline_model(self):
-    return self._timeline_model
 
   def Start(self):
     """Starts recording."""
@@ -59,16 +55,16 @@ class InspectorTimeline(object):
     self._SendSyncRequest(request)
 
   def Stop(self):
-    """Stops recording and makes a TimelineModel with the event data."""
-    assert self._is_recording, 'Stop should be called after Start.'
+    """Stops recording and returns timeline event data."""
+    if not self._is_recording:
+      return None
     request = {'method': 'Timeline.stop'}
     result = self._SendSyncRequest(request)
-    raw_events = result['events']
-    timeline_data = inspector_timeline_data.InspectorTimelineData(raw_events)
-    self._timeline_model = model.TimelineModel(
-        timeline_data=timeline_data, shift_world_to_zero=False)
     self._inspector_backend.UnregisterDomain('Timeline')
     self._is_recording = False
+
+    raw_events = result['events']
+    return inspector_timeline_data.InspectorTimelineData(raw_events)
 
   def _SendSyncRequest(self, request, timeout=60):
     """Sends a devtools remote debugging protocol request.
