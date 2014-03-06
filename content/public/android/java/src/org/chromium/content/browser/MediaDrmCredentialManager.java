192e2b689c564599b0f71c7aceb0e7614189cae3
withlhw@gmail.com <withlhw@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-06 10:56:35 +0000

    Remove unused variable in MediaDrmCredentialManager.java
    R=tedchoc@chromium.org
    
    Review URL: https://codereview.chromium.org/185223002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@255308 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/MediaDrmCredentialManager.java
+++ content/public/android/java/src/org/chromium/content/browser/MediaDrmCredentialManager.java
@@ -13,7 +13,6 @@ import org.chromium.base.JNINamespace;
  */
 @JNINamespace("content")
 public class MediaDrmCredentialManager {
-    private int mNativeMediaDrmCredentialManager;
 
     /**
      * Callback interface for getting notified from credential reset.
