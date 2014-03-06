c76ec9d6eeb86dd1e0f14a4d288bd96798ce288b
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-06 14:12:35 +0000

    [SyncFS] Wire up MetadataDatabaseIndex to MetadataDatabase
    Use MetadataDatabaseIndex as the separate index management class in MetadataDatabase.
    
    BUG=344769
    NOTRY=true
    TEST=unit_tests --gtest_filter='DriveBackendSyncTest.*:MetadataDatabaseTest.*'
    
    Review URL: https://codereview.chromium.org/177233005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@255340 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_backend/metadata_database_unittest.cc
+++ chrome/browser/sync_file_system/drive_backend/metadata_database_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync_file_system/drive_backend/drive_backend_test_util.h"
 #include "chrome/browser/sync_file_system/drive_backend/drive_backend_util.h"
 #include "chrome/browser/sync_file_system/drive_backend/metadata_database.pb.h"
+#include "chrome/browser/sync_file_system/drive_backend/metadata_database_index.h"
 #include "chrome/browser/sync_file_system/sync_file_system_test_util.h"
 #include "google_apis/drive/drive_api_parser.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -469,6 +470,11 @@ class MetadataDatabaseTest : public testing::Test {
                   &metadata_database_2));
     metadata_database_->db_ = metadata_database_2->db_.Pass();
 
+    const MetadataDatabaseIndex* on_memory =
+        metadata_database_->index_.get();
+    const MetadataDatabaseIndex* reloaded =
+        metadata_database_2->index_.get();
+
     {
       SCOPED_TRACE("Expect equivalent service_metadata");
       ExpectEquivalent(metadata_database_->service_metadata_.get(),
@@ -476,39 +482,39 @@ class MetadataDatabaseTest : public testing::Test {
     }
 
     {
-      SCOPED_TRACE("Expect equivalent file_by_id_ contents.");
-      ExpectEquivalent(metadata_database_->metadata_by_id_,
-                       metadata_database_2->metadata_by_id_);
+      SCOPED_TRACE("Expect equivalent metadata_by_id_ contents.");
+      ExpectEquivalent(on_memory->metadata_by_id_,
+                       reloaded->metadata_by_id_);
     }
 
     {
       SCOPED_TRACE("Expect equivalent tracker_by_id_ contents.");
-      ExpectEquivalent(metadata_database_->tracker_by_id_,
-                       metadata_database_2->tracker_by_id_);
+      ExpectEquivalent(on_memory->tracker_by_id_,
+                       reloaded->tracker_by_id_);
     }
 
     {
       SCOPED_TRACE("Expect equivalent trackers_by_file_id_ contents.");
-      ExpectEquivalent(metadata_database_->trackers_by_file_id_,
-                       metadata_database_2->trackers_by_file_id_);
+      ExpectEquivalent(on_memory->trackers_by_file_id_,
+                       reloaded->trackers_by_file_id_);
     }
 
     {
       SCOPED_TRACE("Expect equivalent app_root_by_app_id_ contents.");
-      ExpectEquivalent(metadata_database_->app_root_by_app_id_,
-                       metadata_database_2->app_root_by_app_id_);
+      ExpectEquivalent(on_memory->app_root_by_app_id_,
+                       reloaded->app_root_by_app_id_);
     }
 
     {
       SCOPED_TRACE("Expect equivalent trackers_by_parent_and_title_ contents.");
-      ExpectEquivalent(metadata_database_->trackers_by_parent_and_title_,
-                       metadata_database_2->trackers_by_parent_and_title_);
+      ExpectEquivalent(on_memory->trackers_by_parent_and_title_,
+                       reloaded->trackers_by_parent_and_title_);
     }
 
     {
       SCOPED_TRACE("Expect equivalent dirty_trackers_ contents.");
-      ExpectEquivalent(metadata_database_->dirty_trackers_,
-                       metadata_database_2->dirty_trackers_);
+      ExpectEquivalent(on_memory->dirty_trackers_,
+                       reloaded->dirty_trackers_);
     }
   }
 
