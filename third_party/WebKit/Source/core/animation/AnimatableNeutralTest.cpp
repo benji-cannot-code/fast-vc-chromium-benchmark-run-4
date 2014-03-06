3ce3bf91dca868fbac29234827163ebccc1ea1fa
wibling@chromium.org <wibling@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-06 14:21:24 +0000

    Revert of Move all RefPtr's to CSSValue to oilpan transition types, except for the one in CSSCursorImageValue. (https://codereview.chromium.org/184313004/)
    Reason for revert:
    Broke the build.
    
    Original issue's description:
    > Move all RefPtr's to CSSValue to oilpan transition types, except for the one in CSSCursorImageValue.
    > 
    > This change gets rid of all but one RefPtr to CSSValue. I have introduced a few persistents core/animation/..., core/css/StylePropertySet, and ElementStyleResources. I plan to attack the one in CSSCursorImageValue subsequently, and then change CSSValue from RefCountedGarbageCollected to GarbageCollectedFinalized.
    > 
    > R=ager@chromium.org, erik.corry@gmail.com, haraken@chromium.org, oilpan-reviews@chromium.org, tkent@chromium.org, vegorov@chromium.org, zerny@chromium.org
    > BUG=341815
    > NOTRY=true
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=168466
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=168635
    
    TBR=ager@chromium.org,erik.corry@gmail.com,haraken@chromium.org,oilpan-reviews@chromium.org,tkent@chromium.org,vegorov@chromium.org,zerny@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=341815
    
    Review URL: https://codereview.chromium.org/188693002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168639 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/AnimatableNeutralTest.cpp
+++ third_party/WebKit/Source/core/animation/AnimatableNeutralTest.cpp
@@ -48,7 +48,7 @@ TEST(AnimationAnimatableNeutralTest, Create)
 
 TEST(AnimationAnimatableNeutralTest, Add)
 {
-    RefPtrWillBeRawPtr<CSSValue> cssValue = CSSArrayFunctionValue::create();
+    RefPtr<CSSValue> cssValue = CSSArrayFunctionValue::create();
     RefPtr<AnimatableValue> animatableUnknown = AnimatableUnknown::create(cssValue);
 
     EXPECT_EQ(cssValue, toAnimatableUnknown(AnimatableValue::add(animatableUnknown.get(), AnimatableValue::neutralValue()).get())->toCSSValue());
