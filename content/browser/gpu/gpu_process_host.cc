ccec6b57c15f89bb91360814b7527d7395d3c6d3
jorgelo@chromium.org <jorgelo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-07 09:16:06 +0000

    Implement '--gpu-sandbox-failures-nonfatal' flag.
    Also, move the flag to content:: instead of chromeos::.
    
    BUG=327824
    TEST=Chrome compiles, loads with and without the flag.
    
    Review URL: https://codereview.chromium.org/189313002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@255564 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -49,10 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/switches.h"
 #endif
 
-#if defined(OS_CHROMEOS)
-#include "chromeos/chromeos_switches.h"
-#endif
-
 #if defined(USE_OZONE)
 #include "ui/ozone/ozone_switches.h"
 #endif
@@ -1094,6 +1090,7 @@ bool GpuProcessHost::LaunchGpuProcess(const std::string& channel_id) {
     switches::kEnableShareGroupAsyncTextureUpload,
     switches::kGpuStartupDialog,
     switches::kGpuSandboxAllowSysVShm,
+    switches::kGpuSandboxFailuresNonfatal,
     switches::kLoggingLevel,
     switches::kNoSandbox,
     switches::kTestGLLib,
@@ -1102,9 +1099,6 @@ bool GpuProcessHost::LaunchGpuProcess(const std::string& channel_id) {
     switches::kVModule,
 #if defined(OS_MACOSX)
     switches::kEnableSandboxLogging,
-#endif
-#if defined(OS_CHROMEOS)
-    chromeos::switches::kGpuSandboxFailuresNonfatal,
 #endif
 #if defined(USE_OZONE)
     switches::kOzonePlatform,
