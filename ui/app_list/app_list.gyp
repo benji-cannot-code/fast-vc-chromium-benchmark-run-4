54d5da8b9dbc90fac78df7f9f5c75f01f8a7acb1
calamity@chromium.org <calamity@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-07 09:04:35 +0000

    Remove SearchResultViewDelegate from the app list.
    This CL removes the SearchResultViewDelegate in favor of more directly
    calling the code that was being delegated to. It also removes some
    methods from SearchResultListViewDelegate.
    
    This is a pure refactor without any functionality changes.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/179013003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@255557 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/app_list/app_list.gyp
+++ ui/app_list/app_list.gyp
@@ -131,7 +131,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'views/search_result_list_view_delegate.h',
         'views/search_result_view.cc',
         'views/search_result_view.h',
-        'views/search_result_view_delegate.h',
         'views/signin_view.cc',
         'views/signin_view.h',
         'views/speech_view.cc',
--- ui/app_list/test/app_list_test_view_delegate.h
+++ ui/app_list/test/app_list_test_view_delegate.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_APP_LIST_TEST_APP_LIST_TEST_VIEW_DELEGATE_H_
 #define UI_APP_LIST_TEST_APP_LIST_TEST_VIEW_DELEGATE_H_
 
+#include <map>
 #include <string>
 
 #include "base/callback_forward.h"
@@ -31,6 +32,9 @@ class AppListTestViewDelegate : public AppListViewDelegate {
   int open_search_result_count() { return open_search_result_count_; }
   void SetUsers(const Users& users) {
     users_ = users;
+  }
+  std::map<size_t, int> open_search_result_counts() {
+    return open_search_result_counts_;
   }
 
   void set_auto_launch_timeout(const base::TimeDelta& timeout) {
@@ -85,6 +89,7 @@ class AppListTestViewDelegate : public AppListViewDelegate {
  private:
   int dismiss_count_;
   int open_search_result_count_;
+  std::map<size_t, int> open_search_result_counts_;
   Users users_;
   scoped_ptr<TestSigninDelegate> test_signin_delegate_;
   scoped_ptr<AppListTestModel> model_;
