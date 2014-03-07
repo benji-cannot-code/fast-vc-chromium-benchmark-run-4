16c96f7ee195bc413f6116f7e44abfb0dcdc28aa
mlamouri@chromium.org <mlamouri@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-07 18:23:01 +0000

    Add getUILanguage() to chrome.i18n which returns the browser UI language.
    For the moment, extension and internal code relies on navigator.language
    to return the browser UI language but it should return the preferred
    language for fingerprinting reasons, see http://crbug.com/101138.
    
    BUG=348402
    
    Review URL: https://codereview.chromium.org/185423003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@255663 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/extensions/i18n_custom_bindings.cc
+++ chrome/renderer/extensions/i18n_custom_bindings.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "chrome/common/extensions/extension_messages.h"
 #include "chrome/common/extensions/message_bundle.h"
+#include "content/public/renderer/render_thread.h"
 #include "content/public/renderer/render_view.h"
 #include "grit/renderer_resources.h"
 #include "v8/include/v8.h"
@@ -19,6 +20,9 @@ I18NCustomBindings::I18NCustomBindings(Dispatcher* dispatcher,
     : ChromeV8Extension(dispatcher, context) {
   RouteFunction("GetL10nMessage",
       base::Bind(&I18NCustomBindings::GetL10nMessage, base::Unretained(this)));
+  RouteFunction("GetL10nUILanguage",
+                base::Bind(&I18NCustomBindings::GetL10nUILanguage,
+                           base::Unretained(this)));
 }
 
 void I18NCustomBindings::GetL10nMessage(
@@ -83,6 +87,12 @@ void I18NCustomBindings::GetL10nMessage(
   args.GetReturnValue().Set(
       v8::String::NewFromUtf8(isolate, ReplaceStringPlaceholders(
         message, substitutions, NULL).c_str()));
+}
+
+void I18NCustomBindings::GetL10nUILanguage(
+    const v8::FunctionCallbackInfo<v8::Value>& args) {
+  args.GetReturnValue().Set(v8::String::NewFromUtf8(
+      args.GetIsolate(), content::RenderThread::Get()->GetLocale().c_str()));
 }
 
 }  // namespace extensions
