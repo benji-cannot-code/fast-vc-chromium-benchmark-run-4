43f83cf6bc3ddc772f765947b8cdbc396da12652
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-07 03:48:47 +0000

    Make RenderStyle return const Length&'s instead of copying Lengths.
    The Length copy constructor does a memcpy and a branch and is
    showing up on profiles. There's no need to be copying around
    Lengths in the vast majority of cases.
    
    Review URL: https://codereview.chromium.org/189243002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168696 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/style/RenderStyle.cpp
+++ third_party/WebKit/Source/core/rendering/style/RenderStyle.cpp
@@ -1211,7 +1211,7 @@ bool RenderStyle::setFontDescription(const FontDescription& v)
     return false;
 }
 
-Length RenderStyle::specifiedLineHeight() const { return inherited->line_height; }
+const Length& RenderStyle::specifiedLineHeight() const { return inherited->line_height; }
 Length RenderStyle::lineHeight() const
 {
     const Length& lh = inherited->line_height;
