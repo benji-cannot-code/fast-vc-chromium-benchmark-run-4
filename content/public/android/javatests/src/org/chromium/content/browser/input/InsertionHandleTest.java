12933f04ae24b101fc1efd7805f49012f9468ae2
aurimas@google.com <aurimas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-07 23:59:34 +0000

    In tests call ImeAdapter from UI thread only.
    BUG=None
    R=tedchoc@chromium.org
    
    Review URL: https://codereview.chromium.org/191133003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@255718 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/javatests/src/org/chromium/content/browser/input/InsertionHandleTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/input/InsertionHandleTest.java
@@ -28,6 +28,9 @@ import org.chromium.content.browser.test.util.TestTouchUtils;
 import org.chromium.content.browser.test.util.TouchCommon;
 import org.chromium.content_shell_apk.ContentShellTestBase;
 
+/**
+ * Tests the insertion handle that allows users to paste text.
+ */
 public class InsertionHandleTest extends ContentShellTestBase {
     private static final String META_DISABLE_ZOOM =
         "<meta name=\"viewport\" content=\"" +
@@ -72,7 +75,7 @@ public class InsertionHandleTest extends ContentShellTestBase {
 
         // The TestInputMethodManagerWrapper intercepts showSoftInput so that a keyboard is never
         // brought up.
-        getImeAdapter().setInputMethodManagerWrapper(
+        getContentViewCore().getImeAdapterForTest().setInputMethodManagerWrapper(
                 new TestInputMethodManagerWrapper(getContentViewCore()));
     }
 
@@ -83,7 +86,7 @@ public class InsertionHandleTest extends ContentShellTestBase {
         clickNodeToShowInsertionHandle(TEXTAREA_ID);
 
         // Unselecting should cause the handle to disappear.
-        getImeAdapter().unselect();
+        unselectOnMainSync();
         assertTrue(waitForHandleShowingEquals(false));
     }
 
@@ -363,6 +366,15 @@ public class InsertionHandleTest extends ContentShellTestBase {
         });
     }
 
+    private void unselectOnMainSync() {
+        getInstrumentation().runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+                getContentViewCore().getImeAdapterForTest().unselect();
+            }
+        });
+    }
+
     private int getSelectionStart() {
         return Selection.getSelectionStart(getEditable());
     }
@@ -383,9 +395,5 @@ public class InsertionHandleTest extends ContentShellTestBase {
         ClipboardManager clipMgr =
                 (ClipboardManager) getActivity().getSystemService(Context.CLIPBOARD_SERVICE);
         clipMgr.setPrimaryClip(ClipData.newPlainText(null, text));
-    }
-
-    private ImeAdapter getImeAdapter() {
-        return getContentViewCore().getImeAdapterForTest();
     }
 }
