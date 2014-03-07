6c5c96ea78c8baf7536bf439d9a1de87f27df9b3
skyostil@chromium.org <skyostil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-07 03:34:09 +0000

    cc: Also trace the scheduler client state
    When the disabled-by-default-cc.debug.scheduler category is enabled,
    also include the scheduler client state in the dumped data. This is
    mainly useful for the the draw, commit and activation time duration
    estimates.
    
    No tests since this only affects debugging code.
    
    BUG=347366,309648
    
    Review URL: https://codereview.chromium.org/176973007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@255507 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/scheduler/scheduler.h
+++ cc/scheduler/scheduler.h
@@ -103,7 +103,7 @@ class CC_EXPORT Scheduler {
 
   bool WillDrawIfNeeded() const;
 
-  base::TimeTicks AnticipatedDrawTime();
+  base::TimeTicks AnticipatedDrawTime() const;
 
   base::TimeTicks LastBeginImplFrameTime();
 
@@ -111,9 +111,7 @@ class CC_EXPORT Scheduler {
   void OnBeginImplFrameDeadline();
   void PollForAnticipatedDrawTriggers();
 
-  scoped_ptr<base::Value> StateAsValue() {
-    return state_machine_.AsValue().Pass();
-  }
+  scoped_ptr<base::Value> StateAsValue() const;
 
   bool IsInsideAction(SchedulerStateMachine::Action action) {
     return inside_action_ == action;
