12e8c2d28a8bdd58e4a847e33ae7b718d1f51b58
tommycli@chromium.org <tommycli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-07 03:34:54 +0000

    MTP Streaming: Switch streaming impl for Linux/ChromeOS on.
    BUG=110119
    
    Review URL: https://codereview.chromium.org/187663004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@255508 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media_galleries/linux/mtp_device_delegate_impl_linux.cc
+++ chrome/browser/media_galleries/linux/mtp_device_delegate_impl_linux.cc
@@ -284,7 +284,7 @@ void MTPDeviceDelegateImplLinux::CreateSnapshotFile(
 }
 
 bool MTPDeviceDelegateImplLinux::IsStreaming() {
-  return false;
+  return true;
 }
 
 void MTPDeviceDelegateImplLinux::ReadBytes(
