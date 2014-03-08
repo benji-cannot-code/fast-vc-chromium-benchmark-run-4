322e1b2597c7b256583f7166a92712b05cd77fae
morrita@chromium.org <morrita@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-08 03:24:48 +0000

    Revert 168742 "Introduce HeapTerminatedArray and HeapTerminatedA..."
    This breaks blink_heap_unittests on Android.
    > Introduce HeapTerminatedArray and HeapTerminatedArrayBuilder.
    > 
    > BUG=
    > 
    > Review URL: https://codereview.chromium.org/183893032
    
    TBR=vegorov@chromium.org
    
    Review URL: https://codereview.chromium.org/191843002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168758 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/heap/Handle.h
+++ third_party/WebKit/Source/heap/Handle.h
@@ -43,21 +43,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-template<typename T> class HeapTerminatedArray;
-
-#define COMPILE_ASSERT_IS_GARBAGE_COLLECTED(T, ErrorMessage)                               \
-    typedef WTF::IsSubclassOfTemplate<T, GarbageCollected> GarbageCollectedSubclass;       \
-    typedef WTF::IsSubclass<T, GarbageCollectedMixin> GarbageCollectedMixinSubclass;       \
-    typedef WTF::IsSubclassOfTemplate3<T, HeapHashSet> HeapHashSetSubclass;                \
-    typedef WTF::IsSubclassOfTemplate5<T, HeapHashMap> HeapHashMapSubclass;                \
-    typedef WTF::IsSubclassOfTemplateTypenameSize<T, HeapVector> HeapVectorSubclass;       \
-    typedef WTF::IsSubclassOfTemplate<T, HeapTerminatedArray> HeapTerminatedArraySubclass; \
-    COMPILE_ASSERT(GarbageCollectedSubclass::value ||                                      \
-        GarbageCollectedMixinSubclass::value ||                                            \
-        HeapHashSetSubclass::value ||                                                      \
-        HeapHashMapSubclass::value ||                                                      \
-        HeapVectorSubclass::value ||                                                       \
-        HeapTerminatedArraySubclass::value,                                                \
+#define COMPILE_ASSERT_IS_GARBAGE_COLLECTED(T, ErrorMessage) \
+    typedef WTF::IsSubclassOfTemplate<T, GarbageCollected> GarbageCollectedSubclass; \
+    typedef WTF::IsSubclass<T, GarbageCollectedMixin> GarbageCollectedMixinSubclass; \
+    typedef WTF::IsSubclassOfTemplate3<T, HeapHashSet> HeapHashSetSubclass; \
+    typedef WTF::IsSubclassOfTemplate5<T, HeapHashMap> HeapHashMapSubclass; \
+    typedef WTF::IsSubclassOfTemplateTypenameSize<T, HeapVector> HeapVectorSubclass; \
+    COMPILE_ASSERT(GarbageCollectedSubclass::value || \
+        GarbageCollectedMixinSubclass::value || \
+        HeapHashSetSubclass::value || \
+        HeapHashMapSubclass::value || \
+        HeapVectorSubclass::value, \
         ErrorMessage);
 
 template<typename T> class Member;
@@ -638,8 +634,6 @@ template<typename T, typename U> inline bool operator!=(const Persistent<T>& a, 
 #define WillBeHeapVector WebCore::HeapVector
 #define WillBePersistentHeapVector WebCore::PersistentHeapVector
 #define WillBeGarbageCollectedMixin WebCore::GarbageCollectedMixin
-#define WillBeHeapTerminatedArray WebCore::HeapTerminatedArray
-#define WillBeHeapTerminatedArrayBuilder WebCore::HeapTerminatedArrayBuilder
 
 template<typename T> PassRefPtrWillBeRawPtr<T> adoptRefWillBeNoop(T* ptr)
 {
@@ -699,8 +693,6 @@ public:
 #define WillBeHeapVector WTF::Vector
 #define WillBePersistentHeapVector WTF::Vector
 #define WillBeGarbageCollectedMixin WebCore::DummyBase<void>
-#define WillBeHeapTerminatedArray WTF::TerminatedArray
-#define WillBeHeapTerminatedArrayBuilder WTF::TerminatedArrayBuilder
 
 template<typename T> PassRefPtrWillBeRawPtr<T> adoptRefWillBeNoop(T* ptr) { return adoptRef(ptr); }
 template<typename T> PassRefPtrWillBeRawPtr<T> adoptRefWillBeRefCountedGarbageCollected(T* ptr) { return adoptRef(ptr); }
