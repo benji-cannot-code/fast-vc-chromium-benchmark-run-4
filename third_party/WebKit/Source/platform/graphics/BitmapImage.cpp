3ad9ba7d8202608d524b1a054ed88b4149ed19b4
hclam@chromium.org <hclam@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-08 03:44:39 +0000

    Use bilinear filtering for animated images
    Animated images are eating up a lot of CPU cycles particularly on a
    device with device scale > 1. One example is the Chromebook pixel.
    This change forces low interpolation quality (aka bilinear) for images that are potentially animated.
    
    One change is made to GIFImageDecoder to detect if an image is
    non-animated. It checks for all data received, image fully parsed and
    only contains one frame. This test is extra strict so as not to break
    existing content.
    
    WEBPImageDecoder now also reports cAnimationNone if it is not
    animated.
    
    This change does not affect JPEG, PNG and BMP. The rendering quality of
    progressively decoded images is unchanged.
    
    BUG=347767
    
    Review URL: https://codereview.chromium.org/178193029
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168759 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/BitmapImage.cpp
+++ third_party/WebKit/Source/platform/graphics/BitmapImage.cpp
@@ -531,6 +531,15 @@ void BitmapImage::resetAnimation()
     destroyDecodedDataIfNecessary();
 }
 
+bool BitmapImage::maybeAnimated()
+{
+    if (m_animationFinished)
+        return false;
+    if (frameCount() > 1)
+        return true;
+    return m_source.repetitionCount() != cAnimationNone;
+}
+
 void BitmapImage::advanceAnimation(Timer<BitmapImage>*)
 {
     internalAdvanceAnimation(false);
