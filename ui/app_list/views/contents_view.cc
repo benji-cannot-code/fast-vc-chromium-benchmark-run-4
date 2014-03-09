52fd288e570f3ccaec5b145b7290b5414750fd3f
calamity@chromium.org <calamity@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-09 03:04:44 +0000

    Change content view switching animation for experimental app launcher.
    This CL implements a different animation for the experimental app list.
    The change will allow more views to be added with a default animation
    when switching the contents view.
    
    BUG=349727
    
    Review URL: https://codereview.chromium.org/187083004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@255812 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/app_list/views/contents_view.cc
+++ ui/app_list/views/contents_view.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "ui/app_list/app_list_constants.h"
+#include "ui/app_list/app_list_switches.h"
 #include "ui/app_list/app_list_view_delegate.h"
 #include "ui/app_list/pagination_model.h"
 #include "ui/app_list/views/app_list_main_view.h"
@@ -109,6 +110,31 @@ void ContentsView::CalculateIdealBounds() {
   gfx::Rect rect(GetContentsBounds());
   if (rect.IsEmpty())
     return;
+
+  if (app_list::switches::IsExperimentalAppListEnabled()) {
+    int incoming_view_index = 0;
+    switch (show_state_) {
+      case SHOW_APPS:
+        incoming_view_index = kIndexAppsContainer;
+        break;
+      case SHOW_SEARCH_RESULTS:
+        incoming_view_index = kIndexSearchResults;
+        break;
+      default:
+        NOTREACHED();
+    }
+
+    gfx::Rect incoming_target(rect);
+    gfx::Rect outgoing_target(rect);
+    outgoing_target.set_y(-outgoing_target.height());
+
+    for (int i = 0; i < view_model_->view_size(); ++i) {
+      view_model_->set_ideal_bounds(i,
+                                    i == incoming_view_index ? incoming_target
+                                                             : outgoing_target);
+    }
+    return;
+  }
 
   gfx::Rect container_frame(rect);
   gfx::Rect results_frame(rect);
