066fd0f35b596a233ab132d7509925556526ca05
perkj@chromium.org <perkj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-09 09:14:47 +0000

    Make sure natural_size is set when a frame is cropped. If the natural size is not set, the <video> tag will set its properties videoHeight and videoWidth to the original, uncropped width and height.If that is used for deciding the <video> render size- the aspect ratio will be wrong.
    This cl allow setting the natural_size as well as the visible_rect when calling media::VideoFrame::WrapVideoFrame.
    
    BUG=349450
    R=fischman@chromium.org, tommi@chromium.org
    
    Review URL: https://codereview.chromium.org/189513004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@255839 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/video_frame_pool.cc
+++ media/base/video_frame_pool.cc
@@ -87,7 +87,7 @@ scoped_refptr<VideoFrame> VideoFramePool::PoolImpl::CreateFrame(
   }
 
   return VideoFrame::WrapVideoFrame(
-      frame, frame->visible_rect(),
+      frame, frame->visible_rect(), frame->natural_size(),
       base::Bind(&VideoFramePool::PoolImpl::FrameReleased, this, frame));
 }
 
