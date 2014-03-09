c898e3345b555e0124dbb54ecf7a6c1d6add0746
ch.dumez@samsung.com <ch.dumez@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-09 00:32:29 +0000

    Drop SVGElement::isGradientStop() virtual function
    Drop SVGElement::isGradientStop() virtual function and use the isSVGStopElement()
    helper function instead, for consistency. Unless I am missing something, we don't
    need a virtual function here and checking for the SVGNames::stopTag should be
    sufficient.
    
    R=pdr
    BUG=346095
    
    Review URL: https://codereview.chromium.org/192103002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168795 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGElement.h
+++ third_party/WebKit/Source/core/svg/SVGElement.h
@@ -86,7 +86,6 @@ public:
 
     virtual bool isSVGGraphicsElement() const { return false; }
     virtual bool isFilterEffect() const { return false; }
-    virtual bool isGradientStop() const { return false; }
     virtual bool isTextContent() const { return false; }
     virtual bool isTextPositioning() const { return false; }
     virtual bool isStructurallyExternal() const { return false; }
