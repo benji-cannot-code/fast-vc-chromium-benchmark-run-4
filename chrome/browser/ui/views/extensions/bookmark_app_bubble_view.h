227ed3df538140c8a79c04295423745bcd542212
calamity@chromium.org <calamity@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-09 02:49:41 +0000

    Add icon to the streamlined hosted app creation dialog.
    This CL adds an ImageView that displays the icon to the
    BookmarkAppBubbleView. It also moves the "Open as tab" checkbox to the
    same line as the buttons to save on vertical space which was consumed
    by the square icon.
    
    BUG=318607
    
    Review URL: https://codereview.chromium.org/184213006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@255810 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/extensions/bookmark_app_bubble_view.h
+++ chrome/browser/ui/views/extensions/bookmark_app_bubble_view.h
@@ -8,14 +8,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/strings/string16.h"
+#include "chrome/browser/extensions/app_icon_loader.h"
 #include "chrome/common/web_application_info.h"
 #include "ui/views/bubble/bubble_delegate.h"
 #include "ui/views/controls/button/button.h"
 
 class Profile;
 
+namespace extensions {
+class AppIconLoader;
+}
+
+namespace gfx {
+class ImageSkia;
+}
+
 namespace views {
 class Checkbox;
+class ImageView;
 class LabelButton;
 class Textfield;
 }
@@ -25,7 +35,8 @@ class Textfield;
 // is created with. Don't create a BookmarkAppBubbleView directly, instead use
 // the static ShowBubble method.
 class BookmarkAppBubbleView : public views::BubbleDelegateView,
-                              public views::ButtonListener {
+                              public views::ButtonListener,
+                              public extensions::AppIconLoader::Delegate {
  public:
   virtual ~BookmarkAppBubbleView();
 
@@ -57,6 +68,10 @@ class BookmarkAppBubbleView : public views::BubbleDelegateView,
   virtual void ButtonPressed(views::Button* sender,
                              const ui::Event& event) OVERRIDE;
 
+  // Overridden from extensions::AppIconLoader::Delegate:
+  virtual void SetAppImage(const std::string& id,
+                           const gfx::ImageSkia& image) OVERRIDE;
+
   // Handle the message when the user presses a button.
   void HandleButtonPressed(views::Button* sender);
 
@@ -87,8 +102,14 @@ class BookmarkAppBubbleView : public views::BubbleDelegateView,
   // Textfield showing the title of the app.
   views::Textfield* title_tf_;
 
+  // Image showing the icon of the app.
+  views::ImageView* icon_image_view_;
+
   // When the destructor is invoked should the app be removed?
   bool remove_app_;
+
+  // Used to load the icon.
+  scoped_ptr<extensions::AppIconLoader> app_icon_loader_;
 
   DISALLOW_COPY_AND_ASSIGN(BookmarkAppBubbleView);
 };
