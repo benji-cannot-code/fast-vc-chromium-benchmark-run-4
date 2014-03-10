6ca355c6cdfeb77b9c028293dbf7f3f600aa4d7b
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-10 20:51:51 +0000

    Clean up usage of MediaCodecBridge/MediaDrmBridge::IsAvailable().
    - All static methods in MediaCodecBridge and MediaDrmBridge check IsAvailable()
      internally so that callers don't need to check IsAvailable() before calling
      these static methods.
    - Remove MediaSourcePlayer::IsTypeSupported() because it's obsolete now.
    - Add MediaDrmBridge unittests.
    
    BUG=303864
    
    Review URL: https://codereview.chromium.org/189163003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256032 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/media/android_video_decode_accelerator.cc
+++ content/common/gpu/media/android_video_decode_accelerator.cc
@@ -87,9 +87,6 @@ bool AndroidVideoDecodeAccelerator::Initialize(media::VideoCodecProfile profile,
 
   client_ = client;
 
-  if (!media::MediaCodecBridge::IsAvailable())
-    return false;
-
   if (profile == media::VP8PROFILE_MAIN) {
     codec_ = media::kCodecVP8;
   } else {
