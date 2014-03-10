d638349cbcd59f36f65c2687b2492b5b2d4ac79f
morrita@chromium.org <morrita@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-10 22:17:00 +0000

    Rename allowCookies to allowStoredCredentials
    And remove an redundant flag from WebURLRequest.
    These two represent the same thing thus should be merged.
    
    TEST=none
    BUG=none
    R=japhet@chromium.org, abarth@chromium.org, darin@chromium.org
    
    Review URL: https://codereview.chromium.org/189903002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168867 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/network/ResourceRequest.h
+++ third_party/WebKit/Source/platform/network/ResourceRequest.h
@@ -158,8 +158,8 @@ public:
     FormData* httpBody() const;
     void setHTTPBody(PassRefPtr<FormData> httpBody);
 
-    bool allowCookies() const;
-    void setAllowCookies(bool allowCookies);
+    bool allowStoredCredentials() const;
+    void setAllowStoredCredentials(bool allowCredentials);
 
     ResourceLoadPriority priority() const;
     void setPriority(ResourceLoadPriority);
@@ -225,7 +225,7 @@ private:
     AtomicString m_httpMethod;
     HTTPHeaderMap m_httpHeaderFields;
     RefPtr<FormData> m_httpBody;
-    bool m_allowCookies : 1;
+    bool m_allowStoredCredentials : 1;
     bool m_reportUploadProgress : 1;
     bool m_reportLoadTiming : 1;
     bool m_reportRawHeaders : 1;
@@ -260,7 +260,7 @@ public:
     String m_httpMethod;
     OwnPtr<CrossThreadHTTPHeaderMapData> m_httpHeaders;
     RefPtr<FormData> m_httpBody;
-    bool m_allowCookies;
+    bool m_allowStoredCredentials;
     bool m_reportUploadProgress;
     bool m_hasUserGesture;
     bool m_downloadToFile;
