367fcb4c3b4bbe8a3cb5cd76a9f9a19a4cbf75b4
hajimehoshi@chromium.org <hajimehoshi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-10 20:09:25 +0000

    Bug fix: Combobox: Focus on clicking mouse button
    This CL fixes the regression that Combobox doesn't get focused when
    clicking a right button of a mouse. Before introducing
    TransparentButton, Combobox can get focus by itself on the mouse button
    events, but now, TranparentButton robs the combobox of the events.
    This CL has TransparentButton::OnMousePressed help the combobox with
    focusing it.
    
    BUG=350811
    TEST=manual
    
    Review URL: https://codereview.chromium.org/189913014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256016 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/combobox/combobox.cc
+++ ui/views/controls/combobox/combobox.cc
@@ -92,6 +92,11 @@ class TransparentButton : public CustomButton {
   }
   virtual ~TransparentButton() {}
 
+  virtual bool OnMousePressed(const ui::MouseEvent& mouse_event) OVERRIDE {
+    parent()->RequestFocus();
+    return true;
+  }
+
   double GetAnimationValue() const {
     return hover_animation_->GetCurrentValue();
   }
