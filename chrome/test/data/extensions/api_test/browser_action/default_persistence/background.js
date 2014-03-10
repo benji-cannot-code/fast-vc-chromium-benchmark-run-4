48cbcb9f134fe04aa7721d69e5d981cdcd922ace
rdevlin.cronin@chromium.org <rdevlin.cronin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-10 23:25:11 +0000

    Resubmit "Persist browseraction properties across restarts"
    Resubmit CL after it broke tests. See comments for the fix.
    
    ------------------------------------------
    Persist browseraction properties across restarts
    Persist browser action properties across restarts even if other properties are modified before the old ones are loaded. (Note: only properties which are NOT modified are persisted.)
    BUG=348775
    
    Review URL: https://codereview.chromium.org/191263002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256065 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/api_test/browser_action/default_persistence/background.js
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Set a background color for the badge at OnInstalled().
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.browserAction.setBadgeBackgroundColor({color: [0, 0, 255, 255]});
+  chrome.test.sendMessage('Background Color Set');
+});
+
+// Every startup, change the badge text, but not the background color (which
+// should persist at the blue we set originally).
+chrome.runtime.onStartup.addListener(function() {
+  chrome.browserAction.setBadgeText({text: 'Hello'});
+  chrome.test.sendMessage('Badge Text Set');
+});
