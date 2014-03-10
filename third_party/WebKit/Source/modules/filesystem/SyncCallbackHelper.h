7a40a76839192101e4f6540054fabbc3c052efda
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-10 03:53:15 +0000

    Reland r168780
    r168780 was reverted in r168786 because it broke all oilpan builds.
    This CL relands r168780 with the oilpan-side fix.
    
    TBR=hashimoto
    
    Review URL: https://codereview.chromium.org/190753008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168810 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/filesystem/SyncCallbackHelper.h
+++ third_party/WebKit/Source/modules/filesystem/SyncCallbackHelper.h
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/fileapi/FileError.h"
 #include "core/html/VoidCallback.h"
 #include "modules/filesystem/DirectoryEntry.h"
-#include "modules/filesystem/DirectoryReaderSync.h"
 #include "modules/filesystem/EntriesCallback.h"
 #include "modules/filesystem/EntryCallback.h"
 #include "modules/filesystem/EntrySync.h"
@@ -58,22 +57,6 @@ struct HelperResultType {
     static ReturnType createFromCallbackArg(CallbackArg argument)
     {
         return ResultType::create(argument);
-    }
-};
-
-template <>
-struct HelperResultType<EntrySyncVector, const EntryVector&> {
-    typedef EntrySyncVector ReturnType;
-    typedef EntrySyncVector StorageType;
-
-    static EntrySyncVector createFromCallbackArg(const EntryVector& entries)
-    {
-        EntrySyncVector result;
-        size_t entryCount = entries.size();
-        result.reserveInitialCapacity(entryCount);
-        for (size_t i = 0; i < entryCount; ++i)
-            result.uncheckedAppend(EntrySync::create(entries[i].get()));
-        return result;
     }
 };
 
@@ -176,7 +159,6 @@ struct EmptyType : public RefCounted<EmptyType> {
 };
 
 typedef SyncCallbackHelper<EntryCallback, Entry*, EntrySync> EntrySyncCallbackHelper;
-typedef SyncCallbackHelper<EntriesCallback, const EntryVector&, EntrySyncVector> EntriesSyncCallbackHelper;
 typedef SyncCallbackHelper<MetadataCallback, Metadata*, Metadata> MetadataSyncCallbackHelper;
 typedef SyncCallbackHelper<VoidCallback, EmptyType*, EmptyType> VoidSyncCallbackHelper;
 typedef SyncCallbackHelper<FileSystemCallback, DOMFileSystem*, DOMFileSystemSync> FileSystemSyncCallbackHelper;
