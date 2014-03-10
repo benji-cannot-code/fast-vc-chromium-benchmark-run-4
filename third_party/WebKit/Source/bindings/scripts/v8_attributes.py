918b231ac91ffdd93ffa47a6d895018bdfb00c8c
nbarth@chromium.org <nbarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-10 11:14:10 +0000

    Refactor IDL type handling into idl_types.py
    Preliminary step in moving to using IdlType objects for types,
    instead of strings, to allow more complex and flexible type handling.
    
    Ultimately we will be able to have nested types, and type-handling functions
    will be methods on the IdlType object, which simplifies the interface.
    
    
    Background:
    Currently IDL type handling is spread across idl_definitions.py
    and v8_types.idl, and uses strings internally (except for union types).
    
    We need more complex type handling, e.g.,
    a nullable union of types including a callback function.
    (for Web Animations)
    ...and want to move nullability into the type (instead of attribute,
    operation, or argument), e.g., for use by Closure and for type-checking
    (and for correctness, b/c nullability is a property of the type).
    
    Conveniently, I've written an existing IdlType class, b/c we need this
    flexibility for typedef resolution.
    
    This is a preliminary CL, just moving the code into one module; it's just:
    * move 2 files into 1
    * update calls
    
    In a followup, I'll replace the existing string handling with objects,
    reusing the existing class.
    (Again, this will be primarily reorganization and updating the calls;
    once it's OO, we won't need all the imports and namespaces
    on the functions!)
    After that I'll start adding features, like moving nullability into the type.
    
    BUG=345137
    BUG=345503
    R=haraken
    
    Review URL: https://codereview.chromium.org/192553002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168831 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/scripts/v8_attributes.py
+++ third_party/WebKit/Source/bindings/scripts/v8_attributes.py
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Design doc: http://www.chromium.org/developers/design-documents/idl-compiler
 """
 
+import idl_types
 from v8_globals import includes, interfaces
 import v8_types
 import v8_utilities
@@ -111,7 +112,7 @@ def generate_attribute(interface, attribute):
         'is_setter_raises_exception': is_setter_raises_exception,
         'has_setter_exception_state': (
             is_setter_raises_exception or has_strict_type_checking or
-            v8_types.is_integer_type(idl_type)),
+            idl_types.is_integer_type(idl_type)),
         'is_static': attribute.is_static,
         'is_url': 'URL' in extended_attributes,
         'is_unforgeable': 'Unforgeable' in extended_attributes,
@@ -168,7 +169,7 @@ def generate_getter(interface, attribute, contents):
         contents['cpp_value_original'] = cpp_value
         cpp_value = 'jsValue'
         # EventHandler has special handling
-        if idl_type != 'EventHandler' and v8_types.is_interface_type(idl_type):
+        if idl_type != 'EventHandler' and idl_types.is_interface_type(idl_type):
             release = True
 
     if 'ReflectOnly' in extended_attributes:
@@ -243,8 +244,8 @@ def is_keep_alive_for_gc(interface, attribute):
          # There are some exceptions, however:
          not(
              # Node lifetime is managed by object grouping.
-             v8_types.inherits_interface(interface.name, 'Node') or
-             v8_types.inherits_interface(idl_type, 'Node') or
+             idl_types.inherits_interface(interface.name, 'Node') or
+             idl_types.inherits_interface(idl_type, 'Node') or
              # A self-reference is unnecessary.
              attribute.name == 'self' or
              # FIXME: Remove these hard-coded hacks.
@@ -304,7 +305,7 @@ def setter_expression(interface, attribute, contents):
             arguments.append('V8EventListenerList::findOrCreateWrapper<V8ErrorHandler>(jsValue, true, info.GetIsolate())')
         else:
             arguments.append('V8EventListenerList::getEventListener(jsValue, true, ListenerFindOrCreate)')
-    elif v8_types.is_interface_type(idl_type) and not v8_types.array_type(idl_type):
+    elif idl_types.is_interface_type(idl_type) and not idl_types.array_type(idl_type):
         # FIXME: should be able to eliminate WTF::getPtr in most or all cases
         arguments.append('WTF::getPtr(cppValue)')
     else:
