d2be20cf02924baf668bfc7801b1253d2a9df40f
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-10 22:10:25 +0000

    Add missing forward declare for WebFrame.
    I removed a couple forward declares that were no longer needed in Blink,
    and it caused the chrome build to fail.
    
    BUG=none
    R=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/190953010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256045 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/renderer/test_runner/TestPlugin.h
+++ content/shell/renderer/test_runner/TestPlugin.h
@@ -16,6 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/web/WebPlugin.h"
 #include "third_party/WebKit/public/web/WebPluginContainer.h"
 
+namespace blink {
+class WebFrame;
+}  // namespace blink
+
 namespace WebTestRunner {
 
 class WebTestDelegate;
