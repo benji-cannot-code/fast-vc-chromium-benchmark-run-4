805fc96e7875449dcd829104183f5648e49053aa
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-10 23:24:16 +0000

    Remove --prompt-for-extension-extensions switch. FeatureSwitch now can accept NULL switch names, in which case it can't be controlled by the command line.
    BUG=343979
    
    Review URL: https://codereview.chromium.org/189993004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256064 0039d316-1c4b-4281-b951-d872f2087c98

--- extensions/common/feature_switch.cc
+++ extensions/common/feature_switch.cc
@@ -28,7 +28,7 @@ class CommonSwitches {
             switches::kGlobalCommands,
             FeatureSwitch::DEFAULT_DISABLED),
         prompt_for_external_extensions(
-            switches::kPromptForExternalExtensions,
+            NULL,
 #if defined(OS_WIN)
             FeatureSwitch::DEFAULT_ENABLED),
 #else
@@ -109,6 +109,9 @@ bool FeatureSwitch::IsEnabled() const {
   if (override_value_ != OVERRIDE_NONE)
     return override_value_ == OVERRIDE_ENABLED;
 
+  if (!switch_name_)
+    return default_value_;
+
   std::string temp = command_line_->GetSwitchValueASCII(switch_name_);
   std::string switch_value;
   base::TrimWhitespaceASCII(temp, base::TRIM_ALL, &switch_value);
@@ -129,10 +132,12 @@ bool FeatureSwitch::IsEnabled() const {
 }
 
 std::string FeatureSwitch::GetLegacyEnableFlag() const {
+  DCHECK(switch_name_);
   return std::string("enable-") + switch_name_;
 }
 
 std::string FeatureSwitch::GetLegacyDisableFlag() const {
+  DCHECK(switch_name_);
   return std::string("disable-") + switch_name_;
 }
 
--- extensions/common/switches.h
+++ extensions/common/switches.h
@@ -25,7 +25,6 @@ extern const char kExtensionProcess[];
 extern const char kExtensionsOnChromeURLs[];
 extern const char kForceDevModeHighlighting[];
 extern const char kGlobalCommands[];
-extern const char kPromptForExternalExtensions[];
 extern const char kShowComponentExtensionOptions[];
 
 }  // namespace switches
