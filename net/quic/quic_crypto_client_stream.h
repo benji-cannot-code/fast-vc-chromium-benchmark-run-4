1b025b959feb75d17312d137821ba8be0853546c
rtenneti@chromium.org <rtenneti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-10 06:36:58 +0000

    QUIC - histograms to measure the time spent to read QUIC sever information from disk cache.
    R=rch@chromium.org
    
    Review URL: https://codereview.chromium.org/189963002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@255887 0039d316-1c4b-4281-b951-d872f2087c98

--- net/quic/quic_crypto_client_stream.h
+++ net/quic/quic_crypto_client_stream.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/memory/weak_ptr.h"
+#include "base/time/time.h"
 #include "net/cert/cert_verify_result.h"
 #include "net/cert/x509_certificate.h"
 #include "net/quic/crypto/proof_verifier.h"
@@ -135,6 +136,10 @@ class NET_EXPORT_PRIVATE QuicCryptoClientStream : public QuicCryptoStream {
   // This member is used to store the result of an asynchronous disk cache read.
   // It must not be used after STATE_LOAD_QUIC_SERVER_INFO_COMPLETE.
   int disk_cache_load_result_;
+
+  // Time when call to WaitForDataReady was made, used for computing time spent
+  // to read QUIC server information from disk cache.
+  base::TimeTicks read_start_time_;
 
   base::WeakPtrFactory<QuicCryptoClientStream> weak_factory_;
 
