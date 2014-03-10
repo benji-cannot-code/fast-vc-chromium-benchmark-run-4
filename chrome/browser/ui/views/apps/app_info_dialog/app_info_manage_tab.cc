89267c64cebf18b9f857260005dce007f910280f
sashab@chromium.org <sashab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-10 06:20:20 +0000

    Added tabs to the App Info dialog, and removed the title.
    Removed the title to make the tabs start at the very top of the dialog.
    
    Has 3 tabs: Summary, Permissions and Manage
    - The Summary tab displays the app's icon, name, version and description.
    - The Permissions tab displays the app's permissions as a scrollable list
    - The Manage tab is currently empty.
    
    BUG=266739
    
    Review URL: https://codereview.chromium.org/180723010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@255880 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/ui/views/apps/app_info_dialog/app_info_manage_tab.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/apps/app_info_dialog/app_info_manage_tab.h"
+
+#include "ui/gfx/native_widget_types.h"
+
+AppInfoManageTab::AppInfoManageTab(gfx::NativeWindow parent_window,
+                                   Profile* profile,
+                                   const extensions::Extension* app,
+                                   const base::Closure& close_callback)
+    : AppInfoTab(parent_window, profile, app, close_callback) {
+  // TODO(sashab): Populate this tab.
+}
+
+AppInfoManageTab::~AppInfoManageTab() {}
