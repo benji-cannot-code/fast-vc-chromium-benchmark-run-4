c449756695fd0ffa791c09f18c1335526a5a192c
akuegel@chromium.org <akuegel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-10 17:58:15 +0000

    After a supervised user has been imported, close overlays.
    When successfully importing a supervised user, close both
    the supervised user import overlay and the create new
    profile overlay.
    
    BUG=343723
    
    Review URL: https://codereview.chromium.org/192573003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@255967 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/managed_user_import.js
+++ chrome/browser/resources/options/managed_user_import.js
@@ -227,7 +227,7 @@ cr.define('options', function() {
     onSuccess_: function() {
       this.updateImportInProgress_(false);
       options.ManagedUserListData.resetPromise();
-      OptionsPage.closeOverlay();
+      OptionsPage.closeAllOverlays();
     },
   };
 
