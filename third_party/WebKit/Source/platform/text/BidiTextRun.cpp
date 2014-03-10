47710a3965031acf6d39b56341b74afeed678a49
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-10 16:58:34 +0000

    Change BreakingContext to use right dir for width
    BreakingContext::handleText currently uses the unicode direction from
    the BidiResolver. This works fine for characters with strong
    directionality but fails for all other directionalities as the dir
    value has not yet been resolved (by the UBA).
    Instead change it to use the version of constructTextRun that
    resolved the directionality from the text content.
    
    TEST=fast/text/international/rtl-text-wrapping.html
    BUG=348554
    R=dglazkov@chromium.org
    
    Review URL: https://codereview.chromium.org/190623003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168841 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/text/BidiTextRun.cpp
+++ third_party/WebKit/Source/platform/text/BidiTextRun.cpp
@@ -37,28 +37,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-static inline TextDirection directionForRunInternal(TextRun& run, bool& hasStrongDirectionality)
+TextDirection directionForRun(TextRun& run, bool& hasStrongDirectionality)
 {
     BidiResolver<TextRunIterator, BidiCharacterRun> bidiResolver;
     bidiResolver.setStatus(BidiStatus(run.direction(), run.directionalOverride()));
     bidiResolver.setPositionIgnoringNestedIsolates(TextRunIterator(&run, 0));
-    return bidiResolver.determineParagraphDirectionality(&
-    hasStrongDirectionality);
+    return bidiResolver.determineParagraphDirectionality(&hasStrongDirectionality);
 }
 
 TextDirection determineDirectionality(const String& value, bool& hasStrongDirectionality)
 {
     TextRun run(value);
-    return directionForRunInternal(run, hasStrongDirectionality);
+    return directionForRun(run, hasStrongDirectionality);
 }
 
 TextRun textRunWithDirectionality(const String& value, bool& hasStrongDirectionality)
 {
     TextRun run(value);
-    TextDirection direction = directionForRunInternal(run, hasStrongDirectionality);
+    TextDirection direction = directionForRun(run, hasStrongDirectionality);
     if (hasStrongDirectionality)
         run.setDirection(direction);
     return run;
 }
 
-}
+} // namespace WebCore
+
--- third_party/WebKit/Source/platform/text/BidiTextRun.h
+++ third_party/WebKit/Source/platform/text/BidiTextRun.h
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+PLATFORM_EXPORT TextDirection directionForRun(TextRun&, bool& hasStrongDirectionality);
 PLATFORM_EXPORT TextDirection determineDirectionality(const String& value, bool& hasStrongDirectionality);
 PLATFORM_EXPORT TextRun textRunWithDirectionality(const String& value, bool& hasStrongDirectionality);
 
