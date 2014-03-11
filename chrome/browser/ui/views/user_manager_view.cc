6e4137622c3d7c26683fc865a2cd23a51c834492
dmichael@chromium.org <dmichael@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-11 20:21:38 +0000

    Revert 256244 "Added code to call CloseAsh in place of closing s..."
    Reason for revert:
    This CL appears to have broken 
    NewAvatarMenuButtonTest.SignOut 
    ...on Win 7 Tests. 
    Log: 
    http://build.chromium.org/p/chromium.win/builders/Win7%20Tests%20%28dbg%29%283%29/builds/25574/steps/browser_tests/logs/stdio
    
    
    > Added code to call CloseAsh in place of closing secondary widgets on Windows.
    > Here is what happens in attached bug:
    > 1. UserManagerView (Profile view) adds a reference to browser process by calling StartKeepAlive.
    > 2. UserManagerView is a secondary widget.
    > 3. Current code for windows does EnumWindows for closing all secondary widgets in CloseAllSecondaryWidgets, which works only for desktop mode.
    > 4. In Chrome on ASH EnumWindows doesn't do anything as they are not regular windows.
    > 5. Because UserManagerView doesn't get chance to close through CloseAllSecondaryWidgets, it doesn't release reference and we browser process keeps running.
    > 
    > CloseAsh closes all child widgets, which might be a overkill but works for now.
    > 
    > BUG=340967
    > R=cpu,sky,ananta
    > 
    > Review URL: https://codereview.chromium.org/173403002
    
    TBR=shrikant@chromium.org
    
    Review URL: https://codereview.chromium.org/195553003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256297 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/user_manager_view.cc
+++ chrome/browser/ui/views/user_manager_view.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_dialogs.h"
 #include "chrome/browser/ui/browser_window.h"
-#include "chrome/browser/ui/views/auto_keep_alive.h"
 #include "chrome/common/url_constants.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_view.h"
@@ -66,10 +65,14 @@ UserManagerView::UserManagerView(Profile* profile)
 }
 
 UserManagerView::~UserManagerView() {
+  chrome::DecrementKeepAliveCount();  // Remove shutdown prevention.
 }
 
 // static
 void UserManagerView::Show(const base::FilePath& profile_path_to_focus) {
+  // Prevent the browser process from shutting down while this window is open.
+  chrome::IncrementKeepAliveCount();
+
   ProfileMetrics::LogProfileSwitchUser(ProfileMetrics::OPEN_USER_MANAGER);
   if (instance_) {
     // If there's a user manager window open already, just activate it.
@@ -109,9 +112,6 @@ void UserManagerView::OnGuestProfileCreated(
 
   instance_ = new UserManagerView(guest_profile);
   DialogDelegate::CreateDialogWidget(instance_, NULL, NULL);
-
-  gfx::NativeWindow window = instance_->GetWidget()->GetNativeWindow();
-  instance_->keep_alive_.reset(new AutoKeepAlive(window));
 
 #if defined(OS_WIN)
   // Set the app id for the task manager to the app id of its parent
