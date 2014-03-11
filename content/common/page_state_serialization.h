587224119fd79472bc7c18f201c02e578ac5938c
japhet@chromium.org <japhet@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-11 00:51:43 +0000

    Don't serialize frame id or original url history parameters. They are obsolete.
    BUG=341425,340277
    TEST=PageStateSerializationTest.BackwardsCompat_v18
    
    Review URL: https://codereview.chromium.org/192813004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256089 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/page_state_serialization.h
+++ content/common/page_state_serialization.h
@@ -44,7 +44,6 @@ struct CONTENT_EXPORT ExplodedHttpBody {
 
 struct CONTENT_EXPORT ExplodedFrameState {
   base::NullableString16 url_string;
-  base::NullableString16 original_url_string;
   base::NullableString16 referrer;
   base::NullableString16 target;
   base::NullableString16 state_object;
@@ -52,7 +51,6 @@ struct CONTENT_EXPORT ExplodedFrameState {
   gfx::Point scroll_offset;
   int64 item_sequence_number;
   int64 document_sequence_number;
-  int64 target_frame_id;
   double page_scale_factor;
   blink::WebReferrerPolicy referrer_policy;
   ExplodedHttpBody http_body;
