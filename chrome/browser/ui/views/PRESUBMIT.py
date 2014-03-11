618e260592c1231adb23110e7631ee54defa1eac
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-11 21:45:27 +0000

    Convert most Chromium presubmit files to use new multiple-tryserver-compatible protocol
    BUG=334892
    R=maruel@chromium.org
    
    Review URL: https://codereview.chromium.org/194903007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256318 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/PRESUBMIT.py
+++ chrome/browser/ui/views/PRESUBMIT.py
@@ -9,5 +9,9 @@ See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
 for more details on the presubmit API built into gcl.
 """
 
-def GetPreferredTrySlaves():
-  return ['linux_chromeos']
+def GetPreferredTryMasters(project, change):
+  return {
+    'tryserver.chromium': {
+      'linux_chromeos': set(['defaulttests']),
+    }
+  }
