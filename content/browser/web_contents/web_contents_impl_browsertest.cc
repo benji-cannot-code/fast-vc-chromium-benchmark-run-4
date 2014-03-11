4bddd85de2f4b6d4ae55857b5b71696b17da937e
erikchen@chromium.org <erikchen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-11 01:01:56 +0000

    mac: Fix a bug where navigating away from NTP flashes a grey bar.
    This is the mac implementation for a fix written by kuan@ at
    crrev.com/21843003.
    
    The existing mac layout code relies on an assumption that the RWHV always has
    the same size as the WCV. Any time the size of the WCV changes, the
    relative change is propagated to the RWHV. This CL enables kuan's NTP fix,
    which prematurely increases the size of the RWHV before the WCV size is
    updated. When the size of the WCV changes, instead of propagating a relative
    size change to the RWHV, the WCV resets the size of the RWHV to its own size.
    
    This change gives SizeContents a no-op implementation, as the existing
    implementation has never worked. In the previous implementation, it was
    possible for the contents module to change the size of the wcv (that's the
    whole point of SizeContents). However, the chrome embedder makes the assumption
    that it is the only one to set the size of the wcv, and all size changes are
    propagated down as relative size changes. If SizeContents were to change the
    size of the wcv, the size of the wcv would forever be out of sync.
    
    BUG=264207, 317760
    
    Review URL: https://codereview.chromium.org/183833006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256090 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/web_contents/web_contents_impl_browsertest.cc
+++ content/browser/web_contents/web_contents_impl_browsertest.cc
@@ -32,8 +32,7 @@ void ResizeWebContentsView(Shell* shell, const gfx::Size& size,
   // works on Win and ChromeOS but not Linux - we need to resize the shell
   // window on Linux because if we don't, the next layout of the unchanged shell
   // window will resize WebContentsView back to the previous size.
-  // The cleaner and shorter SizeContents is preferred as more platforms convert
-  // to Aura.
+  // SizeContents is a hack and should not be relied on.
 #if defined(TOOLKIT_GTK) || defined(OS_MACOSX)
   shell->SizeTo(size);
   // If |set_start_page| is true, start with blank page to make sure resize
@@ -232,11 +231,9 @@ IN_PROC_BROWSER_TEST_F(WebContentsImplBrowserTest,
             shell()->web_contents()->GetVisibleURL());
 }
 
-// TODO(sail): enable this for MAC when auto resizing of WebContentsViewCocoa is
-// fixed.
 // TODO(shrikant): enable this for Windows when issue with
 // force-compositing-mode is resolved (http://crbug.com/281726).
-#if defined(OS_WIN) || defined(OS_MACOSX) || defined(OS_ANDROID)
+#if defined(OS_WIN) || defined(OS_ANDROID)
 #define MAYBE_GetSizeForNewRenderView DISABLED_GetSizeForNewRenderView
 #else
 #define MAYBE_GetSizeForNewRenderView GetSizeForNewRenderView
@@ -258,7 +255,7 @@ IN_PROC_BROWSER_TEST_F(WebContentsImplBrowserTest,
   ASSERT_TRUE(shell()->web_contents()->GetDelegate() == delegate.get());
 
   // When no size is set, RenderWidgetHostView adopts the size of
-  // WebContenntsView.
+  // WebContentsView.
   NavigateToURL(shell(), embedded_test_server()->GetURL("/title2.html"));
   EXPECT_EQ(shell()->web_contents()->GetView()->GetContainerSize(),
             shell()->web_contents()->GetRenderWidgetHostView()->GetViewBounds().
@@ -267,7 +264,7 @@ IN_PROC_BROWSER_TEST_F(WebContentsImplBrowserTest,
   // When a size is set, RenderWidgetHostView and WebContentsView honor this
   // size.
   gfx::Size size(300, 300);
-  gfx::Size size_insets(-10, -15);
+  gfx::Size size_insets(10, 15);
   ResizeWebContentsView(shell(), size, true);
   delegate->set_size_insets(size_insets);
   NavigateToURL(shell(), https_server.GetURL("/"));
@@ -275,14 +272,23 @@ IN_PROC_BROWSER_TEST_F(WebContentsImplBrowserTest,
   EXPECT_EQ(size,
             shell()->web_contents()->GetRenderWidgetHostView()->GetViewBounds().
                 size());
-  EXPECT_EQ(size, shell()->web_contents()->GetView()->GetContainerSize());
+  // The web_contents size is set by the embedder, and should not depend on the
+  // rwhv size. The behavior is correct on OSX, but incorrect on other
+  // platforms.
+  gfx::Size exp_wcv_size(300, 300);
+#if !defined(OS_MACOSX)
+  exp_wcv_size.Enlarge(size_insets.width(), size_insets.height());
+#endif
+
+  EXPECT_EQ(exp_wcv_size,
+            shell()->web_contents()->GetView()->GetContainerSize());
 
   // If WebContentsView is resized after RenderWidgetHostView is created but
   // before pending navigation entry is committed, both RenderWidgetHostView and
   // WebContentsView use the new size of WebContentsView.
   gfx::Size init_size(200, 200);
   gfx::Size new_size(100, 100);
-  size_insets = gfx::Size(-20, -30);
+  size_insets = gfx::Size(20, 30);
   ResizeWebContentsView(shell(), init_size, true);
   delegate->set_size_insets(size_insets);
   RenderViewSizeObserver observer(shell(), new_size);
@@ -290,13 +296,17 @@ IN_PROC_BROWSER_TEST_F(WebContentsImplBrowserTest,
   // RenderWidgetHostView is created at specified size.
   init_size.Enlarge(size_insets.width(), size_insets.height());
   EXPECT_EQ(init_size, observer.rwhv_create_size());
-  // RenderViewSizeObserver resizes WebContentsView in
-  // DidStartNavigationToPendingEntry, so both WebContentsView and
-  // RenderWidgetHostView adopt this new size.
+
+// Once again, the behavior is correct on OSX. The embedder explicitly sets
+// the size to (100,100) during navigation. Both the wcv and the rwhv should
+// take on that size.
+#if !defined(OS_MACOSX)
   new_size.Enlarge(size_insets.width(), size_insets.height());
-  EXPECT_EQ(new_size,
-            shell()->web_contents()->GetRenderWidgetHostView()->GetViewBounds().
-                size());
+#endif
+  gfx::Size actual_size = shell()->web_contents()->GetRenderWidgetHostView()->
+      GetViewBounds().size();
+
+  EXPECT_EQ(new_size, actual_size);
   EXPECT_EQ(new_size, shell()->web_contents()->GetView()->GetContainerSize());
 }
 
