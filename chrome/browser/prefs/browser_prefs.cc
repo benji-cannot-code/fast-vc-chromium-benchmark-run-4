8703f56b2c1302430d773cf472c7c99bdae16ee0
guohui@chromium.org <guohui@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-11 02:49:42 +0000

    Implement native mirror tutorial with views
    A new tutorial card is added to the avatar menu bubble for signed-in user. It shows up to 5 times until user clicks 'ok got it' button. For details, including mock and snapshot, please refer to the attached bug.
    
    BUG=324051
    
    Review URL: https://codereview.chromium.org/190803002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256101 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -479,6 +479,10 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
       std::string(),
       user_prefs::PrefRegistrySyncable::UNSYNCABLE_PREF);
 #endif
+
+#if !defined(OS_ANDROID) && !defined(OS_CHROMEOS)
+  RegisterNewProfileUIPrefs(registry);
+#endif
 }
 
 void RegisterUserProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
@@ -498,6 +502,15 @@ void RegisterLoginProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
   RegisterProfilePrefs(registry);
 
   chromeos::PowerPrefs::RegisterLoginProfilePrefs(registry);
+}
+#endif
+
+#if !defined(OS_ANDROID) && !defined(OS_CHROMEOS)
+void RegisterNewProfileUIPrefs(user_prefs::PrefRegistrySyncable* registry) {
+  registry->RegisterIntegerPref(
+      prefs::kProfileAvatarTutorialShown,
+      0,
+      user_prefs::PrefRegistrySyncable::UNSYNCABLE_PREF);
 }
 #endif
 
