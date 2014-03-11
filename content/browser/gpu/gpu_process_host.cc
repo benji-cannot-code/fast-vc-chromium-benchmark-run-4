c1cccde82edd7db82aba4310b2e06a16c36ff82b
jorgelo@chromium.org <jorgelo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-11 18:24:50 +0000

    Implement '--gpu-sandbox-failures-fatal' flag.
    BUG=327824
    TEST=Chrome compiles and loads with all flag combinations, sandbox is enabled.
    
    Review URL: https://codereview.chromium.org/191313005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256276 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -49,10 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/switches.h"
 #endif
 
-#if defined(OS_CHROMEOS)
-#include "chromeos/chromeos_switches.h"
-#endif
-
 #if defined(USE_OZONE)
 #include "ui/ozone/ozone_switches.h"
 #endif
@@ -1096,6 +1092,7 @@ bool GpuProcessHost::LaunchGpuProcess(const std::string& channel_id) {
     switches::kEnableShareGroupAsyncTextureUpload,
     switches::kGpuStartupDialog,
     switches::kGpuSandboxAllowSysVShm,
+    switches::kGpuSandboxFailuresFatal,
     switches::kLoggingLevel,
     switches::kNoSandbox,
     switches::kTestGLLib,
@@ -1104,9 +1101,6 @@ bool GpuProcessHost::LaunchGpuProcess(const std::string& channel_id) {
     switches::kVModule,
 #if defined(OS_MACOSX)
     switches::kEnableSandboxLogging,
-#endif
-#if defined(OS_CHROMEOS)
-    chromeos::switches::kGpuSandboxFailuresNonfatal,
 #endif
 #if defined(USE_AURA)
     switches::kUIPrioritizeInGpuProcess,
