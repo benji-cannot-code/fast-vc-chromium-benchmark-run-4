cf73a00d17d3cb7c242107fe5caffb675cf52f46
peter@chromium.org <peter@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-11 17:56:52 +0000

    Notification.permission's default value of "default" should be testable.
    If no permission request has been answered yet, the Notification.permission
    string of the Web Notification API should be set to "default". The
    presenter used for testing can currently only return "allowed" or "denied".
    
    BUG=349015
    
    Review URL: https://codereview.chromium.org/183663021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256273 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/renderer/test_runner/notification_presenter.h
+++ content/shell/renderer/test_runner/notification_presenter.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_SHELL_RENDERER_TEST_RUNNER_NOTIFICATION_PRESENTER_H_
 
 #include <map>
-#include <set>
 #include <string>
 
 #include "base/basictypes.h"
@@ -27,7 +26,7 @@ class NotificationPresenter : public blink::WebNotificationPresenter {
   virtual ~NotificationPresenter();
 
   // Called by the TestRunner to simulate a user granting permission.
-  void GrantPermission(const std::string& origin);
+  void GrantPermission(const std::string& origin, bool permission_granted);
 
   // Called by the TestRunner to simulate a user clicking on a notification.
   bool SimulateClick(const std::string& title);
@@ -55,8 +54,9 @@ class NotificationPresenter : public blink::WebNotificationPresenter {
  private:
   WebTestRunner::WebTestDelegate* delegate_;
 
-  // Set of origins which are allowed to show notifications.
-  std::set<std::string> allowed_origins_;
+  // Map of known origins and whether they are allowed to show notifications.
+  typedef std::map<std::string, bool> KnownOriginMap;
+  KnownOriginMap known_origins_;
 
   // Map of currently active notifications.
   typedef std::map<std::string, blink::WebNotification> ActiveNotificationMap;
