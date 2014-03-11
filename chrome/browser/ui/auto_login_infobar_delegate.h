3fd97bd17e667598705e92207458a822f96e786a
blundell@chromium.org <blundell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-11 11:48:44 +0000

    Move AutoLoginInfoBarDelegate to observing SigninManager
    This change is part of the effort to eliminate Signin-related notifications.
    
    BUG=345632
    R=pkasting@chromium.org
    
    Review URL: https://codereview.chromium.org/189373009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256209 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/auto_login_infobar_delegate.h
+++ chrome/browser/ui/auto_login_infobar_delegate.h
@@ -8,9 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 #include "chrome/browser/infobars/confirm_infobar_delegate.h"
+#include "chrome/browser/signin/signin_manager.h"
 #include "components/auto_login_parser/auto_login_parser.h"
-#include "content/public/browser/notification_observer.h"
-#include "content/public/browser/notification_registrar.h"
 
 class PrefService;
 class Profile;
@@ -21,7 +20,7 @@ class NavigationController;
 
 // This is the actual infobar displayed to prompt the user to auto-login.
 class AutoLoginInfoBarDelegate : public ConfirmInfoBarDelegate,
-                                 public content::NotificationObserver {
+                                 public SigninManagerBase::Observer {
  public:
   struct Params {
     // Information from a parsed header.
@@ -64,20 +63,17 @@ class AutoLoginInfoBarDelegate : public ConfirmInfoBarDelegate,
   virtual bool Accept() OVERRIDE;
   virtual bool Cancel() OVERRIDE;
 
-  // content::NotificationObserver:
-  virtual void Observe(int type,
-                       const content::NotificationSource& source,
-                       const content::NotificationDetails& details) OVERRIDE;
+  // SigninManagerBase::Observer:
+  virtual void GoogleSignedOut(const std::string& username) OVERRIDE;
 
   void RecordHistogramAction(Actions action);
 
   const Params params_;
 
+  Profile* profile_;
+
   // Whether any UI controls in the infobar were pressed or not.
   bool button_pressed_;
-
-  // For listening to the user signing out.
-  content::NotificationRegistrar registrar_;
 
   DISALLOW_COPY_AND_ASSIGN(AutoLoginInfoBarDelegate);
 };
