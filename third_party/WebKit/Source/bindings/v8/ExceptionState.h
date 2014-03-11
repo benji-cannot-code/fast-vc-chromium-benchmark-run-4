2ceaa0c3bf25313b1a01e0e6a3317a2ca2d5ca79
lushnikov@chromium.org <lushnikov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-11 13:12:21 +0000

    DevTools: protocol error responses to return error details.
    This patch adds ExceptionState::message() method to return exception
    message. The method is used to generate protocol error responses.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/190673002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168919 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/ExceptionState.h
+++ third_party/WebKit/Source/bindings/v8/ExceptionState.h
@@ -83,6 +83,7 @@ public:
     void clearException();
 
     ExceptionCode code() const { return m_code; }
+    const String& message() const { return m_message; }
 
     bool throwIfNeeded()
     {
@@ -108,6 +109,7 @@ public:
 protected:
     ExceptionCode m_code;
     Context m_context;
+    String m_message;
     const char* m_propertyName;
     const char* m_interfaceName;
 
