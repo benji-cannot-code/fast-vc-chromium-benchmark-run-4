5e3e6e5725005a62fd24cefc381e60a5ad492d3f
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-11 06:01:34 +0000

    Revert 168880 "Make DOMWrapperWorld::current() callable from wor..."
    A lot of tests are crashing at DOMWrapperWorld::isIsolatedWorld().
    http://build.chromium.org/p/chromium.webkit/builders/WebKit%20Linux/builds/28365
    
    > Make DOMWrapperWorld::current() callable from workers
    > 
    > This CL does the following two things:
    > 
    > - Makes DOMWrapperWorld::current() callable from workers. Now that a world is always stored in PerContextDataHolder::world(), DOMWrapperWorld::current() can just look up the PerContextDataHolder::world().
    > 
    > - Makes DOMWrapperWorld::current() callable even when a window object is being initialized. It's possible that some methods call DOMWrapperWorld::current() while the window is being initialized. To handles the subtle situation, this CL adds DOMWrapperWorld::worldOfInitializingWindow.
    > 
    > 
    > BUG=341032
    > NOTRY=true
    > 
    > Review URL: https://codereview.chromium.org/182903003
    
    TBR=haraken@chromium.org
    
    Review URL: https://codereview.chromium.org/193563003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168886 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/PageScriptDebugServer.cpp
+++ third_party/WebKit/Source/bindings/v8/PageScriptDebugServer.cpp
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 
 #include "V8Window.h"
-#include "bindings/v8/DOMWrapperWorld.h"
 #include "bindings/v8/ScriptController.h"
 #include "bindings/v8/ScriptSourceCode.h"
 #include "bindings/v8/V8Binding.h"
@@ -58,12 +57,8 @@ static LocalFrame* retrieveFrameWithGlobalObjectCheck(v8::Handle<v8::Context> co
     if (context.IsEmpty())
         return 0;
 
-    // FIXME: This is a temporary hack for crbug.com/345014.
-    // Currently it's possible that V8 can trigger Debugger::ProcessDebugEvent for a context
-    // that is being initialized (i.e., inside Context::New() of the context).
-    // We should fix the V8 side so that it won't trigger the event for a half-baked context
-    // because there is no way in the embedder side to check if the context is half-baked or not.
-    if (isMainThread() && DOMWrapperWorld::windowIsBeingInitialized())
+    // Test that context has associated global dom window object.
+    if (!V8WindowShell::contextHasCorrectPrototype(context))
         return 0;
 
     v8::Handle<v8::Value> global = V8Window::findInstanceInPrototypeChain(context->Global(), context->GetIsolate());
