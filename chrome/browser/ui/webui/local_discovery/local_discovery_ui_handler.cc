c3a1c2c74157a913c3403d9e91ebfab0b8022d09
blundell@chromium.org <blundell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-11 03:00:05 +0000

    Move LocalDiscoveryUIHandler to observing SigninManager
    This change is part of the effort to eliminate Signin-related notifications.
    
    BUG=345634
    
    Review URL: https://codereview.chromium.org/189843010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256105 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/local_discovery/local_discovery_ui_handler.cc
+++ chrome/browser/ui/webui/local_discovery/local_discovery_ui_handler.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
-#include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/local_discovery/privet_confirm_api_flow.h"
 #include "chrome/browser/local_discovery/privet_constants.h"
 #include "chrome/browser/local_discovery/privet_device_lister_impl.h"
@@ -27,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/profile_oauth2_token_service.h"
 #include "chrome/browser/signin/profile_oauth2_token_service_factory.h"
-#include "chrome/browser/signin/signin_manager.h"
 #include "chrome/browser/signin/signin_manager_base.h"
 #include "chrome/browser/signin/signin_manager_factory.h"
 #include "chrome/browser/signin/signin_promo.h"
@@ -35,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_tabstrip.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
-#include "content/public/browser/notification_source.h"
 #include "content/public/browser/user_metrics.h"
 #include "content/public/browser/user_metrics.h"
 #include "content/public/browser/web_ui.h"
@@ -77,6 +74,9 @@ LocalDiscoveryUIHandler::LocalDiscoveryUIHandler() : is_visible_(false) {
 }
 
 LocalDiscoveryUIHandler::~LocalDiscoveryUIHandler() {
+  Profile* profile = Profile::FromWebUI(web_ui());
+  SigninManagerFactory::GetInstance()->GetForProfile(profile)
+      ->RemoveObserver(this);
   ResetCurrentRegistration();
   SetIsVisible(false);
 }
@@ -147,12 +147,8 @@ void LocalDiscoveryUIHandler::HandleStart(const base::ListValue* args) {
 
   CheckUserLoggedIn();
 
-  notification_registrar_.RemoveAll();
-  notification_registrar_.Add(this,
-                              chrome::NOTIFICATION_GOOGLE_SIGNIN_SUCCESSFUL,
-                              content::Source<Profile>(profile));
-  notification_registrar_.Add(this, chrome::NOTIFICATION_GOOGLE_SIGNED_OUT,
-                              content::Source<Profile>(profile));
+  SigninManagerFactory::GetInstance()->GetForProfile(profile)
+      ->AddObserver(this);
 }
 
 void LocalDiscoveryUIHandler::HandleIsVisible(const base::ListValue* args) {
@@ -427,18 +423,14 @@ void LocalDiscoveryUIHandler::OnCloudPrintPrinterListUnavailable() {
       "local_discovery.onCloudDeviceListUnavailable");
 }
 
-void LocalDiscoveryUIHandler::Observe(
-    int type,
-    const content::NotificationSource& source,
-    const content::NotificationDetails& details) {
-  switch (type) {
-    case chrome::NOTIFICATION_GOOGLE_SIGNIN_SUCCESSFUL:
-    case chrome::NOTIFICATION_GOOGLE_SIGNED_OUT:
-      CheckUserLoggedIn();
-      break;
-    default:
-      NOTREACHED();
-  }
+void LocalDiscoveryUIHandler::GoogleSigninSucceeded(
+    const std::string& username,
+    const std::string& password) {
+  CheckUserLoggedIn();
+}
+
+void LocalDiscoveryUIHandler::GoogleSignedOut(const std::string& username) {
+  CheckUserLoggedIn();
 }
 
 void LocalDiscoveryUIHandler::SendRegisterError() {
