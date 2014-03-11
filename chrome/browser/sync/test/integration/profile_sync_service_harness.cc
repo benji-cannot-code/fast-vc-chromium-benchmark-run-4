c310c8ed46b418a4f0650470334d0bfc6cfa0b9d
blundell@chromium.org <blundell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-11 12:35:18 +0000

    Move ProfileSyncService away from listening for Signin notifications.
    As part of the effort to remove Signin-related notifications, move
    ProfileSyncService to observe the SigninManager instead.
    
    BUG=345627
    R=zea@chromium.org
    
    Review URL: https://codereview.chromium.org/189163007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256213 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/test/integration/profile_sync_service_harness.cc
+++ chrome/browser/sync/test/integration/profile_sync_service_harness.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/prefs/pref_service.h"
 #include "base/strings/stringprintf.h"
 #include "base/timer/timer.h"
-#include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/invalidation/p2p_invalidation_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/profile_oauth2_token_service.h"
@@ -39,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 #include "components/sync_driver/data_type_controller.h"
-#include "content/public/browser/notification_service.h"
 #include "google_apis/gaia/gaia_constants.h"
 #include "sync/internal_api/public/base/progress_marker_map.h"
 #include "sync/internal_api/public/sessions/sync_session_snapshot.h"
@@ -218,11 +216,7 @@ bool ProfileSyncServiceHarness::SetupSync(
 
   // Authenticate sync client using GAIA credentials.
   service()->signin()->SetAuthenticatedUsername(username_);
-  GoogleServiceSigninSuccessDetails details(username_, password_);
-  content::NotificationService::current()->Notify(
-      chrome::NOTIFICATION_GOOGLE_SIGNIN_SUCCESSFUL,
-      content::Source<Profile>(profile_),
-      content::Details<const GoogleServiceSigninSuccessDetails>(&details));
+  service()->GoogleSigninSucceeded(username_, password_);
 
 #if defined(ENABLE_MANAGED_USERS)
   std::string account_id = profile_->IsManaged() ?
