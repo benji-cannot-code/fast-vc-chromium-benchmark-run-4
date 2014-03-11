eed833ed9d3df8ca74f39d0738ef72af9f6bf6fa
sivachandra@chromium.org <sivachandra@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-11 20:17:41 +0000

    [chromeos/about:power] Collect and display cpuidle and cpufreq stats
    Salient changes introduced by this CL:
    1. PowerDataCollector collects CPU idle and CPU freq state
       information.
    2. The about:power page is organized into expandable 
       sections.
    3. Two sections, "Idle State Data" and "Frequency State 
       Data" are added to the page.
    4. Plots have a legend.
    
    BUG=335816
    
    Review URL: https://codereview.chromium.org/149973002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256294 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/power/power_data_collector.h
+++ chrome/browser/chromeos/power/power_data_collector.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
 #include "base/time/time.h"
+#include "chrome/browser/chromeos/power/cpu_data_collector.h"
 #include "chromeos/chromeos_export.h"
 #include "chromeos/dbus/power_manager_client.h"
 
@@ -64,8 +65,15 @@ class CHROMEOS_EXPORT PowerDataCollector : public PowerManagerClient::Observer {
     return system_resumed_data_;
   }
 
+  const CpuDataCollector& cpu_data_collector() const {
+    return cpu_data_collector_;
+  }
+
   // Can be called only after DBusThreadManager is initialized.
   static void Initialize();
+
+  // Same as Initialize, but does not start the CpuDataCollector.
+  static void InitializeForTesting();
 
   // Can be called only if initialized via Initialize, and before
   // DBusThreadManager is destroyed.
@@ -84,12 +92,13 @@ class CHROMEOS_EXPORT PowerDataCollector : public PowerManagerClient::Observer {
   static const int kSampleTimeLimitSec;
 
  private:
-  PowerDataCollector();
+  explicit PowerDataCollector(const bool start_cpu_data_collector);
 
   virtual ~PowerDataCollector();
 
   std::deque<PowerSupplySample> power_supply_data_;
   std::deque<SystemResumedSample> system_resumed_data_;
+  CpuDataCollector cpu_data_collector_;
 
   DISALLOW_COPY_AND_ASSIGN(PowerDataCollector);
 };
