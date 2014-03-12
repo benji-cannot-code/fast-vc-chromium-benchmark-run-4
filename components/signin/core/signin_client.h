73b54d89666c05e1c0b8ba65a602d0d921e3359e
blundell@chromium.org <blundell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-12 05:15:52 +0000

    Remove Profile knowledge from MutableProfileOAuth2TokenService
    This CL removes all remaining calls to profile() in
    MutableProfileOAuth2TokenService, changing them to be calls on the
    SigninClient. Concretely, it adds the following APIs to SigninClient:
    - CanRevokeCredentials()
    - GetURLRequestContext()
    
    Note that the latter is a BrowserContext-level API and thus could go in a
    driver within the Signin component. However, at the current time it does not
    appear that there will be any other reason that the Signin component should be
    a layered component; I would like to avoid adding the extra complexity in
    internal structure until there is a compelling need for introducing the
    layering.
    
    BUG=334184
    
    Review URL: https://codereview.chromium.org/192373010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256438 0039d316-1c4b-4281-b951-d872f2087c98

--- components/signin/core/signin_client.h
+++ components/signin/core/signin_client.h
@@ -10,6 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class TokenWebData;
 
+namespace net {
+class URLRequestContextGetter;
+}
+
 // An interface that needs to be supplied to the Signin component by its
 // embedder.
 class SigninClient {
@@ -18,6 +22,12 @@ class SigninClient {
 
   // Gets the TokenWebData instance associated with the client.
   virtual scoped_refptr<TokenWebData> GetDatabase() = 0;
+
+  // Returns whether it is possible to revoke credentials.
+  virtual bool CanRevokeCredentials() = 0;
+
+  // Returns the URL request context information associated with the client.
+  virtual net::URLRequestContextGetter* GetURLRequestContext() = 0;
 };
 
 #endif  // COMPONENTS_SIGNIN_CORE_SIGNIN_CLIENT_H_
