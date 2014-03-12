81f9ae6b467381e9fb6ea2af72a3587f9fa755e4
r.c.ladan@gmail.com <r.c.ladan@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-12 09:28:14 +0000

    Allow compilation on recent versions of FreeBSD, which have libexecinfo in the base system but return size_t instead of int for backtrace()
    This is part of making GN bootstrap on FreeBSD.
    
    Tested on FreeBSD 8.4-i386 and FreeBSD 10.0-amd64
    
    BUG=
    
    Review URL: https://codereview.chromium.org/178193018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256484 0039d316-1c4b-4281-b951-d872f2087c98

--- base/debug/stack_trace_posix.cc
+++ base/debug/stack_trace_posix.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/debug/debugger.h"
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/numerics/safe_conversions.h"
 #include "base/posix/eintr_wrapper.h"
 #include "base/strings/string_number_conversions.h"
 
@@ -473,7 +474,7 @@ StackTrace::StackTrace() {
 
   // Though the backtrace API man page does not list any possible negative
   // return values, we take no chance.
-  count_ = std::max(backtrace(trace_, arraysize(trace_)), 0);
+  count_ = base::saturated_cast<size_t>(backtrace(trace_, arraysize(trace_)));
 }
 
 void StackTrace::Print() const {
