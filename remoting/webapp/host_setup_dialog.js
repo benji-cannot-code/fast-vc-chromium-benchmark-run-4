ae424e36089a7f49d3d60a0d9ca9cea4bca3e9f2
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-12 00:43:52 +0000

    Fix host installation prompt cancelation in CRD webapp.
    When host installation is cancelled HostSetupDialog dialog is supposed
    to hide the installation prompt, but previously it only updated
    internal state, but not webapp UI state.
    
    BUG=350211
    R=jamiewalch@chromium.org
    
    Review URL: https://codereview.chromium.org/196313002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256372 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/webapp/host_setup_dialog.js
+++ remoting/webapp/host_setup_dialog.js
@@ -363,6 +363,7 @@ remoting.HostSetupDialog.prototype.installHost_ = function() {
   /** @param {remoting.Error} error */
   var onError = function(error) {
     flow.switchToErrorState(error);
+    that.updateState_();
   }
 
   /** @param {remoting.HostController.State} state */
