96c06407587b75157be781c337bad2ab5719a24c
mcasas@chromium.org <mcasas@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-12 09:27:25 +0000

    Mac AVFoundation/QTKit: delay DeviceMonitorMac startup to first GetUserMedia.
    Currently DeviceMonitorMac is constructed from BrowserMainLoop in the
    first compasses of Chrome bringup. DeviceMonitorMac creates either a
    QTKitMonitorImpl or a AVFoundationMonitorImpl, that immediately
    starts observing the system capture devices. This monitoring is
    not needed until the user actively starts operating the devices,
    that happens in MediaStreamManager.
    
    This CL adds a method StartMonitoring() to DeviceMonitorMac that is
    exercised from MediaStreamManager::StartMonitoring().
    
    The performance regression in bugs 349616 and 348020 affects only
    AVFoundation, but both implementations would benefit from a delayed
    startup.
    
    Tested locally via using --[enable/disable]-avfoundation, connecting
    and disconnecting devices etc.
    
    BUG=288562, 349616, 348020
    
    Review URL: https://codereview.chromium.org/189443010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256483 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/device_monitor_mac.h
+++ content/browser/device_monitor_mac.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/system_monitor/system_monitor.h"
+#include "base/threading/thread_checker.h"
 
 namespace {
 class DeviceMonitorMacImpl;
@@ -23,6 +24,11 @@ class DeviceMonitorMac {
   DeviceMonitorMac();
   ~DeviceMonitorMac();
 
+  // Registers the observers for the audio/video device removal, connection and
+  // suspension. The AVFoundation library is also loaded and initialised if the
+  // OS supports it.
+  void StartMonitoring();
+
   // Method called by the internal DeviceMonitorMacImpl object
   // |device_monitor_impl_| when a device of type |type| has been added to or
   // removed from the system. This code executes in the notification thread
@@ -31,6 +37,10 @@ class DeviceMonitorMac {
 
  private:
   scoped_ptr<DeviceMonitorMacImpl> device_monitor_impl_;
+
+  // |thread_checker_| is used to check that constructor and StartMonitoring()
+  // are called in the correct thread, that also owns the object.
+  base::ThreadChecker thread_checker_;
 
   DISALLOW_COPY_AND_ASSIGN(DeviceMonitorMac);
 };
--- content/browser/renderer_host/media/media_stream_manager.cc
+++ content/browser/renderer_host/media/media_stream_manager.cc
@@ -43,6 +43,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
+// Forward declaration of DeviceMonitorMac and its only useable method.
+class DeviceMonitorMac {
+ public:
+  void StartMonitoring();
+};
+
 namespace {
 // Creates a random label used to identify requests.
 std::string RandomLabel() {
@@ -785,7 +791,23 @@ void MediaStreamManager::StartMonitoring() {
   audio_input_device_manager_->EnumerateDevices(MEDIA_DEVICE_AUDIO_CAPTURE);
   ++active_enumeration_ref_count_[MEDIA_DEVICE_VIDEO_CAPTURE];
   video_capture_manager_->EnumerateDevices(MEDIA_DEVICE_VIDEO_CAPTURE);
+
+#if defined(OS_MACOSX)
+  BrowserThread::PostTask(
+      BrowserThread::UI, FROM_HERE,
+      base::Bind(&MediaStreamManager::StartMonitoringOnUIThread,
+                 base::Unretained(this)));
+#endif
 }
+
+#if defined(OS_MACOSX)
+void MediaStreamManager::StartMonitoringOnUIThread() {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+  BrowserMainLoop* browser_main_loop = content::BrowserMainLoop::GetInstance();
+  if (browser_main_loop)
+    browser_main_loop->device_monitor_mac()->StartMonitoring();
+}
+#endif
 
 void MediaStreamManager::StopMonitoring() {
   DCHECK_EQ(base::MessageLoop::current(), io_loop_);
