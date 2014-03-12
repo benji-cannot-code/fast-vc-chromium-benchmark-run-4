4c2f7b2a1f08e42945eef9d4be7865ce44765881
japhet@chromium.org <japhet@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-12 07:58:46 +0000

    Prepare for per frame did{Start,Stop}Loading calls
    This teaches LoadProgressTracker about multiple frames loading at once and has it calculate progress as the product of all the individual frames' progresses.
    
    RenderViewImpl has didStartLoading()/didStopLoading() shims still in place, which call the new RenderFrameImpl versions, which in turn call permanent versions in RenderViewImpl. RenderViewImpl's shims are marked as deprecated and will be deleted once blink calls RenderFrameImpl directly in https://codereview.chromium.org/183793002/.
    
    BUG=347643
    
    Review URL: https://codereview.chromium.org/180113003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256477 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/load_progress_tracker.h
+++ content/renderer/load_progress_tracker.h
@@ -6,13 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_RENDERER_LOAD_PROGRESS_TRACKER_H_
 #define CONTENT_RENDERER_LOAD_PROGRESS_TRACKER_H_
 
+#include "base/containers/hash_tables.h"
 #include "base/logging.h"
 #include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
-
-namespace blink {
-class WebFrame;
-}
 
 namespace content {
 class RenderViewImpl;
@@ -22,9 +19,10 @@ class LoadProgressTracker {
   explicit LoadProgressTracker(RenderViewImpl* render_view);
   ~LoadProgressTracker();
 
-  void DidStopLoading();
+  void DidStartLoading(int frame_routing_id);
+  void DidStopLoading(int frame_routing_id);
 
-  void DidChangeLoadProgress(blink::WebFrame* frame, double progress);
+  void DidChangeLoadProgress(int frame_routing_id, double progress);
 
  private:
   void ResetStates();
@@ -33,9 +31,11 @@ class LoadProgressTracker {
 
   RenderViewImpl* render_view_;
 
-  blink::WebFrame* tracked_frame_;
-
-  double progress_;
+  // ProgressMap maps RenderFrame routing ids to a double representing that
+  // frame's completion (from 0 to 1).
+  typedef base::hash_map<int, double> ProgressMap;
+  ProgressMap progresses_;
+  double total_progress_;
 
   base::TimeTicks last_time_progress_sent_;
 
