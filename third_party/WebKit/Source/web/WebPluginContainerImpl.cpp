c79e5b70c2098d6204549738598afbc1736ac264
teravest@chromium.org <teravest@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-12 21:42:41 +0000

    Change event handling order in HTMLPlugInElement.
    Currently, plugins receive events as a "default handler" when an
    event is fired. "Default handlers" are called after the capturing and bubbling
    phases. This means that there is no way for a plugin to stop propagation of an
    event if it so desires.
    
    This change causes plugins to receive events "at target" so they may stop
    propagation of an event for the bubbling phase.
    
    BUG=324351
    
    Review URL: https://codereview.chromium.org/186153003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169059 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebPluginContainerImpl.cpp
+++ third_party/WebKit/Source/web/WebPluginContainerImpl.cpp
@@ -714,8 +714,10 @@ void WebPluginContainerImpl::handleMouseEvent(MouseEvent* event)
     }
 
     WebCursorInfo cursorInfo;
-    if (m_webPlugin->handleInputEvent(webEvent, cursorInfo))
+    if (m_webPlugin->handleInputEvent(webEvent, cursorInfo)) {
+        event->stopPropagation();
         event->setDefaultHandled();
+    }
 
     // A windowless plugin can change the cursor in response to a mouse move
     // event.  We need to reflect the changed cursor in the frame view as the
@@ -759,8 +761,10 @@ void WebPluginContainerImpl::handleWheelEvent(WheelEvent* event)
         return;
 
     WebCursorInfo cursorInfo;
-    if (m_webPlugin->handleInputEvent(webEvent, cursorInfo))
+    if (m_webPlugin->handleInputEvent(webEvent, cursorInfo)) {
+        event->stopPropagation();
         event->setDefaultHandled();
+    }
 }
 
 void WebPluginContainerImpl::handleKeyboardEvent(KeyboardEvent* event)
@@ -802,8 +806,10 @@ void WebPluginContainerImpl::handleKeyboardEvent(KeyboardEvent* event)
         view->client()->handleCurrentKeyboardEvent();
 
     WebCursorInfo cursorInfo;
-    if (m_webPlugin->handleInputEvent(webEvent, cursorInfo))
+    if (m_webPlugin->handleInputEvent(webEvent, cursorInfo)) {
+        event->stopPropagation();
         event->setDefaultHandled();
+    }
 }
 
 void WebPluginContainerImpl::handleTouchEvent(TouchEvent* event)
@@ -820,8 +826,10 @@ void WebPluginContainerImpl::handleTouchEvent(TouchEvent* event)
             focusPlugin();
 
         WebCursorInfo cursorInfo;
-        if (m_webPlugin->handleInputEvent(webEvent, cursorInfo))
+        if (m_webPlugin->handleInputEvent(webEvent, cursorInfo)) {
+            event->stopPropagation();
             event->setDefaultHandled();
+        }
         // FIXME: Can a plugin change the cursor from a touch-event callback?
         return;
     }
@@ -846,6 +854,7 @@ void WebPluginContainerImpl::handleGestureEvent(GestureEvent* event)
         return;
     WebCursorInfo cursorInfo;
     if (m_webPlugin->handleInputEvent(webEvent, cursorInfo)) {
+        event->stopPropagation();
         event->setDefaultHandled();
         return;
     }
@@ -868,8 +877,10 @@ void WebPluginContainerImpl::synthesizeMouseEventIfPossible(TouchEvent* event)
         return;
 
     WebCursorInfo cursorInfo;
-    if (m_webPlugin->handleInputEvent(webEvent, cursorInfo))
+    if (m_webPlugin->handleInputEvent(webEvent, cursorInfo)) {
+        event->stopPropagation();
         event->setDefaultHandled();
+    }
 }
 
 void WebPluginContainerImpl::focusPlugin()
