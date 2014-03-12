c6934c5f177e13d3c9e7ba6219a39b4bcbaf87aa
tnagel@chromium.org <tnagel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-12 20:42:49 +0000

    IWYU in ash/desktop_background/user_wallpaper_delegate.h
    BUG=82098
    TBR=pkotwicz@chromium.org (for login_display_host_impl.cc)
    
    Review URL: https://codereview.chromium.org/196633004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256633 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/desktop_background/user_wallpaper_delegate.h
+++ ash/desktop_background/user_wallpaper_delegate.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_DESKTOP_BACKGROUND_USER_WALLPAPER_DELEGATE_H_
 
 #include "ash/ash_export.h"
-#include "ui/wm/core/window_animations.h"
 
 namespace ash {
 
--- chrome/browser/chromeos/login/login_display_host_impl.cc
+++ chrome/browser/chromeos/login/login_display_host_impl.cc
@@ -83,6 +83,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/transform.h"
 #include "ui/views/focus/focus_manager.h"
 #include "ui/views/widget/widget.h"
+#include "ui/wm/core/window_animations.h"
 #include "url/gurl.h"
 
 namespace {
