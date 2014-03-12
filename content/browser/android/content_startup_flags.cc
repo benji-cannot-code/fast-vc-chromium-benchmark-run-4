ad653802d80ebeb6407d81ce0181f2c9c368a9da
wangxianzhu@chromium.org <wangxianzhu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-12 10:22:04 +0000

    Remove command line switches for accelerated compositing/scrolling frames
    This doesn't change the default behavior of any platform. On platforms other
    than Android, this change makes the accelerated compositing/scrolling frames
    feature automatically enabled if --kForceCompositingMode is specified on high
    DPI devices.
    
    BUG=304900
    
    Review URL: https://codereview.chromium.org/181883002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256508 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/android/content_startup_flags.cc
+++ content/browser/android/content_startup_flags.cc
@@ -61,10 +61,6 @@ void SetContentCommandLineFlags(int max_render_process_count,
   parsed_command_line->AppendSwitch(
       switches::kEnableCompositingForFixedPosition);
   parsed_command_line->AppendSwitch(switches::kEnableAcceleratedOverflowScroll);
-  parsed_command_line->AppendSwitch(
-      switches::kEnableAcceleratedScrollableFrames);
-  parsed_command_line->AppendSwitch(
-      switches::kEnableCompositedScrollingForFrames);
   parsed_command_line->AppendSwitch(switches::kEnableBeginFrameScheduling);
 
   parsed_command_line->AppendSwitch(switches::kDisableGestureDebounce);
