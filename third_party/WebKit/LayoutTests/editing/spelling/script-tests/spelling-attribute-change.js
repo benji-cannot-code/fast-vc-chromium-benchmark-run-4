3fd7407816fc12f56ac020afe35fa1945bac3b4f
g.czajkowski@samsung.com <g.czajkowski@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-12 09:23:16 +0000

    Verify spellcheck attribute asynchronously
    Refactoring spelling tests that verify spellcheck attribute to use
    asynchronous text checking.
    
    Additionally, add the elements that are being tested to to the tree.
    It gives a possibility for non-DumpRenderTree users to see misspellings.
    
    Fix the description message according to the test behavior.
    
    This change is based on WebKit changeset: http://trac.webkit.org/changeset/163432
    
    BUG=295722
    
    Review URL: https://codereview.chromium.org/190663014
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168998 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/editing/spelling/script-tests/spelling-attribute-change.js
+++ third_party/WebKit/LayoutTests/editing/spelling/script-tests/spelling-attribute-change.js
@@ -1,5 +1,13 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-description('Tests if the spellchecker behavior change after the spellcheck attribute changed by the script.');
+description('Tests if the spellchecker behaves correctly when the spellcheck attribute '
+    + 'is being changed by the script.');
+
+jsTestIsAsync = true;
+
+if (window.internals) {
+    internals.settings.setUnifiedTextCheckerEnabled(true);
+    internals.settings.setAsynchronousSpellCheckingEnabled(true);
+}
 
 var parent = document.createElement("div");
 document.body.appendChild(parent);
@@ -23,7 +31,10 @@ function testSpellCheckingEnabled(target, enabled)
 
     window.target = target;
     shouldBe("target.spellcheck", enabled ? "true" : "false");
-    shouldBe("internals.hasSpellingMarker(document, 6, 2)", enabled ? "true" : "false");
+    if (window.internals)
+        shouldBecomeEqual("internals.hasSpellingMarker(document, 6, 2)", enabled ? "true" : "false", done);
+    else
+        done();
 }
 
 function createElement(tagName, spellcheck)
@@ -36,52 +47,49 @@ function createElement(tagName, spellcheck)
     return target;
 }
 
-function testFor(tagName, initialAttribute, expectation)
+function testFor(tagName, spellCheckAttribueValues)
 {
-    var target = createElement(tagName, initialAttribute);
+    var target = createElement(tagName, spellCheckAttribueValues.initialValue);
     parent.appendChild(target);
 
-    testSpellCheckingEnabled(target, expectation);
-    parent.innerHTML = "";
+    testSpellCheckingEnabled(target, spellCheckAttribueValues.destinationValue);
 }
 
-// default -> true
-testFor("SPAN", undefined, true);
-// default -> false
-testFor("SPAN", undefined, false);
-// true -> true
-testFor("SPAN", true, true);
-// true -> false
-testFor("SPAN", true, false);
-// false -> true
-testFor("SPAN", false, true);
-// false -> false
-testFor("SPAN", false, false);
+var testElements = [
+    "SPAN",
+    "INPUT",
+    "TEXTAREA"
+];
 
-// default -> true
-testFor("INPUT", undefined, true);
-// default -> false
-testFor("INPUT", undefined, false);
-// true -> true
-testFor("INPUT", true, true);
-// true -> false
-testFor("INPUT", true, false);
-// false -> true
-testFor("INPUT", false, true);
-// false -> false
-testFor("INPUT", false, false);
+const spellcheckAttributeVariances = [
+   { initialValue: undefined, destinationValue: true },
+   { initialValue: undefined, destinationValue: false },
+   { initialValue: true, destinationValue: true },
+   { initialValue: true, destinationValue: false },
+   { initialValue: false, destinationValue: true },
+   { initialValue: false, destinationValue: false }
+];
 
-// default -> true
-testFor("TEXTAREA", undefined, true);
-// default -> false
-testFor("TEXTAREA", undefined, false);
-// true -> true
-testFor("TEXTAREA", true, true);
-// true -> false
-testFor("TEXTAREA", true, false);
-// false -> true
-testFor("TEXTAREA", false, true);
-// false -> false
-testFor("TEXTAREA", false, false);
+var iterator = 0;
+var currentElement = null;
+
+function done()
+{
+    if (!currentElement) {
+        currentElement = testElements.shift();
+        // All elements have been already taken.
+        if (!currentElement)
+            return finishJSTest();
+    }
+
+    if (iterator != spellcheckAttributeVariances.length)
+        setTimeout(testFor(currentElement, spellcheckAttributeVariances[iterator++]), 0);
+    else {
+        iterator = 0;
+        currentElement = null;
+        done();
+    }
+}
+done();
 
 var successfullyParsed = true;
