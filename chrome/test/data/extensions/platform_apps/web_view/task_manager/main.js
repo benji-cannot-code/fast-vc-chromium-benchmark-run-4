c1d5b883fb1ddc8a0d1c50ae45a1d7851c1b0aea
nick@chromium.org <nick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-12 21:14:13 +0000

    Create a task manager browsertest for <webview>.
    Required moving the Match* functions into task_manager_browsertest_util.
    
    BUG=348836
    TEST=browser_tests
    TBR=yoshiki@chromium.org
    
    Review URL: https://codereview.chromium.org/191623003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256647 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/platform_apps/web_view/task_manager/main.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var startTest = function() {
+  chrome.test.sendMessage('guest-loaded');
+};
+
+chrome.test.getConfig(function(config) {
+  var guestURL = 'http://localhost:' + config.testServer.port +
+      '/extensions/platform_apps/web_view/task_manager/guest.html';
+  document.querySelector('#webview-tag-container').innerHTML =
+      '<webview style="width: 10px; height: 10px; margin: 0; padding: 0;"' +
+      ' src="' + guestURL + '"' +
+      '></webview>';
+  startTest();
+});
