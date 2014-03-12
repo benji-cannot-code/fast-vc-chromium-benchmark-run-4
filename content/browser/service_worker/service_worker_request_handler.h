06a286b0943ea36e3ed5915854e17cd303c90cb2
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-12 17:53:45 +0000

    Add more plumbing for ServiceWorkerURLRequestJob
    In RequestHandler::MaybeCreateJob()
    * If it's for main resource:
    -- Start to look up registration for the requesting URL
    -- If registration is found and it has active_version:
    --- associate the version to the corresponding provider_host
    --- instruct the URLRequestJob to forward the request to SW
    -- Otherwise (registration if not found or it has no active version):
    --- instruct the URLRequestJob to fall-back to network
    * If it's for sub resource:
    -- instruct the URLRequestJob to forward the request to SW
    
    No handling for ServiceWorker cases yet.
    No tests yet (I'm willing to add at some point)
    
    BUG=349319
    
    Review URL: https://codereview.chromium.org/190763011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256576 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/service_worker/service_worker_request_handler.h
+++ content/browser/service_worker/service_worker_request_handler.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/supports_user_data.h"
 #include "content/common/content_export.h"
+#include "content/common/service_worker/service_worker_status_code.h"
 #include "webkit/common/resource_type.h"
 
 namespace net {
@@ -23,6 +24,7 @@ namespace content {
 
 class ServiceWorkerContextCore;
 class ServiceWorkerProviderHost;
+class ServiceWorkerRegistration;
 class ServiceWorkerURLRequestJob;
 
 // Created one per URLRequest and attached to each request.
@@ -55,17 +57,28 @@ class CONTENT_EXPORT ServiceWorkerRequestHandler
       net::NetworkDelegate* network_delegate);
 
  private:
+  typedef ServiceWorkerRequestHandler self;
+
   ServiceWorkerRequestHandler(
       base::WeakPtr<ServiceWorkerContextCore> context,
       base::WeakPtr<ServiceWorkerProviderHost> provider_host,
       ResourceType::Type resource_type);
 
-  bool ShouldFallbackToNetwork() const;
+  // For main resource case.
+  void PrepareForMainResource(const GURL& url);
+  void DidLookupRegistrationForMainResource(
+      ServiceWorkerStatusCode status,
+      const scoped_refptr<ServiceWorkerRegistration>& registration);
+
+  // For sub resource case.
+  void PrepareForSubResource();
 
   base::WeakPtr<ServiceWorkerContextCore> context_;
   base::WeakPtr<ServiceWorkerProviderHost> provider_host_;
   ResourceType::Type resource_type_;
   scoped_refptr<ServiceWorkerURLRequestJob> job_;
+
+  base::WeakPtrFactory<ServiceWorkerRequestHandler> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(ServiceWorkerRequestHandler);
 };
