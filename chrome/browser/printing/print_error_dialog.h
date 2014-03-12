04f2fcc4a7f51cecbf808902e1719d23f93ecff6
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-12 20:43:50 +0000

    Show dialog as separate post task. Dialog runs nested loop and can destroy data necessary for caller.
    BUG=350537
    
    Review URL: https://codereview.chromium.org/194383002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256634 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/printing/print_error_dialog.h
+++ chrome/browser/printing/print_error_dialog.h
@@ -6,12 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_PRINTING_PRINT_ERROR_DIALOG_H_
 #define CHROME_BROWSER_PRINTING_PRINT_ERROR_DIALOG_H_
 
-#include "ui/gfx/native_widget_types.h"
-
 namespace chrome {
 
 // Shows a window-modal error that printing failed for some unknown reason.
-void ShowPrintErrorDialog(gfx::NativeWindow parent);
+// TODO(vitalybuka, crbug.com/351550): Should be replaced with better UI.
+void ShowPrintErrorDialog();
 
 }  // namespace chrome
 
