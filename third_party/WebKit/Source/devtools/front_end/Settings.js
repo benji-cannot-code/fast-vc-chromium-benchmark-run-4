1a744595dac375e59e26c3f855f224f299acf090
dgozman@chromium.org <dgozman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-12 14:47:26 +0000

    [DevTools] Reduce minimum drawer size to avoid layout problems.
    BUG=351237
    
    Review URL: https://codereview.chromium.org/195983005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169018 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/Settings.js
+++ third_party/WebKit/Source/devtools/front_end/Settings.js
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 var Preferences = {
     maxInlineTextChildLength: 80,
-    minDrawerHeight: 25,
+    minDrawerHeight: 24,
     minSidebarWidth: 100,
     minSidebarHeight: 75,
     applicationTitle: "Developer Tools - %s",
