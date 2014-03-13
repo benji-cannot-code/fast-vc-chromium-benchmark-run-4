e7cecf230c4a37a9158d269f18ced964544e5ac6
jyasskin@chromium.org <jyasskin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-13 20:24:46 +0000

    Add a DCHECK_CURRENTLY_ON(BrowserThread::ID) macro.
    I've often lost time when a DCHECK(BrowserThread::CurrentlyOn(id)) fails,
    because I needed to re-run the program in gdb to figure out which thread was
    actually running the function. This new wrapper macro should make that clearer.
    
    I've converted one file in service_worker to make sure everything works.
    It changes:
    
    [...:FATAL:service_worker_context_core.cc(75)] Check failed: BrowserThread::CurrentlyOn(BrowserThread::IO).
    
    into
    
    [...:FATAL:service_worker_context_core.cc(75)] Check failed: ::content::BrowserThread::CurrentlyOn(BrowserThread::IO). Must be called on Chrome_IOThread; actually called on Chrome_UIThread.
    
    Review URL: https://codereview.chromium.org/191763002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256913 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/service_worker/service_worker_context_core.cc
+++ content/browser/service_worker/service_worker_context_core.cc
@@ -67,7 +67,7 @@ void ServiceWorkerContextCore::RegisterServiceWorker(
     const GURL& script_url,
     int source_process_id,
     const RegistrationCallback& callback) {
-  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+  DCHECK_CURRENTLY_ON(BrowserThread::IO);
 
   job_coordinator_->Register(
       pattern,
@@ -82,7 +82,7 @@ void ServiceWorkerContextCore::UnregisterServiceWorker(
     const GURL& pattern,
     int source_process_id,
     const UnregistrationCallback& callback) {
-  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+  DCHECK_CURRENTLY_ON(BrowserThread::IO);
 
   job_coordinator_->Unregister(pattern, source_process_id, callback);
 }
--- content/public/browser/browser_thread.h
+++ content/public/browser/browser_thread.h
@@ -11,14 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/callback.h"
 #include "base/location.h"
+#include "base/logging.h"
 #include "base/message_loop/message_loop_proxy.h"
 #include "base/task_runner_util.h"
 #include "base/time/time.h"
 #include "content/common/content_export.h"
-
-#if defined(UNIT_TEST)
-#include "base/logging.h"
-#endif  // UNIT_TEST
 
 namespace base {
 class MessageLoop;
@@ -30,6 +27,13 @@ namespace content {
 
 class BrowserThreadDelegate;
 class BrowserThreadImpl;
+
+// Use DCHECK_CURRENTLY_ON(BrowserThread::ID) to assert that a function can only
+// be called on the named BrowserThread.
+#define DCHECK_CURRENTLY_ON(thread_identifier)                      \
+  (DCHECK(::content::BrowserThread::CurrentlyOn(thread_identifier)) \
+   << ::content::BrowserThread::GetDCheckCurrentlyOnErrorMessage(   \
+          thread_identifier))
 
 ///////////////////////////////////////////////////////////////////////////////
 // BrowserThread
@@ -189,7 +193,7 @@ class CONTENT_EXPORT BrowserThread {
   static bool IsThreadInitialized(ID identifier);
 
   // Callable on any thread.  Returns whether you're currently on a particular
-  // thread.
+  // thread.  To DCHECK this, use the DCHECK_CURRENTLY_ON() macro above.
   static bool CurrentlyOn(ID identifier);
 
   // Callable on any thread.  Returns whether the threads message loop is valid.
@@ -266,6 +270,9 @@ class CONTENT_EXPORT BrowserThread {
   struct DeleteOnIOThread : public DeleteOnThread<IO> { };
   struct DeleteOnFileThread : public DeleteOnThread<FILE> { };
   struct DeleteOnDBThread : public DeleteOnThread<DB> { };
+
+  // Returns an appropriate error message for when DCHECK_CURRENTLY_ON() fails.
+  static std::string GetDCheckCurrentlyOnErrorMessage(ID expected);
 
  private:
   friend class BrowserThreadImpl;
