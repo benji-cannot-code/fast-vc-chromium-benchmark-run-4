62e84a583923f22ffac4194f237cf2b7fb98fcab
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-13 22:01:34 +0000

    Emit UpdateLayerTree event from InspectorTimelineAgent
    This emits a timeline event when layer tree is rebuilt. The layer tree
    is built by the LayerTreeAgent.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/196133009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169156 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorTimelineAgent.h
+++ third_party/WebKit/Source/core/inspector/InspectorTimelineAgent.h
@@ -69,6 +69,7 @@ class InspectorDOMAgent;
 class InspectorFrontend;
 class InspectorOverlay;
 class InspectorPageAgent;
+class InspectorLayerTreeAgent;
 class InstrumentingAgents;
 class KURL;
 class Node;
@@ -125,9 +126,10 @@ public:
         size_t usedGPUMemoryBytes;
     };
 
-    static PassOwnPtr<InspectorTimelineAgent> create(InspectorPageAgent* pageAgent, InspectorDOMAgent* domAgent, InspectorOverlay* overlay, InspectorType type, InspectorClient* client)
+    static PassOwnPtr<InspectorTimelineAgent> create(InspectorPageAgent* pageAgent, InspectorDOMAgent* domAgent, InspectorLayerTreeAgent* layerTreeAgent,
+        InspectorOverlay* overlay, InspectorType type, InspectorClient* client)
     {
-        return adoptPtr(new InspectorTimelineAgent(pageAgent, domAgent, overlay, type, client));
+        return adoptPtr(new InspectorTimelineAgent(pageAgent, domAgent, layerTreeAgent, overlay, type, client));
     }
 
     virtual ~InspectorTimelineAgent();
@@ -161,6 +163,8 @@ public:
     void didInvalidateLayout(LocalFrame*);
     bool willLayout(LocalFrame*);
     void didLayout(RenderObject*);
+
+    void layerTreeDidChange();
 
     void willAutosizeText(RenderObject*);
     void didAutosizeText(RenderObject*);
@@ -243,7 +247,7 @@ private:
 
     friend class TimelineRecordStack;
 
-    InspectorTimelineAgent(InspectorPageAgent*, InspectorDOMAgent*, InspectorOverlay*, InspectorType, InspectorClient*);
+    InspectorTimelineAgent(InspectorPageAgent*, InspectorDOMAgent*, InspectorLayerTreeAgent*, InspectorOverlay*, InspectorType, InspectorClient*);
 
     // Trace event handlers
     void onBeginImplSideFrame(const TraceEventDispatcher::TraceEvent&);
@@ -302,6 +306,7 @@ private:
 
     InspectorPageAgent* m_pageAgent;
     InspectorDOMAgent* m_domAgent;
+    InspectorLayerTreeAgent* m_layerTreeAgent;
     InspectorFrontend::Timeline* m_frontend;
     InspectorClient* m_client;
     InspectorOverlay* m_overlay;
