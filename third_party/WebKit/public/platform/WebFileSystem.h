4d8f8fedb2c07771fecd7ab5c9b2a85cbde4fe07
hashimoto@chromium.org <hashimoto@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-13 21:35:54 +0000

    Remove API compatibility hack no longer used
    Chromium no longer needs this #define after http://crrev.com/256189
    
    BUG=347900
    
    Review URL: https://codereview.chromium.org/196953002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169143 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebFileSystem.h
+++ third_party/WebKit/public/platform/WebFileSystem.h
@@ -37,9 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebFileSystemType.h"
 #include "WebURL.h"
 
-// FIXME: Remove this #define.
-#define READ_DIRECTORY_RETURNS_INT
-
 namespace blink {
 
 class WebFileWriter;
