80133fff8fd94929d1db120ee94483197df20c11
yoz@chromium.org <yoz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-13 21:26:20 +0000

    Add missing ExtensionSystemFactory dependency to HistoryAPI.
    We get this transitively through ActivityLog but it should be declared explicitly.
    
    Review URL: https://codereview.chromium.org/198713004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256937 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/history/history_api.cc
+++ chrome/browser/extensions/api/history/history_api.cc
@@ -32,6 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_source.h"
 #include "extensions/browser/event_router.h"
 #include "extensions/browser/extension_system.h"
+#include "extensions/browser/extension_system_provider.h"
+#include "extensions/browser/extensions_browser_client.h"
 
 namespace extensions {
 
@@ -228,6 +230,7 @@ BrowserContextKeyedAPIFactory<HistoryAPI>* HistoryAPI::GetFactoryInstance() {
 template <>
 void BrowserContextKeyedAPIFactory<HistoryAPI>::DeclareFactoryDependencies() {
   DependsOn(ActivityLog::GetFactoryInstance());
+  DependsOn(ExtensionsBrowserClient::Get()->GetExtensionSystemFactory());
 }
 
 void HistoryAPI::OnListenerAdded(const EventListenerInfo& details) {
