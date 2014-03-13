fb2d3fb8a549fe4ec404a1e056c40f9644587e67
dgozman@chromium.org <dgozman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-13 14:53:29 +0000

    [DevTools] Resize EditFileSystemDialog on adding excluded row.
    BUG=352038
    
    Review URL: https://codereview.chromium.org/196353011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169132 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/EditFileSystemDialog.js
+++ third_party/WebKit/Source/devtools/front_end/EditFileSystemDialog.js
@@ -344,6 +344,7 @@ WebInspector.EditFileSystemDialog.prototype = {
         var path = entry.path;
         this._excludedFolderEntries.put(path, entry);
         this._excludedFolderList.addItem(path, null);
+        this._resize();
     },
 
     /**
