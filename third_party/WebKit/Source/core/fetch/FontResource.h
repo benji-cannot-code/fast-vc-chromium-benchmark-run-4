338f00d51aaaeb5c69d6c56ce69e9e50567bcc32
clamy@chromium.org <clamy@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-13 10:21:39 +0000

    Resource cache: unlock Resource when the last client goes away
    When the last client of a resource goes away, and it does not have any handle
    anymore, this CL unlocks the resource in order to decrease the memory
    consumption of the MemoryCache. This CL also adds the possibility to unlock FontResources.
    
    BUG=290193
    
    Review URL: https://codereview.chromium.org/177833002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169112 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/fetch/FontResource.h
+++ third_party/WebKit/Source/core/fetch/FontResource.h
@@ -66,6 +66,9 @@ public:
     SVGFontElement* getSVGFontById(const String&) const;
 #endif
 
+protected:
+    virtual bool isSafeToUnlock() const OVERRIDE;
+
 private:
     virtual void checkNotify() OVERRIDE;
     void fontLoadWaitLimitCallback(Timer<FontResource>*);
