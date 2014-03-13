ba4d3775cc5f591d7725d9402ef023b323e85782
eustas@chromium.org <eustas@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-13 05:37:29 +0000

    HAR: export only finished requests.
    BUG=350113
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/197193002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169089 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/NetworkPanel.js
+++ third_party/WebKit/Source/devtools/front_end/NetworkPanel.js
@@ -1076,6 +1076,7 @@ WebInspector.NetworkLogView.prototype = {
     _harRequests: function()
     {
         var httpRequests = this._requests.filter(WebInspector.NetworkLogView.HTTPRequestsFilter);
+        httpRequests = httpRequests.filter(WebInspector.NetworkLogView.FinishedRequestsFilter);
         return httpRequests.filter(WebInspector.NetworkLogView.NonSourceMapRequestsFilter);
     },
 
@@ -1657,6 +1658,15 @@ WebInspector.NetworkLogView.HTTPRequestsFilter = function(request)
 WebInspector.NetworkLogView.NonSourceMapRequestsFilter = function(request)
 {
     return !WebInspector.SourceMap.hasSourceMapRequestHeader(request);
+}
+
+/**
+ * @param {!WebInspector.NetworkRequest} request
+ * @return {boolean}
+ */
+WebInspector.NetworkLogView.FinishedRequestsFilter = function(request)
+{
+    return request.finished;
 }
 
 WebInspector.NetworkLogView.EventTypes = {
