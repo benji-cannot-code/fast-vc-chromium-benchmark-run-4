a25a5960a822ada93db0eb6b9f6f862a496bf084
fjhenigman@chromium.org <fjhenigman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-13 17:02:57 +0000

    ui:: Add --ui-enable-impl-side-painting flag.
    When present, the ui::Compositor enables impl-side painting,
    and uses cc::PictureLayer instead of cc::ContentLayer.
    
    BUG=350737
    
    Review URL: https://codereview.chromium.org/189543007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256850 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/compositor/layer.cc
+++ ui/compositor/layer.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/base/scoped_ptr_algorithm.h"
 #include "cc/layers/content_layer.h"
 #include "cc/layers/delegated_renderer_layer.h"
+#include "cc/layers/picture_layer.h"
 #include "cc/layers/solid_color_layer.h"
 #include "cc/layers/texture_layer.h"
 #include "cc/output/copy_output_request.h"
@@ -40,6 +41,15 @@ const ui::Layer* GetRoot(const ui::Layer* layer) {
     layer = layer->parent();
   return layer;
 }
+
+struct UIImplSidePaintingStatus {
+  UIImplSidePaintingStatus()
+      : enabled(ui::IsUIImplSidePaintingEnabled()) {
+  }
+  bool enabled;
+};
+base::LazyInstance<UIImplSidePaintingStatus> g_ui_impl_side_painting_status =
+    LAZY_INSTANCE_INITIALIZER;
 
 }  // namespace
 
@@ -112,6 +122,11 @@ Layer::~Layer() {
     children_[i]->parent_ = NULL;
   cc_layer_->RemoveLayerAnimationEventObserver(this);
   cc_layer_->RemoveFromParent();
+}
+
+// static
+bool Layer::UsingPictureLayer() {
+  return g_ui_impl_side_painting_status.Get().enabled;
 }
 
 Compositor* Layer::GetCompositor() {
@@ -472,7 +487,11 @@ void Layer::SwitchToLayer(scoped_refptr<cc::Layer> new_layer) {
 }
 
 void Layer::SwitchCCLayerForTest() {
-  scoped_refptr<cc::ContentLayer> new_layer = cc::ContentLayer::Create(this);
+  scoped_refptr<cc::Layer> new_layer;
+  if (Layer::UsingPictureLayer())
+    new_layer = cc::PictureLayer::Create(this);
+  else
+    new_layer = cc::ContentLayer::Create(this);
   SwitchToLayer(new_layer);
   content_layer_ = new_layer;
 }
@@ -539,7 +558,11 @@ void Layer::SetShowPaintedContent() {
   if (content_layer_.get())
     return;
 
-  scoped_refptr<cc::ContentLayer> new_layer = cc::ContentLayer::Create(this);
+  scoped_refptr<cc::Layer> new_layer;
+  if (Layer::UsingPictureLayer())
+    new_layer = cc::PictureLayer::Create(this);
+  else
+    new_layer = cc::ContentLayer::Create(this);
   SwitchToLayer(new_layer);
   content_layer_ = new_layer;
 
@@ -885,7 +908,10 @@ void Layer::CreateWebLayer() {
     solid_color_layer_ = cc::SolidColorLayer::Create();
     cc_layer_ = solid_color_layer_.get();
   } else {
-    content_layer_ = cc::ContentLayer::Create(this);
+    if (Layer::UsingPictureLayer())
+      content_layer_ = cc::PictureLayer::Create(this);
+    else
+      content_layer_ = cc::ContentLayer::Create(this);
     cc_layer_ = content_layer_.get();
   }
   cc_layer_->SetAnchorPoint(gfx::PointF());
