fda6ea542988464ed3cce4182f32ad8680385c54
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-13 06:05:41 +0000

    TimelineGrid: move itemsGraphElement from TimelineGrid to TimelineView where it is actually using.
    BUG=
    R=pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/194713002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169092 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/TimelineGrid.js
+++ third_party/WebKit/Source/devtools/front_end/TimelineGrid.js
@@ -36,10 +36,6 @@ WebInspector.TimelineGrid = function()
 {
     this.element = document.createElement("div");
 
-    this._itemsGraphsElement = document.createElement("div");
-    this._itemsGraphsElement.id = "resources-graphs";
-    this.element.appendChild(this._itemsGraphsElement);
-
     this._dividersElement = this.element.createChild("div", "resources-dividers");
 
     this._gridHeaderElement = document.createElement("div");
@@ -54,11 +50,6 @@ WebInspector.TimelineGrid = function()
 }
 
 WebInspector.TimelineGrid.prototype = {
-    get itemsGraphsElement()
-    {
-        return this._itemsGraphsElement;
-    },
-
     get dividersElement()
     {
         return this._dividersElement;
