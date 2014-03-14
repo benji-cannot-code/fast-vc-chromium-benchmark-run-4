f2dc62e80016a306bb9ba646542dca9bb2bcc6f6
fs@opera.com <fs@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-14 12:09:49 +0000

    Prevent destruction of self in SMILTimeContainer with 'discard'
    If discarding the <svg> root an animation update could end up destroying
    the SVGSVGElement owning the SMILTimeContainer, leading to use-after-free.
    Make sure to keep an additional reference to the owning SVGSVGElement
    across affected callsites.
    
    BUG=351316
    
    Review URL: https://codereview.chromium.org/195233003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169239 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/animation/SMILTimeContainer.cpp
+++ third_party/WebKit/Source/core/svg/animation/SMILTimeContainer.cpp
@@ -38,6 +38,16 @@ using namespace std;
 
 namespace WebCore {
 
+// Every entry-point that calls updateAnimations() should instantiate a
+// DiscardScope to prevent deletion of the ownerElement (and hence itself.)
+class DiscardScope {
+public:
+    explicit DiscardScope(SVGSVGElement& timeContainerOwner) : m_discardScopeElement(&timeContainerOwner) { }
+
+private:
+    RefPtr<SVGSVGElement> m_discardScopeElement;
+};
+
 SMILTimeContainer::SMILTimeContainer(SVGSVGElement& owner)
     : m_beginTime(0)
     , m_pauseTime(0)
@@ -148,6 +158,7 @@ void SMILTimeContainer::begin()
     // If 'm_presetStartTime' is set, the timeline was modified via setElapsed() before the document began.
     // In this case pass on 'seekToTime=true' to updateAnimations().
     m_beginTime = now - m_presetStartTime;
+    DiscardScope discardScope(m_ownerSVGElement);
     SMILTime earliestFireTime = updateAnimations(SMILTime(m_presetStartTime), m_presetStartTime ? true : false);
     m_presetStartTime = 0;
 
@@ -220,6 +231,7 @@ void SMILTimeContainer::setElapsed(SMILTime time)
     m_preventScheduledAnimationsChanges = false;
 #endif
 
+    DiscardScope discardScope(m_ownerSVGElement);
     updateAnimationsAndScheduleFrameIfNeeded(time, true);
 }
 
@@ -262,6 +274,7 @@ void SMILTimeContainer::wakeupTimerFired(Timer<SMILTimeContainer>*)
     } else {
         ASSERT(m_frameSchedulingState == SynchronizeAnimations);
         m_frameSchedulingState = Idle;
+        DiscardScope discardScope(m_ownerSVGElement);
         updateAnimationsAndScheduleFrameIfNeeded(elapsed());
     }
 }
@@ -322,6 +335,7 @@ void SMILTimeContainer::serviceAnimations(double monotonicAnimationStartTime)
 
     m_frameSchedulingState = Idle;
     animationClock().updateTime(monotonicAnimationStartTime);
+    DiscardScope discardScope(m_ownerSVGElement);
     updateAnimationsAndScheduleFrameIfNeeded(elapsed());
     animationClock().unfreeze();
 }
