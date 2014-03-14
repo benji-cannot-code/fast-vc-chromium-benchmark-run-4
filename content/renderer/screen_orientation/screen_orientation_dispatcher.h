39dcd88360fe608c44acf677521da5c927cf6bc2
mlamouri@chromium.org <mlamouri@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-14 22:26:01 +0000

    Fix screen orientation headers guard names.
    BUG=None
    
    Review URL: https://codereview.chromium.org/198173002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257230 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/screen_orientation/screen_orientation_dispatcher.h
+++ content/renderer/screen_orientation/screen_orientation_dispatcher.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_RENDERER_SCREEN_ORIENTATION_DISPATCHER_H_
-#define CONTENT_RENDERER_SCREEN_ORIENTATION_DISPATCHER_H_
+#ifndef CONTENT_RENDERER_SCREEN_ORIENTATION_SCREEN_ORIENTATION_DISPATCHER_H_
+#define CONTENT_RENDERER_SCREEN_ORIENTATION_SCREEN_ORIENTATION_DISPATCHER_H_
 
 #include "base/macros.h"
 #include "content/public/renderer/render_process_observer.h"
@@ -41,4 +41,4 @@ class CONTENT_EXPORT ScreenOrientationDispatcher
 
 }  // namespace content
 
-#endif // CONTENT_RENDERER_SCREEN_ORIENTATION_DISPATCHER_H_
+#endif // CONTENT_RENDERER_SCREEN_ORIENTATION_SCREEN_ORIENTATION_DISPATCHER_H_
