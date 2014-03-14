78a31ec6a90e36ad98470c384edd1b915d3d200e
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-14 17:06:27 +0000

    Revert of Add very simple loading test for non-SFI NaCl. (https://codereview.chromium.org/189553005/)
    Reason for revert:
    The newly added NonSfiLoad test fails on linux asan-lsan bot: http://build.chromium.org/p/chromium.memory/builders/Linux%20ASan%2BLSan%20Tests%20%282%29/builds/491/steps/browser_tests/logs/NonSfiLoad
    
    Original issue's description:
    > Add very simple loading test for non-SFI NaCl.
    > 
    > This is the first step to add test cases for non-SFI NaCl.
    > The testee binary is build by a new target rule introduced by this CL, which is relocatable/libc-free binary. The code has a minimum set of handlers for irt_ppapi, to load the plugin successfully done with communicating hosts (browser/renderer processes).
    > 
    > Along this change, some DCHECK errors are found. So they're fixed, too.
    > 1) AtExitManager is conflicting. In non-SFI mode, AtExitManager is already instantiated in nacl_helper. To avoid the conflicting, skip to create it in plugin_main_irt.cc.
    > 2) Allowed creating Singleton on the plugin's main thread.
    > 3) SRPC may be successfully done, and then NOTREACHED catch it in nacl_listener. It is false alarm, so removed it.
    > 
    > BUG=https://code.google.com/p/nativeclient/issues/detail?id=3734
    > TEST=run ./browser_tests --gtest_filter=NaCl*NonSFI* locally. Run trybots.
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=257118
    
    TBR=mseaborn@chromium.org,dmichael@chromium.org,hidehiko@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=https://code.google.com/p/nativeclient/issues/detail?id=3734
    
    Review URL: https://codereview.chromium.org/200503002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257137 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/nacl/nacl_browsertest_util.cc
+++ chrome/test/nacl/nacl_browsertest_util.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/test/base/ui_test_utils.h"
-#include "components/nacl/common/nacl_switches.h"
 #include "content/public/browser/plugin_service.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/webplugininfo.h"
@@ -318,15 +317,6 @@ bool NaClBrowserTestPnaclDisabled::IsPnaclDisabled() {
 void NaClBrowserTestPnaclDisabled::SetUpCommandLine(CommandLine* command_line) {
   NaClBrowserTestBase::SetUpCommandLine(command_line);
   command_line->AppendSwitch(switches::kDisablePnacl);
-}
-
-base::FilePath::StringType NaClBrowserTestNonSfiMode::Variant() {
-  return FILE_PATH_LITERAL("libc-free");
-}
-
-void NaClBrowserTestNonSfiMode::SetUpCommandLine(CommandLine* command_line) {
-  NaClBrowserTestBase::SetUpCommandLine(command_line);
-  command_line->AppendSwitch(switches::kEnableNaClNonSfiMode);
 }
 
 base::FilePath::StringType NaClBrowserTestStatic::Variant() {
