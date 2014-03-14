efd06cea0b331dccbc06ba88c0b8f1f6c52477bd
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-14 15:22:09 +0000

    Retrieve enterprise device state in OOBE.
    This extends AutoEnrollmentClient to be able to retrieve server-backed
    device state from the cloud during OOBE. This will allow the device to
    restore enrollment state after recovery.
    
    BUG=chromium:351663
    TEST=Existing unit tests.
    R=joaodasilva@chromium.org
    
    Review URL: https://codereview.chromium.org/197043002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257115 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/chromeos/policy/server_backed_device_state.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/policy/server_backed_device_state.h"
+
+namespace policy {
+
+const char kDeviceStateManagementDomain[] = "management_domain";
+const char kDeviceStateRestoreMode[] = "device_mode";
+
+const char kDeviceStateRestoreModeReEnrollmentRequested[] =
+    "re-enrollment-requested";
+const char kDeviceStateRestoreModeReEnrollmentEnforced[] =
+    "re-enrollment-enforced";
+
+}  // namespace policy
--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -770,6 +770,7 @@ extern const char kInitialLocale[];
 extern const char kOobeComplete[];
 extern const char kDeviceRegistered[];
 extern const char kUsedPolicyCertificates[];
+extern const char kServerBackedDeviceState[];
 #endif
 
 extern const char kClearPluginLSODataEnabled[];
