ccc4bf8162fcc580734ef38c4a96d38913d0dad8
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-14 21:22:15 +0000

    Reland "Rename FontItalics to FontStyle to match CSS terminology"
    Rename the FontItalics enum, the FontDescription italics/
    setItalics methods and wrappers to FontStyle, style and style
    respectively to better match the CSS terminology.
    
    TBR=zmo@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/200763004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169278 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderThemeChromiumFontProviderLinux.cpp
+++ third_party/WebKit/Source/core/rendering/RenderThemeChromiumFontProviderLinux.cpp
@@ -66,7 +66,7 @@ void RenderThemeChromiumFontProvider::systemFont(CSSValueID valueID, FontDescrip
     fontDescription.setIsAbsoluteSize(true);
     fontDescription.setGenericFamily(FontDescription::NoFamily);
     fontDescription.setWeight(FontWeightNormal);
-    fontDescription.setItalic(false);
+    fontDescription.setStyle(false);
 }
 
 } // namespace WebCore
