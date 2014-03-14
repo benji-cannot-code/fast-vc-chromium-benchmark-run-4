6cc94c2f96c0a6ac73d75abd18cb630be06e4fc9
keonho07.kim@samsung.com <keonho07.kim@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-14 01:10:16 +0000

    Remove unnecassary parameter "sampleRate" for loadAudioResource().
    Parameter "sampleRate" is not required for loadAudioResource().
    There is FIXME in AudioBus.cpp in blink
    FIXME: the sampleRate parameter is ignored. It should be removed from the API.
    
    It will be consist of 3 steps. 
    1. Removing "sampleRate" in chrome which is called by blink. (included deprecated method) 
    https://codereview.chromium.org/195043002/ 
    
    2. Removing "sampleRate" in blink. 
    https://codereview.chromium.org/194753005/ 
    
    3. Removing deprecated method from #1 
    
    BUG=351284
    
    Review URL: https://codereview.chromium.org/194753005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169188 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/audio/AudioBus.cpp
+++ third_party/WebKit/Source/platform/audio/AudioBus.cpp
@@ -636,10 +636,10 @@ void AudioBus::clearSilentFlag()
         m_channels[i]->clearSilentFlag();
 }
 
-PassRefPtr<AudioBus> decodeAudioFileData(const char* data, size_t size, double sampleRate)
+PassRefPtr<AudioBus> decodeAudioFileData(const char* data, size_t size)
 {
     blink::WebAudioBus webAudioBus;
-    if (blink::Platform::current()->loadAudioResource(&webAudioBus, data, size, sampleRate))
+    if (blink::Platform::current()->loadAudioResource(&webAudioBus, data, size))
         return webAudioBus.release();
     return nullptr;
 }
@@ -650,8 +650,7 @@ PassRefPtr<AudioBus> AudioBus::loadPlatformResource(const char* name, float samp
     if (resource.isEmpty())
         return nullptr;
 
-    // FIXME: the sampleRate parameter is ignored. It should be removed from the API.
-    RefPtr<AudioBus> audioBus = decodeAudioFileData(resource.data(), resource.size(), sampleRate);
+    RefPtr<AudioBus> audioBus = decodeAudioFileData(resource.data(), resource.size());
 
     if (!audioBus.get())
         return nullptr;
@@ -665,8 +664,7 @@ PassRefPtr<AudioBus> AudioBus::loadPlatformResource(const char* name, float samp
 
 PassRefPtr<AudioBus> createBusFromInMemoryAudioFile(const void* data, size_t dataSize, bool mixToMono, float sampleRate)
 {
-    // FIXME: the sampleRate parameter is ignored. It should be removed from the API.
-    RefPtr<AudioBus> audioBus = decodeAudioFileData(static_cast<const char*>(data), dataSize, sampleRate);
+    RefPtr<AudioBus> audioBus = decodeAudioFileData(static_cast<const char*>(data), dataSize);
     if (!audioBus.get())
         return nullptr;
 
