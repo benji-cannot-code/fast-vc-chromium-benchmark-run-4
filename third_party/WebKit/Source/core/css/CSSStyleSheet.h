558753c171f1cf5d14cfc94ba87585dcc3f4f26c
ager@chromium.org <ager@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-14 23:32:38 +0000

    Optimize StyleSheetContents::checkLoaded by keeping track of completed and loading clients.
    For the input-wstyle.html benchmark from the bug report this brings
    the difference between the x-input and the x-input-style down to
    a factor of two and the time scales linearly with the number of
    elements added in the benchmark.
    
    x-input: 52.1520ms
    x-input-style: 105.3900ms
    input: 27.0840ms
    
    R=adamk@chromium.org, esprehn@chromium.org, haraken@chromium.org, tasak@chromium.org
    BUG=350785
    
    Review URL: https://codereview.chromium.org/196653006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169287 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/CSSStyleSheet.h
+++ third_party/WebKit/Source/core/css/CSSStyleSheet.h
@@ -130,6 +130,8 @@ private:
 
     bool canAccessRules() const;
 
+    void setLoadCompleted(bool);
+
     RefPtrWillBeMember<StyleSheetContents> m_contents;
     bool m_isInlineStylesheet;
     bool m_isDisabled;
