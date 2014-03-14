6938c54abcd179913a30b8860572e80456b4b4aa
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-14 21:29:02 +0000

    Upload device state keys on device policy fetch.
    This uploads client-determined device state keys at device policy
    fetches so the server can stage state for the client to retrieve after
    device reset.
    
    BUG=351663
    
    Review URL: https://codereview.chromium.org/195873012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257207 0039d316-1c4b-4281-b951-d872f2087c98

--- components/policy/core/common/cloud/cloud_policy_client.h
+++ components/policy/core/common/cloud/cloud_policy_client.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <set>
 #include <string>
+#include <vector>
 
 #include "base/basictypes.h"
 #include "base/callback.h"
@@ -173,6 +174,11 @@ class POLICY_EXPORT CloudPolicyClient {
   // FetchPolicy() calls won't request the given policy namespace anymore.
   void RemoveNamespaceToFetch(const PolicyNamespaceKey& policy_ns_key);
 
+  // Configures a set of device state keys to transfer to the server in the next
+  // policy fetch. If the fetch is successful, the keys will be cleared so they
+  // are only uploaded once.
+  void SetStateKeysToUpload(const std::vector<std::string>& keys);
+
   // Whether the client is registered with the device management service.
   bool is_registered() const { return !dm_token_.empty(); }
 
@@ -262,6 +268,7 @@ class POLICY_EXPORT CloudPolicyClient {
   const std::string verification_key_hash_;
   const UserAffiliation user_affiliation_;
   NamespaceSet namespaces_to_fetch_;
+  std::vector<std::string> state_keys_to_upload_;
 
   std::string dm_token_;
   DeviceMode device_mode_;
