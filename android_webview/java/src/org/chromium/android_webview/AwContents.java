c6cb89c2f7bab48c6521c8b111d840fe57e19878
boliu@chromium.org <boliu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-14 06:24:42 +0000

    aw: Disable hardware features for software layer
    If the webview is put into a software layer, then disable hardware only
    features such as accelerated canvas even if we are attached to a
    hardware view tree.
    
    Previously we did not take layer type into account.
    
    BUG=332273
    
    Review URL: https://codereview.chromium.org/199573002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257037 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/java/src/org/chromium/android_webview/AwContents.java
+++ android_webview/java/src/org/chromium/android_webview/AwContents.java
@@ -13,6 +13,7 @@ import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Picture;
 import android.graphics.Rect;
 import android.net.http.SslCertificate;
@@ -177,6 +178,7 @@ public class AwContents {
 
     // The base background color, i.e. not accounting for any CSS body from the current page.
     private int mBaseBackgroundColor = Color.WHITE;
+    private int mLayerType = View.LAYER_TYPE_NONE;
 
     // Must call nativeUpdateLastHitTestData first to update this before use.
     private final HitTestData mPossiblyStaleHitTestData = new HitTestData();
@@ -943,6 +945,21 @@ public class AwContents {
         if (mNativeAwContents != 0) nativeSetBackgroundColor(mNativeAwContents, color);
     }
 
+    /**
+     * @see android.view.View#setLayerType()
+     */
+    public void setLayerType(int layerType, Paint paint) {
+        mLayerType = layerType;
+        updateHardwareAcceleratedFeaturesToggle();
+    }
+
+    private void updateHardwareAcceleratedFeaturesToggle() {
+        mSettings.setEnableSupportedHardwareAcceleratedFeatures(
+                mIsAttachedToWindow && mContainerView.isHardwareAccelerated() &&
+                (mLayerType == View.LAYER_TYPE_NONE || mLayerType == View.LAYER_TYPE_HARDWARE));
+    }
+
+
     private int getEffectiveBackgroundColor() {
         // Do not ask the ContentViewCore for the background color, as it will always
         // report white prior to initial navigation or post destruction,  whereas we want
@@ -1557,8 +1574,7 @@ public class AwContents {
         mContentViewCore.onAttachedToWindow();
         nativeOnAttachedToWindow(mNativeAwContents, mContainerView.getWidth(),
                 mContainerView.getHeight());
-        mSettings.setEnableSupportedHardwareAcceleratedFeatures(
-                mContainerView.isHardwareAccelerated());
+        updateHardwareAcceleratedFeaturesToggle();
 
         if (mComponentCallbacks != null) return;
         mComponentCallbacks = new AwComponentCallbacks();
@@ -1589,8 +1605,7 @@ public class AwContents {
         }
 
         mContentViewCore.onDetachedFromWindow();
-
-        mSettings.setEnableSupportedHardwareAcceleratedFeatures(false);
+        updateHardwareAcceleratedFeaturesToggle();
 
         if (mComponentCallbacks != null) {
             mContainerView.getContext().unregisterComponentCallbacks(mComponentCallbacks);
