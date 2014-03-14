25c8a60e1ac411661d4a8b6601a698ca43d94672
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-14 02:17:55 +0000

    Revert 256960 because it causes failures in a number of tests.
    Test failures in: win8 views unittests (e.g. CloseAnchorViewTest), and
    ash unittests (e.g. GlobalAcceleratorsToggleAppList), in chromeos asan
    unit-tests (e.g. CreateAndClose), and various other tests.
    
    > Reland (try 2) r256396 Add views implementation for AXTreeSource.
    > 
    > Original cl:
    > http://codereview.chromium.org/173893003
    > 
    > BUG=318758
    > TBR=dtseng@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/196383008
    
    TBR=dtseng@chromium.org
    
    Review URL: https://codereview.chromium.org/199593005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256973 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/accessibility/ax_tree_serializer.h
+++ ui/accessibility/ax_tree_serializer.h
@@ -345,7 +345,6 @@ void AXTreeSerializer<AXSourceNode>::DeleteClientSubtree(
     DeleteClientSubtree(client_node->children[i]);
   }
   client_node->children.clear();
-  delete client_node;
 }
 
 template<typename AXSourceNode>
