d3f0f76620757beca431f3a3ededfb881fc09c89
zerny@chromium.org <zerny@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-14 21:18:10 +0000

    Rename NO_TRACE_CHECKING to GC_PLUGIN_IGNORE and add support for annotating classes as ignored.
    BUG=334149
    R=ager@chromium.org
    
    Review URL: https://codereview.chromium.org/196023018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257202 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/clang/blink_gc_plugin/tests/ignore_class.cpp
+++ tools/clang/blink_gc_plugin/tests/ignore_class.cpp
@@ -3,14 +3,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ignore_fields.h"
+#include "ignore_class.h"
 
 namespace WebCore {
 
+void B::trace(Visitor* visitor)
+{
+    // Class is ignored so no checking here.
+}
+
 void C::trace(Visitor* visitor)
 {
-    // Missing trace of m_one.
-    // Not missing ignored field m_two.
+    // Missing trace of m_obj.
+    // Ignored base class B does not need tracing.
 }
 
 }
