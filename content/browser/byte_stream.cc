10cc7d2f98706456555912f83afd78d3e908dfde
tyoshino@chromium.org <tyoshino@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-14 19:14:42 +0000

    Explain the reason why we don't have thread check in destructor of ByteStreamReader/Writer.
    They can be deleted before being used and it's safe to do so.
    
    Review URL: https://codereview.chromium.org/22925003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257173 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/byte_stream.cc
+++ content/browser/byte_stream.cc
@@ -195,6 +195,9 @@ ByteStreamWriterImpl::ByteStreamWriterImpl(
 }
 
 ByteStreamWriterImpl::~ByteStreamWriterImpl() {
+  // No RunsTasksOnCurrentThread() check to allow deleting a created writer
+  // before we start using it. Once started, should be deleted on the specified
+  // task runner.
   my_lifetime_flag_->is_alive = false;
 }
 
@@ -324,6 +327,9 @@ ByteStreamReaderImpl::ByteStreamReaderImpl(
 }
 
 ByteStreamReaderImpl::~ByteStreamReaderImpl() {
+  // No RunsTasksOnCurrentThread() check to allow deleting a created writer
+  // before we start using it. Once started, should be deleted on the specified
+  // task runner.
   my_lifetime_flag_->is_alive = false;
 }
 
