4b0583a0e5aa0a5ec5b225a53c31d65161815791
scheib@chromium.org <scheib@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-14 00:07:53 +0000

    Replace --ppapi-keep-alive-throttle command line switch with IPC parameter.
    Part of a broad effort to reduce the number of command line switches.
    
    This is the second land of this change - correcting a static initializer issue.
    
    BUG=350510
    
    Review URL: https://codereview.chromium.org/197693003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256961 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/proxy/plugin_globals.cc
+++ ppapi/proxy/plugin_globals.cc
@@ -14,14 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/proxy/ppapi_messages.h"
 #include "ppapi/proxy/ppb_message_loop_proxy.h"
 #include "ppapi/proxy/resource_reply_thread_registrar.h"
+#include "ppapi/shared_impl/ppapi_constants.h"
 #include "ppapi/shared_impl/proxy_lock.h"
 #include "ppapi/thunk/enter.h"
-
-namespace {
-
-const int kKeepaliveThrottleIntervalDefault = 5000;
-
-}  // namespace
 
 namespace ppapi {
 namespace proxy {
@@ -65,7 +60,7 @@ PluginGlobals::PluginGlobals()
           new ResourceReplyThreadRegistrar(GetMainThreadMessageLoop())),
       plugin_recently_active_(false),
       keepalive_throttle_interval_milliseconds_(
-          kKeepaliveThrottleIntervalDefault),
+          ppapi::kKeepaliveThrottleIntervalDefaultMilliseconds),
       weak_factory_(this) {
   DCHECK(!plugin_globals_);
   plugin_globals_ = this;
@@ -86,7 +81,7 @@ PluginGlobals::PluginGlobals(PerThreadForTest per_thread_for_test)
           new ResourceReplyThreadRegistrar(GetMainThreadMessageLoop())),
       plugin_recently_active_(false),
       keepalive_throttle_interval_milliseconds_(
-          kKeepaliveThrottleIntervalDefault),
+          kKeepaliveThrottleIntervalDefaultMilliseconds),
       weak_factory_(this) {
   DCHECK(!plugin_globals_);
 }
@@ -187,12 +182,13 @@ void PluginGlobals::MarkPluginIsActive() {
     if (!GetBrowserSender() || !base::MessageLoop::current())
       return;
     GetBrowserSender()->Send(new PpapiHostMsg_Keepalive());
-
-    GetMainThreadMessageLoop()->PostDelayedTask(FROM_HERE,
+    DCHECK(keepalive_throttle_interval_milliseconds_);
+    GetMainThreadMessageLoop()->PostDelayedTask(
+        FROM_HERE,
         RunWhileLocked(base::Bind(&PluginGlobals::OnReleaseKeepaliveThrottle,
                                   weak_factory_.GetWeakPtr())),
         base::TimeDelta::FromMilliseconds(
-            keepalive_throttle_interval_milliseconds()));
+            keepalive_throttle_interval_milliseconds_));
   }
 }
 
@@ -226,11 +222,7 @@ MessageLoopResource* PluginGlobals::loop_for_main_thread() {
   return loop_for_main_thread_.get();
 }
 
-int PluginGlobals::keepalive_throttle_interval_milliseconds() const {
-  return keepalive_throttle_interval_milliseconds_;
-}
-
-void PluginGlobals::set_keepalive_throttle_interval_milliseconds(int i) {
+void PluginGlobals::set_keepalive_throttle_interval_milliseconds(unsigned i) {
   keepalive_throttle_interval_milliseconds_ = i;
 }
 
--- ppapi/proxy/plugin_main_irt.cc
+++ ppapi/proxy/plugin_main_irt.cc
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/proxy/plugin_message_filter.h"
 #include "ppapi/proxy/plugin_proxy_delegate.h"
 #include "ppapi/proxy/resource_reply_thread_registrar.h"
-#include "ppapi/shared_impl/ppapi_switches.h"
 #include "ppapi/shared_impl/ppb_audio_shared.h"
 
 #if defined(__native_client__)
@@ -116,8 +115,6 @@ class PpapiDispatcher : public PluginDispatcher::PluginDelegate,
  private:
   void OnMsgInitializeNaClDispatcher(const ppapi::PpapiNaClPluginArgs& args);
   void OnPluginDispatcherMessageReceived(const IPC::Message& msg);
-
-  void SetPpapiKeepAliveThrottleFromCommandLine();
 
   std::set<PP_Instance> instances_;
   std::map<uint32, PluginDispatcher*> plugin_dispatchers_;
@@ -250,7 +247,10 @@ void PpapiDispatcher::OnMsgInitializeNaClDispatcher(
   logging::LoggingSettings settings;
   settings.logging_dest = logging::LOG_TO_SYSTEM_DEBUG_LOG;
   logging::InitLogging(settings);
-  SetPpapiKeepAliveThrottleFromCommandLine();
+
+  ppapi::proxy::PluginGlobals::Get()
+      ->set_keepalive_throttle_interval_milliseconds(
+          args.keepalive_throttle_interval_milliseconds);
 
   // Tell the process-global GetInterface which interfaces it can return to the
   // plugin.
@@ -293,18 +293,6 @@ void PpapiDispatcher::OnPluginDispatcherMessageReceived(
       plugin_dispatchers_.find(id);
   if (dispatcher != plugin_dispatchers_.end())
     dispatcher->second->OnMessageReceived(msg);
-}
-
-void PpapiDispatcher::SetPpapiKeepAliveThrottleFromCommandLine() {
-  unsigned keepalive_throttle_interval_milliseconds = 0;
-  if (base::StringToUint(
-          CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
-              switches::kPpapiKeepAliveThrottle),
-          &keepalive_throttle_interval_milliseconds)) {
-    ppapi::proxy::PluginGlobals::Get()->
-        set_keepalive_throttle_interval_milliseconds(
-            keepalive_throttle_interval_milliseconds);
-  }
 }
 
 }  // namespace
