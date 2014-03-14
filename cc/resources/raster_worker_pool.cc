b3abc63f33126721ed098db3863e8de6aa463e80
reveman@chromium.org <reveman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-14 06:43:27 +0000

    cc: Fix typo that is causing a crash whenever an image decode task is used.
    BUG=350471
    R=vmpstr
    
    Review URL: https://codereview.chromium.org/199383002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257047 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/resources/raster_worker_pool.cc
+++ cc/resources/raster_worker_pool.cc
@@ -288,7 +288,7 @@ class ImageDecodeWorkerPoolTaskImpl : public internal::WorkerPoolTask {
   skia::RefPtr<SkPixelRef> pixel_ref_;
   int layer_id_;
   RenderingStatsInstrumentation* rendering_stats_;
-  const base::Callback<void(bool was_canceled)>& reply_;
+  const base::Callback<void(bool was_canceled)> reply_;
 
   DISALLOW_COPY_AND_ASSIGN(ImageDecodeWorkerPoolTaskImpl);
 };
