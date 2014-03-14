d5026b4f5fee05c7eaa4e1bdf808cf3768cc526e
tnagel@chromium.org <tnagel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-14 15:39:52 +0000

    Coding style: remove JS-like !! bool cast from browser_options_handler.cc
    BUG=none
    
    Review URL: https://codereview.chromium.org/197793002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257120 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options/browser_options_handler.cc
+++ chrome/browser/ui/webui/options/browser_options_handler.cc
@@ -1296,7 +1296,7 @@ BrowserOptionsHandler::GetSyncStateDictionary() {
   DCHECK(signin);
   sync_status->SetBoolean("signoutAllowed", !signout_prohibited);
   sync_status->SetBoolean("signinAllowed", signin->IsSigninAllowed());
-  sync_status->SetBoolean("syncSystemEnabled", !!service);
+  sync_status->SetBoolean("syncSystemEnabled", (service != NULL));
   sync_status->SetBoolean("setupCompleted",
                           service && service->HasSyncSetupCompleted());
   sync_status->SetBoolean("setupInProgress",
@@ -1365,8 +1365,8 @@ void BrowserOptionsHandler::MouseExists(bool exists) {
 void BrowserOptionsHandler::OnUserImagePolicyChanged(
     const base::Value* previous_policy,
     const base::Value* current_policy) {
-  const bool had_policy = !!previous_policy;
-  const bool has_policy = !!current_policy;
+  const bool had_policy = previous_policy;
+  const bool has_policy = current_policy;
   if (had_policy != has_policy)
     OnAccountPictureManagedChanged(has_policy);
 }
