19a3607cbd79654fcf039a17226d0b8fb77a8dfe
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-14 11:13:27 +0000

    Revert of Revert of ui:: Add --ui-enable-map-image flag. (https://codereview.chromium.org/198233004/)
    Reason for revert:
    Reverting the change did not fix the test, so I'm undoing my revert, as scheduled.
    
    Original issue's description:
    > Revert of ui:: Add --ui-enable-map-image flag. (https://codereview.chromium.org/199403002/)
    > 
    > Reason for revert:
    > Attempting to see if this fixes a ChromeOS test that has been failing consistently on the ASAN bot since this change was introduced. Will revert the revert if it doesn't fix it.
    > 
    > http://build.chromium.org/p/chromium.memory/builders/Linux%20Chromium%20OS%20ASan%2BLSan%20Tests%20%281%29/builds/263
    > 
    > Original issue's description:
    > > ui:: Add --ui-enable-map-image flag.
    > > 
    > > BUG=350737
    > > 
    > > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=257020
    > 
    > TBR=danakj@chromium.org,fjhenigman@chromium.org
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=350737
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=257060
    
    TBR=danakj@chromium.org,fjhenigman@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=350737
    
    Review URL: https://codereview.chromium.org/200193003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257076 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/compositor/compositor_switches.cc
+++ ui/compositor/compositor_switches.cc
@@ -16,6 +16,8 @@ const char kUIDisableThreadedCompositing[] = "ui-disable-threaded-compositing";
 
 const char kUIEnableImplSidePainting[] = "ui-enable-impl-side-painting";
 
+const char kUIEnableMapImage[] = "ui-enable-map-image";
+
 const char kUIShowPaintRects[] = "ui-show-paint-rects";
 
 }  // namespace switches
@@ -26,6 +28,12 @@ bool IsUIImplSidePaintingEnabled() {
   const CommandLine& command_line = *CommandLine::ForCurrentProcess();
 
   return command_line.HasSwitch(switches::kUIEnableImplSidePainting);
+}
+
+bool IsUIMapImageEnabled() {
+  const CommandLine& command_line = *CommandLine::ForCurrentProcess();
+
+  return command_line.HasSwitch(switches::kUIEnableMapImage);
 }
 
 }  // namespace ui
