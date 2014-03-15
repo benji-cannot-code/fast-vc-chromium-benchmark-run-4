dc3db74a64d4e1672fd99cc6616ba875d427167f
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-15 01:08:15 +0000

    Move browser_test_utils's ExecuteScript* to use RenderFrameHost.
    BUG=304341
    TEST=no change
    
    Review URL: https://codereview.chromium.org/194903008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257281 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/webui/webui_resource_browsertest.cc
+++ chrome/test/data/webui/webui_resource_browsertest.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "content/public/browser/notification_registrar.h"
-#include "content/public/browser/render_view_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test_utils.h"
 #include "grit/webui_resources.h"
@@ -23,10 +22,10 @@ class WebUIResourceBrowserTest : public InProcessBrowserTest {
         base::FilePath(FILE_PATH_LITERAL("webui")), file);
     ui_test_utils::NavigateToURL(browser(), url);
 
-    content::RenderViewHost* rvh = browser()->tab_strip_model()
-        ->GetActiveWebContents()->GetRenderViewHost();
-    ASSERT_TRUE(rvh);
-    EXPECT_TRUE(ExecuteWebUIResourceTest(rvh, include_libraries_));
+    content::WebContents* web_contents =
+        browser()->tab_strip_model()->GetActiveWebContents();
+    ASSERT_TRUE(web_contents);
+    EXPECT_TRUE(ExecuteWebUIResourceTest(web_contents, include_libraries_));
   }
 
   // Queues the library corresponding to |resource_id| for injection into the
