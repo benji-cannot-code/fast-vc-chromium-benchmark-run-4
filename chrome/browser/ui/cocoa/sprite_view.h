2d0a2df94ea6726a613ab29d4143d7c0f1f77599
andresantoso@chromium.org <andresantoso@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-15 22:52:28 +0000

    Mac: Don't animate throbbers during window minimize animations.
    Only animate the sprites if the view is attached to a window, and that window
    is not currently minimized or in the middle of a minimize or deminimize
    animation.
    I don't know why it was causing the bad unminimize animation, but anyway
    it's good not to animate when not visible.
    
    BUG=350329
    
    Review URL: https://codereview.chromium.org/200853003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257348 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/sprite_view.h
+++ chrome/browser/ui/cocoa/sprite_view.h
@@ -8,9 +8,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Cocoa/Cocoa.h>
 
+#include "base/mac/scoped_nsobject.h"
+
+@class CAKeyframeAnimation;
+
 // A class that knows how to repeatedly animate sprites from an image containing
 // the sprites in the form of a horizontal strip.
-@interface SpriteView : NSView
+@interface SpriteView : NSView {
+ @private
+  base::scoped_nsobject<CAKeyframeAnimation> spriteAnimation_;
+}
 
 // |image| contains square sprites in a horizontal strip.
 // The sprites will be animated, with each sprite shown for 30ms.
