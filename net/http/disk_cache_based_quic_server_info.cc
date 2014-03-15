9ce6860aa44bc6ad9b21990d6c0d5e5bd2e9b62f
rtenneti@chromium.org <rtenneti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-15 05:36:04 +0000

    QUIC - IsDataReady returns false if there is an outstanding write is pending.
    BUG=362800
    R=wtc@chromium.org, rch@chromium.org
    
    Review URL: https://codereview.chromium.org/196343017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257298 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/disk_cache_based_quic_server_info.cc
+++ net/http/disk_cache_based_quic_server_info.cc
@@ -89,7 +89,7 @@ int DiskCacheBasedQuicServerInfo::WaitForDataReady(
 }
 
 bool DiskCacheBasedQuicServerInfo::IsDataReady() {
-  return ready_;
+  return ready_ && state_ == NONE;
 }
 
 void DiskCacheBasedQuicServerInfo::Persist() {
