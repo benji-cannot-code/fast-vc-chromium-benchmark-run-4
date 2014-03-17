9f1a4702a6fb7b0f084f91e0ce161f76d28ebdf1
rijubrata.bhaumik@intel.com <rijubrata.bhaumik@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-17 17:11:41 +0000

    Remove unused header file in Document
    BUG=
    
    Review URL: https://codereview.chromium.org/196943016
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169364 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Document.h
+++ third_party/WebKit/Source/core/dom/Document.h
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/v8/ScriptValue.h"
 #include "core/animation/css/CSSPendingAnimations.h"
 #include "core/dom/ContainerNode.h"
-#include "core/dom/DOMTimeStamp.h"
 #include "core/dom/DocumentEncodingData.h"
 #include "core/dom/DocumentInit.h"
 #include "core/dom/DocumentLifecycle.h"
