e8cf6a7ac25348f2373a994cc3f18d810dcd8f4c
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-17 21:07:36 +0000

    linux-aura: Keep track of whether the drag-client is alive after the drag.
    It is possible for the DesktopWindowTreeHost (and its drag-client) to be
    destroyed during the drag (or after the drag ends in OnXdndFinished(), but
    the control returns in StartDragAndDrop()). So keep a weak-ptr around for
    the drag session, and check whether it is still valid after the drag
    before resetting internal state.
    
    BUG=351504
    R=erg@chromium.org
    
    Review URL: https://codereview.chromium.org/202083004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257493 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/desktop_aura/desktop_drag_drop_client_aurax11.h
+++ ui/views/widget/desktop_aura/desktop_drag_drop_client_aurax11.h
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <X11/Xlib.h>
 
 #include "base/compiler_specific.h"
-#include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "ui/aura/client/drag_drop_client.h"
 #include "ui/aura/window_observer.h"
 #include "ui/base/cursor/cursor.h"
@@ -218,6 +218,8 @@ class VIEWS_EXPORT DesktopDragDropClientAuraX11
   gfx::NativeCursor grab_cursor_;
   gfx::NativeCursor copy_grab_cursor_;
   gfx::NativeCursor move_grab_cursor_;
+
+  base::WeakPtrFactory<DesktopDragDropClientAuraX11> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(DesktopDragDropClientAuraX11);
 };
