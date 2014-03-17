67fbf389b8046e5c0af1a580b82229d93a19683f
haavardm@opera.com <haavardm@opera.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-17 16:44:26 +0000

    Make OpenSSL UpdateServerCert() OS independent.
    UpdateServerCert currently creates the server cert chain
    directly from the openssl struct X509. This works since
    OSCertHandle currently is an OpenSSL X509 struct when
    OpenSSL is used on Android and Linux.
    
    This patch makes the UpdateServerCert() OS independent by creating
    the X509Certificate from DER data instead of OSCertHandle to make
    it compile on the other platforms when USE_OPENSSL is off.
    
    Keep the USE_OPENSSL code to avoid converting back and forth
    between X509 and DER twice and OsCertHandle is X509.
    
    I see that there is a DER cache in x509_certificate_openssl.cc
    which could have simplified the patch a bit. However, if I understand
    a comment correctly,  it shouldn't be mixed with certificates that comes
    from network, which is the case here. Also, that API is not exposed.
    
    Also remove some unused NSS code from x509_certificate_mac.cc.
    
    Review URL: https://codereview.chromium.org/173853014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257449 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/ssl_client_socket_nss.cc
+++ net/socket/ssl_client_socket_nss.cc
@@ -3590,6 +3590,11 @@ void SSLClientSocketNSS::AddSCTInfoToSSLInfo(SSLInfo* ssl_info) const {
   }
 }
 
+scoped_refptr<X509Certificate>
+SSLClientSocketNSS::GetUnverifiedServerCertificateChain() const {
+  return core_->state().server_cert.get();
+}
+
 ServerBoundCertService* SSLClientSocketNSS::GetServerBoundCertService() const {
   return server_bound_cert_service_;
 }
