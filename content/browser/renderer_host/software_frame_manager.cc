9854665aadad270825ff6d5e6a83a648b91fbf51
ccameron@chromium.org <ccameron@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-17 13:40:33 +0000

    Fix integer overflow in software compositor
    Ensure that the size mapped from the renderer
    process for the software frame is not less than
    expected due to integer overflow.
    
    BUG=348332
    
    Review URL: https://codereview.chromium.org/196283018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257417 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/software_frame_manager.cc
+++ content/browser/renderer_host/software_frame_manager.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/callback_helpers.h"
 #include "base/debug/alias.h"
+#include "base/numerics/safe_math.h"
 #include "content/browser/renderer_host/dip_util.h"
 #include "content/public/browser/user_metrics.h"
 
@@ -97,7 +98,15 @@ bool SoftwareFrameManager::SwapToNewFrame(
 
   // The NULL handle is used in testing.
   if (base::SharedMemory::IsHandleValid(shared_memory->handle())) {
-    const size_t size_in_bytes = 4 * frame_data->size.GetArea();
+    base::CheckedNumeric<size_t> size_in_bytes_checked =
+        base::CheckedNumeric<size_t>(4) *
+        base::CheckedNumeric<size_t>(frame_data->size.width()) *
+        base::CheckedNumeric<size_t>(frame_data->size.height());
+    if (!size_in_bytes_checked.IsValid()) {
+      DLOG(ERROR) << "Integer overflow when computing bytes to map.";
+      return false;
+    }
+    size_t size_in_bytes = size_in_bytes_checked.ValueOrDie();
 #ifdef OS_WIN
     if (!shared_memory->Map(0)) {
       DLOG(ERROR) << "Unable to map renderer memory.";
