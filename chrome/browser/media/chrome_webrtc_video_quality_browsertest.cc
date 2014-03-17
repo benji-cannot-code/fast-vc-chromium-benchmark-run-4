cd2b6daf9de7ecb543bdca9a84c38fdbe165afe6
grunell@chromium.org <grunell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-17 13:24:40 +0000

    Disable WebRtcVideoQualityBrowserTest.MANUAL_TestVGAVideoQuality on Win XP.
    TBR=mcasas@chromium.org,xians@chromium.org
    NOTREECHECKS=true
    
    BUG=353078
    
    Review URL: https://codereview.chromium.org/200713006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257416 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media/chrome_webrtc_video_quality_browsertest.cc
+++ chrome/browser/media/chrome_webrtc_video_quality_browsertest.cc
@@ -33,6 +33,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/perf/perf_test.h"
 #include "ui/gl/gl_switches.h"
 
+// For fine-grained suppression on flaky tests.
+#if defined(OS_WIN)
+#include "base/win/windows_version.h"
+#endif
+
 static const base::FilePath::CharType kFrameAnalyzerExecutable[] =
 #if defined(OS_WIN)
     FILE_PATH_LITERAL("frame_analyzer.exe");
@@ -345,6 +350,12 @@ class WebRtcVideoQualityBrowserTest : public WebRtcTestBase {
 
 IN_PROC_BROWSER_TEST_F(WebRtcVideoQualityBrowserTest,
                        MANUAL_TestVGAVideoQuality) {
+#if defined(OS_WIN)
+  // Fails on XP. http://crbug.com/353078
+  if (base::win::GetVersion() <= base::win::VERSION_XP)
+    return;
+#endif
+
   ASSERT_GE(TestTimeouts::action_max_timeout().InSeconds(), 150) <<
       "This is a long-running test; you must specify "
       "--ui-test-action-max-timeout to have a value of at least 150000.";
