3dc9655c66855ed758a76f7f2fc7a99fefb2dc72
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-17 23:13:58 +0000

    Move IsStringASCII/UTF8 to base namespace.
    Use StringPiece for IsStringUTF8.
    
    TBR=sky
    
    Review URL: https://codereview.chromium.org/196793010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257524 0039d316-1c4b-4281-b951-d872f2087c98

--- net/dns/dns_config_service_win.cc
+++ net/dns/dns_config_service_win.cc
@@ -140,7 +140,7 @@ bool ParseDomainASCII(const base::string16& widestr, std::string* domain) {
     return false;
 
   // Check if already ASCII.
-  if (IsStringASCII(widestr)) {
+  if (base::IsStringASCII(widestr)) {
     *domain = base::UTF16ToASCII(widestr);
     return true;
   }
@@ -156,7 +156,7 @@ bool ParseDomainASCII(const base::string16& widestr, std::string* domain) {
   // copy. Since ASCII is a subset of UTF8 the following is equivalent).
   bool success = base::UTF16ToUTF8(punycode.data(), punycode.length(), domain);
   DCHECK(success);
-  DCHECK(IsStringASCII(*domain));
+  DCHECK(base::IsStringASCII(*domain));
   return success && !domain->empty();
 }
 
