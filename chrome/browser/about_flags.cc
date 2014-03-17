9d75bc720ff3c86d9ff7b9e470cae8772242470d
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-17 12:28:00 +0000

    Add about:flags entry for --enable-bleeding-edge-rendering-fast-paths.
    BUG=353107
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/201823002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257415 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1865,6 +1865,13 @@ const Experiment kExperiments[] = {
     kOsWin | kOsLinux | kOsCrOS | kOsAndroid,
     MULTI_VALUE_TYPE(kTouchScrollingModeChoices)
   },
+  {
+    "bleeding-edge-renderer-mode",
+    IDS_FLAGS_BLEEDING_RENDERER_NAME,
+    IDS_FLAGS_BLEEDING_RENDERER_DESCRIPTION,
+    kOsAll,
+    SINGLE_VALUE_TYPE(switches::kEnableBleedingEdgeRenderingFastPaths)
+  },
 };
 
 const Experiment* experiments = kExperiments;
