b00b3aeb9e08b5401b34dec9710d65ed86ff576d
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-17 06:49:53 +0000

    Fix StatusChangeCallback's reentrancy issue
    Follow-up patch for https://codereview.chromium.org/196573005/
    
    BUG=285976
    TEST=ServiceWorkerVersionTest.RepeatedlyObserveStatusChanges
    
    Review URL: https://codereview.chromium.org/197393004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257391 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/service_worker/service_worker_version.cc
+++ content/browser/service_worker/service_worker_version.cc
@@ -26,11 +26,12 @@ void RunSoon(const base::Closure& callback) {
 }
 
 template <typename CallbackArray, typename Arg>
-void RunCallbacks(CallbackArray* callbacks, const Arg& arg) {
-  for (typename CallbackArray::const_iterator i = callbacks->begin();
-       i != callbacks->end(); ++i)
+void RunCallbacks(CallbackArray* callbacks_ptr, const Arg& arg) {
+  CallbackArray callbacks;
+  callbacks.swap(*callbacks_ptr);
+  for (typename CallbackArray::const_iterator i = callbacks.begin();
+       i != callbacks.end(); ++i)
     (*i).Run(arg);
-  callbacks->clear();
 }
 
 // A callback adapter to start a |task| after StartWorker.
@@ -130,6 +131,7 @@ ServiceWorkerVersion::~ServiceWorkerVersion() { DCHECK(is_shutdown_); }
 void ServiceWorkerVersion::Shutdown() {
   is_shutdown_ = true;
   registration_ = NULL;
+  status_change_callbacks_.clear();
   if (embedded_worker_) {
     embedded_worker_->RemoveObserver(this);
     embedded_worker_.reset();
@@ -139,12 +141,12 @@ void ServiceWorkerVersion::Shutdown() {
 void ServiceWorkerVersion::SetStatus(Status status) {
   status_ = status;
 
-  for (std::vector<base::Closure>::const_iterator i =
-           status_change_callbacks_.begin();
-       i != status_change_callbacks_.end(); ++i) {
+  std::vector<base::Closure> callbacks;
+  callbacks.swap(status_change_callbacks_);
+  for (std::vector<base::Closure>::const_iterator i = callbacks.begin();
+       i != callbacks.end(); ++i) {
     (*i).Run();
   }
-  status_change_callbacks_.clear();
 }
 
 void ServiceWorkerVersion::RegisterStatusChangeCallback(
@@ -249,7 +251,7 @@ void ServiceWorkerVersion::DispatchInstallEvent(
     int active_version_embedded_worker_id,
     const StatusCallback& callback) {
   DCHECK_EQ(NEW, status()) << status();
-  status_ = INSTALLING;
+  SetStatus(INSTALLING);
   SendMessageAndRegisterCallback(
       ServiceWorkerMsg_InstallEvent(active_version_embedded_worker_id),
       base::Bind(&HandleEventFinished, weak_factory_.GetWeakPtr(),
@@ -260,7 +262,7 @@ void ServiceWorkerVersion::DispatchInstallEvent(
 void ServiceWorkerVersion::DispatchActivateEvent(
     const StatusCallback& callback) {
   DCHECK_EQ(INSTALLED, status()) << status();
-  status_ = ACTIVATING;
+  SetStatus(ACTIVATING);
   // TODO(kinuko): Implement.
   NOTIMPLEMENTED();
   RunSoon(base::Bind(&HandleEventFinished, weak_factory_.GetWeakPtr(),
