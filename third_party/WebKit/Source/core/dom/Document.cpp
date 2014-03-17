3b7b62779bd8fa1d20081ab1683cd3815f0b528b
zmo@chromium.org <zmo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-17 20:46:18 +0000

    Revert 169354 "DevTools: track stylesheets by document, not loca..."
    Caused a crash on linux asan bot, see crbug.com/353257
    
    > DevTools: track stylesheets by document, not local frame.
    > 
    > R=lushnikov@chromium.org, vsevik@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/201683002
    
    TBR=pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/202443003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169389 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -630,8 +630,6 @@ void Document::dispose()
 
     m_lifecycle.advanceTo(DocumentLifecycle::Disposed);
     lifecycleNotifier().notifyDocumentWasDisposed();
-
-    InspectorInstrumentation::documentDisposed(this);
 }
 
 SelectorQueryCache& Document::selectorQueryCache()
