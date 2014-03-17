6196584fdbd58e4491d47f742b4451121b2e65a5
sigbjornf@opera.com <sigbjornf@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-17 19:34:26 +0000

    Update image data upon changing the img.crossorigin attribute.
    The spec tells us that setting the crossorigin attribute on an img
    element should cause the "update the image data" steps to be invoked,
    
     http://www.whatwg.org/specs/web-apps/current-work/#update-the-image-data
    
    Do so here, along with updating the underlying test's expectation.
    
    R=
    BUG=341047
    
    Review URL: https://codereview.chromium.org/201143002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169382 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/resources/origin.php
+++ third_party/WebKit/LayoutTests/http/tests/resources/origin.php
@@ -1,17 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php
-
-$gotOrigin = 0;
-foreach (getallheaders() as $name => $value) {
-    if ($name == "Origin") {
-        $gotOrigin = 1;
-    }
-}
-
-if ($gotOrigin) {
-    header('Location: square100.png');
-}
-else {
-    header('Location: square200.png');
-}
-header('HTTP/1.1 302 Redirect');
+header('HTTP/1.1 302 Found');
+header('Location: ' . ($_SERVER['HTTP_ORIGIN'] ? 'square100.png' : 'square200.png'));
+?>
