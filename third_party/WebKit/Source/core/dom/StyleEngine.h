3d2792a21feab6349de9dc7a2d5a42aee444aa9d
tasak@google.com <tasak@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-18 05:14:26 +0000

    Move StyleSheetContents cache to StyleEngine.
    Blink should not share StyleSheetContents cache between different WebFrames.
    
    If shares, WebFrameImpl::reload uses StyleSheetContents created by another WebFrameImpl.
    
    So when changing allowImage setting (controlled by chrome) and reloading some page, the
    reloaded frame might use StyleSheetContents cache.
    
    Since StyleImages are cached in CSSValues and are not managed by FrameLoader, allowImage
    change doesn't work.
    
    BUG=346264
    TEST=fast/css/background-image-reload.html, fast/css/two-different-iframe-not-share-style-image.html
    
    Review URL: https://codereview.chromium.org/179873014
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169414 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/StyleEngine.h
+++ third_party/WebKit/Source/core/dom/StyleEngine.h
@@ -185,8 +185,8 @@ public:
 
     void markDocumentDirty();
 
-    static PassRefPtr<CSSStyleSheet> createSheet(Element*, const String& text, TextPosition startPosition, bool createdByParser);
-    static void removeSheet(StyleSheetContents*);
+    PassRefPtr<CSSStyleSheet> createSheet(Element*, const String& text, TextPosition startPosition, bool createdByParser);
+    void removeSheet(StyleSheetContents*);
 
     void trace(Visitor*);
 
@@ -246,6 +246,9 @@ private:
     OwnPtr<StyleResolver> m_resolver;
 
     RefPtr<CSSFontSelector> m_fontSelector;
+
+    HashMap<AtomicString, StyleSheetContents*> m_textToSheetCache;
+    HashMap<StyleSheetContents*, AtomicString> m_sheetToTextCache;
 };
 
 }
