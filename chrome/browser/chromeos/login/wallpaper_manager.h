266cd502a8ce93380f870225defd7faf2a3657d5
tnagel@chromium.org <tnagel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-18 22:18:09 +0000

    Cosmetic improvements to WallpaperManager.
    Add a few const qualifiers and fix grammar and spelling of comments.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/203363002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257780 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/wallpaper_manager.h
+++ chrome/browser/chromeos/login/wallpaper_manager.h
@@ -198,7 +198,7 @@ class WallpaperManager: public content::NotificationObserver {
   // to custom wallpaper directory.
   base::FilePath GetCustomWallpaperPath(const char* sub_dir,
                                         const std::string& user_id_hash,
-                                        const std::string& file);
+                                        const std::string& file) const;
 
   // Returns filepath to save original custom wallpaper for the given user.
   base::FilePath GetOriginalWallpaperPathForUser(const std::string& user_id);
@@ -225,7 +225,7 @@ class WallpaperManager: public content::NotificationObserver {
                        ash::WallpaperLayout layout,
                        int preferred_width,
                        int preferred_height,
-                       scoped_refptr<base::RefCountedBytes>* output);
+                       scoped_refptr<base::RefCountedBytes>* output) const;
 
   // Resizes |wallpaper| to a resolution which is nearest to |preferred_width|
   // and |preferred_height| while maintaining aspect ratio. And saves the
@@ -234,7 +234,7 @@ class WallpaperManager: public content::NotificationObserver {
                               const base::FilePath& path,
                               ash::WallpaperLayout layout,
                               int preferred_width,
-                              int preferred_height);
+                              int preferred_height) const;
 
   // Saves custom wallpaper to file, post task to generate thumbnail and updates
   // local state preferences. If |update_wallpaper| is false, don't change
@@ -416,7 +416,7 @@ class WallpaperManager: public content::NotificationObserver {
                               const UserImage::RawImage& raw_image);
 
   // Record data for User Metrics Analysis.
-  void RecordUma(User::WallpaperType type, int index);
+  void RecordUma(User::WallpaperType type, int index) const;
 
   // Saves original custom wallpaper to |path| (absolute path) on filesystem
   // and starts resizing operation of the custom wallpaper if necessary.
@@ -427,7 +427,7 @@ class WallpaperManager: public content::NotificationObserver {
 
   // Saves wallpaper image raw |data| to |path| (absolute path) in file system.
   void SaveWallpaperInternal(const base::FilePath& path, const char* data,
-                             int size);
+                             int size) const;
 
   // Creates new PendingWallpaper request (or updates currently pending).
   void ScheduleSetUserWallpaper(const std::string& user_id, bool delayed);
