227dfece196cc99205269390697c0f8c1f3be8f5
ccameron@chromium.org <ccameron@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-18 19:08:15 +0000

    Fix remaining SadTabView anti-aliasing issues.
    The SadTabView is in a strange situation where it is set
    up almost entirely programmatically, but yet it is still
    loaded through an xib. Remove the programmatically set up
    and arranged elements from the xib entirely, and allocate them
    manually.
    
    Use a subclass of NSTextField to with isOpaque overwritten to
    enable text anti-aliasing. Use NSTextField instead of NSTextView
    because the manual resizing code is written against NSTextField,
    and lines get broken in different places when switching to
    NSTextView.
    
    BUG=351456
    
    Review URL: https://codereview.chromium.org/202783002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257707 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/tab_contents/sad_tab_view.h
+++ chrome/browser/ui/cocoa/tab_contents/sad_tab_view.h
@@ -18,18 +18,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @interface SadTabView : BaseView<NSTextViewDelegate> {
  @private
   IBOutlet NSImageView* image_;
-  IBOutlet NSTextField* title_;
-  IBOutlet NSTextField* message_;
-  IBOutlet NSTextField* helpPlaceholder_;
+  base::scoped_nsobject<NSTextField> title_;
+  base::scoped_nsobject<NSTextField> message_;
+  base::scoped_nsobject<HyperlinkTextView> help_;
 
   base::scoped_nsobject<NSColor> backgroundColor_;
   NSSize messageSize_;
-
-  // Text fields don't work as well with embedded links as text views, but
-  // text views cannot conveniently be created in IB. The xib file contains
-  // a text field |helpPlaceholder_| that's replaced by this text view |help_|
-  // in -awakeFromNib.
-  base::scoped_nsobject<HyperlinkTextView> help_;
 
   // A weak reference to the parent controller.
   IBOutlet SadTabController* controller_;
--- chrome/chrome_nibs.gypi
+++ chrome/chrome_nibs.gypi
@@ -45,7 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'app/nibs/MainMenu.xib',
       'app/nibs/OneClickSigninBubble.xib',
       'app/nibs/OneClickSigninDialog.xib',
-      'app/nibs/SadTab.xib',
       'app/nibs/SaveAccessoryView.xib',
       'app/nibs/SpeechRecognitionBubble.xib',
       'app/nibs/TaskManager.xib',
@@ -63,6 +62,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'app/nibs/InfoBar.xib',
       'app/nibs/Notification.xib',
       'app/nibs/Panel.xib',
+      'app/nibs/SadTab.xib',
     ],  # mac_untranslated_xibs
     'mac_all_xibs': [
       '<@(mac_translated_xibs)',
