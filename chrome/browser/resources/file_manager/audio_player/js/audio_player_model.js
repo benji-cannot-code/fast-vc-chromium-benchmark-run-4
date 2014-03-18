e62ad4a6fac1296a9d55f2fed2262b408d31386a
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-18 00:30:38 +0000

    [AudioPlayer] Change the default volume value 10 -> 100
    BUG=chrome-os-partner:26345
    TEST=manually tested
    TBR=hirono@chromium.org
    
    Review URL: https://codereview.chromium.org/201973004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257552 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/audio_player/js/audio_player_model.js
+++ chrome/browser/resources/file_manager/audio_player/js/audio_player_model.js
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   var VALUES = Object.freeze({
     shuffle: false,
     repeat: false,
-    volume: 10,
+    volume: 100,
     expanded: false,
   });
 
