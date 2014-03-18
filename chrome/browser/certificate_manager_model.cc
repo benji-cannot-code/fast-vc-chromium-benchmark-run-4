f36ae4182bce74c0d8d1689061c3600afea0281b
tbarzic@chromium.org <tbarzic@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-18 01:29:11 +0000

    Handle cases when user cert database has NULL slots
    The slots can be NULL if the user's username hash is empty string (e.g. in retail mode)
    
    BUG=349085,340505
    TEST=
    Retail mode does not immediately restart (i.e. no crash).
    In retail mode, go to chrome://settings/certificates -> buttons for importing certs should be disabled
    
    Review URL: https://codereview.chromium.org/193273002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257570 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/certificate_manager_model.cc
+++ chrome/browser/certificate_manager_model.cc
@@ -65,9 +65,11 @@ void CertificateManagerModel::Create(
 
 CertificateManagerModel::CertificateManagerModel(
     net::NSSCertDatabase* nss_cert_database,
+    bool is_user_db_available,
     bool is_tpm_available,
     Observer* observer)
     : cert_db_(nss_cert_database),
+      is_user_db_available_(is_user_db_available),
       is_tpm_available_(is_tpm_available),
       observer_(observer) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
@@ -208,13 +210,14 @@ bool CertificateManagerModel::IsHardwareBacked(
 // static
 void CertificateManagerModel::DidGetCertDBOnUIThread(
     net::NSSCertDatabase* cert_db,
+    bool is_user_db_available,
     bool is_tpm_available,
     CertificateManagerModel::Observer* observer,
     const CreationCallback& callback) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
 
-  scoped_ptr<CertificateManagerModel> model(
-      new CertificateManagerModel(cert_db, is_tpm_available, observer));
+  scoped_ptr<CertificateManagerModel> model(new CertificateManagerModel(
+      cert_db, is_user_db_available, is_tpm_available, observer));
   callback.Run(model.Pass());
 }
 
@@ -225,6 +228,7 @@ void CertificateManagerModel::DidGetCertDBOnIOThread(
     net::NSSCertDatabase* cert_db) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
 
+  bool is_user_db_available = cert_db->GetPublicSlot();
   bool is_tpm_available = false;
 #if defined(OS_CHROMEOS)
   is_tpm_available = crypto::IsTPMTokenEnabledForNSS();
@@ -234,6 +238,7 @@ void CertificateManagerModel::DidGetCertDBOnIOThread(
       FROM_HERE,
       base::Bind(&CertificateManagerModel::DidGetCertDBOnUIThread,
                  cert_db,
+                 is_user_db_available,
                  is_tpm_available,
                  observer,
                  callback));
--- chromeos/network/onc/onc_certificate_importer_impl.cc
+++ chromeos/network/onc/onc_certificate_importer_impl.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/network/network_event_log.h"
 #include "chromeos/network/onc/onc_utils.h"
 #include "components/onc/onc_constants.h"
+#include "crypto/scoped_nss_types.h"
 #include "net/base/crypto_module.h"
 #include "net/base/net_errors.h"
 #include "net/cert/nss_cert_database.h"
@@ -319,8 +320,11 @@ bool CertificateImporterImpl::ParseClientCertificate(
   }
 
   // Since this has a private key, always use the private module.
-  scoped_refptr<net::CryptoModule> module(net::CryptoModule::CreateFromHandle(
-      target_nssdb_->GetPrivateSlot().get()));
+  crypto::ScopedPK11Slot private_slot(target_nssdb_->GetPrivateSlot());
+  if (!private_slot)
+    return false;
+  scoped_refptr<net::CryptoModule> module(
+      net::CryptoModule::CreateFromHandle(private_slot.get()));
   net::CertificateList imported_certs;
 
   int import_result = target_nssdb_->ImportFromPKCS12(
