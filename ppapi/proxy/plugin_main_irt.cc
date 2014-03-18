95e20e07a8640b724454f226b4d03d914a7553da
yzshen@chromium.org <yzshen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-18 22:28:36 +0000

    Revert 257743 "Add very simple loading test for non-SFI NaCl."
    Broke Linux ASan+LSan Tests (1).
    
    > Add very simple loading test for non-SFI NaCl.
    > 
    > This is the first step to add test cases for non-SFI NaCl.
    > The testee binary is build by a new target rule introduced by this CL, which is relocatable/libc-free binary. The code has a minimum set of handlers for irt_ppapi, to load the plugin successfully done with communicating hosts (browser/renderer processes).
    > 
    > Along this change, some DCHECK errors are found. So they're fixed, too.
    > 1) AtExitManager is conflicting. In non-SFI mode, AtExitManager is already instantiated in nacl_helper. To avoid the conflicting, skip to create it in plugin_main_irt.cc.
    > 2) Allowed creating Singleton on the plugin's main thread.
    > 3) SRPC may be successfully done, and then NOTREACHED catch it in nacl_listener. It is false alarm, so removed it.
    > 
    > BUG=https://code.google.com/p/nativeclient/issues/detail?id=3734
    > TEST=run ./browser_tests --gtest_filter=NaCl*NonSFI* locally. Run trybots.
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=257118
    > 
    > Review URL: https://codereview.chromium.org/189553005
    
    TBR=hidehiko@chromium.org
    
    Review URL: https://codereview.chromium.org/203993003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257787 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/proxy/plugin_main_irt.cc
+++ ppapi/proxy/plugin_main_irt.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // IPC_MESSAGE_MACROS_LOG_ENABLED so ppapi_messages.h will generate the
 // ViewMsgLog et al. functions.
 
+#include "base/at_exit.h"
 #include "base/command_line.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/message_loop/message_loop.h"
@@ -36,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/shared_impl/ppb_audio_shared.h"
 
 #if defined(__native_client__)
-#include "base/at_exit.h"
 #include "native_client/src/public/chrome_main.h"
 #include "native_client/src/shared/srpc/nacl_srpc.h"
 #endif
@@ -328,12 +328,8 @@ void PpapiPluginRegisterThreadCreator(
 }
 
 int PpapiPluginMain() {
-  // For non-SFI mode, the manager is already instantiated in nacl_helper,
-  // so we don't need to instantiate it here.
-#if defined(__native_client__)
   // Though it isn't referenced here, we must instantiate an AtExitManager.
   base::AtExitManager exit_manager;
-#endif
   base::MessageLoop loop;
 #if defined(IPC_MESSAGE_LOG_ENABLED)
   IPC::Logging::set_log_function_map(&g_log_function_mapping);
