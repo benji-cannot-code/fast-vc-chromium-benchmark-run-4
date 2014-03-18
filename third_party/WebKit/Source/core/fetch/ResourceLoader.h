418de16c3747fdb1dd4f96c3c08634b82074fb27
morrita@chromium.org <morrita@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-18 23:15:02 +0000

    HTML Imports: Send credentials for same origin requests
    This captures following spec chagne: https://www.w3.org/Bugs/Public/show_bug.cgi?id=24905
    The essential part of the change is in HTMLImportsController.cpp.
    Anything else is to make it work with redirect.
    
    The problem here is that allowCredentials flag is held both
    by ResourceLoaderOptions and ResourceRequest and these two
    can go out-of-sync. This change tries to make them in sync.
    
    Such a state duplication should be resolved eventually, but
    that is another story.
    
    BUG=348671
    TEST=import-cors-credentials.html
    R=abarth, dglazkov@chromium.org
    
    Review URL: https://codereview.chromium.org/196043002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169496 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/fetch/ResourceLoader.h
+++ third_party/WebKit/Source/core/fetch/ResourceLoader.h
@@ -94,6 +94,8 @@ private:
 
     bool responseNeedsAccessControlCheck() const;
 
+    ResourceRequest& applyOptions(ResourceRequest&) const;
+
     OwnPtr<blink::WebURLLoader> m_loader;
     RefPtr<ResourceLoaderHost> m_host;
 
