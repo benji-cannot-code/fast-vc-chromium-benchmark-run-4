99128da528fd9de00bd3ad8cfc9332317d1e93cf
raymes@chromium.org <raymes@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-18 07:13:48 +0000

    Make it possible to override the tickmarks for a WebFrame.
    This makes it possible to override the default tickmark behavior for a frame.
    The default behavior is to display tickmarks for find results. However the PDF
    plugin handles finding itself but still wants to set tickmarks. This CL makes
    it possible to specify an array of tickmarks to display.
    
    BUG=303491
    
    Review URL: https://codereview.chromium.org/198683002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169423 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebFrameImpl.h
+++ third_party/WebKit/Source/web/WebFrameImpl.h
@@ -212,6 +212,7 @@ public:
     virtual WebFloatRect activeFindMatchRect() OVERRIDE;
     virtual void findMatchRects(WebVector<WebFloatRect>&) OVERRIDE;
     virtual int selectNearestFindMatch(const WebFloatPoint&, WebRect* selectionRect) OVERRIDE;
+    virtual void setTickmarks(const WebVector<WebRect>&) OVERRIDE;
 
     virtual void sendOrientationChangeEvent(int orientation) OVERRIDE;
 
