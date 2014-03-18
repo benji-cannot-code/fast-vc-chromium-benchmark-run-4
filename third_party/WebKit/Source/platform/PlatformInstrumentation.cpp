547b624580d16da86c02e10e947a19a4327acbde
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-18 14:03:34 +0000

    Only use devtools category for devtools instrumentation trace events
    - change PlatformInstrumentation events to use "devtools" as category
    - change category filter to only include "devtools"
    
    BUG=
    R=pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/180783016
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169445 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/PlatformInstrumentation.cpp
+++ third_party/WebKit/Source/platform/PlatformInstrumentation.cpp
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-const char PlatformInstrumentation::CategoryName[] = "webkit";
+const char PlatformInstrumentation::CategoryName[] = "devtools";
 
 const char PlatformInstrumentation::ImageDecodeEvent[] = "Decode Image";
 const char PlatformInstrumentation::ImageResizeEvent[] = "Resize Image";
