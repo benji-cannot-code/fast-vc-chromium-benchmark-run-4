8b98549694aae1a050881280c07f084d29d97c6f
samuong@chromium.org <samuong@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-18 21:03:12 +0000

    Prevent browser from forwarding Page.handleJavaScriptDialog to Blink.
    The Blink implementation was previously an empty implementation, and was removed recently in https://codereview.chromium.org/188733003.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/202303004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257751 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/devtools/renderer_overrides_handler.cc
+++ content/browser/devtools/renderer_overrides_handler.cc
@@ -308,7 +308,7 @@ RendererOverridesHandler::PageHandleJavaScriptDialog(
           web_contents->GetDelegate()->GetJavaScriptDialogManager();
       if (manager && manager->HandleJavaScriptDialog(
               web_contents, accept, prompt_override_ptr)) {
-        return NULL;
+        return command->SuccessResponse(new base::DictionaryValue());
       }
     }
   }
