aee44a8e5df70116200013af4120174e01eaac6c
acolwell@chromium.org <acolwell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-18 16:23:56 +0000

    Fix DCHECK caused by specifying a negative timestamp offset.
    Timestamp offsets are allowed to be negative, but a DCHECK in
    DoubleToDelta() was causing a crash in debug builds. This patch simply
    moves the "not negative" DCHECK to the methods that actually need to
    verify that constraint.
    
    BUG=353245
    TEST=LayoutTest that verifies this fix is in https://codereview.chromium.org/202423002/.
    
    Review URL: https://codereview.chromium.org/198213008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257657 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/websourcebuffer_impl.cc
+++ content/renderer/media/websourcebuffer_impl.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/renderer/media/websourcebuffer_impl.h"
 
+#include <limits>
+
 #include "base/float_util.h"
 #include "media/filters/chunk_demuxer.h"
 
@@ -12,7 +14,8 @@ namespace content {
 
 static base::TimeDelta DoubleToTimeDelta(double time) {
   DCHECK(!base::IsNaN(time));
-  DCHECK_GE(time, 0);
+  DCHECK_NE(time, -std::numeric_limits<double>::infinity());
+
   if (time == std::numeric_limits<double>::infinity())
     return media::kInfiniteDuration();
 
@@ -90,6 +93,8 @@ void WebSourceBufferImpl::abort() {
 }
 
 void WebSourceBufferImpl::remove(double start, double end) {
+  DCHECK_GE(start, 0);
+  DCHECK_GE(end, 0);
   demuxer_->Remove(id_, DoubleToTimeDelta(start), DoubleToTimeDelta(end));
 }
 
@@ -102,10 +107,12 @@ bool WebSourceBufferImpl::setTimestampOffset(double offset) {
 }
 
 void WebSourceBufferImpl::setAppendWindowStart(double start) {
+  DCHECK_GE(start, 0);
   append_window_start_ = DoubleToTimeDelta(start);
 }
 
 void WebSourceBufferImpl::setAppendWindowEnd(double end) {
+  DCHECK_GE(end, 0);
   append_window_end_ = DoubleToTimeDelta(end);
 }
 
