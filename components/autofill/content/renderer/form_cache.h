b2f3fd94d85dd60633be9e3ee9b7d53a1cc31fc8
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-18 19:08:48 +0000

    Revert 257476 "Add supports that allow Autofill to be initiated ..."
    Caused a NULL pointer dereference crash.
    
    BUG=353549
    
    > Add supports that allow Autofill to be initiated from textarea field
    > 
    > Handling both keyboard and mouse events in textarea that initiate
    > autofill preview and fill form actions. Add test cases from browser
    > tests aspect to cover page click and findformfieldfortextarea cases.
    > 
    > BUG=332557
    > R=isherman@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/140093005
    
    TBR=ziran.sun@samsung.com
    
    Review URL: https://codereview.chromium.org/203543003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257708 0039d316-1c4b-4281-b951-d872f2087c98

--- components/autofill/content/renderer/form_cache.h
+++ components/autofill/content/renderer/form_cache.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 class WebDocument;
-class WebFormControlElement;
 class WebFormElement;
 class WebFrame;
 class WebInputElement;
@@ -52,7 +51,7 @@ class FormCache {
 
   // Clears the values of all input elements in the form that contains
   // |element|.  Returns false if the form is not found.
-  bool ClearFormWithElement(const blink::WebFormControlElement& element);
+  bool ClearFormWithElement(const blink::WebInputElement& element);
 
   // For each field in the |form|, sets the field's placeholder text to the
   // field's overall predicted type.  Also sets the title to include the field's
