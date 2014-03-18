699f3e9af9e53206ef67eaea4a3c14c7ffec545b
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-18 14:16:13 +0000

    Show live objects with given allocation call stack
    When allocation stack trace is selected in the allocation view summary view underneath it is populated with information about live heap objects allocated with that stack trace.
    
    BUG=277984
    R=alph@chromium.org
    
    Review URL: https://codereview.chromium.org/203403002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169451 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/HeapSnapshotProxy.js
+++ third_party/WebKit/Source/devtools/front_end/HeapSnapshotProxy.js
@@ -433,6 +433,10 @@ WebInspector.HeapSnapshotProxy.prototype = {
         this.callMethod(callback, "allocationTracesTops");
     },
 
+    /**
+     * @param {number} nodeId
+     * @param {function(!WebInspector.HeapSnapshotCommon.AllocationNodeCallers)} callback
+     */
     allocationNodeCallers: function(nodeId, callback)
     {
         this.callMethod(callback, "allocationNodeCallers", nodeId);
