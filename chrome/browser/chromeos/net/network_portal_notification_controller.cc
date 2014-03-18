51b12948c55757f85921231dcfa2754e06a0820e
ygorshenin@chromium.org <ygorshenin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-18 17:25:07 +0000

    Changed UI for captive portal notification: * removed bottom bar with button * changed body message * changed icons for notification and network badge
    BUG=240237
    TEST=none
    R=nkostylev@chromium.org, oshima@chromium.org
    
    Review URL: https://codereview.chromium.org/201423004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257674 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/net/network_portal_notification_controller.cc
+++ chrome/browser/chromeos/net/network_portal_notification_controller.cc
@@ -56,8 +56,7 @@ class NetworkPortalNotificationControllerDelegate
   virtual void Display() OVERRIDE {}
   virtual void Error() OVERRIDE {}
   virtual void Close(bool /* by_user */) OVERRIDE {}
-  virtual void Click() OVERRIDE {}
-  virtual void ButtonClick(int button_index) OVERRIDE;
+  virtual void Click() OVERRIDE;
 
  private:
   virtual ~NetworkPortalNotificationControllerDelegate() {}
@@ -65,10 +64,7 @@ class NetworkPortalNotificationControllerDelegate
   DISALLOW_COPY_AND_ASSIGN(NetworkPortalNotificationControllerDelegate);
 };
 
-void NetworkPortalNotificationControllerDelegate::ButtonClick(
-    int button_index) {
-  if (button_index)
-    return;
+void NetworkPortalNotificationControllerDelegate::Click() {
   Profile* profile = ProfileManager::GetActiveUserProfile();
   if (!profile)
     return;
@@ -114,12 +110,6 @@ void NetworkPortalNotificationController::OnPortalDetectionCompleted(
       message_center::NotifierId::SYSTEM_COMPONENT,
       ash::system_notifier::kNotifierNetworkPortalDetector);
 
-  message_center::ButtonInfo signin_button(l10n_util::GetStringUTF16(
-      IDS_PORTAL_DETECTION_NOTIFICATION_SIGNIN_BUTTON));
-  signin_button.icon = bundle.GetImageNamed(IDR_PORTAL_DETECTION_GLOBE);
-  message_center::RichNotificationData data;
-  data.buttons.push_back(signin_button);
-
   scoped_ptr<Notification> notification(new Notification(
       message_center::NOTIFICATION_TYPE_SIMPLE,
       kNotificationId,
@@ -129,7 +119,7 @@ void NetworkPortalNotificationController::OnPortalDetectionCompleted(
       icon,
       base::string16() /* display_source */,
       notifier_id,
-      data,
+      message_center::RichNotificationData(),
       new NetworkPortalNotificationControllerDelegate()));
   notification->SetSystemPriority();
 
