b4ee2b6040c914cb7a6bf32f9b7d9ab3e5c8e906
aboxhall@chromium.org <aboxhall@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-18 15:28:24 +0000

    De-flake and re-enable AutomationApiTest
    BUG=353039
    
    Review URL: https://codereview.chromium.org/201213006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257638 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/api_test/automation/sanity_check/test.js
+++ chrome/test/data/extensions/api_test/automation/sanity_check/test.js
@@ -11,6 +11,7 @@ var assertTrue = chrome.test.assertTrue;
 var RemoveUntestedStates = function(state) {
   delete state['vertical'];
   delete state['horizontal'];
+  delete state['hovered'];
 };
 
 var allTests = [
