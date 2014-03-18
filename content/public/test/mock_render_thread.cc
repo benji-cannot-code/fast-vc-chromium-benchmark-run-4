e607f5af641480bf49d6c5eb50d348ba429c413a
jbauman@chromium.org <jbauman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-18 07:31:15 +0000

    Don't map memory in MockRenderThread::HostAllocateSharedMemory
    This matches the implementation in RenderThreadImpl, and avoids leaking a bit of memory in some tests.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/196413018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257607 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/test/mock_render_thread.cc
+++ content/public/test/mock_render_thread.cc
@@ -149,7 +149,7 @@ scoped_ptr<base::SharedMemory>
     MockRenderThread::HostAllocateSharedMemoryBuffer(
         size_t buffer_size) {
   scoped_ptr<base::SharedMemory> shared_buf(new base::SharedMemory);
-  if (!shared_buf->CreateAndMapAnonymous(buffer_size)) {
+  if (!shared_buf->CreateAnonymous(buffer_size)) {
     NOTREACHED() << "Cannot map shared memory buffer";
     return scoped_ptr<base::SharedMemory>();
   }
