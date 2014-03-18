952252edfcc8f4f8ed8cf3dcf919cefdcacfb4b0
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-18 23:59:00 +0000

    Compile fix for Clang
    BUG=346399
    TEST=none
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257818 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/input/input.cc
+++ chrome/browser/extensions/api/input/input.cc
@@ -22,12 +22,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/keyboard/keyboard_util.h"
 #endif
 
+#if !defined(USE_ASH)
 namespace {
 
 const char kNotYetImplementedError[] =
     "API is not implemented on this platform.";
 
 }  // namespace
+#endif
 
 namespace extensions {
 
