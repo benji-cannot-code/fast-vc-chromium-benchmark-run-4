96969f78ba87406c03f9f2360fbfb67b59dd533c
pilgrim@chromium.org <pilgrim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-18 16:22:21 +0000

    Merge weburlrequest_extradata_impl.cc/h into content/child/request_extra_data.cc/h. Change to empty constructor and use individual setters for each value.
    BUG=338338
    TBR=darin@chromium.org
    
    Review URL: https://codereview.chromium.org/182383015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257655 0039d316-1c4b-4281-b951-d872f2087c98

--- content/child/web_url_loader_impl.cc
+++ content/child/web_url_loader_impl.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "content/child/blink_platform_impl.h"
 #include "content/child/ftp_directory_listing_response_delegate.h"
+#include "content/child/request_extra_data.h"
 #include "content/common/resource_request_body.h"
 #include "net/base/data_url.h"
 #include "net/base/load_flags.h"
@@ -35,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/web/WebSecurityPolicy.h"
 #include "webkit/child/multipart_response_delegate.h"
 #include "webkit/child/resource_loader_bridge.h"
-#include "webkit/child/weburlrequest_extradata_impl.h"
 #include "webkit/child/weburlresponse_extradata_impl.h"
 
 using base::Time;
