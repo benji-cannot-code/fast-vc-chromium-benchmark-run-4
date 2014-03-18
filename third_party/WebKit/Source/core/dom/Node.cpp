0c36fd50a011fca691cd8e5f7004882bfb1cffac
zmo@chromium.org <zmo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-18 00:38:56 +0000

    Revert 169394 "Node::setTextContent should avoid work when nothi..."
    Might have broken countless layout tests (ok I am exaggerating) on almost all platforms
    
    > Node::setTextContent should avoid work when nothing changes
    > 
    > We already had code to handle this case, which is shared
    > by editing as well as HTMLElement::setInnerText.
    > 
    > replaceChildrenWithText does the right thing to avoid
    > doing work when the text content hasn't changed, etc.
    > 
    > BUG=352836
    > R=jchaffraix@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/200763006
    
    TBR=eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/202883002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169401 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Node.cpp
+++ third_party/WebKit/Source/core/dom/Node.cpp
@@ -60,7 +60,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/shadow/InsertionPoint.h"
 #include "core/dom/shadow/ShadowRoot.h"
 #include "core/editing/htmlediting.h"
-#include "core/editing/markup.h"
 #include "core/events/BeforeLoadEvent.h"
 #include "core/events/Event.h"
 #include "core/events/EventDispatchMediator.h"
@@ -1499,7 +1498,11 @@ void Node::setTextContent(const String& text)
         case ELEMENT_NODE:
         case ATTRIBUTE_NODE:
         case DOCUMENT_FRAGMENT_NODE: {
-            replaceChildrenWithText(toContainerNode(this), text, ASSERT_NO_EXCEPTION);
+            RefPtr<ContainerNode> container = toContainerNode(this);
+            ChildListMutationScope mutation(*this);
+            container->removeChildren();
+            if (!text.isEmpty())
+                container->appendChild(document().createTextNode(text), ASSERT_NO_EXCEPTION);
             return;
         }
         case DOCUMENT_NODE:
