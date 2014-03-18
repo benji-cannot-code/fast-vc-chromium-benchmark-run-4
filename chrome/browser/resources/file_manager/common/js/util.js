a41621343bda1b0336746f3ca2e755ca3247edd1
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-18 00:31:59 +0000

    [Files.app] Fix typo (fullfilled -> fulfilled)
    BUG=none
    TBR=hirono@chromium.org
    # TBRing for fixing just typo.
    
    Review URL: https://codereview.chromium.org/196413019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257554 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/common/js/util.js
+++ chrome/browser/resources/file_manager/common/js/util.js
@@ -1197,7 +1197,7 @@ util.isTeleported = function(window) {
     window.chrome.fileBrowserPrivate.getProfiles(function(profiles,
                                                           currentId,
                                                           displayedId) {
-      onFullfilled(currentId !== displayedId);
+      onFulfilled(currentId !== displayedId);
     });
   });
 };
