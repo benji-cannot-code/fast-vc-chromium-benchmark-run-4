a066dacc915de1d97c5cba5e1b3d964b1e02c2a6
yoav@yoav.ws <yoav@yoav.ws@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-18 22:57:15 +0000

    Fixed an assert related to MediaQuerytokenizer code points table
    The assert was a result of an off-by-one error in both the array's definition and the assert itself.
    
    BUG=353577
    
    Review URL: https://codereview.chromium.org/203343004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169494 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/parser/MediaQueryTokenizer.cpp
+++ third_party/WebKit/Source/core/css/parser/MediaQueryTokenizer.cpp
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-const unsigned codePointsNumber = SCHAR_MAX;
+const unsigned codePointsNumber = SCHAR_MAX + 1;
 
 class MediaQueryTokenizer::CodePoints {
 public:
