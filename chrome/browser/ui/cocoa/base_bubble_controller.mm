951497f2b6cdc34895a1c3c7877d1df8aaa91679
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-18 15:39:35 +0000

    [Mac] Manage extension popups more reasonably.
    There were two opportunities for an ExtensionPopupController to be released,
    in +[ExtensionPopupController showURL:...], which called -close and
    -autorelease, and in -[BaseBubbleController windowWillClose:], which would be
    triggered by -close and would also call -autorelease. This result in
    over-freeing the object, which was observed causing ZombieObjectCrashes
    (typically -windowDidResignKey: was the first message sent to the deallocated
    ExtensionPopupController).
    
    The main active ingredient in this change is removing the extra -autorelease
    in +[ExtensionPopupController showURL:...]. Additional care is taken regarding
    management of extension_popup_controller.mm's gObject. Some cleanup tasks in
    BaseBubbleController are moved from -close to -windowWillClose:, because
    -close was observed as being too early.
    
    The popup identity comparison in ExtensionViewHostMac was simplified to only
    consider an ExtensionPopupController as belonging to an EVHM if the object
    matched, without comparing the extension ID. The extension ID check could make
    it close an unrelated popup when repeatedly clicking on an extension icon.
    
    TEST=Repeatedly click on an extension icon. Vary the speed, about 1Hz and
         faster. Doing this for 30 seconds would be enough to see a
         ZombieObjectCrash or other bad behavior (such as a
         disappearing-reappearing main browser window).
    BUG=351278
    R=rsesek@chromium.org
    
    Review URL: https://codereview.chromium.org/200513005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257641 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/base_bubble_controller.mm
+++ chrome/browser/ui/cocoa/base_bubble_controller.mm
@@ -140,8 +140,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)windowWillClose:(NSNotification*)notification {
+  // The bubble will be closing, so remove the event taps.
+  if (eventTap_) {
+    [NSEvent removeMonitor:eventTap_];
+    eventTap_ = nil;
+  }
+  if (resignationObserver_) {
+    [[NSNotificationCenter defaultCenter]
+        removeObserver:resignationObserver_
+                  name:NSWindowDidResignKeyNotification
+                object:nil];
+    resignationObserver_ = nil;
+  }
+
+  tabStripObserverBridge_.reset();
+
+  [[[self window] parentWindow] removeChildWindow:[self window]];
+
   // We caught a close so we don't need to watch for the parent closing.
   [[NSNotificationCenter defaultCenter] removeObserver:self];
+
   [self autorelease];
 }
 
@@ -160,26 +178,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   else
     [window orderFront:nil];
   [self registerKeyStateEventTap];
-}
-
-- (void)close {
-  // The bubble will be closing, so remove the event taps.
-  if (eventTap_) {
-    [NSEvent removeMonitor:eventTap_];
-    eventTap_ = nil;
-  }
-  if (resignationObserver_) {
-    [[NSNotificationCenter defaultCenter]
-        removeObserver:resignationObserver_
-                  name:NSWindowDidResignKeyNotification
-                object:nil];
-    resignationObserver_ = nil;
-  }
-
-  tabStripObserverBridge_.reset();
-
-  [[[self window] parentWindow] removeChildWindow:[self window]];
-  [super close];
 }
 
 // The controller is the delegate of the window so it receives did resign key
