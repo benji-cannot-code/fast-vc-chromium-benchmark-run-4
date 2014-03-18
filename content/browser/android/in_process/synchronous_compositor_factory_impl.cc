295292df9d1a90cf0593810a1e825c95d0a00930
boliu@chromium.org <boliu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-18 16:03:36 +0000

    Clean up unsued instance var in SynchronousCompositorFactoryImpl
    wrapped_gl_context_for_main_thread_ is now unused so remove it.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/183063013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257654 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/android/in_process/synchronous_compositor_factory_impl.cc
+++ content/browser/android/in_process/synchronous_compositor_factory_impl.cc
@@ -101,8 +101,7 @@ class VideoContextProvider
 using webkit::gpu::WebGraphicsContext3DInProcessCommandBufferImpl;
 
 SynchronousCompositorFactoryImpl::SynchronousCompositorFactoryImpl()
-    : wrapped_gl_context_for_main_thread_(NULL),
-      wrapped_gl_context_for_compositor_thread_(NULL),
+    : wrapped_gl_context_for_compositor_thread_(NULL),
       num_hardware_compositors_(0) {
   SynchronousCompositorFactory::SetInstance(this);
 }
@@ -133,7 +132,6 @@ SynchronousCompositorFactoryImpl::GetOffscreenContextProviderForMainThread() {
        offscreen_context_for_main_thread_->DestroyedOnMainThread())) {
     scoped_ptr<gpu::GLInProcessContext> context =
         CreateContext(NULL, NULL, NULL);
-    wrapped_gl_context_for_main_thread_ = context.get();
     offscreen_context_for_main_thread_ =
         webkit::gpu::ContextProviderInProcess::Create(
             WrapContext(context.Pass()),
@@ -144,7 +142,6 @@ SynchronousCompositorFactoryImpl::GetOffscreenContextProviderForMainThread() {
 
   if (failed) {
     offscreen_context_for_main_thread_ = NULL;
-    wrapped_gl_context_for_main_thread_ = NULL;
   }
   return offscreen_context_for_main_thread_;
 }
