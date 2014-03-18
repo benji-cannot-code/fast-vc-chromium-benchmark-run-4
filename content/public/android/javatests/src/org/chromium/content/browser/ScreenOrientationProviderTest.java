861ade449e2c67c6536f3225afd0290e888bfd8a
qsr@chromium.org <qsr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-18 10:34:12 +0000

    Disable test failings on bots.
    org.chromium.content.browser.ScreenOrientationProviderTest#testLandscape
    is failing on the bots and is disabled.
    
    BUG=353500
    TBR=bulach@chromium.org
    R=mlamouri@chromium.org
    
    Review URL: https://codereview.chromium.org/199413024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257617 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/javatests/src/org/chromium/content/browser/ScreenOrientationProviderTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/ScreenOrientationProviderTest.java
@@ -8,6 +8,7 @@ package org.chromium.content.browser;
 import android.test.suitebuilder.annotation.MediumTest;
 import android.test.suitebuilder.annotation.SmallTest;
 
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.UrlUtils;
 import org.chromium.content.browser.test.util.CriteriaHelper;
@@ -143,8 +144,12 @@ public class ScreenOrientationProviderTest extends ContentShellTestBase {
                 ScreenOrientationValues.PORTRAIT_SECONDARY));
     }
 
-    @MediumTest
-    @Feature({"ScreenOrientation"})
+    /**
+     * @MediumTest
+     * @Feature({"ScreenOrientation"})
+     * http://crbug.com/353500
+     */
+    @DisabledTest
     public void testLandscape() throws Exception {
         lockOrientationAndWait(ScreenOrientationValues.LANDSCAPE_PRIMARY);
         assertTrue(checkOrientationForLock(ScreenOrientationValues.LANDSCAPE_PRIMARY));
