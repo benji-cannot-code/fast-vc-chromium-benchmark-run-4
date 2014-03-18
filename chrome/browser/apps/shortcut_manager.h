899b66220ed6e82bcd0e85f52cfe61cc02257ebe
calamity@chromium.org <calamity@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-18 15:58:23 +0000

    Remove unused WeakPtrFactory from AppShortcutManager.
    This CL removes an unused member from AppShortcutManager.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/196713002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257650 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/apps/shortcut_manager.h
+++ chrome/browser/apps/shortcut_manager.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_APPS_SHORTCUT_MANAGER_H_
 #define CHROME_BROWSER_APPS_SHORTCUT_MANAGER_H_
 
-#include "base/memory/weak_ptr.h"
 #include "chrome/browser/profiles/profile_info_cache_observer.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "content/public/browser/notification_observer.h"
@@ -51,9 +50,6 @@ class AppShortcutManager : public KeyedService,
   Profile* profile_;
   bool is_profile_info_cache_observer_;
   PrefService* prefs_;
-
-  // Fields used when installing application shortcuts.
-  base::WeakPtrFactory<AppShortcutManager> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(AppShortcutManager);
 };
