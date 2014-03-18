49f517d94eafcd6cdaf3059049fd2f217c5faa7f
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-18 07:09:04 +0000

    Reapply 169371 "DevTools: defer styles delta calculation to until the end of the task"
    BUG=353257
    TBR=vsevik
    
    Review URL: https://codereview.chromium.org/202893003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169422 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorBaseAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorBaseAgent.cpp
@@ -95,5 +95,11 @@ void InspectorAgentRegistry::discardAgents()
         m_agents[i]->discardAgent();
 }
 
+void InspectorAgentRegistry::flushPendingFrontendMessages()
+{
+    for (size_t i = 0; i < m_agents.size(); i++)
+        m_agents[i]->flushPendingFrontendMessages();
+}
+
 } // namespace WebCore
 
