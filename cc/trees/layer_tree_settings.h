a1bd125bc6e2d5be5ba00d9c4ea6aa0f8ad5d8e2
anton@chromium.org <anton@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-18 22:36:55 +0000

    Add --disable-low-res-tiling. Disable low res tiling to save power.
    Generating low resolution tiles uses about 10% of the power used by
    pages that have gif animations (measured on Nexus 4). These are the
    most common pages on which chrome consumes above baseline power.
    
    It is unclear whether low resolutions are still valuable. It is
    likely that having low resolution tiles is more useful on older
    devices. For the moment providing this as a flag, so that it
    is easier to investigate whether dropping low resolution tiles
    is reasonable. Default behavior is unchanged, low resolution tiles
    are enabled.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/196473007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257790 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/trees/layer_tree_settings.h
+++ cc/trees/layer_tree_settings.h
@@ -37,6 +37,7 @@ class CC_EXPORT LayerTreeSettings {
     GpuRasterization,
   };
   RasterizationSite rasterization_site;
+  bool create_low_res_tiling;
 
   enum ScrollbarAnimator {
     NoAnimator,
