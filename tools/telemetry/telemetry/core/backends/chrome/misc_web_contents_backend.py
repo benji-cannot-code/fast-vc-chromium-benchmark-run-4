1e852d035cf29062649ad838625cea62db6d8190
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-19 22:48:19 +0000

    Only restart ui if we're logged in.
    * Switch to using cryptohome --action=is_mounted instead of manually checking file mounts.
    * oobe_exists can throw an exception as we log in, so handle this.
    * Use the same mechanism for determining if guest is logged in.
    * Move CryptohomePath and the mount-based IsCryptohomeMounted to CrOSTest.
    * Fixup CrOSTest for guest mode.
    
    BUG=
    TEST=manual
    NOTRY=True
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=257996
    
    Review URL: https://codereview.chromium.org/203973003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258135 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/backends/chrome/misc_web_contents_backend.py
+++ tools/telemetry/telemetry/core/backends/chrome/misc_web_contents_backend.py
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 from telemetry.core import web_contents
+from telemetry.core import util
 from telemetry.core.backends.chrome import inspector_backend_list
 
 
@@ -19,7 +20,10 @@ class MiscWebContentsBackend(inspector_backend_list.InspectorBackendList):
 
   @property
   def oobe_exists(self):
-    """Lightweight property to determine if the oobe webui is visible."""
+    """Lightweight property to determine if the oobe webui is visible. Note that
+    the browser goes away when we're logging in, we wait for it to be ready.
+    """
+    util.WaitFor(self._browser_backend.HasBrowserFinishedLaunching, timeout=30)
     return bool(len(self))
 
   def GetOobe(self):
