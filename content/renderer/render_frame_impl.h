a02392efd130c645957cb68fda61b49655168233
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-19 23:39:48 +0000

    Add didChangeSelection to RenderFrameImpl which just calls out to RenderViewImpl for now. This is in preparation for changes on the Blink side to call out to WebFrameClient.
    BUG=304341
    R=nasko@chromium.org
    
    Review URL: https://codereview.chromium.org/205373003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258153 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_frame_impl.h
+++ content/renderer/render_frame_impl.h
@@ -264,6 +264,7 @@ class CONTENT_EXPORT RenderFrameImpl
   virtual void didNavigateWithinPage(blink::WebFrame* frame,
                                      bool is_new_navigation);
   virtual void didUpdateCurrentHistoryItem(blink::WebFrame* frame);
+  virtual void didChangeSelection(bool is_empty_selection);
   virtual void showContextMenu(const blink::WebContextMenuData& data);
   virtual void clearContextMenu();
   virtual void willRequestAfterPreconnect(blink::WebFrame* frame,
