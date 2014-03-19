a5bf2371d54b3b5762e7a365d61a4a8a4f7c3dc6
imcheng@chromium.org <imcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-19 20:43:44 +0000

    Cast: Fix crash in cast streaming api if starting stream before UDP.
    If the user of cast streaming api tries to call rtpStream.start before
    udpTransport.setDestination, CastSessionDelegate now detects this and
    calls rtpStream.onError instead of crashing due to null pointer.
    The CastRtpStream object will be reset as well.
    Since the code to call CastSessionDelegate is asynchronous,
    rtpStream.onStarted will still be called in this case.
    rtpStream.onError will be called later when the
    CastSessionDelegate code actually runs.
    
    Consolidated comments by moving them from
    cast_session_delegate.h to cast_session.h.
    
    BUG=351506
    
    Review URL: https://codereview.chromium.org/195993004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258081 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/media/cast_session.cc
+++ chrome/renderer/media/cast_session.cc
@@ -25,7 +25,8 @@ CastSession::~CastSession() {
 }
 
 void CastSession::StartAudio(const media::cast::AudioSenderConfig& config,
-                             const AudioFrameInputAvailableCallback& callback) {
+                             const AudioFrameInputAvailableCallback& callback,
+                             const ErrorCallback& error_callback) {
   DCHECK(content::RenderThread::Get()
              ->GetMessageLoop()
              ->message_loop_proxy()
@@ -36,11 +37,13 @@ void CastSession::StartAudio(const media::cast::AudioSenderConfig& config,
       base::Bind(&CastSessionDelegate::StartAudio,
                  base::Unretained(delegate_.get()),
                  config,
-                 media::BindToCurrentLoop(callback)));
+                 media::BindToCurrentLoop(callback),
+                 media::BindToCurrentLoop(error_callback)));
 }
 
 void CastSession::StartVideo(const media::cast::VideoSenderConfig& config,
-                             const VideoFrameInputAvailableCallback& callback) {
+                             const VideoFrameInputAvailableCallback& callback,
+                             const ErrorCallback& error_callback) {
   DCHECK(content::RenderThread::Get()
              ->GetMessageLoop()
              ->message_loop_proxy()
@@ -51,7 +54,8 @@ void CastSession::StartVideo(const media::cast::VideoSenderConfig& config,
       base::Bind(&CastSessionDelegate::StartVideo,
                  base::Unretained(delegate_.get()),
                  config,
-                 media::BindToCurrentLoop(callback)));
+                 media::BindToCurrentLoop(callback),
+                 media::BindToCurrentLoop(error_callback)));
 }
 
 void CastSession::StartUDP(const net::IPEndPoint& local_endpoint,
