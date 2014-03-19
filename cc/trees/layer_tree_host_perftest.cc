0d0c6cbefe246ca4f6f63d623284616e1e9d2ab4
clholgat@chromium.org <clholgat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-19 21:16:50 +0000

    Fix and reenable BrowserCompositorInvalidateLayerTreePerfTest.DenseBrowserUI
    Resolved the race condition that lead to the test
    timing out most of the time.
    
    BUG=352683
    
    Review URL: https://codereview.chromium.org/198923006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258098 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/trees/layer_tree_host_perftest.cc
+++ cc/trees/layer_tree_host_perftest.cc
@@ -256,7 +256,9 @@ class BrowserCompositorInvalidateLayerTreePerfTest
     : public LayerTreeHostPerfTestJsonReader {
  public:
   BrowserCompositorInvalidateLayerTreePerfTest()
-      : next_sync_point_(1), clean_up_started_(false) {}
+      : LayerTreeHostPerfTestJsonReader(),
+        next_sync_point_(1),
+        clean_up_started_(false) {}
 
   virtual void BuildTree() OVERRIDE {
     LayerTreeHostPerfTestJsonReader::BuildTree();
@@ -270,6 +272,8 @@ class BrowserCompositorInvalidateLayerTreePerfTest
   }
 
   virtual void WillCommit() OVERRIDE {
+    if (CleanUpStarted())
+      return;
     gpu::Mailbox gpu_mailbox;
     std::ostringstream name_stream;
     name_stream << "name" << next_sync_point_;
@@ -289,11 +293,6 @@ class BrowserCompositorInvalidateLayerTreePerfTest
     layer_tree_host()->SetNeedsCommit();
   }
 
-  virtual void DidCommitAndDrawFrame() OVERRIDE {
-    if (CleanUpStarted())
-      EndTest();
-  }
-
   virtual void CleanUpAndEndTest(LayerTreeHostImpl* host_impl) OVERRIDE {
     clean_up_started_ = true;
     MainThreadTaskRunner()->PostTask(
@@ -306,6 +305,7 @@ class BrowserCompositorInvalidateLayerTreePerfTest
   void CleanUpAndEndTestOnMainThread() {
     tab_contents_->SetTextureMailbox(TextureMailbox(),
                                      scoped_ptr<SingleReleaseCallback>());
+    EndTest();
   }
 
   virtual bool CleanUpStarted() OVERRIDE { return clean_up_started_; }
@@ -316,7 +316,8 @@ class BrowserCompositorInvalidateLayerTreePerfTest
   bool clean_up_started_;
 };
 
-TEST_F(BrowserCompositorInvalidateLayerTreePerfTest, DISABLED_DenseBrowserUI) {
+TEST_F(BrowserCompositorInvalidateLayerTreePerfTest, DenseBrowserUI) {
+  measure_commit_cost_ = true;
   SetTestName("dense_layer_tree");
   ReadTestFile("dense_layer_tree");
   RunTestWithImplSidePainting();
