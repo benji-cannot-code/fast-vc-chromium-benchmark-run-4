aeab57d497ad925c04568736ffbbaef21dab5883
antrim@chromium.org <antrim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-19 18:43:49 +0000

    Update ManagedUserRegistration utility to support new password scheme: 1) Wait until ManagedUserSharedSettings with passwor data will be acknowledged. 2) If ManagedUser has updated signature/encryption key, update ManagdUsers and wait for acknowledgement.
    BUG=244472
    
    Review URL: https://codereview.chromium.org/195303002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258048 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/managed_mode/managed_user_sync_service.h
+++ chrome/browser/managed_mode/managed_user_sync_service.h
@@ -74,6 +74,13 @@ class ManagedUserSyncService : public KeyedService,
                       const std::string& password_signature_key,
                       const std::string& password_encryption_key,
                       int avatar_index);
+  void UpdateManagedUser(const std::string& id,
+                         const std::string& name,
+                         const std::string& master_key,
+                         const std::string& signature_key,
+                         const std::string& encryption_key,
+                         int avatar_index);
+
   void DeleteManagedUser(const std::string& id);
 
   // Updates the managed user avatar only if the managed user has
@@ -121,6 +128,21 @@ class ManagedUserSyncService : public KeyedService,
   explicit ManagedUserSyncService(PrefService* prefs);
 
   void OnLastSignedInUsernameChange();
+
+  scoped_ptr<base::DictionaryValue> CreateDictionary(
+      const std::string& name,
+      const std::string& master_key,
+      const std::string& signature_key,
+      const std::string& encryption_key,
+      int avatar_index);
+
+  void UpdateManagedUserImpl(const std::string& id,
+                             const std::string& name,
+                             const std::string& master_key,
+                             const std::string& signature_key,
+                             const std::string& encryption_key,
+                             int avatar_index,
+                             bool add_user);
 
   void NotifyManagedUserAcknowledged(const std::string& managed_user_id);
   void NotifyManagedUsersSyncingStopped();
