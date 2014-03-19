6bfd0a120fb2f45152448585c35c761104524c71
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-19 23:45:14 +0000

    Oilpan: Prepare to move DatabaseThread and SQLTransactionCoordinator to Oilpan heap.
    BUG=347902
    
    Review URL: https://codereview.chromium.org/201083003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169594 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/webdatabase/SQLTransactionCoordinator.cpp
+++ third_party/WebKit/Source/modules/webdatabase/SQLTransactionCoordinator.cpp
@@ -50,6 +50,11 @@ SQLTransactionCoordinator::SQLTransactionCoordinator()
 {
 }
 
+void SQLTransactionCoordinator::trace(Visitor* visitor)
+{
+    visitor->trace(m_coordinationInfoMap);
+}
+
 void SQLTransactionCoordinator::processPendingTransactions(CoordinationInfo& info)
 {
     if (info.activeWriteTransaction || info.pendingTransactions.isEmpty())
