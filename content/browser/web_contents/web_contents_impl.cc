a3e6cc214c239e311090bc717beb25430d591d3a
miu@chromium.org <miu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-19 19:18:37 +0000

    [Views] WebView will size embedded fullscreen views differently during tab capture.
    This expands on previous fullscreen-within-tab work to improve the user experience during tab capture sessions.  WebView will size an embedded fullscreen view to that of the capture video resolution, thereby preventing unwanted scaling and letterboxing.  When WebView must down-scale the view to fit, it maintains the desired aspect ratio to ensure best possible display on the receiving screen (e.g., a Chromecast).
    
    In addition, this change adds a WebContentsObserver method to notify when entering/leaving tab (i.e., non-Flash) fullscreen.  Thus, for example, HTML5 video fullscreen will also benefit from these improvements.
    
    BUG=256870
    R=joi@chromium.org, sky@chromium.org
    
    Review URL: https://codereview.chromium.org/203093002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258057 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -1257,6 +1257,10 @@ void WebContentsImpl::ToggleFullscreenMode(bool enter_fullscreen) {
 
   if (delegate_)
     delegate_->ToggleFullscreenModeForTab(this, enter_fullscreen);
+
+  FOR_EACH_OBSERVER(WebContentsObserver,
+                    observers_,
+                    DidToggleFullscreenModeForTab(IsFullscreenForCurrentTab()));
 }
 
 bool WebContentsImpl::IsFullscreenForCurrentTab() const {
