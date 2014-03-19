6588a0fa47888b3961bc6757b4474ff0a2e32681
earthdok@chromium.org <earthdok@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-19 18:58:45 +0000

    Fix broken assumption in ResourceProviderTest.TransferSoftwareResources/1.
    The test expects a specific order of array elements. However, the array is
    obtained by iterating over a hash_map, which is unordered. This worked by sheer
    luck in libstdc++ builds, but broke in a libc++ build.
    
    BUG=338382, 318770
    R=ccameron@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/204913002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258052 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/resources/resource_provider_unittest.cc
+++ cc/resources/resource_provider_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 #include <map>
+#include <set>
 
 #include "base/bind.h"
 #include "base/containers/hash_tables.h"
@@ -1004,10 +1005,15 @@ TEST_P(ResourceProviderTest, TransferSoftwareResources) {
     EXPECT_EQ(0u, returned_to_child[1].sync_point);
     EXPECT_EQ(0u, returned_to_child[2].sync_point);
     EXPECT_EQ(0u, returned_to_child[3].sync_point);
-    EXPECT_EQ(id1, returned_to_child[0].id);
-    EXPECT_EQ(id2, returned_to_child[1].id);
-    EXPECT_EQ(id3, returned_to_child[2].id);
-    EXPECT_EQ(id4, returned_to_child[3].id);
+    std::set<ResourceProvider::ResourceId> expected_ids;
+    expected_ids.insert(id1);
+    expected_ids.insert(id2);
+    expected_ids.insert(id3);
+    expected_ids.insert(id4);
+    std::set<ResourceProvider::ResourceId> returned_ids;
+    for(unsigned i = 0; i < 4; i++)
+      returned_ids.insert(returned_to_child[i].id);
+    EXPECT_EQ(expected_ids, returned_ids);
     EXPECT_FALSE(returned_to_child[0].lost);
     EXPECT_FALSE(returned_to_child[1].lost);
     EXPECT_FALSE(returned_to_child[2].lost);
@@ -1079,10 +1085,15 @@ TEST_P(ResourceProviderTest, TransferSoftwareResources) {
   EXPECT_EQ(0u, returned_to_child[1].sync_point);
   EXPECT_EQ(0u, returned_to_child[2].sync_point);
   EXPECT_EQ(0u, returned_to_child[3].sync_point);
-  EXPECT_EQ(id1, returned_to_child[0].id);
-  EXPECT_EQ(id2, returned_to_child[1].id);
-  EXPECT_EQ(id3, returned_to_child[2].id);
-  EXPECT_EQ(id4, returned_to_child[3].id);
+  std::set<ResourceProvider::ResourceId> expected_ids;
+  expected_ids.insert(id1);
+  expected_ids.insert(id2);
+  expected_ids.insert(id3);
+  expected_ids.insert(id4);
+  std::set<ResourceProvider::ResourceId> returned_ids;
+  for(unsigned i = 0; i < 4; i++)
+    returned_ids.insert(returned_to_child[i].id);
+  EXPECT_EQ(expected_ids, returned_ids);
   EXPECT_FALSE(returned_to_child[0].lost);
   EXPECT_FALSE(returned_to_child[1].lost);
   EXPECT_FALSE(returned_to_child[2].lost);
