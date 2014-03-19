3963339ce8b68aa41b4d43ff3e9c5cbbba8941e0
mgiuca@chromium.org <mgiuca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-19 05:50:09 +0000

    CustomFrameView: Do not show borders if the window is full-screen.
    Fixes full screen on Chrome apps on Linux for windows with frame:
    chrome. Previously, the title bar would be visible even in full-screen.
    
    BUG=339370
    TEST=Chrome app with frame: chrome; make full screen (eg
    window-state-sample). Should have no visible borders.
    
    Review URL: https://codereview.chromium.org/196943007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257855 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/window/custom_frame_view.cc
+++ ui/views/window/custom_frame_view.cc
@@ -318,6 +318,9 @@ gfx::Rect CustomFrameView::IconBounds() const {
 }
 
 bool CustomFrameView::ShouldShowTitleBarAndBorder() const {
+  if (frame_->IsFullscreen())
+    return false;
+
   if (ViewsDelegate::views_delegate) {
     return !ViewsDelegate::views_delegate->WindowManagerProvidesTitleBar(
                 frame_->IsMaximized());
