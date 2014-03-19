0312cbcf43b8fd587ec6376f0d17d5f025f03bd3
teravest@chromium.org <teravest@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-19 17:23:18 +0000

    Pepper: Move GetUrlScheme to PPB_NaCl_Private.
    This is part of a series of changes to clean up FileDownloader so that it's
    less dependent on Plugin.
    
    As an added benefit, the code for GetUrlScheme is much shorter once it's on the
    Chromium side.
    
    BUG=239656
    R=dmichael@chromium.org
    
    Review URL: https://codereview.chromium.org/203523003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258027 0039d316-1c4b-4281-b951-d872f2087c98

--- components/nacl/renderer/ppb_nacl_private_impl.cc
+++ components/nacl/renderer/ppb_nacl_private_impl.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/web/WebFrame.h"
 #include "third_party/WebKit/public/web/WebPluginContainer.h"
 #include "third_party/WebKit/public/web/WebView.h"
+#include "url/gurl.h"
 #include "v8/include/v8.h"
 
 namespace {
@@ -560,6 +561,19 @@ const char* GetSandboxArch() {
   return nacl::GetSandboxArch();
 }
 
+PP_UrlSchemeType GetUrlScheme(PP_Var url) {
+  scoped_refptr<ppapi::StringVar> url_string = ppapi::StringVar::FromPPVar(url);
+  if (!url_string)
+    return PP_SCHEME_OTHER;
+
+  GURL gurl(url_string->value());
+  if (gurl.SchemeIs("chrome-extension"))
+    return PP_SCHEME_CHROME_EXTENSION;
+  if (gurl.SchemeIs("data"))
+    return PP_SCHEME_DATA;
+  return PP_SCHEME_OTHER;
+}
+
 const PPB_NaCl_Private nacl_interface = {
   &LaunchSelLdr,
   &StartPpapiProxy,
@@ -578,7 +592,8 @@ const PPB_NaCl_Private nacl_interface = {
   &ReportLoadError,
   &InstanceDestroyed,
   &NaClDebugStubEnabled,
-  &GetSandboxArch
+  &GetSandboxArch,
+  &GetUrlScheme
 };
 
 }  // namespace
--- ppapi/c/private/ppb_nacl_private.h
+++ ppapi/c/private/ppb_nacl_private.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * found in the LICENSE file.
  */
 
-/* From private/ppb_nacl_private.idl modified Wed Mar 12 14:00:41 2014. */
+/* From private/ppb_nacl_private.idl modified Tue Mar 18 11:15:05 2014. */
 
 #ifndef PPAPI_C_PRIVATE_PPB_NACL_PRIVATE_H_
 #define PPAPI_C_PRIVATE_PPB_NACL_PRIVATE_H_
@@ -131,6 +131,12 @@ typedef enum {
   PP_NACL_EVENT_LOADEND,
   PP_NACL_EVENT_CRASH
 } PP_NaClEventType;
+
+typedef enum {
+  PP_SCHEME_CHROME_EXTENSION,
+  PP_SCHEME_DATA,
+  PP_SCHEME_OTHER
+} PP_UrlSchemeType;
 /**
  * @}
  */
@@ -287,6 +293,8 @@ struct PPB_NaCl_Private_1_0 {
    * platform.
    */
   const char* (*GetSandboxArch)(void);
+  /* Returns the scheme type for a given url. */
+  PP_UrlSchemeType (*GetUrlScheme)(struct PP_Var url);
 };
 
 typedef struct PPB_NaCl_Private_1_0 PPB_NaCl_Private;
