2aaee50aa9cdb3da73705b6ab3ec2e48689e2551
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-19 21:52:25 +0000

    cros: Don't prefix test user profile.
    Prefixing test user profile breaks the assumption that
    TestingProfile::kTestUserProfileDir is always in sync with
    chrome::kTestUserProfileDir.
    
    BUG=351243
    
    Review URL: https://codereview.chromium.org/204123002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258114 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_manager_browsertest.cc
+++ chrome/browser/profiles/profile_manager_browsertest.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_paths.h"
+#include "chromeos/chromeos_switches.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #endif
 
@@ -183,8 +184,13 @@ class ProfileManagerCrOSBrowserTest : public ProfileManagerBrowserTest,
                                       public testing::WithParamInterface<bool> {
  protected:
   virtual void SetUpCommandLine(CommandLine* command_line) OVERRIDE {
-    if (GetParam())
+    if (GetParam()) {
       command_line->AppendSwitch(::switches::kMultiProfiles);
+      // Use a user hash other than the default chrome::kTestUserProfileDir
+      // so that the prefix case is tested.
+      command_line->AppendSwitchASCII(chromeos::switches::kLoginProfile,
+                                      "test-user-hash");
+    }
   }
 };
 
@@ -198,11 +204,10 @@ IN_PROC_BROWSER_TEST_P(ProfileManagerCrOSBrowserTest, GetLastUsedProfile) {
 
   const CommandLine& command_line = *CommandLine::ForCurrentProcess();
   if (command_line.HasSwitch(switches::kMultiProfiles)) {
-    profile_path = profile_path.Append(base::FilePath(
-        std::string(chrome::kProfileDirPrefix) + chrome::kTestUserProfileDir));
+    profile_path = profile_path.AppendASCII(
+        std::string(chrome::kProfileDirPrefix) + "test-user-hash");
   } else {
-    profile_path = profile_path.Append(
-        base::FilePath(chrome::kTestUserProfileDir));
+    profile_path = profile_path.AppendASCII(chrome::kTestUserProfileDir);
   }
   EXPECT_EQ(profile_path.value(), last_used_profile->GetPath().value());
 }
