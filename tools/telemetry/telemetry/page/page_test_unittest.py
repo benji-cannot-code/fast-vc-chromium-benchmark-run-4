cf0a16c41a95a554da7bc4d111480a3e9a98f682
bajones@chromium.org <bajones@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-19 16:07:18 +0000

    Revert 257913 "Change the naming convention of compound_action t..."
    This change is suspected of breaking the GPU bots.
    
    > Change the naming convention of compound_action to RunCompoundAction. This unifies
    > the naming convention of json pageset with python pageset.
    > 
    > 
    > BUG=
    > 
    > Review URL: https://codereview.chromium.org/202213005
    
    TBR=nednguyen@google.com
    
    Review URL: https://codereview.chromium.org/204013008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257988 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/page/page_test_unittest.py
+++ tools/telemetry/telemetry/page/page_test_unittest.py
@@ -32,7 +32,7 @@ class PageTestUnitTest(unittest.TestCase):
     super(PageTestUnitTest, self).setUp()
     all_page_actions.RegisterClassForTest('append', AppendAction)
 
-    self._page_test = DoNothingPageTest('RunActionToRun')
+    self._page_test = DoNothingPageTest('action_to_run')
     self._page = _CreatePage('blank.html')
 
   def testRunActions(self):
@@ -40,7 +40,7 @@ class PageTestUnitTest(unittest.TestCase):
     action_to_run = [
       { 'action': 'append', 'var': action_called }
     ]
-    setattr(self._page, 'RunActionToRun', action_to_run)
+    setattr(self._page, 'action_to_run', action_to_run)
 
     self._page_test.Run(self._page, None, None)
 
@@ -49,11 +49,11 @@ class PageTestUnitTest(unittest.TestCase):
   def testReferenceAction(self):
     action_list = []
     action_to_run = [
-      { 'action': 'RunReferencedAction' },
+      { 'action': 'referenced_action' },
     ]
     referenced_action = { 'action': 'append', 'var': action_list }
-    setattr(self._page, 'RunActionToRun', action_to_run)
-    setattr(self._page, 'RunReferencedAction', referenced_action)
+    setattr(self._page, 'action_to_run', action_to_run)
+    setattr(self._page, 'referenced_action', referenced_action)
 
     self._page_test.Run(self._page, None, None)
 
@@ -62,7 +62,7 @@ class PageTestUnitTest(unittest.TestCase):
   def testRepeatAction(self):
     action_list = []
     action_to_run = { 'action': 'append', 'var': action_list, 'repeat': 10 }
-    setattr(self._page, 'RunActionToRun', action_to_run)
+    setattr(self._page, 'action_to_run', action_to_run)
 
     self._page_test.Run(self._page, None, None)
 
@@ -70,12 +70,12 @@ class PageTestUnitTest(unittest.TestCase):
 
   def testRepeatReferenceAction(self):
     action_list = []
-    action_to_run = { 'action': 'RunReferencedAction', 'repeat': 3 }
+    action_to_run = { 'action': 'referenced_action', 'repeat': 3 }
     referenced_action = [
       { 'action': 'append', 'var': action_list },
     ]
-    setattr(self._page, 'RunActionToRun', action_to_run)
-    setattr(self._page, 'RunReferencedAction', referenced_action)
+    setattr(self._page, 'action_to_run', action_to_run)
+    setattr(self._page, 'referenced_action', referenced_action)
 
     self._page_test.Run(self._page, None, None)
 
