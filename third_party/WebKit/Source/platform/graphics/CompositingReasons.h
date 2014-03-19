97360f9bb55858dd27c082fc3547931c61534266
chrishtr@chromium.org <chrishtr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-19 18:47:22 +0000

    Fix restrictions on squashing with incompatible scroll.
    The previous logic could fail in some cases when the heuristic specified for the identity of the layer to squash
    into did not match exactly the choice used later in assignLayersToBackings. The new code is a bit cleaner also.
    
    BUG=261605
    
    Review URL: https://codereview.chromium.org/195893026
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169571 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/CompositingReasons.h
+++ third_party/WebKit/Source/platform/graphics/CompositingReasons.h
@@ -35,7 +35,7 @@ const uint64_t CompositingReasonWillChange                             = UINT64_
 const uint64_t CompositingReasonAssumedOverlap                         = UINT64_C(1) << 16;
 const uint64_t CompositingReasonOverlap                                = UINT64_C(1) << 17;
 const uint64_t CompositingReasonNegativeZIndexChildren                 = UINT64_C(1) << 18;
-const uint64_t CompositingReasonOverlapsWithoutSquashingTarget         = UINT64_C(1) << 19;
+const uint64_t CompositingReasonNoSquashingTargetFound                 = UINT64_C(1) << 19;
 
 // Subtree reasons that require knowing what the status of your subtree is before knowing the answer
 const uint64_t CompositingReasonTransformWithCompositedDescendants     = UINT64_C(1) << 20;
@@ -94,7 +94,7 @@ const uint64_t CompositingReasonComboReasonsThatRequireOwnBacking =
     | CompositingReasonOverlap
     | CompositingReasonAssumedOverlap
     | CompositingReasonNegativeZIndexChildren
-    | CompositingReasonOverlapsWithoutSquashingTarget
+    | CompositingReasonNoSquashingTargetFound
     | CompositingReasonTransformWithCompositedDescendants
     | CompositingReasonOpacityWithCompositedDescendants
     | CompositingReasonMaskWithCompositedDescendants
@@ -176,9 +176,9 @@ static const CompositingReasonStringMap compositingReasonStringMap[] = {
     { CompositingReasonNegativeZIndexChildren,
         "negativeZIndexChildren",
         "Parent with composited negative z-index content" },
-    { CompositingReasonOverlapsWithoutSquashingTarget,
-        "overlapsWithoutSquashingTarget",
-        "Cannot be squashed with content immediately behind it" },
+    {  CompositingReasonNoSquashingTargetFound,
+        "noSquashingTargetFound",
+        "Cannot be squashed due to no compatible squashing owner found" },
     { CompositingReasonTransformWithCompositedDescendants,
         "transformWithCompositedDescendants",
         "Has a transform that needs to be known by compositor because of composited descendants" },
