6150a83952b75def0b54ca57d7bebff2aa6fd037
fischman@chromium.org <fischman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-19 23:30:49 +0000

    Revert 258023 "Adding tests for the MediaStream Constructor."
    http://build.chromium.org/p/chromium.webrtc/builders/Win7%20Tester/builds/6868
    http://build.chromium.org/p/chromium.webrtc/builders/Win8%20Tester/builds/553 (552 which was the first build to contain this CL failed for other reasons earlier in the run)
    http://build.chromium.org/p/chromium.webrtc/builders/Linux%20Tester/builds/17749
    
    > Adding tests for the MediaStream Constructor.
    > BUG=294145
    > 
    > Review URL: https://codereview.chromium.org/183213003
    
    TBR=tommyw@chromium.org
    
    Review URL: https://codereview.chromium.org/205633002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258148 0039d316-1c4b-4281-b951-d872f2087c98

--- content/test/data/media/webrtc_test_utilities.js
+++ content/test/data/media/webrtc_test_utilities.js
@@ -133,10 +133,3 @@ function assertEquals(expected, actual) {
     failTest("expected '" + expected + "', got '" + actual + "'.");
   }
 }
-
-function assertNotEquals(expected, actual) {
-  if (actual === expected) {
-    failTest("expected '" + expected + "', got '" + actual + "'.");
-  }
-}
-
