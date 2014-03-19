9be875fa4c7bd75e5dc8b79c59e5be434791cdcc
sheu@chromium.org <sheu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-19 21:16:15 +0000

    Start the VDA device poll thread in Initialize()
    We may need to be returning completed input buffers from the device while we run
    DecodeBufferInitial() to set up the decoder.  Start the device thread then in
    Initialize() so the device is polled for input buffers to return.
    
    This is can be necessary in some cases where the decoder may run out of buffers
    to enqueue inputs on while scanning for valid input starting positions.
    
    BUG=None
    TEST=local build, run on CrOS snow
    
    Review URL: https://codereview.chromium.org/189993002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258097 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/media/v4l2_video_decode_accelerator.cc
+++ content/common/gpu/media/v4l2_video_decode_accelerator.cc
@@ -289,6 +289,9 @@ bool V4L2VideoDecodeAccelerator::Initialize(media::VideoCodecProfile profile,
     return false;
   }
 
+  if (!StartDevicePoll())
+    return false;
+
   SetDecoderState(kInitialized);
 
   child_message_loop_proxy_->PostTask(FROM_HERE, base::Bind(
@@ -710,7 +713,6 @@ bool V4L2VideoDecodeAccelerator::DecodeBufferInitial(
   DCHECK_EQ(decoder_thread_.message_loop(), base::MessageLoop::current());
   DCHECK_NE(decoder_state_, kUninitialized);
   DCHECK_NE(decoder_state_, kDecoding);
-  DCHECK(!device_poll_thread_.IsRunning());
   // Initial decode.  We haven't been able to get output stream format info yet.
   // Get it, and start decoding.
 
@@ -755,10 +757,6 @@ bool V4L2VideoDecodeAccelerator::DecodeBufferInitial(
   } else {
     *endpos = size;
   }
-
-  // StartDevicePoll will raise the error if there is one.
-  if (!StartDevicePoll())
-    return false;
 
   decoder_state_ = kDecoding;
   ScheduleDecodeBufferTaskIfNeeded();
@@ -886,8 +884,6 @@ void V4L2VideoDecodeAccelerator::ServiceDeviceTask(bool event_pending) {
   DVLOG(3) << "ServiceDeviceTask()";
   DCHECK_EQ(decoder_thread_.message_loop(), base::MessageLoop::current());
   DCHECK_NE(decoder_state_, kUninitialized);
-  DCHECK_NE(decoder_state_, kInitialized);
-  DCHECK_NE(decoder_state_, kAfterReset);
   TRACE_EVENT0("Video Decoder", "V4L2VDA::ServiceDeviceTask");
 
   if (decoder_state_ == kResetting) {
@@ -1354,6 +1350,9 @@ void V4L2VideoDecodeAccelerator::ResetDoneTask() {
     return;
   }
 
+  if (!StartDevicePoll())
+    return;
+
   // We might have received a resolution change event while we were waiting
   // for the reset to finish. The codec will not post another event if the
   // resolution after reset remains the same as the one to which were just
@@ -1408,8 +1407,9 @@ void V4L2VideoDecodeAccelerator::DestroyTask() {
 
 bool V4L2VideoDecodeAccelerator::StartDevicePoll() {
   DVLOG(3) << "StartDevicePoll()";
-  DCHECK_EQ(decoder_thread_.message_loop(), base::MessageLoop::current());
   DCHECK(!device_poll_thread_.IsRunning());
+  if (decoder_thread_.IsRunning())
+    DCHECK_EQ(decoder_thread_.message_loop(), base::MessageLoop::current());
 
   // Start up the device poll thread and schedule its first DevicePollTask().
   if (!device_poll_thread_.Start()) {
@@ -1496,7 +1496,8 @@ bool V4L2VideoDecodeAccelerator::StopDevicePoll(bool keep_input_state) {
 
 void V4L2VideoDecodeAccelerator::StartResolutionChangeIfNeeded() {
   DCHECK_EQ(decoder_thread_.message_loop(), base::MessageLoop::current());
-  DCHECK_EQ(decoder_state_, kDecoding);
+  DCHECK_NE(decoder_state_, kUninitialized);
+  DCHECK_NE(decoder_state_, kResetting);
 
   if (!resolution_change_pending_)
     return;
