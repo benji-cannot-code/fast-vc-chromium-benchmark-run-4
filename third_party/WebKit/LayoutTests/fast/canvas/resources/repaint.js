66d7c5e14e90f8654746c497c486686c9636480e
enne@chromium.org <enne@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-19 02:45:20 +0000

    Convert some repaint tests to not call display()
    BUG=345027
    
    Review URL: https://codereview.chromium.org/196353013
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169508 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/canvas/resources/repaint.js
+++ third_party/WebKit/LayoutTests/fast/canvas/resources/repaint.js
@@ -1,11 +1,14 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+if (window.testRunner)
+    testRunner.waitUntilDone();
+
 function runRepaintTest()
 {
-    if (window.testRunner) {
-        document.body.offsetTop;
-        testRunner.display();
-        repaintTest();
-    } else {
-        setTimeout(repaintTest, 100);
-    }
+    window.requestAnimationFrame(function() {
+        window.setTimeout(function() {
+            repaintTest();
+            if (window.testRunner)
+                testRunner.notifyDone();
+        }, 0);
+    });
 }
