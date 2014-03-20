36fb7ab18a261ff1a50be7abb1c36fc63e30e76c
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-20 06:22:08 +0000

    Introduces ColorCalibrationProfile handling into OutputConfigurator.
    Right now this uses RRSetCrtcGamma(), but the actual data is missing.
    I think the data should be served from the vendor, possibly from
    local files in the ChromeOS image or somewhere.
    
    BUG=350618
    R=oshima@chromium.org, marcheu@chromium.org
    TEST=manually w/ other patches
    
    Review URL: https://codereview.chromium.org/195893005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258221 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/display/chromeos/output_configurator.h
+++ ui/display/chromeos/output_configurator.h
@@ -48,6 +48,7 @@ class DISPLAY_EXPORT OutputConfigurator : public NativeDisplayObserver {
 
   struct DisplayState {
     DisplayState();
+    ~DisplayState();
 
     DisplaySnapshot* display;  // Not owned.
 
@@ -61,6 +62,8 @@ class DISPLAY_EXPORT OutputConfigurator : public NativeDisplayObserver {
 
     // Mode used when displaying the same desktop on multiple outputs.
     const DisplayMode* mirror_mode;
+
+    std::vector<ui::ColorCalibrationProfile> available_color_profiles;
   };
 
   typedef std::vector<DisplayState> DisplayStateList;
@@ -259,6 +262,11 @@ class DISPLAY_EXPORT OutputConfigurator : public NativeDisplayObserver {
   bool EnableOutputProtection(OutputProtectionClientId client_id,
                               int64 display_id,
                               uint32_t desired_protection_mask);
+
+  // Updates the color calibration to |new_profile|.
+  bool SetColorCalibrationProfile(
+      int64 display_id,
+      ui::ColorCalibrationProfile new_profile);
 
  private:
   // Mapping a display_id to a protection request bitmask.
