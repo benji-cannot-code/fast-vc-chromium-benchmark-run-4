78c3072c3b46de8cab66572c733cef10d6b14955
eroman@chromium.org <eroman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-20 07:51:12 +0000

    [webcrypto] Implement structured clone of keys (blink-side).
    The format looks like this:
    
      subtag:byte            // The type of key
      keySpecificProperties  // Block of key specific algorithm info
      usages:uint32          // Bitfield of usages + extractability
      keyDataLength:uint32   // Block of data controlled by embedder
      keyData:byte[keyDataLength]
    
    subtag influences how keySpecificProperties is interpreted:
    
    [If subtag=AesKeyTag]
      keyLengthBytes:uint32  // 16, 24, or 32
      algorithmId:uint32
    
    [If subtag=HmacKeyTag]
      keyLengthBytes:uint32
      hashId:uint32
    
    [If subtag=RsaKeyTag]
      algorithmId:uint32
      type:uint32            // One of {PublicKeyType, PrivateKeyType}
      modulusLengthBits:uint32
      publicExponentLength:uint32
      publicExponent:byte[publicExponentLength]
    
    [If subtag=RsaHashedKeyTag]
      <Same as for RsaKeyTag>
      hashId:uint32
    
    Note that uint32 is encoded as a variable length number. In practice it ends up being a single byte for most of the uses above.
    
    In this design, blink is responsible for serializing all of the key's attributes except for the actual key data which is left to the embedder.
    
    The included tests rely on the chromium side of structured clone landing:
      https://codereview.chromium.org/196513002/
    
    The tests cover serialization of hmac, aes and rsa public keys. I haven't added tests for serialization of rsa private keys yet, since that part is not done on the chromium side.
    
    BUG=245025
    
    Review URL: https://codereview.chromium.org/195543002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169633 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/exported/WebCryptoKeyAlgorithm.cpp
+++ third_party/WebKit/Source/platform/exported/WebCryptoKeyAlgorithm.cpp
@@ -37,6 +37,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+// FIXME: Remove the need for this.
+WebCryptoAlgorithm createHash(WebCryptoAlgorithmId hash)
+{
+    return WebCryptoAlgorithm::adoptParamsAndCreate(hash, 0);
+}
+
 class WebCryptoKeyAlgorithmPrivate : public ThreadSafeRefCounted<WebCryptoKeyAlgorithmPrivate> {
 public:
     WebCryptoKeyAlgorithmPrivate(WebCryptoAlgorithmId id, PassOwnPtr<WebCryptoKeyAlgorithmParams> params)
@@ -57,6 +63,36 @@ WebCryptoKeyAlgorithm::WebCryptoKeyAlgorithm(WebCryptoAlgorithmId id, PassOwnPtr
 WebCryptoKeyAlgorithm WebCryptoKeyAlgorithm::adoptParamsAndCreate(WebCryptoAlgorithmId id, WebCryptoKeyAlgorithmParams* params)
 {
     return WebCryptoKeyAlgorithm(id, adoptPtr(params));
+}
+
+WebCryptoKeyAlgorithm WebCryptoKeyAlgorithm::createAes(WebCryptoAlgorithmId id, unsigned short keyLengthBits)
+{
+    // FIXME: Verify that id is an AES algorithm.
+    // FIXME: Move this somewhere more general.
+    if (keyLengthBits != 128 && keyLengthBits != 192 && keyLengthBits != 256)
+        return WebCryptoKeyAlgorithm();
+    return WebCryptoKeyAlgorithm(id, adoptPtr(new WebCryptoAesKeyAlgorithmParams(keyLengthBits)));
+}
+
+WebCryptoKeyAlgorithm WebCryptoKeyAlgorithm::createHmac(WebCryptoAlgorithmId hash, unsigned keyLengthBits)
+{
+    if (!WebCryptoAlgorithm::isHash(hash))
+        return WebCryptoKeyAlgorithm();
+    return WebCryptoKeyAlgorithm(WebCryptoAlgorithmIdHmac, adoptPtr(new WebCryptoHmacKeyAlgorithmParams(createHash(hash), keyLengthBits)));
+}
+
+WebCryptoKeyAlgorithm WebCryptoKeyAlgorithm::createRsa(WebCryptoAlgorithmId id, unsigned modulusLengthBits, const unsigned char* publicExponent, unsigned publicExponentSize)
+{
+    // FIXME: Verify that id is an RSA algorithm without a hash
+    return WebCryptoKeyAlgorithm(id, adoptPtr(new WebCryptoRsaKeyAlgorithmParams(modulusLengthBits, publicExponent, publicExponentSize)));
+}
+
+WebCryptoKeyAlgorithm WebCryptoKeyAlgorithm::createRsaHashed(WebCryptoAlgorithmId id, unsigned modulusLengthBits, const unsigned char* publicExponent, unsigned publicExponentSize, WebCryptoAlgorithmId hash)
+{
+    // FIXME: Verify that id is an RSA algorithm which expects a hash
+    if (!WebCryptoAlgorithm::isHash(hash))
+        return WebCryptoKeyAlgorithm();
+    return WebCryptoKeyAlgorithm(id, adoptPtr(new WebCryptoRsaHashedKeyAlgorithmParams(modulusLengthBits, publicExponent, publicExponentSize, createHash(hash))));
 }
 
 bool WebCryptoKeyAlgorithm::isNull() const
