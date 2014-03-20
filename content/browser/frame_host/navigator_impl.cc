f64741f38376961db9ec963ea0114a7d5df0bd3e
creis@chromium.org <creis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-20 17:48:01 +0000

    Consistently null-check NavigatorImpl::delegate_.
    The delegate can be null in tests.
    
    BUG=none
    R=nasko@chromium.org
    
    Review URL: https://codereview.chromium.org/201153009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258334 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/frame_host/navigator_impl.cc
+++ content/browser/frame_host/navigator_impl.cc
@@ -252,7 +252,8 @@ void NavigatorImpl::DidFailProvisionalLoadWithError(
   if (controller_->GetPendingEntry() != controller_->GetVisibleEntry())
     controller_->DiscardPendingEntry();
 
-  delegate_->DidFailProvisionalLoadWithError(render_frame_host, params);
+  if (delegate_)
+    delegate_->DidFailProvisionalLoadWithError(render_frame_host, params);
 }
 
 void NavigatorImpl::DidFailLoadWithError(
@@ -261,9 +262,11 @@ void NavigatorImpl::DidFailLoadWithError(
     bool is_main_frame,
     int error_code,
     const base::string16& error_description) {
-  delegate_->DidFailLoadWithError(
-      render_frame_host, url, is_main_frame, error_code,
-      error_description);
+  if (delegate_) {
+    delegate_->DidFailLoadWithError(
+        render_frame_host, url, is_main_frame, error_code,
+        error_description);
+  }
 }
 
 void NavigatorImpl::DidRedirectProvisionalLoad(
@@ -289,8 +292,10 @@ void NavigatorImpl::DidRedirectProvisionalLoad(
   if (!entry || entry->GetURL() != validated_source_url)
     return;
 
-  delegate_->DidRedirectProvisionalLoad(
-      render_frame_host, validated_target_url);
+  if (delegate_) {
+    delegate_->DidRedirectProvisionalLoad(
+        render_frame_host, validated_target_url);
+  }
 }
 
 bool NavigatorImpl::NavigateToEntry(
