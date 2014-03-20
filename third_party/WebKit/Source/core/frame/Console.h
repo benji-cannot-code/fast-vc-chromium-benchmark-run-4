649a6fccdde8c304ab8448acbdc531ade3aa4f6b
sigbjornf@opera.com <sigbjornf@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-20 17:47:52 +0000

    Oilpan: move Console objects to the oilpan heap.
    Turn the ConsoleBase object into a garbage collected object if Oilpan
    is enabled, avoiding its ref forwarding to either Console or WorkerConsole.
    
    R=haraken@chromium.org,ager@chromium.org
    BUG=340522
    
    Review URL: https://codereview.chromium.org/206093005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169670 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/Console.h
+++ third_party/WebKit/Source/core/frame/Console.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/v8/ScriptWrappable.h"
 #include "core/frame/ConsoleBase.h"
 #include "core/frame/DOMWindowProperty.h"
+#include "heap/Handle.h"
 #include "wtf/Forward.h"
 #include "wtf/PassRefPtr.h"
 #include "wtf/RefCounted.h"
@@ -45,15 +46,17 @@ class MemoryInfo;
 class Page;
 class ScriptArguments;
 
-class Console FINAL : public RefCounted<Console>, public ConsoleBase, public ScriptWrappable, public DOMWindowProperty {
+class Console FINAL : public ConsoleBase, public ScriptWrappable, public DOMWindowProperty {
 public:
-    using RefCounted<Console>::ref;
-    using RefCounted<Console>::deref;
-
-    static PassRefPtr<Console> create(LocalFrame* frame) { return adoptRef(new Console(frame)); }
+    static PassRefPtrWillBeRawPtr<Console> create(LocalFrame* frame)
+    {
+        return adoptRefWillBeNoop(new Console(frame));
+    }
     virtual ~Console();
 
     PassRefPtrWillBeRawPtr<MemoryInfo> memory() const;
+
+    void trace(Visitor*) { }
 
 protected:
     virtual ExecutionContext* context() OVERRIDE;
@@ -61,9 +64,6 @@ protected:
 
 private:
     explicit Console(LocalFrame*);
-
-    virtual void refConsole() OVERRIDE { ref(); }
-    virtual void derefConsole() OVERRIDE { deref(); }
 };
 
 } // namespace WebCore
--- third_party/WebKit/Source/core/frame/ConsoleBase.h
+++ third_party/WebKit/Source/core/frame/ConsoleBase.h
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/inspector/ScriptCallStack.h"
 #include "core/frame/ConsoleTypes.h"
 #include "core/frame/DOMWindowProperty.h"
+#include "heap/Handle.h"
 #include "wtf/Forward.h"
 #include "wtf/PassRefPtr.h"
 #include "wtf/RefCounted.h"
@@ -45,11 +46,8 @@ namespace WebCore {
 
 class ScriptArguments;
 
-class ConsoleBase {
+class ConsoleBase : public RefCountedWillBeGarbageCollectedFinalized<ConsoleBase> {
 public:
-    void ref() { refConsole(); }
-    void deref() { derefConsole(); }
-
     void debug(ScriptState*, PassRefPtr<ScriptArguments>);
     void error(ScriptState*, PassRefPtr<ScriptArguments>);
     void info(ScriptState*, PassRefPtr<ScriptArguments>);
@@ -74,15 +72,15 @@ public:
     void groupCollapsed(ScriptState*, PassRefPtr<ScriptArguments>);
     void groupEnd();
 
-protected:
+    virtual void trace(Visitor*) = 0;
+
     virtual ~ConsoleBase();
+
+protected:
     virtual ExecutionContext* context() = 0;
     virtual void reportMessageToClient(MessageLevel, const String& message, PassRefPtr<ScriptCallStack>) = 0;
 
 private:
-    virtual void refConsole() = 0;
-    virtual void derefConsole() = 0;
-
     void internalAddMessage(MessageType, MessageLevel, ScriptState*, PassRefPtr<ScriptArguments>, bool acceptNoArguments = false, bool printTrace = false);
 };
 
