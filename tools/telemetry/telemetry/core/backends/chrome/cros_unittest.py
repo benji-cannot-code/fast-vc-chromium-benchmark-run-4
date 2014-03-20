7f5ef87214edddf5b5b515ab88285b6180dfb58b
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-20 23:55:46 +0000

    Revert 258410 "Only restart ui if we're logged in."
    > Only restart ui if we're logged in.
    > 
    > * Switch to using cryptohome --action=is_mounted instead of manually checking file mounts.
    > * oobe_exists can throw an exception as we log in, so handle this.
    > * Use the same mechanism for determining if guest is logged in.
    > * Move CryptohomePath and the mount-based IsCryptohomeMounted to CrOSTest.
    > * Fixup CrOSTest for guest mode.
    > 
    > BUG=
    > TEST=manual
    > NOTRY=True
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=257996
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=258135
    > 
    > Review URL: https://codereview.chromium.org/203973003
    
    TBR=achuith@chromium.org
    
    Review URL: https://codereview.chromium.org/207363002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258459 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/backends/chrome/cros_unittest.py
+++ tools/telemetry/telemetry/core/backends/chrome/cros_unittest.py
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import json
 import logging
 import os
 import unittest
@@ -21,8 +22,7 @@ class CrOSTest(unittest.TestCase):
     self._cri = cros_interface.CrOSInterface(options.cros_remote,
                                              options.cros_ssh_identity)
     self._is_guest = options.browser_type == 'cros-chrome-guest'
-    self._username = ('$guest' if self._is_guest
-                                else options.browser_options.username)
+    self._username = '' if self._is_guest else options.browser_options.username
     self._password = options.browser_options.password
     self._load_extension = None
 
@@ -52,20 +52,14 @@ class CrOSTest(unittest.TestCase):
     self.assertTrue(extension)
     return extension
 
-  def _CryptohomePath(self, user):
-    """Returns the cryptohome mount point for |user|."""
-    return self._cri.RunCmdOnDevice(
-        ['cryptohome-path', 'user', "'%s'" % user])[0].strip()
-
   def _IsCryptohomeMounted(self):
-    """Returns True iff |user|'s cryptohome is mounted."""
-    profile_path = self._CryptohomePath(self._username)
-    mount = self._cri.FilesystemMountedAt(profile_path)
-    if not mount:
-      return False
-    if self._is_guest:
-      return mount == 'guestfs'
-    return mount.startswith('/home/.shadow/')
+    """Returns True if cryptohome is mounted. as determined by the cmd
+    cryptohome --action=status"""
+    cryptohomeJSON, _ = self._cri.RunCmdOnDevice(['/usr/sbin/cryptohome',
+                                                 '--action=status'])
+    cryptohomeStatus = json.loads(cryptohomeJSON)
+    return (cryptohomeStatus['mounts'] and
+            cryptohomeStatus['mounts'][0]['mounted'])
 
   @test.Enabled('chromeos')
   def testCryptohome(self):
@@ -83,7 +77,7 @@ class CrOSTest(unittest.TestCase):
         self.assertEquals(chronos_fs, 'guestfs')
       else:
         crypto_fs = self._cri.FilesystemMountedAt(
-            self._CryptohomePath(self._username))
+            self._cri.CryptohomePath(self._username))
         self.assertEquals(crypto_fs, chronos_fs)
 
     self.assertFalse(self._IsCryptohomeMounted())
@@ -106,8 +100,6 @@ class CrOSTest(unittest.TestCase):
   @test.Enabled('chromeos')
   def testLoginStatus(self):
     """Tests autotestPrivate.loginStatus"""
-    if self._is_guest:
-      return
     with self._CreateBrowser(autotest_ext=True) as b:
       login_status = self._GetLoginStatus(b)
       self.assertEquals(type(login_status), dict)
@@ -162,8 +154,6 @@ class CrOSTest(unittest.TestCase):
   @test.Enabled('chromeos')
   def testScreenLock(self):
     """Tests autotestPrivate.screenLock"""
-    if self._is_guest:
-      return
     with self._CreateBrowser(autotest_ext=True) as browser:
       self._LockScreen(browser)
       self._AttemptUnlockBadPassword(browser)
@@ -172,8 +162,6 @@ class CrOSTest(unittest.TestCase):
   @test.Enabled('chromeos')
   def testLogout(self):
     """Tests autotestPrivate.logout"""
-    if self._is_guest:
-      return
     with self._CreateBrowser(autotest_ext=True) as b:
       extension = self._GetAutotestExtension(b)
       try:
