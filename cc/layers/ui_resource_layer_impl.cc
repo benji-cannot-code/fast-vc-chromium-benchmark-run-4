c421489743700fc5c25ee19ee0b255c3fc7a6f52
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-20 06:36:13 +0000

    cc: Apply occlusion before creating quads in UIResourceLayerImpl.
    This makes the AppendQuads method query occlusion directly and subtract
    it from the quads it would create before they are created, skipping
    quads that are completely occluded and avoiding a malloc/free for them.
    
    Depends on: https://codereview.chromium.org/201153021/
    
    R=enne
    BUG=344962
    
    Review URL: https://codereview.chromium.org/203463015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258228 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/layers/ui_resource_layer_impl.cc
+++ cc/layers/ui_resource_layer_impl.cc
@@ -117,7 +117,11 @@ void UIResourceLayerImpl::AppendQuads(QuadSink* quad_sink,
 
   gfx::Rect quad_rect(bounds());
   gfx::Rect opaque_rect(opaque ? quad_rect : gfx::Rect());
-  gfx::Rect visible_quad_rect(quad_rect);
+  gfx::Rect visible_quad_rect = quad_sink->UnoccludedContentRect(
+      quad_rect, draw_properties().target_space_transform);
+  if (visible_quad_rect.IsEmpty())
+    return;
+
   scoped_ptr<TextureDrawQuad> quad = TextureDrawQuad::Create();
   quad->SetNew(shared_quad_state,
                quad_rect,
@@ -130,7 +134,7 @@ void UIResourceLayerImpl::AppendQuads(QuadSink* quad_sink,
                SK_ColorTRANSPARENT,
                vertex_opacity_,
                flipped);
-  quad_sink->MaybeAppend(quad.PassAs<DrawQuad>());
+  quad_sink->Append(quad.PassAs<DrawQuad>());
 }
 
 const char* UIResourceLayerImpl::LayerTypeAsString() const {
