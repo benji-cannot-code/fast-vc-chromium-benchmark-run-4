3b62c5bc6a592ecc83cc1ce13b240139971eaa5f
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-20 06:07:26 +0000

    Set error in LoadNativeLibrary() on Windows.
    If the native library fails to load, call GetLastError() immediately and set error with the last system error code. The list of Windows system error codes can be found at:
    
    http://msdn.microsoft.com/en-us/library/windows/desktop/ms681381.aspx
    
    BUG=353771
    TBR=evan@chromium.org
    
    Review URL: https://codereview.chromium.org/199413026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258214 0039d316-1c4b-4281-b951-d872f2087c98

--- base/native_library_win.cc
+++ base/native_library_win.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <windows.h>
 
 #include "base/file_util.h"
+#include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/threading/thread_restrictions.h"
 
@@ -16,7 +17,8 @@ namespace base {
 typedef HMODULE (WINAPI* LoadLibraryFunction)(const wchar_t* file_name);
 
 NativeLibrary LoadNativeLibraryHelper(const FilePath& library_path,
-                                      LoadLibraryFunction load_library_api) {
+                                      LoadLibraryFunction load_library_api,
+                                      std::string* error) {
   // LoadLibrary() opens the file off disk.
   ThreadRestrictions::AssertIOAllowed();
 
@@ -33,6 +35,12 @@ NativeLibrary LoadNativeLibraryHelper(const FilePath& library_path,
   }
 
   HMODULE module = (*load_library_api)(library_path.value().c_str());
+  if (!module && error) {
+    // GetLastError() needs to be called immediately after |load_library_api|.
+    DWORD last_error = GetLastError();
+    *error = StringPrintf("%u", last_error);
+  }
+
   if (restore_directory)
     SetCurrentDirectory(current_directory);
 
@@ -42,7 +50,7 @@ NativeLibrary LoadNativeLibraryHelper(const FilePath& library_path,
 // static
 NativeLibrary LoadNativeLibrary(const FilePath& library_path,
                                 std::string* error) {
-  return LoadNativeLibraryHelper(library_path, LoadLibraryW);
+  return LoadNativeLibraryHelper(library_path, LoadLibraryW, error);
 }
 
 NativeLibrary LoadNativeLibraryDynamically(const FilePath& library_path) {
@@ -52,7 +60,7 @@ NativeLibrary LoadNativeLibraryDynamically(const FilePath& library_path) {
   load_library = reinterpret_cast<LoadLibraryFunction>(
       GetProcAddress(GetModuleHandle(L"kernel32.dll"), "LoadLibraryW"));
 
-  return LoadNativeLibraryHelper(library_path, load_library);
+  return LoadNativeLibraryHelper(library_path, load_library, NULL);
 }
 
 // static
