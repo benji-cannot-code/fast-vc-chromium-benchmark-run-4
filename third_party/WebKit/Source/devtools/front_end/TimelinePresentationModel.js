0fc07e1288a1c1831ebde74adce0c9b5bafc7b91
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-20 05:45:05 +0000

    Timeline: fix expansion arrow appearing on rows with no visible children
    We used to check TimelinePresentationRecord.expandable(), but it was accidrently replaced
    with hasPresentationChildren(), which is not the same now, since we keep hidden records
    in the presentation model.
    
    R=pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/201823003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169620 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/TimelinePresentationModel.js
+++ third_party/WebKit/Source/devtools/front_end/TimelinePresentationModel.js
@@ -347,14 +347,6 @@ WebInspector.TimelinePresentationModel.Record.prototype = {
     /**
      * @return {boolean}
      */
-    hasPresentationChildren: function()
-    {
-        return !!this._presentationChildren.length;
-    },
-
-    /**
-     * @return {boolean}
-     */
     coalesced: function()
     {
         return this._coalesced;
