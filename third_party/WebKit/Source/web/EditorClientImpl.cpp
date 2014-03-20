116857f258a7454cf1b795e8b969ba9b01529732
jam@chromium.org <jam@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-20 03:47:20 +0000

    Move didChangeSelection to WebFrameClient.
    BUG=304341
    
    Review URL: https://codereview.chromium.org/204673007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169603 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/EditorClientImpl.cpp
+++ third_party/WebKit/Source/web/EditorClientImpl.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "EditorClientImpl.h"
 
+#include "WebFrameClient.h"
 #include "WebFrameImpl.h"
 #include "WebPermissionClient.h"
 #include "WebViewClient.h"
@@ -47,10 +48,11 @@ EditorClientImpl::~EditorClientImpl()
 {
 }
 
-void EditorClientImpl::respondToChangedSelection(WebCore::SelectionType selectionType)
+void EditorClientImpl::respondToChangedSelection(LocalFrame* frame, WebCore::SelectionType selectionType)
 {
-    if (m_webView->client())
-        m_webView->client()->didChangeSelection(selectionType != WebCore::RangeSelection);
+    WebFrameImpl* webFrame = WebFrameImpl::fromFrame(frame);
+    if (webFrame->client())
+        webFrame->client()->didChangeSelection(selectionType != WebCore::RangeSelection);
 }
 
 void EditorClientImpl::respondToChangedContents()
