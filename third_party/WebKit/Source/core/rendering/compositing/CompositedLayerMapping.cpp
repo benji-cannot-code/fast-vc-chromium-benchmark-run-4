a80500f2b8f3f53e9e9b5bfb303c8b94b3ef39b1
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-20 21:10:45 +0000

    Simplify CompositingReasonFinder::requiresCompositingForPlugin
    This CL removes the optimization not to create a composited layer for 0x0
    plugins. Removing this optimization simplifies the CompositingReasonFinder and
    removes a dependency on layout information. We've already removed a similar
    optimization for 0x0 iframes.
    
    This CL also removes RenderEmbeddedObject::allowsAcceleratedCompositing. This
    function is redundant with RenderPart::requiresAcceleratedCompositing because
    RenderPart is a base class of RenderEmbeddedObject.
    
    Review URL: https://codereview.chromium.org/202523008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169693 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/compositing/CompositedLayerMapping.cpp
+++ third_party/WebKit/Source/core/rendering/compositing/CompositedLayerMapping.cpp
@@ -140,7 +140,7 @@ static bool contentLayerSupportsDirectBackgroundComposition(const RenderObject* 
 static inline bool isAcceleratedContents(RenderObject* renderer)
 {
     return isAcceleratedCanvas(renderer)
-        || (renderer->isEmbeddedObject() && toRenderEmbeddedObject(renderer)->allowsAcceleratedCompositing())
+        || (renderer->isEmbeddedObject() && toRenderEmbeddedObject(renderer)->requiresAcceleratedCompositing())
         || renderer->isVideo();
 }
 
@@ -528,7 +528,7 @@ bool CompositedLayerMapping::updateGraphicsLayerConfiguration()
     if (isDirectlyCompositedImage())
         updateImageContents();
 
-    if (renderer->isEmbeddedObject() && toRenderEmbeddedObject(renderer)->allowsAcceleratedCompositing()) {
+    if (renderer->isEmbeddedObject() && toRenderEmbeddedObject(renderer)->requiresAcceleratedCompositing()) {
         PluginView* pluginView = toPluginView(toRenderWidget(renderer)->widget());
         m_graphicsLayer->setContentsToPlatformLayer(pluginView->platformLayer());
     } else if (renderer->node() && renderer->node()->isFrameOwnerElement() && toHTMLFrameOwnerElement(renderer->node())->contentFrame()) {
@@ -1542,7 +1542,7 @@ bool CompositedLayerMapping::paintsChildren() const
 
 static bool isCompositedPlugin(RenderObject* renderer)
 {
-    return renderer->isEmbeddedObject() && toRenderEmbeddedObject(renderer)->allowsAcceleratedCompositing();
+    return renderer->isEmbeddedObject() && toRenderEmbeddedObject(renderer)->requiresAcceleratedCompositing();
 }
 
 static bool hasVisibleNonCompositingDescendant(RenderLayer* parent)
