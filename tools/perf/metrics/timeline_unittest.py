0894ac8f9a7f6ff70612eb5639e3f2c31cac8ded
nednguyen@google.com <nednguyen@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-20 00:27:52 +0000

    Remove loading_timeline
     NOTRY=True
    BUG=
    
    Review URL: https://codereview.chromium.org/198533005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258170 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/metrics/timeline_unittest.py
+++ tools/perf/metrics/timeline_unittest.py
@@ -54,7 +54,7 @@ class LoadTimesTimelineMetric(unittest.TestCase):
     renderer_main.EndSlice(20, 20)
     model.FinalizeImport()
 
-    metric = timeline.LoadTimesTimelineMetric(timeline.TRACING_MODE)
+    metric = timeline.LoadTimesTimelineMetric()
     metric.renderer_process = renderer_main.parent
     results = self.GetResultsForModel(metric, model)
     results.AssertHasPageSpecificScalarValue(
@@ -75,7 +75,7 @@ class LoadTimesTimelineMetric(unittest.TestCase):
     x_counter.timestamps += [0, 1]
     model.FinalizeImport()
 
-    metric = timeline.LoadTimesTimelineMetric(timeline.TRACING_MODE)
+    metric = timeline.LoadTimesTimelineMetric()
     metric.renderer_process = renderer_main.parent
     results = self.GetResultsForModel(metric, model)
     results.AssertHasPageSpecificScalarValue(
