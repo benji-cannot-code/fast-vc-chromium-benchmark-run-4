4cf2e929d450140bd5ac7ab97f924bb870213175
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-20 03:49:20 +0000

    Don't call hb_buffer_set_unicode_funcs in HarfBuzzShaper
    Harfbuzz-ng 0.9.27 introduced a HAVE_ICU_BUILTIN define which makes the
    hb_buffer_set_unicode_funcs call redundant. Also call
    hb_buffer_clear_content instead of hb_buffer_reset which is faster now
    that we no longer explicitly set the unicode functions.
    
    BUG=354117
    R=dglazkov@chromium.org
    
    Review URL: https://codereview.chromium.org/197213032
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169604 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/fonts/harfbuzz/HarfBuzzShaper.cpp
+++ third_party/WebKit/Source/platform/fonts/harfbuzz/HarfBuzzShaper.cpp
@@ -779,7 +779,6 @@ bool HarfBuzzShaper::shapeHarfBuzzRuns()
 {
     HarfBuzzScopedPtr<hb_buffer_t> harfBuzzBuffer(hb_buffer_create(), hb_buffer_destroy);
 
-    hb_buffer_set_unicode_funcs(harfBuzzBuffer.get(), hb_icu_get_unicode_funcs());
     HarfBuzzRunCache& runCache = harfBuzzRunCache();
 
     for (unsigned i = 0; i < m_harfBuzzRuns.size(); ++i) {
@@ -809,7 +808,7 @@ bool HarfBuzzShaper::shapeHarfBuzzRuns()
                 currentRun->applyShapeResult(cachedResults->buffer);
                 setGlyphPositionsForHarfBuzzRun(currentRun, cachedResults->buffer);
 
-                hb_buffer_reset(harfBuzzBuffer.get());
+                hb_buffer_clear_contents(harfBuzzBuffer.get());
 
                 runCache.moveToBack(cachedResults);
 
@@ -845,7 +844,6 @@ bool HarfBuzzShaper::shapeHarfBuzzRuns()
         runCache.insert(key, new CachedShapingResults(harfBuzzBuffer.get(), m_font, props.direction));
 
         harfBuzzBuffer.set(hb_buffer_create());
-        hb_buffer_set_unicode_funcs(harfBuzzBuffer.get(), hb_icu_get_unicode_funcs());
     }
 
     return true;
