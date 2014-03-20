7fcbbfb9b17f7e1eef18037492bcbf14474b8cb7
pilgrim@chromium.org <pilgrim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-20 17:23:47 +0000

    Fix fling_animator_impl_android.h header guard
    BUG=338338
    TBR=darin@chromium.org
    
    Review URL: https://codereview.chromium.org/203333003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258316 0039d316-1c4b-4281-b951-d872f2087c98

--- content/child/fling_animator_impl_android.h
+++ content/child/fling_animator_impl_android.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_CHILDFLING_ANIMATOR_IMPL_ANDROID_H_
-#define CONTENT_CHILDFLING_ANIMATOR_IMPL_ANDROID_H_
+#ifndef CONTENT_CHILD_FLING_ANIMATOR_IMPL_ANDROID_H_
+#define CONTENT_CHILD_FLING_ANIMATOR_IMPL_ANDROID_H_
 
 
 #include "third_party/WebKit/public/platform/WebFloatPoint.h"
@@ -45,4 +45,4 @@ class FlingAnimatorImpl : public blink::WebGestureCurve {
 
 }  // namespace webkit_glue
 
-#endif  // CONTENT_CHILDFLING_ANIMATOR_IMPL_ANDROID_H_
+#endif  // CONTENT_CHILD_FLING_ANIMATOR_IMPL_ANDROID_H_
