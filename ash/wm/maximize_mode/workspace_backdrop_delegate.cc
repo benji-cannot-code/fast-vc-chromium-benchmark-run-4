87ae6f17a441514040ea131736614a19767270df
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-20 06:18:05 +0000

    Keep dedicated layers for hiding animation  Introduced ScopedHidingAnimationSettings to set hiding animations  Remove the restacking code from FocusController, and other obsolete tests.
    BUG=315843
    TEST=WindowAnimationTest.HideAnimationDetachLayers, WindowAnimationTest.HideAnimationDetachLayersWithTransientChild
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/180273025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258220 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/maximize_mode/workspace_backdrop_delegate.cc
+++ ash/wm/maximize_mode/workspace_backdrop_delegate.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/compositor/scoped_layer_animation_settings.h"
 #include "ui/views/background.h"
 #include "ui/views/widget/widget.h"
+#include "ui/wm/core/window_animations.h"
 #include "ui/wm/core/window_util.h"
 
 namespace ash {
@@ -49,11 +50,9 @@ WorkspaceBackdropDelegate::WorkspaceBackdropDelegate(aura::Window* container)
 
 WorkspaceBackdropDelegate::~WorkspaceBackdropDelegate() {
   container_->RemoveObserver(this);
-  ui::ScopedLayerAnimationSettings settings(
-      background_->GetNativeView()->layer()->GetAnimator());
+  ::wm::ScopedHidingAnimationSettings hiding_settings(
+      background_->GetNativeView());
   background_->Close();
-  settings.AddObserver(::wm::CreateHidingWindowAnimationObserver(
-      background_->GetNativeView()));
   background_->GetNativeView()->layer()->SetOpacity(0.0f);
 }
 
--- ui/wm/core/window_animations.h
+++ ui/wm/core/window_animations.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "ui/compositor/scoped_layer_animation_settings.h"
 #include "ui/wm/core/wm_core_export.h"
 
 namespace aura {
@@ -81,12 +82,27 @@ WM_CORE_EXPORT void SetWindowVisibilityAnimationVerticalPosition(
     aura::Window* window,
     float position);
 
-// Creates an ImplicitAnimationObserver that takes ownership of the layers
-// associated with a Window so that the animation can continue after the Window
-// has been destroyed.
-// The returned object deletes itself when the animations are done.
-WM_CORE_EXPORT ui::ImplicitAnimationObserver*
-    CreateHidingWindowAnimationObserver(aura::Window* window);
+class HidingWindowAnimationObserver;
+// A wrapper of ui::ScopedLayerAnimationSettings for hiding animations.
+// Use this to ensure that the hiding animation is visible even after
+// the window is deleted or deactivated, instead of using
+// ui::ScopedLayerAnimationSettings directly.
+class WM_CORE_EXPORT ScopedHidingAnimationSettings {
+ public:
+  explicit ScopedHidingAnimationSettings(aura::Window* window);
+  ~ScopedHidingAnimationSettings();
+
+  // Returns the wrapped ScopedLayeAnimationSettings instance.
+  ui::ScopedLayerAnimationSettings* layer_animation_settings() {
+    return &layer_animation_settings_;
+  }
+
+ private:
+  ui::ScopedLayerAnimationSettings layer_animation_settings_;
+  HidingWindowAnimationObserver* observer_;
+
+  DISALLOW_COPY_AND_ASSIGN(ScopedHidingAnimationSettings);
+};
 
 // Returns false if the |window| didn't animate.
 WM_CORE_EXPORT bool AnimateOnChildWindowVisibilityChanged(aura::Window* window,
