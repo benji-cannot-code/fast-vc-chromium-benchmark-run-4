a42ce4c4da0bc81719def3fd6434a8acfb193396
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-20 06:30:37 +0000

    Fix VersionInfo::GetChannel for Android canary.
    BUG=354106
    
    Review URL: https://codereview.chromium.org/204953002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258225 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_version_info_android.cc
+++ chrome/common/chrome_version_info_android.cc
@@ -35,6 +35,8 @@ VersionInfo::Channel VersionInfo::GetChannel() {
     return CHANNEL_BETA;
   if (!strcmp(bi->package_name(), "com.google.android.apps.chrome_dev"))
     return CHANNEL_DEV;
+  if (!strcmp(bi->package_name(), "com.chrome.canary"))
+    return CHANNEL_CANARY;
 
   return CHANNEL_UNKNOWN;
 }
