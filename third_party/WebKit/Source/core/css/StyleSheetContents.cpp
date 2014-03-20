b6b68dfef26f2ae8d0b305e80e76ce03789805ec
tasak@google.com <tasak@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-20 00:00:07 +0000

    Should remove StyleSheetContents from cache even if document is not active.
    StyleSheetContents cache works independent of document's active(),
    because StyleElement::process() always invokes StyleEngine::createSheet().
    
    So removeSheetFromCache should also remove 'this' from the cache independent of document's active().
    
    BUG=353894
    TEST=fast/dom/move-style-element-to-inactive-document-crash.html
    
    Review URL: https://codereview.chromium.org/204423002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169595 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/StyleSheetContents.cpp
+++ third_party/WebKit/Source/core/css/StyleSheetContents.cpp
@@ -589,9 +589,8 @@ void StyleSheetContents::clientLoadStarted(CSSStyleSheet* sheet)
 
 void StyleSheetContents::removeSheetFromCache(Document* document)
 {
+    ASSERT(document);
     if (!maybeCacheable())
-        return;
-    if (!document || !document->isActive())
         return;
     document->styleEngine()->removeSheet(this);
 }
