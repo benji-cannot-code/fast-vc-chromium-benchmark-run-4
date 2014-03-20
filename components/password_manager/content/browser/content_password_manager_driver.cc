fc9b699022db912af2f12f3aa05c4a6762c0af2a
nasko@chromium.org <nasko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-20 00:28:29 +0000

    Remove usage of GetActiveEntry in password manager.
    Please see crbug.com/273710 description and comment 1 for details why this change is needed.
    
    BUG=273710
    
    Review URL: https://codereview.chromium.org/197233005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258173 0039d316-1c4b-4281-b951-d872f2087c98

--- components/password_manager/content/browser/content_password_manager_driver.cc
+++ components/password_manager/content/browser/content_password_manager_driver.cc
@@ -53,7 +53,7 @@ void ContentPasswordManagerDriver::AccountCreationFormsFound(
 bool ContentPasswordManagerDriver::DidLastPageLoadEncounterSSLErrors() {
   DCHECK(web_contents());
   content::NavigationEntry* entry =
-      web_contents()->GetController().GetActiveEntry();
+      web_contents()->GetController().GetLastCommittedEntry();
   if (!entry) {
     NOTREACHED();
     return false;
