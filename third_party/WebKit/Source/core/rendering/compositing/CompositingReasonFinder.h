4592f5054c4d2d06b5684a49b0f09c5a984b7365
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-21 19:49:32 +0000

    Compute style-determined compositing reasons during recalc style
    There's no need to constantly recompute this data. It's
    already determined by the style information, so we might as
    well just compute it once during style recalc.
    
    Note: We're still computing all the direct compositing
    reasons during style recalc (via updateLayerCompositingState in RenderLayer::styleChanged), but now we skip recomputing
    the style determined reasons again when updating layout
    and compositing. ASSERTs verify that we get the same answer
    as before.
    
    This CL reduces the amount of time spent in the compositing
    update for dragging the drawer in the Polymer calculator by
    3%.
    
    Review URL: https://codereview.chromium.org/207513002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169766 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/compositing/CompositingReasonFinder.h
+++ third_party/WebKit/Source/core/rendering/compositing/CompositingReasonFinder.h
@@ -17,9 +17,11 @@ class RenderObject;
 class RenderView;
 
 class CompositingReasonFinder {
+    WTF_MAKE_NONCOPYABLE(CompositingReasonFinder);
 public:
     explicit CompositingReasonFinder(RenderView&);
 
+    CompositingReasons styleDeterminedReasons(RenderObject*) const;
     CompositingReasons directReasons(const RenderLayer*, bool* needToRecomputeCompositingRequirements) const;
 
     void updateTriggers();
@@ -36,7 +38,6 @@ public:
 private:
     bool isMainFrame() const;
 
-    CompositingReasons styleDeterminedDirectReasons(RenderObject*) const;
     CompositingReasons nonStyleDeterminedDirectReasons(const RenderLayer*, bool* needToRecomputeCompositingRequirements) const;
 
     bool requiresCompositingForAnimation(RenderObject*) const;
--- third_party/WebKit/Source/platform/graphics/CompositingReasons.h
+++ third_party/WebKit/Source/platform/graphics/CompositingReasons.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 const uint64_t CompositingReasonNone                                   = 0;
+const uint64_t CompositingReasonAll                                    = ~static_cast<uint64_t>(0);
 
 // Intrinsic reasons that can be known right away by the layer
 const uint64_t CompositingReason3DTransform                            = UINT64_C(1) << 0;
@@ -89,17 +90,11 @@ const uint64_t CompositingReasonComboAllDirectReasons =
     | CompositingReasonVideoOverlay
     | CompositingReasonWillChange;
 
-const uint64_t CompositingReasonComboAllStyleDeterminedDirectReasons =
+const uint64_t CompositingReasonComboAllStyleDeterminedReasons =
     CompositingReason3DTransform
-    | CompositingReasonVideo
-    | CompositingReasonCanvas
-    | CompositingReasonPlugin
-    | CompositingReasonIFrame
     | CompositingReasonBackfaceVisibilityHidden
-    | CompositingReasonActiveAnimation
     | CompositingReasonTransitionProperty
     | CompositingReasonFilters
-    | CompositingReasonVideoOverlay
     | CompositingReasonWillChange;
 
 const uint64_t CompositingReasonComboReasonsThatRequireOwnBacking =
