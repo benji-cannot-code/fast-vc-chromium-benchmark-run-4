6bed9f31726cbc37cac91568d0a71aa477a29180
mef@chromium.org <mef@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-21 11:12:30 +0000

    Cronet Java wrappers to fallback to HttpUrlConnection on Pre-ICS platform.
    Based on https://codereview.chromium.org/145213003/.
    
    BUG=354143
    
    Review URL: https://codereview.chromium.org/183333002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258517 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ net/cronet/android/java/src/org/chromium/net/ChromiumUrlRequestFactory.java
@@ -0,0 +1,53 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.net;
+
+import android.content.Context;
+import android.os.Build;
+
+import java.nio.channels.WritableByteChannel;
+import java.util.Map;
+
+/**
+ * Network request factory using the native http stack implementation.
+ */
+@UsedByReflection("HttpUrlRequestFactory.java")
+class ChromiumUrlRequestFactory extends HttpUrlRequestFactory {
+    private static ChromiumUrlRequestContext sRequestContext;
+
+    @UsedByReflection("HttpUrlRequestFactory.java")
+    public ChromiumUrlRequestFactory(Context context) {
+        if (sRequestContext == null && isEnabled()) {
+            System.loadLibrary("cronet");
+            sRequestContext = ChromiumUrlRequestContext.getInstance(context);
+        }
+    }
+
+    @Override
+    protected boolean isEnabled() {
+        return Build.VERSION.SDK_INT >= 14;
+    }
+
+    @Override
+    protected String getName() {
+        return "Chromium/" + UrlRequestContext.getVersion();
+    }
+
+    @Override
+    protected HttpUrlRequest createRequest(String url, int requestPriority,
+            Map<String, String> headers, HttpUrlRequestListener listener) {
+        return new ChromiumUrlRequest(sRequestContext, url, requestPriority,
+                headers, listener);
+    }
+
+    @Override
+    protected HttpUrlRequest createRequest(String url, int requestPriority,
+            Map<String, String> headers, WritableByteChannel channel,
+            HttpUrlRequestListener listener) {
+        return new ChromiumUrlRequest(sRequestContext, url, requestPriority,
+                headers, channel, listener);
+    }
+}
---
+++ net/cronet/android/java/src/org/chromium/net/HttpUrlRequestListener.java
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.net;
+
+/**
+ * Callback interface.
+ */
+public interface HttpUrlRequestListener {
+    /**
+     * The listener should completely process the response in the callback
+     * method. Immediately after the callback, the request object will be
+     * recycled.
+     */
+    void onRequestComplete(HttpUrlRequest request);
+}
