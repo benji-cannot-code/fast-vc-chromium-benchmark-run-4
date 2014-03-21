a40b9e96eefa655f39a190c7cbea8b6f0d12e2cd
vandebo@chromium.org <vandebo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-21 21:40:35 +0000

    Revert of GN: Remove some dead code that was confusing. (https://codereview.chromium.org/202733003/)
    Reason for revert:
    Compile failure:
    
    ../../tools/gn/tokenizer_unittest.cc: In member function 'virtual void Tokenizer_StringUnescaping_Test::TestBody()':
    ../../tools/gn/tokenizer_unittest.cc:141:3:error: 'class Token' has no member named 'StringValue'
    ../../tools/gn/tokenizer_unittest.cc:142:3:error: 'class Token' has no member named 'StringValue'
    
    
    Original issue's description:
    > GN: Remove some dead code that was confusing.
    > 
    > BUG=354626
    > R=brettw@chromium.org
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=258672
    
    TBR=brettw@chromium.org,scottmg@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=354626
    
    Review URL: https://codereview.chromium.org/208663004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258675 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/gn/token.cc
+++ tools/gn/token.cc
@@ -7,6 +7,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 
+namespace {
+
+std::string UnescapeString(const base::StringPiece& input) {
+  std::string result;
+  result.reserve(input.size());
+
+  for (size_t i = 0; i < input.size(); i++) {
+    if (input[i] == '\\') {
+      DCHECK(i < input.size() - 1);  // Last char shouldn't be a backslash or
+                                     // it would have escaped the terminator.
+      i++;  // Skip backslash, next char is a literal.
+    }
+    result.push_back(input[i]);
+  }
+  return result;
+}
+
+}  // namespace
+
 Token::Token() : type_(INVALID), value_() {
 }
 
@@ -24,4 +43,11 @@ bool Token::IsIdentifierEqualTo(const char* v) const {
 
 bool Token::IsStringEqualTo(const char* v) const {
   return type_ == STRING && value_ == v;
+}
+
+std::string Token::StringValue() const {
+  DCHECK(type() == STRING);
+
+  // Trim off the string terminators at the end.
+  return UnescapeString(value_.substr(1, value_.size() - 2));
 }
