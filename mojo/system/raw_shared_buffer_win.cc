93761712764b48f9676c4dc2dcd7a33886e68d49
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-21 20:45:27 +0000

    Mojo: Add a simple platform-level shared memory implementation.
    (Unfortunately, base::SharedMemory doesn't quite meet our needs right
    now, and it'll be a while -- if ever -- before I manage to refactor it
    so that it does.)
    
    Not yet implemented on Windows. (This should be simple, but I'll do it
    separately.)
    
    R=darin@chromium.org
    
    Review URL: https://codereview.chromium.org/205583008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258667 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ mojo/system/raw_shared_buffer_win.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "mojo/system/raw_shared_buffer.h"
+
+#include "base/logging.h"
+
+namespace mojo {
+namespace system {
+
+// RawSharedBuffer::Mapping ----------------------------------------------------
+
+void RawSharedBuffer::Mapping::Unmap() {
+  // TODO(vtl)
+  NOTIMPLEMENTED();
+}
+
+// RawSharedBuffer -------------------------------------------------------------
+
+bool RawSharedBuffer::Init() {
+  // TODO(vtl)
+  NOTIMPLEMENTED();
+  return false;
+}
+
+scoped_ptr<RawSharedBuffer::Mapping> RawSharedBuffer::MapImpl(size_t offset,
+                                                              size_t length) {
+  // TODO(vtl)
+  NOTIMPLEMENTED();
+  return scoped_ptr<Mapping>();
+}
+
+}  // namespace system
+}  // namespace mojo
