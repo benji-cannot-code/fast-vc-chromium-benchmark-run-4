5164bcc7d107a023a74890ce971fb93b37d85ec8
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-21 07:16:27 +0000

    Fix DevToolsSanityTest.TestScriptsTabIsPopulatedOnInspectedPageRefresh after recent changes.
    TBR=lushnikov
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/207323005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169722 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/Tests.js
+++ third_party/WebKit/Source/devtools/front_end/Tests.js
@@ -899,7 +899,7 @@ TestSuite.prototype._waitUntilScriptsAreParsed = function(expectedScripts, callb
         if (test._scriptsAreParsed(expectedScripts))
             callback();
         else
-            test.addSniffer(WebInspector.panels.sources, "_addUISourceCode", waitForAllScripts);
+            test.addSniffer(WebInspector.panels.sources.sourcesEditor(), "_addUISourceCode", waitForAllScripts);
     }
 
     waitForAllScripts();
