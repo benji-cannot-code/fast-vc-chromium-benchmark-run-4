db32a182a77c9faa9a26fbc32b7ed618d66a5c6c
nasko@chromium.org <nasko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-21 11:21:39 +0000

    Add filtering of IPC messages when RenderFrameHost is swapped out.
    BUG=351815
    
    Review URL: https://codereview.chromium.org/205543002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258521 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/frame_messages.h"
 #include "content/common/input_messages.h"
 #include "content/common/inter_process_time_ticks_converter.h"
+#include "content/common/swapped_out_messages.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/content_browser_client.h"
 #include "content/public/browser/render_process_host.h"
@@ -229,6 +230,26 @@ bool RenderFrameHostImpl::Send(IPC::Message* message) {
 }
 
 bool RenderFrameHostImpl::OnMessageReceived(const IPC::Message &msg) {
+  // Filter out most IPC messages if this renderer is swapped out.
+  // We still want to handle certain ACKs to keep our state consistent.
+  // TODO(nasko): Only check RenderViewHost state, as this object's own state
+  // isn't yet properly updated. Transition this check once the swapped out
+  // state is correct in RenderFrameHost itself.
+  if (render_view_host_->IsSwappedOut()) {
+    if (!SwappedOutMessages::CanHandleWhileSwappedOut(msg)) {
+      // If this is a synchronous message and we decided not to handle it,
+      // we must send an error reply, or else the renderer will be stuck
+      // and won't respond to future requests.
+      if (msg.is_sync()) {
+        IPC::Message* reply = IPC::SyncMessage::GenerateReply(&msg);
+        reply->set_reply_error();
+        Send(reply);
+      }
+      // Don't continue looking for someone to handle it.
+      return true;
+    }
+  }
+
   if (delegate_->OnMessageReceived(this, msg))
     return true;
 
