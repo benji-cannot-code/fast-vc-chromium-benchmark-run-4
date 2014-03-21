87e590f13b5a7088b11aef5facb811a1183a9baf
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-21 13:29:48 +0000

    Fix HeapSnapshotView leaks
    HeapSnapshotView should be unsubscribed from events it has been subscribed to, otherwise it will leak when corresponding profile is removed.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/208053003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169738 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/HeapSnapshotView.js
+++ third_party/WebKit/Source/devtools/front_end/HeapSnapshotView.js
@@ -40,7 +40,7 @@ WebInspector.HeapSnapshotView = function(profile)
 
     this.element.classList.add("heap-snapshot-view");
 
-    profile.profileType().addEventListener(WebInspector.HeapSnapshotProfileType.SnapshotReceived, this._onReceivSnapshot, this);
+    profile.profileType().addEventListener(WebInspector.HeapSnapshotProfileType.SnapshotReceived, this._onReceiveSnapshot, this);
     profile.profileType().addEventListener(WebInspector.ProfileType.Events.RemoveProfileHeader, this._onProfileHeaderRemoved, this);
 
     if (profile._profileType.id === WebInspector.TrackingHeapSnapshotProfileType.TypeId) {
@@ -978,7 +978,7 @@ WebInspector.HeapSnapshotView.prototype = {
     /**
      * @param {!WebInspector.Event} event
      */
-    _onReceivSnapshot: function(event)
+    _onReceiveSnapshot: function(event)
     {
         this._updateControls();
     },
@@ -991,7 +991,7 @@ WebInspector.HeapSnapshotView.prototype = {
         var profile = event.data;
         if (this._profile === profile) {
             this.detach();
-            this._profile.profileType().removeEventListener(WebInspector.ProfileType.Events.AddProfileHeader, this._onReceivSnapshot, this);
+            this._profile.profileType().removeEventListener(WebInspector.HeapSnapshotProfileType.SnapshotReceived, this._onReceiveSnapshot, this);
             this._profile.profileType().removeEventListener(WebInspector.ProfileType.Events.RemoveProfileHeader, this._onProfileHeaderRemoved, this);
         } else {
             this._updateControls();
