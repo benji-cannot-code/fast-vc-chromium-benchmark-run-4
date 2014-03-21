abe8a088c67e5dde7c1a557290d203040cbf7d16
ksimbili@google.com <ksimbili@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-21 00:59:20 +0000

    Expose whether a scroll is active via ContentViewCore
    Track the active fling count in addition to whether touch scrolling is active.
    This is useful for transient GestureStateListeners to precisely determine the
    current state of scroll activity.
    
    BUG=354599
    
    Review URL: https://codereview.chromium.org/202253002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258481 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -416,6 +416,10 @@ public class ContentViewCore
     // sequence, so this will also be true for the duration of a pinch gesture.
     private boolean mTouchScrollInProgress;
 
+    // The outstanding fling start events that hasn't got fling end yet. It may be > 1 because
+    // onNativeFlingStopped() is called asynchronously.
+    private int mPotentiallyActiveFlingCount;
+
     private ViewAndroid mViewAndroid;
 
     private SmartClipDataListener mSmartClipDataListener = null;
@@ -1204,10 +1208,15 @@ public class ContentViewCore
         nativeIgnoreRemainingTouchEvents(mNativeContentViewCore);
     }
 
+    public boolean isScrollInProgress() {
+        return mTouchScrollInProgress || mPotentiallyActiveFlingCount > 0;
+    }
+
     @SuppressWarnings("unused")
     @CalledByNative
     private void onFlingStartEventConsumed(int vx, int vy) {
         mTouchScrollInProgress = false;
+        mPotentiallyActiveFlingCount++;
         temporarilyHideTextHandles();
         for (mGestureStateListenersIterator.rewind();
                     mGestureStateListenersIterator.hasNext();) {
@@ -3113,6 +3122,7 @@ public class ContentViewCore
         // Note that mTouchScrollInProgress should normally be false at this
         // point, but we reset it anyway as another failsafe.
         mTouchScrollInProgress = false;
+        if (mPotentiallyActiveFlingCount > 0) mPotentiallyActiveFlingCount--;
         updateGestureStateListener(GestureEventType.FLING_END);
     }
 
