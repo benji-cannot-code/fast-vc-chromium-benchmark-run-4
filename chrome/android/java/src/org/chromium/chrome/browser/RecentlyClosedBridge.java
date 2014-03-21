60d84f9ded57fc1a299feb0e60ab17752c9b106d
newt@chromium.org <newt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-21 22:45:07 +0000

    [Android] Support opening a recently closed tab in a new tab.
    BUG=321633
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/207273003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258704 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/android/java/src/org/chromium/chrome/browser/RecentlyClosedBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/RecentlyClosedBridge.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser;
 
 import org.chromium.base.CalledByNative;
 import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.ui.WindowOpenDisposition;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -95,6 +96,7 @@ public class RecentlyClosedBridge {
         return received ? tabs : null;
     }
 
+    // TODO(newt): delete this once all callers are using the new method below.
     /**
      * Opens a recently closed tab in the current tab.
      *
@@ -103,7 +105,23 @@ public class RecentlyClosedBridge {
      * @return Whether the tab was successfully opened.
      */
     public boolean openRecentlyClosedTab(Tab tab, RecentlyClosedTab recentTab) {
-        return nativeOpenRecentlyClosedTab(mNativeRecentlyClosedTabsBridge, tab, recentTab.id);
+        return openRecentlyClosedTab(tab, recentTab, WindowOpenDisposition.CURRENT_TAB);
+    }
+
+    /**
+     * Opens a recently closed tab in the current tab or a new tab. If opened in the current tab,
+     * the current tab's entire history is replaced.
+     *
+     * @param tab The current Tab.
+     * @param recentTab The RecentlyClosedTab to open.
+     * @param windowOpenDisposition The WindowOpenDisposition value specifying whether to open in
+     *         the current tab or a new tab.
+     * @return Whether the tab was successfully opened.
+     */
+    public boolean openRecentlyClosedTab(Tab tab, RecentlyClosedTab recentTab,
+            int windowOpenDisposition) {
+        return nativeOpenRecentlyClosedTab(mNativeRecentlyClosedTabsBridge, tab, recentTab.id,
+                windowOpenDisposition);
     }
 
     /**
@@ -119,7 +137,7 @@ public class RecentlyClosedBridge {
             long nativeRecentlyClosedTabsBridge, RecentlyClosedCallback callback);
     private native boolean nativeGetRecentlyClosedTabs(
             long nativeRecentlyClosedTabsBridge, List<RecentlyClosedTab> tabs, int maxTabCount);
-    private native boolean nativeOpenRecentlyClosedTab(
-            long nativeRecentlyClosedTabsBridge, Tab tab, int recentTabId);
+    private native boolean nativeOpenRecentlyClosedTab(long nativeRecentlyClosedTabsBridge,
+            Tab tab, int recentTabId, int windowOpenDisposition);
     private native void nativeClearRecentlyClosedTabs(long nativeRecentlyClosedTabsBridge);
 }
