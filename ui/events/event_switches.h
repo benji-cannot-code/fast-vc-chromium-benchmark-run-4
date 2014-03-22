2794ee5b6e65ed18446e51737e673b8546fa76c2
spang@chromium.org <spang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-22 04:15:20 +0000

    events: Move --touch-calibration flag to event_switches
    We need this from ui/events/ozone to access the touchscreen calibration in
    ozone builds, so move it to event switches.
    
    BUG=355046
    TEST=build
    
    Review URL: https://codereview.chromium.org/208423004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258774 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/events/event_switches.h
+++ ui/events/event_switches.h
@@ -21,6 +21,10 @@ EVENTS_BASE_EXPORT extern const char kTouchEventsDisabled[];
 EVENTS_BASE_EXPORT extern const char kTouchDevices[];
 #endif
 
+#if defined(USE_XI2_MT)
+EVENTS_BASE_EXPORT extern const char kTouchCalibration[];
+#endif
+
 }  // namespace switches
 
 #endif  // UI_EVENTS_EVENTS_SWITCHES_H_
