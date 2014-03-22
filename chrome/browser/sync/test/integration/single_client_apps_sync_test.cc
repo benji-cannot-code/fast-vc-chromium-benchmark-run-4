a4afe534307d46586e94f9164bbbdf6f01b10c7a
rlarocque@chromium.org <rlarocque@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-22 04:50:50 +0000

    sync: Shrink ProfileSyncServiceHarness (retry)
    The first attempt at this change, r258666, had to be reverted because it
    triggered a compile failure on the buildbots.  Apparently the build bots
    still build the sync performance tests, while the try bots do not.
    
    Original commit message follows.
    
    Reduces the number of tasks for which the ProfileSyncServiceHarness is
    responsible.
    
    The goal here is to clean up some organizational issues that are left
    over from the old ProfileSyncServiceHarness design.  Now that the
    harness no longer needs to be in control of everything, we can start to
    shrink it down to a more appropriate size.
    
    Highlights inlcude:
    - Move the responsibility for observing the ProfileSyncService to the
      StatusChangeCheckers.  PSSHarness is no longer a PSSObserver, and no
      longer implements AwaitStatusChange().
    - Move responsibility for initializing P2P invalidations to the sync
      tests.  The PSSHarness no longer owns the invalidations forwarder
      and no longer takes a reference to the InvalidationService.
    - Move AwaitPassphraseRequired, AwaitPassphraseAccepted and
      AwaitCommitActivityCompletion into sync_integration_test_util.h.
      There was no need for them to depend on the harness.  Unlike some
      other functions that were refactored, these were used in several
      different tests, so they were moved to a util file to encourage
      code sharing.
    - Hide some Await functions that are only used inside the harness,
      such as AwaitSyncSetupCompletion() and AwaitBackendInitialized().
    - Move some encryption functionality into sync_test.cc.
    - Convert several functions to use ProfileSyncService* parameters,
      rather than ProfileSyncServiceHarness*.
    
    TBR=tim@chromium.org,dcheng@chromium.org
    BUG=323380
    
    Review URL: https://codereview.chromium.org/196173010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258779 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/test/integration/single_client_apps_sync_test.cc
+++ chrome/browser/sync/test/integration/single_client_apps_sync_test.cc
@@ -6,11 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "chrome/browser/sync/test/integration/apps_helper.h"
 #include "chrome/browser/sync/test/integration/profile_sync_service_harness.h"
+#include "chrome/browser/sync/test/integration/sync_integration_test_util.h"
 #include "chrome/browser/sync/test/integration/sync_test.h"
 
 using apps_helper::AllProfilesHaveSameAppsAsVerifier;
 using apps_helper::InstallApp;
 using apps_helper::InstallPlatformApp;
+using sync_integration_test_util::AwaitCommitActivityCompletion;
 
 class SingleClientAppsSyncTest : public SyncTest {
  public:
@@ -65,7 +67,7 @@ IN_PROC_BROWSER_TEST_F(SingleClientAppsSyncTest, InstallSomeLegacyApps) {
     InstallApp(verifier(), i);
   }
 
-  ASSERT_TRUE(GetClient(0)->AwaitCommitActivityCompletion());
+  ASSERT_TRUE(AwaitCommitActivityCompletion(GetClient(0)->service()));
 
   ASSERT_TRUE(AllProfilesHaveSameAppsAsVerifier());
 }
@@ -79,7 +81,7 @@ IN_PROC_BROWSER_TEST_F(SingleClientAppsSyncTest, InstallSomePlatformApps) {
     InstallPlatformApp(verifier(), i);
   }
 
-  ASSERT_TRUE(GetClient(0)->AwaitCommitActivityCompletion());
+  ASSERT_TRUE(AwaitCommitActivityCompletion(GetClient(0)->service()));
 
   ASSERT_TRUE(AllProfilesHaveSameAppsAsVerifier());
 }
@@ -101,7 +103,7 @@ IN_PROC_BROWSER_TEST_F(SingleClientAppsSyncTest, InstallSomeApps) {
     InstallPlatformApp(verifier(), i);
   }
 
-  ASSERT_TRUE(GetClient(0)->AwaitCommitActivityCompletion());
+  ASSERT_TRUE(AwaitCommitActivityCompletion(GetClient(0)->service()));
 
   ASSERT_TRUE(AllProfilesHaveSameAppsAsVerifier());
 }
--- chrome/browser/sync/test/integration/single_client_themes_sync_test.cc
+++ chrome/browser/sync/test/integration/single_client_themes_sync_test.cc
@@ -5,9 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "chrome/browser/sync/test/integration/profile_sync_service_harness.h"
+#include "chrome/browser/sync/test/integration/sync_integration_test_util.h"
 #include "chrome/browser/sync/test/integration/sync_test.h"
 #include "chrome/browser/sync/test/integration/themes_helper.h"
 
+using sync_integration_test_util::AwaitCommitActivityCompletion;
 using themes_helper::GetCustomTheme;
 using themes_helper::GetThemeID;
 using themes_helper::UseCustomTheme;
@@ -41,7 +43,7 @@ IN_PROC_BROWSER_TEST_F(SingleClientThemesSyncTest, CustomTheme) {
   ASSERT_EQ(GetCustomTheme(0), GetThemeID(GetProfile(0)));
   ASSERT_EQ(GetCustomTheme(0), GetThemeID(verifier()));
 
-  ASSERT_TRUE(GetClient(0)->AwaitCommitActivityCompletion());
+  ASSERT_TRUE(AwaitCommitActivityCompletion(GetClient(0)->service()));
 
   ASSERT_EQ(GetCustomTheme(0), GetThemeID(GetProfile(0)));
   ASSERT_EQ(GetCustomTheme(0), GetThemeID(verifier()));
@@ -60,14 +62,14 @@ IN_PROC_BROWSER_TEST_F(SingleClientThemesSyncTest, NativeTheme) {
   ASSERT_FALSE(UsingNativeTheme(GetProfile(0)));
   ASSERT_FALSE(UsingNativeTheme(verifier()));
 
-  ASSERT_TRUE(GetClient(0)->AwaitCommitActivityCompletion());
+  ASSERT_TRUE(AwaitCommitActivityCompletion(GetClient(0)->service()));
 
   UseNativeTheme(GetProfile(0));
   UseNativeTheme(verifier());
   ASSERT_TRUE(UsingNativeTheme(GetProfile(0)));
   ASSERT_TRUE(UsingNativeTheme(verifier()));
 
-  ASSERT_TRUE(GetClient(0)->AwaitCommitActivityCompletion());
+  ASSERT_TRUE(AwaitCommitActivityCompletion(GetClient(0)->service()));
 
   ASSERT_TRUE(UsingNativeTheme(GetProfile(0)));
   ASSERT_TRUE(UsingNativeTheme(verifier()));
@@ -81,14 +83,14 @@ IN_PROC_BROWSER_TEST_F(SingleClientThemesSyncTest, DefaultTheme) {
   ASSERT_FALSE(UsingDefaultTheme(GetProfile(0)));
   ASSERT_FALSE(UsingDefaultTheme(verifier()));
 
-  ASSERT_TRUE(GetClient(0)->AwaitCommitActivityCompletion());
+  ASSERT_TRUE(AwaitCommitActivityCompletion(GetClient(0)->service()));
 
   UseDefaultTheme(GetProfile(0));
   UseDefaultTheme(verifier());
   ASSERT_TRUE(UsingDefaultTheme(GetProfile(0)));
   ASSERT_TRUE(UsingDefaultTheme(verifier()));
 
-  ASSERT_TRUE(GetClient(0)->AwaitCommitActivityCompletion());
+  ASSERT_TRUE(AwaitCommitActivityCompletion(GetClient(0)->service()));
 
   ASSERT_TRUE(UsingDefaultTheme(GetProfile(0)));
   ASSERT_TRUE(UsingDefaultTheme(verifier()));
