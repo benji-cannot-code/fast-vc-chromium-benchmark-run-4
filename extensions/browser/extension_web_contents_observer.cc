c6f07f3ef7549e77fe978362e09d8cb9400be1f9
d.halman@gmail.com <d.halman@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-22 06:14:18 +0000

    Ensure extensions.getViews(type: "tab") contains tab created by window.open
    A tab's ViewType is set by RenderViewCreated. It turns out window.open does not
    trigger a call to ExtensionWebContentsObserver::RenderViewCreated like
    tabs.create does. This is the reason ViewType is not set for window.open.
    
    The constructor is called for both cases, and I found that sending the same message:
       render_view_host->Send(new ExtensionMsg_NotifyRenderViewType(
          render_view_host->GetRoutingID(),
          GetViewType(web_contents())));
    
    from the constructor (copy pasted from RenderViewCreated) ensures the view's type is
    set from the web contents whether or not the view was rendered. This passes the
    test extension posted in the bug description.
    
    However, this may be a simple cover up for the real issue of RenderViewCreated
    not being called in the window.open case. I am not extremely familiar with the
    way this object is supposed to be treated, so I'll defer to reviewers on the
    proper fix.
    
    BUG=341459
    
    Review URL: https://codereview.chromium.org/200513007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258794 0039d316-1c4b-4281-b951-d872f2087c98

--- extensions/browser/extension_web_contents_observer.cc
+++ extensions/browser/extension_web_contents_observer.cc
@@ -22,14 +22,15 @@ namespace extensions {
 ExtensionWebContentsObserver::ExtensionWebContentsObserver(
     content::WebContents* web_contents)
     : content::WebContentsObserver(web_contents),
-      browser_context_(web_contents->GetBrowserContext()) {}
+      browser_context_(web_contents->GetBrowserContext()) {
+  NotifyRenderViewType(web_contents->GetRenderViewHost());
+}
 
 ExtensionWebContentsObserver::~ExtensionWebContentsObserver() {}
 
 void ExtensionWebContentsObserver::RenderViewCreated(
     content::RenderViewHost* render_view_host) {
-  render_view_host->Send(new ExtensionMsg_NotifyRenderViewType(
-      render_view_host->GetRoutingID(), GetViewType(web_contents())));
+  NotifyRenderViewType(render_view_host);
 
   const Extension* extension = GetExtension(render_view_host);
   if (!extension)
@@ -80,6 +81,14 @@ void ExtensionWebContentsObserver::RenderViewCreated(
     case Manifest::TYPE_THEME:
     case Manifest::TYPE_SHARED_MODULE:
       break;
+  }
+}
+
+void ExtensionWebContentsObserver::NotifyRenderViewType(
+    content::RenderViewHost* render_view_host) {
+  if (render_view_host) {
+    render_view_host->Send(new ExtensionMsg_NotifyRenderViewType(
+        render_view_host->GetRoutingID(), GetViewType(web_contents())));
   }
 }
 
