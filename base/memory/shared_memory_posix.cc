ffcc976772c1a7672a9e87285af9cff9a815144b
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-22 06:37:09 +0000

    Remove the (private) SharedMemory::{PrepareMapFile,FilePathForMemoryName}() on Android.
    The first isn't implemented and the second isn't used.
    
    TBR=brettw@chromium.org
    
    Review URL: https://codereview.chromium.org/206453009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258797 0039d316-1c4b-4281-b951-d872f2087c98

--- base/memory/shared_memory_posix.cc
+++ base/memory/shared_memory_posix.cc
@@ -271,7 +271,6 @@ bool SharedMemory::Open(const std::string& name, bool read_only) {
   }
   return PrepareMapFile(fp.Pass(), readonly_fd.Pass());
 }
-
 #endif  // !defined(OS_ANDROID)
 
 bool SharedMemory::MapAt(off_t offset, size_t bytes) {
@@ -386,7 +385,6 @@ bool SharedMemory::PrepareMapFile(ScopedFILE fp, ScopedFD readonly_fd) {
 
   return true;
 }
-#endif
 
 // For the given shmem named |mem_name|, return a filename to mmap()
 // (and possibly create).  Modifies |filename|.  Return false on
@@ -414,6 +412,7 @@ bool SharedMemory::FilePathForMemoryName(const std::string& mem_name,
   *path = temp_dir.AppendASCII(name_base + ".shmem." + mem_name);
   return true;
 }
+#endif  // !defined(OS_ANDROID)
 
 void SharedMemory::LockOrUnlockCommon(int function) {
   DCHECK_GE(mapped_file_, 0);
