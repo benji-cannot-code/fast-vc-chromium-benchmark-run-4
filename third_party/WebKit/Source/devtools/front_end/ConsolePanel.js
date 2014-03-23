dd6e4c8ddb7dfe88f1cbfb0ee5a208a81c5719ee
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-23 08:13:51 +0000

    DevTools: switching from Console tab does not restore console in the drawer.
    TBR=vsevik
    
    Review URL: https://codereview.chromium.org/199633011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169811 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ConsolePanel.js
+++ third_party/WebKit/Source/devtools/front_end/ConsolePanel.js
@@ -85,6 +85,8 @@ WebInspector.ConsolePanel.WrapperView = function()
     WebInspector.VBox.call(this);
     this.element.classList.add("console-view-wrapper");
 
+    WebInspector.ConsolePanel.WrapperView._instance = this;
+
     this._view = WebInspector.ConsolePanel._view();
     // FIXME: this won't be needed once drawer becomes a view.
     this.wasShown();
