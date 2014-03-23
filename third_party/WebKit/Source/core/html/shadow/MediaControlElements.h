e5f22a6e821ac2cf54f130f202348a0ddb1bd54e
philipj@opera.com <philipj@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-23 13:32:26 +0000

    Implement the activation behavior of media elements (click to play/pause)
    http://www.whatwg.org/specs/web-apps/current-work/multipage/the-video-element.html#user-interface
    
    Added to the spec in http://html5.org/r/8315 after a long discussion:
    http://lists.whatwg.org/htdig.cgi/whatwg-whatwg.org/2013-November/041662.html
    
    Firefox already implements click to play/pause when controls are
    visible, but that implementation predates the spec so accesskey does not
    work.
    
    HTMLMediaElement::togglePlayState() is made public so that it can be
    used to implement play/pause in MediaControls also.
    
    The video-click-dblckick-standalone.html test has been expected to fail
    since April 2013, r148246, so remove it now since the the results would
    change. The new tests provide sufficient coverage.
    
    BUG=354746
    TEST=LayoutTests/media/activation-behavior.html
         LayoutTests/media/activation-behavior-accesskey.html
         LayoutTests/media/activation-behavior-shadow.html
    
    Review URL: https://codereview.chromium.org/208483002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169812 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/shadow/MediaControlElements.h
+++ third_party/WebKit/Source/core/html/shadow/MediaControlElements.h
@@ -50,6 +50,7 @@ private:
     explicit MediaControlPanelElement(MediaControls&);
 
     virtual const AtomicString& shadowPseudoId() const OVERRIDE;
+    virtual void defaultEventHandler(Event*) OVERRIDE;
 
     void startTimer();
     void stopTimer();
