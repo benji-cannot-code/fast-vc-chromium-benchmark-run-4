f610a2c79f2597aff23792cf01fe23f81324370c
nick@chromium.org <nick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-24 02:15:54 +0000

    Step 3 in unifying the task manager's WebContents-based ResourceProviders.
    In this change, GuestResourceProvider is distilled into
    GuestInformation, which plugs into WebContentsResourceProvider.
    
    ( WebContentsResourceProvider is introduced in an earlier patch:
    https://codereview.chromium.org/196203003/ )
    
    BUG=348836
    TEST=browser_tests
    
    Review URL: https://codereview.chromium.org/199413028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258835 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/task_manager/task_manager.cc
+++ chrome/browser/task_manager/task_manager.cc
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/task_manager/browser_process_resource_provider.h"
 #include "chrome/browser/task_manager/child_process_resource_provider.h"
 #include "chrome/browser/task_manager/extension_information.h"
-#include "chrome/browser/task_manager/guest_resource_provider.h"
+#include "chrome/browser/task_manager/guest_information.h"
 #include "chrome/browser/task_manager/panel_information.h"
 #include "chrome/browser/task_manager/resource_provider.h"
 #include "chrome/browser/task_manager/tab_contents_resource_provider.h"
@@ -263,7 +263,10 @@ TaskManagerModel::TaskManagerModel(TaskManager* task_manager)
       task_manager,
       scoped_ptr<WebContentsInformation>(
           new task_manager::ExtensionInformation())));
-  AddResourceProvider(new task_manager::GuestResourceProvider(task_manager));
+  AddResourceProvider(new task_manager::WebContentsResourceProvider(
+      task_manager,
+      scoped_ptr<WebContentsInformation>(
+          new task_manager::GuestInformation())));
 
 #if !defined(OS_CHROMEOS) && defined(ENABLE_NOTIFICATIONS)
   ResourceProvider* provider =
