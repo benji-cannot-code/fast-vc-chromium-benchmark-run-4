04aaff59b6e101c0d9d424fa5a421a3d172e87d5
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-24 16:52:33 +0000

    DevTools: Simplify Page.captureScreenshot API and implementation (Chromium side)
    Currently Page.captureScreenshot shares the parameter list with Page.startScreencast. This is for historical reasons and is in fact redundant. With this patch Page.captureScreenshot takes no parameters and returns a single result (base64 png string).
     
    BUG=242405
    
    Review URL: https://codereview.chromium.org/183803029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258955 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/devtools/renderer_overrides_handler.h
+++ content/browser/devtools/renderer_overrides_handler.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
+#include "base/memory/ref_counted_memory.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
@@ -44,6 +45,8 @@ class CONTENT_EXPORT RendererOverridesHandler
   void ParseCaptureParameters(DevToolsProtocol::Command* command,
                               std::string* format, int* quality,
                               double* scale);
+  base::DictionaryValue* CreateScreenshotResponse(
+      const std::vector<unsigned char>& png_data);
 
   // DOM domain.
   scoped_refptr<DevToolsProtocol::Response>
@@ -76,6 +79,9 @@ class CONTENT_EXPORT RendererOverridesHandler
 
   void ScreenshotCaptured(
       scoped_refptr<DevToolsProtocol::Command> command,
+      scoped_refptr<base::RefCountedBytes> png_data);
+
+  void ScreencastFrameCaptured(
       const std::string& format,
       int quality,
       const cc::CompositorFrameMetadata& metadata,
