d8065e3861b73ceb2109c9b2ce65d743f64cb019
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-24 18:08:42 +0000

    Ignore --google-base-url if the specified URL has a query or ref.
    We also fix up the passed-in string, so users have a little more flexibility in
    syntax, e.g. you can now specify a base URL of just "google.com" instead of
    "http://google.com/".
    
    BUG=240746
    TEST=none
    R=samarth@chromium.org
    
    Review URL: https://codereview.chromium.org/197583011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258969 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/search_engines/search_terms_data.cc
+++ chrome/browser/search_engines/search_terms_data.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/search_engines/search_terms_data.h"
 
-#include "base/command_line.h"
 #include "base/logging.h"
 #include "base/metrics/field_trial.h"
 #include "base/prefs/pref_service.h"
@@ -17,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/glue/device_info.h"
 #include "chrome/browser/themes/theme_service.h"
 #include "chrome/browser/themes/theme_service_factory.h"
-#include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 #include "content/public/browser/browser_thread.h"
 #include "sync/protocol/sync.pb.h"
@@ -97,10 +95,9 @@ std::string UIThreadSearchTermsData::GoogleBaseURLValue() const {
       BrowserThread::CurrentlyOn(BrowserThread::UI));
   if (google_base_url_)
     return *google_base_url_;
-  std::string base_url = CommandLine::ForCurrentProcess()->
-      GetSwitchValueASCII(switches::kGoogleBaseURL);
-  if (!base_url.empty())
-    return base_url;
+  GURL base_url(google_util::CommandLineGoogleBaseURL());
+  if (base_url.is_valid())
+    return base_url.spec();
   return profile_ ? GoogleURLTracker::GoogleURL(profile_).spec() :
       SearchTermsData::GoogleBaseURLValue();
 }
