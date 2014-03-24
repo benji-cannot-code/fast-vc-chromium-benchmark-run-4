6ee065b9b5e1ac73f0e1397b17078d60cf024e6a
tnagel@chromium.org <tnagel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-24 07:05:15 +0000

    Fix display of policy-provisioned wallpaper on login screen.
    BUG=220418
    
    Review URL: https://codereview.chromium.org/208473002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258884 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/wallpaper_manager.cc
+++ chrome/browser/chromeos/login/wallpaper_manager.cc
@@ -829,7 +829,7 @@ void WallpaperManager::ScheduleSetUserWallpaper(const std::string& user_id,
     GetPendingWallpaper(user_id, delayed)
         ->ResetSetWallpaperImage(user_wallpaper, info);
   } else {
-    if (info.type == User::CUSTOMIZED) {
+    if (info.type == User::CUSTOMIZED || info.type == User::POLICY) {
       ash::WallpaperResolution resolution =
           ash::Shell::GetInstance()->
               desktop_background_controller()->
@@ -953,7 +953,7 @@ void WallpaperManager::CacheUserWallpaper(const std::string& user_id) {
   if (GetUserWallpaperInfo(user_id, &info)) {
     base::FilePath wallpaper_dir;
     base::FilePath wallpaper_path;
-    if (info.type == User::CUSTOMIZED) {
+    if (info.type == User::CUSTOMIZED || info.type == User::POLICY) {
       ash::WallpaperResolution resolution = ash::Shell::GetInstance()->
           desktop_background_controller()->GetAppropriateResolution();
       const char* sub_dir  = (resolution == ash::WALLPAPER_RESOLUTION_SMALL) ?
