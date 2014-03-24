a70f7ea898b66e1dac2b9e60102e575db648ca1d
merkulova@chromium.org <merkulova@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-24 15:52:33 +0000

    EULA and other OOBE screens get spoken title for accessibility.
    Screens affected: Sign-in, EULA, username creation for Locally managed user.
    
    Added spoken feedback for camera take/discard user image events. Corresponding strings changed.
    
    BUG=346810
    
    Review URL: https://codereview.chromium.org/190423003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258939 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/login/oobe_screen_user_image.js
+++ chrome/browser/resources/chromeos/login/oobe_screen_user_image.js
@@ -167,6 +167,8 @@ cr.define('login', function() {
       this.profileImageLoading_ = value;
       $('user-image-screen-main').classList[
           value ? 'add' : 'remove']('profile-image-loading');
+      if (value)
+        announceAccessibleMessage(loadTimeData.getString('syncingPreferences'));
       this.updateProfileImageCaption_();
     },
 
@@ -248,8 +250,7 @@ cr.define('login', function() {
       imageGrid.flipPhoto = !imageGrid.flipPhoto;
       var flipMessageId = imageGrid.flipPhoto ?
          'photoFlippedAccessibleText' : 'photoFlippedBackAccessibleText';
-      this.announceAccessibleMessage_(
-          loadTimeData.getString(flipMessageId));
+      announceAccessibleMessage(loadTimeData.getString(flipMessageId));
     },
 
     /**
@@ -266,7 +267,7 @@ cr.define('login', function() {
      */
     handlePhotoTaken_: function(e) {
       chrome.send('photoTaken', [e.dataURL]);
-      this.announceAccessibleMessage_(
+      announceAccessibleMessage(
           loadTimeData.getString('photoCaptureAccessibleText'));
     },
 
@@ -285,26 +286,8 @@ cr.define('login', function() {
       var imageGrid = $('user-image-grid');
       imageGrid.discardPhoto();
       chrome.send('discardPhoto');
-      this.announceAccessibleMessage_(
+      announceAccessibleMessage(
           loadTimeData.getString('photoDiscardAccessibleText'));
-    },
-
-    /**
-     * Add an accessible message to the page that will be announced to
-     * users who have spoken feedback on, but will be invisible to all
-     * other users. It's removed right away so it doesn't clutter the DOM.
-     */
-    announceAccessibleMessage_: function(msg) {
-      var element = document.createElement('div');
-      element.setAttribute('aria-live', 'polite');
-      element.style.position = 'relative';
-      element.style.left = '-9999px';
-      element.style.height = '0px';
-      element.innerText = msg;
-      document.body.appendChild(element);
-      window.setTimeout(function() {
-        document.body.removeChild(element);
-      }, 0);
     },
 
     /**
