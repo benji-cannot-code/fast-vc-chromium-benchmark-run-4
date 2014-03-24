735f15dffb7b00254d08e86f1764d227a71b65fa
vasilii@chromium.org <vasilii@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-24 21:55:05 +0000

    Minor refactoring in password_store_mac_unittest.cc.
    BUG=
    
    Review URL: https://codereview.chromium.org/209393011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259023 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/password_manager/password_store_mac_unittest.cc
+++ chrome/browser/password_manager/password_store_mac_unittest.cc
@@ -57,9 +57,9 @@ ACTION(QuitUIMessageLoop) {
   base::MessageLoop::current()->Quit();
 }
 
-class MockPasswordStoreMac : public PasswordStoreMac {
+class TestPasswordStoreMac : public PasswordStoreMac {
  public:
-  MockPasswordStoreMac(
+  TestPasswordStoreMac(
       scoped_refptr<base::SingleThreadTaskRunner> main_thread_runner,
       scoped_refptr<base::SingleThreadTaskRunner> db_thread_runner,
       crypto::AppleKeychain* keychain,
@@ -73,9 +73,9 @@ class MockPasswordStoreMac : public PasswordStoreMac {
   using PasswordStoreMac::GetBackgroundTaskRunner;
 
  private:
-  virtual ~MockPasswordStoreMac() {}
+  virtual ~TestPasswordStoreMac() {}
 
-  DISALLOW_COPY_AND_ASSIGN(MockPasswordStoreMac);
+  DISALLOW_COPY_AND_ASSIGN(TestPasswordStoreMac);
 };
 
 }  // namespace
@@ -1060,7 +1060,7 @@ class PasswordStoreMacTest : public testing::Test {
 
     keychain_ = new MockAppleKeychain();
 
-    store_ = new MockPasswordStoreMac(
+    store_ = new TestPasswordStoreMac(
         base::MessageLoopProxy::current(),
         base::MessageLoopProxy::current(),
         keychain_,
@@ -1076,8 +1076,6 @@ class PasswordStoreMacTest : public testing::Test {
   void WaitForStoreUpdate() {
     // Do a store-level query to wait for all the operations above to be done.
     MockPasswordStoreConsumer consumer;
-    ON_CALL(consumer, OnGetPasswordStoreResults(_))
-        .WillByDefault(QuitUIMessageLoop());
     EXPECT_CALL(consumer, OnGetPasswordStoreResults(_))
         .WillOnce(DoAll(WithArg<0>(STLDeleteElements0()), QuitUIMessageLoop()));
     store_->GetLogins(PasswordForm(), PasswordStore::ALLOW_PROMPT, &consumer);
@@ -1090,7 +1088,7 @@ class PasswordStoreMacTest : public testing::Test {
 
   MockAppleKeychain* keychain_;  // Owned by store_.
   LoginDatabase* login_db_;  // Owned by store_.
-  scoped_refptr<MockPasswordStoreMac> store_;
+  scoped_refptr<TestPasswordStoreMac> store_;
   base::ScopedTempDir db_dir_;
 };
 
@@ -1219,8 +1217,6 @@ TEST_F(PasswordStoreMacTest, TestDBKeychainAssociation) {
   m_form.signon_realm = "http://m.facebook.com";
   m_form.origin = GURL("http://m.facebook.com/index.html");
   MockPasswordStoreConsumer consumer;
-  ON_CALL(consumer, OnGetPasswordStoreResults(_))
-      .WillByDefault(QuitUIMessageLoop());
   EXPECT_CALL(consumer, OnGetPasswordStoreResults(_)).WillOnce(DoAll(
       WithArg<0>(Invoke(&consumer, &MockPasswordStoreConsumer::CopyElements)),
       WithArg<0>(STLDeleteElements0()),
