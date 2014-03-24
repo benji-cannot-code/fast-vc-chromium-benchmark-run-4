75e47a13a7bf9f5fdcfa362d8d7b9b8c31cf7b9e
vsevik@chromium.org <vsevik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-24 08:42:50 +0000

    Revert of aura: Remove old GL paths from RenderWidgetHostViewAura. (https://codereview.chromium.org/131443007/)
    Reason for revert:
    This is causing many virtual/threaded/transitions and virtual/threaded/animations tests to time out.
    Some of them are timing out flakily, some are consistent.
    
    Here is what I’ve seen so far:
    
    virtual/threaded/transitions/move-after-transition.html - consistent time outs.
    
    virtual/threaded/transitions/cubic-bezier-overflow-svg-length.html - flaky time outs
    virtual/threaded/transitions/change-duration-during-transition.html - flaky time outs
    virtual/threaded/animations/cross-fade-background-image.html - flaky ImageOnlyFailure, looks like it is related.
    
    Original issue's description:
    > aura: Remove old GL paths from RenderWidgetHostViewAura.
    > 
    > Frames from the renderer are either legacy software,
    > software compositing, or ubercomp. Remove the old GL composited
    > paths that are no longer in use.
    > 
    > For layout tests, put them into composited mailbox mode, and
    > instead of sending the frame to the browser at all, just put the
    > frame in a post task to ack back directly to the renderer.
    > 
    > R=piman
    > BUG=332998
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=249226
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=258742
    
    TBR=piman@chromium.org,fsamuel@chromium.org,phajdan.jr@chromium.org,jamesr@chromium.org,danakj@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=332998
    
    Review URL: https://codereview.chromium.org/209903002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258891 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -214,7 +214,6 @@ const Experiment::Choice kLCDTextChoices[] = {
   { IDS_GENERIC_EXPERIMENT_CHOICE_DISABLED, switches::kDisableLCDText, ""}
 };
 
-#ifndef USE_AURA
 const Experiment::Choice kDelegatedRendererChoices[] = {
   { IDS_GENERIC_EXPERIMENT_CHOICE_DEFAULT, "", "" },
   { IDS_GENERIC_EXPERIMENT_CHOICE_ENABLED,
@@ -222,7 +221,6 @@ const Experiment::Choice kDelegatedRendererChoices[] = {
   { IDS_GENERIC_EXPERIMENT_CHOICE_DISABLED,
     switches::kDisableDelegatedRenderer, ""}
 };
-#endif
 
 const Experiment::Choice kMaxTilesForInterestAreaChoices[] = {
   { IDS_GENERIC_EXPERIMENT_CHOICE_DEFAULT, "", "" },
@@ -458,11 +456,7 @@ const Experiment kExperiments[] = {
     "force-compositing-mode-2",
     IDS_FLAGS_FORCE_COMPOSITING_MODE_NAME,
     IDS_FLAGS_FORCE_COMPOSITING_MODE_DESCRIPTION,
-#if !defined(USE_AURA)
     kOsLinux,
-#else
-    0,
-#endif
     ENABLE_DISABLE_VALUE_TYPE(switches::kForceCompositingMode,
                               switches::kDisableForceCompositingMode)
   },
@@ -470,11 +464,7 @@ const Experiment kExperiments[] = {
     "threaded-compositing-mode",
     IDS_FLAGS_THREADED_COMPOSITING_MODE_NAME,
     IDS_FLAGS_THREADED_COMPOSITING_MODE_DESCRIPTION,
-#if !defined(USE_AURA)
     kOsLinux,
-#else
-    0,
-#endif
     ENABLE_DISABLE_VALUE_TYPE(switches::kEnableThreadedCompositing,
                               switches::kDisableThreadedCompositing)
   },
@@ -1268,15 +1258,16 @@ const Experiment kExperiments[] = {
     kOsDesktop,
     MULTI_VALUE_TYPE(kLCDTextChoices)
   },
-#ifndef USE_AURA
   {
     "delegated-renderer",
     IDS_FLAGS_DELEGATED_RENDERER_NAME,
     IDS_FLAGS_DELEGATED_RENDERER_DESCRIPTION,
-    kOsAndroid,
+#ifdef USE_AURA
+    kOsWin | kOsLinux |
+#endif
+    kOsAndroid | kOsCrOS,
     MULTI_VALUE_TYPE(kDelegatedRendererChoices)
   },
-#endif
   {
     "enable-websocket-experimental-implementation",
     IDS_FLAGS_ENABLE_EXPERIMENTAL_WEBSOCKET_NAME,
