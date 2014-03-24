4b5494d5cefbc2f5c8725bc5b25b3e6a2e3e7795
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-24 22:48:18 +0000

    Temporary hack in order to make panel dragging work on Linux Aura for M-35.
    Some background:
    - We use |DesktopWindowTreeHostX11::bounds_| to convert from window coordinates to screen coordinates.
    - We get mouse events (e.g. MotionNotify) with coordinates wrt to the real window bounds.
    - DesktopWindowTreeHostX11::SetBounds() synchronously changes |DesktopWindowTreeHostX11::bounds_| but does not synchronously change the real window bounds.
    As a result, there is a brief period of time where "real window bounds" != "requested window bounds" while a panel is being dragged. During that period of time the computed mouse event position in screen coordinates is wrong.
    
    BUG=351730
    TEST=Manual, see bug
    
    Review URL: https://codereview.chromium.org/197283008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259047 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/panels/panel_frame_view.cc
+++ chrome/browser/ui/views/panels/panel_frame_view.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/font_list.h"
 #include "ui/gfx/path.h"
+#include "ui/gfx/screen.h"
 #include "ui/views/controls/button/image_button.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/widget/widget.h"
@@ -579,10 +580,18 @@ bool PanelFrameView::OnMousePressed(const ui::MouseEvent& event) {
 }
 
 bool PanelFrameView::OnMouseDragged(const ui::MouseEvent& event) {
+#if defined(OS_LINUX) && !defined(OS_CHROMEOS)
+  // Converting the mouse location to screen coordinates returns an incorrect
+  // location while the panel is moving. See crbug.com/353393 for more details.
+  // TODO(pkotwicz): Fix conversion to screen coordinates
+  gfx::Screen* screen = gfx::Screen::GetNativeScreen();
+  gfx::Point mouse_location = screen->GetCursorScreenPoint();
+#else
   // |event.location| is in the view's coordinate system. Convert it to the
   // screen coordinate system.
   gfx::Point mouse_location = event.location();
   views::View::ConvertPointToScreen(this, &mouse_location);
+#endif
 
   if (panel_view_->OnTitlebarMouseDragged(mouse_location))
     return true;
