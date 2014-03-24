9b84331d543b30c122193b40d2c432633a724e15
kaliamoorthi@chromium.org <kaliamoorthi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-24 22:50:30 +0000

    Refactorise the policy_path_parser framework
    This CL performs a refactorisation of the policy path parser framework.
    
    BUG=352627
    
    Review URL: https://codereview.chromium.org/204463005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259049 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/policy_path_parser.h
+++ chrome/browser/policy/policy_path_parser.h
@@ -8,11 +8,34 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/bind.h"
 #include "base/files/file_path.h"
 
 namespace policy {
 
 namespace path_parser {
+
+namespace internal {
+
+typedef base::Callback<bool(base::FilePath::StringType*)> GetValueCallback;
+
+struct VariableNameAndValueCallback {
+  VariableNameAndValueCallback(const base::FilePath::CharType* name,
+                               const GetValueCallback value_callback);
+  virtual ~VariableNameAndValueCallback();
+
+  const base::FilePath::CharType* name;
+  const GetValueCallback value_callback;
+};
+
+// Different set of variables are supported in each platform (see below). Hence
+// this table is filled in with different elements in each platform.
+extern const VariableNameAndValueCallback kVariableNameAndValueCallbacks[];
+
+// Since the number of elements in the table could vary for each platform, this
+// variable is used to keep track of it.
+extern const int kNoOfVariables;
+}
 
 // This function is used to expand the variables in policy strings that
 // represent paths. The set of supported variables differs between platforms
