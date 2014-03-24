66c7765348c331826cc802fb6013604c45f2642d
spang@chromium.org <spang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-24 21:15:49 +0000

    ozone: evdev: Fix missing EVIOCSCLOCKID with older kernel headers
    TBR=dnicoara
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/209423007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259010 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/events/ozone/evdev/event_factory_evdev.cc
+++ ui/events/ozone/evdev/event_factory_evdev.cc
@@ -21,6 +21,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/ozone/evdev/device_manager_udev.h"
 #endif
 
+#ifndef EVIOCSCLOCKID
+#define EVIOCSCLOCKID  _IOW('E', 0xa0, int)
+#endif
+
 namespace ui {
 
 namespace {
