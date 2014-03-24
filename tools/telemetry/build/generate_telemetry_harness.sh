040fa87e5670bacc41dcb508561bc25fe7524709
stip@chromium.org <stip@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-24 18:49:34 +0000

    Add telemetry harness generation script.
    BUG=349109
    
    Review URL: https://codereview.chromium.org/200483006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258984 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/telemetry/build/generate_telemetry_harness.sh
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/bash
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# This is a script meant to be run by a bot to periodically release new versions
+# of the telemetry harness. It needs to be run from one level above src/ (such
+# as build/).
+
+src/tools/telemetry/find_dependencies \
+  src/tools/perf/run_benchmark \
+  src/tools/perf/run_measurement \
+  src/tools/perf/record_wpr \
+  src/content/test/gpu/run_gpu_test.py \
+  -z $1
