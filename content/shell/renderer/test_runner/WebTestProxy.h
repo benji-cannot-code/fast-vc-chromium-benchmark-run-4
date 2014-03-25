bb037819fec25a8761be36801ceacdcc938dfee7
japhet@chromium.org <japhet@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-25 03:02:42 +0000

    Prepare for per frame did{Start,Stop}Loading calls (part 2)
    http://src.chromium.org/viewvc/chrome?view=revision&revision=256477 did not do all the preparation required to make https://codereview.chromium.org/183793002/ work.
    
    Move TestRunner's didStopLoading functionality to WebFrameTestProxy instead of WebTestProxy.
    
    De-virtualize start, stop and progress change notifications on RenderViewImpl. They no longer need to be virtualized, and on mac there were compile issues related to hidden virtual functions.
    
    Add a counter on RenderViewImpl to track how many frames are in progress. For now, only send start/stop IPCs when the view as a whole starts/stops.
    
    BUG=347643
    
    Review URL: https://codereview.chromium.org/208573006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259122 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/renderer/test_runner/WebTestProxy.h
+++ content/shell/renderer/test_runner/WebTestProxy.h
@@ -305,11 +305,6 @@ public:
         WebTestProxyBase::setStatusText(text);
         Base::setStatusText(text);
     }
-    virtual void didStopLoading(blink::WebFrame* frame)
-    {
-        WebTestProxyBase::didStopLoading();
-        Base::didStopLoading(frame);
-    }
     virtual blink::WebUserMediaClient* userMediaClient()
     {
         return WebTestProxyBase::userMediaClient();
