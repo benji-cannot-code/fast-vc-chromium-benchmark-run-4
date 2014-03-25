45569278f3882266834820428dfa2f6fa6d59347
iancottrell@chromium.org <iancottrell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-25 03:48:44 +0000

    New style goma support, also magical gyp defines
    BUG=353992
    
    Review URL: https://codereview.chromium.org/204733005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259137 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/cr/cr/actions/gyp.py
+++ tools/cr/cr/actions/gyp.py
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """A module to add gyp support to cr."""
 
 import cr
+import os
 
+GYP_DEFINE_PREFIX = 'GYP_DEF_'
 
 class GypPrepareOut(cr.PrepareOut):
   """A prepare action that runs gyp whenever you select an output directory."""
@@ -14,11 +16,21 @@ class GypPrepareOut(cr.PrepareOut):
   ENABLED = cr.Config.From(
       GYP_GENERATORS='ninja',
       GYP_GENERATOR_FLAGS='output_dir={CR_OUT_BASE} config={CR_BUILDTYPE}',
+      GPP_DEF_target_arch='{CR_ENVSETUP_ARCH}',
   )
 
   def Prepare(self):
+    # Collapse GYP_DEFINES from all GYP_DEF prefixes
+    gyp_defines = cr.context.Find('GYP_DEFINES') or ''
+    for key, value in cr.context.exported.items():
+      if key.startswith(GYP_DEFINE_PREFIX):
+        gyp_defines += ' %s=%s' % (key[len(GYP_DEFINE_PREFIX):], value)
+    cr.context['GYP_DEFINES'] = gyp_defines.strip()
+
     if cr.context.verbose >= 1:
       print cr.context.Substitute('Invoking gyp with {GYP_GENERATOR_FLAGS}')
+      print cr.context.Substitute('GYP_DEFINES = {GYP_DEFINES}')
+
     cr.Host.Execute(
         '{CR_SRC}/build/gyp_chromium',
         '--depth={CR_SRC}',
--- tools/cr/cr/base/host.py
+++ tools/cr/cr/base/host.py
@@ -160,7 +160,7 @@ class Host(cr.Plugin, cr.Plugin.Type):
     return result in ['y', 'yes']
 
   @classmethod
-  def SearchPath(cls, name):
+  def SearchPath(cls, name, paths=[]):
     """Searches the PATH for an executable.
 
     Args:
@@ -171,7 +171,9 @@ class Host(cr.Plugin, cr.Plugin.Type):
     result = []
     extensions = ['']
     extensions.extend(os.environ.get('PATHEXT', '').split(os.pathsep))
-    for path in os.environ.get('PATH', '').split(os.pathsep):
+    paths = [cr.context.Substitute(path) for path in paths if path]
+    paths = paths + os.environ.get('PATH', '').split(os.pathsep)
+    for path in paths:
       partial = os.path.join(path, name)
       for extension in extensions:
         filename = partial + extension
