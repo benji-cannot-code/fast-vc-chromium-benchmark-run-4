84e1e298f677a863b6354a25ffe65be19c77d574
dmazzoni@chromium.org <dmazzoni@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-25 02:35:51 +0000

    Only enable speech dispatcher on Linux with an explicit switch.
    Now that the speechSynthesis api is part of the drive-by web, we don't
    want just visiting a malicious site to be able to cause speech-dispatcher
    to crash.
    
    BUG=327295,333003
    
    Review URL: https://codereview.chromium.org/209393004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259109 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -406,6 +406,7 @@ extern const char kPasswordStore[];
 #endif
 
 #if defined(OS_LINUX) && !defined(OS_CHROMEOS)
+extern const char kEnableSpeechDispatcher[];
 extern const char kMigrateDataDirForSxS[];
 #endif
 
