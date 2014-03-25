6c238044c824ea9c6a38841c4232eed19a81faa1
aandrey@chromium.org <aandrey@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-25 08:37:24 +0000

    DevTools: [Frameworks] Bugfix: do StepOut in framework code after StepIn limit is up.
    Bump up the limit from 10 StepIns to 20.
    Add a test on framework code.
    
    BUG=267592
    R=pfeldman, yurys
    
    Review URL: https://codereview.chromium.org/210243002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169921 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/inspector/sources/debugger/resources/framework.js
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// A framework for testing.
+
+var Framework = {};
+
+Framework.safeRun = function(callback, onSuccess, onException, breakOnUncaught)
+{
+    try {
+        callback();
+        if (onSuccess)
+            Framework.safeRun(onSuccess, undefined, onException, breakOnUncaught);
+    } catch (e) {
+        if (onException)
+            Framework.safeRun(onException, undefined, breakOnUncaught ? Framework.breakInFramework : undefined);
+        else if (breakOnUncaught)
+            Framework.breakInFramework();
+    }
+}
+
+Framework.throwFrameworkException = function(msg)
+{
+    throw Error("FrameworkException" + (msg ? ": " + msg : ""));
+}
+
+Framework.breakInFramework = function()
+{
+    debugger;
+}
+
+Framework.empty = function()
+{
+}
+
+Framework.doSomeWork = function()
+{
+    const numberOfSteps = 50;
+    for (var i = 0; i < numberOfSteps; ++i) {
+        if (window["dummy property should not exist!" + i]) // Prevent optimizations.
+            return i;
+        Framework.safeRun(Framework.empty, Framework.empty, Framework.empty, true);
+    }
+}
