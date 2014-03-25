c957c20538044ffde5f23d0c3e6a16e43ef1396f
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-25 16:15:07 +0000

    Get rid of ContextMenuParams::frame_id since on the browser side we know which RFH this is for.
    Although it was called frame_id, it had been changed to be a RenderFrame's routing id. RetargetingDetails still uses the ID. It's combined with a RVH so it's unique even with OOPIF. We will have to later on change FrameNavigationState to stop using RVH.
    
    R=creis@chromium.org
    
    Review URL: https://codereview.chromium.org/207353003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259224 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/web_navigation/web_navigation_api.cc
+++ chrome/browser/extensions/api/web_navigation/web_navigation_api.cc
@@ -165,7 +165,7 @@ void WebNavigationEventRouter::Observe(
 }
 
 void WebNavigationEventRouter::Retargeting(const RetargetingDetails* details) {
-  if (details->source_frame_id == 0)
+  if (details->source_render_frame_id == 0)
     return;
   WebNavigationTabObserver* tab_observer =
       WebNavigationTabObserver::Get(details->source_web_contents);
@@ -179,7 +179,7 @@ void WebNavigationEventRouter::Retargeting(const RetargetingDetails* details) {
       tab_observer->frame_navigation_state();
 
   FrameNavigationState::FrameID frame_id(
-      details->source_frame_id,
+      details->source_render_frame_id,
       details->source_web_contents->GetRenderViewHost());
   if (!frame_navigation_state.CanSendEvents(frame_id))
     return;
@@ -190,7 +190,7 @@ void WebNavigationEventRouter::Retargeting(const RetargetingDetails* details) {
     pending_web_contents_[details->target_web_contents] =
         PendingWebContents(
             details->source_web_contents,
-            details->source_frame_id,
+            details->source_render_frame_id,
             frame_navigation_state.IsMainFrame(frame_id),
             details->target_web_contents,
             details->target_url);
@@ -198,7 +198,7 @@ void WebNavigationEventRouter::Retargeting(const RetargetingDetails* details) {
     helpers::DispatchOnCreatedNavigationTarget(
         details->source_web_contents,
         details->target_web_contents->GetBrowserContext(),
-        details->source_frame_id,
+        details->source_render_frame_id,
         frame_navigation_state.IsMainFrame(frame_id),
         details->target_web_contents,
         details->target_url);
