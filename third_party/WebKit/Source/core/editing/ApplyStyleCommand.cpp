0877d9e11fa9f50992c370df6cfda69e23d0407e
rhodovan.u-szeged@partner.samsung.com <rhodovan.u-szeged@partner.samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-25 15:10:21 +0000

    ASSERTION FAILED: extractedStyle in WebCore::ApplyStyleCommand::removeInlineStyleFromElement
    The last extractedStyle parameter of removeInlineStyleFromElement() is not mandatory and it's set
    to default 0. This way we have to check its existence before the usage.
    
    Backported from my WebKit patch: http://trac.webkit.org/changeset/154439
    
    R=hayato@chromium.org
    BUG=350414
    
    Review URL: https://codereview.chromium.org/190993002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169948 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/ApplyStyleCommand.cpp
+++ third_party/WebKit/Source/core/editing/ApplyStyleCommand.cpp
@@ -893,8 +893,8 @@ bool ApplyStyleCommand::removeInlineStyleFromElement(EditingStyle* style, PassRe
     if (isStyledInlineElementToRemove(element.get())) {
         if (mode == RemoveNone)
             return true;
-        ASSERT(extractedStyle);
-        extractedStyle->mergeInlineStyleOfElement(element.get(), EditingStyle::OverrideValues);
+        if (extractedStyle)
+            extractedStyle->mergeInlineStyleOfElement(element.get(), EditingStyle::OverrideValues);
         removeNodePreservingChildren(element);
         return true;
     }
