0bc93c9a9d8a9a12466394a73049a071cd52bb71
sievers@chromium.org <sievers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-25 02:41:34 +0000

    Android: Handle WindowAndroid being destroyed before WebContents
    This is a band-aid for something that shouldn't happen.
    
    The lifetime of objects should be:
    1) WindowAndroid outlives...
    2) (Browser) Compositor which should outlive
    3) all WebContents and therefore RWHVAs
    
    1) and 2) should clearly be the case from code inspection.
    It looks like we might have cases of 1)+2) getting destroyed, while
    there is still a ContentViewCore (with WindowAndroid backpointer)
    around.
    
    BUG=324341
    NOTRY=True
    
    Review URL: https://codereview.chromium.org/210033006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259112 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_view_android.cc
+++ content/browser/renderer_host/render_widget_host_view_android.cc
@@ -157,7 +157,8 @@ RenderWidgetHostViewAndroid::RenderWidgetHostViewAndroid(
                                     switches::kEnableDelegatedRenderer) &&
                                 !CommandLine::ForCurrentProcess()->HasSwitch(
                                     switches::kDisableDelegatedRenderer)),
-      locks_on_frame_count_(0) {
+      locks_on_frame_count_(0),
+      root_window_destroyed_(false) {
   if (!using_delegated_renderer_) {
     texture_layer_ = cc::TextureLayer::Create(NULL);
     layer_ = texture_layer_;
@@ -1337,8 +1338,14 @@ void RenderWidgetHostViewAndroid::SetContentViewCore(
   RunAckCallbacks();
 
   RemoveLayers();
-  if (content_view_core_ && !using_synchronous_compositor_)
+  // TODO: crbug.com/324341
+  // WindowAndroid and Compositor should outlive all WebContents.
+  // Allowing this here at runtime is a bandaid.
+  DCHECK(!root_window_destroyed_);
+  if (content_view_core_ && !root_window_destroyed_ &&
+      !using_synchronous_compositor_) {
     content_view_core_->GetWindowAndroid()->RemoveObserver(this);
+  }
 
   if (content_view_core != content_view_core_)
     ReleaseLocksOnSurface();
@@ -1354,8 +1361,10 @@ void RenderWidgetHostViewAndroid::SetContentViewCore(
   }
 
   AttachLayers();
-  if (content_view_core_ && !using_synchronous_compositor_)
+  if (content_view_core_ && !root_window_destroyed_ &&
+      !using_synchronous_compositor_) {
     content_view_core_->GetWindowAndroid()->AddObserver(this);
+  }
 }
 
 void RenderWidgetHostViewAndroid::RunAckCallbacks() {
@@ -1372,6 +1381,7 @@ void RenderWidgetHostViewAndroid::OnCompositingDidCommit() {
 void RenderWidgetHostViewAndroid::OnDetachCompositor() {
   DCHECK(content_view_core_);
   DCHECK(!using_synchronous_compositor_);
+  root_window_destroyed_ = true;
   RunAckCallbacks();
 }
 
