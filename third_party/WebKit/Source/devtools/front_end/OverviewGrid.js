4205e65eb2d8a7b0e0619175f7586d89fca0b6b8
alph@chromium.org <alph@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-25 16:21:38 +0000

    DevTools: Fix timeline overview window selection
    The window selection is shown in a left corner when drag was initiated on a marker.
    
    R=pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/211343003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169954 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/OverviewGrid.js
+++ third_party/WebKit/Source/devtools/front_end/OverviewGrid.js
@@ -253,8 +253,8 @@ WebInspector.OverviewGrid.Window.prototype = {
     {
         if (!this._enabled)
             return false;
-        this._offsetLeft = event.pageX - event.offsetX;
-        var position = event.pageX - this._offsetLeft;
+        this._offsetLeft = this._parentElement.totalOffsetLeft();
+        var position = event.x - this._offsetLeft;
         this._overviewWindowSelector = new WebInspector.OverviewGrid.WindowSelector(this._parentElement, position);
         return true;
     },
@@ -264,7 +264,7 @@ WebInspector.OverviewGrid.Window.prototype = {
      */
     _windowSelectorDragging: function(event)
     {
-        this._overviewWindowSelector._updatePosition(event.pageX - this._offsetLeft);
+        this._overviewWindowSelector._updatePosition(event.x - this._offsetLeft);
         event.preventDefault();
     },
 
@@ -273,7 +273,7 @@ WebInspector.OverviewGrid.Window.prototype = {
      */
     _endWindowSelectorDragging: function(event)
     {
-        var window = this._overviewWindowSelector._close(event.pageX - this._offsetLeft);
+        var window = this._overviewWindowSelector._close(event.x - this._offsetLeft);
         delete this._overviewWindowSelector;
         if (window.end === window.start) { // Click, not drag.
             var middle = window.end;
