8cf01104c019c41c28342b4c9fd0d4aed1aa005e
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-25 06:38:47 +0000

    Create new Autofill tables as part of DB migration.
    Currently, migration from a version 20 database to the latest version fails,
    because the migration code assumes that the autofill_dates table exists, but it
    was not present at version 20, and is never created during migration.  This CL
    fixes the migration path to be more careful and more sane.
    
    BUG=none
    TEST=unit_tests
    R=pkasting@chromium.org
    
    Review URL: https://codereview.chromium.org/207643002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259166 0039d316-1c4b-4281-b951-d872f2087c98

--- components/webdata/common/web_database_table.cc
+++ components/webdata/common/web_database_table.cc
@@ -5,14 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/webdata/common/web_database_table.h"
 
-WebDatabaseTable::WebDatabaseTable() : db_(NULL), meta_table_(NULL) {
-}
+WebDatabaseTable::WebDatabaseTable() : db_(NULL), meta_table_(NULL) {}
+WebDatabaseTable::~WebDatabaseTable() {}
 
-WebDatabaseTable::~WebDatabaseTable() {
-}
-
-bool WebDatabaseTable::Init(sql::Connection* db, sql::MetaTable* meta_table) {
+void WebDatabaseTable::Init(sql::Connection* db, sql::MetaTable* meta_table) {
   db_ = db;
   meta_table_ = meta_table;
-  return true;
 }
