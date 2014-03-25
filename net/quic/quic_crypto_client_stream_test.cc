d5e3899e31884b94adc17bd372a368ab4e9ea9ca
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-25 23:25:27 +0000

    Fix valgrind error in QuicCryptoClientStreamTest.
    BUG= 356321
    
    Review URL: https://codereview.chromium.org/204813004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259389 0039d316-1c4b-4281-b951-d872f2087c98

--- net/quic/quic_crypto_client_stream_test.cc
+++ net/quic/quic_crypto_client_stream_test.cc
@@ -17,6 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+using testing::_;
+using testing::AnyNumber;
+
 namespace net {
 namespace test {
 namespace {
@@ -35,6 +38,7 @@ class QuicCryptoClientStreamTest : public ::testing::Test {
     session_->SetCryptoStream(stream_.get());
     session_->config()->SetDefaults();
     crypto_config_.SetDefaults();
+    EXPECT_CALL(*session_, OnProofValid(_)).Times(AnyNumber());
   }
 
   void CompleteCryptoHandshake() {
