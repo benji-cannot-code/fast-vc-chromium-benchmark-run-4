a6890ef3cc7a27f91cafc257cb36d5f269da6a77
rockot@chromium.org <rockot@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-25 15:24:39 +0000

    Move DNS API to //extensions.
    This was an easy target to move and it kills one more //chrome dep
    from //extensions.
    
    BUG=355752
    TBR=rpaquay@chromium.org FYI for mechanical sockets changes
    
    Review URL: https://codereview.chromium.org/209473012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259199 0039d316-1c4b-4281-b951-d872f2087c98

--- extensions/browser/api/dns/dns_api.h
+++ extensions/browser/api/dns/dns_api.h
@@ -1,20 +1,21 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_EXTENSIONS_API_DNS_DNS_API_H_
-#define CHROME_BROWSER_EXTENSIONS_API_DNS_DNS_API_H_
+#ifndef EXTENSIONS_BROWSER_API_DNS_DNS_API_H_
+#define EXTENSIONS_BROWSER_API_DNS_DNS_API_H_
 
 #include <string>
 
-#include "chrome/browser/io_thread.h"
 #include "extensions/browser/extension_function.h"
 #include "net/base/address_list.h"
 #include "net/base/completion_callback.h"
 #include "net/dns/host_resolver.h"
 
-class IOThread;
+namespace content {
+class ResourceContext;
+}
 
 namespace extensions {
 
@@ -38,12 +39,10 @@ class DnsResolveFunction : public AsyncExtensionFunction {
 
   std::string hostname_;
 
-  bool response_;  // The value sent in SendResponse().
+  // Not owned.
+  content::ResourceContext* resource_context_;
 
-  // This instance is widely available through BrowserProcess, but we need to
-  // acquire it on the UI thread and then use it on the IO thread, so we keep a
-  // plain pointer to it here as we move from thread to thread.
-  IOThread* io_thread_;
+  bool response_;  // The value sent in SendResponse().
 
   scoped_ptr<net::HostResolver::RequestHandle> request_handle_;
   scoped_ptr<net::AddressList> addresses_;
@@ -51,4 +50,4 @@ class DnsResolveFunction : public AsyncExtensionFunction {
 
 }  // namespace extensions
 
-#endif  // CHROME_BROWSER_EXTENSIONS_API_DNS_DNS_API_H_
+#endif  // EXTENSIONS_BROWSER_API_DNS_DNS_API_H_
