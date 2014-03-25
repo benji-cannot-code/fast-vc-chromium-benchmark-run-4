a64c14804fdec8cecb0fa016dbfc34df32cc52fd
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-25 01:04:19 +0000

    [VideoPlayer] dedicated video player app
    This commit adds a dedicated video player app. This is almost copy of the embedded video player app in Files.app. The embedded video player app in Files.app will be removed in the separated patch.
    
    BUG=271811
    TEST=manually tested
    
    R=asargent@chromium.org, hirono@chromium.org
    TBR=sky@chromium.org, mkosiba@chromium.org
    # TBRing for adding a resource to browser_resources.grd.
    # TBRing for adding a combined script to the whitelist.
    
    Review URL: https://codereview.chromium.org/196383030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259070 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/component_loader.cc
+++ chrome/browser/extensions/component_loader.cc
@@ -288,6 +288,11 @@ void ComponentLoader::AddFileManagerExtension() {
 #endif  // defined(OS_CHROMEOS)
 }
 
+void ComponentLoader::AddVideoPlayerExtension() {
+  Add(IDR_VIDEOPLAYER_MANIFEST,
+      base::FilePath(FILE_PATH_LITERAL("video_player")));
+}
+
 void ComponentLoader::AddHangoutServicesExtension() {
 #if defined(GOOGLE_CHROME_BUILD) || defined(ENABLE_HANGOUT_SERVICES_EXTENSION)
   Add(IDR_HANGOUT_SERVICES_MANIFEST,
@@ -437,6 +442,7 @@ void ComponentLoader::AddDefaultComponentExtensionsForKioskMode(
 
   // Component extensions needed for kiosk apps.
   AddFileManagerExtension();
+  AddVideoPlayerExtension();
 
   // Add virtual keyboard.
   AddKeyboardApp();
@@ -467,6 +473,7 @@ void ComponentLoader::AddDefaultComponentExtensionsWithBackgroundPages(
 
   if (!skip_session_components) {
     AddFileManagerExtension();
+    AddVideoPlayerExtension();
     AddHangoutServicesExtension();
     AddHotwordHelperExtension();
     AddImageLoaderExtension();
---
+++ chrome/browser/resources/video_player/js/video_player_scripts.js
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// The include directives are put into Javascript-style comments to prevent
+// parsing errors in non-flattened mode. The flattener still sees them.
+// Note that this makes the flattener to comment out the first line of the
+// included file but that's all right since any javascript file should start
+// with a copyright comment anyway.
+
+//<include src="../../../../../ui/webui/resources/js/cr.js"/>
+//<include src="../../../../../ui/webui/resources/js/load_time_data.js"/>
+
+(function() {
+'strict mode';
+
+//<include src="../../file_manager/common/js/util.js"/>
+//<include src="../../file_manager/foreground/js/media/media_controls.js"/>
+//<include src="../../file_manager/foreground/js/media/util.js"/>
+
+//<include src="video_player.js"/>
+
+window.unload = unload;
+
+})();
