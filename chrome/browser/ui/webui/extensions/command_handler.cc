01c51c3f6b12ee5fba701d97a3285e8ca3e926ad
zhchbin@gmail.com <zhchbin@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-25 18:42:22 +0000

    Suspend shortcut handling while setting commands.
    BUG=329901
    TEST=0. Run chrome with --global-commands=1
         1. Go to chrome://extensions -> Keyboard shortcuts
         2. When overriding a command with a global commands, the original command
            should not fire while changing the shortcut.
    
    Review URL: https://codereview.chromium.org/206293004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259268 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/extensions/command_handler.cc
+++ chrome/browser/ui/webui/extensions/command_handler.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/extensions/api/commands/command_service.h"
+#include "chrome/browser/extensions/extension_commands_global_registry.h"
 #include "chrome/browser/extensions/extension_keybinding_registry.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/profiles/profile.h"
@@ -125,8 +126,13 @@ void CommandHandler::HandleSetCommandScope(
 void CommandHandler::HandleSetShortcutHandlingSuspended(
     const base::ListValue* args) {
   bool suspended;
-  if (args->GetBoolean(0, &suspended))
+  if (args->GetBoolean(0, &suspended)) {
+    // Suspend/Resume normal shortcut handling.
     ExtensionKeybindingRegistry::SetShortcutHandlingSuspended(suspended);
+
+    // Suspend/Resume global shortcut handling.
+    ExtensionCommandsGlobalRegistry::SetShortcutHandlingSuspended(suspended);
+  }
 }
 
 void CommandHandler::GetAllCommands(base::DictionaryValue* commands) {
