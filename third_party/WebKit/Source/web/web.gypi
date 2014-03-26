d30ae8f7a72a26f0d21066f54546fc22344deae8
scottmg@chromium.org <scottmg@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-26 17:55:45 +0000

    Usage of DirectWrite now determined browser-side
    Enabling DirectWrite requires the sandbox to have knowledge of whether
    DirectWrite or GDI is used for font rendering, so it's now decided
    in the browser. Don't do fallback behavior in the renderer any more.
    
    Browser side is https://codereview.chromium.org/209163002/ .
    
    R=jamesr@chromium.org
    BUG=333029
    
    Review URL: https://codereview.chromium.org/210243004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170075 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/web.gypi
+++ third_party/WebKit/Source/web/web.gypi
@@ -260,6 +260,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'painting/ContinuousPainter.h',
       'painting/PaintAggregator.cpp',
       'painting/PaintAggregator.h',
+      'win/WebFontRendering.cpp',
     ],
     'web_unittest_files': [
       'tests/AssociatedURLLoaderTest.cpp',
---
+++ third_party/WebKit/public/web/win/WebFontRendering.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 Google Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WebFontRendering_h
+#define WebFontRendering_h
+
+#include "public/platform/WebCommon.h"
+
+namespace blink {
+
+class WebFontRendering {
+public:
+    BLINK_EXPORT static void setUseDirectWrite(bool);
+    BLINK_EXPORT static void setUseSubpixelPositioning(bool);
+};
+
+} // namespace blink
+
+#endif
