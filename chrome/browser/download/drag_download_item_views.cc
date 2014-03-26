effd18992547aefda2661bc1704e813865d63424
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-26 18:20:46 +0000

    Enable dragging out apk files from the download bar
    This CL:
    - Enables dragging out files for which loading an icon via IconManager::LoadIcon() failed out of the download bar
    - Makes Gtk2UI::GetIconForContentType() attempt to look up the icon for the "unknown content type" if the lookup for the "requested content type" fails. This is consistent with the behavior of base::nix::GetMimeIcon()
    
    BUG=352622
    TEST=Manual, see comment #2 in the bug
    
    Review URL: https://codereview.chromium.org/199633013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259633 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/drag_download_item_views.cc
+++ chrome/browser/download/drag_download_item_views.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/dragdrop/drag_utils.h"
 #include "ui/base/dragdrop/os_exchange_data.h"
 #include "ui/gfx/image/image.h"
+#include "ui/gfx/image/image_skia.h"
 #include "ui/gfx/point.h"
 #include "ui/gfx/screen.h"
 #include "ui/views/widget/widget.h"
@@ -39,10 +40,10 @@ void DragDownloadItem(const content::DownloadItem* download,
   // Set up our OLE machinery
   ui::OSExchangeData data;
 
-  if (icon) {
-    drag_utils::CreateDragImageForFile(
-        download->GetFileNameToReportUser(), icon->ToImageSkia(), &data);
-  }
+  drag_utils::CreateDragImageForFile(
+      download->GetFileNameToReportUser(),
+      icon ? icon->AsImageSkia() : gfx::ImageSkia(),
+      &data);
 
   base::FilePath full_path = download->GetTargetFilePath();
 #if defined(OS_CHROMEOS)
