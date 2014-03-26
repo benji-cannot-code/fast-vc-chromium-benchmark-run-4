035145768ef0bf73edcc1fef91d5812b5948a515
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-26 20:03:22 +0000

    Revert "aura: The fallback aura tooltip should left align its label text."
    This reverts r354513. It breaks text alignment in single line tooltips.
    
    BUG=348194, 356028, 354739
    
    Review URL: https://codereview.chromium.org/211963002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259669 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/corewm/tooltip_aura.cc
+++ ui/views/corewm/tooltip_aura.cc
@@ -65,7 +65,6 @@ TooltipAura::TooltipAura(gfx::ScreenType screen_type)
       views::Background::CreateSolidBackground(kTooltipBackground));
   label_.set_owned_by_client();
   label_.SetMultiLine(true);
-  label_.SetHorizontalAlignment(gfx::ALIGN_LEFT);
 }
 
 TooltipAura::~TooltipAura() {
