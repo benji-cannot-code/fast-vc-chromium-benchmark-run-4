bfb3a6c51d6f0e9adb25baec0bda121731812fe8
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-26 22:24:37 +0000

    Standardize error reporting of IPEndPoint::ToSockAddr and IPEndPoint::FromSockAddr.
    R=eroman@chromium.org
    BUG=247210
    TEST=none
    
    Review URL: https://codereview.chromium.org/185553013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259719 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/ssl_client_socket_nss.cc
+++ net/socket/ssl_client_socket_nss.cc
@@ -3265,7 +3265,7 @@ int SSLClientSocketNSS::InitializeSSLPeerName() {
 
   SockaddrStorage storage;
   if (!peer_address.ToSockAddr(storage.addr, &storage.addr_len))
-    return ERR_UNEXPECTED;
+    return ERR_ADDRESS_INVALID;
 
   PRNetAddr peername;
   memset(&peername, 0, sizeof(peername));
