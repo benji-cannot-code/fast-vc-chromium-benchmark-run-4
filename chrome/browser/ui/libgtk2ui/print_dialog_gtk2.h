abf6e1b295a96c7ee2833cbb05d36f03008568a2
erikchen@chromium.org <erikchen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-26 00:59:19 +0000

    Revert of Makes PrintDialogGtk2 modal. (https://codereview.chromium.org/210023009/)
    Reason for revert:
    This CL causes a test failure.
    
    http://build.chromium.org/p/chromium.linux/builders/Linux%20Tests%20%28dbg%29%281%29/builds/31374/steps/browser_tests/logs/stdio
    
    Original issue's description:
    > Makes PrintDialogGtk2 modal.
    > 
    > Moves the transient parent utilities to gtk2_util and makes both
    > dialogs use it.
    > 
    > BUG=334331
    > R=erg@chromium.org
    > TEST=manually
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=259318
    
    TBR=erg@chromium.org,mukai@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=334331
    
    Review URL: https://codereview.chromium.org/211433007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259428 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/libgtk2ui/print_dialog_gtk2.h
+++ chrome/browser/ui/libgtk2ui/print_dialog_gtk2.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "printing/print_dialog_gtk_interface.h"
 #include "printing/printing_context_linux.h"
-#include "ui/aura/window_observer.h"
 
 namespace printing {
 class Metafile;
@@ -30,8 +29,7 @@ using printing::PrintingContextLinux;
 class PrintDialogGtk2
     : public printing::PrintDialogGtkInterface,
       public base::RefCountedThreadSafe<
-          PrintDialogGtk2, content::BrowserThread::DeleteOnUIThread>,
-      public aura::WindowObserver {
+          PrintDialogGtk2, content::BrowserThread::DeleteOnUIThread> {
  public:
   // Creates and returns a print dialog.
   static printing::PrintDialogGtkInterface* CreatePrintDialog(
@@ -72,9 +70,6 @@ class PrintDialogGtk2
   // Helper function for initializing |context_|'s PrintSettings with a given
   // |settings|.
   void InitPrintSettings(printing::PrintSettings* settings);
-
-  // aura::WindowObserver implementation.
-  virtual void OnWindowDestroying(aura::Window* window) OVERRIDE;
 
   // Printing dialog callback.
   PrintingContextLinux::PrintSettingsCallback callback_;
