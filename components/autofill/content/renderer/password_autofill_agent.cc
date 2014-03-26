d0c2827e955cd40749684ff1078ba75cd869d5c8
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-26 22:05:45 +0000

    Remove usage of WebFrame::uniqueName when using WebView::findFrameByName
    The implementation of findFrameByName was changed to use WebFrame::assignedName
    in http://src.chromium.org/viewvc/blink?revision=169764&view=revision.
    
    In some cases, usage of findFrameByName() can be replaced by traversing the
    frame manually, and the code will be more reliable in general.
    
    BUG=356061
    NOTRY=true
    R=boliu@chromium.org, estade@chromium.org, vitalybuka@chromium.org
    
    Review URL: https://codereview.chromium.org/213333002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259712 0039d316-1c4b-4281-b951-d872f2087c98

--- components/autofill/content/renderer/password_autofill_agent.cc
+++ components/autofill/content/renderer/password_autofill_agent.cc
@@ -543,7 +543,7 @@ blink::WebFrame* PasswordAutofillAgent::CurrentOrChildFrameWithSavedForms(
     // keep just the first frame found, it might be a good idea to add a UMA
     // statistic or a similar check on how many frames are here to choose from.
     if (current_frame == form_frame ||
-        current_frame->findChildByName(form_frame->uniqueName())) {
+        current_frame->findChildByName(form_frame->assignedName())) {
       return form_frame;
     }
   }
