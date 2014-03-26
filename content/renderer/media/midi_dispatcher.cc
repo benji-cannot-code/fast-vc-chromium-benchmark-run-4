a989cdaa402614ccaea3d60402a05e3896000457
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-26 12:51:03 +0000

    Web MIDI API renaming: the second step of three step changes
    To update Web MIDI API spec, we decided to rename functions
    having SysEx to sysex. This affects blink API and needs update
    Chromium side embedder implementations.
    
    Here are a series of changes on this renaming.
    1. https://codereview.chromium.org/208243014/
    2. this change
    3. https://codereview.chromium.org/208423016/
    
    BUG=349538
    TEST=LayoutTests/webmidi
    
    Review URL: https://codereview.chromium.org/210003002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259560 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/midi_dispatcher.cc
+++ content/renderer/media/midi_dispatcher.cc
@@ -33,7 +33,7 @@ bool MidiDispatcher::OnMessageReceived(const IPC::Message& message) {
   return handled;
 }
 
-void MidiDispatcher::requestSysExPermission(
+void MidiDispatcher::requestSysexPermission(
       const WebMIDIPermissionRequest& request) {
   int bridge_id = requests_.Add(new WebMIDIPermissionRequest(request));
   WebSecurityOrigin security_origin = request.securityOrigin();
@@ -42,7 +42,7 @@ void MidiDispatcher::requestSysExPermission(
   Send(new MidiHostMsg_RequestSysExPermission(routing_id(), bridge_id, url));
 }
 
-void MidiDispatcher::cancelSysExPermissionRequest(
+void MidiDispatcher::cancelSysexPermissionRequest(
     const WebMIDIPermissionRequest& request) {
   for (IDMap<WebMIDIPermissionRequest>::iterator it(&requests_);
        !it.IsAtEnd();
