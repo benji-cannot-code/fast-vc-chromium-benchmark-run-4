cb608882150f5cc91fc49587b87528927077f6fe
antrim@chromium.org <antrim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-26 18:51:35 +0000

    Use actual implementation of RemoveKeyEx in supervised users password change flow.
    R=nkostylev@chromium.org
    
    BUG=244472
    
    Review URL: https://codereview.chromium.org/210263002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259645 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/dbus/fake_cryptohome_client.cc
+++ chromeos/dbus/fake_cryptohome_client.cc
@@ -421,6 +421,14 @@ void FakeCryptohomeClient::AddKeyEx(
   ReturnProtobufMethodCallback(id.email(), callback);
 }
 
+void FakeCryptohomeClient::RemoveKeyEx(
+    const cryptohome::AccountIdentifier& id,
+    const cryptohome::AuthorizationRequest& auth,
+    const cryptohome::RemoveKeyRequest& request,
+    const ProtobufMethodCallback& callback) {
+  ReturnProtobufMethodCallback(id.email(), callback);
+}
+
 void FakeCryptohomeClient::UpdateKeyEx(
     const cryptohome::AccountIdentifier& id,
     const cryptohome::AuthorizationRequest& auth,
