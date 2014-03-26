b9cafb25f3b60706e9d6d1936078e82e6fa35be5
eroman@chromium.org <eroman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-26 05:32:06 +0000

    [refactor] Split importKey.html into smaller files, and delete algoritm-specific tests.
     * importKey-badParameters.html
     * importKey-normalize-usages.html
    
    The reason for deleting the successful key import tests, is that there are much more exhaustive key import tests as part of the clone-* layout tests (which import a large combination of keys in order to serialize them).
    
    Lastly this change fixes a typo in an error message "HmacImporParams" --> "HmacImportParams".
    
    BUG=245025
    
    Review URL: https://codereview.chromium.org/211933003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170019 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/crypto/NormalizeAlgorithm.cpp
+++ third_party/WebKit/Source/modules/crypto/NormalizeAlgorithm.cpp
@@ -624,7 +624,7 @@ bool parseAlgorithmParams(const Dictionary& raw, blink::WebCryptoAlgorithmParams
         context.add("AesKeyGenParams");
         return parseAesKeyGenParams(raw, params, context, errorDetails);
     case blink::WebCryptoAlgorithmParamsTypeHmacImportParams:
-        context.add("HmacImporParams");
+        context.add("HmacImportParams");
         return parseHmacImportParams(raw, params, context, errorDetails);
     case blink::WebCryptoAlgorithmParamsTypeHmacKeyGenParams:
         context.add("HmacKeyGenParams");
