4a7542db8cf58209273f932ca5ac5f3af655c957
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-26 22:34:38 +0000

    cc: Apply occlusion before creating quads in DelegatedRendererLayerImpl.
    This makes the AppendQuads method query occlusion directly and subtract
    it from the quads it would create before they are created, skipping
    quads that are completely occluded and avoiding a malloc/free for them.
    
    R=enne@chromium.org
    BUG=344962
    
    Review URL: https://codereview.chromium.org/213373002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259725 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/test/mock_quad_culler.cc
+++ cc/test/mock_quad_culler.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "cc/test/mock_quad_culler.h"
 
+#include "cc/base/math_util.h"
 #include "cc/quads/draw_quad.h"
 
 namespace cc {
@@ -31,19 +32,32 @@ SharedQuadState* MockQuadCuller::UseSharedQuadState(
 gfx::Rect MockQuadCuller::UnoccludedContentRect(
     const gfx::Rect& content_rect,
     const gfx::Transform& draw_transform) {
-  DCHECK(draw_transform.IsIdentity() || occluded_content_rect_.IsEmpty());
-  gfx::Rect result = content_rect;
-  result.Subtract(occluded_content_rect_);
+  DCHECK(draw_transform.IsIdentityOrIntegerTranslation() ||
+         occluded_target_rect_.IsEmpty());
+  gfx::Rect target_rect =
+      MathUtil::MapEnclosingClippedRect(draw_transform, content_rect);
+  target_rect.Subtract(occluded_target_rect_);
+  gfx::Transform inverse_draw_transform(gfx::Transform::kSkipInitialization);
+  if (!draw_transform.GetInverse(&inverse_draw_transform))
+    NOTREACHED();
+  gfx::Rect result = MathUtil::ProjectEnclosingClippedRect(
+      inverse_draw_transform, target_rect);
   return result;
 }
 
 gfx::Rect MockQuadCuller::UnoccludedContributingSurfaceContentRect(
     const gfx::Rect& content_rect,
     const gfx::Transform& draw_transform) {
-  DCHECK(draw_transform.IsIdentity() ||
-         occluded_content_rect_for_contributing_surface_.IsEmpty());
-  gfx::Rect result = content_rect;
-  result.Subtract(occluded_content_rect_for_contributing_surface_);
+  DCHECK(draw_transform.IsIdentityOrIntegerTranslation() ||
+         occluded_target_rect_for_contributing_surface_.IsEmpty());
+  gfx::Rect target_rect =
+      MathUtil::MapEnclosingClippedRect(draw_transform, content_rect);
+  target_rect.Subtract(occluded_target_rect_for_contributing_surface_);
+  gfx::Transform inverse_draw_transform(gfx::Transform::kSkipInitialization);
+  if (!draw_transform.GetInverse(&inverse_draw_transform))
+    NOTREACHED();
+  gfx::Rect result = MathUtil::ProjectEnclosingClippedRect(
+      inverse_draw_transform, target_rect);
   return result;
 }
 
