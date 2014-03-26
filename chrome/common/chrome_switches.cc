980baf243c599b2f3abd2850b7dd19dd4282e7cd
ksimbili@google.com <ksimbili@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-26 04:19:30 +0000

    Add user toggleable flag for instant-search-clicks.
    Review URL: https://codereview.chromium.org/192293006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259473 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_switches.cc
+++ chrome/common/chrome_switches.cc
@@ -1432,6 +1432,9 @@ const char kEnableZeroSuggestMostVisited[] =
 const char kEnableZeroSuggestPersonalized[] =
     "enable-zero-suggest-personalized";
 
+// Enables instant search clicks feature.
+const char kEnableInstantSearchClicks[] = "enable-instant-search-clicks";
+
 #endif
 
 #if defined(USE_ASH)
