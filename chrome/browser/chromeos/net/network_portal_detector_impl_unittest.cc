c099f3179000ea8e7866dc9299d7d376f65ad4a9
ygorshenin@chromium.org <ygorshenin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-26 12:56:44 +0000

    Captive portal UMA metrics are split to in-session and OOBE metrics.
    BUG=231758
    TEST=unit_tests:NetworkPortalDetector*, browser_tests:NetworkPortalDetector*
    
    Review URL: https://codereview.chromium.org/210183002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259563 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/net/network_portal_detector_impl_unittest.cc
+++ chrome/browser/chromeos/net/network_portal_detector_impl_unittest.cc
@@ -87,7 +87,7 @@ class NetworkPortalDetectorImplTest
 
     if (base::HistogramBase* histogram =
             base::StatisticsRecorder::FindHistogram(
-                NetworkPortalDetectorImpl::kDetectionResultHistogram)) {
+                "CaptivePortal.OOBE.DetectionResult")) {
       original_samples_.reset(histogram->SnapshotSamples().release());
     }
   }
@@ -142,9 +142,7 @@ class NetworkPortalDetectorImplTest
     network_portal_detector()->OnErrorScreenHide();
   }
 
-  void stop_detection() {
-    network_portal_detector()->StopDetection();
-  }
+  void stop_detection() { network_portal_detector()->StopDetection(); }
 
   bool attempt_timeout_is_cancelled() {
     return network_portal_detector()->AttemptTimeoutIsCancelledForTesting();
@@ -226,10 +224,10 @@ class NetworkPortalDetectorImplTest
 
   scoped_ptr<EnumHistogramChecker> MakeResultHistogramChecker() {
     return scoped_ptr<EnumHistogramChecker>(
-        new EnumHistogramChecker(
-            NetworkPortalDetectorImpl::kDetectionResultHistogram,
-            NetworkPortalDetector::CAPTIVE_PORTAL_STATUS_COUNT,
-            original_samples_.get())).Pass();
+               new EnumHistogramChecker(
+                   "CaptivePortal.OOBE.DetectionResult",
+                   NetworkPortalDetector::CAPTIVE_PORTAL_STATUS_COUNT,
+                   original_samples_.get()));
   }
 
  private:
