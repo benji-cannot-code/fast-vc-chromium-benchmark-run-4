b131bc03d60a5131e1a460213b0f52f841328630
morrita@chromium.org <morrita@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-26 17:45:44 +0000

    [Refactring] Turn HTMLImport::m_sync from bool to an enum.
    This change introduces HTMLImport::SyncMode for readability.
    
    TEST=none
    BUG=356257
    R=dominicc@chromium.org
    
    Review URL: https://codereview.chromium.org/211363004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170067 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/imports/HTMLImportChild.cpp
+++ third_party/WebKit/Source/core/html/imports/HTMLImportChild.cpp
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-HTMLImportChild::HTMLImportChild(Document& master, const KURL& url, bool sync)
+HTMLImportChild::HTMLImportChild(Document& master, const KURL& url, SyncMode sync)
     : HTMLImport(sync)
     , m_master(master)
     , m_url(url)
