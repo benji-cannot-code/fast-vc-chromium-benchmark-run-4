c9500dab5a4bcad9c269111c1557645fecb38e76
bokan@chromium.org <bokan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-26 23:57:36 +0000

    Made PinchViewport a ScrollableArea.
    Implemented ScrollableArea on PinchViewport and hooked it up
    to all the layer scrolling mechanisms. This removes the need
    for DistributeScrollOffsetToViewports on the layer tree.
    
    Also did a little cleanup with member names and namespacing.
    
    This is a 2-sided patch, the other CL:
    https://codereview.chromium.org/210543002
    
    BUG=349941
    
    Review URL: https://codereview.chromium.org/199253002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170110 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/scroll/ScrollView.h
+++ third_party/WebKit/Source/platform/scroll/ScrollView.h
@@ -322,8 +322,6 @@ private:
 
     void calculateOverhangAreasForPainting(IntRect& horizontalOverhangRect, IntRect& verticalOverhangRect);
     void updateOverhangAreas();
-
-    virtual int pageStep(ScrollbarOrientation) const OVERRIDE;
 }; // class ScrollView
 
 DEFINE_TYPE_CASTS(ScrollView, Widget, widget, widget->isScrollView(), widget.isScrollView());
