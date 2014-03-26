08e3f487bed3a2728182d9e5add0ca43af1ebb19
yfriedman@chromium.org <yfriedman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-26 17:02:29 +0000

    Mark ScreenOrientationProviderTest.testLandscape as flaky
    BUG=353500
    TBR=mlamouri
    
    Review URL: https://codereview.chromium.org/213193004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259609 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/javatests/src/org/chromium/content/browser/ScreenOrientationProviderTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/ScreenOrientationProviderTest.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.content.browser;
 
 import android.os.Build;
+import android.test.FlakyTest;
 import android.test.suitebuilder.annotation.MediumTest;
 import android.test.suitebuilder.annotation.SmallTest;
 
@@ -149,7 +150,9 @@ public class ScreenOrientationProviderTest extends ContentShellTestBase {
                 ScreenOrientationValues.PORTRAIT_SECONDARY));
     }
 
-    @MediumTest
+    // @MediumTest
+    // crbug.com/353500
+    @FlakyTest
     @Feature({"ScreenOrientation"})
     public void testLandscape() throws Exception {
         int initialOrientation = mObserver.mOrientation;
