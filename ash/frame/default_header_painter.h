271f0384d3645b2981989d79aab257cfbbdc6ffe
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-26 22:45:30 +0000

    Use the opaque header for inactive apps when maximized.
    BUG=350007
    TEST=Manual, see bug
    
    Review URL: https://codereview.chromium.org/212893006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259730 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/frame/default_header_painter.h
+++ ash/frame/default_header_painter.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/animation/animation_delegate.h"
 
 namespace gfx {
+class ImageSkia;
 class Rect;
 class SlideAnimation;
 }
@@ -73,6 +74,12 @@ class ASH_EXPORT DefaultHeaderPainter : public HeaderPainter,
 
   // Returns the bounds for the title.
   gfx::Rect GetTitleBounds() const;
+
+  // Returns the frame image to use when |frame_| is active.
+  gfx::ImageSkia* GetActiveFrameImage() const;
+
+  // Returns the frame image to use when |frame_| is inactive.
+  gfx::ImageSkia* GetInactiveFrameImage() const;
 
   views::Widget* frame_;
   views::View* view_;
