e1b679c991ff6167add9d735da0d0da8936e24f7
sohan.jyoti@samsung.com <sohan.jyoti@samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-26 09:52:03 +0000

    Add GL_TEXTURE_EXTERNAL_OES as supported texture target for CHROMIUM_map_image.
    This adds GL_TEXTURE_EXTERNAL_OES support for
    SharedMemory backed GLImage.
    We use an egl image for binding in GLImageShm, and
    also update GLImageEGL to use an egl image for its 
    "release after use" fallback (required for gpu workaround)
    
    BUG=322780
    
    Review URL: https://codereview.chromium.org/198703002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259538 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gl/gl_image_egl.cc
+++ ui/gl/gl_image_egl.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/gl/gl_bindings.h"
 #include "ui/gl/gl_surface_egl.h"
+#include "ui/gl/scoped_binders.h"
 
 namespace gfx {
 
@@ -15,8 +16,9 @@ GLImageEGL::GLImageEGL(gfx::Size size)
       size_(size),
       release_after_use_(false),
       in_use_(false),
-      target_(0) {
-}
+      target_(0),
+      egl_image_for_unbind_(EGL_NO_IMAGE_KHR),
+      texture_id_for_unbind_(0) {}
 
 GLImageEGL::~GLImageEGL() {
   Destroy();
@@ -45,18 +47,21 @@ bool GLImageEGL::Initialize(gfx::GpuMemoryBufferHandle buffer) {
 }
 
 void GLImageEGL::Destroy() {
-  if (egl_image_ == EGL_NO_IMAGE_KHR)
-    return;
-
-  EGLBoolean success = eglDestroyImageKHR(
-      GLSurfaceEGL::GetHardwareDisplay(), egl_image_);
-
-  if (success == EGL_FALSE) {
-    EGLint error = eglGetError();
-    LOG(ERROR) << "Error destroying EGLImage: " << error;
+  if (egl_image_ != EGL_NO_IMAGE_KHR) {
+    eglDestroyImageKHR(GLSurfaceEGL::GetHardwareDisplay(), egl_image_);
+    egl_image_ = EGL_NO_IMAGE_KHR;
   }
 
-  egl_image_ = EGL_NO_IMAGE_KHR;
+  if (egl_image_for_unbind_ != EGL_NO_IMAGE_KHR) {
+    eglDestroyImageKHR(GLSurfaceEGL::GetHardwareDisplay(),
+                       egl_image_for_unbind_);
+    egl_image_for_unbind_ = EGL_NO_IMAGE_KHR;
+  }
+
+  if (texture_id_for_unbind_) {
+    glDeleteTextures(1, &texture_id_for_unbind_);
+    texture_id_for_unbind_ = 0;
+  }
 }
 
 gfx::Size GLImageEGL::GetSize() {
@@ -109,16 +114,37 @@ void GLImageEGL::DidUseTexImage() {
   if (!release_after_use_)
     return;
 
-  char zero[4] = { 0, };
-  glTexImage2D(target_,
-               0,
-               GL_RGBA,
-               1,
-               1,
-               0,
-               GL_RGBA,
-               GL_UNSIGNED_BYTE,
-               &zero);
+  if (egl_image_for_unbind_ == EGL_NO_IMAGE_KHR) {
+    DCHECK_EQ(0u, texture_id_for_unbind_);
+    glGenTextures(1, &texture_id_for_unbind_);
+
+    {
+      ScopedTextureBinder texture_binder(GL_TEXTURE_2D, texture_id_for_unbind_);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+      char zero[4] = {0, };
+      glTexImage2D(
+          GL_TEXTURE_2D, 0, GL_RGBA, 1, 1, 0, GL_RGBA, GL_UNSIGNED_BYTE, &zero);
+    }
+
+    EGLint attrs[] = {EGL_GL_TEXTURE_LEVEL_KHR, 0,  // mip-level.
+                      EGL_IMAGE_PRESERVED_KHR, EGL_TRUE, EGL_NONE};
+    // Need to pass current EGL rendering context to eglCreateImageKHR for
+    // target type EGL_GL_TEXTURE_2D_KHR.
+    egl_image_for_unbind_ = eglCreateImageKHR(
+        GLSurfaceEGL::GetHardwareDisplay(),
+        eglGetCurrentContext(),
+        EGL_GL_TEXTURE_2D_KHR,
+        reinterpret_cast<EGLClientBuffer>(texture_id_for_unbind_),
+        attrs);
+    DCHECK_NE(EGL_NO_IMAGE_KHR, egl_image_for_unbind_)
+        << "Error creating EGLImage: " << eglGetError();
+  }
+
+  glEGLImageTargetTexture2DOES(target_, egl_image_for_unbind_);
+  DCHECK_EQ(static_cast<GLenum>(GL_NO_ERROR), glGetError());
 }
 
 void GLImageEGL::WillModifyTexImage() {
--- ui/gl/gl_image_shm.h
+++ ui/gl/gl_image_shm.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define UI_GL_GL_IMAGE_SHM_H_
 
 #include "base/memory/scoped_ptr.h"
+#include "ui/gl/gl_bindings.h"
 #include "ui/gl/gl_image.h"
 
 namespace gfx {
@@ -34,6 +35,11 @@ class GL_EXPORT GLImageShm : public GLImage {
   scoped_ptr<base::SharedMemory> shared_memory_;
   gfx::Size size_;
   unsigned internalformat_;
+#if defined(OS_WIN) || defined(USE_X11) || defined(OS_ANDROID) || \
+    defined(USE_OZONE)
+  GLuint egl_texture_id_;
+  EGLImageKHR egl_image_;
+#endif
 
   DISALLOW_COPY_AND_ASSIGN(GLImageShm);
 };
