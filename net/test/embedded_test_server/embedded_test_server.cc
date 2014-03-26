dc3f9395a4e9342392453087bd7fa182b423fd1f
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-26 20:52:04 +0000

    Delete net/tools/fetch. No one is using this.
    BUG=none
    
    Review URL: https://codereview.chromium.org/195863006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259680 0039d316-1c4b-4281-b951-d872f2087c98

--- net/test/embedded_test_server/embedded_test_server.cc
+++ net/test/embedded_test_server/embedded_test_server.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/test/embedded_test_server/http_connection.h"
 #include "net/test/embedded_test_server/http_request.h"
 #include "net/test/embedded_test_server/http_response.h"
-#include "net/tools/fetch/http_listen_socket.h"
 
 namespace net {
 namespace test_server {
