f9be192d4a1bd32efe8679091e00a134b82ee862
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-26 12:22:47 +0000

    Revert of Revert of Remove unused FontPlatformDataHarfBuzz::emSizeInFontUnits (https://codereview.chromium.org/212763002/)
    Reason for revert:
    I don't think this was a reason of failures
    
    Original issue's description:
    > Revert of Remove unused FontPlatformDataHarfBuzz::emSizeInFontUnits (https://codereview.chromium.org/210573002/)
    > 
    > Reason for revert:
    > This might have caused android tests failure: 
    > http://build.chromium.org/p/chromium.webkit/builders/Android%20Tests%20%28dbg%29/builds/18266
    > Speculatively reverting it.
    > 
    > Original issue's description:
    > > Remove unused FontPlatformDataHarfBuzz::emSizeInFontUnits
    > > 
    > > Remove the unused m_emSizeInFontUnits field and emSizeInFontUnits
    > > method from FontPlatformDataHarfBuzz.
    > > 
    > > BUG=355806
    > > R=pdr@chromium.org
    > > 
    > > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=169955
    > 
    > TBR=pdr@chromium.org,dglazkov@chromium.org,eae@chromium.org
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=355806
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=170048
    
    TBR=pdr@chromium.org,dglazkov@chromium.org,eae@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=355806
    
    Review URL: https://codereview.chromium.org/212563005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170051 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/fonts/harfbuzz/FontPlatformDataHarfBuzz.cpp
+++ third_party/WebKit/Source/platform/fonts/harfbuzz/FontPlatformDataHarfBuzz.cpp
@@ -80,7 +80,6 @@ void FontPlatformData::setSubpixelRendering(bool useSubpixelRendering)
 
 FontPlatformData::FontPlatformData(WTF::HashTableDeletedValueType)
     : m_textSize(0)
-    , m_emSizeInFontUnits(0)
     , m_syntheticBold(false)
     , m_syntheticItalic(false)
     , m_orientation(Horizontal)
@@ -90,7 +89,6 @@ FontPlatformData::FontPlatformData(WTF::HashTableDeletedValueType)
 
 FontPlatformData::FontPlatformData()
     : m_textSize(0)
-    , m_emSizeInFontUnits(0)
     , m_syntheticBold(false)
     , m_syntheticItalic(false)
     , m_orientation(Horizontal)
@@ -100,7 +98,6 @@ FontPlatformData::FontPlatformData()
 
 FontPlatformData::FontPlatformData(float textSize, bool syntheticBold, bool syntheticItalic)
     : m_textSize(textSize)
-    , m_emSizeInFontUnits(0)
     , m_syntheticBold(syntheticBold)
     , m_syntheticItalic(syntheticItalic)
     , m_orientation(Horizontal)
@@ -112,7 +109,6 @@ FontPlatformData::FontPlatformData(const FontPlatformData& src)
     : m_typeface(src.m_typeface)
     , m_family(src.m_family)
     , m_textSize(src.m_textSize)
-    , m_emSizeInFontUnits(src.m_emSizeInFontUnits)
     , m_syntheticBold(src.m_syntheticBold)
     , m_syntheticItalic(src.m_syntheticItalic)
     , m_orientation(src.m_orientation)
@@ -126,7 +122,6 @@ FontPlatformData::FontPlatformData(PassRefPtr<SkTypeface> tf, const char* family
     : m_typeface(tf)
     , m_family(family)
     , m_textSize(textSize)
-    , m_emSizeInFontUnits(0)
     , m_syntheticBold(syntheticBold)
     , m_syntheticItalic(syntheticItalic)
     , m_orientation(orientation)
@@ -139,7 +134,6 @@ FontPlatformData::FontPlatformData(const FontPlatformData& src, float textSize)
     : m_typeface(src.m_typeface)
     , m_family(src.m_family)
     , m_textSize(textSize)
-    , m_emSizeInFontUnits(src.m_emSizeInFontUnits)
     , m_syntheticBold(src.m_syntheticBold)
     , m_syntheticItalic(src.m_syntheticItalic)
     , m_orientation(src.m_orientation)
@@ -153,15 +147,6 @@ FontPlatformData::~FontPlatformData()
 {
 }
 
-int FontPlatformData::emSizeInFontUnits() const
-{
-    if (m_emSizeInFontUnits)
-        return m_emSizeInFontUnits;
-
-    m_emSizeInFontUnits = m_typeface->getUnitsPerEm();
-    return m_emSizeInFontUnits;
-}
-
 FontPlatformData& FontPlatformData::operator=(const FontPlatformData& src)
 {
     m_typeface = src.m_typeface;
@@ -172,7 +157,6 @@ FontPlatformData& FontPlatformData::operator=(const FontPlatformData& src)
     m_harfBuzzFace = nullptr;
     m_orientation = src.m_orientation;
     m_style = src.m_style;
-    m_emSizeInFontUnits = src.m_emSizeInFontUnits;
 
     return *this;
 }
