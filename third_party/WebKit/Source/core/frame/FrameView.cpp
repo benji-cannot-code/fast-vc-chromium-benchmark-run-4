54c41fa4b7952126f2c95a96544e4cfc9c5bd8f4
jchaffraix@chromium.org <jchaffraix@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-27 01:07:54 +0000

    Kill outlineBox
    The outline box concept wasn't clearly defined. It was also unneeded
    if we track the outline into the visual overflow, which we now do!
    
    This change is a mechanical removal of the associated code. It keeps
    the logic in repaintAfterLayout that handles changes in visual overflow
    as it is required to correctly invalidate some cases where our
    simplistic visual overflow model using a single rect falls short.
    
    Most tests changes correspond to less invalidation, except for
    * fast/repaint/4776765.html which is getting 2 new invalidations
      coresponding to getting a 5px outline.
    * fast/box-shadow/negative-shadow-box*.html which grew to include
      the full box-shadow instead of being the box's size.
    
    BUG=345452
    
    Review URL: https://codereview.chromium.org/210043008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170115 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/FrameView.cpp
+++ third_party/WebKit/Source/core/frame/FrameView.cpp
@@ -1060,17 +1060,8 @@ void FrameView::repaintTree(RenderObject* root)
             }
 
         } else {
-            LayoutRect oldOutlineRect;
-            LayoutRect newOutlineRect;
-
-            if (renderer->hasOutline()) {
-                newOutlineRect = renderer->newOutlineRect();
-                oldOutlineRect = renderer->oldOutlineRect();
-            }
-
             didFullRepaint = renderer->repaintAfterLayoutIfNeeded(renderer->containerForRepaint(),
-                renderer->shouldDoFullRepaintAfterLayout(), oldRepaintRect, oldOutlineRect,
-                &newRepaintRect, &newOutlineRect);
+                renderer->shouldDoFullRepaintAfterLayout(), oldRepaintRect, &newRepaintRect);
         }
 
         if (!didFullRepaint)
--- third_party/WebKit/Source/core/rendering/RenderLayerRepainter.cpp
+++ third_party/WebKit/Source/core/rendering/RenderLayerRepainter.cpp
@@ -77,12 +77,9 @@ void RenderLayerRepainter::repaintAfterLayout(RenderGeometryMap* geometryMap, bo
 
         RenderLayerModelObject* repaintContainer = m_renderer->containerForRepaint();
         LayoutRect oldRepaintRect = m_repaintRect;
-        LayoutRect oldOutlineBox = m_outlineBox;
         computeRepaintRects(repaintContainer, geometryMap);
         shouldCheckForRepaint &= shouldRepaintLayer();
 
-        // FIXME: Should ASSERT that value calculated for m_outlineBox using the cached offset is the same
-        // as the value not using the cached offset, but we can't due to https://bugs.webkit.org/show_bug.cgi?id=37048
         if (shouldCheckForRepaint) {
             if (view && !view->document().printing()) {
                 if (m_repaintStatus & NeedsFullRepaint) {
@@ -90,7 +87,7 @@ void RenderLayerRepainter::repaintAfterLayout(RenderGeometryMap* geometryMap, bo
                     if (m_repaintRect != oldRepaintRect)
                         m_renderer->repaintUsingContainer(repaintContainer, pixelSnappedIntRect(m_repaintRect));
                 } else {
-                    m_renderer->repaintAfterLayoutIfNeeded(repaintContainer, m_renderer->selfNeedsLayout(), oldRepaintRect, oldOutlineBox, &m_repaintRect, &m_outlineBox);
+                    m_renderer->repaintAfterLayoutIfNeeded(repaintContainer, m_renderer->selfNeedsLayout(), oldRepaintRect, &m_repaintRect);
                 }
             }
         }
@@ -107,7 +104,6 @@ void RenderLayerRepainter::clearRepaintRects()
     ASSERT(!m_renderer->layer()->hasVisibleContent());
 
     m_repaintRect = IntRect();
-    m_outlineBox = IntRect();
 }
 
 void RenderLayerRepainter::computeRepaintRects(const RenderLayerModelObject* repaintContainer, const RenderGeometryMap* geometryMap)
@@ -116,7 +112,6 @@ void RenderLayerRepainter::computeRepaintRects(const RenderLayerModelObject* rep
         return;
 
     m_repaintRect = m_renderer->clippedOverflowRectForRepaint(repaintContainer);
-    m_outlineBox = m_renderer->outlineBoundsForRepaint(repaintContainer, geometryMap);
 }
 
 void RenderLayerRepainter::computeRepaintRectsIncludingDescendants()
