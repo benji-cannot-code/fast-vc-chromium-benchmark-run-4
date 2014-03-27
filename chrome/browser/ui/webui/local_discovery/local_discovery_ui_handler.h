6d5718e8bff2b2437a8c5d218727e3fe02af6f6b
noamsml@chromium.org <noamsml@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-27 22:47:03 +0000

    Enable connector UI in devices page on Mac OSX
    Enable cloud print connector UI on chrome://devices for Mac OSX.
    
    BUG=356913
    
    Review URL: https://codereview.chromium.org/214463006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260021 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/local_discovery/local_discovery_ui_handler.h
+++ chrome/browser/ui/webui/local_discovery/local_discovery_ui_handler.h
@@ -18,8 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/signin/signin_manager.h"
 #include "content/public/browser/web_ui_message_handler.h"
 
-#if defined(ENABLE_FULL_PRINTING) && !defined(OS_CHROMEOS) && \
-  !defined(OS_MACOSX)
+#if defined(ENABLE_FULL_PRINTING) && !defined(OS_CHROMEOS)
 #define CLOUD_PRINT_CONNECTOR_UI_AVAILABLE
 #endif
 
