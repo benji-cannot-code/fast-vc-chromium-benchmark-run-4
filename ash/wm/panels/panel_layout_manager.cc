dae9d638e586e4af9e8f3539fd1b1e7acd94b4f8
flackr@chromium.org <flackr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-27 23:52:51 +0000

    Fix infinite recursion on hiding panel when created during fullscreen mode.
    BUG=355989
    TEST=Ensure hangouts is running, enter fullscreen and receive a message. There should be no crash and panel should show after exiting fullscreen.
    R=oshima@chromium.org
    
    Review URL: https://codereview.chromium.org/215253003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260031 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/panels/panel_layout_manager.cc
+++ ash/wm/panels/panel_layout_manager.cc
@@ -372,7 +372,6 @@ void PanelLayoutManager::OnWindowAddedToLayout(aura::Window* child) {
     panel_info.slide_in = true;
   }
   panel_windows_.push_back(panel_info);
-  child->AddObserver(this);
   wm::GetWindowState(child)->AddObserver(this);
   Relayout();
 }
@@ -389,7 +388,6 @@ void PanelLayoutManager::OnWindowRemovedFromLayout(aura::Window* child) {
     delete found->callout_widget;
     panel_windows_.erase(found);
   }
-  child->RemoveObserver(this);
   wm::GetWindowState(child)->RemoveObserver(this);
 
   if (dragged_panel_ == child)
@@ -403,6 +401,8 @@ void PanelLayoutManager::OnWindowRemovedFromLayout(aura::Window* child) {
 
 void PanelLayoutManager::OnChildWindowVisibilityChanged(aura::Window* child,
                                                         bool visible) {
+  if (visible)
+    wm::GetWindowState(child)->Restore();
   Relayout();
 }
 
@@ -480,12 +480,6 @@ void PanelLayoutManager::OnPostWindowStateTypeChange(
     MinimizePanel(window_state->window());
   else
     RestorePanel(window_state->window());
-}
-
-void PanelLayoutManager::OnWindowVisibilityChanged(
-    aura::Window* window, bool visible) {
-  if (visible)
-    wm::GetWindowState(window)->Restore();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
--- ash/wm/panels/panel_layout_manager.h
+++ ash/wm/panels/panel_layout_manager.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "ui/aura/layout_manager.h"
-#include "ui/aura/window_observer.h"
 #include "ui/keyboard/keyboard_controller.h"
 #include "ui/keyboard/keyboard_controller_observer.h"
 #include "ui/wm/public/activation_change_observer.h"
@@ -57,7 +56,6 @@ class ASH_EXPORT PanelLayoutManager
     : public aura::LayoutManager,
       public ShelfIconObserver,
       public ShellObserver,
-      public aura::WindowObserver,
       public aura::client::ActivationChangeObserver,
       public keyboard::KeyboardControllerObserver,
       public DisplayController::Observer,
@@ -101,10 +99,6 @@ class ASH_EXPORT PanelLayoutManager
   virtual void OnPostWindowStateTypeChange(
       wm::WindowState* window_state,
       wm::WindowStateType old_type) OVERRIDE;
-
-  // Overridden from aura::WindowObserver
-  virtual void OnWindowVisibilityChanged(aura::Window* window,
-                                         bool visible) OVERRIDE;
 
   // Overridden from aura::client::ActivationChangeObserver
   virtual void OnWindowActivated(aura::Window* gained_active,
