a1a6ad4785e15c8eecb4e72111ec5e3da04e5c2d
yhirano@chromium.org <yhirano@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-27 15:58:16 +0000

    Block WebSocket handshake information transfer if necessary.
    WebSocket handshake information is shown in the devtools, but it is sent
    via the renderer process. Since it contains cookies, we should block it
    when ChildProcessSecurityPolicyImpl::CanReadRawCookies returns false.
    
    Since CanReadRawCookies is set only when the inspector is attached, we can
    block other handshake information for the inspector as well as cookies when
    it is unset, for simplicity and performance. In fact, this CL avoids sending
    NotifyStartOpeningHandshake and NotifyFinishOpeningHandshake IPCs
    in such a case.
    
    BUG=310405
    
    Review URL: https://codereview.chromium.org/212523002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259890 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/websocket_dispatcher_host.cc
+++ content/browser/renderer_host/websocket_dispatcher_host.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/logging.h"
 #include "base/stl_util.h"
+#include "content/browser/child_process_security_policy_impl.h"
 #include "content/browser/renderer_host/websocket_host.h"
 #include "content/common/websocket_messages.h"
 
@@ -25,17 +26,21 @@ typedef WebSocketDispatcherHost::WebSocketHostState WebSocketHostState;
 }  // namespace
 
 WebSocketDispatcherHost::WebSocketDispatcherHost(
+    int process_id,
     const GetRequestContextCallback& get_context_callback)
     : BrowserMessageFilter(WebSocketMsgStart),
+      process_id_(process_id),
       get_context_callback_(get_context_callback),
       websocket_host_factory_(
           base::Bind(&WebSocketDispatcherHost::CreateWebSocketHost,
                      base::Unretained(this))) {}
 
 WebSocketDispatcherHost::WebSocketDispatcherHost(
+    int process_id,
     const GetRequestContextCallback& get_context_callback,
     const WebSocketHostFactory& websocket_host_factory)
     : BrowserMessageFilter(WebSocketMsgStart),
+      process_id_(process_id),
       get_context_callback_(get_context_callback),
       websocket_host_factory_(websocket_host_factory) {}
 
@@ -78,6 +83,12 @@ bool WebSocketDispatcherHost::OnMessageReceived(const IPC::Message& message,
     return true;  // We handled the message (by ignoring it).
   }
   return host->OnMessageReceived(message, message_was_ok);
+}
+
+bool WebSocketDispatcherHost::CanReadRawCookies() const {
+  ChildProcessSecurityPolicyImpl* policy =
+      ChildProcessSecurityPolicyImpl::GetInstance();
+  return policy->CanReadRawCookies(process_id_);
 }
 
 WebSocketHost* WebSocketDispatcherHost::GetHost(int routing_id) const {
