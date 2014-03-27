17402e39a397e138abc56a91e804a3c8916797b2
resetswitch@chromium.org <resetswitch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-27 17:26:26 +0000

    Defaulting SingleClient sync_integration_tests to FakeServer
    Fake Server is ready to handle most 'single client'
    integration tests. This makes FakeServer the default and
    adds exceptions for a few negative tests that aren't
    supported yet. Those tests will continue to use the python
    sync server.
    
    Tests opt in to using the old python server using a
    SINGLE_CLIENT_LEGACY TestType.
    
    All tests pass, will run overnight to test stability.
    
    Review URL: https://codereview.chromium.org/210043009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259924 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/test/integration/enable_disable_test.cc
+++ chrome/browser/sync/test/integration/enable_disable_test.cc
@@ -15,17 +15,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-class EnableDisableTest : public SyncTest {
+class EnableDisableSingleClientTest : public SyncTest {
  public:
-  explicit EnableDisableTest(TestType test_type) : SyncTest(test_type) {}
-  virtual ~EnableDisableTest() {}
- private:
-  DISALLOW_COPY_AND_ASSIGN(EnableDisableTest);
-};
-
-class EnableDisableSingleClientTest : public EnableDisableTest {
- public:
-  EnableDisableSingleClientTest() : EnableDisableTest(SINGLE_CLIENT) {}
+  // TODO(pvalenzuela): Switch to SINGLE_CLIENT once FakeServer
+  // supports this scenario.
+  EnableDisableSingleClientTest() : SyncTest(SINGLE_CLIENT_LEGACY) {}
   virtual ~EnableDisableSingleClientTest() {}
  private:
   DISALLOW_COPY_AND_ASSIGN(EnableDisableSingleClientTest);
