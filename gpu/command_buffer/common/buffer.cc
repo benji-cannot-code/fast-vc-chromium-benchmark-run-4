7118b95c76bde695ee8f09a763ba57fa2e1c6cfb
epenner@chromium.org <epenner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-27 03:06:14 +0000

    GPU:'Pass' SharedMemory when possible.
    This avoids some duplication of shared memory handles just
    for the purposes of passing them around. Instead we Map
    immediately when the memory arrives in the process, and then
    'Pass' the shared memory from there.
    
    BUG=177063, 353822
    
    Review URL: https://codereview.chromium.org/211703003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259772 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/common/buffer.cc
+++ gpu/command_buffer/common/buffer.cc
@@ -5,12 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "gpu/command_buffer/common/buffer.h"
 
+#include "base/logging.h"
+
 namespace gpu {
 
 Buffer::Buffer(scoped_ptr<base::SharedMemory> shared_memory, size_t size)
     : shared_memory_(shared_memory.Pass()),
       memory_(shared_memory_->memory()),
-      size_(size) {}
+      size_(size) {
+  DCHECK(memory_) << "The memory must be mapped to create a Buffer";
+}
 
 Buffer::~Buffer() {}
 
