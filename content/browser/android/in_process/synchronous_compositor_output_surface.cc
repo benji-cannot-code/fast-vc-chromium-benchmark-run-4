ba0d65bd199c01e90cc966d0043e8909bdb77c47
reed@google.com <reed@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-27 17:08:51 +0000

    don't create a bitmapdevice (deprecated) when you just want an empty canvas
    TBR=bulach@chromium.org
    NOTRY=True
    
    Review URL: https://codereview.chromium.org/214363004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259919 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/android/in_process/synchronous_compositor_output_surface.cc
+++ content/browser/android/in_process/synchronous_compositor_output_surface.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "gpu/command_buffer/client/gles2_interface.h"
 #include "gpu/command_buffer/common/gpu_memory_allocation.h"
-#include "third_party/skia/include/core/SkBitmapDevice.h"
 #include "third_party/skia/include/core/SkCanvas.h"
 #include "ui/gfx/rect_conversions.h"
 #include "ui/gfx/skia_util.h"
@@ -39,9 +38,7 @@ class SynchronousCompositorOutputSurface::SoftwareDevice
   : public cc::SoftwareOutputDevice {
  public:
   SoftwareDevice(SynchronousCompositorOutputSurface* surface)
-    : surface_(surface),
-      null_device_(SkBitmap::kARGB_8888_Config, 1, 1),
-      null_canvas_(&null_device_) {
+    : surface_(surface) {
   }
   virtual void Resize(const gfx::Size& size) OVERRIDE {
     // Intentional no-op: canvas size is controlled by the embedder.
@@ -63,7 +60,6 @@ class SynchronousCompositorOutputSurface::SoftwareDevice
 
  private:
   SynchronousCompositorOutputSurface* surface_;
-  SkBitmapDevice null_device_;
   SkCanvas null_canvas_;
 
   DISALLOW_COPY_AND_ASSIGN(SoftwareDevice);
