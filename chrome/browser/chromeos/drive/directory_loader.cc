8af3c26193a19d98d7fa1e90ab5ad4f07c5d620b
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-27 05:35:29 +0000

    drive: Send locally found entries before starting load
    After this change, ReadDirectory sends entries to the callback in three steps:
    1. Send any locally found entries before the load starts. (NEW)
    2. Send loaded entries as soon as they are received and stored to the local metadata.
    3. Send any locally added entries after the load finishes.
    
    BUG=340931
    TEST=unit_tests
    
    Review URL: https://codereview.chromium.org/210723003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259794 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/directory_loader.cc
+++ chrome/browser/chromeos/drive/directory_loader.cc
@@ -37,6 +37,7 @@ FileError CheckLocalState(ResourceMetadata* resource_metadata,
                           const google_apis::AboutResource& about_resource,
                           const std::string& local_id,
                           ResourceEntry* entry,
+                          ResourceEntryVector* child_entries,
                           int64* local_changestamp) {
   // Fill My Drive resource ID.
   ResourceEntry mydrive;
@@ -54,6 +55,11 @@ FileError CheckLocalState(ResourceMetadata* resource_metadata,
 
   // Get entry.
   error = resource_metadata->GetResourceEntryById(local_id, entry);
+  if (error != FILE_ERROR_OK)
+    return error;
+
+  // Get child entries.
+  error = resource_metadata->ReadDirectoryById(local_id, child_entries);
   if (error != FILE_ERROR_OK)
     return error;
 
@@ -400,6 +406,7 @@ void DirectoryLoader::ReadDirectoryAfterGetAboutResource(
   // Check the current status of local metadata, and start loading if needed.
   google_apis::AboutResource* about_resource_ptr = about_resource.get();
   ResourceEntry* entry = new ResourceEntry;
+  ResourceEntryVector* child_entries = new ResourceEntryVector;
   int64* local_changestamp = new int64;
   base::PostTaskAndReplyWithResult(
       blocking_task_runner_,
@@ -409,12 +416,14 @@ void DirectoryLoader::ReadDirectoryAfterGetAboutResource(
                  *about_resource_ptr,
                  local_id,
                  entry,
+                 child_entries,
                  local_changestamp),
       base::Bind(&DirectoryLoader::ReadDirectoryAfterCheckLocalState,
                  weak_ptr_factory_.GetWeakPtr(),
                  base::Passed(&about_resource),
                  local_id,
                  base::Owned(entry),
+                 base::Owned(child_entries),
                  base::Owned(local_changestamp)));
 }
 
@@ -422,6 +431,7 @@ void DirectoryLoader::ReadDirectoryAfterCheckLocalState(
     scoped_ptr<google_apis::AboutResource> about_resource,
     const std::string& local_id,
     const ResourceEntry* entry,
+    const ResourceEntryVector* child_entries,
     const int64* local_changestamp,
     FileError error) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
@@ -460,6 +470,8 @@ void DirectoryLoader::ReadDirectoryAfterCheckLocalState(
   if (directory_changestamp + kMinimumChangestampGap > remote_changestamp) {
     OnDirectoryLoadComplete(local_id, FILE_ERROR_OK);
   } else {
+    // Send locally found entries to callbacks.
+    SendEntries(local_id, *child_entries, true /*has_more*/);
     // Start fetching the directory content, and mark it with the changestamp
     // |remote_changestamp|.
     LoadDirectoryFromServer(directory_fetch_info);
