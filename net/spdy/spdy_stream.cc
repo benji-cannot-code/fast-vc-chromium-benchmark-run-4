e7fbd065e836a261c671999f0e3ddd4b20a94070
jgraettinger@chromium.org <jgraettinger@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-27 22:24:49 +0000

    Instrument SpdyStream to catch delegate write handlers closing |this|.
    A long-standing assertion in SpdyStream is that delegate write handlers
    don't close |this|. This assertion was preserved in the recent SpdyStream
    state refactor. However after enabling the REMOTE_CLOSE stream state,
    crbug.com/355511 indicates that this assertion isn't always true.
    
    Insrument SpdyStream to fail with a usable stack-trace within the
    delegate call destroying |this|.
    
    BUG=355511
    
    Review URL: https://codereview.chromium.org/212793008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260014 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_stream.cc
+++ net/spdy/spdy_stream.cc
@@ -104,7 +104,8 @@ SpdyStream::SpdyStream(SpdyStreamType type,
       net_log_(net_log),
       raw_received_bytes_(0),
       send_bytes_(0),
-      recv_bytes_(0) {
+      recv_bytes_(0),
+      write_handler_guard_(false) {
   CHECK(type_ == SPDY_BIDIRECTIONAL_STREAM ||
         type_ == SPDY_REQUEST_RESPONSE_STREAM ||
         type_ == SPDY_PUSH_STREAM);
@@ -113,6 +114,7 @@ SpdyStream::SpdyStream(SpdyStreamType type,
 }
 
 SpdyStream::~SpdyStream() {
+  CHECK(!write_handler_guard_);
   UpdateHistograms();
 }
 
@@ -545,12 +547,14 @@ void SpdyStream::OnFrameWriteComplete(SpdyFrameType frame_type,
   CHECK(delegate_);
   {
     base::WeakPtr<SpdyStream> weak_this = GetWeakPtr();
+    write_handler_guard_ = true;
     if (frame_type == SYN_STREAM) {
       delegate_->OnRequestHeadersSent();
     } else {
       delegate_->OnDataSent();
     }
     CHECK(weak_this);
+    write_handler_guard_ = false;
   }
 
   if (io_state_ == STATE_CLOSED) {
