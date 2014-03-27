d52aa8cf9ef618aed2e105c288f8608882a4cc22
pkasting@chromium.org <pkasting@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-27 18:38:00 +0000

    Change the BMP decoder to not reject paletted images which use color indexes beyond the end of the color table.  Instead, draw these pixels as black.
    This matches Firefox and IE.
    
    BUG=176678
    TEST=none
    R=abarth@chromium.org, hclam@chromium.org
    
    Review URL: https://codereview.chromium.org/213613002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170193 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/image-decoders/bmp/BMPImageReader.h
+++ third_party/WebKit/Source/platform/image-decoders/bmp/BMPImageReader.h
@@ -333,7 +333,6 @@ private:
     int m_bitShiftsLeft[4];
 
     // The color palette, for paletted formats.
-    size_t m_tableSizeInBytes;
     Vector<RGBTriple> m_colorTable;
 
     // The coordinate to which we've decoded the image.
