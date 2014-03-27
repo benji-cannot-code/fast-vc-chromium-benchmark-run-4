8ad8f05e02ab4972d16657141817e1fcca00b1d4
boliu@chromium.org <boliu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-27 15:38:44 +0000

    Fix MessageLoopProxy::current in video surface texture
    If StreamTextureProxyImpl::BindToCurrentThread in android webview is called
    on a thread that does not have a message loop, then it should bind to the
    compositor thread.
    
    BUG=344087
    
    Review URL: https://codereview.chromium.org/208093002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259875 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/android/stream_texture_factory_android_synchronous_impl.cc
+++ content/renderer/media/android/stream_texture_factory_android_synchronous_impl.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/lock.h"
 #include "cc/output/context_provider.h"
 #include "content/common/android/surface_texture_peer.h"
+#include "content/renderer/render_thread_impl.h"
 #include "gpu/command_buffer/client/gles2_interface.h"
 #include "ui/gl/android/surface_texture.h"
 
@@ -35,11 +36,11 @@ class StreamTextureProxyImpl
 
   // StreamTextureProxy implementation:
   virtual void BindToCurrentThread(int32 stream_id) OVERRIDE;
-  virtual bool IsBoundToThread() OVERRIDE { return loop_.get() != NULL; }
   virtual void SetClient(cc::VideoFrameProvider::Client* client) OVERRIDE;
   virtual void Release() OVERRIDE;
 
  private:
+  void BindOnCompositorThread(int stream_id);
   void OnFrameAvailable();
 
   scoped_refptr<base::MessageLoopProxy> loop_;
@@ -60,6 +61,8 @@ class StreamTextureProxyImpl
 StreamTextureProxyImpl::StreamTextureProxyImpl(
     StreamTextureFactorySynchronousImpl::ContextProvider* provider)
     : context_provider_(provider), has_updated_(false) {
+  DCHECK(RenderThreadImpl::current());
+  loop_ = RenderThreadImpl::current()->compositor_message_loop_proxy();
   std::fill(current_matrix_, current_matrix_ + 16, 0);
 }
 
@@ -67,7 +70,7 @@ StreamTextureProxyImpl::~StreamTextureProxyImpl() {}
 
 void StreamTextureProxyImpl::Release() {
   SetClient(NULL);
-  if (loop_.get() && !loop_->BelongsToCurrentThread())
+  if (!loop_->BelongsToCurrentThread())
     loop_->DeleteSoon(FROM_HERE, this);
   else
     delete this;
@@ -79,8 +82,19 @@ void StreamTextureProxyImpl::SetClient(cc::VideoFrameProvider::Client* client) {
 }
 
 void StreamTextureProxyImpl::BindToCurrentThread(int stream_id) {
-  loop_ = base::MessageLoopProxy::current();
+  if (loop_->BelongsToCurrentThread()) {
+    BindOnCompositorThread(stream_id);
+    return;
+  }
 
+  // Weakptr is only used on compositor thread loop, so this is safe.
+  loop_->PostTask(FROM_HERE,
+                  base::Bind(&StreamTextureProxyImpl::BindOnCompositorThread,
+                             AsWeakPtr(),
+                             stream_id));
+}
+
+void StreamTextureProxyImpl::BindOnCompositorThread(int stream_id) {
   surface_texture_ = context_provider_->GetSurfaceTexture(stream_id);
   if (!surface_texture_) {
     LOG(ERROR) << "Failed to get SurfaceTexture for stream.";
