f1216f11adcec90ddd4cc43cd6269ea96a2c5896
zhenyu.liang@intel.com <zhenyu.liang@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-27 07:48:24 +0000

    Add x86_64 ucontext structure for Android x64
    BUG=346626
    
    Review URL: https://codereview.chromium.org/204983015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259817 0039d316-1c4b-4281-b951-d872f2087c98

--- sandbox/linux/services/android_ucontext.h
+++ sandbox/linux/services/android_ucontext.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sandbox/linux/services/android_arm_ucontext.h"
 #elif defined(__i386__)
 #include "sandbox/linux/services/android_i386_ucontext.h"
+#elif defined(__x86_64__)
+#include "sandbox/linux/services/android_x86_64_ucontext.h"
 #else
 #error "No support for your architecture in Android header"
 #endif
