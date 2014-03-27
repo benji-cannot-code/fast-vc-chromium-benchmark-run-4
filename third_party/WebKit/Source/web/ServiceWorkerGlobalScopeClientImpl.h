8cb6dcdd03d7b613e1b7e767cbe7d22c67e79ef2
jkarlin@chromium.org <jkarlin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-27 11:47:37 +0000

    The blink half of the new ServiceWorker 'sync' event.
    The browser side is https://codereview.chromium.org/205033002/
    
    BUG=354112
    
    Review URL: https://codereview.chromium.org/205013002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170165 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/ServiceWorkerGlobalScopeClientImpl.h
+++ third_party/WebKit/Source/web/ServiceWorkerGlobalScopeClientImpl.h
@@ -46,6 +46,7 @@ public:
 
     virtual void didHandleInstallEvent(int installEventID, WebServiceWorkerEventResult) OVERRIDE;
     virtual void didHandleFetchEvent(int fetchEventID, PassRefPtr<WebCore::Response>) OVERRIDE;
+    virtual void didHandleSyncEvent(int syncEventID) OVERRIDE;
 
 private:
     ServiceWorkerGlobalScopeClientImpl(PassOwnPtr<WebServiceWorkerContextClient>);
