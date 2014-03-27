26125ceb6b3247610b266ca604ae91e4803ea525
dstockwell@chromium.org <dstockwell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-27 11:49:08 +0000

    CSS Transforms: Implement transform-style
    Implements transform-style as defined in CSS Transforms[1]. Behavior is
    identical to -webkit-transform-style.
    
    Note, this property is guarded by the CSSTransformsUnprefixed runtime feature
    (enabled only for testing).
    
    [1] http://www.w3.org/TR/css3-transforms/
    
    BUG=154772
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=170013
    
    Review URL: https://codereview.chromium.org/211123002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170166 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/CSSComputedStyleDeclaration.cpp
+++ third_party/WebKit/Source/core/css/CSSComputedStyleDeclaration.cpp
@@ -317,6 +317,7 @@ static const CSSPropertyID staticComputableProperties[] = {
     CSSPropertyWebkitTextStrokeWidth,
     CSSPropertyWebkitTransform,
     CSSPropertyWebkitTransformOrigin,
+    CSSPropertyTransformStyle,
     CSSPropertyWebkitTransformStyle,
     CSSPropertyWebkitTransitionDelay,
     CSSPropertyWebkitTransitionDuration,
@@ -2570,6 +2571,7 @@ PassRefPtrWillBeRawPtr<CSSValue> CSSComputedStyleDeclaration::getPropertyCSSValu
             }
             return list.release();
         }
+        case CSSPropertyTransformStyle:
         case CSSPropertyWebkitTransformStyle:
             return cssValuePool().createIdentifierValue((style->transformStyle3D() == TransformStyle3DPreserve3D) ? CSSValuePreserve3d : CSSValueFlat);
         case CSSPropertyTransitionDelay:
@@ -2769,7 +2771,6 @@ PassRefPtrWillBeRawPtr<CSSValue> CSSComputedStyleDeclaration::getPropertyCSSValu
         case CSSPropertyPerspectiveOrigin:
         case CSSPropertyTransform:
         case CSSPropertyTransformOrigin:
-        case CSSPropertyTransformStyle:
             break;
 
         /* Unimplemented @font-face properties */
--- third_party/WebKit/Source/core/css/resolver/StyleBuilderCustom.cpp
+++ third_party/WebKit/Source/core/css/resolver/StyleBuilderCustom.cpp
@@ -1747,7 +1747,6 @@ void StyleBuilder::oldApplyProperty(CSSPropertyID id, StyleResolverState& state,
     case CSSPropertyPerspectiveOrigin:
     case CSSPropertyTransform:
     case CSSPropertyTransformOrigin:
-    case CSSPropertyTransformStyle:
         return;
     // These properties are aliased and we already applied the property on the prefixed version.
     case CSSPropertyAnimationDelay:
@@ -1969,6 +1968,7 @@ void StyleBuilder::oldApplyProperty(CSSPropertyID id, StyleResolverState& state,
     case CSSPropertyWebkitTransformOriginX:
     case CSSPropertyWebkitTransformOriginY:
     case CSSPropertyWebkitTransformOriginZ:
+    case CSSPropertyTransformStyle:
     case CSSPropertyWebkitTransformStyle:
     case CSSPropertyWebkitTransitionDelay:
     case CSSPropertyWebkitTransitionDuration:
