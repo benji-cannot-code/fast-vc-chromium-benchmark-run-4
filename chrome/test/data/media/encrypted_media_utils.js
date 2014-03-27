29631fcaeaa2815c7cae387d6218ee96f36e13ec
jrummell@chromium.org <jrummell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-27 11:45:47 +0000

    Disable encrypted media tests if CDM loaded as a component
    Tests are failing on Win and Mac when the CDM is loaded as a component,
    so disabling the tests for now until the underlying issue (http://crbug.com/356833) is resolved.
    
    BUG=356721
    TEST=encrypted media browser tests pass
    
    Review URL: https://codereview.chromium.org/212623006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259834 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/media/encrypted_media_utils.js
+++ chrome/test/data/media/encrypted_media_utils.js
@@ -138,6 +138,7 @@ function loadEncryptedMedia(video, mediaFile, keySystem, key, useMSE,
       }
     }
     catch(error) {
+      console.log(error.message);
       setResultInTitle(error.name);
     }
   }
