004c3878f9e4fcc33514323c37ded052ac19fffd
rdevlin.cronin@chromium.org <rdevlin.cronin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-27 15:34:55 +0000

    Now that development is done, make this feature win-only again, and remove the previous browser highlight assets.
    BUG=328441
    TBR=sky@chromium.org (for c/b/ui)
    TBR=oshima@chromium.org (removing old assets, as discussed in https://codereview.chromium.org/205583007/)
    
    Review URL: https://codereview.chromium.org/211153005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259871 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/extensions/extension_message_bubble_view.cc
+++ chrome/browser/ui/views/extensions/extension_message_bubble_view.cc
@@ -285,6 +285,7 @@ ExtensionMessageBubbleFactory::~ExtensionMessageBubbleFactory() {
 }
 
 void ExtensionMessageBubbleFactory::MaybeShow(views::View* anchor_view) {
+#if defined(OS_WIN)
   // The list of suspicious extensions takes priority over the dev mode bubble
   // and the settings API bubble, since that needs to be shown as soon as we
   // disable something. The settings API bubble is shown on first startup after
@@ -307,6 +308,7 @@ void ExtensionMessageBubbleFactory::MaybeShow(views::View* anchor_view) {
     MaybeShowDevModeExtensionsBubble(anchor_view);
 
   RecordProfileCheck(profile_->GetOriginalProfile());
+#endif  // OS_WIN
 }
 
 bool ExtensionMessageBubbleFactory::MaybeShowSuspiciousExtensionsBubble(
