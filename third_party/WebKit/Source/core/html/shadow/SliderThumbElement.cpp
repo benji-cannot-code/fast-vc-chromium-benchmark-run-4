1360c0dd4a9091130017b5bdc3aa1d9e35052050
philipj@opera.com <philipj@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-27 05:25:44 +0000

    Add a missing null check in SliderThumbElement::stopDragging()
    hostInput() is null checked in several other SliderThumbElement
    functions and clearly can return null, so just handle it.
    
    Add GCController.collect() to the test case to make the crash trigger
    reliably, which was necessary in order to debug it.
    
    BUG=356148
    
    Review URL: https://codereview.chromium.org/211403003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170139 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/shadow/SliderThumbElement.cpp
+++ third_party/WebKit/Source/core/html/shadow/SliderThumbElement.cpp
@@ -227,12 +227,12 @@ bool SliderThumbElement::isDisabledFormControl() const
 
 bool SliderThumbElement::matchesReadOnlyPseudoClass() const
 {
-    return hostInput()->matchesReadOnlyPseudoClass();
+    return hostInput() && hostInput()->matchesReadOnlyPseudoClass();
 }
 
 bool SliderThumbElement::matchesReadWritePseudoClass() const
 {
-    return hostInput()->matchesReadWritePseudoClass();
+    return hostInput() && hostInput()->matchesReadWritePseudoClass();
 }
 
 Node* SliderThumbElement::focusDelegate()
@@ -323,7 +323,8 @@ void SliderThumbElement::stopDragging()
     m_inDragMode = false;
     if (renderer())
         renderer()->setNeedsLayout();
-    hostInput()->dispatchFormControlChangeEvent();
+    if (hostInput())
+        hostInput()->dispatchFormControlChangeEvent();
 }
 
 void SliderThumbElement::defaultEventHandler(Event* event)
