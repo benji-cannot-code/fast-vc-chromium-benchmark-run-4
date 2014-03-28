1b25feae9880b98b2000dcc1ed1e19750e9ebf19
keishi@chromium.org <keishi@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-28 03:46:08 +0000

    select.add() should support optgroup adding and before index
    According to the spec (http://www.whatwg.org/specs/web-apps/current-work/multipage/the-button-element.html#the-select-element) the idl for select.add is
    void add((HTMLOptionElement or HTMLOptGroupElement) element, optional (HTMLElement or long)? before = null);
    
    select.add should accept an optgroup as the first argument.
    select.add should support an index as the second argument.
    
    BUG=345345
    
    Review URL: https://codereview.chromium.org/213773003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170259 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLOptionsCollection.cpp
+++ third_party/WebKit/Source/core/html/HTMLOptionsCollection.cpp
@@ -94,7 +94,7 @@ void HTMLOptionsCollection::add(PassRefPtr<HTMLOptionElement> element, int index
     if (index == -1 || unsigned(index) >= length())
         select.add(newOption, 0, exceptionState);
     else
-        select.add(newOption, toHTMLOptionElement(item(index)), exceptionState);
+        select.addBeforeOptionAtIndex(newOption, index, exceptionState);
 
     ASSERT(!exceptionState.hadException());
 }
