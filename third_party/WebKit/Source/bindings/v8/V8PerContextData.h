d43fbaf42d9be599d54ff3187d91e842ca714f8a
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-28 04:14:01 +0000

    Make DOMWrapperWorld::current() return a reference instead of a pointer
    Now that it's guaranteed that DOMWrapperWorld is not 0 at any given time, we can make it a reference instead of a pointer.
    
    BUG=341032
    
    Review URL: https://codereview.chromium.org/209713003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170261 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/V8PerContextData.h
+++ third_party/WebKit/Source/bindings/v8/V8PerContextData.h
@@ -68,7 +68,7 @@ public:
     }
 
     static V8PerContextData* from(v8::Handle<v8::Context>);
-    static DOMWrapperWorld* world(v8::Handle<v8::Context>);
+    static DOMWrapperWorld& world(v8::Handle<v8::Context>);
 
     ~V8PerContextData();
 
--- third_party/WebKit/Source/core/loader/FrameLoader.h
+++ third_party/WebKit/Source/core/loader/FrameLoader.h
@@ -148,7 +148,7 @@ public:
 
     String userAgent(const KURL&) const;
 
-    void dispatchDidClearWindowObjectInWorld(DOMWrapperWorld*);
+    void dispatchDidClearWindowObjectInWorld(DOMWrapperWorld&);
     void dispatchDidClearWindowObjectsInAllWorlds();
     void dispatchDocumentElementAvailable();
 
