deaf285371dfc5d6d94a7dcafc81eeaa682f4c2f
pavely@chromium.org <pavely@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-28 20:59:14 +0000

    Notify cacheinvalidations about network status
    Cacheinvalidations retries sending messages when network becomes available
    and some time has passed since last attempt to send message. This change
    is to notify cacheinvalidations about network status change.
    
    Switch to transient error only when sending message failed.
    Switch to INVALIDATIONS_ENABLED to let cacheinvalidations retry when
    network changes.
    
    BUG=325020
    R=rlarocque@chromium.org
    
    Review URL: https://codereview.chromium.org/215073005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260271 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/notifier/gcm_network_channel.cc
+++ sync/notifier/gcm_network_channel.cc
@@ -156,11 +156,13 @@ GCMNetworkChannel::GCMNetworkChannel(
       register_backoff_entry_(new net::BackoffEntry(&kRegisterBackoffPolicy)),
       diagnostic_info_(this),
       weak_factory_(this) {
+  net::NetworkChangeNotifier::AddNetworkChangeObserver(this);
   delegate_->Initialize();
   Register();
 }
 
 GCMNetworkChannel::~GCMNetworkChannel() {
+  net::NetworkChangeNotifier::RemoveNetworkChangeObserver(this);
 }
 
 void GCMNetworkChannel::UpdateCredentials(
@@ -262,6 +264,10 @@ void GCMNetworkChannel::OnGetTokenComplete(
     // sending message and at that time we'll retry requesting access token.
     DVLOG(1) << "RequestAccessToken failed: " << error.ToString();
     RecordOutgoingMessageStatus(ACCESS_TOKEN_FAILURE);
+    // Message won't get sent because of connection failure. Let's retry once
+    // connection is restored.
+    if (error.state() == GoogleServiceAuthError::CONNECTION_FAILED)
+      NotifyStateChange(TRANSIENT_INVALIDATION_ERROR);
     cached_message_.clear();
     return;
   }
@@ -338,11 +344,21 @@ void GCMNetworkChannel::OnURLFetchComplete(const net::URLFetcher* source) {
        fetcher->GetResponseCode() != net::HTTP_NO_CONTENT)) {
     DVLOG(1) << "URLFetcher failure";
     RecordOutgoingMessageStatus(POST_FAILURE);
+    NotifyStateChange(TRANSIENT_INVALIDATION_ERROR);
     return;
   }
 
   RecordOutgoingMessageStatus(OUTGOING_MESSAGE_SUCCESS);
+  NotifyStateChange(INVALIDATIONS_ENABLED);
   DVLOG(2) << "URLFetcher success";
+}
+
+void GCMNetworkChannel::OnNetworkChanged(
+    net::NetworkChangeNotifier::ConnectionType connection_type) {
+  // Network connection is restored. Let's notify cacheinvalidations so it has
+  // chance to retry.
+  if (connection_type != net::NetworkChangeNotifier::CONNECTION_NONE)
+    NotifyStateChange(INVALIDATIONS_ENABLED);
 }
 
 GURL GCMNetworkChannel::BuildUrl(const std::string& registration_id) {
