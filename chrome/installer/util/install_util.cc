04950b8e7bf17901d2b2048f1c6a6ac5fcaaee5b
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-28 19:38:26 +0000

    Fix the new First Run sentinel file path determination.
    Add first_run/InstallHelper GetFirstRunSentinelFilePath helpers.
    Use PathService to get the correct value of DIR_USER_DATA.
    (may be overridden by a policy, env var, or commandline)
    (InstallUtil::GetSentinelFilePath just uses the default dir)
    
    Make the IsFirstRunSentinelPresent helper platform-specific.
    (simplifies its predecessor's Win-specific legacy abstractions)
    Some related refactoring, cleanup, and test updates.
    
    TODO(msw): Fix InstallUtil::GetSentinelFilePath.
    TODO(msw): Fix/Nix installer::GetChromeUserDataPaths.
    TODO(msw): Further consolidate related/duplicated code.
    
    BUG=354634,341049
    TEST=Chrome install and first run work as intended; respect user-data-dir commandline, env var, and policy overrides.
    R=grt@chromium.org
    TBR=gab@chromium.org
    
    Review URL: https://codereview.chromium.org/208393020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260242 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/util/install_util.cc
+++ chrome/installer/util/install_util.cc
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/metro.h"
 #include "base/win/registry.h"
 #include "base/win/windows_version.h"
+#include "chrome/common/chrome_constants.h"
+#include "chrome/common/chrome_paths.h"
 #include "chrome/installer/util/browser_distribution.h"
 #include "chrome/installer/util/google_update_constants.h"
 #include "chrome/installer/util/helper.h"
@@ -400,10 +402,19 @@ bool InstallUtil::IsChromeSxSProcess() {
   return sxs;
 }
 
-bool InstallUtil::GetSentinelFilePath(
-    const base::FilePath::CharType* file,
-    BrowserDistribution* dist,
-    base::FilePath* path) {
+// static
+bool InstallUtil::IsFirstRunSentinelPresent() {
+  // TODO(msw): Consolidate with first_run::internal::IsFirstRunSentinelPresent.
+  base::FilePath user_data_dir;
+  return !PathService::Get(chrome::DIR_USER_DATA, &user_data_dir) ||
+         base::PathExists(user_data_dir.Append(chrome::kFirstRunSentinel));
+}
+
+// static
+bool InstallUtil::GetSentinelFilePath(const base::FilePath::CharType* file,
+                                      BrowserDistribution* dist,
+                                      base::FilePath* path) {
+  // TODO(msw): Use PathService to obtain the correct DIR_USER_DATA.
   std::vector<base::FilePath> user_data_dir_paths;
   installer::GetChromeUserDataPaths(dist, &user_data_dir_paths);
 
