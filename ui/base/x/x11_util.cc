8fb0745966a0a16e5684e50997fcff644533e9b5
varkha@chromium.org <varkha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-28 00:50:04 +0000

    Allows menu host windows to be enumerated in DragTargetWindowFinder. We create a cache that holds all XIDs of the menu windows that get created in X. The menus are evaluated for being drop targets before enumerating other windows. The menu XIDs cache gets updated in a root X11DesktopHandler when any menu (and Chrome menu in particular) gets created.
    TBR(sky) for OWNERS in ui/base/ui_base.gyp.
    
    BUG=349154, 349156, 344747
    TBR=sky
    
    Review URL: https://codereview.chromium.org/196213004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260046 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/x/x11_util.cc
+++ ui/base/x/x11_util.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "third_party/skia/include/core/SkPostConfig.h"
+#include "ui/base/x/x11_menu_list.h"
 #include "ui/base/x/x11_util_internal.h"
 #include "ui/events/event_utils.h"
 #include "ui/events/keycodes/keyboard_code_conversion_x.h"
@@ -1084,6 +1085,18 @@ bool EnumerateChildren(EnumerateWindowsDelegate* delegate, XID window,
   if (depth > max_depth)
     return false;
 
+  std::vector<XID> windows;
+  std::vector<XID>::iterator iter;
+  if (depth == 0) {
+    XMenuList::GetInstance()->InsertMenuWindowXIDs(&windows);
+    // Enumerate the menus first.
+    for (iter = windows.begin(); iter != windows.end(); iter++) {
+      if (delegate->ShouldStopIterating(*iter))
+        return true;
+    }
+    windows.clear();
+  }
+
   XID root, parent, *children;
   unsigned int num_children;
   int status = XQueryTree(gfx::GetXDisplay(), window, &root, &parent, &children,
@@ -1091,7 +1104,6 @@ bool EnumerateChildren(EnumerateWindowsDelegate* delegate, XID window,
   if (status == 0)
     return false;
 
-  std::vector<XID> windows;
   for (int i = static_cast<int>(num_children) - 1; i >= 0; i--)
     windows.push_back(children[i]);
 
@@ -1099,7 +1111,6 @@ bool EnumerateChildren(EnumerateWindowsDelegate* delegate, XID window,
 
   // XQueryTree returns the children of |window| in bottom-to-top order, so
   // reverse-iterate the list to check the windows from top-to-bottom.
-  std::vector<XID>::iterator iter;
   for (iter = windows.begin(); iter != windows.end(); iter++) {
     if (IsWindowNamed(*iter) && delegate->ShouldStopIterating(*iter))
       return true;
@@ -1135,6 +1146,7 @@ void EnumerateTopLevelWindows(ui::EnumerateWindowsDelegate* delegate) {
     ui::EnumerateAllWindows(delegate, kMaxSearchDepth);
     return;
   }
+  XMenuList::GetInstance()->InsertMenuWindowXIDs(&stack);
 
   std::vector<XID>::iterator iter;
   for (iter = stack.begin(); iter != stack.end(); iter++) {
