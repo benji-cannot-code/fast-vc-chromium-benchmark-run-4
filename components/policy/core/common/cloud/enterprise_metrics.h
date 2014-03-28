bc9c829ff3c4cd0fde5c5536fb78404006ecc9e7
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-28 18:44:20 +0000

    Add error message for domain mismatch on re-enrollment.
    This error shows up if the server indicates to the client that
    enrollment got rejected due to the device belonging do a different
    domain (as indicated by the client-provided state key).
    
    BUG=chromium:357207
    TEST=Manual, check that the error message shows up on domain mismatch.
    R=asvitkine@chromium.org, dpolukhin@chromium.org, joaodasilva@chromium.org
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=260102
    
    Review URL: https://codereview.chromium.org/214963002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260216 0039d316-1c4b-4281-b951-d872f2087c98

--- components/policy/core/common/cloud/enterprise_metrics.h
+++ components/policy/core/common/cloud/enterprise_metrics.h
@@ -166,6 +166,8 @@ enum MetricEnrollment {
   kMetricEnrollmentRobotRefreshTokenStoreFailed,
   // Enrollment failed because the administrator has deprovisioned the device.
   kMetricEnrollmentDeprovisioned,
+  // Enrollment failed because the device doesn't belong to the domain.
+  kMetricEnrollmentDomainMismatch,
 
   kMetricEnrollmentSize  // Must be the last.
 };
