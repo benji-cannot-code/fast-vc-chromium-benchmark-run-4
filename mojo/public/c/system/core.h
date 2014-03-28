bb9cdbb238254b0720757a3012f48edcfffe9701
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-28 15:53:22 +0000

    Mojo: Add mojo/public/c/DEPS and remove some no-longer-needed ifdefs.
    R=darin@chromium.org
    
    Review URL: https://codereview.chromium.org/216983002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260139 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/public/c/system/core.h
+++ mojo/public/c/system/core.h
@@ -237,11 +237,8 @@ struct MojoCreateDataPipeOptions {
   uint32_t element_num_bytes;
   uint32_t capacity_num_bytes;
 };
-// TODO(vtl): Can we make this assertion work in C?
-#ifdef __cplusplus
 MOJO_COMPILE_ASSERT(sizeof(MojoCreateDataPipeOptions) == 16,
                     MojoCreateDataPipeOptions_has_wrong_size);
-#endif
 
 // |MojoWriteDataFlags|: Used to specify different modes to |MojoWriteData()|
 // and |MojoBeginWriteData()|.
@@ -313,11 +310,8 @@ struct MojoCreateSharedBufferOptions {
   uint32_t struct_size;
   MojoCreateSharedBufferOptionsFlags flags;
 };
-// TODO(vtl): Can we make this assertion work in C?
-#ifdef __cplusplus
 MOJO_COMPILE_ASSERT(sizeof(MojoCreateSharedBufferOptions) == 8,
                     MojoCreateSharedBufferOptions_has_wrong_size);
-#endif
 
 // |MojoDuplicateBufferHandleOptions|: Used to specify parameters in duplicating
 // access to a shared buffer to |MojoDuplicateBufferHandle()|.
@@ -344,11 +338,8 @@ struct MojoDuplicateBufferHandleOptions {
   uint32_t struct_size;
   MojoDuplicateBufferHandleOptionsFlags flags;
 };
-// TODO(vtl): Can we make this assertion work in C?
-#ifdef __cplusplus
 MOJO_COMPILE_ASSERT(sizeof(MojoDuplicateBufferHandleOptions) == 8,
                     MojoDuplicateBufferHandleOptions_has_wrong_size);
-#endif
 
 // |MojoMapBufferFlags|: Used to specify different modes to |MojoMapBuffer()|.
 //   |MOJO_MAP_BUFFER_FLAG_NONE| - No flags; default mode.
