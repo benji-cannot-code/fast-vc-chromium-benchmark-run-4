59623c40f6080225942d934fcffaf45bd9eaf2a0
rvargas@chromium.org <rvargas@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-28 00:16:31 +0000

    Remove PlatformFile from image_writer
    BUG=322664
    
    Review URL: https://codereview.chromium.org/208883003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260039 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/utility/image_writer/image_writer.h
+++ chrome/utility/image_writer/image_writer.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/callback.h"
+#include "base/files/file.h"
 #include "base/files/file_path.h"
 #include "base/memory/weak_ptr.h"
 
@@ -52,8 +53,8 @@ class ImageWriter : public base::SupportsWeakPtr<ImageWriter> {
   base::FilePath image_path_;
   base::FilePath device_path_;
 
-  base::PlatformFile image_file_;
-  base::PlatformFile device_file_;
+  base::File image_file_;
+  base::File device_file_;
   int64 bytes_processed_;
 
   ImageWriterHandler* handler_;
