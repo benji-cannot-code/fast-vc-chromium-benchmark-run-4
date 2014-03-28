517b317c3de6f1d6250fdda8c228adf2fe5e30d9
ahernandez.miralles@gmail.com <ahernandez.miralles@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-28 23:55:15 +0000

    Add support for "documented_in" annotation in json api schema
    BUG=341275
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/213673002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260306 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/server2/api_data_source_test.py
+++ chrome/common/extensions/docs/server2/api_data_source_test.py
@@ -65,6 +65,12 @@ class _FakeAPIDataSource(object):
     return self._json[key]
 
 
+class _FakeNamespace(object):
+
+  def __init__(self):
+    self.documentation_options = {}
+
+
 class _FakeAPIModels(object):
 
   def __init__(self, names):
@@ -72,6 +78,9 @@ class _FakeAPIModels(object):
 
   def GetNames(self):
     return self._names
+
+  def GetModel(self, name):
+    return Future(value=_FakeNamespace())
 
 
 class _FakeTemplateCache(object):
@@ -161,15 +170,15 @@ class APIDataSourceTest(unittest.TestCase):
                       _FakeTemplateCache(),
                       self._features_bundle,
                       None).ToDict()
-    self.assertEquals(_MakeLink('ref_test.html#type-type2', 'type2'),
+    self.assertEquals(_MakeLink('ref_test#type-type2', 'type2'),
                       _GetType(dict_, 'type1')['description'])
     self.assertEquals(
-        'A %s, or %s' % (_MakeLink('ref_test.html#type-type3', 'type3'),
-                         _MakeLink('ref_test.html#type-type2', 'type2')),
+        'A %s, or %s' % (_MakeLink('ref_test#type-type3', 'type3'),
+                         _MakeLink('ref_test#type-type2', 'type2')),
         _GetType(dict_, 'type2')['description'])
     self.assertEquals(
-        '%s != %s' % (_MakeLink('other.html#type-type2', 'other.type2'),
-                      _MakeLink('ref_test.html#type-type2', 'type2')),
+        '%s != %s' % (_MakeLink('other#type-type2', 'other.type2'),
+                      _MakeLink('ref_test#type-type2', 'type2')),
         _GetType(dict_, 'type3')['description'])
 
 
