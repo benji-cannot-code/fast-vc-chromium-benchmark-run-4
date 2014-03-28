cf4c1d03c8ee685220087c330b8e627c444fbf15
dbeam@chromium.org <dbeam@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-28 06:12:42 +0000

    Autofill/rAc: dispatch "input" events when changing text contents of <textarea>, <select>, or <input> tags via Autofill.
    This more closely matches the spec behavior as "input" events should be fired
    whenever the text is changed.  From the spec[1]:
    
    """
    When the user agent is to change an input element's value on behalf of the user
    (e.g. as part of a form prefilling feature), the user agent must queue a task to
    first update the value accordingly, then fire a simple event that bubbles named
    input at the input element, then fire a simple event that bubbles named change
    at the input element.
    """
    
    BUG=353691
    R=tkent@chromium.org
    
    [1] http://www.whatwg.org/specs/web-apps/current-work/multipage/common-input-element-attributes.html#event-input-input
    
    Review URL: https://codereview.chromium.org/215903003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170278 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLInputElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLInputElement.cpp
@@ -857,6 +857,7 @@ void HTMLInputElement::setChecked(bool nowChecked, TextFieldEventBehavior eventB
     if (checked() == nowChecked)
         return;
 
+    RefPtr<HTMLInputElement> protector(this);
     m_reflectsCheckedAttribute = false;
     m_isChecked = nowChecked;
     setNeedsStyleRecalc(SubtreeStyleChange);
@@ -883,6 +884,8 @@ void HTMLInputElement::setChecked(bool nowChecked, TextFieldEventBehavior eventB
     // definitely wrong in practice for these types of elements.
     if (eventBehavior != DispatchNoEvent && inDocument() && m_inputType->shouldSendChangeEventAfterCheckedChanged()) {
         setTextAsOfLastFormControlChangeEvent(String());
+        if (eventBehavior == DispatchInputAndChangeEvent)
+            dispatchFormControlInputEvent();
         dispatchFormControlChangeEvent();
     }
 
--- third_party/WebKit/Source/core/html/HTMLSelectElement.h
+++ third_party/WebKit/Source/core/html/HTMLSelectElement.h
@@ -70,7 +70,7 @@ public:
     void remove(int index);
 
     String value() const;
-    void setValue(const String&);
+    void setValue(const String&, bool sendEvents = false);
     String suggestedValue() const;
     void setSuggestedValue(const String&);
 
@@ -146,7 +146,7 @@ private:
 
     virtual void defaultEventHandler(Event*) OVERRIDE;
 
-    void dispatchInputAndChangeEventForMenuList();
+    void dispatchInputAndChangeEventForMenuList(bool requiresUserGesture = true);
 
     void recalcListItems(bool updateSelectedStates = true) const;
 
