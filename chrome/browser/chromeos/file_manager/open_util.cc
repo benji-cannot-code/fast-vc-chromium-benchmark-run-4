d0e98108bf234f0101278ec8179062f842b2a0ee
kinaba@google.com <kinaba@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-28 08:54:03 +0000

    Files.app multi-profile: add back migration to old path format.
    This CL adds Download folder path migration from /home/chronos/u-hash/Downloads
    to /home/chronos/user/Downloads when in the current running environment the
    profile path is set as the latter. It can happen when (1) the user enabled and
    then disabled the multi-profile flag, or (2) performed restart from about:flags.
    In such situations we need back migration not to break configurations.
    
    In addition, inserted path convertion to public functions in open_util so that
    paths stored in download history (during flipped path due to about:flags) can
    be correctly handled.
    
    BUG=356322,329744
    R=hirono@chromium.org
    
    Review URL: https://codereview.chromium.org/216053002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260092 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/file_manager/open_util.cc
+++ chrome/browser/chromeos/file_manager/open_util.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/file_manager/file_tasks.h"
 #include "chrome/browser/chromeos/file_manager/fileapi_util.h"
 #include "chrome/browser/chromeos/file_manager/mime_util.h"
+#include "chrome/browser/chromeos/file_manager/path_util.h"
 #include "chrome/browser/chromeos/file_manager/url_util.h"
 #include "chrome/browser/extensions/api/file_handlers/app_file_handler_util.h"
 #include "chrome/browser/extensions/extension_service.h"
@@ -208,11 +209,18 @@ void OpenRemovableDrive(Profile* profile, const base::FilePath& file_path) {
 void OpenItem(Profile* profile, const base::FilePath& file_path) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
 
+  // The path may have been stored in preferences in old versions.
+  // We need migration here.
+  // TODO(kinaba): crbug.com/313539 remove it in the future.
+  base::FilePath path;
+  if (!util::MigratePathFromOldFormat(profile, file_path, &path))
+    path = file_path;
+
   GURL url;
   if (!ConvertAbsoluteFilePathToFileSystemUrl(
-          profile, file_path, kFileManagerAppId, &url) ||
+          profile, path, kFileManagerAppId, &url) ||
       !GrantFileSystemAccessToFileBrowser(profile)) {
-    ShowWarningMessageBox(profile, file_path);
+    ShowWarningMessageBox(profile, path);
     return;
   }
 
@@ -221,12 +229,19 @@ void OpenItem(Profile* profile, const base::FilePath& file_path) {
           profile, kFileManagerAppId);
 
   CheckIfDirectoryExists(file_system_context, url,
-                         base::Bind(&ContinueOpenItem, profile, file_path));
+                         base::Bind(&ContinueOpenItem, profile, path));
 }
 
 void ShowItemInFolder(Profile* profile, const base::FilePath& file_path) {
+  // The path may have been stored in preferences in old versions.
+  // We need migration here.
+  // TODO(kinaba): crbug.com/313539 remove it in the future.
+  base::FilePath path;
+  if (!util::MigratePathFromOldFormat(profile, file_path, &path))
+    path = file_path;
+
   // This action changes the selection so we do not reuse existing tabs.
-  OpenFileManagerWithInternalActionId(profile, file_path, "select");
+  OpenFileManagerWithInternalActionId(profile, path, "select");
 }
 
 }  // namespace util
