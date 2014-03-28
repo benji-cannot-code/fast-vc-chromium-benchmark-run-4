5e918e59adc777d99349f4132826d5b11bc2ff18
vasilii@chromium.org <vasilii@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-28 13:25:23 +0000

    Settings Override API: localize all URLs.
    Currently the search provider can be localized. The CL adds localization support of homepage and startup page.
    
    BUG=267510
    R=kalman@chromium.org
    
    Review URL: https://codereview.chromium.org/213683006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260116 0039d316-1c4b-4281-b951-d872f2087c98

--- extensions/common/manifest_constants.cc
+++ extensions/common/manifest_constants.cc
@@ -93,6 +93,10 @@ const char kOmnibox[] = "omnibox";
 const char kOmniboxKeyword[] = "omnibox.keyword";
 const char kOptionalPermissions[] = "optional_permissions";
 const char kOptionsPage[] = "options_page";
+const char kOverrideHomepage[] = "chrome_settings_overrides.homepage";
+const char kOverrideSearchProvider[] =
+    "chrome_settings_overrides.search_provider";
+const char kOverrideStartupPage[] = "chrome_settings_overrides.startup_pages";
 const char kPageAction[] = "page_action";
 const char kPageActionDefaultIcon[] = "default_icon";
 const char kPageActionDefaultPopup[] = "default_popup";
@@ -120,7 +124,6 @@ const char kRunAt[] = "run_at";
 const char kSandboxedPages[] = "sandbox.pages";
 const char kSandboxedPagesCSP[] = "sandbox.content_security_policy";
 const char kScriptBadge[] = "script_badge";
-const char kSearchProvider[] = "chrome_settings_overrides.search_provider";
 const char kSettingsOverride[] = "chrome_settings_overrides";
 const char kSettingsOverrideAlternateUrls[] =
     "chrome_settings_overrides.search_provider.alternate_urls";
