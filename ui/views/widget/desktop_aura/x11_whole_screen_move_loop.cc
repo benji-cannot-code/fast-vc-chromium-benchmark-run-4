20d8fdaff406e3c162afba3370cd1db7c5923b7c
varkha@google.com <varkha@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-28 15:58:56 +0000

    Dispatches mouse movement messages in posted task to improve dragging smoothness
    BUG=353869
    TEST=Visual, Drag a bookmark over bookmark bar and bookmark bar folders.
    R=sadrul@chromium.org
    
    Review URL: https://codereview.chromium.org/214113003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260140 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/desktop_aura/x11_whole_screen_move_loop.cc
+++ ui/views/widget/desktop_aura/x11_whole_screen_move_loop.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Get rid of a macro from Xlib.h that conflicts with Aura's RootWindow class.
 #undef RootWindow
 
+#include "base/bind.h"
 #include "base/message_loop/message_loop.h"
 #include "base/message_loop/message_pump_x11.h"
 #include "base/run_loop.h"
@@ -58,13 +59,24 @@ X11WholeScreenMoveLoop::X11WholeScreenMoveLoop(
     : delegate_(delegate),
       in_move_loop_(false),
       should_reset_mouse_flags_(false),
-      grab_input_window_(None) {
+      grab_input_window_(None),
+      weak_factory_(this) {
+  last_xmotion_.type = LASTEvent;
 }
 
 X11WholeScreenMoveLoop::~X11WholeScreenMoveLoop() {}
 
 ////////////////////////////////////////////////////////////////////////////////
 // DesktopWindowTreeHostLinux, MessagePumpDispatcher implementation:
+
+void X11WholeScreenMoveLoop::DispatchMouseMovement() {
+  if (!weak_factory_.HasWeakPtrs())
+    return;
+  weak_factory_.InvalidateWeakPtrs();
+  DCHECK_EQ(MotionNotify, last_xmotion_.type);
+  delegate_->OnMouseMovement(&last_xmotion_);
+  last_xmotion_.type = LASTEvent;
+}
 
 uint32_t X11WholeScreenMoveLoop::Dispatch(const base::NativeEvent& event) {
   XEvent* xev = event;
@@ -80,13 +92,23 @@ uint32_t X11WholeScreenMoveLoop::Dispatch(const base::NativeEvent& event) {
         drag_widget_->SetBounds(gfx::Rect(location, drag_image_.size()));
         drag_widget_->StackAtTop();
       }
-      delegate_->OnMouseMovement(&xev->xmotion);
+      last_xmotion_ = xev->xmotion;
+      if (!weak_factory_.HasWeakPtrs()) {
+        // Post a task to dispatch mouse movement event when control returns to
+        // the message loop. This allows smoother dragging since the events are
+        // dispatched without waiting for the drag widget updates.
+        base::MessageLoopForUI::current()->PostTask(
+            FROM_HERE,
+            base::Bind(&X11WholeScreenMoveLoop::DispatchMouseMovement,
+                       weak_factory_.GetWeakPtr()));
+      }
       break;
     }
     case ButtonRelease: {
       if (xev->xbutton.button == Button1) {
         // Assume that drags are being done with the left mouse button. Only
         // break the drag if the left mouse button was released.
+        DispatchMouseMovement();
         delegate_->OnMouseReleased();
       }
       break;
@@ -163,6 +185,10 @@ void X11WholeScreenMoveLoop::UpdateCursor(gfx::NativeCursor cursor) {
 void X11WholeScreenMoveLoop::EndMoveLoop() {
   if (!in_move_loop_)
     return;
+
+  // Prevent DispatchMouseMovement from dispatching any posted motion event.
+  weak_factory_.InvalidateWeakPtrs();
+  last_xmotion_.type = LASTEvent;
 
   // We undo our emulated mouse click from RunMoveLoop();
   if (should_reset_mouse_flags_) {
--- ui/views/widget/desktop_aura/x11_whole_screen_move_loop.h
+++ ui/views/widget/desktop_aura/x11_whole_screen_move_loop.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/compiler_specific.h"
+#include "base/memory/weak_ptr.h"
 #include "base/message_loop/message_pump_dispatcher.h"
 #include "ui/gfx/image/image_skia.h"
 #include "ui/gfx/native_widget_types.h"
@@ -63,6 +64,9 @@ class X11WholeScreenMoveLoop : public base::MessagePumpDispatcher {
   // (defined as having a pixel with alpha > 32). If so, return true.
   bool CheckIfIconValid();
 
+  // Dispatch mouse movement event to |delegate_| in a posted task.
+  void DispatchMouseMovement();
+
   X11WholeScreenMoveLoopDelegate* delegate_;
 
   // Are we running a nested message loop from RunMoveLoop()?
@@ -82,6 +86,8 @@ class X11WholeScreenMoveLoop : public base::MessagePumpDispatcher {
   scoped_ptr<Widget> drag_widget_;
   gfx::ImageSkia drag_image_;
   gfx::Vector2dF drag_offset_;
+  XMotionEvent last_xmotion_;
+  base::WeakPtrFactory<X11WholeScreenMoveLoop> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(X11WholeScreenMoveLoop);
 };
