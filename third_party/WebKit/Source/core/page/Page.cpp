407607942fb3bf568096d9861c3a7136298e4d9b
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-28 02:16:31 +0000

    Revert of Revert of Make start/stop loading notifications per-frame (https://codereview.chromium.org/215533004/)
    Reason for revert:
    r170192 was not a culprit.
    
    
    Original issue's description:
    > Revert of Make start/stop loading notifications per-frame (https://codereview.chromium.org/183793002/)
    > 
    > Reason for revert:
    > Possibly causing content_browsertests EndToEnd to fail.
    > 
    > See: http://build.chromium.org/p/chromium.webkit/builders/Linux%20Tests%20%28dbg%29/builds/2095/steps/content_browsertests/logs/stdio
    > 
    > Output:
    > [20917:20917:0327/122353:3268961829:INFO:CONSOLE(0)] "Uncaught TypeError: Cannot convert undefined or null to object", source: http://127.0.0.1:34379/files/web_ui_mojo.html (0)
    > [20917:20917:0327/122353:3269022437:INFO:CONSOLE(28)] "Uncaught TypeError: undefined is not a function", source: http://127.0.0.1:34379/files/web_ui_mojo.js (28)
    > BrowserTestBase signal handler received SIGTERM. Backtrace:
    > #0 0x000000a593be base::debug::StackTrace::StackTrace()
    > #1 0x000002bfc17b content::(anonymous namespace)::DumpStackTraceSignalHandler()
    > #2 0x7f9c3e4ab4a0 <unknown>
    > #3 0x7f9c3e55da43 __poll
    > #4 0x7f9c42aabff6 <unknown>
    > #5 0x7f9c42aac124 g_main_context_iteration
    > #6 0x000000aaaf5f base::MessagePumpGlib::RunWithDispatcher()
    > #7 0x000000a8e802 base::RunLoop::Run()
    > #8 0x00000058256e content::(anonymous namespace)::WebUIMojoTest_EndToEnd_Test::RunTestOnMainThread()
    > 
    > Original issue's description:
    > > Make start/stop loading notifications per-frame
    > > 
    > > Also, make ProgressTracker a frame-level concept rather than a page-level concept and merge it with FrameLoader's FrameProgressTracker helper. Send per-frame progress change notifications accordingly.
    > > 
    > > This depends on https://codereview.chromium.org/180113003/ landing in chromium.
    > > 
    > > BUG=347643
    > > 
    > > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=170192
    > 
    > TBR=abarth@chromium.org,japhet@chromium.org
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=347643
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=170218
    
    TBR=abarth@chromium.org,japhet@chromium.org,enne@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=347643
    
    Review URL: https://codereview.chromium.org/216083002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170248 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/Page.cpp
+++ third_party/WebKit/Source/core/page/Page.cpp
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/inspector/InspectorInstrumentation.h"
 #include "core/loader/FrameLoader.h"
 #include "core/loader/HistoryItem.h"
-#include "core/loader/ProgressTracker.h"
 #include "core/page/AutoscrollController.h"
 #include "core/page/Chrome.h"
 #include "core/page/ChromeClient.h"
@@ -119,7 +118,6 @@ Page::Page(PageClients& pageClients)
     , m_inspectorController(InspectorController::create(this, pageClients.inspectorClient))
     , m_pointerLockController(PointerLockController::create(this))
     , m_historyController(adoptPtr(new HistoryController(this)))
-    , m_progress(ProgressTracker::create())
     , m_undoStack(UndoStack::create())
     , m_backForwardClient(pageClients.backForwardClient)
     , m_editorClient(pageClients.editorClient)
