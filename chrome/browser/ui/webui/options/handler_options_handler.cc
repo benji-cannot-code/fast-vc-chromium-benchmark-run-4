6a37350b922cdeabdb782cf57ecb59d409a7cfc2
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-29 00:56:30 +0000

    options: add a learn more link to "Handlers" content settings dialog.
    R=markusheintz@chromium.org
    BUG=356741
    
    Review URL: https://codereview.chromium.org/215273002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260325 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options/handler_options_handler.cc
+++ chrome/browser/ui/webui/options/handler_options_handler.cc
@@ -14,12 +14,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/custom_handlers/protocol_handler_registry_factory.h"
+#include "chrome/browser/google/google_util.h"
 #include "chrome/browser/profiles/profile.h"
 #include "content/public/browser/web_ui.h"
 #include "grit/generated_resources.h"
 #include "ui/base/l10n/l10n_util.h"
 
 namespace options {
+
+namespace {
+
+const char kHandlersLearnMoreUrl[] =
+    "https://support.google.com/chromebook/answer/1382847";
+
+}  // namespace
 
 HandlerOptionsHandler::HandlerOptionsHandler() {
 }
@@ -45,6 +53,10 @@ void HandlerOptionsHandler::GetLocalizedValues(
   RegisterTitle(localized_strings, "handlersPage",
                 IDS_HANDLER_OPTIONS_WINDOW_TITLE);
   RegisterStrings(localized_strings, resources, arraysize(resources));
+
+  localized_strings->SetString(
+      "handlers_learn_more_url",
+      google_util::StringAppendGoogleLocaleParam(kHandlersLearnMoreUrl));
 }
 
 void HandlerOptionsHandler::InitializeHandler() {
