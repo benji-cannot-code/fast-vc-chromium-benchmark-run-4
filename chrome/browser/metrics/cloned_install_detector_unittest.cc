9fae63b42e1fd8d4715b308668756dfbb6374e41
jwd@chromium.org <jwd@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-29 03:07:40 +0000

    Resetting metrics ids on clump detection.
    When a cloned install is detected the metrics client id and low entropy source are reset on the next start of Chrome. This is done by setting a pref in local state. The next time either the client id or low entropy source are loaded from the local state, they are first reset.
    
    BUG=343273
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/213363004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260348 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metrics/cloned_install_detector_unittest.cc
+++ chrome/browser/metrics/cloned_install_detector_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/prefs/testing_pref_service.h"
 #include "chrome/browser/metrics/machine_id_provider.h"
+#include "chrome/browser/metrics/metrics_service.h"
 #include "chrome/common/pref_names.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -20,7 +21,7 @@ const int kTestHashedId = 2216819;
 
 }  // namespace
 
-// TODO(jwd): Change this test to test the full flow and histogram outputs. It
+// TODO(jwd): Change these test to test the full flow and histogram outputs. It
 // should also remove the need to make the test a friend of
 // ClonedInstallDetector.
 TEST(ClonedInstallDetectorTest, SaveId) {
@@ -33,6 +34,22 @@ TEST(ClonedInstallDetectorTest, SaveId) {
   detector->SaveMachineId(&prefs, kTestRawId);
 
   EXPECT_EQ(kTestHashedId, prefs.GetInteger(prefs::kMetricsMachineId));
+}
+
+TEST(ClonedInstallDetectorTest, DetectClone) {
+  TestingPrefServiceSimple prefs;
+  ClonedInstallDetector::RegisterPrefs(prefs.registry());
+  MetricsService::RegisterPrefs(prefs.registry());
+
+  // Save a machine id that will cause a clone to be detected.
+  prefs.SetInteger(prefs::kMetricsMachineId, kTestHashedId + 1);
+
+  scoped_ptr<ClonedInstallDetector> detector(
+      new ClonedInstallDetector(MachineIdProvider::CreateInstance()));
+
+  detector->SaveMachineId(&prefs, kTestRawId);
+
+  EXPECT_TRUE(prefs.GetBoolean(prefs::kMetricsResetIds));
 }
 
 }  // namespace metrics
