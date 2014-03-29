fbf65bcbaf91b4e2e0f71d65d7c903d4576abc0c
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-29 00:43:51 +0000

    Break up with StyleResolveScope
    This class doesn't exist any more so we don't need to be friends.
    
    NOTRY=true
    TBR=eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/217603006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170394 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/resolver/StyleResolverState.h
+++ third_party/WebKit/Source/core/css/resolver/StyleResolverState.h
@@ -140,8 +140,6 @@ public:
     bool useSVGZoomRules() const { return element() && element()->isSVGElement(); }
 
 private:
-    friend class StyleResolveScope;
-
     ElementResolveContext m_elementContext;
     Document& m_document;
 
