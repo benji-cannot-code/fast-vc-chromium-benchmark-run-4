bc7c7a5858a58e8cdfde6a75a95da703f7e721e5
dnicoara@chromium.org <dnicoara@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-29 01:22:34 +0000

    [Ozone] Explicitly whilelist valid GL platforms and fail GL initialization otherwise
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/216433008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260330 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gl/gl_surface_ozone.cc
+++ ui/gl/gl_surface_ozone.cc
@@ -30,10 +30,14 @@ bool GLSurface::InitializeOneOffInternal() {
         LOG(ERROR) << "GLSurfaceEGL::InitializeOneOff failed.";
         return false;
       }
+
+      return true;
+    case kGLImplementationOSMesaGL:
+    case kGLImplementationMockGL:
+      return true;
     default:
-      break;
+      return false;
   }
-  return true;
 }
 
 // static
