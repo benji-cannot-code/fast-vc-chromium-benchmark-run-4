afb711628bb11cc9a115a07853e2538978496946
hirono@chromium.org <hirono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-30 17:21:43 +0000

    Files.app: Add a test to create new folder.
    BUG=345048
    TEST=run the new test
    
    Review URL: https://codereview.chromium.org/216513004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260444 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -759,7 +759,13 @@ INSTANTIATE_TEST_CASE_P(
                       TestParameter(NOT_IN_GUEST_MODE, "keyboardDeleteDrive"),
                       TestParameter(IN_GUEST_MODE, "keyboardCopyDownloads"),
                       TestParameter(NOT_IN_GUEST_MODE, "keyboardCopyDownloads"),
-                      TestParameter(NOT_IN_GUEST_MODE, "keyboardCopyDrive")));
+                      TestParameter(NOT_IN_GUEST_MODE, "keyboardCopyDrive"),
+                      TestParameter(IN_GUEST_MODE,
+                                    "createNewFolderDownloads"),
+                      TestParameter(NOT_IN_GUEST_MODE,
+                                    "createNewFolderDownloads"),
+                      TestParameter(NOT_IN_GUEST_MODE,
+                                    "createNewFolderDrive")));
 
 INSTANTIATE_TEST_CASE_P(
     DriveSpecific,
