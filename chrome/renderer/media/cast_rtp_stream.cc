224d8ac874a46d3017fded8bc084866758d28733
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-30 17:10:00 +0000

    Cast: Clarify the unit of bitrate
    This change is to clarify the unit used in cast.streaming APIs for
    bitrate. It should be in kilobits per second. And the unit conversion
    should be done just before converting to VideoSenderConfig and
    AudioSenderConfig which use bits per second as unit.
    
    This will improve the video quality for existing extensions which use
    kilobits per second.
    
    BUG=354267
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/214663008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260438 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/media/cast_rtp_stream.cc
+++ chrome/renderer/media/cast_rtp_stream.cc
@@ -35,6 +35,9 @@ namespace {
 const char kCodecNameOpus[] = "OPUS";
 const char kCodecNameVp8[] = "VP8";
 
+// To convert from kilobits per second to bits to per second.
+const int kBitrateMultiplier = 1000;
+
 // This constant defines the number of sets of audio data to buffer
 // in the FIFO. If input audio and output data have different resampling
 // rates then buffer is necessary to avoid audio glitches.
@@ -50,6 +53,7 @@ CastRtpPayloadParams DefaultOpusPayload() {
   payload.codec_name = kCodecNameOpus;
   payload.clock_rate = 48000;
   payload.channels = 2;
+  // The value is 0 which means VBR.
   payload.min_bitrate = payload.max_bitrate =
       media::cast::kDefaultAudioEncoderBitrate;
   return payload;
@@ -64,8 +68,8 @@ CastRtpPayloadParams DefaultVp8Payload() {
   payload.clock_rate = 90000;
   payload.width = 1280;
   payload.height = 720;
-  payload.min_bitrate = 50 * 1000;
-  payload.max_bitrate = 2000 * 1000;
+  payload.min_bitrate = 50;
+  payload.max_bitrate = 2000;
   return payload;
 }
 
@@ -91,7 +95,7 @@ bool ToAudioSenderConfig(const CastRtpParams& params,
   config->use_external_encoder = false;
   config->frequency = params.payload.clock_rate;
   config->channels = params.payload.channels;
-  config->bitrate = params.payload.max_bitrate;
+  config->bitrate = params.payload.max_bitrate * kBitrateMultiplier;
   config->codec = media::cast::transport::kPcm16;
   if (params.payload.codec_name == kCodecNameOpus)
     config->codec = media::cast::transport::kOpus;
@@ -108,8 +112,9 @@ bool ToVideoSenderConfig(const CastRtpParams& params,
   config->use_external_encoder = false;
   config->width = params.payload.width;
   config->height = params.payload.height;
-  config->min_bitrate = config->start_bitrate = params.payload.min_bitrate;
-  config->max_bitrate = params.payload.max_bitrate;
+  config->min_bitrate = config->start_bitrate =
+      params.payload.min_bitrate * kBitrateMultiplier;
+  config->max_bitrate = params.payload.max_bitrate * kBitrateMultiplier;
   if (params.payload.codec_name == kCodecNameVp8)
     config->codec = media::cast::transport::kVp8;
   else
