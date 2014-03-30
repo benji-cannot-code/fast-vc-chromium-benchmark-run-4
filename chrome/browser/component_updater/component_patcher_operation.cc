b7beb11c6972058f9fbfc3ba9827bd02e7c809e3
jschuh@chromium.org <jschuh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-30 18:27:30 +0000

    Fix post startup crash from stale IPC message
    NOTREECHECKS=true
    NOTRY=true
    NOPRESUBMIT=true
    BUG=357915
    R=cpu@chromium.org, waffles@chromium.org
    TBR=cpu@chromium.org, waffles@chromium.org
    
    Review URL: https://codereview.chromium.org/218693002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260457 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/component_updater/component_patcher_operation.cc
+++ chrome/browser/component_updater/component_patcher_operation.cc
@@ -292,7 +292,7 @@ void DeltaUpdateOpPatchHost::StartProcess(scoped_ptr<IPC::Message> message) {
   content::UtilityProcessHost* host = content::UtilityProcessHost::Create(
       this, base::MessageLoopProxy::current().get());
   host->DisableSandbox();
-  host->Send(message.get());
+  host->Send(message.release());
 }
 
 bool DeltaUpdateOpPatchHost::OnMessageReceived(const IPC::Message& message) {
