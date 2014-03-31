a2f9237c7cf83521ef1640d07fa57ac4c1030bcd
habib.virji@samsung.com <habib.virji@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-31 11:54:56 +0000

    Value set in onblur should not keep element in focus
    Value set in onblur was resulting in element still in focus due to reset
    of the layout called on setValue. This patch fixes on reset of layout to remove
    focus.
    
    R=tkent, keishi1
    BUG=347684
    TEST=Background color differ when on blur value is set.
    
    Review URL: https://codereview.chromium.org/191293011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170441 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/shadow/DateTimeFieldElement.cpp
+++ third_party/WebKit/Source/core/html/shadow/DateTimeFieldElement.cpp
@@ -55,12 +55,6 @@ DateTimeFieldElement::DateTimeFieldElement(Document& document, FieldOwner& field
 
 void DateTimeFieldElement::defaultEventHandler(Event* event)
 {
-    if (event->type() == EventTypeNames::blur)
-        didBlur();
-
-    if (event->type() == EventTypeNames::focus)
-        didFocus();
-
     if (event->isKeyboardEvent()) {
         KeyboardEvent* keyboardEvent = toKeyboardEvent(event);
         if (!isDisabled() && !isFieldOwnerDisabled() && !isFieldOwnerReadOnly()) {
@@ -130,16 +124,11 @@ void DateTimeFieldElement::defaultKeyboardEventHandler(KeyboardEvent* keyboardEv
     }
 }
 
-void DateTimeFieldElement::didBlur()
+void DateTimeFieldElement::setFocus(bool value)
 {
     if (m_fieldOwner)
-        m_fieldOwner->didBlurFromField();
-}
-
-void DateTimeFieldElement::didFocus()
-{
-    if (m_fieldOwner)
-        m_fieldOwner->didFocusOnField();
+        value ? m_fieldOwner->didFocusOnField() : m_fieldOwner->didBlurFromField();
+    ContainerNode::setFocus(value);
 }
 
 void DateTimeFieldElement::focusOnNextField()
