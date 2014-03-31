f161ac322ad1a8eb3f43da08a53df9cd75545290
jvoung@chromium.org <jvoung@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-31 21:33:23 +0000

    NaClSDK: Have "CONFIG=Debug TOOLCHAIN=pnacl make debug" use bitcode instead.
    Then the release and config modes are similar.
    The only difference is the -O level would have fewer
    optimized-out variables, etc.
    
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=3765
    NOTRY=true
    (nacl sdk only change)
    
    Review URL: https://codereview.chromium.org/217903002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260657 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/resources/common.js
+++ native_client_sdk/src/resources/common.js
@@ -40,7 +40,7 @@ var common = (function() {
         mimetype = 'application/x-ppapi-release';
       else
         mimetype = 'application/x-ppapi-debug';
-    } else if (tool == 'pnacl' && isRelease) {
+    } else if (tool == 'pnacl') {
       mimetype = 'application/x-pnacl';
     }
     return mimetype;
