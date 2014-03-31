57470f07f47fcef9ea76586b4da77f984978c368
philipj@opera.com <philipj@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-31 14:30:32 +0000

    Remove WebSetting::setFullScreenEnabled
    It is no longer called from Chromium:
    https://codereview.chromium.org/216233008/
    
    BUG=344002
    
    Review URL: https://codereview.chromium.org/217293002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170446 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/web/WebSettings.h
+++ third_party/WebKit/public/web/WebSettings.h
@@ -201,7 +201,6 @@ public:
 
     // FIXME: Remove these unused settings.
     virtual void setFixedPositionCreatesStackingContext(bool) { }
-    virtual void setFullScreenEnabled(bool) { }
 
 protected:
     ~WebSettings() { }
