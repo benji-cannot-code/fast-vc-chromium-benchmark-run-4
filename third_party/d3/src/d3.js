7700da5b8b1161f3ae5e5af5628721051236a84e
andrewhayden@chromium.org <andrewhayden@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-31 11:50:10 +0000

    Disable html() in d3 at the request of security, so that d3 can be used in web-facing tools.
    BUG=
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/216813002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260528 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/d3/src/d3.js
+++ third_party/d3/src/d3.js
@@ -714,6 +714,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     }) : this.node().textContent;
   };
   d3_selectionPrototype.html = function(value) {
+    throw "disallowed by chromium security";
     return arguments.length ? this.each(typeof value === "function" ? function() {
       var v = value.apply(this, arguments);
       this.innerHTML = v == null ? "" : v;
@@ -9275,9 +9276,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return JSON.parse(request.responseText);
   }
   d3.html = function(url, callback) {
+    throw "disallowed by chromium security";
     return d3_xhr(url, "text/html", d3_html, callback);
   };
   function d3_html(request) {
+    throw "disallowed by chromium security";
     var range = d3_document.createRange();
     range.selectNode(d3_document.body);
     return range.createContextualFragment(request.responseText);
