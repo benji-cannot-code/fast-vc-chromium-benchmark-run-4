60b8f3002c43a14240a8aac81c224c0ca7ca3b5f
bsalomon@google.com <bsalomon@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-31 16:35:56 +0000

    Wire glDiscardFramebuffer up to Skia.
    BUG=skia:2349
    
    Review URL: https://codereview.chromium.org/216833005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260562 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gl/gl_bindings_skia_in_process.cc
+++ ui/gl/gl_bindings_skia_in_process.cc
@@ -188,6 +188,11 @@ GLvoid StubGLDisableVertexAttribArray(GLuint index) {
   glDisableVertexAttribArray(index);
 }
 
+GLvoid StubGLDiscardFramebuffer(GLenum target, GLsizei numAttachments,
+                                const GLenum *attachments) {
+  glDiscardFramebufferEXT(target, numAttachments, attachments);
+}
+
 GLvoid StubGLDrawArrays(GLenum mode, GLint first, GLsizei count) {
   glDrawArrays(mode, first, count);
 }
@@ -632,6 +637,7 @@ GrGLInterface* CreateInProcessSkiaGLBinding() {
   functions->fDepthMask = StubGLDepthMask;
   functions->fDisable = StubGLDisable;
   functions->fDisableVertexAttribArray = StubGLDisableVertexAttribArray;
+  functions->fDiscardFramebuffer = StubGLDiscardFramebuffer;
   functions->fDrawArrays = StubGLDrawArrays;
   functions->fDrawBuffer = StubGLDrawBuffer;
   functions->fDrawBuffers = StubGLDrawBuffers;
