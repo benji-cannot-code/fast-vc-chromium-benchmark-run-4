9968d78192b231bc383647e5cf7ce75b26c76f2c
rafaelw@chromium.org <rafaelw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-31 17:15:38 +0000

    Remove defunct LSAN leak annotation in ensureDomInJSContext
    TBR=earthdok
    BUG=328552
    
    Review URL: https://codereview.chromium.org/219673002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170465 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/V8PerIsolateData.cpp
+++ third_party/WebKit/Source/bindings/v8/V8PerIsolateData.cpp
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/v8/V8ObjectConstructor.h"
 #include "bindings/v8/V8PerContextData.h"
 #include "bindings/v8/V8ScriptRunner.h"
-#include "wtf/LeakAnnotations.h"
 #include "wtf/MainThread.h"
 
 namespace WebCore {
@@ -130,11 +129,8 @@ void V8PerIsolateData::setDOMTemplate(void* domTemplateKey, v8::Handle<v8::Funct
 
 v8::Local<v8::Context> V8PerIsolateData::ensureDomInJSContext()
 {
-    if (!m_domInJSPerContextData) {
+    if (!m_domInJSPerContextData)
         m_domInJSPerContextData = V8PerContextData::create(v8::Context::New(m_isolate), DOMWrapperWorld::create());
-        // The V8PerContextData is collected via a weak reference callback from the V8Context, which is expected to not always shutdown cleanly.
-        WTF_ANNOTATE_LEAKING_OBJECT_PTR(m_domInJSPerContextData.get());
-    }
     return m_domInJSPerContextData->context();
 }
 
