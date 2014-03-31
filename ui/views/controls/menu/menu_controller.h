882ef7224b419207113c995deb9c406c7b6d97db
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-31 13:46:57 +0000

    views: Some MenuController cleanup.
    Notable changes:
     * Move code out of menu_controller_aura.cc into menu_controller.cc since it
       doesn't make sense to have separate files anymore.
     * Move the message-pump dispatching code out of MenuController into a separate
       MenuMessagePumpDispatcher object.
    
    BUG=343577
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/216423015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260540 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/menu/menu_controller.h
+++ ui/views/controls/menu/menu_controller.h
@@ -14,19 +14,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/memory/scoped_ptr.h"
-#include "base/message_loop/message_pump_dispatcher.h"
 #include "base/timer/timer.h"
 #include "ui/events/event_constants.h"
 #include "ui/views/controls/menu/menu_delegate.h"
 #include "ui/views/controls/menu/menu_item_view.h"
 #include "ui/views/widget/widget_observer.h"
 
-namespace ui {
-class NativeTheme;
-class OSExchangeData;
+namespace base {
+class MessagePumpDispatcher;
 }
 namespace gfx {
 class Screen;
+}
+namespace ui {
+class NativeTheme;
+class OSExchangeData;
 }
 namespace views {
 
@@ -38,6 +40,7 @@ class View;
 
 namespace internal {
 class MenuControllerDelegate;
+class MenuMessagePumpDispatcher;
 class MenuRunnerImpl;
 }
 
@@ -46,8 +49,7 @@ class MenuRunnerImpl;
 // MenuController is used internally by the various menu classes to manage
 // showing, selecting and drag/drop for menus. All relevant events are
 // forwarded to the MenuController from SubmenuView and MenuHost.
-class VIEWS_EXPORT MenuController : public base::MessagePumpDispatcher,
-                                    public WidgetObserver {
+class VIEWS_EXPORT MenuController : public WidgetObserver {
  public:
   // Enumeration of how the menu should exit.
   enum ExitType {
@@ -141,6 +143,7 @@ class VIEWS_EXPORT MenuController : public base::MessagePumpDispatcher,
   static void TurnOffMenuSelectionHoldForTest();
 
  private:
+  friend class internal::MenuMessagePumpDispatcher;
   friend class internal::MenuRunnerImpl;
   friend class MenuHostRootView;
   friend class MenuItemView;
@@ -251,10 +254,6 @@ class VIEWS_EXPORT MenuController : public base::MessagePumpDispatcher,
   void SetSelectionOnPointerDown(SubmenuView* source,
                                  const ui::LocatedEvent& event);
   void StartDrag(SubmenuView* source, const gfx::Point& location);
-
-  // Dispatcher method. This returns true if the menu was canceled, or
-  // if the message is such that the menu should be closed.
-  virtual uint32_t Dispatch(const base::NativeEvent& event) OVERRIDE;
 
   // Key processing. The return value of this is returned from Dispatch.
   // In other words, if this returns false (which happens if escape was
