4d8053df08b607aa8814069e4de7a8b3de406806
mkwst@chromium.org <mkwst@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-31 09:10:05 +0000

    Password bubble: Fix delete button alignment.
    Currently, each password's delete button is poorly aligned under Aura.
    This CL updates the view to use an ImageButton rather than a LabelButton,
    and pokes at the various bits of layout in order to make that choice make
    sense.
    
    BUG=261628
    
    Review URL: https://codereview.chromium.org/216863004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260508 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/passwords/manage_password_item_view.h
+++ chrome/browser/ui/views/passwords/manage_password_item_view.h
@@ -8,10 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/passwords/manage_passwords_bubble_view.h"
 #include "components/autofill/core/common/password_form.h"
-#include "ui/base/resource/resource_bundle.h"
 
 namespace views {
-class LabelButton;
+class ImageButton;
 }
 
 // A custom view for credentials which allows the management of the specific
@@ -51,7 +50,7 @@ class ManagePasswordItemView : public views::View,
 
   // This button is used to set |delete_password_| and to bring up the the undo
   // link in |label_2|.
-  views::LabelButton* delete_button_;
+  views::ImageButton* delete_button_;
 
   ManagePasswordsBubbleModel* manage_passwords_bubble_model_;
   autofill::PasswordForm password_form_;
--- chrome/browser/ui/views/passwords/manage_passwords_bubble_view.cc
+++ chrome/browser/ui/views/passwords/manage_passwords_bubble_view.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents_view.h"
 #include "grit/generated_resources.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/text_utils.h"
 #include "ui/views/controls/button/blue_button.h"
 #include "ui/views/controls/button/label_button.h"
