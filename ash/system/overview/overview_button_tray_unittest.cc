f811d8c17289a1031e9d438a1cd9b66135cf697d
jonross@chromium.org <jonross@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-31 17:08:27 +0000

    Overview Tray Button should not be visible on login screen Restrict OverviewButtonTray from showing on both the login screen as well as the locked screen.
    TEST=OverviewButtonTrayTest
    BUG=354493
    
    Review URL: https://codereview.chromium.org/212553005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260577 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/overview/overview_button_tray_unittest.cc
+++ ash/system/overview/overview_button_tray_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shelf/shelf_widget.h"
 #include "ash/shell.h"
 #include "ash/system/status_area_widget.h"
+#include "ash/system/user/login_status.h"
 #include "ash/test/ash_test_base.h"
 #include "ash/wm/overview/window_selector_controller.h"
 #include "base/time/time.h"
@@ -116,6 +117,20 @@ TEST_F(OverviewButtonTrayTest, SecondaryTrayCreatedVisible) {
   Shell::GetInstance()->EnableMaximizeModeWindowManager(true);
   UpdateDisplay("400x400,200x200");
   EXPECT_TRUE(GetSecondaryTray()->visible());
+  Shell::GetInstance()->EnableMaximizeModeWindowManager(false);
+}
+
+// Tests that the tray loses visibility when a user logs out, and that it
+// regains visibility when a user logs back in.
+TEST_F(OverviewButtonTrayTest, VisibilityChangesForLoginStatus) {
+  Shell::GetInstance()->EnableMaximizeModeWindowManager(true);
+  SetUserLoggedIn(false);
+  Shell::GetInstance()->UpdateAfterLoginStatusChange(user::LOGGED_IN_NONE);
+  EXPECT_FALSE(GetTray()->visible());
+  SetUserLoggedIn(true);
+  SetSessionStarted(true);
+  Shell::GetInstance()->UpdateAfterLoginStatusChange(user::LOGGED_IN_USER);
+  EXPECT_TRUE(GetTray()->visible());
   Shell::GetInstance()->EnableMaximizeModeWindowManager(false);
 }
 
