e0f7da54f94bd5f197c92ef08906fa479b9a3cbe
ltilve@igalia.com <ltilve@igalia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-31 23:52:29 +0000

    Remove unnecessary include at RenderTableSection after bug fixed
    Review URL: https://codereview.chromium.org/219023006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170487 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderTableSection.cpp
+++ third_party/WebKit/Source/core/rendering/RenderTableSection.cpp
@@ -27,8 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "core/rendering/RenderTableSection.h"
 
-// FIXME: Remove 'RuntimeEnabledFeatures.h' when http://crbug.com/78724 is closed.
-#include "RuntimeEnabledFeatures.h"
 #include <limits>
 #include "core/rendering/GraphicsContextAnnotator.h"
 #include "core/rendering/HitTestResult.h"
