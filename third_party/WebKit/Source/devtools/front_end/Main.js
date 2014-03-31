2a0b4691f64949eb8e149c2e3059aacd8e6cef78
dgozman@chromium.org <dgozman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-31 10:11:57 +0000

    Revert of [DevTools] Don't handle shortcuts while dialog is open. (https://codereview.chromium.org/214453003/)
    Reason for revert:
    We should still intercept browser shortcuts, even with dialog open.
    
    Original issue's description:
    > [DevTools] Don't handle shortcuts while dialog is open.
    > 
    > BUG=356616
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=170294
    
    TBR=apavlov@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=356616
    
    Review URL: https://codereview.chromium.org/219013004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170436 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/Main.js
+++ third_party/WebKit/Source/devtools/front_end/Main.js
@@ -521,8 +521,6 @@ WebInspector.Main.prototype = {
     {
         if (event.handled)
             return;
-        if (WebInspector.Dialog.currentInstance())
-            return;
 
         if (WebInspector.inspectorView.currentPanel()) {
             WebInspector.inspectorView.currentPanel().handleShortcut(event);
