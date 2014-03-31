0685b11614c62839df3eb8b75e04ba02841f87bf
nbarth@chromium.org <nbarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-31 09:37:06 +0000

    Move 'IDLExtendedAttributes.txt' filename to idl_validator.py
    Currently 'IDLExtendedAttributes.txt' is hard-coded 4 places:
    twice in .gyp, once in r-b-t (Tools/Scripts/webkitpy/bindings/main.py),
    once in an error message
    ...and then passed down several levels to idl_validator.py where
    it's actually used.
    This is brittle (because r-b-t has a distant dependence on a path)
    and verbose (we have yet another parameter to the compiler script and
    constructors).
    
    This moves the filename to idl_validator, reducing this to 2 places, not 4.
    This means:
    * r-b-t no longer depends on the file organization in bindings/,
    * we can simplify the parameter lists, and
    * the error message is up-to-date.
    
    We still have the filename in .gyp (in the 'inputs'), which ensures it's
    there and prevents build breakage.
    
    R=haraken
    BUG=341748
    
    Review URL: https://codereview.chromium.org/218903002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170435 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/generated_bindings.gyp
+++ third_party/WebKit/Source/bindings/generated_bindings.gyp
@@ -397,8 +397,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'scripts/idl_compiler.py',
         '--output-dir',
         '<(bindings_output_dir)',
-        '--idl-attributes-file',
-        'IDLExtendedAttributes.txt',
         '--interfaces-info',
         '<(SHARED_INTERMEDIATE_DIR)/blink/InterfacesInfo.pickle',
         '<@(write_file_only_if_changed)',
--- third_party/WebKit/Tools/Scripts/webkitpy/bindings/main.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/bindings/main.py
@@ -65,9 +65,6 @@ DEPENDENCY_IDL_FILES = frozenset([
 ])
 
 
-EXTENDED_ATTRIBUTES_FILE = os.path.join(source_path,
-                                        'bindings/IDLExtendedAttributes.txt')
-
 test_input_directory = os.path.join(source_path, 'bindings', 'tests', 'idls')
 reference_directory = os.path.join(source_path, 'bindings', 'tests', 'results')
 
@@ -172,7 +169,6 @@ def bindings_tests(output_directory, verbose):
     try:
         generate_interface_dependencies()
         idl_compiler = IdlCompilerV8(output_directory,
-                                     EXTENDED_ATTRIBUTES_FILE,
                                      interfaces_info=interfaces_info,
                                      only_if_changed=True)
 
