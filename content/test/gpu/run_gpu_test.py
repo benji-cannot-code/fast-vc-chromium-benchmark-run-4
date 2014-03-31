356933c6018cee224961db5cedbd2f76b209ac48
dtu@chromium.org <dtu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-31 21:32:42 +0000

    [telemetry] Plumb environment through test_runner. Remove page_test_runner.
    run_measurement is now almost an alias for run_benchmark.
    
    
    BUG=352799, 354432, 354610
    TEST=Make sure run_benchmark, run_measurement, and run_gpu_tests.py all have tests in their lists.
    
    Review URL: https://codereview.chromium.org/198143005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260656 0039d316-1c4b-4281-b951-d872f2087c98

--- content/test/gpu/run_gpu_test.py
+++ content/test/gpu/run_gpu_test.py
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright 2014 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+
 import os
 import sys
 
@@ -10,6 +11,10 @@ sys.path.append(os.path.join(os.path.dirname(__file__),
     os.pardir, os.pardir, os.pardir, 'tools', 'telemetry'))
 
 from telemetry import test_runner
+from telemetry.core import environment
+
 
 if __name__ == '__main__':
-  sys.exit(test_runner.Main())
+  base_dir = os.path.dirname(os.path.realpath(__file__))
+  test_runner.config = environment.Environment([base_dir])
+  sys.exit(test_runner.main())
--- tools/telemetry/telemetry/core/environment.py
+++ tools/telemetry/telemetry/core/environment.py
@@ -3,10 +3,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+
 class Environment(object):
-  def __init__(self, base_paths):
+  def __init__(self, base_paths, test_aliases=None):
     self._base_paths = base_paths
+    if test_aliases:
+      self._test_aliases = test_aliases
+    else:
+      self._test_aliases = {}
 
   @property
   def base_paths(self):
     return self._base_paths
+
+  @property
+  def test_aliases(self):
+    return self._test_aliases
