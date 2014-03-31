d4046ab4973fec51f84fd61a3b344f1252fb78b7
ed@opera.com <ed@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-31 10:16:10 +0000

    [svg] in img element doesn't render correctly when height is changed dynamically.
    BUG=354822
    
    Review URL: https://codereview.chromium.org/210083003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170437 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/graphics/SVGImage.cpp
+++ third_party/WebKit/Source/core/svg/graphics/SVGImage.cpp
@@ -279,6 +279,7 @@ void SVGImage::draw(GraphicsContext* context, const FloatRect& dstRect, const Fl
         view->layout();
 
     view->paint(context, enclosingIntRect(srcRect));
+    ASSERT(!view->needsLayout());
 
     if (requiresTransparencyLayer)
         context->endLayer();
