4bc3250fc85f9d58a0591498667f87028988bd59
bartfab@chromium.org <bartfab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-31 20:14:14 +0000

    Do not instantiate DriveNotificationManager during shutdown
    The DriveIntegrationService::Shutdown() method was inadvertently
    instantiating a DriveNotificationManager during shutdown (such as when
    starting a guest session or powering down while the login screen is
    shown). This CL ensures that a DriveNotificationManager is instantiated
    only when actually needed.
    
    BUG=356806
    TEST=Manual
    
    Review URL: https://codereview.chromium.org/213483002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260625 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/drive/drive_notification_manager_factory.cc
+++ chrome/browser/drive/drive_notification_manager_factory.cc
@@ -16,6 +16,14 @@ namespace drive {
 
 // static
 DriveNotificationManager*
+DriveNotificationManagerFactory::FindForBrowserContext(
+    content::BrowserContext* context) {
+  return static_cast<DriveNotificationManager*>(
+      GetInstance()->GetServiceForBrowserContext(context, false));
+}
+
+// static
+DriveNotificationManager*
 DriveNotificationManagerFactory::GetForBrowserContext(
     content::BrowserContext* context) {
   if (!ProfileSyncService::IsSyncEnabled())
--- chrome/browser/sync_file_system/drive_backend_v1/drive_file_sync_service.cc
+++ chrome/browser/sync_file_system/drive_backend_v1/drive_file_sync_service.cc
@@ -85,7 +85,7 @@ DriveFileSyncService::~DriveFileSyncService() {
     api_util_->RemoveObserver(this);
 
   drive::DriveNotificationManager* drive_notification_manager =
-      drive::DriveNotificationManagerFactory::GetForBrowserContext(profile_);
+      drive::DriveNotificationManagerFactory::FindForBrowserContext(profile_);
   if (drive_notification_manager)
     drive_notification_manager->RemoveObserver(this);
 }
