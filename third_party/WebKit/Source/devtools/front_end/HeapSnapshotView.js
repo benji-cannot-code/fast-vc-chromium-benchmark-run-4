cdeebd91919cd47a17d5a4dcc8c40bbd1d8b046d
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-31 15:50:57 +0000

    Implement view port for allocation view
    BUG=277984
    
    Review URL: https://codereview.chromium.org/218933003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170455 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/HeapSnapshotView.js
+++ third_party/WebKit/Source/devtools/front_end/HeapSnapshotView.js
@@ -772,6 +772,7 @@ WebInspector.HeapSnapshotView.prototype = {
     {
         var selectedNode = event.target.selectedNode;
         this._constructorsDataGrid.setAllocationNodeId(selectedNode.allocationNodeId());
+        this._setRetainmentDataGridSource(null);
     },
 
     _inspectedObjectChanged: function(event)
