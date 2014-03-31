c02de661618fec2bc025c49f0bae71d7eb01910a
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-31 16:56:28 +0000

    Use GURL(WebString) instead of GURL(WebString.utf8()).
    R=jam@chromium.org
    
    Review URL: https://codereview.chromium.org/214973003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260572 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/content_settings_observer.cc
+++ chrome/renderer/content_settings_observer.cc
@@ -379,7 +379,7 @@ bool ContentSettingsObserver::allowReadFromClipboard(WebFrame* frame,
   bool allowed = false;
   // TODO(dcheng): Should we consider a toURL() method on WebSecurityOrigin?
   Send(new ChromeViewHostMsg_CanTriggerClipboardRead(
-      GURL(frame->document().securityOrigin().toString().utf8()), &allowed));
+      GURL(frame->document().securityOrigin().toString()), &allowed));
   return allowed;
 }
 
@@ -387,7 +387,7 @@ bool ContentSettingsObserver::allowWriteToClipboard(WebFrame* frame,
                                                     bool default_value) {
   bool allowed = false;
   Send(new ChromeViewHostMsg_CanTriggerClipboardWrite(
-      GURL(frame->document().securityOrigin().toString().utf8()), &allowed));
+      GURL(frame->document().securityOrigin().toString()), &allowed));
   return allowed;
 }
 
