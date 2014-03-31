fda45a6b8cb39b724e6c3dde1987b87df4cf17b3
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-31 20:33:34 +0000

    Change AckEvent into request / response
    BUG=None
    
    Review URL: https://codereview.chromium.org/218763002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260635 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/examples/aura_demo/window_tree_host_mojo.cc
+++ mojo/examples/aura_demo/window_tree_host_mojo.cc
@@ -167,10 +167,8 @@ void WindowTreeHostMojo::OnDestroyed() {
   base::MessageLoop::current()->Quit();
 }
 
-void WindowTreeHostMojo::OnEvent(const Event& event) {
-  if (!event.location().is_null())
-    native_viewport_->AckEvent(event);
-
+void WindowTreeHostMojo::OnEvent(const Event& event,
+                                 const mojo::Callback<void()>& callback) {
   switch (event.action()) {
     case ui::ET_MOUSE_PRESSED:
     case ui::ET_MOUSE_DRAGGED:
@@ -195,6 +193,7 @@ void WindowTreeHostMojo::OnEvent(const Event& event) {
     }
     // TODO(beng): touch, etc.
   }
+  callback.Run();
 };
 
 }  // namespace examples
