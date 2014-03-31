44d45643cfd67245b36d96ee10696df5cb7e878c
noamsml@chromium.org <noamsml@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-31 23:15:19 +0000

    Update PWG Raster encoder to latest version and add transformation
    1. Update PWG raster encoder to latest version ported from GCP code
    2. This new version adds the ability to flip the input while encoding,
    use this to implement the transformations specified in PwgRasterConfig.
    
    BUG=354605
    
    Review URL: https://codereview.chromium.org/211783004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260694 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/utility/cloud_print/bitmap_image.h
+++ chrome/utility/cloud_print/bitmap_image.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_UTILITY_CLOUD_PRINT_BITMAP_IMAGE_H_
 
 #include "base/memory/scoped_ptr.h"
+#include "ui/gfx/point.h"
 #include "ui/gfx/size.h"
 
 namespace cloud_print {
@@ -28,6 +29,8 @@ class BitmapImage {
 
   const uint8* pixel_data() const { return data_.get(); }
   uint8* pixel_data() { return data_.get(); }
+
+  const uint8* GetPixel(const gfx::Point& point) const;
 
  private:
   gfx::Size size_;
