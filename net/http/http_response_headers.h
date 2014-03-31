1db32169cfacd2a4d2a44031dd286c04b4da71bd
bengr@chromium.org <bengr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-31 17:54:55 +0000

    Fix UMA reporting of data reduction proxy bypass reasons
    5xx responses from the proxy that should induce proxy bypass were previously reported as bypassing due to a missing via header value, because such responses often also are missing that header value. This change fixes that reporting.
    
    BUG=356445
    
    Review URL: https://codereview.chromium.org/212873004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260598 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_response_headers.h
+++ net/http/http_response_headers.h
@@ -17,6 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_log.h"
 #include "net/http/http_version.h"
 
+#if defined(SPDY_PROXY_AUTH_ORIGIN)
+#include "net/proxy/proxy_service.h"
+#endif
+
 class Pickle;
 class PickleIterator;
 
@@ -287,6 +291,12 @@ class NET_EXPORT HttpResponseHeaders
 
   // Returns true if response headers contain the Chrome proxy Via header value.
   bool IsChromeProxyResponse() const;
+
+  // Returns the reason why the Chrome proxy should be bypassed or not, and
+  // populates |proxy_info| with information on how long to bypass if
+  // applicable.
+  ProxyService::DataReductionProxyBypassEventType
+  GetChromeProxyBypassEventType(ChromeProxyInfo* proxy_info) const;
 #endif
 
   // Creates a Value for use with the NetLog containing the response headers.
