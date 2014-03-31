70b3e2b0da316d517233ae1445710648dded8296
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-31 19:54:15 +0000

    Add VP9 encode support to the remoting host.
    BUG=260879
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=255208
    
    Review URL: https://codereview.chromium.org/28183002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260618 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/codec/video_encoder_vpx.h
+++ remoting/codec/video_encoder_vpx.h
@@ -23,6 +23,7 @@ class VideoEncoderVpx : public VideoEncoder {
  public:
   // Create encoder for the specified protocol.
   static scoped_ptr<VideoEncoderVpx> CreateForVP8();
+  static scoped_ptr<VideoEncoderVpx> CreateForVP9();
 
   virtual ~VideoEncoderVpx();
 
