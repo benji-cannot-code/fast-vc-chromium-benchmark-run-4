e439c1f7ab653c3b19b1c90f62ffea5d224e65f0
ttuttle@chromium.org <ttuttle@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-31 22:46:24 +0000

    HttpAuthCache: add eviction histograms.
    Add histograms to measure how often realms and paths are evicted from
    the HTTP auth cache, along with the time since creation and last use
    for evicted realms.
    
    BUG=354187
    TEST=HttpAuthCacheTest.* in net_unittests still passes
    
    Review URL: https://codereview.chromium.org/205673003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260685 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_auth_cache.h
+++ net/http/http_auth_cache.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/gtest_prod_util.h"
 #include "base/memory/ref_counted.h"
+#include "base/time/time.h"
 #include "net/base/net_export.h"
 #include "net/http/http_auth.h"
 #include "url/gurl.h"
@@ -97,6 +98,11 @@ class NET_EXPORT_PRIVATE HttpAuthCache {
 
     // List of paths that define the realm's protection space.
     PathList paths_;
+
+    // Times the entry was created and last used (by looking up, adding a path,
+    // or updating the challenge.)
+    base::TimeTicks creation_time_;
+    base::TimeTicks last_use_time_;
   };
 
   // Prevent unbounded memory growth. These are safeguards for abuse; it is
