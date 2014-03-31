155ab93c89e1a928f9b95b20f59307dfc19ed642
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-31 23:59:16 +0000

    Add an unimplemented history API to WebView
    This API will function as a temporary shim to enable
    history functionality to move to content/renderer, without
    any broken intermediate steps.
    
    This is step 1 of 5 from http://crbug.com/357327
    
    BUG=357327
    
    Review URL: https://codereview.chromium.org/214723003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170488 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/web/WebFrameClient.h
+++ third_party/WebKit/public/web/WebFrameClient.h
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebDOMMessageEvent.h"
 #include "WebDataSource.h"
 #include "WebFrame.h"
+#include "WebHistoryItem.h"
 #include "WebIconURL.h"
 #include "WebNavigationPolicy.h"
 #include "WebNavigationType.h"
@@ -164,6 +165,10 @@ public:
     virtual WebNavigationPolicy decidePolicyForNavigation(
         WebLocalFrame*, WebDataSource::ExtraData*, const WebURLRequest&, WebNavigationType,
         WebNavigationPolicy defaultPolicy, bool isRedirect) { return defaultPolicy; }
+
+    // During a history navigation, we may choose to load new subframes from history as well.
+    // This returns such a history item if appropriate.
+    virtual WebHistoryItem historyItemForNewChildFrame(WebFrame*) { return WebHistoryItem(); }
 
 
     // Navigational notifications ------------------------------------------
