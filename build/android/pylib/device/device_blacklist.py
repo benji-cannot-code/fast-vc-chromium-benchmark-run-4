038b918e43ffe54438d5eb2ee006a9a427cadabb
jbudorick@chromium.org <jbudorick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-31 21:12:37 +0000

    [Android] Extract device blacklisting into its own module.
    BUG=267773
    
    Review URL: https://codereview.chromium.org/204353007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260645 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ build/android/pylib/device/device_blacklist.py
@@ -0,0 +1,53 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import json
+import os
+
+from pylib import constants
+_BLACKLIST_JSON = os.path.join(
+    constants.DIR_SOURCE_ROOT,
+    os.environ.get('CHROMIUM_OUT_DIR', 'out'),
+    'bad_devices.json')
+
+def ReadBlacklist():
+  """Reads the blacklist from the _BLACKLIST_JSON file.
+
+  Returns:
+    A list containing bad devices.
+  """
+  if not os.path.exists(_BLACKLIST_JSON):
+    return []
+
+  with open(_BLACKLIST_JSON, 'r') as f:
+    return json.load(f)
+
+
+def WriteBlacklist(blacklist):
+  """Writes the provided blacklist to the _BLACKLIST_JSON file.
+
+  Args:
+    blacklist: list of bad devices to write to the _BLACKLIST_JSON file.
+  """
+  with open(_BLACKLIST_JSON, 'w') as f:
+    json.dump(list(set(blacklist)), f)
+
+
+def ExtendBlacklist(devices):
+  """Adds devices to _BLACKLIST_JSON file.
+
+  Args:
+    devices: list of bad devices to be added to the _BLACKLIST_JSON file.
+  """
+  blacklist = ReadBlacklist()
+  blacklist.extend(devices)
+  WriteBlacklist(blacklist)
+
+
+def ResetBlacklist():
+  """Erases the _BLACKLIST_JSON file if it exists."""
+  if os.path.exists(_BLACKLIST_JSON):
+    os.remove(_BLACKLIST_JSON)
+
