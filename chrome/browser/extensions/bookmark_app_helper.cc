6db36885ff8edca58d27058f7987609d2a0c3f75
calamity@chromium.org <calamity@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-01 09:43:30 +0000

    Add a function to create a bookmark app from a WebApplicationInfo.
    This CL adds a function which installs a bookmark app that is created
    from a WebApplicationInfo. This is part of an effort to consolidate
    code that deals with bookmark apps and write tests for them and is part of
    laying the groundwork for syncing of bookmark apps.
    
    BUG=318607
    
    Review URL: https://codereview.chromium.org/202523011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260823 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/bookmark_app_helper.cc
+++ chrome/browser/extensions/bookmark_app_helper.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/tab_helper.h"
 #include "chrome/common/extensions/extension_constants.h"
 #include "chrome/common/extensions/manifest_handlers/app_launch_info.h"
+#include "chrome/common/extensions/manifest_handlers/icons_handler.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/notification_source.h"
 #include "content/public/browser/web_contents.h"
@@ -281,6 +282,14 @@ void BookmarkAppHelper::Observe(int type,
       NOTREACHED();
       break;
   }
+}
+
+void CreateOrUpdateBookmarkApp(ExtensionService* service,
+                               WebApplicationInfo& web_app_info) {
+  scoped_refptr<extensions::CrxInstaller> installer(
+      extensions::CrxInstaller::CreateSilent(service));
+  installer->set_error_on_unsupported_requirements(true);
+  installer->InstallWebApp(web_app_info);
 }
 
 }  // namespace extensions
