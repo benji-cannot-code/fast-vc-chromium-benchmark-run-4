f3c102b0b0a59782485f98fb3d6ec514c88af743
fs@opera.com <fs@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-01 11:52:34 +0000

    Replace NativeImageSkia usage with SkBitmap in ImageFrame
    Make ImageFrame store an SkBitmap directly instead of indirectly via
    NativeImageSkia.
    The direct effect is less indirections in ImageFrame, but this allows
    making NativeImageSkia more immutable-looking since the SkBitmap&
    accessor and setDataComplete() method can be removed.
    
    Review URL: https://codereview.chromium.org/170463002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170553 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/image-decoders/ImageFrame.cpp
+++ third_party/WebKit/Source/platform/image-decoders/ImageFrame.cpp
@@ -31,8 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 ImageFrame::ImageFrame()
-    : m_bitmap(NativeImageSkia::create())
-    , m_allocator(0)
+    : m_allocator(0)
     , m_hasAlpha(false)
     , m_status(FrameEmpty)
     , m_duration(0)
@@ -52,10 +51,10 @@ ImageFrame& ImageFrame::operator=(const ImageFrame& other)
     if (this == &other)
         return *this;
 
-    m_bitmap = other.m_bitmap->clone();
+    m_bitmap = other.m_bitmap;
     // Keep the pixels locked since we will be writing directly into the
     // bitmap throughout this object's lifetime.
-    m_bitmap->bitmap().lockPixels();
+    m_bitmap.lockPixels();
     // Be sure to assign this before calling setStatus(), since setStatus() may
     // call notifyBitmapIfPixelsChanged().
     m_pixelsChanged = other.m_pixelsChanged;
@@ -79,7 +78,7 @@ ImageFrame& ImageFrame::operator=(const ImageFrame& other)
 
 void ImageFrame::clearPixelData()
 {
-    m_bitmap->bitmap().reset();
+    m_bitmap.reset();
     m_status = FrameEmpty;
     // NOTE: Do not reset other members here; clearFrameBufferCache()
     // calls this to free the bitmap data, but other functions like
@@ -89,7 +88,7 @@ void ImageFrame::clearPixelData()
 
 void ImageFrame::zeroFillPixelData()
 {
-    m_bitmap->bitmap().eraseARGB(0, 0, 0, 0);
+    m_bitmap.eraseARGB(0, 0, 0, 0);
     m_hasAlpha = true;
 }
 
@@ -99,9 +98,8 @@ bool ImageFrame::copyBitmapData(const ImageFrame& other)
         return true;
 
     m_hasAlpha = other.m_hasAlpha;
-    m_bitmap->bitmap().reset();
-    const NativeImageSkia* otherBitmap = other.m_bitmap.get();
-    return otherBitmap->bitmap().copyTo(&m_bitmap->bitmap(), otherBitmap->bitmap().colorType());
+    m_bitmap.reset();
+    return other.m_bitmap.copyTo(&m_bitmap, other.m_bitmap.colorType());
 }
 
 bool ImageFrame::setSize(int newWidth, int newHeight)
@@ -109,8 +107,8 @@ bool ImageFrame::setSize(int newWidth, int newHeight)
     // setSize() should only be called once, it leaks memory otherwise.
     ASSERT(!width() && !height());
 
-    m_bitmap->bitmap().setConfig(SkImageInfo::MakeN32Premul(newWidth, newHeight));
-    if (!m_bitmap->bitmap().allocPixels(m_allocator, 0))
+    m_bitmap.setConfig(SkImageInfo::MakeN32Premul(newWidth, newHeight));
+    if (!m_bitmap.allocPixels(m_allocator, 0))
         return false;
 
     zeroFillPixelData();
@@ -119,7 +117,7 @@ bool ImageFrame::setSize(int newWidth, int newHeight)
 
 PassRefPtr<NativeImageSkia> ImageFrame::asNewNativeImage() const
 {
-    return m_bitmap->clone();
+    return NativeImageSkia::create(m_bitmap);
 }
 
 bool ImageFrame::hasAlpha() const
@@ -136,18 +134,18 @@ void ImageFrame::setHasAlpha(bool alpha)
     // always are (e.g. jpeg).
     if (m_status != FrameComplete)
         alpha = true;
-    m_bitmap->bitmap().setAlphaType(alpha ? kPremul_SkAlphaType : kOpaque_SkAlphaType);
+    m_bitmap.setAlphaType(alpha ? kPremul_SkAlphaType : kOpaque_SkAlphaType);
 }
 
 void ImageFrame::setStatus(Status status)
 {
     m_status = status;
     if (m_status == FrameComplete) {
-        m_bitmap->bitmap().setAlphaType(m_hasAlpha ? kPremul_SkAlphaType : kOpaque_SkAlphaType);
+        m_bitmap.setAlphaType(m_hasAlpha ? kPremul_SkAlphaType : kOpaque_SkAlphaType);
         // Send pending pixels changed notifications now, because we can't do this after
-        // the bitmap was set immutable by setDataComplete().
+        // the bitmap has been marked immutable.
         notifyBitmapIfPixelsChanged();
-        m_bitmap->setDataComplete(); // Tell the bitmap it's done.
+        m_bitmap.setImmutable(); // Tell the bitmap it's done.
     }
 }
 
@@ -156,7 +154,7 @@ void ImageFrame::zeroFillFrameRect(const IntRect& rect)
     if (rect.isEmpty())
         return;
 
-    m_bitmap->bitmap().eraseArea(rect, SkColorSetARGB(0, 0, 0, 0));
+    m_bitmap.eraseArea(rect, SkColorSetARGB(0, 0, 0, 0));
     setHasAlpha(true);
 }
 
