104080a68eeaf4630fbf65126c0c9a845cba415c
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-01 01:15:53 +0000

    Revert 260678 "Make http range requests by plugins also go direc..."
    Causing mac debug bot assert.
    
    > Make http range requests by plugins also go directly to the browser process instead of going through the renderers.
    > 
    > BUG=286074
    > R=ananta@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/217593003
    
    TBR=jam@chromium.org
    
    Review URL: https://codereview.chromium.org/220323003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260725 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/plugin_process_host.cc
+++ content/browser/plugin_process_host.cc
@@ -206,9 +206,15 @@ bool PluginProcessHost::Init(const WebPluginInfo& info) {
   // any associated values) if present in the browser command line
   static const char* const kSwitchNames[] = {
     switches::kDisableBreakpad,
-    switches::kDisableDirectNPAPIRequests,
+#if defined(OS_MACOSX)
+    switches::kDisableCoreAnimationPlugins,
+    switches::kEnableSandboxLogging,
+#endif
     switches::kEnableStatsTable,
     switches::kFullMemoryCrashReport,
+#if defined(OS_WIN)
+    switches::kHighDPISupport,
+#endif
     switches::kLoggingLevel,
     switches::kLogPluginMessages,
     switches::kNoSandbox,
@@ -216,13 +222,6 @@ bool PluginProcessHost::Init(const WebPluginInfo& info) {
     switches::kTestSandbox,
     switches::kTraceStartup,
     switches::kUseGL,
-#if defined(OS_MACOSX)
-    switches::kDisableCoreAnimationPlugins,
-    switches::kEnableSandboxLogging,
-#endif
-#if defined(OS_WIN)
-    switches::kHighDPISupport,
-#endif
   };
 
   cmd_line->CopySwitchesFrom(browser_command_line, kSwitchNames,
