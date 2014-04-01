ee140348b208e9c1e63812988de34cf95a819f83
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-01 18:37:27 +0000

    Remove JavaScript execution from RenderViewHost.
    BUG=304341, 316203
    TEST=no change
    TBR=tsepez@chromium.org
    
    Review URL: https://codereview.chromium.org/208793005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260910 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_view_impl.h
+++ content/renderer/render_view_impl.h
@@ -851,11 +851,6 @@ class CONTENT_EXPORT RenderViewImpl
   // Called when the "pinned to left/right edge" state needs to be updated.
   void UpdateScrollState(blink::WebFrame* frame);
 
-  void EvaluateScript(const base::string16& frame_xpath,
-                      const base::string16& jscript,
-                      int id,
-                      bool notify_result);
-
   // IPC message handlers ------------------------------------------------------
   //
   // The documentation for these functions should be in
@@ -919,10 +914,6 @@ class CONTENT_EXPORT RenderViewImpl
   void OnPostMessageEvent(const ViewMsg_PostMessage_Params& params);
   void OnReleaseDisambiguationPopupDIB(TransportDIB::Handle dib_handle);
   void OnResetPageEncodingToDefault();
-  void OnScriptEvalRequest(const base::string16& frame_xpath,
-                           const base::string16& jscript,
-                           int id,
-                           bool notify_result);
   void OnSetAccessibilityMode(AccessibilityMode new_mode);
   void OnSetActive(bool active);
   void OnSetBackground(const SkBitmap& background);
