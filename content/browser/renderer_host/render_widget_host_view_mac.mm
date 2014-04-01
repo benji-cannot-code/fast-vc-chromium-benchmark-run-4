f5b869fbc19a7642b9b39b9f52510a018cee20c5
ccameron@chromium.org <ccameron@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-01 01:39:50 +0000

    Fix crash in Mac RenderWidgetHostImpl::GetBackingStore
    We post a delayed task to RenderWidgetHostViewMac::GotSoftwareFrame and
    need to make sure that the corresponding RenderWidgetHostImpl has not
    been destroyed.
    
    BUG=357574
    
    Review URL: https://codereview.chromium.org/217423009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260735 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_view_mac.mm
+++ content/browser/renderer_host/render_widget_host_view_mac.mm
@@ -1959,6 +1959,9 @@ void RenderWidgetHostViewMac::GotAcceleratedFrame() {
 }
 
 void RenderWidgetHostViewMac::GotSoftwareFrame() {
+  if (!render_widget_host_)
+    return;
+
   EnsureSoftwareLayer();
   LayoutLayers();
   SendVSyncParametersToRenderer();
