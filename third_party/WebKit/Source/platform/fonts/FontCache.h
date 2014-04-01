fc5e3ddb1774be31d56264bb023da29cb3654d09
zvorygin@chromium.org <zvorygin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-01 10:36:05 +0000

    Removed unused includes from UnicodeIcu.h
    Speeds up blink compilation ~ 1.5%
    
    BUG=
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=170466
    
    Review URL: https://codereview.chromium.org/218583007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170542 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/fonts/FontCache.h
+++ third_party/WebKit/Source/platform/fonts/FontCache.h
@@ -48,6 +48,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <mlang.h>
 #endif
 
+#if OS(ANDROID)
+#include <unicode/uscript.h>
+#endif
+
 class SkTypeface;
 
 namespace WebCore {
--- third_party/WebKit/Source/platform/fonts/harfbuzz/HarfBuzzShaper.h
+++ third_party/WebKit/Source/platform/fonts/harfbuzz/HarfBuzzShaper.h
@@ -43,6 +43,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/unicode/CharacterNames.h"
 #include "wtf/Vector.h"
 
+#include <unicode/uscript.h>
+
 namespace WebCore {
 
 class Font;
