9c4e85ced3c79c196d51db0deed3f1411e84bb79
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-01 15:55:26 +0000

    DevTools: disable CSS agent on disconnect.
    BUG=358534
    R=apavlov@chromium.org
    
    Review URL: https://codereview.chromium.org/221033003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170577 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorCSSAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorCSSAgent.cpp
@@ -412,6 +412,8 @@ void InspectorCSSAgent::setFrontend(InspectorFrontend* frontend)
 void InspectorCSSAgent::clearFrontend()
 {
     ASSERT(m_frontend);
+    ErrorString error;
+    disable(&error);
     m_frontend = 0;
     resetNonPersistentData();
 }
@@ -508,6 +510,7 @@ void InspectorCSSAgent::wasEnabled(PassRefPtr<EnableCallback> callback)
 
 void InspectorCSSAgent::disable(ErrorString*)
 {
+    reset();
     m_instrumentingAgents->setInspectorCSSAgent(0);
     m_state->setBoolean(CSSAgentState::cssAgentEnabled, false);
 }
