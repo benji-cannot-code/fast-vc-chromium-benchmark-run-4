982aed19b61f26a2146bc7ed5fbae0e39482db74
dstockwell@chromium.org <dstockwell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-01 06:26:53 +0000

    CSS Transforms: Implement perspective
    Implements perspective as defined in CSS Transforms[1]. Behavior is
    similar to -webkit-perspective, but numbers without units are not
    valid. In both variants we accept zero although Gecko and spec
    require a positive value.
    
    The animation system is modified to avoid running animations and
    transitions on both the prefixed and unprefixed variants at the same
    time. Internally, animations are mapped to the unprefixed variant.
    As animations operate directly on the RenderStyle storage the result
    of specifying an animation/transition on either property is the same.
    
    Transitions now track the specified transition-property such that
    the end event may reference it. (eg. transition:
    -webkit-perspective; will fire an end  event with
    propertyName == '-webkit-perspective'). As transitions operate on
    changes to values within a RenderStyle, 'transition: all;' only
    ever starts transitions on the unprefixed variants.
    
    Note, this property is guarded by the CSSTransformsUnprefixed runtime
    feature (enabled only for testing).
    
    [1] http://www.w3.org/TR/css3-transforms/
    
    BUG=154772
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=170326
    
    Review URL: https://codereview.chromium.org/211233003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170517 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/resolver/StyleBuilderCustom.cpp
+++ third_party/WebKit/Source/core/css/resolver/StyleBuilderCustom.cpp
@@ -1430,6 +1430,7 @@ void StyleBuilder::oldApplyProperty(CSSPropertyID id, StyleResolverState& state,
         state.style()->setTransform(operations);
         return;
     }
+    case CSSPropertyPerspective:
     case CSSPropertyWebkitPerspective: {
         HANDLE_INHERIT_AND_INITIAL(perspective, Perspective)
 
@@ -1444,8 +1445,8 @@ void StyleBuilder::oldApplyProperty(CSSPropertyID id, StyleResolverState& state,
         float perspectiveValue;
         if (primitiveValue->isLength()) {
             perspectiveValue = primitiveValue->computeLength<float>(state.cssToLengthConversionData());
-        } else if (primitiveValue->isNumber()) {
-            // For backward compatibility, treat valueless numbers as px.
+        } else if (id == CSSPropertyWebkitPerspective && primitiveValue->isNumber()) {
+            // Prefixed version treats unitless numbers as px.
             perspectiveValue = CSSPrimitiveValue::create(primitiveValue->getDoubleValue(), CSSPrimitiveValue::CSS_PX)->computeLength<float>(state.cssToLengthConversionData());
         } else {
             return;
@@ -1742,7 +1743,6 @@ void StyleBuilder::oldApplyProperty(CSSPropertyID id, StyleResolverState& state,
     }
 
     // FIXME: crbug.com/154772 Unimplemented css-transforms properties
-    case CSSPropertyPerspective:
     case CSSPropertyPerspectiveOrigin:
     case CSSPropertyTransform:
     case CSSPropertyTransformOrigin:
