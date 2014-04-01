b93c5c7def902a2b916909f5a77d375379987719
wibling@chromium.org <wibling@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-01 12:46:37 +0000

    [Oilpan]: Make ElementResolveContext and SharedStyleFinder STACK_ALLOCATED.
    R=ager@chromium.org, erik.corry@gmail.com, haraken@chromium.org, oilpan-reviews@chromium.org, tkent@chromium.org, vegorov@chromium.org, zerny@chromium.org
    BUG=341815
    
    Review URL: https://codereview.chromium.org/220893002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170554 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/resolver/ElementResolveContext.h
+++ third_party/WebKit/Source/core/css/resolver/ElementResolveContext.h
@@ -35,6 +35,7 @@ class RenderStyle;
 
 // ElementResolveContext is immutable and serves as an input to the style resolve process.
 class ElementResolveContext {
+    STACK_ALLOCATED();
 public:
     ElementResolveContext()
         : m_element(0)
