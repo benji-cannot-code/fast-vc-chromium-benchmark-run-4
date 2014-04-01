c4eb7ea061e9d55cd9df3fd15c36873f0ace91ed
mkosiba@chromium.org <mkosiba@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-01 11:36:23 +0000

    Migrate addMessageToConsole API to WebFrameClient.
    This adds the console message APIs to WebFrameClient.
    
    BUG=304341
    
    Review URL: https://codereview.chromium.org/217283004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170549 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/web/WebFrameClient.h
+++ third_party/WebKit/public/web/WebFrameClient.h
@@ -76,6 +76,7 @@ class WebURL;
 class WebURLLoader;
 class WebURLResponse;
 class WebWorkerPermissionClientProxy;
+struct WebConsoleMessage;
 struct WebContextMenuData;
 struct WebPluginParams;
 struct WebRect;
@@ -148,6 +149,14 @@ public:
 
     // Called when a watched CSS selector matches or stops matching.
     virtual void didMatchCSS(WebLocalFrame*, const WebVector<WebString>& newlyMatchingSelectors, const WebVector<WebString>& stoppedMatchingSelectors) { }
+
+    // Console messages ----------------------------------------------------
+
+    // Whether or not we should report a detailed message for the given source.
+    virtual bool shouldReportDetailedMessageForSource(const WebString& source) { return false; }
+
+    // A new message was added to the console.
+    virtual void didAddMessageToConsole(const WebConsoleMessage&, const WebString& sourceName, unsigned sourceLine, const WebString& stackTrace) { }
 
     // Load commands -------------------------------------------------------
 
