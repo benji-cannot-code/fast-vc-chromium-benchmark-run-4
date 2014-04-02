ccd51a12152874861878586dd63cbea14dfe1ac3
piman@chromium.org <piman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-02 23:54:09 +0000

    Refactor gpu::Buffer to allow different types of backing
    This is so that we can use either base::SharedMemory or mojo::SharedBuffer to
    back gpu::Buffer.
    
    BUG=333157
    R=darin@chromium.org, viettrungluu@chromium.org
    
    Review URL: https://codereview.chromium.org/213353005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@261228 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/common/buffer.cc
+++ gpu/command_buffer/common/buffer.cc
@@ -10,11 +10,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/numerics/safe_math.h"
 
 namespace gpu {
+SharedMemoryBufferBacking::SharedMemoryBufferBacking(
+    scoped_ptr<base::SharedMemory> shared_memory,
+    size_t size)
+    : shared_memory_(shared_memory.Pass()), size_(size) {}
 
-Buffer::Buffer(scoped_ptr<base::SharedMemory> shared_memory, size_t size)
-    : shared_memory_(shared_memory.Pass()),
-      memory_(shared_memory_->memory()),
-      size_(size) {
+SharedMemoryBufferBacking::~SharedMemoryBufferBacking() {}
+
+void* SharedMemoryBufferBacking::GetMemory() const {
+  return shared_memory_->memory();
+}
+
+size_t SharedMemoryBufferBacking::GetSize() const { return size_; }
+
+Buffer::Buffer(scoped_ptr<BufferBacking> backing)
+    : backing_(backing.Pass()),
+      memory_(backing_->GetMemory()),
+      size_(backing_->GetSize()) {
   DCHECK(memory_) << "The memory must be mapped to create a Buffer";
 }
 
--- gpu/command_buffer/service/transfer_buffer_manager.h
+++ gpu/command_buffer/service/transfer_buffer_manager.h
@@ -21,10 +21,8 @@ class GPU_EXPORT TransferBufferManagerInterface {
  public:
   virtual ~TransferBufferManagerInterface();
 
-  virtual bool RegisterTransferBuffer(
-      int32 id,
-      scoped_ptr<base::SharedMemory> shared_memory,
-      size_t size) = 0;
+  virtual bool RegisterTransferBuffer(int32 id,
+                                      scoped_ptr<BufferBacking> buffer) = 0;
   virtual void DestroyTransferBuffer(int32 id) = 0;
   virtual scoped_refptr<Buffer> GetTransferBuffer(int32 id) = 0;
 };
@@ -35,10 +33,9 @@ class GPU_EXPORT TransferBufferManager
   TransferBufferManager();
 
   bool Initialize();
-  virtual bool RegisterTransferBuffer(
-      int32 id,
-      scoped_ptr<base::SharedMemory> shared_memory,
-      size_t size) OVERRIDE;
+  virtual bool RegisterTransferBuffer(int32 id,
+                                      scoped_ptr<BufferBacking> buffer_backing)
+      OVERRIDE;
   virtual void DestroyTransferBuffer(int32 id) OVERRIDE;
   virtual scoped_refptr<Buffer> GetTransferBuffer(int32 id) OVERRIDE;
 
