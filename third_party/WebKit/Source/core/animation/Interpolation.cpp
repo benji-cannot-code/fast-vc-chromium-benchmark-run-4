9f1cace4faa4a324f7d0c0e45e573ce2a0b34eec
shans@chromium.org <shans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-02 19:44:40 +0000

    Web Animations: Introduce String based KeyframeEffectModel
    Note: This is a rework of issue 212023004
    
    This patch refactors KeyframeEffectModel to support different types of
    keyframe values. The common keyframe logic is retained in
    KeyframeEffectModelBase to be inherited by the keyframe type specialisations.
    
    KeyframeEffectModel is then templated on Keyframe type, with a hierarchy of Keyframes
    including StringKeyframe and AnimatableValueKeyframe being introduced.
    
    String based keyframes are being introduced to properly support the JS API
    for Web Animations element.animate().
    
    Review URL: https://codereview.chromium.org/215883005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170690 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/Interpolation.cpp
+++ third_party/WebKit/Source/core/animation/Interpolation.cpp
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "core/animation/Interpolation.h"
 
+#include "core/animation/AnimatableDouble.h"
 #include "core/css/resolver/AnimatedStyleBuilder.h"
 #include "core/css/resolver/StyleResolverState.h"
 
@@ -71,8 +72,7 @@ void StyleInterpolation::trace(Visitor* visitor)
 
 void LegacyStyleInterpolation::apply(StyleResolverState& state) const
 {
-    AnimatableValue* value = currentValue();
-    AnimatedStyleBuilder::applyProperty(m_id, state, value);
+    AnimatedStyleBuilder::applyProperty(m_id, state, currentValue().get());
 }
 
 void LegacyStyleInterpolation::trace(Visitor* visitor)
