c03039a1721590c689388a8cdbb5bb7a53e2043d
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-02 01:46:32 +0000

    Cleanup: Remove unused accessibility constants.
    Found by Scythe.
    
    Review URL: https://codereview.chromium.org/217323007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@261030 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/accessibility/accessibility_extension_api_constants.cc
+++ chrome/browser/accessibility/accessibility_extension_api_constants.cc
@@ -11,7 +11,6 @@ namespace extension_accessibility_api_constants {
 const char kTypeKey[] = "type";
 const char kNameKey[] = "name";
 const char kContextKey[] = "context";
-const char kDetailsKey[] = "details";
 const char kValueKey[] = "details.value";
 const char kChildrenCountKey[] = "details.childrenCount";
 const char kPasswordKey[] = "details.isPassword";
