a6634e8dc4e035fe918b9022111c7776680df38b
lushnikov@chromium.org <lushnikov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-02 09:06:32 +0000

    DevTools: fix primary selection in multiple selections created with ctrl-d
    This patch sets correct index to the multiple selections created with
    ctrl-d.
    
    Review URL: https://codereview.chromium.org/220203007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170643 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/CodeMirrorTextEditor.js
+++ third_party/WebKit/Source/devtools/front_end/CodeMirrorTextEditor.js
@@ -1146,8 +1146,9 @@ WebInspector.CodeMirrorTextEditor.prototype = {
 
     /**
      * @param {!Array.<!WebInspector.TextRange>} ranges
+     * @param {number=} primarySelectionIndex
      */
-    setSelections: function(ranges)
+    setSelections: function(ranges, primarySelectionIndex)
     {
         var selections = [];
         for (var i = 0; i < ranges.length; ++i) {
@@ -1157,7 +1158,8 @@ WebInspector.CodeMirrorTextEditor.prototype = {
                 head: selection.end
             });
         }
-        this._codeMirror.setSelections(selections, 0, { scroll: false });
+        primarySelectionIndex = primarySelectionIndex || 0;
+        this._codeMirror.setSelections(selections, primarySelectionIndex, { scroll: false });
     },
 
     /**
@@ -1837,7 +1839,7 @@ WebInspector.CodeMirrorTextEditor.SelectNextOccurrenceController.prototype = {
         selections.push(next);
 
         this._muteSelectionListener = true;
-        this._textEditor.setSelections(selections);
+        this._textEditor.setSelections(selections, selections.length - 1);
         delete this._muteSelectionListener;
 
         this._textEditor._revealLine(next.startLine);
@@ -1858,7 +1860,7 @@ WebInspector.CodeMirrorTextEditor.SelectNextOccurrenceController.prototype = {
             var newSelection = new WebInspector.TextRange(startRangeWord.startLine, startRangeWord.startColumn, endRangeWord.endLine, endRangeWord.endColumn);
             newSelections.push(newSelection);
         }
-        this._textEditor.setSelections(newSelections);
+        this._textEditor.setSelections(newSelections, newSelections.length - 1);
         this._fullWordSelection = true;
     },
 
