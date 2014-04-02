cf2fe9db5fdcb0860e38767c249e6b4dcbb37d01
jennyz@chromium.org <jennyz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-02 03:55:35 +0000

    Close app launcher folder with keyboard.
    BUG=356286
    
    Review URL: https://codereview.chromium.org/216683003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@261054 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/app_list/views/app_list_view.cc
+++ ui/app_list/views/app_list_view.cc
@@ -17,8 +17,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/app_list/signin_delegate.h"
 #include "ui/app_list/speech_ui_model.h"
 #include "ui/app_list/views/app_list_background.h"
+#include "ui/app_list/views/app_list_folder_view.h"
 #include "ui/app_list/views/app_list_main_view.h"
 #include "ui/app_list/views/app_list_view_observer.h"
+#include "ui/app_list/views/apps_container_view.h"
+#include "ui/app_list/views/contents_view.h"
 #include "ui/app_list/views/search_box_view.h"
 #include "ui/app_list/views/signin_view.h"
 #include "ui/app_list/views/speech_view.h"
@@ -386,6 +389,14 @@ bool AppListView::AcceleratorPressed(const ui::Accelerator& accelerator) {
   if (accelerator.key_code() == ui::VKEY_ESCAPE) {
     if (app_list_main_view_->search_box_view()->HasSearch()) {
       app_list_main_view_->search_box_view()->ClearSearch();
+    } else if (app_list_main_view_->contents_view()
+                   ->apps_container_view()
+                   ->IsInFolderView()) {
+      app_list_main_view_->contents_view()
+          ->apps_container_view()
+          ->app_list_folder_view()
+          ->CloseFolderPage();
+      return true;
     } else {
       GetWidget()->Deactivate();
       Close();
