1a5468ee14f27ac226f5b75d5c00d1ce1d18053a
a.cavalcanti@samsung.com <a.cavalcanti@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-02 17:45:50 +0000

    Remove software path from filters: FEComponentTransfer.
    BUG=357672
    
    Review URL: https://codereview.chromium.org/219183004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170674 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/filters/FEComponentTransfer.h
+++ third_party/WebKit/Source/platform/graphics/filters/FEComponentTransfer.h
@@ -87,7 +87,6 @@ private:
         const ComponentTransferFunction& blueFunc, const ComponentTransferFunction& alphaFunc);
 
     virtual void applySoftware() OVERRIDE;
-    virtual bool applySkia() OVERRIDE;
 
     virtual bool affectsTransparentPixels() OVERRIDE;
 
