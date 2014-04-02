aa37c0e570ed51768a369fb0645f20a77af19910
c.shu@samsung.com <c.shu@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-02 21:54:51 +0000

    Use RenderStyle::isCollapsibleWhiteSpace when renderer is available.
    This patch replaces isCollapsibleWhitespace() with RenderStyle::isCollapsibleWhiteSpace
    in places where renderer is available. The latter provides more accurate decision so we
    should always use it when possible. Some test results require rebaseline but none of
    them failed the test really and the new behavior is correct.
    
    BUG=357226
    
    Review URL: https://codereview.chromium.org/214523008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170706 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/HTMLInterchange.cpp
+++ third_party/WebKit/Source/core/editing/HTMLInterchange.cpp
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/editing/HTMLInterchange.h"
 
 #include "core/dom/Text.h"
-#include "core/editing/TextIterator.h"
+#include "core/editing/htmlediting.h"
 #include "core/rendering/RenderObject.h"
 #include "wtf/text/StringBuilder.h"
 #include "wtf/unicode/CharacterNames.h"
