651755eb1294e56ffdc12a81c2457601d17557eb
vmpstr@chromium.org <vmpstr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-02 00:44:00 +0000

    cc: Remove extra type from tiling raster tile iterator.
    This patch removes an unnecessary type from the tiling raster iterator.
    It is replaced with the priority bin from tile priority.
    
    R=enne
    
    Review URL: https://codereview.chromium.org/220343009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@261006 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/resources/picture_layer_tiling.h
+++ cc/resources/picture_layer_tiling.h
@@ -48,8 +48,6 @@ class CC_EXPORT PictureLayerTiling {
  public:
   class CC_EXPORT TilingRasterTileIterator {
    public:
-    enum Type { VISIBLE, SKEWPORT, EVENTUALLY };
-
     TilingRasterTileIterator();
     TilingRasterTileIterator(PictureLayerTiling* tiling, WhichTree tree);
     ~TilingRasterTileIterator();
@@ -58,13 +56,13 @@ class CC_EXPORT PictureLayerTiling {
       return current_tile_ && TileNeedsRaster(current_tile_);
     }
     Tile* operator*() { return current_tile_; }
-    Type get_type() const { return type_; }
+    TilePriority::PriorityBin get_type() const { return type_; }
 
     TilingRasterTileIterator& operator++();
 
     gfx::Rect TileBounds() const {
       DCHECK(*this);
-      if (type_ == VISIBLE) {
+      if (type_ == TilePriority::NOW) {
         return tiling_->tiling_data_.TileBounds(visible_iterator_.index_x(),
                                                 visible_iterator_.index_y());
       }
@@ -81,7 +79,7 @@ class CC_EXPORT PictureLayerTiling {
 
     PictureLayerTiling* tiling_;
 
-    Type type_;
+    TilePriority::PriorityBin type_;
     gfx::Rect visible_rect_in_content_space_;
     gfx::Rect skewport_in_content_space_;
     gfx::Rect eventually_rect_in_content_space_;
