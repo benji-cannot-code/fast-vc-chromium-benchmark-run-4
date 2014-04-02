78ccaf61caf74eed0a83af187636b295a872276a
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-02 16:36:22 +0000

    Chrome side changes to removing WebFrame parameter from WebPermissionClient, since it's redundant now.
    BUG=304341
    R=nasko@chromium.org
    
    Review URL: https://codereview.chromium.org/216983003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@261136 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/renderer/aw_permission_client.h
+++ android_webview/renderer/aw_permission_client.h
@@ -21,6 +21,7 @@ class AwPermissionClient : public content::RenderFrameObserver,
   virtual ~AwPermissionClient();
 
   // blink::WebPermissionClient implementation.
+  // TODO(jam): remove all these methods after Blink rolls.
   virtual bool allowDisplayingInsecureContent(
       blink::WebFrame* frame,
       bool enabled_per_settings,
@@ -28,6 +29,15 @@ class AwPermissionClient : public content::RenderFrameObserver,
       const blink::WebURL& url) OVERRIDE;
   virtual bool allowRunningInsecureContent(
       blink::WebFrame* frame,
+      bool enabled_per_settings,
+      const blink::WebSecurityOrigin& origin,
+      const blink::WebURL& url) OVERRIDE;
+
+  virtual bool allowDisplayingInsecureContent(
+      bool enabled_per_settings,
+      const blink::WebSecurityOrigin& origin,
+      const blink::WebURL& url) OVERRIDE;
+  virtual bool allowRunningInsecureContent(
       bool enabled_per_settings,
       const blink::WebSecurityOrigin& origin,
       const blink::WebURL& url) OVERRIDE;
