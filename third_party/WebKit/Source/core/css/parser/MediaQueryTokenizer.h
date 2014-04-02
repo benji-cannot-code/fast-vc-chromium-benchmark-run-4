3773d02ba22788068e93952e283682cae25765be
yoav@yoav.ws <yoav@yoav.ws@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-02 21:38:10 +0000

    Fixed MediaQueryTokenizer escape support
    MediaQueryTokenizer handling of escapes was faulty, causing media queries with escaped code points to be considered invalid.
    This CL fixes that.
    
    BUG=358833
    
    Review URL: https://codereview.chromium.org/221383003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170704 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/parser/MediaQueryTokenizer.h
+++ third_party/WebKit/Source/core/css/parser/MediaQueryTokenizer.h
@@ -41,7 +41,7 @@ private:
     String consumeName();
     UChar consumeEscape();
 
-    bool nextTwoCharsAreValidEscape();
+    bool nextTwoCharsAreValidEscape(unsigned offset);
     bool nextCharsAreNumber();
     bool nextCharsAreIdentifier();
 
