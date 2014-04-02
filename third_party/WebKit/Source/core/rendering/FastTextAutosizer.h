0210b82af4018f21bac702c5e4e9f3430846006f
skobes@chromium.org <skobes@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-02 00:41:57 +0000

    Reset multipliers when FTA is disabled.
    Fix assertion in beginLayout on resize.
    
    BUG=353309
    
    Review URL: https://codereview.chromium.org/221193004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170615 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/FastTextAutosizer.h
+++ third_party/WebKit/Source/core/rendering/FastTextAutosizer.h
@@ -199,7 +199,8 @@ private:
     void endLayout(RenderBlock*);
     void inflateTable(RenderTable*);
     void inflate(RenderBlock*);
-    bool enabled();
+    bool enabled() const;
+    bool shouldHandleLayout() const;
     void setAllTextNeedsLayout();
     void resetMultipliers();
     void prepareClusterStack(const RenderObject*);
