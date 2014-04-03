bf4e92fccd23992cc73473f1100c9a93f04c2623
jdduke@chromium.org <jdduke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-03 17:48:21 +0000

    Revert of [Android] Provide unhandled tap event notifications (https://codereview.chromium.org/218633008/)
    Reason for revert:
    Legitimate browsertest failure.
    
    Original issue's description:
    > [Android] Provide unhandled tap event notifications
    > 
    > Allow notifications of unhandled taps via the GestureStateListener.  This change
    > makes GestureTap events blocking, but there are several use-cases for which this
    > will be necessary, e.g., WebView and contextual search.
    > 
    > NOTRY=true
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=261431
    
    TBR=mkosiba@chromium.org,donnd@chromium.org,tedchoc@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/224273002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@261470 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/input/web_input_event_traits.cc
+++ content/common/input/web_input_event_traits.cc
@@ -323,6 +323,7 @@ bool WebInputEventTraits::IgnoresAckDisposition(
     case WebInputEvent::GestureTapDown:
     case WebInputEvent::GestureShowPress:
     case WebInputEvent::GestureTapCancel:
+    case WebInputEvent::GestureTap:
     case WebInputEvent::GesturePinchBegin:
     case WebInputEvent::GesturePinchEnd:
     case WebInputEvent::GestureScrollBegin:
