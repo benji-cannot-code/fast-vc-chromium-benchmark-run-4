de6f5529f7918049973baa9255593a4572746096
horo@chromium.org <horo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-03 08:54:02 +0000

    Revert of Make DevTools support for the embedded SharedWorker. (https://codereview.chromium.org/196503005/)
    Reason for revert:
    buildbot failure in Chromium Memory on Linux ASan+LSan Tests
    
    SharedWorkerDevToolsManagerTest.AttachTest test causes memory leaks.
    
    Original issue's description:
    > Make DevTools support for the embedded SharedWorker.
    > 
    > BUG=327256
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=261314
    
    TBR=kinuko@chromium.org,pfeldman@chromium.org,yurys@chromium.org,sergeyv@chromium.org,jochen@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=327256
    
    Review URL: https://codereview.chromium.org/223583002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@261327 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/devtools/worker_devtools_manager.h
+++ content/browser/devtools/worker_devtools_manager.h
@@ -20,7 +20,6 @@ namespace content {
 class DevToolsAgentHost;
 
 // All methods are supposed to be called on the IO thread.
-// This class is not used when "enable-embedded-shared-worker" flag is set.
 class WorkerDevToolsManager {
  public:
   typedef std::pair<int, int> WorkerId;
@@ -43,8 +42,9 @@ class WorkerDevToolsManager {
 
   // Called on the IO thread.
   // Returns true when the worker must be paused on start.
-  bool WorkerCreated(WorkerProcessHost* process,
-                     const WorkerProcessHost::WorkerInstance& instance);
+  bool WorkerCreated(
+      WorkerProcessHost* process,
+      const WorkerProcessHost::WorkerInstance& instance);
   void WorkerDestroyed(WorkerProcessHost* process, int worker_route_id);
   void WorkerContextStarted(WorkerProcessHost* process, int worker_route_id);
 
