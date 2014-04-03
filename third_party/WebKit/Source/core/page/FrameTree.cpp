ffb193c0d1ed3cf28665dcd1bee3929f4e83999d
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-03 03:47:04 +0000

    Generate FrameTree::uniqueName with a URL attribute value.
    This CL fixes a bug that navigating back can swap plugin contents.
    
    Bug detail:
    When a page is loaded by back navigation, we try to load the last
    contents into frames. SVG content in <object> is also represented as
    a frame internally. The last contents state is stored in a HistoryItem
    tree, and HistoryItems for sub-frames are identified by 'uniqueName,'
    which depends on frame creation order if frames don't have name
    attributes.
    So, if frame creation orders of the first visit and the second visit are
    different, we could get incorrect HistoryItem in WebFrameImpl::
    createChildFrame.
    
    How to fix:
    This CL tries to generate unique uniqueName even if a frame has no
    name attribute. We use an attribute value of Element::
    subResourceAttributeName.
    
    
    The test was created by ed@opera.com.
    
    BUG=352762
    
    Review URL: https://codereview.chromium.org/218693003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170728 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/FrameTree.cpp
+++ third_party/WebKit/Source/core/page/FrameTree.cpp
@@ -54,7 +54,7 @@ FrameTree::~FrameTree()
         child->setView(nullptr);
 }
 
-void FrameTree::setName(const AtomicString& name)
+void FrameTree::setName(const AtomicString& name, const AtomicString& fallbackName)
 {
     m_name = name;
     if (!parent()) {
@@ -62,7 +62,7 @@ void FrameTree::setName(const AtomicString& name)
         return;
     }
     m_uniqueName = AtomicString(); // Remove our old frame name so it's not considered in uniqueChildName.
-    m_uniqueName = parent()->tree().uniqueChildName(name);
+    m_uniqueName = parent()->tree().uniqueChildName(name.isEmpty() ? fallbackName : name);
 }
 
 LocalFrame* FrameTree::parent() const
