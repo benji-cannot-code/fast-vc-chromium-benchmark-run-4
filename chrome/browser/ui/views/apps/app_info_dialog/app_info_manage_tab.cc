e6e1ee1cd016fc08a4beec19b366d8444863e9d3
sashab@chromium.org <sashab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-03 23:35:42 +0000

    App Info Dialog Summary Tab UI fixes
    - Gave the dialog a fixed height
    - Made the app's name bold and multi-lined
    - Centered the app info properly with the icon
    - Swapped the location of the web store link and the description
    - Only displayed the description if the app has one
    - Only displayed the version for non-bookmark apps
    - Limited the description to 200 characters
    - Added an Uninstall button to the summary tab
    
    BUG=356953,348832,357442
    
    Review URL: https://codereview.chromium.org/213833004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@261572 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/apps/app_info_dialog/app_info_manage_tab.cc
+++ chrome/browser/ui/views/apps/app_info_dialog/app_info_manage_tab.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/apps/app_info_dialog/app_info_manage_tab.h"
 
-#include "ui/gfx/native_widget_types.h"
-
 AppInfoManageTab::AppInfoManageTab(gfx::NativeWindow parent_window,
                                    Profile* profile,
                                    const extensions::Extension* app,
