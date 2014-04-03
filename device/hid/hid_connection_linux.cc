2e123d3abfe37212c010c73e01cddcbb7ef85d6f
jracle@logitech.com <jracle@logitech.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-03 19:26:36 +0000

    HID connection matches wrong hidraw devnode to HID device when a bus driver is present on Linux
    Fix : HidConnectionLinux::FindHidrawDevNode should look for presence of
    "hidraw" string immediately after matching parent udev path, so that we
    do not match devnodes created by bus enumerator driver
    
    BUG=358666
    TBR=rockot@chromium.org
    
    Review URL: https://codereview.chromium.org/223803002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@261502 0039d316-1c4b-4281-b951-d872f2087c98

--- device/hid/hid_connection_linux.cc
+++ device/hid/hid_connection_linux.cc
@@ -233,8 +233,11 @@ bool HidConnectionLinux::FindHidrawDevNode(udev_device* parent,
     std::string device_path = udev_device_get_devpath(hid_dev.get());
     if (raw_path &&
         !device_path.compare(0, parent_path.length(), parent_path)) {
-      *result = raw_path;
-      return true;
+      std::string sub_path = device_path.substr(parent_path.length());
+      if (sub_path.substr(0, sizeof(kHidrawSubsystem)-1) == kHidrawSubsystem) {
+        *result = raw_path;
+        return true;
+      }
     }
   }
 
@@ -242,3 +245,4 @@ bool HidConnectionLinux::FindHidrawDevNode(udev_device* parent,
 }
 
 }  // namespace device
+
