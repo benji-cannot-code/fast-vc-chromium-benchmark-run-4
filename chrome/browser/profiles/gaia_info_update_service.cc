07b2413209443e3fbeae5222a460d0a48e3a0361
rogerta@chromium.org <rogerta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-03 07:42:35 +0000

    Just some cleanup before I start on the profile name fix. Make GAIAInfoUpdateService listen to signin manager and use its api instead of directly accessing the username pref.
    BUG=351970
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=260630
    
    Review URL: https://codereview.chromium.org/196243009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@261309 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/gaia_info_update_service.cc
+++ chrome/browser/profiles/gaia_info_update_service.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_info_cache.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/signin/signin_manager_factory.h"
 #include "chrome/browser/sync/profile_sync_service.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/profile_management_switches.h"
@@ -31,24 +32,25 @@ const int kMinUpdateIntervalSeconds = 5;
 
 GAIAInfoUpdateService::GAIAInfoUpdateService(Profile* profile)
     : profile_(profile) {
-  PrefService* prefs = profile_->GetPrefs();
-  username_pref_.Init(prefs::kGoogleServicesUsername, prefs,
-                      base::Bind(&GAIAInfoUpdateService::OnUsernameChanged,
-                                 base::Unretained(this)));
+  SigninManagerBase* signin_manager =
+      SigninManagerFactory::GetForProfile(profile_);
+  signin_manager->AddObserver(this);
 
+  PrefService* prefs = profile_->GetPrefs();
   last_updated_ = base::Time::FromInternalValue(
       prefs->GetInt64(prefs::kProfileGAIAInfoUpdateTime));
   ScheduleNextUpdate();
 }
 
 GAIAInfoUpdateService::~GAIAInfoUpdateService() {
+  DCHECK(!profile_) << "Shutdown not called before dtor";
 }
 
 void GAIAInfoUpdateService::Update() {
   // The user must be logged in.
-  std::string username = profile_->GetPrefs()->GetString(
-      prefs::kGoogleServicesUsername);
-  if (username.empty())
+  SigninManagerBase* signin_manager =
+      SigninManagerFactory::GetForProfile(profile_);
+  if (signin_manager->GetAuthenticatedAccountId().empty())
     return;
 
   if (profile_image_downloader_)
@@ -152,15 +154,13 @@ void GAIAInfoUpdateService::OnProfileDownloadFailure(
   ScheduleNextUpdate();
 }
 
-void GAIAInfoUpdateService::OnUsernameChanged() {
+void GAIAInfoUpdateService::OnUsernameChanged(const std::string& username) {
   ProfileInfoCache& cache =
       g_browser_process->profile_manager()->GetProfileInfoCache();
   size_t profile_index = cache.GetIndexOfProfileWithPath(profile_->GetPath());
   if (profile_index == std::string::npos)
     return;
 
-  std::string username = profile_->GetPrefs()->GetString(
-      prefs::kGoogleServicesUsername);
   if (username.empty()) {
     // Unset the old user's GAIA info.
     cache.SetGAIANameOfProfileAtIndex(profile_index, base::string16());
@@ -175,6 +175,19 @@ void GAIAInfoUpdateService::OnUsernameChanged() {
     // Update the new user's GAIA info.
     Update();
   }
+}
+
+void GAIAInfoUpdateService::Shutdown() {
+  timer_.Stop();
+  profile_image_downloader_.reset();
+  SigninManagerBase* signin_manager =
+      SigninManagerFactory::GetForProfile(profile_);
+  signin_manager->RemoveObserver(this);
+
+  // OK to reset |profile_| pointer here because GAIAInfoUpdateService will not
+  // access it again.  This pointer is also used to implement the delegate for
+  // |profile_image_downloader_|.  However that object was destroyed above.
+  profile_ = NULL;
 }
 
 void GAIAInfoUpdateService::ScheduleNextUpdate() {
@@ -192,4 +205,14 @@ void GAIAInfoUpdateService::ScheduleNextUpdate() {
     delta = desired_delta - update_delta;
 
   timer_.Start(FROM_HERE, delta, this, &GAIAInfoUpdateService::Update);
+}
+
+void GAIAInfoUpdateService::GoogleSigninSucceeded(
+    const std::string& username,
+    const std::string& password) {
+  OnUsernameChanged(username);
+}
+
+void GAIAInfoUpdateService::GoogleSignedOut(const std::string& username) {
+  OnUsernameChanged(std::string());
 }
--- chrome/browser/profiles/gaia_info_update_service_factory.cc
+++ chrome/browser/profiles/gaia_info_update_service_factory.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/profiles/gaia_info_update_service.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/signin/signin_manager_factory.h"
 #include "chrome/common/pref_names.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "components/user_prefs/pref_registry_syncable.h"
@@ -15,6 +16,7 @@ GAIAInfoUpdateServiceFactory::GAIAInfoUpdateServiceFactory()
     : BrowserContextKeyedServiceFactory(
         "GAIAInfoUpdateService",
         BrowserContextDependencyManager::GetInstance()) {
+  DependsOn(SigninManagerFactory::GetInstance());
 }
 
 GAIAInfoUpdateServiceFactory::~GAIAInfoUpdateServiceFactory() {}
