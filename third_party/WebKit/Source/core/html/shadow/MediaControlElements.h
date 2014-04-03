bc365e8bbf4dde413346905ba8c3c87527829163
philipj@opera.com <philipj@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-03 20:00:12 +0000

    Let the overlay playback button fall back to the activation behavior
    Since the button is only visible when togglePlayStateWillPlay(), just
    doing nothing and falling back to the default activation behavior will
    do the right thing.
    
    BUG=354746
    TEST=LayoutTests/media/activation-behavior-shadow.html
    
    Review URL: https://codereview.chromium.org/215323003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170789 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/shadow/MediaControlElements.h
+++ third_party/WebKit/Source/core/html/shadow/MediaControlElements.h
@@ -128,7 +128,6 @@ private:
     explicit MediaControlOverlayPlayButtonElement(MediaControls&);
 
     virtual const AtomicString& shadowPseudoId() const OVERRIDE;
-    virtual void defaultEventHandler(Event*) OVERRIDE;
 };
 
 // ----------------------------
