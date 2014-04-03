16fa57ddac05f9a44d4d603b2cba919c7aa58ca8
stepco@chromium.org <stepco@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-03 19:38:49 +0000

    Add an extra delay for policy invalidations to be available.
    The cloud policy refresh scheduler no longer waits for policy invalidations to become available before issuing the first policy fetch. We want to ensure that any policy change that results from this initial policy fetch is counted as a change without invalidations enabled for metrics purposes.
    
    Currently we determine if invalidations are enabled based on whether the invalidation service has reported that it is on. However, there may be a delay between the invalidation service being enabled and the invalidation service actually delivering a pending invalidation. Additionally, on Android the invalidation service always reports that it is enabled. To account for this, add a "grace period" after the invalidation service is enabled during which we still consider invalidations disabled for metrics reporting.
    
    BUG=355884
    
    Review URL: https://codereview.chromium.org/213743014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@261506 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/cloud/user_cloud_policy_invalidator.cc
+++ chrome/browser/policy/cloud/user_cloud_policy_invalidator.cc
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/policy/cloud/user_cloud_policy_invalidator.h"
 
 #include "base/bind.h"
+#include "base/memory/scoped_ptr.h"
 #include "base/message_loop/message_loop_proxy.h"
+#include "base/time/default_clock.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/invalidation/invalidation_service_factory.h"
 #include "components/policy/core/common/cloud/cloud_policy_manager.h"
@@ -19,7 +21,8 @@ UserCloudPolicyInvalidator::UserCloudPolicyInvalidator(
     CloudPolicyManager* policy_manager)
     : CloudPolicyInvalidator(
           policy_manager->core(),
-          base::MessageLoopProxy::current()),
+          base::MessageLoopProxy::current(),
+          scoped_ptr<base::Clock>(new base::DefaultClock())),
       profile_(profile) {
   DCHECK(profile);
 
