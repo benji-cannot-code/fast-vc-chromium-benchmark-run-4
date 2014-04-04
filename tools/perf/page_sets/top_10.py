201b1b7959f7ac8bef95d33979a70221b4b9ff94
nednguyen@google.com <nednguyen@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-04 06:32:20 +0000

    Set page's _base_dir in PageSet.FromPythonFile(..)
    BUG=239179
    
    Review URL: https://codereview.chromium.org/223993003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@261695 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/page_sets/top_10.py
+++ tools/perf/page_sets/top_10.py
@@ -12,8 +12,8 @@ from telemetry.page.page import Page
 
 
 class SimpleScrollPage(Page):
-  def __init__(self, url='', credentials=''):
-    super(SimpleScrollPage, self).__init__(url, '')
+  def __init__(self, url, page_set, credentials=''):
+    super(SimpleScrollPage, self).__init__(url, page_set=page_set)
     self.credentials = credentials
 
   def RunSmoothness(self, action_runner):
@@ -24,9 +24,9 @@ class SimpleScrollPage(Page):
 
 
 class Google(SimpleScrollPage):
-  def __init__(self):
+  def __init__(self, page_set):
     super(Google, self).__init__(
-      'https://www.google.com/#hl=en&q=barack+obama')
+      url='https://www.google.com/#hl=en&q=barack+obama', page_set=page_set)
 
   def RunNavigateSteps(self, action_runner):
     super(Google, self).RunNavigateSteps(action_runner)
@@ -35,9 +35,11 @@ class Google(SimpleScrollPage):
 
 
 class Gmail(SimpleScrollPage):
-  def __init__(self):
+  def __init__(self, page_set):
     super(Gmail, self).__init__(
-      'https://mail.google.com/mail/', credentials='google')
+      url='https://mail.google.com/mail/',
+      page_set=page_set,
+      credentials='google')
 
   def RunNavigateSteps(self, action_runner):
     super(Gmail, self).RunNavigateSteps(action_runner)
@@ -47,9 +49,11 @@ class Gmail(SimpleScrollPage):
 
 
 class GoogleCalendar(SimpleScrollPage):
-  def __init__(self):
+  def __init__(self, page_set):
     super(GoogleCalendar, self).__init__(
-      'https://www.google.com/calendar/', credentials='google')
+      url='https://www.google.com/calendar/',
+      page_set=page_set,
+      credentials='google')
 
   def RunNavigateSteps(self, action_runner):
     super(GoogleCalendar, self).RunNavigateSteps(action_runner)
@@ -65,9 +69,11 @@ class GoogleCalendar(SimpleScrollPage):
 
 
 class Youtube(SimpleScrollPage):
-  def __init__(self):
+  def __init__(self, page_set):
     super(Youtube, self).__init__(
-      'http://www.youtube.com', credentials='google')
+      url='http://www.youtube.com',
+      page_set=page_set,
+      credentials='google')
 
   def RunNavigateSteps(self, action_runner):
     super(Youtube, self).RunNavigateSteps(action_runner)
@@ -75,9 +81,11 @@ class Youtube(SimpleScrollPage):
 
 
 class Facebook(SimpleScrollPage):
-  def __init__(self):
+  def __init__(self, page_set):
     super(Facebook, self).__init__(
-      'http://www.facebook.com/barackobama', credentials='facebook')
+      url='http://www.facebook.com/barackobama',
+      page_set=page_set,
+      credentials='facebook')
     self.name = "Facebook"
 
   def RunNavigateSteps(self, action_runner):
@@ -95,33 +103,34 @@ class Top10PageSet(PageSet):
       user_agent_type='desktop')
 
     # top google property; a google tab is often open
-    self.AddPage(Google())
+    self.AddPage(Google(self))
 
     # productivity, top google properties
-    self.AddPage(Gmail())
+    self.AddPage(Gmail(self))
 
     # productivity, top google properties
-    self.AddPage(GoogleCalendar())
+    self.AddPage(GoogleCalendar(self))
 
     # #3 (Alexa global)
-    self.AddPage(Youtube())
+    self.AddPage(Youtube(self))
 
     # top social, Public profile
-    self.AddPage(Facebook())
+    self.AddPage(Facebook(self))
 
     # #6 (Alexa) most visited worldwide,Picked an interesting page
-    wikipedia_page = SimpleScrollPage('http://en.wikipedia.org/wiki/Wikipedia')
+    wikipedia_page = SimpleScrollPage('http://en.wikipedia.org/wiki/Wikipedia',
+                                      self)
     wikipedia_page.name = "Wikipedia"
     self.AddPage(wikipedia_page)
 
     # #1 world commerce website by visits; #3 commerce in the US by time spent
-    self.AddPage(SimpleScrollPage('http://www.amazon.com'))
+    self.AddPage(SimpleScrollPage('http://www.amazon.com', self))
 
     # #4 Alexa
-    self.AddPage(SimpleScrollPage('http://www.yahoo.com/'))
+    self.AddPage(SimpleScrollPage('http://www.yahoo.com/', self))
 
     # #16 Alexa
-    self.AddPage(SimpleScrollPage('http://www.bing.com/'))
+    self.AddPage(SimpleScrollPage('http://www.bing.com/', self))
 
     # #20 Alexa
-    self.AddPage(SimpleScrollPage('http://www.ask.com/'))
+    self.AddPage(SimpleScrollPage('http://www.ask.com/', self))
