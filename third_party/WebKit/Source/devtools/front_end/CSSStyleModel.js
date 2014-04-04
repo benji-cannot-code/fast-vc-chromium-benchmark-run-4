f6e74e99ad0a72e4d73f3d87bd3440e0836f2bc8
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-04 11:02:40 +0000

    Revert r170854, 170853, 170852 for breaking build.
    TBR=
    
    Review URL: https://codereview.chromium.org/225413003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170855 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/CSSStyleModel.js
+++ third_party/WebKit/Source/devtools/front_end/CSSStyleModel.js
@@ -246,13 +246,13 @@ WebInspector.CSSStyleModel.prototype = {
     },
 
     /**
-     * @param {!CSSAgent.CSSRule} rule
+     * @param {!CSSAgent.CSSRuleId} ruleId
      * @param {!DOMAgent.NodeId} nodeId
      * @param {string} newSelector
      * @param {function(!WebInspector.CSSRule)} successCallback
      * @param {function()} failureCallback
      */
-    setRuleSelector: function(rule, nodeId, newSelector, successCallback, failureCallback)
+    setRuleSelector: function(ruleId, nodeId, newSelector, successCallback, failureCallback)
     {
         /**
          * @param {!DOMAgent.NodeId} nodeId
@@ -274,8 +274,9 @@ WebInspector.CSSStyleModel.prototype = {
             this._computeMatchingSelectors(rulePayload, nodeId, successCallback, failureCallback);
         }
 
+
         this._pendingCommandsMajorState.push(true);
-        this._agent.setRuleSelector(rule.id.styleSheetId, rule.selectorRange, newSelector, callback.bind(this, nodeId, successCallback, failureCallback, newSelector));
+        this._agent.setRuleSelector(ruleId, newSelector, callback.bind(this, nodeId, successCallback, failureCallback, newSelector));
     },
 
     /**
@@ -918,23 +919,13 @@ WebInspector.CSSStyleDeclaration.prototype = {
     },
 
     /**
-     * @param {number} index
-     * @return {!WebInspector.TextRange}
-     */
-    _insertionRange: function(index)
-    {
-        var property = this.propertyAt(index);
-        return property && property.range ? property.range.collapseToStart() : this.range.collapseToEnd();
-    },
-
-    /**
      * @param {number=} index
      * @return {!WebInspector.CSSProperty}
      */
     newBlankProperty: function(index)
     {
         index = (typeof index === "undefined") ? this.pastLastSourcePropertyIndex() : index;
-        var property = new WebInspector.CSSProperty(this, index, "", "", false, false, true, false, "", this._insertionRange(index));
+        var property = new WebInspector.CSSProperty(this, index, "", "", false, false, true, false, "");
         property._setActive(true);
         return property;
     },
@@ -969,7 +960,7 @@ WebInspector.CSSStyleDeclaration.prototype = {
             throw "No style id";
 
         this._cssModel._pendingCommandsMajorState.push(true);
-        this._cssModel._agent.setPropertyText(this.id.styleSheetId, this._insertionRange(index), name + ": " + value + ";", callback.bind(this));
+        this._cssModel._agent.setPropertyText(this.id, index, name + ": " + value + ";", false, callback.bind(this));
     },
 
     /**
@@ -1272,8 +1263,7 @@ WebInspector.CSSProperty.prototype = {
         // An index past all the properties adds a new property to the style.
         var cssModel = this.ownerStyle._cssModel;
         cssModel._pendingCommandsMajorState.push(majorChange);
-        var range = /** @type {!WebInspector.TextRange} */ (this.range);
-        cssModel._agent.setPropertyText(this.ownerStyle.id.styleSheetId, overwrite ? range : range.collapseToStart(), propertyText, callback.bind(this));
+        cssModel._agent.setPropertyText(this.ownerStyle.id, this.index, propertyText, overwrite, callback.bind(this));
     },
 
     /**
