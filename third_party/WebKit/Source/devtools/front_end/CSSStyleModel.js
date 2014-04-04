f15f8ca06e6f35d0e674c52c16ff20d534828aea
lushnikov@chromium.org <lushnikov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-04 13:02:31 +0000

    DevTools: [CSS] eliminate styleId & propertyIndex arguments in setPropertyText
    This patch modifies CSS domain of devtools protocol.
    Instead of using styleId & propertyIndex in setPropertyText method to
    reference actual property in OM, we now will use styleSheetId and textRange
    of the property.
    
    This patch is the first step to elimiate usage of styleId/ruleId in the
    CSS domain.
    
    BUG=341506
    R=apavlov@chromium.org, jochen@chromium.org, pfeldman@chromium.org
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=170852
    
    Review URL: https://codereview.chromium.org/206543004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170862 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/CSSStyleModel.js
+++ third_party/WebKit/Source/devtools/front_end/CSSStyleModel.js
@@ -919,13 +919,23 @@ WebInspector.CSSStyleDeclaration.prototype = {
     },
 
     /**
+     * @param {number} index
+     * @return {!WebInspector.TextRange}
+     */
+    _insertionRange: function(index)
+    {
+        var property = this.propertyAt(index);
+        return property && property.range ? property.range.collapseToStart() : this.range.collapseToEnd();
+    },
+
+    /**
      * @param {number=} index
      * @return {!WebInspector.CSSProperty}
      */
     newBlankProperty: function(index)
     {
         index = (typeof index === "undefined") ? this.pastLastSourcePropertyIndex() : index;
-        var property = new WebInspector.CSSProperty(this, index, "", "", false, false, true, false, "");
+        var property = new WebInspector.CSSProperty(this, index, "", "", false, false, true, false, "", this._insertionRange(index));
         property._setActive(true);
         return property;
     },
@@ -960,7 +970,7 @@ WebInspector.CSSStyleDeclaration.prototype = {
             throw "No style id";
 
         this._cssModel._pendingCommandsMajorState.push(true);
-        this._cssModel._agent.setPropertyText(this.id, index, name + ": " + value + ";", false, callback.bind(this));
+        this._cssModel._agent.setPropertyText(this.id.styleSheetId, this._insertionRange(index), name + ": " + value + ";", callback.bind(this));
     },
 
     /**
@@ -1263,7 +1273,8 @@ WebInspector.CSSProperty.prototype = {
         // An index past all the properties adds a new property to the style.
         var cssModel = this.ownerStyle._cssModel;
         cssModel._pendingCommandsMajorState.push(majorChange);
-        cssModel._agent.setPropertyText(this.ownerStyle.id, this.index, propertyText, overwrite, callback.bind(this));
+        var range = /** @type {!WebInspector.TextRange} */ (this.range);
+        cssModel._agent.setPropertyText(this.ownerStyle.id.styleSheetId, overwrite ? range : range.collapseToStart(), propertyText, callback.bind(this));
     },
 
     /**
