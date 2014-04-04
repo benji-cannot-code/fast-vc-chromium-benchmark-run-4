e77302fe1c03a79b7401b25ec121c5dfa0fd175b
dbeam@chromium.org <dbeam@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-04 05:03:40 +0000

    rAc: make autocomplete/autocompleteerror events bubble.
    R=esprehn@chromium.org
    BUG=351906
    
    Review URL: https://codereview.chromium.org/198443002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170827 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/events/AutocompleteErrorEvent.h
+++ third_party/WebKit/Source/core/events/AutocompleteErrorEvent.h
@@ -64,7 +64,7 @@ private:
     }
 
     AutocompleteErrorEvent(const String& reason)
-        : Event(EventTypeNames::autocompleteerror, false, false)
+        : Event(EventTypeNames::autocompleteerror, true, false)
         , m_reason(reason)
     {
         ScriptWrappable::init(this);
