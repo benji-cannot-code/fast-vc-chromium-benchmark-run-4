bffadd3eb46c2755e5a386922c50fb65287b334a
mcasas@chromium.org <mcasas@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-04 09:27:07 +0000

    VideoCapturerDelegate: Retrieve supported/in-use format(s) for constraint negotiation.
    VideoCapturerDelegate uses currently a set of fixed 
    resolutions and frame rates for constraint negotiation.
    Instead, the associated VideoCaptureImplManager is
    queried first for the device format in-use; if that
    in non-null, is used, otherwise the whole list of 
    supported formats is retrieved and used. If that last
    list is also empty (platform does not support it,
    such as Mac QTKit or Win DirectShow), then the by-
    default set of resolutions are used.
    
    This CL would change behaviour of scenarios in
    which requested capture resolution or frame rate is
    larger than the one provided by hardware, for example
    Fake Video Capture Device supports only 
    20fps, if the Mandatory MinFps is larger than that,
    the resolution algorithm will fail.
    
    No unittests added to WebRtcGetUserMediaBrowserTest:  
    the unsupported format is added in 
    https://codereview.chromium.org/197213004/ . Rest of
    the tests in the file change the gUM to request fps
    between 10 and 30, since the FakeVCD supports 20, and
    it would fail with a [30-30] fps range requested.
    
    BUG=309554
    
    Review URL: https://codereview.chromium.org/195363002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@261717 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/media/webrtc_browsertest.cc
+++ content/browser/media/webrtc_browsertest.cc
@@ -127,7 +127,7 @@ IN_PROC_BROWSER_TEST_P(WebRtcBrowserTest, MAYBE_CanForwardRemoteStream) {
 
 IN_PROC_BROWSER_TEST_P(WebRtcBrowserTest, MAYBE_CanForwardRemoteStream720p) {
   const std::string javascript = GenerateGetUserMediaCall(
-      "callAndForwardRemoteStream", 1280, 1280, 720, 720, 30, 30);
+      "callAndForwardRemoteStream", 1280, 1280, 720, 720, 10, 30);
   MakeTypicalPeerConnectionCall(javascript);
 }
 
