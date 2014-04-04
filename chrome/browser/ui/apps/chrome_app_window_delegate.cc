c2030f929b7d0aafc9d0809f6fb2ec887eee7b52
rockot@google.com <rockot@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-04 16:22:39 +0000

    Revert "Revert 261654 "Move some extension constants out of //chrome.""
    Re-landing the patch from 261654 with a fix for GTK.
    
    Revert CL: https://codereview.chromium.org/225103004/
    
    BUG=359173
    TBR=asargent@chromium.org
    TBR=cpu@chromium.org for component_updater header update
    TBR=avi@chromium.org for tab_contents header update
    TBR=pkasting@chromium.org for infobars header update
    
    Review URL: https://codereview.chromium.org/224943004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@261774 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/apps/chrome_app_window_delegate.cc
+++ chrome/browser/ui/apps/chrome_app_window_delegate.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_delegate.h"
 #include "content/public/browser/web_contents_view.h"
+#include "extensions/common/constants.h"
 
 #if defined(USE_ASH)
 #include "ash/shelf/shelf_constants.h"
