52b2f2a29b0df0fc40de479b701f7b30659387a2
brianderson@chromium.org <brianderson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-04 04:34:28 +0000

    cc: Avoid dynamic calls to base::Bind in scheduler
    BUG=359245
    
    Review URL: https://codereview.chromium.org/222743004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@261668 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/scheduler/scheduler.h
+++ cc/scheduler/scheduler.h
@@ -100,7 +100,7 @@ class CC_EXPORT Scheduler {
     return state_machine_.MainThreadIsInHighLatencyMode();
   }
   bool BeginImplFrameDeadlinePending() const {
-    return !begin_impl_frame_deadline_closure_.IsCancelled();
+    return !begin_impl_frame_deadline_task_.IsCancelled();
   }
 
   bool WillDrawIfNeeded() const;
@@ -154,9 +154,13 @@ class CC_EXPORT Scheduler {
 
   bool last_set_needs_begin_impl_frame_;
   BeginFrameArgs last_begin_impl_frame_args_;
-  base::CancelableClosure begin_impl_frame_deadline_closure_;
-  base::CancelableClosure poll_for_draw_triggers_closure_;
-  base::CancelableClosure advance_commit_state_closure_;
+
+  base::Closure begin_impl_frame_deadline_closure_;
+  base::Closure poll_for_draw_triggers_closure_;
+  base::Closure advance_commit_state_closure_;
+  base::CancelableClosure begin_impl_frame_deadline_task_;
+  base::CancelableClosure poll_for_draw_triggers_task_;
+  base::CancelableClosure advance_commit_state_task_;
 
   SchedulerStateMachine state_machine_;
   bool inside_process_scheduled_actions_;
