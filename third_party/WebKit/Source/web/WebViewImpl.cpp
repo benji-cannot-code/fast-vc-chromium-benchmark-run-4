b683480bd811d53a3fd0de67d84e6af8de090888
jdduke@chromium.org <jdduke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-04 18:09:29 +0000

    Interface cleanup for WebGestureCurveTarget
    This is the final patch in a three-part series towards early termination of
    flings when scrolling is impossible, cleaning up now unused portions of the
    WebGestureCurveTarget interface.
    
    Blink patch (#1)
    https://codereview.chromium.org/139493003/
    
    Chromium patch (#2):
    https://codereview.chromium.org/136173004/
    
    Review URL: https://codereview.chromium.org/217893004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170887 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebViewImpl.cpp
+++ third_party/WebKit/Source/web/WebViewImpl.cpp
@@ -597,11 +597,6 @@ bool WebViewImpl::scrollBy(const WebFloatSize& delta, const WebFloatSize& veloci
     return false;
 }
 
-void WebViewImpl::scrollBy(const WebFloatSize& delta)
-{
-    scrollBy(delta, WebFloatSize());
-}
-
 bool WebViewImpl::handleGestureEvent(const WebGestureEvent& event)
 {
     bool eventSwallowed = false;
