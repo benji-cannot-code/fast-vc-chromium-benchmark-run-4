ee3f712aa0df03a30a81e13acab816799659beaf
rune@opera.com <rune@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-04 16:14:35 +0000

    Use invalidation sets when selector contains adjacent combinators.
    When we found a direct or indirect adjacent combinator, we fell back on
    using a subtree recalc for all features in the selector in question. That
    is not necessary as we can resume the process of adding separate features
    to the invalidation sets once we encounter a non-adjacent combinator going
    leftwards.
    
    Example:
    
      .a .b + .c { }
    
    We have an empty invalidation set for 'c', 'b' currently needs to do a sub-
    tree recalc to recalculate the 'c' class element when 'b' is added or
    removed. However, once we see the descendant combinator left of '.b' we can
    get away with adding 'c' to the invalidation set for 'a' since 'c' will
    have to be in 'a's sub-tree.
    
    Eventually, I think we could merge collectFeaturesFromSelector with
    updateInvalidationSet to get rid of a pass through the selector.
    
    R=esprehn@chromium.org
    BUG=351613,335247
    
    Review URL: https://codereview.chromium.org/224593002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170881 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/RuleFeature.h
+++ third_party/WebKit/Source/core/css/RuleFeature.h
@@ -154,7 +154,7 @@ private:
 
     static void extractInvalidationSetFeature(const CSSSelector&, InvalidationSetFeatures&);
     const CSSSelector* extractInvalidationSetFeatures(const CSSSelector&, InvalidationSetFeatures&);
-    void addFeaturesToInvalidationSets(const CSSSelector&, const InvalidationSetFeatures&);
+    void addFeaturesToInvalidationSets(const CSSSelector&, const InvalidationSetFeatures&, bool wholeSubtree);
 
     void addClassToInvalidationSet(const AtomicString& className, Element&);
 
