0863a1894535846f6eedc466841171c159f515ca
mkwst@chromium.org <mkwst@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-04 13:57:12 +0000

    Password bubble: Add support for 'Never for this site'
    This patch converts the "Not now" button to a combobox containing two
    options, "Nope" (which is significantly clearer than "Not now", and
    matches Translate's language) and "Never for this site".
    
    BUG=357597
    R=markusheintz@chromium.org, vabr@chromium.org
    TBR=markusheintz@chromium.org
    
    Review URL: https://codereview.chromium.org/220773002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@261743 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/passwords/manage_passwords_bubble_ui_controller.cc
+++ chrome/browser/ui/passwords/manage_passwords_bubble_ui_controller.cc
@@ -71,6 +71,11 @@ void ManagePasswordsBubbleUIController::SavePassword() {
   form_manager_->Save();
 }
 
+void ManagePasswordsBubbleUIController::NeverSavePassword() {
+  DCHECK(form_manager_.get());
+  form_manager_->PermanentlyBlacklist();
+}
+
 void ManagePasswordsBubbleUIController::DidNavigateMainFrame(
     const content::LoadCommittedDetails& details,
     const content::FrameNavigateParams& params) {
--- chrome/browser/ui/views/passwords/manage_passwords_bubble_view.h
+++ chrome/browser/ui/views/passwords/manage_passwords_bubble_view.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/passwords/manage_passwords_bubble_model.h"
 #include "ui/views/bubble/bubble_delegate.h"
 #include "ui/views/controls/button/button.h"
+#include "ui/views/controls/combobox/combobox.h"
+#include "ui/views/controls/combobox/combobox_listener.h"
 #include "ui/views/controls/link.h"
 #include "ui/views/controls/link_listener.h"
 
@@ -27,6 +29,7 @@ class GridLayout;
 
 class ManagePasswordsBubbleView : public views::BubbleDelegateView,
                                   public views::ButtonListener,
+                                  public views::ComboboxListener,
                                   public views::LinkListener {
  public:
   enum FieldType { USERNAME_FIELD, PASSWORD_FIELD };
@@ -104,6 +107,9 @@ class ManagePasswordsBubbleView : public views::BubbleDelegateView,
   // views::LinkListener:
   virtual void LinkClicked(views::Link* source, int event_flags) OVERRIDE;
 
+  // Handles the event when the user changes an index of a combobox.
+  virtual void OnPerformAction(views::Combobox* source) OVERRIDE;
+
   // Singleton instance of the Password bubble. The Password bubble can only be
   // shown on the active browser window, so there is no case in which it will be
   // shown twice at the same time.
@@ -114,7 +120,8 @@ class ManagePasswordsBubbleView : public views::BubbleDelegateView,
 
   // The buttons that are shown in the bubble.
   views::BlueButton* save_button_;
-  views::LabelButton* cancel_button_;
+  views::Combobox* refuse_combobox_;
+
   views::Link* manage_link_;
   views::LabelButton* done_button_;
 
