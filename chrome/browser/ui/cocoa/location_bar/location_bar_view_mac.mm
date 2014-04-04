f6e747587fb63e04fbe37dc3571d5db0760fb254
hajimehoshi@chromium.org <hajimehoshi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-04 06:15:40 +0000

    Bug fix: Mac: Don't show translate icon as long as the flag is not used
    BUG=357148
    TEST=manual
    R=isharman@chromium.org
    
    Review URL: https://codereview.chromium.org/216443003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@261685 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/location_bar/location_bar_view_mac.mm
+++ chrome/browser/ui/cocoa/location_bar/location_bar_view_mac.mm
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/search_engines/template_url.h"
 #include "chrome/browser/search_engines/template_url_service.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
+#include "chrome/browser/translate/translate_service.h"
 #include "chrome/browser/translate/translate_tab_helper.h"
 #include "chrome/browser/ui/browser_instant_controller.h"
 #include "chrome/browser/ui/browser_list.h"
@@ -761,6 +762,9 @@ void LocationBarViewMac::ShowFirstRunBubbleInternal() {
 }
 
 void LocationBarViewMac::UpdateTranslateDecoration() {
+  if (!TranslateService::IsTranslateBubbleEnabled())
+    return;
+
   WebContents* web_contents = GetWebContents();
   if (!web_contents)
     return;
