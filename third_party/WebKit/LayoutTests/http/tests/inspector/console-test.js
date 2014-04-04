d59f28195f466cf02ca22a7f9660d0f3b7542ce9
sergeyv@chromium.org <sergeyv@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-04 12:50:32 +0000

    DevTools: Added "Show all messages" checkbox in consoleView
    BUG=
    
    Review URL: https://codereview.chromium.org/221213002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170860 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/inspector/console-test.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/console-test.js
@@ -40,7 +40,7 @@ InspectorTest.dumpConsoleMessages = function(printOriginatingCommand, dumpClassN
             if (dumpClassNames)
                 InspectorTest.addResult(classNames.join(" > "));
         } else {
-            var messageText = InspectorTest.prepareConsoleMessageText(element)
+            var messageText = InspectorTest.prepareConsoleMessageText(element);
             InspectorTest.addResult(messageText + (dumpClassNames ? " " + classNames.join(" > ") : ""));
         }
 
@@ -102,7 +102,7 @@ InspectorTest.dumpConsoleMessagesWithClasses = function(sortMessages) {
     var messageViews = WebInspector.ConsolePanel._view()._visibleViewMessages;
     for (var i = 0; i < messageViews.length; ++i) {
         var element = messageViews[i].toMessageElement();
-        var messageText = InspectorTest.prepareConsoleMessageText(element)
+        var messageText = InspectorTest.prepareConsoleMessageText(element);
         result.push(messageText + " " + element.getAttribute("class"));
     }
     if (sortMessages)
@@ -168,5 +168,24 @@ InspectorTest.waitUntilNthMessageReceived = function(count, callback)
     }
     InspectorTest.addSniffer(WebInspector.console, "addMessage", override, false);
 }
+
+InspectorTest.changeExecutionContext = function(namePrefix)
+{
+    WebInspector.inspectorView.panel("console");
+    var selector = WebInspector.ConsolePanel._view()._executionContextSelector._selectElement;
+    var option = selector.firstChild;
+    while (option) {
+        if (option.textContent && option.textContent.startsWith(namePrefix))
+            break;
+        option = option.nextSibling;
+    }
+    if (!option) {
+        InspectorTest.addResult("FAILED: context with prefix: "  + namePrefix + " not found in the context list");
+        return;
+    }
+    option.selected = true;
+    WebInspector.ConsolePanel._view()._executionContextChanged();
+}
+
 
 }
