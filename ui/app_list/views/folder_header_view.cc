7d4916a854703fbc25967607669e6e10fbde805b
jennyz@chromium.org <jennyz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-04 01:26:21 +0000

    Remove the red background color when user enters a folder name that exceeds the maximum length.
    BUG=359410
    
    Review URL: https://codereview.chromium.org/224503002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@261625 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/app_list/views/folder_header_view.cc
+++ ui/app_list/views/folder_header_view.cc
@@ -48,12 +48,6 @@ class FolderHeaderView::FolderNameView : public views::Textfield {
   virtual ~FolderNameView() {
   }
 
-  void Update() {
-    SetBackgroundColor(text().size() <= kMaxFolderNameChars
-                           ? kContentsBackgroundColor
-                           : SK_ColorRED);
-  }
-
  private:
   DISALLOW_COPY_AND_ASSIGN(FolderNameView);
 };
@@ -121,7 +115,6 @@ void FolderHeaderView::Update() {
   folder_name_view_->SetVisible(folder_name_visible_);
   if (folder_name_visible_) {
     folder_name_view_->SetText(base::UTF8ToUTF16(folder_item_->name()));
-    folder_name_view_->Update();
   }
 
   Layout();
@@ -194,7 +187,6 @@ void FolderHeaderView::ContentsChanged(views::Textfield* sender,
   if (!folder_item_)
     return;
 
-  folder_name_view_->Update();
   folder_item_->RemoveObserver(this);
   // Enforce the maximum folder name length in UI.
   std::string name = base::UTF16ToUTF8(
