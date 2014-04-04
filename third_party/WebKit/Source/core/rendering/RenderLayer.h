450724a44b17491d199798652db5733503e05220
schenney@chromium.org <schenney@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-04 21:30:32 +0000

    Remove a non-implemented method.
    The RenderLayer::enclosingFilterRepaintLayer method is declared but not
    defined. It has moved to RenderLayerRepainter.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/226493002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170901 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderLayer.h
+++ third_party/WebKit/Source/core/rendering/RenderLayer.h
@@ -256,7 +256,6 @@ public:
     RenderLayer* ancestorScrollingLayer() const;
 
     RenderLayer* enclosingFilterLayer(IncludeSelfOrNot = IncludeSelf) const;
-    RenderLayer* enclosingFilterRepaintLayer() const;
     bool hasAncestorWithFilterOutsets() const;
 
     bool canUseConvertToLayerCoords() const
