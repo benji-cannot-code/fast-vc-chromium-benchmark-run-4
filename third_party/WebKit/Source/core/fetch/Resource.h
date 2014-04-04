d91ee03830d277a66d87fee89dd5b243e6aa687d
jianli@chromium.org <jianli@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-04 22:31:50 +0000

    Revert 170905 "Speculative revert 170900 "Make most decoded reso..."
    > Speculative revert 170900 "Make most decoded resource logic private to Memor..."
    > 
    > http://build.chromium.org/p/chromium.webkit/builders/WebKit%20Mac10.6/builds/26089
    > 
    > > Make most decoded resource logic private to MemoryCache
    > > 
    > > BUG=
    > > 
    > > Review URL: https://codereview.chromium.org/222413005
    > 
    > TBR=japhet@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/225023016
    
    TBR=jianli@chromium.org
    
    Review URL: https://codereview.chromium.org/225023017
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170911 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/fetch/Resource.h
+++ third_party/WebKit/Source/core/fetch/Resource.h
@@ -56,7 +56,6 @@ class SharedBuffer;
 // This class also does the actual communication with the loader to obtain the resource from the network.
 class Resource {
     WTF_MAKE_NONCOPYABLE(Resource); WTF_MAKE_FAST_ALLOCATED;
-    friend class MemoryCache;
     friend class InspectorResource;
 
 public:
@@ -86,13 +85,6 @@ public:
 
     Resource(const ResourceRequest&, Type);
     virtual ~Resource();
-
-    // Determines the order in which CachedResources are evicted
-    // from the decoded resources cache.
-    enum CacheLiveResourcePriority {
-        CacheLiveResourcePriorityLow = 0,
-        CacheLiveResourcePriorityHigh
-    };
 
     virtual void load(ResourceFetcher*, const ResourceLoaderOptions&);
 
@@ -171,9 +163,6 @@ public:
     // FIXME: Remove the stringless variant once all the callsites' error messages are updated.
     bool passesAccessControlCheck(SecurityOrigin*);
     bool passesAccessControlCheck(SecurityOrigin*, String& errorDescription);
-
-    void setCacheLiveResourcePriority(CacheLiveResourcePriority);
-    unsigned cacheLiveResourcePriority() const { return m_cacheLiveResourcePriority; }
 
     void clearLoader();
 
@@ -282,7 +271,7 @@ protected:
 
     void setEncodedSize(size_t);
     void setDecodedSize(size_t);
-    void didAccessDecodedData(double timeStamp);
+    void didAccessDecodedData();
 
     virtual void switchClientsToRevalidatedResource();
     void clearResourceToRevalidate();
@@ -352,7 +341,6 @@ private:
 
     ResourceError m_error;
 
-    double m_lastDecodedAccessTime; // Used as a "thrash guard" in the cache
     double m_loadFinishTime;
 
     unsigned long m_identifier;
@@ -364,7 +352,6 @@ private:
     unsigned m_protectorCount;
 
     unsigned m_preloadResult : 2; // PreloadResult
-    unsigned m_cacheLiveResourcePriority : 2; // CacheLiveResourcePriority
     unsigned m_requestedFromNetworkingLayer : 1;
 
     unsigned m_loading : 1;
