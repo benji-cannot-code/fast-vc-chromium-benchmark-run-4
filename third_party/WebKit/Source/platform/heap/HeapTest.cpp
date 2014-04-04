7b52a13fc7df8fdcbe4e52e7a869a48459287954
fdegans@chromium.org <fdegans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-04 17:18:02 +0000

    Fix -Wunused-local-typedefs errors with GCC 4.7+
    BUG=321833
    
    Review URL: https://codereview.chromium.org/223673005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170884 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/heap/HeapTest.cpp
+++ third_party/WebKit/Source/platform/heap/HeapTest.cpp
@@ -2008,7 +2008,6 @@ TEST(HeapTest, HeapCollectionTypes)
     typedef HeapHashMap<int, Member<IntWrapper> > PrimitiveMember;
 
     typedef HeapHashSet<Member<IntWrapper> > MemberSet;
-    typedef HeapHashSet<WeakMember<IntWrapper> > WeakMemberSet;
 
     typedef HeapVector<Member<IntWrapper>, 2> MemberVector;
 
