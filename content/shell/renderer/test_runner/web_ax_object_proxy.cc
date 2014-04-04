4f51628a799d2170f28e77f7c9f2191b223bf871
dmazzoni@chromium.org <dmazzoni@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-04 19:04:50 +0000

    Force layout before computing bounds on an AX object.
    BUG=358674
    
    Review URL: https://codereview.chromium.org/224173003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@261821 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/renderer/test_runner/web_ax_object_proxy.cc
+++ content/shell/renderer/test_runner/web_ax_object_proxy.cc
@@ -616,18 +616,22 @@ std::string WebAXObjectProxy::StringValue() {
 }
 
 int WebAXObjectProxy::X() {
+  accessibility_object_.updateBackingStoreAndCheckValidity();
   return accessibility_object().boundingBoxRect().x;
 }
 
 int WebAXObjectProxy::Y() {
+  accessibility_object_.updateBackingStoreAndCheckValidity();
   return accessibility_object().boundingBoxRect().y;
 }
 
 int WebAXObjectProxy::Width() {
+  accessibility_object_.updateBackingStoreAndCheckValidity();
   return accessibility_object().boundingBoxRect().width;
 }
 
 int WebAXObjectProxy::Height() {
+  accessibility_object_.updateBackingStoreAndCheckValidity();
   return accessibility_object().boundingBoxRect().height;
 }
 
@@ -784,6 +788,9 @@ int WebAXObjectProxy::LineForIndex(int index) {
 
 std::string WebAXObjectProxy::BoundsForRange(int start, int end) {
   if (accessibility_object().role() != blink::WebAXRoleStaticText)
+    return std::string();
+
+  if (!accessibility_object_.updateBackingStoreAndCheckValidity())
     return std::string();
 
   int len = end - start;
