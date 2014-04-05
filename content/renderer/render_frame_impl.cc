93f75744d85881d1cc981b1e8ed44e6d79cb4148
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-05 07:36:22 +0000

    Update several new history related callbacks to use WebLocalFrame.
    BUG=346764
    
    Review URL: https://codereview.chromium.org/226263006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@261980 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -1719,7 +1719,7 @@ void RenderFrameImpl::didFailProvisionalLoad(blink::WebLocalFrame* frame,
 }
 
 void RenderFrameImpl::didCommitProvisionalLoad(
-    blink::WebFrame* frame,
+    blink::WebLocalFrame* frame,
     const blink::WebHistoryItem& item,
     blink::WebHistoryCommitType commit_type) {
   DocumentState* document_state =
@@ -1950,7 +1950,7 @@ void RenderFrameImpl::didFinishLoad(blink::WebLocalFrame* frame) {
                                       ds->request().url()));
 }
 
-void RenderFrameImpl::didNavigateWithinPage(blink::WebFrame* frame,
+void RenderFrameImpl::didNavigateWithinPage(blink::WebLocalFrame* frame,
     const blink::WebHistoryItem& item,
     blink::WebHistoryCommitType commit_type) {
   DCHECK(!frame_ || frame_ == frame);
