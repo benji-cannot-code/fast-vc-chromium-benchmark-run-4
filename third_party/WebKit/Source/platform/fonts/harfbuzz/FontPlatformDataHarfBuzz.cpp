68832941b2a8c83e82e6a82c1b9e42c9f616fe11
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-05 21:48:21 +0000

    Remove unnecessary includes from FontPlatfromData
    Remove unnecessary include directives from the shared HarfBuzz and the
    Linux version of FontPlatformData.cpp
    
    R=esprehn@chromium.org
    
    Review URL: https://codereview.chromium.org/226043008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170934 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/fonts/harfbuzz/FontPlatformDataHarfBuzz.cpp
+++ third_party/WebKit/Source/platform/fonts/harfbuzz/FontPlatformDataHarfBuzz.cpp
@@ -33,11 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/fonts/harfbuzz/FontPlatformDataHarfBuzz.h"
 
 #include "SkTypeface.h"
-#include "platform/LayoutTestSupport.h"
-#include "platform/NotImplemented.h"
-#include "platform/fonts/FontCache.h"
 #include "platform/fonts/harfbuzz/HarfBuzzFace.h"
-#include "public/platform/Platform.h"
 #include "wtf/text/WTFString.h"
 
 namespace WebCore {
