a78b890a4b80c1c1f74ef32abd4f6dcec9e0c682
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-05 08:02:18 +0000

    content: Update KeyCodeMapping code to chromium's coding style.
    - Put it in content namespace.
    - clang-format it.
    - KeyCodeMapping.* -> key_code_mapping.*
    
    BUG=331299
    TEST=content_unittests, layout tests
    TBR=jochen@chromium.org
    
    Review URL: https://codereview.chromium.org/225403012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@261994 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/renderer/test_runner/key_code_mapping.h
+++ content/shell/renderer/test_runner/key_code_mapping.h
@@ -1,14 +1,15 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2013 The Chromium Authors. All rights reserved.
+// Copyright 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_SHELL_RENDERER_TEST_RUNNER_KEYCODEMAPPING_H_
-#define CONTENT_SHELL_RENDERER_TEST_RUNNER_KEYCODEMAPPING_H_
+#ifndef CONTENT_SHELL_RENDERER_TEST_RUNNER_KEY_CODE_MAPPING_H_
+#define CONTENT_SHELL_RENDERER_TEST_RUNNER_KEY_CODE_MAPPING_H_
 
-namespace WebTestRunner {
+namespace content {
 
-// The keycodes match the values of the virtual keycodes found here http://msdn.microsoft.com/en-us/library/windows/desktop/dd375731(v=vs.85).aspx
+// The keycodes match the values of the virtual keycodes found here
+// http://msdn.microsoft.com/en-us/library/windows/desktop/dd375731(v=vs.85).aspx
 enum {
     VKEY_RETURN   = 0x0D,
     VKEY_ESCAPE   = 0x1B,
@@ -37,6 +38,6 @@ enum {
 // Map a windows keycode to a native keycode on defined(__linux__) && defined(TOOLKIT_GTK).
 int NativeKeyCodeForWindowsKeyCode(int keysym);
 
-}
+}  // namespace content
 
-#endif  // CONTENT_SHELL_RENDERER_TEST_RUNNER_KEYCODEMAPPING_H_
+#endif  // CONTENT_SHELL_RENDERER_TEST_RUNNER_KEY_CODE_MAPPING_H_
