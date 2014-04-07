8c3ec2eefa913d3dc8d9be03f2d87c2e6b408023
akuegel@chromium.org <akuegel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-07 10:35:16 +0000

    Disable the import link when user creation is in progress.
    BUG=360021
    TEST=Create a supervised user. Observe that import link is disabled.
    
    Review URL: https://codereview.chromium.org/226093002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262107 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/manage_profile_overlay.js
+++ chrome/browser/resources/options/manage_profile_overlay.js
@@ -681,6 +681,7 @@ cr.define('options', function() {
       $('create-profile-name').disabled = inProgress;
       $('create-shortcut').disabled = inProgress;
       $('create-profile-ok').disabled = inProgress;
+      $('import-existing-managed-user-link').disabled = inProgress;
 
       $('create-profile-throbber').hidden = !inProgress;
     },
@@ -792,7 +793,7 @@ cr.define('options', function() {
 
       var importManagedUserElement = $('import-existing-managed-user-link');
       importManagedUserElement.hidden = false;
-      importManagedUserElement.disabled = !enable;
+      importManagedUserElement.disabled = !enable || this.createInProgress_;
       importManagedUserElement.textContent = enable ?
           loadTimeData.getString('importExistingManagedUserLink') :
           loadTimeData.getString('signInToImportManagedUsers');
