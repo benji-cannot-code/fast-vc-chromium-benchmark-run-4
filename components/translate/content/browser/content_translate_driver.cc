19d9b7b7045c4b9707c33d564bf215f1fc335745
blundell@chromium.org <blundell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-07 22:51:40 +0000

    Eliminate calls to BrowserContext::IsOffTheRecord in TranslateManager
    These calls are now abstracted through TranslateDriver.
    
    BUG=360008
    
    Review URL: https://codereview.chromium.org/226043010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262235 0039d316-1c4b-4281-b951-d872f2087c98

--- components/translate/content/browser/content_translate_driver.cc
+++ components/translate/content/browser/content_translate_driver.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "components/translate/content/common/translate_messages.h"
+#include "content/public/browser/browser_context.h"
 #include "content/public/browser/navigation_controller.h"
 #include "content/public/browser/navigation_details.h"
 #include "content/public/browser/navigation_entry.h"
@@ -88,4 +89,8 @@ void ContentTranslateDriver::RevertTranslation() {
   content::WebContents* web_contents = navigation_controller_->GetWebContents();
   web_contents->GetRenderViewHost()->Send(new ChromeViewMsg_RevertTranslation(
       web_contents->GetRenderViewHost()->GetRoutingID(), entry->GetPageID()));
+}
+
+bool ContentTranslateDriver::IsOffTheRecord() {
+  return navigation_controller_->GetBrowserContext()->IsOffTheRecord();
 }
