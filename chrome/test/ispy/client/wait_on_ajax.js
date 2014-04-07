414a131c80402257a518872b422eb17854661fe3
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-07 23:00:08 +0000

    undelete ispy and put it in chrome/test/ispy
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262239 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/ispy/client/wait_on_ajax.js
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var target = document.body;
+var callback = arguments[arguments.length - 1]
+
+var timeout_id = setTimeout(function() {
+  callback()
+}, 5000);
+
+var observer = new MutationObserver(function(mutations) {
+  clearTimeout(timeout_id);
+  timeout_id = setTimeout(function() {
+    callback();
+  }, 5000);
+}).observe(target, {attributes: true, childList: true,
+  characterData: true, subtree: true});
