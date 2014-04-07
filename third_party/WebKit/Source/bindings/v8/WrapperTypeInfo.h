427e0de71f519158bfc89dcacf6c72876f19f3f6
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-07 13:02:49 +0000

    Oilpan: Support GarbageCollected objects in WrapperTypeInfo
    Now that we're going to enable oilpan by default in some modules, we need to distinguish three types of objects:
    
    (1) GarbageCollectedObject
    (2) WillBeGarbageCollectedObject
    (3) RefCountedObject
    
    Currently we support only (2) and (3). This CL adds support for (1) to WrapperTypeInfo.
    
    BUG=340522
    
    Review URL: https://codereview.chromium.org/225263014
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170965 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/WrapperTypeInfo.h
+++ third_party/WebKit/Source/bindings/v8/WrapperTypeInfo.h
@@ -64,6 +64,12 @@ namespace WebCore {
         WrapperTypeExceptionPrototype
     };
 
+    enum WrappedObjectType {
+        GarbageCollectedObject,
+        WillBeGarbageCollectedObject,
+        RefCountedObject,
+    };
+
     inline void setObjectGroup(void* object, const v8::Persistent<v8::Object>& wrapper, v8::Isolate* isolate)
     {
         isolate->SetObjectGroupId(wrapper, v8::UniqueId(reinterpret_cast<intptr_t>(object)));
@@ -139,7 +145,7 @@ namespace WebCore {
         const InstallPerContextEnabledPrototypePropertiesFunction installPerContextEnabledMethodsFunction;
         const WrapperTypeInfo* parentClass;
         const WrapperTypePrototype wrapperTypePrototype;
-        const bool isGarbageCollected;
+        const WrappedObjectType wrappedObjectType;
     };
 
 
@@ -190,20 +196,25 @@ namespace WebCore {
     inline void releaseObject(v8::Handle<v8::Object> wrapper)
     {
         const WrapperTypeInfo* typeInfo = toWrapperTypeInfo(wrapper);
-#if ENABLE(OILPAN)
-        if (typeInfo->isGarbageCollected) {
+        if (typeInfo->wrappedObjectType == GarbageCollectedObject) {
             const PersistentNode* handle = toPersistentHandle(wrapper);
             // This will be null iff a wrapper for a hidden wrapper object,
             // see V8DOMWrapper::setNativeInfoForHiddenWrapper().
             delete handle;
+        } else if (typeInfo->wrappedObjectType == WillBeGarbageCollectedObject) {
+#if ENABLE(OILPAN)
+            const PersistentNode* handle = toPersistentHandle(wrapper);
+            // This will be null iff a wrapper for a hidden wrapper object,
+            // see V8DOMWrapper::setNativeInfoForHiddenWrapper().
+            delete handle;
+#else
+            ASSERT(typeInfo->derefObjectFunction);
+            typeInfo->derefObjectFunction(toNative(wrapper));
+#endif
         } else {
             ASSERT(typeInfo->derefObjectFunction);
             typeInfo->derefObjectFunction(toNative(wrapper));
         }
-#else
-        ASSERT(typeInfo->derefObjectFunction);
-        typeInfo->derefObjectFunction(toNative(wrapper));
-#endif
     }
 
     struct WrapperConfiguration {
