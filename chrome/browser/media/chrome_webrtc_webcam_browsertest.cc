a6c069c6aea2892a1645efab12c7f424e339ae38
phoglund@chromium.org <phoglund@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-07 10:48:45 +0000

    Disabled HD physical webcam tests on Windows.
    Turns out we have a regression with C920 webcams on Windows, which the
    new test found. Disabling the test until someone can look at it.
    
    TBR=henrika@chromium.org
    BUG=360512
    
    Review URL: https://codereview.chromium.org/227523002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262108 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media/chrome_webrtc_webcam_browsertest.cc
+++ chrome/browser/media/chrome_webrtc_webcam_browsertest.cc
@@ -66,6 +66,9 @@ IN_PROC_BROWSER_TEST_F(WebRtcWebcamBrowserTest,
   WaitForVideoToPlay(tab);
   EXPECT_EQ("640x360", GetStreamSize(tab, "local-view"));
   CloseLastLocalStream(tab);
+
+  // Broken on Win for C920 webcams: see http://crbug.com/360512.
+#if !defined(OS_WIN)
   GetUserMediaWithSpecificConstraintsAndAccept(tab,
                                                kAudioVideoCallConstraints720p);
   StartDetectingVideo(tab, "local-view");
@@ -78,4 +81,5 @@ IN_PROC_BROWSER_TEST_F(WebRtcWebcamBrowserTest,
   WaitForVideoToPlay(tab);
   EXPECT_EQ("1920x1080", GetStreamSize(tab, "local-view"));
   CloseLastLocalStream(tab);
+#endif
 }
