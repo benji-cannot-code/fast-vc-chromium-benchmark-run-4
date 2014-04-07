34db28fb1c5696c8ca482884a66e58c686043f76
tbarzic@chromium.org <tbarzic@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-07 23:54:07 +0000

    Remove GetPkcs11Id from x509_certificate_model
    We have the same function in chromeos::CertLoader.
    CertLibrary, which is the only user of x509_certificate_model::GetPkcs11Id,
    already depends on the CertLoader, so there is no real reson not to use
    CertLoader::GetPkcs11IdForCert.
    
    Also, update CertLibrary::GetCertIndexByPEM and
    CertLibrary::GetCertIndexByPkcs11 to work only for a single certificate
    type (server ca and client certificates respectively), instead of taking
    the certificate type as param.
    
    BUG=236978
    
    Review URL: https://codereview.chromium.org/182313004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262257 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/options/vpn_config_view.cc
+++ chrome/browser/chromeos/options/vpn_config_view.cc
@@ -448,8 +448,7 @@ const std::string VPNConfigView::GetServerCACertPEM() const {
     return std::string();
   } else {
     int cert_index = index - 1;
-    return CertLibrary::Get()->GetCertPEMAt(
-        CertLibrary::CERT_TYPE_SERVER_CA, cert_index);
+    return CertLibrary::Get()->GetServerCACertPEMAt(cert_index);
   }
 }
 
@@ -459,8 +458,7 @@ const std::string VPNConfigView::GetUserCertID() const {
   } else {
     // Certificates are listed in the order they appear in the model.
     int index = user_cert_combobox_ ? user_cert_combobox_->selected_index() : 0;
-    return CertLibrary::Get()->GetCertPkcs11IdAt(
-        CertLibrary::CERT_TYPE_USER, index);
+    return CertLibrary::Get()->GetUserCertPkcs11IdAt(index);
   }
 }
 
@@ -887,8 +885,8 @@ void VPNConfigView::Refresh() {
     server_ca_cert_combobox_->ModelChanged();
     if (enable_server_ca_cert_ && !ca_cert_pem_.empty()) {
       // Select the current server CA certificate in the combobox.
-      int cert_index = CertLibrary::Get()->GetCertIndexByPEM(
-          CertLibrary::CERT_TYPE_SERVER_CA, ca_cert_pem_);
+      int cert_index =
+          CertLibrary::Get()->GetServerCACertIndexByPEM(ca_cert_pem_);
       if (cert_index >= 0) {
         // Skip item for "Default"
         server_ca_cert_combobox_->SetSelectedIndex(1 + cert_index);
@@ -903,8 +901,8 @@ void VPNConfigView::Refresh() {
   if (user_cert_combobox_) {
     user_cert_combobox_->ModelChanged();
     if (enable_user_cert_ && !client_cert_id_.empty()) {
-      int cert_index = CertLibrary::Get()->GetCertIndexByPkcs11Id(
-          CertLibrary::CERT_TYPE_USER, client_cert_id_);
+      int cert_index =
+          CertLibrary::Get()->GetUserCertIndexByPkcs11Id(client_cert_id_);
       if (cert_index >= 0)
         user_cert_combobox_->SetSelectedIndex(cert_index);
       else
--- chrome/browser/chromeos/options/wifi_config_view.cc
+++ chrome/browser/chromeos/options/wifi_config_view.cc
@@ -825,8 +825,7 @@ std::string WifiConfigView::GetEapServerCaCertPEM() const {
     return std::string();
   } else {
     int cert_index = index - 1;
-    return CertLibrary::Get()->GetCertPEMAt(
-        CertLibrary::CERT_TYPE_SERVER_CA, cert_index);
+    return CertLibrary::Get()->GetServerCACertPEMAt(cert_index);
   }
 }
 
@@ -848,8 +847,7 @@ std::string WifiConfigView::GetEapClientCertPkcs11Id() const {
   } else {
     // Certificates are listed in the order they appear in the model.
     int index = user_cert_combobox_->selected_index();
-    return CertLibrary::Get()->GetCertPkcs11IdAt(
-        CertLibrary::CERT_TYPE_USER, index);
+    return CertLibrary::Get()->GetUserCertPkcs11IdAt(index);
   }
 }
 
@@ -1280,8 +1278,8 @@ void WifiConfigView::InitFromProperties(
       }
     } else {
       // Select the certificate if available.
-      int cert_index = CertLibrary::Get()->GetCertIndexByPEM(
-          CertLibrary::CERT_TYPE_SERVER_CA, eap_ca_cert_pem);
+      int cert_index =
+          CertLibrary::Get()->GetServerCACertIndexByPEM(eap_ca_cert_pem);
       if (cert_index >= 0) {
         // Skip item for "Default".
         server_ca_cert_combobox_->SetSelectedIndex(1 + cert_index);
@@ -1298,8 +1296,8 @@ void WifiConfigView::InitFromProperties(
     properties.GetStringWithoutPathExpansion(
         shill::kEapCertIdProperty, &eap_cert_id);
     if (!eap_cert_id.empty()) {
-      int cert_index = CertLibrary::Get()->GetCertIndexByPkcs11Id(
-          CertLibrary::CERT_TYPE_USER, eap_cert_id);
+      int cert_index =
+          CertLibrary::Get()->GetUserCertIndexByPkcs11Id(eap_cert_id);
       if (cert_index >= 0)
         user_cert_combobox_->SetSelectedIndex(cert_index);
     }
