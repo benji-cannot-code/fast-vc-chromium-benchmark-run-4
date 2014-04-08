c3ef1d6fb6dd1693d2f650b703c2507d7024ef06
shrikant@chromium.org <shrikant@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-08 05:25:01 +0000

    Instead of directly using AddRef/Release on browser process replaced it with Increment/Decrement keep alive count. Short Story: Browser::OnWindowClosing() function checks to see if either trying to quit flag is set or if keep alive count has reached zero to decide if it should shutdown browser or not. (There are other conditions but I am elaborting one which is part of current problem.) In ChromeMetroViewerProcessHost class we were using AddRef/Release directly on browser process, which wasn't incrementing this keep alive count and hence after last window was closed we were partially shutting down.
    One concern with DecrementKeepAliveCount was that it will automatically close all browsers, but after checking it appears guarded with GetTotalBrowserCount()==0 call.
    
    How is this all related to attached bug? Because 
    SadTabHelper::RenderProcessGone checks if browser_shutdown::GetShutdownType() != NOT_VALID before showing SadTab and with above story we were partially in shutdown mode.
    
    BUG=359040
    
    Review URL: https://codereview.chromium.org/215743004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262330 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metro_viewer/chrome_metro_viewer_process_host_aurawin.cc
+++ chrome/browser/metro_viewer/chrome_metro_viewer_process_host_aurawin.cc
@@ -70,7 +70,7 @@ ChromeMetroViewerProcessHost::ChromeMetroViewerProcessHost()
     : MetroViewerProcessHost(
           content::BrowserThread::GetMessageLoopProxyForThread(
               content::BrowserThread::IO)) {
-  g_browser_process->AddRefModule();
+  chrome::IncrementKeepAliveCount();
 }
 
 void ChromeMetroViewerProcessHost::OnChannelError() {
@@ -83,7 +83,7 @@ void ChromeMetroViewerProcessHost::OnChannelError() {
   ::SetEnvironmentVariableA(env_vars::kMetroConnected, NULL);
 
   aura::RemoteWindowTreeHostWin::Instance()->Disconnected();
-  g_browser_process->ReleaseModule();
+  chrome::DecrementKeepAliveCount();
 
   // If browser is trying to quit, we shouldn't reenter the process.
   // TODO(shrikant): In general there seem to be issues with how AttemptExit
