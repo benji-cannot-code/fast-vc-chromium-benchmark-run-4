eaedc69647d436be93f6123d23c137c90d7d9680
jdduke@chromium.org <jdduke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-08 05:08:51 +0000

    Fix several double-tap gesture related issues
    Always dispatch GestureTapCancel if a GestureShowPress fired before a
    GestureDoubleTap, properly clearing up the active state. Also fix an
    issue where a secondary pointer is pressed while a double-tap drag zoom gesture
    is active, as well as a potential crash when double-tap is enabled during
    a touch sequence.
    
    BUG=332418
    
    Review URL: https://codereview.chromium.org/226423003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262322 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/input/touchscreen_tap_suppression_controller_stub.cc
+++ content/browser/renderer_host/input/touchscreen_tap_suppression_controller_stub.cc
@@ -24,17 +24,8 @@ void TouchscreenTapSuppressionController::GestureFlingCancelAck(
     bool /*processed*/) {
 }
 
-bool TouchscreenTapSuppressionController::ShouldDeferGestureTapDown(
+bool TouchscreenTapSuppressionController::FilterTapEvent(
     const GestureEventWithLatencyInfo& /*event*/) {
-  return false;
-}
-
-bool TouchscreenTapSuppressionController::ShouldDeferGestureShowPress(
-    const GestureEventWithLatencyInfo& /*event*/) {
-  return false;
-}
-
-bool TouchscreenTapSuppressionController::ShouldSuppressGestureTapEnd() {
   return false;
 }
 
