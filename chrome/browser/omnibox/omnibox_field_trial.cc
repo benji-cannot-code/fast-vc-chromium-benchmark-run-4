72ec05c48f57a32f64a6429d2d56d2fcd43081a0
mpearson@chromium.org <mpearson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-08 21:27:50 +0000

    Omnibox: Launch HistoryQuick Provider's "Discount Few Visits" Scoring
    Turn on the scoring mode by default, per eval in bug 330214, comment 13.
    
    TBR=sky
    
    BUG=327085
    
    Review URL: https://codereview.chromium.org/227853003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262508 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/omnibox/omnibox_field_trial.cc
+++ chrome/browser/omnibox/omnibox_field_trial.cc
@@ -440,12 +440,6 @@ int OmniboxFieldTrial::HQPBookmarkValue() {
   return bookmark_value;
 }
 
-bool OmniboxFieldTrial::HQPDiscountFrecencyWhenFewVisits() {
-  return chrome_variations::GetVariationParamValue(
-      kBundledExperimentFieldTrialName,
-      kHQPDiscountFrecencyWhenFewVisitsRule) == "true";
-}
-
 bool OmniboxFieldTrial::HQPAllowMatchInTLDValue() {
   return chrome_variations::GetVariationParamValue(
       kBundledExperimentFieldTrialName,
@@ -469,8 +463,6 @@ const char OmniboxFieldTrial::kReorderForLegalDefaultMatchRule[] =
     "ReorderForLegalDefaultMatch";
 const char OmniboxFieldTrial::kHQPBookmarkValueRule[] =
     "HQPBookmarkValue";
-const char OmniboxFieldTrial::kHQPDiscountFrecencyWhenFewVisitsRule[] =
-    "HQPDiscountFrecencyWhenFewVisits";
 const char OmniboxFieldTrial::kHQPAllowMatchInTLDRule[] = "HQPAllowMatchInTLD";
 const char OmniboxFieldTrial::kHQPAllowMatchInSchemeRule[] =
     "HQPAllowMatchInScheme";
