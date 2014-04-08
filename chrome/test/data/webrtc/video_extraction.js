e97fa9bd9953ae1b9b1f358fe0d6165ec0da8a6f
phoglund@chromium.org <phoglund@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-08 14:20:40 +0000

    Fixed quality tests after PyAuto test purge.
    All test resources and reference files have been moved to be handled by
    the webrtc.DEPS solution. The audio test used to find its files in the
    pyauto_private folder, which has been removed. Instead I've uploaded
    the files to the webrtc-resources bundle and put .sha1 files in the
    webrtc test data folder, which are processed by the webrtc.DEPS
    solution.
    
    The downloading of the files is only triggered if you have the WebRTC
    solution, so this should not burden people who do not want to run the
    tests.
    
    The reason for moving the files is that the tests are more healthy
    if they don't have to look for resources outside the chrome/test/data
    folder. I'm able to straighten out some hacks as a result.
    
    Also fixing an issue where https://codereview.chromium.org/222873002
    removed the web socket handler for the video quality test.
    
    Depends on https://codereview.chromium.org/225263018.
    
    BUG=360033,359579
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/227633002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262398 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/webrtc/video_extraction.js
+++ chrome/test/data/webrtc/video_extraction.js
@@ -191,6 +191,7 @@ function sendFrames() {
   if (!gWebSocketOpened) {
     console.log('WebSocket connection is not yet open');
     setTimeout(function() { sendFrames(); }, 100);
+    return;
   }
 
   progressBar = document.getElementById('progress-bar');
