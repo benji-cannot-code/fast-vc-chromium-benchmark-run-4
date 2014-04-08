6033a8f71fae375a94ca489c5e688c5cf1336742
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-08 12:36:34 +0000

    Revert of Hook up gin platform to blink, and explicitly manage the main thread isolate (https://codereview.chromium.org/227653002/)
    Reason for revert:
    Broke browser_tests on Linux: http://build.chromium.org/p/chromium.webkit/builders/Linux%20Tests/builds/35802
    
    
    Original issue's description:
    > Hook up gin platform to blink, and explicitly manage the main thread isolate
    > 
    > We'll soon get rid of the v8 default isolate, so we'll need to manage
    > our own.
    > 
    > To test the platform implementation, run with
    >   --js-flags="--noparallel_sweeping --concurrent-sweeping --job-based-sweeping"
    > 
    > BUG=359977
    > R=dcarney@chromium.org,haraken@chromium.org
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=171036
    
    TBR=dcarney@chromium.org,haraken@chromium.org,jochen@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=359977
    
    Review URL: https://codereview.chromium.org/225023038
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171053 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebKit.cpp
+++ third_party/WebKit/Source/web/WebKit.cpp
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/page/Page.h"
 #include "core/frame/Settings.h"
 #include "core/workers/WorkerGlobalScopeProxy.h"
-#include "gin/public/v8_platform.h"
 #include "platform/LayoutTestSupport.h"
 #include "platform/Logging.h"
 #include "platform/graphics/ImageDecodingStore.h"
@@ -106,9 +105,7 @@ void initialize(Platform* platform)
 {
     initializeWithoutV8(platform);
 
-    v8::V8::InitializePlatform(gin::V8Platform::Get());
-    v8::Isolate* isolate = v8::Isolate::New();
-    isolate->Enter();
+    v8::Isolate* isolate = v8::Isolate::GetCurrent();
     WebCore::V8Initializer::initializeMainThreadIfNeeded(isolate);
     v8::V8::SetEntropySource(&generateEntropy);
     v8::V8::SetArrayBufferAllocator(WebCore::v8ArrayBufferAllocator());
@@ -209,11 +206,9 @@ void shutdown()
 
     ASSERT(s_isolateInterruptor);
     WebCore::ThreadState::current()->removeInterruptor(s_isolateInterruptor);
-    v8::Isolate* isolate = WebCore::V8PerIsolateData::mainThreadIsolate();
 
-    WebCore::V8PerIsolateData::dispose(isolate);
-    isolate->Exit();
-    isolate->Dispose();
+    WebCore::V8PerIsolateData::dispose(WebCore::V8PerIsolateData::mainThreadIsolate());
+    v8::V8::Dispose();
 
     shutdownWithoutV8();
 }
