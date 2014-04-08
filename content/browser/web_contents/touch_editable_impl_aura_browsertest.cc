f0f5193045af0445fac284baaa5aa40fde5ad06b
tapted@chromium.org <tapted@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-08 22:02:21 +0000

    Revert of [Android] Provide unhandled tap event notifications (https://codereview.chromium.org/218633008/)
    Reason for revert:
    TouchEditableImplAuraTest.TouchCursorInTextfieldTest failing in the waterfall on Linux ChromiumOS Tests (2) 
    link: http://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Tests%20%282%29/builds/22962
    
    Original issue's description:
    > [Android] Reland "Provide unhandled tap event notifications"
    > 
    > Allow notifications of unhandled taps via the GestureStateListener.  This change
    > makes GestureTap events blocking, but there are several use-cases for which this
    > will be necessary, e.g., WebView and contextual search.
    > 
    > This was landed in r26143 and reverted in r261470 due to to a failure in
    > TouchEditableImplAuraTest.TouchCursorInTextfieldTest.  The test has been fixed.
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=262497
    
    TBR=mkosiba@chromium.org,donnd@chromium.org,tedchoc@chromium.org,tdresser@chromium.org,sky@chromium.org,jdduke@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/229373002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262517 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/web_contents/touch_editable_impl_aura_browsertest.cc
+++ content/browser/web_contents/touch_editable_impl_aura_browsertest.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/browser/web_contents/web_contents_view_aura.h"
-#include "content/common/input/web_input_event_traits.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents_view.h"
 #include "content/public/common/content_switches.h"
@@ -326,16 +325,9 @@ class TouchEditableImplAuraTest : public ContentBrowserTest {
     // Tap textfield
     touch_editable->Reset();
     generator.GestureTapAt(gfx::Point(bounds.x() + 50, bounds.y() + 40));
-    // Only wait for the tap gesture ack if it's received asynchronously.
-    if (WebInputEventTraits::IgnoresAckDisposition(
-            blink::WebInputEvent::GestureTap)) {
-      touch_editable->WaitForSelectionChangeCallback();
-      touch_editable->Reset();
-    } else {
-      touch_editable->Reset();
-      touch_editable->WaitForGestureAck();
-      touch_editable->WaitForSelectionChangeCallback();
-    }
+    // Tap Down and Tap acks are sent synchronously.
+    touch_editable->WaitForSelectionChangeCallback();
+    touch_editable->Reset();
 
     // Check if cursor handle is showing.
     ui::TouchSelectionController* controller =
--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -1291,15 +1291,6 @@ public class ContentViewCore
 
     @SuppressWarnings("unused")
     @CalledByNative
-    private void onTapEventNotConsumed(int x, int y) {
-        for (mGestureStateListenersIterator.rewind();
-                mGestureStateListenersIterator.hasNext();) {
-            mGestureStateListenersIterator.next().onUnhandledTapEvent(x, y);
-        }
-    }
-
-    @SuppressWarnings("unused")
-    @CalledByNative
     private void onDoubleTapEventAck() {
         temporarilyHideTextHandles();
     }
