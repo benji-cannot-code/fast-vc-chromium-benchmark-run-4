95d9e915b53fbc1ed346faf5cf34ee930b9471ef
sque@chromium.org <sque@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-08 14:12:14 +0000

    metrics: Store multiple protobufs before each UMA upload
    Also allows us to remove the collection state. The size of the protobuf
    vector tells us whether there is perf data ready to upload.
    
    BUG=chromium:358778
    TEST=build successfully
    
    Signed-off-by: Simon Que <sque@chromium.org>
    
    Review URL: https://codereview.chromium.org/226303004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262393 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metrics/perf_provider_chromeos.h
+++ chrome/browser/metrics/perf_provider_chromeos.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_METRICS_PERF_PROVIDER_CHROMEOS_H_
 
 #include <string>
+#include <vector>
 
 #include "base/basictypes.h"
 #include "base/threading/non_thread_safe.h"
@@ -26,19 +27,11 @@ class PerfProvider : public base::NonThreadSafe {
   PerfProvider();
   ~PerfProvider();
 
-  // Gets the collected perf data protobuf and writes it to |perf_data_proto|.
-  // Returns true if it wrote to |perf_data_proto|.
-  bool GetPerfData(PerfDataProto* perf_data_proto);
+  // Writes collected perf data protobufs to |perf_data|. Clears all the stored
+  // perf data. Returns true if it wrote to |perf_data|.
+  bool GetPerfData(std::vector<PerfDataProto>* perf_data);
 
  private:
-  enum PerfDataState {
-    // Indicates that we are ready to collect perf data.
-    READY_TO_COLLECT,
-
-    // Indicates that we are ready to upload perf data.
-    READY_TO_UPLOAD,
-  };
-
   // Starts an internal timer to start collecting perf data. The timer is set to
   // trigger |interval| after this function call.
   void ScheduleCollection(const base::TimeDelta& interval);
@@ -58,11 +51,8 @@ class PerfProvider : public base::NonThreadSafe {
       scoped_ptr<WindowedIncognitoObserver> incognito_observer,
       const std::vector<uint8>& data);
 
-  // The internal state can be one of the enum values above.
-  PerfDataState state_;
-
-  // Protobuf that has the perf data.
-  PerfDataProto perf_data_proto_;
+  // Vector of perf data protobufs.
+  std::vector<PerfDataProto> cached_perf_data_;
 
   // For scheduling collection of perf data.
   base::OneShotTimer<PerfProvider> timer_;
@@ -73,6 +63,6 @@ class PerfProvider : public base::NonThreadSafe {
   DISALLOW_COPY_AND_ASSIGN(PerfProvider);
 };
 
-}  // namespace system
+}  // namespace metrics
 
 #endif  // CHROME_BROWSER_METRICS_PERF_PROVIDER_CHROMEOS_H_
