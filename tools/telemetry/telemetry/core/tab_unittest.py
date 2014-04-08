3853632a4bf85165974773077488aa1518ef3831
bajones@chromium.org <bajones@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-08 23:43:26 +0000

    Disable Telemetry screenshot unittest on mac till we can make it more stable
    BUG=358664
    
    Review URL: https://codereview.chromium.org/228943006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262555 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/tab_unittest.py
+++ tools/telemetry/telemetry/core/tab_unittest.py
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import logging
 
+from telemetry import test
 from telemetry.core import util
 from telemetry.core import exceptions
 from telemetry.unittest import tab_test_case
@@ -94,6 +95,8 @@ class GpuTabTest(tab_test_case.TabTestCase):
     self._extra_browser_args = ['--enable-gpu-benchmarking']
     super(GpuTabTest, self).setUp()
 
+  # Test flaky on mac: http://crbug.com/358664
+  @test.Disabled('mac')
   def testScreenshot(self):
     if not self._tab.screenshot_supported:
       logging.warning('Browser does not support screenshots, skipping test.')
