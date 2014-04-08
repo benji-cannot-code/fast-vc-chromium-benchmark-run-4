3082149b8dec288305c0c3ac8ba1345105029d51
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-08 08:37:15 +0000

    Check cache only durnig startup
    - Made the expiration time longer (7s)
    - Removed AvoidNotificationOnDuplicateEvent test as it's testing basically the same thing as ForceUpdateAfterCacheExpiration.
    - this is formatted by "git cl format"
    
    BUG=350603
    TEST=ForceUpdateAfterCacheExpiration has been updated
    
    Review URL: https://codereview.chromium.org/227433008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262351 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/display/chromeos/x11/native_display_event_dispatcher_x11.cc
+++ ui/display/chromeos/x11/native_display_event_dispatcher_x11.cc
@@ -14,14 +14,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ui {
 
 // static
-const int NativeDisplayEventDispatcherX11::kCachedOutputsExpirationMs = 5000;
+const int NativeDisplayEventDispatcherX11::kUseCacheAfterStartupMs = 7000;
 
 NativeDisplayEventDispatcherX11::NativeDisplayEventDispatcherX11(
     NativeDisplayDelegateX11::HelperDelegate* delegate,
     int xrandr_event_base)
     : delegate_(delegate),
       xrandr_event_base_(xrandr_event_base),
-      tick_clock_(new base::DefaultTickClock) {}
+      tick_clock_(new base::DefaultTickClock) {
+  startup_time_ = tick_clock_->NowTicks();
+}
 
 NativeDisplayEventDispatcherX11::~NativeDisplayEventDispatcherX11() {}
 
@@ -59,10 +61,10 @@ uint32_t NativeDisplayEventDispatcherX11::DispatchEvent(
           << " mode=" << output_change_event->mode
           << " action=" << (connected ? "connected" : "disconnected");
 
-  bool force_notify = last_notified_time_.is_null() ||
-      (tick_clock_->NowTicks() - last_notified_time_).InMilliseconds() >=
-      kCachedOutputsExpirationMs;
-  if (!force_notify) {
+  bool check_cache = (tick_clock_->NowTicks() - startup_time_)
+                         .InMilliseconds() <= kUseCacheAfterStartupMs;
+
+  if (check_cache) {
     bool found_changed_output = false;
     const std::vector<DisplaySnapshot*>& cached_outputs =
         delegate_->GetCachedOutputs();
@@ -92,8 +94,6 @@ uint32_t NativeDisplayEventDispatcherX11::DispatchEvent(
     }
   }
 
-  last_notified_time_ = tick_clock_->NowTicks();
-
   delegate_->NotifyDisplayObservers();
 
   return ui::POST_DISPATCH_PERFORM_DEFAULT;
@@ -102,6 +102,7 @@ uint32_t NativeDisplayEventDispatcherX11::DispatchEvent(
 void NativeDisplayEventDispatcherX11::SetTickClockForTest(
     scoped_ptr<base::TickClock> tick_clock) {
   tick_clock_ = tick_clock.Pass();
+  startup_time_ = tick_clock_->NowTicks();
 }
 
 }  // namespace ui
