cc3d064ecee961d2321edd4f4ad54d45f33ecac7
jdonnelly@chromium.org <jdonnelly@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-08 21:46:02 +0000

    Clean up order and visibility of Views location bar classes' overrides.
    Per discussion in https://codereview.chromium.org/200783003, the override sections should be in the same order as the superclass declarations and overriden methods should be declared private where possible.
    
    Review URL: https://codereview.chromium.org/225873003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262515 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/location_bar/origin_chip_view.h
+++ chrome/browser/ui/views/location_bar/origin_chip_view.h
@@ -72,9 +72,15 @@ class OriginChipView : public views::LabelButton,
   int host_label_x() const { return host_label_->x(); }
 
   // views::LabelButton:
+  virtual gfx::Size GetPreferredSize() OVERRIDE;
+
+ private:
+  // Sets an image grid to represent the current security state.
+  void SetBorderImages(const int images[3][9]);
+
+  // views::LabelButton:
   virtual void AnimationProgressed(const gfx::Animation* animation) OVERRIDE;
   virtual void AnimationEnded(const gfx::Animation* animation) OVERRIDE;
-  virtual gfx::Size GetPreferredSize() OVERRIDE;
   virtual void Layout() OVERRIDE;
   virtual void OnPaintBorder(gfx::Canvas* canvas) OVERRIDE;
 
@@ -87,10 +93,6 @@ class OriginChipView : public views::LabelButton,
       const SafeBrowsingUIManager::UnsafeResource& resource) OVERRIDE;
   virtual void OnSafeBrowsingMatch(
       const SafeBrowsingUIManager::UnsafeResource& resource) OVERRIDE;
-
- private:
-  // Sets an image grid to represent the current security state.
-  void SetBorderImages(const int images[3][9]);
 
   LocationBarView* location_bar_view_;
   Profile* profile_;
