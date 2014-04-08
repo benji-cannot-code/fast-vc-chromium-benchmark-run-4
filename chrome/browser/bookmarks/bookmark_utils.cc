a3c1e3dc7cb5b8d27e8fbbd8169f750aabbabacd
sdefresne@chromium.org <sdefresne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-08 16:45:48 +0000

    Remove unused includes
    Nothing is used from the imported includes:
    - chrome/browser/undo/bookmark_undo_service.h
    - chrome/browser/undo/bookmark_undo_service_factory.h
    
    Remove those two includes as they are obsolete.
    
    BUG=360692
    
    Review URL: https://codereview.chromium.org/224753004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262437 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/bookmarks/bookmark_utils.cc
+++ chrome/browser/bookmarks/bookmark_utils.cc
@@ -18,8 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/bookmarks/bookmark_model.h"
 #include "chrome/browser/bookmarks/bookmark_model_factory.h"
 #include "chrome/browser/history/query_parser.h"
-#include "chrome/browser/undo/bookmark_undo_service.h"
-#include "chrome/browser/undo/bookmark_undo_service_factory.h"
 #include "chrome/common/pref_names.h"
 #include "components/user_prefs/pref_registry_syncable.h"
 #include "content/public/browser/user_metrics.h"
