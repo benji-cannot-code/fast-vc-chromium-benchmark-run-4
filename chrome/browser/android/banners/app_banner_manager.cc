84cd37fd64cc21d52b91492a8b2a6e5a12a433f3
dfalcantara@chromium.org <dfalcantara@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-08 00:13:03 +0000

    Revert of Disable app banners (https://chromiumcodereview.appspot.com/226653002/)
    Reason for revert:
    Reverting to turn them back on.
    
    Original issue's description:
    > Disable app banners
    > 
    > It has been Decided that the UX mocks should be revisited.
    > Disabling on trunk to merge backward to M35.
    > 
    > BUG=360162
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=261934
    
    TBR=thakis@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=360162
    
    Review URL: https://codereview.chromium.org/227083009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262264 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/android/banners/app_banner_manager.cc
+++ chrome/browser/android/banners/app_banner_manager.cc
@@ -153,7 +153,8 @@ jlong Init(JNIEnv* env, jobject obj) {
 }
 
 jboolean IsEnabled(JNIEnv* env, jclass clazz) {
-  return false;
+  return !CommandLine::ForCurrentProcess()->HasSwitch(
+      switches::kDisableAppBanners);
 }
 
 // Register native methods
