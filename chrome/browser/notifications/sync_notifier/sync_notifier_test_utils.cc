ab65da75fbeea4d80138e951717910701534246b
dewittj@chromium.org <dewittj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-08 01:01:21 +0000

    Hook up the new "Info URL" field in Synced Notifications App Info.
    The Info URL is used to send the user to a landing page when they see a
    welcome notification for a synced sending service.
    
    BUG=338864
    
    Review URL: https://codereview.chromium.org/221963008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262272 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/notifications/sync_notifier/sync_notifier_test_utils.cc
+++ chrome/browser/notifications/sync_notifier/sync_notifier_test_utils.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 const char kSendingService1Name[] = "TestService1";
 const char kSendingService2Name[] = "TestService2";
 const char kSendingService3Name[] = "TestService3";
+const char kTestInfoUrl[] = "https://www.google.com/info.html";
 const char kTestIconUrl[] = "https://www.google.com/someicon.png";
 
 // Fake data for creating a SyncData object to use in creating a
