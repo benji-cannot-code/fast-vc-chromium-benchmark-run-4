460ab64e987dd8d47122330033f34e16057e04e7
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-08 19:03:09 +0000

    Adds notification when tabstrip is in the process of closing all
    I'm going to use this to cache the layer for the tab closing. This way
    when a window is closing we won't get a black tab.
    
    BUG=323791
    TEST=covered by unit test
    R=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/225303021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262466 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/tabs/tab_strip_model.cc
+++ chrome/browser/ui/tabs/tab_strip_model.cc
@@ -245,7 +245,8 @@ TabStripModel::TabStripModel(TabStripModelDelegate* delegate, Profile* profile)
     : delegate_(delegate),
       profile_(profile),
       closing_all_(false),
-      in_notify_(false) {
+      in_notify_(false),
+      weak_factory_(this) {
   DCHECK(delegate_);
   order_controller_.reset(new TabStripModelOrderController(this));
 }
@@ -1192,6 +1193,11 @@ bool TabStripModel::InternalCloseTabs(const std::vector<int>& indices,
 
   CloseTracker close_tracker(GetWebContentsFromIndices(indices));
 
+  base::WeakPtr<TabStripModel> ref(weak_factory_.GetWeakPtr());
+  const bool closing_all = indices.size() == contents_data_.size();
+  if (closing_all)
+    FOR_EACH_OBSERVER(TabStripModelObserver, observers_, WillCloseAllTabs());
+
   // We only try the fast shutdown path if the whole browser process is *not*
   // shutting down. Fast shutdown during browser termination is handled in
   // BrowserShutdown.
@@ -1244,6 +1250,11 @@ bool TabStripModel::InternalCloseTabs(const std::vector<int>& indices,
 
     InternalCloseTab(closing_contents, index,
                      (close_types & CLOSE_CREATE_HISTORICAL_TAB) != 0);
+  }
+
+  if (ref && closing_all && !retval) {
+    FOR_EACH_OBSERVER(TabStripModelObserver, observers_,
+                      CloseAllTabsCanceled());
   }
 
   return retval;
