b16a7bfdf5c223e1dd34526f36f78a363aacf3b5
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-08 23:02:06 +0000

    Move DispatchBeforeUnload from RenderFrameHost to WebContents. This is after discussions to move as much stuff as possible to WebContents.
    BUG=304341
    R=creis@chromium.org
    
    Review URL: https://codereview.chromium.org/228653007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262535 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/fast_unload_controller.cc
+++ chrome/browser/ui/fast_unload_controller.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/notification_source.h"
 #include "content/public/browser/notification_types.h"
-#include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_delegate.h"
@@ -100,7 +99,7 @@ bool FastUnloadController::RunUnloadEventsHelper(
     // them. Once they have fired, we'll get a message back saying whether
     // to proceed closing the page or not, which sends us back to this method
     // with the NeedToFireBeforeUnload bit cleared.
-    contents->GetMainFrame()->DispatchBeforeUnload(false);
+    contents->DispatchBeforeUnload(false);
     return true;
   }
   return false;
@@ -397,7 +396,7 @@ void FastUnloadController::ProcessPendingTabs() {
       // and then call beforeunload handlers for |contents|.
       // See DevToolsWindow::InterceptPageBeforeUnload for details.
       if (!DevToolsWindow::InterceptPageBeforeUnload(contents))
-        contents->GetMainFrame()->DispatchBeforeUnload(false);
+        contents->DispatchBeforeUnload(false);
     } else {
       ProcessPendingTabs();
     }
