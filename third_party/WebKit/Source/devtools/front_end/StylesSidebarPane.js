benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit b18605902dc34dc5f2b99a3ea2f244fbd5196cae

Update the metrics side bar pane and computed style pane after main frame resize.
The metrics pane showing width and height of the main frame should be updated upon receiving
the resize event. This also applies while docking/undocking the DevTools window.

BUG=322997

Review URL: https://codereview.chromium.org/84643002

git-svn-id: svn://svn.chromium.org/blink/trunk@163004 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/devtools/front_end/StylesSidebarPane.js
@@ -105,6 +105,7 @@ WebInspector.StylesSidebarPane = function(computedStylePane, setPseudoClassCallb
     WebInspector.domAgent.addEventListener(WebInspector.DOMAgent.Events.AttrRemoved, this._attributeChanged, this);
     WebInspector.domAgent.addEventListener(WebInspector.DOMAgent.Events.PseudoStateChanged, this._pseudoStateChanged, this);
     WebInspector.settings.showUserAgentStyles.addChangeListener(this._showUserAgentStylesSettingChanged.bind(this));
+    WebInspector.resourceTreeModel.addEventListener(WebInspector.ResourceTreeModel.EventTypes.FrameResized, this._frameResized, this);
     this.element.addStyleClass("styles-pane");
     this.element.enableStyleClass("show-user-styles", WebInspector.settings.showUserAgentStyles.get());
     this.element.addEventListener("mousemove", this._mouseMovedOverElement.bind(this), false);
@@ -397,6 +398,20 @@ WebInspector.StylesSidebarPane.prototype = {
             return;
 
         this._rebuildUpdate();
+    },
+
+    _frameResized: function()
+    {
+        function refreshContents()
+        {
+            this._rebuildUpdate();
+            delete this._activeTimer;
+        }
+
+        if (this._activeTimer)
+            clearTimeout(this._activeTimer);
+
+        this._activeTimer = setTimeout(refreshContents.bind(this), 100);
     },
 
     _attributeChanged: function(event)

third_party/WebKit/Source/core/inspector/InspectorPageAgent.cpp
@@ -1004,6 +1004,7 @@ void InspectorPageAgent::didResizeMainFrame()
 {
     if (m_enabled && m_state->getBoolean(PageAgentState::showSizeOnResize))
         m_overlay->showAndHideViewSize(m_state->getBoolean(PageAgentState::showGridOnResize));
+    m_frontend->frameResized();
 }
 
 void InspectorPageAgent::didRecalculateStyle()

third_party/WebKit/Source/devtools/front_end/MetricsSidebarPane.js
@@ -39,6 +39,7 @@ WebInspector.MetricsSidebarPane = function()
     WebInspector.cssModel.addEventListener(WebInspector.CSSStyleModel.Events.MediaQueryResultChanged, this._styleSheetOrMediaQueryResultChanged, this);
     WebInspector.domAgent.addEventListener(WebInspector.DOMAgent.Events.AttrModified, this._attributesUpdated, this);
     WebInspector.domAgent.addEventListener(WebInspector.DOMAgent.Events.AttrRemoved, this._attributesUpdated, this);
+    WebInspector.resourceTreeModel.addEventListener(WebInspector.ResourceTreeModel.EventTypes.FrameResized, this._frameResized, this);
 }
 
 WebInspector.MetricsSidebarPane.prototype = {
@@ -87,6 +88,20 @@ WebInspector.MetricsSidebarPane.prototype = {
     _styleSheetOrMediaQueryResultChanged: function()
     {
         this._innerUpdate();
+    },
+
+    _frameResized: function()
+    {
+        function refreshContents()
+        {
+            this._innerUpdate();
+            delete this._activeTimer;
+        }
+
+        if (this._activeTimer)
+            clearTimeout(this._activeTimer);
+
+        this._activeTimer = setTimeout(refreshContents.bind(this), 100);
     },
 
     _attributesUpdated: function(event)

third_party/WebKit/Source/devtools/front_end/ResourceTreeModel.js
@@ -57,6 +57,7 @@ WebInspector.ResourceTreeModel.EventTypes = {
     FrameAdded: "FrameAdded",
     FrameNavigated: "FrameNavigated",
     FrameDetached: "FrameDetached",
+    FrameResized: "FrameResized",
     MainFrameNavigated: "MainFrameNavigated",
     MainFrameCreatedOrNavigated: "MainFrameCreatedOrNavigated",
     ResourceAdded: "ResourceAdded",
@@ -712,6 +713,11 @@ WebInspector.PageDispatcher.prototype = {
 
     frameClearedScheduledNavigation: function(frameId)
     {
+    },
+
+    frameResized: function()
+    {
+        this._resourceTreeModel.dispatchEventToListeners(WebInspector.ResourceTreeModel.EventTypes.FrameResized, null);
     },
 
     javascriptDialogOpening: function(message)

commit 12d071df72ea1f0d787b8f983e3ff25e79c46c2b

DevTools: [Elements] Do not update sidebar if frame resize event arrives during styles editing
This is regression from r163004.

R=pfeldman, vsevik
BUG=359853

Review URL: https://codereview.chromium.org/227463003

git-svn-id: svn://svn.chromium.org/blink/trunk@171039 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/devtools/front_end/StylesSidebarPane.js
@@ -455,7 +455,7 @@ WebInspector.StylesSidebarPane.prototype = {
          */
         function refreshContents()
         {
-            this._rebuildUpdate();
+            this._styleSheetOrMediaQueryResultChanged();
             delete this._activeTimer;
         }
 