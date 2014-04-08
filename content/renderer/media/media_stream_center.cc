cf6eecfe25c16eb589ca225ce6910a2b29c05243
yurys@chromium.org <yurys@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-08 14:55:35 +0000

    Revert of Only create webrtc MediaStreams when added to a PeerConnection (https://codereview.chromium.org/227163007/)
    Reason for revert:
    This change caused 16 tests under LayoutTests/fast/mediastream/ to crash
    
    http://test-results.appspot.com/dashboards/flakiness_dashboard.html#group=%40ToT%20Blink&tests=fast/mediastream/RTCPeerConnection-AddRemoveStream.html,fast/mediastream/RTCPeerConnection-createAnswer.html,fast/mediastream/RTCPeerConnection-createOffer.html,fast/mediastream/RTCPeerConnection-datachannel.html,fast/mediastream/RTCPeerConnection-dtmf.html,fast/mediastream/RTCPeerConnection-events.html,fast/mediastream/RTCPeerConnection-ice.html,fast/mediastream/RTCPeerConnection-lifetime.html,fast/mediastream/RTCPeerConnection-localDescription.html,fast/mediastream/RTCPeerConnection-onnegotiationneeded.html,fast/mediastream/RTCPeerConnection-remoteDescription.html,fast/mediastream/RTCPeerConnection-state.html,fast/mediastream/RTCPeerConnection-stats.html,fast/mediastream/RTCPeerConnection-statsSelector.html,fast/mediastream/RTCPeerConnection.html,fast/mediastream/constructors.html
    
    Original issue's description:
    > This cl adds a new class WebRtcMediaStreamAdapter. The responsibility of the class is to create and own a representation of a webrtc MediaStream that can be added and removed from a PeerConnection.
    > An instance of WebRtcMediaStreamAdapter is created when a MediaStream is added to RTCPeerConnection object.
    > 
    > The purpose is to clean up the the webrtc specific use of MediaStreams.
    > 
    > BUG=323223
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=262395
    
    TBR=xians@chromium.org,ronghuawu@chromium.org,perkj@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=323223
    
    Review URL: https://codereview.chromium.org/228733002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262403 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/media_stream_center.cc
+++ content/renderer/media/media_stream_center.cc
@@ -186,7 +186,9 @@ void MediaStreamCenter::didCreateMediaStream(blink::WebMediaStream& stream) {
   DVLOG(1) << "MediaStreamCenter::didCreateMediaStream";
   blink::WebMediaStream writable_stream(stream);
   MediaStream* native_stream(
-      new MediaStream(MediaStream::StreamStopCallback(), stream));
+      new MediaStream(rtc_factory_,
+                      MediaStream::StreamStopCallback(),
+                      stream));
   writable_stream.setExtraData(native_stream);
 
   blink::WebVector<blink::WebMediaStreamTrack> video_tracks;
@@ -203,7 +205,7 @@ bool MediaStreamCenter::didAddMediaStreamTrack(
     const blink::WebMediaStreamTrack& track) {
   DVLOG(1) << "MediaStreamCenter::didAddMediaStreamTrack";
   MediaStream* native_stream = MediaStream::GetMediaStream(stream);
-  return native_stream->AddTrack(track);
+  return native_stream->AddTrack(stream, track);
 }
 
 bool MediaStreamCenter::didRemoveMediaStreamTrack(
@@ -211,7 +213,7 @@ bool MediaStreamCenter::didRemoveMediaStreamTrack(
     const blink::WebMediaStreamTrack& track) {
   DVLOG(1) << "MediaStreamCenter::didRemoveMediaStreamTrack";
   MediaStream* native_stream = MediaStream::GetMediaStream(stream);
-  return native_stream->RemoveTrack(track);
+  return native_stream->RemoveTrack(stream, track);
 }
 
 bool MediaStreamCenter::OnControlMessageReceived(const IPC::Message& message) {
