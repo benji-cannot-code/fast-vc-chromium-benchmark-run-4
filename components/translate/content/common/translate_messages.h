aa2214709c7da795374cf17458873bf15ec5f5e6
andrewhayden@chromium.org <andrewhayden@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-09 20:04:00 +0000

    Fix compile-time bugs introduced by r261562.
    BUG=
    
    Review URL: https://codereview.chromium.org/227703004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262785 0039d316-1c4b-4281-b951-d872f2087c98

--- components/translate/content/common/translate_messages.h
+++ components/translate/content/common/translate_messages.h
@@ -11,6 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ipc/ipc_message_macros.h"
 #include "ipc/ipc_message_utils.h"
 
+#if defined(CLD2_DYNAMIC_MODE)
+#include "ipc/ipc_platform_file.h"
+#endif
+
 #define IPC_MESSAGE_START TranslateMsgStart
 
 IPC_ENUM_TRAITS(TranslateErrors::Type)
