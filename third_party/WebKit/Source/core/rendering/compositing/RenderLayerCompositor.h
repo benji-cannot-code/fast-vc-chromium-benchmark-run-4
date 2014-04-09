d0897f32320771a0cb703be3e0f5bbab4109b6cb
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-09 18:01:33 +0000

    Imply m_needsToRecomputeCompositingRequirements from RenderLayerCompositor's updateType
    We eventually want to remove m_needsToRecomputeCompositingRequirements and
    drive the system based on RenderLayerCompositor's updateType. This CL removes
    one place where we set this dirty bit explicitly.
    
    Review URL: https://codereview.chromium.org/229943004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171153 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/compositing/RenderLayerCompositor.h
+++ third_party/WebKit/Source/core/rendering/compositing/RenderLayerCompositor.h
@@ -48,11 +48,11 @@ class StickyPositionViewportConstraints;
 
 enum CompositingUpdateType {
     CompositingUpdateNone,
+    CompositingUpdateAfterCanvasContextChange,
+    CompositingUpdateOnCompositedScroll,
     CompositingUpdateAfterStyleChange,
     CompositingUpdateAfterLayout,
     CompositingUpdateOnScroll,
-    CompositingUpdateOnCompositedScroll,
-    CompositingUpdateAfterCanvasContextChange,
 };
 
 // RenderLayerCompositor manages the hierarchy of
