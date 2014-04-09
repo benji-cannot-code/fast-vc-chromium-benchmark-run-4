5a0a4a7766ff91dfa3131c458e9b01719db0337c
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-09 17:31:57 +0000

    Use the tray bubble used in locked state when system modal dialog is present
    BUG=347985
    TEST=SystemTrayTest.WithSystemModal
    
    Review URL: https://codereview.chromium.org/225263020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262730 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/tray_accessibility.cc
+++ ash/system/tray_accessibility.cc
@@ -81,6 +81,7 @@ class DefaultAccessibilityView : public TrayItemMore {
         IDS_ASH_STATUS_TRAY_ACCESSIBILITY);
     SetLabel(label);
     SetAccessibleName(label);
+    set_id(test::kAccessibilityTrayItemViewId);
   }
 
   virtual ~DefaultAccessibilityView() {
