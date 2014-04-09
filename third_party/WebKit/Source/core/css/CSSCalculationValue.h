251c0dcf85c644905b199a3d2feeeae066676b69
alancutter@chromium.org <alancutter@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-09 17:54:03 +0000

    Add method to flatten CSSPrimitiveValues and CSSCalcValues by length units
    This patch introduces CSSLengthArray as a flat representation
    of a CSS length expression.
    Example: calc(1in + (20% - 12px) / 2) is flattened down to 90px and 10%.
    Methods are added to CSSPrimitiveValue and CSSCalcValue to constuct
    the flattened representation of themselves.
    
    Review URL: https://codereview.chromium.org/227693011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171150 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/CSSCalculationValue.h
+++ third_party/WebKit/Source/core/css/CSSCalculationValue.h
@@ -69,6 +69,7 @@ public:
     virtual PassOwnPtr<CalcExpressionNode> toCalcValue(const CSSToLengthConversionData&) const = 0;
     virtual double doubleValue() const = 0;
     virtual double computeLengthPx(const CSSToLengthConversionData&) const = 0;
+    virtual void accumulateLengthArray(CSSLengthArray&, double multiplier) const = 0;
     virtual String customCSSText() const = 0;
     virtual bool equals(const CSSCalcExpressionNode& other) const { return m_category == other.m_category && m_isInteger == other.m_isInteger; }
     virtual Type type() const = 0;
@@ -111,6 +112,7 @@ public:
     bool isNegative() const { return m_expression->doubleValue() < 0; }
     ValueRange permittedValueRange() { return m_nonNegative ? ValueRangeNonNegative : ValueRangeAll; }
     double computeLengthPx(const CSSToLengthConversionData&) const;
+    void accumulateLengthArray(CSSLengthArray& lengthArray, double multiplier) const { m_expression->accumulateLengthArray(lengthArray, multiplier); }
     CSSCalcExpressionNode* expressionNode() const { return m_expression.get(); }
 
     String customCSSText() const;
