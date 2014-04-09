1f0135c7bf9e233cc2b2d8f6062722263603308c
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-09 01:45:23 +0000

    Move UserActivityNotifier from ash/ to ui/chromeos/.
    This will allow the class to be shared by app_shell.
    
    BUG=354711
    
    Review URL: https://codereview.chromium.org/226223008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262587 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/shell.h
+++ ash/shell.h
@@ -52,6 +52,7 @@ class Rect;
 namespace ui {
 class DisplayConfigurator;
 class Layer;
+class UserActivityNotifier;
 }
 namespace views {
 class NonClientFrameView;
@@ -138,7 +139,6 @@ class SystemTrayNotifier;
 class ToplevelWindowEventHandler;
 class TouchObserverHUD;
 class UserActivityDetector;
-class UserActivityNotifier;
 class UserWallpaperDelegate;
 class VideoActivityNotifier;
 class VideoDetector;
@@ -692,7 +692,7 @@ class ASH_EXPORT Shell : public SystemModalContainerEventFilterDelegate,
 
 #if defined(OS_CHROMEOS)
   scoped_ptr<PowerEventObserver> power_event_observer_;
-  scoped_ptr<UserActivityNotifier> user_activity_notifier_;
+  scoped_ptr<ui::UserActivityNotifier> user_activity_notifier_;
   scoped_ptr<VideoActivityNotifier> video_activity_notifier_;
   scoped_ptr<StickyKeysController> sticky_keys_controller_;
   scoped_ptr<ResolutionNotificationController>
---
+++ ui/chromeos/ui_chromeos_export.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_CHROMEOS_CHROMEOS_EXPORT_H_
+#define UI_CHROMEOS_CHROMEOS_EXPORT_H_
+
+#if defined(COMPONENT_BUILD)
+#if defined(UI_CHROMEOS_IMPLEMENTATION)
+#define UI_CHROMEOS_EXPORT __attribute__((visibility("default")))
+#else
+#define UI_CHROMEOS_EXPORT
+#endif
+
+#else  // defined(COMPONENT_BUILD)
+#define UI_CHROMEOS_EXPORT
+#endif
+
+#endif  // UI_CHROMEOS_CHROMEOS_EXPORT_H_
