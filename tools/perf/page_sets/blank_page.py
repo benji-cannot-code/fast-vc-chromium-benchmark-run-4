9c1a89555b695539fc61665bc9c5590f416f863e
nednguyen@google.com <nednguyen@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-09 14:09:22 +0000

    Update page_sets/PRESUBMIT to prevent people from adding new json page sets.
    BUG=239179
    
    Review URL: https://codereview.chromium.org/229813003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262691 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/page_sets/blank_page.py
+++ tools/perf/page_sets/blank_page.py
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from telemetry.page.actions.navigate import NavigateAction
 from telemetry.page.page_set import PageSet
 from telemetry.page.page import PageWithDefaultRunNavigate
 
