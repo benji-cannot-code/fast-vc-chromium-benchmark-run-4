b6899d41763837ef552e385f848807217c0e7fbc
stepco@chromium.org <stepco@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-09 20:00:34 +0000

    Reduce unneeded policy fetches by detecting expired invalidations.
    User policy fetches currently occur whenever a user with policy signs into Chrome. If the policy was changed while the user was offline, the new policy is received at that time. However, the invalidation service will still send a notification that the policy was updated, possibly causing Chrome to fetch the policy again needlessly (depending on the initial policy fetch time vs the invalidation delivery time).
    
    For versioned invalidations, we can solve this problem by comparing the version, which is a timestamp, to the timestamp of the current policy. If the invalidation was created sufficiently long before the policy was fetched, we can ignore it assuming that we already have the update.
    
    For unknown version invalidations, we did some investigation on the server side indicating that most of these are due to users that have never had a policy change, so the invalidation server determines that the policy version is unknown. We will ignore unknown version invalidations if the policy has been fetched within the past 30 seconds.
    
    BUG=355884
    
    Review URL: https://codereview.chromium.org/226093005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262782 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/cloud/cloud_policy_browsertest.cc
+++ chrome/browser/policy/cloud/cloud_policy_browsertest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/prefs/pref_service.h"
 #include "base/run_loop.h"
 #include "base/strings/stringprintf.h"
+#include "base/time/time.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/invalidation/fake_invalidation_service.h"
@@ -313,10 +314,12 @@ IN_PROC_BROWSER_TEST_F(CloudPolicyTest, InvalidatePolicy) {
 
   // Update the homepage in the policy and trigger an invalidation.
   ASSERT_NO_FATAL_FAILURE(SetServerPolicy(GetTestPolicy("youtube.com", 0)));
+  base::TimeDelta now =
+      base::Time::NowFromSystemTime() - base::Time::UnixEpoch();
   GetInvalidationService()->EmitInvalidationForTest(
       syncer::Invalidation::Init(
           invalidation::ObjectId(16, "test_policy"),
-          1 /* version */,
+          now.InMicroseconds() /* version */,
           "payload"));
   {
     base::RunLoop run_loop;
