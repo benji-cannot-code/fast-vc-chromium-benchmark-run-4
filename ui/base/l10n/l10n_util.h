7c7bd362afc1fcac15801217af194b368d4a828a
alemate@chromium.org <alemate@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-09 21:01:40 +0000

    Allow country-specific languages in UI list at OOBE.
    This CL allows displaying to user and selection of country-specific language
    codes for which we do not have actual translation. A base language locale
    will be used instead.
    
    BUG=357481
    TEST=browsertest
    
    To test manually:
    
    1) Set VPD value of initial_locale to country-specific language codes, for which only the translation to base language exist. For example:
    "fr-CH,it-CH,de-CH"
    
    2) Start OOBE and check they are displayed and selectable like any other languages.
    
    Review URL: https://codereview.chromium.org/224093013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262808 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/l10n/l10n_util.h
+++ ui/base/l10n/l10n_util.h
@@ -22,6 +22,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace l10n_util {
 
+// The same as base::i18n::GetCanonicalLocale(const char*), but takes
+// std::string as an argument.
+UI_BASE_EXPORT std::string GetCanonicalLocale(const std::string& locale);
+
 // This method translates a generic locale name to one of the locally defined
 // ones. This method returns true if it succeeds.
 UI_BASE_EXPORT bool CheckAndResolveLocale(const std::string& locale,
