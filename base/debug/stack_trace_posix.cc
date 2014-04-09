ef29b673bd846b3c089d40685392f282ecc12ffe
mostynb@opera.com <mostynb@opera.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-09 22:29:38 +0000

    don't use glibc-specific execinfo.h on uclibc builds
    The execinfo.h header is glibc-specific, as is the backtrace function.
    Let's skip them for uclibc builds.
    
    BUG=361130
    
    Review URL: https://codereview.chromium.org/228753003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262841 0039d316-1c4b-4281-b951-d872f2087c98

--- base/debug/stack_trace_posix.cc
+++ base/debug/stack_trace_posix.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/debug/stack_trace.h"
 
 #include <errno.h>
-#include <execinfo.h>
 #include <fcntl.h>
 #include <signal.h>
 #include <stdio.h>
@@ -23,6 +22,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(__GLIBCXX__)
 #include <cxxabi.h>
+#endif
+#if !defined(__UCLIBC__)
+#include <execinfo.h>
 #endif
 
 #if defined(OS_MACOSX)
@@ -72,7 +74,7 @@ void DemangleSymbols(std::string* text) {
   // Note: code in this function is NOT async-signal safe (std::string uses
   // malloc internally).
 
-#if defined(__GLIBCXX__)
+#if defined(__GLIBCXX__) && !defined(__UCLIBC__)
 
   std::string::size_type search_from = 0;
   while (search_from < text->size()) {
@@ -109,7 +111,7 @@ void DemangleSymbols(std::string* text) {
     }
   }
 
-#endif  // defined(__GLIBCXX__)
+#endif  // defined(__GLIBCXX__) && !defined(__UCLIBC__)
 }
 #endif  // !defined(USE_SYMBOLIZE)
 
@@ -168,7 +170,7 @@ void ProcessBacktrace(void *const *trace,
 
     handler->HandleOutput("\n");
   }
-#else
+#elif !defined(__UCLIBC__)
   bool printed = false;
 
   // Below part is async-signal unsafe (uses malloc), so execute it only
@@ -280,7 +282,9 @@ void StackDumpSignalHandler(int signal, siginfo_t* info, void* void_context) {
   }
   PrintToStderr("\n");
 
+#if !defined(__UCLIBC__)
   debug::StackTrace().Print();
+#endif
 
 #if defined(OS_LINUX)
 #if ARCH_CPU_X86_FAMILY
@@ -739,11 +743,16 @@ StackTrace::StackTrace() {
   // NOTE: This code MUST be async-signal safe (it's used by in-process
   // stack dumping signal handler). NO malloc or stdio is allowed here.
 
+#if !defined(__UCLIBC__)
   // Though the backtrace API man page does not list any possible negative
   // return values, we take no chance.
   count_ = base::saturated_cast<size_t>(backtrace(trace_, arraysize(trace_)));
+#else
+  count_ = 0;
+#endif
 }
 
+#if !defined(__UCLIBC__)
 void StackTrace::Print() const {
   // NOTE: This code MUST be async-signal safe (it's used by in-process
   // stack dumping signal handler). NO malloc or stdio is allowed here.
@@ -756,6 +765,7 @@ void StackTrace::OutputToStream(std::ostream* os) const {
   StreamBacktraceOutputHandler handler(os);
   ProcessBacktrace(trace_, count_, &handler);
 }
+#endif
 
 namespace internal {
 
