4dd4cfc7af10d008c16921ac8b31c2dd08bcf61f
halton.huo@intel.com <halton.huo@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-09 03:04:38 +0000

    Fix gdbinit error in webkit.py
    BUG=357070
    
    Review URL: https://codereview.chromium.org/214483002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171102 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/gdb/webkit.py
+++ third_party/WebKit/Tools/gdb/webkit.py
@@ -36,6 +36,8 @@ Add this to your gdb by amending your ~/.gdbinit as follows:
   import webkit
 """
 
+from __future__ import print_function
+
 import gdb
 import re
 import struct
@@ -361,7 +363,7 @@ class PrintPathToRootCommand(gdb.Command):
         try:
             val = gdb.Frame.read_var(frame, arg)
         except:
-            print "No such variable, or invalid type"
+            print("No such variable, or invalid type")
             return
 
         target_type = str(val.type.target().strip_typedefs())
@@ -375,10 +377,10 @@ class PrintPathToRootCommand(gdb.Command):
             padding = ''
             while len(stack) > 0:
                 pair = stack.pop()
-                print padding, pair[1], pair[0]
+                print(padding, pair[1], pair[0])
                 padding = padding + '  '
         else:
-            print 'Sorry: I don\'t know how to deal with %s yet.' % target_type
+            print('Sorry: I don\'t know how to deal with %s yet.' % target_type)
 
 
 PrintPathToRootCommand()
