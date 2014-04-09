e983fa4ba0f60d888521bb3e700fca227dcc995d
ericu@chromium.org <ericu@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-09 22:27:26 +0000

    Add blob serialization support [currently unused] to SerializedScriptValue. This will be needed by IDB Blob support, in progress.
    BUG=108012
    R=cmumford,jsbell
    
    Review URL: https://codereview.chromium.org/223123002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171190 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/storage/resources/serialized-script-value.js
+++ third_party/WebKit/LayoutTests/fast/storage/resources/serialized-script-value.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-var kSerializedScriptValueVersion = 5;
+var kSerializedScriptValueVersion = 6;
 
 function forVersion(version, values) {
     var versionTag = 0xff;
