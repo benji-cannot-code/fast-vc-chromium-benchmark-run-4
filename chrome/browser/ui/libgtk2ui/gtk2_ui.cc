bc776e7451a822e2cd36d4f70f4eedca033a969c
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-09 22:23:40 +0000

    linux_aura: Use system configuration for middle clicking the titlebar.
    The action that is taken when the user middle clicks on non client area
    is configurable with gnome-tweak-tool. This allows users to disable any
    action on middle click, lower windows, minimize windows, or toggle the
    maximize state.
    
    Also renames GConfTitlebarListener to GConfListener, since it's now
    doing more than just checking maximize button order.
    
    BUG=132061
    
    Review URL: https://codereview.chromium.org/229783002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262839 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/libgtk2ui/gtk2_ui.cc
+++ chrome/browser/ui/libgtk2ui/gtk2_ui.cc
@@ -47,7 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/linux_ui/window_button_order_observer.h"
 
 #if defined(USE_GCONF)
-#include "chrome/browser/ui/libgtk2ui/gconf_titlebar_listener.h"
+#include "chrome/browser/ui/libgtk2ui/gconf_listener.h"
 #endif
 
 // A minimized port of GtkThemeService into something that can provide colors
@@ -319,7 +319,7 @@ color_utils::HSL GetDefaultTint(int id) {
 
 namespace libgtk2ui {
 
-Gtk2UI::Gtk2UI() {
+Gtk2UI::Gtk2UI() : middle_click_action_(MIDDLE_CLICK_ACTION_LOWER) {
   GtkInitFromCommandLine(*CommandLine::ForCurrentProcess());
 }
 
@@ -351,7 +351,7 @@ void Gtk2UI::Initialize() {
 
 #if defined(USE_GCONF)
   // We must build this after GTK gets initialized.
-  titlebar_listener_.reset(new GConfTitlebarListener(this));
+  gconf_listener_.reset(new GConfListener(this));
 #endif  // defined(USE_GCONF)
 
   indicators_count = 0;
@@ -566,6 +566,10 @@ void Gtk2UI::SetWindowButtonOrdering(
                                                  trailing_buttons_));
 }
 
+void Gtk2UI::SetNonClientMiddleClickAction(NonClientMiddleClickAction action) {
+  middle_click_action_ = action;
+}
+
 scoped_ptr<ui::LinuxInputMethodContext> Gtk2UI::CreateInputMethodContext(
     ui::LinuxInputMethodContextDelegate* delegate) const {
   return scoped_ptr<ui::LinuxInputMethodContext>(
@@ -673,6 +677,11 @@ void Gtk2UI::RemoveNativeThemeChangeObserver(
 
 bool Gtk2UI::UnityIsRunning() {
   return unity::IsRunning();
+}
+
+views::LinuxUI::NonClientMiddleClickAction
+Gtk2UI::GetNonClientMiddleClickAction() {
+  return middle_click_action_;
 }
 
 void Gtk2UI::NotifyWindowManagerStartupComplete() {
--- ui/views/widget/desktop_aura/x11_window_event_filter.cc
+++ ui/views/widget/desktop_aura/x11_window_event_filter.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/event.h"
 #include "ui/events/event_utils.h"
 #include "ui/gfx/x/x11_types.h"
+#include "ui/views/linux_ui/linux_ui.h"
 #include "ui/views/widget/desktop_aura/desktop_window_tree_host.h"
 #include "ui/views/widget/native_widget_aura.h"
 
@@ -108,7 +109,27 @@ void X11WindowEventFilter::OnMouseEvent(ui::MouseEvent* event) {
     return;
 
   if (event->IsMiddleMouseButton() && (component == HTCAPTION)) {
-    XLowerWindow(xdisplay_, xwindow_);
+    LinuxUI::NonClientMiddleClickAction action =
+        LinuxUI::MIDDLE_CLICK_ACTION_LOWER;
+    LinuxUI* linux_ui = LinuxUI::instance();
+    if (linux_ui)
+      action = linux_ui->GetNonClientMiddleClickAction();
+
+    switch (action) {
+      case LinuxUI::MIDDLE_CLICK_ACTION_NONE:
+        break;
+      case LinuxUI::MIDDLE_CLICK_ACTION_LOWER:
+        XLowerWindow(xdisplay_, xwindow_);
+        break;
+      case LinuxUI::MIDDLE_CLICK_ACTION_MINIMIZE:
+        window_tree_host_->Minimize();
+        break;
+      case LinuxUI::MIDDLE_CLICK_ACTION_TOGGLE_MAXIMIZE:
+        if (target->GetProperty(aura::client::kCanMaximizeKey))
+          ToggleMaximizedState();
+        break;
+    }
+
     event->SetHandled();
     return;
   }
@@ -120,10 +141,7 @@ void X11WindowEventFilter::OnMouseEvent(ui::MouseEvent* event) {
     // Our event is a double click in the caption area in a window that can be
     // maximized. We are responsible for dispatching this as a minimize/
     // maximize on X11 (Windows converts this to min/max events for us).
-    if (window_tree_host_->IsMaximized())
-      window_tree_host_->Restore();
-    else
-      window_tree_host_->Maximize();
+    ToggleMaximizedState();
     event->SetHandled();
     return;
   }
@@ -138,6 +156,13 @@ void X11WindowEventFilter::OnMouseEvent(ui::MouseEvent* event) {
       event->StopPropagation();
     }
   }
+}
+
+void X11WindowEventFilter::ToggleMaximizedState() {
+  if (window_tree_host_->IsMaximized())
+    window_tree_host_->Restore();
+  else
+    window_tree_host_->Maximize();
 }
 
 bool X11WindowEventFilter::DispatchHostWindowDragMovement(
