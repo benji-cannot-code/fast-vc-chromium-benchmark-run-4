96f5ae1f9d52d3e13fbfef91773f21017400824d
vogelheim@chromium.org <vogelheim@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-09 17:44:15 +0000

    Remove unused include.
    R=dcarney@chromium.org
    BUG=276323
    
    Review URL: https://codereview.chromium.org/230613006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171145 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/CustomElementConstructorBuilder.cpp
+++ third_party/WebKit/Source/bindings/v8/CustomElementConstructorBuilder.cpp
@@ -41,7 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/v8/DOMWrapperWorld.h"
 #include "bindings/v8/Dictionary.h"
 #include "bindings/v8/ExceptionState.h"
-#include "bindings/v8/UnsafePersistent.h"
 #include "bindings/v8/V8Binding.h"
 #include "bindings/v8/V8HiddenValue.h"
 #include "bindings/v8/V8PerContextData.h"
