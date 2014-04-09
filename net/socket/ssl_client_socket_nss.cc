367665d311ccb1e58478d762be2eb0c89ac9c26e
jar@chromium.org <jar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-09 12:21:15 +0000

    make SetReceiveBufferSize and SetSendBufferSize return net error codes (instead of bools)
    Previously committed as https://src.chromium.org/viewvc/chrome?view=rev&revision=261966 but then reverted when it broke the Mac 10.6 build.
    
    TBR=sergeyu,yzshen,yurys,zea
    R=wtc
    BUG=355222
    
    Review URL: https://codereview.chromium.org/227473008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262652 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/ssl_client_socket_nss.cc
+++ net/socket/ssl_client_socket_nss.cc
@@ -3086,11 +3086,11 @@ int SSLClientSocketNSS::Write(IOBuffer* buf, int buf_len,
   return rv;
 }
 
-bool SSLClientSocketNSS::SetReceiveBufferSize(int32 size) {
+int SSLClientSocketNSS::SetReceiveBufferSize(int32 size) {
   return transport_->socket()->SetReceiveBufferSize(size);
 }
 
-bool SSLClientSocketNSS::SetSendBufferSize(int32 size) {
+int SSLClientSocketNSS::SetSendBufferSize(int32 size) {
   return transport_->socket()->SetSendBufferSize(size);
 }
 
