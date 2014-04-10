bae4e1cadee556cbb75c7df76c08fb6f32541dc2
treib@chromium.org <treib@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-10 13:43:24 +0000

    Before deleting a profile, cancel all in-progress downloads to prevent a "Do you really want to exit Chrome" popup, which (1) is confusing because the message is not accurate, and (2) prevents a window corresponding to the deleted profile from closing, leaving it in an inconsistent state.
    A UI update (warning the user that downloads will be canceled) will follow in a separate CL.
    
    Note: I'm aware that this doesn't address crbug.com/289390 which is another way to trigger the same underlying bug. Working on it...
    
    BUG=336725
    
    Review URL: https://codereview.chromium.org/214523003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262970 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_service.cc
+++ chrome/browser/download/download_service.cc
@@ -96,6 +96,22 @@ int DownloadService::NonMaliciousDownloadCount() const {
       NonMaliciousInProgressCount();
 }
 
+void DownloadService::CancelDownloads() {
+  if (!download_manager_created_)
+    return;
+
+  DownloadManager* download_manager =
+      BrowserContext::GetDownloadManager(profile_);
+  DownloadManager::DownloadVector downloads;
+  download_manager->GetAllDownloads(&downloads);
+  for (DownloadManager::DownloadVector::iterator it = downloads.begin();
+       it != downloads.end();
+       ++it) {
+    if ((*it)->GetState() == content::DownloadItem::IN_PROGRESS)
+      (*it)->Cancel(false);
+  }
+}
+
 // static
 int DownloadService::NonMaliciousDownloadCountAllProfiles() {
   std::vector<Profile*> profiles(
@@ -121,17 +137,9 @@ void DownloadService::CancelAllDownloads() {
   for (std::vector<Profile*>::iterator it = profiles.begin();
        it < profiles.end();
        ++it) {
-    content::DownloadManager* download_manager =
-        content::BrowserContext::GetDownloadManager(*it);
-    content::DownloadManager::DownloadVector downloads;
-    download_manager->GetAllDownloads(&downloads);
-    for (content::DownloadManager::DownloadVector::iterator it =
-             downloads.begin();
-         it != downloads.end();
-         ++it) {
-      if ((*it)->GetState() == content::DownloadItem::IN_PROGRESS)
-        (*it)->Cancel(false);
-    }
+    DownloadService* service =
+        DownloadServiceFactory::GetForBrowserContext(*it);
+    service->CancelDownloads();
   }
 }
 
--- chrome/browser/profiles/profile_manager.cc
+++ chrome/browser/profiles/profile_manager.cc
@@ -25,6 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/content_settings/host_content_settings_map.h"
+#include "chrome/browser/download/download_service.h"
+#include "chrome/browser/download/download_service_factory.h"
 #include "chrome/browser/prefs/incognito_mode_prefs.h"
 #include "chrome/browser/profiles/bookmark_model_loaded_observer.h"
 #include "chrome/browser/profiles/profile_avatar_icon_util.h"
@@ -634,6 +636,17 @@ void ProfileManager::ScheduleProfileForDeletion(
     const base::FilePath& profile_dir,
     const CreateCallback& callback) {
   DCHECK(profiles::IsMultipleProfilesEnabled());
+
+  // Cancel all in-progress downloads before deleting the profile to prevent a
+  // "Do you want to exit Google Chrome and cancel the downloads?" prompt
+  // (crbug.com/336725).
+  Profile* profile = GetProfileByPath(profile_dir);
+  if (profile) {
+    DownloadService* service =
+        DownloadServiceFactory::GetForBrowserContext(profile);
+    service->CancelDownloads();
+  }
+
   PrefService* local_state = g_browser_process->local_state();
   ProfileInfoCache& cache = GetProfileInfoCache();
 
@@ -1073,6 +1086,10 @@ void ProfileManager::FinishDeletingProfile(const base::FilePath& profile_dir) {
   Profile* profile = GetProfileByPath(profile_dir);
 
   if (profile) {
+    // By this point, all in-progress downloads for the profile being deleted
+    // must have been canceled (crbug.com/336725).
+    DCHECK(DownloadServiceFactory::GetForBrowserContext(profile)->
+           NonMaliciousDownloadCount() == 0);
     BrowserList::CloseAllBrowsersWithProfile(profile);
 
     // Disable sync for doomed profile.
