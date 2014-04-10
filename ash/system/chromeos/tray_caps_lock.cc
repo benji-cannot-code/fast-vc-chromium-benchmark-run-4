b1532b81ec4fcad8f174df077b7d2a37c9c02f0c
skuhne@chromium.org <skuhne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-10 17:55:53 +0000

    Fixing caps lock problems under ChromeOS where the tray does not properly follow the real caps lock change
    BUG=356393
    TEST=visual with chromebooks internal keyboard, external keyboard and with linux
    
    Review URL: https://codereview.chromium.org/231753002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263025 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/chromeos/tray_caps_lock.cc
+++ ash/system/chromeos/tray_caps_lock.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/tray/fixed_sized_image_view.h"
 #include "ash/system/tray/system_tray_notifier.h"
 #include "ash/system/tray/tray_constants.h"
+#include "base/sys_info.h"
 #include "chromeos/ime/input_method_manager.h"
 #include "chromeos/ime/xkeyboard.h"
 #include "grit/ash_resources.h"
@@ -130,24 +131,39 @@ TrayCapsLock::TrayCapsLock(SystemTray* system_tray)
       detailed_(NULL),
       caps_lock_enabled_(CapsLockIsEnabled()),
       message_shown_(false) {
-  // Make sure the event is processed by this before the IME.
-  Shell::GetInstance()->PrependPreTargetHandler(this);
+  // Since keyboard handling differs between ChromeOS and Linux we need to
+  // use different observers depending on the two platforms.
+  if (base::SysInfo::IsRunningOnChromeOS()) {
+    chromeos::input_method::XKeyboard* xkeyboard =
+        chromeos::input_method::InputMethodManager::Get()->GetXKeyboard();
+    xkeyboard->AddObserver(this);
+  } else {
+    Shell::GetInstance()->PrependPreTargetHandler(this);
+  }
 }
 
 TrayCapsLock::~TrayCapsLock() {
-  Shell::GetInstance()->RemovePreTargetHandler(this);
+  // Since keyboard handling differs between ChromeOS and Linux we need to
+  // use different observers depending on the two platforms.
+  if (base::SysInfo::IsRunningOnChromeOS()) {
+    chromeos::input_method::XKeyboard* xkeyboard =
+        chromeos::input_method::InputMethodManager::Get()->GetXKeyboard();
+    xkeyboard->RemoveObserver(this);
+  } else {
+    Shell::GetInstance()->RemovePreTargetHandler(this);
+  }
 }
 
 void TrayCapsLock::OnCapsLockChanged(bool enabled) {
-  if (tray_view())
-    tray_view()->SetVisible(enabled);
-
   caps_lock_enabled_ = enabled;
 
+  if (tray_view())
+    tray_view()->SetVisible(caps_lock_enabled_);
+
   if (default_) {
-    default_->Update(enabled);
+    default_->Update(caps_lock_enabled_);
   } else {
-    if (enabled) {
+    if (caps_lock_enabled_) {
       if (!message_shown_) {
         Shell::GetInstance()->metrics()->RecordUserMetricsAction(
             ash::UMA_STATUS_AREA_CAPS_LOCK_POPUP);
