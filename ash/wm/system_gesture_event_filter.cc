ba55ca9edce92e789338c98a86c0c5658fca9255
spang@chromium.org <spang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-10 05:28:01 +0000

    ozone: Protect X11-specific code under ash/wm
    This fixes some build errors for the "chromeos=1 use_ozone=1" build. The
    touch factory class & ui::CopyAreaToCanvas function are currently only
    supported under X11.
    
    BUG=361341
    TEST=chrome --ozone-platform=test
    
    Review URL: https://codereview.chromium.org/232313002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262931 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/system_gesture_event_filter.cc
+++ ash/wm/system_gesture_event_filter.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/event.h"
 #include "ui/events/event_constants.h"
 
-#if defined(OS_CHROMEOS)
+#if defined(OS_CHROMEOS) && defined(USE_X11)
 #include "ui/events/x/touch_factory_x11.h"
 #endif
 
@@ -31,7 +31,7 @@ SystemGestureEventFilter::~SystemGestureEventFilter() {
 }
 
 void SystemGestureEventFilter::OnMouseEvent(ui::MouseEvent* event) {
-#if defined(OS_CHROMEOS) && !defined(USE_OZONE)
+#if defined(OS_CHROMEOS) && defined(USE_X11)
   if (event->type() == ui::ET_MOUSE_PRESSED && event->native_event() &&
       ui::TouchFactory::GetInstance()->IsTouchDevicePresent() &&
       Shell::GetInstance()->delegate()) {
