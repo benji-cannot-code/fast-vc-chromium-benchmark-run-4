e8ed0909b9f237bbb3224e8bf5587a494a43f8f7
keonho07.kim@samsung.com <keonho07.kim@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-10 01:41:23 +0000

    Remove unused AudioParams in PannerNode.
    m_distanceGain and m_coneGain are not used internally in panner node.
    Remove useless valuables.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/231033002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171214 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/webaudio/PannerNode.h
+++ third_party/WebKit/Source/modules/webaudio/PannerNode.h
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/audio/Panner.h"
 #include "modules/webaudio/AudioListener.h"
 #include "modules/webaudio/AudioNode.h"
-#include "modules/webaudio/AudioParam.h"
 #include "platform/geometry/FloatPoint3D.h"
 #include "wtf/HashMap.h"
 #include "wtf/OwnPtr.h"
@@ -160,8 +159,6 @@ private:
     FloatPoint3D m_cachedVelocity;
 
     // Gain
-    RefPtr<AudioParam> m_distanceGain;
-    RefPtr<AudioParam> m_coneGain;
     DistanceEffect m_distanceEffect;
     ConeEffect m_coneEffect;
     float m_lastGain;
