18b78975b8c3bdb614e86d3a35bd94c9d8406b04
aruslan@google.com <aruslan@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-10 00:15:38 +0000

    Revert of [Mirror Android] Temporarily remove the Mirror flag. (https://codereview.chromium.org/231983003/)
    Reason for revert:
    r262868 was droved to M-35 branch as r262870.
    Now we need to re-enable Mirror on trunk.
    
    Original issue's description:
    > [Mirror Android] Temporarily remove the Mirror flag.
    > 
    > Once in trunk this needs to be picked up to M35 beta branch.
    > Once picked up, this needs to be reverted on trunk.
    > 
    > BUG=361827
    > NOTRY=True
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=262868
    
    TBR=rogerta@chromium.org,aruslan@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=361827
    
    Review URL: https://codereview.chromium.org/232183003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262872 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1464,7 +1464,7 @@ const Experiment kExperiments[] = {
     "enable-new-profile-management",
     IDS_FLAGS_ENABLE_NEW_PROFILE_MANAGEMENT_NAME,
     IDS_FLAGS_ENABLE_NEW_PROFILE_MANAGEMENT_DESCRIPTION,
-    kOsMac | kOsWin | kOsLinux,
+    kOsAndroid | kOsMac | kOsWin | kOsLinux,
     SINGLE_VALUE_TYPE(switches::kNewProfileManagement)
   },
   {
