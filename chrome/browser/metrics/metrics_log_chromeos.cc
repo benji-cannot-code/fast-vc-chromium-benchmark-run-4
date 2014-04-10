2797d2080ba79ac2cb6b643c7853f7a3ac2d9ab3
spang@chromium.org <spang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-10 05:24:05 +0000

    ozone: Protect X11-specific code in chrome/browser/metrics
    This fixes some build errors for the "chromeos=1 use_ozone=1" build. The
    touch factory class is currently only supported under X11.
    
    BUG=361341
    TEST=chrome --ozone-platform=test
    
    Review URL: https://codereview.chromium.org/232173003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262930 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metrics/metrics_log_chromeos.cc
+++ chrome/browser/metrics/metrics_log_chromeos.cc
@@ -16,8 +16,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/bluetooth/bluetooth_adapter_factory.h"
 #include "device/bluetooth/bluetooth_device.h"
 #include "ui/events/event_utils.h"
-#include "ui/events/x/touch_factory_x11.h"
 #include "ui/gfx/screen.h"
+
+#if defined(USE_X11)
+#include "ui/events/x/touch_factory_x11.h"
+#endif  // defined(USE_X11)
 
 using metrics::ChromeUserMetricsExtension;
 using metrics::PerfDataProto;
@@ -64,6 +67,7 @@ PairedDevice::Type AsBluetoothDeviceType(
 }
 
 void WriteExternalTouchscreensProto(SystemProfileProto::Hardware* hardware) {
+#if defined(USE_X11)
   std::set<std::pair<int, int> > touchscreen_ids =
       ui::TouchFactory::GetInstance()->GetTouchscreenIds();
   for (std::set<std::pair<int, int> >::iterator it = touchscreen_ids.begin();
@@ -74,6 +78,7 @@ void WriteExternalTouchscreensProto(SystemProfileProto::Hardware* hardware) {
     touchscreen->set_vendor_id(it->first);
     touchscreen->set_product_id(it->second);
   }
+#endif  // defined(USE_X11)
 }
 
 }  // namespace
