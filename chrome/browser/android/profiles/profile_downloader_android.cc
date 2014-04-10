fd44254a12e5c54121e686bbf164a9c5bf498cc9
aruslan@chromium.org <aruslan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-10 21:19:07 +0000

    [Mirror Android] Do not return "old style" avatar icons.
    BUG=360732
    NOTRY=True
    
    Review URL: https://codereview.chromium.org/232743004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263088 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/android/profiles/profile_downloader_android.cc
+++ chrome/browser/android/profiles/profile_downloader_android.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_string.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile_android.h"
+#include "chrome/browser/profiles/profile_avatar_icon_util.h"
 #include "chrome/browser/profiles/profile_downloader.h"
 #include "chrome/browser/profiles/profile_downloader_delegate.h"
 #include "chrome/browser/profiles/profile_manager.h"
@@ -136,9 +137,13 @@ jobject GetCachedAvatarForPrimaryAccount(JNIEnv* env,
 
   ScopedJavaLocalRef<jobject> jbitmap;
   if (index != std::string::npos) {
-    const gfx::Image& img = info.GetAvatarIconOfProfileAtIndex(index);
-    if (!img.IsEmpty() && img.AsImageSkia().bitmap())
-      jbitmap = gfx::ConvertToJavaBitmap(img.AsImageSkia().bitmap());
+    gfx::Image avatar_image = info.GetAvatarIconOfProfileAtIndex(index);
+    if (!avatar_image.IsEmpty() &&
+        avatar_image.Width() > profiles::kAvatarIconWidth &&
+        avatar_image.Height() > profiles::kAvatarIconHeight &&
+        avatar_image.AsImageSkia().bitmap()) {
+      jbitmap = gfx::ConvertToJavaBitmap(avatar_image.AsImageSkia().bitmap());
+    }
   }
 
   return jbitmap.Release();
