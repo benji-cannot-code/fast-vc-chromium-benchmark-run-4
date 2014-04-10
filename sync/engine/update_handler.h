2923fec3ba5d047f72c917bf718337d3f0ee1367
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-10 23:57:57 +0000

    [Sync] Add support for retrying a getupdates due to a context change
    We detect a context conflict using the version value, and if detected force the
    getupdates to retry.
    
    BUG=360280
    
    Review URL: https://codereview.chromium.org/232003005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263134 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/engine/update_handler.h
+++ sync/engine/update_handler.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "sync/base/sync_export.h"
+#include "sync/internal_api/public/util/syncer_error.h"
 
 namespace sync_pb {
 class DataTypeContext;
@@ -50,7 +51,10 @@ class SYNC_EXPORT_PRIVATE UpdateHandler {
   //
   // In this context, "applicable_updates" means the set of updates belonging to
   // this type.
-  virtual void ProcessGetUpdatesResponse(
+  //
+  // Returns SYNCER_OK if the all data was processed successfully, a syncer
+  // error otherwise.
+  virtual SyncerError ProcessGetUpdatesResponse(
       const sync_pb::DataTypeProgressMarker& progress_marker,
       const sync_pb::DataTypeContext& mutated_context,
       const SyncEntityList& applicable_updates,
