aadd486b1a1e2ffa4a244b245effe7daa80bec6f
jshin@chromium.org <jshin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-10 19:29:45 +0000

    Block an invalid UTF-16 string from findPlainText
    We should not pass an invalid UTF-16 input to ICU's
    usearch_setPattern(). Blocking it on the ICU's end does not
    help in terms of the efficiency (the same check has to be done but only
    further down the code path.)
    
    BUG=360431
    TEST=1. webkit_unit_tests --gtest_filter=*findPlainTextInval*
    2. Do the above with an ASAN build and it should not crash.
    
    Review URL: https://codereview.chromium.org/229193002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171298 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/TextIterator.cpp
+++ third_party/WebKit/Source/core/editing/TextIterator.cpp
@@ -53,6 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/text/StringBuilder.h"
 #include "wtf/unicode/CharacterNames.h"
 #include <unicode/usearch.h>
+#include <unicode/utf16.h>
 
 using namespace WTF::Unicode;
 using namespace std;
@@ -2075,10 +2076,31 @@ static PassRefPtrWillBeRawPtr<Range> collapsedToBoundary(const Range* range, boo
     return result.release();
 }
 
+// Check if there's any unpaird surrogate code point.
+// Non-character code points are not checked.
+static bool isValidUTF16(const String& s)
+{
+    if (s.is8Bit())
+        return true;
+    const UChar* ustr = s.characters16();
+    size_t length = s.length();
+    size_t position = 0;
+    while (position < length) {
+        UChar32 character;
+        U16_NEXT(ustr, position, length, character);
+        if (U_IS_SURROGATE(character))
+            return false;
+    }
+    return true;
+}
+
 static size_t findPlainTextInternal(CharacterIterator& it, const String& target, FindOptions options, size_t& matchStart)
 {
     matchStart = 0;
     size_t matchLength = 0;
+
+    if (!isValidUTF16(target))
+        return 0;
 
     SearchBuffer buffer(target, options);
 
