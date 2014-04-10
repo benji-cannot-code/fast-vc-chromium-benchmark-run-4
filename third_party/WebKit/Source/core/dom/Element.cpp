81cdf399110765fe1ede163d201d535246b0a63b
davve@opera.com <davve@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-10 21:41:54 +0000

    Map width/height to style for foreignObject
    As we move towards using the style system to resolve percentages for
    svg, start small by promoting width and height for foreignObject to
    presentation attributes.
    
    This is a preparation for issue 308992 where we plan to make
    height="100%" for svg elements behave as style="height:100%". Unless
    height is mapped to style for foreignObject, percentage style values
    for svg inside foreignObject cannot be resolved.
    
    This has been resolved in the SVG WG, see minutes
    http://www.w3.org/2014/04/08-svg-minutes.html#item04
    
    BUG=308992,358206
    
    Review URL: https://codereview.chromium.org/218403009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171306 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Element.cpp
+++ third_party/WebKit/Source/core/dom/Element.cpp
@@ -3281,6 +3281,7 @@ void Element::removeAllInlineStyleProperties()
 
 void Element::updatePresentationAttributeStyle()
 {
+    synchronizeAllAttributes();
     // ShareableElementData doesn't store presentation attribute style, so make sure we have a UniqueElementData.
     UniqueElementData& elementData = ensureUniqueElementData();
     elementData.m_presentationAttributeStyleIsDirty = false;
