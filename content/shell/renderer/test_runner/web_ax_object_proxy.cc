4f7f111ccee9d7d2e837e7c7025d52ecefcdb7ad
vogelheim@chromium.org <vogelheim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-10 13:16:09 +0000

    Cleanup: Remove unnecessary define, since UnsafePersistent has indeed been removed.
    R=dcarney@chromium.org
    R=jochen@chromium.org (OWNERS)
    BUG=276323
    
    Review URL: https://codereview.chromium.org/230613005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262957 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/renderer/test_runner/web_ax_object_proxy.cc
+++ content/shell/renderer/test_runner/web_ax_object_proxy.cc
@@ -3,9 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// TODO(hajimehoshi): Remove this when UnsafePersistent is removed.
-#define V8_ALLOW_ACCESS_TO_RAW_HANDLE_CONSTRUCTOR
-
 #include "content/shell/renderer/test_runner/web_ax_object_proxy.h"
 
 #include "base/strings/stringprintf.h"
