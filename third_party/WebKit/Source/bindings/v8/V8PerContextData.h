9c043449155052071c6a201bebe52425ee4383f7
vogelheim@chromium.org <vogelheim@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-10 18:08:41 +0000

    Remove UnsafePersistent from V8NPTemplateMap + V8PerContextData.
    (Change depends on 180363004.)
    
    R=dcarney@chromium.org
    BUG=276323
    
    Review URL: https://codereview.chromium.org/206363002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171292 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/V8PerContextData.h
+++ third_party/WebKit/Source/bindings/v8/V8PerContextData.h
@@ -34,8 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "bindings/v8/CustomElementBinding.h"
 #include "bindings/v8/ScopedPersistent.h"
-#include "bindings/v8/UnsafePersistent.h"
 #include "bindings/v8/V8DOMActivityLogger.h"
+#include "bindings/v8/V8PersistentValueMap.h"
 #include "bindings/v8/WrapperTypeInfo.h"
 #include "gin/public/context_holder.h"
 #include "gin/public/gin_embedders.h"
@@ -75,16 +75,14 @@ public:
     // This is faster than going through the full object creation process.
     v8::Local<v8::Object> createWrapperFromCache(const WrapperTypeInfo* type)
     {
-        UnsafePersistent<v8::Object> boilerplate = m_wrapperBoilerplates.get(type);
-        return !boilerplate.isEmpty() ? boilerplate.newLocal(m_isolate)->Clone() : createWrapperFromCacheSlowCase(type);
+        v8::Local<v8::Object> boilerplate = m_wrapperBoilerplates.Get(type);
+        return !boilerplate.IsEmpty() ? boilerplate->Clone() : createWrapperFromCacheSlowCase(type);
     }
 
     v8::Local<v8::Function> constructorForType(const WrapperTypeInfo* type)
     {
-        UnsafePersistent<v8::Function> function = m_constructorMap.get(type);
-        if (!function.isEmpty())
-            return function.newLocal(m_isolate);
-        return constructorForTypeSlowCase(type);
+        v8::Local<v8::Function> function = m_constructorMap.Get(type);
+        return (!function.IsEmpty()) ? function : constructorForTypeSlowCase(type);
     }
 
     v8::Local<v8::Object> prototypeForType(const WrapperTypeInfo*);
@@ -105,10 +103,10 @@ private:
 
     // For each possible type of wrapper, we keep a boilerplate object.
     // The boilerplate is used to create additional wrappers of the same type.
-    typedef WTF::HashMap<const WrapperTypeInfo*, UnsafePersistent<v8::Object> > WrapperBoilerplateMap;
+    typedef V8PersistentValueMap<const WrapperTypeInfo*, v8::Object, false> WrapperBoilerplateMap;
     WrapperBoilerplateMap m_wrapperBoilerplates;
 
-    typedef WTF::HashMap<const WrapperTypeInfo*, UnsafePersistent<v8::Function> > ConstructorMap;
+    typedef V8PersistentValueMap<const WrapperTypeInfo*, v8::Function, false> ConstructorMap;
     ConstructorMap m_constructorMap;
 
     V8NPObjectMap m_v8NPObjectMap;
