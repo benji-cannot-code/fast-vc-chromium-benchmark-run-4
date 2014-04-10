acf84c4ad6e72a087e671badefabbc3181b9ce68
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-10 13:42:11 +0000

    Show tracing buffer usage in Timeline
    - add bufferUsageReportingInterval option to Tracing.Start;
    - add Tracing.bufferUsage event
    - show tracing buffer usage using Timeline progress pane
    
    BUG=
    
    Review URL: https://codereview.chromium.org/224733015
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171271 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/TracingModel.js
+++ third_party/WebKit/Source/devtools/front_end/TracingModel.js
@@ -7,12 +7,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /**
  * @constructor
+ * @extends {WebInspector.Object}
  */
 WebInspector.TracingModel = function()
 {
+    WebInspector.Object.call(this);
     this.reset();
     this._active = false;
     InspectorBackend.registerTracingDispatcher(new WebInspector.TracingDispatcher(this));
+}
+
+WebInspector.TracingModel.Events = {
+    "BufferUsage": "BufferUsage"
 }
 
 /** @typedef {!{
@@ -69,7 +75,8 @@ WebInspector.TracingModel.prototype = {
     start: function(categoryPatterns, options, callback)
     {
         this.reset();
-        TracingAgent.start(categoryPatterns, options, callback);
+        var bufferUsageReportingIntervalMs = 500;
+        TracingAgent.start(categoryPatterns, options, bufferUsageReportingIntervalMs, callback);
         this._active = true;
     },
 
@@ -84,6 +91,14 @@ WebInspector.TracingModel.prototype = {
         }
         this._pendingStopCallback = callback;
         TracingAgent.end();
+    },
+
+    /**
+     * @param {number} usage
+     */
+    _bufferUsage: function(usage)
+    {
+        this.dispatchEventToListeners(WebInspector.TracingModel.Events.BufferUsage, usage);
     },
 
     /**
@@ -175,7 +190,9 @@ WebInspector.TracingModel.prototype = {
     sortedProcesses: function()
     {
         return WebInspector.TracingModel.NamedObject._sort(Object.values(this._processById));
-    }
+    },
+
+    __proto__: WebInspector.Object.prototype
 }
 
 /**
@@ -417,6 +434,11 @@ WebInspector.TracingDispatcher = function(tracingModel)
 }
 
 WebInspector.TracingDispatcher.prototype = {
+    bufferUsage: function(usage)
+    {
+        this._tracingModel._bufferUsage(usage);
+    },
+
     dataCollected: function(data)
     {
         this._tracingModel._eventsCollected(data);
