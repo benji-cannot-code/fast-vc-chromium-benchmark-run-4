f92719778a57441ac184de4a1f37bbcbb94a9943
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-11 18:51:39 +0000

    Add a new Net.AlternateProtocolUsage for tracking usage of Alternate-Protocol.
    BUG=
    
    Review URL: https://codereview.chromium.org/232193002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263298 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_server_properties.cc
+++ net/http/http_server_properties.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_server_properties.h"
 
 #include "base/logging.h"
+#include "base/metrics/histogram.h"
 #include "base/strings/stringprintf.h"
 
 namespace net {
@@ -31,6 +32,11 @@ COMPILE_ASSERT(
     kAlternateProtocolStringsSize_kNumValidAlternateProtocols_not_equal);
 
 }  // namespace
+
+void HistogramAlternateProtocolUsage(AlternateProtocolUsage usage) {
+  UMA_HISTOGRAM_ENUMERATION("Net.AlternateProtocolUsage", usage,
+                            ALTERNATE_PROTOCOL_USAGE_MAX);
+}
 
 bool IsAlternateProtocolValid(AlternateProtocol protocol) {
   return protocol >= ALTERNATE_PROTOCOL_MINIMUM_VALID_VERSION &&
--- net/http/http_stream_factory_impl.cc
+++ net/http/http_stream_factory_impl.cc
@@ -207,8 +207,10 @@ PortAlternateProtocolPair HttpStreamFactoryImpl::GetAlternateProtocolRequestFor(
 
   PortAlternateProtocolPair alternate =
       http_server_properties.GetAlternateProtocol(origin);
-  if (alternate.protocol == ALTERNATE_PROTOCOL_BROKEN)
+  if (alternate.protocol == ALTERNATE_PROTOCOL_BROKEN) {
+    HistogramAlternateProtocolUsage(ALTERNATE_PROTOCOL_USAGE_BROKEN);
     return kNoAlternateProtocol;
+  }
 
   if (!IsAlternateProtocolValid(alternate.protocol)) {
     NOTREACHED();
