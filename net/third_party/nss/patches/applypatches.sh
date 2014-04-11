8878d9283897308cd4fc95626db9987a71489739
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-11 07:19:00 +0000

    Change ssl_padding_xtn to the IANA-assigned value 21.
    R=agl@chromium.org,davidben@chromium.org,rsleevi@chromium.org
    BUG=361311
    
    Review URL: https://codereview.chromium.org/233633002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263167 0039d316-1c4b-4281-b951-d872f2087c98

--- net/third_party/nss/patches/applypatches.sh
+++ net/third_party/nss/patches/applypatches.sh
@@ -47,3 +47,5 @@ patch -p4 < $patches_dir/fallbackscsv.patch
 patch -p4 < $patches_dir/sessioncache.patch
 
 patch -p4 < $patches_dir/nssrwlock.patch
+
+patch -p4 < $patches_dir/paddingextvalue.patch
