6f7b36026852747a31cde6ffe35cd8242ea22ca5
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-11 22:34:16 +0000

    SafeBrowsingDatabaseManagerTest: remove unnecessary FakeSafeBrowsingService
    BUG=none
    
    Review URL: https://codereview.chromium.org/234153003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263380 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/safe_browsing/database_manager_unittest.cc
+++ chrome/browser/safe_browsing/database_manager_unittest.cc
@@ -17,46 +17,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using content::TestBrowserThreadBundle;
 
-namespace {
-
-class FakeSafeBrowsingService : public SafeBrowsingService {
- public:
-  FakeSafeBrowsingService() {}
- private:
-  virtual ~FakeSafeBrowsingService() {}
-
-  DISALLOW_COPY_AND_ASSIGN(FakeSafeBrowsingService);
-};
-
-// Factory that creates FakeSafeBrowsingService instances.
-class TestSafeBrowsingServiceFactory : public SafeBrowsingServiceFactory {
-  virtual SafeBrowsingService* CreateSafeBrowsingService() OVERRIDE {
-    return new FakeSafeBrowsingService();
-  }
-};
-
-}  // namespace
-
 class SafeBrowsingDatabaseManagerTest : public PlatformTest {
  public:
-
-  virtual void SetUp() OVERRIDE {
-    PlatformTest::SetUp();
-    SafeBrowsingService::RegisterFactory(&factory_);
-  }
-
-  virtual void TearDown() OVERRIDE {
-    SafeBrowsingService::RegisterFactory(NULL);
-    PlatformTest::TearDown();
-  }
-
   bool RunSBHashTest(const safe_browsing_util::ListType list_type,
                      const std::vector<SBThreatType>& expected_threats,
                      const std::string& result_list);
 
  private:
   TestBrowserThreadBundle thread_bundle_;
-  TestSafeBrowsingServiceFactory factory_;
 };
 
 bool SafeBrowsingDatabaseManagerTest::RunSBHashTest(
