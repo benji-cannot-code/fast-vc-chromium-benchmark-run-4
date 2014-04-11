fb98c67e2c009725e20502cc48a4250a5863ba19
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-11 16:57:45 +0000

    gn: Don't print ansi escape codes to pipes.
    BUG=362239
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/234463002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263265 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/gn/bin/help_as_html.py
+++ tools/gn/bin/help_as_html.py
@@ -13,17 +13,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # - Spit out other similar formats like wiki, markdown, whatever.
 
 import cgi
-import re
 import subprocess
 import sys
 
 
 def GetOutput(*args):
   try:
-    # TODO: Remove ansi escape code stripping if crbug.com/362239 gets fixed.
-    ansi_escape = re.compile(r'\x1b[^m]*m')
-    return ansi_escape.sub(
-        '', subprocess.check_output([sys.argv[1]] + list(args)))
+    return subprocess.check_output([sys.argv[1]] + list(args))
   except subprocess.CalledProcessError:
     return ''
 
