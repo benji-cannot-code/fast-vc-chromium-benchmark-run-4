72ebad96099ba957a61bf44648e7caed66b274f4
kaliamoorthi@chromium.org <kaliamoorthi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-11 16:44:38 +0000

    Adds support in URLBlacklist to include query parameters.
    This CL adds support in URLBlacklist for blacklist or whitelist an url based on query parameters.
    
    This CL extends crrev.com/219613002
    
    BUG=349997
    
    Review URL: https://codereview.chromium.org/225023026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263261 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/managed_mode/managed_mode_url_filter.cc
+++ chrome/browser/managed_mode/managed_mode_url_filter.cc
@@ -90,11 +90,13 @@ bool FilterBuilder::AddPattern(const std::string& pattern, int site_id) {
   std::string host;
   uint16 port;
   std::string path;
+  std::string query;
   bool match_subdomains = true;
   URLBlacklist::SegmentURLCallback callback =
       static_cast<URLBlacklist::SegmentURLCallback>(URLFixerUpper::SegmentURL);
   if (!URLBlacklist::FilterToComponents(
-          callback, pattern, &scheme, &host, &match_subdomains, &port, &path)) {
+          callback, pattern,
+          &scheme, &host, &match_subdomains, &port, &path, &query)) {
     LOG(ERROR) << "Invalid pattern " << pattern;
     return false;
   }
@@ -102,7 +104,7 @@ bool FilterBuilder::AddPattern(const std::string& pattern, int site_id) {
   scoped_refptr<URLMatcherConditionSet> condition_set =
       URLBlacklist::CreateConditionSet(
           &contents_->url_matcher, ++matcher_id_,
-          scheme, host, match_subdomains, port, path);
+          scheme, host, match_subdomains, port, path, query, true);
   all_conditions_.push_back(condition_set);
   contents_->matcher_site_map[matcher_id_] = site_id;
   return true;
