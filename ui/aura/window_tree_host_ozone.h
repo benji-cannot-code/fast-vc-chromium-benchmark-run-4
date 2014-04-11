e82df91e0338a184ff2632bf2dd9425e6f417f9f
spang@chromium.org <spang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-11 18:56:25 +0000

    ozone: Export WindowTreeHostOzone
    The ash component is now using this for AshWindowTreeHostOzone.
    
    R=sadrul
    TEST=ash_unittests
    BUG=361341
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/235533002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263300 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/window_tree_host_ozone.h
+++ ui/aura/window_tree_host_ozone.h
@@ -17,9 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace aura {
 
-class WindowTreeHostOzone : public WindowTreeHost,
-                            public ui::EventSource,
-                            public ui::PlatformEventDispatcher {
+class AURA_EXPORT WindowTreeHostOzone : public WindowTreeHost,
+                                        public ui::EventSource,
+                                        public ui::PlatformEventDispatcher {
  public:
   explicit WindowTreeHostOzone(const gfx::Rect& bounds);
   virtual ~WindowTreeHostOzone();
