7c924aef43f8ebaadf469ba68dfcd60895a45a85
jdduke@chromium.org <jdduke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-11 19:36:50 +0000

    [Android] Eliminate fling-related JNI access issue
    With the release of M35, an apparent render-process specific issue of accessing
    ViewConfiguration-related constants via JNI has been discovered. The simplest
    fix for now is avoiding the ViewConfiguration calls altogether. Instead, simply
    use the fling scroll friction constant from the Android platform that has not
    changed in 4+ years.
    
    BUG=362614
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/224863008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263312 0039d316-1c4b-4281-b951-d872f2087c98

--- content/child/fling_animator_impl_android.cc
+++ content/child/fling_animator_impl_android.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "third_party/WebKit/public/platform/WebFloatSize.h"
 #include "third_party/WebKit/public/platform/WebGestureCurveTarget.h"
-#include "ui/gfx/android/view_configuration.h"
 #include "ui/gfx/frame_time.h"
 #include "ui/gfx/vector2d.h"
 
@@ -16,10 +15,16 @@ namespace content {
 
 namespace {
 
+// Value taken directly from Android's ViewConfiguration. As the value has not
+// changed in 4+ years, and does not depend on any device-specific configuration
+// parameters, copy it directly to avoid potential JNI interop issues in the
+// render process (see crbug.com/362614).
+const float kDefaultAndroidPlatformScrollFriction = 0.015f;
+
 gfx::Scroller::Config GetScrollerConfig() {
   gfx::Scroller::Config config;
   config.flywheel_enabled = false;
-  config.fling_friction = gfx::ViewConfiguration::GetScrollFriction();
+  config.fling_friction = kDefaultAndroidPlatformScrollFriction;
   return config;
 }
 
