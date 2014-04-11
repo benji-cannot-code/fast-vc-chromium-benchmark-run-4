84922fd66b3411ab4b4f19f2b7a4d4750608d813
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-11 02:39:03 +0000

    Fix windows unit_tests after r263150
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263155 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/notifications/message_center_notifications_unittest_win.cc
+++ chrome/browser/notifications/message_center_notifications_unittest_win.cc
@@ -28,7 +28,9 @@ namespace message_center {
 
 class MessageCenterNotificationManagerTest : public testing::Test {
  protected:
-  MessageCenterNotificationManagerTest() {}
+  MessageCenterNotificationManagerTest() {
+    MessageCenterNotificationManager::RegisterPrefs(local_state_.registry());
+  }
 
   virtual void SetUp() {
     // Clear the preference and initialize.
