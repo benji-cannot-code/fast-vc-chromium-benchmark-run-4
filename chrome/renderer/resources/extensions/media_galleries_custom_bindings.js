c67c65395af46ef78cbbb23550155fd4cdf48cae
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-11 08:04:14 +0000

    Media Galleries: Set chrome.runtime.lastError in chrome/mediaGalleries.dropPermissionForMediaFileSystems.
    BUG=359186
    
    Review URL: https://codereview.chromium.org/234543002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263187 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/resources/extensions/media_galleries_custom_bindings.js
+++ chrome/renderer/resources/extensions/media_galleries_custom_bindings.js
@@ -69,10 +69,9 @@ binding.registerCustomHook(function(bindingsAPI, extensionId) {
 
   apiFunctions.setCustomCallback('dropPermissionForMediaFileSystem',
                                  function(name, request, response) {
-    var galleryId = response[0];
-    var result = response[1];
+    var galleryId = response;
 
-    if (galleryId && result) {
+    if (galleryId) {
       for (var key in mediaGalleriesMetadata) {
         if (mediaGalleriesMetadata[key].galleryId == galleryId) {
           delete mediaGalleriesMetadata[key];
@@ -81,7 +80,7 @@ binding.registerCustomHook(function(bindingsAPI, extensionId) {
       }
     }
     if (request.callback)
-      request.callback(result);
+      request.callback();
     request.callback = null;
   });
 
