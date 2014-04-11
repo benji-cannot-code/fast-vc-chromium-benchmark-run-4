eae2b6e57090e11648b0baee59ab7d5a99d15ee3
mlerman@chromium.org <mlerman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-11 16:51:28 +0000

    Newly created profiles should have the grey silhouette as avatar. The name should still be automatically generated.
    Also, all the avatar icons are becoming squares (31x38 -> 38x38). New resources will be committed and necessary code changes have been made.
    
    
    BUG=337986
    TEST=When creating a new profile from anywhere except the settings. Also, verify avatar icons are still rendered correctly, whether GAIA, the placeholder or a cartoon icon.
    page, the automatically assigned avatar should the grey
    placeholder (silhouette).
    
    Review URL: https://codereview.chromium.org/212603011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263262 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/password_manager/chrome_password_manager_client.h"
 #include "chrome/browser/profiles/avatar_menu.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_avatar_icon_util.h"
 #include "chrome/browser/profiles/profile_info_cache.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/profiles/profiles_state.h"
@@ -581,10 +582,6 @@ int BrowserView::GetOTRIconResourceID() const {
   }
 
   return otr_resource_id;
-}
-
-int BrowserView::GetGuestIconResourceID() const {
-  return IDR_LOGIN_GUEST;
 }
 
 bool BrowserView::ShouldShowAvatar() const {
