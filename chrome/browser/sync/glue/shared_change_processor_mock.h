785fba93a02f38ab8a28755f0bbb31bc1d2e847e
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-11 22:09:54 +0000

    [Sync] Notify the datatype of the current context on startup
    In order to support context triggered refreshes, the datatype must know
    what the current context is at startup to decide whether a refresh must
    be performed or not. We will now invoke UpdateDataTypeContext before
    MergeDataAndStartSyncing if a valid context exists.
    
    BUG=357368
    R=rlarocque@chromium.org
    
    Review URL: https://codereview.chromium.org/233833003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263376 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/glue/shared_change_processor_mock.h
+++ chrome/browser/sync/glue/shared_change_processor_mock.h
@@ -35,6 +35,7 @@ class SharedChangeProcessorMock : public SharedChangeProcessor {
   MOCK_METHOD1(SyncModelHasUserCreatedNodes,
                bool(bool*));
   MOCK_METHOD0(CryptoReadyIfNecessary, bool());
+  MOCK_CONST_METHOD1(GetDataTypeContext, bool(std::string*));
   MOCK_METHOD1(ActivateDataType,
                void(syncer::ModelSafeGroup));
 
