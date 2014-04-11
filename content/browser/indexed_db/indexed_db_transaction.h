d1b9c9d3e4151989db3d780d3e1aaf4d95c70ab8
cmumford@chromium.org <cmumford@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-11 20:19:24 +0000

    Added IndexedDBTransactionTest::SchedulePreemptiveTask.
    Unrelated to a bug, just adding a unit test to verify operation of transactions
    containing preemptive tasks.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/233463002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263328 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/indexed_db/indexed_db_transaction.h
+++ content/browser/indexed_db/indexed_db_transaction.h
@@ -88,6 +88,8 @@ class CONTENT_EXPORT IndexedDBTransaction
  private:
   FRIEND_TEST_ALL_PREFIXES(IndexedDBTransactionTestMode, AbortPreemptive);
   FRIEND_TEST_ALL_PREFIXES(IndexedDBTransactionTest, Timeout);
+  FRIEND_TEST_ALL_PREFIXES(IndexedDBTransactionTest,
+                           SchedulePreemptiveTask);
 
   friend class base::RefCounted<IndexedDBTransaction>;
   virtual ~IndexedDBTransaction();
