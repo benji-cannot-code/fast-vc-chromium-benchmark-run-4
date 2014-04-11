83c584656c33331cc1f9a5abf0eb15d6b932ab89
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-11 13:35:38 +0000

    kiosk: Show "Apps" menu when user pods are disabled.
    A temp fix for http://crbug.com/346881 before we have
    http://crbug.com/362062 implemented.
    
    BUG=346881
    
    Review URL: https://codereview.chromium.org/233703004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263225 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chromeos/login/kiosk_app_menu_handler.h
+++ chrome/browser/ui/webui/chromeos/login/kiosk_app_menu_handler.h
@@ -31,6 +31,9 @@ class KioskAppMenuHandler : public content::WebUIMessageHandler,
   // content::WebUIMessageHandler overrides:
   virtual void RegisterMessages() OVERRIDE;
 
+  // Returns true if new kiosk UI is enabled.
+  static bool EnableNewKioskUI();
+
  private:
   // Sends all kiosk apps to webui.
   void SendKioskApps();
