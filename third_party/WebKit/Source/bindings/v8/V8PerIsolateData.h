4a0009162e0dfe77a550d93389a660f7e7df9a4d
vogelheim@chromium.org <vogelheim@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-11 14:27:27 +0000

    Remove UnsafePersistent from V8ValueCache / StringCache.
    R=dcarney@chromium.org
    BUG=276323
    
    Review URL: https://codereview.chromium.org/206533004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171348 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/V8PerIsolateData.h
+++ third_party/WebKit/Source/bindings/v8/V8PerIsolateData.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "bindings/v8/NewScriptState.h"
 #include "bindings/v8/ScopedPersistent.h"
-#include "bindings/v8/UnsafePersistent.h"
 #include "bindings/v8/V8HiddenValue.h"
 #include "bindings/v8/WrapperTypeInfo.h"
 #include "gin/public/gin_embedders.h"
