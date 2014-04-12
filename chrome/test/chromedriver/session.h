811b39bba2cdd37bd8921730490cd2d5d7466551
samuong@chromium.org <samuong@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-12 05:33:22 +0000

    [chromedriver] Implement WebDriver.switchTo().parentFrame() command
    BUG=chromedriver:760
    
    Review URL: https://codereview.chromium.org/235003006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263487 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/chromedriver/session.h
+++ chrome/test/chromedriver/session.h
@@ -46,6 +46,7 @@ struct Session {
   Status GetTargetWindow(WebView** web_view);
 
   void SwitchToTopFrame();
+  void SwitchToParentFrame();
   void SwitchToSubFrame(const std::string& frame_id,
                         const std::string& chromedriver_frame_id);
   std::string GetCurrentFrameId() const;
