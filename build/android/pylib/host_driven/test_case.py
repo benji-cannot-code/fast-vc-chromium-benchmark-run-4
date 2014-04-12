cf889637072eaab5ff11efbffb4e0947b80483e6
jbudorick@chromium.org <jbudorick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-12 05:26:08 +0000

    [Android] Fix UIAutomator, Linker, and downstream tests.
    BUG=267773
    
    Review URL: https://codereview.chromium.org/235943002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263484 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/pylib/host_driven/test_case.py
+++ build/android/pylib/host_driven/test_case.py
@@ -51,6 +51,7 @@ class HostDrivenTestCase(object):
       instrumentation_options: An InstrumentationOptions object.
     """
     class_name = self.__class__.__name__
+    self.adb = None
     self.cleanup_test_files = False
     self.device = None
     self.device_id = ''
@@ -75,6 +76,7 @@ class HostDrivenTestCase(object):
     self.device_id = device
     self.shard_index = shard_index
     self.device = device_utils.DeviceUtils(self.device_id)
+    self.adb = self.device.old_interface
     self.push_deps = push_deps
     self.cleanup_test_files = cleanup_test_files
     if ports_to_forward:
--- build/android/pylib/uiautomator/test_package.py
+++ build/android/pylib/uiautomator/test_package.py
@@ -24,5 +24,6 @@ class TestPackage(test_jar.TestJar):
     return os.path.basename(self._jar_path)
 
   # Override.
-  def Install(self, adb):
-    adb.PushIfNeeded(self._jar_path, constants.TEST_EXECUTABLE_DIR)
+  def Install(self, device):
+    device.old_interface.PushIfNeeded(self._jar_path,
+                                      constants.TEST_EXECUTABLE_DIR)
