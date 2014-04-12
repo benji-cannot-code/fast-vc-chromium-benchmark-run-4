06cd77039db774b204c61c2ff71d4882e324a30f
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-12 00:08:15 +0000

    Get rid of code duplication in LinkRelAttribute
    There was a "fast path" that just compared the strings and then another
    that split and then compared. The two paths were slightly different for
    no obvious reasons which doesn't really follow the spec where the tokens
    can be all mixed with each other (except for "shortcut icon" apparently).
    
    Allocating the separate strings for "alternate stylesheet" should not be
    an issue.
    
    I also removed the extra constructor, we can just use a default argument
    value.
    
    The only behavior change is that now we allow things like "alternate import"
    (or dns-prefetch) and just ignore the alternate part when deciding what
    to do with the link.
    
    Review URL: https://codereview.chromium.org/235063003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171380 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/LinkRelAttribute.h
+++ third_party/WebKit/Source/core/html/LinkRelAttribute.h
@@ -34,17 +34,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define LinkRelAttribute_h
 
 #include "core/dom/IconURL.h"
-
-namespace WTF {
-class String;
-}
+#include "wtf/text/WTFString.h"
 
 namespace WebCore {
 
 class LinkRelAttribute {
 public:
-    LinkRelAttribute();
-    explicit LinkRelAttribute(const String&);
+    explicit LinkRelAttribute(const String& = "");
 
     bool isStyleSheet() const { return m_isStyleSheet; }
     IconType iconType() const { return m_iconType; }
