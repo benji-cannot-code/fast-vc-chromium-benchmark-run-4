d6c94ff194fa1d75b5ad69df55e0b4027b76d3ae
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-12 03:17:14 +0000

    Remove deprecated WebAutofillClient function from AutofillAgent
    BUG=354897
    
    Review URL: https://codereview.chromium.org/234163004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263465 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/autofill/autofill_renderer_browsertest.cc
+++ chrome/renderer/autofill/autofill_renderer_browsertest.cc
@@ -16,12 +16,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/platform/WebString.h"
 #include "third_party/WebKit/public/platform/WebURLRequest.h"
 #include "third_party/WebKit/public/platform/WebVector.h"
+#include "third_party/WebKit/public/web/WebAutocompleteParams.h"
 #include "third_party/WebKit/public/web/WebDocument.h"
 #include "third_party/WebKit/public/web/WebFormElement.h"
 #include "third_party/WebKit/public/web/WebInputElement.h"
 #include "third_party/WebKit/public/web/WebLocalFrame.h"
 
 using base::ASCIIToUTF16;
+using blink::WebAutocompleteParams;
 using blink::WebDocument;
 using blink::WebElement;
 using blink::WebFormElement;
@@ -228,7 +230,8 @@ class RequestAutocompleteRendererTest : public AutofillRendererTest {
     render_thread_->sink().ClearMessages();
 
     // Invoke requestAutocomplete to show the dialog.
-    autofill_agent_->didRequestAutocomplete(invoking_frame(), invoking_form());
+    autofill_agent_->didRequestAutocomplete(invoking_form(),
+                                            blink::WebAutocompleteParams());
     ASSERT_TRUE(render_thread_->sink().GetFirstMessageMatching(
         AutofillHostMsg_RequestAutocomplete::ID));
 
@@ -302,7 +305,8 @@ TEST_F(RequestAutocompleteRendererTest, NoCancelOnMainFrameNavigateAfterDone) {
 
 TEST_F(RequestAutocompleteRendererTest, InvokingTwiceOnlyShowsOnce) {
   // Attempting to show the requestAutocomplete dialog again should be ignored.
-  autofill_agent_->didRequestAutocomplete(invoking_frame(), invoking_form());
+  autofill_agent_->didRequestAutocomplete(invoking_form(),
+                                          blink::WebAutocompleteParams());
   EXPECT_FALSE(render_thread_->sink().GetFirstMessageMatching(
       AutofillHostMsg_RequestAutocomplete::ID));
 }
