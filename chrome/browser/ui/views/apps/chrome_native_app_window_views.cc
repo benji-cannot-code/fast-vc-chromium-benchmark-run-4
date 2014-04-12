59dfa7b697458cbfafbb27ad035f0457ad5b7182
skuhne@chromium.org <skuhne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-12 01:11:55 +0000

    Removing the shadow for application windows which are likely not to be rectangular
    BUG=359102
    TEST=visual
    
    Review URL: https://codereview.chromium.org/235543002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263437 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/apps/chrome_native_app_window_views.cc
+++ chrome/browser/ui/views/apps/chrome_native_app_window_views.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/webview/webview.h"
 #include "ui/views/widget/widget.h"
 #include "ui/wm/core/easy_resize_window_targeter.h"
+#include "ui/wm/core/shadow_types.h"
 
 #if defined(OS_LINUX)
 #include "chrome/browser/shell_integration_linux.h"
@@ -247,6 +248,17 @@ void ChromeNativeAppWindowViews::InitializeDefaultWindow(
       widget()->CenterWindow(window_bounds.size());
     else
       widget()->SetBounds(window_bounds);
+  }
+
+  if (IsFrameless() &&
+      init_params.opacity == views::Widget::InitParams::TRANSLUCENT_WINDOW &&
+      !create_params.resizable) {
+    // The given window is most likely not rectangular since it uses
+    // transparency, has no standard frame and the user cannot resize it using
+    // the OS supplied methods. Therefore we do not use a shadow for it.
+    // TODO(skuhne): If we run into an application which should have a shadow
+    // but does not have, a new attribute has to be added.
+    wm::SetShadowType(widget()->GetNativeWindow(), wm::SHADOW_TYPE_NONE);
   }
 
   // Register accelarators supported by app windows.
