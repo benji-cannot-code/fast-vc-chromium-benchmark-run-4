7c2c2255f182097d7537b6d7c3d9b22aefe74177
jinho.bang@samsung.com <jinho.bang@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-13 15:39:28 +0000

    Canvas element that use the alpha attribute don't display correctly.
    Current blink is delaying the creation of the backing store until drawing something.
    When alpha is false, the backing store must be initialized to opaque black instead of transparent black.
    
    BUG=352018
    
    Review URL: https://codereview.chromium.org/200003002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171408 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/canvas/alpha.js
+++ third_party/WebKit/LayoutTests/fast/canvas/alpha.js
@@ -1,6 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 description("Series of tests for canvas alpha");
 
+if (window.testRunner)
+    testRunner.dumpAsTextWithPixelResults();
+
 var canvas1 = document.getElementById("canvas1");
 var canvas2 = document.getElementById("canvas2");
 var canvas3 = document.getElementById("canvas3");
@@ -26,11 +29,6 @@ shouldBe("imgData2.data[2]", "0");
 shouldBe("imgData2.data[3]", "0");
 
 shouldBe("ctx3.getContextAttributes().alpha", "false");
-var imgData3 = ctx3.getImageData(0, 0, 1, 1);
-shouldBe("imgData3.data[0]", "0");
-shouldBe("imgData3.data[1]", "0");
-shouldBe("imgData3.data[2]", "0");
-shouldBe("imgData3.data[3]", "255");
 
 shouldBe("ctx4.getContextAttributes().alpha", "true");
 var imgData4 = ctx4.getImageData(0, 0, 1, 1);
