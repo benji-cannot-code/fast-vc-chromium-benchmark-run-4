38599225152271b93baf57213959163617e11b20
jgraettinger@chromium.org <jgraettinger@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-14 17:50:46 +0000

    Update SPDY4/HTTP2 identifiers/flags/NPN tokens.
    SPDY4 will not be a separate specification. It has instead become an
    alias for the upcoming HTTP/2 standard. The next differentiated SPDY
    protocol will be SPDY/5.
    
    Chromium will primarily refer to HTTP/2 as 'SPDY4', purely because it is
    more consistent naming in the context of Chromium's codebase. This CL removes
    the separate 'HTTP2Draft04' protocol identifer, and renames 'SPDY4a2' to
    simply 'SPDY4'. Flags are similarly updated.
    
    The SPDY4 NPN token has been changed to 'h2-11', which is the next
    interop HTTP/2 draft which Chromium will target.
    
    BUG=345769
    
    Review URL: https://codereview.chromium.org/222553005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263664 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_stream_factory_impl_request_unittest.cc
+++ net/http/http_stream_factory_impl_request_unittest.cc
@@ -21,8 +21,7 @@ INSTANTIATE_TEST_CASE_P(
     NextProto,
     HttpStreamFactoryImplRequestTest,
     testing::Values(kProtoDeprecatedSPDY2,
-                    kProtoSPDY3, kProtoSPDY31, kProtoSPDY4a2,
-                    kProtoHTTP2Draft04));
+                    kProtoSPDY3, kProtoSPDY31, kProtoSPDY4));
 
 namespace {
 
