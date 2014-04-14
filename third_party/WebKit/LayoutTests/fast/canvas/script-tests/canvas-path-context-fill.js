92a2a5ba2e115439f5dd888c6dd22b696985416d
fs@opera.com <fs@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-14 08:08:52 +0000

    Simplify CRC2D winding rule handling
    Since the bindings already check the enumeration, there's no need to
    check that the value passed is valid. Make the parseWinding helper function
    just return the translated value instead of a status boolean.
    
    Review URL: https://codereview.chromium.org/226733004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171438 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/canvas/script-tests/canvas-path-context-fill.js
+++ third_party/WebKit/LayoutTests/fast/canvas/script-tests/canvas-path-context-fill.js
@@ -54,7 +54,7 @@ function testFillWith(fillRule, path) {
 // Execute test.
 function prepareTestScenario() {
     fillRules = [undefined, 'nonzero', 'evenodd'];
-    var path = new Path2D();
+    path = new Path2D();
     drawRectanglesOn(path);
 
     for (var i = 0; i < fillRules.length; i++) {
@@ -71,6 +71,8 @@ function prepareTestScenario() {
     shouldThrow("ctx.fill(null, 'evenodd')");
     shouldThrow("ctx.fill([], 'evenodd')");
     shouldThrow("ctx.fill({}, 'evenodd')");
+    shouldThrow("ctx.fill('gazonk')");
+    shouldThrow("ctx.fill(path, 'gazonk')");
 }
 
 // Run test and allow variation of results.
--- third_party/WebKit/Source/core/html/canvas/CanvasRenderingContext2D.cpp
+++ third_party/WebKit/Source/core/html/canvas/CanvasRenderingContext2D.cpp
@@ -965,16 +965,15 @@ static bool isFullCanvasCompositeMode(CompositeOperator op)
     return op == CompositeSourceIn || op == CompositeSourceOut || op == CompositeDestinationIn || op == CompositeDestinationAtop;
 }
 
-static bool parseWinding(const String& windingRuleString, WindRule& windRule)
+static WindRule parseWinding(const String& windingRuleString)
 {
     if (windingRuleString == "nonzero")
-        windRule = RULE_NONZERO;
-    else if (windingRuleString == "evenodd")
-        windRule = RULE_EVENODD;
-    else
-        return false;
+        return RULE_NONZERO;
+    if (windingRuleString == "evenodd")
+        return RULE_EVENODD;
 
-    return true;
+    ASSERT_NOT_REACHED();
+    return RULE_EVENODD;
 }
 
 void CanvasRenderingContext2D::fillInternal(const Path& path, const String& windingRuleString)
@@ -1001,11 +1000,7 @@ void CanvasRenderingContext2D::fillInternal(const Path& path, const String& wind
     }
 
     WindRule windRule = c->fillRule();
-    WindRule newWindRule = RULE_NONZERO;
-    if (!parseWinding(windingRuleString, newWindRule)) {
-        return;
-    }
-    c->setFillRule(newWindRule);
+    c->setFillRule(parseWinding(windingRuleString));
 
     if (isFullCanvasCompositeMode(state().m_globalComposite)) {
         fullCanvasCompositedFill(path);
@@ -1110,13 +1105,8 @@ void CanvasRenderingContext2D::clipInternal(const Path& path, const String& wind
         return;
     }
 
-    WindRule newWindRule = RULE_NONZERO;
-    if (!parseWinding(windingRuleString, newWindRule)) {
-        return;
-    }
-
     realizeSaves();
-    c->canvasClip(path, newWindRule);
+    c->canvasClip(path, parseWinding(windingRuleString));
 }
 
 void CanvasRenderingContext2D::clip(const String& windingRuleString)
@@ -1173,11 +1163,7 @@ bool CanvasRenderingContext2D::isPointInPathInternal(const Path& path, const flo
     if (!std::isfinite(transformedPoint.x()) || !std::isfinite(transformedPoint.y()))
         return false;
 
-    WindRule windRule = RULE_NONZERO;
-    if (!parseWinding(windingRuleString, windRule))
-        return false;
-
-    return path.contains(transformedPoint, windRule);
+    return path.contains(transformedPoint, parseWinding(windingRuleString));
 }
 
 bool CanvasRenderingContext2D::isPointInStroke(const float x, const float y)
