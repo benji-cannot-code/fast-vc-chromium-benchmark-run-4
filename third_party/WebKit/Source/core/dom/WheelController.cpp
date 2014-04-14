7d21a9dabe27c1808c5e878e2805f40fa8273891
skyostil@chromium.org <skyostil@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-14 19:10:12 +0000

    Refactor wheel event handler registration in ScrollingCoordinator
    This patch simplifies the process of observing wheel event handlers in
    the ScrollingCoordinator. We do not need to compute exact counts of
    handlers but instead check whether there are any or not.
    
    We also add an API for indicating the presence of scroll event handlers.
    This will be used by the compositor to schedule scrolling differently when
    handlers are consuming scroll updates.
    
    See https://codereview.chromium.org/206603002/ for full review history.
    
    BUG=347366
    
    Review URL: https://codereview.chromium.org/234913005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171506 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/WheelController.cpp
+++ third_party/WebKit/Source/core/dom/WheelController.cpp
@@ -70,11 +70,7 @@ static void wheelEventHandlerCountChanged(Document& document)
     if (!scrollingCoordinator)
         return;
 
-    FrameView* frameView = document.view();
-    if (!frameView)
-        return;
-
-    scrollingCoordinator->frameViewWheelEventHandlerCountChanged(frameView);
+    scrollingCoordinator->updateHaveWheelEventHandlers();
 }
 
 void WheelController::didAddWheelEventHandler(Document& document)
