0cb035c7f32aaa07c7b3f3d76158569f1516e92e
yoav@yoav.ws <yoav@yoav.ws@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-14 17:19:13 +0000

    Align srcset parsing with spec changed
    This CL align the srcset parsing algorithm with recent picture spec changes.
    Specifically:
    * Src URL are now not part of the source selection algoritm, as per https://github.com/ResponsiveImagesCG/picture-element/pull/147
    * 0w resources are ignored, to avoid 0/0 operations, and because they make no sense, as per https://github.com/ResponsiveImagesCG/picture-element/pull/151
    
    BUG=357586
    
    Review URL: https://codereview.chromium.org/236593003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171489 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/parser/HTMLSrcsetParser.cpp
+++ third_party/WebKit/Source/core/html/parser/HTMLSrcsetParser.cpp
@@ -76,7 +76,7 @@ static bool parseDescriptors(const CharType* descriptorsStart, const CharType* d
             if (result.foundDescriptor())
                 return false;
             result.resourceWidth = charactersToInt(currentDescriptorStart, descriptorLength, &isValid);
-            if (!isValid || result.resourceWidth < 0)
+            if (!isValid || result.resourceWidth <= 0)
                 return false;
         }
     }
@@ -123,7 +123,11 @@ static void parseImageCandidatesFromSrcsetAttribute(const String& attribute, con
                 continue;
         }
 
-        imageCandidates.append(ImageCandidate(attribute, imageURLStart - attributeStart, imageURLEnd - imageURLStart, result));
+        ASSERT(imageURLEnd > attributeStart);
+        unsigned imageURLStartingPosition = imageURLStart - attributeStart;
+        ASSERT(imageURLEnd > imageURLStart);
+        unsigned imageURLLength = imageURLEnd - imageURLStart;
+        imageCandidates.append(ImageCandidate(attribute, imageURLStartingPosition, imageURLLength, result, ImageCandidate::SrcsetOrigin));
         // 11. Return to the step labeled splitting loop.
     }
 }
@@ -141,20 +145,23 @@ static void parseImageCandidatesFromSrcsetAttribute(const String& attribute, Vec
 
 static ImageCandidate pickBestImageCandidate(float deviceScaleFactor, int effectiveSize, Vector<ImageCandidate>& imageCandidates)
 {
+    bool ignoreSrc = false;
     if (imageCandidates.isEmpty())
         return ImageCandidate();
 
     // http://picture.responsiveimages.org/#normalize-source-densities
     for (Vector<ImageCandidate>::iterator it = imageCandidates.begin(); it != imageCandidates.end(); ++it) {
-        if (it->scaleFactor() < 0)
+        if (it->resourceWidth() > 0) {
             it->setScaleFactor((float)it->resourceWidth() / (float)effectiveSize);
+            ignoreSrc = true;
+        }
     }
 
     std::stable_sort(imageCandidates.begin(), imageCandidates.end(), compareByScaleFactor);
 
     unsigned i;
     for (i = 0; i < imageCandidates.size() - 1; ++i) {
-        if (imageCandidates[i].scaleFactor() >= deviceScaleFactor)
+        if ((imageCandidates[i].scaleFactor() >= deviceScaleFactor) && (!ignoreSrc || !imageCandidates[i].srcOrigin()))
             break;
     }
 
@@ -185,7 +192,7 @@ ImageCandidate bestFitSourceForImageAttributes(float deviceScaleFactor, int effe
     if (srcsetAttribute.isNull()) {
         if (srcAttribute.isNull())
             return ImageCandidate();
-        return ImageCandidate(srcAttribute, 0, srcAttribute.length(), defaultResult);
+        return ImageCandidate(srcAttribute, 0, srcAttribute.length(), defaultResult, ImageCandidate::SrcOrigin);
     }
 
     Vector<ImageCandidate> imageCandidates;
@@ -193,7 +200,7 @@ ImageCandidate bestFitSourceForImageAttributes(float deviceScaleFactor, int effe
     parseImageCandidatesFromSrcsetAttribute(srcsetAttribute, imageCandidates);
 
     if (!srcAttribute.isEmpty())
-        imageCandidates.append(ImageCandidate(srcAttribute, 0, srcAttribute.length(), defaultResult));
+        imageCandidates.append(ImageCandidate(srcAttribute, 0, srcAttribute.length(), defaultResult, ImageCandidate::SrcOrigin));
 
     return pickBestImageCandidate(deviceScaleFactor, effectiveSize, imageCandidates);
 }
@@ -210,7 +217,7 @@ String bestFitSourceForImageAttributes(float deviceScaleFactor, int effectiveSiz
     imageCandidates.append(srcsetImageCandidate);
 
     if (!srcAttribute.isEmpty())
-        imageCandidates.append(ImageCandidate(srcAttribute, 0, srcAttribute.length(), defaultResult));
+        imageCandidates.append(ImageCandidate(srcAttribute, 0, srcAttribute.length(), defaultResult, ImageCandidate::SrcOrigin));
 
     return pickBestImageCandidate(deviceScaleFactor, effectiveSize, imageCandidates).toString();
 }
