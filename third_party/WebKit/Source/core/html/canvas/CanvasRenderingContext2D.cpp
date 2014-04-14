2ae4d01b45aa6cc301f35ba61fd65e3cf77628a3
fs@opera.com <fs@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-14 15:24:04 +0000

    Add StrictTypeChecking to CRC2D.{drawFocusIfNeeded,drawCustomFocusRing}
    'null' should not be allowed per current spec. so modify tests accordingly.
    
    BUG=363148
    
    Review URL: https://codereview.chromium.org/237063002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171474 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/canvas/CanvasRenderingContext2D.cpp
+++ third_party/WebKit/Source/core/html/canvas/CanvasRenderingContext2D.cpp
@@ -2373,11 +2373,12 @@ bool CanvasRenderingContext2D::drawCustomFocusRing(Element* element)
 
 bool CanvasRenderingContext2D::focusRingCallIsValid(const Path& path, Element* element)
 {
+    ASSERT(element);
     if (!state().m_invertibleCTM)
         return false;
     if (path.isEmpty())
         return false;
-    if (!element || !element->isDescendantOf(canvas()))
+    if (!element->isDescendantOf(canvas()))
         return false;
 
     return true;
