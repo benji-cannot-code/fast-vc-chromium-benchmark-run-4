5fec4b498267a604def1fec591f9c029db5e51a8
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-14 20:54:02 +0000

    Test runner is in the Chromium repo, so clean up some cross-repo machinery that's no longer needed.
    BUG=324658
    TEST=no change
    
    Review URL: https://codereview.chromium.org/228663012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263723 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/renderer/test_runner/WebFrameTestProxy.h
+++ content/shell/renderer/test_runner/WebFrameTestProxy.h
@@ -22,19 +22,13 @@ class WebFrameTestProxy : public Base {
 public:
     WebFrameTestProxy(P p, R r)
         : Base(p, r)
-        , m_baseProxy(0)
-        , m_version(0) { }
+        , m_baseProxy(0) { }
 
     virtual ~WebFrameTestProxy() { }
 
     void setBaseProxy(WebTestProxyBase* proxy)
     {
         m_baseProxy = proxy;
-    }
-
-    void setVersion(int version)
-    {
-        m_version = version;
     }
 
     blink::WebPlugin* createPlugin(blink::WebLocalFrame* frame, const blink::WebPluginParams& params)
@@ -60,8 +54,7 @@ public:
     }
     virtual void didStartProvisionalLoad(blink::WebLocalFrame* frame)
     {
-        if (m_version > 2)
-            m_baseProxy->didStartProvisionalLoad(frame);
+        m_baseProxy->didStartProvisionalLoad(frame);
         Base::didStartProvisionalLoad(frame);
     }
     virtual void didReceiveServerRedirectForProvisionalLoad(blink::WebLocalFrame* frame)
@@ -203,11 +196,6 @@ public:
 
 private:
     WebTestProxyBase* m_baseProxy;
-
-    // This is used to incrementally change code between Blink and Chromium.
-    // It is used instead of a #define and is set by layouttest_support when
-    // creating this object.
-    int m_version;
 
     DISALLOW_COPY_AND_ASSIGN(WebFrameTestProxy);
 };
