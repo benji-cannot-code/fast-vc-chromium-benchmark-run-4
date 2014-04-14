fc715a6237ae49b28d258a84ed5648663725b29c
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-14 16:17:38 +0000

    bookmarks: Duplicate kJavaScriptScheme constant in bookmark_node_data_views.cc
    In order to make us less reliant on content/ API, we just duplicate this
    constant. This will allow us to move bookmark_node_data_views.cc into
    components/bookmarks/ in a next CL.
    
    BUG=360616
    TEST=None, no functional changes
    R=sky@chromium.org,droger@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/235943005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263640 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/bookmarks/bookmark_node_data_views.cc
+++ chrome/browser/bookmarks/bookmark_node_data_views.cc
@@ -8,8 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/pickle.h"
 #include "base/strings/utf_string_conversions.h"
-#include "content/public/common/url_constants.h"
 #include "ui/base/clipboard/clipboard.h"
+
+namespace {
+
+const char kJavaScriptScheme[] = "javascript";
+
+}  // namespace
 
 // static
 const ui::OSExchangeData::CustomFormat&
@@ -28,7 +33,7 @@ void BookmarkNodeData::Write(Profile* profile, ui::OSExchangeData* data) const {
   // If there is only one element and it is a URL, write the URL to the
   // clipboard.
   if (elements.size() == 1 && elements[0].is_url) {
-    if (elements[0].url.SchemeIs(content::kJavaScriptScheme)) {
+    if (elements[0].url.SchemeIs(kJavaScriptScheme)) {
       data->SetString(base::UTF8ToUTF16(elements[0].url.spec()));
     } else {
       data->SetURL(elements[0].url, elements[0].title);
