2f4ecfe1ab13d5a3bef39fe1be806c741cdbcd10
timloh@chromium.org <timloh@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-14 08:50:23 +0000

    Move default StyleBuilderFunctions handlers from StyleBuilder.cpp to StyleBuilderFunctions.cpp
    Some of the StyleBuilderFunctions handlers were being defined in
    StyleBuilder.cpp. This patch cleans moves them to the correct place and
    makes some minor clean-ups inside (removing the unused condition check
    and a redundant isPrimitiveValue check).
    
    Review URL: https://codereview.chromium.org/236763002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171443 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/build/scripts/make_style_builder.py
+++ third_party/WebKit/Source/build/scripts/make_style_builder.py
@@ -47,7 +47,6 @@ class StyleBuilderWriter(in_generator.Writer):
         'custom_value': [True, False],
     }
     defaults = {
-        'condition': None,
         'name_for_methods': None,
         'use_handlers_for': None,
         'svg': False,
