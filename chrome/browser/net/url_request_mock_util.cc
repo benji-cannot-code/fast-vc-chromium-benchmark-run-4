7d0795a0a2991003f8834cea5e70d6053f22da91
mmenke@chromium.org <mmenke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-15 18:59:58 +0000

    Revert 262846, as the revert has now been merged to M35.
    Still need to fix the underlying tracking issue before M36 branch.
    
    > Revert 255617, due to it not tracking use of the link doctor page properly.
    > 
    > Not reverting the string changes, because the new strings work with the old
    > file, and we've passed string freeze.
    > 
    > > Switch to using the new Link Doctor API.
    > >
    > > The new API allows Link Doctor results to be integrated into Chrome's
    > > own error pages.
    > 
    > BUG=64832, 359131
    > TBR=mmenke@chromium.org
    > 
    > Originally Landed: https://src.chromium.org/viewvc/chrome?view=rev&revision=255617
    > 
    > Review URL: https://codereview.chromium.org/228803002
    
    TBR=mmenke@chromium.org
    
    Review URL: https://codereview.chromium.org/235883010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263956 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/url_request_mock_util.cc
+++ chrome/browser/net/url_request_mock_util.cc
@@ -42,7 +42,7 @@ void SetUrlRequestMocksEnabled(bool enabled) {
     content::URLRequestMockHTTPJob::AddUrlHandler(root_http);
     content::URLRequestMockHTTPJob::AddHostnameToFileHandler(
         google_util::LinkDoctorBaseURL().host(),
-        root_http.AppendASCII("mock-link-doctor.html"));
+        root_http.AppendASCII("mock-link-doctor.json"));
   } else {
     // Revert to the default handlers.
     net::URLRequestFilter::GetInstance()->ClearHandlers();
