64b8b2ea73a3024efd2165827f155635a1669eed
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-15 05:40:31 +0000

    Don't allow sharing between elements that have different :host styles
    Elements can only share style if they either both don't have any
    ShadowRoots or they both have ShadowRoots and all their ShadowRoots have
    the same styles.
    
    We were incorrectly allowing sharing between elements without shadow roots
    and ones with shadow roots, and also allowing sharing between elements that
    had styles in an older ShadowRoot but no <shadow> in the younger shadow
    root. The second case was previously correct before r170091 when we made
    styles apply even when <shadow> was not present.
    
    Review URL: https://codereview.chromium.org/236193006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171533 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/shadow/ElementShadow.cpp
+++ third_party/WebKit/Source/core/dom/shadow/ElementShadow.cpp
@@ -209,18 +209,7 @@ void ElementShadow::setNeedsDistributionRecalc()
     clearDistribution();
 }
 
-bool ElementShadow::containsActiveStyles() const
-{
-    for (ShadowRoot* root = youngestShadowRoot(); root; root = root->olderShadowRoot()) {
-        if (root->hasScopedHTMLStyleChild())
-            return true;
-        if (!root->containsShadowElements())
-            return false;
-    }
-    return false;
-}
-
-bool ElementShadow::hasSameStyles(ElementShadow *other) const
+bool ElementShadow::hasSameStyles(const ElementShadow* other) const
 {
     ShadowRoot* root = youngestShadowRoot();
     ShadowRoot* otherRoot = other->youngestShadowRoot();
--- third_party/WebKit/Source/core/dom/shadow/ElementShadow.h
+++ third_party/WebKit/Source/core/dom/shadow/ElementShadow.h
@@ -53,8 +53,7 @@ public:
 
     ShadowRoot& addShadowRoot(Element& shadowHost, ShadowRoot::ShadowRootType);
 
-    bool containsActiveStyles() const;
-    bool hasSameStyles(ElementShadow *) const;
+    bool hasSameStyles(const ElementShadow*) const;
 
     void attach(const Node::AttachContext&);
     void detach(const Node::AttachContext&);
