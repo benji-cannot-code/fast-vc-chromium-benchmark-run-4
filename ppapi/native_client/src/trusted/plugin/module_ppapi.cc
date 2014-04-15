0f5c734fe054e0d587558794df13ce1a2053ef57
teravest@chromium.org <teravest@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-15 18:39:51 +0000

    Pepper: Fix MODULE_PRINTF behavior.
    An earlier change to use VLOG() as the destination for pluging logging messages
    caused problems with MODULE_PRINTF() statements when NACL_PLUGIN_DEBUG is set.
    
    This change alters MODULE_PRINTF() and PLUGIN_PRINTF() to log to stderr when
    the NaCl interface isn't available, and tightens up where the PPB_NaCl_Private
    interface is set.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/236943004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263942 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/native_client/src/trusted/plugin/module_ppapi.cc
+++ ppapi/native_client/src/trusted/plugin/module_ppapi.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/native_client/src/trusted/plugin/module_ppapi.h"
 #include "ppapi/native_client/src/trusted/plugin/nacl_entry_points.h"
 #include "ppapi/native_client/src/trusted/plugin/plugin.h"
+#include "ppapi/native_client/src/trusted/plugin/utility.h"
 
 namespace plugin {
 
@@ -42,6 +43,7 @@ bool ModulePpapi::Init() {
                    "GetBrowserInterface returned NULL\n"));
     return false;
   }
+  SetNaClInterface(private_interface_);
 
   launch_nacl_process = reinterpret_cast<LaunchNaClProcessFunc>(
       private_interface_->LaunchSelLdr);
