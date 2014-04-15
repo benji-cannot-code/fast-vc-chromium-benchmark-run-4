64edad5e5b5e630e4b4ad84aba3bea6d1134d181
ziran.sun@samsung.com <ziran.sun@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-15 14:06:10 +0000

    Wrap isActivatedSubmit() in WebFormControlElement
    This is related refactoring old Blink code, specifically moving functions in
    WebPasswordFormData and WebPasswordFormUtils into chromium side. Inevitably,
    HTMLFormControlElement.isActivatedSubmit() that is called in WebPasswordFormData
    needs to be exposed to chromium side.
    
    R=gcasto@chromium.org, tkent@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/234803004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171571 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebInputElement.cpp
+++ third_party/WebKit/Source/web/WebInputElement.cpp
@@ -83,11 +83,6 @@ int WebInputElement::maxLength() const
     return constUnwrap<HTMLInputElement>()->maxLength();
 }
 
-bool WebInputElement::isActivatedSubmit() const
-{
-    return constUnwrap<HTMLInputElement>()->isActivatedSubmit();
-}
-
 void WebInputElement::setActivatedSubmit(bool activated)
 {
     unwrap<HTMLInputElement>()->setActivatedSubmit(activated);
