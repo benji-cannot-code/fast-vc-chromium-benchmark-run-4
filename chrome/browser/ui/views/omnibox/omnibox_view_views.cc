benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 77df27f4c53a9e50baf27a437ca76414a092cf9d

[OriginChip] Use the proper trigger conditions for hiding the chip.
BUG=338563

Review URL: https://codereview.chromium.org/149793009

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250041 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/views/omnibox/omnibox_view_views.cc
@@ -184,6 +184,14 @@ void OmniboxViewViews::Init() {
 ////////////////////////////////////////////////////////////////////////////////
 // OmniboxViewViews, views::Textfield implementation:
 
+void OmniboxViewViews::AboutToRequestFocusFromTabTraversal(bool reverse) {
+  if (chrome::GetOriginChipV2HideTrigger() ==
+      chrome::ORIGIN_CHIP_V2_HIDE_ON_MOUSE_RELEASE) {
+    controller()->GetToolbarModel()->set_origin_chip_enabled(false);
+    controller()->OnChanged();
+  }
+}
+
 const char* OmniboxViewViews::GetClassName() const {
   return kViewClassName;
 }
@@ -247,15 +255,27 @@ bool OmniboxViewViews::OnMouseDragged(const ui::MouseEvent& event) {
 
 void OmniboxViewViews::OnMouseReleased(const ui::MouseEvent& event) {
   views::Textfield::OnMouseReleased(event);
-  // When the user has clicked and released to give us focus, select all unless
-  // we're omitting the URL (in which case refining an existing query is common
-  // enough that we do click-to-place-cursor).
-  if ((event.IsOnlyLeftMouseButton() || event.IsOnlyRightMouseButton()) &&
-      select_all_on_mouse_release_ &&
-      !controller()->GetToolbarModel()->WouldReplaceURL()) {
-    // Select all in the reverse direction so as not to scroll the caret
-    // into view and shift the contents jarringly.
-    SelectAll(true);
+  if (event.IsOnlyLeftMouseButton() || event.IsOnlyRightMouseButton()) {
+    // When the user has clicked and released to give us focus, select all
+    // unless we're omitting the URL (in which case refining an existing query
+    // is common enough that we do click-to-place-cursor).
+    if (select_all_on_mouse_release_ &&
+        !controller()->GetToolbarModel()->WouldReplaceURL()) {
+      // Select all in the reverse direction so as not to scroll the caret
+      // into view and shift the contents jarringly.
+      SelectAll(true);
+    }
+
+    // If we should hide the origin chip in response to mouse release, check
+    // first that only the left or only the right button was pressed prior to
+    // the release.  If multiple buttons were pressed, we don't want to hide the
+    // chip yet.  If only the middle mouse button was pressed, the Omnibox won't
+    // receive focus so we don't want to hide the chip in this case.
+    if (chrome::GetOriginChipV2HideTrigger() ==
+        chrome::ORIGIN_CHIP_V2_HIDE_ON_MOUSE_RELEASE) {
+      controller()->GetToolbarModel()->set_origin_chip_enabled(false);
+      controller()->OnChanged();
+    }
   }
   select_all_on_mouse_release_ = false;
 }
@@ -373,11 +393,6 @@ void OmniboxViewViews::OnFocus() {
   if (saved_selection_for_focus_change_.IsValid()) {
     SelectRange(saved_selection_for_focus_change_);
     saved_selection_for_focus_change_ = gfx::Range::InvalidRange();
-  }
-
-  if (chrome::ShouldDisplayOriginChipV2()) {
-    controller()->GetToolbarModel()->set_origin_chip_enabled(false);
-    controller()->OnChanged();
   }
 }
 

chrome/browser/ui/omnibox/omnibox_edit_model.cc
@@ -510,6 +510,10 @@ void OmniboxEditModel::SetInputInProgress(bool in_progress) {
     controller_->GetToolbarModel()->set_url_replacement_enabled(true);
   }
 
+  if (chrome::GetOriginChipV2HideTrigger() ==
+      chrome::ORIGIN_CHIP_V2_HIDE_ON_USER_INPUT)
+    controller()->GetToolbarModel()->set_origin_chip_enabled(!in_progress);
+
   controller_->GetToolbarModel()->set_input_in_progress(in_progress);
   controller_->Update(NULL);
 

chrome/browser/ui/views/omnibox/omnibox_view_views.h
@@ -54,6 +54,7 @@ class OmniboxViewViews
   void Init();
 
   // views::Textfield:
+  virtual void AboutToRequestFocusFromTabTraversal(bool reverse) OVERRIDE;
   virtual const char* GetClassName() const OVERRIDE;
   virtual void OnGestureEvent(ui::GestureEvent* event) OVERRIDE;
   virtual void GetAccessibleState(ui::AccessibleViewState* state) OVERRIDE;

commit 104f4c699877def9d666465eab0fbf6977a4d9ac

Fix the Views Omnibox AboutToRequestFocusFromTabTraversal.
Views omnibox tab-to-focus select-all regressed in r250041.
Call the base impl in AboutToRequestFocusFromTabTraversal.

BUG=361507
TEST=Navigate then tab-to-focus selects all omnibox text.
R=pkasting@chromium.org

Review URL: https://codereview.chromium.org/235683002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263832 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/views/omnibox/omnibox_view_views.cc
@@ -791,6 +791,7 @@ void OmniboxViewViews::OnGestureEvent(ui::GestureEvent* event) {
 }
 
 void OmniboxViewViews::AboutToRequestFocusFromTabTraversal(bool reverse) {
+  views::Textfield::AboutToRequestFocusFromTabTraversal(reverse);
   // Tabbing into the omnibox should affect the origin chip in the same way
   // clicking it should.
   HandleOriginChipMouseRelease();

chrome/browser/ui/views/omnibox/omnibox_view_views_browsertest.cc
@@ -223,3 +223,21 @@ IN_PROC_BROWSER_TEST_F(OmniboxViewViewsTest, SelectAllOnTap) {
   EXPECT_FALSE(omnibox_view->IsSelectAll());
 }
 #endif // defined(USE_AURA)
+
+IN_PROC_BROWSER_TEST_F(OmniboxViewViewsTest, SelectAllOnTabToFocus) {
+  OmniboxView* omnibox_view = NULL;
+  ASSERT_NO_FATAL_FAILURE(GetOmniboxViewForBrowser(browser(), &omnibox_view));
+  ui_test_utils::NavigateToURL(browser(), GURL("http://www.google.com/"));
+  // RevertAll after navigation to invalidate the selection range saved on blur.
+  omnibox_view->RevertAll();
+  EXPECT_FALSE(ui_test_utils::IsViewFocused(browser(), VIEW_ID_OMNIBOX));
+  EXPECT_FALSE(omnibox_view->IsSelectAll());
+
+  // Pressing tab to focus the omnibox should select all text.
+  while (!ui_test_utils::IsViewFocused(browser(), VIEW_ID_OMNIBOX)) {
+    ASSERT_TRUE(ui_test_utils::SendKeyPressSync(browser(), ui::VKEY_TAB,
+                                                false, false, false, false));
+  }
+  EXPECT_TRUE(ui_test_utils::IsViewFocused(browser(), VIEW_ID_OMNIBOX));
+  EXPECT_TRUE(omnibox_view->IsSelectAll());
+}