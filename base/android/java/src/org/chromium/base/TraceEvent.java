2ec4ff404af689245148a3f7051dd1837f936de9
epenner@chromium.org <epenner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-15 02:50:32 +0000

    Android: Make looper traces toplevel.
    All other top-level events are now labelled 'toplevel'.
    
    BUG=None
    NOTRY=true
    
    No-try since failing tests are flakes in unrelated code.
    
    Review URL: https://codereview.chromium.org/236203002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263769 0039d316-1c4b-4281-b951-d872f2087c98

--- base/android/java/src/org/chromium/base/TraceEvent.java
+++ base/android/java/src/org/chromium/base/TraceEvent.java
@@ -23,9 +23,6 @@ public class TraceEvent {
     private static volatile boolean sEnabled = false;
 
     private static class BasicLooperMonitor implements Printer {
-        private static final String DISPATCH_EVENT_NAME =
-                "Looper.dispatchMessage";
-
         @Override
         public void println(final String line) {
             if (line.startsWith(">")) {
@@ -37,11 +34,11 @@ public class TraceEvent {
         }
 
         void beginHandling(final String line) {
-            TraceEvent.begin(DISPATCH_EVENT_NAME, line);
+            if (sEnabled) nativeBeginToplevel();
         }
 
         void endHandling(final String line) {
-            TraceEvent.end(DISPATCH_EVENT_NAME);
+            if (sEnabled) nativeEndToplevel();
         }
     }
 
@@ -350,6 +347,8 @@ public class TraceEvent {
     private static native void nativeInstant(String name, String arg);
     private static native void nativeBegin(String name, String arg);
     private static native void nativeEnd(String name, String arg);
+    private static native void nativeBeginToplevel();
+    private static native void nativeEndToplevel();
     private static native void nativeStartAsync(String name, long id, String arg);
     private static native void nativeFinishAsync(String name, long id, String arg);
 }
