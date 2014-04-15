53f492f15a22ba9250d9f76b07ffee8fd3b0505e
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-15 20:58:09 +0000

    remove redundant ifdefs
    OS_CHROMEOS implies OS_LINUX, so OS_LINUX || OS_CHROMEOS can be simplified to OS_LINUX
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/237673002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263993 0039d316-1c4b-4281-b951-d872f2087c98

--- base/security_unittest.cc
+++ base/security_unittest.cc
@@ -60,7 +60,7 @@ const size_t kTooBigAllocSize = INT_MAX;
 
 // Detect runtime TCMalloc bypasses.
 bool IsTcMallocBypassed() {
-#if defined(OS_LINUX) || defined(OS_CHROMEOS)
+#if defined(OS_LINUX)
   // This should detect a TCMalloc bypass from Valgrind.
   char* g_slice = getenv("G_SLICE");
   if (g_slice && !strcmp(g_slice, "always-malloc"))
@@ -231,7 +231,7 @@ TEST(SecurityTest, CallocOverflow) {
   }
 }
 
-#if (defined(OS_LINUX) || defined(OS_CHROMEOS)) && defined(__x86_64__)
+#if defined(OS_LINUX) && defined(__x86_64__)
 // Check if ptr1 and ptr2 are separated by less than size chars.
 bool ArePointersToSameArea(void* ptr1, void* ptr2, size_t size) {
   ptrdiff_t ptr_diff = reinterpret_cast<char*>(std::max(ptr1, ptr2)) -
@@ -287,6 +287,6 @@ TEST(SecurityTest, TCMALLOC_TEST(RandomMemoryAllocations)) {
   EXPECT_FALSE(impossible_random_address);
 }
 
-#endif  // (defined(OS_LINUX) || defined(OS_CHROMEOS)) && defined(__x86_64__)
+#endif  // defined(OS_LINUX) && defined(__x86_64__)
 
 }  // namespace
