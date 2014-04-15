65676f90bfceadabd39752c6d011743171cbd0d8
dominikg@chromium.org <dominikg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-15 13:08:01 +0000

    Remove Skia legacy API from Chrome.
    Skia paint effects have recently been refactored to make them only allocatable
    on the heap. This has been achieved by making constructors non-public and
    providing factory methods instead.
    
    If necessary, we kept the old API behind a flag so as to not break Chrome or
    Blink. This CL updates Chrome to use the new API and disable the flag.
    
    BUG=skia:2187
    
    Review URL: https://codereview.chromium.org/236833008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263827 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/output/render_surface_filters.cc
+++ cc/output/render_surface_filters.cc
@@ -152,7 +152,7 @@ skia::RefPtr<SkImageFilter> CreateMatrixImageFilter(
     const SkScalar matrix[20],
     const skia::RefPtr<SkImageFilter>& input) {
   skia::RefPtr<SkColorFilter> color_filter =
-      skia::AdoptRef(new SkColorMatrixFilter(matrix));
+      skia::AdoptRef(SkColorMatrixFilter::Create(matrix));
   return skia::AdoptRef(
       SkColorFilterImageFilter::Create(color_filter.get(), input.get()));
 }
@@ -200,11 +200,11 @@ skia::RefPtr<SkImageFilter> RenderSurfaceFilters::BuildImageFilter(
         image_filter = CreateMatrixImageFilter(matrix, image_filter);
         break;
       case FilterOperation::BLUR:
-        image_filter = skia::AdoptRef(new SkBlurImageFilter(
+        image_filter = skia::AdoptRef(SkBlurImageFilter::Create(
             op.amount(), op.amount(), image_filter.get()));
         break;
       case FilterOperation::DROP_SHADOW:
-        image_filter = skia::AdoptRef(new SkDropShadowImageFilter(
+        image_filter = skia::AdoptRef(SkDropShadowImageFilter::Create(
             SkIntToScalar(op.drop_shadow_offset().x()),
             SkIntToScalar(op.drop_shadow_offset().y()),
             SkIntToScalar(op.amount()),
@@ -215,8 +215,8 @@ skia::RefPtr<SkImageFilter> RenderSurfaceFilters::BuildImageFilter(
         image_filter = CreateMatrixImageFilter(op.matrix(), image_filter);
         break;
       case FilterOperation::ZOOM: {
-        skia::RefPtr<SkImageFilter> zoom_filter = skia::AdoptRef(
-            new SkMagnifierImageFilter(
+        skia::RefPtr<SkImageFilter> zoom_filter =
+            skia::AdoptRef(SkMagnifierImageFilter::Create(
                 SkRect::MakeXYWH(
                     (size.width() - (size.width() / op.amount())) / 2.f,
                     (size.height() - (size.height() / op.amount())) / 2.f,
@@ -227,7 +227,7 @@ skia::RefPtr<SkImageFilter> RenderSurfaceFilters::BuildImageFilter(
           // TODO(ajuma): When there's a 1-input version of
           // SkMagnifierImageFilter, use that to handle the input filter
           // instead of using an SkComposeImageFilter.
-          image_filter = skia::AdoptRef(new SkComposeImageFilter(
+          image_filter = skia::AdoptRef(SkComposeImageFilter::Create(
               zoom_filter.get(), image_filter.get()));
         } else {
           image_filter = zoom_filter;
@@ -254,7 +254,7 @@ skia::RefPtr<SkImageFilter> RenderSurfaceFilters::BuildImageFilter(
             !op.image_filter()->getInput(0)) {
           image_filter = CreateMatrixImageFilter(matrix, image_filter);
         } else if (image_filter) {
-          image_filter = skia::AdoptRef(new SkComposeImageFilter(
+          image_filter = skia::AdoptRef(SkComposeImageFilter::Create(
               op.image_filter().get(), image_filter.get()));
         } else {
           image_filter = op.image_filter();
