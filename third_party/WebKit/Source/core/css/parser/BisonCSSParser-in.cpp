fe442405116c4cfb7cd173e730ba3ac0d826a759
zvorygin@chromium.org <zvorygin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-15 12:36:55 +0000

    Added backround-repeat-x and -y as keyword properties.
    Now code like <element>.style['background-repeat-x'] = 'no-repeat' would work.
    
    BUG=352140
    
    Review URL: https://codereview.chromium.org/231153003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171568 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/parser/BisonCSSParser-in.cpp
+++ third_party/WebKit/Source/core/css/parser/BisonCSSParser-in.cpp
@@ -368,6 +368,11 @@ bool isValidKeywordPropertyAndValue(CSSPropertyID propertyId, int valueID, const
         return false;
 
     switch (propertyId) {
+    case CSSPropertyBackgroundRepeatX: // repeat | no-repeat | inherit
+    case CSSPropertyBackgroundRepeatY: // repeat | no-repeat | inherit
+        if (valueID == CSSValueRepeat || valueID == CSSValueNoRepeat)
+            return true;
+        break;
     case CSSPropertyBorderCollapse: // collapse | separate | inherit
         if (valueID == CSSValueCollapse || valueID == CSSValueSeparate)
             return true;
@@ -740,6 +745,8 @@ bool isKeywordPropertyID(CSSPropertyID propertyId)
     switch (propertyId) {
     case CSSPropertyMixBlendMode:
     case CSSPropertyIsolation:
+    case CSSPropertyBackgroundRepeatX:
+    case CSSPropertyBackgroundRepeatY:
     case CSSPropertyBorderBottomStyle:
     case CSSPropertyBorderCollapse:
     case CSSPropertyBorderLeftStyle:
