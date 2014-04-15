deef52b8cb709b88bf2f1621d5d0f100ef0df552
enne@chromium.org <enne@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-15 07:35:00 +0000

    Disable compositing asserts for background color
    R=abarth@chromium.org
    BUG=362750
    
    Review URL: https://codereview.chromium.org/236033003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171543 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/FrameView.cpp
+++ third_party/WebKit/Source/core/frame/FrameView.cpp
@@ -1902,6 +1902,8 @@ void FrameView::setBaseBackgroundColor(const Color& backgroundColor)
 {
     m_baseBackgroundColor = backgroundColor;
 
+    // FIXME: http://code.google.com/p/chromium/issues/detail?id=362750
+    DisableCompositingQueryAsserts disabler;
     if (renderView() && renderView()->layer()->hasCompositedLayerMapping()) {
         CompositedLayerMappingPtr compositedLayerMapping = renderView()->layer()->compositedLayerMapping();
         compositedLayerMapping->updateContentsOpaque();
