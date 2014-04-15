7a9e8fdc966d7ebacaaf2a0737520df19efeab2f
sky@chromium.org <sky@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-15 19:47:25 +0000

    Makes windows repaint scrollbars when position changes
    This is needed since arrows draw differently when thumb is at end
    points.
    
    BUG=358566
    TEST=see bug
    R=eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/237943002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171683 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/scroll/Scrollbar.cpp
+++ third_party/WebKit/Source/platform/scroll/Scrollbar.cpp
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using namespace std;
 
-#if OS(POSIX) && !OS(MACOSX)
+#if ((OS(POSIX) && !OS(MACOSX)) || OS(WIN))
 // The position of the scrollbar thumb affects the appearance of the steppers, so
 // when the thumb moves, we have to invalidate them for painting.
 #define THUMB_POSITION_AFFECTS_BUTTONS
