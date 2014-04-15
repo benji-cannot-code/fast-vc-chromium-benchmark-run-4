43680f78857ac5b0ff4947bf03e2d8984a59afde
hidehiko@chromium.org <hidehiko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-15 15:21:15 +0000

    Enable PPAPI tests for non-SFI mode.
    This CL adds more tests of PPAPI for NaCl in non-SFI mode.
    
    TEST=Run locally, and ran trybots.
    BUG=358425
    
    Review URL: https://codereview.chromium.org/223423006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263858 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/ppapi/ppapi_test.cc
+++ chrome/test/ppapi/ppapi_test.cc
@@ -446,6 +446,12 @@ std::string PPAPINaClPNaClTest::BuildQuery(const std::string& base,
                             test_case.c_str());
 }
 
+void PPAPIPrivateNaClPNaClTest::SetUpCommandLine(
+    base::CommandLine* command_line) {
+  PPAPINaClPNaClTest::SetUpCommandLine(command_line);
+  AddPrivateSwitches(command_line);
+}
+
 void PPAPINaClPNaClNonSfiTest::SetUpCommandLine(
     base::CommandLine* command_line) {
   PPAPINaClTest::SetUpCommandLine(command_line);
@@ -459,9 +465,9 @@ std::string PPAPINaClPNaClNonSfiTest::BuildQuery(
                             base.c_str(), test_case.c_str());
 }
 
-void PPAPIPrivateNaClPNaClTest::SetUpCommandLine(
+void PPAPIPrivateNaClPNaClNonSfiTest::SetUpCommandLine(
     base::CommandLine* command_line) {
-  PPAPINaClPNaClTest::SetUpCommandLine(command_line);
+  PPAPINaClPNaClNonSfiTest::SetUpCommandLine(command_line);
   AddPrivateSwitches(command_line);
 }
 
