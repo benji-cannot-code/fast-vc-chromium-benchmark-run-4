627e51a83b439d34f1a705f0a50aca2638ea8d1b
stevenjb@chromium.org <stevenjb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-15 17:04:57 +0000

    Separate popup window size pref, fix Ash logic
    The current window sizing logic for popup windows is a bit inconsistent.
    This CL separates the preferered popup window size from the preferred
    browser window size (since the sizing and layout logic is different),
    and fixes the ASH behavior.
    
    Currently normal popup windows are unusual, but the settings window is
    treated as a popup window so the the sizing quirks are more noticible.
    
    BUG=360106
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/233313006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263890 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser_ui_prefs.cc
+++ chrome/browser/ui/browser_ui_prefs.cc
@@ -132,6 +132,9 @@ void RegisterBrowserUserPrefs(user_prefs::PrefRegistrySyncable* registry) {
       prefs::kBrowserWindowPlacement,
       user_prefs::PrefRegistrySyncable::UNSYNCABLE_PREF);
   registry->RegisterDictionaryPref(
+      prefs::kBrowserWindowPlacementPopup,
+      user_prefs::PrefRegistrySyncable::UNSYNCABLE_PREF);
+  registry->RegisterDictionaryPref(
       prefs::kPreferencesWindowPlacement,
       user_prefs::PrefRegistrySyncable::UNSYNCABLE_PREF);
   registry->RegisterBooleanPref(
