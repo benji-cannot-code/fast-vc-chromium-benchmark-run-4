a3fde7c83de855b7e469ec4b4f9fb41d12481d4a
tmdiep@chromium.org <tmdiep@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-15 15:20:15 +0000

    Promote new app window bounds API to stable
    This patch removes the dev channel restrictions for the app window
    bounds API. Old bounds properties and functions are marked deprecated
    in the documentation.
    
    BUG=315471
    TEST=browser_tests (AppWindowAPITest.*)
    
    Review URL: https://codereview.chromium.org/223073003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263856 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/app_current_window_internal/app_current_window_internal_api.cc
+++ chrome/browser/extensions/api/app_current_window_internal/app_current_window_internal_api.cc
@@ -202,12 +202,6 @@ bool AppCurrentWindowInternalSetBoundsFunction::RunWithWindow(
     return false;
   }
 
-  if (bounds_type != bounds::DEPRECATED_BOUNDS &&
-      GetCurrentChannel() > chrome::VersionInfo::CHANNEL_DEV) {
-    error_ = kDevChannelOnly;
-    return false;
-  }
-
   // Start with the current bounds, and change any values that are specified in
   // the incoming parameters.
   gfx::Rect original_window_bounds = window->GetBaseWindow()->GetBounds();
@@ -263,11 +257,6 @@ bool AppCurrentWindowInternalSetBoundsFunction::RunWithWindow(
 
 bool AppCurrentWindowInternalSetSizeConstraintsFunction::RunWithWindow(
     AppWindow* window) {
-  if (GetCurrentChannel() > chrome::VersionInfo::CHANNEL_DEV) {
-    error_ = kDevChannelOnly;
-    return false;
-  }
-
   scoped_ptr<SetSizeConstraints::Params> params(
       SetSizeConstraints::Params::Create(*args_));
   CHECK(params.get());
