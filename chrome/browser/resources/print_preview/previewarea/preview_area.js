861a1df0635673c4301eb3530e62a907d704f343
raymes@google.com <raymes@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-15 01:25:07 +0000

    Fix PrintPreviewWebUITest.* browser_tests
    The test hadn't been changed to stub out the correct function after refactoring
    in https://codereview.chromium.org/224903018/
    
    BUG=363435
    R=thestig@chromium.org
    
    Review URL: https://codereview.chromium.org/238243003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263765 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/print_preview/previewarea/preview_area.js
+++ chrome/browser/resources/print_preview/previewarea/preview_area.js
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 cr.define('print_preview', function() {
   'use strict';
 
-<include src="../../pdf/pdf_scripting_api.js"></include>
+<include src="../../pdf/pdf_scripting_api.js" />
 
   /**
    * Creates a PreviewArea object. It represents the area where the preview
