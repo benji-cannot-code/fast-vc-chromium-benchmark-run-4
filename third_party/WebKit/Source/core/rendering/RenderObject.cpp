83e40dff13b8a89a17621520ad1e47bc54a111fb
leviw@chromium.org <leviw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-16 01:02:57 +0000

    Add FIXMEs to canUseLayoutStateForContainer and null repaint container codepaths
    Ideally, LayoutState should work for all repaint containers, and
    we want to kill the null repaint container code path.
    
    Review URL: https://codereview.chromium.org/239823002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171723 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderObject.cpp
+++ third_party/WebKit/Source/core/rendering/RenderObject.cpp
@@ -1332,6 +1332,7 @@ RenderLayerModelObject* RenderObject::containerForRepaint() const
     if (!isRooted())
         return 0;
 
+    // FIXME: Repaint container should never be null when we're rooted. crbug.com/363699
     RenderLayerModelObject* repaintContainer = 0;
 
     RenderView* v = view();
@@ -1368,6 +1369,7 @@ void RenderObject::repaintUsingContainer(const RenderLayerModelObject* repaintCo
         "info", TRACE_STR_COPY(String::format("rect: %d,%d %dx%d, invalidation_reason: %s",
             r.x(), r.y(), r.width(), r.height(), invalidationReasonToString(invalidationReason)).ascii().data()));
 
+    // FIXME: Repaint container should never be null. crbug.com/363699
     if (!repaintContainer) {
         view()->repaintViewRectangle(r);
         return;
@@ -1550,6 +1552,7 @@ bool RenderObject::repaintAfterLayoutIfNeeded(const RenderLayerModelObject* repa
     if (invalidationReason == InvalidationIncremental && newBounds.location() != oldBounds.location())
         invalidationReason = InvalidationBoundsChange;
 
+    // FIXME: Repaint container should never be null. crbug.com/363699
     if (!repaintContainer)
         repaintContainer = v;
 
