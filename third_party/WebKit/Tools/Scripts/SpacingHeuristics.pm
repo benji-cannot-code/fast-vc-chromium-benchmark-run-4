03ac9c5f6ec4f67da1a1618c974ecc264302fec9
nbarth@chromium.org <nbarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-16 04:56:33 +0000

    Fix naming and shebang in Tools/Scripts
    * Removes excess '.py'
    * Removes incorrect #! on a '.pm' (Perl module, not executable)
    
    R=dpranke
    
    Review URL: https://codereview.chromium.org/239533008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171744 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/SpacingHeuristics.pm
+++ third_party/WebKit/Tools/Scripts/SpacingHeuristics.pm
@@ -1,6 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/perl -w
-
 # Copyright (C) 2006 Apple Computer, Inc.  All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
