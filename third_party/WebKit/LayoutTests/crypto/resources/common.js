5b365d329b6a44478bf8288d8efce46fc1c5f487
eroman@chromium.org <eroman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-16 18:01:50 +0000

    [webcrypto] Update layout tests to match idl generator changes.
    Functions that return Promises no longer throw TypeError exceptions.
    
    BUG=362992,245025
    
    Review URL: https://codereview.chromium.org/240063002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171789 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/crypto/resources/common.js
+++ third_party/WebKit/LayoutTests/crypto/resources/common.js
@@ -1,4 +1,14 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function shouldBeTypeError(toEval)
+{
+    var value = eval(toEval);
+    if (value instanceof TypeError) {
+        testPassed(toEval + " is: " + value.toString());
+    } else {
+        testFailed(toEval + " is not a TypeError: " + value);
+    }
+}
+
 // Verifies that the given "bytes" holds the same value as "expectedHexString".
 // "bytes" can be anything recognized by "bytesToHexString()".
 function bytesShouldMatchHexString(testDescription, expectedHexString, bytes)
