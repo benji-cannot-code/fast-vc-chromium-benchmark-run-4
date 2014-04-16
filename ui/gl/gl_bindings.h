7221634618dcc73509e9df56db3966df94b27f12
reveman@chromium.org <reveman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-16 11:37:09 +0000

    gpu: Add CHROMIUM_sync_query extension.
    This adds a GL_COMMANDS_COMPLETED_CHROMIUM query target that provide
    a finer granularity of synchronizing GL command completion than offered
    by glFinish().
    
    BUG=269808,356871,273274
    TEST=gpu_unittests --gtest_filter=GLES2DecoderManualInitTest.BeginEndQueryEXTCommandsCompletedCHROMIUM
    
    Review URL: https://codereview.chromium.org/238933003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264173 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gl/gl_bindings.h
+++ ui/gl/gl_bindings.h
@@ -129,6 +129,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define GL_ASYNC_PIXEL_UNPACK_COMPLETED_CHROMIUM         0x84F5
 #define GL_ASYNC_PIXEL_PACK_COMPLETED_CHROMIUM           0x84F6
 
+// GL_CHROMIUM_sync_query
+#define GL_COMMANDS_COMPLETED_CHROMIUM                   0x84F7
+
 // GL_OES_texure_3D
 #define GL_SAMPLER_3D_OES                                0x8B5F
 
