33c1ca23ccb284d9815d24ebb9987626f37d3a2f
guohui@chromium.org <guohui@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-16 16:58:19 +0000

    Implement preview tutorials on Mac
    This CL also hooks the avatar menu tutorial with the user manager tutorial.
    
    See mock 3-5 at https://docs.google.com/a/google.com/presentation/d/1UMAexroivw01osOXpnEqbQcE3FMyDkc9eZoIszm4DP4/edit#slide=id.g12716c613_09
    
    snapshot at https://x20web.corp.google.com/users/gu/guohui/www/preview_mac.png
    https://x20web.corp.google.com/users/gu/guohui/www/preview_enabled_mac.png
    
    BUG=358265
    
    Review URL: https://codereview.chromium.org/235893013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264220 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/browser_window_controller.mm
+++ chrome/browser/ui/cocoa/browser_window_controller.mm
@@ -1543,7 +1543,7 @@ enum {
 }
 
 - (BOOL)shouldUseNewAvatarButton {
-  return switches::IsNewProfileManagement() &&
+  return switches::IsNewAvatarMenu() &&
       profiles::IsRegularOrGuestSession(browser_.get());
 }
 
--- chrome/browser/ui/cocoa/profiles/profile_chooser_controller_unittest.mm
+++ chrome/browser/ui/cocoa/profiles/profile_chooser_controller_unittest.mm
@@ -33,6 +33,9 @@ class ProfileChooserControllerTest : public CocoaProfileTest {
   }
 
   virtual void SetUp() OVERRIDE {
+    CommandLine::ForCurrentProcess()->AppendSwitch(
+        switches::kNewProfileManagement);
+
     CocoaProfileTest::SetUp();
     ASSERT_TRUE(browser()->profile());
 
