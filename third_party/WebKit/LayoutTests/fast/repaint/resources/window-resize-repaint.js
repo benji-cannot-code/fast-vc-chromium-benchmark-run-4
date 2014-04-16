41f7642dcf6e095e7c8c98113b0ea60e09a1dc25
wangxianzhu@chromium.org <wangxianzhu@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-16 18:24:03 +0000

    Add repaint tests on window resize
    Though before bug 258219 is fixed, all window resizes trigger full repaint,
    the tests cover cases needing proper repaint on window resizes. Some also
    cover failures caused by previous patches for bug 258219.
    
    Will update expectations after bug 258219 is fixed. Should carefully examine
    possible under-repaints.
    
    BUG=258219,362407
    
    Review URL: https://codereview.chromium.org/236073002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171792 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/repaint/resources/window-resize-repaint.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+var testSizes = [
+    { width: 200, height: 200 }, // initial size
+    { width: 200, height: 300 }, // height increase
+    { width: 300, height: 300 }, // width increase
+    { width: 300, height: 250 }, // height decrease
+    { width: 250, height: 250 }  // width decrease
+    // Tests can add more testSizes.
+];
+
+var sizeIndex = 0;
+
+function repaintTest() {
+    window.resizeTo(testSizes[sizeIndex].width, testSizes[sizeIndex].height);
+}
+
+if (window.internals) {
+    internals.settings.setOverlayScrollbarsEnabled(true);
+    internals.settings.setMockScrollbarsEnabled(true);
+}
+
+if (window.testRunner) {
+    testRunner.useUnfortunateSynchronousResizeMode();
+
+    window.onload = function() {
+        window.resizeTo(testSizes[0].width, testSizes[0].height);
+
+        for (sizeIndex = 1; sizeIndex < testSizes.length; ++sizeIndex)
+            runRepaintTest();
+    }
+}
