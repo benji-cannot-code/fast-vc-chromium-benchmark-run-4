94ed8c1937c52ad548ae8064aaedb9d45f7b70ee
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-16 17:35:35 +0000

    DevTools: the colors for 'margin/border/padding' were not retained on mouseout.
    BUG=364021
    
    Review URL: https://codereview.chromium.org/240333002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171786 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/MetricsSidebarPane.js
+++ third_party/WebKit/Source/devtools/front_end/MetricsSidebarPane.js
@@ -160,6 +160,11 @@ WebInspector.MetricsSidebarPane.prototype = {
         return { left: left, top: top, right: right, bottom: bottom };
     },
 
+    /**
+     * @param {boolean} showHighlight
+     * @param {string} mode
+     * @param {?Event} event
+     */
     _highlightDOMNode: function(showHighlight, mode, event)
     {
         event.consume();
@@ -334,7 +339,7 @@ WebInspector.MetricsSidebarPane.prototype = {
         }
 
         metricsElement.appendChild(previousBox);
-        metricsElement.addEventListener("mouseover", this._highlightDOMNode.bind(this, false, ""), false);
+        metricsElement.addEventListener("mouseover", this._highlightDOMNode.bind(this, false, "all"), false);
         this.bodyElement.removeChildren();
         this.bodyElement.appendChild(metricsElement);
     },
