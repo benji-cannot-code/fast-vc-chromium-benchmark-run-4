6202fa9bdddeb69dd845eff59f6961fc51df3ea7
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-16 02:55:14 +0000

    app_list: Improve app search.
    - Fix a bug that a common prefix in text tokens prevents matching
      in secondary tokens.
    - Add sub string match as a fallback;
    
    BUG=313413
    
    Review URL: https://codereview.chromium.org/236553002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264084 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/app_list/search/tokenized_string_char_iterator.cc
+++ chrome/browser/ui/app_list/search/tokenized_string_char_iterator.cc
@@ -11,6 +11,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace app_list {
 
+TokenizedStringCharIterator::State::State() : token_index(0u), char_index(0) {}
+
+TokenizedStringCharIterator::State::State(size_t token_index, int char_index)
+    : token_index(token_index),
+      char_index(char_index) {
+}
+
 TokenizedStringCharIterator::TokenizedStringCharIterator(
     const TokenizedString& tokenized)
     : tokens_(tokenized.tokens()),
@@ -56,6 +63,21 @@ size_t TokenizedStringCharIterator::GetCharSize() const {
 
 bool TokenizedStringCharIterator::IsFirstCharOfToken() const {
   return current_token_iter_ && current_token_iter_->char_pos() == 0;
+}
+
+TokenizedStringCharIterator::State
+TokenizedStringCharIterator::GetState() const {
+  return State(current_token_,
+               current_token_iter_ ? current_token_iter_->char_pos() : 0);
+}
+
+void TokenizedStringCharIterator::SetState(const State& state) {
+  current_token_ = state.token_index;
+  CreateTokenCharIterator();
+  if (current_token_iter_) {
+    while (current_token_iter_->char_pos() < state.char_index)
+      current_token_iter_->Advance();
+  }
 }
 
 void TokenizedStringCharIterator::CreateTokenCharIterator() {
