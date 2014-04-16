10fe8fe38ae58d8188f94027568b3d39f1eb26ee
jinho.bang@samsung.com <jinho.bang@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-16 04:01:49 +0000

    We can reuse the variable which is already declared.
    It is better to reuse the already declared variable than function call.
    
    Review URL: https://codereview.chromium.org/238903002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171730 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/canvas/CanvasRenderingContext2D.cpp
+++ third_party/WebKit/Source/core/html/canvas/CanvasRenderingContext2D.cpp
@@ -989,7 +989,7 @@ void CanvasRenderingContext2D::fillInternal(const Path& path, const String& wind
         return;
     }
     FloatRect clipBounds;
-    if (!drawingContext()->getTransformedClipBounds(&clipBounds)) {
+    if (!c->getTransformedClipBounds(&clipBounds)) {
         return;
     }
 
@@ -1271,7 +1271,7 @@ void CanvasRenderingContext2D::fillRect(float x, float y, float width, float hei
     if (!state().m_invertibleCTM)
         return;
     FloatRect clipBounds;
-    if (!drawingContext()->getTransformedClipBounds(&clipBounds))
+    if (!c->getTransformedClipBounds(&clipBounds))
         return;
 
     // from the HTML5 Canvas spec:
@@ -1608,9 +1608,10 @@ template<class T> void  CanvasRenderingContext2D::fullCanvasCompositedDrawImage(
 {
     ASSERT(isFullCanvasCompositeMode(op));
 
-    drawingContext()->beginLayer(1, op);
-    drawImageToContext(image, drawingContext(), dest, src, CompositeSourceOver);
-    drawingContext()->endLayer();
+    GraphicsContext* c = drawingContext();
+    c->beginLayer(1, op);
+    drawImageToContext(image, c, dest, src, CompositeSourceOver);
+    c->endLayer();
 }
 
 static void fillPrimitive(const FloatRect& rect, GraphicsContext* context)
