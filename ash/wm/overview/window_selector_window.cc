a5d8b1a61d9870edaa38aaa23483db6750a983cd
flackr@chromium.org <flackr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-16 18:41:44 +0000

    Display close buttons in correct position on correct root window.
    BUG=354951
    TEST=Enter overview with windows on multiple monitors. Close buttons should be displayed on both monitors.
    
    Review URL: https://codereview.chromium.org/240153004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264264 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/overview/window_selector_window.cc
+++ ash/wm/overview/window_selector_window.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/wm/overview/window_selector_window.h"
 
+#include "ash/screen_util.h"
 #include "ash/shell.h"
 #include "ash/shell_window_ids.h"
 #include "ash/wm/overview/scoped_transform_overview_window.h"
@@ -108,7 +109,7 @@ void WindowSelectorWindow::SetItemBounds(aura::Window* root_window,
   transform_window_.SetTransform(root_window,
       ScopedTransformOverviewWindow::GetTransformForRect(src_rect, bounds()),
       animate);
-  UpdateCloseButtonBounds();
+  UpdateCloseButtonBounds(root_window);
 }
 
 void WindowSelectorWindow::ButtonPressed(views::Button* sender,
@@ -117,9 +118,9 @@ void WindowSelectorWindow::ButtonPressed(views::Button* sender,
       transform_window_.window())->Close();
 }
 
-void WindowSelectorWindow::UpdateCloseButtonBounds() {
-  aura::Window* root_window = GetRootWindow();
-  gfx::Rect align_bounds(bounds());
+void WindowSelectorWindow::UpdateCloseButtonBounds(aura::Window* root_window) {
+  gfx::Rect align_bounds(
+      ScreenUtil::ConvertRectFromScreen(root_window, bounds()));
   gfx::Transform close_button_transform;
   close_button_transform.Translate(align_bounds.right(), align_bounds.y());
 
