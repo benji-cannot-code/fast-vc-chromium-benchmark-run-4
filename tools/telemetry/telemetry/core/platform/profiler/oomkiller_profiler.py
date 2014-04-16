5cce6cd4a0099f3b157bb7aa9a5d3b4438ffd68d
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-16 03:46:58 +0000

    [Telemetry] Make Telemetry binaries platform aware.
    BUG=355646
    
    Review URL: https://codereview.chromium.org/238513003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264100 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/platform/profiler/oomkiller_profiler.py
+++ tools/telemetry/telemetry/core/platform/profiler/oomkiller_profiler.py
@@ -5,9 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import os
 
-from telemetry.core import util
 from telemetry.core.backends.chrome import android_browser_finder
 from telemetry.core.platform import profiler
+from telemetry.util import support_binaries
 
 
 class UnableToFindApplicationException(Exception):
@@ -30,9 +30,8 @@ class OOMKillerProfiler(profiler.Profiler):
         browser_backend, platform_backend, output_path, state)
     if not 'mem_consumer_launched' in state:
       state['mem_consumer_launched'] = True
-      mem_consumer_path = util.FindSupportBinary(
-          os.path.join('apks', 'MemConsumer.apk'),
-          executable=False)
+      mem_consumer_path = support_binaries.FindPath(
+          os.path.join('apks', 'MemConsumer.apk'), 'android')
       assert mem_consumer_path, ('Could not find memconsumer app. Please build '
                                  'memconsumer target.')
       self._browser_backend.adb.Install(mem_consumer_path)
