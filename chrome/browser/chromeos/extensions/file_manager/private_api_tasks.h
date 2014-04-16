ac0cda0b05bdd5f5257ad769b9b1196cb50d6d3a
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-16 14:04:16 +0000

    Revert 264167 "Sniff MIME type for files which have unknown exte..."
    This revert is requested by fukino, who is the author of the patch.
    
    This patch breaks a browser test on Windows. 
    
    > Sniff MIME type for files which have unknown extensions.
    > 
    > What I want to do:
    > Open text files which have unknown extensions(e.g. access.log.1) using packaged apps which can handle 'text/plain'(e.g. Text, Caret,...).
    > 
    > Current situation:
    > By following reasons, text files with unknow extension can't be opened.
    > 1. FileManager guess the MIME type as empty, so 'text/plain'-supporting apps are not shown as handlers.
    > 2. PlatformAppLauncher guess the MIME type as 'application/octet-stream', and it launch apps with no data because those apps don't handle 'application/octet-stream'.
    > 
    > What I changed:
    > Modified FileManager and PlatformAppLauncher to sniff MIME types if they are unknown based on extensions.
    > 
    > BUG=352250
    > R=benwells@chromium.org, hashimoto@chromium.org, jorgelo@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/224883008
    > 
    > Patch from Naoki Fukino <fukino@chromium.org>.
    
    TBR=yoshiki@chromium.org
    
    Review URL: https://codereview.chromium.org/240313002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264192 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/extensions/file_manager/private_api_tasks.h
+++ chrome/browser/chromeos/extensions/file_manager/private_api_tasks.h
@@ -8,10 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CHROMEOS_EXTENSIONS_FILE_MANAGER_PRIVATE_API_TASKS_H_
 #define CHROME_BROWSER_CHROMEOS_EXTENSIONS_FILE_MANAGER_PRIVATE_API_TASKS_H_
 
-#include <vector>
-
 #include "chrome/browser/chromeos/extensions/file_manager/private_api_base.h"
-#include "chrome/browser/chromeos/file_manager/file_tasks.h"
 #include "chrome/common/extensions/api/file_browser_private.h"
 
 namespace extensions {
@@ -46,11 +43,6 @@ class FileBrowserPrivateGetFileTasksFunction
 
   // AsyncExtensionFunction overrides.
   virtual bool RunImpl() OVERRIDE;
-
- private:
-  void OnSniffingMimeTypeCompleted(
-      scoped_ptr<app_file_handler_util::PathAndMimeTypeSet> path_mime_set,
-      scoped_ptr<std::vector<GURL> > file_urls);
 };
 
 // Implements the chrome.fileBrowserPrivate.setDefaultTask method.
