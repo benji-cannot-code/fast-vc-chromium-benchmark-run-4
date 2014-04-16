f0bab39b5a59e35bc3db2696b8ea664c2ef18179
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-16 22:15:49 +0000

    kiosk: Fix keyboard flow on network config UI.
    - Shift stripping in KeyboardDrivenEventRewriter should happen before
      session start to cover network config UI which is after login but
      before session start;
    - Fix html and css so that the blue halo shows up;
    
    BUG=364123
    
    Review URL: https://codereview.chromium.org/236303019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264325 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/login/display_manager.js
+++ chrome/browser/resources/login/display_manager.js
@@ -201,7 +201,7 @@ cr.define('cr.ui.login', function() {
       this.forceKeyboardFlow_ = value;
       if (value) {
         keyboard.initializeKeyboardFlow();
-        cr.ui.Dropdown.enableKeyboardFlow();
+        cr.ui.DropDown.enableKeyboardFlow();
       }
     },
 
