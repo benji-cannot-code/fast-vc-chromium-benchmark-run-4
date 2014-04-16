c2a8247e1edce933cc912d341b3d02d6dec07e2c
perkj@chromium.org <perkj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-16 19:19:14 +0000

    Revert "Revert of Only create webrtc MediaStreams when added to a PeerConnection https://codereview.chromium.org/227163007/
    This reverts commit caa70d180ff72cbf5f5ffd542b97de86794d5e00.
    
    The reason for the original revert is fixed in https://codereview.chromium.org/237433003/.
    
    Original CL desription:
    This cl adds a new class WebRtcMediaStreamAdapter. The responsibility of the class is to create and own a representation of a webrtc MediaStream that can be added and removed from a PeerConnection.
    An instance of WebRtcMediaStreamAdapter is created when a MediaStream is added to RTCPeerConnection object.
    
    The purpose is to clean up the the webrtc specific use of MediaStreams.
    
    TBR= ronghuawu@chromium.org
    BUG=323223
    
    Review URL: https://codereview.chromium.org/235503011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264277 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/media_stream_impl.cc
+++ content/renderer/media/media_stream_impl.cc
@@ -342,7 +342,6 @@ void MediaStreamImpl::OnStreamGenerated(
                          video_track_vector);
   web_stream->setExtraData(
       new MediaStream(
-          dependency_factory_,
           *web_stream));
 
   // Wait for the tracks to be started successfully or to fail.
