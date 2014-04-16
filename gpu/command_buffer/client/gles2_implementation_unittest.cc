9347609e466c29f94e4c93661880dee2ca070d45
puttaraju.r@samsung.com <puttaraju.r@samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-16 01:45:32 +0000

    Reduce the number of flushes while using glQueries.
    BUG=none
    
    Review URL: https://codereview.chromium.org/228653004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264057 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/client/gles2_implementation_unittest.cc
+++ gpu/command_buffer/client/gles2_implementation_unittest.cc
@@ -2997,7 +2997,6 @@ TEST_F(GLES2ImplementationTest, BeginEndQueryEXT) {
   // Test GetQueryObjectuivEXT CheckResultsAvailable
   ClearCommands();
   gl_->GetQueryObjectuivEXT(id1, GL_QUERY_RESULT_AVAILABLE_EXT, &available);
-  EXPECT_TRUE(NoCommandsWritten());
   EXPECT_EQ(0u, available);
 }
 
