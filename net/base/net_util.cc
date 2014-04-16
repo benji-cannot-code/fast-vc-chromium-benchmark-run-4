f8cf1850c0f6f328c8fabd01f076542ffbaeceed
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-16 06:39:56 +0000

    net: Fix compilation errors in net_util.cc
    ../../net/base/net_util.cc:615:16: error: use of undeclared identifier
    'gethostname'; did you mean 'gethostbyname2'?
      int result = gethostname(buffer, sizeof(buffer));
                     ^~~~~~~~~~~
                                gethostbyname2
    /usr/include/netdb.h:156:24: note: 'gethostbyname2' declared here
    extern struct hostent *gethostbyname2 (__const char *__name, int __af);
    ^
    
    I think this might have started after
    https://codereview.chromium.org/220253002/ - which touched net_util.cc
    recently.
    
    BUG=None
    TEST=projects links again
    R=eroman@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/236303014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264140 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/net_util.cc
+++ net/base/net_util.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <fcntl.h>
 #include <netdb.h>
 #include <netinet/in.h>
+#include <unistd.h>
 #if !defined(OS_NACL)
 #include <net/if.h>
 #if !defined(OS_ANDROID)
