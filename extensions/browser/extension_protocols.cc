5a19bc7b08cf822d9b19ed80b6d3549b54d6aa0c
asargent@chromium.org <asargent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-16 22:14:47 +0000

    Break a single histogram into two histograms
    This histogram records a networking result that if positive indicates a
    byte count, and if negative indicates an error code. Since histograms
    can't handle negative values, this splits that up into two: one for the
    byte count, and one that will use an existing enum for the network
    error code.
    
    This was based off of a suggestion in codereview.chromium.org/236023005
    
    BUG=360909
    
    Review URL: https://codereview.chromium.org/235893017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264323 0039d316-1c4b-4281-b951-d872f2087c98

--- extensions/browser/extension_protocols.cc
+++ extensions/browser/extension_protocols.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_loop.h"
 #include "base/metrics/field_trial.h"
 #include "base/metrics/histogram.h"
+#include "base/metrics/sparse_histogram.h"
 #include "base/path_service.h"
 #include "base/sha1.h"
 #include "base/strings/string_number_conversions.h"
@@ -220,7 +221,11 @@ class URLRequestExtensionJob : public net::URLRequestFileJob {
   }
 
   virtual void OnReadComplete(net::IOBuffer* buffer, int result) OVERRIDE {
-    UMA_HISTOGRAM_COUNTS("ExtensionUrlRequest.OnReadCompleteResult", result);
+    if (result >= 0)
+      UMA_HISTOGRAM_COUNTS("ExtensionUrlRequest.OnReadCompleteResult", result);
+    else
+      UMA_HISTOGRAM_SPARSE_SLOWLY("ExtensionUrlRequest.OnReadCompleteError",
+                                  -result);
     if (result > 0) {
       bytes_read_ += result;
       if (hash_.get()) {
