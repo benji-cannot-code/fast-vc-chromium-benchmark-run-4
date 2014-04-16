d1c42391a7e54a0c73333a682293ea0bccb15786
scherkus@chromium.org <scherkus@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-16 20:02:59 +0000

    Simplify compositing layout tests that make use of media elements.
    There's no need to do a seek-play-pause dance to make sure video has
    been displayed.
    
    This also fixes flakiness in
    compositing/visibility/visibility-simple-video-layer.html caused by the
    three videos racing to complete its respective seek-play-pause dance.
    
    Review URL: https://codereview.chromium.org/238063008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171800 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/compositing/resources/media-testing.js
+++ third_party/WebKit/LayoutTests/compositing/resources/media-testing.js
@@ -3,30 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 if (window.testRunner)
     testRunner.waitUntilDone();
 
-function ensureFrameVisible(video, videoRenderedCallback)
-{
-    var playingHandler = function() {
-        video.pause();
-        video.currentTime = 0;
-    };
-
-    var seekedHandler = function() {
-        if (videoRenderedCallback)
-            videoRenderedCallback();
-    };
-
-    video.addEventListener('playing', playingHandler, false);
-    video.addEventListener('seeked', seekedHandler, false);
-    video.play();
-}
-
-function setupVideo(videoElement, videoPath, canPlayThroughCallback, videoRenderedCallback)
+function setupVideo(videoElement, videoPath, canPlayThroughCallback)
 {
     var mediaFile = findMediaFile("video", videoPath);
-    videoElement.addEventListener("canplaythrough", function () {
-        if (canPlayThroughCallback)
-            canPlayThroughCallback();
-        ensureFrameVisible(this, videoRenderedCallback);
-    }, false);
+    videoElement.addEventListener("canplaythrough", canPlayThroughCallback);
     videoElement.src = mediaFile;
 }
