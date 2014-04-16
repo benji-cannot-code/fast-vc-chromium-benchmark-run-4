4d9fd480e1db2b84d018f8efd944aad124551554
fs@opera.com <fs@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-16 18:02:28 +0000

    Use StrictTypeChecking for ImageBitmap
    Add [StrictTypeChecking] to the 'ImageBitmapFactories' interface and
    the 'Window' partial interface. This doesn't change behavior in practice,
    but signals that it has been considered.
    Drop the unnecessary null-checks in the implementation.
    
    Review URL: https://codereview.chromium.org/240103002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171790 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/imagebitmap/ImageBitmapFactories.cpp
+++ third_party/WebKit/Source/modules/imagebitmap/ImageBitmapFactories.cpp
@@ -89,10 +89,6 @@ ScriptPromise ImageBitmapFactories::createImageBitmap(EventTarget& eventTarget, 
     // This variant does not work in worker threads.
     ASSERT(eventTarget.toDOMWindow());
 
-    if (!image) {
-        exceptionState.throwTypeError("The image element provided is invalid.");
-        return ScriptPromise();
-    }
     if (!image->cachedImage()) {
         exceptionState.throwDOMException(InvalidStateError, "No image can be retrieved from the provided element.");
         return ScriptPromise();
@@ -128,10 +124,6 @@ ScriptPromise ImageBitmapFactories::createImageBitmap(EventTarget& eventTarget, 
     // This variant does not work in worker threads.
     ASSERT(eventTarget.toDOMWindow());
 
-    if (!video) {
-        exceptionState.throwTypeError("The video element provided is invalid.");
-        return ScriptPromise();
-    }
     if (!video->player()) {
         exceptionState.throwDOMException(InvalidStateError, "No player can be retrieved from the provided video element.");
         return ScriptPromise();
@@ -180,10 +172,6 @@ ScriptPromise ImageBitmapFactories::createImageBitmap(EventTarget& eventTarget, 
     // This variant does not work in worker threads.
     ASSERT(eventTarget.toDOMWindow());
 
-    if (!canvas) {
-        exceptionState.throwTypeError("The canvas element provided is invalid.");
-        return ScriptPromise();
-    }
     if (!canvas->originClean()) {
         exceptionState.throwSecurityError("The canvas element provided is tainted with cross-origin data.");
         return ScriptPromise();
@@ -199,10 +187,6 @@ ScriptPromise ImageBitmapFactories::createImageBitmap(EventTarget& eventTarget, 
 
 ScriptPromise ImageBitmapFactories::createImageBitmap(EventTarget& eventTarget, Blob* blob, ExceptionState& exceptionState)
 {
-    if (!blob) {
-        exceptionState.throwTypeError("The blob provided is invalid.");
-        return ScriptPromise();
-    }
     RefPtr<ImageBitmapLoader> loader = ImageBitmapFactories::ImageBitmapLoader::create(from(eventTarget), eventTarget.executionContext(), IntRect());
     ScriptPromise promise = loader->promise();
     from(eventTarget).addLoader(loader);
@@ -212,10 +196,6 @@ ScriptPromise ImageBitmapFactories::createImageBitmap(EventTarget& eventTarget, 
 
 ScriptPromise ImageBitmapFactories::createImageBitmap(EventTarget& eventTarget, Blob* blob, int sx, int sy, int sw, int sh, ExceptionState& exceptionState)
 {
-    if (!blob) {
-        exceptionState.throwTypeError("The blob provided is invalid.");
-        return ScriptPromise();
-    }
     if (!sw || !sh) {
         exceptionState.throwDOMException(IndexSizeError, String::format("The source %s provided is 0.", sw ? "height" : "width"));
         return ScriptPromise();
@@ -234,10 +214,6 @@ ScriptPromise ImageBitmapFactories::createImageBitmap(EventTarget& eventTarget, 
 
 ScriptPromise ImageBitmapFactories::createImageBitmap(EventTarget& eventTarget, ImageData* data, int sx, int sy, int sw, int sh, ExceptionState& exceptionState)
 {
-    if (!data) {
-        exceptionState.throwTypeError("The ImageData provided is invalid.");
-        return ScriptPromise();
-    }
     if (!sw || !sh) {
         exceptionState.throwDOMException(IndexSizeError, String::format("The source %s provided is 0.", sw ? "height" : "width"));
         return ScriptPromise();
@@ -253,10 +229,6 @@ ScriptPromise ImageBitmapFactories::createImageBitmap(EventTarget& eventTarget, 
 
 ScriptPromise ImageBitmapFactories::createImageBitmap(EventTarget& eventTarget, ImageBitmap* bitmap, int sx, int sy, int sw, int sh, ExceptionState& exceptionState)
 {
-    if (!bitmap) {
-        exceptionState.throwTypeError("The ImageBitmap provided is invalid.");
-        return ScriptPromise();
-    }
     if (!sw || !sh) {
         exceptionState.throwDOMException(IndexSizeError, String::format("The source %s provided is 0.", sw ? "height" : "width"));
         return ScriptPromise();
