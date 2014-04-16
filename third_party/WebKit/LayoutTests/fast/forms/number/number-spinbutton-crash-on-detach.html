benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 15b1d1c43434d6719263cb415acd1ec9a3960b68

Do not dispatch 'change' events during pressing spin buttons for input[type=number].
InputType to triggers change event when not in focus and input event when in focus.
The change event is triggered only when user stops pressing spin button.

R=tkent, keishi1
BUG=155747
TEST=Change event is only trigger when mouse press is stopped. Only 1 change event is triggered when moving out of focus.

Review URL: https://codereview.chromium.org/196933006

git-svn-id: svn://svn.chromium.org/blink/trunk@171006 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/html/forms/BaseMultipleFieldsDateAndTimeInputType.cpp
@@ -241,6 +241,11 @@ void BaseMultipleFieldsDateAndTimeInputType::spinButtonStepUp()
         edit->stepUp();
 }
 
+void BaseMultipleFieldsDateAndTimeInputType::spinButtonDidReleaseMouseCapture()
+{
+    element().dispatchFormControlChangeEvent();
+}
+
 bool BaseMultipleFieldsDateAndTimeInputType::isPickerIndicatorOwnerDisabledOrReadOnly() const
 {
     return element().isDisabledOrReadOnly();

third_party/WebKit/Source/core/html/forms/BaseMultipleFieldsDateAndTimeInputType.h
@@ -75,6 +75,7 @@ private:
     virtual bool shouldSpinButtonRespondToWheelEvents() OVERRIDE;
     virtual void spinButtonStepDown() OVERRIDE;
     virtual void spinButtonStepUp() OVERRIDE;
+    virtual void spinButtonDidReleaseMouseCapture() OVERRIDE;
 
     // PickerIndicatorElement::PickerIndicatorOwner functions
     virtual bool isPickerIndicatorOwnerDisabledOrReadOnly() const OVERRIDE FINAL;

third_party/WebKit/Source/core/html/forms/TextFieldInputType.cpp
@@ -213,6 +213,7 @@ void TextFieldInputType::handleKeydownEventForSpinButton(KeyboardEvent* event)
         spinButtonStepDown();
     else
         return;
+    element().dispatchFormControlChangeEvent();
     event->setDefaultHandled();
 }
 
@@ -255,6 +256,8 @@ void TextFieldInputType::handleBlurEvent()
 {
     InputType::handleBlurEvent();
     element().endEditing();
+    if (SpinButtonElement *spinButton = spinButtonElement())
+        spinButton->releaseCapture();
 }
 
 bool TextFieldInputType::shouldSubmitImplicitly(Event* event)
@@ -557,6 +560,11 @@ bool TextFieldInputType::shouldSpinButtonRespondToMouseEvents()
 bool TextFieldInputType::shouldSpinButtonRespondToWheelEvents()
 {
     return shouldSpinButtonRespondToMouseEvents() && element().focused();
+}
+
+void TextFieldInputType::spinButtonDidReleaseMouseCapture()
+{
+    element().dispatchFormControlChangeEvent();
 }
 
 } // namespace WebCore

third_party/WebKit/Source/core/html/forms/TextFieldInputType.h
@@ -94,6 +94,7 @@ private:
     virtual bool shouldSpinButtonRespondToWheelEvents() OVERRIDE FINAL;
     virtual void spinButtonStepDown() OVERRIDE FINAL;
     virtual void spinButtonStepUp() OVERRIDE FINAL;
+    virtual void spinButtonDidReleaseMouseCapture() OVERRIDE FINAL;
 
     SpinButtonElement* spinButtonElement() const;
 };

third_party/WebKit/Source/core/html/shadow/SpinButtonElement.cpp
@@ -61,12 +61,6 @@ PassRefPtr<SpinButtonElement> SpinButtonElement::create(Document& document, Spin
     return element.release();
 }
 
-void SpinButtonElement::detach(const AttachContext& context)
-{
-    releaseCapture();
-    HTMLDivElement::detach(context);
-}
-
 void SpinButtonElement::defaultEventHandler(Event* event)
 {
     if (!event->isMouseEvent()) {
@@ -112,7 +106,7 @@ void SpinButtonElement::defaultEventHandler(Event* event)
             event->setDefaultHandled();
         }
     } else if (mouseEvent->type() == EventTypeNames::mouseup && mouseEvent->button() == LeftButton) {
-        stopRepeatingTimer();
+        releaseCapture();
     } else if (event->type() == EventTypeNames::mousemove) {
         if (box->pixelSnappedBorderBoxRect().contains(local)) {
             if (!m_capturing) {
@@ -199,6 +193,9 @@ void SpinButtonElement::releaseCapture()
                 page->chrome().unregisterPopupOpeningObserver(this);
         }
     }
+    if (m_spinButtonOwner)
+        m_spinButtonOwner->spinButtonDidReleaseMouseCapture();
+
 }
 
 bool SpinButtonElement::matchesReadOnlyPseudoClass() const

third_party/WebKit/Source/core/html/shadow/SpinButtonElement.h
@@ -48,6 +48,7 @@ public:
         virtual void focusAndSelectSpinButtonOwner() = 0;
         virtual bool shouldSpinButtonRespondToMouseEvents() = 0;
         virtual bool shouldSpinButtonRespondToWheelEvents() = 0;
+        virtual void spinButtonDidReleaseMouseCapture() = 0;
         virtual void spinButtonStepDown() = 0;
         virtual void spinButtonStepUp() = 0;
     };
@@ -70,7 +71,6 @@ public:
 private:
     SpinButtonElement(Document&, SpinButtonOwner&);
 
-    virtual void detach(const AttachContext& = AttachContext()) OVERRIDE;
     virtual bool isSpinButtonElement() const OVERRIDE { return true; }
     virtual bool isDisabledFormControl() const OVERRIDE { return shadowHost() && shadowHost()->isDisabledFormControl(); }
     virtual bool matchesReadOnlyPseudoClass() const OVERRIDE;

third_party/WebKit/LayoutTests/fast/forms/number/number-spinbutton-changeevent-trigger-expected.txt
@@ -1,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+Should not trigger any event
+PASS inputEventCounter is 0
+PASS changeEventCounter is 0
+Input event is triggered but no change event is triggered on mouse down
+PASS inputEventCounter is 1
+PASS changeEventCounter is 0
+PASS numberInput1.value is "1"
+Change events occurs on mouse up
+PASS inputEventCounter is 1
+PASS changeEventCounter is 1
+PASS numberInput1.value is "1"
+Pressed more than once, still events are triggered
+PASS inputEventCounter is 2
+PASS changeEventCounter is 2
+PASS numberInput1.value is "2"
+When moving away from field, it should not trigger event, only on value change
+PASS inputEventCounter is 2
+PASS changeEventCounter is 2
+PASS numberInput1.value is "2"
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

third_party/WebKit/LayoutTests/fast/forms/number/number-spinbutton-changeevent-trigger.html
@@ -1,0 +1,56 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<!DOCTYPE>
+<html>
+<body>
+<script src="../../../resources/js-test.js"></script>
+<script src="../resources/common.js"></script>
+<input type="number" id="num1" onchange="handleChangeEvent(this);" oninput="handleInputEvent(this);">
+<script>
+var changeEventCounter = 0;
+var inputEventCounter = 0;
+
+function handleChangeEvent(element) {
+    ++changeEventCounter;
+}
+
+function handleInputEvent(element) {
+    ++inputEventCounter;
+}
+
+var numberInput1= document.getElementById('num1');
+numberInput1.focus();
+debug('Should not trigger any event');
+hoverOverElement(numberInput1);
+shouldBe('inputEventCounter',  '0');
+shouldBe('changeEventCounter', '0');
+
+numberInput1.value = 0;
+eventSender.mouseMoveTo(numberInput1.offsetLeft + numberInput1.offsetWidth - 10, numberInput1.offsetTop + numberInput1.offsetHeight / 4);
+eventSender.mouseDown();
+debug('Input event is triggered but no change event is triggered on mouse down');
+shouldBe('inputEventCounter',  '1');
+shouldBe('changeEventCounter', '0');
+shouldBeEqualToString('numberInput1.value', '1');
+eventSender.mouseUp();
+debug('Change events occurs on mouse up');
+shouldBe('inputEventCounter',  '1');
+shouldBe('changeEventCounter', '1');
+shouldBeEqualToString('numberInput1.value', '1');
+
+debug('Pressed more than once, still events are triggered');
+eventSender.mouseMoveTo(numberInput1.offsetLeft + numberInput1.offsetWidth - 10, numberInput1.offsetTop + numberInput1.offsetHeight / 4);
+eventSender.mouseDown();
+eventSender.mouseUp();
+shouldBe('inputEventCounter',  '2');
+shouldBe('changeEventCounter', '2');
+shouldBeEqualToString('numberInput1.value', '2');
+
+debug('When moving away from field, it should not trigger event, only on value change');
+numberInput1.blur();
+shouldBe('inputEventCounter',  '2');
+shouldBe('changeEventCounter', '2');
+shouldBeEqualToString('numberInput1.value', '2');
+
+</script>
+</body>
+</html>

commit 62f43a1dc45d8469e740d67e1145fb9c9dc785f6

SpinButtonElement should release mouse capturing on detach.
This is a regression by r171006.
We have to release mouse capturing on detach() because a SpinButtonElement
captures mouse events even if the host element isn't focused.

The main change is to call releaseCapture() in SpinButtonElement::
detach(). Because releaseCapture() can dispatch 'change' events and
dispatching events in detach() isn't allowed, this CL adds a flag to
releaseCapture() and SpinButtonOwner::spinButtonDidReleaseMouseCapture().

BUG=362480

Review URL: https://codereview.chromium.org/238783002

git-svn-id: svn://svn.chromium.org/blink/trunk@171728 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/html/forms/BaseMultipleFieldsDateAndTimeInputType.cpp
@@ -242,9 +242,10 @@ void BaseMultipleFieldsDateAndTimeInputType::spinButtonStepUp()
         edit->stepUp();
 }
 
-void BaseMultipleFieldsDateAndTimeInputType::spinButtonDidReleaseMouseCapture()
+void BaseMultipleFieldsDateAndTimeInputType::spinButtonDidReleaseMouseCapture(SpinButtonElement::EventDispatch eventDispatch)
 {
-    element().dispatchFormControlChangeEvent();
+    if (eventDispatch == SpinButtonElement::EventDispatchAllowed)
+        element().dispatchFormControlChangeEvent();
 }
 
 bool BaseMultipleFieldsDateAndTimeInputType::isPickerIndicatorOwnerDisabledOrReadOnly() const

third_party/WebKit/Source/core/html/forms/BaseMultipleFieldsDateAndTimeInputType.h
@@ -76,7 +76,7 @@ private:
     virtual bool shouldSpinButtonRespondToWheelEvents() OVERRIDE;
     virtual void spinButtonStepDown() OVERRIDE;
     virtual void spinButtonStepUp() OVERRIDE;
-    virtual void spinButtonDidReleaseMouseCapture() OVERRIDE;
+    virtual void spinButtonDidReleaseMouseCapture(SpinButtonElement::EventDispatch) OVERRIDE;
 
     // PickerIndicatorElement::PickerIndicatorOwner functions
     virtual bool isPickerIndicatorOwnerDisabledOrReadOnly() const OVERRIDE FINAL;

third_party/WebKit/Source/core/html/forms/TextFieldInputType.cpp
@@ -566,9 +566,10 @@ bool TextFieldInputType::shouldSpinButtonRespondToWheelEvents()
     return shouldSpinButtonRespondToMouseEvents() && element().focused();
 }
 
-void TextFieldInputType::spinButtonDidReleaseMouseCapture()
+void TextFieldInputType::spinButtonDidReleaseMouseCapture(SpinButtonElement::EventDispatch eventDispatch)
 {
-    element().dispatchFormControlChangeEvent();
+    if (eventDispatch == SpinButtonElement::EventDispatchAllowed)
+        element().dispatchFormControlChangeEvent();
 }
 
 } // namespace WebCore

third_party/WebKit/Source/core/html/forms/TextFieldInputType.h
@@ -94,7 +94,7 @@ private:
     virtual bool shouldSpinButtonRespondToWheelEvents() OVERRIDE FINAL;
     virtual void spinButtonStepDown() OVERRIDE FINAL;
     virtual void spinButtonStepUp() OVERRIDE FINAL;
-    virtual void spinButtonDidReleaseMouseCapture() OVERRIDE FINAL;
+    virtual void spinButtonDidReleaseMouseCapture(SpinButtonElement::EventDispatch) OVERRIDE FINAL;
 
     SpinButtonElement* spinButtonElement() const;
 };

third_party/WebKit/Source/core/html/shadow/SpinButtonElement.cpp
@@ -61,6 +61,12 @@ PassRefPtr<SpinButtonElement> SpinButtonElement::create(Document& document, Spin
     return element.release();
 }
 
+void SpinButtonElement::detach(const AttachContext& context)
+{
+    releaseCapture(EventDispatchDisallowed);
+    HTMLDivElement::detach(context);
+}
+
 void SpinButtonElement::defaultEventHandler(Event* event)
 {
     if (!event->isMouseEvent()) {
@@ -182,7 +188,7 @@ void SpinButtonElement::doStepAction(int amount)
         m_spinButtonOwner->spinButtonStepDown();
 }
 
-void SpinButtonElement::releaseCapture()
+void SpinButtonElement::releaseCapture(EventDispatch eventDispatch)
 {
     stopRepeatingTimer();
     if (m_capturing) {
@@ -194,7 +200,7 @@ void SpinButtonElement::releaseCapture()
         }
     }
     if (m_spinButtonOwner)
-        m_spinButtonOwner->spinButtonDidReleaseMouseCapture();
+        m_spinButtonOwner->spinButtonDidReleaseMouseCapture(eventDispatch);
 
 }
 

third_party/WebKit/Source/core/html/shadow/SpinButtonElement.h
@@ -41,14 +41,17 @@ public:
         Down,
         Up,
     };
-
+    enum EventDispatch {
+        EventDispatchAllowed,
+        EventDispatchDisallowed,
+    };
     class SpinButtonOwner {
     public:
         virtual ~SpinButtonOwner() { }
         virtual void focusAndSelectSpinButtonOwner() = 0;
         virtual bool shouldSpinButtonRespondToMouseEvents() = 0;
         virtual bool shouldSpinButtonRespondToWheelEvents() = 0;
-        virtual void spinButtonDidReleaseMouseCapture() = 0;
+        virtual void spinButtonDidReleaseMouseCapture(EventDispatch) = 0;
         virtual void spinButtonStepDown() = 0;
         virtual void spinButtonStepUp() = 0;
     };
@@ -58,7 +61,7 @@ public:
     // implementation, e.g. during event handling.
     static PassRefPtr<SpinButtonElement> create(Document&, SpinButtonOwner&);
     UpDownState upDownState() const { return m_upDownState; }
-    void releaseCapture();
+    void releaseCapture(EventDispatch = EventDispatchAllowed);
     void removeSpinButtonOwner() { m_spinButtonOwner = 0; }
 
     void step(int amount);
@@ -71,6 +74,7 @@ public:
 private:
     SpinButtonElement(Document&, SpinButtonOwner&);
 
+    virtual void detach(const AttachContext&) OVERRIDE;
     virtual bool isSpinButtonElement() const OVERRIDE { return true; }
     virtual bool isDisabledFormControl() const OVERRIDE { return shadowHost() && shadowHost()->isDisabledFormControl(); }
     virtual bool matchesReadOnlyPseudoClass() const OVERRIDE;

third_party/WebKit/LayoutTests/fast/forms/number/number-spinbutton-crash-on-detach-expected.txt
@@ -1,0 +1,3 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+ALERT: alert
+PASS if not crashed.

third_party/WebKit/LayoutTests/fast/forms/number/number-spinbutton-crash-on-detach.html
@@ -1,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<!DOCTYPE html>
+<body>
+<style>
+body, iframe {
+    margin: 0; 
+    padding: 0;
+    border: 0;
+}
+</style>
+<iframe src="resources/number-spinbutton-crash-on-detach-frame.html"></iframe>
+<input id="num1" type="number" value="0">
+<script>
+if (window.testRunner) {
+    testRunner.dumpAsText();
+    testRunner.waitUntilDone();
+}
+window.onload = function() {
+    var iframe = document.querySelector('iframe');
+    iframe.src = '';
+    iframe.srcdoc = '<script>gc(); alert("alert"); parent.finish();</' + 'script>';
+};
+
+function finish() {
+    document.body.innerHTML = 'PASS if not crashed.';
+    testRunner.notifyDone();
+}
+</script>

third_party/WebKit/LayoutTests/fast/forms/number/resources/number-spinbutton-crash-on-detach-frame.html
@@ -1,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<input id="num1" type="number" value="0">
+<script>
+var num1 = document.getElementById('num1');
+var mouseX = num1.offsetLeft + num1.offsetWidth - 5;
+var mouseY = num1.offsetTop + 5;
+eventSender.mouseMoveTo(mouseX, mouseY);
+eventSender.mouseMoveTo(mouseX, mouseY - 1);
+</script>