094c5722db26f2616bfd5153a10670ad5a60dc15
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-16 23:04:47 +0000

    Docs: Remove unnecessary redirects from directories to 'index' files since we now do that automatically. Fix a bug preventing the root directory from redirecting to 'index'.
    R=jyasskin@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/240303006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264337 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/server2/content_provider_test.py
+++ chrome/common/extensions/docs/server2/content_provider_test.py
@@ -64,6 +64,7 @@ _TEST_DATA = {
   'dir.txt': 'dir.txt content',
   'dir5.html': 'dir5.html content',
   'img.png': 'img.png content',
+  'index.html': 'index.html content',
   'read.txt': 'read.txt content',
   'redirects.json': _REDIRECTS_JSON,
   'noextension': 'noextension content',
@@ -184,6 +185,7 @@ class ContentProviderUnittest(unittest.TestCase):
         self._content_provider.GetContentAndType('oops').Get)
 
   def testIndexRedirect(self):
+    self._assertTemplateContent(u'index.html content', '')
     self._assertTemplateContent(u'index.html content 1', 'dir4')
     self._assertTemplateContent(u'dir5.html content', 'dir5')
     self._assertMarkdownContent(
