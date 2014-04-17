1c7c5cbdbc0698ea39592397226340f21211bcd9
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-17 16:50:56 +0000

    Revert accidental dartium code push
    This reverts r264517 to r264538 from terry@google.com
    
    BUG=none
    TEST=none
    NOTRY=true
    NOTREECHECKS=true
    TBR=terry@google.com
    
    Review URL: https://codereview.chromium.org/239993009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264545 0039d316-1c4b-4281-b951-d872f2087c98

--- dartium_tools/export_overrides.py
+++
@@ -1,7 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env python
-#
-# Copyright 2012 Google Inc. All Rights Reserved.
-
-# TODO(vsm): Remove this file once we remove the reference from
-# dartium.deps/DEPS.
