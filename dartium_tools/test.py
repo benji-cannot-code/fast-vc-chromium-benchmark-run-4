df9efcb8321862e80489fb54e402da7d049d4b33
terry@google.com <terry@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-17 15:55:34 +0000

    Modify test/archive to assume ninja on win
    I believe the 'non-ninja' bot is failing because it's actually running ninja
    and placing everything under "out" instead of "build".  If this works, I'll
    strip out the ninja specific code in another CL.
    
    R=jacobr@google.com
    
    Review URL: https://codereview.chromium.org/202483005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264537 0039d316-1c4b-4281-b951-d872f2087c98

--- dartium_tools/test.py
+++ dartium_tools/test.py
@@ -168,7 +168,7 @@ def main():
         'dart',
       ),
       'linux': ('out', 'content_shell', 'chrome', 'dart'),
-      'win': ('build', 'content_shell.exe', 'chrome.exe', 'dart.exe'),
+      'win': ('out', 'content_shell.exe', 'chrome.exe', 'dart.exe'),
       'win-ninja': ('out', 'content_shell.exe', 'chrome.exe', 'dart.exe'),
   }[host_os]
 
