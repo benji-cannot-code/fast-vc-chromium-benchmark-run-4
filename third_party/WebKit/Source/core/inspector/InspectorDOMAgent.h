d56c1720b87258053af3562c65dc534ab608d2b5
ch.dumez@samsung.com <ch.dumez@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-17 14:04:59 +0000

    Remove some dead code from inspector/
    Remove some dead code from inspector/. Several functions but defined but not
    used.
    
    Review URL: https://codereview.chromium.org/240013007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171858 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorDOMAgent.h
+++ third_party/WebKit/Source/core/inspector/InspectorDOMAgent.h
@@ -176,7 +176,6 @@ public:
     void pseudoElementCreated(PseudoElement*);
     void pseudoElementDestroyed(PseudoElement*);
 
-    int pushNodeToFrontend(ErrorString*, int documentNodeId, Node*);
     Node* nodeForId(int nodeId);
     int boundNodeId(Node*);
     void setDOMListener(DOMListener*);
