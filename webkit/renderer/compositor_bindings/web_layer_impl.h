622eeaefd3c62ee3a04a28e547bacf99a0d1ae4f
aelias@chromium.org <aelias@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-17 12:26:14 +0000

    Delete unused methods after Blink roll.
    BUG=361923
    
    Review URL: https://codereview.chromium.org/239053006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264481 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/renderer/compositor_bindings/web_layer_impl.h
+++ webkit/renderer/compositor_bindings/web_layer_impl.h
@@ -26,9 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cc { class Layer; }
 
-// TODO(senorblanco):  Remove this once WebKit changes have landed.
-class SkImageFilter;
-
 namespace blink {
 class WebFilterOperations;
 class WebLayerClient;
