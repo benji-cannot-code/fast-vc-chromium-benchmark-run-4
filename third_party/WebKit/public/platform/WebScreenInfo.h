bfe4b6ad83e7512efb956a74c8d72cb24723e735
mlamouri@chromium.org <mlamouri@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-17 23:32:07 +0000

    Add Undefined value to WebScreenOrientationType.
    ScreenOrientation object will compute the orientation type based on the
    information it has if the orientation type is set to undefined in the
    WebScreenInfo instance.
    
    BUG=162827
    
    Review URL: https://codereview.chromium.org/241333003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171903 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebScreenInfo.h
+++ third_party/WebKit/public/platform/WebScreenInfo.h
@@ -83,6 +83,7 @@ struct WebScreenInfo {
         , depth(0)
         , depthPerComponent(0)
         , isMonochrome(false)
+        , orientationType(WebScreenOrientationUndefined)
         , orientationAngle(0) { }
 };
 
--- third_party/WebKit/public/platform/WebScreenOrientationType.h
+++ third_party/WebKit/public/platform/WebScreenOrientationType.h
@@ -9,7 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 enum WebScreenOrientationType {
-    WebScreenOrientationPortraitPrimary = 1,
+    WebScreenOrientationUndefined = 0,
+    WebScreenOrientationPortraitPrimary,
     WebScreenOrientationPortraitSecondary,
     WebScreenOrientationLandscapePrimary,
     WebScreenOrientationLandscapeSecondary
