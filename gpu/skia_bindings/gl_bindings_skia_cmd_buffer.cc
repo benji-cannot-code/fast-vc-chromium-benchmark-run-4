67eb6b70dfff6652bbc8c51d29713f90f850318b
bsalomon@google.com <bsalomon@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-17 00:01:26 +0000

    Wire GL_CHROMIUM_map_sub functions up to Skia.
    BUG=skia:2402
    
    Review URL: https://codereview.chromium.org/231993002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264358 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/skia_bindings/gl_bindings_skia_cmd_buffer.cc
+++ gpu/skia_bindings/gl_bindings_skia_cmd_buffer.cc
@@ -75,6 +75,8 @@ GrGLInterface* CreateCommandBufferSkiaGLBinding() {
   functions->fInsertEventMarker = glInsertEventMarkerEXT;
   functions->fLineWidth = glLineWidth;
   functions->fLinkProgram = glLinkProgram;
+  functions->fMapBufferSubData = glMapBufferSubDataCHROMIUM;
+  functions->fMapTexSubImage2D = glMapTexSubImage2DCHROMIUM;
   functions->fPixelStorei = glPixelStorei;
   functions->fPopGroupMarker = glPopGroupMarkerEXT;
   functions->fPushGroupMarker = glPushGroupMarkerEXT;
@@ -111,6 +113,8 @@ GrGLInterface* CreateCommandBufferSkiaGLBinding() {
   functions->fUniformMatrix2fv = glUniformMatrix2fv;
   functions->fUniformMatrix3fv = glUniformMatrix3fv;
   functions->fUniformMatrix4fv = glUniformMatrix4fv;
+  functions->fUnmapBufferSubData = glUnmapBufferSubDataCHROMIUM;
+  functions->fUnmapTexSubImage2D = glUnmapTexSubImage2DCHROMIUM;
   functions->fUseProgram = glUseProgram;
   functions->fVertexAttrib4fv = glVertexAttrib4fv;
   functions->fVertexAttribPointer = glVertexAttribPointer;
