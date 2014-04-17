157b84ae1f4f063f46debaea815bf758df2aa207
rouslan@chromium.org <rouslan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-17 05:31:27 +0000

    requestAutocomplete should not unlatinize administrative region names.
    requestAutocomplete canonicalizes administrative region names before
    sending an address to Wallet. For example, "California" becomes "CA".
    "California" is a "name" and "CA" is a "key". Similar logic cannot be
    used for switching from "lname" to "key", however. For example, "Tokyo"
    should not become "東京都", because we should avoid mixed-language
    addresses. This patch prevents unlatinizing of administrative region
    names when canonicalizing an address to be sent to Wallet.
    
    BUG=362691
    
    Review URL: https://codereview.chromium.org/237693002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264435 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/libaddressinput/chromium/cpp/src/address_validator.cc
+++ third_party/libaddressinput/chromium/cpp/src/address_validator.cc
@@ -221,7 +221,7 @@ class AddressValidatorImpl : public AddressValidator {
       const std::string& user_input = address.GetFieldValue(sub_field_type);
       if (!user_input.empty() &&
           FilterAllows(filter, sub_field_type, AddressProblem::UNKNOWN_VALUE) &&
-          !rule.CanonicalizeSubKey(user_input, &sub_key)) {
+          !rule.CanonicalizeSubKey(user_input, false, &sub_key)) {
         problems->push_back(AddressProblem(
             sub_field_type,
             AddressProblem::UNKNOWN_VALUE,
@@ -444,6 +444,7 @@ class AddressValidatorImpl : public AddressValidator {
         ruleset_it->second->GetLanguageCodeRule(address_data->language_code);
 
     return rule.CanonicalizeSubKey(address_data->administrative_area,
+                                   true,  // Keep input latin.
                                    &address_data->administrative_area);
   }
 
