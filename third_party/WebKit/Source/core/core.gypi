f52b10c802631b8cbddd01f1c2608de7eb5707f6
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-17 23:26:32 +0000

    Delete HistoryController and associated plumbing
    It is dead code as of http://src.chromium.org/viewvc/chrome?view=revision&revision=263767
    
    BUG=357327
    
    Review URL: https://codereview.chromium.org/239183004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171901 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/core.gypi
+++ third_party/WebKit/Source/core/core.gypi
@@ -1387,8 +1387,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'page/FocusController.cpp',
             'page/FocusType.h',
             'page/FrameTree.cpp',
-            'page/HistoryController.cpp',
-            'page/HistoryController.h',
             'page/InjectedStyleSheets.cpp',
             'page/InjectedStyleSheets.h',
             'page/MouseEventWithHitTestResults.cpp',
--- third_party/WebKit/Source/core/page/Page.h
+++ third_party/WebKit/Source/core/page/Page.h
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/ViewportDescription.h"
 #include "core/frame/SettingsDelegate.h"
 #include "core/frame/UseCounter.h"
-#include "core/page/HistoryController.h"
 #include "core/page/PageAnimator.h"
 #include "core/page/PageVisibilityState.h"
 #include "platform/LifecycleContext.h"
@@ -129,8 +128,6 @@ public:
     EditorClient& editorClient() const { return *m_editorClient; }
     SpellCheckerClient& spellCheckerClient() const { return *m_spellCheckerClient; }
     UndoStack& undoStack() const { return *m_undoStack; }
-
-    HistoryController& historyController() const { return *m_historyController; }
 
     void setMainFrame(PassRefPtr<LocalFrame>);
     LocalFrame* mainFrame() const { return m_mainFrame.get(); }
@@ -254,8 +251,6 @@ private:
     const OwnPtr<InspectorController> m_inspectorController;
     const OwnPtr<PointerLockController> m_pointerLockController;
     OwnPtr<ScrollingCoordinator> m_scrollingCoordinator;
-
-    const OwnPtr<HistoryController> m_historyController;
     const OwnPtr<UndoStack> m_undoStack;
 
     RefPtr<LocalFrame> m_mainFrame;
