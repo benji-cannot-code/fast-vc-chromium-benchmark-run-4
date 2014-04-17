e85964b9d794b7640ccb66981330bf8fac532fbf
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-17 17:15:43 +0000

    DevTools: Hide inspected element info tooltip on Ctrl/Cmd in Inspect mode
    R=pfeldman
    BUG=282493
    
    Review URL: https://codereview.chromium.org/231023003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171866 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorDOMAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorDOMAgent.cpp
@@ -1101,7 +1101,7 @@ bool InspectorDOMAgent::handleGestureEvent(LocalFrame* frame, const PlatformGest
         return false;
     Node* node = hoveredNodeForEvent(frame, event, false);
     if (node && m_inspectModeHighlightConfig) {
-        m_overlay->highlightNode(node, 0 /* eventTarget */, *m_inspectModeHighlightConfig);
+        m_overlay->highlightNode(node, 0 /* eventTarget */, *m_inspectModeHighlightConfig, false);
         inspect(node);
         return true;
     }
@@ -1114,7 +1114,7 @@ bool InspectorDOMAgent::handleTouchEvent(LocalFrame* frame, const PlatformTouchE
         return false;
     Node* node = hoveredNodeForEvent(frame, event, false);
     if (node && m_inspectModeHighlightConfig) {
-        m_overlay->highlightNode(node, 0 /* eventTarget */, *m_inspectModeHighlightConfig);
+        m_overlay->highlightNode(node, 0 /* eventTarget */, *m_inspectModeHighlightConfig, false);
         inspect(node);
         return true;
     }
@@ -1166,7 +1166,7 @@ void InspectorDOMAgent::handleMouseMove(LocalFrame* frame, const PlatformMouseEv
         eventTarget = 0;
 
     if (node && m_inspectModeHighlightConfig)
-        m_overlay->highlightNode(node, eventTarget, *m_inspectModeHighlightConfig);
+        m_overlay->highlightNode(node, eventTarget, *m_inspectModeHighlightConfig, event.ctrlKey() || event.metaKey());
 }
 
 void InspectorDOMAgent::setSearchingForNode(ErrorString* errorString, SearchMode searchMode, JSONObject* highlightInspectorObject)
@@ -1259,7 +1259,7 @@ void InspectorDOMAgent::highlightNode(ErrorString* errorString, const RefPtr<JSO
     if (!highlightConfig)
         return;
 
-    m_overlay->highlightNode(node, 0 /* eventTarget */, *highlightConfig);
+    m_overlay->highlightNode(node, 0 /* eventTarget */, *highlightConfig, false);
 }
 
 void InspectorDOMAgent::highlightFrame(
@@ -1274,7 +1274,7 @@ void InspectorDOMAgent::highlightFrame(
         highlightConfig->showInfo = true; // Always show tooltips for frames.
         highlightConfig->content = parseColor(color);
         highlightConfig->contentOutline = parseColor(outlineColor);
-        m_overlay->highlightNode(frame->ownerElement(), 0 /* eventTarget */, *highlightConfig);
+        m_overlay->highlightNode(frame->ownerElement(), 0 /* eventTarget */, *highlightConfig, false);
     }
 }
 
--- third_party/WebKit/Source/core/inspector/InspectorOverlay.h
+++ third_party/WebKit/Source/core/inspector/InspectorOverlay.h
@@ -131,7 +131,7 @@ public:
     void setInspectModeEnabled(bool);
 
     void hideHighlight();
-    void highlightNode(Node*, Node* eventTarget, const HighlightConfig&);
+    void highlightNode(Node*, Node* eventTarget, const HighlightConfig&, bool omitTooltip);
     void highlightQuad(PassOwnPtr<FloatQuad>, const HighlightConfig&);
     void showAndHideViewSize(bool showGrid);
 
@@ -178,6 +178,7 @@ private:
     IntSize m_size;
     bool m_drawViewSize;
     bool m_drawViewSizeWithGrid;
+    bool m_omitTooltip;
     Timer<InspectorOverlay> m_timer;
     int m_activeProfilerCount;
 };
