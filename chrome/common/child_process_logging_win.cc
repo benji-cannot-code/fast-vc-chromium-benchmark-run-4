5ec9adf98036f58b7ae53ec649bcdd9af02bd0b7
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-17 17:35:30 +0000

    [Win] Fix reporting of the crash client ID/guid for non-browser crash reports.
    BUG=364493
    
    Review URL: https://codereview.chromium.org/238253014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264565 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/child_process_logging_win.cc
+++ chrome/common/child_process_logging_win.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/crash_keys.h"
+#include "chrome/installer/util/google_update_settings.h"
 
 namespace child_process_logging {
 
@@ -64,6 +65,13 @@ void Init() {
   crash_keys::RegisterChromeCrashKeys();
   base::debug::SetCrashKeyReportingFunctions(
       &SetCrashKeyValueTrampoline, &ClearCrashKeyValueTrampoline);
+
+  // This would be handled by BreakpadClient::SetClientID(), but because of the
+  // aforementioned issue, crash keys aren't ready yet at the time of Breakpad
+  // initialization.
+  std::string client_id;
+  if (GoogleUpdateSettings::GetMetricsId(&client_id))
+    base::debug::SetCrashKeyValue(crash_keys::kClientID, client_id);
 }
 
 }  // namespace child_process_logging
