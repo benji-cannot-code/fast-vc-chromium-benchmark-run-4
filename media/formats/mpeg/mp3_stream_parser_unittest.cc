e4c78f91d46bd9e5e33a5c3ee4105fe3e06c0c8d
dalecurtis@google.com <dalecurtis@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-18 19:58:13 +0000

    Revert 264803 "Revert 264763 "Wire up codec_delay() to MP3Stream..."
    > Revert 264763 "Wire up codec_delay() to MP3StreamParser and FFmp..."
    > 
    > Possible candidate for broken media_unittests:
    > 
    > http://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Tests%20%28dbg%29%281%29/builds/30043/steps/media_unittests/logs/MediaSource_MP3_TimestampOffset_0
    > 
    > > Wire up codec_delay() to MP3StreamParser and FFmpegAudioDecoder.
    > > 
    > > Allows us to drop silent samples from the decoder after initialization
    > > and resets.
    > > 
    > > codec_delay() is always in frames, so I've changed all call sites to use
    > > frames instead of a TimeDelta.
    > > 
    > > Coincidentally also fixes an issue where not enough data was dropped from
    > > Vorbis buffers with negative timestamps.
    > > 
    > > BUG=360961
    > > TEST=new pipeline_integration_test
    > > 
    > > Review URL: https://codereview.chromium.org/239423005
    > 
    > TBR=dalecurtis@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/242203006
    
    TBR=rockot@chromium.org
    
    Review URL: https://codereview.chromium.org/243653002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264834 0039d316-1c4b-4281-b951-d872f2087c98

--- media/formats/mpeg/mp3_stream_parser_unittest.cc
+++ media/formats/mpeg/mp3_stream_parser_unittest.cc
@@ -41,6 +41,7 @@ TEST_F(MP3StreamParserTest, UnalignedAppend) {
       "{ 0K }"
       "EndOfSegment";
   EXPECT_EQ(expected, ParseFile("sfx.mp3", 17));
+  EXPECT_GT(last_audio_config().codec_delay(), 0);
 }
 
 // Test parsing with a larger piece size to verify that multiple buffers
@@ -56,6 +57,7 @@ TEST_F(MP3StreamParserTest, UnalignedAppend512) {
       "{ 0K }"
       "EndOfSegment";
   EXPECT_EQ(expected, ParseFile("sfx.mp3", 512));
+  EXPECT_GT(last_audio_config().codec_delay(), 0);
 }
 
 TEST_F(MP3StreamParserTest, MetadataParsing) {
