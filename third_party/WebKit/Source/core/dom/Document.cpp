8cde19d68631299a223592c4611dda097f3d8dfd
dmazzoni@chromium.org <dmazzoni@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-18 00:33:41 +0000

    Simplify assert using DocumentLifecycle
    BUG=358674
    
    Review URL: https://codereview.chromium.org/232543002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171910 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -1909,6 +1909,8 @@ void Document::updateLayout()
     // Only do a layout if changes have occurred that make it necessary.
     if (isActive() && frameView && renderView() && (frameView->layoutPending() || renderView()->needsLayout()))
         frameView->layout();
+    else if (lifecycle().state() < DocumentLifecycle::LayoutClean)
+        lifecycle().advanceTo(DocumentLifecycle::LayoutClean);
 }
 
 void Document::setNeedsFocusedElementCheck()
