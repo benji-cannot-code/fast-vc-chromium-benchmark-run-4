d96c9c5e275478a9cd1ae655147629b85f2e4b60
morrita@chromium.org <morrita@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-18 20:06:33 +0000

    REGRESSION(r157081): Fix multipart handling of ResourceFetcher
    r157081 broke multipart handling in ResourceFetcher.
    The breakage was harmless except Motion JPEG case - ResourceFetcher
    lost the reference to Motion JPEG resource so it was no longer
    able to cancel the ongoing resource fetching.
    
    This change makes the tracking working again.
    Also, the change adds a stopFetching() call to DocumentLoder::stopLoading()
    that is used to be there but was lost at some point.
    
    TEST=stop-loading.html
    R=japhet@chromium.org,abarth@chromium.org
    BUG=309641
    
    Review URL: https://codereview.chromium.org/242373002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171969 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/DocumentLoader.cpp
+++ third_party/WebKit/Source/core/loader/DocumentLoader.cpp
@@ -189,6 +189,8 @@ void DocumentLoader::stopLoading()
             m_frame->loader().stopLoading();
     }
 
+    m_fetcher->stopFetching();
+
     if (!loading)
         return;
 
@@ -204,8 +206,6 @@ void DocumentLoader::stopLoading()
         // (A back/forward navigation has no resource loaders because its resources are cached.)
         mainReceivedError(ResourceError::cancelledError(m_request.url()));
     }
-
-    m_fetcher->stopFetching();
 }
 
 void DocumentLoader::commitIfReady()
