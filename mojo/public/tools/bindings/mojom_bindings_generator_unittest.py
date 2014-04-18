857c2923230b488b55b11d39a71f158bcbe0bac6
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-18 04:51:51 +0000

    Mojo: Minor mojom bindings generator error message refactor.
    Slightly refactor the part of error message generation that prints the
    import stack. This is so I'll be able to unify our exceptions, so that
    error message printing can happen at the top level. (I'm not doing that
    in this CL.)
    
    R=darin@chromium.org
    
    Review URL: https://codereview.chromium.org/241763002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264726 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ mojo/public/tools/bindings/mojom_bindings_generator_unittest.py
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import unittest
+
+from mojom_bindings_generator import MakeImportStackMessage
+
+
+class MojoBindingsGeneratorTest(unittest.TestCase):
+  """Tests mojo_bindings_generator."""
+
+  def testMakeImportStackMessage(self):
+    """Tests MakeImportStackMessage()."""
+    self.assertEquals(MakeImportStackMessage(["x"]), "")
+    self.assertEquals(MakeImportStackMessage(["x", "y"]),
+        "\n  y was imported by x")
+    self.assertEquals(MakeImportStackMessage(["x", "y", "z"]),
+        "\n  z was imported by y\n  y was imported by x")
+
+
+if __name__ == "__main__":
+  unittest.main()
