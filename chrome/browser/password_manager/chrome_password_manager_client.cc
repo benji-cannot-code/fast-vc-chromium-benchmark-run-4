benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 86b361fe1a27be0eb4bba2380a2066b3e0612d1f

[Password Generation] Don't generate passwords for custom passphrase users.
BUG=358998

Review URL: https://codereview.chromium.org/217423018

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263156 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/password_manager/chrome_password_manager_client.cc
@@ -162,8 +162,13 @@ ChromePasswordManagerClient::GetProbabilityForExperiment(
 bool ChromePasswordManagerClient::IsPasswordSyncEnabled() {
   ProfileSyncService* sync_service =
       ProfileSyncServiceFactory::GetForProfile(GetProfile());
-  if (sync_service && sync_service->HasSyncSetupCompleted())
+  // Don't consider sync enabled if the user has a custom passphrase. See
+  // crbug.com/358998 for more details.
+  if (sync_service &&
+      sync_service->HasSyncSetupCompleted() &&
+      !sync_service->IsUsingSecondaryPassphrase()) {
     return sync_service->GetActiveDataTypes().Has(syncer::PASSWORDS);
+  }
   return false;
 }
 

commit 12c768aa2d5d28b49edb69dbb09e8ec189468033

[Password Generation] Fix crash due to checking uninitialized sync state
Introduced in r263156

BUG=362875,358998

Review URL: https://codereview.chromium.org/236303020

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264683 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/password_manager/chrome_password_manager_client.cc
@@ -162,8 +162,14 @@ ChromePasswordManagerClient::GetProbabilityForExperiment(
 bool ChromePasswordManagerClient::IsPasswordSyncEnabled() {
   ProfileSyncService* sync_service =
       ProfileSyncServiceFactory::GetForProfile(GetProfile());
-  if (sync_service && sync_service->HasSyncSetupCompleted())
+  // Don't consider sync enabled if the user has a custom passphrase. See
+  // crbug.com/358998 for more details.
+  if (sync_service &&
+      sync_service->HasSyncSetupCompleted() &&
+      sync_service->sync_initialized() &&
+      !sync_service->IsUsingSecondaryPassphrase()) {
     return sync_service->GetActiveDataTypes().Has(syncer::PASSWORDS);
+  }
   return false;
 }
 