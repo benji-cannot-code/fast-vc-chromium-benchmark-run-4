b2da3fc8655e58b1a64882e78550d3abc9b5d947
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-18 23:01:33 +0000

    Mojo: Add support for arrays of interface handles
    This also fixes a TODO for closing any handles held in an Array that has not been sent.
    
    BUG=363809
    
    Review URL: https://codereview.chromium.org/238043009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264881 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/public/cpp/bindings/lib/bindings_internal.h
+++ mojo/public/cpp/bindings/lib/bindings_internal.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/system/core.h"
 
 namespace mojo {
+template <typename S> class InterfaceHandle;
+
 namespace internal {
 template <typename T> class Array_Data;
 
@@ -83,54 +85,75 @@ inline typename T::Data* Unwrap(const T& object) {
 }
 
 template <typename T> struct TypeTraits {
+  static const bool kIsHandle = false;
   static const bool kIsObject = true;
 };
 template <> struct TypeTraits<bool> {
+  static const bool kIsHandle = false;
   static const bool kIsObject = false;
 };
 template <> struct TypeTraits<char> {
+  static const bool kIsHandle = false;
   static const bool kIsObject = false;
 };
 template <> struct TypeTraits<int8_t> {
+  static const bool kIsHandle = false;
   static const bool kIsObject = false;
 };
 template <> struct TypeTraits<int16_t> {
+  static const bool kIsHandle = false;
   static const bool kIsObject = false;
 };
 template <> struct TypeTraits<int32_t> {
+  static const bool kIsHandle = false;
   static const bool kIsObject = false;
 };
 template <> struct TypeTraits<int64_t> {
+  static const bool kIsHandle = false;
   static const bool kIsObject = false;
 };
 template <> struct TypeTraits<uint8_t> {
+  static const bool kIsHandle = false;
   static const bool kIsObject = false;
 };
 template <> struct TypeTraits<uint16_t> {
+  static const bool kIsHandle = false;
   static const bool kIsObject = false;
 };
 template <> struct TypeTraits<uint32_t> {
+  static const bool kIsHandle = false;
   static const bool kIsObject = false;
 };
 template <> struct TypeTraits<uint64_t> {
+  static const bool kIsHandle = false;
   static const bool kIsObject = false;
 };
 template <> struct TypeTraits<float> {
+  static const bool kIsHandle = false;
   static const bool kIsObject = false;
 };
 template <> struct TypeTraits<double> {
+  static const bool kIsHandle = false;
   static const bool kIsObject = false;
 };
 template <> struct TypeTraits<Handle> {
+  static const bool kIsHandle = true;
   static const bool kIsObject = false;
 };
 template <> struct TypeTraits<DataPipeConsumerHandle> {
+  static const bool kIsHandle = true;
   static const bool kIsObject = false;
 };
 template <> struct TypeTraits<DataPipeProducerHandle> {
+  static const bool kIsHandle = true;
   static const bool kIsObject = false;
 };
 template <> struct TypeTraits<MessagePipeHandle> {
+  static const bool kIsHandle = true;
+  static const bool kIsObject = false;
+};
+template <typename S> struct TypeTraits<InterfaceHandle<S> > {
+  static const bool kIsHandle = true;
   static const bool kIsObject = false;
 };
 
