a08da2eb4cbae286f964f050de4cd225dc41c3b0
hirono@chromium.org <hirono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-18 10:35:23 +0000

    Gallery.app: Remove onBeforeUnload handlers.
    Previously the handlers are used for the Gallery.app closing confirmation.
    But the dialog is broken and not shown on ToT. We just remove them.
    
    BUG=364386
    TEST=manually
    
    Review URL: https://codereview.chromium.org/242023004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264769 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/foreground/js/photo/gallery.js
+++ chrome/browser/resources/file_manager/foreground/js/photo/gallery.js
@@ -7,12 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /**
  * Called from the main frame when unloading.
- * @return {string?} User-visible message on null if it is OK to close.
- */
-function beforeunload() { return Gallery.instance.onBeforeUnload() }
-
-/**
- * Called from the main frame when unloading.
  * @param {boolean=} opt_exiting True if the app is exiting.
  */
 function unload(opt_exiting) { Gallery.instance.onUnload(opt_exiting) }
@@ -141,14 +135,6 @@ Gallery.prototype.onExternallyUnmounted_ = function(event) {
       event.volumeInfo) {
     this.onBack_();
   }
-};
-
-/**
- * Beforeunload handler.
- * @return {string?} User-visible message on null if it is OK to close.
- */
-Gallery.prototype.onBeforeUnload = function() {
-  return this.slideMode_.onBeforeUnload();
 };
 
 /**
--- chrome/browser/resources/file_manager/foreground/js/photo/gallery_scripts.js
+++ chrome/browser/resources/file_manager/foreground/js/photo/gallery_scripts.js
@@ -65,7 +65,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Exports
 window.ImageUtil = ImageUtil;
 window.Gallery = Gallery;
-window.beforeunload = beforeunload;
 window.unload = unload;
 
 })();
