f4d2b8f908f83ded889a144323cf6b084d4e4c1f
mtomasz@chromium.org <mtomasz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-18 11:45:43 +0000

    [fsp] Initial implementation of timeout logic for requests.
    This patch introduces a basic mechanism for timing out stale requests.
    If none of callbacks are called within a fixed amount of time, then the request
    is considered as stale, and aborted with an FILE_ERROR_ABORT error code.
    
    TEST=unit_test: FileSystemProviderRequestManagerTest.AbortOnTimeout
    BUG=248427
    
    Review URL: https://codereview.chromium.org/241673005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264773 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/file_system_provider/request_manager_unittest.cc
+++ chrome/browser/chromeos/file_system_provider/request_manager_unittest.cc
@@ -6,13 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/bind.h"
+#include "base/callback.h"
 #include "base/files/file.h"
 #include "base/files/file_path.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/scoped_vector.h"
 #include "base/memory/weak_ptr.h"
+#include "base/run_loop.h"
 #include "base/values.h"
 #include "chrome/browser/chromeos/file_system_provider/request_manager.h"
+#include "content/public/test/test_browser_thread_bundle.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace chromeos {
@@ -82,6 +85,7 @@ class FileSystemProviderRequestManagerTest : public testing::Test {
     request_manager_.reset(new RequestManager());
   }
 
+  content::TestBrowserThreadBundle thread_bundle_;
   scoped_ptr<RequestManager> request_manager_;
 };
 
@@ -302,6 +306,24 @@ TEST_F(FileSystemProviderRequestManagerTest, AbortOnDestroy) {
   EXPECT_EQ(base::File::FILE_ERROR_ABORT, event->error());
 
   EXPECT_EQ(0u, logger.success_events().size());
+}
+
+TEST_F(FileSystemProviderRequestManagerTest, AbortOnTimeout) {
+  EventLogger logger;
+  base::RunLoop run_loop;
+
+  request_manager_->SetTimeoutForTests(base::TimeDelta::FromSeconds(0));
+  int request_id = request_manager_->CreateRequest(
+      base::Bind(&EventLogger::OnSuccess, logger.GetWeakPtr()),
+      base::Bind(&EventLogger::OnError, logger.GetWeakPtr()));
+  EXPECT_LT(0, request_id);
+
+  // Wait until the request is timeouted.
+  run_loop.RunUntilIdle();
+
+  ASSERT_EQ(1u, logger.error_events().size());
+  EventLogger::ErrorEvent* event = logger.error_events()[0];
+  EXPECT_EQ(base::File::FILE_ERROR_ABORT, event->error());
 }
 
 }  // namespace file_system_provider
