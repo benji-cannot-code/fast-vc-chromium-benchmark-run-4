4f6bfb37028268dba88a90ec8498336a95a1b8b9
horo@chromium.org <horo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-18 13:03:22 +0000

    Fix bug in identification of ServiceWorker versions in chrome://serviceworker_internals.
    Because version ids are unique within a storage partition but not across them, verion_id by itself is not sufficient to identify a version.
    
    BUG=359517
    
    Review URL: https://codereview.chromium.org/242923002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264782 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/service_worker/service_worker_internals_ui.h
+++ content/browser/service_worker/service_worker_internals_ui.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <set>
 
+#include "base/containers/scoped_ptr_hash_map.h"
 #include "base/files/file_path.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
@@ -30,31 +31,17 @@ class ServiceWorkerVersion;
 
 class ServiceWorkerInternalsUI
     : public WebUIController,
-      public ServiceWorkerContextObserver,
       public base::SupportsWeakPtr<ServiceWorkerInternalsUI> {
  public:
   explicit ServiceWorkerInternalsUI(WebUI* web_ui);
 
-  // ServiceWorkerContextObserver overrides:
-  virtual void OnWorkerStarted(int64 version_id,
-                               int process_id,
-                               int thread_id) OVERRIDE;
-  virtual void OnWorkerStopped(int64 version_id,
-                               int process_id,
-                               int thread_id) OVERRIDE;
-  virtual void OnVersionStateChanged(int64 version_id) OVERRIDE;
-  virtual void OnErrorReported(int64 version_id,
-                               int process_id,
-                               int thread_id,
-                               const ErrorInfo& info) OVERRIDE;
-
  private:
   class OperationProxy;
+  class PartitionObserver;
 
   virtual ~ServiceWorkerInternalsUI();
   void AddContextFromStoragePartition(StoragePartition* partition);
 
-  void AddObserverToStoragePartition(StoragePartition* partition);
   void RemoveObserverFromStoragePartition(StoragePartition* partition);
 
   // Called from Javascript.
@@ -69,7 +56,9 @@ class ServiceWorkerInternalsUI
       base::FilePath* partition_path,
       GURL* scope,
       scoped_refptr<ServiceWorkerContextWrapper>* context) const;
-  std::set<StoragePartition*> registered_partitions_;
+
+  base::ScopedPtrHashMap<uintptr_t, PartitionObserver> observers_;
+  int next_partition_id_;
 };
 
 }  // namespace content
