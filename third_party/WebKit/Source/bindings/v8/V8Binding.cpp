69f5c5e5b81134b9a0850afbc09631fee21525c1
nbarth@chromium.org <nbarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-18 06:45:11 +0000

    Rename macros TONATIVE_BOOL* and TOSTRING_BOOL to *_DEFAULT
    These macros do not always take a boolean value;
    correction per comment by Philipp (pneubeck@):
    https://codereview.chromium.org/232563003/#msg30
    
    R=haraken
    
    Review URL: https://codereview.chromium.org/242223004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171925 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/V8Binding.cpp
+++ third_party/WebKit/Source/bindings/v8/V8Binding.cpp
@@ -207,7 +207,7 @@ static inline T toSmallerInt(v8::Handle<v8::Value> value, IntegerConversionConfi
     }
 
     // Can the value be converted to a number?
-    TONATIVE_BOOL_EXCEPTIONSTATE(v8::Local<v8::Number>, numberObject, value->ToNumber(), exceptionState, 0);
+    TONATIVE_DEFAULT_EXCEPTIONSTATE(v8::Local<v8::Number>, numberObject, value->ToNumber(), exceptionState, 0);
     if (numberObject.IsEmpty()) {
         exceptionState.throwTypeError("Not convertible to a number value (of type '" + String(typeName) + "'.");
         return 0;
@@ -244,7 +244,7 @@ static inline T toSmallerUInt(v8::Handle<v8::Value> value, IntegerConversionConf
     }
 
     // Can the value be converted to a number?
-    TONATIVE_BOOL_EXCEPTIONSTATE(v8::Local<v8::Number>, numberObject, value->ToNumber(), exceptionState, 0);
+    TONATIVE_DEFAULT_EXCEPTIONSTATE(v8::Local<v8::Number>, numberObject, value->ToNumber(), exceptionState, 0);
     if (numberObject.IsEmpty()) {
         exceptionState.throwTypeError("Not convertible to a number value (of type '" + String(typeName) + "'.");
         return 0;
@@ -316,7 +316,7 @@ int32_t toInt32(v8::Handle<v8::Value> value, IntegerConversionConfiguration conf
         return value->Int32Value();
 
     // Can the value be converted to a number?
-    TONATIVE_BOOL_EXCEPTIONSTATE(v8::Local<v8::Number>, numberObject, value->ToNumber(), exceptionState, 0);
+    TONATIVE_DEFAULT_EXCEPTIONSTATE(v8::Local<v8::Number>, numberObject, value->ToNumber(), exceptionState, 0);
     if (numberObject.IsEmpty()) {
         exceptionState.throwTypeError("Not convertible to a number value (of type 'long'.)");
         return 0;
@@ -333,7 +333,7 @@ int32_t toInt32(v8::Handle<v8::Value> value, IntegerConversionConfiguration conf
     if (configuration == Clamp)
         return clampTo<int32_t>(numberObject->Value());
 
-    TONATIVE_BOOL_EXCEPTIONSTATE(int32_t, result, numberObject->Int32Value(), exceptionState, 0);
+    TONATIVE_DEFAULT_EXCEPTIONSTATE(int32_t, result, numberObject->Int32Value(), exceptionState, 0);
     return result;
 }
 
@@ -362,7 +362,7 @@ uint32_t toUInt32(v8::Handle<v8::Value> value, IntegerConversionConfiguration co
     }
 
     // Can the value be converted to a number?
-    TONATIVE_BOOL_EXCEPTIONSTATE(v8::Local<v8::Number>, numberObject, value->ToNumber(), exceptionState, 0);
+    TONATIVE_DEFAULT_EXCEPTIONSTATE(v8::Local<v8::Number>, numberObject, value->ToNumber(), exceptionState, 0);
     if (numberObject.IsEmpty()) {
         exceptionState.throwTypeError("Not convertible to a number value (of type 'unsigned long'.)");
         return 0;
@@ -379,7 +379,7 @@ uint32_t toUInt32(v8::Handle<v8::Value> value, IntegerConversionConfiguration co
     if (configuration == Clamp)
         return clampTo<uint32_t>(numberObject->Value());
 
-    TONATIVE_BOOL(uint32_t, result, numberObject->Uint32Value(), 0);
+    TONATIVE_DEFAULT(uint32_t, result, numberObject->Uint32Value(), 0);
     return result;
 }
 
@@ -396,7 +396,7 @@ int64_t toInt64(v8::Handle<v8::Value> value, IntegerConversionConfiguration conf
         return value->Int32Value();
 
     // Can the value be converted to a number?
-    TONATIVE_BOOL_EXCEPTIONSTATE(v8::Local<v8::Number>, numberObject, value->ToNumber(), exceptionState, 0);
+    TONATIVE_DEFAULT_EXCEPTIONSTATE(v8::Local<v8::Number>, numberObject, value->ToNumber(), exceptionState, 0);
     if (numberObject.IsEmpty()) {
         exceptionState.throwTypeError("Not convertible to a number value (of type 'long long'.)");
         return 0;
@@ -442,7 +442,7 @@ uint64_t toUInt64(v8::Handle<v8::Value> value, IntegerConversionConfiguration co
     }
 
     // Can the value be converted to a number?
-    TONATIVE_BOOL_EXCEPTIONSTATE(v8::Local<v8::Number>, numberObject, value->ToNumber(), exceptionState, 0);
+    TONATIVE_DEFAULT_EXCEPTIONSTATE(v8::Local<v8::Number>, numberObject, value->ToNumber(), exceptionState, 0);
     if (numberObject.IsEmpty()) {
         exceptionState.throwTypeError("Not convertible to a number value (of type 'unsigned long long'.)");
         return 0;
@@ -471,7 +471,7 @@ uint64_t toUInt64(v8::Handle<v8::Value> value)
 
 float toFloat(v8::Handle<v8::Value> value, ExceptionState& exceptionState)
 {
-    TONATIVE_BOOL_EXCEPTIONSTATE(v8::Local<v8::Number>, numberObject, value->ToNumber(), exceptionState, 0);
+    TONATIVE_DEFAULT_EXCEPTIONSTATE(v8::Local<v8::Number>, numberObject, value->ToNumber(), exceptionState, 0);
     return numberObject->NumberValue();
 }
 
