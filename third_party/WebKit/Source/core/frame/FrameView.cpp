benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit f57824e20da8088e6ec3ed11f74a0ab42b328afb

[DevTools] Switch from blink-based to content-based touch emulation.
BUG=337142

Review URL: https://codereview.chromium.org/145003002

git-svn-id: svn://svn.chromium.org/blink/trunk@171859 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/frame/FrameView.cpp
@@ -1337,7 +1337,7 @@ IntPoint FrameView::lastKnownMousePosition() const
 bool FrameView::shouldSetCursor() const
 {
     Page* page = frame().page();
-    return page && page->visibilityState() != PageVisibilityStateHidden && page->focusController().isActive();
+    return page && page->visibilityState() != PageVisibilityStateHidden && page->focusController().isActive() && page->settings().deviceSupportsMouse();
 }
 
 void FrameView::scrollContentsIfNeededRecursive()
@@ -3193,6 +3193,8 @@ AXObjectCache* FrameView::axObjectCache() const
 
 void FrameView::setCursor(const Cursor& cursor)
 {
+    if (!shouldSetCursor())
+        return;
     Page* page = frame().page();
     if (!page)
         return;

third_party/WebKit/LayoutTests/fast/events/touch/emulate-touch-events-expected.txt
@@ -1,64 +1,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-This tests single touch event emulation using mouse events.
-
-On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
-
-
-PASS 'ontouchstart' in window is true
-PASS 'ontouchend' in document is true
-PASS lastEvent.type is "touchstart"
-PASS lastEvent.touches.length is 1
-PASS lastEvent.changedTouches.length is 1
-PASS lastEvent.targetTouches.length is 1
-PASS lastEvent.pageX is 0
-PASS lastEvent.pageY is 0
-PASS lastEvent.shiftKey is true
-PASS lastEvent.altKey is true
-PASS lastEvent.ctrlKey is false
-PASS lastEvent.metaKey is false
-PASS lastEvent.touches[0].target.id is "touchtarget"
-PASS lastEvent.touches[0].pageX is 10
-PASS lastEvent.touches[0].pageY is 10
-PASS lastEvent.touches[0].clientX is 10
-PASS lastEvent.touches[0].clientY is 10
-PASS lastEvent.touches[0].identifier is 0
-PASS lastEvent.changedTouches[0].pageX is 10
-PASS lastEvent.changedTouches[0].pageY is 10
-PASS lastEvent.changedTouches[0].clientX is 10
-PASS lastEvent.changedTouches[0].clientY is 10
-PASS lastEvent.changedTouches[0].identifier is 0
-PASS lastEvent.targetTouches[0].pageX is 10
-PASS lastEvent.targetTouches[0].pageY is 10
-PASS lastEvent.targetTouches[0].clientX is 10
-PASS lastEvent.targetTouches[0].clientY is 10
-PASS lastEvent.targetTouches[0].identifier is 0
-PASS lastEvent.type is "touchmove"
-PASS lastEvent.touches.length is 1
-PASS lastEvent.changedTouches.length is 1
-PASS lastEvent.targetTouches.length is 1
-PASS lastEvent.pageX is 0
-PASS lastEvent.pageY is 0
-PASS lastEvent.touches[0].pageX is 20
-PASS lastEvent.touches[0].pageY is 30
-PASS lastEvent.touches[0].clientX is 20
-PASS lastEvent.touches[0].clientY is 30
-PASS lastEvent.touches[0].identifier is 0
-PASS lastEvent.type is "touchend"
-PASS lastEvent.touches.length is 0
-PASS lastEvent.changedTouches.length is 1
-PASS lastEvent.targetTouches.length is 0
-PASS lastEvent.pageX is 0
-PASS lastEvent.pageY is 0
-PASS lastEvent.changedTouches[0].pageX is 20
-PASS lastEvent.changedTouches[0].pageY is 30
-PASS lastEvent.changedTouches[0].clientX is 20
-PASS lastEvent.changedTouches[0].clientY is 30
-PASS lastEvent.changedTouches[0].identifier is 0
-PASS lastEvent.shiftKey is false
-PASS lastEvent.altKey is true
-PASS lastEvent.ctrlKey is true
-PASS lastEvent.metaKey is false
-PASS successfullyParsed is true
-
-TEST COMPLETE
-

third_party/WebKit/LayoutTests/fast/events/touch/emulate-touch-events.html
@@ -1,113 +1,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-<!DOCTYPE html>
-<html>
-<body>
-<script src="../../../resources/js-test.js"></script>
-<div id="touchtarget" style="width: 100px; height: 100px; background-color: blue"></div>
-<p id="description"></p>
-<div id="console"></div>
-<script>
-var div = document.getElementById("touchtarget");
-var lastEvent = null;
-var touchEventsReceived = 0;
-var EXPECTED_TOUCH_EVENTS_TOTAL = 3;
-
-function touchEventCallback(event) {
-    if (window.eventSender) {
-        lastEvent = event;
-        verifyTouch(touchEventsReceived++);
-    } else
-        debug(event.type);
-
-    // TODO: Touch emulation shouldn't generate mouse events regardless of whether
-    // preventDefault is called.  http://crbug.com/278300.
-    event.preventDefault();
-
-    if (window.testRunner && touchEventsReceived == EXPECTED_TOUCH_EVENTS_TOTAL) {
-        window.internals.settings.setTouchEventEmulationEnabled(false);
-        finishJSTest();
-    }
-}
-
-function mouseMoveCallback(e) {
-    debug("FAIL: Unexpected mousemove event: " + e.clientX + ", " + e.clientY);
-}
-
-div.addEventListener("touchstart", touchEventCallback, false);
-div.addEventListener("touchmove", touchEventCallback, false);
-div.addEventListener("touchend", touchEventCallback, false);
-div.addEventListener("mousemove", mouseMoveCallback, false);
-
-function verifyTouchEvent(type, totalTouchCount, changedTouchCount, targetTouchCount)
-{
-    shouldBeEqualToString("lastEvent.type", type);
-    shouldBe("lastEvent.touches.length", totalTouchCount.toString());
-    shouldBe("lastEvent.changedTouches.length", changedTouchCount.toString());
-    shouldBe("lastEvent.targetTouches.length", targetTouchCount.toString());
-    shouldBe("lastEvent.pageX", "0");
-    shouldBe("lastEvent.pageY", "0");
-}
-
-function verifyTouchPoint(list, point, x, y, id)
-{
-    shouldBe("lastEvent." + list + "[" + point + "].pageX", x.toString());
-    shouldBe("lastEvent." + list + "[" + point + "].pageY", y.toString());
-    shouldBe("lastEvent." + list + "[" + point + "].clientX", x.toString());
-    shouldBe("lastEvent." + list + "[" + point + "].clientY", y.toString());
-    shouldBe("lastEvent." + list + "[" + point + "].identifier", id.toString());
-}
-
-function verifyTouch(which) {
-    switch (which) {
-    case 0:
-        verifyTouchEvent("touchstart", 1, 1, 1);
-        shouldBe("lastEvent.shiftKey", "true");
-        shouldBe("lastEvent.altKey", "true");
-        shouldBe("lastEvent.ctrlKey", "false");
-        shouldBe("lastEvent.metaKey", "false");
-        shouldBeEqualToString("lastEvent.touches[0].target.id", "touchtarget");
-        verifyTouchPoint("touches", 0, 10, 10, 0);
-        verifyTouchPoint("changedTouches", 0, 10, 10, 0);
-        verifyTouchPoint("targetTouches", 0, 10, 10, 0);
-        break;
-    case 1:
-        verifyTouchEvent("touchmove", 1, 1, 1);
-        verifyTouchPoint("touches", 0, 20, 30, 0);
-        break;
-    case 2:
-        verifyTouchEvent("touchend", 0, 1, 0);
-        verifyTouchPoint("changedTouches", 0, 20, 30, 0);
-        shouldBe("lastEvent.shiftKey", "false");
-        shouldBe("lastEvent.altKey", "true");
-        shouldBe("lastEvent.ctrlKey", "true");
-        shouldBe("lastEvent.metaKey", "false");
-        break;
-    default:
-        testFailed("Wrong number of touch events! (" + which + ")");
-    }
-}
-
-function mouseEventSequence()
-{
-    eventSender.mouseMoveTo(10, 10);
-    eventSender.mouseDown(0, ["shiftKey", "altKey"]);
-    eventSender.mouseMoveTo(20, 30);
-    eventSender.mouseUp(0, ["altKey", "ctrlKey"]);
-}
-
-if (window.eventSender && window.internals && window.internals.settings) {
-    description("This tests single touch event emulation using mouse events.");
-
-    window.eventSender.dragMode = false;
-    window.jsTestIsAsync = true;
-    window.internals.settings.setTouchEventEmulationEnabled(true);
-
-    shouldBe("'ontouchstart' in window", "true");
-    shouldBe("'ontouchend' in document", "true");
-
-    mouseEventSequence();
-} else
-    debug("This test requires DumpRenderTree. Tap on the blue rect to log.");
-</script>
-</body>
-</html>

third_party/WebKit/LayoutTests/fast/events/touch/emulated-touch-iframe-expected.txt
@@ -1,9 +1,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-
-Test touchend events are emulated in iframes correctly.
-
-PASS successfullyParsed is true
-
-TEST COMPLETE
-touchend (test will time out if broken)
-

third_party/WebKit/LayoutTests/fast/events/touch/emulated-touch-iframe.html
@@ -1,64 +1,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-<html>
-<script src="../../../resources/js-test.js"></script>
-<body>
-<iframe style="position:absolute; top:100px; left:100px;" src="./resources/emulated-touch-iframe2.html"></iframe>
-<p>Test touchend events are emulated in iframes correctly.</p>
-<div id="console"></div>
-<script type="text/javascript">
-
-var touchstartCount = 0;
-var touchmoveCount = 0;
-var EXPECTED_TOUCHSTART_COUNT = 1;
-var EXPECTED_TOUCHMOVE_COUNT = 1;
-var shouldBail;
-
-function testComplete()
-{
-    if (shouldBail)
-        return;
-
-    if (touchstartCount > EXPECTED_TOUCHSTART_COUNT) {
-        debug("touchstart fired for the top frame, too, but should only fire for the iframe!");
-        shouldBail = true;
-    }
-
-    if (!touchmoveCount)
-        debug("touchmove not fired for the iframe!");
-    else if (touchmoveCount > EXPECTED_TOUCHMOVE_COUNT)
-        debug("touchmove fired too many times!");
-
-    debug("touchend (test will time out if broken)");
-    window.internals.settings.setTouchEventEmulationEnabled(false);
-    window.testRunner.notifyDone();
-}
-
-function handleTouchmove()
-{
-    ++touchmoveCount;
-}
-
-function handleTouchstart()
-{
-    ++touchstartCount;
-}
-
-function runTest() {
-    if (window.eventSender && window.internals && window.internals.settings) {
-        window.eventSender.dragMode = false;
-        window.internals.settings.setTouchEventEmulationEnabled(true);
-
-        eventSender.mouseMoveTo(110, 110);
-        eventSender.mouseDown(0);
-        eventSender.mouseMoveTo(120, 130);
-        eventSender.mouseUp(0);
-    } else
-       debug('This test requires DRT.');
-}
-
-if (window.testRunner)
-    window.testRunner.waitUntilDone();
-
-</script>
-</body>
-</html>

third_party/WebKit/Source/core/frame/Settings.in
@@ -267,7 +267,6 @@ viewportMetaEnabled initial=false
 
 dnsPrefetchingEnabled initial=false, invalidate=DNSPrefetching
 
-touchEventEmulationEnabled initial=false
 
 # FIXME: This is a temporary flag and should be removed once
 # accelerated overflow scroll is ready (crbug.com/254111).

third_party/WebKit/Source/core/inspector/InspectorClient.h
@@ -58,6 +58,7 @@ public:
     virtual void stopGPUEventsRecording() { }
 
     virtual void overrideDeviceMetrics(int /*width*/, int /*height*/, float /*deviceScaleFactor*/, bool /*emulateViewport*/, bool /*fitWindow*/) { }
+    virtual void setTouchEventEmulationEnabled(bool) { }
 
     virtual bool overridesShowPaintRects() { return false; }
     virtual void setShowPaintRects(bool) { }

third_party/WebKit/Source/core/inspector/InspectorPageAgent.cpp
@@ -1161,8 +1161,7 @@ void InspectorPageAgent::updateViewMetrics(int width, int height, double deviceS
 void InspectorPageAgent::updateTouchEventEmulationInPage(bool enabled)
 {
     m_state->setBoolean(PageAgentState::touchEventEmulationEnabled, enabled);
-    if (mainFrame() && mainFrame()->settings())
-        mainFrame()->settings()->setTouchEventEmulationEnabled(enabled);
+    m_client->setTouchEventEmulationEnabled(enabled);
 }
 
 void InspectorPageAgent::setTouchEmulationEnabled(ErrorString*, bool enabled)

commit 05116c0c1d5d983836cf3403a5e478b00ef0442e

Fix cursor regression introduced in r171859.
We actually used to change cursor when page is inactive, even though
FrameView::shouldSetCursor returns false.
Changed FrameView::setCursor to only check for mouse support.

BUG=337142

Review URL: https://codereview.chromium.org/243073002

git-svn-id: svn://svn.chromium.org/blink/trunk@171950 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/frame/FrameView.cpp
@@ -3205,10 +3205,8 @@ AXObjectCache* FrameView::axObjectCache() const
 
 void FrameView::setCursor(const Cursor& cursor)
 {
-    if (!shouldSetCursor())
-        return;
     Page* page = frame().page();
-    if (!page)
+    if (!page || !page->settings().deviceSupportsMouse())
         return;
     page->chrome().setCursor(cursor);
 }