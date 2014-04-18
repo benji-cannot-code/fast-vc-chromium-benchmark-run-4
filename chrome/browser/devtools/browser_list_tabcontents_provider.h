0e89311d535bfc5d7433a20aa4f2bf096d7d61cd
rockot@google.com <rockot@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-18 15:04:59 +0000

    Revert 264779 "DevTools: Add browser test for port forwarding"
    This broke PortForwardingTest.LoadPageWithStyleAnsScript
    
    http://build.chromium.org/p/chromium.win/builders/Vista%20Tests%20%282%29/builds/42061/steps/browser_tests/logs/LoadPageWithStyleAnsScript
    http://build.chromium.org/p/chromium.mac/builders/Mac%2010.6%20Tests%20%28dbg%29%281%29/builds/48774/steps/browser_tests/logs/LoadPageWithStyleAnsScript
    http://build.chromium.org/p/chromium.mac/builders/Mac%2010.6%20Tests%20%28dbg%29%281%29/builds/48775/steps/browser_tests/logs/LoadPageWithStyleAnsScript
    
    
    > DevTools: Add browser test for port forwarding
    > 
    > BUG=356617
    > TEST=out/Release/browser_tests --gtest_filter=PortForwardingTest*
    > 
    > Review URL: https://codereview.chromium.org/240283009
    
    TBR=kaznacheev@chromium.org
    
    Review URL: https://codereview.chromium.org/243203003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264789 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/devtools/browser_list_tabcontents_provider.h
+++ chrome/browser/devtools/browser_list_tabcontents_provider.h
@@ -17,8 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class BrowserListTabContentsProvider
     : public content::DevToolsHttpHandlerDelegate {
  public:
-  static void EnableTethering();
-
   explicit BrowserListTabContentsProvider(
       chrome::HostDesktopType host_desktop_type);
   virtual ~BrowserListTabContentsProvider();
@@ -37,7 +35,6 @@ class BrowserListTabContentsProvider
 
  private:
   chrome::HostDesktopType host_desktop_type_;
-  int last_tethering_port_;
   DISALLOW_COPY_AND_ASSIGN(BrowserListTabContentsProvider);
 };
 
--- chrome/browser/devtools/devtools_adb_bridge.cc
+++ chrome/browser/devtools/devtools_adb_bridge.cc
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/devtools/adb/android_rsa.h"
 #include "chrome/browser/devtools/adb_client_socket.h"
-#include "chrome/browser/devtools/browser_list_tabcontents_provider.h"
 #include "chrome/browser/devtools/devtools_protocol.h"
 #include "chrome/browser/devtools/devtools_target_impl.h"
 #include "chrome/browser/devtools/devtools_window.h"
@@ -39,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/devtools_external_agent_proxy_delegate.h"
 #include "content/public/browser/devtools_manager.h"
 #include "content/public/browser/user_metrics.h"
-#include "content/public/common/content_switches.h"
 #include "crypto/rsa_private_key.h"
 #include "net/base/escape.h"
 #include "net/base/net_errors.h"
@@ -1306,17 +1304,7 @@ void DevToolsAdbBridge::ReceivedDeviceCount(int count) {
 void DevToolsAdbBridge::CreateDeviceProviders() {
   device_providers_.clear();
 #if defined(DEBUG_DEVTOOLS)
-  if (CommandLine::ForCurrentProcess()->
-      HasSwitch(switches::kRemoteDebuggingPort)) {
-    std::string port_str = CommandLine::ForCurrentProcess()->
-        GetSwitchValueASCII(switches::kRemoteDebuggingPort);
-    int port;
-    if (base::StringToInt(port_str, &port)) {
-      BrowserListTabContentsProvider::EnableTethering();
-      device_providers_.push_back(
-          AndroidDeviceManager::GetSelfAsDeviceProvider(port));
-    }
-  }
+  device_providers_.push_back(AndroidDeviceManager::GetSelfAsDeviceProvider());
 #endif
   device_providers_.push_back(AndroidDeviceManager::GetAdbDeviceProvider());
 
