c9b377d03a3ac52c1ce706644e6a762d9cb4e3d5
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-18 04:59:30 +0000

    [MemSheriff] Exclude error hashes
    Sometimes, a large amount of new errors is detected. When the
    offending CL is rolled back, it takes 3 non-failed builds before
    this reports are excluded from waterfall.sh's "match" report.
    
    This changes allows specifying a JSON file with excluded error
    hashes, in the format
    { "hashes" : [
      "00106434C54F4C69",
      ... more hashes ...
      ]
    }
    
    BUG=none
    R=thestig@chromium.org
    
    Review URL: https://codereview.chromium.org/241293002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264729 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/valgrind/test_suppressions.py
+++ tools/valgrind/test_suppressions.py
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import argparse
 from collections import defaultdict
+import json
 import os
 import re
 import subprocess
@@ -88,6 +89,12 @@ def PrintTopSymbols(symbol_reports, top_count):
   for (symbol, suppressions) in sorted_reports[:top_count]:
     print "%4d occurrences : %s" % (len(suppressions), symboltable[symbol])
 
+def ReadHashExclusions(exclusions):
+  input_file = file(exclusions, 'r')
+  contents = json.load(input_file)
+  return contents['hashes']
+
+
 def main(argv):
   supp = suppressions.GetSuppressions()
 
@@ -106,14 +113,27 @@ def main(argv):
   parser.add_argument('--exclude-symbol', action='append',
     help='Filter out all suppressions containing the specified symbol(s). '
          'Matches against the mangled names.')
+  parser.add_argument('--exclude-hashes', action='append',
+    help='Specify a .json file with a list of hashes to exclude.')
 
   parser.add_argument('reports', metavar='report file', nargs='+',
     help='List of report files')
   args = parser.parse_args(argv)
 
+  # exclude_hashes is a list of strings, each string an error hash.
+  exclude_hashes = []
+
+  exclude_hashes = []
+  if args.exclude_hashes:
+    for excl in args.exclude_hashes:
+      print "reading exclusion", excl
+      exclude_hashes += ReadHashExclusions(excl)
+
   for f in args.reports:
     f_reports, url = ReadReportsFromFile(f)
     for (hash, report) in f_reports:
+      if hash in exclude_hashes:
+        continue
       all_reports[report] += [url]
       report_hashes[report] = hash
 
