416bc17a2682249bd19d1d18be248cbd800b4092
dalecurtis@chromium.org <dalecurtis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-18 10:24:58 +0000

    Wire up codec_delay() to MP3StreamParser and FFmpegAudioDecoder.
    Allows us to drop silent samples from the decoder after initialization
    and resets.
    
    codec_delay() is always in frames, so I've changed all call sites to use
    frames instead of a TimeDelta.
    
    Coincidentally also fixes an issue where not enough data was dropped from
    Vorbis buffers with negative timestamps.
    
    BUG=360961
    TEST=new pipeline_integration_test
    
    Review URL: https://codereview.chromium.org/239423005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264763 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/pipeline_integration_test.cc
+++ media/filters/pipeline_integration_test.cc
@@ -850,7 +850,7 @@ TEST_P(PipelineIntegrationTest, MediaSource_ADTS_TimestampOffset) {
 
 TEST_P(PipelineIntegrationTest, MediaSource_MP3) {
   MockMediaSource source("sfx.mp3", kMP3, kAppendWholeFile, GetParam());
-  StartPipelineWithMediaSource(&source);
+  StartHashedPipelineWithMediaSource(&source);
   source.EndOfStream();
 
   EXPECT_EQ(1u, pipeline_->GetBufferedTimeRanges().size());
@@ -860,6 +860,10 @@ TEST_P(PipelineIntegrationTest, MediaSource_MP3) {
   Play();
 
   EXPECT_TRUE(WaitUntilOnEnded());
+
+  // Verify that codec delay was stripped, if it wasn't the hash would be:
+  // "5.16,1.25,7.78,4.29,8.98,2.76,"
+  EXPECT_EQ("5.81,2.71,8.97,4.32,7.83,1.12,", GetAudioHash());
 }
 
 TEST_P(PipelineIntegrationTest, MediaSource_MP3_TimestampOffset) {
