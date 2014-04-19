f606ddaf604d141274be730901511b31f01828c0
rdevlin.cronin@chromium.org <rdevlin.cronin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-19 01:58:48 +0000

    Promote Apps Developer Tools in the chrome:extensions page
    If a user does not have the Apps Developer Tools installed, display a promotion
    for it under the Developer Tools controls (the promo only shows up in Dev Mode).
    
    The promo will go away (or not appear in the first place) if the user has ADT
    installed.
    
    BUG=312396
    
    Review URL: https://codereview.chromium.org/196413028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264906 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/extensions/extensions.js
+++ chrome/browser/resources/extensions/extensions.js
@@ -87,6 +87,13 @@ cr.define('extensions', function() {
     __proto__: HTMLDivElement.prototype,
 
     /**
+     * Whether the App Developer Tools promo has been dismissed on this page.
+     * @type {boolean}
+     * @private
+     */
+    promoDismissed_: false,
+
+    /**
      * Perform initial setup.
      */
     initialize: function() {
@@ -114,6 +121,13 @@ cr.define('extensions', function() {
           this.handlePackExtension_.bind(this));
       $('update-extensions-now').addEventListener('click',
           this.handleUpdateExtensionNow_.bind(this));
+
+      // Set up the close dialog for the apps developer tools promo.
+      $('apps-developer-tools-promo').querySelector('.close-button').
+          addEventListener('click', function(e) {
+        this.promoDismissed_ = true;
+        $('extension-settings').classList.remove('adt-promo');
+      }.bind(this));
 
       if (!loadTimeData.getBoolean('offStoreInstallEnabled')) {
         this.dragWrapper_ = new cr.ui.DragWrapper(document.documentElement,
@@ -297,6 +311,9 @@ cr.define('extensions', function() {
       pageDiv.classList.remove('dev-mode');
       $('toggle-dev-on').checked = false;
     }
+
+    var showPromo = extensionsData.promoteAppsDevTools && !this.promoDismissed_;
+    pageDiv.classList.toggle('adt-promo', showPromo);
 
     $('load-unpacked').disabled = extensionsData.loadUnpackedDisabled;
 
