8f2fcb4f6926db86df33225a50cf47de3012849f
mstensho@opera.com <mstensho@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-19 10:11:21 +0000

    [New Multicolumn] Make floats affect heights correctly.
    Don't let overhanging floats bleed into sibling column sets or into the parent block flow.
    
    Let RenderFlowThread establish a new block formatting context, so that its
    height encompasses all descendant floats.
    
    BUG=361551
    
    Review URL: https://codereview.chromium.org/242203003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171996 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderBlock.cpp
+++ third_party/WebKit/Source/core/rendering/RenderBlock.cpp
@@ -1444,7 +1444,7 @@ void RenderBlock::addVisualOverflowFromTheme()
 bool RenderBlock::createsBlockFormattingContext() const
 {
     return isInlineBlockOrInlineTable() || isFloatingOrOutOfFlowPositioned() || hasOverflowClip() || (parent() && parent()->isFlexibleBoxIncludingDeprecated())
-        || style()->specifiesColumns() || isTableCell() || isTableCaption() || isFieldset() || isWritingModeRoot() || isDocumentElement() || style()->columnSpan();
+        || style()->specifiesColumns() || isRenderFlowThread() || isTableCell() || isTableCaption() || isFieldset() || isWritingModeRoot() || isDocumentElement() || style()->columnSpan();
 }
 
 void RenderBlock::updateBlockChildDirtyBitsBeforeLayout(bool relayoutChildren, RenderBox* child)
