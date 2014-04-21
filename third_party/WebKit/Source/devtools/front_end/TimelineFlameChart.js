eed4651fcd293781cc2d26337d2c033f9e2887bb
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-21 19:03:30 +0000

    DevTools: mark only text-decorated timeline entries as such.
    Review URL: https://codereview.chromium.org/245183002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172061 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/TimelineFlameChart.js
+++ third_party/WebKit/Source/devtools/front_end/TimelineFlameChart.js
@@ -305,7 +305,6 @@ WebInspector.TimelineFlameChartDataProvider.prototype = {
         var record = this._records[entryIndex];
         var timelineData = this._timelineData;
 
-        var decorated = false;
         if (record.children.length) {
             var category = WebInspector.TimelineUIUtils.categoryForRecord(record);
             // Paint text using white color on dark background.
@@ -331,8 +330,6 @@ WebInspector.TimelineFlameChartDataProvider.prototype = {
                 context.fillText(text, barX + this.textPadding(), barY + barHeight - this.textBaseline());
                 context.restore();
             }
-
-            decorated = true;
         }
 
         if (record.warnings() || record.childHasWarnings()) {
@@ -349,10 +346,9 @@ WebInspector.TimelineFlameChartDataProvider.prototype = {
             context.fill();
 
             context.restore();
-            decorated = true;
         }
 
-        return decorated;
+        return record.children.length;
     },
 
     /**
