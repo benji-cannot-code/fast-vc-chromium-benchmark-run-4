1b359b178d4c0788e3ccbd17f6f5ab2d8a5048ec
vollick@chromium.org <vollick@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-21 16:34:17 +0000

    Enable universal accelerated overflow scroll
    Now that we've enabled layer squashing, we can enable universal overflow
    scrolling.
    
    Note: This is just an updated version of the patch abarth@ kindly
    started here: https://codereview.chromium.org/243713002/
    
    I've removed some now meaningless layout tests (which cover the old,
    opt-in path), and updated the TestExpectaions for the remaining,
    affected tests.
    
    R=abarth@chromium.org
    BUG=249354
    
    Review URL: https://codereview.chromium.org/244193002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172041 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/testing/Internals.h
+++ third_party/WebKit/Source/core/testing/Internals.h
@@ -226,7 +226,6 @@ public:
 
     bool scrollsWithRespectTo(Element*, Element*, ExceptionState&);
     bool isUnclippedDescendant(Element*, ExceptionState&);
-    bool needsCompositedScrolling(Element*, ExceptionState&);
 
     void setNeedsCompositedScrolling(Element*, unsigned value, ExceptionState&);
 
