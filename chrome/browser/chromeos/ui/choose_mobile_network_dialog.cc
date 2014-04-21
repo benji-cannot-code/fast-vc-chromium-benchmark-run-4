54264ee8ff1b8e34324cfa904c99206c876a44de
stevenjb@chromium.org <stevenjb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-21 19:55:50 +0000

    InternetOptionsHandler cleanup
    Some preliminary InternetOptionsHandler cleanup. No functional changes.
    
    BUG=279351
    For c/b/ui/ash/system_tray_delegate_chromeos.cc:
    TBR=derat@chromium.org
    
    Review URL: https://codereview.chromium.org/242763006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265050 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/ui/choose_mobile_network_dialog.cc
+++ chrome/browser/chromeos/ui/choose_mobile_network_dialog.cc
@@ -1,9 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/chromeos/choose_mobile_network_dialog.h"
+#include "chrome/browser/chromeos/ui/choose_mobile_network_dialog.h"
 
 #include "chrome/browser/chromeos/login/user_manager.h"
 #include "chrome/browser/profiles/profile_manager.h"
