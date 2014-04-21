e13c1d38121c55908f23c94ef23f59bec48b9fdf
peria@chromium.org <peria@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-21 10:43:43 +0000

    Remove all method-calls between SyncEngine and SyncWorker, and make them to post tasks instead.
    BUG=347425
    
    Review URL: https://codereview.chromium.org/236313009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264976 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_backend/sync_engine.h
+++ chrome/browser/sync_file_system/drive_backend/sync_engine.h
@@ -63,10 +63,9 @@ class SyncEngine : public RemoteFileSyncService,
 
   virtual ~SyncEngine();
 
-  void Initialize(
-      const base::FilePath& base_dir,
-      base::SequencedTaskRunner* task_runner,
-      leveldb::Env* env_override);
+  void Initialize(const base::FilePath& base_dir,
+                  base::SequencedTaskRunner* file_task_runner,
+                  leveldb::Env* env_override);
 
   // RemoteFileSyncService overrides.
   virtual void AddServiceObserver(SyncServiceObserver* observer) OVERRIDE;
@@ -148,6 +147,7 @@ class SyncEngine : public RemoteFileSyncService,
 
   SyncEngine(scoped_ptr<drive::DriveServiceInterface> drive_service,
              scoped_ptr<drive::DriveUploaderInterface> drive_uploader,
+             base::SequencedTaskRunner* worker_task_runner,
              drive::DriveNotificationManager* notification_manager,
              ExtensionServiceInterface* extension_service,
              SigninManagerBase* signin_manager);
@@ -174,6 +174,7 @@ class SyncEngine : public RemoteFileSyncService,
   ObserverList<FileStatusObserver> file_status_observers_;
 
   scoped_ptr<SyncWorker> sync_worker_;
+  scoped_refptr<base::SequencedTaskRunner> worker_task_runner_;
 
   base::WeakPtrFactory<SyncEngine> weak_ptr_factory_;
   DISALLOW_COPY_AND_ASSIGN(SyncEngine);
