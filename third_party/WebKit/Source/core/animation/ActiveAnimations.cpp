benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit e7d812105f5dcab40afb25cd4c5849c2f536a5de

Web Animations: Break reference cycle keeping Elements alive
We currently have a couple of reference cycles, basically stemming from the
strong pointer Animation->Element. As a result, we will not be able to free
Elements when they have current or in effect Animations, even if there are
no longer any references around (or we've navigated to a separate page!)

This is a temporary patch (until Element is moved to Oilpan) to break the
reference cycles that we have (see referenced bug). We simply change the
Animation->Element pointer to a raw pointer, and clean it upon Element
destruction. Note that this is somewhat incorrect behaviour, since
Animations should keep their target alive if they are still reachable,
but we probably can't easily do this without leaking before we have Oilpan.

BUG=362404

Review URL: https://codereview.chromium.org/236833002

git-svn-id: svn://svn.chromium.org/blink/trunk@171537 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/animation/ActiveAnimations.cpp
@@ -36,6 +36,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+ActiveAnimations::~ActiveAnimations()
+{
+    for (size_t i = 0; i < m_animations.size(); ++i)
+        m_animations[i]->notifyElementDestroyed();
+}
+
 void ActiveAnimations::updateAnimationFlags(RenderStyle& style)
 {
     for (AnimationPlayerSet::const_iterator it = m_players.begin(); it != players().end(); ++it) {

third_party/WebKit/Source/core/animation/ActiveAnimations.h
@@ -53,6 +53,8 @@ public:
     {
     }
 
+    ~ActiveAnimations();
+
     // Animations that are currently active for this element, their effects will be applied
     // during a style recalc. CSS Transitions are included in this stack.
     AnimationStack& defaultStack() { return m_defaultStack; }
@@ -67,12 +69,15 @@ public:
     const AnimationPlayerSet& players() const { return m_players; }
     AnimationPlayerSet& players() { return m_players; }
 
-    bool isEmpty() const { return m_defaultStack.isEmpty() && m_cssAnimations.isEmpty(); }
+    bool isEmpty() const { return m_defaultStack.isEmpty() && m_cssAnimations.isEmpty() && m_animations.isEmpty(); }
 
     void cancelAnimationOnCompositor();
 
     void updateAnimationFlags(RenderStyle&);
     void setAnimationStyleChange(bool animationStyleChange) { m_animationStyleChange = animationStyleChange; }
+
+    void addAnimation(Animation* animation) { m_animations.append(animation); }
+    void notifyAnimationDestroyed(Animation* animation) { m_animations.remove(m_animations.find(animation)); }
 
     void trace(Visitor*);
 
@@ -83,6 +88,10 @@ private:
     CSSAnimations m_cssAnimations;
     AnimationPlayerSet m_players;
     bool m_animationStyleChange;
+
+    // This is to avoid a reference cycle that keeps Elements alive and
+    // won't be needed once Element and Animation are moved to Oilpan.
+    Vector<Animation*> m_animations;
 
     // CSSAnimations checks if a style change is due to animation.
     friend class CSSAnimations;

third_party/WebKit/Source/core/animation/Animation.cpp
@@ -47,7 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-PassRefPtr<Animation> Animation::create(PassRefPtr<Element> target, PassRefPtrWillBeRawPtr<AnimationEffect> effect, const Timing& timing, Priority priority, PassOwnPtr<EventDelegate> eventDelegate)
+PassRefPtr<Animation> Animation::create(Element* target, PassRefPtrWillBeRawPtr<AnimationEffect> effect, const Timing& timing, Priority priority, PassOwnPtr<EventDelegate> eventDelegate)
 {
     return adoptRef(new Animation(target, effect, timing, priority, eventDelegate));
 }
@@ -86,13 +86,21 @@ PassRefPtr<Animation> Animation::create(Element* element, const Vector<Dictionar
     return create(element, EffectInput::convert(element, keyframeDictionaryVector, exceptionState), Timing());
 }
 
-Animation::Animation(PassRefPtr<Element> target, PassRefPtrWillBeRawPtr<AnimationEffect> effect, const Timing& timing, Priority priority, PassOwnPtr<EventDelegate> eventDelegate)
+Animation::Animation(Element* target, PassRefPtrWillBeRawPtr<AnimationEffect> effect, const Timing& timing, Priority priority, PassOwnPtr<EventDelegate> eventDelegate)
     : TimedItem(timing, eventDelegate)
     , m_target(target)
     , m_effect(effect)
     , m_sampledEffect(0)
     , m_priority(priority)
 {
+    if (m_target)
+        m_target->ensureActiveAnimations().addAnimation(this);
+}
+
+Animation::~Animation()
+{
+    if (m_target)
+        m_target->activeAnimations()->notifyAnimationDestroyed(this);
 }
 
 void Animation::didAttach()
@@ -140,7 +148,7 @@ void Animation::applyEffects()
     } else if (!interpolations->isEmpty()) {
         OwnPtr<SampledEffect> sampledEffect = SampledEffect::create(this, interpolations.release());
         m_sampledEffect = sampledEffect.get();
-        ensureAnimationStack(m_target.get()).add(sampledEffect.release());
+        ensureAnimationStack(m_target).add(sampledEffect.release());
     } else {
         return;
     }
@@ -216,6 +224,18 @@ void Animation::notifySampledEffectRemovedFromAnimationStack()
     m_sampledEffect = 0;
 }
 
+void Animation::notifyElementDestroyed()
+{
+    // If our player is kept alive just by the sampledEffect, we might get our
+    // destructor called when we call SampledEffect::clear(), so we need to
+    // clear m_sampledEffect first.
+    m_target = 0;
+    SampledEffect* sampledEffect = m_sampledEffect;
+    m_sampledEffect = 0;
+    if (sampledEffect)
+        sampledEffect->clear();
+}
+
 bool Animation::isCandidateForAnimationOnCompositor() const
 {
     if (!effect() || !m_target)
@@ -228,9 +248,9 @@ bool Animation::maybeStartAnimationOnCompositor(double startTime)
     ASSERT(!hasActiveAnimationsOnCompositor());
     if (!isCandidateForAnimationOnCompositor())
         return false;
-    if (!CompositorAnimations::instance()->canStartAnimationOnCompositor(*m_target.get()))
+    if (!CompositorAnimations::instance()->canStartAnimationOnCompositor(*m_target))
         return false;
-    if (!CompositorAnimations::instance()->startAnimationOnCompositor(*m_target.get(), startTime, specifiedTiming(), *effect(), m_compositorAnimationIds))
+    if (!CompositorAnimations::instance()->startAnimationOnCompositor(*m_target, startTime, specifiedTiming(), *effect(), m_compositorAnimationIds))
         return false;
     ASSERT(!m_compositorAnimationIds.isEmpty());
     return true;
@@ -262,7 +282,7 @@ void Animation::cancelAnimationOnCompositor()
     if (!m_target || !m_target->renderer())
         return;
     for (size_t i = 0; i < m_compositorAnimationIds.size(); ++i)
-        CompositorAnimations::instance()->cancelAnimationOnCompositor(*m_target.get(), m_compositorAnimationIds[i]);
+        CompositorAnimations::instance()->cancelAnimationOnCompositor(*m_target, m_compositorAnimationIds[i]);
     m_compositorAnimationIds.clear();
 }
 
@@ -272,7 +292,7 @@ void Animation::pauseAnimationForTestingOnCompositor(double pauseTime)
     if (!m_target || !m_target->renderer())
         return;
     for (size_t i = 0; i < m_compositorAnimationIds.size(); ++i)
-        CompositorAnimations::instance()->pauseAnimationForTestingOnCompositor(*m_target.get(), m_compositorAnimationIds[i], pauseTime);
+        CompositorAnimations::instance()->pauseAnimationForTestingOnCompositor(*m_target, m_compositorAnimationIds[i], pauseTime);
 }
 
 } // namespace WebCore

third_party/WebKit/Source/core/animation/Animation.h
@@ -51,7 +51,7 @@ class Animation FINAL : public TimedItem {
 public:
     enum Priority { DefaultPriority, TransitionPriority };
 
-    static PassRefPtr<Animation> create(PassRefPtr<Element>, PassRefPtrWillBeRawPtr<AnimationEffect>, const Timing&, Priority = DefaultPriority, PassOwnPtr<EventDelegate> = nullptr);
+    static PassRefPtr<Animation> create(Element*, PassRefPtrWillBeRawPtr<AnimationEffect>, const Timing&, Priority = DefaultPriority, PassOwnPtr<EventDelegate> = nullptr);
     // Web Animations API Bindings constructors.
     static PassRefPtr<Animation> create(Element*, PassRefPtrWillBeRawPtr<AnimationEffect>, const Dictionary& timingInputDictionary);
     static PassRefPtr<Animation> create(Element*, PassRefPtrWillBeRawPtr<AnimationEffect>, double duration);
@@ -60,15 +60,18 @@ public:
     static PassRefPtr<Animation> create(Element*, const Vector<Dictionary>& keyframeDictionaryVector, double duration, ExceptionState&);
     static PassRefPtr<Animation> create(Element*, const Vector<Dictionary>& keyframeDictionaryVector, ExceptionState&);
 
+    virtual ~Animation();
+
     virtual bool isAnimation() const OVERRIDE { return true; }
 
     bool affects(CSSPropertyID) const;
     const AnimationEffect* effect() const { return m_effect.get(); }
     AnimationEffect* effect() { return m_effect.get(); }
     Priority priority() const { return m_priority; }
-    Element* target() { return m_target.get(); }
+    Element* target() { return m_target; }
 
     void notifySampledEffectRemovedFromAnimationStack();
+    void notifyElementDestroyed();
 
     bool isCandidateForAnimationOnCompositor() const;
     // Must only be called once.
@@ -88,9 +91,9 @@ protected:
     virtual double calculateTimeToEffectChange(bool forwards, double inheritedTime, double timeToNextIteration) const OVERRIDE;
 
 private:
-    Animation(PassRefPtr<Element>, PassRefPtrWillBeRawPtr<AnimationEffect>, const Timing&, Priority, PassOwnPtr<EventDelegate>);
+    Animation(Element*, PassRefPtrWillBeRawPtr<AnimationEffect>, const Timing&, Priority, PassOwnPtr<EventDelegate>);
 
-    RefPtr<Element> m_target;
+    Element* m_target;
     RefPtrWillBePersistent<AnimationEffect> m_effect;
 
     SampledEffect* m_sampledEffect;

third_party/WebKit/Source/core/animation/AnimationPlayerTest.cpp
@@ -74,7 +74,7 @@ protected:
         Timing timing;
         timing.iterationDuration = duration;
         timing.playbackRate = playbackRate;
-        return Animation::create(nullptr, nullptr, timing);
+        return Animation::create(0, nullptr, timing);
     }
 
     bool updateTimeline(double time)
@@ -466,7 +466,7 @@ TEST_F(AnimationAnimationPlayerTest, FinishRaisesException)
     Timing timing;
     timing.iterationDuration = 1;
     timing.iterationCount = std::numeric_limits<double>::infinity();
-    player->setSource(Animation::create(nullptr, nullptr, timing).get());
+    player->setSource(Animation::create(0, nullptr, timing).get());
     player->setCurrentTime(10);
 
     player->finish(exceptionState);
@@ -635,7 +635,7 @@ TEST_F(AnimationAnimationPlayerTest, AnimationPlayersReturnTimeToNextEffect)
     timing.startDelay = 1;
     timing.iterationDuration = 1;
     timing.endDelay = 1;
-    RefPtr<Animation> animation = Animation::create(nullptr, nullptr, timing);
+    RefPtr<Animation> animation = Animation::create(0, nullptr, timing);
     player = timeline->createAnimationPlayer(animation.get());
     player->setStartTime(0);
 
@@ -712,7 +712,7 @@ TEST_F(AnimationAnimationPlayerTest, AttachedAnimationPlayers)
     RefPtr<Element> element = document->createElement("foo", ASSERT_NO_EXCEPTION);
 
     Timing timing;
-    RefPtr<Animation> animation = Animation::create(element, nullptr, timing);
+    RefPtr<Animation> animation = Animation::create(element.get(), nullptr, timing);
     RefPtr<AnimationPlayer> player = timeline->createAnimationPlayer(animation.get());
     timeline->serviceAnimations(AnimationPlayer::UpdateForAnimationFrame);
     EXPECT_EQ(1U, element->activeAnimations()->players().find(player.get())->value);

third_party/WebKit/Source/core/animation/AnimationStackTest.cpp
@@ -69,7 +69,7 @@ protected:
         Timing timing;
         timing.fillMode = Timing::FillModeBoth;
         timing.iterationDuration = duration;
-        return Animation::create(element, effect, timing);
+        return Animation::create(element.get(), effect, timing);
     }
 
     AnimatableValue* interpolationValue(Interpolation* interpolation)

third_party/WebKit/Source/core/animation/AnimationTest.cpp
@@ -360,7 +360,7 @@ TEST_F(AnimationAnimationTest, TimeToEffectChange)
     timing.startDelay = 100;
     timing.endDelay = 100;
     timing.fillMode = Timing::FillModeNone;
-    RefPtr<Animation> animation = Animation::create(nullptr, nullptr, timing);
+    RefPtr<Animation> animation = Animation::create(0, nullptr, timing);
     RefPtr<AnimationPlayer> player = document->timeline().play(animation.get());
     double inf = std::numeric_limits<double>::infinity();
 
@@ -393,7 +393,7 @@ TEST_F(AnimationAnimationTest, TimeToEffectChangeWithPlaybackRate)
     timing.endDelay = 100;
     timing.playbackRate = 2;
     timing.fillMode = Timing::FillModeNone;
-    RefPtr<Animation> animation = Animation::create(nullptr, nullptr, timing);
+    RefPtr<Animation> animation = Animation::create(0, nullptr, timing);
     RefPtr<AnimationPlayer> player = document->timeline().play(animation.get());
     double inf = std::numeric_limits<double>::infinity();
 
@@ -426,7 +426,7 @@ TEST_F(AnimationAnimationTest, TimeToEffectChangeWithNegativePlaybackRate)
     timing.endDelay = 100;
     timing.playbackRate = -2;
     timing.fillMode = Timing::FillModeNone;
-    RefPtr<Animation> animation = Animation::create(nullptr, nullptr, timing);
+    RefPtr<Animation> animation = Animation::create(0, nullptr, timing);
     RefPtr<AnimationPlayer> player = document->timeline().play(animation.get());
     double inf = std::numeric_limits<double>::infinity();
 
@@ -448,6 +448,19 @@ TEST_F(AnimationAnimationTest, TimeToEffectChangeWithNegativePlaybackRate)
     player->setCurrentTime(200);
     EXPECT_EQ(inf, animation->timeToForwardsEffectChange());
     EXPECT_EQ(50, animation->timeToReverseEffectChange());
+}
+
+TEST_F(AnimationAnimationTest, ElementDestructorClearsAnimationTarget)
+{
+    // This test expects incorrect behaviour should be removed once Element
+    // and Animation are moved to Oilpan. See crbug.com/362404 for context.
+    Timing timing;
+    timing.iterationDuration = 5;
+    RefPtr<Animation> animation = Animation::create(0, nullptr, timing);
+    RefPtr<AnimationPlayer> player = document->timeline().play(animation.get());
+    document.clear();
+    element.clear();
+    EXPECT_EQ(0, animation->target());
 }
 
 } // namespace WebCore

third_party/WebKit/Source/core/animation/DocumentTimelineTest.cpp
@@ -284,7 +284,7 @@ TEST_F(AnimationDocumentTimelineTest, PlayAfterDocumentDeref)
     element = nullptr;
     document = nullptr;
 
-    RefPtr<Animation> anim = Animation::create(nullptr, nullptr, timing);
+    RefPtr<Animation> anim = Animation::create(0, nullptr, timing);
     // Test passes if this does not crash.
     timeline->play(anim.get());
 }

commit 00b65d7794a494c8d4184516ea1182ffc46f8f95

Oilpan: Animation::m_target shouldn't become a stale pointer
This is a CL to fix a regression caused by r171537.

Currently Animation::notifyElementDestroyed() is called in the destructor of ActiveAnimations. This is too late because ActiveAnimations is on heap and the destructor is delayed to the next GC. Consequently, Animation::m_target can become a stale poiter and cause crash in oilpan builds.

To fix the issue, this CL changes code so that Animation::notifyElementDestroyed() is called in the destructor of Element.

BUG=340522

Review URL: https://codereview.chromium.org/242773003

git-svn-id: svn://svn.chromium.org/blink/trunk@172009 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/animation/ActiveAnimations.cpp
@@ -38,8 +38,13 @@ namespace WebCore {
 
 ActiveAnimations::~ActiveAnimations()
 {
+}
+
+void ActiveAnimations::dispose()
+{
     for (size_t i = 0; i < m_animations.size(); ++i)
         m_animations[i]->notifyElementDestroyed();
+    m_animations.clear();
 }
 
 void ActiveAnimations::updateAnimationFlags(RenderStyle& style)

third_party/WebKit/Source/core/animation/ActiveAnimations.h
@@ -54,6 +54,7 @@ public:
     }
 
     ~ActiveAnimations();
+    void dispose();
 
     // Animations that are currently active for this element, their effects will be applied
     // during a style recalc. CSS Transitions are included in this stack.

third_party/WebKit/Source/core/dom/Element.cpp
@@ -170,6 +170,9 @@ Element::~Element()
     if (hasRareData())
         elementRareData()->clearShadow();
 
+    if (hasActiveAnimations())
+        activeAnimations()->dispose();
+
     if (isCustomElement())
         CustomElement::wasDestroyed(this);
 