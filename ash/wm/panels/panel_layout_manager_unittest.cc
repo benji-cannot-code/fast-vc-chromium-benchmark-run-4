33d878ec9ca2069eebeedd1ac915bb5568c7788a
flackr@chromium.org <flackr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-22 19:35:41 +0000

    Remove remaining window states where window is shown with opacity 0.
    BUG=351553
    TEST=Unit tests pass, can enter overview mode and show/hide panels in debug mode without triggering DCHECK in window.cc.
    
    Review URL: https://codereview.chromium.org/237963019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265330 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/panels/panel_layout_manager_unittest.cc
+++ ash/wm/panels/panel_layout_manager_unittest.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/test/shell_test_api.h"
 #include "ash/test/test_shelf_delegate.h"
 #include "ash/wm/mru_window_tracker.h"
+#include "ash/wm/window_state.h"
 #include "ash/wm/window_util.h"
 #include "base/basictypes.h"
 #include "base/command_line.h"
@@ -68,9 +69,6 @@ class PanelLayoutManagerTest : public test::AshTestBase {
     test::TestShelfDelegate* shelf_delegate =
         test::TestShelfDelegate::instance();
     shelf_delegate->AddShelfItem(window);
-    PanelLayoutManager* manager = static_cast<PanelLayoutManager*>(
-        GetPanelContainer(window)->layout_manager());
-    manager->Relayout();
     shelf_view_test()->RunMessageLoopUntilAnimationsDone();
     return window;
   }
@@ -578,12 +576,12 @@ TEST_F(PanelLayoutManagerTest, MinimizeRestorePanel) {
   RunAllPendingInMessageLoop();
   EXPECT_TRUE(IsPanelCalloutVisible(window.get()));
   // Minimize the panel, callout should be hidden.
-  window->SetProperty(aura::client::kShowStateKey, ui::SHOW_STATE_MINIMIZED);
+  wm::GetWindowState(window.get())->Minimize();
   RunAllPendingInMessageLoop();
   EXPECT_FALSE(IsPanelCalloutVisible(window.get()));
-  // Restore the pantel; panel should not be activated by default but callout
+  // Restore the panel; panel should not be activated by default but callout
   // should be visible.
-  window->SetProperty(aura::client::kShowStateKey, ui::SHOW_STATE_NORMAL);
+  wm::GetWindowState(window.get())->Unminimize();
   RunAllPendingInMessageLoop();
   EXPECT_TRUE(IsPanelCalloutVisible(window.get()));
   // Activate the window, ensure callout is visible.
@@ -751,7 +749,7 @@ TEST_F(PanelLayoutManagerTest, PanelsHideAndRestoreWithShelf) {
   scoped_ptr<aura::Window> w2(CreatePanelWindow(bounds));
   scoped_ptr<aura::Window> w3;
   // Minimize w2.
-  w2->SetProperty(aura::client::kShowStateKey, ui::SHOW_STATE_MINIMIZED);
+  wm::GetWindowState(w2.get())->Minimize();
   RunAllPendingInMessageLoop();
   EXPECT_TRUE(w1->IsVisible());
   EXPECT_FALSE(w2->IsVisible());
