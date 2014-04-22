da494ea358a0cbda57e0d69ab93639253bb06590
tnagel@chromium.org <tnagel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-22 11:24:31 +0000

    Remove unused variable declaration, fix IWYU.
    BUG=82098
    
    Review URL: https://codereview.chromium.org/233123002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265230 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/extensions/wallpaper_manager_util.h
+++ chrome/browser/chromeos/extensions/wallpaper_manager_util.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CHROMEOS_EXTENSIONS_WALLPAPER_MANAGER_UTIL_H_
 #define CHROME_BROWSER_CHROMEOS_EXTENSIONS_WALLPAPER_MANAGER_UTIL_H_
 
-extern const char kWallpaperManagerDomain[];
-
 namespace wallpaper_manager_util {
 
 // Opens wallpaper manager application.
