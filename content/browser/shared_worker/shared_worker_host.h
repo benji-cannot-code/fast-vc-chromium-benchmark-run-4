2160d5bf7cb9c446e72eba77b9179d42497a940e
horo@chromium.org <horo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-22 08:07:53 +0000

    Fix the race issue in SharedWorkerServiceImpl::CreateWorker.
    To avoid the race issue in SharedWorkerServiceImpl::CreateWorker pointed out in https://crbug.com/362355, we reserve the renderer process bounce through the UI thread before sending ViewMsg_WorkerCreated on the IO thread.
    If the reservation failed, SharedWorkerServiceImpl retries to reserve the another renderer process in RenderProcessReserveFailedCallback().
    
    BUG=362355
    
    Review URL: https://codereview.chromium.org/240473007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265208 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/shared_worker/shared_worker_host.h
+++ content/browser/shared_worker/shared_worker_host.h
@@ -30,7 +30,8 @@ class SharedWorkerInstance;
 class SharedWorkerHost {
  public:
   SharedWorkerHost(SharedWorkerInstance* instance,
-                   SharedWorkerMessageFilter* filter);
+                   SharedWorkerMessageFilter* filter,
+                   int worker_route_id);
   ~SharedWorkerHost();
 
   // Sends |message| to the SharedWorker.
@@ -85,6 +86,7 @@ class SharedWorkerHost {
   int worker_route_id() const { return worker_route_id_; }
   bool load_failed() const { return load_failed_; }
   bool closed() const { return closed_; }
+  void set_fast_shutdown_detected() { fast_shutdown_detected_ = true; }
 
  private:
   // Unique identifier for a worker client.
@@ -127,6 +129,7 @@ class SharedWorkerHost {
   int worker_route_id_;
   bool load_failed_;
   bool closed_;
+  bool fast_shutdown_detected_;
   const base::TimeTicks creation_time_;
   DISALLOW_COPY_AND_ASSIGN(SharedWorkerHost);
 };
