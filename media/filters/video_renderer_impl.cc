26edcfea5dc8f5310238ee7f7530f6b3b29571ae
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-22 00:10:43 +0000

    Remove defunct media::VideoRendererImpl::kMaxLastFrameDuration code.
    It was originally added in r150467 to fix a certain type of end of stream bugs but was ultimately discarded in r185335 when end of stream logic was reworked.
    
    BUG=110814
    
    Review URL: https://codereview.chromium.org/242733006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265122 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/video_renderer_impl.cc
+++ media/filters/video_renderer_impl.cc
@@ -19,10 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
-base::TimeDelta VideoRendererImpl::kMaxLastFrameDuration() {
-  return base::TimeDelta::FromMilliseconds(250);
-}
-
 VideoRendererImpl::VideoRendererImpl(
     const scoped_refptr<base::SingleThreadTaskRunner>& task_runner,
     ScopedVector<VideoDecoder> decoders,
