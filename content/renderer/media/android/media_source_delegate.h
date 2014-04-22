48675310c4391f027b2d0c458aaecc93957a4c66
gunsch@chromium.org <gunsch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-22 00:09:00 +0000

    Fixes WebMediaPlayerAndroid to respect infinite-duration media streams.
    Tested primarily on Android from the following stream:
    https://www.youtube.com/tv#/watch?v=xrM34fdmloc
    
    R=damienv@chromium.org,qinmin@chromium.org
    BUG=b/13981784
    
    Review URL: https://codereview.chromium.org/239743005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265119 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/android/media_source_delegate.h
+++ content/renderer/media/android/media_source_delegate.h
@@ -169,7 +169,7 @@ class MediaSourceDelegate : public media::DemuxerHost {
                      const scoped_refptr<media::DecoderBuffer>& buffer);
 
   // Helper function for calculating duration.
-  int GetDurationMs();
+  base::TimeDelta GetDuration() const;
 
   bool IsSeeking() const;
 
