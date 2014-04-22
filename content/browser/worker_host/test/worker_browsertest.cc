65eb9888918fccb5e722be10177529f74201fa48
horo@chromium.org <horo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-22 14:28:06 +0000

    Reverse the embedded-shared-worker flag to turn the feature on by default.
    BUG=327256
    
    Review URL: https://codereview.chromium.org/240843002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265251 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/worker_host/test/worker_browsertest.cc
+++ content/browser/worker_host/test/worker_browsertest.cc
@@ -149,6 +149,9 @@ IN_PROC_BROWSER_TEST_F(WorkerTest, DISABLED_LimitPerPage) {
 #else
 IN_PROC_BROWSER_TEST_F(WorkerTest, LimitPerPage) {
 #endif
+  // There is no limitation of SharedWorker if EmbeddedSharedWorker is enabled.
+  if (WorkerService::EmbeddedSharedWorkerEnabled())
+    return;
   int max_workers_per_tab = WorkerServiceImpl::kMaxWorkersPerFrameWhenSeparate;
   std::string query = base::StringPrintf("?count=%d", max_workers_per_tab + 1);
 
@@ -166,6 +169,9 @@ IN_PROC_BROWSER_TEST_F(WorkerTest, DISABLED_LimitTotal) {
 // http://crbug.com/36800
 IN_PROC_BROWSER_TEST_F(WorkerTest, LimitTotal) {
 #endif
+  // There is no limitation of SharedWorker if EmbeddedSharedWorker is enabled.
+  if (WorkerService::EmbeddedSharedWorkerEnabled())
+    return;
   if (base::SysInfo::AmountOfPhysicalMemoryMB() < 8192) {
     VLOG(0) << "WorkerTest.LimitTotal not running because it needs 8 GB RAM.";
     return;
