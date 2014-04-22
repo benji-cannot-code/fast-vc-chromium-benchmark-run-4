23070853e9c2f73ec2b1acb3ae251fa9cabaed21
vabr@chromium.org <vabr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-22 10:56:40 +0000

    Fixes in UserGestureIndicator
    This CL fixes problems discovered in https://codereview.chromium.org/235983016/ post landing.
    
    BUG=163072
    
    Review URL: https://codereview.chromium.org/239963005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172128 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/UserGestureIndicator.cpp
+++ third_party/WebKit/Source/platform/UserGestureIndicator.cpp
@@ -131,14 +131,15 @@ UserGestureIndicator::UserGestureIndicator(ProcessingUserGestureState state)
             m_token = s_topmostIndicator->currentToken();
         }
         s_state = state;
-        if (state == DefinitelyProcessingNewUserGesture || state == DefinitelyProcessingUserGesture)
-            s_processedUserGestureInPast = true;
     }
 
-    if (state == DefinitelyProcessingNewUserGesture)
+    if (state == DefinitelyProcessingNewUserGesture) {
         static_cast<GestureToken*>(m_token.get())->addGesture();
-    else if (state == DefinitelyProcessingUserGesture && s_topmostIndicator == this)
+        s_processedUserGestureInPast = true;
+    } else if (state == DefinitelyProcessingUserGesture && s_topmostIndicator == this) {
         static_cast<GestureToken*>(m_token.get())->addGesture();
+        s_processedUserGestureInPast = true;
+    }
     ASSERT(isDefinite(s_state));
 }
 
@@ -162,7 +163,6 @@ UserGestureIndicator::UserGestureIndicator(PassRefPtr<UserGestureToken> token)
             }
         }
         s_state = DefinitelyProcessingUserGesture;
-        s_processedUserGestureInPast = true;
     }
 
     ASSERT(isDefinite(s_state));
@@ -202,7 +202,7 @@ UserGestureToken* UserGestureIndicator::currentToken()
 void UserGestureIndicator::clearProcessedUserGestureInPast()
 {
     if (isMainThread())
-        s_processedUserGestureInPast = true;
+        s_processedUserGestureInPast = false;
 }
 
 bool UserGestureIndicator::processedUserGestureInPast()
--- third_party/WebKit/Source/web/tests/WebUserGestureTokenTest.cpp
+++ third_party/WebKit/Source/web/tests/WebUserGestureTokenTest.cpp
@@ -47,6 +47,8 @@ TEST(WebUserGestureTokenTest, Basic)
 {
     WebUserGestureToken token;
     EXPECT_FALSE(token.hasGestures());
+    UserGestureIndicator::clearProcessedUserGestureInPast();
+    EXPECT_FALSE(UserGestureIndicator::processedUserGestureInPast());
 
     {
         WebScopedUserGesture indicator(token);
@@ -57,6 +59,7 @@ TEST(WebUserGestureTokenTest, Basic)
         UserGestureIndicator indicator(DefinitelyProcessingNewUserGesture);
         EXPECT_TRUE(WebUserGestureIndicator::isProcessingUserGesture());
         token = WebUserGestureIndicator::currentUserGestureToken();
+        EXPECT_TRUE(UserGestureIndicator::processedUserGestureInPast());
     }
 
     EXPECT_TRUE(token.hasGestures());
@@ -75,6 +78,8 @@ TEST(WebUserGestureTokenTest, Basic)
         WebScopedUserGesture indicator(token);
         EXPECT_FALSE(WebUserGestureIndicator::isProcessingUserGesture());
     }
+
+    EXPECT_TRUE(UserGestureIndicator::processedUserGestureInPast());
 }
 
 }
