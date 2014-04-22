317c4281a9625b94353dbb83e71466fe8dd03184
ellyjones@chromium.org <ellyjones@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-22 16:32:57 +0000

    Move auto-reload count increment to timer fire.
    Incrementing the auto-reload count when the timer starts can have adverse
    effects if the timer is stopped and restarted by network change notifications -
    the backoff count can rapidly grow, effectively disabling auto-reload. To fix
    this, increment the count when the timer actually fires, and adjust unit tests
    to match.
    
    BUG=chromium:363258
    TEST=unit
    
    Review URL: https://codereview.chromium.org/245583002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265275 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/net/net_error_helper_core.h
+++ chrome/renderer/net/net_error_helper_core.h
@@ -183,6 +183,7 @@ class NetErrorHelperCore {
   void Reload();
   bool MaybeStartAutoReloadTimer();
   void StartAutoReloadTimer();
+  void AutoReloadTimerFired();
 
   static bool IsReloadableError(const ErrorPageInfo& info);
 
