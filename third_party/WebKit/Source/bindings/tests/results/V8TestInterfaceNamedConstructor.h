dee7594d41318e245f628167d7d323f9f68a8388
sigbjornf@opera.com <sigbjornf@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-22 05:17:07 +0000

    In generated interface header files, #include Handle.h from its new home.
    It has moved to platform/heap/Handle.h.
    
    R=haraken, nbarth
    
    Review URL: https://codereview.chromium.org/245783002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172117 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/tests/results/V8TestInterfaceNamedConstructor.h
+++ third_party/WebKit/Source/bindings/tests/results/V8TestInterfaceNamedConstructor.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/v8/V8Binding.h"
 #include "bindings/v8/V8DOMWrapper.h"
 #include "bindings/v8/WrapperTypeInfo.h"
-#include "heap/Handle.h"
+#include "platform/heap/Handle.h"
 
 namespace WebCore {
 
