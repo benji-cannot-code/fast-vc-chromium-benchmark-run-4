05553cccc9744dbb4e645379fa155b1cb89f5418
piman@chromium.org <piman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-22 23:44:14 +0000

    Allow empty overhang image
    When resources fail to load (e.g. tests), the overscroll theme images are not
    NULL, they're empty, so handle accordingly.
    This is needed to unblock https://codereview.chromium.org/236363019/
    
    BUG=362164
    
    Review URL: https://codereview.chromium.org/246633002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172287 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebViewImpl.cpp
+++ third_party/WebKit/Source/web/WebViewImpl.cpp
@@ -3859,7 +3859,7 @@ void WebViewImpl::setIsAcceleratedCompositingActive(bool active)
             m_layerTreeView->setHasTransparentBackground(isTransparent());
 #if USE(RUBBER_BANDING)
             RefPtr<Image> overhangImage = OverscrollTheme::theme()->getOverhangImage();
-            if (overhangImage)
+            if (overhangImage && overhangImage->nativeImageForCurrentFrame())
                 m_layerTreeView->setOverhangBitmap(overhangImage->nativeImageForCurrentFrame()->bitmap());
 #endif
             updateLayerTreeViewport();
