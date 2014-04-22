da2bcb42dfbbfd17ccd079193a7bdf07d14f42ce
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-22 21:00:34 +0000

    clang/win: Fix a few warnings, mostly by deleting dead code.
    The OptionalHeader change fixes a bug, but a benign one since the subclass
    OptionalHeaderImpl doesn't have a destructor or non-POD members.
    
    BUG=82385
    R=scottmg@chromium.org
    TBR=scherkus
    
    Review URL: https://codereview.chromium.org/247483003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265355 0039d316-1c4b-4281-b951-d872f2087c98

--- media/video/capture/win/video_capture_device_mf_win.cc
+++ media/video/capture/win/video_capture_device_mf_win.cc
@@ -174,7 +174,7 @@ bool LoadMediaFoundationDlls() {
 
 }  // namespace
 
-class MFReaderCallback
+class MFReaderCallback FINAL
     : public base::RefCountedThreadSafe<MFReaderCallback>,
       public IMFSourceReaderCallback {
  public:
