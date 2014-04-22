a6f54fb189dc3ef77d2c380f61e4ee76cbbd148d
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-22 22:39:55 +0000

    Prevent Control+two-finger-scroll from zooming the page.
    Make WebContentsImpl avoid zooming in response to a
    two-finger-scroll event while the Control key is held. It's
    very easy to accidentally trigger this feature and difficult
    to undo it when a windowed app is focused.
    
    Control+scrollwheel is still supported for zooming.
    
    BUG=323164
    TBR=avi@chromium.org
    
    Review URL: https://codereview.chromium.org/242733008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265391 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/input/synthetic_web_input_event_builders.h
+++ content/common/input/synthetic_web_input_event_builders.h
@@ -18,9 +18,9 @@ class CONTENT_EXPORT SyntheticWebMouseEventBuilder {
  public:
   static blink::WebMouseEvent Build(blink::WebInputEvent::Type type);
   static blink::WebMouseEvent Build(blink::WebInputEvent::Type type,
-                                     int window_x,
-                                     int window_y,
-                                     int modifiers);
+                                    int window_x,
+                                    int window_y,
+                                    int modifiers);
 };
 
 class CONTENT_EXPORT SyntheticWebMouseWheelEventBuilder {
@@ -28,9 +28,9 @@ class CONTENT_EXPORT SyntheticWebMouseWheelEventBuilder {
   static blink::WebMouseWheelEvent Build(
       blink::WebMouseWheelEvent::Phase phase);
   static blink::WebMouseWheelEvent Build(float dx,
-                                          float dy,
-                                          int modifiers,
-                                          bool precise);
+                                         float dy,
+                                         int modifiers,
+                                         bool precise);
 };
 
 class CONTENT_EXPORT SyntheticWebKeyboardEventBuilder {
@@ -44,14 +44,14 @@ class CONTENT_EXPORT SyntheticWebGestureEventBuilder {
       blink::WebInputEvent::Type type,
       blink::WebGestureEvent::SourceDevice sourceDevice);
   static blink::WebGestureEvent BuildScrollBegin(float dx_hint,
-                                                  float dy_hint);
+                                                 float dy_hint);
   static blink::WebGestureEvent BuildScrollUpdate(float dx,
-                                                   float dy,
-                                                   int modifiers);
-  static blink::WebGestureEvent BuildPinchUpdate(float scale,
-                                                  float anchor_x,
-                                                  float anchor_y,
+                                                  float dy,
                                                   int modifiers);
+  static blink::WebGestureEvent BuildPinchUpdate(float scale,
+                                                 float anchor_x,
+                                                 float anchor_y,
+                                                 int modifiers);
   static blink::WebGestureEvent BuildFling(
       float velocity_x,
       float velocity_y,
