ea9be597826ba4f394255ab4d37ecf721eec9b81
dnicoara@chromium.org <dnicoara@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-23 20:11:10 +0000

    [Ozone-DRI] Fix clang build (missing virtual and OVERRIDE)
    TBR=spang@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/248743003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265720 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/ozone/platform/test/ozone_platform_test.cc
+++ ui/ozone/platform/test/ozone_platform_test.cc
@@ -47,7 +47,8 @@ class OzonePlatformTest : public OzonePlatform {
   }
 
 #if defined(OS_CHROMEOS)
-  scoped_ptr<ui::NativeDisplayDelegate> CreateNativeDisplayDelegate() {
+  virtual scoped_ptr<ui::NativeDisplayDelegate> CreateNativeDisplayDelegate()
+      OVERRIDE {
     return scoped_ptr<ui::NativeDisplayDelegate>(
         new NativeDisplayDelegateOzone());
   }
