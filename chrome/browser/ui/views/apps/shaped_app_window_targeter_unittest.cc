c04d6526adf8ebc1fbc64c453053df56607f6403
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-23 09:22:41 +0000

    Remove title and icon from chrome apps native style title bars.
    These windows aren't meant to have titles or icons, but still need to
    get a proper title as far as the OS is concerned. This means using a
    custom but glass frame, similarly to how unthemed browser windows look.
    
    BUG=339558
    TEST=Try out frame:chrome app windows windows on Windows 7, 8 and XP.
    Use the windows in various states (maximised, fullscreen, normal).
    Make sure they work after switching in and out of aero mode.
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=262516
    
    Review URL: https://codereview.chromium.org/213743017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265598 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/apps/shaped_app_window_targeter_unittest.cc
+++ chrome/browser/ui/views/apps/shaped_app_window_targeter_unittest.cc
@@ -185,9 +185,9 @@ TEST_F(ShapedAppWindowTargeterTest, ResizeInsetsWithinBounds) {
     EXPECT_EQ(window, move.target());
   }
 
-  // The EasyResizeTargeter specifies an inset of 5px within the window.
-  app_window_views()->InstallEasyResizeTargeterOnContainer();
-
+#if !defined(OS_CHROMEOS)
+  // The non standard app frame has a easy resize targetter installed.
+  app_window_views()->CreateNonStandardAppFrame();
   {
     // Ensure that the window has an event targeter (there should be an
     // EasyResizeWindowTargeter installed).
@@ -224,4 +224,5 @@ TEST_F(ShapedAppWindowTargeterTest, ResizeInsetsWithinBounds) {
     ASSERT_FALSE(details.dispatcher_destroyed);
     EXPECT_EQ(window, move.target());
   }
+#endif  // defined (OS_CHROMEOS)
 }
