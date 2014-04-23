584f43d1c5116250aa815e9d2ae6ec3c699097de
dfalcantara@chromium.org <dfalcantara@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-23 20:01:52 +0000

    App install alerts: Disable on all channels for M35
    Disables app install alerts with no way to turn them back on through flags.
    
    BUG=366169
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/229533002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265716 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/android/banners/app_banner_manager.cc
+++ chrome/browser/android/banners/app_banner_manager.cc
@@ -7,14 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/android/jni_android.h"
 #include "base/android/jni_string.h"
-#include "base/command_line.h"
 #include "base/metrics/histogram.h"
 #include "chrome/browser/android/banners/app_banner_metrics_ids.h"
 #include "chrome/browser/android/banners/app_banner_settings_helper.h"
 #include "chrome/browser/android/banners/app_banner_utilities.h"
 #include "chrome/browser/bitmap_fetcher.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/common/chrome_switches.h"
 #include "content/public/browser/android/content_view_core.h"
 #include "content/public/browser/navigation_details.h"
 #include "content/public/browser/web_contents.h"
@@ -153,8 +151,7 @@ jlong Init(JNIEnv* env, jobject obj) {
 }
 
 jboolean IsEnabled(JNIEnv* env, jclass clazz) {
-  return !CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kDisableAppBanners);
+  return false;
 }
 
 // Register native methods
--- chrome/common/chrome_switches.cc
+++ chrome/common/chrome_switches.cc
@@ -1293,9 +1293,6 @@ const char kPluginsMetadataServerURL[]      = "plugins-metadata-server-url";
 #endif
 
 #if defined(OS_ANDROID)
-// Disables the app banner <meta> tag.
-const char kDisableAppBanners[]              = "disable-app-banners";
-
 // Disables support for playing videos on Chromecast devices.
 const char kDisableCast[]                    = "disable-cast";
 
