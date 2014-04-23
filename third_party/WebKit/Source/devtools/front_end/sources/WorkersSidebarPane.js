65b110dcb096c088ae0727a458faffed731ed34e
sergeyv@chromium.org <sergeyv@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-23 17:20:07 +0000

    DevTools: prevent opening third devtools window while opening worker frontend
    BUG=
    
    Review URL: https://codereview.chromium.org/245893007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172394 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sources/WorkersSidebarPane.js
+++ third_party/WebKit/Source/devtools/front_end/sources/WorkersSidebarPane.js
@@ -107,7 +107,7 @@ WebInspector.WorkersSidebarPane.prototype = {
 
     _workerItemClicked: function(workerId, event)
     {
-        event.preventDefault();
+        event.consume(true);
         WebInspector.workerFrontendManager.openWorkerInspector(workerId);
     },
 
