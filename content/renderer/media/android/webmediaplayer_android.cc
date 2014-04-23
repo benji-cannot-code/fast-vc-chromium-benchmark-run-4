4cacbc26508e5ce012cd2565938d9e652f119dae
ycheo@chromium.org <ycheo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-23 08:57:05 +0000

    Add a flag to use the video overlay for the embedded video forcefully.
    This flag is for the purpose to test the hole punching logic in Android WebView.
    
    BUG=329447
    
    Review URL: https://codereview.chromium.org/231643007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265590 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/android/webmediaplayer_android.cc
+++ content/renderer/media/android/webmediaplayer_android.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "cc/layers/video_layer.h"
 #include "content/public/common/content_client.h"
+#include "content/public/common/content_switches.h"
 #include "content/public/renderer/render_frame.h"
 #include "content/renderer/media/android/renderer_demuxer_android.h"
 #include "content/renderer/media/android/renderer_media_player_manager.h"
@@ -131,6 +132,8 @@ WebMediaPlayerAndroid::WebMediaPlayerAndroid(
   // Defer stream texture creation until we are sure it's necessary.
   needs_establish_peer_ = false;
   current_frame_ = VideoFrame::CreateBlackFrame(gfx::Size(1, 1));
+  force_use_overlay_embedded_video_ = CommandLine::ForCurrentProcess()->
+      HasSwitch(switches::kForceUseOverlayEmbeddedVideo);
 #endif  // defined(VIDEO_HOLE)
   TryCreateStreamTextureProxyIfNeeded();
 }
@@ -673,7 +676,8 @@ void WebMediaPlayerAndroid::OnVideoSizeChanged(int width, int height) {
 #if defined(VIDEO_HOLE)
   // Use H/W surface for encrypted video.
   // TODO(qinmin): Change this so that only EME needs the H/W surface
-  if (media_source_delegate_ && media_source_delegate_->IsVideoEncrypted()) {
+  if (force_use_overlay_embedded_video_ ||
+      (media_source_delegate_ && media_source_delegate_->IsVideoEncrypted())) {
     needs_external_surface_ = true;
     if (!paused() && !manager_->IsInFullscreen(frame_))
       manager_->RequestExternalSurface(player_id_, last_computed_rect_);
