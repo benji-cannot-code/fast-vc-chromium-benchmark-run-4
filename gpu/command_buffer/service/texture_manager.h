e5ca639c9c8a8b8a337791c1b6a65134917cc264
vmiura@chromium.org <vmiura@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-23 23:31:03 +0000

    Fix slow TextureManager::StopTracking()
    Iterating an ObserverList which uses WeakPtr items seems very slow.
    
    Changing to std::vector<> gives about a 15X speedup.
    Further skipping the observer call backs for TextureRefs with no current observers should cut this close to 0.
    
    BUG=365536
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=265469
    
    Review URL: https://codereview.chromium.org/246353002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265771 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/texture_manager.h
+++ gpu/command_buffer/service/texture_manager.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/hash_tables.h"
 #include "base/logging.h"
 #include "base/memory/ref_counted.h"
-#include "base/observer_list.h"
 #include "gpu/command_buffer/service/async_pixel_transfer_delegate.h"
 #include "gpu/command_buffer/service/gl_utils.h"
 #include "gpu/command_buffer/service/memory_tracking.h"
@@ -405,10 +404,15 @@ class GPU_EXPORT TextureRef : public base::RefCounted<TextureRef> {
   static scoped_refptr<TextureRef> Create(TextureManager* manager,
                                           GLuint client_id,
                                           GLuint service_id);
+
+  void AddObserver() { num_observers_++; }
+  void RemoveObserver() { num_observers_--; }
+
   const Texture* texture() const { return texture_; }
   Texture* texture() { return texture_; }
   GLuint client_id() const { return client_id_; }
   GLuint service_id() const { return texture_->service_id(); }
+  GLint num_observers() const { return num_observers_; }
 
  private:
   friend class base::RefCounted<TextureRef>;
@@ -423,6 +427,7 @@ class GPU_EXPORT TextureRef : public base::RefCounted<TextureRef> {
   TextureManager* manager_;
   Texture* texture_;
   GLuint client_id_;
+  GLint num_observers_;
 
   DISALLOW_COPY_AND_ASSIGN(TextureRef);
 };
@@ -683,11 +688,18 @@ class GPU_EXPORT TextureManager {
       std::string* signature) const;
 
   void AddObserver(DestructionObserver* observer) {
-    destruction_observers_.AddObserver(observer);
+    destruction_observers_.push_back(observer);
   }
 
   void RemoveObserver(DestructionObserver* observer) {
-    destruction_observers_.RemoveObserver(observer);
+    for (unsigned int i = 0; i < destruction_observers_.size(); i++) {
+      if (destruction_observers_[i] == observer) {
+        std::swap(destruction_observers_[i], destruction_observers_.back());
+        destruction_observers_.pop_back();
+        return;
+      }
+    }
+    NOTREACHED();
   }
 
   struct DoTextImage2DArguments {
@@ -797,7 +809,7 @@ class GPU_EXPORT TextureManager {
   // The default textures for each target (texture name = 0)
   scoped_refptr<TextureRef> default_textures_[kNumDefaultTextures];
 
-  ObserverList<DestructionObserver> destruction_observers_;
+  std::vector<DestructionObserver*> destruction_observers_;
 
   DISALLOW_COPY_AND_ASSIGN(TextureManager);
 };
