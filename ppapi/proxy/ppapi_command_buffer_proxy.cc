3081edcff3efad202485a4cacf4eefb1aa146b1c
epenner@chromium.org <epenner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-23 02:07:54 +0000

    GPU/Pepper: Always return -1 for ID when we return a NULL buffer.
    In this particular case we could return NULL but also return
    a valid ID. This can result in a crash if the caller looks at
    the ID returned and assumes the buffer is also valid.
    
    BUG=365373
    
    Review URL: https://codereview.chromium.org/248193004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265499 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/proxy/ppapi_command_buffer_proxy.cc
+++ ppapi/proxy/ppapi_command_buffer_proxy.cc
@@ -133,6 +133,7 @@ scoped_refptr<gpu::Buffer> PpapiCommandBufferProxy::CreateTransferBuffer(
   // Map the shared memory on demand.
   if (!shared_memory->memory()) {
     if (!shared_memory->Map(handle.size())) {
+      *id = -1;
       return NULL;
     }
   }
