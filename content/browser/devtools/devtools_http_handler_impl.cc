cd0c721633598d6e620ed674bd00f6affcc72c44
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-23 15:43:17 +0000

    DevTools: Update protocol version number in chrome.debugger API and remote debugging.
    BUG=365673
    
    Review URL: https://codereview.chromium.org/247413002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265649 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/devtools/devtools_http_handler_impl.cc
+++ content/browser/devtools/devtools_http_handler_impl.cc
@@ -49,8 +49,6 @@ namespace content {
 
 namespace {
 
-const char kProtocolVersion[] = "1.0";
-
 const char kDevToolsHandlerThreadName[] = "Chrome_DevToolsHandlerThread";
 
 const char kThumbUrlPrefix[] = "/thumb/";
@@ -136,7 +134,7 @@ static bool TimeComparator(const DevToolsTarget* target1,
 // static
 bool DevToolsHttpHandler::IsSupportedProtocolVersion(
     const std::string& version) {
-  return version == kProtocolVersion;
+  return devtools::IsSupportedProtocolVersion(version);
 }
 
 // static
@@ -451,7 +449,7 @@ void DevToolsHttpHandlerImpl::OnJsonRequestUI(
 
   if (command == "version") {
     base::DictionaryValue version;
-    version.SetString("Protocol-Version", kProtocolVersion);
+    version.SetString("Protocol-Version", devtools::kProtocolVersion);
     version.SetString("WebKit-Version", GetWebKitVersion());
     version.SetString("Browser", GetContentClient()->GetProduct());
     version.SetString("User-Agent", GetContentClient()->GetUserAgent());
