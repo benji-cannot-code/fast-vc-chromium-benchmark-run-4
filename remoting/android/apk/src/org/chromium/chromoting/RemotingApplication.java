71d8458986404e2f08b55da887c217c42a07e4eb
lambroslambrou@chromium.org <lambroslambrou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-23 22:06:30 +0000

    Refactor GYP targets for Android Chromoting client
    This creates a separate target, remoting_java, that builds the Java
    code and resources. The remoting_apk target depends on this target and
    builds the final APK.
    
    Several changes were needed, since the build/java.gypi file has some
    features and checks not present in build/java_apk.gypi:
    
    * switch/case statements changed to if/else chains, since the generated
    R.java uses 'int' instead of 'const int' for resource IDs.
    * XML style fixes necessitated by the lint-style checking, and v14
    resource generation.
    * A new Java class added: RemotingApplication. This is needed right now
    because the APK build step requires at least one Java source file. In
    future, this class will override onCreate(), and a different override
    will be used for official builds, to dependency-inject Java code that
    requires closed-source dependencies (such as the Help+Feedback
    implementation).
    
    TEST=Builds and runs. Touching various files triggers rebuild.
    
    Review URL: https://codereview.chromium.org/245893004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265750 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ remoting/android/apk/src/org/chromium/chromoting/RemotingApplication.java
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chromoting;
+
+import android.app.Application;
+
+/** Main context for the application. */
+public class RemotingApplication extends Application {
+}
