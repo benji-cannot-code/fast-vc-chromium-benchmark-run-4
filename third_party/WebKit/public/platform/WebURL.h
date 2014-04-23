ed1be80926de24d797425cce0536f327175eb626
vitalybuka@chromium.org <vitalybuka@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-23 00:17:51 +0000

    Use url:: instead of url_util:: url_parse:: url_canon::
    BUG=364747
    TEST=none, no functional changes
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=172109
    
    Review URL: https://codereview.chromium.org/245223004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172295 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebURL.h
+++ third_party/WebKit/public/platform/WebURL.h
@@ -82,7 +82,7 @@ public:
         return m_string;
     }
 
-    const url_parse::Parsed& parsed() const
+    const url::Parsed& parsed() const
     {
         return m_parsed;
     }
@@ -130,7 +130,7 @@ public:
 
 private:
     WebString m_string;
-    url_parse::Parsed m_parsed;
+    url::Parsed m_parsed;
     bool m_isValid;
 };
 
