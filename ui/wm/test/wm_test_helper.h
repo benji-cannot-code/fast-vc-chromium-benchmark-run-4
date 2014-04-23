a82212953c73c72cf74d9c10e15012a15333d804
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-23 22:19:19 +0000

    aura: A couple of API cleanups.
    * Make Window::SetTransform() non-virtual.
    * Remove Window::SetEventFilter() (and use AddPreTargetHandler() instead).
    
    BUG=none
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/248773002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265755 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/wm/test/wm_test_helper.h
+++ ui/wm/test/wm_test_helper.h
@@ -49,9 +49,7 @@ class WMTestHelper : public aura::client::WindowTreeClient {
  private:
   scoped_ptr<aura::WindowTreeHost> host_;
 
-  // Owned by the root Window.
-  wm::CompoundEventFilter* root_window_event_filter_;
-
+  scoped_ptr<wm::CompoundEventFilter> root_window_event_filter_;
   scoped_ptr<aura::client::DefaultCaptureClient> capture_client_;
   scoped_ptr<wm::InputMethodEventFilter> input_method_filter_;
   scoped_ptr<aura::client::DefaultActivationClient> activation_client_;
