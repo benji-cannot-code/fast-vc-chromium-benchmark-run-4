43b679a07bdfcad013492025f09940607841f17a
donnd@chromium.org <donnd@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-23 03:37:21 +0000

    Add a navigation redirect-chain to Sync sessions proto for offline analysis.
    Google design doc at https://docs.google.com/a/google.com/document/d/1bh7aqvCMYzkobVA2MDuqWbQgymhdhdoqaOfvcLOaAYk
    
    BUG=310373
    
    Review URL: https://codereview.chromium.org/101573003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265515 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/protocol/proto_value_conversions.h
+++ sync/protocol/proto_value_conversions.h
@@ -51,6 +51,7 @@ class Media;
 class ManagedUserSettingSpecifics;
 class ManagedUserSharedSettingSpecifics;
 class ManagedUserSpecifics;
+class NavigationRedirect;
 class NigoriSpecifics;
 class PasswordSpecifics;
 class PasswordSpecificsData;
@@ -118,6 +119,9 @@ SYNC_EXPORT_PRIVATE base::DictionaryValue* SessionWindowToValue(
 
 SYNC_EXPORT_PRIVATE base::DictionaryValue* TabNavigationToValue(
     const sync_pb::TabNavigation& tab_navigation);
+
+SYNC_EXPORT_PRIVATE base::DictionaryValue* NavigationRedirectToValue(
+    const sync_pb::NavigationRedirect& navigation_redirect);
 
 // Sub-protocol of PasswordSpecifics.
 
