922d205c9af76582c05f1fa94f168f27678ad697
wangxianzhu@chromium.org <wangxianzhu@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-23 07:15:59 +0000

    Separate repaint and layout requirements of StyleDifference (Step 1)
    Previously StyleDifference was an enum that proximately bigger values
    imply smaller values (e.g. StyleDifferenceLayout implies
    StyleDifferenceRepaint). This causes unnecessary repaints in some cases
    on layout change.
    
    Convert StyleDifference to a structure containing relatively independent
    flags.
    
    This change doesn't directly improve the result, but can make further
    repaint optimizations possible.
    
    Step 1 doesn't change any functionality. RenderStyle still generate the
    legacy StyleDifference enum when comparing styles and convert the result
    to the new StyleDifference. Implicit requirements are not handled during
    the conversion.
    
    Converted call sites to use the new StyleDifference according to the
    following conversion rules:
    - diff == StyleDifferenceEqual (&& !context) => diff.hasNoChange()
    - diff == StyleDifferenceRepaint => diff.needsRepaintObjectOnly()
    - diff == StyleDifferenceRepaintLayer => diff.needsRepaintLayer()
    - diff == StyleDifferenceRepaint || diff == StyleDifferenceRepaintLayer => diff.needsRepaintLayer()
    - diff >= StyleDifferenceRepaint => diff.needsRepaint() || diff.needsLayout()
    - diff >= StyleDifferenceRepaintLayer => diff.needsRepaintLayer() || diff.needsLayout()
    - diff > StyleDifferenceRepaintLayer => diff.needsLayout()
    - diff == StyleDifferencePositionedMovementLayoutOnly => diff.needsPositionedMovementLayoutOnly()
    - diff == StyleDifferenceLayout => diff.needsFullLayout()
    
    BUG=358460
    TEST=All existing layout tests.
    R=eseidel@chromium.org, esprehn@chromium.org, jchaffraix@chromium.org
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=171983
    
    Review URL: https://codereview.chromium.org/236203020
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172331 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/style/SVGRenderStyle.h
+++ third_party/WebKit/Source/core/rendering/style/SVGRenderStyle.h
@@ -49,7 +49,7 @@ public:
     void inheritFrom(const SVGRenderStyle*);
     void copyNonInheritedFrom(const SVGRenderStyle*);
 
-    StyleDifference diff(const SVGRenderStyle*) const;
+    StyleDifferenceLegacy diff(const SVGRenderStyle*) const;
 
     bool operator==(const SVGRenderStyle&) const;
     bool operator!=(const SVGRenderStyle& o) const { return !(*this == o); }
