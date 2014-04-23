594951e245833700c41edb22452c05888675940c
tkent@chromium.org <tkent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-23 06:14:46 +0000

    Fix two accessibility audit errors of Autofill edit address overlay.
    - Country selector didn't have any accessibility label.
    - Delete button of DeletableItem didn't have any accessibility label.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/246603002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265550 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options/autofill_options_browsertest.js
+++ chrome/browser/ui/webui/options/autofill_options_browsertest.js
@@ -36,16 +36,11 @@ AutofillEditAddressWebUITest.prototype = {
 
   /**
    * Browse to autofill edit address overlay.
-   **/
+   */
   browsePreload: 'chrome://settings-frame/autofillEditAddress',
 
-  /** @inheritDoc  */
+  /** @override  */
   isAsync: true,
-
-  /**
-   * TODO(tkent): Fix an accessibility error.
-   */
-  runAccessibilityChecks: false,
 };
 
 TEST_F('AutofillEditAddressWebUITest',
