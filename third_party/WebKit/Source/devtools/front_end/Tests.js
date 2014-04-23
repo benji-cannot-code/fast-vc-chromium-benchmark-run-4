264d4d0eb0204e6016535471fb543100c9fa706f
dgozman@chromium.org <dgozman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-23 15:49:35 +0000

    [DevTools] Expose |enableTouchEmulation| method for tests.
    BUG=337142
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/249583002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172384 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/Tests.js
+++ third_party/WebKit/Source/devtools/front_end/Tests.js
@@ -596,6 +596,11 @@ TestSuite.prototype.testTimelineFrames = function()
     test.takeControl();
 }
 
+TestSuite.prototype.enableTouchEmulation = function()
+{
+    WebInspector.targetManager.activeTarget().domModel.emulateTouchEventObjects(true);
+};
+
 // Regression test for http://webk.it/97466
 TestSuite.prototype.testPageOverlayUpdate = function()
 {
