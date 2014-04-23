44aca1c655acf988bce8ab32857ba361f4cea904
aandrey@chromium.org <aandrey@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-23 09:21:50 +0000

    DevTools: Add missing jsdocs for Target members and fix revealed errors.
    This reveals and fixes a REGRESSION introduced in Blink r170439.
    
    R=pfeldman@chromium.org, pfeldman, sergeyv@chromium.org
    
    Review URL: https://codereview.chromium.org/247333004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172349 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/RuntimeModel.js
+++ third_party/WebKit/Source/devtools/front_end/RuntimeModel.js
@@ -143,12 +143,23 @@ WebInspector.RuntimeModel.prototype = {
     },
 
     /**
+     * @param {!RuntimeAgent.RemoteObject} payload
      * @return {!WebInspector.RemoteObject}
      */
     createRemoteObject: function(payload)
     {
         console.assert(typeof payload === "object", "Remote object payload should only be an object");
         return new WebInspector.RemoteObjectImpl(this.target(), payload.objectId, payload.type, payload.subtype, payload.value, payload.description, payload.preview);
+    },
+
+    /**
+     * @param {!RuntimeAgent.RemoteObject} payload
+     * @param {!WebInspector.ScopeRef} scopeRef
+     * @return {!WebInspector.RemoteObject}
+     */
+    createScopeRemoteObject: function(payload, scopeRef)
+    {
+        return new WebInspector.ScopeRemoteObject(this.target(), payload.objectId, scopeRef, payload.type, payload.subtype, payload.value, payload.description, payload.preview);
     },
 
     /**
@@ -162,25 +173,12 @@ WebInspector.RuntimeModel.prototype = {
 
     /**
      * @param {string} name
-     * @param {string} value
+     * @param {number|string|boolean} value
      * @return {!WebInspector.RemoteObjectProperty}
      */
     createRemotePropertyFromPrimitiveValue: function(name, value)
     {
         return new WebInspector.RemoteObjectProperty(name, this.createRemoteObjectFromPrimitiveValue(value));
-    },
-
-    /**
-     * @param {!RuntimeAgent.RemoteObject} payload
-     * @param {!WebInspector.ScopeRef=} scopeRef
-     * @return {!WebInspector.RemoteObject}
-     */
-    createScopedObject: function(payload, scopeRef)
-    {
-        if (scopeRef)
-            return new WebInspector.ScopeRemoteObject(this.target(), payload.objectId, scopeRef, payload.type, payload.subtype, payload.value, payload.description, payload.preview);
-        else
-            return new WebInspector.RemoteObjectImpl(this.target(), payload.objectId, payload.type, payload.subtype, payload.value, payload.description, payload.preview);
     },
 
     __proto__: WebInspector.TargetAwareObject.prototype
