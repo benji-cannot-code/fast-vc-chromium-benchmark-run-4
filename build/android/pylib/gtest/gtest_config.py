229900b712784c7419106d23cee0a9e79aca079e
kjellander@chromium.org <kjellander@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-23 14:03:25 +0000

    WebRTC: Remove neteq_unittests test.
    These tests are being removed in favor for
    the test for NetEQ v4 that are a part of the
    modules_unittests binary.
    
    BUG=webrtc:2996
    NOTRY=True
    
    Review URL: https://codereview.chromium.org/249193002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265633 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/pylib/gtest/gtest_config.py
+++ build/android/pylib/gtest/gtest_config.py
@@ -53,7 +53,6 @@ WEBRTC_NATIVE_TEST_SUITES = [
     'common_video_unittests',
     'modules_tests',
     'modules_unittests',
-    'neteq_unittests',
     'system_wrappers_unittests',
     'test_support_unittests',
     'tools_unittests',
