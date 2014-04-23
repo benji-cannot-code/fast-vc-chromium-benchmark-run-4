a086704083610ccc886b585224493149e8da31ef
hans@chromium.org <hans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-23 00:32:41 +0000

    Remove unnecessary include of atlbase.h
    BUG=82385
    TBR=cpu
    
    Review URL: https://codereview.chromium.org/247973003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265440 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/chrome_main_delegate.cc
+++ chrome/app/chrome_main_delegate.cc
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ui_base_switches.h"
 
 #if defined(OS_WIN)
-#include <atlbase.h>
 #include <malloc.h>
 #include <algorithm>
 #include "base/strings/string_util.h"
