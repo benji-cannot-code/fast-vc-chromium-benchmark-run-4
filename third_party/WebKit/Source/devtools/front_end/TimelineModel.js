6fcf00f72b905e1165b63a18a7d265153fbf6783
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-23 08:50:31 +0000

    TimelinePanel: remove unnecessary fields, getters and setters from TimelineModel.Records
    model field getter was removed
    isRoot field getter was removed
    startTime field setter was removed
    _startTimeOffset field was removed
    calculateAggregatedStats was marked as private
    addWarning was marked as private
    
    BUG=361045
    R=pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/247653002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172346 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/TimelineModel.js
+++ third_party/WebKit/Source/devtools/front_end/TimelineModel.js
@@ -384,7 +384,7 @@ WebInspector.TimelineModel.prototype = {
         for (var i = 0; payload.children && i < payload.children.length; ++i)
             this._innerAddRecord.call(this, payload.children[i], record);
 
-        record.calculateAggregatedStats();
+        record._calculateAggregatedStats();
         if (parentRecord)
             parentRecord._selfTime -= record.endTime - record.startTime;
         return record;
@@ -579,7 +579,6 @@ WebInspector.TimelineModel.Record = function(model, record, parentRecord)
     }
 
     this._selfTime = this.endTime - this.startTime;
-    this._startTimeOffset = this.startTime - model.minimumRecordTime();
 
     if (record.data) {
         if (record.data["url"])
@@ -674,7 +673,7 @@ WebInspector.TimelineModel.Record = function(model, record, parentRecord)
         if (layoutInvalidateStack)
             this.callSiteStackTrace = layoutInvalidateStack;
         if (this.stackTrace)
-            this.addWarning(WebInspector.UIString("Forced synchronous layout is a possible performance bottleneck."));
+            this._addWarning(WebInspector.UIString("Forced synchronous layout is a possible performance bottleneck."));
 
         bindings._layoutInvalidateStack[this.frameId] = null;
         this.highlightQuad = record.data.root || WebInspector.TimelineModel._quadFromRectData(record.data);
@@ -683,7 +682,7 @@ WebInspector.TimelineModel.Record = function(model, record, parentRecord)
 
     case recordTypes.AutosizeText:
         if (record.data.needsRelayout && parentRecord.type === recordTypes.Layout)
-            parentRecord.addWarning(WebInspector.UIString("Layout required two passes due to text autosizing, consider setting viewport."));
+            parentRecord._addWarning(WebInspector.UIString("Layout required two passes due to text autosizing, consider setting viewport."));
         break;
 
     case recordTypes.Paint:
@@ -723,25 +722,9 @@ WebInspector.TimelineModel.Record.prototype = {
         return this._model.target();
     },
 
-    /**
-     * @return {!WebInspector.TimelineModel}
-     */
-    model: function()
-    {
-        return this._model;
-    },
-
     get selfTime()
     {
         return this._selfTime;
-    },
-
-    /**
-     * @return {boolean}
-     */
-    isRoot: function()
-    {
-        return this.type === WebInspector.TimelineModel.RecordType.Root;
     },
 
     /**
@@ -765,7 +748,7 @@ WebInspector.TimelineModel.Record.prototype = {
      */
     title: function()
     {
-        return WebInspector.TimelineUIUtils.recordTitle(this);
+        return WebInspector.TimelineUIUtils.recordTitle(this, this._model);
     },
 
     /**
@@ -773,12 +756,7 @@ WebInspector.TimelineModel.Record.prototype = {
      */
     get startTime()
     {
-        return this._startTime || this._record.startTime;
-    },
-
-    set startTime(startTime)
-    {
-        this._startTime = startTime;
+        return this._record.startTime;
     },
 
     /**
@@ -787,14 +765,6 @@ WebInspector.TimelineModel.Record.prototype = {
     get thread()
     {
         return this._record.thread;
-    },
-
-    /**
-     * @return {number}
-     */
-    get startTimeOffset()
-    {
-        return this._startTimeOffset;
     },
 
     /**
@@ -874,7 +844,7 @@ WebInspector.TimelineModel.Record.prototype = {
         return this._relatedBackendNodeId;
     },
 
-    calculateAggregatedStats: function()
+    _calculateAggregatedStats: function()
     {
         this._aggregatedStats = {};
 
@@ -894,7 +864,7 @@ WebInspector.TimelineModel.Record.prototype = {
     /**
      * @param {string} message
      */
-    addWarning: function(message)
+    _addWarning: function(message)
     {
         if (this._warnings)
             this._warnings.push(message);
