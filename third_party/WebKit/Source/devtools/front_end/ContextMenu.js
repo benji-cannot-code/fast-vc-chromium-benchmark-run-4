180de09fdc96acddeee033910938445923fd835e
lushnikov@chromium.org <lushnikov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-23 10:56:55 +0000

    DevTools: [CodeMirror] fix context menu for text editor
    At present it is impossible to do a right-click for selected text in
    text editor: every time one tries to do this - selections gets disabled.
    
    This patch fixes this behavior by catching contextmenu event in a way
    coordinated with codemirror text editor.
    
    Review URL: https://codereview.chromium.org/247013004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172362 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ContextMenu.js
+++ third_party/WebKit/Source/devtools/front_end/ContextMenu.js
@@ -224,7 +224,7 @@ WebInspector.ContextMenu.prototype = {
             } else {
                 InspectorFrontendHost.showContextMenu(this._event, menuObject);
             }
-            this._event.consume();
+            this._event.consume(true);
         }
     },
 
