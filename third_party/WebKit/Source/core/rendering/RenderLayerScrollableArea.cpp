bbe0e6d759e76130ffe7d61b8aefb679ded1ae56
weiliangc@chromium.org <weiliangc@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-23 21:43:24 +0000

    Enable Scroll Animation for RenderLayerScrollableArea
    In Blink, only FrameView scrollbars animation is enabled and serviced.
    This patch enables scroll animation for non-mainframe scrollbars (
    RenderLayerScrollableArea) and service them.
    
    BUG=575, 299059, 307578, 359028
    
    Review URL: https://codereview.chromium.org/214953004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172418 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderLayerScrollableArea.cpp
+++ third_party/WebKit/Source/core/rendering/RenderLayerScrollableArea.cpp
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/editing/FrameSelection.h"
 #include "core/frame/FrameView.h"
 #include "core/frame/LocalFrame.h"
+#include "core/frame/Settings.h"
 #include "core/html/HTMLFrameOwnerElement.h"
 #include "core/inspector/InspectorInstrumentation.h"
 #include "core/page/EventHandler.h"
@@ -477,6 +478,20 @@ IntSize RenderLayerScrollableArea::overhangAmount() const
 IntPoint RenderLayerScrollableArea::lastKnownMousePosition() const
 {
     return m_box->frame() ? m_box->frame()->eventHandler().lastKnownMousePosition() : IntPoint();
+}
+
+bool RenderLayerScrollableArea::scrollAnimatorEnabled() const
+{
+    return m_box->frame()->settings() && m_box->frame()->settings()->scrollAnimatorEnabled();
+}
+
+bool RenderLayerScrollableArea::scheduleAnimation()
+{
+    if (HostWindow* window = m_box->frameView()->hostWindow()) {
+        window->scheduleAnimation();
+        return true;
+    }
+    return false;
 }
 
 bool RenderLayerScrollableArea::shouldSuspendScrollAnimations() const
