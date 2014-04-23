06ef68c57f7c1caee345bb818dcb1d94c2adcaaa
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-23 07:10:58 +0000

    Revert "Move Files.app files to ui/file_manager"
    > Move Files.app files to ui/file_manager
    >
    > BUG=345171
    > TEST=manually tested
    > R=hirono@chromium.org
    >
    > Review URL: https://codereview.chromium.org/247123002
    
    TBR=yoshiki@chromium.org
    
    Review URL: https://codereview.chromium.org/246533008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265561 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/audio_player/js/audio_player_scripts.js
+++ chrome/browser/resources/file_manager/audio_player/js/audio_player_scripts.js
@@ -3,9 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-<include src="../../../../webui/resources/js/cr.js"/>
-<include src="../../../../webui/resources/js/cr/event_target.js"/>
-<include src="../../../../webui/resources/js/cr/ui/array_data_model.js"/>
+<include src="../../../../../../ui/webui/resources/js/cr.js"/>
+<include src="../../../../../../ui/webui/resources/js/cr/event_target.js"/>
+<include src="../../../../../../ui/webui/resources/js/cr/ui/array_data_model.js"/>
 
 // Hack for polymer, notifying that CSP is enabled here.
 // TODO(yoshiki): Find a way to remove the hack.
@@ -18,8 +18,8 @@ if (!('allowsEval' in document.securityPolicy))
 Object['observe'] = undefined;
 Object['unobserve'] = undefined;
 
-<include src="../../../../../third_party/polymer/platform/platform.js">
-<include src="../../../../../third_party/polymer/polymer/polymer.js">
+<include src="../../../../../../third_party/polymer/platform/platform.js">
+<include src="../../../../../../third_party/polymer/polymer/polymer.js">
 
 (function() {
 
--- ui/file_manager/video_player/js/video_player_scripts.js
+++ ui/file_manager/video_player/js/video_player_scripts.js
@@ -17,9 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 (function() {
 'use strict';
 
-//<include src="../../file_manager/common/js/util.js"/>
-//<include src="../../file_manager/foreground/js/media/media_controls.js"/>
-//<include src="../../file_manager/foreground/js/media/util.js"/>
+//<include src="../../../../chrome/browser/resources/file_manager/common/js/util.js"/>
+//<include src="../../../../chrome/browser/resources/file_manager/foreground/js/media/media_controls.js"/>
+//<include src="../../../../chrome/browser/resources/file_manager/foreground/js/media/util.js"/>
 
 //<include src="video_player.js"/>
 
