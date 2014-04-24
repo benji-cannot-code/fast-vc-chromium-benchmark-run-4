40915ecc36bdec29e69b34c95c37e7405411c4d1
nyquist@chromium.org <nyquist@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-24 00:21:20 +0000

    Simplify DistillerPage and change API towards platform dependent code.
    This CL removes simplifies the generic DistillerPage and moves more logic
    to the platform specific implementation in DistillerPageWebContents.
    
    The DistillerPage is now only responsible for:
    - Storing the callback when kicking off a distillation.
    - Providing the JS to use for distillation.
    - Parsing the returned content (base::Value) and calling the callback.
    
    Depends on: https://codereview.chromium.org/231933005/
    
    BUG=361939
    
    Review URL: https://codereview.chromium.org/226613013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265787 0039d316-1c4b-4281-b951-d872f2087c98

--- components/dom_distiller/core/distiller.cc
+++ components/dom_distiller/core/distiller.cc
@@ -40,7 +40,6 @@ DistillerFactoryImpl::~DistillerFactoryImpl() {}
 scoped_ptr<Distiller> DistillerFactoryImpl::CreateDistiller() {
   scoped_ptr<DistillerImpl> distiller(new DistillerImpl(
       *distiller_page_factory_, *distiller_url_fetcher_factory_));
-  distiller->Init();
   return distiller.PassAs<Distiller>();
 }
 
@@ -60,11 +59,6 @@ DistillerImpl::DistillerImpl(
 
 DistillerImpl::~DistillerImpl() {
   DCHECK(destruction_allowed_);
-}
-
-void DistillerImpl::Init() {
-  DCHECK(AreAllPagesFinished());
-  distiller_page_->Init();
 }
 
 void DistillerImpl::SetMaxNumPagesInArticle(size_t max_num_pages) {
