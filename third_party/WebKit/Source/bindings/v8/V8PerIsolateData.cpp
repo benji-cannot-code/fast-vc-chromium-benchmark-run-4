adc730d45588bbe5fcb330c1d803ff0303da3df4
jochen@chromium.org <jochen@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-24 09:30:36 +0000

    Reland r172366 - Switch to use per isolate CallCompletedCallbacks"
    Don't install the callbacks during unit tests.
    
    > Switch to use per isolate CallCompletedCallbacks
    >
    > BUG=none
    > R=adamk@chromium.org
    >
    > Review URL: https://codereview.chromium.org/247623006/
    
    R=haraken@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/256523002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172474 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/V8PerIsolateData.cpp
+++ third_party/WebKit/Source/bindings/v8/V8PerIsolateData.cpp
@@ -34,12 +34,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/v8/V8Binding.h"
 #include "bindings/v8/V8HiddenValue.h"
 #include "bindings/v8/V8ObjectConstructor.h"
+#include "bindings/v8/V8RecursionScope.h"
 #include "bindings/v8/V8ScriptRunner.h"
+#include "public/platform/Platform.h"
 #include "wtf/MainThread.h"
 
 namespace WebCore {
 
 static V8PerIsolateData* mainThreadPerIsolateData = 0;
+
+#ifndef NDEBUG
+static void assertV8RecursionScope()
+{
+    ASSERT(V8RecursionScope::properlyUsed(v8::Isolate::GetCurrent()));
+}
+#endif
 
 V8PerIsolateData::V8PerIsolateData(v8::Isolate* isolate)
     : m_isolate(isolate)
@@ -54,6 +63,11 @@ V8PerIsolateData::V8PerIsolateData(v8::Isolate* isolate)
     , m_gcEventData(adoptPtr(new GCEventData()))
     , m_performingMicrotaskCheckpoint(false)
 {
+#ifndef NDEBUG
+    // currentThread will always be non-null in production, but can be null in Chromium unit tests.
+    if (blink::Platform::current()->currentThread())
+        isolate->AddCallCompletedCallback(&assertV8RecursionScope);
+#endif
     if (isMainThread()) {
         mainThreadPerIsolateData = this;
         PageScriptDebugServer::setMainThreadIsolate(isolate);
@@ -93,6 +107,10 @@ v8::Persistent<v8::Value>& V8PerIsolateData::ensureLiveRoot()
 
 void V8PerIsolateData::dispose(v8::Isolate* isolate)
 {
+#ifndef NDEBUG
+    if (blink::Platform::current()->currentThread())
+        isolate->RemoveCallCompletedCallback(&assertV8RecursionScope);
+#endif
     void* data = isolate->GetData(gin::kEmbedderBlink);
     delete static_cast<V8PerIsolateData*>(data);
     isolate->SetData(gin::kEmbedderBlink, 0);
