bce76287d4ddd046f90a7eecec8785b8756e5289
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-24 23:33:33 +0000

    Automatically call OnNativeThemeChanged when a widget is added to a hierarchy with a widget.
    Split off from https://codereview.chromium.org/245863002/
    
    BUG=347832 (tangentially)
    
    Review URL: https://codereview.chromium.org/248073005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266042 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/view.cc
+++ ui/views/view.cc
@@ -218,8 +218,9 @@ void View::AddChildViewAt(View* view, int index) {
 
   // If |view| has a parent, remove it from its parent.
   View* parent = view->parent_;
-  const ui::NativeTheme* old_theme = view->GetNativeTheme();
+  ui::NativeTheme* old_theme = NULL;
   if (parent) {
+    old_theme = view->GetNativeTheme();
     if (parent == this) {
       ReorderChildView(view, index);
       return;
@@ -234,6 +235,13 @@ void View::AddChildViewAt(View* view, int index) {
   view->parent_ = this;
   children_.insert(children_.begin() + index, view);
 
+  views::Widget* widget = GetWidget();
+  if (widget) {
+    const ui::NativeTheme* new_theme = view->GetNativeTheme();
+    if (new_theme != old_theme)
+      view->PropagateNativeThemeChanged(new_theme);
+  }
+
   ViewHierarchyChangedDetails details(true, this, view, parent);
 
   for (View* v = this; v; v = v->parent_)
@@ -241,12 +249,9 @@ void View::AddChildViewAt(View* view, int index) {
 
   view->PropagateAddNotifications(details);
   UpdateTooltip();
-  views::Widget* widget = GetWidget();
   if (widget) {
     RegisterChildrenForVisibleBoundsNotification(view);
-    const ui::NativeTheme* new_theme = widget->GetNativeTheme();
-    if (new_theme != old_theme)
-      PropagateNativeThemeChanged(new_theme);
+
     if (view->visible())
       view->SchedulePaint();
   }
