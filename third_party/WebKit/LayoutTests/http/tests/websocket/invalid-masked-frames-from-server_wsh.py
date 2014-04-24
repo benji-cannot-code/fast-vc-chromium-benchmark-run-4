b892af14bafe2936c5ea6339d4678a072c6006dc
ricea@chromium.org <ricea@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-24 08:56:29 +0000

    Apply pyformat to WebSocket layout tests
    Applied pyformat to WebSocket layout tests to bring them closer to pep8
    compliance.
    
    Command-line used was
    
    pyformat -s4 -r -i --force_quote_type single --ignore 'E501' .
    
    It doesn't seem to be possible to completely prevent pyformat from
    wrapping long lines, so some changes were reverted by hand.
    
    BUG=237056
    TEST=ran layout tests
    
    Review URL: https://codereview.chromium.org/246293012
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172470 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/websocket/invalid-masked-frames-from-server_wsh.py
+++ third_party/WebKit/LayoutTests/http/tests/websocket/invalid-masked-frames-from-server_wsh.py
@@ -1,8 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 from mod_pywebsocket import stream
 
+
 def web_socket_do_extra_handshake(request):
     pass
+
 
 def web_socket_transfer_data(request):
     # pywebsocket does not mask message by default. We need to build a frame manually to mask it.
