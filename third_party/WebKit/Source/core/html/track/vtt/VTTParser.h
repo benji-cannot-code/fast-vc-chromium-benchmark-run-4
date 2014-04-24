ac4dbebe9a7bd73412eec604711ebb66b8d18f9c
philipj@opera.com <philipj@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-24 18:24:30 +0000

    Remove RuntimeEnabledFeatures.h includes from a few headers
    Changing RuntimeEnabledFeatures.in requires many files to be recompiled.
    Getting rid of these unnecessary includes didn't help much, but still...
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/250583003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172527 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/track/vtt/VTTParser.h
+++ third_party/WebKit/Source/core/html/track/vtt/VTTParser.h
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define VTTParser_h
 
 #include "HTMLNames.h"
-#include "RuntimeEnabledFeatures.h"
 #include "core/dom/DocumentFragment.h"
 #include "core/html/parser/TextResourceDecoder.h"
 #include "core/html/track/vtt/BufferedLineReader.h"
--- third_party/WebKit/Source/platform/animation/TimingFunction.h
+++ third_party/WebKit/Source/platform/animation/TimingFunction.h
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef TimingFunction_h
 #define TimingFunction_h
 
-#include "RuntimeEnabledFeatures.h"
 #include "platform/animation/AnimationUtilities.h" // For blend()
 #include "platform/animation/UnitBezier.h"
 #include "wtf/OwnPtr.h"
