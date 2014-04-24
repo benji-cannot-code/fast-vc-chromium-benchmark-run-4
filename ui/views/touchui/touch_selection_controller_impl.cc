e5c2326577a7afcfadd7e7c5c60c62307c9fea74
tapted@chromium.org <tapted@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-24 08:32:07 +0000

    MacViews: Better layering for the touch selection controller
    This CL moves ViewsTouchSelectionControllerFactory out of
    views/touchui/touch_selection_controller_impl.cc and into its own file
    one level higher.
    
    Result: the single touchui dependency moves from views_delegate.cc to a
    file that can be controlled on a per-platform basis. The touchui
    subfolder assumes aura is available. This allows touchui to be excluded
    from a build that doesn't need it.
    
    BUG=366007
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=265814
    
    Review URL: https://codereview.chromium.org/248763003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265882 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/touchui/touch_selection_controller_impl.cc
+++ ui/views/touchui/touch_selection_controller_impl.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/ui_resources.h"
 #include "grit/ui_strings.h"
 #include "ui/base/resource/resource_bundle.h"
-#include "ui/base/ui_base_switches_util.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/image/image.h"
 #include "ui/gfx/path.h"
@@ -608,16 +607,6 @@ bool TouchSelectionControllerImpl::IsSelectionHandle2Visible() {
 
 bool TouchSelectionControllerImpl::IsCursorHandleVisible() {
   return cursor_handle_->IsWidgetVisible();
-}
-
-ViewsTouchSelectionControllerFactory::ViewsTouchSelectionControllerFactory() {
-}
-
-ui::TouchSelectionController* ViewsTouchSelectionControllerFactory::create(
-    ui::TouchEditable* client_view) {
-  if (switches::IsTouchEditingEnabled())
-    return new views::TouchSelectionControllerImpl(client_view);
-  return NULL;
 }
 
 }  // namespace views
--- ui/views/touchui/touch_selection_controller_impl_unittest.cc
+++ ui/views/touchui/touch_selection_controller_impl_unittest.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/textfield/textfield.h"
 #include "ui/views/test/views_test_base.h"
 #include "ui/views/touchui/touch_selection_controller_impl.h"
+#include "ui/views/views_touch_selection_controller_factory.h"
 #include "ui/views/widget/widget.h"
 
 using base::ASCIIToUTF16;
