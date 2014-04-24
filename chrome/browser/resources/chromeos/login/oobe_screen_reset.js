498e08793574402b87d232416b0a0a9e48388ff9
merkulova@chromium.org <merkulova@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-24 11:29:42 +0000

    Fix for help-app article link on OOBE reset screen.
    BUG=297874
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/245013002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265905 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/login/oobe_screen_reset.js
+++ chrome/browser/resources/chromeos/login/oobe_screen_reset.js
@@ -14,6 +14,9 @@ login.createScreen('ResetScreen', 'reset', function() {
       $('reset-powerwash-help-link-on-rollback').addEventListener(
           'click', function(event) {
         chrome.send('resetOnLearnMore');
+      $('powerwash-help-link').addEventListener(
+          'click', function(event) {
+        chrome.send('resetOnLearnMore');
       });
     },
 
