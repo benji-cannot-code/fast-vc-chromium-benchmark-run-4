fa8fed0d2de5566a6780822092a50e1989b1c6a9
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-24 05:18:10 +0000

    Remove title and icon from chrome apps native style title bars.
    These windows aren't meant to have titles or icons, but still need to
    get a proper title as far as the OS is concerned. This means using a
    custom but glass frame, similarly to how unthemed browser windows look.
    
    BUG=339558
    TEST=Try out frame:chrome app windows windows on Windows 7, 8 and XP.
    Use the windows in various states (maximised, fullscreen, normal).
    Make sure they work after switching in and out of aero mode.
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=262516
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=265598
    
    Review URL: https://codereview.chromium.org/213743017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265860 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/apps/shaped_app_window_targeter_unittest.cc
+++ chrome/browser/ui/views/apps/shaped_app_window_targeter_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/apps/shaped_app_window_targeter.h"
 
+#include "apps/ui/views/app_window_frame_view.h"
 #include "chrome/browser/ui/views/apps/chrome_native_app_window_views.h"
 #include "ui/aura/test/aura_test_base.h"
 #include "ui/aura/window.h"
@@ -185,9 +186,10 @@ TEST_F(ShapedAppWindowTargeterTest, ResizeInsetsWithinBounds) {
     EXPECT_EQ(window, move.target());
   }
 
-  // The EasyResizeTargeter specifies an inset of 5px within the window.
-  app_window_views()->InstallEasyResizeTargeterOnContainer();
-
+#if !defined(OS_CHROMEOS)
+  // The non standard app frame has a easy resize targetter installed.
+  scoped_ptr<apps::AppWindowFrameView> frame(
+      app_window_views()->CreateNonStandardAppFrame());
   {
     // Ensure that the window has an event targeter (there should be an
     // EasyResizeWindowTargeter installed).
@@ -224,4 +226,5 @@ TEST_F(ShapedAppWindowTargeterTest, ResizeInsetsWithinBounds) {
     ASSERT_FALSE(details.dispatcher_destroyed);
     EXPECT_EQ(window, move.target());
   }
+#endif  // defined (OS_CHROMEOS)
 }
