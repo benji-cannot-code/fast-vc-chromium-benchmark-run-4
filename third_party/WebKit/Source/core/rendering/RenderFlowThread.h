1953aa9f66be59abd1d97f4480aea2fee1f32d19
mstensho@opera.com <mstensho@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-24 23:07:39 +0000

    Remove unused methods and variables in RenderFlowThread.
    Review URL: https://codereview.chromium.org/253393004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172549 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderFlowThread.h
+++ third_party/WebKit/Source/core/rendering/RenderFlowThread.h
@@ -81,14 +81,11 @@ public:
     void invalidateRegions();
     bool hasValidRegionInfo() const { return !m_regionsInvalidated && !m_regionList.isEmpty(); }
 
-    static PassRefPtr<RenderStyle> createFlowThreadStyle(RenderStyle* parentStyle);
-
     void repaintRectangleInRegions(const LayoutRect&) const;
 
     LayoutPoint adjustedPositionRelativeToOffsetParent(const RenderBoxModelObject&, const LayoutPoint&);
 
     LayoutUnit pageLogicalTopForOffset(LayoutUnit);
-    LayoutUnit pageLogicalWidthForOffset(LayoutUnit);
     LayoutUnit pageLogicalHeightForOffset(LayoutUnit);
     LayoutUnit pageRemainingLogicalHeightForOffset(LayoutUnit, PageBoundaryRule = IncludePageBoundary);
 
@@ -107,9 +104,6 @@ public:
 
     RenderRegion* firstRegion() const;
     RenderRegion* lastRegion() const;
-
-    bool previousRegionCountChanged() const { return m_previousRegionCount != m_regionList.size(); }
-    void updatePreviousRegionCount() { m_previousRegionCount = m_regionList.size(); }
 
     void setRegionRangeForBox(const RenderBox*, LayoutUnit offsetFromLogicalTopOfFirstPage);
     void getRegionRangeForBox(const RenderBox*, RenderRegion*& startRegion, RenderRegion*& endRegion) const;
@@ -151,7 +145,6 @@ protected:
     const RenderBox* currentStatePusherRenderBox() const;
 
     RenderRegionList m_regionList;
-    unsigned short m_previousRegionCount;
 
     class RenderRegionRange {
     public:
