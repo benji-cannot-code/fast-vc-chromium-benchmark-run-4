a3c61c5a5ab0627131d1157acd74ea54d68d5f3d
tapted@chromium.org <tapted@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-24 01:48:20 +0000

    MacViews: Better layering for the touch selection controller
    This CL moves ViewsTouchSelectionControllerFactory out of
    views/touchui/touch_selection_controller_impl.cc and into its own file
    one level higher.
    
    Result: the single touchui dependency moves from views_delegate.cc to a
    file that can be controlled on a per-platform basis. The touchui
    subfolder assumes aura is available. This allows touchui to be excluded
    from a build that doesn't need it.
    
    BUG=366007
    
    Review URL: https://codereview.chromium.org/248763003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265814 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/touchui/touch_selection_controller_impl_unittest.cc
+++ ui/views/touchui/touch_selection_controller_impl_unittest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/render_text.h"
 #include "ui/views/controls/textfield/textfield.h"
 #include "ui/views/test/views_test_base.h"
+#include "ui/views/touch_selection_controller_factory.h"
 #include "ui/views/touchui/touch_selection_controller_impl.h"
 #include "ui/views/widget/widget.h"
 
