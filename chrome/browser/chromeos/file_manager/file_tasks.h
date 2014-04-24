420ad5bff973f8d2c3921f1917b14a5cb6c0b341
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-24 05:25:20 +0000

    Adjust to the new Files.app path in comment and watchlist.
    BUG=345171
    TEST=none
    R=mtomasz@chromium.org
    TBR=jochen@chromium.org
    # TBRing for modifying comments.
    
    Review URL: https://codereview.chromium.org/250383003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265864 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/file_manager/file_tasks.h
+++ chrome/browser/chromeos/file_manager/file_tasks.h
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // This information also comes from FileBrowserHandler::GetHandlers().
 //
 // See also:
-// chrome/browser/resources/file_manager/manifest.json
+// ui/file_manager/file_manager/manifest.json
 //
 // 3) Drive app, which is a hosted app (i.e. just web site), that can work
 // with Drive (ex. Pixlr Editor). This information comes from
@@ -105,7 +105,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // should be simplified: crbug.com/267313
 //
 // See also:
-// chrome/browser/resources/file_manager/js/file_tasks.js
+// ui/file_manager/file_manager/js/file_tasks.js
 //
 
 #ifndef CHROME_BROWSER_CHROMEOS_FILE_MANAGER_FILE_TASKS_H_
