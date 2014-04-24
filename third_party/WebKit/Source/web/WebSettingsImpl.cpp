759c03912907648ea18dcc30adbca611f6c90a51
tyoshino@chromium.org <tyoshino@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-24 04:53:49 +0000

    Add a WebSettings item to turn off mixed content check for WebSocket
    https://codereview.chromium.org/222153002/ will ban use of insecure
    WebSockets from HTTPS origin. To give time for app developers to fix their
    app and roll-out fixed version widely to get ready for this change, we'll
    provide a WebSettings item to turn off the security check.
    
    To limit this exception to WebSockets, I added a new item that controls
    only WebSockets.
    
    After this CL is landed, I'll land
    https://codereview.chromium.org/248863003/ and then
    https://codereview.chromium.org/222153002/
    
    BUG=85271
    
    Review URL: https://codereview.chromium.org/246893014
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172454 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebSettingsImpl.cpp
+++ third_party/WebKit/Source/web/WebSettingsImpl.cpp
@@ -577,6 +577,11 @@ void WebSettingsImpl::setAllowRunningOfInsecureContent(bool enabled)
     m_settings->setAllowRunningOfInsecureContent(enabled);
 }
 
+void WebSettingsImpl::setAllowConnectingInsecureWebSocket(bool enabled)
+{
+    m_settings->setAllowConnectingInsecureWebSocket(enabled);
+}
+
 void WebSettingsImpl::setPasswordEchoEnabled(bool flag)
 {
     m_settings->setPasswordEchoEnabled(flag);
