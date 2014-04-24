f905642f767c300a93f5dc18987da2747508fe00
tnagel@chromium.org <tnagel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-24 23:05:02 +0000

    Some cleanup of WallpaperManager.
    * Add const, static and/or private in some places.
    * Integrate ProcessCustomWallpaper() into SetCustomWallpaper() to avoid PostTask() and deep copy of wallpaper in case it is not persistent.
    * Replace UserImage by gfx::ImageSkia in some places to simplify code and to avoid useless temporary objects.
    * Remove unused DAILY User::WallpaperType.
    * Remove unused GetOriginalWallpaperPathForUser().
    * Replace SetLastSelectedUser() by direct member access.
    * Add initialization of WallpaperInfo.date that was missing in one case.
    * Rename all ImageSkia method parameters to "image" and all UserImage method parameters to "user_image" for consistency.
    * Rename ResizeWallpaper() to ResizeImage().
    * Improve some comments.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/221873005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266026 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/extensions/wallpaper_private_api.h
+++ chrome/browser/chromeos/extensions/wallpaper_private_api.h
@@ -43,7 +43,7 @@ class WallpaperPrivateSetWallpaperIfExistsFunction
   virtual bool RunImpl() OVERRIDE;
 
  private:
-  virtual void OnWallpaperDecoded(const gfx::ImageSkia& wallpaper) OVERRIDE;
+  virtual void OnWallpaperDecoded(const gfx::ImageSkia& image) OVERRIDE;
 
   // File doesn't exist. Sets javascript callback parameter to false.
   void OnFileNotExists(const std::string& error);
@@ -78,13 +78,13 @@ class WallpaperPrivateSetWallpaperFunction : public WallpaperFunctionBase {
   virtual bool RunImpl() OVERRIDE;
 
  private:
-  virtual void OnWallpaperDecoded(const gfx::ImageSkia& wallpaper) OVERRIDE;
+  virtual void OnWallpaperDecoded(const gfx::ImageSkia& image) OVERRIDE;
 
   // Saves the image data to a file.
   void SaveToFile();
 
   // Sets wallpaper to the decoded image.
-  void SetDecodedWallpaper(scoped_ptr<gfx::ImageSkia> wallpaper);
+  void SetDecodedWallpaper(scoped_ptr<gfx::ImageSkia> image);
 
   scoped_ptr<extensions::api::wallpaper_private::SetWallpaper::Params> params;
 
--- chrome/browser/chromeos/login/user_manager_impl.h
+++ chrome/browser/chromeos/login/user_manager_impl.h
@@ -315,7 +315,7 @@ class UserManagerImpl
   // Update the global LoginState.
   void UpdateLoginState();
 
-  // Insert |user| at the front of the LRU user list..
+  // Insert |user| at the front of the LRU user list.
   void SetLRUUser(User* user);
 
   // Adds |user| to users list, and adds it to front of LRU list. It is assumed
