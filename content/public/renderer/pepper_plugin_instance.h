1cbef428ca052bd314be0a1c2ba2f6b17f6462f8
teravest@chromium.org <teravest@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-24 23:43:26 +0000

    Pepper: Move IME workaround from the trusted plugin.
    This moves a MACOSX-specific IME workaround outside the trusted plugin as part
    of the effort to refactor all that code to components/nacl.
    
    BUG=239656
    
    Review URL: https://codereview.chromium.org/250063003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266050 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/renderer/pepper_plugin_instance.h
+++ content/public/renderer/pepper_plugin_instance.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/c/pp_resource.h"
 #include "ppapi/c/pp_var.h"
 #include "ppapi/c/private/ppb_instance_private.h"
+#include "ui/base/ime/text_input_type.h"
 
 class GURL;
 
@@ -111,6 +112,9 @@ class PepperPluginInstance {
 
   // Sets the link currently under the cursor.
   virtual void SetLinkUnderCursor(const std::string& url) = 0;
+
+  // Sets the text input type for this plugin.
+  virtual void SetTextInputType(ui::TextInputType type) = 0;
 };
 
 }  // namespace content
