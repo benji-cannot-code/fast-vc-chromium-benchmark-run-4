deee88698296e13c58729f0cf5e498d8476be5a0
robhogan@gmail.com <robhogan@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-24 18:39:15 +0000

    Floats Don't Overhang Their Block Formatting Context Per http://www.w3.org/TR/CSS2/visuren.html#float-position: "References to [the elements] in the [rules that govern the behaviour of floats] refer only to other elements in the same block formatting context as the float."
    This means we don't need to look for intruding or overhanging floats
    in boxes that establish a new block formatting context. I couldn't
    find a test where we failed this rule but we do appear to be doing
    unnecessary work in rebuildFloatsFromIntruding() so make sure we
    avoid adding floats from different block formatting contexts.
    
    The next step here is to merge the appropriate parts of avoidFloats()
    and createsBlockFormattingContext() as they are frequently synonymous.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/237803002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172530 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderBlock.cpp
+++ third_party/WebKit/Source/core/rendering/RenderBlock.cpp
@@ -1444,7 +1444,7 @@ void RenderBlock::addVisualOverflowFromTheme()
 
 bool RenderBlock::createsBlockFormattingContext() const
 {
-    return isInlineBlockOrInlineTable() || isFloatingOrOutOfFlowPositioned() || hasOverflowClip() || (parent() && parent()->isFlexibleBoxIncludingDeprecated())
+    return isInlineBlockOrInlineTable() || isFloatingOrOutOfFlowPositioned() || hasOverflowClip() || isFlexItemIncludingDeprecated()
         || style()->specifiesColumns() || isRenderFlowThread() || isTableCell() || isTableCaption() || isFieldset() || isWritingModeRoot() || isDocumentElement() || style()->columnSpan();
 }
 
