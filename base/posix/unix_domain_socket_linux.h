1cfff7fa34ba7155fde95fa0c783d7cfa58c0504
mdempsky@chromium.org <mdempsky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-24 23:42:42 +0000

    Extend UnixDomainSocket::RecvMsg to return sender's PID
    BUG=357670
    
    Review URL: https://codereview.chromium.org/241863002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266048 0039d316-1c4b-4281-b951-d872f2087c98

--- base/posix/unix_domain_socket_linux.h
+++ base/posix/unix_domain_socket_linux.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/base_export.h"
+#include "base/process/process_handle.h"
 
 class Pickle;
 
@@ -18,6 +19,11 @@ class BASE_EXPORT UnixDomainSocket {
  public:
   // Maximum number of file descriptors that can be read by RecvMsg().
   static const size_t kMaxFileDescriptors;
+
+  // Use to enable receiving process IDs in RecvMsgWithPid.  Should be called on
+  // the receiving socket (i.e., the socket passed to RecvMsgWithPid). Returns
+  // true if successful.
+  static bool EnableReceiveProcessId(int fd);
 
   // Use sendmsg to write the given msg and include a vector of file
   // descriptors. Returns true if successful.
@@ -32,6 +38,16 @@ class BASE_EXPORT UnixDomainSocket {
                          void* msg,
                          size_t length,
                          std::vector<int>* fds);
+
+  // Same as RecvMsg above, but also returns the sender's process ID (as seen
+  // from the caller's namespace).  However, before using this function to
+  // receive process IDs, EnableReceiveProcessId() should be called on the
+  // receiving socket.
+  static ssize_t RecvMsgWithPid(int fd,
+                                void* msg,
+                                size_t length,
+                                std::vector<int>* fds,
+                                base::ProcessId* pid);
 
   // Perform a sendmsg/recvmsg pair.
   //   1. This process creates a UNIX SEQPACKET socketpair. Using
@@ -71,7 +87,8 @@ class BASE_EXPORT UnixDomainSocket {
                                   void* msg,
                                   size_t length,
                                   int flags,
-                                  std::vector<int>* fds);
+                                  std::vector<int>* fds,
+                                  base::ProcessId* pid);
 };
 
 #endif  // BASE_POSIX_UNIX_DOMAIN_SOCKET_LINUX_H_
