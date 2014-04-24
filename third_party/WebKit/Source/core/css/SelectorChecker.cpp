e4adb66e1abb7df39b5a995c70a6348b272344c3
tasak@google.com <tasak@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-24 12:49:17 +0000

    Added previousElement to SelectorCheckingContext.
    To find whether a given last matched element should be considered
    as a shadow host or not, added previousElement:
    
    - the last compound selector of a given complex selector matched the
      previousElement,
    
    e.g. suppose that we have a complex selector: "div#foo > .bar span".
    In the case, if context.element matches "div#foo", context.previousElement
    matches ".bar span".
    
    If a given selector doesn't have any combinator, context.previousElement is
    null.
    
    If context.previousElement is null or context.previousElement's treeScope
    is different from context.element's treeScope, the context.element is obtained
    by walking up from its descendant treeScope.
    
    TEST=fast/dom/shadow/content-pseudo-element-with-nested-shadow.html
    BUG=360679
    
    Review URL: https://codereview.chromium.org/247043002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172492 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/SelectorChecker.cpp
+++ third_party/WebKit/Source/core/css/SelectorChecker.cpp
@@ -112,7 +112,7 @@ bool SelectorChecker::scopeContainsLastMatchedElement(const SelectorCheckingCont
         return context.scope->contains(context.element);
 
     // If a given element is scope, i.e. shadow host, matches.
-    if (context.element == context.scope)
+    if (context.element == context.scope && (!context.previousElement || context.previousElement->isInDescendantTreeOf(context.element)))
         return true;
 
     ShadowRoot* root = context.element->containingShadowRoot();
@@ -256,6 +256,7 @@ template<typename SiblingTraversalStrategy>
 SelectorChecker::Match SelectorChecker::matchForRelation(const SelectorCheckingContext& context, const SiblingTraversalStrategy& siblingTraversalStrategy, MatchResult* result) const
 {
     SelectorCheckingContext nextContext = prepareNextContextForRelation(context);
+    nextContext.previousElement = context.element;
 
     CSSSelector::Relation relation = context.selector->relation();
 
--- third_party/WebKit/Source/core/css/SelectorChecker.h
+++ third_party/WebKit/Source/core/css/SelectorChecker.h
@@ -65,6 +65,7 @@ public:
         SelectorCheckingContext(const CSSSelector& selector, Element* element, VisitedMatchType visitedMatchType)
             : selector(&selector)
             , element(element)
+            , previousElement(0)
             , scope(0)
             , visitedMatchType(visitedMatchType)
             , pseudoId(NOPSEUDO)
@@ -79,6 +80,7 @@ public:
 
         const CSSSelector* selector;
         Element* element;
+        Element* previousElement;
         const ContainerNode* scope;
         VisitedMatchType visitedMatchType;
         PseudoId pseudoId;
