7d5f6d7fdfbdcecf85c20ce16dea66f118dbc16a
flackr@chromium.org <flackr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-24 23:33:04 +0000

    Reland Remove remaining window states where window is shown with opacity 0. https://codereview.chromium.org/237963019/
    BUG=351553
    
    Review URL: https://codereview.chromium.org/258533003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266041 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/overview/window_overview.cc
+++ ash/wm/overview/window_overview.cc
@@ -161,8 +161,8 @@ WindowOverview::~WindowOverview() {
         ScopedTransformOverviewWindow::kTransitionMilliseconds));
     settings.SetPreemptionStrategy(
         ui::LayerAnimator::IMMEDIATELY_ANIMATE_TO_NEW_TARGET);
-    (*iter)->Show();
     (*iter)->layer()->SetOpacity(1);
+    (*iter)->Show();
   }
   if (cursor_client_)
     cursor_client_->UnlockCursor();
@@ -218,10 +218,13 @@ void WindowOverview::SetSelection(size_t index) {
       old_selection->SetBoundsInScreen(
           GetSelectionBounds(selection_index_) + fade_out_direction,
           dst_display);
+      old_selection->Hide();
       old_selection->layer()->SetOpacity(0);
       InitializeSelectionWidget();
       selection_widget_->GetNativeWindow()->SetBoundsInScreen(
           target_bounds - fade_out_direction, dst_display);
+      // New selection widget starts with 0 opacity and fades in.
+      selection_widget_->GetNativeWindow()->layer()->SetOpacity(0);
     }
     ui::ScopedLayerAnimationSettings animation_settings(
         selection_widget_->GetNativeWindow()->layer()->GetAnimator());
@@ -235,8 +238,6 @@ void WindowOverview::SetSelection(size_t index) {
   } else {
     InitializeSelectionWidget();
     selection_widget_->SetBounds(target_bounds);
-    selection_widget_->GetNativeWindow()->layer()->SetOpacity(
-        kWindowOverviewSelectionOpacity);
   }
   selection_index_ = index;
 }
@@ -448,7 +449,8 @@ void WindowOverview::InitializeSelectionWidget() {
   selection_widget_->Show();
   selection_widget_->GetNativeWindow()->parent()->StackChildAtBottom(
       selection_widget_->GetNativeWindow());
-  selection_widget_->GetNativeWindow()->layer()->SetOpacity(0);
+  selection_widget_->GetNativeWindow()->layer()->SetOpacity(
+      kWindowOverviewSelectionOpacity);
 }
 
 gfx::Rect WindowOverview::GetSelectionBounds(size_t index) {
