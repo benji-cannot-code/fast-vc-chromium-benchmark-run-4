6913507e383978f79bffde4fa7cc19ff3d30b6d3
sigbjornf@opera.com <sigbjornf@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-25 22:44:33 +0000

    Oilpan: fix build after r172658.
    DOMExceptions aren't yet on the Oilpan heap; adjust transition types
    accordingly in ServiceWorkerError.
    
    R=
    BUG=340522
    
    Review URL: https://codereview.chromium.org/250883005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172676 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/serviceworkers/ServiceWorkerError.h
+++ third_party/WebKit/Source/modules/serviceworkers/ServiceWorkerError.h
@@ -45,7 +45,7 @@ class ServiceWorkerError {
 public:
     // For CallbackPromiseAdapter
     typedef blink::WebServiceWorkerError WebType;
-    static PassRefPtrWillBeRawPtr<DOMException> from(ScriptPromiseResolverWithContext*, WebType* webErrorRaw);
+    static PassRefPtr<DOMException> from(ScriptPromiseResolverWithContext*, WebType* webErrorRaw);
 
 private:
     WTF_MAKE_NONCOPYABLE(ServiceWorkerError);
