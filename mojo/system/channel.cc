9a5ce605be2ddaabeac144c7bf2dfab92ea33571
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-25 20:02:27 +0000

    Mojo: Windows: Enable Channel::Shutdown() on fatal error.
    r266056 (crbug.com/366768) fixed the nested deletion bug.
    
    R=yzshen@chromium.org
    
    Review URL: https://codereview.chromium.org/253493003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266227 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/system/channel.cc
+++ mojo/system/channel.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/logging.h"
 #include "base/strings/stringprintf.h"
-#include "build/build_config.h"  // TODO(vtl): Remove this.
 #include "mojo/system/message_pipe_endpoint.h"
 
 namespace mojo {
@@ -291,12 +290,7 @@ void Channel::OnReadMessage(const MessageInTransit::View& message_view) {
 
 void Channel::OnFatalError(FatalError fatal_error) {
   LOG(ERROR) << "RawChannel fatal error (type " << fatal_error << ")";
-  // TODO(vtl): We have some nested-deletion bugs on Windows, so this crashes.
-#if defined(OS_WIN)
-  LOG(ERROR) << "Not shutting down due Windows-only bug";
-#else
   Shutdown();
-#endif
 }
 
 bool Channel::ValidateReadMessage(const MessageInTransit::View& message_view) {
