7c4bb6a21e6b04647dc7d7b3eb10ef82a6864128
rmsousa@chromium.org <rmsousa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-25 00:53:24 +0000

    Revert 266042 "Automatically call OnNativeThemeChanged when a wi..."
    This seems to have broken views_unittest (label_unittest) on ChromiumOS
    
    > Automatically call OnNativeThemeChanged when a widget is added to a hierarchy
    > with a widget.
    > 
    > Split off from https://codereview.chromium.org/245863002/
    > 
    > BUG=347832 (tangentially)
    > 
    > Review URL: https://codereview.chromium.org/248073005
    
    TBR=estade@chromium.org
    
    Review URL: https://codereview.chromium.org/256683003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266072 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/autofill/autofill_dialog_views.cc
+++ chrome/browser/ui/views/autofill/autofill_dialog_views.cc
@@ -400,6 +400,8 @@ class LoadingAnimationView : public views::View,
       container_->AddChildView(
           new views::Label(base::ASCIIToUTF16("."), font_list));
     }
+
+    OnNativeThemeChanged(GetNativeTheme());
   }
 
   virtual ~LoadingAnimationView() {}
@@ -578,6 +580,8 @@ AutofillDialogViews::OverlayView::OverlayView(
 
   AddChildView(image_view_);
   AddChildView(message_view_);
+
+  OnNativeThemeChanged(GetNativeTheme());
 }
 
 AutofillDialogViews::OverlayView::~OverlayView() {}
@@ -1628,6 +1632,7 @@ views::View* AutofillDialogViews::CreateFootnoteView() {
       views::Background::CreateSolidBackground(kShadingColor));
 
   legal_document_view_ = new views::StyledLabel(base::string16(), this);
+  OnNativeThemeChanged(GetNativeTheme());
 
   footnote_view_->AddChildView(legal_document_view_);
   footnote_view_->SetVisible(false);
