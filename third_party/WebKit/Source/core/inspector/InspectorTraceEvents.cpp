2221bdb59175ae4857412a61c0c487b2d21f2491
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-25 09:56:35 +0000

    Fix pointer formatting in devtools trace events
    The pointer is now formatted in platform-independent way.
    
    BUG=361045
    R=alph@chromium.org
    
    Review URL: https://codereview.chromium.org/254583003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172608 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorTraceEvents.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorTraceEvents.cpp
@@ -13,8 +13,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/JSONValues.h"
 #include "platform/TracedValue.h"
 #include "wtf/Vector.h"
+#include <inttypes.h>
 
 namespace WebCore {
+
+static String toHexString(void* p)
+{
+    return String::format("0x%" PRIx64, static_cast<uint64>(reinterpret_cast<intptr_t>(p)));
+}
 
 PassRefPtr<TraceEvent::ConvertableToTraceFormat> InspectorLayoutEvent::beginData(FrameView* frameView)
 {
@@ -28,7 +34,7 @@ PassRefPtr<TraceEvent::ConvertableToTraceFormat> InspectorLayoutEvent::beginData
     data->setNumber("dirtyObjects", needsLayoutObjects);
     data->setNumber("totalObjects", totalObjects);
     data->setBoolean("partialLayout", isPartial);
-    data->setString("frame", String::format("%p", &frame));
+    data->setString("frame", toHexString(&frame));
     return TracedValue::fromJSONValue(data);
 }
 
