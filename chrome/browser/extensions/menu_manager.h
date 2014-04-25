d39c16d7a5ab0e69f1e81355d5b9dc5787ee49bb
limasdf@gmail.com <limasdf@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-25 09:25:52 +0000

    Remove NOTIFICATION_EXTENSION_LOADED_DEPRECATED from MenuManager.
    R=kalman@chromium.org
    BUG=354046
    TEST=unit_tests
    
    Review URL: https://codereview.chromium.org/250443002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266173 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/menu_manager.h
+++ chrome/browser/extensions/menu_manager.h
@@ -16,14 +16,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/scoped_observer.h"
 #include "base/strings/string16.h"
 #include "base/values.h"
 #include "chrome/browser/extensions/extension_icon_manager.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
+#include "extensions/browser/extension_registry_observer.h"
 #include "extensions/common/url_pattern_set.h"
-
 
 class Profile;
 class SkBitmap;
@@ -35,6 +36,7 @@ struct ContextMenuParams;
 
 namespace extensions {
 class Extension;
+class ExtensionRegistry;
 class StateStore;
 
 // Represents a menu item added by an extension.
@@ -268,7 +270,8 @@ class MenuItem {
 // This class keeps track of menu items added by extensions.
 class MenuManager : public content::NotificationObserver,
                     public base::SupportsWeakPtr<MenuManager>,
-                    public KeyedService {
+                    public KeyedService,
+                    public ExtensionRegistryObserver {
  public:
   static const char kOnContextMenus[];
   static const char kOnWebviewContextMenus[];
@@ -335,9 +338,17 @@ class MenuManager : public content::NotificationObserver,
   // default extension icon.
   const SkBitmap& GetIconForExtension(const std::string& extension_id);
 
-  // Implements the content::NotificationObserver interface.
+  // content::NotificationObserver implementation.
   virtual void Observe(int type, const content::NotificationSource& source,
                        const content::NotificationDetails& details) OVERRIDE;
+
+  // ExtensionRegistryObserver implementation.
+  virtual void OnExtensionLoaded(content::BrowserContext* browser_context,
+                                 const Extension* extension) OVERRIDE;
+  virtual void OnExtensionUnloaded(
+      content::BrowserContext* browser_context,
+      const Extension* extension,
+      UnloadedExtensionInfo::Reason reason) OVERRIDE;
 
   // Stores the menu items for the extension in the state storage.
   void WriteToStorage(const Extension* extension,
@@ -378,6 +389,10 @@ class MenuManager : public content::NotificationObserver,
   std::map<MenuItem::Id, MenuItem*> items_by_id_;
 
   content::NotificationRegistrar registrar_;
+
+  // Listen to extension load, unloaded notifications.
+  ScopedObserver<ExtensionRegistry, ExtensionRegistryObserver>
+      extension_registry_observer_;
 
   ExtensionIconManager icon_manager_;
 
