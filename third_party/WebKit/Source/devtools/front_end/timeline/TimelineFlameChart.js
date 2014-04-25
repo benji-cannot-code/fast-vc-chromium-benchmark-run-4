eb229cd6639a57fda9c88f5ff04e731d971bbbfb
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-25 12:40:58 +0000

    TimelinePanel: convert another portion of getters on TimelineModel.Record into functions
    Mechanical change. No business logic changes.
    thread, type, frameId, stackTrace and aggregatedStats were converted.
    
    BUG=361045
    R=yurys@chromium.org
    
    Review URL: https://codereview.chromium.org/251783002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172621 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/timeline/TimelineFlameChart.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TimelineFlameChart.js
@@ -155,7 +155,7 @@ WebInspector.TimelineFlameChartDataProvider.prototype = {
         var records = this._model.records();
         for (var i = 0; i < records.length; ++i) {
             var record = records[i];
-            var thread = record.thread;
+            var thread = record.thread();
             if (thread === "gpu")
                 continue;
             if (!thread) {
@@ -194,7 +194,7 @@ WebInspector.TimelineFlameChartDataProvider.prototype = {
                 else if (record === this._gpuThreadRecord)
                     level = cpuStackDepth + 2;
                 else if (record.thread)
-                    level += threadBaselines[record.thread];
+                    level += threadBaselines[record.thread()];
                 this._timelineData.entryLevels[i] = level;
             }
         }
@@ -260,7 +260,7 @@ WebInspector.TimelineFlameChartDataProvider.prototype = {
         this._timelineData.entryOffsets[index] = startTime - this._zeroTime;
         this._timelineData.entryLevels[index] = level;
         this._timelineData.entryTotalTimes[index] = endTime - startTime;
-        this._entryThreadDepths[record.thread] = Math.max(level, this._entryThreadDepths[record.thread] || 0);
+        this._entryThreadDepths[record.thread()] = Math.max(level, this._entryThreadDepths[record.thread()] || 0);
         return index;
     },
 
@@ -292,7 +292,7 @@ WebInspector.TimelineFlameChartDataProvider.prototype = {
         if (record === this._cpuThreadRecord || record === this._gpuThreadRecord)
             return "#555";
 
-        if (record.type === WebInspector.TimelineModel.RecordType.JSFrame)
+        if (record.type() === WebInspector.TimelineModel.RecordType.JSFrame)
             return WebInspector.TimelineFlameChartDataProvider.jsFrameColorGenerator().colorForID(record.data["functionName"]);
 
         var category = WebInspector.TimelineUIUtils.categoryForRecord(record);
