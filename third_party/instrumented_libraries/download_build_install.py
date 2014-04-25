ecddaeaa0b46d07b287a38f4bac43de4384f9ebd
earthdok@chromium.org <earthdok@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-25 16:09:16 +0000

    Instrumented libraries: use 'rm -rf' instead of 'shutil.rmtree'.
    The latter may fail because permissions.
    
    BUG=313751
    TBR=glider@chromium.org
    NOTRY=true
    TEST=msan
    
    Review URL: https://codereview.chromium.org/253493004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266200 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/instrumented_libraries/download_build_install.py
+++ third_party/instrumented_libraries/download_build_install.py
@@ -297,7 +297,7 @@ def download_build_install(parsed_arguments):
 
   # A failed build might have left a dirty source tree behind.
   if os.path.exists(library_directory):
-    shutil.rmtree(library_directory)
+    shell_call('rm -rf %s' % library_directory, parsed_arguments.verbose)
   os.makedirs(library_directory)
 
   with ScopedChangeDirectory(library_directory) as cd_library:
@@ -331,7 +331,7 @@ def download_build_install(parsed_arguments):
   # Remove downloaded package and generated temporary build files.
   # Failed builds intentionally skip this step, in order to aid in tracking down
   # build failures.
-  shutil.rmtree(library_directory)
+  shell_call('rm -rf %s' % library_directory, parsed_arguments.verbose)
 
 
 def main():
