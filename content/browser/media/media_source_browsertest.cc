c2d61ca55689fd43547c2f4ebe0293c010fd2821
dalecurtis@google.com <dalecurtis@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-25 19:30:01 +0000

    Fix unused variable warning.
    BUG=none
    TEST=compile
    TBR=zturner
    
    Review URL: https://codereview.chromium.org/258823004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266221 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/media/media_source_browsertest.cc
+++ content/browser/media/media_source_browsertest.cc
@@ -13,7 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Common media types.
 const char kWebMAudioOnly[] = "audio/webm; codecs=\"vorbis\"";
 #if !defined(OS_ANDROID)
-const char kWebMOpusAudioOnly[] = "audio/webm; codecs=\"opus\"";
+// TODO(dalecurtis): Disabled while the test data file is updated.
+// const char kWebMOpusAudioOnly[] = "audio/webm; codecs=\"opus\"";
 #endif
 const char kWebMVideoOnly[] = "video/webm; codecs=\"vp8\"";
 const char kWebMAudioVideo[] = "video/webm; codecs=\"vorbis, vp8\"";
