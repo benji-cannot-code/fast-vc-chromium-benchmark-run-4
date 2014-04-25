88f18d51c5359533872855813ad848288205f70a
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-25 07:34:44 +0000

    Store SupportedCodecs in KeySystemInfo and KeySystems.
    Previously we convert SupportedCodecs (a uint32 bit mask indicating which
    codec is supported) to ContainerCodecMap (a map from a container type to a set
    of supported codecs in that container). Then we pass ContainerCodecMap to
    KeySystems and store it there for easy look up.
    
    This results in a lot of duplicate converting code. This CL stores
    SupportedCodecs in KeySystmeInfo and KeySystems so that we don't need to convert
    anything. Then when IsTypeSupported() is called, we find the bit masks for
    queried container and codec types and compare it with the SupportedCodecs stored
    in KeySystems.
    
    BUG=362769
    TEST=All existing tests pass.
    
    Review URL: https://codereview.chromium.org/246033002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266148 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/renderer/aw_key_systems.cc
+++ android_webview/renderer/aw_key_systems.cc
@@ -9,17 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/logging.h"
+#include "content/public/common/eme_codec.h"
 #include "third_party/widevine/cdm/widevine_cdm_common.h"
 
 using content::KeySystemInfo;
 
 namespace {
-
-const char kAudioMp4[] = "audio/mp4";
-const char kVideoMp4[] = "video/mp4";
-const char kMp4a[] = "mp4a";
-const char kAvc1[] = "avc1";
-const char kAvc3[] = "avc3";
 
 // Return |name|'s parent key system.
 std::string GetDirectParentName(const std::string& name) {
@@ -36,10 +31,7 @@ void AddWidevineWithCodecs(const std::string& key_system_name,
   if (add_parent_name)
     info.parent_key_system = GetDirectParentName(key_system_name);
 
-  info.supported_types[kAudioMp4].insert(kMp4a);
-  info.supported_types[kVideoMp4] = info.supported_types[kAudioMp4];
-  info.supported_types[kVideoMp4].insert(kAvc1);
-  info.supported_types[kVideoMp4].insert(kAvc3);
+  info.supported_codecs = content::EME_CODEC_MP4_ALL;
 
   concrete_key_systems->push_back(info);
 }
--- content/public/renderer/key_system_info.h
+++ content/public/renderer/key_system_info.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/containers/hash_tables.h"
 #include "content/common/content_export.h"
+#include "content/public/common/eme_codec.h"
 
 // Definitions:
 // * Key system
@@ -33,22 +34,13 @@ namespace content {
 // Contains information about an EME key system as well as how to instantiate
 // the corresponding CDM.
 struct CONTENT_EXPORT KeySystemInfo {
-  // Represents the set of codecs supported within a container.
-  typedef base::hash_set<std::string> CodecSet;
-
-  // Represents container-codec combinations. The CodecSet may contain zero
-  // or more codecs.
-  typedef std::map<std::string, CodecSet> ContainerCodecsMap;
-
   explicit KeySystemInfo(const std::string& key_system);
   ~KeySystemInfo();
 
   std::string key_system;
 
-  // Specifies container and codec combinations supported by |key_system|.
-  // Multiple codecs may be listed for each container.
-  // In all cases, the container without a codec is also always supported.
-  ContainerCodecsMap supported_types;
+  // Specifies codecs supported by |key_system|.
+  SupportedCodecs supported_codecs;
 
   // A hierarchical parent for |key_system|. This value can be used to check
   // supported types but cannot be used to instantiate a MediaKeys object.
