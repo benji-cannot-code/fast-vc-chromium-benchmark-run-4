66f61101a1b7f384ea808719b8560fefd9f0d508
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-25 01:00:25 +0000

    Add media::StreamParser::InitParameter struct.
    The new struct is used to pass parameters to StreamParser::InitCB. This
    change makes it easier to add new parameters.
    
    BUG=338529
    R=acolwell@chromium.org
    
    Review URL: https://codereview.chromium.org/254473006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266077 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/stream_parser.cc
+++ media/base/stream_parser.cc
@@ -10,6 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
+StreamParser::InitParameters::InitParameters(base::TimeDelta duration)
+    : duration(duration),
+      auto_update_timestamp_offset(false) {
+}
+
 StreamParser::StreamParser() {}
 
 StreamParser::~StreamParser() {}
