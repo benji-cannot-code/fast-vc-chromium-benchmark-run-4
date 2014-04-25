0b2dd3ed395dd4d00f46594821e20d503a47107f
kenrb@chromium.org <kenrb@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-25 14:21:23 +0000

    Mouse event handlers use user gesture tokens on top-level LocalFrames.
    The last mouseDown and mouseUp user gesture tokens are set on the top
    level frame's EventHandler. For the FrameTree to be able to contain
    RemoteFrames, we need to account for instances where the current
    frame has RemoteFrame ancestors. This change causes the tokens to be
    used on the EventHandler attached to the frame that is the root of the
    local subtree.
    
    This will not make gestures work with RemoteFrames, as that
    will need a much larger refactoring. Eventually gesture
    tracking will need to be synchronized across processes.
    
    BUG=346764
    
    Review URL: https://codereview.chromium.org/248083003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172633 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/EventHandler.cpp
+++ third_party/WebKit/Source/core/page/EventHandler.cpp
@@ -1200,7 +1200,7 @@ bool EventHandler::handleMousePressEvent(const PlatformMouseEvent& mouseEvent)
     RefPtr<FrameView> protector(m_frame->view());
 
     UserGestureIndicator gestureIndicator(DefinitelyProcessingUserGesture);
-    m_frame->tree().top()->eventHandler().m_lastMouseDownUserGestureToken = gestureIndicator.currentToken();
+    m_frame->localFrameRoot()->eventHandler().m_lastMouseDownUserGestureToken = gestureIndicator.currentToken();
 
     cancelFakeMouseMoveEvent();
     if (m_eventHandlerWillResetCapturingMouseEventsNode)
@@ -1516,7 +1516,7 @@ bool EventHandler::handleMouseReleaseEvent(const PlatformMouseEvent& mouseEvent)
 
     OwnPtr<UserGestureIndicator> gestureIndicator;
 
-    if (m_frame->tree().top()->eventHandler().m_lastMouseDownUserGestureToken)
+    if (m_frame->localFrameRoot()->eventHandler().m_lastMouseDownUserGestureToken)
         gestureIndicator = adoptPtr(new UserGestureIndicator(m_frame->tree().top()->eventHandler().m_lastMouseDownUserGestureToken.release()));
     else
         gestureIndicator = adoptPtr(new UserGestureIndicator(DefinitelyProcessingUserGesture));
