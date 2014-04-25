293d0131ee7a80cdfd73da4091082ca9305bdbc1
nhiroki@chromium.org <nhiroki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-25 03:18:22 +0000

    ServiceWorker: Clean up ServiceWorkerDatabase
    BUG=364099
    TEST=content_unittests --gtest_filter=ServiceWorkerDatabaseTest.*
    
    Review URL: https://codereview.chromium.org/257593003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266116 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/service_worker/service_worker_database.h
+++ content/browser/service_worker/service_worker_database.h
@@ -18,17 +18,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace leveldb {
 class DB;
 class Env;
-class Slice;
+class Status;
 class WriteBatch;
 }
 
 namespace content {
 
-// Class to persist serviceworker regitration data in a database.
+// Class to persist serviceworker registration data in a database.
 // Should NOT be used on the IO thread since this does blocking
 // file io. The ServiceWorkerStorage class owns this class and
 // is responsible for only calling it serially on background
-// nonIO threads (ala SequencedWorkerPool).
+// non-IO threads (ala SequencedWorkerPool).
 class CONTENT_EXPORT ServiceWorkerDatabase {
  public:
   // We do leveldb stuff in |path| or in memory if |path| is empty.
@@ -78,11 +78,15 @@ class CONTENT_EXPORT ServiceWorkerDatabase {
   // and next available IDs.
   bool PopulateInitialData();
 
-  bool ReadInt64(const leveldb::Slice& key, int64* value_out);
-  bool WriteBatch(scoped_ptr<leveldb::WriteBatch> batch);
+  bool ReadNextAvailableId(const char* id_key,
+                           int64* next_avail_id);
+  bool WriteBatch(leveldb::WriteBatch* batch);
 
   bool IsOpen();
   bool IsEmpty();
+
+  void HandleError(const tracked_objects::Location& from_here,
+                   const leveldb::Status& status);
 
   base::FilePath path_;
   scoped_ptr<leveldb::Env> env_;
