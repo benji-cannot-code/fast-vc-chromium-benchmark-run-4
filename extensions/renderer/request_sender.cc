a2741779ebb5ff111af87e288282c9c1f1ddf80a
wjywbs@gmail.com <wjywbs@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-25 02:24:17 +0000

    Add the feature to retain the user gesture in the extension callback.
    R=kalman@chromium.org,mek@chromium.org
    BUG=351958,361116
    
    Review URL: https://codereview.chromium.org/227413008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266085 0039d316-1c4b-4281-b951-d872f2087c98

--- extensions/renderer/request_sender.cc
+++ extensions/renderer/request_sender.cc
@@ -12,18 +12,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/renderer/script_context.h"
 #include "third_party/WebKit/public/web/WebDocument.h"
 #include "third_party/WebKit/public/web/WebFrame.h"
+#include "third_party/WebKit/public/web/WebScopedUserGesture.h"
 #include "third_party/WebKit/public/web/WebUserGestureIndicator.h"
+#include "third_party/WebKit/public/web/WebUserGestureToken.h"
 
 namespace extensions {
 
 // Contains info relevant to a pending API request.
 struct PendingRequest {
  public:
-  PendingRequest(const std::string& name, RequestSender::Source* source)
-      : name(name), source(source) {}
+  PendingRequest(const std::string& name,
+                 RequestSender::Source* source,
+                 blink::WebUserGestureToken token)
+      : name(name), source(source), token(token) {}
 
   std::string name;
   RequestSender::Source* source;
+  blink::WebUserGestureToken token;
 };
 
 RequestSender::ScopedTabID::ScopedTabID(RequestSender* request_sender,
@@ -96,7 +101,8 @@ void RequestSender::StartRequest(Source* source,
   if (blink::WebFrame* webframe = context->web_frame())
     source_url = webframe->document().url();
 
-  InsertRequest(request_id, new PendingRequest(name, source));
+  InsertRequest(request_id, new PendingRequest(name, source,
+      blink::WebUserGestureIndicator::currentUserGestureToken()));
 
   ExtensionHostMsg_Request_Params params;
   params.name = name;
@@ -128,6 +134,7 @@ void RequestSender::HandleResponse(int request_id,
     return;
   }
 
+  blink::WebScopedUserGesture gesture(request->token);
   request->source->OnResponseReceived(
       request->name, request_id, success, response, error);
 }
