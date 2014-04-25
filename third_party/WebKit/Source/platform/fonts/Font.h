67e04dc92fab3c6e993445786e7c7f32615b9db8
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-25 21:22:10 +0000

    Simplify and share selection rect snapping logic
    Simplify the logic for pixel snapping a selection rect and share the
    implementation across the simple and complex text paths.
    
    BUG=366705
    R=dglazkov@chromium.org
    
    Review URL: https://codereview.chromium.org/256683006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172669 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/fonts/Font.h
+++ third_party/WebKit/Source/platform/fonts/Font.h
@@ -167,6 +167,7 @@ public:
 
     void willUseFontData() const;
 
+    static FloatRect pixelSnappedSelectionRect(float fromX, float toX, float y, float height);
 private:
     bool loadingCustomFonts() const
     {
