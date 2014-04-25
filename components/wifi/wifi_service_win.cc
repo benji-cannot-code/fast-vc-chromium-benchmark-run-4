81a30f623f8218a5dc6a93ccb5c695f8ea576a8b
mef@chromium.org <mef@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-25 03:19:16 +0000

    Ignore ERROR_ADDRESS_NOT_ASSOCIATED from IpReleaseAddress in WiFiService::ResetDHCP. It is reported on Win 8.1 running on MS Surface Pro after connection to factory-reset Chromecast device.
    BUG=366582
    
    Review URL: https://codereview.chromium.org/250223002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266117 0039d316-1c4b-4281-b951-d872f2087c98

--- components/wifi/wifi_service_win.cc
+++ components/wifi/wifi_service_win.cc
@@ -907,6 +907,8 @@ void WiFiServiceImpl::WaitForNetworkConnect(const std::string& network_guid,
     // e.g. after Chromecast device reset. Reset DHCP on wireless network to
     // work around this issue.
     error = ResetDHCP();
+    if (error != ERROR_SUCCESS)
+      LOG(ERROR) << error;
     // There is no need to keep created profile as network is connected.
     created_profiles_.RemoveWithoutPathExpansion(network_guid, NULL);
     // Restore previously suppressed notifications.
@@ -974,14 +976,14 @@ DWORD WiFiServiceImpl::LoadWlanLibrary() {
   // Use an absolute path to load the DLL to avoid DLL preloading attacks.
   base::FilePath path;
   if (!PathService::Get(base::DIR_SYSTEM, &path)) {
-    DLOG(ERROR) << "Unable to get system path.";
+    LOG(ERROR) << "Unable to get system path.";
     return ERROR_NOT_FOUND;
   }
   wlan_api_library_ = ::LoadLibraryEx(path.Append(kWlanApiDll).value().c_str(),
                                       NULL,
                                       LOAD_WITH_ALTERED_SEARCH_PATH);
   if (!wlan_api_library_) {
-    DLOG(ERROR) << "Unable to load WlanApi.dll.";
+    LOG(ERROR) << "Unable to load WlanApi.dll.";
     return ERROR_NOT_FOUND;
   }
 
@@ -1045,7 +1047,7 @@ DWORD WiFiServiceImpl::LoadWlanLibrary() {
       !WlanRegisterNotification_function_ ||
       !WlanScan_function_ ||
       !WlanSetProfile_function_) {
-    DLOG(ERROR) << "Unable to find required WlanApi function.";
+    LOG(ERROR) << "Unable to find required WlanApi function.";
     FreeLibrary(wlan_api_library_);
     wlan_api_library_ = NULL;
     return ERROR_NOT_FOUND;
@@ -1101,12 +1103,21 @@ DWORD WiFiServiceImpl::OpenClientHandle() {
 DWORD WiFiServiceImpl::ResetDHCP() {
   IP_ADAPTER_INDEX_MAP adapter_index_map = {0};
   DWORD error = FindAdapterIndexMapByGUID(interface_guid_, &adapter_index_map);
-  if (error == ERROR_SUCCESS) {
-    error = ::IpReleaseAddress(&adapter_index_map);
-    if (error == ERROR_SUCCESS) {
-      error = ::IpRenewAddress(&adapter_index_map);
-    }
+  if (error != ERROR_SUCCESS) {
+    LOG(ERROR) << error;
+    return error;
   }
+  error = ::IpReleaseAddress(&adapter_index_map);
+  if (error != ERROR_SUCCESS) {
+    if (error != ERROR_ADDRESS_NOT_ASSOCIATED) {
+      LOG(ERROR) << error;
+      return error;
+    }
+    DVLOG(1) << "Ignoring IpReleaseAddress Error: " << error;
+  }
+  error = ::IpRenewAddress(&adapter_index_map);
+  if (error != ERROR_SUCCESS)
+    LOG(ERROR) << error;
   return error;
 }
 
--- components/wifi/wifi_test.cc
+++ components/wifi/wifi_test.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/message_loop/message_loop.h"
+#include "base/run_loop.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
@@ -53,10 +54,26 @@ class WiFiTest {
       base::MessageLoop::current()->Quit();
   }
 
+  void OnNetworksChanged(
+      const WiFiService::NetworkGuidList& network_guid_list) {
+    VLOG(0) << "Networks Changed: " << network_guid_list[0];
+    base::DictionaryValue properties;
+    std::string error;
+    wifi_service_->GetProperties(network_guid_list[0], &properties, &error);
+    VLOG(0) << error << ":\n" << properties;
+  }
+
+  void OnNetworkListChanged(
+      const WiFiService::NetworkGuidList& network_guid_list) {
+    VLOG(0) << "Network List Changed: " << network_guid_list.size();
+  }
+
 #if defined(OS_MACOSX)
   // Without this there will be a mem leak on osx.
   base::mac::ScopedNSAutoreleasePool scoped_pool_;
 #endif
+
+  scoped_ptr<WiFiService> wifi_service_;
 
   // Need AtExitManager to support AsWeakPtr (in NetLog).
   base::AtExitManager exit_manager_;
@@ -112,17 +129,14 @@ bool WiFiTest::ParseCommandLine(int argc, const char* argv[]) {
     MessageBoxA(NULL, __FUNCTION__, "Debug Me!", MB_OK);
 #endif
 
-#if defined(OS_WIN) || defined(OS_MACOSX)
-  scoped_ptr<WiFiService> wifi_service(WiFiService::Create());
-#else
-  scoped_ptr<WiFiService> wifi_service(WiFiService::CreateForTest());
-#endif
+  base::MessageLoopForIO loop;
 
-  wifi_service->Initialize(NULL);
+  wifi_service_.reset(WiFiService::Create());
+  wifi_service_->Initialize(loop.message_loop_proxy());
 
   if (parsed_command_line.HasSwitch("list")) {
     base::ListValue network_list;
-    wifi_service->GetVisibleNetworks(std::string(), &network_list);
+    wifi_service_->GetVisibleNetworks(std::string(), &network_list);
     VLOG(0) << network_list;
     return true;
   }
@@ -131,7 +145,7 @@ bool WiFiTest::ParseCommandLine(int argc, const char* argv[]) {
     if (network_guid.length() > 0) {
       base::DictionaryValue properties;
       std::string error;
-      wifi_service->GetProperties(network_guid, &properties, &error);
+      wifi_service_->GetProperties(network_guid, &properties, &error);
       VLOG(0) << error << ":\n" << properties;
       return true;
     }
@@ -160,10 +174,8 @@ bool WiFiTest::ParseCommandLine(int argc, const char* argv[]) {
       std::string new_network_guid;
       properties->SetString("WiFi.SSID", network_guid);
       VLOG(0) << "Creating Network: " << *properties;
-      wifi_service->CreateNetwork(false,
-                                  properties.Pass(),
-                                  &new_network_guid,
-                                  &error);
+      wifi_service_->CreateNetwork(
+          false, properties.Pass(), &new_network_guid, &error);
       VLOG(0) << error << ":\n" << new_network_guid;
       return true;
     }
@@ -174,12 +186,17 @@ bool WiFiTest::ParseCommandLine(int argc, const char* argv[]) {
       std::string error;
       if (!properties->empty()) {
         VLOG(0) << "Using connect properties: " << *properties;
-        wifi_service->SetProperties(network_guid,
-                                    properties.Pass(),
-                                    &error);
+        wifi_service_->SetProperties(network_guid, properties.Pass(), &error);
       }
-      wifi_service->StartConnect(network_guid, &error);
+
+      wifi_service_->SetEventObservers(
+          loop.message_loop_proxy(),
+          base::Bind(&WiFiTest::OnNetworksChanged, base::Unretained(this)),
+          base::Bind(&WiFiTest::OnNetworkListChanged, base::Unretained(this)));
+
+      wifi_service_->StartConnect(network_guid, &error);
       VLOG(0) << error;
+      base::MessageLoop::current()->Run();
       return true;
     }
   }
@@ -187,7 +204,7 @@ bool WiFiTest::ParseCommandLine(int argc, const char* argv[]) {
   if (parsed_command_line.HasSwitch("disconnect")) {
     if (network_guid.length() > 0) {
       std::string error;
-      wifi_service->StartDisconnect(network_guid, &error);
+      wifi_service_->StartDisconnect(network_guid, &error);
       VLOG(0) << error;
       return true;
     }
@@ -197,9 +214,7 @@ bool WiFiTest::ParseCommandLine(int argc, const char* argv[]) {
     if (network_guid.length() > 0) {
       std::string error;
       std::string key_data;
-      wifi_service->GetKeyFromSystem(network_guid,
-                                     &key_data,
-                                     &error);
+      wifi_service_->GetKeyFromSystem(network_guid, &key_data, &error);
       VLOG(0) << key_data << error;
       return true;
     }
