2164d74c571ff6f74a7da1683350881f3ca66ded
hanxi@chromium.org <hanxi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-26 00:11:21 +0000

    1. Handle the case of empty embedder_extension_id in    guestview.h.
    2. Introduce a template class GuestView<T> to handle the    *guestview-specific functions in guestview.h:
       . Rename GuestView to GuestViewBase, introduce GuestView<T> class, and move *ViewGuest-specific functions to     GuestView<T>:
            -- FromWebContents<T>(...)
            -- From<T>(...).
       . Introduce a template member function As() in      GuestViewBase to replace all of the As*View functions.
    
    3. In each derived *ViewGuest class , e.g., WebViewGuest &    AdViewGuest, define a static variable to show its view type    (string).
    
    
    BUG=351824
    
    Review URL: https://codereview.chromium.org/237533008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266297 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/guest_view/web_view/web_view_find_helper.h
+++ chrome/browser/guest_view/web_view/web_view_find_helper.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_GUESTVIEW_WEBVIEW_WEBVIEW_FIND_HELPER_H_
-#define CHROME_BROWSER_GUESTVIEW_WEBVIEW_WEBVIEW_FIND_HELPER_H_
+#ifndef CHROME_BROWSER_GUEST_VIEW_WEB_VIEW_WEB_VIEW_FIND_HELPER_H_
+#define CHROME_BROWSER_GUEST_VIEW_WEB_VIEW_WEB_VIEW_FIND_HELPER_H_
 
 #include <map>
 #include <vector>
@@ -184,4 +184,4 @@ class WebviewFindHelper {
   DISALLOW_COPY_AND_ASSIGN(WebviewFindHelper);
 };
 
-#endif  // CHROME_BROWSER_GUESTVIEW_WEBVIEW_WEBVIEW_FIND_HELPER_H_
+#endif  // CHROME_BROWSER_GUEST_VIEW_WEB_VIEW_WEB_VIEW_FIND_HELPER_H_
