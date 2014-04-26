7a04de40a4ab97a195ad9cb34efade21e10a7f96
robliao@chromium.org <robliao@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-26 10:14:52 +0000

    Suppress Errors for chrome.alarm.get
    Removed the error signalling to prevent log spam from the runtime.lastError
    checks for non-existent alarms.
    
    BUG=367136
    
    Review URL: https://codereview.chromium.org/258793006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266352 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/alarms/alarms_api.cc
+++ chrome/browser/extensions/api/alarms/alarms_api.cc
@@ -20,7 +20,6 @@ namespace extensions {
 namespace {
 
 const char kDefaultAlarmName[] = "";
-const char kAlarmNotFound[] = "No alarm named '*' exists.";
 const char kBothRelativeAndAbsoluteTime[] =
     "Cannot set both when and delayInMinutes.";
 const char kNoScheduledTime[] =
@@ -148,11 +147,8 @@ void AlarmsGetFunction::Callback(
     const std::string& name, extensions::Alarm* alarm) {
   if (alarm) {
     results_ = alarms::Get::Results::Create(*alarm->js_alarm);
-    SendResponse(true);
-  } else {
-    error_ = ErrorUtils::FormatErrorMessage(kAlarmNotFound, name);
-    SendResponse(false);
   }
+  SendResponse(true);
 }
 
 bool AlarmsGetAllFunction::RunImpl() {
