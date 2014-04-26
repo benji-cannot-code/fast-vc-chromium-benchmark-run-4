126533a3ea85100e9fefeef607f83254d67740e2
tyoshino@chromium.org <tyoshino@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-26 00:47:56 +0000

    Add a chrome://flags item "allow-insecure-websocket-from-https-origin"
    There're web apps served over HTTPS but are using insecure WebSockets.
    To improve security, we're going to prohibit that, but should provide
    an option to turn off the check until everyone finishes fixing their apps
    to get ready for stopping use of insecure WebSockets from HTTPS origin.
    
    The shield page action allows users to manually turn off mixed content
    check, but for apps designed to open new windows frequently, this
    workaround is too bothering.
    
    So, we'll provide a chrome://flags item which turns off the mixed
    content check (only for WebSocket). This flag is scheduled to go away
    when everyone should be ready for accepting this security check.
    
    This CL depends on this Blink side CL:
    https://codereview.chromium.org/246893014/
    
    BUG=85271
    
    Review URL: https://codereview.chromium.org/248863003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266309 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1002,6 +1002,7 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
   // Propagate the following switches to the renderer command line (along
   // with any associated values) if present in the browser command line.
   static const char* const kSwitchNames[] = {
+    switches::kAllowInsecureWebSocketFromHttpsOrigin,
     switches::kAllowLoopbackInPeerConnection,
     switches::kAudioBufferSize,
     switches::kAuditAllHandles,
