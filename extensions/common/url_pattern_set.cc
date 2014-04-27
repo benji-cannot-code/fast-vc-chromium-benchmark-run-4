c2f4ef92a2bc997999de972f96ffdedbfa345fb9
sungmann.cho@navercorp.com <sungmann.cho@navercorp.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-27 00:28:05 +0000

    Use base::STLSet* in place of std::set_* for simplicity and consistency.
    BUG=254066
    
    Review URL: https://codereview.chromium.org/255903002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266403 0039d316-1c4b-4281-b951-d872f2087c98

--- extensions/common/url_pattern_set.cc
+++ extensions/common/url_pattern_set.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "extensions/common/url_pattern_set.h"
 
-#include <algorithm>
 #include <iterator>
 
 #include "base/logging.h"
@@ -29,7 +28,6 @@ const char kInvalidURLPatternError[] = "Invalid url pattern '*'";
 void URLPatternSet::CreateDifference(const URLPatternSet& set1,
                                      const URLPatternSet& set2,
                                      URLPatternSet* out) {
-  out->ClearPatterns();
   out->patterns_ = base::STLSetDifference<std::set<URLPattern> >(
       set1.patterns_, set2.patterns_);
 }
@@ -38,22 +36,16 @@ void URLPatternSet::CreateDifference(const URLPatternSet& set1,
 void URLPatternSet::CreateIntersection(const URLPatternSet& set1,
                                        const URLPatternSet& set2,
                                        URLPatternSet* out) {
-  out->ClearPatterns();
-  std::set_intersection(set1.patterns_.begin(), set1.patterns_.end(),
-                        set2.patterns_.begin(), set2.patterns_.end(),
-                        std::inserter<std::set<URLPattern> >(
-                            out->patterns_, out->patterns_.begin()));
+  out->patterns_ = base::STLSetIntersection<std::set<URLPattern> >(
+      set1.patterns_, set2.patterns_);
 }
 
 // static
 void URLPatternSet::CreateUnion(const URLPatternSet& set1,
                                 const URLPatternSet& set2,
                                 URLPatternSet* out) {
-  out->ClearPatterns();
-  std::set_union(set1.patterns_.begin(), set1.patterns_.end(),
-                 set2.patterns_.begin(), set2.patterns_.end(),
-                 std::inserter<std::set<URLPattern> >(
-                     out->patterns_, out->patterns_.begin()));
+  out->patterns_ = base::STLSetUnion<std::set<URLPattern> >(
+      set1.patterns_, set2.patterns_);
 }
 
 // static
