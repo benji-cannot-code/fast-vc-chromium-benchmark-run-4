3f8b31fb3e37c497a029e417a2266dd9da398a10
ziran.sun@samsung.com <ziran.sun@samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-28 06:03:06 +0000

    Rewrite functions from WebPasswordFormData and WebPasswordFormUtils in password_form_conversion_utils.
    There are Some old Blink code implemented in Webkit that predates the
    Chromium/WebKit API, specifically WebPasswordFormData and WebPasswordFormUtils. At
    this point the split just makes this code harder to update, as you need to wait
    for a Blink roll when adding a new element to autofill::PasswordForm. This patch is
    re-writing this code in components/autofill/content/renderer/password_form_conversion_util
    s to make it cleaner.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/249153002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266438 0039d316-1c4b-4281-b951-d872f2087c98

--- components/autofill/content/renderer/password_autofill_agent.cc
+++ components/autofill/content/renderer/password_autofill_agent.cc
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/web/WebLocalFrame.h"
 #include "third_party/WebKit/public/web/WebNode.h"
 #include "third_party/WebKit/public/web/WebNodeList.h"
-#include "third_party/WebKit/public/web/WebPasswordFormData.h"
 #include "third_party/WebKit/public/web/WebSecurityOrigin.h"
 #include "third_party/WebKit/public/web/WebUserGestureIndicator.h"
 #include "third_party/WebKit/public/web/WebView.h"
