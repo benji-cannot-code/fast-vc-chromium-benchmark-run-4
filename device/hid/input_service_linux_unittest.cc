dd7c3d1c3b9161a6c05c1cb55023f2c9402fa2ce
ygorshenin@chromium.org <ygorshenin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-28 13:51:07 +0000

    Devices without devnode are filtered out.
    BUG=127016
    TEST=manual
    R=rockot@chromium.org
    
    Review URL: https://codereview.chromium.org/250803005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266546 0039d316-1c4b-4281-b951-d872f2087c98

--- device/hid/input_service_linux_unittest.cc
+++ device/hid/input_service_linux_unittest.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/message_loop/message_loop.h"
-#include "device/hid/input_service_linux.cc"
+#include "device/hid/input_service_linux.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace device {
