7107dc1f1a160f90cda890da5647680de3d309a6
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-28 12:30:37 +0000

    TimelinePanel: REGRESSION. Followup fix for r172735 and r172617.
    It fixes problem with background event style for all events and endTime for tracing events.
    
    BUG=361045
    
    Review URL: https://codereview.chromium.org/254793005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172753 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/timeline/TimelineView.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TimelineView.js
@@ -1017,7 +1017,7 @@ WebInspector.TimelineRecordListRow.prototype = {
         for (var currentRecord = presentationRecord.presentationParent() ? presentationRecord.presentationParent().presentationParent() : null; currentRecord; currentRecord = currentRecord.presentationParent())
             paddingLeft += 12 / (Math.max(1, step++));
         this.element.style.paddingLeft = paddingLeft + "px";
-        if (record.thread)
+        if (record.thread())
             this.element.classList.add("background");
 
         this._typeElement.textContent = record.title();
@@ -1148,7 +1148,7 @@ WebInspector.TimelineRecordGraphRow.prototype = {
         this._record = presentationRecord;
         var record = presentationRecord.record();
         this.element.className = "timeline-graph-side timeline-category-" + record.category().name;
-        if (record.thread)
+        if (record.thread())
             this.element.classList.add("background");
 
         var barPosition = calculator.computeBarGraphWindowPosition(record);
