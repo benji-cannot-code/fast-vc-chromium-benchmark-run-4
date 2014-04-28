23f74453aa8e54a1699fb41a25ffed70a77715bc
vollick@chromium.org <vollick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-28 12:20:33 +0000

    Don't clear render surfaces unnecessarily.
    As an optimization, we used to clear render surfaces when they were
    elided from the RenderSurfaceLayerList. It turns out that these surfaces
    are still important from time to time; a scroll child will need to
    access the transforms of these surfaces in order to put its clip in the
    right space.
    
    This CL retains the layer lists and it also updates RenderSurface to
    own a LayerList rather than a RenderSurfaceLayerList (which was always
    a bit of a kludge).
    
    R=danakj@chromium.org
    BUG=366386
    
    Review URL: https://codereview.chromium.org/250803013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266519 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/trees/layer_tree_host_common_unittest.cc
+++ cc/trees/layer_tree_host_common_unittest.cc
@@ -1457,7 +1457,6 @@ TEST_F(LayerTreeHostCommonTest,
   // are unexpected at draw time (e.g. we might try to create a content texture
   // of size 0).
   ASSERT_TRUE(parent->render_surface());
-  ASSERT_FALSE(render_surface1->render_surface());
   EXPECT_EQ(1U, render_surface_layer_list.size());
 }
 
@@ -2167,9 +2166,6 @@ TEST_F(LayerTreeHostCommonTest, ClipRectIsPropagatedCorrectlyToSurfaces) {
   ASSERT_TRUE(grand_child1->render_surface());
   ASSERT_TRUE(grand_child2->render_surface());
   ASSERT_TRUE(grand_child3->render_surface());
-  // Because grand_child4 is entirely clipped, it is expected to not have a
-  // render surface.
-  EXPECT_FALSE(grand_child4->render_surface());
 
   // Surfaces are clipped by their parent, but un-affected by the owning layer's
   // masksToBounds.
