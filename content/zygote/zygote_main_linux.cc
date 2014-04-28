eb4e0bb8c24be1c26d0ae3aee20ef46bd5ac7b33
mdempsky@chromium.org <mdempsky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-28 19:07:56 +0000

    Use RecvMsgWithPid to find zygote PID instead of chrome-sandbox
    BUG=357670
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/258893004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266618 0039d316-1c4b-4281-b951-d872f2087c98

--- content/zygote/zygote_main_linux.cc
+++ content/zygote/zygote_main_linux.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/linux_util.h"
 #include "base/native_library.h"
 #include "base/pickle.h"
+#include "base/posix/eintr_wrapper.h"
 #include "base/posix/unix_domain_socket_linux.h"
 #include "base/rand_util.h"
 #include "base/sys_info.h"
@@ -345,19 +346,10 @@ static void ZygotePreSandboxInit() {
       new FontConfigIPC(GetSandboxFD()))->unref();
 }
 
-static void CloseFdAndHandleEintr(int fd) {
-  close(fd);
-}
-
 static bool CreateInitProcessReaper() {
-  // This "magic" socket must only appear in one process, so make sure
-  // it gets closed in the parent after fork().
-  base::Closure zygoteid_fd_closer =
-      base::Bind(CloseFdAndHandleEintr, kZygoteIdFd);
   // The current process becomes init(1), this function returns from a
   // newly created process.
-  const bool init_created =
-      sandbox::CreateInitProcessReaper(&zygoteid_fd_closer);
+  const bool init_created = sandbox::CreateInitProcessReaper(NULL);
   if (!init_created) {
     LOG(ERROR) << "Error creating an init process to reap zombies";
     return false;
@@ -461,6 +453,13 @@ bool ZygoteMain(const MainFunctionParams& params,
 
   const bool must_enable_setuid_sandbox =
       linux_sandbox->setuid_sandbox_client()->IsSuidSandboxChild();
+  if (must_enable_setuid_sandbox) {
+    // When we're launched through the setuid sandbox, ZygoteHostImpl::Init
+    // arranges for kZygoteIdFd to be a dummy file descriptor to satisfy an
+    // ancient setuid sandbox ABI requirement. However, the descriptor is no
+    // longer needed, so we can simply close it right away now.
+    CHECK_EQ(0, IGNORE_EINTR(close(kZygoteIdFd)));
+  }
 
   if (forkdelegate != NULL) {
     VLOG(1) << "ZygoteMain: initializing fork delegate";
