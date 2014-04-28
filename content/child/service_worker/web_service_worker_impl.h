a209ce2e54c0ca3f75d210635bd05fe27596bc40
falken@chromium.org <falken@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-28 13:50:28 +0000

    ServiceWorker: Queue worker state changes until Blink is ready for them
    Chromium-side part of the following:
    https://codereview.chromium.org/247263010/
    https://codereview.chromium.org/256723002/
    
    BUG=365252
    
    Review URL: https://codereview.chromium.org/255813003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266544 0039d316-1c4b-4281-b951-d872f2087c98

--- content/child/service_worker/web_service_worker_impl.h
+++ content/child/service_worker/web_service_worker_impl.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_CHILD_SERVICE_WORKER_WEB_SERVICE_WORKER_IMPL_H_
 #define CONTENT_CHILD_SERVICE_WORKER_WEB_SERVICE_WORKER_IMPL_H_
 
+#include <vector>
+
 #include "base/compiler_specific.h"
 #include "base/memory/ref_counted.h"
 #include "base/strings/string16.h"
@@ -29,9 +31,13 @@ class WebServiceWorkerImpl
                        ThreadSafeSender* thread_safe_sender);
   virtual ~WebServiceWorkerImpl();
 
-  void SetState(blink::WebServiceWorkerState new_state);
+  // Notifies that the service worker's state changed. This function may queue
+  // the state change for later processing, if the proxy is not yet ready to
+  // handle state changes.
+  void OnStateChanged(blink::WebServiceWorkerState new_state);
 
   virtual void setProxy(blink::WebServiceWorkerProxy* proxy);
+  virtual void proxyReadyChanged();
   virtual blink::WebURL scope() const;
   virtual blink::WebURL url() const;
   virtual blink::WebServiceWorkerState state() const;
@@ -39,12 +45,16 @@ class WebServiceWorkerImpl
                            blink::WebMessagePortChannelArray* channels);
 
  private:
+  // Commits the new state internally and notifies the proxy of the change.
+  void ChangeState(blink::WebServiceWorkerState new_state);
+
   const int handle_id_;
   const GURL scope_;
   const GURL url_;
   blink::WebServiceWorkerState state_;
   scoped_refptr<ThreadSafeSender> thread_safe_sender_;
   blink::WebServiceWorkerProxy* proxy_;
+  std::vector<blink::WebServiceWorkerState> queued_states_;
 
   DISALLOW_COPY_AND_ASSIGN(WebServiceWorkerImpl);
 };
