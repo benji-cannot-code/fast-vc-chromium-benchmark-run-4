73ae2b6089c8133cf40c9c93419a5c901f4fe8d4
ch.dumez@samsung.com <ch.dumez@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-28 15:38:12 +0000

    Make NodeIterator.detach() a no-op
    Make NodeIterator.detach() a no-op as per the DOM specification:
    http://dom.spec.whatwg.org/#dom-nodeiterator-detach
    
    This makes the code simpler, and a bit more efficient.
    
    Firefox already does this since version 22:
    https://bugzilla.mozilla.org/show_bug.cgi?id=823549
    
    However, this method is not a no-op in IE11.
    
    R=arv@chromium.org, tkent@chromium.org
    BUG=367558
    TEST=fast/dom/NodeIterator/detach-no-op.html
    
    Review URL: https://codereview.chromium.org/256013002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172768 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/NodeIterator.cpp
+++ third_party/WebKit/Source/core/dom/NodeIterator.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/Document.h"
 #include "core/dom/ExceptionCode.h"
 #include "core/dom/NodeTraversal.h"
+#include "core/frame/UseCounter.h"
 
 namespace WebCore {
 
@@ -75,7 +76,6 @@ bool NodeIterator::NodePointer::moveToPrevious(Node* root)
 NodeIterator::NodeIterator(PassRefPtr<Node> rootNode, unsigned whatToShow, PassRefPtr<NodeFilter> filter)
     : NodeIteratorBase(rootNode, whatToShow, filter)
     , m_referenceNode(root(), true)
-    , m_detached(false)
 {
     ScriptWrappable::init(this);
     root()->document().attachNodeIterator(this);
@@ -88,11 +88,6 @@ NodeIterator::~NodeIterator()
 
 PassRefPtr<Node> NodeIterator::nextNode(ExceptionState& exceptionState)
 {
-    if (m_detached) {
-        exceptionState.throwDOMException(InvalidStateError, "The iterator is detached.");
-        return nullptr;
-    }
-
     RefPtr<Node> result;
 
     m_candidateNode = m_referenceNode;
@@ -117,11 +112,6 @@ PassRefPtr<Node> NodeIterator::nextNode(ExceptionState& exceptionState)
 
 PassRefPtr<Node> NodeIterator::previousNode(ExceptionState& exceptionState)
 {
-    if (m_detached) {
-        exceptionState.throwDOMException(InvalidStateError, "The iterator is detached.");
-        return nullptr;
-    }
-
     RefPtr<Node> result;
 
     m_candidateNode = m_referenceNode;
@@ -146,9 +136,7 @@ PassRefPtr<Node> NodeIterator::previousNode(ExceptionState& exceptionState)
 
 void NodeIterator::detach()
 {
-    root()->document().detachNodeIterator(this);
-    m_detached = true;
-    m_referenceNode.node.clear();
+    // This is now a no-op as per the DOM specification.
 }
 
 void NodeIterator::nodeWillBeRemoved(Node& removedNode)
@@ -159,7 +147,6 @@ void NodeIterator::nodeWillBeRemoved(Node& removedNode)
 
 void NodeIterator::updateForNodeRemoval(Node& removedNode, NodePointer& referenceNode) const
 {
-    ASSERT(!m_detached);
     ASSERT(root()->document() == removedNode.document());
 
     // Iterator is not affected if the removed node is the reference node and is the root.
--- third_party/WebKit/Source/core/dom/NodeIterator.h
+++ third_party/WebKit/Source/core/dom/NodeIterator.h
@@ -71,7 +71,6 @@ private:
 
     NodePointer m_referenceNode;
     NodePointer m_candidateNode;
-    bool m_detached;
 };
 
 } // namespace WebCore
