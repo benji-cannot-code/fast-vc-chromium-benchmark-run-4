90a1a5d19518cb1069242c3c3e63e92d242c09cb
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-28 19:50:04 +0000

    Update VideoFrameScheduler API and add clockless and testing implementations.
    API changes:
      - Frame done callbacks must not be reentrant
      - Reset is fire-and-forget; frames are no longer return via the done callback
    
    BUG=110814
    
    Review URL: https://codereview.chromium.org/257793004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266635 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ media/filters/clockless_video_frame_scheduler.h
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MEDIA_FILTERS_CLOCKLESS_VIDEO_FRAME_SCHEDULER_H_
+#define MEDIA_FILTERS_CLOCKLESS_VIDEO_FRAME_SCHEDULER_H_
+
+#include "media/filters/video_frame_scheduler.h"
+
+namespace media {
+
+// A scheduler that immediately displays frames.
+class ClocklessVideoFrameScheduler : public VideoFrameScheduler {
+ public:
+  typedef base::Callback<void(const scoped_refptr<VideoFrame>&)> DisplayCB;
+
+  explicit ClocklessVideoFrameScheduler(const DisplayCB& display_cb);
+  virtual ~ClocklessVideoFrameScheduler();
+
+  // VideoFrameScheduler implementation.
+  virtual void ScheduleVideoFrame(const scoped_refptr<VideoFrame>& frame,
+                                  base::TimeTicks wall_ticks,
+                                  const DoneCB& done_cb) OVERRIDE;
+  virtual void Reset() OVERRIDE;
+
+ private:
+  DisplayCB display_cb_;
+
+  DISALLOW_COPY_AND_ASSIGN(ClocklessVideoFrameScheduler);
+};
+
+}  // namespace media
+
+#endif  // MEDIA_FILTERS_CLOCKLESS_VIDEO_FRAME_SCHEDULER_H_
--- media/filters/video_frame_scheduler_impl_unittest.cc
+++ media/filters/video_frame_scheduler_impl_unittest.cc
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
+using testing::_;
+
 // NOTE: millisecond-level resolution is used for times as real delayed tasks
 // are posted. Don't use large values if you want to keep tests running fast.
 class VideoFrameSchedulerImplTest : public testing::Test {
@@ -139,8 +141,8 @@ TEST_F(VideoFrameSchedulerImplTest, Reset) {
       VideoFrame::CreateBlackFrame(gfx::Size(8, 8));
   Schedule(frame, 10);
 
-  // Despite being on time, frames are returned immediately.
-  EXPECT_CALL(*this, OnFrameDone(frame, VideoFrameScheduler::RESET));
+  // Despite being on time, frame callback isn't run.
+  EXPECT_CALL(*this, OnFrameDone(_, _)).Times(0);
   AdvanceTime(10);
   Reset();
   RunUntilTimeHasElapsed(10);
