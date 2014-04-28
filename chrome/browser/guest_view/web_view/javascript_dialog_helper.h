995c25d6f8910486fea3973586770bd3b055788d
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-28 12:29:59 +0000

    Revert "Revert 266297 "1. Handle the case of empty embedder_extension_id...""
    Retry r266297 with static initializer fix.
    
    This reverts commit 9ca50c9f36065f24fb1e39dbbf6f09b52eac285e.
    
    BUG=351824
    TBR=rmsousa@chromium.org
    
    Review URL: https://codereview.chromium.org/259943002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266523 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/guest_view/web_view/javascript_dialog_helper.h
+++ chrome/browser/guest_view/web_view/javascript_dialog_helper.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_GUESTVIEW_WEBVIEW_JAVASCRIPT_DIALOG_HELPER_H_
-#define CHROME_BROWSER_GUESTVIEW_WEBVIEW_JAVASCRIPT_DIALOG_HELPER_H_
+#ifndef CHROME_BROWSER_GUEST_VIEW_WEB_VIEW_JAVASCRIPT_DIALOG_HELPER_H_
+#define CHROME_BROWSER_GUEST_VIEW_WEB_VIEW_JAVASCRIPT_DIALOG_HELPER_H_
 
 #include "content/public/browser/javascript_dialog_manager.h"
 
@@ -51,4 +51,4 @@ class JavaScriptDialogHelper : public content::JavaScriptDialogManager {
   DISALLOW_COPY_AND_ASSIGN(JavaScriptDialogHelper);
 };
 
-#endif  // CHROME_BROWSER_GUESTVIEW_WEBVIEW_JAVASCRIPT_DIALOG_HELPER_H_
+#endif  // CHROME_BROWSER_GUEST_VIEW_WEB_VIEW_JAVASCRIPT_DIALOG_HELPER_H_
