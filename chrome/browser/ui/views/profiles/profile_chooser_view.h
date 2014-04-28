f7fa4269b823f4e304dc65a7da1fc6cc85a44a81
noms@chromium.org <noms@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-28 06:41:54 +0000

    [Win] Redesign the avatar bubble UI
    Mocks: https://docs.google.com/a/google.com/file/d/0BxMIIGI80eU-VW16NEloUFdyaXc/edit
    
    This CL is related to its Mac cousin, https://codereview.chromium.org/235833002/.
    Like the related CL, this one also does not implement the "round" avatar icon, nor the
    top-left corner info button, but it does set up the layout for the other elements.
    
    I've also added the option to be able to center the text in a textfield.
    
    BUG=353398
    
    Review URL: https://codereview.chromium.org/249813002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266447 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/profiles/profile_chooser_view.h
+++ chrome/browser/ui/views/profiles/profile_chooser_view.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "google_apis/gaia/oauth2_token_service.h"
 #include "ui/views/bubble/bubble_delegate.h"
 #include "ui/views/controls/button/button.h"
-#include "ui/views/controls/button/menu_button_listener.h"
 #include "ui/views/controls/link_listener.h"
 #include "ui/views/controls/styled_label_listener.h"
 #include "ui/views/controls/textfield/textfield_controller.h"
@@ -41,7 +40,6 @@ class Browser;
 class ProfileChooserView : public views::BubbleDelegateView,
                            public views::ButtonListener,
                            public views::LinkListener,
-                           public views::MenuButtonListener,
                            public views::StyledLabelListener,
                            public views::TextfieldController,
                            public AvatarMenuObserver,
@@ -88,7 +86,7 @@ class ProfileChooserView : public views::BubbleDelegateView,
 
   typedef std::vector<size_t> Indexes;
   typedef std::map<views::Button*, int> ButtonIndexes;
-  typedef std::map<views::View*, std::string> AccountButtonIndexes;
+  typedef std::map<views::Button*, std::string> AccountButtonIndexes;
 
   ProfileChooserView(views::View* anchor_view,
                      views::BubbleBorder::Arrow arrow,
@@ -107,10 +105,6 @@ class ProfileChooserView : public views::BubbleDelegateView,
 
   // views::LinkListener:
   virtual void LinkClicked(views::Link* sender, int event_flags) OVERRIDE;
-
-  // views::MenuButtonListener:
-  virtual void OnMenuButtonClicked(views::View* source,
-                                   const gfx::Point& point) OVERRIDE;
 
   // views::StyledLabelListener implementation.
   virtual void StyledLabelLinkClicked(
@@ -152,8 +146,6 @@ class ProfileChooserView : public views::BubbleDelegateView,
   views::View* CreateOptionsView(bool enable_lock);
 
   // Account Management view for the profile |avatar_item|.
-  views::View* CreateCurrentProfileEditableView(
-      const AvatarMenu::Item& avatar_item);
   views::View* CreateCurrentProfileAccountsView(
       const AvatarMenu::Item& avatar_item);
   void CreateAccountButton(views::GridLayout* layout,
@@ -210,7 +202,7 @@ class ProfileChooserView : public views::BubbleDelegateView,
 
   // Links displayed in the active profile card.
   views::Link* manage_accounts_link_;
-  views::Link* signin_current_profile_link_;
+  views::LabelButton* signin_current_profile_link_;
 
   // The profile name and photo in the active profile card. Owned by the
   // views hierarchy.
@@ -220,7 +212,7 @@ class ProfileChooserView : public views::BubbleDelegateView,
   // Action buttons.
   views::LabelButton* users_button_;
   views::LabelButton* lock_button_;
-  views::LabelButton* add_account_button_;
+  views::Link* add_account_link_;
 
   // Buttons displayed in the gaia signin view.
   views::ImageButton* gaia_signin_cancel_button_;
