7c2f1d85615343f9eb155d11eb5c56029d10f55f
costan@gmail.com <costan@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-28 06:45:13 +0000

    Remove contentDisposition from WebBlobData.
    contentDisposition is not used anywhere, so it is dead code.
    
    Review URL: https://codereview.chromium.org/250923006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172729 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/blob/BlobData.h
+++ third_party/WebKit/Source/platform/blob/BlobData.h
@@ -170,9 +170,6 @@ public:
     const String& contentType() const { return m_contentType; }
     void setContentType(const String& contentType) { m_contentType = contentType; }
 
-    const String& contentDisposition() const { return m_contentDisposition; }
-    void setContentDisposition(const String& contentDisposition) { m_contentDisposition = contentDisposition; }
-
     const BlobDataItemList& items() const { return m_items; }
     void swapItems(BlobDataItemList&);
 
@@ -199,7 +196,6 @@ private:
     BlobData() { }
 
     String m_contentType;
-    String m_contentDisposition;
     BlobDataItemList m_items;
 };
 
