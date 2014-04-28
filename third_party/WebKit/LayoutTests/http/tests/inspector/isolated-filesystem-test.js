b185c146b9a9c82bcffa504642c4b416de55bced
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-28 05:36:00 +0000

    DevTools: Fix normalizing file system paths on windows.
    BUG=367040
    
    Review URL: https://codereview.chromium.org/250933003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172726 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/inspector/isolated-filesystem-test.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/isolated-filesystem-test.js
@@ -89,7 +89,9 @@ MockIsolatedFileSystem.prototype = {
     }
 }
 
+var normalizePath = WebInspector.IsolatedFileSystem.normalizePath
 WebInspector.IsolatedFileSystem = MockIsolatedFileSystem;
+WebInspector.IsolatedFileSystem.normalizePath = normalizePath;
 
 var MockIsolatedFileSystemManager = function() {};
 MockIsolatedFileSystemManager.prototype = {
--- third_party/WebKit/Source/devtools/front_end/sdk/IsolatedFileSystem.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/IsolatedFileSystem.js
@@ -69,6 +69,17 @@ WebInspector.IsolatedFileSystem.errorMessage = function(error)
     return WebInspector.UIString("File system error: %s", msg);
 }
 
+/**
+ * @param {string} fileSystemPath
+ * @return {string}
+ */
+WebInspector.IsolatedFileSystem.normalizePath = function(fileSystemPath)
+{
+    if (WebInspector.isWin())
+        return fileSystemPath.replace(/\\/g, "/");
+    return fileSystemPath;
+}
+
 WebInspector.IsolatedFileSystem.prototype = {
     /**
      * @return {string}
@@ -85,9 +96,7 @@ WebInspector.IsolatedFileSystem.prototype = {
     {
         if (this._normalizedPath)
             return this._normalizedPath;
-        this._normalizedPath = this._path;
-        if (WebInspector.isWin())
-            this._normalizedPath = this._normalizedPath.replace(/\\/g, "/");
+        this._normalizedPath = WebInspector.IsolatedFileSystem.normalizePath(this._path);
         return this._normalizedPath;
     },
 
