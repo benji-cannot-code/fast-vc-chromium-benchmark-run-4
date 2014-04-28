b737204d6c75d5487f72b625342ef3a582c4c752
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-28 06:59:20 +0000

    [Telemetry] Enable the measurement smoke test on android.
    This required disabling the benchmarks that were causing it to fail. It looks
    like those failures were due to lack of tab support in ChromeShell.
    
    BUG=337829
    
    Review URL: https://codereview.chromium.org/250593003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266457 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/benchmarks/memory_pressure.py
+++ tools/perf/benchmarks/memory_pressure.py
@@ -3,9 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from telemetry import test
 from measurements import memory_pressure
+from telemetry import test
 
+
+@test.Disabled('android')
 class MemoryPressure(test.Test):
   test = memory_pressure.MemoryPressure
   page_set = 'page_sets/typical_25.py'
