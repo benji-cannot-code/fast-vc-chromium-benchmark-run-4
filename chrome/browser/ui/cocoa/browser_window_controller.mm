6b5e9544ee64ce891510d343cda6863a8578803e
ajwong@chromium.org <ajwong@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-28 23:18:33 +0000

    Revert 266653 "[Mac] Allow fullscreen transitions while constrai..."
    Mac Tests (dbg) failing for PrintPreviewTest.TaskManagerNewPrintPreview
    http://build.chromium.org/p/chromium.mac/builders/Mac%2010.7%20Tests%20%28dbg%29%281%29/builds/23012
    
    
    > [Mac] Allow fullscreen transitions while constrained windows are open.
    > 
    > r166410 disabled the browser from entering fullscreen mode when any one
    > tab has a constrained window open (e.g., Print dialog).  However, this
    > is inconsistent with the behavior on all other desktop platforms.  This
    > change removes the restriction.
    > 
    > Landing on behalf of miu@chromium.org manually, as to make a branch cut.
    > 
    > BUG=366567,146451
    > TEST=Repro steps in bug 366567 (easier to understand from the multiple demo videos).  Also, manual testing of browser transition into and out of fullscreen before/after a print dialog is open.
    > R=avi@chromium.org, sky@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/253493005
    
    TBR=avi@chromium.org
    
    Review URL: https://codereview.chromium.org/252693007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266683 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/browser_window_controller.mm
+++ chrome/browser/ui/cocoa/browser_window_controller.mm
@@ -2084,6 +2084,9 @@ willAnimateFromState:(BookmarkBar::State)oldState
   if (fullscreen == [self isFullscreen])
     return;
 
+  if (!chrome::IsCommandEnabled(browser_.get(), IDC_FULLSCREEN))
+    return;
+
   if (fullscreen) {
     const BOOL shouldUseSystemFullscreen =
         chrome::mac::SupportsSystemFullscreen() && !fullscreenWindow_ &&
