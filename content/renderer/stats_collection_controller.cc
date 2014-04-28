8308564f07829706a947d828ee5645e9a92d2b6c
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-28 13:50:59 +0000

    Make sure gin Handles exist before using them
    When we run out of stack space for example, we can't create an handle.
    
    BUG=363417
    R=dcarney@chromium.org
    
    Review URL: https://codereview.chromium.org/259993003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266545 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/stats_collection_controller.cc
+++ content/renderer/stats_collection_controller.cc
@@ -88,6 +88,8 @@ void StatsCollectionController::Install(blink::WebFrame* frame) {
 
   gin::Handle<StatsCollectionController> controller =
       gin::CreateHandle(isolate, new StatsCollectionController());
+  if (controller.IsEmpty())
+    return;
   v8::Handle<v8::Object> global = context->Global();
   global->Set(gin::StringToV8(isolate, "statsCollectionController"),
               controller.ToV8());
