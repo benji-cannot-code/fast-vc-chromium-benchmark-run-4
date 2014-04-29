4b11b16cb5e5b90bc0525718b9d8337b7e76988e
machenbach@chromium.org <machenbach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-29 13:06:25 +0000

    Revert of [WebsiteSettings] Update permission bubble manager policy (https://codereview.chromium.org/243543003/)
    Reason for revert:
    This breaks Linux ASAN, heap use after free:
    http://build.chromium.org/p/chromium.memory/builders/Linux%20ASan%20LSan%20Tests%20%283%29/builds/2360
    
    Original issue's description:
    > [WebsiteSettings] Update permission bubble manager policy
    > 
    > This policy is more in accord with the design doc changes. Moves to
    > showing the dialog upon DOMContentLoaded in source pages, and then
    > subsequently for user gestures.
    > 
    > R=leng@chromium.org
    > BUG=332115,364159
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=266855
    
    TBR=leng@chromium.org,markusheintz@chromium.org,rdsmith@chromium.org,phajdan.jr@chromium.org,gbillock@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=332115,364159
    
    Review URL: https://codereview.chromium.org/260373002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266861 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/website_settings/permission_bubble_manager.h
+++ chrome/browser/ui/website_settings/permission_bubble_manager.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "base/timer/timer.h"
 #include "chrome/browser/ui/website_settings/permission_bubble_view.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
@@ -56,6 +57,10 @@ class PermissionBubbleManager
   // take ownership of the view.
   virtual void SetView(PermissionBubbleView* view) OVERRIDE;
 
+ protected:
+  // Sets the coalesce time interval to |interval_ms|. For testing only.
+  void SetCoalesceIntervalForTesting(int interval_ms);
+
  private:
   friend class PermissionBubbleManagerTest;
   friend class DownloadRequestLimiterTest;
@@ -63,12 +68,15 @@ class PermissionBubbleManager
 
   explicit PermissionBubbleManager(content::WebContents* web_contents);
 
-  // WebContentsObserver:
-
-  // TODO(leng): Finalize policy for permission requests with iFrames.
-  // DocumentLoadedInFrame() might be needed as well.
-  virtual void DocumentOnLoadCompletedInMainFrame(int32 page_id) OVERRIDE;
-
+  // contents::WebContentsObserver:
+  // TODO(leng):  Investigate the ordering and timing of page loading and
+  // permission requests with iFrames. DocumentOnLoadCompletedInMainFrame()
+  // and DocumentLoadedInFrame() might be needed as well.
+  virtual void DidFinishLoad(
+      int64 frame_id,
+      const GURL& validated_url,
+      bool is_main_frame,
+      content::RenderViewHost* render_view_host) OVERRIDE;
   // If a page on which permissions requests are pending is navigated,
   // they will be finalized as if canceled by the user.
   virtual void NavigationEntryCommitted(
@@ -83,6 +91,7 @@ class PermissionBubbleManager
   virtual void Deny() OVERRIDE;
   virtual void Closing() OVERRIDE;
 
+  // Called when the coalescing timer is done. Presents the bubble.
   void ShowBubble();
 
   // Finalize the pending permissions request.
@@ -102,9 +111,10 @@ class PermissionBubbleManager
   std::vector<PermissionBubbleRequest*> requests_;
   std::vector<PermissionBubbleRequest*> queued_requests_;
   GURL request_url_;
-  bool request_url_has_loaded_;
   std::vector<bool> accept_states_;
   bool customization_mode_;
+
+  scoped_ptr<base::Timer> timer_;
 };
 
 #endif  // CHROME_BROWSER_UI_WEBSITE_SETTINGS_PERMISSION_BUBBLE_MANAGER_H_
