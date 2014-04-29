38afea6296996abd6ba1a80765ec9a2e97a1d705
kbr@chromium.org <kbr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-29 03:52:52 +0000

    Add win_gpu to the default set of try servers for Blink CLs.
    This configuration is successfully handling the experimental load from
    the Chromium and Blink commit queues. Expand its testing.
    
    BUG=353864
    
    Review URL: https://codereview.chromium.org/253873007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172839 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/PRESUBMIT.py
+++ third_party/WebKit/PRESUBMIT.py
@@ -327,5 +327,6 @@ def GetPreferredTryMasters(project, change):
         },
         'tryserver.chromium.gpu': {
             'linux_gpu': set(['defaulttests']),
+            'win_gpu': set(['defaulttests']),
         }
     }
