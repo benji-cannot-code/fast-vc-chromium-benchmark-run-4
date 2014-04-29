c752de3cb11983fe255d80536fd3d2f1020ebaec
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-29 12:01:41 +0000

    Don't upload extension IDs in the cloud policy protocol.
    The cloud policy protocol can also be used to fetch policy for extensions; this change makes an update to the protocol such that extension IDs are never uploaded to the server.
    
    Note that this feature is still disabled by default behind the --enable-component-cloud-policy flag.
    
    BUG=361156
    
    Review URL: https://codereview.chromium.org/233423002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266857 0039d316-1c4b-4281-b951-d872f2087c98

--- components/policy/core/common/cloud/component_cloud_policy_service.h
+++ components/policy/core/common/cloud/component_cloud_policy_service.h
@@ -122,7 +122,7 @@ class POLICY_EXPORT ComponentCloudPolicyService
 
   void InitializeIfReady();
   void OnBackendInitialized(scoped_ptr<PolicyBundle> initial_policy);
-  void SetCurrentSchema();
+  void ReloadSchema();
   void OnPolicyUpdated(scoped_ptr<PolicyBundle> policy);
 
   Delegate* delegate_;
