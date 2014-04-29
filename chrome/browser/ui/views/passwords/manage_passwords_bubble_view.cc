3ac56fe84c9abd18ba39371bccdd5bcc18183283
machenbach@chromium.org <machenbach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-29 13:11:30 +0000

    Revert of Password bubble: Introduce a command to open the bubble. (https://codereview.chromium.org/246393004/)
    Reason for revert:
    This is causing leaks on linux asan lsan:
    http://build.chromium.org/p/chromium.memory/builders/Linux%20ASan%20LSan%20Tests%20%283%29/builds/2361
    
    Original issue's description:
    > Password bubble: ManagePasswordsIconView is now a BubbleIconView.
    > 
    > This CL introduces a new browser command to open the Manage Passwords bubble,
    > and converts the ManagePasswordsIconView class into a subclass of
    > BubbleIconView, which uses the new command to control the bubble's state.
    > This allows us to more easily test the view and the UI controller, as each
    > object's job is now more clearly defined (and the view is now doing a good
    > deal less work), and to independently verify that the command is doing the
    > right thing.
    > 
    > After this CL, we'll (finally!) have something approaching reasonable test
    > coverage for the core of the views code.
    > 
    > BUG=365678
    > TBR=cpu@chromium.org
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=266859
    
    TBR=markusheintz@chromium.org,vabr@chromium.org,cpu@chromium.org,pkasting@chromium.org,mkwst@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=365678
    
    Review URL: https://codereview.chromium.org/256333003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266862 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/passwords/manage_passwords_bubble_view.cc
+++ chrome/browser/ui/views/passwords/manage_passwords_bubble_view.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/passwords/manage_passwords_bubble_model.h"
-#include "chrome/browser/ui/passwords/manage_passwords_bubble_ui_controller.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/location_bar/location_bar_view.h"
 #include "chrome/browser/ui/views/passwords/manage_password_item_view.h"
@@ -75,23 +74,6 @@ class SavePasswordRefusalComboboxModel : public ui::ComboboxModel {
 };
 
 }  // namespace
-
-
-// Globals --------------------------------------------------------------------
-
-namespace chrome {
-
-void ShowManagePasswordsBubble(content::WebContents* web_contents) {
-  ManagePasswordsBubbleUIController* controller =
-      ManagePasswordsBubbleUIController::FromWebContents(web_contents);
-  ManagePasswordsBubbleView::ShowBubble(
-      web_contents,
-      controller->manage_passwords_bubble_needs_showing() ?
-          ManagePasswordsBubbleView::AUTOMATIC :
-          ManagePasswordsBubbleView::USER_ACTION);
-}
-
-}  // namespace chrome
 
 
 // ManagePasswordsBubbleView --------------------------------------------------
