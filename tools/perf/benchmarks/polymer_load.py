63c67014e22209e5c6523b3b5a4728fa029d7e21
ykyyip@chromium.org <ykyyip@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-29 20:07:49 +0000

    copy the pica benchmark to polymer_load
    Rename the pica benchmark to something more generic
    to reuse with other page sets. The pica benchmark
    will be removed after it's also renamed in the
    perf bot.
    
    R=dtu@chromium.org,adamk@chromium.org,tonyg@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/255063002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266943 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/perf/benchmarks/polymer_load.py
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from measurements import polymer_load
+from telemetry import test
+
+
+class PolymerLoadPica(test.Test):
+  """Measures time to polymer-ready for PICA
+  """
+  test = polymer_load.PolymerLoadMeasurement
+  page_set = 'page_sets/pica.py'
