51d855a84564be192b9758fdeb62389e2acec04e
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-29 01:52:52 +0000

    Mojo: Add String type to codec.js.
    Also simplify the encoding/decoding code in the bindings generator.
    
    Review URL: https://codereview.chromium.org/250253002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266733 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/public/js/bindings/codec.js
+++ mojo/public/js/bindings/codec.js
@@ -230,6 +230,10 @@ define("mojo/public/js/bindings/codec", [
     return val;
   };
 
+  Decoder.prototype.decodeStruct = function(cls) {
+    return cls.decode(this);
+  };
+
   Decoder.prototype.decodeStructPointer = function(cls) {
     return cls.decode(this.decodeAndCreateDecoder());
   };
@@ -357,6 +361,10 @@ define("mojo/public/js/bindings/codec", [
     for (var i = 0; i < numberOfElements; ++i) {
       cls.encode(this, val[i]);
     }
+  };
+
+  Encoder.prototype.encodeStruct = function(cls, val) {
+    return cls.encode(this, val);
   };
 
   Encoder.prototype.encodeStructPointer = function(cls, val) {
@@ -489,6 +497,10 @@ define("mojo/public/js/bindings/codec", [
     return decoder.readInt8();
   };
 
+  Int8.encode = function(encoder, val) {
+    encoder.writeInt8(val);
+  };
+
   Uint8.encode = function(encoder, val) {
     encoder.writeUint8(val);
   };
@@ -584,8 +596,19 @@ define("mojo/public/js/bindings/codec", [
     encoder.writeUint64(val);
   };
 
-  // TODO(abarth): Add missing types:
-  // * String
+  function String() {
+  };
+
+  String.encodedSize = 8;
+
+  String.decode = function(decoder) {
+    return decoder.decodeStringPointer();
+  };
+
+  String.encode = function(encoder, val) {
+    encoder.encodeStringPointer(val);
+  };
+
 
   function Float() {
   }
@@ -635,11 +658,11 @@ define("mojo/public/js/bindings/codec", [
   ArrayOf.prototype.encodedSize = 8;
 
   ArrayOf.prototype.decode = function(decoder) {
-    return decoder.decodeArrayPointer(self.cls);
+    return decoder.decodeArrayPointer(this.cls);
   };
 
   ArrayOf.prototype.encode = function(encoder, val) {
-    encoder.encodeArrayPointer(self.cls, val);
+    encoder.encodeArrayPointer(this.cls, val);
   };
 
   function Handle() {
@@ -677,6 +700,7 @@ define("mojo/public/js/bindings/codec", [
   exports.Uint64 = Uint64;
   exports.Float = Float;
   exports.Double = Double;
+  exports.String = String;
   exports.PointerTo = PointerTo;
   exports.ArrayOf = ArrayOf;
   exports.Handle = Handle;
