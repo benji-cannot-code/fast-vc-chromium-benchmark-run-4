8ce53648ea7f4fd9fbf4f97338d65f0c00b999e5
nhiroki@chromium.org <nhiroki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-29 09:46:00 +0000

    ServiceWorker: Initialize member variable in ctor of ServiceWorkerDatabase
    BUG=none
    TEST=content_unittests --gtest_filter=ServiceWorkerDatabase.* (with valgrind)
    TBR=michaeln@chromium.org
    
    Review URL: https://codereview.chromium.org/253103003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266811 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/service_worker/service_worker_database.cc
+++ content/browser/service_worker/service_worker_database.cc
@@ -156,7 +156,8 @@ ServiceWorkerDatabase::ServiceWorkerDatabase(const base::FilePath& path)
       next_avail_resource_id_(0),
       next_avail_version_id_(0),
       is_disabled_(false),
-      was_corruption_detected_(false) {
+      was_corruption_detected_(false),
+      is_initialized_(false) {
 }
 
 ServiceWorkerDatabase::~ServiceWorkerDatabase() {
