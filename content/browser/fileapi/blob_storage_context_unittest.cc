ff2d731c16b2f8f943ed98a8a15ea46f7f225bd9
machenbach@chromium.org <machenbach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-29 10:58:20 +0000

    Revert of Allow BlobDataHandles to be copied, and have their UUIDs read, on any thread. (https://codereview.chromium.org/259773006/)
    Reason for revert:
    Causes leaks:
    http://build.chromium.org/p/chromium.memory/builders/Linux%20ASan%20LSan%20Tests%20%282%29/builds/2069
    
    Original issue's description:
    > Allow BlobDataHandles to be copied, and have their UUIDs read, on any thread.
    > 
    > BUG=108012
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=266817
    
    TBR=michaeln@chromium.org,ericu@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=108012
    
    Review URL: https://codereview.chromium.org/252163002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266837 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/fileapi/blob_storage_context_unittest.cc
+++ content/browser/fileapi/blob_storage_context_unittest.cc
@@ -42,7 +42,6 @@ TEST(BlobStorageContextTest, IncrementDecrementRef) {
   blob_data_handle = context.GetBlobDataFromUUID(kId);
   EXPECT_TRUE(blob_data_handle);
   blob_data_handle.reset();
-  fake_io_message_loop.RunUntilIdle();
 
   // Make sure its still there after inc/dec.
   EXPECT_TRUE(host.IncrementBlobRefCount(kId));
@@ -50,7 +49,6 @@ TEST(BlobStorageContextTest, IncrementDecrementRef) {
   blob_data_handle = context.GetBlobDataFromUUID(kId);
   EXPECT_TRUE(blob_data_handle);
   blob_data_handle.reset();
-  fake_io_message_loop.RunUntilIdle();
 
   // Make sure it goes away in the end.
   EXPECT_TRUE(host.DecrementBlobRefCount(kId));
@@ -85,7 +83,6 @@ TEST(BlobStorageContextTest, BlobDataHandle) {
   // Should disappear after dropping both handles.
   blob_data_handle.reset();
   another_handle.reset();
-  fake_io_message_loop.RunUntilIdle();
   blob_data_handle = context.GetBlobDataFromUUID(kId);
   EXPECT_FALSE(blob_data_handle);
 }
@@ -154,7 +151,6 @@ TEST(BlobStorageContextTest, PublicBlobUrls) {
   ASSERT_TRUE(blob_data_handle.get());
   EXPECT_EQ(kId, blob_data_handle->data()->uuid());
   blob_data_handle.reset();
-  fake_io_message_loop.RunUntilIdle();
 
   // The url registration should keep the blob alive even after
   // explicit references are dropped.
@@ -162,7 +158,6 @@ TEST(BlobStorageContextTest, PublicBlobUrls) {
   blob_data_handle = context.GetBlobDataFromPublicURL(kUrl);
   EXPECT_TRUE(blob_data_handle);
   blob_data_handle.reset();
-  fake_io_message_loop.RunUntilIdle();
 
   // Finally get rid of the url registration and the blob.
   EXPECT_TRUE(host.RevokePublicBlobURL(kUrl));
