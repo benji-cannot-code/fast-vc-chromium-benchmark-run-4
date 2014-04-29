4947d66ad72910ca69cbfeda5350cf291f3ab3d5
stepco@chromium.org <stepco@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-29 07:39:34 +0000

    Add ScopedStubEnterpriseInstallAttributes for tests to set test install attributes.
    This change is motivated by the need to set the test install attributes before the browser policy connector is initialized for the first time. In particular, test class members may cause the policy connector to be initialized in their constructors, so we also need to set the install attributes in the constructor of a member.
    
    BUG=243341
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=266094
    
    Review URL: https://codereview.chromium.org/247283007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266771 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/policy/browser_policy_connector_chromeos.cc
+++ chrome/browser/chromeos/policy/browser_policy_connector_chromeos.cc
@@ -85,8 +85,10 @@ BrowserPolicyConnectorChromeOS::BrowserPolicyConnectorChromeOS()
     : device_cloud_policy_manager_(NULL),
       global_user_cloud_policy_provider_(NULL),
       weak_ptr_factory_(this) {
-  if (g_testing_install_attributes)
+  if (g_testing_install_attributes) {
     install_attributes_.reset(g_testing_install_attributes);
+    g_testing_install_attributes = NULL;
+  }
 
   // SystemSaltGetter or DBusThreadManager may be uninitialized on unit tests.
 
@@ -96,7 +98,7 @@ BrowserPolicyConnectorChromeOS::BrowserPolicyConnectorChromeOS()
       chromeos::DBusThreadManager::IsInitialized()) {
     chromeos::CryptohomeClient* cryptohome_client =
         chromeos::DBusThreadManager::Get()->GetCryptohomeClient();
-    if (!g_testing_install_attributes) {
+    if (!install_attributes_) {
       install_attributes_.reset(
           new EnterpriseInstallAttributes(cryptohome_client));
     }
@@ -242,6 +244,13 @@ void BrowserPolicyConnectorChromeOS::SetInstallAttributesForTesting(
     EnterpriseInstallAttributes* attributes) {
   DCHECK(!g_testing_install_attributes);
   g_testing_install_attributes = attributes;
+}
+
+void BrowserPolicyConnectorChromeOS::RemoveInstallAttributesForTesting() {
+  if (g_testing_install_attributes) {
+    delete g_testing_install_attributes;
+    g_testing_install_attributes = NULL;
+  }
 }
 
 // static
