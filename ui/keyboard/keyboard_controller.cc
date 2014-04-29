cdf3ba985eaea2740c006b3b69bb945550d6ff68
bshe@chromium.org <bshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-29 01:53:28 +0000

    Fix white virtual keyboard at login screen
    BUG=367788
    
    Review URL: https://codereview.chromium.org/254743005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266734 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/keyboard/keyboard_controller.cc
+++ ui/keyboard/keyboard_controller.cc
@@ -317,9 +317,8 @@ void KeyboardController::OnWindowHierarchyChanged(
 }
 
 void KeyboardController::Reload() {
-  // Makes sure the keyboard window is initialized.
-  proxy_->GetKeyboardWindow();
-  proxy_->ReloadKeyboardIfNeeded();
+  if (proxy_->HasKeyboardWindow())
+    proxy_->ReloadKeyboardIfNeeded();
 }
 
 void KeyboardController::OnTextInputStateChanged(
