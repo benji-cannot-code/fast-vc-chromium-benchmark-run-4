c77f9c445e658b6b9d19e7638cfb4d79afe0b423
trchen@chromium.org <trchen@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-29 03:43:47 +0000

    Reland "Recompute overflow after transform changes"
    This CL adds a special code path that is very similar to simplifiedLayout.
    When the only change on an element is the transform, we can do the minimal
    work to recompute the layout overflow without much overhead from layout.
    
    This is done by adding an overflow recalculation phase after style changes,
    if a RenderObject doesn't need layout and has moved transformed children,
    the special code path will kick in.
    
    BUG=352460
    
    Review URL: https://codereview.chromium.org/250773002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172833 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -1890,6 +1890,7 @@ void Document::updateStyle(StyleRecalcChange change)
 
     ensureStyleResolver().printStats();
 
+    view()->recalcOverflowAfterStyleChange();
     view()->updateCompositingLayersAfterStyleChange();
 
     clearChildNeedsStyleRecalc();
--- third_party/WebKit/Source/core/rendering/RenderLayerScrollableArea.h
+++ third_party/WebKit/Source/core/rendering/RenderLayerScrollableArea.h
@@ -126,6 +126,7 @@ public:
 
     void updateAfterLayout();
     void updateAfterStyleChange(const RenderStyle*);
+    void updateAfterOverflowRecalc();
 
     virtual void updateAfterCompositingChange() OVERRIDE;
 
