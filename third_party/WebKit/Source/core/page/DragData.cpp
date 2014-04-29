1d7dd5060cacba35e07dbaa033c9e11b7443d462
c.shu@samsung.com <c.shu@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-29 03:26:54 +0000

    Remove modules/filesystem/DraggedIsolatedFileSystem.h dependency from core/DEPS.
    Review URL: https://codereview.chromium.org/256103007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172825 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/DragData.cpp
+++ third_party/WebKit/Source/core/page/DragData.cpp
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/Range.h"
 #include "core/editing/markup.h"
 #include "core/frame/LocalFrame.h"
-#include "modules/filesystem/DraggedIsolatedFileSystem.h"
 #include "platform/FileMetadata.h"
 #include "platform/clipboard/ClipboardMimeTypes.h"
 #include "platform/weborigin/KURL.h"
@@ -164,10 +163,7 @@ PassRefPtr<DocumentFragment> DragData::asFragment(LocalFrame* frame, PassRefPtrW
 
 String DragData::droppedFileSystemId() const
 {
-    DraggedIsolatedFileSystem* filesystem = DraggedIsolatedFileSystem::from(m_platformDragData);
-    if (!filesystem)
-        return String();
-    return filesystem->filesystemId();
+    return m_platformDragData->filesystemId();
 }
 
 } // namespace WebCore
