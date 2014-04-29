abea8bf8b83252e7dc604006c27cce2fbc670263
rsorokin@chromium.org <rsorokin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-29 09:01:34 +0000

    Start session fail causes restart chrome
    BUG=244628
    TEST=manual check on Acer C720
    
    Review URL: https://codereview.chromium.org/228703004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266791 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/dbus/session_manager_client.cc
+++ chromeos/dbus/session_manager_client.cc
@@ -72,7 +72,8 @@ class SessionManagerClientImpl : public SessionManagerClient {
                    weak_ptr_factory_.GetWeakPtr()));
   }
 
-  virtual void StartSession(const std::string& user_email) OVERRIDE {
+  virtual void StartSession(const std::string& user_email,
+                            const StartSessionCallback& callback) OVERRIDE {
     dbus::MethodCall method_call(login_manager::kSessionManagerInterface,
                                  login_manager::kSessionManagerStartSession);
     dbus::MessageWriter writer(&method_call);
@@ -82,7 +83,8 @@ class SessionManagerClientImpl : public SessionManagerClient {
         &method_call,
         dbus::ObjectProxy::TIMEOUT_USE_DEFAULT,
         base::Bind(&SessionManagerClientImpl::OnStartSession,
-                   weak_ptr_factory_.GetWeakPtr()));
+                   weak_ptr_factory_.GetWeakPtr(),
+                   callback));
   }
 
   virtual void StopSession() OVERRIDE {
@@ -333,10 +335,18 @@ class SessionManagerClientImpl : public SessionManagerClient {
   }
 
   // Called when kSessionManagerStartSession method is complete.
-  void OnStartSession(dbus::Response* response) {
-    LOG_IF(ERROR, !response)
-        << "Failed to call "
-        << login_manager::kSessionManagerStartSession;
+  void OnStartSession(const StartSessionCallback& callback,
+                      dbus::Response* response) {
+    bool success = false;
+    if (!response) {
+      LOG(ERROR) << "Failed to call "
+                 << login_manager::kSessionManagerStartSession;
+    } else {
+      dbus::MessageReader reader(response);
+      if (!reader.PopBool(&success))
+        LOG(ERROR) << "Invalid response: " << response->ToString();
+    }
+    callback.Run(success);
   }
 
   // Called when kSessionManagerStopSession method is complete.
@@ -519,7 +529,10 @@ class SessionManagerClientStubImpl : public SessionManagerClient {
   }
   virtual void EmitLoginPromptVisible() OVERRIDE {}
   virtual void RestartJob(int pid, const std::string& command_line) OVERRIDE {}
-  virtual void StartSession(const std::string& user_email) OVERRIDE {}
+  virtual void StartSession(const std::string& user_email,
+                            const StartSessionCallback& callback) OVERRIDE {
+    callback.Run(true);
+  }
   virtual void StopSession() OVERRIDE {}
   virtual void StartDeviceWipe() OVERRIDE {}
   virtual void RequestLockScreen() OVERRIDE {
