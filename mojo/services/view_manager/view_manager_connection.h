0e31220a25367a7b8ba2f52afb6a0681936a6aa9
machenbach@chromium.org <machenbach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-29 09:22:32 +0000

    Revert of Makes change_id unsigned (https://codereview.chromium.org/253883005/)
    Reason for revert:
    Breaks compile on win builder:
    http://build.chromium.org/p/chromium.win/builders/Win%20Builder%20%28dbg%29/builds/29440
    
    Original issue's description:
    > Makes change_id unsigned
    > 
    > Easier this way for clients to track and not worry about overflow.
    > 
    > BUG=none
    > TEST=none
    > R=ben@chromium.org
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=266793
    
    TBR=ben@chromium.org,sky@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=none
    
    Review URL: https://codereview.chromium.org/252143003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266799 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/services/view_manager/view_manager_connection.h
+++ mojo/services/view_manager/view_manager_connection.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "mojo/public/cpp/shell/service.h"
 #include "mojo/services/public/interfaces/view_manager/view_manager.mojom.h"
-#include "mojo/services/view_manager/ids.h"
 #include "mojo/services/view_manager/node_delegate.h"
 #include "mojo/services/view_manager/view_manager_export.h"
 
@@ -49,11 +48,11 @@ class MOJO_VIEW_MANAGER_EXPORT ViewManagerConnection
   void NotifyNodeHierarchyChanged(const NodeId& node,
                                   const NodeId& new_parent,
                                   const NodeId& old_parent,
-                                  ChangeId change_id);
+                                  int32_t change_id);
   void NotifyNodeViewReplaced(const NodeId& node,
                               const ViewId& new_view_id,
                               const ViewId& old_view_id,
-                              ChangeId change_id);
+                              int32_t change_id);
 
  private:
   typedef std::map<uint16_t, Node*> NodeMap;
@@ -63,41 +62,41 @@ class MOJO_VIEW_MANAGER_EXPORT ViewManagerConnection
   // is the connection that originated the change.
   bool DeleteNodeImpl(ViewManagerConnection* source,
                       const NodeId& node_id,
-                      ChangeId change_id);
+                      int32_t change_id);
 
   // Deletes a view owned by this connection. Returns true on success. |source|
   // is the connection that originated the change.
   bool DeleteViewImpl(ViewManagerConnection* source,
                       const ViewId& view_id,
-                      ChangeId change_id);
+                      int32_t change_id);
 
   // Sets the view associated with a node.
   bool SetViewImpl(const NodeId& node_id,
                    const ViewId& view_id,
-                   ChangeId change_id);
+                   int32_t change_id);
 
   // Overridden from ViewManager:
   virtual void CreateNode(uint16_t node_id,
                           const Callback<void(bool)>& callback) OVERRIDE;
   virtual void DeleteNode(uint32_t transport_node_id,
-                          ChangeId change_id,
+                          int32_t change_id,
                           const mojo::Callback<void(bool)>& callback) OVERRIDE;
   virtual void AddNode(uint32_t parent_id,
                        uint32_t child_id,
-                       ChangeId change_id,
+                       int32_t change_id,
                        const Callback<void(bool)>& callback) OVERRIDE;
   virtual void RemoveNodeFromParent(
       uint32_t node_id,
-      ChangeId change_id,
+      int32_t change_id,
       const Callback<void(bool)>& callback) OVERRIDE;
   virtual void CreateView(uint16_t view_id,
                           const mojo::Callback<void(bool)>& callback) OVERRIDE;
   virtual void DeleteView(uint32_t transport_view_id,
-                          ChangeId change_id,
+                          int32_t change_id,
                           const mojo::Callback<void(bool)>& callback) OVERRIDE;
   virtual void SetView(uint32_t transport_node_id,
                        uint32_t transport_view_id,
-                       ChangeId change_id,
+                       int32_t change_id,
                        const mojo::Callback<void(bool)>& callback) OVERRIDE;
 
   // Overriden from NodeDelegate:
