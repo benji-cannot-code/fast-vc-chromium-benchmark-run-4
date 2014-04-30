9790132343aec53e1f0badf16c5bd5f7a4df7791
alekseys@chromium.org <alekseys@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-30 05:34:44 +0000

    Add media size select element and ticket item. Do not expose in the UI yet.
    BUG=239879
    
    Review URL: https://codereview.chromium.org/253163002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267090 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/print_preview/print_preview.js
+++ chrome/browser/resources/print_preview/print_preview.js
@@ -117,6 +117,15 @@ cr.define('print_preview', function() {
     this.addChild(this.copiesSettings_);
 
     /**
+     * Component that renders the media size settings.
+     * @type {!print_preview.MediaSizeSettings}
+     * @private
+     */
+    this.mediaSizeSettings_ =
+        new print_preview.MediaSizeSettings(this.printTicketStore_.mediaSize);
+    this.addChild(this.mediaSizeSettings_);
+
+    /**
      * Component that renders the layout settings.
      * @type {!print_preview.LayoutSettings}
      * @private
@@ -383,6 +392,7 @@ cr.define('print_preview', function() {
       this.destinationSettings_.decorate($('destination-settings'));
       this.pageSettings_.decorate($('page-settings'));
       this.copiesSettings_.decorate($('copies-settings'));
+      this.mediaSizeSettings_.decorate($('media-size-settings'));
       this.layoutSettings_.decorate($('layout-settings'));
       this.colorSettings_.decorate($('color-settings'));
       this.marginSettings_.decorate($('margin-settings'));
@@ -406,6 +416,7 @@ cr.define('print_preview', function() {
       this.destinationSettings_.isEnabled = isEnabled;
       this.pageSettings_.isEnabled = isEnabled;
       this.copiesSettings_.isEnabled = isEnabled;
+      this.mediaSizeSettings_.isEnabled = isEnabled;
       this.layoutSettings_.isEnabled = isEnabled;
       this.colorSettings_.isEnabled = isEnabled;
       this.marginSettings_.isEnabled = isEnabled;
@@ -989,6 +1000,7 @@ cr.define('print_preview', function() {
 <include src="data/ticket_items/copies.js"/>
 <include src="data/ticket_items/duplex.js"/>
 <include src="data/ticket_items/header_footer.js"/>
+<include src="data/ticket_items/media_size.js"/>
 <include src="data/ticket_items/landscape.js"/>
 <include src="data/ticket_items/margins_type.js"/>
 <include src="data/ticket_items/page_range.js"/>
@@ -1005,6 +1017,7 @@ cr.define('print_preview', function() {
 
 <include src="settings/page_settings.js"/>
 <include src="settings/copies_settings.js"/>
+<include src="settings/media_size_settings.js"/>
 <include src="settings/layout_settings.js"/>
 <include src="settings/color_settings.js"/>
 <include src="settings/margin_settings.js"/>
