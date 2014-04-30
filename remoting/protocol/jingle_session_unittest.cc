c0c1619478c9f7b09d1c5810b705e7ff5f1a97a1
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-30 04:41:13 +0000

    Revert 267030 "Build remoting for PNaCl"
    > Build remoting for PNaCl
    > 
    > BUG=276739
    > R=jamiewalch@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/234023003
    
    TBR=sergeyu@chromium.org
    
    Review URL: https://codereview.chromium.org/254243002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267064 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/protocol/jingle_session_unittest.cc
+++ remoting/protocol/jingle_session_unittest.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/test/test_timeouts.h"
 #include "base/time/time.h"
-#include "jingle/glue/thread_wrapper.h"
 #include "net/socket/socket.h"
 #include "net/socket/stream_socket.h"
 #include "net/url_request/url_request_context_getter.h"
@@ -96,7 +95,6 @@ class JingleSessionTest : public testing::Test {
  public:
   JingleSessionTest() {
     message_loop_.reset(new base::MessageLoopForIO());
-    jingle_glue::JingleThreadWrapper::EnsureForCurrentMessageLoop();
   }
 
   // Helper method that handles OnIncomingSession().
