6a613847adad7abdfbf3cacb8786f05c59ed1839
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-30 05:16:56 +0000

    Stop prompting for the file system write permission in packaged apps.
    The user has to grant permission to specific files via a file picker,
    which is considered permission enough.
    
    BUG=363399
    
    Review URL: https://codereview.chromium.org/253433002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267081 0039d316-1c4b-4281-b951-d872f2087c98

--- extensions/common/permissions/permission_message.h
+++ extensions/common/permissions/permission_message.h
@@ -48,7 +48,7 @@ class PermissionMessage {
     kAudioCapture,
     kVideoCapture,
     kDownloads,
-    kFileSystemWrite,
+    kDeleted_FileSystemWrite,
     kMediaGalleriesAllGalleriesRead,
     kSerial,
     kSocketAnyHost,
