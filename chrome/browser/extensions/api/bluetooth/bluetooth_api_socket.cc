8b30fd0a4d878993e4a9def1783b7145bb854294
keybuk@chromium.org <keybuk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-30 21:45:44 +0000

    Move methods from chrome.bluetooth to chrome.bluetoothSocket
    The updateSocket(), setSocketPaused(), send(), getSocket() and
    getSockets() are moved to chrome.bluetoothSocket, renaming as necessary,
    with the old implementation replaced by a stub that returns an error.
    When un-paused sockets generate chrome.bluetoothSocket.onReceive and
    chrome.bluetoothSocket.onReceiveError events instead of the old events
    which are now unused.
    
    Also implements chrome.bluetoothSocket.create() and close() to complete
    the lifetime of the socket.
    
    Since BluetoothSocket is self-contained, and does not need references to
    the adapter or observer methods, BluetoothSocketEventDispatcher changes
    from being a refcounted object to BrowserContextKeyedAPI, matching the
    sockets API.
    
    BUG=349475,364581
    
    Review URL: https://codereview.chromium.org/252673004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267344 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/bluetooth/bluetooth_api_socket.cc
+++ chrome/browser/extensions/api/bluetooth/bluetooth_api_socket.cc
@@ -8,6 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/bluetooth/bluetooth_socket.h"
 #include "net/base/io_buffer.h"
 
+namespace {
+
+const char kSocketNotConnectedError[] = "Socket not connected";
+
+}  // namespace
+
 namespace extensions {
 
 // static
@@ -22,6 +28,15 @@ ApiResourceManager<BluetoothApiSocket>::GetFactoryInstance() {
   return g_server_factory.Pointer();
 }
 
+BluetoothApiSocket::BluetoothApiSocket(const std::string& owner_extension_id)
+    : ApiResource(owner_extension_id),
+      persistent_(false),
+      buffer_size_(0),
+      paused_(false),
+      connected_(false) {
+  DCHECK(content::BrowserThread::CurrentlyOn(kThreadId));
+}
+
 BluetoothApiSocket::BluetoothApiSocket(
     const std::string& owner_extension_id,
     scoped_refptr<device::BluetoothSocket> socket,
@@ -33,17 +48,25 @@ BluetoothApiSocket::BluetoothApiSocket(
       uuid_(uuid),
       persistent_(false),
       buffer_size_(0),
-      paused_(true) {
+      paused_(true),
+      connected_(true) {
   DCHECK(content::BrowserThread::CurrentlyOn(kThreadId));
 }
 
 BluetoothApiSocket::~BluetoothApiSocket() {
   DCHECK(content::BrowserThread::CurrentlyOn(kThreadId));
-  socket_->Close();
+  if (socket_.get())
+    socket_->Close();
 }
 
 void BluetoothApiSocket::Disconnect(const base::Closure& success_callback) {
   DCHECK(content::BrowserThread::CurrentlyOn(kThreadId));
+
+  if (!socket_.get() || !IsConnected()) {
+    success_callback.Run();
+    return;
+  }
+
   socket_->Disconnect(success_callback);
 }
 
@@ -55,8 +78,15 @@ bool BluetoothApiSocket::IsPersistent() const {
 void BluetoothApiSocket::Receive(
     int count,
     const ReceiveCompletionCallback& success_callback,
-    const ReceiveErrorCompletionCallback& error_callback) {
+    const ErrorCompletionCallback& error_callback) {
   DCHECK(content::BrowserThread::CurrentlyOn(kThreadId));
+
+  if (!socket_.get() || !IsConnected()) {
+    error_callback.Run(BluetoothApiSocket::kNotConnected,
+                       kSocketNotConnectedError);
+    return;
+  }
+
   socket_->Receive(count,
                    success_callback,
                    base::Bind(&OnSocketReceiveError, error_callback));
@@ -64,7 +94,7 @@ void BluetoothApiSocket::Receive(
 
 // static
 void BluetoothApiSocket::OnSocketReceiveError(
-    const ReceiveErrorCompletionCallback& error_callback,
+    const ErrorCompletionCallback& error_callback,
     device::BluetoothSocket::ErrorReason reason,
     const std::string& message) {
   DCHECK(content::BrowserThread::CurrentlyOn(kThreadId));
@@ -90,7 +120,26 @@ void BluetoothApiSocket::Send(scoped_refptr<net::IOBuffer> buffer,
                               const SendCompletionCallback& success_callback,
                               const ErrorCompletionCallback& error_callback) {
   DCHECK(content::BrowserThread::CurrentlyOn(kThreadId));
-  socket_->Send(buffer, buffer_size, success_callback, error_callback);
+
+  if (!socket_.get() || !IsConnected()) {
+    error_callback.Run(BluetoothApiSocket::kNotConnected,
+                       kSocketNotConnectedError);
+    return;
+  }
+
+  socket_->Send(buffer,
+                buffer_size,
+                success_callback,
+                base::Bind(&OnSocketSendError, error_callback));
+}
+
+// static
+void BluetoothApiSocket::OnSocketSendError(
+    const ErrorCompletionCallback& error_callback,
+    const std::string& message) {
+  DCHECK(content::BrowserThread::CurrentlyOn(kThreadId));
+  error_callback.Run(BluetoothApiSocket::kSystemError, message);
+
 }
 
 }  // namespace extensions
--- chrome/browser/extensions/browser_context_keyed_service_factories.cc
+++ chrome/browser/extensions/browser_context_keyed_service_factories.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/bluetooth/bluetooth_api.h"
 #include "chrome/browser/extensions/api/bluetooth/bluetooth_private_api.h"
 #include "chrome/browser/extensions/api/bluetooth_low_energy/bluetooth_low_energy_api.h"
+#include "chrome/browser/extensions/api/bluetooth_socket/bluetooth_socket_event_dispatcher.h"
 #include "chrome/browser/extensions/api/bookmark_manager_private/bookmark_manager_private_api.h"
 #include "chrome/browser/extensions/api/bookmarks/bookmarks_api.h"
 #include "chrome/browser/extensions/api/braille_display_private/braille_display_private_api.h"
@@ -164,6 +165,7 @@ void EnsureBrowserContextKeyedServiceFactoriesBuilt() {
 #endif
   TokenCacheServiceFactory::GetInstance();
   extensions::ExtensionGCMAppHandler::GetFactoryInstance();
+  extensions::api::BluetoothSocketEventDispatcher::GetFactoryInstance();
 }
 
 }  // namespace chrome_extensions
