15e5a0ef9fe6b903c519a3a3bbaf2bdea8157c6a
eustas@chromium.org <eustas@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-30 14:57:19 +0000

    Network Panel: add method and status code filters.
    BUG=133733
    
    Review URL: https://codereview.chromium.org/261623002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172983 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/network/NetworkPanel.js
+++ third_party/WebKit/Source/devtools/front_end/network/NetworkPanel.js
@@ -852,7 +852,9 @@ WebInspector.NetworkLogView.prototype = {
             return;
 
         this._suggestionBuilder.addItem(WebInspector.NetworkPanel.FilterType.Domain, request.domain);
+        this._suggestionBuilder.addItem(WebInspector.NetworkPanel.FilterType.Method, request.requestMethod);
         this._suggestionBuilder.addItem(WebInspector.NetworkPanel.FilterType.MimeType, request.mimeType);
+        this._suggestionBuilder.addItem(WebInspector.NetworkPanel.FilterType.StatusCode, "" + request.statusCode);
 
         var responseHeaders = request.responseHeaders;
         for (var i = 0, l = responseHeaders.length; i < l; ++i)
@@ -1377,6 +1379,9 @@ WebInspector.NetworkLogView.prototype = {
         case WebInspector.NetworkPanel.FilterType.HasResponseHeader:
             return WebInspector.NetworkLogView._requestResponseHeaderFilter.bind(null, value);
 
+        case WebInspector.NetworkPanel.FilterType.Method:
+            return WebInspector.NetworkLogView._requestMethodFilter.bind(null, value);
+
         case WebInspector.NetworkPanel.FilterType.MimeType:
             return WebInspector.NetworkLogView._requestMimeTypeFilter.bind(null, value);
 
@@ -1388,6 +1393,9 @@ WebInspector.NetworkLogView.prototype = {
 
         case WebInspector.NetworkPanel.FilterType.SetCookieValue:
             return WebInspector.NetworkLogView._requestSetCookieValueFilter.bind(null, value);
+
+        case WebInspector.NetworkPanel.FilterType.StatusCode:
+            return WebInspector.NetworkLogView._statusCodeFilter.bind(null, value);
         }
         return this._createTextFilter(type + ":" + value);
     },
@@ -1590,6 +1598,16 @@ WebInspector.NetworkLogView._requestResponseHeaderFilter = function(value, reque
  * @param {!WebInspector.NetworkRequest} request
  * @return {boolean}
  */
+WebInspector.NetworkLogView._requestMethodFilter = function(value, request)
+{
+    return request.requestMethod === value;
+}
+
+/**
+ * @param {string} value
+ * @param {!WebInspector.NetworkRequest} request
+ * @return {boolean}
+ */
 WebInspector.NetworkLogView._requestMimeTypeFilter = function(value, request)
 {
     return request.mimeType === value;
@@ -1638,6 +1656,16 @@ WebInspector.NetworkLogView._requestSetCookieValueFilter = function(value, reque
             return false;
     }
     return false;
+}
+
+/**
+ * @param {string} value
+ * @param {!WebInspector.NetworkRequest} request
+ * @return {boolean}
+ */
+WebInspector.NetworkLogView._statusCodeFilter = function(value, request)
+{
+    return ("" + request.statusCode) === value;
 }
 
 /**
@@ -1749,10 +1777,12 @@ WebInspector.NetworkPanel = function()
 WebInspector.NetworkPanel.FilterType = {
     Domain: "Domain",
     HasResponseHeader: "HasResponseHeader",
+    Method: "Method",
     MimeType: "MimeType",
     SetCookieDomain: "SetCookieDomain",
     SetCookieName: "SetCookieName",
-    SetCookieValue: "SetCookieValue"
+    SetCookieValue: "SetCookieValue",
+    StatusCode: "StatusCode"
 };
 
 /** @type {!Array.<string>} */
