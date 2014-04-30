4f01ac43b7767696bc49c795c5dbafec4b33f9d6
leng@chromium.org <leng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-30 20:30:35 +0000

    Makes bubble correctly track original anchor point when parent window resizes.
    BUG=368178
    
    Review URL: https://codereview.chromium.org/262533003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267322 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/website_settings/permission_bubble_controller.h
+++ chrome/browser/ui/cocoa/website_settings/permission_bubble_controller.h
@@ -29,6 +29,9 @@ class PermissionBubbleRequest;
 
   // Bridge to the C++ class that created this object.
   PermissionBubbleCocoa* bridge_;  // Weak.
+
+  // Anchor's offset from parent window's upper-left-hand corner.
+  NSPoint anchorOffset_;
 }
 
 // Designated initializer.  |parentWindow| and |bridge| must both be non-nil.
