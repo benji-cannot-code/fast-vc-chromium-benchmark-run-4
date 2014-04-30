6a2a4cb5f4acee93aba9d63fc9ad44e887a1ad1c
peter@chromium.org <peter@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-30 17:15:32 +0000

    Add a UseCounter for Notification.permission
    While this is a relatively new property, it does require a synchronous IPC
    to the browser process in order to get the permission status, and thus would
    be better as a Notification.hasPermission() method returning a Promise (or
    similar).
    
    Since we will need a similar construct for Push Messaging, and potentially
    other APIs, let's get some data on whether developers use this to see if
    we can get a more common, asynchronous approach.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/254023002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173004 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/notifications/Notification.cpp
+++ third_party/WebKit/Source/modules/notifications/Notification.cpp
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/v8/Dictionary.h"
 #include "bindings/v8/ScriptWrappable.h"
 #include "core/dom/Document.h"
+#include "core/frame/UseCounter.h"
 #include "core/page/WindowFocusAllowedIndicator.h"
 #include "modules/notifications/NotificationClient.h"
 #include "modules/notifications/NotificationController.h"
@@ -162,6 +163,8 @@ const String& Notification::permissionString(NotificationClient::Permission perm
 const String& Notification::permission(ExecutionContext* context)
 {
     ASSERT(toDocument(context)->page());
+
+    UseCounter::count(context, UseCounter::NotificationPermission);
     return permissionString(NotificationController::from(toDocument(context)->frame())->client()->checkPermission(context));
 }
 
