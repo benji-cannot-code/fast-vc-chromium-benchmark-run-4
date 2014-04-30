7ba98ee6dadb1be0b935936a84101d21f8ec2b00
jbauman@chromium.org <jbauman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-30 05:39:17 +0000

    Remove RenderWidgetHostView*::DidUpdateBackingStore.
    This isn't necessary anymore with the legacy 2D path gone. Also remove some UpdateRect parameters that aren't used anymore.
    
    BUG=362164
    
    Review URL: https://codereview.chromium.org/253913003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267094 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/web_contents/aura/overscroll_navigation_overlay_unittest.cc
+++ content/browser/web_contents/aura/overscroll_navigation_overlay_unittest.cc
@@ -85,8 +85,6 @@ class OverscrollNavigationOverlayTest : public RenderViewHostImplTestHarness {
     ViewHostMsg_UpdateRect_Params params;
     memset(&params, 0, sizeof(params));
     params.view_size = gfx::Size(10, 10);
-    params.bitmap_rect = gfx::Rect(params.view_size);
-    params.scroll_rect = gfx::Rect();
     ViewHostMsg_UpdateRect rect(test_rvh()->GetRoutingID(), params);
     RenderViewHostTester::TestOnMessageReceived(test_rvh(), rect);
 
@@ -165,8 +163,6 @@ TEST_F(OverscrollNavigationOverlayTest, PaintUpdateWithoutNonEmptyPaint) {
   ViewHostMsg_UpdateRect_Params params;
   memset(&params, 0, sizeof(params));
   params.view_size = gfx::Size(10, 10);
-  params.bitmap_rect = gfx::Rect(params.view_size);
-  params.scroll_rect = gfx::Rect();
   params.flags = ViewHostMsg_UpdateRect_Flags::IS_REPAINT_ACK;
   ViewHostMsg_UpdateRect rect(test_rvh()->GetRoutingID(), params);
   RenderViewHostTester::TestOnMessageReceived(test_rvh(), rect);
