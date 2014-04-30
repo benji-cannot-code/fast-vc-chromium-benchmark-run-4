b49cf4c07c773b86c6e04b8644234ac2da45944f
pliard@chromium.org <pliard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-30 11:15:49 +0000

    Revert 267170 "Use DiscardableMemoryManager on Android."
    > Use DiscardableMemoryManager on Android.
    > 
    > This allows userspace (DiscardableMemoryManager) to control eviction of
    > unlocked DiscardableMemory instances to prevent the process from running out of
    > address space in cases of heavy use of unlocked DiscardableMemory.
    > 
    > This also removes all the occurences of 'Android' from the ashmem allocator to
    > allow it to be later used on ChromeOS.
    > 
    > BUG=327516, 334996
    > R=reveman@chromium.org, willchan@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/195863005
    
    TBR=pliard@chromium.org
    
    Review URL: https://codereview.chromium.org/257383004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267175 0039d316-1c4b-4281-b951-d872f2087c98

--- base/memory/discardable_memory_android.cc
+++ base/memory/discardable_memory_android.cc
@@ -10,30 +10,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/lazy_instance.h"
 #include "base/logging.h"
-#include "base/memory/discardable_memory_ashmem.h"
-#include "base/memory/discardable_memory_ashmem_allocator.h"
+#include "base/memory/discardable_memory_allocator_android.h"
 #include "base/memory/discardable_memory_emulated.h"
 #include "base/memory/discardable_memory_malloc.h"
 
 namespace base {
 namespace {
 
-const char kAshmemAllocatorName[] = "DiscardableMemoryAshmemAllocator";
+const char kAshmemAllocatorName[] = "DiscardableMemoryAllocator";
 
-// When ashmem is used, have the DiscardableMemoryManager trigger userspace
-// eviction when address space usage gets too high (e.g. 512 MBytes).
-const size_t kAshmemMaxAddressSpaceUsage = 512 * 1024 * 1024;
-
-// Holds the state used for ashmem allocations.
-struct AshmemGlobalContext {
-  AshmemGlobalContext()
+struct DiscardableMemoryAllocatorWrapper {
+  DiscardableMemoryAllocatorWrapper()
       : allocator(kAshmemAllocatorName,
                   GetOptimalAshmemRegionSizeForAllocator()) {
-    manager.SetMemoryLimit(kAshmemMaxAddressSpaceUsage);
   }
 
-  internal::DiscardableMemoryAshmemAllocator allocator;
-  internal::DiscardableMemoryManager manager;
+  internal::DiscardableMemoryAllocator allocator;
 
  private:
   // Returns 64 MBytes for a 512 MBytes device, 128 MBytes for 1024 MBytes...
@@ -44,7 +36,8 @@ struct AshmemGlobalContext {
   }
 };
 
-LazyInstance<AshmemGlobalContext>::Leaky g_context = LAZY_INSTANCE_INITIALIZER;
+LazyInstance<DiscardableMemoryAllocatorWrapper>::Leaky g_context =
+    LAZY_INSTANCE_INITIALIZER;
 
 }  // namespace
 
@@ -62,7 +55,7 @@ void DiscardableMemory::UnregisterMemoryPressureListeners() {
 void DiscardableMemory::GetSupportedTypes(
     std::vector<DiscardableMemoryType>* types) {
   const DiscardableMemoryType supported_types[] = {
-    DISCARDABLE_MEMORY_TYPE_ASHMEM,
+    DISCARDABLE_MEMORY_TYPE_ANDROID,
     DISCARDABLE_MEMORY_TYPE_EMULATED,
     DISCARDABLE_MEMORY_TYPE_MALLOC
   };
@@ -76,15 +69,8 @@ scoped_ptr<DiscardableMemory> DiscardableMemory::CreateLockedMemoryWithType(
     case DISCARDABLE_MEMORY_TYPE_NONE:
     case DISCARDABLE_MEMORY_TYPE_MAC:
       return scoped_ptr<DiscardableMemory>();
-    case DISCARDABLE_MEMORY_TYPE_ASHMEM: {
-      AshmemGlobalContext* const global_context = g_context.Pointer();
-      scoped_ptr<internal::DiscardableMemoryAshmem> memory(
-          new internal::DiscardableMemoryAshmem(
-              size, &global_context->allocator, &global_context->manager));
-      if (!memory->Initialize())
-        return scoped_ptr<DiscardableMemory>();
-
-      return memory.PassAs<DiscardableMemory>();
+    case DISCARDABLE_MEMORY_TYPE_ANDROID: {
+      return g_context.Pointer()->allocator.Allocate(size);
     }
     case DISCARDABLE_MEMORY_TYPE_EMULATED: {
       scoped_ptr<internal::DiscardableMemoryEmulated> memory(
@@ -109,9 +95,13 @@ scoped_ptr<DiscardableMemory> DiscardableMemory::CreateLockedMemoryWithType(
 }
 
 // static
+bool DiscardableMemory::PurgeForTestingSupported() {
+  return false;
+}
+
+// static
 void DiscardableMemory::PurgeForTesting() {
-  g_context.Pointer()->manager.PurgeAll();
-  internal::DiscardableMemoryEmulated::PurgeForTesting();
+  NOTIMPLEMENTED();
 }
 
 }  // namespace base
--- base/memory/discardable_memory_unittest.cc
+++ base/memory/discardable_memory_unittest.cc
@@ -48,7 +48,7 @@ TEST_P(DiscardableMemoryTest, IsNamed) {
 
 bool IsNativeType(DiscardableMemoryType type) {
   return
-      type == DISCARDABLE_MEMORY_TYPE_ASHMEM ||
+      type == DISCARDABLE_MEMORY_TYPE_ANDROID ||
       type == DISCARDABLE_MEMORY_TYPE_MAC;
 }
 
@@ -92,8 +92,11 @@ TEST_P(DiscardableMemoryTest, DeleteWhileLocked) {
   ASSERT_TRUE(memory);
 }
 
+#if !defined(OS_ANDROID)
 // Test forced purging.
 TEST_P(DiscardableMemoryTest, Purge) {
+  ASSERT_TRUE(DiscardableMemory::PurgeForTestingSupported());
+
   const scoped_ptr<DiscardableMemory> memory(CreateLockedMemory(kSize));
   ASSERT_TRUE(memory);
   memory->Unlock();
@@ -101,6 +104,7 @@ TEST_P(DiscardableMemoryTest, Purge) {
   DiscardableMemory::PurgeForTesting();
   EXPECT_EQ(DISCARDABLE_MEMORY_LOCK_STATUS_PURGED, memory->Lock());
 }
+#endif  // !OS_ANDROID
 
 #if !defined(NDEBUG) && !defined(OS_ANDROID)
 // Death tests are not supported with Android APKs.
