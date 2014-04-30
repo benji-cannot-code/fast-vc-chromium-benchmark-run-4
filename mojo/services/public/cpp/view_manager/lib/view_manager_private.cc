8e7dee996900cc27de6c779323bba6916bd41190
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-30 19:20:09 +0000

    First step at synchronizing client model changes with service.
    R=sky@chromium.org
    http://crbug.com/365012
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=266940
    
    Review URL: https://codereview.chromium.org/258623005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267293 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ mojo/services/public/cpp/view_manager/lib/view_manager_private.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "mojo/services/public/cpp/view_manager/lib/view_manager_private.h"
+
+namespace mojo {
+namespace services {
+namespace view_manager {
+
+ViewManagerPrivate::ViewManagerPrivate(ViewManager* manager)
+    : manager_(manager) {}
+ViewManagerPrivate::~ViewManagerPrivate() {}
+
+}  // namespace view_manager
+}  // namespace services
+}  // namespace mojo
