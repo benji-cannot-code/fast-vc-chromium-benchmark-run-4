6f11a3dca3a427b1f35c79282f906c7e45d41936
grunell@chromium.org <grunell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-30 11:28:18 +0000

    Disable WebRtcDisableEncryptionFlagBrowserTest.MANUAL_VerifyEncryption on XP.
    Flaky timeout on a certain bot.
    
    NOTRY=true
    
    BUG=368163
    
    Review URL: https://codereview.chromium.org/260083010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267182 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media/chrome_webrtc_disable_encryption_flag_browsertest.cc
+++ chrome/browser/media/chrome_webrtc_disable_encryption_flag_browsertest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/command_line.h"
+#include "base/win/windows_version.h"
 #include "chrome/browser/media/webrtc_browsertest_base.h"
 #include "chrome/browser/media/webrtc_browsertest_common.h"
 #include "chrome/common/chrome_version_info.h"
@@ -54,9 +55,15 @@ class WebRtcDisableEncryptionFlagBrowserTest : public WebRtcTestBase {
 // Makes a call and checks that there's encryption or not in the SDP offer.
 // TODO(phoglund): this is unreliable on non-webrtc bots because its peer
 // connection server could clash with other tests running in parallel,
-// therefore only running manually.
+// therefore only running manually. http://crbug.com/358207.
 IN_PROC_BROWSER_TEST_F(WebRtcDisableEncryptionFlagBrowserTest,
                        MANUAL_VerifyEncryption) {
+// Flaky timeout on a webrtc Win XP bot. http://crbug.com/368163.
+#if defined (OS_WIN)
+  if (base::win::GetVersion() < base::win::VERSION_VISTA)
+    return;
+#endif
+
   ASSERT_TRUE(embedded_test_server()->InitializeAndWaitUntilReady());
   ASSERT_TRUE(peerconnection_server_.Start());
 
