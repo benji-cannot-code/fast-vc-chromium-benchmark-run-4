dc8bf98a88469b6d1ab063a2ca77fb239a99aa12
vmpstr@chromium.org <vmpstr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-30 02:01:26 +0000

    cc: prevent TileManager raster/eviction tile iterators copy.
    This patch ensures that iterators cannot be copied..
    Since we rely on a member heap that points to another member vector,
    we can't do a naive copy. Instead we would have to reconstruct the
    heap to point to the new members, but it's better to just restrict
    copy altogether.
    
    R=enne
    
    Review URL: https://codereview.chromium.org/251003002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267042 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/resources/tile_manager.h
+++ cc/resources/tile_manager.h
@@ -104,6 +104,8 @@ class CC_EXPORT TileManager : public RasterizerClient,
     std::vector<PairedPictureLayerIterator*> iterator_heap_;
     TreePriority tree_priority_;
     RasterOrderComparator comparator_;
+
+    DISALLOW_COPY_AND_ASSIGN(RasterTileIterator);
   };
 
   struct CC_EXPORT EvictionTileIterator {
@@ -148,6 +150,8 @@ class CC_EXPORT TileManager : public RasterizerClient,
     std::vector<PairedPictureLayerIterator*> iterator_heap_;
     TreePriority tree_priority_;
     EvictionOrderComparator comparator_;
+
+    DISALLOW_COPY_AND_ASSIGN(EvictionTileIterator);
   };
 
   static scoped_ptr<TileManager> Create(
