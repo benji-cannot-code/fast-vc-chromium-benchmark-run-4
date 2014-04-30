9a7ec867409c94440edccb26c8fd8df7e6c632b0
mkwst@chromium.org <mkwst@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-30 12:14:11 +0000

    Password bubble: ManagePasswordsIconView is now a BubbleIconView.
    This CL introduces a new browser command to open the Manage Passwords bubble,
    and converts the ManagePasswordsIconView class into a subclass of
    BubbleIconView, which uses the new command to control the bubble's state.
    This allows us to more easily test the view and the UI controller, as each
    object's job is now more clearly defined (and the view is now doing a good
    deal less work), and to independently verify that the command is doing the
    right thing.
    
    After this CL, we'll (finally!) have something approaching reasonable test
    coverage for the core of the views code.
    
    ----------------------------------------------------------------------------
    This is a re-land of r266859, which was reverted due to LSAN errors. The
    original review is https://codereview.chromium.org/246393004/
    ----------------------------------------------------------------------------
    
    BUG=365678
    TBR=vabr@chromium.org,pkasting@chromium.org,cpu@chromium.org
    
    Review URL: https://codereview.chromium.org/254263002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267195 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/passwords/manage_passwords_icon.cc
+++ chrome/browser/ui/passwords/manage_passwords_icon.cc
@@ -15,5 +15,5 @@ void ManagePasswordsIcon::SetState(State state) {
   if (state_ == state)
     return;
   state_ = state;
-  SetStateInternal(state);
+  UpdateVisibleUI();
 }
--- chrome/browser/ui/passwords/manage_passwords_icon_mock.cc
+++ chrome/browser/ui/passwords/manage_passwords_icon_mock.cc
@@ -15,5 +15,5 @@ void ManagePasswordsIconMock::ShowBubbleWithoutUserInteraction() {
   ++bubble_shown_count_;
 }
 
-void ManagePasswordsIconMock::SetStateInternal(ManagePasswordsIcon::State) {
+void ManagePasswordsIconMock::UpdateVisibleUI() {
 }
