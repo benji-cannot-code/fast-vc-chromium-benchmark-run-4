713ba935c121baa6fd8ca2d38bd81e032c2b1a20
pdr@chromium.org <pdr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-30 19:52:38 +0000

    Re-enable the Robohornet pro test on Android
    This patch re-enables the robohornet pro test on android. This test was
    disabled in [1] due to timeouts caused by the fasttextautosizer. Once
    [2] lands we can re-enable this test.
    
    [1] https://src.chromium.org/viewvc/chrome?view=rev&revision=266233
    [2] https://codereview.chromium.org/251053002
    
    BUG=357338
    BUG=302005
    
    Review URL: https://codereview.chromium.org/254953002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267308 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/benchmarks/robohornet_pro.py
+++ tools/perf/benchmarks/robohornet_pro.py
@@ -37,7 +37,6 @@ class _RobohornetProMeasurement(page_measurement.PageMeasurement):
     results.Add('Total', 'ms', result)
 
 
-@test.Disabled('android')  # crbug.com/357338
 class RobohornetPro(test.Test):
   test = _RobohornetProMeasurement
 
