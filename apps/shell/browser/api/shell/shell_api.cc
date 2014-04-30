8129b615c04d5e35eca15ea46195dae676ddb116
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-30 21:08:04 +0000

    Use EXTENSION_FUNCTION_VALIDATE for ShellCreateWindowFunction in app_shell
    Instead of attempting to validate the parameters by hand.
    
    I did not change ShellAppWindowCreateFunction as that class is going away
    in https://codereview.chromium.org/258053010/
    
    BUG=368706
    TEST=manual, changing Calculator example app to call createWindow() with bad params causes appropriate validation failures, with good params still opens a window
    
    Review URL: https://codereview.chromium.org/261653002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267330 0039d316-1c4b-4281-b951-d872f2087c98

--- apps/shell/browser/api/shell/shell_api.cc
+++ apps/shell/browser/api/shell/shell_api.cc
@@ -7,9 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "apps/shell/browser/shell_app_window.h"
 #include "apps/shell/browser/shell_desktop_controller.h"
+#include "apps/shell/common/api/shell.h"
 #include "base/values.h"
 
 using base::DictionaryValue;
+
+namespace CreateWindow = apps::shell_api::shell::CreateWindow;
 
 namespace apps {
 namespace {
@@ -32,17 +35,11 @@ ShellCreateWindowFunction::~ShellCreateWindowFunction() {
 }
 
 bool ShellCreateWindowFunction::RunImpl() {
-  // Arguments must contain a URL and may contain options and a callback.
-  if (args_->GetSize() < 1 || args_->GetSize() > 3)
-    return false;
-
-  // Extract the URL for the window contents, e.g. "main.html".
-  std::string url_string;
-  if (!args_->GetString(0, &url_string))
-    return false;
+  scoped_ptr<CreateWindow::Params> params(CreateWindow::Params::Create(*args_));
+  EXTENSION_FUNCTION_VALIDATE(params.get());
 
   // Convert "main.html" to "chrome-extension:/<id>/main.html".
-  GURL url = GetExtension()->GetResourceURL(url_string);
+  GURL url = GetExtension()->GetResourceURL(params->url);
   if (!url.is_valid())
     return false;
 
