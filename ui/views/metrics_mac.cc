78f64a0ddd77e9e97e5d6a1c94d733e4599e6c55
tapted@chromium.org <tapted@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-30 11:19:32 +0000

    MacViews: additional mac stubs in ui/views for menus, dragging
    Adds Mac stubs for menu_config_mac and metrics_mac.
    
    - Moves menu_config_views.cc to menu_config_aura.cc.
    - Moves mouse_watcher.cc to mouse_watcher_aura.cc.
    - Moves the aura-specific RunShellDrag in drag_utils.cc to
    drag_utils_aura.cc.
    
    BUG=366007
    
    Review URL: https://codereview.chromium.org/248883002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267177 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ ui/views/metrics_mac.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/views/metrics.h"
+
+namespace {
+
+// Default double click interval in milliseconds.
+// Same as what gtk uses.
+const int kDefaultDoubleClickInterval = 500;
+
+}  // namespace
+
+namespace views {
+
+int GetDoubleClickInterval() {
+  return kDefaultDoubleClickInterval;
+}
+
+int GetMenuShowDelay() {
+  return 0;
+}
+
+}  // namespace views
