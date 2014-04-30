a667c59790f69cd98722e8a94934ed13a2870bfc
alemate@chromium.org <alemate@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-30 02:00:23 +0000

    Add debug to  WallpaperManagerBrowserTest.DisplayChange test to diagnose failures.
    BUG=362847
    
    Review URL: https://codereview.chromium.org/239703014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267040 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/wallpaper_manager_browsertest.cc
+++ chrome/browser/chromeos/login/wallpaper_manager_browsertest.cc
@@ -96,6 +96,9 @@ class WallpaperManagerBrowserTest : public InProcessBrowserTest,
     ash::test::DisplayManagerTestApi display_manager_test_api(
         ash::Shell::GetInstance()->display_manager());
     display_manager_test_api.UpdateDisplay(display_specs);
+    LOG(ERROR) << "UpdateDisplay(display_specs='" << display_specs
+               << "') done.";
+    WallpaperManager::GetAppropriateResolutionForTesting();
   }
 
   void WaitAsyncWallpaperLoadStarted() {
@@ -767,8 +770,7 @@ class TestObserver : public WallpaperManager::Observer {
   DISALLOW_COPY_AND_ASSIGN(TestObserver);
 };
 
-// Disabled due to flaky failures. crbug.com/362847
-IN_PROC_BROWSER_TEST_P(WallpaperManagerBrowserTest, DISABLED_DisplayChange) {
+IN_PROC_BROWSER_TEST_P(WallpaperManagerBrowserTest, DisplayChange) {
   // TODO(derat|oshima|bshe): Host windows can't be resized on Win8.
   if (!ash::test::AshTestHelper::SupportsHostWindowResize())
     return;
@@ -785,18 +787,21 @@ IN_PROC_BROWSER_TEST_P(WallpaperManagerBrowserTest, DISABLED_DisplayChange) {
   // multiple displays are connected.
   UpdateDisplay("800x600");
   WaitAsyncWallpaperLoadFinished();
+  WallpaperManager::GetAppropriateResolutionForTesting();
   EXPECT_EQ(WallpaperManager::WALLPAPER_RESOLUTION_SMALL,
             WallpaperManager::Get()->GetAppropriateResolution());
   EXPECT_EQ(0, observer.GetUpdateWallpaperCountAndReset());
 
   UpdateDisplay("800x600,800x600");
   WaitAsyncWallpaperLoadFinished();
+  WallpaperManager::GetAppropriateResolutionForTesting();
   EXPECT_EQ(WallpaperManager::WALLPAPER_RESOLUTION_SMALL,
             WallpaperManager::Get()->GetAppropriateResolution());
   EXPECT_EQ(0, observer.GetUpdateWallpaperCountAndReset());
 
   UpdateDisplay("1366x800");
   WaitAsyncWallpaperLoadFinished();
+  WallpaperManager::GetAppropriateResolutionForTesting();
   EXPECT_EQ(WallpaperManager::WALLPAPER_RESOLUTION_SMALL,
             WallpaperManager::Get()->GetAppropriateResolution());
   EXPECT_EQ(1, observer.GetUpdateWallpaperCountAndReset());
@@ -804,18 +809,21 @@ IN_PROC_BROWSER_TEST_P(WallpaperManagerBrowserTest, DISABLED_DisplayChange) {
   // At larger sizes, large wallpapers should be used.
   UpdateDisplay("1367x800");
   WaitAsyncWallpaperLoadFinished();
+  WallpaperManager::GetAppropriateResolutionForTesting();
   EXPECT_EQ(WallpaperManager::WALLPAPER_RESOLUTION_LARGE,
             WallpaperManager::Get()->GetAppropriateResolution());
   EXPECT_EQ(1, observer.GetUpdateWallpaperCountAndReset());
 
   UpdateDisplay("1367x801");
   WaitAsyncWallpaperLoadFinished();
+  WallpaperManager::GetAppropriateResolutionForTesting();
   EXPECT_EQ(WallpaperManager::WALLPAPER_RESOLUTION_LARGE,
             WallpaperManager::Get()->GetAppropriateResolution());
   EXPECT_EQ(1, observer.GetUpdateWallpaperCountAndReset());
 
   UpdateDisplay("2560x1700");
   WaitAsyncWallpaperLoadFinished();
+  WallpaperManager::GetAppropriateResolutionForTesting();
   EXPECT_EQ(WallpaperManager::WALLPAPER_RESOLUTION_LARGE,
             WallpaperManager::Get()->GetAppropriateResolution());
   EXPECT_EQ(1, observer.GetUpdateWallpaperCountAndReset());
@@ -823,16 +831,19 @@ IN_PROC_BROWSER_TEST_P(WallpaperManagerBrowserTest, DISABLED_DisplayChange) {
   // Rotated smaller screen may use larger image.
   UpdateDisplay("800x600/r");
   WaitAsyncWallpaperLoadFinished();
+  WallpaperManager::GetAppropriateResolutionForTesting();
   EXPECT_EQ(WallpaperManager::WALLPAPER_RESOLUTION_SMALL,
             WallpaperManager::Get()->GetAppropriateResolution());
   EXPECT_EQ(1, observer.GetUpdateWallpaperCountAndReset());
 
   UpdateDisplay("800x600/r,800x600");
   WaitAsyncWallpaperLoadFinished();
+  WallpaperManager::GetAppropriateResolutionForTesting();
   EXPECT_EQ(WallpaperManager::WALLPAPER_RESOLUTION_SMALL,
             WallpaperManager::Get()->GetAppropriateResolution());
   EXPECT_EQ(1, observer.GetUpdateWallpaperCountAndReset());
   UpdateDisplay("1366x800/r");
+  WallpaperManager::GetAppropriateResolutionForTesting();
   WaitAsyncWallpaperLoadFinished();
   EXPECT_EQ(WallpaperManager::WALLPAPER_RESOLUTION_LARGE,
             WallpaperManager::Get()->GetAppropriateResolution());
@@ -840,6 +851,7 @@ IN_PROC_BROWSER_TEST_P(WallpaperManagerBrowserTest, DISABLED_DisplayChange) {
 
   // Max display size didn't chagne.
   UpdateDisplay("900x800/r,400x1366");
+  WallpaperManager::GetAppropriateResolutionForTesting();
   WaitAsyncWallpaperLoadFinished();
   EXPECT_EQ(0, observer.GetUpdateWallpaperCountAndReset());
 }
