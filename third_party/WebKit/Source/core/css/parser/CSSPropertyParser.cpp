75085af0644624b52b30a9510765c4d6b1795c7a
jfernandez@igalia.com <jfernandez@igalia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-30 21:25:37 +0000

    Clamping the number of repetitions when using the repeat() function.
    The ED suggests now to be able to clamp the number of repetitions when
    using the repeat() function, taking precautions about excessive memory
    usage.
    
    BUG=79180
    
    Review URL: https://codereview.chromium.org/251643002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173031 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
@@ -90,6 +90,7 @@ using namespace std;
 namespace WebCore {
 
 static const double MAX_SCALE = 1000000;
+static const unsigned minRepetitions = 10000;
 
 template <unsigned N>
 static bool equal(const CSSParserString& a, const char (&b)[N])
@@ -3798,6 +3799,10 @@ bool CSSPropertyParser::parseGridTrackRepeatFunction(CSSValueList& list)
 
     ASSERT_WITH_SECURITY_IMPLICATION(arguments->valueAt(0)->fValue > 0);
     size_t repetitions = arguments->valueAt(0)->fValue;
+    // Clamp repetitions at minRepetitions.
+    // http://www.w3.org/TR/css-grid-1/#repeat-notation
+    if (repetitions > minRepetitions)
+        repetitions = minRepetitions;
     RefPtrWillBeRawPtr<CSSValueList> repeatedValues = CSSValueList::createSpaceSeparated();
     arguments->next(); // Skip the repetition count.
     arguments->next(); // Skip the comma.
