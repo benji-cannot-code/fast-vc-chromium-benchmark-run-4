a9ab2323b0a253ff76e7fe7e79ae64b4bd9f873a
falken@chromium.org <falken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-30 09:33:13 +0000

    activation-after-registration.html is no longer flaky
    Fixed in Chromium r266544 <https://codereview.chromium.org/255813003/>
    
    BUG=365252
    
    Review URL: https://codereview.chromium.org/262493003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172965 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/serviceworkers/ServiceWorker.cpp
+++ third_party/WebKit/Source/modules/serviceworkers/ServiceWorker.cpp
@@ -92,7 +92,7 @@ bool ServiceWorker::isReady()
 
 void ServiceWorker::dispatchStateChangeEvent()
 {
-    // FIXME: Add ASSERT(isReady()) when Chromium side change rolls in.
+    ASSERT(isReady());
     this->dispatchEvent(Event::create(EventTypeNames::statechange));
 }
 
