7611d868c98d26c241405f4438da66589f3603f8
ajuma@chromium.org <ajuma@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-30 10:23:01 +0000

    Trigger GPU rasterization based on viewport rather than per-layer hints
    This makes GPU rasterization triggerable on a LayerTreeHost rather
    than per-layer. GPU rasterization is triggered on a LayerTreeHost
    based on WebViewClient::heuristicsForGpuRasterizationUpdated.
    
    This depends on https://codereview.chromium.org/259683002/
    
    BUG=367202
    
    Review URL: https://codereview.chromium.org/256613002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267152 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/layers/picture_layer_unittest.cc
+++ cc/layers/picture_layer_unittest.cc
@@ -81,7 +81,7 @@ TEST(PictureLayerTest, ForcedCpuRaster) {
   EXPECT_FALSE(layer->ShouldUseGpuRasterization());
 
   // Gpu rasterization cannot be enabled even with raster hint.
-  layer->SetHasGpuRasterizationHint(true);
+  host->set_has_gpu_rasterization_trigger(true);
   EXPECT_FALSE(layer->ShouldUseGpuRasterization());
 }
 
@@ -97,7 +97,7 @@ TEST(PictureLayerTest, ForcedGpuRaster) {
   EXPECT_TRUE(layer->ShouldUseGpuRasterization());
 
   // Gpu rasterization cannot be disabled even with raster hint.
-  layer->SetHasGpuRasterizationHint(false);
+  host->set_has_gpu_rasterization_trigger(false);
   EXPECT_TRUE(layer->ShouldUseGpuRasterization());
 
   // Gpu rasterization cannot be disabled even with skia veto.
@@ -118,17 +118,17 @@ TEST(PictureLayerTest, HybridRaster) {
   // The default value is false.
   EXPECT_FALSE(layer->ShouldUseGpuRasterization());
 
-  // Gpu rasterization can be enabled first time.
-  layer->SetHasGpuRasterizationHint(true);
+  // Gpu rasterization can be enabled.
+  host->set_has_gpu_rasterization_trigger(true);
   EXPECT_TRUE(layer->ShouldUseGpuRasterization());
 
-  // Gpu rasterization can always be disabled.
-  layer->SetHasGpuRasterizationHint(false);
+  // Gpu rasterization can be disabled.
+  host->set_has_gpu_rasterization_trigger(false);
   EXPECT_FALSE(layer->ShouldUseGpuRasterization());
 
-  // Gpu rasterization cannot be enabled once disabled.
-  layer->SetHasGpuRasterizationHint(true);
-  EXPECT_FALSE(layer->ShouldUseGpuRasterization());
+  // Gpu rasterization can be enabled again.
+  host->set_has_gpu_rasterization_trigger(true);
+  EXPECT_TRUE(layer->ShouldUseGpuRasterization());
 }
 
 TEST(PictureLayerTest, VetoGpuRaster) {
@@ -141,7 +141,7 @@ TEST(PictureLayerTest, VetoGpuRaster) {
 
   EXPECT_FALSE(layer->ShouldUseGpuRasterization());
 
-  layer->SetHasGpuRasterizationHint(true);
+  host->set_has_gpu_rasterization_trigger(true);
   EXPECT_TRUE(layer->ShouldUseGpuRasterization());
 
   // Veto gpu rasterization.
--- cc/trees/layer_tree_host_unittest.cc
+++ cc/trees/layer_tree_host_unittest.cc
@@ -4982,7 +4982,7 @@ class LayerTreeHostTestHybridRasterizationSetting : public LayerTreeHostTest {
     child->SetBounds(gfx::Size(10, 10));
     parent->AddChild(child);
 
-    parent->SetHasGpuRasterizationHint(true);
+    layer_tree_host()->set_has_gpu_rasterization_trigger(true);
   }
 
   virtual void BeginTest() OVERRIDE { PostSetNeedsCommitToMainThread(); }
@@ -4994,9 +4994,8 @@ class LayerTreeHostTestHybridRasterizationSetting : public LayerTreeHostTest {
     PictureLayerImpl* child =
         static_cast<PictureLayerImpl*>(parent->children()[0]);
 
-    // Only layers with a GPU rasterization hint should use GPU rasterization.
     EXPECT_TRUE(parent->ShouldUseGpuRasterization());
-    EXPECT_FALSE(child->ShouldUseGpuRasterization());
+    EXPECT_TRUE(child->ShouldUseGpuRasterization());
   }
 
   virtual void DidActivateTreeOnThread(LayerTreeHostImpl* host_impl) OVERRIDE {
@@ -5006,9 +5005,8 @@ class LayerTreeHostTestHybridRasterizationSetting : public LayerTreeHostTest {
     PictureLayerImpl* child =
         static_cast<PictureLayerImpl*>(parent->children()[0]);
 
-    // Only layers with a GPU rasterization hint should use GPU rasterization.
     EXPECT_TRUE(parent->ShouldUseGpuRasterization());
-    EXPECT_FALSE(child->ShouldUseGpuRasterization());
+    EXPECT_TRUE(child->ShouldUseGpuRasterization());
     EndTest();
   }
 
@@ -5037,7 +5035,7 @@ class LayerTreeHostTestGpuRasterizationSetting : public LayerTreeHostTest {
     child->SetBounds(gfx::Size(10, 10));
     parent->AddChild(child);
 
-    parent->SetHasGpuRasterizationHint(true);
+    layer_tree_host()->set_has_gpu_rasterization_trigger(false);
   }
 
   virtual void BeginTest() OVERRIDE { PostSetNeedsCommitToMainThread(); }
