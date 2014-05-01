a3f881c3eaed79520e9ea99787368d6de03ba838
mlerman@chromium.org <mlerman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-01 16:30:32 +0000

    Refresh Token status is tracked on the signin-internals page. The refresh token is acquired when the user signs in using the inline signin flow from the avatar menu.
    BUG=326577
    TEST=The signin internals page has a detail called "Refresh Token
    Received" which gets a date and time set whenever the user
    signs in to chrome through the avatar menu.
    
    Review URL: https://codereview.chromium.org/259103004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267526 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/signin/inline_login_handler_impl.cc
+++ chrome/browser/ui/webui/signin/inline_login_handler_impl.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/signin/about_signin_internals_factory.h"
 #include "chrome/browser/signin/profile_oauth2_token_service_factory.h"
 #include "chrome/browser/signin/signin_promo.h"
 #include "chrome/browser/sync/profile_sync_service.h"
@@ -24,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
+#include "components/signin/core/browser/about_signin_internals.h"
 #include "components/signin/core/browser/profile_oauth2_token_service.h"
 #include "components/signin/core/browser/signin_error_controller.h"
 #include "components/signin/core/browser/signin_oauth_helper.h"
@@ -104,6 +106,10 @@ void InlineSigninHelper::OnSigninOAuthInformationAvailable(
     browser = handler_->GetDesktopBrowser();
   }
 
+  AboutSigninInternals* about_signin_internals =
+    AboutSigninInternalsFactory::GetForProfile(profile_);
+  about_signin_internals->OnRefreshTokenReceived("Successful");
+
   signin::Source source = signin::GetSourceForPromoURL(current_url_);
   if (source == signin::SOURCE_AVATAR_BUBBLE_ADD_ACCOUNT) {
     ProfileOAuth2TokenServiceFactory::GetForProfile(profile_)->
@@ -168,6 +174,10 @@ void InlineSigninHelper::OnSigninOAuthInformationFailure(
   const GoogleServiceAuthError& error) {
   if (handler_)
     handler_->HandleLoginError(error.ToString());
+
+  AboutSigninInternals* about_signin_internals =
+    AboutSigninInternalsFactory::GetForProfile(profile_);
+  about_signin_internals->OnRefreshTokenReceived("Failure");
 
   base::MessageLoop::current()->DeleteSoon(FROM_HERE, this);
 }
