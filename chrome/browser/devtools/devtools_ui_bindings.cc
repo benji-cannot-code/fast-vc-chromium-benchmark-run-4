d2a87ee9cd2733383e8bc2b9943ae27991076a99
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-01 10:49:12 +0000

    DevTools: remove OpenUrlAndInspect. Open url and inspect instead.
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/267443002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267511 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/devtools/devtools_ui_bindings.cc
+++ chrome/browser/devtools/devtools_ui_bindings.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/chrome_page_zoom.h"
+#include "chrome/browser/devtools/devtools_target_impl.h"
 #include "chrome/browser/extensions/chrome_extension_web_contents_observer.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/infobars/confirm_infobar_delegate.h"
@@ -538,11 +539,18 @@ void DevToolsUIBindings::ResetZoom() {
   chrome_page_zoom::Zoom(web_contents(), content::PAGE_ZOOM_RESET);
 }
 
+static void InspectTarget(Profile* profile, DevToolsTargetImpl* target) {
+  if (target)
+    target->Inspect(profile);
+}
+
 void DevToolsUIBindings::OpenUrlOnRemoteDeviceAndInspect(
     const std::string& browser_id,
     const std::string& url) {
-  if (remote_targets_handler_)
-    remote_targets_handler_->OpenAndInspect(browser_id, url, profile_);
+  if (remote_targets_handler_) {
+    remote_targets_handler_->Open(browser_id, url,
+        base::Bind(&InspectTarget, profile_));
+  }
 }
 
 void DevToolsUIBindings::StartRemoteDevicesListener() {
