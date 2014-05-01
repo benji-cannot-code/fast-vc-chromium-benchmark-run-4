5541ee5cc050d5c8bb82be11170c6d56433ba0cf
senorblanco@chromium.org <senorblanco@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-01 23:58:15 +0000

    Fix image filter invalidation.
    When drawing a filter, if the filter is in the PaintingSource state,
    restore it to the Built state after processing. This is the same 
    as the non-deferred case in the code below it.
    
    BUG=369196
    
    Review URL: https://codereview.chromium.org/268713003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173138 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/svg/RenderSVGResourceFilter.cpp
+++ third_party/WebKit/Source/core/rendering/svg/RenderSVGResourceFilter.cpp
@@ -314,6 +314,7 @@ void RenderSVGResourceFilter::postApplyResource(RenderObject* object, GraphicsCo
 
     if (object->document().settings()->deferredFiltersEnabled() && (filterData->state == FilterData::PaintingSource || filterData->state == FilterData::Built)) {
         endDeferredFilter(context, filterData);
+        filterData->state = FilterData::Built;
         return;
     }
 
