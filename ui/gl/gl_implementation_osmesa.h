3a21ba4beb30571e5ff5261674e9fc272914a6f7
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-01 21:54:37 +0000

    gl: When failing to load a libGL library, print the reason why.
    R=piman, sievers
    BUG=364729
    
    Review URL: https://codereview.chromium.org/266713004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267641 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gl/gl_implementation_osmesa.h
+++ ui/gl/gl_implementation_osmesa.h
@@ -12,8 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace gfx {
 
 bool InitializeStaticGLBindingsOSMesaGL();
-base::NativeLibrary LoadLibrary(const char* filename);
-base::NativeLibrary LoadLibrary(const base::FilePath& filename);
+base::NativeLibrary LoadLibraryAndPrintError(const char* filename);
+base::NativeLibrary LoadLibraryAndPrintError(const base::FilePath& filename);
 
 }  // namespace gfx
 
