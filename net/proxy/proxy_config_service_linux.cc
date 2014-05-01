9dc4cd18f01d9757d27f1a77d81ab7bc897182bb
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-01 06:04:46 +0000

    Fix Trusty build with clang.
    In file included from ../../net/proxy/proxy_config_service_linux.cc:42:
    gen/library_loaders/libgio.h:29:13: error: 'g_settings_list_schemas' is
    deprecated [-Werror,-Wdeprecated-declarations]
      typeof(&::g_settings_list_schemas) g_settings_list_schemas;
                ^
    /usr/include/glib-2.0/gio/gsettings.h:74:25: note: 'g_settings_list_sch
    emas' has been explicitly marked deprecated here
    const gchar * const *   g_settings_list_schemas                 (void);
                            ^
    1 error generated.
    
    Review URL: https://codereview.chromium.org/267623002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267454 0039d316-1c4b-4281-b951-d872f2087c98

--- net/proxy/proxy_config_service_linux.cc
+++ net/proxy/proxy_config_service_linux.cc
@@ -40,6 +40,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/url_canon.h"
 
 #if defined(USE_GIO)
+#if __clang__
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+#endif
 #include "library_loaders/libgio.h"
 #endif  // defined(USE_GIO)
 
