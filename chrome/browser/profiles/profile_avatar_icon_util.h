dee34a0309cc39bbcc25455f52d895da0f8d84c4
noms@chromium.org <noms@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-01 16:38:53 +0000

    [Mac, Win] Avatar photo is now a circle in the new avatar bubble. Also cleaned up all the padding/margins to match the red lines in the mocks. But most importantly: LOOK AT THAT CIRCLE. <3
    Screenshots: https://drive.google.com/open?id=0B1B1Up4p2NRMU2trNTBvZ3c5aHc&authuser=1
    
    BUG=353398
    TEST=With the --new-profile-management flag on, start Chrome. Clicking on the avatar button should open the avatar
     bubble. Most notably, the avatar icon should be a circle.
    
    Review URL: https://codereview.chromium.org/250383007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267531 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_avatar_icon_util.h
+++ chrome/browser/profiles/profile_avatar_icon_util.h
@@ -27,7 +27,6 @@ extern const char kHighResAvatarFolderName[];
 // Avatar formatting.
 extern const int kAvatarIconWidth;
 extern const int kAvatarIconHeight;
-extern const int kAvatarIconPadding;
 extern const SkColor kAvatarTutorialBackgroundColor;
 extern const SkColor kAvatarTutorialContentTextColor;
 extern const SkColor kAvatarBubbleAccountsBackgroundColor;
