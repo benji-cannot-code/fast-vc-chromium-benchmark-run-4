f9fcf49ab1aac8ebcdb6cb66fe72ece6884028c0
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-01 21:53:56 +0000

    Enable Enterprise enrollment on desktop builds.
    This change implements some of the DBus stub methods so that enterprise enrollment works on desktop builds. That will make development of features that depend on enrollment faster for developers that use this workflow (e.g. for kiosk enterprise apps, public accounts, testing some device policies, etc).
    
    - Override some of the directories and files involved with the enrollment state
    - Simple stub implementation of the DBus calls involved
    - Write a persistent cache of the install attributes
    - Cleaned up the stub for user cloud policy and made them persistent too
    - Updated some tests
    
    This change doesn't affect production code.
    
    TBR=jochen@chromium.org
    BUG=240269, 367674
    
    Review URL: https://codereview.chromium.org/258743005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267640 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/policy/device_policy_cros_browser_test.h
+++ chrome/browser/chromeos/policy/device_policy_cros_browser_test.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
-#include "base/files/scoped_temp_dir.h"
 #include "chrome/browser/chromeos/policy/device_policy_builder.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chromeos/dbus/fake_dbus_thread_manager.h"
@@ -35,8 +34,7 @@ class DevicePolicyCrosTestHelper {
   DevicePolicyBuilder* device_policy() { return &device_policy_; }
 
  private:
-  // Stores the device owner key and the install attributes.
-  base::ScopedTempDir temp_dir_;
+  void OverridePaths();
 
   // Carries Chrome OS device policies for tests.
   DevicePolicyBuilder device_policy_;
