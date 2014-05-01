3cb476c1ae6239e92fa716758b3aceb330b2f99a
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-01 20:41:22 +0000

    Revert of Make SandboxIPCProcess a thread. (https://codereview.chromium.org/255693002/)
    Reason for revert:
    TSANv2 bot reports races on many many tests.
    
    Original issue's description:
    > Make SandboxIPCProcess a thread.
    > 
    > BUG=322185
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=267583
    
    TBR=jln@chromium.org,piman@chromium.org,jorgelo@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=322185
    
    Review URL: https://codereview.chromium.org/261783008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267625 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_sandbox_host_linux.h
+++ content/browser/renderer_host/render_sandbox_host_linux.h
@@ -9,9 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/logging.h"
-#include "base/memory/scoped_ptr.h"
-#include "base/threading/simple_thread.h"
-#include "content/browser/renderer_host/sandbox_ipc_linux.h"
 #include "content/common/content_export.h"
 
 template <typename T> struct DefaultSingletonTraits;
@@ -31,6 +28,10 @@ class CONTENT_EXPORT RenderSandboxHostLinux {
     DCHECK(initialized_);
     return renderer_socket_;
   }
+  pid_t pid() const {
+    DCHECK(initialized_);
+    return pid_;
+  }
   void Init(const std::string& sandbox_path);
 
  private:
@@ -43,9 +44,8 @@ class CONTENT_EXPORT RenderSandboxHostLinux {
   bool initialized_;
 
   int renderer_socket_;
-
-  scoped_ptr<SandboxIPCHandler> ipc_handler_;
-  scoped_ptr<base::DelegateSimpleThread> ipc_thread_;
+  int childs_lifeline_fd_;
+  pid_t pid_;
 
   DISALLOW_COPY_AND_ASSIGN(RenderSandboxHostLinux);
 };
