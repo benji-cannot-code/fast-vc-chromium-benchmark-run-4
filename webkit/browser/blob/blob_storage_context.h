dfc2f53c2bf00a7fdbad32b796fcb30010aefb2e
falken@chromium.org <falken@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-01 08:03:24 +0000

    Revert of Allow BlobDataHandles to be copied, and have their UUIDs read, on any thread. (https://codereview.chromium.org/259773006/)
    Reason for revert:
    I'm sorry to revert this change. It looks like it breaks the ASAN bot:
    
    http://build.chromium.org/p/chromium.memory/builders/Linux%20ASan%20LSan%20Tests%20%282%29/builds/2161/steps/content_unittests/logs/ResolveBlobAndCreateUploadDataStream
    
    Direct leak of 120 byte(s) in 3 object(s) allocated from:
        #0 0x520dbb in operator new(unsigned long) /usr/local/google/work/chromium/src/third_party/llvm/projects/compiler-rt/lib/asan/asan_new_delete.cc:62
        #1 0x3a0f611 in webkit_blob::BlobDataHandle::BlobDataHandle(webkit_blob::BlobData*, webkit_blob::BlobStorageContext*, base::SequencedTaskRunner*) webkit/browser/blob/blob_data_handle.cc:42
        #2 0x3a0fd71 in webkit_blob::BlobStorageContext::GetBlobDataFromUUID(std::string const&) webkit/browser/blob/blob_storage_context.cc:69
        #3 0x268955d in ResolveBlobReference content/browser/loader/upload_data_stream_builder.cc:73
    ...
    
    Original issue's description:
    > Allow BlobDataHandles to be copied, and have their UUIDs read, on any thread.
    > 
    > BUG=108012
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=266817
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=267423
    
    TBR=michaeln@chromium.org,piman@chromium.org,ericu@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=108012
    
    Review URL: https://codereview.chromium.org/261683005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267479 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/browser/blob/blob_storage_context.h
+++ webkit/browser/blob/blob_storage_context.h
@@ -54,7 +54,7 @@ class WEBKIT_STORAGE_BROWSER_EXPORT BlobStorageContext
 
  private:
   friend class content::BlobStorageHost;
-  friend class BlobDataHandle::BlobDataHandleShared;
+  friend class BlobDataHandle;
   friend class ViewBlobInternalsJob;
 
   enum EntryFlags {
