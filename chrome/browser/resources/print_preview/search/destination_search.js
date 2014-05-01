0e908807abac5ff1e91daa20943382347b221341
alekseys@chromium.org <alekseys@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-01 00:38:07 +0000

    Add metrics to printing destination account selection.
    BUG=139170
    
    Review URL: https://codereview.chromium.org/262523002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267395 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/print_preview/search/destination_search.js
+++ chrome/browser/resources/print_preview/search/destination_search.js
@@ -584,6 +584,8 @@ cr.define('print_preview', function() {
       if (account) {
         this.userInfo_.activeUser = account;
         this.destinationStore_.reloadUserCookieBasedDestinations();
+        this.metrics_.incrementDestinationSearchBucket(
+            print_preview.Metrics.DestinationSearchBucket.ACCOUNT_CHANGED);
       } else {
         cr.dispatchSimpleEvent(this, DestinationSearch.EventType.ADD_ACCOUNT);
         // Set selection back to the active user.
@@ -593,6 +595,8 @@ cr.define('print_preview', function() {
             break;
           }
         }
+        this.metrics_.incrementDestinationSearchBucket(
+            print_preview.Metrics.DestinationSearchBucket.ADD_ACCOUNT_SELECTED);
       }
     },
 
