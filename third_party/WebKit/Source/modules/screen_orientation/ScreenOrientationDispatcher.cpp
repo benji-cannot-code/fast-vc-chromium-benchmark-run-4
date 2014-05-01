6ccbb4432aec7b4e67e3fa7b3fc604b427db16a1
jsbell@chromium.org <jsbell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-01 17:06:34 +0000

    Oilpan: speculative build fix for ScreenOrientationDispatcher
    Oilpan builds broken by r173084 - just giving this a whirl to green
    those bots.
    
    TBR=haraken@chromium.org,zerny@chromium.org
    
    Review URL: https://codereview.chromium.org/266633008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173090 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/screen_orientation/ScreenOrientationDispatcher.cpp
+++ third_party/WebKit/Source/modules/screen_orientation/ScreenOrientationDispatcher.cpp
@@ -110,8 +110,8 @@ void ScreenOrientationDispatcher::didChangeScreenOrientation(blink::WebScreenOri
 
     if (m_needsPurge)
         purgeControllers();
-}
 #endif
+}
 
 void ScreenOrientationDispatcher::startListening()
 {
