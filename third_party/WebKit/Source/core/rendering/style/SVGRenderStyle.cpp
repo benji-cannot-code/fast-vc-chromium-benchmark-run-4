02b11afeac0c2e8d6ca4b5856a05c199198b42c8
wangxianzhu@chromium.org <wangxianzhu@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-01 18:35:53 +0000

    Optimize comparison of DataRefs
    This follows the following comment in RenderStyle::visualInvalidationDiff():
        // Note, we use .get() on each DataRef below because DataRef::operator== will do a deep
        // compare, which is duplicate work when we're going to compare each property inside
        // this function anyway.
    
    - Changed some unnecessary deep comparisons of DataRefs before
      comparing the fields of the DataRefs in SVGRenderStyle::diff()
      to .get() comparisons.
    
    - Added .get() comparison before multiple field comparisons.
    
    Review URL: https://codereview.chromium.org/260913002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173100 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/style/SVGRenderStyle.cpp
+++ third_party/WebKit/Source/core/rendering/style/SVGRenderStyle.cpp
@@ -152,7 +152,7 @@ bool SVGRenderStyle::diffNeedsLayout(const SVGRenderStyle* other) const
         return true;
 
     // Text related properties influence layout.
-    if (misc != other->misc && misc->baselineShiftValue != other->misc->baselineShiftValue)
+    if (misc->baselineShiftValue != other->misc->baselineShiftValue)
         return true;
 
     // These properties affect the cached stroke bounding box rects.
@@ -165,7 +165,7 @@ bool SVGRenderStyle::diffNeedsLayout(const SVGRenderStyle* other) const
         return true;
 
     // Some stroke properties, requires relayouts, as the cached stroke boundaries need to be recalculated.
-    if (stroke != other->stroke) {
+    if (stroke.get() != other->stroke.get()) {
         if (stroke->width != other->stroke->width
             || stroke->paintType != other->stroke->paintType
             || stroke->paintColor != other->stroke->paintColor
@@ -188,7 +188,7 @@ bool SVGRenderStyle::diffNeedsRepaint(const SVGRenderStyle* other) const
         return true;
 
     // Painting related properties only need repaints.
-    if (misc != other->misc) {
+    if (misc.get() != other->misc.get()) {
         if (misc->floodColor != other->misc->floodColor
             || misc->floodOpacity != other->misc->floodOpacity
             || misc->lightingColor != other->misc->lightingColor)
@@ -196,9 +196,13 @@ bool SVGRenderStyle::diffNeedsRepaint(const SVGRenderStyle* other) const
     }
 
     // If fill changes, we just need to repaint. Fill boundaries are not influenced by this, only by the Path, that RenderSVGPath contains.
-    if (fill->paintType != other->fill->paintType || fill->paintColor != other->fill->paintColor
-        || fill->paintUri != other->fill->paintUri || fill->opacity != other->fill->opacity)
-        return true;
+    if (fill.get() != other->fill.get()) {
+        if (fill->paintType != other->fill->paintType
+            || fill->paintColor != other->fill->paintColor
+            || fill->paintUri != other->fill->paintUri
+            || fill->opacity != other->fill->opacity)
+            return true;
+    }
 
     // If gradient stops change, we just need to repaint. Style updates are already handled through RenderSVGGradientSTop.
     if (stops != other->stops)
