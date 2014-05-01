841b64ecf05e4686ac3b40db49c314ab77d42d9d
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-01 23:49:14 +0000

    Remove URL from base's DEPS
    +url is added at the root, and we don't want base to depend on it. Remove this rule explicitly in base to prevent regressions.
    
    R=viettrungluu@chromium.org
    
    Review URL: https://codereview.chromium.org/262773006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267672 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/gn/value.cc
+++ tools/gn/value.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "tools/gn/value.h"
 
 #include "base/strings/string_number_conversions.h"
+#include "base/strings/string_util.h"
 #include "tools/gn/scope.h"
 
 Value::Value()
@@ -131,8 +132,17 @@ std::string Value::ToString(bool quote_string) const {
     case INTEGER:
       return base::Int64ToString(int_value_);
     case STRING:
-      if (quote_string)
-        return "\"" + string_value_ + "\"";
+      if (quote_string) {
+        std::string escaped = string_value_;
+        // First escape all special uses of a backslash.
+        ReplaceSubstringsAfterOffset(&escaped, 0, "\\$", "\\\\$");
+        ReplaceSubstringsAfterOffset(&escaped, 0, "\\\"", "\\\\\"");
+
+        // Now escape special chars.
+        ReplaceSubstringsAfterOffset(&escaped, 0, "$", "\\$");
+        ReplaceSubstringsAfterOffset(&escaped, 0, "\"", "\\\"");
+        return "\"" + escaped + "\"";
+      }
       return string_value_;
     case LIST: {
       std::string result = "[";
