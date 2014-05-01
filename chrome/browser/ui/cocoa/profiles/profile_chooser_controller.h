46bf6d4414620fb5487b13dd5ade51390bcdf53b
noms@chromium.org <noms@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-01 00:27:38 +0000

    [Mac, Win] The avatar bubble button for Guest profiles should close all guest browsers.
    BUG=365719
    TEST=Start Chrome with the --new-profile-management flag. Start a guest session
    from the User Manager. The avatar bubble should only display one button, "Exit
    Guest", which when pressed should close all guest browser windows and show the
    User Manager.
    
    Review URL: https://codereview.chromium.org/254073002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267389 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/profiles/profile_chooser_controller.h
+++ chrome/browser/ui/cocoa/profiles/profile_chooser_controller.h
@@ -90,6 +90,9 @@ class WebContents;
 // Shows the User Manager.
 - (IBAction)showUserManager:(id)sender;
 
+// Closes all guest browsers and shows the User Manager.
+- (IBAction)exitGuest:(id)sender;
+
 // Shows the account management view.
 - (IBAction)showAccountManagement:(id)sender;
 
