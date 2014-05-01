6908861ae319909a21961b2d51290084ffbcc236
michaelbai@chromium.org <michaelbai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-01 08:52:51 +0000

    This patch is also implements the general permission request.
    - AwPermissionRequest wraps a permission request.
    - AwPermissionRequestDelegate interface should be implemented by specific permission request like media access permission request.
    - PermissionRequestHandler, send the request to AwContentsClient or Cancel a ongoing request.
    
    NOTRY=true
    BUG=363301
    
    Review URL: https://codereview.chromium.org/239793002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267495 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/native/android_webview_jni_registrar.cc
+++ android_webview/native/android_webview_jni_registrar.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "android_webview/native/input_stream_impl.h"
 #include "android_webview/native/intercepted_request_data_impl.h"
 #include "android_webview/native/java_browser_view_renderer_helper.h"
+#include "android_webview/native/permission/aw_permission_request.h"
 #include "base/android/jni_android.h"
 #include "base/android/jni_registrar.h"
 #include "base/debug/trace_event.h"
@@ -43,6 +44,7 @@ static base::android::RegistrationMethod kWebViewRegisteredMethods[] = {
   { "AwSettings", RegisterAwSettings },
   { "AwHttpAuthHandler", RegisterAwHttpAuthHandler },
   { "AwPdfExporter", RegisterAwPdfExporter },
+  { "AwPermissionRequest", RegisterAwPermissionRequest },
   { "AwQuotaManagerBridge", RegisterAwQuotaManagerBridge },
   { "AwResource", AwResource::RegisterAwResource },
   { "AwWebContentsDelegate", RegisterAwWebContentsDelegate },
