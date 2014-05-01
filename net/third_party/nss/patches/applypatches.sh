f2823e19516aa0e532110a44178a82b84fe05985
davidben@chromium.org <davidben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-01 00:39:00 +0000

    Move signature_algorithm extension to the end in NSS.
    WebSphere Application Server 7.0 appears to be intolerant of an
    empty extension at the end. To that end, also ensure we never
    send an empty padding extension.
    
    BUG=363583
    
    Review URL: https://codereview.chromium.org/240633006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267396 0039d316-1c4b-4281-b951-d872f2087c98

--- net/third_party/nss/patches/applypatches.sh
+++ net/third_party/nss/patches/applypatches.sh
@@ -49,3 +49,5 @@ patch -p4 < $patches_dir/sessioncache.patch
 patch -p4 < $patches_dir/nssrwlock.patch
 
 patch -p4 < $patches_dir/paddingextvalue.patch
+
+patch -p4 < $patches_dir/reorderextensions.patch
