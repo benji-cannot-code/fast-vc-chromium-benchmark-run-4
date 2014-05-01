0731e6007b285c302d3da6824e1c393926116e8e
morrita@chromium.org <morrita@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-01 01:20:41 +0000

    HTML Imports: Let @async import not block its sibling.
    An import with @async attribute did block its following sibling,
    even though it doesn't block its parent. This is not what it should behave.
    This change removes that unwanted blocking.
    
    Also, the change adds a set of test for @async behavior: The original coverage
    was too poor.
    
    TBR=dglazkov@chromium.org,dominicc@chromium.org
    TEST=ipmort-async-grandchild.html, import-async-grandchild-not-child.html
         import-async-previous.html, import-async-previous-async.html
         import-async-sync-grandchild.html
    BUG=367371
    
    Review URL: https://codereview.chromium.org/251813006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173051 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/imports/HTMLImportStateResolver.cpp
+++ third_party/WebKit/Source/core/html/imports/HTMLImportStateResolver.cpp
@@ -38,6 +38,8 @@ namespace WebCore {
 
 inline bool HTMLImportStateResolver::isBlockingFollowers(HTMLImport* import)
 {
+    if (!import->isSync())
+        return false;
     if (!import->loader())
         return true;
     return !import->state().isReady();
@@ -51,7 +53,7 @@ inline bool HTMLImportStateResolver::shouldBlockScriptExecution() const
     }
 
     for (HTMLImport* child = m_import->firstChild(); child; child = child->next()) {
-        if (child->isSync() && isBlockingFollowers(child))
+        if (isBlockingFollowers(child))
             return true;
     }
 
