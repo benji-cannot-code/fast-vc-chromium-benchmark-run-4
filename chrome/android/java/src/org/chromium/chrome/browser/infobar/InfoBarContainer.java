ee7654fc8ace99f16af3037feed0a7361c3edb03
bulach@chromium.org <bulach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-01 17:09:50 +0000

    Android: minor cleanup on Infobar.
    Remove unused method (found when searching for "int native").
    
    BUG=346626, 354405
    
    Review URL: https://codereview.chromium.org/269653004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267553 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/android/java/src/org/chromium/chrome/browser/infobar/InfoBarContainer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/infobar/InfoBarContainer.java
@@ -39,6 +39,9 @@ public class InfoBarContainer extends LinearLayout {
     private static final String TAG = "InfoBarContainer";
     private static final long REATTACH_FADE_IN_MS = 250;
 
+    /**
+     * A listener for the InfoBar animation.
+     */
     public interface InfoBarAnimationListener {
         /**
          * Notifies the subscriber when an animation is completed.
@@ -209,15 +212,6 @@ public class InfoBarContainer extends LinearLayout {
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
         setVisibility(INVISIBLE);
-    }
-
-    public InfoBar findInfoBar(int nativeInfoBar) {
-        for (InfoBar infoBar : mInfoBars) {
-            if (infoBar.ownsNativeInfoBar(nativeInfoBar)) {
-                return infoBar;
-            }
-        }
-        return null;
     }
 
     /**
