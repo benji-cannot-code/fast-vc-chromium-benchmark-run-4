63b26e3e9c067152384e31202895d7cce438e743
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-01 06:12:27 +0000

    Renamed namespaces in src/content. url_util -> url url_parse -> url url_canon -> url
    BUG=364747
    
    Review URL: https://codereview.chromium.org/252323006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267457 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/url_schemes.cc
+++ content/common/url_schemes.cc
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 void AddStandardSchemeHelper(const std::string& scheme) {
-  url_util::AddStandardScheme(scheme.c_str());
+  url::AddStandardScheme(scheme.c_str());
 }
 
 }  // namespace
@@ -34,10 +34,10 @@ void RegisterContentSchemes(bool lock_standard_schemes) {
   GetContentClient()->AddAdditionalSchemes(&additional_standard_schemes,
                                            &additional_savable_schemes);
 
-  url_util::AddStandardScheme(kChromeDevToolsScheme);
-  url_util::AddStandardScheme(kChromeUIScheme);
-  url_util::AddStandardScheme(kGuestScheme);
-  url_util::AddStandardScheme(kMetadataScheme);
+  url::AddStandardScheme(kChromeDevToolsScheme);
+  url::AddStandardScheme(kChromeUIScheme);
+  url::AddStandardScheme(kGuestScheme);
+  url::AddStandardScheme(kMetadataScheme);
   std::for_each(additional_standard_schemes.begin(),
                 additional_standard_schemes.end(),
                 AddStandardSchemeHelper);
@@ -48,7 +48,7 @@ void RegisterContentSchemes(bool lock_standard_schemes) {
   // thread. This is really easy to mess up, so we say that all calls to
   // AddStandardScheme in Chrome must be inside this function.
   if (lock_standard_schemes)
-    url_util::LockStandardSchemes();
+    url::LockStandardSchemes();
 
   // We rely on the above lock to protect this part from being invoked twice.
   if (!additional_savable_schemes.empty()) {
