860f082a2c83fa6f3411711fd585ab8f506fd819
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-01 18:01:49 +0000

    Renamed namespaces of url lib. url_util -> url url_parse -> url url_canon -> url
    BUG=364747
    TBR=atwilson,battre
    
    Review URL: https://codereview.chromium.org/262593002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267579 0039d316-1c4b-4281-b951-d872f2087c98

--- printing/backend/cups_helper.cc
+++ printing/backend/cups_helper.cc
@@ -315,7 +315,7 @@ HttpConnectionCUPS::HttpConnectionCUPS(const GURL& print_server_url,
     return;
 
   int port = print_server_url.IntPort();
-  if (port == url_parse::PORT_UNSPECIFIED)
+  if (port == url::PORT_UNSPECIFIED)
     port = kDefaultIPPServerPort;
 
   http_ = httpConnectEncrypt(print_server_url.host().c_str(), port, encryption);
