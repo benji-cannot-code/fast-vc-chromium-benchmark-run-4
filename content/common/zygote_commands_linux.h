1365bcc8f7630381970270c4becd6e52191c92ff
mdempsky@chromium.org <mdempsky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-01 06:03:40 +0000

    Make PID translation sanity checks more robust
    Instead of checking that the PID the kernel told us for the zygote
    happens to be one of our descendents, we have the zygote send us an
    extra message right away.  This gives us a check of PID translation
    without any false positives or negatives.
    
    BUG=357670
    
    Review URL: https://codereview.chromium.org/254093003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267453 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/zygote_commands_linux.h
+++ content/common/zygote_commands_linux.h
@@ -11,6 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
+// Contents of the initial message sent from the zygote to the browser right
+// after it starts.
+static const char kZygoteBootMessage[] = "ZYGOTE_BOOT";
+
 // Contents of the initial message sent from the zygote to the browser when it
 // is ready to go.
 static const char kZygoteHelloMessage[] = "ZYGOTE_OK";
