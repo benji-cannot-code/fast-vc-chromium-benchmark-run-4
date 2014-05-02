30c921b56da1418721786ea60105ec31fe5780e6
maniscalco@chromium.org <maniscalco@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-02 19:00:11 +0000

    Keep track of which attachments are referenced by which sync entries.
    Relanding https://codereview.chromium.org/247983002/ after fixing
    memory leak.
    
    PutAttachmentMetadata on MutableEntry now notifies the Directory when
    the attachments associated with an entry change.
    
    Give Directory::InitializeIndices a ScopedKernelLock to be consistent
    and make it easier to ensure we are locking when we need to.
    
    GenericChangeProcess passes new attachments to AttachmentService for
    storage and upload.
    
    Add an output parameter to GenericChangeProcessor's HandleActionAdd and
    HandleActionUpdate methods so they can keep track of potentially new
    attachments and pass them to AttachmentService for storage/upload.
    
    Convert AttachmentService's OnSyncDataAdd to StoreAttachments.
    
    Implement HasAttachmentNotOnServer.
    
    BUG=348625,353303,354530,356266
    
    Review URL: https://codereview.chromium.org/264793007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267887 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/api/attachments/attachment_service_proxy.h
+++ sync/api/attachments/attachment_service_proxy.h
@@ -59,7 +59,8 @@ class SYNC_EXPORT AttachmentServiceProxy : public AttachmentService {
       const GetOrDownloadCallback& callback) OVERRIDE;
   virtual void DropAttachments(const AttachmentIdList& attachment_ids,
                                const DropCallback& callback) OVERRIDE;
-  virtual void OnSyncDataAdd(const SyncData& sync_data) OVERRIDE;
+  virtual void StoreAttachments(const AttachmentList& attachment,
+                                const StoreCallback& callback) OVERRIDE;
   virtual void OnSyncDataDelete(const SyncData& sync_data) OVERRIDE;
   virtual void OnSyncDataUpdate(const AttachmentIdList& old_attachment_ids,
                                 const SyncData& updated_sync_data) OVERRIDE;
@@ -90,7 +91,8 @@ class SYNC_EXPORT AttachmentServiceProxy : public AttachmentService {
         const GetOrDownloadCallback& callback) OVERRIDE;
     virtual void DropAttachments(const AttachmentIdList& attachment_ids,
                                  const DropCallback& callback) OVERRIDE;
-    virtual void OnSyncDataAdd(const SyncData& sync_data) OVERRIDE;
+    virtual void StoreAttachments(const AttachmentList& attachment,
+                                  const StoreCallback& callback) OVERRIDE;
     virtual void OnSyncDataDelete(const SyncData& sync_data) OVERRIDE;
     virtual void OnSyncDataUpdate(const AttachmentIdList& old_attachment_ids,
                                   const SyncData& updated_sync_data) OVERRIDE;
