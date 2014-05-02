39f66cafb50063990f8c449f6bb3cb44ef926b26
mgiuca@chromium.org <mgiuca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-02 17:35:57 +0000

    Renamed app list position flag to --enable-centered-app-list.
    Previous name was --enable-experimental-app-list-position.
    
    We will soon be using this on real devices without the flag under
    certain conditions (when the virtual keyboard is enabled), so it is no
    longer experimental.
    
    Updated strings and internal constant/function names to match.
    
    BUG=369382
    
    Review URL: https://codereview.chromium.org/263883002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267844 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/app_list/app_list_switches.h
+++ ui/app_list/app_list_switches.h
@@ -15,8 +15,8 @@ namespace switches {
 APP_LIST_EXPORT extern const char kDisableSyncAppList[];
 APP_LIST_EXPORT extern const char kDisableVoiceSearch[];
 APP_LIST_EXPORT extern const char kEnableAppInfo[];
+APP_LIST_EXPORT extern const char kEnableCenteredAppList[];
 APP_LIST_EXPORT extern const char kEnableExperimentalAppList[];
-APP_LIST_EXPORT extern const char kEnableExperimentalAppListPosition[];
 APP_LIST_EXPORT extern const char kEnableHotwordAlwaysOn[];
 APP_LIST_EXPORT extern const char kEnableSyncAppList[];
 
@@ -30,7 +30,7 @@ bool APP_LIST_EXPORT IsAppInfoEnabled();
 
 bool APP_LIST_EXPORT IsExperimentalAppListEnabled();
 
-bool APP_LIST_EXPORT IsExperimentalAppListPositionEnabled();
+bool APP_LIST_EXPORT IsCenteredAppListEnabled();
 
 }  // namespace switches
 }  // namespace app_list
