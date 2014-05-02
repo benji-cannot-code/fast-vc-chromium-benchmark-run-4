ea43566eb038241daf756760f2969837716059ef
engedy@chromium.org <engedy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-02 18:08:29 +0000

    Now that zoom levels are visible in Content Settings, we need to make sure no garbage custom zoom levels are stored.
    This CL addresses the following two problematic cases:
     * In certain scenarios, we inadvertently saved (default_zoom_level + EPSILON) values as custom zoom levels.
     * In some cases, we inadvertently stored a zoom level for the empty host.
    
    Make so neither can happen any more, and also sanitize data persisted by past versions.
    
    BUG=364399
    
    Review URL: https://codereview.chromium.org/266523003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267865 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/off_the_record_profile_impl_unittest.cc
+++ chrome/browser/profiles/off_the_record_profile_impl_unittest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/testing_profile_manager.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "content/public/browser/host_zoom_map.h"
+#include "content/public/common/page_zoom.h"
 #include "net/dns/mock_host_resolver.h"
 
 using content::HostZoomMap;
@@ -58,7 +59,7 @@ class TestingProfileWithHostZoomMap : public TestingProfile {
     double level = change.zoom_level;
     DictionaryPrefUpdate update(prefs_.get(), prefs::kPerHostZoomLevels);
     base::DictionaryValue* host_zoom_dictionary = update.Get();
-    if (level == host_zoom_map->GetDefaultZoomLevel()) {
+    if (content::ZoomValuesEqual(level, host_zoom_map->GetDefaultZoomLevel())) {
       host_zoom_dictionary->RemoveWithoutPathExpansion(change.host, NULL);
     } else {
       host_zoom_dictionary->SetWithoutPathExpansion(
