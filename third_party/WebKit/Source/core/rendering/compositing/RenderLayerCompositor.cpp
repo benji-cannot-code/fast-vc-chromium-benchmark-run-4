6f790d4e329166f2ec112c718e9be23f4a1ca240
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-02 22:15:49 +0000

    Don't clobber equivalent paint info when changing squashing assignment.
    This CL makes two changes:
    
    1) If the new and old GraphicsLayerPaintInfo objects are
    equivalent for squashing, there's no need to overwrite the
    other fields in the struct.
    
    2) When we add or remove a RenderLayer from a squashing
    layer, we now mark the CompositedLayerMapping for a graphics
    update. This change ensures that we'll program the data about
    the newly squashed layers into the compositor.
    
    This CL is based on https://codereview.chromium.org/265083002
    by vollick@
    
    BUG=368504
    
    Review URL: https://codereview.chromium.org/261983006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173237 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/compositing/RenderLayerCompositor.cpp
+++ third_party/WebKit/Source/core/rendering/compositing/RenderLayerCompositor.cpp
@@ -616,6 +616,10 @@ bool RenderLayerCompositor::updateSquashingAssignment(RenderLayer* layer, Squash
         if (!changedSquashingLayer)
             return true;
 
+        // If we've modified the collection of squashed layers, we must update
+        // the graphics layer geometry.
+        squashingState.mostRecentMapping->setNeedsGraphicsLayerUpdate();
+
         layer->clipper().clearClipRectsIncludingDescendants();
 
         // If we need to repaint, do so before allocating the layer to the squashing layer.
@@ -631,7 +635,10 @@ bool RenderLayerCompositor::updateSquashingAssignment(RenderLayer* layer, Squash
 
         return true;
     } else if (compositedLayerUpdate == RemoveFromSquashingLayer) {
-        layer->setGroupedMapping(0);
+        if (layer->groupedMapping()) {
+            layer->groupedMapping()->setNeedsGraphicsLayerUpdate();
+            layer->setGroupedMapping(0);
+        }
 
         // This layer and all of its descendants have cached repaints rects that are relative to
         // the repaint container, so change when compositing changes; we need to update them here.
