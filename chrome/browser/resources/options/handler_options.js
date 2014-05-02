bfdef6608558cfbf641fe3dfe37dbf0bbd080003
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-02 23:45:37 +0000

    options: fix content settings exceptions dialog regression.
    Stupid natural numbers.
    
    BUG=367636
    R=estade@chromium.org
    
    Review URL: https://codereview.chromium.org/264713008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267975 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/handler_options.js
+++ chrome/browser/resources/options/handler_options.js
@@ -28,7 +28,7 @@ cr.define('options', function() {
 
     /**
      * The handlers list.
-     * @type {DeletableItemList}
+     * @type {options.HandlersList}
      * @private
      */
     handlersList_: null,
