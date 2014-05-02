36b4ae35d31254abea251e5479db718e3d08d01c
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-02 05:01:14 +0000

    Change SelectionRequestor::PerformBlockingConvertSelection() so that it can be used after the ui::PlatformEventSource is destroyed.
    This CL is a prerequisite for https://codereview.chromium.org/235733003/
    
    BUG=365947
    TEST=None
    
    Review URL: https://codereview.chromium.org/263513002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267746 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/x/selection_requestor.h
+++ ui/base/x/selection_requestor.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/x/x11_atom_cache.h"
 
 namespace ui {
+class PlatformEventDispatcher;
 class SelectionData;
 
 // Requests and later receives data from the X11 server through the selection
@@ -34,7 +35,8 @@ class UI_BASE_EXPORT SelectionRequestor {
  public:
   SelectionRequestor(Display* xdisplay,
                      ::Window xwindow,
-                     ::Atom selection_name);
+                     ::Atom selection_name,
+                     PlatformEventDispatcher* dispatcher);
   ~SelectionRequestor();
 
   // Does the work of requesting |target| from the selection we handle,
@@ -58,16 +60,9 @@ class UI_BASE_EXPORT SelectionRequestor {
   void OnSelectionNotify(const XSelectionEvent& event);
 
  private:
-  // Our X11 state.
-  Display* x_display_;
-  ::Window x_window_;
-
-  // The X11 selection that this instance communicates on.
-  ::Atom selection_name_;
-
   // A request that has been issued and we are waiting for a response to.
   struct PendingRequest {
-    PendingRequest(Atom target, base::Closure quit_closure);
+    explicit PendingRequest(Atom target);
     ~PendingRequest();
 
     // Data to the current XConvertSelection request. Used for error detection;
@@ -86,6 +81,24 @@ class UI_BASE_EXPORT SelectionRequestor {
     // Set to true when return_property is populated.
     bool returned;
   };
+
+  // Blocks till SelectionNotify is received for the target specified in
+  // |request|.
+  void BlockTillSelectionNotifyForRequest(PendingRequest* request);
+
+  // Our X11 state.
+  Display* x_display_;
+  ::Window x_window_;
+
+  // The X11 selection that this instance communicates on.
+  ::Atom selection_name_;
+
+  // Dispatcher which handles SelectionNotify and SelectionRequest for
+  // |selection_name_|. PerformBlockingConvertSelection() calls the
+  // dispatcher directly if PerformBlockingConvertSelection() is called after
+  // the PlatformEventSource is destroyed.
+  // Not owned.
+  PlatformEventDispatcher* dispatcher_;
 
   // A list of requests for which we are waiting for responses.
   std::list<PendingRequest*> pending_requests_;
