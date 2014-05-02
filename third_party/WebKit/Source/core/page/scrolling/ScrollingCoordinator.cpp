913aed96930683c9b249f769410a1b7f5489155a
skyostil@chromium.org <skyostil@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-02 11:08:08 +0000

    Hook up scroll event handler tracking to ScrollingCoordinator
    BUG=359566
    
    Review URL: https://codereview.chromium.org/265883002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173195 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/scrolling/ScrollingCoordinator.cpp
+++ third_party/WebKit/Source/core/page/scrolling/ScrollingCoordinator.cpp
@@ -33,10 +33,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/FullscreenElementStack.h"
 #include "core/dom/Node.h"
 #include "core/dom/WheelController.h"
-#include "core/html/HTMLElement.h"
 #include "core/frame/FrameView.h"
 #include "core/frame/LocalFrame.h"
 #include "core/frame/Settings.h"
+#include "core/html/HTMLElement.h"
+#include "core/page/EventHandlerRegistry.h"
 #include "core/page/Page.h"
 #include "core/plugins/PluginView.h"
 #include "core/rendering/RenderGeometryMap.h"
@@ -679,8 +680,7 @@ void ScrollingCoordinator::updateHaveScrollEventHandlers()
     // instead on a per-layer basis. We therefore only update this information for the root
     // scrolling layer.
     if (WebLayer* scrollLayer = toWebLayer(m_page->mainFrame()->view()->layerForScrolling())) {
-        // TODO(skyostil): Hook this up.
-        bool haveHandlers = false;
+        bool haveHandlers = EventHandlerRegistry::from(*m_page)->hasEventHandlers(EventHandlerRegistry::ScrollEvent);
         scrollLayer->setHaveScrollEventHandlers(haveHandlers);
     }
 }
