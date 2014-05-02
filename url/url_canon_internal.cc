221082a03543bd67dfde0318c4dfcb99bdc3bf47
mmenke@chromium.org <mmenke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-02 15:58:31 +0000

    Remove url::ReadUTFChar's dependency on icu.  Use base instead.
    BUG=362608
    
    Review URL: https://codereview.chromium.org/250233002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267800 0039d316-1c4b-4281-b951-d872f2087c98

--- url/url_canon_internal.cc
+++ url/url_canon_internal.cc
@@ -3,13 +3,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "url/url_canon_internal.h"
+
 #include <errno.h>
 #include <stdlib.h>
 
 #include <cstdio>
 #include <string>
 
-#include "url/url_canon_internal.h"
+#include "base/strings/utf_string_conversion_utils.h"
 
 namespace url {
 
@@ -244,6 +246,32 @@ void AppendStringOfType(const base::char16* source, int length,
                         CanonOutput* output) {
   DoAppendStringOfType<base::char16, base::char16>(
       source, length, type, output);
+}
+
+bool ReadUTFChar(const char* str, int* begin, int length,
+                 unsigned* code_point_out) {
+  // This depends on ints and int32s being the same thing.  If they're not, it
+  // will fail to compile.
+  // TODO(mmenke):  This should probably be fixed.
+  if (!base::ReadUnicodeCharacter(str, length, begin, code_point_out) ||
+      !base::IsValidCharacter(*code_point_out)) {
+    *code_point_out = kUnicodeReplacementCharacter;
+    return false;
+  }
+  return true;
+}
+
+bool ReadUTFChar(const base::char16* str, int* begin, int length,
+                 unsigned* code_point_out) {
+  // This depends on ints and int32s being the same thing.  If they're not, it
+  // will fail to compile.
+  // TODO(mmenke):  This should probably be fixed.
+  if (!base::ReadUnicodeCharacter(str, length, begin, code_point_out) ||
+      !base::IsValidCharacter(*code_point_out)) {
+    *code_point_out = kUnicodeReplacementCharacter;
+    return false;
+  }
+  return true;
 }
 
 void AppendInvalidNarrowString(const char* spec, int begin, int end,
