0ba95377cdc52b850058dc87e267ec8795c2613f
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-02 09:47:42 +0000

    Add _api_features.json for chrome.shell API
    The chrome.shell API is only available in app_shell, so it should not be part
    of the common extensions _api_features.json.
    
    This fixes a CHECK failure in the renderer when loading the Calculator sample
    app, which tests for the existence of chrome.shell.  (The schema is not
    available, because the API is only implemented in app_shell, but chrome.shell
    returns true because the "shell" is in the shared extensions _api_features.json.)
    
    BUG=369318
    TEST=manual
    
    Review URL: https://codereview.chromium.org/260963006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267776 0039d316-1c4b-4281-b951-d872f2087c98

--- apps/shell/common/shell_extensions_client.cc
+++ apps/shell/common/shell_extensions_client.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/permissions/permission_message_provider.h"
 #include "extensions/common/permissions/permissions_provider.h"
 #include "extensions/common/url_pattern_set.h"
+#include "grit/app_shell_resources.h"
 #include "grit/common_resources.h"
 #include "grit/extensions_resources.h"
 
@@ -133,8 +134,9 @@ scoped_ptr<FeatureProvider> ShellExtensionsClient::CreateFeatureProvider(
     const std::string& name) const {
   extensions::JSONFeatureProviderSource source(name);
   if (name == "api") {
-    // TODO(yoz): Only include src/extensions resources.
     source.LoadJSON(IDR_EXTENSION_API_FEATURES);
+    source.LoadJSON(IDR_SHELL_EXTENSION_API_FEATURES);
+    // TODO(yoz): Don't include Chrome resources.
     source.LoadJSON(IDR_CHROME_EXTENSION_API_FEATURES);
     return scoped_ptr<FeatureProvider>(new BaseFeatureProvider(
         source.dictionary(), CreateFeature<extensions::APIFeature>));
