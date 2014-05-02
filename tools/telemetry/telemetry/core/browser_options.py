733be6b46fe355b5ac0445fef9cd34a431e90398
falken@chromium.org <falken@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-02 07:47:59 +0000

    Revert of [Telemetry] Cache browser finder. (https://codereview.chromium.org/240593005/)
    Reason for revert:
    Speculative revert. After this patch the following tests continued to fail on Linux ChromiumOS Tests:
    testCommandLineOverriding
    testIFrame
    
    Log: http://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Tests%20%281%29/builds/42811
    
    Snippet from log:
    File "/mnt/data/b/build/slave/Linux_ChromiumOS_Tests__1_/build/src/tools/telemetry/telemetry/core/browser_unittest.py", line 72, in testCommandLineOverriding
    'telemetry')
    AssertionError: u'Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus 7 Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/27.0.1453.111 Safari/535.19' != 'telemetry'
    
    Original issue's description:
    > [Telemetry] Cache browser finder.
    > 
    > It turns out we were creating one of these for nearly every unittest. This led
    > to a couple of problems:
    > 
    > 1. Cycle time. With this patch, "tools/telemetry/run_tests Browser" is 20-25%
    >     faster (195s -> 150s).
    > 2. Spam. With this patch, we output many fewer warnings about missing flash.
    > 
    > BUG=337829
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=267744
    
    TBR=tonyg
    NOTREECHECKS=true
    NOTRY=true
    BUG=337829
    
    Review URL: https://codereview.chromium.org/269693006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267764 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/browser_options.py
+++ tools/telemetry/telemetry/core/browser_options.py
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import collections
 import copy
 import logging
 import optparse
@@ -22,7 +21,7 @@ util.AddDirToPythonPath(
 import net_configs  # pylint: disable=F0401
 
 
-class BrowserFinderOptions(optparse.Values, object):
+class BrowserFinderOptions(optparse.Values):
   """Options to be used for discovering a browser."""
 
   def __init__(self, browser_type=None):
@@ -52,17 +51,8 @@ class BrowserFinderOptions(optparse.Values, object):
 
     self.android_rndis = False
 
-  def __repr__(self):
-    return str(sorted(self.__dict__.items()))
-
-  def __copy__(self):
-    cls = self.__class__
-    result = cls.__new__(cls)
-    for k, v in self.__dict__.items():
-      if isinstance(v, collections.Container):
-        v = copy.copy(v)
-      setattr(result, k, v)
-    return result
+  def Copy(self):
+    return copy.deepcopy(self)
 
   def CreateParser(self, *args, **kwargs):
     parser = optparse.OptionParser(*args, **kwargs)
@@ -190,6 +180,8 @@ class BrowserOptions(object):
     self.browser_type = None
     self.show_stdout = False
 
+    self.warn_if_no_flash = True
+
     # When set to True, the browser will use the default profile.  Telemetry
     # will not provide an alternate profile directory.
     self.dont_override_profile = False
@@ -209,18 +201,6 @@ class BrowserOptions(object):
     # Background pages of built-in component extensions can interfere with
     # performance measurements.
     self.disable_component_extensions_with_background_pages = True
-
-  def __copy__(self):
-    cls = self.__class__
-    result = cls.__new__(cls)
-    for k, v in self.__dict__.items():
-      if isinstance(v, collections.Container):
-        v = copy.copy(v)
-      setattr(result, k, v)
-    return result
-
-  def __repr__(self):
-    return str(sorted(self.__dict__.items()))
 
   @classmethod
   def AddCommandLineArgs(cls, parser):
