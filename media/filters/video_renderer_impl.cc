3f14ad68a8bf757bf00104fa64e849761d94054f
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-02 17:43:37 +0000

    Disable preroll for live media streams.
    BUG=338529
    R=acolwell@chromium.org, scherkus@chromium.org
    
    Review URL: https://codereview.chromium.org/262763003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267849 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/video_renderer_impl.cc
+++ media/filters/video_renderer_impl.cc
@@ -27,6 +27,7 @@ VideoRendererImpl::VideoRendererImpl(
     bool drop_frames)
     : task_runner_(task_runner),
       video_frame_stream_(task_runner, decoders.Pass(), set_decryptor_ready_cb),
+      low_delay_(false),
       received_end_of_stream_(false),
       frame_available_(&lock_),
       state_(kUninitialized),
@@ -168,6 +169,8 @@ void VideoRendererImpl::Initialize(DemuxerStream* stream,
   DCHECK(!get_time_cb.is_null());
   DCHECK(!get_duration_cb.is_null());
   DCHECK_EQ(kUninitialized, state_);
+
+  low_delay_ = low_delay;
 
   init_cb_ = init_cb;
   statistics_cb_ = statistics_cb;
@@ -408,7 +411,8 @@ void VideoRendererImpl::FrameReady(VideoFrameStream::Status status,
 bool VideoRendererImpl::ShouldTransitionToPrerolled_Locked() {
   return state_ == kPrerolling &&
       (!video_frame_stream_.CanReadWithoutStalling() ||
-       ready_frames_.size() >= static_cast<size_t>(limits::kMaxVideoFrames));
+       ready_frames_.size() >= static_cast<size_t>(limits::kMaxVideoFrames) ||
+       (low_delay_ && ready_frames_.size() > 0));
 }
 
 void VideoRendererImpl::AddReadyFrame_Locked(
@@ -454,6 +458,7 @@ void VideoRendererImpl::AttemptRead_Locked() {
   switch (state_) {
     case kPaused:
     case kPrerolling:
+    case kPrerolled:
     case kPlaying:
       pending_read_ = true;
       video_frame_stream_.Read(base::Bind(&VideoRendererImpl::FrameReady,
@@ -462,7 +467,6 @@ void VideoRendererImpl::AttemptRead_Locked() {
 
     case kUninitialized:
     case kInitializing:
-    case kPrerolled:
     case kFlushing:
     case kFlushed:
     case kEnded:
