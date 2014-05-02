be6a32d59e4c77f2756ddc482ccc89dbf68e4e44
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-02 17:49:47 +0000

    Mojo: Add simple test utils to run the mojom parser and translater.
    (I previously broke the ability to run parser.py directly, due to the
    relative imports.)
    
    R=yzshen@chromium.org
    
    Review URL: https://codereview.chromium.org/265893004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267853 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/public/tools/bindings/pylib/mojom/parse/parser.py
+++ mojo/public/tools/bindings/pylib/mojom/parse/parser.py
@@ -1,5 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env python
 # Copyright 2014 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -361,24 +360,3 @@ def Parse(source, filename):
 
   tree = yacc.parse(source)
   return tree
-
-
-def main(argv):
-  if len(argv) < 2:
-    print "usage: %s filename" % argv[0]
-    return 0
-
-  for filename in argv[1:]:
-    with open(filename) as f:
-      print "%s:" % filename
-      try:
-        print Parse(f.read(), filename)
-      except ParseError, e:
-        print e
-        return 1
-
-  return 0
-
-
-if __name__ == '__main__':
-  sys.exit(main(sys.argv))
