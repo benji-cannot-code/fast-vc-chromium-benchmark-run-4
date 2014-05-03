1b1d5bf7b5ce6fb18e2bf240facdf4a86afc6d03
newt@chromium.org <newt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-03 00:29:37 +0000

    Allow overriding Google doodle URL on Android.
    This makes the LogoService respect the --google-base-url command line
    flag when determining the URL from which to download Google doodles.
    This allows easier manual testing of the doodle: if there's a doodle in
    France today, set the flag --google-base-url=https://www.google.fr and
    the current google in France will be shown.
    
    BUG=369676
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/264713013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267991 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/android/logo_service.cc
+++ chrome/browser/android/logo_service.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/google/google_url_tracker.h"
+#include "chrome/browser/google/google_util.h"
 #include "chrome/browser/image_decoder.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search_engines/template_url_service.h"
@@ -36,7 +37,11 @@ GURL GetGoogleDoodleURL(Profile* profile) {
   std::string path = kGoogleDoodleURLPath;
   GURL::Replacements replacements;
   replacements.SetPathStr(path);
-  return GoogleURLTracker::GoogleURL(profile).ReplaceComponents(replacements);
+
+  GURL base_url(google_util::CommandLineGoogleBaseURL());
+  if (!base_url.is_valid())
+    base_url = GoogleURLTracker::GoogleURL(profile);
+  return base_url.ReplaceComponents(replacements);
 }
 
 class LogoDecoderDelegate : public ImageDecoder::Delegate {
