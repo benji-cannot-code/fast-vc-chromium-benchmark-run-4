b11b597983d04c3cec81d2c53462d912a631528f
matthewturk@gmail.com <matthewturk@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-03 02:58:22 +0000

    [NaCl SDK] Modifying size_t and int to off_t.
    This modifies a few locations that utilize size_t and int to track the
    number of bytes to instead use off_t.
    
    BUG=369279
    R=binji@chromium.org, sbc@chromium.org
    
    Review URL: https://codereview.chromium.org/269593011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@268022 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/tests/nacl_io_test/mem_fs_node_test.cc
+++ native_client_sdk/src/tests/nacl_io_test/mem_fs_node_test.cc
@@ -69,7 +69,7 @@ class DirNodeForTesting : public DirNode {
 TEST(MemFsNodeTest, File) {
   MemFsNodeForTesting file;
   ScopedNode result_node;
-  size_t result_size = 0;
+  off_t result_size = 0;
   int result_bytes = 0;
 
   EXPECT_EQ(0, file.Init(0));
@@ -121,7 +121,7 @@ TEST(MemFsNodeTest, File) {
 
 TEST(MemFsNodeTest, FTruncate) {
   MemFsNodeForTesting file;
-  size_t result_size = 0;
+  off_t result_size = 0;
   int result_bytes = 0;
 
   char data[1024];
@@ -198,7 +198,7 @@ TEST(MemFsNodeTest, Directory) {
   s_alloc_num = 0;
   DirNodeForTesting root;
   ScopedNode result_node;
-  size_t result_size = 0;
+  off_t result_size = 0;
   int result_bytes = 0;
 
   root.Init(0);
