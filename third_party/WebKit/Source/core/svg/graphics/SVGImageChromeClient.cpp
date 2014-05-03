fdbf63fd8de4f37df806eac2b64e5e5ccae72628
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-03 00:03:28 +0000

    Add a ScriptForbiddenScope to SVGImageChromeClient
    The comment in ::animationTimerFired was wrong, the PageAnimator's
    serviceScriptedAnimations does run requestAnimationFrame callbacks, but
    SVGImages never have any so lets just add a ScriptForbiddenScope.
    
    Review URL: https://codereview.chromium.org/265053004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173251 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/graphics/SVGImageChromeClient.cpp
+++ third_party/WebKit/Source/core/svg/graphics/SVGImageChromeClient.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "core/svg/graphics/SVGImageChromeClient.h"
 
+#include "core/dom/ScriptForbiddenScope.h"
 #include "core/frame/FrameView.h"
 #include "core/svg/graphics/SVGImage.h"
 #include "platform/graphics/ImageObserver.h"
@@ -81,12 +82,13 @@ void SVGImageChromeClient::scheduleAnimation()
 
 void SVGImageChromeClient::animationTimerFired(Timer<SVGImageChromeClient>*)
 {
-    // In principle, we should call requestAnimationFrame callbacks here, but
-    // we know there aren't any because script is forbidden inside SVGImages.
-    if (m_image) {
-        m_image->frameView()->page()->animator().serviceScriptedAnimations(monotonicallyIncreasingTime());
-        m_image->frameView()->updateLayoutAndStyleForPainting();
-    }
+    if (!m_image)
+        return;
+    // serviceScriptedAnimations runs requestAnimationFrame callbacks, but SVG
+    // images can't have any so we assert there's no script.
+    ScriptForbiddenScope forbidScript;
+    m_image->frameView()->page()->animator().serviceScriptedAnimations(monotonicallyIncreasingTime());
+    m_image->frameView()->updateLayoutAndStyleForPainting();
 }
 
 }
