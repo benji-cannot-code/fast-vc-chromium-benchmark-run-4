801eda33328c892a8fe4c95b02fe3b76b12f91eb
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-03 00:54:01 +0000

    Move didHandleOnloadEvents to RenderFrame.
    BUG=361761
    TEST=no visible change
    
    Review URL: https://codereview.chromium.org/261783007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267994 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_view_host_impl.cc
+++ content/browser/renderer_host/render_view_host_impl.cc
@@ -1005,8 +1005,6 @@ bool RenderViewHostImpl::OnMessageReceived(const IPC::Message& msg) {
                         OnDidChangeLoadProgress)
     IPC_MESSAGE_HANDLER(ViewHostMsg_DocumentAvailableInMainFrame,
                         OnDocumentAvailableInMainFrame)
-    IPC_MESSAGE_HANDLER(ViewHostMsg_DocumentOnLoadCompletedInMainFrame,
-                        OnDocumentOnLoadCompletedInMainFrame)
     IPC_MESSAGE_HANDLER(ViewHostMsg_ToggleFullscreen, OnToggleFullscreen)
     IPC_MESSAGE_HANDLER(ViewHostMsg_DidContentsPreferredSizeChange,
                         OnDidContentsPreferredSizeChange)
@@ -1237,11 +1235,6 @@ void RenderViewHostImpl::OnDidChangeLoadProgress(double load_progress) {
 
 void RenderViewHostImpl::OnDocumentAvailableInMainFrame() {
   delegate_->DocumentAvailableInMainFrame(this);
-}
-
-void RenderViewHostImpl::OnDocumentOnLoadCompletedInMainFrame(
-    int32 page_id) {
-  delegate_->DocumentOnLoadCompletedInMainFrame(this, page_id);
 }
 
 void RenderViewHostImpl::OnToggleFullscreen(bool enter_fullscreen) {
