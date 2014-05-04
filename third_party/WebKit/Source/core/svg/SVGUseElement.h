ec267a2037e2675bf4b69e9d08c59dbac760305a
rob.buis@samsung.com <rob.buis@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-04 04:47:35 +0000

    Use m_targetElementInstance in fewer places
    Use m_targetElementInstance in fewer places since SVGElementInstance
    is scheduled for removal.
    
    BUG=313438
    
    Review URL: https://codereview.chromium.org/269843002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173273 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGUseElement.h
+++ third_party/WebKit/Source/core/svg/SVGUseElement.h
@@ -85,7 +85,7 @@ private:
     bool hasCycleUseReferencing(SVGUseElement*, SVGElementInstance* targetInstance, SVGElement*& newTarget);
 
     // Shadow tree handling
-    void buildShadowTree(SVGElement* target, SVGElementInstance* targetInstance);
+    void buildShadowTree(SVGElement* target, SVGElementInstance* targetInstance, ShadowRoot* shadowTreeRootElement);
 
     void expandUseElementsInShadowTree(Node* element);
     void expandSymbolElementsInShadowTree(Node* element);
@@ -95,7 +95,7 @@ private:
     SVGElementInstance* instanceForShadowTreeElement(Node* element, SVGElementInstance* instance) const;
 
     void transferUseAttributesToReplacedElement(SVGElement* from, SVGElement* to) const;
-    void transferEventListenersToShadowTree(SVGElementInstance* target);
+    void transferEventListenersToShadowTree(SVGElement* shadowTreeTargetElement);
 
     RefPtr<SVGAnimatedLength> m_x;
     RefPtr<SVGAnimatedLength> m_y;
