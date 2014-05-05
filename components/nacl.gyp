98996c0f78284367704b51bfc0d2e7e0482c5d49
miu@chromium.org <miu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-05 22:21:14 +0000

    Revert 268280 "Pepper: Move manifest logic to components/nacl."
    Caused build breakage because static initializer was added to ppb_nacl_private_impl.cc.
    
    Candidate (line 96): + const int32_t kPNaClManifestId = std::numeric_limits<int32_t>::max();
    
    > Pepper: Move manifest logic to components/nacl.
    > 
    > The goal of the trusted plugin refactor is to move as much code as possible out of ppapi/native_client/src/trusted/plugin. This change moves all the manifest parsing and lookup logic out of the trusted plugin at once; it's hard to move piecemeal.
    > 
    > BUG=353592
    > R=bbudge@chromium.org, mallinath@chromium.org
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=268250
    > 
    > Review URL: https://codereview.chromium.org/264943003
    
    TBR=teravest@chromium.org
    
    Review URL: https://codereview.chromium.org/265393004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@268295 0039d316-1c4b-4281-b951-d872f2087c98

--- components/nacl.gyp
+++ components/nacl.gyp
@@ -160,8 +160,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'nacl/renderer/manifest_downloader.h',
             'nacl/renderer/manifest_service_channel.cc',
             'nacl/renderer/manifest_service_channel.h',
-            'nacl/renderer/json_manifest.cc',
-            'nacl/renderer/json_manifest.h',
             'nacl/renderer/nexe_load_manager.cc',
             'nacl/renderer/nexe_load_manager.h',
             'nacl/renderer/pnacl_translation_resource_host.cc',
@@ -178,7 +176,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           ],
           'dependencies': [
             '../content/content.gyp:content_renderer',
-            '../third_party/jsoncpp/jsoncpp.gyp:jsoncpp',
             '../third_party/WebKit/public/blink.gyp:blink',
             '../webkit/common/webkit_common.gyp:webkit_common',
           ],
--- ppapi/native_client/src/trusted/plugin/service_runtime.cc
+++ ppapi/native_client/src/trusted/plugin/service_runtime.cc
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/cpp/core.h"
 #include "ppapi/cpp/completion_callback.h"
 
+#include "ppapi/native_client/src/trusted/plugin/manifest.h"
 #include "ppapi/native_client/src/trusted/plugin/plugin.h"
 #include "ppapi/native_client/src/trusted/plugin/plugin_error.h"
 #include "ppapi/native_client/src/trusted/plugin/pnacl_resources.h"
@@ -198,13 +199,13 @@ void OpenManifestEntryResource::MaybeRunCallback(int32_t pp_error) {
 PluginReverseInterface::PluginReverseInterface(
     nacl::WeakRefAnchor* anchor,
     Plugin* plugin,
-    int32_t manifest_id,
+    const Manifest* manifest,
     ServiceRuntime* service_runtime,
     pp::CompletionCallback init_done_cb,
     pp::CompletionCallback crash_cb)
       : anchor_(anchor),
         plugin_(plugin),
-        manifest_id_(manifest_id),
+        manifest_(manifest),
         service_runtime_(service_runtime),
         shutting_down_(false),
         init_done_cb_(init_done_cb),
@@ -347,13 +348,9 @@ void PluginReverseInterface::OpenManifestEntry_MainThreadContinuation(
 
   NaClLog(4, "Entered OpenManifestEntry_MainThreadContinuation\n");
 
-  PP_Var pp_mapped_url;
+  std::string mapped_url;
   PP_PNaClOptions pnacl_options = {PP_FALSE, PP_FALSE, 2};
-  if (!GetNaClInterface()->ManifestResolveKey(plugin_->pp_instance(),
-                                              manifest_id_,
-                                              p->url.c_str(),
-                                              &pp_mapped_url,
-                                              &pnacl_options)) {
+  if (!manifest_->ResolveKey(p->url, &mapped_url, &pnacl_options)) {
     NaClLog(4, "OpenManifestEntry_MainThreadContinuation: ResolveKey failed\n");
     // Failed, and error_info has the details on what happened.  Wake
     // up requesting thread -- we are done.
@@ -366,7 +363,6 @@ void PluginReverseInterface::OpenManifestEntry_MainThreadContinuation(
     p->MaybeRunCallback(PP_OK);
     return;
   }
-  nacl::string mapped_url = pp::Var(pp_mapped_url).AsString();
   NaClLog(4,
           "OpenManifestEntry_MainThreadContinuation: "
           "ResolveKey: %s -> %s (pnacl_translate(%d))\n",
@@ -565,7 +561,7 @@ void PluginReverseInterface::AddTempQuotaManagedFile(
 }
 
 ServiceRuntime::ServiceRuntime(Plugin* plugin,
-                               int32_t manifest_id,
+                               const Manifest* manifest,
                                bool main_service_runtime,
                                bool uses_nonsfi_mode,
                                pp::CompletionCallback init_done_cb,
@@ -576,7 +572,7 @@ ServiceRuntime::ServiceRuntime(Plugin* plugin,
       reverse_service_(NULL),
       anchor_(new nacl::WeakRefAnchor()),
       rev_interface_(new PluginReverseInterface(anchor_, plugin,
-                                                manifest_id,
+                                                manifest,
                                                 this,
                                                 init_done_cb, crash_cb)),
       exit_status_(-1),
