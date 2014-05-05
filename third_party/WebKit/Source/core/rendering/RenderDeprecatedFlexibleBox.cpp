78c1c185c9fc98c403a6f14054c68b2d1d583cfe
dsinclair@chromium.org <dsinclair@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-05 23:28:37 +0000

    Mark when we may have been invalidated to early out on repaintTreeAfterLayout.
    This CL adds in a flag to mark if we may have been invalidated. If the flag is
    set we will propagate the flag up to our parents. This allows us to early
    out of the repaintTreeAfterLayout walk if at any point the node does not
    need to be invalidated.
    
    This improves the performance of the
    large-table-with-collapsed-borders-and-colspans-wider-than-table.html test
    by ~17% and removes some over-invalidations that were output.
    
    I visually verified the items in TestExpectations to make sure they matched.
    
    BUG=320139, 368739
    
    Review URL: https://codereview.chromium.org/264963004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173348 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderDeprecatedFlexibleBox.cpp
+++ third_party/WebKit/Source/core/rendering/RenderDeprecatedFlexibleBox.cpp
@@ -1000,6 +1000,9 @@ void RenderDeprecatedFlexibleBox::placeChild(RenderBox* child, const LayoutPoint
 {
     LayoutRect oldRect = child->frameRect();
 
+    // FIXME Investigate if this can be removed based on other flags. crbug.com/370010
+    child->setMayNeedInvalidation(true);
+
     // Place the child.
     child->setLocation(location);
 
