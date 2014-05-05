10caeb8692c4aa8e15020665b1827b8bc72502b2
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-05 16:20:34 +0000

    TimelinePanel: introduce TimelineModel.Record interface and
    and rename original TimelineModel.Record into TimelineModel.RecordImpl
    
    BUG=361045
    
    Review URL: https://codereview.chromium.org/269013004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173316 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/timeline/TimelineJSProfile.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TimelineJSProfile.js
@@ -47,7 +47,7 @@ WebInspector.TimelineJSProfileProcessor.mergeJSProfileIntoTimeline = function(ti
                 startTime: startTime
             };
             putOriginalChildrenUpToTime(startTime);
-            record = new WebInspector.TimelineModel.Record(timelineModel, event, record);
+            record = new WebInspector.TimelineModel.RecordImpl(timelineModel, event, record);
         }
 
         /**
