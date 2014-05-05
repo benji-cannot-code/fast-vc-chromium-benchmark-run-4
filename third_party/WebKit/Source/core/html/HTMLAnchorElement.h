10b9650e373272b5229b15b6ada85b4f43815e96
rob.buis@samsung.com <rob.buis@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-05 19:42:35 +0000

    Remove HTMLAnchorElement::create overload
    This overload is not needed since we always call the other create().
    
    Review URL: https://codereview.chromium.org/266923005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173335 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLAnchorElement.h
+++ third_party/WebKit/Source/core/html/HTMLAnchorElement.h
@@ -58,7 +58,6 @@ enum {
 class HTMLAnchorElement : public HTMLElement, public DOMURLUtils {
 public:
     static PassRefPtr<HTMLAnchorElement> create(Document&);
-    static PassRefPtr<HTMLAnchorElement> create(const QualifiedName&, Document&);
 
     virtual ~HTMLAnchorElement();
 
