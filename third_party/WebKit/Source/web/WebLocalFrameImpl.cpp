6d4374603dc72ec554fda084fc41d91b98afa1d6
manykey@yandex-team.ru <manykey@yandex-team.ru@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-05 08:49:54 +0000

    Fix search in hidden subframes.
    * Change way of determining if frame has visible content. Subframe lookups in style of ownerRenderer. 
    
    BUG=367141
    
    Review URL: https://codereview.chromium.org/256683005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173291 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebLocalFrameImpl.cpp
+++ third_party/WebKit/Source/web/WebLocalFrameImpl.cpp
@@ -595,6 +595,10 @@ WebSize WebLocalFrameImpl::contentsSize() const
 
 bool WebLocalFrameImpl::hasVisibleContent() const
 {
+    if (RenderPart* renderer = frame()->ownerRenderer()) {
+        if (renderer->style()->visibility() != VISIBLE)
+            return false;
+    }
     return frame()->view()->visibleWidth() > 0 && frame()->view()->visibleHeight() > 0;
 }
 
