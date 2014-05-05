49a708b2079d7823bf1c9902fb16fcd75e8025e6
haitaol@chromium.org <haitaol@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-05 21:42:28 +0000

    Add a checkbox on stop-sync dialog to allow user to choose whether to delete all their data(profile), too. If checked, user will be prompted again to confirm profile deletion.
    BUG=363272
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/235943011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@268276 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/browser_options.js
+++ chrome/browser/resources/options/browser_options.js
@@ -1289,6 +1289,15 @@ cr.define('options', function() {
              'There should always be a current profile, but none found.');
     },
 
+    /**
+     * Propmpts user to confirm deletion of the profile for this browser
+     * window.
+     * @private
+     */
+    deleteCurrentProfile_: function() {
+      ManageProfileOverlay.showDeleteDialog(this.getCurrentProfile_());
+    },
+
     setNativeThemeButtonEnabled_: function(enabled) {
       var button = $('themes-native-button');
       if (button)
@@ -1756,6 +1765,7 @@ cr.define('options', function() {
   //Forward public APIs to private implementations.
   [
     'addBluetoothDevice',
+    'deleteCurrentProfile',
     'enableCertificateButton',
     'enableFactoryResetSection',
     'getCurrentProfile',
