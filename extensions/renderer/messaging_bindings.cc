8e4f171cd15393b2619070c0ae750d58d22602cd
mek@chromium.org <mek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-06 23:23:13 +0000

    Revert of Mark forwarded user gestures as forwarded, and don't forward already forwarded user gestures. (https://codereview.chromium.org/256623008/)
    Reason for revert:
    The blink-side change for this had the unintended side effect of breaking websites,
    so reverting this while we figure out a proper fix.
    
    BUG=369963
    
    Original issue's description:
    > Mark forwarded user gestures as forwarded, and don't forward already forwarded user gestures.
    > 
    > BUG=354217
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=267057
    
    TBR=kalman@chromium.org
    NOTRY=true
    BUG=354217
    
    Review URL: https://codereview.chromium.org/261753019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@268635 0039d316-1c4b-4281-b951-d872f2087c98

--- extensions/renderer/messaging_bindings.cc
+++ extensions/renderer/messaging_bindings.cc
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/web/WebScopedUserGesture.h"
 #include "third_party/WebKit/public/web/WebScopedWindowFocusAllowedIndicator.h"
 #include "third_party/WebKit/public/web/WebUserGestureIndicator.h"
-#include "third_party/WebKit/public/web/WebUserGestureToken.h"
 #include "v8/include/v8.h"
 
 // Message passing API example (in a content script):
@@ -106,12 +105,6 @@ class ExtensionImpl : public ObjectBackedNativeHandler {
     dispatcher_->ClearPortData(port_id);
   }
 
-  bool ShouldForwardUserGesture() {
-    return blink::WebUserGestureIndicator::isProcessingUserGesture() &&
-           !blink::WebUserGestureIndicator::currentUserGestureToken()
-                .wasForwarded();
-  }
-
   // Sends a message along the given channel.
   void PostMessage(const v8::FunctionCallbackInfo<v8::Value>& args) {
     content::RenderView* renderview = context()->GetRenderView();
@@ -129,9 +122,9 @@ class ExtensionImpl : public ObjectBackedNativeHandler {
     }
 
     renderview->Send(new ExtensionHostMsg_PostMessage(
-        renderview->GetRoutingID(),
-        port_id,
-        Message(*v8::String::Utf8Value(args[1]), ShouldForwardUserGesture())));
+        renderview->GetRoutingID(), port_id,
+        Message(*v8::String::Utf8Value(args[1]),
+                blink::WebUserGestureIndicator::isProcessingUserGesture())));
   }
 
   // Forcefully disconnects a port.
@@ -362,7 +355,6 @@ void MessagingBindings::DeliverMessage(
   scoped_ptr<blink::WebScopedWindowFocusAllowedIndicator> allow_window_focus;
   if (message.user_gesture) {
     web_user_gesture.reset(new blink::WebScopedUserGesture);
-    blink::WebUserGestureIndicator::currentUserGestureToken().setForwarded();
     allow_window_focus.reset(new blink::WebScopedWindowFocusAllowedIndicator);
   }
 
