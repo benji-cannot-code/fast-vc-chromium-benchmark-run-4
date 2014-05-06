462950fbbf1ce124a88c9069ac2477c34d99866c
horo@chromium.org <horo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-06 05:13:14 +0000

    Add WorkerScriptLoaded message to support attaching DevTools while starting ServiceWorker.
    EmbeddedWorkerHostMsg_WorkerStarted is sent after the worker script has been evaluated.
    But if the break point is set in the top-level script, the worker thread is blocked and the message will not be sent.
    
    To let the browser process know that the script has been loaded and the worker thread has started, the renderer process need to send EmbeddedWorkerHostMsg_WorkerScriptLoaded.
    
    BUG=358657
    
    Review URL: https://codereview.chromium.org/268753004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@268431 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/service_worker/embedded_worker_context_client.cc
+++ content/renderer/service_worker/embedded_worker_context_client.cc
@@ -132,6 +132,8 @@ void EmbeddedWorkerContextClient::workerContextFailedToStart() {
   DCHECK(main_thread_proxy_->RunsTasksOnCurrentThread());
   DCHECK(!script_context_);
 
+  Send(new EmbeddedWorkerHostMsg_WorkerScriptLoadFailed(embedded_worker_id_));
+
   RenderThreadImpl::current()->embedded_worker_dispatcher()->
       WorkerContextDestroyed(embedded_worker_id_);
 }
@@ -146,6 +148,8 @@ void EmbeddedWorkerContextClient::workerContextStarted(
   DCHECK(!script_context_);
   g_worker_client_tls.Pointer()->Set(this);
   script_context_.reset(new ServiceWorkerScriptContext(this, proxy));
+
+  Send(new EmbeddedWorkerHostMsg_WorkerScriptLoaded(embedded_worker_id_));
 
   // Schedule a task to send back WorkerStarted asynchronously,
   // so that at the time we send it we can be sure that the worker
