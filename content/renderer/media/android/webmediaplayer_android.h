ccf3ae113f0484110bd304ae1cb5aebbcedae7b8
acolwell@chromium.org <acolwell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-06 04:40:44 +0000

    Add timelineOffset() support to WebMediaPlayerAndroid.
    Adding minimal timelineOffset() support to Android so Blink can report
    this information via HTMLMediaElement.getStartDate() for MSE playbacks.
    
    BUG=312699
    TESTS=Will be covered by the getStartDate() LayoutTests
    
    Review URL: https://codereview.chromium.org/263333004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@268426 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/android/webmediaplayer_android.h
+++ content/renderer/media/android/webmediaplayer_android.h
@@ -130,6 +130,7 @@ class WebMediaPlayerAndroid : public blink::WebMediaPlayer,
   virtual bool paused() const;
   virtual bool seeking() const;
   virtual double duration() const;
+  virtual double timelineOffset() const;
   virtual double currentTime() const;
 
   virtual bool didLoadingProgress() const;
