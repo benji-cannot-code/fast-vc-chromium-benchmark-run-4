b3554e1874686fc35538eb9aaf1200e01038cd86
rtoy@chromium.org <rtoy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-06 04:17:04 +0000

    Check for invalid chunk sizes in WAVEDecoder.
    When reading a chunk from a WAVE file, make sure the specified chunk
    size is sensible. That is, the chunk size isn't greater than the size
    of the file.
    
    BUG=369760
    
    Review URL: https://codereview.chromium.org/269113003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@268416 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/android/audio_decoder_android.cc
+++ content/renderer/media/android/audio_decoder_android.cc
@@ -166,6 +166,9 @@ class WAVEDecoder {
   // The number of bytes in the data portion of the chunk.
   size_t chunk_size_;
 
+  // The total number of bytes in the encoded data.
+  size_t data_size_;
+
   // The current position within the WAVE file.
   const uint8_t* buffer_;
 
@@ -184,7 +187,8 @@ class WAVEDecoder {
 };
 
 WAVEDecoder::WAVEDecoder(const uint8_t* encoded_data, size_t data_size)
-    : buffer_(encoded_data),
+    : data_size_(data_size),
+      buffer_(encoded_data),
       buffer_end_(encoded_data + 1),
       bytes_per_sample_(0),
       number_of_channels_(0),
@@ -247,6 +251,10 @@ bool WAVEDecoder::ReadChunkHeader() {
   // Adjust for padding
   if (chunk_size_ % 2)
     ++chunk_size_;
+
+  // Check for completely bogus chunk size.
+  if (chunk_size_ > data_size_)
+    return false;
 
   return true;
 }
