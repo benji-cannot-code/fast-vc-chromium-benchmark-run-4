f82eaeacbce4a0965bfbb1965f9b82e7a56449bf
maniscalco@chromium.org <maniscalco@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-06 21:35:14 +0000

    Generate AttachmentId's unique_id using base::GenerateGUID.
    Use GenerateGUID instead of generating a random number "by hand" with
    base::RandBytesAsString.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/266363002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@268616 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/internal_api/public/base/attachment_id_proto.cc
+++ sync/internal_api/public/base/attachment_id_proto.cc
@@ -5,15 +5,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "sync/internal_api/public/base/attachment_id_proto.h"
 
-#include "base/rand_util.h"
+#include "base/guid.h"
+#include "base/logging.h"
 
 namespace syncer {
 
 sync_pb::AttachmentIdProto CreateAttachmentIdProto() {
   // Only requirement here is that this id must be globally unique.
-  // TODO(maniscalco): Consider making this base64 encoded.
   sync_pb::AttachmentIdProto proto;
-  proto.set_unique_id(base::RandBytesAsString(16));
+  std::string guid = base::GenerateGUID();
+  DCHECK(!guid.empty());
+  proto.set_unique_id(guid);
   return proto;
 }
 
