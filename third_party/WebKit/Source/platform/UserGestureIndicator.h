1c9c5c60fdc35662cc06cd88fe62f606421cdde0
mek@chromium.org <mek@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-06 23:30:36 +0000

    Revert 172810 "Add a "forwarded" flag to usergesture tokens."
    This had the unintended side effect of breaking websites,
    so reverting this while we figure out a proper fix.
    
    BUG=369963
    
    > Add a "forwarded" flag to usergesture tokens.
    > 
    > Exposing this and using it in DOMTimer to check if a user gesture token
    > should be forwarded allows for other ways of forwarding user gestures
    > while still preventing a user gesture from being forwarded more than
    > once.
    > 
    > BUG=354217
    > 
    > Review URL: https://codereview.chromium.org/258783004
    
    TBR=mek@chromium.org
    
    Review URL: https://codereview.chromium.org/264333002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173445 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/UserGestureIndicator.h
+++ third_party/WebKit/Source/platform/UserGestureIndicator.h
@@ -49,8 +49,6 @@ public:
     virtual bool hasGestures() const = 0;
     virtual void setOutOfProcess() = 0;
     virtual void setJavascriptPrompt() = 0;
-    virtual bool wasForwarded() const = 0;
-    virtual void setForwarded() = 0;
 };
 
 class PLATFORM_EXPORT UserGestureIndicatorDisabler {
