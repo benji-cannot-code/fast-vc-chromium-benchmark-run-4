27db24154759b27ea41d2c6e4316940ac5dcce85
reveman@chromium.org <reveman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-06 01:03:16 +0000

    content: Prevent ScheduleDelayedWork from being called without a context current.
    A call to ScheduleDelayedWork can result in GL API calls. Hence, we
    must have a context current when calling this function.
    
    BUG=369836
    
    Review URL: https://codereview.chromium.org/263803010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@268352 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/gpu_command_buffer_stub.cc
+++ content/common/gpu/gpu_command_buffer_stub.cc
@@ -187,6 +187,7 @@ bool GpuCommandBufferStub::OnMessageReceived(const IPC::Message& message) {
   devtools_gpu_instrumentation::ScopedGpuTask task(channel());
   FastSetActiveURL(active_url_, active_url_hash_);
 
+  bool have_context = false;
   // Ensure the appropriate GL context is current before handling any IPC
   // messages directed at the command buffer. This ensures that the message
   // handler can assume that the context is current (not necessary for
@@ -198,6 +199,7 @@ bool GpuCommandBufferStub::OnMessageReceived(const IPC::Message& message) {
       message.type() != GpuCommandBufferMsg_SetLatencyInfo::ID) {
     if (!MakeCurrent())
       return false;
+    have_context = true;
   }
 
   // Always use IPC_MESSAGE_HANDLER_DELAY_REPLY for synchronous message handlers
@@ -250,8 +252,10 @@ bool GpuCommandBufferStub::OnMessageReceived(const IPC::Message& message) {
 
   CheckCompleteWaits();
 
-  // Ensure that any delayed work that was created will be handled.
-  ScheduleDelayedWork(kHandleMoreWorkPeriodMs);
+  if (have_context) {
+    // Ensure that any delayed work that was created will be handled.
+    ScheduleDelayedWork(kHandleMoreWorkPeriodMs);
+  }
 
   DCHECK(handled);
   return handled;
