821fa131dfa2198b233c060249f59655c3f8b9e6
sdefresne@chromium.org <sdefresne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-06 11:04:45 +0000

    Fix headers ordering
    BUG=364044
    TBR=blundell
    
    Review URL: https://codereview.chromium.org/270093004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@268485 0039d316-1c4b-4281-b951-d872f2087c98

--- components/bookmarks/core/browser/bookmark_utils.cc
+++ components/bookmarks/core/browser/bookmark_utils.cc
@@ -17,13 +17,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
 #include "components/bookmarks/core/browser/bookmark_model.h"
+#include "components/bookmarks/core/browser/scoped_group_bookmark_actions.h"
 #include "components/bookmarks/core/common/bookmark_pref_names.h"
 #include "components/query_parser/query_parser.h"
 #include "components/user_prefs/pref_registry_syncable.h"
 #include "net/base/net_util.h"
 #include "ui/base/models/tree_node_iterator.h"
 #include "url/gurl.h"
-#include "components/bookmarks/core/browser/scoped_group_bookmark_actions.h"
 
 using base::Time;
 
