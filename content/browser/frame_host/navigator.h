24e766e95f9b8f973383c199bf90c1ec57c8674f
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-06 01:14:44 +0000

    Clarification with comments.
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/262173008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@268358 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/frame_host/navigator.h
+++ content/browser/frame_host/navigator.h
@@ -38,6 +38,9 @@ class CONTENT_EXPORT Navigator : public base::RefCounted<Navigator> {
   // Returns the NavigationController associated with this Navigator.
   virtual NavigationController* GetController();
 
+
+  // Notifications coming from the RenderFrameHosts ----------------------------
+
   // The RenderFrameHostImpl started a provisional load.
   virtual void DidStartProvisionalLoad(RenderFrameHostImpl* render_frame_host,
                                        int parent_routing_id,
@@ -88,6 +91,9 @@ class CONTENT_EXPORT Navigator : public base::RefCounted<Navigator> {
   virtual bool NavigateToPendingEntry(
       RenderFrameHostImpl* render_frame_host,
       NavigationController::ReloadType reload_type);
+
+
+  // Navigation requests -------------------------------------------------------
 
   virtual base::TimeTicks GetCurrentLoadStart();
 
