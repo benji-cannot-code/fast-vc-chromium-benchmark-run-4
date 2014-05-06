0bd0e2e8115fd007a36c8741c24f86422f4d73d0
malch@chromium.org <malch@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-06 10:14:18 +0000

    Add snapshot command log.
    Review URL: https://codereview.chromium.org/260853004
    
    Review URL: https://codereview.chromium.org/260853004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173373 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/GraphicsContextRecorder.h
+++ third_party/WebKit/Source/platform/graphics/GraphicsContextRecorder.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef GraphicsContextRecorder_h
 #define GraphicsContextRecorder_h
 
+#include "platform/JSONValues.h"
 #include "platform/PlatformExport.h"
 #include "platform/graphics/GraphicsContext.h"
 #include "third_party/skia/include/core/SkPicture.h"
@@ -49,6 +50,7 @@ public:
 
     PassOwnPtr<ImageBuffer> replay(unsigned fromStep = 0, unsigned toStep = 0) const;
     PassOwnPtr<Timings> profile(unsigned minIterations, double minDuration) const;
+    PassRefPtr<JSONArray> snapshotCommandLog() const;
 
 private:
     friend class GraphicsContextRecorder;
