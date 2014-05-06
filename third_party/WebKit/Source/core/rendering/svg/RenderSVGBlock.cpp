6da1e45c530bf2bdd55a7d2105a30880182b37db
fs@opera.com <fs@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-06 18:53:38 +0000

    Add early-out to RenderSVG{Block,ModelObject}::repaintTreeAfterLayout
    Follow-up to https://codereview.chromium.org/264963004/ and
    https://codereview.chromium.org/260963012/.
    
    BUG=369486, 320139
    
    Review URL: https://codereview.chromium.org/263393003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173415 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/svg/RenderSVGBlock.cpp
+++ third_party/WebKit/Source/core/rendering/svg/RenderSVGBlock.cpp
@@ -115,6 +115,9 @@ bool RenderSVGBlock::nodeAtPoint(const HitTestRequest&, HitTestResult&, const Hi
 
 void RenderSVGBlock::repaintTreeAfterLayout()
 {
+    if (!shouldCheckForInvalidationAfterLayout())
+        return;
+
     LayoutStateDisabler layoutStateDisabler(*this);
     RenderBlockFlow::repaintTreeAfterLayout();
 }
