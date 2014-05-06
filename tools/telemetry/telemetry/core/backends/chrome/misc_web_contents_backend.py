270b4d15b8004e61293d79f335f303ede63b4db9
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-06 22:39:14 +0000

    Catch exceptions in oobe_exists.
    BUG=342884
    TEST=manual
    NOTRY=True
    
    Review URL: https://codereview.chromium.org/265413006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@268629 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/backends/chrome/misc_web_contents_backend.py
+++ tools/telemetry/telemetry/core/backends/chrome/misc_web_contents_backend.py
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from telemetry.core import exceptions
 from telemetry.core.backends.chrome import inspector_backend_list
 from telemetry.core.backends.chrome import oobe
 
@@ -19,7 +20,12 @@ class MiscWebContentsBackend(inspector_backend_list.InspectorBackendList):
   @property
   def oobe_exists(self):
     """Lightweight property to determine if the oobe webui is visible."""
-    return bool(len(self))
+    try:
+      return bool(len(self))
+    except (exceptions.BrowserGoneException,
+            exceptions.BrowserConnectionGoneException,
+            exceptions.TabCrashException):
+      return False
 
   def GetOobe(self):
     if not len(self):
