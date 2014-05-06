d2aa9f96ab80561de2223c02c768ef062877962b
enne@chromium.org <enne@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-06 22:38:10 +0000

    Always attach overflow controls to the right parent
    Overflow controls like scrollbars and scroll corners can either be
    parented by root transform layer (which optionally might exist) or the
    overflow controls host layer (if the root transform layer does not).
    
    Previously, overflow controls were parented to the first control parent
    that they were attached to, and if the root transform layer got created,
    they wouldn't be reparented to it.  This leads to raciness between
    adding scrollbars and adding the root transform layer.
    
    To fix this, always reparent these controls if the controlsParent has
    changed.
    
    BUG=361729
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=173263
    
    Review URL: https://codereview.chromium.org/266003004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173442 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/compositing/RenderLayerCompositor.cpp
+++ third_party/WebKit/Source/core/rendering/compositing/RenderLayerCompositor.cpp
@@ -1162,6 +1162,9 @@ void RenderLayerCompositor::updateOverflowControlsLayers()
     if (requiresHorizontalScrollbarLayer()) {
         if (!m_layerForHorizontalScrollbar) {
             m_layerForHorizontalScrollbar = GraphicsLayer::create(graphicsLayerFactory(), this);
+        }
+
+        if (m_layerForHorizontalScrollbar->parent() != controlsParent) {
             controlsParent->addChild(m_layerForHorizontalScrollbar.get());
 
             if (ScrollingCoordinator* scrollingCoordinator = this->scrollingCoordinator())
@@ -1178,6 +1181,9 @@ void RenderLayerCompositor::updateOverflowControlsLayers()
     if (requiresVerticalScrollbarLayer()) {
         if (!m_layerForVerticalScrollbar) {
             m_layerForVerticalScrollbar = GraphicsLayer::create(graphicsLayerFactory(), this);
+        }
+
+        if (m_layerForVerticalScrollbar->parent() != controlsParent) {
             controlsParent->addChild(m_layerForVerticalScrollbar.get());
 
             if (ScrollingCoordinator* scrollingCoordinator = this->scrollingCoordinator())
