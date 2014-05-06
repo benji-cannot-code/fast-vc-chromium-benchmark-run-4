00758869b6a6aad2cc55ed6501358497c280ec09
mef@chromium.org <mef@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-06 14:45:26 +0000

    Added WiFi security check to Windows implementation of WiFiService::StartConnect. If WiFi network is available, but is not open security, then it cannot be connected without profile, so return 'access denied' error.
    BUG=NONE
    
    Review URL: https://codereview.chromium.org/262293002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@268514 0039d316-1c4b-4281-b951-d872f2087c98

--- components/wifi/wifi_service.cc
+++ components/wifi/wifi_service.cc
@@ -12,6 +12,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace wifi {
 
+const char WiFiService::kErrorAssociateToNetwork[] = "Error.AssociateToNetwork";
+const char WiFiService::kErrorInvalidData[] = "Error.InvalidData";
+const char WiFiService::kErrorNotConfigured[] ="Error.NotConfigured";
+const char WiFiService::kErrorNotConnected[] = "Error.NotConnected";
+const char WiFiService::kErrorNotFound[] = "Error.NotFound";
+const char WiFiService::kErrorNotImplemented[] = "Error.NotImplemented";
+const char WiFiService::kErrorScanForNetworksWithName[] =
+    "Error.ScanForNetworksWithName";
+const char WiFiService::kErrorWiFiService[] = "Error.WiFiService";
+
 WiFiService::NetworkProperties::NetworkProperties()
     : connection_state(onc::connection_state::kNotConnected),
       security(onc::wifi::kNone),
