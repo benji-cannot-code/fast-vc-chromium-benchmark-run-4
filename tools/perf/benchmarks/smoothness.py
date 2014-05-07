110b93b42d5e5e33a40ca3dfa6d086982af0cfef
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-07 01:44:42 +0000

    [Telemetry] Disable crashy GPU rasterization tests on android.
    BUG=363783
    TBR=dtu@chromium.org
    NOTRY=True
    
    Review URL: https://codereview.chromium.org/264373002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@268655 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/benchmarks/smoothness.py
+++ tools/perf/benchmarks/smoothness.py
@@ -67,6 +67,7 @@ class SmoothnessFastPathKeySilkCases(test.Test):
     silk_flags.CustomizeBrowserOptionsForFastPath(options)
 
 
+@test.Disabled('android')  # crbug.com/363783
 class SmoothnessGpuRasterizationTop25(test.Test):
   """Measures rendering statistics for the top 25 with GPU rasterization
   """
@@ -77,6 +78,7 @@ class SmoothnessGpuRasterizationTop25(test.Test):
     silk_flags.CustomizeBrowserOptionsForGpuRasterization(options)
 
 
+@test.Disabled('android')  # crbug.com/363783
 class SmoothnessGpuRasterizationKeyMobileSites(test.Test):
   """Measures rendering statistics for the key mobile sites with GPU
   rasterization
