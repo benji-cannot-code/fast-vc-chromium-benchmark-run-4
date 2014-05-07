0a40ed92b5b588be2e016896116f3a60b790cb95
pavely@chromium.org <pavely@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-07 15:48:19 +0000

    GCMNetworkChannel: Status of outgoing messages recorded incorrectly in UMA.
    When recording UMA histogram for outgoing message status I passed
    constant instead of actual parameter. Fixing it.
    
    R=zea@chromium.org
    
    Review URL: https://codereview.chromium.org/263083015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@268790 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/notifier/gcm_network_channel.cc
+++ sync/notifier/gcm_network_channel.cc
@@ -96,7 +96,7 @@ void RecordIncomingMessageStatus(IncomingMessageStatus status) {
 
 void RecordOutgoingMessageStatus(OutgoingMessageStatus status) {
   UMA_HISTOGRAM_ENUMERATION(kOutgoingMessageStatusHistogram,
-                            MESSAGE_DISCARDED,
+                            status,
                             OUTGOING_MESSAGE_STATUS_COUNT);
 }
 
