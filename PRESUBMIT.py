6eaa1c184e6330a8ef0b5d05b5079b09bebf618d
dpranke@chromium.org <dpranke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-07 14:26:01 +0000

    Add GN bots to default try set to match the CQ.
    R=brettw@chromium.org, phajdan.jr@chromium.org
    BUG=353690
    
    Review URL: https://codereview.chromium.org/263293004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@268748 0039d316-1c4b-4281-b951-d872f2087c98

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -1317,6 +1317,7 @@ def GetDefaultTryConfigs(bots=None):
       # TODO(maruel): An option would be to run 'sizes' but not count a failure
       # of this step as a try job failure.
       'android_aosp': ['compile'],
+      'android_chromium_gn_compile_rel': ['compile'],
       'android_clang_dbg': ['slave_steps'],
       'android_dbg': ['slave_steps'],
       'cros_x86': ['defaulttests'],
@@ -1339,6 +1340,7 @@ def GetDefaultTryConfigs(bots=None):
       'linux_chromium_chromeos_clang_dbg': ['defaulttests'],
       'linux_chromium_chromeos_rel': ['defaulttests'],
       'linux_chromium_compile_dbg': ['defaulttests'],
+      'linux_chromium_gn_rel': ['defaulttests'],
       'linux_chromium_rel': ['defaulttests'],
       'linux_chromium_clang_dbg': ['defaulttests'],
       'linux_gpu': ['defaulttests'],
@@ -1465,12 +1467,14 @@ def GetPreferredTryMasters(project, change):
     return GetDefaultTryConfigs(['ios_rel_device', 'ios_dbg_simulator'])
 
   builders = [
+      'android_chromium_gn_compile_rel',
       'android_clang_dbg',
       'android_dbg',
       'ios_dbg_simulator',
       'ios_rel_device',
       'linux_chromium_chromeos_rel',
       'linux_chromium_clang_dbg',
+      'linux_chromium_gn_rel',
       'linux_chromium_rel',
       'linux_gpu',
       'mac_chromium_compile_dbg',
