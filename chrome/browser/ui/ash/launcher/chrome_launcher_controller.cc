8747b3e0455c1d439d8f912575115d14a3faedc5
kevers@chromium.org <kevers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-07 14:46:43 +0000

    Reset on-screen keyboard on a change to the active user.
    Addresses issue when switching users in multi-profile mode. User could not type on the VK after switching profiles.  The patch forces the keyboard to reinitialize.
    
    BUG=362533
    
    Review URL: https://codereview.chromium.org/263903003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@268755 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/ash/launcher/chrome_launcher_controller.cc
+++ chrome/browser/ui/ash/launcher/chrome_launcher_controller.cc
@@ -85,6 +85,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window.h"
 #include "ui/aura/window_event_dispatcher.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/keyboard/keyboard_util.h"
 #include "ui/wm/core/window_animations.h"
 
 #if defined(OS_CHROMEOS)
@@ -1103,6 +1104,9 @@ void ChromeLauncherController::ActiveUserChanged(
   RestoreUnpinnedRunningApplicationOrder(user_email);
   // Inform the system tray of the change.
   ash::Shell::GetInstance()->system_tray_delegate()->ActiveUserWasChanged();
+  // Force on-screen keyboard to reset.
+  if (keyboard::IsKeyboardEnabled())
+    ash::Shell::GetInstance()->CreateKeyboard();
 }
 
 void ChromeLauncherController::AdditionalUserAddedToSession(Profile* profile) {
