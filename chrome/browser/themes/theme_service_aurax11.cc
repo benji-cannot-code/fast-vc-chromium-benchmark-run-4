0b934ff776119eddfff1e4826583870ae32f4bf8
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-07 13:53:42 +0000

    Linux Aura: Update Widget when the native theme instance in use changes.
    (like when switching between GTK+ and classic theme)
    
    BUG=347832
    
    Review URL: https://codereview.chromium.org/261753005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@268734 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/themes/theme_service_aurax11.cc
+++ chrome/browser/themes/theme_service_aurax11.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/themes/custom_theme_supplier.h"
 #include "chrome/common/pref_names.h"
 #include "ui/gfx/image/image.h"
+#include "ui/native_theme/native_theme_aura.h"
 #include "ui/views/linux_ui/linux_ui.h"
 
 namespace {
@@ -43,10 +44,15 @@ NativeThemeX11::NativeThemeX11(PrefService* pref_service)
 
 void NativeThemeX11::StartUsingTheme() {
   pref_service_->SetBoolean(prefs::kUsesSystemTheme, true);
+  // Have the former theme notify its observers of change.
+  ui::NativeThemeAura::instance()->NotifyObservers();
 }
 
 void NativeThemeX11::StopUsingTheme() {
   pref_service_->SetBoolean(prefs::kUsesSystemTheme, false);
+  // Have the former theme notify its observers of change.
+  if (linux_ui_)
+    linux_ui_->GetNativeTheme(NULL)->NotifyObservers();
 }
 
 bool NativeThemeX11::GetColor(int id, SkColor* color) const {
