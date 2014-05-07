47f1287495d33232b7bfa23308c823d6be90035e
caitkp@chromium.org <caitkp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-07 15:30:27 +0000

    Test fixes and clean up in preparation for moving DSE to Prefs.
    TBR= dbeam@chromium.org
    (TBR'ing for a pref name change in options UI). 
    BUG=365762
    
    Review URL: https://codereview.chromium.org/265393007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@268781 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/search_engines/search_provider_install_data.h
+++ chrome/browser/search_engines/search_provider_install_data.h
@@ -14,11 +14,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/scoped_vector.h"
 #include "base/memory/weak_ptr.h"
-#include "chrome/browser/webdata/web_data_service.h"
 
 class GURL;
+class Profile;
 class SearchHostToURLsMap;
 class TemplateURL;
+class TemplateURLService;
 
 namespace content {
 class RenderProcessHost;
@@ -28,7 +29,7 @@ class RenderProcessHost;
 // loading the data on demand (when CallWhenLoaded is called) and then throwing
 // away the results after the callbacks are done, so the results are always up
 // to date with what is in the database.
-class SearchProviderInstallData : public WebDataServiceConsumer {
+class SearchProviderInstallData {
  public:
   enum State {
     // The search provider is not installed.
@@ -62,13 +63,9 @@ class SearchProviderInstallData : public WebDataServiceConsumer {
   void OnGoogleURLChange(const std::string& google_base_url);
 
  private:
-  // WebDataServiceConsumer
-  // Notification that the keywords have been loaded.
-  // This is invoked from WebDataService, and should not be directly
-  // invoked.
-  virtual void OnWebDataServiceRequestDone(
-      WebDataService::Handle h,
-      const WDTypedResult* result) OVERRIDE;
+  // Receives a copy of the TemplateURLService's keywords on the IO thread.
+  void OnTemplateURLsLoaded(ScopedVector<TemplateURL> template_urls,
+                            TemplateURL* default_provider);
 
   // Stores information about the default search provider.
   void SetDefault(const TemplateURL* template_url);
@@ -81,14 +78,12 @@ class SearchProviderInstallData : public WebDataServiceConsumer {
   // install state has been loaded.
   void NotifyLoaded();
 
-  // The list of closures to call after the load has finished.
+  // The original data source. Only accessed on the UI thread.
+  TemplateURLService* template_url_service_;
+
+  // The list of closures to call after the load has finished. If empty, there
+  // is no pending load.
   std::vector<base::Closure> closure_queue_;
-
-  // Service used to store entries.
-  scoped_refptr<WebDataService> web_service_;
-
-  // If non-zero, we're waiting on a load.
-  WebDataService::Handle load_handle_;
 
   // Holds results of a load that was done using this class.
   scoped_ptr<SearchHostToURLsMap> provider_map_;
