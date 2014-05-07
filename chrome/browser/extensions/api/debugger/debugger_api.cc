3f875e92dbc9af2a902a5cffa2cedf1b6f25225d
limasdf@gmail.com <limasdf@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-07 13:54:15 +0000

    Remove NOTIFICATION_EXTENSION_UNLOADED_DEPRECATED from debugger api
    BUG=354458
    
    Review URL: https://codereview.chromium.org/267023004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@268735 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/debugger/debugger_api.cc
+++ chrome/browser/extensions/api/debugger/debugger_api.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_writer.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/singleton.h"
+#include "base/scoped_observer.h"
 #include "base/stl_util.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
@@ -44,6 +45,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/url_utils.h"
 #include "extensions/browser/event_router.h"
 #include "extensions/browser/extension_host.h"
+#include "extensions/browser/extension_registry.h"
+#include "extensions/browser/extension_registry_observer.h"
 #include "extensions/browser/extension_system.h"
 #include "extensions/common/error_utils.h"
 #include "extensions/common/extension.h"
@@ -67,11 +70,16 @@ namespace OnDetach = extensions::api::debugger::OnDetach;
 namespace OnEvent = extensions::api::debugger::OnEvent;
 namespace SendCommand = extensions::api::debugger::SendCommand;
 
+namespace extensions {
+class ExtensionRegistry;
+}
 
 // ExtensionDevToolsClientHost ------------------------------------------------
 
-class ExtensionDevToolsClientHost : public DevToolsClientHost,
-                                    public content::NotificationObserver {
+class ExtensionDevToolsClientHost
+    : public DevToolsClientHost,
+      public content::NotificationObserver,
+      public extensions::ExtensionRegistryObserver {
  public:
   ExtensionDevToolsClientHost(Profile* profile,
                               DevToolsAgentHost* agent_host,
@@ -104,6 +112,12 @@ class ExtensionDevToolsClientHost : public DevToolsClientHost,
                        const content::NotificationSource& source,
                        const content::NotificationDetails& details) OVERRIDE;
 
+  // extensions::ExtensionRegistryObserver implementation.
+  virtual void OnExtensionUnloaded(
+      content::BrowserContext* browser_context,
+      const extensions::Extension* extension,
+      extensions::UnloadedExtensionInfo::Reason reason) OVERRIDE;
+
   Profile* profile_;
   scoped_refptr<DevToolsAgentHost> agent_host_;
   std::string extension_id_;
@@ -115,6 +129,11 @@ class ExtensionDevToolsClientHost : public DevToolsClientHost,
   PendingRequests pending_requests_;
   infobars::InfoBar* infobar_;
   OnDetach::Reason detach_reason_;
+
+  // Listen to extension unloaded notification.
+  ScopedObserver<extensions::ExtensionRegistry,
+                 extensions::ExtensionRegistryObserver>
+      extension_registry_observer_;
 
   DISALLOW_COPY_AND_ASSIGN(ExtensionDevToolsClientHost);
 };
@@ -303,14 +322,16 @@ ExtensionDevToolsClientHost::ExtensionDevToolsClientHost(
       extension_id_(extension_id),
       last_request_id_(0),
       infobar_(infobar),
-      detach_reason_(OnDetach::REASON_TARGET_CLOSED) {
+      detach_reason_(OnDetach::REASON_TARGET_CLOSED),
+      extension_registry_observer_(this) {
   CopyDebuggee(&debuggee_, debuggee);
 
   AttachedClientHosts::GetInstance()->Add(this);
 
-  // Detach from debugger when extension unloads.
-  registrar_.Add(this, chrome::NOTIFICATION_EXTENSION_UNLOADED_DEPRECATED,
-                 content::Source<Profile>(profile_));
+  // ExtensionRegistryObserver listen extension unloaded and detach debugger
+  // from there.
+  extension_registry_observer_.Add(
+      extensions::ExtensionRegistry::Get(profile_));
 
   // RVH-based agents disconnect from their clients when the app is terminating
   // but shared worker-based agents do not.
@@ -399,24 +420,32 @@ void ExtensionDevToolsClientHost::SendDetachedEvent() {
       ->DispatchEventToExtension(extension_id_, event.Pass());
 }
 
+void ExtensionDevToolsClientHost::OnExtensionUnloaded(
+    content::BrowserContext* browser_context,
+    const extensions::Extension* extension,
+    extensions::UnloadedExtensionInfo::Reason reason) {
+  if (extension->id() == extension_id_)
+    Close();
+}
+
 void ExtensionDevToolsClientHost::Observe(
     int type,
     const content::NotificationSource& source,
     const content::NotificationDetails& details) {
-  if (type == chrome::NOTIFICATION_EXTENSION_UNLOADED_DEPRECATED) {
-    if (content::Details<extensions::UnloadedExtensionInfo>(details)->
-        extension->id() == extension_id_)
+  switch (type) {
+    case chrome::NOTIFICATION_APP_TERMINATING:
       Close();
-  } else if (type == chrome::NOTIFICATION_APP_TERMINATING) {
-    Close();
-  } else {
-    DCHECK_EQ(chrome::NOTIFICATION_TAB_CONTENTS_INFOBAR_REMOVED, type);
-    if (content::Details<infobars::InfoBar::RemovedDetails>(details)->first ==
-        infobar_) {
-      infobar_ = NULL;
-      SendDetachedEvent();
-      Close();
-    }
+      break;
+    case chrome::NOTIFICATION_TAB_CONTENTS_INFOBAR_REMOVED:
+      if (content::Details<infobars::InfoBar::RemovedDetails>(details)->first ==
+          infobar_) {
+        infobar_ = NULL;
+        SendDetachedEvent();
+        Close();
+      }
+      break;
+    default:
+      NOTREACHED();
   }
 }
 
@@ -698,7 +727,7 @@ base::Value* SerializeTarget(const DevToolsTargetImpl& target) {
   return dictionary;
 }
 
-} // namespace
+}  // namespace
 
 DebuggerGetTargetsFunction::DebuggerGetTargetsFunction() {
 }
