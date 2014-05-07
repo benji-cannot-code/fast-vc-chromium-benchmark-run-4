2e484b15a509f5d94b66112cc15022705ea96872
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-07 15:59:02 +0000

    SimpleMessageBoxViews: UI message loop does not need to be running on Linux.
    Fixes ProcessSingletonDialogLinux not showing a dialog.
    
    BUG=367048
    TEST=run "mkdir /tmp/locktest && ln -s fakehost-1234 /tmp/locktest/SingletonLock && chrome --user-data-dir=/tmp/locktest". A dialog should appear allowing you to quit or unlock the profile. If you click quit, /tmp/locktest/SingletonLock file should remain untouched. If you click unlock, chrome should launch normally.
    
    Review URL: https://codereview.chromium.org/261753015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@268793 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/simple_message_box_views.cc
+++ chrome/browser/ui/views/simple_message_box_views.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/generated_resources.h"
 #include "ui/aura/window.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/native_widget_types.h"
 #include "ui/views/controls/message_box_view.h"
 #include "ui/views/widget/widget.h"
@@ -196,21 +197,26 @@ MessageBoxResult ShowMessageBoxImpl(gfx::NativeWindow parent,
                                     MessageBoxType type,
                                     const base::string16& yes_text,
                                     const base::string16& no_text) {
-  // Views dialogs cannot be shown outside the UI thread message loop.
+  // Views dialogs cannot be shown outside the UI thread message loop or if the
+  // ResourceBundle is not initialized yet.
   // Fallback to logging with a default response or a Windows MessageBox.
-  if (!base::MessageLoopForUI::IsCurrent() ||
-      !base::MessageLoopForUI::current()->is_running()) {
 #if defined(OS_WIN)
+  if (!base::MessageLoopForUI::IsCurrent() ||
+      !base::MessageLoopForUI::current()->is_running() ||
+      !ResourceBundle::HasSharedInstance()) {
     int result = ui::MessageBox(views::HWNDForNativeWindow(parent), message,
                                 title, GetMessageBoxFlagsFromType(type));
     return (result == IDYES || result == IDOK) ?
         MESSAGE_BOX_RESULT_YES : MESSAGE_BOX_RESULT_NO;
+  }
 #else
+  if (!base::MessageLoopForUI::IsCurrent() ||
+      !ResourceBundle::HasSharedInstance()) {
     LOG(ERROR) << "Unable to show a dialog outside the UI thread message loop: "
                << title << " - " << message;
     return MESSAGE_BOX_RESULT_NO;
-#endif
   }
+#endif
 
   MessageBoxResult result = MESSAGE_BOX_RESULT_NO;
   SimpleMessageBoxViews* dialog = new SimpleMessageBoxViews(
