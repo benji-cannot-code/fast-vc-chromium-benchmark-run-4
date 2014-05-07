36b166412224d0cb364bc4d84c42e6cf2ec16bdb
jkarlin@chromium.org <jkarlin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-07 19:32:02 +0000

    Disable prefetch on WiFi.  Eventually we'll tie this to a new Chrome preference to predict network actions when on cellular connections.
    BUG=370455
    
    Review URL: https://codereview.chromium.org/267053008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@268895 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prefetch/prefetch.cc
+++ chrome/browser/prefetch/prefetch.cc
@@ -11,11 +11,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "chrome/browser/prefetch/prefetch_field_trial.h"
 #include "chrome/browser/profiles/profile_io_data.h"
+#include "net/base/network_change_notifier.h"
 
 namespace prefetch {
 
 bool IsPrefetchEnabled(content::ResourceContext* resource_context) {
   DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::IO));
+
+  // TODO(jkarlin): Eventually tie this to a new Chrome preference to predict
+  // network actions when on cellular connections.  See crbug.com/370454.
+  if (net::NetworkChangeNotifier::IsConnectionCellular(
+          net::NetworkChangeNotifier::GetConnectionType()))
+    return false;
 
   ProfileIOData* io_data = ProfileIOData::FromResourceContext(resource_context);
   if (io_data != NULL && io_data->network_prediction_enabled()->GetValue())
