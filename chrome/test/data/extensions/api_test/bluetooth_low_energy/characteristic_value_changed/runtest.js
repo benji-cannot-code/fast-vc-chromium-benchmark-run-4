48fb46e320b3f436205b4597fbf7df5bfeef63b9
armansito@chromium.org <armansito@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-07 14:36:56 +0000

    chrome.bluetoothLowEnergy: Implement the onCharacteristicValueChanged event.
    This CL implements the onCharacteristicValueChanged event for the
    bluetoothLowEnergy API.
    
    BUG=265663
    TEST=browser_tests --gtest_filter=BluetoothLowEnergyApiTest.*
    
    Review URL: https://codereview.chromium.org/265293007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@268752 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/api_test/bluetooth_low_energy/characteristic_value_changed/runtest.js
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function testCharacteristicValueChanged() {
+  chrome.test.assertEq(2, Object.keys(changedChrcs).length);
+
+  chrome.test.assertEq(charId0, changedChrcs[charId0].instanceId);
+  chrome.test.assertEq(charId2, changedChrcs[charId2].instanceId);
+
+  chrome.test.succeed();
+}
+
+var charId0 = 'char_id0';
+var charId2 = 'char_id2';
+
+var changedChrcs = {}
+
+chrome.bluetoothLowEnergy.onCharacteristicValueChanged.addListener(
+    function (chrc) {
+      changedChrcs[chrc.instanceId] = chrc;
+});
+
+chrome.test.sendMessage('ready', function (message) {
+  chrome.test.runTests([testCharacteristicValueChanged]);
+});
