1a6b19c46b9f80b5df194ab756ce8a7f114fd24c
jiayl@chromium.org <jiayl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-07 23:05:12 +0000

    Adds StartRtpDump and StopRtpDump methods to webrtcLoggingPrivate API. It's only a stub for now.
    Design doc: https://docs.google.com/a/google.com/document/d/1qnzq1JABVObATtwGcstmmglXbOtaNZawxEjtNQCWwq8/edit?disco=AAAAAJYJFCI#heading=h.hom4iccr2c3o
    
    BUG=363459
    
    Review URL: https://codereview.chromium.org/267623003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@268988 0039d316-1c4b-4281-b951-d872f2087c98

--- extensions/browser/extension_function_histogram_value.h
+++ extensions/browser/extension_function_histogram_value.h
@@ -814,6 +814,8 @@ enum HistogramValue {
   BROWSER_OPENTAB,
   MANAGEMENT_CREATEAPPSHORTCUT,
   WEBVIEW_SHOWCONTEXTMENU,
+  WEBRTCLOGGINGPRIVATE_STARTRTPDUMP,
+  WEBRTCLOGGINGPRIVATE_STOPRTPDUMP,
   // Last entry: Add new entries above and ensure to update
   // tools/metrics/histograms/histograms/histograms.xml.
   ENUM_BOUNDARY
