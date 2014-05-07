470b905cc7f0c88d4acf5669d864128708190732
kinuko@chromium.org <kinuko@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-07 08:26:39 +0000

    Add blink-side plumbing for ServiceWorker -> Document postMessage
    - Initialize NavigatorServiceWorker and ServiceWorkerContainer earlier, right after a document is available (for Documents)
    - Add public interfaces:
    -- WebServiceWorkerContextClient.postMessageToClient()
    -- WebServiceWorkerProviderClient.dispatchMessageEvent()
    
    This is the first patch for postMessage plumbing: 
    1/3: THIS PATCH
    2/3: https://codereview.chromium.org/246023007/ (chromium) 
    3/3: https://codereview.chromium.org/264233003/ (blink, adds Layout test)
    
    BUG=366063
    TEST=to be added after chromium-side change is landed
    
    Review URL: https://codereview.chromium.org/263143004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173495 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/serviceworkers/NavigatorServiceWorker.h
+++ third_party/WebKit/Source/modules/serviceworkers/NavigatorServiceWorker.h
@@ -6,13 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NavigatorServiceWorker_h
 #define NavigatorServiceWorker_h
 
-#include "bindings/v8/ScriptPromise.h"
 #include "core/frame/Navigator.h"
 #include "platform/Supplementable.h"
 #include "platform/heap/Handle.h"
 
 namespace WebCore {
 
+class Document;
 class Navigator;
 class ServiceWorkerContainer;
 
@@ -20,6 +20,7 @@ class NavigatorServiceWorker FINAL : public NoBaseWillBeGarbageCollectedFinalize
     WILL_BE_USING_GARBAGE_COLLECTED_MIXIN(NavigatorServiceWorker);
 public:
     virtual ~NavigatorServiceWorker();
+    static NavigatorServiceWorker* from(Document&);
     static NavigatorServiceWorker& from(Navigator&);
     static NavigatorServiceWorker* toNavigatorServiceWorker(Navigator&);
     static const char* supplementName();
