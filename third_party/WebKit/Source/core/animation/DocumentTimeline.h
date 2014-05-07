a9dafbd613b357cf54956e0bdaa81bbb319d56f3
dstockwell@chromium.org <dstockwell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-07 08:18:38 +0000

    Web Animations: Avoid outdating players when setting start times
    This avoids spurious on-demand updates.
    
    BUG=368841
    
    Review URL: https://codereview.chromium.org/273523003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173493 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/DocumentTimeline.h
+++ third_party/WebKit/Source/core/animation/DocumentTimeline.h
@@ -94,6 +94,7 @@ public:
 
     Document* document() { return m_document; }
     void detachFromDocument();
+    void wake();
 
 protected:
     DocumentTimeline(Document*, PassOwnPtr<PlatformTiming>);
@@ -105,8 +106,6 @@ private:
     // i.e. current, in effect, or had timing changed
     HashSet<RefPtr<AnimationPlayer> > m_playersNeedingUpdate;
     HashSet<AnimationPlayer*> m_players;
-
-    void wake();
 
     friend class SMILTimeContainer;
     static const double s_minimumDelay;
