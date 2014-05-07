fff3a90960f7ac25cd1b76e1761943f3f71a6ada
yiyaoliu@google.com <yiyaoliu@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-07 19:54:19 +0000

    Remove the learn more link in the sesion crashed bubble.
    BUG=293702
    
    Review URL: https://codereview.chromium.org/269773002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@268906 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/session_crashed_bubble_view.h
+++ chrome/browser/ui/views/session_crashed_bubble_view.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents_observer.h"
 #include "ui/views/bubble/bubble_delegate.h"
 #include "ui/views/controls/button/button.h"
-#include "ui/views/controls/link_listener.h"
 
 namespace views {
 class Checkbox;
@@ -34,7 +33,6 @@ class Browser;
 class SessionCrashedBubbleView
     : public views::BubbleDelegateView,
       public views::ButtonListener,
-      public views::LinkListener,
       public content::WebContentsObserver,
       public content::NotificationObserver,
       public TabStripModelObserver {
@@ -56,9 +54,6 @@ class SessionCrashedBubbleView
   // views::ButtonListener methods.
   virtual void ButtonPressed(views::Button* sender,
                              const ui::Event& event) OVERRIDE;
-
-  // views::LinkListener methods.
-  virtual void LinkClicked(views::Link* source, int event_floags) OVERRIDE;
 
   // content::WebContentsObserver methods.
   virtual void DidStartNavigationToPendingEntry(
