f848a3dcee43fb06a21ab9dc83bd02c7357babf1
yoav@yoav.ws <yoav@yoav.ws@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-07 23:26:06 +0000

    Add the picture element
    Add the DOM part of the picture element, without actually using it for anything useful just yet.
    
    BUG=368830
    
    Review URL: https://codereview.chromium.org/261823002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173575 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/Source/core/html/HTMLPictureElement.cpp
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "config.h"
+#include "core/html/HTMLPictureElement.h"
+
+#include "HTMLNames.h"
+
+namespace WebCore {
+
+using namespace HTMLNames;
+
+HTMLPictureElement::HTMLPictureElement(Document& document)
+    : HTMLElement(pictureTag, document)
+{
+    ScriptWrappable::init(this);
+}
+
+PassRefPtr<HTMLPictureElement> HTMLPictureElement::create(Document& document)
+{
+    return adoptRef(new HTMLPictureElement(document));
+}
+
+} // namespace
