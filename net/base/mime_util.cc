b1f09ff9239cbe28580303e5d591dbd2160f93ad
sudarshan.p@samsung.com <sudarshan.p@samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-08 23:51:03 +0000

    Support for png images served with "image/x-png" mime type
    The image/x-png is a non-standard mimetype, now replaced by image/png.
    Adding support to be backward compatible and also match the behavior
    of other browsers like Firefox.
    
    BUG=25293
    
    Review URL: https://codereview.chromium.org/264043018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@269105 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/mime_util.cc
+++ net/base/mime_util.cc
@@ -159,6 +159,7 @@ static const MimeInfo secondary_mappings[] = {
   { "image/tiff", "tiff,tif" },
   { "image/x-xbitmap", "xbm" },
   { "image/svg+xml", "svg,svgz" },
+  { "image/x-png", "png"},
   { "message/rfc822", "eml" },
   { "text/plain", "txt,text" },
   { "text/html", "ehtml" },
@@ -261,7 +262,8 @@ static const char* const supported_image_types[] = {
   "image/bmp",
   "image/vnd.microsoft.icon",    // ico
   "image/x-icon",    // ico
-  "image/x-xbitmap"  // xbm
+  "image/x-xbitmap",  // xbm
+  "image/x-png"
 };
 
 // A list of media types: http://en.wikipedia.org/wiki/Internet_media_type
