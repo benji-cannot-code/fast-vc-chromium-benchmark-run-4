6ed8672b0690d517e8bba52832b9a115821935ac
ch.dumez@samsung.com <ch.dumez@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-08 00:07:07 +0000

    Make HTMLAnchorElement.text getter behave according to specification
    Make HTMLAnchorElement.text getter behave according to specification:
    http://www.whatwg.org/specs/web-apps/current-work/multipage/text-level-semantics.html#dom-a-text
    
    According to the specification, the 'text' IDL attribute, on getting, must
    return the same value as the textContent IDL attribute on the element. However,
    Chromium was returning 'innerText' instead. Upon setting, Chromium was
    correctly updating the 'textContent' attribute though.
    
    The new behavior is consistent with Firefox 29 and IE11.
    
    This CL is based on arv's comment at:
    https://codereview.chromium.org/263353004/#msg2
    
    R=arv@chromium.org, tkent@chromium.org
    BUG=369950
    TEST=fast/dom/HTMLAnchorElement/get-text.html
    
    Review URL: https://codereview.chromium.org/272693002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173579 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLAnchorElement.h
+++ third_party/WebKit/Source/core/html/HTMLAnchorElement.h
@@ -72,9 +72,6 @@ public:
     virtual String input() const OVERRIDE FINAL;
     virtual void setInput(const String&) OVERRIDE FINAL;
 
-    String text();
-    void setText(const String&);
-
     bool isLiveLink() const;
 
     virtual bool willRespondToMouseClickEvents() OVERRIDE FINAL;
