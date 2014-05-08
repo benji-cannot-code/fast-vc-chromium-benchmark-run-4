3ab2dd86b309df92c6f2fd6665588e544ff42fd0
enne@chromium.org <enne@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-08 20:27:26 +0000

    Force composite editing/ tests
    BUG=361729
    
    Review URL: https://codereview.chromium.org/275603002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173673 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/base.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/base.py
@@ -1531,6 +1531,8 @@ class Port(object):
                               ['--force-compositing-mode']),
             PhysicalTestSuite('css',
                               ['--force-compositing-mode']),
+            PhysicalTestSuite('editing',
+                              ['--force-compositing-mode']),
             PhysicalTestSuite('fast',
                               ['--force-compositing-mode']),
             ]
