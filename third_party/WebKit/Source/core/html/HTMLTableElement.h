a34cdc733aea1a74181a5b6542d4c535c4859b8c
ch.dumez@samsung.com <ch.dumez@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-08 00:02:16 +0000

    HTMLTableElement.insertRow()'s argument default value should be -1
    HTMLTableElement.insertRow()'s argument default value should be -1, not
    0, as per the specification:
    http://www.whatwg.org/specs/web-apps/current-work/multipage/tabular-data.html#htmltableelement
    
    The new behavior matches the one of Firefox 29 and IE11.
    
    R=arv@chromium.org, tkent@chromium.org
    TEST=fast/dom/HTMLTableElement/insertRow-default-argument.html
    BUG=369803
    
    Review URL: https://codereview.chromium.org/270213007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173578 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLTableElement.h
+++ third_party/WebKit/Source/core/html/HTMLTableElement.h
@@ -57,6 +57,7 @@ public:
     PassRefPtrWillBeRawPtr<HTMLElement> createTBody();
     PassRefPtrWillBeRawPtr<HTMLElement> createCaption();
     void deleteCaption();
+    PassRefPtrWillBeRawPtr<HTMLElement> insertRow(ExceptionState&);
     PassRefPtrWillBeRawPtr<HTMLElement> insertRow(int index, ExceptionState&);
     void deleteRow(int index, ExceptionState&);
 
