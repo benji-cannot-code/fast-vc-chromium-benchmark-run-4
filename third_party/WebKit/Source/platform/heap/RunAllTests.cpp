ac5b44c7d80d7b137dbaecf179be3b70a41bee01
wibling@chromium.org <wibling@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-08 13:23:05 +0000

    [oilpan]: Make parking threads for GC timeout in the case parking exceeds 100 MS.
    This is to avoid issues where a GC is hanging forever waiting for a thread to reach a safepoint. This should not happen, but in the case it does we would rather abandon the GC and attempt it again later.
    
    R=ager@chromium.org, erik.corry@gmail.com, haraken@chromium.org, oilpan-reviews@chromium.org, tkent@chromium.org, vegorov@chromium.org, zerny@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/260723003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173646 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/heap/RunAllTests.cpp
+++ third_party/WebKit/Source/platform/heap/RunAllTests.cpp
@@ -37,11 +37,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/MainThread.h"
 #include "wtf/WTF.h"
 #include <base/test/test_suite.h>
+#include <base/time/time.h>
 #include <string.h>
 
 static double CurrentTime()
 {
-    return 0.0;
+    return base::Time::Now().ToDoubleT();
 }
 
 static void AlwaysZeroNumberSource(unsigned char* buf, size_t len)
