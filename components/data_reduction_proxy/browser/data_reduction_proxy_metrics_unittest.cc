c3e5303d6e1846bfc03b3f20c97697bc8d9c927d
bengr@chromium.org <bengr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-08 04:12:04 +0000

    Collect data reduction proxy UMA on all platforms
    UMA for the data reduction proxy was gated behind preprocessor
    directives that resticted the logic to Android and iOS. This
    change opens UMA calculations and collection up to all platforms.
    
    BUG=369619
    
    Review URL: https://codereview.chromium.org/266033002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@269026 0039d316-1c4b-4281-b951-d872f2087c98

--- components/data_reduction_proxy/browser/data_reduction_proxy_metrics_unittest.cc
+++ components/data_reduction_proxy/browser/data_reduction_proxy_metrics_unittest.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-#if defined(OS_ANDROID) || defined(OS_IOS)
 const size_t kNumDaysInHistory = 60;
 
 int64 GetListPrefInt64Value(
@@ -28,8 +27,6 @@ int64 GetListPrefInt64Value(
   EXPECT_TRUE(base::StringToInt64(string_value, &value));
   return value;
 }
-
-#endif  // defined(OS_ANDROID) || defined(OS_IOS)
 
 }  // namespace
 
@@ -47,7 +44,6 @@ class ChromeNetworkDataSavingMetricsTest : public testing::Test {
     registry->RegisterInt64Pref(
         data_reduction_proxy::prefs::kHttpOriginalContentLength, 0);
 
-#if defined(OS_ANDROID) || defined(OS_IOS)
     registry->RegisterListPref(data_reduction_proxy::prefs::
                                    kDailyHttpOriginalContentLength);
     registry->RegisterListPref(data_reduction_proxy::prefs::
@@ -79,7 +75,6 @@ class ChromeNetworkDataSavingMetricsTest : public testing::Test {
     registry->RegisterInt64Pref(
         data_reduction_proxy::prefs::
             kDailyHttpContentLengthLastUpdateDate, 0L);
-#endif  // defined(OS_ANDROID) || defined(OS_IOS)
   }
 
   TestingPrefServiceSimple pref_service_;
@@ -110,8 +105,6 @@ TEST_F(ChromeNetworkDataSavingMetricsTest, TotalLengths) {
             pref_service_.GetInt64(
                 data_reduction_proxy::prefs::kHttpOriginalContentLength));
 }
-
-#if defined(OS_ANDROID) || defined(OS_IOS)
 
 // The initial last update time used in test. There is no leap second a few
 // days around this time used in the test.
@@ -680,6 +673,5 @@ TEST_F(ChromeNetworkDailyDataSavingMetricsTest, BackwardTwoDays) {
       original, 1, received, 1,
       original, 1, received, 1);
 }
-#endif  // defined(OS_ANDROID) || defined(OS_IOS)
 
 }  // namespace data_reduction_proxy
