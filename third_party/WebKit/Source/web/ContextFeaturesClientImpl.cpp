4a87ad9c35db30106781b4654f3a5371d1603bc4
zerny@chromium.org <zerny@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-08 08:20:55 +0000

    Oilpan: Remove unnecessary trace methods in the supplement hierarchy.
    R=tkent@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/270213006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173617 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/ContextFeaturesClientImpl.cpp
+++ third_party/WebKit/Source/web/ContextFeaturesClientImpl.cpp
@@ -92,8 +92,6 @@ public:
 
     void validateAgainst(Document*);
 
-    virtual void trace(Visitor* visitor) OVERRIDE { DocumentSupplement::trace(visitor); }
-
 private:
     String m_domain;
     Entry m_entries[ContextFeatures::FeatureTypeSize];
