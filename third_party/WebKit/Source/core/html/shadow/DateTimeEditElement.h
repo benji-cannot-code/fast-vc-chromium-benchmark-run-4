f421e7ba19449ca2146033ab7e0e4f6180a6f40c
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-08 02:31:38 +0000

    Oilpan: Make DateTimeEditElement::m_editControlOwner and DateTimeFieldElement::m_fieldOwner strong references.
    We don't need to use WeakMember for them because owners never die before owned
    elements.
    In non-Oilpan, owners can die before owned elements because parentNode is not a
    strong reference.
    
    BUG=357163
    
    Review URL: https://codereview.chromium.org/275513003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173590 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/shadow/DateTimeEditElement.h
+++ third_party/WebKit/Source/core/html/shadow/DateTimeEditElement.h
@@ -152,7 +152,7 @@ private:
     virtual void fieldDidChangeValueByKeyboard() OVERRIDE;
 
     WillBeHeapVector<RawPtrWillBeMember<DateTimeFieldElement>, maximumNumberOfFields> m_fields;
-    RawPtrWillBeWeakMember<EditControlOwner> m_editControlOwner;
+    RawPtrWillBeMember<EditControlOwner> m_editControlOwner;
 };
 
 DEFINE_TYPE_CASTS(DateTimeEditElement, Element, element, element->isDateTimeEditElement(), element.isDateTimeEditElement());
