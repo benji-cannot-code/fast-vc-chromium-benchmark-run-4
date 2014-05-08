542eb18cd0f8024a44af252891ed6e847a01f461
morrita@chromium.org <morrita@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-08 21:02:43 +0000

    Remove setNeedsRecalcStyle() in Document::didLoadAllImports().
    r173248 added it but there is a performance concern.
    This change repalces it with a modest alternative that does same as what didRemoveAllPendingStylesheet() has been doing.
    
    This change also removed 500ms sleep from a test that was added at r173248.
    
    TEST=none
    R=ojan@chromium.org,esprehn@chromium.org
    BUG=367386
    
    Review URL: https://codereview.chromium.org/279463002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173680 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -2999,7 +2999,8 @@ void Document::didLoadAllImports()
 {
     if (!haveStylesheetsLoaded())
         return;
-    setNeedsStyleRecalc(SubtreeStyleChange);
+    if (!importLoader())
+        styleResolverMayHaveChanged();
     didLoadAllScriptBlockingResources();
 }
 
@@ -3007,11 +3008,11 @@ void Document::didRemoveAllPendingStylesheet()
 {
     m_needsNotifyRemoveAllPendingStylesheet = false;
 
-    styleResolverChanged(RecalcStyleDeferred, hasNodesWithPlaceholderStyle() ? FullStyleUpdate : AnalyzedStyleUpdate);
+    styleResolverMayHaveChanged();
 
+    // Only imports on master documents can trigger rendering.
     if (HTMLImportLoader* import = importLoader())
         import->didRemoveAllPendingStylesheet();
-
     if (!haveImportsLoaded())
         return;
     didLoadAllScriptBlockingResources();
@@ -3464,6 +3465,11 @@ void Document::styleResolverChanged(RecalcStyleTime updateTime, StyleResolverUpd
 
     if (updateTime == RecalcStyleImmediately)
         updateRenderTreeIfNeeded();
+}
+
+void Document::styleResolverMayHaveChanged()
+{
+    styleResolverChanged(RecalcStyleDeferred, hasNodesWithPlaceholderStyle() ? FullStyleUpdate : AnalyzedStyleUpdate);
 }
 
 void Document::setHoverNode(PassRefPtr<Node> newHoverNode)
