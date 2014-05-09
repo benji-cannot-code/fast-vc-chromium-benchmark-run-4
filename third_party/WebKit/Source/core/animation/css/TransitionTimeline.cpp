6dd1f0dc02542fcc4e3efaabeed618141d620a13
dstockwell@chromium.org <dstockwell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-09 11:43:06 +0000

    Revert of Web Animations: Timeline should not advance during task execution (https://codereview.chromium.org/275753002/)
    Reason for revert:
    Still failing Win7 debug:
    http://build.chromium.org/p/chromium.webkit/builders/WebKit%20Win7%20%28dbg%29/builds/5195
    
    Original issue's description:
    > Web Animations: Timeline should not advance during task execution
    > 
    > Previously the animation clock was only frozen during animation frame
    > callbacks. This meant that the timeline was able to advance during other
    > tasks.
    > 
    > This patch allows the clock to advance once per task, either to the
    > compositor supplied frame start time or to an approximation of the next
    > expected frame time.
    > 
    > http://dev.w3.org/fxtf/web-animations/#script-execution-and-live-updates-to-the-model
    > 
    > This is a reland of r173583[1], the previous patch hit added assertions
    > due to double rounding issues, the new approach avoids these. Also ensures
    > that other unit tests see a non-zero currentTime (seen in some webkit_unit_test
    > runs on Android).
    > 
    > [1] https://src.chromium.org/viewvc/blink?view=rev&revision=173583
    > BUG=367903
    > TBR=abarth
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=173729
    
    TBR=noel@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=367903
    
    Review URL: https://codereview.chromium.org/275863002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173744 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/css/TransitionTimeline.cpp
+++ third_party/WebKit/Source/core/animation/css/TransitionTimeline.cpp
@@ -46,6 +46,7 @@ TransitionTimeline::TransitionTimeline(Document* document, PassOwnPtr<PlatformTi
     : DocumentTimeline(document, timing)
 {
     setZeroTime(document->animationClock().currentTime());
+    document->animationClock().unfreeze();
 }
 
 } // namespace WebCore
--- third_party/WebKit/Source/web/WebKit.cpp
+++ third_party/WebKit/Source/web/WebKit.cpp
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/v8/V8Binding.h"
 #include "bindings/v8/V8Initializer.h"
 #include "core/Init.h"
-#include "core/animation/AnimationClock.h"
 #include "core/dom/Microtask.h"
 #include "core/frame/Settings.h"
 #include "core/page/Page.h"
@@ -68,11 +67,8 @@ namespace {
 
 class EndOfTaskRunner : public WebThread::TaskObserver {
 public:
-    virtual void willProcessTask() OVERRIDE
-    {
-        WebCore::AnimationClock::notifyTaskStart();
-    }
-    virtual void didProcessTask() OVERRIDE
+    virtual void willProcessTask() { }
+    virtual void didProcessTask()
     {
         WebCore::Microtask::performCheckpoint();
     }
