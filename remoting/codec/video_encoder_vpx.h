e230aec4f6ab441d7e616d9caa11eb90aab2b832
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-09 02:16:22 +0000

    Clean up common parts of VPX codec initialization.
    This includes some improvements & fixes:
    - VP9 is now initialized for multi-threaded encode.
    - Frame timestamp base is correctly set to milliseconds.
    - Frame timestamps are now set based on elapsed time.
    
    Review URL: https://codereview.chromium.org/265113002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@269151 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/codec/video_encoder_vpx.h
+++ remoting/codec/video_encoder_vpx.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define REMOTING_CODEC_VIDEO_ENCODER_VPX_H_
 
 #include "base/callback.h"
+#include "base/time/time.h"
 #include "remoting/codec/scoped_vpx_codec.h"
 #include "remoting/codec/video_encoder.h"
 
@@ -56,7 +57,7 @@ class VideoEncoderVpx : public VideoEncoder {
   scoped_ptr<uint8[]> active_map_;
   int active_map_width_;
   int active_map_height_;
-  int last_timestamp_;
+  base::TimeTicks timestamp_base_;
 
   // Buffer for storing the yuv image.
   scoped_ptr<uint8[]> yuv_image_;
