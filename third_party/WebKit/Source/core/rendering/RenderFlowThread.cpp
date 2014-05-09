4c07a76d84b015c270e5345cd19865d9ef36cf66
mstensho@opera.com <mstensho@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-09 19:06:37 +0000

    [New Multicolumn] Update sets' flow thread portion rectangles properly.
    logicalHeightOfAllFlowThreadContent() was wrong. This still doesn't matter
    much, but it will once we have support for multiple sets (column-span:all).
    
    Review URL: https://codereview.chromium.org/265293005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173769 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderFlowThread.cpp
+++ third_party/WebKit/Source/core/rendering/RenderFlowThread.cpp
@@ -129,8 +129,6 @@ void RenderFlowThread::layout()
 {
     m_pageLogicalSizeChanged = m_regionsInvalidated && everHadLayout();
 
-    validateRegions();
-
     CurrentRenderFlowThreadMaintainer currentFlowThreadSetter(this);
     RenderBlockFlow::layout();
 
