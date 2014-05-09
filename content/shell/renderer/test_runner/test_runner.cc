d160135b936c922c484440417b8a295de1e3671d
noel@chromium.org <noel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-09 14:20:13 +0000

    Add testRunner support for dynamic color profile layout tests
    Add testRunner JS API for layout tests to change the system color
    profile during a test by profile name and a non-optional callback
    that indicates profile change completion (done callback).
    
    The default test color profile is sRGB denoted 'sRGB'. Add a test
    color profile, denoted 'test', that rotates colors by 120 degrees
    to make it easy to see and test color changes.
    
    Here, plumb the JS API from testRunner.SetColorProfile through to
    the render_view_impl: implementation there is a TODO later.
    
    BUG=369787
    
    Review URL: https://codereview.chromium.org/266873002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@269281 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/renderer/test_runner/test_runner.cc
+++ content/shell/renderer/test_runner/test_runner.cc
@@ -229,6 +229,8 @@ class TestRunnerBindings : public gin::Wrappable<TestRunnerBindings> {
   void SetWindowIsKey(bool value);
   std::string PathToLocalResource(const std::string& path);
   void SetBackingScaleFactor(double value, v8::Handle<v8::Function> callback);
+  void SetColorProfile(const std::string& name,
+                       v8::Handle<v8::Function> callback);
   void SetPOSIXLocale(const std::string& locale);
   void SetMIDIAccessorResult(bool result);
   void SetMIDISysexPermission(bool value);
@@ -451,6 +453,8 @@ gin::ObjectTemplateBuilder TestRunnerBindings::GetObjectTemplateBuilder(
                  &TestRunnerBindings::PathToLocalResource)
       .SetMethod("setBackingScaleFactor",
                  &TestRunnerBindings::SetBackingScaleFactor)
+      .SetMethod("setColorProfile",
+                 &TestRunnerBindings::SetColorProfile)
       .SetMethod("setPOSIXLocale", &TestRunnerBindings::SetPOSIXLocale)
       .SetMethod("setMIDIAccessorResult",
                  &TestRunnerBindings::SetMIDIAccessorResult)
@@ -1147,6 +1151,12 @@ void TestRunnerBindings::SetBackingScaleFactor(
     runner_->SetBackingScaleFactor(value, callback);
 }
 
+void TestRunnerBindings::SetColorProfile(
+    const std::string& name, v8::Handle<v8::Function> callback) {
+  if (runner_)
+    runner_->SetColorProfile(name, callback);
+}
+
 void TestRunnerBindings::SetPOSIXLocale(const std::string& locale) {
   if (runner_)
     runner_->SetPOSIXLocale(locale);
@@ -1414,6 +1424,7 @@ void TestRunner::Reset() {
   if (delegate_) {
     // Reset the default quota for each origin to 5MB
     delegate_->setDatabaseQuota(5 * 1024 * 1024);
+    delegate_->setDeviceColorProfile("sRGB");
     delegate_->setDeviceScaleFactor(1);
     delegate_->setAcceptAllCookies(false);
     delegate_->setLocale("");
@@ -2494,6 +2505,12 @@ void TestRunner::SetBackingScaleFactor(double value,
                                        v8::Handle<v8::Function> callback) {
   delegate_->setDeviceScaleFactor(value);
   proxy_->discardBackingStore();
+  delegate_->postTask(new InvokeCallbackTask(this, callback));
+}
+
+void TestRunner::SetColorProfile(const std::string& name,
+                                 v8::Handle<v8::Function> callback) {
+  delegate_->setDeviceColorProfile(name);
   delegate_->postTask(new InvokeCallbackTask(this, callback));
 }
 
