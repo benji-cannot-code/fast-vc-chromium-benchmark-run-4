f3167c7d91fadb277b2ccb06190fc8e24fb83026
armansito@chromium.org <armansito@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-09 01:41:50 +0000

    chrome.bluetoothLowEnergy: Implement readCharacteristicValue.
    This CL implements the readCharacteristicValue function of the
    bluetoothLowEnergy API.
    
    BUG=265663
    TEST=browser_tests --gtest_filter=BluetoothLowEnergyApiTest.*
    
    Review URL: https://codereview.chromium.org/269023008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@269137 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/api_test/bluetooth_low_energy/read_characteristic_value/runtest.js
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function testReadCharacteristicValue() {
+  chrome.test.assertTrue(characteristic != null, '\'characteristic\' is null');
+  chrome.test.assertEq(charId, characteristic.instanceId);
+
+  chrome.test.succeed();
+}
+
+var readCharacteristicValue = chrome.bluetoothLowEnergy.readCharacteristicValue;
+var charId = 'char_id0';
+var badCharId = 'char_id1';
+
+var characteristic = null;
+
+// 1. Unknown characteristic instanceId.
+readCharacteristicValue(badCharId, function (result) {
+  if (result || !chrome.runtime.lastError) {
+    chrome.test.fail('\'badCharId\' did not cause failure');
+  }
+
+  // 2. Known characteristic instanceId, but call failure.
+  readCharacteristicValue(charId, function (result) {
+    if (result || !chrome.runtime.lastError) {
+      chrome.test.fail('readCharacteristicValue should have failed');
+    }
+
+    // 3. Call should succeed.
+    readCharacteristicValue(charId, function (result) {
+      if (chrome.runtime.lastError) {
+        chrome.test.fail(chrome.runtime.lastError.message);
+      }
+
+      characteristic = result;
+
+      chrome.test.sendMessage('ready', function (message) {
+        chrome.test.runTests([testReadCharacteristicValue]);
+      });
+    });
+  });
+});
