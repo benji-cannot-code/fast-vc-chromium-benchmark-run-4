7efd37e47e53504005307c2bf14ea4dcd4ec0ba4
wjywbs@gmail.com <wjywbs@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-09 11:06:44 +0000

    Fix compile error if both inline and reference enums are used in a type.
    The ToString() overloading function of inline and reference enums
    will conflict because they exist in both global namespace and local
    classes. This is a workaround to generate EnumNameToString() and use
    it in ToValue() of the type. ToString() is still generated for
    compatibility because some other code still use it.
    
    R=kalman@chromium.org
    BUG=371042
    
    Review URL: https://codereview.chromium.org/270523005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@269220 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/json_schema_compiler/cc_generator.py
+++ tools/json_schema_compiler/cc_generator.py
@@ -498,7 +498,9 @@ class _Generator(object):
         vardot = '(%s).' % var
       return '%sDeepCopy()' % vardot
     elif underlying_type.property_type == PropertyType.ENUM:
-      return 'new base::StringValue(ToString(%s))' % var
+      classname = cpp_util.Classname(schema_util.StripNamespace(
+          underlying_type.name))
+      return 'new base::StringValue(%sToString(%s))' % (classname, var)
     elif underlying_type.property_type == PropertyType.BINARY:
       if is_ptr:
         vardot = var + '->'
@@ -887,8 +889,8 @@ class _Generator(object):
       c.Append('// static')
     maybe_namespace = '' if cpp_namespace is None else '%s::' % cpp_namespace
 
-    c.Sblock('std::string %sToString(%s enum_param) {' %
-                 (maybe_namespace, classname))
+    c.Sblock('std::string %s%sToString(%s enum_param) {' %
+                 (maybe_namespace, classname, classname))
     c.Sblock('switch (enum_param) {')
     for enum_value in self._type_helper.FollowRef(type_).enum_values:
       (c.Append('case %s: ' % self._type_helper.GetEnumValue(type_, enum_value))
@@ -900,6 +902,12 @@ class _Generator(object):
       .Append('return "";')
       .Eblock('}')
     )
+
+    (c.Append()
+      .Sblock('std::string %sToString(%s enum_param) {' %
+                  (maybe_namespace, classname))
+      .Append('return %s%sToString(enum_param);' % (maybe_namespace, classname))
+      .Eblock('}'))
     return c
 
   def _GenerateEnumFromString(self, cpp_namespace, type_):
