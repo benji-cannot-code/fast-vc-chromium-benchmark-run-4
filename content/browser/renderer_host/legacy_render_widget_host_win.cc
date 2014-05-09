24f59fb3c9c6d1f712b64948e8a76d067db902f2
dmazzoni@chromium.org <dmazzoni@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-09 22:02:33 +0000

    Add back the ALERT trick to detect screen readers.
    This fires an EVENT_OBJECT_ALERT notification on the legacy hwnd
    with a custom id, and if we get a response we enable full web
    accessibility.
    
    This code used to be in render_widget_host_view_win. The replacement
    code was not working reliably for unknown reasons, and other accessibility
    clients were relying on the previous trick - see bugs.
    
    BUG=319523,342319
    NOTRY=true
    R=ananta@chromium.org, creis@chromium.org
    
    Review URL: https://codereview.chromium.org/272803003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@269429 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/legacy_render_widget_host_win.cc
+++ content/browser/renderer_host/legacy_render_widget_host_win.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/windows_version.h"
 #include "content/browser/accessibility/browser_accessibility_manager_win.h"
 #include "content/browser/accessibility/browser_accessibility_win.h"
+#include "content/public/browser/browser_accessibility_state.h"
 #include "content/public/common/content_switches.h"
 #include "ui/base/touch/touch_enabled.h"
 #include "ui/base/view_prop.h"
@@ -18,6 +19,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/rect.h"
 
 namespace content {
+
+// A custom MSAA object id used to determine if a screen reader or some
+// other client is listening on MSAA events - if so, we enable full web
+// accessibility support.
+const int kIdScreenReaderHoneyPot = 1;
 
 LegacyRenderWidgetHostHWND::~LegacyRenderWidgetHostHWND() {
   ::DestroyWindow(hwnd());
@@ -101,6 +107,14 @@ bool LegacyRenderWidgetHostHWND::Init() {
       hwnd(), OBJID_WINDOW, IID_IAccessible,
       reinterpret_cast<void **>(window_accessible_.Receive()));
   DCHECK(SUCCEEDED(hr));
+
+  if (!BrowserAccessibilityState::GetInstance()->IsAccessibleBrowser()) {
+    // Attempt to detect screen readers or other clients who want full
+    // accessibility support, by seeing if they respond to this event.
+    NotifyWinEvent(EVENT_SYSTEM_ALERT, hwnd(), kIdScreenReaderHoneyPot,
+                   CHILDID_SELF);
+  }
+
   return !!SUCCEEDED(hr);
 }
 
@@ -120,6 +134,13 @@ LRESULT LegacyRenderWidgetHostHWND::OnEraseBkGnd(UINT message,
 LRESULT LegacyRenderWidgetHostHWND::OnGetObject(UINT message,
                                                 WPARAM w_param,
                                                 LPARAM l_param) {
+  if (kIdScreenReaderHoneyPot == l_param) {
+    // When an MSAA client has responded to our fake event on this id,
+    // enable screen reader support.
+    BrowserAccessibilityState::GetInstance()->OnScreenReaderDetected();
+    return static_cast<LRESULT>(0L);
+  }
+
   if (OBJID_CLIENT != l_param || !manager_)
     return static_cast<LRESULT>(0L);
 
