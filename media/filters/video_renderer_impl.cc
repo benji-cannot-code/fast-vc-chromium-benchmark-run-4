23ce00925fa7d7895c467affa984fa974dab53fd
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-09 18:03:58 +0000

    Replace VideoRendererImpl's kEnded state with a bool.
    This is consistent with how AudioRendererImpl handles firing the ended
    callback, which keeps the renderer in the "playing" state but remaining
    idle.
    
    BUG=110814
    
    Review URL: https://codereview.chromium.org/272783002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@269372 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/video_renderer_impl.cc
+++ media/filters/video_renderer_impl.cc
@@ -29,6 +29,7 @@ VideoRendererImpl::VideoRendererImpl(
       video_frame_stream_(task_runner, decoders.Pass(), set_decryptor_ready_cb),
       low_delay_(false),
       received_end_of_stream_(false),
+      rendered_end_of_stream_(false),
       frame_available_(&lock_),
       state_(kUninitialized),
       thread_(),
@@ -76,6 +77,7 @@ void VideoRendererImpl::Flush(const base::Closure& callback) {
   // stream and needs to drain it before flushing it.
   ready_frames_.clear();
   received_end_of_stream_ = false;
+  rendered_end_of_stream_ = false;
   video_frame_stream_.Reset(
       base::Bind(&VideoRendererImpl::OnVideoFrameStreamResetDone,
                  weak_factory_.GetWeakPtr()));
@@ -254,12 +256,9 @@ void VideoRendererImpl::ThreadMain() {
 
     // Remain idle until we have the next frame ready for rendering.
     if (ready_frames_.empty()) {
-      if (received_end_of_stream_) {
-        state_ = kEnded;
+      if (received_end_of_stream_ && !rendered_end_of_stream_) {
+        rendered_end_of_stream_ = true;
         ended_cb_.Run();
-
-        // No need to sleep here as we idle when |state_ != kPlaying|.
-        continue;
       }
 
       UpdateStatsAndWait_Locked(kIdleTimeDelta);
@@ -469,7 +468,6 @@ void VideoRendererImpl::AttemptRead_Locked() {
     case kInitializing:
     case kFlushing:
     case kFlushed:
-    case kEnded:
     case kStopped:
     case kError:
       return;
@@ -485,6 +483,7 @@ void VideoRendererImpl::OnVideoFrameStreamResetDone() {
   DCHECK(!pending_read_);
   DCHECK(ready_frames_.empty());
   DCHECK(!received_end_of_stream_);
+  DCHECK(!rendered_end_of_stream_);
 
   state_ = kFlushed;
   last_timestamp_ = kNoTimestamp();
