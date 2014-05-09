e33d707e0804e670c36586b66e9a64335695b77d
atwilson@chromium.org <atwilson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-09 14:04:06 +0000

    Removed obsolete --client-login-to-oauth2-url flag
    BUG=367025
    
    Review URL: https://codereview.chromium.org/258823008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@269276 0039d316-1c4b-4281-b951-d872f2087c98

--- google_apis/gaia/gaia_switches.h
+++ google_apis/gaia/gaia_switches.h
@@ -8,9 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace switches {
 
-// Supplies custom client login to OAuth2 URL for testing purposes.
-extern const char kClientLoginToOAuth2Url[];
-
 // Specifies the path for GAIA authentication URL. The default value is
 // "https://accounts.google.com".
 extern const char kGaiaUrl[];
