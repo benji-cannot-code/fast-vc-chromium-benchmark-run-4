d0babc256b60b252878551c4464b083cc777575f
marja@chromium.org <marja@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-09 10:18:23 +0000

    Revert 269197 "Mojo: Add the ability to allocate space for platf..."
    Reason: regresses sizes/si-initializers
    by adding static initializer transport_data.cc mojo:
    :system::TransportData::kMaxBufferSize.
    
    > Mojo: Add the ability to allocate space for platform handle info in TransportData.
    > 
    > We don't really need this for POSIX, but we'll need it for Windows, so
    > all the interfaces will need to take it into account. E.g., if the
    > source process can duplicate handles into the target, it needs space to
    > store the new handle value. (If we're using a broker, other data is
    > needed.)
    > 
    > R=yzshen@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/278513003
    
    TBR=viettrungluu@chromium.org
    
    Review URL: https://codereview.chromium.org/276623003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@269204 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/system/channel.h
+++ mojo/system/channel.h
@@ -112,9 +112,6 @@ class MOJO_SYSTEM_IMPL_EXPORT Channel
   void DetachMessagePipeEndpoint(MessageInTransit::EndpointId local_id,
                                  MessageInTransit::EndpointId remote_id);
 
-  // See |RawChannel::GetSerializedPlatformHandleSize()|.
-  size_t GetSerializedPlatformHandleSize() const;
-
  private:
   struct EndpointInfo {
     enum State {
--- mojo/system/raw_channel_win.cc
+++ mojo/system/raw_channel_win.cc
@@ -75,9 +75,6 @@ class RawChannelWin : public RawChannel {
   RawChannelWin(embedder::ScopedPlatformHandle handle);
   virtual ~RawChannelWin();
 
-  // |RawChannel| public methods:
-  virtual size_t GetSerializedPlatformHandleSize() const OVERRIDE;
-
  private:
   // RawChannelIOHandler receives OS notifications for I/O completion. It must
   // be created on the I/O thread.
@@ -160,7 +157,7 @@ class RawChannelWin : public RawChannel {
     DISALLOW_COPY_AND_ASSIGN(RawChannelIOHandler);
   };
 
-  // |RawChannel| private methods:
+  // |RawChannel| implementation:
   virtual IOResult Read(size_t* bytes_read) OVERRIDE;
   virtual IOResult ScheduleRead() OVERRIDE;
   virtual IOResult WriteNoLock(size_t* bytes_written) OVERRIDE;
@@ -348,11 +345,6 @@ RawChannelWin::RawChannelWin(embedder::ScopedPlatformHandle handle)
 
 RawChannelWin::~RawChannelWin() {
   DCHECK(!io_handler_);
-}
-
-size_t RawChannelWin::GetSerializedPlatformHandleSize() const {
-  // TODO(vtl): Implement.
-  return 0;
 }
 
 RawChannel::IOResult RawChannelWin::Read(size_t* bytes_read) {
