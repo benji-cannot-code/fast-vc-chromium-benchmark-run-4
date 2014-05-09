ea28c1ce83c24e2d766cb65f7d73f4f8e09cd03e
ellyjones@chromium.org <ellyjones@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-09 01:33:17 +0000

    Enable auto-reload by default on mobile dev channel and desktop canary channel. Also, right now there's no way to disable auto-reload if the user is put into the experiment by Finch, so add a flag to disable it.
    BUG=chromium:355569
    TEST=adhoc
    
    Review URL: https://codereview.chromium.org/267113007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@269135 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -94,6 +94,7 @@ extern const char kDisableFullscreenWithinTab[];
 extern const char kDisableIPv6[];
 extern const char kDisableMinimizeOnSecondLauncherItemClick[];
 extern const char kDisableNTPOtherSessionsMenu[];
+extern const char kDisableOfflineAutoReload[];
 extern const char kDisableOriginChip[];
 extern const char kDisableOriginChipV2[];
 extern const char kDisablePasswordManagerReauthentication[];
