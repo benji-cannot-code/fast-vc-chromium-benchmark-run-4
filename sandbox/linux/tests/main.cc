f36741a3d4a7442f76f97495b20ed135d5b5b9eb
hamaji@chromium.org <hamaji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-10 11:38:57 +0000

    Non-SFI NaCl: Stop running sandbox tests in components_unittests
    Also revert the temporary change for the sandbox test framework,
    which allows fork() with threads.
    
    https://codereview.chromium.org/242383003
    
    TEST=nacl_loader_unittests, components_unittests, and trybots
    BUG=364751
    
    Review URL: https://codereview.chromium.org/273623002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@269558 0039d316-1c4b-4281-b951-d872f2087c98

--- sandbox/linux/tests/main.cc
+++ sandbox/linux/tests/main.cc
@@ -11,9 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace sandbox {
-
-extern const bool kAllowForkWithThreads = false;
-
 namespace {
 
 // Check for leaks in our tests.
