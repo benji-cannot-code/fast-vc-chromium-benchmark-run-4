5da3a0e140b0ee973c1cf2c32421a07ff3d579ab
skyostil@chromium.org <skyostil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-10 11:40:30 +0000

    Android: Enable vsync for synthetic gestures
    A recent refactoring accidentally removed the code that turns on vsync
    for synthetic gestures. This causes the gesture to never start if
    nothing else happens to turn on vsync at the same time.
    
    BUG=370967
    TEST=tools/perf/run_benchmark rasterize_and_record_micro.key_silk_cases
    --browser=android-chrome --page-filter=slide_drawer
    
    Review URL: https://codereview.chromium.org/269343013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@269561 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_view_android.cc
+++ content/browser/renderer_host/render_widget_host_view_android.cc
@@ -1167,6 +1167,7 @@ void RenderWidgetHostViewAndroid::OnSetNeedsFlushInput() {
     return;
   TRACE_EVENT0("input", "RenderWidgetHostViewAndroid::OnSetNeedsFlushInput");
   flush_input_requested_ = true;
+  content_view_core_->GetWindowAndroid()->RequestVSyncUpdate();
 }
 
 void RenderWidgetHostViewAndroid::CreateBrowserAccessibilityManagerIfNeeded() {
