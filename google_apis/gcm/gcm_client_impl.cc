4446a7f2914bf84684626db9081d9e1989068fe4
juyik@chromium.org <juyik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-10 19:49:40 +0000

    Enable auto-refreshing of the gcm-internals page whenever a GCM activity is recorded, and fix a bug that previously causes this cl to crash the browser upon signin.
    Basically GCMStatsRecorder::RecordRegistrationResponse() method did not check whether is_recording_ is true or not, so it always records. When the user signs in, a registration event is automatically triggered, but since the gcm-internals page has not been loaded yet, a callback object is still NULL. Therefore recorder calling this callback will crash the browser. I have added the check as well as test case for this scenario.
    
    BUG=341256, 371259
    
    Review URL: https://codereview.chromium.org/277743002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@269622 0039d316-1c4b-4281-b951-d872f2087c98

--- google_apis/gcm/gcm_client_impl.cc
+++ google_apis/gcm/gcm_client_impl.cc
@@ -214,7 +214,7 @@ void GCMClientImpl::Initialize(
     const scoped_refptr<base::SequencedTaskRunner>& blocking_task_runner,
     const scoped_refptr<net::URLRequestContextGetter>&
         url_request_context_getter,
-    Delegate* delegate) {
+    GCMClient::Delegate* delegate) {
   DCHECK_EQ(UNINITIALIZED, state_);
   DCHECK(url_request_context_getter);
   DCHECK(delegate);
@@ -232,6 +232,8 @@ void GCMClientImpl::Initialize(
   gcm_store_.reset(new GCMStoreImpl(path, blocking_task_runner));
 
   delegate_ = delegate;
+
+  recorder_.SetDelegate(this);
 
   state_ = INITIALIZED;
 }
@@ -665,6 +667,10 @@ GCMClient::GCMStatistics GCMClientImpl::GetStatistics() const {
     stats.registered_app_ids.push_back(it->first);
   }
   return stats;
+}
+
+void GCMClientImpl::OnActivityRecorded() {
+  delegate_->OnActivityRecorded();
 }
 
 void GCMClientImpl::OnMessageReceivedFromMCS(const gcm::MCSMessage& message) {
