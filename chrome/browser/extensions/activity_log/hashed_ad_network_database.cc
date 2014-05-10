9096fd795d0f196984b4a7b9d6a189b9292661d1
rdevlin.cronin@chromium.org <rdevlin.cronin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-10 02:46:31 +0000

    Resubmit: Add real file for AdNetworks DB
    This is a resubmit of https://codereview.chromium.org/268063004/.
    
    The previous version was reverted because it caused a memory leak. This was a
    problem with RefCounting from ui::ResourceBundle::LoadDataResourceBytes(), which
    has been fixed.
    
    Details:
    Even though the method returns a non-refcounted ptr to a RefCountedStaticMemory
    (which according to the class comment, "the ref counting does not matter" [1]),
    the ref-counting _does_ matter. The fix for this went in as part of a refactor
    in https://codereview.chromium.org/263953003/.
    
    [1] http://src.chromium.org/viewvc/chrome/trunk/src/base/memory/ref_counted_memory.h?revision=267321 line 44
    
    BUG=357204
    
    TBR=jhawkins@chromium.org (previously approved)
    TBR=felt@chromium.org (very minor changes)
    
    Review URL: https://codereview.chromium.org/274563003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@269494 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/activity_log/hashed_ad_network_database.cc
+++ chrome/browser/extensions/activity_log/hashed_ad_network_database.cc
@@ -32,7 +32,8 @@ const size_t kChecksumHashSize = 32u;
 }  // namespace
 
 HashedAdNetworkDatabase::HashedAdNetworkDatabase(
-    scoped_refptr<base::RefCountedStaticMemory> entries_memory) {
+    scoped_refptr<base::RefCountedStaticMemory> entries_memory)
+    : is_valid_(false) {
   // This can legitimately happen in unit tests.
   if (!entries_memory)
     return;
@@ -68,6 +69,8 @@ HashedAdNetworkDatabase::HashedAdNetworkDatabase(
     memcpy(&value, index, kUrlHashSize);
     entries_.insert(value);
   }
+
+  is_valid_ = true;
 }
 
 HashedAdNetworkDatabase::~HashedAdNetworkDatabase() {}
