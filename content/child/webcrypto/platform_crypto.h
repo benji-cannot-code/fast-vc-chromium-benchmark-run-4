a24435c3fcebbf1b2ccb31b1350270485be34e56
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-10 02:00:26 +0000

    [refactor] Change ordering of wrapkey parameters
    crypto.subtle.wrapKey() orders the key to be wrapped before the wrapping key. Use the same convention throughout webcrypto code to avoid confusion.
    
    BUG=245025
    
    Review URL: https://codereview.chromium.org/272033003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@269478 0039d316-1c4b-4281-b951-d872f2087c98

--- content/child/webcrypto/platform_crypto.h
+++ content/child/webcrypto/platform_crypto.h
@@ -230,10 +230,10 @@ Status ExportKeyPkcs8(PrivateKey* key,
                       std::vector<uint8>* buffer);
 
 // Preconditions:
-//  * |wrapping_key| is non-null
 //  * |key| is non-null
-Status WrapSymKeyAesKw(SymKey* wrapping_key,
-                       SymKey* key,
+//  * |wrapping_key| is non-null
+Status WrapSymKeyAesKw(SymKey* key,
+                       SymKey* wrapping_key,
                        std::vector<uint8>* buffer);
 
 // Unwraps (decrypts) |wrapped_key_data| using AES-KW and places the results in
@@ -264,10 +264,10 @@ Status DecryptAesKw(SymKey* key,
                     std::vector<uint8>* buffer);
 
 // Preconditions:
-//  * |wrapping_key| is non-null
 //  * |key| is non-null
-Status WrapSymKeyRsaEs(PublicKey* wrapping_key,
-                       SymKey* key,
+//  * |wrapping_key| is non-null
+Status WrapSymKeyRsaEs(SymKey* key,
+                       PublicKey* wrapping_key,
                        std::vector<uint8>* buffer);
 
 // Preconditions:
