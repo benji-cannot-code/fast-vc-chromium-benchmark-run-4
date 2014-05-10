e337b2c245504d9292dc63a59826d567734c96bc
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-10 02:58:30 +0000

    Fix iOS some more after r269483. You can’t use mach/mach_vm.h on iOS.
    TBR=rsesek
    
    Review URL: https://codereview.chromium.org/273183003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@269501 0039d316-1c4b-4281-b951-d872f2087c98

--- base/mac/scoped_mach_vm.cc
+++ base/mac/scoped_mach_vm.cc
@@ -5,6 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/mac/scoped_mach_vm.h"
 
+#if defined(OS_IOS)
+#define mach_vm_address_t vm_address_t
+#define mach_vm_size_t vm_size_t
+#define mach_vm_deallocate vm_deallocate
+#endif  // OS_IOS
+
 namespace base {
 namespace mac {
 
