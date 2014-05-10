b489e7e514440e8fa71379a30ff54ab4c1730f35
mgiuca@chromium.org <mgiuca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-10 07:51:27 +0000

    Use centered app list position whenever virtual keyboard is allowed.
    Previously, the app list would be centered only when the virtual
    keyboard is enabled. Now it is centered whenever the virtual keyboard
    might be enabled, with the following new cases:
    - If --ash-enable-touch-view-testing is on.
    - If any accelerometer data is detected (as this can trigger virtual
    keyboard).
    
    BUG=369382
    TEST=Run ChromeOS with --ash-enable-touch-view-testing. App launcher
    should be centered and landscape.
    
    Review URL: https://codereview.chromium.org/264523002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@269556 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/maximize_mode/maximize_mode_controller.cc
+++ ash/wm/maximize_mode/maximize_mode_controller.cc
@@ -6,9 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/maximize_mode/maximize_mode_controller.h"
 
 #include "ash/accelerometer/accelerometer_controller.h"
+#include "ash/ash_switches.h"
 #include "ash/display/display_manager.h"
 #include "ash/shell.h"
 #include "ash/wm/maximize_mode/maximize_mode_event_blocker.h"
+#include "base/command_line.h"
 #include "ui/gfx/vector3d_f.h"
 
 namespace ash {
@@ -83,7 +85,8 @@ float ClockwiseAngleBetweenVectorsInDegrees(const gfx::Vector3dF& base,
 }  // namespace
 
 MaximizeModeController::MaximizeModeController()
-    : rotation_locked_(false) {
+    : rotation_locked_(false),
+      have_seen_accelerometer_data_(false) {
   Shell::GetInstance()->accelerometer_controller()->AddObserver(this);
 }
 
@@ -91,9 +94,21 @@ MaximizeModeController::~MaximizeModeController() {
   Shell::GetInstance()->accelerometer_controller()->RemoveObserver(this);
 }
 
+bool MaximizeModeController::CanEnterMaximizeMode() {
+  // If we have ever seen accelerometer data, then HandleHingeRotation may
+  // trigger maximize mode at some point in the future.
+  // The --enable-touch-view-testing switch can also mean that we may enter
+  // maximize mode.
+  return have_seen_accelerometer_data_ ||
+         CommandLine::ForCurrentProcess()->HasSwitch(
+             switches::kAshEnableTouchViewTesting);
+}
+
 void MaximizeModeController::OnAccelerometerUpdated(
     const gfx::Vector3dF& base,
     const gfx::Vector3dF& lid) {
+  have_seen_accelerometer_data_ = true;
+
   // Ignore the reading if it appears unstable. The reading is considered
   // unstable if it deviates too much from gravity and/or the magnitude of the
   // reading from the lid differs too much from the reading from the base.
