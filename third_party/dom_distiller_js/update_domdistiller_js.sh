1ed8cb256183e367ee6dde5bcf42bb0315ccee97
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-10 06:51:07 +0000

    Revert of Update DomDistillerJsUpdater and Roll DomDistillerJs (https://codereview.chromium.org/277563002/)
    Reason for revert:
    check_licenses failure: http://build.chromium.org/p/chromium/builders/Linux/builds/49667
    
    Original issue's description:
    > Update DomDistillerJsUpdater and Roll DomDistillerJs
    > 
    > The update script should build `ant package` and copy the entire
    > out/package directory.
    > 
    > This moves the path to domdistiller.js to .../package/js so update
    > that reference.
    > 
    > The old (non-proto) API is still supported, so this keeps using that.
    > 
    > Picked up changes:
    > fdb023d Fix Proto module's path
    > 44e9366 This adds two simple protoc plugins.
    > 
    > NOTRY=true
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=269485
    
    TBR=yfriedman@chromium.org,cjhopman@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/278143003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@269553 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/dom_distiller_js/update_domdistiller_js.sh
+++ third_party/dom_distiller_js/update_domdistiller_js.sh
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 (
   dom_distiller_js_path=third_party/dom_distiller_js
-  dom_distiller_js_package=$dom_distiller_js_path/package
+  compiled_js_path=$dom_distiller_js_path/js/domdistiller.js
   readme_chromium=$dom_distiller_js_path/README.chromium
   tmpdir=/tmp/domdistiller-$$
   changes=/tmp/domdistiller.changes
@@ -24,14 +24,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   pushd $tmpdir
   git clone https://code.google.com/p/dom-distiller/ .
+  ant extractjs
   new_gitsha=$(git rev-parse --short=10 HEAD)
   git log --oneline ${curr_gitsha}.. > $changes
-  ant package
   popd
 
-  rm -rf $dom_distiller_js_package
-  mkdir $dom_distiller_js_package
-  cp -rf $tmpdir/out/package/* $dom_distiller_js_package
+  mkdir -p $(dirname $compiled_js_path)
+  cp $tmpdir/out/domdistiller.js $compiled_js_path
   cp $tmpdir/LICENSE $dom_distiller_js_path/
   sed -i "s/Version: [0-9a-f]*/Version: $new_gitsha/" $readme_chromium
 
