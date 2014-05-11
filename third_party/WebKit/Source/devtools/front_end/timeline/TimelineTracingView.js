158ee49f1b02c504d93ddc4364b6447499b62d6a
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-11 07:46:07 +0000

    DevTools: sync timeline and tracing grid times in timeline panel.
    TBR=yurys
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/277153003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173830 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/timeline/TimelineTracingView.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TimelineTracingView.js
@@ -12,15 +12,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @extends {WebInspector.VBox}
  * @param {!WebInspector.TimelineModeViewDelegate} delegate
  * @param {!WebInspector.TracingModel} tracingModel
+ * @param {!WebInspector.TimelineModel} modelForZeroTime
  */
-WebInspector.TimelineTracingView = function(delegate, tracingModel)
+WebInspector.TimelineTracingView = function(delegate, tracingModel, modelForZeroTime)
 {
     WebInspector.VBox.call(this);
     this._delegate = delegate;
     this._tracingModel = tracingModel;
     this.element.classList.add("timeline-flamechart");
     this.registerRequiredCSS("flameChart.css");
-    this._dataProvider = new WebInspector.TraceViewFlameChartDataProvider(this._tracingModel);
+    this._dataProvider = new WebInspector.TraceViewFlameChartDataProvider(this._tracingModel, modelForZeroTime);
     this._mainView = new WebInspector.FlameChart(this._dataProvider, this, true);
     this._mainView.show(this.element);
     this._mainView.addEventListener(WebInspector.FlameChart.Events.EntrySelected, this._onEntrySelected, this);
@@ -205,12 +206,14 @@ WebInspector.TimelineTracingView.prototype = {
  * @constructor
  * @implements {WebInspector.FlameChartDataProvider}
  * @param {!WebInspector.TracingModel} model
+ * @param {!WebInspector.TimelineModel} timelineModelForZeroTime
  */
-WebInspector.TraceViewFlameChartDataProvider = function(model)
+WebInspector.TraceViewFlameChartDataProvider = function(model, timelineModelForZeroTime)
 {
     WebInspector.FlameChartDataProvider.call(this);
     this._model = model;
-    this._font = "bold 12px " + WebInspector.fontFamily();
+    this._timelineModelForZeroTime = timelineModelForZeroTime;
+    this._font = "12px " + WebInspector.fontFamily();
     this._palette = new WebInspector.TraceViewPalette();
     var dummyEventPayload = {
         cat: "dummy",
@@ -310,7 +313,7 @@ WebInspector.TraceViewFlameChartDataProvider.prototype = {
 
         this._currentLevel = 0;
         this._headerTitles = {};
-        this._zeroTime = this._model.minimumRecordTime() || 0;
+        this._zeroTime = this._timelineModelForZeroTime.minimumRecordTime() * 1000;
         this._timeSpan = Math.max((this._model.maximumRecordTime() || 0) - this._zeroTime, 1000000);
         var processes = this._model.sortedProcesses();
         for (var processIndex = 0; processIndex < processes.length; ++processIndex) {
