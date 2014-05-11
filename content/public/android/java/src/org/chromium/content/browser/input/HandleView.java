1a52dc2278e238d34c9bb27ee9718c26e6b05ba8
jdduke@chromium.org <jdduke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-11 01:08:10 +0000

    Revert of Android-side of insertion/selection handles visibility. (https://codereview.chromium.org/188023002/)
    Reason for revert:
    There are a number of cases where this incorrectly hides the selection/insertion
    handles in an editable node.
    
    Original issue's description:
    > Android-side of insertion/selection handles visibility.
    > 
    > This patch relies on other two patches:
    > https://codereview.chromium.org/177903010/
    > https://codereview.chromium.org/186753002/
    > to correctly compute the visibility of selection and
    > insertion handles when editing text input, text area
    > and contenteditable elements.
    > 
    > BUG=236033
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=258839
    
    BUG=372056
    TBR=tedchoc@chromium.org
    
    Review URL: https://codereview.chromium.org/278923003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@269667 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/input/HandleView.java
+++ content/public/android/java/src/org/chromium/content/browser/input/HandleView.java
@@ -19,8 +19,6 @@ import android.view.ViewParent;
 import android.view.animation.AnimationUtils;
 import android.widget.PopupWindow;
 
-import com.google.common.annotations.VisibleForTesting;
-
 import org.chromium.content.browser.PositionObserver;
 
 /**
@@ -233,8 +231,7 @@ public class HandleView extends View {
         return mContainer.isShowing();
     }
 
-    @VisibleForTesting
-    boolean isPositionVisible() {
+    private boolean isPositionVisible() {
         // Always show a dragging handle.
         if (mIsDragging) {
             return true;
@@ -254,17 +251,8 @@ public class HandleView extends View {
         final int posX = getContainerPositionX() + (int) mHotspotX;
         final int posY = getContainerPositionY() + (int) mHotspotY;
 
-        boolean result = posX >= clip.left && posX <= clip.right &&
+        return posX >= clip.left && posX <= clip.right &&
                 posY >= clip.top && posY <= clip.bottom;
-
-        final Rect clippingRect = mController.getVisibleClippingRectangle();
-        if (result && clippingRect != null) {
-            // We need to clip against the visible areas as supplied by Blink,
-            // e.g. textaread and text input elements.
-            return clippingRect.contains(getAdjustedPositionX(), getAdjustedPositionY());
-        }
-
-        return result;
     }
 
     // x and y are in physical pixels.
