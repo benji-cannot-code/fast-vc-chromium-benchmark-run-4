43bfdedcaf09680e92af9e281c19adc7e3cfbe69
philipj@opera.com <philipj@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-11 21:51:59 +0000

    Tighten the asserts in VTTCue::calculateDisplayParameters()
    BUG=none
    
    Review URL: https://codereview.chromium.org/276183002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173842 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/track/vtt/VTTCue.cpp
+++ third_party/WebKit/Source/core/html/track/vtt/VTTCue.cpp
@@ -630,7 +630,7 @@ void VTTCue::calculateDisplayParameters()
             else
                 m_displayPosition.first = 100 - m_textPosition - m_displaySize / 2;
             break;
-        case NumberOfAlignments:
+        default:
             ASSERT_NOT_REACHED();
         }
     } else {
@@ -647,7 +647,7 @@ void VTTCue::calculateDisplayParameters()
         case Middle:
             m_displayPosition.second = m_textPosition - m_displaySize / 2;
             break;
-        case NumberOfAlignments:
+        default:
             ASSERT_NOT_REACHED();
         }
     }
