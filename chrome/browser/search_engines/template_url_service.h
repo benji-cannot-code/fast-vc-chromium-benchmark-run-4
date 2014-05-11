bb354e568f3b1466b3dff64af477f0703c9f0c3e
erikwright@chromium.org <erikwright@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-11 19:48:53 +0000

    Handle TemplateURLService load failure better, and make some test correctness fixes that will be needed later.
    This also does a variety of miscellaneous cleanups to the modified files.
    
    BUG=364183
    TEST=none
    R=jered@chromium.org, pkasting@chromium.org
    TBR=engedy
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=269329
    
    Review URL: https://codereview.chromium.org/272573004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@269716 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/search_engines/template_url_service.h
+++ chrome/browser/search_engines/template_url_service.h
@@ -113,11 +113,6 @@ class TemplateURLService : public WebDataServiceConsumer,
       scoped_ptr<TemplateURLData>* default_provider_data,
       bool* is_managed);
 
-  // Saves enough of url to |prefs| so that it can be loaded from preferences on
-  // start up.
-  static void SaveDefaultSearchProviderToPrefs(const TemplateURL* url,
-                                               PrefService* prefs);
-
   // Generates a suitable keyword for the specified url, which must be valid.
   // This is guaranteed not to return an empty string, since TemplateURLs should
   // never have an empty keyword.
@@ -140,6 +135,11 @@ class TemplateURLService : public WebDataServiceConsumer,
   static GURL GenerateSearchURLUsingTermsData(
       const TemplateURL* t_url,
       const SearchTermsData& search_terms_data);
+
+  // Saves enough of url to |prefs| so that it can be loaded from preferences on
+  // start up.
+  void SaveDefaultSearchProviderToPrefs(const TemplateURL* url,
+                                        PrefService* prefs) const;
 
   // Returns true if there is no TemplateURL that conflicts with the
   // keyword/url pair, or there is one but it can be replaced. If there is an
@@ -410,16 +410,12 @@ class TemplateURLService : public WebDataServiceConsumer,
                            DontUpdateKeywordSearchForNonReplaceable);
   FRIEND_TEST_ALL_PREFIXES(TemplateURLServiceTest, ChangeGoogleBaseValue);
   FRIEND_TEST_ALL_PREFIXES(TemplateURLServiceTest, MergeDeletesUnusedProviders);
-  FRIEND_TEST_ALL_PREFIXES(TemplateURLServiceSyncTest,
-                           CreateSyncDataFromTemplateURL);
-  FRIEND_TEST_ALL_PREFIXES(TemplateURLServiceSyncTest,
-                           CreateTemplateURLFromSyncData);
   FRIEND_TEST_ALL_PREFIXES(TemplateURLServiceSyncTest, UniquifyKeyword);
+  FRIEND_TEST_ALL_PREFIXES(TemplateURLServiceSyncTest,
+                           IsLocalTemplateURLBetter);
   FRIEND_TEST_ALL_PREFIXES(TemplateURLServiceSyncTest,
                            ResolveSyncKeywordConflict);
   FRIEND_TEST_ALL_PREFIXES(TemplateURLServiceSyncTest, PreSyncDeletes);
-  FRIEND_TEST_ALL_PREFIXES(TemplateURLServiceSyncTest,
-                           IsLocalTemplateURLBetter);
   FRIEND_TEST_ALL_PREFIXES(TemplateURLServiceSyncTest, MergeInSyncTemplateURL);
 
   friend class TemplateURLServiceTestUtilBase;
@@ -569,6 +565,13 @@ class TemplateURLService : public WebDataServiceConsumer,
   // Caller is responsible for notifying observers.
   void RemoveNoNotify(TemplateURL* template_url);
 
+  // Like ResetTemplateURL(), but instead of notifying observers, returns
+  // whether anything has changed.
+  bool ResetTemplateURLNoNotify(TemplateURL* url,
+                                const base::string16& title,
+                                const base::string16& keyword,
+                                const std::string& search_url);
+
   // Notify the observers that the model has changed.  This is done only if the
   // model is loaded.
   void NotifyObservers();
@@ -714,7 +717,9 @@ class TemplateURLService : public WebDataServiceConsumer,
   // Whether the keywords have been loaded.
   bool loaded_;
 
-  // Did loading fail? This is only valid if loaded_ is true.
+  // Set when the web data service fails to load properly.  This prevents
+  // further communication with sync or writing to prefs, so we don't persist
+  // inconsistent state data anywhere.
   bool load_failed_;
 
   // If non-zero, we're waiting on a load.
