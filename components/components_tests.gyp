0752ae72b0396f416fb0a73751e53c6c343b1a92
vabr@chromium.org <vabr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-11 01:48:40 +0000

    Password manager internals page service: introduction
    Introduce a BrowserContextKeyedService to collect logs from the PasswordManagerClient instances, and distribute them to PasswordManagerInternalsUI instances for display. The service will also ensure that nothing is logged in Incognito profiles, and that logs are flushed as soon as no one is displaying them.
    
    Except for checking the incognito bit, the whole functionality is implemented in the LogRouter class inside the password_manager component, for easy code sharing across platforms.
    The service inherits from LogRouter, and adds nothing except for leveraging the BrowserContextKeyedServiceFactory framework to keep the functionality from OTR profiles.
    
    This CL only introduces the service, it does not put it to use (outside tests) yet.
    For that there will be another CL (https://codereview.chromium.org/269513003, but WIP, not fit for (re)viewing yet).
    Yet another CL deals with renaming PasswordManagerLogger to LogReceiver: https://codereview.chromium.org/264793010/
    
    Design doc: https://docs.google.com/document/d/1ArDhTo0w-8tOPiTwqM1gG6ZGqODo8UTpXlJjjnCNK4s/edit?usp=sharing#heading=h.cqh4wuj8j4yl
    
    BUG=347927
    
    Review URL: https://codereview.chromium.org/262583007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@269686 0039d316-1c4b-4281-b951-d872f2087c98

--- components/components_tests.gyp
+++ components/components_tests.gyp
@@ -109,6 +109,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'os_crypt/keychain_password_mac_unittest.mm',
             'os_crypt/os_crypt_unittest.cc',
             'password_manager/core/browser/browser_save_password_progress_logger_unittest.cc',
+            'password_manager/core/browser/log_router_unittest.cc',
             'password_manager/core/browser/login_database_unittest.cc',
             'password_manager/core/browser/password_autofill_manager_unittest.cc',
             'password_manager/core/browser/password_form_manager_unittest.cc',
---
+++ components/password_manager/content/browser/password_manager_internals_service_factory.cc
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/password_manager/content/browser/password_manager_internals_service_factory.h"
+
+#include "components/keyed_service/content/browser_context_dependency_manager.h"
+#include "components/password_manager/core/browser/password_manager_internals_service.h"
+
+namespace password_manager {
+
+// static
+PasswordManagerInternalsService*
+PasswordManagerInternalsServiceFactory::GetForBrowserContext(
+    content::BrowserContext* context) {
+  return static_cast<PasswordManagerInternalsService*>(
+      GetInstance()->GetServiceForBrowserContext(context, /* create = */ true));
+}
+
+// static
+PasswordManagerInternalsServiceFactory*
+PasswordManagerInternalsServiceFactory::GetInstance() {
+  return Singleton<PasswordManagerInternalsServiceFactory>::get();
+}
+
+PasswordManagerInternalsServiceFactory::PasswordManagerInternalsServiceFactory()
+    : BrowserContextKeyedServiceFactory(
+          "PasswordManagerInternalsService",
+          BrowserContextDependencyManager::GetInstance()) {
+}
+
+PasswordManagerInternalsServiceFactory::
+    ~PasswordManagerInternalsServiceFactory() {
+}
+
+KeyedService* PasswordManagerInternalsServiceFactory::BuildServiceInstanceFor(
+    content::BrowserContext* /* context */) const {
+  return new PasswordManagerInternalsService();
+}
+
+}  // namespace password_manager
