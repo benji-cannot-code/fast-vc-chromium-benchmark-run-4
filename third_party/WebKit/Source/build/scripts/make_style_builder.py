7f2ec9f36519ce86d2ac595e8eb3b07db4c79362
timloh@chromium.org <timloh@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-12 18:35:55 +0000

    Support direction aware properties in the jinja2-based StyleBuilder
    This patch adds support in the jinja2-based StyleBuilder for direction
    aware properties. These are vendor-specific properties which map to
    regular properties based on text direction and block flow direction.
    
    BUG=237400
    
    Review URL: https://codereview.chromium.org/272093002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173902 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/build/scripts/make_style_builder.py
+++ third_party/WebKit/Source/build/scripts/make_style_builder.py
@@ -50,6 +50,7 @@ class StyleBuilderWriter(in_generator.Writer):
         'custom_initial': [True, False],
         'custom_inherit': [True, False],
         'custom_value': [True, False],
+        'direction_aware': [True, False],
     }
     defaults = {
         'name_for_methods': None,
@@ -57,6 +58,7 @@ class StyleBuilderWriter(in_generator.Writer):
         'svg': False,
         'font': False,
         'converter': None,
+        'direction_aware': False,
 # These depend on property name by default
         'type_name': None,
         'getter': None,
@@ -96,6 +98,7 @@ class StyleBuilderWriter(in_generator.Writer):
                 property['custom_initial'] = True
                 property['custom_inherit'] = True
                 property['custom_value'] = True
+            property['should_declare_functions'] = not property['use_handlers_for'] and not property['direction_aware']
 
         self._properties = dict((property['property_id'], property) for property in self._properties)
 
--- third_party/WebKit/Source/core/css/resolver/StyleBuilderCustom.cpp
+++ third_party/WebKit/Source/core/css/resolver/StyleBuilderCustom.cpp
@@ -1382,7 +1382,6 @@ void StyleBuilder::oldApplyProperty(CSSPropertyID id, StyleResolverState& state,
     }
     case CSSPropertyInvalid:
         return;
-    // Directional properties are resolved by resolveDirectionAwareProperty() before the switch.
     case CSSPropertyWebkitBorderEndColor:
     case CSSPropertyWebkitBorderEndStyle:
     case CSSPropertyWebkitBorderEndWidth:
@@ -1413,11 +1412,8 @@ void StyleBuilder::oldApplyProperty(CSSPropertyID id, StyleResolverState& state,
     case CSSPropertyWebkitMinLogicalHeight:
     case CSSPropertyWebkitMaxLogicalWidth:
     case CSSPropertyWebkitMaxLogicalHeight:
-    {
-        CSSPropertyID newId = CSSProperty::resolveDirectionAwareProperty(id, state.style()->direction(), state.style()->writingMode());
-        ASSERT(newId != id);
-        return applyProperty(newId, state, value);
-    }
+        ASSERT_NOT_REACHED();
+        return;
     case CSSPropertyFontStretch:
     case CSSPropertyPage:
     case CSSPropertyTextLineThroughColor:
