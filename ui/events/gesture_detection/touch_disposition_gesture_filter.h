c3821a8fd4417ca3ddecba9cd1ef38b79558ea33
tdresser@chromium.org <tdresser@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-12 22:19:43 +0000

    Pass location with gestures created in TouchDispositionGestureFilter
    Aura uses the locations of some ending events which weren't
    being assigned locations in the TDGF. To use the unified GR
    on Aura, we need these locations to be assigned correctly.
    
    We use the location of the first pointer down when the gesture was
    created a the location of the gestures created by the TDGF.
    
    BUG=371875
    TEST=TouchDispositionGestureFilterTest.*
    
    Review URL: https://codereview.chromium.org/277373002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@269893 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/events/gesture_detection/touch_disposition_gesture_filter.h
+++ ui/events/gesture_detection/touch_disposition_gesture_filter.h
@@ -79,9 +79,9 @@ class GESTURE_DETECTION_EXPORT TouchDispositionGestureFilter {
 
   void FilterAndSendPacket(const GestureEventDataPacket& packet);
   void SendGesture(const GestureEventData& gesture);
-  void CancelTapIfNecessary(const base::TimeTicks& timestamp);
-  void CancelFlingIfNecessary(const base::TimeTicks& timestamp);
-  void EndScrollIfNecessary(const base::TimeTicks& timestamp);
+  void CancelTapIfNecessary();
+  void CancelFlingIfNecessary();
+  void EndScrollIfNecessary();
   void PopGestureSequence();
   GestureSequence& Head();
   GestureSequence& Tail();
@@ -99,6 +99,8 @@ class GESTURE_DETECTION_EXPORT TouchDispositionGestureFilter {
   bool needs_show_press_event_;
   bool needs_fling_ending_event_;
   bool needs_scroll_ending_event_;
+  // Only valid in the scope of |FilterAndSendPacket()|.
+  const GestureEventDataPacket* packet_being_sent_;
 
   DISALLOW_COPY_AND_ASSIGN(TouchDispositionGestureFilter);
 };
