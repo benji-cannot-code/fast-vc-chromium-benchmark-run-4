9d4be00f15fddaa15b93b587a71e63de382d3eb1
pstew@chromium.org <pstew@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-13 00:51:28 +0000

    Allow option to ignore default route
    In OpenVPN, it is up to the client to decide whether or not to
    create a default route through the OpenVPN server.  Even when the
    server supplies a gateway address, it isn't always clear if the
    intent is for the client to set a default route through this
    gateway.
    
    By default, ChromeOS creates a default route to the gateway.
    This CL adds an ONC configuration option to allow ONC to override
    this default behavior, thus providing the means for a "split
    tunnel" configuration in situations where the route configuration
    provided by the VPN server is amenable to this.  Supplying this
    option does not override any explicit routes pushed by the server.
    
    BUG=370460
    R=pneubeck@chromium.org
    
    Review URL: https://codereview.chromium.org/275723002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@269942 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/network/onc/onc_translation_tables.cc
+++ chromeos/network/onc/onc_translation_tables.cc
@@ -68,6 +68,8 @@ const FieldTranslationEntry openvpn_fields[] = {
     { ::onc::openvpn::kCipher, shill::kOpenVPNCipherProperty},
     { ::onc::openvpn::kCompLZO, shill::kOpenVPNCompLZOProperty},
     { ::onc::openvpn::kCompNoAdapt, shill::kOpenVPNCompNoAdaptProperty},
+    { ::onc::openvpn::kIgnoreDefaultRoute,
+      shill::kOpenVPNIgnoreDefaultRouteProperty},
     { ::onc::openvpn::kKeyDirection, shill::kOpenVPNKeyDirectionProperty},
     { ::onc::openvpn::kNsCertType, shill::kOpenVPNNsCertTypeProperty},
     { ::onc::vpn::kPassword, shill::kOpenVPNPasswordProperty},
