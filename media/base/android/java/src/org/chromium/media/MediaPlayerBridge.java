4aa0ef5e8adb2ff78250fa08fe86f709e81ab22e
qinmin@chromium.org <qinmin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-13 00:07:12 +0000

    Fix an issue that jni generator is ignoring input param
    If there is an extra white space in the param, the param will not be used in the auto generated jni file
    
    BUG=372459
    R=trchen@chromium.org
    
    Review URL: https://codereview.chromium.org/286563002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@269932 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/android/java/src/org/chromium/media/MediaPlayerBridge.java
+++ media/base/android/java/src/org/chromium/media/MediaPlayerBridge.java
@@ -43,7 +43,7 @@ public class MediaPlayerBridge {
     private long mNativeMediaPlayerBridge;
 
     @CalledByNative
-    private static MediaPlayerBridge create(long  nativeMediaPlayerBridge) {
+    private static MediaPlayerBridge create(long nativeMediaPlayerBridge) {
         return new MediaPlayerBridge(nativeMediaPlayerBridge);
     }
 
