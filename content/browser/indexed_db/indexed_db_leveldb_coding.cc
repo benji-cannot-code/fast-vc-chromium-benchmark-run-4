2128626f1c5085d66fc0d625d46df8618d205ae6
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-13 06:55:43 +0000

    Revert 270016 "Add most of the metadata-handling code for blobs...."
    This may have caused indexed_db layout tests to start failing.
    
    > Add most of the metadata-handling code for blobs.  It's not quite all there, but
    > this is the biggest chunk I can pull out vaguely cleanly.  It does contain a
    > couple of fake calls to keep the compiler happy.
    > This CL also makes SetUpMetadata a member in order to ease testing in a later CL.
    > 
    > This depends on https://codereview.chromium.org/261843004/.
    > 
    > BUG=108012
    > 
    > Review URL: https://codereview.chromium.org/266333002
    
    TBR=ericu@chromium.org
    
    Review URL: https://codereview.chromium.org/281623004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@270038 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/indexed_db/indexed_db_leveldb_coding.cc
+++ content/browser/indexed_db/indexed_db_leveldb_coding.cc
@@ -47,8 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // <0, 0, 0, 2> => SerializedScriptValue version [DataVersionKey]
 // <0, 0, 0, 3>
 //   => Blob journal
-//     The format of the journal is:
-//         {database_id (var int), blobKey (var int)}*.
+//     The format of the journal is: {database_id, blobKey}*.
 //     If the blobKey is kAllBlobsKey, the whole database should be deleted.
 //     [BlobJournalKey]
 // <0, 0, 0, 4> => Live blob journal; same format. [LiveBlobJournalKey]
