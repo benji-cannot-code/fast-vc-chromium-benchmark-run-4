d87907c372096f51cdc8ebbe5955b0bb95c53617
dmazzoni@chromium.org <dmazzoni@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-13 11:59:20 +0000

    Expose an accessible relation between the main window and active alert.
    This will specifically allow screen readers to query the accessible
    object for the main window and quickly get a list of an alerts that
    are visible - for example infobars, bubbles, and the Chrome menu itself
    if it has an alert icon in it.
    
    BUG=369903
    
    Review URL: https://codereview.chromium.org/266963002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@270074 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/accessibility/native_view_accessibility_win.h
+++ ui/views/accessibility/native_view_accessibility_win.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <UIAutomationCore.h>
 
 #include <set>
+#include <vector>
 
 #include "third_party/iaccessible2/ia2_api_all.h"
 #include "ui/accessibility/ax_view_state.h"
@@ -39,7 +40,7 @@ namespace views {
 class __declspec(uuid("26f5641a-246d-457b-a96d-07f3fae6acf2"))
 NativeViewAccessibilityWin
   : public CComObjectRootEx<CComMultiThreadModel>,
-    public IDispatchImpl<IAccessible2, &IID_IAccessible2,
+    public IDispatchImpl<IAccessible2_2, &IID_IAccessible2_2,
                            &LIBID_IAccessible2Lib>,
     public IAccessibleText,
     public IServiceProvider,
@@ -48,9 +49,9 @@ NativeViewAccessibilityWin
     public NativeViewAccessibility {
  public:
   BEGIN_COM_MAP(NativeViewAccessibilityWin)
-    COM_INTERFACE_ENTRY2(IDispatch, IAccessible2)
-    COM_INTERFACE_ENTRY2(IAccessible, IAccessible2)
-    COM_INTERFACE_ENTRY(IAccessible2)
+    COM_INTERFACE_ENTRY2(IDispatch, IAccessible2_2)
+    COM_INTERFACE_ENTRY2(IAccessible, IAccessible2_2)
+    COM_INTERFACE_ENTRY(IAccessible2_2)
     COM_INTERFACE_ENTRY(IAccessibleText)
     COM_INTERFACE_ENTRY(IServiceProvider)
     COM_INTERFACE_ENTRY(IAccessibleEx)
@@ -146,11 +147,19 @@ NativeViewAccessibilityWin
 
   STDMETHODIMP get_windowHandle(HWND* window_handle);
 
+  STDMETHODIMP get_relationTargetsOfType(BSTR type,
+                                         long max_targets,
+                                         IUnknown ***targets,
+                                         long *n_targets);
+
   //
   // IAccessible2 methods not implemented.
   //
 
   STDMETHODIMP get_attributes(BSTR* attributes) {
+    return E_NOTIMPL;
+  }
+  STDMETHODIMP get_attribute(BSTR name, VARIANT* attribute) {
     return E_NOTIMPL;
   }
   STDMETHODIMP get_indexInParent(LONG* index_in_parent) {
@@ -204,6 +213,10 @@ NativeViewAccessibilityWin
     return E_NOTIMPL;
   }
   STDMETHODIMP get_locale(IA2Locale* locale) {
+    return E_NOTIMPL;
+  }
+  STDMETHODIMP get_accessibleWithCaret(IUnknown** accessible,
+                                       long* caret_offset) {
     return E_NOTIMPL;
   }
 
@@ -397,6 +410,12 @@ NativeViewAccessibilityWin
   // NativeViewHost.
   void PopulateChildWidgetVector(std::vector<Widget*>* child_widgets);
 
+  // Adds this view to alert_target_view_storage_ids_.
+  void AddAlertTarget();
+
+  // Removes this view from alert_target_view_storage_ids_.
+  void RemoveAlertTarget();
+
   // Give CComObject access to the class constructor.
   template <class Base> friend class CComObject;
 
@@ -418,6 +437,11 @@ NativeViewAccessibilityWin
 
   // Next index into |view_storage_ids_| to use.
   static int next_view_storage_id_index_;
+
+  // A vector of view storage ids of views that have been the target of
+  // an alert event, in order to provide an api to quickly identify all
+  // open alerts.
+  static std::vector<int> alert_target_view_storage_ids_;
 
   DISALLOW_COPY_AND_ASSIGN(NativeViewAccessibilityWin);
 };
--- ui/views/views.gyp
+++ ui/views/views.gyp
@@ -684,6 +684,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           ],
         }],
         ['OS=="win"', {
+          'dependencies': [
+            '../../third_party/iaccessible2/iaccessible2.gyp:iaccessible2',
+          ],
           'link_settings': {
             'libraries': [
               '-limm32.lib',
