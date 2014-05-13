f10c346380f8ff1d3172be64a43f72d2b6a609f3
sungmann.cho@navercorp.com <sungmann.cho@navercorp.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-13 03:36:10 +0000

    Fix lint issues with Chromoting for Android.
    Currently, the lint warns at:
    Error: Field requires API level 18 (current min is 14):
        android.provider.Settings#EXTRA_ACCOUNT_TYPES [InlinedApi]
    Error: Field requires API level 16 (current min is 14):
        android.view.View#SYSTEM_UI_FLAG_FULLSCREEN [InlinedApi]
    
    So we need to add @TargetApi to suppress these lint error.
    
    BUG=327768
    
    Review URL: https://codereview.chromium.org/273313003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@269995 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/android/java/src/org/chromium/chromoting/Chromoting.java
+++ remoting/android/java/src/org/chromium/chromoting/Chromoting.java
@@ -11,6 +11,7 @@ import android.accounts.AccountManagerCallback;
 import android.accounts.AccountManagerFuture;
 import android.accounts.AuthenticatorException;
 import android.accounts.OperationCanceledException;
+import android.annotation.TargetApi;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -19,6 +20,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
@@ -99,6 +101,7 @@ public class Chromoting extends Activity implements JniInterface.ConnectionListe
         builder.setMessage(R.string.noaccounts_message);
         builder.setPositiveButton(R.string.noaccounts_add_account,
                 new DialogInterface.OnClickListener() {
+                    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
                         Intent intent = new Intent(Settings.ACTION_ADD_ACCOUNT);
