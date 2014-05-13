283bd76dbb9d076174a5269095212a632eb210b6
mfomitchev@chromium.org <mfomitchev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-13 21:43:49 +0000

    Implementation of the Touch Exploration Mode - Part III (enabled by default)
    This CL makes Touch Exploration Mode enabled by default. This is dependent on Part I and II:
    https://codereview.chromium.org/262483003
    https://codereview.chromium.org/261863002
    
    BUG=368828
    
    Review URL: https://codereview.chromium.org/266923003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@270216 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/accessibility/touch_exploration_controller_browsertest.cc
+++ chrome/browser/chromeos/accessibility/touch_exploration_controller_browsertest.cc
@@ -6,10 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/chromeos/touch_exploration_controller.h"
 
 #include "ash/accessibility_delegate.h"
-#include "ash/ash_switches.h"
 #include "ash/shell.h"
 #include "ash/test/ash_test_base.h"
-#include "base/command_line.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "ui/aura/test/event_generator.h"
@@ -24,10 +22,6 @@ class TouchExplorationTest : public InProcessBrowserTest {
  public:
   TouchExplorationTest() {}
   virtual ~TouchExplorationTest() {}
-
-  virtual void SetUpCommandLine(CommandLine* command_line) OVERRIDE {
-    command_line->AppendSwitch(ash::switches::kAshEnableTouchExplorationMode);
-  }
 
  protected:
   void SwitchTouchExplorationMode(bool on) {
