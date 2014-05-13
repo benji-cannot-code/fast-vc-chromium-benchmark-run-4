1ff7591eb179762d558219d4f161cde5e006398c
jsbell@chromium.org <jsbell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-13 12:24:36 +0000

    Update testharness.js to latest
    Update all copies of testharness.js, testharness.css and idlharness.js,
    update expectations where needed, and address a couple of FIXMEs.
    
    Default timeouts are extended per r170136.
    
    R=dpranke@chromium.org
    BUG=303251
    
    Review URL: https://codereview.chromium.org/263983002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173952 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/media/media-source/mediasource-util.js
+++ third_party/WebKit/LayoutTests/http/tests/media/media-source/mediasource-util.js
@@ -4,9 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     // the LayoutTest timeout. This prevents testharness.js from prematurely
     // terminating tests and allows the LayoutTest runner to control when to
     // timeout the test.
-    // FIXME: Change this to use explicit_timeout instead once /resources/testharness.js
-    //        is updated to a more recent version.
-    setup({ timeout: 120000 });
+    setup({ explicit_timeout: 120000 });
 
     var SEGMENT_INFO_LIST = [
         {
