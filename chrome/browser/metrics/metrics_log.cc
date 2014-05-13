de04b8926cc6e95e5a949677c733704a16d2c26b
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-13 01:07:44 +0000

    Remove unused include from metrics_log.cc.
    BUG=371469
    
    Review URL: https://codereview.chromium.org/280673002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@269949 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metrics/metrics_log.cc
+++ chrome/browser/metrics/metrics_log.cc
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/plugins/plugin_prefs.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/common/chrome_version_info.h"
-#include "chrome/common/logging_chrome.h"
 #include "chrome/common/metrics/variations/variations_util.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/installer/util/google_update_settings.h"
