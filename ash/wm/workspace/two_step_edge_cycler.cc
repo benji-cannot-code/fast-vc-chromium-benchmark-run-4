3514ec27ba2a9fa20b7c3dda3c0c161e3db86a11
glider@chromium.org <glider@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-13 13:19:09 +0000

    Fix the "call to 'abs' is ambiguous" error in line 42
    std::abs() for integers is declared in <cstdlib>, not <cmath>
    
    R=pkotwicz@chromium.org
    BUG=None
    
    Review URL: https://codereview.chromium.org/278303002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@270082 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/workspace/two_step_edge_cycler.cc
+++ ash/wm/workspace/two_step_edge_cycler.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/wm/workspace/two_step_edge_cycler.h"
 
-#include <cmath>
+#include <cstdlib>
 
 namespace ash {
 namespace {
