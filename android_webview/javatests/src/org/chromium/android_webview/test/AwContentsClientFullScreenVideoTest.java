267852c9453709f104e74075b6f8e77a872ccd2f
qsr@chromium.org <qsr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-13 10:48:28 +0000

    Disabling failing video tests.
    TBR=boliu@chromium.org
    NOTRY=true
    NOTREECHECKS=true
    BUG=372748
    
    Review URL: https://codereview.chromium.org/285673003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@270068 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/javatests/src/org/chromium/android_webview/test/AwContentsClientFullScreenVideoTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwContentsClientFullScreenVideoTest.java
@@ -5,11 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.android_webview.test;
 
-import android.test.suitebuilder.annotation.MediumTest;
 import android.view.KeyEvent;
 
 import org.chromium.android_webview.test.util.VideoTestWebServer;
-import org.chromium.base.test.util.Feature;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.content.browser.ContentVideoView;
 import org.chromium.content.browser.test.util.TouchCommon;
 
@@ -19,8 +18,12 @@ import org.chromium.content.browser.test.util.TouchCommon;
 public class AwContentsClientFullScreenVideoTest extends AwTestBase {
     private FullScreenVideoTestAwContentsClient mContentsClient;
 
-    @MediumTest
-    @Feature({"AndroidWebView"})
+    /**
+     * @MediumTest
+     * @Feature({"AndroidWebView"})
+     * http://crbug.com/372748
+     */
+    @DisabledTest
     public void testOnShowAndHideCustomViewWithCallback() throws Throwable {
         doOnShowAndHideCustomViewTest(new Runnable() {
             @Override
@@ -30,8 +33,12 @@ public class AwContentsClientFullScreenVideoTest extends AwTestBase {
         });
     }
 
-    @MediumTest
-    @Feature({"AndroidWebView"})
+    /**
+     * @MediumTest
+     * @Feature({"AndroidWebView"})
+     * http://crbug.com/372748
+     */
+    @DisabledTest
     public void testOnShowAndHideCustomViewWithBackKey() throws Throwable {
         doOnShowAndHideCustomViewTest(new Runnable() {
             @Override
