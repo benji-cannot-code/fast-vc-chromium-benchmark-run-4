15e3b60070d3fdf528e5a9abba7d835af7439407
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-13 23:48:34 +0000

    [Telemetry] Enable BitmapUnittests now that the binary is checked in.
    BUG=
    
    Review URL: https://codereview.chromium.org/272023004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@270250 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/bitmap_unittest.py
+++ tools/telemetry/telemetry/core/bitmap_unittest.py
@@ -7,7 +7,6 @@ import tempfile
 import os
 import unittest
 
-from telemetry import test
 from telemetry.core import bitmap
 from telemetry.core import util
 
@@ -98,7 +97,6 @@ class BitmapTest(unittest.TestCase):
     new_file = bitmap.Bitmap.FromPngFile(temp_file)
     self.assertTrue(orig.IsEqual(new_file))
 
-  @test.Disabled
   def testWriteCroppedBmpToPngFile(self):
     pixels = [255,0,0, 255,255,0, 0,0,0,
               255,255,0, 0,255,0, 0,0,0]
@@ -144,7 +142,6 @@ class BitmapTest(unittest.TestCase):
     diff_bmp.GetPixelColor(2, 1).AssertIsRGB(255, 255, 255)
     diff_bmp.GetPixelColor(2, 2).AssertIsRGB(255, 255, 255)
 
-  @test.Disabled
   def testGetBoundingBox(self):
     pixels = [0,0,0, 0,0,0, 0,0,0, 0,0,0,
               0,0,0, 1,0,0, 1,0,0, 0,0,0,
@@ -158,7 +155,6 @@ class BitmapTest(unittest.TestCase):
     self.assertEquals(box, None)
     self.assertEquals(count, 0)
 
-  @test.Disabled
   def testCrop(self):
     pixels = [0,0,0, 1,0,0, 2,0,0, 3,0,0,
               0,1,0, 1,1,0, 2,1,0, 3,1,0,
@@ -172,7 +168,6 @@ class BitmapTest(unittest.TestCase):
     bmp.GetPixelColor(1, 0).AssertIsRGB(2, 2, 0)
     self.assertEquals(bmp.pixels, bytearray([1,2,0, 2,2,0]))
 
-  @test.Disabled
   def testHistogram(self):
     pixels = [1,2,3, 1,2,3, 1,2,3, 1,2,3,
               1,2,3, 8,7,6, 5,4,6, 1,2,3,
@@ -192,7 +187,6 @@ class BitmapTest(unittest.TestCase):
     self.assertEquals(histogram.b[3], 0)
     self.assertEquals(histogram.b[6], 4)
 
-  @test.Disabled
   def testHistogramIgnoreColor(self):
     pixels = [1,2,3, 1,2,3, 1,2,3, 1,2,3,
               1,2,3, 8,7,6, 5,4,6, 1,2,3,
@@ -209,7 +203,6 @@ class BitmapTest(unittest.TestCase):
     self.assertEquals(histogram.b[3], 0)
     self.assertEquals(histogram.b[6], 4)
 
-  @test.Disabled
   def testHistogramIgnoreColorTolerance(self):
     pixels = [1,2,3, 4,5,6,
               7,8,9, 8,7,6]
@@ -229,7 +222,6 @@ class BitmapTest(unittest.TestCase):
     self.assertEquals(histogram.b[6], 2)
     self.assertEquals(histogram.b[9], 1)
 
-  @test.Disabled
   def testHistogramDistanceIgnoreColor(self):
     pixels = [1,2,3, 1,2,3,
               1,2,3, 1,2,3]
