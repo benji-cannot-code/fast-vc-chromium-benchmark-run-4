f90c36cea8cdc807c32d655feb62b0fedf3af381
lazyboy@chromium.org <lazyboy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-14 10:10:51 +0000

    <webview>: Make printing work from within webview. This would bring system print dialog when one calls window.print() from within the <webview>. We used to not create browser/ side counterparts for <webview>, this CL does that.
    TBD:
    1. What about chrome's internal print preview?
    2. If we print an embedder with a guest, overlaying the guest's content along with the embedder is necessary, this will not work yet.
    
    BUG=286487
    Test=Call window.print() from <webview>'s context, see it work.
    
    Review URL: https://codereview.chromium.org/281623008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@270373 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/guest_view/web_view/web_view_guest.cc
+++ chrome/browser/guest_view/web_view/web_view_guest.cc
@@ -45,6 +45,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/web/WebFindOptions.h"
 #include "ui/base/models/simple_menu_model.h"
 
+#if defined(ENABLE_PRINTING)
+#if defined(ENABLE_FULL_PRINTING)
+#include "chrome/browser/printing/print_preview_message_handler.h"
+#include "chrome/browser/printing/print_view_manager.h"
+#else
+#include "chrome/browser/printing/print_view_manager_basic.h"
+#endif  // defined(ENABLE_FULL_PRINTING)
+#endif  // defined(ENABLE_PRINTING)
+
 #if defined(ENABLE_PLUGINS)
 #include "chrome/browser/guest_view/web_view/plugin_permission_helper.h"
 #endif
@@ -128,6 +137,14 @@ void AttachWebViewHelpers(WebContents* contents) {
 #if defined(ENABLE_PLUGINS)
   PluginPermissionHelper::CreateForWebContents(contents);
 #endif
+#if defined(ENABLE_PRINTING)
+#if defined(ENABLE_FULL_PRINTING)
+  printing::PrintViewManager::CreateForWebContents(contents);
+  printing::PrintPreviewMessageHandler::CreateForWebContents(contents);
+#else
+  printing::PrintViewManagerBasic::CreateForWebContents(contents);
+#endif  // defined(ENABLE_FULL_PRINTING)
+#endif  // defined(ENABLE_PRINTING)
 }
 
 }  // namespace
