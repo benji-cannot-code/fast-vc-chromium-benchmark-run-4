67a540fb145e1d11df8c5833da75cd56f7041d3a
zerny@chromium.org <zerny@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-14 09:53:58 +0000

    Oilpan: Use transition types for HTMLAnchorElement.
    BUG=357163
    R=haraken@chromium.org
    
    Review URL: https://codereview.chromium.org/271593011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174014 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLAnchorElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLAnchorElement.cpp
@@ -71,11 +71,11 @@ void preconnectToURL(const KURL& url, blink::WebPreconnectMotivation motivation)
 
 }
 
-class HTMLAnchorElement::PrefetchEventHandler {
+class HTMLAnchorElement::PrefetchEventHandler FINAL : public NoBaseWillBeGarbageCollected<HTMLAnchorElement::PrefetchEventHandler> {
 public:
-    static PassOwnPtr<PrefetchEventHandler> create(HTMLAnchorElement* anchorElement)
+    static PassOwnPtrWillBeRawPtr<PrefetchEventHandler> create(HTMLAnchorElement* anchorElement)
     {
-        return adoptPtr(new HTMLAnchorElement::PrefetchEventHandler(anchorElement));
+        return adoptPtrWillBeNoop(new HTMLAnchorElement::PrefetchEventHandler(anchorElement));
     }
 
     void reset();
@@ -83,6 +83,8 @@ public:
     void handleEvent(Event* e);
     void didChangeHREF() { m_hadHREFChanged = true; }
     bool hasIssuedPreconnect() const { return m_hasIssuedPreconnect; }
+
+    void trace(Visitor* visitor) { visitor->trace(m_anchorElement); }
 
 private:
     explicit PrefetchEventHandler(HTMLAnchorElement*);
@@ -97,7 +99,7 @@ private:
     bool shouldPrefetch(const KURL&);
     void prefetch(blink::WebPreconnectMotivation);
 
-    HTMLAnchorElement* m_anchorElement;
+    RawPtrWillBeMember<HTMLAnchorElement> m_anchorElement;
     double m_mouseOverTimestamp;
     double m_mouseDownTimestamp;
     double m_tapDownTimestamp;
@@ -581,6 +583,12 @@ void HTMLAnchorElement::PrefetchEventHandler::prefetch(blink::WebPreconnectMotiv
 bool HTMLAnchorElement::isInteractiveContent() const
 {
     return isLink();
+}
+
+void HTMLAnchorElement::trace(Visitor* visitor)
+{
+    visitor->trace(m_prefetchEventHandler);
+    HTMLElement::trace(visitor);
 }
 
 }
--- third_party/WebKit/Source/core/html/HTMLAnchorElement.h
+++ third_party/WebKit/Source/core/html/HTMLAnchorElement.h
@@ -82,6 +82,8 @@ public:
     LinkHash visitedLinkHash() const;
     void invalidateCachedVisitedLinkHash() { m_cachedVisitedLinkHash = 0; }
 
+    virtual void trace(Visitor*) OVERRIDE;
+
 protected:
     HTMLAnchorElement(const QualifiedName&, Document&);
 
@@ -109,7 +111,7 @@ private:
     PrefetchEventHandler* prefetchEventHandler();
 
     uint32_t m_linkRelations;
-    OwnPtr<PrefetchEventHandler> m_prefetchEventHandler;
+    OwnPtrWillBeMember<PrefetchEventHandler> m_prefetchEventHandler;
     mutable LinkHash m_cachedVisitedLinkHash;
 };
 
