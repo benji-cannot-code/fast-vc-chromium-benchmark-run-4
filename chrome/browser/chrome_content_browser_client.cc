851d61ae078ef4d8874eb9893c258d28d58bb655
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-14 04:01:43 +0000

    Remove obsolete kiosk new window blocking code.
    New window blocking for kiosk is done by a browser list observer now.
    The blocking code in CanCreateWindow is no longer needed. And it prevents
    webview's newwindow event being fired in kiosk mode.
    
    BUG=373023
    
    Review URL: https://codereview.chromium.org/281903002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@270326 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/threading/sequenced_worker_pool.h"
-#include "chrome/browser/app_mode/app_mode_utils.h"
 #include "chrome/browser/browser_about_handler.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/browser_shutdown.h"
@@ -2177,12 +2176,6 @@ bool ChromeContentBrowserClient::CanCreateWindow(
       *no_javascript_access = true;
 
     return true;
-  }
-
-  // No new browser window (popup or tab) in app mode.
-  if (container_type == WINDOW_CONTAINER_TYPE_NORMAL &&
-      chrome::IsRunningInForcedAppMode()) {
-    return false;
   }
 
   if (is_guest)
