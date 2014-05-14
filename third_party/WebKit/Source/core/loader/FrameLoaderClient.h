8acf032c22ee037db46969dbacb999f609be8c61
dcheng@chromium.org <dcheng@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-14 16:13:56 +0000

    Remove a stray typedef
    BUG=none
    
    Review URL: https://codereview.chromium.org/286773003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174030 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/FrameLoaderClient.h
+++ third_party/WebKit/Source/core/loader/FrameLoaderClient.h
@@ -40,8 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/Forward.h"
 #include "wtf/Vector.h"
 
-typedef class _jobject* jobject;
-
 namespace v8 {
 class Context;
 template<class T> class Handle;
