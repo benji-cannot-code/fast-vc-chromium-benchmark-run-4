463792f405be88d1802a85de8c60c7dd7c148f40
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-14 23:33:21 +0000

    Fix memory leak in BindingsSampleTest.Basic
    BUG=370560
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/281843002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@270503 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/public/cpp/bindings/tests/sample_service_unittest.cc
+++ mojo/public/cpp/bindings/tests/sample_service_unittest.cc
@@ -353,6 +353,8 @@ TEST(BindingsSampleTest, Basic) {
 
   PortPtr port;
   service->Frobinate(foo, Service::BAZ_EXTRA, port.Pass());
+
+  delete service;
 }
 
 TEST(BindingsSampleTest, DefaultValues) {
