1e804316df0bfa8280fdf502b0d3340a4c37b5f5
noms@chromium.org <noms@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-14 12:03:07 +0000

    Send out new-style notifications when the profile photos have been downloaded.
    The User Manager tries to live in the brave new world of new-style notifications,
    however the ProfileInfoCache only notifies the old-style NotificationService that
    the profile photos have finished being loaded from disk/downloaded from gaia.
    
    BUG=354533,340478
    
    Review URL: https://codereview.chromium.org/270343003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@270387 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_avatar_downloader.cc
+++ chrome/browser/profiles/profile_avatar_downloader.cc
@@ -19,8 +19,10 @@ const char kHighResAvatarDownloadUrlPrefix[] =
 
 ProfileAvatarDownloader::ProfileAvatarDownloader(
     size_t icon_index,
+    const base::FilePath& profile_path,
     ProfileInfoCache* cache)
     : icon_index_(icon_index),
+      profile_path_(profile_path),
       cache_(cache) {
   GURL url(std::string(kHighResAvatarDownloadUrlPrefix) +
            profiles::GetDefaultAvatarIconFileNameAtIndex(icon_index));
@@ -52,5 +54,6 @@ void ProfileAvatarDownloader::OnFetchComplete(const GURL url,
   gfx::Image image = gfx::Image::CreateFrom1xBitmap(*bitmap);
   cache_->SaveAvatarImageAtPath(&image,
       profiles::GetDefaultAvatarIconFileNameAtIndex(icon_index_),
-      profiles::GetPathOfHighResAvatarAtIndex(icon_index_));
+      profiles::GetPathOfHighResAvatarAtIndex(icon_index_),
+      profile_path_);
 }
