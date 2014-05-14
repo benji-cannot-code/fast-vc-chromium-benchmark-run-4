a845aaea609d89cf0721b87b6beefdd4e9abbc59
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-14 04:30:30 +0000

    drive: ResourceMetadataStorage behaves as if FileCacheEntry is merged to ResourceEntry
    DB layout is not changed yet.
    
    After this change, all user code should be changed to stop using PutCacheEntry, GetCacheEntry, RemoveCacheEntry, and GetCacheEntryIterator.
    
    BUG=275271
    TEST=unit_tests
    
    Review URL: https://codereview.chromium.org/289453002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@270333 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/resource_metadata_storage.h
+++ chrome/browser/chromeos/drive/resource_metadata_storage.h
@@ -57,9 +57,6 @@ class ResourceMetadataStorage {
     // Returns the entry currently pointed by this object.
     const ResourceEntry& GetValue() const;
 
-    // Gets the cache entry which corresponds to |entry_| if available.
-    bool GetCacheEntry(FileCacheEntry* cache_entry);
-
     // Advances to the next entry.
     void Advance();
 
@@ -67,6 +64,9 @@ class ResourceMetadataStorage {
     bool HasError() const;
 
    private:
+    // Gets the cache entry which corresponds to |entry_| if available.
+    bool GetCacheEntry(FileCacheEntry* cache_entry);
+
     ResourceEntry entry_;
     scoped_ptr<leveldb::Iterator> it_;
 
