af69865d378151e0db05d5745258c81aef0b6412
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-14 10:27:58 +0000

    DevTools: Move PieChart, FlameChart and CPUProfileModel out of "components"
    These types are ui- and sdk-related, having nothing to do with components
    
    R=pfeldman, vsevik
    
    Review URL: https://codereview.chromium.org/280803002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174017 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/timeline/TimelinePanel.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TimelinePanel.js
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-importScript("../components/CPUProfileModel.js");
+importScript("../sdk/CPUProfileModel.js");
 importScript("CountersGraph.js");
 importScript("MemoryCountersGraph.js");
 importScript("TimelineModel.js");
