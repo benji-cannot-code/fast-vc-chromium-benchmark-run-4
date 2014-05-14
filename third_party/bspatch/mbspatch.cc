fbee4f3c779b25381f56c780a479b0446d99945b
deymo@chromium.org <deymo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-14 00:27:21 +0000

    bspatch: Report errors on MBS_ApplyPatch.
    ApplyBinaryPatch() returns an integer value different from 0 on
    error when opening files. Nevertheless, it doesn't report the errors
    when they happen on the underlying MBS_ApplyPatch() function.
    
    BUG=None
    TEST=Manually compiled and called ApplyBinaryPatch with an invalid patch.
    Function returns error instead of 0.
    
    Review URL: https://codereview.chromium.org/284733002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@270266 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/bspatch/mbspatch.cc
+++ third_party/bspatch/mbspatch.cc
@@ -87,7 +87,7 @@ MBS_ReadHeader(int fd, MBSPatchHeader *header)
 
   return OK;
 }
-         
+
 int
 MBS_ApplyPatch(const MBSPatchHeader *header, int patchfd,
                unsigned char *fbuffer, int filefd)
@@ -264,7 +264,7 @@ int ApplyBinaryPatch(const wchar_t *old_file, const wchar_t *patch_file,
       break;
     }
 
-    MBS_ApplyPatch(&header, pfd, buf, nfd);
+    ret = MBS_ApplyPatch(&header, pfd, buf, nfd);
   } while (0);
 
   free(buf);
