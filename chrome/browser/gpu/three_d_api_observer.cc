a1fe0f7b3e9186781ff65c94562960bcb3b09045
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-14 03:06:20 +0000

    Move ThreeDAPIObserver from chrome/browser to chrome/browser/gpu.
    Review URL: https://codereview.chromium.org/277623003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@270315 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gpu/three_d_api_observer.cc
+++ chrome/browser/gpu/three_d_api_observer.cc
@@ -1,9 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/three_d_api_observer.h"
+#include "chrome/browser/gpu/three_d_api_observer.h"
 
 #include "base/metrics/histogram.h"
 #include "chrome/browser/infobars/confirm_infobar_delegate.h"
--- chrome/browser/gpu/three_d_api_observer.h
+++ chrome/browser/gpu/three_d_api_observer.h
@@ -1,10 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_THREE_D_API_OBSERVER_H_
-#define CHROME_BROWSER_THREE_D_API_OBSERVER_H_
+#ifndef CHROME_BROWSER_GPU_THREE_D_API_OBSERVER_H_
+#define CHROME_BROWSER_GPU_THREE_D_API_OBSERVER_H_
 
 #include "base/compiler_specific.h"
 #include "content/public/browser/gpu_data_manager_observer.h"
@@ -23,4 +23,4 @@ class ThreeDAPIObserver : public content::GpuDataManagerObserver {
   DISALLOW_COPY_AND_ASSIGN(ThreeDAPIObserver);
 };
 
-#endif  // CHROME_BROWSER_THREE_D_API_OBSERVER_H_
+#endif  // CHROME_BROWSER_GPU_THREE_D_API_OBSERVER_H_
