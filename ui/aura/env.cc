446e519a3a1644c933af08ec8852d904b2f1f339
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-14 04:06:51 +0000

    Makes Env thread local
    This will enable aura to be used on multiple threads.
    
    BUG=none
    TEST=none
    R=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/285793002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@270330 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/env.cc
+++ ui/aura/env.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/aura/env.h"
 
+#include "base/lazy_instance.h"
+#include "base/threading/thread_local.h"
 #include "ui/aura/env_observer.h"
 #include "ui/aura/input_state_lookup.h"
 #include "ui/events/event_target_iterator.h"
@@ -12,41 +14,34 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace aura {
 
-// static
-Env* Env::instance_ = NULL;
+namespace {
+
+// Env is thread local so that aura may be used on multiple threads.
+base::LazyInstance<base::ThreadLocalPointer<Env> >::Leaky lazy_tls_ptr =
+    LAZY_INSTANCE_INITIALIZER;
+
+}  // namespace
 
 ////////////////////////////////////////////////////////////////////////////////
 // Env, public:
 
-Env::Env()
-    : mouse_button_flags_(0),
-      is_touch_down_(false),
-      input_state_lookup_(InputStateLookup::Create().Pass()) {
-}
-
-Env::~Env() {
-  FOR_EACH_OBSERVER(EnvObserver, observers_, OnWillDestroyEnv());
-}
-
-//static
+// static
 void Env::CreateInstance(bool create_event_source) {
-  if (!instance_) {
-    instance_ = new Env;
-    instance_->Init(create_event_source);
-  }
+  if (!lazy_tls_ptr.Pointer()->Get())
+    (new Env())->Init(create_event_source);
 }
 
 // static
 Env* Env::GetInstance() {
-  DCHECK(instance_) << "Env::CreateInstance must be called before getting "
-                       "the instance of Env.";
-  return instance_;
+  Env* env = lazy_tls_ptr.Pointer()->Get();
+  DCHECK(env) << "Env::CreateInstance must be called before getting the "
+                 "instance of Env.";
+  return env;
 }
 
 // static
 void Env::DeleteInstance() {
-  delete instance_;
-  instance_ = NULL;
+  delete lazy_tls_ptr.Pointer()->Get();
 }
 
 void Env::AddObserver(EnvObserver* observer) {
@@ -64,6 +59,20 @@ bool Env::IsMouseButtonDown() const {
 
 ////////////////////////////////////////////////////////////////////////////////
 // Env, private:
+
+Env::Env()
+    : mouse_button_flags_(0),
+      is_touch_down_(false),
+      input_state_lookup_(InputStateLookup::Create().Pass()) {
+  DCHECK(lazy_tls_ptr.Pointer()->Get() == NULL);
+  lazy_tls_ptr.Pointer()->Set(this);
+}
+
+Env::~Env() {
+  FOR_EACH_OBSERVER(EnvObserver, observers_, OnWillDestroyEnv());
+  DCHECK_EQ(this, lazy_tls_ptr.Pointer()->Get());
+  lazy_tls_ptr.Pointer()->Set(NULL);
+}
 
 void Env::Init(bool create_event_source) {
   if (create_event_source && !ui::PlatformEventSource::GetInstance())
