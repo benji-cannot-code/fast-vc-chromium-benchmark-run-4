b42ffa4f6d8eb37aae2d04387c525945a8984746
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-14 23:21:20 +0000

    Improved ServiceDiscoveryClientMdns restart logic to avoid crashes.
    BUG=369549
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/284843009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@270500 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/local_discovery/service_discovery_client_mdns.h
+++ chrome/browser/local_discovery/service_discovery_client_mdns.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/cancelable_callback.h"
+#include "base/observer_list.h"
 #include "chrome/browser/local_discovery/service_discovery_shared_client.h"
 #include "chrome/common/local_discovery/service_discovery_client.h"
 #include "net/base/network_change_notifier.h"
@@ -51,11 +52,12 @@ class ServiceDiscoveryClientMdns
   void OnMdnsInitialized(bool success);
   void ReportSuccess();
   void InvalidateWeakPtrs();
-  void Reset();
+  void OnBeforeMdnsDestroy();
+  void DestroyMdns();
 
   bool PostToMdnsThread(const base::Closure& task);
 
-  std::set<Proxy*> proxies_;
+  ObserverList<Proxy, true> proxies_;
 
   scoped_refptr<base::SequencedTaskRunner> mdns_runner_;
 
