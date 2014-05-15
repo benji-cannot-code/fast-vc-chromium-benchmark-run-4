6650686bd3afc0e364706f1f8f4f2359a7f67729
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-15 01:31:19 +0000

    Fix include in metrics_reporting_scheduler.cc
    BUG=371469
    
    Review URL: https://codereview.chromium.org/96423007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@270561 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metrics/metrics_reporting_scheduler.cc
+++ chrome/browser/metrics/metrics_reporting_scheduler.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/metrics/metrics_reporting_scheduler.h"
 
 #include "base/compiler_specific.h"
-#include "chrome/browser/metrics/metrics_service.h"
+#include "base/metrics/histogram.h"
 
 using base::TimeDelta;
 
