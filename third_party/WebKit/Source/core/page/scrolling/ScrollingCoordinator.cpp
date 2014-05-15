90fcaefa9cf6560c910036f15d96c332777c4039
skyostil@chromium.org <skyostil@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-15 17:39:48 +0000

    Fix crash when closing page with a scroll event handler
    This patches fixes a null dereference when the final scroll event
    listener is removed from the EventHandlerRegistry during WebView
    teardown. More specifically, if ScrollingCoordinator is told to update
    its knowledge about scroll event listeners after the main frame has
    become detached from its respective view, the coordinator will try
    to deref a null pointer when looking for the root scrolling layer.
    
    This patch adds a guard against a detached frame into
    ScrollingCoordinator's scroll handler logic. The wheel and touch event
    handler functions are also guarded as they will soon be triggered by the
    same mechanism in EventHandlerRegistry.
    
    TEST=ScrollingCoordinatorChromiumTest.updateEventHandlersDuringTeardown
    BUG=371905,359566
    
    Review URL: https://codereview.chromium.org/275563006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174099 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/scrolling/ScrollingCoordinator.cpp
+++ third_party/WebKit/Source/core/page/scrolling/ScrollingCoordinator.cpp
@@ -621,7 +621,7 @@ void ScrollingCoordinator::touchEventTargetRectsDidChange()
         return;
 
     // Wait until after layout to update.
-    if (m_page->mainFrame()->view()->needsLayout())
+    if (!m_page->mainFrame()->view() || m_page->mainFrame()->view()->needsLayout())
         return;
 
     // FIXME: scheduleAnimation() is just a method of forcing the compositor to realize that it
@@ -660,6 +660,8 @@ void ScrollingCoordinator::updateHaveWheelEventHandlers()
 {
     ASSERT(isMainThread());
     ASSERT(m_page);
+    if (!m_page->mainFrame()->view())
+        return;
 
     if (WebLayer* scrollLayer = toWebLayer(m_page->mainFrame()->view()->layerForScrolling())) {
         unsigned wheelEventHandlerCount = 0;
@@ -676,6 +678,8 @@ void ScrollingCoordinator::updateHaveScrollEventHandlers()
 {
     ASSERT(isMainThread());
     ASSERT(m_page);
+    if (!m_page->mainFrame()->view())
+        return;
 
     // Currently the compositor only cares whether there are scroll handlers anywhere on the page
     // instead on a per-layer basis. We therefore only update this information for the root
