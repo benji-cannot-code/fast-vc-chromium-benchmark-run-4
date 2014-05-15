850d723a773a82d60c9d7fbb1d1895b517e1f702
tapted@chromium.org <tapted@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-15 07:35:02 +0000

    Revert of Add a whole-app-list unit test. (https://codereview.chromium.org/210083009/)
    Reason for revert:
    Despite all the green, and earlier full compile on the same bot, now fails compile on Linux Builder.
    
    ../../ui/app_list/views/app_list_view_unittest.cc:239:8:error: 'host' was not declared in this scope
    
    link: http://build.chromium.org/p/chromium.linux/builders/Linux%20Builder/builds/31637
    
    Original issue's description:
    > Add a whole-app-list unit test.
    > 
    > This establishes a test harness for doing tests that touch on multiple
    > parts of the app launcher UI, or that need coverage for the various app
    > launcher configurations: Ash or Desktop; regular or experimental
    > "landscape" mode.
    > 
    > It currently just tests displaying and closing the app launcher in its
    > default state with a flat (no folders), 2- or 3-page model.
    > 
    > BUG=169114
    > TEST=Added unit tests AppListViewTest{Aura,Desktop}.*
    > 
    > Previously committed: http://crrev.com/260762 http://crrev.com/260803
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=270615
    
    TBR=tapted@chromium.org
    NOTRY=true
    NOTREECHECKS=true
    
    Review URL: https://codereview.chromium.org/290433003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@270619 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/app_list/views/app_list_view.cc
+++ ui/app_list/views/app_list_view.cc
@@ -54,6 +54,8 @@ namespace app_list {
 
 namespace {
 
+void (*g_next_paint_callback)();
+
 // The margin from the edge to the speech UI.
 const int kSpeechUIMargin = 12;
 
@@ -205,9 +207,9 @@ gfx::Size AppListView::GetPreferredSize() {
 
 void AppListView::Paint(gfx::Canvas* canvas, const views::CullSet& cull_set) {
   views::BubbleDelegateView::Paint(canvas, cull_set);
-  if (!next_paint_callback_.is_null()) {
-    next_paint_callback_.Run();
-    next_paint_callback_.Reset();
+  if (g_next_paint_callback) {
+    g_next_paint_callback();
+    g_next_paint_callback = NULL;
   }
 }
 
@@ -249,8 +251,8 @@ void AppListView::RemoveObserver(AppListViewObserver* observer) {
 }
 
 // static
-void AppListView::SetNextPaintCallback(const base::Closure& callback) {
-  next_paint_callback_ = callback;
+void AppListView::SetNextPaintCallback(void (*callback)()) {
+  g_next_paint_callback = callback;
 }
 
 #if defined(OS_WIN)
