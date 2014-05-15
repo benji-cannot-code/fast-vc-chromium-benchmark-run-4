03e14664bdb7b437f9587d490059012a2c32a737
vabr@chromium.org <vabr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-15 17:23:24 +0000

    Password manager internals page: Introduce logger in browser (readability review)
    This CL contains files from components/* added by vabr@ during implementing a new logger framework in these CLs:
    * https://codereview.chromium.org/216183008/, which landed as http://crrev.com/262671, and
    * https://codereview.chromium.org/228263004/, which landed as http://crrev.com/263002.
    The CL also contains changes made by vabr@ in https://codereview.chromium.org/235623002/.
    
    Design doc for the above changes is https://docs.google.com/document/d/1ArDhTo0w-8tOPiTwqM1gG6ZGqODo8UTpXlJjjnCNK4s/edit?usp=sharing
    
    
    Further, this CL contains files from chrome/browser/extensions/api/web_request/* added by vabr@ back in 2012. Most of the code there is still vabr@'s original code. For more details please use, e.g., the Chromium code search https://code.google.com/p/chromium/codesearch#chromium/src/chrome/browser/extensions/api/web_request/&sq=package:chromium and the View in->Subversion Annotate link for particular files.
    
    
    The purpose of this CL is to host the C++ readability review for vabr@, as indicated in https://wiki.corp.google.com/twiki/bin/view/Main/ChromeReadabilityReview .
    
    BUG=347927,91191
    
    Review URL: https://codereview.chromium.org/229683006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@270715 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/web_request/upload_data_presenter.cc
+++ chrome/browser/extensions/api/web_request/upload_data_presenter.cc
@@ -52,11 +52,7 @@ void AppendKeyValuePair(const char* key,
 
 }  // namespace subtle
 
-// Implementation of UploadDataPresenter.
-
 UploadDataPresenter::~UploadDataPresenter() {}
-
-// Implementation of RawDataPresenter.
 
 RawDataPresenter::RawDataPresenter()
   : success_(true),
@@ -103,8 +99,6 @@ void RawDataPresenter::FeedNextFile(const std::string& filename) {
                              new base::StringValue(filename),
                              list_.get());
 }
-
-// Implementation of ParsedDataPresenter.
 
 ParsedDataPresenter::ParsedDataPresenter(const net::URLRequest& request)
   : parser_(FormDataParser::Create(request)),
