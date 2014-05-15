835d6ab01919dc4d18c95189070580f4849c45e7
mariakhomenko@chromium.org <mariakhomenko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-15 23:52:20 +0000

    Upstream AutocompleteController and related classes.
    Upstreams AutocompleteController and OmniboxSuggestion on java side.
    Upstreams autocomplete_bridge.cc and renames it to
    autocomplete_controller_android.cc.
    
    BUG=222130
    
    Review URL: https://codereview.chromium.org/285633002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@270861 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/android/chrome_jni_registrar.cc
+++ chrome/browser/android/chrome_jni_registrar.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/logo_bridge.h"
 #include "chrome/browser/android/most_visited_sites.h"
 #include "chrome/browser/android/new_tab_page_prefs.h"
+#include "chrome/browser/android/omnibox/autocomplete_controller_android.h"
 #include "chrome/browser/android/omnibox/omnibox_prerender.h"
 #include "chrome/browser/android/password_authentication_manager.h"
 #include "chrome/browser/android/password_ui_view_android.h"
@@ -97,6 +98,7 @@ static base::android::RegistrationMethod kChromeRegisteredMethods[] = {
     AndroidProfileOAuth2TokenService::Register },
   { "AppBannerManager", banners::RegisterAppBannerManager },
   { "ApplicationLifetime", RegisterApplicationLifetimeAndroid },
+  { "AutocompleteControllerAndroid", RegisterAutocompleteControllerAndroid },
   { "AutofillDialogControllerAndroid",
     autofill::AutofillDialogControllerAndroid::
         RegisterAutofillDialogControllerAndroid },
