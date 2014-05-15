4a0b02d59af23e12cac6cb293b7a74a2e13c1063
tbarzic@chromium.org <tbarzic@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-15 20:34:37 +0000

    Improve process output watcher's handling of multi-byte UTF8 characters
    Speciffically, handle case when a non ASCII UTF8 character is read
    partially from the process output. Instead of reporting read character
    bytes imediatelly, cache them until the rest of the character is read
    from the process output.
    
    BUG=278340
    TEST=in crosh (see comment #1 in the bug):
    $python
    >>> print(u"\u20ac" * 10000)
    Verify a series of EURO signs is displayed (without "unknown" characters)
    
    Review URL: https://codereview.chromium.org/261743002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@270799 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/process_proxy/process_proxy_unittest.cc
+++ chromeos/process_proxy/process_proxy_unittest.cc
@@ -6,13 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <gtest/gtest.h>
 
 #include <string>
-#include <sys/wait.h>
 
 #include "base/bind.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/message_loop/message_loop.h"
 #include "base/process/kill.h"
-#include "base/synchronization/waitable_event.h"
 #include "base/threading/thread.h"
 #include "chromeos/process_proxy/process_proxy_registry.h"
 
