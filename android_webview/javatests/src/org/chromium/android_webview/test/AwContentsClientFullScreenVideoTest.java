eb9ff58fcb00610f76edfed9c423ab2472056607
qinmin@chromium.org <qinmin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-15 00:43:28 +0000

    reenable failing video tests
    The tests should no longer fail after the offending CL is reverted in blink
    
    BUG=372748
    
    Review URL: https://codereview.chromium.org/286913002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@270544 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/javatests/src/org/chromium/android_webview/test/AwContentsClientFullScreenVideoTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwContentsClientFullScreenVideoTest.java
@@ -5,10 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.android_webview.test;
 
+import android.test.suitebuilder.annotation.MediumTest;
 import android.view.KeyEvent;
 
 import org.chromium.android_webview.test.util.VideoTestWebServer;
-import org.chromium.base.test.util.DisabledTest;
+import org.chromium.base.test.util.Feature;
 import org.chromium.content.browser.ContentVideoView;
 import org.chromium.content.browser.test.util.TouchCommon;
 
@@ -18,12 +19,8 @@ import org.chromium.content.browser.test.util.TouchCommon;
 public class AwContentsClientFullScreenVideoTest extends AwTestBase {
     private FullScreenVideoTestAwContentsClient mContentsClient;
 
-    /**
-     * @MediumTest
-     * @Feature({"AndroidWebView"})
-     * http://crbug.com/372748
-     */
-    @DisabledTest
+    @MediumTest
+    @Feature({"AndroidWebView"})
     public void testOnShowAndHideCustomViewWithCallback() throws Throwable {
         doOnShowAndHideCustomViewTest(new Runnable() {
             @Override
@@ -33,12 +30,8 @@ public class AwContentsClientFullScreenVideoTest extends AwTestBase {
         });
     }
 
-    /**
-     * @MediumTest
-     * @Feature({"AndroidWebView"})
-     * http://crbug.com/372748
-     */
-    @DisabledTest
+    @MediumTest
+    @Feature({"AndroidWebView"})
     public void testOnShowAndHideCustomViewWithBackKey() throws Throwable {
         doOnShowAndHideCustomViewTest(new Runnable() {
             @Override
