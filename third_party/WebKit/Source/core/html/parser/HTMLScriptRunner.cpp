729e86c1ab01bd106d21435e1bc115c5de754e59
morrita@chromium.org <morrita@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-15 07:20:41 +0000

    Revert 174019 "Make ScriptLoader into a ResourceOwner"
    > Make ScriptLoader into a ResourceOwner
    > 
    > This change turns ScriptLoader a ResourceOwner, that ensures that
    > it subscribes Resource if the owner has a reference to it, and that
    > it unsubscribes the Resource otherwise. Before this change,
    > There are some situation where ScriptLoader has a reference to Resource
    > but doesn't subscribe it, that is error prone and left deleted instance
    > being subscribed. ResourceOwner guarantees reference and subscription
    > consistency.
    > 
    > This is the second attempt to land this change. The original one was
    > reverted at r173996 because it caused a lot of crashes. These crahses
    > shouldn't happen with this revised version.
    > 
    > TEST=script-cancel-crash.html
    > BUG=368551,372242
    > TBR=eseidel@chromium.org, japhet@chromium.org, dglazkov@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/288673002
    
    TBR=morrita@chromium.org
    
    Review URL: https://codereview.chromium.org/283333002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174071 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/parser/HTMLScriptRunner.cpp
+++ third_party/WebKit/Source/core/html/parser/HTMLScriptRunner.cpp
@@ -245,9 +245,9 @@ bool HTMLScriptRunner::executeScriptsWaitingForParsing()
     return true;
 }
 
-void HTMLScriptRunner::requestParsingBlockingScript(const ResourcePtr<ScriptResource>& resource, Element* element)
+void HTMLScriptRunner::requestParsingBlockingScript(Element* element)
 {
-    if (!requestPendingScript(resource, m_parserBlockingScript, element))
+    if (!requestPendingScript(m_parserBlockingScript, element))
         return;
 
     ASSERT(m_parserBlockingScript.resource());
@@ -259,26 +259,27 @@ void HTMLScriptRunner::requestParsingBlockingScript(const ResourcePtr<ScriptReso
         watchForLoad(m_parserBlockingScript);
 }
 
-void HTMLScriptRunner::requestDeferredScript(const ResourcePtr<ScriptResource>& resource, Element* element)
+void HTMLScriptRunner::requestDeferredScript(Element* element)
 {
     PendingScript pendingScript;
-    if (!requestPendingScript(resource, pendingScript, element))
+    if (!requestPendingScript(pendingScript, element))
         return;
 
     ASSERT(pendingScript.resource());
     m_scriptsToExecuteAfterParsing.append(pendingScript);
 }
 
-bool HTMLScriptRunner::requestPendingScript(const ResourcePtr<ScriptResource>& resource, PendingScript& pendingScript, Element* script) const
+bool HTMLScriptRunner::requestPendingScript(PendingScript& pendingScript, Element* script) const
 {
     ASSERT(!pendingScript.element());
     pendingScript.setElement(script);
     // This should correctly return 0 for empty or invalid srcValues.
+    ScriptResource* resource = toScriptLoaderIfPossible(script)->resource().get();
     if (!resource) {
         notImplemented(); // Dispatch error event.
         return false;
     }
-    pendingScript.setScriptResource(resource.get());
+    pendingScript.setScriptResource(resource);
     return true;
 }
 
@@ -307,13 +308,13 @@ void HTMLScriptRunner::runScript(Element* script, const TextPosition& scriptStar
         InsertionPointRecord insertionPointRecord(m_host->inputStream());
         NestingLevelIncrementer nestingLevelIncrementer(m_scriptNestingLevel);
 
-        ScriptPrep prep = scriptLoader->prepareScript(scriptStartPosition);
+        scriptLoader->prepareScript(scriptStartPosition);
 
         if (!scriptLoader->willBeParserExecuted())
             return;
 
         if (scriptLoader->willExecuteWhenDocumentFinishedParsing()) {
-            requestDeferredScript(prep.resource(), script);
+            requestDeferredScript(script);
         } else if (scriptLoader->readyToBeParserExecuted()) {
             if (m_scriptNestingLevel == 1) {
                 m_parserBlockingScript.setElement(script);
@@ -323,7 +324,7 @@ void HTMLScriptRunner::runScript(Element* script, const TextPosition& scriptStar
                 scriptLoader->executeScript(sourceCode);
             }
         } else {
-            requestParsingBlockingScript(prep.resource(), script);
+            requestParsingBlockingScript(script);
         }
     }
 }
