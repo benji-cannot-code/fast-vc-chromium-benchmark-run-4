f7054758f56f4cdde3e450b93f95a9141e6d528d
plundblad@chromium.org <plundblad@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-15 00:44:28 +0000

    ChromeVox migration: Non-test sources.
    This is all ChromeVox js files that are used when building the extension.
    externs (for static checking with closure compiler) will come in a follow-up cl.
    Later cls will also contain build rules and PRESUBMIT chesk for these files.
    
    
    BUG=371692
    R=dmazzoni@chromium.org
    
    Review URL: https://codereview.chromium.org/289703002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@270545 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/resources/chromeos/chromevox/chromevox/messages/spoken_message.js
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview A basic abstraction of messages.
+ *
+ */
+
+goog.provide('cvox.SpokenMessage');
+
+/**
+ * @constructor
+ */
+cvox.SpokenMessage = function() {
+  /** @type {?Number} */
+  this.count = null;
+
+  /** @type {Array} */
+  this.id = null;
+
+  /**
+   * A message that has been already localized and should be sent to tts raw.
+   * @type {?string}
+   */
+  this.raw = null;
+};
---
+++ chrome/browser/resources/chromeos/chromevox/common/buildinfo.js
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+
+/**
+ * @fileoverview Build info for ChromeVox.
+ *
+ * This file may be replaced in package.py for dev builds.
+ * For releases, it is updated by hand with meaningful values.
+ *
+ */
+
+goog.provide('cvox.BuildInfo');
+
+/**
+ * An identified for this build.
+ * @type {string}
+ */
+cvox.BuildInfo.build = 'development build';
