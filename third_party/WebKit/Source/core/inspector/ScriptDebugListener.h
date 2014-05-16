a9bb5334613783a8ecbb78a3afde6b3b0a0d94fa
aandrey@chromium.org <aandrey@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-16 19:12:43 +0000

    DevTools: Encapsulate should-skip-breakpoint check in debugger agent.
    This way we collect debugger stacks (now top call frame) in V8 event handler
    only when we need it, e.g. when framework/blackboxing feature is turned on.
    
    R=pfeldman, yurys
    
    Review URL: https://codereview.chromium.org/285923002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174203 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/ScriptDebugListener.h
+++ third_party/WebKit/Source/core/inspector/ScriptDebugListener.h
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 class ScriptValue;
-class JavaScriptCallFrame;
 
 class ScriptDebugListener {
 public:
@@ -64,13 +63,6 @@ public:
         bool isContentScript;
     };
 
-    virtual ~ScriptDebugListener() { }
-
-    virtual void didParseSource(const String& scriptId, const Script&) = 0;
-    virtual void failedToParseSource(const String& url, const String& data, int firstLine, int errorLine, const String& errorMessage) = 0;
-    virtual void didPause(ScriptState*, const ScriptValue& callFrames, const ScriptValue& exception, const Vector<String>& hitBreakpoints) = 0;
-    virtual void didContinue() = 0;
-
     enum SkipPauseRequest {
         NoSkip,
         Continue,
@@ -78,9 +70,12 @@ public:
         StepOut
     };
 
-    virtual SkipPauseRequest shouldSkipExceptionPause(RefPtr<JavaScriptCallFrame>& topFrame) = 0;
-    virtual SkipPauseRequest shouldSkipBreakpointPause(RefPtr<JavaScriptCallFrame>& topFrame) = 0;
-    virtual SkipPauseRequest shouldSkipStepPause(RefPtr<JavaScriptCallFrame>& topFrame) = 0;
+    virtual ~ScriptDebugListener() { }
+
+    virtual void didParseSource(const String& scriptId, const Script&) = 0;
+    virtual void failedToParseSource(const String& url, const String& data, int firstLine, int errorLine, const String& errorMessage) = 0;
+    virtual SkipPauseRequest didPause(ScriptState*, const ScriptValue& callFrames, const ScriptValue& exception, const Vector<String>& hitBreakpoints) = 0;
+    virtual void didContinue() = 0;
 };
 
 } // namespace WebCore
