ba6aee8af518aacf33646fa414b493480544e894
trchen@chromium.org <trchen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-16 01:46:03 +0000

    Revert of Disable org.chromium.android_webview.test.ExternalVideoSurfaceContainerTest#testEnableVideoOverlayF… (https://codereview.chromium.org/292473002/)
    Reason for revert:
    Forgot to import DisabledTest
    
    Original issue's description:
    > Disable org.chromium.android_webview.test.ExternalVideoSurfaceContainerTest#testEnableVideoOverlayForEmbeddedVideo
    > 
    > The test is still failing on Android Tests (dbg) bots. See:
    > http://build.chromium.org/p/chromium.webkit/builders/Android%20Tests%20%28dbg%29
    > 
    > BUG=372748,372174
    > TBR=boliu
    > NOTRY=true
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=270889
    
    TBR=boliu@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=372748,372174
    
    Review URL: https://codereview.chromium.org/290083003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@270896 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/javatests/src/org/chromium/android_webview/test/ExternalVideoSurfaceContainerTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/ExternalVideoSurfaceContainerTest.java
@@ -98,12 +98,8 @@ public class ExternalVideoSurfaceContainerTest extends AwTestBase {
         });
     }
 
-    /**
-     * @SmallTest
-     * @Feature({"AndroidWebView"})
-     * http://crbug.com/372748
-     */
-    @DisabledTest
+    @SmallTest
+    @Feature({"AndroidWebView"})
     public void testEnableVideoOverlayForEmbeddedVideo() throws Throwable {
         setUpMockExternalVideoSurfaceContainer();
 
