ce52c39db2bdfeb8772b5eca92f1b7ddee7bf7db
enne@chromium.org <enne@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-16 18:38:02 +0000

    Remove isCompositorFramePending plumbing
    Originally added: https://codereview.chromium.org/139103009.
    
    This is only used for one test, and is somewhat a lie because
    WebTestProxy implements its own (very different) animate/composite
    scheduling.  Removing this plumbing makes it possible to better clean up
    old non-composited paint logic inside of WebTestProxy.
    
    Due to changes elsewhere, this test no longer catches the original
    regression.  The scheduleAnimate call inside of RenderLayerCompositor
    that was added in the original patch can be removed and the page still
    paints properly.  In fact, all scheduleAnimate calls and calls to the
    page animator in RLC can be removed and the page still gets updated.
    
    The code in RenderLayerCompositor has changed drastically since m33, and
    if possible I'd like to remove this one-off plumbing that is no longer
    catching any regressions.
    
    TBR=abarth@chromium.org
    BUG=none
    
    Review URL: https://codereview.chromium.org/289873002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174200 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/testing/Internals.cpp
+++ third_party/WebKit/Source/core/testing/Internals.cpp
@@ -2247,16 +2247,6 @@ void Internals::forceCompositingUpdate(Document* document, ExceptionState& excep
     document->frame()->view()->updateLayoutAndStyleForPainting();
 }
 
-bool Internals::isCompositorFramePending(Document* document, ExceptionState& exceptionState)
-{
-    if (!document || !document->renderView()) {
-        exceptionState.throwDOMException(InvalidAccessError, document ? "The document's render view cannot be retrieved." : "The document provided is invalid.");
-        return false;
-    }
-
-    return document->page()->chrome().client().isCompositorFramePending();
-}
-
 void Internals::setZoomFactor(float factor)
 {
     frame()->setPageZoomFactor(factor);
--- third_party/WebKit/Source/core/testing/Internals.h
+++ third_party/WebKit/Source/core/testing/Internals.h
@@ -303,8 +303,6 @@ public:
 
     void forceCompositingUpdate(Document*, ExceptionState&);
 
-    bool isCompositorFramePending(Document*, ExceptionState&);
-
     void setZoomFactor(float);
 
     void setShouldRevealPassword(Element*, bool, ExceptionState&);
