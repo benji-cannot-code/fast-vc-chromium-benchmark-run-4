c93a1504e57e77f9764b27f1b7c103558939cfc7
dtu@chromium.org <dtu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-16 01:01:26 +0000

    [Telemetry] Ensure we try to get browser crash stacks everywhere.
    There were several places we could crash but not try to get a stack. This should
    take care of everything.
    
    BUG=371613, 373385
    R=achuith@chromium.org, dtu@chromium.org
    
    Review URL: https://codereview.chromium.org/282223004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@270884 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/backends/chrome/chrome_browser_backend.py
+++ tools/telemetry/telemetry/core/backends/chrome/chrome_browser_backend.py
@@ -145,7 +145,9 @@ class ChromeBrowserBackend(browser_backend.BrowserBackend):
     try:
       util.WaitFor(self.HasBrowserFinishedLaunching, timeout=30)
     except (util.TimeoutException, exceptions.ProcessGoneException) as e:
-      raise exceptions.BrowserGoneException(self.GetStackTrace())
+      if not self.IsBrowserRunning():
+        raise exceptions.BrowserGoneException(self.browser, e)
+      raise exceptions.BrowserConnectionGoneException(self.browser, e)
 
     def AllExtensionsLoaded():
       # Extension pages are loaded from an about:blank page,
@@ -236,8 +238,8 @@ class ChromeBrowserBackend(browser_backend.BrowserBackend):
       if throw_network_exception:
         raise e
       if not self.IsBrowserRunning():
-        raise exceptions.BrowserGoneException(e)
-      raise exceptions.BrowserConnectionGoneException(e)
+        raise exceptions.BrowserGoneException(self.browser, e)
+      raise exceptions.BrowserConnectionGoneException(self.browser, e)
 
   @property
   def browser_directory(self):
