6c66c7aa9632074eb2024dce02858b6eef5b2e3c
sl.ostapenko@samsung.com <sl.ostapenko@samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-16 15:03:26 +0000

    Use WebScreenInfo members to provide screen orientation information for layout test support.
    BUG=162827
    
    Review URL: https://codereview.chromium.org/284583003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@271017 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/renderer/webkit_test_runner.cc
+++ content/shell/renderer/webkit_test_runner.cc
@@ -229,7 +229,7 @@ void WebKitTestRunner::setDeviceOrientationData(
 
 void WebKitTestRunner::setScreenOrientation(
     const WebScreenOrientationType& orientation) {
-  SetMockScreenOrientation(orientation);
+  SetMockScreenOrientation(render_view(), orientation);
 }
 
 void WebKitTestRunner::resetScreenOrientation() {
