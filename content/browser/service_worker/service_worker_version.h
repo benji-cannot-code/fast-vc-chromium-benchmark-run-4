d229b3f95fb9711f0d03587e0f728d581991101f
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-16 13:18:03 +0000

    Retry: Stop ServiceWorker context when no controllee is associated
    Re-landing https://codereview.chromium.org/272183005
    with leak fix.
    
    BUG=372673
    TBR=michaeln@chromium.org,horo@chromium.org
    
    Review URL: https://codereview.chromium.org/282263008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@271004 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/service_worker/service_worker_version.h
+++ content/browser/service_worker/service_worker_version.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/observer_list.h"
+#include "base/timer/timer.h"
 #include "content/browser/service_worker/embedded_worker_instance.h"
 #include "content/browser/service_worker/service_worker_script_cache_map.h"
 #include "content/common/content_export.h"
@@ -189,7 +190,7 @@ class CONTENT_EXPORT ServiceWorkerVersion
   void AddPendingControllee(ServiceWorkerProviderHost* provider_host);
   void RemovePendingControllee(ServiceWorkerProviderHost* provider_host);
 
-  // Returns if it has (non-pending) controllee.
+  // Returns if it has controllee.
   bool HasControllee() const { return !controllee_map_.empty(); }
 
   // Adds and removes Listeners.
@@ -241,6 +242,8 @@ class CONTENT_EXPORT ServiceWorkerVersion
                                const base::string16& message,
                                const std::vector<int>& sent_message_port_ids);
 
+  void ScheduleStopWorker();
+
   const int64 version_id_;
   int64 registration_id_;
   GURL script_url_;
@@ -262,6 +265,7 @@ class CONTENT_EXPORT ServiceWorkerVersion
   base::WeakPtr<ServiceWorkerContextCore> context_;
   ObserverList<Listener> listeners_;
   ServiceWorkerScriptCacheMap script_cache_map_;
+  base::OneShotTimer<ServiceWorkerVersion> stop_worker_timer_;
 
   base::WeakPtrFactory<ServiceWorkerVersion> weak_factory_;
 
