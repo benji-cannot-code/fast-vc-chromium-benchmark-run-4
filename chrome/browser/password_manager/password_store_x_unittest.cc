0711f87ee8652bede8e87f2eee6672bb6010f640
vasilii@chromium.org <vasilii@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-16 13:28:15 +0000

    Password Login Database: report correct changes from AddLogin().
    BUG=346210
    
    Review URL: https://codereview.chromium.org/283563002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@271005 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/password_manager/password_store_x_unittest.cc
+++ chrome/browser/password_manager/password_store_x_unittest.cc
@@ -30,6 +30,7 @@ using autofill::PasswordForm;
 using content::BrowserThread;
 using password_manager::ContainsAllPasswordForms;
 using password_manager::PasswordStoreChange;
+using password_manager::PasswordStoreChangeList;
 using testing::_;
 using testing::DoAll;
 using testing::ElementsAreArray;
@@ -59,7 +60,9 @@ class FailingBackend : public PasswordStoreX::NativeBackend {
  public:
   virtual bool Init() OVERRIDE { return true; }
 
-  virtual bool AddLogin(const PasswordForm& form) OVERRIDE { return false; }
+  virtual PasswordStoreChangeList AddLogin(const PasswordForm& form) OVERRIDE {
+    return PasswordStoreChangeList();
+  }
   virtual bool UpdateLogin(const PasswordForm& form) OVERRIDE { return false; }
   virtual bool RemoveLogin(const PasswordForm& form) OVERRIDE { return false; }
 
@@ -92,9 +95,10 @@ class MockBackend : public PasswordStoreX::NativeBackend {
  public:
   virtual bool Init() OVERRIDE { return true; }
 
-  virtual bool AddLogin(const PasswordForm& form) OVERRIDE {
+  virtual PasswordStoreChangeList AddLogin(const PasswordForm& form) OVERRIDE {
     all_forms_.push_back(form);
-    return true;
+    PasswordStoreChange change(PasswordStoreChange::ADD, form);
+    return PasswordStoreChangeList(1, change);
   }
 
   virtual bool UpdateLogin(const PasswordForm& form) OVERRIDE {
--- components/password_manager/core/browser/login_database.h
+++ components/password_manager/core/browser/login_database.h
@@ -12,13 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/pickle.h"
 #include "base/strings/string16.h"
+#include "components/password_manager/core/browser/password_store_change.h"
 #include "components/password_manager/core/browser/psl_matching_helper.h"
 #include "sql/connection.h"
 #include "sql/meta_table.h"
-
-namespace autofill {
-struct PasswordForm;
-}  // namespace autofill
 
 namespace password_manager {
 
@@ -37,8 +34,11 @@ class LoginDatabase {
   // Reports usage metrics to UMA.
   void ReportMetrics();
 
-  // Adds |form| to the list of remembered password forms.
-  bool AddLogin(const autofill::PasswordForm& form);
+  // Adds |form| to the list of remembered password forms. Returns the list of
+  // changes applied ({}, {ADD}, {REMOVE, ADD}). If it returns {REMOVE, ADD}
+  // then the REMOVE is associated with the form that was added. Thus only the
+  // primary key columns contain the values associated with the removed form.
+  PasswordStoreChangeList AddLogin(const autofill::PasswordForm& form);
 
   // Updates remembered password form. Returns true on success and sets
   // items_changed (if non-NULL) to the number of logins updated.
@@ -84,8 +84,6 @@ class LoginDatabase {
   bool DeleteAndRecreateDatabaseFile();
 
  private:
-  friend class LoginDatabaseTest;
-
   // Result values for encryption/decryption actions.
   enum EncryptionResult {
     // Success.
@@ -128,10 +126,6 @@ class LoginDatabase {
   // |forms|.
   bool GetAllLoginsWithBlacklistSetting(
       bool blacklisted, std::vector<autofill::PasswordForm*>* forms) const;
-
-  // Serialization routines for vectors.
-  Pickle SerializeVector(const std::vector<base::string16>& vec) const;
-  std::vector<base::string16> DeserializeVector(const Pickle& pickle) const;
 
   base::FilePath db_path_;
   mutable sql::Connection db_;
