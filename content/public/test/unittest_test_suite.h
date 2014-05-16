2b31f36eb521bd6a51d20b25813ab5a3cbe12422
enne@chromium.org <enne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-16 01:35:22 +0000

    Use real PlatformSupport in content unit tests
    Once all pages are force composited by default, this allows these tests
    to not explode when trying to create GraphicsLayers because of a lack of
    Platform::compositorSupport.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/287043002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@270894 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/test/unittest_test_suite.h
+++ content/public/test/unittest_test_suite.h
@@ -14,6 +14,7 @@ class TestSuite;
 }
 
 namespace content {
+class TestWebKitPlatformSupport;
 
 // A special test suite that also initializes WebKit once for all unittests.
 // This is useful for two reasons:
@@ -32,8 +33,7 @@ class UnitTestTestSuite {
   scoped_ptr<base::TestSuite> test_suite_;
 
 #if !defined(OS_IOS)
-  class UnitTestWebKitPlatformSupport;
-  scoped_ptr<UnitTestWebKitPlatformSupport> webkit_platform_support_;
+  scoped_ptr<TestWebKitPlatformSupport> platform_support_;
 #endif
 
   DISALLOW_COPY_AND_ASSIGN(UnitTestTestSuite);
