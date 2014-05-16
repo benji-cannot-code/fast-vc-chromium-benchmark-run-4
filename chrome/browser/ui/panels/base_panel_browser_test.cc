a93fde8434bf8bb1dd5a1f4025bd13d55d450ecd
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-16 01:32:45 +0000

    linux_aura: Reenable a bunch of tests that were disabled.
    BUG=163931
    
    Review URL: https://codereview.chromium.org/288083002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@270892 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/panels/base_panel_browser_test.cc
+++ chrome/browser/ui/panels/base_panel_browser_test.cc
@@ -39,10 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/x/x11_util.h"
 #endif
 
-#if defined(OS_LINUX) && !defined(USE_AURA)
-#include "ui/base/x/active_window_watcher_x.h"
-#endif
-
 #if defined(OS_MACOSX)
 #include "base/mac/scoped_nsautorelease_pool.h"
 #include "chrome/browser/ui/cocoa/run_loop_testing.h"
@@ -623,9 +619,5 @@ std::string BasePanelBrowserTest::MakePanelName(int index) {
 }
 
 bool BasePanelBrowserTest::WmSupportWindowActivation() {
-#if defined(OS_LINUX) && !defined(USE_AURA)
-  return ui::ActiveWindowWatcherX::WMSupportsActivation();
-#else
   return true;
-#endif
 }
