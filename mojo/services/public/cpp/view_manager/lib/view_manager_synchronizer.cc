4ebd535682cd8a9a31a35b572ccccd71611ef9cf
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-16 00:02:08 +0000

    Mojo: Internalize ServiceConnector<>
    Authored by davemoore@chromium.org, see https://codereview.chromium.org/275363002/
    
    R=viettrungluu@chromium.org, darin@chromium.org
    
    Review URL: https://codereview.chromium.org/284113009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@270867 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/services/public/cpp/view_manager/lib/view_manager_synchronizer.cc
+++ mojo/services/public/cpp/view_manager/lib/view_manager_synchronizer.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/run_loop.h"
-#include "mojo/public/cpp/shell/service.h"
+#include "mojo/public/cpp/shell/connect.h"
 #include "mojo/public/interfaces/shell/shell.mojom.h"
 #include "mojo/services/public/cpp/view_manager/lib/view_manager_private.h"
 #include "mojo/services/public/cpp/view_manager/lib/view_private.h"
