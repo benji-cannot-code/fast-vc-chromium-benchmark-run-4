cb97ee2c182eb08e269ca2ffe962aaac1ee26ea4
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-17 06:21:47 +0000

    Update pdf browser tests to use test data from new location.
    R=scottmg@chromium.org
    
    Review URL: https://codereview.chromium.org/289873007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@271185 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/pdf/pdf_browsertest.cc
+++ chrome/browser/ui/pdf/pdf_browsertest.cc
@@ -52,7 +52,8 @@ class PDFBrowserTest : public InProcessBrowserTest,
         load_stop_notification_count_(0) {
     base::FilePath src_dir;
     EXPECT_TRUE(PathService::Get(base::DIR_SOURCE_ROOT, &src_dir));
-    pdf_test_server_.ServeFilesFromDirectory(src_dir.AppendASCII("pdf/test"));
+    pdf_test_server_.ServeFilesFromDirectory(src_dir.AppendASCII(
+        "chrome/test/data/pdf_private"));
   }
 
  protected:
@@ -63,12 +64,6 @@ class PDFBrowserTest : public InProcessBrowserTest,
 
   int load_stop_notification_count() const {
     return load_stop_notification_count_;
-  }
-
-  base::FilePath GetPDFTestDir() {
-    return base::FilePath(base::FilePath::kCurrentDirectory).AppendASCII("..").
-        AppendASCII("..").AppendASCII("..").AppendASCII("pdf").
-        AppendASCII("test");
   }
 
   void Load() {
@@ -83,7 +78,7 @@ class PDFBrowserTest : public InProcessBrowserTest,
     browser()->window()->SetBounds(bounds);
 
     GURL url(ui_test_utils::GetTestUrl(
-        GetPDFTestDir(),
+        base::FilePath(FILE_PATH_LITERAL("pdf_private")),
         base::FilePath(FILE_PATH_LITERAL("pdf_browsertest.pdf"))));
     ui_test_utils::NavigateToURL(browser(), url);
   }
@@ -134,7 +129,7 @@ class PDFBrowserTest : public InProcessBrowserTest,
     base::MessageLoopForUI::current()->Quit();
     ASSERT_EQ(success, true);
     base::FilePath reference = ui_test_utils::GetTestFilePath(
-        GetPDFTestDir(),
+        base::FilePath(FILE_PATH_LITERAL("pdf_private")),
         base::FilePath().AppendASCII(expected_filename_));
     base::File::Info info;
     ASSERT_TRUE(base::GetFileInfo(reference, &info));
@@ -320,7 +315,8 @@ IN_PROC_BROWSER_TEST_P(PDFBrowserTest, MAYBE_Loading) {
   std::string base_url = std::string("/");
 
   base::FileEnumerator file_enumerator(
-      ui_test_utils::GetTestFilePath(GetPDFTestDir(), base::FilePath()),
+      ui_test_utils::GetTestFilePath(
+          base::FilePath(FILE_PATH_LITERAL("pdf_private")), base::FilePath()),
       false,
       base::FileEnumerator::FILES,
       FILE_PATH_LITERAL("*.pdf"));
