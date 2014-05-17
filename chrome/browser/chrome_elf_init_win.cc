8120ed685890ef8fab06ca28b4ed94316b5fa74e
krstnmnlsn@chromium.org <krstnmnlsn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-17 17:35:20 +0000

    Changed the default Blacklist case (for no finch) from disabled to enabled.
    BUG=
    
    Review URL: https://codereview.chromium.org/272213006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@271246 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_elf_init_win.cc
+++ chrome/browser/chrome_elf_init_win.cc
@@ -16,10 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "version.h"  // NOLINT
 
-namespace {
-
 const char kBrowserBlacklistTrialName[] = "BrowserBlacklist";
-const char kBrowserBlacklistTrialEnabledGroupName[] = "Enabled";
+const char kBrowserBlacklistTrialDisabledGroupName[] = "NoBlacklist";
+
+namespace {
 
 // How long to wait, in seconds, before reporting for the second (and last
 // time), what dlls were blocked from the browser process.
@@ -83,12 +83,12 @@ void ReportSuccessfulBlocks() {
 
 void InitializeChromeElf() {
   if (base::FieldTrialList::FindFullName(kBrowserBlacklistTrialName) ==
-      kBrowserBlacklistTrialEnabledGroupName) {
-    BrowserBlacklistBeaconSetup();
-  } else {
+      kBrowserBlacklistTrialDisabledGroupName) {
     // Disable the blacklist for all future runs by removing the beacon.
     base::win::RegKey blacklist_registry_key(HKEY_CURRENT_USER);
     blacklist_registry_key.DeleteKey(blacklist::kRegistryBeaconPath);
+  } else {
+    BrowserBlacklistBeaconSetup();
   }
 
   // Report all successful blacklist interceptions.
--- chrome/browser/chrome_elf_init_win.h
+++ chrome/browser/chrome_elf_init_win.h
@@ -6,6 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CHROME_ELF_INIT_WIN_H_
 #define CHROME_BROWSER_CHROME_ELF_INIT_WIN_H_
 
+// Field trial name and full name for the blacklist disabled group.
+extern const char kBrowserBlacklistTrialName[];
+extern const char kBrowserBlacklistTrialDisabledGroupName[];
+
 // Prepare any initialization code for Chrome Elf's setup (This will generally
 // only affect future runs since Chrome Elf is already setup by this point).
 void InitializeChromeElf();
