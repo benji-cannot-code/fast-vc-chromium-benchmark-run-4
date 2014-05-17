437e816d16ec62f60ba093f5ddaa714cd1322177
hanxi@chromium.org <hanxi@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-17 02:09:35 +0000

    1. Add requestFileSystemAccessSync(), which will replace allowFileSystem() in the future. 2. Rename requestFileSystemAccess to requestFileSystemAccessAsync.
    This is the first patch for user story of "file system permission in webview implementation".
    
    BUG=343382
    
    Review URL: https://codereview.chromium.org/289793002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174223 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/web/WebWorkerPermissionClientProxy.h
+++ third_party/WebKit/public/web/WebWorkerPermissionClientProxy.h
@@ -59,7 +59,12 @@ public:
         return true;
     }
 
-    virtual void requestFileSystemAccess(const WebPermissionCallbacks& callbacks)
+    virtual bool requestFileSystemAccessSync()
+    {
+        return true;
+    }
+
+    virtual void requestFileSystemAccessAsync(const WebPermissionCallbacks& callbacks)
     {
         WebPermissionCallbacks permissionCallbacks(callbacks);
         permissionCallbacks.doAllow();
