1355de847cc653e990a7db03c8eeac5515b6c4bb
nsatragno@chromium.org <nsatragno@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-17 01:46:22 +0000

    Merge WindowOverview into WindowSelector
    This change reduces the amount of code its complexity, and is the natural follow up from having alt tab separated into a different class.
    
    Please note that no functionality has been changed, and that the change is pretty much only moving code around.
    
    BUG=371884
    TEST=WindowSelectorTest.*
    
    Review URL: https://codereview.chromium.org/280423008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@271142 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/ash.gyp
+++ ash/ash.gyp
@@ -593,8 +593,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'wm/overview/scoped_transform_overview_window.h',
         'wm/overview/scoped_window_copy.cc',
         'wm/overview/scoped_window_copy.h',
-        'wm/overview/window_overview.cc',
-        'wm/overview/window_overview.h',
         'wm/overview/window_selector.cc',
         'wm/overview/window_selector.h',
         'wm/window_cycle_controller.cc',
--- ash/wm/overview/window_selector.h
+++ ash/wm/overview/window_selector.h
@@ -15,25 +15,42 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_vector.h"
 #include "base/time/time.h"
 #include "ui/aura/window_observer.h"
+#include "ui/aura/window_tracker.h"
+#include "ui/events/event_handler.h"
+#include "ui/gfx/display_observer.h"
 #include "ui/wm/public/activation_change_observer.h"
 
 namespace aura {
 class RootWindow;
+class Window;
+namespace client {
+class CursorClient;
+}  // namespace client
+}  // namespace aura
+
+namespace gfx {
+class Rect;
+}
+
+namespace ui {
+class LocatedEvent;
 }
 
 namespace ash {
-class WindowOverview;
 class WindowSelectorDelegate;
 class WindowSelectorItem;
 class WindowSelectorTest;
 
-// The WindowSelector allows selecting a window by clicking or tapping on it
-// (entering Overview Mode).
+// The WindowSelector shows a grid of all of your windows, allowing to select
+// one by clicking or tapping on it.
 class ASH_EXPORT WindowSelector
-    : public aura::WindowObserver,
+    : public ui::EventHandler,
+      public gfx::DisplayObserver,
+      public aura::WindowObserver,
       public aura::client::ActivationChangeObserver {
  public:
   typedef std::vector<aura::Window*> WindowList;
+  typedef ScopedVector<WindowSelectorItem> WindowSelectorItemList;
 
   WindowSelector(const WindowList& windows,
                  WindowSelectorDelegate* delegate);
@@ -45,6 +62,17 @@ class ASH_EXPORT WindowSelector
   // Cancels window selection.
   void CancelSelection();
 
+  // ui::EventHandler:
+  virtual void OnKeyEvent(ui::KeyEvent* event) OVERRIDE;
+  virtual void OnMouseEvent(ui::MouseEvent* event) OVERRIDE;
+  virtual void OnScrollEvent(ui::ScrollEvent* event) OVERRIDE;
+  virtual void OnTouchEvent(ui::TouchEvent* event) OVERRIDE;
+
+  // gfx::DisplayObserver:
+  virtual void OnDisplayBoundsChanged(const gfx::Display& display) OVERRIDE;
+  virtual void OnDisplayAdded(const gfx::Display& display) OVERRIDE;
+  virtual void OnDisplayRemoved(const gfx::Display& display) OVERRIDE;
+
   // aura::WindowObserver:
   virtual void OnWindowAdded(aura::Window* new_window) OVERRIDE;
   virtual void OnWindowBoundsChanged(aura::Window* window,
@@ -52,7 +80,7 @@ class ASH_EXPORT WindowSelector
                                      const gfx::Rect& new_bounds) OVERRIDE;
   virtual void OnWindowDestroying(aura::Window* window) OVERRIDE;
 
-  // Overridden from aura::client::ActivationChangeObserver:
+  // aura::client::ActivationChangeObserver:
   virtual void OnWindowActivated(aura::Window* gained_active,
                                  aura::Window* lost_active) OVERRIDE;
   virtual void OnAttemptToReactivateWindow(
@@ -65,9 +93,25 @@ class ASH_EXPORT WindowSelector
   // Begins positioning windows such that all windows are visible on the screen.
   void StartOverview();
 
+  // Position all of the windows based on the current selection mode.
+  void PositionWindows(bool animate);
+  // Position all of the windows from |root_window| on |root_window|.
+  void PositionWindowsFromRoot(aura::Window* root_window, bool animate);
+
   // Resets the stored window from RemoveFocusAndSetRestoreWindow to NULL. If
+  // Hide and track all hidden windows not in overview.
+  void HideAndTrackNonOverviewWindows();
+
   // |focus|, restores focus to the stored window.
   void ResetFocusRestoreWindow(bool focus);
+
+  // Returns the target of |event| or NULL if the event is not targeted at
+  // any of the windows in the selector.
+  aura::Window* GetEventTarget(ui::LocatedEvent* event);
+
+  // Returns the top-level window selected by targeting |window| or NULL if
+  // no overview window was found for |window|.
+  aura::Window* GetTargetedWindow(aura::Window* window);
 
   // The collection of items in the overview wrapped by a helper class which
   // restores their state and helps transform them to other root windows.
@@ -75,8 +119,6 @@ class ASH_EXPORT WindowSelector
 
   // Tracks observed windows.
   std::set<aura::Window*> observed_windows_;
-
-  scoped_ptr<WindowOverview> window_overview_;
 
   // Weak pointer to the selector delegate which will be called when a
   // selection is made.
@@ -90,6 +132,16 @@ class ASH_EXPORT WindowSelector
   // True when performing operations that may cause window activations. This is
   // used to prevent handling the resulting expected activation.
   bool ignore_activations_;
+
+  // The cursor client used to lock the current cursor during overview.
+  aura::client::CursorClient* cursor_client_;
+
+  // The time when overview was started.
+  base::Time overview_start_time_;
+
+  // Tracks windows which were hidden because they were not part of the
+  // overview.
+  aura::WindowTracker hidden_windows_;
 
   DISALLOW_COPY_AND_ASSIGN(WindowSelector);
 };
