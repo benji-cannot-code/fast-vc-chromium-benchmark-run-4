73aac059799a25bce58c75155da420372d9d2dd3
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-18 19:01:02 +0000

    tcmalloc: Let DEFINE_string define const char*s.
    DEFINE_string is used in 3 files in tcmalloc, but we only compile one of
    these.  In this one file, the string is converted to char every time it's used,
    and since the string is used after global destructors have run it needs to be
    copied to a second string in a static initializer.
    
    Instead of all that silliness, just let DEFINE_string define a const char*
    (like it does in v8 or webrtc).
    
    BUG=94925
    R=willchan@chromium.org
    
    Review URL: https://codereview.chromium.org/286953011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@271307 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/tcmalloc/chromium/src/base/commandlineflags.h
+++ third_party/tcmalloc/chromium/src/base/commandlineflags.h
@@ -60,13 +60,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #define DECLARE_VARIABLE(type, name)                                          \
-  namespace FLAG__namespace_do_not_use_directly_use_DECLARE_##type##_instead {  \
+  namespace FLAG__namespace_do_not_use_directly_use_DECLARE_##type##_instead {\
   extern PERFTOOLS_DLL_DECL type FLAGS_##name;                                \
   }                                                                           \
   using FLAG__namespace_do_not_use_directly_use_DECLARE_##type##_instead::FLAGS_##name
 
 #define DEFINE_VARIABLE(type, name, value, meaning) \
-  namespace FLAG__namespace_do_not_use_directly_use_DECLARE_##type##_instead {  \
+  namespace FLAG__namespace_do_not_use_directly_use_DECLARE_##type##_instead {\
   PERFTOOLS_DLL_DECL type FLAGS_##name(value);                                \
   char FLAGS_no##name;                                                        \
   }                                                                           \
@@ -101,16 +101,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define DEFINE_double(name, value, meaning) \
   DEFINE_VARIABLE(double, name, value, meaning)
 
-// Special case for string, because we have to specify the namespace
-// std::string, which doesn't play nicely with our FLAG__namespace hackery.
+// Special case for string, because of the pointer type.
 #define DECLARE_string(name)                                          \
   namespace FLAG__namespace_do_not_use_directly_use_DECLARE_string_instead {  \
-  extern std::string FLAGS_##name;                                                   \
+  extern const char* FLAGS_##name;                                            \
   }                                                                           \
   using FLAG__namespace_do_not_use_directly_use_DECLARE_string_instead::FLAGS_##name
 #define DEFINE_string(name, value, meaning) \
   namespace FLAG__namespace_do_not_use_directly_use_DECLARE_string_instead {  \
-  std::string FLAGS_##name(value);                                                   \
+  const char* FLAGS_##name = value;                                           \
   char FLAGS_no##name;                                                        \
   }                                                                           \
   using FLAG__namespace_do_not_use_directly_use_DECLARE_string_instead::FLAGS_##name
