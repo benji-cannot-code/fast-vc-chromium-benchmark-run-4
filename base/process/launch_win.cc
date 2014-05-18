6f49b7d506854c8f220cf3ddfbb4e52f8298fd39
huangs@chromium.org <huangs@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-18 14:00:13 +0000

    Outputting command line if CreateProcess() fails.
    Specifically, this is to help us debug Win 8 Aura test flakes.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/289033002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@271285 0039d316-1c4b-4281-b951-d872f2087c98

--- base/process/launch_win.cc
+++ base/process/launch_win.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_loop.h"
 #include "base/metrics/histogram.h"
 #include "base/process/kill.h"
+#include "base/strings/utf_string_conversions.h"
 #include "base/sys_info.h"
 #include "base/win/object_watcher.h"
 #include "base/win/scoped_handle.h"
@@ -194,7 +195,8 @@ bool LaunchProcess(const string16& cmdline,
                             &temp_process_info);
     DestroyEnvironmentBlock(enviroment_block);
     if (!launched) {
-      DPLOG(ERROR);
+      DPLOG(ERROR) << "Command line:" << std::endl << UTF16ToUTF8(cmdline)
+                   << std::endl;;
       return false;
     }
   } else {
@@ -202,7 +204,8 @@ bool LaunchProcess(const string16& cmdline,
                        const_cast<wchar_t*>(cmdline.c_str()), NULL, NULL,
                        inherit_handles, flags, NULL, NULL,
                        startup_info, &temp_process_info)) {
-      DPLOG(ERROR);
+      DPLOG(ERROR) << "Command line:" << std::endl << UTF16ToUTF8(cmdline)
+                   << std::endl;;
       return false;
     }
   }
