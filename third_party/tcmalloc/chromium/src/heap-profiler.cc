2c75b093e7fa08f69fde990d5af1f4836f4efa14
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-19 17:39:17 +0000

    tcmalloc: Honor HEAPPROFILE envvar only in profiling=1 builds.
    Several systems in chromium (about:tracing, some code in blink) use
    tcmalloc's heap profiler, so keep most of it around.  Do disable support
    for the HEAPPROFILE envvar, and for the exit-time dump to "Exiting" (unless in
    profiling=1 builds), as that causes a static initializer.
    
    BUG=94925
    R=willchan@chromium.org
    
    Review URL: https://codereview.chromium.org/295583002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@271415 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/tcmalloc/chromium/src/heap-profiler.cc
+++ third_party/tcmalloc/chromium/src/heap-profiler.cc
@@ -663,7 +663,7 @@ extern "C" void HeapProfilerDumpAliveObjects(const char* filename) {
 //----------------------------------------------------------------------
 // Initialization/finalization code
 //----------------------------------------------------------------------
-
+#if defined(ENABLE_PROFILING)
 // Initialization code
 static void HeapProfilerInit() {
   // Everything after this point is for setting up the profiler based on envvar
@@ -694,3 +694,4 @@ struct HeapProfileEndWriter {
 static const TCMallocGuard tcmalloc_initializer;
 REGISTER_MODULE_INITIALIZER(heapprofiler, HeapProfilerInit());
 static HeapProfileEndWriter heap_profile_end_writer;
+#endif   // defined(ENABLE_PROFILING)
