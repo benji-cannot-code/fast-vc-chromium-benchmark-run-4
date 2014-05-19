588176a3979424ae132c688e408d2e0ab1a7e6fa
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-19 00:52:05 +0000

    Revert of Elimate NOTIFICATION_GOOGLE_URL_UPDATED (https://codereview.chromium.org/284343003/)
    Reason for revert:
    speculative, browser_tests stated timing out on all non-dbg win main waterfall test bots, with a message such as
    
    
    E:\b\depot_tools\python276_bin\python_slave.exe E:\b\build\scripts\slave\runisolatedtest.py --test_name browser_tests --builder_name "Win7 Tests (1)" --checkout_dir E:\b\build\slave\Win7_Tests__1_\build E:\b\build\slave\Win7_Tests__1_\build\src\out\Release\browser_tests.exe -- E:\b\build\slave\Win7_Tests__1_\build\src\out\Release\browser_tests.exe --brave-new-test-launcher --test-launcher-bot-mode --test-launcher-total-shards=3 --test-launcher-shard-index=0 --lib=browser_tests --gtest_print_time --gtest_output=xml:E:\b\build\slave\Win7_Tests__1_\build\gtest-results\browser_tests\browser_tests.xml --test-launcher-summary-output=c:\users\chrome~2\appdata\local\temp\tmpfbmzn9
    
    E:\b\depot_tools\python276_bin\python_slave.exe E:\b\build\slave\Win7_Tests__1_\build\src\tools\swarming_client\isolate.py run --isolated E:\b\build\slave\Win7_Tests__1_\build\src\out\Release\browser_tests.isolated -v -- --no-cr --test-launcher-bot-mode --test-launcher-total-shards=3 --test-launcher-shard-index=0 --gtest_output=xml:E:\b\build\slave\Win7_Tests__1_\build\gtest-results\browser_tests\browser_tests.xml --test-launcher-summary-output=c:\users\chrome~2\appdata\local\temp\tmpfbmzn9
     INFO         isolate(685): CompleteState.load_isolate(E:\b\build\slave\Win7_Tests__1_\build, E:\b\build\slave\Win7_Tests__1_\build\src\chrome\browser_tests.isolate, {}, {}, {'EXECUTABLE_SUFFIX': '.exe'}, False)
     INFO         isolate(156): normalize_path_variables(E:\b\build\slave\Win7_Tests__1_\build, {}, E:\b\build\slave\Win7_Tests__1_\build\src\chrome)
     INFO  isolate_format( 57): determine_root_dir(E:\b\build\slave\Win7_Tests__1_\build\src\chrome, 80 files) -> E:\b\build\slave\Win7_Tests__1_\build\src
     INFO         isolate( 84): recreate_tree(outdir=E:\b\build\slave\Win7_Tests__1_\isolate-2014-05-18gqemnl, indir=E:\b\build\slave\Win7_Tests__1_\build\src, files=9443, action=4, as_hash=False)
     INFO         isolate(1266): Running ['E:\\b\\depot_tools\\python276_bin\\python_slave.exe', '../testing/test_env.py', u'..\\out\\Release/browser_tests.exe', '--test-launcher-bot-mode', '--no-cr', '--test-launcher-bot-mode', '--test-launcher-total-shards=3', '--test-launcher-shard-index=0', '--gtest_output=xml:E:\\b\\build\\slave\\Win7_Tests__1_\\build\\gtest-results\\browser_tests\\browser_tests.xml', '--test-launcher-summary-output=c:\\users\\chrome~2\\appdata\\local\\temp\\tmpfbmzn9'], cwd=E:\b\build\slave\Win7_Tests__1_\isolate-2014-05-18gqemnl\chrome
    
    command timed out: 600 seconds without output, attempting to kill
    program finished with exit code 1
    
    Original issue's description:
    > Elimate NOTIFICATION_GOOGLE_URL_UPDATED
    > 
    > This CL moves the remaining client of the NOTIFICATION_GOOGLE_URL_UPDATED
    > notification to instead register a callback with GoogleURLTracker and
    > eliminates the notification. It also migrate the GoogleURLTracker unittest from
    > listening for the notification to listening for the callback.
    > 
    > BUG=373261,373237
    > TBR=thakis
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=271313
    
    TBR=pkasting@chromium.org,blundell@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=373261,373237
    
    Review URL: https://codereview.chromium.org/294563002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@271320 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/navigation_correction_tab_observer.h
+++ chrome/browser/ui/navigation_correction_tab_observer.h
@@ -7,7 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_NAVIGATION_CORRECTION_TAB_OBSERVER_H_
 
 #include "base/prefs/pref_change_registrar.h"
-#include "chrome/browser/google/google_url_tracker.h"
+#include "content/public/browser/notification_observer.h"
+#include "content/public/browser/notification_registrar.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
 
@@ -20,6 +21,7 @@ class PrefRegistrySyncable;
 // Per-tab class to implement navigation suggestion service functionality.
 class NavigationCorrectionTabObserver
     : public content::WebContentsObserver,
+      public content::NotificationObserver,
       public content::WebContentsUserData<NavigationCorrectionTabObserver> {
  public:
   virtual ~NavigationCorrectionTabObserver();
@@ -34,10 +36,12 @@ class NavigationCorrectionTabObserver
   virtual void RenderViewCreated(
       content::RenderViewHost* render_view_host) OVERRIDE;
 
-  // Internal helpers ----------------------------------------------------------
+  // content::NotificationObserver overrides:
+  virtual void Observe(int type,
+                       const content::NotificationSource& source,
+                       const content::NotificationDetails& details) OVERRIDE;
 
-  // Callback that is called when the Google URL is updated.
-  void OnGoogleURLUpdated(GURL old_url, GURL new_url);
+  // Internal helpers ----------------------------------------------------------
 
   // Returns the URL for the correction service.  If the returned URL
   // is empty, the default error pages will be used.
@@ -50,8 +54,8 @@ class NavigationCorrectionTabObserver
   void UpdateNavigationCorrectionInfo(content::RenderViewHost* rvh);
 
   Profile* profile_;
+  content::NotificationRegistrar registrar_;
   PrefChangeRegistrar pref_change_registrar_;
-  scoped_ptr<GoogleURLTracker::Subscription> google_url_updated_subscription_;
 
   DISALLOW_COPY_AND_ASSIGN(NavigationCorrectionTabObserver);
 };
