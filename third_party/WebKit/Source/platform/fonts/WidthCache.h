5ffca8afeeb1ed66d6d2b6ff266b74c57ec1bc43
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-19 22:08:12 +0000

    Revert r174230 - "Extend WidthCache to include glyph overflow bounds"
    Several devtools layout tests began timing out on 10.6 (dbg) in
    the first build that contained this change. This is a speculative
    revert to see if things start working again, though it seems unlikely.
    
    TBR=eae@chromium.org
    BUG=374961
    
    Review URL: https://codereview.chromium.org/291033003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174318 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/fonts/WidthCache.h
+++ third_party/WebKit/Source/platform/fonts/WidthCache.h
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WidthCache_h
 #define WidthCache_h
 
-#include "platform/geometry/IntRectExtent.h"
 #include "platform/text/TextRun.h"
 #include "wtf/Forward.h"
 #include "wtf/HashFunctions.h"
@@ -38,16 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 struct GlyphOverflow;
-
-struct WidthCacheEntry {
-    WidthCacheEntry()
-    {
-        width = std::numeric_limits<float>::quiet_NaN();
-    }
-    bool isValid() const { return !std::isnan(width); }
-    float width;
-    IntRectExtent glyphBounds;
-};
 
 class WidthCache {
 private:
@@ -131,7 +120,7 @@ public:
     {
     }
 
-    WidthCacheEntry* add(const TextRun& run, WidthCacheEntry entry)
+    float* add(const TextRun& run, float entry)
     {
         if (static_cast<unsigned>(run.length()) > SmallStringKey::capacity())
             return 0;
@@ -151,11 +140,11 @@ public:
     }
 
 private:
-    WidthCacheEntry* addSlowCase(const TextRun& run, WidthCacheEntry entry)
+    float* addSlowCase(const TextRun& run, float entry)
     {
         int length = run.length();
         bool isNewEntry;
-        WidthCacheEntry *value;
+        float *value;
         if (length == 1) {
             SingleCharMap::AddResult addResult = m_singleCharMap.add(run[0], entry);
             isNewEntry = addResult.isNewEntry;
@@ -192,8 +181,8 @@ private:
         return 0;
     }
 
-    typedef HashMap<SmallStringKey, WidthCacheEntry, SmallStringKeyHash, SmallStringKeyHashTraits> Map;
-    typedef HashMap<uint32_t, WidthCacheEntry, DefaultHash<uint32_t>::Hash, WTF::UnsignedWithZeroKeyHashTraits<uint32_t> > SingleCharMap;
+    typedef HashMap<SmallStringKey, float, SmallStringKeyHash, SmallStringKeyHashTraits> Map;
+    typedef HashMap<uint32_t, float, DefaultHash<uint32_t>::Hash, WTF::UnsignedWithZeroKeyHashTraits<uint32_t> > SingleCharMap;
     static const int s_minInterval = -3; // A cache hit pays for about 3 cache misses.
     static const int s_maxInterval = 20; // Sampling at this interval has almost no overhead.
     static const unsigned s_maxSize = 500000; // Just enough to guard against pathological growth.
