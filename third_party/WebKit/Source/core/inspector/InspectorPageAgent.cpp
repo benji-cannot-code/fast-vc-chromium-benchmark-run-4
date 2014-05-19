fba05ce064df8873c1601263d259a5805196d3da
rune@opera.com <rune@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-19 12:18:52 +0000

    Use RecalcStyleDeferred for inspector style changes.
    I could not find a reason why it needs to be immediate and all tests seem to
    still pass.
    
    R=pfeldman@chromium.org,ojan@chromium.org
    BUG=374162
    
    Review URL: https://codereview.chromium.org/290313002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174273 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorPageAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorPageAgent.cpp
@@ -1158,7 +1158,7 @@ void InspectorPageAgent::updateViewMetrics(bool enabled, int width, int height, 
 
     Document* document = mainFrame()->document();
     if (document) {
-        document->styleResolverChanged(RecalcStyleImmediately);
+        document->styleResolverChanged(RecalcStyleDeferred);
         document->mediaQueryAffectingValueChanged();
     }
     InspectorInstrumentation::mediaQueryResultChanged(document);
@@ -1225,7 +1225,7 @@ void InspectorPageAgent::setEmulatedMedia(ErrorString*, const String& media)
         document = m_page->mainFrame()->document();
     if (document) {
         document->mediaQueryAffectingValueChanged();
-        document->styleResolverChanged(RecalcStyleImmediately);
+        document->styleResolverChanged(RecalcStyleDeferred);
         document->updateLayout();
     }
 }
