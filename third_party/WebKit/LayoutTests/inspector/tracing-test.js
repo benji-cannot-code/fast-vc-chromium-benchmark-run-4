d8964f49146e0d78c5322698226b1b31441a586f
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-19 15:36:54 +0000

    DevTools: resolve layer owner nodes while importing layer tree from trace
    Also, retain layer object instances when updating the same layer,
    as we used to do with AgentLayers.
    
    BUG=361045
    
    Review URL: https://codereview.chromium.org/291563005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174293 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/inspector/tracing-test.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function initialize_TracingTest()
+{
+
+// FIXME: remove when tracing is out of experimental
+WebInspector.inspectorView.showPanel("timeline");
+InspectorTest.tracingModel = new WebInspector.TracingModel();
+
+InspectorTest.invokeWithTracing = function(categoryFilter, functionName, callback)
+{
+    InspectorTest.tracingModel.start(categoryFilter, "", onTracingStarted);
+
+    function onTracingStarted(error)
+    {
+        InspectorTest.invokePageFunctionAsync(functionName, onPageActionsDone)
+    }
+
+    function onPageActionsDone()
+    {
+        InspectorTest.tracingModel.stop(callback);
+    }
+}
+
+}
