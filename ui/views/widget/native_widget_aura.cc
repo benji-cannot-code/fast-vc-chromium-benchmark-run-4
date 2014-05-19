daddd5ebcce4f87beef0d4ed569bb027d46d55aa
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-19 20:40:32 +0000

    Revert of [Refactor] Consolidate the logic for whether a widget can be activated. (https://codereview.chromium.org/286733002/)
    Reason for revert:
    This CL was causing problems when the value of views::Widget::CanActivate() changes crbug.com/374095
    
    I also need to audit the code to find out whether there are any 'non-activatable' widgets which were intended to be activatable
    
    Original issue's description:
    > [Refactor] Consolidate the logic for whether a widget can be activated.
    >
    > This is a first step towards giving tooltips their own WindowTreeHosts on Linux.
    >
    > BUG=353533
    > TEST=None
    >
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=270597
    
    Review URL: https://codereview.chromium.org/297503003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@271468 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/native_widget_aura.cc
+++ ui/views/widget/native_widget_aura.cc
@@ -77,6 +77,7 @@ NativeWidgetAura::NativeWidgetAura(internal::NativeWidgetDelegate* delegate)
       window_(new aura::Window(this)),
       ownership_(Widget::InitParams::NATIVE_WIDGET_OWNS_WIDGET),
       close_widget_factory_(this),
+      can_activate_(true),
       destroying_(false),
       cursor_(gfx::kNullCursor),
       saved_window_state_(ui::SHOW_STATE_DEFAULT) {
@@ -161,6 +162,9 @@ void NativeWidgetAura::InitNativeWidget(const Widget::InitParams& params) {
   else
     SetBounds(window_bounds);
   window_->set_ignore_events(!params.accept_events);
+  can_activate_ = params.can_activate &&
+      params.type != Widget::InitParams::TYPE_CONTROL &&
+      params.type != Widget::InitParams::TYPE_TOOLTIP;
   DCHECK(GetWidget()->GetRootView());
   if (params.type != Widget::InitParams::TYPE_TOOLTIP)
     tooltip_manager_.reset(new views::TooltipManagerAura(GetWidget()));
@@ -484,7 +488,7 @@ void NativeWidgetAura::ShowWithWindowState(ui::WindowShowState state) {
   if (state == ui::SHOW_STATE_MAXIMIZED || state == ui::SHOW_STATE_FULLSCREEN)
     window_->SetProperty(aura::client::kShowStateKey, state);
   window_->Show();
-  if (delegate_->CanActivate()) {
+  if (can_activate_) {
     if (state != ui::SHOW_STATE_INACTIVE)
       Activate();
     // SetInitialFocus() should be always be called, even for
@@ -770,7 +774,7 @@ bool NativeWidgetAura::ShouldDescendIntoChildForEventHandling(
 }
 
 bool NativeWidgetAura::CanFocus() {
-  return ShouldActivate();
+  return can_activate_;
 }
 
 void NativeWidgetAura::OnCaptureLost() {
@@ -867,7 +871,7 @@ void NativeWidgetAura::OnGestureEvent(ui::GestureEvent* event) {
 // NativeWidgetAura, aura::client::ActivationDelegate implementation:
 
 bool NativeWidgetAura::ShouldActivate() const {
-  return delegate_->CanActivate();
+  return can_activate_ && delegate_->CanActivate();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
