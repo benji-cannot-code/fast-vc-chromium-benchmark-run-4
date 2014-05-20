b34c29687bc7006146d43868680d81a2f165c7ed
haven@chromium.org <haven@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-20 03:03:37 +0000

    Unmounts volumes before writing to a drive.
    When writing to a drive with mounted volumes, Chrome OS will recreate the partition table if it is observe to be missing and the volumes are accessed after writing.  This only occurs when erasing just the partition table because enough of the volume information is still present.
    
    We also expand the amount of data erased when destroying partitions to 8k to account for 4k block sizes.  This has the side effect of overwriting the LBA for 512-byte-block drives, which only increases the effect of destroying partitions.
    
    BUG=284834
    BUG=335390
    
    Review URL: https://codereview.chromium.org/282853003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@271551 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/image_writer_private/destroy_partitions_operation.cc
+++ chrome/browser/extensions/api/image_writer_private/destroy_partitions_operation.cc
@@ -11,10 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace extensions {
 namespace image_writer {
 
-// Number of bytes for the maximum partition table size.  By wiping this many
-// bytes we can essentially guarantee the header and associated information will
-// be wiped. See http://crbug.com/328246 for more information.
-const int kPartitionTableSize = 1 * 1024;
+// Number of bytes for the maximum partition table size.  GUID partition tables
+// reside in the second sector of the disk.  Disks can have up to 4k sectors.
+// See http://crbug.com/328246 for more information.
+const int kPartitionTableSize = 2 * 4096;
 
 DestroyPartitionsOperation::DestroyPartitionsOperation(
     base::WeakPtr<OperationManager> manager,
