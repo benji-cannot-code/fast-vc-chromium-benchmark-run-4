383578f05c00a1cfe40ce4a2178342732038a5e7
mvanouwerkerk@chromium.org <mvanouwerkerk@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-20 11:32:13 +0000

    Push API: call through to client from PushManager for registration.
    BUG=350377
    
    Review URL: https://codereview.chromium.org/274563014
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174368 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/push_messaging/PushManager.cpp
+++ third_party/WebKit/Source/modules/push_messaging/PushManager.cpp
@@ -12,10 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/v8/ScriptState.h"
 #include "core/dom/Document.h"
 #include "core/dom/ExecutionContext.h"
+#include "modules/push_messaging/PushController.h"
 #include "modules/push_messaging/PushError.h"
 #include "modules/push_messaging/PushRegistration.h"
-#include "public/platform/WebPushError.h"
-#include "wtf/OwnPtr.h"
+#include "public/platform/WebPushClient.h"
+#include "wtf/RefPtr.h"
 
 namespace WebCore {
 
@@ -30,11 +31,12 @@ PushManager::~PushManager()
 
 ScriptPromise PushManager::registerPushMessaging(ExecutionContext* context, const String& senderId)
 {
+    ASSERT(context->isDocument());
     RefPtr<ScriptPromiseResolverWithContext> resolver = ScriptPromiseResolverWithContext::create(ScriptState::current(toIsolate(context)));
     ScriptPromise promise = resolver->promise();
-    // FIXME: Implement registration.
-    OwnPtr<CallbackPromiseAdapter<PushRegistration, PushError> > adapter = adoptPtr(new CallbackPromiseAdapter<PushRegistration, PushError>(resolver));
-    adapter->onError(new blink::WebPushError(blink::WebPushError::ErrorTypeAbort, "FIXME"));
+    blink::WebPushClient* client = PushController::clientFrom(toDocument(context)->page());
+    ASSERT(client);
+    client->registerPushMessaging(senderId, new CallbackPromiseAdapter<PushRegistration, PushError>(resolver));
     return promise;
 }
 
