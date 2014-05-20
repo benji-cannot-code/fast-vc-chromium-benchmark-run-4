3db0d8afa2fe069b1bb124e061361f8966556bc9
rlarocque@chromium.org <rlarocque@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-20 21:42:52 +0000

    sync: Enable typed tombstones commits
    Modifies commit logic to include the model type of items in commit
    requests.  The model type is implicitly specified by the presence of
    a sub-message in the EntitySpecifics field.
    
    Prior to this commit, only bookmarks would populate the sync entities on
    commit.  The special case for bookmarks was introduced recently, in
    r265587.
    
    Unlike the bookmarks case, the non-bookmark items send up only enough
    specifics to identify the model type.  The rest of the specifics are not
    set.
    
    This commit also adds some tests of the bookmark and non-bookmark
    deletion request behaviors.
    
    BUG=365752,373859
    
    Review URL: https://codereview.chromium.org/298503002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@271763 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/syncable/model_type_unittest.cc
+++ sync/syncable/model_type_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/test/values_test_util.h"
 #include "base/values.h"
+#include "sync/protocol/sync.pb.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace syncer {
@@ -116,6 +117,26 @@ TEST_F(ModelTypeTest, ModelTypeSetFromString) {
       one.Equals(ModelTypeSetFromString(ModelTypeSetToString(one))));
   EXPECT_TRUE(
       two.Equals(ModelTypeSetFromString(ModelTypeSetToString(two))));
+}
+
+TEST_F(ModelTypeTest, DefaultFieldValues) {
+  syncer::ModelTypeSet types = syncer::ProtocolTypes();
+  for (ModelTypeSet::Iterator it = types.First(); it.Good(); it.Inc()) {
+    SCOPED_TRACE(ModelTypeToString(it.Get()));
+
+    sync_pb::EntitySpecifics specifics;
+    syncer::AddDefaultFieldValue(it.Get(), &specifics);
+    EXPECT_TRUE(specifics.IsInitialized());
+
+    std::string tmp;
+    EXPECT_TRUE(specifics.SerializeToString(&tmp));
+
+    sync_pb::EntitySpecifics from_string;
+    EXPECT_TRUE(from_string.ParseFromString(tmp));
+    EXPECT_TRUE(from_string.IsInitialized());
+
+    EXPECT_EQ(it.Get(), syncer::GetModelTypeFromSpecifics(from_string));
+  }
 }
 
 }  // namespace
