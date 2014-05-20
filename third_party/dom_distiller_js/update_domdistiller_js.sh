d3e8ffcc0f14f4c91fc15c2c0972bd2c6b8782f1
cjhopman@chromium.org <cjhopman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-20 06:23:13 +0000

    Roll DomDistillerJs (and make updater generate BUG=)
    The updater now pulls BUG= lines out of the git log and 
    processes/sorts/uniqifies them and prints them for even better 
    commit messages.
    
    Picked up changes:
    6ef4764 Extend visibility checks to images that are extracted.
    6e8c385 Updated Boilerpipe with some more modern tags.
    cde81dc Add extract_text_only option
    0a31800 filter out invisible elements
    82b35ba Fix crash when content extraction fails.
    
    BUG=354157,356336,362085,364356,367243
    
    Review URL: https://codereview.chromium.org/297563002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@271582 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/dom_distiller_js/update_domdistiller_js.sh
+++ third_party/dom_distiller_js/update_domdistiller_js.sh
@@ -16,7 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   dom_distiller_js_package=$dom_distiller_js_path/package
   readme_chromium=$dom_distiller_js_path/README.chromium
   tmpdir=/tmp/domdistiller-$$
-  changes=/tmp/domdistiller.changes
+  changes=$tmpdir/domdistiller.changes
+  bugs=$tmpdir/domdistiller.bugs
   curr_gitsha=$(grep 'Version:' $readme_chromium | awk '{print $2}')
 
   rm -rf $tmpdir
@@ -24,8 +25,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   pushd $tmpdir
   git clone https://code.google.com/p/dom-distiller/ .
+
   new_gitsha=$(git rev-parse --short=10 HEAD)
   git log --oneline ${curr_gitsha}.. > $changes
+
+  echo -n BUG= > $bugs
+
+  # This extracts BUG= lines from the log, extracts the numbers part, removes
+  # whitespace and deletes empty lines. Then, split on ',', sort, uniquify and
+  # rejoin. Finally, remove the trailing ',' and concat to $bugs.
+  git log \
+    | grep BUG= \
+    | sed -e 's/.*BUG=\(.*\)/\1/' -e 's/\s*//g' -e '/^$/d' \
+    | tr ',' '\n' \
+    | sort \
+    | uniq \
+    | tr '\n' ',' \
+    | head --bytes=-1 \
+    >> $bugs
+
+  echo >> $bugs  # add a newline
+
   ant package
   popd
 
@@ -35,13 +55,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   cp $tmpdir/LICENSE $dom_distiller_js_path/
   sed -i "s/Version: [0-9a-f]*/Version: $new_gitsha/" $readme_chromium
 
+  echo
+  echo
+  echo "---Generated commit message---"
+  echo
   echo "Picked up changes:"
   cat $changes
+  echo
+  cat $bugs
 
   # Run checklicenses.py on the pulled files, but only print the output on
   # failures.
   tools/checklicenses/checklicenses.py $dom_distiller_js_path > $tmpdir/checklicenses.out || cat $tmpdir/checklicenses.out
 
   rm -rf $tmpdir
-  rm $changes
 )
