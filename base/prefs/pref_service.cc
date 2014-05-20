58b82ecd290d97f7e72cc23c63442272209f26c9
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-20 11:25:12 +0000

    Revert 271416 "Store a stacktrace of PrefService destruction in ..."
    Speculative revert, suspecting this CL resulted in timeouts of the
    following tests on ChromiumOS:
    
    ExtensionUpdaterTest.TestGalleryRequestsWithNonOrganicBrand
    ExtensionUpdaterTest.TestGalleryRequestsWithOrganicBrand
    
    Test run time went from ~2s per test to >40s per test (and
    higher) causing timeouts.
    
    > Store a stacktrace of PrefService destruction in PrefChangeRegistrar
    > 
    > Store a stacktrace of PrefService destruction in PrefChangeRegistrar to debug a
    > race condition: ~PrefChangeRegistrar causes a crash. The current hypothesis is
    > that this is caused because the PrefChangeRegistrar is registered to a Profile
    > that is already destroyed. This code stores stacktrace when the Profile's
    > PrefService is destroyed to see whether this is correct.
    > 
    > BUG=373435
    > 
    > Review URL: https://codereview.chromium.org/290083006
    
    TBR=battre@chromium.org
    
    Review URL: https://codereview.chromium.org/289373006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@271622 0039d316-1c4b-4281-b951-d872f2087c98

--- base/prefs/pref_service.cc
+++ base/prefs/pref_service.cc
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 
 #include "base/bind.h"
-// TODO(battre): Delete this. See crbug.com/373435.
-#include "base/debug/stack_trace.h"
 #include "base/files/file_path.h"
 #include "base/logging.h"
 #include "base/message_loop/message_loop.h"
@@ -61,13 +59,6 @@ PrefService::PrefService(
 
 PrefService::~PrefService() {
   DCHECK(CalledOnValidThread());
-
-  // TODO(battre): Remove the following code. It's purpose is to understand
-  // whether a crash in PrefChangeRegistrar::~PrefChangeRegistrar() is caused
-  // by a PrefService already being dead. See crbug.com/373435.
-  std::string stack_trace = base::debug::StackTrace().ToString();
-  static_cast<PrefNotifier*>(pref_notifier_.get())
-      ->BroadcastPrefServiceDestructionTrace(stack_trace);
 
   // Reset pointers so accesses after destruction reliably crash.
   pref_value_store_.reset();
