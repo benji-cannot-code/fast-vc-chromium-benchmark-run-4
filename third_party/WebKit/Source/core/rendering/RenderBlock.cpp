dd11e36f52a60c508c5fee9d314aa237e13dc7bd
robhogan@gmail.com <robhogan@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-20 22:48:05 +0000

    Strip anonymous blocks when change in style removes need for them
    When a block element becomes a float or out of flow we should strip
    any anonymous blocks wrapping its inline siblings.
    
    BUG=331251
    
    Review URL: https://codereview.chromium.org/253313005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174420 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderBlock.cpp
+++ third_party/WebKit/Source/core/rendering/RenderBlock.cpp
@@ -329,6 +329,9 @@ void RenderBlock::styleDidChange(StyleDifference diff, const RenderStyle* oldSty
 {
     RenderBox::styleDidChange(diff, oldStyle);
 
+    if (isFloatingOrOutOfFlowPositioned() && oldStyle && !oldStyle->isFloating() && !oldStyle->hasOutOfFlowPosition() && parent() && parent()->isRenderBlockFlow())
+        toRenderBlock(parent())->removeAnonymousWrappersIfRequired();
+
     RenderStyle* newStyle = style();
 
     if (!isAnonymousBlock()) {
@@ -1076,6 +1079,21 @@ static bool canMergeContiguousAnonymousBlocks(RenderObject* oldChild, RenderObje
     // Make sure the types of the anonymous blocks match up.
     return prev->isAnonymousColumnsBlock() == next->isAnonymousColumnsBlock()
            && prev->isAnonymousColumnSpanBlock() == next->isAnonymousColumnSpanBlock();
+}
+
+void RenderBlock::removeAnonymousWrappersIfRequired()
+{
+    RenderBox* child = firstChildBox();
+    while (child && (child->isAnonymousBlock() || child->isFloatingOrOutOfFlowPositioned())) {
+        RenderBox* next = child->nextSiblingBox();
+        // A continuation means the wrappers are still required.
+        if (next && next->isRenderBlock() && toRenderBlock(next)->continuation())
+            return;
+        if (child->isAnonymousBlock())
+            collapseAnonymousBlockChild(this, toRenderBlock(child));
+        // |child| may have been destroyed.
+        child = next;
+    }
 }
 
 void RenderBlock::collapseAnonymousBlockChild(RenderBlock* parent, RenderBlock* child)
--- third_party/WebKit/Source/core/rendering/RenderBlock.h
+++ third_party/WebKit/Source/core/rendering/RenderBlock.h
@@ -369,6 +369,8 @@ private:
 
     void addChildIgnoringAnonymousColumnBlocks(RenderObject* newChild, RenderObject* beforeChild = 0);
 
+    void removeAnonymousWrappersIfRequired();
+
     virtual bool isSelfCollapsingBlock() const OVERRIDE;
 
     void insertIntoTrackedRendererMaps(RenderBox* descendant, TrackedDescendantsMap*&, TrackedContainerMap*&);
