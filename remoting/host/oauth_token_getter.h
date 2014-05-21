30f6bd90ca36a9829cd1a82ae055e430215577af
rmsousa@chromium.org <rmsousa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-21 01:39:47 +0000

    Add auto-refresh support to OAuthTokenGetter (disabled for now).
    Some fixes to GetUserInfo flow to avoid superfluous GetUserInfo calls.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/294813002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@271795 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/oauth_token_getter.h
+++ remoting/host/oauth_token_getter.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/callback.h"
 #include "base/threading/non_thread_safe.h"
+#include "base/time/time.h"
+#include "base/timer/timer.h"
 #include "google_apis/gaia/gaia_oauth_client.h"
 
 namespace net {
@@ -57,7 +59,8 @@ class OAuthTokenGetter :
 
   OAuthTokenGetter(
       scoped_ptr<OAuthCredentials> oauth_credentials,
-      scoped_refptr<net::URLRequestContextGetter> url_request_context_getter);
+      scoped_refptr<net::URLRequestContextGetter> url_request_context_getter,
+      bool auto_refresh);
   virtual ~OAuthTokenGetter();
 
   // Call |on_access_token| with an access token, or the failure status.
@@ -85,8 +88,10 @@ class OAuthTokenGetter :
 
   bool refreshing_oauth_token_;
   std::string oauth_access_token_;
+  std::string verified_email_;
   base::Time auth_token_expiry_time_;
   std::queue<OAuthTokenGetter::TokenCallback> pending_callbacks_;
+  scoped_ptr<base::OneShotTimer<OAuthTokenGetter> > refresh_timer_;
 
   DISALLOW_COPY_AND_ASSIGN(OAuthTokenGetter);
 };
