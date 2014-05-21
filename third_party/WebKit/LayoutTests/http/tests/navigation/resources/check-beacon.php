79efb5888ab6d561dd7e4f23d5b4018999194136
sigbjornf@opera.com <sigbjornf@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-21 22:02:22 +0000

    Save ping+beacon test status in temporary directory.
    Try to address occasional Windows-only test failures by explicitly saving
    away the ping + beacon requests in the system temporary directory. Also
    make read access to the test status file a bit more robust.
    
    R=
    BUG=374569
    
    Review URL: https://codereview.chromium.org/286063003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174484 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/navigation/resources/check-beacon.php
+++ third_party/WebKit/LayoutTests/http/tests/navigation/resources/check-beacon.php
@@ -1,7 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php
-$beaconFilename = "beacon" . (isset($_REQUEST['name']) ? $_REQUEST['name'] : "") . ".txt";
-$retries = isset($_REQUEST['retries']) ? (int)$_REQUEST['retries'] : -1;
+require_once '../../resources/portabilityLayer.php';
+
+$beaconFilename = sys_get_temp_dir() . "/beacon" . (isset($_REQUEST['name']) ? $_REQUEST['name'] : "") . ".txt";
+
+$max_attempts = 700;
+$retries = isset($_REQUEST['retries']) ? (int)$_REQUEST['retries'] : $max_attempts;
 while (!file_exists($beaconFilename) && $retries != 0) {
     usleep(10000);
     # file_exists() caches results, we want to invalidate the cache.
@@ -12,15 +16,22 @@ while (!file_exists($beaconFilename) && $retries != 0) {
 header('Content-Type: text/plain');
 header('Access-Control-Allow-Origin: *');
 if (file_exists($beaconFilename)) {
-    echo "Beacon sent successfully\n";
-    $beaconFile = fopen($beaconFilename, 'r');
-    while ($line = fgets($beaconFile)) {
-        $trimmed = trim($line);
-        if ($trimmed != "")
-            echo "$trimmed\n";
+    $beaconFile = false;
+    if (is_readable($beaconFilename)) {
+        $beaconFile = fopen($beaconFilename, 'r');
     }
-    fclose($beaconFile);
-    unlink($beaconFilename);
+    if ($beaconFile) {
+        echo "Beacon sent successfully\n";
+        while ($line = fgets($beaconFile)) {
+            $trimmed = trim($line);
+            if ($trimmed != "")
+                echo "$trimmed\n";
+        }
+        fclose($beaconFile);
+        unlink($beaconFilename);
+    } else {
+        echo "Beacon status not readable\n";
+    }
 } else {
     echo "Beacon not sent\n";
 }
--- third_party/WebKit/LayoutTests/http/tests/navigation/resources/save-beacon.php
+++ third_party/WebKit/LayoutTests/http/tests/navigation/resources/save-beacon.php
@@ -1,10 +1,12 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php
+require_once '../../resources/portabilityLayer.php';
+
 function prettify($name) {
    return str_replace(' ', '-', ucwords(str_replace('_', ' ', str_replace('http_', '', strtolower($name)))));
 }
 
-$beaconFilename = "beacon" . (isset($_REQUEST['name']) ? $_REQUEST['name'] : "") . ".txt";
+$beaconFilename = sys_get_temp_dir() . "/beacon" . (isset($_REQUEST['name']) ? $_REQUEST['name'] : "") . ".txt";
 $beaconFile = fopen($beaconFilename . ".tmp", 'w');
 $httpHeaders = $_SERVER;
 ksort($httpHeaders, SORT_STRING);
