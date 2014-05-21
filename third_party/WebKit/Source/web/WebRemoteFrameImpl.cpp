230913ab9348705e12fd35215e9c0f02ced82f81
simonhatch@chromium.org <simonhatch@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-21 20:30:29 +0000

    Navigation transitions: Added "addStyleSheetByURL" function to insert stylesheet links.
    If transition-entering-stylesheet is defined in the response headers for the incoming document, this stylesheet will be applied as the incoming document is shown.
    
    This is the blink side of the CL.
    
    Design doc: https://docs.google.com/a/chromium.org/document/d/17jg1RRL3RI969cLwbKBIcoGDsPwqaEdBxafGNYGwiY4/edit# 
    Implementation details: https://docs.google.com/a/chromium.org/document/d/1kREPtFJaeLoDKwrfmrYTD7DHCdxX1RzFBga2gNY8lyE/edit#heading=h.bng2kpmyvxq5 
    BUG=370696
    
    Review URL: https://codereview.chromium.org/285623003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174470 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebRemoteFrameImpl.cpp
+++ third_party/WebKit/Source/web/WebRemoteFrameImpl.cpp
@@ -531,6 +531,11 @@ void WebRemoteFrameImpl::extendSelectionAndDelete(int before, int after)
     ASSERT_NOT_REACHED();
 }
 
+void WebRemoteFrameImpl::addStyleSheetByURL(const WebString& url)
+{
+    ASSERT_NOT_REACHED();
+}
+
 void WebRemoteFrameImpl::setCaretVisible(bool)
 {
     ASSERT_NOT_REACHED();
