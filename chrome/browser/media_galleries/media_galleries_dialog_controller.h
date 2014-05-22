1e756416633da1804b272acae0ef95b83bda57c8
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-22 18:16:31 +0000

    Media Galleries: Refactor DeviceMediaAsyncFileUtil.
    And other misc cleanups.
    
    Review URL: https://codereview.chromium.org/298843002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272239 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media_galleries/media_galleries_dialog_controller.h
+++ chrome/browser/media_galleries/media_galleries_dialog_controller.h
@@ -6,8 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_MEDIA_GALLERIES_MEDIA_GALLERIES_DIALOG_CONTROLLER_H_
 #define CHROME_BROWSER_MEDIA_GALLERIES_MEDIA_GALLERIES_DIALOG_CONTROLLER_H_
 
-#include <list>
 #include <map>
+#include <string>
+#include <vector>
 
 #include "base/callback.h"
 #include "base/memory/scoped_ptr.h"
