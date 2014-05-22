008ad163713bc394f13cbc07e989539baea29a33
alexst@chromium.org <alexst@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-22 18:28:30 +0000

    Pass information about overlay support in a mailbox from blink to CC.
    BUG=372449
    
    Review URL: https://codereview.chromium.org/282673002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272246 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/renderer/compositor_bindings/web_external_texture_layer_impl.cc
+++ webkit/renderer/compositor_bindings/web_external_texture_layer_impl.cc
@@ -83,6 +83,7 @@ bool WebExternalTextureLayerImpl::PrepareTextureMailbox(
     *mailbox =
         cc::TextureMailbox(name, GL_TEXTURE_2D, client_mailbox.syncPoint);
   }
+  mailbox->set_allow_overlay(client_mailbox.allowOverlay);
 
   if (mailbox->IsValid()) {
     *release_callback = cc::SingleReleaseCallback::Create(base::Bind(
