77e894d5073df8896aad0a6de2238551af856547
stevenjb@chromium.org <stevenjb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-22 06:21:40 +0000

    Add Device properties for Cellular to MNCH for networkingPrivate
    BUG=374003
    
    Review URL: https://codereview.chromium.org/291553002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272119 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/network/device_state.cc
+++ chromeos/network/device_state.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/values.h"
 #include "chromeos/network/network_event_log.h"
+#include "chromeos/network/shill_property_util.h"
 #include "third_party/cros_system_api/dbus/service_constants.h"
 
 namespace chromeos {
@@ -46,28 +47,8 @@ bool DeviceState::PropertyChanged(const std::string& key,
   } else if (key == shill::kProviderRequiresRoamingProperty) {
     return GetBooleanValue(key, value, &provider_requires_roaming_);
   } else if (key == shill::kHomeProviderProperty) {
-    const base::DictionaryValue* dict = NULL;
-    if (!value.GetAsDictionary(&dict))
-      return false;
-    std::string home_provider_country;
-    std::string home_provider_name;
-    dict->GetStringWithoutPathExpansion(shill::kOperatorCountryKey,
-                                        &home_provider_country);
-    dict->GetStringWithoutPathExpansion(shill::kOperatorNameKey,
-                                        &home_provider_name);
-    // Set home_provider_id_
-    if (!home_provider_name.empty() && !home_provider_country.empty()) {
-      home_provider_id_ = base::StringPrintf(
-          "%s (%s)",
-          home_provider_name.c_str(),
-          home_provider_country.c_str());
-    } else {
-      dict->GetStringWithoutPathExpansion(shill::kOperatorCodeKey,
-                                          &home_provider_id_);
-      LOG(WARNING) << "Carrier ID not defined, using code instead: "
-                   << home_provider_id_;
-    }
-    return true;
+    return shill_property_util::GetHomeProviderFromProperty(
+        value, &home_provider_id_);
   } else if (key == shill::kTechnologyFamilyProperty) {
     return GetStringValue(key, value, &technology_family_);
   } else if (key == shill::kCarrierProperty) {
