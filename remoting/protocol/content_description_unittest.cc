537dd7fa90944e0440a59b111fd9942550f7e862
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-22 21:31:20 +0000

    Switch CandidateSession to use lists rather than vectors.
    This makes it possible to add candidate configurations at either the
    back (least-preferred) or front (most-preferred) of each channel's
    candidate list.
    
    This CL also disables VP9, by default, in candidate session
    configurations, replaces DisableVideoCodec with 
    EnableVideoCodec, and makes the Enable/Disable<foo>Codec
    helpers members of CandidateSessionConfig..
    
    Review URL: https://codereview.chromium.org/292093002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272324 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/protocol/content_description_unittest.cc
+++ remoting/protocol/content_description_unittest.cc
@@ -52,7 +52,7 @@ TEST(ContentDescriptionTest, ParseUnknown) {
       ContentDescription::ParseXml(xml.get()));
   ASSERT_TRUE(parsed.get());
   EXPECT_EQ(1U, parsed->config()->event_configs().size());
-  EXPECT_TRUE(parsed->config()->event_configs()[0] ==
+  EXPECT_TRUE(parsed->config()->event_configs().front() ==
               ChannelConfig(ChannelConfig::TRANSPORT_STREAM,
                             kDefaultStreamVersion,
                             ChannelConfig::CODEC_UNDEFINED));
@@ -75,7 +75,7 @@ TEST(ContentDescriptionTest, NoneTransport) {
       ContentDescription::ParseXml(xml.get()));
   ASSERT_TRUE(parsed.get());
   EXPECT_EQ(1U, parsed->config()->audio_configs().size());
-  EXPECT_TRUE(parsed->config()->audio_configs()[0] == ChannelConfig());
+  EXPECT_TRUE(parsed->config()->audio_configs().front() == ChannelConfig());
 }
 
 // Verify that we can parse configs with none transport with version and
@@ -95,7 +95,7 @@ TEST(ContentDescriptionTest, NoneTransportWithCodec) {
       ContentDescription::ParseXml(xml.get()));
   ASSERT_TRUE(parsed.get());
   EXPECT_EQ(1U, parsed->config()->audio_configs().size());
-  EXPECT_TRUE(parsed->config()->audio_configs()[0] == ChannelConfig());
+  EXPECT_TRUE(parsed->config()->audio_configs().front() == ChannelConfig());
 }
 
 }  // namespace protocol
