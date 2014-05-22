dd5c302e41995a23e22802629b9f1aa9866b77cd
yzshen@chromium.org <yzshen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-22 03:00:30 +0000

    Improve compiler output for users who forget to include TypeConverter specializations.
    Currently when users forget to include their TypeConverter specializations, they get errors about various methods not defined in TypeConverter<x, y>. By removing the definition of the general template, users will get errors about incomplete type TypeConverter<x, y> used.
    
    TEST=None
    BUG=None
    
    Review URL: https://codereview.chromium.org/294473010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272054 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/public/cpp/bindings/type_converter.h
+++ mojo/public/cpp/bindings/type_converter.h
@@ -8,9 +8,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace mojo {
 
-// Specialize to perform type conversion for Mojom-defined structs and arrays.
-// Here, T is the Mojom-defined struct or array, and U is some other non-Mojom
+// Specialize the following class:
+//   template <typename T, typename U> class TypeConverter;
+// to perform type conversion for Mojom-defined structs and arrays. Here, T is
+// the Mojom-defined struct or array, and U is some other non-Mojom
 // struct or array type.
+//
+// Specializations should implement the following interface:
+//   namespace mojo {
+//   template <>
+//   class TypeConverter<T, U> {
+//    public:
+//     static T ConvertFrom(const U& input, Buffer* buf);
+//     static U ConvertTo(const T& input);
+//
+//     // Maybe (mutually exclusive):
+//     MOJO_ALLOW_IMPLICIT_TYPE_CONVERSION();
+//     MOJO_INHERIT_IMPLICIT_TYPE_CONVERSION(X, Y);
+//   };
+//   }
 //
 // EXAMPLE:
 //
@@ -95,14 +111,7 @@ namespace mojo {
 // Although these macros are convenient, they make conversions less obvious.
 // Users may do conversions excessively without paying attention to the cost. So
 // please use them wisely.
-template <typename T, typename U> class TypeConverter {
-  // static T ConvertFrom(const U& input, Buffer* buf);
-  // static U ConvertTo(const T& input);
-
-  // Maybe (mutually exclusive):
-  // MOJO_ALLOW_IMPLICIT_TYPE_CONVERSION();
-  // MOJO_INHERIT_IMPLICIT_TYPE_CONVERSION(X, Y);
-};
+template <typename T, typename U> class TypeConverter;
 
 }  // namespace mojo
 
