929ddf29fa1b9d09e77746676483eafeac525804
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-22 10:14:56 +0000

    Mojo: Make channel.cc and friend log at a less verbose level.
    Currently, ever renderer shutdown results in 2 lines of LOG(ERROR)s.
    BUG=370731
    
    Review URL: https://codereview.chromium.org/297753005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272154 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/system/channel.cc
+++ mojo/system/channel.cc
@@ -294,7 +294,7 @@ void Channel::OnReadMessage(
 }
 
 void Channel::OnFatalError(FatalError fatal_error) {
-  LOG(ERROR) << "RawChannel fatal error (type " << fatal_error << ")";
+  LOG(WARNING) << "RawChannel fatal error (type " << fatal_error << ")";
   Shutdown();
 }
 
