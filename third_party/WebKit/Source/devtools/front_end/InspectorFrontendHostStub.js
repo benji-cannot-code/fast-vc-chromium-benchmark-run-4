benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit bbebb846685344c49b55f238eaf1190eb2a6db24

[DevTools] Switch from insets to bounds to position inspected page.
BUG=373669

Review URL: https://codereview.chromium.org/223803003

git-svn-id: svn://svn.chromium.org/blink/trunk@174361 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/devtools/front_end/InspectorFrontendHostStub.js
@@ -92,6 +92,14 @@ WebInspector.InspectorFrontendHostStub.prototype = {
     },
 
     /**
+     * Requests inspected page to be placed atop of the inspector frontend with specified bounds.
+     * @param {{x: number, y: number, width: number, height: number}} bounds
+     */
+    setInspectedPageBounds: function(bounds)
+    {
+    },
+
+    /**
      * Requests inspected page to be placed atop of the inspector frontend
      * with passed insets from the frontend sides, respecting minimum size passed.
      * @param {{top: number, left: number, right: number, bottom: number}} insets

third_party/WebKit/Source/devtools/front_end/externs.js
@@ -197,6 +197,11 @@ InspectorFrontendHostAPI.prototype.getSelectionForegroundColor = function() {}
 /** @return {boolean} */
 InspectorFrontendHost.isUnderTest = function() {}
 /**
+ * Requests inspected page to be placed atop of the inspector frontend with specified bounds.
+ * @param {{x: number, y: number, width: number, height: number}} bounds
+ */
+InspectorFrontendHostAPI.prototype.setInspectedPageBounds = function(bounds) {}
+/**
  * Requests inspected page to be placed atop of the inspector frontend
  * with passed insets from the frontend sides, respecting minimum size passed.
  * @param {{top: number, left: number, right: number, bottom: number}} insets

third_party/WebKit/Source/devtools/front_end/ui/InspectedPagePlaceholder.js
@@ -11,13 +11,8 @@ WebInspector.InspectedPagePlaceholder = function()
 {
     WebInspector.View.call(this);
     WebInspector.zoomManager.addEventListener(WebInspector.ZoomManager.Events.ZoomChanged, this._onZoomChanged, this);
-    this._margins = { top: false, right: false, bottom: false, left: false };
-    this.setMinimumSize(WebInspector.InspectedPagePlaceholder.Constraints.Width, WebInspector.InspectedPagePlaceholder.Constraints.Height);
-};
-
-WebInspector.InspectedPagePlaceholder.Constraints = {
-    Width: 50,
-    Height: 50
+    this._margins = { top: 0, right: 0, bottom: 0, left: 0 };
+    this.setMinimumSize(50, 50);
 };
 
 WebInspector.InspectedPagePlaceholder.MarginValue = 3;
@@ -25,8 +20,8 @@ WebInspector.InspectedPagePlaceholder.MarginValue = 3;
 WebInspector.InspectedPagePlaceholder.prototype = {
     _findMargins: function()
     {
-        var margins = { top: false, right: false, bottom: false, left: false };
-        var adjacent = { top: true, right: true, bottom: true, left: true};
+        var margins = { top: 0, right: 0, bottom: 0, left: 0 };
+        var adjacent = { top: true, right: true, bottom: true, left: true };
         var view = this;
         while (view.parentView()) {
             var parent = view.parentView();
@@ -35,7 +30,7 @@ WebInspector.InspectedPagePlaceholder.prototype = {
             if (parent instanceof WebInspector.SplitView) {
                 var side = parent.sidebarSide();
                 if (adjacent[side] && !parent.hasCustomResizer())
-                    margins[side] = true;
+                    margins[side] = WebInspector.InspectedPagePlaceholder.MarginValue;
                 adjacent[side] = false;
             }
             view = parent;
@@ -43,23 +38,12 @@ WebInspector.InspectedPagePlaceholder.prototype = {
 
         if (this._margins.top !== margins.top || this._margins.left !== margins.left || this._margins.right !== margins.right || this._margins.bottom !== margins.bottom) {
             this._margins = margins;
-            this._updateMarginValue();
+            this._scheduleUpdate();
         }
-    },
-
-    _updateMarginValue: function()
-    {
-        var marginValue = Math.round(WebInspector.InspectedPagePlaceholder.MarginValue / WebInspector.zoomManager.zoomFactor()) + "px ";
-        var margins = this._margins.top ? marginValue : "0 ";
-        margins += this._margins.right ? marginValue : "0 ";
-        margins += this._margins.bottom ? marginValue : "0 ";
-        margins += this._margins.left ? marginValue : "0 ";
-        this.element.style.margin = margins;
     },
 
     _onZoomChanged: function()
     {
-        this._updateMarginValue();
         this._scheduleUpdate();
     },
 
@@ -79,38 +63,27 @@ WebInspector.InspectedPagePlaceholder.prototype = {
         }
     },
 
+    _dipPageRect: function()
+    {
+        var zoomFactor = WebInspector.zoomManager.zoomFactor();
+        var rect = this.element.getBoundingClientRect();
+        var bodyRect = document.body.getBoundingClientRect();
+
+        var left = Math.max(rect.left * zoomFactor + this._margins.left, bodyRect.left * zoomFactor);
+        var top = Math.max(rect.top * zoomFactor + this._margins.top, bodyRect.top * zoomFactor);
+        var bottom = Math.min(rect.bottom * zoomFactor - this._margins.bottom, bodyRect.bottom * zoomFactor);
+        var right = Math.min(rect.right * zoomFactor - this._margins.right, bodyRect.right * zoomFactor);
+
+        return { x: left, y: top, width: right - left, height: bottom - top };
+    },
+
     _update: function()
     {
         delete this._updateId;
 
-        var zoomFactor = WebInspector.zoomManager.zoomFactor();
-
-        var marginValue = WebInspector.InspectedPagePlaceholder.MarginValue;
-        var insets = {
-            top: this._margins.top ? marginValue : 0,
-            left: this._margins.left ? marginValue : 0,
-            right: this._margins.right ? marginValue : 0,
-            bottom: this._margins.bottom ? marginValue : 0};
-
-        var minSize = {
-            width: WebInspector.InspectedPagePlaceholder.Constraints.Width - Math.round(insets.left * zoomFactor) - Math.round(insets.right * zoomFactor),
-            height: WebInspector.InspectedPagePlaceholder.Constraints.Height - Math.round(insets.top * zoomFactor) - Math.round(insets.bottom * zoomFactor)};
-
-        // This view assumes it's always inside the main split view element, not a sidebar.
-        var view = this;
-        while (view) {
-            if ((view instanceof WebInspector.SplitView) && view.sidebarSide())
-                insets[view.sidebarSide()] += view.preferredSidebarSize();
-            view = view.parentView();
-        }
-
-        var roundedInsets = {
-            top: Math.ceil(insets.top),
-            left: Math.ceil(insets.left),
-            right: Math.ceil(insets.right),
-            bottom: Math.ceil(insets.bottom)};
-
-        InspectorFrontendHost.setContentsResizingStrategy(roundedInsets, minSize);
+        var rect = this._dipPageRect();
+        var bounds = { x: Math.round(rect.x), y: Math.round(rect.y), height: Math.round(rect.height), width: Math.round(rect.width) };
+        InspectorFrontendHost.setInspectedPageBounds(bounds);
     },
 
     __proto__: WebInspector.View.prototype

third_party/WebKit/Source/web/InspectorFrontendClientImpl.cpp
@@ -122,6 +122,7 @@ void InspectorFrontendClientImpl::windowObjectCleared()
             "     ['searchInPath', 3],"
             "     ['setWhitelistedShortcuts', 1],"
             "     ['setContentsResizingStrategy', 2],"
+            "     ['setInspectedPageBounds', 1],"
             "     ['setIsDocked', 1],"
             "     ['subscribe', 1],"
             "     ['stopIndexing', 1],"

commit 01b4da5e63d582cc36a26197112d4be4ac20495d

[DevTools] Fix regression introduced in r174361.
After switching from insets to bounds, we are dependent on the right
window size when docking/undocking.
So, we should not update page bounds until we got final window size.

BUG=373669

Review URL: https://codereview.chromium.org/287053005

git-svn-id: svn://svn.chromium.org/blink/trunk@174588 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/devtools/front_end/InspectorFrontendHostStub.js
@@ -87,7 +87,7 @@ WebInspector.InspectorFrontendHostStub.prototype = {
         this._windowVisible = false;
     },
 
-    setIsDocked: function(isDocked)
+    setIsDocked: function(isDocked, callback)
     {
     },
 

third_party/WebKit/Source/devtools/front_end/externs.js
@@ -240,7 +240,7 @@ InspectorFrontendHostAPI.prototype.recordPanelShown = function(panelCode) {}
 InspectorFrontendHostAPI.prototype.sendMessageToBackend = function(message) {}
 InspectorFrontendHostAPI.prototype.sendMessageToEmbedder = function(message) {}
 InspectorFrontendHostAPI.prototype.setInjectedScriptForOrigin = function(origin, script) {}
-InspectorFrontendHostAPI.prototype.setIsDocked = function(isDocked) {}
+InspectorFrontendHostAPI.prototype.setIsDocked = function(isDocked, callback) {}
 InspectorFrontendHostAPI.prototype.setZoomFactor = function(zoom) {}
 InspectorFrontendHostAPI.prototype.subscribe = function(eventType) {}
 InspectorFrontendHostAPI.prototype.unsubscribe = function(eventType) {}

third_party/WebKit/Source/devtools/front_end/ui/DockController.js
@@ -67,8 +67,13 @@ WebInspector.DockController.State = {
     Undocked: "undocked"
 }
 
+// Use BeforeDockSideChanged to do something before all the UI bits are updated,
+// DockSideChanged to update UI, and AfterDockSideChanged to perform actions
+// after frontend is docked/undocked in the browser.
 WebInspector.DockController.Events = {
-    DockSideChanged: "DockSideChanged"
+    BeforeDockSideChanged: "BeforeDockSideChanged",
+    DockSideChanged: "DockSideChanged",
+    AfterDockSideChanged: "AfterDockSideChanged"
 }
 
 WebInspector.DockController.prototype = {
@@ -112,12 +117,16 @@ WebInspector.DockController.prototype = {
         if (this._dockSide === dockSide)
             return;
 
+        this.dispatchEventToListeners(WebInspector.DockController.Events.BeforeDockSideChanged, dockSide);
+        InspectorFrontendHost.setIsDocked(dockSide !== WebInspector.DockController.State.Undocked, this._setIsDockedResponse.bind(this));
         this._dockSide = dockSide;
         this._updateUI();
         this.dispatchEventToListeners(WebInspector.DockController.Events.DockSideChanged, this._dockSide);
+    },
 
-        if (this._canDock)
-            InspectorFrontendHost.setIsDocked(dockSide !== WebInspector.DockController.State.Undocked);
+    _setIsDockedResponse: function()
+    {
+        this.dispatchEventToListeners(WebInspector.DockController.Events.AfterDockSideChanged, this._dockSide);
     },
 
     _updateUI: function()

third_party/WebKit/Source/devtools/front_end/ui/InspectedPagePlaceholder.js
@@ -14,6 +14,8 @@ WebInspector.InspectedPagePlaceholder = function()
     WebInspector.zoomManager.addEventListener(WebInspector.ZoomManager.Events.ZoomChanged, this._onZoomChanged, this);
     this._margins = { top: 0, right: 0, bottom: 0, left: 0 };
     this.restoreMinimumSizeAndMargins();
+    WebInspector.dockController.addEventListener(WebInspector.DockController.Events.BeforeDockSideChanged, this._beforeDockSideChange, this);
+    WebInspector.dockController.addEventListener(WebInspector.DockController.Events.AfterDockSideChanged, this._afterDockSideChange, this);
 };
 
 WebInspector.InspectedPagePlaceholder.MarginValue = 3;
@@ -59,6 +61,9 @@ WebInspector.InspectedPagePlaceholder.prototype = {
 
     _scheduleUpdate: function()
     {
+        if (this._noUpdates)
+            return;
+
         var dockSide = WebInspector.dockController.dockSide();
         if (dockSide !== WebInspector.DockController.State.Undocked) {
             if (this._updateId)
@@ -119,10 +124,23 @@ WebInspector.InspectedPagePlaceholder.prototype = {
     _update: function()
     {
         delete this._updateId;
+        if (this._noUpdates)
+            return;
 
         var rect = this._dipPageRect();
         var bounds = { x: Math.round(rect.x), y: Math.round(rect.y), height: Math.round(rect.height), width: Math.round(rect.width) };
         InspectorFrontendHost.setInspectedPageBounds(bounds);
+    },
+
+    _beforeDockSideChange: function()
+    {
+        this._noUpdates = true;
+    },
+
+    _afterDockSideChange: function()
+    {
+        this._noUpdates = false;
+        this._scheduleUpdate();
     },
 
     __proto__: WebInspector.View.prototype