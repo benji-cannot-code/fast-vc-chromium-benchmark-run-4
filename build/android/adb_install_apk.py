6bc719f3f44f94432a6e73abe8cc9a32eb007894
jbudorick@chromium.org <jbudorick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-22 11:13:40 +0000

    [Android] Support generic parallel execution across devices.
    BUG=267773
    
    Review URL: https://codereview.chromium.org/290573004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272159 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/adb_install_apk.py
+++ build/android/adb_install_apk.py
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 """Utility script to install APKs from the command line quickly."""
 
-import multiprocessing
 import optparse
 import os
 import sys
@@ -51,13 +50,6 @@ def ValidateInstallAPKOption(option_parser, options):
                                options.apk)
 
 
-def _InstallApk(args):
-  apk_path, apk_package, keep_data, device = args
-  device_utils.DeviceUtils(device=device).old_interface.ManagedInstall(
-      apk_path, keep_data, apk_package)
-  print '-----  Installed on %s  -----' % device
-
-
 def main(argv):
   parser = optparse.OptionParser()
   AddInstallAPKOption(parser)
@@ -74,13 +66,10 @@ def main(argv):
   if not options.apk_package:
     options.apk_package = apk_helper.GetPackageName(options.apk)
 
-  pool = multiprocessing.Pool(len(devices))
-  # Send a tuple (apk_path, apk_package, device) per device.
-  pool.map(_InstallApk, zip([options.apk] * len(devices),
-                            [options.apk_package] * len(devices),
-                            [options.keep_data] * len(devices),
-                            devices))
+  device_utils.DeviceUtils.parallel(devices).old_interface.ManagedInstall(
+      options.apk, options.keep_data, options.apk_package).pFinish(None)
 
 
 if __name__ == '__main__':
   sys.exit(main(sys.argv))
+
