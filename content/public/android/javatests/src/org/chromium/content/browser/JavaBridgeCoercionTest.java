0c773e5469fe07763bb8125b21002149acbbe0f4
anton@chromium.org <anton@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-22 17:53:28 +0000

    Java bridge relax case requirement on "inf" and "nan".
    Needed for 64-bit which is using a newer version of the runtime.
    
    Prior to this CL the test requires that Infinity is converted to the string "Inf" and NaN is converted to the string "NaN". However, these conversions are done by a
    format %g option which is defined as providing a lower case string (http://en.wikipedia.org/wiki/Printf_format_string).
    
    As LiveConnect provides "no guarantee" as to number formatting (https://jdk6.java.net/plugin2/liveconnect/#JS_NUMBER_VALUE) it seems best to just relax these tests.
    
    BUG=346626, 354405
    
    Review URL: https://codereview.chromium.org/292183006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272229 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/javatests/src/org/chromium/content/browser/JavaBridgeCoercionTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/JavaBridgeCoercionTest.java
@@ -313,7 +313,7 @@ public class JavaBridgeCoercionTest extends JavaBridgeTestBase {
         assertNull(mTestObject.waitForObjectValue());
 
         executeJavaScript("testObject.setStringValue(Number.NaN);");
-        assertEquals("NaN", mTestObject.waitForStringValue());
+        assertTrue("nan".equalsIgnoreCase(mTestObject.waitForStringValue()));
 
         executeJavaScript("testObject.setBooleanValue(Number.NaN);");
         assertFalse(mTestObject.waitForBooleanValue());
@@ -354,7 +354,7 @@ public class JavaBridgeCoercionTest extends JavaBridgeTestBase {
         assertNull(mTestObject.waitForObjectValue());
 
         executeJavaScript("testObject.setStringValue(Infinity);");
-        assertEquals("Inf", mTestObject.waitForStringValue());
+        assertTrue("inf".equalsIgnoreCase(mTestObject.waitForStringValue()));
 
         executeJavaScript("testObject.setBooleanValue(Infinity);");
         assertFalse(mTestObject.waitForBooleanValue());
