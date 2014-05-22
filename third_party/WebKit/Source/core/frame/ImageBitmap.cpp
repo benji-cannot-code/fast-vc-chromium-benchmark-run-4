eb09ccadc69c5695b57e56bc2fe654381d5a2cf3
keishi@chromium.org <keishi@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-22 03:17:38 +0000

    Oilpan: Prepare moving HTMLImageElement to oilpan.
    BUG=357163
    
    Review URL: https://codereview.chromium.org/290333004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174517 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/ImageBitmap.cpp
+++ third_party/WebKit/Source/core/frame/ImageBitmap.cpp
@@ -146,8 +146,10 @@ ImageBitmap::ImageBitmap(Image* image, const IntRect& cropRect)
 
 ImageBitmap::~ImageBitmap()
 {
+#if !ENABLE(OILPAN)
     if (m_imageElement)
         m_imageElement->removeClient(this);
+#endif
 }
 
 PassRefPtrWillBeRawPtr<ImageBitmap> ImageBitmap::create(HTMLImageElement* image, const IntRect& cropRect)
@@ -227,6 +229,7 @@ FloatSize ImageBitmap::sourceSize() const
 
 void ImageBitmap::trace(Visitor* visitor)
 {
+    visitor->trace(m_imageElement);
     ImageLoaderClient::trace(visitor);
 }
 
