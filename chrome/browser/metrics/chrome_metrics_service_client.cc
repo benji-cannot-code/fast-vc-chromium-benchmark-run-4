acaada5d3caf538235453fd68ede6559007e9793
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-22 03:59:18 +0000

    [Metrics] Move non-idle notifications from MetricsService to ChromeMetricsServiceClient
    BUG=374207
    TEST=none
    R=asvitkine@chromium.org
    
    Review URL: https://codereview.chromium.org/287193005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272083 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metrics/chrome_metrics_service_client.cc
+++ chrome/browser/metrics/chrome_metrics_service_client.cc
@@ -7,10 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/google/google_util.h"
+#include "chrome/browser/metrics/metrics_service.h"
 #include "chrome/browser/ui/browser_otr_state.h"
 #include "chrome/common/chrome_version_info.h"
 #include "chrome/common/crash_keys.h"
+#include "content/public/browser/notification_service.h"
 
 namespace {
 
@@ -34,7 +37,8 @@ metrics::SystemProfileProto::Channel AsProtobufChannel(
 
 }  // namespace
 
-ChromeMetricsServiceClient::ChromeMetricsServiceClient() {
+ChromeMetricsServiceClient::ChromeMetricsServiceClient() : service_(NULL) {
+  RegisterForNotifications();
 }
 
 ChromeMetricsServiceClient::~ChromeMetricsServiceClient() {
@@ -63,4 +67,51 @@ metrics::SystemProfileProto::Channel ChromeMetricsServiceClient::GetChannel() {
 std::string ChromeMetricsServiceClient::GetVersionString() {
   // TODO(asvitkine): Move over from metrics_log.cc
   return std::string();
+}
+
+void ChromeMetricsServiceClient::RegisterForNotifications() {
+  registrar_.Add(this, chrome::NOTIFICATION_BROWSER_OPENED,
+                 content::NotificationService::AllBrowserContextsAndSources());
+  registrar_.Add(this, chrome::NOTIFICATION_BROWSER_CLOSED,
+                 content::NotificationService::AllSources());
+  registrar_.Add(this, chrome::NOTIFICATION_TAB_PARENTED,
+                 content::NotificationService::AllSources());
+  registrar_.Add(this, chrome::NOTIFICATION_TAB_CLOSING,
+                 content::NotificationService::AllSources());
+  registrar_.Add(this, content::NOTIFICATION_LOAD_START,
+                 content::NotificationService::AllSources());
+  registrar_.Add(this, content::NOTIFICATION_LOAD_STOP,
+                 content::NotificationService::AllSources());
+  registrar_.Add(this, content::NOTIFICATION_RENDERER_PROCESS_CLOSED,
+                 content::NotificationService::AllSources());
+  registrar_.Add(this, content::NOTIFICATION_RENDER_WIDGET_HOST_HANG,
+                 content::NotificationService::AllSources());
+  registrar_.Add(this, chrome::NOTIFICATION_OMNIBOX_OPENED_URL,
+                 content::NotificationService::AllSources());
+}
+
+void ChromeMetricsServiceClient::Observe(
+    int type,
+    const content::NotificationSource& source,
+    const content::NotificationDetails& details) {
+  DCHECK(thread_checker_.CalledOnValidThread());
+
+  switch (type) {
+    case chrome::NOTIFICATION_BROWSER_OPENED:
+    case chrome::NOTIFICATION_BROWSER_CLOSED:
+    case chrome::NOTIFICATION_OMNIBOX_OPENED_URL:
+    case chrome::NOTIFICATION_TAB_PARENTED:
+    case chrome::NOTIFICATION_TAB_CLOSING:
+    case content::NOTIFICATION_LOAD_STOP:
+    case content::NOTIFICATION_LOAD_START:
+    case content::NOTIFICATION_RENDERER_PROCESS_CLOSED:
+    case content::NOTIFICATION_RENDER_WIDGET_HOST_HANG:
+      // TODO(isherman): Remove this NULL check: http://crbug.com/375248
+      if (service_)
+        service_->OnApplicationNotIdle();
+      break;
+
+    default:
+      NOTREACHED();
+  }
 }
