5f65021b5117a2824c5adb7a21b2af541505fbcd
michaelbai@chromium.org <michaelbai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-22 19:20:15 +0000

    Keep the reference of passing request
    Otherwise the request will be gc-ed, and There is no onRequestCancelled invoked.
    
    BUG=375972
    
    Review URL: https://codereview.chromium.org/292183007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272268 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/javatests/src/org/chromium/android_webview/test/MediaAccessPermissionRequestTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/MediaAccessPermissionRequestTest.java
@@ -33,7 +33,7 @@ public class MediaAccessPermissionRequestTest extends AwTestBase {
         }
     }
 
-    private final String mData = "<html> <script> " +
+    private static final String mData = "<html> <script> " +
             "var constraints = {audio: true, video: true};" +
             "var video = document.querySelector('video');" +
             "function successCallback(stream) {" +
@@ -131,15 +131,20 @@ public class MediaAccessPermissionRequestTest extends AwTestBase {
         final OnPermissionRequestHelper helper = new OnPermissionRequestHelper();
         TestAwContentsClient contentsClient =
                 new TestAwContentsClient() {
+                    private AwPermissionRequest mRequest;
                     @Override
                     public void onPermissionRequest(AwPermissionRequest awPermissionRequest) {
+                        assertNull(mRequest);
+                        mRequest = awPermissionRequest;
                         // Don't respond and wait for the request canceled.
                         helper.notifyCalled();
                     }
                     @Override
                     public void onPermissionRequestCanceled(
                             AwPermissionRequest awPermissionRequest) {
-                        helper.notifyCanceled();
+                        assertNotNull(mRequest);
+                        if (mRequest == awPermissionRequest) helper.notifyCanceled();
+                        mRequest = null;
                     }
                 };
         final AwTestContainerView testContainerView =
@@ -153,6 +158,6 @@ public class MediaAccessPermissionRequestTest extends AwTestBase {
         // Load the same page again, the previous request should be canceled.
         loadUrlAsync(awContents, mWebRTCPage, null);
         helper.waitForCallback(callCount);
-        assert (helper.canceled());
+        assertTrue(helper.canceled());
     }
 }
