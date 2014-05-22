47f5cbeae442aee89bbdae619f3cf9fede59e60f
fgorski@chromium.org <fgorski@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-22 23:08:04 +0000

    Revert 272352 "Reland Linux Window Control Alignment"
    > Reland Linux Window Control Alignment
    > 
    > Modifying CustomFrameView to layout window controls based on user configurations. For linux begin observing the configs and changes. For other systems fall back on a default ordering.
    > 
    > Update tests that were originally missed.
    > 
    > Original review: https://codereview.chromium.org/240163006/
    > Revert review: https://codereview.chromium.org/276173002/
    > Revert "Revert of Linux Aura Task Manager Frame Buttons Misaligned (https://codereview.chromium.org/240163006/)"
    > 
    > This reverts commit 9031ef57ba679037fc675bf2f081f3880380de3a.
    > 
    > TEST=CustomFrameViewTest
    > BUG=351917
    > 
    > Review URL: https://codereview.chromium.org/281353009
    
    TBR=jonross@chromium.org
    
    Review URL: https://codereview.chromium.org/294473016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272356 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/system_gesture_event_filter_unittest.cc
+++ ash/wm/system_gesture_event_filter_unittest.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/wm/system_gesture_event_filter.h"
 
-#include <vector>
-
 #include "ash/accelerators/accelerator_controller.h"
 #include "ash/ash_switches.h"
 #include "ash/display/display_manager.h"
@@ -41,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/widget.h"
 #include "ui/views/widget/widget_delegate.h"
 #include "ui/views/window/non_client_view.h"
-#include "ui/views/window/window_button_order_provider.h"
 
 namespace ash {
 namespace test {
@@ -142,20 +139,6 @@ class SystemGestureEventFilterTest
 
   // Overridden from AshTestBase:
   virtual void SetUp() OVERRIDE {
-    // TODO(jonross): TwoFingerDragDelayed() and ThreeFingerGestureStopsDrag()
-    // both use hardcoded touch points, assuming that they target empty header
-    // space. Window control order now reflects configuration files and can
-    // change. The tests should be improved to dynamically decide touch points.
-    // To address this we specify the originally expected window control
-    // positions to be consistent across tests.
-    std::vector<views::FrameButton> leading;
-    std::vector<views::FrameButton> trailing;
-    trailing.push_back(views::FRAME_BUTTON_MINIMIZE);
-    trailing.push_back(views::FRAME_BUTTON_MAXIMIZE);
-    trailing.push_back(views::FRAME_BUTTON_CLOSE);
-    views::WindowButtonOrderProvider::GetInstance()->
-        SetWindowButtonOrder(leading, trailing);
-
     if (!docked_enabled_) {
       CommandLine::ForCurrentProcess()->AppendSwitch(
           ash::switches::kAshDisableDockedWindows);
@@ -455,7 +438,7 @@ TEST_P(SystemGestureEventFilterTest,
 }
 
 TEST_P(SystemGestureEventFilterTest, TwoFingerDragDelayed) {
-  gfx::Rect bounds(0, 0, 200, 100);
+  gfx::Rect bounds(0, 0, 100, 100);
   aura::Window* root_window = Shell::GetPrimaryRootWindow();
   views::Widget* toplevel = views::Widget::CreateWindowWithContextAndBounds(
       new ResizableWidgetDelegate, root_window, bounds);
@@ -490,7 +473,7 @@ TEST_P(SystemGestureEventFilterTest, TwoFingerDragDelayed) {
 }
 
 TEST_P(SystemGestureEventFilterTest, ThreeFingerGestureStopsDrag) {
-  gfx::Rect bounds(0, 0, 200, 100);
+  gfx::Rect bounds(0, 0, 100, 100);
   aura::Window* root_window = Shell::GetPrimaryRootWindow();
   views::Widget* toplevel = views::Widget::CreateWindowWithContextAndBounds(
       new ResizableWidgetDelegate, root_window, bounds);
