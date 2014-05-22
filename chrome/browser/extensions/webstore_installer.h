ce3c24a29548a5592b117b2b779bb638821398b7
limasdf@gmail.com <limasdf@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-22 08:45:15 +0000

    Add a function triggering extension installed to ExtensionRegistryObserver. This can be replace chrome::NOTIFICATION_EXTENSION_INSTALLED with ExtensionRegistryObserver.
    And apply it to WebStoreInstaller and InstallTracker.
    
    R=kalman@chromium.org
    BUG=354459
    TEST=unit_test, manual(install app from webstore)
    
    Review URL: https://codereview.chromium.org/279073003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272143 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/webstore_installer.h
+++ chrome/browser/extensions/webstore_installer.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/scoped_observer.h"
 #include "base/supports_user_data.h"
 #include "base/timer/timer.h"
 #include "base/values.h"
@@ -23,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
 #include "content/public/browser/web_contents_observer.h"
+#include "extensions/browser/extension_registry_observer.h"
 #include "extensions/common/manifest_handlers/shared_module_info.h"
 #include "ui/gfx/image/image_skia.h"
 #include "url/gurl.h"
@@ -41,14 +43,17 @@ namespace extensions {
 
 class CrxInstaller;
 class Extension;
+class ExtensionRegistry;
 class Manifest;
 
 // Downloads and installs extensions from the web store.
 class WebstoreInstaller : public content::NotificationObserver,
+                          public ExtensionRegistryObserver,
                           public content::DownloadItem::Observer,
                           public content::WebContentsObserver,
                           public base::RefCountedThreadSafe<
-  WebstoreInstaller, content::BrowserThread::DeleteOnUIThread> {
+                              WebstoreInstaller,
+                              content::BrowserThread::DeleteOnUIThread> {
  public:
   enum InstallSource {
     // Inline installs trigger slightly different behavior (install source
@@ -189,10 +194,17 @@ class WebstoreInstaller : public content::NotificationObserver,
   // Starts downloading and installing the extension.
   void Start();
 
-  // content::NotificationObserver
+  // content::NotificationObserver.
   virtual void Observe(int type,
                        const content::NotificationSource& source,
                        const content::NotificationDetails& details) OVERRIDE;
+
+  // ExtensionRegistryObserver.
+  virtual void OnExtensionWillBeInstalled(
+      content::BrowserContext* browser_context,
+      const Extension* extension,
+      bool is_update,
+      const std::string& old_name) OVERRIDE;
 
   // Removes the reference to the delegate passed in the constructor. Used when
   // the delegate object must be deleted before this object.
@@ -250,6 +262,8 @@ class WebstoreInstaller : public content::NotificationObserver,
   void RecordInterrupt(const content::DownloadItem* download) const;
 
   content::NotificationRegistrar registrar_;
+  ScopedObserver<ExtensionRegistry, ExtensionRegistryObserver>
+      extension_registry_observer_;
   Profile* profile_;
   Delegate* delegate_;
   std::string id_;
--- extensions/browser/extension_registry_unittest.cc
+++ extensions/browser/extension_registry_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/memory/ref_counted.h"
+#include "base/strings/string_util.h"
 #include "extensions/browser/extension_registry_observer.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/test_util.h"
@@ -38,10 +39,12 @@ class TestObserver : public ExtensionRegistryObserver {
   void Reset() {
     loaded_.clear();
     unloaded_.clear();
+    installed_.clear();
   }
 
   const ExtensionList& loaded() { return loaded_; }
   const ExtensionList& unloaded() { return unloaded_; }
+  const ExtensionList& installed() { return installed_; }
 
  private:
   virtual void OnExtensionLoaded(content::BrowserContext* browser_context,
@@ -56,8 +59,17 @@ class TestObserver : public ExtensionRegistryObserver {
     unloaded_.push_back(extension);
   }
 
+  virtual void OnExtensionWillBeInstalled(
+      content::BrowserContext* browser_context,
+      const Extension* extension,
+      bool is_update,
+      const std::string& old_name) OVERRIDE {
+    installed_.push_back(extension);
+  }
+
   ExtensionList loaded_;
   ExtensionList unloaded_;
+  ExtensionList installed_;
 };
 
 TEST_F(ExtensionRegistryTest, FillAndClearRegistry) {
@@ -216,12 +228,18 @@ TEST_F(ExtensionRegistryTest, Observer) {
 
   EXPECT_TRUE(observer.loaded().empty());
   EXPECT_TRUE(observer.unloaded().empty());
+  EXPECT_TRUE(observer.installed().empty());
 
   scoped_refptr<const Extension> extension =
       test_util::CreateExtensionWithID("id");
 
+  registry.TriggerOnWillBeInstalled(extension, false, base::EmptyString());
+  EXPECT_TRUE(HasSingleExtension(observer.installed(), extension.get()));
+
   registry.AddEnabled(extension);
   registry.TriggerOnLoaded(extension);
+
+  registry.TriggerOnWillBeInstalled(extension, true, "foo");
 
   EXPECT_TRUE(HasSingleExtension(observer.loaded(), extension.get()));
   EXPECT_TRUE(observer.unloaded().empty());
