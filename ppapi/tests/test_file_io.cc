bc29ac6de1da7f648b3d8805efb5fb85ff4eee11
bbudge@chromium.org <bbudge@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-22 06:25:18 +0000

    Make asynchronous writes with PPB_FileIO copy the plugin's buffer. This fixes a problem that was introduced by moving the implementation of Write to the plugin side. The plugin must maintain its buffer until completion.
    BUG=none
    
    Review URL: https://codereview.chromium.org/296803002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272120 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/tests/test_file_io.cc
+++ ppapi/tests/test_file_io.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sys/stat.h>
 #include <sys/types.h>
 
+#include <algorithm>
 #include <vector>
 
 #include "ppapi/c/pp_errors.h"
@@ -876,12 +877,20 @@ std::string TestFileIO::TestParallelWrites() {
   int32_t rv_2 = PP_OK;
   while (size_1 >= 0 && size_2 >= 0 && size_1 + size_2 > 0) {
     if (size_1 > 0) {
-      rv_1 = file_io.Write(write_offset_1, buf_1, size_1,
-                           callback_1.GetCallback());
+      // Copy the buffer so we can erase it below.
+      std::string str_1(buf_1);
+      rv_1 = file_io.Write(
+          write_offset_1, &str_1[0], str_1.size(), callback_1.GetCallback());
+      // Erase the buffer to test that async writes copy it.
+      std::fill(str_1.begin(), str_1.end(), 0);
     }
     if (size_2 > 0) {
-      rv_2 = file_io.Write(write_offset_2, buf_2, size_2,
-                           callback_2.GetCallback());
+      // Copy the buffer so we can erase it below.
+      std::string str_2(buf_2);
+      rv_2 = file_io.Write(
+          write_offset_2, &str_2[0], str_2.size(), callback_2.GetCallback());
+      // Erase the buffer to test that async writes copy it.
+      std::fill(str_2.begin(), str_2.end(), 0);
     }
 
     if (size_1 > 0) {
