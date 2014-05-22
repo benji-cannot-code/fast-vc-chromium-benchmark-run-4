8fa57dbd2bb36d2a50ea6682d806ecbd9ffdfdd5
jyasskin@chromium.org <jyasskin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-22 08:36:53 +0000

    Save running SW instance info, including its SiteInstance, into the ProcessManager.
    We can use this map to drop all the process references on shutdown, in order to
    fix http://crbug.com/368570.
    
    This starts to fix an assumption in the content layer that SiteInstances outlive
    their RPHs so that process permissions can be controlled through the
    SiteInstance instead of by process id. However, there's still work to do before
    that's completely fixed.
    
    BUG=368570
    
    Review URL: https://codereview.chromium.org/292903002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272142 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/service_worker/embedded_worker_test_helper.cc
+++ content/browser/service_worker/embedded_worker_test_helper.cc
@@ -16,10 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-static bool AlwaysTrue(int process_id) {
-  return true;
-}
-
 EmbeddedWorkerTestHelper::EmbeddedWorkerTestHelper(int mock_render_process_id)
     : wrapper_(new ServiceWorkerContextWrapper(NULL)),
       next_thread_id_(0),
@@ -30,8 +26,7 @@ EmbeddedWorkerTestHelper::EmbeddedWorkerTestHelper(int mock_render_process_id)
                          NULL);
   scoped_ptr<ServiceWorkerProcessManager> process_manager(
       new ServiceWorkerProcessManager(wrapper_));
-  process_manager->SetProcessRefcountOpsForTest(base::Bind(AlwaysTrue),
-                                                base::Bind(AlwaysTrue));
+  process_manager->SetProcessIdForTest(mock_render_process_id);
   wrapper_->context()->SetProcessManagerForTest(process_manager.Pass());
   registry()->AddChildProcessSender(mock_render_process_id, this);
 }
