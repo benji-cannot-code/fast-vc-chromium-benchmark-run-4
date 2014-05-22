95e11b427e6be26df664d93d187b7b8202a18535
kbr@chromium.org <kbr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-22 03:43:29 +0000

    Revert 174516 "Oilpan: Use transition types for HTMLCanvasElement."
    Broke Android build. See
    https://code.google.com/p/chromium/issues/detail?id=357163#c65 .
    
    > Oilpan: Use transition types for HTMLCanvasElement.
    > 
    > BUG=357163
    > R=haraken@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/292113004
    
    TBR=zerny@chromium.org
    
    Review URL: https://codereview.chromium.org/292203006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174520 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLCanvasElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLCanvasElement.cpp
@@ -93,10 +93,11 @@ PassRefPtrWillBeRawPtr<HTMLCanvasElement> HTMLCanvasElement::create(Document& do
 HTMLCanvasElement::~HTMLCanvasElement()
 {
     v8::Isolate::GetCurrent()->AdjustAmountOfExternalAllocatedMemory(-m_externallyAllocatedMemory);
-#if !ENABLE(OILPAN)
-    HashSet<RawPtr<CanvasObserver> >::iterator end = m_observers.end();
-    for (HashSet<RawPtr<CanvasObserver> >::iterator it = m_observers.begin(); it != end; ++it)
+    HashSet<CanvasObserver*>::iterator end = m_observers.end();
+    for (HashSet<CanvasObserver*>::iterator it = m_observers.begin(); it != end; ++it)
         (*it)->canvasDestroyed(this);
+
+#if !ENABLE(OILPAN)
     m_context.clear(); // Ensure this goes away before the ImageBuffer.
 #endif
 }
@@ -217,8 +218,8 @@ void HTMLCanvasElement::didDraw(const FloatRect& rect)
 
 void HTMLCanvasElement::notifyObserversCanvasChanged(const FloatRect& rect)
 {
-    WillBeHeapHashSet<RawPtrWillBeWeakMember<CanvasObserver> >::iterator end = m_observers.end();
-    for (WillBeHeapHashSet<RawPtrWillBeWeakMember<CanvasObserver> >::iterator it = m_observers.begin(); it != end; ++it)
+    HashSet<CanvasObserver*>::iterator end = m_observers.end();
+    for (HashSet<CanvasObserver*>::iterator it = m_observers.begin(); it != end; ++it)
         (*it)->canvasChanged(this, rect);
 }
 
@@ -275,8 +276,8 @@ void HTMLCanvasElement::reset()
         }
     }
 
-    WillBeHeapHashSet<RawPtrWillBeWeakMember<CanvasObserver> >::iterator end = m_observers.end();
-    for (WillBeHeapHashSet<RawPtrWillBeWeakMember<CanvasObserver> >::iterator it = m_observers.begin(); it != end; ++it)
+    HashSet<CanvasObserver*>::iterator end = m_observers.end();
+    for (HashSet<CanvasObserver*>::iterator it = m_observers.begin(); it != end; ++it)
         (*it)->canvasResized(this);
 }
 
@@ -530,7 +531,6 @@ void HTMLCanvasElement::notifySurfaceInvalid()
 
 void HTMLCanvasElement::trace(Visitor* visitor)
 {
-    visitor->trace(m_observers);
     visitor->trace(m_context);
     HTMLElement::trace(visitor);
 }
