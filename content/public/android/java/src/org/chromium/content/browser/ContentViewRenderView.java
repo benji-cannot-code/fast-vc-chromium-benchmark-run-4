09d9ff93afd35df9fb1efd080269e6a01a643afa
yusufo@chromium.org <yusufo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-22 20:45:03 +0000

    Set background color for surface view only once in ContentViewRenderView
    Right now we set the surface view background color everytime we create a new
    surface and this causes a white flash everytime the activity is resumed
    
    BUG=365589
    
    Review URL: https://codereview.chromium.org/293983011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272312 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/ContentViewRenderView.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewRenderView.java
@@ -61,8 +61,6 @@ public class ContentViewRenderView extends FrameLayout {
 
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
-                setSurfaceViewBackgroundColor(Color.WHITE);
-
                 assert mNativeContentViewRenderView != 0;
                 nativeSurfaceCreated(mNativeContentViewRenderView);
 
@@ -76,6 +74,7 @@ public class ContentViewRenderView extends FrameLayout {
             }
         };
         mSurfaceView.getHolder().addCallback(mSurfaceCallback);
+        setSurfaceViewBackgroundColor(Color.WHITE);
 
         addView(mSurfaceView,
                 new FrameLayout.LayoutParams(
