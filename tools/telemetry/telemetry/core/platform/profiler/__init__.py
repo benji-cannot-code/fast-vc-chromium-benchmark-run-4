f1d35abe0fc6b79a8155587a394f2b68a5348169
dominikg@chromium.org <dominikg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-22 18:18:30 +0000

    Telemetry: Profilers ignore disappearing processes.
    Make profiler code more robust by catching ProcessGoneException when listing
    all processes. Before the test would crash if processes disappeared between
    getting all PIDs and querying their name.
    
    BUG=375231
    
    Review URL: https://codereview.chromium.org/297753007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272242 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/platform/profiler/__init__.py
+++ tools/telemetry/telemetry/core/platform/profiler/__init__.py
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import collections
 
+from telemetry.core import exceptions
+
 class Profiler(object):
   """A sampling profiler provided by the platform.
 
@@ -50,12 +52,16 @@ class Profiler(object):
     process_name_counts = collections.defaultdict(int)
     process_output_file_map = {}
     for pid in all_pids:
-      cmd_line = self._platform_backend.GetCommandLine(pid)
-      process_name = self._browser_backend.GetProcessName(cmd_line)
-      output_file = '%s.%s%s' % (self._output_path, process_name,
-                                 process_name_counts[process_name])
-      process_name_counts[process_name] += 1
-      process_output_file_map[pid] = output_file
+      try:
+        cmd_line = self._platform_backend.GetCommandLine(pid)
+        process_name = self._browser_backend.GetProcessName(cmd_line)
+        output_file = '%s.%s%s' % (self._output_path, process_name,
+                                   process_name_counts[process_name])
+        process_name_counts[process_name] += 1
+        process_output_file_map[pid] = output_file
+      except exceptions.ProcessGoneException:
+        # Ignore processes that disappeared since calling GetChildPids().
+        continue
     return process_output_file_map
 
   def CollectProfile(self):
