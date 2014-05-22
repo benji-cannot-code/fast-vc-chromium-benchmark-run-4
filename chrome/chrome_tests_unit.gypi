97bc210bc42e95de362403eedbad01d22dcd303c
rdevlin.cronin@chromium.org <rdevlin.cronin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-22 20:33:52 +0000

    Make ActiveScriptController use Active Tab-style permissions Also make ActiveScriptController used for all extensions with pseudo-all-hosts (e.g., *://*.com/*). Add unittests for ActiveScriptController (to complement the browsertests).
    BUG=362353
    
    Review URL: https://codereview.chromium.org/293003008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272307 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/chrome_tests_unit.gypi
+++ chrome/chrome_tests_unit.gypi
@@ -838,6 +838,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/drive/fake_drive_service_unittest.cc',
         'browser/enumerate_modules_model_unittest_win.cc',
         'browser/extensions/active_tab_unittest.cc',
+        'browser/extensions/active_script_controller_unittest.cc',
         'browser/extensions/activity_log/activity_database_unittest.cc',
         'browser/extensions/activity_log/activity_log_enabled_unittest.cc',
         'browser/extensions/activity_log/activity_log_unittest.cc',
--- extensions/renderer/user_script_slave.cc
+++ extensions/renderer/user_script_slave.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/extension_set.h"
 #include "extensions/common/manifest_handlers/csp_info.h"
 #include "extensions/common/permissions/permissions_data.h"
+#include "extensions/renderer/extension_helper.h"
 #include "extensions/renderer/extensions_renderer_client.h"
 #include "extensions/renderer/script_context.h"
 #include "third_party/WebKit/public/web/WebFrame.h"
@@ -184,7 +185,10 @@ void UserScriptSlave::InjectScripts(WebFrame* frame,
     const Extension* extension = GetExtension(injection->extension_id());
     DCHECK(extension);
 
-    if (PermissionsData::RequiresActionForScriptExecution(extension)) {
+    if (PermissionsData::RequiresActionForScriptExecution(
+            extension,
+            ExtensionHelper::Get(top_render_view)->tab_id(),
+            document_url)) {
       // TODO(rdevlin.cronin): Right now, this is just a notification, but soon
       // we should block without user consent.
       top_render_view->Send(
