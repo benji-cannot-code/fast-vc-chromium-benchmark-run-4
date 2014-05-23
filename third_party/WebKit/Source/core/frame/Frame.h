0830c41d36a3f01b3680163d2fc509a63d4386ac
dcheng@chromium.org <dcheng@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-23 21:18:17 +0000

    Remove frameID hack for history.
    This isn't needed anymore since history is maintained by the embedder.
    
    BUG=none
    R=japhet@chromium.org
    
    Review URL: https://codereview.chromium.org/296893009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174712 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/Frame.h
+++ third_party/WebKit/Source/core/frame/Frame.h
@@ -83,8 +83,6 @@ public:
 
     RenderPart* ownerRenderer() const; // Renderer for the element that contains this frame.
 
-    int64_t frameID() const { return m_frameID; }
-
     // FIXME: These should move to RemoteFrame when that is instantiated.
     void setRemotePlatformLayer(blink::WebLayer* remotePlatformLayer) { m_remotePlatformLayer = remotePlatformLayer; }
     blink::WebLayer* remotePlatformLayer() const { return m_remotePlatformLayer; }
@@ -108,9 +106,6 @@ protected:
 private:
 
     HashSet<FrameDestructionObserver*> m_destructionObservers;
-
-    // Temporary hack for history.
-    int64_t m_frameID;
 
     blink::WebLayer* m_remotePlatformLayer;
 };
