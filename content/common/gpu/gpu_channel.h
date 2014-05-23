2c25d9ead1148b5aa0432a3172ccdd05f695b535
piman@chromium.org <piman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-23 20:18:05 +0000

    Fix leak in GpuChannel
    There may be remaining messages in the deferred queue on unclean teardown.
    Delete those.
    
    Also, a little bit of cleanup in the area:
    - GpuChannel doesn't need to be refcounted
    - It's ok to copy WeakPtr across threads, as long as they're only dereferenced
    on the thread that the factory is destroyed. So no need for the extra
    indirection.
    
    BUG=374843
    
    Review URL: https://codereview.chromium.org/299003004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272584 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/gpu_channel.h
+++ content/common/gpu/gpu_channel.h
@@ -51,9 +51,7 @@ class GpuWatchdog;
 
 // Encapsulates an IPC channel between the GPU process and one renderer
 // process. On the renderer side there's a corresponding GpuChannelHost.
-class GpuChannel : public IPC::Listener,
-                   public IPC::Sender,
-                   public base::RefCountedThreadSafe<GpuChannel> {
+class GpuChannel : public IPC::Listener, public IPC::Sender {
  public:
   // Takes ownership of the renderer process handle.
   GpuChannel(GpuChannelManager* gpu_channel_manager,
@@ -62,6 +60,7 @@ class GpuChannel : public IPC::Listener,
              gpu::gles2::MailboxManager* mailbox_manager,
              int client_id,
              bool software);
+  virtual ~GpuChannel();
 
   void Init(base::MessageLoopProxy* io_message_loop,
             base::WaitableEvent* shutdown_event);
@@ -128,9 +127,6 @@ class GpuChannel : public IPC::Listener,
   void LoseAllContexts();
   void MarkAllContextsLost();
 
-  // Destroy channel and all contained contexts.
-  void DestroySoon();
-
   // Called to add a listener for a particular message routing ID.
   // Returns true if succeeded.
   bool AddRoute(int32 route_id, IPC::Listener* listener);
@@ -155,11 +151,7 @@ class GpuChannel : public IPC::Listener,
 
   uint64 GetMemoryUsage();
 
- protected:
-  virtual ~GpuChannel();
-
  private:
-  friend class base::RefCountedThreadSafe<GpuChannel>;
   friend class GpuChannelMessageFilter;
 
   void OnDestroy();
--- content/common/gpu/gpu_channel_manager.h
+++ content/common/gpu/gpu_channel_manager.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "base/containers/hash_tables.h"
+#include "base/containers/scoped_ptr_hash_map.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
@@ -106,7 +106,7 @@ class GpuChannelManager : public IPC::Listener,
     int32 sync_point;
     base::Closure callback;
   };
-  typedef base::hash_map<int, scoped_refptr<GpuChannel> > GpuChannelMap;
+  typedef base::ScopedPtrHashMap<int, GpuChannel> GpuChannelMap;
   typedef std::deque<ImageOperation*> ImageOperationQueue;
 
   // Message handlers.
