097abc1d99f6a77fc367fb1373b1b019bef97842
blundell@chromium.org <blundell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-23 12:10:05 +0000

    Revert of Turn GoogleURLTrackerNavigationHelper(Impl) into a per-tab object. (https://codereview.chromium.org/283413002/)
    Reason for revert:
    Introduced flake on Mac: crbug.com/376550
    
    Original issue's description:
    > Turn GoogleURLTrackerNavigationHelper(Impl) into a per-tab object.
    > 
    > The goal of this CL is to eliminate the dependence of
    > GoogleURLTracker(MapEntry) on NavigationController. To accomplish this goal,
    > GoogleURLTrackerNavigationHelper is turned into a conceptually per-tab
    > interface. GoogleURLTracker::OnNavigationPending() now takes in the
    > GoogleURLTrackerNavigationHelper with which the navigation is associated rather
    > than the associated NavigationController; GoogleURLTracker performs per-tab
    > actions by calling the navigation helper associated with the tab.
    > 
    > A followup CL will turn GoogleURLTrackerNavigationHelper(Impl) into the
    > GoogleURLTrackerDriver interface and ContentURLTrackerDriver implementation.
    > 
    > BUG=373230
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=272094
    
    TBR=pkasting@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=373230
    
    Review URL: https://codereview.chromium.org/294193005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272503 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/google/google_url_tracker.h
+++ chrome/browser/google/google_url_tracker.h
@@ -27,6 +27,10 @@ class GoogleURLTrackerNavigationHelper;
 class PrefService;
 class Profile;
 
+namespace content {
+class NavigationController;
+}
+
 namespace infobars {
 class InfoBar;
 }
@@ -69,6 +73,7 @@ class GoogleURLTracker : public net::URLFetcherDelegate,
   // GoogleURLTrackerFactory::GetForProfile().
   GoogleURLTracker(Profile* profile,
                    scoped_ptr<GoogleURLTrackerClient> client,
+                   scoped_ptr<GoogleURLTrackerNavigationHelper> nav_helper,
                    Mode mode);
 
   virtual ~GoogleURLTracker();
@@ -109,14 +114,14 @@ class GoogleURLTracker : public net::URLFetcherDelegate,
 
   // Called by the client after SearchCommitted() registers listeners, to
   // indicate that we've received the "load now pending" notification.
-  // |nav_helper| is the GoogleURLTrackerNavigationHelper associated with this
-  // navigation; |infobar_service| is the InfoBarService of the associated tab;
-  // and |pending_id| is the unique ID of the newly pending NavigationEntry.
-  // If there is already a visible GoogleURLTracker infobar for this tab, this
+  // |navigation_controller| is the NavigationController for this load;
+  // |infobar_service| is the InfoBarService of the associated tab; and
+  // |pending_id| is the unique ID of the newly pending NavigationEntry.  If
+  // there is already a visible GoogleURLTracker infobar for this tab, this
   // function resets its associated pending entry ID to the new ID.  Otherwise
   // this function creates a map entry for the associated tab.
   virtual void OnNavigationPending(
-      scoped_ptr<GoogleURLTrackerNavigationHelper> nav_helper,
+      content::NavigationController* navigation_controller,
       InfoBarService* infobar_service,
       int pending_id);
 
@@ -127,7 +132,8 @@ class GoogleURLTracker : public net::URLFetcherDelegate,
                                      const GURL& search_url);
 
   // Called by the navigation observer when a tab closes.
-  virtual void OnTabClosed(GoogleURLTrackerNavigationHelper* nav_helper);
+  virtual void OnTabClosed(
+      content::NavigationController* navigation_controller);
 
   scoped_ptr<Subscription> RegisterCallback(
       const OnGoogleURLUpdatedCallback& cb);
@@ -169,14 +175,14 @@ class GoogleURLTracker : public net::URLFetcherDelegate,
   // Google TLD.
   void CloseAllEntries(bool redo_searches);
 
-  // Unregisters any listeners for the navigation helper in |map_entry|.
+  // Unregisters any listeners for the navigation controller in |map_entry|.
   // This sanity-DCHECKs that these are registered (or not) in the specific
   // cases we expect.  (|must_be_listening_for_commit| is used purely for this
   // sanity-checking.)  This also unregisters the global navigation pending
   // listener if there are no remaining listeners for navigation commits, as we
   // no longer need them until another search is committed.
   void UnregisterForEntrySpecificNotifications(
-      GoogleURLTrackerMapEntry* map_entry,
+      const GoogleURLTrackerMapEntry& map_entry,
       bool must_be_listening_for_commit);
 
   void NotifyGoogleURLUpdated(GURL old_url, GURL new_url);
@@ -186,6 +192,8 @@ class GoogleURLTracker : public net::URLFetcherDelegate,
   Profile* profile_;
 
   scoped_ptr<GoogleURLTrackerClient> client_;
+
+  scoped_ptr<GoogleURLTrackerNavigationHelper> nav_helper_;
 
   // Creates an infobar and adds it to the provided InfoBarService.  Returns the
   // infobar on success or NULL on failure.  The caller does not own the
