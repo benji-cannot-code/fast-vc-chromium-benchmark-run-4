a3988b1e792e4942b83de7ce43777b8cf127a900
pdr@chromium.org <pdr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-23 01:14:09 +0000

    Reland "Fix table pre-inflation", blink@r174263
    This patch relands the table cell inflation patch from [1] but with
    a fix for a crash on top sites including amazon.co.jp. The bug was
    that tables can have two layout scopes (one from RenderTable::layout
    and a second from AutoTableLayout::computeIntrinsicLogicalWidths)
    which can cause endLayout to be called twice for the same node. If
    the first endLayout removes all clusters, the second will crash
    when accessing currentCluster(). This patch fixes this by simply
    checking that the cluster stack is non-empty in endLayout.
    
    [1] https://src.chromium.org/viewvc/blink?view=rev&revision=174263
    
    TEST=table-subtree-layout-crash.html
    BUG=371611, 375439
    
    Review URL: https://codereview.chromium.org/294123010
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174624 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/AutoTableLayout.cpp
+++ third_party/WebKit/Source/core/rendering/AutoTableLayout.cpp
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "core/rendering/AutoTableLayout.h"
 
+#include "core/rendering/FastTextAutosizer.h"
 #include "core/rendering/RenderTable.h"
 #include "core/rendering/RenderTableCell.h"
 #include "core/rendering/RenderTableCol.h"
@@ -208,6 +209,8 @@ static bool shouldScaleColumns(RenderTable* table)
 
 void AutoTableLayout::computeIntrinsicLogicalWidths(LayoutUnit& minWidth, LayoutUnit& maxWidth)
 {
+    FastTextAutosizer::TableLayoutScope fastTextAutosizerTableLayoutScope(m_table);
+
     fullRecalc();
 
     int spanMaxLogicalWidth = calcEffectiveLogicalWidth();
--- third_party/WebKit/Source/core/rendering/FastTextAutosizer.h
+++ third_party/WebKit/Source/core/rendering/FastTextAutosizer.h
@@ -71,9 +71,14 @@ public:
     public:
         explicit LayoutScope(RenderBlock*);
         ~LayoutScope();
-    private:
+    protected:
         FastTextAutosizer* m_textAutosizer;
         RenderBlock* m_block;
+    };
+
+    class TableLayoutScope : LayoutScope {
+    public:
+        explicit TableLayoutScope(RenderTable*);
     };
 
     class DeferUpdatePageInfo {
@@ -240,7 +245,7 @@ private:
 
     void beginLayout(RenderBlock*);
     void endLayout(RenderBlock*);
-    void inflateTable(RenderTable*);
+    void inflateAutoTable(RenderTable*);
     void inflate(RenderBlock*);
     bool shouldHandleLayout() const;
     void setAllTextNeedsLayout();
@@ -274,7 +279,6 @@ private:
     // from descendant clusters but because descendant clusters may not exist, this is only an approximation.
     // The TraversalDirection controls whether we return the first or the last text leaf.
     const RenderObject* findTextLeaf(const RenderObject*, size_t&, TextLeafSearch);
-    bool shouldDescendForTableInflation(RenderObject*);
     BlockFlags classifyBlock(const RenderObject*, BlockFlags mask = UINT_MAX);
 #ifdef AUTOSIZING_DOM_DEBUG_INFO
     void writeClusterDebugInfo(Cluster*);
