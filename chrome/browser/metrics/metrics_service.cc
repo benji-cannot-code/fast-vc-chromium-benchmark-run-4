db4df46d10ec85d1f10a35c82909cbfa808536af
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-23 07:16:02 +0000

    [Metrics] Fetch more embedder concepts via MetricsServiceClient.
    BUG=374235
    TEST=none
    R=asvitkine@chromium.org
    
    Review URL: https://codereview.chromium.org/291843008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272425 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metrics/metrics_service.cc
+++ chrome/browser/metrics/metrics_service.cc
@@ -192,9 +192,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/metrics/network_metrics_provider.h"
 #include "chrome/browser/metrics/omnibox_metrics_provider.h"
 #include "chrome/browser/metrics/tracking_synchronizer.h"
-#include "chrome/browser/ui/browser_otr_state.h"
 #include "chrome/common/chrome_constants.h"
-#include "chrome/common/crash_keys.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/variations/variations_util.h"
 #include "components/metrics/metrics_log_base.h"
@@ -549,7 +547,7 @@ void MetricsService::EnableRecording() {
   recording_active_ = true;
 
   state_manager_->ForceClientIdCreation();
-  crash_keys::SetClientID(state_manager_->client_id());
+  client_->SetClientID(state_manager_->client_id());
   if (!log_manager_.current_log())
     OpenNewLog();
 
@@ -1765,7 +1763,7 @@ bool MetricsService::ShouldLogEvents() {
   // We simply don't log events to UMA if there is a single incognito
   // session visible. The problem is that we always notify using the orginal
   // profile in order to simplify notification processing.
-  return !chrome::IsOffTheRecordSessionActive();
+  return !client_->IsOffTheRecordSessionActive();
 }
 
 void MetricsService::RecordBooleanPrefValue(const char* path, bool value) {
