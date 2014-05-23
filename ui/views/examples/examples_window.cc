f08440fe31bbc4a0cfe5d7bd24aae0e2e05e73d6
ckocagil@chromium.org <ckocagil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-23 22:30:36 +0000

    Views Examples: Notify Combobox after updating the model
    BUG=
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/299203003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272617 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/examples/examples_window.cc
+++ ui/views/examples/examples_window.cc
@@ -132,6 +132,7 @@ class ExamplesWindowContents : public WidgetDelegateView,
     instance_ = this;
     combobox_->set_listener(this);
     combobox_model_.SetExamples(examples.Pass());
+    combobox_->ModelChanged();
 
     set_background(Background::CreateStandardPanelBackground());
     GridLayout* layout = new GridLayout(this);
