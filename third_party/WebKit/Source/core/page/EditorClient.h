e111bf84f7ce69ba3b36658d6685abc270cc28c0
yosin@chromium.org <yosin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-23 01:54:03 +0000

    Get rid of unused API function WebViewClient:: didExecuteCommand()
    This patch gets rid of unused API function WebViewClient::didExecuteCommand(),
    which is used for counting part of |document.execCommand|. We provide another
    counter for |document.execCommand| which counts all commands and partial
    counter is no longer used.
    
    Chromium changes will be committed once this patch is rolled into.
    
    BUG=376154
    TEST=n/a; no behavior changes
    
    Review URL: https://codereview.chromium.org/292433011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174631 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/EditorClient.h
+++ third_party/WebKit/Source/core/page/EditorClient.h
@@ -47,7 +47,6 @@ public:
     virtual bool canCopyCut(LocalFrame*, bool defaultValue) const = 0;
     virtual bool canPaste(LocalFrame*, bool defaultValue) const = 0;
 
-    virtual void didExecuteCommand(String) = 0;
     virtual bool handleKeyboardEvent() = 0;
 };
 
