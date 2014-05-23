831ac14cba57310d9cc82ab03a01b99b02ddc0d7
kkinnunen@nvidia.com <kkinnunen@nvidia.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-23 13:05:03 +0000

    Fix an early return and the error status in glLoseContextCHROMIUM impl
    The implementation of glLoseContextCHROMIUM did not return early
    in gl error conditions of invalid enums passed as arguments. Add
    the early returns and tests for the invalid enums.
    
    Upon successful glLoseContextCHROMIUM, the subsequent calls
    such as glGetError would also fail, because the implementation
    would set the decoder error variable as well as return the error
    value. Fix this by just setting the decoder error variable. Add
    a test for this (LoseContextCHROMIUMValidArgs)
    
    BUG=373159
    
    Review URL: https://codereview.chromium.org/298753004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272507 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/build_gles2_cmd_buffer.py
+++ gpu/command_buffer/build_gles2_cmd_buffer.py
@@ -2541,8 +2541,8 @@ _FUNCTION_INFO = {
     'extension_flag': 'ext_discard_framebuffer',
   },
   'LoseContextCHROMIUM': {
-    'type': 'Manual',
-    'impl_func': True,
+    'decoder_func': 'DoLoseContextCHROMIUM',
+    'unit_test': False,
     'extension': True,
     'chromium': True,
   },
--- gpu/command_buffer/service/gles2_cmd_decoder_unittest_3_autogen.h
+++ gpu/command_buffer/service/gles2_cmd_decoder_unittest_3_autogen.h
@@ -60,7 +60,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // TODO(gman): DiscardFramebufferEXT
 // TODO(gman): DiscardFramebufferEXTImmediate
 // TODO(gman): LoseContextCHROMIUM
-
 // TODO(gman): InsertSyncPointCHROMIUM
 
 // TODO(gman): WaitSyncPointCHROMIUM
