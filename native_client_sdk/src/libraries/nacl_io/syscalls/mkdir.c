463ce19054b356925cfb9cd41e579593f9ce38cc
binji@chromium.org <binji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-23 16:58:53 +0000

    [NaCl SDK] Add realpath(3) implementation.
    This really should be in nacl-newlib, but it's easier to land here now and
    remove it later when it lands in newlib and rolls into the SDK.
    
    This implementation does not support symlinks, but neither does nacl_io
    currently.
    
    BUG=none
    R=sbc@chromium.org
    
    Review URL: https://codereview.chromium.org/295933009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272551 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/libraries/nacl_io/syscalls/mkdir.c
+++ native_client_sdk/src/libraries/nacl_io/syscalls/mkdir.c
@@ -6,6 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "nacl_io/kernel_intercept.h"
 #include "nacl_io/kernel_wrap.h"
 
+#if defined(__native_client__)
+// Don't wrap mkdir on host builds. This allows us to test against the real
+// "mkdir" on Linux standalone builds.
+
 int mkdir(const char* pathname, mode_t mode) {
   return ki_mkdir(pathname, mode);
 }
+
+#endif
--- native_client_sdk/src/tests/nacl_io_test/example.dsc
+++ native_client_sdk/src/tests/nacl_io_test/example.dsc
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'mock_node.cc',
         'mock_node.h',
         'mock_util.h',
+        'syscalls_test.cc',
         'path_test.cc',
         'pepper_interface_mock.cc',
         'pepper_interface_mock.h',
