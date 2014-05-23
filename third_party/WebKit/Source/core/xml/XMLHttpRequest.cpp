0f8106368db0b6e8596ee3332143133e384092fb
tyoshino@chromium.org <tyoshino@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-23 05:49:20 +0000

    Remove unnecessary return in XMLHttpRequest::dispatchThrottledProgressEventSnapshot
    BUG=none
    
    Review URL: https://codereview.chromium.org/293113006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174657 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/xml/XMLHttpRequest.cpp
+++ third_party/WebKit/Source/core/xml/XMLHttpRequest.cpp
@@ -1018,7 +1018,7 @@ void XMLHttpRequest::dispatchThrottledProgressEvent(const AtomicString& type, lo
 
 void XMLHttpRequest::dispatchThrottledProgressEventSnapshot(const AtomicString& type)
 {
-    return dispatchThrottledProgressEvent(type, m_receivedLength, m_response.expectedContentLength());
+    dispatchThrottledProgressEvent(type, m_receivedLength, m_response.expectedContentLength());
 }
 
 void XMLHttpRequest::handleNetworkError()
