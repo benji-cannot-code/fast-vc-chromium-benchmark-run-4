10aa69d5bae343bac23c15f5b0cb1c9659446e6f
kkinnunen@nvidia.com <kkinnunen@nvidia.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-23 07:07:53 +0000

    Return early after errors in glVertexAttribDivisor and glCopyTexImage2D implementations
    Command buffer command decoder is missing few early returns.  Add early
    returns after error handling in glVertexAttribDivisorANGLE if no
    extension is present and glCopyTexImage2D if texture is immutable.
    
    BUG=373159
    
    Review URL: https://codereview.chromium.org/285703005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272424 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/gles2_cmd_decoder.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder.cc
@@ -7165,6 +7165,7 @@ error::Error GLES2DecoderImpl::HandleVertexAttribDivisorANGLE(
     LOCAL_SET_GL_ERROR(
         GL_INVALID_OPERATION,
         "glVertexAttribDivisorANGLE", "function not available");
+    return error::kNoError;
   }
   GLuint index = c.index;
   GLuint divisor = c.divisor;
@@ -8390,6 +8391,7 @@ void GLES2DecoderImpl::DoCopyTexImage2D(
   if (texture->IsImmutable()) {
     LOCAL_SET_GL_ERROR(
         GL_INVALID_OPERATION, "glCopyTexImage2D", "texture is immutable");
+    return;
   }
   if (!texture_manager()->ValidForTarget(target, level, width, height, 1) ||
       border != 0) {
