05475a13c9330d708be8105044221a2492220616
piman@chromium.org <piman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-23 23:36:12 +0000

    Don't expose --disable-gpu-compositing, only set internally.
    The flag isn't meant to be set externally, it doesn't work on most
    configs. It's only meant internally to make sure the renderer doesn't
    try to use GPU when the browser is in software.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/300523002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272636 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1039,7 +1039,6 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kDisableFastTextAutosizing,
     switches::kDisableFileSystem,
     switches::kDisableFiltersOverIPC,
-    switches::kDisableGpuCompositing,
     switches::kDisableGpuVsync,
     switches::kDisableLowResTiling,
     switches::kDisableHistogramCustomizer,
