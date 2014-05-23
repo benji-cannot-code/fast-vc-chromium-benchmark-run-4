697025b013c9766674c955258aec622e9f94a072
jackhou@chromium.org <jackhou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-23 07:07:20 +0000

    Remove unneeded functions in web_app.cc.
    The main change here is that web_app::GetWebAppDataDirectory
    is now being called on the UI thread.
    
    BUG=356889
    
    Review URL: https://codereview.chromium.org/286933012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272422 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/web_applications/web_app.cc
+++ chrome/browser/web_applications/web_app.cc
@@ -69,41 +69,10 @@ bool IconPrecedes(const WebApplicationInfo::IconInfo& left,
 }
 #endif
 
-bool CreateShortcutsWithInfoOnFileThread(
-    web_app::ShortcutCreationReason reason,
-    const web_app::ShortcutLocations& locations,
-    const web_app::ShortcutInfo& shortcut_info,
-    const extensions::FileHandlersInfo& file_handlers_info) {
-  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::FILE));
-
-  base::FilePath shortcut_data_dir =
-      web_app::GetWebAppDataDirectory(shortcut_info.profile_path,
-                                      shortcut_info.extension_id,
-                                      shortcut_info.url);
-  return web_app::internals::CreatePlatformShortcuts(
-      shortcut_data_dir, shortcut_info, file_handlers_info, locations, reason);
-}
-
-void DeleteShortcutsOnFileThread(
-    const web_app::ShortcutInfo& shortcut_info) {
-  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::FILE));
-
-  base::FilePath shortcut_data_dir = web_app::GetWebAppDataDirectory(
-      shortcut_info.profile_path, shortcut_info.extension_id, GURL());
-  return web_app::internals::DeletePlatformShortcuts(
-      shortcut_data_dir, shortcut_info);
-}
-
-void UpdateShortcutsOnFileThread(
-    const base::string16& old_app_title,
-    const web_app::ShortcutInfo& shortcut_info,
-    const extensions::FileHandlersInfo& file_handlers_info) {
-  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::FILE));
-
-  base::FilePath shortcut_data_dir = web_app::GetWebAppDataDirectory(
-      shortcut_info.profile_path, shortcut_info.extension_id, GURL());
-  return web_app::internals::UpdatePlatformShortcuts(
-      shortcut_data_dir, old_app_title, shortcut_info, file_handlers_info);
+base::FilePath GetShortcutDataDir(const web_app::ShortcutInfo& shortcut_info) {
+  return web_app::GetWebAppDataDirectory(shortcut_info.profile_path,
+                                         shortcut_info.extension_id,
+                                         shortcut_info.url);
 }
 
 void CreateShortcutsWithInfo(
@@ -117,8 +86,9 @@ void CreateShortcutsWithInfo(
       BrowserThread::FILE,
       FROM_HERE,
       base::Bind(
-          base::IgnoreResult(&CreateShortcutsWithInfoOnFileThread),
-          reason, locations, shortcut_info, file_handlers_info));
+          base::IgnoreResult(&web_app::internals::CreatePlatformShortcuts),
+          GetShortcutDataDir(shortcut_info),
+          shortcut_info, file_handlers_info, locations, reason));
 }
 
 void UpdateAllShortcutsForShortcutInfo(
@@ -128,7 +98,8 @@ void UpdateAllShortcutsForShortcutInfo(
   BrowserThread::PostTask(
       BrowserThread::FILE,
       FROM_HERE,
-      base::Bind(&UpdateShortcutsOnFileThread,
+      base::Bind(&web_app::internals::UpdatePlatformShortcuts,
+                 GetShortcutDataDir(shortcut_info),
                  old_app_title, shortcut_info, file_handlers_info));
 }
 
@@ -248,8 +219,9 @@ bool CreateShortcutsOnFileThread(ShortcutCreationReason reason,
                                  const ShortcutInfo& shortcut_info) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::FILE));
 
-  return CreateShortcutsWithInfoOnFileThread(
-      reason, locations, shortcut_info, extensions::FileHandlersInfo());
+  return CreatePlatformShortcuts(
+      GetShortcutDataDir(shortcut_info),
+      shortcut_info, extensions::FileHandlersInfo(), locations, reason);
 }
 
 }  // namespace internals
@@ -357,11 +329,10 @@ base::FilePath GetWebAppDataDirectory(const base::FilePath& profile_path,
 }
 
 std::string GenerateApplicationNameFromInfo(const ShortcutInfo& shortcut_info) {
-  if (!shortcut_info.extension_id.empty()) {
+  if (!shortcut_info.extension_id.empty())
     return GenerateApplicationNameFromExtensionId(shortcut_info.extension_id);
-  } else {
+  else
     return GenerateApplicationNameFromURL(shortcut_info.url);
-  }
 }
 
 std::string GenerateApplicationNameFromURL(const GURL& url) {
@@ -412,11 +383,13 @@ void CreateShortcuts(ShortcutCreationReason reason,
 void DeleteAllShortcuts(Profile* profile, const extensions::Extension* app) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
 
+  ShortcutInfo shortcut_info =
+      ShortcutInfoForExtensionAndProfile(app, profile);
   BrowserThread::PostTask(
       BrowserThread::FILE,
       FROM_HERE,
-      base::Bind(&DeleteShortcutsOnFileThread,
-                 ShortcutInfoForExtensionAndProfile(app, profile)));
+      base::Bind(&web_app::internals::DeletePlatformShortcuts,
+                 GetShortcutDataDir(shortcut_info), shortcut_info));
 }
 
 void UpdateAllShortcuts(const base::string16& old_app_title,
