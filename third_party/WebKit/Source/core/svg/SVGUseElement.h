0b9ef155e21a62cb84aad2b426c17450844438e9
pdr@chromium.org <pdr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-23 23:19:01 +0000

    Remove unnecessary SVGUseElement::finishParsingChildren
    After [1], this function is no longer needed.
    
    [1] https://src.chromium.org/viewvc/blink?view=rev&revision=174662
    
    Review URL: https://codereview.chromium.org/299293002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174730 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGUseElement.h
+++ third_party/WebKit/Source/core/svg/SVGUseElement.h
@@ -79,7 +79,6 @@ private:
     void scheduleShadowTreeRecreation();
     virtual bool haveLoadedRequiredResources() OVERRIDE { return !isStructurallyExternal() || m_haveFiredLoadEvent; }
 
-    virtual void finishParsingChildren() OVERRIDE;
     virtual bool selfHasRelativeLengths() const OVERRIDE;
 
     // Instance tree handling
