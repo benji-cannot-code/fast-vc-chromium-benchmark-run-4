d06c56719267de1800c971fb0c838a5663272b14
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-23 06:34:38 +0000

    Revert of Oilpan: Make HTMLImageElement::m_form and HTMLFormElement::m_imageElements traceable. (https://codereview.chromium.org/294123014/)
    Reason for revert:
    Broke Oilpan build
    
    
    Original issue's description:
    > Oilpan: Make HTMLImageElement::m_form and HTMLFormElement::m_imageElements traceable.
    > 
    > This fixes flaky crash of fast/forms/form-submission-crash-successful-submit-button.html
    > 
    > BUG=357163
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=174658
    
    TBR=keishi@chromium.org,oilpan-reviews@chromium.org,haraken@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=357163
    
    Review URL: https://codereview.chromium.org/294233002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174659 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLFormElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLFormElement.cpp
@@ -98,7 +98,6 @@ void HTMLFormElement::trace(Visitor* visitor)
     visitor->trace(m_pastNamesMap);
     visitor->trace(m_radioButtonGroupScope);
     visitor->trace(m_associatedElements);
-    visitor->trace(m_imageElements);
 #endif
     HTMLElement::trace(visitor);
 }
@@ -167,10 +166,10 @@ void HTMLFormElement::removedFrom(ContainerNode* insertionPoint)
         }
 
         if (!m_imageElementsAreDirty) {
-            WillBeHeapVector<RawPtrWillBeMember<HTMLImageElement> > images(imageElements());
+            Vector<HTMLImageElement*> images(imageElements());
             notifyFormRemovedFromTree(images, root);
         } else {
-            WillBeHeapVector<RawPtrWillBeMember<HTMLImageElement> > images;
+            Vector<HTMLImageElement*> images;
             collectImageElements(insertionPoint->highestAncestorOrSelf(), images);
             notifyFormRemovedFromTree(images, root);
             collectImageElements(root, images);
@@ -582,7 +581,7 @@ const FormAssociatedElement::List& HTMLFormElement::associatedElements() const
     return m_associatedElements;
 }
 
-void HTMLFormElement::collectImageElements(Node& root, WillBeHeapVector<RawPtrWillBeMember<HTMLImageElement> >& elements)
+void HTMLFormElement::collectImageElements(Node& root, Vector<HTMLImageElement*>& elements)
 {
     elements.clear();
     for (HTMLImageElement* image = Traversal<HTMLImageElement>::firstWithin(root); image; image = Traversal<HTMLImageElement>::next(*image)) {
@@ -591,7 +590,7 @@ void HTMLFormElement::collectImageElements(Node& root, WillBeHeapVector<RawPtrWi
     }
 }
 
-const WillBeHeapVector<RawPtrWillBeMember<HTMLImageElement> >& HTMLFormElement::imageElements()
+const Vector<HTMLImageElement*>& HTMLFormElement::imageElements()
 {
     if (!m_imageElementsAreDirty)
         return m_imageElements;
--- third_party/WebKit/Source/core/html/HTMLImageElement.h
+++ third_party/WebKit/Source/core/html/HTMLImageElement.h
@@ -43,7 +43,6 @@ public:
     static PassRefPtrWillBeRawPtr<HTMLImageElement> createForJSConstructor(Document&, int width, int height);
 
     virtual ~HTMLImageElement();
-    virtual void trace(Visitor*) OVERRIDE;
 
     int width(bool ignorePendingStylesheets = false);
     int height(bool ignorePendingStylesheets = false);
@@ -130,11 +129,8 @@ private:
     void setBestFitURLAndDPRFromImageCandidate(const ImageCandidate&);
 
     HTMLImageLoader m_imageLoader;
-#if ENABLE(OILPAN)
-    Member<HTMLFormElement> m_form;
-#else
+    // m_form should be a strong reference in Oilpan.
     WeakPtr<HTMLFormElement> m_form;
-#endif
     CompositeOperator m_compositeOperator;
     AtomicString m_bestFitImageURL;
     AtomicString m_currentSrc;
