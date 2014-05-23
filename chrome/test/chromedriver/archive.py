b37a2a5a82d023540eab056a4ce8c811686d918e
samuong@chromium.org <samuong@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-23 16:38:50 +0000

    [chromedriver] Fix CHROME_36_REVISION
    BUG=
    NOTRY=True
    
    Review URL: https://codereview.chromium.org/296153010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272546 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/chromedriver/archive.py
+++ chrome/test/chromedriver/archive.py
@@ -13,7 +13,7 @@ import util
 
 CHROME_34_REVISION = '251854'
 CHROME_35_REVISION = '260135'
-CHROME_36_REVISION = '269467'
+CHROME_36_REVISION = '269696'
 
 _SITE = 'http://commondatastorage.googleapis.com'
 
