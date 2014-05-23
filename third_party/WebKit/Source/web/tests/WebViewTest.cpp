171aeef9eb3a94a9dcc9b640e1ffdf9d3506fa9f
danakj@chromium.org <danakj@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-23 01:41:16 +0000

    Stop disabling compositing in SetBaseBackgroundColorBeforeMainFrame.
    This test wants to verify that the WebView's output and background color
    blend with existing content. Instead of asking the WebView to paint, which
    is the old software path, we ask the root RenderLayer to paint (record)
    itself in the way of the compositor asking for it to paint via the
    GraphicsLayer.
    
    R=boliu, enne, ojan
    BUG=363772, 375864
    
    Review URL: https://codereview.chromium.org/299753010
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174630 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/tests/WebViewTest.cpp
+++ third_party/WebKit/Source/web/tests/WebViewTest.cpp
@@ -42,6 +42,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/html/HTMLTextAreaElement.h"
 #include "core/loader/FrameLoadRequest.h"
 #include "core/page/Chrome.h"
+#include "core/rendering/RenderLayer.h"
+#include "core/rendering/RenderView.h"
 #include "platform/KeyboardCodes.h"
 #include "platform/graphics/Color.h"
 #include "public/platform/Platform.h"
@@ -246,11 +248,6 @@ TEST_F(WebViewTest, SetBaseBackgroundColorBeforeMainFrame)
     EXPECT_EQ(kBlue, webView->backgroundColor());
 }
 
-static void disableAccleratedCompositing(WebSettings* webSettings)
-{
-    webSettings->setAcceleratedCompositingEnabled(false);
-}
-
 TEST_F(WebViewTest, SetBaseBackgroundColorAndBlendWithExistingContent)
 {
     const WebColor kAlphaRed = 0x80FF0000;
@@ -258,11 +255,7 @@ TEST_F(WebViewTest, SetBaseBackgroundColorAndBlendWithExistingContent)
     const int kWidth = 100;
     const int kHeight = 100;
 
-    // Make a WebView that is not composited.
-    bool enableJavascript = false;
-    FrameTestHelpers::TestWebFrameClient* frameClient = 0;
-    WebViewClient* viewClient = 0;
-    WebView* webView = m_webViewHelper.initialize(enableJavascript, frameClient, viewClient, &disableAccleratedCompositing);
+    WebView* webView = m_webViewHelper.initialize();
 
     // Set WebView background to green with alpha.
     webView->setBaseBackgroundColor(kAlphaGreen);
@@ -275,7 +268,15 @@ TEST_F(WebViewTest, SetBaseBackgroundColorAndBlendWithExistingContent)
     ASSERT_TRUE(bitmap.allocN32Pixels(kWidth, kHeight));
     SkCanvas canvas(bitmap);
     canvas.clear(kAlphaRed);
-    webView->paint(&canvas, WebRect(0, 0, kWidth, kHeight));
+
+    WebCore::GraphicsContext context(&canvas);
+
+    // Paint the root of the main frame in the way that CompositedLayerMapping would.
+    WebCore::FrameView* view = m_webViewHelper.webViewImpl()->mainFrameImpl()->frameView();
+    WebCore::RenderLayer* rootLayer = view->renderView()->layer();
+    WebCore::IntRect paintRect(0, 0, kWidth, kHeight);
+    WebCore::LayerPaintingInfo paintingInfo(rootLayer, paintRect, WebCore::PaintBehaviorNormal, WebCore::LayoutSize());
+    rootLayer->paintLayerContents(&context, paintingInfo, WebCore::PaintLayerPaintingCompositingAllPhases);
 
     // The result should be a blend of red and green.
     SkColor color = bitmap.getColor(kWidth / 2, kHeight / 2);
