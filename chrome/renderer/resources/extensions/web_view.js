25a22f2b572f57441d6d33d0dbe9773c4bcd9505
lazyboy@chromium.org <lazyboy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-23 23:32:52 +0000

    Fix some webview plugin attach-ment code. We would show error message on attach failure now, we used to always return true from attachWindowAndSetUpEvents(). attachWindowAndSetUpEvents() now return the value of -internal-attach call. -internal-attach did not used to return value, now it returns the status of the attachment call from BrowserPluginBindings.
    Removed 3 unused variables referring to browserPluginNode.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/284183013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272634 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/resources/extensions/web_view.js
+++ chrome/renderer/resources/extensions/web_view.js
@@ -707,9 +707,7 @@ WebViewInternal.prototype.handleDialogEvent =
   };
 
   var self = this;
-  var browserPluginNode = this.browserPluginNode;
   var webviewNode = this.webviewNode;
-
   var requestId = event.requestId;
   var actionTaken = false;
 
@@ -824,7 +822,6 @@ WebViewInternal.prototype.handleNewWindowEvent =
   };
 
   var self = this;
-  var browserPluginNode = this.browserPluginNode;
   var webviewNode = this.webviewNode;
 
   var requestId = event.requestId;
@@ -930,9 +927,7 @@ WebViewInternal.prototype.handlePermissionEvent =
     return;
   }
 
-  var browserPluginNode = this.browserPluginNode;
   var webviewNode = this.webviewNode;
-
   var decisionMade = false;
 
   var validateCall = function() {
@@ -1079,9 +1074,7 @@ WebViewInternal.prototype.attachWindowAndSetUpEvents = function(instanceId) {
     this.setupEvent(eventName, events[eventName]);
   }
 
-  this.browserPluginNode['-internal-attach'](this.instanceId, params);
-
-  return true;
+  return this.browserPluginNode['-internal-attach'](this.instanceId, params);
 };
 
 // Registers browser plugin <object> custom element.
