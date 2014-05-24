ce0124febf7b352fd91c20c79b977f8ed2eda6ae
rlarocque@chromium.org <rlarocque@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-24 03:16:12 +0000

    sync: Fix crash on backend initialize failure
    Fixes a crash in ungraceful sync backend shutdown.  The problem was that
    the sync backend host core might try to unregister itself as a listener
    of some information generated in the ModelTypeRegistry, but the
    ModelTypeRegistry did not exist if backend initialization failed.
    
    The fix used here is to track whether or not we were registered for
    these notifications in the first place, and only unregister if we
    had previously registered successfully.  In the failure shutdown
    case, the flag is unset, so we will not attempt to unregister.
    
    BUG=376863
    
    Review URL: https://codereview.chromium.org/294023017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272697 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/glue/sync_backend_host_core.cc
+++ chrome/browser/sync/glue/sync_backend_host_core.cc
@@ -110,6 +110,7 @@ SyncBackendHostCore::SyncBackendHostCore(
       registrar_(NULL),
       has_sync_setup_completed_(has_sync_setup_completed),
       forward_protocol_events_(false),
+      forward_type_info_(false),
       weak_ptr_factory_(this) {
   DCHECK(backend.get());
 }
@@ -578,8 +579,8 @@ void SyncBackendHostCore::DoShutdown(bool sync_disabled) {
 void SyncBackendHostCore::DoDestroySyncManager() {
   DCHECK_EQ(base::MessageLoop::current(), sync_loop_);
   if (sync_manager_) {
-    save_changes_timer_.reset();
     DisableDirectoryTypeDebugInfoForwarding();
+    save_changes_timer_.reset();
     sync_manager_->RemoveObserver(this);
     sync_manager_->ShutdownOnSyncThread();
     sync_manager_.reset();
@@ -670,6 +671,9 @@ void SyncBackendHostCore::DisableProtocolEventForwarding() {
 
 void SyncBackendHostCore::EnableDirectoryTypeDebugInfoForwarding() {
   DCHECK(sync_manager_);
+
+  forward_type_info_ = true;
+
   if (!sync_manager_->HasDirectoryTypeDebugInfoObserver(this))
     sync_manager_->RegisterDirectoryTypeDebugInfoObserver(this);
   sync_manager_->RequestEmitDebugInfo();
@@ -677,6 +681,12 @@ void SyncBackendHostCore::EnableDirectoryTypeDebugInfoForwarding() {
 
 void SyncBackendHostCore::DisableDirectoryTypeDebugInfoForwarding() {
   DCHECK(sync_manager_);
+
+  if (!forward_type_info_)
+    return;
+
+  forward_type_info_ = false;
+
   if (sync_manager_->HasDirectoryTypeDebugInfoObserver(this))
     sync_manager_->UnregisterDirectoryTypeDebugInfoObserver(this);
 }
