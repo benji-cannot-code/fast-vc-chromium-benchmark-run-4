55c75f7df68ef7e98b722d9e5464d626a71f3fcd
rbyers@chromium.org <rbyers@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-24 00:55:40 +0000

    Cleanup TouchEvent not to derive from MouseRelatedEvent
    MouseReleatedEvent has coordinate fields that make no sense for a
    TouchEvent (where the co-ordinates are in each TouchObject).  We should
    derive from UIEventWithKeyState directly instead of using
    MouseRelatedEvent.
    
    There are no web-exposed changes here.  I've left the confusing
    TouchEvent.initTouchEvent signature the same, ignoring the parameters
    Safari has for screen/window co-ordinates (it looks like these would
    never be used for anything before).
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/293153003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174737 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/events/TouchEvent.h
+++ third_party/WebKit/Source/core/events/TouchEvent.h
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-class TouchEvent FINAL : public MouseRelatedEvent {
+class TouchEvent FINAL : public UIEventWithKeyState {
 public:
     virtual ~TouchEvent();
 
@@ -45,18 +45,16 @@ public:
     static PassRefPtrWillBeRawPtr<TouchEvent> create(TouchList* touches,
         TouchList* targetTouches, TouchList* changedTouches,
         const AtomicString& type, PassRefPtrWillBeRawPtr<AbstractView> view,
-        int screenX, int screenY, int pageX, int pageY,
         bool ctrlKey, bool altKey, bool shiftKey, bool metaKey, bool cancelable)
     {
-        return adoptRefWillBeNoop(new TouchEvent(touches, targetTouches, changedTouches,
-            type, view, screenX, screenY, pageX, pageY,
+        return adoptRefWillBeNoop(new TouchEvent(touches, targetTouches, changedTouches, type, view,
             ctrlKey, altKey, shiftKey, metaKey, cancelable));
     }
 
     void initTouchEvent(TouchList* touches, TouchList* targetTouches,
         TouchList* changedTouches, const AtomicString& type,
-        PassRefPtrWillBeRawPtr<AbstractView>, int screenX, int screenY,
-        int clientX, int clientY,
+        PassRefPtrWillBeRawPtr<AbstractView>,
+        int, int, int, int, // unused useless members of web exposed API
         bool ctrlKey, bool altKey, bool shiftKey, bool metaKey);
 
     TouchList* touches() const { return m_touches.get(); }
@@ -79,8 +77,7 @@ private:
     TouchEvent();
     TouchEvent(TouchList* touches, TouchList* targetTouches,
             TouchList* changedTouches, const AtomicString& type,
-            PassRefPtrWillBeRawPtr<AbstractView>, int screenX, int screenY, int pageX,
-            int pageY,
+            PassRefPtrWillBeRawPtr<AbstractView>,
             bool ctrlKey, bool altKey, bool shiftKey, bool metaKey, bool cancelable);
 
     RefPtrWillBeMember<TouchList> m_touches;
