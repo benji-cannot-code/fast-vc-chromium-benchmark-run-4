4ccc5c5093ac2804248a0721f750930b97720f8d
philipj@opera.com <philipj@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-24 01:02:39 +0000

    Refine the UseCounter for Document.charset
    The DocumentCharset counter is very high, around 4.5%:
    http://www.chromestatus.com/metrics/feature/timeline/popularity/127
    
    Measure the setter separately, since it doesn't make much sense and
    getting rid of it would make charset a simple alias for characterSet.
    
    Add a separate counter for defaultCharset, which is likely used less.
    
    Note that Document.characterSet is the only attribute in the DOM spec:
    http://dom.spec.whatwg.org/#document
    
    Review URL: https://codereview.chromium.org/291823009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174738 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -1165,6 +1165,7 @@ String Document::defaultCharset() const
 
 void Document::setCharset(const String& charset)
 {
+    UseCounter::count(*this, UseCounter::DocumentSetCharset);
     if (DocumentLoader* documentLoader = loader())
         documentLoader->setUserChosenEncoding(charset);
     WTF::TextEncoding encoding(charset);
