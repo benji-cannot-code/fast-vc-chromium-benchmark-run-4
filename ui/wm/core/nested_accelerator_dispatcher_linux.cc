953809374e641ce8ea0f9d0dd9dada8a0bcfd09c
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-24 13:23:04 +0000

    Refactor and move ash independent accelerator handling code in nested loop to ui/wm/core
    I also renamed classes to NestedAcceleratorXxx. I felt this is a bit more clearer than NestedDispatcher, especially in ui/wm/core. Please let me know if you disagree or have better suggestion. I'm happy to rename them.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/298703007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272740 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/wm/core/nested_accelerator_dispatcher_linux.cc
+++ ui/wm/core/nested_accelerator_dispatcher_linux.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ash/accelerators/accelerator_dispatcher.h"
+#include "ui/wm/core/nested_accelerator_dispatcher.h"
 
 #include "base/memory/scoped_ptr.h"
 #include "base/run_loop.h"
@@ -11,12 +11,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/platform/platform_event_dispatcher.h"
 #include "ui/events/platform/platform_event_source.h"
 #include "ui/events/platform/scoped_event_dispatcher.h"
+#include "ui/wm/core/nested_accelerator_delegate.h"
 
 #if defined(USE_X11)
 #include <X11/Xlib.h>
 #endif
 
-namespace ash {
+namespace wm {
 
 namespace {
 
@@ -42,13 +43,14 @@ scoped_ptr<ui::ScopedEventDispatcher> OverrideDispatcher(
 
 }  // namespace
 
-class AcceleratorDispatcherLinux : public AcceleratorDispatcher,
-                                   public ui::PlatformEventDispatcher {
+class NestedAcceleratorDispatcherLinux : public NestedAcceleratorDispatcher,
+                                         public ui::PlatformEventDispatcher {
  public:
-  AcceleratorDispatcherLinux()
-      : restore_dispatcher_(OverrideDispatcher(this)) {}
+  explicit NestedAcceleratorDispatcherLinux(NestedAcceleratorDelegate* delegate)
+      : NestedAcceleratorDispatcher(delegate),
+        restore_dispatcher_(OverrideDispatcher(this)) {}
 
-  virtual ~AcceleratorDispatcherLinux() {}
+  virtual ~NestedAcceleratorDispatcherLinux() {}
 
  private:
   // AcceleratorDispatcher:
@@ -64,7 +66,7 @@ class AcceleratorDispatcherLinux : public AcceleratorDispatcher,
   virtual uint32_t DispatchEvent(const ui::PlatformEvent& event) OVERRIDE {
     if (IsKeyEvent(event)) {
       ui::KeyEvent key_event(event, false);
-      if (MenuClosedForPossibleAccelerator(key_event)) {
+      if (!delegate_->ShouldProcessEventNow(key_event)) {
 #if defined(USE_X11)
         XPutBackEvent(event->xany.display, event);
 #else
@@ -73,23 +75,25 @@ class AcceleratorDispatcherLinux : public AcceleratorDispatcher,
         return ui::POST_DISPATCH_NONE;
       }
 
-      if (AcceleratorProcessedForKeyEvent(key_event))
+      if (delegate_->ProcessEvent(key_event))
         return ui::POST_DISPATCH_NONE;
     }
-
     ui::PlatformEventDispatcher* prev = *restore_dispatcher_;
+
     return prev ? prev->DispatchEvent(event)
                 : ui::POST_DISPATCH_PERFORM_DEFAULT;
   }
 
   scoped_ptr<ui::ScopedEventDispatcher> restore_dispatcher_;
 
-  DISALLOW_COPY_AND_ASSIGN(AcceleratorDispatcherLinux);
+  DISALLOW_COPY_AND_ASSIGN(NestedAcceleratorDispatcherLinux);
 };
 
-scoped_ptr<AcceleratorDispatcher> AcceleratorDispatcher::Create(
+scoped_ptr<NestedAcceleratorDispatcher> NestedAcceleratorDispatcher::Create(
+    NestedAcceleratorDelegate* delegate,
     base::MessagePumpDispatcher* nested_dispatcher) {
-  return scoped_ptr<AcceleratorDispatcher>(new AcceleratorDispatcherLinux());
+  return scoped_ptr<NestedAcceleratorDispatcher>(
+      new NestedAcceleratorDispatcherLinux(delegate));
 }
 
-}  // namespace ash
+}  // namespace wm
