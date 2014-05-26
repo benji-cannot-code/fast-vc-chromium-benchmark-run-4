56e83435089081d6bfb34577c0bef7dd1f55aee7
earthdok@chromium.org <earthdok@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-26 23:52:32 +0000

    Remove conflicting function declaration.
    We used to declare sanitizer interface functions explicitly because we didn't
    have access to sanitizer interface headers. That has not been the case for a
    while. Also, a recent clang roll changed the signature of
    __sanitizer_sandbox_on_notify() causing conflicts with our own declaration.
    
    BUG=none
    TBR=jln@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/294093012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272885 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/sandbox_linux/sandbox_linux.cc
+++ content/common/sandbox_linux/sandbox_linux.cc
@@ -35,6 +35,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sandbox/linux/services/yama.h"
 #include "sandbox/linux/suid/client/setuid_sandbox_client.h"
 
+#if (defined(ADDRESS_SANITIZER) || defined(MEMORY_SANITIZER) || \
+     defined(LEAK_SANITIZER))
+#include <sanitizer/common_interface_defs.h>
+#endif
+
 using sandbox::Yama;
 
 namespace {
@@ -125,12 +130,6 @@ LinuxSandbox* LinuxSandbox::GetInstance() {
   return instance;
 }
 
-#if (defined(ADDRESS_SANITIZER) || defined(MEMORY_SANITIZER) || \
-     defined(LEAK_SANITIZER))  && defined(OS_LINUX)
-// Sanitizer API call to notify the tool the sandbox is going to be turned on.
-extern "C" void __sanitizer_sandbox_on_notify(void *reserved);
-#endif
-
 void LinuxSandbox::PreinitializeSandbox() {
   CHECK(!pre_initialized_);
   seccomp_bpf_supported_ = false;
@@ -138,7 +137,7 @@ void LinuxSandbox::PreinitializeSandbox() {
      defined(LEAK_SANITIZER))  && defined(OS_LINUX)
   // Sanitizers need to open some resources before the sandbox is enabled.
   // This should not fork, not launch threads, not open a directory.
-  __sanitizer_sandbox_on_notify(/*reserved*/ NULL);
+  __sanitizer_sandbox_on_notify(/*args*/ NULL);
 #endif
 
 #if !defined(NDEBUG)
