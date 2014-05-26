e846cadc5a983215616405192a6f3905f58f46c2
sanjoy.pal@samsung.com <sanjoy.pal@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-26 13:22:19 +0000

    Fix layoutOverflowRect in case of explicit 'overflow: scroll', but no overflow content.
    BUG=371359
    
    Review URL: https://codereview.chromium.org/285293007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174808 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderBox.cpp
+++ third_party/WebKit/Source/core/rendering/RenderBox.cpp
@@ -4284,12 +4284,8 @@ void RenderBox::addContentsVisualOverflow(const LayoutRect& rect)
         return;
     }
 
-    if (!m_overflow) {
-        LayoutRect clientBox = clientBoxRect();
-        if (style()->shouldPlaceBlockDirectionScrollbarOnLogicalLeft())
-            clientBox.move(-verticalScrollbarWidth(), 0);
-        m_overflow = adoptPtr(new RenderOverflow(clientBox, borderBoxRect()));
-    }
+    if (!m_overflow)
+        m_overflow = adoptPtr(new RenderOverflow(noOverflowRect(), borderBoxRect()));
     m_overflow->addContentsVisualOverflow(rect);
 }
 
