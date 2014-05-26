cb8d923d87579eb22dee10379bb5c405767d3650
fs@opera.com <fs@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-26 09:04:15 +0000

    Rudimentary keyboard navigation support for <video>/<audio>
    Remove the override of hasCustomFocusLogic() for HTMLMediaElement. This
    allows focus navigation (<tab>ing) to reach the controls.
    
    BUG=135661
    
    Review URL: https://codereview.chromium.org/292173006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174795 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLMediaElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLMediaElement.cpp
@@ -404,11 +404,6 @@ void HTMLMediaElement::didMoveToNewDocument(Document& oldDocument)
     HTMLElement::didMoveToNewDocument(oldDocument);
 }
 
-bool HTMLMediaElement::hasCustomFocusLogic() const
-{
-    return true;
-}
-
 bool HTMLMediaElement::supportsFocus() const
 {
     if (ownerDocument()->isMediaDocument())
