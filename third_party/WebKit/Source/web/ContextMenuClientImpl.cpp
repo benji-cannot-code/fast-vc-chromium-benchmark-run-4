c1937e71d6721a4a2d7ef556539f631e2148d03f
bokan@chromium.org <bokan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-26 17:26:27 +0000

    Made context menu aware of pinch viewport offset
    Fixed issue where context menu wouldn't take into account the pinch
    viewport's offset so the menu would appear in the wrong place if the user
    was pinched in.
    
    BUG=371441
    
    Review URL: https://codereview.chromium.org/301433003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174820 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/ContextMenuClientImpl.cpp
+++ third_party/WebKit/Source/web/ContextMenuClientImpl.cpp
@@ -40,7 +40,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/DocumentMarkerController.h"
 #include "core/editing/Editor.h"
 #include "core/editing/SpellChecker.h"
+#include "core/frame/FrameHost.h"
 #include "core/frame/FrameView.h"
+#include "core/frame/PinchViewport.h"
 #include "core/frame/Settings.h"
 #include "core/html/HTMLFormElement.h"
 #include "core/html/HTMLInputElement.h"
@@ -191,6 +193,11 @@ void ContextMenuClientImpl::showContextMenu(const WebCore::ContextMenu* defaultM
 
     WebContextMenuData data;
     IntPoint mousePoint = selectedFrame->view()->contentsToWindow(r.roundedPointInInnerNodeFrame());
+
+    // FIXME(bokan): crbug.com/371902 - We shouldn't be making these scale
+    // related coordinate transformatios in an ad hoc way.
+    PinchViewport& pinchViewport = selectedFrame->host()->pinchViewport();
+    mousePoint -= flooredIntSize(pinchViewport.visibleRect().location());
     mousePoint.scale(m_webView->pageScaleFactor(), m_webView->pageScaleFactor());
     data.mousePosition = mousePoint;
 
