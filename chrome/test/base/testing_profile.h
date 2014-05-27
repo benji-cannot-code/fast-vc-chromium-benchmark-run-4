4287d5a7bea71b91b51f0dc8b978ddf24587ae3a
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-27 22:17:32 +0000

    Cleanup: Remove unused bits of TestingProfile.
    Review URL: https://codereview.chromium.org/297193002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273051 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/base/testing_profile.h
+++ chrome/test/base/testing_profile.h
@@ -19,10 +19,6 @@ namespace content {
 class MockResourceContext;
 }
 
-namespace extensions {
-class ExtensionPrefs;
-}
-
 namespace history {
 class TopSites;
 }
@@ -46,8 +42,6 @@ class BrowserContextDependencyManager;
 class ExtensionSpecialStoragePolicy;
 class HostContentSettingsMap;
 class PrefServiceSyncable;
-class ProfileSyncService;
-class TemplateURLService;
 class TestingPrefServiceSyncable;
 
 class TestingProfile : public Profile {
@@ -310,17 +304,9 @@ class TestingProfile : public Profile {
       content::ProtocolHandlerScopedVector protocol_interceptors) OVERRIDE;
   virtual net::SSLConfigService* GetSSLConfigService() OVERRIDE;
   virtual HostContentSettingsMap* GetHostContentSettingsMap() OVERRIDE;
-  virtual std::wstring GetName();
-  virtual void SetName(const std::wstring& name) {}
-  virtual std::wstring GetID();
-  virtual void SetID(const std::wstring& id);
   void set_last_session_exited_cleanly(bool value) {
     last_session_exited_cleanly_ = value;
   }
-  virtual void MergeResourceString(int message_id,
-                                   std::wstring* output_string) {}
-  virtual void MergeResourceInteger(int message_id, int* output_value) {}
-  virtual void MergeResourceBoolean(int message_id, bool* output_value) {}
   virtual bool IsSameProfile(Profile *p) OVERRIDE;
   virtual base::Time GetStartTime() const OVERRIDE;
   virtual base::FilePath last_selected_directory() OVERRIDE;
@@ -387,8 +373,6 @@ class TestingProfile : public Profile {
   // Internally, this is a TestURLRequestContextGetter that creates a dummy
   // request context. Currently, only the CookieMonster is hooked up.
   scoped_refptr<net::URLRequestContextGetter> extensions_request_context_;
-
-  std::wstring id_;
 
   bool incognito_;
   bool force_incognito_;
