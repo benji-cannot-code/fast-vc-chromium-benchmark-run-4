8f0d7833e0cba4b73cb0aa56e43f1c5cd7a5545f
mstensho@opera.com <mstensho@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-27 16:15:42 +0000

    [New Multicolumn] fast/multicol/multicol-with-child-renderLayer-for-input.html puts the textfield in the wrong place
    FrameView.cpp changes merged from WebKit. Original patch by David Hyatt <hyatt@apple.com>.
    Reviewed in WebKit by Brady Eidson <beidson@apple.com>.
    Make sure FrameView is patched to pass in the update pagination flag when subtree layout
    is done on textfields. It had code for the old multicolumn system but did not have a check
    for the new multicolumn system.
    
    Additionally, in Blink we need to avoid switching to a different renderer "container" in RenderBoxModelObject::mapAbsoluteToLocalPoint(), or an assertion will fail at the beginning of RenderBox::offsetFromContainer(). Just include the correct column offset instead and proceed as normally.
    
    BUG=369992
    
    Review URL: https://codereview.chromium.org/264103002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174871 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/FrameView.cpp
+++ third_party/WebKit/Source/core/frame/FrameView.cpp
@@ -99,7 +99,7 @@ static RenderLayer::UpdateLayerPositionsFlags updateLayerPositionFlags(RenderLay
 {
     RenderLayer::UpdateLayerPositionsFlags flags = didFullRepaint ? RenderLayer::NeedsFullRepaintInBacking : RenderLayer::CheckForRepaint;
 
-    if (isRelayoutingSubtree && layer->isPaginated())
+    if (isRelayoutingSubtree && (layer->isPaginated() || layer->enclosingPaginationLayer()))
         flags |= RenderLayer::UpdatePagination;
 
     return flags;
