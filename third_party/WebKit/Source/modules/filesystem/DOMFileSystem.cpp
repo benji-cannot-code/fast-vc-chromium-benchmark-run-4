3f3f2e084b9c7cf9f88f58b07a48f562772ab536
alph@chromium.org <alph@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-27 15:53:01 +0000

    Revert of Delay filesystem API async callbacks while JS debugger is on pause. (https://codereview.chromium.org/303523002/)
    Reason for revert:
    Broke Oilpan compile http://build.chromium.org/p/chromium.webkit/builders/WebKit%20Mac%20Oilpan/builds/5165
    
    Original issue's description:
    > Delay filesystem API async callbacks while JS debugger is on pause.
    > 
    > While the Javascript VM is paused all callbacks into the javascipt from C++ bindings are ignored.
    > For async filesystem API callbacks we can postpone the callbacks while debugger is on pause, rather than just ignore them.
    > 
    > BUG=370100
    > R=kinuko@chromium.org, nhiroki@chromium.org
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=174865
    
    TBR=kinuko@chromium.org,nhiroki@chromium.org,yurys@chromium.org,aandrey@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=370100
    
    Review URL: https://codereview.chromium.org/301543006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174869 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/filesystem/DOMFileSystem.cpp
+++ third_party/WebKit/Source/modules/filesystem/DOMFileSystem.cpp
@@ -150,7 +150,7 @@ void DOMFileSystem::createWriter(const FileEntry* fileEntry, PassOwnPtr<FileWrit
 
     RefPtrWillBeRawPtr<FileWriter> fileWriter = FileWriter::create(executionContext());
     OwnPtr<FileWriterBaseCallback> conversionCallback = ConvertToFileWriterCallback::create(successCallback);
-    OwnPtr<AsyncFileSystemCallbacks> callbacks = FileWriterBaseCallbacks::create(fileWriter, conversionCallback.release(), errorCallback, m_context);
+    OwnPtr<AsyncFileSystemCallbacks> callbacks = FileWriterBaseCallbacks::create(fileWriter, conversionCallback.release(), errorCallback);
     fileSystem()->createFileWriter(createFileSystemURL(fileEntry), fileWriter.get(), callbacks.release());
 }
 
@@ -158,9 +158,9 @@ namespace {
 
 class SnapshotFileCallback : public FileSystemCallbacksBase {
 public:
-    static PassOwnPtr<AsyncFileSystemCallbacks> create(PassRefPtrWillBeRawPtr<DOMFileSystem> filesystem, const String& name, const KURL& url, PassOwnPtr<FileCallback> successCallback, PassOwnPtr<ErrorCallback> errorCallback, ExecutionContext* context)
+    static PassOwnPtr<AsyncFileSystemCallbacks> create(PassRefPtrWillBeRawPtr<DOMFileSystem> filesystem, const String& name, const KURL& url, PassOwnPtr<FileCallback> successCallback, PassOwnPtr<ErrorCallback> errorCallback)
     {
-        return adoptPtr(static_cast<AsyncFileSystemCallbacks*>(new SnapshotFileCallback(filesystem, name, url, successCallback, errorCallback, context)));
+        return adoptPtr(static_cast<AsyncFileSystemCallbacks*>(new SnapshotFileCallback(filesystem, name, url, successCallback, errorCallback)));
     }
 
     virtual void didCreateSnapshotFile(const FileMetadata& metadata, PassRefPtr<BlobDataHandle> snapshot)
@@ -191,8 +191,8 @@ public:
     }
 
 private:
-    SnapshotFileCallback(PassRefPtrWillBeRawPtr<DOMFileSystem> filesystem, const String& name, const KURL& url, PassOwnPtr<FileCallback> successCallback, PassOwnPtr<ErrorCallback> errorCallback, ExecutionContext* context)
-        : FileSystemCallbacksBase(errorCallback, filesystem.get(), context)
+    SnapshotFileCallback(PassRefPtrWillBeRawPtr<DOMFileSystem> filesystem, const String& name, const KURL& url, PassOwnPtr<FileCallback> successCallback, PassOwnPtr<ErrorCallback> errorCallback)
+        : FileSystemCallbacksBase(errorCallback, filesystem.get())
         , m_name(name)
         , m_url(url)
         , m_successCallback(successCallback)
@@ -209,7 +209,7 @@ private:
 void DOMFileSystem::createFile(const FileEntry* fileEntry, PassOwnPtr<FileCallback> successCallback, PassOwnPtr<ErrorCallback> errorCallback)
 {
     KURL fileSystemURL = createFileSystemURL(fileEntry);
-    fileSystem()->createSnapshotFileAndReadMetadata(fileSystemURL, SnapshotFileCallback::create(this, fileEntry->name(), fileSystemURL, successCallback, errorCallback, m_context));
+    fileSystem()->createSnapshotFileAndReadMetadata(fileSystemURL, SnapshotFileCallback::create(this, fileEntry->name(), fileSystemURL, successCallback, errorCallback));
 }
 
 } // namespace WebCore
