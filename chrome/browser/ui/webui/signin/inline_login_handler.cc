e84f5da4cd9cc8e005d6f20a1ab1382ead5c399a
rogerta@chromium.org <rogerta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-27 22:27:49 +0000

    Fix some problems with new reauth: - use embedded view instead of servicelogin - don't show settings page when reauth'ing primary account - use new UMA action for logging reauth
    BUG=311235
    
    Review URL: https://codereview.chromium.org/300523003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273054 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/signin/inline_login_handler.cc
+++ chrome/browser/ui/webui/signin/inline_login_handler.cc
@@ -48,7 +48,8 @@ void InlineLoginHandler::HandleInitializeMessage(const base::ListValue* args) {
   const GURL& current_url = web_ui()->GetWebContents()->GetURL();
   signin::Source source = signin::GetSourceForPromoURL(current_url);
   if (source == signin::SOURCE_AVATAR_BUBBLE_ADD_ACCOUNT ||
-      source == signin::SOURCE_AVATAR_BUBBLE_SIGN_IN) {
+      source == signin::SOURCE_AVATAR_BUBBLE_SIGN_IN ||
+      source == signin::SOURCE_REAUTH) {
     // Drop the leading slash in the path.
     params.SetString(
         "gaiaPath",
@@ -60,7 +61,8 @@ void InlineLoginHandler::HandleInitializeMessage(const base::ListValue* args) {
       signin::GetLandingURL("source", static_cast<int>(source)).spec());
 
   std::string default_email;
-  if (source != signin::SOURCE_AVATAR_BUBBLE_ADD_ACCOUNT) {
+  if (source != signin::SOURCE_AVATAR_BUBBLE_ADD_ACCOUNT &&
+      source != signin::SOURCE_REAUTH) {
     default_email = Profile::FromWebUI(web_ui())->GetPrefs()->GetString(
         prefs::kGoogleServicesLastUsername);
   } else {
