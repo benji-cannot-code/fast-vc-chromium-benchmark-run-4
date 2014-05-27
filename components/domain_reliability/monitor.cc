263f23fc72a9ba132c19c793336cb9e9c25a0495
rdsmith@chromium.org <rdsmith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-27 12:55:27 +0000

    Create a TrivialURLRequestContextGetter to shim an existing URLRequestContext.
    BUG=None
    R=willchan@chromium.org
    
    Review URL: https://codereview.chromium.org/292063002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272953 0039d316-1c4b-4281-b951-d872f2087c98

--- components/domain_reliability/monitor.cc
+++ components/domain_reliability/monitor.cc
@@ -24,33 +24,6 @@ bool OnIOThread() {
   return content::BrowserThread::CurrentlyOn(content::BrowserThread::IO);
 }
 
-// Shamelessly stolen from net/tools/get_server_time/get_server_time.cc.
-// TODO(ttuttle): Merge them, if possible.
-class TrivialURLRequestContextGetter : public net::URLRequestContextGetter {
- public:
-  TrivialURLRequestContextGetter(
-      net::URLRequestContext* context,
-      const scoped_refptr<base::SingleThreadTaskRunner>& main_task_runner)
-      : context_(context),
-        main_task_runner_(main_task_runner) {}
-
-  // net::URLRequestContextGetter implementation:
-  virtual net::URLRequestContext* GetURLRequestContext() OVERRIDE {
-    return context_;
-  }
-
-  virtual scoped_refptr<base::SingleThreadTaskRunner>
-  GetNetworkTaskRunner() const OVERRIDE {
-    return main_task_runner_;
-  }
-
- private:
-  virtual ~TrivialURLRequestContextGetter() {}
-
-  net::URLRequestContext* context_;
-  const scoped_refptr<base::SingleThreadTaskRunner> main_task_runner_;
-};
-
 }  // namespace
 
 namespace domain_reliability {
@@ -60,7 +33,7 @@ DomainReliabilityMonitor::DomainReliabilityMonitor(
     const std::string& upload_reporter_string)
     : time_(new ActualTime()),
       url_request_context_getter_(scoped_refptr<net::URLRequestContextGetter>(
-          new TrivialURLRequestContextGetter(
+          new net::TrivialURLRequestContextGetter(
               url_request_context,
               content::BrowserThread::GetMessageLoopProxyForThread(
                   content::BrowserThread::IO)))),
@@ -81,7 +54,7 @@ DomainReliabilityMonitor::DomainReliabilityMonitor(
     scoped_ptr<MockableTime> time)
     : time_(time.Pass()),
       url_request_context_getter_(scoped_refptr<net::URLRequestContextGetter>(
-          new TrivialURLRequestContextGetter(
+          new net::TrivialURLRequestContextGetter(
               url_request_context,
               content::BrowserThread::GetMessageLoopProxyForThread(
                   content::BrowserThread::IO)))),
