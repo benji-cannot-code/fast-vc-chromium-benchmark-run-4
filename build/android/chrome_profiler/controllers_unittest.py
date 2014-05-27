e6cf9976ebcbbdc72cd14e457c0a15bf5f1856e9
skyostil@chromium.org <skyostil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-27 11:26:27 +0000

    adb_profile_chrome: Refactor into multiple modules and add tests
    The adb_profile_chrome tool has grown quite a bit since its inception,
    so now seems to be a good time to split it into smaller modules and add
    some rudimentary tests.
    
    BUG=375754
    TEST=build/android/chrome_profiler/run_tests
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/290013006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272946 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ build/android/chrome_profiler/controllers_unittest.py
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import unittest
+
+from chrome_profiler import profiler
+
+from pylib import android_commands
+from pylib.device import device_utils
+
+
+class BaseControllerTest(unittest.TestCase):
+  def setUp(self):
+    devices = android_commands.GetAttachedDevices()
+    self.browser = 'stable'
+    self.package_info = profiler.GetSupportedBrowsers()[self.browser]
+    self.device = device_utils.DeviceUtils(devices[0])
+
+    adb = android_commands.AndroidCommands(devices[0])
+    adb.StartActivity(self.package_info.package,
+                      self.package_info.activity,
+                      wait_for_completion=True)
---
+++ build/android/chrome_profiler/systrace_controller_unittest.py
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+
+from chrome_profiler import controllers_unittest
+from chrome_profiler import systrace_controller
+
+
+class SystraceControllerTest(controllers_unittest.BaseControllerTest):
+  def testGetCategories(self):
+    categories = \
+        systrace_controller.SystraceController.GetCategories(self.device)
+    self.assertTrue(categories)
+    assert 'gfx' in ' '.join(categories)
+
+  def testTracing(self):
+    categories = ['gfx', 'input', 'view']
+    ring_buffer = False
+    controller = systrace_controller.SystraceController(self.device,
+                                                        categories,
+                                                        ring_buffer)
+
+    interval = 1
+    try:
+      controller.StartTracing(interval)
+    finally:
+      controller.StopTracing()
+
+    result = controller.PullTrace()
+    try:
+      with open(result) as f:
+        self.assertTrue('CPU#' in f.read())
+    finally:
+      os.remove(result)
