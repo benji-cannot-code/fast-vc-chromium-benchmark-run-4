e16cb2f36df8c6e8d13fc0155313557492706161
erik.corry@gmail.com <erik.corry@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-27 11:35:59 +0000

    Simplify handling of SVGElementRareData
    The previous implementation maintained a global map from SVGElement to
    SVGElementRareData in order to avoid a field from SVGElement to
    SVGElementRareData. This  is a rather complicated implementation that is much
    less obviously correct than a simple OwnPtr. The SVGElement is already 21
    pointers large (on 64 bit, perhaps more on 32 bit), so adding a 22nd pointer
    seems like a small price to pay for a nice simplification in some code that has
    had lifetime issues. Post-oilpan this is still a nice simplification. SVG is used
    by less than 0.25% of pages, so I think that trading simplicity for a small
    space penalty is justified. If at some future point, SVG becomes more popular
    then we could get the space back by combining the 1-bit m_isContextElement
    field with the m_SVGRareData field in one pointer-sized field. At the moment
    that doesn't seem justified by the uses.
    
    Kouhei, I'm afraid this will textually conflict with the stuff you are doing. I
    won't land until after you.
    
    R=kouhei@chromium.org, zerny@chromium.org, wibling@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/305453004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174857 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGElement.cpp
+++ third_party/WebKit/Source/core/svg/SVGElement.cpp
@@ -77,7 +77,7 @@ SVGElement::SVGElement(const QualifiedName& tagName, Document& document, Constru
 #endif
     // |m_isContextElement| must be initialized before |m_className|, as SVGAnimatedString tear-off c-tor currently set this to true.
     , m_isContextElement(false)
-    , m_hasSVGRareData(false)
+    , m_SVGRareData(nullptr)
     , m_className(SVGAnimatedString::create(this, HTMLNames::classAttr, SVGString::create()))
 {
     ScriptWrappable::init(this);
@@ -91,29 +91,14 @@ SVGElement::~SVGElement()
 
     // The below teardown is all handled by weak pointer processing in oilpan.
 #if !ENABLE(OILPAN)
-    if (!hasSVGRareData())
-        ASSERT(!SVGElementRareData::rareDataMap().contains(this));
-    else {
-        SVGElementRareData::SVGElementRareDataMap& rareDataMap = SVGElementRareData::rareDataMap();
-        SVGElementRareData::SVGElementRareDataMap::iterator it = rareDataMap.find(this);
-        ASSERT_WITH_SECURITY_IMPLICATION(it != rareDataMap.end());
-
-        SVGElementRareData* rareData = it->value;
-        if (SVGCursorElement* cursorElement = rareData->cursorElement())
+    if (hasSVGRareData()) {
+        if (SVGCursorElement* cursorElement = svgRareData()->cursorElement())
             cursorElement->removeReferencedElement(this);
-        if (CSSCursorImageValue* cursorImageValue = rareData->cursorImageValue())
+        if (CSSCursorImageValue* cursorImageValue = svgRareData()->cursorImageValue())
             cursorImageValue->removeReferencedElement(this);
-
-        delete rareData;
-
-        // The rare data cleanup may have caused other SVG nodes to be deleted,
-        // modifying the rare data map. Do not rely on the existing iterator.
-        ASSERT(rareDataMap.contains(this));
-        rareDataMap.remove(this);
-        // Clear HasSVGRareData flag now so that we are in a consistent state when
-        // calling rebuildAllElementReferencesForTarget() and
-        // removeAllElementReferencesForTarget() below.
-        clearHasSVGRareData();
+        // Clear the rare data now so that we are in a consistent state when
+        // calling rebuildAllElementReferencesForTarget() below.
+        m_SVGRareData.clear();
     }
 
     // With Oilpan, either removedFrom has been called or the document is dead
@@ -183,22 +168,13 @@ bool SVGElement::rendererIsNeeded(const RenderStyle& style)
     return false;
 }
 
-SVGElementRareData* SVGElement::svgRareData() const
-{
-    ASSERT(hasSVGRareData());
-    return SVGElementRareData::rareDataFromMap(this);
-}
-
 SVGElementRareData* SVGElement::ensureSVGRareData()
 {
     if (hasSVGRareData())
         return svgRareData();
 
-    ASSERT(!SVGElementRareData::rareDataMap().contains(this));
-    SVGElementRareData* data = new SVGElementRareData(this);
-    SVGElementRareData::rareDataMap().set(this, data);
-    setHasSVGRareData();
-    return data;
+    m_SVGRareData = adoptPtrWillBeNoop(new SVGElementRareData(this));
+    return m_SVGRareData.get();
 }
 
 bool SVGElement::isOutermostSVGSVGElement() const
@@ -549,7 +525,6 @@ void SVGElement::removeInstanceMapping(SVGElement* instance)
 {
     ASSERT(instance);
     ASSERT(instance->inUseShadowTree());
-    ASSERT(hasSVGRareData());
 
     WillBeHeapHashSet<RawPtrWillBeWeakMember<SVGElement> >& instances = svgRareData()->elementInstances();
     ASSERT(instances.contains(instance));
@@ -598,7 +573,6 @@ void SVGElement::setCursorElement(SVGCursorElement* cursorElement)
 #if !ENABLE(OILPAN)
 void SVGElement::cursorElementRemoved()
 {
-    ASSERT(hasSVGRareData());
     svgRareData()->setCursorElement(0);
 }
 #endif
@@ -619,7 +593,6 @@ void SVGElement::setCursorImageValue(CSSCursorImageValue* cursorImageValue)
 #if !ENABLE(OILPAN)
 void SVGElement::cursorImageValueRemoved()
 {
-    ASSERT(hasSVGRareData());
     svgRareData()->setCursorImageValue(0);
 }
 #endif
@@ -1196,6 +1169,7 @@ bool SVGElement::isAnimatableAttribute(const QualifiedName& name) const
 void SVGElement::trace(Visitor* visitor)
 {
     visitor->trace(m_elementsWithRelativeLengths);
+    visitor->trace(m_SVGRareData);
     Element::trace(visitor);
 }
 
