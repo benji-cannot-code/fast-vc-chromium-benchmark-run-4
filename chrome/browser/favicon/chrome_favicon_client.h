8a0650206ea5546e49c1650c39884a1acec7e39f
jif@chromium.org <jif@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-27 00:10:40 +0000

    Move the FaviconClient implementation to ChromeFaviconClient.
    This is an intermediary CL. The FaviconClient is moving towards being a KeyedService.
    
    BUG=377505
    
    Review URL: https://codereview.chromium.org/303453004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272896 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/favicon/chrome_favicon_client.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_FAVICON_CHROME_FAVICON_CLIENT_H_
+#define CHROME_BROWSER_FAVICON_CHROME_FAVICON_CLIENT_H_
+
+#include "components/favicon/core/browser/favicon_client.h"
+
+#include "base/macros.h"
+
+class FaviconService;
+class GURL;
+class Profile;
+
+// ChromeFaviconClient implements the the FaviconClient interface.
+class ChromeFaviconClient : public FaviconClient {
+ public:
+  explicit ChromeFaviconClient(Profile* profile);
+  virtual ~ChromeFaviconClient();
+
+  // FaviconClient implementation:
+  virtual FaviconService* GetFaviconService() OVERRIDE;
+  virtual bool IsBookmarked(const GURL& url) OVERRIDE;
+
+ private:
+  Profile* profile_;
+  DISALLOW_COPY_AND_ASSIGN(ChromeFaviconClient);
+};
+
+#endif  // CHROME_BROWSER_FAVICON_CHROME_FAVICON_CLIENT_H_
--- chrome/chrome_browser.gypi
+++ chrome/chrome_browser.gypi
@@ -629,6 +629,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/extensions/token_cache/token_cache_service_factory.h',
         'browser/external_protocol/external_protocol_handler.cc',
         'browser/external_protocol/external_protocol_handler.h',
+        'browser/favicon/chrome_favicon_client.cc',
+        'browser/favicon/chrome_favicon_client.h',
         'browser/favicon/favicon_changed_details.cc',
         'browser/favicon/favicon_changed_details.h',
         'browser/favicon/favicon_handler.cc',
