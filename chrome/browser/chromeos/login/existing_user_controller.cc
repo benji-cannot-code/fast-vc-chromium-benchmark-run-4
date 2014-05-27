d94b464b933d0e1ffa91acfcfc03c6a0d11b352a
sergeyv@chromium.org <sergeyv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-27 11:18:03 +0000

    Revert of ChromeOS login webui refactoring: split user selection/gaia login screens. (https://codereview.chromium.org/293613003/)
    Reason for revert:
    Speculative Revert: looks like it can cause failure on chromium.chromiumos: http://build.chromium.org/p/chromium.chromiumos/buildstatus?builder=Linux%20ChromiumOS%20Tests%20%28dbg%29%282%29&number=10843
    
    Original issue's description:
    > ChromeOS login webui refactoring: split user selection/gaia login screens.
    > Phase one : introduce future screen classes before moving code from handler.
    > 
    > BUG=373314
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=272870
    
    TBR=nkostylev@chromium.org,antrim@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=373314
    
    Review URL: https://codereview.chromium.org/298193002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272944 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/existing_user_controller.cc
+++ chrome/browser/chromeos/login/existing_user_controller.cc
@@ -610,6 +610,7 @@ void ExistingUserController::SetDisplayEmail(const std::string& email) {
 void ExistingUserController::ShowWrongHWIDScreen() {
   scoped_ptr<base::DictionaryValue> params;
   host_->StartWizard(WizardController::kWrongHWIDScreenName, params.Pass());
+  login_display_->OnFadeOut();
 }
 
 void ExistingUserController::Signout() {
@@ -654,22 +655,26 @@ void ExistingUserController::ShowEnrollmentScreen(bool is_auto_enrollment,
   }
   host_->StartWizard(WizardController::kEnrollmentScreenName,
                      params.Pass());
+  login_display_->OnFadeOut();
 }
 
 void ExistingUserController::ShowResetScreen() {
   scoped_ptr<base::DictionaryValue> params;
   host_->StartWizard(WizardController::kResetScreenName, params.Pass());
+  login_display_->OnFadeOut();
 }
 
 void ExistingUserController::ShowKioskEnableScreen() {
   scoped_ptr<base::DictionaryValue> params;
   host_->StartWizard(WizardController::kKioskEnableScreenName, params.Pass());
+  login_display_->OnFadeOut();
 }
 
 void ExistingUserController::ShowKioskAutolaunchScreen() {
   scoped_ptr<base::DictionaryValue> params;
   host_->StartWizard(WizardController::kKioskAutolaunchScreenName,
                      params.Pass());
+  login_display_->OnFadeOut();
 }
 
 void ExistingUserController::ShowTPMError() {
@@ -780,6 +785,9 @@ void ExistingUserController::OnLoginSuccess(const UserContext& user_context) {
                                     this);
 
   display_email_.clear();
+
+  // Notify LoginDisplay to allow it provide visual feedback to user.
+  login_display_->OnLoginSuccess(user_context.GetUserID());
 }
 
 void ExistingUserController::OnProfilePrepared(Profile* profile) {
@@ -820,6 +828,7 @@ void ExistingUserController::OnProfilePrepared(Profile* profile) {
   // Inform |login_status_consumer_| about successful login.
   if (login_status_consumer_)
     login_status_consumer_->OnLoginSuccess(UserContext());
+  login_display_->OnFadeOut();
 }
 
 void ExistingUserController::OnOffTheRecordLoginSuccess() {
