a70f131a5df7d7366d13ced1363419775ce20748
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-27 01:13:31 +0000

    Make PostEventDispatchHandler::OnGestureEvent() only show the context menu on long press if the events are targetted at a view which can show the context menu. If a different view is the gesture handler and the event was targetted at a different view (e.g. because the maximize button handled ET_GESTURE_TAP_DOWN) the parent view should not show a context menu on long press.
    Previously, we were assuming that events were in the coordinates of
    |PostEventDispatchHandler::owner_| and were sometimes passing an
    incorrect position to BrowserFrame::ShowContextMenuForView().
    
    BUG=375232
    TEST=RootViewTest.ContextMenuFromLongPress
    
    Review URL: https://codereview.chromium.org/291273007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272907 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/view.h
+++ ui/views/view.h
@@ -1599,7 +1599,6 @@ class VIEWS_EXPORT View : public ui::LayerDelegate,
 
   // Input  --------------------------------------------------------------------
 
-  scoped_ptr<internal::PostEventDispatchHandler> post_dispatch_handler_;
   scoped_ptr<ui::EventTargeter> targeter_;
 
   // Accessibility -------------------------------------------------------------
