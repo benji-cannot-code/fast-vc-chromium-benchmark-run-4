c4e62a89e5adf21cc91128430f50a1589cd51d0d
eustas@chromium.org <eustas@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-28 11:00:25 +0000

    DevTools: Do not use displayNameForURL in NetworkRequest
    BUG=377723
    
    Review URL: https://codereview.chromium.org/302783010
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174958 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sdk/NetworkRequest.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/NetworkRequest.js
@@ -898,7 +898,7 @@ WebInspector.NetworkRequest.prototype = {
     },
 
     /**
-     * @return {!{type: !WebInspector.NetworkRequest.InitiatorType, url: string, source: string, lineNumber: number, columnNumber: number}}
+     * @return {!{type: !WebInspector.NetworkRequest.InitiatorType, url: string, lineNumber: number, columnNumber: number}}
      */
     initiatorInfo: function()
     {
@@ -929,7 +929,7 @@ WebInspector.NetworkRequest.prototype = {
             }
         }
 
-        this._initiatorInfo = {type: type, url: url, source: WebInspector.displayNameForURL(url), lineNumber: lineNumber, columnNumber: columnNumber};
+        this._initiatorInfo = {type: type, url: url, lineNumber: lineNumber, columnNumber: columnNumber};
         return this._initiatorInfo;
     },
 
