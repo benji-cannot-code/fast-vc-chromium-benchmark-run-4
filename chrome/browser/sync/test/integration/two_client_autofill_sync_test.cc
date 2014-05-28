d8f3f13e6c59f8ece63af92dc17b77abd339d797
rlarocque@chromium.org <rlarocque@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-28 02:51:35 +0000

    sync: Refactor autofill integration tests
    Refactors the autofill integration tests so they no longer need to rely
    on snapshots or self-notifications.
    
    Deletes the integration tests around disabling the type or disabling
    sync, since we decided in the review of r269198 that those tests are not
    valuable.
    
    Introduces KeysMatchStatusChangeChecker, a fairly typical
    MultiClientStatusChangeChecker based on the existing KeysMatch()
    function.
    
    Introduces ProfilesMatchStatusChecker, which might be the first
    StatusChangeChecker to not register as an observer of the
    ProfileSyncService.  Autofill profiles are stored and synced on the DB
    thread but cached on the UI thread.  There's no guarantee that the UI
    thread cache of autofill profile data would be up to date by the time
    the sync cycle notificaiton is emitted.  Instead of listening for sync
    notifications, the ProfileMatchStatusChangeChecker registers with the
    owner of the UI thread cache of autofill profile data, the
    PersonalDataManager, so it can be notified directly when the cache is
    updated.
    
    Since it uses a new notification mechanism, there is some reason to
    believe this could fix issue 152551.
    
    BUG=97780,95742,152551
    
    Review URL: https://codereview.chromium.org/303643003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273122 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/test/integration/two_client_autofill_sync_test.cc
+++ chrome/browser/sync/test/integration/two_client_autofill_sync_test.cc
@@ -23,6 +23,8 @@ using autofill::CreditCard;
 using autofill::PersonalDataManager;
 using autofill_helper::AddKeys;
 using autofill_helper::AddProfile;
+using autofill_helper::AwaitKeysMatch;
+using autofill_helper::AwaitProfilesMatch;
 using autofill_helper::CreateAutofillProfile;
 using autofill_helper::GetAllKeys;
 using autofill_helper::GetAllProfiles;
@@ -48,6 +50,8 @@ class TwoClientAutofillSyncTest : public SyncTest {
   TwoClientAutofillSyncTest() : SyncTest(TWO_CLIENT) { count = 0; }
   virtual ~TwoClientAutofillSyncTest() {}
 
+  virtual bool TestUsesSelfNotifications() OVERRIDE { return false; }
+
   // We do this so as to make a change that will trigger the autofill to sync.
   // By default autofill does not sync unless there is some other change.
   void MakeABookmarkChange(int profile) {
@@ -68,8 +72,7 @@ IN_PROC_BROWSER_TEST_F(TwoClientAutofillSyncTest, WebDataServiceSanity) {
   keys.insert(AutofillKey("name0", "value0"));
   AddKeys(0, keys);
   MakeABookmarkChange(0);
-  ASSERT_TRUE(GetClient(0)->AwaitMutualSyncCycleCompletion(GetClient(1)));
-  ASSERT_TRUE(KeysMatch(0, 1));
+  ASSERT_TRUE(AwaitKeysMatch(0, 1));
   ASSERT_EQ(1U, GetAllKeys(0).size());
 
   // Client1 adds a key.
@@ -77,8 +80,7 @@ IN_PROC_BROWSER_TEST_F(TwoClientAutofillSyncTest, WebDataServiceSanity) {
   keys.insert(AutofillKey("name1", "value1-0"));
   AddKeys(1, keys);
   MakeABookmarkChange(1);
-  ASSERT_TRUE(GetClient(1)->AwaitMutualSyncCycleCompletion(GetClient(0)));
-  ASSERT_TRUE(KeysMatch(0, 1));
+  ASSERT_TRUE(AwaitKeysMatch(0, 1));
   ASSERT_EQ(2U, GetAllKeys(0).size());
 
   // Client0 adds a key with the same name.
@@ -86,23 +88,20 @@ IN_PROC_BROWSER_TEST_F(TwoClientAutofillSyncTest, WebDataServiceSanity) {
   keys.insert(AutofillKey("name1", "value1-1"));
   AddKeys(0, keys);
   MakeABookmarkChange(0);
-  ASSERT_TRUE(GetClient(0)->AwaitMutualSyncCycleCompletion(GetClient(1)));
-  ASSERT_TRUE(KeysMatch(0, 1));
+  ASSERT_TRUE(AwaitKeysMatch(0, 1));
   ASSERT_EQ(3U, GetAllKeys(0).size());
 
   // Client1 removes a key.
   RemoveKey(1, AutofillKey("name1", "value1-0"));
   MakeABookmarkChange(1);
-  ASSERT_TRUE(GetClient(1)->AwaitMutualSyncCycleCompletion(GetClient(0)));
-  ASSERT_TRUE(KeysMatch(0, 1));
+  ASSERT_TRUE(AwaitKeysMatch(0, 1));
   ASSERT_EQ(2U, GetAllKeys(0).size());
 
   // Client0 removes the rest.
   RemoveKey(0, AutofillKey("name0", "value0"));
   RemoveKey(0, AutofillKey("name1", "value1-1"));
   MakeABookmarkChange(0);
-  ASSERT_TRUE(GetClient(0)->AwaitMutualSyncCycleCompletion(GetClient(1)));
-  ASSERT_TRUE(KeysMatch(0, 1));
+  ASSERT_TRUE(AwaitKeysMatch(0, 1));
   ASSERT_EQ(0U, GetAllKeys(0).size());
 }
 
@@ -115,8 +114,7 @@ IN_PROC_BROWSER_TEST_F(TwoClientAutofillSyncTest, AddUnicodeProfile) {
                           base::WideToUTF16(L"\u00C1g\u00E6tis byrjun")));
   AddKeys(0, keys);
   ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
-  ASSERT_TRUE(AwaitQuiescence());
-  ASSERT_TRUE(KeysMatch(0, 1));
+  ASSERT_TRUE(AwaitKeysMatch(0, 1));
 }
 
 IN_PROC_BROWSER_TEST_F(TwoClientAutofillSyncTest,
@@ -129,8 +127,7 @@ IN_PROC_BROWSER_TEST_F(TwoClientAutofillSyncTest,
   keys.insert(AutofillKey("name1", "value1"));
   AddKeys(0, keys);
   ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
-  ASSERT_TRUE(AwaitQuiescence());
-  ASSERT_TRUE(KeysMatch(0, 1));
+  ASSERT_TRUE(AwaitKeysMatch(0, 1));
   ASSERT_EQ(2U, GetAllKeys(0).size());
 }
 
@@ -150,8 +147,7 @@ IN_PROC_BROWSER_TEST_F(TwoClientAutofillSyncTest,
   AddKeys(1, keys1);
 
   ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
-  ASSERT_TRUE(AwaitQuiescence());
-  ASSERT_TRUE(KeysMatch(0, 1));
+  ASSERT_TRUE(AwaitKeysMatch(0, 1));
   ASSERT_EQ(5U, GetAllKeys(0).size());
 }
 
@@ -162,29 +158,25 @@ IN_PROC_BROWSER_TEST_F(TwoClientAutofillSyncTest,
   // Client0 adds a profile.
   AddProfile(0, CreateAutofillProfile(PROFILE_HOMER));
   MakeABookmarkChange(0);
-  ASSERT_TRUE(GetClient(0)->AwaitMutualSyncCycleCompletion(GetClient(1)));
-  ASSERT_TRUE(ProfilesMatch(0, 1));
+  ASSERT_TRUE(AwaitProfilesMatch(0, 1));
   ASSERT_EQ(1U, GetAllProfiles(0).size());
 
   // Client1 adds a profile.
   AddProfile(1, CreateAutofillProfile(PROFILE_MARION));
   MakeABookmarkChange(1);
-  ASSERT_TRUE(GetClient(1)->AwaitMutualSyncCycleCompletion(GetClient(0)));
-  ASSERT_TRUE(ProfilesMatch(0, 1));
+  ASSERT_TRUE(AwaitProfilesMatch(0, 1));
   ASSERT_EQ(2U, GetAllProfiles(0).size());
 
   // Client0 adds the same profile.
   AddProfile(0, CreateAutofillProfile(PROFILE_MARION));
   MakeABookmarkChange(0);
-  ASSERT_TRUE(GetClient(0)->AwaitMutualSyncCycleCompletion(GetClient(1)));
-  ASSERT_TRUE(ProfilesMatch(0, 1));
+  ASSERT_TRUE(AwaitProfilesMatch(0, 1));
   ASSERT_EQ(2U, GetAllProfiles(0).size());
 
   // Client1 removes a profile.
   RemoveProfile(1, GetAllProfiles(1)[0]->guid());
   MakeABookmarkChange(1);
-  ASSERT_TRUE(GetClient(1)->AwaitMutualSyncCycleCompletion(GetClient(0)));
-  ASSERT_TRUE(ProfilesMatch(0, 1));
+  ASSERT_TRUE(AwaitProfilesMatch(0, 1));
   ASSERT_EQ(1U, GetAllProfiles(0).size());
 
   // Client0 updates a profile.
@@ -193,15 +185,13 @@ IN_PROC_BROWSER_TEST_F(TwoClientAutofillSyncTest,
                 AutofillType(autofill::NAME_FIRST),
                 base::ASCIIToUTF16("Bart"));
   MakeABookmarkChange(0);
-  ASSERT_TRUE(GetClient(0)->AwaitMutualSyncCycleCompletion(GetClient(1)));
-  ASSERT_TRUE(ProfilesMatch(0, 1));
+  ASSERT_TRUE(AwaitProfilesMatch(0, 1));
   ASSERT_EQ(1U, GetAllProfiles(0).size());
 
   // Client1 removes remaining profile.
   RemoveProfile(1, GetAllProfiles(1)[0]->guid());
   MakeABookmarkChange(1);
-  ASSERT_TRUE(GetClient(1)->AwaitMutualSyncCycleCompletion(GetClient(0)));
-  ASSERT_TRUE(ProfilesMatch(0, 1));
+  ASSERT_TRUE(AwaitProfilesMatch(0, 1));
   ASSERT_EQ(0U, GetAllProfiles(0).size());
 }
 
@@ -212,8 +202,7 @@ IN_PROC_BROWSER_TEST_F(TwoClientAutofillSyncTest, AddDuplicateProfiles) {
   AddProfile(0, CreateAutofillProfile(PROFILE_HOMER));
   AddProfile(0, CreateAutofillProfile(PROFILE_HOMER));
   ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
-  ASSERT_TRUE(AwaitQuiescence());
-  ASSERT_TRUE(ProfilesMatch(0, 1));
+  ASSERT_TRUE(AwaitProfilesMatch(0, 1));
   ASSERT_EQ(1U, GetAllProfiles(0).size());
 }
 
@@ -229,8 +218,7 @@ IN_PROC_BROWSER_TEST_F(TwoClientAutofillSyncTest, SameProfileWithConflict) {
   AddProfile(0, profile0);
   AddProfile(1, profile1);
   ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
-  ASSERT_TRUE(AwaitQuiescence());
-  ASSERT_TRUE(ProfilesMatch(0, 1));
+  ASSERT_TRUE(AwaitProfilesMatch(0, 1));
   ASSERT_EQ(1U, GetAllProfiles(0).size());
 }
 
@@ -239,8 +227,7 @@ IN_PROC_BROWSER_TEST_F(TwoClientAutofillSyncTest, AddEmptyProfile) {
   ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
 
   AddProfile(0, CreateAutofillProfile(PROFILE_NULL));
-  ASSERT_TRUE(GetClient(0)->AwaitMutualSyncCycleCompletion(GetClient(1)));
-  ASSERT_TRUE(ProfilesMatch(0, 1));
+  ASSERT_TRUE(AwaitProfilesMatch(0, 1));
   ASSERT_EQ(0U, GetAllProfiles(0).size());
 }
 
@@ -250,8 +237,7 @@ IN_PROC_BROWSER_TEST_F(TwoClientAutofillSyncTest, AddProfile) {
 
   AddProfile(0, CreateAutofillProfile(PROFILE_HOMER));
   MakeABookmarkChange(0);
-  ASSERT_TRUE(GetClient(0)->AwaitMutualSyncCycleCompletion(GetClient(1)));
-  ASSERT_TRUE(ProfilesMatch(0, 1));
+  ASSERT_TRUE(AwaitProfilesMatch(0, 1));
   ASSERT_EQ(1U, GetAllProfiles(0).size());
 }
 
@@ -263,8 +249,7 @@ IN_PROC_BROWSER_TEST_F(TwoClientAutofillSyncTest, AddMultipleProfiles) {
   AddProfile(0, CreateAutofillProfile(PROFILE_MARION));
   AddProfile(0, CreateAutofillProfile(PROFILE_FRASIER));
   MakeABookmarkChange(0);
-  ASSERT_TRUE(GetClient(0)->AwaitMutualSyncCycleCompletion(GetClient(1)));
-  ASSERT_TRUE(ProfilesMatch(0, 1));
+  ASSERT_TRUE(AwaitProfilesMatch(0, 1));
   ASSERT_EQ(3U, GetAllProfiles(0).size());
 }
 
@@ -274,14 +259,12 @@ IN_PROC_BROWSER_TEST_F(TwoClientAutofillSyncTest, DeleteProfile) {
 
   AddProfile(0, CreateAutofillProfile(PROFILE_HOMER));
   MakeABookmarkChange(0);
-  ASSERT_TRUE(GetClient(0)->AwaitMutualSyncCycleCompletion(GetClient(1)));
-  ASSERT_TRUE(ProfilesMatch(0, 1));
+  ASSERT_TRUE(AwaitProfilesMatch(0, 1));
   ASSERT_EQ(1U, GetAllProfiles(0).size());
 
   RemoveProfile(1, GetAllProfiles(1)[0]->guid());
   MakeABookmarkChange(1);
-  ASSERT_TRUE(GetClient(1)->AwaitMutualSyncCycleCompletion(GetClient(0)));
-  ASSERT_TRUE(ProfilesMatch(0, 1));
+  ASSERT_TRUE(AwaitProfilesMatch(0, 1));
   ASSERT_EQ(0U, GetAllProfiles(0).size());
 }
 
@@ -293,8 +276,7 @@ IN_PROC_BROWSER_TEST_F(TwoClientAutofillSyncTest, MergeProfiles) {
   AddProfile(1, CreateAutofillProfile(PROFILE_MARION));
   AddProfile(1, CreateAutofillProfile(PROFILE_FRASIER));
   ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
-  ASSERT_TRUE(AwaitQuiescence());
-  ASSERT_TRUE(ProfilesMatch(0, 1));
+  ASSERT_TRUE(AwaitProfilesMatch(0, 1));
   ASSERT_EQ(3U, GetAllProfiles(0).size());
 }
 
@@ -304,8 +286,7 @@ IN_PROC_BROWSER_TEST_F(TwoClientAutofillSyncTest, UpdateFields) {
 
   AddProfile(0, CreateAutofillProfile(PROFILE_HOMER));
   MakeABookmarkChange(0);
-  ASSERT_TRUE(GetClient(0)->AwaitMutualSyncCycleCompletion(GetClient(1)));
-  ASSERT_TRUE(ProfilesMatch(0, 1));
+  ASSERT_TRUE(AwaitProfilesMatch(0, 1));
   ASSERT_EQ(1U, GetAllProfiles(0).size());
 
   UpdateProfile(0,
@@ -317,20 +298,17 @@ IN_PROC_BROWSER_TEST_F(TwoClientAutofillSyncTest, UpdateFields) {
                 AutofillType(autofill::EMAIL_ADDRESS),
                 base::ASCIIToUTF16("grrrl@TV.com"));
   MakeABookmarkChange(0);
-  ASSERT_TRUE(GetClient(0)->AwaitMutualSyncCycleCompletion(GetClient(1)));
-  ASSERT_TRUE(ProfilesMatch(0, 1));
+  ASSERT_TRUE(AwaitProfilesMatch(0, 1));
   ASSERT_EQ(1U, GetAllProfiles(0).size());
 }
 
 // TCM ID - 3628299.
-// This test is flaky on all platforms.  See crbug.com/152551.
-IN_PROC_BROWSER_TEST_F(TwoClientAutofillSyncTest, DISABLED_ConflictingFields) {
+IN_PROC_BROWSER_TEST_F(TwoClientAutofillSyncTest, ConflictingFields) {
   ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
 
   AddProfile(0, CreateAutofillProfile(PROFILE_HOMER));
   MakeABookmarkChange(0);
-  ASSERT_TRUE(GetClient(0)->AwaitMutualSyncCycleCompletion(GetClient(1)));
-  ASSERT_TRUE(ProfilesMatch(0, 1));
+  ASSERT_TRUE(AwaitProfilesMatch(0, 1));
   ASSERT_EQ(1U, GetAllProfiles(0).size());
   UpdateProfile(0,
                 GetAllProfiles(0)[0]->guid(),
@@ -342,58 +320,8 @@ IN_PROC_BROWSER_TEST_F(TwoClientAutofillSyncTest, DISABLED_ConflictingFields) {
                 AutofillType(autofill::NAME_FIRST),
                 base::ASCIIToUTF16("Bart"));
   MakeABookmarkChange(1);
-  ASSERT_TRUE(AwaitQuiescence());
-  ASSERT_TRUE(ProfilesMatch(0, 1));
+  ASSERT_TRUE(AwaitProfilesMatch(0, 1));
   ASSERT_EQ(1U, GetAllProfiles(0).size());
-}
-
-// TCM ID - 3663293.
-IN_PROC_BROWSER_TEST_F(TwoClientAutofillSyncTest, DisableAutofill) {
-  ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
-
-  AddProfile(0, CreateAutofillProfile(PROFILE_HOMER));
-  MakeABookmarkChange(0);
-  ASSERT_TRUE(GetClient(0)->AwaitMutualSyncCycleCompletion(GetClient(1)));
-  ASSERT_TRUE(ProfilesMatch(0, 1));
-  ASSERT_EQ(1U, GetAllProfiles(0).size());
-
-  ASSERT_TRUE(GetClient(0)->DisableSyncForDatatype(syncer::AUTOFILL));
-  AddProfile(0, CreateAutofillProfile(PROFILE_FRASIER));
-  MakeABookmarkChange(0);
-  ASSERT_TRUE(AwaitQuiescence());
-  ASSERT_FALSE(ProfilesMatch(0, 1));
-  ASSERT_EQ(2U, GetAllProfiles(0).size());
-  ASSERT_EQ(1U, GetAllProfiles(1).size());
-
-  ASSERT_TRUE(GetClient(0)->EnableSyncForDatatype(syncer::AUTOFILL));
-  MakeABookmarkChange(0);
-  ASSERT_TRUE(AwaitQuiescence());
-  ASSERT_TRUE(ProfilesMatch(0, 1));
-  ASSERT_EQ(2U, GetAllProfiles(0).size());
-}
-
-// TCM ID - 3661291.
-IN_PROC_BROWSER_TEST_F(TwoClientAutofillSyncTest, DisableSync) {
-  ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
-
-  AddProfile(0, CreateAutofillProfile(PROFILE_HOMER));
-  MakeABookmarkChange(0);
-  ASSERT_TRUE(GetClient(0)->AwaitMutualSyncCycleCompletion(GetClient(1)));
-  ASSERT_TRUE(ProfilesMatch(0, 1));
-  ASSERT_EQ(1U, GetAllProfiles(0).size());
-
-  ASSERT_TRUE(GetClient(1)->DisableSyncForAllDatatypes());
-  AddProfile(0, CreateAutofillProfile(PROFILE_FRASIER));
-  MakeABookmarkChange(0);
-  ASSERT_TRUE(AwaitCommitActivityCompletion(GetSyncService((0))));
-  ASSERT_FALSE(ProfilesMatch(0, 1));
-  ASSERT_EQ(2U, GetAllProfiles(0).size());
-  ASSERT_EQ(1U, GetAllProfiles(1).size());
-
-  ASSERT_TRUE(GetClient(1)->EnableSyncForAllDatatypes());
-  ASSERT_TRUE(AwaitQuiescence());
-  ASSERT_TRUE(ProfilesMatch(0, 1));
-  ASSERT_EQ(2U, GetAllProfiles(0).size());
 }
 
 // TCM ID - 3608295.
@@ -402,8 +330,7 @@ IN_PROC_BROWSER_TEST_F(TwoClientAutofillSyncTest, MaxLength) {
 
   AddProfile(0, CreateAutofillProfile(PROFILE_HOMER));
   MakeABookmarkChange(0);
-  ASSERT_TRUE(GetClient(0)->AwaitMutualSyncCycleCompletion(GetClient(1)));
-  ASSERT_TRUE(ProfilesMatch(0, 1));
+  ASSERT_TRUE(AwaitProfilesMatch(0, 1));
   ASSERT_EQ(1U, GetAllProfiles(0).size());
 
   base::string16 max_length_string(AutofillTable::kMaxDataLength, '.');
@@ -425,8 +352,7 @@ IN_PROC_BROWSER_TEST_F(TwoClientAutofillSyncTest, MaxLength) {
                 max_length_string);
 
   MakeABookmarkChange(0);
-  ASSERT_TRUE(GetClient(0)->AwaitMutualSyncCycleCompletion(GetClient(1)));
-  ASSERT_TRUE(ProfilesMatch(0, 1));
+  ASSERT_TRUE(AwaitProfilesMatch(0, 1));
 }
 
 IN_PROC_BROWSER_TEST_F(TwoClientAutofillSyncTest, ExceedsMaxLength) {
@@ -434,8 +360,7 @@ IN_PROC_BROWSER_TEST_F(TwoClientAutofillSyncTest, ExceedsMaxLength) {
 
   AddProfile(0, CreateAutofillProfile(PROFILE_HOMER));
   MakeABookmarkChange(0);
-  ASSERT_TRUE(GetClient(0)->AwaitMutualSyncCycleCompletion(GetClient(1)));
-  ASSERT_TRUE(ProfilesMatch(0, 1));
+  ASSERT_TRUE(AwaitProfilesMatch(0, 1));
   ASSERT_EQ(1U, GetAllProfiles(0).size());
 
   base::string16 exceeds_max_length_string(
@@ -458,8 +383,8 @@ IN_PROC_BROWSER_TEST_F(TwoClientAutofillSyncTest, ExceedsMaxLength) {
                 exceeds_max_length_string);
 
   MakeABookmarkChange(0);
-  ASSERT_TRUE(GetClient(0)->AwaitMutualSyncCycleCompletion(GetClient(1)));
-  ASSERT_FALSE(ProfilesMatch(0, 1));
+  ASSERT_TRUE(bookmarks_helper::AwaitAllModelsMatch());
+  EXPECT_FALSE(ProfilesMatch(0, 1));
 }
 
 // Test credit cards don't sync.
@@ -476,8 +401,7 @@ IN_PROC_BROWSER_TEST_F(TwoClientAutofillSyncTest, NoCreditCardSync) {
   SetCreditCards(0, &credit_cards);
 
   MakeABookmarkChange(0);
-  ASSERT_TRUE(GetClient(0)->AwaitMutualSyncCycleCompletion(GetClient(1)));
-  ASSERT_TRUE(ProfilesMatch(0, 1));
+  ASSERT_TRUE(AwaitProfilesMatch(0, 1));
   ASSERT_EQ(1U, GetAllProfiles(0).size());
 
   PersonalDataManager* pdm = GetPersonalDataManager(1);
