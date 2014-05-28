c99a4adacd20774312a53d719b81fbe3e382d2c4
dtrebbien@gmail.com <dtrebbien@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-28 23:34:04 +0000

    Fix: Incorrect display of HTML select multiple tag on Android
    This CL adds to RenderMenuList::setTextFromOption(int) so that when the
    associated HTML <select> element has the multiple attribute, the <select>
    element's options are iterated over and a count of the number of selected
    options is determined. When the number of selected options is 0 or 2 or
    more, then the RenderMenuList's text is set to "SELECTED_COUNT selected"
    (e.g. "2 selected"). When exactly one option is selected, then the text
    is set to the selected option's text.
    
    This is similar to iOS. When 0 or 2 or more options are selected, then
    the text of the menulist button is set to "SELECTED_COUNT Items". When
    exactly one option is selected, then the text is set to the selected
    option's text.
    
    Depends on:
    - https://codereview.chromium.org/280483002/
    - https://codereview.chromium.org/295933003/
    
    BUG=314704
    
    Review URL: https://codereview.chromium.org/302473003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175010 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderMenuList.cpp
+++ third_party/WebKit/Source/core/rendering/RenderMenuList.cpp
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/rendering/RenderView.h"
 #include "platform/fonts/FontCache.h"
 #include "platform/geometry/IntSize.h"
+#include "platform/text/PlatformLocale.h"
 
 using namespace std;
 
@@ -216,19 +217,51 @@ void RenderMenuList::setTextFromOption(int optionIndex)
 {
     HTMLSelectElement* select = selectElement();
     const WillBeHeapVector<RawPtrWillBeMember<HTMLElement> >& listItems = select->listItems();
-    int size = listItems.size();
+    const int size = listItems.size();
 
-    int i = select->optionToListIndex(optionIndex);
     String text = emptyString();
-    if (i >= 0 && i < size) {
-        Element* element = listItems[i];
-        if (isHTMLOptionElement(*element)) {
-            text = toHTMLOptionElement(element)->textIndentedToRespectGroupLabel();
-            m_optionStyle = element->renderStyle();
+    m_optionStyle.clear();
+
+    if (multiple()) {
+        unsigned selectedCount = 0;
+        int firstSelectedIndex = -1;
+        for (int i = 0; i < size; ++i) {
+            Element* element = listItems[i];
+            if (!isHTMLOptionElement(*element))
+                continue;
+
+            if (toHTMLOptionElement(element)->selected()) {
+                if (++selectedCount == 1)
+                    firstSelectedIndex = i;
+            }
+        }
+
+        if (selectedCount == 1) {
+            ASSERT(0 <= firstSelectedIndex);
+            ASSERT(firstSelectedIndex < size);
+            HTMLOptionElement* selectedOptionElement = toHTMLOptionElement(listItems[firstSelectedIndex]);
+            ASSERT(selectedOptionElement->selected());
+            text = selectedOptionElement->textIndentedToRespectGroupLabel();
+            m_optionStyle = selectedOptionElement->renderStyle();
+        } else {
+            Locale& locale = select->locale();
+            String localizedNumberString = locale.convertToLocalizedNumber(String::number(selectedCount));
+            text = locale.queryString(blink::WebLocalizedString::SelectMenuListText, localizedNumberString);
+            ASSERT(!m_optionStyle);
+        }
+    } else {
+        const int i = select->optionToListIndex(optionIndex);
+        if (i >= 0 && i < size) {
+            Element* element = listItems[i];
+            if (isHTMLOptionElement(*element)) {
+                text = toHTMLOptionElement(element)->textIndentedToRespectGroupLabel();
+                m_optionStyle = element->renderStyle();
+            }
         }
     }
 
     setText(text.stripWhiteSpace());
+
     didUpdateActiveOption(optionIndex);
 }
 
