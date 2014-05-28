40b7501afee180080e5dc528a4c0ef8546b24062
mkwst@chromium.org <mkwst@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-28 18:55:59 +0000

    Password bubble: Block UI on non-webby URLs.
    We shouldn't display the password management UI on the Chrome sign-in
    page, or any other non-webby scheme.
    
    BUG=378181
    
    Review URL: https://codereview.chromium.org/300873005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273305 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/passwords/manage_passwords_ui_controller.cc
+++ chrome/browser/ui/passwords/manage_passwords_ui_controller.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/passwords/manage_passwords_ui_controller.h"
 
 #include "chrome/app/chrome_command_ids.h"
+#include "chrome/browser/browsing_data/browsing_data_helper.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/password_manager/password_store_factory.h"
 #include "chrome/browser/ui/browser_command_controller.h"
@@ -47,6 +48,13 @@ ManagePasswordsUIController::ManagePasswordsUIController(
 ManagePasswordsUIController::~ManagePasswordsUIController() {}
 
 void ManagePasswordsUIController::UpdateBubbleAndIconVisibility() {
+  // If we're not on a "webby" URL (e.g. "chrome://sign-in"), we shouldn't
+  // display either the bubble or the icon.
+  if (!BrowsingDataHelper::IsWebScheme(
+          web_contents()->GetLastCommittedURL().scheme())) {
+    state_ = password_manager::ui::INACTIVE_STATE;
+  }
+
   #if !defined(OS_ANDROID)
     Browser* browser = chrome::FindBrowserWithWebContents(web_contents());
     if (!browser)
@@ -153,6 +161,7 @@ void ManagePasswordsUIController::UnblacklistSite() {
   // form. We can safely pull it out, send it over to the password store
   // for removal, and update our internal state.
   DCHECK(!password_form_map_.empty());
+  DCHECK(password_form_map_.begin()->second);
   DCHECK(state_ == password_manager::ui::BLACKLIST_STATE);
   password_manager::PasswordStore* password_store =
       GetPasswordStore(web_contents());
--- chrome/browser/ui/views/passwords/manage_passwords_view_test.cc
+++ chrome/browser/ui/views/passwords/manage_passwords_view_test.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 
 void ManagePasswordsViewTest::SetUpOnMainThread() {
+  AddTabAtIndex(0, GURL("http://example.com/"), content::PAGE_TRANSITION_TYPED);
   // Create the test UIController here so that it's bound to the currently
   // active WebContents.
   new ManagePasswordsUIControllerMock(
