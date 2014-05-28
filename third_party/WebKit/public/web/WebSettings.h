1e4792512c2807f5498a9b9f4133db8daffd3c99
jbroman@chromium.org <jbroman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-28 13:46:02 +0000

    Remove WebSettings::setFixedPositionCreatesStackingContext.
    After https://codereview.chromium.org/289973008/ lands,
    there are no remaining call sites. And it's been a no-op
    for over a month anyhow.
    
    BUG=354688
    
    Review URL: https://codereview.chromium.org/286263004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174969 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/web/WebSettings.h
+++ third_party/WebKit/public/web/WebSettings.h
@@ -205,9 +205,6 @@ public:
     virtual void setWideViewportQuirkEnabled(bool) = 0;
     virtual void setXSSAuditorEnabled(bool) = 0;
 
-    // FIXME: Remove these unused settings.
-    virtual void setFixedPositionCreatesStackingContext(bool) { }
-
 protected:
     ~WebSettings() { }
 };
