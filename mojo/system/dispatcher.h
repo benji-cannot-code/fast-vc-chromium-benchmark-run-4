b5318a07e2a4943fef2eb86aa4a251c7a4c769c7
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-28 19:14:10 +0000

    Mojo: Move (shared) buffer types/constants/functions from mojo/public/c/system/core.h to .../buffer.h.
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/303653009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273313 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/system/dispatcher.h
+++ mojo/system/dispatcher.h
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/lock.h"
 #include "mojo/embedder/platform_handle.h"
 #include "mojo/embedder/platform_handle_vector.h"
-#include "mojo/public/c/system/core.h"
+#include "mojo/public/c/system/buffer.h"
 #include "mojo/public/c/system/data_pipe.h"
 #include "mojo/public/c/system/message_pipe.h"
 #include "mojo/public/c/system/types.h"
--- mojo/system/entrypoints.cc
+++ mojo/system/entrypoints.cc
@@ -3,6 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "mojo/system/entrypoints.h"
+
+#include "mojo/public/c/system/buffer.h"
+#include "mojo/public/c/system/data_pipe.h"
+#include "mojo/public/c/system/functions.h"
+#include "mojo/public/c/system/message_pipe.h"
 #include "mojo/system/core.h"
 
 static mojo::system::Core* g_core = NULL;
