91a0d96279c7a4b3d0171ae092d2f9668814acfb
dalecurtis@chromium.org <dalecurtis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-28 08:31:03 +0000

    Enable ADTS demuxing by default in Media Source.
    We now support ADTS demuxing in via src attribute, so lets enable
    it for Media Source as well.
    
    BUG=371633
    TEST=existing tests
    
    Review URL: https://codereview.chromium.org/294403002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273177 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1062,7 +1062,6 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kDomAutomationController,
     switches::kEnableAcceleratedFixedRootBackground,
     switches::kEnableAcceleratedOverflowScroll,
-    switches::kEnableADTSStreamParser,
     switches::kEnableBeginFrameScheduling,
     switches::kEnableBleedingEdgeRenderingFastPaths,
     switches::kEnableCompositingForFixedPosition,
