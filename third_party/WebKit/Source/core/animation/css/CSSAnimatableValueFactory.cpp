677a271c1281390ef75ba00f5629654224ddf5e5
alancutter@chromium.org <alancutter@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-28 06:11:38 +0000

    Fix setting shape-outside: none
    The CSS parser and CSSComputedStyleDeclaration use the none keyword for the
    shape-outside property. CSSAnimatableValueFactory and StyleBuilderFunctions
    treat the same shape value as the auto keyword.
    The effect of this is that the none shape value is never applied.
    
    According to the spec we should be using the none keyword.
    http://dev.w3.org/csswg/css-shapes-1/#shape-outside-property
    
    BUG=378071
    
    Review URL: https://codereview.chromium.org/303473009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174936 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/css/CSSAnimatableValueFactory.cpp
+++ third_party/WebKit/Source/core/animation/css/CSSAnimatableValueFactory.cpp
@@ -225,7 +225,7 @@ inline static PassRefPtrWillBeRawPtr<AnimatableValue> createFromShapeValue(Shape
 {
     if (value)
         return AnimatableShapeValue::create(value);
-    return AnimatableUnknown::create(CSSValueAuto);
+    return AnimatableUnknown::create(CSSValueNone);
 }
 
 static double fontWeightToDouble(FontWeight fontWeight)
