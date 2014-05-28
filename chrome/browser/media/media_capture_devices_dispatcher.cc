92283abfa3cb68c0c038c7433cf6816695be7088
gbillock@chromium.org <gbillock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-28 01:21:18 +0000

    [Media] Add a Finch experiment to enable the permission bubble request.
    R=xhwang@chromium.org
    
    Review URL: https://codereview.chromium.org/286883002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273099 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media/media_capture_devices_dispatcher.cc
+++ chrome/browser/media/media_capture_devices_dispatcher.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "apps/app_window_registry.h"
 #include "base/command_line.h"
 #include "base/logging.h"
+#include "base/metrics/field_trial.h"
 #include "base/prefs/pref_service.h"
 #include "base/prefs/scoped_user_pref_update.h"
 #include "base/sha1.h"
@@ -64,6 +65,16 @@ using content::MediaCaptureDevices;
 using content::MediaStreamDevices;
 
 namespace {
+
+// A finch experiment to enable the permission bubble for media requests only.
+bool MediaStreamPermissionBubbleExperimentEnabled() {
+  const std::string group =
+      base::FieldTrialList::FindFullName("MediaStreamPermissionBubble");
+  if (group == "enabled")
+    return true;
+
+  return false;
+}
 
 // Finds a device in |devices| that has |device_id|, or NULL if not found.
 const content::MediaStreamDevice* FindDeviceWithId(
@@ -624,7 +635,8 @@ void MediaCaptureDevicesDispatcher::ProcessQueuedAccessRequest(
 
   DCHECK(!it->second.empty());
 
-  if (PermissionBubbleManager::Enabled()) {
+  if (PermissionBubbleManager::Enabled() ||
+      MediaStreamPermissionBubbleExperimentEnabled()) {
     scoped_ptr<MediaStreamDevicesController> controller(
         new MediaStreamDevicesController(web_contents,
             it->second.front().request,
