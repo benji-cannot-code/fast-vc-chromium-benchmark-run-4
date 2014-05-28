2190e8edba6be5ecf03066a7182d588dfc8b77fc
dstockwell@chromium.org <dstockwell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-28 08:02:21 +0000

    Web Animations: Make AnimationPlayer an ActiveDOMObject
    Previously the AnimationPlayer's finish event could have been dispatched
    after the event handlers had been collected. As these events are queued
    then dispatched asynchronously via ScriptedAnimationController some
    special handling is required (tracking when the event is actually
    dispatched).
    
    BUG=376349
    
    Review URL: https://codereview.chromium.org/284323004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174943 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/AnimationPlayer.h
+++ third_party/WebKit/Source/core/animation/AnimationPlayer.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define AnimationPlayer_h
 
 #include "core/animation/AnimationNode.h"
+#include "core/dom/ActiveDOMObject.h"
 #include "core/events/EventTarget.h"
 #include "wtf/RefPtr.h"
 
@@ -41,12 +42,14 @@ namespace WebCore {
 class AnimationTimeline;
 class ExceptionState;
 
-class AnimationPlayer FINAL : public RefCountedWillBeRefCountedGarbageCollected<AnimationPlayer>, public EventTargetWithInlineData {
+class AnimationPlayer FINAL : public RefCountedWillBeRefCountedGarbageCollected<AnimationPlayer>
+    , public ActiveDOMObject
+    , public EventTargetWithInlineData {
     DEFINE_EVENT_TARGET_REFCOUNTING(RefCountedWillBeRefCountedGarbageCollected<AnimationPlayer>);
 public:
 
     ~AnimationPlayer();
-    static PassRefPtrWillBeRawPtr<AnimationPlayer> create(AnimationTimeline&, AnimationNode*);
+    static PassRefPtrWillBeRawPtr<AnimationPlayer> create(ExecutionContext*, AnimationTimeline&, AnimationNode*);
 
     // Returns whether the player is finished.
     bool update(TimingUpdateReason);
@@ -79,6 +82,9 @@ public:
 
     virtual const AtomicString& interfaceName() const OVERRIDE;
     virtual ExecutionContext* executionContext() const OVERRIDE;
+    virtual bool hasPendingActivity() const OVERRIDE;
+    virtual void stop() OVERRIDE;
+    virtual bool dispatchEvent(PassRefPtrWillBeRawPtr<Event>) OVERRIDE;
 
     double playbackRate() const { return m_playbackRate; }
     void setPlaybackRate(double);
@@ -151,7 +157,7 @@ public:
     void trace(Visitor*);
 
 private:
-    AnimationPlayer(AnimationTimeline&, AnimationNode*);
+    AnimationPlayer(ExecutionContext*, AnimationTimeline&, AnimationNode*);
     double sourceEnd() const;
     bool limited(double currentTime) const;
     double currentTimeWithoutLag() const;
@@ -178,6 +184,10 @@ private:
     bool m_outdated;
 
     bool m_finished;
+    // Holds a 'finished' event queued for asynchronous dispatch via the
+    // ScriptedAnimationController. This object remains active until the
+    // event is actually dispatched.
+    RefPtrWillBeMember<Event> m_pendingFinishedEvent;
 };
 
 } // namespace
