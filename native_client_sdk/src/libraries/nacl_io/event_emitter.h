bc8d191dbeee883fe5d2a96ef5747d8cf970a91b
sbc@chromium.org <sbc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-28 20:15:10 +0000

    [NaCl SDK] nacl_io: fix all tsan warnings.
    tsan found a few issues in the test code as well as in nacl_io
    itself.
    
    R=binji@chromium.org
    
    Review URL: https://codereview.chromium.org/306583004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273339 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/libraries/nacl_io/event_emitter.h
+++ native_client_sdk/src/libraries/nacl_io/event_emitter.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "nacl_io/error.h"
 
+#include "sdk_util/auto_lock.h"
 #include "sdk_util/macros.h"
 #include "sdk_util/ref_object.h"
 #include "sdk_util/scoped_ref.h"
@@ -43,8 +44,13 @@ class EventEmitter : public sdk_util::RefObject {
   EventEmitter();
 
   // This returns a snapshot, to ensure the status doesn't change from
-  // fetch to use, hold the lock.
-  uint32_t GetEventStatus() { return event_status_; }
+  // fetch to use, hold the lock and call GetEventStatus_Locked.
+  uint32_t GetEventStatus() {
+    AUTO_LOCK(emitter_lock_);
+    return GetEventStatus_Locked();
+  }
+
+  uint32_t GetEventStatus_Locked() { return event_status_; }
 
   sdk_util::SimpleLock& GetLock() { return emitter_lock_; }
 
