0f4b983c37006789ae9fa57cf0e4126e2fd7c31b
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-28 02:40:55 +0000

    Refactor and move ash independent accelerator handling code in nested loop to ui/wm/core
    I also renamed classes to NestedAcceleratorXxx. I felt this is a bit more clearer than NestedDispatcher, especially in ui/wm/core. Please let me know if you disagree or have better suggestion. I'm happy to rename them.
    
    BUG=None
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=272740
    
    R=ben@chromium.org
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=272995
    
    Review URL: https://codereview.chromium.org/298703007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273114 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/shell.h
+++ ash/shell.h
@@ -65,6 +65,7 @@ class TooltipController;
 namespace wm {
 class CompoundEventFilter;
 class InputMethodEventFilter;
+class NestedAcceleratorController;
 class ShadowController;
 class VisibilityController;
 class UserActivityDetector;
@@ -107,7 +108,6 @@ class MaximizeModeWindowManager;
 class MediaDelegate;
 class MouseCursorEventFilter;
 class MruWindowTracker;
-class NestedDispatcherController;
 class NewWindowDelegate;
 class OverlayEventFilter;
 class PartialMagnificationController;
@@ -632,7 +632,7 @@ class ASH_EXPORT Shell : public SystemModalContainerEventFilterDelegate,
   std::vector<WindowAndBoundsPair> to_restore_;
 
   scoped_ptr<UserMetricsRecorder> user_metrics_recorder_;
-  scoped_ptr<NestedDispatcherController> nested_dispatcher_controller_;
+  scoped_ptr< ::wm::NestedAcceleratorController> nested_accelerator_controller_;
   scoped_ptr<AcceleratorController> accelerator_controller_;
   scoped_ptr<ShellDelegate> delegate_;
   scoped_ptr<SystemTrayDelegate> system_tray_delegate_;
