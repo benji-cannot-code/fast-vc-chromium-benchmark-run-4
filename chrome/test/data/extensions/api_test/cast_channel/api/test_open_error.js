29a56446a14fd254c02298c7d711a915e67d77c3
munjal@chromium.org <munjal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-28 00:13:44 +0000

    Cast Channel API test for open error case.
    BUG=
    
    Review URL: https://codereview.chromium.org/298993005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273084 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/api_test/cast_channel/api/test_open_error.js
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var onClose = function(channel) {
+  assertClosedChannel(channel);
+}
+
+var onError = function(channel) {
+  chrome.cast.channel.close(channel, onClose);
+  chrome.test.notifyPass();
+}
+
+var onOpen = function(channel) {
+  assertClosedChannel(channel);
+  chrome.test.succeed();
+};
+
+chrome.cast.channel.onError.addListener(onError);
+chrome.cast.channel.open('cast://192.168.1.1:8009', onOpen);
