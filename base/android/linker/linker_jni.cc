2c20cb9d8c42538e67453c52c3586b5b197cfe0d
simonb@chromium.org <simonb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-28 16:50:41 +0000

    Unconditionally log chromium android linker errors.
    Always print crazy linker error messages to logcat, instead of only when
    debug tracing is enabled.
    
    Before:
    E/chromium_android_linker(11242): Unable to load library: libchrome.2016.0.so
    
    After:
    E/chromium_android_linker(11641): LoadLibrary: Could not open libchrome.2016.0.so: Could not find symbol 'isnanf'
    E/chromium_android_linker(11641): Unable to load library: libchrome.2016.0.so
    
    BUG=376828
    
    Review URL: https://codereview.chromium.org/298323002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273269 0039d316-1c4b-4281-b951-d872f2087c98

--- base/android/linker/linker_jni.cc
+++ base/android/linker/linker_jni.cc
@@ -29,11 +29,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if DEBUG
 #define LOG_INFO(...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
-#define LOG_ERROR(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
 #else
 #define LOG_INFO(...) ((void)0)
-#define LOG_ERROR(...) ((void)0)
 #endif
+#define LOG_ERROR(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
 
 #define UNUSED __attribute__((unused))
 
