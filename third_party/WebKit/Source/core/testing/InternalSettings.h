039ee9efef2226e2b8a7748fd48bb9e4c0bd639b
hayato@chromium.org <hayato@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-28 04:29:46 +0000

    Support a negation pseudo-class, :not(), in matching criteria of content insertion points.
    The spec: http://w3c.github.io/webcomponents/spec/shadow/#satisfying-matching-criteria
    
    BUG=370670
    
    Review URL: https://codereview.chromium.org/279763002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174929 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/testing/InternalSettings.h
+++ third_party/WebKit/Source/core/testing/InternalSettings.h
@@ -71,6 +71,7 @@ public:
         bool m_imagesEnabled;
         String m_defaultVideoPosterURL;
         bool m_originalLayerSquashingEnabled;
+        bool m_originalPseudoClassesInMatchingCriteriaInAuthorShadowTreesEnabled;
     };
 
     static PassRefPtrWillBeRawPtr<InternalSettings> create(Page& page)
@@ -117,6 +118,7 @@ public:
     void setOverlayScrollbarsEnabled(bool);
     void setStyleScopedEnabled(bool);
     void setExperimentalContentSecurityPolicyFeaturesEnabled(bool);
+    void setPseudoClassesInMatchingCriteriaInAuthorShadowTreesEnabled(bool);
 
     virtual void trace(Visitor*) OVERRIDE;
 
