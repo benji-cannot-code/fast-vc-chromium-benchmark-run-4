4286454aee6f409c929569ccfa9197e741af6c07
sievers@chromium.org <sievers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-28 05:47:48 +0000

    Android: Disable EGL_KHR_fence_sync on IMG
    Testing fences does not work there.
    
    BUG=371530
    
    Review URL: https://codereview.chromium.org/303473008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273165 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/config/gpu_driver_bug_list_json.cc
+++ gpu/config/gpu_driver_bug_list_json.cc
@@ -20,7 +20,7 @@ const char kGpuDriverBugListJson[] = LONG_STRING_CONST(
 {
   "name": "gpu driver bug list",
   // Please update the version number whenever you change this file.
-  "version": "5.6",
+  "version": "5.7",
   "entries": [
     {
       "id": 1,
@@ -1009,6 +1009,21 @@ LONG_STRING_CONST(
       },
       "features": [
         "disable_multisampling"
+      ]
+    },
+    {
+      "id": 76,
+      "cr_bugs": [371530],
+      "description": "Testing EGL sync fences is broken on IMG",
+      "os": {
+        "type": "android"
+      },
+      "gl_vendor": {
+        "op": "beginwith",
+        "value": "Imagination Technologies"
+      },
+      "features": [
+        "disable_egl_khr_fence_sync"
       ]
     }
   ]
