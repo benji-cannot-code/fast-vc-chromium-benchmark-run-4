2cf348c7400457d8ee4acbd096bfa47d7a4cd283
jinho.bang@samsung.com <jinho.bang@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-29 05:31:29 +0000

    Remove canvas.drawCustomFocusRing()
    The method was removed from the HTML Canvas2D Context Level 1 Specification:
      http://www.w3.org/html/wg/drafts/2dcontext/html5_canvas_CR#removals
    
    Review URL: https://codereview.chromium.org/289283019
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175036 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/canvas/CanvasRenderingContext2D.h
+++ third_party/WebKit/Source/core/html/canvas/CanvasRenderingContext2D.h
@@ -227,7 +227,6 @@ public:
 
     void drawFocusIfNeeded(Element*);
     void drawFocusIfNeeded(Path2D*, Element*);
-    bool drawCustomFocusRing(Element*);
 
     void loseContext();
     void restoreContext();
