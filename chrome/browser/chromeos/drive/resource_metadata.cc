a64f1dae9fa33e3968858d3180a68909dbac635c
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-29 20:42:09 +0000

    drive: Merge FileCacheEntry to ResourceEntry
    Update DB version to 13.
    Stop storing FileCacheEntry separately from ResourceEntry.
    Rewrite UpgradeOldDB.
    Allow modifying entries with empty parent_local_id (i.e. cache-only entries) with ResourceMetadata::RefreshEntry.
    Check local ID matching in ResourceMetadataStorage::CheckValidity
    
    BUG=275271
    TEST=unit_tests
    TEST=Manually confirmed that DB created by M-[32-35] can be opened and pinned files are still shown as pinned.
    
    Review URL: https://codereview.chromium.org/283373005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273581 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/resource_metadata.cc
+++ chrome/browser/chromeos/drive/resource_metadata.cc
@@ -57,6 +57,13 @@ FileError EntryCanUseName(ResourceMetadataStorage* storage,
   return FILE_ERROR_OK;
 }
 
+// Returns true when the ID is used by an immutable entry.
+bool IsImmutableEntry(const std::string& id) {
+  return id == util::kDriveGrandRootLocalId ||
+      id == util::kDriveOtherDirLocalId ||
+      id == util::kDriveTrashDirLocalId;
+}
+
 }  // namespace
 
 ResourceMetadata::ResourceMetadata(
@@ -268,9 +275,7 @@ FileError ResourceMetadata::RemoveEntry(const std::string& id) {
     return FILE_ERROR_NO_LOCAL_SPACE;
 
   // Disallow deletion of default entries.
-  if (id == util::kDriveGrandRootLocalId ||
-      id == util::kDriveOtherDirLocalId ||
-      id == util::kDriveTrashDirLocalId)
+  if (IsImmutableEntry(id))
     return FILE_ERROR_ACCESS_DENIED;
 
   ResourceEntry entry;
@@ -356,7 +361,7 @@ FileError ResourceMetadata::RefreshEntry(const ResourceEntry& entry) {
   if (error != FILE_ERROR_OK)
     return error;
 
-  if (old_entry.parent_local_id().empty() ||  // Reject root.
+  if (IsImmutableEntry(entry.local_id()) ||
       old_entry.file_info().is_directory() !=  // Reject incompatible input.
       entry.file_info().is_directory())
     return FILE_ERROR_INVALID_OPERATION;
