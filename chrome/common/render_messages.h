cc9bd4e7403a8612966e896bf55117b6d4c7db07
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-29 17:47:16 +0000

    Limit a few renderer messages to the platforms that support the given features.
    Review URL: https://codereview.chromium.org/303653006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273516 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/render_messages.h
+++ chrome/common/render_messages.h
@@ -593,6 +593,7 @@ IPC_MESSAGE_CONTROL1(ChromeViewHostMsg_ResourceTypeStats,
 // only when the renderer is prerendering.
 IPC_MESSAGE_ROUTED0(ChromeViewHostMsg_CancelPrerenderForPrinting)
 
+#if defined(ENABLE_EXTENSIONS)
 // Sent by the renderer to check if a URL has permission to trigger a clipboard
 // read/write operation from the DOM.
 IPC_SYNC_MESSAGE_CONTROL1_1(ChromeViewHostMsg_CanTriggerClipboardRead,
@@ -601,6 +602,7 @@ IPC_SYNC_MESSAGE_CONTROL1_1(ChromeViewHostMsg_CanTriggerClipboardRead,
 IPC_SYNC_MESSAGE_CONTROL1_1(ChromeViewHostMsg_CanTriggerClipboardWrite,
                             GURL /* origin */,
                             bool /* allowed */)
+#endif
 
 // Sent when the renderer was prevented from displaying insecure content in
 // a secure page by a security policy.  The page may appear incomplete.
@@ -737,6 +739,8 @@ IPC_MESSAGE_ROUTED4(ChromeViewHostMsg_DetailedConsoleMessageAdded,
                     extensions::StackTrace /* stack trace */,
                     int32 /* severity level */)
 
+#if defined(ENABLE_PLUGINS)
 // Sent by the renderer to check if crash reporting is enabled.
 IPC_SYNC_MESSAGE_CONTROL0_1(ChromeViewHostMsg_IsCrashReportingEnabled,
                             bool /* enabled */)
+#endif
