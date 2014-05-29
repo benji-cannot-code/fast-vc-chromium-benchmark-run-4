355941f6b83bf44d4d744a28b74f1c7da31fc21f
mathp@chromium.org <mathp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-29 15:19:51 +0000

    Revert of Fall back to X11 system tray icons if libappindicator is not available  (https://codereview.chromium.org/291893009/)
    Reason for revert:
    Suspected ASAN leak on the bots. 
    
    http://build.chromium.org/p/chromium.memory/builders/Linux%20ASan%20LSan%20Tests%20%281%29/builds/2617
    
    Original issue's description:
    > Fall back to X11 system tray icons if libappindicator is not available.
    > 
    > This CL also splits out the code wrt to handling the GTK menu for the app indicator icon out of app_indicator_icon.cc in order to share code with gtk2_status_icon.cc
    > 
    > BUG=267195
    > TEST=Manual, see steps below.
    > 1) Run chrome in a system which does not have libappindicator
    > 2) Generate HTML 5 notifications via https://developer.cdn.mozilla.net/media/uploads/demos/e/l/elfoxero/c17223c414d8ddafb7808972b5617d9e/html5-notifications_1400214081_demo_package/index.html
    > 3) Ensure that the bell icon shows up in the system tray and that clicking on the bell icon brings up the message center
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=273337
    
    TBR=erg@chromium.org,pkotwicz@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=267195
    
    Review URL: https://codereview.chromium.org/304933002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273485 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/libgtk2ui/gtk2_ui.cc
+++ chrome/browser/ui/libgtk2ui/gtk2_ui.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/libgtk2ui/gtk2_event_loop.h"
 #include "chrome/browser/ui/libgtk2ui/gtk2_key_bindings_handler.h"
 #include "chrome/browser/ui/libgtk2ui/gtk2_signal_registrar.h"
-#include "chrome/browser/ui/libgtk2ui/gtk2_status_icon.h"
 #include "chrome/browser/ui/libgtk2ui/gtk2_util.h"
 #include "chrome/browser/ui/libgtk2ui/native_theme_gtk2.h"
 #include "chrome/browser/ui/libgtk2ui/print_dialog_gtk2.h"
@@ -506,7 +505,7 @@ void Gtk2UI::SetProgressFraction(float percentage) const {
 }
 
 bool Gtk2UI::IsStatusIconSupported() const {
-  return true;
+  return AppIndicatorIcon::CouldOpen();
 }
 
 scoped_ptr<views::StatusIconLinux> Gtk2UI::CreateLinuxStatusIcon(
@@ -519,8 +518,7 @@ scoped_ptr<views::StatusIconLinux> Gtk2UI::CreateLinuxStatusIcon(
         image,
         tool_tip));
   } else {
-    return scoped_ptr<views::StatusIconLinux>(new Gtk2StatusIcon(
-        image, tool_tip));
+    return scoped_ptr<views::StatusIconLinux>();
   }
 }
 
--- chrome/browser/ui/libgtk2ui/libgtk2ui.gyp
+++ chrome/browser/ui/libgtk2ui/libgtk2ui.gyp
@@ -40,8 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'sources': [
         'app_indicator_icon.cc',
         'app_indicator_icon.h',
-        'app_indicator_icon_menu.cc',
-        'app_indicator_icon_menu.h',
         'chrome_gtk_frame.cc',
         'chrome_gtk_frame.h',
         'chrome_gtk_menu_subclasses.cc',
@@ -58,8 +56,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'gtk2_key_bindings_handler.h',
         'gtk2_signal_registrar.cc',
         'gtk2_signal_registrar.h',
-        'gtk2_status_icon.cc',
-        'gtk2_status_icon.h',
         'gtk2_ui.cc',
         'gtk2_ui.h',
         'gtk2_util.cc',
