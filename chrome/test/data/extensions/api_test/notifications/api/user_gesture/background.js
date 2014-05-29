7b020e35038cdfeb3dde4a4638aa8dfc03ea64f9
dewittj@chromium.org <dewittj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-29 23:26:34 +0000

    Add user gesture handling to notifications api.
    This fixes the bug where user gestures were not taken into account
    after user interacts with a notification.
    
    BUG=181124
    
    Review URL: https://codereview.chromium.org/296193009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273654 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/api_test/notifications/api/user_gesture/background.js
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function userActionTest() {
+  chrome.test.sendMessage("");
+}
+
+chrome.notifications.onClicked.addListener(userActionTest);
+chrome.notifications.onButtonClicked.addListener(userActionTest);
+chrome.notifications.onClosed.addListener(userActionTest);
+
+chrome.notifications.create("test", {
+  iconUrl: "",
+  title: "hi",
+  message: "there",
+  type: "basic",
+  buttons: [{title: "Button"}, {title: "Button"}],
+}, function () {});
