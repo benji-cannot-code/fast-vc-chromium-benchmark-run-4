03db16d4a448ec577184256034f8447736ed24a4
mlerman@chromium.org <mlerman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-29 22:23:07 +0000

    UMA histogram for attempts to add accounts to Chrome.
    BUG=357693
    TEST=Histogram is called Signin.AddAccounts.
    
    Review URL: https://codereview.chromium.org/298253003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273623 0039d316-1c4b-4281-b951-d872f2087c98

--- components/signin/core/browser/mutable_profile_oauth2_token_service.cc
+++ components/signin/core/browser/mutable_profile_oauth2_token_service.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/core/browser/mutable_profile_oauth2_token_service.h"
 
 #include "components/signin/core/browser/signin_client.h"
+#include "components/signin/core/browser/signin_metrics.h"
 #include "components/signin/core/browser/webdata/token_web_data.h"
 #include "components/webdata/common/web_data_service_base.h"
 #include "google_apis/gaia/gaia_auth_fetcher.h"
@@ -301,6 +302,8 @@ void MutableProfileOAuth2TokenService::UpdateCredentials(
   DCHECK(thread_checker_.CalledOnValidThread());
   DCHECK(!account_id.empty());
   DCHECK(!refresh_token.empty());
+
+  signin_metrics::LogSigninAddAccount();
 
   bool refresh_token_present = refresh_tokens_.count(account_id) > 0;
   if (!refresh_token_present ||
