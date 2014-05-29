d268d256f227c3d088a360117c53eba23ca218f6
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-29 17:23:19 +0000

    Add support for GetByExtensionId
    This is necessary for testing apps that are not installed using --load-extension.
    
    GetByExtensionId returns a list of extensions as each extension may have a number of related contexts for its various windows and background page.
    
    BUG=378408
    TEST=testExtensionGetByExtensionId and testWebApp
    
    Review URL: https://codereview.chromium.org/304773003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273510 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/extension_dict.py
+++ tools/telemetry/telemetry/core/extension_dict.py
@@ -17,7 +17,7 @@ class ExtensionDict(object):
     ExtensionPage instance."""
     if not isinstance(load_extension, extension_to_load.ExtensionToLoad):
       raise TypeError("Input param must be of type ExtensionToLoad")
-    return self._extension_backend[load_extension.extension_id]
+    return self.GetByExtensionId(load_extension.extension_id)[0]
 
   def __contains__(self, load_extension):
     """Checks if this ExtensionToLoad instance has been loaded"""
@@ -27,3 +27,8 @@ class ExtensionDict(object):
 
   def keys(self):
     return self._extension_backend.keys()
+
+  def GetByExtensionId(self, extension_id):
+    """Returns a list of extensions given an extension id. This is useful for
+    connecting to built-in apps and component extensions."""
+    return self._extension_backend[extension_id]
