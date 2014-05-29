7780eb1dded4606844850927d1a39280fcf5ae38
chrishtr@chromium.org <chrishtr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-29 00:31:21 +0000

    Refactor repainting logic to abstract away the specific repaint container.
    In basically all cases it was calling containerForRepaint(),
    so no need to pass that extra argument.
    
    BUG=370664
    
    Review URL: https://codereview.chromium.org/307713002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175015 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/compositing/RenderLayerCompositor.cpp
+++ third_party/WebKit/Source/core/rendering/compositing/RenderLayerCompositor.cpp
@@ -623,9 +623,7 @@ void RenderLayerCompositor::repaintOnCompositingChange(RenderLayer* layer)
     if (layer->renderer() != &m_renderView && !layer->renderer()->parent())
         return;
 
-    const RenderLayerModelObject* repaintContainer = layer->renderer()->containerForRepaint();
-    ASSERT(repaintContainer);
-    layer->repainter().repaintIncludingNonCompositingDescendants(repaintContainer);
+    layer->repainter().repaintIncludingNonCompositingDescendants();
 }
 
 // This method assumes that layout is up-to-date, unlike repaintOnCompositingChange().
