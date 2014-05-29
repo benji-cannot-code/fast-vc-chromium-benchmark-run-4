d0929a458c24e8d9f7692da27eea939e2013ae19
rsadam@chromium.org <rsadam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-29 00:45:37 +0000

    Update latest test IME Inputview Keyboard crx and updates the test function accordingly.
    BUG=
    
    Review URL: https://codereview.chromium.org/300923004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273423 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/chromeos/virtual_keyboard/inputview/typing_test.js
+++ chrome/test/data/chromeos/virtual_keyboard/inputview/typing_test.js
@@ -8,12 +8,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /**
  * Tests typing in the lowercase keyset.
  */
-function testLowercaseKeyset() {
-  // Mouse events.
-  mockMouseType('l');
-  mockMouseType('p');
+function testLowercaseKeysetAsync(testDoneCallback) {
+  var test = function() {
+    // Mouse events.
+    mockMouseType('l');
+    mockMouseType('p');
 
-  // Touch events.
-  mockTouchType('l');
-  mockTouchType('p');
+    // Touch events.
+    mockTouchType('l');
+    mockTouchType('p');
+  }
+  RunTest(test, testDoneCallback);
 }
