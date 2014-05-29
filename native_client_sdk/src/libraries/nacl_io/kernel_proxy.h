57cdcddc77e7fd9fff54d960280b4ebee2a18a73
binji@chromium.org <binji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-29 19:41:38 +0000

    [NaCl SDK] Create Filesystem nodes in /dev/fs/...
    These can be used to pass data to a filesystem. I'll use this in the next CL to
    provide a way to pass data to a JavaScript filesystem.
    
    BUG=none
    R=sbc@chromium.org
    
    Review URL: https://codereview.chromium.org/304013002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273552 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/libraries/nacl_io/kernel_proxy.h
+++ native_client_sdk/src/libraries/nacl_io/kernel_proxy.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <string>
 
+#include "nacl_io/devfs/dev_fs.h"
 #include "nacl_io/event_emitter.h"
 #include "nacl_io/fs_factory.h"
 #include "nacl_io/host_resolver.h"
@@ -207,8 +208,20 @@ class KernelProxy : protected KernelObject {
 #endif  // PROVIDES_SOCKET_API
 
  protected:
+  Error MountInternal(const char* source,
+                      const char* target,
+                      const char* filesystemtype,
+                      unsigned long mountflags,
+                      const void* data,
+                      bool create_fs_node,
+                      ScopedFilesystem* out_filesystem);
+
+  Error CreateFsNode(const ScopedFilesystem& fs);
+
+ protected:
   FsFactoryMap_t factories_;
-  sdk_util::ScopedRef<StreamFs> stream_mount_;
+  sdk_util::ScopedRef<StreamFs> stream_fs_;
+  sdk_util::ScopedRef<DevFs> dev_fs_;
   int dev_;
   PepperInterface* ppapi_;
   static KernelProxy *s_instance_;
