c74d87f5ad4ea9675e3b0a4d97ad6fd50ca6ed54
kouhei@chromium.org <kouhei@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-30 00:56:48 +0000

    Prepare SVGSMILElement for oilpan.
    In oilpan builds, |m_syncBaseDependents| and |m_syncBase| are changed to weak references from manual detach in d-tors.
    
    |m_syncBase| is changed from Element* -> SVGSMILElement* as its only valid value is SVGSMILElement*.
    
    BUG=357163
    
    Review URL: https://codereview.chromium.org/305783004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175088 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/animation/SVGSMILElement.cpp
+++ third_party/WebKit/Source/core/svg/animation/SVGSMILElement.cpp
@@ -206,7 +206,6 @@ SVGSMILElement::~SVGSMILElement()
     smilRepeatEventSender().cancelEvent(this);
     smilRepeatNEventSender().cancelEvent(this);
 #if !ENABLE(OILPAN)
-    // FIXME: Oilpan: Clear |m_syncBaseDependents| using weak processing.
     clearConditions();
 
     if (m_timeContainer && m_targetElement && hasValidAttributeName())
@@ -603,12 +602,14 @@ void SVGSMILElement::connectSyncBaseConditions()
         Condition* condition = m_conditions[n].get();
         if (condition->type() == Condition::Syncbase) {
             ASSERT(!condition->baseID().isEmpty());
-            condition->setSyncBase(treeScope().getElementById(AtomicString(condition->baseID())));
-            if (!condition->syncBase() || !isSVGSMILElement(*condition->syncBase())) {
+            Element* element = treeScope().getElementById(AtomicString(condition->baseID()));
+            if (!element || !isSVGSMILElement(*element)) {
                 condition->setSyncBase(0);
                 continue;
             }
-            toSVGSMILElement(condition->syncBase())->addSyncBaseDependent(this);
+            SVGSMILElement* svgSMILElement = toSVGSMILElement(element);
+            condition->setSyncBase(svgSMILElement);
+            svgSMILElement->addSyncBaseDependent(this);
         }
     }
 }
@@ -622,7 +623,7 @@ void SVGSMILElement::disconnectSyncBaseConditions()
         Condition* condition = m_conditions[n].get();
         if (condition->type() == Condition::Syncbase) {
             if (condition->syncBase())
-                toSVGSMILElement(condition->syncBase())->removeSyncBaseDependent(this);
+                condition->syncBase()->removeSyncBaseDependent(this);
             condition->setSyncBase(0);
         }
     }
@@ -1247,8 +1248,8 @@ bool SVGSMILElement::progress(SMILTime elapsed, SVGSMILElement* resultElement, b
 void SVGSMILElement::notifyDependentsIntervalChanged()
 {
     ASSERT(m_intervalBegin.isFinite());
-    DEFINE_STATIC_LOCAL(HashSet<SVGSMILElement*>, loopBreaker, ());
-    if (!loopBreaker.add(this).isNewEntry)
+    DEFINE_STATIC_LOCAL(OwnPtrWillBePersistent<WillBeHeapHashSet<RawPtrWillBeMember<SVGSMILElement> > >, loopBreaker, (adoptPtrWillBeNoop(new WillBeHeapHashSet<RawPtrWillBeMember<SVGSMILElement> >())));
+    if (!loopBreaker->add(this).isNewEntry)
         return;
 
     TimeDependentSet::iterator end = m_syncBaseDependents.end();
@@ -1257,7 +1258,7 @@ void SVGSMILElement::notifyDependentsIntervalChanged()
         dependent->createInstanceTimesFromSyncbase(this);
     }
 
-    loopBreaker.remove(this);
+    loopBreaker->remove(this);
 }
 
 void SVGSMILElement::createInstanceTimesFromSyncbase(SVGSMILElement* syncBase)
