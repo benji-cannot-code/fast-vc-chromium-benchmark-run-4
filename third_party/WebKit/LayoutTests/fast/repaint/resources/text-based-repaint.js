0b7dbf39f5e8ef28c3a024dd9c038784c4dbb102
wangxianzhu@chromium.org <wangxianzhu@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-30 22:52:36 +0000

    Increase repaint test delay when running in browser
    When running a repaint test in browser, sometimes we may want to see
    the page contents before and after repaintTest(). The origianl 100ms
    delay seems too short to show the page contents before repaintTest().
    
    This may be more useful when we have repaint overlay feature when
    showing layout test results (https://codereview.chromium.org/303223008/).
    
    Review URL: https://codereview.chromium.org/304273005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175165 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/repaint/resources/text-based-repaint.js
+++ third_party/WebKit/LayoutTests/fast/repaint/resources/text-based-repaint.js
@@ -7,7 +7,7 @@ window.outputRepaintRects = true;
 function runRepaintTest()
 {
     if (!window.testRunner || !window.internals) {
-        setTimeout(repaintTest, 100);
+        setTimeout(repaintTest, 500);
         return;
     }
 
