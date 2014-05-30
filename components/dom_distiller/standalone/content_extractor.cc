b118dec3c0e7a72a7c544e800a21cfaac477bbf0
yfriedman@chromium.org <yfriedman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-30 05:23:38 +0000

    Configure DomDistiller debug output.
    Passes the "v" level to DomDistiller for debug output. Now starting
    chrome with something like:
    out/Debug/chrome --enable-dom-distiller --vmodule=dom_distiller*=1
    will log the boilerpipe debug phases to the console.
    
    For content extractor (which doesn't use DomDistillerServiceFactory),
    adds a --debug-level flag.
    
    BUG=375443
    
    Review URL: https://codereview.chromium.org/302793002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273721 0039d316-1c4b-4281-b951-d872f2087c98

--- components/dom_distiller/standalone/content_extractor.cc
+++ components/dom_distiller/standalone/content_extractor.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_loop.h"
 #include "base/path_service.h"
 #include "base/run_loop.h"
+#include "base/strings/string_number_conversions.h"
 #include "components/dom_distiller/content/distiller_page_web_contents.h"
 #include "components/dom_distiller/core/distiller.h"
 #include "components/dom_distiller/core/dom_distiller_database.h"
@@ -45,7 +46,11 @@ const char* kOutputFile = "output-file";
 // output.
 const char* kShouldOutputBinary = "output-binary";
 
+// Indicates to output only the text of the article and not the enclosing html.
 const char* kExtractTextOnly = "extract-text-only";
+
+// Indicates to include debug output.
+const char* kDebugLevel = "debug-level";
 
 scoped_ptr<DomDistillerService> CreateDomDistillerService(
     content::BrowserContext* context,
@@ -69,6 +74,14 @@ scoped_ptr<DomDistillerService> CreateDomDistillerService(
   dom_distiller::proto::DomDistillerOptions options;
   if (base::CommandLine::ForCurrentProcess()->HasSwitch(kExtractTextOnly)) {
     options.set_extract_text_only(true);
+  }
+  int debug_level = 0;
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(kDebugLevel) &&
+      base::StringToInt(
+          base::CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
+              kDebugLevel),
+          &debug_level)) {
+    options.set_debug_level(debug_level);
   }
   scoped_ptr<DistillerFactory> distiller_factory(
       new DistillerFactoryImpl(distiller_url_fetcher_factory.Pass(), options));
