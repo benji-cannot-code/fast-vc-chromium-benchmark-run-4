6d74624fcea36b54fa06ea5fe7e6e42fdf1dd577
dmziegler@google.com <dmziegler@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-30 07:11:25 +0000

    ChannelIDSource looks up a channel ID key for a given hostname, and ChannelIDKey does the signing.
    Right now ChannelIDSource::GetChannelIDKey is still a synchronous function.
    It will become asynchronous in a future CL. The assumption is that channel
    ID keys may be stored on a disk, so the lookup may require disk access, but
    once a channel ID key is loaded into memory, it can sign data without
    blocking.
    
    Corresponds to internal CL 68051215.
    
    R=wtc@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/300223007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273748 0039d316-1c4b-4281-b951-d872f2087c98

--- net/quic/test_tools/crypto_test_utils.h
+++ net/quic/test_tools/crypto_test_utils.h
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace net {
 
-class ChannelIDSigner;
+class ChannelIDSource;
 class CommonCertSets;
 class ProofSource;
 class ProofVerifier;
@@ -49,8 +49,7 @@ class CryptoTestUtils {
     bool dont_verify_certs;
 
     // If channel_id_enabled is true then the client will attempt to send a
-    // ChannelID. The key will be the same as is returned by
-    // ChannelIDSigner's |GetKeyForHostname|.
+    // ChannelID.
     bool channel_id_enabled;
   };
 
@@ -134,9 +133,9 @@ class CryptoTestUtils {
   static CryptoHandshakeMessage BuildMessage(const char* message_tag,
                                              va_list ap);
 
-  // ChannelIDSignerForTesting returns a ChannelIDSigner that generates keys
-  // deterministically based on the hostname given in the Sign call.
-  static ChannelIDSigner* ChannelIDSignerForTesting();
+  // ChannelIDSourceForTesting returns a ChannelIDSource that generates keys
+  // deterministically based on the hostname given in the GetChannelID call.
+  static ChannelIDSource* ChannelIDSourceForTesting();
 
  private:
   static void CompareClientAndServerKeys(QuicCryptoClientStream* client,
