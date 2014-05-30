7c0f09cf2ff4b15fac59bd7026972e8edf8cc68e
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-30 23:20:13 +0000

    Skip child ShadowRoots when invalidation does not have boundary crossing rules
    When the only things that changed are not tree boundary crossing rules we
    can skip invalidation in descendant ShadowRoots. This means that
    if you have lots of nested components with duplicate class names then using
    a descendant selector in an enclosing component won't invalidate style in
    nested ShadowRoots.
    
    I also packed the booleans on DescendantInvalidationSet since we keep growing
    it with new flags.
    
    In the future we can make this tree boundary crossing logic even smarter so that
    if you have both tree boundary and not tree boundary crossing invalidation
    sets on the same subtree we can skip running the non-tree boundary crossing
    rules when recursing into a ShadowRoot.
    
    I also took this as an opportunity to clean up the addFeaturesToInvalidationSets
    logic and merged the new treeBoundaryCrossing flag and the wholeSubtreeInvalid
    flag into the InvalidationSetFeatures.
    
    Review URL: https://codereview.chromium.org/286903024
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175171 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/invalidation/DescendantInvalidationSet.cpp
+++ third_party/WebKit/Source/core/css/invalidation/DescendantInvalidationSet.cpp
@@ -40,6 +40,7 @@ namespace WebCore {
 DescendantInvalidationSet::DescendantInvalidationSet()
     : m_allDescendantsMightBeInvalid(false)
     , m_customPseudoInvalid(false)
+    , m_treeBoundaryCrossing(false)
 {
 }
 
@@ -85,6 +86,9 @@ void DescendantInvalidationSet::combine(const DescendantInvalidationSet& other)
 
     if (other.customPseudoInvalid())
         setCustomPseudoInvalid();
+
+    if (other.treeBoundaryCrossing())
+        setTreeBoundaryCrossing();
 
     if (other.m_classes) {
         WillBeHeapHashSet<AtomicString>::const_iterator end = other.m_classes->end();
@@ -173,6 +177,7 @@ void DescendantInvalidationSet::setWholeSubtreeInvalid()
         return;
 
     m_allDescendantsMightBeInvalid = true;
+    m_treeBoundaryCrossing = false;
     m_classes = nullptr;
     m_ids = nullptr;
     m_tagNames = nullptr;
