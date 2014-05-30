b226eaf5a25897cf808f1035437751bd95cd1975
jgraettinger@chromium.org <jgraettinger@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-30 00:23:40 +0000

    Defer SpdySession destruction to support closing writes
    Replace the STATE_CLOSED availability state with STATE_DRAINING, where:
     * The read-pump will not run, and no frames can be read.
     * No new frames may be queued in the sessions's write_queue.
     * However, the write-pump will continue to run.
    
    SpdySession's write-pump will destroy the session iff it's draining, the
    write queue is empty and no in-flight writes remain. As this (and
    ~SpdySessionPool()) are now the only locations where SpdySessions may be
    destroyed, session lifetime is more know-able and some WeakPtrs are
    removed.
    
    Some tests have been updated to include closing RST_STREAMS sent by the
    session. This reflects current behavior. A future CL will add GOAWAY
    frames upon session errors.
    
    BUG=375033
    
    Review URL: https://codereview.chromium.org/305823003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273680 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_session_pool.cc
+++ net/spdy/spdy_session_pool.cc
@@ -72,6 +72,12 @@ SpdySessionPool::SpdySessionPool(
 SpdySessionPool::~SpdySessionPool() {
   CloseAllSessions();
 
+  while (!sessions_.empty()) {
+    // Destroy sessions to enforce that lifetime is scoped to SpdySessionPool.
+    // Write callbacks queued upon session drain are not invoked.
+    RemoveUnavailableSession((*sessions_.begin())->GetWeakPtr());
+  }
+
   if (ssl_config_service_.get())
     ssl_config_service_->RemoveObserver(this);
   NetworkChangeNotifier::RemoveIPAddressObserver(this);
@@ -245,7 +251,7 @@ void SpdySessionPool::CloseCurrentIdleSessions() {
 }
 
 void SpdySessionPool::CloseAllSessions() {
-  while (!sessions_.empty()) {
+  while (!available_sessions_.empty()) {
     CloseCurrentSessionsHelper(ERR_ABORTED, "Closing all sessions.",
                                false /* idle_only */);
   }
@@ -281,7 +287,7 @@ void SpdySessionPool::OnIPAddressChanged() {
 #else
     (*it)->CloseSessionOnError(ERR_NETWORK_CHANGED,
                                "Closing current sessions.");
-    DCHECK(!*it);
+    DCHECK((*it)->IsDraining());
 #endif  // defined(OS_ANDROID) || defined(OS_WIN) || defined(OS_IOS)
     DCHECK(!IsSessionAvailable(*it));
   }
@@ -390,7 +396,6 @@ void SpdySessionPool::CloseCurrentSessionsHelper(
 
     (*it)->CloseSessionOnError(error, description);
     DCHECK(!IsSessionAvailable(*it));
-    DCHECK(!*it);
   }
 }
 
