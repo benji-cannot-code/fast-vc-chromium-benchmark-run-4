8d4271de63b0e0db91c74b7e1f35acd9122d9868
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-30 00:06:00 +0000

    Revert 273652 "Host extensions"
    gyp failure on Win:
    
    Exception: Missing input files:
    
    C:\b\build\slave\Win\build\src\remoting\host\extension.h
    
    C:\b\build\slave\Win\build\src\remoting\host\extension_session.h
    
    
    > Host extensions
    > 
    > This CL introduces HostExtension, an interface for classes that
    > extend the host with non-core functionality.
    > 
    > Extensions are added to the ChromotingHost. They are used to compile
    > the list of capabilities reported to the client, which can be used by
    > the client to determine the availability of the extension.
    > 
    > When a client connects, a HostExtension has the opportunity to create
    > an HostExtensionSession to hold client/extension state, and to handle
    > extension messages from that client.
    > 
    > BUG=
    > 
    > Review URL: https://codereview.chromium.org/301453003
    
    TBR=dcaiafa@chromium.org
    
    Review URL: https://codereview.chromium.org/308743006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273674 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/chromoting_host.h
+++ remoting/host/chromoting_host.h
@@ -9,16 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <list>
 #include <string>
 
-#include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
-#include "base/memory/scoped_vector.h"
+#include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
 #include "base/threading/non_thread_safe.h"
 #include "base/threading/thread.h"
 #include "net/base/backoff_entry.h"
 #include "remoting/host/client_session.h"
-#include "remoting/host/host_extension.h"
 #include "remoting/host/host_status_monitor.h"
 #include "remoting/host/host_status_observer.h"
 #include "remoting/protocol/authenticator.h"
@@ -94,9 +92,6 @@ class ChromotingHost : public base::NonThreadSafe,
   virtual void AddStatusObserver(HostStatusObserver* observer) OVERRIDE;
   virtual void RemoveStatusObserver(HostStatusObserver* observer) OVERRIDE;
 
-  // Registers a host extension.
-  void AddExtension(scoped_ptr<HostExtension> extension);
-
   // This method may be called only from
   // HostStatusObserver::OnClientAuthenticated() to reject the new
   // client.
@@ -124,7 +119,6 @@ class ChromotingHost : public base::NonThreadSafe,
   virtual void OnSessionAuthenticating(ClientSession* client) OVERRIDE;
   virtual bool OnSessionAuthenticated(ClientSession* client) OVERRIDE;
   virtual void OnSessionChannelsConnected(ClientSession* client) OVERRIDE;
-  virtual void OnSessionClientCapabilities(ClientSession* client) OVERRIDE;
   virtual void OnSessionAuthenticationFailed(ClientSession* client) OVERRIDE;
   virtual void OnSessionClosed(ClientSession* session) OVERRIDE;
   virtual void OnSessionSequenceNumber(ClientSession* session,
@@ -161,7 +155,6 @@ class ChromotingHost : public base::NonThreadSafe,
   friend class ChromotingHostTest;
 
   typedef std::list<ClientSession*> ClientList;
-  typedef ScopedVector<HostExtension> HostExtensionList;
 
   // Immediately disconnects all active clients. Host-internal components may
   // shutdown asynchronously, but the caller is guaranteed not to receive
@@ -211,9 +204,6 @@ class ChromotingHost : public base::NonThreadSafe,
 
   // The pairing registry for PIN-less authentication.
   scoped_refptr<protocol::PairingRegistry> pairing_registry_;
-
-  // List of host extensions.
-  HostExtensionList extensions_;
 
   base::WeakPtrFactory<ChromotingHost> weak_factory_;
 
