0b489f307d4c7f18ec1f42d948be7f8aa4103e95
zard17@gmail.com <zard17@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-30 01:16:41 +0000

    Use base::STLSet* in place of std::set_* for simplicity.
    BUG=254066
    
    Review URL: https://codereview.chromium.org/301963002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273689 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/base/capabilities.cc
+++ remoting/base/capabilities.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <vector>
 
+#include "base/stl_util.h"
 #include "base/strings/string_util.h"
 
 namespace remoting {
@@ -28,16 +29,9 @@ std::string IntersectCapabilities(const std::string& client_capabilities,
   Tokenize(host_capabilities, " ", &host_caps);
   std::sort(host_caps.begin(), host_caps.end());
 
-  std::vector<std::string> result(std::min(client_caps.size(),
-                                           host_caps.size()));
-  std::vector<std::string>::iterator end =
-      std::set_intersection(client_caps.begin(),
-                            client_caps.end(),
-                            host_caps.begin(),
-                            host_caps.end(),
-                            result.begin());
-  if (end != result.end())
-    result.erase(end, result.end());
+  std::vector<std::string> result =
+      base::STLSetIntersection<std::vector<std::string> >(
+          client_caps, host_caps);
 
   return JoinString(result, " ");
 }
