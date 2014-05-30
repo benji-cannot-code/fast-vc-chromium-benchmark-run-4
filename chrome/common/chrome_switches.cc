ebc7876e8be675e8077000acaff0640b78b97d68
jeremycho@chromium.org <jeremycho@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-30 08:26:05 +0000

    Make the contextual search flag tri-state.  This allows users to opt-out even if they're in the Finch trial.
    BUG=378999
    
    Review URL: https://codereview.chromium.org/304253005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273772 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_switches.cc
+++ chrome/common/chrome_switches.cc
@@ -1265,6 +1265,9 @@ const char kPluginsMetadataServerURL[]      = "plugins-metadata-server-url";
 // Disables support for playing videos on Chromecast devices.
 const char kDisableCast[]                    = "disable-cast";
 
+// Disables Contextual Search.
+const char kDisableContextualSearch[]        = "disable-contextual-search";
+
 // Disables the new NTP.
 const char kDisableNewNTP[]                  = "disable-new-ntp";
 
@@ -1278,8 +1281,8 @@ const char kEnableAccessibilityTabSwitcher[] =
 // Enables app install alerts.
 const char kEnableAppInstallAlerts[]        = "enable-app-install-alerts";
 
-// Enables Contextual Search
-const char kEnableContextualSearch[]        = "contextual-search";
+// Enables Contextual Search.
+const char kEnableContextualSearch[]        = "enable-contextual-search";
 
 // Enables the new NTP.
 const char kEnableNewNTP[]                  = "enable-new-ntp";
