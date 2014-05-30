0c3b398fe25f93ffc98d7a551554ede753c6b7d6
kihong.kwon@samsung.com <kihong.kwon@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-30 14:12:35 +0000

    Set default values for timeout and maximumAge of PositionOptions
    Geolocation spec defines default values for both timeout and 
    maximumAge but current implementation is working with
    hasTimeout and hasMaximumAge functions for emulating that.
    
    Setting initial values of timeout and maximumAge is more proper than using these "has" functions.
    In addition, hasZeroTimeout can be removed because it is
    enough to check timeout is zero or not and clearMaximumAge
    can also be removed, as it is enough to set 0 to maximumAge.
    
    BUG=368184
    
    Review URL: https://codereview.chromium.org/297143003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175131 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/geolocation/Geolocation.cpp
+++ third_party/WebKit/Source/modules/geolocation/Geolocation.cpp
@@ -120,11 +120,6 @@ void Geolocation::GeoNotifier::setUseCachedPosition()
     m_timer.startOneShot(0, FROM_HERE);
 }
 
-bool Geolocation::GeoNotifier::hasZeroTimeout() const
-{
-    return m_options->hasTimeout() && m_options->timeout() == 0;
-}
-
 void Geolocation::GeoNotifier::runSuccessCallback(Geoposition* position)
 {
     // If we are here and the Geolocation permission is not approved, something has
@@ -141,10 +136,9 @@ void Geolocation::GeoNotifier::runErrorCallback(PositionError* error)
         m_errorCallback->handleEvent(error);
 }
 
-void Geolocation::GeoNotifier::startTimerIfNeeded()
+void Geolocation::GeoNotifier::startTimer()
 {
-    if (m_options->hasTimeout())
-        m_timer.startOneShot(m_options->timeout() / 1000.0, FROM_HERE);
+    m_timer.startOneShot(m_options->timeout() / 1000.0, FROM_HERE);
 }
 
 void Geolocation::GeoNotifier::stopTimer()
@@ -351,14 +345,14 @@ void Geolocation::startRequest(GeoNotifier *notifier)
         notifier->setFatalError(PositionError::create(PositionError::PERMISSION_DENIED, permissionDeniedErrorMessage));
     else if (haveSuitableCachedPosition(notifier->options()))
         notifier->setUseCachedPosition();
-    else if (notifier->hasZeroTimeout())
-        notifier->startTimerIfNeeded();
+    else if (!notifier->options()->timeout())
+        notifier->startTimer();
     else if (!isAllowed()) {
         // if we don't yet have permission, request for permission before calling startUpdating()
         m_pendingForPermissionNotifiers.add(notifier);
         requestPermission();
     } else if (startUpdating(notifier))
-        notifier->startTimerIfNeeded();
+        notifier->startTimer();
     else
         notifier->setFatalError(PositionError::create(PositionError::POSITION_UNAVAILABLE, failedToStartServiceErrorMessage));
 }
@@ -409,8 +403,8 @@ void Geolocation::makeCachedPositionCallbacks()
         if (m_oneShots.contains(notifier))
             m_oneShots.remove(notifier);
         else if (m_watchers.contains(notifier)) {
-            if (notifier->hasZeroTimeout() || startUpdating(notifier))
-                notifier->startTimerIfNeeded();
+            if (!notifier->options()->timeout() || startUpdating(notifier))
+                notifier->startTimer();
             else
                 notifier->setFatalError(PositionError::create(PositionError::POSITION_UNAVAILABLE, failedToStartServiceErrorMessage));
         }
@@ -436,8 +430,6 @@ bool Geolocation::haveSuitableCachedPosition(PositionOptions* options)
     Geoposition* cachedPosition = lastPosition();
     if (!cachedPosition)
         return false;
-    if (!options->hasMaximumAge())
-        return true;
     if (!options->maximumAge())
         return false;
     DOMTimeStamp currentTimeMillis = convertSecondsToDOMTimeStamp(currentTime());
@@ -699,7 +691,7 @@ void Geolocation::handlePendingPermissionNotifiers()
             // start all pending notification requests as permission granted.
             // The notifier is always ref'ed by m_oneShots or m_watchers.
             if (startUpdating(notifier))
-                notifier->startTimerIfNeeded();
+                notifier->startTimer();
             else
                 notifier->setFatalError(PositionError::create(PositionError::POSITION_UNAVAILABLE, failedToStartServiceErrorMessage));
         } else {
