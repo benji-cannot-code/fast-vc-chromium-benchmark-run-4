bea0770a46c9f071fdde4ae0321159e11bd8c75b
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-30 23:37:15 +0000

    Mojo: Specify/check alignment of pointers more carefully.
    Improve the verification routines especially in the case that no count
    is necessary.
    
    Also add a test.
    
    R=darin@chromium.org
    
    Review URL: https://codereview.chromium.org/304303006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273936 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/system/data_pipe_producer_dispatcher.cc
+++ mojo/system/data_pipe_producer_dispatcher.cc
@@ -57,9 +57,9 @@ MojoResult DataPipeProducerDispatcher::WriteDataImplNoLock(
     MojoWriteDataFlags flags) {
   lock().AssertAcquired();
 
-  if (!VerifyUserPointer<uint32_t>(num_bytes, 1))
+  if (!VerifyUserPointer<uint32_t>(num_bytes))
     return MOJO_RESULT_INVALID_ARGUMENT;
-  if (!VerifyUserPointer<void>(elements, *num_bytes))
+  if (!VerifyUserPointerWithSize<1>(elements, *num_bytes))
     return MOJO_RESULT_INVALID_ARGUMENT;
 
   return data_pipe_->ProducerWriteData(
@@ -72,9 +72,9 @@ MojoResult DataPipeProducerDispatcher::BeginWriteDataImplNoLock(
     MojoWriteDataFlags flags) {
   lock().AssertAcquired();
 
-  if (!VerifyUserPointer<void*>(buffer, 1))
+  if (!VerifyUserPointerWithCount<void*>(buffer, 1))
     return MOJO_RESULT_INVALID_ARGUMENT;
-  if (!VerifyUserPointer<uint32_t>(buffer_num_bytes, 1))
+  if (!VerifyUserPointer<uint32_t>(buffer_num_bytes))
     return MOJO_RESULT_INVALID_ARGUMENT;
 
   return data_pipe_->ProducerBeginWriteData(
