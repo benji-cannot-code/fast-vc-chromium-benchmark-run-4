e7b5556a5a4d8bf77ca898688df76fa6b081ec22
zmo@chromium.org <zmo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-30 00:13:23 +0000

    Workaround VAO related driver bug.
    If a VAO is bound, glVertexAttribPointer will incorrectly generate an
    GL_INVALID_OPERATION if no ARRAY_BUFFER is bound and pointer is NULL.
    
    BUG=373974
    TEST=gpu_unittests, webgl conformance tests on AMD
    R=kbr@chromium.org, piman@chromium.org, bajones@chromium.org, vmiura@chromium.org
    
    Review URL: https://codereview.chromium.org/293033014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273678 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/gles2_cmd_decoder_unittest_base.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder_unittest_base.cc
@@ -28,6 +28,8 @@ using ::gfx::MockGLInterface;
 using ::testing::_;
 using ::testing::DoAll;
 using ::testing::InSequence;
+using ::testing::Invoke;
+using ::testing::InvokeWithoutArgs;
 using ::testing::MatcherCast;
 using ::testing::Pointee;
 using ::testing::Return;
@@ -36,6 +38,7 @@ using ::testing::SetArgPointee;
 using ::testing::SetArgumentPointee;
 using ::testing::StrEq;
 using ::testing::StrictMock;
+using ::testing::WithArg;
 
 namespace {
 
@@ -153,6 +156,8 @@ void GLES2DecoderTestBase::InitDecoderWithCommandLine(
 
   gl_.reset(new StrictMock<MockGLInterface>());
   ::gfx::MockGLInterface::SetGLInterface(gl_.get());
+
+  SetupMockGLBehaviors();
 
   // Only create stream texture manager if extension is requested.
   std::vector<std::string> list;
@@ -1534,12 +1539,6 @@ void GLES2DecoderTestBase::AddExpectationsForSimulatedAttrib0WithError(
     EXPECT_CALL(*gl_, VertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, NULL))
         .Times(1)
         .RetiresOnSaturation();
-    EXPECT_CALL(*gl_, BindBuffer(GL_ARRAY_BUFFER, 0))
-        .Times(1)
-        .RetiresOnSaturation();
-    EXPECT_CALL(*gl_, VertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, NULL))
-        .Times(1)
-        .RetiresOnSaturation();
     EXPECT_CALL(*gl_, BindBuffer(GL_ARRAY_BUFFER, buffer_id))
         .Times(1)
         .RetiresOnSaturation();
@@ -1550,6 +1549,21 @@ void GLES2DecoderTestBase::AddExpectationsForSimulatedAttrib0(
     GLsizei num_vertices, GLuint buffer_id) {
   AddExpectationsForSimulatedAttrib0WithError(
       num_vertices, buffer_id, GL_NO_ERROR);
+}
+
+void GLES2DecoderTestBase::SetupMockGLBehaviors() {
+  ON_CALL(*gl_, BindVertexArrayOES(_))
+      .WillByDefault(Invoke(
+          &gl_states_,
+          &GLES2DecoderTestBase::MockGLStates::OnBindVertexArrayOES));
+  ON_CALL(*gl_, BindBuffer(GL_ARRAY_BUFFER, _))
+      .WillByDefault(WithArg<1>(Invoke(
+          &gl_states_,
+          &GLES2DecoderTestBase::MockGLStates::OnBindArrayBuffer)));
+  ON_CALL(*gl_, VertexAttribPointer(_, _, _, _, _, NULL))
+      .WillByDefault(InvokeWithoutArgs(
+          &gl_states_,
+          &GLES2DecoderTestBase::MockGLStates::OnVertexAttribNullPointer));
 }
 
 GLES2DecoderWithShaderTestBase::MockCommandBufferEngine::
