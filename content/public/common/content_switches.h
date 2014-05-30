6727d5f3eb8e7c099a712d891205796fb10b3458
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-30 10:33:36 +0000

    Remove --enable-webrtc-tcp-server-socket
    This switch is not used. Remove it.
    
    Review URL: https://codereview.chromium.org/302633012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273790 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/common/content_switches.h
+++ content/public/common/content_switches.h
@@ -266,7 +266,6 @@ CONTENT_EXPORT extern const char kDisableDeviceEnumeration[];
 CONTENT_EXPORT extern const char kDisableWebRtcHWDecoding[];
 CONTENT_EXPORT extern const char kDisableWebRtcEncryption[];
 CONTENT_EXPORT extern const char kDisableWebRtcHWEncoding[];
-extern const char kEnableWebRtcTcpServerSocket[];
 CONTENT_EXPORT extern const char kEnableWebRtcHWVp8Encoding[];
 #endif
 
