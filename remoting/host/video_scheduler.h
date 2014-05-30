f4ca4bd006dc48f59af7090eee4e8a757923c311
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-30 07:21:20 +0000

    Extend VideoControl to allow clients to request lossless modes.
    - Lossless encode/color requests may be ignored by codecs.
    - VideoEncoderVpx supports lossless color when using VP9.
    - The --enable-i444 flag now controls the default color mode for VP9.
    
    BUG=260879,134202
    
    Review URL: https://codereview.chromium.org/304653002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273752 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/video_scheduler.h
+++ remoting/host/video_scheduler.h
@@ -113,6 +113,11 @@ class VideoScheduler : public base::RefCountedThreadSafe<VideoScheduler>,
   // Sequence numbers are used for performance measurements.
   void UpdateSequenceNumber(int64 sequence_number);
 
+  // Sets whether the video encoder should be requested to encode losslessly,
+  // or to use a lossless color space (typically requiring higher bandwidth).
+  void SetLosslessEncode(bool want_lossless);
+  void SetLosslessColor(bool want_lossless);
+
  private:
   friend class base::RefCountedThreadSafe<VideoScheduler>;
   virtual ~VideoScheduler();
@@ -198,7 +203,7 @@ class VideoScheduler : public base::RefCountedThreadSafe<VideoScheduler>,
   // True if capture of video frames is paused.
   bool is_paused_;
 
-  // This is a number updated by client to trace performance.
+  // Number updated by the caller to trace performance.
   int64 sequence_number_;
 
   // An object to schedule capturing.
