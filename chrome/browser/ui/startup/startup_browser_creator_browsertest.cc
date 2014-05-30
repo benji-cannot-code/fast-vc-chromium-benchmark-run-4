6b2fd8bfc267e006076466cd4838e1a2c64181e7
marja@chromium.org <marja@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-30 13:56:46 +0000

    Tentatively enable StartupBrowserCreatorFirstRunTest.* on official Mac builder.
    I recently fixed some places which passed empty URLs around (r272182), and these
    tests might have been failing because of the empty URL behavior (empty URLs were
    treated as the home page, and maybe the home page is something different on the
    official Mac builder than other builders).
    
    Feel free to revert this commit if the tests fail.
    
    R=bartfab@chromium.org
    TBR=sky@chromium.org
    BUG=314819
    
    Review URL: https://codereview.chromium.org/308083004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273825 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/startup/startup_browser_creator_browsertest.cc
+++ chrome/browser/ui/startup/startup_browser_creator_browsertest.cc
@@ -1078,14 +1078,7 @@ void StartupBrowserCreatorFirstRunTest::SetUpInProcessBrowserTestFixture() {
 #endif  // defined(ENABLE_CONFIGURATION_POLICY)
 }
 
-#if defined(GOOGLE_CHROME_BUILD) && defined(OS_MACOSX)
-// http://crbug.com/314819
-#define MAYBE_SyncPromoForbidden DISABLED_SyncPromoForbidden
-#else
-#define MAYBE_SyncPromoForbidden SyncPromoForbidden
-#endif
-IN_PROC_BROWSER_TEST_F(StartupBrowserCreatorFirstRunTest,
-                       MAYBE_SyncPromoForbidden) {
+IN_PROC_BROWSER_TEST_F(StartupBrowserCreatorFirstRunTest, SyncPromoForbidden) {
   // Consistently enable the welcome page on all platforms.
   first_run::SetShouldShowWelcomePage();
 
@@ -1119,14 +1112,7 @@ IN_PROC_BROWSER_TEST_F(StartupBrowserCreatorFirstRunTest,
             tab_strip->GetWebContentsAt(1)->GetURL());
 }
 
-#if defined(GOOGLE_CHROME_BUILD) && defined(OS_MACOSX)
-// http://crbug.com/314819
-#define MAYBE_SyncPromoAllowed DISABLED_SyncPromoAllowed
-#else
-#define MAYBE_SyncPromoAllowed SyncPromoAllowed
-#endif
-IN_PROC_BROWSER_TEST_F(StartupBrowserCreatorFirstRunTest,
-                       MAYBE_SyncPromoAllowed) {
+IN_PROC_BROWSER_TEST_F(StartupBrowserCreatorFirstRunTest, SyncPromoAllowed) {
   // Consistently enable the welcome page on all platforms.
   first_run::SetShouldShowWelcomePage();
 
@@ -1160,14 +1146,8 @@ IN_PROC_BROWSER_TEST_F(StartupBrowserCreatorFirstRunTest,
             tab_strip->GetWebContentsAt(1)->GetURL());
 }
 
-#if defined(GOOGLE_CHROME_BUILD) && defined(OS_MACOSX)
-// http://crbug.com/314819
-#define MAYBE_FirstRunTabsPromoAllowed DISABLED_FirstRunTabsPromoAllowed
-#else
-#define MAYBE_FirstRunTabsPromoAllowed FirstRunTabsPromoAllowed
-#endif
 IN_PROC_BROWSER_TEST_F(StartupBrowserCreatorFirstRunTest,
-                       MAYBE_FirstRunTabsPromoAllowed) {
+                       FirstRunTabsPromoAllowed) {
   // Simulate the following master_preferences:
   // {
   //  "first_run_tabs" : [
@@ -1202,15 +1182,8 @@ IN_PROC_BROWSER_TEST_F(StartupBrowserCreatorFirstRunTest,
             tab_strip->GetWebContentsAt(1)->GetURL().ExtractFileName());
 }
 
-#if defined(GOOGLE_CHROME_BUILD) && defined(OS_MACOSX)
-// http://crbug.com/314819
-#define MAYBE_FirstRunTabsContainSyncPromo \
-    DISABLED_FirstRunTabsContainSyncPromo
-#else
-#define MAYBE_FirstRunTabsContainSyncPromo FirstRunTabsContainSyncPromo
-#endif
 IN_PROC_BROWSER_TEST_F(StartupBrowserCreatorFirstRunTest,
-                       MAYBE_FirstRunTabsContainSyncPromo) {
+                       FirstRunTabsContainSyncPromo) {
   // Simulate the following master_preferences:
   // {
   //  "first_run_tabs" : [
@@ -1250,16 +1223,8 @@ IN_PROC_BROWSER_TEST_F(StartupBrowserCreatorFirstRunTest,
             tab_strip->GetWebContentsAt(1)->GetURL());
 }
 
-#if defined(GOOGLE_CHROME_BUILD) && defined(OS_MACOSX)
-// http://crbug.com/314819
-#define MAYBE_FirstRunTabsContainNTPSyncPromoAllowed \
-    DISABLED_FirstRunTabsContainNTPSyncPromoAllowed
-#else
-#define MAYBE_FirstRunTabsContainNTPSyncPromoAllowed \
-    FirstRunTabsContainNTPSyncPromoAllowed
-#endif
 IN_PROC_BROWSER_TEST_F(StartupBrowserCreatorFirstRunTest,
-                       MAYBE_FirstRunTabsContainNTPSyncPromoAllowed) {
+                       FirstRunTabsContainNTPSyncPromoAllowed) {
   // Simulate the following master_preferences:
   // {
   //  "first_run_tabs" : [
@@ -1297,16 +1262,8 @@ IN_PROC_BROWSER_TEST_F(StartupBrowserCreatorFirstRunTest,
             tab_strip->GetWebContentsAt(1)->GetURL().ExtractFileName());
 }
 
-#if defined(GOOGLE_CHROME_BUILD) && defined(OS_MACOSX)
-// http://crbug.com/314819
-#define MAYBE_FirstRunTabsContainNTPSyncPromoForbidden \
-    DISABLED_FirstRunTabsContainNTPSyncPromoForbidden
-#else
-#define MAYBE_FirstRunTabsContainNTPSyncPromoForbidden \
-    FirstRunTabsContainNTPSyncPromoForbidden
-#endif
 IN_PROC_BROWSER_TEST_F(StartupBrowserCreatorFirstRunTest,
-                       MAYBE_FirstRunTabsContainNTPSyncPromoForbidden) {
+                       FirstRunTabsContainNTPSyncPromoForbidden) {
   // Simulate the following master_preferences:
   // {
   //  "first_run_tabs" : [
@@ -1344,15 +1301,8 @@ IN_PROC_BROWSER_TEST_F(StartupBrowserCreatorFirstRunTest,
             tab_strip->GetWebContentsAt(1)->GetURL().ExtractFileName());
 }
 
-#if defined(GOOGLE_CHROME_BUILD) && defined(OS_MACOSX)
-// http://crbug.com/314819
-#define MAYBE_FirstRunTabsSyncPromoForbidden \
-    DISABLED_FirstRunTabsSyncPromoForbidden
-#else
-#define MAYBE_FirstRunTabsSyncPromoForbidden FirstRunTabsSyncPromoForbidden
-#endif
 IN_PROC_BROWSER_TEST_F(StartupBrowserCreatorFirstRunTest,
-                       MAYBE_FirstRunTabsSyncPromoForbidden) {
+                       FirstRunTabsSyncPromoForbidden) {
   // Simulate the following master_preferences:
   // {
   //  "first_run_tabs" : [
@@ -1386,16 +1336,8 @@ IN_PROC_BROWSER_TEST_F(StartupBrowserCreatorFirstRunTest,
 }
 
 #if defined(ENABLE_CONFIGURATION_POLICY)
-#if defined(GOOGLE_CHROME_BUILD) && defined(OS_MACOSX)
-// http://crbug.com/314819
-#define MAYBE_RestoreOnStartupURLsPolicySpecified \
-    DISABLED_RestoreOnStartupURLsPolicySpecified
-#else
-#define MAYBE_RestoreOnStartupURLsPolicySpecified \
-    RestoreOnStartupURLsPolicySpecified
-#endif
 IN_PROC_BROWSER_TEST_F(StartupBrowserCreatorFirstRunTest,
-                       MAYBE_RestoreOnStartupURLsPolicySpecified) {
+                       RestoreOnStartupURLsPolicySpecified) {
   // Simulate the following master_preferences:
   // {
   //  "sync_promo": {
