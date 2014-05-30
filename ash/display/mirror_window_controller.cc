ae0fff8d3a603d2b6296b15cc9d3ee61f71953dc
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-30 23:15:30 +0000

    Removes usage of Compositor/ContextFactory methods
    ContextFactory::Set/GetInstance is going away and not necessary
    anymore.
    
    BUG=none
    TEST=none
    R=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/306123007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273926 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/display/mirror_window_controller.cc
+++ ash/display/mirror_window_controller.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "base/strings/stringprintf.h"
 #include "ui/aura/client/capture_client.h"
+#include "ui/aura/env.h"
 #include "ui/aura/window_delegate.h"
 #include "ui/aura/window_event_dispatcher.h"
 #include "ui/aura/window_tree_host.h"
@@ -110,7 +111,7 @@ void MirrorWindowController::UpdateWindow(const DisplayInfo& display_info) {
     host->window()->AddChild(mirror_window);
     mirror_window->SetBounds(host->window()->bounds());
     mirror_window->Show();
-    reflector_ = ui::ContextFactory::GetInstance()->CreateReflector(
+    reflector_ = aura::Env::GetInstance()->context_factory()->CreateReflector(
         Shell::GetPrimaryRootWindow()->GetHost()->compositor(),
         mirror_window->layer());
   } else {
@@ -141,7 +142,7 @@ void MirrorWindowController::UpdateWindow() {
 void MirrorWindowController::Close() {
   if (ash_host_.get()) {
     aura::WindowTreeHost* host = ash_host_->AsWindowTreeHost();
-    ui::ContextFactory::GetInstance()->RemoveReflector(reflector_);
+    aura::Env::GetInstance()->context_factory()->RemoveReflector(reflector_);
     reflector_ = NULL;
     NoneCaptureClient* capture_client = static_cast<NoneCaptureClient*>(
         aura::client::GetCaptureClient(host->window()));
