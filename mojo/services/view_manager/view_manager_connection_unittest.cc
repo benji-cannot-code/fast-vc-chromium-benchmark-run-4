c1b1523b6221b3358d14ed3570182bee7cfc1e35
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-31 04:23:57 +0000

    Fixes bug where IViewManagerClient could be messaged unnecessarily
    When deleting a node view manager could end up messaging client both
    of the delete and to advance. If delete is sent there is no need to
    send an advance too.
    
    BUG=365012
    TEST=covered by tests
    R=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/307003004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274013 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/services/view_manager/view_manager_connection_unittest.cc
+++ mojo/services/view_manager/view_manager_connection_unittest.cc
@@ -901,12 +901,10 @@ TEST_F(ViewManagerConnectionTest, DeleteNode) {
     ASSERT_TRUE(connection_->DeleteNode(BuildNodeId(1, 2)));
     EXPECT_TRUE(connection_->changes().empty());
 
-    // TODO(sky): fix this, client should not get ServerChangeIdAdvanced.
-    connection2_->DoRunLoopUntilChangesCount(2);
+    connection2_->DoRunLoopUntilChangesCount(1);
     const Changes changes(ChangesToDescription1(connection2_->changes()));
-    ASSERT_EQ(2u, changes.size());
+    ASSERT_EQ(1u, changes.size());
     EXPECT_EQ("NodeDeleted change_id=2 node=1,2", changes[0]);
-    EXPECT_EQ("ServerChangeIdAdvanced 3", changes[1]);
   }
 }
 
@@ -946,12 +944,10 @@ TEST_F(ViewManagerConnectionTest, ReuseDeletedNodeId) {
   {
     ASSERT_TRUE(connection_->DeleteNode(BuildNodeId(1, 2)));
 
-    // TODO(sky): fix this, shouldn't get ServerChangeIdAdvanced.
-    connection2_->DoRunLoopUntilChangesCount(2);
+    connection2_->DoRunLoopUntilChangesCount(1);
     const Changes changes(ChangesToDescription1(connection2_->changes()));
-    ASSERT_EQ(2u, changes.size());
+    ASSERT_EQ(1u, changes.size());
     EXPECT_EQ("NodeDeleted change_id=2 node=1,2", changes[0]);
-    EXPECT_EQ("ServerChangeIdAdvanced 3", changes[1]);
   }
 
   // Create 2 again, and add it back to 1. Should get the same notification.
@@ -1048,12 +1044,10 @@ TEST_F(ViewManagerConnectionTest, DeleteNodeWithView) {
   {
     ASSERT_TRUE(connection_->DeleteNode(BuildNodeId(1, 3)));
 
-    // TODO(sky): shouldn't get ServerChangeIdAdvanced here.
-    connection2_->DoRunLoopUntilChangesCount(2);
+    connection2_->DoRunLoopUntilChangesCount(1);
     const Changes changes(ChangesToDescription1(connection2_->changes()));
-    ASSERT_EQ(2u, changes.size());
+    ASSERT_EQ(1u, changes.size());
     EXPECT_EQ("NodeDeleted change_id=3 node=1,3", changes[0]);
-    EXPECT_EQ("ServerChangeIdAdvanced 4", changes[1]);
   }
 }
 
