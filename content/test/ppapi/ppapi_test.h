d0516763a84d66305177e22bd61aa4f6dbc10d8c
teravest@chromium.org <teravest@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-31 01:12:55 +0000

    Pepper: Move more stuff to content_browsertests.
    This moves additional tests to content_browsertests. I keep these migrations
    relatively small so I don't have to debug much if something fails.
    
    Additionally, this change adds test coverage for NaCl for MessageLoop testing,
    and restores testing for MessageLoop_Post.
    
    BUG=371873
    
    Review URL: https://codereview.chromium.org/303313002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273974 0039d316-1c4b-4281-b951-d872f2087c98

--- content/test/ppapi/ppapi_test.h
+++ content/test/ppapi/ppapi_test.h
@@ -56,6 +56,10 @@ class PPAPITestBase : public ContentBrowserTest {
   GURL GetTestFileUrl(const std::string& test_case);
   virtual void RunTest(const std::string& test_case);
 
+  // Run the test and reload. This can test for clean shutdown, including
+  // leaked instance object vars.
+  virtual void RunTestAndReload(const std::string& test_case);
+
  protected:
   // Runs the test for a tab given the tab that's already navigated to the
   // given URL.
