5121a0e9b5bc735d2c8822f7ed8d73763755d95f
jiayl@chromium.org <jiayl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-31 01:32:29 +0000

    Implements RTP header dumping.
    WebRtcRtpDumpHandler implements RTP header dump creation. It's owned by WebRtcLoggingHandlerHost and receives RTP packet callbacks through P2PSocketHost.
    
    WebRtcRtpDumpWriter is owned by WebRtcDumpHandler and writes the RTP header into a memory buffer. When the in-memory buffer is full, it compresses the data and writes it to the disk.
    
    After the dumping is stopped, WebRtcLoggingHandlerHost calls ReleaseDump to get the completed dump as a .gz file and uploads it.
    
    
    BUG=363459
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=273745
    
    Review URL: https://codereview.chromium.org/264793017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273980 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/p2p/socket_dispatcher_host.h
+++ content/browser/renderer_host/p2p/socket_dispatcher_host.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/p2p_socket_type.h"
 #include "content/public/browser/browser_message_filter.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/render_process_host.h"
 #include "net/base/ip_endpoint.h"
 #include "net/base/network_change_notifier.h"
 
@@ -45,6 +46,15 @@ class P2PSocketDispatcherHost
 
   // net::NetworkChangeNotifier::IPAddressObserver interface.
   virtual void OnIPAddressChanged() OVERRIDE;
+
+  // Starts the RTP packet header dumping. Must be called on the IO thread.
+  void StartRtpDump(
+      bool incoming,
+      bool outgoing,
+      const RenderProcessHost::WebRtcRtpPacketCallback& packet_callback);
+
+  // Stops the RTP packet header dumping. Must be Called on the UI thread.
+  void StopRtpDumpOnUIThread(bool incoming, bool outgoing);
 
  protected:
   virtual ~P2PSocketDispatcherHost();
@@ -86,6 +96,8 @@ class P2PSocketDispatcherHost
   void OnAddressResolved(DnsRequest* request,
                          const net::IPAddressList& addresses);
 
+  void StopRtpDumpOnIOThread(bool incoming, bool outgoing);
+
   content::ResourceContext* resource_context_;
   scoped_refptr<net::URLRequestContextGetter> url_context_;
 
@@ -95,6 +107,10 @@ class P2PSocketDispatcherHost
 
   std::set<DnsRequest*> dns_requests_;
   P2PMessageThrottler throttler_;
+
+  bool dump_incoming_rtp_packet_;
+  bool dump_outgoing_rtp_packet_;
+  RenderProcessHost::WebRtcRtpPacketCallback packet_callback_;
 
   DISALLOW_COPY_AND_ASSIGN(P2PSocketDispatcherHost);
 };
