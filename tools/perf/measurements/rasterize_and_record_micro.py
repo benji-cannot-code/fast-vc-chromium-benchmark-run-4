1f79ca7c18bff65410ca60eb9f8b8f36950515df
skyostil@chromium.org <skyostil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-31 01:39:27 +0000

    Reuse same browser for all pages in rasterize_and_record_micro
    Don't restart the browser between each tested page in
    rasterize_and_record_micro. This improves stability and -- somewhat
    surprisingly -- reduces noise in the results. As an added bonus the
    rasterize_and_record_micro.key_silk_cases test completes in about 9 min
    compared to an earlier 16 min.
    
    To measure the impact of the change I ran 18 rasterize_and_record_micro
    iterations for 19 pages in key_silk_cases on a Nexus 5 (strapped to a
    water bottle for cooling) and logged the per-page record_time results
    for each run. The results were:
    
    With browser restarts (baseline):
    - Average record time: 1.78 ms
    - Average record time standard deviation: 0.0978
    
    Without browser restarts (patch):
    - Average record time: 1.63 ms
    - Average record time standard deviation: 0.0379
    
    BUG=355952
    
    Review URL: https://codereview.chromium.org/306923007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273984 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/measurements/rasterize_and_record_micro.py
+++ tools/perf/measurements/rasterize_and_record_micro.py
@@ -12,7 +12,7 @@ from telemetry.page import page_test
 
 class RasterizeAndRecordMicro(page_measurement.PageMeasurement):
   def __init__(self):
-    super(RasterizeAndRecordMicro, self).__init__('', True)
+    super(RasterizeAndRecordMicro, self).__init__('')
     self._chrome_branch_number = None
 
   @classmethod
