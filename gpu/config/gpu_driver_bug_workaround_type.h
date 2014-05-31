e892510805df55cc789228f78ad610d1017ae747
reveman@chromium.org <reveman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-31 04:06:21 +0000

    gpu: Add disable_arb_sync workaround and blacklist some MacOSX drivers.
    BUG=371530
    
    Review URL: https://codereview.chromium.org/301243005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274004 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/config/gpu_driver_bug_workaround_type.h
+++ gpu/config/gpu_driver_bug_workaround_type.h
@@ -19,6 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
          count_all_in_varyings_packing)                      \
   GPU_OP(DISABLE_ANGLE_INSTANCED_ARRAYS,                     \
          disable_angle_instanced_arrays)                     \
+  GPU_OP(DISABLE_ARB_SYNC,                                   \
+         disable_arb_sync)                                   \
   GPU_OP(DISABLE_ASYNC_READPIXELS,                           \
          disable_async_readpixels)                           \
   GPU_OP(DISABLE_D3D11,                                      \
