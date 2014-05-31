c12fa770c4cfc5a421c3e87a16a9f3b1538e3801
aboxhall@chromium.org <aboxhall@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-31 00:37:05 +0000

    Allow requesting Automation tree by tabId
    BUG=309681
    R=asvitkine@chromium.org, dtseng@chromium.org, kalman@chromium.org
    
    Review URL: https://codereview.chromium.org/308003003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273958 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/api_test/automation/tests/tabs/tab_id.js
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function createBackgroundTab(url, callback) {
+  chrome.tabs.query({ active: true }, function(tabs) {
+    chrome.test.assertEq(1, tabs.length);
+    var originalActiveTab = tabs[0];
+    createTab(url, function(tab) {
+      chrome.tabs.update(originalActiveTab.id, { active: true }, function() {
+        callback(tab);
+      });
+    })
+  });
+}
+
+var allTests = [
+  function testGetTabById() {
+    getUrlFromConfig(function(url) {
+      // Keep the NTP as the active tab so that we know we're requesting the
+      // tab by ID rather than just getting the active tab still.
+      createBackgroundTab(url, function(tab) {
+        chrome.automation.getTree(tab.id, function(tree) {
+          tree.addEventListener('loadComplete', function() {
+            var title = tree.root.attributes['docTitle'];
+            chrome.test.assertEq('Automation Tests', title);
+            chrome.test.succeed();
+          });
+        })
+      });
+    });
+  }
+];
+
+chrome.test.runTests(allTests);
