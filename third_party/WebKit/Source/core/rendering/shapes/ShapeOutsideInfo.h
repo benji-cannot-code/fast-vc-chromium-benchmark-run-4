241456cc5516dbdd0beef858eebde1cb6f04c313
hmuller@adobe.com <hmuller@adobe.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-31 00:10:11 +0000

    Heap-use-after-free in WebCore::GraphicsContext::drawImage
    Replaced getShapeImageAndRect() in ShapeOutsideInfo.cpp with
    ShapeOutsideInfo::createShapeForImage(). The original function
    failed to protect the PassRefPtr return value returned by StyleImage::image(). 
    
    BUG=378469
    
    Review URL: https://codereview.chromium.org/305173003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175176 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/shapes/ShapeOutsideInfo.h
+++ third_party/WebKit/Source/core/rendering/shapes/ShapeOutsideInfo.h
@@ -102,6 +102,8 @@ protected:
     { }
 
 private:
+    PassOwnPtr<Shape> createShapeForImage(StyleImage*, float shapeImageThreshold, WritingMode, float margin) const;
+
     LayoutUnit logicalTopOffset() const;
     LayoutUnit logicalLeftOffset() const;
 
