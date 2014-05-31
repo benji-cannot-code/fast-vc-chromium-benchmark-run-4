c9d630682ec4ce5d31dbe365d59030b946a3cb4f
mpearson@chromium.org <mpearson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-31 00:52:18 +0000

    Omnibox: Create Disable-Inlining Experiment
    Sets up a experiment that, when active, demotes all suggestions with
    inline autocompletions so that a suggestion without one (or, more precisely,
    a suggestion with an empty inline autocompletion) appears first / within
    the omnibox.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/303013002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273966 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/omnibox/omnibox_field_trial.cc
+++ chrome/browser/omnibox/omnibox_field_trial.cc
@@ -426,6 +426,12 @@ bool OmniboxFieldTrial::BookmarksIndexURLsValue() {
       kBookmarksIndexURLsRule) == "true";
 }
 
+bool OmniboxFieldTrial::DisableInlining() {
+  return chrome_variations::GetVariationParamValue(
+      kBundledExperimentFieldTrialName,
+      kDisableInliningRule) == "true";
+}
+
 const char OmniboxFieldTrial::kBundledExperimentFieldTrialName[] =
     "OmniboxBundledExperimentV1";
 const char OmniboxFieldTrial::kShortcutsScoringMaxRelevanceRule[] =
@@ -440,6 +446,7 @@ const char OmniboxFieldTrial::kHQPAllowMatchInSchemeRule[] =
 const char OmniboxFieldTrial::kZeroSuggestRule[] = "ZeroSuggest";
 const char OmniboxFieldTrial::kZeroSuggestVariantRule[] = "ZeroSuggestVariant";
 const char OmniboxFieldTrial::kBookmarksIndexURLsRule[] = "BookmarksIndexURLs";
+const char OmniboxFieldTrial::kDisableInliningRule[] = "DisableInlining";
 
 const char OmniboxFieldTrial::kHUPNewScoringEnabledParam[] =
     "HUPExperimentalScoringEnabled";
