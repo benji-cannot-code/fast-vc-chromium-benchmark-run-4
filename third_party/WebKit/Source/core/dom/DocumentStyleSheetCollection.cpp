6f92e713424d43aa4c9b441a6a285a66774d97e0
rob.buis@samsung.com <rob.buis@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-01 01:24:03 +0000

    Cleanup DocumentStyleSheetCollection
    It seems a lot of code moved out of this class, so remove various includes and forward references.
    
    Review URL: https://codereview.chromium.org/302093006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175211 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/DocumentStyleSheetCollection.cpp
+++ third_party/WebKit/Source/core/dom/DocumentStyleSheetCollection.cpp
@@ -28,26 +28,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "core/dom/DocumentStyleSheetCollection.h"
 
-#include "HTMLNames.h"
 #include "RuntimeEnabledFeatures.h"
-#include "SVGNames.h"
-#include "core/css/CSSStyleSheet.h"
 #include "core/css/resolver/StyleResolver.h"
 #include "core/dom/Document.h"
 #include "core/dom/DocumentStyleSheetCollector.h"
-#include "core/dom/Element.h"
 #include "core/dom/ProcessingInstruction.h"
 #include "core/dom/StyleEngine.h"
 #include "core/dom/StyleSheetCandidate.h"
-#include "core/html/HTMLIFrameElement.h"
-#include "core/html/HTMLLinkElement.h"
-#include "core/html/HTMLStyleElement.h"
-#include "core/frame/Settings.h"
-#include "core/svg/SVGStyleElement.h"
 
 namespace WebCore {
-
-using namespace HTMLNames;
 
 DocumentStyleSheetCollection::DocumentStyleSheetCollection(TreeScope& treeScope)
     : TreeScopeStyleSheetCollection(treeScope)
