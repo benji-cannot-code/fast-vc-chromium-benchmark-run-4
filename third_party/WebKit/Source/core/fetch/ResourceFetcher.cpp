c726f46031d8814acb89844b3bd7c9ada585abee
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-01 05:16:32 +0000

    HTMLImportsController does not need to hold Document ptr.
    Since it owns the HTMLImportTreeRoot, it can just ask it for the
    master document. Additionally, remove indirection plumbing that's
    just straightforward queries of master document.
    
    No changes in behavior, refactoring.
    
    R=morrita
    BUG=
    
    Review URL: https://codereview.chromium.org/305743003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175214 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/fetch/ResourceFetcher.cpp
+++ third_party/WebKit/Source/core/fetch/ResourceFetcher.cpp
@@ -263,7 +263,7 @@ LocalFrame* ResourceFetcher::frame() const
     if (m_documentLoader)
         return m_documentLoader->frame();
     if (m_document && m_document->importsController())
-        return m_document->importsController()->frame();
+        return m_document->importsController()->master()->frame();
     return 0;
 }
 
--- third_party/WebKit/Source/core/html/imports/HTMLImportsController.h
+++ third_party/WebKit/Source/core/html/imports/HTMLImportsController.h
@@ -62,17 +62,13 @@ public:
 
     HTMLImportTreeRoot* root() const { return m_root.get(); }
 
-    bool isMaster(const Document& document) const { return m_master == &document; }
     bool shouldBlockScriptExecution(const Document&) const;
     void wasDetachedFrom(const Document&);
 
     HTMLImportChild* load(HTMLImport* parent, HTMLImportChildClient*, FetchRequest);
     void showSecurityErrorMessage(const String&);
 
-    SecurityOrigin* securityOrigin() const;
-    ResourceFetcher* fetcher() const;
-    LocalFrame* frame() const;
-    Document* master() const { return m_master; }
+    Document* master() const;
 
     HTMLImportLoader* createLoader();
 
@@ -83,8 +79,6 @@ public:
 private:
     HTMLImportChild* createChild(const KURL&, HTMLImportLoader*, HTMLImport* parent, HTMLImportChildClient*);
     void clear();
-
-    Document* m_master;
 
     OwnPtr<HTMLImportTreeRoot> m_root;
 
