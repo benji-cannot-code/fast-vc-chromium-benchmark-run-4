c1231574feb0e6eb0d3a6a04399479dcea4f6fa7
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-01 17:17:23 +0000

    [Telemetry] Disable benchmarks that are failing on Android.
    BUG=379561
    TBR=dtu@chromium.org
    NOTRY=True
    
    Review URL: https://codereview.chromium.org/309623006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274125 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/benchmarks/memory_pressure.py
+++ tools/perf/benchmarks/memory_pressure.py
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 from telemetry import test
 from measurements import memory_pressure
 
+
+@test.Disabled('android')  # crbug.com/379561
 class MemoryPressure(test.Test):
   test = memory_pressure.MemoryPressure
   page_set = 'page_sets/typical_25.py'
