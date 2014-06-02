e54a632d9e429ba2be4354d9e26186baaf86dc59
raymes@chromium.org <raymes@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-02 08:15:49 +0000

    Re-enable PDFExtensionTest.BasicPlugin
    This test was disabled because it was failing. A few changes have been made:
    -The page zoom cannot be assumed to be 100% on load as it will depend on the screen size being tested on. So testPageSize now doesn't make that assumption.
    -testAccessibilityWithPage was flaking because accessibility information was not always returned for the two text boxes. I spent a bit of time investigating, but couldn't work it out so I've left it partially disabled for now and filed crbug.com/378091.
    -The files have all been moved to a more appropriate location (in particular the test was moved out of the data directory).
    
    BUG=303491
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=273819
    
    Review URL: https://codereview.chromium.org/290803007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274186 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/pdf/pdf_extension_test.cc
+++ chrome/browser/resources/pdf/pdf_extension_test.cc
@@ -38,7 +38,6 @@ class PDFExtensionTest : public ExtensionApiTest {
   }
 
   void RunTestsInFile(std::string filename, bool requiresPlugin) {
-#if !defined(GOOGLE_CHROME_BUILD)
     base::FilePath pdf_plugin_src;
     PathService::Get(base::DIR_SOURCE_ROOT, &pdf_plugin_src);
     pdf_plugin_src = pdf_plugin_src.AppendASCII("pdf");
@@ -47,7 +46,6 @@ class PDFExtensionTest : public ExtensionApiTest {
           " because it requires the PDF plugin which is not available.";
       return;
     }
-#endif
     ExtensionService* service = extensions::ExtensionSystem::Get(
         profile())->extension_service();
     service->component_loader()->Add(IDR_PDF_MANIFEST,
@@ -88,9 +86,7 @@ IN_PROC_BROWSER_TEST_F(PDFExtensionTest, Basic) {
   RunTestsInFile("basic_test.js", false);
 }
 
-// TODO(raymes): investigate why this started failing after PDF plugin became
-// open source.
-IN_PROC_BROWSER_TEST_F(PDFExtensionTest, DISABLED_BasicPlugin) {
+IN_PROC_BROWSER_TEST_F(PDFExtensionTest, BasicPlugin) {
   RunTestsInFile("basic_plugin_test.js", true);
 }
 
