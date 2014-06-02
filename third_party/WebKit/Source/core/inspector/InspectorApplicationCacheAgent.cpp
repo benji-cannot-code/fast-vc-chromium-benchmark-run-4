27b7813a08bc8305a4bd7875f9eb982949ddcb35
kenrb@chromium.org <kenrb@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-02 17:55:36 +0000

    Make Inspector aware of RemoteFrames in FrameTree
    We will soon make FrameTree operate on the Frame base class, which can
    be instantiated as either LocalFrame or RemoteFrame. This patch modifies
    the call sites in the Inspector that will be affected by this change.
    
    BUG=346764
    TBR=pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/301983003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175303 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorApplicationCacheAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorApplicationCacheAgent.cpp
@@ -101,8 +101,10 @@ void InspectorApplicationCacheAgent::getFramesWithManifests(ErrorString*, RefPtr
     result = TypeBuilder::Array<TypeBuilder::ApplicationCache::FrameWithManifest>::create();
 
     LocalFrame* mainFrame = m_pageAgent->mainFrame();
-    for (LocalFrame* frame = mainFrame; frame; frame = frame->tree().traverseNext(mainFrame)) {
-        DocumentLoader* documentLoader = frame->loader().documentLoader();
+    for (Frame* frame = mainFrame; frame; frame = frame->tree().traverseNext(mainFrame)) {
+        if (!frame->isLocalFrame())
+            continue;
+        DocumentLoader* documentLoader = toLocalFrame(frame)->loader().documentLoader();
         if (!documentLoader)
             continue;
 
@@ -111,7 +113,7 @@ void InspectorApplicationCacheAgent::getFramesWithManifests(ErrorString*, RefPtr
         String manifestURL = info.m_manifest.string();
         if (!manifestURL.isEmpty()) {
             RefPtr<TypeBuilder::ApplicationCache::FrameWithManifest> value = TypeBuilder::ApplicationCache::FrameWithManifest::create()
-                .setFrameId(m_pageAgent->frameId(frame))
+                .setFrameId(m_pageAgent->frameId(toLocalFrame(frame)))
                 .setManifestURL(manifestURL)
                 .setStatus(static_cast<int>(host->status()));
             result->addItem(value);
