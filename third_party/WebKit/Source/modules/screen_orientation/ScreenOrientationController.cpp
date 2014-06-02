392f40c7ff2fd0fe7a09f6e19ec6756d04ee8a4d
sigbjornf@opera.com <sigbjornf@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-02 11:25:21 +0000

    Oilpan: fix build after r175264.
    Finalization of ScreenOrientationController now needed, adjust GC class.
    
    R=haraken@chromium.org,ager@chromium.org
    BUG=162827
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/305363003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175274 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/screen_orientation/ScreenOrientationController.cpp
+++ third_party/WebKit/Source/modules/screen_orientation/ScreenOrientationController.cpp
@@ -17,7 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-DEFINE_EMPTY_DESTRUCTOR_WILL_BE_REMOVED(ScreenOrientationController)
+ScreenOrientationController::~ScreenOrientationController()
+{
+}
 
 ScreenOrientationController& ScreenOrientationController::from(Document& document)
 {
