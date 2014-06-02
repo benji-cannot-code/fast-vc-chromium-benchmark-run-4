9e9b08370d44175efbfd14825da8cef71571e0f0
donnd@chromium.org <donnd@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-02 22:36:32 +0000

    Update EventSender::GestureTap to return a bool (if handled).
    BUG=355154
    
    Review URL: https://codereview.chromium.org/301103002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274357 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/renderer/test_runner/event_sender.cc
+++ content/shell/renderer/test_runner/event_sender.cc
@@ -2068,7 +2068,7 @@ void EventSender::GestureEvent(WebInputEvent::Type type,
   if (force_layout_on_events_)
     view_->layout();
 
-  view_->handleInputEvent(event);
+  bool result = view_->handleInputEvent(event);
 
   // Long press might start a drag drop session. Complete it if so.
   if (type == WebInputEvent::GestureLongPress && !current_drag_data_.isNull()) {
@@ -2083,6 +2083,7 @@ void EventSender::GestureEvent(WebInputEvent::Type type,
 
     FinishDragAndDrop(mouse_event, blink::WebDragOperationNone);
   }
+  args->Return(result);
 }
 
 void EventSender::UpdateClickCountForButton(
