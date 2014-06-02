7c012758128d7b2a4a914a937b6b145f9d329894
bokan@chromium.org <bokan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-02 17:33:35 +0000

    Made RenderBox::scrollRectToVisible pinch viewport aware.
    Made the visible rect correspond to the actual visible area viewable
    through the pinch viewport. The scrolling now takes into account
    the pinch viewport so if the given rect can't be scrolled into view
    via scrolling the main FrameView, the PinchViewport is scrolled so
    that it does.
    
    This fixes "Find In Page" to scroll matches into view while the ChromeOS
    keyboard is up or when the page is pinch-zoomed in. It also fixes
    autoscroll while pinch-zoomed in.
    
    BUG=369321
    
    Review URL: https://codereview.chromium.org/299363006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175301 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderBox.cpp
+++ third_party/WebKit/Source/core/rendering/RenderBox.cpp
@@ -32,8 +32,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLNames.h"
 #include "core/dom/Document.h"
 #include "core/editing/htmlediting.h"
+#include "core/frame/FrameHost.h"
 #include "core/frame/FrameView.h"
 #include "core/frame/LocalFrame.h"
+#include "core/frame/PinchViewport.h"
+#include "core/frame/Settings.h"
 #include "core/html/HTMLElement.h"
 #include "core/html/HTMLFrameElementBase.h"
 #include "core/html/HTMLFrameOwnerElement.h"
@@ -501,9 +504,15 @@ void RenderBox::scrollRectToVisible(const LayoutRect& rect, const ScrollAlignmen
                     }
                 }
             } else {
-                LayoutRect viewRect = frameView->visibleContentRect();
-                LayoutRect r = ScrollAlignment::getRectToExpose(viewRect, rect, alignX, alignY);
-                frameView->setScrollPosition(roundedIntPoint(r.location()));
+                if (frame()->settings()->pinchVirtualViewportEnabled()) {
+                    PinchViewport& pinchViewport = frame()->page()->frameHost().pinchViewport();
+                    LayoutRect r = ScrollAlignment::getRectToExpose(LayoutRect(pinchViewport.visibleRectInDocument()), rect, alignX, alignY);
+                    pinchViewport.scrollIntoView(r);
+                } else {
+                    LayoutRect viewRect = frameView->visibleContentRect();
+                    LayoutRect r = ScrollAlignment::getRectToExpose(viewRect, rect, alignX, alignY);
+                    frameView->setScrollPosition(roundedIntPoint(r.location()));
+                }
             }
         }
     }
