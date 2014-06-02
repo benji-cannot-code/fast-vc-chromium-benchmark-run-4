3b6937a4ec917887b3f93ab79614b67baa61c5af
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-02 23:42:14 +0000

    Fix some views unittests on desktop Linux.
    BUG=369289
    
    Review URL: https://codereview.chromium.org/291093012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274369 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/desktop_aura/desktop_window_tree_host_x11.h
+++ ui/views/widget/desktop_aura/desktop_window_tree_host_x11.h
@@ -158,7 +158,7 @@ class VIEWS_EXPORT DesktopWindowTreeHostX11
   // Overridden frm ui::EventSource
   virtual ui::EventProcessor* GetEventProcessor() OVERRIDE;
 
-private:
+ private:
   // Initializes our X11 surface to draw on. This method performs all
   // initialization related to talking to the X11 server.
   void InitX11Window(const Widget::InitParams& params);
