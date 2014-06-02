9ac0a79dee9d729a66e8c419ed0841348830ad67
mkosiba@chromium.org <mkosiba@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-02 10:25:11 +0000

    Don't stop the documentLoader on navigations.
    If the new navigation will be canceled by a Browser-side throttle we
    don't want to cancel the currently loading document.
    
    BUG=308257, 378716
    
    Review URL: https://codereview.chromium.org/181493007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175269 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/FrameLoader.cpp
+++ third_party/WebKit/Source/core/loader/FrameLoader.cpp
@@ -942,6 +942,10 @@ bool FrameLoader::checkLoadCompleteForThisFrame()
     ASSERT(client()->hasWebView());
     RefPtr<LocalFrame> protect(m_frame);
 
+    bool allChildrenAreDoneLoading = true;
+    for (LocalFrame* child = m_frame->tree().firstChild(); child; child = child->tree().nextSibling())
+        allChildrenAreDoneLoading &= child->loader().checkLoadCompleteForThisFrame();
+
     if (m_state == FrameStateProvisional && m_provisionalDocumentLoader) {
         const ResourceError& error = m_provisionalDocumentLoader->mainDocumentError();
         if (error.isNull())
@@ -957,9 +961,6 @@ bool FrameLoader::checkLoadCompleteForThisFrame()
         return true;
     }
 
-    bool allChildrenAreDoneLoading = true;
-    for (LocalFrame* child = m_frame->tree().firstChild(); child; child = child->tree().nextSibling())
-        allChildrenAreDoneLoading &= child->loader().checkLoadCompleteForThisFrame();
     if (!allChildrenAreDoneLoading)
         return false;
 
@@ -1294,8 +1295,13 @@ void FrameLoader::loadWithNavigationAction(const NavigationAction& action, Frame
         return;
     }
 
-    // A new navigation is in progress, so don't clear the history's provisional item.
-    stopAllLoaders();
+    if (m_provisionalDocumentLoader) {
+        m_provisionalDocumentLoader->stopLoading();
+        if (m_provisionalDocumentLoader)
+            m_provisionalDocumentLoader->detachFromFrame();
+        m_provisionalDocumentLoader = nullptr;
+    }
+    m_checkTimer.stop();
 
     // <rdar://problem/6250856> - In certain circumstances on pages with multiple frames, stopAllLoaders()
     // might detach the current FrameLoader, in which case we should bail on this newly defunct load.
