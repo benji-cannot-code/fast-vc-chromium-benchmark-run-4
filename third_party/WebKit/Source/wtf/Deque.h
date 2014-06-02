bf8460fbaa855f905974b1905eeee104e130378e
erik.corry@gmail.com <erik.corry@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-02 12:05:18 +0000

    Oilpan: Clear unused slots in Deque so they are not found in conservative GCs
    If we find a direct pointer to the backing store of the deque (this will
    normally be an iterator) we have to trace the backing without knowing how
    much of it is in use (that information is in the Deque itself, not the
    backing store).  This means we have to ensure that unused areas of the
    backing store are zeroed out.
    R=ager@chromium.org, mikhail.pozdnyakov@untel.com
    BUG=
    
    Review URL: https://codereview.chromium.org/308323002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175277 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/Deque.h
+++ third_party/WebKit/Source/wtf/Deque.h
@@ -247,11 +247,14 @@ namespace WTF {
     template<typename T, size_t inlineCapacity, typename Allocator>
     inline void Deque<T, inlineCapacity, Allocator>::destroyAll()
     {
-        if (m_start <= m_end)
+        if (m_start <= m_end) {
             TypeOperations::destruct(m_buffer.buffer() + m_start, m_buffer.buffer() + m_end);
-        else {
+            m_buffer.clearUnusedSlots(m_buffer.buffer() + m_start, m_buffer.buffer() + m_end);
+        } else {
             TypeOperations::destruct(m_buffer.buffer(), m_buffer.buffer() + m_end);
+            m_buffer.clearUnusedSlots(m_buffer.buffer(), m_buffer.buffer() + m_end);
             TypeOperations::destruct(m_buffer.buffer() + m_start, m_buffer.buffer() + m_buffer.capacity());
+            m_buffer.clearUnusedSlots(m_buffer.buffer() + m_start, m_buffer.buffer() + m_buffer.capacity());
         }
     }
 
@@ -376,6 +379,7 @@ namespace WTF {
     {
         ASSERT(!isEmpty());
         TypeOperations::destruct(&m_buffer.buffer()[m_start], &m_buffer.buffer()[m_start + 1]);
+        m_buffer.clearUnusedSlots(&m_buffer.buffer()[m_start], &m_buffer.buffer()[m_start + 1]);
         if (m_start == m_buffer.capacity() - 1)
             m_start = 0;
         else
@@ -391,6 +395,7 @@ namespace WTF {
         else
             --m_end;
         TypeOperations::destruct(&m_buffer.buffer()[m_end], &m_buffer.buffer()[m_end + 1]);
+        m_buffer.clearUnusedSlots(&m_buffer.buffer()[m_end], &m_buffer.buffer()[m_end + 1]);
     }
 
     template<typename T, size_t inlineCapacity, typename Allocator>
@@ -417,9 +422,11 @@ namespace WTF {
         // Find which segment of the circular buffer contained the remove element, and only move elements in that part.
         if (position >= m_start) {
             TypeOperations::moveOverlapping(buffer + m_start, buffer + position, buffer + m_start + 1);
+            m_buffer.clearUnusedSlots(buffer + m_start, buffer + m_start + 1);
             m_start = (m_start + 1) % m_buffer.capacity();
         } else {
             TypeOperations::moveOverlapping(buffer + position + 1, buffer + m_end, buffer + position);
+            m_buffer.clearUnusedSlots(buffer + m_end - 1, buffer + m_end);
             m_end = (m_end - 1 + m_buffer.capacity()) % m_buffer.capacity();
         }
     }
