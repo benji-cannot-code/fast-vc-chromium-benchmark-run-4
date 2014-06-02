1008d2362da294dca8c5cd22d645ddf95b9b1ea7
tasak@google.com <tasak@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-02 12:24:08 +0000

    Removed v8::Isolate::GetCurrent() and fixed toV8 with empty context.
    BUG=369451
    
    Review URL: https://codereview.chromium.org/302973003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175279 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/web/WebArrayBufferConverter.h
+++ third_party/WebKit/public/web/WebArrayBufferConverter.h
@@ -35,6 +35,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "public/platform/WebArrayBuffer.h"
 
 namespace v8 {
+class Isolate;
+class Object;
 class Value;
 template <class T> class Handle;
 }
@@ -43,8 +45,12 @@ namespace blink {
 
 class WebArrayBufferConverter {
 public:
+    // FIXME: remove these two APIs which doesn't take either context or
+    // isolate as their parameters after the embedder side is fixed.
     BLINK_EXPORT static v8::Handle<v8::Value> toV8Value(WebArrayBuffer*);
     BLINK_EXPORT static WebArrayBuffer* createFromV8Value(v8::Handle<v8::Value>);
+    BLINK_EXPORT static v8::Handle<v8::Value> toV8Value(WebArrayBuffer*, v8::Handle<v8::Object>, v8::Isolate*);
+    BLINK_EXPORT static WebArrayBuffer* createFromV8Value(v8::Handle<v8::Value>, v8::Isolate*);
 };
 
 } // namespace blink
