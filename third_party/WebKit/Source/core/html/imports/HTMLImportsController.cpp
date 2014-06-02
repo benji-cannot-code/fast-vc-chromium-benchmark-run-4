7736a198aef72650e183f6b0efab6b678966a2f7
morrita@chromium.org <morrita@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-02 22:47:08 +0000

    Teach V8GCController about HTML Imports
    In terms of wrapper lifecycle, an imported document is a child of
    the master document (or some ancestor imports). This change tells
    that to V8GCController so that it can form correct object groups.
    
    R=haraken@chromium.org, adamk@chromium.org
    TEST=import-expando-gc.html
    BUG=379267
    
    Review URL: https://codereview.chromium.org/309723002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175322 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/imports/HTMLImportsController.cpp
+++ third_party/WebKit/Source/core/html/imports/HTMLImportsController.cpp
@@ -160,4 +160,9 @@ HTMLImportLoader* HTMLImportsController::loaderFor(const Document& document) con
     return 0;
 }
 
+Document* HTMLImportsController::loaderDocumentAt(size_t i) const
+{
+    return loaderAt(i)->document();
+}
+
 } // namespace WebCore
