bf78a3438a0c5b8a36af6e7e343f8e6041e2a5bb
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-02 19:57:44 +0000

    cros: Expose client_id via chromeosInfoPrivate.
    Expose client_id in chromeosInforPrivate for enterprise
    kiosk.
    
    BUG=328437
    
    Review URL: https://codereview.chromium.org/308913004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274313 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/extensions/info_private_api.cc
+++ chrome/browser/chromeos/extensions/info_private_api.cc
@@ -9,10 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/prefs/pref_service.h"
 #include "base/sys_info.h"
 #include "base/values.h"
+#include "chrome/browser/app_mode/app_mode_utils.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/chromeos/login/startup_utils.h"
 #include "chrome/browser/chromeos/login/users/user_manager.h"
+#include "chrome/browser/chromeos/policy/browser_policy_connector_chromeos.h"
 #include "chrome/browser/chromeos/settings/cros_settings.h"
 #include "chrome/browser/chromeos/system/timezone_util.h"
+#include "chrome/browser/metrics/metrics_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/pref_names.h"
 #include "chromeos/network/device_state.h"
@@ -44,8 +48,11 @@ const char kPropertyInitialLocale[] = "initialLocale";
 // Key which corresponds to the board property in JS.
 const char kPropertyBoard[] = "board";
 
-// Key which corresponds to the board property in JS.
+// Key which corresponds to the isOwner property in JS.
 const char kPropertyOwner[] = "isOwner";
+
+// Key which corresponds to the clientId property in JS.
+const char kPropertyClientId[] = "clientId";
 
 // Key which corresponds to the timezone property in JS.
 const char kPropertyTimezone[] = "timezone";
@@ -104,6 +111,21 @@ const char* GetBoolPrefNameForApiProperty(const char* api_name) {
   return NULL;
 }
 
+bool IsEnterpriseKiosk() {
+  if (!chrome::IsRunningInForcedAppMode())
+    return false;
+
+  policy::BrowserPolicyConnectorChromeOS* connector =
+      g_browser_process->platform_part()->browser_policy_connector_chromeos();
+  return connector->IsEnterpriseManaged();
+}
+
+std::string GetClientId() {
+  return IsEnterpriseKiosk()
+             ? g_browser_process->metrics_service()->GetClientId()
+             : std::string();
+}
+
 }  // namespace
 
 ChromeosInfoPrivateGetFunction::ChromeosInfoPrivateGetFunction() {
@@ -159,6 +181,8 @@ base::Value* ChromeosInfoPrivateGetFunction::GetValue(
   } else if (property_name == kPropertyOwner) {
     return base::Value::CreateBooleanValue(
         chromeos::UserManager::Get()->IsCurrentUserOwner());
+  } else if (property_name == kPropertyClientId) {
+    return base::Value::CreateStringValue(GetClientId());
   } else if (property_name == kPropertyTimezone) {
     return chromeos::CrosSettings::Get()->GetPref(
             chromeos::kSystemTimezone)->DeepCopy();
