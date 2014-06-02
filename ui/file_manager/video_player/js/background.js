af7acec1c0517381c38c7457ce18c138cb3d24ee
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-02 07:37:04 +0000

    [Video Player] Re-factoring the code
    Convert the video app into a class. No functionality change.
    
    BUG=171191
    TEST=browser test passes.
    R=mtomasz@chromium.org
    
    Review URL: https://codereview.chromium.org/307863004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274182 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/file_manager/video_player/js/background.js
+++ ui/file_manager/video_player/js/background.js
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 'use strict';
 
-
 // Stores the app windows OLNY for test purpose.
 // We SHOULD NOT use it as it is except for test, since the files which have
 // the same name will be overridden each other.
@@ -15,10 +14,27 @@ chrome.app.runtime.onLaunched.addListener(function(launchData) {
   if (!launchData || !launchData.items || launchData.items.length == 0)
     return;
 
-  var entry = launchData.items[0].entry;
-  entry.file(function(file) {
-    var url = window.URL.createObjectURL(file);
-    open(url, entry.name);
+  var getFilePromises = launchData.items.map(function(item) {
+    var entry = item.entry;
+    return new Promise(function(fullfill, reject) {
+      entry.file(
+          function(file) {
+            fullfill({
+              entry: entry,
+              file: file,
+              fileUrl: window.URL.createObjectURL(file)
+            });
+          },
+          function() {
+            fullfill({entry: entry, file: null, fileUrl: null});
+          });
+    });
+  });
+
+  Promise.all(getFilePromises).then(function(results) {
+    results = results.filter(function(result) { return result.file !== null; });
+    if (results.length > 0)
+      open(results);
   }.wrap(),
   function() {
     // TODO(yoshiki): handle error in a smarter way.
@@ -26,7 +42,11 @@ chrome.app.runtime.onLaunched.addListener(function(launchData) {
   }.wrap());
 }.wrap());
 
-function open(url, title) {
+/**
+ * Opens player window.
+ * @param {Array.<Object>} videos List of videos to play.
+ **/
+function open(videos) {
   chrome.app.window.create('video_player.html', {
     id: 'video',
     singleton: false,
@@ -35,10 +55,10 @@ function open(url, title) {
   },
   function(createdWindow) {
     // Stores the window for test purpose.
-    appWindowsForTest[title] = createdWindow;
+    appWindowsForTest[videos[0].entry.name] = createdWindow;
 
     createdWindow.setIcon('images/200/icon.png');
-    createdWindow.contentWindow.videoUrl = url;
-    createdWindow.contentWindow.videoTitle = title;
+    createdWindow.contentWindow.videos = videos;
+    chrome.runtime.sendMessage({ready: true}, function() {});
   }.wrap());
 }
