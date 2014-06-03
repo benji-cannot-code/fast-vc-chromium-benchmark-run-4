76ac8403436849166758ad548b876809b34a0336
lauren.yeun.kim@gmail.com <lauren.yeun.kim@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-03 12:04:33 +0000

    Cleanup: Style fixes in extensions/browser
    1) Fix Namespace comment in admin_policy.h
    2) Mark explicit to single-argument constructors in file_highlighter.h
       and test_extensions_browser_client.h
    3) Use CHECK_GT instead of CHECK in process_manager.cc
    
    BUG=
    
    Review URL: https://codereview.chromium.org/308913006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274475 0039d316-1c4b-4281-b951-d872f2087c98

--- extensions/browser/file_highlighter.h
+++ extensions/browser/file_highlighter.h
@@ -42,7 +42,7 @@ class FileHighlighter {
   void SetHighlightedRegions(base::DictionaryValue* dict) const;
 
  protected:
-  FileHighlighter(const std::string& contents);
+  explicit FileHighlighter(const std::string& contents);
 
   // The contents of the file we are parsing.
   std::string contents_;
