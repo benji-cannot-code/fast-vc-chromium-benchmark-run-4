f0ea1dc37d9815231dd80f3a831d40111d78560a
pvalenzuela@chromium.org <pvalenzuela@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-03 23:05:38 +0000

    Convert Sync enable/disable test to use FakeServer
    BUG=323265
    
    Review URL: https://codereview.chromium.org/312623003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274663 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/test/integration/enable_disable_test.cc
+++ chrome/browser/sync/test/integration/enable_disable_test.cc
@@ -17,10 +17,11 @@ namespace {
 
 class EnableDisableSingleClientTest : public SyncTest {
  public:
-  // TODO(pvalenzuela): Switch to SINGLE_CLIENT once FakeServer
-  // supports this scenario.
-  EnableDisableSingleClientTest() : SyncTest(SINGLE_CLIENT_LEGACY) {}
+  EnableDisableSingleClientTest() : SyncTest(SINGLE_CLIENT) {}
   virtual ~EnableDisableSingleClientTest() {}
+
+  // Don't use self-notifications as they can trigger additional sync cycles.
+  virtual bool TestUsesSelfNotifications() OVERRIDE { return false; }
  private:
   DISALLOW_COPY_AND_ASSIGN(EnableDisableSingleClientTest);
 };
@@ -37,11 +38,6 @@ IN_PROC_BROWSER_TEST_F(EnableDisableSingleClientTest, EnableOneAtATime) {
 
   // Setup sync with no enabled types.
   ASSERT_TRUE(GetClient(0)->SetupSync(syncer::ModelTypeSet()));
-
-  // TODO(rlarocque, 97780): It should be possible to disable notifications
-  // before calling SetupSync().  We should move this line back to the top
-  // of this function when this is supported.
-  DisableNotifications();
 
   const syncer::ModelTypeSet registered_types =
       GetSyncService((0))->GetRegisteredDataTypes();
@@ -74,8 +70,6 @@ IN_PROC_BROWSER_TEST_F(EnableDisableSingleClientTest, EnableOneAtATime) {
                                         syncer::SESSIONS));
     }
   }
-
-  EnableNotifications();
 }
 
 IN_PROC_BROWSER_TEST_F(EnableDisableSingleClientTest, DisableOneAtATime) {
@@ -83,11 +77,6 @@ IN_PROC_BROWSER_TEST_F(EnableDisableSingleClientTest, DisableOneAtATime) {
 
   // Setup sync with no disabled types.
   ASSERT_TRUE(GetClient(0)->SetupSync());
-
-  // TODO(rlarocque, 97780): It should be possible to disable notifications
-  // before calling SetupSync().  We should move this line back to the top
-  // of this function when this is supported.
-  DisableNotifications();
 
   const syncer::ModelTypeSet registered_types =
       GetSyncService((0))->GetRegisteredDataTypes();
@@ -152,8 +141,6 @@ IN_PROC_BROWSER_TEST_F(EnableDisableSingleClientTest, DisableOneAtATime) {
                                          syncer::PRIORITY_PREFERENCES));
     }
   }
-
-  EnableNotifications();
 }
 
 }  // namespace
