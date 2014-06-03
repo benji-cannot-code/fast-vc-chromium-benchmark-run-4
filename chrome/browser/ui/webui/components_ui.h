367791c838ae39ebcf7300a161fd098e8e766113
shrikant@chromium.org <shrikant@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-03 00:10:56 +0000

    Modified components ui to address concern of all the time disabled check update button. With this change we take into account current status of update from update service.
    +jhawkins for owners
    
    BUG=272540
    R=sorin,cpu,jhawkins
    
    Review URL: https://codereview.chromium.org/209313002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274385 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/components_ui.h
+++ chrome/browser/ui/webui/components_ui.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_WEBUI_COMPONENTS_UI_H_
 #define CHROME_BROWSER_UI_WEBUI_COMPONENTS_UI_H_
 
+#include "chrome/browser/component_updater/component_updater_service.h"
+#include "chrome/browser/component_updater/crx_update_item.h"
 #include "content/public/browser/web_ui_controller.h"
 #include "ui/base/layout.h"
 
@@ -17,16 +19,26 @@ namespace user_prefs {
 class PrefRegistrySyncable;
 }
 
-class ComponentsUI : public content::WebUIController {
+class ComponentsUI : public content::WebUIController,
+                     public component_updater::ServiceObserver {
  public:
   explicit ComponentsUI(content::WebUI* web_ui);
+  virtual ~ComponentsUI();
 
   static void OnDemandUpdate(const std::string& component_id);
+
+  static base::ListValue* LoadComponents();
 
   static base::RefCountedMemory* GetFaviconResourceBytes(
       ui::ScaleFactor scale_factor);
 
+  // ServiceObserver implementation.
+  virtual void OnEvent(Events event, const std::string& id) OVERRIDE;
+
  private:
+  static base::string16 ComponentEventToString(Events event);
+  static base::string16 ServiceStatusToString(
+      component_updater::CrxUpdateItem::Status status);
   DISALLOW_COPY_AND_ASSIGN(ComponentsUI);
 };
 
