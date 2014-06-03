bbda904a74c326c2c167990a50f0b9b947925c79
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-03 05:39:00 +0000

    Implement a demo that shows one app embedding rendering in another.
    launcher inits the view manager, running the "window manager" app @ root.
    window manager embeds another app.
    
    ** not tested by main chrome integration testing **
    
    R=sky@chromium.org
    TEST=mojo_shell --origin=http://127.0.0.1:4444 --disable-cache mojo:mojo_demo_launcher
    http://crbug.com/365012
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=274322
    
    Review URL: https://codereview.chromium.org/303163005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274422 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/mojo_services.gypi
+++ mojo/mojo_services.gypi
@@ -190,6 +190,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         '../skia/skia.gyp:skia',
         '../ui/gfx/gfx.gyp:gfx',
         '../ui/gfx/gfx.gyp:gfx_geometry',
+        'mojo_application',
         'mojo_geometry_bindings',
         'mojo_geometry_lib',
         'mojo_service_provider_bindings',
--- mojo/services/public/cpp/view_manager/lib/view_manager_synchronizer.h
+++ mojo/services/public/cpp/view_manager/lib/view_manager_synchronizer.h
@@ -16,10 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class SkBitmap;
 
-namespace base {
-class RunLoop;
-}
-
 namespace mojo {
 namespace view_manager {
 
@@ -27,7 +23,7 @@ class ViewManager;
 class ViewManagerTransaction;
 
 // Manages the connection with the View Manager service.
-class ViewManagerSynchronizer : public IViewManagerClient {
+class ViewManagerSynchronizer : public InterfaceImpl<IViewManagerClient> {
  public:
   explicit ViewManagerSynchronizer(ViewManager* view_manager);
   virtual ~ViewManagerSynchronizer();
@@ -55,6 +51,8 @@ class ViewManagerSynchronizer : public IViewManagerClient {
   void SetBounds(TransportNodeId node_id, const gfx::Rect& bounds);
   void SetViewContents(TransportViewId view_id, const SkBitmap& contents);
 
+  void Embed(const String& url, TransportNodeId node_id);
+
   void set_changes_acked_callback(const base::Callback<void(void)>& callback) {
     changes_acked_callback_ = callback;
   }
@@ -65,6 +63,9 @@ class ViewManagerSynchronizer : public IViewManagerClient {
  private:
   friend class ViewManagerTransaction;
   typedef ScopedVector<ViewManagerTransaction> Transactions;
+
+  // Overridden from InterfaceImpl:
+  virtual void OnConnectionEstablished() OVERRIDE;
 
   // Overridden from IViewManagerClient:
   virtual void OnViewManagerConnectionEstablished(
@@ -97,7 +98,9 @@ class ViewManagerSynchronizer : public IViewManagerClient {
   // front of the queue.
   void RemoveFromPendingQueue(ViewManagerTransaction* transaction);
 
-  ViewManager* view_manager_;
+  ViewManager* view_manager() { return view_manager_.get(); }
+
+  scoped_ptr<ViewManager> view_manager_;
   bool connected_;
   TransportConnectionId connection_id_;
   uint16_t next_id_;
@@ -107,13 +110,9 @@ class ViewManagerSynchronizer : public IViewManagerClient {
 
   base::WeakPtrFactory<ViewManagerSynchronizer> sync_factory_;
 
-  // Non-NULL while blocking on the connection to |service_| during
-  // construction.
-  base::RunLoop* init_loop_;
-
   base::Callback<void(void)> changes_acked_callback_;
 
-  IViewManagerPtr service_;
+  IViewManager* service_;
 
   DISALLOW_COPY_AND_ASSIGN(ViewManagerSynchronizer);
 };
