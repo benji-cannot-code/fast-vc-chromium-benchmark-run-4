f35f637cbd82024fc768cb46a7085c29955216cd
reveman@chromium.org <reveman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-03 21:51:13 +0000

    skia: Avoid drawing of images completely in PaintSimplifier.
    Images are expensive to draw, especially when they need to be
    scaled. This skips draw operations with images completely to
    ensure that drawing using the PaintSimplifier has a predictable
    cost.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/310643004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274642 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/ext/paint_simplifier.cc
+++ skia/ext/paint_simplifier.cc
@@ -5,8 +5,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "skia/ext/paint_simplifier.h"
 #include "third_party/skia/include/core/SkPaint.h"
+#include "third_party/skia/include/core/SkShader.h"
 
 namespace skia {
+namespace {
+
+bool PaintHasBitmap(const SkPaint &paint) {
+  SkShader* shader = paint.getShader();
+  if (!shader)
+    return false;
+
+  if (shader->asAGradient(NULL) == SkShader::kNone_GradientType)
+    return false;
+
+  return shader->asABitmap(NULL, NULL, NULL) != SkShader::kNone_BitmapType;
+}
+
+}  // namespace
 
 PaintSimplifier::PaintSimplifier()
   : INHERITED() {
@@ -18,6 +33,9 @@ PaintSimplifier::~PaintSimplifier() {
 }
 
 bool PaintSimplifier::filter(SkPaint* paint, Type type) {
+  // Bitmaps are expensive. Skip draw if type has a bitmap.
+  if (type == kBitmap_Type || PaintHasBitmap(*paint))
+    return false;
 
   // Preserve a modicum of text quality; black & white text is
   // just too blocky, even during a fling.
