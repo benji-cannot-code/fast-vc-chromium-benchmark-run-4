2ef08f2ceb235f01424379e97b8ab505572ef46b
jackhou@chromium.org <jackhou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-03 13:25:17 +0000

    [Mac] Change --apps-keep-chrome-alive notification icon to the Chrome icon.
    Previously this had the default app icon (rounded rectangle).
    
    In future we may show the icon of the specific apps that are running.
    
    BUG=333429
    
    Review URL: https://codereview.chromium.org/316503002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274505 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/apps/quit_with_apps_controller_mac.cc
+++ chrome/browser/ui/cocoa/apps/quit_with_apps_controller_mac.cc
@@ -22,10 +22,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 #include "extensions/common/extension.h"
+#include "grit/chrome_unscaled_resources.h"
 #include "grit/chromium_strings.h"
 #include "grit/generated_resources.h"
 #include "grit/google_chrome_strings.h"
-#include "grit/theme_resources.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/l10n/l10n_util_mac.h"
 #include "ui/base/resource/resource_bundle.h"
@@ -59,7 +59,7 @@ QuitWithAppsController::QuitWithAppsController()
       l10n_util::GetStringUTF16(IDS_QUIT_WITH_APPS_TITLE),
       l10n_util::GetStringUTF16(IDS_QUIT_WITH_APPS_EXPLANATION),
       ui::ResourceBundle::GetSharedInstance().GetImageNamed(
-          IDR_APP_DEFAULT_ICON),
+          IDR_PRODUCT_LOGO_128),
       blink::WebTextDirectionDefault,
       message_center::NotifierId(message_center::NotifierId::SYSTEM_COMPONENT,
                                  kQuitWithAppsNotificationID),
