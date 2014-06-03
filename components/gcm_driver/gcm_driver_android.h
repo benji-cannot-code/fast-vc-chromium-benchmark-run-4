4fa082f0cdf4ad0a513bae721fe59a1d019c2a06
johnme@chromium.org <johnme@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-03 23:16:54 +0000

    JNI bindings for GCMDriverAndroid
    Bidirectional plumbing between C++ and Java.
    
    BUG=350378
    
    Review URL: https://codereview.chromium.org/312683004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274667 0039d316-1c4b-4281-b951-d872f2087c98

--- components/gcm_driver/gcm_driver_android.h
+++ components/gcm_driver/gcm_driver_android.h
@@ -8,13 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <jni.h>
 
+#include "base/android/scoped_java_ref.h"
 #include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "components/gcm_driver/gcm_driver.h"
 
 namespace gcm {
 
-// GCMDriver implementation for Android.
+// GCMDriver implementation for Android, using Android GCM APIs.
 class GCMDriverAndroid : public GCMDriver {
  public:
   GCMDriverAndroid();
@@ -41,10 +42,32 @@ class GCMDriverAndroid : public GCMDriver {
                                bool recording) OVERRIDE;
   virtual std::string SignedInUserName() const OVERRIDE;
 
+  // Methods called from Java via JNI:
+  void OnRegisterFinished(JNIEnv* env,
+                          jobject obj,
+                          jstring app_id,
+                          jstring registration_id,
+                          jboolean success);
+  void OnUnregisterFinished(JNIEnv* env,
+                           jobject obj,
+                           jstring app_id,
+                           jboolean success);
+  void OnMessageReceived(JNIEnv* env,
+                         jobject obj,
+                         jstring app_id,
+                         jstring sender_id,
+                         jstring collapse_key,
+                         jobjectArray data_keys_and_values);
+  void OnMessagesDeleted(JNIEnv* env,
+                         jobject obj,
+                         jstring app_id);
+
   // Register JNI methods.
   static bool RegisterBindings(JNIEnv* env);
 
  private:
+  base::android::ScopedJavaGlobalRef<jobject> java_ref_;
+
   DISALLOW_COPY_AND_ASSIGN(GCMDriverAndroid);
 };
 
