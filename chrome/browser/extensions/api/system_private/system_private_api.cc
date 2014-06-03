23dabffb6df3cf1e6328144fa174ef00cd91d0e7
merkulova@chromium.org <merkulova@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-03 13:24:37 +0000

    Added processing for a lost status to UpdateEngineClient.
    BUG=378865
    
    Review URL: https://codereview.chromium.org/309533006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274504 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/system_private/system_private_api.cc
+++ chrome/browser/extensions/api/system_private/system_private_api.cc
@@ -115,6 +115,7 @@ bool SystemPrivateGetUpdateStatusFunction::RunSync() {
       download_progress = 1;
       break;
     case chromeos::UpdateEngineClient::UPDATE_STATUS_REPORTING_ERROR_EVENT:
+    case chromeos::UpdateEngineClient::UPDATE_STATUS_ATTEMPTING_ROLLBACK:
       state = kNotAvailableState;
       break;
   }
