13b4db5dde18e200a07c072438182a908762a1fc
robert.bradford@intel.com <robert.bradford@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-03 19:59:34 +0000

    Include the display description in the picker on ChromeOS
    The DesktopMediaListAsh uses the AuraWindow's title as the description
    for the desktop source (window or screen.) To ensure that the display's
    description appears we need to set the root window's title to the
    display's name. This display name matches the name shown in the display
    configuration UI.
    
    BUG=372444
    TEST=In a multiple display environment trigger the picker and observe
    that the displays' names are included and match those in the display
    settings UI.
    
    Review URL: https://codereview.chromium.org/314733003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274599 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/display/display_controller.cc
+++ ash/display/display_controller.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/coordinate_conversion.h"
 #include "base/command_line.h"
 #include "base/strings/stringprintf.h"
+#include "base/strings/utf_string_conversions.h"
 #include "ui/aura/client/capture_client.h"
 #include "ui/aura/client/focus_client.h"
 #include "ui/aura/client/screen_position_client.h"
@@ -725,6 +726,7 @@ AshWindowTreeHost* DisplayController::AddWindowTreeHostForDisplay(
   aura::WindowTreeHost* host = ash_host->AsWindowTreeHost();
 
   host->window()->SetName(base::StringPrintf("RootWindow-%d", host_count++));
+  host->window()->set_title(base::UTF8ToUTF16(display_info.name()));
   host->compositor()->SetBackgroundColor(SK_ColorBLACK);
   // No need to remove our observer observer because the DisplayController
   // outlives the host.
