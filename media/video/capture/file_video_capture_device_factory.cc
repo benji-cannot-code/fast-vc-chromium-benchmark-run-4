51c20b6c14bbc3f4f73cc9b60fcffa1aaf709ec0
mcasas@chromium.org <mcasas@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-03 13:59:16 +0000

    VideoCaptureDeviceFactory: change device enumeration to callback + QTKit enumerates in UI thread.
    This CL changes the Enumeration to be based on 
    a callback passed to the VideoCaptureDeviceFactory.
    It is used for QTKit enumeration on UI thread:
    this is speculatively better than trying to run
    -performSelectorOnMainThread:withObject:waitUntilDone:
    
    -- Description --
    
    Currently VCM::EnumerateDevices() issues an internal 
    PostTaskAndReplyWithResult where the first part
    is GetAvailableDevicesInfoOnDeviceThread() on 
    DeviceThread and the second is 
    OnDevicesInfoEnumerated() in IO thread.
    
    This CL changes this to a simple PostTask to the 
    VCDFactory::EnumerateDeviceNames() with a callback
    to VCM::ConsolidateDevicesInfoOnDeviceThread().
    This method has a jump at the end to 
    VCM::OnDevicesInfoEnumerated() on IO thread. So
    what used to be a simple back-and-forth jump is
    now a 3 hop process. 
    
    This is used for QTKit to be able to override the
    VCDF::EnumerateDeviceNames and run the device
    enumeration in UI thread. 
    
    Note 1: The UI thread is currently passed in Create()
    method for all implementations. This is changed to
    earlier: on VCDF::CreateFactory(); injected from 
    MediaStreamManager; and only used in VCDFLinux 
    (for ChromeOS) and in VCDFMac for QTKit enumeration.
    This approach is cleaner since we don't send UI thread
    references to derived classes that don't need it.
    
    Note 2: VideoCaptureDeviceFactoryMac::Create()
    used to have a search-and-find for |device_id| that
    is removed; the search was a precaution for devices
    that disappeared between first enumeration and use.
    However, using a |device_name| that is gone would
    just cause a fail on AllocateAndStart() anyhow, and
    enumerating twice is very expensive.
     For compatibility reasons, this behaviour is kept
    for AVFoundation devices and blacklisted QTKit
    devices (that behave as a subset of AVF-type ones).
    
    
    
    BUG=255552, 115327
    
    Review URL: https://codereview.chromium.org/294893006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274518 0039d316-1c4b-4281-b951-d872f2087c98

--- media/video/capture/file_video_capture_device_factory.cc
+++ media/video/capture/file_video_capture_device_factory.cc
@@ -26,8 +26,8 @@ base::FilePath GetFilePathFromCommandLine() {
 }
 
 scoped_ptr<VideoCaptureDevice> FileVideoCaptureDeviceFactory::Create(
-    scoped_refptr<base::SingleThreadTaskRunner> ui_task_runner,
     const VideoCaptureDevice::Name& device_name) {
+  DCHECK(thread_checker_.CalledOnValidThread());
 #if defined(OS_WIN)
   return scoped_ptr<VideoCaptureDevice>(new FileVideoCaptureDevice(
       base::FilePath(base::SysUTF8ToWide(device_name.name()))));
@@ -39,6 +39,7 @@ scoped_ptr<VideoCaptureDevice> FileVideoCaptureDeviceFactory::Create(
 
 void FileVideoCaptureDeviceFactory::GetDeviceNames(
     VideoCaptureDevice::Names* const device_names) {
+  DCHECK(thread_checker_.CalledOnValidThread());
   DCHECK(device_names->empty());
   base::FilePath command_line_file_path = GetFilePathFromCommandLine();
 #if defined(OS_WIN)
@@ -55,6 +56,7 @@ void FileVideoCaptureDeviceFactory::GetDeviceNames(
 void FileVideoCaptureDeviceFactory::GetDeviceSupportedFormats(
     const VideoCaptureDevice::Name& device,
     VideoCaptureFormats* supported_formats) {
+  DCHECK(thread_checker_.CalledOnValidThread());
   base::File file =
       FileVideoCaptureDevice::OpenFileForRead(GetFilePathFromCommandLine());
   VideoCaptureFormat capture_format;
