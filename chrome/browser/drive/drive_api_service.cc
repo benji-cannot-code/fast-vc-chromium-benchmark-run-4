7539e2fefff0071b37c6d57ac3d56bf08a139fd8
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-03 08:55:11 +0000

    [Drive] Fetch id field of the parents in FileResource.
    BUG=379667
    
    Review URL: https://codereview.chromium.org/306073007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274437 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/drive/drive_api_service.cc
+++ chrome/browser/drive/drive_api_service.cc
@@ -106,7 +106,7 @@ const char kFileResourceFields[] =
     "kind,id,title,createdDate,sharedWithMeDate,mimeType,"
     "md5Checksum,fileSize,labels/trashed,imageMediaMetadata/width,"
     "imageMediaMetadata/height,imageMediaMetadata/rotation,etag,"
-    "parents/parentLink,alternateLink,"
+    "parents(id,parentLink),alternateLink,"
     "modifiedDate,lastViewedByMeDate,shared";
 const char kFileResourceOpenWithLinksFields[] =
     "kind,id,openWithLinks/*";
@@ -114,14 +114,15 @@ const char kFileListFields[] =
     "kind,items(kind,id,title,createdDate,sharedWithMeDate,"
     "mimeType,md5Checksum,fileSize,labels/trashed,imageMediaMetadata/width,"
     "imageMediaMetadata/height,imageMediaMetadata/rotation,etag,"
-    "parents/parentLink,alternateLink,"
+    "parents(id,parentLink),alternateLink,"
     "modifiedDate,lastViewedByMeDate,shared),nextLink";
 const char kChangeListFields[] =
     "kind,items(file(kind,id,title,createdDate,sharedWithMeDate,"
     "mimeType,md5Checksum,fileSize,labels/trashed,imageMediaMetadata/width,"
     "imageMediaMetadata/height,imageMediaMetadata/rotation,etag,"
-    "parents/parentLink,alternateLink,modifiedDate,lastViewedByMeDate,shared),"
-    "deleted,id,fileId,modificationDate),nextLink,largestChangeId";
+    "parents(id,parentLink),alternateLink,modifiedDate,"
+    "lastViewedByMeDate,shared),deleted,id,fileId,modificationDate),nextLink,"
+    "largestChangeId";
 
 // Converts the FileResource value to ResourceEntry and runs |callback| on the
 // UI thread.
