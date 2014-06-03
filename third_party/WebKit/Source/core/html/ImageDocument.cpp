974946833abb76e208674f6bc7ebf373bd5e3796
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-03 15:05:33 +0000

    Speculative fix for ImageDocument crash.
    HTMLImageElement::cachedImage can be null.
    This CL has no tests. Unable to reproduce the crash of crbug.com/379720
    locally.
    
    BUG=379720
    
    Review URL: https://codereview.chromium.org/314633002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175375 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/ImageDocument.cpp
+++ third_party/WebKit/Source/core/html/ImageDocument.cpp
@@ -129,7 +129,8 @@ void ImageDocumentParser::appendBytes(const char* data, size_t length)
     if (!frame->loader().client()->allowImage(!settings || settings->imagesEnabled(), document()->url()))
         return;
 
-    document()->cachedImage()->appendData(data, length);
+    if (document()->cachedImage())
+        document()->cachedImage()->appendData(data, length);
     // Make sure the image renderer gets created because we need the renderer
     // to read the aspect ratio. See crbug.com/320244
     document()->updateRenderTreeIfNeeded();
@@ -138,7 +139,7 @@ void ImageDocumentParser::appendBytes(const char* data, size_t length)
 
 void ImageDocumentParser::finish()
 {
-    if (!isStopped() && document()->imageElement()) {
+    if (!isStopped() && document()->imageElement() && document()->cachedImage()) {
         ImageResource* cachedImage = document()->cachedImage();
         cachedImage->finish();
         cachedImage->setResponse(document()->frame()->loader().documentLoader()->response());
