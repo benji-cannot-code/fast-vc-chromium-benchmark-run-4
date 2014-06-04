5dd50bcb56b51ac97ad948b509c28f996ae83821
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-04 22:34:12 +0000

    Add requestor_url to ConnectToService()
    R=darin@chromium.org, darin
    BUG=
    
    Review URL: https://codereview.chromium.org/318773002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274937 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/service_manager/service_manager.cc
+++ mojo/service_manager/service_manager.cc
@@ -30,17 +30,23 @@ class ServiceManager::ServiceFactory : public InterfaceImpl<ServiceProvider> {
   virtual ~ServiceFactory() {
   }
 
-  void ConnectToClient(const std::string& name,
-                       ScopedMessagePipeHandle handle) {
-    if (handle.is_valid())
-      client()->ConnectToService(url_.spec(), name, handle.Pass());
+  void ConnectToClient(const std::string& service_name,
+                       ScopedMessagePipeHandle handle,
+                       const GURL& requestor_url) {
+    if (handle.is_valid()) {
+      client()->ConnectToService(
+          url_.spec(), service_name, handle.Pass(), requestor_url.spec());
+    }
   }
 
   // ServiceProvider implementation:
-  virtual void ConnectToService(const String& url,
-                                const String& name,
-                                ScopedMessagePipeHandle client_pipe) OVERRIDE {
-    manager_->ConnectToService(GURL(url), name, client_pipe.Pass());
+  virtual void ConnectToService(const String& service_url,
+                                const String& service_name,
+                                ScopedMessagePipeHandle client_pipe,
+                                const String& requestor_url) OVERRIDE {
+    // Ignore provided requestor_url and use url from connection.
+    manager_->ConnectToService(
+        GURL(service_url), service_name, client_pipe.Pass(), url_);
   }
 
   const GURL& url() const { return url_; }
@@ -68,10 +74,14 @@ class ServiceManager::TestAPI::TestServiceProviderConnection
   }
 
   // ServiceProvider:
-  virtual void ConnectToService(const String& url,
-                                const String& name,
-                                ScopedMessagePipeHandle client_pipe) OVERRIDE {
-    manager_->ConnectToService(GURL(url), name, client_pipe.Pass());
+  virtual void ConnectToService(const String& service_url,
+                                const String& service_name,
+                                ScopedMessagePipeHandle client_pipe,
+                                const String& requestor_url) OVERRIDE {
+    manager_->ConnectToService(GURL(service_url),
+                               service_name,
+                               client_pipe.Pass(),
+                               GURL(requestor_url));
   }
 
  private:
@@ -124,7 +134,8 @@ ServiceManager* ServiceManager::GetInstance() {
 
 void ServiceManager::ConnectToService(const GURL& url,
                                       const std::string& name,
-                                      ScopedMessagePipeHandle client_handle) {
+                                      ScopedMessagePipeHandle client_handle,
+                                      const GURL& requestor_url) {
   URLToServiceFactoryMap::const_iterator service_it =
       url_to_service_factory_.find(url);
   ServiceFactory* service_factory;
@@ -141,9 +152,11 @@ void ServiceManager::ConnectToService(const GURL& url,
   }
   if (interceptor_) {
     service_factory->ConnectToClient(
-        name, interceptor_->OnConnectToClient(url, client_handle.Pass()));
+        name,
+        interceptor_->OnConnectToClient(url, client_handle.Pass()),
+        requestor_url);
   } else {
-    service_factory->ConnectToClient(name, client_handle.Pass());
+    service_factory->ConnectToClient(name, client_handle.Pass(), requestor_url);
   }
 }
 
