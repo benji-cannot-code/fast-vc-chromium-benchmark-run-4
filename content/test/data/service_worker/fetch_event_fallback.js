6d1fa13d53e3932426510689124d4b23349348fe
falken@chromium.org <falken@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-04 21:52:32 +0000

    ServiceWorker: Read the blob from SW's response to a fetch event
    Also remove browser tests for the fetch event that will be replaced with layout
    tests in the next Blink patch. Since layout tests can't test HTTP response
    headers, one of the browser tests remains.
    
    Multi-sided patch to implement blob-type fetch event
    response bodies:
    1: blink-side: https://codereview.chromium.org/307063002/
    2: chromium-side, THIS PATCH
    3: blink-side: https://codereview.chromium.org/304233017
    
    Originally landed in r274773 and reverted in r274799 for breaking ASAN.
    Relanding after fixing a buffer overflow caused by using the wrong
    std::string ctor.
    
    BUG=379074
    TBR=michaeln
    
    Review URL: https://codereview.chromium.org/315023003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274915 0039d316-1c4b-4281-b951-d872f2087c98

--- content/test/data/service_worker/fetch_event_fallback.js
+++
@@ -1,9 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-this.onfetch = function(event) {
-    event.respondWith('This is not a Response object, so browser should ' +
-        'fallback to native');
-};
--- content/test/data/service_worker/sync.js
+++ content/test/data/service_worker/sync.js
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 var code = 404;
 
 this.onfetch = function(event) {
-    response = new Response({
+    response = new Response(new Blob(), {
         status: code,
         statusText: 'OK'
     });
