f51dd70445dfb1a9d595a190bddc68503333236f
tbarzic@chromium.org <tbarzic@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-04 11:20:21 +0000

    Fix a crash when chooseDesktopMedia is called with no target tab
    DesktopMediaPicker was getting created with web_contents returned by
    web_contents(), which is part of WebContentsObserver interface,
    and which does not get set when the target tab is not specified.
    
    Also, fix handling of a case when the method is called when there are
    no web contents to attach the picker dialog to.
    
    BUG=375576,364050
    TEST=from a extension with desktopCapture permission:
    chrome.desktopCapture.chooseDesktopMedia(['screen'], undefined, function())
    does not crash
    
    Review URL: https://codereview.chromium.org/307453010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274771 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/desktop_capture/desktop_capture_api.cc
+++ chrome/browser/extensions/api/desktop_capture/desktop_capture_api.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/api/desktop_capture/desktop_capture_api.h"
 
+#include "ash/shell.h"
 #include "base/command_line.h"
 #include "base/compiler_specific.h"
 #include "base/strings/utf_string_conversions.h"
@@ -14,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/media/media_capture_devices_dispatcher.h"
 #include "chrome/browser/media/native_desktop_media_list.h"
 #include "chrome/browser/ui/ash/ash_util.h"
+#include "chrome/browser/ui/host_desktop.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/extensions/api/tabs.h"
 #include "content/public/browser/render_process_host.h"
@@ -89,8 +91,9 @@ bool DesktopCaptureChooseDesktopMediaFunction::RunAsync() {
   DesktopCaptureRequestsRegistry::GetInstance()->AddRequest(
       render_view_host()->GetProcess()->GetID(), request_id_, this);
 
-  gfx::NativeWindow parent_window;
-  content::RenderViewHost* render_view;
+  gfx::NativeWindow parent_window = NULL;
+  content::RenderViewHost* render_view = NULL;
+  content::WebContents* web_contents = NULL;
   base::string16 target_name;
   if (params->target_tab) {
     if (!params->target_tab->url) {
@@ -113,7 +116,6 @@ bool DesktopCaptureChooseDesktopMediaFunction::RunAsync() {
       return false;
     }
 
-    content::WebContents* web_contents = NULL;
     if (!ExtensionTabUtil::GetTabById(*(params->target_tab->id), GetProfile(),
                                       true, NULL, NULL, &web_contents, NULL)) {
       error_ = kInvalidTabIdError;
@@ -136,9 +138,18 @@ bool DesktopCaptureChooseDesktopMediaFunction::RunAsync() {
     origin_ = GetExtension()->url();
     target_name = base::UTF8ToUTF16(GetExtension()->name());
     render_view = render_view_host();
-    parent_window =
-        GetAssociatedWebContents()->GetTopLevelNativeWindow();
+
+    web_contents = GetAssociatedWebContents();
+    if (web_contents) {
+      parent_window = web_contents->GetTopLevelNativeWindow();
+    } else {
+#if defined(USE_ASH)
+      if (chrome::GetActiveDesktop() == chrome::HOST_DESKTOP_TYPE_ASH)
+        parent_window = ash::Shell::GetPrimaryRootWindow();
+#endif
+    }
   }
+
   render_process_id_ = render_view->GetProcess()->GetID();
   render_view_id_ = render_view->GetRoutingID();
 
@@ -209,7 +220,7 @@ bool DesktopCaptureChooseDesktopMediaFunction::RunAsync() {
   DesktopMediaPicker::DoneCallback callback = base::Bind(
       &DesktopCaptureChooseDesktopMediaFunction::OnPickerDialogResults, this);
 
-  picker_->Show(web_contents(),
+  picker_->Show(web_contents,
                 parent_window, parent_window,
                 base::UTF8ToUTF16(GetExtension()->name()),
                 target_name,
