ad298729cf3855ed460f980f0a003b5ba08938e6
eustas@chromium.org <eustas@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-04 09:55:33 +0000

    DevTools: Preserve tab selection in NetworkItemView
    BUG=380541
    
    Review URL: https://codereview.chromium.org/311073002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175466 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ui/TabbedPane.js
+++ third_party/WebKit/Source/devtools/front_end/ui/TabbedPane.js
@@ -300,15 +300,16 @@ WebInspector.TabbedPane.prototype = {
     /**
      * @param {string} id
      * @param {boolean=} userGesture
+     * @return {boolean}
      */
     selectTab: function(id, userGesture)
     {
         var focused = this.hasFocus();
         var tab = this._tabsById[id];
         if (!tab)
-            return;
+            return false;
         if (this._currentTab && this._currentTab.id === id)
-            return;
+            return true;
 
         this._hideCurrentTab();
         this._showTab(tab);
@@ -323,6 +324,7 @@ WebInspector.TabbedPane.prototype = {
 
         var eventData = { tabId: id, view: tab.view, isUserGesture: userGesture };
         this.dispatchEventToListeners(WebInspector.TabbedPane.EventTypes.TabSelected, eventData);
+        return true;
     },
 
     /**
