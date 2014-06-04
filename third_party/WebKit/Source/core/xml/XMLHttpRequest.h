075492c961983a64bb2be395d32e58b9b6ca02a1
yhirano@chromium.org <yhirano@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-04 01:51:45 +0000

    Introduce XMLHttpRequest.responseURL.
    Introduce XMLHttpRequest.responseURL as specified in [1].
    
    1: http://xhr.spec.whatwg.org/#the-responseurl-attribute
    
    BUG=377583
    
    Review URL: https://codereview.chromium.org/312493004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175422 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/xml/XMLHttpRequest.h
+++ third_party/WebKit/Source/core/xml/XMLHttpRequest.h
@@ -135,6 +135,8 @@ public:
     String responseType();
     ResponseTypeCode responseTypeCode() const { return m_responseTypeCode; }
 
+    String responseURL();
+
     // response attribute has custom getter.
     ArrayBuffer* responseArrayBuffer();
 
