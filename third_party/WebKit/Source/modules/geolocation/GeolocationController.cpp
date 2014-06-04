d30058574fb3ea85b81d3e39ffd6cb6e13070cb2
jam@chromium.org <jam@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-04 19:45:44 +0000

    Fix use-after-free in geolocation layout test code introduced in r175404.
    BUG=380588
    TBR=ojan
    
    Review URL: https://codereview.chromium.org/319503004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175510 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/geolocation/GeolocationController.cpp
+++ third_party/WebKit/Source/modules/geolocation/GeolocationController.cpp
@@ -182,6 +182,8 @@ GeolocationPosition* GeolocationController::lastPosition()
 
 void GeolocationController::setClientForTest(GeolocationClient* client)
 {
+    if (m_hasClientForTest)
+        m_client->controllerForTestRemoved(this);
     m_client = client;
     m_hasClientForTest = true;
 
