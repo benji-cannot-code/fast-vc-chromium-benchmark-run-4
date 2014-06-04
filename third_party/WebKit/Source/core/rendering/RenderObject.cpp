4afd88376dde97d2f4b155862d73b55dbc32c8d7
leviw@chromium.org <leviw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-04 01:59:03 +0000

    [RAL] Fix nested non-composited RenderView repainting
    When a nested RenderView isn't composited, it acts like a repaint
    boundary, but then relies on repaintViewRectangle to translate
    the repaint coordinates into the coordinate space of the actual
    repaint container. This patch overrides repaintTreeAfterLayout for
    RenderView to simply repaint the viewRect. It also pushes some
    repaint logic out of RenderObject and into RenderView.
    
    BUG=370366
    
    Review URL: https://codereview.chromium.org/271803002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175423 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderObject.cpp
+++ third_party/WebKit/Source/core/rendering/RenderObject.cpp
@@ -1537,11 +1537,8 @@ void RenderObject::repaintUsingContainer(const RenderLayerModelObject* repaintCo
     RenderView* v = view();
     if (repaintContainer->isRenderView()) {
         ASSERT(repaintContainer == v);
-        bool viewHasCompositedLayer = v->hasLayer() && v->layer()->compositingState() == PaintsIntoOwnBacking;
-        if (!viewHasCompositedLayer) {
-            v->repaintViewRectangle(r);
-            return;
-        }
+        v->repaintViewRectangle(r);
+        return;
     }
 
     if (v->usesCompositing()) {
