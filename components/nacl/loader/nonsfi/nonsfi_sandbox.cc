e495431a0b5c0f43c6e76842c0228a2011af5c6f
hamaji@chromium.org <hamaji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-04 12:16:28 +0000

    Non-SFI NaCl: Allow CLOCK_SYSTEM_TRACE on Chrome OS
    chrome://tracing uses TimeTicks::NowFromSystemTraceTime in
    base/time/time_posix.cc.
    
    TEST=nacl_loader_unittests  # on i686 and ARM Chrome OS
    TEST=trybot
    BUG=378063
    
    Review URL: https://codereview.chromium.org/298163008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274784 0039d316-1c4b-4281-b951-d872f2087c98

--- components/nacl/loader/nonsfi/nonsfi_sandbox.cc
+++ components/nacl/loader/nonsfi/nonsfi_sandbox.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/logging.h"
+#include "base/time/time.h"
 #include "build/build_config.h"
 #include "content/public/common/sandbox_init.h"
 #include "sandbox/linux/seccomp-bpf-helpers/sigsys_handlers.h"
@@ -81,19 +82,26 @@ ErrorCode RestrictClockID(SandboxBPF* sb) {
   // returned by {clock,pthread}_getcpuclockid), which can leak information
   // about the state of the host OS.
   COMPILE_ASSERT(4 == sizeof(clockid_t), clockid_is_not_32bit);
-  return sb->Cond(0, ErrorCode::TP_32BIT,
-                  ErrorCode::OP_EQUAL, CLOCK_MONOTONIC,
-                  ErrorCode(ErrorCode::ERR_ALLOWED),
-         sb->Cond(0, ErrorCode::TP_32BIT,
-                  ErrorCode::OP_EQUAL, CLOCK_PROCESS_CPUTIME_ID,
-                  ErrorCode(ErrorCode::ERR_ALLOWED),
-         sb->Cond(0, ErrorCode::TP_32BIT,
-                  ErrorCode::OP_EQUAL, CLOCK_REALTIME,
-                  ErrorCode(ErrorCode::ERR_ALLOWED),
-         sb->Cond(0, ErrorCode::TP_32BIT,
-                  ErrorCode::OP_EQUAL, CLOCK_THREAD_CPUTIME_ID,
-                  ErrorCode(ErrorCode::ERR_ALLOWED),
-         sb->Trap(sandbox::CrashSIGSYS_Handler, NULL)))));
+  ErrorCode result = sb->Cond(0, ErrorCode::TP_32BIT,
+                              ErrorCode::OP_EQUAL, CLOCK_MONOTONIC,
+                              ErrorCode(ErrorCode::ERR_ALLOWED),
+                     sb->Cond(0, ErrorCode::TP_32BIT,
+                              ErrorCode::OP_EQUAL, CLOCK_PROCESS_CPUTIME_ID,
+                              ErrorCode(ErrorCode::ERR_ALLOWED),
+                     sb->Cond(0, ErrorCode::TP_32BIT,
+                              ErrorCode::OP_EQUAL, CLOCK_REALTIME,
+                              ErrorCode(ErrorCode::ERR_ALLOWED),
+                     sb->Cond(0, ErrorCode::TP_32BIT,
+                              ErrorCode::OP_EQUAL, CLOCK_THREAD_CPUTIME_ID,
+                              ErrorCode(ErrorCode::ERR_ALLOWED),
+                     sb->Trap(sandbox::CrashSIGSYS_Handler, NULL)))));
+#if defined(OS_CHROMEOS)
+  // Allow the special clock for Chrome OS used by Chrome tracing.
+  result = sb->Cond(0, ErrorCode::TP_32BIT,
+                    ErrorCode::OP_EQUAL, base::TimeTicks::kClockSystemTrace,
+                    ErrorCode(ErrorCode::ERR_ALLOWED), result);
+#endif
+  return result;
 }
 
 ErrorCode RestrictClone(SandboxBPF* sb) {
