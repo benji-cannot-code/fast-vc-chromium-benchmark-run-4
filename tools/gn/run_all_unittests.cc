af76a2275928f47e0d31c47aad3100dedb96c0cc
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-04 16:57:40 +0000

    Make GN unit tests single process.
    Clean up gin run all unittests file. I noticed some extra includes while searching for some code to copy.
    
    BUG=
    R=jochen@chromium.org
    
    Review URL: https://codereview.chromium.org/318543002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274850 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/gn/run_all_unittests.cc
+++ tools/gn/run_all_unittests.cc
@@ -1,16 +1,14 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2013 The Chromium Authors. All rights reserved.
+// Copyright 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "base/bind.h"
-#include "base/test/launcher/unit_test_launcher.h"
 #include "base/test/test_suite.h"
 
+// Don't use the multiprocess test harness. This test suite is fast enough and
+// it makes the output more difficult to read.
 int main(int argc, char** argv) {
   base::TestSuite test_suite(argc, argv);
-
-  return base::LaunchUnitTests(
-      argc, argv, base::Bind(&base::TestSuite::Run,
-                             base::Unretained(&test_suite)));
+  return test_suite.Run();
 }
