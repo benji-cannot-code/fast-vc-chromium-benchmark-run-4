d8754b0cb66956155077a057ba599e08cc517f37
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-04 11:30:29 +0000

    Add TextfieldTest.OnKeyPressBinding, which requires r273791.
    Fulfills the unit test request in the review of r273791.
    https://codereview.chromium.org/307783003#msg3
    
    Use a no-op TextEditKeyBindingsDelegateAuraLinux.
    (replicates Emacs bindings ignoring [Shift+]Ctrl+Z)
    Minor cleanup and test renaming.
    
    BUG=377355
    TEST=NONE
    R=oshima@chromium.org
    
    Review URL: https://codereview.chromium.org/310283002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274777 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/textfield/textfield_unittest.cc
+++ ui/views/controls/textfield/textfield_unittest.cc
@@ -38,6 +38,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/windows_version.h"
 #endif
 
+#if defined(OS_LINUX) && !defined(OS_CHROMEOS)
+#include "ui/events/linux/text_edit_key_bindings_delegate_auralinux.h"
+#endif
+
 using base::ASCIIToUTF16;
 using base::UTF8ToUTF16;
 using base::WideToUTF16;
@@ -141,12 +145,6 @@ class TextfieldTest : public ViewsTestBase, public TextfieldController {
     // paste action did not change the content. So |new_contents| may match
     // |last_contents_|. For more info, see http://crbug.com/79002
     last_contents_ = new_contents;
-  }
-
-  virtual bool HandleKeyEvent(Textfield* sender,
-                              const ui::KeyEvent& key_event) OVERRIDE {
-    // TODO(oshima): figure out how to test the keystroke.
-    return false;
   }
 
   virtual void OnBeforeUserAction(Textfield* sender) OVERRIDE {
@@ -512,7 +510,7 @@ TEST_F(TextfieldTest, TextInputType) {
   EXPECT_EQ(ui::TEXT_INPUT_TYPE_PASSWORD, textfield_->GetTextInputType());
 }
 
-TEST_F(TextfieldTest, OnKeyPressReturnValueTest) {
+TEST_F(TextfieldTest, OnKeyPress) {
   InitTextfield();
 
   // Character keys are handled by the input method.
@@ -557,6 +555,53 @@ TEST_F(TextfieldTest, OnKeyPressReturnValueTest) {
   EXPECT_TRUE(textfield_->key_received());
   EXPECT_FALSE(textfield_->key_handled());
   textfield_->clear();
+}
+
+// Tests that default key bindings are handled even with a delegate installed.
+TEST_F(TextfieldTest, OnKeyPressBinding) {
+  InitTextfield();
+
+#if defined(OS_LINUX) && !defined(OS_CHROMEOS)
+  // Install a TextEditKeyBindingsDelegateAuraLinux that does nothing.
+  class TestDelegate : public ui::TextEditKeyBindingsDelegateAuraLinux {
+   public:
+    TestDelegate() {}
+    virtual ~TestDelegate() {}
+
+    virtual bool MatchEvent(
+        const ui::Event& event,
+        std::vector<ui::TextEditCommandAuraLinux>* commands) OVERRIDE {
+      return false;
+    }
+
+   private:
+    DISALLOW_COPY_AND_ASSIGN(TestDelegate);
+  };
+
+  TestDelegate delegate;
+  ui::SetTextEditKeyBindingsDelegate(&delegate);
+#endif
+
+  SendKeyEvent(ui::VKEY_A, false, false);
+  EXPECT_STR_EQ("a", textfield_->text());
+  textfield_->clear();
+
+  // Undo/Redo command keys are handled by the textfield.
+  SendKeyEvent(ui::VKEY_Z, false, true);
+  EXPECT_TRUE(textfield_->key_received());
+  EXPECT_TRUE(textfield_->key_handled());
+  EXPECT_TRUE(textfield_->text().empty());
+  textfield_->clear();
+
+  SendKeyEvent(ui::VKEY_Z, true, true);
+  EXPECT_TRUE(textfield_->key_received());
+  EXPECT_TRUE(textfield_->key_handled());
+  EXPECT_STR_EQ("a", textfield_->text());
+  textfield_->clear();
+
+#if defined(OS_LINUX) && !defined(OS_CHROMEOS)
+  ui::SetTextEditKeyBindingsDelegate(NULL);
+#endif
 }
 
 TEST_F(TextfieldTest, CursorMovement) {
