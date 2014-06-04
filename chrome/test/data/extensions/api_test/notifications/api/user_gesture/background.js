1f4b5aa2525011891f3a40c285aa9073689134ea
dewittj@chromium.org <dewittj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-04 08:40:41 +0000

    Reland: Allow high-res bitmaps to be passed in from notifications API.
    Previously the custom binding in JS would prevent an image that's larger
    than the size of the template from being sent to the browser process.
    This relaxes the maximum to be the largest supported scale factor of the machine.
    
    TBR=kalman@chromium.org,dimich@chromium.org
    BUG=239676
    
    Review URL: https://codereview.chromium.org/291193009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274730 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/api_test/notifications/api/user_gesture/background.js
+++ chrome/test/data/extensions/api_test/notifications/api/user_gesture/background.js
@@ -3,6 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+const red_dot = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUA" +
+    "AAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO" +
+    "9TXL0Y4OHwAAAABJRU5ErkJggg==";
+
 function userActionTest() {
   chrome.test.sendMessage("");
 }
@@ -12,7 +16,7 @@ chrome.notifications.onButtonClicked.addListener(userActionTest);
 chrome.notifications.onClosed.addListener(userActionTest);
 
 chrome.notifications.create("test", {
-  iconUrl: "",
+  iconUrl: red_dot,
   title: "hi",
   message: "there",
   type: "basic",
