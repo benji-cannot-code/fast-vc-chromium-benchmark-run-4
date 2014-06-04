9286644b282f544ad749e8405d88097a7f947b71
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-04 02:09:10 +0000

    Revert 258215 "Remove muting for extreme playbackRates."
    It exposed a bug in the WSOLA algorithm that causes it to incorrectly
    trigger underflows.
    
    > Remove muting for extreme playbackRates.
    > 
    > Audio was muted below 0.5x and above 4x as the quality degraded
    > significantly under the crossfade algorithm. The quality is now much
    > better under the WSLOA algorithm (r220343).
    > 
    > BUG=289354
    > R=scherkus@chromium.org
    > R=dalecurtis@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/205093002
    
    BUG=368083
    TBR=sandersd@chromium.org
    
    Review URL: https://codereview.chromium.org/312963003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274701 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/audio_renderer_algorithm.h
+++ media/filters/audio_renderer_algorithm.h
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // are preserved. See audio_renderer_algorith.cc for a more elaborate
 // description of the algorithm.
 //
+// Audio at very low or very high playback rates are muted to preserve quality.
+//
 
 #ifndef MEDIA_FILTERS_AUDIO_RENDERER_ALGORITHM_H_
 #define MEDIA_FILTERS_AUDIO_RENDERER_ALGORITHM_H_
@@ -83,6 +85,9 @@ class MEDIA_EXPORT AudioRendererAlgorithm {
   // Returns the samples per second for this audio stream.
   int samples_per_second() { return samples_per_second_; }
 
+  // Is the sound currently muted?
+  bool is_muted() { return muted_; }
+
  private:
   // Within |search_block_|, find the block of data that is most similar to
   // |target_block_|, and write it in |optimal_block_|. This method assumes that
@@ -135,6 +140,12 @@ class MEDIA_EXPORT AudioRendererAlgorithm {
 
   // Buffered audio data.
   AudioBufferQueue audio_buffer_;
+
+  // True if the audio should be muted.
+  bool muted_;
+
+  // If muted, keep track of partial frames that should have been skipped over.
+  double muted_partial_frame_;
 
   // How many frames to have in the queue before we report the queue is full.
   int capacity_;
