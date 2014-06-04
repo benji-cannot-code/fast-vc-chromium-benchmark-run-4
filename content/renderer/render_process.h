67b258f0b059df0c3b3c24406391a6311929ab08
simonhong@chromium.org <simonhong@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-04 14:01:24 +0000

    Remove unused code in the RenderProcess[Impl]
    R=jamesr@chromium.org
    BUG=NONE
    TEST=NONE(No functional change)
    
    Review URL: https://codereview.chromium.org/310733003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274814 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_process.h
+++ content/renderer/render_process.h
@@ -7,13 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_RENDERER_RENDER_PROCESS_H_
 
 #include "content/child/child_process.h"
-#include "skia/ext/platform_canvas.h"
-
-class TransportDIB;
-
-namespace gfx {
-class Rect;
-}
 
 namespace content {
 
