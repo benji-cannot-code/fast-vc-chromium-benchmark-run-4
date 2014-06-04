a038372ef71de328aee49c3416503e4eb573f2b9
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-04 09:02:47 +0000

    Oilpan: Replace RefPtrs to Node and its subclasses in core/rendering with Oilpan transition types.
    BUG=357163
    
    Review URL: https://codereview.chromium.org/318583002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175457 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/FastTextAutosizer.cpp
+++ third_party/WebKit/Source/core/rendering/FastTextAutosizer.cpp
@@ -56,7 +56,7 @@ namespace WebCore {
 #ifdef AUTOSIZING_DOM_DEBUG_INFO
 class WriteDebugInfoTask : public ExecutionContextTask {
 public:
-    WriteDebugInfoTask(PassRefPtr<Element> element, AtomicString value)
+    WriteDebugInfoTask(PassRefPtrWillBeRawPtr<Element> element, AtomicString value)
         : m_element(element)
         , m_value(value)
     {
@@ -68,7 +68,7 @@ public:
     }
 
 private:
-    RefPtr<Element> m_element;
+    RefPtrWillBePersistent<Element> m_element;
     AtomicString m_value;
 };
 
