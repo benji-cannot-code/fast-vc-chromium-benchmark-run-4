cc9760fcd1207a98c8e1c96fcf7d550eba197f91
teravest@chromium.org <teravest@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-05 19:40:40 +0000

    Pepper: Clean up trusted plugin error reporting.
    This change cleans up some error reporting and removes some unnecessary and
    dead code. Not a very exciting change; just doing more cleanup.
    
    BUG=239656
    
    Review URL: https://codereview.chromium.org/311273007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@275220 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/c/private/ppb_nacl_private.h
+++ ppapi/c/private/ppb_nacl_private.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * found in the LICENSE file.
  */
 
-/* From private/ppb_nacl_private.idl modified Wed Jun  4 09:50:07 2014. */
+/* From private/ppb_nacl_private.idl modified Thu Jun  5 08:59:03 2014. */
 
 #ifndef PPAPI_C_PRIVATE_PPB_NACL_PRIVATE_H_
 #define PPAPI_C_PRIVATE_PPB_NACL_PRIVATE_H_
@@ -350,8 +350,7 @@ struct PPB_NaCl_Private_1_0 {
   /* Report an error that occured while attempting to load a nexe. */
   void (*ReportLoadError)(PP_Instance instance,
                           PP_NaClError error,
-                          const char* error_message,
-                          const char* console_message);
+                          const char* error_message);
   /* Reports that loading a nexe was aborted. */
   void (*ReportLoadAbort)(PP_Instance instance);
   /* Reports that the nexe has crashed. */
