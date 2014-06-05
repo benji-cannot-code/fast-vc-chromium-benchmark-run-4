c79357fd978dfcb5bf40d413e01b7dc8eb0741b3
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-05 15:33:36 +0000

    DevTools: [JsDocValidator] Check validity of @param and @return annotations
    R=vsevik,sergeyv
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/315213003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175579 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/scripts/jsdoc-validator/tests/function.js
+++ third_party/WebKit/Source/devtools/scripts/jsdoc-validator/tests/function.js
@@ -35,6 +35,24 @@ function badReturnsShouldBeReturnFunc() // ERROR - @returns, should be @return.
 }
 
 /**
+ * @return number
+ */
+function badReturnShouldBeTypedFunc() // ERROR - number, not {number}.
+{
+    return 1;
+}
+
+/**
+ * @param number foo
+ * @param bar
+ */
+function badParamAnnotationsFunc(foo, bar) // ERROR - @param's should be well-formed
+{
+    return 1;
+}
+
+
+/**
  * @returns {number}
  */
 function badReturnsShouldBeReturnNoValueFunc() // ERROR - @returns, should be @return.
@@ -274,6 +292,14 @@ TypeThree.prototype = {
     },
 
     /**
+     * @returns number
+     */
+    badMethodReturnShouldBeTyped: function() // ERROR - number, not {number}
+    {
+        return 1;
+    },
+
+    /**
      * @returns {number}
      */
     badMethodReturnsShouldBeAbsentToo: function() // ERROR - @returns, should be absent
@@ -287,6 +313,15 @@ TypeThree.prototype = {
     badMethodReturnsShouldBeAbsent: function() // ERROR - @returns, should be absent
     {
         var foo = 1;
+    },
+
+    /**
+     * @param number foo
+     * @param bar
+     */
+    badMethodParamAnnotations: function(foo, bar) // ERROR - @param's should be well-formed
+    {
+        return 1;
     }
 }
 
