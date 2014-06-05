d63ac1b219749c0e9a6e95d4a0d7acb289037a40
kochi@chromium.org <kochi@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-05 08:28:33 +0000

    Remove scoped styles.
    Removes <style scoped>.
    
    This was discussed and approved with LGTMs in blink-dev:
    https://groups.google.com/a/chromium.org/d/msg/blink-dev/R1x18ZLS5qQ/Bjuh_cENhlQJ
    
    Chrome status dashboard entry:
    http://www.chromestatus.com/features/5374137958662144
    
    Note:
    this is based on esprehn's original cl:
    https://codereview.chromium.org/214693002/
    
    BUG=379096
    TEST=pass all layout tests
    
    Review URL: https://codereview.chromium.org/310443002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175550 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Node.h
+++ third_party/WebKit/Source/core/dom/Node.h
@@ -379,9 +379,6 @@ public:
 
     void setIsLink(bool f);
 
-    bool hasScopedHTMLStyleChild() const { return getFlag(HasScopedHTMLStyleChildFlag); }
-    void setHasScopedHTMLStyleChild(bool flag) { setFlag(flag, HasScopedHTMLStyleChildFlag); }
-
     bool hasEventTargetData() const { return getFlag(HasEventTargetDataFlag); }
     void setHasEventTargetData(bool flag) { setFlag(flag, HasEventTargetDataFlag); }
 
@@ -656,10 +653,6 @@ public:
     void unregisterTransientMutationObserver(MutationObserverRegistration*);
     void notifyMutationObserversNodeWillDetach();
 
-    virtual void registerScopedHTMLStyleChild();
-    virtual void unregisterScopedHTMLStyleChild();
-    size_t numberOfScopedHTMLStyleChildren() const;
-
     unsigned connectedSubframeCount() const;
     void incrementConnectedSubframeCount(unsigned amount = 1);
     void decrementConnectedSubframeCount(unsigned amount = 1);
@@ -725,13 +718,10 @@ private:
         // HTML dir=auto.
         SelfOrAncestorHasDirAutoFlag = 1 << 29,
 
-        // FIXME: Remove <style scoped> support.
-        HasScopedHTMLStyleChildFlag = 1 << 30,
-
         DefaultNodeFlags = IsFinishedParsingChildrenFlag | ChildNeedsStyleRecalcFlag | NeedsReattachStyleChange
     };
 
-    // 1 bits remaining.
+    // 2 bits remaining.
 
     bool getFlag(NodeFlags mask) const { return m_nodeFlags & mask; }
     void setFlag(bool f, NodeFlags mask) const { m_nodeFlags = (m_nodeFlags & ~mask) | (-(int32_t)f & mask); }
