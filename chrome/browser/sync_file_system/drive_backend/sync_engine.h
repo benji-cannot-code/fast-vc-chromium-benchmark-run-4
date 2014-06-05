d5f87a2a7b4e77b82846fa243b74f1799661d912
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-05 14:31:07 +0000

    [SyncFS] Use CallbackTracker in SyncEngine
    BUG=380615
    
    Review URL: https://codereview.chromium.org/313013004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@275100 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_backend/sync_engine.h
+++ chrome/browser/sync_file_system/drive_backend/sync_engine.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "chrome/browser/drive/drive_notification_observer.h"
 #include "chrome/browser/drive/drive_service_interface.h"
+#include "chrome/browser/sync_file_system/drive_backend/callback_tracker.h"
 #include "chrome/browser/sync_file_system/local_change_processor.h"
 #include "chrome/browser/sync_file_system/remote_file_sync_service.h"
 #include "chrome/browser/sync_file_system/sync_action.h"
@@ -149,6 +150,8 @@ class SyncEngine : public RemoteFileSyncService,
   // database which may live in another thread.
   void UpdateRegisteredAppsForTesting();
 
+  SyncStatusCallback TrackCallback(const SyncStatusCallback& callback);
+
   scoped_ptr<drive::DriveServiceInterface> drive_service_;
   scoped_ptr<DriveServiceWrapper> drive_service_wrapper_;
   scoped_ptr<drive::DriveUploaderInterface> drive_uploader_;
@@ -174,6 +177,8 @@ class SyncEngine : public RemoteFileSyncService,
   scoped_ptr<WorkerObserver> worker_observer_;
   scoped_ptr<SyncWorker> sync_worker_;
   scoped_refptr<base::SequencedTaskRunner> worker_task_runner_;
+
+  CallbackTracker callback_tracker_;
 
   base::WeakPtrFactory<SyncEngine> weak_ptr_factory_;
   DISALLOW_COPY_AND_ASSIGN(SyncEngine);
