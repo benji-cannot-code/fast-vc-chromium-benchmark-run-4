e4652d2e1b76b37bbb5298ab155bc4e3e2ef14b3
kinaba@chromium.org <kinaba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-05 16:45:41 +0000

    Fix flakiness of OpenAudioFiles/FileManagerBrowserTest and re-enable it.
    BUG=374097
    
    Review URL: https://codereview.chromium.org/318773004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@275160 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/file_manager/file_manager/audio_player/js/audio_player.js
+++ ui/file_manager/file_manager/audio_player/js/audio_player.js
@@ -365,7 +365,7 @@ AudioPlayer.prototype.syncHeight_ = function() {
  */
 AudioPlayer.TrackInfo = function(entry, onClick) {
   this.url = entry.toURL();
-  this.title = entry.name;
+  this.title = this.getDefaultTitle();
   this.artist = this.getDefaultArtist();
 
   // TODO(yoshiki): implement artwork.
@@ -407,13 +407,8 @@ AudioPlayer.TrackInfo.prototype.getDefaultArtist = function() {
  */
 AudioPlayer.TrackInfo.prototype.setMetadata = function(
     metadata, error) {
-  if (error) {
-    // TODO(yoshiki): Handle error in better way.
-    this.title = entry.name;
-    this.artist = this.getDefaultArtist();
-  } else if (metadata.thumbnail && metadata.thumbnail.url) {
-    // TODO(yoshiki): implement artwork.
-  }
+  // TODO(yoshiki): Handle error in better way.
+  // TODO(yoshiki): implement artwork (metadata.thumbnail)
   this.title = (metadata.media && metadata.media.title) ||
       this.getDefaultTitle();
   this.artist = error ||
