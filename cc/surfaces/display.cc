047a77999c336586058c23e129aeb8101f679a4d
jbauman@chromium.org <jbauman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-05 05:43:07 +0000

    Use WeakPtr when scheduling cc::Display compositor.
    The Display can go away before the callback's called.
    
    BUG=334090
    
    Review URL: https://codereview.chromium.org/315103002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@275016 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/surfaces/display.cc
+++ cc/surfaces/display.cc
@@ -25,7 +25,8 @@ Display::Display(DisplayClient* client,
       client_(client),
       manager_(manager),
       aggregator_(manager),
-      bitmap_manager_(bitmap_manager) {
+      bitmap_manager_(bitmap_manager),
+      schedule_draw_factory_(this) {
 }
 
 Display::~Display() {
@@ -89,7 +90,8 @@ void Display::ScheduleComposite() {
   scheduled_draw_ = true;
 
   base::MessageLoop::current()->PostTask(
-      FROM_HERE, base::Bind(&Display::DoComposite, base::Unretained(this)));
+      FROM_HERE,
+      base::Bind(&Display::DoComposite, schedule_draw_factory_.GetWeakPtr()));
 }
 
 void Display::ScheduleAnimation() {
