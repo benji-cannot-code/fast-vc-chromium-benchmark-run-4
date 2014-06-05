cdeb4d837c22ec323dcb5bdeabf6b44bafe1e8e3
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-05 19:22:05 +0000

    Tiny code cleanup and added comments.
    Review URL: https://codereview.chromium.org/316103005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@275214 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/local_discovery/local_discovery_ui_handler.cc
+++ chrome/browser/ui/webui/local_discovery/local_discovery_ui_handler.cc
@@ -156,7 +156,7 @@ void LocalDiscoveryUIHandler::RegisterMessages() {
         base::Bind(&LocalDiscoveryUIHandler::HandleDisableCloudPrintConnector,
                    base::Unretained(this)));
   }
-#endif  // defined(ENABLE_FULL_PRINTING)
+#endif  // defined(CLOUD_PRINT_CONNECTOR_UI_AVAILABLE)
 }
 
 void LocalDiscoveryUIHandler::HandleStart(const base::ListValue* args) {
