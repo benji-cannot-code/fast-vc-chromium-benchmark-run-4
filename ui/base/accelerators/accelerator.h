a458076398f69ee11259243c9d3d8f4190cf3ee9
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-05 14:33:55 +0000

    Add EF_IS_REPEAT flag to KeyEvent to handle repeated accelerators correctly.
    Remove AcceleratorController::Context. Just remember previous accelerator in AcceleratorController.
    
    BUG=379171
    TEST=covered by unit test.
    R=sadrul@chromium.org, sky@chromium.org, yusukes@chromium.org
    
    Review URL: https://codereview.chromium.org/308023002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@275101 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/accelerators/accelerator.h
+++ ui/base/accelerators/accelerator.h
@@ -55,6 +55,7 @@ class UI_BASE_EXPORT Accelerator {
   bool IsCtrlDown() const;
   bool IsAltDown() const;
   bool IsCmdDown() const;
+  bool IsRepeat() const;
 
   // Returns a string with the localized shortcut if any.
   base::string16 GetShortcutText() const;
@@ -68,6 +69,7 @@ class UI_BASE_EXPORT Accelerator {
     return platform_accelerator_.get();
   }
 
+  void set_is_repeat(bool is_repeat) { is_repeat_ = is_repeat; }
 
  protected:
   // The keycode (VK_...).
@@ -78,6 +80,9 @@ class UI_BASE_EXPORT Accelerator {
 
   // The state of the Shift/Ctrl/Alt keys.
   int modifiers_;
+
+  // True if the accelerator is created for an auto repeated key event.
+  bool is_repeat_;
 
   // Stores platform specific data. May be NULL.
   scoped_ptr<PlatformAccelerator> platform_accelerator_;
--- ui/events/event.h
+++ ui/events/event.h
@@ -82,6 +82,7 @@ class EVENTS_EXPORT Event {
   bool IsCapsLockDown() const { return (flags_ & EF_CAPS_LOCK_DOWN) != 0; }
   bool IsAltDown() const { return (flags_ & EF_ALT_DOWN) != 0; }
   bool IsAltGrDown() const { return (flags_ & EF_ALTGR_DOWN) != 0; }
+  bool IsRepeat() const { return (flags_ & EF_IS_REPEAT) != 0; }
 
   bool IsKeyEvent() const {
     return type_ == ET_KEY_PRESSED ||
@@ -582,6 +583,10 @@ class EVENTS_EXPORT KeyEvent : public Event {
   bool is_char_;
 
   uint16 character_;
+
+  static bool IsRepeated(const KeyEvent& event);
+
+  static KeyEvent* last_key_event_;
 };
 
 class EVENTS_EXPORT ScrollEvent : public MouseEvent {
