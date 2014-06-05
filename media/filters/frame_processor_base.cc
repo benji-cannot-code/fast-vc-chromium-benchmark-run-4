cecd375209263be488a7eab8126b489421c80fd5
dalecurtis@chromium.org <dalecurtis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-05 05:54:30 +0000

    Fix a couple edges cases with audio preroll processing.
    - Avoids unnecessary work when there's no preroll and no partial
    append window trimming to be done.
    - Avoids incorrectly discarding the preroll buffer when frames
    are less than a single sample apart.
    - Avoids incorrectly keeping the preroll buffer when the timestamp
    offset is changed.
    
    BUG=371633
    TEST=new unittest
    
    Review URL: https://codereview.chromium.org/310293003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@275027 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/frame_processor_base.cc
+++ media/filters/frame_processor_base.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/filters/frame_processor_base.h"
 
+#include <cstdlib>
+
 #include "base/stl_util.h"
 #include "media/base/buffers.h"
 
@@ -56,6 +58,9 @@ void FrameProcessorBase::SetGroupStartTimestampIfInSequenceMode(
   DCHECK(kNoTimestamp() != timestamp_offset);
   if (sequence_mode_)
     group_start_timestamp_ = timestamp_offset;
+
+  // Changes to timestampOffset should invalidate the preroll buffer.
+  audio_preroll_buffer_ = NULL;
 }
 
 bool FrameProcessorBase::AddTrack(StreamParser::TrackId id,
@@ -127,6 +132,11 @@ bool FrameProcessorBase::HandlePartialAppendWindowTrimming(
     return false;
   }
 
+  // There's nothing to be done if we have no preroll and the buffer starts on
+  // the append window start.
+  if (buffer->timestamp() == append_window_start && !audio_preroll_buffer_)
+    return false;
+
   // See if a partial discard can be done around |append_window_start|.
   DCHECK(buffer->timestamp() <= append_window_start);
   DCHECK(buffer->IsKeyframe());
@@ -138,9 +148,12 @@ bool FrameProcessorBase::HandlePartialAppendWindowTrimming(
   // the last buffer discarded for preroll.  This ensures that the partially
   // trimmed buffer can be correctly decoded.
   if (audio_preroll_buffer_) {
-    if (audio_preroll_buffer_->timestamp() +
-            audio_preroll_buffer_->duration() ==
-        buffer->timestamp()) {
+    // We only want to use the preroll buffer if it directly precedes (less than
+    // one sample apart) the current buffer.
+    const int64 delta = std::abs((audio_preroll_buffer_->timestamp() +
+                                  audio_preroll_buffer_->duration() -
+                                  buffer->timestamp()).InMicroseconds());
+    if (delta < sample_duration_.InMicroseconds()) {
       buffer->SetPrerollBuffer(audio_preroll_buffer_);
     } else {
       // TODO(dalecurtis): Add a MEDIA_LOG() for when this is dropped unused.
@@ -162,6 +175,21 @@ bool FrameProcessorBase::HandlePartialAppendWindowTrimming(
   buffer->set_timestamp(append_window_start);
   buffer->SetDecodeTimestamp(append_window_start);
   return true;
+}
+
+void FrameProcessorBase::OnPossibleAudioConfigUpdate(
+    const AudioDecoderConfig& config) {
+  DCHECK(config.IsValidConfig());
+
+  // Always clear the preroll buffer when a config update is received.
+  audio_preroll_buffer_ = NULL;
+
+  if (config.Matches(current_audio_config_))
+    return;
+
+  current_audio_config_ = config;
+  sample_duration_ = base::TimeDelta::FromSecondsD(
+      1.0 / current_audio_config_.samples_per_second());
 }
 
 }  // namespace media
