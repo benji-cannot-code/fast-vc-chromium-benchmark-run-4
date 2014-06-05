e40c7cc3262e0d40ac3006f1ded00a3fa77e77f2
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-05 07:10:08 +0000

    Makes IViewManager::Connects reuse connections as appropriate
    Reuse happens if the request originates from the same connection and
    has the same url.
    
    BUG=365012
    TEST=none
    R=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/319573004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@275032 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/services/view_manager/view_manager_connection.h
+++ mojo/services/view_manager/view_manager_connection.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define MOJO_SERVICES_VIEW_MANAGER_VIEW_MANAGER_CONNECTION_H_
 
 #include <set>
+#include <string>
 #include <vector>
 
 #include "base/basictypes.h"
@@ -41,7 +42,9 @@ class MOJO_VIEW_MANAGER_EXPORT ViewManagerConnection
     : public InterfaceImpl<IViewManager>,
       public NodeDelegate {
  public:
-  explicit ViewManagerConnection(RootNodeManager* root_node_manager);
+  ViewManagerConnection(RootNodeManager* root_node_manager,
+                        TransportConnectionId creator_id,
+                        const std::string& url);
   virtual ~ViewManagerConnection();
 
   // Used to mark this connection as originating from a call to
@@ -49,6 +52,8 @@ class MOJO_VIEW_MANAGER_EXPORT ViewManagerConnection
   void set_delete_on_connection_error() { delete_on_connection_error_ = true; }
 
   TransportConnectionId id() const { return id_; }
+  TransportConnectionId creator_id() const { return creator_id_; }
+  const std::string& url() const { return url_; }
 
   // Returns the Node with the specified id.
   Node* GetNode(const NodeId& id) {
@@ -65,6 +70,9 @@ class MOJO_VIEW_MANAGER_EXPORT ViewManagerConnection
   const View* GetView(const ViewId& id) const;
 
   void SetRoots(const Array<TransportNodeId>& node_ids);
+
+  // Invoked when a connection is destroyed.
+  void OnViewManagerConnectionDestroyed(TransportConnectionId id);
 
   // The following methods are invoked after the corresponding change has been
   // processed. They do the appropriate bookkeeping and update the client as
@@ -126,6 +134,11 @@ class MOJO_VIEW_MANAGER_EXPORT ViewManagerConnection
   // Removes |node| and all its descendants from |known_nodes_|. This does not
   // recurse through nodes that were created by this connection.
   void RemoveFromKnown(const Node* node);
+
+  // Adds |node_ids| to roots, returning true if at least one of the nodes was
+  // not already a root. If at least one of the nodes was not already a root
+  // the client is told of the new roots.
+  bool AddRoots(const std::vector<TransportNodeId>& node_ids);
 
   // Returns true if |node| is a non-null and a descendant of |roots_| (or
   // |roots_| is empty).
@@ -195,6 +208,13 @@ class MOJO_VIEW_MANAGER_EXPORT ViewManagerConnection
 
   // Id of this connection as assigned by RootNodeManager.
   const TransportConnectionId id_;
+
+  // URL this connection was created for.
+  const std::string url_;
+
+  // ID of the connection that created us. If 0 it indicates either we were
+  // created by the root, or the connection that created us has been destroyed.
+  TransportConnectionId creator_id_;
 
   NodeMap node_map_;
 
