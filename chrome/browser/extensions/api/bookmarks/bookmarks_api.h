dc246f18368ac580b7f670672090d6da90cd3612
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-06 10:50:56 +0000

    Added the "unmodifiable" property to chrome.bookmarks.BookmarkTreeNode.
    This optional property indicates the reason why this BookmarkTreeNode can't
    be modified by the user nor the extension. For now the only value defined is
    "managed", indicating that this bookmark was configured by the system
    administrator.
    
    If the property is not present then the bookmark is modifiable (default).
    
    BUG=49598
    R=kalman@chromium.org
    
    Review URL: https://codereview.chromium.org/320473002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@275387 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/bookmarks/bookmarks_api.h
+++ chrome/browser/extensions/api/bookmarks/bookmarks_api.h
@@ -42,7 +42,7 @@ struct CreateDetails;
 // the extension system.
 class BookmarkEventRouter : public BookmarkModelObserver {
  public:
-  BookmarkEventRouter(content::BrowserContext* context, BookmarkModel* model);
+  explicit BookmarkEventRouter(Profile* profile);
   virtual ~BookmarkEventRouter();
 
   // BookmarkModelObserver:
@@ -81,6 +81,7 @@ class BookmarkEventRouter : public BookmarkModelObserver {
 
   content::BrowserContext* browser_context_;
   BookmarkModel* model_;
+  ChromeBookmarkClient* client_;
 
   DISALLOW_COPY_AND_ASSIGN(BookmarkEventRouter);
 };
