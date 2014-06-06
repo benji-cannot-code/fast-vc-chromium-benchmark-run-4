435bba19bc9cbdeeeeb9722b302100ac57abd1a0
kkimlabs@chromium.org <kkimlabs@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-06 10:40:59 +0000

    [Android] Add a field trial to disable enhanced bookmark image saving.
    We will be saving bookmark salient images even if enhanced bookmark
    feature is not turned on. This is to have some images available when we
    turn on the feature. For just in case, add a field trial to disable
    saving images.
    
    BUG=368034
    
    Review URL: https://codereview.chromium.org/314253002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@275381 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/bookmarks/enhanced_bookmarks_features.h
+++ chrome/browser/bookmarks/enhanced_bookmarks_features.h
@@ -48,6 +48,10 @@ void ForceFinchBookmarkExperimentIfNeeded(
 // Experiment could be enable from Chrome sync or from Finch.
 bool IsEnhancedBookmarksExperimentEnabled();
 
+#if defined(OS_ANDROID)
+bool IsEnhancedBookmarkImageFetchingEnabled();
+#endif
+
 // Returns true when flag enable-dom-distiller is set or enabled from Finch.
 bool IsEnableDomDistillerSet();
 
