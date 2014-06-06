06c33cb9ad72f3b4bee2ed23f44462f4495caa36
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-06 18:27:38 +0000

    Revert of Remove now obsolete checkdeps copy (https://codereview.chromium.org/307333003/)
    Reason for revert:
    Still referenced from blink
    
    Original issue's description:
    > Remove now obsolete checkdeps copy
    > 
    > BUG=none
    > R=brettw@chromium.org
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=275419
    
    TBR=brettw@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=none
    
    Review URL: https://codereview.chromium.org/320043002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@275482 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/checkdeps/testdata/allowed/test.h
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "tools/checkdeps/testdata/allowed/good.h"
+#include "tools/checkdeps/testdata/disallowed/bad.h"
+#include "tools/checkdeps/testdata/disallowed/allowed/good.h"
+#include "tools/checkdeps/testdata/disallowed/temporarily_allowed.h"
+#include "third_party/explicitly_disallowed/bad.h"
+#include "third_party/allowed_may_use/good.h"
+#include "third_party/no_rule/bad.h"
---
+++ tools/checkdeps/testdata/disallowed/test.h
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "tools/checkdeps/testdata/allowed/good.h"
+// Always allowed to include self.
+#include "tools/checkdeps/testdata/disallowed/good.h"
+#include "tools/checkdeps/testdata/disallowed/allowed/good.h"
+#include "third_party/explicitly_disallowed/bad.h"
+// Only allowed for code under allowed/.
+#include "third_party/allowed_may_use/bad.h"
+#include "third_party/no_rule/bad.h"
