c188d1a463412ad07ce912a8acb9af5d6e25897f
dgozman@chromium.org <dgozman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-06 13:52:17 +0000

    [DevTools] UI improvements for responsive design view.
    BUG=none
    TBR=pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/314303005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175667 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ResponsiveDesignView.js
+++ third_party/WebKit/Source/devtools/front_end/ResponsiveDesignView.js
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 WebInspector.ResponsiveDesignView = function(inspectedPagePlaceholder)
 {
     WebInspector.VBox.call(this);
+    this.setMinimumSize(150, 150);
     this.registerRequiredCSS("responsiveDesignView.css");
     this.element.classList.add("overflow-hidden");
 
