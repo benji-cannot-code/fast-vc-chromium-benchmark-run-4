b4f08191c9973e18402a843f9c6b92926cabbdb9
jiayl@chromium.org <jiayl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-06 05:14:52 +0000

    Do not build webrtc-internals when ENABLE_WEBRTC is false.
    BUG=357517
    
    Review URL: https://codereview.chromium.org/281323004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@275336 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/webui/content_web_ui_controller_factory.cc
+++ content/browser/webui/content_web_ui_controller_factory.cc
@@ -9,13 +9,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/gpu/gpu_internals_ui.h"
 #include "content/browser/indexed_db/indexed_db_internals_ui.h"
 #include "content/browser/media/media_internals_ui.h"
-#include "content/browser/media/webrtc_internals_ui.h"
 #include "content/browser/service_worker/service_worker_internals_ui.h"
 #include "content/browser/tracing/tracing_ui.h"
 #include "content/public/browser/storage_partition.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
 #include "content/public/common/url_constants.h"
+
+#if defined(ENABLE_WEBRTC)
+#include "content/browser/media/webrtc_internals_ui.h"
+#endif
 
 namespace content {
 
@@ -47,8 +50,6 @@ bool ContentWebUIControllerFactory::UseWebUIBindingsForURL(
 
 WebUIController* ContentWebUIControllerFactory::CreateWebUIControllerForURL(
     WebUI* web_ui, const GURL& url) const {
-  if (url.host() == kChromeUIWebRTCInternalsHost)
-    return new WebRTCInternalsUI(web_ui);
   if (url.host() == kChromeUIGpuHost)
     return new GpuInternalsUI(web_ui);
   if (url.host() == kChromeUIIndexedDBInternalsHost)
@@ -62,6 +63,11 @@ WebUIController* ContentWebUIControllerFactory::CreateWebUIControllerForURL(
 #if !defined(OS_ANDROID)
   if (url.host() == kChromeUITracingHost)
     return new TracingUI(web_ui);
+#endif
+
+#if defined(ENABLE_WEBRTC)
+  if (url.host() == kChromeUIWebRTCInternalsHost)
+    return new WebRTCInternalsUI(web_ui);
 #endif
 
   return NULL;
--- content/content_tests.gypi
+++ content/content_tests.gypi
@@ -1105,7 +1105,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'browser/media/media_browsertest.h',
             'browser/media/media_canplaytype_browsertest.cc',
             'browser/media/media_source_browsertest.cc',
-            'browser/media/webrtc_internals_browsertest.cc',
             'browser/plugin_data_remover_impl_browsertest.cc',
             'browser/plugin_browsertest.cc',
             'browser/plugin_service_impl_browsertest.cc',
@@ -1269,6 +1268,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
               'sources': [
                 'browser/media/webrtc_browsertest.cc',
                 'browser/media/webrtc_getusermedia_browsertest.cc',
+                'browser/media/webrtc_internals_browsertest.cc',
                 'test/webrtc_content_browsertest_base.cc',
                 'test/webrtc_content_browsertest_base.h',
               ],
