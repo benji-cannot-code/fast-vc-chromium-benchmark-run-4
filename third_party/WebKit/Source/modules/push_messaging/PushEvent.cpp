61380d2f01dcbdc3b6e390f15fc1e510b93187af
mvanouwerkerk@chromium.org <mvanouwerkerk@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-06 15:30:46 +0000

    Push API: define push event on Service Worker. BUG=350394 TBR=peter@chromium.org,dominicc@chromium.org,jochen@chromium.org,michaeln@chromium.org,kinuko@chromium.org
    Review URL: https://codereview.chromium.org/320503005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175672 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/Source/modules/push_messaging/PushEvent.cpp
@@ -0,0 +1,43 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "config.h"
+#include "modules/push_messaging/PushEvent.h"
+
+namespace WebCore {
+
+PushEventInit::PushEventInit()
+{
+}
+
+PushEvent::PushEvent()
+{
+    ScriptWrappable::init(this);
+}
+
+PushEvent::PushEvent(const AtomicString& type, const String& data)
+    : Event(type, /*canBubble=*/false, /*cancelable=*/false)
+    , m_data(data)
+{
+    ScriptWrappable::init(this);
+}
+
+PushEvent::PushEvent(const AtomicString& type, const PushEventInit& initializer)
+    : Event(type, initializer)
+    , m_data(initializer.data)
+{
+    ScriptWrappable::init(this);
+}
+
+PushEvent::~PushEvent()
+{
+}
+
+const AtomicString& PushEvent::interfaceName() const
+{
+    return EventNames::PushEvent;
+}
+
+} // namespace WebCore
