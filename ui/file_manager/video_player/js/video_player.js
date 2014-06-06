364712abd9025bb60731cc04743b5224a78618bb
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-06 04:56:19 +0000

    Video Player: Supports bound arguments of wrap() util function
    And with this patch, the error detail is shown in case of error.
    
    BUG=355545
    TEST=manually tested
    R=hirono@chromium.org
    
    Review URL: https://codereview.chromium.org/315103004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@275328 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/file_manager/video_player/js/video_player.js
+++ ui/file_manager/video_player/js/video_player.js
@@ -52,7 +52,7 @@ function FullWindowVideoControls(
   this.inactivityWatcher_ = new MouseInactivityWatcher(playerContainer);
   this.__defineGetter__('inactivityWatcher', function() {
     return this.inactivityWatcher_;
-  });
+  }.wrap(this));
 
   this.inactivityWatcher_.check();
 }
@@ -127,11 +127,11 @@ VideoPlayer.prototype = {
  * @param {Array.<Object.<string, Object>>} videos List of videos.
  */
 VideoPlayer.prototype.prepare = function(videos) {
-  document.ondragstart = function(e) { e.preventDefault() };
-
   this.videos_ = videos;
 
-  var preventDefault = function(event) { event.preventDefault(); };
+  var preventDefault = function(event) { event.preventDefault(); }.wrap(null);
+
+  document.ondragstart = preventDefault;
 
   var maximizeButton = document.querySelector('.maximize-button');
   maximizeButton.addEventListener(
@@ -142,7 +142,7 @@ VideoPlayer.prototype.prepare = function(videos) {
         appWindow.restore();
       else
         appWindow.maximize();
-    });
+    }.wrap(null));
   maximizeButton.addEventListener('mousedown', preventDefault);
 
   var minimizeButton = document.querySelector('.minimize-button');
@@ -150,13 +150,13 @@ VideoPlayer.prototype.prepare = function(videos) {
     'click',
     function() {
       chrome.app.window.current().minimize()
-    });
+    }.wrap(null));
   minimizeButton.addEventListener('mousedown', preventDefault);
 
   var closeButton = document.querySelector('.close-button');
   closeButton.addEventListener(
     'click',
-    function() { close(); });
+    function() { close(); }.wrap(null));
   closeButton.addEventListener('mousedown', preventDefault);
 
   this.controls_ = new FullWindowVideoControls(
@@ -303,7 +303,7 @@ function initVideos(callback) {
         var videos = window.videos;
         window.videos = null;
         callback(videos);
-      });
+      }.wrap(null));
 }
 
 var player = new VideoPlayer();
@@ -316,7 +316,7 @@ function initStrings(callback) {
   chrome.fileBrowserPrivate.getStrings(function(strings) {
     loadTimeData.data = strings;
     callback();
-  });
+  }.wrap(null));
 }
 
 var initPromise = Promise.all(
@@ -328,4 +328,4 @@ initPromise.then(function(results) {
   var videos = results[0];
   player.prepare(videos);
   return new Promise(player.playVideo.wrap(player));
-});
+}.wrap(null));
