4ce02ff7fd09d77923a4c16a23b5380b1d349945
c.shu@samsung.com <c.shu@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-06 02:10:26 +0000

    Implement enableFastMobileScrolling web api.
    The effort needs to be done in 3 steps:
    1. Create the runtime flag interface in blink;
    2. Enable the flag for Android from chromium even it does not do anything in blink;
    3. Replace compile time guard with runtime flag in blink and remove ENABLE_FAST_MOBILE_SCROLLING in gyp file.
    
    This is the 1st set of changes.
    
    BUG=381391
    
    Review URL: https://codereview.chromium.org/321473002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175621 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebRuntimeFeatures.cpp
+++ third_party/WebKit/Source/web/WebRuntimeFeatures.cpp
@@ -106,6 +106,11 @@ void WebRuntimeFeatures::enableExperimentalCanvasFeatures(bool enable)
     RuntimeEnabledFeatures::setExperimentalCanvasFeaturesEnabled(enable);
 }
 
+void WebRuntimeFeatures::enableFastMobileScrolling(bool enable)
+{
+    RuntimeEnabledFeatures::setFastMobileScrollingEnabled(enable);
+}
+
 void WebRuntimeFeatures::enableFastTextAutosizing(bool enable)
 {
     RuntimeEnabledFeatures::setFastTextAutosizingEnabled(enable);
