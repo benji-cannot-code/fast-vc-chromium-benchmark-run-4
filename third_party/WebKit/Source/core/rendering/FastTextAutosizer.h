69897f80a874123a9b6d5826b469095a04a86c84
skobes@chromium.org <skobes@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-06 00:18:52 +0000

    Set multiplier to 1 on blocks with no direct text children.
    This fixes cases where a node that previously had a >1 multiplier ceases to have direct text children.  This can happen if its text content is removed in script, or if its 'display' style is changed from inline to block.  (There are probably other ways for it to happen also.)
    
    Previously such a node would have a "stale" multiplier, since the autosizer only updated multipliers on text nodes and their immediate parents.  In theory the stale multiplier should not have affected the rendering, but:
    
    - it is observable through the lineHeight property, and
    - due to http://crbug.com/380903 the stale multiplier on an ancestor might be used to render text, even when the text node has the correct multiplier
    
    BUG=378959
    
    Review URL: https://codereview.chromium.org/319013003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175603 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/FastTextAutosizer.h
+++ third_party/WebKit/Source/core/rendering/FastTextAutosizer.h
@@ -246,7 +246,7 @@ private:
     void beginLayout(RenderBlock*);
     void endLayout(RenderBlock*);
     void inflateAutoTable(RenderTable*);
-    void inflate(RenderBlock*);
+    float inflate(RenderObject*, float multiplier = 0);
     bool shouldHandleLayout() const;
     void setAllTextNeedsLayout();
     void resetMultipliers();
