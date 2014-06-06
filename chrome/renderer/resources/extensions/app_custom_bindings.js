be146e1bedce359a75d63732d0ee17674147a391
rockot@chromium.org <rockot@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-06 05:01:45 +0000

    Fix crash from missing app.binding in extensions.
    This fixes an issue where a lazy require of app bindings (when unavailable) will not yield a properly constructed bindings export.
    
    BUG=373179
    
    Review URL: https://codereview.chromium.org/319783002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@275329 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/resources/extensions/app_custom_bindings.js
+++ chrome/renderer/resources/extensions/app_custom_bindings.js
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 var GetAvailability = requireNative('v8_context').GetAvailability;
 if (!GetAvailability('app').is_available) {
-  exports.chromeApp = {};
+  exports.binding = {};
   exports.onInstallStateResponse = function(){};
   return;
 }
