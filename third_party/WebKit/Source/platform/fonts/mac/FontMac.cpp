27f3467bbefe84c7d3261f070dca85e4be1daa04
jbroman@chromium.org <jbroman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-06 20:05:26 +0000

    Replace GraphicsContext::setupPaintFor* with const getters.
    This is simpler, and marginally more efficient (as it avoids setting up
    the paint in a default state only to immediately overwrite it).
    
    GraphicsContext::setupShader is declared but never defined, so its declaration has been removed while here.
    
    BUG=381750
    
    Review URL: https://codereview.chromium.org/311193009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175697 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/fonts/mac/FontMac.cpp
+++ third_party/WebKit/Source/platform/fonts/mac/FontMac.cpp
@@ -143,8 +143,7 @@ void Font::drawGlyphs(GraphicsContext* gc, const SimpleFontData* font,
 
     // We draw text up to two times (once for fill, once for stroke).
     if (textMode & TextModeFill) {
-        SkPaint paint;
-        gc->setupPaintForFilling(&paint);
+        SkPaint paint = gc->fillPaint();
         setupPaint(&paint, font, this, shouldAntialias, shouldSmoothFonts);
         gc->adjustTextRenderMode(&paint);
         paint.setTextEncoding(SkPaint::kGlyphID_TextEncoding);
@@ -156,8 +155,7 @@ void Font::drawGlyphs(GraphicsContext* gc, const SimpleFontData* font,
         && gc->strokeStyle() != NoStroke
         && gc->strokeThickness() > 0) {
 
-        SkPaint paint;
-        gc->setupPaintForStroking(&paint);
+        SkPaint paint = gc->strokePaint();
         setupPaint(&paint, font, this, shouldAntialias, shouldSmoothFonts);
         gc->adjustTextRenderMode(&paint);
         paint.setTextEncoding(SkPaint::kGlyphID_TextEncoding);
