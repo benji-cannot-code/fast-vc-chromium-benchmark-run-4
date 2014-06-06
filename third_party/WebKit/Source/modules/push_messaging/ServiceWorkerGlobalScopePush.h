a98f65b85829c71e5c86b12f2ddec5c7f0d438b7
mvanouwerkerk@chromium.org <mvanouwerkerk@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-06 09:55:06 +0000

    Push API: define push event on Service Worker.
    BUG=350394
    
    Review URL: https://codereview.chromium.org/285403002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175652 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/Source/modules/push_messaging/ServiceWorkerGlobalScopePush.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ServiceWorkerGlobalScopePush_h
+#define ServiceWorkerGlobalScopePush_h
+
+#include "core/events/EventTarget.h"
+
+namespace WebCore {
+
+class ServiceWorkerGlobalScopePush {
+public:
+    DEFINE_STATIC_ATTRIBUTE_EVENT_LISTENER(push);
+};
+
+} // namespace WebCore
+
+#endif // ServiceWorkerGlobalScopePush_h
