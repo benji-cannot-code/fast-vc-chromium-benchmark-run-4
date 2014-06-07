272ec8ca3dae28c6925d379f9b73bd29789bf0f8
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-07 08:47:07 +0000

    Add a new histogram Net.QuicSession.HandshakeConfirmedTime which tracks the elapsed time between starting the crypto handshake, and receiving confirmation from the server.
    Review URL: https://codereview.chromium.org/318143002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@275654 0039d316-1c4b-4281-b951-d872f2087c98

--- net/quic/quic_client_session.h
+++ net/quic/quic_client_session.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/containers/hash_tables.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/time/time.h"
 #include "net/base/completion_callback.h"
 #include "net/proxy/proxy_server.h"
 #include "net/quic/quic_client_session_base.h"
@@ -236,6 +237,7 @@ class NET_EXPORT_PRIVATE QuicClientSession : public QuicClientSessionBase {
   size_t num_total_streams_;
   base::TaskRunner* task_runner_;
   BoundNetLog net_log_;
+  base::TimeTicks handshake_start_;  // Time the handshake was started.
   QuicConnectionLogger logger_;
   // Number of packets read in the current read loop.
   size_t num_packets_read_;
