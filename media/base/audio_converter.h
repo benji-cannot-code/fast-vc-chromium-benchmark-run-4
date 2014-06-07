efcbc9d7ecda8cd481c4f7a55d8836095ac39c13
dalecurtis@chromium.org <dalecurtis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-07 01:46:58 +0000

    Deliver RenderCallbackErrors even when mixer inputs are paused.
    See the bug for more details.  Fixes potential stuck playback.
    
    - Separates error callback registration from mixer input addition.
    - Removes |mixer_inputs_| in favor of letting AudioConverter manage.
    - Adds a new unittest to verify paused inputs receive errors.
    
    This fixes the problem implicitly because AudioRendererImpl will
    deliver a decode error which shuts down the pipeline.
    
    BUG=379374
    TEST=new unittest.
    
    Review URL: https://codereview.chromium.org/301223012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@275585 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/audio_converter.h
+++ media/base/audio_converter.h
@@ -91,6 +91,8 @@ class MEDIA_EXPORT AudioConverter {
   // to each input's ProvideInput for more data.
   int ChunkSize() const;
 
+  bool empty() const { return transform_inputs_.empty(); }
+
  private:
   // Provides input to the MultiChannelResampler.  Called by the resampler when
   // more data is necessary.
--- media/base/audio_renderer_mixer_input.cc
+++ media/base/audio_renderer_mixer_input.cc
@@ -26,8 +26,10 @@ AudioRendererMixerInput::AudioRendererMixerInput(
 
 AudioRendererMixerInput::~AudioRendererMixerInput() {
   // Mixer is no longer safe to use after |remove_mixer_cb_| has been called.
-  if (initialized_)
+  if (initialized_) {
+    mixer_->RemoveErrorCallback(error_cb_);
     remove_mixer_cb_.Run(params_);
+  }
 }
 
 void AudioRendererMixerInput::Initialize(
@@ -36,6 +38,7 @@ void AudioRendererMixerInput::Initialize(
   DCHECK(!initialized_);
   params_ = params;
   mixer_ = get_mixer_cb_.Run(params_);
+  mixer_->AddErrorCallback(error_cb_);
   callback_ = callback;
   initialized_ = true;
 }
@@ -61,7 +64,7 @@ void AudioRendererMixerInput::Play() {
   if (playing_)
     return;
 
-  mixer_->AddMixerInput(this, error_cb_);
+  mixer_->AddMixerInput(this);
   playing_ = true;
 }
 
