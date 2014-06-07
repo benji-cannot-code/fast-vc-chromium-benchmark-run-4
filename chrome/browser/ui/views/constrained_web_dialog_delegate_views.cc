9da52611a59609444bcb898ee4b925d9743363b4
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-07 00:01:04 +0000

    Fix the print preview dialog frame.
    My r275303 mistakenly removed a function override.
    (it was a reasonable change for an earlier patch set)
    
    Restore ConstrainedWebDialogDelegateViewViews code.
    (uses dialog frame views via CreateNonClientFrameView)
    
    BUG=381524
    TEST=Print preview dialog frame is back to normal.
    TBR=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/317243004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@275559 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/constrained_web_dialog_delegate_views.cc
+++ chrome/browser/ui/views/constrained_web_dialog_delegate_views.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/webview/unhandled_keyboard_event_handler.h"
 #include "ui/views/controls/webview/webview.h"
 #include "ui/views/widget/widget.h"
-#include "ui/views/widget/widget_delegate.h"
+#include "ui/views/window/dialog_delegate.h"
 #include "ui/web_dialogs/web_dialog_delegate.h"
 #include "ui/web_dialogs/web_dialog_ui.h"
 
@@ -117,6 +117,10 @@ class ConstrainedWebDialogDelegateViewViews
   }
   virtual views::View* GetContentsView() OVERRIDE {
     return this;
+  }
+  virtual views::NonClientFrameView* CreateNonClientFrameView(
+      views::Widget* widget) OVERRIDE {
+    return views::DialogDelegate::CreateDialogFrameView(widget);
   }
   virtual bool ShouldShowCloseButton() const OVERRIDE {
     // No close button if the dialog doesn't want a title bar.
