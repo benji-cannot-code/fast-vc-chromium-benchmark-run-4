c85ac6d5dcd2738ddec7054eabeededfac9653dc
acleung@chromium.org <acleung@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-07 03:31:56 +0000

    Revert of Invert DSF to map from delegated frame to layer space (https://codereview.chromium.org/311253004/)
    Reason for revert:
    https://code.google.com/p/chromium/issues/detail?id=381982
    
    Original issue's description:
    > Invert DSF to map from delegated frame to layer space
    > 
    > The size of the frame data from a delegated source will not in general match up
    > exactly with the delegated layer's bounds. Frame sizes and layer bounds are both
    > expressed as integers, but frame sizes are in physical pixels and layer bounds
    > are in device-independent pixels. If the device scale is not an integer, there
    > may not be an exact mapping between the two spaces. For instance, if the layer
    > size is 105 x 211 and the device scale is 1.5, the delegated content is
    > responsible for filling 157.5 x 316.5 physical pixels in the final output. This
    > isn't really possible so what actually happens is the delegated renderer
    > produces a frame with a physical size of 158 x 317 pixels and sends that up to
    > the DRLI. To map this frame into physical space, the DRLI should apply the
    > inverse of the device scale factor to reflect that the frame covers 105.333 x
    > 211.3333 logical pixels.
    > 
    > Before this patch, DRLI would attempt to scale the frame by (105/158, 211/317)
    > resulting in the frame being scaled down non-uniformly to try to make 158 x 317
    > pixels fit into a 157.5 x 316.6 space. This scaling looks bad and ends up
    > mapping pixels partially outside of the layer's logical bounds into the layer's
    > logical bounds.
    > 
    > BUG=370074
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=275544
    
    TBR=enne@chromium.org,danakj@chromium.org,jamesr@chromium.org,jam@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=370074,381982
    
    Review URL: https://codereview.chromium.org/313403005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@275600 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/test/fake_delegated_renderer_layer_impl.cc
+++ cc/test/fake_delegated_renderer_layer_impl.cc
@@ -14,10 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace cc {
 
 FakeDelegatedRendererLayerImpl::FakeDelegatedRendererLayerImpl(
-    LayerTreeImpl* tree_impl,
-    int id)
-    : DelegatedRendererLayerImpl(tree_impl, id) {
-}
+    LayerTreeImpl* tree_impl, int id)
+    : DelegatedRendererLayerImpl(tree_impl, id) {}
 
 FakeDelegatedRendererLayerImpl::~FakeDelegatedRendererLayerImpl() {}
 
@@ -52,10 +50,8 @@ ResourceProvider::ResourceIdSet FakeDelegatedRendererLayerImpl::Resources()
 void NoopReturnCallback(const ReturnedResourceArray& returned) {}
 
 void FakeDelegatedRendererLayerImpl::SetFrameDataForRenderPasses(
-    float device_scale_factor,
     ScopedPtrVector<RenderPass>* pass_list) {
   scoped_ptr<DelegatedFrameData> delegated_frame(new DelegatedFrameData);
-  delegated_frame->device_scale_factor = device_scale_factor;
   delegated_frame->render_pass_list.swap(*pass_list);
 
   ResourceProvider* resource_provider = layer_tree_impl()->resource_provider();
