ab1697d4c304254176c1a68d73acc9b5d949ac7a
dcheng@chromium.org <dcheng@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-07 09:14:20 +0000

    Revert of Delete CompositingUpdateAfterLayout (https://codereview.chromium.org/323653002/)
    Reason for revert:
    Triggers ASSERT when running browser_test CastStreamingApiTestWithPixelOutput.RtpStreamError
    
    Original issue's description:
    > Delete CompositingUpdateAfterLayout
    > 
    > Rather than using a different update type, we can just mark the root as needing
    > its properties updated, which will cause us to update the entire tree. This CL
    > alone isn't a huge win, but it will let us remove
    > RenderLayerCompositor::m_compositingLayersNeedRebuild in the next CL.
    > 
    > Also, move the call to enableCompositingModeIfNeeded into the new
    > RenderLayerCompositor::didLayout function. This makes the sequence look a bit
    > silly (because we mark the dirty bit only to clear it immediately), but that's
    > what the code did before (albeit in a more roundabout way).
    > 
    > BUG=332248
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=175738
    
    TBR=esprehn@chromium.org,abarth@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=332248
    
    Review URL: https://codereview.chromium.org/321663002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175742 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/compositing/RenderLayerCompositor.h
+++ third_party/WebKit/Source/core/rendering/compositing/RenderLayerCompositor.h
@@ -50,6 +50,7 @@ enum CompositingUpdateType {
     CompositingUpdateNone,
     CompositingUpdateOnCompositedScroll,
     CompositingUpdateAfterCompositingInputChange,
+    CompositingUpdateAfterLayout,
 };
 
 enum CompositingStateTransitionType {
@@ -101,8 +102,6 @@ public:
 
     // Used to indicate that a compositing update will be needed for the next frame that gets drawn.
     void setNeedsCompositingUpdate(CompositingUpdateType);
-
-    void didLayout();
 
     enum UpdateLayerCompositingStateOptions {
         Normal,
