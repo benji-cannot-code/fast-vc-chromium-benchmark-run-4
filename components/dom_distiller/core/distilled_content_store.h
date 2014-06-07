2ea13828aa28c0989fb8e32d2636e5f14e39dd80
nyquist@chromium.org <nyquist@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-07 05:04:49 +0000

    [dom_distiller] Add support for lookup by URL for InMemoryContentStore.
    The InMemoryContentStore currently only supports lookup by entry ID, but this
    CL adds support for also looking up articles by URLs. This will make the class
    more usable for when users manually distill web pages.
    
    BUG=379642
    
    Review URL: https://codereview.chromium.org/314573005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@275618 0039d316-1c4b-4281-b951-d872f2087c98

--- components/dom_distiller/core/distilled_content_store.h
+++ components/dom_distiller/core/distilled_content_store.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/bind.h"
+#include "base/containers/hash_tables.h"
 #include "base/containers/mru_cache.h"
 #include "components/dom_distiller/core/article_entry.h"
 #include "components/dom_distiller/core/proto/distilled_article.pb.h"
@@ -41,6 +42,7 @@ class DistilledContentStore {
 
 // This content store keeps up to |max_num_entries| of the last accessed items
 // in its cache. Both loading and saving content is counted as access.
+// Lookup can be done based on entry ID or URL.
 class InMemoryContentStore : public DistilledContentStore {
  public:
   explicit InMemoryContentStore(const int max_num_entries);
@@ -58,9 +60,31 @@ class InMemoryContentStore : public DistilledContentStore {
                      const DistilledArticleProto& proto);
 
  private:
-  typedef base::MRUCache<std::string, DistilledArticleProto> ContentMap;
+  // The CacheDeletor gets called when anything is removed from the ContentMap.
+  class CacheDeletor {
+   public:
+    explicit CacheDeletor(InMemoryContentStore* store);
+    ~CacheDeletor();
+    void operator()(const DistilledArticleProto& proto);
+
+   private:
+    InMemoryContentStore* store_;
+  };
+
+  void AddUrlToIdMapping(const ArticleEntry& entry,
+                         const DistilledArticleProto& proto);
+
+  void EraseUrlToIdMapping(const DistilledArticleProto& proto);
+
+  typedef base::MRUCacheBase<std::string,
+                             DistilledArticleProto,
+                             InMemoryContentStore::CacheDeletor> ContentMap;
+  typedef base::hash_map<std::string, std::string> UrlMap;
+
   ContentMap cache_;
+  UrlMap url_to_id_;
 };
+
 }  // dom_distiller
 
 #endif  // COMPONENTS_DOM_DISTILLER_CORE_DOM_DISTILLER_CONTENT_CACHE_H_
