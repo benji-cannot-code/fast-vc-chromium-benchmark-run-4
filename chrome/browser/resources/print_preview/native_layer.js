4d5c5e64d968d668204e9b97ec422c14e3130dc8
alekseys@chromium.org <alekseys@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-08 02:08:05 +0000

    Generalize printer color model handling, get rid of CUPS specific case. Enable supported paper szes reporting for all platforms.
    BUG=239879
    NOTRY=true
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=275646
    
    Review URL: https://codereview.chromium.org/324523002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@275716 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/print_preview/native_layer.js
+++ chrome/browser/resources/print_preview/native_layer.js
@@ -169,6 +169,23 @@ cr.define('print_preview', function() {
     },
 
     /**
+     * @param {!print_preview.Destination} destination Destination to print to.
+     * @param {!print_preview.ticket_items.Color} color Color ticket item.
+     * @return {number} Native layer color model.
+     * @private
+     */
+    getNativeColorModel_: function(destination, color) {
+      // For non-local printers native color model is ignored anyway.
+      var option = destination.isLocal ? color.getSelectedOption() : null;
+      var nativeColorModel = parseInt(option ? option.vendor_id : null);
+      if (isNaN(nativeColorModel)) {
+        return color.getValue() ?
+            NativeLayer.ColorMode_.COLOR : NativeLayer.ColorMode_.GRAY;
+      }
+      return nativeColorModel;
+    },
+
+    /**
      * Requests that a preview be generated. The following events may be
      * dispatched in response:
      *   - PAGE_COUNT_READY
@@ -191,8 +208,7 @@ cr.define('print_preview', function() {
         'pageRange': printTicketStore.pageRange.getDocumentPageRanges(),
         'mediaSize': printTicketStore.mediaSize.getValue(),
         'landscape': printTicketStore.landscape.getValue(),
-        'color': printTicketStore.color.getValue() ?
-            NativeLayer.ColorMode_.COLOR : NativeLayer.ColorMode_.GRAY,
+        'color': this.getNativeColorModel_(destination, printTicketStore.color),
         'headerFooterEnabled': printTicketStore.headerFooter.getValue(),
         'marginsType': printTicketStore.marginsType.getValue(),
         'isFirstRequest': requestId == 0,
@@ -264,8 +280,7 @@ cr.define('print_preview', function() {
         'pageRange': printTicketStore.pageRange.getDocumentPageRanges(),
         'pageCount': printTicketStore.pageRange.getPageNumberSet().size,
         'landscape': printTicketStore.landscape.getValue(),
-        'color': printTicketStore.color.getValue() ?
-            NativeLayer.ColorMode_.COLOR : NativeLayer.ColorMode_.GRAY,
+        'color': this.getNativeColorModel_(destination, printTicketStore.color),
         'headerFooterEnabled': printTicketStore.headerFooter.getValue(),
         'marginsType': printTicketStore.marginsType.getValue(),
         'generateDraftData': true, // TODO(rltoscano): What should this be?
