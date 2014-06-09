b40ab533110d3cdc2ddde0da5266a62968189525
tasak@google.com <tasak@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-09 09:16:07 +0000

    Should check whether document.settings() is available or not when updating GenericFontFamilySettings.
    Looking at crash reports - WebCore::StyleEngine::updateGenericFontFamilySettings, document().settings() is probably null. In the case, we should not update GenericFontSettings.
    
    BUG=376525
    TEST=It is very hard to reproduce crashes reported in crbug.com/376525. Will check # of crash reports to see whether the bug is really fixed or not.
    
    Review URL: https://codereview.chromium.org/304033005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175783 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/CSSFontSelector.cpp
+++ third_party/WebKit/Source/core/css/CSSFontSelector.cpp
@@ -160,7 +160,8 @@ void CSSFontSelector::clearDocument()
 
 void CSSFontSelector::updateGenericFontFamilySettings(Document& document)
 {
-    ASSERT(document.settings());
+    if (!document.settings())
+        return;
     m_genericFontFamilySettings = document.settings()->genericFontFamilySettings();
 }
 
--- third_party/WebKit/Source/core/dom/StyleEngine.cpp
+++ third_party/WebKit/Source/core/dom/StyleEngine.cpp
@@ -575,6 +575,10 @@ void StyleEngine::clearFontCache()
 
 void StyleEngine::updateGenericFontFamilySettings()
 {
+    // FIXME: we should not update generic font family settings when
+    // document is inactive.
+    ASSERT(document().isActive());
+
     if (!m_fontSelector)
         return;
 
