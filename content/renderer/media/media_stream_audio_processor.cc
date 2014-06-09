94cec72bd773cd3615296e6b39a82a6cd6fade82
xians@chromium.org <xians@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-09 20:34:02 +0000

    Temporarily disable media stream audio track processing.
    NOTRY=true
    
    TBR=jochen@chromium.org
    BUG=382094
    TEST=bots
    
    Review URL: https://codereview.chromium.org/320813002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@275853 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/media_stream_audio_processor.cc
+++ content/renderer/media/media_stream_audio_processor.cc
@@ -163,8 +163,8 @@ class MediaStreamAudioProcessor::MediaStreamAudioConverter
 };
 
 bool MediaStreamAudioProcessor::IsAudioTrackProcessingEnabled() {
-  return !CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kDisableAudioTrackProcessing);
+  return CommandLine::ForCurrentProcess()->HasSwitch(
+      switches::kEnableAudioTrackProcessing);
 }
 
 MediaStreamAudioProcessor::MediaStreamAudioProcessor(
--- content/renderer/media/media_stream_audio_processor_unittest.cc
+++ content/renderer/media/media_stream_audio_processor_unittest.cc
@@ -155,9 +155,7 @@ class MediaStreamAudioProcessorTest : public ::testing::Test {
 };
 
 TEST_F(MediaStreamAudioProcessorTest, WithoutAudioProcessing) {
-  // Setup the audio processor with disabled flag on.
-  CommandLine::ForCurrentProcess()->AppendSwitch(
-      switches::kDisableAudioTrackProcessing);
+  // Setup the audio processor without enabling the flag.
   MockMediaConstraintFactory constraint_factory;
   scoped_refptr<WebRtcAudioDeviceImpl> webrtc_audio_device(
       new WebRtcAudioDeviceImpl());
@@ -178,6 +176,9 @@ TEST_F(MediaStreamAudioProcessorTest, WithoutAudioProcessing) {
 }
 
 TEST_F(MediaStreamAudioProcessorTest, WithAudioProcessing) {
+  // Setup the audio processor with the flag enabled.
+  CommandLine::ForCurrentProcess()->AppendSwitch(
+      switches::kEnableAudioTrackProcessing);
   MockMediaConstraintFactory constraint_factory;
   scoped_refptr<WebRtcAudioDeviceImpl> webrtc_audio_device(
       new WebRtcAudioDeviceImpl());
@@ -199,6 +200,9 @@ TEST_F(MediaStreamAudioProcessorTest, WithAudioProcessing) {
 }
 
 TEST_F(MediaStreamAudioProcessorTest, VerifyTabCaptureWithoutAudioProcessing) {
+  // Setup the audio processor with enabling the flag.
+  CommandLine::ForCurrentProcess()->AppendSwitch(
+      switches::kEnableAudioTrackProcessing);
   scoped_refptr<WebRtcAudioDeviceImpl> webrtc_audio_device(
       new WebRtcAudioDeviceImpl());
   // Create MediaStreamAudioProcessor instance for kMediaStreamSourceTab source.
@@ -235,6 +239,10 @@ TEST_F(MediaStreamAudioProcessorTest, VerifyTabCaptureWithoutAudioProcessing) {
 }
 
 TEST_F(MediaStreamAudioProcessorTest, TurnOffDefaultConstraints) {
+  // Setup the audio processor with enabling the flag.
+  CommandLine::ForCurrentProcess()->AppendSwitch(
+      switches::kEnableAudioTrackProcessing);
+
   // Turn off the default constraints and pass it to MediaStreamAudioProcessor.
   MockMediaConstraintFactory constraint_factory;
   constraint_factory.DisableDefaultAudioConstraints();
@@ -257,6 +265,10 @@ TEST_F(MediaStreamAudioProcessorTest, TurnOffDefaultConstraints) {
 }
 
 TEST_F(MediaStreamAudioProcessorTest, VerifyConstraints) {
+  // Setup the audio processor with enabling the flag.
+  CommandLine::ForCurrentProcess()->AppendSwitch(
+      switches::kEnableAudioTrackProcessing);
+
   static const char* kDefaultAudioConstraints[] = {
     MediaAudioConstraints::kEchoCancellation,
     MediaAudioConstraints::kGoogAudioMirroring,
@@ -353,6 +365,10 @@ TEST_F(MediaStreamAudioProcessorTest, ValidateConstraints) {
 }
 
 TEST_F(MediaStreamAudioProcessorTest, TestAllSampleRates) {
+  // Setup the audio processor with enabling the flag.
+  CommandLine::ForCurrentProcess()->AppendSwitch(
+      switches::kEnableAudioTrackProcessing);
+
   MockMediaConstraintFactory constraint_factory;
   scoped_refptr<WebRtcAudioDeviceImpl> webrtc_audio_device(
       new WebRtcAudioDeviceImpl());
