1510dd86cc48564361244b69e500a875943e8dfa
kozyatinskiy@google.com <kozyatinskiy@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-09 13:24:58 +0000

    Add Debugger.ExceptionDetails to protocol. Use ExceptionDetails for pass error when exception in snippet (run or compile).
    Console show the position of exception when exception happens in snippet.
    
    BUG=346013
    
    Review URL: https://codereview.chromium.org/290633009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175794 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/ScriptDebugServer.cpp
+++ third_party/WebKit/Source/bindings/v8/ScriptDebugServer.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "bindings/core/v8/V8JavaScriptCallFrame.h"
 #include "bindings/v8/ScopedPersistent.h"
+#include "bindings/v8/ScriptCallStackFactory.h"
 #include "bindings/v8/ScriptController.h"
 #include "bindings/v8/ScriptSourceCode.h"
 #include "bindings/v8/ScriptValue.h"
@@ -569,7 +570,7 @@ bool ScriptDebugServer::isPaused()
     return m_pausedScriptState;
 }
 
-void ScriptDebugServer::compileScript(ScriptState* scriptState, const String& expression, const String& sourceURL, String* scriptId, String* exceptionMessage)
+void ScriptDebugServer::compileScript(ScriptState* scriptState, const String& expression, const String& sourceURL, String* scriptId, String* exceptionDetailsText, int* lineNumber, int* columnNumber, RefPtr<ScriptCallStack>* stackTrace)
 {
     if (scriptState->contextIsEmpty())
         return;
@@ -580,8 +581,12 @@ void ScriptDebugServer::compileScript(ScriptState* scriptState, const String& ex
     v8::Local<v8::Script> script = V8ScriptRunner::compileScript(source, sourceURL, TextPosition(), 0, m_isolate);
     if (tryCatch.HasCaught()) {
         v8::Local<v8::Message> message = tryCatch.Message();
-        if (!message.IsEmpty())
-            *exceptionMessage = toCoreStringWithUndefinedOrNullCheck(message->Get());
+        if (!message.IsEmpty()) {
+            *exceptionDetailsText = toCoreStringWithUndefinedOrNullCheck(message->Get());
+            *lineNumber = message->GetLineNumber();
+            *columnNumber = message->GetStartColumn();
+            *stackTrace = createScriptCallStack(message->GetStackTrace(), message->GetStackTrace()->GetFrameCount(), m_isolate);
+        }
         return;
     }
     if (script.IsEmpty())
@@ -596,7 +601,7 @@ void ScriptDebugServer::clearCompiledScripts()
     m_compiledScripts.clear();
 }
 
-void ScriptDebugServer::runScript(ScriptState* scriptState, const String& scriptId, ScriptValue* result, bool* wasThrown, String* exceptionMessage)
+void ScriptDebugServer::runScript(ScriptState* scriptState, const String& scriptId, ScriptValue* result, bool* wasThrown, String* exceptionDetailsText, int* lineNumber, int* columnNumber, RefPtr<ScriptCallStack>* stackTrace)
 {
     if (!m_compiledScripts.contains(scriptId))
         return;
@@ -617,8 +622,12 @@ void ScriptDebugServer::runScript(ScriptState* scriptState, const String& script
         *wasThrown = true;
         *result = ScriptValue(scriptState, tryCatch.Exception());
         v8::Local<v8::Message> message = tryCatch.Message();
-        if (!message.IsEmpty())
-            *exceptionMessage = toCoreStringWithUndefinedOrNullCheck(message->Get());
+        if (!message.IsEmpty()) {
+            *exceptionDetailsText = toCoreStringWithUndefinedOrNullCheck(message->Get());
+            *lineNumber = message->GetLineNumber();
+            *columnNumber = message->GetStartColumn();
+            *stackTrace = createScriptCallStack(message->GetStackTrace(), message->GetStackTrace()->GetFrameCount(), m_isolate);
+        }
     } else {
         *result = ScriptValue(scriptState, value);
     }
