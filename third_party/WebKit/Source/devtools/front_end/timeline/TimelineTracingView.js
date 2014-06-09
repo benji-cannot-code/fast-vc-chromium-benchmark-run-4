6b7e46b929a21987f8e16aa815df0545b73fbe22
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-09 17:56:43 +0000

    Timeline: fix reset logic of TracingTimelineModel & co
    The way we perform reset (i.e. clear records) was
    badly wrong in a couple of places:
     - we generally should only reset the objects that we own,
    so TimelineTracingView should have not tried to reset TracingModel;
     - TracingTimelineModel should reset before the record is
    started,not after.
    
    BUG=361045
    
    Review URL: https://codereview.chromium.org/326693002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175810 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/timeline/TimelineTracingView.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TimelineTracingView.js
@@ -56,7 +56,6 @@ WebInspector.TimelineTracingView.prototype = {
 
     reset: function()
     {
-        this._tracingModel.reset();
         this._dataProvider.reset();
         this._mainView.setWindowTimes(0, Infinity);
     },
--- third_party/WebKit/Source/devtools/front_end/timeline/TracingTimelineModel.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TracingTimelineModel.js
@@ -102,6 +102,7 @@ WebInspector.TracingTimelineModel.prototype = {
      */
     startRecording: function(captureStacks, captureMemory)
     {
+        this.reset();
         var categories;
         if (WebInspector.experimentsSettings.timelineTracingMode.isEnabled()) {
             categories = WebInspector.TracingTimelineModel.defaultTracingCategoryFilter;
@@ -125,6 +126,7 @@ WebInspector.TracingTimelineModel.prototype = {
      */
     setEventsForTest: function(sessionId, events)
     {
+        this.reset();
         this._didStartRecordingTraceEvents();
         this._tracingModel.setEventsForTest(sessionId, events);
         this._didStopRecordingTraceEvents();
@@ -140,7 +142,6 @@ WebInspector.TracingTimelineModel.prototype = {
 
     _didStartRecordingTraceEvents: function()
     {
-        this.reset();
         this.dispatchEventToListeners(WebInspector.TimelineModel.Events.RecordingStarted);
     },
 
