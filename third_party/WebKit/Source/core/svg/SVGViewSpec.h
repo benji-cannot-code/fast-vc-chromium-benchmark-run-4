f7a2a6702d73c20b6cb7d48d5e1914907c507bc5
kouhei@chromium.org <kouhei@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-09 04:42:19 +0000

    SVGViewSpec wrapper should keep its context SVGSVGElement wrapper alive.
    There seems to be some cases where a SVGSVGElement wrapper is gc-ed while the SVGSVGElement is alive.
    However, |m_transform| and other properties held by SVGViewSpec manage its lifetime via the SVGSVGElement v8 wrapper.
    
    This patch avoids the situation by keeping the context SVGSVGElement wrapper alive from SVGViewSpec wrapper.
    
    BUG=379998
    
    Review URL: https://codereview.chromium.org/315893002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175772 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGViewSpec.h
+++ third_party/WebKit/Source/core/svg/SVGViewSpec.h
@@ -61,6 +61,8 @@ public:
     void trace(Visitor*);
     void clearWeakMembers(Visitor*);
 
+    SVGSVGElement* contextElement() { return m_contextElement.get(); }
+
 private:
     explicit SVGViewSpec(SVGSVGElement*);
 
