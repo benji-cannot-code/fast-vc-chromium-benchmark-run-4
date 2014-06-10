700e6c394e97d4637f6c45a8700685ce7d70d6c1
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-10 13:31:30 +0000

    Correctly calculate highlighted quad for tracing based timeline events
    highlightQuad method is added to TimelineMode.Rectord. The method encapsulates logic specific to timeline events and trace events.
    
    BUG=361045
    R=caseq@chromium.org
    
    Review URL: https://codereview.chromium.org/326063004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175892 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/timeline/TimelineModel.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TimelineModel.js
@@ -389,6 +389,11 @@ WebInspector.TimelineModel.Record.prototype = {
     data: function() { },
 
     /**
+     * @return {?Object}
+     */
+    highlightQuad: function() { },
+
+    /**
      * @return {string}
      */
     type: function() { },
