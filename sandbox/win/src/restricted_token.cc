f14f17a33a778c8d4511e6f09d8f4e9e5d563806
jschuh@chromium.org <jschuh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-10 23:31:09 +0000

    Make sbox_unittests work with DCHECKs enabled
    We have a DCHECK and a NOTREACHED in the test path,
    so these tests crash rather than return error codes.
    This patch just removes the crashing macros and lets
    the error codes through.
    
    BUG=381071
    R=rvargas
    TBR=rvargas
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/326133002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276204 0039d316-1c4b-4281-b951-d872f2087c98

--- sandbox/win/src/restricted_token.cc
+++ sandbox/win/src/restricted_token.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace sandbox {
 
 unsigned RestrictedToken::Init(const HANDLE effective_token) {
-  DCHECK(!init_);
   if (init_)
     return ERROR_ALREADY_INITIALIZED;
 
--- sandbox/win/src/restricted_token_unittest.cc
+++ sandbox/win/src/restricted_token_unittest.cc
@@ -409,8 +409,8 @@ TEST(RestrictedTokenTest, DeletePrivilege) {
 // elements in the restricting sids list has to be equal.
 void CheckRestrictingSid(const ATL::CAccessToken &restricted_token,
                          ATL::CSid sid, int count) {
-  DWORD length = 1000;
-  BYTE *memory = new BYTE[1000];
+  DWORD length = 8192;
+  BYTE *memory = new BYTE[length];
   TOKEN_GROUPS *groups = reinterpret_cast<TOKEN_GROUPS*>(memory);
   ASSERT_TRUE(::GetTokenInformation(restricted_token.GetHandle(),
                                     TokenRestrictedSids,
@@ -531,8 +531,8 @@ TEST(RestrictedTokenTest, AddMultipleRestrictingSids) {
   ATL::CSid session;
   restricted_token.GetLogonSid(&session);
 
-  DWORD length = 1000;
-  BYTE *memory = new BYTE[1000];
+  DWORD length = 8192;
+  BYTE *memory = new BYTE[length];
   TOKEN_GROUPS *groups = reinterpret_cast<TOKEN_GROUPS*>(memory);
   ASSERT_TRUE(::GetTokenInformation(restricted_token.GetHandle(),
                                     TokenRestrictedSids,
@@ -578,9 +578,6 @@ TEST(RestrictedTokenTest, AddAllSidToRestrictingSids) {
   CheckRestrictingSid(restricted_token, user, -1);
 }
 
-// Test to be executed only in release because they are triggering DCHECKs.
-#ifndef _DEBUG
-
 // Checks the error code when the object is initialized twice.
 TEST(RestrictedTokenTest, DoubleInit) {
   RestrictedToken token;
@@ -588,7 +585,5 @@ TEST(RestrictedTokenTest, DoubleInit) {
 
   ASSERT_EQ(ERROR_ALREADY_INITIALIZED, token.Init(NULL));
 }
-
-#endif
 
 }  // namespace sandbox
