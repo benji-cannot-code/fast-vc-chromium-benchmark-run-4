5fb7ffa5fa3cf51b898ff6c29db76812a7b222bd
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-10 06:30:20 +0000

    [SyncFS] Drop PostTaskAndReply from LocalToRemoteSyncer
    BUG=347425, 380615
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/321783002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@275973 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_backend/local_to_remote_syncer.cc
+++ chrome/browser/sync_file_system/drive_backend/local_to_remote_syncer.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/drive/drive_api_util.h"
 #include "chrome/browser/drive/drive_service_interface.h"
 #include "chrome/browser/drive/drive_uploader.h"
+#include "chrome/browser/sync_file_system/drive_backend/callback_helper.h"
 #include "chrome/browser/sync_file_system/drive_backend/drive_backend_util.h"
 #include "chrome/browser/sync_file_system/drive_backend/folder_creator.h"
 #include "chrome/browser/sync_file_system/drive_backend/metadata_database.h"
@@ -396,13 +397,19 @@ void LocalToRemoteSyncer::UploadExistingFile(
     const SyncStatusCallback& callback)  {
   DCHECK(remote_file_tracker_);
   DCHECK(remote_file_tracker_->has_synced_details());
+  DCHECK(sync_context_->GetWorkerTaskRunner()->RunsTasksOnCurrentThread());
 
-  base::PostTaskAndReplyWithResult(
-      sync_context_->GetFileTaskRunner(), FROM_HERE,
-      base::Bind(&drive::util::GetMd5Digest, local_path_),
+  base::Callback<void(const std::string&)> did_calculate_callback =
       base::Bind(&LocalToRemoteSyncer::DidGetMD5ForUpload,
-                 weak_ptr_factory_.GetWeakPtr(),
-                 callback));
+                 weak_ptr_factory_.GetWeakPtr(), callback);
+
+  sync_context_->GetWorkerTaskRunner()->PostTask(
+      FROM_HERE,
+      CreateComposedFunction(
+          base::Bind(&drive::util::GetMd5Digest, local_path_),
+          RelayCallbackToTaskRunner(
+              sync_context_->GetWorkerTaskRunner(), FROM_HERE,
+              did_calculate_callback)));
 }
 
 void LocalToRemoteSyncer::DidGetMD5ForUpload(
