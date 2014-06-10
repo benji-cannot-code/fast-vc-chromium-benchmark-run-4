085cbe6393c75b7ac9f56117ed4ef8c0ceed018b
kouhei@chromium.org <kouhei@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-10 18:46:08 +0000

    Remove param |context_type| from InitOriginPermissions
    This CL is to prepare for https://codereview.chromium.org/315143003 .
    
    This param |context_type| was added in https://codereview.chromium.org/169053005 ,
    but its only usage in |ChromeExtensionsDispatcherDelegate::InitOriginPermissions|
    seems unnecesary. The reason is that |WebSecurityPolicy::addOriginAccessWhitelistEntry|
    operates on the global whitelist map valid for all context within
    the renderer process.
    
    BUG=380346, 380502
    
    Review URL: https://codereview.chromium.org/315413004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276101 0039d316-1c4b-4281-b951-d872f2087c98

--- extensions/renderer/dispatcher.cc
+++ extensions/renderer/dispatcher.cc
@@ -260,7 +260,7 @@ void Dispatcher::DidCreateScriptContext(
   script_context_set_.Add(context);
 
   if (extension) {
-    InitOriginPermissions(extension, context_type);
+    InitOriginPermissions(extension);
   }
 
   {
@@ -798,9 +798,9 @@ void Dispatcher::UpdateActiveExtensions() {
   delegate_->OnActiveExtensionsUpdated(active_extensions);
 }
 
-void Dispatcher::InitOriginPermissions(const Extension* extension,
-                                       Feature::Context context_type) {
-  delegate_->InitOriginPermissions(extension, context_type);
+void Dispatcher::InitOriginPermissions(const Extension* extension) {
+  delegate_->InitOriginPermissions(extension,
+                                   IsExtensionActive(extension->id()));
   UpdateOriginPermissions(
       UpdatedExtensionPermissionsInfo::ADDED,
       extension,
--- extensions/renderer/dispatcher.h
+++ extensions/renderer/dispatcher.h
@@ -190,8 +190,7 @@ class Dispatcher : public content::RenderProcessObserver {
   void UpdateActiveExtensions();
 
   // Sets up the host permissions for |extension|.
-  void InitOriginPermissions(const Extension* extension,
-                             Feature::Context context_type);
+  void InitOriginPermissions(const Extension* extension);
   void UpdateOriginPermissions(UpdatedExtensionPermissionsInfo::Reason reason,
                                const Extension* extension,
                                const URLPatternSet& origins);
