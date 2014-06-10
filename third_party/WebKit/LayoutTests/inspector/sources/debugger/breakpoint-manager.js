54eb9b4ee07a75e5e313a5dfe43514c4de7da5f9
sergeyv@chromium.org <sergeyv@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-10 15:09:13 +0000

    [WIP]Devtools: Support disabling and enabling of debugger in TargetBreakpoints
    BUG=
    
    Review URL: https://codereview.chromium.org/323503002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175897 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/inspector/sources/debugger/breakpoint-manager.js
+++ third_party/WebKit/LayoutTests/inspector/sources/debugger/breakpoint-manager.js
@@ -42,6 +42,11 @@ InspectorTest.DebuggerModelMock.prototype = {
         return this._target;
     },
 
+    debuggerEnabled: function()
+    {
+        return true;
+    },
+
     _addScript: function(scriptId, url)
     {
         this._scripts[scriptId] = new WebInspector.Script(this._target, scriptId, url);
