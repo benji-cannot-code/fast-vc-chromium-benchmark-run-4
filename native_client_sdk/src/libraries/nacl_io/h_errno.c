1c0e8a09e5d1aa8721cc69dbbe8a7573438aa559
mseaborn@chromium.org <mseaborn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-10 14:45:42 +0000

    NaCl: Fix nacl_io library to build with -Wstrict-prototypes
    BUG=https://code.google.com/p/nativeclient/issues/detail?id=3114
    TEST=compile "nacl_io_untrusted" ninja target
    
    Review URL: https://codereview.chromium.org/320103004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276023 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/libraries/nacl_io/h_errno.c
+++ native_client_sdk/src/libraries/nacl_io/h_errno.c
@@ -13,11 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 static pthread_key_t s_h_errno_key;
 static pthread_once_t s_h_errno_once = PTHREAD_ONCE_INIT;
 
-static void __h_errno_create() {
+static void __h_errno_create(void) {
   pthread_key_create(&s_h_errno_key, NULL);
 }
 
-int *__h_errno_location() {
+int *__h_errno_location(void) {
   int* h_errno_ptr;
   pthread_once(&s_h_errno_once, __h_errno_create);
   h_errno_ptr = (int *) pthread_getspecific(s_h_errno_key);
@@ -32,7 +32,7 @@ int *__h_errno_location() {
 }
 
 #if defined(__BIONIC__)
-int *__get_h_errno() {
+int *__get_h_errno(void) {
   return __h_errno_location();
 }
 #endif
