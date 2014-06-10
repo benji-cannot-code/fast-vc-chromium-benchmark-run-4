741776577cf9992aa7d733a1cdc079973b2da74a
simonb@chromium.org <simonb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-10 15:57:52 +0000

    Add a host tool to pack R_ARM_RELATIVE relocations in libchrome.<ver>.so.
    R_ARM_RELATIVE relocations are the bulk of dynamic relocations (the .rel.dyn
    section) in libchrome.<version>.so.  The ELF standard representation of them
    is wasteful.
    
    Packing uses run length encoding to store them more efficiently.  Packed
    relocations are placed in a new .android.rel.dyn section.  Packing reduces
    the footprint of libchrome.<version>.so in the filesystem, in APK downloads,
    and in memory when loaded on the device.
    
    A packed libchrome.<version>.so is designed so that it can be loaded directly
    on Android, but requires the explicit support of a crazy linker that has been
    extended to understand packed relocations.
    
    A packed libchrome.<version>.so cannot currently be used with the standard
    Android runtime linker.
    
    See README.TXT and src/*.h for design and implementation notes.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/310483003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276043 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/relocation_packer/src/debug.cc
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stdarg.h>
+#include <stdio.h>
+
+#include "debug.h"
+
+namespace relocation_packer {
+
+Logger* Logger::instance_ = NULL;
+
+Logger* Logger::GetInstance() {
+  if (instance_ == NULL)
+    instance_ = new Logger;
+  return instance_;
+}
+
+void Logger::Log(const char* format, va_list args) {
+  vfprintf(stdout, format, args);
+}
+
+void Logger::Log(const char* format, ...) {
+  va_list args;
+  va_start(args, format);
+  GetInstance()->Log(format, args);
+  va_end(args);
+}
+
+void Logger::VLog(const char* format, ...) {
+  if (GetInstance()->is_verbose_) {
+    va_list args;
+    va_start(args, format);
+    GetInstance()->Log(format, args);
+    va_end(args);
+  }
+}
+
+void Logger::SetVerbose(bool flag) {
+  GetInstance()->is_verbose_ = flag;
+}
+
+}  // namespace relocation_packer
---
+++ tools/relocation_packer/src/run_all_unittests.cc
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+int main(int argc, char** argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
