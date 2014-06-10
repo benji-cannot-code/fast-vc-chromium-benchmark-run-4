ceccb550adc658dbdd901c62cefb563c94bb3dbc
zerny@chromium.org <zerny@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-10 16:41:44 +0000

    Oilpan: Make LocalFrame PersistentHeapSupplementable and use transition types for its supplements.
    R=haraken@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/323873007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175900 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/notifications/NotificationController.cpp
+++ third_party/WebKit/Source/modules/notifications/NotificationController.cpp
@@ -41,9 +41,9 @@ NotificationController::~NotificationController()
 {
 }
 
-PassOwnPtr<NotificationController> NotificationController::create(PassOwnPtr<NotificationClient> client)
+PassOwnPtrWillBeRawPtr<NotificationController> NotificationController::create(PassOwnPtr<NotificationClient> client)
 {
-    return adoptPtr(new NotificationController(client));
+    return adoptPtrWillBeNoop(new NotificationController(client));
 }
 
 NotificationClient& NotificationController::clientFrom(LocalFrame* frame)
--- third_party/WebKit/Source/platform/Supplementable.h
+++ third_party/WebKit/Source/platform/Supplementable.h
@@ -152,7 +152,7 @@ template<typename T, bool>
 class SupplementableTracing;
 
 template<typename T>
-class SupplementableTracing<T, true> : public GarbageCollectedMixin { };
+class SupplementableTracing<T, true> { };
 
 template<typename T>
 class SupplementableTracing<T, false> { };
@@ -196,6 +196,8 @@ public:
     }
 
 private:
+    // FIXME: Oilpan: Remove this ignore once PersistentHeapSupplementable is removed again.
+    GC_PLUGIN_IGNORE("")
     typename SupplementableTraits<T, isGarbageCollected>::SupplementMap m_supplements;
 
 #if !ASSERT_DISABLED
@@ -210,8 +212,25 @@ private:
 template<typename T>
 class HeapSupplement : public SupplementBase<T, true> { };
 
+// FIXME: Oilpan: Move GarbageCollectedMixin to SupplementableBase<T, true> once PersistentHeapSupplementable is removed again.
 template<typename T>
-class HeapSupplementable : public SupplementableBase<T, true> { };
+class HeapSupplementable : public SupplementableBase<T, true>, public GarbageCollectedMixin { };
+
+template<typename T>
+class PersistentHeapSupplementable : public SupplementableBase<T, true> {
+public:
+    PersistentHeapSupplementable() : m_root(this) { }
+private:
+    class TraceDelegate : PersistentBase<ThreadLocalPersistents<AnyThread>, TraceDelegate> {
+    public:
+        TraceDelegate(PersistentHeapSupplementable* owner) : m_owner(owner) { }
+        void trace(Visitor* visitor) { m_owner->trace(visitor); }
+    private:
+        PersistentHeapSupplementable* m_owner;
+    };
+
+    TraceDelegate m_root;
+};
 
 template<typename T>
 class Supplement : public SupplementBase<T, false> { };
