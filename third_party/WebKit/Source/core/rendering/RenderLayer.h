1ed4985f9d85ea6f93c54012bb3ba1a5f3f08b7a
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-10 22:16:54 +0000

    Remove unused geometry map from RenderLayer::updateLayerPositionsAfterScroll
    No one uses this geometry map. We shouldn't pay the cost of maintaining it.
    
    BUG=382770
    
    Review URL: https://codereview.chromium.org/321223003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175924 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderLayer.h
+++ third_party/WebKit/Source/core/rendering/RenderLayer.h
@@ -517,7 +517,7 @@ private:
         HasChangedAncestor = 1 << 3
     };
     typedef unsigned UpdateLayerPositionsAfterScrollFlags;
-    void updateLayerPositionsAfterScroll(RenderGeometryMap*, UpdateLayerPositionsAfterScrollFlags = NoFlag);
+    void updateLayerPositionsAfterScroll(UpdateLayerPositionsAfterScrollFlags = NoFlag);
 
     void setNextSibling(RenderLayer* next) { m_next = next; }
     void setPreviousSibling(RenderLayer* prev) { m_previous = prev; }
