ecd7444bc573ff8b07c3c94575243f85ed05465d
kouhei@chromium.org <kouhei@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-10 06:35:27 +0000

    Prepare |SVGDocumentExtensions::m_relativeLengthSVGRoots| for oilpan
    |SVGDocumentExtensions::m_relativeLengthSVGRoots| is made a HashSet<Member<SVGSVGELement> > in oilpan.
    This is valid in oilpan, because document and <svg> elements are guaranteed to die at the same time. When a <svg> element is removed from the document, |SVGSVGElement::removedFrom| would remove itself from the set.
    
    BUG=357163
    
    Review URL: https://codereview.chromium.org/323803002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175865 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGDocumentExtensions.cpp
+++ third_party/WebKit/Source/core/svg/SVGDocumentExtensions.cpp
@@ -412,8 +412,8 @@ void SVGDocumentExtensions::invalidateSVGRootsWithRelativeLengthDescendents(Subt
     TemporaryChange<bool> inRelativeLengthSVGRootsChange(m_inRelativeLengthSVGRootsInvalidation, true);
 #endif
 
-    HashSet<SVGSVGElement*>::iterator end = m_relativeLengthSVGRoots.end();
-    for (HashSet<SVGSVGElement*>::iterator it = m_relativeLengthSVGRoots.begin(); it != end; ++it)
+    WillBeHeapHashSet<RawPtrWillBeMember<SVGSVGElement> >::iterator end = m_relativeLengthSVGRoots.end();
+    for (WillBeHeapHashSet<RawPtrWillBeMember<SVGSVGElement> >::iterator it = m_relativeLengthSVGRoots.begin(); it != end; ++it)
         (*it)->invalidateRelativeLengthClients(scope);
 }
 
@@ -484,6 +484,7 @@ void SVGDocumentExtensions::trace(Visitor* visitor)
     visitor->trace(m_timeContainers);
     visitor->trace(m_svgFontFaceElements);
     visitor->trace(m_pendingSVGFontFaceElementsForRemoval);
+    visitor->trace(m_relativeLengthSVGRoots);
 }
 
 }
