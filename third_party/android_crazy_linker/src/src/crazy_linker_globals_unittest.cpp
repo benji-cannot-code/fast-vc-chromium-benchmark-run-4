ee31dd0e6e691f113ce03877cf575dd1d02e7dae
simonb@chromium.org <simonb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-11 10:14:34 +0000

    Fork of the Android NDK crazy linker.
    - Forked from:
      android/platform/ndk/2b709a01b4d9fd1302689277ccfd475cab70a5e4 
    
    BUG=
    
    Review URL: https://codereview.chromium.org/322433006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276328 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ third_party/android_crazy_linker/src/src/crazy_linker_globals_unittest.cpp
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "crazy_linker_globals.h"
+
+#include <minitest/minitest.h>
+
+#include "crazy_linker_system_mock.h"
+
+namespace crazy {
+
+TEST(Globals, Get) {
+  SystemMock sys;
+  ASSERT_TRUE(Globals::Get());
+  ASSERT_TRUE(Globals::GetLibraries());
+  ASSERT_TRUE(Globals::GetSearchPaths());
+  ASSERT_TRUE(Globals::GetRDebug());
+}
+
+}  // namespace crazy
