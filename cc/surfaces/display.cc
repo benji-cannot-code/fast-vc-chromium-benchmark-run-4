e2e6868b1a652295c6d15f747123544312bcd407
jamesr@chromium.org <jamesr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-11 11:31:49 +0000

    Fix header guard in cc/surfaces/display
    R=jbauman@chromium.org
    
    Review URL: https://codereview.chromium.org/327103002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276347 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/surfaces/display.cc
+++ cc/surfaces/display.cc
@@ -3,9 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CC_SURFACES_DISPLAY_H_
-#define CC_SURFACES_DISPLAY_H_
-
 #include "cc/surfaces/display.h"
 
 #include "base/message_loop/message_loop.h"
@@ -166,5 +163,3 @@ void Display::ReturnResources(const ReturnedResourceArray& resources) {
 }
 
 }  // namespace cc
-
-#endif  // CC_SURFACES_DISPLAY_H_
--- cc/surfaces/display.h
+++ cc/surfaces/display.h
@@ -3,6 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifndef CC_SURFACES_DISPLAY_H_
+#define CC_SURFACES_DISPLAY_H_
+
 #include "base/memory/scoped_ptr.h"
 
 #include "cc/output/output_surface_client.h"
@@ -82,3 +85,5 @@ class CC_SURFACES_EXPORT Display : public SurfaceClient,
 };
 
 }  // namespace cc
+
+#endif  // CC_SURFACES_DISPLAY_H_
