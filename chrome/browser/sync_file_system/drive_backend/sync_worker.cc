e43bc2d13d44fb0b6211107bfb3b0566b032b000
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-11 06:26:04 +0000

    [SyncFS] Fix database path of MetadataDatabase
    BUG=380615
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/329023002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276279 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_backend/sync_worker.cc
+++ chrome/browser/sync_file_system/drive_backend/sync_worker.cc
@@ -106,6 +106,7 @@ SyncWorker::SyncWorker(
       has_refresh_token_(false),
       weak_ptr_factory_(this) {
   sequence_checker_.DetachFromSequence();
+  DCHECK(base_dir_.IsAbsolute());
 }
 
 SyncWorker::~SyncWorker() {}
