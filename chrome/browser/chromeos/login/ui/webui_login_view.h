338ccf7751f38021fc8af151725df29194905d9f
yukishiino@chromium.org <yukishiino@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-11 17:25:04 +0000

    aura: Updates the text input client when native activation changes.
    It turned out that we should update the focused text input client not only when native focus/blur but also when native activate/deactivate.
    
    See crbug.com/377479 for details.
    
    Also this CL fixes the initial text input focus on CrOS login screen.
    
    BUG=323376,290701
    TEST=Confirmed that the Omnibox gets the text input focus by default when a new window is created.
    
    Review URL: https://codereview.chromium.org/294023002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276437 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/ui/webui_login_view.h
+++ chrome/browser/chromeos/login/ui/webui_login_view.h
@@ -58,6 +58,7 @@ class WebUILoginView : public views::View,
   virtual bool AcceleratorPressed(
       const ui::Accelerator& accelerator) OVERRIDE;
   virtual const char* GetClassName() const OVERRIDE;
+  virtual void RequestFocus() OVERRIDE;
 
   // Overridden from ChromeWebModalDialogManagerDelegate:
   virtual web_modal::WebContentsModalDialogHost*
