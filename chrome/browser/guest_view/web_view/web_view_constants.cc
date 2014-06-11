80e41a23253ac685b88f17becbe16a7f8392b4b0
hanxi@chromium.org <hanxi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-11 04:26:52 +0000

    Plumb file system permission into WebviewGuest.
    Two remaining issues:
    1. It seems that WebviewGuest doesn't support SharedWorker yet, so this CL
       doesn't include the changes for SharedWorker.
    2. To request file system access for operations like getFile(), removeFile() etc., I have to set "unlimitedstorage"
       permission in manifest.json, otherwise will get QuotaExceededError (request persistent storage without "unlimitedstorage" permission). Any idea?  
    
    
    BUG=343382
    
    Review URL: https://codereview.chromium.org/306473012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276251 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/guest_view/web_view/web_view_constants.cc
+++ chrome/browser/guest_view/web_view/web_view_constants.cc
@@ -52,6 +52,7 @@ const char kOldURL[] = "oldUrl";
 const char kPermission[] = "permission";
 const char kPermissionTypeDialog[] = "dialog";
 const char kPermissionTypeDownload[] = "download";
+const char kPermissionTypeFileSystem[] = "filesystem";
 const char kPermissionTypeGeolocation[] = "geolocation";
 const char kPermissionTypeLoadPlugin[] = "loadplugin";
 const char kPermissionTypeMedia[] = "media";
--- chrome/browser/renderer_host/chrome_render_message_filter.h
+++ chrome/browser/renderer_host/chrome_render_message_filter.h
@@ -76,7 +76,7 @@ class ChromeRenderMessageFilter : public content::BrowserMessageFilter {
   void OnRequestFileSystemAccessSync(int render_frame_id,
                                      const GURL& origin_url,
                                      const GURL& top_origin_url,
-                                     bool* allowed);
+                                     IPC::Message* message);
   void OnRequestFileSystemAccessAsync(int render_frame_id,
                                       int request_id,
                                       const GURL& origin_url,
