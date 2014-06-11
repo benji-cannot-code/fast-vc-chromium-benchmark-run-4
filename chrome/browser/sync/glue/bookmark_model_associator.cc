6932dd211289818203399a06aa393dd54ef5d418
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-11 11:29:05 +0000

    Documented that the new nodes at the root aren't meant to sync.
    The BookmarkClient can now provide additional nodes to the root of the
    BookmarkModel, which are optional. Currently none of those nodes should sync;
    this change makes that intention explicit in the bookmarks sync glue code.
    
    BUG=49598
    
    Review URL: https://codereview.chromium.org/319133007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276346 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/glue/bookmark_model_associator.cc
+++ chrome/browser/sync/glue/bookmark_model_associator.cc
@@ -247,6 +247,9 @@ void BookmarkModelAssociator::UpdatePermanentNodeVisibility() {
       bookmark_node_types[i],
       id_map_.find(id) != id_map_.end());
   }
+
+  // Note: the root node may have additional extra nodes. Currently their
+  // visibility is not affected by sync.
 }
 
 syncer::SyncError BookmarkModelAssociator::DisassociateModels() {
@@ -464,6 +467,9 @@ syncer::SyncError BookmarkModelAssociator::BuildAssociations(
         model_type());
   }
 
+  // Note: the root node may have additional extra nodes. Currently none of
+  // them are meant to sync.
+
   int64 bookmark_bar_sync_id = GetSyncIdFromChromeId(
       bookmark_model_->bookmark_bar_node()->id());
   DCHECK_NE(bookmark_bar_sync_id, syncer::kInvalidId);
@@ -616,6 +622,8 @@ int64 BookmarkModelAssociator::ApplyDeletesFromSyncJournal(
   dfs_stack.push(bookmark_model_->other_node());
   if (expect_mobile_bookmarks_folder_)
     dfs_stack.push(bookmark_model_->mobile_node());
+  // Note: the root node may have additional extra nodes. Currently none of
+  // them are meant to sync.
 
   // Remember folders that match delete journals in first pass but don't delete
   // them in case there are bookmarks left under them. After non-folder
