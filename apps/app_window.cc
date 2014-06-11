160e7e6a6fbf842b01b4cace5dcb677c4980b1f0
earthdok@chromium.org <earthdok@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-11 18:28:20 +0000

    Add missing member initialization in AppWindow::CreateParams.
    MemorySanitizer complained when those values were sent over IPC.
    
    BUG=383363
    R=kalman@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/327293002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276462 0039d316-1c4b-4281-b951-d872f2087c98

--- apps/app_window.cc
+++ apps/app_window.cc
@@ -158,6 +158,8 @@ AppWindow::CreateParams::CreateParams()
     : window_type(AppWindow::WINDOW_TYPE_DEFAULT),
       frame(AppWindow::FRAME_CHROME),
       has_frame_color(false),
+      active_frame_color(SK_ColorBLACK),
+      inactive_frame_color(SK_ColorBLACK),
       transparent_background(false),
       creator_process_id(0),
       state(ui::SHOW_STATE_DEFAULT),
