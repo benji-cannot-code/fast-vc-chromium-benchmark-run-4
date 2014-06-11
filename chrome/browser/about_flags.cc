45f3f2c68183fc9f5c232c3537fe9b971e82c29b
bolian@chromium.org <bolian@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-11 07:33:38 +0000

    Add an about flag and switches to enable/disable data reduction dev proxy specifically. This is Android Dev channel only.
    BUG=381409
    
    Review URL: https://codereview.chromium.org/315333003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276295 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -40,6 +40,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/native_theme/native_theme_switches.h"
 #include "ui/views/views_switches.h"
 
+#if defined(OS_ANDROID)
+#include "chrome/common/chrome_version_info.h"
+#include "components/data_reduction_proxy/common/data_reduction_proxy_switches.h"
+#endif
+
 #if defined(USE_ASH)
 #include "ash/ash_switches.h"
 #endif
@@ -1915,6 +1920,17 @@ const Experiment kExperiments[] = {
     kOsAll,
     MULTI_VALUE_TYPE(kMalwareInterstitialVersions)
   },
+#if defined(OS_ANDROID)
+  {
+    "enable-data-reduction-proxy-dev",
+    IDS_FLAGS_ENABLE_DATA_REDUCTION_PROXY_DEV_NAME,
+    IDS_FLAGS_ENABLE_DATA_REDUCTION_PROXY_DEV_DESCRIPTION,
+    kOsAndroid,
+    ENABLE_DISABLE_VALUE_TYPE(
+        data_reduction_proxy::switches::kEnableDataReductionProxyDev,
+        data_reduction_proxy::switches::kDisableDataReductionProxyDev)
+  },
+#endif
 };
 
 const Experiment* experiments = kExperiments;
@@ -2031,6 +2047,14 @@ bool SkipConditionalExperiment(const Experiment& experiment) {
            std::string("manual-enhanced-bookmarks-optout"))) {
     return true;
   }
+
+#if defined(OS_ANDROID)
+  // enable-data-reduction-proxy-dev is only available for the Dev channel.
+  if (!strcmp("enable-data-reduction-proxy-dev", experiment.internal_name) &&
+      chrome::VersionInfo::GetChannel() != chrome::VersionInfo::CHANNEL_DEV) {
+    return true;
+  }
+#endif
 
   return false;
 }
