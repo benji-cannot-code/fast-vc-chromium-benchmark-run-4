73b7fd9097e5924b78a97f1fdc95dc2acdec149c
vabr@chromium.org <vabr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-11 07:41:55 +0000

    [Password Manager] Do neither save nor fill passwords on SSL errors
    BUG=337488
    TBR=gcasto@chromium.org
    
    Review URL: https://codereview.chromium.org/311853002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276298 0039d316-1c4b-4281-b951-d872f2087c98

--- components/autofill/core/common/save_password_progress_logger.h
+++ components/autofill/core/common/save_password_progress_logger.h
@@ -100,6 +100,7 @@ class SavePasswordProgressLogger {
     STRING_PASSWORD_FORM_REAPPEARED,
     STRING_SAVING_DISABLED,
     STRING_NO_MATCHING_FORM,
+    STRING_SSL_ERRORS_PRESENT,
     STRING_INVALID,  // Represents a string returned in a case of an error.
     STRING_MAX = STRING_INVALID
   };
