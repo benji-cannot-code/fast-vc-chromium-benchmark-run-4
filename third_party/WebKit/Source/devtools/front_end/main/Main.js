benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit b2cc663c21f6ee3d85d942a780939f5caeb57b91

DevTools: Implement extension-based status bar buttons
This allows to extract code from "core" into "settings"

R=pfeldman, vsevik

Review URL: https://codereview.chromium.org/283063003

git-svn-id: svn://svn.chromium.org/blink/trunk@175358 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/devtools/front_end/main/Main.js
@@ -53,15 +53,48 @@ WebInspector.Main.prototype = {
     {
         var configuration;
         if (!Capabilities.isMainFrontend) {
-            configuration = ["components", "main", "sources", "timeline", "profiler", "console", "source_frame", "search"];
+            configuration = ["main", "sources", "timeline", "profiler", "console", "source_frame", "search"];
         } else {
-            configuration = ["components", "main", "elements", "network", "sources", "timeline", "profiler", "resources", "audits", "console", "source_frame", "extensions", "settings", "search"];
+            configuration = ["main", "elements", "network", "sources", "timeline", "profiler", "resources", "audits", "console", "source_frame", "extensions", "settings", "search"];
             if (WebInspector.experimentsSettings.layersPanel.isEnabled())
                 configuration.push("layers");
             if (WebInspector.experimentsSettings.devicesPanel.isEnabled())
                 configuration.push("devices");
         }
         WebInspector.moduleManager.registerModules(configuration);
+    },
+
+    _createGlobalStatusBarItems: function()
+    {
+        var extensions = WebInspector.moduleManager.extensions(WebInspector.StatusBarButton.Provider);
+        extensions.forEach(function(extension) {
+            var button;
+            switch (extension.descriptor()["location"]) {
+            case "toolbar-left":
+                button = createButton(extension);
+                if (button)
+                    WebInspector.inspectorView.appendToLeftToolbar(button.element);
+                break;
+            case "toolbar-right":
+                button = createButton(extension);
+                if (button)
+                    WebInspector.inspectorView.appendToRightToolbar(button.element);
+                break;
+            }
+            if (button && extension.descriptor()["actionId"]) {
+                button.addEventListener("click", function() {
+                    WebInspector.actionRegistry.execute(extension.descriptor()["actionId"]);
+                });
+            }
+        });
+
+        function createButton(extension)
+        {
+            var descriptor = extension.descriptor();
+            if (descriptor.className)
+                return extension.instance().button();
+            return new WebInspector.StatusBarButton(WebInspector.UIString(descriptor["title"]), descriptor["elementClass"]);
+        }
     },
 
     _calculateWorkerInspectorTitle: function()
@@ -285,8 +318,6 @@ WebInspector.Main.prototype = {
             screen.showModal();
         }
 
-        WebInspector.settingsController = new WebInspector.SettingsController();
-
         WebInspector.domBreakpointsSidebarPane = new WebInspector.DOMBreakpointsSidebarPane();
 
         var autoselectPanel = WebInspector.UIString("a panel chosen automatically");
@@ -314,16 +345,9 @@ WebInspector.Main.prototype = {
         WebInspector.zoomManager = new WebInspector.ZoomManager();
         WebInspector.inspectorView = new WebInspector.InspectorView();
         WebInspector.app.createRootView();
-        WebInspector.app.createGlobalStatusBarItems();
+        this._createGlobalStatusBarItems();
 
         this._addMainEventListeners(document);
-
-        function onResize()
-        {
-            if (WebInspector.settingsController)
-                WebInspector.settingsController.resize();
-        }
-        window.addEventListener("resize", onResize, true);
 
         var errorWarningCount = document.getElementById("error-warning-count");
 

third_party/WebKit/LayoutTests/http/tests/inspector-enabled/open-with-emulation-enabled-expected.txt
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 This test checks that loading with emulation turned on by default loads elements module.
 
 Loaded modules:
+    main
     elements
     console
 

third_party/WebKit/LayoutTests/inspector/initial-modules-load-expected.txt
@@ -4,20 +4,24 @@ This test validates initial set of loaded modules.
 
 Running: testInitialLoad
 Loaded modules:
+    main
     elements
 
 Running: testCreateElementsPanel
 Loaded modules:
+    main
     elements
 
 Running: testCreateNetworkPanel
 Loaded modules:
+    main
     elements
     network
     source_frame
 
 Running: testShowSourcesPanel
 Loaded modules:
+    main
     elements
     network
     sources
@@ -25,6 +29,7 @@ Loaded modules:
 
 Running: testOpenUISourceCode
 Loaded modules:
+    main
     elements
     network
     sources

third_party/WebKit/LayoutTests/inspector/open-with-rendering-option-enabled-expected.txt
@@ -2,5 +2,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 This test checks load with rendering options turned on by default.
 
 Loaded modules:
+    main
     elements
 

third_party/WebKit/Source/devtools/devtools.gyp
@@ -132,6 +132,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                             '<(PRODUCT_DIR)/resources/inspector/extensions/ExtensionServer.js',
                             '<(PRODUCT_DIR)/resources/inspector/resources/ResourcesPanel.js',
                             '<(PRODUCT_DIR)/resources/inspector/network/NetworkPanel.js',
+                            '<(PRODUCT_DIR)/resources/inspector/settings/SettingsScreen.js',
                             '<(PRODUCT_DIR)/resources/inspector/source_frame/SourceFrame.js',
                             '<(PRODUCT_DIR)/resources/inspector/sources/SourcesPanel.js',
                             '<(PRODUCT_DIR)/resources/inspector/timeline/TimelinePanel.js',
@@ -302,7 +303,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                             'destination': '<(PRODUCT_DIR)/resources/inspector/components',
                             'files': [
                                 '<@(devtools_components_js_files)',
-                                'front_end/components/module.json',
                             ],
                         },
                         {
@@ -709,12 +709,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'type': 'none',
             'conditions': [
                 ['debug_devtools==0', { # Release
+                    'actions': [{
+                        'action_name': 'build_settings_module',
+                        'script_name': 'scripts/inline_js_imports.py',
+                        'input_file': 'front_end/settings/SettingsScreen.js',
+                        'inputs': [
+                            '<@(_script_name)',
+                            '<@(devtools_settings_js_files)',
+                        ],
+                        'outputs': ['<(PRODUCT_DIR)/resources/inspector/settings/SettingsScreen.js'],
+                        'action': ['python', '<@(_script_name)', '<@(_input_file)', '<@(_outputs)'],
+                    }],
                 },
                 { # Debug
                     'copies': [
                         {
                             'destination': '<(PRODUCT_DIR)/resources/inspector/settings',
                             'files': [
+                                '<@(devtools_settings_js_files)',
                                 'front_end/settings/module.json',
                             ],
                         }

third_party/WebKit/Source/devtools/devtools.gypi
@@ -44,7 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'devtools_core_base_js_files': [
             'front_end/inspector.html',
             'front_end/Tests.js',
-            'front_end/EditFileSystemDialog.js',
             'front_end/ForwardedInputEventHandler.js',
             'front_end/InspectorFrontendAPI.js',
             'front_end/InspectorFrontendEventSink.js',
@@ -52,7 +51,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'front_end/jsdifflib.js',
             'front_end/ResponsiveDesignView.js',
             'front_end/ScreencastView.js',
-            'front_end/SettingsScreen.js',
             'front_end/TestController.js',
             'front_end/dialog.css',
             'front_end/inspector.css',
@@ -221,7 +219,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         ],
         'devtools_module_json_files': [
             'front_end/audits/module.json',
-            'front_end/components/module.json',
             'front_end/console/module.json',
             'front_end/devices/module.json',
             'front_end/elements/module.json',
@@ -292,6 +289,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         ],
         'devtools_devices_js_files': [
             'front_end/devices/DevicesView.js',
+        ],
+        'devtools_settings_js_files': [
+            'front_end/settings/EditFileSystemDialog.js',
+            'front_end/settings/SettingsScreen.js',
         ],
         'devtools_elements_js_files': [
             'front_end/elements/DOMSyntaxHighlighter.js',

third_party/WebKit/Source/devtools/front_end/components/DockController.js
@@ -45,19 +45,6 @@ WebInspector.DockController = function(canDock)
 
     WebInspector.settings.currentDockState = WebInspector.settings.createSetting("currentDockState", "");
     WebInspector.settings.lastDockState = WebInspector.settings.createSetting("lastDockState", "");
-    var states = [WebInspector.DockController.State.DockedToBottom, WebInspector.DockController.State.Undocked, WebInspector.DockController.State.DockedToRight];
-    var titles = [WebInspector.UIString("Dock to main window."), WebInspector.UIString("Undock into separate window."), WebInspector.UIString("Dock to main window.")];
-    if (WebInspector.experimentsSettings.dockToLeft.isEnabled()) {
-        states.push(WebInspector.DockController.State.DockedToLeft);
-        titles.push(WebInspector.UIString("Dock to main window."));
-    }
-    this._dockToggleButton = new WebInspector.StatusBarStatesSettingButton(
-        "dock-status-bar-item",
-        states,
-        titles,
-        WebInspector.settings.currentDockState,
-        WebInspector.settings.lastDockState,
-        this._dockSideChanged.bind(this));
 }
 
 WebInspector.DockController.State = {
@@ -79,16 +66,18 @@ WebInspector.DockController.Events = {
 WebInspector.DockController.prototype = {
     initialize: function()
     {
-        if (this._canDock)
-            this._dockToggleButton.toggleInitialState();
-    },
+        if (!this._canDock)
+            return;
 
-    /**
-     * @return {?Element}
-     */
-    get element()
-    {
-        return this._canDock ? this._dockToggleButton.element : null;
+        this._states = [WebInspector.DockController.State.DockedToBottom, WebInspector.DockController.State.Undocked, WebInspector.DockController.State.DockedToRight];
+        this._titles = [WebInspector.UIString("Dock to main window."), WebInspector.UIString("Undock into separate window."), WebInspector.UIString("Dock to main window.")];
+        if (WebInspector.experimentsSettings.dockToLeft.isEnabled()) {
+            this._states.push(WebInspector.DockController.State.DockedToLeft);
+            this._titles.push(WebInspector.UIString("Dock to main window."));
+        }
+        var initialState = WebInspector.settings.currentDockState.get();
+        initialState = this._states.indexOf(initialState) >= 0 ? initialState : this._states[0];
+        this._dockSideChanged(initialState);
     },
 
     /**
@@ -167,6 +156,37 @@ WebInspector.DockController.prototype = {
     },
 
     __proto__: WebInspector.Object.prototype
+}
+
+/**
+ * @constructor
+ * @implements {WebInspector.StatusBarButton.Provider}
+ */
+WebInspector.DockController.ButtonProvider = function()
+{
+}
+
+WebInspector.DockController.ButtonProvider.prototype = {
+    /**
+     * @return {?WebInspector.StatusBarButton}
+     */
+    button: function()
+    {
+        if (!WebInspector.dockController.canDock())
+            return null;
+
+        if (!this._dockToggleButton) {
+            this._dockToggleButton = new WebInspector.StatusBarStatesSettingButton(
+                    "dock-status-bar-item",
+                    WebInspector.dockController._states,
+                    WebInspector.dockController._titles,
+                    WebInspector.dockController.dockSide(),
+                    WebInspector.settings.currentDockState,
+                    WebInspector.settings.lastDockState,
+                    WebInspector.dockController._dockSideChanged.bind(WebInspector.dockController));
+        }
+        return this._dockToggleButton;
+    }
 }
 
 /**

third_party/WebKit/Source/devtools/front_end/components/InspectElementModeController.js
@@ -32,8 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 WebInspector.InspectElementModeController = function()
 {
-    this.toggleSearchButton = new WebInspector.StatusBarButton(WebInspector.UIString("Select an element in the page to inspect it."), "node-search-status-bar-item");
-    this.toggleSearchButton.addEventListener("click", this.toggleSearch, this);
+    this._toggleSearchButton = new WebInspector.StatusBarButton(WebInspector.UIString("Select an element in the page to inspect it."), "node-search-status-bar-item");
     this._shortcut = WebInspector.InspectElementModeController.createShortcut();
 }
 
@@ -48,7 +47,7 @@ WebInspector.InspectElementModeController.prototype = {
      */
     enabled: function()
     {
-        return this.toggleSearchButton.toggled;
+        return this._toggleSearchButton.toggled;
     },
 
     disable: function()
@@ -60,7 +59,7 @@ WebInspector.InspectElementModeController.prototype = {
     toggleSearch: function()
     {
         var enabled = !this.enabled();
-        this.toggleSearchButton.toggled = enabled;
+        this._toggleSearchButton.toggled = enabled;
 
         var targets = WebInspector.targetManager.targets();
         for (var i = 0; i < targets.length; ++i)
@@ -89,5 +88,26 @@ WebInspector.InspectElementModeController.ToggleSearchActionDelegate.prototype =
     }
 }
 
-/** @type {!WebInspector.InspectElementModeController} */
-WebInspector.inspectElementModeController;
+/**
+ * @constructor
+ * @implements {WebInspector.StatusBarButton.Provider}
+ */
+WebInspector.InspectElementModeController.ToggleButtonProvider = function()
+{
+}
+
+WebInspector.InspectElementModeController.ToggleButtonProvider.prototype = {
+    /**
+     * @return {?WebInspector.StatusBarButton}
+     */
+    button: function()
+    {
+        if (!WebInspector.inspectElementModeController)
+            return null;
+
+        return WebInspector.inspectElementModeController._toggleSearchButton;
+    }
+}
+
+/** @type {?WebInspector.InspectElementModeController} */
+WebInspector.inspectElementModeController = null;

commit 0b8b02bedeb2e9209a77cde41da4b9566cef3082

DevTools: Restore toolbar button order regressed in r175358
The Dock button should be the rightmost one, otherwise it gets overlapped
by a button to its right (Settings).

R=eustas, pfeldman@chromium.org, vsevik
BUG=383313

Review URL: https://codereview.chromium.org/329113002

git-svn-id: svn://svn.chromium.org/blink/trunk@175978 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/devtools/front_end/main/Main.js
@@ -67,6 +67,16 @@ WebInspector.Main.prototype = {
     _createGlobalStatusBarItems: function()
     {
         var extensions = WebInspector.moduleManager.extensions(WebInspector.StatusBarButton.Provider);
+
+        /**
+         * @param {!WebInspector.ModuleManager.Extension} left
+         * @param {!WebInspector.ModuleManager.Extension} right
+         */
+        function orderComparator(left, right)
+        {
+            return left.descriptor()["order"] - right.descriptor()["order"];
+        }
+        extensions.sort(orderComparator);
         extensions.forEach(function(extension) {
             var button;
             switch (extension.descriptor()["location"]) {