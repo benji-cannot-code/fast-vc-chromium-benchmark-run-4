11476a615f2c40ca18841baeb70802f5eb91f4a8
merkulova@chromium.org <merkulova@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-11 08:20:49 +0000

    Timeout for network-problems alert on OOBE increased.
    BUG=378745
    
    Review URL: https://codereview.chromium.org/322973003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276303 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/screens/network_screen.cc
+++ chrome/browser/chromeos/login/screens/network_screen.cc
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // Time in seconds for connection timeout.
-const int kConnectionTimeoutSec = 15;
+const int kConnectionTimeoutSec = 40;
 
 }  // namespace
 
