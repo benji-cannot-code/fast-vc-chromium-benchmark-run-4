008e06f17f97330d3fb5ec21187cf38d9bc958a4
hirono@chromium.org <hirono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-11 07:44:50 +0000

    Files.app: Change the OK button label of the delete confirmation dialog into 'Delete'.
    The CL also remove 1 method from the FileManager root class.
    
    BUG=375362
    TEST=manually
    R=fukino@chromium.org, mtomasz@chromium.org
    
    Review URL: https://codereview.chromium.org/326863002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276299 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/file_manager/file_manager/foreground/js/ui/file_manager_ui.js
+++ ui/file_manager/file_manager/foreground/js/ui/file_manager_ui.js
@@ -46,6 +46,12 @@ var FileManagerUI = function(element, dialogType) {
   this.confirmDialog = null;
 
   /**
+   * Confirm dialog for delete.
+   * @type {cr.ui.dialogs.ConfirmDialog}
+   */
+  this.deleteConfirmDialog = null;
+
+  /**
    * Prompt dialog.
    * @type {cr.ui.dialogs.PromptDialog}
    */
@@ -179,6 +185,8 @@ FileManagerUI.prototype.initDialogs = function() {
   this.errorDialog = new ErrorDialog(this.element_);
   this.alertDialog = new dialogs.AlertDialog(this.element_);
   this.confirmDialog = new dialogs.ConfirmDialog(this.element_);
+  this.deleteConfirmDialog = new dialogs.ConfirmDialog(this.element_);
+  this.deleteConfirmDialog.setOkLabel(str('DELETE_BUTTON_LABEL'));
   this.promptDialog = new dialogs.PromptDialog(this.element_);
   this.shareDialog = new ShareDialog(this.element_);
   this.multiProfileShareDialog = new MultiProfileShareDialog(this.element_);
