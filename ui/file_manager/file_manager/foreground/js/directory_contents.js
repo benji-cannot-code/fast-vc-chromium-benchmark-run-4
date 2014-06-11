802e0922725ca4f5a9cc90e37259fcbac93e4f6e
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-11 07:29:57 +0000

    Files.app: Refresh metadata one by one when the directory contents are changed
    Previously, when the directory contents are changed, once all the metadata were invalidated and then new metadata were fetched. But it caused flicker between invalidation and re-fetching.
    
    This patch introduces the new method to force retrieving the metadata. And with this method, the filelist refreshes the metadata one by one and flicker is not occurred.
    
    BUG=373629
    TEST=manually
    R=hirono@chromium.org
    
    Review URL: https://codereview.chromium.org/317833003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276293 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/file_manager/file_manager/foreground/js/directory_contents.js
+++ ui/file_manager/file_manager/foreground/js/directory_contents.js
@@ -716,7 +716,7 @@ DirectoryContents.prototype.onNewEntries_ = function(entries) {
  * @param {function(Object)} callback Callback on done.
  */
 DirectoryContents.prototype.prefetchMetadata = function(entries, callback) {
-  this.context_.metadataCache.get(entries, 'filesystem|drive', callback);
+  this.context_.metadataCache.getLatest(entries, 'filesystem|drive', callback);
 };
 
 /**
