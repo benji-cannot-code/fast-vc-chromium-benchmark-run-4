e569f0eed9063ccaf34faa9d4b84a4918eb5a6a4
epenner@chromium.org <epenner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-11 07:30:08 +0000

    CC: Some early outs for off-screen layers.
    This cuts off ~0.5ms on the 200 layer tough-compositor pages.
    
    BUG=383107
    
    Review URL: https://codereview.chromium.org/326033002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276294 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/layers/picture_layer_impl.cc
+++ cc/layers/picture_layer_impl.cc
@@ -804,6 +804,10 @@ void PictureLayerImpl::MarkVisibleResourcesAsRequired() const {
   // be ready to draw in order to activate without flashing content from a
   // higher res on the active tree to a lower res on the pending tree.
 
+  // First, early out for layers with no visible content.
+  if (visible_content_rect().IsEmpty())
+    return;
+
   gfx::Rect rect(visible_content_rect());
 
   float min_acceptable_scale =
@@ -1398,6 +1402,9 @@ bool PictureLayerImpl::AllTilesRequiredForActivationAreReadyToDraw() const {
     return true;
 
   if (!tilings_)
+    return true;
+
+  if (visible_content_rect().IsEmpty())
     return true;
 
   for (size_t i = 0; i < tilings_->num_tilings(); ++i) {
