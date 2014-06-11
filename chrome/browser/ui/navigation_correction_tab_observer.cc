475409eea3d520ea9711dd5731e754a192d703e7
blundell@chromium.org <blundell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-11 15:24:52 +0000

    Change google_util::GetGoogleLocale() to take in the application locale
    This function will be componentized and so cannot reference g_browser_process.
    google_util::AppendGoogleLocaleToURL() will be handled in followup work.
    
    BUG=373203
    
    Review URL: https://codereview.chromium.org/327833002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276406 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/navigation_correction_tab_observer.cc
+++ chrome/browser/ui/navigation_correction_tab_observer.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/navigation_correction_tab_observer.h"
 
 #include "base/prefs/pref_service.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/google/google_profile_helper.h"
 #include "chrome/browser/google/google_url_tracker_factory.h"
@@ -95,7 +96,7 @@ void NavigationCorrectionTabObserver::UpdateNavigationCorrectionInfo(
   rfh->Send(new ChromeViewMsg_SetNavigationCorrectionInfo(
       rfh->GetRoutingID(),
       GetNavigationCorrectionURL(),
-      google_util::GetGoogleLocale(),
+      google_util::GetGoogleLocale(g_browser_process->GetApplicationLocale()),
       google_util::GetGoogleCountryCode(
           google_profile_helper::GetGoogleHomePageURL(profile_)),
       google_apis::GetAPIKey(),
