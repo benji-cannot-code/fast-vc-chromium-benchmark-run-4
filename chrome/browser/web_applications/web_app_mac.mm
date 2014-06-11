8d24834a280966eb14b0405736e24d26ab5adb5c
jackhou@chromium.org <jackhou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-11 12:32:08 +0000

    [Mac] Add interactive App Shim test.
    This test creates shims in the user data dir and actually starts them up and
    expects them to connect to the IPC socket.
    
    BUG=168080
    
    Review URL: https://codereview.chromium.org/316493002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276368 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/web_applications/web_app_mac.mm
+++ chrome/browser/web_applications/web_app_mac.mm
@@ -48,6 +48,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/image/image_family.h"
 
+bool g_app_shims_allow_update_and_launch_in_tests = false;
+
 namespace {
 
 // Launch Services Key to run as an agent app, which doesn't launch in the dock.
@@ -858,8 +860,10 @@ base::FilePath GetAppInstallPath(const ShortcutInfo& shortcut_info) {
 }
 
 void MaybeLaunchShortcut(const ShortcutInfo& shortcut_info) {
-  if (AppShimsDisabledForTest())
+  if (AppShimsDisabledForTest() &&
+      !g_app_shims_allow_update_and_launch_in_tests) {
     return;
+  }
 
   content::BrowserThread::PostTask(
       content::BrowserThread::FILE, FROM_HERE,
@@ -971,8 +975,10 @@ void UpdatePlatformShortcuts(
     const ShortcutInfo& shortcut_info,
     const extensions::FileHandlersInfo& file_handlers_info) {
   DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::FILE));
-  if (AppShimsDisabledForTest())
+  if (AppShimsDisabledForTest() &&
+      !g_app_shims_allow_update_and_launch_in_tests) {
     return;
+  }
 
   WebAppShortcutCreator shortcut_creator(
       app_data_path, shortcut_info, file_handlers_info);
