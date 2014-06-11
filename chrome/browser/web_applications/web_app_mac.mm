8ec98a3ce2610226da18374aea4a4a4e16300a73
phoglund@chromium.org <phoglund@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-11 13:59:17 +0000

    Revert 276368 "[Mac] Add interactive App Shim test."
    The Launch test failed on mac 10.7 tests:
    http://build.chromium.org/p/chromium.mac/builders/Mac%2010.7%20Tests%20%28dbg%29%283%29/builds/24632.
    
    > [Mac] Add interactive App Shim test.
    > 
    > This test creates shims in the user data dir and actually starts them up and
    > expects them to connect to the IPC socket.
    > 
    > BUG=168080
    > 
    > Review URL: https://codereview.chromium.org/316493002
    
    TBR=jackhou@chromium.org
    
    Review URL: https://codereview.chromium.org/325123004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276385 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/web_applications/web_app_mac.mm
+++ chrome/browser/web_applications/web_app_mac.mm
@@ -48,8 +48,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/image/image_family.h"
 
-bool g_app_shims_allow_update_and_launch_in_tests = false;
-
 namespace {
 
 // Launch Services Key to run as an agent app, which doesn't launch in the dock.
@@ -860,10 +858,8 @@ base::FilePath GetAppInstallPath(const ShortcutInfo& shortcut_info) {
 }
 
 void MaybeLaunchShortcut(const ShortcutInfo& shortcut_info) {
-  if (AppShimsDisabledForTest() &&
-      !g_app_shims_allow_update_and_launch_in_tests) {
+  if (AppShimsDisabledForTest())
     return;
-  }
 
   content::BrowserThread::PostTask(
       content::BrowserThread::FILE, FROM_HERE,
@@ -975,10 +971,8 @@ void UpdatePlatformShortcuts(
     const ShortcutInfo& shortcut_info,
     const extensions::FileHandlersInfo& file_handlers_info) {
   DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::FILE));
-  if (AppShimsDisabledForTest() &&
-      !g_app_shims_allow_update_and_launch_in_tests) {
+  if (AppShimsDisabledForTest())
     return;
-  }
 
   WebAppShortcutCreator shortcut_creator(
       app_data_path, shortcut_info, file_handlers_info);
