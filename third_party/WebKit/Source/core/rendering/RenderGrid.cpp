a8fb3f8f4d409cc27ca758f45068f197f65986e8
skobes@chromium.org <skobes@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-12 21:44:23 +0000

    Create an autosizing layout scope in RenderGrid.
    BUG=341119
    
    Review URL: https://codereview.chromium.org/324183002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176056 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderGrid.cpp
+++ third_party/WebKit/Source/core/rendering/RenderGrid.cpp
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "core/rendering/RenderGrid.h"
 
+#include "core/rendering/FastTextAutosizer.h"
 #include "core/rendering/LayoutRepainter.h"
 #include "core/rendering/RenderLayer.h"
 #include "core/rendering/RenderView.h"
@@ -304,6 +305,8 @@ void RenderGrid::layoutBlock(bool relayoutChildren)
 
     setLogicalHeight(0);
     updateLogicalWidth();
+
+    FastTextAutosizer::LayoutScope fastTextAutosizerLayoutScope(this);
 
     layoutGridItems();
 
