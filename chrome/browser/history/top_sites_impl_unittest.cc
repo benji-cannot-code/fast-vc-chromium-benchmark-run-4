a4af94b427a17507a7fbc75ea9604cfd21ee6dea
fangjue23303@gmail.com <fangjue23303@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-12 07:31:17 +0000

    Rename HistoryService::NotifyRenderProcessHostDestruction
    Rename HistoryService::NotifyRenderProcessHostDestruction to
    ClearCachedDataForContextID.
    
    BUG=371827
    R=sdefresne
    TBR=sky (chrome/test/base/testing_profile.cc)
    
    Review URL: https://codereview.chromium.org/322983003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276563 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/top_sites_impl_unittest.cc
+++ chrome/browser/history/top_sites_impl_unittest.cc
@@ -238,7 +238,7 @@ class TopSitesImplTest : public HistoryUnitTestBase {
     RedirectList redirects;
     redirects.push_back(url);
     history_service()->AddPage(
-        url, base::Time::Now(), static_cast<void*>(this), 0, GURL(),
+        url, base::Time::Now(), static_cast<ContextID>(this), 0, GURL(),
         redirects, content::PAGE_TRANSITION_TYPED, history::SOURCE_BROWSED,
         false);
   }
@@ -248,7 +248,7 @@ class TopSitesImplTest : public HistoryUnitTestBase {
     RedirectList redirects;
     redirects.push_back(url);
     history_service()->AddPage(
-        url, base::Time::Now(), static_cast<void*>(this), 0, GURL(),
+        url, base::Time::Now(), static_cast<ContextID>(this), 0, GURL(),
         redirects, content::PAGE_TRANSITION_TYPED, history::SOURCE_BROWSED,
         false);
     history_service()->SetPageTitle(url, title);
@@ -260,7 +260,7 @@ class TopSitesImplTest : public HistoryUnitTestBase {
                         const history::RedirectList& redirects,
                         base::Time time) {
     history_service()->AddPage(
-        url, time, static_cast<void*>(this), 0, GURL(),
+        url, time, static_cast<ContextID>(this), 0, GURL(),
         redirects, content::PAGE_TRANSITION_TYPED, history::SOURCE_BROWSED,
         false);
     history_service()->SetPageTitle(url, title);
