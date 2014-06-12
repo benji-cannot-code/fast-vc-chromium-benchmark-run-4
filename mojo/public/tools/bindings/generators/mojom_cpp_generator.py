f505879b659609a28496de5bf1ec425291cf3732
sammc@chromium.org <sammc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-12 09:21:06 +0000

    Mojo: Fix the C++ bindings generator for methods taking imported interfaces.
    Previously, the namespace was omitted for interface and interface
    request arguments imported from another module.
    
    Review URL: https://codereview.chromium.org/332653002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276598 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/public/tools/bindings/generators/mojom_cpp_generator.py
+++ mojo/public/tools/bindings/generators/mojom_cpp_generator.py
@@ -104,9 +104,9 @@ def GetCppResultWrapperType(kind):
   if isinstance(kind, mojom.Array):
     return "mojo::Array<%s>" % GetCppArrayArgWrapperType(kind.kind)
   if isinstance(kind, mojom.Interface):
-    return "%sPtr" % kind.name
+    return "%sPtr" % GetNameForKind(kind)
   if isinstance(kind, mojom.InterfaceRequest):
-    return "mojo::InterfaceRequest<%s>" % kind.kind.name
+    return "mojo::InterfaceRequest<%s>" % GetNameForKind(kind.kind)
   if kind.spec == 's':
     return "mojo::String"
   if kind.spec == 'h':
@@ -152,9 +152,9 @@ def GetCppConstWrapperType(kind):
   if isinstance(kind, mojom.Array):
     return "mojo::Array<%s>" % GetCppArrayArgWrapperType(kind.kind)
   if isinstance(kind, mojom.Interface):
-    return "%sPtr" % kind.name
+    return "%sPtr" % GetNameForKind(kind)
   if isinstance(kind, mojom.InterfaceRequest):
-    return "mojo::InterfaceRequest<%s>" % kind.kind.name
+    return "mojo::InterfaceRequest<%s>" % GetNameForKind(kind.kind)
   if isinstance(kind, mojom.Enum):
     return GetNameForKind(kind)
   if kind.spec == 's':
@@ -238,6 +238,7 @@ class Generator(generator.Generator):
     "cpp_wrapper_type": GetCppWrapperType,
     "default_value": DefaultValue,
     "expression_to_text": ExpressionToText,
+    "get_name_for_kind": GetNameForKind,
     "get_pad": pack.GetPad,
     "has_callbacks": HasCallbacks,
     "should_inline": ShouldInlineStruct,
