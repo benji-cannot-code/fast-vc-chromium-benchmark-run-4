01e1720b91ec384cf1072c801ab6abe682e027ae
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-12 09:42:17 +0000

    Create a prominent omnibox UI element to add pages to the app launcher.
    This is experimental and behind a flag. It does not currently do
    anything except allow us to explore what kind of UI we want.
    
    BUG=378171
    
    Review URL: https://codereview.chromium.org/324043002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276608 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/location_bar/location_bar_view.h
+++ chrome/browser/ui/views/location_bar/location_bar_view.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/drag_controller.h"
 
 class ActionBoxButtonView;
+class AddToAppLauncherView;
 class CommandUpdater;
 class ContentSettingBubbleModelDelegate;
 class ContentSettingImageView;
@@ -495,6 +496,9 @@ class LocationBarView : public LocationBar,
 
   // The icon for Translate.
   TranslateIconView* translate_icon_view_;
+
+  // The view to add pages to the app launcher.
+  AddToAppLauncherView* add_to_app_launcher_view_;
 
   // The star.
   StarView* star_view_;
