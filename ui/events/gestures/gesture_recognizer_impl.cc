ae10fa0f76911c41774e0a0b03ba38350ab99139
tdresser@chromium.org <tdresser@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-12 17:32:45 +0000

    Revert 276728 "Fix slop region boundary handling."
    > Fix slop region boundary handling.
    > 
    > Ensure the screenPosition of touches is never truncated to an integer.
    > 
    > Check if the unified gesture detector is enabled before disabling
    > the inclusive slop region.
    > 
    > BUG=381174
    > TEST=RenderWidgetHostViewAuraTest.TouchEventPositionsArentRounded
    > 
    > Review URL: https://codereview.chromium.org/328733003
    
    TBR=tdresser@chromium.org
    
    Review URL: https://codereview.chromium.org/330613003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276732 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/events/gestures/gesture_recognizer_impl.cc
+++ ui/events/gestures/gesture_recognizer_impl.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/gestures/gesture_configuration.h"
 #include "ui/events/gestures/gesture_sequence.h"
 #include "ui/events/gestures/gesture_types.h"
-#include "ui/events/gestures/unified_gesture_detector_enabled.h"
 
 namespace ui {
 
@@ -71,7 +70,28 @@ GestureProviderAura* CreateGestureProvider(GestureProviderAuraClient* client) {
 // GestureRecognizerImpl, public:
 
 GestureRecognizerImpl::GestureRecognizerImpl() {
-  use_unified_gesture_detector_ = IsUnifiedGestureDetectorEnabled();
+  // Default to not using the unified gesture detector.
+  const CommandLine& command_line = *CommandLine::ForCurrentProcess();
+  const std::string unified_gd_enabled_switch =
+      command_line.HasSwitch(switches::kUnifiedGestureDetector) ?
+      command_line.GetSwitchValueASCII(switches::kUnifiedGestureDetector) :
+      switches::kUnifiedGestureDetectorAuto;
+
+  const bool kUseUnifiedGestureDetectorByDefault = false;
+  if (unified_gd_enabled_switch.empty() ||
+      unified_gd_enabled_switch == switches::kUnifiedGestureDetectorEnabled) {
+    use_unified_gesture_detector_ = true;
+  } else if (unified_gd_enabled_switch ==
+                 switches::kUnifiedGestureDetectorDisabled) {
+    use_unified_gesture_detector_ = false;
+  } else if (unified_gd_enabled_switch ==
+             switches::kUnifiedGestureDetectorAuto) {
+    use_unified_gesture_detector_ = kUseUnifiedGestureDetectorByDefault;
+  } else {
+    LOG(ERROR) << "Invalid --unified-gesture-detector option: "
+               << unified_gd_enabled_switch;
+    use_unified_gesture_detector_ = false;
+  }
 }
 
 GestureRecognizerImpl::~GestureRecognizerImpl() {
