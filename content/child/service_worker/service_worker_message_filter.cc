f26a0bb3fa0f5552a329f500e7a9e3e7cfcc506e
dominicc@chromium.org <dominicc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-12 18:27:35 +0000

    Set the "waiting" Service Worker of Service Worker providers
    This provides the Service Worker for the implementation of navigator.serviceWorker.waiting in Blink. This will be covered by subsequent layout tests in Blink.
    
    BUG=379012
    
    Review URL: https://codereview.chromium.org/317953004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276751 0039d316-1c4b-4281-b951-d872f2087c98

--- content/child/service_worker/service_worker_message_filter.cc
+++ content/child/service_worker/service_worker_message_filter.cc
@@ -62,8 +62,10 @@ void ServiceWorkerMessageFilter::OnStaleMessageReceived(
   IPC_BEGIN_MESSAGE_MAP(ServiceWorkerMessageFilter, msg)
     IPC_MESSAGE_HANDLER(ServiceWorkerMsg_ServiceWorkerRegistered,
                         OnStaleRegistered)
+    IPC_MESSAGE_HANDLER(ServiceWorkerMsg_SetWaitingServiceWorker,
+                        OnStaleSetServiceWorker)
     IPC_MESSAGE_HANDLER(ServiceWorkerMsg_SetCurrentServiceWorker,
-                        OnStaleSetCurrentServiceWorker)
+                        OnStaleSetServiceWorker)
   IPC_END_MESSAGE_MAP()
 }
 
@@ -74,7 +76,7 @@ void ServiceWorkerMessageFilter::OnStaleRegistered(
   SendServiceWorkerObjectDestroyed(thread_safe_sender_, info.handle_id);
 }
 
-void ServiceWorkerMessageFilter::OnStaleSetCurrentServiceWorker(
+void ServiceWorkerMessageFilter::OnStaleSetServiceWorker(
     int thread_id,
     int provider_id,
     const ServiceWorkerObjectInfo& info) {
