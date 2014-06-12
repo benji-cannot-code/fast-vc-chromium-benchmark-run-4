b0ee6eec7d769c6fd12098d5b164684d80cf14af
calamity@chromium.org <calamity@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-12 13:12:34 +0000

    Add search results to experimental app list start page.
    This CL moves the SearchResultListView into the StartPageView for
    the experimental app list.
    
    BUG=349727
    
    Review URL: https://codereview.chromium.org/307333002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276655 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/app_list/views/search_result_list_view.cc
+++ ui/app_list/views/search_result_list_view.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_loop.h"
 #include "base/time/time.h"
 #include "third_party/skia/include/core/SkColor.h"
+#include "ui/app_list/app_list_switches.h"
 #include "ui/app_list/app_list_view_delegate.h"
 #include "ui/app_list/views/search_result_list_view_delegate.h"
 #include "ui/app_list/views/search_result_view.h"
@@ -22,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 const int kMaxResults = 6;
+const int kExperimentAppListMaxResults = 3;
 const int kTimeoutIndicatorHeight = 2;
 const int kTimeoutFramerate = 60;
 const SkColor kTimeoutIndicatorColor = SkColorSetRGB(30, 144, 255);
@@ -44,7 +46,11 @@ SearchResultListView::SearchResultListView(
   results_container_->SetLayoutManager(
       new views::BoxLayout(views::BoxLayout::kVertical, 0, 0, 0));
 
-  for (int i = 0; i < kMaxResults; ++i)
+  int max_results = kMaxResults;
+  if (app_list::switches::IsExperimentalAppListEnabled())
+    max_results = kExperimentAppListMaxResults;
+
+  for (int i = 0; i < max_results; ++i)
     results_container_->AddChildView(new SearchResultView(this));
   AddChildView(results_container_);
 
