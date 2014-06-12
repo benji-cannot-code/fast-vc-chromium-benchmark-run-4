74567e4683292e6adfa930298954da0dbbaba8e1
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-12 23:47:57 +0000

    No longer relaunch into metro mode by default on touch devices.
    r232828 removed the touch device check in the delegate execute handler.
    It should be removed from the "You're almost done" dialog and the
    process singleton as well.
    
    BUG=314818,374360
    
    Review URL: https://codereview.chromium.org/329393004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276853 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/process_singleton_win.cc
+++ chrome/browser/process_singleton_win.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/metro.h"
 #include "base/win/registry.h"
 #include "base/win/scoped_handle.h"
-#include "base/win/win_util.h"
 #include "base/win/windows_version.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/browser_process_platform_part.h"
@@ -228,7 +227,7 @@ bool ShouldLaunchInWindows8ImmersiveMode(const base::FilePath& user_data_dir) {
                           &reg_value) == ERROR_SUCCESS) {
     return reg_value == 1;
   }
-  return base::win::IsTouchEnabledDevice();
+  return false;
 #endif
 }
 
