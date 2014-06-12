8474267570a2750a7856d338f6b183d21c0f69c3
chrishenry@google.com <chrishenry@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-12 20:27:00 +0000

    Kill wait_until support, which is unused once ClickElementAction is converted to use the new ActionRunner API.
    BUG=361809
    
    Review URL: https://codereview.chromium.org/321253004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276786 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/page/actions/page_action.py
+++ tools/telemetry/telemetry/page/actions/page_action.py
@@ -2,7 +2,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-from telemetry.page.actions import wait_until
 
 class PageActionNotSupported(Exception):
   pass
@@ -18,24 +17,11 @@ class PageAction(object):
     if attributes:
       for k, v in attributes.iteritems():
         setattr(self, k, v)
-    if hasattr(self, 'wait_until'):
-      self.wait_until = wait_until.WaitUntil(self, self.wait_until)
-    else:
-      self.wait_until = None
 
   def WillRunAction(self, tab):
     """Override to do action-specific setup before
     Test.WillRunAction is called."""
     pass
-
-  def WillWaitAfterRun(self):
-    return self.wait_until is not None
-
-  def RunActionAndMaybeWait(self, tab):
-    if self.wait_until:
-      self.wait_until.RunActionAndWait(tab)
-    else:
-      self.RunAction(tab)
 
   def RunAction(self, tab):
     raise NotImplementedError()
