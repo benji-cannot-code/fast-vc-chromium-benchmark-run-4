3c64181fbc6e29bf75aabe55b58023e7870d950b
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-12 20:27:42 +0000

    Load the default favicon as a native image, rather than as a Skia image.
    This silences a line of log spew that's generated on every launch of a local
    debug build.
    
    BUG=none
    TEST=less log spew
    R=rsesek@chromium.org
    TBR=jam@chromium.org
    
    Review URL: https://codereview.chromium.org/333493002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276787 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/search_engines/template_url_table_model.cc
+++ chrome/browser/ui/search_engines/template_url_table_model.cc
@@ -37,7 +37,7 @@ static const int kExtensionGroupID = 2;
 // ModelEntry also tracks state information about the URL.
 
 // Icon used while loading, or if a specific favicon can't be found.
-static gfx::ImageSkia* default_icon = NULL;
+static const gfx::ImageSkia* default_icon = NULL;
 
 class ModelEntry {
  public:
@@ -47,7 +47,7 @@ class ModelEntry {
         model_(model) {
     if (!default_icon) {
       default_icon = ResourceBundle::GetSharedInstance().
-          GetImageSkiaNamed(IDR_DEFAULT_FAVICON);
+          GetNativeImageNamed(IDR_DEFAULT_FAVICON).ToImageSkia();
     }
   }
 
