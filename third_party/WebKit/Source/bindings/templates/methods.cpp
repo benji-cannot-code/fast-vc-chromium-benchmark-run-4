14d1f91da74f180fd1b9d5c06195b05ee859c253
jl@opera.com <jl@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-12 14:33:00 +0000

    IDL: support per-overload [RuntimeEnabled] extended attribute
    If different overloads of a method have different [RuntimeEnabled]
    extended attributes, check the runtime flags as part of overload
    resolution instead of when configuring the prototype object template.
    If the runtime feature is disabled, the overload resolution algorithm
    will act as if the method did not exist.
    
    If all overloads have the same [RuntimeEnabled] extended attribute,
    it is checked when configured the prototype object template as for
    regular non-overloaded methods.
    
    BUG=339000
    
    Review URL: https://codereview.chromium.org/299203002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176028 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/templates/methods.cpp
+++ third_party/WebKit/Source/bindings/templates/methods.cpp
@@ -348,10 +348,12 @@ static void {{overloads.name}}Method{{world_suffix}}(const v8::FunctionCallbackI
     switch (std::min({{overloads.maxarg}}, info.Length())) {
     {# 3. Remove from S all entries whose type list is not of length argcount. #}
     {% for length, tests_methods in overloads.length_tests_methods %}
+    {# 10. If i = d, then: #}
     case {{length}}:
         {# Then resolve by testing argument #}
         {% for test, method in tests_methods %}
-        {# 10. If i = d, then: #}
+        {% filter runtime_enabled(not overloads.runtime_enabled_function_all and
+                                  method.runtime_enabled_function) %}
         if ({{test}}) {
             {% if method.measure_as and not overloads.measure_all_as %}
             UseCounter::count(callingExecutionContext(isolate), UseCounter::{{method.measure_as}});
@@ -362,6 +364,7 @@ static void {{overloads.name}}Method{{world_suffix}}(const v8::FunctionCallbackI
             {{method.name}}{{method.overload_index}}Method{{world_suffix}}(info);
             return;
         }
+        {% endfilter %}
         {% endfor %}
         break;
     {% endfor %}
