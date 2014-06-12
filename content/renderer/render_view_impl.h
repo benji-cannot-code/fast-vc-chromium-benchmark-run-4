6d76cce51e08dccaf4955c9d232d6c4941d8f9db
nasko@chromium.org <nasko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-12 16:46:58 +0000

    Remove RenderViewImpl::FrameDidCommitProvisionalLoad
    BUG=304341
    
    Review URL: https://codereview.chromium.org/324403007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276712 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_view_impl.h
+++ content/renderer/render_view_impl.h
@@ -202,6 +202,8 @@ class CONTENT_EXPORT RenderViewImpl
   // Returns the RenderViewImpl for the given routing ID.
   static RenderViewImpl* FromRoutingID(int routing_id);
 
+  static size_t GetRenderViewCount();
+
   // May return NULL when the view is closing.
   blink::WebView* webview() const;
 
@@ -275,9 +277,6 @@ class CONTENT_EXPORT RenderViewImpl
 
   void FrameDidStartLoading(blink::WebFrame* frame);
   void FrameDidStopLoading(blink::WebFrame* frame);
-
-  void FrameDidCommitProvisionalLoad(blink::WebLocalFrame* frame,
-                                     bool is_new_navigation);
 
   // Plugin-related functions --------------------------------------------------
 
@@ -796,9 +795,6 @@ class CONTENT_EXPORT RenderViewImpl
   // frames, the frame whose size is image_size is returned. If the image
   // doesn't have a frame at the specified size, the first is returned.
   bool DownloadFavicon(int id, const GURL& image_url, int image_size);
-
-  // Returns the URL being loaded by the given frame's request.
-  GURL GetLoadingUrl(blink::WebFrame* frame) const;
 
   // Called to get the WebPlugin to handle find requests in the document.
   // Returns NULL if there is no such WebPlugin.
