4a75d4b9d0c5bd425e171715d320d09d78dec8b3
skobes@chromium.org <skobes@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-12 21:25:16 +0000

    Descend into inner blocks for inflateAutoTable.
    BUG=371611
    
    Review URL: https://codereview.chromium.org/320393003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176047 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/FastTextAutosizer.h
+++ third_party/WebKit/Source/core/rendering/FastTextAutosizer.h
@@ -108,6 +108,11 @@ private:
         ContinueLayout
     };
 
+    enum InflateBehavior {
+        ThisBlockOnly,
+        DescendToInnerBlocks
+    };
+
     enum BlockFlag {
         // A block that is evaluated for becoming a cluster root.
         POTENTIAL_ROOT = 1 << 0,
@@ -246,7 +251,7 @@ private:
     void beginLayout(RenderBlock*);
     void endLayout(RenderBlock*);
     void inflateAutoTable(RenderTable*);
-    float inflate(RenderObject*, float multiplier = 0);
+    float inflate(RenderObject*, InflateBehavior = ThisBlockOnly, float multiplier = 0);
     bool shouldHandleLayout() const;
     void setAllTextNeedsLayout();
     void resetMultipliers();
