c060df30441a22c4694769b7f67a7db88c9029be
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-12 18:28:19 +0000

    Update OS version functions.
    BUG=none
    TEST=covered
    R=asvitkine@chromium.org, mark@chromium.org
    
    Review URL: https://codereview.chromium.org/325423005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276752 0039d316-1c4b-4281-b951-d872f2087c98

--- base/test/expectations/expectation.cc
+++ base/test/expectations/expectation.cc
@@ -51,7 +51,8 @@ static bool IsValidPlatform(const Platform* platform) {
         variant != "10.6" &&
         variant != "10.7" &&
         variant != "10.8" &&
-        variant != "10.9") {
+        variant != "10.9" &&
+        variant != "10.10") {
       return false;
     }
   } else if (name == "Linux") {
@@ -112,6 +113,8 @@ Platform GetCurrentPlatform() {
     platform.variant = "10.8";
   else if (base::mac::IsOSMavericks())
     platform.variant = "10.9";
+  else if (base::mac::IsOSYosemite())
+    platform.variant = "10.10";
 #elif defined(OS_CHROMEOS)
   platform.name = "ChromeOS";
 #elif defined(OS_ANDROID)
