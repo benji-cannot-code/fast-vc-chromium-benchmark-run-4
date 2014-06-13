2d870f915b075e0a83112769f9d325c69f4310cc
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-13 16:43:35 +0000

    Remove now-unnecessary #pragma optimize offs
    These should be fixed on the current version of VS2013.
    
    R=brettw@chromium.org
    BUG=288948
    
    Review URL: https://codereview.chromium.org/338523002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277037 0039d316-1c4b-4281-b951-d872f2087c98

--- base/files/important_file_writer.cc
+++ base/files/important_file_writer.cc
@@ -3,12 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#if defined _MSC_VER && _MSC_VER == 1800
-// TODO(scottmg): Internal errors on VS2013 RC in LTCG. This should be removed
-// after RTM. http://crbug.com/288948
-#pragma optimize("", off)
-#endif
-
 #include "base/files/important_file_writer.h"
 
 #include <stdio.h>
