b97432558e9eb3a028e7dfd2731c72f71588e6bd
yukishiino@chromium.org <yukishiino@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-13 12:59:11 +0000

    aura: Supports text input on non UTF-8 environments.
    We have assumed that XLookupString always returns a UTF-8 string, but it's not true.  It could be any other encoding depending on the system locale and we have to support it.
    
    The root cause of the Issue 376893 is
    1. XLookupString returns a ISO-8859-1 string (for example)
    2. We converts it by UTF8ToUTF16.
    3. The resulting character code is zero.
    4. Cannot input a (non-ASCII) character.
    
    BUG=376893
    
    Review URL: https://codereview.chromium.org/323023002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277003 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/events/keycodes/keyboard_code_conversion_x.cc
+++ ui/events/keycodes/keyboard_code_conversion_x.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/logging.h"
 #include "base/strings/stringprintf.h"
+#include "base/strings/sys_string_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "ui/events/keycodes/dom4/keycode_converter.h"
 
@@ -437,9 +438,11 @@ uint16 GetCharacterFromXEvent(XEvent* xev) {
   int bytes_written = XLookupString(&xev->xkey, buf, 6, NULL, NULL);
   DCHECK_LE(bytes_written, 6);
 
-  base::string16 result;
-  return (bytes_written > 0 && base::UTF8ToUTF16(buf, bytes_written, &result) &&
-          result.length() == 1) ? result[0] : 0;
+  if (bytes_written <= 0)
+    return 0;
+  const base::string16& result = base::WideToUTF16(
+      base::SysNativeMBToWide(base::StringPiece(buf, bytes_written)));
+  return result.length() == 1 ? result[0] : 0;
 }
 
 unsigned int DefaultXKeysymFromHardwareKeycode(unsigned int hardware_code) {
