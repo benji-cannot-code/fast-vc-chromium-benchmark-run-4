093cfa0df127b8f5069a6318b63f2be4c996d06e
jln@chromium.org <jln@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-13 12:59:59 +0000

    Clean-up the SandboxSyscall interface
    Create a new Syscall class with a static method to replace SandboxSyscall()
    and clean-up some documentation.
    
    BUG=369594
    
    Review URL: https://codereview.chromium.org/330723003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277004 0039d316-1c4b-4281-b951-d872f2087c98

--- sandbox/linux/seccomp-bpf/sandbox_bpf.h
+++ sandbox/linux/seccomp-bpf/sandbox_bpf.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace sandbox {
 
+// This must match the kernel's seccomp_data structure.
 struct arch_seccomp_data {
   int nr;
   uint32_t arch;
