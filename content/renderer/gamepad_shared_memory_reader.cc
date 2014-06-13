70fa2470ebe7b000c1076997bf9c56f0e206e011
b.kelemen@samsung.com <b.kelemen@samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-13 22:06:00 +0000

    Fix race condition in gamepad that prevents it to work without a rAF loop
    GamepadSharedMemoryReader checks for user gesture. But it only does that
    when someone actually calls SampleGamepads, so in case that does not
    happen periodically we will miss it. The browser side already has
    the logic to detect user interaction so let's rely on that.
    
    BUG=380870
    
    Review URL: https://codereview.chromium.org/316013007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277102 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/gamepad_shared_memory_reader.cc
+++ content/renderer/gamepad_shared_memory_reader.cc
@@ -102,16 +102,12 @@ void GamepadSharedMemoryReader::SampleGamepads(blink::WebGamepads& gamepads) {
   memcpy(&gamepads, &read_into, sizeof(gamepads));
 
   if (!ever_interacted_with_) {
-    if (GamepadsHaveUserGesture(gamepads)) {
-      ever_interacted_with_ = true;
-    } else {
-      // Clear the connected flag if the user hasn't interacted with any of the
-      // gamepads to prevent fingerprinting. The actual data is not cleared.
-      // WebKit will only copy out data into the JS buffers for connected
-      // gamepads so this is sufficient.
-      for (unsigned i = 0; i < blink::WebGamepads::itemsLengthCap; i++)
-        gamepads.items[i].connected = false;
-    }
+    // Clear the connected flag if the user hasn't interacted with any of the
+    // gamepads to prevent fingerprinting. The actual data is not cleared.
+    // WebKit will only copy out data into the JS buffers for connected
+    // gamepads so this is sufficient.
+    for (unsigned i = 0; i < blink::WebGamepads::itemsLengthCap; i++)
+      gamepads.items[i].connected = false;
   }
 }
 
@@ -145,6 +141,9 @@ bool GamepadSharedMemoryReader::OnControlMessageReceived(
 void GamepadSharedMemoryReader::OnGamepadConnected(
     int index,
     const blink::WebGamepad& gamepad) {
+  // The browser already checks if the user actually interacted with a device.
+  ever_interacted_with_ = true;
+
   if (gamepad_listener_)
     gamepad_listener_->didConnectGamepad(index, gamepad);
 }
