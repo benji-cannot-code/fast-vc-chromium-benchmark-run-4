a9353dcf0ba61327bfcca67a00c9447d6c489a85
alekseys@chromium.org <alekseys@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-13 22:00:57 +0000

    Add paper size reporting for CUPS printers (to let user to select one in the Print Preview).
    BUG=239879
    
    Review URL: https://codereview.chromium.org/334763002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277098 0039d316-1c4b-4281-b951-d872f2087c98

--- printing/backend/print_backend.h
+++ printing/backend/print_backend.h
@@ -53,7 +53,8 @@ struct PRINTING_EXPORT PrinterSemanticCapsAndDefaults {
   ColorModel bw_model;
 
   struct Paper {
-    std::string name;
+    std::string display_name;
+    std::string vendor_id;
     gfx::Size size_um;
   };
   std::vector<Paper> papers;
