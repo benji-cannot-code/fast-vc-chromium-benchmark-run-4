9c19bf31646795be1818f5eb87fc55bfd96d2aeb
jiayl@chromium.org <jiayl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-13 21:30:40 +0000

    Add WebRtc logging in IpcPacketSocket when the socket is blocked or unblocked. This is for investigating an issue with unexpectedly frequent EWOULDBLOCK error.
    BUG=
    
    Review URL: https://codereview.chromium.org/325023002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277081 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/p2p/ipc_socket_factory.cc
+++ content/renderer/p2p/ipc_socket_factory.cc
@@ -12,7 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/debug/trace_event.h"
 #include "base/message_loop/message_loop.h"
 #include "base/message_loop/message_loop_proxy.h"
+#include "base/strings/stringprintf.h"
 #include "base/threading/non_thread_safe.h"
+#include "content/renderer/media/webrtc_logging.h"
 #include "content/renderer/p2p/host_address_request.h"
 #include "content/renderer/p2p/socket_client_delegate.h"
 #include "content/renderer/p2p/socket_client_impl.h"
@@ -311,7 +313,14 @@ int IpcPacketSocket::SendTo(const void *data, size_t data_size,
                          TRACE_EVENT_SCOPE_THREAD,
                          "id",
                          client_->GetSocketID());
-    writable_signal_expected_ = true;
+    if (!writable_signal_expected_) {
+      WebRtcLogMessage(base::StringPrintf(
+          "IpcPacketSocket: sending is blocked. %d packets_in_flight.",
+          static_cast<int>(in_flight_packet_sizes_.size())));
+
+      writable_signal_expected_ = true;
+    }
+
     error_ = EWOULDBLOCK;
     return -1;
   }
@@ -463,11 +472,17 @@ void IpcPacketSocket::OnSendComplete() {
 
   CHECK(!in_flight_packet_sizes_.empty());
   send_bytes_available_ += in_flight_packet_sizes_.front();
+
   DCHECK_LE(send_bytes_available_, kMaximumInFlightBytes);
+
   in_flight_packet_sizes_.pop_front();
   TraceSendThrottlingState();
 
   if (writable_signal_expected_ && send_bytes_available_ > 0) {
+    WebRtcLogMessage(base::StringPrintf(
+        "IpcPacketSocket: sending is unblocked. %d packets in flight.",
+        static_cast<int>(in_flight_packet_sizes_.size())));
+
     SignalReadyToSend(this);
     writable_signal_expected_ = false;
   }
