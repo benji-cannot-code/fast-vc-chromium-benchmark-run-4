ffdb2fa598411ea3d01aeaddc398b8c8cbb06c0a
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-13 19:02:32 +0000

    [Telemetry] Use total power measurement calculated by `powermetrics`
    `powermetrics` reports the total amount of power used over the measurement period in Joules.  Use that instead of calculating the number ourselves based on the reported average.
    
    Also fix the unit tests.
    
    BUG=None
    R=tonyg@chromium.org
    
    Review URL: https://codereview.chromium.org/337753002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277065 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/platform/power_monitor/powermetrics_power_monitor_unittest.py
+++ tools/telemetry/telemetry/core/platform/power_monitor/powermetrics_power_monitor_unittest.py
@@ -49,7 +49,6 @@ class PowerMetricsPowerMonitorTest(unittest.TestCase):
     # Supported hardware reports power samples and energy consumption.
     result = getOutput('powermetrics_output.output')
 
-    self.assertTrue(len(result['power_samples_mw']) > 1)
     self.assertTrue(result['energy_consumption_mwh'] > 0)
 
     # Verify that all component entries exist in output.
@@ -60,5 +59,4 @@ class PowerMetricsPowerMonitorTest(unittest.TestCase):
 
     # Unsupported hardware doesn't.
     result = getOutput('powermetrics_output_unsupported_hardware.output')
-    self.assertNotIn('power_samples_mw', result)
     self.assertNotIn('energy_consumption_mwh', result)
