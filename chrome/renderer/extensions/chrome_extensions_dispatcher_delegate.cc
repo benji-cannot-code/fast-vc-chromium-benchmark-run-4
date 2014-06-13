8a6c0047f98df59871037fa4eefbd9dff3a15cee
vabr@chromium.org <vabr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-13 21:54:36 +0000

    Remove extension-specific page load time histogram support for webrequest
    This CL removes the code responsible for providing PLT histogram data for specific extensions using the WebRequest API.
    
    Code providing such data not bound to specific extensions is kept.
    
    BUG=100411
    
    Review URL: https://codereview.chromium.org/335573003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277096 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/extensions/chrome_extensions_dispatcher_delegate.cc
+++ chrome/renderer/extensions/chrome_extensions_dispatcher_delegate.cc
@@ -51,9 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using extensions::NativeHandler;
 
 ChromeExtensionsDispatcherDelegate::ChromeExtensionsDispatcherDelegate()
-    : webrequest_adblock_(false),
-      webrequest_adblock_plus_(false),
-      webrequest_other_(false) {
+    : webrequest_used_(false) {
 }
 
 ChromeExtensionsDispatcherDelegate::~ChromeExtensionsDispatcherDelegate() {
@@ -355,10 +353,6 @@ void ChromeExtensionsDispatcherDelegate::UpdateTabSpecificPermissions(
 }
 
 void ChromeExtensionsDispatcherDelegate::HandleWebRequestAPIUsage(
-    bool adblock,
-    bool adblock_plus,
-    bool other) {
-  webrequest_adblock_ = adblock;
-  webrequest_adblock_plus_ = adblock_plus;
-  webrequest_other_ = other;
+    bool webrequest_used) {
+  webrequest_used_ = webrequest_used;
 }
