91c7cefefefe4ddc3288bd795958d0deb8a4bf67
dpranke@chromium.org <dpranke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-13 02:32:59 +0000

    Disable ActionRunnerTest.testTapElement on Win Debug.
    This test is crashing on the Win (dbg) bot on the
    chromium.webkit waterfall; the test was added in
    https://codereview.chromium.org/323833003/ and
    has never run successfully, so this may be exposing
    a real bug. Disable it temporarily to green up
    the tree while we investigate.
    
    TBR=tonyg@chromium.org
    BUG=384181
    
    Review URL: https://codereview.chromium.org/331773003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276894 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/page/actions/action_runner_unittest.py
+++ tools/telemetry/telemetry/page/actions/action_runner_unittest.py
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from telemetry import test
 from telemetry.core import util
 from telemetry.core.backends.chrome import tracing_backend
 from telemetry.core.timeline import model
@@ -169,6 +170,7 @@ class ActionRunnerTest(tab_test_case.TabTestCase):
         element_function='document.body.firstElementChild')
     self.assertEquals(3, action_runner.EvaluateJavaScript('valueToTest'))
 
+  @test.Disabled('debug')
   def testTapElement(self):
     self.Navigate('page_with_clickables.html')
     action_runner = action_runner_module.ActionRunner(self._tab)
