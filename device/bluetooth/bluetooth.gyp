c054f0e7dbb81df5abf2bc73488aec82977a04c7
keybuk@chromium.org <keybuk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-13 03:58:53 +0000

    Remove BluetoothServiceRecord base class.
    Since only a Windows implementation remains, move the methods that
    platform uses into BluetoothServiceRecordWin and delete the base
    class.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/328483004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276915 0039d316-1c4b-4281-b951-d872f2087c98

--- device/bluetooth/bluetooth.gyp
+++ device/bluetooth/bluetooth.gyp
@@ -62,8 +62,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'bluetooth_remote_gatt_service_chromeos.h',
         'bluetooth_rfcomm_channel_mac.mm',
         'bluetooth_rfcomm_channel_mac.h',
-        'bluetooth_service_record.cc',
-        'bluetooth_service_record.h',
         'bluetooth_service_record_win.cc',
         'bluetooth_service_record_win.h',
         'bluetooth_socket.cc',
--- device/bluetooth/bluetooth_device_win_unittest.cc
+++ device/bluetooth/bluetooth_device_win_unittest.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/test/test_simple_task_runner.h"
 #include "device/bluetooth/bluetooth_device_win.h"
-#include "device/bluetooth/bluetooth_service_record.h"
+#include "device/bluetooth/bluetooth_service_record_win.h"
 #include "device/bluetooth/bluetooth_socket_thread.h"
 #include "device/bluetooth/bluetooth_task_manager_win.h"
 #include "device/bluetooth/bluetooth_uuid.h"
