efd5fac5e20a9fe825e9fb7f6fe859cfa0de0e6a
zerny@chromium.org <zerny@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-13 13:20:30 +0000

    Oilpan: Make MediaControllers ExecutionContext a weak pointer.
    Cause of a flaky ASAN use-after-poison in media/video-frame-accurate-seek.html
    
    R=ager@chromium.org, haraken@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/333813004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176111 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/MediaController.h
+++ third_party/WebKit/Source/core/html/MediaController.h
@@ -85,7 +85,9 @@ public:
     bool isRestrained() const;
     bool isBlocked() const;
 
-    void clearExecutionContext() { m_executionContext = 0; }
+#if !ENABLE(OILPAN)
+    void clearExecutionContext() { m_executionContext = nullptr; }
+#endif
 
     virtual void trace(Visitor*) OVERRIDE;
 
@@ -125,7 +127,7 @@ private:
     OwnPtrWillBeMember<GenericEventQueue> m_pendingEventsQueue;
     mutable Timer<MediaController> m_clearPositionTimer;
     OwnPtr<Clock> m_clock;
-    ExecutionContext* m_executionContext;
+    RawPtrWillBeWeakMember<ExecutionContext> m_executionContext;
     Timer<MediaController> m_timeupdateTimer;
     double m_previousTimeupdateTime;
 };
