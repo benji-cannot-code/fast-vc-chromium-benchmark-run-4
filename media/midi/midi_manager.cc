c6ba00e5c01f9d6cb492eb54241a4f0a4f82f9bc
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-13 09:40:33 +0000

    Web MIDI: MidiManager crashes if a session is ended while initializing
    If a MidiManagerClient was destructed while an asynchronous MidiManager
    initialization performs, the browser process crashes when the
    initilization is finished. On EndSession(), MidiManager should remove
    the client even from |pending_clients_|.
    
    Also, this change fixes a problem that CompleteStartSession() may not be
    invoked on a MidiManagerClient if its |client_id| is confclicting with
    another MidiManagerClient.
    
    BUG=382522
    TEST=media_unittests --gtest_filter='Midi*'
    
    Review URL: https://codereview.chromium.org/323323002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276987 0039d316-1c4b-4281-b951-d872f2087c98

--- media/midi/midi_manager.cc
+++ media/midi/midi_manager.cc
@@ -44,8 +44,7 @@ void MidiManager::StartSession(MidiManagerClient* client, int client_id) {
       if (!too_many_pending_clients_exist) {
         // Call StartInitialization() only for the first request.
         session_needs_initialization = pending_clients_.empty();
-        pending_clients_.insert(
-            std::pair<int, MidiManagerClient*>(client_id, client));
+        pending_clients_.insert(std::make_pair(client, client_id));
       }
     }
   }
@@ -82,9 +81,8 @@ void MidiManager::StartSession(MidiManagerClient* client, int client_id) {
 
 void MidiManager::EndSession(MidiManagerClient* client) {
   base::AutoLock auto_lock(lock_);
-  ClientList::iterator i = clients_.find(client);
-  if (i != clients_.end())
-    clients_.erase(i);
+  clients_.erase(client);
+  pending_clients_.erase(client);
 }
 
 void MidiManager::DispatchSendMidiData(MidiManagerClient* client,
@@ -133,7 +131,6 @@ void MidiManager::CompleteInitializationInternal(MidiResult result) {
 
   base::AutoLock auto_lock(lock_);
   DCHECK(clients_.empty());
-  DCHECK(!pending_clients_.empty());
   DCHECK(!initialized_);
   initialized_ = true;
   result_ = result;
@@ -142,8 +139,8 @@ void MidiManager::CompleteInitializationInternal(MidiResult result) {
        it != pending_clients_.end();
        ++it) {
     if (result_ == MIDI_OK)
-      clients_.insert(it->second);
-    it->second->CompleteStartSession(it->first, result_);
+      clients_.insert(it->first);
+    it->first->CompleteStartSession(it->second, result_);
   }
   pending_clients_.clear();
 }
