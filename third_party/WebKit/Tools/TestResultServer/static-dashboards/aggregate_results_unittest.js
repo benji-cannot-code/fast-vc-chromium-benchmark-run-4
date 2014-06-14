512a0e363d3575e0ed1d56fd7cfd7ae88316571b
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-14 01:40:43 +0000

    Switch buildershandler over to using the json mirror.
    The json mirror is apparently better able to scale to load
    and is less dependent on the buildbot master being in
    good state.
    
    As part of this, we how have a way of querying the step
    information for the last build on a builder, so we can
    skip the request and code related to finding out the
    build number of the latest run.
    
    Change the masters data structure to store a url name
    instead of the whole URL. The url name is the master name
    to use for constructing URLs. This way buildershandler
    can use the mirror, but builders.js can continue pointing
    to the actual master for it's non-json queries.
    
    Also, get rid of the force_update code path in buildershandler.
    This seemed like a good idea at the time, but in practice it
    just masks errors by making it seem like things are working.
    The downside of deleting this is that persistent errors loading
    the JSON won't be noticed until the memcache is flushed for
    some reason. We should add monitoring of 500s to catch these
    things earlier.
    
    Review URL: https://codereview.chromium.org/337433003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176147 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/TestResultServer/static-dashboards/aggregate_results_unittest.js
+++ third_party/WebKit/Tools/TestResultServer/static-dashboards/aggregate_results_unittest.js
@@ -44,12 +44,12 @@ function setupAggregateResultsData(includeRevisonNumbers)
             {
                 "groups": [ "@ToT Blink" ],
                 "name": "ChromiumWebkit",
+                "url_name": "chromium.webkit",
                 "tests": {
                     "layout-tests": {
                         "builders": [builderName]
                     }
                 },
-                "url": "http://build.chromium.org/p/chromium.win"
             }
         ]
     });
