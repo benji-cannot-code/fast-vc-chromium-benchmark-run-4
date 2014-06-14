939a290878d88e289b0e91a305fa777adee5a4fd
girard@chromium.org <girard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-14 02:44:00 +0000

    Add UMA metrics for time spent in touchview.
    BUG=360051
    
    Review URL: https://codereview.chromium.org/233223004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277159 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/maximize_mode/maximize_mode_controller.h
+++ ash/wm/maximize_mode/maximize_mode_controller.h
@@ -10,9 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_export.h"
 #include "ash/display/display_controller.h"
 #include "ash/display/display_manager.h"
+#include "ash/shell_observer.h"
 #include "base/macros.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/observer_list.h"
+#include "base/power_monitor/power_observer.h"
 #include "ui/gfx/display.h"
 
 namespace ui {
@@ -30,7 +32,9 @@ class MaximizeModeWindowManagerTest;
 // enters and exits maximize mode when the lid is opened beyond the triggering
 // angle and rotates the display to match the device when in maximize mode.
 class ASH_EXPORT MaximizeModeController : public AccelerometerObserver,
-                                          public DisplayController::Observer {
+    public base::PowerObserver,
+    public ShellObserver,
+    public DisplayController::Observer {
  public:
   // Observer that reports changes to the state of MaximizeModeController's
   // rotation lock.
@@ -90,6 +94,15 @@ class ASH_EXPORT MaximizeModeController : public AccelerometerObserver,
   virtual void OnAccelerometerUpdated(const gfx::Vector3dF& base,
                                       const gfx::Vector3dF& lid) OVERRIDE;
 
+  // ShellObserver:
+  virtual void OnAppTerminating() OVERRIDE;
+  virtual void OnMaximizeModeStarted() OVERRIDE;
+  virtual void OnMaximizeModeEnded() OVERRIDE;
+
+  // base::PowerObserver:
+  virtual void OnSuspend() OVERRIDE;
+  virtual void OnResume() OVERRIDE;
+
   // DisplayController::Observer:
   virtual void OnDisplayConfigurationChanged() OVERRIDE;
 
@@ -117,6 +130,9 @@ class ASH_EXPORT MaximizeModeController : public AccelerometerObserver,
   // Removes MaximizeModeWindowManager and resets the display rotation if there
   // is no rotation lock.
   void LeaveMaximizeMode();
+
+  // Record UMA stats tracking touchview usage.
+  void RecordTouchViewStateTransition();
 
   // The maximized window manager (if enabled).
   scoped_ptr<MaximizeModeWindowManager> maximize_mode_window_manager_;
@@ -148,6 +164,11 @@ class ASH_EXPORT MaximizeModeController : public AccelerometerObserver,
 
   // Rotation Lock observers.
   ObserverList<Observer> observers_;
+
+  // Tracks time spent in (and out of) touchview mode.
+  base::Time last_touchview_transition_time_;
+  base::TimeDelta total_touchview_time_;
+  base::TimeDelta total_non_touchview_time_;
 
   DISALLOW_COPY_AND_ASSIGN(MaximizeModeController);
 };
