742b37badd072aa905ee2f51c1e3ee6286bfdfb4
rdevlin.cronin@chromium.org <rdevlin.cronin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-14 02:55:37 +0000

    Give GlobalErrorWithStandardBubble the choice of dismissing on deactivate
    Since some GlobalErrors need to ensure the user acknowledges them (e.g., if the
    extension has been disabled), allow each error to choose whether it is dismissed
    on deactivation.
    
    BUG=383307
    
    Review URL: https://codereview.chromium.org/328393003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277162 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_disabled_ui.cc
+++ chrome/browser/extensions/extension_disabled_ui.cc
@@ -166,6 +166,7 @@ class ExtensionDisabledGlobalError
   virtual void OnBubbleViewDidClose(Browser* browser) OVERRIDE;
   virtual void BubbleViewAcceptButtonPressed(Browser* browser) OVERRIDE;
   virtual void BubbleViewCancelButtonPressed(Browser* browser) OVERRIDE;
+  virtual bool ShouldCloseOnDeactivate() const OVERRIDE;
 
   // ExtensionUninstallDialog::Delegate implementation.
   virtual void ExtensionUninstallAccepted() OVERRIDE;
@@ -355,6 +356,13 @@ void ExtensionDisabledGlobalError::BubbleViewCancelButtonPressed(
                  uninstall_dialog_->AsWeakPtr(),
                  extension_));
 #endif  // !defined(OS_ANDROID)
+}
+
+bool ExtensionDisabledGlobalError::ShouldCloseOnDeactivate() const {
+  // Since this indicates that an extension was disabled, we should definitely
+  // have the user acknowledge it, rather than having the bubble disappear when
+  // a new window pops up.
+  return false;
 }
 
 void ExtensionDisabledGlobalError::ExtensionUninstallAccepted() {
