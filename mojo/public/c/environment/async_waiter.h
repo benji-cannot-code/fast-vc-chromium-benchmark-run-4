9109af94d62a94dee0b08b953fac5b7c12e14097
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-14 01:06:21 +0000

    Mojo: Trim #includes in mojo/public/c.
    Include mojo/public/c/system/types.h instead of .../core.h.
    
    TBR=darin@chromium.org
    
    Review URL: https://codereview.chromium.org/335923002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277147 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/public/c/environment/async_waiter.h
+++ mojo/public/c/environment/async_waiter.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MOJO_PUBLIC_C_ENVIRONMENT_ASYNC_WAITER_H_
 #define MOJO_PUBLIC_C_ENVIRONMENT_ASYNC_WAITER_H_
 
-#include "mojo/public/c/system/core.h"
+#include "mojo/public/c/system/types.h"
 
 #ifdef __cplusplus
 extern "C" {
