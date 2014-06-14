c2296137daae8141ab51a17947e979f1dba0e726
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-14 00:02:49 +0000

    Fix text color of WebsiteSettings PermissionsMenuButton
    The arrow should also be themed, which can be accomplished easily after https://codereview.chromium.org/337613002/
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/331913002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277135 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/website_settings/permission_selector_view.cc
+++ chrome/browser/ui/views/website_settings/permission_selector_view.cc
@@ -54,6 +54,7 @@ class PermissionMenuButton : public views::MenuButton,
 
   // Overridden from views::View.
   virtual void GetAccessibleState(ui::AXViewState* state) OVERRIDE;
+  virtual void OnNativeThemeChanged(const ui::NativeTheme* theme) OVERRIDE;
 
  private:
   // Overridden from views::MenuButtonListener.
@@ -75,12 +76,7 @@ PermissionMenuButton::PermissionMenuButton(const base::string16& text,
                                            bool show_menu_marker)
     : MenuButton(NULL, text, this, show_menu_marker),
       menu_model_(model) {
-  SetEnabledColor(GetNativeTheme()->GetSystemColor(
-      ui::NativeTheme::kColorId_LabelEnabledColor));
-  SetHoverColor(GetNativeTheme()->GetSystemColor(
-      ui::NativeTheme::kColorId_LabelEnabledColor));
-  SetDisabledColor(GetNativeTheme()->GetSystemColor(
-      ui::NativeTheme::kColorId_LabelDisabledColor));
+
 }
 
 PermissionMenuButton::~PermissionMenuButton() {
@@ -110,6 +106,15 @@ void PermissionMenuButton::SetText(const base::string16& text) {
 void PermissionMenuButton::GetAccessibleState(ui::AXViewState* state) {
   MenuButton::GetAccessibleState(state);
   state->value = text();
+}
+
+void PermissionMenuButton::OnNativeThemeChanged(const ui::NativeTheme* theme) {
+  SetEnabledColor(theme->GetSystemColor(
+      ui::NativeTheme::kColorId_LabelEnabledColor));
+  SetHoverColor(theme->GetSystemColor(
+      ui::NativeTheme::kColorId_LabelEnabledColor));
+  SetDisabledColor(theme->GetSystemColor(
+      ui::NativeTheme::kColorId_LabelDisabledColor));
 }
 
 void PermissionMenuButton::OnMenuButtonClicked(View* source,
