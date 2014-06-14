7a550dec7b3150f9c3f75293dd8fcc61f7cd8362
sungmann.cho@navercorp.com <sungmann.cho@navercorp.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-14 00:47:00 +0000

    Fix lint issues with GamepadList.java.
    Currently, the lint warns at:
    Error: Overriding method should call super.onDetachedFromWindow [MissingSuperCall]
    Error: Field requires API level 16 (current min is 14):
        android.content.Context#INPUT_SERVICE [InlinedApi]
    Error: Class requires API level 16 (current min is 14):
        android.hardware.input.InputManager.InputDeviceListener [NewApi]
    Error: Call requires API level 16 (current min is 14):
        android.hardware.input.InputManager#getInputDeviceIds [NewApi]
    Error: Call requires API level 16 (current min is 14):
        android.hardware.input.InputManager#registerInputDeviceListener [NewApi]
    Error: Call requires API level 16 (current min is 14):
        android.hardware.input.InputManager#unregisterInputDeviceListener [NewApi]
    
    So we need to add @SuppressLint and @TargetApi to suppress these lint errors.
    
    BUG=327768
    
    Review URL: https://codereview.chromium.org/336453002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277143 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/input/GamepadList.java
+++ content/public/android/java/src/org/chromium/content/browser/input/GamepadList.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.content.browser.input;
 
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.hardware.input.InputManager;
 import android.hardware.input.InputManager.InputDeviceListener;
@@ -36,6 +38,7 @@ public class GamepadList {
     private boolean mIsGamepadAccessed;
     private InputDeviceListener mInputDeviceListener;
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     private GamepadList() {
         mInputDeviceListener = new InputDeviceListener() {
             // Override InputDeviceListener methods
@@ -56,6 +59,7 @@ public class GamepadList {
         };
     }
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     private void initializeDevices() {
         // Get list of all the attached input devices.
         int[] deviceIds = mInputManager.getInputDeviceIds();
@@ -80,6 +84,7 @@ public class GamepadList {
         getInstance().attachedToWindow(context);
     }
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     private void attachedToWindow(Context context) {
         if (mAttachedToWindowCounter++ == 0) {
             mInputManager = (InputManager) context.getSystemService(Context.INPUT_SERVICE);
@@ -94,12 +99,14 @@ public class GamepadList {
     /**
      * Notifies the GamepadList that a {@link ContentView} is detached from it's window.
      */
+    @SuppressLint("MissingSuperCall")
     public static void onDetachedFromWindow() {
         assert ThreadUtils.runningOnUiThread();
         if (!isGamepadSupported()) return;
         getInstance().detachedFromWindow();
     }
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     private void detachedFromWindow() {
         if (--mAttachedToWindowCounter == 0) {
             synchronized (mLock) {
