246f76fe07d4f43134d353bbc069f5f7682b6663
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-14 19:34:12 +0000

    Cleanup some garden-o-matic cruft and dead code.
    The biggest change is removing RelativeTime. We were creating these,
    but not doing anything with them. These were also the only
    things that ever called setIndex on Notification, so we can remove
    index/setIndex from Notification.
    
    The rest of the patch is just removing dead code.
    
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/336033002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176156 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/GardeningServer/scripts/garden-o-matic.js
+++ third_party/WebKit/Tools/GardeningServer/scripts/garden-o-matic.js
@@ -69,10 +69,6 @@ function update()
     if (g_revisionHint)
         g_revisionHint.dismiss();
 
-    var gtestIframe = document.querySelector('#chromium-gtests iframe');
-    if (gtestIframe)
-        gtestIframe.src = gtestIframe.src;
-
     // FIXME: This should be a button with a progress element.
     var numberOfTestsAnalyzed = 0;
     var updating = new ui.notifications.Info('Loading commit data ...');
@@ -99,7 +95,7 @@ function update()
 
             Object.keys(config.builders).forEach(function(builderName) {
                 if (!model.state.resultsByBuilder[builderName])
-                    g_info.add(new ui.notifications.Info('Could not find test results for ' + builderName + ' in the last ' + config.kBuildNumberLimit + ' runs.'));
+                    g_info.add(new ui.notifications.Info('Could not find test results for ' + builderName + '.'));
             });
 
             updating.dismiss();
