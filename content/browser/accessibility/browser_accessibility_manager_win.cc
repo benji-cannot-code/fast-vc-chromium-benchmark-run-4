3a26530b70a5b62d4f943d69ac447bec78f7ab07
dmazzoni@chromium.org <dmazzoni@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-15 12:28:50 +0000

    Fix crash if legacy hwnd is NULL on win 8.
    Change r276950 (https://codereview.chromium.org/329053004/)
    fixed a bug but introduced a new crash on win 8 by not
    checking the legacy hwnd for NULL.
    
    BUG=384732,372478,379019
    TBR=ananta
    
    Review URL: https://codereview.chromium.org/337543005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277319 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/accessibility/browser_accessibility_manager_win.cc
+++ content/browser/accessibility/browser_accessibility_manager_win.cc
@@ -38,13 +38,14 @@ BrowserAccessibilityManagerWin::BrowserAccessibilityManagerWin(
     BrowserAccessibilityDelegate* delegate,
     BrowserAccessibilityFactory* factory)
     : BrowserAccessibilityManager(initial_tree, delegate, factory),
+      parent_hwnd_(NULL),
       parent_iaccessible_(parent_iaccessible),
       tracked_scroll_object_(NULL),
       accessible_hwnd_(accessible_hwnd) {
   ui::win::CreateATLModuleIfNeeded();
   if (accessible_hwnd_) {
     accessible_hwnd_->set_browser_accessibility_manager(this);
-    parent_hwnd_ = accessible_hwnd->GetParent();
+    parent_hwnd_ = accessible_hwnd_->GetParent();
   }
 }
 
@@ -75,8 +76,10 @@ ui::AXTreeUpdate BrowserAccessibilityManagerWin::GetEmptyDocument() {
 void BrowserAccessibilityManagerWin::SetAccessibleHWND(
     LegacyRenderWidgetHostHWND* accessible_hwnd) {
   accessible_hwnd_ = accessible_hwnd;
-  accessible_hwnd_->set_browser_accessibility_manager(this);
-  parent_hwnd_ = accessible_hwnd->GetParent();
+  if (accessible_hwnd_) {
+    accessible_hwnd_->set_browser_accessibility_manager(this);
+    parent_hwnd_ = accessible_hwnd_->GetParent();
+  }
 }
 
 void BrowserAccessibilityManagerWin::MaybeCallNotifyWinEvent(DWORD event,
