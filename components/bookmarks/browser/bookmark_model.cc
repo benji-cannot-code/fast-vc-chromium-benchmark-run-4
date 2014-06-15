07401b2fe5cc9c96e00255676b69cc8cbc987e05
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-15 18:32:38 +0000

    Removed deprecated methods.
    BookmarkModel::RemoveAll() has been renamed to RemoveAllUserBookmarks();
    removed the legacy method.
    
    Also removed the corresponding methods in the BookmarkModelObserver.
    
    Removed deprecated methods from the ChromeBrowserProviderClient too.
    
    BUG=49598
    
    Review URL: https://codereview.chromium.org/323183002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277333 0039d316-1c4b-4281-b951-d872f2087c98

--- components/bookmarks/browser/bookmark_model.cc
+++ components/bookmarks/browser/bookmark_model.cc
@@ -234,10 +234,6 @@ void BookmarkModel::RemoveAllUserBookmarks() {
                     BookmarkAllUserNodesRemoved(this, removed_urls));
 }
 
-void BookmarkModel::RemoveAll() {
-  RemoveAllUserBookmarks();
-}
-
 void BookmarkModel::Move(const BookmarkNode* node,
                          const BookmarkNode* new_parent,
                          int index) {
