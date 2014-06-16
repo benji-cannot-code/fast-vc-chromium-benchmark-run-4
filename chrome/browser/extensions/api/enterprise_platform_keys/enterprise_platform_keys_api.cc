18624d024721740bc8a2b4fc2d027d16ac470da3
pneubeck@chromium.org <pneubeck@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-16 14:55:37 +0000

    Replace Profile by BrowserContext in (enterprise)platformKeys.
    The Profile dependency was not required, BrowserContext is sufficient.
    
    BUG=382983
    
    Review URL: https://codereview.chromium.org/338633005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277431 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/enterprise_platform_keys/enterprise_platform_keys_api.cc
+++ chrome/browser/extensions/api/enterprise_platform_keys/enterprise_platform_keys_api.cc
@@ -55,7 +55,7 @@ EnterprisePlatformKeysInternalGenerateKeyFunction::Run() {
       base::Bind(
           &EnterprisePlatformKeysInternalGenerateKeyFunction::OnGeneratedKey,
           this),
-      GetProfile());
+      browser_context());
   return RespondLater();
 }
 
@@ -88,7 +88,7 @@ EnterprisePlatformKeysInternalSignFunction::Run() {
       params->public_key,
       params->data,
       base::Bind(&EnterprisePlatformKeysInternalSignFunction::OnSigned, this),
-      GetProfile());
+      browser_context());
   return RespondLater();
 }
 
@@ -119,7 +119,7 @@ EnterprisePlatformKeysGetCertificatesFunction::Run() {
       base::Bind(
           &EnterprisePlatformKeysGetCertificatesFunction::OnGotCertificates,
           this),
-      GetProfile());
+      browser_context());
   return RespondLater();
 }
 
@@ -171,7 +171,7 @@ EnterprisePlatformKeysImportCertificateFunction::Run() {
       base::Bind(&EnterprisePlatformKeysImportCertificateFunction::
                      OnImportedCertificate,
                  this),
-      GetProfile());
+      browser_context());
   return RespondLater();
 }
 
@@ -208,7 +208,7 @@ EnterprisePlatformKeysRemoveCertificateFunction::Run() {
       base::Bind(&EnterprisePlatformKeysRemoveCertificateFunction::
                      OnRemovedCertificate,
                  this),
-      GetProfile());
+      browser_context());
   return RespondLater();
 }
 
