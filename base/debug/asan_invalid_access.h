fdc66de490cf64eab6eed1fe3d7faa824490ab5c
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-16 09:05:15 +0000

    Export ASAN helpers for component builds.
    Fixes ASAN component build after r277201.
    
    TBR=ajwong@chromium.org
    
    Review URL: https://codereview.chromium.org/339543002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277390 0039d316-1c4b-4281-b951-d872f2087c98

--- base/debug/asan_invalid_access.h
+++ base/debug/asan_invalid_access.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_DEBUG_ASAN_INVALID_ACCESS_H_
 #define BASE_DEBUG_ASAN_INVALID_ACCESS_H_
 
+#include "base/base_export.h"
 #include "base/compiler_specific.h"
 
 namespace base {
@@ -17,13 +18,13 @@ namespace debug {
 #if defined(ADDRESS_SANITIZER) || defined(SYZYASAN)
 
 // Generates an heap buffer overflow.
-NOINLINE void AsanHeapOverflow();
+BASE_EXPORT NOINLINE void AsanHeapOverflow();
 
 // Generates an heap buffer underflow.
-NOINLINE void AsanHeapUnderflow();
+BASE_EXPORT NOINLINE void AsanHeapUnderflow();
 
 // Generates an use after free.
-NOINLINE void AsanHeapUseAfterFree();
+BASE_EXPORT NOINLINE void AsanHeapUseAfterFree();
 
 #endif  // ADDRESS_SANITIZER || SYZYASAN
 
@@ -33,11 +34,11 @@ NOINLINE void AsanHeapUseAfterFree();
 
 // Corrupts a memory block and makes sure that the corruption gets detected when
 // we try to free this block.
-NOINLINE void AsanCorruptHeapBlock();
+BASE_EXPORT NOINLINE void AsanCorruptHeapBlock();
 
 // Corrupts the heap and makes sure that the corruption gets detected when a
 // crash occur.
-NOINLINE void AsanCorruptHeap();
+BASE_EXPORT NOINLINE void AsanCorruptHeap();
 
 #endif  // SYZYASAN
 
