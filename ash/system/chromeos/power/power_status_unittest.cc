584f3b3cd06135e8f1ec4036e5f5721e084adcaa
sungmann.cho@navercorp.com <sungmann.cho@navercorp.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-16 17:26:13 +0000

    Remove unnecessary #includes in power_status_unittest.cc.
    BUG=none
    
    Review URL: https://codereview.chromium.org/336233002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277476 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/chromeos/power/power_status_unittest.cc
+++ ash/system/chromeos/power/power_status_unittest.cc
@@ -5,10 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/system/chromeos/power/power_status.h"
 
-#include <set>
-#include <string>
-
-#include "base/command_line.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/message_loop/message_loop.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
