e224c07ab2d30eef62986c87a5eaf57dd9734554
andresantoso@chromium.org <andresantoso@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-16 19:42:00 +0000

    MacViews: Fill in InputMethod access in NativeWidgetMac.
    Add NativeWidgetMac implementation of CreateInputMethod() and
     GetHostInputMethod().
    We can now pass more unit tests including TextfieldTest and ComboboxTest.
    
    BUG=374077
    
    Review URL: https://codereview.chromium.org/332893003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277521 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/cocoa/bridged_native_widget_unittest.mm
+++ ui/views/cocoa/bridged_native_widget_unittest.mm
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "testing/gtest_mac.h"
 #import "ui/gfx/test/ui_cocoa_test_helper.h"
 #import "ui/views/cocoa/bridged_content_view.h"
+#include "ui/views/ime/input_method.h"
 #include "ui/views/view.h"
 
 namespace views {
@@ -103,6 +104,15 @@ TEST_F(BridgedNativeWidgetTest, ViewSizeTracksWindow) {
                   display:NO];
   EXPECT_EQ(kTestNewWidth, view_->width());
   EXPECT_EQ(kTestNewHeight, view_->height());
+}
+
+TEST_F(BridgedNativeWidgetTest, CreateInputMethod) {
+  scoped_ptr<views::InputMethod> input_method(bridge_->CreateInputMethod());
+  EXPECT_TRUE(input_method);
+}
+
+TEST_F(BridgedNativeWidgetTest, GetHostInputMethod) {
+  EXPECT_TRUE(bridge_->GetHostInputMethod());
 }
 
 }  // namespace views
