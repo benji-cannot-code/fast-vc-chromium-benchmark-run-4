6df16d3618feca6928f5696d18db6d3ed9ee8054
eustas@chromium.org <eustas@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-16 15:07:52 +0000

    DevTools: do not send notifications when RVTDAH is reattached.
    When domain is changed in page URL,
    RenderViewDevToolsAgentHost issues notifications
    that it is detached and then attached again.
    
    This doesn't seem to be desired behaviour.
    
    BUG=245436
    
    Review URL: https://codereview.chromium.org/304453004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277440 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/devtools/render_view_devtools_agent_host.h
+++ content/browser/devtools/render_view_devtools_agent_host.h
@@ -79,6 +79,8 @@ class CONTENT_EXPORT RenderViewDevToolsAgentHost
                        const NotificationSource& source,
                        const NotificationDetails& details) OVERRIDE;
 
+  void ReattachToRenderViewHost(RenderViewHost* rvh);
+
   bool DispatchIPCMessage(const IPC::Message& message);
 
   void SetRenderViewHost(RenderViewHost* rvh);
@@ -92,6 +94,9 @@ class CONTENT_EXPORT RenderViewDevToolsAgentHost
 
   void ClientDetachedFromRenderer();
 
+  void InnerOnClientAttached();
+  void InnerClientDetachedFromRenderer();
+
   RenderViewHost* render_view_host_;
   scoped_ptr<RendererOverridesHandler> overrides_handler_;
   scoped_ptr<DevToolsTracingHandler> tracing_handler_;
@@ -101,6 +106,7 @@ class CONTENT_EXPORT RenderViewDevToolsAgentHost
 #endif
   std::string state_;
   NotificationRegistrar registrar_;
+  bool reattaching_;
 
   DISALLOW_COPY_AND_ASSIGN(RenderViewDevToolsAgentHost);
 };
