5823965a3417222a7026eee78544f68f1d2c5b94
timvolodine@chromium.org <timvolodine@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-16 17:00:15 +0000

    Battery Status API: blink promise implementation and layout tests.
    Implement Battery Status API using promises in blink as
    according to the (modified) spec:
    https://dvcs.w3.org/hg/dap/raw-file/tip/battery/Overview.html.
    Using this patch the Battery Status API is accessible using:
    navigator.getBattery().then(...) syntax.
    
    Also added layout tests for the various use-cases.
    
    BUG=122593, 360068
    
    Review URL: https://codereview.chromium.org/329723005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176222 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/battery/NavigatorBattery.h
+++ third_party/WebKit/Source/modules/battery/NavigatorBattery.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NavigatorBattery_h
 #define NavigatorBattery_h
 
+#include "bindings/v8/ScriptPromise.h"
 #include "core/frame/Navigator.h"
 #include "platform/Supplementable.h"
 #include "platform/heap/Handle.h"
@@ -22,8 +23,8 @@ public:
 
     static NavigatorBattery& from(Navigator&);
 
-    static BatteryManager* battery(Navigator&);
-    BatteryManager* batteryManager(Navigator&);
+    static ScriptPromise getBattery(ScriptState*, Navigator&);
+    ScriptPromise getBattery(ScriptState*);
 
     void trace(Visitor*);
 
