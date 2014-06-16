834b4729281c92b7e0c236fb698b853d2d598ce0
sungmann.cho@navercorp.com <sungmann.cho@navercorp.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-16 21:48:10 +0000

    Remove unnecessary codes in renderer_accessibility.cc and renderer_accessibility_complete.cc.
    BUG=none
    TEST=compile
    
    Review URL: https://codereview.chromium.org/331133005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277544 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/accessibility/renderer_accessibility.cc
+++ content/renderer/accessibility/renderer_accessibility.cc
@@ -5,14 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/renderer/accessibility/renderer_accessibility.h"
 
-#include "base/command_line.h"
 #include "content/renderer/render_view_impl.h"
-#include "third_party/WebKit/public/web/WebAXObject.h"
 #include "third_party/WebKit/public/web/WebDocument.h"
 #include "third_party/WebKit/public/web/WebFrame.h"
 #include "third_party/WebKit/public/web/WebView.h"
 
-using blink::WebAXObject;
 using blink::WebDocument;
 using blink::WebFrame;
 using blink::WebView;
--- content/renderer/accessibility/renderer_accessibility_complete.cc
+++ content/renderer/accessibility/renderer_accessibility_complete.cc
@@ -24,7 +24,6 @@ using blink::WebDocument;
 using blink::WebNode;
 using blink::WebPoint;
 using blink::WebRect;
-using blink::WebSize;
 using blink::WebView;
 
 namespace content {
