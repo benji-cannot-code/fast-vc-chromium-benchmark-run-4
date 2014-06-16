89f4eb2fea7dbf71ce8a13c779f80138383d8b35
wangxianzhu@chromium.org <wangxianzhu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-16 21:38:48 +0000

    Add testRunner.capturePixelsThen() method
    This method is similar to displayAsyncThen(), but allows the caller
    to get the captured pixel data.
    
    This is to be used in repaint tests to produce the minimum repaint
    baselines. This is useful to discover under-repaint bugs with the help
    of repaint visualization in layout test results, and to estimate
    over-repaints.
    
    R=enne
    TBR=dpranke
    
    Review URL: https://codereview.chromium.org/301243022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277540 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/renderer/test_runner/test_runner.h
+++ content/shell/renderer/test_runner/test_runner.h
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/shell/renderer/test_runner/web_test_runner.h"
 #include "v8/include/v8.h"
 
+class SkBitmap;
+
 namespace blink {
 class WebFrame;
 class WebNotificationPresenter;
@@ -532,12 +534,20 @@ class TestRunner : public WebTestRunner,
   void DisplayAsync();
   void DisplayAsyncThen(v8::Handle<v8::Function> callback);
 
+  // Similar to DisplayAsyncThen(), but pass parameters of the captured
+  // snapshot (width, height, snapshot) to the callback.
+  void CapturePixelsAsyncThen(v8::Handle<v8::Function> callback);
+
   void SetMockPushClientSuccess(const std::string& end_point,
                                 const std::string& registration_id);
   void SetMockPushClientError(const std::string& message);
 
   ///////////////////////////////////////////////////////////////////////////
   // Internal helpers
+
+  void CapturePixelsCallback(scoped_ptr<InvokeCallbackTask> task,
+                             const SkBitmap& snapshot);
+
   void CheckResponseMimeType();
   void CompleteNotifyDone();
 
