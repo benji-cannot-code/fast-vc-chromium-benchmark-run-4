9b566f8dd828f57758b7bdf4fdd9948a0f049564
sergeyv@chromium.org <sergeyv@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-16 13:24:54 +0000

    Devtools: Update breakpoints only in one specific target on UiSourceCodeMappingChanged
    BUG=
    
    Review URL: https://codereview.chromium.org/332203002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176208 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sdk/BreakpointManager.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/BreakpointManager.js
@@ -156,11 +156,12 @@ WebInspector.BreakpointManager.prototype = {
     {
         var uiSourceCode = /** @type {!WebInspector.UISourceCode} */ (event.target);
         var isIdentity = /** @type {boolean} */ (event.data.isIdentity);
+        var target = /** @type {!WebInspector.Target} */ (event.data.target);
         if (isIdentity)
             return;
         var breakpoints = this._breakpointsForPrimaryUISourceCode.get(uiSourceCode) || [];
         for (var i = 0; i < breakpoints.length; ++i)
-            breakpoints[i]._updateInDebugger();
+            breakpoints[i]._updateInDebuggerForTarget(target);
     },
 
     /**
@@ -585,7 +586,9 @@ WebInspector.BreakpointManager.Breakpoint.prototype = {
     {
         this._removeFakeBreakpointAtPrimaryLocation();
         this._fakeBreakpointAtPrimaryLocation();
-        this._updateInDebugger();
+        var targetBreakpoints = this._targetBreakpoints.values();
+        for (var i = 0; i < targetBreakpoints.length; ++i)
+            targetBreakpoints[i]._updateInDebugger();
     },
 
     /**
@@ -606,11 +609,12 @@ WebInspector.BreakpointManager.Breakpoint.prototype = {
         this._breakpointManager._targetManager.unobserveTargets(this);
     },
 
-    _updateInDebugger: function()
+    /**
+     * @param {!WebInspector.Target} target
+     */
+    _updateInDebuggerForTarget: function(target)
     {
-        var targetBreakpoints = this._targetBreakpoints.values();
-        for (var i = 0; i < targetBreakpoints.length; ++i)
-            targetBreakpoints[i]._updateInDebugger();
+        this._targetBreakpoints.get(target)._updateInDebugger();
     },
 
     /**
