1c238ee8f8a7e722022c83a432d76cfac7ff2d17
shuchen@chromium.org <shuchen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-16 15:53:56 +0000

    Moves DetermineAndSaveHardwareKeyboard method from LoginDisplayHostImpl to InputMethodManager.
    BUG=379696
    TEST=None
    
    Review URL: https://codereview.chromium.org/333863002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277455 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/input_method/mock_input_method_manager.cc
+++ chrome/browser/chromeos/input_method/mock_input_method_manager.cc
@@ -116,6 +116,10 @@ void MockInputMethodManager::SetEnabledExtensionImes(
 void MockInputMethodManager::SetInputMethodLoginDefault() {
 }
 
+void MockInputMethodManager::SetInputMethodLoginDefaultFromVPD(
+    const std::string& locale, const std::string& layout) {
+}
+
 bool MockInputMethodManager::SwitchToNextInputMethod() {
   return true;
 }
