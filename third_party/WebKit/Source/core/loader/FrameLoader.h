e0cdd9ecfa4a4a4ca3f36f7ba48f46b67cf5bb1d
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-16 15:39:07 +0000

    DevTools: Load document (html) content from disk cache in page agent enabling.
    BUG=354447
    R=pfeldman@chromium.org
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=174557
    
    Review URL: https://codereview.chromium.org/267393003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176213 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/FrameLoader.h
+++ third_party/WebKit/Source/core/loader/FrameLoader.h
@@ -191,6 +191,8 @@ public:
 
     void restoreScrollPositionAndViewState();
 
+    bool requestFromHistoryForInspector(ResourceRequestCachePolicy, ResourceRequest*);
+
 private:
     bool allChildrenAreComplete() const; // immediate children, not all descendants
 
