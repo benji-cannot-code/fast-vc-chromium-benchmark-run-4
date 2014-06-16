1c2c319d21455d968b387ef3d57566ce4a6e629b
tyoshino@chromium.org <tyoshino@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-16 22:32:10 +0000

    Make Ctor/Dtor of ExecutionContext protected.
    BUG=305497
    
    Review URL: https://codereview.chromium.org/328263003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176248 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/ExecutionContext.h
+++ third_party/WebKit/Source/core/dom/ExecutionContext.h
@@ -67,8 +67,6 @@ class ExecutionContext
     , public LifecycleContext<ExecutionContext>
     , public Supplementable<ExecutionContext> {
 public:
-    ExecutionContext();
-    virtual ~ExecutionContext();
     virtual void trace(Visitor*);
 
     // Delegating to ExecutionContextClient
@@ -137,6 +135,9 @@ public:
     virtual EventQueue* eventQueue() const = 0;
 
 protected:
+    ExecutionContext();
+    virtual ~ExecutionContext();
+
     void setClient(ExecutionContextClient* client) { m_client = client; }
 
     virtual const KURL& virtualURL() const = 0;
