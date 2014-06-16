21c403e87fd010d1c78dc9cf5e2d589f64d74949
sigbjornf@opera.com <sigbjornf@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-16 12:03:13 +0000

    Oilpan: have GeolocationInspectorAgent persistently track its controllers.
    With GeolocationController (now back) on the heap, maintain a persistent
    set of them from GeolocationInspectorAgent.
    
    R=haraken@chromium.org,ager@chromium.org,zerny@chromium.org
    BUG=304341
    
    Review URL: https://codereview.chromium.org/332153002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176202 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/geolocation/testing/GeolocationClientMock.h
+++ third_party/WebKit/Source/modules/geolocation/testing/GeolocationClientMock.h
@@ -75,7 +75,8 @@ private:
 
     void clearError();
 
-    HashSet<GeolocationController*> m_controllers;
+    typedef WillBeHeapHashSet<RawPtrWillBeMember<GeolocationController> > GeolocationControllers;
+    WillBePersistentHeapHashSet<RawPtrWillBeMember<GeolocationController> > m_controllers;
     Persistent<GeolocationPosition> m_lastPosition;
     bool m_hasError;
     String m_errorMessage;
@@ -90,8 +91,8 @@ private:
     };
 
     PermissionState m_permissionState;
-    typedef PersistentHeapHashSet<Member<Geolocation> > GeolocationSet;
-    GeolocationSet m_pendingPermissions;
+    typedef HeapHashSet<Member<Geolocation> > GeolocationSet;
+    PersistentHeapHashSet<Member<Geolocation> > m_pendingPermissions;
 };
 
 }
