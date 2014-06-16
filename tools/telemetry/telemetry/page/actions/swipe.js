81ab0fa844eff7e3bc7490798ea3b0c8f6939429
chrishenry@google.com <chrishenry@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-16 09:04:04 +0000

    Add SwipePage/SwipeElement API to action_runner, wrapping over SwipeAction.
    SwipeAction now also able to perform swipe on element with
    selector and text (not just element_function).
    
    SwipeAction no longer accepts attributes and instead accept
    normal parameters.
    
    Added unit tests.
    
    BUG=361809
    
    Review URL: https://codereview.chromium.org/337643002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277389 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/page/actions/swipe.js
+++ tools/telemetry/telemetry/page/actions/swipe.js
@@ -9,15 +9,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   function SwipeGestureOptions(opt_options) {
     if (opt_options) {
       this.element_ = opt_options.element;
-      this.left_start_percentage_ = opt_options.left_start_percentage;
-      this.top_start_percentage_ = opt_options.top_start_percentage;
+      this.left_start_ratio_ = opt_options.left_start_ratio;
+      this.top_start_ratio_ = opt_options.top_start_ratio;
       this.direction_ = opt_options.direction;
       this.distance_ = opt_options.distance;
       this.speed_ = opt_options.speed;
     } else {
       this.element_ = document.body;
-      this.left_start_percentage_ = 0.5;
-      this.top_start_percentage_ = 0.5;
+      this.left_start_ratio_ = 0.5;
+      this.top_start_ratio_ = 0.5;
       this.direction_ = 'left';
       this.distance_ = 0;
       this.speed_ = 800;
@@ -53,9 +53,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     var rect = __GestureCommon_GetBoundingVisibleRect(this.options_.element_);
     var start_left =
-        rect.left + rect.width * this.options_.left_start_percentage_;
+        rect.left + rect.width * this.options_.left_start_ratio_;
     var start_top =
-        rect.top + rect.height * this.options_.top_start_percentage_;
+        rect.top + rect.height * this.options_.top_start_ratio_;
     chrome.gpuBenchmarking.swipe(this.options_.direction_,
                                  this.options_.distance_,
                                  this.onGestureComplete_.bind(this),
