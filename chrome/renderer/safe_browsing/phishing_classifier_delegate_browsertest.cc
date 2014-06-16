65d1a36cd328d7eb9c484cd3b7fa94cb2cca5f42
sorin@chromium.org <sorin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-16 17:39:41 +0000

    Fixed typo ADDRESS_SANITIZER TBR=shess@chromium.org BUG=384583 NOTRY=true
    Review URL: https://codereview.chromium.org/338153002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277481 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/safe_browsing/phishing_classifier_delegate_browsertest.cc
+++ chrome/renderer/safe_browsing/phishing_classifier_delegate_browsertest.cc
@@ -568,7 +568,7 @@ IN_PROC_BROWSER_TEST_F(PhishingClassifierDelegateTest,
   EXPECT_CALL(*classifier_, CancelPendingClassification());
 }
 
-#if defined(ADDERSS_SANITIZER)
+#if defined(ADDRESS_SANITIZER)
 #define Maybe_DuplicatePageCapture DISABLED_DuplicatePageCapture
 #else
 #define Maybe_DuplicatePageCapture DuplicatePageCapture
