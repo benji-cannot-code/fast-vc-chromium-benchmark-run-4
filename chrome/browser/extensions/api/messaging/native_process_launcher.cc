1c266957da5c74d2047bc9781412c69b64b0c65c
sungmann.cho@navercorp.com <sungmann.cho@navercorp.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-16 18:23:18 +0000

    Remove unnecessary #includes in native_process_launcher.cc.
    After the landing https://codereview.chromium.org/208843005,
    native_process_launcher.cc doesn't need the command line switch
    related headers anymore. So we can remove these.
    
    BUG=355197
    
    Review URL: https://codereview.chromium.org/334173002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277493 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/messaging/native_process_launcher.cc
+++ chrome/browser/extensions/api/messaging/native_process_launcher.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/sequenced_worker_pool.h"
 #include "chrome/browser/extensions/api/messaging/native_messaging_host_manifest.h"
 #include "chrome/common/chrome_paths.h"
-#include "chrome/common/chrome_switches.h"
 #include "content/public/browser/browser_thread.h"
 #include "url/gurl.h"
 
