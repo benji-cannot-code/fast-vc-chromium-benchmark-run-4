e97be3960b1144daaa9aadfa9c507d4365b92acb
yoichio@chromium.org <yoichio@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-16 09:01:19 +0000

    Change a FrameSelection::setSelectedRange argument type from bool to enum.
    Let's use FrameSelection::DirectoinalOption instead of bool.
    
    
    BUG=
    
    Review URL: https://codereview.chromium.org/328243004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176192 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/FrameSelection.cpp
+++ third_party/WebKit/Source/core/editing/FrameSelection.cpp
@@ -1408,7 +1408,7 @@ void FrameSelection::selectAll()
     notifyRendererOfSelectionChange(UserTriggered);
 }
 
-bool FrameSelection::setSelectedRange(Range* range, EAffinity affinity, bool isDirectional, SetSelectionOptions options)
+bool FrameSelection::setSelectedRange(Range* range, EAffinity affinity, DirectoinalOption directional, SetSelectionOptions options)
 {
     if (!range || !range->startContainer() || !range->endContainer())
         return false;
@@ -1419,7 +1419,7 @@ bool FrameSelection::setSelectedRange(Range* range, EAffinity affinity, bool isD
     m_logicalRange = nullptr;
     stopObservingVisibleSelectionChangeIfNecessary();
 
-    VisibleSelection newSelection(range, affinity, isDirectional);
+    VisibleSelection newSelection(range, affinity, directional == Directional);
     setSelection(newSelection, options);
 
     m_logicalRange = range->cloneRange();
