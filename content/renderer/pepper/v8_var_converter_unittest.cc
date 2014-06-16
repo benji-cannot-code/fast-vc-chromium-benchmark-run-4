03f038fe0a37d2805703fcb0be063d258e24d98c
raymes@chromium.org <raymes@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-16 07:23:40 +0000

    Change V8VarConverter::FromV8Value to complete synchronously if possible
    Previously V8VarConverter::FromV8Value would always complete asynchronously
    (we would always post a task to the message loop to run the callback). However,
    this adds latency to the transmission of postMessage'd messages. It also has
    the unfortunate consequence that posted messages can arrive out of order with
    other messages (such as view changes). We should attempt to convert
    synchronously if possible and only resort to asynchronous conversion
    if needed.
    
    BUG=367896
    
    Review URL: https://codereview.chromium.org/331593002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277374 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/pepper/v8_var_converter_unittest.cc
+++ content/renderer/pepper/v8_var_converter_unittest.cc
@@ -44,11 +44,18 @@ namespace content {
 
 namespace {
 
+void FromV8ValueComplete(const ScopedPPVar& scoped_var,
+                         bool success) {
+  NOTREACHED();
+}
+
 class MockResourceConverter : public content::ResourceConverter {
  public:
   virtual ~MockResourceConverter() {}
+  virtual void Reset() OVERRIDE {}
+  virtual bool NeedsFlush() OVERRIDE { return false; }
   virtual void Flush(const base::Callback<void(bool)>& callback) OVERRIDE {
-    callback.Run(true);
+    NOTREACHED();
   }
   virtual bool FromV8Value(v8::Handle<v8::Object> val,
                            v8::Handle<v8::Context> context,
@@ -157,7 +164,7 @@ bool Equals(const PP_Var& var, v8::Handle<v8::Value> val) {
 class V8VarConverterTest : public testing::Test {
  public:
   V8VarConverterTest()
-      : isolate_(v8::Isolate::GetCurrent()), conversion_success_(false) {
+      : isolate_(v8::Isolate::GetCurrent()) {
     PP_Instance dummy = 1234;
     converter_.reset(new V8VarConverter(
         dummy,
@@ -182,27 +189,15 @@ class V8VarConverterTest : public testing::Test {
   bool FromV8ValueSync(v8::Handle<v8::Value> val,
                        v8::Handle<v8::Context> context,
                        PP_Var* result) {
-    base::RunLoop loop;
-    converter_->FromV8Value(val,
-                            context,
-                            base::Bind(&V8VarConverterTest::FromV8ValueComplete,
-                                       base::Unretained(this),
-                                       loop.QuitClosure()));
-    loop.Run();
-    if (conversion_success_)
-      *result = conversion_result_;
-    return conversion_success_;
-  }
+    V8VarConverter::VarResult conversion_result =
+        converter_->FromV8Value(val,
+                                context,
+                                base::Bind(&FromV8ValueComplete));
+    DCHECK(conversion_result.completed_synchronously);
+    if (conversion_result.success)
+      *result = conversion_result.var.Release();
 
-  void FromV8ValueComplete(base::Closure quit_closure,
-                           const ScopedPPVar& scoped_var,
-                           bool success) {
-    conversion_success_ = success;
-    if (success) {
-      ScopedPPVar var = scoped_var;
-      conversion_result_ = var.Release();
-    }
-    quit_closure.Run();
+    return conversion_result.success;
   }
 
   bool RoundTrip(const PP_Var& var, PP_Var* result) {
@@ -240,8 +235,6 @@ class V8VarConverterTest : public testing::Test {
  private:
   TestGlobals globals_;
 
-  PP_Var conversion_result_;
-  bool conversion_success_;
   base::MessageLoop message_loop_;
 };
 
