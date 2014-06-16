f347591f65c643c9a6cecc976b1560f001878636
ljagielski@opera.com <ljagielski@opera.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-16 10:17:27 +0000

    NSSDecryptor::ReadAndParseSignons() - improved password form scheme detection, when database row has non-empty realm, assume autofill::PasswordForm::SCHEME_BASIC.
    BUG=375158
    
    Review URL: https://codereview.chromium.org/306123004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277393 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/utility/importer/firefox_importer_unittest_utils_mac.cc
+++ chrome/utility/importer/firefox_importer_unittest_utils_mac.cc
@@ -84,9 +84,17 @@ class FFDecryptorServerChannelListener : public IPC::Listener {
     base::MessageLoop::current()->Quit();
   }
 
-  void OnDecryptedTextResonse(const base::string16& decrypted_text) {
+  void OnDecryptedTextResponse(const base::string16& decrypted_text) {
     DCHECK(!got_result);
     result_string = decrypted_text;
+    got_result = true;
+    base::MessageLoop::current()->Quit();
+  }
+
+  void OnParseSignonsResponse(
+      const std::vector<autofill::PasswordForm>& parsed_vector) {
+    DCHECK(!got_result);
+    result_vector = parsed_vector;
     got_result = true;
     base::MessageLoop::current()->Quit();
   }
@@ -100,7 +108,8 @@ class FFDecryptorServerChannelListener : public IPC::Listener {
     bool handled = true;
     IPC_BEGIN_MESSAGE_MAP(FFDecryptorServerChannelListener, msg)
       IPC_MESSAGE_HANDLER(Msg_Decryptor_InitReturnCode, OnInitDecryptorResponse)
-      IPC_MESSAGE_HANDLER(Msg_Decryptor_Response, OnDecryptedTextResonse)
+      IPC_MESSAGE_HANDLER(Msg_Decryptor_Response, OnDecryptedTextResponse)
+      IPC_MESSAGE_HANDLER(Msg_ParseSignons_Response, OnParseSignonsResponse)
       IPC_MESSAGE_UNHANDLED(handled = false)
     IPC_END_MESSAGE_MAP()
     return handled;
@@ -114,6 +123,7 @@ class FFDecryptorServerChannelListener : public IPC::Listener {
 
   // Results of IPC calls.
   base::string16 result_string;
+  std::vector<autofill::PasswordForm> result_vector;
   bool result_bool;
   // True if IPC call succeeded and data in above variables is valid.
   bool got_result;
@@ -211,6 +221,17 @@ base::string16 FFUnitTestDecryptorProxy::Decrypt(const std::string& crypt) {
   return base::string16();
 }
 
+std::vector<autofill::PasswordForm> FFUnitTestDecryptorProxy::ParseSignons(
+    const base::FilePath& signons_path) {
+  channel_->Send(new Msg_ParseSignons(signons_path));
+  bool ok = WaitForClientResponse();
+  if (ok && listener_->got_result) {
+    listener_->got_result = false;
+    return listener_->result_vector;
+  }
+  return std::vector<autofill::PasswordForm>();
+}
+
 //---------------------------- Child Process -----------------------
 
 // Class to listen on the client side of the ipc channel, it calls through
@@ -234,6 +255,12 @@ class FFDecryptorClientChannelListener : public IPC::Listener {
     sender_->Send(new Msg_Decryptor_Response(unencrypted_str));
   }
 
+  void OnParseSignons(base::FilePath signons_path) {
+    std::vector<autofill::PasswordForm> forms;
+    decryptor_.ReadAndParseSignons(signons_path, &forms);
+    sender_->Send(new Msg_ParseSignons_Response(forms));
+  }
+
   void OnQuitRequest() {
     base::MessageLoop::current()->Quit();
   }
@@ -243,6 +270,7 @@ class FFDecryptorClientChannelListener : public IPC::Listener {
     IPC_BEGIN_MESSAGE_MAP(FFDecryptorClientChannelListener, msg)
       IPC_MESSAGE_HANDLER(Msg_Decryptor_Init, OnDecryptor_Init)
       IPC_MESSAGE_HANDLER(Msg_Decrypt, OnDecrypt)
+      IPC_MESSAGE_HANDLER(Msg_ParseSignons, OnParseSignons)
       IPC_MESSAGE_HANDLER(Msg_Decryptor_Quit, OnQuitRequest)
       IPC_MESSAGE_UNHANDLED(handled = false)
     IPC_END_MESSAGE_MAP()
