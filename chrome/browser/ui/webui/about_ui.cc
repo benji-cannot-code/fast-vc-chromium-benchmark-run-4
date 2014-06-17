a05ba7811eeb79bc866cdda4e335627c063c008b
sungmann.cho@navercorp.com <sungmann.cho@navercorp.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-17 08:23:19 +0000

    Remove unnecessary #includes in about_ui.cc.
    After the landing https://codereview.chromium.org/190473002,
    about_ui.cc doesn't need the command line switch related
    headers anymore. So we can remove these.
    
    BUG=350323
    
    Review URL: https://codereview.chromium.org/334713007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277669 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/about_ui.cc
+++ chrome/browser/ui/webui/about_ui.cc
@@ -80,7 +80,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process_platform_part_chromeos.h"
 #include "chrome/browser/chromeos/customization_document.h"
 #include "chrome/browser/chromeos/memory/oom_priority_manager.h"
-#include "chromeos/chromeos_switches.h"
 #endif
 
 using base::Time;
