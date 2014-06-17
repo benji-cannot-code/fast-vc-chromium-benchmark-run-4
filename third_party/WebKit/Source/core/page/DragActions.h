25e728c56182b46a7261174c9b8aad84364925c3
costan@gmail.com <costan@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-17 22:21:07 +0000

    Minor cleanup in EventHandler::dragHysteresisExceeded.
    The IntPoint version of EventHandler::dragHysteresisExceeded converts
    its parameter to a FloatPoint and calls the FloatPoint version. In turn,
    that converts the parameter back to an IntPoint, and then does the real
    work. Last, the IntPoint version's parameter is called
    floatDragViewportLocation, whereas the FloatPoint version's parameter is
    called dragViewportLocation.
    
    This change moves the real work in the IntPoint version, and uses the
    FloatPoint->IntPoint conversion code inside the old FloatPoint version
    to delegate from the FloatPoint version to the IntPoint version. The
    main goal is to reduce cognitive overhead on readers.
    
    The change also removes an obsolete comment in DragActions. The comment
    refers to WebDragDestinationAction, which does not currently exist in
    blink.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/339123003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176354 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/DragActions.h
+++ third_party/WebKit/Source/core/page/DragActions.h
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-    // WebCoreDragDestinationAction should be kept in sync with WebDragDestinationAction
     typedef enum {
         DragDestinationActionNone    = 0,
         DragDestinationActionDHTML   = 1,
