2096b0680b5badc2aef031a7f65bfdeec9fd0efa
sungmann.cho@navercorp.com <sungmann.cho@navercorp.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-17 20:17:24 +0000

    Remove unnecessary #includes in background_contents_service.cc.
    After the landing https://codereview.chromium.org/317203004,
    background_contents_service.cc doesn't need the command line switch
    related headers anymore. So we can remove these.
    
    BUG=350898
    
    Review URL: https://codereview.chromium.org/338083002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277845 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/background/background_contents_service.cc
+++ chrome/browser/background/background_contents_service.cc
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_tabstrip.h"
 #include "chrome/browser/ui/host_desktop.h"
-#include "chrome/common/chrome_switches.h"
 #include "chrome/common/extensions/extension_constants.h"
 #include "chrome/common/pref_names.h"
 #include "content/public/browser/notification_service.h"
