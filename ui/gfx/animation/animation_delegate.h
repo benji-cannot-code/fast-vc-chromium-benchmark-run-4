6cf7b1d519ac27640ca484e9bad6d118224b24b2
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-17 10:24:56 +0000

    Make BoundsAnimator always own its animation delegate.
    All non-unittest code wanted this behavior.
    
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/331323005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277707 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/animation/animation_delegate.h
+++ ui/gfx/animation/animation_delegate.h
@@ -18,6 +18,8 @@ class Animation;
 //  state of an animation.
 class GFX_EXPORT AnimationDelegate {
  public:
+  virtual ~AnimationDelegate() {}
+
   // Called when an animation has completed.
   virtual void AnimationEnded(const Animation* animation) {}
 
@@ -26,9 +28,6 @@ class GFX_EXPORT AnimationDelegate {
 
   // Called when an animation has been canceled.
   virtual void AnimationCanceled(const Animation* animation) {}
-
- protected:
-  virtual ~AnimationDelegate() {}
 };
 
 }  // namespace gfx
