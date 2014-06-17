2f7a5002facc06d2cfd57e0f1adc79ae49285b3d
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-17 20:38:31 +0000

    Fix the Windows 8 mode avatar button menu.
    Metro's avatar button is no-op w/--new-profile-management.
    This may have regressed with r270126, but I'm not sure how.
    
    IsNewAvatarMenu returns true in metro (r263677, r263396).
    However, GetNewAvatarMenuButton returns NULL in metro.
    (the new button isn't implemented for that frame, afaict)
    So ShowAvatarBubbleFromAvatarButton currently does nothing.
    Instead, anchor to the avatar menu button or app menu.
    
    Remove the unused |anchor_rect| parameter.
    
    BUG=382895
    R=noms@chromium.org,sky@chromium.org
    TEST=Avatar button works in Windows 8 mode (metro) with .
    
    Review URL: https://codereview.chromium.org/333353004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277857 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser_window.h
+++ chrome/browser/ui/browser_window.h
@@ -373,7 +373,8 @@ class BrowserWindow : public ui::BaseWindow {
     AVATAR_BUBBLE_MODE_SIGNIN,
     AVATAR_BUBBLE_MODE_REAUTH,
   };
-  virtual void ShowAvatarBubbleFromAvatarButton(AvatarBubbleMode mode,
+  virtual void ShowAvatarBubbleFromAvatarButton(
+      AvatarBubbleMode mode,
       signin::GAIAServiceType service_type) = 0;
 
   // Show bubble for password generation positioned relative to |rect|. The
--- chrome/browser/ui/views/profiles/profile_chooser_view.h
+++ chrome/browser/ui/views/profiles/profile_chooser_view.h
@@ -57,7 +57,6 @@ class ProfileChooserView : public views::BubbleDelegateView,
                          views::View* anchor_view,
                          views::BubbleBorder::Arrow arrow,
                          views::BubbleBorder::BubbleAlignment border_alignment,
-                         const gfx::Rect& anchor_rect,
                          Browser* browser);
   static bool IsShowing();
   static void Hide();
@@ -79,7 +78,6 @@ class ProfileChooserView : public views::BubbleDelegateView,
 
   ProfileChooserView(views::View* anchor_view,
                      views::BubbleBorder::Arrow arrow,
-                     const gfx::Rect& anchor_rect,
                      Browser* browser,
                      profiles::BubbleViewMode view_mode,
                      signin::GAIAServiceType service_type);
