e587c3ebfefba7c3a6dc36ede8f4b3af2a861bc2
falken@chromium.org <falken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-17 04:57:36 +0000

    Don't assert when ServiceWorker::from is passed an empty promise
    If the document gets detached before Service Worker registration
    completes, the CallbackPromiseAdapter promise gets cleared. Then when
    registration completes, ServiceWorker::from is passed the resolver with
    the empty promise. Before this patch, an assert would then occur because
    ScriptPromise::then bails before adopting its fulfilled/rejected
    arguments. This patch fixes an overly aggressive assert in
    V8GarbageCollected and also makes ServiceWorker creation skip waiting
    on an empty promise.
    
    BUG=384498
    
    Review URL: https://codereview.chromium.org/337053004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176282 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/V8GarbageCollected.h
+++ third_party/WebKit/Source/bindings/v8/V8GarbageCollected.h
@@ -52,12 +52,14 @@ protected:
     V8GarbageCollected(v8::Isolate* isolate)
         : m_isolate(isolate)
         , m_handle(isolate, v8::External::New(isolate, static_cast<T*>(this)))
+        , m_releasedToV8GarbageCollector(false)
     {
     }
 
     v8::Handle<v8::External> releaseToV8GarbageCollector()
     {
         ASSERT(!m_handle.isWeak()); // Call this exactly once.
+        m_releasedToV8GarbageCollector = true;
         v8::Handle<v8::External> result = m_handle.newLocal(m_isolate);
         m_handle.setWeak(static_cast<T*>(this), &weakCallback);
         return result;
@@ -65,7 +67,7 @@ protected:
 
     ~V8GarbageCollected()
     {
-        ASSERT(m_handle.isEmpty());
+        ASSERT(!m_releasedToV8GarbageCollector || m_handle.isEmpty());
     }
 
     v8::Isolate* isolate() { return m_isolate; }
@@ -80,6 +82,7 @@ private:
 
     v8::Isolate* m_isolate;
     ScopedPersistent<v8::External> m_handle;
+    bool m_releasedToV8GarbageCollector;
 };
 
 } // namespace WebCore
