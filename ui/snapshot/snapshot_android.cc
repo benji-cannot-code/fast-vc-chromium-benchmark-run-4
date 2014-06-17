aecc3e9beb9e939b0897b4303b0f3fd75e4cd1c3
powei@chromium.org <powei@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-17 09:47:56 +0000

    android: Fix set_area size for snapshot_android
    For Chrome for Android, the size of the layers correspond to their physical
    pixel size.  We should scale the copy request subrect by the device scale
    factor.
    
    The reason why content/test/gpu/page_sets/pixel_tests.py never failed is
    because the test only takes a 300x300 sub-region of the output bitmap; so
    as long as the request subrect is greater than that region, the test will
    pass through.  After this change, ui::GrabViewSnapshot will return a bitmap
    of the content in phyiscal pixel size.  Should not affect pixel_tests.py.
    
    BUG=384551
    
    Review URL: https://codereview.chromium.org/333413002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277690 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/snapshot/snapshot_android.cc
+++ ui/snapshot/snapshot_android.cc
@@ -11,6 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/android/view_android.h"
 #include "ui/base/android/window_android.h"
 #include "ui/base/android/window_android_compositor.h"
+#include "ui/gfx/display.h"
+#include "ui/gfx/geometry/rect_conversions.h"
+#include "ui/gfx/screen.h"
 #include "ui/snapshot/snapshot_async.h"
 
 namespace ui {
@@ -35,7 +38,14 @@ static void MakeAsyncCopyRequest(
     const cc::CopyOutputRequest::CopyOutputRequestCallback& callback) {
   scoped_ptr<cc::CopyOutputRequest> request =
       cc::CopyOutputRequest::CreateBitmapRequest(callback);
-  request->set_area(source_rect);
+
+  const gfx::Display& display =
+      gfx::Screen::GetNativeScreen()->GetPrimaryDisplay();
+  float device_scale_factor = display.device_scale_factor();
+  gfx::Rect source_rect_in_pixel =
+      gfx::ToEnclosingRect(gfx::ScaleRect(source_rect, device_scale_factor));
+
+  request->set_area(source_rect_in_pixel);
   window->GetCompositor()->RequestCopyOfOutputOnRootLayer(request.Pass());
 }
 
