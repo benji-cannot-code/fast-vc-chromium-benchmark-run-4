a2bbf9fc128a319cfc7976bff75a037e34c5d846
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-17 18:03:49 +0000

    Revert of Change HungPluginTabHelper to listen for infobar changes through Observer style. (https://codereview.chromium.org/297293002/)
    Reason for revert:
    Caused a crash on Windows. See crbug.com/354380
    
    Original issue's description:
    > Change HungPluginTabHelper to listen for infobar changes through Observer style.
    > 
    > Test plan:
    > 
    > 1) out/Debug/chrome --no-sandbox --ppapi-flash-path=/opt/google/chrome/PepperFlash/libpepflashplayer.so --ppapi-flash-version=`grep -i version /opt/google/chrome/PepperFlash/manifest.json | awk '{print $2}' | awk -F"\"" '{print $2}'`
    > 2) Go to a site that has flash, e.g., http://www.flash-game.net/
    > 3) Open Task Manager, Shift+Esc
    > 4) Search for "Plug-in: Shockwave Flash" in the list.
    > 5) Select it and click on "End process" button.
    > 6) Go to the website and observe the infobar.
    > 
    > Another way to test it is:
    > 
    > 1) Add an infinite loop into ppapi/examples/scripting/post_message.cc
    > 2) Build ppapi_example_post_message
    > 3) out/Debug/chrome --no-sandbox --register-pepper-plugins="/home/tfarina/chromium/src/out/Debug/lib/libppapi_example_post_message.so;application/x-ppapi-post-message-example" ppapi/examples/scripting/post_message.html
    > 
    > BUG=354380
    > TEST=see above
    > R=pkasting@chromium.org
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=277292
    NOTRY=true
    TBR=pkasting@chromium.org
    
    Review URL: https://codereview.chromium.org/338353003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277804 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/hung_plugin_tab_helper.cc
+++ chrome/browser/ui/hung_plugin_tab_helper.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process/process.h"
 #include "base/rand_util.h"
 #include "build/build_config.h"
+#include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/infobars/infobar_service.h"
 #include "chrome/common/chrome_version_info.h"
 #include "components/infobars/core/confirm_infobar_delegate.h"
@@ -18,6 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_child_process_host_iterator.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/child_process_data.h"
+#include "content/public/browser/notification_details.h"
+#include "content/public/browser/notification_service.h"
 #include "content/public/browser/plugin_service.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/common/process_type.h"
@@ -259,15 +262,12 @@ HungPluginTabHelper::PluginState::~PluginState() {
 DEFINE_WEB_CONTENTS_USER_DATA_KEY(HungPluginTabHelper);
 
 HungPluginTabHelper::HungPluginTabHelper(content::WebContents* contents)
-    : content::WebContentsObserver(contents),
-      number_of_infobars_(0) {
+    : content::WebContentsObserver(contents) {
+  registrar_.Add(this, chrome::NOTIFICATION_TAB_CONTENTS_INFOBAR_REMOVED,
+                 content::NotificationService::AllSources());
 }
 
 HungPluginTabHelper::~HungPluginTabHelper() {
-  // If we're getting shut down before the InfoBarService, we need to unregister
-  // as an observer.
-  if (number_of_infobars_ != 0)
-    InfoBarService::FromWebContents(web_contents())->RemoveObserver(this);
 }
 
 void HungPluginTabHelper::PluginCrashed(const base::FilePath& plugin_path,
@@ -322,16 +322,17 @@ void HungPluginTabHelper::PluginHungStatusChanged(
   ShowBar(plugin_child_id, state.get());
 }
 
-void HungPluginTabHelper::OnInfoBarRemoved(infobars::InfoBar* infobar,
-                                           bool animate) {
+void HungPluginTabHelper::Observe(
+    int type,
+    const content::NotificationSource& source,
+    const content::NotificationDetails& details) {
+  DCHECK_EQ(chrome::NOTIFICATION_TAB_CONTENTS_INFOBAR_REMOVED, type);
+  infobars::InfoBar* infobar =
+      content::Details<infobars::InfoBar::RemovedDetails>(details)->first;
   for (PluginStateMap::iterator i = hung_plugins_.begin();
        i != hung_plugins_.end(); ++i) {
     PluginState* state = i->second.get();
     if (state->infobar == infobar) {
-      --number_of_infobars_;
-      if (number_of_infobars_ == 0)
-        InfoBarService::FromWebContents(web_contents())->RemoveObserver(this);
-
       state->infobar = NULL;
 
       // Schedule the timer to re-show the infobar if the plugin continues to be
@@ -412,12 +413,6 @@ void HungPluginTabHelper::ShowBar(int child_id, PluginState* state) {
   DCHECK(!state->infobar);
   state->infobar = HungPluginInfoBarDelegate::Create(infobar_service, this,
                                                      child_id, state->name);
-
-  if (state->infobar) {
-    ++number_of_infobars_;
-    if (number_of_infobars_ == 1)
-      infobar_service->AddObserver(this);
-  }
 }
 
 void HungPluginTabHelper::CloseBar(PluginState* state) {
