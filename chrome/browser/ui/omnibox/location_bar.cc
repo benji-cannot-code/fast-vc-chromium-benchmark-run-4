1baeae896b55ba0898c3c760b5c660b05b10c564
wittman@chromium.org <wittman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-17 08:22:06 +0000

    Remove bookmarks_ui settings from chrome_settings_overrides manifest section
    The bookmarks_ui settings have been moved to the chrome_ui_overrides
    manifest section. This change removes the legacy support for the settings
    under chrome_settings_overrides.
    
    BUG=349049
    
    Review URL: https://codereview.chromium.org/321623002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277667 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/omnibox/location_bar.cc
+++ chrome/browser/ui/omnibox/location_bar.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/common/extensions/manifest_handlers/settings_overrides_handler.h"
 #include "chrome/common/extensions/manifest_handlers/ui_overrides_handler.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/common/extension_set.h"
@@ -26,14 +25,7 @@ bool LocationBar::IsBookmarkStarHiddenByExtension() const {
       extensions::ExtensionRegistry::Get(profile_)->enabled_extensions();
   for (extensions::ExtensionSet::const_iterator i = extension_set.begin();
        i != extension_set.end(); ++i) {
-    using extensions::UIOverrides;
-    using extensions::SettingsOverrides;
-    const UIOverrides* ui_overrides = UIOverrides::Get(i->get());
-    const SettingsOverrides* settings_overrides =
-        SettingsOverrides::Get(i->get());
-    if (((settings_overrides &&
-          SettingsOverrides::RemovesBookmarkButton(*settings_overrides)) ||
-         (ui_overrides && UIOverrides::RemovesBookmarkButton(*ui_overrides))) &&
+    if (extensions::UIOverrides::RemovesBookmarkButton(*i) &&
         ((*i)->permissions_data()->HasAPIPermission(
              extensions::APIPermission::kBookmarkManagerPrivate) ||
          extensions::FeatureSwitch::enable_override_bookmarks_ui()
