64e5787bcefe4fdf001d879fb6cfddde386ce57f
kouhei@chromium.org <kouhei@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-17 04:56:57 +0000

    Revert of Forbid creating new wrapper from SetWrapperReferenceTo (https://codereview.chromium.org/332643003/)
    Reason for revert:
    There seems to be an existing bug which hits this callpath. Reverting the change until the bug is fixed.
    
    Original issue's description:
    > Forbid creating new wrapper from SetWrapperReferenceTo
    > 
    > SetWrapperReferenceTo IDL attribute is a hack to use V8 wrapper reference
    > to avoid reference cycles in Blink. The |visitDOMWrapper| callback is
    > used to create a temporary strong reference between the wrappers in GC
    > prologue.
    > 
    > Before this patch, |visitDOMWrapper| was allowed to create a new wrapper
    > if the wrapper for target object didn't exist. However this is dangerous,
    > as the target object may be already destructed. The target wrapper must
    > already exist for this reference hack to work correctly.
    > 
    > This patch forbids creating a new wrapper from |visitDOMWrapper| callback,
    > and adds an ASSERT that checks that the target object wrapper already
    > exists.
    > 
    > BUG=None
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=175998
    
    TBR=nbarth@chromium.org,haraken@chromium.org
    BUG=384459
    
    Review URL: https://codereview.chromium.org/332183008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176281 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/tests/results/V8TestInterface.cpp
+++ third_party/WebKit/Source/bindings/tests/results/V8TestInterface.cpp
@@ -1309,7 +1309,8 @@ void V8TestInterface::visitDOMWrapper(void* object, const v8::Persistent<v8::Obj
     V8WrapperInstantiationScope scope(creationContext, isolate);
     TestInterfaceImplementation* referencedName = impl->referencedName();
     if (referencedName) {
-        ASSERT(DOMDataStore::containsWrapper<V8TestInterface>(referencedName, isolate));
+        if (!DOMDataStore::containsWrapper<V8TestInterface>(referencedName, isolate))
+            wrap(referencedName, creationContext, isolate);
         DOMDataStore::setWrapperReference<V8TestInterface>(wrapper, referencedName, isolate);
     }
     setObjectGroup(object, wrapper, isolate);
