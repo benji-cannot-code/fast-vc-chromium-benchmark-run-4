f4e287e6dfa512d8cb8e8f77ea636cdf796b0806
kouhei@chromium.org <kouhei@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-17 02:46:36 +0000

    Prepare SVGLengthContext for oilpan
    SVGLengthContext is made STACK_ALLOCATED, as it is currently never used
    on a heap.
    
    BUG=357163
    
    Review URL: https://codereview.chromium.org/339793004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176268 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGLengthContext.h
+++ third_party/WebKit/Source/core/svg/SVGLengthContext.h
@@ -51,6 +51,7 @@ enum SVGLengthMode {
 };
 
 class SVGLengthContext {
+    STACK_ALLOCATED();
 public:
     explicit SVGLengthContext(const SVGElement*);
 
@@ -81,7 +82,7 @@ private:
     float convertValueFromUserUnitsToEXS(float value, ExceptionState&) const;
     float convertValueFromEXSToUserUnits(float value, ExceptionState&) const;
 
-    const SVGElement* m_context;
+    RawPtrWillBeMember<const SVGElement> m_context;
     FloatRect m_overridenViewport;
 };
 
