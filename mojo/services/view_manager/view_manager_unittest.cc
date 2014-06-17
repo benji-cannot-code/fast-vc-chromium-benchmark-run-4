04fe48156ea7f4c085490bda7a0b3047151c95f3
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-17 20:24:48 +0000

    Makes IViewManager::DeleteNode take the server_change_id
    Clearer this way, especially since a deletion may entail a hierarchy
    change.
    
    BUG=365012
    TEST=none
    R=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/331243002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277847 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/services/view_manager/view_manager_unittest.cc
+++ mojo/services/view_manager/view_manager_unittest.cc
@@ -163,10 +163,11 @@ class ViewManagerProxy : public TestChangeTracker::Delegate {
     RunMainLoop();
     return result;
   }
-  bool DeleteNode(Id node_id) {
+  bool DeleteNode(Id node_id, Id server_change_id) {
     changes_.clear();
     bool result = false;
     view_manager_->DeleteNode(node_id,
+                              server_change_id,
                               base::Bind(&ViewManagerProxy::GotResult,
                                          base::Unretained(this), &result));
     RunMainLoop();
@@ -879,7 +880,7 @@ TEST_F(ViewManagerTest, DeleteNode) {
 
   // Delete 2.
   {
-    ASSERT_TRUE(connection_->DeleteNode(BuildNodeId(1, 2)));
+    ASSERT_TRUE(connection_->DeleteNode(BuildNodeId(1, 2), 2));
     EXPECT_TRUE(connection_->changes().empty());
 
     connection2_->DoRunLoopUntilChangesCount(1);
@@ -892,7 +893,7 @@ TEST_F(ViewManagerTest, DeleteNode) {
 // Verifies DeleteNode isn't allowed from a separate connection.
 TEST_F(ViewManagerTest, DeleteNodeFromAnotherConnectionDisallowed) {
   ASSERT_NO_FATAL_FAILURE(EstablishSecondConnection(true));
-  EXPECT_FALSE(connection2_->DeleteNode(BuildNodeId(1, 1)));
+  EXPECT_FALSE(connection2_->DeleteNode(BuildNodeId(1, 1), 1));
 }
 
 // Verifies DeleteView isn't allowed from a separate connection.
@@ -923,7 +924,7 @@ TEST_F(ViewManagerTest, ReuseDeletedNodeId) {
 
   // Delete 2.
   {
-    ASSERT_TRUE(connection_->DeleteNode(BuildNodeId(1, 2)));
+    ASSERT_TRUE(connection_->DeleteNode(BuildNodeId(1, 2), 2));
 
     connection2_->DoRunLoopUntilChangesCount(1);
     const Changes changes(ChangesToDescription1(connection2_->changes()));
@@ -999,7 +1000,7 @@ TEST_F(ViewManagerTest, DeleteNodeWithView) {
   // Delete node 2. The second connection should not see this because the node
   // was not known to it.
   {
-    ASSERT_TRUE(connection_->DeleteNode(BuildNodeId(1, 2)));
+    ASSERT_TRUE(connection_->DeleteNode(BuildNodeId(1, 2), 1));
 
     connection2_->DoRunLoopUntilChangesCount(1);
     const Changes changes(ChangesToDescription1(connection2_->changes()));
@@ -1023,7 +1024,7 @@ TEST_F(ViewManagerTest, DeleteNodeWithView) {
 
   // Delete 3.
   {
-    ASSERT_TRUE(connection_->DeleteNode(BuildNodeId(1, 3)));
+    ASSERT_TRUE(connection_->DeleteNode(BuildNodeId(1, 3), 3));
 
     connection2_->DoRunLoopUntilChangesCount(1);
     const Changes changes(ChangesToDescription1(connection2_->changes()));
@@ -1190,7 +1191,7 @@ TEST_F(ViewManagerTest, SetRoots) {
   // Delete 4, client shouldn't receive a delete since it should no longer know
   // about 4.
   {
-    ASSERT_TRUE(connection_->DeleteNode(BuildNodeId(1, 4)));
+    ASSERT_TRUE(connection_->DeleteNode(BuildNodeId(1, 4), 5));
 
     connection2_->DoRunLoopUntilChangesCount(1);
     const Changes changes(ChangesToDescription1(connection2_->changes()));
