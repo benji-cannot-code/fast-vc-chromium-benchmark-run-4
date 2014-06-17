556c69cf4a10e351e2cf6590afa51c2efac9c52f
kmadhusu@chromium.org <kmadhusu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-17 18:42:03 +0000

    To support Instant search in Android Chrome, this CL (1) Prerenders an Instant search base page on the receipt of a high-confidence search suggestion. (2) Prefetches the search results for the high-confidence suggestion using the prerendered page. (3) When the user commits the search query, Android Chrome submits the query to the prerendered page and swaps the active tab contents with the prerendered contents.
    BUG=382694
    TEST= Apply this patch, build Android Chrome and run it against the server url specified in go/yzcku. Type a search query in the LocationBar. Android Chrome should identify the high-confidence suggestion for the typed query and prefetch the results for that suggestion. Commit the typed query. Android Chrome should swap the active tab contents with the prerendered page contents. You should see the search results instantaneously.
    
    Review URL: https://codereview.chromium.org/321943002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277818 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/android/omnibox/omnibox_prerender.cc
+++ chrome/browser/android/omnibox/omnibox_prerender.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/predictors/autocomplete_action_predictor_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_android.h"
+#include "chrome/browser/ui/search/instant_search_prerenderer.h"
 #include "content/public/browser/web_contents.h"
 #include "jni/OmniboxPrerender_jni.h"
 #include "url/gurl.h"
@@ -89,18 +90,25 @@ void OmniboxPrerender::PrerenderMaybe(JNIEnv* env,
   if (default_match == autocomplete_result->end())
     return;
 
-  AutocompleteActionPredictor* action_predictor =
-      AutocompleteActionPredictorFactory::GetForProfile(profile);
-  if (!action_predictor)
-    return;
-
   AutocompleteActionPredictor::Action recommended_action =
       AutocompleteActionPredictor::ACTION_NONE;
-  if (action_predictor) {
-    action_predictor->
-        RegisterTransitionalMatches(url_string, *autocomplete_result);
-    recommended_action =
-        action_predictor->RecommendAction(url_string, *default_match);
+  InstantSearchPrerenderer* prerenderer =
+      InstantSearchPrerenderer::GetForProfile(profile);
+  if (prerenderer &&
+      prerenderer->IsAllowed(*default_match, web_contents)) {
+    recommended_action = AutocompleteActionPredictor::ACTION_PRERENDER;
+  } else {
+    AutocompleteActionPredictor* action_predictor =
+        AutocompleteActionPredictorFactory::GetForProfile(profile);
+    if (!action_predictor)
+      return;
+
+    if (action_predictor) {
+      action_predictor->
+          RegisterTransitionalMatches(url_string, *autocomplete_result);
+      recommended_action =
+          action_predictor->RecommendAction(url_string, *default_match);
+    }
   }
 
   GURL current_url = GURL(current_url_string);
@@ -136,6 +144,14 @@ void OmniboxPrerender::DoPrerender(const AutocompleteMatch& match,
   if (!web_contents)
     return;
   gfx::Rect container_bounds = web_contents->GetContainerBounds();
+  InstantSearchPrerenderer* prerenderer =
+      InstantSearchPrerenderer::GetForProfile(profile);
+  if (prerenderer && prerenderer->IsAllowed(match, web_contents)) {
+    prerenderer->Init(
+        web_contents->GetController().GetSessionStorageNamespaceMap(),
+        container_bounds.size());
+    return;
+  }
   predictors::AutocompleteActionPredictorFactory::GetForProfile(profile)->
       StartPrerendering(
           match.destination_url,
