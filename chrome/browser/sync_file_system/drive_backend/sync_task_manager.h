abdf05b3f6acf92bb44c5f5aed080bfaf9598ec1
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-17 11:16:28 +0000

    [SyncFS] Remove unneeded #include from sync_task_manager.h
    BUG=
    
    Review URL: https://codereview.chromium.org/337313002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277722 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_backend/sync_task_manager.h
+++ chrome/browser/sync_file_system/drive_backend/sync_task_manager.h
@@ -11,13 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/containers/scoped_ptr_hash_map.h"
-#include "base/files/file_path.h"
-#include "base/location.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/sequence_checker.h"
 #include "base/threading/non_thread_safe.h"
-#include "chrome/browser/sync_file_system/drive_backend/sync_task.h"
 #include "chrome/browser/sync_file_system/drive_backend/task_dependency_manager.h"
 #include "chrome/browser/sync_file_system/sync_callbacks.h"
 #include "chrome/browser/sync_file_system/sync_status_code.h"
@@ -30,6 +27,7 @@ class Location;
 namespace sync_file_system {
 namespace drive_backend {
 
+class SyncTask;
 class SyncTaskToken;
 struct BlockingFactor;
 
--- chrome/browser/sync_file_system/drive_backend/sync_task_manager_unittest.cc
+++ chrome/browser/sync_file_system/drive_backend/sync_task_manager_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
+#include "chrome/browser/sync_file_system/drive_backend/sync_task.h"
 #include "chrome/browser/sync_file_system/drive_backend/sync_task_manager.h"
 #include "chrome/browser/sync_file_system/drive_backend/sync_task_token.h"
 #include "chrome/browser/sync_file_system/sync_file_system_test_util.h"
