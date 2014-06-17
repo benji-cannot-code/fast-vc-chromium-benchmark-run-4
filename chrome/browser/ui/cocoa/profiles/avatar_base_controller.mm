e5068f363752d3f14d11c6677cf95d15faa4af28
noms@chromium.org <noms@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-17 23:52:48 +0000

    [Mac] Show a warning in the new avatar button/menu if there's an auth error.
    This error is shown both in the avatar button, and in the avatar menu. When the 
    account for which we're displaying the auth error is clicked in the avatar menu,
    we show the Gaia reauth view.
    
    Screenshot: 
    https://drive.google.com/file/d/0B1B1Up4p2NRMQW90VmcwcGZQVEU/edit?usp=sharing
    
    I've also fixed a bug where the available text for the account name was 
    incorrectly calculated, and eliding overlapped the delete button. Screenshot:
    https://drive.google.com/file/d/0B1B1Up4p2NRMaU9DcWdqTVZNMVk/edit?usp=sharing
    BUG=311235
    TEST= Sign into Chrome with the --new-profile-management flag on. Go to 
    accounts.google.com and revoke Chrome's access to that account, from the 
    Security tab. Try to bookmark the current page. The avatar button should be
    badged like in the attached screenshot.
    
    Review URL: https://codereview.chromium.org/303463010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277908 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/profiles/avatar_base_controller.mm
+++ chrome/browser/ui/cocoa/profiles/avatar_base_controller.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/profiles/profile_metrics.h"
 #include "chrome/browser/signin/signin_header_helper.h"
+#include "chrome/browser/profiles/profiles_state.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_window.h"
@@ -19,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/ui/cocoa/browser_window_controller.h"
 #import "chrome/browser/ui/cocoa/profiles/avatar_menu_bubble_controller.h"
 #import "chrome/browser/ui/cocoa/profiles/profile_chooser_controller.h"
+#include "components/signin/core/browser/signin_error_controller.h"
 #include "components/signin/core/common/profile_management_switches.h"
 #include "ui/base/resource/resource_bundle.h"
 
@@ -28,50 +30,79 @@ const CGFloat kMenuYOffsetAdjust = 1.0;
 @interface AvatarBaseController (Private)
 // Shows the avatar bubble.
 - (IBAction)buttonClicked:(id)sender;
+
 - (void)bubbleWillClose:(NSNotification*)notif;
+
 // Updates the profile name displayed by the avatar button. If |layoutParent| is
 // yes, then the BrowserWindowController is notified to relayout the subviews,
 // as the button needs to be repositioned.
 - (void)updateAvatarButtonAndLayoutParent:(BOOL)layoutParent;
+
+// Displays an error icon if any accounts associated with this profile have an
+// auth error.
+- (void)updateErrorStatus:(BOOL)hasError;
 @end
 
-class ProfileInfoUpdateObserver : public ProfileInfoCacheObserver {
+class ProfileInfoUpdateObserver : public ProfileInfoCacheObserver,
+                                  public SigninErrorController::Observer {
  public:
-  ProfileInfoUpdateObserver(AvatarBaseController* avatarButton)
-      : avatarButton_(avatarButton) {
+  ProfileInfoUpdateObserver(Profile* profile,
+                            AvatarBaseController* avatarController)
+      : profile_(profile),
+        avatarController_(avatarController) {
     g_browser_process->profile_manager()->
         GetProfileInfoCache().AddObserver(this);
+
+    // Subscribe to authentication error changes so that the avatar button
+    // can update itself.
+    SigninErrorController* errorController =
+        profiles::GetSigninErrorController(profile_);
+    if (errorController)
+      errorController->AddObserver(this);
   }
 
   virtual ~ProfileInfoUpdateObserver() {
     g_browser_process->profile_manager()->
         GetProfileInfoCache().RemoveObserver(this);
+    SigninErrorController* errorController =
+        profiles::GetSigninErrorController(profile_);
+    if (errorController)
+      errorController->RemoveObserver(this);
   }
 
   // ProfileInfoCacheObserver:
   virtual void OnProfileAdded(const base::FilePath& profile_path) OVERRIDE {
-    [avatarButton_ updateAvatarButtonAndLayoutParent:YES];
+    [avatarController_ updateAvatarButtonAndLayoutParent:YES];
   }
 
   virtual void OnProfileWasRemoved(
       const base::FilePath& profile_path,
       const base::string16& profile_name) OVERRIDE {
-    [avatarButton_ updateAvatarButtonAndLayoutParent:YES];
+    [avatarController_ updateAvatarButtonAndLayoutParent:YES];
   }
 
   virtual void OnProfileNameChanged(
       const base::FilePath& profile_path,
       const base::string16& old_profile_name) OVERRIDE {
-    [avatarButton_ updateAvatarButtonAndLayoutParent:YES];
+    [avatarController_ updateAvatarButtonAndLayoutParent:YES];
   }
 
   virtual void OnProfileAvatarChanged(
       const base::FilePath& profile_path) OVERRIDE {
-    [avatarButton_ updateAvatarButtonAndLayoutParent:YES];
+    [avatarController_ updateAvatarButtonAndLayoutParent:YES];
+  }
+
+  // SigninErrorController::Observer:
+  virtual void OnErrorChanged() OVERRIDE {
+    SigninErrorController* errorController =
+        profiles::GetSigninErrorController(profile_);
+    if (errorController)
+      [avatarController_ updateErrorStatus:errorController->HasError()];
   }
 
  private:
-  AvatarBaseController* avatarButton_;  // Weak; owns this.
+  Profile* profile_;
+  AvatarBaseController* avatarController_;  // Weak; owns this.
 
   DISALLOW_COPY_AND_ASSIGN(ProfileInfoUpdateObserver);
 };
@@ -81,7 +112,8 @@ class ProfileInfoUpdateObserver : public ProfileInfoCacheObserver {
 - (id)initWithBrowser:(Browser*)browser {
   if ((self = [super init])) {
     browser_ = browser;
-    profileInfoObserver_.reset(new ProfileInfoUpdateObserver(self));
+    profileInfoObserver_.reset(
+        new ProfileInfoUpdateObserver(browser_->profile(), self));
   }
   return self;
 }
@@ -169,6 +201,9 @@ class ProfileInfoUpdateObserver : public ProfileInfoCacheObserver {
 
 - (void)updateAvatarButtonAndLayoutParent:(BOOL)layoutParent {
   NOTREACHED();
+}
+
+- (void)updateErrorStatus:(BOOL)hasError {
 }
 
 - (BaseBubbleController*)menuController {
