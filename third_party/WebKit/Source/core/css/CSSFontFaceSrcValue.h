4d1c45f8a3e3de8fa0e26298454fa1d4f25fc2ca
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-17 02:54:35 +0000

    Oilpan: Trace CSSFontFaceSrcValue::m_svgFontFaceElement.
    BUG=357163
    
    Review URL: https://codereview.chromium.org/333403004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176270 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/CSSFontFaceSrcValue.h
+++ third_party/WebKit/Source/core/css/CSSFontFaceSrcValue.h
@@ -74,7 +74,7 @@ public:
 
     bool equals(const CSSFontFaceSrcValue&) const;
 
-    void traceAfterDispatch(Visitor* visitor) { CSSValue::traceAfterDispatch(visitor); }
+    void traceAfterDispatch(Visitor*);
 
 private:
     CSSFontFaceSrcValue(const String& resource, bool local)
@@ -82,7 +82,7 @@ private:
         , m_resource(resource)
         , m_isLocal(local)
 #if ENABLE(SVG_FONTS)
-        , m_svgFontFaceElement(0)
+        , m_svgFontFaceElement(nullptr)
 #endif
     {
     }
@@ -98,7 +98,7 @@ private:
     ResourcePtr<FontResource> m_fetched;
 
 #if ENABLE(SVG_FONTS)
-    SVGFontFaceElement* m_svgFontFaceElement;
+    RawPtrWillBeMember<SVGFontFaceElement> m_svgFontFaceElement;
 #endif
 };
 
