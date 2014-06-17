adc1add9540c9b1799b2ad375d79d0a3f8764422
Chansik.Yun@gmail.com <Chansik.Yun@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-17 23:13:30 +0000

    Remove unnecessary #includes in create_profile_handler.cc and manage_profile_handler.cc.
    BUG=none
    TEST=compile
    
    Review URL: https://codereview.chromium.org/338323002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277893 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options/create_profile_handler.cc
+++ chrome/browser/ui/webui/options/create_profile_handler.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/options/create_profile_handler.h"
 
 #include "base/bind.h"
-#include "base/command_line.h"
 #include "base/files/file_path.h"
 #include "base/metrics/histogram.h"
 #include "base/prefs/pref_service.h"
