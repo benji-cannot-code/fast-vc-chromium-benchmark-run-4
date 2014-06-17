92baf996ecb93d911ea5b5969ed47386332467eb
sungmann.cho@navercorp.com <sungmann.cho@navercorp.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-17 03:30:40 +0000

    Remove unnecessary #includes in shortcut_manager.cc.
    After the landing https://codereview.chromium.org/298953002,
    shortcut_manager.cc doesn't need the command line
    switch related headers anymore. So we can remove these.
    
    BUG=350161
    
    Review URL: https://codereview.chromium.org/331063003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277647 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/apps/shortcut_manager.cc
+++ chrome/browser/apps/shortcut_manager.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/shortcut_manager.h"
 
 #include "base/bind.h"
-#include "base/command_line.h"
 #include "base/compiler_specific.h"
 #include "base/prefs/pref_service.h"
 #include "base/strings/string16.h"
@@ -19,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/shell_integration.h"
 #include "chrome/browser/web_applications/web_app.h"
-#include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "content/public/browser/browser_thread.h"
