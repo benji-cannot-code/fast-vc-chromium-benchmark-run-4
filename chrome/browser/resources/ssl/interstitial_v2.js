2c2051f620611bb4530d74c4e7a7fc232b189aaf
felt@chromium.org <felt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-17 13:14:28 +0000

    This makes the Safe Browsing interstitial unit & browser tests work for both the V2 and V3 versions. For the unit tests, I did this by using typed tests. Typed tests aren't supported for browser tests, so for the browser tests I moved the tests into methods that could be invoked twice.
    Also includes a small bugfix in a string that was causing a failure.
    
    BUG=381260
    
    Review URL: https://codereview.chromium.org/330753002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277749 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/ssl/interstitial_v2.js
+++ chrome/browser/resources/ssl/interstitial_v2.js
@@ -31,7 +31,7 @@ function setupEvents() {
       sendCommand(ssl ? CMD_PROCEED : SB_CMD_PROCEED);
     });
   } else if (!ssl) {
-    $('finalParagraph').classList.add('hidden');
+    $('final-paragraph').classList.add('hidden');
   }
 
   if (ssl && overridable) {
