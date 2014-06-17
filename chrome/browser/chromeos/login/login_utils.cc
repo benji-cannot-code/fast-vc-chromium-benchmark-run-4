361d8d64b53cb1f87868b47034288a7035f30875
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-17 12:19:03 +0000

    Remove unused function GetRlzDisabledFlagPath() from login_utils.cc
    To suppress "unused function" error by Clang when building RLZ enabled binary
    
    BUG=None
    TEST=build with RLZ enabled
    
    Review URL: https://codereview.chromium.org/338183002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277739 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/login_utils.cc
+++ chrome/browser/chromeos/login/login_utils.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/singleton.h"
 #include "base/memory/weak_ptr.h"
-#include "base/path_service.h"
 #include "base/prefs/pref_member.h"
 #include "base/prefs/pref_service.h"
 #include "base/strings/string_util.h"
@@ -89,22 +88,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using content::BrowserThread;
 
 namespace chromeos {
-
-namespace {
-
-#if defined(ENABLE_RLZ)
-// Flag file that disables RLZ tracking, when present.
-const base::FilePath::CharType kRLZDisabledFlagName[] =
-    FILE_PATH_LITERAL(".rlz_disabled");
-
-base::FilePath GetRlzDisabledFlagPath() {
-  base::FilePath homedir;
-  PathService::Get(base::DIR_HOME, &homedir);
-  return homedir.Append(kRLZDisabledFlagName);
-}
-#endif
-
-}  // namespace
 
 struct DoBrowserLaunchOnLocaleLoadedData;
 
