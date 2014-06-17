9f6e1f24f519cd7491a20afa20b8019b3cb5b8f5
pneubeck@chromium.org <pneubeck@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-17 16:57:10 +0000

    Add the Sign-At-Most-Once restriction the enterprise.platformKeys API.
    This change ensures that the extension that generated a key with enterprise.platformKeys can use that key at most once for signing data. Any attempt to sign data afterwards with the same key will fail.
    
    BUG=382983
    
    Review URL: https://codereview.chromium.org/323093003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277784 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/enterprise_platform_keys/enterprise_platform_keys_api.cc
+++ chrome/browser/extensions/api/enterprise_platform_keys/enterprise_platform_keys_api.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/values.h"
 #include "chrome/browser/chromeos/platform_keys/platform_keys.h"
+#include "chrome/browser/chromeos/platform_keys/platform_keys_service.h"
+#include "chrome/browser/chromeos/platform_keys/platform_keys_service_factory.h"
 #include "chrome/common/extensions/api/enterprise_platform_keys.h"
 #include "chrome/common/extensions/api/enterprise_platform_keys_internal.h"
 #include "content/public/browser/browser_thread.h"
@@ -49,13 +51,18 @@ EnterprisePlatformKeysInternalGenerateKeyFunction::Run() {
   if (!ValidateToken(params->token_id))
     return RespondNow(Error(kErrorInvalidToken));
 
-  chromeos::platform_keys::GenerateRSAKey(
+  chromeos::PlatformKeysService* service =
+      chromeos::PlatformKeysServiceFactory::GetForBrowserContext(
+          browser_context());
+  DCHECK(service);
+
+  service->GenerateRSAKey(
       params->token_id,
       params->modulus_length,
+      extension_id(),
       base::Bind(
           &EnterprisePlatformKeysInternalGenerateKeyFunction::OnGeneratedKey,
-          this),
-      browser_context());
+          this));
   return RespondLater();
 }
 
@@ -83,12 +90,17 @@ EnterprisePlatformKeysInternalSignFunction::Run() {
   if (!ValidateToken(params->token_id))
     return RespondNow(Error(kErrorInvalidToken));
 
-  chromeos::platform_keys::Sign(
+  chromeos::PlatformKeysService* service =
+      chromeos::PlatformKeysServiceFactory::GetForBrowserContext(
+          browser_context());
+  DCHECK(service);
+
+  service->Sign(
       params->token_id,
       params->public_key,
       params->data,
-      base::Bind(&EnterprisePlatformKeysInternalSignFunction::OnSigned, this),
-      browser_context());
+      extension_id(),
+      base::Bind(&EnterprisePlatformKeysInternalSignFunction::OnSigned, this));
   return RespondLater();
 }
 
