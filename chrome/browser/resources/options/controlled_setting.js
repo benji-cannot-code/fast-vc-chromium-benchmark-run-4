30eaa4f3bedcb278beb0277a25e9e3344f5bb4b6
michaelpg@chromium.org <michaelpg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-17 01:24:33 +0000

    Use consistent shared setting indicator for Accounts options
    Remove the old warning icon and text.
    
    BUG=349124
    R=nkostylev@chromium.org
    TEST=# See test in issue 322293002. Once that test lands,
         # changes to that test will be added to this CL.
    
    Review URL: https://codereview.chromium.org/321303003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277610 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/controlled_setting.js
+++ chrome/browser/resources/options/controlled_setting.js
@@ -95,10 +95,13 @@ cr.define('options', function() {
             'extension': loadTimeData.getString('controlledSettingsExtension'),
             'extensionWithName': loadTimeData.getString(
                 'controlledSettingsExtensionWithName'),
+            'policy': loadTimeData.getString('controlledSettingsPolicy'),
           };
           if (cr.isChromeOS) {
             defaultStrings.shared =
                 loadTimeData.getString('controlledSettingsShared');
+            defaultStrings.owner =
+                loadTimeData.getString('controlledSettingsOwner');
           }
         } else {
           var defaultStrings = {
