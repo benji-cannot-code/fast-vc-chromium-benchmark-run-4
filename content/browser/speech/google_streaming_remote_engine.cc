187dc4f531db695876753f3a692889bb47ddfd3c
sungmann.cho@navercorp.com <sungmann.cho@navercorp.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-17 09:20:01 +0000

    Remove unnecessary #includes in google_streaming_remote_engine.cc.
    After the landing https://codereview.chromium.org/189603005,
    google_streaming_remote_engine.cc doesn't need the command line
    switch related headers anymore. So we can remove these.
    
    BUG=350333
    
    Review URL: https://codereview.chromium.org/333273003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277677 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/speech/google_streaming_remote_engine.cc
+++ content/browser/speech/google_streaming_remote_engine.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "content/browser/speech/audio_buffer.h"
 #include "content/browser/speech/proto/google_streaming_api.pb.h"
-#include "content/public/common/content_switches.h"
 #include "content/public/common/speech_recognition_error.h"
 #include "content/public/common/speech_recognition_result.h"
 #include "google_apis/google_api_keys.h"
