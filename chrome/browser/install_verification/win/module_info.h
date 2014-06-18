550bd7949dee2dca8be14e83943c7333815cc088
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-18 10:01:24 +0000

    Module size is uint32_t on Windows.
    BUG=none
    R=robertshield@chromium.org
    
    Review URL: https://codereview.chromium.org/338353002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278004 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/install_verification/win/module_info.h
+++ chrome/browser/install_verification/win/module_info.h
@@ -16,7 +16,7 @@ struct ModuleInfo {
 
   ModuleInfo(const base::char16* const module_name,
              uintptr_t module_base_address,
-             size_t module_size)
+             uint32_t module_size)
       : base_address(module_base_address),
         size(module_size),
         name(module_name) {
@@ -33,8 +33,8 @@ struct ModuleInfo {
   }
 
   base::string16 name;
-  uintptr_t  base_address;
-  size_t size;
+  uintptr_t base_address;
+  uint32_t size;
 };
 
 #endif  // CHROME_BROWSER_INSTALL_VERIFICATION_WIN_MODULE_INFO_H_
