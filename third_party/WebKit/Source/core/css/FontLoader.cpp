bb61698113bc0edfd2dce1dd579886566cbca310
ksakamoto@chromium.org <ksakamoto@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-18 23:49:32 +0000

    Notify font loads via FontLoader
    Before this patch, font load was noticed to CSSFontSelector through the
    following path:
    
     RemoteFontFaceSource
     -> CSSFontFace
     -> CSSSegmentedFontFace
     -> CSSFontSelector
    
    However, StyleEngine clears FontFaceCache upon stylesheet changes and
    reconstruct it lazily, it is possible that CSSSegmentedFontFace is
    absent when a load is completed. As a result, loaded font is not
    rendered.
    
    This patch fixes the problem by changing the notification path as follows:
     RemoteFontFaceSource -> FontLoader -> CSSFontSelector.
    
    BUG=378238
    TEST=fast/css/font-face-css-change-while-loading.html
    
    Review URL: https://codereview.chromium.org/321403003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176463 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/FontLoader.cpp
+++ third_party/WebKit/Source/core/css/FontLoader.cpp
@@ -6,13 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "core/css/FontLoader.h"
 
+#include "core/css/CSSFontSelector.h"
 #include "core/fetch/FontResource.h"
 #include "core/fetch/ResourceFetcher.h"
 
 namespace WebCore {
 
-FontLoader::FontLoader(ResourceFetcher* resourceFetcher)
+FontLoader::FontLoader(CSSFontSelector* fontSelector, ResourceFetcher* resourceFetcher)
     : m_beginLoadingTimer(this, &FontLoader::beginLoadTimerFired)
+    , m_fontSelector(fontSelector)
     , m_resourceFetcher(resourceFetcher)
 {
 }
@@ -64,8 +66,14 @@ void FontLoader::loadPendingFonts()
     // that were just loaded.
 }
 
+void FontLoader::fontFaceInvalidated()
+{
+    if (m_fontSelector)
+        m_fontSelector->fontFaceInvalidated();
+}
+
 #if !ENABLE(OILPAN)
-void FontLoader::clearResourceFetcher()
+void FontLoader::clearResourceFetcherAndFontSelector()
 {
     if (!m_resourceFetcher) {
         ASSERT(m_fontsToBeginLoading.isEmpty());
@@ -75,12 +83,14 @@ void FontLoader::clearResourceFetcher()
     m_beginLoadingTimer.stop();
     m_fontsToBeginLoading.clear();
     m_resourceFetcher = nullptr;
+    m_fontSelector = nullptr;
 }
 #endif
 
 void FontLoader::trace(Visitor* visitor)
 {
     visitor->trace(m_resourceFetcher);
+    visitor->trace(m_fontSelector);
 }
 
 } // namespace WebCore
--- third_party/WebKit/Source/core/css/FontLoader.h
+++ third_party/WebKit/Source/core/css/FontLoader.h
@@ -14,33 +14,36 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+class CSSFontSelector;
 class FontResource;
 
 class FontLoader : public RefCountedWillBeGarbageCollectedFinalized<FontLoader> {
 public:
-    static PassRefPtrWillBeRawPtr<FontLoader> create(ResourceFetcher* fetcher)
+    static PassRefPtrWillBeRawPtr<FontLoader> create(CSSFontSelector* fontSelector, ResourceFetcher* fetcher)
     {
-        return adoptRefWillBeNoop(new FontLoader(fetcher));
+        return adoptRefWillBeNoop(new FontLoader(fontSelector, fetcher));
     }
     ~FontLoader();
 
     void addFontToBeginLoading(FontResource*);
     void loadPendingFonts();
+    void fontFaceInvalidated();
 
 #if !ENABLE(OILPAN)
-    void clearResourceFetcher();
+    void clearResourceFetcherAndFontSelector();
 #endif
 
     void trace(Visitor*);
 
 private:
-    explicit FontLoader(ResourceFetcher*);
+    FontLoader(CSSFontSelector*, ResourceFetcher*);
     void beginLoadTimerFired(Timer<FontLoader>*);
 
     Timer<FontLoader> m_beginLoadingTimer;
 
     typedef Vector<std::pair<ResourcePtr<FontResource>, ResourceLoader::RequestCountTracker> > FontsToLoadVector;
     FontsToLoadVector m_fontsToBeginLoading;
+    RawPtrWillBeMember<CSSFontSelector> m_fontSelector;
     RawPtrWillBeMember<ResourceFetcher> m_resourceFetcher;
 };
 
