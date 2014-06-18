6723558a61a66237622548509671e15821d5da28
nbarth@chromium.org <nbarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-18 03:25:47 +0000

    Fix a few header order violations
    2 real violations were masked by skipping check-webkit-style in this CL:
    Manually commit changes that cause false-alarm on check-webkit-style.
    https://codereview.chromium.org/330323005/
    
    This fixes them.
    
    BUG=381876
    R=haraken
    
    Review URL: https://codereview.chromium.org/347473002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176384 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/xml/XSLTProcessorLibxslt.cpp
+++ third_party/WebKit/Source/core/xml/XSLTProcessorLibxslt.cpp
@@ -24,10 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "core/xml/XSLTProcessor.h"
 
-#include <libxslt/imports.h>
-#include <libxslt/security.h>
-#include <libxslt/variables.h>
-#include <libxslt/xsltutils.h>
 #include "core/FetchInitiatorTypeNames.h"
 #include "core/dom/Document.h"
 #include "core/dom/TransformSource.h"
@@ -51,6 +47,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/text/CString.h"
 #include "wtf/text/StringBuffer.h"
 #include "wtf/unicode/UTF8.h"
+#include <libxslt/imports.h>
+#include <libxslt/security.h>
+#include <libxslt/variables.h>
+#include <libxslt/xsltutils.h>
 
 namespace WebCore {
 
