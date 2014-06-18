88f63e0526e879b6bc4b49b7f5a84be5fc8398b3
hirono@chromium.org <hirono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-18 08:39:55 +0000

    Revert "Improve the no-op reconcilor so that it doesn't "hang" after the first execution, but instead successfully completes the reconciliation." (https://codereview.chromium.org/309843002/)
    The original CL causes crashes about 20 secs after launching the linux build of chromeos.
    
    This reverts commit 660a4d00c835a21b6972647df37e75f4e22f8d73.
    
    Conflicts:
    	chrome/browser/signin/account_reconcilor_unittest.cc
    
    Original issue's description:
    > Improve the no-op reconcilor so that it doesn't "hang" after the first execution, but instead successfully completes the reconciliation.
    > Improve the UMAHistogramHelper so that it can nicely take snapshots between tests, allowing the test writer to easily test ONLY the changes logged in "this" test.
    > Write a new unit test that executes the reconcilor twice, and make all reconcilor unit tests execute with and without the flag.
    >
    > BUG=357693
    > TBR=phajdan.jr@chromium.org
    >
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=277605
    
    BUG=357693,386045
    TEST=None
    TBR=mlerman@chromium.org
    
    Review URL: https://codereview.chromium.org/344513005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277990 0039d316-1c4b-4281-b951-d872f2087c98

--- components/signin/core/browser/account_reconcilor.cc
+++ components/signin/core/browser/account_reconcilor.cc
@@ -360,10 +360,8 @@ void AccountReconcilor::GoogleSignedOut(const std::string& username) {
 }
 
 void AccountReconcilor::PerformMergeAction(const std::string& account_id) {
-  if (!switches::IsNewProfileManagement()) {
-    MarkAccountAsAddedToCookie(account_id);
+  if (!switches::IsNewProfileManagement())
     return;
-  }
   VLOG(1) << "AccountReconcilor::PerformMergeAction: " << account_id;
   merge_session_helper_.LogIn(account_id);
 }
@@ -401,10 +399,8 @@ void AccountReconcilor::PerformFinishRemoveAction(
 
 void AccountReconcilor::PerformAddToChromeAction(const std::string& account_id,
                                                  int session_index) {
-  if (!switches::IsNewProfileManagement()) {
-    MarkAccountAsAddedToChrome(account_id);
+  if (!switches::IsNewProfileManagement())
     return;
-  }
   VLOG(1) << "AccountReconcilor::PerformAddToChromeAction:"
           << " account=" << account_id << " session_index=" << session_index;
 
@@ -711,9 +707,13 @@ void AccountReconcilor::ScheduleStartReconcileIfChromeAccountsChanged() {
   }
 }
 
-// Remove the account from the list that is being merged.
-void AccountReconcilor::MarkAccountAsAddedToCookie(
-    const std::string& account_id) {
+void AccountReconcilor::MergeSessionCompleted(
+    const std::string& account_id,
+    const GoogleServiceAuthError& error) {
+  VLOG(1) << "AccountReconcilor::MergeSessionCompleted: account_id="
+          << account_id;
+
+  // Remove the account from the list that is being merged.
   for (std::vector<std::string>::iterator i = add_to_cookie_.begin();
        i != add_to_cookie_.end();
        ++i) {
@@ -722,15 +722,7 @@ void AccountReconcilor::MarkAccountAsAddedToCookie(
       break;
     }
   }
-}
 
-void AccountReconcilor::MergeSessionCompleted(
-    const std::string& account_id,
-    const GoogleServiceAuthError& error) {
-  VLOG(1) << "AccountReconcilor::MergeSessionCompleted: account_id="
-          << account_id;
-
-  MarkAccountAsAddedToCookie(account_id);
   CalculateIfReconcileIsDone();
   ScheduleStartReconcileIfChromeAccountsChanged();
 }
@@ -757,9 +749,13 @@ void AccountReconcilor::PerformAddAccountToTokenService(
   token_service_->UpdateCredentials(account_id, refresh_token);
 }
 
-// Remove the account from the list that is being updated.
-void AccountReconcilor::MarkAccountAsAddedToChrome(
-    const std::string& account_id) {
+void AccountReconcilor::HandleRefreshTokenFetched(
+    const std::string& account_id,
+    const std::string& refresh_token) {
+  if (!refresh_token.empty()) {
+    PerformAddAccountToTokenService(account_id, refresh_token);
+  }
+  // Remove the account from the list that is being updated.
   for (std::vector<std::pair<std::string, int> >::iterator i =
            add_to_chrome_.begin();
        i != add_to_chrome_.end();
@@ -769,14 +765,6 @@ void AccountReconcilor::MarkAccountAsAddedToChrome(
       break;
     }
   }
-}
 
-void AccountReconcilor::HandleRefreshTokenFetched(
-    const std::string& account_id,
-    const std::string& refresh_token) {
-  if (!refresh_token.empty())
-    PerformAddAccountToTokenService(account_id, refresh_token);
-
-  MarkAccountAsAddedToChrome(account_id);
   CalculateIfReconcileIsDone();
 }
