ebe8963f8f98dd3dbc949d2f151a3e44e092098d
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-18 00:23:43 +0000

    Remove AttrNodeListMap.
    Change the ownership of AttrNodeList from a static HashMap to ElementRareData.
    
    ElementRareData size increases by this change. A complex web page will
    have slight increase of memory consumption even if the page doesn't
    use Attr nodes.
    
    BUG=357163,381629
    
    Review URL: https://codereview.chromium.org/328243005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176366 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/ElementRareData.h
+++ third_party/WebKit/Source/core/dom/ElementRareData.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ElementRareData_h
 
 #include "core/animation/ActiveAnimations.h"
+#include "core/dom/Attr.h"
 #include "core/dom/DatasetDOMStringMap.h"
 #include "core/dom/NamedNodeMap.h"
 #include "core/dom/NodeRareData.h"
@@ -124,6 +125,10 @@ public:
     void setCustomElementDefinition(PassRefPtr<CustomElementDefinition> definition) { m_customElementDefinition = definition; }
     CustomElementDefinition* customElementDefinition() const { return m_customElementDefinition.get(); }
 
+    WillBeHeapVector<RefPtrWillBeMember<Attr> >& ensureAttrNodeList();
+    WillBeHeapVector<RefPtrWillBeMember<Attr> >* attrNodeList() { return m_attrNodeList.get(); }
+    void removeAttrNodeList() { m_attrNodeList.clear(); }
+
     void traceAfterDispatch(Visitor*);
 
 private:
@@ -136,6 +141,7 @@ private:
     OwnPtrWillBeMember<ClassList> m_classList;
     OwnPtrWillBeMember<ElementShadow> m_shadow;
     OwnPtrWillBeMember<NamedNodeMap> m_attributeMap;
+    OwnPtrWillBeMember<WillBeHeapVector<RefPtrWillBeMember<Attr> > > m_attrNodeList;
     OwnPtrWillBeMember<InputMethodContext> m_inputMethodContext;
     OwnPtrWillBeMember<ActiveAnimations> m_activeAnimations;
     OwnPtrWillBeMember<InlineCSSStyleDeclaration> m_cssomWrapper;
