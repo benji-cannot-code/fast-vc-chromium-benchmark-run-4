cd84ba9b7798641c5f98b63c05e743d58411e2d9
marja@chromium.org <marja@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-18 10:22:20 +0000

    Remove unused text encoding quirks.
    1) Remove more Backslash-as-JPY hacks (some encodings interpret backslash as yen
    sign).
    
    The code was added in https://bugs.webkit.org/show_bug.cgi?id=49714 (r73566).
    
    The test used to be
    editing/selection/find-yensign-and-backslash-with-japanese-fonts-expected.html
    
    The feature was removed by https://codereview.chromium.org/27030014 (r159589),
    but the removal was incomplete. This CL removes the leftover code and test
    expectation files (the corresponding tests were removed by r159589).
    
    2) Remove isJapaneseEncoding from TextEncodingRegistry.cpp; it was dead
    code. There used to be bool TextEncoding::isJapanese() (see r16245) which was
    used by WebCore/loader/Decoder.cpp... but TextEncoding::isJapanese no longer
    exists, and isJapaneseEncoding appears not to be used any more.
    
    BUG=
    R=abarth@chromium.org, jshin@chromium.org
    
    Review URL: https://codereview.chromium.org/335803002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176407 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/text/TextEncoding.cpp
+++ third_party/WebKit/Source/wtf/text/TextEncoding.cpp
@@ -46,7 +46,6 @@ static const TextEncoding& UTF7Encoding()
 
 TextEncoding::TextEncoding(const char* name)
     : m_name(atomicCanonicalTextEncodingName(name))
-    , m_backslashAsCurrencySymbol(backslashAsCurrencySymbol())
 {
     // Aliases are valid, but not "replacement" itself.
     if (m_name && isReplacementEncoding(name))
@@ -55,7 +54,6 @@ TextEncoding::TextEncoding(const char* name)
 
 TextEncoding::TextEncoding(const String& name)
     : m_name(atomicCanonicalTextEncodingName(name))
-    , m_backslashAsCurrencySymbol(backslashAsCurrencySymbol())
 {
     // Aliases are valid, but not "replacement" itself.
     if (m_name && isReplacementEncoding(name))
@@ -133,11 +131,6 @@ bool TextEncoding::usesVisualOrdering() const
 
     static const char* const a = atomicCanonicalTextEncodingName("ISO-8859-8");
     return m_name == a;
-}
-
-UChar TextEncoding::backslashAsCurrencySymbol() const
-{
-    return shouldShowBackslashAsCurrencySymbolIn(m_name) ? 0x00A5 : '\\';
 }
 
 bool TextEncoding::isNonByteBasedEncoding() const
