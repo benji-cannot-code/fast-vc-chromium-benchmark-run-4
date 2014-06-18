a467f9b167c14dd96a7b88503ec6c21a8f34e4bf
tsepez@chromium.org <tsepez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-18 17:32:42 +0000

    Fix "inlcude" typo in comments.
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/343473006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278113 0039d316-1c4b-4281-b951-d872f2087c98

--- ipc/ipc_message_macros.h
+++ ipc/ipc_message_macros.h
@@ -114,8 +114,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //      class_some_other_class;  // Another incomplete class declaration
 //      #endif  // SOME_GUARD_MACRO
 //      #ifdef IPC_MESSAGE_IMPL
-//      #inlcude "path/to/some_class.h"        // Full class declaration
-//      #inlcude "path/to/some_other_class.h"  // Full class declaration
+//      #include "path/to/some_class.h"        // Full class declaration
+//      #include "path/to/some_other_class.h"  // Full class declaration
 //      #endif  // IPC_MESSAGE_IMPL
 //        (.. IPC macros using some_class and some_other_class ...)
 //
