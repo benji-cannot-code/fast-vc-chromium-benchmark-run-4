19477830b55b26e3f7713e57d997fbe4af7a81d8
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-18 11:48:43 +0000

    DevTools: pass agent host id to the renderer (Blink).
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/338993006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176408 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorController.h
+++ third_party/WebKit/Source/core/inspector/InspectorController.h
@@ -55,6 +55,7 @@ class InspectorFrontendChannel;
 class InspectorFrontendClient;
 class InspectorLayerTreeAgent;
 class InspectorPageAgent;
+class InspectorResourceAgent;
 class InspectorTimelineAgent;
 class InspectorTracingAgent;
 class InspectorOverlay;
@@ -90,10 +91,10 @@ public:
 
     void dispatchMessageFromFrontend(const String& message);
 
-    void connectFrontend(InspectorFrontendChannel*);
+    void connectFrontend(const String& hostId, InspectorFrontendChannel*);
     void disconnectFrontend();
     void reconnectFrontend();
-    void reuseFrontend(InspectorFrontendChannel*, const String& inspectorStateCookie);
+    void reuseFrontend(const String& hostId, InspectorFrontendChannel*, const String& inspectorStateCookie);
     void setProcessId(long);
     void setLayerTreeId(int);
 
@@ -146,6 +147,7 @@ private:
 
     InspectorDOMAgent* m_domAgent;
     InspectorPageAgent* m_pageAgent;
+    InspectorResourceAgent* m_resourceAgent;
     InspectorTimelineAgent* m_timelineAgent;
     InspectorLayerTreeAgent* m_layerTreeAgent;
     InspectorTracingAgent* m_tracingAgent;
@@ -159,6 +161,7 @@ private:
     Vector<InspectorAgent*> m_moduleAgents;
     bool m_isUnderTest;
     bool m_deferredAgentsInitialized;
+    String m_hostId;
 };
 
 }
