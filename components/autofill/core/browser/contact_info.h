b6853339cd35eb483544e66bcecc536e89967169
pritam.nikam@samsung.com <pritam.nikam@samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-18 14:36:05 +0000

    Modified to allow to preserve two-word string in first-name and last-name in autofill profile. BUG=132332
    Review URL: https://codereview.chromium.org/261993006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278058 0039d316-1c4b-4281-b951-d872f2087c98

--- components/autofill/core/browser/contact_info.h
+++ components/autofill/core/browser/contact_info.h
@@ -24,6 +24,10 @@ class NameInfo : public FormGroup {
 
   NameInfo& operator=(const NameInfo& info);
 
+  // Compares |NameInfo| objects for |first_|, |middle_| and |last_| names,
+  // ignoring their case differences.
+  bool EqualsIgnoreCase(const NameInfo& info);
+
   // FormGroup:
   virtual base::string16 GetRawInfo(ServerFieldType type) const OVERRIDE;
   virtual void SetRawInfo(ServerFieldType type,
