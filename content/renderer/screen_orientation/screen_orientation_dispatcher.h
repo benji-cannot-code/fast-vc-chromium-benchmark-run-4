461acf71627b79c4a6ee69018a80b368c13f8e2a
mlamouri@chromium.org <mlamouri@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-18 15:14:47 +0000

    Cleanup: remove not used forward declaration.
    BUG=None
    
    Review URL: https://codereview.chromium.org/340033002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278079 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/screen_orientation/screen_orientation_dispatcher.h
+++ content/renderer/screen_orientation/screen_orientation_dispatcher.h
@@ -15,10 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/platform/WebScreenOrientationLockType.h"
 #include "third_party/WebKit/public/platform/WebScreenOrientationType.h"
 
-namespace blink {
-class WebScreenOrientationListener;
-}
-
 namespace content {
 
 class RenderFrame;
