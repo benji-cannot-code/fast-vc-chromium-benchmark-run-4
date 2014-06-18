31e5b9b4d71de6bb6b5cc98b19be3e672554d1cb
sungmann.cho@navercorp.com <sungmann.cho@navercorp.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-18 09:54:02 +0000

    Remove unnecessary #includes in browser_tab_strip_controller.cc.
    After the landing https://codereview.chromium.org/291093005,
    browser_tab_strip_controller.cc doesn't need the command line switch
    related headers anymore. So we can remove these.
    
    BUG=332077
    
    Review URL: https://codereview.chromium.org/339023002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278003 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/tabs/browser_tab_strip_controller.cc
+++ chrome/browser/ui/views/tabs/browser_tab_strip_controller.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/tabs/browser_tab_strip_controller.h"
 
 #include "base/auto_reset.h"
-#include "base/command_line.h"
 #include "base/prefs/pref_service.h"
 #include "base/task_runner_util.h"
 #include "base/threading/sequenced_worker_pool.h"
