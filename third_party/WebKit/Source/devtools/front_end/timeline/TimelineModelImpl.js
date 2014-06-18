e9b98625a42dfe5e77ece15b25529da77e83e31f
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-18 07:41:03 +0000

    Inline usages of TimelineUIUtils.categoryForRecord
    BUG=361045
    R=caseq@chromium.org
    
    Review URL: https://codereview.chromium.org/337283004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176400 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/timeline/TimelineModelImpl.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TimelineModelImpl.js
@@ -421,7 +421,7 @@ WebInspector.TimelineModel.RecordImpl.prototype = {
      */
     category: function()
     {
-        return WebInspector.TimelineUIUtils.categoryForRecord(this);
+        return WebInspector.TimelineUIUtils.recordStyle(this).category;
     },
 
     /**
