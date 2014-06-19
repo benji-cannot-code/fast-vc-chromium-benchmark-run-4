53bee479cbe211a8008a74eba4fb0b3e12ab51f2
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-19 01:33:43 +0000

    Address views session crashed bubble style nits.
    Use the built-in bubble title and close button views.
    Use the bubble's title insets for the other views.
    Use BookmarkSyncPromoView colors for the UMA promo.
    Use views layout constants for the spacing values.
    Use default label/checkbox/bubble colors, not hard-coded.
    (black/off-white + supports inversion for accessibility)
    Fix the UMA promo separator line (wasn't fully extended).
    
    Inline some BrowserRemovalObserver function implementations.
    Other misc. code cleanup. See before/after pics on the bug.
    
    BUG=384123
    TEST=Session crashes bubble looks good, works well.
    R=yiyaoliu@chromium.org,sky@chromium.org
    
    Review URL: https://codereview.chromium.org/331353002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278228 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/session_crashed_bubble_view.h
+++ chrome/browser/ui/views/session_crashed_bubble_view.h
@@ -61,6 +61,9 @@ class SessionCrashedBubbleView
 
   // WidgetDelegateView methods.
   virtual views::View* GetInitiallyFocusedView() OVERRIDE;
+  virtual base::string16 GetWindowTitle() const OVERRIDE;
+  virtual bool ShouldShowWindowTitle() const OVERRIDE;
+  virtual bool ShouldShowCloseButton() const OVERRIDE;
 
   // views::BubbleDelegateView methods.
   virtual void Init() OVERRIDE;
@@ -118,9 +121,6 @@ class SessionCrashedBubbleView
 
   // Button for the user to confirm a session restore.
   views::LabelButton* restore_button_;
-
-  // Button for the user to close this bubble.
-  views::LabelButton* close_;
 
   // Checkbox for the user to opt-in to UMA reporting.
   views::Checkbox* uma_option_;
