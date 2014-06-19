ad03fdd17a7cf6a2e78513fa5fd5b157e0b7e33d
jackhou@chromium.org <jackhou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-19 16:26:23 +0000

    [Mac]. Increment app shortcut version.
    This also increments the app list shortcut version.
    
    This will trigger a rebuild of all app shims.
    
    This also disables updating all shortcuts in tests.
    
    BUG=353047
    
    Review URL: https://codereview.chromium.org/346753002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278397 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/apps/shortcut_manager.cc
+++ chrome/browser/apps/shortcut_manager.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/shortcut_manager.h"
 
 #include "base/bind.h"
+#include "base/command_line.h"
 #include "base/compiler_specific.h"
 #include "base/prefs/pref_service.h"
 #include "base/strings/string16.h"
@@ -18,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/shell_integration.h"
 #include "chrome/browser/web_applications/web_app.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "content/public/browser/browser_thread.h"
@@ -34,7 +36,11 @@ namespace {
 // This version number is stored in local prefs to check whether app shortcuts
 // need to be recreated. This might happen when we change various aspects of app
 // shortcuts like command-line flags or associated icons, binaries, etc.
+#if defined(OS_MACOSX)
+const int kCurrentAppShortcutsVersion = 1;
+#else
 const int kCurrentAppShortcutsVersion = 0;
+#endif
 
 // Delay in seconds before running UpdateShortcutsForAllApps.
 const int kUpdateShortcutsForAllAppsDelay = 10;
@@ -148,6 +154,9 @@ void AppShortcutManager::OnProfileWillBeRemoved(
 }
 
 void AppShortcutManager::UpdateShortcutsForAllAppsIfNeeded() {
+  if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kTestType))
+    return;
+
   int last_version = prefs_->GetInteger(prefs::kAppShortcutsVersion);
   if (last_version >= kCurrentAppShortcutsVersion)
     return;
