331e4c9e30c8aaa144d36ba2b8bae9ee78717e5f
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-19 07:31:29 +0000

    Move needsPreviewElement to TimelineUIUtilsImple and make it private
    Drive-by: removed formatter for counters field which is not in the protocol anymore.
    
    BUG=361045
    R=loislo@chromium.org
    
    Review URL: https://codereview.chromium.org/343613004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176495 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/inspector/timeline-test.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/timeline-test.js
@@ -14,7 +14,6 @@ InspectorTest.timelinePropertyFormatters = {
     usedHeapSizeDelta: "skip",
     mimeType: "formatAsTypeName",
     id: "formatAsTypeName",
-    counters: "formatAsTypeName",
     timerId: "formatAsTypeName",
     scriptLine: "formatAsTypeName",
     layerId: "formatAsTypeName",
