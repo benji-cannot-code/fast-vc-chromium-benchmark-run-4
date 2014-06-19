df21c001a669794319f3f57be31ac200f9d97670
stuartmorgan@chromium.org <stuartmorgan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-19 22:49:55 +0000

    Remove IsRunningOnIOS6OrLater
    iOS 5 hasn't been supported for several releases, so this is always
    true.
    
    BUG=300130
    
    Review URL: https://codereview.chromium.org/349453002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278519 0039d316-1c4b-4281-b951-d872f2087c98

--- ios/consumer/base/util.mm
+++ ios/consumer/base/util.mm
@@ -8,10 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ios {
 
-bool IsRunningOnIOS6OrLater() {
-  return base::ios::IsRunningOnIOS6OrLater();
-}
-
 bool IsRunningOnOrLater(int major, int minor, int bug_fix) {
   return base::ios::IsRunningOnOrLater(major, minor, bug_fix);
 }
--- ios/public/consumer/base/util.h
+++ ios/public/consumer/base/util.h
@@ -8,9 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ios {
 
-// Returns whether the operating system is iOS 6 or later.
-bool IsRunningOnIOS6OrLater();
-
 // Returns whether the operating system is at the given version or later.
 bool IsRunningOnOrLater(int major, int minor, int bug_fix);
 
