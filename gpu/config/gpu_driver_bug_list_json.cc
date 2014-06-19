3e755e04edd257cf8d5e7b8e37f16aac8635498b
sievers@chromium.org <sievers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-19 22:46:00 +0000

    Deprecate disable_egl_khr_fence_sync on future Android releases
    NOTRY=True
    
    Review URL: https://codereview.chromium.org/347893002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278515 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/config/gpu_driver_bug_list_json.cc
+++ gpu/config/gpu_driver_bug_list_json.cc
@@ -20,7 +20,7 @@ const char kGpuDriverBugListJson[] = LONG_STRING_CONST(
 {
   "name": "gpu driver bug list",
   // Please update the version number whenever you change this file.
-  "version": "6.1",
+  "version": "6.2",
   "entries": [
     {
       "id": 1,
@@ -954,9 +954,13 @@ LONG_STRING_CONST(
     {
       "id": 74,
       "cr_bugs": [278606, 382686],
-      "description": "Testing EGL sync fences is broken on most Qualcomm drivers",
+      "description": "Testing EGL sync fences was broken on most Qualcomm drivers",
       "os": {
-        "type": "android"
+        "type": "android",
+        "version": {
+          "op": "<=",
+          "value": "4.4.3"
+        }
       },
       "gl_vendor": {
         "op": "beginwith",
@@ -992,9 +996,13 @@ LONG_STRING_CONST(
     {
       "id": 76,
       "cr_bugs": [371530],
-      "description": "Testing EGL sync fences is broken on IMG",
+      "description": "Testing EGL sync fences was broken on IMG",
       "os": {
-        "type": "android"
+        "type": "android",
+        "version": {
+          "op": "<=",
+          "value": "4.4.3"
+        }
       },
       "gl_vendor": {
         "op": "beginwith",
@@ -1007,9 +1015,13 @@ LONG_STRING_CONST(
     {
       "id": 77,
       "cr_bugs": [378691, 373360, 371530],
-      "description": "Testing fences is broken on Mali-400 MP drivers",
+      "description": "Testing fences was broken on Mali-400 MP drivers",
       "os": {
-        "type": "android"
+        "type": "android",
+        "version": {
+          "op": "<=",
+          "value": "4.4.3"
+        }
       },
       "gl_vendor": {
         "op": "beginwith",
@@ -1026,9 +1038,13 @@ LONG_STRING_CONST(
     {
       "id": 78,
       "cr_bugs": [378691, 373360, 371530],
-      "description": "Testing fences is broken on Broadcom drivers",
+      "description": "Testing fences was broken on Broadcom drivers",
       "os": {
-        "type": "android"
+        "type": "android",
+        "version": {
+          "op": "<=",
+          "value": "4.4.3"
+        }
       },
       "gl_vendor": {
         "op": "beginwith",
