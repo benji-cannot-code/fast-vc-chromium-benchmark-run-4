1eaa1644fd48fb18ca8c18a688e23d66f7aed6d2
hans@chromium.org <hans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-19 00:45:35 +0000

    Add PLATFORM_EXPORT to MediaStreamComponent::AudioSourceProviderImpl
    On non-Windows, PLATFORM_EXPORT expands to a visibility attribute,
    which is inherited by nested classes. On Windows, it's expanded to
    a dllexport/dllimport attribute, which is not inherited.
    
    When using Clang on Windows, MediaStreamComponent's dtor got
    inlined, which destroys m_sourceProvider, whose destructor got inlined,
    and tried to reference provideInput for the vtable, but failed to link
    since it wasn't in the right dll. Exporting the inner class fixes this.
    
    BUG=82385
    TEST=build blink_web.dll with Clang on Windows in Release mode
    
    Review URL: https://codereview.chromium.org/343763002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176468 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/mediastream/MediaStreamComponent.h
+++ third_party/WebKit/Source/platform/mediastream/MediaStreamComponent.h
@@ -80,7 +80,7 @@ private:
     // AudioSourceProviderImpl wraps a WebAudioSourceProvider::provideInput()
     // calls into chromium to get a rendered audio stream.
 
-    class AudioSourceProviderImpl FINAL: public AudioSourceProvider {
+    class PLATFORM_EXPORT AudioSourceProviderImpl FINAL: public AudioSourceProvider {
     public:
         AudioSourceProviderImpl()
             : m_webAudioSourceProvider(0)
