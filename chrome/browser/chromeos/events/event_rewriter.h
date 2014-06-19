a4077a025bbc2ef9169eb8848a597550006f3699
kpschoedel@chromium.org <kpschoedel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-19 12:32:53 +0000

    Convert sticky keys to a chromeos::EventRewriter phase.
    BUG=354035
    TEST=unit_tests,ash_unittests,manual
    R=sadrul@chromium.org,derat@chromium.org
    
    Review URL: https://codereview.chromium.org/255033003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278342 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/events/event_rewriter.h
+++ chrome/browser/chromeos/events/event_rewriter.h
@@ -24,6 +24,10 @@ typedef union _XEvent XEvent;
 
 class PrefService;
 
+namespace ash {
+class StickyKeysController;
+}
+
 namespace chromeos {
 namespace input_method {
 class ImeKeyboard;
@@ -52,7 +56,10 @@ class EventRewriter
     kDeviceAppleKeyboard,
   };
 
-  EventRewriter();
+  // Does not take ownership of the |sticky_keys_controller|, which may also
+  // be NULL (for testing without ash), in which case sticky key operations
+  // don't happen.
+  explicit EventRewriter(ash::StickyKeysController* sticky_keys_controller);
   virtual ~EventRewriter();
 
   // Calls DeviceAddedInternal.
@@ -99,7 +106,7 @@ class EventRewriter
 #endif
 
  private:
-  // Things that internal rewriter phases can change about an Event.
+  // Things that keyboard-related rewriter phases can change about an Event.
   struct MutableKeyState {
     int flags;
     ui::KeyboardCode key_code;
@@ -158,6 +165,9 @@ class EventRewriter
   ui::EventRewriteStatus RewriteTouchEvent(
       const ui::TouchEvent& touch_event,
       scoped_ptr<ui::Event>* rewritten_event);
+  ui::EventRewriteStatus RewriteScrollEvent(
+      const ui::ScrollEvent& scroll_event,
+      scoped_ptr<ui::Event>* rewritten_event);
 
   // Rewriter phases. These can inspect the original |event|, but operate using
   // the current |state|, which may have been modified by previous phases.
@@ -175,6 +185,10 @@ class EventRewriter
 
   chromeos::input_method::ImeKeyboard* ime_keyboard_for_testing_;
   const PrefService* pref_service_for_testing_;
+
+  // The sticky keys controller is not owned here;
+  // at time of writing it is a singleton in ash::Shell>
+  ash::StickyKeysController* sticky_keys_controller_;
 
   DISALLOW_COPY_AND_ASSIGN(EventRewriter);
 };
