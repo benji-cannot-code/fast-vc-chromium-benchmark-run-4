94a610f9ffd3c9de9967ddd37a52c1c38f145d8c
peria@chromium.org <peria@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-19 13:57:49 +0000

    [SyncFS] Remove a friendship between MetadataDatabase and MetadataDatabaseIndex
    BUG=347425
    TEST=./unit_tests --gtest_filter="MetadataDatabaseTest.*:MetadataDatabaseIndexTest.*"
    
    Review URL: https://codereview.chromium.org/340243004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278359 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_backend/metadata_database_index.h
+++ chrome/browser/sync_file_system/drive_backend/metadata_database_index.h
@@ -119,7 +119,12 @@ class MetadataDatabaseIndex {
   // Promotes all demoted dirty trackers to normal dirty trackers.
   void PromoteDemotedDirtyTrackers();
 
+  size_t CountDirtyTracker() const;
+  size_t CountFileMetadata() const;
+  size_t CountFileTracker() const;
   std::vector<std::string> GetRegisteredAppIDs() const;
+  std::vector<int64> GetAllTrackerIDs() const;
+  std::vector<std::string> GetAllMetadataIDs() const;
 
  private:
   typedef base::ScopedPtrHashMap<std::string, FileMetadata> MetadataByID;
@@ -132,7 +137,6 @@ class MetadataDatabaseIndex {
   typedef base::hash_set<ParentIDAndTitle> PathSet;
   typedef std::set<int64> DirtyTrackers;
 
-  friend class MetadataDatabase;
   friend class MetadataDatabaseTest;
 
   // Maintains |app_root_by_app_id_|.
