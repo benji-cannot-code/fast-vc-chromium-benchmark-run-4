eb09eee8fcc764aba142a21851c61653a132e035
mathp@chromium.org <mathp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-19 01:10:58 +0000

    [Android NTP] Use UMA histograms instead of local ones.
    An oversight, the histograms should have the UMA_ prefix.
    
    BUG=385159
    
    Review URL: https://codereview.chromium.org/343643002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278215 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/android/most_visited_sites.cc
+++ chrome/browser/android/most_visited_sites.cc
@@ -279,7 +279,7 @@ void MostVisitedSites::RecordOpenedMostVisitedItem(JNIEnv* env,
                                                    jint index) {
   switch (mv_source_) {
     case TOP_SITES: {
-      HISTOGRAM_SPARSE_SLOWLY(kOpenedItemClientHistogramName, index);
+      UMA_HISTOGRAM_SPARSE_SLOWLY(kOpenedItemClientHistogramName, index);
       break;
     }
     case SUGGESTIONS_SERVICE: {
@@ -290,7 +290,7 @@ void MostVisitedSites::RecordOpenedMostVisitedItem(JNIEnv* env,
               server_suggestions_.suggestions(index).providers(0));
           LogHistogramEvent(histogram, index, num_sites_);
         } else {
-          HISTOGRAM_SPARSE_SLOWLY(kOpenedItemServerHistogramName, index);
+          UMA_HISTOGRAM_SPARSE_SLOWLY(kOpenedItemServerHistogramName, index);
         }
       }
       break;
@@ -354,9 +354,9 @@ void MostVisitedSites::OnMostVisitedURLsAvailable(
   mv_source_ = TOP_SITES;
 
   int num_tiles = urls.size();
-  HISTOGRAM_SPARSE_SLOWLY(kNumTilesHistogramName, num_tiles);
+  UMA_HISTOGRAM_SPARSE_SLOWLY(kNumTilesHistogramName, num_tiles);
   for (int i = 0; i < num_tiles; ++i) {
-    HISTOGRAM_SPARSE_SLOWLY(kImpressionClientHistogramName, i);
+    UMA_HISTOGRAM_SPARSE_SLOWLY(kImpressionClientHistogramName, i);
   }
 
   JNIEnv* env = AttachCurrentThread();
@@ -390,10 +390,10 @@ void MostVisitedSites::OnSuggestionsProfileAvailable(
           kImpressionServerHistogramFormat, suggestion.providers(0));
       LogHistogramEvent(histogram, i, num_sites_);
     } else {
-      HISTOGRAM_SPARSE_SLOWLY(kImpressionServerHistogramName, i);
+      UMA_HISTOGRAM_SPARSE_SLOWLY(kImpressionServerHistogramName, i);
     }
   }
-  HISTOGRAM_SPARSE_SLOWLY(kNumTilesHistogramName, i);
+  UMA_HISTOGRAM_SPARSE_SLOWLY(kNumTilesHistogramName, i);
 
   mv_source_ = SUGGESTIONS_SERVICE;
   // Keep a copy of the suggestions for eventual logging.
@@ -455,12 +455,12 @@ void MostVisitedSites::OnSuggestionsThumbnailAvailable(
 }
 
 void MostVisitedSites::RecordUMAMetrics() {
-  HISTOGRAM_SPARSE_SLOWLY(kNumLocalThumbnailTilesHistogramName,
-                          num_local_thumbs_);
+  UMA_HISTOGRAM_SPARSE_SLOWLY(kNumLocalThumbnailTilesHistogramName,
+                              num_local_thumbs_);
   num_local_thumbs_ = 0;
-  HISTOGRAM_SPARSE_SLOWLY(kNumEmptyTilesHistogramName, num_empty_thumbs_);
+  UMA_HISTOGRAM_SPARSE_SLOWLY(kNumEmptyTilesHistogramName, num_empty_thumbs_);
   num_empty_thumbs_ = 0;
-  HISTOGRAM_SPARSE_SLOWLY(kNumServerTilesHistogramName, num_server_thumbs_);
+  UMA_HISTOGRAM_SPARSE_SLOWLY(kNumServerTilesHistogramName, num_server_thumbs_);
   num_server_thumbs_ = 0;
 }
 
