2ddaf8e8cac998f911c96af6d611639d1a0b084d
macourteau@chromium.org <macourteau@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-19 06:39:07 +0000

    Disables the testOnPageFinishedCalledAfterError test (it appears to be flaky).
    BUG=386300
    TBR=mnaganov@chromium.org
    
    Review URL: https://codereview.chromium.org/342713003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278284 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/javatests/src/org/chromium/android_webview/test/ClientOnPageFinishedTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/ClientOnPageFinishedTest.java
@@ -10,6 +10,7 @@ import android.test.suitebuilder.annotation.MediumTest;
 import org.chromium.android_webview.AwContents;
 import org.chromium.android_webview.test.util.CommonResources;
 import org.chromium.android_webview.test.util.JSUtils;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.content.browser.test.util.TestCallbackHelperContainer;
 import org.chromium.net.test.util.TestWebServer;
@@ -49,8 +50,13 @@ public class ClientOnPageFinishedTest extends AwTestBase {
         assertEquals("data:text/html," + html, onPageFinishedHelper.getUrl());
     }
 
-    @MediumTest
-    @Feature({"AndroidWebView"})
+    /**
+     * @MediumTest
+     * @Feature({"AndroidWebView"})
+     *
+     * http://crbug.com/386300
+     */
+    @DisabledTest
     public void testOnPageFinishedCalledAfterError() throws Throwable {
         class LocalTestClient extends TestAwContentsClient {
             private boolean isOnReceivedErrorCalled = false;
