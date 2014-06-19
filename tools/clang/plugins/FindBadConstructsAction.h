dfb13ea6a51a7ddba3b77f6929527747b6ee84d9
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-19 21:14:37 +0000

    Chrome clang plugin cleanup part 1 of ??
    The clang plugin has grown organically over its lifetime. This patch
    just splits apart the options struct, the PluginASTAction (which most
    developers don't care about), and the actual ASTConsumer into separate
    units.
    
    R=erg@chromium.org
    
    Review URL: https://codereview.chromium.org/348543002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278487 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/clang/plugins/FindBadConstructsAction.h
@@ -0,0 +1,33 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef TOOLS_CLANG_PLUGINS_FINDBADCONSTRUCTIONS_ACTION_H_
+#define TOOLS_CLANG_PLUGINS_FINDBADCONSTRUCTIONS_ACTION_H_
+
+#include "clang/Frontend/FrontendAction.h"
+
+#include "Options.h"
+
+namespace chrome_checker {
+
+class FindBadConstructsAction : public clang::PluginASTAction {
+ public:
+  FindBadConstructsAction();
+
+ protected:
+  // Overridden from PluginASTAction:
+  virtual clang::ASTConsumer* CreateASTConsumer(
+      clang::CompilerInstance& instance,
+      llvm::StringRef ref);
+  virtual bool ParseArgs(const clang::CompilerInstance& instance,
+                         const std::vector<std::string>& args);
+
+ private:
+  Options options_;
+};
+
+}  // namespace chrome_checker
+
+#endif  // TOOLS_CLANG_PLUGINS_FINDBADCONSTRUCTIONS_ACTION_H_
