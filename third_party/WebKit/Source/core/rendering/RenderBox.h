bce7dac78c4ae86a93499fd7a5f0bf7a742524b7
robhogan@gmail.com <robhogan@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-19 22:29:09 +0000

    Refactor avoidsFloats() to reflect CSS2.1 spec more clearly
    Introduce the 'createsBlockFormatting' concept to RenderBlock::avoidsFloats()
    and remove a couple of now-redundant virtualizations of the function.
    
    BUG=
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=173549
    
    Review URL: https://codereview.chromium.org/268833003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176557 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderBox.h
+++ third_party/WebKit/Source/core/rendering/RenderBox.h
@@ -665,6 +665,8 @@ protected:
 
     void updateIntrinsicContentLogicalHeight(LayoutUnit intrinsicContentLogicalHeight) const { m_intrinsicContentLogicalHeight = intrinsicContentLogicalHeight; }
 
+    bool createsBlockFormattingContext() const;
+
 private:
     void updateShapeOutsideInfoAfterStyleChange(const RenderStyle&, const RenderStyle* oldStyle);
     void updateGridPositionAfterStyleChange(const RenderStyle*);
