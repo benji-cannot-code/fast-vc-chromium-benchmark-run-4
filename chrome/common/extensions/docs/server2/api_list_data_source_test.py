d31984caacab9d4a49d5a27cb708518f203cbb01
ahernandez.miralles@gmail.com <ahernandez.miralles@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-19 19:04:08 +0000

    Docserver: Replace instances of 'Api' with 'API'
    The 'API' spelling variant is favored over the 'Api' spelling in current docserver
    code, so to maintain consistency the 'API' spelling has been enforced.
    
    NOTRY=True
    
    Review URL: https://codereview.chromium.org/337373004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278449 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/server2/api_list_data_source_test.py
+++ chrome/common/extensions/docs/server2/api_list_data_source_test.py
@@ -34,13 +34,13 @@ def _ToTestFeatures(names):
   return features
 
 
-def _ToTestApiData(names):
+def _ToTestAPIData(names):
   api_data = dict((name, [{'namespace': name, 'description': description}])
               for name, description in names)
   return api_data
 
 
-def _ToTestApiSchema(names, apis):
+def _ToTestAPISchema(names, apis):
   for name, json_file in names:
     apis['api'][json_file] = json.dumps(_TEST_API_DATA[name])
   return apis
@@ -61,7 +61,7 @@ _TEST_API_FEATURES = _ToTestFeatures([
 ])
 
 
-_TEST_API_DATA = _ToTestApiData([
+_TEST_API_DATA = _ToTestAPIData([
   ('alarms', u'<code>alarms</code>'),
   ('app.window', u'<code>app.window</code>'),
   ('browserAction', u'<code>browserAction</code>'),
@@ -91,7 +91,7 @@ _TEST_API_SCHEMA = [
 ]
 
 
-_TEST_DATA = _ToTestApiSchema(_TEST_API_SCHEMA, {
+_TEST_DATA = _ToTestAPISchema(_TEST_API_SCHEMA, {
   'api': {
     '_api_features.json': json.dumps(_TEST_API_FEATURES),
     '_manifest_features.json': '{}',
