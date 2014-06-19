5bf47bac605388c8b856ac6d18d4de8ad4830bf4
timloh@chromium.org <timloh@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-19 05:08:49 +0000

    Support unknown property strings in transition-property
    The spec mandates that transition-property (and thus the transition
    shorthand) are able to support arbitrary unknown properties, for
    backwards and forwards compatibility. This patch adds support for such
    unknown properties.
    
    Note that this makes the shorthand strings like "ease" or "ease ease-in"
    ambiguous. In this patch we take the first case to refer to a timing
    function, and in the second case the first token as a timing function
    and the second token as the property name.
    
    This change progresses the w3c transitions test transition-property-002,
    although the first two cases of the test are incorrect.
    
    BUG=304020,375098
    
    Review URL: https://codereview.chromium.org/298043007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176489 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
@@ -1898,7 +1898,7 @@ bool CSSPropertyParser::parseAnimationShorthand(CSSPropertyID propId, bool impor
 bool CSSPropertyParser::parseTransitionShorthand(CSSPropertyID propId, bool important)
 {
     const unsigned numProperties = 4;
-    const StylePropertyShorthand& shorthand = shorthandForProperty(propId);
+    const StylePropertyShorthand& shorthand = parsingShorthandForProperty(propId);
     ASSERT(numProperties == shorthand.length());
 
     ShorthandScope scope(this, propId);
@@ -1939,8 +1939,8 @@ bool CSSPropertyParser::parseTransitionShorthand(CSSPropertyID propId, bool impo
             return false;
     }
 
-    ASSERT(shorthand.properties()[0] == CSSPropertyTransitionProperty || shorthand.properties()[0] == CSSPropertyWebkitTransitionProperty);
-    if (!isValidTransitionPropertyList(values[0].get()))
+    ASSERT(shorthand.properties()[3] == CSSPropertyTransitionProperty || shorthand.properties()[3] == CSSPropertyWebkitTransitionProperty);
+    if (!isValidTransitionPropertyList(values[3].get()))
         return false;
 
     // Fill in any remaining properties with the initial value and add
@@ -3088,7 +3088,9 @@ PassRefPtrWillBeRawPtr<CSSValue> CSSPropertyParser::parseAnimationProperty()
         return cssValuePool().createIdentifierValue(result);
     if (equalIgnoringCase(value, "none"))
         return cssValuePool().createIdentifierValue(CSSValueNone);
-    return nullptr;
+    if (equalIgnoringCase(value, "inherit") || equalIgnoringCase(value, "initial"))
+        return nullptr;
+    return createPrimitiveStringValue(value);
 }
 
 bool CSSPropertyParser::parseWebkitTransformOriginShorthand(RefPtrWillBeRawPtr<CSSValue>& value1, RefPtrWillBeRawPtr<CSSValue>& value2, RefPtrWillBeRawPtr<CSSValue>& value3)
