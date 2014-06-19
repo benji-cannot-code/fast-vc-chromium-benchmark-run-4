4117c6865e37270305e64778d464290a642334f6
jdonnelly@chromium.org <jdonnelly@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-19 01:14:45 +0000

    [AiS] Add a structured representation of Answer JSON.
    BUG=383907
    
    Review URL: https://codereview.chromium.org/332943002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278217 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/OmniboxSuggestion.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/OmniboxSuggestion.java
@@ -20,6 +20,7 @@ public class OmniboxSuggestion {
     private final String mDescription;
     private final String mAnswerContents;
     private final String mAnswerType;
+    private final SuggestionAnswer mAnswer;
     private final String mFillIntoEdit;
     private final String mUrl;
     private final String mFormattedUrl;
@@ -105,6 +106,14 @@ public class OmniboxSuggestion {
         mFormattedUrl = formattedUrl;
         mIsStarred = isStarred;
         mIsDeletable = isDeletable;
+
+        if (!TextUtils.isEmpty(mAnswerContents)) {
+            // If any errors are encountered parsing the answer contents, this will return null and
+            // hasAnswer will return false, just as if there were no answer contents at all.
+            mAnswer = SuggestionAnswer.parseAnswerContents(mAnswerContents);
+        } else {
+            mAnswer = null;
+        }
     }
 
     /* TODO(groby): Remove - see http://crbug.com/375482 */
@@ -137,6 +146,14 @@ public class OmniboxSuggestion {
 
     public String getAnswerType() {
         return mAnswerType;
+    }
+
+    public SuggestionAnswer getAnswer() {
+        return mAnswer;
+    }
+
+    public boolean hasAnswer() {
+        return mAnswer != null;
     }
 
     public String getFillIntoEdit() {
