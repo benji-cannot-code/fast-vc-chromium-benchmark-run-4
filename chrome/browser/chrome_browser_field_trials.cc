ddab2be8e719f2da6881fe7cc7227c074c88d372
cthomp@chromium.org <cthomp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-20 00:36:30 +0000

    Removed NPAPI plugin infobar, with Finch trial gate (disabled by default).
    This change is related to the new plugin page action UI https://codereview.chromium.org/319553008/
    
    BUG=307323
    
    Review URL: https://codereview.chromium.org/300873002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278549 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_browser_field_trials.cc
+++ chrome/browser/chrome_browser_field_trials.cc
@@ -62,6 +62,7 @@ void ChromeBrowserFieldTrials::InstantiateDynamicTrials() {
   // Mark here so they will be sync-ed.
   base::FieldTrialList::FindValue("CLD1VsCLD2");
   base::FieldTrialList::FindValue("MouseEventPreconnect");
+  base::FieldTrialList::FindValue("UnauthorizedPluginInfoBar");
   // Activate the autocomplete dynamic field trials.
   OmniboxFieldTrial::ActivateDynamicTrials();
 }
