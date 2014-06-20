38d93e6d42bc59df4b2bf00d12dd12d09780e750
armansito@chromium.org <armansito@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-20 04:40:45 +0000

    device/bluetooth: Implement BluetoothGattConnection on Chrome OS.
    This patch adds BluetoothGattConnectionChromeOS and implements
    BluetoothDeviceChromeOS::CreateGattConnection. At this point, creating
    a connection simply calls org.bluez.Device1.Connect without pairing.
    BluetoothGattConnectionChromeOS::Disconnect currently doesn't do anything,
    at least until there is a system in bluetoothd for managing GATT connections
    properly between Chrome and its profiles and plugins.
    
    BUG=384620,381305
    TEST=device_unittests
    
    Review URL: https://codereview.chromium.org/333983003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278595 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/dbus/fake_bluetooth_device_client.cc
+++ chromeos/dbus/fake_bluetooth_device_client.cc
@@ -294,7 +294,8 @@ void FakeBluetoothDeviceClient::Connect(
   }
 
   if (properties->paired.value() != true &&
-      object_path != dbus::ObjectPath(kConnectUnpairablePath)) {
+      object_path != dbus::ObjectPath(kConnectUnpairablePath) &&
+      object_path != dbus::ObjectPath(kLowEnergyPath)) {
     // Must be paired.
     error_callback.Run(bluetooth_device::kErrorFailed, "Not paired");
     return;
---
+++ device/bluetooth/test/mock_bluetooth_gatt_connection.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef DEVICE_BLUETOOTH_TEST_MOCK_BLUETOOTH_GATT_CONNECTION_H_
+#define DEVICE_BLUETOOTH_TEST_MOCK_BLUETOOTH_GATT_CONNECTION_H_
+
+#include "device/bluetooth/bluetooth_gatt_connection.h"
+#include "testing/gmock/include/gmock/gmock.h"
+
+namespace device {
+
+class MockBluetoothGattConnection : public BluetoothGattConnection {
+ public:
+  MockBluetoothGattConnection(const std::string& device_address);
+  virtual ~MockBluetoothGattConnection();
+
+  MOCK_CONST_METHOD0(GetDeviceAddress, std::string());
+  MOCK_METHOD0(IsConnected, bool());
+  MOCK_METHOD1(Disconnect, void(const base::Closure&));
+};
+
+}  // namespace device
+
+#endif  // DEVICE_BLUETOOTH_TEST_MOCK_BLUETOOTH_GATT_CONNECTION_H_
