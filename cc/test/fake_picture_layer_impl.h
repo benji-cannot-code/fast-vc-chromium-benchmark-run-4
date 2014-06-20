d12b643ebed7028823926c4c410f4d1aa819a93f
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-20 05:03:23 +0000

    cc: Don't add new tilings while syncing a tiling.
    When a layer adds a tiling, it syncs to its pending/active twin and
    adds the tiling there. We "cleverly" do an UpdateTilePriorities() then
    immediately if possible. But now UpdateTilePriorities() does much more
    than it used to, and also adds new tilings etc, which can cause a sync
    back to the other twin and re-entrancy and bad things.
    
    Instead, split everything that isn't updating tiles priorities off to
    UpdateTiles() which calls UpdateTilePriorities(), and have
    UpdateDrawProperties() call that instead. This way syncing only updates
    priorities, and doesn't do things like adding new tilings or changing
    what is considered high res.
    
    R=enne
    BUG=386076
    
    Review URL: https://codereview.chromium.org/343653004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278604 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/test/fake_picture_layer_impl.h
+++ cc/test/fake_picture_layer_impl.h
@@ -51,7 +51,6 @@ class FakePictureLayerImpl : public PictureLayerImpl {
 
   using PictureLayerImpl::UpdateIdealScales;
   using PictureLayerImpl::MaximumTilingContentsScale;
-  using PictureLayerImpl::ManageTilings;
 
   void SetNeedsPostCommitInitialization() {
     needs_post_commit_initialization_ = true;
@@ -94,15 +93,6 @@ class FakePictureLayerImpl : public PictureLayerImpl {
   void SetAllTilesReady();
   void SetAllTilesReadyInTiling(PictureLayerTiling* tiling);
   void ResetAllTilesPriorities();
-
-  void ScaleAndManageTilings(bool animating_transform_to_screen,
-                             float maximum_animation_contents_scale) {
-    if (CanHaveTilings()) {
-      UpdateIdealScales();
-      ManageTilings(animating_transform_to_screen,
-                    maximum_animation_contents_scale);
-    }
-  }
 
  protected:
   FakePictureLayerImpl(
