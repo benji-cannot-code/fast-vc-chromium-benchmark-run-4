1f024c283de875c03fac43e50fc19cf7dddb2180
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-20 06:49:31 +0000

    Android: Optimize some PNG files in chrome/android/java/res.
    Result: 283628 => 128506 bytes (155122 bytes: 54%)
    
    R=newt@chromium.org
    
    Review URL: https://codereview.chromium.org/334053007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278635 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/resources/optimize-png-files.sh
+++ tools/resources/optimize-png-files.sh
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 readonly ALL_DIRS="
 ash/resources
+chrome/android/java/res
 chrome/app/theme
 chrome/browser/resources
 chrome/renderer/resources
