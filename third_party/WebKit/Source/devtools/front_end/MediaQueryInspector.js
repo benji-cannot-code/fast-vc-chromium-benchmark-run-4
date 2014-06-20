df36c2f79cc8e198154fd0661da7fabeaf1a2f16
lushnikov@chromium.org <lushnikov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-20 18:30:06 +0000

    DevTools: [Media Inspector] do not show bottom border if inspector is empty
    This patch removes one-pixel bottom border in media inspector which
    appears to be excessive in case of empty media inspector.
    
    R=pfeldman@chromium.org, dgozman, pfeldman
    
    Review URL: https://codereview.chromium.org/339323003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176632 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/MediaQueryInspector.js
+++ third_party/WebKit/Source/devtools/front_end/MediaQueryInspector.js
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 WebInspector.MediaQueryInspector = function()
 {
     WebInspector.View.call(this);
-    this.element.classList.add("media-inspector-view");
+    this.element.classList.add("media-inspector-view", "media-inspector-view-empty");
     this.element.addEventListener("click", this._onMediaQueryClicked.bind(this), false);
     this.element.addEventListener("contextmenu", this._onContextMenu.bind(this), false);
     this._mediaThrottler = new WebInspector.Throttler(100);
@@ -300,6 +300,7 @@ WebInspector.MediaQueryInspector.prototype = {
             this.element.appendChild(bar);
         }
         this.element.scrollTop = scrollTop;
+        this.element.classList.toggle("media-inspector-view-empty", !this.element.children.length);
         if (heightChanges)
             this.dispatchEventToListeners(WebInspector.MediaQueryInspector.Events.HeightUpdated);
     },
