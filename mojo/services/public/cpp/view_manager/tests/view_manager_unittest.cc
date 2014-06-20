be0b62376f1a0f32d4f8f7c0de803051667b3ae6
qsr@chromium.org <qsr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-20 09:48:11 +0000

    Fix mojo build on android.
    Android stl library has bugs related to std::set ->
    https://code.google.com/p/android/issues/detail?id=69153
    
    This change allow this code to compile on android.
    
    R=viettrungluu@chromium.org
    
    Review URL: https://codereview.chromium.org/335903004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278657 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/services/public/cpp/view_manager/tests/view_manager_unittest.cc
+++ mojo/services/public/cpp/view_manager/tests/view_manager_unittest.cc
@@ -200,7 +200,7 @@ class DestructionObserver : public NodeObserver, public ViewObserver {
       NodeObserver::DispositionChangePhase phase) OVERRIDE {
     if (phase != NodeObserver::DISPOSITION_CHANGED)
       return;
-    std::set<Id>::const_iterator it = nodes_->find(node->id());
+    std::set<Id>::iterator it = nodes_->find(node->id());
     if (it != nodes_->end())
       nodes_->erase(it);
     if (CanQuit())
@@ -213,7 +213,7 @@ class DestructionObserver : public NodeObserver, public ViewObserver {
       ViewObserver::DispositionChangePhase phase) OVERRIDE {
     if (phase != ViewObserver::DISPOSITION_CHANGED)
       return;
-    std::set<Id>::const_iterator it = views_->find(view->id());
+    std::set<Id>::iterator it = views_->find(view->id());
     if (it != views_->end())
       views_->erase(it);
     if (CanQuit())
