e40d64f3821f1841c6fd3fa1241e17ad8994904b
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-20 21:43:10 +0000

    Define a bootstrap sandbox policy for renderer processes and enable it.
    BUG=367863
    R=avi@chromium.org, mark@chromium.org
    
    Review URL: https://codereview.chromium.org/341073005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278831 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -171,6 +171,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/win/dpi.h"
 #endif
 
+#if defined(OS_MACOSX)
+#include "content/public/common/sandbox_type_mac.h"
+#endif
+
 #if defined(ENABLE_WEBRTC)
 #include "content/browser/media/webrtc_internals.h"
 #include "content/browser/renderer_host/media/media_stream_track_metrics_host.h"
@@ -330,6 +334,11 @@ class RendererSandboxedProcessLauncherDelegate
   virtual int GetIpcFd() OVERRIDE {
     return ipc_fd_;
   }
+#if defined(OS_MACOSX)
+  virtual SandboxType GetSandboxType() OVERRIDE {
+    return SANDBOX_TYPE_RENDERER;
+  }
+#endif
 #endif  // OS_WIN
 
  private:
