deaa36860ecce2747ae6267ef7db937e3b29c49b
pmonette@google.com <pmonette@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-20 09:46:52 +0000

    Include loaded modules in safe browsing client incident reports.
    This includes the sanitized paths of all loaded modules and an
    indication for those that host active LSPs.
    
    BUG=386156
    
    Review URL: https://codereview.chromium.org/323953002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278655 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/safe_browsing/environment_data_collection_win.h
+++ chrome/browser/safe_browsing/environment_data_collection_win.h
@@ -8,7 +8,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace safe_browsing {
 
-// TODO(pmonette): put DLL and LSP collection declarations here.
+class ClientIncidentReport_EnvironmentData_Process;
+
+// Collects then populates |process| with the sanitized paths of all DLLs
+// loaded in the current process. Return false if an error occurred while
+// querying for the loaded dlls.
+bool CollectDlls(ClientIncidentReport_EnvironmentData_Process* process);
+
+// For each of the dlls in this already populated incident report,
+// check one of them is a registered LSP.
+void RecordLspFeature(ClientIncidentReport_EnvironmentData_Process* process);
 
 }  // namespace safe_browsing
 
