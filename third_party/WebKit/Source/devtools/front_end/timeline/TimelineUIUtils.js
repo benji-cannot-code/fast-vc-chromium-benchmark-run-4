387bc6f125fc4735ec7bc016f50220521d355442
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-20 08:33:42 +0000

    Move list of hidden record types into TimelineUIUtils
    Drive-by: changed type enum used in TracingTimelineModel.js to the one describing tracing events.
    
    BUG=361045
    R=loislo@chromium.org
    
    Review URL: https://codereview.chromium.org/348003002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176598 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/timeline/TimelineUIUtils.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TimelineUIUtils.js
@@ -137,6 +137,13 @@ WebInspector.TimelineUIUtils.prototype = {
     testContentMatching: function(record, regExp)
     {
         throw new Error("Not implemented.");
+    },
+    /**
+     * @return {!Object.<string, boolean>}
+     */
+    hiddenRecordTypes: function()
+    {
+        throw new Error("Not implemented.");
     }
 }
 
