f6bf83aa3d58caf581bdd328d5091e3548a24b8f
jamesr@chromium.org <jamesr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-20 19:11:25 +0000

    Revert of Notify the browser when the page lost interest about gamepads (https://codereview.chromium.org/345013002/)
    Reason for revert:
    Causing content_browsertest crashes on all platforms
    
    Original issue's description:
    > Notify the browser when the page lost interest about gamepads
    > 
    > The page visibility logic used in NavigatorGamepad via DeviceEventControllerBase
    > assumes that we only want to tell the dispatcher that we are uninterested in
    > events if we have an event listener. This is not perfect for gamepad. If the
    > page doesn't add event listener for gamepad events it can still poll the data
    > via navigator.getGamepads() at which point we tell the browser that we need
    > gamepad data. We also want to tell the browser if we are not interested so that
    > it can suspend the polling thread.
    > Long story short, we shouldn't check m_hasEventListener in
    > pageVisibilityChanged. This CL doesn't change visible behavior thus no tests.
    > Visibility related tests should be added for gamepad though, I will do that
    > later.
    > 
    > BUG=386846
    > 
    > R=bajones@chromium.org
    > TBR=abarth@chromium.org
    > 
    > NOTRY=true
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=176618
    
    TBR=abarth@chromium.org,bajones@chromium.org,b.kelemen@samsung.com
    NOTREECHECKS=true
    NOTRY=true
    BUG=386846
    
    Review URL: https://codereview.chromium.org/348083003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176635 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/DeviceEventControllerBase.h
+++ third_party/WebKit/Source/core/frame/DeviceEventControllerBase.h
@@ -37,7 +37,7 @@ protected:
 
 private:
     // Inherited from PageLifecycleObserver.
-    virtual void pageVisibilityChanged() OVERRIDE;
+    virtual void pageVisibilityChanged() OVERRIDE FINAL;
 
     void oneShotCallback(Timer<DeviceEventControllerBase>*);
 
