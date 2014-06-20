28cc1b91431e0ed813c9523ab2c966f796a2ff6a
epenner@chromium.org <epenner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-20 22:33:25 +0000

    Telemetry: Set scaling governor on all cpus (on all devices)
    We try to set the scaling governor for all CPU cores, but
    this doesn't work SOCs that dynamically disable cores
    entirely (ie. Qualcomm). Since this only happens on Qcomm,
    this patch stops the 'mpdecision' process on Qualcomm, which
    makes it behave like other devices.
    
    PERF-SHERIFFS: This may improve metrics for the better given
    all cores are in 'performance' mode now. It should hopefully
    reduce noise, since a different number of cores might have
    had 'performance' set on each run, and any code that runs on
    a non-performance core will suffer from noisy timings/rates.
    
    Example: Tough-compositor-cases std-dev went from 0.25ms to
    0.04ms with this change.
    
    BUG=383566
    
    Review URL: https://codereview.chromium.org/338233003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278837 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/platform/profiler/perf_profiler.py
+++ tools/telemetry/telemetry/core/platform/profiler/perf_profiler.py
@@ -173,7 +173,7 @@ class PerfProfiler(profiler.Profiler):
         device = browser_backend.adb.device()
         perf_binary = android_profiling_helper.PrepareDeviceForPerf(device)
         self._perf_control = perf_control.PerfControl(device)
-        self._perf_control.ForceAllCpusOnline(True)
+        self._perf_control.SetPerfProfilingMode()
       else:
         _PrepareHostForPerf()
 
@@ -186,7 +186,7 @@ class PerfProfiler(profiler.Profiler):
                 perf_binary, perfhost_binary))
     except:
       if self._is_android:
-        self._perf_control.ForceAllCpusOnline(False)
+        self._perf_control.SetDefaultPerfMode()
       raise
 
   @classmethod
@@ -210,7 +210,7 @@ class PerfProfiler(profiler.Profiler):
 
   def CollectProfile(self):
     if self._is_android:
-      self._perf_control.ForceAllCpusOnline(False)
+      self._perf_control.SetDefaultPerfMode()
     output_files = []
     for single_process in self._process_profilers:
       output_files.append(single_process.CollectProfile())
