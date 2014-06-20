a63f7908682fc470f2b32d8256e72ab03f44dc84
jamesr@chromium.org <jamesr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-20 22:36:59 +0000

    Revert of Accelerate small canvases if Ganesh is on. (https://codereview.chromium.org/326283002/)
    Reason for revert:
    Need to revert the blink side and this (incorrectly) adds an OVERRIDE for a cross-repository override.
    
    Original issue's description:
    > Accelerate small canvases if Ganesh is on.
    >
    > cc side of change.
    >
    > TBR=phajdan.jr@chromium.org
    > BUG=382738
    >
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=276753
    
    TBR=ajuma@chromium.org,kbr@chromium.org,phajdan.jr@chromium.org,chrishtr@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=382738
    
    Review URL: https://codereview.chromium.org/349733002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278842 0039d316-1c4b-4281-b951-d872f2087c98

--- content/test/web_layer_tree_view_impl_for_testing.cc
+++ content/test/web_layer_tree_view_impl_for_testing.cc
@@ -168,8 +168,4 @@ void WebLayerTreeViewImplForTesting::clearViewportLayers() {
                                            scoped_refptr<cc::Layer>());
 }
 
-bool WebLayerTreeViewImplForTesting::usesGpuRasterization() {
-  return false;
-}
-
 }  // namespace content
