923ad4245f9be19494cafb127bed1ca8bf5054f4
erik.corry@gmail.com <erik.corry@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-20 21:20:06 +0000

    Fix compile error in some MSVC versions in Vector.h
    R=sigbjornf@opera.com
    BUG=
    
    Review URL: https://codereview.chromium.org/348833002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176645 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/webdatabase/DatabaseManager.h
+++ third_party/WebKit/Source/modules/webdatabase/DatabaseManager.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define DatabaseManager_h
 
 #include "modules/webdatabase/DatabaseBasicTypes.h"
+#include "modules/webdatabase/DatabaseContext.h"
 #include "modules/webdatabase/DatabaseError.h"
 #include "platform/heap/Handle.h"
 #include "wtf/Assertions.h"
