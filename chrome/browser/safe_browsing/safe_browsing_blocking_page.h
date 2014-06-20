1a1f158dbbfc4c8f24c0c0eff07a6f9442ea5091
felt@chromium.org <felt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-20 05:33:07 +0000

    Add experimental versions of the malware interstitial
    This adds a few experimental versions of the new malware interstitial. They are all teh addition of an extra string.
    
    1. Prior history
    2. "(not recommended)" next to link
    3. Advice
    4. Social proof
    
    BUG=386033
    
    Review URL: https://codereview.chromium.org/346733003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278618 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/safe_browsing/safe_browsing_blocking_page.h
+++ chrome/browser/safe_browsing/safe_browsing_blocking_page.h
@@ -282,9 +282,6 @@ class SafeBrowsingBlockingPageV2 : public SafeBrowsingBlockingPage {
                                 const base::string16& description2,
                                 const base::string16& description3);
 
-  // For the FieldTrial: this contains the name of the condition.
-  std::string trialCondition_;
-
   DISALLOW_COPY_AND_ASSIGN(SafeBrowsingBlockingPageV2);
 };
 
@@ -302,6 +299,9 @@ class SafeBrowsingBlockingPageV3 : public SafeBrowsingBlockingPage {
   // when creating the HTML.
   void PopulateMalwareLoadTimeData(base::DictionaryValue* load_time_data);
   void PopulatePhishingLoadTimeData(base::DictionaryValue* load_time_data);
+
+  // For the M37 FieldTrial: this contains the name of the condition.
+  std::string trial_condition_;
 
   DISALLOW_COPY_AND_ASSIGN(SafeBrowsingBlockingPageV3);
 };
