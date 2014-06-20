7c95ae1911bf7b7e3758060067324f3e577a810b
reillyg@chromium.org <reillyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-20 12:54:24 +0000

    Descriptive log for HID device open failure on Linux.
    Log a human readable message describing the reason why Chromium failed
    to open a HID device node on Linux instead of the base::File error code.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/339503007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278684 0039d316-1c4b-4281-b951-d872f2087c98

--- device/hid/hid_service_linux.cc
+++ device/hid/hid_service_linux.cc
@@ -115,7 +115,7 @@ void HidServiceLinux::OnDeviceAdded(udev_device* device) {
 
   std::string dev_node;
   if (!FindHidrawDevNode(device, &dev_node)) {
-    LOG(ERROR) << "Cannot open HID device as hidraw device.";
+    LOG(ERROR) << "Cannot find device node for HID device.";
     return;
   }
 
@@ -123,7 +123,8 @@ void HidServiceLinux::OnDeviceAdded(udev_device* device) {
 
   base::File device_file(base::FilePath(dev_node), flags);
   if (!device_file.IsValid()) {
-    LOG(ERROR) << device_file.error_details();
+    LOG(ERROR) << "Cannot open '" << dev_node << "': "
+        << base::File::ErrorToString(device_file.error_details());
     return;
   }
 
