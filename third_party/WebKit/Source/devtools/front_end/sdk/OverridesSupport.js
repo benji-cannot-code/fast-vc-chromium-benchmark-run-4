f73f112c44ae67d8ca2b3bdf27097b7270a64117
dgozman@chromium.org <dgozman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-20 14:32:17 +0000

    [DevTools] Disable touch emulation while inspect mode is on.
    BUG=none
    R=pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/345063002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176610 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sdk/OverridesSupport.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/OverridesSupport.js
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 WebInspector.OverridesSupport = function(responsiveDesignAvailable)
 {
     WebInspector.resourceTreeModel.addEventListener(WebInspector.ResourceTreeModel.EventTypes.MainFrameNavigated, this._onMainFrameNavigated.bind(this), this);
+    this._touchEmulationSuspended = false;
     this._emulateViewportEnabled = false;
     this._userAgent = "";
     this._pageResizer = null;
@@ -554,6 +555,16 @@ WebInspector.OverridesSupport.prototype = {
             && this.settings.emulateViewport.get() === device.viewport;
     },
 
+    /**
+     * @param {boolean} suspended
+     */
+    setTouchEmulationSuspended: function(suspended)
+    {
+        this._touchEmulationSuspended = suspended;
+        if (this._initialized)
+            this._emulateTouchEventsChanged();
+    },
+
     applyInitialOverrides: function()
     {
         if (!this._target) {
@@ -760,7 +771,7 @@ WebInspector.OverridesSupport.prototype = {
 
     _emulateTouchEventsChanged: function()
     {
-        var emulateTouch = this.emulationEnabled() && this.settings.emulateTouch.get();
+        var emulateTouch = this.emulationEnabled() && this.settings.emulateTouch.get() && !this._touchEmulationSuspended;
         var targets = WebInspector.targetManager.targets();
         for (var i = 0; i < targets.length; ++i)
             targets[i].domModel.emulateTouchEventObjects(emulateTouch);
