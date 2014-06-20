79db7ff9aed403f4d2a8b0f7509a4e3b518e4700
eustas@chromium.org <eustas@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-20 12:32:38 +0000

    DevTools: support different SPDY versions in HAR export.
    BUG=350113
    
    Review URL: https://codereview.chromium.org/344143004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176605 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/network/HAREntry.js
+++ third_party/WebKit/Source/devtools/front_end/network/HAREntry.js
@@ -98,7 +98,7 @@ WebInspector.HAREntry.prototype = {
         return {
             status: this._request.statusCode,
             statusText: this._request.statusText,
-            httpVersion: this._request.responseHttpVersion,
+            httpVersion: this._request.responseHttpVersion(),
             headers: this._request.responseHeaders,
             cookies: this._buildCookies(this._request.responseCookies || []),
             content: this._buildContent(),
