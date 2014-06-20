f03af4689f13b4f2eaecc86ff381d5b20790b3dd
zmo@chromium.org <zmo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-20 13:56:48 +0000

    Add semantics to apply gpu blakclist to AMD swichable when a discrete/integrated GPU is in use
    BUG=376562
    TEST=gpu_unittests
    R=kbr@chromium.org
    
    Review URL: https://codereview.chromium.org/338183005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278703 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/config/gpu_control_list.h
+++ gpu/config/gpu_control_list.h
@@ -325,6 +325,8 @@ class GPU_EXPORT GpuControlList {
     enum MultiGpuStyle {
       kMultiGpuStyleOptimus,
       kMultiGpuStyleAMDSwitchable,
+      kMultiGpuStyleAMDSwitchableIntegrated,
+      kMultiGpuStyleAMDSwitchableDiscrete,
       kMultiGpuStyleNone
     };
 
--- gpu/config/software_rendering_list_json.cc
+++ gpu/config/software_rendering_list_json.cc
@@ -19,7 +19,7 @@ const char kSoftwareRenderingListJson[] = LONG_STRING_CONST(
 {
   "name": "software rendering list",
   // Please update the version number whenever you change this file.
-  "version": "8.5",
+  "version": "8.6",
   "entries": [
     {
       "id": 1,
@@ -1093,7 +1093,7 @@ LONG_STRING_CONST(
       "os": {
         "type": "win"
       },
-      "multi_gpu_style": "amd_switchable",
+      "multi_gpu_style": "amd_switchable_discrete",
       "features": [
         "accelerated_video_decode"
       ]
