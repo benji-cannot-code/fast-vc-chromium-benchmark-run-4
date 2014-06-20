8db9c001ee77b5b642f8afd1a5af2a402abf1d38
vollick@chromium.org <vollick@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-20 07:49:56 +0000

    CompositingInputsUpdater should compute scrollParent
    Instead of walking up the tree to find the scroll parent 
    for every RenderLayer when computing the compositing
    requirements, we now compute it while walking down the tree
    for only the RenderLayers with dirty compositing state.
    
    Note: This CL is a minor tweak of abarth's patch
    (https://codereview.chromium.org/340303004). It adds a
    small walk to ensure that there is no stacking context
    between the layer and the scroller.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/344023002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176595 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderLayer.h
+++ third_party/WebKit/Source/core/rendering/RenderLayer.h
@@ -333,7 +333,13 @@ public:
 
     bool clipsCompositingDescendantsWithBorderRadius() const;
 
-    RenderLayer* scrollParent() const;
+    RenderLayer* scrollParent() const
+    {
+        if (stackingNode()->isNormalFlowOnly())
+            return 0;
+        return const_cast<RenderLayer*>(compositingInputs().inheritedScrollParent);
+    }
+
     RenderLayer* clipParent() const;
 
     // Computes the position of the given render object in the space of |repaintContainer|.
@@ -449,6 +455,7 @@ public:
             : opacityAncestor(0)
             , transformAncestor(0)
             , filterAncestor(0)
+            , inheritedScrollParent(0)
             , isUnclippedDescendant(false)
         { }
 
@@ -456,6 +463,7 @@ public:
         const RenderLayer* opacityAncestor;
         const RenderLayer* transformAncestor;
         const RenderLayer* filterAncestor;
+        const RenderLayer* inheritedScrollParent;
         unsigned isUnclippedDescendant : 1;
     };
 
