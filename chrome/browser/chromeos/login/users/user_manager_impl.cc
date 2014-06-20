a1965711430402dd7059e1bcb4fe78e4da372104
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-20 00:35:44 +0000

    [multi-profiles] Allow bringing up sign in UI even if all users are restricted per their policies
    * Don't allow launching GAIA sign in for user pod that is shown with multi-profile policy restriction
    * Always show policy bubble when hovering over user pod / clicking/tapping on it.
    
    BUG=385498
    
    Review URL: https://codereview.chromium.org/341453002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278547 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/users/user_manager_impl.cc
+++ chrome/browser/chromeos/login/users/user_manager_impl.cc
@@ -325,7 +325,6 @@ UserList UserManagerImpl::GetUsersAdmittedForMultiProfile() const {
   }
 
   UserList result;
-  int num_users_allowed = 0;
   const UserList& users = GetUsers();
   for (UserList::const_iterator it = users.begin(); it != users.end(); ++it) {
     if ((*it)->GetType() == User::USER_TYPE_REGULAR && !(*it)->is_logged_in()) {
@@ -344,16 +343,9 @@ UserList UserManagerImpl::GetUsersAdmittedForMultiProfile() const {
           check == MultiProfileUserController::NOT_ALLOWED_POLICY_FORBIDS ||
           check == MultiProfileUserController::NOT_ALLOWED_OWNER_AS_SECONDARY) {
         result.push_back(*it);
-        if (check == MultiProfileUserController::ALLOWED)
-          num_users_allowed++;
       }
     }
   }
-
-  // We only show multi-profiles sign in UI if there's at least one user that
-  // is allowed to be added to the session.
-  if (!num_users_allowed)
-    result.clear();
 
   return result;
 }
