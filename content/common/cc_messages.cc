d67ec923814e216d687defd77b73b990c6ea94ba
sungmann.cho@navercorp.com <sungmann.cho@navercorp.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-21 05:45:44 +0000

    Remove unnecessary #includes in /content/common/cc_messages.cc.
    After the landing https://codereview.chromium.org/303143002,
    cc_messages.cc doesn't need the command line switch related
    headers anymore. So we can remove these.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/334673003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278935 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/cc_messages.cc
+++ content/common/cc_messages.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/output/compositor_frame.h"
 #include "cc/output/filter_operations.h"
 #include "content/public/common/common_param_traits.h"
-#include "content/public/common/content_switches.h"
 #include "third_party/skia/include/core/SkData.h"
 #include "third_party/skia/include/core/SkFlattenableSerialization.h"
 #include "ui/gfx/transform.h"
