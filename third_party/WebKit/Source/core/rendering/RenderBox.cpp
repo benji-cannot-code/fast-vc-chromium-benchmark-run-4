de76893e935c30debbf1007f1c94258c98559567
wangxianzhu@chromium.org <wangxianzhu@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-21 02:00:57 +0000

    Rename InvalidationSelfLayout to InvalidationFull
    Previously selfNeedsLayout implies full invalidation, but it has been
    not true after we separated layout and invalidation requirements.
    
    Review URL: https://codereview.chromium.org/348073002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176669 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderBox.cpp
+++ third_party/WebKit/Source/core/rendering/RenderBox.cpp
@@ -1602,7 +1602,8 @@ void RenderBox::invalidateTreeAfterLayout(const RenderLayerModelObject& paintInv
     const LayoutRect& newPaintInvalidationRect = previousPaintInvalidationRect();
     const LayoutPoint& newPositionFromPaintInvalidationContainer = previousPositionFromPaintInvalidationContainer();
     bool didFullPaintInvalidation = invalidatePaintAfterLayoutIfNeeded(&newPaintInvalidationContainer,
-        shouldDoFullPaintInvalidationAfterLayout(), oldPaintInvalidationRect, oldPositionFromPaintInvalidationContainer,
+        shouldDoFullPaintInvalidationAfterLayout() ? InvalidationFull : InvalidationIncremental,
+        oldPaintInvalidationRect, oldPositionFromPaintInvalidationContainer,
         &newPaintInvalidationRect, &newPositionFromPaintInvalidationContainer);
 
     if (!didFullPaintInvalidation)
