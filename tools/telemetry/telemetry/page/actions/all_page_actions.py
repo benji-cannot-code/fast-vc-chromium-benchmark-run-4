740ddefa004ce181acd8f999f80ffbd881fa5465
chrishenry@google.com <chrishenry@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-21 05:53:11 +0000

    Kill dead code in all_page_actions.py
    BUG=361809
    
    Review URL: https://codereview.chromium.org/349683005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278938 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/page/actions/all_page_actions.py
+++ tools/telemetry/telemetry/page/actions/all_page_actions.py
@@ -2,11 +2,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-import os
-
-from telemetry.core import discover
-from telemetry.core import util
-from telemetry.page.actions import page_action
 
 # pylint: disable=W0611
 # TODO(nednguyen): Remove all of these imports when we done porting all actions
@@ -23,13 +18,3 @@ from telemetry.page.actions.scroll import ScrollAction
 from telemetry.page.actions.scroll_bounce import ScrollBounceAction
 from telemetry.page.actions.seek import SeekAction
 from telemetry.page.actions.swipe import SwipeAction
-
-
-_page_action_classes = discover.DiscoverClasses(
-    os.path.dirname(__file__), util.GetTelemetryDir(), page_action.PageAction)
-
-def GetAllClasses():
-  return list(_page_action_classes.values())
-
-def FindClassWithName(name):
-  return _page_action_classes.get(name)
