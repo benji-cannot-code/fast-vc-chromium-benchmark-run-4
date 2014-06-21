ceaa317841163fa40093c5bbd1bc354abd779fd8
chrishenry@google.com <chrishenry@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-21 05:35:35 +0000

    NavigateAction no longer uses attributes-style params and instead use normal Python params.
    Cleanup:
    * As requested, s/timeout/timeout_in_seconds/
    * SwipeAction/TapAction no longer pass attributes to superclass.
    
    BUG=361809
    
    Review URL: https://codereview.chromium.org/339773004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278930 0039d316-1c4b-4281-b951-d872f2087c98

--- content/test/gpu/gpu_tests/webgl_conformance.py
+++ content/test/gpu/gpu_tests/webgl_conformance.py
@@ -98,7 +98,7 @@ class WebglConformancePage(page_module.Page):
   def RunNavigateSteps(self, action_runner):
     action_runner.NavigateToPage(self)
     action_runner.WaitForJavaScriptCondition(
-        'webglTestHarness._finished', timeout=120)
+        'webglTestHarness._finished', timeout_in_seconds=120)
 
 
 class WebglConformance(test_module.Test):
