78a4dea7d7e0cea0bba64fe48e76b7fd6c5d77f5
plundblad@chromium.org <plundblad@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-21 05:26:37 +0000

    Update latest ChromeVox from upstream
    Change log for this version:
      Support backspace and other keys emulated by a braille display.
      Fixes to routing key clicking with a braille display.
      Give ChromeVox a speech queue.
    
    BUG=310285
    TBR=dtseng@chromium.org
    
    Review URL: https://codereview.chromium.org/346813006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278923 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/chromevox/common/nav_description.js
+++ chrome/browser/resources/chromeos/chromevox/common/nav_description.js
@@ -25,7 +25,8 @@ goog.require('cvox.CursorSelection');
  *          annotation: (undefined|string),
  *          earcons: (undefined|Array.<number>),
  *          personality: (undefined|Object),
- *          hint: (undefined|string)}} kwargs The arguments for this
+ *          hint: (undefined|string),
+            category: (undefined|string)}} kwargs The arguments for this
  *  description.
  *  context The context, for example descriptions of objects
  *     that were crossed into, like "Toolbar" or "Menu Bar" or "List with
@@ -38,6 +39,7 @@ goog.require('cvox.CursorSelection');
  *     with the spoken description of this object.
  *  personality Optional TTS personality to use for the text.
  *  hint Optional Hint text (.e.g. aria-describedby).
+ *  category Optional category (for speech queueing behavior).
  * @constructor
  */
 cvox.NavDescription = function(kwargs) {
@@ -48,6 +50,7 @@ cvox.NavDescription = function(kwargs) {
   this.earcons = kwargs.earcons ? kwargs.earcons : [];
   this.personality = kwargs.personality;
   this.hint = kwargs.hint ? kwargs.hint : '';
+  this.category = kwargs.category ? kwargs.category : null;
 };
 
 
@@ -70,7 +73,9 @@ cvox.NavDescription.prototype.toString = function() {
   return 'NavDescription(context="' + this.context + '" ' +
          ' text="' + this.text + '" ' +
          ' userValue="' + this.userValue + '" ' +
-         ' annotation="' + this.annotation + '")';
+         ' annotation="' + this.annotation +
+         (this.category ? '" category="' + this.category + '")' : '') +
+         '")';
 };
 
 
@@ -136,6 +141,9 @@ cvox.NavDescription.prototype.speak = function(
     }
     if (i == length - 1 && endCallback) {
       speakArgs[i][2]['endCallback'] = endCallback;
+    }
+    if (this.category) {
+      speakArgs[i][2]['category'] = this.category;
     }
     cvox.ChromeVox.tts.speak.apply(cvox.ChromeVox.tts, speakArgs[i]);
   }
