72d81fbd67302c181ddb145a368642221305f9ad
jamesr@chromium.org <jamesr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-21 02:36:53 +0000

    Add support for fixed-sized arrays in mojom
    This adds support for the syntax:
    
      typename[20] name;
    
    in mojom files to specify an array of 'typename' containing exactly 20
    entries. The serialization is the same as 'typename[] name' and the length
    of the array is checked at message validation time.
    
    This implementation does not support nested fixed arrays as in:
    
      typename[3][4] name;
    
    but does handle fixed size arrays inside structs, etc.
    
    Review URL: https://codereview.chromium.org/340663002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278902 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/public/tools/bindings/generators/mojom_cpp_generator.py
+++ mojo/public/tools/bindings/generators/mojom_cpp_generator.py
@@ -55,7 +55,7 @@ def GetNameForKind(kind, internal = False):
 def GetCppType(kind):
   if isinstance(kind, mojom.Struct):
     return "%s_Data*" % GetNameForKind(kind, internal=True)
-  if isinstance(kind, mojom.Array):
+  if isinstance(kind, (mojom.Array, mojom.FixedArray)):
     return "mojo::internal::Array_Data<%s>*" % GetCppType(kind.kind)
   if isinstance(kind, mojom.Interface) or \
      isinstance(kind, mojom.InterfaceRequest):
@@ -76,7 +76,7 @@ def GetCppArrayArgWrapperType(kind):
     return GetNameForKind(kind)
   if isinstance(kind, mojom.Struct):
     return "%sPtr" % GetNameForKind(kind)
-  if isinstance(kind, mojom.Array):
+  if isinstance(kind, (mojom.Array, mojom.FixedArray)):
     return "mojo::Array<%s> " % GetCppArrayArgWrapperType(kind.kind)
   if isinstance(kind, mojom.Interface):
     raise Exception("Arrays of interfaces not yet supported!")
@@ -101,7 +101,7 @@ def GetCppResultWrapperType(kind):
     return GetNameForKind(kind)
   if isinstance(kind, mojom.Struct):
     return "%sPtr" % GetNameForKind(kind)
-  if isinstance(kind, mojom.Array):
+  if isinstance(kind, (mojom.Array, mojom.FixedArray)):
     return "mojo::Array<%s>" % GetCppArrayArgWrapperType(kind.kind)
   if isinstance(kind, mojom.Interface):
     return "%sPtr" % GetNameForKind(kind)
@@ -126,7 +126,7 @@ def GetCppWrapperType(kind):
     return GetNameForKind(kind)
   if isinstance(kind, mojom.Struct):
     return "%sPtr" % GetNameForKind(kind)
-  if isinstance(kind, mojom.Array):
+  if isinstance(kind, (mojom.Array, mojom.FixedArray)):
     return "mojo::Array<%s>" % GetCppArrayArgWrapperType(kind.kind)
   if isinstance(kind, mojom.Interface):
     return "mojo::ScopedMessagePipeHandle"
@@ -149,7 +149,7 @@ def GetCppWrapperType(kind):
 def GetCppConstWrapperType(kind):
   if isinstance(kind, mojom.Struct):
     return "%sPtr" % GetNameForKind(kind)
-  if isinstance(kind, mojom.Array):
+  if isinstance(kind, (mojom.Array, mojom.FixedArray)):
     return "mojo::Array<%s>" % GetCppArrayArgWrapperType(kind.kind)
   if isinstance(kind, mojom.Interface):
     return "%sPtr" % GetNameForKind(kind)
@@ -177,7 +177,7 @@ def GetCppFieldType(kind):
   if isinstance(kind, mojom.Struct):
     return ("mojo::internal::StructPointer<%s_Data>" %
         GetNameForKind(kind, internal=True))
-  if isinstance(kind, mojom.Array):
+  if isinstance(kind, (mojom.Array, mojom.FixedArray)):
     return "mojo::internal::ArrayPointer<%s>" % GetCppType(kind.kind)
   if isinstance(kind, mojom.Interface) or \
      isinstance(kind, mojom.InterfaceRequest):
@@ -237,11 +237,13 @@ class Generator(generator.Generator):
     "cpp_type": GetCppType,
     "cpp_wrapper_type": GetCppWrapperType,
     "default_value": DefaultValue,
+    "expected_array_size": generator.ExpectedArraySize,
     "expression_to_text": ExpressionToText,
     "get_name_for_kind": GetNameForKind,
     "get_pad": pack.GetPad,
     "has_callbacks": HasCallbacks,
     "should_inline": ShouldInlineStruct,
+    "is_array_kind": generator.IsArrayKind,
     "is_enum_kind": generator.IsEnumKind,
     "is_move_only_kind": generator.IsMoveOnlyKind,
     "is_handle_kind": generator.IsHandleKind,
