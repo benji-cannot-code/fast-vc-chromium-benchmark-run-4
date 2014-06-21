ff14e300fc2b3760c2d85a58a5e7040910da49ad
skuhne@chromium.org <skuhne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-21 13:52:12 +0000

    Fixing tab-ability of multi user items in system tray menu.
    BUG=364652
    TEST=visual (Tab in, tab out, Tab in, activate, add user, tabbable, ..)
    
    Review URL: https://codereview.chromium.org/339783003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278972 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/user/button_from_view.h
+++ ash/system/user/button_from_view.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_SYSTEM_USER_BUTTON_FROM_VIEW_H_
 
 #include "base/macros.h"
-
+#include "ui/gfx/insets.h"
 #include "ui/views/controls/button/custom_button.h"
 
 namespace ash {
@@ -16,9 +16,16 @@ namespace tray {
 // This view is used to wrap it's content and transform it into button.
 class ButtonFromView : public views::CustomButton {
  public:
+  // The |content| is the content which is shown within the button. The
+  // |button_listener| will be informed - if provided - when a button was
+  // pressed. If |highlight_on_hover| is set to true, the button will be
+  // highlighted upon hover and show the accessibility caret.
+  // The |tab_frame_inset| will be used to inset the blue tab frame inside the
+  // button.
   ButtonFromView(views::View* content,
                  views::ButtonListener* listener,
-                 bool highlight_on_hover);
+                 bool highlight_on_hover,
+                 const gfx::Insets& tab_frame_inset);
   virtual ~ButtonFromView();
 
   // Called when the border should remain even in the non highlighted state.
@@ -27,6 +34,9 @@ class ButtonFromView : public views::CustomButton {
   // Overridden from views::View
   virtual void OnMouseEntered(const ui::MouseEvent& event) OVERRIDE;
   virtual void OnMouseExited(const ui::MouseEvent& event) OVERRIDE;
+  virtual void OnPaint(gfx::Canvas* canvas) OVERRIDE;
+  virtual void OnFocus() OVERRIDE;
+  virtual void OnBlur() OVERRIDE;
 
   // Check if the item is hovered.
   bool is_hovered_for_test() { return button_hovered_; }
@@ -46,6 +56,9 @@ class ButtonFromView : public views::CustomButton {
 
   // True if the border should be always visible.
   bool show_border_;
+
+  // The insets which get used for the drawn accessibility (tab) frame.
+  gfx::Insets tab_frame_inset_;
 
   DISALLOW_COPY_AND_ASSIGN(ButtonFromView);
 };
--- ash/system/user/user_view.h
+++ ash/system/user/user_view.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/user/tray_user.h"
 #include "base/macros.h"
 #include "ui/views/controls/button/button.h"
+#include "ui/views/focus/focus_manager.h"
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/mouse_watcher.h"
 #include "ui/views/view.h"
@@ -31,7 +32,8 @@ namespace tray {
 // The view of a user item in system tray bubble.
 class UserView : public views::View,
                  public views::ButtonListener,
-                 public views::MouseWatcherListener {
+                 public views::MouseWatcherListener,
+                 public views::FocusChangeListener {
  public:
   UserView(SystemTrayItem* owner,
            ash::user::LoginStatus login,
@@ -55,12 +57,21 @@ class UserView : public views::View,
   virtual void ButtonPressed(views::Button* sender,
                              const ui::Event& event) OVERRIDE;
 
+  // Overridden from views::FocusChangeListener:
+  virtual void OnWillChangeFocus(View* focused_before,
+                                 View* focused_now) OVERRIDE;
+  virtual void OnDidChangeFocus(View* focused_before,
+                                View* focused_now) OVERRIDE;
+
   void AddLogoutButton(user::LoginStatus login);
   void AddUserCard(user::LoginStatus login);
 
   // Create the menu option to add another user. If |disabled| is set the user
   // cannot actively click on the item.
   void ToggleAddUserMenuOption();
+
+  // Removes the add user menu option.
+  void RemoveAddUserMenuOption();
 
   MultiProfileIndex multiprofile_index_;
   // The view of the user card.
