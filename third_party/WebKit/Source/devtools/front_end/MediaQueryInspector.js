a6da14bf5557895692f458a08694b60eba88e9ad
lushnikov@chromium.org <lushnikov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-22 22:13:36 +0000

    DevTools: [Media Inspector] Support inspected page scale.
    This patch adds support for inspected page scale. It should be landed
    after the full-featured support of the inspected page scale.
    
    Review URL: https://codereview.chromium.org/332423003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176714 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/MediaQueryInspector.js
+++ third_party/WebKit/Source/devtools/front_end/MediaQueryInspector.js
@@ -14,7 +14,9 @@ WebInspector.MediaQueryInspector = function()
     this.element.addEventListener("click", this._onMediaQueryClicked.bind(this), false);
     this.element.addEventListener("contextmenu", this._onContextMenu.bind(this), false);
     this._mediaThrottler = new WebInspector.Throttler(100);
-    this._zeroOffset = 0;
+
+    this._translateZero = 0;
+    this._scale = 1;
 
     this._rulerDecorationLayer = document.createElementWithClass("div", "fill");
     this._rulerDecorationLayer.classList.add("media-inspector-ruler-decoration");
@@ -41,8 +43,6 @@ WebInspector.MediaQueryInspector.Events = {
     HeightUpdated: "HeightUpdated"
 }
 
-WebInspector.MediaQueryInspector._ThresholdPadding = 1;
-
 WebInspector.MediaQueryInspector.prototype = {
     /**
      * @return {!Element}
@@ -68,12 +68,7 @@ WebInspector.MediaQueryInspector.prototype = {
                 thresholds.push(model.maxWidthExpression().computedLength());
         }
         thresholds.sortNumbers();
-        var filtered = [];
-        for (var i = 0; i < thresholds.length; ++i) {
-            if (i == 0 || thresholds[i] - filtered.peekLast() > WebInspector.MediaQueryInspector._ThresholdPadding)
-                filtered.push(thresholds[i]);
-        }
-        return filtered;
+        return thresholds;
     },
 
     /**
@@ -88,8 +83,8 @@ WebInspector.MediaQueryInspector.prototype = {
         var revealValue = thresholdElement._value;
         for (var mediaQueryContainer = this.element.firstChild; mediaQueryContainer; mediaQueryContainer = mediaQueryContainer.nextSibling) {
             var model = mediaQueryContainer._model;
-            if ((model.minWidthExpression() && Math.abs(model.minWidthExpression().computedLength() - revealValue) <= WebInspector.MediaQueryInspector._ThresholdPadding)
-                || (model.maxWidthExpression() && Math.abs(model.maxWidthExpression().computedLength() - revealValue) <= WebInspector.MediaQueryInspector._ThresholdPadding)) {
+            if ((model.minWidthExpression() && Math.abs(model.minWidthExpression().computedLength() - revealValue) === 0)
+                || (model.maxWidthExpression() && Math.abs(model.maxWidthExpression().computedLength() - revealValue) === 0)) {
                 mediaQueryContainer.scrollIntoViewIfNeeded(false);
                 return;
             }
@@ -97,11 +92,15 @@ WebInspector.MediaQueryInspector.prototype = {
     },
 
     /**
-     * @param {number} offset
+     * @param {number} translate
+     * @param {number} scale
      */
-    translateZero: function(offset)
+    setAxisTransform: function(translate, scale)
     {
-        this._zeroOffset = offset;
+        if (this._translateZero === translate && Math.abs(this._scale - scale) < 1e-8)
+            return;
+        this._translateZero = translate;
+        this._scale = scale;
         this._renderMediaQueries();
     },
 
@@ -305,10 +304,18 @@ WebInspector.MediaQueryInspector.prototype = {
             this.dispatchEventToListeners(WebInspector.MediaQueryInspector.Events.HeightUpdated);
     },
 
+    /**
+     * @return {number}
+     */
+    _zoomFactor: function()
+    {
+        return WebInspector.zoomManager.zoomFactor() / this._scale;
+    },
+
     _renderRulerDecorations: function()
     {
         this._rulerDecorationLayer.removeChildren();
-        var zoomFactor = WebInspector.zoomManager.zoomFactor();
+        var zoomFactor = this._zoomFactor();
 
         var thresholds = this._mediaQueryThresholds();
         for (var i = 0; i < thresholds.length; ++i) {
@@ -329,7 +336,7 @@ WebInspector.MediaQueryInspector.prototype = {
      */
     _createElementFromMediaQueryModel: function(model)
     {
-        var zoomFactor = WebInspector.zoomManager.zoomFactor();
+        var zoomFactor = this._zoomFactor();
         var minWidthValue = model.minWidthExpression() ? model.minWidthExpression().computedLength() : 0;
 
         var container = document.createElementWithClass("div", "media-inspector-marker-container hbox");
@@ -340,13 +347,13 @@ WebInspector.MediaQueryInspector.prototype = {
             "media-inspector-marker-min-width"
         ];
         markerElement.classList.add(styleClassPerSection[model.section()]);
-        var leftPixelValue = minWidthValue ? minWidthValue / zoomFactor + this._zeroOffset : 0;
+        var leftPixelValue = minWidthValue ? minWidthValue / zoomFactor + this._translateZero : 0;
         markerElement.style.left = leftPixelValue + "px";
         var widthPixelValue = null;
         if (model.maxWidthExpression() && model.minWidthExpression())
             widthPixelValue = (model.maxWidthExpression().computedLength() - minWidthValue) / zoomFactor;
         else if (model.maxWidthExpression())
-            widthPixelValue = model.maxWidthExpression().computedLength() / zoomFactor + this._zeroOffset;
+            widthPixelValue = model.maxWidthExpression().computedLength() / zoomFactor + this._translateZero;
         else
             markerElement.style.right = "0";
         if (typeof widthPixelValue === "number")
