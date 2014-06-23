e0dfbede8cfcf22daab4ef774f188957ec4d13c6
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-23 05:11:00 +0000

    DevTools: fix overrides dirty marker wrt emulateResolution bit.
    TBR=dgozman
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/347373002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176721 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/elements/OverridesView.js
+++ third_party/WebKit/Source/devtools/front_end/elements/OverridesView.js
@@ -177,8 +177,7 @@ WebInspector.OverridesView.Tab.prototype = {
 WebInspector.OverridesView.DeviceTab = function()
 {
     WebInspector.OverridesView.Tab.call(this, "device", WebInspector.UIString("Device"),  [
-        WebInspector.overridesSupport.settings.deviceWidth,
-        WebInspector.overridesSupport.settings.deviceHeight,
+        WebInspector.overridesSupport.settings.emulateResolution,
         WebInspector.overridesSupport.settings.deviceScaleFactor,
         WebInspector.overridesSupport.settings.emulateViewport,
         WebInspector.overridesSupport.settings.deviceTextAutosizing
