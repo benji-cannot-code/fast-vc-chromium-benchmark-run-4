65996b201c29836b6b100ad08fb4adbc5764b5e5
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-23 20:55:41 +0000

    Revert 279121 "Rewriting automation_node update handling based o..."
    AutomationApiTest.GeneratedTree test crashes now:
    http://build.chromium.org/p/chromium.linux/builders/Linux%20Tests%20%28dbg%29%281%29/builds/32326
    
    > Rewriting automation_node update handling based on ax_tree
    > 
    > BUG=309681
    > 
    > Review URL: https://codereview.chromium.org/326233002
    
    TBR=aboxhall@chromium.org
    
    Review URL: https://codereview.chromium.org/354503002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279173 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/resources/extensions/automation_custom_bindings.js
+++ chrome/renderer/resources/extensions/automation_custom_bindings.js
@@ -13,7 +13,8 @@ var eventBindings = require('event_bindings');
 var Event = eventBindings.Event;
 var forEach = require('utils').forEach;
 var lastError = require('lastError');
-var schema = requireNative('automationInternal').GetSchemaAdditions();
+var schema =
+    requireNative('automationInternal').GetSchemaAdditions();
 
 // TODO(aboxhall): Look into using WeakMap
 var idToAutomationRootNode = {};
@@ -87,7 +88,7 @@ automation.registerCustomHook(function(bindingsAPI) {
   });
 });
 
-// Listen to the automationInternal.onAccessibilityEvent event, which is
+// Listen to the automationInternal.onaccessibilityEvent event, which is
 // essentially a proxy for the AccessibilityHostMsg_Events IPC from the
 // renderer.
 automationInternal.onAccessibilityEvent.addListener(function(data) {
@@ -102,8 +103,7 @@ automationInternal.onAccessibilityEvent.addListener(function(data) {
     targetTree = new AutomationRootNode(pid, rid);
     idToAutomationRootNode[id] = targetTree;
   }
-  if (!privates(targetTree).impl.onAccessibilityEvent(data))
-    return;
+  privates(targetTree).impl.update(data);
   var eventType = data.eventType;
   if (eventType == 'loadComplete' || eventType == 'layoutComplete') {
     // If the tree wasn't available when getTree() was called, the callback will
@@ -119,15 +119,6 @@ automationInternal.onAccessibilityEvent.addListener(function(data) {
   }
 });
 
-automationInternal.onAccessibilityTreeDestroyed.addListener(function(pid, rid) {
-  var id = createAutomationRootNodeID(pid, rid);
-  var targetTree = idToAutomationRootNode[id];
-  if (targetTree)
-    privates(targetTree).impl.destroy();
-  else
-    console.log('no targetTree to destroy');
-  delete idToAutomationRootNode[id];
-});
 exports.binding = automation.generate();
 
 // Add additional accessibility bindings not specified in the automation IDL.
--- ui/accessibility/accessibility.gyp
+++ ui/accessibility/accessibility.gyp
@@ -47,18 +47,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       ]
     },
     {
-      'target_name': 'accessibility_test_support',
-      'type': 'static_library',
-      'dependencies': [
-        '../../base/base.gyp:base',
-        'accessibility'
-      ],
-      'sources': [
-        'tree_generator.cc',
-        'tree_generator.h'
-      ]
-    },
-    {
       'target_name': 'accessibility_unittests',
       'type': 'executable',
       'dependencies': [
@@ -68,7 +56,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         '../gfx/gfx.gyp:gfx',
         '../gfx/gfx.gyp:gfx_geometry',
         'accessibility',
-        'accessibility_test_support',
         'ax_gen',
       ],
       'sources': [
