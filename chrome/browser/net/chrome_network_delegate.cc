47e86d17527ed3ff8ff9b3780e0be97e559ea363
kundaji@chromium.org <kundaji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-23 23:21:37 +0000

    Display an error message on usage settings page when proxy cannot be contacted.
    Add data_reduction_proxy_usage_stats which keeps track of number of requests
    completed through data reduction proxy vs number of requests eligible to use
    data reduction proxy. Instantiate and set on io thread globals so that we can
    pass it to chrome network delegate. Expose isDataReductionProxyUnreachable()
    method to Android code through DataReductionProxySettings where it will be
    used to decide whether to display the unreachable proxy message.
    
    BUG=380195
    
    Review URL: https://codereview.chromium.org/338273003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279222 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/chrome_network_delegate.cc
+++ chrome/browser/net/chrome_network_delegate.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/data_reduction_proxy/browser/data_reduction_proxy_metrics.h"
 #include "components/data_reduction_proxy/browser/data_reduction_proxy_params.h"
 #include "components/data_reduction_proxy/browser/data_reduction_proxy_protocol.h"
+#include "components/data_reduction_proxy/browser/data_reduction_proxy_usage_stats.h"
 #include "components/domain_reliability/monitor.h"
 #include "components/google/core/browser/google_util.h"
 #include "content/public/browser/browser_thread.h"
@@ -297,7 +298,8 @@ ChromeNetworkDelegate::ChromeNetworkDelegate(
       original_content_length_(0),
       first_request_(true),
       prerender_tracker_(NULL),
-      data_reduction_proxy_params_(NULL) {
+      data_reduction_proxy_params_(NULL),
+      data_reduction_proxy_usage_stats_(NULL) {
   DCHECK(enable_referrers);
   extensions_delegate_.reset(
       ChromeExtensionsNetworkDelegate::Create(event_router));
@@ -535,6 +537,9 @@ void ChromeNetworkDelegate::OnRawBytesRead(const net::URLRequest& request,
 
 void ChromeNetworkDelegate::OnCompleted(net::URLRequest* request,
                                         bool started) {
+  if (data_reduction_proxy_usage_stats_)
+    data_reduction_proxy_usage_stats_->OnUrlRequestCompleted(request, started);
+
   TRACE_EVENT_ASYNC_END0("net", "URLRequest", request);
   if (request->status().status() == net::URLRequestStatus::SUCCESS) {
     // For better accuracy, we use the actual bytes read instead of the length
