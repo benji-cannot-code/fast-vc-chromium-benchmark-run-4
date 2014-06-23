f81bf898c784a11ed1675d5786bc5cad23c74d9e
dominik.rottsches@intel.com <dominik.rottsches@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-23 18:09:19 +0000

    Remove WebFontFamily.h
    Last part of the rename from WebFontFamily to WebFallbackFont.
    
    BUG=382411
    R=jamesr,abarth
    
    Review URL: https://codereview.chromium.org/330573007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176771 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/linux/WebFontInfo.h
+++ third_party/WebKit/public/platform/linux/WebFontInfo.h
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "../WebCString.h"
 #include "WebFallbackFont.h"
-#include "WebFontFamily.h"
 #include "WebFontRenderStyle.h"
 
 #include <string.h>
