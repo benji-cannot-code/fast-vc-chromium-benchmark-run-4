de3527110b1cc9f518dab1584b1932d6215ff011
suyash.s@samsung.com <suyash.s@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-23 07:42:50 +0000

    Renaming didNavigate to continueProgram
    There was a TODO to rename didNavigate() to continueProgram upstream. 
    As the changes were on both chrome and blink side hence uploading separate patches for the same. 
    
    This patch deals with blink side change.
    
    Link to chromium patch : https://codereview.chromium.org/330663002/
    
    Review URL: https://codereview.chromium.org/330603003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176727 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/web/WebDevToolsAgent.h
+++ third_party/WebKit/public/web/WebDevToolsAgent.h
@@ -56,6 +56,7 @@ public:
     virtual void detach() = 0;
 
     virtual void didNavigate() = 0;
+    virtual void continueProgram() = 0;
 
     virtual void dispatchOnInspectorBackend(const WebString& message) = 0;
 
