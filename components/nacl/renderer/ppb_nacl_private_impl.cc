5b5af7cf328011f5d3bf747f042c3076c2c578a9
hidehiko@chromium.org <hidehiko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-23 18:24:45 +0000

    Use base::File for the OpenResourceCallback's argument.
    ManifestServiceChannel can be removed while the DownloadFile is running.
    In such a case, ManifestServiceProxy::DidDownloadFile is called upon
    the completion of DownloadFile, and then callback is invoked.
    However, as the callback refers the ManifestServiceChannel weakly, so
    the native file descriptor will be leaked.
    By this change, OpenResourceCallback takes the base::File instead of
    base::PlatformFile. Since base::File will take the ownership of the
    native file descriptor, it will be closed correctly, even if the
    ManifestServiceChannel is deleted before.
    
    BUG=239656
    TEST=Ran browser_tests --gtest_filter=*NonSfi*:*NonSFI* locally and trybots.
    CQ_EXTRA_TRYBOTS=tryserver.chromium:linux_rel_precise32
    
    Review URL: https://codereview.chromium.org/348323002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279146 0039d316-1c4b-4281-b951-d872f2087c98

--- components/nacl/renderer/ppb_nacl_private_impl.cc
+++ components/nacl/renderer/ppb_nacl_private_impl.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/containers/scoped_ptr_hash_map.h"
 #include "base/cpu.h"
+#include "base/files/file.h"
 #include "base/lazy_instance.h"
 #include "base/logging.h"
 #include "base/rand_util.h"
@@ -212,7 +213,7 @@ class ManifestServiceProxy : public ManifestServiceChannel::Delegate {
     if (!ManifestResolveKey(pp_instance_, false, key, &url, &pnacl_options)) {
       base::MessageLoop::current()->PostTask(
           FROM_HERE,
-          base::Bind(callback, PP_kInvalidFileHandle));
+          base::Bind(callback, base::Passed(base::File())));
       return;
     }
 
@@ -233,10 +234,10 @@ class ManifestServiceProxy : public ManifestServiceChannel::Delegate {
       int32_t pp_error,
       const PP_NaClFileInfo& file_info) {
     if (pp_error != PP_OK) {
-      callback.Run(base::kInvalidPlatformFileValue);
+      callback.Run(base::File());
       return;
     }
-    callback.Run(file_info.handle);
+    callback.Run(base::File(file_info.handle));
   }
 
   void Quit() {
