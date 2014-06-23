43bda1591bc2c60561d72e9cd7351fca1dfd625b
jackhou@chromium.org <jackhou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-23 06:56:55 +0000

    Revert of Implement NativeViewHostAura::InstallClip. (https://codereview.chromium.org/317823002/)
    Reason for revert:
    Breaking ASan LSan:
    http://build.chromium.org/p/chromium.memory/builders/Linux%20ASan%20LSan%20Tests%20%282%29/builds/4038
    http://build.chromium.org/p/chromium.memory/builders/Linux%20ASan%20LSan%20Tests%20%283%29/builds/4410
    
    Original issue's description:
    > Implement NativeViewHostAura::InstallClip.
    > 
    > Despite the NOTIMPLEMENTED comment saying that this method is only
    > needed for performance, it causes bugs with aura windows in side
    > Bubbles which need to clip to their parent view rather than the parent
    > window.
    > 
    > This CL is based on https://codereview.chromium.org/30993004/ but strips
    > out the gravity code and simplifies the windowing code.
    > 
    > Since the window hierarchy of NPAPI plugins has changed due to this
    > patch, it was also necessary to modify WebContentsViewAura to reflect
    > this change.
    > 
    > BUG=377378
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=279051
    
    TBR=calamity@chromium.org,sky@chromium.org
    NOTRY=true
    NOTREECHECKS=true
    
    Review URL: https://codereview.chromium.org/339663003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279053 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/native/native_view_host_aura.h
+++ ui/views/controls/native/native_view_host_aura.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
-#include "ui/aura/window.h"
 #include "ui/aura/window_observer.h"
 #include "ui/views/controls/native/native_view_host_wrapper.h"
 #include "ui/views/views_export.h"
@@ -25,7 +24,7 @@ class VIEWS_EXPORT NativeViewHostAura : public NativeViewHostWrapper,
   virtual ~NativeViewHostAura();
 
   // Overridden from NativeViewHostWrapper:
-  virtual void AttachNativeView() OVERRIDE;
+  virtual void NativeViewWillAttach() OVERRIDE;
   virtual void NativeViewDetaching(bool destroyed) OVERRIDE;
   virtual void AddedToWidget() OVERRIDE;
   virtual void RemovedFromWidget() OVERRIDE;
@@ -39,27 +38,14 @@ class VIEWS_EXPORT NativeViewHostAura : public NativeViewHostWrapper,
   virtual gfx::NativeCursor GetCursor(int x, int y) OVERRIDE;
 
  private:
-  friend class NativeViewHostAuraTest;
-
   // Overridden from aura::WindowObserver:
   virtual void OnWindowDestroyed(aura::Window* window) OVERRIDE;
-
-  // Reparents the native view with the clipping window existing between it and
-  // its old parent, so that the fast resize path works.
-  void AddClippingWindow();
-
-  // If the native view has been reparented via AddClippingWindow, this call
-  // undoes it.
-  void RemoveClippingWindow();
 
   // Our associated NativeViewHost.
   NativeViewHost* host_;
 
-  // Window that exists between the native view and the parent that allows for
-  // clipping to occur. This is positioned in the coordinate space of
-  // host_->GetWidget().
-  aura::Window clipping_window_;
-  scoped_ptr<gfx::Rect> clip_rect_;
+  // Have we installed a clip region?
+  bool installed_clip_;
 
   DISALLOW_COPY_AND_ASSIGN(NativeViewHostAura);
 };
