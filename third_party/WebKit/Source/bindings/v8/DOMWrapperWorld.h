8c10ea09a7a954ef6e59f01f9ff77948e6c66bee
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-23 13:25:04 +0000

    Implement an infrastructure of Blink-in-JS (The IDL compiler part)
    * A design doc is here: https://docs.google.com/a/google.com/document/d/13cT9Klgvt_ciAR3ONGvzKvw6fz9-f6E0FrqYFqfoc8Y/edit
    
    * A full CL is here:
    https://codereview.chromium.org/345893002/
    
    * This CL implements the IDL compile part of the Blink-in-JS.
    
    -- IDLExtendedAttributes.txt: Added a [PrivateScriptInterface] IDL extended attribute, which indicates that a given interface is an interface for Blink-in-JS.
    
    -- v8_private_script_interface.py: A code generator for Blink-in-JS' IDL interface.
    
    -- private_script_interface.{h,cpp}: Template files for Blink-in-JS' IDL interface.
    
    -- TestPrivateScriptInterface.idl: Binding tests for a Blink-in-JS' interface.
    
    -- DOMWrapperWorld.{h,cpp}: Added a new dedicated world for Blink-in-JS execution.
    
    * This CL doesn't change any existing behavior, since Blink-in-JS is not yet used by any real code.
    
    BUG=341031
    
    Review URL: https://codereview.chromium.org/331863013
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176753 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/DOMWrapperWorld.h
+++ third_party/WebKit/Source/bindings/v8/DOMWrapperWorld.h
@@ -52,6 +52,7 @@ enum WorldIdConstants {
     // Embedder isolated worlds can use IDs in [1, 1<<29).
     EmbedderWorldIdLimit = (1 << 29),
     ScriptPreprocessorIsolatedWorldId,
+    PrivateScriptIsolatedWorldId,
     IsolatedWorldIdLimit,
     WorkerWorldId,
     TestingWorldId,
@@ -63,6 +64,7 @@ public:
     static PassRefPtr<DOMWrapperWorld> create(int worldId = -1, int extensionGroup = -1);
 
     static const int mainWorldExtensionGroup = 0;
+    static const int privateScriptIsolatedWorldExtensionGroup = 1;
     static PassRefPtr<DOMWrapperWorld> ensureIsolatedWorld(int worldId, int extensionGroup);
     ~DOMWrapperWorld();
     void dispose();
@@ -88,6 +90,7 @@ public:
     }
 
     static DOMWrapperWorld& mainWorld();
+    static DOMWrapperWorld& privateScriptIsolatedWorld();
 
     // Associates an isolated world (see above for description) with a security
     // origin. XMLHttpRequest instances used in that world will be considered
@@ -107,6 +110,7 @@ public:
     bool isolatedWorldHasContentSecurityPolicy();
 
     bool isMainWorld() const { return m_worldId == MainWorldId; }
+    bool isPrivateScriptIsolatedWorld() const { return m_worldId == PrivateScriptIsolatedWorldId; }
     bool isWorkerWorld() const { return m_worldId == WorkerWorldId; }
     bool isIsolatedWorld() const { return MainWorldId < m_worldId  && m_worldId < IsolatedWorldIdLimit; }
 
