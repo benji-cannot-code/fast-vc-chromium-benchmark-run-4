684cd2910a8b350a92bf920604bc3fe5437da4a1
eseidel@chromium.org <eseidel@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-23 22:57:16 +0000

    Refactor ResourceFetcher::determineRevalidationPolicy
    This change should not have any functional change.
    The only possible behavioral change is that we no longer
    will re-use main-document resources which do not match
    CORS. Speaking with Nate, we believe that main-document
    check to be a noop and thus I removed it.
    
    This moves the logic for validating ResourceLoaderOptions
    into that class, and adds a ton of FIXMES about how
    we are incorrectly re-using requests with non-matching
    ResourceLoaderOptions.  In my change for:
    https://codereview.chromium.org/258013009/
    (which causes us to go down the preloader codepaths more regularly)
    those FIXMEs cause us to re-use preload requests which
    we should not be.  I'll have to fix some of them in
    that change in order to pass the tests.
    
    The majority of this change is refactoring
    determineRevalidationPolicy to take a FetchRequest instead
    of individual arguments and then using the non-mutable
    resourceRequest() accessor and fix ResourceRequest
    non-const methods to just use a 'mutable' marked cache.
    ResourceRequest was confusing 'physical' const with
    'logical' const, I fixed these methods to be logically const.
    
    BUG=356292
    
    Review URL: https://codereview.chromium.org/333353006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176790 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/fetch/ResourceFetcher.cpp
+++ third_party/WebKit/Source/core/fetch/ResourceFetcher.cpp
@@ -690,7 +690,7 @@ ResourcePtr<Resource> ResourceFetcher::requestResource(Resource::Type type, Fetc
     // See if we can use an existing resource from the cache.
     ResourcePtr<Resource> resource = memoryCache()->resourceForURL(url);
 
-    const RevalidationPolicy policy = determineRevalidationPolicy(type, request.mutableResourceRequest(), request.forPreload(), resource.get(), request.defer(), request.options());
+    const RevalidationPolicy policy = determineRevalidationPolicy(type, request, resource.get());
     switch (policy) {
     case Reload:
         memoryCache()->remove(resource.get());
@@ -903,13 +903,15 @@ void ResourceFetcher::storeResourceTimingInitiatorInformation(Resource* resource
     }
 }
 
-ResourceFetcher::RevalidationPolicy ResourceFetcher::determineRevalidationPolicy(Resource::Type type, ResourceRequest& request, bool forPreload, Resource* existingResource, FetchRequest::DeferOption defer, const ResourceLoaderOptions& options) const
+ResourceFetcher::RevalidationPolicy ResourceFetcher::determineRevalidationPolicy(Resource::Type type, const FetchRequest& fetchRequest, Resource* existingResource) const
 {
+    const ResourceRequest& request = fetchRequest.resourceRequest();
+
     if (!existingResource)
         return Load;
 
     // We already have a preload going for this URL.
-    if (forPreload && existingResource->isPreloaded())
+    if (fetchRequest.forPreload() && existingResource->isPreloaded())
         return Use;
 
     // If the same URL has been loaded as a different type, we need to reload.
@@ -924,7 +926,7 @@ ResourceFetcher::RevalidationPolicy ResourceFetcher::determineRevalidationPolicy
 
     // Do not load from cache if images are not enabled. The load for this image will be blocked
     // in ImageResource::load.
-    if (FetchRequest::DeferredByClient == defer)
+    if (FetchRequest::DeferredByClient == fetchRequest.defer())
         return Reload;
 
     // Always use data uris.
@@ -953,8 +955,7 @@ ResourceFetcher::RevalidationPolicy ResourceFetcher::determineRevalidationPolicy
     if (m_allowStaleResources)
         return Use;
 
-    // If fetching a resource with a different 'CORS enabled' flag, reload.
-    if (type != Resource::MainResource && options.corsEnabled != existingResource->options().corsEnabled)
+    if (!fetchRequest.options().canReuseRequest(existingResource->options()))
         return Reload;
 
     // Always use preloads.
