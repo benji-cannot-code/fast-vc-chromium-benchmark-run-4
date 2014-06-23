58bb93e29da47c308b65e6002cf8964d5a2c7b1c
mcasas@chromium.org <mcasas@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-23 10:03:55 +0000

    Video Capture: Allow 0 fps frame rates to be deserializable
    Some webcams, notably the Win XSplit broadcaster, 
    indicate 0 fps frame rate when they are not started/
    configured. This rate is not deserializable 
    (because is not IsValid()) [1]. This CL allows 
    supported formats with 0 fps to be received via 
    IPC.
    
    XSplit walkthrough before-after:
    
    Currently when XSplit lists 0 fps, the message
    cannot be deserialized from the IPC and is 
    discarded; getUserMedia constraint resolution 
    @MediaStreamVideoCapturerSource never 
    progresses since is expecting that message.
    
    With this change, 0 fps will be used, and since
    for unconfigured XSplit this is the only frame 
    rate, it's used (unless the user constrains it 
    via Mandatory constraint). When the 0 fps 
    selected format is passed to the 
    VideoCaptureDeviceWin to be used, this ignores
    it (since is zero), and the capture happens
    at whatever frame rate is native to the
    source. And then we see an XSplit picture
    indicating this fact. When streaming starts,
    the picture is substituted with the feed.
    
    BUG=383949
    
    [1] https://code.google.com/p/chromium/codesearch#chromium/src/media/video/capture/video_capture_types.cc&sq=package:chromium&type=cs&q=IsValid%20video%20&l=26
    
    Review URL: https://codereview.chromium.org/332863002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279078 0039d316-1c4b-4281-b951-d872f2087c98

--- media/video/capture/video_capture_types.cc
+++ media/video/capture/video_capture_types.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace media {
 
 VideoCaptureFormat::VideoCaptureFormat()
-    : frame_rate(0), pixel_format(PIXEL_FORMAT_UNKNOWN) {}
+    : frame_rate(0.0f), pixel_format(PIXEL_FORMAT_UNKNOWN) {}
 
 VideoCaptureFormat::VideoCaptureFormat(const gfx::Size& frame_size,
                                        float frame_rate,
@@ -24,7 +24,7 @@ bool VideoCaptureFormat::IsValid() const {
          (frame_size.height() < media::limits::kMaxDimension) &&
          (frame_size.GetArea() >= 0) &&
          (frame_size.GetArea() < media::limits::kMaxCanvas) &&
-         (frame_rate > 0) &&
+         (frame_rate >= 0.0f) &&
          (frame_rate < media::limits::kMaxFramesPerSecond) &&
          (pixel_format >= PIXEL_FORMAT_UNKNOWN) &&
          (pixel_format < PIXEL_FORMAT_MAX);
