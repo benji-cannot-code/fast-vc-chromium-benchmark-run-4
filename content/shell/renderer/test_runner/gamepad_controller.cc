a308349515966990e15663f988242c3becc50e8e
loislo@chromium.org <loislo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-23 10:37:49 +0000

    Make ASAN happy. Do not call setGamepadProvider on null pointer.
    BUG=344556
    TBR=bajones, jam, kbalazs
    
    Review URL: https://codereview.chromium.org/349943002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279081 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/renderer/test_runner/gamepad_controller.cc
+++ content/shell/renderer/test_runner/gamepad_controller.cc
@@ -155,6 +155,8 @@ void GamepadController::Install(WebFrame* frame) {
 }
 
 void GamepadController::SetDelegate(WebTestDelegate* delegate) {
+  if (!delegate)
+    return;
   delegate->setGamepadProvider(this);
 }
 
