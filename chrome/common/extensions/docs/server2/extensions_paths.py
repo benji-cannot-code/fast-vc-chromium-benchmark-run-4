279800e9ea41b5a36a93f31f1d2cd0a233e4362e
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-23 18:15:19 +0000

    Docserver: Add tests for docs_server_utils.py.
    R=rockot@chromium.org
    
    Review URL: https://codereview.chromium.org/348333002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279142 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/server2/extensions_paths.py
+++ chrome/common/extensions/docs/server2/extensions_paths.py
@@ -8,11 +8,9 @@ from posixpath import join
 
 # Extensions-related paths within the Chromium repository.
 
-APPS = 'apps/common/'
 EXTENSIONS = 'extensions/common/'
 CHROME_EXTENSIONS = 'chrome/common/extensions/'
 
-APPS_API = join(APPS, 'api/')
 EXTENSIONS_API = join(EXTENSIONS, 'api/')
 CHROME_API = join(CHROME_EXTENSIONS, 'api/')
 
@@ -20,7 +18,6 @@ CHROME_API = join(CHROME_EXTENSIONS, 'api/')
 API_PATHS = (
   CHROME_API,
   EXTENSIONS_API,
-  APPS_API,
 )
 
 DOCS = join(CHROME_EXTENSIONS, 'docs/')
