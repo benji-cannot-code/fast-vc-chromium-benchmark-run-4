5804f66202999e62216db27c7cc006c4f8423ac0
simonb@chromium.org <simonb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-23 17:40:08 +0000

    Fix MIPS breakage introduced when adding support for arm64.
    MIPS shared libraries lack a DT_PLTREL, but current code insists on
    seeing either DT_REL or DT_RELA.  This stops the crazy linker from
    working correctly on MIPS:
    
      /LibraryLoader( 4421): Loading: chromium_android_linker
      I/LibraryLoader( 4421): Loading: chrome.2056.0
      E/chromium_android_linker( 4421): LoadLibrary: Could not open
      libchrome.2056.0.so: Unsupported or missing DT_PLTREL in dynamic section
    
    Remove the requirement for relocations_type_ to be set by DT_PLTREL.
    If not set by DT_PLTREL then assign based on the presence of either
    DT_REL or DT_RELA.
    
    BUG=386594
    
    Review URL: https://codereview.chromium.org/343933002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279134 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/android_crazy_linker/src/src/crazy_linker_elf_relocations.cpp
+++ third_party/android_crazy_linker/src/src/crazy_linker_elf_relocations.cpp
@@ -243,9 +243,18 @@ bool ElfRelocations::Init(const ElfView* view, Error* error) {
     }
   }
 
-  if (relocations_type_ != DT_REL && relocations_type_ != DT_RELA) {
-    *error = "Unsupported or missing DT_PLTREL in dynamic section";
+  if (has_rel_relocations && has_rela_relocations) {
+    *error = "Combining DT_REL and DT_RELA is not currently supported";
     return false;
+  }
+
+  // If DT_PLTREL did not explicitly assign relocations_type_, set it
+  // here based on the type of relocations found.
+  if (relocations_type_ != DT_REL && relocations_type_ != DT_RELA) {
+    if (has_rel_relocations)
+      relocations_type_ = DT_REL;
+    else if (has_rela_relocations)
+      relocations_type_ = DT_RELA;
   }
 
   if (relocations_type_ == DT_REL && has_rela_relocations) {
@@ -287,7 +296,7 @@ bool ElfRelocations::ApplyAll(const ElfSymbols* symbols,
       return false;
   }
 
-  else if (relocations_type_ == DT_RELA) {
+  if (relocations_type_ == DT_RELA) {
     if (!ApplyRelaRelocs(reinterpret_cast<ELF::Rela*>(plt_relocations_),
                          plt_relocations_size_ / sizeof(ELF::Rela),
                          symbols,
@@ -579,13 +588,16 @@ bool ElfRelocations::ApplyRelRelocs(const ELF::Rel* rel,
 
     // If this is a symbolic relocation, compute the symbol's address.
     if (__builtin_expect(rel_symbol != 0, 0)) {
-      resolved = ResolveSymbol(rel_type,
-                               rel_symbol,
-                               symbols,
-                               resolver,
-                               reloc,
-                               &sym_addr,
-                               error);
+      if (!ResolveSymbol(rel_type,
+                         rel_symbol,
+                         symbols,
+                         resolver,
+                         reloc,
+                         &sym_addr,
+                         error)) {
+        return false;
+      }
+      resolved = true;
     }
 
     if (!ApplyRelReloc(rel, sym_addr, resolved, error))
@@ -626,13 +638,16 @@ bool ElfRelocations::ApplyRelaRelocs(const ELF::Rela* rela,
 
     // If this is a symbolic relocation, compute the symbol's address.
     if (__builtin_expect(rel_symbol != 0, 0)) {
-      resolved = ResolveSymbol(rel_type,
-                               rel_symbol,
-                               symbols,
-                               resolver,
-                               reloc,
-                               &sym_addr,
-                               error);
+      if (!ResolveSymbol(rel_type,
+                         rel_symbol,
+                         symbols,
+                         resolver,
+                         reloc,
+                         &sym_addr,
+                         error)) {
+        return false;
+      }
+      resolved = true;
     }
 
     if (!ApplyRelaReloc(rela, sym_addr, resolved, error))
@@ -804,7 +819,7 @@ void ElfRelocations::CopyAndRelocate(size_t src_addr,
   if (relocations_type_ == DT_REL)
     RelocateRel(src_addr, dst_addr, map_addr, size);
 
-  else if (relocations_type_ == DT_RELA)
+  if (relocations_type_ == DT_RELA)
     RelocateRela(src_addr, dst_addr, map_addr, size);
 
 #ifdef __mips__
