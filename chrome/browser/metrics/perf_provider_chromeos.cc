929c1b1bed7a00b569d9bc4cd573f48a3fa0765e
sque@chromium.org <sque@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-23 23:23:06 +0000

    metrics: Collect perf profile upon restoring session
    BUG=chromium:358778
    TEST=Add logging to ParseProtoIfValid(). Open some tabs in ChromeOS, log out of
    Chrome, and then log back in so that the session is restored. Should see the
    logging trace displayed in /var/log/ui/ui.LATEST.
    
    Signed-off-by: Simon Que <sque@chromium.org>
    
    R=dhsharp@chromium.org, isherman@chromium.org, rapati@chromium.org
    
    Review URL: https://codereview.chromium.org/344713002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279225 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metrics/perf_provider_chromeos.cc
+++ chrome/browser/metrics/perf_provider_chromeos.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/rand_util.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/threading/sequenced_worker_pool.h"
+#include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/metrics/perf_provider_chromeos.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
@@ -22,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/debug_daemon_client.h"
+#include "content/public/browser/notification_service.h"
 
 namespace {
 
@@ -42,6 +44,16 @@ const size_t kCachedPerfDataProtobufSizeThreshold = 4 * 1024 * 1024;
 // resume. To limit the number of profiles, collect one for 1 in 10 resumes.
 // Adjust this number as needed.
 const int kResumeSamplingFactor = 10;
+
+// There may be too many session restores to collect a profile each time. Limit
+// the collection rate by collecting one per 10 restores. Adjust this number as
+// needed.
+const int kRestoreSessionSamplingFactor = 10;
+
+// This is used to space out session restore collections in the face of several
+// notifications in a short period of time. There should be no less than this
+// much time between collections. The current value is 30 seconds.
+const int kMinIntervalBetweenSessionRestoreCollectionsMs = 30 * 1000;
 
 // Enumeration representing success and various failure modes for collecting and
 // sending perf data.
@@ -118,6 +130,13 @@ PerfProvider::PerfProvider()
   chromeos::DBusThreadManager::Get()->GetPowerManagerClient()->
       AddObserver(this);
 
+  // Register as an observer of session restore.
+  // TODO(sque): clean this up to use something other than notifications.
+  session_restore_registrar_.Add(
+      this,
+      chrome::NOTIFICATION_SESSION_RESTORE_DONE,
+      content::NotificationService::AllBrowserContextsAndSources());
+
   // Check the login state. At the time of writing, this class is instantiated
   // before login. A subsequent login would activate the profiling. However,
   // that behavior may change in the future so that the user is already logged
@@ -180,6 +199,41 @@ void PerfProvider::SuspendDone(const base::TimeDelta& sleep_duration) {
   // http://crbug.com/358778.
   sampled_profile->set_ms_after_resume(0);
   CollectIfNecessary(sampled_profile.Pass());
+}
+
+void PerfProvider::Observe(int type,
+                           const content::NotificationSource& source,
+                           const content::NotificationDetails& details) {
+  // Only handle session restore notifications.
+  DCHECK_EQ(type, chrome::NOTIFICATION_SESSION_RESTORE_DONE);
+
+  // Collect a profile only 1/|kRestoreSessionSamplingFactor| of the time, to
+  // avoid collecting too much data and potentially causing UI latency.
+  if (base::RandGenerator(kRestoreSessionSamplingFactor) != 0)
+    return;
+
+  const base::TimeDelta min_interval =
+      base::TimeDelta::FromMilliseconds(
+          kMinIntervalBetweenSessionRestoreCollectionsMs);
+  const base::TimeDelta time_since_last_collection =
+      (base::TimeTicks::Now() - last_session_restore_collection_time_);
+  // Do not collect if there hasn't been enough elapsed time since the last
+  // collection.
+  if (!last_session_restore_collection_time_.is_null() &&
+      time_since_last_collection < min_interval) {
+    return;
+  }
+
+  // Fill out a SampledProfile protobuf that will contain the collected data.
+  scoped_ptr<SampledProfile> sampled_profile(new SampledProfile);
+  sampled_profile->set_trigger_event(SampledProfile::RESTORE_SESSION);
+  // TODO(sque): Vary the time after restore at which to collect a profile,
+  // and find a way to determine the number of tabs restored.
+  // http://crbug.com/358778.
+  sampled_profile->set_ms_after_restore(0);
+
+  CollectIfNecessary(sampled_profile.Pass());
+  last_session_restore_collection_time_ = base::TimeTicks::Now();
 }
 
 void PerfProvider::OnUserLoggedIn() {
--- chrome/browser/metrics/perf_provider_chromeos.h
+++ chrome/browser/metrics/perf_provider_chromeos.h
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/power_manager_client.h"
 #include "chromeos/login/login_state.h"
 #include "components/metrics/proto/sampled_profile.pb.h"
+#include "content/public/browser/notification_observer.h"
+#include "content/public/browser/notification_registrar.h"
 
 namespace metrics {
 
@@ -25,7 +27,8 @@ class WindowedIncognitoObserver;
 // performance profiling infrastructure built into the linux kernel. For more
 // information, see: https://perf.wiki.kernel.org/index.php/Main_Page.
 class PerfProvider : public base::NonThreadSafe,
-                     public chromeos::PowerManagerClient::Observer {
+                     public chromeos::PowerManagerClient::Observer,
+                     public content::NotificationObserver {
  public:
   PerfProvider();
   virtual ~PerfProvider();
@@ -59,6 +62,12 @@ class PerfProvider : public base::NonThreadSafe,
   // Turns on perf collection. Resets the timer that's used to schedule
   // collections.
   void OnUserLoggedIn();
+
+  // Called when a session restore has finished.
+  // Inherited from content::NotificationObserver.
+  virtual void Observe(int type,
+                       const content::NotificationSource& source,
+                       const content::NotificationDetails& details) OVERRIDE;
 
   // Turns off perf collection. Does not delete any data that was already
   // collected and stored in |cached_perf_data_|.
@@ -100,6 +109,13 @@ class PerfProvider : public base::NonThreadSafe,
 
   // Record of the start of the upcoming profiling interval.
   base::TimeTicks next_profiling_interval_start_;
+
+  // Used to register objects of this class as observers to be notified of
+  // session restore events.
+  content::NotificationRegistrar session_restore_registrar_;
+
+  // Tracks the last time a session restore was collected.
+  base::TimeTicks last_session_restore_collection_time_;
 
   // To pass around the "this" pointer across threads safely.
   base::WeakPtrFactory<PerfProvider> weak_factory_;
