06e705819aa05d00633164214dc4f94b53bb4f05
jmadill@chromium.org <jmadill@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-23 21:23:14 +0000

    Roll ANGLE DEPS for M37.
    Includes several fixes for WebGL tests, large arrays in shaders,
    and regression on older nVidia drivers.
    
    BUG=384420,380353,379799
    
    Review URL: https://codereview.chromium.org/344203006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279179 0039d316-1c4b-4281-b951-d872f2087c98

--- build/secondary/third_party/angle/angle_commit.h
+++
@@ -1,9 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#define ANGLE_COMMIT_HASH "dummy_gn_cid"
-#define ANGLE_COMMIT_HASH_SIZE 12 
-#define ANGLE_COMMIT_DATE "0000-00-00 00:00:00 +0000"
-
