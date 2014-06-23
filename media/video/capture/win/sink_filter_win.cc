e1a2bf8927af16a0fb09fd74ca339408f552c842
mcasas@chromium.org <mcasas@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-23 11:45:45 +0000

    Win Video Capture: support HDYC pixel format.
    This is a reland of a previous crrev.com/317073010
    that caused instabilities with Third party drivers,
    probably because of some subtle changes in
    enumeration. This CL lands only the new pixel format.
    
    Original description -------------------------------------
    
    This pixel format is a synonym of YUYV and is needed
    for at least the DeckLink video capture card.
    This format is not specified in Windows headers
    probably because this is just a synonym of other
    pixel formats, IOW they are all the same except for
    the first word, which is the name read in hex
    backwards:
    
    30323449 --> 0x30 0x32 0x34 0x49 -> 024I
    43594448 --> 0x43 0x59 0x44 0x48 -> CYDH
    
    and then all the YUV (422 and 420 alike) use the same
    trailing sequence
    ... 0x0000, 0x0010, {0x80, 0x00, 0x00, 0xaa, 0x00, 0x38, 0x9b, 0x71}}
    
    HDYC: http://www.fourcc.org/yuv.php#HDYC
    
    
    BUG=380939
    
    Review URL: https://codereview.chromium.org/348033004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279087 0039d316-1c4b-4281-b951-d872f2087c98

--- media/video/capture/win/sink_filter_win.cc
+++ media/video/capture/win/sink_filter_win.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "media/video/capture/win/sink_input_pin_win.h"
 
+namespace media {
+
 // Define GUID for I420. This is the color format we would like to support but
 // it is not defined in the DirectShow SDK.
 // http://msdn.microsoft.com/en-us/library/dd757532.aspx
@@ -16,7 +18,12 @@ GUID kMediaSubTypeI420 = {
   0x30323449, 0x0000, 0x0010, { 0x80, 0x00, 0x00, 0xAA, 0x00, 0x38, 0x9B, 0x71}
 };
 
-namespace media {
+// UYVY synonym with BT709 color components, used in HD video. This variation
+// might appear in non-USB capture cards and it's implemented as a normal YUV
+// pixel format with the characters HDYC encoded in the first array word.
+GUID kMediaSubTypeHDYC = {
+  0x43594448, 0x0000, 0x0010, { 0x80, 0x00, 0x00, 0xaa, 0x00, 0x38, 0x9b, 0x71}
+};
 
 SinkFilterObserver::~SinkFilterObserver() {}
 
