aa732770a920eeaf89c6d2b64339376e837316f9
gab@chromium.org <gab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-23 23:36:55 +0000

    Protect the kSyncRemainingRollbackTries pref.
    BUG=387933
    
    Review URL: https://codereview.chromium.org/347103005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279230 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prefs/chrome_pref_service_factory.cc
+++ chrome/browser/prefs/chrome_pref_service_factory.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/search_engines/search_engines_pref_names.h"
+#include "components/sync_driver/pref_names.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_thread.h"
 #include "extensions/browser/pref_names.h"
@@ -181,10 +182,15 @@ const PrefHashFilter::TrackedPreferenceMetadata kTrackedPrefs[] = {
     PrefHashFilter::ENFORCE_ON_LOAD,
     PrefHashFilter::TRACKING_STRATEGY_ATOMIC
   },
+  {
+    17, sync_driver::prefs::kSyncRemainingRollbackTries,
+    PrefHashFilter::ENFORCE_ON_LOAD,
+    PrefHashFilter::TRACKING_STRATEGY_ATOMIC
+  },
 };
 
 // The count of tracked preferences IDs across all platforms.
-const size_t kTrackedPrefsReportingIDsCount = 17;
+const size_t kTrackedPrefsReportingIDsCount = 18;
 COMPILE_ASSERT(kTrackedPrefsReportingIDsCount >= arraysize(kTrackedPrefs),
                need_to_increment_ids_count);
 
