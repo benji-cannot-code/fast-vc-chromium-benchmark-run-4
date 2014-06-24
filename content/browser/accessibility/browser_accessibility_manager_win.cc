48cdfee47a795d99a3d3cd1623907d163160c5ca
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-24 01:58:19 +0000

    Updated the WindowEventTarget interface in ui\base to return a bool from all its methods which indicates whether the message was handled.
    This is to ensure that the default handling on the Windows message is performed by the caller
    (LegacyRenderWidgetHostHWND).
    
    Most of the changes in this patch are around passing a bool flag to the methods in the WindowEventTarget
    interface and passing that information from the LegacyRenderWidgetHostHWND to the message crackers in ATL.
    
    The changes apart from those are as below:-
    1. Added a handler for WM_SYSCOMMAND in the LegacyRenderWidgetHostHWND class. This forwards the message to
       the parent. This is needed because there are cases when the LegacyRenderWidgetHostHWND class is of the same
       size as the parent. For e.g. App windows etc. In these cases the LegacyRenderWidgetHostHWND window receives
       non client mouse messages which when defproced result in the WM_SYSCOMMAND message coming in.
    
    2. There are cases when the LegacyRenderWidgetHostHWND is created after the parent is made visible and the
       associated RenderWidget is visible. Added code to make the LegacyRenderWidgetHostHWND hwnd visible.
    
    3. Added a Destroy method to the LegacyRenderWidgetHostHWND class. This method destroys the
    window. The instance is destroyed in OnFinalMessage. This
    ensures that we don't have issues with the LegacyRenderWidgetHostHWND  instance getting deleted in 
    the context of a message and crashes due to us calling SetMsgHandled which dereferences members in the ATL base 
    class.
    
    BUG=381609
    R=sky
    
    Review URL: https://codereview.chromium.org/349563002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279269 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/accessibility/browser_accessibility_manager_win.cc
+++ content/browser/accessibility/browser_accessibility_manager_win.cc
@@ -22,7 +22,7 @@ BrowserAccessibilityManager* BrowserAccessibilityManager::Create(
     BrowserAccessibilityDelegate* delegate,
     BrowserAccessibilityFactory* factory) {
   return new BrowserAccessibilityManagerWin(
-      content::LegacyRenderWidgetHostHWND::Create(GetDesktopWindow()).get(),
+      content::LegacyRenderWidgetHostHWND::Create(GetDesktopWindow()),
       NULL, initial_tree, delegate, factory);
 }
 
--- content/browser/renderer_host/render_widget_host_view_aura.cc
+++ content/browser/renderer_host/render_widget_host_view_aura.cc
@@ -454,6 +454,9 @@ RenderWidgetHostViewAura::RenderWidgetHostViewAura(RenderWidgetHost* host)
   bool overscroll_enabled = CommandLine::ForCurrentProcess()->
       GetSwitchValueASCII(switches::kOverscrollHistoryNavigation) != "0";
   SetOverscrollControllerEnabled(overscroll_enabled);
+#if defined(OS_WIN)
+  legacy_render_widget_host_HWND_ = NULL;
+#endif
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -1009,15 +1012,22 @@ void RenderWidgetHostViewAura::InternalSetBounds(const gfx::Rect& rect) {
     if (!legacy_render_widget_host_HWND_) {
       legacy_render_widget_host_HWND_ = LegacyRenderWidgetHostHWND::Create(
           reinterpret_cast<HWND>(GetNativeViewId()));
-      BrowserAccessibilityManagerWin* manager =
-          static_cast<BrowserAccessibilityManagerWin*>(
-              GetBrowserAccessibilityManager());
-      if (manager)
-        manager->SetAccessibleHWND(legacy_render_widget_host_HWND_.get());
     }
     if (legacy_render_widget_host_HWND_) {
       legacy_render_widget_host_HWND_->SetBounds(
           window_->GetBoundsInRootWindow());
+      // There are cases where the parent window is created, made visible and
+      // the associated RenderWidget is also visible before the
+      // LegacyRenderWidgetHostHWND instace is created. Ensure that it is shown
+      // here.
+      if (!host_->is_hidden())
+        legacy_render_widget_host_HWND_->Show();
+
+      BrowserAccessibilityManagerWin* manager =
+          static_cast<BrowserAccessibilityManagerWin*>(
+              GetBrowserAccessibilityManager());
+      if (manager)
+        manager->SetAccessibleHWND(legacy_render_widget_host_HWND_);
     }
   }
 
@@ -1245,10 +1255,8 @@ void RenderWidgetHostViewAura::CreateBrowserAccessibilityManagerIfNeeded() {
   if (!GetBrowserAccessibilityManager()) {
     gfx::NativeViewAccessible accessible_parent =
         host_->GetParentNativeViewAccessible();
-    LegacyRenderWidgetHostHWND* parent_hwnd =
-        legacy_render_widget_host_HWND_.get();
     SetBrowserAccessibilityManager(new BrowserAccessibilityManagerWin(
-        legacy_render_widget_host_HWND_.get(), accessible_parent,
+        legacy_render_widget_host_HWND_, accessible_parent,
         BrowserAccessibilityManagerWin::GetEmptyDocument(), host_));
   }
 #else
@@ -1684,6 +1692,19 @@ void RenderWidgetHostViewAura::OnWindowDestroying(aura::Window* window) {
   }
   LPARAM lparam = reinterpret_cast<LPARAM>(this);
   EnumChildWindows(parent, WindowDestroyingCallback, lparam);
+
+  // The LegacyRenderWidgetHostHWND instance is destroyed when its window is
+  // destroyed. Normally we control when that happens via the Destroy call
+  // in the dtor. However there may be cases where the window is destroyed
+  // by Windows, i.e. the parent window is destroyed before the
+  // RenderWidgetHostViewAura instance goes away etc. To avoid that we
+  // destroy the LegacyRenderWidgetHostHWND instance here.
+  if (legacy_render_widget_host_HWND_) {
+    legacy_render_widget_host_HWND_->Destroy();
+    // The Destroy call above will delete the LegacyRenderWidgetHostHWND
+    // instance.
+    legacy_render_widget_host_HWND_ = NULL;
+  }
 #endif
 
   // Make sure that the input method no longer references to this object before
@@ -2173,7 +2194,10 @@ RenderWidgetHostViewAura::~RenderWidgetHostViewAura() {
   DetachFromInputMethod();
 
 #if defined(OS_WIN)
-  legacy_render_widget_host_HWND_.reset(NULL);
+  // The LegacyRenderWidgetHostHWND window should have been destroyed in
+  // RenderWidgetHostViewAura::OnWindowDestroying and the pointer should
+  // be set to NULL.
+  DCHECK(!legacy_render_widget_host_HWND_);
 #endif
 }
 
