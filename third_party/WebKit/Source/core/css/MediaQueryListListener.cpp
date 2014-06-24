3c2ab86ae3992f1f0cbd818dcd68bcd7a49e3ac0
kihong.kwon@samsung.com <kihong.kwon@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-24 06:36:30 +0000

    Use ScriptController to call callback in MediaQueryListListener
    MediaQueryListListener uses invokeCallback which is in "V8Binding".
    But it could use "ScriptController" directly. This change makes
    invokeCallback could be removed from V8Binding.
    
    BUG=388049
    
    Review URL: https://codereview.chromium.org/349183006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176818 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/MediaQueryListListener.cpp
+++ third_party/WebKit/Source/core/css/MediaQueryListListener.cpp
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/css/MediaQueryListListener.h"
 
 #include "bindings/core/v8/V8MediaQueryList.h"
-#include "bindings/v8/V8Binding.h"
+#include "bindings/v8/ScriptController.h"
 #include <v8.h>
 
 namespace WebCore {
@@ -40,7 +40,7 @@ void MediaQueryListListener::queryChanged(MediaQueryList* query)
         return;
     ScriptState::Scope scope(m_scriptState.get());
     v8::Handle<v8::Value> args[] = { toV8(query, m_scriptState->context()->Global(), m_scriptState->isolate()) };
-    invokeCallback(m_scriptState.get(), v8::Handle<v8::Function>::Cast(m_function.v8Value()), WTF_ARRAY_LENGTH(args), args);
+    ScriptController::callFunction(m_scriptState->executionContext(), v8::Handle<v8::Function>::Cast(m_function.v8Value()), m_scriptState->context()->Global(), WTF_ARRAY_LENGTH(args), args, m_scriptState->isolate());
 }
 
 }
