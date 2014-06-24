16c7948b69eaa445ad87dcd9e38ab60534489005
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-24 00:54:55 +0000

    Update comments referencing TextButton.
    No code changes, just updating stale comments.
    
    BUG=155363
    TEST=none
    TBR=pkasting@chromium.org
    
    Review URL: https://codereview.chromium.org/350443009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279250 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/website_settings/permission_selector_view.cc
+++ chrome/browser/ui/views/website_settings/permission_selector_view.cc
@@ -46,7 +46,7 @@ class PermissionMenuButton : public views::MenuButton,
                        bool show_menu_marker);
   virtual ~PermissionMenuButton();
 
-  // Overridden from views::TextButton.
+  // Overridden from views::LabelButton.
   virtual void SetText(const base::string16& text) OVERRIDE;
 
   // Overridden from views::View.
