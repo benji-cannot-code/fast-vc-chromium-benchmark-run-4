5c671468c936fb6acca5e4f4852d0fb05e68d00e
krstnmnlsn@chromium.org <krstnmnlsn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-24 01:52:43 +0000

    Blacklist registry keys now stored under chromium name for chromium builds.
    BUG=
    
    Review URL: https://codereview.chromium.org/349693002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279267 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_elf/chrome_elf_constants.cc
+++ chrome_elf/chrome_elf_constants.cc
@@ -6,6 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome_elf/chrome_elf_constants.h"
 
 #if defined(GOOGLE_CHROME_BUILD)
+#define PRODUCT_STRING_PATH L"Google\\Chrome"
+#elif defined(CHROMIUM_BUILD)
+#define PRODUCT_STRING_PATH L"Chromium"
+#else
+#error Unknown branding
+#endif
+
+#if defined(GOOGLE_CHROME_BUILD)
 const wchar_t kAppDataDirName[] = L"Google\\Chrome";
 #else
 const wchar_t kAppDataDirName[] = L"Chromium";
@@ -17,9 +25,10 @@ const wchar_t kUserDataDirName[] = L"User Data";
 
 namespace blacklist {
 
-const wchar_t kRegistryBeaconPath[] = L"SOFTWARE\\Google\\Chrome\\BLBeacon";
+const wchar_t kRegistryBeaconPath[] =
+    L"SOFTWARE\\" PRODUCT_STRING_PATH L"\\BLBeacon";
 const wchar_t kRegistryFinchListPath[] =
-    L"SOFTWARE\\Google\\Chrome\\BLFinchList";
+    L"SOFTWARE\\" PRODUCT_STRING_PATH L"\\BLFinchList";
 const wchar_t kBeaconVersion[] = L"version";
 const wchar_t kBeaconState[] = L"state";
 const wchar_t kBeaconAttemptCount[] = L"failed_count";
