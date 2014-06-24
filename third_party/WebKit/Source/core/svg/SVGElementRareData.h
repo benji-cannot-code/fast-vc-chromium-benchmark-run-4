0ef12fdaa90c80504c8662d1f906391a46abb0eb
kouhei@chromium.org <kouhei@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-24 04:16:16 +0000

    Fix oilpan build breakage from r176803 (take 2)
    Looks like the patch also reduced dependencies, and if ENABLED(OILPAN)
    blocks need additional includes.
    
    BUG=378670
    R=haraken@chromium.org
    
    Review URL: https://codereview.chromium.org/353593002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176810 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGElementRareData.h
+++ third_party/WebKit/Source/core/svg/SVGElementRareData.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/css/StylePropertySet.h"
 #include "core/css/resolver/StyleResolver.h"
+#include "core/svg/SVGCursorElement.h"
 #include "wtf/HashSet.h"
 #include "wtf/Noncopyable.h"
 #include "wtf/StdLibExtras.h"

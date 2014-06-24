benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 16b1a9f92baf31df1e13959529d78d1cf66f2801

SVG: Remove ElementDependenciesMap.
Before this patch, SVGElement references were managed in
|SVGDocumentExtentions::m_elementReferencesMap|. This was implemented as
a map<SVGElement*, set<SVGElement*>>, and was hard to manage.
In particular, clearing out map entry with empty set<> is hard in oilpan.

This patch moves this bookkeeping into SVGElementRareData.
We introduce two set<SVGElement*> to manage incoming/outgoing references.

This patch keeps the current reference management interfaces in
SVGDocumentExtentions class. The management methods are to be moved to
SVGElement in a follow up CL.

BUG=378670

Review URL: https://codereview.chromium.org/338833007

git-svn-id: svn://svn.chromium.org/blink/trunk@176803 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/svg/SVGElementRareData.h
@@ -51,6 +51,11 @@ public:
     {
     }
 
+    WillBeHeapHashSet<RawPtrWillBeWeakMember<SVGElement> >& referencingElements() { return m_referencingElements; }
+    const WillBeHeapHashSet<RawPtrWillBeWeakMember<SVGElement> >& referencingElements() const { return m_referencingElements; }
+    WillBeHeapHashSet<RawPtrWillBeWeakMember<SVGElement> >& referencedElements() { return m_referencedElements; }
+    const WillBeHeapHashSet<RawPtrWillBeWeakMember<SVGElement> >& referencedElements() const { return m_referencedElements; }
+
     WillBeHeapHashSet<RawPtrWillBeWeakMember<SVGElement> >& elementInstances() { return m_elementInstances; }
     const WillBeHeapHashSet<RawPtrWillBeWeakMember<SVGElement> >& elementInstances() const { return m_elementInstances; }
 
@@ -95,6 +100,8 @@ public:
     void trace(Visitor* visitor)
     {
 #if ENABLE(OILPAN)
+        visitor->trace(m_referencingElements);
+        visitor->trace(m_referencedElements);
         visitor->trace(m_animatedSMILStyleProperties);
         visitor->trace(m_elementInstances);
         visitor->trace(m_correspondingElement);
@@ -128,6 +135,8 @@ private:
 #if ENABLE(OILPAN)
     Member<SVGElement> m_owner;
 #endif
+    WillBeHeapHashSet<RawPtrWillBeMember<SVGElement> > m_referencingElements;
+    WillBeHeapHashSet<RawPtrWillBeMember<SVGElement> > m_referencedElements;
     WillBeHeapHashSet<RawPtrWillBeWeakMember<SVGElement> > m_elementInstances;
     RawPtrWillBeWeakMember<SVGCursorElement> m_cursorElement;
     RawPtrWillBeWeakMember<CSSCursorImageValue> m_cursorImageValue;

third_party/WebKit/Source/core/svg/SVGDocumentExtensions.cpp
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/Document.h"
 #include "core/rendering/RenderView.h"
 #include "core/rendering/svg/SVGResourcesCache.h"
+#include "core/svg/SVGElementRareData.h"
 #include "core/svg/SVGFontFaceElement.h"
 #include "core/svg/SVGSVGElement.h"
 #include "core/svg/SVGViewSpec.h"
@@ -238,8 +239,8 @@ void SVGDocumentExtensions::removeElementFromPendingResources(Element* element)
         clearHasPendingResourcesIfPossible(element);
 
         // We use the removePendingResource function here because it deals with set lifetime correctly.
-        Vector<AtomicString>::iterator vectorEnd = toBeRemoved.end();
-        for (Vector<AtomicString>::iterator it = toBeRemoved.begin(); it != vectorEnd; ++it)
+        Vector<AtomicString>::iterator itEnd = toBeRemoved.end();
+        for (Vector<AtomicString>::iterator it = toBeRemoved.begin(); it != itEnd; ++it)
             removePendingResource(*it);
     }
 
@@ -258,8 +259,8 @@ void SVGDocumentExtensions::removeElementFromPendingResources(Element* element)
         }
 
         // We use the removePendingResourceForRemoval function here because it deals with set lifetime correctly.
-        Vector<AtomicString>::iterator vectorEnd = toBeRemoved.end();
-        for (Vector<AtomicString>::iterator it = toBeRemoved.begin(); it != vectorEnd; ++it)
+        Vector<AtomicString>::iterator itEnd = toBeRemoved.end();
+        for (Vector<AtomicString>::iterator it = toBeRemoved.begin(); it != itEnd; ++it)
             removePendingResourceForRemoval(*it);
     }
 }
@@ -311,10 +312,9 @@ Element* SVGDocumentExtensions::removeElementFromPendingResourcesForRemoval(cons
 SVGElementSet* SVGDocumentExtensions::setOfElementsReferencingTarget(SVGElement* referencedElement) const
 {
     ASSERT(referencedElement);
-    const ElementDependenciesMap::const_iterator it = m_elementDependencies.find(referencedElement);
-    if (it == m_elementDependencies.end())
+    if (!referencedElement->hasSVGRareData())
         return 0;
-    return it->value.get();
+    return &referencedElement->svgRareData()->referencingElements();
 }
 
 void SVGDocumentExtensions::addElementReferencingTarget(SVGElement* referencingElement, SVGElement* referencedElement)
@@ -322,68 +322,59 @@ void SVGDocumentExtensions::addElementReferencingTarget(SVGElement* referencingE
     ASSERT(referencingElement);
     ASSERT(referencedElement);
 
-    if (SVGElementSet* elements = m_elementDependencies.get(referencedElement)) {
-        elements->add(referencingElement);
-        return;
-    }
-
-    OwnPtrWillBeRawPtr<SVGElementSet> elements = adoptPtrWillBeNoop(new SVGElementSet);
-    elements->add(referencingElement);
-    m_elementDependencies.set(referencedElement, elements.release());
+    referencedElement->ensureSVGRareData()->referencingElements().add(referencingElement);
+    referencingElement->ensureSVGRareData()->referencedElements().add(referencedElement);
 }
 
 void SVGDocumentExtensions::removeAllTargetReferencesForElement(SVGElement* referencingElement)
 {
-    WillBeHeapVector<RawPtrWillBeMember<SVGElement> > toBeRemoved;
+    if (!referencingElement->hasSVGRareData())
+        return;
 
-    ElementDependenciesMap::iterator end = m_elementDependencies.end();
-    for (ElementDependenciesMap::iterator it = m_elementDependencies.begin(); it != end; ++it) {
-        SVGElement* referencedElement = it->key;
-        SVGElementSet* referencingElements = it->value.get();
-        SVGElementSet::iterator setIt = referencingElements->find(referencingElement);
-        if (setIt == referencingElements->end())
-            continue;
-
-        referencingElements->remove(setIt);
-        if (referencingElements->isEmpty())
-            toBeRemoved.append(referencedElement);
+    SVGElementSet& referencedElements = referencingElement->svgRareData()->referencedElements();
+    for (SVGElementSet::iterator it = referencedElements.begin(), itEnd = referencedElements.end(); it != itEnd; ++it) {
+        SVGElement* referencedElement = *it;
+        ASSERT(referencedElement->hasSVGRareData());
+        referencedElement->ensureSVGRareData()->referencingElements().remove(referencingElement);
     }
-
-    m_elementDependencies.removeAll(toBeRemoved);
+    referencedElements.clear();
 }
 
 void SVGDocumentExtensions::rebuildAllElementReferencesForTarget(SVGElement* referencedElement)
 {
     ASSERT(referencedElement);
-    ElementDependenciesMap::iterator it = m_elementDependencies.find(referencedElement);
-    if (it == m_elementDependencies.end())
+    if (!referencedElement->hasSVGRareData())
         return;
-    ASSERT(it->key == referencedElement);
 
-    WillBeHeapVector<RawPtrWillBeMember<SVGElement> > toBeNotified;
-    SVGElementSet* referencingElements = it->value.get();
-    copyToVector(*referencingElements, toBeNotified);
+    const SVGElementSet& referencingElements = referencedElement->svgRareData()->referencingElements();
+
+    // Iterate on a snapshot as |referencingElements| may be altered inside loop.
+    WillBeHeapVector<RawPtrWillBeMember<SVGElement> > referencingElementsSnapShot;
+    copyToVector(referencingElements, referencingElementsSnapShot);
 
     // Force rebuilding the referencingElement so it knows about this change.
-    WillBeHeapVector<RawPtrWillBeMember<SVGElement> >::iterator vectorEnd = toBeNotified.end();
-    for (WillBeHeapVector<RawPtrWillBeMember<SVGElement> >::iterator vectorIt = toBeNotified.begin(); vectorIt != vectorEnd; ++vectorIt) {
+    for (WillBeHeapVector<RawPtrWillBeMember<SVGElement> >::iterator it = referencingElementsSnapShot.begin(), itEnd = referencingElementsSnapShot.end(); it != itEnd; ++it) {
+        SVGElement* referencingElement = *it;
+
         // Before rebuilding referencingElement ensure it was not removed from under us.
-        if (SVGElementSet* referencingElements = setOfElementsReferencingTarget(referencedElement)) {
-            if (referencingElements->contains(*vectorIt))
-                (*vectorIt)->svgAttributeChanged(XLinkNames::hrefAttr);
-        }
+        if (referencingElements.contains(referencingElement))
+            referencingElement->svgAttributeChanged(XLinkNames::hrefAttr);
     }
 }
 
 void SVGDocumentExtensions::removeAllElementReferencesForTarget(SVGElement* referencedElement)
 {
     ASSERT(referencedElement);
-    ElementDependenciesMap::iterator it = m_elementDependencies.find(referencedElement);
-    if (it == m_elementDependencies.end())
+    if (!referencedElement->hasSVGRareData())
         return;
-    ASSERT(it->key == referencedElement);
 
-    m_elementDependencies.remove(it);
+    SVGElementSet& referencingElements = referencedElement->svgRareData()->referencingElements();
+    for (SVGElementSet::iterator it = referencingElements.begin(), itEnd = referencingElements.end(); it != itEnd; ++it) {
+        SVGElement* referencingElement = *it;
+        ASSERT(referencedElement->hasSVGRareData());
+        referencingElement->ensureSVGRareData()->referencedElements().remove(referencedElement);
+    }
+    referencingElements.clear();
 }
 
 void SVGDocumentExtensions::addSVGRootWithRelativeLengthDescendents(SVGSVGElement* svgRoot)
@@ -485,7 +476,6 @@ void SVGDocumentExtensions::trace(Visitor* visitor)
     visitor->trace(m_svgFontFaceElements);
     visitor->trace(m_pendingSVGFontFaceElementsForRemoval);
 #endif
-    visitor->trace(m_elementDependencies);
     visitor->trace(m_relativeLengthSVGRoots);
 }
 

third_party/WebKit/Source/core/svg/SVGDocumentExtensions.h
@@ -110,8 +110,6 @@ private:
     HashMap<AtomicString, RenderSVGResourceContainer*> m_resources;
     HashMap<AtomicString, OwnPtr<SVGPendingElements> > m_pendingResources; // Resources that are pending.
     HashMap<AtomicString, OwnPtr<SVGPendingElements> > m_pendingResourcesForRemoval; // Resources that are pending and scheduled for removal.
-    typedef WillBeHeapHashMap<RawPtrWillBeMember<SVGElement>, OwnPtrWillBeMember<SVGElementSet> > ElementDependenciesMap;
-    ElementDependenciesMap m_elementDependencies;
     OwnPtr<SVGResourcesCache> m_resourcesCache;
     WillBeHeapHashSet<RawPtrWillBeMember<SVGSVGElement> > m_relativeLengthSVGRoots; // Root SVG elements with relative length descendants.
     FloatPoint m_translate;

third_party/WebKit/Source/core/svg/SVGElement.cpp
@@ -96,15 +96,12 @@ SVGElement::~SVGElement()
             cursorElement->removeReferencedElement(this);
         if (CSSCursorImageValue* cursorImageValue = svgRareData()->cursorImageValue())
             cursorImageValue->removeReferencedElement(this);
-        // Clear the rare data now so that we are in a consistent state when
-        // calling rebuildAllElementReferencesForTarget() below.
-        m_SVGRareData.clear();
-    }
 
-    // With Oilpan, either removedFrom has been called or the document is dead
-    // as well and there is no reason to clear out the references.
-    document().accessSVGExtensions().rebuildAllElementReferencesForTarget(this);
-    document().accessSVGExtensions().removeAllElementReferencesForTarget(this);
+        // With Oilpan, either removedFrom has been called or the document is dead
+        // as well and there is no reason to clear out the references.
+        document().accessSVGExtensions().rebuildAllElementReferencesForTarget(this);
+        document().accessSVGExtensions().removeAllElementReferencesForTarget(this);
+    }
 #endif
 }
 
@@ -544,7 +541,6 @@ void SVGElement::removeInstanceMapping(SVGElement* instance)
         return;
 
     WillBeHeapHashSet<RawPtrWillBeWeakMember<SVGElement> >& instances = svgRareData()->elementInstances();
-    ASSERT(instances.contains(instance));
 
     instances.remove(instance);
 }

third_party/WebKit/Source/core/svg/SVGElement.h
@@ -209,8 +209,9 @@ protected:
 
     virtual bool selfHasRelativeLengths() const { return false; }
 
+    // FIXME: This "friend" is temporary to keep the current |SVGDocumentExtensions::*ReferencesFor*| methods. This will be removed after they are moved to SVGElement.
+    friend class SVGDocumentExtensions;
     SVGElementRareData* ensureSVGRareData();
-
     inline bool hasSVGRareData() const { return m_SVGRareData; }
     inline SVGElementRareData* svgRareData() const
     {

commit 0ef12fdaa90c80504c8662d1f906391a46abb0eb

Fix oilpan build breakage from r176803 (take 2)
Looks like the patch also reduced dependencies, and if ENABLED(OILPAN)
blocks need additional includes.

BUG=378670
R=haraken@chromium.org

Review URL: https://codereview.chromium.org/353593002

git-svn-id: svn://svn.chromium.org/blink/trunk@176810 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/svg/SVGElementRareData.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/css/StylePropertySet.h"
 #include "core/css/resolver/StyleResolver.h"
+#include "core/svg/SVGCursorElement.h"
 #include "wtf/HashSet.h"
 #include "wtf/Noncopyable.h"
 #include "wtf/StdLibExtras.h"