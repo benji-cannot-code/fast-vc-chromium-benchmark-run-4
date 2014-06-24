450095326e7bb2ef450e5dfa6b864b0761165329
jochen@chromium.org <jochen@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-24 12:07:38 +0000

    Revert of Oilpan: Build fix after r176830 (https://codereview.chromium.org/355633002/)
    Reason for revert:
    because it doesn't compile
    
    Original issue's description:
    > Oilpan: Build fix after r176830
    > 
    > TBR=wibling@chromium.org
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=176832
    
    TBR=wibling@chromium.org,oilpan-reviews@chromium.org,haraken@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/341303005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176834 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGElementRareData.h
+++ third_party/WebKit/Source/core/svg/SVGElementRareData.h
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SVGElementRareData_h
 #define SVGElementRareData_h
 
-#include "core/css/CSSCursorImageValue.cpp"
 #include "core/css/StylePropertySet.h"
 #include "core/css/resolver/StyleResolver.h"
 #include "core/svg/SVGCursorElement.h"
