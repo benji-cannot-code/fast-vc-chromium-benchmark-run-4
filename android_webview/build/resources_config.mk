534f1dc414a272e0829923f65f99c0bb88c2e3e1
mkosiba@chromium.org <mkosiba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-24 14:41:10 +0000

    [android_webview] Enable new android_webview resource target on Android.
    This enables compiling the android_webview_java target against R.java
    files generated by aapt (and not against the static resource map files).
    
    BUG=387155
    TEST=compile on android_aosp bot
    Change only relevant to android_aosp, builds fine there.
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/350173004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279402 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/build/resources_config.mk
+++ android_webview/build/resources_config.mk
@@ -5,8 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 android_webview_manifest_file := $(call my-dir)/AndroidManifest.xml
 
-# resources
+# Resources.
+# The res_hack folder is necessary to defeat a build system "optimization" which
+# ends up skipping running aapt if there are no resource files in any of the
+# resources dirs. Unfortunately, because all of our resources are generated at
+# build time and because this check is performed when processing the Makefile
+# it tests positive when building from clean resulting in a build failure.
+# We defeat the optimization by including an empty values.xml file in the list.
 android_webview_resources_dirs := \
+    $(call my-dir)/res_hack \
     $(call intermediates-dir-for,GYP,shared,,,$(TARGET_2ND_ARCH))/android_webview_jarjar_content_resources/jarjar_res \
     $(call intermediates-dir-for,GYP,shared,,,$(TARGET_2ND_ARCH))/android_webview_jarjar_ui_resources/jarjar_res \
     $(call intermediates-dir-for,GYP,ui_strings_grd,,,$(TARGET_2ND_ARCH))/ui_strings_grd/res_grit \
