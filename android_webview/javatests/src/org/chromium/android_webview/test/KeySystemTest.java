79bba7b2b5f5d222ffdbbbbdf1a7ae838f4c730d
ycheo@chromium.org <ycheo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-24 07:23:32 +0000

    aw: Fix the broken KeySystemTest on JB.
    The test was failed on the bot, because the bot uses the JB devices for testing and JB devices doesn't have MediaDrm/Crypto api.
    
    BUG=384753, 388141
    
    Review URL: https://codereview.chromium.org/339843003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279346 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/javatests/src/org/chromium/android_webview/test/KeySystemTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/KeySystemTest.java
@@ -5,10 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.android_webview.test;
 
+import android.os.Build;
 import android.test.suitebuilder.annotation.SmallTest;
 
 import org.chromium.android_webview.AwContents;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 
 /**
@@ -52,13 +52,12 @@ public class KeySystemTest extends AwTestBase {
         assertEquals("\"maybe\"", IsKeySystemSupported("webkit-org.w3.clearkey"));
     }
 
-    /*
     @Feature({"AndroidWebView"})
     @SmallTest
-    crbug.com/384753.
-    */
-    @DisabledTest
     public void testSupportWidevineKeySystem() throws Throwable {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+            return;  // MediaDrm/Crypto is supported from KitKat.
+        }
         assertEquals("\"maybe\"", IsKeySystemSupported("com.widevine.alpha"));
     }
 
@@ -71,6 +70,9 @@ public class KeySystemTest extends AwTestBase {
     @Feature({"AndroidWebView"})
     @SmallTest
     public void testSupportPlatformKeySystem() throws Throwable {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+            return;  // MediaDrm/Crypto is supported from KitKat.
+        }
         assertEquals("\"maybe\"", IsKeySystemSupported("com.oem.test-keysystem"));
     }
 }
