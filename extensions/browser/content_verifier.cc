b9c256a3a5acef12ef738cfe63a90cb81db9c73c
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-24 03:26:17 +0000

    Extensions: Add some CHECKs to help track down a flaky TestAdblockExtensionCrash.
    BUG=387866
    
    Review URL: https://codereview.chromium.org/354493003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279293 0039d316-1c4b-4281-b951-d872f2087c98

--- extensions/browser/content_verifier.cc
+++ extensions/browser/content_verifier.cc
@@ -27,6 +27,8 @@ ContentVerifier::ContentVerifier(content::BrowserContext* context,
           context,
           delegate,
           base::Bind(&ContentVerifier::OnFetchComplete, this))) {
+  // TODO(thestig): Remove after investigating http://crbug.com/387866
+  CHECK(context);
 }
 
 ContentVerifier::~ContentVerifier() {
