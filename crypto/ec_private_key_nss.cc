b11c02f7ab03ec4b9d64c44a0b954f3cca16577e
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-24 04:12:34 +0000

    crypto: Add ECPrivateKey::Copy (not needed for OpenSSL) and ECPrivateKey::ExportRawPublicKey.
    R=rsleevi@chromium.org
    
    Review URL: https://codereview.chromium.org/279973005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279308 0039d316-1c4b-4281-b951-d872f2087c98

--- crypto/ec_private_key_nss.cc
+++ crypto/ec_private_key_nss.cc
@@ -160,6 +160,13 @@ bool ECPrivateKey::ImportFromEncryptedPrivateKeyInfo(
     return false;
   }
 
+  if (SECKEY_GetPublicKeyType(*public_key) != ecKey) {
+    DLOG(ERROR) << "The public key is not an EC key";
+    SECKEY_DestroyPublicKey(*public_key);
+    *public_key = NULL;
+    return false;
+  }
+
   SECItem encoded_epki = {
     siBuffer,
     const_cast<unsigned char*>(encrypted_private_key_info),
@@ -207,6 +214,21 @@ bool ECPrivateKey::ImportFromEncryptedPrivateKeyInfo(
   }
 
   return true;
+}
+
+ECPrivateKey* ECPrivateKey::Copy() const {
+  scoped_ptr<ECPrivateKey> copy(new ECPrivateKey);
+  if (key_) {
+    copy->key_ = SECKEY_CopyPrivateKey(key_);
+    if (!copy->key_)
+      return NULL;
+  }
+  if (public_key_) {
+    copy->public_key_ = SECKEY_CopyPublicKey(public_key_);
+    if (!copy->public_key_)
+      return NULL;
+  }
+  return copy.release();
 }
 
 bool ECPrivateKey::ExportEncryptedPrivateKey(
@@ -265,6 +287,23 @@ bool ECPrivateKey::ExportPublicKey(std::vector<uint8>* output) {
   return true;
 }
 
+bool ECPrivateKey::ExportRawPublicKey(std::string* output) {
+  // public_key_->u.ec.publicValue is an ANSI X9.62 public key which, for
+  // a P-256 key, is 0x04 (meaning uncompressed) followed by the x and y field
+  // elements as 32-byte, big-endian numbers.
+  static const unsigned int kExpectedKeyLength = 65;
+
+  CHECK_EQ(ecKey, SECKEY_GetPublicKeyType(public_key_));
+  const unsigned char* const data = public_key_->u.ec.publicValue.data;
+  const unsigned int len = public_key_->u.ec.publicValue.len;
+  if (len != kExpectedKeyLength || data[0] != 0x04)
+    return false;
+
+  output->assign(reinterpret_cast<const char*>(data + 1),
+                 kExpectedKeyLength - 1);
+  return true;
+}
+
 bool ECPrivateKey::ExportValue(std::vector<uint8>* output) {
   return ReadAttribute(key_, CKA_VALUE, output);
 }
@@ -316,6 +355,7 @@ ECPrivateKey* ECPrivateKey::CreateWithParams(PK11SlotInfo* slot,
     DLOG(ERROR) << "PK11_GenerateKeyPair: " << PORT_GetError();
     return NULL;
   }
+  CHECK_EQ(ecKey, SECKEY_GetPublicKeyType(result->public_key_));
 
   return result.release();
 }
@@ -355,8 +395,10 @@ ECPrivateKey* ECPrivateKey::CreateFromEncryptedPrivateKeyInfoWithParams(
 
   SECKEY_DestroySubjectPublicKeyInfo(decoded_spki);
 
-  if (success)
+  if (success) {
+    CHECK_EQ(ecKey, SECKEY_GetPublicKeyType(result->public_key_));
     return result.release();
+  }
 
   return NULL;
 }
