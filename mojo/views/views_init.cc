1aac27d6cc2b9cdef3a4a1181096db0bf6ca5959
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-24 19:41:07 +0000

    A couple of changes that make views work from multiple threads
    The change to TextInputFocusManager makes me sad, but is the best I
    can do fow now. Will revisit ASAP.
    
    I had to fork off the parts of ViewsInit that need to run only once into a dll. This way I can guarantee the setup only happens once.
    
    BUG=388045
    TEST=none
    R=aa@chromium.org, ben@chromium.org, yukishiino@chromium.org
    
    Review URL: https://codereview.chromium.org/354513002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279468 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/views/views_init.cc
+++ mojo/views/views_init.cc
@@ -5,21 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "mojo/views/views_init.h"
 
-#include "base/i18n/icu_util.h"
-#include "base/path_service.h"
-#include "ui/base/resource/resource_bundle.h"
-#include "ui/base/ui_base_paths.h"
+#include "mojo/views/views_init_internal.h"
 
 namespace mojo {
 
 ViewsInit::ViewsInit() {
-  base::i18n::InitializeICU();
-
-  ui::RegisterPathProvider();
-
-  base::FilePath ui_test_pak_path;
-  CHECK(PathService::Get(ui::UI_TEST_PAK, &ui_test_pak_path));
-  ui::ResourceBundle::InitSharedInstanceWithPakPath(ui_test_pak_path);
+  InitViews();
 }
 
 ViewsInit::~ViewsInit() {
