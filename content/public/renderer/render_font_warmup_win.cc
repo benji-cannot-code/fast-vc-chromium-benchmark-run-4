2a748e95d61f9f0bc428a295957ab105c0d760dc
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-24 18:29:16 +0000

    Add GetLastError when loading DWrite factory
    Try to see if there's any useful error code when the GetProcAddress
    fails.
    
    R=cpu@chromium.org
    BUG=387867
    
    Review URL: https://codereview.chromium.org/349663004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279451 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/renderer/render_font_warmup_win.cc
+++ content/public/renderer/render_font_warmup_win.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <dwrite.h>
 
+#include "base/debug/alias.h"
 #include "base/logging.h"
 #include "third_party/WebKit/public/web/win/WebFontRendering.h"
 #include "third_party/skia/include/core/SkPaint.h"
@@ -23,10 +24,25 @@ SkFontMgr* g_warmup_fontmgr = NULL;
 // before sandbox lock down to allow Skia access to the Font Manager service.
 void CreateDirectWriteFactory(IDWriteFactory** factory) {
   typedef decltype(DWriteCreateFactory)* DWriteCreateFactoryProc;
+  HMODULE dwrite_dll = LoadLibraryW(L"dwrite.dll");
+  // TODO(scottmg): Temporary code to track crash in http://crbug.com/387867.
+  if (!dwrite_dll) {
+    DWORD load_library_get_last_error = GetLastError();
+    base::debug::Alias(&dwrite_dll);
+    base::debug::Alias(&load_library_get_last_error);
+    CHECK(false);
+  }
+
   DWriteCreateFactoryProc dwrite_create_factory_proc =
       reinterpret_cast<DWriteCreateFactoryProc>(
-          GetProcAddress(LoadLibraryW(L"dwrite.dll"), "DWriteCreateFactory"));
-  CHECK(dwrite_create_factory_proc);
+          GetProcAddress(dwrite_dll, "DWriteCreateFactory"));
+  // TODO(scottmg): Temporary code to track crash in http://crbug.com/387867.
+  if (!dwrite_create_factory_proc) {
+    DWORD get_proc_address_get_last_error = GetLastError();
+    base::debug::Alias(&dwrite_create_factory_proc);
+    base::debug::Alias(&get_proc_address_get_last_error);
+    CHECK(false);
+  }
   CHECK(SUCCEEDED(
       dwrite_create_factory_proc(DWRITE_FACTORY_TYPE_ISOLATED,
                                  __uuidof(IDWriteFactory),
