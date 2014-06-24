e6636a7ac7dabf1e8e03cfa1d6a7f833726215ab
mlamouri@chromium.org <mlamouri@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-24 16:20:57 +0000

    Successfuly resolve a screen lock request when the orientation has changed.
    The promise returned by screen.lockOrientation() is resolved with the new
    orientation when the screen orientation changes to follow the locking rules.
    
    BUG=162827
    
    Review URL: https://codereview.chromium.org/328303006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279422 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/screen_orientation/screen_orientation_dispatcher_host.cc
+++ content/browser/screen_orientation/screen_orientation_dispatcher_host.cc
@@ -9,7 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/screen_orientation_messages.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
+#include "content/public/browser/render_view_host.h"
+#include "content/public/browser/render_widget_host.h"
 #include "content/public/browser/web_contents.h"
+#include "third_party/WebKit/public/platform/WebScreenInfo.h"
 
 namespace content {
 
@@ -63,15 +66,23 @@ ScreenOrientationDispatcherHost::GetRenderFrameHostForRequestID(
                                  current_lock_->routing_id);
 }
 
-void ScreenOrientationDispatcherHost::NotifyLockSuccess(
-    int request_id, int angle, blink::WebScreenOrientationType type) {
+void ScreenOrientationDispatcherHost::NotifyLockSuccess(int request_id) {
   RenderFrameHost* render_frame_host =
       GetRenderFrameHostForRequestID(request_id);
   if (!render_frame_host)
     return;
 
+  DCHECK(web_contents()->GetRenderViewHost());
+
+  RenderWidgetHost* rwh = web_contents()->GetRenderViewHost();
+  blink::WebScreenInfo screen_info;
+  rwh->GetWebScreenInfo(&screen_info);
+
   render_frame_host->Send(new ScreenOrientationMsg_LockSuccess(
-      render_frame_host->GetRoutingID(), request_id, angle, type));
+      render_frame_host->GetRoutingID(),
+      request_id,
+      screen_info.orientationAngle,
+      screen_info.orientationType));
   ResetCurrentLock();
 }
 
@@ -87,9 +98,9 @@ void ScreenOrientationDispatcherHost::NotifyLockError(
   ResetCurrentLock();
 }
 
-void ScreenOrientationDispatcherHost::OnOrientationChange(
-    blink::WebScreenOrientationType orientation) {
-  Send(new ScreenOrientationMsg_OrientationChange(orientation));
+void ScreenOrientationDispatcherHost::OnOrientationChange() {
+  if (provider_)
+    provider_->OnOrientationChange();
 }
 
 void ScreenOrientationDispatcherHost::OnLockRequest(
@@ -112,9 +123,6 @@ void ScreenOrientationDispatcherHost::OnLockRequest(
   }
 
   provider_->LockOrientation(request_id, orientation);
-
-  // TODO(mlamouri): pass real values.
-  NotifyLockSuccess(request_id, 0, blink::WebScreenOrientationPortraitPrimary);
 }
 
 void ScreenOrientationDispatcherHost::OnUnlockRequest(
