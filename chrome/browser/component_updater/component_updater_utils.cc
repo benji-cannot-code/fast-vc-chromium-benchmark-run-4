91dda115626c87d10484fa0dba47818e22e43b2e
sorin@chromium.org <sorin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-24 01:50:36 +0000

    Remove dependencies of the Configurator on the ComponentUpdateService.
    This is a mechanical change (no difference in behavior is introduced).
    
    The class was an inner class, now it is a free standing class in the
    component_updater namespace.
    
    BUG=371463
    
    Review URL: https://codereview.chromium.org/344253010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279266 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/component_updater/component_updater_utils.cc
+++ chrome/browser/component_updater/component_updater_utils.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/sys_info.h"
 #include "base/win/windows_version.h"
-#include "chrome/browser/component_updater/component_updater_service.h"
 #include "chrome/browser/component_updater/crx_update_item.h"
 #include "chrome/browser/omaha_query_params/omaha_query_params.h"
 #include "chrome/common/chrome_version_info.h"
