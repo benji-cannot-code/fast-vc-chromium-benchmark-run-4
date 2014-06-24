1f2488843a9b768a20ed07c12b6d2b894b0d0603
kouhei@chromium.org <kouhei@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-24 05:44:33 +0000

    Oilpan: Fix leak from FontLoader.
    Prior to this patch, FontLoader referenced ResourceFetcher as a Member, but this is wrong, as FontLoader lifetime and ResourceFetcher lifetime is not correlated.
    
    Font->CSSFontSelector->FontLoader can outlive Document when cached by FontCache.
    
    ResourceFetcher may belong to Document OR DocumentLoader.
    Their lifetime is independent from (cached) Fonts lifetime.
    
    This caused leaks in svg/batik/paints/*.svg LayoutTests.
    
    TEST=svg/batik/paints/gradientLimit.svg w/ --enable-leak-detection
    BUG=None
    
    Review URL: https://codereview.chromium.org/339663006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176814 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/FontLoader.h
+++ third_party/WebKit/Source/core/css/FontLoader.h
@@ -44,7 +44,7 @@ private:
     typedef Vector<std::pair<ResourcePtr<FontResource>, ResourceLoader::RequestCountTracker> > FontsToLoadVector;
     FontsToLoadVector m_fontsToBeginLoading;
     RawPtrWillBeMember<CSSFontSelector> m_fontSelector;
-    RawPtrWillBeMember<ResourceFetcher> m_resourceFetcher;
+    RawPtrWillBeWeakMember<ResourceFetcher> m_resourceFetcher;
 };
 
 } // namespace WebCore
