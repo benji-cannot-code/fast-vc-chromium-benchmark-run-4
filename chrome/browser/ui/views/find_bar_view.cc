f66e1cde7d462308702aefc9fe0fe6cad71820fd
rsadam@chromium.org <rsadam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-24 21:17:49 +0000

    Show IME if needed on programmatic focus on the find_bar. We currently do not show the virtual keyboard on programmatic focus, however the find bar is a specific case where it makes sense to do so if the keyboard is enabled.
    BUG=380699
    
    Review URL: https://codereview.chromium.org/344793004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279491 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/find_bar_view.cc
+++ chrome/browser/ui/views/find_bar_view.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/border.h"
 #include "ui/views/controls/button/image_button.h"
 #include "ui/views/controls/label.h"
+#include "ui/views/ime/input_method.h"
 #include "ui/views/painter.h"
 #include "ui/views/widget/widget.h"
 
@@ -231,6 +232,7 @@ void FindBarView::ClearMatchCount() {
 
 void FindBarView::SetFocusAndSelection(bool select_all) {
   find_text_->RequestFocus();
+  GetInputMethod()->ShowImeIfNeeded();
   if (select_all && !find_text_->text().empty())
     find_text_->SelectAll(true);
 }
