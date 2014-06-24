9b1ab5b914c5cc6f437061ab05667bc3b75f049e
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-24 16:17:27 +0000

    Timeline: skip low-level trace events in TracingTimelineModel
    - pass filter of hidden record to TracingTimelineModel;
    - introduce Array.prototype.toObjectKeys(), as we frequently want
        to build set-like objects from a list of keys;
    - return hidden record filter, not an Object from TimelineUIUtils;
    
    BUG=361045
    R=yurys@chromium.org
    
    Review URL: https://codereview.chromium.org/346523003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176851 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/timeline/TracingTimelineModel.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TracingTimelineModel.js
@@ -4,16 +4,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 /**
- * @param {!WebInspector.TracingModel} tracingModel
  * @constructor
+ * @param {!WebInspector.TracingModel} tracingModel
+ * @param {!WebInspector.TimelineModel.Filter} recordFilter
  * @extends {WebInspector.TimelineModel}
  */
-WebInspector.TracingTimelineModel = function(tracingModel)
+WebInspector.TracingTimelineModel = function(tracingModel, recordFilter)
 {
     WebInspector.TimelineModel.call(this, tracingModel.target());
     this._tracingModel = tracingModel;
     this._mainThreadEvents = [];
     this._inspectedTargetEvents = [];
+    this._recordFilter = recordFilter;
 
     this.reset();
 }
@@ -228,10 +230,14 @@ WebInspector.TracingTimelineModel.prototype = {
                 if (!recordStack.length)
                     this._addTopLevelRecord(top);
             }
-            var parentRecord = recordStack.peekLast() || null;
-            var record = new WebInspector.TracingTimelineModel.TraceEventRecord(this, event, parentRecord);
+            var record = new WebInspector.TracingTimelineModel.TraceEventRecord(this, event);
             if (WebInspector.TracingTimelineUIUtils.isEventDivider(record))
                 this._eventDividerRecords.push(record);
+            if (!this._recordFilter.accept(record))
+                continue;
+            var parentRecord = recordStack.peekLast();
+            if (parentRecord)
+                parentRecord._addChild(record);
             if (event.endTime)
                 recordStack.push(record);
         }
@@ -474,17 +480,12 @@ WebInspector.TracingTimelineModel.prototype = {
  * @implements {WebInspector.TimelineModel.Record}
  * @param {!WebInspector.TimelineModel} model
  * @param {!WebInspector.TracingModel.Event} traceEvent
- * @param {?WebInspector.TracingTimelineModel.TraceEventRecord} parentRecord
  */
-WebInspector.TracingTimelineModel.TraceEventRecord = function(model, traceEvent, parentRecord)
+WebInspector.TracingTimelineModel.TraceEventRecord = function(model, traceEvent)
 {
     this._model = model;
     this._event = traceEvent;
     traceEvent._timelineRecord = this;
-    if (parentRecord) {
-        this.parent = parentRecord;
-        parentRecord._children.push(this);
-    }
     this._children = [];
 }
 
@@ -643,5 +644,16 @@ WebInspector.TracingTimelineModel.TraceEventRecord.prototype = {
     traceEvent: function()
     {
         return this._event;
-    }
+    },
+
+    /**
+     * @param {!WebInspector.TracingTimelineModel.TraceEventRecord} child
+     */
+    _addChild: function(child)
+    {
+        this._children.push(child);
+        child.parent = this;
+    },
+
+
 }
