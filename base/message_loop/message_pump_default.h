d35a82c0c80520ecc1bdb604b57625347225bbe6
miletus@chromium.org <miletus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-24 20:05:16 +0000

    Revert "[Mac] Reland r276808 Use a native MessagePump instead of a MessagePumpDefault"
    This reverts commit e82339d44d0dde725295785b9fea8c448d382621.
    
    See if this fixes the perf regression.
    
    BUG=388360
    TBR=jeremy@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/354673005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279474 0039d316-1c4b-4281-b951-d872f2087c98

--- base/message_loop/message_pump_default.h
+++ base/message_loop/message_pump_default.h
@@ -6,14 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_MESSAGE_LOOP_MESSAGE_PUMP_DEFAULT_H_
 #define BASE_MESSAGE_LOOP_MESSAGE_PUMP_DEFAULT_H_
 
-#include "base/base_export.h"
 #include "base/message_loop/message_pump.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/time/time.h"
 
 namespace base {
 
-class BASE_EXPORT MessagePumpDefault : public MessagePump {
+class MessagePumpDefault : public MessagePump {
  public:
   MessagePumpDefault();
   virtual ~MessagePumpDefault();
