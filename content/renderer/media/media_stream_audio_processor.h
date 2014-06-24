2600a0117c679972f89ec6fd849f0d663dcedf1f
grunell@chromium.org <grunell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-24 22:19:09 +0000

    More test cases for AEC dump.
    * Content browser test case for AEC dump in multiple renderers.
    * MediaStreamAudioProcessor unit test case for getting the AEC dump message filter correctly.
    
    BUG=350347
    
    Review URL: https://codereview.chromium.org/340783003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279516 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/media_stream_audio_processor.h
+++ content/renderer/media/media_stream_audio_processor.h
@@ -112,6 +112,8 @@ class CONTENT_EXPORT MediaStreamAudioProcessor :
 
  private:
   friend class MediaStreamAudioProcessorTest;
+  FRIEND_TEST_ALL_PREFIXES(MediaStreamAudioProcessorTest,
+                           GetAecDumpMessageFilter);
 
   class MediaStreamAudioConverter;
 
