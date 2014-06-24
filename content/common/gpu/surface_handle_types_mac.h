af53946fe448421266471b2fc400dbade182c32c
costan@gmail.com <costan@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-24 17:14:40 +0000

    Fix build on OSX 10.9.
    This fixes a build error on OSX 10.9 introduced by
    https://codereview.chromium.org/347653005/
    
    BUG=388160
    TBR=ccameron@chromium.org,piman@chromium.org,avi@chromium.org
    
    Review URL: https://codereview.chromium.org/350833002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279428 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/surface_handle_types_mac.h
+++ content/common/gpu/surface_handle_types_mac.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_COMMON_GPU_SURFACE_HANDLE_TYPES_MAC_H_
 #define CONTENT_COMMON_GPU_SURFACE_HANDLE_TYPES_MAC_H_
 
+#include <IOSurface/IOSurface.h>
 #include <OpenGL/CGLIOSurface.h>
 
 #include "base/basictypes.h"
