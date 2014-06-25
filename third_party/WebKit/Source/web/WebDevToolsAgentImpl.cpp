5c723bfc06ffa34e0a939cc91aff44fd84e30b0d
suyash.s@samsung.com <suyash.s@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-25 18:59:21 +0000

    The function is now replaced by continueProgram(),hence there is no need of it.
    The original TODO required changes on both chromium and blink side. Since both patches have landed we can safely remove the unused function.
    
    Link to blink patch : https://codereview.chromium.org/330603003/
    
    Link to chromium patch : https://codereview.chromium.org/330663002/
    
    Review URL: https://codereview.chromium.org/356623004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176942 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebDevToolsAgentImpl.cpp
+++ third_party/WebKit/Source/web/WebDevToolsAgentImpl.cpp
@@ -257,11 +257,6 @@ void WebDevToolsAgentImpl::detach()
     m_attached = false;
 }
 
-void WebDevToolsAgentImpl::didNavigate()
-{
-    ClientMessageLoopAdapter::didNavigate();
-}
-
 void WebDevToolsAgentImpl::continueProgram()
 {
     ClientMessageLoopAdapter::didNavigate();
