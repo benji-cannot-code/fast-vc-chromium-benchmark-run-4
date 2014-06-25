e37da7a1c44f82ca51b44732da49ec5037e58454
yosin@chromium.org <yosin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-25 00:37:28 +0000

    Make document.caretRangeFromPoint to return correct value for ":before" pseudo element
    This patch introduces |HitTestResult::position()| to utilize pseudo element
    information in |HitTestResult| and considering margin for making
    |document.caretRangeFromPoint| to return correct value for point at ":before"
    pseudo element.
    
    This patch is a part of fixing issue 269556, broken text selection ":before"
    pseudo-element with negative margin-left and display inline-block. Following
    patch will replace |HitTestResult::renderer()|+|RenderObject::positionForPoint|
    pair with |HitTestResult::position()|.
    
    BUG=269556
    TEST=LayoutTests/fast/dom/Document/CaretRangeFromPoint/caretRangeFromPoint-with-before-style.html
    
    Review URL: https://codereview.chromium.org/331313008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176891 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/HitTestResult.h
+++ third_party/WebKit/Source/core/rendering/HitTestResult.h
@@ -44,6 +44,7 @@ class Image;
 class KURL;
 class Node;
 class RenderObject;
+class PositionWithAffinity;
 class Scrollbar;
 
 class HitTestResult {
@@ -85,6 +86,7 @@ public:
     const LayoutPoint& localPoint() const { return m_localPoint; }
     void setLocalPoint(const LayoutPoint& p) { m_localPoint = p; }
 
+    PositionWithAffinity position() const;
     RenderObject* renderer() const;
 
     void setToNodesInDocumentTreeScope();
