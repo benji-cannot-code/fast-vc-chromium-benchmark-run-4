17ad0249d1e4b9b5a6afb0a51baa257d18b35205
sorin@chromium.org <sorin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-25 23:25:26 +0000

    Move the component_updater::Configurator to its own module.
    The idea is to reduce the coupling of smaller concrete classes on
    the definition of the ComponentUpdaterService.
    
    BUG=371463
    
    Review URL: https://codereview.chromium.org/357823002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279853 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/component_updater/component_updater_configurator.cc
+++ chrome/browser/component_updater/component_updater_configurator.cc
@@ -12,11 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/compiler_specific.h"
 #include "base/strings/string_util.h"
-#include "base/win/windows_version.h"
 #include "build/build_config.h"
-#include "chrome/browser/component_updater/component_patcher.h"
 #include "chrome/common/chrome_switches.h"
 #include "net/url_request/url_request_context_getter.h"
+#include "url/gurl.h"
 
 namespace component_updater {
 
