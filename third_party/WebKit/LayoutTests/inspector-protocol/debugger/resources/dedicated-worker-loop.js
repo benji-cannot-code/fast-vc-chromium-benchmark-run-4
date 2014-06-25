af9e3597e18862480890406bb271d706248df348
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-25 13:55:53 +0000

    Test that DevTools commands can be handled on worker thread when it is in a tight loop
    The new test checks that when worker thread is blocked on a tight loop in JavaScript it can be interrupted to handle pending DevTools commands.
    
    BUG=340522
    
    Review URL: https://codereview.chromium.org/352103002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176916 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/inspector-protocol/debugger/resources/dedicated-worker-loop.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+var message_id = 1;
+onmessage = function(event) {
+  doWork();
+};
+
+function doWork() {
+  postMessage("Message #" + message_id++);
+
+  var ts = Date.now();
+  while (true) {
+    if (Date.now() - ts > 1000) {
+        ts = Date.now();
+        postMessage("Message #" + message_id++);
+    }
+  }
+}
+
