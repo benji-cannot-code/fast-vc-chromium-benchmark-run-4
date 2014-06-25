6bace7aef144d0b3dc8803110a78e0b4257c9d52
sievers@chromium.org <sievers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-25 20:32:00 +0000

    gpu: Always use EGL_KHR_fence_sync with mailbox sync
    This is the only GLFence implementation that works across
    contexts and share groups. This codepath is only used by
    Android WebView.
    
    Review URL: https://codereview.chromium.org/342983006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279800 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/texture_definition.h
+++ gpu/command_buffer/service/texture_definition.h
@@ -6,19 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef GPU_COMMAND_BUFFER_SERVICE_TEXTURE_DEFINITION_H_
 #define GPU_COMMAND_BUFFER_SERVICE_TEXTURE_DEFINITION_H_
 
-#include <list>
 #include <vector>
 
-#include "base/callback.h"
-#include "base/memory/linked_ptr.h"
 #include "base/memory/ref_counted.h"
-#include "base/memory/scoped_ptr.h"
-#include "base/synchronization/lock.h"
 #include "gpu/command_buffer/service/gl_utils.h"
-#include "ui/gl/gl_fence.h"
 
 namespace gfx {
-class GLFence;
 class GLImage;
 }
 
