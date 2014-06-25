194ff8f1f93d144146e7c5c78bda8c17cb81059b
mostynb@opera.com <mostynb@opera.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-25 23:43:06 +0000

    socket_dispatcher_host.h uses webrtc, skip when webrtc is disabled
    https://codereview.chromium.org/264793017 added some webrtc specific
    code to socket_dispatcher_host.h, and it only appears to be used for
    webrtc as far as I can see.  So we should only include this file
    elsewhere when webrtc is enabled.
    
    BUG=363459
    TEST=build with enable_webrtc=0
    
    Review URL: https://codereview.chromium.org/345823003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279857 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -86,7 +86,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/media/peer_connection_tracker_host.h"
 #include "content/browser/renderer_host/media/video_capture_host.h"
 #include "content/browser/renderer_host/memory_benchmark_message_filter.h"
-#include "content/browser/renderer_host/p2p/socket_dispatcher_host.h"
 #include "content/browser/renderer_host/pepper/pepper_message_filter.h"
 #include "content/browser/renderer_host/pepper/pepper_renderer_connection.h"
 #include "content/browser/renderer_host/render_message_filter.h"
@@ -174,6 +173,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/media/webrtc_internals.h"
 #include "content/browser/renderer_host/media/media_stream_track_metrics_host.h"
 #include "content/browser/renderer_host/media/webrtc_identity_service_host.h"
+#include "content/browser/renderer_host/p2p/socket_dispatcher_host.h"
 #include "content/common/media/aec_dump_messages.h"
 #include "content/common/media/media_stream_messages.h"
 #endif
