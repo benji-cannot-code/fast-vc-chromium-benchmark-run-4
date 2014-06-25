823c918b09574076166fae8d45161a8ec495cbfa
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-25 12:51:37 +0000

    Files.app: On showing size column, detect a hosted doc with precise way
    Previously, we detected hosted docs only by file-size and extension and it causes the issue described in crbug.com/375575#7. This patch prevents the issue.
    
    BUG=387522, 375575
    TEST=manually tested
    
    Review URL: https://codereview.chromium.org/331863012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279697 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/file_manager/file_manager/foreground/js/file_manager.js
+++ ui/file_manager/file_manager/foreground/js/file_manager.js
@@ -801,7 +801,8 @@ var BOTTOM_MARGIN_FOR_PREVIEW_PANEL_PX = 52;
     this.showSpinner_(true);
 
     var fullPage = this.dialogType == DialogType.FULL_PAGE;
-    FileTable.decorate(this.table_, this.metadataCache_, fullPage);
+    FileTable.decorate(
+        this.table_, this.metadataCache_, this.volumeManager_, fullPage);
     FileGrid.decorate(this.grid_, this.metadataCache_, this.volumeManager_);
 
     this.previewPanel_ = new PreviewPanel(
@@ -1707,7 +1708,9 @@ var BOTTOM_MARGIN_FOR_PREVIEW_PANEL_PX = 52;
     this.metadataCache_.get(
         entries,
         'filesystem',
-        this.updateMetadataInUI_.bind(this, 'filesystem', entries));
+        function() {
+          this.updateMetadataInUI_('filesystem', entries);
+        }.bind(this));
 
     setTimeout(this.dailyUpdateModificationTime_.bind(this),
                MILLISECONDS_IN_DAY);
@@ -1716,16 +1719,13 @@ var BOTTOM_MARGIN_FOR_PREVIEW_PANEL_PX = 52;
   /**
    * @param {string} type Type of metadata changed.
    * @param {Array.<Entry>} entries Array of entries.
-   * @param {Object.<string, Object>} props Map from entry URLs to metadata
-   *     props.
    * @private
    */
-  FileManager.prototype.updateMetadataInUI_ = function(
-      type, entries, properties) {
+  FileManager.prototype.updateMetadataInUI_ = function(type, entries) {
     if (this.listType_ == FileManager.ListType.DETAIL)
-      this.table_.updateListItemsMetadata(type, properties);
+      this.table_.updateListItemsMetadata(type, entries);
     else
-      this.grid_.updateListItemsMetadata(type, properties);
+      this.grid_.updateListItemsMetadata(type, entries);
     // TODO: update bottom panel thumbnails.
   };
 
