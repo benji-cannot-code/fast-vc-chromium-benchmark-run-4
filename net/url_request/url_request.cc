04203e93fc3919e5c7ce2eb63cb410dd532953fa
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-25 06:43:01 +0000

    Remove Net.URLRequest_SetReferrer_IsEmptyOrValid histogram.
    According to internal stats, this was the top 5 histogram
    that's taking up space in UMA and it was never documented
    in histograms.xml, which means no one has ever looked at
    its data. Delete it so that it stop taking resources without
    providing value.
    
    Document it in histograms.xml so that previous data can
    still be viewed on the dashboard, if needed.
    
    BUG=387734
    
    Review URL: https://codereview.chromium.org/331343003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279639 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request.cc
+++ net/url_request/url_request.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/lazy_instance.h"
 #include "base/memory/singleton.h"
 #include "base/message_loop/message_loop.h"
-#include "base/metrics/histogram.h"
 #include "base/metrics/stats_counters.h"
 #include "base/metrics/user_metrics.h"
 #include "base/stl_util.h"
@@ -596,8 +595,6 @@ std::string URLRequest::ComputeMethodForRedirect(
 void URLRequest::SetReferrer(const std::string& referrer) {
   DCHECK(!is_pending_);
   GURL referrer_url(referrer);
-  UMA_HISTOGRAM_BOOLEAN("Net.URLRequest_SetReferrer_IsEmptyOrValid",
-                        referrer_url.is_empty() || referrer_url.is_valid());
   if (referrer_url.is_valid()) {
     referrer_ = referrer_url.GetAsReferrer().spec();
   } else {
