ef67e5554f651af7b48c5c69d26c0669f139fdaa
jincheol.jo@navercorp.com <jincheol.jo@navercorp.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-25 05:28:41 +0000

    Fix Lint error message "Single-argument constructors should be marked explicit" referred  https://codereview.chromium.org/lint_patch/issue196283013_240001_250004
    Review URL: https://codereview.chromium.org/335783002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279611 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/test/content_browser_test_utils.cc
+++ content/public/test/content_browser_test_utils.cc
@@ -95,7 +95,7 @@ void ShellAddedObserver::ShellCreated(Shell* shell) {
 
 class RenderViewCreatedObserver : public WebContentsObserver {
  public:
-  RenderViewCreatedObserver(WebContents* web_contents)
+  explicit RenderViewCreatedObserver(WebContents* web_contents)
       : WebContentsObserver(web_contents),
         render_view_created_called_(false) {
   }
