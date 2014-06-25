d87cdf73f122a424c1c7e589d117190c02b89896
miguelg@chromium.org <miguelg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-25 17:44:45 +0000

    Manual revert with a fix for the failing test.
    Revert 279437 "Implement a permission check for push."
    
        Suspect this is cause of test failure:
        http://build.chromium.org/p/chromium.mac/builders/Mac%2010.6%20Tests%20%28dbg%29%283%29/builds/50906/steps/browser_tests/logs/Init
    
        > Implement a permission check for push.
        >
        > Extracted common functionality from the midi permission check.
        > A subsequent CL will move the common classes to chrome/browser/content_settings/
        > and implement the midi permission check in terms of these classes.
        >
        > Note that bubbles are not yet implemented so only infobars work now.
        > Note also that only way to rever the permission decision right now
        > is to clear browser settings.
        >
        > Review URL: https://codereview.chromium.org/343743004
    
    BUG=350378
    TBR=bauerb,jam,fgorski,mvanouwerker
    
    Review URL: https://codereview.chromium.org/352063003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279753 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/content_settings/content_settings_default_provider.cc
+++ chrome/browser/content_settings/content_settings_default_provider.cc
@@ -52,6 +52,7 @@ const ContentSetting kDefaultSettings[] = {
   CONTENT_SETTING_ASK,      // CONTENT_SETTINGS_TYPE_PPAPI_BROKER
   CONTENT_SETTING_ASK,      // CONTENT_SETTINGS_TYPE_AUTOMATIC_DOWNLOADS
   CONTENT_SETTING_ASK,      // CONTENT_SETTINGS_TYPE_MIDI_SYSEX
+  CONTENT_SETTING_ASK,      // CONTENT_SETTINGS_TYPE_PUSH_MESSAGING
 #if defined(OS_WIN)
   CONTENT_SETTING_ASK,      // CONTENT_SETTINGS_TYPE_METRO_SWITCH_TO_DESKTOP
 #elif defined(OS_ANDROID) || defined(OS_CHROMEOS)
@@ -169,6 +170,8 @@ DefaultProvider::DefaultProvider(PrefService* prefs, bool incognito)
       ValueToContentSetting(
           default_settings_[CONTENT_SETTINGS_TYPE_MIDI_SYSEX].get()),
       CONTENT_SETTING_NUM_SETTINGS);
+
+  // TODO(miguelg): Add UMA for Push messaging here
 
   pref_change_registrar_.Init(prefs_);
   PrefChangeRegistrar::NamedChangeCallback callback = base::Bind(
--- chrome/browser/content_settings/permission_queue_controller.cc
+++ chrome/browser/content_settings/permission_queue_controller.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/infobars/infobar_service.h"
 #include "chrome/browser/media/midi_permission_infobar_delegate.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/services/gcm/push_messaging_infobar_delegate.h"
 #include "chrome/browser/tab_contents/tab_util.h"
 #include "chrome/common/content_settings.h"
 #include "chrome/common/pref_names.h"
@@ -115,6 +116,11 @@ void PermissionQueueController::PendingInfobarRequest::CreateInfoBar(
       break;
     case CONTENT_SETTINGS_TYPE_MIDI_SYSEX:
       infobar_ = MidiPermissionInfoBarDelegate::Create(
+          GetInfoBarService(id_), controller, id_, requesting_frame_,
+          display_languages);
+      break;
+    case CONTENT_SETTINGS_TYPE_PUSH_MESSAGING:
+      infobar_ = gcm::PushMessagingInfoBarDelegate::Create(
           GetInfoBarService(id_), controller, id_, requesting_frame_,
           display_languages);
       break;
