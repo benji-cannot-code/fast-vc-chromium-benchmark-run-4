b423068f1ba8a6ddc6c91ab435dfbf187ee62404
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-25 05:26:30 +0000

    [rAC] Thwart clickjacking in autofill popups
    Judicious movement of the autofill dialog could have theoretically
    intercepted the second click of a double click, leaking form info.
    
    BUG=369848
    
    Review URL: https://codereview.chromium.org/336513007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279610 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/autofill/autofill_popup_base_view_cocoa.mm
+++ chrome/browser/ui/cocoa/autofill/autofill_popup_base_view_cocoa.mm
@@ -94,6 +94,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (!delegate_)
     return;
 
+  // Only accept single-click.
+  if ([theEvent clickCount] > 1)
+    return;
+
   NSPoint location = [self convertPoint:[theEvent locationInWindow]
                                fromView:nil];
 
