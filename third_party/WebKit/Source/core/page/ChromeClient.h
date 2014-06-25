f73cd0791dcd3b8e05fce748c0ac013a83270935
philipj@opera.com <philipj@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-25 14:12:18 +0000

    Remove forward declared NSMenu/NSResponder from ChromeClient.h
    This was added in http://trac.webkit.org/changeset/30740
    
    Neither symbol is used in ChromeClient.h.
    
    core/rendering/RenderThemeChromiumMac.h uses NSMenu, but appears to
    get the declaration/definition from elsewhere, together with a few
    other NS* symbols.
    
    Review URL: https://codereview.chromium.org/352783003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176923 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/ChromeClient.h
+++ third_party/WebKit/Source/core/page/ChromeClient.h
@@ -40,12 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/PassOwnPtr.h"
 #include "wtf/Vector.h"
 
-
-#ifndef __OBJC__
-class NSMenu;
-class NSResponder;
-#endif
-
 namespace WebCore {
 
 class AXObject;
