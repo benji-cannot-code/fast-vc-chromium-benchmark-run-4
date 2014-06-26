d46e09420ed51ac300a706000c5f0b11f6c1368d
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-26 19:58:16 +0000

    Revert of Adding WaitForIncomingMethodCall for InterfacePtr (https://codereview.chromium.org/341193006/)
    Reason for revert:
    ASan build failed with mojo in the callstack, and the implicated call ReadAllAvailableMessages was introduced in this CL.
    
    http://build.chromium.org/p/chromium.memory/builders/Linux%20ASan%20LSan%20Tests%20%282%29/builds/4229/steps/browser_tests/logs/stdio
    
    ==32447==ERROR: AddressSanitizer: heap-use-after-free on address 0x611000024ae8 at pc 0x64ea589 bp 0x7fffc974d810 sp 0x7fffc974d808
    READ of size 1 at 0x611000024ae8 thread T0 (browser_tests)
    #0 0x64ea588 in mojo::internal::Connector::ReadAllAvailableMessages() mojo/public/cpp/bindings/lib/connector.cc:165
    #1 0xe30a3ee in Run base/callback.h:441
    #2 0xe30a3ee in mojo::common::HandleWatcher::State::OnHandleReady(int) mojo/common/handle_watcher.cc:278
    #3 0x38890c8 in Run base/callback.h:441
    #4 0x38890c8 in MakeItSo base/bind_internal.h:871
    
    
    Original issue's description:
    > Adding WaitForIncomingMethodCall for InterfacePtr
    > 
    > BUG=386485
    > R=darin@chromium.org
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=280069
    
    TBR=darin@chromium.org,yzshen@chromium.org,qsr@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=386485
    
    Review URL: https://codereview.chromium.org/341993005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@280077 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/public/cpp/bindings/lib/interface_ptr_internal.h
+++ mojo/public/cpp/bindings/lib/interface_ptr_internal.h
@@ -57,11 +57,6 @@ class InterfacePtrState {
     proxy_ = proxy;
   }
 
-  bool WaitForIncomingMethodCall() {
-    assert(router_);
-    return router_->WaitForIncomingMessage();
-  }
-
   void set_client(typename Interface::Client* client) {
     assert(proxy_);
     proxy_->stub.set_sink(client);
