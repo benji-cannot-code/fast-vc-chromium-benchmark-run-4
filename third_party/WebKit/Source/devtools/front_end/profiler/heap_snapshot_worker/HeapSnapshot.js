832121dc902447b2563a251effc39c91538d9aad
alph@chromium.org <alph@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-26 12:34:34 +0000

    DevTools: Refactor heap snapshot showHiddenData
    Also make class aggregates builder and class nodes provider use the same filter.
    
    Review URL: https://codereview.chromium.org/350463003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176981 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/profiler/heap_snapshot_worker/HeapSnapshot.js
+++ third_party/WebKit/Source/devtools/front_end/profiler/heap_snapshot_worker/HeapSnapshot.js
@@ -815,10 +815,9 @@ WebInspector.HeapSnapshotProgress.prototype = {
 /**
  * @param {!Object} profile
  * @param {!WebInspector.HeapSnapshotProgress} progress
- * @param {boolean} showHiddenData
  * @constructor
  */
-WebInspector.HeapSnapshot = function(profile, progress, showHiddenData)
+WebInspector.HeapSnapshot = function(profile, progress)
 {
     this._nodes = profile.nodes;
     this._containmentEdges = profile.edges;
@@ -836,7 +835,6 @@ WebInspector.HeapSnapshot = function(profile, progress, showHiddenData)
     this._aggregatesForDiff = null;
     this._aggregates = {};
     this._aggregatesSortedFlags = {};
-    this._showHiddenData = showHiddenData;
 
     this._init();
 
