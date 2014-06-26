f8af70ecf0b72eef14b47e12d01852eb84ef534e
malch@chromium.org <malch@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-26 21:30:37 +0000

    Fix incorrect constructor in PaintProfilerView.
    Review URL: https://codereview.chromium.org/355023002
    
    Review URL: https://codereview.chromium.org/355023002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177024 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/timeline/PaintProfilerView.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/PaintProfilerView.js
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 WebInspector.PaintProfilerView = function(showImageCallback)
 {
-    WebInspector.View.call(this);
+    WebInspector.HBox.call(this);
     this.element.classList.add("paint-profiler-view");
 
     this._showImageCallback = showImageCallback;
