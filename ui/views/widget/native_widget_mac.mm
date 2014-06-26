69819b80b068ce851273dd90e91eb209c6b24e11
tapted@chromium.org <tapted@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-26 10:25:56 +0000

    Revert of MacViews: Implement Set/Get*Bounds for NativeWidgetMac (https://codereview.chromium.org/353643002/)
    Reason for revert:
    Breaks on 64-bit mac (doh). NSRect is declared differently, so a forward declare won't work.
    
    Original issue's description:
    > MacViews: Implement Set/Get*Bounds for NativeWidgetMac.
    > 
    > In toolkit-views, "Bounds" corresponds to the NSWindow frame (including
    > titlebar and borders). This CL maps that onto NSWindow in
    > InitNativeWidget, GetWindowBoundsInScreen, GetClientAreaBoundsInScreen,
    > SetBounds and SetSize, performing the required coordinate system
    > conversions.
    > 
    > 5 more views_unittests pass after this:
    > BubbleDelegateTest.NonClientHitTest
    > CustomFrameViewTest.{CannotMaximizeHidesButton, DefaultButtonLayout,
    > LeadingButtonLayout}, ViewTest.ConversionsToFromScreen.
    > 
    > The CL also adds a cross-platform WidgetTest.GetWindowBoundsInScreen to
    > ensure the behavior is consistent across platforms, and because there
    > wasn't really anything to cover just bounds setting (e.g. to verify the
    > coordinate flipping Cocoa needs to do).
    > 
    > BUG=378134
    > TEST=views_unittests
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=279975
    
    TBR=rsesek@chromium.org,sky@chromium.org,andresantoso@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=378134
    
    Review URL: https://codereview.chromium.org/351603004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279976 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/native_widget_mac.mm
+++ ui/views/widget/native_widget_mac.mm
@@ -10,38 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/foundation_util.h"
 #include "base/mac/scoped_nsobject.h"
 #include "ui/gfx/font_list.h"
-#import "ui/gfx/mac/coordinate_conversion.h"
 #include "ui/native_theme/native_theme.h"
 #import "ui/views/cocoa/bridged_content_view.h"
 #import "ui/views/cocoa/bridged_native_widget.h"
 #import "ui/views/cocoa/views_nswindow_delegate.h"
 
 namespace views {
-namespace {
-
-NSInteger StyleMaskForParams(const Widget::InitParams& params) {
-  // TODO(tapted): Determine better masks when there are use cases for it.
-  if (params.type == Widget::InitParams::TYPE_WINDOW) {
-    return NSTitledWindowMask | NSClosableWindowMask |
-           NSMiniaturizableWindowMask | NSResizableWindowMask;
-  }
-  return NSBorderlessWindowMask;
-}
-
-NSRect ValidateContentRect(NSRect content_rect) {
-  // A contentRect with zero width or height is a banned practice in Chrome, due
-  // to unpredictable OSX treatment. For now, silently give a minimum dimension.
-  // TODO(tapted): Add a DCHECK, or add emulation logic (e.g. to auto-hide).
-  if (NSWidth(content_rect) == 0)
-    content_rect.size.width = 1;
-
-  if (NSHeight(content_rect) == 0)
-    content_rect.size.height = 1;
-
-  return content_rect;
-}
-
-}  // namespace
 
 ////////////////////////////////////////////////////////////////////////////////
 // NativeWidgetMac, public:
@@ -75,11 +49,12 @@ void NativeWidgetMac::OnWindowWillClose() {
 void NativeWidgetMac::InitNativeWidget(const Widget::InitParams& params) {
   ownership_ = params.ownership;
 
-  NSInteger style_mask = StyleMaskForParams(params);
-  NSRect content_rect = ValidateContentRect(
-      [NSWindow contentRectForFrameRect:gfx::ScreenRectToNSRect(params.bounds)
-                              styleMask:style_mask]);
-
+  // TODO(tapted): Convert position into Cocoa's flipped coordinate space.
+  NSRect content_rect =
+      NSMakeRect(0, 0, params.bounds.width(), params.bounds.height());
+  // TODO(tapted): Determine a good initial style mask from |params|.
+  NSInteger style_mask = NSTitledWindowMask | NSClosableWindowMask |
+                         NSMiniaturizableWindowMask | NSResizableWindowMask;
   base::scoped_nsobject<NSWindow> window(
       [[NSWindow alloc] initWithContentRect:content_rect
                                   styleMask:style_mask
@@ -219,13 +194,13 @@ void NativeWidgetMac::InitModalType(ui::ModalType modal_type) {
 }
 
 gfx::Rect NativeWidgetMac::GetWindowBoundsInScreen() const {
-  return gfx::ScreenRectFromNSRect([GetNativeWindow() frame]);
+  NOTIMPLEMENTED();
+  return gfx::Rect();
 }
 
 gfx::Rect NativeWidgetMac::GetClientAreaBoundsInScreen() const {
-  NSWindow* window = GetNativeWindow();
-  return gfx::ScreenRectFromNSRect(
-      [window contentRectForFrameRect:[window frame]]);
+  NOTIMPLEMENTED();
+  return gfx::Rect();
 }
 
 gfx::Rect NativeWidgetMac::GetRestoredBounds() const {
@@ -234,15 +209,11 @@ gfx::Rect NativeWidgetMac::GetRestoredBounds() const {
 }
 
 void NativeWidgetMac::SetBounds(const gfx::Rect& bounds) {
-  [GetNativeWindow() setFrame:gfx::ScreenRectToNSRect(bounds)
-                      display:YES
-                      animate:NO];
+  NOTIMPLEMENTED();
 }
 
 void NativeWidgetMac::SetSize(const gfx::Size& size) {
-  // Ensure the top-left corner stays in-place (rather than the bottom-left,
-  // which -[NSWindow setContentSize:] would do).
-  SetBounds(gfx::Rect(GetWindowBoundsInScreen().origin(), size));
+  [GetNativeWindow() setContentSize:NSMakeSize(size.width(), size.height())];
 }
 
 void NativeWidgetMac::StackAbove(gfx::NativeView native_view) {
@@ -262,13 +233,10 @@ void NativeWidgetMac::SetShape(gfx::NativeRegion shape) {
 }
 
 void NativeWidgetMac::Close() {
-  NSWindow* window = GetNativeWindow();
-  // Calling performClose: will momentarily highlight the close button, but
-  // AppKit will reject it if there is no close button.
-  SEL close_selector = ([window styleMask] & NSClosableWindowMask)
-                           ? @selector(performClose:)
-                           : @selector(close);
-  [window performSelector:close_selector withObject:nil afterDelay:0];
+  // Calling performClose: will momentarily highlight the close button.
+  [GetNativeWindow() performSelector:@selector(performClose:)
+                          withObject:nil
+                          afterDelay:0];
 }
 
 void NativeWidgetMac::CloseNow() {
