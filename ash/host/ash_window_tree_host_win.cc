25e772215c88dc262d8df88734e15d94e974fa4d
hans@chromium.org <hans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-26 17:35:36 +0000

    AshWindowTreeHostWin is an implementation detail in an anonymous namespace and should not be exported
    BUG=82385
    
    Review URL: https://codereview.chromium.org/359563002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@280037 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/host/ash_window_tree_host_win.cc
+++ ash/host/ash_window_tree_host_win.cc
@@ -20,8 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 namespace {
 
-class ASH_EXPORT AshWindowTreeHostWin : public AshWindowTreeHost,
-                                        public aura::WindowTreeHostWin {
+class AshWindowTreeHostWin : public AshWindowTreeHost,
+                             public aura::WindowTreeHostWin {
  public:
   explicit AshWindowTreeHostWin(const gfx::Rect& initial_bounds)
       : aura::WindowTreeHostWin(initial_bounds),
