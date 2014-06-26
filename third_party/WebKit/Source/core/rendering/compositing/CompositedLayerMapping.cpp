c4e3019e1a99e84dbcb1ef13238307afe2814581
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-26 07:20:42 +0000

    ClipRectsCache never uses 1/3 of its slots
    Previously, ClipRectsCache allocated two slots for each type of cached clip
    rects, but the only clients that wanted to ignore overflow clip were those that
    cared about PaintingClipRects. Rather than using two enums and dodgy math, this
    CL makes the slots in the cache explicit.
    
    In this CL, I've also reworked the ClipRectsCache and ClipRectsContext API to
    be less baroque and to avoid doing extra work when not necessary.
    
    This CL makes the compositing update for Polymer's calculator 2% faster.
    
    Review URL: https://codereview.chromium.org/357443009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176969 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/compositing/CompositedLayerMapping.cpp
+++ third_party/WebKit/Source/core/rendering/compositing/CompositedLayerMapping.cpp
@@ -726,7 +726,8 @@ void CompositedLayerMapping::updateAncestorClippingLayerGeometry(const RenderLay
 
     // FIXME: this should use cached clip rects, but this sometimes give
     // inaccurate results (and trips the ASSERTS in RenderLayerClipper).
-    ClipRectsContext clipRectsContext(compositingContainer, TemporaryClipRects, IgnoreOverlayScrollbarSize, IgnoreOverflowClip);
+    ClipRectsContext clipRectsContext(compositingContainer, UncachedClipRects, IgnoreOverlayScrollbarSize);
+    clipRectsContext.setIgnoreOverflowClip();
     IntRect parentClipRect = pixelSnappedIntRect(m_owningLayer.clipper().backgroundClipRect(clipRectsContext).rect());
     ASSERT(parentClipRect != PaintInfo::infiniteRect());
     m_ancestorClippingLayer->setPosition(FloatPoint(parentClipRect.location() - graphicsLayerParentLocation));
@@ -1936,7 +1937,7 @@ IntRect CompositedLayerMapping::localClipRectForSquashedLayer(const RenderLayer&
     ASSERT(ancestorPaintInfo);
 
     // FIXME: this is a potential performance issue. We shoudl consider caching these clip rects or otherwise optimizing.
-    ClipRectsContext clipRectsContext(ancestorPaintInfo->renderLayer, TemporaryClipRects);
+    ClipRectsContext clipRectsContext(ancestorPaintInfo->renderLayer, UncachedClipRects);
     IntRect parentClipRect = pixelSnappedIntRect(paintInfo.renderLayer->clipper().backgroundClipRect(clipRectsContext).rect());
     ASSERT(parentClipRect != PaintInfo::infiniteRect());
 
