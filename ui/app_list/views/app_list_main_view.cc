41b3fdf9c0193fa820376b5d2020dcce8abba0d5
calamity@chromium.org <calamity@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-26 07:04:05 +0000

    Make app list search box always be part of layout.
    In order to prevent the experimental app list from laying out incorrectly,
    this CL changes the search box view to be inside a container view which
    takes its preferred size. The container view is always visible so it is
    laid out even if the search box view is not visible.
    
    BUG=386912
    
    Review URL: https://codereview.chromium.org/332953003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279944 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/app_list/views/app_list_main_view.cc
+++ ui/app_list/views/app_list_main_view.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/app_list/views/search_box_view.h"
 #include "ui/views/controls/textfield/textfield.h"
 #include "ui/views/layout/box_layout.h"
+#include "ui/views/layout/fill_layout.h"
 #include "ui/views/widget/widget.h"
 
 namespace app_list {
@@ -39,6 +40,32 @@ const int kInnerPadding = 1;
 
 // The maximum allowed time to wait for icon loading in milliseconds.
 const int kMaxIconLoadingWaitTimeInMs = 50;
+
+// A view that holds another view and takes its preferred size. This is used for
+// wrapping the search box view so it still gets laid out while hidden. This is
+// a separate class so it can notify the main view on search box visibility
+// change.
+class SearchBoxContainerView : public views::View {
+ public:
+  SearchBoxContainerView(AppListMainView* host, SearchBoxView* search_box)
+      : host_(host), search_box_(search_box) {
+    SetLayoutManager(new views::FillLayout());
+    AddChildView(search_box);
+  }
+  virtual ~SearchBoxContainerView() {}
+
+ private:
+  // Overridden from views::View:
+  virtual void ChildVisibilityChanged(views::View* child) OVERRIDE {
+    DCHECK_EQ(search_box_, child);
+    host_->NotifySearchBoxVisibilityChanged();
+  }
+
+  AppListMainView* host_;
+  SearchBoxView* search_box_;
+
+  DISALLOW_COPY_AND_ASSIGN(SearchBoxContainerView);
+};
 
 }  // namespace
 
@@ -97,7 +124,7 @@ AppListMainView::AppListMainView(AppListViewDelegate* delegate,
                                         kInnerPadding));
 
   search_box_view_ = new SearchBoxView(this, delegate);
-  AddChildView(search_box_view_);
+  AddChildView(new SearchBoxContainerView(this, search_box_view_));
   AddContentsViews();
 
   // Switch the apps grid view to the specified page.
@@ -254,10 +281,11 @@ void AppListMainView::OnItemIconLoaded(IconLoader* loader) {
   }
 }
 
-void AppListMainView::ChildVisibilityChanged(views::View* child) {
+void AppListMainView::NotifySearchBoxVisibilityChanged() {
   // Repaint the AppListView's background which will repaint the background for
-  // the search box.
-  if (child == search_box_view_ && parent())
+  // the search box. This is needed because this view paints to a layer and
+  // won't propagate paints upward.
+  if (parent())
     parent()->SchedulePaint();
 }
 
