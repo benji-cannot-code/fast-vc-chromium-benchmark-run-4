7dfbd846fffd559e51e7f044e82f71414f9a91a8
sigbjornf@opera.com <sigbjornf@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-26 06:59:43 +0000

    Oilpan: make shutdown of plugin container objects work better.
    The plugin container object/widget that's kept by the plugin element has
    a destructor that accesses its element's document in a couple of ways:
    
     - Optionally notifying that a touch event handler has been removed.
     - Tries to notify element (and its document) that compositing update
       is needed.
     - Indirectly clears out the script objects registered on the document
       frame's ScriptController when destroying the container's plugin object.
    
    With Oilpan, none of these can be done during finalization as the document
    cannot be assumed to be alive.
    
    To address, introduce an explicit detach() step for when the element is
    cleanly removed from the document (along with its container.) Otherwise,
    the plugin container is assumed to go away at the same time as the
    element (and the document.) The container's destructor will then only
    notify any off-heap objects.
    
    R=tkent@chromium.org,haraken@chromium.org
    BUG=340522
    
    Review URL: https://codereview.chromium.org/357443005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176967 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLPlugInElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLPlugInElement.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/events/Event.h"
 #include "core/frame/FrameView.h"
 #include "core/frame/LocalFrame.h"
+#include "core/frame/Settings.h"
 #include "core/frame/csp/ContentSecurityPolicy.h"
 #include "core/html/HTMLContentElement.h"
 #include "core/html/HTMLImageLoader.h"
@@ -41,7 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/loader/FrameLoaderClient.h"
 #include "core/page/EventHandler.h"
 #include "core/page/Page.h"
-#include "core/frame/Settings.h"
 #include "core/plugins/PluginView.h"
 #include "core/rendering/RenderEmbeddedObject.h"
 #include "core/rendering/RenderImage.h"
@@ -202,6 +202,10 @@ void HTMLPlugInElement::detach(const AttachContext& context)
     Widget* plugin = ownedWidget();
     if (plugin && plugin->pluginShouldPersist())
         m_persistedPluginWidget = plugin;
+#if ENABLE(OILPAN)
+    else if (plugin)
+        plugin->detach();
+#endif
     resetInstance();
     // FIXME - is this next line necessary?
     setWidget(nullptr);
--- third_party/WebKit/Source/web/WebPluginContainerImpl.h
+++ third_party/WebKit/Source/web/WebPluginContainerImpl.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WebPluginContainerImpl_h
 #define WebPluginContainerImpl_h
 
+#include "core/frame/FrameDestructionObserver.h"
 #include "core/plugins/PluginView.h"
 #include "platform/Widget.h"
 #include "public/web/WebPluginContainer.h"
@@ -66,7 +67,7 @@ class WebPlugin;
 class WebPluginLoadObserver;
 class WebExternalTextureLayer;
 
-class WebPluginContainerImpl FINAL : public WebCore::PluginView, public WebPluginContainer {
+class WebPluginContainerImpl FINAL : public WebCore::PluginView, public WebPluginContainer, public WebCore::FrameDestructionObserver {
 public:
     static PassRefPtr<WebPluginContainerImpl> create(WebCore::HTMLPlugInElement* element, WebPlugin* webPlugin)
     {
@@ -163,6 +164,10 @@ public:
 
     bool paintCustomOverhangArea(WebCore::GraphicsContext*, const WebCore::IntRect&, const WebCore::IntRect&, const WebCore::IntRect&);
 
+#if ENABLE(OILPAN)
+    virtual void detach() OVERRIDE;
+#endif
+
 private:
     WebPluginContainerImpl(WebCore::HTMLPlugInElement* element, WebPlugin* webPlugin);
     virtual ~WebPluginContainerImpl();
@@ -186,6 +191,9 @@ private:
     void windowCutOutRects(const WebCore::IntRect& frameRect,
                            Vector<WebCore::IntRect>& cutOutRects);
 
+    // FIXME: Oilpan: consider moving Widget to the heap and turn this
+    // into a traced member. For the time being, it is a bare pointer
+    // of its owning PlugInElement and managed as such.
     WebCore::HTMLPlugInElement* m_element;
     WebPlugin* m_webPlugin;
     Vector<WebPluginLoadObserver*> m_pluginLoadObservers;
