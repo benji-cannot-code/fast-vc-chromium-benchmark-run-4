537f8f1ecdca82f49fad712decca10c9a73f0ea6
xrenishere@gmail.com <xrenishere@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-26 15:29:04 +0000

    Fix a DCHECK() crash in PluginPlaceholder.
      Change ASCIIToUTF16 to UTF8ToUTF16. messge_ is string16.
      It might contain non ascii.
    
    BUG=389004
    
    Review URL: https://codereview.chromium.org/350963003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@280013 0039d316-1c4b-4281-b951-d872f2087c98

--- components/plugins/renderer/plugin_placeholder.cc
+++ components/plugins/renderer/plugin_placeholder.cc
@@ -164,7 +164,7 @@ void PluginPlaceholder::UpdateMessage() {
   std::string script =
       "window.setMessage(" + base::GetQuotedJSONString(message_) + ")";
   plugin_->web_view()->mainFrame()->executeScript(
-      WebScriptSource(base::ASCIIToUTF16(script)));
+      WebScriptSource(base::UTF8ToUTF16(script)));
 }
 
 void PluginPlaceholder::ShowContextMenu(const WebMouseEvent& event) {
