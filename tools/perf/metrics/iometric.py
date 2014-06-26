fcb227c6541be22b2a23316b06663bf597a50601
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-26 22:13:42 +0000

    [Telemetry] Remove an unused 'platform' field from BrowserOptions.
    It doesn't belong here anyway.
    
    BUG=346956
    
    Review URL: https://codereview.chromium.org/355923002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@280128 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/metrics/iometric.py
+++ tools/perf/metrics/iometric.py
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import sys
+
 from metrics import Metric
 from telemetry.value import scalar
 
@@ -12,8 +14,9 @@ class IOMetric(Metric):
 
   @classmethod
   def CustomizeBrowserOptions(cls, options):
-    if options.platform.GetOSName() != 'mac':
-      # FIXME: Get rid of this on all platforms - http://crbug.com/361049 .
+    # TODO(tonyg): This is the host platform, so not totally correct.
+    if sys.platform != 'darwin':
+      # TODO(playmobil): Get rid of this on all platforms crbug.com/361049.
       options.AppendExtraBrowserArgs('--no-sandbox')
 
   def Start(self, page, tab):
