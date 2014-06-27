861d3a8014770274572d3294ab211d8d4ba030c8
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-27 04:17:38 +0000

    Mojo: Add a cross-app nav test to the window manager.
    BUG=
    R=ben@chromium.org, sky@chromium.org
    
    Review URL: https://codereview.chromium.org/354083002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@280236 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/examples/embedded_app/embedded_app.cc
+++ mojo/examples/embedded_app/embedded_app.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
-#include "mojo/examples/window_manager/window_manager.mojom.h"
 #include "mojo/public/cpp/application/application_connection.h"
 #include "mojo/public/cpp/application/application_delegate.h"
 #include "mojo/public/cpp/application/application_impl.h"
@@ -79,7 +78,6 @@ class EmbeddedApp : public ApplicationDelegate,
     // TODO(aa): Weird for embeddee to talk to embedder by URL. Seems like
     // embedder should be able to specify the SP embeddee receives, then
     // communication can be anonymous.
-    app->ConnectToService("mojo:mojo_window_manager", &window_manager_);
     app->ConnectToService("mojo:mojo_window_manager", &navigator_host_);
   }
 
@@ -105,11 +103,9 @@ class EmbeddedApp : public ApplicationDelegate,
   virtual void OnViewInputEvent(View* view, const EventPtr& event) OVERRIDE {
     if (event->action == ui::ET_MOUSE_RELEASED) {
       if (event->flags & ui::EF_LEFT_MOUSE_BUTTON) {
-        window_manager_->CloseWindow(view->node()->id());
-      } else if (event->flags & ui::EF_RIGHT_MOUSE_BUTTON) {
         navigation::NavigationDetailsPtr nav_details(
             navigation::NavigationDetails::New());
-        nav_details->url = "http://ranchtastic.com/s6.png";
+        nav_details->url = "http://www.aaronboodman.com/z_dropbox/test.html";
         navigator_host_->RequestNavigate(view->node()->id(),
                                          navigation::SOURCE_NODE,
                                          nav_details.Pass());
@@ -152,7 +148,6 @@ class EmbeddedApp : public ApplicationDelegate,
   }
 
   view_manager::ViewManager* view_manager_;
-  IWindowManagerPtr window_manager_;
   navigation::NavigatorHostPtr navigator_host_;
   std::map<Node*, View*> views_to_reap_;
 
