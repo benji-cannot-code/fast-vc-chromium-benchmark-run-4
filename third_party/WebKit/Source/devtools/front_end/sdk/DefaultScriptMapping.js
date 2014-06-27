fc6760cd27006175e726f7b9f6f858d30cc91e5b
sergeyv@chromium.org <sergeyv@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-27 12:34:48 +0000

    DevTools: properly support targets in LiveEditSupport
    BUG=
    
    Review URL: https://codereview.chromium.org/352953002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177096 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sdk/DefaultScriptMapping.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/DefaultScriptMapping.js
@@ -39,7 +39,7 @@ WebInspector.DefaultScriptMapping = function(debuggerModel, workspace)
 {
     this._debuggerModel = debuggerModel;
     this._workspace = workspace;
-    this._projectId = "debugger:" + debuggerModel.target().id();
+    this._projectId = WebInspector.DefaultScriptMapping.projectIdForTarget(debuggerModel.target());
     this._projectDelegate = new WebInspector.DebuggerProjectDelegate(this._workspace, this._projectId, WebInspector.projectTypes.Debugger);
     debuggerModel.addEventListener(WebInspector.DebuggerModel.Events.GlobalObjectCleared, this._debuggerReset, this);
     this._debuggerReset();
@@ -121,6 +121,15 @@ WebInspector.DefaultScriptMapping.prototype = {
     {
         this._workspace.removeProject(this._projectId);
     }
+}
+
+/**
+ * @param {!WebInspector.Target} target
+ * @return {string}
+ */
+WebInspector.DefaultScriptMapping.projectIdForTarget = function(target)
+{
+    return "debugger:" + target.id();
 }
 
 /**
