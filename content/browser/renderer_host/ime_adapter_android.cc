30af981d3e6614deb85a97c911572779dd5a3e8d
guohui@chromium.org <guohui@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-27 21:41:07 +0000

    Changed IME messages from Frame to Input
    All IME messages should go through the same compositor, otherwise, race condition may occur.
    
    A followup of cl 324133002
    
    BUG=376273
    
    Review URL: https://codereview.chromium.org/355073003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@280416 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/ime_adapter_android.cc
+++ content/browser/renderer_host/ime_adapter_android.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/render_widget_host_impl.h"
 #include "content/browser/renderer_host/render_widget_host_view_android.h"
 #include "content/common/frame_messages.h"
+#include "content/common/input_messages.h"
 #include "content/common/view_messages.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/native_web_keyboard_event.h"
@@ -277,7 +278,7 @@ void ImeAdapterAndroid::SetComposingRegion(JNIEnv*, jobject,
   underlines.push_back(blink::WebCompositionUnderline(
       0, end - start, SK_ColorBLACK, false, SK_ColorTRANSPARENT));
 
-  rfh->Send(new FrameMsg_SetCompositionFromExistingText(
+  rfh->Send(new InputMsg_SetCompositionFromExistingText(
       rfh->GetRoutingID(), start, end, underlines));
 }
 
