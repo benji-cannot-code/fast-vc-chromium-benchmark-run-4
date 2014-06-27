c248434824e7de33996c2ed4dff836c261459305
l.gombos@samsung.com <l.gombos@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-27 16:53:14 +0000

    In general the project should avoid using OS() guards when possible.
    In this particular case both OS() and PTHREAD_KEYS_MAX are used as a guard, even though using PTHREAD_KEYS_MAX as a guard is sufficient. Not only that this solution works not just for ANDROID but also for other systems where chromium might be ported to (in or out of the tree). 
    
    As an example the proposed change would work on HURD systems as well (the support for HURD is not maintained in the tree, but it demonstrates that the change handles those systems as well).
    
    Review URL: https://codereview.chromium.org/351683007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177115 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/ThreadIdentifierDataPthreads.cpp
+++ third_party/WebKit/Source/wtf/ThreadIdentifierDataPthreads.cpp
@@ -38,14 +38,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/Assertions.h"
 #include "wtf/WTF.h"
 
-#if OS(ANDROID)
-// PTHREAD_KEYS_MAX is not defined in bionic, so explicitly define it here.
+#include <limits.h>
+
+// PTHREAD_KEYS_MAX might be not defined (e.g. in certain bionic versions), so explicitly define it here.
 #ifndef PTHREAD_KEYS_MAX
 #define PTHREAD_KEYS_MAX 1024
 #endif // PTHREAD_KEYS_MAX
-#else
-#include <limits.h>
-#endif
 
 namespace WTF {
 
