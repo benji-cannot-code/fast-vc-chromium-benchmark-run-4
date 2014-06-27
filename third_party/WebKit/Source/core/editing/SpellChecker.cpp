625be3eb297ac0a92bf214dbf226dc3ae8b7a903
yoichio@chromium.org <yoichio@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-27 08:25:00 +0000

    Remove an unused argument from SpellChecker::spellCheckOldSelection.
    I'm not sure since when, anyway |newSelectedSentence| is no longer used.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/350993002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177080 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/SpellChecker.cpp
+++ third_party/WebKit/Source/core/editing/SpellChecker.cpp
@@ -754,7 +754,7 @@ void SpellChecker::respondToChangedSelection(const VisibleSelection& oldSelectio
             && oldSelection.isContentEditable()
             && oldSelection.start().inDocument()
             && !isSelectionInTextField(oldSelection)) {
-            spellCheckOldSelection(oldSelection, newAdjacentWords, newSelectedSentence);
+            spellCheckOldSelection(oldSelection, newAdjacentWords);
         }
 
         if (textChecker().shouldEraseMarkersAfterChangeSelection(TextCheckingTypeSpelling)) {
@@ -790,10 +790,10 @@ void SpellChecker::spellCheckAfterBlur()
     }
 
     VisibleSelection empty;
-    spellCheckOldSelection(m_frame.selection().selection(), empty, empty);
+    spellCheckOldSelection(m_frame.selection().selection(), empty);
 }
 
-void SpellChecker::spellCheckOldSelection(const VisibleSelection& oldSelection, const VisibleSelection& newAdjacentWords, const VisibleSelection& newSelectedSentence)
+void SpellChecker::spellCheckOldSelection(const VisibleSelection& oldSelection, const VisibleSelection& newAdjacentWords)
 {
     VisiblePosition oldStart(oldSelection.visibleStart());
     VisibleSelection oldAdjacentWords = VisibleSelection(startOfWord(oldStart, LeftWordIfOnBoundary), endOfWord(oldStart, RightWordIfOnBoundary));
