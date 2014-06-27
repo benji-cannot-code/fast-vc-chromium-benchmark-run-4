c2065781c086dc31b0e31c2519da127e9f46d968
eseidel@chromium.org <eseidel@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-27 22:14:23 +0000

    Fix GardeningServer/scripts/ui_unittests.js
    This testing framework is horrible.  When you have a syntax error
    you have to go digging in the console to fine it, it doesn't notice.
    
    BUG=389681
    R=abarth@chromium.org, abarth
    
    Review URL: https://codereview.chromium.org/359813002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177145 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/GardeningServer/scripts/ui_unittests.js
+++ third_party/WebKit/Tools/GardeningServer/scripts/ui_unittests.js
@@ -254,9 +254,7 @@ asyncTest("RevisionDetailsSmallRoll", 2, function() {
                         '</tr>' +
                     '</table>' +
                 '</details>' +
-                ', trunk is at <a href="http://src.chromium.org/viewvc/blink?view=rev&amp;revision=555">555</a>' +
-                '<br>' +
-                'Last roll is to <a href="http://src.chromium.org/viewvc/blink?view=rev&amp;revision=540">540</a>, current autoroll <a href="https://codereview.chromium.org/1000">540:550</a>');
+                ', trunk is at <a href="http://src.chromium.org/viewvc/blink?view=rev&amp;revision=555">555</a>');
         start();
     });
 });
@@ -311,9 +309,7 @@ asyncTest("RevisionDetailsMediumRoll", 2, function() {
                         '</tr>' +
                     '</table>' +
                 '</details>' +
-                ', trunk is at <a href="http://src.chromium.org/viewvc/blink?view=rev&amp;revision=555">555</a>' +
-                '<br>';
-                // 'Last roll is to <a href="http://src.chromium.org/viewvc/blink?view=rev&amp;revision=500">500</a><span class="warning">(55 revisions behind)</span>, current autoroll <a href="https://codereview.chromium.org/1000">500:550</a>');
+                ', trunk is at <a href="http://src.chromium.org/viewvc/blink?view=rev&amp;revision=555">555</a>');
         start();
     });
 });
@@ -368,9 +364,7 @@ asyncTest("RevisionDetailsBigRoll", 2, function() {
                         '</tr>' +
                     '</table>' +
                 '</details>' +
-                ', trunk is at <a href="http://src.chromium.org/viewvc/blink?view=rev&amp;revision=555">555</a>' +
-                '<br>' +
-                'Last roll is to <a href="http://src.chromium.org/viewvc/blink?view=rev&amp;revision=440">440</a><span class="critical">(115 revisions behind)</span>, current autoroll <a href="https://codereview.chromium.org/1000">440:550</a>');
+                ', trunk is at <a href="http://src.chromium.org/viewvc/blink?view=rev&amp;revision=555">555</a>');
         start();
     });
 });
