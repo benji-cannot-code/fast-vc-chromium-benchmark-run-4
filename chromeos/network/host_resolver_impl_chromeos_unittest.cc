80aea830a92bbf0ba04bc724d5878376f7a9dec8
stevenjb@chromium.org <stevenjb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-27 22:16:27 +0000

    Reset host resolver in unittest
    BUG=386418
    
    Review URL: https://codereview.chromium.org/356933003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@280429 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/network/host_resolver_impl_chromeos_unittest.cc
+++ chromeos/network/host_resolver_impl_chromeos_unittest.cc
@@ -69,6 +69,7 @@ class HostResolverImplChromeOSTest : public testing::Test {
   }
 
   virtual void TearDown() OVERRIDE {
+    host_resolver_.reset();
     network_state_handler_.reset();
     chromeos::DBusThreadManager::Shutdown();
   }
