cbbeba1a03ba2d6c704ecb722f2c51b6c787c42c
mfomitchev@chromium.org <mfomitchev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-27 17:37:07 +0000

    Fixing flaky overscroll and touch exploration mode browser tests
    The issue is that right after the WebCOntents window is created, it gets resized. The resize message is sent to the rendeer and until the confirmation is received, all mouse moves and touch moves are "held", which affects a bunch of tests.
    
    - For in-process browser tests, ensure the resize is completed before the main   test body is executed
    - For content browser tests the solution is a bit more tricky. The WebContents   window is lazily created when the first navigation happens and for content browser tests the first navigation doesn't happen as part of the setup. So the fix for this case is to add "wait for resize" functionality to TestNavigationObserver, which is a helper class used to wait for navigations to complete.
    
    BUG=305722,357311
    
    Review URL: https://codereview.chromium.org/330113002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@280356 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ ui/aura/test/window_event_dispatcher_test_api.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/aura/test/window_event_dispatcher_test_api.h"
+
+#include "ui/aura/window_event_dispatcher.h"
+
+namespace aura {
+namespace test {
+
+WindowEventDispatcherTestApi::WindowEventDispatcherTestApi(
+    WindowEventDispatcher* dispatcher) : dispatcher_(dispatcher) {
+}
+
+bool WindowEventDispatcherTestApi::HoldingPointerMoves() const {
+  return dispatcher_->move_hold_count_ > 0;
+}
+
+}  // namespace test
+}  // namespace aura
+
--- ui/aura/window_event_dispatcher.h
+++ ui/aura/window_event_dispatcher.h
@@ -47,6 +47,10 @@ class TestScreen;
 class WindowTargeter;
 class WindowTreeHost;
 
+namespace test {
+class WindowEventDispatcherTestApi;
+}
+
 // WindowEventDispatcher orchestrates event dispatch within a window tree
 // owned by WindowTreeHost. WTH also owns the WED.
 // TODO(beng): In progress, remove functionality not directly related to
@@ -120,6 +124,7 @@ class AURA_EXPORT WindowEventDispatcher : public ui::EventProcessor,
   FRIEND_TEST_ALL_PREFIXES(WindowEventDispatcherTest,
                            KeepTranslatedEventInRoot);
 
+  friend class test::WindowEventDispatcherTestApi;
   friend class Window;
   friend class TestScreen;
 
