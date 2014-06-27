fac43f4915c6e4dfab4f7f582fba8e37d62f15c8
eseidel@chromium.org <eseidel@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-27 22:40:40 +0000

    Remove StatusArea code from GOM it's dead.
    Jim.
    
    R=abarth@chromium.org
    BUG=389681
    
    Review URL: https://codereview.chromium.org/356273002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177147 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/GardeningServer/scripts/controllers.js
+++ third_party/WebKit/Tools/GardeningServer/scripts/controllers.js
@@ -38,17 +38,6 @@ function isAnyReftest(testName, resultsByTest)
     });
 }
 
-// FIXME: Where should this function go?
-function updateExpectationsWithStatusUpdates(failureInfoList)
-{
-    var statusView = new ui.StatusArea('Expectations Update');
-    var id = statusView.newId();
-
-    var testNames = base.uniquifyArray(failureInfoList.map(function(failureInfo) { return failureInfo.testName; }));
-    var testName = testNames.length == 1 ? testNames[0] : testNames.length + ' tests';
-    statusView.addMessage(id, 'Updating expectations of ' + testName + '...');
-}
-
 controllers.ResultsDetails = base.extends(Object, {
     init: function(view, resultsByTest)
     {
@@ -60,7 +49,6 @@ controllers.ResultsDetails = base.extends(Object, {
 
         $(this._view).bind('next', this.onNext.bind(this));
         $(this._view).bind('previous', this.onPrevious.bind(this));
-        $(this._view).bind('expectfailure', this.onUpdateExpectations.bind(this));
     },
     onNext: function()
     {
@@ -77,10 +65,6 @@ controllers.ResultsDetails = base.extends(Object, {
             return results.failureInfoForTestAndBuilder(this._resultsByTest, testName, builderName);
         }.bind(this));
     },
-    onUpdateExpectations: function()
-    {
-        updateExpectationsWithStatusUpdates(this._failureInfoList());
-    }
 });
 
 var FailureStreamController = base.extends(Object, {
@@ -104,9 +88,6 @@ var FailureStreamController = base.extends(Object, {
             this._view.add(failure);
             $(failure).bind('examine', function() {
                 this.onExamine(failure);
-            }.bind(this));
-            $(failure).bind('expectfailure', function() {
-                this.onUpdateExpectations(failure);
             }.bind(this));
         }
         failure.addFailureAnalysis(failureAnalysis);
@@ -141,10 +122,6 @@ var FailureStreamController = base.extends(Object, {
     {
         return base.flattenArray(failures.testNameList().map(model.unexpectedFailureInfoForTestName));
     },
-    onUpdateExpectations: function(failures)
-    {
-        updateExpectationsWithStatusUpdates(this._toFailureInfoList(failures));
-    }
 });
 
 controllers.UnexpectedFailures = base.extends(FailureStreamController, {
