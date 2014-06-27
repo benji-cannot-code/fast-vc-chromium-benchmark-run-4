4c6b57ec6472244c2d0bae4ad6cc11a715f78da6
b.kelemen@samsung.com <b.kelemen@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-27 00:54:32 +0000

    Remove an unneccessary special case for the main resource from InspectorPageClient
    Miniature cleanup, the special case is not needed as the default: case handles
    main resources well: it returns early with false as resourceBuffer() is null.
    
    R=pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/350263002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177032 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorPageAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorPageAgent.cpp
@@ -249,8 +249,6 @@ bool InspectorPageAgent::cachedResourceContent(Resource* cachedResource, String*
         case Resource::Script:
             *result = toScriptResource(cachedResource)->script();
             return true;
-        case Resource::MainResource:
-            return false;
         case Resource::Raw: {
             SharedBuffer* buffer = cachedResource->resourceBuffer();
             if (!buffer)
