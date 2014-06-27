69b8da2b77025f7c04f7acc8d0aeaef298cdb5c1
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-27 04:00:55 +0000

    Mojo demo: Add support for mouse wheel events to html_viewer.
    BUG=387172
    
    Review URL: https://codereview.chromium.org/358843004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@280234 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/services/native_viewport/native_viewport_x11.cc
+++ mojo/services/native_viewport/native_viewport_x11.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/message_loop/message_loop.h"
 #include "ui/events/event.h"
+#include "ui/events/event_utils.h"
 #include "ui/events/platform/platform_event_dispatcher.h"
 #include "ui/events/platform/platform_event_source.h"
 #include "ui/events/platform/x11/x11_event_source.h"
@@ -151,8 +152,14 @@ class NativeViewportX11 : public NativeViewport,
       delegate_->OnEvent(&key_event);
     } else if (event->type == ButtonPress || event->type == ButtonRelease ||
                event->type == MotionNotify) {
-      ui::MouseEvent mouse_event(event);
-      delegate_->OnEvent(&mouse_event);
+      ui::EventType event_type = ui::EventTypeFromNative(event);
+      if (event_type == ui::ET_MOUSEWHEEL) {
+        ui::MouseWheelEvent mouse_event(event);
+        delegate_->OnEvent(&mouse_event);
+      } else {
+        ui::MouseEvent mouse_event(event);
+        delegate_->OnEvent(&mouse_event);
+      }
     }
     return ui::POST_DISPATCH_NONE;
   }
