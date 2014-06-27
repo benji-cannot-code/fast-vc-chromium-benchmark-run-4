e954856aee46dc64e6d30c985eb1c475d76e7280
eseidel@chromium.org <eseidel@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-27 04:53:28 +0000

    Clean up usage of CSP functions
    I was very confused by the ScriptLoader code which called
    CSP::allowScriptNonce and the corresponding variable
    which was called isValidScriptNonce.  I searched around
    for nonce functions and found that they only checked
    the nonce (not all policies) and I originally thought
    this was an error!  However after further investigation
    I found that allowScriptNonce actually checks all active
    CSPs and returns true only if they all pass, thus making
    the following line (which disables further CSP checks
    on the load) OK.
    
    I renamed these allowScriptNonce and allowStyleNonce
    functions to have a 'With' in their name and added
    a comment in the header explaining their behavior.
    
    While making this change, I also reduced the verbosity
    of several repeated uses of the string ContentSecurityPolicy.
    I think most blink hackers would be able to look up that CSP
    refers to ContentSecurityPolicy.
    
    I removed several redundant calls to Document::contentSecurityPolicy
    (again just caused the lines to be needlessly verbose).
    
    Finally I made ScriptLoader::executeScript use the preferred
    early-return pattern instead of a long indented block
    for the !frame case.  I suspect that the !frame check
    can actually be moved much earlier in the function
    or even turned into an ASSERT.
    
    This is still my long yack-shave to actually make the
    preloader correct so I can fix the bugs which my
    patch to make the HTML parser yield more agressively
    (and thus not starve the preloader) possible to land:
    https://codereview.chromium.org/258013009/
    
    BUG=356292
    
    Review URL: https://codereview.chromium.org/353873003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177065 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/ImageLoader.cpp
+++ third_party/WebKit/Source/core/loader/ImageLoader.cpp
@@ -65,17 +65,17 @@ class ImageLoader::Task : public blink::WebThread::Task {
 public:
     Task(ImageLoader* loader)
         : m_loader(loader)
-        , m_shouldBypassMainWorldContentSecurityPolicy(false)
+        , m_shouldBypassMainWorldCSP(false)
         , m_weakFactory(this)
     {
         LocalFrame* frame = loader->m_element->document().frame();
-        m_shouldBypassMainWorldContentSecurityPolicy = frame->script().shouldBypassMainWorldContentSecurityPolicy();
+        m_shouldBypassMainWorldCSP = frame->script().shouldBypassMainWorldCSP();
     }
 
     virtual void run() OVERRIDE
     {
         if (m_loader) {
-            m_loader->doUpdateFromElement(m_shouldBypassMainWorldContentSecurityPolicy);
+            m_loader->doUpdateFromElement(m_shouldBypassMainWorldCSP);
         }
     }
 
@@ -91,7 +91,7 @@ public:
 
 private:
     ImageLoader* m_loader;
-    bool m_shouldBypassMainWorldContentSecurityPolicy;
+    bool m_shouldBypassMainWorldCSP;
     WeakPtrFactory<Task> m_weakFactory;
 };
 
