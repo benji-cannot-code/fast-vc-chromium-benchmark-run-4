098489625055f871eaf2a33ba57451bc3d51f040
clholgat@chromium.org <clholgat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-27 02:57:12 +0000

    Send the show tab notification before switching the index
    This hides the overview layout before showing the static 
    layout instead of trying to show the static layout and then 
    trying to hide it when the tab index is switched.
    
    BUG=388334
    
    Review URL: https://codereview.chromium.org/357843003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@280216 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/android/java/src/org/chromium/chrome/browser/widget/accessibility/AccessibilityTabModelAdapter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/accessibility/AccessibilityTabModelAdapter.java
@@ -45,9 +45,9 @@ public class AccessibilityTabModelAdapter extends BaseAdapter {
             new AccessibilityTabModelListItemListener() {
         @Override
         public void tabSelected(int tab) {
+            if (mListener != null) mListener.showTab(tab);
             TabModelUtils.setIndex(mActualTabModel,
                     TabModelUtils.getTabIndexById(mActualTabModel, tab));
-            if (mListener != null) mListener.showTab(tab);
             notifyDataSetChanged();
         }
 
