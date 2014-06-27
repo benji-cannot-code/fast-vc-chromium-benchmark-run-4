b229e39f639ce7b6096d2ed502c6243aa51ef790
dgozman@chromium.org <dgozman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-27 11:43:23 +0000

    [DevTools] Hide popover on window resize.
    Popover is a modal element, and should be hidden on resize,
    toggling fullscreen, zoom change akin to TextPrompt.
    
    BUG=388659
    
    Review URL: https://codereview.chromium.org/353833003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177092 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ui/Popover.js
+++ third_party/WebKit/Source/devtools/front_end/ui/Popover.js
@@ -50,6 +50,7 @@ WebInspector.Popover = function(popoverHelper)
     this.element.appendChild(this._contentDiv);
 
     this._popoverHelper = popoverHelper;
+    this._hideBound = this.hide.bind(this);
 }
 
 WebInspector.Popover.prototype = {
@@ -100,6 +101,7 @@ WebInspector.Popover.prototype = {
         preferredWidth = preferredWidth || preferredSize.width;
         preferredHeight = preferredHeight || preferredSize.height;
 
+        window.addEventListener("resize", this._hideBound, false);
         document.body.appendChild(this._containerElement);
         WebInspector.View.prototype.show.call(this, this._containerElement);
 
@@ -118,6 +120,7 @@ WebInspector.Popover.prototype = {
 
     hide: function()
     {
+        window.removeEventListener("resize", this._hideBound, false);
         this.detach();
         this._containerElement.remove();
         delete WebInspector.Popover._popover;
