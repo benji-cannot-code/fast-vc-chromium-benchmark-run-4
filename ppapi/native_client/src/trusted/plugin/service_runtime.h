f18052ce988480bf4260df2cceb842de2f3c980a
ncbray@chromium.org <ncbray@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-27 01:41:42 +0000

    NaCl: begin reducing ServiceRuntime's dependance on Plugin in the trusted plugin.
    BUG=
    
    Review URL: https://codereview.chromium.org/342553004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@280197 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/native_client/src/trusted/plugin/service_runtime.h
+++ ppapi/native_client/src/trusted/plugin/service_runtime.h
@@ -35,6 +35,7 @@ class DescWrapper;
 
 namespace plugin {
 
+class ErrorInfo;
 class Plugin;
 class SrpcClient;
 class ServiceRuntime;
@@ -93,7 +94,7 @@ struct OpenManifestEntryResource {
 class PluginReverseInterface: public nacl::ReverseInterface {
  public:
   PluginReverseInterface(nacl::WeakRefAnchor* anchor,
-                         Plugin* plugin,
+                         PP_Instance pp_instance,
                          ServiceRuntime* service_runtime,
                          pp::CompletionCallback init_done_cb,
                          pp::CompletionCallback crash_cb);
@@ -130,8 +131,8 @@ class PluginReverseInterface: public nacl::ReverseInterface {
 
  private:
   nacl::WeakRefAnchor* anchor_;  // holds a ref
-  Plugin* plugin_;  // value may be copied, but should be used only in
-                    // main thread in WeakRef-protected callbacks.
+  // Should be used only in main thread in WeakRef-protected callbacks.
+  PP_Instance pp_instance_;
   ServiceRuntime* service_runtime_;
   NaClMutex mu_;
   NaClCondVar cv_;
@@ -147,6 +148,7 @@ class ServiceRuntime {
   // TODO(sehr): This class should also implement factory methods, using the
   // Start method below.
   ServiceRuntime(Plugin* plugin,
+                 PP_Instance pp_instance,
                  bool main_service_runtime,
                  bool uses_nonsfi_mode,
                  pp::CompletionCallback init_done_cb,
@@ -210,8 +212,11 @@ class ServiceRuntime {
   bool StartModule();
   void ReapLogs();
 
+  void ReportLoadError(const ErrorInfo& error_info);
+
   NaClSrpcChannel command_channel_;
   Plugin* plugin_;
+  PP_Instance pp_instance_;
   bool main_service_runtime_;
   bool uses_nonsfi_mode_;
   nacl::ReverseService* reverse_service_;
