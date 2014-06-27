351283c38b0401227053567f3101d7755290a2ac
lushnikov@chromium.org <lushnikov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-27 13:46:56 +0000

    DevTools: [CodeMirror] indent autodetection should fallback to default indent
    This patch fixes indent autodetection to fallback to default indent
    from user settings instead of "four spaces" value.
    
    This patch additionally adds a change listener on the "autodetect indentation"
    setting.
    
    BUG=389109
    
    Review URL: https://codereview.chromium.org/351213006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177103 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/source_frame/CodeMirrorTextEditor.js
+++ third_party/WebKit/Source/devtools/front_end/source_frame/CodeMirrorTextEditor.js
@@ -119,6 +119,7 @@ WebInspector.CodeMirrorTextEditor = function(url, delegate)
     };
 
     WebInspector.settings.textEditorIndent.addChangeListener(this._updateEditorIndentation, this);
+    WebInspector.settings.textEditorAutoDetectIndent.addChangeListener(this._updateEditorIndentation, this);
     this._updateEditorIndentation();
     WebInspector.settings.showWhitespacesInEditor.addChangeListener(this._updateCodeMirrorMode, this);
     WebInspector.settings.textEditorBracketMatching.addChangeListener(this._enableBracketMatchingIfNeeded, this);
@@ -452,6 +453,7 @@ WebInspector.CodeMirrorTextEditor.prototype = {
     dispose: function()
     {
         WebInspector.settings.textEditorIndent.removeChangeListener(this._updateEditorIndentation, this);
+        WebInspector.settings.textEditorAutoDetectIndent.removeChangeListener(this._updateEditorIndentation, this);
         WebInspector.settings.showWhitespacesInEditor.removeChangeListener(this._updateCodeMirrorMode, this);
         WebInspector.settings.textEditorBracketMatching.removeChangeListener(this._enableBracketMatchingIfNeeded, this);
     },
@@ -504,7 +506,7 @@ WebInspector.CodeMirrorTextEditor.prototype = {
                 minimumIndent = indent;
         }
         if (minimumIndent === Infinity)
-            return WebInspector.TextUtils.Indent.FourSpaces;
+            return WebInspector.settings.textEditorIndent.get();
         return new Array(minimumIndent + 1).join(" ");
     },
 
