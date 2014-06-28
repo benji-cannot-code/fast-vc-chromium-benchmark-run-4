52d0fe98e499778004922d06c9fcaa6a4aef586d
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-28 20:55:33 +0000

    Remove unused include of location_icon_view.h from c/b/chromeos/ file.
    This allow us to remove this entry from the DEPS file.
    
    BUG=125846
    TBR=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/272763004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@280534 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/ui/simple_web_view_dialog.cc
+++ chrome/browser/chromeos/login/ui/simple_web_view_dialog.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/content_settings/content_setting_bubble_model_delegate.h"
 #include "chrome/browser/ui/toolbar/toolbar_model_impl.h"
 #include "chrome/browser/ui/view_ids.h"
-#include "chrome/browser/ui/views/location_bar/location_icon_view.h"
 #include "chrome/browser/ui/views/toolbar/reload_button.h"
 #include "components/password_manager/core/browser/password_manager.h"
 #include "content/public/browser/navigation_controller.h"
