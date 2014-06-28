ae86c25474f1f95ad70e8635171da8478b956ed9
yukishiino@chromium.org <yukishiino@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-28 20:54:09 +0000

    aura: Enables --enable-text-input-focus-manager by default.
    This CL enables the experimental text input focus manager by default.
    
    This is second try.  First try http://crrev.com/323983005 was reverted.
    Reason for revert:
    Suspected of breaking interactive_ui_tests on Win:
    http://build.chromium.org/p/chromium.win/builders/Interactive%20Tests%20%28dbg%29/builds/48698
    
    BUG=290701
    
    Review URL: https://codereview.chromium.org/348033003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@280532 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/ui_base_switches_util.cc
+++ ui/base/ui_base_switches_util.cc
@@ -11,8 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace switches {
 
 bool IsTextInputFocusManagerEnabled() {
-  return CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kEnableTextInputFocusManager);
+  return !CommandLine::ForCurrentProcess()->HasSwitch(
+      switches::kDisableTextInputFocusManager);
 }
 
 bool IsTouchDragDropEnabled() {
