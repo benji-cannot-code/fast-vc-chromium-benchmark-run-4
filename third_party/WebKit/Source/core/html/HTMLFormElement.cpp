e2f321dd7e90dcd4d0fdae648bd9dd0f37542943
pmarch@chromium.org <pmarch@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-30 23:19:42 +0000

    Enables Activity Logger to log extension adding the following elements to the DOM: - script - link - anchor - iframe - form - input - button
    BUG=388287
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=177105
    
    Review URL: https://codereview.chromium.org/349263002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177245 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLFormElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLFormElement.cpp
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <limits>
 #include "bindings/v8/ScriptController.h"
 #include "bindings/v8/ScriptEventListener.h"
+#include "bindings/v8/V8DOMActivityLogger.h"
 #include "core/HTMLNames.h"
 #include "core/dom/Attribute.h"
 #include "core/dom/Document.h"
@@ -137,6 +138,16 @@ bool HTMLFormElement::rendererIsNeeded(const RenderStyle& style)
 
 Node::InsertionNotificationRequest HTMLFormElement::insertedInto(ContainerNode* insertionPoint)
 {
+    if (insertionPoint->inDocument()) {
+        V8DOMActivityLogger* activityLogger = V8DOMActivityLogger::currentActivityLoggerIfIsolatedWorld();
+        if (activityLogger) {
+            Vector<String> argv;
+            argv.append("form");
+            argv.append(fastGetAttribute(methodAttr));
+            argv.append(fastGetAttribute(actionAttr));
+            activityLogger->logEvent("blinkAddElement", argv.size(), argv.data());
+        }
+    }
     HTMLElement::insertedInto(insertionPoint);
     if (insertionPoint->inDocument())
         this->document().didAssociateFormControl(this);
--- third_party/WebKit/Source/core/html/HTMLLinkElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLLinkElement.cpp
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/html/HTMLLinkElement.h"
 
 #include "bindings/v8/ScriptEventListener.h"
+#include "bindings/v8/V8DOMActivityLogger.h"
 #include "core/HTMLNames.h"
 #include "core/css/MediaList.h"
 #include "core/css/MediaQueryEvaluator.h"
@@ -261,6 +262,16 @@ void HTMLLinkElement::enableIfExitTransitionStyle()
 
 Node::InsertionNotificationRequest HTMLLinkElement::insertedInto(ContainerNode* insertionPoint)
 {
+    if (insertionPoint->inDocument()) {
+        V8DOMActivityLogger* activityLogger = V8DOMActivityLogger::currentActivityLoggerIfIsolatedWorld();
+        if (activityLogger) {
+            Vector<String> argv;
+            argv.append("link");
+            argv.append(fastGetAttribute(relAttr));
+            argv.append(fastGetAttribute(hrefAttr));
+            activityLogger->logEvent("blinkAddElement", argv.size(), argv.data());
+        }
+    }
     HTMLElement::insertedInto(insertionPoint);
     if (!insertionPoint->inDocument())
         return InsertionDone;
