fce0d67c1fd1bdc321150a83a608066baa7a465c
rsadam@chromium.org <rsadam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-30 23:17:11 +0000

    Updates tests to work with IME Keyboard background/foreground refactor. This re-enables the typing tests, though the keyset transition tests will be fixed in a later patch.
    The issue of LSAN memory leaks is also fixed by the latest CRX - the old crx was violating the constraint that the component.id had to be unique.
    
    BUG=388816,386589
    
    Review URL: https://codereview.chromium.org/358693002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@280691 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/chromeos/virtual_keyboard/inputview/test_base.js
+++ chrome/test/data/chromeos/virtual_keyboard/inputview/test_base.js
@@ -7,11 +7,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 var mockController;
 var mockTimer;
-var setComposition;
 var realSetTimeout;
-
+var sendMessage;
 var DEFAULT_CONTEXT_ID = 1;
 var LONGPRESS_DELAY = 1100;
+
+/**
+ * The enumeration of message types. This should be kept in sync with the
+ * InputView enums.
+ * @const
+ * @enum {number}
+ */
+var Type = {
+  COMMIT_TEXT: 7,
+};
+
 
 /**
  * Create mocks for the virtualKeyboardPrivate API. Any tests that trigger API
@@ -32,16 +42,15 @@ function setUp() {
                  observedEvent.text,
                  'Mismatched commit text.');
   };
+  sendMessage = chrome.runtime.sendMessage;
+  chrome.runtime.sendMessage = function(msg){
+    // Forward message to the mocked method.
+    if (msg.type == Type.COMMIT_TEXT)
+      chrome.input.ime.commitText(msg)
+    else
+      console.error("Unknown message type: " + msg.type);
+  };
   chrome.input.ime.commitText.validateCall = validateCommit;
-
-  setComposition = chrome.input.ime.setComposition;
-  // Mocks setComposition manually to immediately callback. The mock controller
-  // does not support callback functions.
-  chrome.input.ime.setComposition = function(obj, callback) {
-    callback();
-  }
-  window.setContext({'contextID': DEFAULT_CONTEXT_ID, 'type': 'text'});
-  // TODO(rsadam): Mock additional extension API calls as required.
 }
 
 function RunTest(testFn, testDoneCallback) {
@@ -49,8 +58,10 @@ function RunTest(testFn, testDoneCallback) {
     if (window.isKeyboardReady()) {
       testFn();
       testDoneCallback();
-    } else
+    } else {
+      window.startTest();
       realSetTimeout(pollTillReady, 100);
+    }
   }
   pollTillReady();
 }
@@ -61,8 +72,8 @@ function RunTest(testFn, testDoneCallback) {
 function tearDown() {
   mockController.verifyMocks();
   mockController.reset();
+  chrome.runtime.sendMessage = sendMessage;
   mockTimer.uninstall();
-  chrome.input.ime.setComposition = setComposition;
 }
 
 /**
