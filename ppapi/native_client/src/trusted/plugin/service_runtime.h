3d771cef017638e53033a4ffd0cb1952142f8f12
teravest@chromium.org <teravest@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-30 22:56:28 +0000

    Pepper: SelLdrLauncherChrome cleanup.
    The indirection provided by SelLdrLauncherChrome::Start() isn't much help.
    Instead, we just expose the channel so that ServiceRuntime can pass it to
    LaunchSelLdr directly.
    
    BUG=239656
    
    Review URL: https://codereview.chromium.org/360743002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@280686 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/native_client/src/trusted/plugin/service_runtime.h
+++ ppapi/native_client/src/trusted/plugin/service_runtime.h
@@ -37,6 +37,7 @@ namespace plugin {
 
 class ErrorInfo;
 class Plugin;
+class SelLdrLauncherChrome;
 class SrpcClient;
 class ServiceRuntime;
 
@@ -220,7 +221,7 @@ class ServiceRuntime {
   bool main_service_runtime_;
   bool uses_nonsfi_mode_;
   nacl::ReverseService* reverse_service_;
-  nacl::scoped_ptr<nacl::SelLdrLauncherBase> subprocess_;
+  nacl::scoped_ptr<SelLdrLauncherChrome> subprocess_;
 
   nacl::WeakRefAnchor* anchor_;
 
@@ -232,6 +233,8 @@ class ServiceRuntime {
   bool start_sel_ldr_done_;
   bool start_nexe_done_;
   bool nexe_started_ok_;
+
+  NaClHandle bootstrap_channel_;
 };
 
 }  // namespace plugin
