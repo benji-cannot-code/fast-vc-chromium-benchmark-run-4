08fee0fb7568ddc467fcfa95c41bd7fd6396b1e4
sungmann.cho@navercorp.com <sungmann.cho@navercorp.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-30 06:42:06 +0000

    Remove unnecessary #includes in web_history_service_factory.cc and sync_prefs.cc.
    After the landing of https://codereview.chromium.org/195893014,
    web_history_service_factory.cc and sync_prefs.cc don't need the command line switch
    related headers anymore. So we can remove these.
    
    BUG=343274
    
    Review URL: https://codereview.chromium.org/345393006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@280568 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/web_history_service_factory.cc
+++ chrome/browser/history/web_history_service_factory.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/history/web_history_service_factory.h"
 
-#include "base/command_line.h"
 #include "chrome/browser/content_settings/cookie_settings.h"
 #include "chrome/browser/history/web_history_service.h"
 #include "chrome/browser/signin/profile_oauth2_token_service_factory.h"
