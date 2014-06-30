ebaea856766b1c477f14ce6792a7851e8d1fed1a
vollick@chromium.org <vollick@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-30 16:55:41 +0000

    4th side of a 3-sided patch
    Remove the now unnecessary defines introduced in
    https://codereview.chromium.org/348193002
    
    R=abarth@chromium.org
    BUG=None
    
    Review URL: https://codereview.chromium.org/355563002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177217 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/web/WebPluginContainer.h
+++ third_party/WebKit/public/web/WebPluginContainer.h
@@ -34,8 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "../platform/WebCommon.h"
 
-#define SCROLL_RECT_REQUIRES_NO_DELTA 1
-
 struct NPObject;
 
 namespace blink {
--- third_party/WebKit/public/web/WebWidgetClient.h
+++ third_party/WebKit/public/web/WebWidgetClient.h
@@ -32,8 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WebWidgetClient_h
 #define WebWidgetClient_h
 
-#define DID_SCROLL_RECT_DOES_NOT_EXIST 1
-
 #include "WebNavigationPolicy.h"
 #include "public/platform/WebCommon.h"
 #include "public/platform/WebLayerTreeView.h"
