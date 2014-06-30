8984c7dfd6d6c41448958500ba7665d0af122214
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-30 05:49:59 +0000

    Remove PTHREAD_SETNAME_NP from config.h
    This define is only used in one place.
    
    Review URL: https://codereview.chromium.org/355353002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177190 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/config.h
+++ third_party/WebKit/Source/config.h
@@ -113,7 +113,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if OS(MACOSX)
-#define HAVE_PTHREAD_SETNAME_NP 1
 #define WTF_USE_NEW_THEME 1
 #endif /* OS(MACOSX) */
 
