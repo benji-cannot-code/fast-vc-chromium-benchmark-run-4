4ba6e6cf952afcd8d4e74fb49192b2bb93cd8a6b
sashab@chromium.org <sashab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-30 22:30:48 +0000

    Added overlay back to App List when child dialogs are opened
    This prevents activity in the app list while a dialog is open that doesn't fully cover the app list. Partial revert of revision 277691 (CL 306023011).
    
    Also removed all calls that wait for the message loop in
    AppListControllerAppInfoDialogBrowserTest.DoShowAppInfoFlow by calling
    Widget::CloseNow() instead of the async alternative Widget::Close().
    This should make the test more reliable (reduce/remove flakiness),
    although the test still remains disabled.
    
    BUG=387509,386866,378251
    
    Review URL: https://codereview.chromium.org/352223004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@280681 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/app_list/app_list_controller_delegate_views.cc
+++ chrome/browser/ui/app_list/app_list_controller_delegate_views.cc
@@ -31,10 +31,14 @@ void AppListControllerDelegateViews::ViewClosing() {
 }
 
 void AppListControllerDelegateViews::OnShowChildDialog() {
+  DCHECK(service_->shower().app_list());
+  service_->shower().app_list()->SetAppListOverlayVisible(true);
   service_->set_can_dismiss(false);
 }
 
 void AppListControllerDelegateViews::OnCloseChildDialog() {
+  DCHECK(service_->shower().app_list());
+  service_->shower().app_list()->SetAppListOverlayVisible(false);
   service_->set_can_dismiss(true);
 }
 
