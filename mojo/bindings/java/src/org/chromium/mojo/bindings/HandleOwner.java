83cfb0f680c1117279a548da53b3b68eb61e778f
qsr@chromium.org <qsr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-01 11:33:40 +0000

    Adding Connector class to observe a message pipe.
    R=rmcilroy@chromium.org, viettrungluu@chromium.org
    
    Review URL: https://codereview.chromium.org/324903003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@280799 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ mojo/bindings/java/src/org/chromium/mojo/bindings/HandleOwner.java
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.mojo.bindings;
+
+import org.chromium.mojo.system.Handle;
+
+import java.io.Closeable;
+
+/**
+ * Describes a class that owns a handle.
+ *
+ * @param <H> The type of the owned handle.
+ */
+public interface HandleOwner<H extends Handle> extends Closeable {
+
+    /**
+     * Pass the handle owned by this class.
+     */
+    public H passHandle();
+}
