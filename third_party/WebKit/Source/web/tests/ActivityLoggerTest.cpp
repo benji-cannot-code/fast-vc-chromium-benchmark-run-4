2449f86f4ac76117ee3eb398579018ea1b23b7e3
pmarch@chromium.org <pmarch@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-01 23:55:03 +0000

    Disable all ActivityLogger tests, they're flaky
    These caused the last Blink roll to be reverted:
    http://src.chromium.org/viewvc/chrome?view=rev&revision=280864
    
    NOTRY=true
    BUG= 388287
    
    Review URL: https://codereview.chromium.org/366723002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177340 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/tests/ActivityLoggerTest.cpp
+++ third_party/WebKit/Source/web/tests/ActivityLoggerTest.cpp
@@ -111,7 +111,7 @@ private:
     TestActivityLogger* m_activityLogger;
 };
 
-TEST_F(ActivityLoggerTest, EventHandler)
+TEST_F(ActivityLoggerTest, DISABLED_EventHandler)
 {
     const char* code =
         "document.body.innerHTML = '<a onclick=\\\'do()\\\'>test</a>';"
@@ -130,7 +130,7 @@ TEST_F(ActivityLoggerTest, EventHandler)
     ASSERT_TRUE(verifyActivities(expectedActivities));
 }
 
-TEST_F(ActivityLoggerTest, ScriptElement)
+TEST_F(ActivityLoggerTest, DISABLED_ScriptElement)
 {
     const char* code =
         "document.body.innerHTML = '<script src=\\\'data:text/html;charset=utf-8,\\\'></script>';"
@@ -153,7 +153,7 @@ TEST_F(ActivityLoggerTest, ScriptElement)
     ASSERT_TRUE(verifyActivities(expectedActivities));
 }
 
-TEST_F(ActivityLoggerTest, IFrameElement)
+TEST_F(ActivityLoggerTest, DISABLED_IFrameElement)
 {
     const char* code =
         "document.body.innerHTML = '<iframe src=\\\'data:text/html;charset=utf-8,\\\'></iframe>';"
@@ -176,7 +176,7 @@ TEST_F(ActivityLoggerTest, IFrameElement)
     ASSERT_TRUE(verifyActivities(expectedActivities));
 }
 
-TEST_F(ActivityLoggerTest, AnchorElement)
+TEST_F(ActivityLoggerTest, DISABLED_AnchorElement)
 {
     const char* code =
         "document.body.innerHTML = '<a href=\\\'data:text/css;charset=utf-8,\\\'></a>';"
@@ -199,7 +199,7 @@ TEST_F(ActivityLoggerTest, AnchorElement)
     ASSERT_TRUE(verifyActivities(expectedActivities));
 }
 
-TEST_F(ActivityLoggerTest, LinkElement)
+TEST_F(ActivityLoggerTest, DISABLED_LinkElement)
 {
     const char* code =
         "document.body.innerHTML = '<link rel=\\\'stylesheet\\\' href=\\\'data:text/css;charset=utf-8,\\\'></link>';"
@@ -223,7 +223,7 @@ TEST_F(ActivityLoggerTest, LinkElement)
     ASSERT_TRUE(verifyActivities(expectedActivities));
 }
 
-TEST_F(ActivityLoggerTest, InputElement)
+TEST_F(ActivityLoggerTest, DISABLED_InputElement)
 {
     const char* code =
         "document.body.innerHTML = '<input type=\\\'submit\\\' formaction=\\\'data:text/html;charset=utf-8,\\\'></input>';"
@@ -247,7 +247,7 @@ TEST_F(ActivityLoggerTest, InputElement)
     ASSERT_TRUE(verifyActivities(expectedActivities));
 }
 
-TEST_F(ActivityLoggerTest, ButtonElement)
+TEST_F(ActivityLoggerTest, DISABLED_ButtonElement)
 {
     const char* code =
         "document.body.innerHTML = '<button type=\\\'submit\\\' formmethod=\\\'post\\\' formaction=\\\'data:text/html;charset=utf-8,\\\'></input>';"
@@ -272,7 +272,7 @@ TEST_F(ActivityLoggerTest, ButtonElement)
     ASSERT_TRUE(verifyActivities(expectedActivities));
 }
 
-TEST_F(ActivityLoggerTest, FormElement)
+TEST_F(ActivityLoggerTest, DISABLED_FormElement)
 {
     const char* code =
         "document.body.innerHTML = '<form method=\\\'post\\\' action=\\\'data:text/html;charset=utf-8,\\\'></form>';"
@@ -296,7 +296,7 @@ TEST_F(ActivityLoggerTest, FormElement)
     ASSERT_TRUE(verifyActivities(expectedActivities));
 }
 
-TEST_F(ActivityLoggerTest, IFrameSrcAttribute)
+TEST_F(ActivityLoggerTest, DISABLED_IFrameSrcAttribute)
 {
     const char* code =
         "document.body.innerHTML = '<iframe src=\\\'data:text/html;charset=utf-8,A\\\'></iframe>';"
@@ -319,7 +319,7 @@ TEST_F(ActivityLoggerTest, IFrameSrcAttribute)
     ASSERT_TRUE(verifyActivities(expectedActivities));
 }
 
-TEST_F(ActivityLoggerTest, AnchorHrefAttribute)
+TEST_F(ActivityLoggerTest, DISABLED_AnchorHrefAttribute)
 {
     const char* code =
         "document.body.innerHTML = '<a href=\\\'data:text/html;charset=utf-8,A\\\'></a>';"
@@ -342,7 +342,7 @@ TEST_F(ActivityLoggerTest, AnchorHrefAttribute)
     ASSERT_TRUE(verifyActivities(expectedActivities));
 }
 
-TEST_F(ActivityLoggerTest, LinkHrefAttribute)
+TEST_F(ActivityLoggerTest, DISABLED_LinkHrefAttribute)
 {
     const char* code =
         "document.body.innerHTML = '<link rel=\\\'stylesheet\\\' href=\\\'data:text/css;charset=utf-8,A\\\'></link>';"
@@ -365,7 +365,7 @@ TEST_F(ActivityLoggerTest, LinkHrefAttribute)
     ASSERT_TRUE(verifyActivities(expectedActivities));
 }
 
-TEST_F(ActivityLoggerTest, InputFormActionAttribute)
+TEST_F(ActivityLoggerTest, DISABLED_InputFormActionAttribute)
 {
     const char* code =
         "document.body.innerHTML = '<input type=\\\'button\\\' formaction=\\\'data:text/html;charset=utf-8,A\\\'></input>';"
@@ -388,7 +388,7 @@ TEST_F(ActivityLoggerTest, InputFormActionAttribute)
     ASSERT_TRUE(verifyActivities(expectedActivities));
 }
 
-TEST_F(ActivityLoggerTest, ButtonFormActionAttribute)
+TEST_F(ActivityLoggerTest, DISABLED_ButtonFormActionAttribute)
 {
     const char* code =
         "document.body.innerHTML = '<button type=\\\'submit\\\' formmethod=\\\'post\\\' formaction=\\\'data:text/html;charset=utf-8,A\\\'></input>';"
@@ -411,7 +411,7 @@ TEST_F(ActivityLoggerTest, ButtonFormActionAttribute)
     ASSERT_TRUE(verifyActivities(expectedActivities));
 }
 
-TEST_F(ActivityLoggerTest, FormActionAttribute)
+TEST_F(ActivityLoggerTest, DISABLED_FormActionAttribute)
 {
     const char* code =
         "document.body.innerHTML = '<form action=\\\'data:text/html;charset=utf-8,A\\\'></form>';"
@@ -434,7 +434,7 @@ TEST_F(ActivityLoggerTest, FormActionAttribute)
     ASSERT_TRUE(verifyActivities(expectedActivities));
 }
 
-TEST_F(ActivityLoggerTest, LocalDOMWindowAttribute)
+TEST_F(ActivityLoggerTest, DISABLED_LocalDOMWindowAttribute)
 {
     const char* code =
         "location.href = 'data:text/html;charset=utf-8,A';"
