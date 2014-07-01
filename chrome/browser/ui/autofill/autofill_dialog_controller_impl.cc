d55a5c6f13b17a3492c40f46f266b23cb74c9bb6
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-01 07:08:53 +0000

    Respect wallet allowed shipping country codes
    BUG=342940
    
    Review URL: https://codereview.chromium.org/339523006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@280765 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/autofill/autofill_dialog_controller_impl.cc
+++ chrome/browser/ui/autofill/autofill_dialog_controller_impl.cc
@@ -665,10 +665,28 @@ AutofillDialogControllerImpl::~AutofillDialogControllerImpl() {
   GetMetricLogger().LogDialogInitialUserState(initial_user_state_);
 }
 
-bool CountryFilter(const std::set<base::string16>& possible_values,
-                   const std::string& country_code) {
-  if (!possible_values.empty() &&
-      !possible_values.count(base::ASCIIToUTF16(country_code))) {
+// Checks the country code against the values the form structure enumerates.
+bool AutofillCountryFilter(
+    const std::set<base::string16>& form_structure_values,
+    const std::string& country_code) {
+  if (!form_structure_values.empty() &&
+      !form_structure_values.count(base::ASCIIToUTF16(country_code))) {
+    return false;
+  }
+
+  return true;
+}
+
+// Checks the country code against the values the form structure enumerates and
+// against the ones Wallet allows.
+bool WalletCountryFilter(
+    const std::set<base::string16>& form_structure_values,
+    const std::set<std::string>& wallet_allowed_values,
+    const std::string& country_code) {
+  if ((!form_structure_values.empty() &&
+       !form_structure_values.count(base::ASCIIToUTF16(country_code))) ||
+      (!wallet_allowed_values.empty() &&
+       !wallet_allowed_values.count(country_code))) {
     return false;
   }
 
@@ -768,14 +786,18 @@ void AutofillDialogControllerImpl::Show() {
     return;
   }
 
-  billing_country_combobox_model_.reset(new CountryComboboxModel(
+  billing_country_combobox_model_.reset(new CountryComboboxModel());
+  billing_country_combobox_model_->SetCountries(
       *GetManager(),
-      base::Bind(CountryFilter,
-                 form_structure_.PossibleValues(ADDRESS_BILLING_COUNTRY))));
-  shipping_country_combobox_model_.reset(new CountryComboboxModel(
+      base::Bind(AutofillCountryFilter,
+                 form_structure_.PossibleValues(ADDRESS_BILLING_COUNTRY)));
+  shipping_country_combobox_model_.reset(new CountryComboboxModel());
+  acceptable_shipping_countries_ =
+      form_structure_.PossibleValues(ADDRESS_HOME_COUNTRY);
+  shipping_country_combobox_model_->SetCountries(
       *GetManager(),
-      base::Bind(CountryFilter,
-                 form_structure_.PossibleValues(ADDRESS_HOME_COUNTRY))));
+      base::Bind(AutofillCountryFilter,
+                 base::ConstRef(acceptable_shipping_countries_)));
 
   // If the form has a country <select> but none of the options are valid, bail.
   if (billing_country_combobox_model_->GetItemCount() == 0 ||
@@ -2657,6 +2679,20 @@ void AutofillDialogControllerImpl::OnDidGetWalletItems(
         usernames, wallet_items_->active_account_index());
   }
 
+  if (wallet_items_) {
+    shipping_country_combobox_model_->SetCountries(
+        *GetManager(),
+        base::Bind(WalletCountryFilter,
+                   acceptable_shipping_countries_,
+                   wallet_items_->allowed_shipping_countries()));
+
+    // If the site doesn't ship to any of the countries Wallet allows shipping
+    // to, the merchant is not supported. (Note we generally shouldn't get here
+    // as such a merchant wouldn't make it onto the Wallet whitelist.)
+    if (shipping_country_combobox_model_->GetItemCount() == 0)
+      DisableWallet(wallet::WalletClient::UNSUPPORTED_MERCHANT);
+  }
+
   ConstructLegalDocumentsText();
   OnWalletOrSigninUpdate();
 }
@@ -3033,6 +3069,10 @@ void AutofillDialogControllerImpl::SuggestionsUpdated() {
         suggested_cc_billing_.SetCheckedItem(kAddNewItemKey);
     }
   } else {
+    shipping_country_combobox_model_->SetCountries(
+        *GetManager(),
+        base::Bind(AutofillCountryFilter, acceptable_shipping_countries_));
+
     if (IsAutofillEnabled()) {
       PersonalDataManager* manager = GetManager();
       const std::vector<CreditCard*>& cards = manager->GetCreditCards();
--- chrome/browser/ui/autofill/country_combobox_model.cc
+++ chrome/browser/ui/autofill/country_combobox_model.cc
@@ -20,9 +20,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 
-CountryComboboxModel::CountryComboboxModel(
+CountryComboboxModel::CountryComboboxModel() {}
+
+CountryComboboxModel::~CountryComboboxModel() {}
+
+void CountryComboboxModel::SetCountries(
     const PersonalDataManager& manager,
     const base::Callback<bool(const std::string&)>& filter) {
+  countries_.clear();
+
   // Insert the default country at the top as well as in the ordered list.
   std::string default_country_code =
       manager.GetDefaultCountryCodeForNewAddress();
@@ -58,8 +64,6 @@ CountryComboboxModel::CountryComboboxModel(
                     sorted_countries.begin(),
                     sorted_countries.end());
 }
-
-CountryComboboxModel::~CountryComboboxModel() {}
 
 int CountryComboboxModel::GetItemCount() const {
   return countries_.size();
