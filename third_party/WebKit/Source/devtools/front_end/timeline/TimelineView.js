abd0f17c4c5b34adc54109709b889e898b7664d7
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-01 15:25:38 +0000

    DevTools: Tracing timeline do not mark CPU events as background
    BUG=361045
    
    Review URL: https://codereview.chromium.org/362693003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177316 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/timeline/TimelineView.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TimelineView.js
@@ -1065,7 +1065,7 @@ WebInspector.TimelineRecordListRow.prototype = {
         for (var currentRecord = presentationRecord.presentationParent() ? presentationRecord.presentationParent().presentationParent() : null; currentRecord; currentRecord = currentRecord.presentationParent())
             paddingLeft += 12 / (Math.max(1, step++));
         this.element.style.paddingLeft = paddingLeft + "px";
-        if (record.thread())
+        if (record.thread() && record.thread() !== "CPU")
             this.element.classList.add("background");
 
         this._typeElement.textContent = uiUtils.titleForRecord(record);
@@ -1198,7 +1198,7 @@ WebInspector.TimelineRecordGraphRow.prototype = {
         this._record = presentationRecord;
         var record = presentationRecord.record();
         this.element.className = "timeline-graph-side timeline-category-" + uiUtils.categoryForRecord(record).name;
-        if (record.thread())
+        if (record.thread() && record.thread() !== "CPU")
             this.element.classList.add("background");
 
         var barPosition = calculator.computeBarGraphWindowPosition(presentationRecord);
