1057583114fc300e3be6f186977ab8d1f0dc2fda
hirono@chromium.org <hirono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-01 06:51:45 +0000

    Files.app: Add MetadataCache test of getLatest.
    BUG=386553
    TEST=run the test
    
    Review URL: https://codereview.chromium.org/351413002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@280755 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/file_manager/file_manager/foreground/js/metadata/metadata_cache.js
+++ ui/file_manager/file_manager/foreground/js/metadata/metadata_cache.js
@@ -213,7 +213,7 @@ MetadataCache.prototype.get = function(entries, type, callback) {
  * @param {string} type The metadata type.
  * @param {function(Object)} callback The metadata is passed to callback.
  */
-MetadataCache.prototype.getLatest = function(entries, type, callback, refresh) {
+MetadataCache.prototype.getLatest = function(entries, type, callback) {
   return this.getInternal_(entries, type, true, callback);
 };
 
