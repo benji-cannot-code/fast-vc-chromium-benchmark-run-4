c1a314d95882a0a92d0ed0d662cf389c56d42959
peria@chromium.org <peria@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-01 07:29:18 +0000

    [SyncFS] Add a schema comment of leveldb in MetadataDatabase.
    BUG=None
    TEST=None
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/359983004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@280775 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_backend/metadata_database_index_on_disk.cc
+++ chrome/browser/sync_file_system/drive_backend/metadata_database_index_on_disk.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync_file_system/drive_backend/drive_backend_util.h"
 #include "chrome/browser/sync_file_system/drive_backend/metadata_database.pb.h"
 
+// See metadata_database_index.cc for the database schema.
+
 namespace sync_file_system {
 namespace drive_backend {
 
