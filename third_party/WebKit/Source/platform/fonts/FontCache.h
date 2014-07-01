04d6b3424ab80caa1defe988d57e783565d375f9
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-01 18:28:04 +0000

    Prefer synthetic bold/italic over alternate family
    Change the font fallback code to prefer the specified font family with
    syntetic bold and/or italic over falling back to an alternate font.
    
    R=pdr@chromium.org
    BUG=385069
    TEST=fast/text/font-fallback-synthetic-italics.html
    
    Review URL: https://codereview.chromium.org/361833003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177327 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/fonts/FontCache.h
+++ third_party/WebKit/Source/platform/fonts/FontCache.h
@@ -156,6 +156,7 @@ private:
     PassRefPtr<SkTypeface> createTypeface(const FontDescription&, const FontFaceCreationParams&, CString& name);
 
     PassRefPtr<SimpleFontData> fontDataFromFontPlatformData(const FontPlatformData*, ShouldRetain = Retain);
+    PassRefPtr<SimpleFontData> fallbackOnStandardFontStyle(const FontDescription&, UChar32);
 
     // Don't purge if this count is > 0;
     int m_purgePreventCount;
