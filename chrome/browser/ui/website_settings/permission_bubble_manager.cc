59aa8f3cd02f05161c4602d3f33e858b1e365362
gbillock@chromium.org <gbillock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-02 07:04:52 +0000

    [WebsiteSettings] Allow same-page navigations to not close permission bubble (eg. fragment nav)
    BUG=
    
    Review URL: https://codereview.chromium.org/359403002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@280957 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/website_settings/permission_bubble_manager.cc
+++ chrome/browser/ui/website_settings/permission_bubble_manager.cc
@@ -237,7 +237,10 @@ void PermissionBubbleManager::NavigationEntryCommitted(
     return;
 
   // If we have navigated to a new url or reloaded the page...
-  if (request_url_ != web_contents()->GetLastCommittedURL() ||
+  // GetAsReferrer strips fragment and username/password, meaning
+  // the navigation is really to the same page.
+  if ((request_url_.GetAsReferrer() !=
+       web_contents()->GetLastCommittedURL().GetAsReferrer()) ||
       details.type == content::NAVIGATION_TYPE_EXISTING_PAGE) {
     // Kill off existing bubble and cancel any pending requests.
     CancelPendingQueues();
@@ -319,8 +322,9 @@ void PermissionBubbleManager::TriggerShowBubble() {
   if (!request_url_has_loaded_)
     return;
   if (requests_.empty() && queued_requests_.empty() &&
-      queued_frame_requests_.empty())
+      queued_frame_requests_.empty()) {
     return;
+  }
 
   if (requests_.empty()) {
     // Queues containing a user-gesture-generated request have priority.
@@ -359,11 +363,10 @@ void PermissionBubbleManager::FinalizeBubble() {
   }
   requests_.clear();
   accept_states_.clear();
-  if (queued_requests_.size() || queued_frame_requests_.size()) {
+  if (queued_requests_.size() || queued_frame_requests_.size())
     TriggerShowBubble();
-  } else {
+  else
     request_url_ = GURL();
-  }
 }
 
 void PermissionBubbleManager::CancelPendingQueues() {
@@ -412,3 +415,4 @@ bool PermissionBubbleManager::HasUserGestureRequest(
   }
   return false;
 }
+
