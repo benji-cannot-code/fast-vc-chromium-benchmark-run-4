b6bb98dd5199b2d2aedafb4621edfc559ae2e0d6
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-02 01:50:00 +0000

    Clean up BluetoothDiscoverySession a bit.
    * Avoid unhelpful logging for a common path through the destructor.
    * Remove the need for a test-only constructor.
    
    BUG=none
    TEST=none
    R=armansito@chromium.org
    
    Review URL: https://codereview.chromium.org/368493002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@280937 0039d316-1c4b-4281-b951-d872f2087c98

--- device/bluetooth/test/mock_bluetooth_discovery_session.cc
+++ device/bluetooth/test/mock_bluetooth_discovery_session.cc
@@ -5,11 +5,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "device/bluetooth/test/mock_bluetooth_discovery_session.h"
 
-#include "device/bluetooth/bluetooth_adapter.h"
+#include "device/bluetooth/test/mock_bluetooth_adapter.h"
 
 namespace device {
 
-MockBluetoothDiscoverySession::MockBluetoothDiscoverySession() {}
+// Note: Because |this| class mocks out all the interesting method calls, the
+// mock BluetoothAdapter will not be used, except for a trivial call from the
+// destructor. It's passed in simply because the base class expects one, and
+// it's nice not to need to complicate production code for the sake of simpler
+// test code.
+MockBluetoothDiscoverySession::MockBluetoothDiscoverySession()
+    : BluetoothDiscoverySession(
+        scoped_refptr<BluetoothAdapter>(
+            new testing::NiceMock<MockBluetoothAdapter>())) {}
 MockBluetoothDiscoverySession::~MockBluetoothDiscoverySession() {}
 
 }  // namespace device
