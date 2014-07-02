9062eebd934fcb737c907fa0637440fb430d63e0
engedy@chromium.org <engedy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-02 12:41:26 +0000

    Remove recovery diagnostics for the archived history database.
    The reason is that the archived history database feature has been removed as of r275159.
    
    BUG=359377
    
    Review URL: https://codereview.chromium.org/334633006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281003 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/diagnostics/diagnostics_model.cc
+++ chrome/browser/diagnostics/diagnostics_model.cc
@@ -25,14 +25,14 @@ namespace diagnostics {
 // This is the count of diagnostic tests on each platform.  This should
 // only be used by testing code.
 #if defined(OS_WIN)
-const int DiagnosticsModel::kDiagnosticsTestCount = 18;
+const int DiagnosticsModel::kDiagnosticsTestCount = 17;
 #elif defined(OS_MACOSX)
-const int DiagnosticsModel::kDiagnosticsTestCount = 14;
+const int DiagnosticsModel::kDiagnosticsTestCount = 13;
 #elif defined(OS_POSIX)
 #if defined(OS_CHROMEOS)
-const int DiagnosticsModel::kDiagnosticsTestCount = 18;
+const int DiagnosticsModel::kDiagnosticsTestCount = 17;
 #else
-const int DiagnosticsModel::kDiagnosticsTestCount = 16;
+const int DiagnosticsModel::kDiagnosticsTestCount = 15;
 #endif
 #endif
 
@@ -165,7 +165,6 @@ class DiagnosticsModelWin : public DiagnosticsModelImpl {
     tests_.push_back(MakeSqliteWebDataDbTest());
     tests_.push_back(MakeSqliteCookiesDbTest());
     tests_.push_back(MakeSqliteHistoryDbTest());
-    tests_.push_back(MakeSqliteArchivedHistoryDbTest());
     tests_.push_back(MakeSqliteThumbnailsDbTest());
     tests_.push_back(MakeSqliteWebDatabaseTrackerDbTest());
   }
@@ -189,7 +188,6 @@ class DiagnosticsModelMac : public DiagnosticsModelImpl {
     tests_.push_back(MakeSqliteWebDataDbTest());
     tests_.push_back(MakeSqliteCookiesDbTest());
     tests_.push_back(MakeSqliteHistoryDbTest());
-    tests_.push_back(MakeSqliteArchivedHistoryDbTest());
     tests_.push_back(MakeSqliteThumbnailsDbTest());
     tests_.push_back(MakeSqliteWebDatabaseTrackerDbTest());
   }
@@ -215,7 +213,6 @@ class DiagnosticsModelPosix : public DiagnosticsModelImpl {
     tests_.push_back(MakeSqliteWebDataDbTest());
     tests_.push_back(MakeSqliteCookiesDbTest());
     tests_.push_back(MakeSqliteHistoryDbTest());
-    tests_.push_back(MakeSqliteArchivedHistoryDbTest());
     tests_.push_back(MakeSqliteThumbnailsDbTest());
     tests_.push_back(MakeSqliteWebDatabaseTrackerDbTest());
 #if defined(OS_CHROMEOS)
