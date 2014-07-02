42ce818a4d4835afada4c70b9bf9baac1c73f2d0
mkosiba@chromium.org <mkosiba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-02 16:53:36 +0000

    [android_webview] Add a zoomBy method to AwContents.
    BUG=390921, 175854
    TEST=AndroidWebViewTest
    
    Review URL: https://codereview.chromium.org/368023002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281052 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/javatests/src/org/chromium/android_webview/test/AwContentsClientOnScaleChangedTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwContentsClientOnScaleChangedTest.java
@@ -5,9 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.android_webview.test;
 
+import android.test.suitebuilder.annotation.SmallTest;
+
 import org.chromium.android_webview.AwContents;
 import org.chromium.android_webview.test.util.CommonResources;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.content.browser.ContentViewCore;
 
 /**
@@ -31,11 +32,7 @@ public class AwContentsClientOnScaleChangedTest extends AwTestBase {
         super.tearDown();
     }
 
-    /*
     @SmallTest
-    This test is timing out on ICS bots including cq. See crbug.com/175854.
-    */
-    @DisabledTest
     public void testScaleUp() throws Throwable {
         getAwSettingsOnUiThread(mAwContents).setUseWideViewPort(true);
         loadDataSync(mAwContents, mContentsClient.getOnPageFinishedHelper(),
