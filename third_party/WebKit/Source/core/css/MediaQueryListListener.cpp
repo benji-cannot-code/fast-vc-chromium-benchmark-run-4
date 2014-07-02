ca033cd414db85aa4af4f376a13295d696860146
cbiesinger@chromium.org <cbiesinger@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-02 00:01:57 +0000

    Make it possible to subclass MediaQueryListListener
    ...so that C++ code can listen to media query changes.
    
    I can't think of a way to test this directly, so this
    will be tested via https://codereview.chromium.org/287163010/ when that lands.
    
    Review URL: https://codereview.chromium.org/361003004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177343 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/MediaQueryListListener.cpp
+++ third_party/WebKit/Source/core/css/MediaQueryListListener.cpp
@@ -27,6 +27,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+MediaQueryListListener::MediaQueryListListener()
+{
+    // only for use by subclasses
+}
+
 MediaQueryListListener::MediaQueryListListener(ScriptState* scriptState, const ScriptValue& function)
     : m_scriptState(scriptState)
     , m_function(function)
