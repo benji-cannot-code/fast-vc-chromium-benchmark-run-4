b9f15502a137066cd6efc081b2c9117872dc5b9d
sunil.ratnu@samsung.com <sunil.ratnu@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-02 00:02:26 +0000

    Remove unncessary forward declarations from CSSPropertyParser.h
    Some of the forward declarations are not necessary, hence removing them.
    
    BUG: None
    
    Review URL: https://codereview.chromium.org/363643003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177344 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParser.h
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParser.h
@@ -42,22 +42,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-// FIXME: Many of these may not be used.
-class CSSArrayFunctionValue;
 class CSSBorderImageSliceValue;
 class CSSPrimitiveValue;
-class CSSSelectorList;
 class CSSValue;
 class CSSValueList;
 class CSSBasicShape;
 class CSSBasicShapeInset;
 class CSSGridLineNamesValue;
-class Document;
-class Element;
 class ImmutableStylePropertySet;
-class StyleKeyframe;
 class StylePropertyShorthand;
-class StyleKeyframe;
 class UseCounter;
 
 // Inputs: PropertyID, isImportant bool, CSSParserValueList.
