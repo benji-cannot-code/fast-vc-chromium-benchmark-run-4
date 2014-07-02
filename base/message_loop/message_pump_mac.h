327d76b8c0a0e50404a66840ec17cd922e1c2487
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-02 02:03:57 +0000

    Revert 221427 "Add instrumentation to the MessagePumpMac family ..."
    This code is no longer needed, since the experiment is not viable.
    
    > Add instrumentation to the MessagePumpMac family of classes.
    >
    > This adds UMA-uploaded histograms of sampling intervals for key points of data:
    > * Total time spent in a CFRunLoop
    > * Total time waiting in CFRunLoop
    > * Total time servicing MessagePump::Delegate (the MessageLoop)
    > * The MessageLoop queue size and queueing delay before servicing
    >
    > It adds 1 second sampling for 15 seconds at startup, only for the main thread
    > (NSApplication-based) run loops.
    >
    > The data will be used to determine if adding scheduling-like behavior to the
    > MessagePump will result in more efficient processing of the MessageLoop work.
    >
    > An unforunate side effect of this change is exposing another method on the
    > MessagePump::Delegate interface, but there does not appear to be a better way
    > to do this.
    >
    > BUG=264886
    > R=jar@chromium.org, mark@chromium.org
    >
    > Review URL: https://codereview.chromium.org/22911026
    
    TBR=rsesek@chromium.org
    
    Review URL: https://codereview.chromium.org/360373004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@280939 0039d316-1c4b-4281-b951-d872f2087c98

--- base/message_loop/message_pump_mac.h
+++ base/message_loop/message_pump_mac.h
@@ -59,7 +59,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
-class MessagePumpInstrumentation;
 class RunLoop;
 class TimeTicks;
 
@@ -113,11 +112,6 @@ class MessagePumpCFRunLoopBase : public MessagePump {
   // objects autoreleased by work to fall into the current autorelease pool.
   virtual AutoreleasePoolType* CreateAutoreleasePool();
 
-  // Enables instrumentation of the MessagePump. See MessagePumpInstrumentation
-  // in the implementation for details.
-  void EnableInstrumentation();
-  WeakPtr<MessagePumpInstrumentation> instrumentation_;
-
  private:
   // Timer callback scheduled by ScheduleDelayedWork.  This does not do any
   // work, but it signals work_source_ so that delayed work can be performed
@@ -131,11 +125,11 @@ class MessagePumpCFRunLoopBase : public MessagePump {
   static void RunWorkSource(void* info);
   bool RunWork();
 
-  // Perform idle-priority work.  This is normally called by
-  // StartOrEndWaitObserver, but is also associated with idle_work_source_. When
-  // this function actually does perform idle work, it will resignal that
-  // source.  The static method calls the instance method; the instance method
-  // returns true if idle work was done.
+  // Perform idle-priority work.  This is normally called by PreWaitObserver,
+  // but is also associated with idle_work_source_.  When this function
+  // actually does perform idle work, it will resignal that source.  The
+  // static method calls the instance method; the instance method returns
+  // true if idle work was done.
   static void RunIdleWorkSource(void* info);
   bool RunIdleWork();
 
@@ -157,8 +151,8 @@ class MessagePumpCFRunLoopBase : public MessagePump {
 
   // Observer callback responsible for performing idle-priority work, before
   // the run loop goes to sleep.  Associated with idle_work_observer_.
-  static void StartOrEndWaitObserver(CFRunLoopObserverRef observer,
-                                     CFRunLoopActivity activity, void* info);
+  static void PreWaitObserver(CFRunLoopObserverRef observer,
+                              CFRunLoopActivity activity, void* info);
 
   // Observer callback called before the run loop processes any sources.
   // Associated with pre_source_observer_.
