36910b5eb82f90726b526e24890508462c4a3db0
alemate@chromium.org <alemate@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-02 18:01:50 +0000

    Store unsaved trace data on exit.
    This CL enables chromeos chrome startup trace when chrome in restarted on
    user log in: startup trace should be saved to disk on process exit if
    "trace-startup-duration" period has not ended yet.
    
    BUG=346913
    TEST=manual
    
    Review URL: https://codereview.chromium.org/359473006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281061 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/browser_main_loop.h
+++ content/browser/browser_main_loop.h
@@ -7,8 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_BROWSER_BROWSER_MAIN_LOOP_H_
 
 #include "base/basictypes.h"
+#include "base/files/file_path.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/timer/timer.h"
 #include "content/browser/browser_process_sub_thread.h"
 #include "content/public/browser/browser_main_runner.h"
 
@@ -105,6 +107,12 @@ class CONTENT_EXPORT BrowserMainLoop {
 
   bool is_tracing_startup() const { return is_tracing_startup_; }
 
+  const base::FilePath& startup_trace_file() const {
+    return startup_trace_file_;
+  }
+
+  void StopStartupTracingTimer();
+
 #if defined(OS_MACOSX) && !defined(OS_IOS)
   DeviceMonitorMac* device_monitor_mac() const {
     return device_monitor_mac_.get();
@@ -131,8 +139,10 @@ class CONTENT_EXPORT BrowserMainLoop {
 
   void MainMessageLoopRun();
 
+  base::FilePath GetStartupTraceFileName(
+      const base::CommandLine& command_line) const;
   void InitStartupTracing(const base::CommandLine& command_line);
-  void EndStartupTracing(const base::FilePath& trace_file);
+  void EndStartupTracing();
 
   // Members initialized on construction ---------------------------------------
   const MainFunctionParams& parameters_;
@@ -191,6 +201,10 @@ class CONTENT_EXPORT BrowserMainLoop {
   scoped_ptr<base::debug::TraceEventSystemStatsMonitor> system_stats_monitor_;
 
   bool is_tracing_startup_;
+  base::FilePath startup_trace_file_;
+
+  // This timer initiates trace file saving.
+  base::OneShotTimer<BrowserMainLoop> startup_trace_timer_;
 
   DISALLOW_COPY_AND_ASSIGN(BrowserMainLoop);
 };
--- content/browser/browser_shutdown_profile_dumper.h
+++ content/browser/browser_shutdown_profile_dumper.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/basictypes.h"
+#include "base/files/file_path.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/ref_counted_memory.h"
 #include "content/common/content_export.h"
@@ -30,18 +31,18 @@ namespace content {
 // |SequencedWorkerPool| will get killed in the shutdown process.
 class BrowserShutdownProfileDumper {
  public:
-  BrowserShutdownProfileDumper();
+  explicit BrowserShutdownProfileDumper(const base::FilePath& dump_file_name);
 
   ~BrowserShutdownProfileDumper();
 
+  // Returns the file name where we should save the shutdown trace dump to.
+  static base::FilePath GetShutdownProfileFileName();
+
  private:
   // Writes all traces which happened to disk.
-  void WriteTracesToDisc(const base::FilePath& file_name);
+  void WriteTracesToDisc();
 
   void EndTraceAndFlush(base::WaitableEvent* flush_complete_event);
-
-  // Returns the file name where we should save the trace dump to.
-  base::FilePath GetFileName();
 
   // The callback for the |TraceLog::Flush| function. It saves all traces to
   // disc.
@@ -61,6 +62,9 @@ class BrowserShutdownProfileDumper {
 
   // Closes the dump file.
   void CloseFile();
+
+  // The name of the dump file.
+  const base::FilePath dump_file_name_;
 
   // The number of blocks we have already written.
   int blocks_;
