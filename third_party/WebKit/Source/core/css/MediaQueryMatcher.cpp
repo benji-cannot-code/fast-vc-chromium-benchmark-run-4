2b9bc701446ef4f59c883a5616a67b8ef6aa3e6b
sigbjornf@opera.com <sigbjornf@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-02 14:06:51 +0000

    Oilpan: fix build after r177376.
    Move implementation of create() into .cpp to bring the MediaQueryEvaluator
    type completely into scope.
    
    TBR=haraken@chromium.org,oilpan-reviews@chromium.org
    BUG=
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/366913007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177388 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/MediaQueryMatcher.cpp
+++ third_party/WebKit/Source/core/css/MediaQueryMatcher.cpp
@@ -32,6 +32,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+PassRefPtrWillBeRawPtr<MediaQueryMatcher> MediaQueryMatcher::create(Document& document)
+{
+    return adoptRefWillBeNoop(new MediaQueryMatcher(document));
+}
+
 MediaQueryMatcher::MediaQueryMatcher(Document& document)
     : m_document(&document)
 {
