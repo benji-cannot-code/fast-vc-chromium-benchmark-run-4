b047b7a6ede97cedda288503a54307e12aa63f68
fmalita@chromium.org <fmalita@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-02 16:04:26 +0000

    Encapsulate FontFallbackList's page access logic.
    Minor cleanup: add a getter & setter to avoid code duplication in
    Font.cpp.
    
    R=jbroman@chormium.org,eae@chromium.org
    
    Review URL: https://codereview.chromium.org/362963003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177397 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/fonts/Font.cpp
+++ third_party/WebKit/Source/platform/fonts/Font.cpp
@@ -415,13 +415,10 @@ std::pair<GlyphData, GlyphPage*> Font::glyphDataAndPageForCharacter(UChar32 c, b
 
     unsigned pageNumber = (c / GlyphPage::size);
 
-    GlyphPageTreeNode* node = pageNumber ? m_fontFallbackList->m_pages.get(pageNumber) : m_fontFallbackList->m_pageZero;
+    GlyphPageTreeNode* node = m_fontFallbackList->getPageNode(pageNumber);
     if (!node) {
         node = GlyphPageTreeNode::getRootChild(fontDataAt(0), pageNumber);
-        if (pageNumber)
-            m_fontFallbackList->m_pages.set(pageNumber, node);
-        else
-            m_fontFallbackList->m_pageZero = node;
+        m_fontFallbackList->setPageNode(pageNumber, node);
     }
 
     GlyphPage* page = 0;
@@ -455,10 +452,7 @@ std::pair<GlyphData, GlyphPage*> Font::glyphDataAndPageForCharacter(UChar32 c, b
 
             // Proceed with the fallback list.
             node = node->getChild(fontDataAt(node->level()), pageNumber);
-            if (pageNumber)
-                m_fontFallbackList->m_pages.set(pageNumber, node);
-            else
-                m_fontFallbackList->m_pageZero = node;
+            m_fontFallbackList->setPageNode(pageNumber, node);
         }
     }
     if (variant != NormalVariant) {
@@ -492,10 +486,7 @@ std::pair<GlyphData, GlyphPage*> Font::glyphDataAndPageForCharacter(UChar32 c, b
 
             // Proceed with the fallback list.
             node = node->getChild(fontDataAt(node->level()), pageNumber);
-            if (pageNumber)
-                m_fontFallbackList->m_pages.set(pageNumber, node);
-            else
-                m_fontFallbackList->m_pageZero = node;
+            m_fontFallbackList->setPageNode(pageNumber, node);
         }
     }
 
