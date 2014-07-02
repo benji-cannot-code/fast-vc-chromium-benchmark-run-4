daddd02a921ed692d88cded0d2c008c498e3d151
dgozman@chromium.org <dgozman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-02 13:19:08 +0000

    [DevTools] Only issue Page.viewportChanged event when device emulation is enabled.
    BUG=none
    
    Review URL: https://codereview.chromium.org/362773003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177378 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorPageAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorPageAgent.cpp
@@ -1168,7 +1168,7 @@ void InspectorPageAgent::didScroll()
 
 void InspectorPageAgent::viewportChanged()
 {
-    if (!m_enabled)
+    if (!m_enabled || !m_deviceMetricsOverridden)
         return;
     IntSize contentsSize = m_page->deprecatedLocalMainFrame()->view()->contentsSize();
     IntRect viewRect = m_page->deprecatedLocalMainFrame()->view()->visibleContentRect();
