94757f8c51715747f883371f22779109ef46d035
ricea@chromium.org <ricea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-03 12:09:53 +0000

    Fix a use-after-free bug of the failure message.
    If the WebSocket connection fails, it is possible for
    WebSocketHandshakeStreamCreateHelper to have a pointer to a
    WebSocketBasicHandshakeStream that has been deleted. So it is not safe
    to store the failure message in WebSocketBasicHandshakeStream.
    
    Instead, store it in StreamRequestImpl where it is guaranteed to stay
    alive until the handshake completes.
    
    BUG=379645
    
    Review URL: https://codereview.chromium.org/368533002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281239 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_stream_factory_impl_unittest.cc
+++ net/http/http_stream_factory_impl_unittest.cc
@@ -101,10 +101,6 @@ class MockWebSocketHandshakeStream : public WebSocketHandshakeStreamBase {
     return scoped_ptr<WebSocketStream>();
   }
 
-  virtual std::string GetFailureMessage() const OVERRIDE {
-    return std::string();
-  }
-
  private:
   const StreamType type_;
 };
--- net/websockets/websocket_handshake_stream_create_helper_test.cc
+++ net/websockets/websocket_handshake_stream_create_helper_test.cc
@@ -86,6 +86,7 @@ class WebSocketHandshakeStreamCreateHelperTest : public ::testing::Test {
       const std::string& extra_response_headers) {
     WebSocketHandshakeStreamCreateHelper create_helper(&connect_delegate_,
                                                        sub_protocols);
+    create_helper.set_failure_message(&failure_message_);
 
     scoped_ptr<ClientSocketHandle> socket_handle =
         socket_handle_factory_.CreateClientSocketHandle(
@@ -139,6 +140,7 @@ class WebSocketHandshakeStreamCreateHelperTest : public ::testing::Test {
 
   MockClientSocketHandleFactory socket_handle_factory_;
   TestConnectDelegate connect_delegate_;
+  std::string failure_message_;
 };
 
 // Confirm that the basic case works as expected.
