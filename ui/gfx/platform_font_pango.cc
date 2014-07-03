67ecc86d8f030deb135143bb786ff72787ec6131
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-03 07:50:39 +0000

    linux: Get UI font from GtkLabel style.
    Make Gtk2UI use the Pango font description from GtkLabel's
    style rather than the gtk-font-name property from
    GtkSettings. Also log an error if the two values differ.
    
    BUG=375824
    
    Review URL: https://codereview.chromium.org/368483002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281219 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/platform_font_pango.cc
+++ ui/gfx/platform_font_pango.cc
@@ -258,10 +258,7 @@ std::string PlatformFontPango::GetDefaultFont() {
   return *default_font_description_;
 #else
   const gfx::LinuxFontDelegate* delegate = gfx::LinuxFontDelegate::instance();
-  if (delegate)
-    return delegate->GetDefaultFontName();
-
-  return "sans 10";
+  return delegate ? delegate->GetDefaultFontDescription() : "sans 10";
 #endif    // defined(OS_CHROMEOS)
 }
 
