51b1db801b4c587dfa658bcfe647b5dfc6c19638
mathp@chromium.org <mathp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-03 21:56:01 +0000

    [SuggestionsService] Add a caching layer for server thumbnails.
    In the ThumbnailManager, hook up a ProtoDatabase which will store the data between restarts. The data is loaded in memory at startup.
    
    BUG=385747
    TEST=ThumbnailManagerTest,ThumbnailManagerBrowserTest
    
    Review URL: https://codereview.chromium.org/358153002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281330 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/search/suggestions/suggestions_service_factory.cc
+++ chrome/browser/search/suggestions/suggestions_service_factory.cc
@@ -9,11 +9,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/incognito_helpers.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search/suggestions/blacklist_store.h"
+#include "chrome/browser/search/suggestions/proto/suggestions.pb.h"
 #include "chrome/browser/search/suggestions/suggestions_service.h"
 #include "chrome/browser/search/suggestions/suggestions_store.h"
 #include "chrome/common/pref_names.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
+#include "components/leveldb_proto/proto_database.h"
+#include "components/leveldb_proto/proto_database_impl.h"
 #include "components/pref_registry/pref_registry_syncable.h"
+#include "content/public/browser/browser_context.h"
+#include "content/public/browser/browser_thread.h"
 
 namespace suggestions {
 
@@ -46,13 +51,27 @@ content::BrowserContext* SuggestionsServiceFactory::GetBrowserContextToUse(
 
 KeyedService* SuggestionsServiceFactory::BuildServiceInstanceFor(
     content::BrowserContext* profile) const {
+  scoped_refptr<base::SequencedTaskRunner> background_task_runner =
+      content::BrowserThread::GetBlockingPool()->GetSequencedTaskRunner(
+          content::BrowserThread::GetBlockingPool()->GetSequenceToken());
+
   Profile* the_profile = static_cast<Profile*>(profile);
   scoped_ptr<SuggestionsStore> suggestions_store(
       new SuggestionsStore(the_profile->GetPrefs()));
-  scoped_ptr<ThumbnailManager> thumbnail_manager(
-      new ThumbnailManager(the_profile->GetRequestContext()));
   scoped_ptr<BlacklistStore> blacklist_store(
       new BlacklistStore(the_profile->GetPrefs()));
+
+  scoped_ptr<leveldb_proto::ProtoDatabaseImpl<ThumbnailData> > db(
+      new leveldb_proto::ProtoDatabaseImpl<ThumbnailData>(
+          background_task_runner));
+
+  base::FilePath database_dir(
+      the_profile->GetPath().Append(FILE_PATH_LITERAL("Thumbnails")));
+
+  scoped_ptr<ThumbnailManager> thumbnail_manager(new ThumbnailManager(
+      the_profile->GetRequestContext(),
+      db.PassAs<leveldb_proto::ProtoDatabase<ThumbnailData> >(),
+      database_dir));
   return new SuggestionsService(the_profile, suggestions_store.Pass(),
                                 thumbnail_manager.Pass(),
                                 blacklist_store.Pass());
