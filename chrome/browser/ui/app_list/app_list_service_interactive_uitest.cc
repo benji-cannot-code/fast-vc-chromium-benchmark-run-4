473252d2fc85fff11e596a46cf8808278bea509a
tapted@chromium.org <tapted@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-03 07:28:41 +0000

    Fix a DCHECK hit during app list warm start cases.
    The DCHECK was added to ensure the UMA collected was consistent with the
    assumptions being made in r270615.
    
    The assumptions were wrong, but we don't want to change the data
    collected without first correcting those assumptions.
    
    ShowAppListInteractiveTest.ShowAppListFlag is updated to cover the fixed
    DCHECK code path.
    
    BUG=386966
    
    Review URL: https://codereview.chromium.org/362933002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281213 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/app_list/app_list_service_interactive_uitest.cc
+++ chrome/browser/ui/app_list/app_list_service_interactive_uitest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/app_list/test/chrome_app_list_test_support.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/host_desktop.h"
+#include "chrome/browser/ui/startup/startup_browser_creator.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
@@ -173,13 +174,29 @@ IN_PROC_BROWSER_TEST_F(ShowAppListInteractiveTest, ShowAppListFlag) {
   AppListService* service = test::GetAppListService();
   // The app list should already be shown because we passed
   // switches::kShowAppList.
-  ASSERT_TRUE(service->IsAppListVisible());
+  EXPECT_TRUE(service->IsAppListVisible());
 
   // Create a browser to prevent shutdown when we dismiss the app list.  We
   // need to do this because switches::kShowAppList suppresses the creation of
   // any browsers.
-  CreateBrowser(service->GetCurrentAppListProfile());
+  Profile* profile = service->GetCurrentAppListProfile();
+  CreateBrowser(profile);
+
   service->DismissAppList();
+  EXPECT_FALSE(service->IsAppListVisible());
+
+  // With Chrome still running, test receiving a second --show-app-list request
+  // via the process singleton. ChromeOS has no process singleton so exclude it.
+#if !defined(OS_CHROMEOS)
+  CommandLine command_line(CommandLine::NO_PROGRAM);
+  command_line.AppendSwitch(switches::kShowAppList);
+  StartupBrowserCreator::ProcessCommandLineAlreadyRunning(
+      command_line, base::FilePath(), profile->GetPath());
+
+  EXPECT_TRUE(service->IsAppListVisible());
+  service->DismissAppList();
+  EXPECT_FALSE(service->IsAppListVisible());
+#endif
 }
 
 // Interactive UI test that creates a non-default profile and configures it for
