e39462c561801a3476ca51e30bfe51fdbec3539b
brandonsalmon@chromium.org <brandonsalmon@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-03 06:37:14 +0000

    Improving and adding an in-memory MRU cache to DiskBasedCertCache.
    Review URL: https://codereview.chromium.org/361513003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281191 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/disk_based_cert_cache.h
+++ net/http/disk_based_cert_cache.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/containers/hash_tables.h"
+#include "base/containers/mru_cache.h"
 #include "base/memory/weak_ptr.h"
 #include "net/base/net_export.h"
 #include "net/cert/x509_certificate.h"
@@ -47,9 +48,28 @@ class NET_EXPORT_PRIVATE DiskBasedCertCache {
   void Set(const X509Certificate::OSCertHandle cert_handle,
            const SetCallback& cb);
 
+  // Returns the number of in-memory MRU cache hits that have occured
+  // on Set and Get operations. Intended for test purposes only.
+  size_t mem_cache_hits_for_testing() const { return mem_cache_hits_; }
+
+  // Returns the number of in-memory MRU cache misses that have occured
+  // on Set and Get operations. Intended for test purposes only.
+  size_t mem_cache_misses_for_testing() const { return mem_cache_misses_; }
+
  private:
   class ReadWorker;
   class WriteWorker;
+
+  // A functor used to free an OSCertHandle. Used by the MRUCertCache.
+  struct CertFree {
+    void operator()(X509Certificate::OSCertHandle cert_handle);
+  };
+
+  // An in-memory cache that is used to prevent redundant reads and writes
+  // to and from the disk cache.
+  typedef base::MRUCacheBase<std::string,
+                             X509Certificate::OSCertHandle,
+                             CertFree> MRUCertCache;
 
   // ReadWorkerMap and WriteWorkerMap map cache keys to their
   // corresponding Workers.
@@ -59,13 +79,20 @@ class NET_EXPORT_PRIVATE DiskBasedCertCache {
   // FinishedReadOperation and FinishedWriteOperation are used by callbacks
   // given to the workers to signal the DiskBasedCertCache they have completed
   // their work.
-  void FinishedReadOperation(const std::string& key);
-  void FinishedWriteOperation(const std::string& key);
+  void FinishedReadOperation(const std::string& key,
+                             X509Certificate::OSCertHandle cert_handle);
+  void FinishedWriteOperation(const std::string& key,
+                              X509Certificate::OSCertHandle cert_handle);
+
+  disk_cache::Backend* backend_;
 
   ReadWorkerMap read_worker_map_;
   WriteWorkerMap write_worker_map_;
+  MRUCertCache mru_cert_cache_;
 
-  disk_cache::Backend* backend_;
+  int mem_cache_hits_;
+  int mem_cache_misses_;
+
   base::WeakPtrFactory<DiskBasedCertCache> weak_factory_;
   DISALLOW_COPY_AND_ASSIGN(DiskBasedCertCache);
 };
