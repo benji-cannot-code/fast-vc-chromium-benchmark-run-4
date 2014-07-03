a9197182db60b6ee6e068a85474e67614213dac1
sohan.jyoti@samsung.com <sohan.jyoti@samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-03 08:50:11 +0000

    cc: Hide Gpu Rasterization details in Resource Provider.
    This moves ganesh and gl context code out to resource
    provider from gpurasterworkerpool to avoid making any
    assumptions how the gpu raster buffers are implemented.
    
    BUG=390399
    
    Review URL: https://codereview.chromium.org/363563006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281225 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/resources/gpu_raster_worker_pool.h
+++ cc/resources/gpu_raster_worker_pool.h
@@ -22,8 +22,7 @@ class CC_EXPORT GpuRasterWorkerPool : public RasterWorkerPool,
 
   static scoped_ptr<RasterWorkerPool> Create(
       base::SequencedTaskRunner* task_runner,
-      ResourceProvider* resource_provider,
-      ContextProvider* context_provider);
+      ResourceProvider* resource_provider);
 
   // Overridden from RasterWorkerPool:
   virtual Rasterizer* AsRasterizer() OVERRIDE;
@@ -40,8 +39,7 @@ class CC_EXPORT GpuRasterWorkerPool : public RasterWorkerPool,
 
  private:
   GpuRasterWorkerPool(base::SequencedTaskRunner* task_runner,
-                      ResourceProvider* resource_provider,
-                      ContextProvider* context_provider);
+                      ResourceProvider* resource_provider);
 
   void OnRasterFinished();
   void OnRasterRequiredForActivationFinished();
@@ -54,7 +52,6 @@ class CC_EXPORT GpuRasterWorkerPool : public RasterWorkerPool,
   const NamespaceToken namespace_token_;
   RasterizerClient* client_;
   ResourceProvider* resource_provider_;
-  ContextProvider* context_provider_;
 
   bool run_tasks_on_origin_thread_pending_;
 
