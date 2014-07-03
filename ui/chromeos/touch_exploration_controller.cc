ebec2a8f9b418b011f47e27a3f22da820fe640da
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-03 11:49:10 +0000

    Revert of Added touch event permutations test to touch_exploration_controller. (https://codereview.chromium.org/358693004/)
    Reason for revert:
    New test is failing on Linux ChromiumOS Tests (dbg) 
    http://goo.gl/rwFMMz
    
    Original issue's description:
    > Added touch event permutations test to touch_exploration_controller.
    > All permutations of three fingers pressing, moving, and releasing are now being tested. Fingers that were recorded as pressed during the permutation but not released yet are released, and then there is a check for no fingers down.
    > 
    > BUG=388520
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=281195
    
    TBR=dmazzoni@chromium.org,aboxhall@chromium.org,evy@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=388520
    
    Review URL: https://codereview.chromium.org/367263005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281235 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/chromeos/touch_exploration_controller.cc
+++ ui/chromeos/touch_exploration_controller.cc
@@ -24,8 +24,7 @@ TouchExplorationController::TouchExplorationController(
     : root_window_(root_window),
       state_(NO_FINGERS_DOWN),
       event_handler_for_testing_(NULL),
-      prev_state_(NO_FINGERS_DOWN),
-      VLOG_on_(true) {
+      prev_state_(NO_FINGERS_DOWN) {
   CHECK(root_window);
   root_window->GetHost()->GetEventSource()->AddEventRewriter(this);
 }
@@ -41,13 +40,6 @@ void TouchExplorationController::CallTapTimerNowForTesting() {
   OnTapTimerFired();
 }
 
-void TouchExplorationController::CallTapTimerNowIfRunningForTesting() {
-  if (tap_timer_.IsRunning()) {
-    tap_timer_.Stop();
-    OnTapTimerFired();
-  }
-}
-
 void TouchExplorationController::SetEventHandlerForTesting(
     ui::EventHandler* event_handler_for_testing) {
   event_handler_for_testing_ = event_handler_for_testing;
@@ -57,18 +49,14 @@ bool TouchExplorationController::IsInNoFingersDownStateForTesting() const {
   return state_ == NO_FINGERS_DOWN;
 }
 
-void TouchExplorationController::SuppressVLOGsForTesting(bool suppress) {
-    VLOG_on_ = !suppress;
-}
-
 ui::EventRewriteStatus TouchExplorationController::RewriteEvent(
     const ui::Event& event,
     scoped_ptr<ui::Event>* rewritten_event) {
   if (!event.IsTouchEvent()) {
     if (event.IsKeyEvent()) {
       const ui::KeyEvent& key_event = static_cast<const ui::KeyEvent&>(event);
-      VLOG(0) << "\nKeyboard event: " << key_event.name()
-              << "\n Key code: " << key_event.key_code()
+      VLOG(0) << "\nKeyboard event: " << key_event.name() << "\n"
+              << " Key code: " << key_event.key_code()
               << ", Flags: " << key_event.flags()
               << ", Is char: " << key_event.is_char();
     }
@@ -169,7 +157,7 @@ ui::EventRewriteStatus TouchExplorationController::InNoFingersDown(
     VLOG_STATE();
     return ui::EVENT_REWRITE_DISCARD;
   }
-  NOTREACHED() << "Unexpected event type received: " << event.name();;
+  NOTREACHED() << "Unexpected event type received.";
   return ui::EVENT_REWRITE_CONTINUE;
 }
 
@@ -209,7 +197,7 @@ ui::EventRewriteStatus TouchExplorationController::InSingleTapPressed(
     }
     return EVENT_REWRITE_DISCARD;
   }
-  NOTREACHED() << "Unexpected event type received: " << event.name();;
+  NOTREACHED() << "Unexpected event type received.";
   return ui::EVENT_REWRITE_CONTINUE;
 }
 
@@ -241,10 +229,8 @@ TouchExplorationController::InSingleTapOrTouchExploreReleased(
       ResetToNoFingersDown();
     }
     return ui::EVENT_REWRITE_DISCARD;
-  } else if (type == ui::ET_TOUCH_MOVED){
-    return ui::EVENT_REWRITE_DISCARD;
   }
-  NOTREACHED() << "Unexpected event type received: " << event.name();
+  NOTREACHED() << "Unexpected event type received.";
   return ui::EVENT_REWRITE_CONTINUE;
 }
 
@@ -270,7 +256,7 @@ ui::EventRewriteStatus TouchExplorationController::InDoubleTapPressed(
   } else if (type == ui::ET_TOUCH_MOVED) {
     return ui::EVENT_REWRITE_DISCARD;
   }
-  NOTREACHED() << "Unexpected event type received: " << event.name();
+  NOTREACHED() << "Unexpected event type received.";
   return ui::EVENT_REWRITE_CONTINUE;
 }
 
@@ -301,7 +287,7 @@ ui::EventRewriteStatus TouchExplorationController::InTouchExploration(
     state_ = TOUCH_EXPLORE_RELEASED;
     VLOG_STATE();
   } else if (type != ui::ET_TOUCH_MOVED) {
-    NOTREACHED() << "Unexpected event type received: " << event.name();
+    NOTREACHED() << "Unexpected event type received.";
     return ui::EVENT_REWRITE_CONTINUE;
   }
 
@@ -310,6 +296,7 @@ ui::EventRewriteStatus TouchExplorationController::InTouchExploration(
   last_touch_exploration_.reset(new TouchEvent(event));
   return ui::EVENT_REWRITE_REWRITTEN;
 }
+
 
 ui::EventRewriteStatus TouchExplorationController::InTwoToOneFinger(
     const ui::TouchEvent& event,
@@ -369,7 +356,7 @@ ui::EventRewriteStatus TouchExplorationController::InTwoToOneFinger(
       return ui::EVENT_REWRITE_REWRITTEN;
     }
   }
-  NOTREACHED() << "Unexpected event type received: " << event.name();
+  NOTREACHED() << "Unexpected event type received";
   return ui::EVENT_REWRITE_CONTINUE;
 }
 
@@ -380,7 +367,7 @@ ui::EventRewriteStatus TouchExplorationController::InPassthrough(
 
   if (!(type == ui::ET_TOUCH_RELEASED || type == ui::ET_TOUCH_CANCELLED ||
         type == ui::ET_TOUCH_MOVED || type == ui::ET_TOUCH_PRESSED)) {
-    NOTREACHED() << "Unexpected event type received: " << event.name();
+    NOTREACHED() << "Unexpected event type received.";
     return ui::EVENT_REWRITE_CONTINUE;
   }
 
@@ -432,7 +419,7 @@ ui::EventRewriteStatus TouchExplorationController::InTouchExploreSecondPress(
     VLOG_STATE();
     return ui::EVENT_REWRITE_REWRITTEN;
   }
-  NOTREACHED() << "Unexpected event type received: " << event.name();
+  NOTREACHED() << "Unexpected event type received.";
   return ui::EVENT_REWRITE_CONTINUE;
 }
 
@@ -442,7 +429,7 @@ ui::EventRewriteStatus TouchExplorationController::InWaitForRelease(
   ui::EventType type = event.type();
   if (!(type == ui::ET_TOUCH_PRESSED || type == ui::ET_TOUCH_MOVED ||
         type == ui::ET_TOUCH_RELEASED || type == ui::ET_TOUCH_CANCELLED)) {
-    NOTREACHED() << "Unexpected event type received: " << event.name();
+    NOTREACHED() << "Unexpected event type received.";
     return ui::EVENT_REWRITE_CONTINUE;
   }
   if (current_touch_ids_.size() == 0) {
@@ -514,8 +501,6 @@ void TouchExplorationController::ResetToNoFingersDown() {
 }
 
 void TouchExplorationController::VlogState(const char* function_name) {
-  if (!VLOG_on_)
-    return;
   if (prev_state_ == state_)
     return;
   prev_state_ = state_;
@@ -526,9 +511,6 @@ void TouchExplorationController::VlogState(const char* function_name) {
 
 void TouchExplorationController::VlogEvent(const ui::TouchEvent& touch_event,
                                            const char* function_name) {
-  if (!VLOG_on_)
-    return;
-
   CHECK(touch_event.IsTouchEvent());
   if (prev_event_ != NULL &&
       prev_event_->type() == touch_event.type() &&
--- ui/chromeos/touch_exploration_controller.h
+++ ui/chromeos/touch_exploration_controller.h
@@ -91,12 +91,8 @@ class UI_CHROMEOS_EXPORT TouchExplorationController
   virtual ~TouchExplorationController();
 
   void CallTapTimerNowForTesting();
-  void CallTapTimerNowIfRunningForTesting();
   void SetEventHandlerForTesting(ui::EventHandler* event_handler_for_testing);
   bool IsInNoFingersDownStateForTesting() const;
-  // VLOGs should be suppressed in tests that generate a lot of logs,
-  // for example permutations of nine touch events.
-  void SuppressVLOGsForTesting(bool suppress);
 
  private:
   // Overridden from ui::EventRewriter
@@ -254,9 +250,6 @@ class UI_CHROMEOS_EXPORT TouchExplorationController
 
   // A copy of the previous event passed.
   scoped_ptr<ui::TouchEvent> prev_event_;
-
-  // This toggles whether VLOGS are turned on or not.
-  bool VLOG_on_;
 
   DISALLOW_COPY_AND_ASSIGN(TouchExplorationController);
 };
