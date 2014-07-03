2eb76c969b7f13d34ec3de94072f3e0636e69002
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-03 12:29:19 +0000

    Files.app: Update the metadata of changed file instead of full-update
    Since the detailed changed event has been provided, we can make the metadata-updating efficiently. With this patch, instead of updating full-file, Files.app updates only changed file.
    
    BUG=131198
    
    Review URL: https://codereview.chromium.org/365993010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281248 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/file_manager/file_manager/foreground/js/file_watcher.js
+++ ui/file_manager/file_manager/foreground/js/file_watcher.js
@@ -51,6 +51,7 @@ FileWatcher.prototype.onDirectoryChanged_ = function(event) {
   if (this.watchedDirectoryEntry_ &&
       event.entry.toURL() === this.watchedDirectoryEntry_.toURL()) {
     var e = new Event('watcher-directory-changed');
+    e.changedFiles = event.changedFiles;
     this.dispatchEvent(e);
   }
 };
