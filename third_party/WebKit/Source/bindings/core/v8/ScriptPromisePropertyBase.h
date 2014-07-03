8c2f620a17d48f87581dbfd4b1e8d24661beea30
sigbjornf@opera.com <sigbjornf@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-03 10:01:50 +0000

    Oilpan: fix build after r177450.
    Update ScriptPromise tests to be Oilpan compatible.
    
    R=haraken@chromium.org
    BUG=363967
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/367263004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177463 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/core/v8/ScriptPromisePropertyBase.h
+++ third_party/WebKit/Source/bindings/core/v8/ScriptPromisePropertyBase.h
@@ -17,7 +17,7 @@ namespace WebCore {
 
 class ExecutionContext;
 
-class ScriptPromisePropertyBase : public ContextLifecycleObserver, public RefCounted<ScriptPromisePropertyBase> {
+class ScriptPromisePropertyBase : public RefCountedWillBeRefCountedGarbageCollected<ScriptPromisePropertyBase>, public ContextLifecycleObserver {
 public:
     virtual ~ScriptPromisePropertyBase();
 
@@ -35,6 +35,8 @@ public:
     State state() const { return m_state; }
 
     ScriptPromise promise(DOMWrapperWorld&);
+
+    virtual void trace(Visitor*) { }
 
 protected:
     ScriptPromisePropertyBase(ExecutionContext*, Name);
