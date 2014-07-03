e99a837b4e73691baa8b93d94346b7358bb89dd3
csharp@chromium.org <csharp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-03 18:20:04 +0000

    Revert 281282 "Revert 281133 "chrome.hid: enrich model with repo..."
    Tests still failed, undoing revert.
    
    > Revert 281133 "chrome.hid: enrich model with report IDs"
    > 
    > Speculative revert to try and fix Win browser tests.
    > 
    > > chrome.hid: enrich model with report IDs
    > > 
    > > - add report IDs and max report size
    > > - don't expose sensitive usages
    > > 
    > > BUG=364423
    > > R=rockot@chromium.org
    > > TESTS=run device_unittests (HidReportDescriptorTest)
    > > 
    > > Review URL: https://codereview.chromium.org/317783010
    > 
    > TBR=jracle@logitech.com
    > 
    > Review URL: https://codereview.chromium.org/369923002
    
    TBR=csharp@chromium.org
    
    Review URL: https://codereview.chromium.org/364213005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281300 0039d316-1c4b-4281-b951-d872f2087c98

--- device/hid/hid_connection_linux.h
+++ device/hid/hid_connection_linux.h
@@ -9,10 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <queue>
 
 #include "base/files/file.h"
-#include "base/memory/ref_counted.h"
 #include "base/message_loop/message_pump_libevent.h"
 #include "device/hid/hid_connection.h"
-#include "device/hid/hid_device_info.h"
 
 namespace device {
 
@@ -21,19 +19,22 @@ class HidConnectionLinux : public HidConnection,
  public:
   HidConnectionLinux(HidDeviceInfo device_info, std::string dev_node);
 
-  virtual void Read(scoped_refptr<net::IOBufferWithSize> buffer,
-                    const IOCallback& callback) OVERRIDE;
-  virtual void Write(uint8_t report_id,
-                     scoped_refptr<net::IOBufferWithSize> buffer,
-                     const IOCallback& callback) OVERRIDE;
-  virtual void GetFeatureReport(uint8_t report_id,
-                                scoped_refptr<net::IOBufferWithSize> buffer,
-                                const IOCallback& callback) OVERRIDE;
-  virtual void SendFeatureReport(uint8_t report_id,
-                                 scoped_refptr<net::IOBufferWithSize> buffer,
-                                 const IOCallback& callback) OVERRIDE;
+  // HidConnection implementation.
+  virtual void PlatformRead(scoped_refptr<net::IOBufferWithSize> buffer,
+                            const IOCallback& callback) OVERRIDE;
+  virtual void PlatformWrite(uint8_t report_id,
+                             scoped_refptr<net::IOBufferWithSize> buffer,
+                             const IOCallback& callback) OVERRIDE;
+  virtual void PlatformGetFeatureReport(
+      uint8_t report_id,
+      scoped_refptr<net::IOBufferWithSize> buffer,
+      const IOCallback& callback) OVERRIDE;
+  virtual void PlatformSendFeatureReport(
+      uint8_t report_id,
+      scoped_refptr<net::IOBufferWithSize> buffer,
+      const IOCallback& callback) OVERRIDE;
 
-  // Implements base::MessagePumpLibevent::Watcher
+  // base::MessagePumpLibevent::Watcher implementation.
   virtual void OnFileCanReadWithoutBlocking(int fd) OVERRIDE;
   virtual void OnFileCanWriteWithoutBlocking(int fd) OVERRIDE;
 
@@ -41,16 +42,17 @@ class HidConnectionLinux : public HidConnection,
   friend class base::RefCountedThreadSafe<HidConnectionLinux>;
   virtual ~HidConnectionLinux();
 
-  void ProcessReadQueue();
   void Disconnect();
+
+  void Flush();
+  void ProcessInputReport(scoped_refptr<net::IOBufferWithSize> buffer);
+  void ProcessReadQueue();
 
   base::File device_file_;
   base::MessagePumpLibevent::FileDescriptorWatcher device_file_watcher_;
 
   std::queue<PendingHidReport> pending_reports_;
   std::queue<PendingHidRead> pending_reads_;
-
-  base::ThreadChecker thread_checker_;
 
   DISALLOW_COPY_AND_ASSIGN(HidConnectionLinux);
 };
