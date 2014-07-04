a65fb9df7282de6156fb76631b9c75e1829fbab9
mathp@chromium.org <mathp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-04 17:25:28 +0000

    [Constants] Add URL constants for new chrome:// page.
    Related to contextual search feature.
    
    BUG=374932
    
    Review URL: https://codereview.chromium.org/371463010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281419 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/url_constants.cc
+++ chrome/common/url_constants.cc
@@ -33,6 +33,8 @@ const char kChromeUICloudPrintResourcesURL[] = "chrome://cloudprintresources/";
 const char kChromeUIComponentsURL[] = "chrome://components/";
 const char kChromeUIConflictsURL[] = "chrome://conflicts/";
 const char kChromeUIConstrainedHTMLTestURL[] = "chrome://constrained-test/";
+const char kChromeUIContextualSearchPromoURL[] =
+    "chrome://contextual-search-promo";
 const char kChromeUICrashesURL[] = "chrome://crashes/";
 const char kChromeUICreditsURL[] = "chrome://credits/";
 const char kChromeUIDevicesURL[] = "chrome://devices/";
@@ -162,6 +164,7 @@ const char kChromeUICloudPrintResourcesHost[] = "cloudprintresources";
 const char kChromeUICloudPrintSetupHost[] = "cloudprintsetup";
 const char kChromeUIConflictsHost[] = "conflicts";
 const char kChromeUIConstrainedHTMLTestHost[] = "constrained-test";
+const char kChromeUIContextualSearchPromoHost[] = "contextual-search-promo";
 const char kChromeUICrashesHost[] = "crashes";
 const char kChromeUICrashHost[] = "crash";
 const char kChromeUICreditsHost[] = "credits";
