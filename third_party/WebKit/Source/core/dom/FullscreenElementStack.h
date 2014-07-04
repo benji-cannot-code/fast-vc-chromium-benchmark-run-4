29f3f6016acb01aa3ad326a4fa34daba511036d7
philipj@opera.com <philipj@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-04 05:38:45 +0000

    Merge the fullscreen event queues
    The two queues are both processed by a single timer, so they might just
    as well be a single queue, which also avoids looping over each queue
    separately.
    
    Instead of queuing Nodes, Events are created and queued. This means that
    the event type is made explicit, which will make it simpler to support
    both prefixed and unprefixed events using the same code.
    
    BUG=383813
    
    Review URL: https://codereview.chromium.org/365903006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177509 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/FullscreenElementStack.h
+++ third_party/WebKit/Source/core/dom/FullscreenElementStack.h
@@ -106,8 +106,10 @@ private:
     void clearFullscreenElementStack();
     void popFullscreenElementStack();
     void pushFullscreenElementStack(Element&);
-    void addDocumentToFullScreenChangeEventQueue(Document&);
-    void fullScreenChangeDelayTimerFired(Timer<FullscreenElementStack>*);
+
+    void enqueueChangeEvent(Document&);
+    void enqueueErrorEvent(Element&);
+    void eventQueueTimerFired(Timer<FullscreenElementStack>*);
 
     void fullScreenElementRemoved();
 
@@ -115,9 +117,8 @@ private:
     RefPtrWillBeMember<Element> m_fullScreenElement;
     WillBeHeapVector<RefPtrWillBeMember<Element> > m_fullScreenElementStack;
     RenderFullScreen* m_fullScreenRenderer;
-    Timer<FullscreenElementStack> m_fullScreenChangeDelayTimer;
-    WillBeHeapDeque<RefPtrWillBeMember<Node> > m_fullScreenChangeEventTargetQueue;
-    WillBeHeapDeque<RefPtrWillBeMember<Node> > m_fullScreenErrorEventTargetQueue;
+    Timer<FullscreenElementStack> m_eventQueueTimer;
+    WillBeHeapDeque<RefPtrWillBeMember<Event> > m_eventQueue;
     LayoutRect m_savedPlaceholderFrameRect;
     RefPtr<RenderStyle> m_savedPlaceholderRenderStyle;
 };
