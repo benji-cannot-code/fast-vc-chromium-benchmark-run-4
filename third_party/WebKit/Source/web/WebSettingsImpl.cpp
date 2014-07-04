377001ff301d6403301729ff6e88438e638da88d
senorblanco@chromium.org <senorblanco@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-04 02:05:43 +0000

    Revert "Don't show video fullscreen until DidEnterFullscreen() is called, and disable fullscreen for non-media elements for android webview" (r177458).
    Seems to be breaking android_webview_instrumentation_tests
    AwContentsClientGetVideoLoadingProgressViewTest#testGetVideoLoadingProgressView
    
    This reverts commit 3c62291af9ab9781c454868f6a9bf8a038c6ffda.
    
    TBR=lushnikov,pilgrim@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/363323006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177498 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebSettingsImpl.cpp
+++ third_party/WebKit/Source/web/WebSettingsImpl.cpp
@@ -60,7 +60,6 @@ WebSettingsImpl::WebSettingsImpl(Settings* settings, InspectorController* inspec
     , m_viewportMetaNonUserScalableQuirk(false)
     , m_clobberUserAgentInitialScaleQuirk(false)
     , m_mainFrameResizesAreOrientationChanges(false)
-    , m_disallowFullscreenForNonMediaElements(false)
 {
     ASSERT(settings);
 }
@@ -711,11 +710,6 @@ void WebSettingsImpl::setUseSolidColorScrollbars(bool enabled)
 void WebSettingsImpl::setMainFrameResizesAreOrientationChanges(bool enabled)
 {
     m_mainFrameResizesAreOrientationChanges = enabled;
-}
-
-void WebSettingsImpl::setDisallowFullscreenForNonMediaElements(bool enabled)
-{
-    m_disallowFullscreenForNonMediaElements = enabled;
 }
 
 } // namespace blink
