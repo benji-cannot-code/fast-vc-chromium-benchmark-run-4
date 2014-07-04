0c914c3714c4a379f75a2eaff9d63cc8144027f6
lushnikov@chromium.org <lushnikov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-04 07:33:36 +0000

    DevTools: reveal console in drawer on "evaluate in console" command
    Reveal console view in drawer on "evaluate in console" command from
    JavaScriptSourceFrame.
    
    BUG=389501
    
    Review URL: https://codereview.chromium.org/368243003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177514 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sdk/ConsoleModel.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/ConsoleModel.js
@@ -183,6 +183,7 @@ WebInspector.ConsoleModel.evaluateCommandInConsole = function(executionContext, 
         if (!result)
             return;
 
+        this.show();
         this.dispatchEventToListeners(WebInspector.ConsoleModel.Events.CommandEvaluated, {result: result, wasThrown: wasThrown, text: text, commandMessage: commandMessage});
     }
 
