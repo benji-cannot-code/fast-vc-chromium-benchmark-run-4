4b2548effad7d3146a9c454d1e4e7fa18be2761d
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-07 23:02:22 +0000

    Move hasInlineTransform to RenderStyle
    Moving this bit into RenderStyle makes the handling of inline transforms less
    of a special case.
    
    R=esprehn@chromium.org
    
    Review URL: https://codereview.chromium.org/369813004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177612 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/style/StyleRareNonInheritedData.cpp
+++ third_party/WebKit/Source/core/rendering/style/StyleRareNonInheritedData.cpp
@@ -95,6 +95,7 @@ StyleRareNonInheritedData::StyleRareNonInheritedData()
     , m_justifySelfOverflowAlignment(RenderStyle::initialJustifySelfOverflowAlignment())
     , m_scrollBehavior(RenderStyle::initialScrollBehavior())
     , m_requiresAcceleratedCompositingForExternalReasons(false)
+    , m_hasInlineTransform(false)
 {
     m_maskBoxImage.setMaskDefaults();
 }
@@ -178,6 +179,7 @@ StyleRareNonInheritedData::StyleRareNonInheritedData(const StyleRareNonInherited
     , m_justifySelfOverflowAlignment(o.m_justifySelfOverflowAlignment)
     , m_scrollBehavior(o.m_scrollBehavior)
     , m_requiresAcceleratedCompositingForExternalReasons(o.m_requiresAcceleratedCompositingForExternalReasons)
+    , m_hasInlineTransform(o.m_hasInlineTransform)
 {
 }
 
@@ -264,7 +266,8 @@ bool StyleRareNonInheritedData::operator==(const StyleRareNonInheritedData& o) c
         && m_justifySelf == o.m_justifySelf
         && m_justifySelfOverflowAlignment == o.m_justifySelfOverflowAlignment
         && m_scrollBehavior == o.m_scrollBehavior
-        && m_requiresAcceleratedCompositingForExternalReasons == o.m_requiresAcceleratedCompositingForExternalReasons;
+        && m_requiresAcceleratedCompositingForExternalReasons == o.m_requiresAcceleratedCompositingForExternalReasons
+        && m_hasInlineTransform == o.m_hasInlineTransform;
 }
 
 bool StyleRareNonInheritedData::contentDataEquivalent(const StyleRareNonInheritedData& o) const
