5b5eba68706941fc1ac04e1d99378b23eeb7d239
dmikurube@chromium.org <dmikurube@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-07 10:56:51 +0000

    Send memory usage growth per 30 minutes in renderer processes via UMA.
    BUG=379659
    
    Review URL: https://codereview.chromium.org/308963005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281517 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/memory_details_android.cc
+++ chrome/browser/memory_details_android.cc
@@ -98,7 +98,8 @@ void GetAllChildren(const std::vector<ProcessEntry>& processes,
 }  // namespace
 
 MemoryDetails::MemoryDetails()
-    : user_metrics_mode_(UPDATE_USER_METRICS) {
+    : user_metrics_mode_(UPDATE_USER_METRICS),
+      memory_growth_tracker_(NULL) {
 }
 
 ProcessData* MemoryDetails::ChromeBrowser() {
--- chrome/browser/metrics/chrome_metrics_service_client.h
+++ chrome/browser/metrics/chrome_metrics_service_client.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/threading/thread_checker.h"
+#include "chrome/browser/memory_details.h"
 #include "chrome/browser/metrics/network_stats_uploader.h"
 #include "chrome/browser/metrics/tracking_synchronizer_observer.h"
 #include "components/metrics/metrics_service_client.h"
@@ -165,6 +166,10 @@ class ChromeMetricsServiceClient
 
   // Callback that is called when initial metrics gathering is complete.
   base::Closure finished_gathering_initial_metrics_callback_;
+
+  // The MemoryGrowthTracker instance that tracks memory usage growth in
+  // MemoryDetails.
+  MemoryGrowthTracker memory_growth_tracker_;
 
   base::WeakPtrFactory<ChromeMetricsServiceClient> weak_ptr_factory_;
 
