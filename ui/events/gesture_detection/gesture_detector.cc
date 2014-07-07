43460391a8207aac271d5cf91bb523c63c243ff8
tdresser@chromium.org <tdresser@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-07 16:18:05 +0000

    Destroying a window on long press doesn't crash.
    The most obvious symptom of this issue is triggered by
    https://code.google.com/p/chromium/codesearch#chromium/src/ash/drag_drop/drag_drop_controller.cc&l=227.
    
    When you long press a bookmark, for example, a nested
    message loop is created, while the gesture detector hasn't
    finished processing the long press gesture. The window is
    deleted within this nested message loop, which ends up
    deleting the gesture detector.
    
    This change ensures that the gesture detector doesn't
    touch any local variables after dispatching events
    triggered by timeouts.
    
    BUG=391063
    TEST=GestureRecognizerTest.GestureEventLongPressDeletingWindow
    TBR=sadrul@chromium.org
    
    Review URL: https://codereview.chromium.org/370013002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281549 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/events/gesture_detection/gesture_detector.cc
+++ ui/events/gesture_detection/gesture_detector.cc
@@ -70,8 +70,7 @@ bool GestureDetector::SimpleGestureListener::OnSingleTapUp(
   return false;
 }
 
-bool GestureDetector::SimpleGestureListener::OnLongPress(const MotionEvent& e) {
-  return false;
+void GestureDetector::SimpleGestureListener::OnLongPress(const MotionEvent& e) {
 }
 
 bool GestureDetector::SimpleGestureListener::OnScroll(const MotionEvent& e1,
@@ -180,7 +179,6 @@ GestureDetector::GestureDetector(
       min_swipe_direction_component_ratio_(0),
       still_down_(false),
       defer_confirm_single_tap_(false),
-      in_longpress_(false),
       always_in_tap_region_(false),
       always_in_bigger_tap_region_(false),
       two_finger_tap_allowed_for_gesture_(false),
@@ -313,7 +311,6 @@ bool GestureDetector::OnTouchEvent(const MotionEvent& ev) {
       always_in_tap_region_ = true;
       always_in_bigger_tap_region_ = true;
       still_down_ = true;
-      in_longpress_ = false;
       defer_confirm_single_tap_ = false;
       two_finger_tap_allowed_for_gesture_ = two_finger_tap_enabled_;
 
@@ -326,9 +323,6 @@ bool GestureDetector::OnTouchEvent(const MotionEvent& ev) {
       break;
 
     case MotionEvent::ACTION_MOVE:
-      if (in_longpress_)
-        break;
-
       {
         const float scroll_x = last_focus_x_ - focus_x;
         const float scroll_y = last_focus_y_ - focus_y;
@@ -392,9 +386,6 @@ bool GestureDetector::OnTouchEvent(const MotionEvent& ev) {
           // Finally, give the up event of the double-tap.
           DCHECK(double_tap_listener_);
           handled |= double_tap_listener_->OnDoubleTapEvent(ev);
-        } else if (in_longpress_) {
-          timeout_handler_->StopTimeout(TAP);
-          in_longpress_ = false;
         } else if (always_in_tap_region_) {
           handled = listener_->OnSingleTapUp(ev);
           if (defer_confirm_single_tap_ && double_tap_listener_ != NULL) {
@@ -490,7 +481,7 @@ void GestureDetector::OnShowPressTimeout() {
 void GestureDetector::OnLongPressTimeout() {
   timeout_handler_->StopTimeout(TAP);
   defer_confirm_single_tap_ = false;
-  in_longpress_ = listener_->OnLongPress(*current_down_event_);
+  listener_->OnLongPress(*current_down_event_);
 }
 
 void GestureDetector::OnTapTimeout() {
@@ -514,7 +505,6 @@ void GestureDetector::CancelTaps() {
   always_in_tap_region_ = false;
   always_in_bigger_tap_region_ = false;
   defer_confirm_single_tap_ = false;
-  in_longpress_ = false;
 }
 
 bool GestureDetector::IsConsideredDoubleTap(
