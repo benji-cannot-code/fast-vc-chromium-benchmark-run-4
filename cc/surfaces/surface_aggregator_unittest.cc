5d73037a120a8f958f75b0d43db11510a4c46a8e
earthdok@chromium.org <earthdok@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-07 22:00:27 +0000

    Fix SurfaceAggregatorWithResourcesTest.TakeResourcesOneSurface in libc++ builds.
    The test no longer depends on the ordering of returned resources.
    
    BUG=391285
    R=jamesr@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/371943004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281594 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/surfaces/surface_aggregator_unittest.cc
+++ cc/surfaces/surface_aggregator_unittest.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/test/render_pass_test_common.h"
 #include "cc/test/render_pass_test_utils.h"
 #include "cc/test/test_shared_bitmap_manager.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/skia/include/core/SkColor.h"
 
@@ -876,9 +877,12 @@ TEST_F(SurfaceAggregatorWithResourcesTest, TakeResourcesOneSurface) {
   frame = aggregator_->Aggregate(surface_id);
 
   ASSERT_EQ(3u, client.returned_resources().size());
+  ResourceProvider::ResourceId returned_ids[3];
   for (size_t i = 0; i < 3; ++i) {
-    EXPECT_EQ(ids[i], client.returned_resources()[i].id);
+    returned_ids[i] = client.returned_resources()[i].id;
   }
+  EXPECT_THAT(returned_ids,
+              testing::WhenSorted(testing::ElementsAreArray(ids)));
   factory.Destroy(surface_id);
 }
 
