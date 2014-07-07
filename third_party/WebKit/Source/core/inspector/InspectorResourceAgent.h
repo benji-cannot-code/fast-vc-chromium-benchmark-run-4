95c1080a728b016c32cc3daccfffb1350d6774e3
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-07 16:49:54 +0000

    DevTools: Fix imported stylesheets editing.
    BUG=385035
    
    Review URL: https://codereview.chromium.org/373753004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177592 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorResourceAgent.h
+++ third_party/WebKit/Source/core/inspector/InspectorResourceAgent.h
@@ -145,7 +145,7 @@ public:
 
     // Called from other agents.
     void setHostId(const String&);
-    bool fetchResourceContent(LocalFrame*, const KURL&, String* content, bool* base64Encoded);
+    bool fetchResourceContent(Document*, const KURL&, String* content, bool* base64Encoded);
 
 private:
     InspectorResourceAgent(InspectorPageAgent*);
