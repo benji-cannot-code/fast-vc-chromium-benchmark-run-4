64b693faa0c0a6bf737c23f0eff42c37a671cd76
alemate@chromium.org <alemate@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-07 20:04:58 +0000

    Temporary disable early browser restart on ChromeOS.
    Disabling early restart until crbug.com/391793 is fixed.
    
    BUG=391357
    TEST=none
    
    Review URL: https://codereview.chromium.org/376623003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281576 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/login_utils.cc
+++ chrome/browser/chromeos/login/login_utils.cc
@@ -121,6 +121,10 @@ bool NeedRestartToApplyPerSessionFlags(const CommandLine& user_flags) {
 }
 
 bool CanPerformEarlyRestart() {
+  // TODO(alemate): crbug/391357 - temporary disabling early restart before
+  // crbug.com/391793 is fixed.
+  return false;
+
   const ExistingUserController* controller =
       ExistingUserController::current_controller();
   if (!controller)
