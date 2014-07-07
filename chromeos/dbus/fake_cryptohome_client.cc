2fd22209860c9e5488497f9accfd2fd3e2d2790f
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-07 09:04:30 +0000

    Fix flakiness in VerifyFakeInstallAttributesCache.
    This test races with a background file write on a WorkerPool thread.
    The RunLoop doesn't help because it just spawns another thread in the
    pool, instead of flushing the file writing thread.
    
    Fixed with a ScopedAllowIO instead, which mimics the ScopedAllowWait
    of the real implementation.
    
    BUG=388148
    R=satorux@chromium.org
    
    Review URL: https://codereview.chromium.org/349313008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281506 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/dbus/fake_cryptohome_client.cc
+++ chromeos/dbus/fake_cryptohome_client.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/message_loop/message_loop.h"
 #include "base/path_service.h"
-#include "base/threading/worker_pool.h"
+#include "base/threading/thread_restrictions.h"
 #include "chromeos/chromeos_paths.h"
 #include "chromeos/dbus/cryptohome/key.pb.h"
 #include "chromeos/dbus/cryptohome/rpc.pb.h"
@@ -19,15 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/protobuf/src/google/protobuf/io/coded_stream.h"
 #include "third_party/protobuf/src/google/protobuf/io/zero_copy_stream.h"
 #include "third_party/protobuf/src/google/protobuf/io/zero_copy_stream_impl_lite.h"
-
-namespace {
-
-// Helper to asynchronously write a file in the WorkerPool.
-void PersistFile(const base::FilePath& path, const std::string& content) {
-  base::WriteFile(path, content.data(), content.size());
-}
-
-}  // namespace
 
 namespace chromeos {
 
@@ -315,8 +306,10 @@ bool FakeCryptohomeClient::InstallAttributesFinalize(bool* successful) {
     }
   }
 
-  base::WorkerPool::PostTask(
-      FROM_HERE, base::Bind(&PersistFile, cache_path, result), false);
+  // The real implementation does a blocking wait on the dbus call; the fake
+  // implementation must have this file written before returning.
+  base::ThreadRestrictions::ScopedAllowIO allow_io;
+  base::WriteFile(cache_path, result.data(), result.size());
 
   return true;
 }
