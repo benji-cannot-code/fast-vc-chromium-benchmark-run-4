2923fbda45f1d251550f1b7afe694392e7b6b03d
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-07 18:05:31 +0000

    Directly update opacity
    This CL expands the codepath that directly updates transforms to directly
    update opacity. We don't need to do anything clever for opacity because having
    opacity doesn't affect the early stages of the compositing pipeline.
    
    I've also added some comments explaining why we bail out from a direct update
    in a couple places.
    
    R=esprehn@chromium.org
    
    Review URL: https://codereview.chromium.org/372433002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177595 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderObject.h
+++ third_party/WebKit/Source/core/rendering/RenderObject.h
@@ -877,7 +877,7 @@ public:
 
     bool isFloatingOrOutOfFlowPositioned() const { return (isFloating() || isOutOfFlowPositioned()); }
 
-    bool isTransparent() const { return style()->opacity() < 1.0f; }
+    bool isTransparent() const { return style()->hasOpacity(); }
     float opacity() const { return style()->opacity(); }
 
     bool hasReflection() const { return m_bitfields.hasReflection(); }
