901a2a2662639d4e8a23efd0b4e501e66741ee4e
skyostil@chromium.org <skyostil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-07 20:41:24 +0000

    Add a benchmark for synchronized scrolling
    Add a benchmark that listens for scroll events and updates the DOM in
    response. The mean_input_event_latency metric will measure the delay at
    which the DOM updates lag behind the actual scroll offset. This delay
    should ideally be under one vsync interval.
    
    Based on http://tdresser.github.io/sync-scroll-offset-test.
    
    BUG=347366
    
    Review URL: https://codereview.chromium.org/372743003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281581 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/page_sets/tough_scheduling_cases.py
+++ tools/perf/page_sets/tough_scheduling_cases.py
@@ -297,6 +297,7 @@ class Page19(ToughSchedulingCasesPage):
   def RunSmoothness(self, action_runner):
     action_runner.Wait(3)
 
+
 class Page20(ToughSchedulingCasesPage):
 
   """ Why: Simple JS touch dragging """
@@ -316,6 +317,7 @@ class Page20(ToughSchedulingCasesPage):
         speed_in_pixels_per_second=150,
         distance=400)
     interaction.End()
+
 
 class EmptyTouchHandlerPage(ToughSchedulingCasesPage):
 
@@ -346,6 +348,20 @@ class EmptyTouchHandlerPage(ToughSchedulingCasesPage):
       action_runner.ScrollPage(use_touch=True, speed_in_pixels_per_second=400,
                                distance=2100)
       interaction.End()
+
+
+class SynchronizedScrollOffsetPage(ToughSchedulingCasesPage):
+
+  """Why: For measuring the latency of scroll-synchronized effects."""
+
+  def __init__(self, page_set):
+    super(SynchronizedScrollOffsetPage, self).__init__(
+      url='file://tough_scheduling_cases/sync_scroll_offset.html',
+      page_set=page_set)
+
+  def RunSmoothness(self, action_runner):
+    action_runner.RunAction(ScrollBounceAction())
+
 
 class ToughSchedulingCasesPageSet(page_set_module.PageSet):
 
@@ -444,3 +460,5 @@ class ToughSchedulingCasesPageSet(page_set_module.PageSet):
       slow_handler=True,
       bounce=True,
       page_set=self))
+    # Why: For measuring the latency of scroll-synchronized effects.
+    self.AddPage(SynchronizedScrollOffsetPage(page_set=self))
