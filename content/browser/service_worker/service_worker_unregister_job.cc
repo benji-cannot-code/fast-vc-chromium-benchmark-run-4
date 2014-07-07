e694a8999c6b6bb6009df1c21470cda320b6585d
falken@chromium.org <falken@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-07 14:28:46 +0000

    Service Worker: Delete stale resources on startup
    The browser might shutdown before purgeable resources
    are purged, so we should clean up on startup. This patch
    extends ServiceWorkerStorage initialization to read
    purgeable resources from the db and purge them. This
    must happen before initialization completes or else we 
    could purge resources that are not stale (i.e., a live
    version's resources).
    
    BUG=388095
    
    Review URL: https://codereview.chromium.org/370203003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281532 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/service_worker/service_worker_unregister_job.cc
+++ content/browser/service_worker/service_worker_unregister_job.cc
@@ -77,8 +77,6 @@ void ServiceWorkerUnregisterJob::DeleteRegistration(
         ServiceWorkerVersion::REDUNDANT);
   }
 
-  // TODO(michaeln): Eventually call storage->DeleteVersionResources() when the
-  // version no longer has any controllees.
   context_->storage()->DeleteRegistration(
       registration->id(),
       registration->script_url().GetOrigin(),
