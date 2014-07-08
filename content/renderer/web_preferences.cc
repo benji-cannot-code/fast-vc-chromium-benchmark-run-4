a810bc43541f53498e0fadf767d1dbc15efbf589
chrishtr@chromium.org <chrishtr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-08 19:11:14 +0000

    Add a tracing line to UpdateWebPreferences. This is a potentially very expensive operation.
    BUG=390799
    
    Review URL: https://codereview.chromium.org/375623002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281785 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/web_preferences.cc
+++ content/renderer/web_preferences.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/public/renderer/web_preferences.h"
 
+#include "base/debug/trace_event.h"
 #include "base/strings/utf_string_conversions.h"
 #include "content/renderer/net_info_helper.h"
 #include "third_party/WebKit/public/platform/WebConnectionType.h"
@@ -115,6 +116,7 @@ void ApplyFontsFromMap(const webkit_glue::ScriptFontFamilyMap& map,
 }  // namespace
 
 void ApplyWebPreferences(const WebPreferences& prefs, WebView* web_view) {
+  TRACE_EVENT0("renderer", "ApplyWebPreferences");
   WebSettings* settings = web_view->settings();
   ApplyFontsFromMap(prefs.standard_font_family_map,
                     setStandardFontFamilyWrapper, settings);
