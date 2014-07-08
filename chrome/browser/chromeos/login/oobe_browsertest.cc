c802b10ef40e8dc6ed47a8c8f01a9ef95400be70
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-08 14:47:07 +0000

    Reenable OobeTest.NewUser
    Since root cause http://crbug.com/342478 has been fixed.
    
    BUG=363183
    
    Review URL: https://codereview.chromium.org/371353002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281749 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/oobe_browsertest.cc
+++ chrome/browser/chromeos/login/oobe_browsertest.cc
@@ -80,12 +80,7 @@ class OobeTest : public InProcessBrowserTest {
   DISALLOW_COPY_AND_ASSIGN(OobeTest);
 };
 
-#if defined(OS_CHROMEOS)
-#define MAYBE_NewUser DISABLED_NewUser
-#else
-#define MAYBE_NewUser NewUser
-#endif
-IN_PROC_BROWSER_TEST_F(OobeTest, MAYBE_NewUser) {
+IN_PROC_BROWSER_TEST_F(OobeTest, NewUser) {
   chromeos::WizardController::SkipPostLoginScreensForTesting();
   chromeos::WizardController* wizard_controller =
       chromeos::WizardController::default_controller();
@@ -105,12 +100,7 @@ IN_PROC_BROWSER_TEST_F(OobeTest, MAYBE_NewUser) {
     content::NotificationService::AllSources()).Wait();
 }
 
-#if defined(OS_CHROMEOS)
-#define MAYBE_Accelerator DISABLED_Accelerator
-#else
-#define MAYBE_Accelerator Accelerator
-#endif
-IN_PROC_BROWSER_TEST_F(OobeTest, MAYBE_Accelerator) {
+IN_PROC_BROWSER_TEST_F(OobeTest, Accelerator) {
   chromeos::WizardController::SkipPostLoginScreensForTesting();
   chromeos::WizardController* wizard_controller =
       chromeos::WizardController::default_controller();
