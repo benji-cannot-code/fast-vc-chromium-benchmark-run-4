b24d4538111fbe50cc2768c09af1291e4d679d54
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-08 04:12:00 +0000

    [webcrypto] Fix a crash when RSA key generation fails. BUG=391570 NOTRY=true
    Review URL: https://codereview.chromium.org/374743002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281655 0039d316-1c4b-4281-b951-d872f2087c98

--- content/child/webcrypto/platform_crypto_nss.cc
+++ content/child/webcrypto/platform_crypto_nss.cc
@@ -1505,6 +1505,9 @@ Status GenerateRsaKeyPair(const blink::WebCryptoAlgorithm& algorithm,
     return Status::OperationError();
 
   PK11RSAGenParams rsa_gen_params;
+  // keySizeInBits is a signed type, don't pass in a negative value.
+  if (modulus_length_bits > INT_MAX)
+    return Status::OperationError();
   rsa_gen_params.keySizeInBits = modulus_length_bits;
   rsa_gen_params.pe = public_exponent;
 
@@ -1532,7 +1535,7 @@ Status GenerateRsaKeyPair(const blink::WebCryptoAlgorithm& algorithm,
 
   // Note: NSS does not generate an sec_public_key if the call below fails,
   // so there is no danger of a leaked sec_public_key.
-  SECKEYPublicKey* sec_public_key;
+  SECKEYPublicKey* sec_public_key = NULL;
   crypto::ScopedSECKEYPrivateKey scoped_sec_private_key(
       PK11_GenerateKeyPairWithOpFlags(slot.get(),
                                       CKM_RSA_PKCS_KEY_PAIR_GEN,
@@ -1542,7 +1545,7 @@ Status GenerateRsaKeyPair(const blink::WebCryptoAlgorithm& algorithm,
                                       operation_flags,
                                       operation_flags_mask,
                                       NULL));
-  if (!private_key)
+  if (!scoped_sec_private_key)
     return Status::OperationError();
 
   blink::WebCryptoKeyAlgorithm key_algorithm;
