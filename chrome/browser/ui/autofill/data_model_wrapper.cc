ab24be67a28596d62607eadcadb716bca234b977
rouslan@chromium.org <rouslan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-08 19:54:12 +0000

    Reland of "Use address_data.h from upstream libaddressinput".
    This patch fixes autofill tests on Windows and relands
    https://src.chromium.org/viewvc/chrome?view=rev&revision=281192, which was
    reverted in https://src.chromium.org/viewvc/chrome?view=rev&revision=281226
    
    BUG=389918
    
    Review URL: https://codereview.chromium.org/368243007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281798 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/autofill/data_model_wrapper.cc
+++ chrome/browser/ui/autofill/data_model_wrapper.cc
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/credit_card.h"
 #include "components/autofill/core/browser/form_structure.h"
 #include "third_party/libaddressinput/chromium/cpp/include/libaddressinput/address_data.h"
-#include "third_party/libaddressinput/chromium/cpp/include/libaddressinput/address_ui.h"
+#include "third_party/libaddressinput/chromium/cpp/include/libaddressinput/address_formatter.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/image/image.h"
 
@@ -62,7 +62,7 @@ bool DataModelWrapper::GetDisplayText(
           base::Bind(&DataModelWrapper::GetInfo, base::Unretained(this)));
   address_data->language_code = GetLanguageCode();
   std::vector<std::string> lines;
-  address_data->FormatForDisplay(&lines);
+  ::i18n::addressinput::GetFormattedNationalAddress(*address_data, &lines);
 
   // Email and phone number aren't part of address formatting.
   base::string16 non_address_info;
@@ -72,14 +72,12 @@ bool DataModelWrapper::GetDisplayText(
 
   non_address_info += base::ASCIIToUTF16("\n") + phone;
 
-  // The separator is locale-specific.
-  std::string compact_separator =
-      ::i18n::addressinput::GetCompactAddressLinesSeparator(GetLanguageCode());
-  *vertically_compact =
-      base::UTF8ToUTF16(JoinString(lines, compact_separator)) +
-          non_address_info;
-  *horizontally_compact = base::UTF8ToUTF16(JoinString(lines, "\n")) +
-      non_address_info;
+  std::string line_address;
+  ::i18n::addressinput::GetFormattedNationalAddressLine(*address_data,
+                                                        &line_address);
+  *vertically_compact = base::UTF8ToUTF16(line_address) + non_address_info;
+  *horizontally_compact =
+      base::UTF8ToUTF16(JoinString(lines, "\n")) + non_address_info;
 
   return true;
 }
@@ -389,7 +387,7 @@ base::string16 I18nAddressDataWrapper::GetInfo(const AutofillType& type) const {
     return base::string16();
 
   if (field == ::i18n::addressinput::COUNTRY) {
-    return AutofillCountry(address_->country_code,
+    return AutofillCountry(address_->region_code,
                            g_browser_process->GetApplicationLocale()).name();
   }
 
--- components/autofill/core/browser/autofill_profile.cc
+++ components/autofill/core/browser/autofill_profile.cc
@@ -27,7 +27,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/validation.h"
 #include "components/autofill/core/common/form_field_data.h"
 #include "grit/components_strings.h"
+#include "third_party/libaddressinput/chromium/addressinput_util.h"
 #include "third_party/libaddressinput/chromium/cpp/include/libaddressinput/address_data.h"
+#include "third_party/libaddressinput/chromium/cpp/include/libaddressinput/address_formatter.h"
 #include "ui/base/l10n/l10n_util.h"
 
 using base::ASCIIToUTF16;
@@ -299,11 +301,11 @@ base::string16 AutofillProfile::GetInfo(const AutofillType& type,
   if (type.html_type() == HTML_TYPE_FULL_ADDRESS) {
     scoped_ptr< ::i18n::addressinput::AddressData> address_data =
         i18n::CreateAddressDataFromAutofillProfile(*this, app_locale);
-    if (!address_data->HasAllRequiredFields())
+    if (!addressinput::HasAllRequiredFields(*address_data))
       return base::string16();
 
     std::vector<std::string> lines;
-    address_data->FormatForDisplay(&lines);
+    ::i18n::addressinput::GetFormattedNationalAddress(*address_data, &lines);
     return base::UTF8ToUTF16(JoinString(lines, '\n'));
   }
 
