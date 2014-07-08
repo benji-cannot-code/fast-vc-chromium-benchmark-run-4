0cd421577fcf6564e58cda23ce9576a699243669
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-08 19:52:09 +0000

    Avoid unnecessary scrollbars for sub-pixel layers
    Change RenderLayerScrollableArea to compare the pixel snapped scroll
    and client sizes. This mirrors the logic for RenderBlock and avoids
    showing scrollbars and enabling scrolling in cases where the sub-pixel
    values are different but snap to the same pixel value.
    
    TEST=fast/sub-pixel/flexbox-unnecessary-scrollbar.htm
    BUG=390407
    R=leviw@chromium.org
    
    Review URL: https://codereview.chromium.org/372863002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177686 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderLayerScrollableArea.cpp
+++ third_party/WebKit/Source/core/rendering/RenderLayerScrollableArea.cpp
@@ -543,6 +543,16 @@ LayoutUnit RenderLayerScrollableArea::scrollHeight() const
     return m_overflowRect.height();
 }
 
+int RenderLayerScrollableArea::pixelSnappedScrollWidth() const
+{
+    return snapSizeToPixel(scrollWidth(), box().clientLeft() + box().x());
+}
+
+int RenderLayerScrollableArea::pixelSnappedScrollHeight() const
+{
+    return snapSizeToPixel(scrollHeight(), box().clientTop() + box().y());
+}
+
 void RenderLayerScrollableArea::computeScrollDimensions()
 {
     m_scrollDimensionsDirty = false;
@@ -657,14 +667,14 @@ bool RenderLayerScrollableArea::hasHorizontalOverflow() const
 {
     ASSERT(!m_scrollDimensionsDirty);
 
-    return scrollWidth() > box().clientWidth();
+    return pixelSnappedScrollWidth() > box().pixelSnappedClientWidth();
 }
 
 bool RenderLayerScrollableArea::hasVerticalOverflow() const
 {
     ASSERT(!m_scrollDimensionsDirty);
 
-    return scrollHeight() > box().clientHeight();
+    return pixelSnappedScrollHeight() > box().pixelSnappedClientHeight();
 }
 
 bool RenderLayerScrollableArea::hasScrollableHorizontalOverflow() const
--- third_party/WebKit/Source/core/rendering/RenderLayerScrollableArea.h
+++ third_party/WebKit/Source/core/rendering/RenderLayerScrollableArea.h
@@ -142,6 +142,8 @@ public:
 
     LayoutUnit scrollWidth() const;
     LayoutUnit scrollHeight() const;
+    int pixelSnappedScrollWidth() const;
+    int pixelSnappedScrollHeight() const;
 
     int verticalScrollbarWidth(OverlayScrollbarSizeRelevancy = IgnoreOverlayScrollbarSize) const;
     int horizontalScrollbarHeight(OverlayScrollbarSizeRelevancy = IgnoreOverlayScrollbarSize) const;
