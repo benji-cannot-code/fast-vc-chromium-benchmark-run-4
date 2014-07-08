4861dad3c7bb6aebffb28b59ea278275a9ded403
dtu@chromium.org <dtu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-08 20:19:15 +0000

    [telemetry] Refactor run_tests to remove old cruft.
    - Remove dead code and circuitous code paths.
    - Make its API consistent with run_benchmark, taking advantage of command_line.OptparseCommand.
    
    BUG=346956
    TEST=tools/telemetry/run_tests && tools/perf/run_tests
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=280232
    
    R=kbr@chromium.org, nduca@chromium.org
    
    Review URL: https://codereview.chromium.org/228073002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281813 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/unittest/options_for_unittests.py
+++ tools/telemetry/telemetry/unittest/options_for_unittests.py
@@ -10,14 +10,16 @@ if unit tests are not running.
 
 This allows multiple unit tests to use a specific
 browser, in face of multiple options."""
+
+
 _options = None
-_browser_type = None
-def Set(options, browser_type):
+
+
+def Set(options):
   global _options
-  global _browser_type
 
   _options = options
-  _browser_type = browser_type
+
 
 def GetCopy():
   if not _options:
@@ -25,10 +27,8 @@ def GetCopy():
 
   return _options.Copy()
 
+
 def AreSet():
   if _options:
     return True
   return False
-
-def GetBrowserType():
-  return _browser_type
