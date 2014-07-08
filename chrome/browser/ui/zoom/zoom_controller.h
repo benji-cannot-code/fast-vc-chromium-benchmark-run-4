ed83d2cc0e5255842edda2be0cc386ae2593fe0a
wjmaclean@chromium.org <wjmaclean@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-08 12:07:40 +0000

    Refactor to convert vector to scoped_ptr in ZoomController.
    A scoped_ptr has less overhead than a vector. This CL refactors to
    store ZoomChangedEventData structs via a scoped_ptr since there will
    be (at most) one of these at a time.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/373603007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281726 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/zoom/zoom_controller.h
+++ chrome/browser/ui/zoom/zoom_controller.h
@@ -6,10 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_ZOOM_ZOOM_CONTROLLER_H_
 #define CHROME_BROWSER_UI_ZOOM_ZOOM_CONTROLLER_H_
 
-#include <vector>
-
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
+#include "base/memory/scoped_ptr.h"
 #include "base/observer_list.h"
 #include "base/prefs/pref_member.h"
 #include "content/public/browser/host_zoom_map.h"
@@ -137,7 +136,7 @@ class ZoomController : public content::WebContentsObserver,
   // Used to access the default zoom level preference.
   DoublePrefMember default_zoom_level_;
 
-  std::vector<ZoomChangedEventData> event_data_;
+  scoped_ptr<ZoomChangedEventData> event_data_;
 
   // Keeps track of the extension (if any) that initiated the last zoom change
   // that took effect.
