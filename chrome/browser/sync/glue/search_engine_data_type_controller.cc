16b85171b7f5e9e7750746bbd7939598dc852538
caitkp@chromium.org <caitkp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-08 15:03:48 +0000

    Remove TemplateURLService subscription as soon as models are stopped.
    BUG=347295
    
    Review URL: https://codereview.chromium.org/368873004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281751 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/glue/search_engine_data_type_controller.cc
+++ chrome/browser/sync/glue/search_engine_data_type_controller.cc
@@ -30,6 +30,11 @@ SearchEngineDataTypeController::SearchEngineDataTypeController(
       profile_(profile) {
 }
 
+TemplateURLService::Subscription*
+SearchEngineDataTypeController::GetSubscriptionForTesting() {
+  return template_url_subscription_.get();
+}
+
 SearchEngineDataTypeController::~SearchEngineDataTypeController() {}
 
 // We want to start the TemplateURLService before we begin associating.
@@ -51,6 +56,10 @@ bool SearchEngineDataTypeController::StartModels() {
                  this));
 
   return false;  // Don't continue Start.
+}
+
+void SearchEngineDataTypeController::StopModels() {
+  template_url_subscription_.reset();
 }
 
 void SearchEngineDataTypeController::OnTemplateURLServiceLoaded() {
