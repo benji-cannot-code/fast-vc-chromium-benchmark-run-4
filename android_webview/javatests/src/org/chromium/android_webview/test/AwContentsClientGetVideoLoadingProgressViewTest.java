c96807a07cfaf48f187fb69662bd5ad97a8513db
qinmin@chromium.org <qinmin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-08 01:50:03 +0000

    Disable another WebView fullscreen video test until we land fullscreen IPC changes
    there are 2 changes needs to be submitted before we reenable the tests:
    1. https://codereview.chromium.org/290643005/ and
    2. https://codereview.chromium.org/292573004/
    
    the blink change will break all the tests, so we need to reenable the test after the chromium change lands
    
    BUG=367346
    
    Review URL: https://codereview.chromium.org/371933003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281627 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/javatests/src/org/chromium/android_webview/test/AwContentsClientGetVideoLoadingProgressViewTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwContentsClientGetVideoLoadingProgressViewTest.java
@@ -5,12 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.android_webview.test;
 
-import android.test.suitebuilder.annotation.SmallTest;
 import android.view.View;
 
 import org.chromium.android_webview.AwContents;
 import org.chromium.android_webview.test.util.VideoTestWebServer;
-import org.chromium.base.test.util.Feature;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.content.browser.test.util.CallbackHelper;
 import org.chromium.content.browser.test.util.TouchCommon;
 
@@ -46,9 +45,12 @@ public class AwContentsClientGetVideoLoadingProgressViewTest extends AwTestBase
                 TimeUnit.MILLISECONDS);
     }
 
-
+    /*
     @Feature({"AndroidWebView"})
     @SmallTest
+    crbug.com/367346
+    */
+    @DisabledTest
     public void testGetVideoLoadingProgressView() throws Throwable {
         TestAwContentsClient contentsClient =
                 new FullScreenVideoTestAwContentsClient(getActivity()) {
