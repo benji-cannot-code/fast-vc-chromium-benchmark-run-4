687da125ede6919c6df6e194ebfa848fdc08606c
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-09 09:23:43 +0000

    DevTools: merge search module into sources.
    Review URL: https://codereview.chromium.org/364063003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177729 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sources/SourcesPanel.js
+++ third_party/WebKit/Source/devtools/front_end/sources/SourcesPanel.js
@@ -50,6 +50,11 @@ importScript("InplaceFormatterEditorAction.js");
 importScript("ScriptFormatter.js");
 importScript("SourcesView.js");
 
+// Search results pane
+importScript("AdvancedSearchView.js");
+importScript("FileBasedSearchResultsPane.js");
+importScript("SourcesSearchScope.js");
+
 /**
  * @constructor
  * @implements {WebInspector.ContextMenu.Provider}
