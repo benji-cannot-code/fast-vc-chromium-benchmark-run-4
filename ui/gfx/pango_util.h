21db50b181a0befaaff873f5821067883d5becfa
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-09 04:24:14 +0000

    Simplify PangoLayout configuration.
    Remove SetupPangoLayout() and
    SetupPangoLayoutWithoutFontDescription().
    Rename SetupPangoLayoutWithFontDescription() to
    SetUpPangoLayout() and pass a FontList to it instead of a
    PangoFontDescription, along with removing an unused "width"
    parameter.
    
    BUG=125235
    
    Review URL: https://codereview.chromium.org/374833005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281925 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/pango_util.h
+++ ui/gfx/pango_util.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <cairo/cairo.h>
 #include <pango/pango.h>
-#include <string>
 
 #include "base/i18n/rtl.h"
 #include "base/logging.h"
@@ -16,20 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/gfx/gfx_export.h"
 
-typedef struct _PangoContext PangoContext;
-
 namespace gfx {
 
-class Font;
-class PlatformFontPango;
-class Rect;
-
-// Creates and returns a PangoContext. The caller owns the context.
-PangoContext* GetPangoContext();
-
-// Returns the resolution (DPI) used by pango. A negative values means the
-// resolution hasn't been set.
-double GetPangoResolution();
+class FontList;
 
 // Utility class to ensure that PangoFontDescription is freed.
 class ScopedPangoFontDescription {
@@ -56,14 +44,11 @@ class ScopedPangoFontDescription {
 // They are shared with internal skia interfaces.
 // ----------------------------------------------------------------------------
 
-// Setup pango |layout|; set the |text|, the font description based on
-// |font_description|, the |width| in PANGO_SCALE for RTL locale, the base
-// |text_direction|, alignment, ellipsis, word wrapping, resolution, etc.
-void SetupPangoLayoutWithFontDescription(
+// Configures |layout| for the passed-in parameters.
+void SetUpPangoLayout(
     PangoLayout* layout,
     const base::string16& text,
-    const std::string& font_description,
-    int width,
+    const FontList& font_list,
     base::i18n::TextDirection text_direction,
     int flags);
 
