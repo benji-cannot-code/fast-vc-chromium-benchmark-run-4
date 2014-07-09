88cca8d0f2d42058cda4d26170d665d51dad84b9
egor.starkov@samsung.com <egor.starkov@samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-09 22:49:29 +0000

    Remove burst mode from VSyncMonitor.
    Extra vsyncs after each rendering cause 1% more power consumption.
    Modify VSyncMonitorTest to follow the rule 1 request == 1 vsync.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/377483002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@282154 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/android/java/src/org/chromium/ui/VSyncMonitor.java
+++ ui/android/java/src/org/chromium/ui/VSyncMonitor.java
@@ -16,9 +16,6 @@ import org.chromium.base.TraceEvent;
 
 /**
  * Notifies clients of the default displays's vertical sync pulses.
- * This class works in "burst" mode: once the update is requested, the listener will be
- * called MAX_VSYNC_COUNT times on the vertical sync pulses (on JB) or on every refresh
- * period (on ICS, see below), unless stop() is called.
  * On ICS, VSyncMonitor relies on setVSyncPointForICS() being called to set a reasonable
  * approximation of a vertical sync starting point; see also http://crbug.com/156397.
  */
@@ -27,7 +24,6 @@ public class VSyncMonitor {
     private static final long NANOSECONDS_PER_SECOND = 1000000000;
     private static final long NANOSECONDS_PER_MILLISECOND = 1000000;
     private static final long NANOSECONDS_PER_MICROSECOND = 1000;
-    public static final int MAX_AUTO_ONVSYNC_COUNT = 5;
 
     /**
      * VSync listener class
@@ -47,7 +43,6 @@ public class VSyncMonitor {
     private final long mRefreshPeriodNano;
 
     private boolean mHaveRequestInFlight;
-    private int mTriggerNextVSyncCount;
 
     // Choreographer is used to detect vsync on >= JB.
     private final Choreographer mChoreographer;
@@ -85,7 +80,6 @@ public class VSyncMonitor {
                 .getDefaultDisplay().getRefreshRate();
         if (refreshRate <= 0) refreshRate = 60;
         mRefreshPeriodNano = (long) (NANOSECONDS_PER_SECOND / refreshRate);
-        mTriggerNextVSyncCount = 0;
 
         if (enableJBVSync && Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
             // Use Choreographer on JB+ to get notified of vsync.
@@ -142,20 +136,10 @@ public class VSyncMonitor {
     }
 
     /**
-     * Stop reporting vsync events. Note that at most one pending vsync event can still be delivered
-     * after this function is called.
-     */
-    public void stop() {
-        mTriggerNextVSyncCount = 0;
-    }
-
-    /**
      * Request to be notified of the closest display vsync events.
      * Listener.onVSync() will be called soon after the upcoming vsync pulses.
-     * It will be called at most MAX_AUTO_ONVSYNC_COUNT times unless requestUpdate() is called.
      */
     public void requestUpdate() {
-        mTriggerNextVSyncCount = MAX_AUTO_ONVSYNC_COUNT;
         postCallback();
     }
 
@@ -175,10 +159,6 @@ public class VSyncMonitor {
         assert mHaveRequestInFlight;
         mHaveRequestInFlight = false;
         mLastVSyncCpuTimeNano = currentTimeNanos;
-        if (mTriggerNextVSyncCount >= 0) {
-            mTriggerNextVSyncCount--;
-            postCallback();
-        }
         if (mListener != null) {
             mListener.onVSync(this, frameTimeNanos / NANOSECONDS_PER_MICROSECOND);
         }
