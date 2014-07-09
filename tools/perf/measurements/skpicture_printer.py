8a24b5a1557d21a6d4eb11aa39e235f3aab4e7ca
nednguyen@google.com <nednguyen@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-09 20:05:02 +0000

    Update skpicture_printer to use results.AddValue(...)
    BUG=391943
    TBR=nduca@chromium.org
    
    Review URL: https://codereview.chromium.org/379843005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@282105 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/measurements/skpicture_printer.py
+++ tools/perf/measurements/skpicture_printer.py
@@ -6,6 +6,7 @@ import glob
 import os
 
 from telemetry.page import page_measurement
+from telemetry.value import scalar
 
 
 _JS = 'chrome.gpuBenchmarking.printToSkPicture("{0}");'
@@ -39,4 +40,5 @@ class SkpicturePrinter(page_measurement.PageMeasurement):
     js = _JS.format(outpath.replace('\\', '\\\\'))
     tab.EvaluateJavaScript(js)
     pictures = glob.glob(os.path.join(outpath, '*.skp'))
-    results.Add('saved_picture_count', 'count', len(pictures))
+    results.AddValue(scalar.ScalarValue(
+        results.current_page, 'saved_picture_count', 'count', len(pictures)))
