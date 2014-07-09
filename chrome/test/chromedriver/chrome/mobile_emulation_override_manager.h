12485a986560355caf30936abbb9bb17a86b49e1
samuong@chromium.org <samuong@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-09 18:41:50 +0000

    [chromedriver] Rename "emulateViewport" to "mobile".
    BUG=chromedriver:843
    TBR=stgao@chromium.org
    
    Review URL: https://codereview.chromium.org/379803007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@282089 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/chromedriver/chrome/mobile_emulation_override_manager.h
+++ chrome/test/chromedriver/chrome/mobile_emulation_override_manager.h
@@ -17,6 +17,7 @@ namespace base {
 class DictionaryValue;
 }
 
+struct BrowserInfo;
 class DevToolsClient;
 struct DeviceMetrics;
 class Status;
@@ -26,7 +27,8 @@ class Status;
 class MobileEmulationOverrideManager : public DevToolsEventListener {
  public:
   MobileEmulationOverrideManager(DevToolsClient* client,
-                                 const DeviceMetrics* device_metrics);
+                                 const DeviceMetrics* device_metrics,
+                                 const BrowserInfo* browser_info);
   virtual ~MobileEmulationOverrideManager();
 
   // Overridden from DevToolsEventListener:
@@ -40,6 +42,7 @@ class MobileEmulationOverrideManager : public DevToolsEventListener {
 
   DevToolsClient* client_;
   const DeviceMetrics* overridden_device_metrics_;
+  const BrowserInfo* browser_info_;
 
   DISALLOW_COPY_AND_ASSIGN(MobileEmulationOverrideManager);
 };
