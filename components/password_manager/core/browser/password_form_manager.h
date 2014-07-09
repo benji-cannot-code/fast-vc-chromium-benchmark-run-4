3bccc650463c5e575cf30b63fe1e050a9dac8492
vabr@chromium.org <vabr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-09 13:14:30 +0000

    PasswordFormManager::best_matches should return a const ref
    Currently, it returns 'const PasswordFormMap'. While returning a value, marking it const does not make much sense (it's a copy anyway). So it should be either 'const PasswordFormMap&' or just 'PasswordFormMap'. The only place this call is used at makes a copy of the result anyway, so I chose the former. That also seems to be a more versatile choice -- if somebody needs just a read-only copy in the future, returning by value would result in an unnecessary copy.
    
    BUG=261628
    R=mkwst@chromium.org
    
    Review URL: https://codereview.chromium.org/382493003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@282018 0039d316-1c4b-4281-b951-d872f2087c98

--- components/password_manager/core/browser/password_form_manager.h
+++ components/password_manager/core/browser/password_form_manager.h
@@ -158,7 +158,7 @@ class PasswordFormManager : public PasswordStoreConsumer {
   }
 
   // Returns the best matches.
-  const autofill::PasswordFormMap best_matches() const {
+  const autofill::PasswordFormMap& best_matches() const {
     return best_matches_;
   }
 
