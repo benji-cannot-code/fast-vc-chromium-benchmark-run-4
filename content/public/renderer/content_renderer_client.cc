f17f4a7bbc62dd5ce0caa96fdb3b89092e6c7668
wfh@chromium.org <wfh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-09 00:23:32 +0000

    Test to remove calls to tab idleNotification, to try and diagnose a bug.  This will be removed after a Canary run.
    BUG=381820
    
    Review URL: https://codereview.chromium.org/371293006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281901 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/renderer/content_renderer_client.cc
+++ content/public/renderer/content_renderer_client.cc
@@ -86,7 +86,9 @@ blink::WebSpeechSynthesizer* ContentRendererClient::OverrideSpeechSynthesizer(
 }
 
 bool ContentRendererClient::RunIdleHandlerWhenWidgetsHidden() {
-  return true;
+  // TODO(wfh): http://crbug.com/381820 change this back to true after testing
+  // whether this affects tabs hanging.
+  return false;
 }
 
 bool ContentRendererClient::AllowPopup() {
