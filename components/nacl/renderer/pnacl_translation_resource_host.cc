0e367b7c1bccd5704b7ccd9804cc7a4b38d00006
teravest@chromium.org <teravest@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-09 20:33:08 +0000

    Pepper: Use base::Callback in RequestNexeFd.
    GetNexeFd() in ppb_nacl_private_impl will change soon to be an internal method,
    not one exposed through the PPB_NaCl_Private interface. From inside Chromium,
    base::Callback is much easier to use than PP_CompletionCallback or
    TrackedCallback.
    
    This is one of a series of changes to refactor the pexe downloading logic in
    the trusted plugin.
    
    BUG=370556
    
    Review URL: https://codereview.chromium.org/380773002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@282112 0039d316-1c4b-4281-b951-d872f2087c98

--- components/nacl/renderer/pnacl_translation_resource_host.cc
+++ components/nacl/renderer/pnacl_translation_resource_host.cc
@@ -9,16 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/c/pp_errors.h"
 #include "ppapi/shared_impl/ppapi_globals.h"
 
-using ppapi::TrackedCallback;
 using ppapi::PpapiGlobals;
-
-PnaclTranslationResourceHost::CacheRequestInfo::CacheRequestInfo(
-    PP_Bool* hit,
-    PP_FileHandle* handle,
-    scoped_refptr<TrackedCallback> cb)
-    : is_hit(hit), file_handle(handle), callback(cb) {}
-
-PnaclTranslationResourceHost::CacheRequestInfo::~CacheRequestInfo() {}
 
 PnaclTranslationResourceHost::PnaclTranslationResourceHost(
     const scoped_refptr<base::MessageLoopProxy>& io_message_loop)
@@ -59,9 +50,7 @@ void PnaclTranslationResourceHost::RequestNexeFd(
     int render_view_id,
     PP_Instance instance,
     const nacl::PnaclCacheInfo& cache_info,
-    PP_Bool* is_hit,
-    PP_FileHandle* file_handle,
-    scoped_refptr<TrackedCallback> callback) {
+    RequestNexeFdCallback callback) {
   DCHECK(PpapiGlobals::Get()->
          GetMainThreadMessageLoop()->BelongsToCurrentThread());
   io_message_loop_->PostTask(
@@ -71,8 +60,6 @@ void PnaclTranslationResourceHost::RequestNexeFd(
                  render_view_id,
                  instance,
                  cache_info,
-                 is_hit,
-                 file_handle,
                  callback));
   return;
 }
@@ -81,21 +68,19 @@ void PnaclTranslationResourceHost::SendRequestNexeFd(
     int render_view_id,
     PP_Instance instance,
     const nacl::PnaclCacheInfo& cache_info,
-    PP_Bool* is_hit,
-    PP_FileHandle* file_handle,
-    scoped_refptr<TrackedCallback> callback) {
+    RequestNexeFdCallback callback) {
   DCHECK(io_message_loop_->BelongsToCurrentThread());
   if (!sender_ || !sender_->Send(new NaClHostMsg_NexeTempFileRequest(
           render_view_id, instance, cache_info))) {
-    PpapiGlobals::Get()->GetMainThreadMessageLoop()
-        ->PostTask(FROM_HERE,
-                   base::Bind(&TrackedCallback::Run,
-                              callback,
-                              static_cast<int32_t>(PP_ERROR_FAILED)));
+    PpapiGlobals::Get()->GetMainThreadMessageLoop()->PostTask(
+        FROM_HERE,
+        base::Bind(callback,
+                   static_cast<int32_t>(PP_ERROR_FAILED),
+                   false,
+                   PP_kInvalidFileHandle));
     return;
   }
-  pending_cache_requests_.insert(std::make_pair(
-      instance, CacheRequestInfo(is_hit, file_handle, callback)));
+  pending_cache_requests_.insert(std::make_pair(instance, callback));
 }
 
 void PnaclTranslationResourceHost::ReportTranslationFinished(
@@ -134,10 +119,9 @@ void PnaclTranslationResourceHost::OnNexeTempFileReply(
   CacheRequestInfoMap::iterator it = pending_cache_requests_.find(instance);
   int32_t status = PP_ERROR_FAILED;
   // Handle the expected successful case first.
-  if (it != pending_cache_requests_.end() && base_file.IsValid() &&
-      TrackedCallback::IsPending(it->second.callback)) {
-    *it->second.is_hit = PP_FromBool(is_hit);
-    *it->second.file_handle = base_file.TakePlatformFile();
+  PP_FileHandle file_handle = PP_kInvalidFileHandle;
+  if (it != pending_cache_requests_.end() && base_file.IsValid()) {
+    file_handle = base_file.TakePlatformFile();
     status = PP_OK;
   }
   if (it == pending_cache_requests_.end()) {
@@ -145,7 +129,7 @@ void PnaclTranslationResourceHost::OnNexeTempFileReply(
   } else {
     PpapiGlobals::Get()->GetMainThreadMessageLoop()->PostTask(
         FROM_HERE,
-        base::Bind(&TrackedCallback::Run, it->second.callback, status));
+        base::Bind(it->second, status, is_hit, file_handle));
     pending_cache_requests_.erase(it);
   }
   if (!base_file.IsValid()) {
@@ -158,7 +142,12 @@ void PnaclTranslationResourceHost::CleanupCacheRequests() {
   for (CacheRequestInfoMap::iterator it = pending_cache_requests_.begin();
        it != pending_cache_requests_.end();
        ++it) {
-    it->second.callback->PostAbort();
+    PpapiGlobals::Get()->GetMainThreadMessageLoop()->PostTask(
+        FROM_HERE,
+        base::Bind(it->second,
+                   static_cast<int32_t>(PP_ERROR_ABORTED),
+                   false,
+                   PP_kInvalidFileHandle));
   }
   pending_cache_requests_.clear();
 }
