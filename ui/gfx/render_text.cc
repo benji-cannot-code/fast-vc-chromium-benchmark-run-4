d88038145137021f4700bdcba7ab16f26e3e88b0
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-09 19:53:38 +0000

    Strip carriage return from find text; show newline symbols.
    Blink won't find carriage returns from Windows clipboards.
    Strip those characters before calling WebContents::Find.
    
    The newline character is displayed wrong in textfields.
    Swap it out for the Unicode newline symbol: ␤ (0x2424).
    (the glyph isn't available on Win Views for me now...)
    
    See before/after pics at http://crbug.com/371450#c7
    
    BUG=371450
    TEST=Find-in-page works well for string containing line breaks.
    R=pkasting@chromium.org
    TBR=asvitkine@chromium.org
    
    Review URL: https://codereview.chromium.org/374253002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@282102 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/render_text.cc
+++ ui/gfx/render_text.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/break_iterator.h"
 #include "base/logging.h"
 #include "base/stl_util.h"
+#include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "third_party/icu/source/common/unicode/rbbi.h"
 #include "third_party/icu/source/common/unicode/utf16.h"
@@ -1171,6 +1172,12 @@ void RenderText::UpdateLayoutText() {
     // style than the preceding text. See crbug.com/327850.
     layout_text_.assign(ElideText(layout_text_));
   }
+
+  // Replace the newline character with a newline symbol in single line mode.
+  static const base::char16 kNewline[] = { '\n', 0 };
+  static const base::char16 kNewlineSymbol[] = { 0x2424, 0 };
+  if (!multiline_)
+    base::ReplaceChars(layout_text_, kNewline, kNewlineSymbol, &layout_text_);
 
   ResetLayout();
 }
