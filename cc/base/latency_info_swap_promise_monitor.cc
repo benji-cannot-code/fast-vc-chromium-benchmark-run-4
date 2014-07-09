ff026cb2442cf497ce7eadca7ef55a41eb28b22c
skyostil@chromium.org <skyostil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-09 20:48:41 +0000

    Implement scroll handler latency tracking
    Since scroll handlers do not block composited scrolling, the existing
    input latency metric will only report the response time for the
    compositor when scrolling a page with a handler. This is not accurate
    for pages that implement scroll-synchronized effects, because the
    scroll handler needs to be invoked so that the page contents are
    updated with the new scroll offset.
    
    This patch fixes the problem by forwarding latency information from the
    impl to the main thread when we are scrolling a layer that has an active
    scroll event handler.
    
    In the case the scroll handler is no-op, the latency information will be
    terminated as a failed commit.
    
    Sample trace:
    https://drive.google.com/file/d/0ByyxMXB38gLDRE40bkU5TVd0U00/edit?usp=sharing
    
    BUG=347366
    
    Review URL: https://codereview.chromium.org/365463003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@282117 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/base/latency_info_swap_promise_monitor.cc
+++ cc/base/latency_info_swap_promise_monitor.cc
@@ -10,6 +10,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/trees/layer_tree_host_impl.h"
 #include "cc/trees/layer_tree_impl.h"
 
+namespace {
+
+bool AddRenderingScheduledComponent(ui::LatencyInfo* latency_info) {
+  if (latency_info->FindLatency(
+          ui::INPUT_EVENT_LATENCY_RENDERING_SCHEDULED_COMPONENT, 0, 0))
+    return false;
+  latency_info->AddLatencyNumber(
+      ui::INPUT_EVENT_LATENCY_RENDERING_SCHEDULED_COMPONENT, 0, 0);
+  return true;
+}
+
+}  // namespace
+
 namespace cc {
 
 LatencyInfoSwapPromiseMonitor::LatencyInfoSwapPromiseMonitor(
@@ -22,22 +35,24 @@ LatencyInfoSwapPromiseMonitor::LatencyInfoSwapPromiseMonitor(
 LatencyInfoSwapPromiseMonitor::~LatencyInfoSwapPromiseMonitor() {}
 
 void LatencyInfoSwapPromiseMonitor::OnSetNeedsCommitOnMain() {
-  if (!latency_->FindLatency(
-          ui::INPUT_EVENT_LATENCY_RENDERING_SCHEDULED_COMPONENT, 0, 0)) {
-    latency_->AddLatencyNumber(
-        ui::INPUT_EVENT_LATENCY_RENDERING_SCHEDULED_COMPONENT, 0, 0);
+  if (AddRenderingScheduledComponent(latency_)) {
     scoped_ptr<SwapPromise> swap_promise(new LatencyInfoSwapPromise(*latency_));
     layer_tree_host_->QueueSwapPromise(swap_promise.Pass());
   }
 }
 
 void LatencyInfoSwapPromiseMonitor::OnSetNeedsRedrawOnImpl() {
-  if (!latency_->FindLatency(
-          ui::INPUT_EVENT_LATENCY_RENDERING_SCHEDULED_COMPONENT, 0, 0)) {
-    latency_->AddLatencyNumber(
-        ui::INPUT_EVENT_LATENCY_RENDERING_SCHEDULED_COMPONENT, 0, 0);
+  if (AddRenderingScheduledComponent(latency_)) {
     scoped_ptr<SwapPromise> swap_promise(new LatencyInfoSwapPromise(*latency_));
     layer_tree_host_impl_->active_tree()->QueueSwapPromise(swap_promise.Pass());
+  }
+}
+
+void LatencyInfoSwapPromiseMonitor::OnForwardScrollUpdateToMainThreadOnImpl() {
+  if (AddRenderingScheduledComponent(latency_)) {
+    scoped_ptr<SwapPromise> swap_promise(new LatencyInfoSwapPromise(*latency_));
+    layer_tree_host_impl_->QueueSwapPromiseForMainThreadScrollUpdate(
+        swap_promise.Pass());
   }
 }
 
--- cc/trees/layer_tree_host.h
+++ cc/trees/layer_tree_host.h
@@ -227,7 +227,7 @@ class CC_EXPORT LayerTreeHost {
                                float scale,
                                base::TimeDelta duration);
 
-  void ApplyScrollAndScale(const ScrollAndScaleSet& info);
+  void ApplyScrollAndScale(ScrollAndScaleSet* info);
   void SetImplTransform(const gfx::Transform& transform);
 
   // Virtual for tests.
