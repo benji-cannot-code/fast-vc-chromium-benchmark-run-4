ab0bf46bd458cf0372392a00962b7dd9a48ffe9c
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-09 19:41:12 +0000

    Add API stubs for GCD device commands. Changed registration API to work for one network at once. Fixed comments. Replaced argument GCDDevice with just string id.
    BUG=383167
    R=alexsemenov@chromium.org, asargent@chromium.org, asvitkine@chromium.org, noamsml@chromium.org
    
    Review URL: https://codereview.chromium.org/363883002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@282099 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/api_test/gcd_private/api/send_query.js
+++ chrome/test/data/extensions/api_test/gcd_private/api/send_query.js
@@ -5,11 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 onload = function() {
   chrome.test.runTests([
-    function receiveNewDevice() {
-      chrome.gcdPrivate.onCloudDeviceStateChanged.addListener(
-        function(available, device) {});
-
-      chrome.gcdPrivate.queryForNewLocalDevices();
-      chrome.test.notifyPass();
-  }]);
+      function receiveNewDevice() {
+        chrome.gcdPrivate.onDeviceStateChanged.addListener(function(device) {});
+        chrome.gcdPrivate.queryForNewLocalDevices();
+        chrome.test.notifyPass();
+      }
+  ]);
 };
