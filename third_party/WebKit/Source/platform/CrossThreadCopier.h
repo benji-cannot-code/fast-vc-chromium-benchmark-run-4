362b34241513d9372b271dfd49b1658cd8b215b3
sigbjornf@opera.com <sigbjornf@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-10 13:30:34 +0000

    Oilpan: fix build after r177815
    R=haraken@chromium.org,jochen@chromium.org
    BUG=
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/388463002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177829 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/CrossThreadCopier.h
+++ third_party/WebKit/Source/platform/CrossThreadCopier.h
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/Forward.h"
 #include "wtf/PassOwnPtr.h"
 #include "wtf/PassRefPtr.h"
+#include "wtf/RawPtr.h"
 #include "wtf/RefPtr.h"
 #include "wtf/ThreadSafeRefCounted.h"
 #include "wtf/TypeTraits.h"
@@ -152,11 +153,38 @@ namespace WebCore {
         }
     };
 
+    template<typename T> struct CrossThreadCopierBase<false, false, true, RawPtr<T> > {
+        typedef RawPtr<T> Type;
+        static Type copy(const Type& ptr)
+        {
+            return ptr;
+        }
+    };
+
+    template<typename T> struct CrossThreadCopierBase<false, false, true, Member<T> > {
+        typedef RawPtr<T> Type;
+        static Type copy(const Member<T>& ptr)
+        {
+            return ptr;
+        }
+    };
+
+    template<typename T> struct CrossThreadCopierBase<false, false, true, WeakMember<T> > {
+        typedef RawPtr<T> Type;
+        static Type copy(const WeakMember<T>& ptr)
+        {
+            return ptr;
+        }
+    };
+
     template<typename T> struct CrossThreadCopier : public CrossThreadCopierBase<WTF::IsConvertibleToInteger<T>::value,
         WTF::IsSubclassOfTemplate<typename WTF::RemoveTemplate<T, RefPtr>::Type, ThreadSafeRefCounted>::value
             || WTF::IsSubclassOfTemplate<typename WTF::RemovePointer<T>::Type, ThreadSafeRefCounted>::value
             || WTF::IsSubclassOfTemplate<typename WTF::RemoveTemplate<T, PassRefPtr>::Type, ThreadSafeRefCounted>::value,
-        WTF::IsSubclassOfTemplate<typename WTF::RemovePointer<T>::Type, GarbageCollected>::value,
+        WTF::IsSubclassOfTemplate<typename WTF::RemovePointer<T>::Type, GarbageCollected>::value
+            || WTF::IsSubclassOfTemplate<typename WTF::RemoveTemplate<T, RawPtr>::Type, GarbageCollected>::value
+            || WTF::IsSubclassOfTemplate<typename WTF::RemoveTemplate<T, Member>::Type, GarbageCollected>::value
+            || WTF::IsSubclassOfTemplate<typename WTF::RemoveTemplate<T, WeakMember>::Type, GarbageCollected>::value,
         T> {
     };
 
