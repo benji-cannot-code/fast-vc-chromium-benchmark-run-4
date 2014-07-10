d7a63846f493b4413e6c98022181e0f6c39f4ac0
jl@opera.com <jl@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-10 17:29:50 +0000

    IDL: Treat undefined as missing for optional arguments with defaults
    For optional arguments with defaults, treat undefined as missing simply
    by checking 'info[N].IsUndefined()' instead of 'N <= info.Length()'.
    
    This works since V8's indexing operator returns an undefined value for
    all out-of-range indexes.
    
    Changes in behavior that had (or now has) testing coverade matches
    Firefox (Nightly), except in the case of WebKitCSSMatrix's constructor,
    which Firefox naturally doesn't have.
    
    BUG=335871
    
    Review URL: https://codereview.chromium.org/373043004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177857 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/workers/resources/shared-worker-name.js
+++ third_party/WebKit/LayoutTests/fast/workers/resources/shared-worker-name.js
@@ -61,6 +61,17 @@ function test3()
 
 function test4()
 {
+    // Creating a worker with an undefined name should match an existing worker with no name.
+    var worker = new SharedWorker('resources/shared-worker-common.js', undefined);
+    worker.port.postMessage("eval self.foo");
+    worker.port.onmessage = function(event) {
+        shouldBeEqual("creating worker with an undefined name", event.data, "self.foo: 1234");
+        nextTest();
+    }
+}
+
+function test5()
+{
     // Creating a worker with a different name should not be the same as a worker with no name.
     var worker = new SharedWorker('resources/shared-worker-common.js', 'name');
     worker.port.postMessage("eval self.foo");
@@ -70,7 +81,7 @@ function test4()
     };
 }
 
-function test5()
+function test6()
 {
     // Creating a worker for an alternate URL with no name should work.
     var worker = new SharedWorker('resources/shared-worker-common.js?url=1');
@@ -81,7 +92,7 @@ function test5()
     };
 }
 
-function test6()
+function test7()
 {
     // Creating a worker for an alternate URL with empty name should work.
     var worker = new SharedWorker('resources/shared-worker-common.js?url=2', '');
@@ -92,7 +103,7 @@ function test6()
     };
 }
 
-function test7()
+function test8()
 {
     // Make sure we can create a shared worker with name 'null'.
     try {
@@ -109,7 +120,7 @@ function test7()
     }
 }
 
-function test8()
+function test9()
 {
     // Creating a worker with a null name should match an existing worker with name 'null'
     var worker = new SharedWorker('resources/shared-worker-common.js', null);
@@ -120,35 +131,7 @@ function test8()
     }
 }
 
-function test9()
-{
-    // Make sure we can create a shared worker with name 'undefined'.
-    try {
-        var worker = new SharedWorker('resources/shared-worker-common.js', 'undefined');
-        testPassed("created SharedWorker with name 'undefined'");
-        worker.port.postMessage("eval self.foo = 1111");
-        worker.port.onmessage = function(event) {
-            shouldBeEqual("setting self.foo", event.data, "self.foo = 1111: 1111");
-            nextTest();
-        };
-    } catch (e) {
-        testFailed("SharedWorker with name 'undefined' threw an exception: " + e);
-        done();
-    }
-}
-
 function test10()
-{
-    // Creating a worker with an undefined name should match an existing worker with name 'undefined'
-    var worker = new SharedWorker('resources/shared-worker-common.js', undefined);
-    worker.port.postMessage("eval self.foo");
-    worker.port.onmessage = function(event) {
-        shouldBeEqual("creating worker with an undefined name", event.data, "self.foo: 1111");
-        nextTest();
-    }
-}
-
-function test11()
 {
     // Creating a worker with a specific name, the name attribute should be set to worker correctly.
     var worker = new SharedWorker('resources/shared-worker-common.js', "testingNameAttribute");
