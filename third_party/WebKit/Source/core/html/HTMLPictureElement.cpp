9bf79f54bd77b35a0daf4021cb0b365b61c8dd03
yoav@yoav.ws <yoav@yoav.ws@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-10 00:15:05 +0000

    Fold ImageLoader::updateFromElementIgnoringPreviousError into updateFromElement
    This CL is just a refactor that should have no functional impact. It is done as ground work in order to later add yet a third mode to updateFromElement.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/378163002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177787 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLPictureElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLPictureElement.cpp
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/HTMLNames.h"
 #include "core/dom/ElementTraversal.h"
 #include "core/html/HTMLImageElement.h"
+#include "core/loader/ImageLoader.h"
 
 namespace WebCore {
 
@@ -25,7 +26,7 @@ DEFINE_NODE_FACTORY(HTMLPictureElement)
 void HTMLPictureElement::sourceOrMediaChanged()
 {
     for (HTMLImageElement* imageElement = Traversal<HTMLImageElement>::firstChild(*this); imageElement; imageElement = Traversal<HTMLImageElement>::nextSibling(*imageElement)) {
-        imageElement->selectSourceURL(HTMLImageElement::UpdateNormal);
+        imageElement->selectSourceURL(ImageLoader::UpdateNormal);
     }
 }
 
--- third_party/WebKit/Source/core/loader/ImageLoader.cpp
+++ third_party/WebKit/Source/core/loader/ImageLoader.cpp
@@ -268,9 +268,12 @@ void ImageLoader::doUpdateFromElement(bool bypassMainWorldCSP)
     updatedHasPendingEvent();
 }
 
-void ImageLoader::updateFromElement(LoadType loadType)
+void ImageLoader::updateFromElement(UpdateFromElementBehavior behavior, LoadType loadType)
 {
     AtomicString attr = m_element->imageSourceURL();
+
+    if (behavior == UpdateIgnorePreviousError)
+        clearFailedLoadURL();
 
     if (!m_failedLoadURL.isEmpty() && attr == m_failedLoadURL)
         return;
@@ -297,12 +300,6 @@ void ImageLoader::updateFromElement(LoadType loadType)
     } else {
         doUpdateFromElement(false);
     }
-}
-
-void ImageLoader::updateFromElementIgnoringPreviousError()
-{
-    clearFailedLoadURL();
-    updateFromElement();
 }
 
 KURL ImageLoader::imageURL() const
