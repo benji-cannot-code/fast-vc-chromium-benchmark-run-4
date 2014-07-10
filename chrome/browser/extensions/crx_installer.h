9df9e647c4fb9fd013b2cf42203085f101ac0536
tmdiep@chromium.org <tmdiep@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-10 04:21:18 +0000

    Update the CrxInstaller and UnpackedInstaller to use the ExtensionInstallChecker
    This patch removes the deprecated ExtensionInstaller and uses
    ExtensionInstallChecker in CrxInstaller and UnpackedInstaller.
    
    BUG=386404
    TEST=browser_tests
    TBR=pam@chromium.org (for include in supervised_user_service_unittest.cc)
    
    Review URL: https://codereview.chromium.org/381553002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@282250 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/crx_installer.h
+++ chrome/browser/extensions/crx_installer.h
@@ -15,8 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/version.h"
 #include "chrome/browser/extensions/blacklist.h"
+#include "chrome/browser/extensions/extension_install_checker.h"
 #include "chrome/browser/extensions/extension_install_prompt.h"
-#include "chrome/browser/extensions/extension_installer.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/sandboxed_unpacker.h"
 #include "chrome/browser/extensions/webstore_installer.h"
@@ -196,9 +196,9 @@ class CrxInstaller
 
   bool did_handle_successfully() const { return did_handle_successfully_; }
 
-  Profile* profile() { return installer_.profile(); }
+  Profile* profile() { return install_checker_.profile(); }
 
-  const Extension* extension() { return installer_.extension().get(); }
+  const Extension* extension() { return install_checker_.extension().get(); }
 
   const std::string& current_version() const { return current_version_; }
 
@@ -230,11 +230,12 @@ class CrxInstaller
                                const Extension* extension,
                                const SkBitmap& install_icon) OVERRIDE;
 
-  // Called on the UI thread to start the requirements check on the extension.
-  void CheckImportsAndRequirements();
+  // Called on the UI thread to start the requirements, policy and blacklist
+  // checks on the extension.
+  void CheckInstall();
 
-  // Runs on the UI thread. Callback from RequirementsChecker.
-  void OnRequirementsChecked(std::vector<std::string> requirement_errors);
+  // Runs on the UI thread. Callback from ExtensionInstallChecker.
+  void OnInstallChecksComplete(int failed_checks);
 
   // Runs on the UI thread. Callback from Blacklist.
   void OnBlacklistChecked(
@@ -418,8 +419,8 @@ class CrxInstaller
   // The flags for ExtensionService::OnExtensionInstalled.
   int install_flags_;
 
-  // Gives access to common methods and data of an extension installer.
-  ExtensionInstaller installer_;
+  // Performs requirements, policy and blacklist checks on the extension.
+  ExtensionInstallChecker install_checker_;
 
   DISALLOW_COPY_AND_ASSIGN(CrxInstaller);
 };
--- chrome/browser/supervised_user/supervised_user_service_unittest.cc
+++ chrome/browser/supervised_user/supervised_user_service_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/prefs/scoped_user_pref_update.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/chrome_notification_types.h"
+#include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/extension_service_test_base.h"
 #include "chrome/browser/extensions/unpacked_installer.h"
 #include "chrome/browser/profiles/profile.h"
