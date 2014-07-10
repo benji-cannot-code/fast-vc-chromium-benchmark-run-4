0eea66dbe8929fa129819ed42690314e4f12228d
horo@chromium.org <horo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-10 09:43:59 +0000

    Revert 282286 "Expose "visible" and "authenticated" properties."
    We need to revert r282212 which breaks XP tests.
    > Expose "visible" and "authenticated" properties.
    > 
    > They are always "true" for now, due to the way BLE devices work under Windows 8.
    > 
    > BUG=388016
    > 
    > Review URL: https://codereview.chromium.org/378293007
    
    TBR=rpaquay@chromium.org
    
    Review URL: https://codereview.chromium.org/388453003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@282294 0039d316-1c4b-4281-b951-d872f2087c98

--- device/bluetooth/bluetooth_low_energy_win.h
+++ device/bluetooth/bluetooth_low_energy_win.h
@@ -114,8 +114,6 @@ struct BluetoothLowEnergyDeviceInfo {
   std::string id;
   std::string friendly_name;
   BLUETOOTH_ADDRESS address;
-  bool visible;
-  bool authenticated;
   bool connected;
 };
 
