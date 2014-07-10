524974ddce489457cab02c5a0cb0ea9f0a651f3c
tnagel@chromium.org <tnagel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-10 16:30:56 +0000

    Fix spelling of 'Enterprise' in several places.
    TBR=dzhioev
    TBR=grt
    BUG=none
    
    Review URL: https://codereview.chromium.org/380233002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@282350 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/policy/enterprise_install_attributes.cc
+++ chrome/browser/chromeos/policy/enterprise_install_attributes.cc
@@ -29,7 +29,7 @@ std::string GetDeviceModeString(DeviceMode mode) {
     case DEVICE_MODE_CONSUMER:
       return EnterpriseInstallAttributes::kConsumerDeviceMode;
     case DEVICE_MODE_ENTERPRISE:
-      return EnterpriseInstallAttributes::kEnterpiseDeviceMode;
+      return EnterpriseInstallAttributes::kEnterpriseDeviceMode;
     case DEVICE_MODE_RETAIL_KIOSK:
       return EnterpriseInstallAttributes::kRetailKioskDeviceMode;
     case DEVICE_MODE_CONSUMER_KIOSK_AUTOLAUNCH:
@@ -47,7 +47,7 @@ DeviceMode GetDeviceModeFromString(
     const std::string& mode) {
   if (mode == EnterpriseInstallAttributes::kConsumerDeviceMode)
     return DEVICE_MODE_CONSUMER;
-  else if (mode == EnterpriseInstallAttributes::kEnterpiseDeviceMode)
+  else if (mode == EnterpriseInstallAttributes::kEnterpriseDeviceMode)
     return DEVICE_MODE_ENTERPRISE;
   else if (mode == EnterpriseInstallAttributes::kRetailKioskDeviceMode)
     return DEVICE_MODE_RETAIL_KIOSK;
@@ -71,7 +71,7 @@ bool ReadMapKey(const std::map<std::string, std::string>& map,
 }  // namespace
 
 const char EnterpriseInstallAttributes::kConsumerDeviceMode[] = "consumer";
-const char EnterpriseInstallAttributes::kEnterpiseDeviceMode[] = "enterprise";
+const char EnterpriseInstallAttributes::kEnterpriseDeviceMode[] = "enterprise";
 const char EnterpriseInstallAttributes::kRetailKioskDeviceMode[] = "kiosk";
 const char EnterpriseInstallAttributes::kConsumerKioskDeviceMode[] =
     "consumer_kiosk";
