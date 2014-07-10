c9ac0b338b2313d8e167cfda6531c8180dbac85c
horo@chromium.org <horo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-10 11:15:37 +0000

    Temporary disables 2 tests in service_worker_browsertest.cc.
    These tests will be broken by the blink side change which removes HeaderMap and introduces Headers class.
    https://codereview.chromium.org/373613004/
    
    After the blink side change will land, I will re-enable the tests.
    
    BUG=373120
    
    Review URL: https://codereview.chromium.org/379323003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@282309 0039d316-1c4b-4281-b951-d872f2087c98

--- content/test/data/service_worker/fetch_event.js
+++ content/test/data/service_worker/fetch_event.js
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 this.onfetch = function(event) {
-    var headers = new HeaderMap;
+    var headers = new Headers;
     headers.set('Content-Language', 'fi');
     headers.set('Content-Type', 'text/html; charset=UTF-8');
     var blob = new Blob(["This resource is gone. Gone, gone, gone."]);
