45478b30ba403ae42af74244c7185e03e08a1baa
horo@chromium.org <horo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-10 11:31:17 +0000

    [ServiceWorker] Make Response class better conformance with the spec.
    In current implementation Response class only supports status, statusText, and headers attributes.
    This change introduce type, url attributes and change the class of headers attribute from HeaderMap to Headers and make all attributes readonly.
    And also this change implements the logic which is defined in the spec.
    
    http://fetch.spec.whatwg.org/#response-class 
    http://fetch.spec.whatwg.org/#response
    
    
    This cl depends on http://crrev.com/329853012 and http://crrev.com/370733002
    
    BUG=373120
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=177793
    
    Review URL: https://codereview.chromium.org/373613004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177817 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/serviceworkers/ResponseInit.h
+++ third_party/WebKit/Source/modules/serviceworkers/ResponseInit.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ResponseInit_h
 
 #include "bindings/core/v8/Dictionary.h"
-#include "modules/serviceworkers/HeaderMap.h"
+#include "modules/serviceworkers/Headers.h"
 #include "wtf/RefPtr.h"
 
 namespace WebCore {
@@ -26,11 +26,15 @@ struct ResponseInit {
         // FIXME: Spec uses ByteString for statusText. http://crbug.com/347426
         options.get("statusText", statusText);
         options.get("headers", headers);
+        if (!headers) {
+            options.get("headers", headersDictionary);
+        }
     }
 
     unsigned short status;
     String statusText;
-    RefPtr<HeaderMap> headers;
+    RefPtr<Headers> headers;
+    Dictionary headersDictionary;
 };
 
 }
