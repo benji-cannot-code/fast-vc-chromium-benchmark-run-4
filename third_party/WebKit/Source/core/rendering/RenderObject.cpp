a25ce847c62a5716334695f648bfea41f5e90a62
chrishtr@chromium.org <chrishtr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-10 04:59:19 +0000

    Get rid of paint invalidation code in RenderObject::styleWillChange when layout is needed
    BUG=
    
    Review URL: https://codereview.chromium.org/366503004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177801 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderObject.cpp
+++ third_party/WebKit/Source/core/rendering/RenderObject.cpp
@@ -2094,8 +2094,6 @@ void RenderObject::styleWillChange(StyleDifference diff, const RenderStyle& newS
                     layer->setHasVisibleContent();
                 } else if (layer->hasVisibleContent() && (this == layer->renderer() || layer->renderer()->style()->visibility() != VISIBLE)) {
                     layer->dirtyVisibleContentStatus();
-                    if (diff.needsLayout())
-                        paintInvalidationForWholeRenderer();
                 }
             }
         }
