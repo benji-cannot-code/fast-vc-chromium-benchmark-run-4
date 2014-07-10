66b9549f9471154ffa124773ebfa981bca48f99a
horo@chromium.org <horo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-10 02:29:43 +0000

    Revert 282098 "Add function queryForNewLocalDevices to send quer..."
    > Add function queryForNewLocalDevices to send query for new local devices,
    > implement it for mDNS (WiFi devices forthcoming)
    > 
    > BUG=383167
    > 
    > Review URL: https://codereview.chromium.org/356613002
    
    TBR=noamsml@chromium.org
    
    Review URL: https://codereview.chromium.org/377383008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@282217 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/gcd_private/gcd_private_api.h
+++ chrome/browser/extensions/api/gcd_private/gcd_private_api.h
@@ -38,8 +38,6 @@ class GcdPrivateAPI : public BrowserContextKeyedAPI,
   // BrowserContextKeyedAPI implementation.
   static BrowserContextKeyedAPIFactory<GcdPrivateAPI>* GetFactoryInstance();
 
-  bool QueryForDevices();
-
  private:
   friend class BrowserContextKeyedAPIFactory<GcdPrivateAPI>;
 
@@ -101,7 +99,7 @@ class GcdPrivateGetCloudDeviceListFunction
 };
 
 class GcdPrivateQueryForNewLocalDevicesFunction
-    : public ChromeSyncExtensionFunction {
+    : public ChromeAsyncExtensionFunction {
  public:
   DECLARE_EXTENSION_FUNCTION("gcdPrivate.queryForNewLocalDevices",
                              GCDPRIVATE_QUERYFORNEWLOCALDEVICES)
@@ -111,8 +109,8 @@ class GcdPrivateQueryForNewLocalDevicesFunction
  protected:
   virtual ~GcdPrivateQueryForNewLocalDevicesFunction();
 
-  // SyncExtensionFunction overrides.
-  virtual bool RunSync() OVERRIDE;
+  // AsyncExtensionFunction overrides.
+  virtual bool RunAsync() OVERRIDE;
 };
 
 class GcdPrivateStartSetupFunction : public ChromeAsyncExtensionFunction {
