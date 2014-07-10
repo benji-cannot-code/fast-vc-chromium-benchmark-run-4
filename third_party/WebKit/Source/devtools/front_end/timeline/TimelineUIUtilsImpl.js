bf4722e04832233ca27ff7e36ca232fa1463b7a0
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-10 18:04:30 +0000

    Whitelist events visible on tracing based timeline
    We use blacklist for records that we want to hide in the old timeline implentation. However, in case of tracing we don't know all event names upfront, so whitelist approach is more appropriate in this case.
    
    BUG=361045
    
    Review URL: https://codereview.chromium.org/385623002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177863 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/timeline/TimelineUIUtilsImpl.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TimelineUIUtilsImpl.js
@@ -174,7 +174,7 @@ WebInspector.TimelineUIUtilsImpl.prototype = {
             recordTypes.ScheduleStyleRecalculation,
             recordTypes.UpdateCounters
         ]
-        return new WebInspector.TimelineRecordTypeFilter(hiddenRecords);
+        return new WebInspector.TimelineRecordHiddenTypeFilter(hiddenRecords);
     },
 
     /**
