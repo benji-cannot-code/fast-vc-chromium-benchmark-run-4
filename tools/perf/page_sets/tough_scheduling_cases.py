940f3b4897d2d859463c504ff506b381e69d0cae
picksi@chromium.org <picksi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-11 23:55:06 +0000

    Four new pages added to tough_scheduling_cases
    BUG=391005
    
    Review URL: https://codereview.chromium.org/372243003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@282740 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/page_sets/tough_scheduling_cases.py
+++ tools/perf/page_sets/tough_scheduling_cases.py
@@ -332,7 +332,6 @@ class EmptyTouchHandlerPage(ToughSchedulingCasesPage):
       self.synthetic_delays = {
         'blink.HandleInputEvent': {'target_duration': 0.2}
       }
-
     self.bounce = bounce
 
   def RunSmoothness(self, action_runner):
@@ -372,7 +371,8 @@ class ToughSchedulingCasesPageSet(page_set_module.PageSet):
   """ Tough scheduler latency test cases """
 
   def __init__(self):
-    super(ToughSchedulingCasesPageSet, self).__init__()
+    super(ToughSchedulingCasesPageSet, self).__init__(
+        archive_data_file='data/tough_scheduling_cases.json')
 
     # Why: Simple scrolling baseline
     self.AddPage(ToughSchedulingCasesPage(
@@ -466,3 +466,14 @@ class ToughSchedulingCasesPageSet(page_set_module.PageSet):
       page_set=self))
     # Why: For measuring the latency of scroll-synchronized effects.
     self.AddPage(SynchronizedScrollOffsetPage(page_set=self))
+
+    real_pages_url_list = [
+          'http://www.latimes.com',
+          ('http://io9.com/the-10-most-shocking-characters-that-george-r-r-' +
+           'martin-1579481155/+tinaamini'),
+          'http://ftw.usatoday.com/2014/05/spelling-bee-rules-shenanigans',
+          'http://m.espn.go.com/nhl/rankings'
+    ]
+
+    for url in real_pages_url_list:
+      self.AddPage(ToughSchedulingCasesPage(url, self))
