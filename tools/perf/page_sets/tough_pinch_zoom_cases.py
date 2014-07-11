1e2a007f73d9a82d9b36ac0f00f982d2180863b5
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-11 14:55:15 +0000

    [Telemetry] Fix tough_pinch_zoom_cases after r282218.
    BUG=361809
    TBR=dtu@chromium.org,chrishenry@google.com
    NOTRY=True
    
    Review URL: https://codereview.chromium.org/383963006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@282628 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/page_sets/tough_pinch_zoom_cases.py
+++ tools/perf/page_sets/tough_pinch_zoom_cases.py
@@ -106,7 +106,7 @@ class GooglePlusPage(ToughPinchZoomCasesPage):
   def RunSmoothness(self, action_runner):
     interaction = action_runner.BeginGestureInteraction(
         'PinchAction', is_smooth=True)
-    action_runner.PinchElement(selector='110031535020051778989-tab-bar')
+    action_runner.PinchElement(selector='#110031535020051778989-tab-bar')
     interaction.End()
 
 
@@ -237,7 +237,7 @@ class YahooAnswersPage(ToughPinchZoomCasesPage):
   def RunSmoothness(self, action_runner):
     interaction = action_runner.BeginGestureInteraction(
         'PinchAction', is_smooth=True)
-    action_runner.PinchElement(selector='ya-content-apps')
+    action_runner.PinchElement(selector='#ya-content-apps')
     interaction.End()
 
 
