7e715401d039ba60a3c72dca7384f3f8f2985835
alph@chromium.org <alph@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-11 11:04:35 +0000

    DevTools: Show other threads besides main on the timeline flame chart.
    BUG=361045
    
    Review URL: https://codereview.chromium.org/380253004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177924 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/timeline/TracingTimelineModel.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TracingTimelineModel.js
@@ -13,7 +13,6 @@ WebInspector.TracingTimelineModel = function(tracingModel, recordFilter)
 {
     WebInspector.TimelineModel.call(this, tracingModel.target());
     this._tracingModel = tracingModel;
-    this._mainThreadEvents = [];
     this._inspectedTargetEvents = [];
     this._recordFilter = recordFilter;
 
@@ -215,12 +214,20 @@ WebInspector.TracingTimelineModel.prototype = {
      */
     mainThreadEvents: function()
     {
-        return this._mainThreadEvents;
+        return this._virtualThreads[WebInspector.TimelineModel.MainThreadName] || [];
+    },
+
+    /**
+     * @return {!Object.<string, !Array.<!WebInspector.TracingModel.Event>>}
+     */
+    virtualThreads: function()
+    {
+        return this._virtualThreads;
     },
 
     reset: function()
     {
-        this._mainThreadEvents = [];
+        this._virtualThreads = {};
         this._inspectedTargetEvents = [];
         WebInspector.TimelineModel.prototype.reset.call(this);
     },
@@ -228,7 +235,7 @@ WebInspector.TracingTimelineModel.prototype = {
     _buildTimelineRecords: function()
     {
         var recordStack = [];
-        var mainThreadEvents = this._mainThreadEvents;
+        var mainThreadEvents = this.mainThreadEvents();
         for (var i = 0, size = mainThreadEvents.length; i < size; ++i) {
             var event = mainThreadEvents[i];
             while (recordStack.length) {
@@ -301,8 +308,13 @@ WebInspector.TracingTimelineModel.prototype = {
             if (endTime && event.startTime >= endTime)
                 break;
             this._processEvent(event);
-            if (thread === mainThread)
-                this._mainThreadEvents.push(event);
+            var threadName = thread === mainThread ? WebInspector.TimelineModel.MainThreadName : thread.name();
+            var threadEvents = this._virtualThreads[threadName];
+            if (!threadEvents) {
+                threadEvents = [];
+                this._virtualThreads[threadName] = threadEvents;
+            }
+            threadEvents.push(event);
             this._inspectedTargetEvents.push(event);
         }
     },
@@ -332,7 +344,7 @@ WebInspector.TracingTimelineModel.prototype = {
 
         switch (event.name) {
         case recordTypes.CallStack:
-            var lastMainThreadEvent = this._mainThreadEvents.peekLast();
+            var lastMainThreadEvent = this.mainThreadEvents().peekLast();
             if (lastMainThreadEvent && event.args.stack && event.args.stack.length)
                 lastMainThreadEvent.stackTrace = event.args.stack;
             break;
