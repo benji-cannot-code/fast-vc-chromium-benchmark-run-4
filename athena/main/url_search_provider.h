6faeb655135cff9a5ee1c74a7191a43a7a1e41c8
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-11 01:47:17 +0000

    Add basic support of URL/search query to Athena home card.
    It still takes time to componentize autocomplete input and search
    features. However, we have componentized some important parts
    already. The added code will be replaced by the real autocomplete
    code in the future, but meanwhile, this is good enough.
    
    Feature:
    - the logic to check if the input looks like URL / host names
    - canonicalize the input to form the actual URL (http://...)
    - use TemplateURLService to create the search result page URL
      if it looks like a query.
    
    BUG=387207
    R=oshima@chromium.org, hashimoto@chromium.org
    TBR=pkasting@chromium.org, jar@chromium.org
    TEST=manually
    
    Review URL: https://codereview.chromium.org/382683002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@282497 0039d316-1c4b-4281-b951-d872f2087c98

--- athena/main/url_search_provider.h
+++ athena/main/url_search_provider.h
@@ -6,7 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ATHENA_MAIN_URL_SEARCH_PROVIDER_H_
 #define ATHENA_MAIN_URL_SEARCH_PROVIDER_H_
 
+#include "base/memory/scoped_ptr.h"
 #include "ui/app_list/search_provider.h"
+
+class TemplateURLService;
 
 namespace content {
 class BrowserContext;
@@ -26,6 +29,9 @@ class UrlSearchProvider : public app_list::SearchProvider {
 
  private:
   content::BrowserContext* browser_context_;
+
+  // TODO(mukai): This should be provided through BrowserContextKeyedService.
+  scoped_ptr<TemplateURLService> template_url_service_;
 
   DISALLOW_COPY_AND_ASSIGN(UrlSearchProvider);
 };
