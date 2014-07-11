e74816d4a11a7579bc38d7696fba97964fc7eb27
rouslan@chromium.org <rouslan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-11 23:18:33 +0000

    Reland "Use upstream libaddressinput in Chrome."
    Remove static initializer and reland https://crrev.com/282408 which was
    reverted in https://crrev.com/282426.
    
    TBR=estade@chromium.org,thestig@chromium.org
    BUG=389918
    
    Review URL: https://codereview.chromium.org/386873002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@282726 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/autofill/autofill_dialog_controller_browsertest.cc
+++ chrome/browser/ui/autofill/autofill_dialog_controller_browsertest.cc
@@ -82,7 +82,6 @@ namespace {
 
 using testing::Return;
 using testing::_;
-using ::i18n::addressinput::AddressValidator;
 
 void MockCallback(AutofillClient::RequestAutocompleteResult,
                   const base::string16& message,
--- chrome/browser/ui/autofill/autofill_dialog_i18n_input.h
+++ chrome/browser/ui/autofill/autofill_dialog_i18n_input.h
@@ -13,13 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/autofill/autofill_dialog_common.h"
 #include "chrome/browser/ui/autofill/autofill_dialog_types.h"
 #include "components/autofill/core/browser/autofill_type.h"
-#include "third_party/libaddressinput/chromium/cpp/include/libaddressinput/address_field.h"
-
-namespace i18n {
-namespace addressinput {
-struct AddressData;
-}
-}
+#include "third_party/libaddressinput/src/cpp/include/libaddressinput/address_field.h"
 
 namespace autofill {
 
