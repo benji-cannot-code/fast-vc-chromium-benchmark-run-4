222ca1d1cb5f5fc329101db1feb6dfc4a57215af
aandrey@chromium.org <aandrey@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-11 13:15:53 +0000

    DevTools: Introduce String.prototype.isDigitAt.
    R=eustas, apavlov
    
    Review URL: https://codereview.chromium.org/388783002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177943 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/common/utilities.js
+++ third_party/WebKit/Source/devtools/front_end/common/utilities.js
@@ -277,6 +277,16 @@ String.prototype.hashCode = function()
 }
 
 /**
+ * @param {number} index
+ * @return {boolean}
+ */
+String.prototype.isDigitAt = function(index)
+{
+    var c = this.charCodeAt(index);
+    return 48 <= c && c <= 57;
+}
+
+/**
  * @param {string} a
  * @param {string} b
  * @return {number}
@@ -904,11 +914,6 @@ String.tokenizeFormatString = function(format, formatters)
         tokens.push({ type: "specifier", specifier: specifier, precision: precision, substitutionIndex: substitutionIndex });
     }
 
-    function isDigit(c)
-    {
-        return !!/[0-9]/.exec(c);
-    }
-
     var index = 0;
     for (var precentIndex = format.indexOf("%", index); precentIndex !== -1; precentIndex = format.indexOf("%", index)) {
         addStringToken(format.substring(index, precentIndex));
@@ -921,10 +926,10 @@ String.tokenizeFormatString = function(format, formatters)
             continue;
         }
 
-        if (isDigit(format[index])) {
+        if (format.isDigitAt(index)) {
             // The first character is a number, it might be a substitution index.
             var number = parseInt(format.substring(index), 10);
-            while (isDigit(format[index]))
+            while (format.isDigitAt(index))
                 ++index;
 
             // If the number is greater than zero and ends with a "$",
@@ -944,7 +949,7 @@ String.tokenizeFormatString = function(format, formatters)
             if (isNaN(precision))
                 precision = 0;
 
-            while (isDigit(format[index]))
+            while (format.isDigitAt(index))
                 ++index;
         }
 
