27749575997e41df18c111906d9cde59b22f692c
brandonsalmon@chromium.org <brandonsalmon@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-11 07:45:07 +0000

    Adding cache hit/miss histograms to DiskBasedCertCache. There are separate histograms for the in-memory MRU cache and the disk cache.
    This cl is closely related to https://codereview.chromium.org/356953003/, which implements similar histograms in the http_cache.
    
    and a follow up to https://codereview.chromium.org/361513003/
    
    BUG=
    
    Review URL: https://codereview.chromium.org/378063002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@282584 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/disk_based_cert_cache.h
+++ net/http/disk_based_cert_cache.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2014 The Chromium Authors. All rights reserved.
+// Copyright (c) 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -48,11 +48,11 @@ class NET_EXPORT_PRIVATE DiskBasedCertCache {
   void Set(const X509Certificate::OSCertHandle cert_handle,
            const SetCallback& cb);
 
-  // Returns the number of in-memory MRU cache hits that have occured
+  // Returns the number of in-memory MRU cache hits that have occurred
   // on Set and Get operations. Intended for test purposes only.
   size_t mem_cache_hits_for_testing() const { return mem_cache_hits_; }
 
-  // Returns the number of in-memory MRU cache misses that have occured
+  // Returns the number of in-memory MRU cache misses that have occurred
   // on Set and Get operations. Intended for test purposes only.
   size_t mem_cache_misses_for_testing() const { return mem_cache_misses_; }
 
