fe40214d2ebb70a36b3e6fa93223f02cfc58d642
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-11 04:48:15 +0000

    Remove RenderView::GetPageId.
    BUG=371151
    TEST=everything still works
    
    Review URL: https://codereview.chromium.org/367113006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@282545 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_view_impl.h
+++ content/renderer/render_view_impl.h
@@ -481,7 +481,6 @@ class CONTENT_EXPORT RenderViewImpl
   virtual bool Send(IPC::Message* message) OVERRIDE;
   virtual RenderFrame* GetMainRenderFrame() OVERRIDE;
   virtual int GetRoutingID() const OVERRIDE;
-  virtual int GetPageId() const OVERRIDE;
   virtual gfx::Size GetSize() const OVERRIDE;
   virtual WebPreferences& GetWebkitPreferences() OVERRIDE;
   virtual void SetWebkitPreferences(const WebPreferences& preferences) OVERRIDE;
