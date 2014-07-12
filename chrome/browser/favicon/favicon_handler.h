6aefa22e84ca684b34208af7416e0dead723bc39
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-12 12:53:38 +0000

    Cleanup: favicon: Remove some chrome/ includes from favicon_handler.
    BUG=359514
    TEST=None
    R=blundell@chromium.org
    
    Review URL: https://codereview.chromium.org/373463004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@282819 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/favicon/favicon_handler.h
+++ chrome/browser/favicon/favicon_handler.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_forward.h"
 #include "base/memory/ref_counted.h"
 #include "base/task/cancelable_task_tracker.h"
-#include "chrome/browser/favicon/favicon_tab_helper.h"
 #include "components/favicon/core/favicon_url.h"
 #include "components/favicon_base/favicon_callback.h"
 #include "ui/gfx/favicon_size.h"
