e18046667ccb22643933ca078f9a71b96403109f
pkasting@chromium.org <pkasting@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-12 00:27:09 +0000

    Fixes for re-enabling more MSVC level 4 warnings: Source/core/rendering/ edition
    This contains fixes for the following sorts of issues:
    * Assignment inside conditional
    * Possibly-uninitialized local variable
    
    BUG=81439
    TEST=none
    R=fmalita@chromium.org
    
    Review URL: https://codereview.chromium.org/386043003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177987 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/FastTextAutosizer.cpp
+++ third_party/WebKit/Source/core/rendering/FastTextAutosizer.cpp
@@ -122,7 +122,7 @@ static const RenderObject* parentElementRenderer(const RenderObject* renderer)
     if (!node)
         return 0;
 
-    while ((node = node->parentNode())) {
+    for (node = node->parentNode(); node; node = node->parentNode()) {
         if (node->isElementNode())
             return node->renderer();
     }
@@ -832,7 +832,7 @@ float FastTextAutosizer::widthFromBlock(const RenderBlock* block)
 
     // Tables may be inflated before computing their preferred widths. Try several methods to
     // obtain a width, and fall back on a containing block's width.
-    do {
+    for (; block; block = block->containingBlock()) {
         float width;
         Length specifiedWidth = block->isTableCell()
             ? toRenderTableCell(block)->styleOrColLogicalWidth() : block->style()->logicalWidth();
@@ -848,7 +848,7 @@ float FastTextAutosizer::widthFromBlock(const RenderBlock* block)
         }
         if ((width = block->contentLogicalWidth().toFloat()) > 0)
             return width;
-    } while ((block = block->containingBlock()));
+    }
     return 0;
 }
 
