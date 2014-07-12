9a0e66041300df8343ac937af0407f5d94ba25f6
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-12 04:46:37 +0000

    aura: Make sure redirected events have the correct location.
    If an event is redirected to a different WindowTreeHost because the target
    window belongs to that, then make sure the event is in the WindowTreeHost's
    root-window's coordinate space. This is necessary to make sure that the
    target (and the pre/post-target handlers) see the event in the correct
    location.
    
    BUG=385770
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/380343002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@282794 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/window_targeter.cc
+++ ui/aura/window_targeter.cc
@@ -16,6 +16,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace aura {
 
+namespace {
+
+bool IsLocatedEvent(const ui::Event& event) {
+  return event.IsMouseEvent() || event.IsTouchEvent() ||
+         event.IsScrollEvent() || event.IsGestureEvent();
+}
+
+}  // namespace
+
 WindowTargeter::WindowTargeter() {}
 WindowTargeter::~WindowTargeter() {}
 
@@ -25,16 +34,25 @@ ui::EventTarget* WindowTargeter::FindTargetForEvent(ui::EventTarget* root,
   Window* target = event->IsKeyEvent() ?
       FindTargetForKeyEvent(window, *static_cast<ui::KeyEvent*>(event)) :
       static_cast<Window*>(EventTargeter::FindTargetForEvent(root, event));
-  if (target && !window->parent()) {
-    // |window| is the root window.
-    if (!window->Contains(target)) {
-      // |target| is not a descendent of |window|. So do not allow dispatching
-      // from here. Instead, dispatch the event through the
-      // WindowEventDispatcher that owns |target|.
-      ui::EventDispatchDetails details ALLOW_UNUSED =
-          target->GetHost()->event_processor()->OnEventFromSource(event);
-      target = NULL;
+  if (target && !window->parent() && !window->Contains(target)) {
+    // |window| is the root window, but |target| is not a descendent of
+    // |window|. So do not allow dispatching from here. Instead, dispatch the
+    // event through the WindowEventDispatcher that owns |target|.
+    aura::Window* new_root = target->GetRootWindow();
+    if (IsLocatedEvent(*event)) {
+      // The event has been transformed to be in |target|'s coordinate system.
+      // But dispatching the event through the EventProcessor requires the event
+      // to be in the host's coordinate system. So, convert the event to be in
+      // the root's coordinate space, and then to the host's coordinate space by
+      // applying the host's transform.
+      ui::LocatedEvent* located_event = static_cast<ui::LocatedEvent*>(event);
+      located_event->ConvertLocationToTarget(target, new_root);
+      located_event->UpdateForRootTransform(
+          new_root->GetHost()->GetRootTransform());
     }
+    ui::EventDispatchDetails details ALLOW_UNUSED =
+        new_root->GetHost()->event_processor()->OnEventFromSource(event);
+    target = NULL;
   }
   return target;
 }
