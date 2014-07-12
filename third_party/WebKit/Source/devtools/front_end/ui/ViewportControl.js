e93c24615c346fe4e3913cf188b43579731422d1
lushnikov@chromium.org <lushnikov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-12 19:33:26 +0000

    DevTools: ViewportControl.scrollIntoView synchronously updates viewport
    This patch makes ViewportControl.scrollIntoView to synchronously update
    its viewport.
    
    As a result, it becomes legit to fetch rendered element
    via ViewportControl.renderedElementAt after bringing it into viewport.
    (that's what FilteredItemSelectionDialog does in its _updateSelection
    method)
    
    BUG=393432
    R=aandrey
    
    Review URL: https://codereview.chromium.org/389563003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178001 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ui/ViewportControl.js
+++ third_party/WebKit/Source/devtools/front_end/ui/ViewportControl.js
@@ -571,6 +571,7 @@ WebInspector.ViewportControl.prototype = {
     {
         this._rebuildCumulativeHeightsIfNeeded();
         this.element.scrollTop = index > 0 ? this._cumulativeHeights[index - 1] : 0;
+        this.refresh();
     },
 
     /**
@@ -580,5 +581,6 @@ WebInspector.ViewportControl.prototype = {
     {
         this._rebuildCumulativeHeightsIfNeeded();
         this.element.scrollTop = this._cumulativeHeights[index] - this.element.clientHeight;
+        this.refresh();
     }
 }
