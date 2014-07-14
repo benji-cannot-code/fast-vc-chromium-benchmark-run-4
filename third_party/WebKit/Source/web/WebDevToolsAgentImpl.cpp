549ccaf3faffab8e043bff5bd6a340065856582d
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-14 07:45:54 +0000

    DevTools: Make WebDevToolsAgent load processId from its client.
    Review URL: https://codereview.chromium.org/381403002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178030 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebDevToolsAgentImpl.cpp
+++ third_party/WebKit/Source/web/WebDevToolsAgentImpl.cpp
@@ -217,6 +217,10 @@ WebDevToolsAgentImpl::WebDevToolsAgentImpl(
     , m_pageScaleLimitsOverriden(false)
     , m_touchEventEmulationEnabled(false)
 {
+    long processId = client->processId();
+    ASSERT(processId > 0);
+    inspectorController()->setProcessId(processId);
+
     ASSERT(m_debuggerId > 0);
     ClientMessageLoopAdapter::ensureClientMessageLoopCreated(m_client);
 }
@@ -677,7 +681,6 @@ void WebDevToolsAgentImpl::updateInspectorStateCookie(const String& state)
 
 void WebDevToolsAgentImpl::setProcessId(long processId)
 {
-    inspectorController()->setProcessId(processId);
 }
 
 void WebDevToolsAgentImpl::setLayerTreeId(int layerTreeId)
