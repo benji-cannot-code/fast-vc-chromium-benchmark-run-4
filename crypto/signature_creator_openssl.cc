9593343c985da8b9d05f69fd84259b38d81c1186
davidben@chromium.org <davidben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-14 21:01:52 +0000

    Fix memory leaks when calling EVP_PKEY_get1_RSA.
    EVP_PKEY_get1_RSA passes the caller a reference that needs to be released
    afterwards.
    
    BUG=393659
    
    Review URL: https://codereview.chromium.org/392653005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283020 0039d316-1c4b-4281-b951-d872f2087c98

--- crypto/signature_creator_openssl.cc
+++ crypto/signature_creator_openssl.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util.h"
 #include "crypto/openssl_util.h"
 #include "crypto/rsa_private_key.h"
+#include "crypto/scoped_openssl_types.h"
 
 namespace crypto {
 
@@ -31,14 +32,14 @@ bool SignatureCreator::Sign(RSAPrivateKey* key,
                             const uint8* data,
                             int data_len,
                             std::vector<uint8>* signature) {
-  RSA* rsa_key = EVP_PKEY_get1_RSA(key->key());
+  ScopedRSA rsa_key(EVP_PKEY_get1_RSA(key->key()));
   if (!rsa_key)
     return false;
-  signature->resize(RSA_size(rsa_key));
+  signature->resize(RSA_size(rsa_key.get()));
 
   unsigned int len = 0;
   bool success = RSA_sign(NID_sha1, data, data_len, vector_as_array(signature),
-                          &len, rsa_key);
+                          &len, rsa_key.get());
   if (!success) {
     signature->clear();
     return false;
