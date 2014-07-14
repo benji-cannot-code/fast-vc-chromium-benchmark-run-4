740111f6a1d753ec7dbea7a9747c669481e2a6af
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-14 08:52:23 +0000

    Refactoring: Make ~CSSStyleSheetResource empty.
    Introduce Resource::dispose, which is called before |delete this|, and
    move the content of ~CSSStyleSheetResource to CSSStyleSheetResource::dispose.
    
    This makes Resource class Oilpan-friendly.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/389553002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178036 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/fetch/Resource.cpp
+++ third_party/WebKit/Source/core/fetch/Resource.cpp
@@ -151,6 +151,10 @@ Resource::~Resource()
 #endif
 }
 
+void Resource::dispose()
+{
+}
+
 void Resource::failBeforeStarting()
 {
     WTF_LOG(ResourceLoading, "Cannot start loading '%s'", url().string().latin1().data());
@@ -552,6 +556,7 @@ bool Resource::deleteIfPossible()
 {
     if (canDelete() && !memoryCache()->contains(this)) {
         InspectorInstrumentation::willDestroyResource(this);
+        dispose();
         delete this;
         return true;
     }
