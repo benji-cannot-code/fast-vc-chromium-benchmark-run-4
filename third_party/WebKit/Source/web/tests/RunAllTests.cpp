c3fcb72f0c762331d84398efd4a278973cdf25b5
tfarina@chromium.org <tfarina@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-14 06:01:56 +0000

    Rename webkit.dll to blink_web.dll.
    So far this is the new name of this target.
    
    BUG=None
    TEST=None
    R=abarth@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/384283002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178027 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/tests/RunAllTests.cpp
+++ third_party/WebKit/Source/web/tests/RunAllTests.cpp
@@ -49,7 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // TestSuite must be created before SetUpTestEnvironment so it performs
 // initializations needed by WebKit support. This is slightly complicated by the
 // fact that chromium multi-dll build requires that the TestSuite object be created
-// and run inside webkit.dll.
+// and run inside blink_web.dll.
 int main(int argc, char** argv)
 {
 #if defined(BLINK_DLL_UNITTEST)
