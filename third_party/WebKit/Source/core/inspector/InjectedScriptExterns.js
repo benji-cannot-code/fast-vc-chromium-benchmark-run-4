afa4eba93d165b27cf304f7a409c0cdc6977dae2
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-14 16:29:18 +0000

    Revert of DevTools: fixed compilation problem with InjectedScriptSource.js (https://chromiumcodereview.appspot.com/390853002/)
    Reason for revert:
    The original patch that was fixed by this one was reverted.
    
    Original issue's description:
    > Added declarations to InjectedScriptExterns:
    > JavaScriptCallFrame.prototype.evaluateWithExceptionDetails = function(expression) { }
    > InjectedScriptHostClass.prototype.eval = function(expression) 
    > InjectedScriptHostClass.prototype.evaluateWithExceptionDetails = function(expression) { }
    > These methods was added in https://codereview.chromium.org/289423002/
    > 
    > NOTRY=true
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=178059
    
    TBR=pfeldman@chromium.org,kozyatinskiy@google.com
    NOTREECHECKS=true
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/388253005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178074 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InjectedScriptExterns.js
+++ third_party/WebKit/Source/core/inspector/InjectedScriptExterns.js
@@ -88,11 +88,7 @@ InjectedScriptHostClass.prototype.getEventListeners = function(object) { }
 /**
  * @param {string} expression
  */
-InjectedScriptHostClass.prototype.eval = function(expression) { }
-/**
- * @param {string} expression
- */
-InjectedScriptHostClass.prototype.evaluateWithExceptionDetails = function(expression) { }
+InjectedScriptHostClass.prototype.evaluate = function(expression) { }
 /**
  * @param {*} fn
  */
@@ -148,10 +144,7 @@ function JavaScriptCallFrame()
 JavaScriptCallFrame.prototype.scopeType = function(index) { }
 
 JavaScriptCallFrame.prototype.restart = function() { }
-/**
- * @param {string} expression
- */
-JavaScriptCallFrame.prototype.evaluateWithExceptionDetails = function(expression) { }
+
 /**
  * @param {number} scopeNumber
  * @param {string} variableName
