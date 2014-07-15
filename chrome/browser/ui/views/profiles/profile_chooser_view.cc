fa127e3ff6d30b66b7014fb85425916503350a14
noms@chromium.org <noms@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-15 10:25:59 +0000

    [Linux] Set the new avatar bubble's background correctly
    BUG=378982
    
    Review URL: https://codereview.chromium.org/386903004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283176 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/profiles/profile_chooser_view.cc
+++ chrome/browser/ui/views/profiles/profile_chooser_view.cc
@@ -485,7 +485,9 @@ ProfileChooserView::ProfileChooserView(views::View* anchor_view,
       gaia_service_type_(service_type) {
   // Reset the default margins inherited from the BubbleDelegateView.
   set_margins(gfx::Insets());
-
+  set_background(views::Background::CreateSolidBackground(
+      GetNativeTheme()->GetSystemColor(
+          ui::NativeTheme::kColorId_DialogBackground)));
   ResetView();
 
   avatar_menu_.reset(new AvatarMenu(
@@ -614,10 +616,6 @@ void ProfileChooserView::ShowView(profiles::BubbleViewMode view_to_display,
       layout = CreateSingleColumnLayout(this, kFixedMenuWidth);
       sub_view = CreateProfileChooserView(avatar_menu, last_tutorial_mode);
   }
-  sub_view->set_background(views::Background::CreateSolidBackground(
-      GetNativeTheme()->GetSystemColor(
-          ui::NativeTheme::kColorId_DialogBackground)));
-
   layout->StartRow(1, 0);
   layout->AddView(sub_view);
   Layout();
