20b8eeda420f484d05e87dafbcbe29df36ed991c
jdduke@chromium.org <jdduke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-15 18:09:37 +0000

    Clear velocity tracker upon delayed ACTION_UP
    The velocity tracker currently ignores ACTION_{UP,POINTER_UP} events.  While
    these events don't convey additional motion information, they do convey timing
    and lack of motion. This is particularly relevent if the final ACTION_UP event
    is delayed, e.g., the user's finger moves and then is perfectly stationary for
    a time before being lifted. In that case, the tracker would report the non-zero
    velocity of the movement, when it really ought to have been cleared because of
    the delayed release.
    
    Add an additional check for such delayed pointer release events, using a value
    larger than the 40ms |kAssumePointerMoveStoppedTimeMs| to allow for slightly
    delayed ACTION_UP detection. Also remove the smooth scroll synthetic gesture
    workaround that was previously required to prevent fling detection.
    
    BUG=392652
    
    Review URL: https://codereview.chromium.org/381233002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283218 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/input/synthetic_gesture_controller_unittest.cc
+++ content/browser/renderer_host/input/synthetic_gesture_controller_unittest.cc
@@ -571,9 +571,8 @@ TEST_F(SyntheticGestureControllerTest, SingleScrollGestureTouchVertical) {
       static_cast<MockScrollGestureTarget*>(target_);
   EXPECT_EQ(1, num_success_);
   EXPECT_EQ(0, num_failure_);
-  // TODO(dominikg): Remove adjustment when crbug.com/332418 is fixed.
   EXPECT_EQ(AddTouchSlopToVector(params.distances[0], target_),
-            scroll_target->start_to_end_distance() - gfx::Vector2dF(0, 0.001f));
+            scroll_target->start_to_end_distance());
 }
 
 TEST_F(SyntheticGestureControllerTest, SingleScrollGestureTouchHorizontal) {
@@ -593,15 +592,8 @@ TEST_F(SyntheticGestureControllerTest, SingleScrollGestureTouchHorizontal) {
       static_cast<MockScrollGestureTarget*>(target_);
   EXPECT_EQ(1, num_success_);
   EXPECT_EQ(0, num_failure_);
-  // TODO(dominikg): Use vector comparison when crbug.com/332418 is fixed.
-  //EXPECT_EQ(AddTouchSlopToVector(params.distances[0], target_),
-  //          scroll_target->start_to_end_distance());
-  EXPECT_EQ(AddTouchSlopToVector(params.distances[0], target_).x(),
-            scroll_target->start_to_end_distance().x());
-  EXPECT_LT(AddTouchSlopToVector(params.distances[0], target_).y(),
-            scroll_target->start_to_end_distance().y());
-  EXPECT_GE(AddTouchSlopToVector(params.distances[0], target_).y(),
-            scroll_target->start_to_end_distance().y() - 0.001f);
+  EXPECT_EQ(AddTouchSlopToVector(params.distances[0], target_),
+            scroll_target->start_to_end_distance());
 }
 
 void CheckIsWithinRangeSingle(float scroll_distance,
@@ -891,14 +883,13 @@ TEST_F(SyntheticGestureControllerTest, MultiScrollGestureTouchVertical) {
       static_cast<MockScrollGestureTarget*>(target_);
   EXPECT_EQ(1, num_success_);
   EXPECT_EQ(0, num_failure_);
-  // TODO(dominikg): Remove adjustment when crbug.com/332418 is fixed.
   EXPECT_FLOAT_EQ(
       params.distances[0].Length() + params.distances[1].Length() +
           target_->GetTouchSlopInDips(),
-      scroll_target->total_abs_scroll_distance_length() - 0.001f);
+      scroll_target->total_abs_scroll_distance_length());
   EXPECT_EQ(AddTouchSlopToVector(params.distances[0] + params.distances[1],
                                  target_),
-            scroll_target->start_to_end_distance() - gfx::Vector2dF(0, 0.001f));
+            scroll_target->start_to_end_distance());
 }
 
 TEST_F(SyntheticGestureControllerTest, PinchGestureTouchZoomIn) {
