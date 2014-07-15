b043f406b536768daece2737e4248a95dd8d0082
jiayl@chromium.org <jiayl@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-15 17:12:10 +0000

    Add iceTransports to RTCConfiguration and add RTCOfferOptions for CreateOffer.
    BUG=https://code.google.com/p/webrtc/issues/detail?id=3282
    
    Review URL: https://codereview.chromium.org/361093002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178184 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebRTCConfiguration.h
+++ third_party/WebKit/public/platform/WebRTCConfiguration.h
@@ -75,6 +75,12 @@ private:
     WebPrivatePtr<WebCore::RTCIceServer> m_private;
 };
 
+enum WebRTCIceTransports {
+    WebRTCIceTransportsNone,
+    WebRTCIceTransportsRelay,
+    WebRTCIceTransportsAll
+};
+
 class WebRTCConfiguration {
 public:
     WebRTCConfiguration() { }
@@ -94,6 +100,8 @@ public:
 
     BLINK_PLATFORM_EXPORT size_t numberOfServers() const;
     BLINK_PLATFORM_EXPORT WebRTCICEServer server(size_t index) const;
+
+    BLINK_PLATFORM_EXPORT WebRTCIceTransports iceTransports() const;
 
 #if INSIDE_BLINK
     BLINK_PLATFORM_EXPORT WebRTCConfiguration(const WTF::PassRefPtr<WebCore::RTCConfiguration>&);
