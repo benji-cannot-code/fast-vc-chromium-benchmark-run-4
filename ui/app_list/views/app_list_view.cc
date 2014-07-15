c1ed869e198057b6b25bd7bc8c412abdf2db20fd
mgiuca@chromium.org <mgiuca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-15 21:50:25 +0000

    Experimental app list banner now appears on top of launcher pages.
    The banner is now its own view (instead of being painted directly onto
    the background), and the view paints to a layer, so that it appears on
    top of other layers such as the WebContents of the custom launcher
    pages.
    
    BUG=393858
    
    Review URL: https://codereview.chromium.org/390273002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283256 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/app_list/views/app_list_view.cc
+++ ui/app_list/views/app_list_view.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/strings/string_util.h"
 #include "base/win/windows_version.h"
+#include "grit/ui_resources.h"
 #include "ui/app_list/app_list_constants.h"
 #include "ui/app_list/app_list_model.h"
 #include "ui/app_list/app_list_switches.h"
@@ -34,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/path.h"
 #include "ui/gfx/skia_util.h"
 #include "ui/views/bubble/bubble_frame_view.h"
+#include "ui/views/controls/image_view.h"
 #include "ui/views/controls/textfield/textfield.h"
 #include "ui/views/layout/fill_layout.h"
 #include "ui/views/widget/widget.h"
@@ -149,6 +151,7 @@ AppListView::AppListView(AppListViewDelegate* delegate)
     : delegate_(delegate),
       app_list_main_view_(NULL),
       speech_view_(NULL),
+      experimental_banner_view_(NULL),
       overlay_view_(NULL),
       animation_observer_(new HideViewAnimationObserver()) {
   CHECK(delegate);
@@ -311,6 +314,18 @@ void AppListView::InitAsBubbleInternal(gfx::NativeView parent,
     AddChildView(speech_view_);
   }
 
+  if (app_list::switches::IsExperimentalAppListEnabled()) {
+    // Draw a banner in the corner of the experimental app list.
+    experimental_banner_view_ = new views::ImageView;
+    const gfx::ImageSkia& experimental_icon =
+        *ui::ResourceBundle::GetSharedInstance().GetImageSkiaNamed(
+            IDR_APP_LIST_EXPERIMENTAL_ICON);
+    experimental_banner_view_->SetImage(experimental_icon);
+    experimental_banner_view_->SetPaintToLayer(true);
+    experimental_banner_view_->SetFillsBoundsOpaquely(false);
+    AddChildView(experimental_banner_view_);
+  }
+
   OnProfilesChanged();
   set_color(kContentsBackgroundColor);
   set_margins(gfx::Insets());
@@ -457,6 +472,15 @@ void AppListView::Layout() {
                                       preferred_height));
     speech_bounds.Inset(-speech_view_->GetInsets());
     speech_view_->SetBoundsRect(speech_bounds);
+  }
+
+  if (experimental_banner_view_) {
+    // Position the experimental banner in the lower right corner.
+    gfx::Rect image_bounds = experimental_banner_view_->GetImageBounds();
+    image_bounds.set_origin(
+        gfx::Point(contents_bounds.width() - image_bounds.width(),
+                   contents_bounds.height() - image_bounds.height()));
+    experimental_banner_view_->SetBoundsRect(image_bounds);
   }
 }
 
