7e923f039c7896eb5a9ca2149415f7a1fc1c699d
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-15 02:59:51 +0000

    WebAudio: Split AudioNode::finishDeref into two parts.
    Split finishDeref into
     - Disconnection processing part, and
     - Normal lifetime management part.
    
    This CL adds AudioContext::addDeferredBreakConnection() to defer them
    independently.
    
    At this momenet deref() and breakConnection() have very similar code to acquire
    lock. We're going to remove deref() by Oilpan.
    
    BUG=392788
    TEST=none; no behavior changes.
    
    Review URL: https://codereview.chromium.org/386403002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178123 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/webaudio/AudioNode.h
+++ third_party/WebKit/Source/modules/webaudio/AudioNode.h
@@ -109,6 +109,7 @@ public:
 
     // Can be called from main thread or context's audio thread.  It must be called while the context's graph lock is held.
     void finishDeref();
+    void breakConnectionWithLock();
 
     // The AudioNodeInput(s) (if any) will already have their input data available when process() is called.
     // Subclasses will take this input data and put the results in the AudioBus(s) of its AudioNodeOutput(s) (if any).
@@ -230,7 +231,6 @@ private:
     // Ref-counting
     volatile int m_normalRefCount;
     volatile int m_connectionRefCount;
-    bool m_wasDisconnected;
 
     bool m_isMarkedForDeletion;
     bool m_isDisabled;
