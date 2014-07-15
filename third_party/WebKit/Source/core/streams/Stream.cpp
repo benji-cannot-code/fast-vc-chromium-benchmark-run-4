84fa33537d675180941d3ef5c6aa66ed77899abc
yhirano@chromium.org <yhirano@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-15 20:55:41 +0000

    Move Source/core/fileapi/Stream to Source/core/streams/Stream.
    Create Source/core/streams and move Stream class to the directory.
    
    BUG=240603
    R=tyoshino
    
    Review URL: https://codereview.chromium.org/391953002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178203 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/streams/Stream.cpp
+++ third_party/WebKit/Source/core/streams/Stream.cpp
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
-#include "core/fileapi/Stream.h"
+#include "core/streams/Stream.h"
 
 #include "platform/blob/BlobData.h"
 #include "platform/blob/BlobRegistry.h"
