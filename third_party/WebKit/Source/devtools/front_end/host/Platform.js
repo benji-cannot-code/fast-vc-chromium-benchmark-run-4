ed4f465ff54eea15b7d9fc2688c2a32ac3fb4055
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-15 06:45:11 +0000

    DevTools: get rid of the global capabilities set.
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/388963004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178145 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/host/Platform.js
+++ third_party/WebKit/Source/devtools/front_end/host/Platform.js
@@ -160,3 +160,11 @@ WebInspector.monospaceFontFamily = function()
     }
     return WebInspector._monospaceFontFamily;
 }
+
+/**
+ * @return {boolean}
+ */
+WebInspector.isWorkerFrontend = function()
+{
+    return !!WebInspector.queryParam("dedicatedWorkerId") || !!WebInspector.queryParam("isSharedWorker");
+}
--- third_party/WebKit/Source/devtools/front_end/sdk/OverridesSupport.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/OverridesSupport.js
@@ -812,7 +812,7 @@ WebInspector.OverridesSupport.prototype = {
      */
     hasTouchInputs: function()
     {
-        return !!this._target && this._target.hasTouchInputs;
+        return !!this._target && this._target.hasCapability(WebInspector.Target.Capabilities.hasTouchInputs);
     },
 
     /**
