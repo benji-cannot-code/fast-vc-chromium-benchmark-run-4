1d1c55f3ac629435f73263eba89ed29e0c6efbaa
mostynb@opera.com <mostynb@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-15 22:16:00 +0000

    disable inline ARM saturated arithmetic assembly for debug builds
    When building third_party/WebKit/Source/wtf/asm/SaturatedArithmeticARM.h
    in debug builds, we get this error:
    cc1plus: warnings being treated as errors
     SaturatedArithmeticARM.h: In function 'int saturatedSet(int, int)':
     SaturatedArithmeticARM.h:70:43: error: asm operand 2 probably doesn't match constraints
     SaturatedArithmeticARM.h:70:43: error: asm operand 3 probably doesn't match constraints
    
    By switching -O0 to -O1 or -O2 in the compile command, the compile
    succeeds. So let's disable the inline ARM assembly unless the build
    is optimized- gcc defines _OPTIMIZE_=1 in this case.
    I think this is a reasonable workaround until we can find a "proper"
    solution.
    
    BUG=325746
    
    Review URL: https://codereview.chromium.org/396603002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178209 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/SaturatedArithmetic.h
+++ third_party/WebKit/Source/wtf/SaturatedArithmetic.h
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <limits>
 #include <stdint.h>
 
-#if CPU(ARM) && COMPILER(GCC)
+#if CPU(ARM) && COMPILER(GCC) && __OPTIMIZE__
 
 // If we're building ARM on GCC we replace the C++ versions with some
 // native ARM assembly for speed.
