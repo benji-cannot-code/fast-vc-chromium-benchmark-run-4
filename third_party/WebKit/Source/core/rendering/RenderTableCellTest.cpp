7d774449a8f5c351c639a46282f5e2332822f2b7
jchaffraix@chromium.org <jchaffraix@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-15 20:47:32 +0000

    Clamp colspan to 8190
    The RenderTableCell logic crashes if we try to set a
    column index that would overflow the bitfield. The
    assumption was that it would be rarely hit in the wild:
    having such a big column index means a giant table, which
    would be slow. Unfortunately, it seems like people (not to
    mention fuzzers) are testing big values for colspan that
    triggers the previous crash frequently. As a work-around,
    this change clamps colspan - like rowspan - to 8190, which
    should reduce greatly the amount of crashes.
    
    BUG=386260
    
    Review URL: https://codereview.chromium.org/393673002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178202 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderTableCellTest.cpp
+++ third_party/WebKit/Source/core/rendering/RenderTableCellTest.cpp
@@ -26,24 +26,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 
+#include "core/rendering/RenderingTestHelper.h"
 #include "core/rendering/RenderTableCell.h"
-
-#include "core/testing/DummyPageHolder.h"
-
-#include <gtest/gtest.h>
-
-using namespace blink;
 
 namespace WebCore {
 
 namespace {
 
-class RenderTableCellDeathTest : public testing::Test {
+class RenderTableCellDeathTest : public RenderingTest {
 protected:
     virtual void SetUp()
     {
-        m_pageHolder = DummyPageHolder::create(IntSize(800, 600));
-        m_cell = RenderTableCell::createAnonymous(&m_pageHolder->document());
+        RenderingTest::SetUp();
+        m_cell = RenderTableCell::createAnonymous(&document());
     }
 
     virtual void TearDown()
@@ -51,7 +46,6 @@ protected:
         m_cell->destroy();
     }
 
-    OwnPtr<DummyPageHolder> m_pageHolder;
     RenderTableCell* m_cell;
 };
 
@@ -83,6 +77,40 @@ TEST_F(RenderTableCellDeathTest, CrashIfSettingUnsetColumnIndex)
 }
 
 #endif
+
+class RenderTableCellTest : public RenderingTest { };
+
+TEST_F(RenderTableCellTest, ResetColspanIfTooBig)
+{
+    setBodyInnerHTML("<table><td colspan='14000'></td></table>");
+
+    RenderTableCell* cell = toRenderTableCell(document().body()->firstChild()->firstChild()->firstChild()->firstChild()->renderer());
+    ASSERT_EQ(cell->colSpan(), 8190U);
+}
+
+TEST_F(RenderTableCellTest, DoNotResetColspanJustBelowBoundary)
+{
+    setBodyInnerHTML("<table><td colspan='8190'></td></table>");
+
+    RenderTableCell* cell = toRenderTableCell(document().body()->firstChild()->firstChild()->firstChild()->firstChild()->renderer());
+    ASSERT_EQ(cell->colSpan(), 8190U);
+}
+
+TEST_F(RenderTableCellTest, ResetRowspanIfTooBig)
+{
+    setBodyInnerHTML("<table><td rowspan='14000'></td></table>");
+
+    RenderTableCell* cell = toRenderTableCell(document().body()->firstChild()->firstChild()->firstChild()->firstChild()->renderer());
+    ASSERT_EQ(cell->rowSpan(), 8190U);
+}
+
+TEST_F(RenderTableCellTest, DoNotResetRowspanJustBelowBoundary)
+{
+    setBodyInnerHTML("<table><td rowspan='8190'></td></table>");
+
+    RenderTableCell* cell = toRenderTableCell(document().body()->firstChild()->firstChild()->firstChild()->firstChild()->renderer());
+    ASSERT_EQ(cell->rowSpan(), 8190U);
+}
 
 }
 
