d15ff9e5daa00d07126122b2a84a102925bb445d
tfarina@chromium.org <tfarina@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-15 02:51:37 +0000

    Remove TestViewType enum from WebUnitTestSupport.h
    This was a FIXME for danakj.
    
    BUG=None
    TEST=None
    R=jamesr@chromium.org
    
    Review URL: https://codereview.chromium.org/387263002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178122 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebUnitTestSupport.h
+++ third_party/WebKit/public/platform/WebUnitTestSupport.h
@@ -61,11 +61,6 @@ public:
     virtual WebLayerTreeView* createLayerTreeViewForTesting() { return 0; }
 
     virtual WebData readFromFile(const WebString& path) { return WebData(); }
-
-    // FIXME(danakj): Remove this.
-    enum TestViewType {
-        TestViewTypeUnitTest,
-    };
 };
 
 }
