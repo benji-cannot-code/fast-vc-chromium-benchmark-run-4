0aea68219005fea247f6458a71530eb249b56abe
ager@chromium.org <ager@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-16 14:12:06 +0000

    Oilpan: Remove support for tracing off-heap LinkedHashSets.
    R=haraken@chromium.org, oilpan-reviews@chromium.org, wibling@chromium.org
    BUG=382557
    
    Review URL: https://codereview.chromium.org/395173002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178280 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/MediaQueryMatcher.cpp
+++ third_party/WebKit/Source/core/css/MediaQueryMatcher.cpp
@@ -123,8 +123,10 @@ void MediaQueryMatcher::mediaFeaturesChanged()
 
 void MediaQueryMatcher::trace(Visitor* visitor)
 {
+#if ENABLE(OILPAN)
     visitor->trace(m_document);
     visitor->trace(m_mediaLists);
+#endif
 }
 
 }
