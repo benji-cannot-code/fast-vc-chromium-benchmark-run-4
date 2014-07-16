0fc98372a013d5e024077e0b6e5ca10ce6b2caed
kmadhusu@chromium.org <kmadhusu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-16 05:55:29 +0000

    Enable prefetch-search-results on Desktop Chrome by default.
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/360373006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283367 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/search/search.cc
+++ chrome/browser/search/search.cc
@@ -66,15 +66,18 @@ const uint64 kEmbeddedPageVersionDefault = 2;
 #endif
 
 const char kHideVerbatimFlagName[] = "hide_verbatim";
-const char kPrefetchSearchResultsFlagName[] = "prefetch_results";
 const char kPrefetchSearchResultsOnSRP[] = "prefetch_results_srp";
 const char kAllowPrefetchNonDefaultMatch[] = "allow_prefetch_non_default_match";
 const char kPrerenderInstantUrlOnOmniboxFocus[] =
     "prerender_instant_url_on_omnibox_focus";
 
+#if defined(OS_ANDROID)
+const char kPrefetchSearchResultsFlagName[] = "prefetch_results";
+
 // Controls whether to reuse prerendered Instant Search base page to commit any
 // search query.
 const char kReuseInstantSearchBasePage[] = "reuse_instant_search_base_page";
+#endif
 
 // Controls whether to use the alternate Instant search base URL. This allows
 // experimentation of Instant search.
@@ -597,6 +600,7 @@ bool ShouldPrefetchSearchResults() {
   if (!IsInstantExtendedAPIEnabled())
     return false;
 
+#if defined(OS_ANDROID)
   if (CommandLine::ForCurrentProcess()->HasSwitch(
           switches::kPrefetchSearchResults)) {
     return true;
@@ -605,6 +609,9 @@ bool ShouldPrefetchSearchResults() {
   FieldTrialFlags flags;
   return GetFieldTrialInfo(&flags) && GetBoolValueForFlagWithDefault(
       kPrefetchSearchResultsFlagName, false, flags);
+#else
+  return true;
+#endif
 }
 
 bool ShouldAllowPrefetchNonDefaultMatch() {
@@ -629,9 +636,13 @@ bool ShouldReuseInstantSearchBasePage() {
   if (!ShouldPrefetchSearchResults())
     return false;
 
+#if defined(OS_ANDROID)
   FieldTrialFlags flags;
   return GetFieldTrialInfo(&flags) && GetBoolValueForFlagWithDefault(
       kReuseInstantSearchBasePage, false, flags);
+#else
+  return true;
+#endif
 }
 
 GURL GetLocalInstantURL(Profile* profile) {
