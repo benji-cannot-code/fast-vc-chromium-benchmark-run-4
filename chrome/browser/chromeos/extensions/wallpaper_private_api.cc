858ba0864ae5f01847d65af45ee78f7c8e62c05e
beaufort.francois@gmail.com <beaufort.francois@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-16 21:53:07 +0000

    [Wallpaper Picker] s/commondatastorage.googleapis.com/storage.googleapis.com/g
    BUG=329247
    
    Review URL: https://codereview.chromium.org/393803002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283530 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/extensions/wallpaper_private_api.cc
+++ chrome/browser/chromeos/extensions/wallpaper_private_api.cc
@@ -54,8 +54,8 @@ namespace get_offline_wallpaper_list =
 namespace {
 
 #if defined(GOOGLE_CHROME_BUILD)
-const char kWallpaperManifestBaseURL[] = "https://commondatastorage.googleapis."
-    "com/chromeos-wallpaper-public/manifest_";
+const char kWallpaperManifestBaseURL[] =
+    "https://storage.googleapis.com/chromeos-wallpaper-public/manifest_";
 #endif
 
 bool IsOEMDefaultWallpaper() {
