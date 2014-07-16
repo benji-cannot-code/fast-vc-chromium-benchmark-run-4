34239e8de8c50cb0076870db8694414dbbfb04d1
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-16 04:43:25 +0000

    Simplify WebNavigationApi by using RenderFrameHost internally.
    WebNavigationApi formerly passed around a lot of redundant state, but
    now it just uses RenderFrameHost. Code that needs to know the routing
    ID, parent, etc can just derive it from the RenderFrameHost.
    
    Since WebNavigationApi is storing raw pointers to RenderFrameHost, it
    also observers RFH deletions and removes them as needed. This fixes
    some use-after-frees when other observers are incorrectly causing the
    RenderFrameHost to be destroyed.
    
    BUG=391058
    
    Review URL: https://codereview.chromium.org/384993004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283336 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/web_request/web_request_api.cc
+++ chrome/browser/extensions/api/web_request/web_request_api.cc
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/declarative_webrequest/request_stage.h"
 #include "chrome/browser/extensions/api/declarative_webrequest/webrequest_constants.h"
 #include "chrome/browser/extensions/api/declarative_webrequest/webrequest_rules_registry.h"
-#include "chrome/browser/extensions/api/web_navigation/web_navigation_api_helpers.h"
 #include "chrome/browser/extensions/api/web_request/upload_data_presenter.h"
 #include "chrome/browser/extensions/api/web_request/web_request_api_constants.h"
 #include "chrome/browser/extensions/api/web_request/web_request_api_helpers.h"
@@ -43,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/url_constants.h"
 #include "content/public/browser/browser_message_filter.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/resource_request_info.h"
 #include "content/public/browser/user_metrics.h"
@@ -85,7 +85,6 @@ using extensions::ExtensionWarningSet;
 using extensions::InfoMap;
 using extensions::Feature;
 using extensions::RulesRegistryService;
-using extensions::web_navigation_api_helpers::GetFrameId;
 
 namespace helpers = extension_web_request_api_helpers;
 namespace keys = extension_web_request_api_constants;
@@ -138,6 +137,11 @@ const char* GetRequestStageAsString(
   }
   NOTREACHED();
   return "Not reached";
+}
+
+// TODO(dcheng): Fix plumbing. Frame ID is not an int64--it's just an int.
+int GetFrameId(bool is_main_frame, int64 frame_id) {
+  return is_main_frame ? 0 : static_cast<int>(frame_id);
 }
 
 bool IsWebRequestEvent(const std::string& event_name) {
