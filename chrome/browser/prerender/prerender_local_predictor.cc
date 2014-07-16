c24676a488c95dd5c007aca54db5e3d296441f1b
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-16 04:07:51 +0000

    Fixes for re-enabling more MSVC level 4 warnings: chrome/browser/ edition
    This contains fixes for the following sorts of issues:
    * Possibly-uninitialized local variable
    * Signedness mismatch
    * Assignment inside conditional
    
    This also contains a small number of other cleanups/simplifications to nearby
     code.
    
    BUG=81439
    TEST=none
    
    Review URL: https://codereview.chromium.org/368133005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283325 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prerender/prerender_local_predictor.cc
+++ chrome/browser/prerender/prerender_local_predictor.cc
@@ -840,9 +840,11 @@ bool PrerenderLocalPredictor::ApplyParsedPrerenderServiceResponse(
         if (!list->GetDictionary(i, &d))
           return false;
         string url;
+        if (!d->GetString("url", &url) || !GURL(url).is_valid())
+          return false;
         double priority;
-        if (!d->GetString("url", &url) || !d->GetDouble("likelihood", &priority)
-            || !GURL(url).is_valid()) {
+        if (!d->GetDouble("likelihood", &priority) || priority < 0.0 ||
+            priority > 1.0) {
           return false;
         }
         int in_index_timed_out = 0;
@@ -852,8 +854,8 @@ bool PrerenderLocalPredictor::ApplyParsedPrerenderServiceResponse(
             !d->GetInteger("in_index", &in_index)) {
           return false;
         }
-        if (priority < 0.0 || priority > 1.0 || in_index < 0 || in_index > 1 ||
-            in_index_timed_out < 0 || in_index_timed_out > 1) {
+        if (in_index < 0 || in_index > 1 || in_index_timed_out < 0 ||
+            in_index_timed_out > 1) {
           return false;
         }
         if (in_index_timed_out == 1)
@@ -861,7 +863,7 @@ bool PrerenderLocalPredictor::ApplyParsedPrerenderServiceResponse(
         info->MaybeAddCandidateURLFromService(GURL(url),
                                               priority,
                                               in_index == 1,
-                                              (1 - in_index_timed_out) == 1);
+                                              !in_index_timed_out);
       }
       if (list->GetSize() > 0)
         RecordEvent(EVENT_PRERENDER_SERIVCE_RETURNED_HINTING_CANDIDATES);
