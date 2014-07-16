50a7ec67c609cb2a96576c7a0ef8013f0a94d76f
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-16 00:34:50 +0000

    Fixes for re-enabling more MSVC level 4 warnings: chrome/ edition
    This contains fixes for the following sorts of issues:
    * Signedness mismatch
    * Assignment inside conditional
    * Constant value was truncated
    * Possibly-uninitialized local variable
    
    BUG=81439
    TEST=none
    
    Review URL: https://codereview.chromium.org/377683002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283305 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/chromedriver/session_commands.cc
+++ chrome/test/chromedriver/session_commands.cc
@@ -478,7 +478,8 @@ Status ExecuteSetWindowPosition(
     Session* session,
     const base::DictionaryValue& params,
     scoped_ptr<base::Value>* value) {
-  double x, y;
+  double x = 0;
+  double y = 0;
   if (!params.GetDouble("x", &x) || !params.GetDouble("y", &y))
     return Status(kUnknownError, "missing or invalid 'x' or 'y'");
 
@@ -529,7 +530,8 @@ Status ExecuteSetWindowSize(
     Session* session,
     const base::DictionaryValue& params,
     scoped_ptr<base::Value>* value) {
-  double width, height;
+  double width = 0;
+  double height = 0;
   if (!params.GetDouble("width", &width) ||
       !params.GetDouble("height", &height))
     return Status(kUnknownError, "missing or invalid 'width' or 'height'");
