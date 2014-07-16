1909f2d9c69f0722826d8b8766be0185fa20f706
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-16 00:15:29 +0000

    Fix misspelling
    TBR=brettw@chromium.org
    BUG=none
    
    Review URL: https://codereview.chromium.org/390173002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283295 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_context_menu/render_view_context_menu.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu.cc
@@ -211,7 +211,7 @@ const struct UmaEnumCommandIdPair {
 };
 
 // Collapses large ranges of ids before looking for UMA enum.
-int CollapleCommandsForUMA(int id) {
+int CollapseCommandsForUMA(int id) {
   if (id >= IDC_CONTENT_CONTEXT_CUSTOM_FIRST &&
       id <= IDC_CONTENT_CONTEXT_CUSTOM_LAST) {
     return IDC_CONTENT_CONTEXT_CUSTOM_FIRST;
@@ -242,7 +242,7 @@ int CollapleCommandsForUMA(int id) {
 
 // Returns UMA enum value for command specified by |id| or -1 if not found.
 int FindUMAEnumValueForCommand(int id) {
-  id = CollapleCommandsForUMA(id);
+  id = CollapseCommandsForUMA(id);
   const size_t kMappingSize = arraysize(kUmaEnumToControlId);
   for (size_t i = 0; i < kMappingSize; ++i) {
     if (kUmaEnumToControlId[i].control_id == id) {
