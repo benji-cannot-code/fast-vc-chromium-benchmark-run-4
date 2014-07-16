0faf9bd34f0efe3b28b49e4b66261719cb4570b5
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-16 18:13:11 +0000

    Rename WebCore namespace to blink
    This CL uses a #define to rename the WebCore namespace to blink. The #define is
    scoped to only affect files inside the Blink repository. After this CL rolls
    into the main waterfall, I'll post CLs that update the source code to use
    "blink" instead of "WebCore". Once we've done all the renames, I'll revert this
    CL.
    
    R=eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/399513002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178289 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/config.h
+++ third_party/WebKit/Source/config.h
@@ -140,6 +140,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #endif
 
+#define WebCore blink
+
 #if COMPILER(MSVC)
 #define SKIP_STATIC_CONSTRUCTORS_ON_MSVC 1
 #else
