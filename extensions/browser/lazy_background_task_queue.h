d091203e3a34e95196234d6b2e410eef066299a3
limasdf@gmail.com <limasdf@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-16 21:15:18 +0000

    Remove extension unload notification from LazyBackgroundLazyBackgroundTaskQueue and use ExtensionRegistryObserver instead.
    BUG=354046
    
    Review URL: https://codereview.chromium.org/391563002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283507 0039d316-1c4b-4281-b951-d872f2087c98

--- extensions/browser/lazy_background_task_queue.h
+++ extensions/browser/lazy_background_task_queue.h
@@ -13,8 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/gtest_prod_util.h"
 #include "base/memory/linked_ptr.h"
+#include "base/scoped_observer.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
+#include "extensions/browser/extension_registry_observer.h"
 
 namespace content {
 class BrowserContext;
@@ -23,6 +25,7 @@ class BrowserContext;
 namespace extensions {
 class Extension;
 class ExtensionHost;
+class ExtensionRegistry;
 
 // This class maintains a queue of tasks that should execute when an
 // extension's lazy background page is loaded. It is also in charge of loading
@@ -30,7 +33,8 @@ class ExtensionHost;
 //
 // It is the consumer's responsibility to use this class when appropriate, i.e.
 // only with extensions that have not-yet-loaded lazy background pages.
-class LazyBackgroundTaskQueue : public content::NotificationObserver {
+class LazyBackgroundTaskQueue : public content::NotificationObserver,
+                                public ExtensionRegistryObserver {
  public:
   typedef base::Callback<void(ExtensionHost*)> PendingTask;
 
@@ -73,6 +77,12 @@ class LazyBackgroundTaskQueue : public content::NotificationObserver {
                        const content::NotificationSource& source,
                        const content::NotificationDetails& details) OVERRIDE;
 
+  // ExtensionRegistryObserver interface.
+  virtual void OnExtensionUnloaded(
+      content::BrowserContext* browser_context,
+      const Extension* extension,
+      UnloadedExtensionInfo::Reason reason) OVERRIDE;
+
   // Called when a lazy background page has finished loading, or has failed to
   // load (host is NULL in that case). All enqueued tasks are run in order.
   void ProcessPendingTasks(
@@ -83,6 +93,9 @@ class LazyBackgroundTaskQueue : public content::NotificationObserver {
   content::BrowserContext* browser_context_;
   content::NotificationRegistrar registrar_;
   PendingTasksMap pending_tasks_;
+
+  ScopedObserver<ExtensionRegistry, ExtensionRegistryObserver>
+      extension_registry_observer_;
 };
 
 }  // namespace extensions
