eb9dd54d503afa969f5c9a5d63d35704638c4ea9
avi.nitk@samsung.com <avi.nitk@samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-16 18:19:20 +0000

    Fix Handle above IME issue
    Added flag to check if IME state is updated
    This will prevent resetting of mFocusPreOSKViewportRect
    on onTouchEvent.
    
    So, if cancelRequestToScrollFocusedEditableNodeIntoView()
    is triggerred from onTouchEvent()<ACTION_MOVE> and it
    happens before onSizeChanged is triggerred, then the
    input field will not be scrolled to view.
    
    BUG=393948
    
    Review URL: https://codereview.chromium.org/392943002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283434 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -1172,9 +1172,11 @@ public class ContentViewCore
     public boolean onTouchEvent(MotionEvent event) {
         TraceEvent.begin("onTouchEvent");
         try {
-            cancelRequestToScrollFocusedEditableNodeIntoView();
-
             int eventAction = event.getActionMasked();
+
+            if (eventAction == MotionEvent.ACTION_DOWN) {
+                cancelRequestToScrollFocusedEditableNodeIntoView();
+            }
 
             if (isSPenSupported(mContext))
                 eventAction = convertSPenEventAction(eventAction);
