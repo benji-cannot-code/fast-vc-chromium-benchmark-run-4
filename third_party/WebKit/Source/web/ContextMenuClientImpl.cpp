8e47b36479c3a33c6aa990eb6d0648876e10b9ed
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-16 03:05:33 +0000

    showContextMenu should use the host, not the user agent shadow elements.
    This code does not expect to hit the inside of user agent shadow elements,
    so we need to call setToShadowHostIfInUserAgentShadowRoot().
    
    I'm not sure how to test this, it seems we have very little test coverage
    for context menus?
    
    BUG=393406
    
    Review URL: https://codereview.chromium.org/393023005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178234 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/ContextMenuClientImpl.cpp
+++ third_party/WebKit/Source/web/ContextMenuClientImpl.cpp
@@ -191,6 +191,9 @@ void ContextMenuClientImpl::showContextMenu(const WebCore::ContextMenu* defaultM
         return;
 
     HitTestResult r = m_webView->page()->contextMenuController().hitTestResult();
+
+    r.setToShadowHostIfInUserAgentShadowRoot();
+
     LocalFrame* selectedFrame = r.innerNodeFrame();
 
     WebContextMenuData data;
