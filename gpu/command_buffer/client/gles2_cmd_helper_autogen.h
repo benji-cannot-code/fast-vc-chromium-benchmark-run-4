f87dae4deced2c7bc370b1a77df2b6a8077c9c49
jungjik.lee@samsung.com <jungjik.lee@samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-16 20:46:09 +0000

    generate GetAttribLocation/GetUniformLocation by build_gles2_cmd_buffer.py
    change to generate GetAttribLocation/GetUniformLocation functions by build_gles2_cmd_buffer.py 
    and remove HandWritten code.
    
    Bug=None
    TEST=gpu_tests --gtest_filter="GLES2FormatTest.*"
    
    Review URL: https://codereview.chromium.org/333303002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283494 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/client/gles2_cmd_helper_autogen.h
+++ gpu/command_buffer/client/gles2_cmd_helper_autogen.h
@@ -558,6 +558,17 @@ void GetAttachedShaders(GLuint program,
   }
 }
 
+void GetAttribLocation(GLuint program,
+                       uint32_t name_bucket_id,
+                       uint32_t location_shm_id,
+                       uint32_t location_shm_offset) {
+  gles2::cmds::GetAttribLocation* c =
+      GetCmdSpace<gles2::cmds::GetAttribLocation>();
+  if (c) {
+    c->Init(program, name_bucket_id, location_shm_id, location_shm_offset);
+  }
+}
+
 void GetBooleanv(GLenum pname,
                  uint32_t params_shm_id,
                  uint32_t params_shm_offset) {
@@ -726,6 +737,17 @@ void GetUniformiv(GLuint program,
   gles2::cmds::GetUniformiv* c = GetCmdSpace<gles2::cmds::GetUniformiv>();
   if (c) {
     c->Init(program, location, params_shm_id, params_shm_offset);
+  }
+}
+
+void GetUniformLocation(GLuint program,
+                        uint32_t name_bucket_id,
+                        uint32_t location_shm_id,
+                        uint32_t location_shm_offset) {
+  gles2::cmds::GetUniformLocation* c =
+      GetCmdSpace<gles2::cmds::GetUniformLocation>();
+  if (c) {
+    c->Init(program, name_bucket_id, location_shm_id, location_shm_offset);
   }
 }
 
