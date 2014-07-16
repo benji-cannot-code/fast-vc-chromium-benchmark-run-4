f5f78e8f6b27022b07e1fe65e539f6c5acaef0c0
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-16 05:04:01 +0000

    Get rid of some uses of CreateDoubleValue
    BUG=160586
    
    Review URL: https://codereview.chromium.org/396493004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283348 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/performance_monitor/performance_monitor_handler.cc
+++ chrome/browser/ui/webui/performance_monitor/performance_monitor_handler.cc
@@ -276,9 +276,9 @@ void DoGetEvents(base::ListValue* results,
         // The property 'eventType' is set in HandleGetEvents as part of the
         // entire result set, so we don't need to include this here in the
         // event.
-        if (data.key() == "eventType")
+        if (data.key() == "eventType") {
           continue;
-        else if (data.key() == "time") {
+        } else if (data.key() == "time") {
           // The property 'time' is also used computationally, but must be
           // converted to JS-style time.
           double time = 0.0;
@@ -286,9 +286,9 @@ void DoGetEvents(base::ListValue* results,
             LOG(ERROR) << "Failed to get 'time' field from event.";
             continue;
           }
-          value = base::Value::CreateDoubleValue(
-              base::Time::FromInternalValue(
-                  static_cast<int64>(time)).ToJsTime());
+          value = new base::FundamentalValue(
+              base::Time::FromInternalValue(static_cast<int64>(time))
+                  .ToJsTime());
         } else {
           // All other values are user-facing, so we create a new value for
           // localized display.
