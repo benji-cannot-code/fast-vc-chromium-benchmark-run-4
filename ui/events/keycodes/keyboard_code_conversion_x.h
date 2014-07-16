6859b5a0188a5377f82d225948130b2bba10fc05
kpschoedel@chromium.org <kpschoedel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-16 04:19:23 +0000

    Fix some surprising reverse key mappings.
    Added a guard in |chromeos::EventRewriter::RewriteKeyEvent()|.
    Moved |ui::XKeyEventKeyCode()| to share it. Added unit test cases.
    
    BUG=390263
    TEST=EventRewriterTest.TestRewriteModifiersRemapMany
    R=derat@chromium.org,sadrul@chromium.org
    
    Review URL: https://codereview.chromium.org/378503007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283328 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/events/keycodes/keyboard_code_conversion_x.h
+++ ui/events/keycodes/keyboard_code_conversion_x.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/keycodes/keyboard_codes_posix.h"
 
 typedef union _XEvent XEvent;
+typedef struct _XDisplay XDisplay;
 
 namespace ui {
 
@@ -26,6 +27,14 @@ EVENTS_BASE_EXPORT uint16 GetCharacterFromXEvent(const XEvent* xev);
 // Converts a KeyboardCode into an X KeySym.
 EVENTS_BASE_EXPORT int XKeysymForWindowsKeyCode(KeyboardCode keycode,
                                                 bool shift);
+
+// Returns a XKeyEvent keycode (scancode) for a KeyboardCode. Keyboard layouts
+// are usually not injective, so inverse mapping should be avoided when
+// practical. A round-trip keycode -> KeyboardCode -> keycode will not
+// necessarily return the original keycode.
+EVENTS_BASE_EXPORT unsigned int XKeyCodeForWindowsKeyCode(KeyboardCode key_code,
+                                                          int flags,
+                                                          XDisplay* display);
 
 // Converts an X keycode into ui::KeyboardCode.
 EVENTS_BASE_EXPORT KeyboardCode
