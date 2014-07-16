2cac6e6505b13ba97abcb9e0a272f0b261a54c90
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-16 20:23:12 +0000

    Replace setsockopt(IP_PKTINFO) and setsockopt(IPV6_RECVPKTINFO) calls by gfe_quic::QuicSocketUtils::SetGetAddressInfo() calls. Update gfe_quic::QuicSocketUtils::SetGetAddressInfo() to the best-known implementation.
    Merge internal CL: 70966702
    
    R=rtenneti@chromium.org
    BUG=none
    
    Review URL: https://codereview.chromium.org/397793004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283482 0039d316-1c4b-4281-b951-d872f2087c98

--- net/tools/quic/quic_server.cc
+++ net/tools/quic/quic_server.cc
@@ -29,12 +29,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define SO_RXQ_OVFL 40
 #endif
 
-const int kEpollFlags = EPOLLIN | EPOLLOUT | EPOLLET;
-static const char kSourceAddressTokenSecret[] = "secret";
-const uint32 kServerInitialFlowControlWindow = 100 * net::kMaxPacketSize;
-
 namespace net {
 namespace tools {
+
+namespace {
+
+const int kEpollFlags = EPOLLIN | EPOLLOUT | EPOLLET;
+const char kSourceAddressTokenSecret[] = "secret";
+const uint32 kServerInitialFlowControlWindow = 100 * net::kMaxPacketSize;
+
+}  // namespace
 
 QuicServer::QuicServer()
     : port_(0),
@@ -93,6 +97,8 @@ bool QuicServer::Listen(const IPEndPoint& address) {
     return false;
   }
 
+  // Enable the socket option that allows the local address to be
+  // returned if the socket is bound to more than one address.
   int rc = QuicSocketUtils::SetGetAddressInfo(fd_, address_family);
 
   if (rc < 0) {
@@ -120,20 +126,6 @@ bool QuicServer::Listen(const IPEndPoint& address) {
 
   if (!QuicSocketUtils::SetSendBufferSize(fd_,
                                           TcpReceiver::kReceiveWindowTCP)) {
-    return false;
-  }
-
-  // Enable the socket option that allows the local address to be
-  // returned if the socket is bound to more than on address.
-  int get_local_ip = 1;
-  rc = setsockopt(fd_, IPPROTO_IP, IP_PKTINFO,
-                  &get_local_ip, sizeof(get_local_ip));
-  if (rc == 0 && address_family == AF_INET6) {
-    rc = setsockopt(fd_, IPPROTO_IPV6, IPV6_RECVPKTINFO,
-                    &get_local_ip, sizeof(get_local_ip));
-  }
-  if (rc != 0) {
-    LOG(ERROR) << "Failed to set required socket options";
     return false;
   }
 
