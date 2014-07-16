c0a1393917f8f4c453edc21fad885c34518e9386
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-16 23:28:39 +0000

    Remove extensions_manifest_types extensions API on Android.
    BUG=305852
    
    Review URL: https://codereview.chromium.org/396933002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283575 0039d316-1c4b-4281-b951-d872f2087c98

--- extensions/renderer/messaging_bindings.cc
+++ extensions/renderer/messaging_bindings.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/renderer/v8_value_converter.h"
 #include "extensions/common/api/messaging/message.h"
 #include "extensions/common/extension_messages.h"
-#include "extensions/common/manifest_handlers/externally_connectable.h"
 #include "extensions/renderer/dispatcher.h"
 #include "extensions/renderer/event_bindings.h"
 #include "extensions/renderer/object_backed_native_handler.h"
@@ -31,6 +30,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/web/WebScopedWindowFocusAllowedIndicator.h"
 #include "third_party/WebKit/public/web/WebUserGestureIndicator.h"
 #include "v8/include/v8.h"
+
+// TODO(thestig): Remove #ifdef from this file when extensions are no longer
+// used on mobile.
+#if defined(ENABLE_EXTENSIONS)
+#include "extensions/common/manifest_handlers/externally_connectable.h"
+#endif
 
 // Message passing API example (in a content script):
 // var extension =
@@ -262,6 +267,7 @@ void DispatchOnConnectToScriptContext(
     if (!source_tab->empty() && !extension->is_platform_app())
       tab = converter->ToV8Value(source_tab, script_context->v8_context());
 
+#if defined(ENABLE_EXTENSIONS)
     ExternallyConnectableInfo* externally_connectable =
         ExternallyConnectableInfo::Get(extension);
     if (externally_connectable &&
@@ -271,6 +277,7 @@ void DispatchOnConnectToScriptContext(
                                                      v8::String::kNormalString,
                                                      tls_channel_id.size());
     }
+#endif
   }
 
   v8::Handle<v8::Value> arguments[] = {
