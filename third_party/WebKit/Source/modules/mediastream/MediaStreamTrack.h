2e9a6eee5fe98925b1efa8aa675ba33a28f26fb9
mcasas@chromium.org <mcasas@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-17 08:41:50 +0000

    MediaStreamTrack: Add |mute| attribute and connection.
    This patch adds |muted| read only property [1] to MediaStreamTrack 
    [2] as per W3C doco.
    
    [1] http://dev.w3.org/2011/webrtc/editor/getusermedia.html#widl-MediaStreamTrack-muted
    [2] http://dev.w3.org/2011/webrtc/editor/getusermedia.html#media-stream-track-interface-definition
    
    BUG=389159
    
    Review URL: https://codereview.chromium.org/397603002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178337 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/mediastream/MediaStreamTrack.h
+++ third_party/WebKit/Source/modules/mediastream/MediaStreamTrack.h
@@ -59,6 +59,8 @@ public:
     bool enabled() const;
     void setEnabled(bool);
 
+    bool muted() const;
+
     String readyState() const;
 
     static void getSources(ExecutionContext*, PassOwnPtr<MediaStreamTrackSourcesCallback>, ExceptionState&);
