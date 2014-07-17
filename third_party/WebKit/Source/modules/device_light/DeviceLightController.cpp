b02a7230ddb5e4894ab9d79d887479f6e4b38128
rijubrata.bhaumik@intel.com <rijubrata.bhaumik@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-17 09:42:55 +0000

    Remove unused class/includes for DeviceLight after r175792
    Review URL: https://codereview.chromium.org/393023009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178349 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/device_light/DeviceLightController.cpp
+++ third_party/WebKit/Source/modules/device_light/DeviceLightController.cpp
@@ -7,8 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "modules/device_light/DeviceLightController.h"
 
 #include "core/dom/Document.h"
-#include "core/frame/LocalDOMWindow.h"
-#include "core/page/Page.h"
+#include "modules/EventModules.h"
 #include "modules/device_light/DeviceLightDispatcher.h"
 #include "modules/device_light/DeviceLightEvent.h"
 #include "platform/RuntimeEnabledFeatures.h"
