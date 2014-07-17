8965e7f272f96f57dcf6dbbed9324116a9229cdb
eseidel@chromium.org <eseidel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-17 21:51:21 +0000

    Make provision_devices failures abort the build
    Otherwise if the devices are gone lots of tests will just fail.
    
    Already fixed this for recipe based builders in crbug.com/393681
    
    I don't know how to test this, but happy to write one if shown.
    
    BUG=393681
    NOTRY=True
    
    Review URL: https://codereview.chromium.org/390993003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283890 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/buildbot/bb_device_steps.py
+++ build/android/buildbot/bb_device_steps.py
@@ -415,7 +415,7 @@ def ProvisionDevices(options):
     provision_cmd.append('--auto-reconnect')
   if options.skip_wipe:
     provision_cmd.append('--skip-wipe')
-  RunCmd(provision_cmd)
+  RunCmd(provision_cmd, halt_on_failure=True)
 
 
 def DeviceStatusCheck(options):
