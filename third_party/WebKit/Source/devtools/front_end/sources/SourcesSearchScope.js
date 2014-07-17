4dbe1dbed9900bb07145daddb2b8c02d34858d8d
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-17 19:14:01 +0000

    DevTools: don't fail on advanced search in case opened before workspace is initialized.
    TBR=vsevik
    
    Review URL: https://codereview.chromium.org/397273005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178390 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sources/SourcesSearchScope.js
+++ third_party/WebKit/Source/devtools/front_end/sources/SourcesSearchScope.js
@@ -35,7 +35,6 @@ WebInspector.SourcesSearchScope = function()
 {
     // FIXME: Add title once it is used by search controller.
     this._searchId = 0;
-    this._workspace = WebInspector.workspace;
 }
 
 WebInspector.SourcesSearchScope.prototype = {
@@ -87,7 +86,7 @@ WebInspector.SourcesSearchScope.prototype = {
             return WebInspector.settings.searchInContentScripts.get() || project.type() !== WebInspector.projectTypes.ContentScripts;
         }
 
-        return this._workspace.projects().filter(filterOutServiceProjects).filter(filterOutContentScriptsIfNeeded);
+        return WebInspector.workspace.projects().filter(filterOutServiceProjects).filter(filterOutContentScriptsIfNeeded);
     },
 
     /**
