6821eafa356617e7327c21180f67815bf902774f
v.keshav@samsung.com <v.keshav@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-17 08:43:27 +0000

    Implement sequence<MediaStreamTrack> getTracks () This is to conform to the new spec- http://dev.w3.org/2011/webrtc/editor/getusermedia.html#mediastream
    BUG=394212
    
    Review URL: https://codereview.chromium.org/397903003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178338 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/mediastream/MediaStream.h
+++ third_party/WebKit/Source/modules/mediastream/MediaStream.h
@@ -65,6 +65,7 @@ public:
 
     MediaStreamTrackVector getAudioTracks() const { return m_audioTracks; }
     MediaStreamTrackVector getVideoTracks() const { return m_videoTracks; }
+    MediaStreamTrackVector getTracks();
 
     bool ended() const;
     void stop();
