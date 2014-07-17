ef2f03508735bb0ba50759cb5f621146b0998658
spang@chromium.org <spang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-17 21:13:30 +0000

    ozone: test: Convert to PlatformWindow
    A test window is just a path to write an image file to. Each window has
    a unique image path.
    
    BUG=392280
    TEST=built with chromeos==1 use_ozone==1 & ran test platform
    NOTRY=true
    
    depends on https://codereview.chromium.org/375053002/
    
    Review URL: https://codereview.chromium.org/377973005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283875 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/ozone/platform/test/ozone_platform_test.cc
+++ ui/ozone/platform/test/ozone_platform_test.cc
@@ -9,9 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "ui/events/ozone/device/device_manager.h"
 #include "ui/events/ozone/evdev/event_factory_evdev.h"
-#include "ui/ozone/common/window/platform_window_compat.h"
-#include "ui/ozone/platform/test/file_surface_factory.h"
 #include "ui/ozone/platform/test/test_cursor_factory.h"
+#include "ui/ozone/platform/test/test_window.h"
+#include "ui/ozone/platform/test/test_window_manager.h"
+#include "ui/ozone/public/cursor_factory_ozone.h"
 #include "ui/ozone/public/gpu_platform_support.h"
 #include "ui/ozone/public/gpu_platform_support_host.h"
 #include "ui/ozone/public/ozone_platform.h"
@@ -36,7 +37,7 @@ class OzonePlatformTest : public OzonePlatform {
 
   // OzonePlatform:
   virtual ui::SurfaceFactoryOzone* GetSurfaceFactoryOzone() OVERRIDE {
-    return surface_factory_ozone_.get();
+    return window_manager_.get();
   }
   virtual EventFactoryOzone* GetEventFactoryOzone() OVERRIDE {
     return event_factory_ozone_.get();
@@ -54,7 +55,7 @@ class OzonePlatformTest : public OzonePlatform {
       PlatformWindowDelegate* delegate,
       const gfx::Rect& bounds) OVERRIDE {
     return make_scoped_ptr<PlatformWindow>(
-        new PlatformWindowCompat(delegate, bounds));
+        new TestWindow(delegate, window_manager_.get(), bounds));
   }
 
 #if defined(OS_CHROMEOS)
@@ -71,7 +72,8 @@ class OzonePlatformTest : public OzonePlatform {
 
   virtual void InitializeUI() OVERRIDE {
     device_manager_ = CreateDeviceManager();
-    surface_factory_ozone_.reset(new FileSurfaceFactory(file_path_));
+    window_manager_.reset(new TestWindowManager(file_path_));
+    window_manager_->Initialize();
     event_factory_ozone_.reset(
         new EventFactoryEvdev(NULL, device_manager_.get()));
     cursor_factory_ozone_.reset(new TestCursorFactory());
@@ -84,7 +86,7 @@ class OzonePlatformTest : public OzonePlatform {
 
  private:
   scoped_ptr<DeviceManager> device_manager_;
-  scoped_ptr<FileSurfaceFactory> surface_factory_ozone_;
+  scoped_ptr<TestWindowManager> window_manager_;
   scoped_ptr<EventFactoryEvdev> event_factory_ozone_;
   scoped_ptr<CursorFactoryOzone> cursor_factory_ozone_;
   scoped_ptr<GpuPlatformSupport> gpu_platform_support_;
