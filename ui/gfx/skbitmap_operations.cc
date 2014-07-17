0063aa34e03b1a3e3796bb60ac8beed181d72e97
hyunki.baik@samsung.com <hyunki.baik@samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-17 17:59:40 +0000

    ui/gfx: remove redundant erase operations and remove unused function
    It removes erase operations for SkBitmaps
    which touch whole inside pixels
    since all pixels should be drawn fully.
    
    Additionally, it removes a unused function, CreateSuperimposedBitmap().
    
    BUG=NONE
    TEST=gfx_unittests --gtest_filter="SkBitmapOperationsTest*"
    
    Review URL: https://codereview.chromium.org/394183002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283814 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/skbitmap_operations.cc
+++ ui/gfx/skbitmap_operations.cc
@@ -28,7 +28,6 @@ SkBitmap SkBitmapOperations::CreateInvertedBitmap(const SkBitmap& image) {
 
   SkBitmap inverted;
   inverted.allocN32Pixels(image.width(), image.height());
-  inverted.eraseARGB(0, 0, 0, 0);
 
   for (int y = 0; y < image.height(); ++y) {
     uint32* image_row = image.getAddr32(0, y);
@@ -42,35 +41,6 @@ SkBitmap SkBitmapOperations::CreateInvertedBitmap(const SkBitmap& image) {
   }
 
   return inverted;
-}
-
-// static
-SkBitmap SkBitmapOperations::CreateSuperimposedBitmap(const SkBitmap& first,
-                                                      const SkBitmap& second) {
-  DCHECK(first.width() == second.width());
-  DCHECK(first.height() == second.height());
-  DCHECK(first.bytesPerPixel() == second.bytesPerPixel());
-  DCHECK(first.colorType() == kN32_SkColorType);
-
-  SkAutoLockPixels lock_first(first);
-  SkAutoLockPixels lock_second(second);
-
-  SkBitmap superimposed;
-  superimposed.allocN32Pixels(first.width(), first.height());
-  superimposed.eraseARGB(0, 0, 0, 0);
-
-  SkCanvas canvas(superimposed);
-
-  SkRect rect;
-  rect.fLeft = 0;
-  rect.fTop = 0;
-  rect.fRight = SkIntToScalar(first.width());
-  rect.fBottom = SkIntToScalar(first.height());
-
-  canvas.drawBitmapRect(first, NULL, rect);
-  canvas.drawBitmapRect(second, NULL, rect);
-
-  return superimposed;
 }
 
 // static
@@ -96,7 +66,6 @@ SkBitmap SkBitmapOperations::CreateBlendedBitmap(const SkBitmap& first,
 
   SkBitmap blended;
   blended.allocN32Pixels(first.width(), first.height());
-  blended.eraseARGB(0, 0, 0, 0);
 
   double first_alpha = 1 - alpha;
 
@@ -136,7 +105,6 @@ SkBitmap SkBitmapOperations::CreateMaskedBitmap(const SkBitmap& rgb,
 
   SkBitmap masked;
   masked.allocN32Pixels(rgb.width(), rgb.height());
-  masked.eraseARGB(0, 0, 0, 0);
 
   SkAutoLockPixels lock_rgb(rgb);
   SkAutoLockPixels lock_alpha(alpha);
@@ -552,7 +520,6 @@ SkBitmap SkBitmapOperations::CreateHSLShiftedBitmap(
 
   SkBitmap shifted;
   shifted.allocN32Pixels(bitmap.width(), bitmap.height());
-  shifted.eraseARGB(0, 0, 0, 0);
 
   SkAutoLockPixels lock_bitmap(bitmap);
   SkAutoLockPixels lock_shifted(shifted);
@@ -576,7 +543,6 @@ SkBitmap SkBitmapOperations::CreateTiledBitmap(const SkBitmap& source,
 
   SkBitmap cropped;
   cropped.allocN32Pixels(dst_w, dst_h);
-  cropped.eraseARGB(0, 0, 0, 0);
 
   SkAutoLockPixels lock_source(source);
   SkAutoLockPixels lock_cropped(cropped);
