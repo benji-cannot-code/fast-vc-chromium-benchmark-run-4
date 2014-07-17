f9e248ec0e68316d565a6e17d79c6c8a9fc89b1b
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-17 19:08:55 +0000

    Replace usage of NOTIFICATION_EXTENSION_UNLOADED with ExtensionRegistryObserver.
    This also fixes a bug caused by registering for notifications in all profiles instead of just our own.
    
    BUG=394158,354046
    
    Review URL: https://codereview.chromium.org/399603002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283831 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options/font_settings_handler.cc
+++ chrome/browser/ui/webui/options/font_settings_handler.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/character_encoding.h"
-#include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/extension_tab_util.h"
 #include "chrome/browser/profiles/profile.h"
@@ -30,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_details.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/web_ui.h"
+#include "extensions/browser/extension_registry.h"
 #include "extensions/browser/extension_system.h"
 #include "extensions/common/extension.h"
 #include "grit/chromium_strings.h"
@@ -65,7 +65,8 @@ const char kAdvancedFontSettingsExtensionId[] =
 
 namespace options {
 
-FontSettingsHandler::FontSettingsHandler() {
+FontSettingsHandler::FontSettingsHandler()
+    : extension_registry_observer_(this) {
 }
 
 FontSettingsHandler::~FontSettingsHandler() {
@@ -114,11 +115,8 @@ void FontSettingsHandler::GetLocalizedValues(
 }
 
 void FontSettingsHandler::InitializeHandler() {
-  registrar_.Add(this,
-                 chrome::NOTIFICATION_EXTENSION_LOADED_DEPRECATED,
-                 content::NotificationService::AllSources());
-  registrar_.Add(this, chrome::NOTIFICATION_EXTENSION_UNLOADED_DEPRECATED,
-                 content::NotificationService::AllSources());
+  Profile* profile = Profile::FromWebUI(web_ui());
+  extension_registry_observer_.Add(extensions::ExtensionRegistry::Get(profile));
 }
 
 void FontSettingsHandler::InitializePage() {
@@ -178,11 +176,16 @@ void FontSettingsHandler::RegisterMessages() {
                  base::Unretained(this)));
 }
 
-void FontSettingsHandler::Observe(int type,
-                                  const content::NotificationSource& source,
-                                  const content::NotificationDetails& details) {
-  DCHECK(type == chrome::NOTIFICATION_EXTENSION_LOADED_DEPRECATED ||
-         type == chrome::NOTIFICATION_EXTENSION_UNLOADED_DEPRECATED);
+void FontSettingsHandler::OnExtensionLoaded(
+    content::BrowserContext* browser_context,
+    const extensions::Extension* extension) {
+  NotifyAdvancedFontSettingsAvailability();
+}
+
+void FontSettingsHandler::OnExtensionUnloaded(
+    content::BrowserContext* browser_context,
+    const extensions::Extension* extension,
+    extensions::UnloadedExtensionInfo::Reason reason) {
   NotifyAdvancedFontSettingsAvailability();
 }
 
--- chrome/browser/ui/webui/options/font_settings_handler.h
+++ chrome/browser/ui/webui/options/font_settings_handler.h
@@ -8,9 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/scoped_ptr.h"
 #include "base/prefs/pref_member.h"
+#include "base/scoped_observer.h"
 #include "chrome/browser/ui/webui/options/options_ui.h"
-#include "content/public/browser/notification_observer.h"
-#include "content/public/browser/notification_registrar.h"
+#include "extensions/browser/extension_registry_observer.h"
 
 namespace base {
 class ListValue;
@@ -18,13 +18,14 @@ class ListValue;
 
 namespace extensions {
 class Extension;
+class ExtensionRegistry;
 }
 
 namespace options {
 
 // Font settings overlay page UI handler.
 class FontSettingsHandler : public OptionsPageUIHandler,
-                            public content::NotificationObserver {
+                            public extensions::ExtensionRegistryObserver {
  public:
   FontSettingsHandler();
   virtual ~FontSettingsHandler();
@@ -38,12 +39,16 @@ class FontSettingsHandler : public OptionsPageUIHandler,
   // WebUIMessageHandler implementation.
   virtual void RegisterMessages() OVERRIDE;
 
- private:
-  // content::NotificationObserver implementation.
-  virtual void Observe(int type,
-                       const content::NotificationSource& source,
-                       const content::NotificationDetails& details) OVERRIDE;
+  // ExtensionRegistryObserver implementation.
+  virtual void OnExtensionLoaded(
+      content::BrowserContext* browser_context,
+      const extensions::Extension* extension) OVERRIDE;
+  virtual void OnExtensionUnloaded(
+      content::BrowserContext* browser_context,
+      const extensions::Extension* extension,
+      extensions::UnloadedExtensionInfo::Reason reason) OVERRIDE;
 
+ private:
   void HandleFetchFontsData(const base::ListValue* args);
 
   void FontsListHasLoaded(scoped_ptr<base::ListValue> list);
@@ -74,7 +79,9 @@ class FontSettingsHandler : public OptionsPageUIHandler,
   IntegerPrefMember default_fixed_font_size_;
   IntegerPrefMember minimum_font_size_;
 
-  content::NotificationRegistrar registrar_;
+  ScopedObserver<extensions::ExtensionRegistry,
+                 extensions::ExtensionRegistryObserver>
+      extension_registry_observer_;
 
   DISALLOW_COPY_AND_ASSIGN(FontSettingsHandler);
 };
