1d2f3eb88a6919b6cf475032812086a5f50c647e
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-17 11:22:33 +0000

    Fix RLZ test support dependency.
    BUG=393815
    R=thakis@chromium.org
    
    Review URL: https://codereview.chromium.org/393903002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283753 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prefs/tracked/pref_hash_calculator_helper_win_unittest.cc
+++ chrome/browser/prefs/tracked/pref_hash_calculator_helper_win_unittest.cc
@@ -18,8 +18,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "content/public/test/test_utils.h"
 #include "crypto/hmac.h"
-#include "rlz/lib/machine_id.h"
 #include "testing/gtest/include/gtest/gtest.h"
+
+#if defined(ENABLE_RLZ)
+#include "rlz/lib/machine_id.h"
+#endif
 
 namespace {
 
@@ -71,6 +74,7 @@ std::string GetDeviceIdFromRawDeviceId(const std::string& raw_device_id) {
   return StringToLowerASCII(base::HexEncode(digest.data(), digest.size()));
 }
 
+#if defined(ENABLE_RLZ)
 std::string GetLegacyIdBasedOnRlzId() {
   std::string rlz_machine_id;
   rlz_lib::GetMachineId(&rlz_machine_id);
@@ -89,6 +93,7 @@ std::string GetLegacyIdBasedOnRlzId() {
 
   return legacy_device_id;
 }
+#endif  // ENABLE_RLZ
 
 // Simulate browser threads (required by extensions::api::DeviceId) off of the
 // main message loop.
@@ -105,14 +110,18 @@ class PrefHashCalculatorHelperTest : public testing::Test {
 // results in the mean time (it will be okay for the extension API's
 // implementation to diverge on M34+ and this test can be removed once M34 ships
 // to stable).
+#if defined(ENABLE_RLZ)
 TEST_F(PrefHashCalculatorHelperTest, ResultMatchesMediaId) {
   EXPECT_EQ(GetMediaDeviceIdSynchronously(), GetLegacyIdBasedOnRlzId());
 }
+#endif  // ENABLE_RLZ
 
 TEST_F(PrefHashCalculatorHelperTest, MediaIdIsDeterministic) {
   EXPECT_EQ(GetMediaDeviceIdSynchronously(), GetMediaDeviceIdSynchronously());
 }
 
+#if defined(ENABLE_RLZ)
 TEST_F(PrefHashCalculatorHelperTest, RlzBasedIdIsDeterministic) {
   EXPECT_EQ(GetLegacyIdBasedOnRlzId(), GetLegacyIdBasedOnRlzId());
 }
+#endif  // ENABLE_RLZ
--- chrome/chrome_tests_unit.gypi
+++ chrome/chrome_tests_unit.gypi
@@ -493,7 +493,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         '../content/content.gyp:content_app_both',
         '../net/net.gyp:net',
         '../net/net.gyp:net_test_support',
-        '../rlz/rlz.gyp:test_support_rlz',
         '../sync/sync.gyp:test_support_sync_api',
         '../sync/sync.gyp:test_support_sync_core',
         '../sync/sync.gyp:test_support_sync_internal_api',
@@ -2485,7 +2484,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'utility/media_galleries/iphoto_library_parser_unittest.cc',
           ],
         }],
-        ['enable_rlz==0', {
+        ['enable_rlz!=0', {
+          'dependencies': [
+            '../rlz/rlz.gyp:test_support_rlz',
+          ],
+        }, {  # enable_rlz==0
           'sources!': [
             'browser/rlz/rlz_unittest.cc',
           ],
