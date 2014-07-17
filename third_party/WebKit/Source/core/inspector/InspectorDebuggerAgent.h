16204706b03c4496dd9ff9b2f10bde56f360edf4
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-17 18:58:14 +0000

    DevTools: Add scripts sourceURL plumbing from v8.
    BUG=v8:2948
    
    Review URL: https://codereview.chromium.org/323523004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178389 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorDebuggerAgent.h
+++ third_party/WebKit/Source/core/inspector/InspectorDebuggerAgent.h
@@ -227,7 +227,7 @@ private:
     bool assertPaused(ErrorString*);
     void clearBreakDetails();
 
-    String sourceMapURLForScript(const Script&);
+    String sourceMapURLForScript(const Script&, CompileResult);
 
     String scriptURL(JavaScriptCallFrame*);
 
--- third_party/WebKit/Source/core/inspector/ScriptDebugListener.h
+++ third_party/WebKit/Source/core/inspector/ScriptDebugListener.h
@@ -58,8 +58,9 @@ public:
         }
 
         String url;
-        String source;
+        String sourceURL;
         String sourceMappingURL;
+        String source;
         int startLine;
         int startColumn;
         int endLine;
