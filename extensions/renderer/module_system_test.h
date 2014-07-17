4dc00a1cdbdf43cce86f05c34adadf430501fd19
sammc@chromium.org <sammc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-17 22:15:56 +0000

    Don't create a gin::IsolateHolder in extensions unittests.
    This isn't necessary because the test environment set up includes Blink
    creating an isolate holder.
    
    Review URL: https://codereview.chromium.org/402553002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283904 0039d316-1c4b-4281-b951-d872f2087c98

--- extensions/renderer/module_system_test.h
+++ extensions/renderer/module_system_test.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/renderer/scoped_persistent.h"
 #include "extensions/renderer/script_context.h"
 #include "gin/public/context_holder.h"
-#include "gin/public/isolate_holder.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "v8/include/v8.h"
 
@@ -21,7 +20,7 @@ class ModuleSystemTestEnvironment {
   class AssertNatives;
   class StringSourceMap;
 
-  explicit ModuleSystemTestEnvironment(gin::IsolateHolder* isolate_holder);
+  explicit ModuleSystemTestEnvironment(v8::Isolate* isolate);
   ~ModuleSystemTestEnvironment();
 
   // Register a named JS module in the module system.
@@ -51,12 +50,12 @@ class ModuleSystemTestEnvironment {
 
   ScriptContext* context() { return context_.get(); }
 
-  v8::Isolate* isolate() { return isolate_holder_->isolate(); }
+  v8::Isolate* isolate() { return isolate_; }
 
   AssertNatives* assert_natives() { return assert_natives_; }
 
  private:
-  gin::IsolateHolder* isolate_holder_;
+  v8::Isolate* isolate_;
   scoped_ptr<gin::ContextHolder> context_holder_;
   v8::HandleScope handle_scope_;
   scoped_ptr<ScriptContext> context_;
@@ -99,7 +98,7 @@ class ModuleSystemTest : public testing::Test {
   void RunResolvedPromises();
 
  private:
-  gin::IsolateHolder isolate_holder_;
+  v8::Isolate* isolate_;
   scoped_ptr<ModuleSystemTestEnvironment> env_;
   bool should_assertions_be_made_;
 
