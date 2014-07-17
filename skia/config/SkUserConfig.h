b626252020dc8db74f21a9b71586bb473d89e904
reed@google.com <reed@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-17 05:09:01 +0000

    move compile guard into chrome
    will remove the flag from the skia side after this lands
    
    BUG=
    
    Review URL: https://codereview.chromium.org/396733009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283649 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -254,6 +254,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
     #define SK_SUPPORT_LEGACY_GETTOPDEVICE
 #endif
 
+#ifndef SK_SUPPORT_LEGACY_GETDEVICE
+    #define SK_SUPPORT_LEGACY_GETDEVICE
+#endif
+
 // ===== End Chrome-specific definitions =====
 
 #endif
