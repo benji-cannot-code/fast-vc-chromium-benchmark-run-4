bcc3157e25db76c82e33d5618652679a7c3fcb36
yukishiino@chromium.org <yukishiino@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-17 15:43:22 +0000

    dom: Makes ConsoleBase inherit from ScriptWrappable.
    Makes ConsoleBase, instead of its subclasses, inherit from ScriptWrappable.
    
    BUG=235436
    
    Review URL: https://codereview.chromium.org/396803011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178377 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/workers/WorkerConsole.h
+++ third_party/WebKit/Source/core/workers/WorkerConsole.h
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WorkerConsole_h
 #define WorkerConsole_h
 
-#include "bindings/core/v8/ScriptWrappable.h"
 #include "core/inspector/ConsoleAPITypes.h"
 #include "core/frame/ConsoleBase.h"
 #include "core/frame/ConsoleTypes.h"
@@ -46,7 +45,7 @@ namespace WebCore {
 
 class ScriptArguments;
 
-class WorkerConsole FINAL : public ConsoleBase, public ScriptWrappable {
+class WorkerConsole FINAL : public ConsoleBase {
 public:
     static PassRefPtrWillBeRawPtr<WorkerConsole> create(WorkerGlobalScope* scope)
     {
