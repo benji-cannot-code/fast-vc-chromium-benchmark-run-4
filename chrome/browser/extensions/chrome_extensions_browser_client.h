4f4fb7ce6172b833f7ec45ded7b84484ca3f8841
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-17 07:05:49 +0000

    Refactor code that defers extension background page loading
    src/extensions depends on chrome::NOTIFICATION_PROFILE_CREATED to support deferred loading of extension background pages when the profile isn't ready yet. This is a layering violation.
    
    * Remove Chrome concepts like "browser window ready" and "profile created" from ProcessManager. Introduce ProcessManagerDelegate with a general concept of deferring background page loading.
    * Consolidate all the tricky Chrome-specific background page loading rules into ChromeProcessManagerDelegate. This keeps all the rules in one place. Annotate each block of special case code with the bug that inspired it.
    * Extend unit test coverage for ProcessManager.
    
    This will make it easier to eliminate chrome::NOTIFICATION_PROFILE_DESTROYED in ProcessManager in a later CL.
    
    BUG=392658
    TEST=unit_tests ProcessManagerTest, browser_tests ProcessManagerBrowserTest, manual
    
    Review URL: https://codereview.chromium.org/381283002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283678 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/chrome_extensions_browser_client.h
+++ chrome/browser/extensions/chrome_extensions_browser_client.h
@@ -27,6 +27,7 @@ namespace extensions {
 
 class ChromeComponentExtensionResourceManager;
 class ChromeExtensionsAPIClient;
+class ChromeProcessManagerDelegate;
 class ContentSettingsPrefsObserver;
 
 // Implementation of extensions::BrowserClient for Chrome, which includes
@@ -77,10 +78,7 @@ class ChromeExtensionsBrowserClient : public ExtensionsBrowserClient {
   virtual void GetEarlyExtensionPrefsObservers(
       content::BrowserContext* context,
       std::vector<ExtensionPrefsObserver*>* observers) const OVERRIDE;
-  virtual bool DeferLoadingBackgroundHosts(
-      content::BrowserContext* context) const OVERRIDE;
-  virtual bool IsBackgroundPageAllowed(
-      content::BrowserContext* context) const OVERRIDE;
+  virtual ProcessManagerDelegate* GetProcessManagerDelegate() const OVERRIDE;
   virtual scoped_ptr<ExtensionHostDelegate> CreateExtensionHostDelegate()
       OVERRIDE;
   virtual bool DidVersionUpdate(content::BrowserContext* context) OVERRIDE;
@@ -103,6 +101,9 @@ class ChromeExtensionsBrowserClient : public ExtensionsBrowserClient {
   ChromeNotificationObserver notification_observer_;
 
 #if defined(ENABLE_EXTENSIONS)
+  // Support for ProcessManager.
+  scoped_ptr<ChromeProcessManagerDelegate> process_manager_delegate_;
+
   // Client for API implementations.
   scoped_ptr<ChromeExtensionsAPIClient> api_client_;
 #endif
