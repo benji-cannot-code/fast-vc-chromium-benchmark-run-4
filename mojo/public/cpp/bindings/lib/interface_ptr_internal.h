bacc1b8184d7f9c4a7461768c8c4f8c06ce2d3a7
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-17 21:22:26 +0000

    Mojo: Remove unnecessary <stdio.h> include.
    R=yzshen@chromium.org
    
    Review URL: https://codereview.chromium.org/405433005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283881 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/public/cpp/bindings/lib/interface_ptr_internal.h
+++ mojo/public/cpp/bindings/lib/interface_ptr_internal.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define MOJO_PUBLIC_CPP_BINDINGS_LIB_INTERFACE_PTR_INTERNAL_H_
 
 #include <assert.h>
-#include <stdio.h>
 
 #include "mojo/public/cpp/bindings/lib/filter_chain.h"
 #include "mojo/public/cpp/bindings/lib/message_header_validator.h"
