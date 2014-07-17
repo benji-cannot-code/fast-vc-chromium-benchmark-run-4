b31ffde72a03a2b188247f00b9f3b00956c2c146
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-17 18:37:28 +0000

    Simplify the logic in DesktopWindowTreeHostX11 for FocusOut
    We should have the same handling regardless of whether the 'mode' member is
    NotifyGrab or NotifyUngrab. According to
    http://tronche.com/gui/x/xlib/events/input-focus/grab.html, whether the mode
    field is NotifyGrab or NotifyUngrab depends on whether there is a new
    capturer for the keyboard. The X window has lost keyboard capture in both cases.
    
    BUG=392480
    TEST=None
    
    Review URL: https://codereview.chromium.org/379773003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283823 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/desktop_aura/desktop_window_tree_host_x11.cc
+++ ui/views/widget/desktop_aura/desktop_window_tree_host_x11.cc
@@ -1596,13 +1596,8 @@ uint32_t DesktopWindowTreeHostX11::DispatchEvent(
       break;
     }
     case FocusOut:
-      if (xev->xfocus.mode != NotifyGrab) {
-        ReleaseCapture();
-        OnHostLostWindowCapture();
-        X11DesktopHandler::get()->ProcessXEvent(xev);
-      } else {
-        dispatcher()->OnHostLostMouseGrab();
-      }
+      ReleaseCapture();
+      X11DesktopHandler::get()->ProcessXEvent(xev);
       break;
     case FocusIn:
       X11DesktopHandler::get()->ProcessXEvent(xev);
