d5eb823f22e5d0a5d0800a6be6b33f23e1528b4a
cbiesinger@chromium.org <cbiesinger@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-17 04:39:07 +0000

    Remove some special-casing of EventListener
    No need to make the bindings use a raw ptr for removeEventListener;
    we can just call .get() in the implementation.
    
    Also make the bindings code a bit more generic by not assuming that the
    event listener is the second argument.
    
    BUG=383699
    
    Review URL: https://codereview.chromium.org/383873002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178318 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/events/EventTarget.cpp
+++ third_party/WebKit/Source/core/events/EventTarget.cpp
@@ -107,7 +107,7 @@ bool EventTarget::addEventListener(const AtomicString& eventType, PassRefPtr<Eve
     return ensureEventTargetData().eventListenerMap.add(eventType, listener, useCapture);
 }
 
-bool EventTarget::removeEventListener(const AtomicString& eventType, EventListener* listener, bool useCapture)
+bool EventTarget::removeEventListener(const AtomicString& eventType, PassRefPtr<EventListener> listener, bool useCapture)
 {
     EventTargetData* d = eventTargetData();
     if (!d)
@@ -115,7 +115,7 @@ bool EventTarget::removeEventListener(const AtomicString& eventType, EventListen
 
     size_t indexOfRemovedListener;
 
-    if (!d->eventListenerMap.remove(eventType, listener, useCapture, indexOfRemovedListener))
+    if (!d->eventListenerMap.remove(eventType, listener.get(), useCapture, indexOfRemovedListener))
         return false;
 
     // Notify firing events planning to invoke the listener at 'index' that
--- third_party/WebKit/Source/modules/netinfo/NetworkInformation.cpp
+++ third_party/WebKit/Source/modules/netinfo/NetworkInformation.cpp
@@ -94,7 +94,7 @@ bool NetworkInformation::addEventListener(const AtomicString& eventType, PassRef
     return true;
 }
 
-bool NetworkInformation::removeEventListener(const AtomicString& eventType, EventListener* listener, bool useCapture)
+bool NetworkInformation::removeEventListener(const AtomicString& eventType, PassRefPtr<EventListener> listener, bool useCapture)
 {
     if (!EventTargetWithInlineData::removeEventListener(eventType, listener, useCapture))
         return false;
