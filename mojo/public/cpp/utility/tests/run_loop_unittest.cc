3ed667e1de14af6fb9a04766bec3e90487cad6b5
morrita@chromium.org <morrita@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-17 09:32:18 +0000

    Mojo: Remove extra blank line
    This is a follow up of https://codereview.chromium.org/392063002/
    
    BUG=none
    TBR=viettrungluu@chromium.org
    TEST=none
    
    Review URL: https://codereview.chromium.org/397063002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283727 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/public/cpp/utility/tests/run_loop_unittest.cc
+++ mojo/public/cpp/utility/tests/run_loop_unittest.cc
@@ -191,7 +191,6 @@ TEST_F(RunLoopTest, Current) {
   EXPECT_TRUE(RunLoop::current() == NULL);
 }
 
-
 class NestingRunLoopHandler : public TestRunLoopHandler {
  public:
   static const size_t kDepthLimit;
