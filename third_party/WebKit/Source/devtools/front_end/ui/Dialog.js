71ffafd9eb0bc2aefa67718736dd436c15050a9d
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-17 14:10:32 +0000

    DevTools: Fix DefaultFocusedViewStack handling for dialogs.
    BUG=392793
    R=apavlov, dgozman
    
    Review URL: https://codereview.chromium.org/403493003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178372 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ui/Dialog.js
+++ third_party/WebKit/Source/devtools/front_end/ui/Dialog.js
@@ -88,6 +88,11 @@ WebInspector.Dialog.hide = function()
 }
 
 WebInspector.Dialog.prototype = {
+    focus: function()
+    {
+        this._element.focus();
+    },
+
     _hide: function()
     {
         if (this._isHiding)
--- third_party/WebKit/Source/devtools/front_end/ui/UIUtils.js
+++ third_party/WebKit/Source/devtools/front_end/ui/UIUtils.js
@@ -164,7 +164,7 @@ WebInspector.GlassPane.prototype = {
 }
 
 /**
- * @type {!Array.<!WebInspector.View>}
+ * @type {!Array.<!WebInspector.View|!WebInspector.Dialog>}
  */
 WebInspector.GlassPane.DefaultFocusedViewStack = [];
 
