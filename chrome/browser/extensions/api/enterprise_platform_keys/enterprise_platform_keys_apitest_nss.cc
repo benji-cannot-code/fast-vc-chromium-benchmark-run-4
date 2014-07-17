6feeb4bbb695d61d759264cf44d4fffd8cd5459b
tbarzic@chromium.org <tbarzic@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-17 20:57:37 +0000

    Cleanup tests using SetSkipCertificateImporterCreationForTest
    UserNetworkConfigurationUpdater::SetSkipCertificateImporterCreationForTest
    is not needed anymore, so it's safe to remove it and cleanup tests
    using it.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/325313004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283868 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/enterprise_platform_keys/enterprise_platform_keys_apitest_nss.cc
+++ chrome/browser/extensions/api/enterprise_platform_keys/enterprise_platform_keys_apitest_nss.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/strings/stringprintf.h"
 #include "chrome/browser/chrome_notification_types.h"
-#include "chrome/browser/chromeos/policy/user_network_configuration_updater.h"
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/net/nss_context.h"
@@ -19,9 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_service.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/test/test_utils.h"
-#include "content/public/test/test_utils.h"
 #include "content/test/net/url_request_mock_http_job.h"
-#include "crypto/nss_util.h"
 #include "net/base/net_errors.h"
 #include "net/cert/nss_cert_database.h"
 #include "policy/policy_constants.h"
@@ -102,12 +99,6 @@ void DidGetCertDatabase(base::RunLoop* loop, net::NSSCertDatabase* cert_db) {
 
 class EnterprisePlatformKeysTest : public ExtensionApiTest {
  public:
-  virtual void SetUp() {
-    policy::UserNetworkConfigurationUpdater::
-        SetSkipCertificateImporterCreationForTest(true /*skip*/);
-    ExtensionApiTest::SetUp();
-  }
-
   virtual void SetUpCommandLine(CommandLine* command_line) OVERRIDE {
     ExtensionApiTest::SetUpCommandLine(command_line);
 
@@ -136,16 +127,6 @@ class EnterprisePlatformKeysTest : public ExtensionApiTest {
         FROM_HERE,
         base::Bind(chrome_browser_net::SetUrlRequestMocksEnabled, true));
 
-    {
-      base::RunLoop loop;
-      content::BrowserThread::PostTask(
-          content::BrowserThread::IO,
-          FROM_HERE,
-          base::Bind(&EnterprisePlatformKeysTest::SetupTestNSSDBOnIOThread,
-                     base::Unretained(this),
-                     &loop));
-      loop.Run();
-    }
 
     {
       base::RunLoop loop;
@@ -157,31 +138,7 @@ class EnterprisePlatformKeysTest : public ExtensionApiTest {
     SetPolicy();
   }
 
-  virtual void CleanUpOnMainThread() OVERRIDE {
-    base::RunLoop loop;
-    content::BrowserThread::PostTask(
-        content::BrowserThread::IO,
-        FROM_HERE,
-        base::Bind(&EnterprisePlatformKeysTest::DeleteTestNSSDBOnIOThread,
-                   base::Unretained(this),
-                   &loop));
-    loop.Run();
-    ExtensionApiTest::CleanUpOnMainThread();
-  }
-
  private:
-  void SetupTestNSSDBOnIOThread(base::RunLoop* loop) {
-    test_nssdb_.reset(new crypto::ScopedTestNSSDB);
-    content::BrowserThread::PostTask(
-        content::BrowserThread::UI, FROM_HERE, loop->QuitClosure());
-  }
-
-  void DeleteTestNSSDBOnIOThread(base::RunLoop* loop) {
-    test_nssdb_.reset();
-    content::BrowserThread::PostTask(
-        content::BrowserThread::UI, FROM_HERE, loop->QuitClosure());
-  }
-
   void SetPolicy() {
     // Extensions that are force-installed come from an update URL, which
     // defaults to the webstore. Use a mock URL for this test with an update
@@ -210,7 +167,6 @@ class EnterprisePlatformKeysTest : public ExtensionApiTest {
     observer.Wait();
   }
 
-  scoped_ptr<crypto::ScopedTestNSSDB> test_nssdb_;
   policy::MockConfigurationPolicyProvider policy_provider_;
 };
 
