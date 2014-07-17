dc00b4ca632573511eed9e22c2b30945d670b898
juyik@chromium.org <juyik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-17 20:09:19 +0000

    Record GCM received data message intervals in UMA.
    BUG=386273
    
    Review URL: https://codereview.chromium.org/344613003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283847 0039d316-1c4b-4281-b951-d872f2087c98

--- components/gcm_driver/gcm_stats_recorder_impl.cc
+++ components/gcm_driver/gcm_stats_recorder_impl.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace gcm {
 
 const uint32 MAX_LOGGED_ACTIVITY_COUNT = 100;
+const int64 RECEIVED_DATA_MESSAGE_BURST_LENGTH_SECONDS = 2;
 
 namespace {
 
@@ -363,6 +364,20 @@ void GCMStatsRecorderImpl::RecordDataMessageReceived(
     ReceivedMessageType message_type) {
   if (to_registered_app)
     UMA_HISTOGRAM_COUNTS("GCM.DataMessageReceived", 1);
+
+  base::Time new_timestamp = base::Time::Now();
+  if (last_received_data_message_burst_start_time_.is_null()) {
+    last_received_data_message_burst_start_time_ = new_timestamp;
+  } else if ((new_timestamp - last_received_data_message_burst_start_time_) >=
+             base::TimeDelta::FromSeconds(
+                 RECEIVED_DATA_MESSAGE_BURST_LENGTH_SECONDS)) {
+    UMA_HISTOGRAM_COUNTS(
+        "GCM.DataMessageBurstReceivedIntervalSeconds",
+        (new_timestamp - last_received_data_message_burst_start_time_)
+            .InSeconds());
+    last_received_data_message_burst_start_time_ = new_timestamp;
+  }
+
   if (!is_recording_)
     return;
   if (!to_registered_app) {
