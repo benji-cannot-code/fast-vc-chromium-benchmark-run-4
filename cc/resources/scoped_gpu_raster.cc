cf82e0e7cd497045e598683933d82cca99a967ca
sohan.jyoti@samsung.com <sohan.jyoti@samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-17 20:23:19 +0000

    cc: Add TODO's in ScopedGpuRaster.
    TestContextProvider returns NULL GrContext now, adding todo's to make
    it clear that code path is used only because of that shortcoming.
    BUG=
    
    Review URL: https://codereview.chromium.org/396853003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283853 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/resources/scoped_gpu_raster.cc
+++ cc/resources/scoped_gpu_raster.cc
@@ -31,6 +31,7 @@ void ScopedGpuRaster::BeginGpuRaster() {
   gl->PushGroupMarkerEXT(0, "GpuRasterization");
 
   class GrContext* gr_context = context_provider_->GrContext();
+  // TODO(sohanjg): Remove when TestContextProvider gives a GrContext.
   if (gr_context)
     gr_context->resetContext();
 }
@@ -39,6 +40,7 @@ void ScopedGpuRaster::EndGpuRaster() {
   GLES2Interface* gl = context_provider_->ContextGL();
 
   class GrContext* gr_context = context_provider_->GrContext();
+  // TODO(sohanjg): Remove when TestContextProvider gives a GrContext.
   if (gr_context)
     gr_context->flush();
 
