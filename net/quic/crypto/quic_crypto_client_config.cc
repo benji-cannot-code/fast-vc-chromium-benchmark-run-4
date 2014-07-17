6f886a46521a883f74a57f08a79351fd345792b0
rtenneti@chromium.org <rtenneti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-17 18:06:57 +0000

    QUIC - Record reject reasons for CHLO message.
    Added "Net.QuicClientHelloRejectReasons" histogram to track 
    the reject reason in UMA. Currently it tracks only reject 
    reason. 
    
    Once we have more data, will add the multiple reject reasons
    to histograms.xml (instead of listing all potential 
    combinations). We are guessing, that we would have few 
    failures due to multiple errors.
    
    R=jar@chromium.org, rch@chromium.org, asvitkine@chromium.org, wtc@chromium.org
    
    Review URL: https://codereview.chromium.org/342863005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283816 0039d316-1c4b-4281-b951-d872f2087c98

--- net/quic/crypto/quic_crypto_client_config.cc
+++ net/quic/crypto/quic_crypto_client_config.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/quic/crypto/quic_crypto_client_config.h"
 
+#include "base/metrics/sparse_histogram.h"
 #include "base/stl_util.h"
 #include "base/strings/string_util.h"
 #include "net/quic/crypto/cert_compressor.h"
@@ -593,7 +594,6 @@ QuicErrorCode QuicCryptoClientConfig::ProcessRejection(
   COMPILE_ASSERT(sizeof(QuicTag) == sizeof(uint32), header_out_of_sync);
   if (rej.GetTaglist(kRREJ, &reject_reasons,
                      &num_reject_reasons) == QUIC_NO_ERROR) {
-#if defined(DEBUG)
     uint32 packed_error = 0;
     for (size_t i = 0; i < num_reject_reasons; ++i) {
       // HANDSHAKE_OK is 0 and don't report that as error.
@@ -605,7 +605,8 @@ QuicErrorCode QuicCryptoClientConfig::ProcessRejection(
       packed_error |= 1 << (reason - 1);
     }
     DVLOG(1) << "Reasons for rejection: " << packed_error;
-#endif
+    UMA_HISTOGRAM_SPARSE_SLOWLY("Net.QuicClientHelloRejectReasons",
+                                packed_error);
   }
 
   return QUIC_NO_ERROR;
