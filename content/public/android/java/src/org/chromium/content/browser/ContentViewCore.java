a431eb9508d64c9384c2331cbb3c544bf41242b1
ajith.v@samsung.com <ajith.v@samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-17 18:17:51 +0000

    Hide the insertion handle when performing a copy
    Make behavior uniform with other apps on Android, where after a copy from
    an editable field, the insertion handle is hidden and only the cursor
    remains visible.
    
    BUG=394773
    
    Review URL: https://codereview.chromium.org/394953005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283818 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -2192,6 +2192,7 @@ public class ContentViewCore
             public void onDestroyActionMode() {
                 mActionMode = null;
                 if (mUnselectAllOnActionModeDismiss) {
+                    hideHandles();
                     if (isSelectionEditable()) {
                         int selectionEnd = Selection.getSelectionEnd(mEditable);
                         mInputConnection.setSelection(selectionEnd, selectionEnd);
