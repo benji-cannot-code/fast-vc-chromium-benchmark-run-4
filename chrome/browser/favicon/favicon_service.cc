784394387ed4c57fc69059563344352d0fe721fb
jif@chromium.org <jif@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-18 14:20:20 +0000

    Revert of Bypass the favicon_service when importing favicons. (https://codereview.chromium.org/375613002/)
    Reason for revert:
    There's a better approach.
    
    Original issue's description:
    > Bypass the favicon_service when importing favicons.
    > 
    > BUG=391795
    > TBR=gab
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=284045
    
    TBR=blundell@chromium.org,jif@google.com,gab@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=391795
    
    Review URL: https://codereview.chromium.org/407513002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@284092 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/favicon/favicon_service.cc
+++ chrome/browser/favicon/favicon_service.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/history/history_service.h"
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/ui/webui/chrome_web_ui_controller_factory.h"
+#include "chrome/common/importer/imported_favicon_usage.h"
 #include "chrome/common/url_constants.h"
 #include "components/favicon_base/favicon_types.h"
 #include "components/favicon_base/favicon_util.h"
@@ -281,6 +282,12 @@ void FaviconService::CloneFavicon(const GURL& old_page_url,
                                   const GURL& new_page_url) {
   if (history_service_)
     history_service_->CloneFavicons(old_page_url, new_page_url);
+}
+
+void FaviconService::SetImportedFavicons(
+    const std::vector<ImportedFaviconUsage>& favicon_usage) {
+  if (history_service_)
+    history_service_->SetImportedFavicons(favicon_usage);
 }
 
 void FaviconService::MergeFavicon(
