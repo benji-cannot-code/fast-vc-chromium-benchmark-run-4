caa3cf74cffd13b26e6dc073c8aa5df831934f59
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-18 18:02:29 +0000

    Drop time from media::{Audio,Video}Renderer::StartPlayingFrom().
    In preparation for migrating to TimeSource, StartPlayingFrom() is
    renamed to StartPlaying() with renderers expected to fetch the current
    time instead of being provided the time.
    
    AudioRenderer::SetMediaTime() is temporarily added until we can migrate
    the interface to being based on TimeSource.
    
    BUG=370634
    R=xhwang@chromium.org
    
    Review URL: https://codereview.chromium.org/400853002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@284155 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/video_renderer_impl.h
+++ media/filters/video_renderer_impl.h
@@ -69,7 +69,7 @@ class MEDIA_EXPORT VideoRendererImpl
                           const TimeDeltaCB& get_time_cb,
                           const TimeDeltaCB& get_duration_cb) OVERRIDE;
   virtual void Flush(const base::Closure& callback) OVERRIDE;
-  virtual void StartPlayingFrom(base::TimeDelta timestamp) OVERRIDE;
+  virtual void StartPlaying() OVERRIDE;
   virtual void Stop(const base::Closure& callback) OVERRIDE;
 
   // PlatformThread::Delegate implementation.
@@ -156,7 +156,7 @@ class MEDIA_EXPORT VideoRendererImpl
   //         |
   //         V            Decoders reset
   //      kFlushed <------------------ kFlushing
-  //         | StartPlayingFrom()         ^
+  //         | StartPlaying()             ^
   //         |                            |
   //         |                            | Flush()
   //         `---------> kPlaying --------'
