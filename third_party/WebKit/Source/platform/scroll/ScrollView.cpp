37b365612bb329f8cb1901f2fcbde896c165a258
ccameron@chromium.org <ccameron@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-18 08:41:45 +0000

    Update scrollbar layer opacity when scrollbar theme changes
    If the scrollbar theme changes from being opaque to non-opaque (as happen
    when unplugging an external mouse on Mac), a situation results where the
    scrollbar layers think they were opaque (because they were when they were
    created), but the content that they draw assumes that it is not opaque, and
    black scrollbars result.
    
    BUG=383205
    
    Review URL: https://codereview.chromium.org/383603002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178421 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/scroll/ScrollView.cpp
+++ third_party/WebKit/Source/platform/scroll/ScrollView.cpp
@@ -241,6 +241,18 @@ IntPoint ScrollView::adjustScrollPositionWithinRange(const IntPoint& scrollPoint
     return newScrollPosition;
 }
 
+void ScrollView::adjustScrollbarOpacity()
+{
+    if (m_horizontalScrollbar && layerForHorizontalScrollbar()) {
+        bool isOpaqueScrollbar = !m_horizontalScrollbar->isOverlayScrollbar();
+        layerForHorizontalScrollbar()->setContentsOpaque(isOpaqueScrollbar);
+    }
+    if (m_verticalScrollbar && layerForVerticalScrollbar()) {
+        bool isOpaqueScrollbar = !m_verticalScrollbar->isOverlayScrollbar();
+        layerForVerticalScrollbar()->setContentsOpaque(isOpaqueScrollbar);
+    }
+}
+
 int ScrollView::scrollSize(ScrollbarOrientation orientation) const
 {
     Scrollbar* scrollbar = ((orientation == HorizontalScrollbar) ? m_horizontalScrollbar : m_verticalScrollbar).get();
@@ -813,6 +825,7 @@ bool ScrollView::isScrollCornerVisible() const
 
 void ScrollView::scrollbarStyleChanged()
 {
+    adjustScrollbarOpacity();
     contentsResized();
     updateScrollbars(scrollOffset());
     positionScrollbarLayers();
