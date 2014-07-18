e1aa26cb310d142c7cb3d26765912ac6ff67c749
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-18 01:40:47 +0000

    Fixes for re-enabling more MSVC level 4 warnings: misc edition #2
    This contains fixes for the following sorts of issues:
    * Assignment inside conditional
    * Taking the address of a temporary
    * Octal escape sequence terminated by decimal number
    * Signedness mismatch
    * Possibly-uninitialized local variable
    
    This also contains a small number of cleanups to nearby code (e.g. no else after return).
    
    BUG=81439
    TEST=none
    
    Review URL: https://codereview.chromium.org/382673002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283967 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/drag_drop/drag_drop_controller.cc
+++ ash/drag_drop/drag_drop_controller.cc
@@ -245,11 +245,12 @@ int DragDropController::StartDragAndDrop(
 
 void DragDropController::DragUpdate(aura::Window* target,
                                     const ui::LocatedEvent& event) {
-  aura::client::DragDropDelegate* delegate = NULL;
   int op = ui::DragDropTypes::DRAG_NONE;
   if (target != drag_window_) {
     if (drag_window_) {
-      if ((delegate = aura::client::GetDragDropDelegate(drag_window_)))
+      aura::client::DragDropDelegate* delegate =
+          aura::client::GetDragDropDelegate(drag_window_);
+      if (delegate)
         delegate->OnDragExited();
       if (drag_window_ != drag_source_window_)
         drag_window_->RemoveObserver(this);
@@ -258,7 +259,9 @@ void DragDropController::DragUpdate(aura::Window* target,
     // We are already an observer of |drag_source_window_| so no need to add.
     if (drag_window_ != drag_source_window_)
       drag_window_->AddObserver(this);
-    if ((delegate = aura::client::GetDragDropDelegate(drag_window_))) {
+    aura::client::DragDropDelegate* delegate =
+        aura::client::GetDragDropDelegate(drag_window_);
+    if (delegate) {
       ui::DropTargetEvent e(*drag_data_,
                             event.location(),
                             event.root_location(),
@@ -267,7 +270,9 @@ void DragDropController::DragUpdate(aura::Window* target,
       delegate->OnDragEntered(e);
     }
   } else {
-    if ((delegate = aura::client::GetDragDropDelegate(drag_window_))) {
+    aura::client::DragDropDelegate* delegate =
+        aura::client::GetDragDropDelegate(drag_window_);
+    if (delegate) {
       ui::DropTargetEvent e(*drag_data_,
                             event.location(),
                             event.root_location(),
@@ -299,7 +304,6 @@ void DragDropController::DragUpdate(aura::Window* target,
 void DragDropController::Drop(aura::Window* target,
                               const ui::LocatedEvent& event) {
   ash::Shell::GetInstance()->cursor_manager()->SetCursor(ui::kCursorPointer);
-  aura::client::DragDropDelegate* delegate = NULL;
 
   // We must guarantee that a target gets a OnDragEntered before Drop. WebKit
   // depends on not getting a Drop without DragEnter. This behavior is
@@ -308,7 +312,9 @@ void DragDropController::Drop(aura::Window* target,
     DragUpdate(target, event);
   DCHECK(target == drag_window_);
 
-  if ((delegate = aura::client::GetDragDropDelegate(target))) {
+  aura::client::DragDropDelegate* delegate =
+      aura::client::GetDragDropDelegate(target);
+  if (delegate) {
     ui::DropTargetEvent e(
         *drag_data_, event.location(), event.root_location(), drag_operation_);
     e.set_flags(event.flags());
