a26114416234cc391a960c080bd1cc2432f750ee
sdefresne@chromium.org <sdefresne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-18 12:57:01 +0000

    Change AndroidHistoryProviderService to use CancelableTaskTracker (5/6)
    Port UpdateHistoryAndBookmarks and UpdateSearchTerms to use a
    base::CancelableTaskTracker instead of a CancelableRequestConsumer.
    
    BUG=371818
    
    Review URL: https://codereview.chromium.org/368283003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@284069 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/android/android_history_provider_service_unittest.cc
+++ chrome/browser/history/android/android_history_provider_service_unittest.cc
@@ -117,10 +117,8 @@ class CallbackHelper : public base::RefCountedThreadSafe<CallbackHelper> {
     base::MessageLoop::current()->Quit();
   }
 
-  void OnUpdated(AndroidHistoryProviderService::Handle handle,
-                 bool success,
-                 int count) {
-    success_ = success;
+  void OnUpdated(int count) {
+    success_ = count != 0;
     count_ = count;
     base::MessageLoop::current()->Quit();
   }
@@ -199,9 +197,12 @@ TEST_F(AndroidHistoryProviderServiceTest, TestHistoryAndBookmark) {
   // Update the row.
   HistoryAndBookmarkRow update_row;
   update_row.set_visit_count(3);
-  service_->UpdateHistoryAndBookmarks(update_row, std::string(),
-      std::vector<base::string16>(), &cancelable_consumer_,
-      Bind(&CallbackHelper::OnUpdated, callback.get()));
+  service_->UpdateHistoryAndBookmarks(
+      update_row,
+      std::string(),
+      std::vector<base::string16>(),
+      Bind(&CallbackHelper::OnUpdated, callback.get()),
+      &cancelable_tracker_);
   base::MessageLoop::current()->Run();
   EXPECT_TRUE(callback->success());
   EXPECT_EQ(1, callback->count());
@@ -264,9 +265,11 @@ TEST_F(AndroidHistoryProviderServiceTest, TestSearchTerm) {
   // Update the row.
   SearchRow update_row;
   update_row.set_search_time(Time::Now());
-  service_->UpdateSearchTerms(update_row, std::string(),
-      std::vector<base::string16>(), &cancelable_consumer_,
-      Bind(&CallbackHelper::OnUpdated, callback.get()));
+  service_->UpdateSearchTerms(update_row,
+                              std::string(),
+                              std::vector<base::string16>(),
+                              Bind(&CallbackHelper::OnUpdated, callback.get()),
+                              &cancelable_tracker_);
   base::MessageLoop::current()->Run();
   EXPECT_TRUE(callback->success());
   EXPECT_EQ(1, callback->count());
