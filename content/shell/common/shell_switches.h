1ff3e80fc151190f852edb7b16bcaa06baf54d8d
kouhei@chromium.org <kouhei@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-18 12:09:32 +0000

    LeakDetector: Add commandline option to force crash when leak is found.
    This CL modifies the LeakDetector to force immediate crash when an
    optional commandline argument is specified to content_shell.
    
    Usage:
    $ content_shell --dump-render-tree --enable-leak-detection=crash-on-failure
    
    This enables us to use the ClusterFuzz infrastructure to investigate and
    minimize leaks, as the tool currently detects the test case pass/fail by
    seeing if the process crashes.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/405433011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@284064 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/common/shell_switches.h
+++ content/shell/common/shell_switches.h
@@ -15,6 +15,7 @@ extern const char kCheckLayoutTestSysDeps[];
 extern const char kContentBrowserTest[];
 extern const char kContentShellDataPath[];
 extern const char kCrashDumpsDir[];
+extern const char kCrashOnFailure[];
 extern const char kDumpRenderTree[];
 extern const char kEnableAccelerated2DCanvas[];
 extern const char kEnableFontAntialiasing[];
