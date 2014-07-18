cf6ea01c628b94c610e7026600f0f60aeea9645e
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-18 20:12:33 +0000

    clang/win: Make winstorage:: not ambiguous.
    stdafx.h declared a global winstorage namespace alias, this file
    declared a winstorage namespace alias in an unnamed namespace.
    clang correctly points out that
    
      error(clang): reference to 'winstorage' is ambiguous
      note(clang): candidate found by name lookup is 'winstorage'
      note(clang): candidate found by name lookup is '(anonymous namespace)::winstorage'
    
    Just remove the unneeded duplicate.
    
    BUG=82385
    TBR=ananta@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/393083006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@284190 0039d316-1c4b-4281-b951-d872f2087c98

--- win8/metro_driver/file_picker_ash.cc
+++ win8/metro_driver/file_picker_ash.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-namespace winstorage = ABI::Windows::Storage;
 typedef winfoundtn::Collections::IVector<HSTRING> StringVectorItf;
 
 // TODO(siggi): Complete this implementation and move it to a common place.
