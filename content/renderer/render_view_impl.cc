dd902500656591e38776034b86827e5920712bc2
miletus@chromium.org <miletus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-18 00:27:53 +0000

    Trace RenderViewImpl::OnResize
    The trace comes handy when unexpected resize message triggers a main
    frame.
    
    BUG=392652
    TEST=do a trace for scrolling a page on clank during which the top control
         becomes hidden, and found that at the end of scroll there is a
         RenderViewImpl::OnResize trace event.
    
    Review URL: https://codereview.chromium.org/403593003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283947 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -3312,6 +3312,7 @@ void RenderViewImpl::OnMoveOrResizeStarted() {
 }
 
 void RenderViewImpl::OnResize(const ViewMsg_Resize_Params& params) {
+  TRACE_EVENT0("renderer", "RenderViewImpl::OnResize");
   if (webview()) {
     webview()->hidePopups();
     if (send_preferred_size_changes_) {
