a8f200bd697ee118e190d2b6e5d874b6a8d52644
dgozman@chromium.org <dgozman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-18 10:38:21 +0000

    [DevTools] Move DevToolsWindow testing code to a separate class.
    This change:
    - allows for proper docking testing (including toolbox);
    - removes all test friends from DevToolsWindow;
    - unifies testing approach to use load callback instead of notifications;
    - adds simple toolbox tests.
    
    BUG=393566
    
    Review URL: https://codereview.chromium.org/395783002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@284050 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/devtools/devtools_window.h
+++ chrome/browser/devtools/devtools_window.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class Browser;
 class BrowserWindow;
+class DevToolsWindowTesting;
 class DevToolsEventForwarder;
 
 namespace content {
@@ -72,11 +73,6 @@ class DevToolsWindow : public DevToolsUIBindings::Delegate,
   static DevToolsWindow* OpenDevToolsWindow(
       content::RenderViewHost* inspected_rvh);
 
-  static DevToolsWindow* OpenDevToolsWindowForTest(
-      content::RenderViewHost* inspected_rvh, bool is_docked);
-  static DevToolsWindow* OpenDevToolsWindowForTest(
-      Browser* browser, bool is_docked);
-
   // Perform specified action for current WebContents inside a |browser|.
   // This may close currently open DevTools window.
   static DevToolsWindow* ToggleDevToolsWindow(
@@ -96,9 +92,6 @@ class DevToolsWindow : public DevToolsUIBindings::Delegate,
 
   static void InspectElement(
       content::RenderViewHost* inspected_rvh, int x, int y);
-
-  Browser* browser_for_test() { return browser_; }
-  content::WebContents* web_contents_for_test() { return main_web_contents_; }
 
   // Sets closure to be called after load is done. If already loaded, calls
   // closure immediately.
@@ -182,8 +175,7 @@ class DevToolsWindow : public DevToolsUIBindings::Delegate,
   static void OnPageCloseCanceled(content::WebContents* contents);
 
  private:
-  friend class DevToolsSanityTest;
-  friend class BrowserWindowControllerTest;
+  friend class DevToolsWindowTesting;
 
   // DevTools lifecycle typically follows this way:
   // - Toggle/Open: client call;
@@ -216,19 +208,22 @@ class DevToolsWindow : public DevToolsUIBindings::Delegate,
                                 content::RenderViewHost* inspected_rvh,
                                 bool shared_worker_frontend,
                                 bool external_frontend,
-                                bool can_dock);
+                                bool can_dock,
+                                const std::string& settings);
   static GURL GetDevToolsURL(Profile* profile,
                              const GURL& base_url,
                              bool shared_worker_frontend,
                              bool external_frontend,
-                             bool can_dock);
+                             bool can_dock,
+                             const std::string& settings);
   static DevToolsWindow* FindDevToolsWindow(content::DevToolsAgentHost*);
   static DevToolsWindow* AsDevToolsWindow(content::WebContents*);
   static DevToolsWindow* CreateDevToolsWindowForWorker(Profile* profile);
   static DevToolsWindow* ToggleDevToolsWindow(
       content::RenderViewHost* inspected_rvh,
       bool force_open,
-      const DevToolsToggleAction& action);
+      const DevToolsToggleAction& action,
+      const std::string& settings);
 
   static std::string GetDevToolsWindowPlacementPrefKey();
 
@@ -296,7 +291,6 @@ class DevToolsWindow : public DevToolsUIBindings::Delegate,
   void Show(const DevToolsToggleAction& action);
   void DoAction(const DevToolsToggleAction& action);
   void LoadCompleted();
-  void SetIsDockedAndShowImmediatelyForTest(bool is_docked);
   void UpdateBrowserToolbar();
   void UpdateBrowserWindow();
   content::WebContents* GetInspectedWebContents();
@@ -312,12 +306,12 @@ class DevToolsWindow : public DevToolsUIBindings::Delegate,
   const bool can_dock_;
   LifeStage life_stage_;
   DevToolsToggleAction action_on_load_;
-  bool ignore_set_is_docked_;
   DevToolsContentsResizingStrategy contents_resizing_strategy_;
   // True if we're in the process of handling a beforeunload event originating
   // from the inspected webcontents, see InterceptPageBeforeUnload for details.
   bool intercepted_page_beforeunload_;
   base::Closure load_completed_callback_;
+  base::Closure close_callback_;
 
   base::TimeTicks inspect_element_start_time_;
   scoped_ptr<DevToolsEventForwarder> event_forwarder_;
--- chrome/chrome_tests.gypi
+++ chrome/chrome_tests.gypi
@@ -1021,6 +1021,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/devtools/device/adb/mock_adb_server.h',
         'browser/devtools/device/port_forwarding_browsertest.cc',
         'browser/devtools/device/usb/android_usb_browsertest.cc',
+        'browser/devtools/devtools_window_testing.cc',
+        'browser/devtools/devtools_window_testing.h',
         'browser/devtools/devtools_sanity_browsertest.cc',
         'browser/dom_distiller/dom_distiller_viewer_source_browsertest.cc',
         'browser/dom_distiller/tab_utils_browsertest.cc',
