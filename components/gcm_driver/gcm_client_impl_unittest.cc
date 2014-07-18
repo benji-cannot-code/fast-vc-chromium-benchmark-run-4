85493f72681293f6dd091f236eeb00b522f60f08
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-18 02:18:55 +0000

    Leverage profile's http network session HttpAuthCache to support proxy auth.
    This allows the connect job to leverage any auth cache credentials the profile
    has, including those the user gets prompted for as part of loading a page
    while behind an authenticated proxy. GCM continues to use its own network
    session for creating the connections, but at connection time the HttpAuthCache
    is coped over from the profile's http network session (if available).
    
    BUG=385748
    
    Review URL: https://codereview.chromium.org/375663002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283973 0039d316-1c4b-4281-b951-d872f2087c98

--- components/gcm_driver/gcm_client_impl_unittest.cc
+++ components/gcm_driver/gcm_client_impl_unittest.cc
@@ -169,7 +169,8 @@ class FakeGCMInternalsBuilder : public GCMInternalsBuilder {
   virtual scoped_ptr<ConnectionFactory> BuildConnectionFactory(
       const std::vector<GURL>& endpoints,
       const net::BackoffEntry::Policy& backoff_policy,
-      scoped_refptr<net::HttpNetworkSession> network_session,
+      const scoped_refptr<net::HttpNetworkSession>& gcm_network_session,
+      const scoped_refptr<net::HttpNetworkSession>& http_network_session,
       net::NetLog* net_log,
       GCMStatsRecorder* recorder) OVERRIDE;
 
@@ -202,7 +203,8 @@ scoped_ptr<MCSClient> FakeGCMInternalsBuilder::BuildMCSClient(
 scoped_ptr<ConnectionFactory> FakeGCMInternalsBuilder::BuildConnectionFactory(
     const std::vector<GURL>& endpoints,
     const net::BackoffEntry::Policy& backoff_policy,
-    scoped_refptr<net::HttpNetworkSession> network_session,
+    const scoped_refptr<net::HttpNetworkSession>& gcm_network_session,
+    const scoped_refptr<net::HttpNetworkSession>& http_network_session,
     net::NetLog* net_log,
     GCMStatsRecorder* recorder) {
   return make_scoped_ptr<ConnectionFactory>(new FakeConnectionFactory());
--- google_apis/gcm/engine/connection_factory_impl_unittest.cc
+++ google_apis/gcm/engine/connection_factory_impl_unittest.cc
@@ -160,6 +160,7 @@ TestConnectionFactoryImpl::TestConnectionFactoryImpl(
                             net::BackoffEntry::Policy(),
                             NULL,
                             NULL,
+                            NULL,
                             &dummy_recorder_),
       connect_result_(net::ERR_UNEXPECTED),
       num_expected_attempts_(0),
