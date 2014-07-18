200876d6df2062133119ce324ce987068078d2dc
markdittmer@chromium.org <markdittmer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-18 10:29:20 +0000

    Prepare declarativeContent API for new script injection feature. Added Javascript types and functions, as well as C++ ContentAction that currently does nothing.
    BUG=377978
    
    Review URL: https://codereview.chromium.org/344433003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@284049 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/declarative_content/content_action.h
+++ chrome/browser/extensions/api/declarative_content/content_action.h
@@ -32,6 +32,7 @@ class ContentAction : public base::RefCounted<ContentAction> {
   // Type identifiers for concrete ContentActions.
   enum Type {
     ACTION_SHOW_PAGE_ACTION,
+    ACTION_REQUEST_CONTENT_SCRIPT,
   };
 
   struct ApplyInfo {
--- chrome/renderer/resources/extensions/declarative_content_custom_bindings.js
+++ chrome/renderer/resources/extensions/declarative_content_custom_bindings.js
@@ -61,6 +61,9 @@ binding.registerCustomHook( function(api) {
   declarativeContent.ShowPageAction = function(parameters) {
     setupInstance(this, parameters, 'ShowPageAction');
   };
+  declarativeContent.RequestContentScript = function(parameters) {
+    setupInstance(this, parameters, 'RequestContentScript');
+  };
 });
 
 exports.binding = binding.generate();
