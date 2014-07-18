82d7f90aea3ef4d9dd1db5d3b62793c8fcb90180
harpreet.sk@samsung.com <harpreet.sk@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-18 19:53:31 +0000

    flex shorthand incorrectly accepting 1 0% 1 (i.e. flex-basis in the middle)
    On flex items, specifying a flex shorthand with a flex basis in the
    middle of two flex factors, e.g. flex: 1 0% 1 is getting accepted in
    blink.
    
    According to http://www.w3.org/TR/css3-flexbox/#flex-property, the
    grammar of the flex shorthand is as follows:
    
    Value:	none | [ <‘flex-grow’> <‘flex-shrink’>? || <‘flex-basis’> ]
    
    It means that flex-grow and flex-shrink must appear together. Therefore flex
    basis in the middle of two flex factors is invalid.
    
    Currently blink consider flex: 1 0% 1 as valid, mapping the values as
     <flex-grow> <flex-basis> <flex-shrink>, i.e. flex: 1 0% 1 is equivalent
    to flex: 1 1 0%.
    
    This patch fixes this bug by adding an additional condition checking
    if flex-basis is in the middle or not in flex argument list
    while setting values for flex basis.
    
    BUG=380984
    
    Review URL: https://codereview.chromium.org/396263003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178479 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
@@ -5709,6 +5709,11 @@ bool CSSPropertyParser::parseReflect(CSSPropertyID propId, bool important)
     return true;
 }
 
+static bool isFlexBasisMiddleArg(double flexGrow, double flexShrink, double unsetValue, int argSize)
+{
+    return flexGrow != unsetValue && flexShrink == unsetValue &&  argSize == 3;
+}
+
 bool CSSPropertyParser::parseFlex(CSSParserValueList* args, bool important)
 {
     if (!args || !args->size() || args->size() > 3)
@@ -5731,7 +5736,7 @@ bool CSSPropertyParser::parseFlex(CSSParserValueList* args, bool important)
                 // We only allow 3 numbers without units if the last value is 0. E.g., flex:1 1 1 is invalid.
                 return false;
             }
-        } else if (!flexBasis && (arg->id == CSSValueAuto || validUnit(arg, FLength | FPercent | FNonNeg)))
+        } else if (!flexBasis && (arg->id == CSSValueAuto || (validUnit(arg, FLength | FPercent | FNonNeg) && !isFlexBasisMiddleArg(flexGrow, flexShrink, unsetValue, args->size()))))
             flexBasis = parseValidPrimitive(arg->id, arg);
         else {
             // Not a valid arg for flex.
