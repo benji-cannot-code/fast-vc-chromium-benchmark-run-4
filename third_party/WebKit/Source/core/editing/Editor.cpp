b87562557e20a3cb52098dd08fc395bbd27a90d7
sigbjornf@opera.com <sigbjornf@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-19 07:09:44 +0000

    Oilpan: tidy up transition type usage after r178508.
    The Image object is not on the heap, adjust transition type usage to
    reflect that. Not doing so lead to
    
      virtual/threaded/compositing/webgl/webgl-copy-image.html
    
    failing in Oilpan builds --
    
     http://test-results.appspot.com/dashboards/flakiness_dashboard.html#tests=virtual%2Fthreaded%2Fcompositing%2Fwebgl%2Fwebgl-copy-image.html
    
    TBR=haraken@chromium.org,oilpan-reviews@chromium.org
    BUG=392765
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/401013003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178514 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/Editor.cpp
+++ third_party/WebKit/Source/core/editing/Editor.cpp
@@ -451,7 +451,7 @@ static void writeImageNodeToPasteboard(Pasteboard* pasteboard, Node* node, const
     ASSERT(pasteboard);
     ASSERT(node);
 
-    RefPtrWillBeRawPtr<Image> image = imageFromNode(*node);
+    RefPtr<Image> image = imageFromNode(*node);
     if (!image.get())
         return;
 
