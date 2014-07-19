d864a23fdbd5ad40445a82c666414fce67d7ba25
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-19 00:54:51 +0000

    Refactor WebCrypto code.
    Split up the monstrously large platform_crypto_nss.cc, platform_crypto_openssl.cc into multiple files.
    
    -----------
    Overview:
    -----------
    
    * algorithm_implementation.h:
    
    This defines a base class AlgorithmImplementation, which has virtual methods for synchronous encrypt/decrypt/generatekey. All of the information about an algorithm is now encapsulated by an AlgorithmImplementation. So for instance the JWK specific knowledge, key usages for each key type are pulled into this interface.
    
    * algorithm_registry.cc:
    
    Contains a mapping from WebCryptoAlgorithmID --> AlgorithmImplementation, stored by a singleton.
    
    * algorithm_dispatch.cc:
    
    Given parameters from Blink, looks up the appropriate AlgorithmImplementation in the registry and dispatches the operation. Also implements wrap/unwrap in terms of encrypt/decrypt.
    
    * structured_clone.cc:
    
    Contains the code related to structured cloning (which still needs some cleanup, and is implemented in terms of import/export).
    
    * nss/*, openssl/*:
    
    Contains the AlgorithmImplementation concrete classes for each algorithm.
    
    This reorganization also unintentionally fixes a few bugs.
    
      * ExportKey() for spki/pkcs8/raw uses the already serialized key data rather than re-exporting
      * Some exception codes were fixed.
    
    BUG=389325,389342,389327,374912
    R=jochen@chromium.org, rsleevi@chromium.org
    
    Review URL: https://codereview.chromium.org/379383002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@284267 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ content/child/webcrypto/openssl/util_openssl.cc
@@ -0,0 +1,49 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/child/webcrypto/openssl/util_openssl.h"
+
+#include <openssl/evp.h>
+
+#include "content/child/webcrypto/crypto_data.h"
+#include "content/child/webcrypto/platform_crypto.h"
+#include "crypto/openssl_util.h"
+
+namespace content {
+
+namespace webcrypto {
+
+void PlatformInit() {
+  crypto::EnsureOpenSSLInit();
+}
+
+const EVP_MD* GetDigest(blink::WebCryptoAlgorithmId id) {
+  switch (id) {
+    case blink::WebCryptoAlgorithmIdSha1:
+      return EVP_sha1();
+    case blink::WebCryptoAlgorithmIdSha256:
+      return EVP_sha256();
+    case blink::WebCryptoAlgorithmIdSha384:
+      return EVP_sha384();
+    case blink::WebCryptoAlgorithmIdSha512:
+      return EVP_sha512();
+    default:
+      return NULL;
+  }
+}
+
+AlgorithmImplementation* CreatePlatformRsaOaepImplementation() {
+  // TODO(eroman):
+  return NULL;
+}
+
+AlgorithmImplementation* CreatePlatformRsaSsaImplementation() {
+  // TODO(eroman):
+  return NULL;
+}
+
+}  // namespace webcrypto
+
+}  // namespace content
