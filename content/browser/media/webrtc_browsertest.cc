d11b8bfc1323c3611fb49e1601ca8e8fde57ea9d
nick@chromium.org <nick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-19 04:43:13 +0000

    Repair WebRtcBrowserTest names in drmemory exclusions file (these tests changed names when they went from TEST_P to  TEST_F in r283700).
    Fix spelling error in three test names.
    
    Eliminate memory error suppressions that were put in as fixes:
    * Revert "[MemSheriff] Suppress unreadable mem access"
    * This reverts commit a922608663a6fd861aeff6fd2bc3984dcbda86db.
    * (aka svn r283943)
    
    * Revert "[MemSheriff] Disable failing tests"
    * This reverts commit 565a8552b8e194807358b33ff741ae55ac9da901.
    * (aka svn r284133)
    
    BUG=394957,395172,389132,389098
    
    Review URL: https://codereview.chromium.org/401073002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@284308 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/media/webrtc_browsertest.cc
+++ content/browser/media/webrtc_browsertest.cc
@@ -116,7 +116,8 @@ IN_PROC_BROWSER_TEST_F(WebRtcBrowserTest, MAYBE_CanSetupDefaultVideoCall) {
       "callAndExpectResolution({video: true}, 640, 480);");
 }
 
-IN_PROC_BROWSER_TEST_F(WebRtcBrowserTest, CanSetupVideoCallWith1To1AspecRatio) {
+IN_PROC_BROWSER_TEST_F(WebRtcBrowserTest,
+                       CanSetupVideoCallWith1To1AspectRatio) {
   const std::string javascript =
       "callAndExpectResolution({video: {mandatory: {minWidth: 320,"
       " maxWidth: 320, minHeight: 320, maxHeight: 320}}}, 320, 320);";
@@ -124,7 +125,7 @@ IN_PROC_BROWSER_TEST_F(WebRtcBrowserTest, CanSetupVideoCallWith1To1AspecRatio) {
 }
 
 IN_PROC_BROWSER_TEST_F(WebRtcBrowserTest,
-                       CanSetupVideoCallWith16To9AspecRatio) {
+                       CanSetupVideoCallWith16To9AspectRatio) {
   const std::string javascript =
       "callAndExpectResolution({video: {mandatory: {minWidth: 640,"
       " maxWidth: 640, minAspectRatio: 1.777}}}, 640, 360);";
@@ -132,7 +133,7 @@ IN_PROC_BROWSER_TEST_F(WebRtcBrowserTest,
 }
 
 IN_PROC_BROWSER_TEST_F(WebRtcBrowserTest,
-                       CanSetupVideoCallWith4To3AspecRatio) {
+                       CanSetupVideoCallWith4To3AspectRatio) {
   const std::string javascript =
       "callAndExpectResolution({video: {mandatory: {minWidth: 960,"
       "maxAspectRatio: 1.333}}}, 960, 720);";
