874bf454204d0a0786170c72036439ba99bceeab
reillyg@chromium.org <reillyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-19 05:11:08 +0000

    Remove HID devices not found during enumeration on Windows.
    Windows doesn't give us fine grained notifications for device add
    and remove so we must do a complete enumeration every time. This
    patch figures out which devices are no longer present in the
    enumeration and calls HidService::RemoveDevice.
    
    BUG=395272
    R=rockot@chromium.org
    
    Review URL: https://codereview.chromium.org/399313008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@284321 0039d316-1c4b-4281-b951-d872f2087c98

--- device/hid/hid_service.h
+++ device/hid/hid_service.h
@@ -51,6 +51,7 @@ class HidService : public base::MessageLoop::DestructionObserver {
 
   void AddDevice(const HidDeviceInfo& info);
   void RemoveDevice(const HidDeviceId& device_id);
+  const DeviceMap& GetDevicesNoEnumerate() const;
 
   base::ThreadChecker thread_checker_;
 
