9a8e5095e357d14d59e6bad8802e13f8375bec9b
sigbjornf@opera.com <sigbjornf@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-20 06:50:49 +0000

    Remove unused threading includes from FileReader.
    Tidy up after r178426 and remove now-unused includes.
    
    R=haraken@chromium.org
    BUG=394828
    
    Review URL: https://codereview.chromium.org/409433002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178525 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/fileapi/FileReader.cpp
+++ third_party/WebKit/Source/core/fileapi/FileReader.cpp
@@ -48,8 +48,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/CurrentTime.h"
 #include "wtf/Deque.h"
 #include "wtf/HashSet.h"
-#include "wtf/ThreadSpecific.h"
-#include "wtf/Threading.h"
 #include "wtf/text/CString.h"
 
 namespace blink {
