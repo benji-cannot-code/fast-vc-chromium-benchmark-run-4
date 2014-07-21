3485be7474df3116fab3b92ad52fb06212f76c3f
bw80.lee@samsung.com <bw80.lee@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-21 12:09:09 +0000

    Remove redundant forward declaration and header inclusions.
    Remove forward declaration from HTMLStyleElement.h - StyleSheet
    Remove header inclusions from HTMLStyleElement.cpp - ContextFeatures.h, StyleEngine.h, StyleSheetContents.h
    
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/405563002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178552 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLStyleElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLStyleElement.cpp
@@ -27,12 +27,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/HTMLNames.h"
 #include "core/css/MediaList.h"
-#include "core/css/StyleSheetContents.h"
-#include "core/dom/ContextFeatures.h"
 #include "core/dom/Document.h"
 #include "core/events/Event.h"
 #include "core/events/EventSender.h"
-#include "core/dom/StyleEngine.h"
 #include "core/dom/shadow/ShadowRoot.h"
 
 namespace blink {
