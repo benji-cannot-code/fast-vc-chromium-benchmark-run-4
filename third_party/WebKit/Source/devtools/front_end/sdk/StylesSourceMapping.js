8b8669f8bba066c9e01f6b43784fd39747bad3c7
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-21 11:23:38 +0000

    DevTools: Inject styleSheetId in WebInspector.CSSLocation constructor
    Style sheet ID is required for the live location construction, and it is
    always known at the point where CSSLocation instances are constructed.
    
    R=vsevik
    
    Review URL: https://codereview.chromium.org/404763002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178550 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sdk/StylesSourceMapping.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/StylesSourceMapping.js
@@ -73,7 +73,7 @@ WebInspector.StylesSourceMapping.prototype = {
      */
     uiLocationToRawLocation: function(uiSourceCode, lineNumber, columnNumber)
     {
-        return new WebInspector.CSSLocation(this._cssModel.target(), uiSourceCode.url || "", lineNumber, columnNumber);
+        return new WebInspector.CSSLocation(this._cssModel.target(), null, uiSourceCode.url || "", lineNumber, columnNumber);
     },
 
     /**
