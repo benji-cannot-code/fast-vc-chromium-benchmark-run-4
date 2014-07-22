ae577b0cf4acce873dbf29b2c0f3b3c024918a90
sigbjornf@opera.com <sigbjornf@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-22 13:45:38 +0000

    Oilpan: fix compilation after r178648.
    R=haraken@chromium.org
    BUG=
    NOTRY=true
    NOTREECHECKS=true
    
    Review URL: https://codereview.chromium.org/413463002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178651 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/canvas/WebGLFramebuffer.cpp
+++ third_party/WebKit/Source/core/html/canvas/WebGLFramebuffer.cpp
@@ -631,7 +631,9 @@ GLenum WebGLFramebuffer::getDrawBuffer(GLenum drawBuffer)
 
 void WebGLFramebuffer::trace(Visitor* visitor)
 {
+#if ENABLE(OILPAN)
     visitor->trace(m_attachments);
+#endif
     WebGLContextObject::trace(visitor);
 }
 
