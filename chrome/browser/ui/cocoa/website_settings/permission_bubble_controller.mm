7c8b7a20b6bd942f1233f8a380312333a2fdbc99
leng@chromium.org <leng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-22 01:03:04 +0000

    Re-anchors the permission bubble when its parent window has moved.
    BUG=381290
    
    Review URL: https://codereview.chromium.org/390553003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@284574 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/website_settings/permission_bubble_controller.mm
+++ chrome/browser/ui/cocoa/website_settings/permission_bubble_controller.mm
@@ -245,6 +245,11 @@ class MenuDelegate : public ui::SimpleMenuModel::Delegate {
     [self setShouldOpenAsKeyWindow:NO];
     [[self bubble] setArrowLocation:info_bubble::kTopLeft];
     bridge_ = bridge;
+    NSNotificationCenter* center = [NSNotificationCenter defaultCenter];
+    [center addObserver:self
+               selector:@selector(parentWindowDidMove:)
+                   name:NSWindowDidMoveNotification
+                 object:parentWindow];
   }
   return self;
 }
@@ -259,6 +264,11 @@ class MenuDelegate : public ui::SimpleMenuModel::Delegate {
 }
 
 - (void)parentWindowDidResize:(NSNotification*)notification {
+  DCHECK(bridge_);
+  [self setAnchorPoint:bridge_->GetAnchorPoint()];
+}
+
+- (void)parentWindowDidMove:(NSNotification*)notification {
   DCHECK(bridge_);
   [self setAnchorPoint:bridge_->GetAnchorPoint()];
 }
