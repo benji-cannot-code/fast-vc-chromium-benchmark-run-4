92dbf4184c6cf52e5e9fbb9098a5203eb773e8b8
limasdf@gmail.com <limasdf@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-22 18:03:04 +0000

    Use ExtensionRegistryObserver instead of DEPRECATED extension notify.
    BUG=354458
    
    Review URL: https://codereview.chromium.org/262223003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@284725 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/panels/panel.h
+++ chrome/browser/ui/panels/panel.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/scoped_observer.h"
 #include "base/strings/string16.h"
 #include "chrome/browser/command_updater.h"
 #include "chrome/browser/command_updater_delegate.h"
@@ -18,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/panels/panel_constants.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
+#include "extensions/browser/extension_registry_observer.h"
 #include "ui/base/base_window.h"
 #include "ui/gfx/image/image.h"
 #include "ui/gfx/rect.h"
@@ -37,6 +39,7 @@ struct NativeWebKeyboardEvent;
 
 namespace extensions {
 class Extension;
+class ExtensionRegistry;
 class WindowController;
 }
 
@@ -51,7 +54,8 @@ class WindowController;
 //   other Panels. For example deleting a panel would rearrange other panels.
 class Panel : public ui::BaseWindow,
               public CommandUpdaterDelegate,
-              public content::NotificationObserver {
+              public content::NotificationObserver,
+              public extensions::ExtensionRegistryObserver {
  public:
   enum ExpansionState {
     // The panel is fully expanded with both title-bar and the client-area.
@@ -145,6 +149,12 @@ class Panel : public ui::BaseWindow,
   virtual void Observe(int type,
                        const content::NotificationSource& source,
                        const content::NotificationDetails& details) OVERRIDE;
+
+  // extensions::ExtensionRegistryObserver implementation.
+  virtual void OnExtensionUnloaded(
+      content::BrowserContext* browser_context,
+      const extensions::Extension* extension,
+      extensions::UnloadedExtensionInfo::Reason reason) OVERRIDE;
 
   // Construct a native panel implementation.
   static NativePanel* CreateNativePanel(Panel* panel,
@@ -392,6 +402,11 @@ class Panel : public ui::BaseWindow,
   const SessionID session_id_;
   scoped_ptr<extensions::WindowController> extension_window_controller_;
   scoped_ptr<PanelHost> panel_host_;
+
+  // Listen to extension unloaded notification.
+  ScopedObserver<extensions::ExtensionRegistry,
+                 extensions::ExtensionRegistryObserver>
+      extension_registry_observer_;
 
   // Icon showed in the task bar.
   gfx::Image app_icon_;
