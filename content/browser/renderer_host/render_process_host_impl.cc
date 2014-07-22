896f9878218a6109fe3ae8d3b0d7afe2935c4d67
chrishtr@chromium.org <chrishtr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-22 02:10:36 +0000

    Cache the current WebPreferences on RenderViewHostImpl.
    This makes lookups of a WebPreferences field fast.
    In order do this, add code to listen to all preferences updates in order to recompute the cache.
    
    BUG=390799
    
    Review URL: https://codereview.chromium.org/373323003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@284592 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -2141,7 +2141,7 @@ void RenderProcessHostImpl::OnGpuSwitching() {
       continue;
 
     RenderViewHost* rvh = RenderViewHost::From(widget);
-    rvh->UpdateWebkitPreferences(rvh->GetWebkitPreferences());
+    rvh->OnWebkitPreferencesChanged();
   }
 }
 
