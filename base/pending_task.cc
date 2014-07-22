03f09c475a79279d556416e6b68434d82930c885
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-22 04:29:58 +0000

    This is jamesr@ code I am landing.
    On Windows the message pump code tried to manage the systemwide timer resolution to fire delayed tasks with better than 15ms resolution but it was buggy:
    
    1- A short task that was not followed by any other task will leave the systemwide timer pegged to 1ms
    
    2- After we decided to crank up the timer we would 'lease' the timer for 1 second, for no good reason.
    
    Both issues are detrimental to battery power.
    
    The source of both problems is that we tried to decide with incomplete information. This patch solves that by having 1 bit for each pending task that requires a high resolution timer and a sum of the number of tasks that require high res timers.
    
    BUG=153139
    TEST=included here, also see the bug for manual testing.
    
    Review URL: https://codereview.chromium.org/395913006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@284625 0039d316-1c4b-4281-b951-d872f2087c98

--- base/pending_task.cc
+++ base/pending_task.cc
@@ -9,19 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
-#if _MSC_VER >= 1700
-// This a temporary fix for compiling on VS2012. http://crbug.com/154744
-PendingTask::PendingTask() : sequence_num(-1), nestable(false) {
-}
-#endif
-
 PendingTask::PendingTask(const tracked_objects::Location& posted_from,
                          const base::Closure& task)
     : base::TrackingInfo(posted_from, TimeTicks()),
       task(task),
       posted_from(posted_from),
       sequence_num(0),
-      nestable(true) {
+      nestable(true),
+      is_high_res(false) {
 }
 
 PendingTask::PendingTask(const tracked_objects::Location& posted_from,
@@ -32,7 +27,8 @@ PendingTask::PendingTask(const tracked_objects::Location& posted_from,
       task(task),
       posted_from(posted_from),
       sequence_num(0),
-      nestable(nestable) {
+      nestable(nestable),
+      is_high_res(false) {
 }
 
 PendingTask::~PendingTask() {
