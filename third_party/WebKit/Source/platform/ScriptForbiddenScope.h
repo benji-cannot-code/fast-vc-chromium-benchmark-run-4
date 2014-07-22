b3dbf9d12db92dd18859411e2478ef78a71d9449
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-22 03:08:59 +0000

    Enable the AllowSuperUnsafeScript assert disabler in debug builds.
    Don't crash debug builds when there's an AllowSuperUnsafeScript assert
    disabler in place.
    
    R=haraken@chromium.org,abarth@chromium.org
    BUG=395164
    
    Review URL: https://codereview.chromium.org/402303003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178615 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/ScriptForbiddenScope.h
+++ third_party/WebKit/Source/platform/ScriptForbiddenScope.h
@@ -33,10 +33,8 @@ public:
     public:
         AllowSuperUnsafeScript();
         ~AllowSuperUnsafeScript();
-#if !ASSERT_ENABLED
     private:
         TemporaryChange<unsigned> m_change;
-#endif
     };
 
     static bool isScriptForbidden();
