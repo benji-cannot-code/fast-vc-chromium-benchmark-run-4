44e3cc5e6ae963291c2a683b776faee048c29563
mrunal.kapade@intel.com <mrunal.kapade@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-22 17:21:36 +0000

    Remove dependency of profile from TtsMessageFilter
    This change also removes redundant header includes
    for tts_message_filter.h and tts_controller.h.
    This is part of an effort to move TTS to content.
    
    BUG=347045
    
    Review URL: https://codereview.chromium.org/408123002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@284718 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/speech/tts_message_filter.cc
+++ chrome/browser/speech/tts_message_filter.cc
@@ -7,9 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/logging.h"
-#include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/speech/tts_controller.h"
-#include "chrome/browser/speech/tts_message_filter.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/render_process_host.h"
 
