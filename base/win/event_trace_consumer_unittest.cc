09b2c68dcec66ab706d0163095eade0153d31d9a
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-22 16:56:22 +0000

    clang/win: Fix calling delete[] on void* in base.
    Calling delete[] on void* is undefined behavior. Cast to the right
    type before calling delete[]. Fixes a clang warning.
    
    BUG=82385
    TBR=siggi
    
    Review URL: https://codereview.chromium.org/401423002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@284709 0039d316-1c4b-4281-b951-d872f2087c98

--- base/win/event_trace_consumer_unittest.cc
+++ base/win/event_trace_consumer_unittest.cc
@@ -46,7 +46,7 @@ class TestConsumer: public EtwTraceConsumerBase<TestConsumer> {
   void ClearQueue() {
     for (EventQueue::const_iterator it(events_.begin()), end(events_.end());
          it != end; ++it) {
-      delete[] it->MofData;
+      delete[] reinterpret_cast<char*>(it->MofData);
     }
 
     events_.clear();
