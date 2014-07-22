d2f7f7c3bca135580c9199f06c3027629fd284e0
stgao@chromium.org <stgao@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-22 21:19:30 +0000

    [Findit] Initiate Findit with a unittest skeleton.
    This is to create a sub-directory for Findit and add a unittest skeleton with a script to parse DEPS file in chromium.
    
    R=mbarbella@chromium.org
    
    Review URL: https://codereview.chromium.org/391173002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@284775 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/findit/run_all_tests.py
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env python
+# Copyright (c) 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import sys
+import unittest
+
+from chromium_deps_unittest import ChromiumDEPSTest
+
+
+if __name__ == '__main__':
+   all_tests_suite = unittest.defaultTestLoader.loadTestsFromModule(
+             sys.modules[__name__])
+   tests = unittest.TestSuite(all_tests_suite)
+   result = unittest.TextTestRunner(stream=sys.stdout, verbosity=2).run(tests)
+   sys.exit(len(result.failures) + len(result.errors))
