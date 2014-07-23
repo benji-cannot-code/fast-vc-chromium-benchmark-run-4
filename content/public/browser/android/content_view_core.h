c7d469a7b9a3c396647a210e15079a0f8e572ca9
powei@chromium.org <powei@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-23 06:27:56 +0000

    android: add ThumbnailCache
    The purpose of the thumbnail cache is to save resources and use 
    static image instead of live pages when contents of a tab are
    needed for the browser UI.
    
    There are three forms of caching:
      - High-res
      - Low-res
      - On-disk
    
    If a high-res thumbnail is not available, then a low-res thumbnail
    is used while the on-disk version (always high-res) is being read.
    
    Eviction from the high/low-res caches is determined by a
    priority-ordered list of tab ids that is updated by the user.
    
    Compression of the thumbnail bitmaps are supported through
    third_party/android_opengl/etc1.  The raw bitmaps are discarded
    once a compressed version is available.  Compressed bitmaps are
    written to disk and uploaded as UIResource.  Once both of tasks
    are complete, the compressed bitmap is also discarded.
    
    depends on: https://codereview.chromium.org/287593002/
    
    android= https://chrome-internal-review.googlesource.com/#/c/160907/
    
    BUG=341406, 357740
    
    Review URL: https://codereview.chromium.org/262543002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@284861 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/browser/android/content_view_core.h
+++ content/public/browser/android/content_view_core.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "content/common/content_export.h"
 #include "content/public/browser/navigation_controller.h"
+#include "third_party/skia/include/core/SkImageInfo.h"
 #include "ui/gfx/rect.h"
 
 class SkBitmap;
@@ -56,7 +57,7 @@ class CONTENT_EXPORT ContentViewCore {
   // success or not. The content is passed through the SkBitmap parameter.
   virtual void GetScaledContentBitmap(
       float scale,
-      jobject bitmap_config,
+      SkColorType color_type,
       gfx::Rect src_rect,
       const base::Callback<void(bool, const SkBitmap&)>& result_callback) = 0;
   virtual float GetDpiScale() const = 0;
