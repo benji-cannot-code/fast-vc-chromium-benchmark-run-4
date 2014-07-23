f82b6e45d989c13efc5f84820d92a0c5376e5548
flackr@chromium.org <flackr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-23 22:00:16 +0000

    Handle WebContents created for new windows and create activities for these.
    BUG=391473
    TEST=Follow a link from Gmail or hold shift when clicking a link. A new activity following the link is created.
    
    Review URL: https://codereview.chromium.org/398603002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@285049 0039d316-1c4b-4281-b951-d872f2087c98

--- athena/content/web_activity.h
+++ athena/content/web_activity.h
@@ -30,6 +30,7 @@ class WebActivity : public Activity,
                     public content::WebContentsObserver {
  public:
   WebActivity(content::BrowserContext* context, const GURL& gurl);
+  WebActivity(AthenaWebView* web_view);
   virtual ~WebActivity();
 
  protected:
