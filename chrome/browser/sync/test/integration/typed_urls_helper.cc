6f84ed3f0717425c077fa7e1f49ce5937277f5c1
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-23 16:53:42 +0000

    Make HistoryDBTask not refcounted, and ensure it's destroyed on its origin thread.
    BUG=393149
    
    Review URL: https://codereview.chromium.org/387923002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@284958 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/test/integration/typed_urls_helper.cc
+++ chrome/browser/sync/test/integration/typed_urls_helper.cc
@@ -146,7 +146,10 @@ void WaitForHistoryDBThread(int index) {
   HistoryService* service = HistoryServiceFactory::GetForProfileWithoutCreating(
       test()->GetProfile(index));
   base::WaitableEvent wait_event(true, false);
-  service->ScheduleDBTask(new FlushHistoryDBQueueTask(&wait_event), &tracker);
+  service->ScheduleDBTask(
+      scoped_ptr<history::HistoryDBTask>(
+          new FlushHistoryDBQueueTask(&wait_event)),
+      &tracker);
   wait_event.Wait();
 }
 
@@ -173,7 +176,10 @@ history::URLRows GetTypedUrlsFromHistoryService(HistoryService* service) {
   base::CancelableTaskTracker tracker;
   history::URLRows rows;
   base::WaitableEvent wait_event(true, false);
-  service->ScheduleDBTask(new GetTypedUrlsTask(&rows, &wait_event), &tracker);
+  service->ScheduleDBTask(
+      scoped_ptr<history::HistoryDBTask>(
+          new GetTypedUrlsTask(&rows, &wait_event)),
+      &tracker);
   wait_event.Wait();
   return rows;
 }
@@ -183,8 +189,10 @@ bool GetUrlFromHistoryService(HistoryService* service,
   base::CancelableTaskTracker tracker;
   base::WaitableEvent wait_event(true, false);
   bool found = false;
-  service->ScheduleDBTask(new GetUrlTask(url, row, &found, &wait_event),
-                          &tracker);
+  service->ScheduleDBTask(
+      scoped_ptr<history::HistoryDBTask>(
+          new GetUrlTask(url, row, &found, &wait_event)),
+      &tracker);
   wait_event.Wait();
   return found;
 }
@@ -194,8 +202,10 @@ history::VisitVector GetVisitsFromHistoryService(HistoryService* service,
   base::CancelableTaskTracker tracker;
   base::WaitableEvent wait_event(true, false);
   history::VisitVector visits;
-  service->ScheduleDBTask(new GetVisitsTask(id, &visits, &wait_event),
-                          &tracker);
+  service->ScheduleDBTask(
+      scoped_ptr<history::HistoryDBTask>(
+          new GetVisitsTask(id, &visits, &wait_event)),
+      &tracker);
   wait_event.Wait();
   return visits;
 }
@@ -204,7 +214,10 @@ void RemoveVisitsFromHistoryService(HistoryService* service,
                                     const history::VisitVector& visits) {
   base::CancelableTaskTracker tracker;
   base::WaitableEvent wait_event(true, false);
-  service->ScheduleDBTask(new RemoveVisitsTask(visits, &wait_event), &tracker);
+  service->ScheduleDBTask(
+      scoped_ptr<history::HistoryDBTask>(
+          new RemoveVisitsTask(visits, &wait_event)),
+      &tracker);
   wait_event.Wait();
 }
 
--- chrome/browser/ui/webui/signin/inline_login_handler_impl.cc
+++ chrome/browser/ui/webui/signin/inline_login_handler_impl.cc
@@ -39,8 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-class InlineSigninHelper : public SigninOAuthHelper,
-                           public SigninOAuthHelper::Consumer {
+class InlineSigninHelper : public SigninOAuthHelper::Consumer {
  public:
   InlineSigninHelper(
       base::WeakPtr<InlineLoginHandlerImpl> handler,
@@ -62,6 +61,7 @@ class InlineSigninHelper : public SigninOAuthHelper,
   virtual void OnSigninOAuthInformationFailure(
       const GoogleServiceAuthError& error) OVERRIDE;
 
+  SigninOAuthHelper signin_oauth_helper_;
   base::WeakPtr<InlineLoginHandlerImpl> handler_;
   Profile* profile_;
   GURL current_url_;
@@ -83,7 +83,8 @@ InlineSigninHelper::InlineSigninHelper(
     const std::string& session_index,
     const std::string& signin_scoped_device_id,
     bool choose_what_to_sync)
-    : SigninOAuthHelper(getter, session_index, signin_scoped_device_id, this),
+    : signin_oauth_helper_(getter, session_index, signin_scoped_device_id,
+                           this),
       handler_(handler),
       profile_(profile),
       current_url_(current_url),
