66b475925ea2fb41d066d2ff080d8e4786f50a25
jinho.bang@samsung.com <jinho.bang@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-23 10:30:43 +0000

    Implement DOMPoint of geometry interfaces.
    The DOMPoint can represent a 2D point or 3D point.
    This will replace non-standard WebKitPoint in the future.
    
    This CL is dependent on:
    https://crrev.com/404473004/
    
    The specification:
    http://dev.w3.org/fxtf/geometry/
    
    Intent to Implement:
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/V_bJNtOg0oM
    
    BUG=388780
    
    Review URL: https://codereview.chromium.org/404803002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178747 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/Source/core/dom/DOMPoint.cpp
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "config.h"
+#include "core/dom/DOMPoint.h"
+
+namespace WebCore {
+
+DOMPoint* DOMPoint::create(const Dictionary& point)
+{
+    double x = 0;
+    double y = 0;
+    double z = 0;
+    double w = 0;
+
+    if (!DictionaryHelper::get(point, "x", x))
+        x = 0;
+    if (!DictionaryHelper::get(point, "y", y))
+        y = 0;
+    if (!DictionaryHelper::get(point, "z", z))
+        z = 0;
+    if (!DictionaryHelper::get(point, "w", w))
+        w = 1;
+
+    return new DOMPoint(x, y, z, w);
+}
+
+DOMPoint* DOMPoint::create(double x, double y, double z, double w)
+{
+    return new DOMPoint(x, y, z, w);
+}
+
+DOMPoint::DOMPoint(double x, double y, double z, double w)
+    : DOMPointReadOnly(x, y, z, w)
+{
+}
+
+} // namespace WebCore
---
+++ third_party/WebKit/Source/core/dom/DOMPoint.h
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef DOMPoint_h
+#define DOMPoint_h
+
+#include "bindings/core/v8/Dictionary.h"
+#include "core/dom/DOMPointReadOnly.h"
+
+namespace WebCore {
+
+class DOMPoint FINAL : public DOMPointReadOnly {
+public:
+    static DOMPoint* create(const Dictionary&);
+    static DOMPoint* create(double x, double y, double z = 0, double w = 1);
+
+    void setX(double x) { m_x = x; }
+    void setY(double y) { m_y = y; }
+    void setZ(double z) { m_z = z; }
+    void setW(double w) { m_w = w; }
+
+protected:
+    DOMPoint(double x, double y, double z, double w);
+};
+
+} // namespace WebCore
+
+#endif
