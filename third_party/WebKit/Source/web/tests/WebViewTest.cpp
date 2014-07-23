b1f87b1bd6721fa138d6d97f7cf1e8accaaa6483
sunil.ratnu@samsung.com <sunil.ratnu@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-23 05:46:57 +0000

    Cursor does not blink on longpress in an emptyField.
    When we long press on any input field containing any text,
    sendContextMenuEventForGesture gets called and context menu is displayed
    providing us the options to cut, copy, paste etc.
    
    But when we long press on any empty field, the caret blinking gets suspended and
    even after typing some text, the cursor remains in suspended state and unless we
    tap on the input field, it's not set back to false. So, in case of longpress
    scenario, it was never set back to false and due to this cursor does not blink
    for any operations which is done after longpress. So, we handle the case now and
    setCaretBlinkingSuspended to false when context menu event is sent.
    
    BUG=386331
    
    Review URL: https://codereview.chromium.org/364193002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178735 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/tests/WebViewTest.cpp
+++ third_party/WebKit/Source/web/tests/WebViewTest.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/dom/Document.h"
 #include "core/dom/Element.h"
+#include "core/editing/FrameSelection.h"
 #include "core/frame/EventHandlerRegistry.h"
 #include "core/frame/FrameView.h"
 #include "core/frame/LocalFrame.h"
@@ -1169,6 +1170,29 @@ TEST_F(WebViewTest, LongPressSelection)
     EXPECT_EQ("", std::string(frame->selectionAsText().utf8().data()));
     EXPECT_TRUE(tapElementById(webView, WebInputEvent::GestureLongPress, target));
     EXPECT_EQ("testword", std::string(frame->selectionAsText().utf8().data()));
+}
+
+TEST_F(WebViewTest, BlinkCaretOnTypingAfterLongPress)
+{
+    URLTestHelpers::registerMockedURLFromBaseURL(WebString::fromUTF8(m_baseURL.c_str()), WebString::fromUTF8("blink_caret_on_typing_after_long_press.html"));
+
+    WebView* webView = m_webViewHelper.initializeAndLoad(m_baseURL + "blink_caret_on_typing_after_long_press.html", true);
+    webView->resize(WebSize(640, 480));
+    webView->layout();
+    runPendingTasks();
+
+    WebString target = WebString::fromUTF8("target");
+    WebLocalFrameImpl* mainFrame = toWebLocalFrameImpl(webView->mainFrame());
+
+    EXPECT_TRUE(tapElementById(webView, WebInputEvent::GestureLongPress, target));
+    EXPECT_TRUE(mainFrame->frame()->selection().isCaretBlinkingSuspended());
+
+    WebKeyboardEvent keyEvent;
+    keyEvent.type = WebInputEvent::RawKeyDown;
+    webView->handleInputEvent(keyEvent);
+    keyEvent.type = WebInputEvent::KeyUp;
+    webView->handleInputEvent(keyEvent);
+    EXPECT_FALSE(mainFrame->frame()->selection().isCaretBlinkingSuspended());
 }
 #endif
 
