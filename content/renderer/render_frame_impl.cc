1e7aa078fb40e15d2b5330c257891bb918fa6813
mkwst@chromium.org <mkwst@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-23 14:41:47 +0000

    Get ResourceType from WebURLRequest rather than TargetType.
    'WebURLRequest::TargetType' has been dropped in Blink in favor of
    'RequestContext' and 'FrameType', matching the Fetch specification's
    changes, and allowing both more granularity in various Blink-side checks
    and more clarity around the thing that caused a request, and the frame
    that the request affects.
    
    This CL should have zero practical effect, as it's simply hoisting the
    translation work that 'WebURLRequest::targetType' is already doing
    up into the content layer to bring us one step closer to removing the
    concept entirely.
    
    BUG=390497
    
    Review URL: https://codereview.chromium.org/409593002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@284924 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -2628,7 +2628,7 @@ void RenderFrameImpl::didLoadResourceFromMemoryCache(
       response.securityInfo(),
       request.httpMethod().utf8(),
       response.mimeType().utf8(),
-      ResourceType::FromTargetType(request.targetType())));
+      ResourceType::FromWebURLRequest(request)));
 }
 
 void RenderFrameImpl::didDisplayInsecureContent(blink::WebLocalFrame* frame) {
