1ef144e3f9be1b332016778a905d274ba763858d
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-23 09:52:20 +0000

    VideoRenderer: Fires all pending callbacks in dtor.
    This make VideoRenderer consistent with the rest of the media pipeline.
    
    BUG=349211
    TEST=All existing tests pass.
    
    Review URL: https://codereview.chromium.org/414583002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@284898 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/video_renderer_impl_unittest.cc
+++ media/filters/video_renderer_impl_unittest.cc
@@ -27,10 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using ::testing::_;
 using ::testing::AnyNumber;
-using ::testing::AtLeast;
 using ::testing::Invoke;
 using ::testing::NiceMock;
-using ::testing::NotNull;
 using ::testing::Return;
 using ::testing::SaveArg;
 using ::testing::StrictMock;
@@ -358,13 +356,8 @@ TEST_F(VideoRendererImplTest, InitializeAndStartPlaying) {
   Destroy();
 }
 
-static void ExpectNotCalled(PipelineStatus) {
-  base::debug::StackTrace stack;
-  ADD_FAILURE() << "Expected callback not to be called\n" << stack.ToString();
-}
-
 TEST_F(VideoRendererImplTest, DestroyWhileInitializing) {
-  CallInitialize(base::Bind(&ExpectNotCalled), false, PIPELINE_OK);
+  CallInitialize(NewExpectedStatusCB(PIPELINE_ERROR_ABORT), false, PIPELINE_OK);
   Destroy();
 }
 
@@ -375,7 +368,7 @@ TEST_F(VideoRendererImplTest, DestroyWhileFlushing) {
   EXPECT_CALL(mock_cb_, BufferingStateChange(BUFFERING_HAVE_ENOUGH));
   StartPlaying();
   EXPECT_CALL(mock_cb_, BufferingStateChange(BUFFERING_HAVE_NOTHING));
-  renderer_->Flush(base::Bind(&ExpectNotCalled, PIPELINE_OK));
+  renderer_->Flush(NewExpectedClosure());
   Destroy();
 }
 
