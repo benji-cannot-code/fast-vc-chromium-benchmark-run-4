bfdf6706285d7da71938a27ce2aca2e165685027
japhet@chromium.org <japhet@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-23 19:21:41 +0000

    Make <a ping> work for context menus.
    This adds a link_followed parameter to CustomContextMenuContext. This object is sent back to the renderer process when a context menu is closed, allowing us to provide sufficient information to send pings if appropriate.
    
    BUG=257281
    
    Review URL: https://codereview.chromium.org/384243004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@285011 0039d316-1c4b-4281-b951-d872f2087c98

--- content/content_renderer.gypi
+++ content/content_renderer.gypi
@@ -189,6 +189,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'renderer/dom_storage/webstoragearea_impl.h',
       'renderer/dom_storage/webstoragenamespace_impl.cc',
       'renderer/dom_storage/webstoragenamespace_impl.h',
+      'renderer/dom_utils.cc',
+      'renderer/dom_utils.h',
       'renderer/drop_data_builder.cc',
       'renderer/drop_data_builder.h',
       'renderer/fetchers/image_resource_fetcher.cc',
--- content/renderer/context_menu_params_builder.cc
+++ content/renderer/context_menu_params_builder.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "content/common/ssl_status_serialization.h"
 #include "content/public/common/context_menu_params.h"
+#include "content/renderer/dom_utils.h"
 #include "content/renderer/history_serialization.h"
 #include "content/renderer/menu_item_builder.h"
 #include "third_party/WebKit/public/web/WebElement.h"
@@ -56,14 +57,7 @@ ContextMenuParams ContextMenuParamsBuilder::Build(
   }
 
   if (!params.link_url.is_empty()) {
-    blink::WebNode selectedNode = data.node;
-
-    // If there are other embedded tags (like <a ..>Some <b>text</b></a>)
-    // we need to extract the parent <a/> node.
-    while (!selectedNode.isLink() && !selectedNode.parentNode().isNull()) {
-      selectedNode = selectedNode.parentNode();
-    }
-
+    blink::WebNode selectedNode = DomUtils::ExtractParentAnchorNode(data.node);
     blink::WebElement selectedElement = selectedNode.to<blink::WebElement>();
     if (selectedNode.isLink() && !selectedElement.isNull()) {
       params.link_text = selectedElement.innerText();
