584faf918e18088f418863f4db7e87da7c87d0e4
sigbjornf@opera.com <sigbjornf@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-23 19:36:36 +0000

    Oilpan: fix WebSockets unit tests after r178758.
    Explicitly disconnect the channel on test completion, putting the heap
    object in the expected state by the time the next GC sweep happens.
    
    TBR=oilpan-reviews@chromium.org,tyoshino@chromium.org,yhirano@chromium.org,hiroshige@chromium.org
    BUG=
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/410263002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178778 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/websockets/NewWebSocketChannelImplTest.cpp
+++ third_party/WebKit/Source/modules/websockets/NewWebSocketChannelImplTest.cpp
@@ -94,7 +94,12 @@ public:
         ON_CALL(*channelClient(), didConsumeBufferedAmount(_)).WillByDefault(Invoke(this, &NewWebSocketChannelImplTest::didConsumeBufferedAmount));
     }
 
-    ~NewWebSocketChannelImplTest() { }
+    ~NewWebSocketChannelImplTest()
+    {
+#if ENABLE(OILPAN)
+        channel()->disconnect();
+#endif
+    }
 
     MockWebSocketChannelClient* channelClient()
     {
