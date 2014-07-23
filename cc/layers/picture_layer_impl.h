342b10872bac4b4e7c332a104750efa9ee1565ae
vmpstr@chromium.org <vmpstr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-23 20:57:14 +0000

    cc: Add const versions of operator* to layer iterators.
    This patch adds const versions of operator* for layer iterators, so
    that we can use them for sorting/heapifying the iterators.
    
    R=reveman
    
    Review URL: https://codereview.chromium.org/413603002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@285038 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/layers/picture_layer_impl.h
+++ cc/layers/picture_layer_impl.h
@@ -35,6 +35,7 @@ class CC_EXPORT PictureLayerImpl
     ~LayerRasterTileIterator();
 
     Tile* operator*();
+    const Tile* operator*() const;
     LayerRasterTileIterator& operator++();
     operator bool() const;
 
@@ -63,6 +64,7 @@ class CC_EXPORT PictureLayerImpl
     ~LayerEvictionTileIterator();
 
     Tile* operator*();
+    const Tile* operator*() const;
     LayerEvictionTileIterator& operator++();
     operator bool() const;
 
