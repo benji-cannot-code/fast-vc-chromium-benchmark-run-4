554f9fcb411c68ae9470c012d31c1fe5db22d961
sigbjornf@opera.com <sigbjornf@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-23 19:37:46 +0000

    LocalWindow::printErrorMessage(): check if frame is available.
    If there's no frame, there's no console to add message to either.
    
    R=haraken@chromium.org
    BUG=376704
    
    Review URL: https://codereview.chromium.org/413683002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178779 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/LocalDOMWindow.cpp
+++ third_party/WebKit/Source/core/frame/LocalDOMWindow.cpp
@@ -1724,6 +1724,9 @@ void LocalDOMWindow::setLocation(const String& urlString, LocalDOMWindow* callin
 
 void LocalDOMWindow::printErrorMessage(const String& message)
 {
+    if (!isCurrentlyDisplayedInFrame())
+        return;
+
     if (message.isEmpty())
         return;
 
