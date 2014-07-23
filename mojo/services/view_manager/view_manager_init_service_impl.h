db1e6ea46b9904a5f28efc18a236b85dd43465ef
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-23 16:43:43 +0000

    Add EmbedRoot to ViewManagerService.
    This means if you have an existing ViewManager connection you can just use that to ask the WM to embed a new node for you.
    
    Also fix the issue where only one call to EmbedRoot() could be made to the init service prior to the window tree host being ready.
    
    R=sky@chromium.org
    BUG=none
    
    Review URL: https://codereview.chromium.org/405163002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@284952 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/services/view_manager/view_manager_init_service_impl.h
+++ mojo/services/view_manager/view_manager_init_service_impl.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
+#include "base/memory/scoped_vector.h"
 #include "mojo/services/public/interfaces/view_manager/view_manager.mojom.h"
 #include "mojo/services/view_manager/root_node_manager.h"
 #include "mojo/services/view_manager/root_view_manager_delegate.h"
@@ -48,12 +49,11 @@ class MOJO_VIEW_MANAGER_EXPORT ViewManagerInitServiceImpl
     Callback<void(bool)> callback;
   };
 
-  void MaybeEmbedRoot(const std::string& url,
-                      const Callback<void(bool)>& callback);
+  void MaybeEmbed();
 
   // ViewManagerInitService overrides:
-  virtual void EmbedRoot(const String& url,
-                         const Callback<void(bool)>& callback) OVERRIDE;
+  virtual void Embed(const String& url,
+                     const Callback<void(bool)>& callback) OVERRIDE;
 
   // RootViewManagerDelegate overrides:
   virtual void OnRootViewManagerWindowTreeHostCreated() OVERRIDE;
@@ -64,8 +64,9 @@ class MOJO_VIEW_MANAGER_EXPORT ViewManagerInitServiceImpl
 
   RootNodeManager root_node_manager_;
 
-  // Parameters passed to Connect(). If non-null Connect() has been invoked.
-  scoped_ptr<ConnectParams> connect_params_;
+  // Stores information about inbound calls to Embed() pending execution on
+  // the window tree host being ready to use.
+  ScopedVector<ConnectParams> connect_params_;
 
   bool is_tree_host_ready_;
 
