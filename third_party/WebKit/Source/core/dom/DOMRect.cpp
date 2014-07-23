d3e0a7516e009b39a2392b86d8be912203d538f4
jinho.bang@samsung.com <jinho.bang@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-23 15:44:32 +0000

    Implement DOMRect of geometry interfaces.
    The DOMRect can represent a rectangle.
    
    The specification:
    http://dev.w3.org/fxtf/geometry/
    
    Intent to Implement:
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/V_bJNtOg0oM
    
    BUG=388780
    
    Review URL: https://codereview.chromium.org/403383002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178770 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/Source/core/dom/DOMRect.cpp
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "config.h"
+#include "core/dom/DOMRect.h"
+
+namespace WebCore {
+
+DOMRect* DOMRect::create(double x, double y, double width, double height)
+{
+    return new DOMRect(x, y, width, height);
+}
+
+DOMRect::DOMRect(double x, double y, double width, double height)
+    : DOMRectReadOnly(x, y, width, height)
+{
+}
+
+} // namespace WebCore
