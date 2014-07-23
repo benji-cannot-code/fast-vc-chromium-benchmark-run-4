5ed58d6bfc7faa3900be17c3905f440a1a338f1b
eakuefner@chromium.org <eakuefner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-23 08:45:15 +0000

    Add SkipValue to Telemetry.
    This adds logic for representing page skips as values in Telemetry, in the
    spirit of how failures are to be handled with regard to the linked bug. The
    goal for this CL is just to add the logic, and then once it's safely in the
    codebase, we can wire it up and make sure Buildbot is happy with it.
    
    BUG=392901
    
    Review URL: https://codereview.chromium.org/409023002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@284882 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/value/merge_values.py
+++ tools/telemetry/telemetry/value/merge_values.py
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 from telemetry.value import failure
+from telemetry.value import skip
 
 def MergeLikeValuesFromSamePage(all_values):
   """Merges values that measure the same thing on the same page.
@@ -113,9 +114,12 @@ def GroupStably(all_values, key_func):
   merge_groups_in_creation_order = []
   for value in all_values:
     # TODO(chrishenry): This is temporary. When we figure out the
-    # right summarization strategy for page runs with failures, we
+    # right summarization strategy for page runs with failures/skips, we
     # should use that instead.
-    if isinstance(value, failure.FailureValue):
+    should_skip_value = (isinstance(value, failure.FailureValue) or
+                         isinstance(value, skip.SkipValue))
+
+    if should_skip_value:
       continue
 
     key = key_func(value)
