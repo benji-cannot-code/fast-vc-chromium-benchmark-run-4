b5e0e512f763ef62a5c080f0cacaadd9d36ac08d
sayan.nayak@samsung.com <sayan.nayak@samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-23 21:34:08 +0000

    Hide the Paste popup when input is received from IME.
    Paste popup should be hidden from the corresponding
    input box whenever there is a change in focus.This
    change takes care of hiding the paste popup when
    focus is changing from edit field to URL bar.
    
    BUG=396620
    
    Review URL: https://codereview.chromium.org/416493003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@285047 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -1684,6 +1684,7 @@ public class ContentViewCore
         if (!gainFocus) {
             hideImeIfNeeded();
             cancelRequestToScrollFocusedEditableNodeIntoView();
+            hidePastePopup();
         }
         if (mNativeContentViewCore != 0) nativeSetFocus(mNativeContentViewCore, gainFocus);
     }
