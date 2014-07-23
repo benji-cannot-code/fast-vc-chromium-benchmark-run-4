992213efc2a0aac936b9c451faf3c6c6a20bb596
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-23 06:04:04 +0000

    aura: Use PlatformWindow from WindowTreeHostWin.
    With this patch, both Windows and Ozone implementations use PlatformWindow for
    interacting with the native windowing system. Eventually, the X11 implementations
    will also use this, and WindowTreeHost will have a single implementation on all
    platforms.
    
    BUG=none
    R=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/400413002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@284850 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/window_tree_host_ozone.cc
+++ ui/aura/window_tree_host_ozone.cc
@@ -57,6 +57,9 @@ void WindowTreeHostOzone::OnAcceleratedWidgetAvailable(
   CreateCompositor(widget_);
 }
 
+void WindowTreeHostOzone::OnActivationChanged(bool active) {
+}
+
 ui::EventSource* WindowTreeHostOzone::GetEventSource() {
   return this;
 }
