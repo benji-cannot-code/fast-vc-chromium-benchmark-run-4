29431bec2e057615d927c03d398042981f2dbc77
zeeshanq@chromium.org <zeeshanq@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-23 19:28:11 +0000

    Remove flag to disable cc touch hit testing.
    Compositor touch hit testing was re-enabled on Android webview back in February
    (https://codereview.chromium.org/183943002) and we haven't had any complaints
    since. It's probably safe to remove the flag and related code now.
    
    This is part 1 of 2, will be followed by:
    https://codereview.chromium.org/406053002
    
    BUG=304518
    
    Review URL: https://codereview.chromium.org/399953004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@285013 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1545,13 +1545,6 @@ const Experiment kExperiments[] = {
   },
 #endif
   {
-    "disable-compositor-touch-hit-testing",
-    IDS_FLAGS_DISABLE_COMPOSITOR_TOUCH_HIT_TESTING_NAME,
-    IDS_FLAGS_DISABLE_COMPOSITOR_TOUCH_HIT_TESTING_DESCRIPTION,
-    kOsAll,
-    SINGLE_VALUE_TYPE(cc::switches::kDisableCompositorTouchHitTesting),
-  },
-  {
     "enable-streamlined-hosted-apps",
     IDS_FLAGS_ENABLE_STREAMLINED_HOSTED_APPS_NAME,
     IDS_FLAGS_ENABLE_STREAMLINED_HOSTED_APPS_DESCRIPTION,
