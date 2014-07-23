0520962c0f008067e8b98c5b2b75bde966a1e1f1
ch.dumez@samsung.com <ch.dumez@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-23 00:08:09 +0000

    Stop passing dummy ParentDetails to NodeRenderingTraversal::parent()
    Stop passing dummy ParentDetails to NodeRenderingTraversal::parent(). It
    handles a null pointer just fine so just make the argument optional and
    default it to 0.
    
    R=esprehn@chromium.org
    
    Review URL: https://codereview.chromium.org/410733004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178712 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/resolver/StyleResolver.cpp
+++ third_party/WebKit/Source/core/css/resolver/StyleResolver.cpp
@@ -955,8 +955,7 @@ PassRefPtr<RenderStyle> StyleResolver::styleForText(Text* textNode)
 {
     ASSERT(textNode);
 
-    NodeRenderingTraversal::ParentDetails parentDetails;
-    Node* parentNode = NodeRenderingTraversal::parent(textNode, &parentDetails);
+    Node* parentNode = NodeRenderingTraversal::parent(textNode);
     if (!parentNode || !parentNode->renderStyle())
         return defaultStyleForElement();
     return parentNode->renderStyle();
--- third_party/WebKit/Source/core/dom/NodeRenderingTraversal.h
+++ third_party/WebKit/Source/core/dom/NodeRenderingTraversal.h
@@ -56,8 +56,7 @@ private:
     const InsertionPoint* m_insertionPoint;
 };
 
-ContainerNode* parent(const Node*);
-ContainerNode* parent(const Node*, ParentDetails*);
+ContainerNode* parent(const Node*, ParentDetails* = 0);
 bool contains(const ContainerNode*, const Node*);
 Node* nextSibling(const Node*);
 Node* previousSibling(const Node*);
@@ -66,12 +65,6 @@ Node* next(const Node*, const Node* stayWithin);
 RenderObject* nextSiblingRenderer(const Node*);
 RenderObject* previousSiblingRenderer(const Node*);
 RenderObject* nextInTopLayer(const Element*);
-
-inline ContainerNode* parent(const Node* node)
-{
-    ParentDetails unusedDetails;
-    return parent(node, &unusedDetails);
-}
 
 inline Element* parentElement(const Node* node)
 {
