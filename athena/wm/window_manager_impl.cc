ecf80885f4c87b6704f93780816946facec8eee7
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-24 23:01:41 +0000

    Revert 285393 "Exit overview mode when a new activity is opened ..."
    Broke HomeCardTest.Accelerators:
    http://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Tests%20%281%29/builds/46117
    
    > Exit overview mode when a new activity is opened in athena.
    > 
    > This has the side effect of fixing the crash in 396368
    > 
    > BUG=396368
    > TEST=Manual, see bug
    > 
    > Review URL: https://codereview.chromium.org/411813002
    
    TBR=pkotwicz@chromium.org
    
    Review URL: https://codereview.chromium.org/414133002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@285398 0039d316-1c4b-4281-b951-d872f2087c98

--- athena/wm/window_manager_impl.cc
+++ athena/wm/window_manager_impl.cc
@@ -40,9 +40,6 @@ class WindowManagerImpl : public WindowManager,
     COMMAND_TOGGLE_OVERVIEW,
   };
 
-  // Sets whether overview mode is active.
-  void SetInOverview(bool active);
-
   void InstallAccelerators();
 
   // WindowManager:
@@ -116,7 +113,6 @@ WindowManagerImpl::~WindowManagerImpl() {
 void WindowManagerImpl::Layout() {
   if (!container_)
     return;
-  SetInOverview(false);
   gfx::Rect bounds = gfx::Rect(container_->bounds().size());
   const aura::Window::Windows& children = container_->children();
   for (aura::Window::Windows::const_iterator iter = children.begin();
@@ -129,22 +125,14 @@ void WindowManagerImpl::Layout() {
 }
 
 void WindowManagerImpl::ToggleOverview() {
-  SetInOverview(overview_.get() == NULL);
-}
-
-void WindowManagerImpl::SetInOverview(bool active) {
-  bool in_overview = !!overview_;
-  if (active == in_overview)
-    return;
-
-  if (active) {
-    overview_ = WindowOverviewMode::Create(container_.get(), this);
-    FOR_EACH_OBSERVER(WindowManagerObserver, observers_,
-                      OnOverviewModeEnter());
-  } else {
+  if (overview_) {
     overview_.reset();
     FOR_EACH_OBSERVER(WindowManagerObserver, observers_,
                       OnOverviewModeExit());
+  } else {
+    overview_ = WindowOverviewMode::Create(container_.get(), this);
+    FOR_EACH_OBSERVER(WindowManagerObserver, observers_,
+                      OnOverviewModeEnter());
   }
 }
 
@@ -169,7 +157,9 @@ void WindowManagerImpl::OnSelectWindow(aura::Window* window) {
   CHECK_EQ(container_.get(), window->parent());
   container_->StackChildAtTop(window);
   wm::ActivateWindow(window);
-  SetInOverview(false);
+  overview_.reset();
+  FOR_EACH_OBSERVER(WindowManagerObserver, observers_,
+                    OnOverviewModeExit());
 }
 
 void WindowManagerImpl::OnWindowDestroying(aura::Window* window) {
