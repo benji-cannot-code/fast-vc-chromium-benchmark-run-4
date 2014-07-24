0311c324ee730f6ffbab81b693a4e7b7478da599
limasdf@gmail.com <limasdf@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-24 03:58:09 +0000

    Make AppSyncUIStateFactory depends on ExtensionRegistryFactory
    Now AppSyncUIState has ExtensionRegistry to listen extesnions's notification
    To make sure ExtesnionRegistry is destroyed after AppSyncUIState.
    AppSyncUIStateFactory should depends on ExtensionRegistryFactory.
    
    BUG=354046
    
    Review URL: https://codereview.chromium.org/411093002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@285150 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/ash/app_sync_ui_state_factory.cc
+++ chrome/browser/ui/ash/app_sync_ui_state_factory.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/profile_sync_service_factory.h"
 #include "chrome/browser/ui/ash/app_sync_ui_state.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
+#include "extensions/browser/extension_registry_factory.h"
 
 // static
 AppSyncUIState* AppSyncUIStateFactory::GetForProfile(Profile* profile) {
@@ -28,6 +29,7 @@ AppSyncUIStateFactory::AppSyncUIStateFactory()
     : BrowserContextKeyedServiceFactory(
         "AppSyncUIState",
         BrowserContextDependencyManager::GetInstance()) {
+  DependsOn(extensions::ExtensionRegistryFactory::GetInstance());
   DependsOn(ProfileSyncServiceFactory::GetInstance());
 }
 
