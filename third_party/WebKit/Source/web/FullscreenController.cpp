1b0b0c5ac760f31eff0ff83df51c2fb1102118fc
philipj@opera.com <philipj@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-24 10:40:24 +0000

    Remove WebSettings::setDisallowFullscreenForNonMediaElements(bool)
    It is never called from Chromium.
    
    Review URL: https://codereview.chromium.org/416013002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178831 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/FullscreenController.cpp
+++ third_party/WebKit/Source/web/FullscreenController.cpp
@@ -156,9 +156,6 @@ void FullscreenController::didExitFullScreen()
 
 void FullscreenController::enterFullScreenForElement(blink::Element* element)
 {
-    if (m_webViewImpl->settingsImpl()->disallowFullscreenForNonMediaElements() && !isHTMLMediaElement(element))
-        return;
-
     // We are already transitioning to fullscreen for a different element.
     if (m_provisionalFullScreenElement) {
         m_provisionalFullScreenElement = element;
