a6e2736528081249ff9c188cc747cafee40fb134
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-24 01:24:53 +0000

    Build fix after r178795
    TBR=tkent@chromium.org
    
    Review URL: https://codereview.chromium.org/416763002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178800 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/TreeScope.h
+++ third_party/WebKit/Source/core/dom/TreeScope.h
@@ -191,7 +191,7 @@ private:
 
     OwnPtrWillBeMember<IdTargetObserverRegistry> m_idTargetObserverRegistry;
 
-    OwnPtrWillBeMember<ScopedStyleResolver> m_scopedStyleResolver;
+    OwnPtr<ScopedStyleResolver> m_scopedStyleResolver;
 
     mutable RefPtrWillBeMember<DOMSelection> m_selection;
 };
