benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit b76ec08a529998d01ea51c9da86910e5962f8688

Implement NativeViewHostAura::InstallClip.
Despite the NOTIMPLEMENTED comment saying that this method is only
needed for performance, it causes bugs with aura windows in side
Bubbles which need to clip to their parent view rather than the parent
window.

This CL is based on https://codereview.chromium.org/30993004/ but strips
out the gravity code and simplifies the windowing code.

Since the window hierarchy of NPAPI plugins has changed due to this
patch, it was also necessary to modify WebContentsViewAura to reflect
this change.

BUG=377378

Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=279051

Review URL: https://codereview.chromium.org/317823002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279316 0039d316-1c4b-4281-b951-d872f2087c98

ui/views/controls/native/native_view_host_aura.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/native/native_view_host_aura.h"
 
 #include "base/logging.h"
+#include "ui/aura/client/aura_constants.h"
 #include "ui/aura/client/focus_client.h"
 #include "ui/aura/window.h"
 #include "ui/base/cursor/cursor.h"
@@ -17,31 +18,43 @@ namespace views {
 
 NativeViewHostAura::NativeViewHostAura(NativeViewHost* host)
     : host_(host),
-      installed_clip_(false) {
+      clipping_window_(NULL) {
+  clipping_window_.Init(aura::WINDOW_LAYER_NOT_DRAWN);
+  clipping_window_.set_owned_by_parent(false);
+  clipping_window_.SetName("NativeViewHostAuraClip");
+  clipping_window_.layer()->SetMasksToBounds(true);
+  clipping_window_.SetProperty(views::kHostViewKey, static_cast<View*>(host_));
 }
 
 NativeViewHostAura::~NativeViewHostAura() {
   if (host_->native_view()) {
-    host_->native_view()->ClearProperty(views::kHostViewKey);
     host_->native_view()->RemoveObserver(this);
+    host_->native_view()->ClearProperty(views::kHostViewKey);
+    host_->native_view()->ClearProperty(aura::client::kHostWindowKey);
+    clipping_window_.ClearProperty(views::kHostViewKey);
+    if (host_->native_view()->parent() == &clipping_window_)
+      clipping_window_.RemoveChild(host_->native_view());
   }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // NativeViewHostAura, NativeViewHostWrapper implementation:
-void NativeViewHostAura::NativeViewWillAttach() {
+void NativeViewHostAura::AttachNativeView() {
   host_->native_view()->AddObserver(this);
   host_->native_view()->SetProperty(views::kHostViewKey,
       static_cast<View*>(host_));
+  AddClippingWindow();
 }
 
 void NativeViewHostAura::NativeViewDetaching(bool destroyed) {
+  RemoveClippingWindow();
   if (!destroyed) {
-    host_->native_view()->ClearProperty(views::kHostViewKey);
     host_->native_view()->RemoveObserver(this);
+    host_->native_view()->ClearProperty(views::kHostViewKey);
+    host_->native_view()->ClearProperty(aura::client::kHostWindowKey);
     host_->native_view()->Hide();
     if (host_->native_view()->parent())
-      Widget::ReparentNativeView(host_->native_view(), NULL);
+      host_->native_view()->parent()->RemoveChild(host_->native_view());
   }
 }
 
@@ -49,9 +62,7 @@ void NativeViewHostAura::AddedToWidget() {
   if (!host_->native_view())
     return;
 
-  aura::Window* widget_window = host_->GetWidget()->GetNativeView();
-  if (host_->native_view()->parent() != widget_window)
-    widget_window->AddChild(host_->native_view());
+  AddClippingWindow();
   if (host_->IsDrawn())
     host_->native_view()->Show();
   else
@@ -62,28 +73,42 @@ void NativeViewHostAura::AddedToWidget() {
 void NativeViewHostAura::RemovedFromWidget() {
   if (host_->native_view()) {
     host_->native_view()->Hide();
+    host_->native_view()->ClearProperty(aura::client::kHostWindowKey);
     if (host_->native_view()->parent())
       host_->native_view()->parent()->RemoveChild(host_->native_view());
+    RemoveClippingWindow();
   }
 }
 
 void NativeViewHostAura::InstallClip(int x, int y, int w, int h) {
-  // Note that this does not pose a problem functionality wise - it might
-  // however pose a speed degradation if not implemented.
-  LOG(WARNING) << "NativeViewHostAura::InstallClip is not implemented yet.";
+  clip_rect_.reset(
+      new gfx::Rect(host_->ConvertRectToWidget(gfx::Rect(x, y, w, h))));
 }
 
 bool NativeViewHostAura::HasInstalledClip() {
-  return installed_clip_;
+  return clip_rect_;
 }
 
 void NativeViewHostAura::UninstallClip() {
-  installed_clip_ = false;
+  clip_rect_.reset();
 }
 
 void NativeViewHostAura::ShowWidget(int x, int y, int w, int h) {
-  // TODO: need to support fast resize.
-  host_->native_view()->SetBounds(gfx::Rect(x, y, w, h));
+  int width = w;
+  int height = h;
+  if (host_->fast_resize()) {
+    gfx::Point origin(x, y);
+    views::View::ConvertPointFromWidget(host_, &origin);
+    InstallClip(origin.x(), origin.y(), w, h);
+    width = host_->native_view()->bounds().width();
+    height = host_->native_view()->bounds().height();
+  }
+  clipping_window_.SetBounds(clip_rect_ ? *clip_rect_
+                                        : gfx::Rect(x, y, w, h));
+
+  gfx::Point clip_offset = clipping_window_.bounds().origin();
+  host_->native_view()->SetBounds(
+      gfx::Rect(x - clip_offset.x(), y - clip_offset.y(), width, height));
   host_->native_view()->Show();
 }
 
@@ -117,6 +142,42 @@ void NativeViewHostAura::OnWindowDestroyed(aura::Window* window) {
 NativeViewHostWrapper* NativeViewHostWrapper::CreateWrapper(
     NativeViewHost* host) {
   return new NativeViewHostAura(host);
+}
+
+void NativeViewHostAura::AddClippingWindow() {
+  RemoveClippingWindow();
+
+  gfx::Rect bounds = host_->native_view()->bounds();
+  if (host_->GetWidget()->GetNativeView()) {
+    Widget::ReparentNativeView(&clipping_window_,
+                               host_->GetWidget()->GetNativeView());
+  }
+  host_->native_view()->SetProperty(aura::client::kHostWindowKey,
+      host_->GetWidget()->GetNativeView());
+  Widget::ReparentNativeView(host_->native_view(),
+                             &clipping_window_);
+  clipping_window_.SetBounds(bounds);
+  bounds.set_origin(gfx::Point(0, 0));
+  host_->native_view()->SetBounds(bounds);
+  clipping_window_.Show();
+}
+
+void NativeViewHostAura::RemoveClippingWindow() {
+  if (host_->native_view())
+    host_->native_view()->ClearProperty(aura::client::kHostWindowKey);
+
+  if (host_->native_view()->parent() == &clipping_window_) {
+    if (host_->GetWidget()->GetNativeView()) {
+      Widget::ReparentNativeView(host_->native_view(),
+                                 host_->GetWidget()->GetNativeView());
+    } else {
+      clipping_window_.RemoveChild(host_->native_view());
+    }
+    host_->native_view()->SetBounds(clipping_window_.bounds());
+  }
+  clipping_window_.Hide();
+  if (clipping_window_.parent())
+    clipping_window_.parent()->RemoveChild(&clipping_window_);
 }
 
 }  // namespace views

ui/views/controls/native/native_view_host_aura_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/memory/scoped_ptr.h"
+#include "ui/aura/client/aura_constants.h"
 #include "ui/aura/window.h"
 #include "ui/base/cursor/cursor.h"
 #include "ui/views/controls/native/native_view_host.h"
@@ -16,6 +17,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/widget.h"
 
 namespace views {
+
+// Testing wrapper of the NativeViewHost
+class NativeViewHostTesting : public NativeViewHost {
+ public:
+  NativeViewHostTesting() {}
+  virtual ~NativeViewHostTesting() { destroyed_count_++; }
+
+  static void ResetDestroyedCount() { destroyed_count_ = 0; }
+
+  static int destroyed_count() { return destroyed_count_; }
+
+ private:
+  static int destroyed_count_;
+
+  DISALLOW_COPY_AND_ASSIGN(NativeViewHostTesting);
+};
+int NativeViewHostTesting::destroyed_count_ = 0;
 
 class NativeViewHostAuraTest : public ViewsTestBase {
  public:
@@ -38,6 +56,8 @@ class NativeViewHostAuraTest : public ViewsTestBase {
     return child_.get();
   }
 
+  aura::Window* clipping_window() { return &(native_host()->clipping_window_); }
+
   void CreateHost() {
     // Create the top level widget.
     toplevel_.reset(new Widget);
@@ -56,7 +76,7 @@ class NativeViewHostAuraTest : public ViewsTestBase {
     child_->SetContentsView(test_view);
 
     // Owned by |toplevel|.
-    host_.reset(new NativeViewHost);
+    host_.reset(new NativeViewHostTesting);
     toplevel_->GetRootView()->AddChildView(host_.get());
     host_->Attach(child_->GetNativeView());
   }
@@ -65,9 +85,13 @@ class NativeViewHostAuraTest : public ViewsTestBase {
     host_.reset();
   }
 
+  NativeViewHostTesting* ReleaseHost() { return host_.release(); }
+
+  void DestroyTopLevel() { toplevel_.reset(); }
+
  private:
   scoped_ptr<Widget> toplevel_;
-  scoped_ptr<NativeViewHost> host_;
+  scoped_ptr<NativeViewHostTesting> host_;
   scoped_ptr<Widget> child_;
 
   DISALLOW_COPY_AND_ASSIGN(NativeViewHostAuraTest);
@@ -90,15 +114,24 @@ TEST_F(NativeViewHostAuraTest, HostViewPropertyKey) {
   CreateHost();
   aura::Window* child_win = child()->GetNativeView();
   EXPECT_EQ(host(), child_win->GetProperty(views::kHostViewKey));
+  EXPECT_EQ(host()->GetWidget()->GetNativeView(),
+            child_win->GetProperty(aura::client::kHostWindowKey));
+  EXPECT_EQ(host(), clipping_window()->GetProperty(views::kHostViewKey));
 
   host()->Detach();
   EXPECT_FALSE(child_win->GetProperty(views::kHostViewKey));
+  EXPECT_FALSE(child_win->GetProperty(aura::client::kHostWindowKey));
+  EXPECT_TRUE(clipping_window()->GetProperty(views::kHostViewKey));
 
   host()->Attach(child_win);
   EXPECT_EQ(host(), child_win->GetProperty(views::kHostViewKey));
+  EXPECT_EQ(host()->GetWidget()->GetNativeView(),
+            child_win->GetProperty(aura::client::kHostWindowKey));
+  EXPECT_EQ(host(), clipping_window()->GetProperty(views::kHostViewKey));
 
   DestroyHost();
   EXPECT_FALSE(child_win->GetProperty(views::kHostViewKey));
+  EXPECT_FALSE(child_win->GetProperty(aura::client::kHostWindowKey));
 }
 
 // Tests that the NativeViewHost reports the cursor set on its native view.
@@ -111,6 +144,96 @@ TEST_F(NativeViewHostAuraTest, CursorForNativeView) {
                             gfx::Point(0, 0), 0, 0);
 
   EXPECT_EQ(ui::kCursorWait, host()->GetCursor(move_event).native_type());
+
+  DestroyHost();
+}
+
+// Test that destroying the top level widget before destroying the attached
+// NativeViewHost works correctly. Specifically the associated NVH should be
+// destroyed and there shouldn't be any errors.
+TEST_F(NativeViewHostAuraTest, DestroyWidget) {
+  NativeViewHostTesting::ResetDestroyedCount();
+  CreateHost();
+  ReleaseHost();
+  EXPECT_EQ(0, NativeViewHostTesting::destroyed_count());
+  DestroyTopLevel();
+  EXPECT_EQ(1, NativeViewHostTesting::destroyed_count());
+}
+
+// Test that the fast resize path places the clipping and content windows were
+// they are supposed to be.
+TEST_F(NativeViewHostAuraTest, FastResizePath) {
+  CreateHost();
+  toplevel()->SetBounds(gfx::Rect(20, 20, 100, 100));
+
+  // Without fast resize, the clipping window should size to the native view
+  // with the native view positioned at the origin of the clipping window and
+  // the clipping window positioned where the native view was requested.
+  host()->set_fast_resize(false);
+  native_host()->ShowWidget(5, 10, 100, 100);
+  EXPECT_EQ(gfx::Rect(0, 0, 100, 100).ToString(),
+            host()->native_view()->bounds().ToString());
+  EXPECT_EQ(gfx::Rect(5, 10, 100, 100).ToString(),
+            clipping_window()->bounds().ToString());
+
+  // With fast resize, the native view should remain the same size but be
+  // clipped the requested size.
+  host()->set_fast_resize(true);
+  native_host()->ShowWidget(10, 25, 50, 50);
+  EXPECT_EQ(gfx::Rect(0, 0, 100, 100).ToString(),
+            host()->native_view()->bounds().ToString());
+  EXPECT_EQ(gfx::Rect(10, 25, 50, 50).ToString(),
+            clipping_window()->bounds().ToString());
+
+  // Turning off fast resize should make the native view start resizing again.
+  host()->set_fast_resize(false);
+  native_host()->ShowWidget(10, 25, 50, 50);
+  EXPECT_EQ(gfx::Rect(0, 0, 50, 50).ToString(),
+            host()->native_view()->bounds().ToString());
+  EXPECT_EQ(gfx::Rect(10, 25, 50, 50).ToString(),
+            clipping_window()->bounds().ToString());
+
+  DestroyHost();
+}
+
+// Test installing and uninstalling a clip.
+TEST_F(NativeViewHostAuraTest, InstallClip) {
+  CreateHost();
+  toplevel()->SetBounds(gfx::Rect(20, 20, 100, 100));
+
+  // Without a clip, the clipping window should always be positioned at the
+  // requested coordinates with the native view positioned at the origin of the
+  // clipping window.
+  native_host()->ShowWidget(10, 20, 100, 100);
+  EXPECT_EQ(gfx::Rect(0, 0, 100, 100).ToString(),
+            host()->native_view()->bounds().ToString());
+  EXPECT_EQ(gfx::Rect(10, 20, 100, 100).ToString(),
+            clipping_window()->bounds().ToString());
+
+  // Clip to the bottom right quarter of the native view.
+  native_host()->InstallClip(60, 70, 50, 50);
+  native_host()->ShowWidget(10, 20, 100, 100);
+  EXPECT_EQ(gfx::Rect(-50, -50, 100, 100).ToString(),
+            host()->native_view()->bounds().ToString());
+  EXPECT_EQ(gfx::Rect(60, 70, 50, 50).ToString(),
+            clipping_window()->bounds().ToString());
+
+  // Clip to the center of the native view.
+  native_host()->InstallClip(35, 45, 50, 50);
+  native_host()->ShowWidget(10, 20, 100, 100);
+  EXPECT_EQ(gfx::Rect(-25, -25, 100, 100).ToString(),
+            host()->native_view()->bounds().ToString());
+  EXPECT_EQ(gfx::Rect(35, 45, 50, 50).ToString(),
+            clipping_window()->bounds().ToString());
+
+  // Uninstalling the clip should make the clipping window match the native view
+  // again.
+  native_host()->UninstallClip();
+  native_host()->ShowWidget(10, 20, 100, 100);
+  EXPECT_EQ(gfx::Rect(0, 0, 100, 100).ToString(),
+            host()->native_view()->bounds().ToString());
+  EXPECT_EQ(gfx::Rect(10, 20, 100, 100).ToString(),
+            clipping_window()->bounds().ToString());
 
   DestroyHost();
 }

content/browser/web_contents/web_contents_view_aura.cc
@@ -478,7 +478,7 @@ class WebContentsViewAura::WindowObserver
  public:
   explicit WindowObserver(WebContentsViewAura* view)
       : view_(view),
-        parent_(NULL) {
+        host_window_(NULL) {
     view_->window_->AddObserver(this);
 
 #if defined(OS_WIN)
@@ -491,12 +491,11 @@ class WebContentsViewAura::WindowObserver
     view_->window_->RemoveObserver(this);
     if (view_->window_->GetHost())
       view_->window_->GetHost()->RemoveObserver(this);
-    if (parent_)
-      parent_->RemoveObserver(this);
-
+    if (host_window_)
+      host_window_->RemoveObserver(this);
 #if defined(OS_WIN)
-    if (parent_) {
-      const aura::Window::Windows& children = parent_->children();
+    if (host_window_) {
+      const aura::Window::Windows& children = host_window_->children();
       for (size_t i = 0; i < children.size(); ++i)
         children[i]->RemoveObserver(this);
     }
@@ -520,19 +519,19 @@ class WebContentsViewAura::WindowObserver
   // going to be deprecated in a year, this is ok for now. The test for this is
   // PrintPreviewTest.WindowedNPAPIPluginHidden.
   virtual void OnWindowAdded(aura::Window* new_window) OVERRIDE {
-    if (new_window != view_->window_) {
+    if (!new_window->Contains(view_->window_.get())) {
       // Skip the case when the parent moves to the root window.
-      if (new_window != parent_) {
+      if (new_window != host_window_) {
         // Observe sibling windows of the WebContents, or children of the root
         // window.
-        if (new_window->parent() == parent_ ||
+        if (new_window->parent() == host_window_ ||
             new_window->parent() == view_->window_->GetRootWindow()) {
           new_window->AddObserver(this);
         }
       }
     }
 
-    if (new_window->parent() == parent_) {
+    if (new_window->parent() == host_window_) {
       UpdateConstrainedWindows(NULL);
     }
   }
@@ -548,7 +547,7 @@ class WebContentsViewAura::WindowObserver
   virtual void OnWindowVisibilityChanged(aura::Window* window,
                                          bool visible) OVERRIDE {
     if (window == view_->window_ ||
-        window->parent() == parent_ ||
+        window->parent() == host_window_ ||
         window->parent() == view_->window_->GetRootWindow()) {
       UpdateConstrainedWindows(NULL);
     }
@@ -559,12 +558,18 @@ class WebContentsViewAura::WindowObserver
                                      aura::Window* parent) OVERRIDE {
     if (window != view_->window_)
       return;
-    if (parent_)
-      parent_->RemoveObserver(this);
+
+    aura::Window* host_window =
+        window->GetProperty(aura::client::kHostWindowKey);
+    if (!host_window)
+      host_window = parent;
+
+    if (host_window_)
+      host_window_->RemoveObserver(this);
 
 #if defined(OS_WIN)
-    if (parent_) {
-      const aura::Window::Windows& children = parent_->children();
+    if (host_window_) {
+      const aura::Window::Windows& children = host_window_->children();
       for (size_t i = 0; i < children.size(); ++i)
         children[i]->RemoveObserver(this);
 
@@ -575,29 +580,29 @@ class WebContentsViewAura::WindowObserver
     }
 
     // When we get parented to the root window, the code below will watch the
-    // parent, aka root window. Since we already watch the root window on
+    // host window, aka root window. Since we already watch the root window on
     // Windows, unregister first so that the debug check doesn't fire.
-    if (parent && parent == window->GetRootWindow())
-      parent->RemoveObserver(this);
+    if (host_window && host_window == window->GetRootWindow())
+      host_window->RemoveObserver(this);
 
     // We need to undo the above if we were parented to the root window and then
     // got parented to another window. At that point, the code before the ifdef
     // would have stopped watching the root window.
     if (window->GetRootWindow() &&
-        parent != window->GetRootWindow() &&
+        host_window != window->GetRootWindow() &&
         !window->GetRootWindow()->HasObserver(this)) {
       window->GetRootWindow()->AddObserver(this);
     }
 #endif
 
-    parent_ = parent;
-    if (parent) {
-      parent->AddObserver(this);
+    host_window_ = host_window;
+    if (host_window) {
+      host_window->AddObserver(this);
 #if defined(OS_WIN)
-      if (parent != window->GetRootWindow()) {
-        const aura::Window::Windows& children = parent->children();
+      if (host_window != window->GetRootWindow()) {
+        const aura::Window::Windows& children = host_window->children();
         for (size_t i = 0; i < children.size(); ++i) {
-          if (children[i] != view_->window_)
+          if (!children[i]->Contains(view_->window_.get()))
             children[i]->AddObserver(this);
         }
       }
@@ -608,7 +613,7 @@ class WebContentsViewAura::WindowObserver
   virtual void OnWindowBoundsChanged(aura::Window* window,
                                      const gfx::Rect& old_bounds,
                                      const gfx::Rect& new_bounds) OVERRIDE {
-    if (window == parent_ || window == view_->window_) {
+    if (window == host_window_ || window == view_->window_) {
       SendScreenRects();
       if (view_->touch_editable_)
         view_->touch_editable_->UpdateEditingController();
@@ -616,6 +621,13 @@ class WebContentsViewAura::WindowObserver
     } else {
       UpdateConstrainedWindows(NULL);
 #endif
+    }
+  }
+
+  virtual void OnWindowDestroying(aura::Window* window) OVERRIDE {
+    if (window == host_window_) {
+      host_window_->RemoveObserver(this);
+      host_window_ = NULL;
     }
   }
 
@@ -639,8 +651,10 @@ class WebContentsViewAura::WindowObserver
       const aura::Window::Windows& root_children =
           window->GetRootWindow()->children();
       for (size_t i = 0; i < root_children.size(); ++i) {
-        if (root_children[i] != view_->window_ && root_children[i] != parent_)
+        if (root_children[i] != view_->window_ &&
+            root_children[i] != host_window_) {
           root_children[i]->RemoveObserver(this);
+        }
       }
 #endif
     }
@@ -671,10 +685,10 @@ class WebContentsViewAura::WindowObserver
       return;
 
     std::vector<gfx::Rect> constrained_windows;
-    if (parent_) {
-      const aura::Window::Windows& children = parent_->children();
+    if (host_window_) {
+      const aura::Window::Windows& children = host_window_->children();
       for (size_t i = 0; i < children.size(); ++i) {
-        if (children[i] != view_->window_ &&
+        if (!children[i]->Contains(view_->window_.get()) &&
             children[i] != exclude &&
             children[i]->IsVisible()) {
           constrained_windows.push_back(children[i]->GetBoundsInRootWindow());
@@ -700,9 +714,9 @@ class WebContentsViewAura::WindowObserver
 
   WebContentsViewAura* view_;
 
-  // We cache the old parent so that we can unregister when it's not the parent
-  // anymore.
-  aura::Window* parent_;
+  // The parent window that hosts the constrained windows. We cache the old host
+  // view so that we can unregister when it's not the parent anymore.
+  aura::Window* host_window_;
 
   DISALLOW_COPY_AND_ASSIGN(WindowObserver);
 };

ui/aura/client/aura_constants.cc
@@ -25,6 +25,7 @@ DEFINE_WINDOW_PROPERTY_KEY(bool, kCanMaximizeKey, false);
 DEFINE_WINDOW_PROPERTY_KEY(bool, kCanResizeKey, true);
 DEFINE_WINDOW_PROPERTY_KEY(bool, kConstrainedWindowKey, false);
 DEFINE_WINDOW_PROPERTY_KEY(bool, kDrawAttentionKey, false);
+DEFINE_WINDOW_PROPERTY_KEY(Window*, kHostWindowKey, NULL);
 DEFINE_WINDOW_PROPERTY_KEY(ui::ModalType, kModalKey, ui::MODAL_TYPE_NONE);
 // gfx::Rect object for RestoreBoundsKey property is owned by the window
 // and will be freed automatically.

ui/aura/client/aura_constants.h
@@ -39,6 +39,10 @@ AURA_EXPORT extern const WindowProperty<bool>* const kConstrainedWindowKey;
 // attention.
 AURA_EXPORT extern const aura::WindowProperty<bool>* const kDrawAttentionKey;
 
+// A property key to store the host window of a window. This lets
+// WebContentsViews find the windows that should constrain NPAPI plugins.
+AURA_EXPORT extern const WindowProperty<Window*>* const kHostWindowKey;
+
 // A property key to store the window modality.
 AURA_EXPORT extern const WindowProperty<ui::ModalType>* const kModalKey;
 

ui/views/controls/native/native_view_host.cc
@@ -39,8 +39,7 @@ void NativeViewHost::Attach(gfx::NativeView native_view) {
   // be seen as focused when the native view receives focus.
   if (!focus_view_)
     focus_view_ = this;
-  native_wrapper_->NativeViewWillAttach();
-  Widget::ReparentNativeView(native_view_, GetWidget()->GetNativeView());
+  native_wrapper_->AttachNativeView();
   Layout();
 
   Widget* widget = Widget::GetWidgetForNativeView(native_view);

ui/views/controls/native/native_view_host_aura.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
+#include "ui/aura/window.h"
 #include "ui/aura/window_observer.h"
 #include "ui/views/controls/native/native_view_host_wrapper.h"
 #include "ui/views/views_export.h"
@@ -24,7 +25,7 @@ class VIEWS_EXPORT NativeViewHostAura : public NativeViewHostWrapper,
   virtual ~NativeViewHostAura();
 
   // Overridden from NativeViewHostWrapper:
-  virtual void NativeViewWillAttach() OVERRIDE;
+  virtual void AttachNativeView() OVERRIDE;
   virtual void NativeViewDetaching(bool destroyed) OVERRIDE;
   virtual void AddedToWidget() OVERRIDE;
   virtual void RemovedFromWidget() OVERRIDE;
@@ -38,14 +39,27 @@ class VIEWS_EXPORT NativeViewHostAura : public NativeViewHostWrapper,
   virtual gfx::NativeCursor GetCursor(int x, int y) OVERRIDE;
 
  private:
+  friend class NativeViewHostAuraTest;
+
   // Overridden from aura::WindowObserver:
   virtual void OnWindowDestroyed(aura::Window* window) OVERRIDE;
+
+  // Reparents the native view with the clipping window existing between it and
+  // its old parent, so that the fast resize path works.
+  void AddClippingWindow();
+
+  // If the native view has been reparented via AddClippingWindow, this call
+  // undoes it.
+  void RemoveClippingWindow();
 
   // Our associated NativeViewHost.
   NativeViewHost* host_;
 
-  // Have we installed a clip region?
-  bool installed_clip_;
+  // Window that exists between the native view and the parent that allows for
+  // clipping to occur. This is positioned in the coordinate space of
+  // host_->GetWidget().
+  aura::Window clipping_window_;
+  scoped_ptr<gfx::Rect> clip_rect_;
 
   DISALLOW_COPY_AND_ASSIGN(NativeViewHostAura);
 };

commit 32a36ab20a4e52d9755cbc55bd5aa48f5b196f5d

Fix positioning of children of NativeViewHostAura
regressed in r279316

BUG=391316

Review URL: https://codereview.chromium.org/397293005

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@285168 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/views/autofill/autofill_popup_base_view_browsertest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/autofill/autofill_popup_view_delegate.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window.h"
+#include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -42,14 +43,16 @@ class AutofillPopupBaseViewTest : public InProcessBrowserTest {
   virtual ~AutofillPopupBaseViewTest() {}
 
   virtual void SetUpOnMainThread() OVERRIDE {
-    gfx::NativeWindow window = browser()->window()->GetNativeWindow();
+    gfx::NativeView native_view =
+        browser()->tab_strip_model()->GetActiveWebContents()->GetNativeView();
     EXPECT_CALL(mock_delegate_, container_view())
-        .WillRepeatedly(Return(window));
+        .WillRepeatedly(Return(native_view));
     EXPECT_CALL(mock_delegate_, ViewDestroyed());
 
-    view_ = new AutofillPopupBaseView(
-        &mock_delegate_,
-        views::Widget::GetWidgetForNativeWindow(window));
+    view_ =
+        new AutofillPopupBaseView(&mock_delegate_,
+                                  views::Widget::GetWidgetForNativeWindow(
+                                      browser()->window()->GetNativeWindow()));
   }
 
   void ShowView() {
@@ -127,6 +130,19 @@ IN_PROC_BROWSER_TEST_F(AutofillPopupBaseViewTest, DoubleClickTest) {
   // Ignore double clicks.
   mouse_down.SetClickCount(2);
   EXPECT_FALSE(static_cast<views::View*>(view_)->OnMousePressed(mouse_down));
+}
+
+// Regression test for crbug.com/391316
+IN_PROC_BROWSER_TEST_F(AutofillPopupBaseViewTest, CorrectBoundsTest) {
+  gfx::Rect bounds(100, 150, 5, 5);
+  EXPECT_CALL(mock_delegate_, popup_bounds()).WillRepeatedly(ReturnRef(bounds));
+
+  ShowView();
+
+  gfx::Point display_point =
+      static_cast<views::View*>(view_)->GetBoundsInScreen().origin();
+  gfx::Point expected_point = bounds.origin();
+  EXPECT_EQ(expected_point, display_point);
 }
 
 }  // namespace autofill

ui/views/controls/native/native_view_host_aura.cc
@@ -65,6 +65,9 @@ NativeViewHostAura::NativeViewHostAura(NativeViewHost* host)
     : host_(host),
       clipping_window_delegate_(new ClippingWindowDelegate()),
       clipping_window_(clipping_window_delegate_.get()) {
+  // Set the type so descendant views (including popups) get positioned
+  // appropriately.
+  clipping_window_.SetType(ui::wm::WINDOW_TYPE_CONTROL);
   clipping_window_.Init(aura::WINDOW_LAYER_NOT_DRAWN);
   clipping_window_.set_owned_by_parent(false);
   clipping_window_.SetName("NativeViewHostAuraClip");

ui/views/controls/native/native_view_host_aura_unittest.cc
@@ -110,13 +110,12 @@ class NativeViewHostAuraTest : public ViewsTestBase {
     toplevel_->Init(toplevel_params);
 
     // And the child widget.
-    View* test_view = new View;
     child_.reset(new Widget);
     Widget::InitParams child_params(Widget::InitParams::TYPE_CONTROL);
     child_params.ownership = Widget::InitParams::WIDGET_OWNS_NATIVE_WIDGET;
     child_params.parent = toplevel_->GetNativeView();
     child_->Init(child_params);
-    child_->SetContentsView(test_view);
+    child_->SetContentsView(new View);
 
     // Owned by |toplevel|.
     host_.reset(new NativeViewHostTesting);