2218d2338a11a44e5a8a3ccb8ec78ade14f2ac98
piotaixr@chromium.org <piotaixr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-24 15:52:32 +0000

    Move logic from NativeImageSkia to GraphicsContext and SkiaUtils
    Related to https://codereview.chromium.org/358893002: in this CL, preparePaint[...] is used  in NativeImageSkia and will be used in StaticBitmapImage::draw()
    
    To avoid having a huge CL (and because it is possible), this change is proposed as a separate CL.
    
    BUG=344804, 170021
    
    Review URL: https://codereview.chromium.org/416543002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178847 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/GraphicsContext.h
+++ third_party/WebKit/Source/platform/graphics/GraphicsContext.h
@@ -238,7 +238,7 @@ public:
     AnnotationModeFlags annotationMode() const { return m_annotationMode; }
     void setAnnotationMode(const AnnotationModeFlags mode) { m_annotationMode = mode; }
 
-    SkColorFilter* colorFilter();
+    SkColorFilter* colorFilter() const;
     void setColorFilter(ColorFilter);
     // ---------- End state management methods -----------------
 
@@ -412,6 +412,14 @@ public:
     void beginAnnotation(const AnnotationList&);
     void endAnnotation();
 
+    void preparePaintForDrawRectToRect(
+        SkPaint*,
+        const SkRect& srcRect,
+        const SkRect& destRect,
+        CompositeOperator,
+        blink::WebBlendMode,
+        bool isLazyDecoded = false,
+        bool isDataComplete = true) const;
 private:
     const GraphicsContextState* immutableState() const { return m_paintState; }
 
