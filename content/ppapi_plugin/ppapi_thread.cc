4efc280cba3d4591fb98cbe5c37023f86bdbfe82
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-24 12:38:17 +0000

    Preload dxva2.dll on Windows when loading Widevine CDM adapter.
    BUG=395147
    TEST=To be tested on win build.
    
    Review URL: https://codereview.chromium.org/407333003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@285212 0039d316-1c4b-4281-b951-d872f2087c98

--- content/ppapi_plugin/ppapi_thread.cc
+++ content/ppapi_plugin/ppapi_thread.cc
@@ -55,6 +55,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(OS_WIN)
+const char kWidevineCdmAdapterFileName[] = "widevinecdmadapter.dll";
+
 extern sandbox::TargetServices* g_target_services;
 
 // Used by EnumSystemLocales for warming up.
@@ -327,13 +329,16 @@ void PpapiThread::OnLoadPlugin(const base::FilePath& path,
   // can be loaded. TODO(cpu): consider changing to the loading style of
   // regular plugins.
   if (g_target_services) {
-    // Let Flash load DXVA before lockdown on Vista+.
-    if (permissions.HasPermission(ppapi::PERMISSION_FLASH)) {
+    // Let Flash and Widevine CDM adapter load DXVA before lockdown on Vista+.
+    if (permissions.HasPermission(ppapi::PERMISSION_FLASH) ||
+        path.BaseName().MaybeAsASCII() == kWidevineCdmAdapterFileName) {
       if (base::win::OSInfo::GetInstance()->version() >=
           base::win::VERSION_VISTA) {
         LoadLibraryA("dxva2.dll");
       }
+    }
 
+    if (permissions.HasPermission(ppapi::PERMISSION_FLASH)) {
       if (base::win::OSInfo::GetInstance()->version() >=
           base::win::VERSION_WIN7) {
         base::CPU cpu;
