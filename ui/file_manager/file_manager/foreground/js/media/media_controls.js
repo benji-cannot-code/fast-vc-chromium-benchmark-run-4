22f2aa6b4a5798947e4e7d9006238d95bd5bc998
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-24 13:55:32 +0000

    Video Player and Audio Player: Make the controls robust  while media is not attached
    Media controls, which is used by the video player and the audio player, is asynchronously, attached a media to so we need to consider the time while the media is not attached yet. This patch adds following changes for such a time.
    
    Major changes:
    - Adds checks of attached media in operations. If no media is attached, just skip it rather than exception.
    - Disables the controls at first. User can easily know that no media is attached yet.
    
    BUG=none
    TEST=Video player and audio player work
    
    Review URL: https://codereview.chromium.org/412103002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@285238 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/file_manager/file_manager/foreground/js/media/media_controls.js
+++ ui/file_manager/file_manager/foreground/js/media/media_controls.js
@@ -148,7 +148,7 @@ MediaControls.prototype.pause = function() {
  * @return {boolean} True if the media is currently playing.
  */
 MediaControls.prototype.isPlaying = function() {
-  return !this.media_.paused && !this.media_.ended;
+  return this.media_ && !this.media_.paused && !this.media_.ended;
 };
 
 /**
@@ -246,6 +246,9 @@ MediaControls.prototype.onProgressChange_ = function(value) {
  * @private
  */
 MediaControls.prototype.onProgressDrag_ = function(on) {
+  if (!this.media_)
+    return;
+
   if (on) {
     this.resumeAfterDrag_ = this.isPlaying();
     this.media_.pause();
@@ -377,6 +380,9 @@ MediaControls.prototype.detachMedia = function() {
  * but we want the media pipeline to deinitialize ASAP to minimize leakage.
  */
 MediaControls.prototype.cleanup = function() {
+  if (!this.media_)
+    return;
+
   this.media_.src = '';
   this.media_.load();
   this.detachMedia();
@@ -400,7 +406,7 @@ MediaControls.prototype.onMediaPlay_ = function(playing) {
  * @private
  */
 MediaControls.prototype.onMediaDuration_ = function() {
-  if (!this.media_.duration) {
+  if (!this.media_ || !this.media_.duration) {
     this.enableControls_('.media-control', false);
     return;
   }
@@ -431,7 +437,7 @@ MediaControls.prototype.onMediaDuration_ = function() {
  * @private
  */
 MediaControls.prototype.onMediaProgress_ = function() {
-  if (!this.media_.duration) {
+  if (!this.media_ || !this.media_.duration) {
     this.displayProgress_(0, 1);
     return;
   }
@@ -488,7 +494,7 @@ MediaControls.prototype.restorePlayState = function() {};
  * Encode current state into the page URL or the app state.
  */
 MediaControls.prototype.encodeState = function() {
-  if (!this.media_.duration)
+  if (!this.media_ || !this.media_.duration)
     return;
 
   if (window.appState) {
@@ -503,7 +509,7 @@ MediaControls.prototype.encodeState = function() {
  * @return {boolean} True if decode succeeded.
  */
 MediaControls.prototype.decodeState = function() {
-  if (!window.appState || !('time' in window.appState))
+  if (!this.media_ || !window.appState || !('time' in window.appState))
     return false;
   // There is no page reload for apps v2, only app restart.
   // Always restart in paused state.
@@ -963,6 +969,9 @@ function VideoControls(containerElement, onMediaError, stringFunction,
     this.textBanner_ = this.createControl('text-banner', opt_stateIconParent);
   }
 
+  // Disables all controls at first.
+  this.enableControls_('.media-control', false);
+
   var videoControls = this;
   chrome.mediaPlayerPrivate.onTogglePlayState.addListener(
       function() { videoControls.togglePlayStateWithFeedback(); });
@@ -1085,7 +1094,8 @@ VideoControls.prototype.togglePlayState = function() {
  *     (required when closing app windows).
  */
 VideoControls.prototype.savePosition = function(opt_sync) {
-  if (!this.media_.duration ||
+  if (!this.media_ ||
+      !this.media_.duration ||
       this.media_.duration < VideoControls.RESUME_THRESHOLD) {
     return;
   }
@@ -1117,7 +1127,7 @@ VideoControls.prototype.savePosition = function(opt_sync) {
  * Resumes the playback position saved in the persistent storage.
  */
 VideoControls.prototype.restorePlayState = function() {
-  if (this.media_.duration >= VideoControls.RESUME_THRESHOLD) {
+  if (this.media_ && this.media_.duration >= VideoControls.RESUME_THRESHOLD) {
     util.AppCache.getValue(this.media_.src, function(position) {
       if (position)
         this.media_.currentTime = position;
@@ -1169,6 +1179,9 @@ function AudioControls(container, advanceTrack, onError) {
   /* No volume controls */
   this.createButton('previous', this.onAdvanceClick_.bind(this, false));
   this.createButton('next', this.onAdvanceClick_.bind(this, true));
+
+  // Disables all controls at first.
+  this.enableControls_('.media-control', false);
 
   var audioControls = this;
   chrome.mediaPlayerPrivate.onNextTrack.addListener(
