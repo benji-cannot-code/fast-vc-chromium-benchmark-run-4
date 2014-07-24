86436599f8ab0fcb20b438a908e307f2e7e00a52
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-24 22:23:43 +0000

    Exit overview mode when a new activity is opened in athena.
    This has the side effect of fixing the crash in 396368
    
    BUG=396368
    TEST=Manual, see bug
    
    Review URL: https://codereview.chromium.org/411813002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@285393 0039d316-1c4b-4281-b951-d872f2087c98

--- athena/home/home_card_unittest.cc
+++ athena/home/home_card_unittest.cc
@@ -58,10 +58,6 @@ TEST_F(HomeCardTest, AppSelection) {
       athena::ActivityFactory::Get()->CreateWebActivity(
           NULL, GURL("http://www.google.com/")));
   EXPECT_EQ(HomeCard::VISIBLE_MINIMIZED, HomeCard::Get()->GetState());
-
-  // Overview mode has to finish before ending test, otherwise it crashes.
-  // TODO(mukai): fix this.
-  WindowManager::GetInstance()->ToggleOverview();
 }
 
 TEST_F(HomeCardTest, Accelerators) {
@@ -85,10 +81,6 @@ TEST_F(HomeCardTest, Accelerators) {
   EXPECT_EQ(HomeCard::VISIBLE_CENTERED, HomeCard::Get()->GetState());
   generator.PressKey(ui::VKEY_L, ui::EF_CONTROL_DOWN);
   EXPECT_EQ(HomeCard::VISIBLE_CENTERED, HomeCard::Get()->GetState());
-
-  // Overview mode has to finish before ending test, otherwise it crashes.
-  // TODO(mukai): fix this.
-  WindowManager::GetInstance()->ToggleOverview();
 }
 
 }  // namespace athena
