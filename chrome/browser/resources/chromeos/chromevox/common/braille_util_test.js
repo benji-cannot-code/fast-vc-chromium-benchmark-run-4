04dbd3203a394484280e58210684feea1425f749
dmazzoni@chromium.org <dmazzoni@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-24 18:58:28 +0000

    Fix CvoxBrailleUtilUnitTest.TextFieldSelection
    It was assuming that modifying selectionStart/selectionEnd on an input
    element focused the element, but Blink just changed to remove that
    behavior.
    
    Fixed by explicitly focusing the element.
    
    BUG=397212,393504
    R=asvitkine@chromium.org
    TBR=dtseng
    
    Review URL: https://codereview.chromium.org/418053004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@285344 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/chromevox/common/braille_util_test.js
+++ chrome/browser/resources/chromeos/chromevox/common/braille_util_test.js
@@ -207,6 +207,7 @@ TEST_F('CvoxBrailleUtilUnitTest', 'TextFieldSelection', function() {
       );
 
   var inputElem = $('1');
+  inputElem.focus();
   var input = cvox.CursorSelection.fromNode(inputElem);
 
   // Selection.
