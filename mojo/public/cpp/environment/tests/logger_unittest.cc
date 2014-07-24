b65766ba9eb66b80126d36520f9c26c8cb12ca13
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-24 13:42:57 +0000

    Mojo: Use EXPECT_DEATH_IF_SUPPORTED() instead of EXPECT_DEATH().
    It'll make it easier to build stuff on platforms that don't support
    death tests (<cough> iOS).
    
    R=darin@chromium.org
    
    Review URL: https://codereview.chromium.org/413963002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@285232 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/public/cpp/environment/tests/logger_unittest.cc
+++ mojo/public/cpp/environment/tests/logger_unittest.cc
@@ -21,7 +21,7 @@ TEST(LoggerTest, Basic) {
   logger->LogMessage(MOJO_LOG_LEVEL_ERROR, "Logged at ERROR level");
 
   // This should kill us:
-  EXPECT_DEATH({
+  EXPECT_DEATH_IF_SUPPORTED({
     logger->LogMessage(MOJO_LOG_LEVEL_FATAL, "Logged at FATAL level");
   }, "");
 }
@@ -47,7 +47,7 @@ TEST(LoggerTest, LogLevels) {
     logger->LogMessage(MOJO_LOG_LEVEL_ERROR, "Logged at ERROR level");
 
     // This should kill us:
-    EXPECT_DEATH({
+    EXPECT_DEATH_IF_SUPPORTED({
       logger->LogMessage(MOJO_LOG_LEVEL_FATAL, "Logged at FATAL level");
     }, "");
   }
--- mojo/public/cpp/utility/tests/thread_unittest.cc
+++ mojo/public/cpp/utility/tests/thread_unittest.cc
@@ -71,7 +71,7 @@ TEST(ThreadTest, CreateAndJoin) {
 #if !defined(NDEBUG)
 TEST(ThreadTest, DebugAssertionFailures) {
   // Can only start once.
-  EXPECT_DEATH({
+  EXPECT_DEATH_IF_SUPPORTED({
     int value = 0;
     SetIntThread thread(&value, 1);
     thread.Start();
@@ -79,14 +79,14 @@ TEST(ThreadTest, DebugAssertionFailures) {
   }, "");
 
   // Must join (if you start).
-  EXPECT_DEATH({
+  EXPECT_DEATH_IF_SUPPORTED({
     int value = 0;
     SetIntThread thread(&value, 2);
     thread.Start();
   }, "");
 
   // Can only join once.
-  EXPECT_DEATH({
+  EXPECT_DEATH_IF_SUPPORTED({
     int value = 0;
     SetIntThread thread(&value, 3);
     thread.Start();
@@ -95,7 +95,7 @@ TEST(ThreadTest, DebugAssertionFailures) {
   }, "");
 
   // Stack too big (we're making certain assumptions here).
-  EXPECT_DEATH({
+  EXPECT_DEATH_IF_SUPPORTED({
     int value = 0;
     Thread::Options options;
     options.set_stack_size(static_cast<size_t>(-1));
