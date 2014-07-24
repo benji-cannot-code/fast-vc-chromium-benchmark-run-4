1b855558d1cfde5a23999d4139ea5001b23657fd
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-24 21:09:10 +0000

    Introduce 'grab_inputs' property for athena container.
    BUG=395655
    R=oshima@chromium.org
    TEST=athena_unittests
    
    Review URL: https://codereview.chromium.org/414903002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@285371 0039d316-1c4b-4281-b951-d872f2087c98

--- athena/test/athena_test_helper.cc
+++ athena/test/athena_test_helper.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "athena/test/athena_test_helper.h"
 
 #include "athena/main/athena_launcher.h"
+#include "athena/screen/public/screen_manager.h"
 #include "athena/test/sample_activity_factory.h"
 #include "athena/test/test_app_model_builder.h"
 #include "base/command_line.h"
@@ -22,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ime/input_method_initializer.h"
 #include "ui/compositor/scoped_animation_duration_scale_mode.h"
 #include "ui/gfx/screen.h"
-#include "ui/wm/core/default_activation_client.h"
+#include "ui/wm/core/focus_controller.h"
 #include "ui/wm/core/input_method_event_filter.h"
 
 #if defined(USE_X11)
@@ -73,11 +74,12 @@ void AthenaTestHelper::SetUp(ui::ContextFactory* context_factory) {
   input_method_filter_->SetInputMethodPropertyInRootWindow(
       root_window());
 
-  // TODO(oshima): Switch to athena implementation.
-  focus_client_.reset(new aura::test::TestFocusClient);
-  aura::client::SetFocusClient(root_window(),
-                               focus_client_.get());
-  new ::wm::DefaultActivationClient(root_window());
+  wm::FocusController* focus_controller =
+      new wm::FocusController(ScreenManager::CreateFocusRules());
+  aura::client::SetFocusClient(root_window(), focus_controller);
+  root_window()->AddPreTargetHandler(focus_controller);
+  aura::client::SetActivationClient(root_window(), focus_controller);
+  focus_client_.reset(focus_controller);
 
   root_window()->Show();
   // Ensure width != height so tests won't confuse them.
