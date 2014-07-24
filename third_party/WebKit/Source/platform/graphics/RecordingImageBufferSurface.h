8eb1b4c7329417cb2d70b6827fcb603d3bf160fb
junov@chromium.org <junov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-24 03:52:52 +0000

    Fixing RecordingImageBufferSurface to stay in display list mode
    This fix makes RecordingImageBufferSurface track the initial
    stack level of the SkPicture recording canvas to avoid falsely
    rejecting canvases from display list mode due to state present
    on the stack
    
    BUG=396800
    TEST=RecordingImageBufferSurfaceTest platform unit tests
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/412993004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178804 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/RecordingImageBufferSurface.h
+++ third_party/WebKit/Source/platform/graphics/RecordingImageBufferSurface.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class SkPicture;
 class SkPictureRecorder;
+class RecordingImageBufferSurfaceTest;
 
 namespace WebCore {
 
@@ -32,6 +33,7 @@ public:
     virtual void setImageBuffer(ImageBuffer*) OVERRIDE;
 
 private:
+    friend class ::RecordingImageBufferSurfaceTest; // for unit testing
     void fallBackToRasterCanvas();
     void initializeCurrentFrame();
     bool handleOpaqueFrame();
@@ -40,6 +42,7 @@ private:
     RefPtr<SkPicture> m_previousFrame;
     OwnPtr<SkCanvas> m_rasterCanvas;
     GraphicsContext* m_graphicsContext;
+    int m_initialSaveCount;
     bool m_frameWasCleared;
 };
 
