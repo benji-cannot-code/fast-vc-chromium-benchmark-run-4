a64dcd68375321c51cc377216538140fc8a0b06e
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-25 09:13:02 +0000

    Oilpan: Prepare to move MemoryCache and MemoryCahce::MemoryCacheEntry to Oilpan heap.
    Also, this CL makes MemoryCacheLRUList traceable.
    
    setMemoryCacheForTesting was renamed to replaceMemoryCacheFor Testing,
    and it returns PassOwnPtr<> for the previous one.
    
    This is a prepration to move Resource hierarchy to Oilpan heap.
    
    BUG=393516
    
    Review URL: https://codereview.chromium.org/416803002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178915 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/fetch/CachingCorrectnessTest.cpp
+++ third_party/WebKit/Source/core/fetch/CachingCorrectnessTest.cpp
@@ -140,10 +140,7 @@ private:
         blink::Platform::initialize(&m_proxyPlatform);
 
         // Save the global memory cache to restore it upon teardown.
-        m_globalMemoryCache = adoptPtr(memoryCache());
-        // Create the test memory cache instance and hook it in.
-        m_testingMemoryCache = adoptPtr(new MemoryCache());
-        setMemoryCacheForTesting(m_testingMemoryCache.leakPtr());
+        m_globalMemoryCache = replaceMemoryCacheForTesting(MemoryCache::create());
 
         // Create a ResourceFetcher that has a real DocumentLoader and Document, but is not attached to a LocalFrame.
         const KURL kDocumentURL(ParsedURLString, "http://document.com/");
@@ -157,12 +154,8 @@ private:
     {
         memoryCache()->evictResources();
 
-        // Regain the ownership of testing memory cache, so that it will be
-        // destroyed.
-        m_testingMemoryCache = adoptPtr(memoryCache());
-
         // Yield the ownership of the global memory cache back.
-        setMemoryCacheForTesting(m_globalMemoryCache.leakPtr());
+        replaceMemoryCacheForTesting(m_globalMemoryCache.release());
 
         blink::Platform::initialize(m_savedPlatform);
     }
@@ -170,8 +163,7 @@ private:
     blink::Platform* m_savedPlatform;
     ProxyPlatform m_proxyPlatform;
 
-    OwnPtr<MemoryCache> m_testingMemoryCache;
-    OwnPtr<MemoryCache> m_globalMemoryCache;
+    OwnPtrWillBePersistent<MemoryCache> m_globalMemoryCache;
 
     RefPtr<DocumentLoader> m_documentLoader;
 
--- third_party/WebKit/Source/core/frame/ImageBitmapTest.cpp
+++ third_party/WebKit/Source/core/frame/ImageBitmapTest.cpp
@@ -62,10 +62,7 @@ protected:
         m_bitmap2.eraseColor(0xAAAAAAAA);
 
         // Save the global memory cache to restore it upon teardown.
-        m_globalMemoryCache = adoptPtr(memoryCache());
-        // Create the test memory cache instance and hook it in.
-        m_testingMemoryCache = adoptPtr(new MemoryCache());
-        setMemoryCacheForTesting(m_testingMemoryCache.leakPtr());
+        m_globalMemoryCache = replaceMemoryCacheForTesting(MemoryCache::create());
     }
     virtual void TearDown()
     {
@@ -74,16 +71,11 @@ protected:
         // them from the cache.
         Heap::collectGarbage(ThreadState::NoHeapPointersOnStack);
 
-        // Regain the ownership of testing memory cache, so that it will be
-        // destroyed.
-        m_testingMemoryCache = adoptPtr(memoryCache());
-        // Yield the ownership of the global memory cache back.
-        setMemoryCacheForTesting(m_globalMemoryCache.leakPtr());
+        replaceMemoryCacheForTesting(m_globalMemoryCache.release());
     }
 
     SkBitmap m_bitmap, m_bitmap2;
-    OwnPtr<MemoryCache> m_testingMemoryCache;
-    OwnPtr<MemoryCache> m_globalMemoryCache;
+    OwnPtrWillBePersistent<MemoryCache> m_globalMemoryCache;
 };
 
 // Verifies that the image resource held by an ImageBitmap is the same as the
