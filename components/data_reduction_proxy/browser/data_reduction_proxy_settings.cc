45889e95aa344f91edb565561c2b48232dc542ce
bengr@chromium.org <bengr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-25 22:26:10 +0000

    UMA for data reduction proxy probe net errors
    Added UMA to report the type of net error that occurred
    when the data reduction proxy probe failed due to a
    network error. Note the probe can also fail due to
    the response being modified, which this UMA does not
    track.
    
    BUG=397256
    
    Review URL: https://codereview.chromium.org/417253002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@285711 0039d316-1c4b-4281-b951-d872f2087c98

--- components/data_reduction_proxy/browser/data_reduction_proxy_settings.cc
+++ components/data_reduction_proxy/browser/data_reduction_proxy_settings.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/metrics/field_trial.h"
 #include "base/metrics/histogram.h"
+#include "base/metrics/sparse_histogram.h"
 #include "base/prefs/pref_member.h"
 #include "base/prefs/pref_service.h"
 #include "base/prefs/scoped_user_pref_update.h"
@@ -53,6 +54,9 @@ const char kUMAProxyStartupStateHistogram[] =
 
 // Key of the UMA DataReductionProxy.ProbeURL histogram.
 const char kUMAProxyProbeURL[] = "DataReductionProxy.ProbeURL";
+
+// Key of the UMA DataReductionProxy.ProbeURLNetError histogram.
+const char kUMAProxyProbeURLNetError[] = "DataReductionProxy.ProbeURLNetError";
 
 // Record a network change event.
 void RecordNetworkChangeEvent(DataReductionProxyNetworkChangeEvent event) {
@@ -235,10 +239,16 @@ void DataReductionProxySettings::OnURLFetchComplete(
 
   DCHECK(source == fetcher_.get());
   net::URLRequestStatus status = source->GetStatus();
-  if (status.status() == net::URLRequestStatus::FAILED &&
-      status.error() == net::ERR_INTERNET_DISCONNECTED) {
-    RecordProbeURLFetchResult(INTERNET_DISCONNECTED);
-    return;
+  if (status.status() == net::URLRequestStatus::FAILED) {
+    if (status.error() == net::ERR_INTERNET_DISCONNECTED) {
+      RecordProbeURLFetchResult(INTERNET_DISCONNECTED);
+      return;
+    }
+    // TODO(bengr): Remove once we understand the reasons probes are failing.
+    // Probe errors are either due to fetcher-level errors or modified
+    // responses. This only tracks the former.
+    UMA_HISTOGRAM_SPARSE_SLOWLY(
+        kUMAProxyProbeURLNetError, std::abs(status.error()));
   }
 
   std::string response;
