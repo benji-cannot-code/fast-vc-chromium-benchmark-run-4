d131db20277da5fb46bccfe4bf502b7e8d20bfeb
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-25 09:47:28 +0000

    Fix flaky WindowsApiSetIcon test.
    BUG=396466
    
    Review URL: https://codereview.chromium.org/411253002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@285545 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/platform_apps/windows_api_set_icon/background.js
+++ chrome/test/data/extensions/platform_apps/windows_api_set_icon/background.js
@@ -4,9 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.test.sendMessage("Launched");
   chrome.app.window.create('test.html', {}, function(win) {
-    win.setIcon("icon.png");
-    chrome.test.sendMessage('IconSet');
+    chrome.test.sendMessage('ready', function() {
+      win.setIcon("icon.png");
+    });
   });
 });
