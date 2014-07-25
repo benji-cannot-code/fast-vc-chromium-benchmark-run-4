dbee4c85822543825483ad8368184c6df5376af4
bashi@chromium.org <bashi@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-25 05:29:26 +0000

    bindings: make Nullable::get() return const reference
    The Nullable template can contain a large value. It would be better
    not to create a copy for each get() call.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/413413002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178902 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/core/v8/Nullable.h
+++ third_party/WebKit/Source/bindings/core/v8/Nullable.h
@@ -34,7 +34,7 @@ public:
         return *this;
     }
 
-    T get() const { ASSERT(!m_isNull); return m_value; }
+    const T& get() const { ASSERT(!m_isNull); return m_value; }
     bool isNull() const { return m_isNull; }
 
     operator bool() const { return !m_isNull && m_value; }
