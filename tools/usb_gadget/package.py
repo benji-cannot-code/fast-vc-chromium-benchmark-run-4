1790d8f3a663fd17ec4a8b2a67e0010a9c178990
reillyg@chromium.org <reillyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-25 08:35:56 +0000

    [usb_gadget p09] Software updates over HTTP.
    Add a /update resource which accepts a new software package (assuming
    the hash is correct) and performs an in-place upgrade. To avoid leaving
    the device in an unusable state the old version remains running until
    the new version starts to accept HTTP connections. At this point the
    Tornado I/O loop is running correctly and even if USB functionality
    is broken the device should accept another software update to fix it.
    
    BUG=396682
    R=rockot@chromium.org,rpaquay@chromium.org,kalman@chromium.org
    
    Review URL: https://codereview.chromium.org/412243002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@285531 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/usb_gadget/package.py
+++ tools/usb_gadget/package.py
@@ -4,13 +4,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Utility to package the USB gadget framework.
+"""Utility to package and upload the USB gadget framework.
 """
 
 import argparse
 import hashlib
 import os
 import StringIO
+import urllib2
 import zipfile
 
 
@@ -38,6 +39,27 @@ def MakeZip(directory=None, files=None):
   return content, md5
 
 
+def EncodeBody(filename, buf):
+  return '\r\n'.join([
+      '--foo',
+      'Content-Disposition: form-data; name="file"; filename="{}"'
+      .format(filename),
+      'Content-Type: application/octet-stream',
+      '',
+      buf,
+      '--foo--',
+      ''
+  ])
+
+
+def UploadZip(content, md5, host):
+  filename = 'usb_gadget-{}.zip'.format(md5)
+  req = urllib2.Request(url='http://{}/update'.format(host),
+                        data=EncodeBody(filename, content))
+  req.add_header('Content-Type', 'multipart/form-data; boundary=foo')
+  urllib2.urlopen(req)
+
+
 def main():
   parser = argparse.ArgumentParser(
       description='Package (and upload) the USB gadget framework.')
@@ -51,6 +73,9 @@ def main():
       '--hash-file', type=str, metavar='FILE',
       help='save package hash as FILE')
   parser.add_argument(
+      '--upload', type=str, metavar='HOST[:PORT]',
+      help='upload package to target system')
+  parser.add_argument(
       'files', metavar='FILE', type=str, nargs='*',
       help='source files')
 
@@ -63,6 +88,8 @@ def main():
   if args.hash_file:
     with open(args.hash_file, 'w') as hash_file:
       hash_file.write(md5)
+  if args.upload:
+    UploadZip(content, md5, args.upload)
 
 
 if __name__ == '__main__':
