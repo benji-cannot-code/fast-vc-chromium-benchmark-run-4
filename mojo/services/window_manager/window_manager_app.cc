72d7fa91fa8cf4bbe7fe3eb15d24b6398ab80809
jamesr@chromium.org <jamesr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-25 07:38:26 +0000

    Re-enable mojo_core_window_manager_unittests
    These now pass thanks to Tim's shutdown fixes.
    
    BUG=396295
    
    Review URL: https://codereview.chromium.org/421473002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@285522 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/services/window_manager/window_manager_app.cc
+++ mojo/services/window_manager/window_manager_app.cc
@@ -172,7 +172,8 @@ void WindowManagerApp::OnRootAdded(ViewManager* view_manager,
   focus_client_->AddObserver(this);
   activation_client_->AddObserver(this);
 
-  wrapped_delegate_->OnRootAdded(view_manager, root);
+  if (wrapped_delegate_)
+    wrapped_delegate_->OnRootAdded(view_manager, root);
 
   for (Connections::const_iterator it = connections_.begin();
        it != connections_.end(); ++it) {
@@ -183,7 +184,8 @@ void WindowManagerApp::OnRootAdded(ViewManager* view_manager,
 void WindowManagerApp::OnViewManagerDisconnected(
     ViewManager* view_manager) {
   DCHECK_EQ(view_manager_, view_manager);
-  wrapped_delegate_->OnViewManagerDisconnected(view_manager);
+  if (wrapped_delegate_)
+    wrapped_delegate_->OnViewManagerDisconnected(view_manager);
   root_->RemoveObserver(this);
   root_ = NULL;
   view_manager_ = NULL;
