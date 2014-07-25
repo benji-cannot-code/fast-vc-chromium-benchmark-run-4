ba36be909b81130ae5bcb021320aaaa4cf63bd5a
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-25 08:04:02 +0000

    Clear current target in TimelineModelImpl only when recording has stopped
    reset can be called when clearing records in Timeline that is being recorded. After such action recording continues and current Target doesn't change.
    
    BUG=397216
    R=loislo@chromium.org
    
    Review URL: https://codereview.chromium.org/417123002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178907 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/timeline/TimelineModelImpl.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TimelineModelImpl.js
@@ -167,6 +167,7 @@ WebInspector.TimelineModelImpl.prototype = {
         if (cpuProfile)
             WebInspector.TimelineJSProfileProcessor.mergeJSProfileIntoTimeline(this, cpuProfile);
         this.dispatchEventToListeners(WebInspector.TimelineModel.Events.RecordingStopped);
+        this._currentTarget = null;
     },
 
     /**
@@ -263,7 +264,6 @@ WebInspector.TimelineModelImpl.prototype = {
 
     reset: function()
     {
-        this._currentTarget = null;
         this._payloads = [];
         this._stringPool = {};
         this._bindings._reset();
