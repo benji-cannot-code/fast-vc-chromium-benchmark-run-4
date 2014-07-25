10cf5ef609a33f7eeed78b85ed42e0840fca3068
hirono@chromium.org <hirono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-25 08:58:58 +0000

    Files.app: Move the position of dummy element for measuring the size of text.
    BUG=392981
    TEST=manually
    
    Review URL: https://codereview.chromium.org/419063004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@285536 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/file_manager/file_manager/foreground/js/text_measure.js
+++ ui/file_manager/file_manager/foreground/js/text_measure.js
@@ -18,25 +18,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *     width of text is measures like as it is rendered in this element.
  */
 var TextMeasure = function(element) {
-  var doc = element.ownerDocument;
-  this.dummySpan_ = doc.createElement('span');
-  this.dummySpan_ = doc.getElementsByTagName('body')[0].
-                        appendChild(this.dummySpan_);
-  this.dummySpan_.style.position = 'absolute';
-  this.dummySpan_.style.visibility = 'hidden';
+  this.dummySpan_ = createElementWithClassName('span', 'text-measure');
   var styles = window.getComputedStyle(element, '');
-  var stylesToBeCopied = [
-    'fontSize',
-    'fontStyle',
-    'fontWeight',
-    'fontFamily',
-    'letterSpacing'
-  ];
-  for (var i = 0; i < stylesToBeCopied.length; i++) {
-    this.dummySpan_.style[stylesToBeCopied[i]] = styles[stylesToBeCopied[i]];
+  for (var i = 0; i < TextMeasure.STYLES_TO_BE_COPYED.length; i++) {
+    var name = TextMeasure.STYLES_TO_BE_COPYED[i];
+    this.dummySpan_.style[name] = styles[name];
   }
+  element.ownerDocument.body.appendChild(this.dummySpan_);
   Object.seal(this);
 };
+
+/**
+ * Style names to be copied to the measured dummy span.
+ * @type {Array.<String>}
+ * @const
+ */
+TextMeasure.STYLES_TO_BE_COPYED = Object.freeze([
+  'fontSize',
+  'fontStyle',
+  'fontWeight',
+  'fontFamily',
+  'letterSpacing'
+]);
 
 /**
  * Measures the width of text.
