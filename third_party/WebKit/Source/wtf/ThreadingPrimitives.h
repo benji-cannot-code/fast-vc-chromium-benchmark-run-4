c00ff5c7d7263a7ebbdc82ee8b0c12fa5aa8388e
wibling@chromium.org <wibling@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-25 11:55:12 +0000

    [oilpan]: fix deadlock when leaving a safepoint and sweeping.
    In the case where a thread takes a lock/mutex within a safepoint scope and leaves the safepoint with the lock held we risk deadlocking if the same thread takes the same lock in a destructor. The reason is that if another thread did a GC while the first thread was in the safepoint the sweep done by the first thread will potentially call a destructor which could then take the same lock. Since the WTF:Mutex is not recursive/reentrant the thread will deadlock on itself.
    This change fixes this by introducing a RecursiveMutex which allows a thread to take a lock it is already holding. Interestingly the Windows WTF:Mutex is currently reentrant wrt. the lock() method, but not wrt. tryLock() since tryLock mimics the behavior of the pthread_mutex by keeping a recursion count. To deal with this I introduce a separate tryLock method for the Mutex and the RecursiveMutex that behaves different on windows. On pthreads (Linux/Mac) the tryLock method is identical since it changes behavior depending on how the mutex is created.
    
    I have added a test which demonstrates the dead-lock issue.
    
    R=ager@chromium.org, erik.corry@gmail.com, haraken@chromium.org, oilpan-reviews@chromium.org, sigbjornf@opera.com, tkent@chromium.org, zerny@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/415083002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178928 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/ThreadingPrimitives.h
+++ third_party/WebKit/Source/wtf/ThreadingPrimitives.h
@@ -49,7 +49,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WTF {
 
 #if USE(PTHREADS)
-typedef pthread_mutex_t PlatformMutex;
+struct PlatformMutex {
+    pthread_mutex_t m_internalMutex;
+#if ENABLE(ASSERT)
+    size_t m_recursionCount;
+#endif
+};
 typedef pthread_cond_t PlatformCondition;
 #elif OS(WIN)
 struct PlatformMutex {
@@ -72,23 +77,39 @@ typedef void* PlatformMutex;
 typedef void* PlatformCondition;
 #endif
 
-class WTF_EXPORT Mutex {
-    WTF_MAKE_NONCOPYABLE(Mutex); WTF_MAKE_FAST_ALLOCATED;
+class WTF_EXPORT MutexBase {
+    WTF_MAKE_NONCOPYABLE(MutexBase); WTF_MAKE_FAST_ALLOCATED;
 public:
-    Mutex();
-    ~Mutex();
+    ~MutexBase();
 
     void lock();
-    bool tryLock();
     void unlock();
+#if ENABLE(ASSERT)
+    bool locked() { return m_mutex.m_recursionCount > 0; }
+#endif
 
 public:
     PlatformMutex& impl() { return m_mutex; }
-private:
+
+protected:
+    MutexBase(bool recursive);
+
     PlatformMutex m_mutex;
 };
 
-typedef Locker<Mutex> MutexLocker;
+class WTF_EXPORT Mutex : public MutexBase {
+public:
+    Mutex() : MutexBase(false) { }
+    bool tryLock();
+};
+
+class WTF_EXPORT RecursiveMutex : public MutexBase {
+public:
+    RecursiveMutex() : MutexBase(true) { }
+    bool tryLock();
+};
+
+typedef Locker<MutexBase> MutexLocker;
 
 class MutexTryLocker {
     WTF_MAKE_NONCOPYABLE(MutexTryLocker);
@@ -113,10 +134,10 @@ public:
     ThreadCondition();
     ~ThreadCondition();
 
-    void wait(Mutex&);
+    void wait(MutexBase&);
     // Returns true if the condition was signaled before absoluteTime, false if the absoluteTime was reached or is in the past.
     // The absoluteTime is in seconds, starting on January 1, 1970. The time is assumed to use the same time zone as WTF::currentTime().
-    bool timedWait(Mutex&, double absoluteTime);
+    bool timedWait(MutexBase&, double absoluteTime);
     void signal();
     void broadcast();
 
@@ -132,7 +153,9 @@ DWORD absoluteTimeToWaitTimeoutInterval(double absoluteTime);
 
 } // namespace WTF
 
+using WTF::MutexBase;
 using WTF::Mutex;
+using WTF::RecursiveMutex;
 using WTF::MutexLocker;
 using WTF::MutexTryLocker;
 using WTF::ThreadCondition;
