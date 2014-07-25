0006fc59e4804a80dd3efdc1abe01cc7867e6006
ch.dumez@samsung.com <ch.dumez@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-25 20:39:24 +0000

    Use tighter typing in editing/
    Use tighter typing in editing/ to increase code readability. This CL focuses
    on ApplyStyleCommand.
    
    R=leviw@chromium.org
    
    Review URL: https://codereview.chromium.org/418223004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178955 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/WrapContentsInDummySpanCommand.h
+++ third_party/WebKit/Source/core/editing/WrapContentsInDummySpanCommand.h
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class HTMLElement;
+class HTMLSpanElement;
 
 class WrapContentsInDummySpanCommand FINAL : public SimpleEditCommand {
 public:
@@ -51,7 +51,7 @@ private:
     void executeApply();
 
     RefPtrWillBeMember<Element> m_element;
-    RefPtrWillBeMember<HTMLElement> m_dummySpan;
+    RefPtrWillBeMember<HTMLSpanElement> m_dummySpan;
 };
 
 } // namespace blink
