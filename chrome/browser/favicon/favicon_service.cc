6279afd3477fa8c6b2c9469a88dcf3c2c7bf8807
dhnishi@chromium.org <dhnishi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-25 23:44:55 +0000

    Modify a favicon DCHECK.
    There may be more than one favicon if loading an extension favicon,
    which loads for all supported scale factors immediately rather than
    in an as-needed fashion.
    
    BUG=397341
    
    Review URL: https://codereview.chromium.org/421633002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@285725 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/favicon/favicon_service.cc
+++ chrome/browser/favicon/favicon_service.cc
@@ -404,7 +404,6 @@ void FaviconService::RunFaviconRawBitmapCallbackWithBitmapResults(
     return;
   }
 
-  DCHECK_EQ(1u, favicon_bitmap_results.size());
   favicon_base::FaviconRawBitmapResult bitmap_result =
       favicon_bitmap_results[0];
 
