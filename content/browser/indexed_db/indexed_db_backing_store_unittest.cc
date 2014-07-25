01045e233493d3059c1c6322f31d92c70dca8a53
cmumford@chromium.org <cmumford@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-25 00:02:22 +0000

    IndexedDB: Added MessageLoop to IndexedDBBackingStoreTest
    Tests use //net so a message loop is required.
    
    BUG=395358
    
    Review URL: https://codereview.chromium.org/411393003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@285419 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/indexed_db/indexed_db_backing_store_unittest.cc
+++ content/browser/indexed_db/indexed_db_backing_store_unittest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/indexed_db/indexed_db_value.h"
 #include "content/browser/indexed_db/leveldb/leveldb_factory.h"
 #include "content/public/test/mock_special_storage_policy.h"
+#include "content/public/test/test_browser_thread_bundle.h"
 #include "net/url_request/url_request_test_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/WebKit/public/platform/WebIDBTypes.h"
@@ -348,6 +349,8 @@ class IndexedDBBackingStoreTest : public testing::Test {
   std::vector<IndexedDBBlobInfo> m_blob_info;
 
  private:
+  content::TestBrowserThreadBundle thread_bundle_;
+
   DISALLOW_COPY_AND_ASSIGN(IndexedDBBackingStoreTest);
 };
 
