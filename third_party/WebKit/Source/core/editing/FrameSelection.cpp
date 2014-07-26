9aed15d07e7a386adbf9664e29f7f8c538486a8a
ch.dumez@samsung.com <ch.dumez@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-26 06:10:21 +0000

    Use tighter typing in editing: BreakBlockquoteCommand & Caret
    Use tighter typing in editing/ to increase code readability and avoid
    unnecessary type checks. This CL focuses on BreakBlockquoteCommand and
    Caret.
    
    R=leviw@chromium.org
    
    Review URL: https://codereview.chromium.org/419313002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178985 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/FrameSelection.cpp
+++ third_party/WebKit/Source/core/editing/FrameSelection.cpp
@@ -1213,7 +1213,7 @@ void FrameSelection::setExtent(const VisiblePosition &pos, EUserTriggered userTr
     setSelection(VisibleSelection(m_selection.base(), pos.deepEquivalent(), pos.affinity(), selectionHasDirection), CloseTyping | ClearTypingStyle | userTriggered);
 }
 
-RenderObject* FrameSelection::caretRenderer() const
+RenderBlock* FrameSelection::caretRenderer() const
 {
     return CaretBase::caretRenderer(m_selection.start().deprecatedNode());
 }
