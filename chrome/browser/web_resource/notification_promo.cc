f5ddb486400e2b32e10f5f162cccccda72e495e4
justincohen@chromium.org <justincohen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-26 20:11:23 +0000

    Limit the window of time a promo can be visible after first view.
    Notification promos currently have start, end times, and a number of maximum
    views. However, the length of time a promo can be visible was not capped.
    This change adds a flag to cap the length of time a promo is visible by
    max_seconds_.
    
    BUG=None
    Test=None
    
    Review URL: https://codereview.chromium.org/419033007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@285803 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/web_resource/notification_promo.cc
+++ chrome/browser/web_resource/notification_promo.cc
@@ -50,6 +50,8 @@ const char kPrefPromoIncrement[] = "increment";
 const char kPrefPromoIncrementFrequency[] = "increment_frequency";
 const char kPrefPromoIncrementMax[] = "increment_max";
 const char kPrefPromoMaxViews[] = "max_views";
+const char kPrefPromoMaxSeconds[] = "max_seconds";
+const char kPrefPromoFirstViewTime[] = "first_view_time";
 const char kPrefPromoGroup[] = "group";
 const char kPrefPromoViews[] = "views";
 const char kPrefPromoClosed[] = "closed";
@@ -200,6 +202,8 @@ NotificationPromo::NotificationPromo()
       time_slice_(0),
       max_group_(0),
       max_views_(0),
+      max_seconds_(0),
+      first_view_time_(0),
       group_(0),
       views_(0),
       closed_(false),
@@ -285,6 +289,9 @@ void NotificationPromo::InitFromJson(const base::DictionaryValue& json,
   promo->GetInteger("max_views", &max_views_);
   DVLOG(1) << "max_views_ " << max_views_;
 
+  promo->GetInteger("max_seconds", &max_seconds_);
+  DVLOG(1) << "max_seconds_ " << max_seconds_;
+
   CheckForNewNotification();
 }
 
@@ -342,6 +349,8 @@ void NotificationPromo::WritePrefs() {
   ntp_promo->SetInteger(kPrefPromoIncrementMax, max_group_);
 
   ntp_promo->SetInteger(kPrefPromoMaxViews, max_views_);
+  ntp_promo->SetInteger(kPrefPromoMaxSeconds, max_seconds_);
+  ntp_promo->SetDouble(kPrefPromoFirstViewTime, first_view_time_);
 
   ntp_promo->SetInteger(kPrefPromoGroup, group_);
   ntp_promo->SetInteger(kPrefPromoViews, views_);
@@ -389,6 +398,8 @@ void NotificationPromo::InitFromPrefs(PromoType promo_type) {
   ntp_promo->GetInteger(kPrefPromoIncrementMax, &max_group_);
 
   ntp_promo->GetInteger(kPrefPromoMaxViews, &max_views_);
+  ntp_promo->GetInteger(kPrefPromoMaxSeconds, &max_seconds_);
+  ntp_promo->GetDouble(kPrefPromoFirstViewTime, &first_view_time_);
 
   ntp_promo->GetInteger(kPrefPromoGroup, &group_);
   ntp_promo->GetInteger(kPrefPromoViews, &views_);
@@ -413,6 +424,7 @@ bool NotificationPromo::CanShow() const {
          !promo_text_.empty() &&
          !ExceedsMaxGroup() &&
          !ExceedsMaxViews() &&
+         !ExceedsMaxSeconds() &&
          CheckAppLauncher() &&
          base::Time::FromDoubleT(StartTimeForGroup()) < base::Time::Now() &&
          base::Time::FromDoubleT(EndTime()) > base::Time::Now();
@@ -435,8 +447,11 @@ bool NotificationPromo::HandleViewed(PromoType promo_type) {
   NotificationPromo promo;
   promo.InitFromPrefs(promo_type);
   ++promo.views_;
+  if (promo.first_view_time_ == 0) {
+    promo.first_view_time_ = base::Time::Now().ToDoubleT();
+  }
   promo.WritePrefs();
-  return promo.ExceedsMaxViews();
+  return promo.ExceedsMaxViews() || promo.ExceedsMaxSeconds();
 }
 
 bool NotificationPromo::ExceedsMaxGroup() const {
@@ -445,6 +460,15 @@ bool NotificationPromo::ExceedsMaxGroup() const {
 
 bool NotificationPromo::ExceedsMaxViews() const {
   return (max_views_ == 0) ? false : views_ >= max_views_;
+}
+
+bool NotificationPromo::ExceedsMaxSeconds() const {
+  if (max_seconds_ == 0 || first_view_time_ == 0)
+    return false;
+
+  const base::Time last_view_time = base::Time::FromDoubleT(first_view_time_) +
+                                    base::TimeDelta::FromSeconds(max_seconds_);
+  return last_view_time < base::Time::Now();
 }
 
 // static
