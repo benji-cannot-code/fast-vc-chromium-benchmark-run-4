71afd33d76032a0b87ea8e456f37603834886cee
ch.dumez@samsung.com <ch.dumez@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-28 23:11:21 +0000

    Move Node::lastDescendantOrSelf() to NodeTraversal and rename to lastWithinOrSelf()
    Move Node::lastDescendantOrSelf() to NodeTraversal and rename to
    lastWithinOrSelf() as this is a node traversal utility function and it avoids
    code duplication with NodeTraversal::lastWithin().
    
    R=esprehn@chromium.org, adamk@chromium.org
    
    Review URL: https://codereview.chromium.org/420323002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179062 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/ReplaceSelectionCommand.cpp
+++ third_party/WebKit/Source/core/editing/ReplaceSelectionCommand.cpp
@@ -1486,7 +1486,7 @@ void ReplaceSelectionCommand::updateNodesInserted(Node *node)
     if (m_startOfInsertedContent.isNull())
         m_startOfInsertedContent = firstPositionInOrBeforeNode(node);
 
-    m_endOfInsertedContent = lastPositionInOrAfterNode(&node->lastDescendantOrSelf());
+    m_endOfInsertedContent = lastPositionInOrAfterNode(&NodeTraversal::lastWithinOrSelf(*node));
 }
 
 // During simple pastes, where we're just pasting a text node into a run of text, we insert the text node
