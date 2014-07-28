d167583f8fc8ad76b7e001852883878fb3bb2336
halton.huo@intel.com <halton.huo@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-28 15:35:18 +0000

    Add arch support for tombstone.py
    'stack' tool has archtechture support by passing --arch argument, if it
    is not given, arm will be used by default. Thus the stack trace will be
    wrongly reported.
    
    To resolve it, 'arch' prop is added to tombstone when fetching from
    device, and pass it to symbol.ARCH at end.
    
    R=jbudorick@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/416983004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@285910 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/tombstones.py
+++ build/android/tombstones.py
@@ -77,12 +77,13 @@ def _EraseTombstone(device, tombstone_file):
       'rm /data/tombstones/' + tombstone_file, as_root=True)
 
 
-def _ResolveSymbols(tombstone_data, include_stack):
+def _ResolveSymbols(tombstone_data, include_stack, arch):
   """Run the stack tool for given tombstone input.
 
   Args:
     tombstone_data: a list of strings of tombstone data.
     include_stack: boolean whether to include stack data in output.
+    arch: the device architecture of tombstone data.
 
   Yields:
     A string for each line of resolved stack output.
@@ -90,7 +91,7 @@ def _ResolveSymbols(tombstone_data, include_stack):
   stack_tool = os.path.join(os.path.dirname(__file__), '..', '..',
                             'third_party', 'android_platform', 'development',
                             'scripts', 'stack')
-  proc = subprocess.Popen(stack_tool, stdin=subprocess.PIPE,
+  proc = subprocess.Popen([stack_tool, '--arch', arch], stdin=subprocess.PIPE,
                           stdout=subprocess.PIPE)
   output = proc.communicate(input='\n'.join(tombstone_data))[0]
   for line in output.split('\n'):
@@ -107,7 +108,8 @@ def _ResolveTombstone(tombstone):
             ' Device: ' + tombstone['serial'])]
   print '\n'.join(lines)
   print 'Resolving...'
-  lines += _ResolveSymbols(tombstone['data'], tombstone['stack'])
+  lines += _ResolveSymbols(tombstone['data'], tombstone['stack'],
+                           tombstone['arch'])
   return lines
 
 
@@ -152,6 +154,7 @@ def _GetTombstonesForDevice(device, options):
   device_now = _GetDeviceDateTime(device)
   for tombstone_file, tombstone_time in tombstones:
     ret += [{'serial': str(device),
+             'arch': device.GetProp('ro.product.cpu.abi'),
              'device_now': device_now,
              'time': tombstone_time,
              'file': tombstone_file,
@@ -164,6 +167,7 @@ def _GetTombstonesForDevice(device, options):
       _EraseTombstone(device, tombstone_file)
 
   return ret
+
 
 def main():
   parser = optparse.OptionParser()
