913691da3c1a917a6dd975d4da489d23590966ed
sammc@chromium.org <sammc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-28 04:39:26 +0000

    Move the serial api and permission features to extensions/.
    BUG=389016
    
    Review URL: https://codereview.chromium.org/420713002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@285861 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/apps/app_info_dialog/app_info_permissions_panel_unittest.cc
+++ chrome/browser/ui/views/apps/app_info_dialog/app_info_permissions_panel_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/manifest.h"
 #include "extensions/common/permissions/permission_set.h"
 #include "extensions/common/permissions/permissions_data.h"
+#include "grit/extensions_strings.h"
 #include "grit/generated_resources.h"
 #include "testing/gmock/include/gmock/gmock-matchers.h"
 #include "testing/gtest/include/gtest/gtest.h"
