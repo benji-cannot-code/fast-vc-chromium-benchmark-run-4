fb4ab3f9af2d2da5f55157c78fd083c9b30192ff
andersr@opera.com <andersr@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-28 16:15:19 +0000

    Remove unused and incorrect RenderStyle::initialTextEmphasisColor.
    Initial -webkit-text-emphasis-color (correctly) uses
    StyleColor::currentColor, like most of the other colors.
    
    Also, initialTextEmphasisColor returns TextEmphasisFillFilled, which is not a Color.
    
    Review URL: https://codereview.chromium.org/425763002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179046 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/style/RenderStyle.h
+++ third_party/WebKit/Source/core/rendering/style/RenderStyle.h
@@ -1663,7 +1663,6 @@ public:
     static Length initialPerspectiveOriginX() { return Length(50.0, Percent); }
     static Length initialPerspectiveOriginY() { return Length(50.0, Percent); }
     static Color initialBackgroundColor() { return Color::transparent; }
-    static Color initialTextEmphasisColor() { return TextEmphasisFillFilled; }
     static TextEmphasisFill initialTextEmphasisFill() { return TextEmphasisFillFilled; }
     static TextEmphasisMark initialTextEmphasisMark() { return TextEmphasisMarkNone; }
     static const AtomicString& initialTextEmphasisCustomMark() { return nullAtom; }
