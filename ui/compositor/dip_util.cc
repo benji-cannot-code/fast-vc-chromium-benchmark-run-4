995a1d6ea27ec4dac0aaae3ac69a7d43c1a686dc
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-28 20:47:58 +0000

    Use target bounds to verify pixel snapping if the layer is is_animating
    This was causing check failure (i believe) when new animation is applied while animating.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/411673004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@285971 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/compositor/dip_util.cc
+++ ui/compositor/dip_util.cc
@@ -19,6 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/size.h"
 #include "ui/gfx/size_conversions.h"
 
+#if DCHECK_IS_ON
+#include "ui/compositor/layer_animator.h"
+#endif
+
 namespace ui {
 
 float GetDeviceScaleFactor(const Layer* layer) {
@@ -73,7 +77,7 @@ gfx::Rect ConvertRectToPixel(const Layer* layer,
                  gfx::ScaleSize(rect_in_dip.size(), scale)));
 }
 
-#if !defined(NDEBUG)
+#if DCHECK_IS_ON
 namespace {
 
 void CheckSnapped(float snapped_position) {
@@ -104,13 +108,20 @@ void SnapLayerToPhysicalPixelBoundary(ui::Layer* snapped_layer,
   gfx::Vector2dF fudge = view_offset_snapped - view_offset;
   fudge.Scale(1.0 / scale_factor);
   layer_to_snap->SetSubpixelPositionOffset(fudge);
-#if !defined(NDEBUG)
-  gfx::Point p;
-  Layer::ConvertPointToLayer(layer_to_snap->parent(), snapped_layer, &p);
-  cc::Layer* cc_layer = layer_to_snap->cc_layer();
-  gfx::PointF origin = cc_layer->position();
-  CheckSnapped((p.x() + origin.x()) * scale_factor);
-  CheckSnapped((p.y() + origin.y()) * scale_factor);
+#if DCHECK_IS_ON
+  gfx::Point layer_offset;
+  gfx::PointF origin;
+  Layer::ConvertPointToLayer(
+      layer_to_snap->parent(), snapped_layer, &layer_offset);
+  if (layer_to_snap->GetAnimator()->is_animating()) {
+    origin = layer_to_snap->GetTargetBounds().origin() +
+             layer_to_snap->subpixel_position_offset();
+  } else {
+    cc::Layer* cc_layer = layer_to_snap->cc_layer();
+    origin = cc_layer->position();
+  }
+  CheckSnapped((layer_offset.x() + origin.x()) * scale_factor);
+  CheckSnapped((layer_offset.y() + origin.y()) * scale_factor);
 #endif
 }
 
