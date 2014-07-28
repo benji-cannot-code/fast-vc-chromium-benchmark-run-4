cfe5ae2c065a322f3b5d18a9fd0fffda2c48c933
ch.dumez@samsung.com <ch.dumez@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-28 20:32:05 +0000

    Remove ContainerNode overload for NodeTraversal::nextSkippingChildren()
    Remove ContainerNode overload for NodeTraversal::nextSkippingChildren() as it
    adds no benefit compared to the one taking a Node in argument.
    
    The nextSkippingChildren() implementation does not involves visiting the
    children (and thus calling firstChild() / lastChild()). Therefore, there is no
    benefit to knowing that the argument is a ContainerNode (unlike for
    NodeTraversal::next()).
    
    This CL also does the same for ElementTraversal::nextSkippingChildren().
    
    R=esprehn@chromium.org, adamk@chromium.org
    
    Review URL: https://codereview.chromium.org/420603012
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179055 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/ElementTraversal.h
+++ third_party/WebKit/Source/core/dom/ElementTraversal.h
@@ -66,10 +66,8 @@ public:
     static ElementType* previous(const Node& current, const Node* stayWithin) { return previousTemplate(current, stayWithin); }
 
     // Like next, but skips children.
-    static ElementType* nextSkippingChildren(const ContainerNode& current) { return nextSkippingChildrenTemplate(current); }
-    static ElementType* nextSkippingChildren(const Node& current) { return nextSkippingChildrenTemplate(current); }
-    static ElementType* nextSkippingChildren(const ContainerNode& current, const Node* stayWithin) { return nextSkippingChildrenTemplate(current, stayWithin); }
-    static ElementType* nextSkippingChildren(const Node& current, const Node* stayWithin) { return nextSkippingChildrenTemplate(current, stayWithin); }
+    static ElementType* nextSkippingChildren(const Node&);
+    static ElementType* nextSkippingChildren(const Node&, const Node* stayWithin);
 
     // Pre-order traversal including the pseudo-elements.
     static ElementType* previousIncludingPseudo(const Node&, const Node* stayWithin = 0);
@@ -102,10 +100,6 @@ private:
     static ElementType* previousTemplate(NodeType&);
     template <class NodeType>
     static ElementType* previousTemplate(NodeType&, const Node* stayWithin);
-    template <class NodeType>
-    static ElementType* nextSkippingChildrenTemplate(NodeType&);
-    template <class NodeType>
-    static ElementType* nextSkippingChildrenTemplate(NodeType&, const Node* stayWithin);
 };
 
 typedef Traversal<Element> ElementTraversal;
@@ -268,8 +262,7 @@ inline ElementType* Traversal<ElementType>::previousTemplate(NodeType& current, 
 }
 
 template <class ElementType>
-template <class NodeType>
-inline ElementType* Traversal<ElementType>::nextSkippingChildrenTemplate(NodeType& current)
+inline ElementType* Traversal<ElementType>::nextSkippingChildren(const Node& current)
 {
     Node* node = NodeTraversal::nextSkippingChildren(current);
     while (node && !isElementOfType<const ElementType>(*node))
@@ -278,8 +271,7 @@ inline ElementType* Traversal<ElementType>::nextSkippingChildrenTemplate(NodeTyp
 }
 
 template <class ElementType>
-template <class NodeType>
-inline ElementType* Traversal<ElementType>::nextSkippingChildrenTemplate(NodeType& current, const Node* stayWithin)
+inline ElementType* Traversal<ElementType>::nextSkippingChildren(const Node& current, const Node* stayWithin)
 {
     Node* node = NodeTraversal::nextSkippingChildren(current, stayWithin);
     while (node && !isElementOfType<const ElementType>(*node))
