776b26110ffddd107628ac1b57830496cb290338
fs@opera.com <fs@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-28 12:15:07 +0000

    HTMLImageElement.natural{Width,Height} should return intrinsic dimensions
    HTML says:
    
     "The IDL attributes naturalWidth and naturalHeight must return the
      intrinsic width and height of the image, in CSS pixels, if the image is
      available, or else 0."
    
    Change HTMLImageElement::natural{Width,Height} to request the intrinsic
    size from ImageResource::imageSizeForRenderer by passing IntrinsicSize as
    the third parameter. This makes the attributes return more appropriate
    values for SVG-images in <img>.
    
    BUG=396955
    
    Review URL: https://codereview.chromium.org/422503002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179030 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLImageElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLImageElement.cpp
@@ -375,7 +375,7 @@ int HTMLImageElement::naturalWidth() const
     if (!imageLoader().image())
         return 0;
 
-    return imageLoader().image()->imageSizeForRenderer(renderer(), 1.0f).width();
+    return imageLoader().image()->imageSizeForRenderer(renderer(), 1.0f, ImageResource::IntrinsicSize).width();
 }
 
 int HTMLImageElement::naturalHeight() const
@@ -383,7 +383,7 @@ int HTMLImageElement::naturalHeight() const
     if (!imageLoader().image())
         return 0;
 
-    return imageLoader().image()->imageSizeForRenderer(renderer(), 1.0f).height();
+    return imageLoader().image()->imageSizeForRenderer(renderer(), 1.0f, ImageResource::IntrinsicSize).height();
 }
 
 const String& HTMLImageElement::currentSrc() const
