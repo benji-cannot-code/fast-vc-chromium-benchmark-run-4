9c07b51c3396683af7d69e6637993c80d0e835c3
chrishtr@chromium.org <chrishtr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-28 17:55:54 +0000

    Make RecursionData a stack-allocated variable in StyleInvalidator, since it is only used during a single invalidation.
    These makes makes its intended object data lifetime clearer and avoids possible bugs.
    
    BUG=379908
    
    Review URL: https://codereview.chromium.org/416243003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179050 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/invalidation/StyleInvalidator.h
+++ third_party/WebKit/Source/core/css/invalidation/StyleInvalidator.h
@@ -30,11 +30,6 @@ public:
     void trace(Visitor*);
 
 private:
-    bool invalidate(Element&);
-    bool invalidateChildren(Element&);
-
-    bool checkInvalidationSetsAgainstElement(Element&);
-
     struct RecursionData {
         RecursionData()
             : m_invalidateCustomPseudo(false)
@@ -57,6 +52,10 @@ private:
         bool m_wholeSubtreeInvalid;
         bool m_treeBoundaryCrossing;
     };
+
+    bool invalidate(Element&, RecursionData&);
+    bool invalidateChildren(Element&, RecursionData&);
+    bool checkInvalidationSetsAgainstElement(Element&, RecursionData&);
 
     class RecursionCheckpoint {
     public:
@@ -89,7 +88,6 @@ private:
     InvalidationList& ensurePendingInvalidationList(Element&);
 
     PendingInvalidationMap m_pendingInvalidationMap;
-    RecursionData m_recursionData;
 };
 
 } // namespace blink
