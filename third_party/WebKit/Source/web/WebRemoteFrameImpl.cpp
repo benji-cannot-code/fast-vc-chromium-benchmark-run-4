58665e7c2d426ad1ae45f6514644dbbe98734d08
n.bansal@samsung.com <n.bansal@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-28 15:47:27 +0000

    Support NumCopies print preset
    Blink side changes to support NumCopies print preset option.
    Copies in print preview dialog should be set to value set for
    NumCopies in pdf source document.
    
    BUG=169120
    
    Review URL: https://codereview.chromium.org/379793002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179042 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebRemoteFrameImpl.cpp
+++ third_party/WebKit/Source/web/WebRemoteFrameImpl.cpp
@@ -634,6 +634,12 @@ bool WebRemoteFrameImpl::isPrintScalingDisabledForPlugin(const WebNode&)
     return false;
 }
 
+int WebRemoteFrameImpl::getPrintCopiesForPlugin(const WebNode&)
+{
+    ASSERT_NOT_REACHED();
+    return 1;
+}
+
 bool WebRemoteFrameImpl::hasCustomPageSizeStyle(int pageIndex)
 {
     ASSERT_NOT_REACHED();
