18dc5da2cc853ccc7050af0699dc1643c9caf196
chrishenry@google.com <chrishenry@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-28 00:36:46 +0000

    Introduce OutputFormatter interface and wire it through PageTestResults.
    Make --output-format=csv uses the new OutputFormatter instead of
    subclassing PageMeasurementResults.
    
    BUG=346956
    
    Review URL: https://codereview.chromium.org/416993008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@285855 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/results/csv_output_formatter_unittest.py
+++ tools/telemetry/telemetry/results/csv_output_formatter_unittest.py
@@ -7,7 +7,8 @@ import csv
 import os
 import unittest
 
-from telemetry.results import csv_page_measurement_results
+from telemetry.results import csv_output_formatter
+from telemetry.results import page_test_results
 from telemetry.page import page_set
 from telemetry.value import histogram
 from telemetry.value import scalar
@@ -19,18 +20,12 @@ def _MakePageSet():
   ps.AddPageWithDefaultRunNavigate('http://www.bar.com/')
   return ps
 
-class NonPrintingCsvPageMeasurementResults(
-    csv_page_measurement_results.CsvPageMeasurementResults):
-  def __init__(self, *args):
-    super(NonPrintingCsvPageMeasurementResults, self).__init__(*args)
 
-  def _PrintPerfResult(self, *args):
-    pass
-
-class CsvPageMeasurementResultsTest(unittest.TestCase):
+class CsvOutputFormatterTest(unittest.TestCase):
   def setUp(self):
     self._output = StringIO.StringIO()
     self._page_set = _MakePageSet()
+    self._formatter = csv_output_formatter.CsvOutputFormatter(self._output)
 
   @property
   def lines(self):
@@ -48,7 +43,7 @@ class CsvPageMeasurementResultsTest(unittest.TestCase):
     return rows[1:]
 
   def test_with_no_results_on_second_run(self):
-    results = NonPrintingCsvPageMeasurementResults(self._output)
+    results = page_test_results.PageTestResults()
     results.StartTest(self._page_set[0])
     results.AddValue(scalar.ScalarValue(self._page_set[0], 'foo', 'seconds', 3))
     results.StopTest(self._page_set[0])
@@ -56,7 +51,8 @@ class CsvPageMeasurementResultsTest(unittest.TestCase):
     results.StartTest(self._page_set[1])
     results.StopTest(self._page_set[1])
 
-    results.PrintSummary()
+    self._formatter.Format(results)
+
     self.assertEqual(['page_name', 'foo (seconds)'], self.output_header_row)
     # TODO(chrishenry): Is this really the right behavior? Should this
     # not output a second row with '-' as its results?
@@ -64,7 +60,7 @@ class CsvPageMeasurementResultsTest(unittest.TestCase):
     self.assertEqual(expected, self.output_data_rows)
 
   def test_fewer_results_on_second_run(self):
-    results = NonPrintingCsvPageMeasurementResults(self._output)
+    results = page_test_results.PageTestResults()
     results.StartTest(self._page_set[0])
     results.AddValue(scalar.ScalarValue(self._page_set[0], 'foo', 'seconds', 3))
     results.AddValue(scalar.ScalarValue(self._page_set[0], 'bar', 'seconds', 4))
@@ -74,7 +70,7 @@ class CsvPageMeasurementResultsTest(unittest.TestCase):
     results.AddValue(scalar.ScalarValue(self._page_set[1], 'bar', 'seconds', 5))
     results.StopTest(self._page_set[1])
 
-    results.PrintSummary()
+    self._formatter.Format(results)
     self.assertEqual(['page_name', 'bar (seconds)', 'foo (seconds)'],
                      self.output_header_row)
     expected = [[self._page_set[0].url, '4.0', '3.0'],
@@ -82,7 +78,7 @@ class CsvPageMeasurementResultsTest(unittest.TestCase):
     self.assertEqual(expected, self.output_data_rows)
 
   def test_with_output_at_print_summary_time(self):
-    results = NonPrintingCsvPageMeasurementResults(self._output)
+    results = page_test_results.PageTestResults()
     results.StartTest(self._page_set[0])
     results.AddValue(scalar.ScalarValue(self._page_set[0], 'foo', 'seconds', 3))
     results.StopTest(self._page_set[0])
@@ -91,7 +87,7 @@ class CsvPageMeasurementResultsTest(unittest.TestCase):
     results.AddValue(scalar.ScalarValue(self._page_set[1], 'bar', 'seconds', 4))
     results.StopTest(self._page_set[1])
 
-    results.PrintSummary()
+    self._formatter.Format(results)
 
     self.assertEqual(
       self.output_header_row,
@@ -102,7 +98,7 @@ class CsvPageMeasurementResultsTest(unittest.TestCase):
     self.assertEqual(expected, self.output_data_rows)
 
   def test_histogram(self):
-    results = NonPrintingCsvPageMeasurementResults(self._output)
+    results = page_test_results.PageTestResults()
     results.StartTest(self._page_set[0])
     results.AddValue(histogram.HistogramValue(
         self._page_set[0], 'a', '',
@@ -115,7 +111,7 @@ class CsvPageMeasurementResultsTest(unittest.TestCase):
         raw_value_json='{"buckets": [{"low": 2, "high": 3, "count": 1}]}'))
     results.StopTest(self._page_set[1])
 
-    results.PrintSummary()
+    self._formatter.Format(results)
 
     self.assertEqual(
         self.output_header_row,
