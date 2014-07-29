1c18def25ba114cd69d55ccb2a4de505dc527b4b
jbudorick@chromium.org <jbudorick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-29 08:14:55 +0000

    [Android] Fix DeviceUtils.__str__ when no serial is provided.
    This CL also changes two instances of DeviceUtils.__init__(None) s.t.
    an explicit serial number is now provided.
    
    BUG=398127
    
    Review URL: https://codereview.chromium.org/420273006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286124 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/chromedriver/test/test_environment.py
+++ chrome/test/chromedriver/test/test_environment.py
@@ -19,8 +19,10 @@ _THIS_DIR = os.path.abspath(os.path.dirname(__file__))
 
 if util.IsLinux():
   sys.path.insert(0, os.path.join(chrome_paths.GetSrc(), 'build', 'android'))
+  from pylib import android_commands
   from pylib import forwarder
   from pylib import valgrind_tools
+  from pylib.device import device_errors
   from pylib.device import device_utils
 
 ANDROID_TEST_HTTP_PORT = 2311
@@ -95,7 +97,12 @@ class AndroidTestEnvironment(DesktopTestEnvironment):
   def GlobalSetUp(self):
     os.putenv('TEST_HTTP_PORT', str(ANDROID_TEST_HTTP_PORT))
     os.putenv('TEST_HTTPS_PORT', str(ANDROID_TEST_HTTPS_PORT))
-    self._device = device_utils.DeviceUtils(None)
+    devices = android_commands.GetAttachedDevices()
+    if not devices:
+      raise device_errors.NoDevicesError()
+    elif len(devices) > 1:
+      logging.warning('Multiple devices attached. Using %s.' % devices[0])
+    self._device = device_utils.DeviceUtils(devices[0])
     forwarder.Forwarder.Map(
         [(ANDROID_TEST_HTTP_PORT, ANDROID_TEST_HTTP_PORT),
          (ANDROID_TEST_HTTPS_PORT, ANDROID_TEST_HTTPS_PORT)],
@@ -103,7 +110,8 @@ class AndroidTestEnvironment(DesktopTestEnvironment):
 
   # override
   def GlobalTearDown(self):
-    forwarder.Forwarder.UnmapAllDevicePorts(self._device)
+    if self._device:
+      forwarder.Forwarder.UnmapAllDevicePorts(self._device)
 
   # override
   def GetOS(self):
