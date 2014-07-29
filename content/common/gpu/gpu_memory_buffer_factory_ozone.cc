01676f503c193e15a87772c008999b65655ac072
reveman@chromium.org <reveman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-29 02:51:21 +0000

    ui: Add OZONE_NATIVE_BUFFER GPU memory buffer type.
    BUG=
    
    Review URL: https://codereview.chromium.org/419593002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286056 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/gpu_memory_buffer_factory_ozone.cc
+++ content/common/gpu/gpu_memory_buffer_factory_ozone.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "ui/gl/gl_image.h"
 #include "ui/gl/gl_image_shared_memory.h"
+#include "ui/ozone/public/gpu_memory_buffer_factory_ozone_native_buffer.h"
 
 namespace content {
 namespace {
@@ -20,12 +21,29 @@ class GpuMemoryBufferFactoryImpl : public GpuMemoryBufferFactory {
       const gfx::Size& size,
       unsigned internalformat,
       unsigned usage) OVERRIDE {
-    NOTREACHED();
-    return gfx::GpuMemoryBufferHandle();
+    switch (handle.type) {
+      case gfx::OZONE_NATIVE_BUFFER:
+        return ui::GpuMemoryBufferFactoryOzoneNativeBuffer::GetInstance()
+                       ->CreateGpuMemoryBuffer(
+                           handle.global_id, size, internalformat, usage)
+                   ? handle
+                   : gfx::GpuMemoryBufferHandle();
+      default:
+        NOTREACHED();
+        return gfx::GpuMemoryBufferHandle();
+    }
   }
   virtual void DestroyGpuMemoryBuffer(
       const gfx::GpuMemoryBufferHandle& handle) OVERRIDE {
-    NOTREACHED();
+    switch (handle.type) {
+      case gfx::OZONE_NATIVE_BUFFER:
+        ui::GpuMemoryBufferFactoryOzoneNativeBuffer::GetInstance()
+            ->DestroyGpuMemoryBuffer(handle.global_id);
+        break;
+      default:
+        NOTREACHED();
+        break;
+    }
   }
   virtual scoped_refptr<gfx::GLImage> CreateImageForGpuMemoryBuffer(
       const gfx::GpuMemoryBufferHandle& handle,
@@ -41,6 +59,14 @@ class GpuMemoryBufferFactoryImpl : public GpuMemoryBufferFactory {
 
         return image;
       }
+      case gfx::OZONE_NATIVE_BUFFER:
+        // Verify that client is the owner of the buffer we're about to use.
+        if (handle.global_id.secondary_id != client_id)
+          return scoped_refptr<gfx::GLImage>();
+
+        return ui::GpuMemoryBufferFactoryOzoneNativeBuffer::GetInstance()
+            ->CreateImageForGpuMemoryBuffer(
+                handle.global_id, size, internalformat);
       default:
         NOTREACHED();
         return scoped_refptr<gfx::GLImage>();
---
+++ ui/ozone/public/gpu_memory_buffer_factory_ozone_native_buffer.cc
@@ -0,0 +1,59 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/ozone/public/gpu_memory_buffer_factory_ozone_native_buffer.h"
+
+#include "base/logging.h"
+
+namespace ui {
+
+// static
+GpuMemoryBufferFactoryOzoneNativeBuffer*
+    GpuMemoryBufferFactoryOzoneNativeBuffer::impl_ = NULL;
+
+GpuMemoryBufferFactoryOzoneNativeBuffer::
+    GpuMemoryBufferFactoryOzoneNativeBuffer() {
+  CHECK(!impl_) << "There should only be a single "
+                   "GpuMemoryBufferFactoryOzoneNativeBuffer.";
+  impl_ = this;
+}
+
+GpuMemoryBufferFactoryOzoneNativeBuffer::
+    ~GpuMemoryBufferFactoryOzoneNativeBuffer() {
+  CHECK_EQ(impl_, this);
+  impl_ = NULL;
+}
+
+GpuMemoryBufferFactoryOzoneNativeBuffer*
+GpuMemoryBufferFactoryOzoneNativeBuffer::GetInstance() {
+  CHECK(impl_)
+      << "No GpuMemoryBufferFactoryOzoneNativeBuffer implementation set.";
+  return impl_;
+}
+
+bool GpuMemoryBufferFactoryOzoneNativeBuffer::CreateGpuMemoryBuffer(
+    const gfx::GpuMemoryBufferId& id,
+    const gfx::Size& size,
+    unsigned internalformat,
+    unsigned usage) {
+  NOTIMPLEMENTED();
+  return false;
+}
+
+void GpuMemoryBufferFactoryOzoneNativeBuffer::DestroyGpuMemoryBuffer(
+    const gfx::GpuMemoryBufferId& id) {
+  NOTIMPLEMENTED();
+}
+
+scoped_refptr<gfx::GLImage>
+GpuMemoryBufferFactoryOzoneNativeBuffer::CreateImageForGpuMemoryBuffer(
+    const gfx::GpuMemoryBufferId& id,
+    const gfx::Size& size,
+    unsigned internalformat) {
+  NOTIMPLEMENTED();
+  return scoped_refptr<gfx::GLImage>();
+}
+
+}  // namespace ui
