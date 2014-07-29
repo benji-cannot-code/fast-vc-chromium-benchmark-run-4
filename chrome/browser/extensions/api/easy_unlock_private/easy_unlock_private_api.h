a67e5f62ceb2f8030a547e78fd7e8885d9fb0880
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-29 21:51:25 +0000

    Add a private Bluetooth API to connect to the SDP service on a remote device (ChromeOS only).
    BUG=387122
    TEST=
      1. Create an app that can call easyUnlockPrivate APIs.
      2. Call chrome.easyUnlockPrivate.seekBluetoothDeviceByAddress(),
         passing in the Bluetooth address of an unpaired device.
      3. The connection should succeed, and the DeviceAdded callback should be called.
    
    R=tengs@chromium.org
    
    Review URL: https://codereview.chromium.org/404543006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286309 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/easy_unlock_private/easy_unlock_private_api.h
+++ chrome/browser/extensions/api/easy_unlock_private/easy_unlock_private_api.h
@@ -16,6 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace extensions {
 namespace api {
 
+namespace easy_unlock {
+struct SeekDeviceResult;
+}  // easy_unlock
+
 class EasyUnlockPrivatePerformECDHKeyAgreementFunction
     : public AsyncExtensionFunction {
  public:
@@ -83,6 +87,26 @@ class EasyUnlockPrivateUnwrapSecureMessageFunction
 
   DECLARE_EXTENSION_FUNCTION("easyUnlockPrivate.unwrapSecureMessage",
                              EASYUNLOCKPRIVATE_UNWRAPSECUREMESSAGE)
+};
+
+class EasyUnlockPrivateSeekBluetoothDeviceByAddressFunction
+    : public AsyncExtensionFunction {
+ public:
+  DECLARE_EXTENSION_FUNCTION("easyUnlockPrivate.seekBluetoothDeviceByAddress",
+                             EASYUNLOCKPRIVATE_SEEKBLUETOOTHDEVICEBYADDRESS)
+  EasyUnlockPrivateSeekBluetoothDeviceByAddressFunction();
+
+ private:
+  virtual ~EasyUnlockPrivateSeekBluetoothDeviceByAddressFunction();
+
+  // AsyncExtensionFunction:
+  virtual bool RunAsync() OVERRIDE;
+
+  // Callback that is called when the seek operation succeeds.
+  void OnSeekCompleted(const easy_unlock::SeekDeviceResult& seek_result);
+
+  DISALLOW_COPY_AND_ASSIGN(
+      EasyUnlockPrivateSeekBluetoothDeviceByAddressFunction);
 };
 
 }  // namespace api
---
+++ chrome/browser/extensions/api/easy_unlock_private/easy_unlock_private_bluetooth_util.h
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_EXTENSIONS_API_EASY_UNLOCK_PRIVATE_EASY_UNLOCK_PRIVATE_BLUETOOTH_UTIL_H_
+#define CHROME_BROWSER_EXTENSIONS_API_EASY_UNLOCK_PRIVATE_EASY_UNLOCK_PRIVATE_BLUETOOTH_UTIL_H_
+
+#include <string>
+
+#include "base/callback_forward.h"
+
+namespace extensions {
+namespace api {
+namespace easy_unlock {
+
+struct SeekDeviceResult {
+  // Whether the connection to the device succeeded.
+  bool success;
+
+  // If the connection failed, an error message describing the failure.
+  std::string error_message;
+};
+typedef base::Callback<void(const SeekDeviceResult& result)> SeekDeviceCallback;
+
+// Connects to the SDP service on the Bluetooth device with the given
+// |device_address|, if possible. Calls the |callback| with an indicator of
+// success or an error message on failure.
+void SeekBluetoothDeviceByAddress(const std::string& device_address,
+                                  const SeekDeviceCallback& callback);
+
+}  // namespace easy_unlock
+}  // namespace api
+}  // namespace extensions
+
+#endif  // CHROME_BROWSER_EXTENSIONS_API_EASY_UNLOCK_PRIVATE_EASY_UNLOCK_PRIVATE_BLUETOOTH_UTIL_H_
