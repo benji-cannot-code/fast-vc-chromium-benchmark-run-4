7d855ad89e65a799bf1eb2b223d3d5abf367dea0
powei@chromium.org <powei@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-29 10:45:14 +0000

    android: Use WorkerPool instead of custom thread for thumbnail compression
    BUG=398329
    TBR=dtrainor@chromium.org
    
    Review URL: https://codereview.chromium.org/428843002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286165 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/android/thumbnail/thumbnail_store.h
+++ chrome/browser/android/thumbnail/thumbnail_store.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/memory/ref_counted.h"
 #include "base/observer_list.h"
-#include "base/threading/thread.h"
 #include "base/time/time.h"
 #include "chrome/browser/android/thumbnail/scoped_ptr_expiring_cache.h"
 #include "chrome/browser/android/thumbnail/thumbnail.h"
@@ -150,7 +149,6 @@ class ThumbnailStore : ThumbnailDelegate {
   TabIdList visible_ids_;
 
   content::UIResourceProvider* ui_resource_provider_;
-  base::Thread compression_thread_;
 
   base::WeakPtrFactory<ThumbnailStore> weak_factory_;
 
