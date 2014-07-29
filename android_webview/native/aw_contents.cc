af3c020be74135b72beb841691ef660a459dcdb4
sievers@chromium.org <sievers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-29 11:17:47 +0000

    Android WebView: Perform idle gpu service work
    Review URL: https://codereview.chromium.org/411173002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286171 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/native/aw_contents.cc
+++ android_webview/native/aw_contents.cc
@@ -368,8 +368,12 @@ void AwContents::DrawGL(AwDrawGLInfo* draw_info) {
     return;
   }
 
-  if (draw_info->mode != AwDrawGLInfo::kModeDraw)
+  if (draw_info->mode != AwDrawGLInfo::kModeDraw) {
+    if (draw_info->mode == AwDrawGLInfo::kModeProcess) {
+      DeferredGpuCommandService::GetInstance()->PerformIdleWork(true);
+    }
     return;
+  }
 
   if (!hardware_renderer_) {
     hardware_renderer_.reset(new HardwareRenderer(&shared_renderer_state_));
@@ -379,6 +383,7 @@ void AwContents::DrawGL(AwDrawGLInfo* draw_info) {
   hardware_renderer_->DrawGL(state_restore.stencil_enabled(),
                              state_restore.framebuffer_binding_ext(),
                              draw_info);
+  DeferredGpuCommandService::GetInstance()->PerformIdleWork(false);
 }
 
 namespace {
