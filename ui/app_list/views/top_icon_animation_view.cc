a3787485d129298f5962fe9f60490625b060f3eb
calamity@chromium.org <calamity@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-29 09:15:24 +0000

    Fix hang when dragging dialogs with the app list open.
    For some reason, deleting a TopIconAnimationView in its animation completed
    callback caused app list dialogs to hang when dragged. This CL defers the
    deletion of the offending view which fixes this problem.
    
    BUG=386555
    TEST=On Windows, Open Chrome task manager and leave it open (note that the taskmanager
    window can be dragged). Open app list, right-click an app, select uninstall and
    leave that dialog open (note that the taskmanger window can still be dragged).
    Now cancel uninstall dialog, expand an app list folder, select uninstall on an
    app in that folder and leave that dialog open. Task manager window should still
    be able to be dragged. (before this CL, the task manager could not be dragged in
    the last case: mouse events were queued until `Esc` was pressed).
    
    Review URL: https://codereview.chromium.org/418263002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286138 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/app_list/views/top_icon_animation_view.cc
+++ ui/app_list/views/top_icon_animation_view.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/app_list/views/top_icon_animation_view.h"
 
+#include "base/message_loop/message_loop_proxy.h"
 #include "ui/app_list/app_list_constants.h"
 #include "ui/compositor/scoped_layer_animation_settings.h"
 #include "ui/gfx/image/image_skia_operations.h"
@@ -45,6 +46,9 @@ void TopIconAnimationView::RemoveObserver(TopIconAnimationObserver* observer) {
 }
 
 void TopIconAnimationView::TransformView() {
+  // This view will delete itself on animation completion.
+  set_owned_by_client();
+
   // Transform used for scaling down the icon and move it back inside to the
   // original folder icon.
   const float kIconTransformScale = 0.33333f;
@@ -81,7 +85,7 @@ void TopIconAnimationView::OnImplicitAnimationsCompleted() {
   FOR_EACH_OBSERVER(TopIconAnimationObserver,
                     observers_,
                     OnTopIconAnimationsComplete());
-  delete this;
+  base::MessageLoopProxy::current()->DeleteSoon(FROM_HERE, this);
 }
 
 bool TopIconAnimationView::RequiresNotificationWhenAnimatorDestroyed() const {
