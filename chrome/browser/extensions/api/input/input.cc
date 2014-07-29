7113f64a131e0d74f518dcd983c72b1295c1031d
bshe@chromium.org <bshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-29 22:37:45 +0000

    Noop gear button in VK menu popup if not logged in
    BUG=395621
    
    Review URL: https://codereview.chromium.org/411463007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286320 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/input/input.cc
+++ chrome/browser/extensions/api/input/input.cc
@@ -19,6 +19,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/keyboard/keyboard_controller.h"
 #include "ui/keyboard/keyboard_switches.h"
 
+#if defined(OS_CHROMEOS)
+#include "chrome/browser/chromeos/login/lock/screen_locker.h"
+#include "chrome/browser/chromeos/login/ui/user_adding_screen.h"
+#include "chrome/browser/chromeos/login/users/user_manager.h"
+#endif  // OS_CHROMEOS
+
 #if defined(USE_ASH)
 #include "ash/root_window_controller.h"
 #include "ash/shell.h"
@@ -169,6 +175,16 @@ bool VirtualKeyboardPrivateGetKeyboardConfigFunction::RunSync() {
 bool VirtualKeyboardPrivateOpenSettingsFunction::RunSync() {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 #if defined(USE_ASH)
+#if defined(OS_CHROMEOS)
+  // Do not try to open language options page if user is not logged in or
+  // locked.
+  if (!chromeos::UserManager::Get()->IsUserLoggedIn() ||
+      chromeos::UserAddingScreen::Get()->IsRunning() ||
+      (chromeos::ScreenLocker::default_screen_locker() &&
+          chromeos::ScreenLocker::default_screen_locker()->locked()))
+    return true;
+#endif  // OS_CHROMEOS
+
   content::RecordAction(base::UserMetricsAction("OpenLanguageOptionsDialog"));
   chrome::ShowSettingsSubPageForProfile(
       ProfileManager::GetActiveUserProfile(), chrome::kLanguageOptionsSubPage);
