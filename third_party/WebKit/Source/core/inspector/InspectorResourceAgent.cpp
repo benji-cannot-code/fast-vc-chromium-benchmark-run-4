baab947d341e79d6d6d0e7837f1c56c6a877d35e
aandrey@chromium.org <aandrey@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-29 09:11:32 +0000

    DevTools: Send async stack trace with Network.Initiator.
    Send async stack trace for Network.Initiator to frontend, and show it in console
    and popover in the Network panel.
    
    BUG=169721
    R=eustas, apavlov
    
    Review URL: https://codereview.chromium.org/416843002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179109 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorResourceAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorResourceAgent.cpp
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/inspector/InspectorState.h"
 #include "core/inspector/InstrumentingAgents.h"
 #include "core/inspector/NetworkResourcesData.h"
+#include "core/inspector/ScriptAsyncCallStack.h"
 #include "core/inspector/ScriptCallStack.h"
 #include "core/loader/DocumentLoader.h"
 #include "core/loader/DocumentThreadableLoader.h"
@@ -562,6 +563,9 @@ PassRefPtr<TypeBuilder::Network::Initiator> InspectorResourceAgent::buildInitiat
         RefPtr<TypeBuilder::Network::Initiator> initiatorObject = TypeBuilder::Network::Initiator::create()
             .setType(TypeBuilder::Network::Initiator::Type::Script);
         initiatorObject->setStackTrace(stackTrace->buildInspectorArray());
+        RefPtrWillBeRawPtr<ScriptAsyncCallStack> asyncStackTrace = stackTrace->asyncCallStack();
+        if (asyncStackTrace)
+            initiatorObject->setAsyncStackTrace(asyncStackTrace->buildInspectorObject());
         return initiatorObject;
     }
 
