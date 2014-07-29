23b9f1b46416936e1baa690911d33452d821b8e0
abhishek.a21@samsung.com <abhishek.a21@samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-29 02:43:36 +0000

    test_runner: Migrate MockWebMediaStreamCenter to Chromium C++ style.
    Changes:
    1) Run clang-format through source and header files.
    2) Rename data member variables to use unix_hacker_ style.
    3) Rename methods to use CamelCase style.
    4) Rename file name to mock_web_media_stream_center.
    
    BUG=331299
    
    Review URL: https://codereview.chromium.org/418303002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286052 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/renderer/test_runner/WebTestInterfaces.cpp
+++ content/shell/renderer/test_runner/WebTestInterfaces.cpp
@@ -5,9 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/shell/renderer/test_runner/WebTestInterfaces.h"
 
-#include "content/shell/renderer/test_runner/MockWebMediaStreamCenter.h"
 #include "content/shell/renderer/test_runner/TestInterfaces.h"
 #include "content/shell/renderer/test_runner/mock_web_audio_device.h"
+#include "content/shell/renderer/test_runner/mock_web_media_stream_center.h"
 #include "content/shell/renderer/test_runner/mock_web_midi_accessor.h"
 #include "content/shell/renderer/test_runner/mock_webrtc_peer_connection_handler.h"
 #include "content/shell/renderer/test_runner/test_runner.h"
