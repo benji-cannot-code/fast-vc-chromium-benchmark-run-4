28be1a72489ba1b8fc8488c6e4129d73cb244cb1
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-29 07:55:22 +0000

    Make IPC macros work with -Wunused-local-typedefs.
    The typedef can be used by the IPC_MESSAGE_HANDLER macro, but that macro isn't
    always used when IPC_BEGIN_MESSAGE_MAP is used. The alternatives are to either
    pass ClassName::HanderFunction to IPC_MESSAGE_HANDLER (like what one has to do
    to use IPC_MESSAGE_FORWARD), or to mark the typedef in IPC_BEGIN_MESSAGE_MAP
    as ALLOW_UNUSED so that it isn't warned on.
    
    BUG=321833
    TBR=brettw
    
    Review URL: https://codereview.chromium.org/422083002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286115 0039d316-1c4b-4281-b951-d872f2087c98

--- ipc/ipc_message_macros.h
+++ ipc/ipc_message_macros.h
@@ -893,7 +893,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #define IPC_BEGIN_MESSAGE_MAP(class_name, msg) \
   { \
-    typedef class_name _IpcMessageHandlerClass; \
+    typedef class_name _IpcMessageHandlerClass ALLOW_UNUSED; \
     void* param__ = NULL; \
     const IPC::Message& ipc_message__ = msg; \
     switch (ipc_message__.type()) {
@@ -906,11 +906,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IPC_DECLTYPE typeof
 #endif
 
-#define IPC_BEGIN_MESSAGE_MAP_WITH_PARAM(class_name, msg, param)               \
-  {                                                                            \
-    typedef class_name _IpcMessageHandlerClass;                                \
-    IPC_DECLTYPE(param) param__ = param;                                       \
-    const IPC::Message& ipc_message__ = msg;                                   \
+#define IPC_BEGIN_MESSAGE_MAP_WITH_PARAM(class_name, msg, param) \
+  {                                                              \
+    typedef class_name _IpcMessageHandlerClass ALLOW_UNUSED;     \
+    IPC_DECLTYPE(param) param__ = param;                         \
+    const IPC::Message& ipc_message__ = msg;                     \
     switch (ipc_message__.type()) {
 
 #define IPC_MESSAGE_FORWARD(msg_class, obj, member_func)                       \
