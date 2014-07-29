4cb9066bbf52a7e61327e010b65fc95a2d79e80f
kouhei@chromium.org <kouhei@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-29 08:18:42 +0000

    SVGElement::parseAttribute resolves property via property map (Step 2)
    This is to replace the existing |SVGElement::parseAttribute| implementation,
    but as there are many callers, we would like to proceed this incrementally.
    a) Introduce |SVGElement::parseAttributeNew| *LANDED*
    b) Convert all overrides |SVGElement::parseAttribute| to
       a |parseAttributeNew| call. <-- This patch
    c) Replace existing |parseAttribute| with |parseAttributeNew|, and remove all
       overrides simply calling |parseAttributeNew|
    
    This patch is (b).
    
    Bonus: Remove {parseAttribute,isSupportedAttribute,svgAttributeChanged} on SVGGElement which was a noop.
    
    BUG=397902
    
    Review URL: https://codereview.chromium.org/420193011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179105 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGGElement.h
+++ third_party/WebKit/Source/core/svg/SVGGElement.h
@@ -37,12 +37,7 @@ protected:
     virtual RenderObject* createRenderer(RenderStyle*) OVERRIDE;
 
 private:
-    bool isSupportedAttribute(const QualifiedName&);
-    virtual void parseAttribute(const QualifiedName&, const AtomicString&) OVERRIDE;
-    virtual void svgAttributeChanged(const QualifiedName&) OVERRIDE;
-
     virtual bool rendererIsNeeded(const RenderStyle&) OVERRIDE;
-
 };
 
 } // namespace blink
