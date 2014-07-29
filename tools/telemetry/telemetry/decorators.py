412265f04310862db67858567d9db4c57d49161e
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-29 07:45:56 +0000

    [Telemetry] Fix @Cache to maintain cache for life of instance for bound methods
    Without this fix the final assert in the unittest fails.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/425613002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286111 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/decorators.py
+++ tools/telemetry/telemetry/decorators.py
@@ -2,8 +2,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright 2014 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+# pylint: disable=W0212
 
 import functools
+import inspect
 import types
 
 
@@ -17,15 +19,18 @@ def Cache(obj):
 
   If CreateFoo() accepts parameters, a separate cached value is maintained
   for each unique parameter combination.
-  """
-  cache = obj.__cache = {}
 
+  Cached methods maintain their cache for the lifetime of the /instance/, while
+  cached functions maintain their cache for the lifetime of the /module/.
+  """
   @functools.wraps(obj)
   def Cacher(*args, **kwargs):
-    key = str(args) + str(kwargs)
-    if key not in cache:
-      cache[key] = obj(*args, **kwargs)
-    return cache[key]
+    cacher = args[0] if inspect.getargspec(obj).args[:1] == ['self'] else obj
+    cacher.__cache = cacher.__cache if hasattr(cacher, '__cache') else {}
+    key = str(obj) + str(args) + str(kwargs)
+    if key not in cacher.__cache:
+      cacher.__cache[key] = obj(*args, **kwargs)
+    return cacher.__cache[key]
   return Cacher
 
 
@@ -88,7 +93,6 @@ def Enabled(*args):
   return _Enabled
 
 
-# pylint: disable=W0212
 def IsEnabled(test, possible_browser):
   """Returns True iff |test| is enabled given the |possible_browser|.
 
