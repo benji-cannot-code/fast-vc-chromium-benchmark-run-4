952fbcde085e981691245e811bc15c3c4bde8096
philipj@opera.com <philipj@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-29 12:18:45 +0000

    Remove HTMLPreElement.wrap IDL attribute
    Intent to Deprecate and Remove:
    https://groups.google.com/a/chromium.org/d/msg/blink-dev/XiDb0IKd2rw/Bb8IPddHejMJ
    
    BUG=398353
    
    Review URL: https://codereview.chromium.org/427533003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179131 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLPreElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLPreElement.cpp
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/CSSValueKeywords.h"
 #include "core/HTMLNames.h"
 #include "core/css/StylePropertySet.h"
-#include "core/frame/UseCounter.h"
 
 namespace blink {
 
@@ -51,12 +50,10 @@ bool HTMLPreElement::isPresentationAttribute(const QualifiedName& name) const
 
 void HTMLPreElement::collectStyleForPresentationAttribute(const QualifiedName& name, const AtomicString& value, MutableStylePropertySet* style)
 {
-    if (name == wrapAttr) {
-        UseCounter::count(document(), UseCounter::HTMLPreElementWrap);
+    if (name == wrapAttr)
         style->setProperty(CSSPropertyWhiteSpace, CSSValuePreWrap);
-    } else {
+    else
         HTMLElement::collectStyleForPresentationAttribute(name, value, style);
-    }
 }
 
 }
