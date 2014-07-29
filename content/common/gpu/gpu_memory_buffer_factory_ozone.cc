5998561b3a22796a58a8b97c41df20ec57798c70
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-29 05:25:17 +0000

    Revert of ui: Add OZONE_NATIVE_BUFFER GPU memory buffer type. (https://codereview.chromium.org/419593002/)
    Reason for revert:
    Failing the compile step on "Linux ChromiumOS Ozone Builder":
    http://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Ozone%20Builder/builds/1093
    
    Original issue's description:
    > ui: Add OZONE_NATIVE_BUFFER GPU memory buffer type.
    > 
    > BUG=
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=286056
    
    TBR=spang@chromium.org,alexst@chromium.org,piman@chromium.org,reveman@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=
    
    Review URL: https://codereview.chromium.org/428463004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286103 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/gpu_memory_buffer_factory_ozone.cc
+++ content/common/gpu/gpu_memory_buffer_factory_ozone.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "ui/gl/gl_image.h"
 #include "ui/gl/gl_image_shared_memory.h"
-#include "ui/ozone/public/gpu_memory_buffer_factory_ozone_native_buffer.h"
 
 namespace content {
 namespace {
@@ -21,29 +20,12 @@ class GpuMemoryBufferFactoryImpl : public GpuMemoryBufferFactory {
       const gfx::Size& size,
       unsigned internalformat,
       unsigned usage) OVERRIDE {
-    switch (handle.type) {
-      case gfx::OZONE_NATIVE_BUFFER:
-        return ui::GpuMemoryBufferFactoryOzoneNativeBuffer::GetInstance()
-                       ->CreateGpuMemoryBuffer(
-                           handle.global_id, size, internalformat, usage)
-                   ? handle
-                   : gfx::GpuMemoryBufferHandle();
-      default:
-        NOTREACHED();
-        return gfx::GpuMemoryBufferHandle();
-    }
+    NOTREACHED();
+    return gfx::GpuMemoryBufferHandle();
   }
   virtual void DestroyGpuMemoryBuffer(
       const gfx::GpuMemoryBufferHandle& handle) OVERRIDE {
-    switch (handle.type) {
-      case gfx::OZONE_NATIVE_BUFFER:
-        ui::GpuMemoryBufferFactoryOzoneNativeBuffer::GetInstance()
-            ->DestroyGpuMemoryBuffer(handle.global_id);
-        break;
-      default:
-        NOTREACHED();
-        break;
-    }
+    NOTREACHED();
   }
   virtual scoped_refptr<gfx::GLImage> CreateImageForGpuMemoryBuffer(
       const gfx::GpuMemoryBufferHandle& handle,
@@ -59,14 +41,6 @@ class GpuMemoryBufferFactoryImpl : public GpuMemoryBufferFactory {
 
         return image;
       }
-      case gfx::OZONE_NATIVE_BUFFER:
-        // Verify that client is the owner of the buffer we're about to use.
-        if (handle.global_id.secondary_id != client_id)
-          return scoped_refptr<gfx::GLImage>();
-
-        return ui::GpuMemoryBufferFactoryOzoneNativeBuffer::GetInstance()
-            ->CreateImageForGpuMemoryBuffer(
-                handle.global_id, size, internalformat);
       default:
         NOTREACHED();
         return scoped_refptr<gfx::GLImage>();
