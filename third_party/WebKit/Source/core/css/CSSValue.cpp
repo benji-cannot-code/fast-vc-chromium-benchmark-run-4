7bfec44bc035816f118f42c2be86bbd51026ed9f
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-29 16:28:25 +0000

    Oilpan: Build fix after r179102
    It looks like r179102 increased sizeof(CSSValue) only in Windows builds with Oilpan enabled.
    I don't know why. At the moment, this CL fixes the size expectation and fixed the compile error.
    
    TBR=yukishiino@chromium.org
    
    Review URL: https://codereview.chromium.org/427803003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179158 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/CSSValue.cpp
+++ third_party/WebKit/Source/core/css/CSSValue.cpp
@@ -59,7 +59,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-struct SameSizeAsCSSValue : public RefCountedWillBeGarbageCollectedFinalized<SameSizeAsCSSValue> {
+struct SameSizeAsCSSValue : public RefCountedWillBeGarbageCollectedFinalized<SameSizeAsCSSValue>, public ScriptWrappableBase {
     uint32_t bitfields;
 };
 
