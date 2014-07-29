a4cf41f0adecb849cc4e567754e0e0ce303a3a84
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-29 16:32:38 +0000

    Remove the #define WebCore blink
    The namespace migration is complete.
    
    TBR=eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/429593004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179159 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/config.h
+++ third_party/WebKit/Source/config.h
@@ -140,8 +140,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #endif
 
-#define WebCore blink
-
 #if OS(LINUX) || OS(ANDROID) || OS(WIN)
 #define WTF_USE_HARFBUZZ 1
 #endif
