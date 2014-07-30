de6ff04ea909936fe387998d474215c562dd3671
spang@chromium.org <spang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-30 22:44:33 +0000

    Cast: Build fix for linux_chromium_chromeos_ozone bots
    This code says OS_LINUX when it should say USE_X11.
    
    TBR=miu
    
    Review URL: https://codereview.chromium.org/426313002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286622 0039d316-1c4b-4281-b951-d872f2087c98

--- media/cast/test/receiver.cc
+++ media/cast/test/receiver.cc
@@ -43,9 +43,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/cast/test/utility/standalone_cast_environment.h"
 #include "net/base/net_util.h"
 
-#if defined(OS_LINUX)
+#if defined(USE_X11)
 #include "media/cast/test/linux_output_window.h"
-#endif  // OS_LINUX
+#endif  // defined(USE_X11)
 
 namespace media {
 namespace cast {
@@ -61,10 +61,10 @@ namespace cast {
 #define DEFAULT_VIDEO_INCOMING_SSRC "11"
 #define DEFAULT_VIDEO_PAYLOAD_TYPE "96"
 
-#if defined(OS_LINUX)
+#if defined(USE_X11)
 const char* kVideoWindowWidth = "1280";
 const char* kVideoWindowHeight = "720";
-#endif  // OS_LINUX
+#endif  // defined(USE_X11)
 
 void GetPorts(int* tx_port, int* rx_port) {
   test::InputBuilder tx_input(
@@ -107,7 +107,7 @@ void GetVideoSsrcs(FrameReceiverConfig* video_config) {
   video_config->incoming_ssrc = input_rx.GetIntInput();
 }
 
-#if defined(OS_LINUX)
+#if defined(USE_X11)
 void GetWindowSize(int* width, int* height) {
   // Resolution values based on sender settings
   test::InputBuilder input_w(
@@ -118,7 +118,7 @@ void GetWindowSize(int* width, int* height) {
       "Choose window height.", kVideoWindowHeight, 176, 1080);
   *height = input_h.GetIntInput();
 }
-#endif  // OS_LINUX
+#endif  // defined(USE_X11)
 
 void GetAudioPayloadtype(FrameReceiverConfig* audio_config) {
   test::InputBuilder input("Choose audio receiver payload type.",
@@ -196,9 +196,9 @@ class NaivePlayer : public InProcessReceiver,
         // arbitrarily, but seems to work well.
         max_frame_age_(base::TimeDelta::FromSeconds(1) * 3 /
                            video_config.max_frame_rate),
-#if defined(OS_LINUX)
+#if defined(USE_X11)
         render_(0, 0, window_width, window_height, "Cast_receiver"),
-#endif  // OS_LINUX
+#endif  // defined(USE_X11)
         num_video_frames_processed_(0),
         num_audio_frames_processed_(0),
         currently_playing_audio_frame_start_(-1) {}
@@ -413,9 +413,9 @@ class NaivePlayer : public InProcessReceiver,
     DCHECK(cast_env()->CurrentlyOn(CastEnvironment::MAIN));
     if (!video_playout_queue_.empty()) {
       const scoped_refptr<VideoFrame> video_frame = PopOneVideoFrame(false);
-#ifdef OS_LINUX
+#if defined(USE_X11)
       render_.RenderFrame(video_frame);
-#endif  // OS_LINUX
+#endif  // defined(USE_X11)
     }
     ScheduleVideoPlayout();
     CheckAVSync();
@@ -507,9 +507,9 @@ class NaivePlayer : public InProcessReceiver,
   const base::TimeDelta max_frame_age_;
 
   // Outputs created, started, and destroyed by this NaivePlayer.
-#ifdef OS_LINUX
+#if defined(USE_X11)
   test::LinuxOutputWindow render_;
-#endif  // OS_LINUX
+#endif  // defined(USE_X11)
   scoped_ptr<AudioOutputStream> audio_output_stream_;
 
   // Video playout queue.
@@ -584,9 +584,9 @@ int main(int argc, char** argv) {
   // Create and start the player.
   int window_width = 0;
   int window_height = 0;
-#if defined(OS_LINUX)
+#if defined(USE_X11)
   media::cast::GetWindowSize(&window_width, &window_height);
-#endif  // OS_LINUX
+#endif  // defined(USE_X11)
   media::cast::NaivePlayer player(cast_environment,
                                   local_end_point,
                                   remote_end_point,
