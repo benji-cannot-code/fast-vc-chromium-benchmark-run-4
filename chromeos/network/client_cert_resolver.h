20eca9cf3735165dc42b53675091afa91e5bc48d
pneubeck@chromium.org <pneubeck@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-30 19:40:09 +0000

    Use correct slot id for client certs in network config.
    - Now all client cert related Shill properties are set through client_cert_util.*
    - Slot id is obtained from the slot in which the private key is stored
    - All client cert pattern resolution now happens in ClientCertResolver
    
    BUG=358366
    
    Review URL: https://codereview.chromium.org/421113002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286583 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/network/client_cert_resolver.h
+++ chromeos/network/client_cert_resolver.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "chromeos/cert_loader.h"
 #include "chromeos/chromeos_export.h"
+#include "chromeos/network/client_cert_util.h"
 #include "chromeos/network/network_policy_observer.h"
 #include "chromeos/network/network_state_handler.h"
 #include "chromeos/network/network_state_handler_observer.h"
@@ -47,6 +48,15 @@ class CHROMEOS_EXPORT ClientCertResolver : public NetworkStateHandlerObserver,
   // to the NSS database. If not set, uses base::WorkerPool.
   void SetSlowTaskRunnerForTest(
       const scoped_refptr<base::TaskRunner>& task_runner);
+
+  // Returns true and sets the Shill properties that have to be configured in
+  // |shill_properties| if the certificate pattern |pattern| could be resolved.
+  // Returns false otherwise and sets empty Shill properties to clear the
+  // certificate configuration.
+  static bool ResolveCertificatePatternSync(
+      const client_cert::ConfigType client_cert_type,
+      const CertificatePattern& pattern,
+      base::DictionaryValue* shill_properties);
 
  private:
    // NetworkStateHandlerObserver overrides
