68e19a87bcefa61563004a0d0e921a5cbd140341
jln@chromium.org <jln@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-30 08:30:04 +0000

    Android seccomp-bpf: fix a few unittests
    This CL fixes the following tests:
    - SandboxBPF.UseOpenBroker
    - SandboxBPF.ErrnoTest
    - SandboxBPF.SimpleCondTest
    
    The cause of failure was assuming that dup(), open() and access() are always
    implemented with specific system calls (https://codereview.chromium.org/226923003).
    
    This CL also introduces two trivial sanity checks tests of our testing framework.
    
    BUG=398611
    
    Review URL: https://codereview.chromium.org/424253002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286444 0039d316-1c4b-4281-b951-d872f2087c98

--- sandbox/linux/seccomp-bpf/bpf_tests_unittest.cc
+++ sandbox/linux/seccomp-bpf/bpf_tests_unittest.cc
@@ -135,6 +135,16 @@ BPF_TEST_C(BPFTest, BPFTestWithInlineTest, EnosysPtracePolicy) {
   BPF_ASSERT(ENOSYS == errno);
 }
 
+const char kHelloMessage[] = "Hello";
+
+BPF_DEATH_TEST_C(BPFTest,
+                 BPFDeathTestWithInlineTest,
+                 DEATH_MESSAGE(kHelloMessage),
+                 EnosysPtracePolicy) {
+  LOG(ERROR) << kHelloMessage;
+  _exit(1);
+}
+
 }  // namespace
 
 }  // namespace sandbox
