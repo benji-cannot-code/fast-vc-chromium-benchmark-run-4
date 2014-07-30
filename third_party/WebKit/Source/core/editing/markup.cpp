772f82d8c93c86684af16dab5e99a4ea60d5e202
ch.dumez@samsung.com <ch.dumez@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-30 20:28:11 +0000

    Use tighter typing in editing: EditingStyle
    Use tighter typing in editing to increase code readability. This CL focuses
    on EditingStyle.
    
    R=leviw@chromium.org
    
    Review URL: https://codereview.chromium.org/423673002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179251 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/markup.cpp
+++ third_party/WebKit/Source/core/editing/markup.cpp
@@ -314,7 +314,7 @@ void StyledMarkupAccumulator::appendElement(StringBuilder& out, Element& element
         if (shouldApplyWrappingStyle(element)) {
             newInlineStyle = m_wrappingStyle->copy();
             newInlineStyle->removePropertiesInElementDefaultStyle(&element);
-            newInlineStyle->removeStyleConflictingWithStyleOfNode(&element);
+            newInlineStyle->removeStyleConflictingWithStyleOfElement(&element);
         } else
             newInlineStyle = EditingStyle::create();
 
