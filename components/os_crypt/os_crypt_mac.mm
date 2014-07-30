fd0e3c3b5d4f00f6d98c58abb583a997df1cccf2
erikchen@chromium.org <erikchen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-30 19:57:12 +0000

    mac: Cache the encryption key.
    Failing to do so causes Chromium to hit the Mac Keychain hundreds of times on
    startup. This presents 2 problems.
    1. Performance. ~40% of CPU usage during startup is spent accessing the
    keychain.
    2. Keychain prompts. Each attempt to access the Keychain causes a prompt if the
    Keychain is locked.
    
    BUG=397308
    
    Review URL: https://codereview.chromium.org/424833004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286585 0039d316-1c4b-4281-b951-d872f2087c98

--- components/os_crypt/os_crypt_mac.mm
+++ components/os_crypt/os_crypt_mac.mm
@@ -8,9 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <CommonCrypto/CommonCryptor.h>  // for kCCBlockSizeAES128
 
 #include "base/command_line.h"
+#include "base/debug/leak_annotations.h"
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/synchronization/lock.h"
 #include "components/os_crypt/keychain_password_mac.h"
 #include "components/os_crypt/os_crypt_switches.h"
 #include "crypto/apple_keychain.h"
@@ -39,10 +41,17 @@ static bool use_mock_keychain = false;
 const char kEncryptionVersionPrefix[] = "v10";
 
 // Generates a newly allocated SymmetricKey object based on the password found
-// in the Keychain.  The generated key is for AES encryption.  Ownership of the
-// key is passed to the caller.  Returns NULL key in the case password access
-// is denied or key generation error occurs.
+// in the Keychain.  The generated key is for AES encryption.  Returns NULL key
+// in the case password access is denied or key generation error occurs.
 crypto::SymmetricKey* GetEncryptionKey() {
+  static crypto::SymmetricKey* cached_encryption_key = NULL;
+  static bool key_is_cached = false;
+  static base::Lock lock;
+  base::AutoLock auto_lock(lock);
+
+  if (key_is_cached)
+    return cached_encryption_key;
+
   static bool mock_keychain_command_line_flag =
       CommandLine::ForCurrentProcess()->HasSwitch(
           os_crypt::switches::kUseMockKeychain);
@@ -56,21 +65,26 @@ crypto::SymmetricKey* GetEncryptionKey() {
     password = encryptor_password.GetPassword();
   }
 
+  // Subsequent code must guarantee that the correct key is cached before
+  // returning.
+  key_is_cached = true;
+
   if (password.empty())
-    return NULL;
+    return cached_encryption_key;
 
   std::string salt(kSalt);
 
-  // Create an encryption key from our password and salt.
-  scoped_ptr<crypto::SymmetricKey> encryption_key(
+  // Create an encryption key from our password and salt. The key is
+  // intentionally leaked.
+  cached_encryption_key =
       crypto::SymmetricKey::DeriveKeyFromPassword(crypto::SymmetricKey::AES,
                                                   password,
                                                   salt,
                                                   kEncryptionIterations,
-                                                  kDerivedKeySizeInBits));
-  DCHECK(encryption_key.get());
-
-  return encryption_key.release();
+                                                  kDerivedKeySizeInBits);
+  ANNOTATE_LEAKING_OBJECT_PTR(cached_encryption_key);
+  DCHECK(cached_encryption_key);
+  return cached_encryption_key;
 }
 
 }  // namespace
@@ -97,13 +111,13 @@ bool OSCrypt::EncryptString(const std::string& plaintext,
     return true;
   }
 
-  scoped_ptr<crypto::SymmetricKey> encryption_key(GetEncryptionKey());
-  if (!encryption_key.get())
+  crypto::SymmetricKey* encryption_key = GetEncryptionKey();
+  if (!encryption_key)
     return false;
 
   std::string iv(kCCBlockSizeAES128, ' ');
   crypto::Encryptor encryptor;
-  if (!encryptor.Init(encryption_key.get(), crypto::Encryptor::CBC, iv))
+  if (!encryptor.Init(encryption_key, crypto::Encryptor::CBC, iv))
     return false;
 
   if (!encryptor.Encrypt(plaintext, ciphertext))
@@ -135,13 +149,13 @@ bool OSCrypt::DecryptString(const std::string& ciphertext,
   std::string raw_ciphertext =
       ciphertext.substr(strlen(kEncryptionVersionPrefix));
 
-  scoped_ptr<crypto::SymmetricKey> encryption_key(GetEncryptionKey());
-  if (!encryption_key.get())
+  crypto::SymmetricKey* encryption_key = GetEncryptionKey();
+  if (!encryption_key)
     return false;
 
   std::string iv(kCCBlockSizeAES128, ' ');
   crypto::Encryptor encryptor;
-  if (!encryptor.Init(encryption_key.get(), crypto::Encryptor::CBC, iv))
+  if (!encryptor.Init(encryption_key, crypto::Encryptor::CBC, iv))
     return false;
 
   if (!encryptor.Decrypt(raw_ciphertext, plaintext))
