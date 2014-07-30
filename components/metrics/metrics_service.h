08f6733b84f8fb573e6d7d5a89ee6f27d3ce79c3
tnagel@chromium.org <tnagel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-30 07:49:35 +0000

    Add two bits of UMA docs.
    BUG=none
    
    Review URL: https://codereview.chromium.org/426953004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286436 0039d316-1c4b-4281-b951-d872f2087c98

--- components/metrics/metrics_service.h
+++ components/metrics/metrics_service.h
@@ -77,6 +77,7 @@ struct SyntheticTrialGroup {
   SyntheticTrialGroup(uint32 trial, uint32 group);
 };
 
+// See metrics_service.cc for a detailed description.
 class MetricsService : public base::HistogramFlattener {
  public:
   // The execution phase of the browser.
