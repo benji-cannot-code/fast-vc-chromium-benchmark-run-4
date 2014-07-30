57b6981bdf1913f8ac63595a2c19087cbb08ae96
dtu@chromium.org <dtu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-30 17:01:13 +0000

    [telemetry] Add IPPET power monitor.
    This power monitor lets our automated testers track power usage on Windows on Intel Sandy Bridge or later.
    
    
    BUG=336558, 153139
    
    Review URL: https://codereview.chromium.org/394923003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286541 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/telemetry/telemetry/core/platform/power_monitor/ippet_power_monitor_unittest.py
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging
+import unittest
+
+from telemetry import decorators
+from telemetry.core.platform import win_platform_backend
+from telemetry.core.platform.power_monitor import ippet_power_monitor
+
+
+class IppetPowerMonitorTest(unittest.TestCase):
+  @decorators.Enabled('win')
+  def testFindOrInstallIppet(self):
+    self.assertTrue(ippet_power_monitor.IppetPath())
+
+  @decorators.Enabled('win')
+  def testIppetRunsWithoutErrors(self):
+    # Very basic test, doesn't validate any output data.
+    platform_backend = win_platform_backend.WinPlatformBackend()
+    power_monitor = ippet_power_monitor.IppetPowerMonitor(platform_backend)
+    if not power_monitor.CanMonitorPower():
+      logging.warning('Test not supported on this platform.')
+      return
+
+    power_monitor.StartMonitoringPower(None)
+    statistics = power_monitor.StopMonitoringPower()
+
+    self.assertEqual(statistics['identifier'], 'ippet')
