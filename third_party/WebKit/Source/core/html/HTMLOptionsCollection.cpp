0d407f8af8a6274790c4726e51f67ec73e788b42
philipj@opera.com <philipj@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-30 10:00:59 +0000

    Remove HTMLOptionsCollection.remove(element)
    Intent to Deprecate and Remove:
    https://groups.google.com/a/chromium.org/d/msg/blink-dev/Iyc84XnZl8w/JiOO3znf-L4J
    
    BUG=398627
    
    Review URL: https://codereview.chromium.org/426513002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179219 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLOptionsCollection.cpp
+++ third_party/WebKit/Source/core/html/HTMLOptionsCollection.cpp
@@ -104,11 +104,6 @@ void HTMLOptionsCollection::remove(int index)
     toHTMLSelectElement(ownerNode()).remove(index);
 }
 
-void HTMLOptionsCollection::remove(HTMLOptionElement* option)
-{
-    return remove(option->index());
-}
-
 int HTMLOptionsCollection::selectedIndex() const
 {
     return toHTMLSelectElement(ownerNode()).selectedIndex();
--- third_party/WebKit/Source/core/html/HTMLOptionsCollection.h
+++ third_party/WebKit/Source/core/html/HTMLOptionsCollection.h
@@ -40,7 +40,6 @@ public:
     void add(PassRefPtrWillBeRawPtr<HTMLOptionElement>, ExceptionState&);
     void add(PassRefPtrWillBeRawPtr<HTMLOptionElement>, int index, ExceptionState&);
     void remove(int index);
-    void remove(HTMLOptionElement*);
 
     int selectedIndex() const;
     void setSelectedIndex(int);
