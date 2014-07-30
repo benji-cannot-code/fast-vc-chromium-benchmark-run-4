825c53670a8c4bc90307e43fa9e618f19b9ef2af
tmdiep@chromium.org <tmdiep@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-30 03:50:42 +0000

    Hide the ephemeral launch button in the app launcher for V1 apps
    Ephemeral V1 apps are no longer supported, so the launch button
    for them should be hidden in the app launcher.
    
    BUG=392776
    TEST=browser_tests
    
    Review URL: https://codereview.chromium.org/397923002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286403 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/app_list/search/webstore/webstore_result.h
+++ chrome/browser/ui/app_list/search/webstore/webstore_result.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/app_list/search/chrome_search_result.h"
 #include "chrome/common/extensions/webstore_install_result.h"
 #include "extensions/browser/extension_registry_observer.h"
+#include "extensions/common/manifest.h"
 #include "url/gurl.h"
 
 class AppListControllerDelegate;
@@ -34,8 +35,13 @@ class WebstoreResult : public ChromeSearchResult,
                  const std::string& app_id,
                  const std::string& localized_name,
                  const GURL& icon_url,
+                 extensions::Manifest::Type item_type,
                  AppListControllerDelegate* controller);
   virtual ~WebstoreResult();
+
+  const std::string& app_id() const { return app_id_; }
+  const GURL& icon_url() const { return icon_url_; }
+  extensions::Manifest::Type item_type() const { return item_type_; }
 
   // ChromeSearchResult overides:
   virtual void Open(int event_flags) OVERRIDE;
@@ -78,13 +84,15 @@ class WebstoreResult : public ChromeSearchResult,
   const std::string app_id_;
   const std::string localized_name_;
   const GURL icon_url_;
+  extensions::Manifest::Type item_type_;
 
   gfx::ImageSkia icon_;
-  base::WeakPtrFactory<WebstoreResult> weak_factory_;
 
   AppListControllerDelegate* controller_;
   extensions::InstallTracker* install_tracker_;  // Not owned.
   extensions::ExtensionRegistry* extension_registry_;  // Not owned.
+
+  base::WeakPtrFactory<WebstoreResult> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(WebstoreResult);
 };
