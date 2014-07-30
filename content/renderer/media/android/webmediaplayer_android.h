ff09c87b6a24185a3c3adcf39a6227bbc33ac0e5
srirama.m@samsung.com <srirama.m@samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-30 08:24:20 +0000

    Eliminate MediaPlayer abstraction(paint APIs- chromium changes)
    BUG=350571
    
    Review URL: https://codereview.chromium.org/409793006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286443 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/android/webmediaplayer_android.h
+++ content/renderer/media/android/webmediaplayer_android.h
@@ -30,6 +30,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/platform/WebMediaPlayer.h"
 #include "third_party/WebKit/public/platform/WebSize.h"
 #include "third_party/WebKit/public/platform/WebURL.h"
+#include "third_party/skia/include/core/SkBitmap.h"
+#include "third_party/skia/include/gpu/GrContext.h"
+#include "third_party/skia/include/gpu/SkGrPixelRef.h"
 #include "ui/gfx/rect_f.h"
 
 namespace base {
@@ -104,6 +107,10 @@ class WebMediaPlayerAndroid : public blink::WebMediaPlayer,
   virtual void setPoster(const blink::WebURL& poster) OVERRIDE;
 
   // Methods for painting.
+  // FIXME: This path "only works" on Android. It is a workaround for the
+  // issue that Skia could not handle Android's GL_TEXTURE_EXTERNAL_OES texture
+  // internally. It should be removed and replaced by the normal paint path.
+  // https://code.google.com/p/skia/issues/detail?id=1189
   virtual void paint(blink::WebCanvas* canvas,
                      const blink::WebRect& rect,
                      unsigned char alpha);
@@ -278,6 +285,11 @@ class WebMediaPlayerAndroid : public blink::WebMediaPlayer,
   // NULL immediately and reset.
   void SetDecryptorReadyCB(const media::DecryptorReadyCB& decryptor_ready_cb);
 
+  bool EnsureTextureBackedSkBitmap(GrContext* gr, SkBitmap& bitmap,
+                                   const blink::WebSize& size,
+                                   GrSurfaceOrigin origin,
+                                   GrPixelConfig config);
+
   blink::WebFrame* const frame_;
 
   blink::WebMediaPlayerClient* const client_;
@@ -436,6 +448,8 @@ class WebMediaPlayerAndroid : public blink::WebMediaPlayer,
   // systems, a browser side CDM will be used and we set CDM by calling
   // player_manager_->SetCdm() directly.
   media::DecryptorReadyCB decryptor_ready_cb_;
+
+  SkBitmap bitmap_;
 
   // NOTE: Weak pointers must be invalidated before all other member variables.
   base::WeakPtrFactory<WebMediaPlayerAndroid> weak_factory_;
