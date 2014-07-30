f68b982a96a813d15fb526d6ae0889443aeecdbd
pavely@chromium.org <pavely@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-30 05:01:48 +0000

    Pass device_id to IssueToken endpoint to workaround refresh token per user limit
    Session scoped device id is available from SigninClient. This change
    passes it to IssueToken endpoint through SupervisedUserRefreshTokenFetcher.
    
    BUG=382968
    R=pam@chromium.org
    
    Review URL: https://codereview.chromium.org/415703003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286408 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/supervised_user/supervised_user_refresh_token_fetcher.cc
+++ chrome/browser/supervised_user/supervised_user_refresh_token_fetcher.cc
@@ -41,6 +41,10 @@ static const char kIssueTokenBodyFormat[] =
     "&profile_id=%s"
     "&device_name=%s";
 
+// kIssueTokenBodyFormatDeviceIdAddendum is appended to kIssueTokenBodyFormat
+// if device_id is provided.
+static const char kIssueTokenBodyFormatDeviceIdAddendum[] = "&device_id=%s";
+
 static const char kAuthorizationHeaderFormat[] =
     "Authorization: Bearer %s";
 
@@ -55,6 +59,7 @@ class SupervisedUserRefreshTokenFetcherImpl
   SupervisedUserRefreshTokenFetcherImpl(
       OAuth2TokenService* oauth2_token_service,
       const std::string& account_id,
+      const std::string& device_id,
       URLRequestContextGetter* context);
   virtual ~SupervisedUserRefreshTokenFetcherImpl();
 
@@ -93,6 +98,7 @@ class SupervisedUserRefreshTokenFetcherImpl
                                       const std::string& token);
   OAuth2TokenService* oauth2_token_service_;
   std::string account_id_;
+  std::string device_id_;
   URLRequestContextGetter* context_;
 
   std::string device_name_;
@@ -109,10 +115,12 @@ class SupervisedUserRefreshTokenFetcherImpl
 SupervisedUserRefreshTokenFetcherImpl::SupervisedUserRefreshTokenFetcherImpl(
     OAuth2TokenService* oauth2_token_service,
     const std::string& account_id,
+    const std::string& device_id,
     URLRequestContextGetter* context)
     : OAuth2TokenService::Consumer("supervised_user"),
       oauth2_token_service_(oauth2_token_service),
       account_id_(account_id),
+      device_id_(device_id),
       context_(context),
       access_token_expired_(false) {}
 
@@ -165,6 +173,11 @@ void SupervisedUserRefreshTokenFetcherImpl::OnGetTokenSuccess(
       net::EscapeUrlEncodedData(kChromeSyncSupervisedOAuth2Scope, true).c_str(),
       net::EscapeUrlEncodedData(supervised_user_id_, true).c_str(),
       net::EscapeUrlEncodedData(device_name_, true).c_str());
+  if (!device_id_.empty()) {
+    body.append(base::StringPrintf(
+        kIssueTokenBodyFormatDeviceIdAddendum,
+        net::EscapeUrlEncodedData(device_id_, true).c_str()));
+  }
   url_fetcher_->SetUploadData("application/x-www-form-urlencoded", body);
 
   url_fetcher_->Start();
@@ -278,10 +291,12 @@ scoped_ptr<SupervisedUserRefreshTokenFetcher>
 SupervisedUserRefreshTokenFetcher::Create(
     OAuth2TokenService* oauth2_token_service,
     const std::string& account_id,
+    const std::string& device_id,
     URLRequestContextGetter* context) {
   scoped_ptr<SupervisedUserRefreshTokenFetcher> fetcher(
       new SupervisedUserRefreshTokenFetcherImpl(oauth2_token_service,
                                                 account_id,
+                                                device_id,
                                                 context));
   return fetcher.Pass();
 }
