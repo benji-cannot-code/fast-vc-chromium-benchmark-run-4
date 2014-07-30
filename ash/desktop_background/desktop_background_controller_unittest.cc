7697dad2230fac8466b1ff893d388f7dd9cb6a89
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-30 14:59:38 +0000

    Reland: Introduce NON_ZERO_DURATION for animation unit tests
    Animations are usually disabled in unit tests for performance. However, to
    test the animation system itself some tests request "normal" durations.
    Under some conditions (remote desktop, animation disabled for accessibility)
    the "normal" duration could still be zero.
    
    This was causing test failures and use-after-frees in ash_unittests on the
    drmemory bots and for developers using remote desktop.
    
    Introduce NON_ZERO_DURATION to signal that animation must be enabled and the
    duration must be non-zero, but other than that can be as short as possible.
    
    (Original patch https://codereview.chromium.org/420013002/ was correlated with
    app_list_unittests failures in AppListMainViewTest.DragLastItemFromFolderAndDropAtLastSlot
    AppListViewTestAura.ReshowWithOpenFolder but testing locally and on the try
    bots can't repro it, so calling it flake.)
    
    BUG=397478, 396969
    TEST=ash_unittests on drmemory, Windows and Chrome OS bots
    TBR=oshima@chromium.org
    
    Review URL: https://codereview.chromium.org/426973002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286517 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/desktop_background/desktop_background_controller_unittest.cc
+++ ash/desktop_background/desktop_background_controller_unittest.cc
@@ -69,7 +69,8 @@ void RunAnimationForWidget(views::Widget* widget) {
 class DesktopBackgroundControllerTest : public test::AshTestBase {
  public:
   DesktopBackgroundControllerTest()
-      : controller_(NULL) {
+      : controller_(NULL),
+        wallpaper_delegate_(NULL) {
   }
   virtual ~DesktopBackgroundControllerTest() {}
 
@@ -150,8 +151,8 @@ TEST_F(DesktopBackgroundControllerTest, BasicReparenting) {
 
 TEST_F(DesktopBackgroundControllerTest, ControllerOwnership) {
   // We cannot short-circuit animations for this test.
-  ui::ScopedAnimationDurationScaleMode normal_duration_mode(
-      ui::ScopedAnimationDurationScaleMode::NORMAL_DURATION);
+  ui::ScopedAnimationDurationScaleMode test_duration_mode(
+      ui::ScopedAnimationDurationScaleMode::NON_ZERO_DURATION);
 
   // Create wallpaper and background view.
   DesktopBackgroundController* controller =
@@ -182,8 +183,8 @@ TEST_F(DesktopBackgroundControllerTest, ControllerOwnership) {
 // move all desktop views if there are more than one.
 TEST_F(DesktopBackgroundControllerTest, BackgroundMovementDuringUnlock) {
   // We cannot short-circuit animations for this test.
-  ui::ScopedAnimationDurationScaleMode normal_duration_mode(
-      ui::ScopedAnimationDurationScaleMode::NORMAL_DURATION);
+  ui::ScopedAnimationDurationScaleMode test_duration_mode(
+      ui::ScopedAnimationDurationScaleMode::NON_ZERO_DURATION);
 
   // Reset wallpaper state, see ControllerOwnership above.
   DesktopBackgroundController* controller =
@@ -230,8 +231,8 @@ TEST_F(DesktopBackgroundControllerTest, BackgroundMovementDuringUnlock) {
 // animation and replace current wallpaper before next animation starts.
 TEST_F(DesktopBackgroundControllerTest, ChangeWallpaperQuick) {
   // We cannot short-circuit animations for this test.
-  ui::ScopedAnimationDurationScaleMode normal_duration_mode(
-      ui::ScopedAnimationDurationScaleMode::NORMAL_DURATION);
+  ui::ScopedAnimationDurationScaleMode test_duration_mode(
+      ui::ScopedAnimationDurationScaleMode::NON_ZERO_DURATION);
 
   // Reset wallpaper state, see ControllerOwnership above.
   DesktopBackgroundController* controller =
