abb899791c87f014b328ebde5fa2e72bc482408b
shrikant@chromium.org <shrikant@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-30 08:56:17 +0000

    Relanding after a fix: Attempt to load system fonts using Custom Font interface of directwrite instead of system font collection interface. The theory is that system font collection may sometime appear to hang or just not load fonts on systems with large number of fonts (in order of 1000+). With custom font collection we want to take control of font loading part and explore ways to improve it further.
    This particular approach is loading all fonts inside renderer, we can potentially use this same code load fonts from browser side as and when required.
    
    
    BUG=394164
    R=cpu@chromium.org, jam@chromium.org, scottmg@chromium.org
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=285977
    
    Review URL: https://codereview.chromium.org/406693003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286449 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_font_warmup_win.cc
+++ content/renderer/render_font_warmup_win.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/win/iat_patch_function.h"
 #include "base/win/windows_version.h"
+#include "content/renderer/renderer_font_platform_win.h"
 #include "third_party/WebKit/public/web/win/WebFontRendering.h"
 #include "third_party/skia/include/core/SkPaint.h"
 #include "third_party/skia/include/ports/SkFontMgr.h"
@@ -127,7 +128,6 @@ void PatchServiceManagerCalls() {
   DCHECK(patched == 0);
 }
 
-
 // Windows-only DirectWrite support. These warm up the DirectWrite paths
 // before sandbox lock down to allow Skia access to the Font Manager service.
 void CreateDirectWriteFactory(IDWriteFactory** factory) {
@@ -159,6 +159,25 @@ void CreateDirectWriteFactory(IDWriteFactory** factory) {
                                  reinterpret_cast<IUnknown**>(factory))));
 }
 
+HRESULT STDMETHODCALLTYPE StubFontCollection(IDWriteFactory* factory,
+                                             IDWriteFontCollection** col,
+                                             BOOL checkUpdates) {
+  // We always return pre-created font collection from here.
+  IDWriteFontCollection* custom_collection = GetCustomFontCollection(factory);
+  DCHECK(custom_collection != NULL);
+  *col = custom_collection;
+  return S_OK;
+}
+
+void PatchDWriteFactory(IDWriteFactory* factory) {
+  const unsigned int kGetSystemFontCollectionVTableIndex = 3;
+
+  PROC* vtable = *reinterpret_cast<PROC**>(factory);
+  PROC* function_ptr = &vtable[kGetSystemFontCollectionVTableIndex];
+  void* stub_function = &StubFontCollection;
+  base::win::ModifyCode(function_ptr, &stub_function, sizeof(PROC));
+}
+
 }  // namespace
 
 void DoPreSandboxWarmupForTypeface(SkTypeface* typeface) {
@@ -172,6 +191,11 @@ SkFontMgr* GetPreSandboxWarmupFontMgr() {
   if (!g_warmup_fontmgr) {
     IDWriteFactory* factory;
     CreateDirectWriteFactory(&factory);
+
+    IDWriteFontCollection* collection = GetCustomFontCollection(factory);
+
+    PatchDWriteFactory(factory);
+
     blink::WebFontRendering::setDirectWriteFactory(factory);
     g_warmup_fontmgr = SkFontMgr_New_DirectWrite(factory);
   }
---
+++ content/renderer/renderer_font_platform_win.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_RENDERER_RENDERER_FONT_PLATFORM_WIN_H_
+#define CHROME_RENDERER_RENDERER_FONT_PLATFORM_WIN_H_
+
+struct IDWriteFactory;
+struct IDWriteFontCollection;
+
+namespace content {
+
+IDWriteFontCollection* GetCustomFontCollection(IDWriteFactory* factory);
+
+}  // namespace content
+
+#endif  // CHROME_RENDERER_RENDERER_FONT_PLATFORM_WIN_H_
