c843c4732a1cb34dea05e36b2a0d861c8c1a6e38
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-30 18:09:21 +0000

    Linux: Fix bash "unset variable" error and fix the path to the out/Release directory in the official build scripts.
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/426173002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286561 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/linux/debian/build.sh
+++ chrome/installer/linux/debian/build.sh
@@ -217,9 +217,7 @@ fi
 # call cleanup() on exit
 trap cleanup 0
 process_opts "$@"
-if [ ! "$BUILDDIR" ]; then
-  BUILDDIR=$(readlink -f "${SCRIPTDIR}/../../../../../out/Release")
-fi
+BUILDDIR=${BUILDDIR:=$(readlink -f "${SCRIPTDIR}/../../../../out/Release")}
 
 source ${BUILDDIR}/installer/common/installer.include
 
