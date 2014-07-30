e73d6183bfb2336d41565aa889baeac920ae1cbd
jdduke@chromium.org <jdduke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-30 03:42:53 +0000

    [Android] Properly acquire themed selection handles on L
    The Android L selection handle resources are tinted. Consequently, it is not
    sufficient to simply extract the Bitmap from the BitmapDrawable when loading
    the resource into a native SkBitmap.  Instead, raster the Drawable to a canvas,
    automatically obtaining the themed tinting.
    
    Review URL: https://codereview.chromium.org/420313004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286401 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/input/HandleViewResources.java
+++ content/public/android/java/src/org/chromium/content/browser/input/HandleViewResources.java
@@ -11,7 +11,6 @@ import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 
 import org.chromium.base.CalledByNative;
@@ -69,17 +68,12 @@ public class HandleViewResources {
 
         Drawable drawable = getHandleDrawable(context, attrs);
         assert drawable != null;
-        if (drawable instanceof BitmapDrawable) {
-            final Bitmap drawableBitmap = ((BitmapDrawable)drawable).getBitmap();
-            if (drawableBitmap != null && drawableBitmap.getConfig() == config) {
-                return drawableBitmap;
-            }
-        }
 
         final int width = drawable.getIntrinsicWidth();
         final int height = drawable.getIntrinsicHeight();
         Bitmap canvasBitmap = Bitmap.createBitmap(width, height, config);
         Canvas canvas = new Canvas(canvasBitmap);
+        drawable.setBounds(0, 0, width, height);
         drawable.draw(canvas);
         return canvasBitmap;
     }
