6485384d838d269980a215b3dd3911034ebfa0c8
philipj@opera.com <philipj@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-31 17:16:16 +0000

    Add a UseCounter for the composite attribute on <img>
    This was added for Apple's Dashboard in 2004:
    http://trac.webkit.org/changeset/6491
    
    Hopefully it's not used on the Web and can be removed.
    
    Remove the FIXME as it certainly shouldn't be expanded.
    
    Review URL: https://codereview.chromium.org/438623002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179326 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLImageElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLImageElement.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/Attribute.h"
 #include "core/dom/NodeTraversal.h"
 #include "core/fetch/ImageResource.h"
+#include "core/frame/UseCounter.h"
 #include "core/html/HTMLAnchorElement.h"
 #include "core/html/HTMLCanvasElement.h"
 #include "core/html/HTMLFormElement.h"
@@ -197,10 +198,11 @@ void HTMLImageElement::parseAttribute(const QualifiedName& name, const AtomicStr
     } else if (name == usemapAttr) {
         setIsLink(!value.isNull());
     } else if (name == compositeAttr) {
-        // FIXME: images don't support blend modes in their compositing attribute.
         blink::WebBlendMode blendOp = blink::WebBlendModeNormal;
         if (!parseCompositeAndBlendOperator(value, m_compositeOperator, blendOp))
             m_compositeOperator = CompositeSourceOver;
+        else if (m_compositeOperator != CompositeSourceOver)
+            UseCounter::count(document(), UseCounter::HTMLImageElementComposite);
     } else {
         HTMLElement::parseAttribute(name, value);
     }
