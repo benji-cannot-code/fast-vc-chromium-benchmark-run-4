bece197d7211d3c2e95e54a4a5b44dc1d46ef91e
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-31 03:34:07 +0000

    Check for the existence a SpecialStoragePolicy before using it.
    There may not be a SpecialStoragePolicy if extensions are disabled.
    
    Review URL: https://codereview.chromium.org/427873004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286710 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/appcache/appcache_storage_impl.cc
+++ content/browser/appcache/appcache_storage_impl.cc
@@ -82,7 +82,7 @@ void ClearSessionOnlyOrigins(
     return;
 
   bool has_session_only_appcaches =
-      special_storage_policy.get() &&
+      special_storage_policy &&
       special_storage_policy->HasSessionOnlyOrigins();
 
   // Clearning only session-only databases, and there are none.
@@ -101,11 +101,11 @@ void ClearSessionOnlyOrigins(
   }
 
   std::set<GURL>::const_iterator origin;
+  DCHECK(special_storage_policy);
   for (origin = origins.begin(); origin != origins.end(); ++origin) {
     if (!special_storage_policy->IsStorageSessionOnly(*origin))
       continue;
-    if (special_storage_policy.get() &&
-        special_storage_policy->IsStorageProtected(*origin))
+    if (special_storage_policy->IsStorageProtected(*origin))
       continue;
 
     std::vector<AppCacheDatabase::GroupRecord> groups;
@@ -385,7 +385,7 @@ void AppCacheStorageImpl::GetAllInfoTask::Run() {
 }
 
 void AppCacheStorageImpl::GetAllInfoTask::RunCompleted() {
-  DCHECK(delegates_.size() == 1);
+  DCHECK_EQ(1U, delegates_.size());
   FOR_EACH_DELEGATE(delegates_, OnAllInfo(info_collection_.get()));
 }
 
