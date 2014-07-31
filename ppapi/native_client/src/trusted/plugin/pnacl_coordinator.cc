22b5423c16421cdaa36db51d951f3dbf47cc095c
teravest@chromium.org <teravest@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-31 21:45:46 +0000

    Pepper: Move some histogram code out of plugin.
    This is a tiny change that moves PNaCl cache hit histogram logic out of the
    trusted plugin.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/423363003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286897 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/native_client/src/trusted/plugin/pnacl_coordinator.cc
+++ ppapi/native_client/src/trusted/plugin/pnacl_coordinator.cc
@@ -53,11 +53,6 @@ void HistogramRatio(pp::UMAPrivate& uma,
                             kRatioBuckets);
 }
 
-void HistogramEnumerateTranslationCache(pp::UMAPrivate& uma, bool hit) {
-  uma.HistogramEnumeration("NaCl.Perf.PNaClCache.IsHit",
-                           hit, 2);
-}
-
 nacl::string GetArchitectureAttributes(Plugin* plugin) {
   pp::Var attrs_var(pp::PASS_REF,
                     plugin->nacl_interface()->GetCpuFeatureAttrs());
@@ -322,7 +317,6 @@ void PnaclCoordinator::OpenBitcodeStream() {
 }
 
 void PnaclCoordinator::BitcodeStreamCacheHit(PP_FileHandle handle) {
-  HistogramEnumerateTranslationCache(plugin_->uma_interface(), true);
   if (handle == PP_kInvalidFileHandle) {
     ReportNonPpapiError(
         PP_NACL_ERROR_PNACL_CREATE_TEMP,
@@ -337,7 +331,6 @@ void PnaclCoordinator::BitcodeStreamCacheHit(PP_FileHandle handle) {
 }
 
 void PnaclCoordinator::BitcodeStreamCacheMiss(int64_t expected_pexe_size) {
-  HistogramEnumerateTranslationCache(plugin_->uma_interface(), false);
   expected_pexe_size_ = expected_pexe_size;
 
   for (int i = 0; i < split_module_count_; i++) {
