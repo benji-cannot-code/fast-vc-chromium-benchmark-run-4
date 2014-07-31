7f76bc170bc3de37483ec06c0ef6d6af45e1793a
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-31 12:51:43 +0000

    Add HostExtensionSessionManager to help ClientSession handle extensions.
    This CL changes the HostExtension[Session] interface:
    1. Each HostExtension may optionally specify one required capability; if the client does not offer the capability then the HostExtension is not instantiated for the session.
    2. Each HostExtensionSession may optionally wrap or replace the video encoder or capturer for the session.
    3. HostExtension[Session]s are passed the ClientSession's ClientSessionControl and ClientStub interfaces, rather than the concrete ClientSession class itself.
    4. HostExtensionSessions may force the video pipeline to be re-created, if necessary. This will be used by the VideoFrameRecorder extension to allow it to wrap the VideoEncoder when it is first created.
    
    This CL also changes the host implementation and tests:
    1. ChromotingHost passes the set of configured HostExtensions to each ClientSession to work with, rather than creating & applying HostExtensionSession instances directly.
    2. All logic for managing HostExtension[Session]s is moved out of ClientSession into the new HostExtensionSessionManager sub-component.
    3. The FakeExtension test implementation of HostExtension is moved out of the ClientSession unittests implementation, and updated to allow testing of e.g. encoder wrapping capabilities.
    
    Review URL: https://codereview.chromium.org/402233003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286773 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/chromoting_host_unittest.cc
+++ remoting/host/chromoting_host_unittest.cc
@@ -196,7 +196,8 @@ class ChromotingHostTest : public testing::Test {
         connection.Pass(),
         desktop_environment_factory_.get(),
         base::TimeDelta(),
-        NULL));
+        NULL,
+        std::vector<HostExtension*>()));
 
     connection_ptr->set_host_stub(client.get());
 
---
+++ remoting/host/host_extension_session.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "remoting/host/host_extension_session.h"
+
+#include "remoting/codec/video_encoder.h"
+#include "third_party/webrtc/modules/desktop_capture/screen_capturer.h"
+
+namespace remoting {
+
+scoped_ptr<webrtc::ScreenCapturer> HostExtensionSession::OnCreateVideoCapturer(
+    scoped_ptr<webrtc::ScreenCapturer> capturer) {
+  return capturer.Pass();
+}
+
+scoped_ptr<VideoEncoder> HostExtensionSession::OnCreateVideoEncoder(
+    scoped_ptr<VideoEncoder> encoder) {
+  return encoder.Pass();
+}
+
+bool HostExtensionSession::ModifiesVideoPipeline() const {
+  return false;
+}
+
+} // namespace remoting
