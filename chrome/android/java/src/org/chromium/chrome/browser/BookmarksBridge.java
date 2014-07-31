ce1216d74972e788978f14a7f290094b67268ecf
kkimlabs@chromium.org <kkimlabs@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-31 23:05:03 +0000

    [Android] Add BookmarksBridge JNI function checking bookmark existance.
    BUG=386785
    
    Review URL: https://codereview.chromium.org/430473009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286910 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/android/java/src/org/chromium/chrome/browser/BookmarksBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/BookmarksBridge.java
@@ -235,6 +235,14 @@ public class BookmarksBridge {
     }
 
     /**
+     * @return Whether the given bookmark exist in the current bookmark model, e.g., not deleted.
+     */
+    public boolean doesBookmarkExist(BookmarkId id) {
+        assert mIsNativeBookmarkModelLoaded;
+        return nativeDoesBookmarkExist(mNativeBookmarksBridge, id.mId, id.mType);
+    }
+
+    /**
      * Fetches the bookmarks of the given folder. This is an always-synchronous version of another
      * getBookmarksForForder function.
      *
@@ -417,6 +425,7 @@ public class BookmarksBridge {
             String title);
     private native void nativeSetBookmarkUrl(long nativeBookmarksBridge, long id, int type,
             String url);
+    private native boolean nativeDoesBookmarkExist(long nativeBookmarksBridge, long id, int type);
     private native void nativeGetBookmarksForFolder(long nativeBookmarksBridge,
             BookmarkId folderId, BookmarksCallback callback,
             List<BookmarkItem> bookmarksList);
