67ba1144d9014d82a92043c24f9d4a9aa94bcf07
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-31 19:50:11 +0000

    Move implementation from .h into .cc file.
    TBR=noamsml@chromium.org
    
    Review URL: https://codereview.chromium.org/428393002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286871 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/gcd_private/gcd_private_apitest.cc
+++ chrome/browser/extensions/api/gcd_private/gcd_private_apitest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/gcd_private/gcd_private_api.h"
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/browser/extensions/extension_service.h"
+#include "chrome/browser/local_discovery/gcd_api_flow.h"
 #include "chrome/common/extensions/api/mdns.h"
 #include "extensions/common/switches.h"
 #include "net/url_request/test_url_fetcher_factory.h"
