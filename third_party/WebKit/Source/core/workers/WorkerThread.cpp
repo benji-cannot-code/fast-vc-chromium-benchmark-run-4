d91a1f521788283fc16a8cb1ac09fa88763f4786
nasko@chromium.org <nasko@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-31 19:19:29 +0000

    Follow-up fixes for WorkerThread.
    In my initial patch (https://codereview.chromium.org/438523002/) to move WorkerThread to WebThread there were two issues:
    * When WorkerThread::stop is called, it doesn't kill the debug message queue
    * The shared timer didn't account for WorkerGlobalScope in closing state.
    
    This CL fixes those two issues.
    
    BUG=301515
    
    Review URL: https://codereview.chromium.org/436663002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179339 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/workers/WorkerThread.cpp
+++ third_party/WebKit/Source/core/workers/WorkerThread.cpp
@@ -77,8 +77,8 @@ unsigned WorkerThread::workerThreadCount()
 
 class WorkerSharedTimer : public SharedTimer {
 public:
-    explicit WorkerSharedTimer(blink::WebThread* webThread)
-        : m_thread(webThread)
+    explicit WorkerSharedTimer(WorkerThread* workerThread)
+        : m_workerThread(workerThread)
         , m_nextFireTime(0.0)
         , m_running(false)
     { }
@@ -106,7 +106,7 @@ public:
 
         m_running = true;
         m_nextFireTime = currentTime() + interval;
-        m_thread->postDelayedTask(new Task(WTF::bind(&WorkerSharedTimer::OnTimeout, this)), delay);
+        m_workerThread->webThread()->postDelayedTask(new Task(WTF::bind(&WorkerSharedTimer::OnTimeout, this)), delay);
     }
 
     virtual void stop()
@@ -119,11 +119,11 @@ public:
 private:
     void OnTimeout()
     {
-        if (m_sharedTimerFunction && m_running)
+        if (m_sharedTimerFunction && m_running && !m_workerThread->workerGlobalScope()->isClosing())
             m_sharedTimerFunction();
     }
 
-    WebThread* m_thread;
+    WorkerThread* m_workerThread;
     SharedTimerFunction m_sharedTimerFunction;
     double m_nextFireTime;
     bool m_running;
@@ -231,7 +231,7 @@ void WorkerThread::initialize()
         ThreadState::attach();
         m_workerGlobalScope = createWorkerGlobalScope(m_startupData.release());
 
-        m_sharedTimer = adoptPtr(new WorkerSharedTimer(m_thread.get()));
+        m_sharedTimer = adoptPtr(new WorkerSharedTimer(this));
         PlatformThreadData::current().threadTimers().setSharedTimer(m_sharedTimer.get());
 
         if (m_terminated) {
@@ -362,6 +362,7 @@ void WorkerThread::stop()
     m_workerGlobalScope->script()->scheduleExecutionTermination();
     m_workerGlobalScope->wasRequestedToTerminate();
     InspectorInstrumentation::didKillAllExecutionContextTasks(m_workerGlobalScope.get());
+    m_debuggerMessageQueue.kill();
     postTask(WorkerThreadShutdownStartTask::create());
     m_terminated = true;
 }
