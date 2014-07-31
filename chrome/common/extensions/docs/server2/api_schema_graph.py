bbe3d1efc180098c4f2ec5efd83ce0668e2a09be
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-31 22:57:12 +0000

    Docserver: Add missing logging import.
    TBR=jyasskin@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/434803002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286909 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/server2/api_schema_graph.py
+++ chrome/common/extensions/docs/server2/api_schema_graph.py
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 import json
+import logging
+
 from api_models import GetNodeCategories
 from collections import Iterable, Mapping
 
