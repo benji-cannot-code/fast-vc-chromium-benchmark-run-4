78984a3fd28a1ca54cf575be57c344ec4b6863a5
timloh@chromium.org <timloh@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-31 04:45:57 +0000

    Simplify StyleBuilder handling of font-stretch
    The generated code basically does what we want for font-stretch, so we
    should use it instead of defining custom handlers.
    
    BUG=374530
    
    Review URL: https://codereview.chromium.org/432523003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179286 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/resolver/FontBuilder.h
+++ third_party/WebKit/Source/core/css/resolver/FontBuilder.h
@@ -100,6 +100,7 @@ public:
     static FontStyle initialStyle() { return FontStyleNormal; }
     static FontDescription::Kerning initialKerning() { return FontDescription::AutoKerning; }
     static FontSmoothingMode initialFontSmoothing() { return AutoSmoothing; }
+    static FontStretch initialStretch() { return FontStretchNormal; }
 
     friend class FontDescriptionChangeScope;
 
