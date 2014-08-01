8a2972159d14a11ce34c4bd865cd71d40cfb8f36
mrunal.kapade@intel.com <mrunal.kapade@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-01 01:03:57 +0000

    Move TTS related command line switch to content
    This part of an effort to move TTS to content
    BUG=347045
    
    Review URL: https://codereview.chromium.org/434473002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286930 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/speech/tts_linux.cc
+++ chrome/browser/speech/tts_linux.cc
@@ -13,8 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/singleton.h"
 #include "base/synchronization/lock.h"
 #include "chrome/browser/speech/tts_platform.h"
-#include "chrome/common/chrome_switches.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/common/content_switches.h"
 
 #include "library_loaders/libspeechd.h"
 
