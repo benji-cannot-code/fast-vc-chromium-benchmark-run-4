5dc7ba72111bbc991987e7c5ce45a6d96aa73116
fs@opera.com <fs@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-01 06:46:44 +0000

    Add support for case-insensitive attribute value selectors
    Selectors Level 4 adds the syntax '*[foo="bar" i]' for making an attribute
    value selector case-insensitive. Add support for that.
    
    Does not alter the actual case-sensitivity match - i.e. matching is
    performed using Unicode case-insensitivity rather than ASCII which
    the spec expects.
    
    Based on patch by <jl@opera.com>.
    
    BUG=366563
    
    Review URL: https://codereview.chromium.org/330043003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179370 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/CSSSelector.h
+++ third_party/WebKit/Source/core/css/CSSSelector.h
@@ -240,6 +240,11 @@ namespace blink {
             RightBottomMarginBox,
         };
 
+        enum AttributeMatchType {
+            CaseSensitive,
+            CaseInsensitive,
+        };
+
         PseudoType pseudoType() const
         {
             if (m_pseudoType == PseudoNotParsed)
@@ -263,6 +268,7 @@ namespace blink {
         // how you use the returned QualifiedName.
         // http://www.w3.org/TR/css3-selectors/#attrnmsp
         const QualifiedName& attribute() const;
+        AttributeMatchType attributeMatchType() const;
         // Returns the argument of a parameterized selector. For example, nth-child(2) would have an argument of 2.
         const AtomicString& argument() const { return m_hasRareData ? m_data.m_rareData->m_argument : nullAtom; }
         const CSSSelectorList* selectorList() const { return m_hasRareData ? m_data.m_rareData->m_selectorList.get() : 0; }
@@ -273,7 +279,7 @@ namespace blink {
 #endif
 
         void setValue(const AtomicString&);
-        void setAttribute(const QualifiedName&);
+        void setAttribute(const QualifiedName&, AttributeMatchType);
         void setArgument(const AtomicString&);
         void setSelectorList(PassOwnPtr<CSSSelectorList>);
         void setMatchUserAgentOnly();
@@ -345,10 +351,19 @@ namespace blink {
 
             bool parseNth();
             bool matchNth(int count);
+            int nthAValue() const { return m_bits.m_nth.m_a; }
+            void setNthAValue(int nthA) { m_bits.m_nth.m_a = nthA; }
+            int nthBValue() const { return m_bits.m_nth.m_b; }
+            void setNthBValue(int nthB) { m_bits.m_nth.m_b = nthB; }
 
             AtomicString m_value;
-            int m_a; // Used for :nth-*
-            int m_b; // Used for :nth-*
+            union {
+                struct {
+                    int m_a; // Used for :nth-*
+                    int m_b; // Used for :nth-*
+                } m_nth;
+                AttributeMatchType m_attributeMatchType; // used for attribute selector (with value)
+            } m_bits;
             QualifiedName m_attribute; // used for attribute selector
             AtomicString m_argument; // Used for :contains, :lang, :nth-*
             OwnPtr<CSSSelectorList> m_selectorList; // Used for :-webkit-any and :not
@@ -371,6 +386,13 @@ inline const QualifiedName& CSSSelector::attribute() const
     ASSERT(isAttributeSelector());
     ASSERT(m_hasRareData);
     return m_data.m_rareData->m_attribute;
+}
+
+inline CSSSelector::AttributeMatchType CSSSelector::attributeMatchType() const
+{
+    ASSERT(isAttributeSelector());
+    ASSERT(m_hasRareData);
+    return m_data.m_rareData->m_bits.m_attributeMatchType;
 }
 
 inline bool CSSSelector::matchesPseudoElement() const
