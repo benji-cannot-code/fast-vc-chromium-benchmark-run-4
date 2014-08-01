6fad38955d0667b355f4286b9765229d2eb3c64c
fmeawad@chromium.org <fmeawad@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-01 01:01:42 +0000

    Use HighResNow whenever possible on Windows.
    Relanding simonjam@'s CL https://codereview.chromium.org/41953002/ to enable QPC.
    
    This may result in perf changes, the perf changes may only be in the measurements and not actual regressions.
    
    BUG=158234
    
    Review URL: https://codereview.chromium.org/429053002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286928 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1229,7 +1229,6 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
 #endif
 #if defined(OS_WIN)
     switches::kDisableDirectWrite,
-    switches::kEnableHighResolutionTime,
 #endif
 #if defined(OS_CHROMEOS)
     switches::kEnableVaapiAcceleratedVideoEncode,
