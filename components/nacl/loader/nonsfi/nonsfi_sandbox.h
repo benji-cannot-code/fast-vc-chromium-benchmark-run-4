193ed6573647d1cd7856abad9654a53a9610b297
mdempsky@chromium.org <mdempsky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-01 19:15:06 +0000

    Convert NaCl non-SFI sandbox to BPF DSL
    BUG=375497
    TBR=mseaborn@chromium.org
    
    Review URL: https://codereview.chromium.org/414013004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@287067 0039d316-1c4b-4281-b951-d872f2087c98

--- components/nacl/loader/nonsfi/nonsfi_sandbox.h
+++ components/nacl/loader/nonsfi/nonsfi_sandbox.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_NACL_LOADER_NONSFI_NONSFI_SANDBOX_H_
 
 #include "base/basictypes.h"
-#include "sandbox/linux/seccomp-bpf/sandbox_bpf_policy.h"
+#include "sandbox/linux/bpf_dsl/bpf_dsl.h"
 
 namespace nacl {
 namespace nonsfi {
@@ -15,15 +15,15 @@ namespace nonsfi {
 // The seccomp sandbox policy for NaCl non-SFI mode. Note that this
 // policy must be as strong as possible, as non-SFI mode heavily
 // depends on seccomp sandbox.
-class NaClNonSfiBPFSandboxPolicy : public sandbox::SandboxBPFPolicy {
+class NaClNonSfiBPFSandboxPolicy
+    : public sandbox::bpf_dsl::SandboxBPFDSLPolicy {
  public:
   explicit NaClNonSfiBPFSandboxPolicy() {}
   virtual ~NaClNonSfiBPFSandboxPolicy() {}
 
-  virtual sandbox::ErrorCode EvaluateSyscall(sandbox::SandboxBPF* sb,
-                                             int sysno) const OVERRIDE;
-  virtual sandbox::ErrorCode InvalidSyscall(
-      sandbox::SandboxBPF* sb) const OVERRIDE;
+  virtual sandbox::bpf_dsl::ResultExpr EvaluateSyscall(
+      int sysno) const OVERRIDE;
+  virtual sandbox::bpf_dsl::ResultExpr InvalidSyscall() const OVERRIDE;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(NaClNonSfiBPFSandboxPolicy);
