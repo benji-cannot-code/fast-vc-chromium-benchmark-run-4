3136d707bff6f3bf7c186696031ed42309ea4072
dgozman@chromium.org <dgozman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-01 17:14:39 +0000

    [DevTools] Move DevToolsClientHost functionality out of DevToolsFrontendHost.
    DevToolsFrontendHost is just a transport now.
    
    Next step is to remove HandleMessageFromDevToolsFrontendToBackend,
    make it one of the embedder messages, and allow embedder to talk
    to backend in itself.
    
    BUG=398046
    
    Review URL: https://codereview.chromium.org/427143003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@287027 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/devtools/devtools_window.cc
+++ chrome/browser/devtools/devtools_window.cc
@@ -428,7 +428,7 @@ DevToolsWindow* DevToolsWindow::OpenDevToolsWindowForWorker(
     window = DevToolsWindow::CreateDevToolsWindowForWorker(profile);
     // Will disconnect the current client host if there is one.
     content::DevToolsManager::GetInstance()->RegisterDevToolsClientHostFor(
-        worker_agent, window->bindings_->frontend_host());
+        worker_agent, window->bindings_);
   }
   window->ScheduleShow(DevToolsToggleAction::Show());
   return window;
@@ -480,7 +480,7 @@ void DevToolsWindow::OpenExternalFrontend(
     window = Create(profile, DevToolsUI::GetProxyURL(frontend_url), NULL,
                     false, true, false, "");
     content::DevToolsManager::GetInstance()->RegisterDevToolsClientHostFor(
-        agent_host, window->bindings_->frontend_host());
+        agent_host, window->bindings_);
   }
   window->ScheduleShow(DevToolsToggleAction::Show());
 }
@@ -503,8 +503,7 @@ DevToolsWindow* DevToolsWindow::ToggleDevToolsWindow(
         base::UserMetricsAction("DevTools_InspectRenderer"));
     window = Create(
         profile, GURL(), inspected_rvh, false, false, true, settings);
-    manager->RegisterDevToolsClientHostFor(agent.get(),
-                                           window->bindings_->frontend_host());
+    manager->RegisterDevToolsClientHostFor(agent.get(), window->bindings_);
     do_open = true;
   }
 
@@ -800,8 +799,7 @@ DevToolsWindow* DevToolsWindow::FindDevToolsWindow(
   content::DevToolsManager* manager = content::DevToolsManager::GetInstance();
   for (DevToolsWindows::iterator it(instances->begin()); it != instances->end();
        ++it) {
-    if (manager->GetDevToolsAgentHostFor((*it)->bindings_->frontend_host()) ==
-        agent_host)
+    if (manager->GetDevToolsAgentHostFor((*it)->bindings_) == agent_host)
       return *it;
   }
   return NULL;
@@ -833,12 +831,12 @@ WebContents* DevToolsWindow::OpenURLFromTab(
 
   content::DevToolsManager* manager = content::DevToolsManager::GetInstance();
   scoped_refptr<DevToolsAgentHost> agent_host(
-      manager->GetDevToolsAgentHostFor(bindings_->frontend_host()));
+      manager->GetDevToolsAgentHostFor(bindings_));
   if (!agent_host.get())
     return NULL;
-  manager->ClientHostClosing(bindings_->frontend_host());
+  manager->ClientHostClosing(bindings_);
   manager->RegisterDevToolsClientHostFor(agent_host.get(),
-                                         bindings_->frontend_host());
+                                         bindings_);
 
   content::NavigationController::LoadURLParams load_url_params(params.url);
   main_web_contents_->GetController().LoadURLWithParams(load_url_params);
@@ -917,7 +915,7 @@ void DevToolsWindow::BeforeUnloadFired(WebContents* tab,
     // Docked devtools window closed directly.
     if (proceed) {
       content::DevToolsManager::GetInstance()->ClientHostClosing(
-          bindings_->frontend_host());
+          bindings_);
     }
     *proceed_to_fire_unload = proceed;
   } else {
