43b744375ffb85bb4e634023a36199be9efa8eb9
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-01 23:23:41 +0000

    Oilpan: WebAudio: Speculative fix for a crash in AudioContext::uninitialize.
    AudioContext object might be destructed between stop() and uninitialize(). This
    CL ensures that |this| is alive by WTF::bind.
    
    BUG=399110
    
    Review URL: https://codereview.chromium.org/437773002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179425 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/webaudio/AudioContext.cpp
+++ third_party/WebKit/Source/modules/webaudio/AudioContext.cpp
@@ -241,17 +241,7 @@ void AudioContext::uninitialize()
     derefUnfinishedSourceNodes();
 
     m_isInitialized = false;
-}
-
-void AudioContext::stopDispatch(void* userData)
-{
-    AudioContext* context = reinterpret_cast<AudioContext*>(userData);
-    ASSERT(context);
-    if (!context)
-        return;
-
-    context->uninitialize();
-    context->clear();
+    clear();
 }
 
 void AudioContext::stop()
@@ -265,7 +255,7 @@ void AudioContext::stop()
     // of dealing with all of its ActiveDOMObjects at this point. uninitialize() can de-reference other
     // ActiveDOMObjects so let's schedule uninitialize() to be called later.
     // FIXME: see if there's a more direct way to handle this issue.
-    callOnMainThread(stopDispatch, this);
+    callOnMainThread(bind(&AudioContext::uninitialize, PassRefPtrWillBeRawPtr<AudioContext>(this)));
 }
 
 bool AudioContext::hasPendingActivity() const
