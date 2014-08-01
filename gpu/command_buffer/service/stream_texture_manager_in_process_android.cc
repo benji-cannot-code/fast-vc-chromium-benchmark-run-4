8985d54672c400563809eccd5d292543ec5f98a2
reveman@chromium.org <reveman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-01 21:50:17 +0000

    ui: Add ScheduleOverlayPlane API to GLImage.
    This makes it possible to use GLImage instances as overlay planes.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/415283002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@287093 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/stream_texture_manager_in_process_android.cc
+++ gpu/command_buffer/service/stream_texture_manager_in_process_android.cc
@@ -32,6 +32,11 @@ class GLImageImpl : public gfx::GLImage {
   virtual void DidUseTexImage() OVERRIDE {}
   virtual void WillModifyTexImage() OVERRIDE {}
   virtual void DidModifyTexImage() OVERRIDE {}
+  virtual bool ScheduleOverlayPlane(gfx::AcceleratedWidget widget,
+                                    int z_order,
+                                    gfx::OverlayTransform transform,
+                                    const gfx::Rect& bounds_rect,
+                                    const gfx::RectF& crop_rect) OVERRIDE;
 
  private:
   virtual ~GLImageImpl();
@@ -55,8 +60,8 @@ void GLImageImpl::Destroy(bool have_context) {
   NOTREACHED();
 }
 
-void GLImageImpl::WillUseTexImage() {
-  surface_texture_->UpdateTexImage();
+gfx::Size GLImageImpl::GetSize() {
+  return gfx::Size();
 }
 
 bool GLImageImpl::BindTexImage(unsigned target) {
@@ -68,8 +73,17 @@ void GLImageImpl::ReleaseTexImage(unsigned target) {
   NOTREACHED();
 }
 
-gfx::Size GLImageImpl::GetSize() {
-  return gfx::Size();
+void GLImageImpl::WillUseTexImage() {
+  surface_texture_->UpdateTexImage();
+}
+
+bool GLImageImpl::ScheduleOverlayPlane(gfx::AcceleratedWidget widget,
+                                       int z_order,
+                                       gfx::OverlayTransform transform,
+                                       const gfx::Rect& bounds_rect,
+                                       const gfx::RectF& crop_rect) {
+  NOTREACHED();
+  return false;
 }
 
 }  // anonymous namespace
