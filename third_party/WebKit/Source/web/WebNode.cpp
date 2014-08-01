886f29f8848c8b1ec7eb6333c12c0340888ece51
estade@chromium.org <estade@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-01 19:39:16 +0000

    Remove WebNode::addEventListener
    BUG=396953
    
    Review URL: https://codereview.chromium.org/437433002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179415 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebNode.cpp
+++ third_party/WebKit/Source/web/WebNode.cpp
@@ -48,13 +48,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "public/platform/WebString.h"
 #include "public/platform/WebVector.h"
 #include "public/web/WebDOMEvent.h"
-#include "public/web/WebDOMEventListener.h"
 #include "public/web/WebDocument.h"
 #include "public/web/WebElement.h"
 #include "public/web/WebElementCollection.h"
 #include "public/web/WebNodeList.h"
 #include "public/web/WebPluginContainer.h"
-#include "web/EventListenerWrapper.h"
 #include "web/FrameLoaderClientImpl.h"
 #include "web/WebLocalFrameImpl.h"
 #include "web/WebPluginContainerImpl.h"
@@ -169,18 +167,6 @@ bool WebNode::isContentEditable() const
 bool WebNode::isElementNode() const
 {
     return m_private->isElementNode();
-}
-
-void WebNode::addEventListener(const WebString& eventType, WebDOMEventListener* listener, bool useCapture)
-{
-    // Please do not add more eventTypes to this list without an API review.
-    RELEASE_ASSERT(eventType == "mousedown");
-
-    EventListenerWrapper* listenerWrapper = listener->createEventListenerWrapper(eventType, useCapture, m_private.get());
-    // The listenerWrapper is only referenced by the actual Node.  Once it goes
-    // away, the wrapper notifies the WebEventListener so it can clear its
-    // pointer to it.
-    m_private->addEventListener(eventType, adoptRef(listenerWrapper), useCapture);
 }
 
 bool WebNode::dispatchEvent(const WebDOMEvent& event)
