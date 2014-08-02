9bbc8b62b7ea7d2b7adb46c5a7a419816e888d83
siva.gunturi@samsung.com <siva.gunturi@samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-02 11:43:42 +0000

    Remove unused ReadbackRequest variable.
    BUG=399240
    
    Review URL: https://codereview.chromium.org/432353002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@287204 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/trees/thread_proxy.h
+++ cc/trees/thread_proxy.h
@@ -95,8 +95,6 @@ class CC_EXPORT ThreadProxy : public Proxy,
     base::TimeTicks last_monotonic_frame_begin_time;
   };
 
-  struct ReadbackRequest;
-
   struct CompositorThreadOnly {
     CompositorThreadOnly(
         ThreadProxy* proxy,
