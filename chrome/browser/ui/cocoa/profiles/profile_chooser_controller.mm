8e83cd3413559dfaf57bebbd378e3307dadbdf83
noms@chromium.org <noms@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-02 23:09:06 +0000

    [Mac] Fix promo text background colour in the new avatar bubble.
    Threw in a bonus refactor, because noone was passing the BackgroundHoverButton
    and alternate image that was different than the default image.
    
    BUG=399693
    TEST=Start Chrome with --new-avatar-menu. The signin promo for local profiles
    should not have a white background (i.e. its background should match the
    bubble background)
    
    Review URL: https://codereview.chromium.org/440493002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@287231 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/profiles/profile_chooser_controller.mm
+++ chrome/browser/ui/cocoa/profiles/profile_chooser_controller.mm
@@ -757,12 +757,10 @@ class ActiveProfileObserverBridge : public AvatarMenuObserver,
 - (NSView*)buildEndPreviewView;
 
 // Creates a button with |text|, an icon given by |imageResourceId| and with
-// |action|. The icon |alternateImageResourceId| is displayed in the button's
-// hovered and pressed states.
+// |action|.
 - (NSButton*)hoverButtonWithRect:(NSRect)rect
                             text:(NSString*)text
                  imageResourceId:(int)imageResourceId
-        alternateImageResourceId:(int)alternateImageResourceId
                           action:(SEL)action;
 
 // Creates a generic link button with |title| and an |action| positioned at
@@ -1432,7 +1430,7 @@ class ActiveProfileObserverBridge : public AvatarMenuObserver,
     NSTextField* promo = BuildLabel(
         l10n_util::GetNSString(IDS_PROFILES_SIGNIN_PROMO),
         NSMakePoint(0, NSMaxY([link frame]) + kVerticalSpacing),
-        nil, nil);
+        GetDialogBackgroundColor(), nil);
     [promo setFrameSize:NSMakeSize(rect.size.width, 0)];
     [GTMUILocalizerAndLayoutTweaker sizeToFitFixedWidthTextField:promo];
     [container addSubview:promo];
@@ -1515,7 +1513,6 @@ class ActiveProfileObserverBridge : public AvatarMenuObserver,
                              text:l10n_util::GetNSString(
                                   IDS_PROFILES_PROFILE_SIGNOUT_BUTTON)
                   imageResourceId:IDR_ICON_PROFILES_MENU_LOCK
-         alternateImageResourceId:IDR_ICON_PROFILES_MENU_LOCK
                            action:@selector(lockProfile:)];
     [container addSubview:lockButton];
     viewRect.origin.y = NSMaxY([lockButton frame]);
@@ -1532,7 +1529,6 @@ class ActiveProfileObserverBridge : public AvatarMenuObserver,
                              text:l10n_util::GetNSString(
                                   IDS_PROFILES_GO_INCOGNITO_BUTTON)
                   imageResourceId:IDR_ICON_PROFILES_MENU_AVATAR
-         alternateImageResourceId:IDR_ICON_PROFILES_MENU_AVATAR
                            action:@selector(goIncognito:)];
     viewRect.origin.y = NSMaxY([goIncognitoButton frame]);
     [container addSubview:goIncognitoButton];
@@ -1549,7 +1545,6 @@ class ActiveProfileObserverBridge : public AvatarMenuObserver,
       [self hoverButtonWithRect:viewRect
                            text:text
                 imageResourceId:IDR_ICON_PROFILES_MENU_AVATAR
-       alternateImageResourceId:IDR_ICON_PROFILES_MENU_AVATAR
                          action:isGuestSession_? @selector(exitGuest:) :
                                                  @selector(showUserManager:)];
   viewRect.origin.y = NSMaxY([switchUsersButton frame]);
@@ -1839,7 +1834,6 @@ class ActiveProfileObserverBridge : public AvatarMenuObserver,
 - (NSButton*)hoverButtonWithRect:(NSRect)rect
                             text:(NSString*)text
                  imageResourceId:(int)imageResourceId
-        alternateImageResourceId:(int)alternateImageResourceId
                           action:(SEL)action {
   base::scoped_nsobject<BackgroundColorHoverButton> button(
       [[BackgroundColorHoverButton alloc]
@@ -1849,11 +1843,10 @@ class ActiveProfileObserverBridge : public AvatarMenuObserver,
 
   [button setTitle:text];
   ui::ResourceBundle* rb = &ui::ResourceBundle::GetSharedInstance();
-  NSImage* alternateImage = rb->GetNativeImageNamed(
-      alternateImageResourceId).ToNSImage();
-  [button setDefaultImage:rb->GetNativeImageNamed(imageResourceId).ToNSImage()];
-  [button setHoverImage:alternateImage];
-  [button setPressedImage:alternateImage];
+  NSImage* image = rb->GetNativeImageNamed(imageResourceId).ToNSImage();
+  [button setDefaultImage:image];
+  [button setHoverImage:image];
+  [button setPressedImage:image];
   [button setImagePosition:NSImageLeft];
   [button setAlignment:NSLeftTextAlignment];
   [button setBordered:NO];
