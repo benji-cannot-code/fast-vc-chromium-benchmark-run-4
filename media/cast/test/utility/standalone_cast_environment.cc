12ae8638b82e973eda4f9905b9e3cdf4f946bc60
miu@chromium.org <miu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-02 11:19:43 +0000

    Fix flaky cast_unittests where threads were being joined incorrectly.
    Root cause of the flakiness is that the StandaloneCastEnvironment, as a
    ref-counted object, was being destroyed on a thread other than the one
    that created it.  At destruction, it attempted to join on the threads it
    owns, but these threads must be joined with the thread that they were
    spawned from.
    
    BUG=396480
    
    Review URL: https://codereview.chromium.org/436673004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@287194 0039d316-1c4b-4281-b951-d872f2087c98

--- media/cast/test/utility/standalone_cast_environment.cc
+++ media/cast/test/utility/standalone_cast_environment.cc
@@ -31,11 +31,14 @@ StandaloneCastEnvironment::StandaloneCastEnvironment()
 }
 
 StandaloneCastEnvironment::~StandaloneCastEnvironment() {
-  DCHECK(CalledOnValidThread());
+  CHECK(CalledOnValidThread());
+  CHECK(!main_thread_.IsRunning());
+  CHECK(!audio_thread_.IsRunning());
+  CHECK(!video_thread_.IsRunning());
 }
 
 void StandaloneCastEnvironment::Shutdown() {
-  DCHECK(CalledOnValidThread());
+  CHECK(CalledOnValidThread());
   main_thread_.Stop();
   audio_thread_.Stop();
   video_thread_.Stop();
