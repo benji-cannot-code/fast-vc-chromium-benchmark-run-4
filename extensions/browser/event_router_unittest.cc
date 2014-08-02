11eebda70540f1ec326898a00a3bdaf5e2cfbe20
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-02 05:55:17 +0000

    Move tests from Chrome's unit_tests to extensions_unittests
    Part of extensions module componentization.
    
    * Add a NotificationService for EventRouterTest
    * Move the test data for ValueStoreFrontendTest
    * The rest can move unchanged
    
    BUG=397164
    TEST=unit_tests, extensions_unittests
    
    Review URL: https://codereview.chromium.org/435703002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@287159 0039d316-1c4b-4281-b951-d872f2087c98

--- extensions/browser/event_router_unittest.cc
+++ extensions/browser/event_router_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/values.h"
+#include "content/public/browser/notification_service.h"
 #include "extensions/browser/event_listener_map.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -82,9 +83,18 @@ scoped_ptr<EventListener> CreateEventListenerForURL(
 }  // namespace
 
 class EventRouterTest : public testing::Test {
+ public:
+  EventRouterTest()
+      : notification_service_(content::NotificationService::Create()) {}
+
  protected:
   // Tests adding and removing observers from EventRouter.
   void RunEventRouterObserverTest(const EventListenerConstructor& constructor);
+
+ private:
+  scoped_ptr<content::NotificationService> notification_service_;
+
+  DISALLOW_COPY_AND_ASSIGN(EventRouterTest);
 };
 
 TEST_F(EventRouterTest, GetBaseEventName) {
