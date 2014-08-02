20c24c8243af00d6f0dcefae4e27c1f4c7b7adf1
thildebr@chromium.org <thildebr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-02 07:53:49 +0000

    BSP Tree perf tests to match LayerSorter perf tests
    The BspTree perf tests should ideally perform equally or better than the
    current LayerSorter tests do in the same tests (i.e. "rubik" vs.
    "rubik").
    
    Performance results on Z600:
    layer_sort_cubes= 3.547ms
    layer_sort_rubik= 0.634ms
    bsp_tree_cubes= 1.485ms
    bsp_tree_rubik= 0.136ms
    bsp_tree_cubes_2= 2.915ms (2x duplicated layers)
    bsp_tree_cubes_4= 5.667ms (4x duplicated layers)
    
    BUG=230833
    
    Review URL: https://codereview.chromium.org/416273002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@287182 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/base/math_util.h
+++ cc/base/math_util.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 #include <cmath>
+#include <vector>
 
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
@@ -124,6 +125,10 @@ class CC_EXPORT MathUtil {
                              const gfx::QuadF& src_quad,
                              gfx::PointF clipped_quad[8],
                              int* num_vertices_in_clipped_quad);
+  static bool MapClippedQuad3d(const gfx::Transform& transform,
+                               const gfx::QuadF& src_quad,
+                               gfx::Point3F clipped_quad[8],
+                               int* num_vertices_in_clipped_quad);
 
   static gfx::RectF ComputeEnclosingRectOfVertices(const gfx::PointF vertices[],
                                                    int num_vertices);
@@ -138,6 +143,10 @@ class CC_EXPORT MathUtil {
   static gfx::QuadF MapQuad(const gfx::Transform& transform,
                             const gfx::QuadF& quad,
                             bool* clipped);
+  static gfx::QuadF MapQuad3d(const gfx::Transform& transform,
+                              const gfx::QuadF& q,
+                              gfx::Point3F* p,
+                              bool* clipped);
   static gfx::PointF MapPoint(const gfx::Transform& transform,
                               const gfx::PointF& point,
                               bool* clipped);
