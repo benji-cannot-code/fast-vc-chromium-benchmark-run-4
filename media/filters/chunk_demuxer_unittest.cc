b24829aaa898a7daeb146d68ebc4caa40acaf8f8
dalecurtis@chromium.org <dalecurtis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-02 07:35:55 +0000

    Add support for partial append window end trimming.
    Facilitates gapless playback across mp3 and aac.  Relying on splice
    frames and crossfading doesn't work in cases where the prior segment
    signal diverges to null-padding values too quickly relative to the
    newly appended segment.
    
    BUG=395899
    TEST=new unittests. llama-demo works.
    
    Review URL: https://codereview.chromium.org/414603002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@287174 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/chunk_demuxer_unittest.cc
+++ media/filters/chunk_demuxer_unittest.cc
@@ -459,6 +459,9 @@ class ChunkDemuxerTest : public ::testing::Test {
             << " All text blocks must be keyframes";
       }
 
+      if (track_number == kAudioTrackNum)
+        ASSERT_TRUE(block_info.flags & kWebMFlagKeyframe);
+
       blocks->push_back(block_info);
     }
   }
@@ -1371,19 +1374,19 @@ TEST_F(ChunkDemuxerTest, InitSegmentSetsNeedRandomAccessPointFlag) {
   ASSERT_TRUE(audio_stream && video_stream && text_stream);
 
   AppendMuxedCluster(
-      MuxedStreamInfo(kAudioTrackNum, "0 23K"),
+      MuxedStreamInfo(kAudioTrackNum, "23K"),
       MuxedStreamInfo(kVideoTrackNum, "0 30K"),
       MuxedStreamInfo(kTextTrackNum, "25K 40K"));
   CheckExpectedRanges(kSourceId, "{ [23,46) }");
 
   AppendInitSegment(HAS_TEXT | HAS_AUDIO | HAS_VIDEO);
   AppendMuxedCluster(
-      MuxedStreamInfo(kAudioTrackNum, "46 69K"),
+      MuxedStreamInfo(kAudioTrackNum, "46K 69K"),
       MuxedStreamInfo(kVideoTrackNum, "60 90K"),
       MuxedStreamInfo(kTextTrackNum, "80K 90K"));
   CheckExpectedRanges(kSourceId, "{ [23,92) }");
 
-  CheckExpectedBuffers(audio_stream, "23 69");
+  CheckExpectedBuffers(audio_stream, "23 46 69");
   CheckExpectedBuffers(video_stream, "30 90");
   CheckExpectedBuffers(text_stream, "25 40 80 90");
 }
@@ -2502,7 +2505,7 @@ TEST_F(ChunkDemuxerTest, GetBufferedRanges_AudioVideoText) {
 
   // Append audio & video data
   AppendMuxedCluster(
-      MuxedStreamInfo(kAudioTrackNum, "0K 23"),
+      MuxedStreamInfo(kAudioTrackNum, "0K 23K"),
       MuxedStreamInfo(kVideoTrackNum, "0K 33"));
 
   // Verify that a text track with no cues does not result in an empty buffered
@@ -2511,7 +2514,7 @@ TEST_F(ChunkDemuxerTest, GetBufferedRanges_AudioVideoText) {
 
   // Add some text cues.
   AppendMuxedCluster(
-      MuxedStreamInfo(kAudioTrackNum, "100K 123"),
+      MuxedStreamInfo(kAudioTrackNum, "100K 123K"),
       MuxedStreamInfo(kVideoTrackNum, "100K 133"),
       MuxedStreamInfo(kTextTrackNum, "100K 200K"));
 
@@ -3357,7 +3360,7 @@ TEST_F(ChunkDemuxerTest, AppendWindow_Audio) {
   //
   // The first 50ms of the range should be truncated since it overlaps
   // the start of the append window.
-  CheckExpectedRanges(kSourceId, "{ [50,270) }");
+  CheckExpectedRanges(kSourceId, "{ [50,280) }");
 
   // The "50P" buffer is the "0" buffer marked for complete discard.  The next
   // "50" buffer is the "30" buffer marked with 20ms of start discard.
@@ -3370,7 +3373,7 @@ TEST_F(ChunkDemuxerTest, AppendWindow_Audio) {
   AppendSingleStreamCluster(
       kSourceId, kAudioTrackNum,
       "360K 390K 420K 450K 480K 510K 540K 570K 600K 630K");
-  CheckExpectedRanges(kSourceId, "{ [50,270) [360,630) }");
+  CheckExpectedRanges(kSourceId, "{ [50,280) [360,650) }");
 }
 
 TEST_F(ChunkDemuxerTest, AppendWindow_AudioOverlapStartAndEnd) {
@@ -3383,9 +3386,8 @@ TEST_F(ChunkDemuxerTest, AppendWindow_AudioOverlapStartAndEnd) {
   // Append a cluster that starts before and ends after the append window.
   AppendSingleStreamCluster(kSourceId, kAudioTrackNum, "0K");
 
-  // Verify that everything is dropped in this case.  No partial append should
-  // be generated.
-  CheckExpectedRanges(kSourceId, "{ }");
+  // Verify the append is clipped to the append window.
+  CheckExpectedRanges(kSourceId, "{ [10,20) }");
 }
 
 TEST_F(ChunkDemuxerTest, AppendWindow_WebMFile_AudioOnly) {
