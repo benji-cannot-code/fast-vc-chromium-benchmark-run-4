4915dc0bce62dbce879a06ddf52da23e182a1791
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-04 10:58:44 +0000

    Worker threads should be registered to task observers
    r179309 switched the implementation of worker threads to
    web threads. This caused timeout in a lot of worker tests
    in oilpan builds. This is because the worker threads
    were not registered to task observers, and thus the
    worker threads didn't enter safe points at the end of
    each task execution.
    
    This CL registers worker threads to task observers
    and fixes the issue. 
    
    BUG=399762
    
    Review URL: https://codereview.chromium.org/441653002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179463 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/workers/WorkerThread.h
+++ third_party/WebKit/Source/core/workers/WorkerThread.h
@@ -32,6 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/frame/csp/ContentSecurityPolicy.h"
 #include "core/workers/WorkerGlobalScope.h"
 #include "platform/SharedTimer.h"
+#include "platform/heap/glue/MessageLoopInterruptor.h"
+#include "platform/heap/glue/PendingGCRunner.h"
 #include "platform/weborigin/SecurityOrigin.h"
 #include "public/platform/WebThread.h"
 #include "wtf/Forward.h"
@@ -115,6 +117,8 @@ namespace blink {
         OwnPtr<blink::WebThread> m_thread;
         OwnPtr<WorkerSharedTimer> m_sharedTimer;
         MessageQueue<WorkerThreadTask> m_debuggerMessageQueue;
+        OwnPtr<PendingGCRunner> m_pendingGCRunner;
+        OwnPtr<MessageLoopInterruptor> m_messageLoopInterruptor;
 
         WorkerLoaderProxy& m_workerLoaderProxy;
         WorkerReportingProxy& m_workerReportingProxy;
