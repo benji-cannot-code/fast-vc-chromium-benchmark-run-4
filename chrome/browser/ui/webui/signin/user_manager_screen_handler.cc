4af407c9b1273034da969b6e468c817bf375e0af
treib@chromium.org <treib@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-05 13:52:50 +0000

    Rename "managed (mode|user)" to "supervised user" (part 9)
    Followup to https://codereview.chromium.org/403343002/
    
    This CL renames ChromeOS-specific resource IDs (strings/images)
    
    Still missing:
    - The global ENABLE_MANAGED_USERS define
    
    TBRing a bunch of renames.
    TBR=oshima@chromium.org (ash and c/b/ui/ash)
    TBR=antrim@chromium.org (c/b/chromeos and ui/login)
    
    BUG=385113
    
    Review URL: https://codereview.chromium.org/425293002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@287534 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/signin/user_manager_screen_handler.cc
+++ chrome/browser/ui/webui/signin/user_manager_screen_handler.cc
@@ -39,10 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/image/image_skia.h"
 #include "ui/gfx/image/image_util.h"
 
-#if defined(ENABLE_MANAGED_USERS)
-#include "chrome/browser/supervised_user/supervised_user_service.h"
-#endif
-
 namespace {
 // User dictionary keys.
 const char kKeyUsername[] = "username";
--- ui/login/display_manager.js
+++ ui/login/display_manager.js
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /** @const */ var SCREEN_ERROR_MESSAGE = 'error-message';
 /** @const */ var SCREEN_TPM_ERROR = 'tpm-error-message';
 /** @const */ var SCREEN_PASSWORD_CHANGED = 'password-changed';
-/** @const */ var SCREEN_CREATE_MANAGED_USER_FLOW =
+/** @const */ var SCREEN_CREATE_SUPERVISED_USER_FLOW =
     'supervised-user-creation';
 /** @const */ var SCREEN_APP_LAUNCH_SPLASH = 'app-launch-splash';
 /** @const */ var SCREEN_CONFIRM_PASSWORD = 'confirm-password';
