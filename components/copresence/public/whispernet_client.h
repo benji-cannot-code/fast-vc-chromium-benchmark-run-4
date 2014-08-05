4b96c622244efdc020f667b1c467548ecd08172e
rkc@chromium.org <rkc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-05 00:13:59 +0000

    Fix header guard.
    Fix header guard and include for the whispernet_client public interface.
    
    R=ckehoe@chromium.org
    BUG=None
    
    Review URL: https://codereview.chromium.org/438163002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@287418 0039d316-1c4b-4281-b951-d872f2087c98

--- components/copresence/public/whispernet_client.h
+++ components/copresence/public/whispernet_client.h
@@ -3,15 +3,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef COMPONENTS_COPRESENCE_INTERFACE_WHISPERNET_CLIENT_H_
-#define COMPONENTS_COPRESENCE_INTERFACE_WHISPERNET_CLIENT_H_
+#ifndef COMPONENTS_COPRESENCE_PUBLIC_WHISPERNET_CLIENT_H_
+#define COMPONENTS_COPRESENCE_PUBLIC_WHISPERNET_CLIENT_H_
 
 #include <string>
 #include <vector>
 
 #include "base/callback.h"
 #include "base/macros.h"
-#include "base/memory/scoped_ptr.h"
+#include "base/memory/ref_counted.h"
 
 namespace media {
 class AudioBusRefCounted;
@@ -68,4 +68,4 @@ class WhispernetClient {
 
 }  // namespace copresence
 
-#endif  // COMPONENTS_COPRESENCE_INTERFACE_WHISPERNET_CLIENT_H_
+#endif  // COMPONENTS_COPRESENCE_PUBLIC_WHISPERNET_CLIENT_H_
