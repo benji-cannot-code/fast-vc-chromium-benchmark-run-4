f72bd317c5157f322df4a9f7ed2b5e5862e0baa2
zerny@chromium.org <zerny@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-05 22:43:08 +0000

    Added missing export declarations to WTF print facilities.
    R=tkent@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/444493004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179536 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/FilePrintStream.h
+++ third_party/WebKit/Source/wtf/FilePrintStream.h
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WTF {
 
-class FilePrintStream FINAL : public PrintStream {
+class WTF_EXPORT FilePrintStream FINAL : public PrintStream {
 public:
     enum AdoptionMode {
         Adopt,
