3675d4531954d624a17eab0297eeae321ce4333f
kouhei@chromium.org <kouhei@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-05 17:43:26 +0000

    Revert of [DevTools] Move DispatchOnDevToolsFrontend to embedder. (https://codereview.chromium.org/418243003/)
    Reason for revert:
    I think this CL broke inspector/ layout tests:
    http://build.chromium.org/p/chromium.webkit/builders/WebKit%20Linux/builds/35062
    
    Original issue's description:
    > [DevTools] Move DispatchOnDevToolsFrontend to embedder.
    > 
    > We already have a way to send message via ExecuteJavaScript,
    > no need for another mechanism.
    > 
    > BUG=398046
    > TBR=jam
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=287514
    
    TBR=pfeldman@chromium.org,jam@chromium.org,dgozman@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=398046
    
    Review URL: https://codereview.chromium.org/444633003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@287558 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/devtools/devtools_ui_bindings.cc
+++ chrome/browser/devtools/devtools_ui_bindings.cc
@@ -413,9 +413,8 @@ void DevToolsUIBindings::HandleMessageFromDevToolsFrontendToBackend(
 // content::DevToolsClientHost implementation ---------------------------------
 void DevToolsUIBindings::DispatchOnInspectorFrontend(
     const std::string& message) {
-  base::StringValue message_value(message);
-  CallClientFunction("InspectorFrontendAPI.dispatchMessage",
-                     &message_value, NULL, NULL);
+  if (frontend_host_)
+    frontend_host_->DispatchOnDevToolsFrontend(message);
 }
 
 void DevToolsUIBindings::InspectedContentsClosing() {
@@ -806,9 +805,8 @@ void DevToolsUIBindings::CallClientFunction(const std::string& function_name,
       }
     }
   }
-
-  base::string16 javascript = base::UTF8ToUTF16(
-      function_name + "(" + params + ");");
+  base::string16 javascript =
+      base::UTF8ToUTF16(function_name + "(" + params + ");");
   web_contents_->GetMainFrame()->ExecuteJavaScript(javascript);
 }
 
