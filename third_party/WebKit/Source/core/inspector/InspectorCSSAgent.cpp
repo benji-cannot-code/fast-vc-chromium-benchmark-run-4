1dd1e945583e25b10ab9bd6e783b7f02365495d5
rob.buis@samsung.com <rob.buis@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-05 19:08:39 +0000

    Call style directly in InspectorCSSAgent::asInspectorStyleSheet
    We can just call style() directly here, internally it does the same check.
    
    Review URL: https://codereview.chromium.org/442473002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179516 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorCSSAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorCSSAgent.cpp
@@ -1134,7 +1134,7 @@ InspectorStyleSheetForInlineStyle* InspectorCSSAgent::asInspectorStyleSheet(Elem
     if (it != m_nodeToInspectorStyleSheet.end())
         return it->value.get();
 
-    CSSStyleDeclaration* style = element->isStyledElement() ? element->style() : 0;
+    CSSStyleDeclaration* style = element->style();
     if (!style)
         return 0;
 
