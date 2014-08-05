e68936122fb589a5e1d1a86b7396a1e0746fbabc
ccameron@chromium.org <ccameron@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-05 09:40:31 +0000

    Make remote CALayers work with browser compositor on Mac
    This adds the equivalent remote layer code from RenderWidgetHostViewMac
    over to the BrowserCompositorViewMac.
    
    This code still isn't activated yet -- backpressure is still not appropriately
    applied via ImageTransportLayer.
    
    BUG=312462
    
    Review URL: https://codereview.chromium.org/441743003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@287502 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/compositor/browser_compositor_view_mac.mm
+++ content/browser/compositor/browser_compositor_view_mac.mm
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/debug/trace_event.h"
 #include "base/lazy_instance.h"
 #include "content/browser/compositor/browser_compositor_view_private_mac.h"
-#include "content/common/gpu/surface_handle_types_mac.h"
 
 ////////////////////////////////////////////////////////////////////////////////
 // BrowserCompositorViewMac
@@ -81,11 +80,10 @@ void BrowserCompositorViewMac::GotAcceleratedFrame(
     gfx::Size pixel_size, float scale_factor) {
   BrowserCompositorViewMacInternal* internal_view =
       BrowserCompositorViewMacInternal::FromAcceleratedWidget(widget);
-  if (!internal_view)
-    return;
-  IOSurfaceID io_surface_id = IOSurfaceIDFromSurfaceHandle(surface_handle);
-  internal_view->GotAcceleratedIOSurfaceFrame(
-      io_surface_id, surface_id, latency_info, pixel_size, scale_factor);
+  if (internal_view) {
+    internal_view->GotAcceleratedFrame(
+        surface_handle, surface_id, latency_info, pixel_size, scale_factor);
+  }
 }
 
 // static
@@ -94,9 +92,8 @@ void BrowserCompositorViewMac::GotSoftwareFrame(
     cc::SoftwareFrameData* frame_data, float scale_factor, SkCanvas* canvas) {
   BrowserCompositorViewMacInternal* internal_view =
       BrowserCompositorViewMacInternal::FromAcceleratedWidget(widget);
-  if (!internal_view)
-    return;
-  internal_view->GotSoftwareFrame(frame_data, scale_factor, canvas);
+  if (internal_view)
+    internal_view->GotSoftwareFrame(frame_data, scale_factor, canvas);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
