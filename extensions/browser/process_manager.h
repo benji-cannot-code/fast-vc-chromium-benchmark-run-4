64371137da0d85247a2422337c4bbdf3ac39353c
akuegel@chromium.org <akuegel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-05 13:29:30 +0000

    Revert of Resubmit: Remove deprecated extension notifications from ProcessManager (https://codereview.chromium.org/434593002/)
    Reason for revert:
    Speculative revert, see http://build.chromium.org/p/chromium.memory/builders/Linux%20ASan%20LSan%20Tests%20%282%29/builds/5773/steps/unit_tests/logs/GetHostZoomMap
    The stack trace mentions extensions::ExtensionRegistry::RemoveObserver
    
    Original issue's description:
    > Remove deprecated extension notifications from ProcessManager and use ExtensionRegistry instead.
    > 
    > ProcessManagerTest inherts ExtensionsTest for the test settings. So now unit test is passed.
    > 
    > R=rdevlin.cronin@chromium.org
    > BUG=354046
    > TEST=extensions_unittests
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=287521
    
    TBR=rdevlin.cronin@chromium.org,limasdf@gmail.com
    NOTREECHECKS=true
    NOTRY=true
    BUG=354046
    
    Review URL: https://codereview.chromium.org/443623002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@287530 0039d316-1c4b-4281-b951-d872f2087c98

--- extensions/browser/process_manager.h
+++ extensions/browser/process_manager.h
@@ -15,11 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
-#include "base/scoped_observer.h"
 #include "base/time/time.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
-#include "extensions/browser/extension_registry_observer.h"
 #include "extensions/common/view_type.h"
 
 class GURL;
@@ -43,8 +41,7 @@ class ProcessManagerObserver;
 // Manages dynamic state of running Chromium extensions. There is one instance
 // of this class per Profile. OTR Profiles have a separate instance that keeps
 // track of split-mode extensions only.
-class ProcessManager : public content::NotificationObserver,
-                       public ExtensionRegistryObserver {
+class ProcessManager : public content::NotificationObserver {
  public:
   typedef std::set<extensions::ExtensionHost*> ExtensionHostSet;
   typedef ExtensionHostSet::const_iterator const_iterator;
@@ -177,14 +174,6 @@ class ProcessManager : public content::NotificationObserver,
                        const content::NotificationSource& source,
                        const content::NotificationDetails& details) OVERRIDE;
 
-  // ExtensionRegistryObserver:
-  virtual void OnExtensionLoaded(content::BrowserContext* browser_context,
-                                 const Extension* extension) OVERRIDE;
-  virtual void OnExtensionUnloaded(
-      content::BrowserContext* browser_context,
-      const Extension* extension,
-      UnloadedExtensionInfo::Reason reason) OVERRIDE;
-
   content::NotificationRegistrar registrar_;
 
   // The set of ExtensionHosts running viewless background extensions.
@@ -290,9 +279,6 @@ class ProcessManager : public content::NotificationObserver,
 
   // Must be last member, see doc on WeakPtrFactory.
   base::WeakPtrFactory<ProcessManager> weak_ptr_factory_;
-
-  ScopedObserver<ExtensionRegistry, ExtensionRegistryObserver>
-      extension_registry_observer_;
 
   DISALLOW_COPY_AND_ASSIGN(ProcessManager);
 };
