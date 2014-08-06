8b57e02b69dea0e1d3ca0ca4e1a0f4c5cb805b86
timloh@chromium.org <timloh@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-06 16:01:10 +0000

    Fix fading out media controls
    This patch fixes the media controls panel to fade out, instead of appear
    instantly. This behaviour was broken in blink r175153 which changed how
    we stored the parsed data for CSS transitions.
    
    The fade in behaviour has actually been broken for a long time, since
    CSS transitions will not start if the before style has display set to
    none. I've not tried to fix this issue in this patch.
    
    BUG=398279
    
    Review URL: https://codereview.chromium.org/445893002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179622 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/media/media-controls.js
+++ third_party/WebKit/LayoutTests/media/media-controls.js
@@ -2,9 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 var captionsButtonElement;
 var captionsButtonCoordinates;
 
-// These reflect the values used to fade in/out the media controls. Should
-// mirror the values 'fadeInDuration'/'fadeOutDuration' in MediaControlElements.cpp.
-const controlsFadeInDurationMs = 100;
+// As specified in mediaControls.css, this is how long it takes to fade out controls
 const controlsFadeOutDurationMs = 300;
 
 // The timeout for the hide-after-no-mouse-movement behavior. Defined (and
