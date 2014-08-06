4cf6b2e3aca3bb2f773cdf564e7202e29611fc0c
miu@chromium.org <miu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-06 18:51:26 +0000

    Plumb frame rate configuration into Cast Streaming API.
    BUG=400930
    
    Review URL: https://codereview.chromium.org/442863005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@287814 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/media/cast_rtp_stream.h
+++ chrome/renderer/media/cast_rtp_stream.h
@@ -61,6 +61,9 @@ struct CastRtpPayloadParams {
   // Number of audio channels.
   int channels;
 
+  // The maximum frame rate.
+  double max_frame_rate;
+
   // Width and height of the video content.
   int width;
   int height;
