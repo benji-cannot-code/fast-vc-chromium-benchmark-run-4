2224e39817604ac2aaa6dc84335826fe37a5ef76
igsolla@chromium.org <igsolla@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-06 17:10:56 +0000

    Hide fullscreen button when fullscreen not supported.
    BUG=400333
    
    Review URL: https://codereview.chromium.org/442603006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179633 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/shadow/MediaControls.cpp
+++ third_party/WebKit/Source/core/html/shadow/MediaControls.cpp
@@ -41,6 +41,11 @@ namespace blink {
 // LayoutTests/media/media-controls.js.
 static const double timeWithoutMouseMovementBeforeHidingMediaControls = 3;
 
+static bool fullscreenIsSupported(const Document& document)
+{
+    return !document.settings() || document.settings()->fullscreenSupported();
+}
+
 MediaControls::MediaControls(HTMLMediaElement& mediaElement)
     : HTMLDivElement(mediaElement.document())
     , m_mediaElement(&mediaElement)
@@ -179,7 +184,7 @@ void MediaControls::reset()
 
     refreshClosedCaptionsButtonVisibility();
 
-    if (mediaElement().hasVideo())
+    if (mediaElement().hasVideo() && fullscreenIsSupported(document()))
         m_fullScreenButton->show();
     else
         m_fullScreenButton->hide();
