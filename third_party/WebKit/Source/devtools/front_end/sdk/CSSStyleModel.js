14d7b9f5b087755f9505a5e98668be18a0487669
lushnikov@chromium.org <lushnikov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-06 01:17:36 +0000

    DevTools: [CSS] invert bool arguments for CSS.getMatchedStylesForNode
    This patch inverts optional boolean arguments of CSS.getMatchedStylesForNode so
    that their default value is false.
    
    R=yurys, aandrey, apavlov, vsevik
    
    Review URL: https://codereview.chromium.org/437563002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179554 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sdk/CSSStyleModel.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/CSSStyleModel.js
@@ -131,11 +131,11 @@ WebInspector.CSSStyleModel.prototype = {
 
     /**
      * @param {!DOMAgent.NodeId} nodeId
-     * @param {boolean} needPseudo
-     * @param {boolean} needInherited
+     * @param {boolean} excludePseudo
+     * @param {boolean} excludeInherited
      * @param {function(?*)} userCallback
      */
-    getMatchedStylesAsync: function(nodeId, needPseudo, needInherited, userCallback)
+    getMatchedStylesAsync: function(nodeId, excludePseudo, excludeInherited, userCallback)
     {
         /**
          * @param {function(?*)} userCallback
@@ -181,7 +181,7 @@ WebInspector.CSSStyleModel.prototype = {
                 userCallback(result);
         }
 
-        this._agent.getMatchedStylesForNode(nodeId, needPseudo, needInherited, callback.bind(this, userCallback));
+        this._agent.getMatchedStylesForNode(nodeId, excludePseudo, excludeInherited, callback.bind(this, userCallback));
     },
 
     /**
