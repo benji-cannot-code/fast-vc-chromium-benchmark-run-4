e34d991746728443a90f1d4a959b27a659415a62
philipj@opera.com <philipj@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-06 06:58:04 +0000

    Remove WheelEvent.webkitDirectionInvertedFromDevice
    Intent to Deprecate... and Remove:
    https://groups.google.com/a/chromium.org/d/msg/blink-dev/jQjCy5ynZ58/bpfowBXCsDMJ
    https://groups.google.com/a/chromium.org/d/msg/blink-dev/jQjCy5ynZ58/TrNqY9VJ42MJ
    
    BUG=400794
    
    Review URL: https://codereview.chromium.org/440963002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179565 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -379,7 +379,6 @@ public:
         PrefixedTouchForce = 381,
         PrefixedMouseEventMovementX = 382,
         PrefixedMouseEventMovementY = 383,
-        PrefixedWheelEventDirectionInvertedFromDevice = 384,
         PrefixedFileRelativePath = 386,
         DocumentCaretRangeFromPoint = 387,
         DocumentGetCSSCanvasContext = 388,
