f16a45536e3b31c7a68c42e1759cbf42b72f487b
haitaol@chromium.org <haitaol@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-06 17:43:36 +0000

    It's decided that it's unnecessary to ask user to confirm profile deletion if he chooses to remove other data while disconnecting sync because the confirmation dialog can get lost sometimes and user's data is left behind.
    BUG=395441
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/426123003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@287802 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/sync_setup_handler.cc
+++ chrome/browser/ui/webui/sync_setup_handler.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/singleton_tabs.h"
 #include "chrome/browser/ui/sync/signin_histogram.h"
+#include "chrome/browser/ui/webui/options/options_handlers_helper.h"
 #include "chrome/browser/ui/webui/signin/login_ui_service.h"
 #include "chrome/browser/ui/webui/signin/login_ui_service_factory.h"
 #include "chrome/common/chrome_switches.h"
@@ -818,8 +819,8 @@ void SyncSetupHandler::HandleStopSyncing(const base::ListValue* args) {
 
   bool delete_profile = false;
   if (args->GetBoolean(0, &delete_profile) && delete_profile) {
-    web_ui()->CallJavascriptFunction(
-        "BrowserOptions.deleteCurrentProfile");
+    // Do as BrowserOptionsHandler::DeleteProfile().
+    options::helper::DeleteProfileAtPath(GetProfile()->GetPath(), web_ui());
   }
 }
 #endif
