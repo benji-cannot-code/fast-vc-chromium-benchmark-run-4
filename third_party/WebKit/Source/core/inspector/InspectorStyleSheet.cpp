49f94788403d6a32229fd5158b0ce3588d7c6d01
lushnikov@chromium.org <lushnikov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-06 09:23:44 +0000

    DevTools: speculative fix for CSS inspection code crash.
    The patch adds a check that will eliminate any possibility of trying to
    populate properties for non-StyleRule rules.
    
    BUG=397300
    
    Review URL: https://codereview.chromium.org/436233003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179591 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorStyleSheet.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorStyleSheet.cpp
@@ -638,7 +638,7 @@ void InspectorStyle::populateAllProperties(WillBeHeapVector<InspectorStyleProper
     HashSet<String> sourcePropertyNames;
 
     RefPtrWillBeRawPtr<CSSRuleSourceData> sourceData = extractSourceData();
-    if (sourceData) {
+    if (sourceData && sourceData->styleSourceData) {
         String styleDeclaration;
         bool isStyleTextKnown = styleText(&styleDeclaration);
         ASSERT_UNUSED(isStyleTextKnown, isStyleTextKnown);
