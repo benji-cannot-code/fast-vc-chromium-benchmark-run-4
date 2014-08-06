13cc9340ca35cec4ce728fc7ffda4a223dcb0954
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-06 16:38:37 +0000

    athena: Add support for fling-scroll in overview mode.
    Install an animation-observer in the compositor when a fling happens, and use a
    FlingCurve (which implements the same fling-curve used for fling-scrolling
    web-pages) to progress the fling at each animation step in the compositor.
    
    BUG=398117
    R=mukai@chromium.org, sky@chromium.org
    
    Review URL: https://codereview.chromium.org/436213002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@287783 0039d316-1c4b-4281-b951-d872f2087c98

--- athena/athena.gyp
+++ athena/athena.gyp
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         '../ui/accessibility/accessibility.gyp:ax_gen',
         '../ui/app_list/app_list.gyp:app_list',
         '../ui/aura/aura.gyp:aura',
+        '../ui/events/events.gyp:events_base',
         '../ui/strings/ui_strings.gyp:ui_strings',
         '../ui/views/views.gyp:views',
       ],
---
+++ ui/events/gestures/fling_curve_unittest.cc
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/events/gestures/fling_curve.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+#include "ui/gfx/frame_time.h"
+
+namespace ui {
+
+TEST(FlingCurveTest, Basic) {
+  const gfx::Vector2dF velocity(0, 5000);
+  base::TimeTicks now = gfx::FrameTime::Now();
+  FlingCurve curve(velocity, now);
+
+  gfx::Vector2dF scroll =
+      curve.GetScrollAmountAtTime(now + base::TimeDelta::FromMilliseconds(20));
+  EXPECT_EQ(0, scroll.x());
+  EXPECT_NEAR(scroll.y(), 96, 1);
+
+  scroll =
+      curve.GetScrollAmountAtTime(now + base::TimeDelta::FromMilliseconds(250));
+  EXPECT_EQ(0, scroll.x());
+  EXPECT_NEAR(scroll.y(), 705, 1);
+
+  scroll =
+      curve.GetScrollAmountAtTime(now + base::TimeDelta::FromSeconds(10));
+  EXPECT_EQ(0, scroll.x());
+  EXPECT_NEAR(scroll.y(), 392, 1);
+
+  EXPECT_TRUE(curve.GetScrollAmountAtTime(
+      now + base::TimeDelta::FromSeconds(20)).IsZero());
+}
+
+}  // namespace ui
