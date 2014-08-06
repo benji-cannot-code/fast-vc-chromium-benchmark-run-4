0212fe47e33cb924a7d2440fc082f568b1550392
tasak@google.com <tasak@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-06 04:31:17 +0000

    Moved imagebitmap from module to core.
    Since window in core depends on imagebitmap in module, we need to move imagebitmap from module to core.
    
    i.e. Window implements ImageBitmapFactories;
    
    BUG=358074
    
    Review URL: https://codereview.chromium.org/428853002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179561 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/imagebitmap/ImageBitmapFactories.cpp
+++ third_party/WebKit/Source/core/imagebitmap/ImageBitmapFactories.cpp
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
-#include "modules/imagebitmap/ImageBitmapFactories.h"
+#include "core/imagebitmap/ImageBitmapFactories.h"
 
 #include "bindings/core/v8/ExceptionState.h"
 #include "bindings/core/v8/ScriptPromiseResolver.h"
