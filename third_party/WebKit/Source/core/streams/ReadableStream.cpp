c9c0036b36fa84ecd96ac166c053d749d57c936d
ager@chromium.org <ager@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-06 11:03:28 +0000

    Fix oilpan build after stream changes.
    R=haraken@chromium.org, tkent@chromium.org, yhirano@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/445913002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179600 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/streams/ReadableStream.cpp
+++ third_party/WebKit/Source/core/streams/ReadableStream.cpp
@@ -176,6 +176,7 @@ void ReadableStream::trace(Visitor* visitor)
     visitor->trace(m_source);
     visitor->trace(m_wait);
     visitor->trace(m_closed);
+    visitor->trace(m_exception);
 }
 
 } // namespace blink
