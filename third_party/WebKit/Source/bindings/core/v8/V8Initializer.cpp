32bab336610059feb266871bc8d6ad63763c68c7
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-06 20:01:12 +0000

    Allow private scripts to dispatch error events even in a NoEventDispatchAssertion scope
    This CL allows a private script to dispatch error events even in a NoEventDispatchAssertion scope.
    Without having this ability, it's hard to debug the private script because syntax errors
    in the private script are not reported to a console (the private script just crashes silently).
    
    Allowing error events in private scripts is safe because error events don't propagate to
    other isolated worlds (which means that the error events won't fire any event listeners
    in user's scripts).
    
    BUG=341031
    
    Review URL: https://codereview.chromium.org/440303002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179648 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/core/v8/V8Initializer.cpp
+++ third_party/WebKit/Source/bindings/core/v8/V8Initializer.cpp
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/V8Window.h"
 #include "core/dom/Document.h"
 #include "core/dom/ExceptionCode.h"
+#include "core/dom/NoEventDispatchAssertion.h"
 #include "core/frame/ConsoleTypes.h"
 #include "core/frame/LocalDOMWindow.h"
 #include "core/frame/LocalFrame.h"
@@ -131,7 +132,19 @@ static void messageHandlerInMainThread(v8::Handle<v8::Message> message, v8::Hand
     if (frame && frame->script().existingWindowShell(scriptState->world())) {
         V8ErrorHandler::storeExceptionOnErrorEventWrapper(event.get(), data, scriptState->context()->Global(), isolate);
     }
-    enteredWindow->document()->reportException(event.release(), callStack, corsStatus);
+
+    if (scriptState->world().isPrivateScriptIsolatedWorld()) {
+        // We allow a private script to dispatch error events even in a NoEventDispatchAssertion scope.
+        // Without having this ability, it's hard to debug the private script because syntax errors
+        // in the private script are not reported to console (the private script just crashes silently).
+        // Allowing error events in private scripts is safe because error events don't propagate to
+        // other isolated worlds (which means that the error events won't fire any event listeners
+        // in user's scripts).
+        NoEventDispatchAssertion::AllowUserAgentEvents allowUserAgentEvents;
+        enteredWindow->document()->reportException(event.release(), callStack, corsStatus);
+    } else {
+        enteredWindow->document()->reportException(event.release(), callStack, corsStatus);
+    }
 }
 
 static void failedAccessCheckCallbackInMainThread(v8::Local<v8::Object> host, v8::AccessType type, v8::Local<v8::Value> data)
