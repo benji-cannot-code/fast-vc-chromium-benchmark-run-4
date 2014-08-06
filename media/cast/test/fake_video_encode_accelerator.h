be692144d4af16ebfa179e4cb459492a1dfc757d
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-06 07:06:01 +0000

    Cast: Use fixed bitrate and set it once for hardware encoder
    Some hardware encoder cannot handle bitrate changing too quickly. The
    solution is to have a fixed bitrate and only set it once.
    
    BUG=392086
    
    Review URL: https://codereview.chromium.org/439863003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@287721 0039d316-1c4b-4281-b951-d872f2087c98

--- media/cast/test/fake_video_encode_accelerator.h
+++ media/cast/test/fake_video_encode_accelerator.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/video/video_encode_accelerator.h"
 
 #include <list>
+#include <vector>
 
 #include "base/memory/weak_ptr.h"
 #include "media/base/bitstream_buffer.h"
@@ -24,7 +25,8 @@ namespace test {
 class FakeVideoEncodeAccelerator : public VideoEncodeAccelerator {
  public:
   explicit FakeVideoEncodeAccelerator(
-      const scoped_refptr<base::SingleThreadTaskRunner>& task_runner);
+      const scoped_refptr<base::SingleThreadTaskRunner>& task_runner,
+      std::vector<uint32>* stored_bitrates);
   virtual ~FakeVideoEncodeAccelerator();
 
   virtual bool Initialize(media::VideoFrame::Format input_format,
@@ -53,8 +55,8 @@ class FakeVideoEncodeAccelerator : public VideoEncodeAccelerator {
                               size_t payload_size,
                               bool key_frame) const;
 
-  scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
-
+  const scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
+  std::vector<uint32>* const stored_bitrates_;
   VideoEncodeAccelerator::Client* client_;
   bool first_;
 
