aeed3636230266759660b94d3fce016d5c3cd4a6
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-06 08:47:13 +0000

    Remove WebHeap::attachThread/detachThread
    No one uses the APIs. We can add them when necessary.
    
    This is a part of clean-up to implement a better abstraction layer for Web threads registered to Oilpan.
    
    BUG=340522
    
    Review URL: https://codereview.chromium.org/442133003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179584 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebHeap.cpp
+++ third_party/WebKit/Source/web/WebHeap.cpp
@@ -36,16 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-void WebHeap::attachThread()
-{
-    blink::ThreadState::attach();
-}
-
-void WebHeap::detachThread()
-{
-    blink::ThreadState::detach();
-}
-
 WebHeap::SafePointScope::SafePointScope()
 {
     blink::ThreadState::current()->enterSafePointWithPointers(this);
