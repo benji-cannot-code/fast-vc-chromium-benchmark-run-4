9edd31b573a67fad70a791796b2c78aec02b51c5
nhiroki@chromium.org <nhiroki@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-06 07:25:43 +0000

    ServiceWorker: Remove wildcard from scope (Blink)
    Wildcard '*' was removed from scope matching algorithm in the spec.
    This change removes a wildcard from scopes used in layout tests and
    the default scope for register/unregister interfaces.
    
    Spec: https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#registration-option-list-dictionary
    Issue: https://github.com/slightlyoff/ServiceWorker/issues/287
    
    
    BUG=398892
    TEST=run_webkit_tests.py http/tests/serviceworker
    
    Review URL: https://codereview.chromium.org/426603005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179574 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/serviceworkers/RegistrationOptionList.h
+++ third_party/WebKit/Source/modules/serviceworkers/RegistrationOptionList.h
@@ -12,7 +12,7 @@ namespace blink {
 
 struct RegistrationOptionList  {
     explicit RegistrationOptionList(const Dictionary& options)
-        : scope("/*")
+        : scope("/")
     {
         // FIXME: Should be ScalarValueString. http://crbug.com/379009
         DictionaryHelper::get(options, "scope", scope);
