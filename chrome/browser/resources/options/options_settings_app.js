92af2cc5101dbf5b1d1f017acd28a60fc84eb759
michaelpg@google.com <michaelpg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-07 07:36:11 +0000

    typo fix
    TBR=dbeam@chromium.org
    
    Review URL: https://codereview.chromium.org/444373003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@287991 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/options_settings_app.js
+++ chrome/browser/resources/options/options_settings_app.js
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   OptionsPage.setIsSettingsApp(true);
 
   // Override the offset in the options page.
-  PageManager.horizentalOffset = 38;
+  PageManager.horizontalOffset = 38;
 
   document.addEventListener('DOMContentLoaded', function() {
     // Hide everything by default.
