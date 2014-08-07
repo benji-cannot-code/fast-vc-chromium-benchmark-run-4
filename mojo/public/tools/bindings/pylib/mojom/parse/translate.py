cf3942945ccbb5c826cb713e7a19ef84a4ed52de
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-07 07:28:11 +0000

    Mojo: Add PRESUBMIT.py to mojo/.
    Currently, it only adds pylint checks, with a blacklist for all the
    files that fail horribly (or in otherwise mysterious ways).
    
    Also fix a few (less mysterious) pylint failures. Remove some pylint
    suppressions (since we now add appropriate directories to the python
    module path for pylint).
    
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/443293003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@287988 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/public/tools/bindings/pylib/mojom/parse/translate.py
+++ mojo/public/tools/bindings/pylib/mojom/parse/translate.py
@@ -6,8 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """Translates parse tree to Mojom IR."""
 
 
-import ast
 import re
+
+from . import ast
 
 
 def _MapTreeForType(func, tree, type_to_map):
