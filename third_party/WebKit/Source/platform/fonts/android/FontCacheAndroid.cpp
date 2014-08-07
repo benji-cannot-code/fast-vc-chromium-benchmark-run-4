3c3b6f4e9ef9f4902c26656ded1b72764bcbed1f
bungeman@chromium.org <bungeman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-07 21:03:26 +0000

    Use font manager for fallback on Android.
    The corresponding Chromium side change is https://codereview.chromium.org/450733002/ . These two can be committed in any order.
    
    Review URL: https://codereview.chromium.org/447233003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179753 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/fonts/android/FontCacheAndroid.cpp
+++ third_party/WebKit/Source/platform/fonts/android/FontCacheAndroid.cpp
@@ -36,8 +36,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/fonts/SimpleFontData.h"
 #include "platform/fonts/FontDescription.h"
 #include "platform/fonts/FontFaceCreationParams.h"
-
-#include "SkTypeface_android.h"
+#include "third_party/skia/include/core/SkTypeface.h"
+#include "third_party/skia/include/ports/SkFontMgr.h"
 
 namespace blink {
 
@@ -64,10 +64,13 @@ static AtomicString getFamilyNameForCharacter(UChar32 c, UScriptCode script)
         break;
     }
 
-    SkString skiaFamilyName;
-    if (!SkGetFallbackFamilyNameForChar(c, locale, &skiaFamilyName) || skiaFamilyName.isEmpty())
+    RefPtr<SkFontMgr> fm = adoptRef(SkFontMgr::RefDefault());
+    RefPtr<SkTypeface> typeface = adoptRef(fm->matchFamilyStyleCharacter(0, SkFontStyle(), locale, c));
+    if (!typeface)
         return emptyAtom;
 
+    SkString skiaFamilyName;
+    typeface->getFamilyName(&skiaFamilyName);
     return skiaFamilyName.c_str();
 }
 
