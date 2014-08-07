8fa8b0781e89a906cf4d736da8fadbb44e7e906c
enne@chromium.org <enne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-07 03:15:45 +0000

    Make SingleThreadProxy a SchedulerClient
    This makes ui::Compositor no longer in charge of
    scheduling commits and draws, deferring it to cc::Scheduler.
    
    Other compositors that use SingleThreadProxy are left calling composite
    synchronously and now pass a flag to indicate that this is their
    intention.  This patch doesn't remove synchronous composite, but now
    makes it mutually exclusive with scheduling.
    
    BUG=329552, 287250
    
    Review URL: https://codereview.chromium.org/134623005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@287950 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/gpu/render_widget_compositor.h
+++ content/renderer/gpu/render_widget_compositor.h
@@ -45,9 +45,7 @@ class RenderWidgetCompositor : public blink::WebLayerTreeView,
   virtual ~RenderWidgetCompositor();
 
   const base::WeakPtr<cc::InputHandler>& GetInputHandler();
-  void SetSuppressScheduleComposite(bool suppress);
   bool BeginMainFrameRequested() const;
-  void UpdateAnimations(base::TimeTicks time);
   void SetNeedsDisplayOnAllLayers();
   void SetRasterizeOnlyVisibleContent();
   void UpdateTopControlsState(cc::TopControlsState constraints,
@@ -144,7 +142,6 @@ class RenderWidgetCompositor : public blink::WebLayerTreeView,
   virtual void RateLimitSharedMainThreadContext() OVERRIDE;
 
   // cc::LayerTreeHostSingleThreadClient implementation.
-  virtual void ScheduleComposite() OVERRIDE;
   virtual void ScheduleAnimation() OVERRIDE;
   virtual void DidPostSwapBuffers() OVERRIDE;
   virtual void DidAbortSwapBuffers() OVERRIDE;
@@ -155,7 +152,6 @@ class RenderWidgetCompositor : public blink::WebLayerTreeView,
   void Initialize(cc::LayerTreeSettings settings);
 
   bool threaded_;
-  bool suppress_schedule_composite_;
   RenderWidget* widget_;
   scoped_ptr<cc::LayerTreeHost> layer_tree_host_;
 };
--- ui/compositor/compositor.h
+++ ui/compositor/compositor.h
@@ -25,8 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/size.h"
 #include "ui/gfx/vector2d.h"
 
-class SkBitmap;
-
 namespace base {
 class MessageLoopProxy;
 class RunLoop;
@@ -160,9 +158,6 @@ class COMPOSITOR_EXPORT Compositor
   // compositing layers on.
   float device_scale_factor() const { return device_scale_factor_; }
 
-  // Draws the scene created by the layer tree and any visual effects.
-  void Draw();
-
   // Where possible, draws are scissored to a damage region calculated from
   // changes to layer properties.  This bypasses that and indicates that
   // the whole frame needs to be drawn.
@@ -235,13 +230,8 @@ class COMPOSITOR_EXPORT Compositor
   virtual void DidCompleteSwapBuffers() OVERRIDE;
 
   // cc::LayerTreeHostSingleThreadClient implementation.
-  virtual void ScheduleComposite() OVERRIDE;
-  virtual void ScheduleAnimation() OVERRIDE;
   virtual void DidPostSwapBuffers() OVERRIDE;
   virtual void DidAbortSwapBuffers() OVERRIDE;
-
-  int last_started_frame() { return last_started_frame_; }
-  int last_ended_frame() { return last_ended_frame_; }
 
   bool IsLocked() { return compositor_lock_ != NULL; }
 
@@ -261,9 +251,6 @@ class COMPOSITOR_EXPORT Compositor
 
   // Called to release any pending CompositorLock
   void CancelCompositorLock();
-
-  // Notifies the compositor that compositing is complete.
-  void NotifyEnd();
 
   gfx::Size size_;
 
@@ -295,18 +282,7 @@ class COMPOSITOR_EXPORT Compositor
 
   CompositorLock* compositor_lock_;
 
-  // Prevent more than one draw from being scheduled.
-  bool defer_draw_scheduling_;
-
-  // Used to prevent Draw()s while a composite is in progress.
-  bool waiting_on_compositing_end_;
-  bool draw_on_compositing_end_;
-  enum SwapState { SWAP_NONE, SWAP_POSTED, SWAP_COMPLETED };
-  SwapState swap_state_;
-
   LayerAnimatorCollection layer_animator_collection_;
-
-  base::WeakPtrFactory<Compositor> schedule_draw_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(Compositor);
 };
