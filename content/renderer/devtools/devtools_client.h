decb5a6acb5a71480f2bd291a861de861a2288a8
dgozman@chromium.org <dgozman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-07 11:04:47 +0000

    [DevTools] Move DispatchOnDevToolsFrontend to embedder.
    We already have a way to send message via ExecuteJavaScript,
    no need for another mechanism.
    
    BUG=398046
    TBR=jam
    
    Review URL: https://codereview.chromium.org/418243003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288027 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/devtools/devtools_client.h
+++ content/renderer/devtools/devtools_client.h
@@ -36,9 +36,6 @@ class CONTENT_EXPORT DevToolsClient
   virtual ~DevToolsClient();
 
  private:
-  // RenderView::Observer implementation.
-  virtual bool OnMessageReceived(const IPC::Message& message) OVERRIDE;
-
   // WebDevToolsFrontendClient implementation.
   virtual void sendMessageToBackend(const blink::WebString&) OVERRIDE;
   virtual void sendMessageToEmbedder(const blink::WebString&) OVERRIDE;
