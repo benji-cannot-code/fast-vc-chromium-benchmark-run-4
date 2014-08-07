1ef4452fc6af155200248608dd674804515df319
yutak@chromium.org <yutak@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-07 07:53:15 +0000

    Move a boolean flag in RenderInline to RenderObject.
    RenderInline has a 1-bit bitfield for a boolean flag, which takes 4 or 8 bytes
    of space for each object. The number of RenderInline objects created in a
    browsing session can be fairly large, so the extra space allocated for
    RenderInline objects might be pretty wasteful.
    
    This patch moves the flag into RenderObject, which still has some unused slots
    in its bitfield area. It should cut the extra space in RenderInline.
    
    The similar approach is already attempted for RenderBlocks; there are a couple
    of RenderBlock-specific flags defined in RenderObject already. The patch adds
    a compile-time assertion, which is similar to the one in RenderBlock, in order
    to prevent future regressions.
    
    The patch also makes some clean-ups of the flags defined in RenderObject-
    BitFields. Most notably, the comment about the size of RenderObjectBitFields
    was wrong :-/
    
    No behavior change, hence no new tests.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/431923002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179690 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderInline.cpp
+++ third_party/WebKit/Source/core/rendering/RenderInline.cpp
@@ -45,9 +45,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+struct SameSizeAsRenderInline : public RenderBoxModelObject {
+    virtual ~SameSizeAsRenderInline() { }
+    RenderObjectChildList m_children;
+    RenderLineBoxList m_lineBoxes;
+};
+
+COMPILE_ASSERT(sizeof(RenderInline) == sizeof(SameSizeAsRenderInline), RenderInline_should_stay_small);
+
 RenderInline::RenderInline(Element* element)
     : RenderBoxModelObject(element)
-    , m_alwaysCreateLineBoxes(false)
 {
     setChildrenInline(true);
 }
@@ -189,13 +196,13 @@ void RenderInline::styleDidChange(StyleDifference diff, const RenderStyle* oldSt
         updateStyleOfAnonymousBlockContinuations(block, newStyle, oldStyle);
     }
 
-    if (!m_alwaysCreateLineBoxes) {
-        bool alwaysCreateLineBoxes = hasSelfPaintingLayer() || hasBoxDecorationBackground() || newStyle->hasPadding() || newStyle->hasMargin() || hasOutline();
-        if (oldStyle && alwaysCreateLineBoxes) {
+    if (!alwaysCreateLineBoxes()) {
+        bool alwaysCreateLineBoxesNew = hasSelfPaintingLayer() || hasBoxDecorationBackground() || newStyle->hasPadding() || newStyle->hasMargin() || hasOutline();
+        if (oldStyle && alwaysCreateLineBoxesNew) {
             dirtyLineBoxes(false);
             setNeedsLayoutAndFullPaintInvalidation();
         }
-        m_alwaysCreateLineBoxes = alwaysCreateLineBoxes;
+        setAlwaysCreateLineBoxes(alwaysCreateLineBoxesNew);
     }
 }
 
@@ -203,32 +210,32 @@ void RenderInline::updateAlwaysCreateLineBoxes(bool fullLayout)
 {
     // Once we have been tainted once, just assume it will happen again. This way effects like hover highlighting that change the
     // background color will only cause a layout on the first rollover.
-    if (m_alwaysCreateLineBoxes)
+    if (alwaysCreateLineBoxes())
         return;
 
     RenderStyle* parentStyle = parent()->style();
     RenderInline* parentRenderInline = parent()->isRenderInline() ? toRenderInline(parent()) : 0;
     bool checkFonts = document().inNoQuirksMode();
-    bool alwaysCreateLineBoxes = (parentRenderInline && parentRenderInline->alwaysCreateLineBoxes())
+    bool alwaysCreateLineBoxesNew = (parentRenderInline && parentRenderInline->alwaysCreateLineBoxes())
         || (parentRenderInline && parentStyle->verticalAlign() != BASELINE)
         || style()->verticalAlign() != BASELINE
         || style()->textEmphasisMark() != TextEmphasisMarkNone
         || (checkFonts && (!parentStyle->font().fontMetrics().hasIdenticalAscentDescentAndLineGap(style()->font().fontMetrics())
         || parentStyle->lineHeight() != style()->lineHeight()));
 
-    if (!alwaysCreateLineBoxes && checkFonts && document().styleEngine()->usesFirstLineRules()) {
+    if (!alwaysCreateLineBoxesNew && checkFonts && document().styleEngine()->usesFirstLineRules()) {
         // Have to check the first line style as well.
         parentStyle = parent()->style(true);
         RenderStyle* childStyle = style(true);
-        alwaysCreateLineBoxes = !parentStyle->font().fontMetrics().hasIdenticalAscentDescentAndLineGap(childStyle->font().fontMetrics())
+        alwaysCreateLineBoxesNew = !parentStyle->font().fontMetrics().hasIdenticalAscentDescentAndLineGap(childStyle->font().fontMetrics())
         || childStyle->verticalAlign() != BASELINE
         || parentStyle->lineHeight() != childStyle->lineHeight();
     }
 
-    if (alwaysCreateLineBoxes) {
+    if (alwaysCreateLineBoxesNew) {
         if (!fullLayout)
             dirtyLineBoxes(false);
-        m_alwaysCreateLineBoxes = true;
+        setAlwaysCreateLineBoxes();
     }
 }
 
