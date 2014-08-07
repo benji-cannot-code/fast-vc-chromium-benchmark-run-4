dcbed63a2a68531a37d5c76612658fd015ecaef8
sohan.jyoti@samsung.com <sohan.jyoti@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-07 19:25:16 +0000

    Avoid passing uninitialized value to markRectAsNonOpaque.
    While applying opaque region for layer if device clip is not a rect 
    we don't alter the opaque rect, and if we have a non-opaque
    preserving transfer mode along with it, we mark the opaque rect
    as empty.
    
    BUG=390999
    
    Review URL: https://codereview.chromium.org/417153002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179741 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/RegionTracker.cpp
+++ third_party/WebKit/Source/platform/graphics/RegionTracker.cpp
@@ -174,8 +174,10 @@ static inline bool paintIsOpaque(const SkPaint& paint, RegionTracker::DrawType d
 static inline bool getDeviceClipAsRect(const GraphicsContext* context, SkRect& deviceClipRect)
 {
     // Get the current clip in device coordinate space.
-    if (!context->canvas()->isClipRect())
+    if (!context->canvas()->isClipRect()) {
+        deviceClipRect.setEmpty();
         return false;
+    }
 
     SkIRect deviceClipIRect;
     if (context->canvas()->getClipDeviceBounds(&deviceClipIRect))
@@ -354,7 +356,7 @@ void RegionTracker::applyOpaqueRegionFromLayer(const GraphicsContext* context, c
     SkRect deviceClipRect;
     bool deviceClipIsARect = getDeviceClipAsRect(context, deviceClipRect);
 
-    if (deviceClipRect.isEmpty())
+    if (deviceClipIsARect && deviceClipRect.isEmpty())
         return;
 
     SkRect sourceOpaqueRect = layerOpaqueRect;
@@ -362,8 +364,13 @@ void RegionTracker::applyOpaqueRegionFromLayer(const GraphicsContext* context, c
     SkRect destinationOpaqueRect = currentTrackingOpaqueRect();
 
     bool outsideSourceOpaqueRectPreservesOpaque = xfermodePreservesOpaque(paint, false);
-    if (!outsideSourceOpaqueRectPreservesOpaque)
+    if (!outsideSourceOpaqueRectPreservesOpaque) {
+        if (!deviceClipIsARect) {
+            markAllAsNonOpaque();
+            return;
+        }
         markRectAsNonOpaque(deviceClipRect);
+    }
 
     if (!deviceClipIsARect)
         return;
