efecbc0fa7a1ca8c5cfcfaff48f7f647a7b45a2e
ed@opera.com <ed@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-07 12:41:11 +0000

    [SVG2] Make transform, gradientTransform and patternTransform presentation attributes.
    Spec:
    http://www.w3.org/TR/css3-transforms/#svg-transform
    http://www.w3.org/TR/css3-transforms/#svg-syntax
    http://www.w3.org/TR/css3-transforms/#svg-gradient-transform-pattern-transform
    
    BUG=369942
    
    Review URL: https://codereview.chromium.org/423093014
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179702 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGElement.h
+++ third_party/WebKit/Source/core/svg/SVGElement.h
@@ -192,6 +192,8 @@ public:
 
     static const AtomicString& eventParameterName();
 
+    bool getStyleTransform(AffineTransform& matrix) const;
+
 protected:
     SVGElement(const QualifiedName&, Document&, ConstructionType = CreateSVGElement);
 
