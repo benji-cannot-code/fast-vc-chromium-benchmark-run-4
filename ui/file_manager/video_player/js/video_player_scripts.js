85db30e84087feabed13e80876fa025b41f9451e
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-07 07:56:17 +0000

    Video Player: Check Cast extension before launching cast feature
    If the cast extension is not install, video player doesn't activate the cast-related feature.
    
    BUG=399557
    TEST=manually tested
    R=fukino@chromium.org
    
    Review URL: https://codereview.chromium.org/443083002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288003 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/file_manager/video_player/js/video_player_scripts.js
+++ ui/file_manager/video_player/js/video_player_scripts.js
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //<include src="../../file_manager/foreground/js/media/media_controls.js"/>
 //<include src="../../file_manager/foreground/js/media/mouse_inactivity_watcher.js"/>
 
+//<include src="cast/cast_extension_discoverer.js"/>
 //<include src="cast/cast_video_element.js"/>
 //<include src="cast/media_manager.js"/>
 //<include src="cast/caster.js"/>
