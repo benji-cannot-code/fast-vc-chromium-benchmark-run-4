8f90714c0187c33124c5fd50677728f593ee3578
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-07 22:52:16 +0000

    Cleanup: Make ExtensionIconSet::Get return a const std::string& rather than a std::string.
    R=mek@chromium.org
    
    Review URL: https://codereview.chromium.org/448193002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288156 0039d316-1c4b-4281-b951-d872f2087c98

--- extensions/common/manifest_handlers/icons_handler.cc
+++ extensions/common/manifest_handlers/icons_handler.cc
@@ -38,7 +38,7 @@ ExtensionResource IconsInfo::GetIconResource(
     const Extension* extension,
     int size,
     ExtensionIconSet::MatchType match_type) {
-  std::string path = GetIcons(extension).Get(size, match_type);
+  const std::string& path = GetIcons(extension).Get(size, match_type);
   return path.empty() ? ExtensionResource() : extension->GetResource(path);
 }
 
@@ -46,7 +46,7 @@ ExtensionResource IconsInfo::GetIconResource(
 GURL IconsInfo::GetIconURL(const Extension* extension,
                            int size,
                            ExtensionIconSet::MatchType match_type) {
-  std::string path = GetIcons(extension).Get(size, match_type);
+  const std::string& path = GetIcons(extension).Get(size, match_type);
   return path.empty() ? GURL() : extension->GetResourceURL(path);
 }
 
