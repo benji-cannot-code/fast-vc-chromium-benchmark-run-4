c6621c66d6c7156bcc8983066ee622f9e929dff3
wangxianzhu@chromium.org <wangxianzhu@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-07 00:04:32 +0000

    Improve performance of log_to_server during rebaseline
    Previously log_to_server sent each line in a separate http request.
    On my mac, each request takes 1 to 2 seconds. When doing a big rebaseline,
    thousands of lines of messages (even non-verbose) will take an hour.
    
    Make logging asynchronous. Let log_to_server send logs in batches.
    
    Review URL: https://codereview.chromium.org/442923002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179667 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/tool/commands/rebaseline_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/commands/rebaseline_unittest.py
@@ -1204,19 +1204,19 @@ class TestRebaselineOMatic(_BaseTestCase):
         super(TestRebaselineOMatic, self).setUp()
         self._logs = []
 
-    def _mock_log_to_server(self, log='', is_new_entry=False):
-        self._logs.append({'log': log, 'newentry': is_new_entry})
+    def _mock_log_to_server(self, log=''):
+        self._logs.append(log)
 
     def test_run_logged_command(self):
         self.command._verbose = False
-        self.command._log_to_server = self._mock_log_to_server
+        self.command._post_log_to_server = self._mock_log_to_server
         self.command._run_logged_command(['echo', 'foo'])
         self.assertEqual(self.tool.executive.calls, [['echo', 'foo']])
-        self.assertEqual(self._logs, [{'log': 'MOCK STDOUT', 'newentry': False}])
+        self.assertEqual(self._logs, ['MOCK STDOUT'])
 
     def test_do_one_rebaseline(self):
         self.command._verbose = False
-        self.command._log_to_server = self._mock_log_to_server
+        self.command._post_log_to_server = self._mock_log_to_server
 
         oc = OutputCapture()
         oc.capture_output()
@@ -1228,14 +1228,11 @@ class TestRebaselineOMatic(_BaseTestCase):
             ['git', 'pull'],
             ['/mock-checkout/third_party/WebKit/Tools/Scripts/webkit-patch', 'auto-rebaseline'],
         ])
-        self.assertEqual(self._logs, [
-            {'log': '', 'newentry': True},
-            {'log': 'MOCK STDOUT', 'newentry': False},
-        ])
+        self.assertEqual(self._logs, ['MOCK STDOUT'])
 
     def test_do_one_rebaseline_verbose(self):
         self.command._verbose = True
-        self.command._log_to_server = self._mock_log_to_server
+        self.command._post_log_to_server = self._mock_log_to_server
 
         oc = OutputCapture()
         oc.capture_output()
@@ -1247,7 +1244,4 @@ class TestRebaselineOMatic(_BaseTestCase):
             ['git', 'pull'],
             ['/mock-checkout/third_party/WebKit/Tools/Scripts/webkit-patch', 'auto-rebaseline', '--verbose'],
         ])
-        self.assertEqual(self._logs, [
-            {'log': '', 'newentry': True},
-            {'log': 'MOCK STDOUT', 'newentry': False},
-        ])
+        self.assertEqual(self._logs, ['MOCK STDOUT'])
