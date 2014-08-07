c794f17b3d6bb3a02eccbdf9047bdfc9be4cc5f9
mkwst@chromium.org <mkwst@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-07 16:58:20 +0000

    Credential Manager: basic Credential interfaces
    This is step towards a stub implementation of the Credential Manager API
    defined in http://projects.mikewest.org/credentialmanagement/spec/. For
    obvious reasons, it isn't web-exposed, and isn't yet enabled by the
    experimental flag. There's still a good amount of work to do before the
    API actually _does_ anything, much less anything we'd like folks to
    start banging on.
    
    BUG=400674
    
    Review URL: https://codereview.chromium.org/450633002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179727 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/Source/modules/credentialmanager/FederatedCredential.cpp
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "config.h"
+#include "modules/credentialmanager/FederatedCredential.h"
+
+namespace blink {
+
+FederatedCredential* FederatedCredential::create(const String& id, const String& name, const String& avatarURL, const String& federation)
+{
+    return new FederatedCredential(id, name, avatarURL, federation);
+}
+
+FederatedCredential::FederatedCredential(const String& id, const String& name, const String& avatarURL, const String& federation)
+    : Credential(id, name, avatarURL)
+    , m_federation(federation)
+{
+}
+
+FederatedCredential::~FederatedCredential()
+{
+}
+
+} // namespace blink
---
+++ third_party/WebKit/Source/modules/credentialmanager/LocalCredential.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef LocalCredential_h
+#define LocalCredential_h
+
+#include "bindings/core/v8/ScriptWrappable.h"
+#include "bindings/core/v8/SerializedScriptValue.h"
+#include "modules/credentialmanager/Credential.h"
+#include "platform/heap/Handle.h"
+
+namespace blink {
+
+class LocalCredential FINAL : public Credential {
+public:
+    static LocalCredential* create(const String& id, const String& name, const String& avatarURL, const String& password);
+    virtual ~LocalCredential();
+
+    // LocalCredential.idl
+    const String& password() const { return m_password; }
+
+private:
+    LocalCredential(const String& id, const String& name, const String& avatarURL, const String& password);
+
+    String m_password;
+};
+
+} // namespace blink
+
+#endif // Credential_h
