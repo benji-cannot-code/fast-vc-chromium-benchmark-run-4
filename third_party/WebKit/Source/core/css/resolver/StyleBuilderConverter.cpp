5961112624ad7fb13a76623563353d7913b55268
andersr@opera.com <andersr@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-07 23:03:11 +0000

    Remove custom style building for -webkit-font-feature-settings.
    This patch replaces the custom style application for
    -webkit-font-feature-settings with a function in StyleBuilderConverter.
    
    Also:
    
     * Less use of CSSValue in FontBuilder.
     * We now handle 'initial' and 'inherit'.
     * Eliminates FontDescription::makeNormalFeatureSettings.
    
    Note that in StyleBuilderConverter::convertFontFeatureSettings, the check
    for isFontFeatureValue() is removed. Asserting (via toCSSFontFeatureValue)
    should be enough, since the parser will only add CSSFontFeatureValues to the
    list.
    
    Review URL: https://codereview.chromium.org/444333005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179764 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/resolver/StyleBuilderConverter.cpp
+++ third_party/WebKit/Source/core/css/resolver/StyleBuilderConverter.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "core/css/resolver/StyleBuilderConverter.h"
 
+#include "core/css/CSSFontFeatureValue.h"
 #include "core/css/CSSFunctionValue.h"
 #include "core/css/CSSGridLineNamesValue.h"
 #include "core/css/CSSPrimitiveValueMappings.h"
@@ -89,6 +90,21 @@ AtomicString StyleBuilderConverter::convertFragmentIdentifier(StyleResolverState
     if (primitiveValue->isURI())
         return SVGURIReference::fragmentIdentifierFromIRIString(primitiveValue->getStringValue(), state.element()->treeScope());
     return nullAtom;
+}
+
+PassRefPtr<FontFeatureSettings> StyleBuilderConverter::convertFontFeatureSettings(StyleResolverState& state, CSSValue* value)
+{
+    if (value->isPrimitiveValue() && toCSSPrimitiveValue(value)->getValueID() == CSSValueNormal)
+        return FontBuilder::initialFeatureSettings();
+
+    CSSValueList* list = toCSSValueList(value);
+    RefPtr<FontFeatureSettings> settings = FontFeatureSettings::create();
+    int len = list->length();
+    for (int i = 0; i < len; ++i) {
+        CSSFontFeatureValue* feature = toCSSFontFeatureValue(list->item(i));
+        settings->append(FontFeature(feature->tag(), feature->value()));
+    }
+    return settings;
 }
 
 FontWeight StyleBuilderConverter::convertFontWeight(StyleResolverState& state, CSSValue* value)
