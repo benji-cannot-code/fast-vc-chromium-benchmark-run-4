31e2fe68b87d3c1ca85e0008ba8a8965df1cb0b0
michaeln@chromium.org <michaeln@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-08 21:17:41 +0000

    Bust the MemoryCache to ensure script requests reach the browser-side and get added to and retrieved from the ServiceWorker's script cache.
    BUG=387589
    
    Review URL: https://codereview.chromium.org/419693002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179868 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/serviceworkers/ServiceWorkerGlobalScope.cpp
+++ third_party/WebKit/Source/modules/serviceworkers/ServiceWorkerGlobalScope.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/ScriptPromise.h"
 #include "bindings/core/v8/ScriptState.h"
 #include "bindings/core/v8/V8ThrowException.h"
+#include "core/fetch/MemoryCache.h"
 #include "core/fetch/ResourceLoaderOptions.h"
 #include "core/inspector/ScriptCallStack.h"
 #include "core/loader/ThreadableLoader.h"
@@ -176,6 +177,16 @@ void ServiceWorkerGlobalScope::trace(Visitor* visitor)
     visitor->trace(m_clients);
     visitor->trace(m_caches);
     WorkerGlobalScope::trace(visitor);
+}
+
+void ServiceWorkerGlobalScope::importScripts(const Vector<String>& urls, ExceptionState& exceptionState)
+{
+    // Bust the MemoryCache to ensure script requests reach the browser-side
+    // and get added to and retrieved from the ServiceWorker's script cache.
+    // FIXME: Revisit in light of the solution to crbug/388375.
+    for (Vector<String>::const_iterator it = urls.begin(); it != urls.end(); ++it)
+        MemoryCache::removeURLFromCache(this->executionContext(), completeURL(*it));
+    WorkerGlobalScope::importScripts(urls, exceptionState);
 }
 
 void ServiceWorkerGlobalScope::logExceptionToConsole(const String& errorMessage, const String& sourceURL, int lineNumber, int columnNumber, PassRefPtrWillBeRawPtr<ScriptCallStack> callStack)
