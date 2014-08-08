f2bb6a9dfb1b7424a18ee09542e14ea91b9cbf3d
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-08 18:28:39 +0000

    content: Rename blink_glue.* to file_info_util.*.
    Reason: blink_glue.* is too generic for this.
    
    It just contains a single function to convert from base::File::Info to
    blink::WebFileInfo. Similar to web_url_request_util.h and
    pepper_file_util.h.
    
    BUG=345892
    TEST=None
    R=avi@chromium.org
    
    Review URL: https://codereview.chromium.org/455623002
    
    Cr-Commit-Position: refs/heads/master@{#288399}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288399 0039d316-1c4b-4281-b951-d872f2087c98

--- content/child/file_info_util.h
+++ content/child/file_info_util.h
@@ -3,11 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_CHILD_BLINK_GLUE_H_
-#define CONTENT_CHILD_BLINK_GLUE_H_
+#ifndef CONTENT_CHILD_FILE_INFO_UTIL_H_
+#define CONTENT_CHILD_FILE_INFO_UTIL_H_
 
 #include "base/files/file.h"
-#include "content/common/content_export.h"
 
 namespace blink {
 struct WebFileInfo;
@@ -16,9 +15,9 @@ struct WebFileInfo;
 namespace content {
 
 // File info conversion
-CONTENT_EXPORT void FileInfoToWebFileInfo(const base::File::Info& file_info,
-                                          blink::WebFileInfo* web_file_info);
+void FileInfoToWebFileInfo(const base::File::Info& file_info,
+                           blink::WebFileInfo* web_file_info);
 
 }  // namespace content
 
-#endif  // CONTENT_CHILD_BLINK_GLUE_H_
+#endif  // CONTENT_CHILD_FILE_INFO_UTIL_H_
--- content/child/webfileutilities_impl.cc
+++ content/child/webfileutilities_impl.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_util.h"
 #include "base/files/file_path.h"
 #include "base/logging.h"
-#include "content/child/blink_glue.h"
+#include "content/child/file_info_util.h"
 #include "net/base/file_stream.h"
 #include "net/base/filename_util.h"
 #include "third_party/WebKit/public/platform/WebFileInfo.h"
