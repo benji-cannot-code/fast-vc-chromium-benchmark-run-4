120e4eb930c2fa54d5a43c244e32231447b252e1
jbroman@chromium.org <jbroman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-08 14:45:39 +0000

    Also disable PinchViewportTest.TestScrollingDocumentRegionIntoView
    It has started failing on the Mac bots, especially on Mac10.7.
    http://build.chromium.org/p/chromium.webkit/builders/WebKit%20Mac10.7/builds/30251
    
    Again, there is no apparent culprit in the blame list.
    
    BUG=401151
    TBR=bokan@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/451873003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179836 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/tests/PinchViewportTest.cpp
+++ third_party/WebKit/Source/web/tests/PinchViewportTest.cpp
@@ -767,7 +767,7 @@ TEST_F(PinchViewportTest, TestContextMenuShownInCorrectLocation)
 
 // Test that the scrollIntoView correctly scrolls the main frame
 // and pinch viewports such that the given rect is centered in the viewport.
-TEST_F(PinchViewportTest, TestScrollingDocumentRegionIntoView)
+TEST_F(PinchViewportTest, DISABLED_TestScrollingDocumentRegionIntoView)
 {
     initializeWithDesktopSettings();
     webViewImpl()->resize(IntSize(100, 150));
