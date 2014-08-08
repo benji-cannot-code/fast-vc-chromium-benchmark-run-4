ae31e05e4c203d0eb79d1eb95437359330b95cb7
felt@chromium.org <felt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-08 22:52:26 +0000

    Simplify overridable logic in SSLBlockingPage
    Right now, SSLBlockingPage has both overridable_ and strict_enforcement_. 
    This CL collapses both into overridable_, so that we only need to check one. 
    However, strict_enforcement_ also remains on its own because it is checked 
    separately in one place.
    
    BUG=
    R=palmer@chromium.org
    
    Review URL: https://codereview.chromium.org/448163002
    
    Cr-Commit-Position: refs/heads/master@{#288456}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288456 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ssl/ssl_blocking_page.cc
+++ chrome/browser/ssl/ssl_blocking_page.cc
@@ -309,11 +309,12 @@ SSLBlockingPage::SSLBlockingPage(
       captive_portal_detected_(false) {
   Profile* profile = Profile::FromBrowserContext(
       web_contents->GetBrowserContext());
+  if (strict_enforcement_) overridable_ = false;
   // For UMA stats.
   if (net::IsHostnameNonUnique(request_url_.HostNoBrackets()))
     internal_ = true;
   RecordSSLBlockingPageEventStats(SHOW_ALL);
-  if (overridable_ && !strict_enforcement_) {
+  if (overridable_) {
     RecordSSLBlockingPageEventStats(SHOW_OVERRIDABLE);
     if (internal_)
       RecordSSLBlockingPageEventStats(SHOW_INTERNAL_HOSTNAME);
@@ -332,8 +333,7 @@ SSLBlockingPage::SSLBlockingPage(
       base::Time::NowFromSystemTime(),
       request_url_,
       *ssl_info_.cert.get());
-  ssl_error_classification.RecordUMAStatistics(
-      overridable_ && !strict_enforcement_, cert_error_);
+  ssl_error_classification.RecordUMAStatistics(overridable_, cert_error_);
 
 #if defined(ENABLE_CAPTIVE_PORTAL_DETECTION)
   CaptivePortalService* captive_portal_service =
@@ -368,7 +368,7 @@ SSLBlockingPage::~SSLBlockingPage() {
   if (!callback_.is_null()) {
     RecordSSLBlockingPageDetailedStats(false,
                                        cert_error_,
-                                       overridable_ && !strict_enforcement_,
+                                       overridable_,
                                        internal_,
                                        num_visits_,
                                        captive_portal_detection_enabled_,
@@ -397,8 +397,7 @@ std::string SSLBlockingPage::GetHTMLContents() {
 
   // Shared values for both the overridable and non-overridable versions.
   load_time_data.SetBoolean("ssl", true);
-  load_time_data.SetBoolean(
-      "overridable", overridable_ && !strict_enforcement_);
+  load_time_data.SetBoolean("overridable", overridable_);
   load_time_data.SetString(
       "tabTitle", l10n_util::GetStringUTF16(IDS_SSL_V2_TITLE));
   load_time_data.SetString(
@@ -425,7 +424,7 @@ std::string SSLBlockingPage::GetHTMLContents() {
      l10n_util::GetStringUTF16(IDS_SSL_V2_CLOSE_DETAILS_BUTTON));
   load_time_data.SetString("errorCode", net::ErrorToString(cert_error_));
 
-  if (overridable_ && !strict_enforcement_) {  // Overridable.
+  if (overridable_) {  // Overridable.
     SSLErrorInfo error_info =
         SSLErrorInfo::CreateError(
             SSLErrorInfo::NetErrorToErrorType(cert_error_),
@@ -558,7 +557,7 @@ void SSLBlockingPage::OverrideRendererPrefs(
 void SSLBlockingPage::OnProceed() {
   RecordSSLBlockingPageDetailedStats(true,
                                      cert_error_,
-                                     overridable_ && !strict_enforcement_,
+                                     overridable_,
                                      internal_,
                                      num_visits_,
                                      captive_portal_detection_enabled_,
@@ -577,7 +576,7 @@ void SSLBlockingPage::OnProceed() {
 void SSLBlockingPage::OnDontProceed() {
   RecordSSLBlockingPageDetailedStats(false,
                                      cert_error_,
-                                     overridable_ && !strict_enforcement_,
+                                     overridable_,
                                      internal_,
                                      num_visits_,
                                      captive_portal_detection_enabled_,
