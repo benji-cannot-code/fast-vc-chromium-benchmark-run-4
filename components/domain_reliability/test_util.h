6c67efa13eedc65fea4834f0ffd8a566920d0315
ttuttle@chromium.org <ttuttle@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-08 21:05:37 +0000

    Domain Reliability: Add wildcard domain support.
    Add support for configs with domains *.foo.bar.  Given a domain foo.bar, it
    will first look for a config with domain foo.bar, then *.foo.bar, then *.bar.
    
    BUG=396139
    
    Review URL: https://codereview.chromium.org/404363003
    
    Cr-Commit-Position: refs/heads/master@{#288428}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288428 0039d316-1c4b-4281-b951-d872f2087c98

--- components/domain_reliability/test_util.h
+++ components/domain_reliability/test_util.h
@@ -116,6 +116,8 @@ class MockTime : public MockableTime {
 };
 
 scoped_ptr<const DomainReliabilityConfig> MakeTestConfig();
+scoped_ptr<const DomainReliabilityConfig> MakeTestConfigWithDomain(
+    const std::string& domain);
 DomainReliabilityScheduler::Params MakeTestSchedulerParams();
 
 }  // namespace domain_reliability
