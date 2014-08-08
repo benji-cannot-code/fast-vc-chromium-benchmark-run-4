00fdffe8a804ffbadd12f1ffbc2a122964a124a2
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-08 01:47:41 +0000

    base: Remove now-unused MessagePumpObserver.
    Jankometer is the last instance of a MessagePumpObserver. Since it has been
    removed, MessagePumpObserver can also be removed.
    
    BUG=none
    R=darin@chromium.org
    
    Review URL: https://codereview.chromium.org/421173003
    
    Cr-Commit-Position: refs/heads/master@{#288203}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288203 0039d316-1c4b-4281-b951-d872f2087c98

--- base/message_loop/message_loop.h
+++ base/message_loop/message_loop.h
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 
 class HistogramBase;
-class MessagePumpObserver;
 class RunLoop;
 class ThreadTaskRunnerHandle;
 class WaitableEvent;
@@ -550,14 +549,6 @@ class BASE_EXPORT MessageLoopForUI : public MessageLoop {
   // never be called. Instead use Start(), which will forward all the native UI
   // events to the Java message loop.
   void Start();
-#endif
-
-#if defined(OS_WIN)
-  typedef MessagePumpObserver Observer;
-
-  // Please see message_pump_win for definitions of these methods.
-  void AddObserver(Observer* observer);
-  void RemoveObserver(Observer* observer);
 #endif
 
 #if defined(USE_OZONE) || (defined(USE_X11) && !defined(USE_GLIB))
