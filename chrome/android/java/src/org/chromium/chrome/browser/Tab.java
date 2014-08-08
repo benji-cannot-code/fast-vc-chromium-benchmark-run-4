73cdbf05979a1de477b07abe09d7ecde018e2c2f
abarth@chromium.org <abarth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-08 10:25:49 +0000

    Enable HTMLMediaElement@autoplay on Android
    This CL sets user_gesture_required_for_media_playback to false in Chrome for
    Android. Previously, we removed this requirement as a special case for the
    Google Search Results page. Now that this setting matches Chrome's behavior on
    other platforms, we don't need the special case for google.com and can remove
    the entire VoiceSearchTabHelper.
    
    This CL leaves the user_gesture_required_for_media_playback WebPreference
    because that's used by android_webview.
    
    R=klobag@chromium.org, darin@chromium.org
    BUG=178297
    
    Review URL: https://codereview.chromium.org/447353002
    
    Cr-Commit-Position: refs/heads/master@{#288308}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288308 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/android/java/src/org/chromium/chrome/browser/Tab.java
+++ chrome/android/java/src/org/chromium/chrome/browser/Tab.java
@@ -122,7 +122,6 @@ public class Tab implements NavigationClient {
     // Content layer Observers and Delegates
     private ContentViewClient mContentViewClient;
     private WebContentsObserverAndroid mWebContentsObserver;
-    private VoiceSearchTabHelper mVoiceSearchTabHelper;
     private TabChromeWebContentsDelegateAndroid mWebContentsDelegate;
     private DomDistillerFeedbackReporter mDomDistillerFeedbackReporter;
 
@@ -796,7 +795,6 @@ public class Tab implements NavigationClient {
 
         mWebContentsDelegate = createWebContentsDelegate();
         mWebContentsObserver = new TabWebContentsObserverAndroid(mContentViewCore.getWebContents());
-        mVoiceSearchTabHelper = new VoiceSearchTabHelper(mContentViewCore.getWebContents());
 
         if (mContentViewClient != null) mContentViewCore.setContentViewClient(mContentViewClient);
 
@@ -963,7 +961,6 @@ public class Tab implements NavigationClient {
         mContentViewCore = null;
         mWebContentsDelegate = null;
         mWebContentsObserver = null;
-        mVoiceSearchTabHelper = null;
 
         assert mNativeTabAndroid != 0;
         nativeDestroyWebContents(mNativeTabAndroid, deleteNativeWebContents);
--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1239,7 +1239,6 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
 #endif
     switches::kLowEndDeviceMode,
 #if defined(OS_ANDROID)
-    switches::kDisableGestureRequirementForMediaPlayback,
     switches::kDisableWebRTC,
     switches::kEnableSpeechRecognition,
     switches::kMediaDrmEnableNonCompositing,
