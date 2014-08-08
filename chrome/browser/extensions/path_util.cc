ebb1ba492c51a28f4753c1e7d0fff43e4f39c60b
gpdavis.chromium@gmail.com <gpdavis.chromium@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-08 01:03:51 +0000

    Check for empty paths in path_util
    BUG=400530
    
    Review URL: https://codereview.chromium.org/439873002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288187 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/path_util.cc
+++ chrome/browser/extensions/path_util.cc
@@ -20,8 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace extensions {
 namespace path_util {
 
-#if defined(OS_MACOSX)
 namespace {
+#if defined(OS_MACOSX)
 
 // Retrieves the localized display name for the base name of the given path.
 // If the path is not localized, this will just return the base name.
@@ -40,11 +40,22 @@ std::string GetDisplayBaseName(const base::FilePath& path) {
   return result;
 }
 
+#endif  // defined(OS_MACOSX)
+
+const base::FilePath::CharType kHomeShortcut[] = FILE_PATH_LITERAL("~");
+
 }  // namespace
 
 base::FilePath PrettifyPath(const base::FilePath& source_path) {
   base::FilePath home_path;
-  PathService::Get(base::DIR_HOME, &home_path);
+  if (source_path.empty() || !PathService::Get(base::DIR_HOME, &home_path))
+    return source_path;
+
+  base::FilePath display_path = base::FilePath(kHomeShortcut);
+  if (source_path == home_path)
+    return display_path;
+
+#if defined(OS_MACOSX)
   DCHECK(source_path.IsAbsolute());
 
   // Break down the incoming path into components, and grab the display name
@@ -54,13 +65,13 @@ base::FilePath PrettifyPath(const base::FilePath& source_path) {
   // show up as the HDD name.
   std::vector<base::FilePath::StringType> components;
   source_path.GetComponents(&components);
-  base::FilePath display_path = base::FilePath(components[0]);
+  display_path = base::FilePath(components[0]);
   base::FilePath actual_path = display_path;
   for (std::vector<base::FilePath::StringType>::iterator i =
            components.begin() + 1; i != components.end(); ++i) {
     actual_path = actual_path.Append(*i);
     if (actual_path == home_path) {
-      display_path = base::FilePath("~");
+      display_path = base::FilePath(kHomeShortcut);
       home_path = base::FilePath();
       continue;
     }
@@ -69,17 +80,12 @@ base::FilePath PrettifyPath(const base::FilePath& source_path) {
   }
   DCHECK_EQ(actual_path.value(), source_path.value());
   return display_path;
-}
-#else   // defined(OS_MACOSX)
-base::FilePath PrettifyPath(const base::FilePath& source_path) {
-  base::FilePath home_path;
-  base::FilePath display_path = base::FilePath::FromUTF8Unsafe("~");
-  if (PathService::Get(base::DIR_HOME, &home_path) &&
-      home_path.AppendRelativePath(source_path, &display_path))
+#else  // defined(OS_MACOSX)
+  if (home_path.AppendRelativePath(source_path, &display_path))
     return display_path;
   return source_path;
-}
 #endif  // defined(OS_MACOSX)
+}
 
 }  // namespace path_util
 }  // namespace extensions
