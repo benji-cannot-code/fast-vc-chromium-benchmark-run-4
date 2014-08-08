7bfcd91f73fbb52cd8d7200572b3e10f720e1459
luken@chromium.org <luken@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-08 20:40:11 +0000

    Revert 288418 "Extend contextMenus API to support browser/page a..."
    > Extend contextMenus API to support browser/page actions
    > 
    > Screenshot:
    > http://i.imgur.com/RILqQqe.png
    > 
    > BUG=234425
    > 
    > Review URL: https://codereview.chromium.org/359493005
    
    TBR=gpdavis.chromium@gmail.com
    
    Review URL: https://codereview.chromium.org/455023002
    
    Cr-Commit-Position: refs/heads/master@{#288422}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288422 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/menu_manager.h
+++ chrome/browser/extensions/menu_manager.h
@@ -95,9 +95,7 @@ class MenuItem {
     VIDEO = 64,
     AUDIO = 128,
     FRAME = 256,
-    LAUNCHER = 512,
-    BROWSER_ACTION = 1024,
-    PAGE_ACTION = 2048
+    LAUNCHER = 512
   };
 
   // An item can be only one of these types.
@@ -170,7 +168,7 @@ class MenuItem {
   const Id& id() const { return id_; }
   Id* parent_id() const { return parent_id_.get(); }
   int child_count() const { return children_.size(); }
-  const ContextList& contexts() const { return contexts_; }
+  ContextList contexts() const { return contexts_; }
   Type type() const { return type_; }
   bool checked() const { return checked_; }
   bool enabled() const { return enabled_; }
