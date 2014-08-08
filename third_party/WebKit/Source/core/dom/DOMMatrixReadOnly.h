73fec3cb7d22a7462a09cdc0e71de483ab173246
yukishiino@chromium.org <yukishiino@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-08 08:38:59 +0000

    bindings: Makes DOMMatrixReadOnly inherit from ScriptWrappableBase.
    BUG=235436
    
    Review URL: https://codereview.chromium.org/456623002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179803 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/DOMMatrixReadOnly.h
+++ third_party/WebKit/Source/core/dom/DOMMatrixReadOnly.h
@@ -6,11 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef DOMMatrixReadOnly_h
 #define DOMMatrixReadOnly_h
 
+#include "bindings/core/v8/ScriptWrappable.h"
 #include "platform/heap/Handle.h"
 
 namespace blink {
 
-class DOMMatrixReadOnly : public GarbageCollected<DOMMatrixReadOnly> {
+class DOMMatrixReadOnly : public GarbageCollected<DOMMatrixReadOnly>, public ScriptWrappableBase {
 public:
     double a() const { return m11(); }
     double b() const { return m12(); }
