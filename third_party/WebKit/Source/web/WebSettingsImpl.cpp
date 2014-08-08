ae966ca7374857b6ba2185e58bb950d57ab80684
mkosiba@chromium.org <mkosiba@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-08 11:45:21 +0000

    Add a force-layout-height-to-zero layout mode to WebViewImpl
    This makes it possible for the android_webview to implement
    'wrap_content' mode for the view's height without completely ignoring
    the viewport meta tag (like it's doing now).
    
    TBR=abarth@chromium.org, aelias@chromium.org, mnaganov@chromium.org
    BUG=392460
    
    Review URL: https://codereview.chromium.org/453893002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179823 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebSettingsImpl.cpp
+++ third_party/WebKit/Source/web/WebSettingsImpl.cpp
@@ -75,6 +75,11 @@ void WebSettingsImpl::setFixedFontFamily(const WebString& font, UScriptCode scri
         m_settings->notifyGenericFontFamilyChange();
 }
 
+void WebSettingsImpl::setForceZeroLayoutHeight(bool enabled)
+{
+    m_settings->setForceZeroLayoutHeight(enabled);
+}
+
 void WebSettingsImpl::setFullscreenSupported(bool enabled)
 {
     m_settings->setFullscreenSupported(enabled);
