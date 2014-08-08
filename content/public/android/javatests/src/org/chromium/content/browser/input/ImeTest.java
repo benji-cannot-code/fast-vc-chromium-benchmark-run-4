1aeaf8a56b48943d211973bc8c1e5217cf07a532
bcwhite@chromium.org <bcwhite@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-08 00:49:55 +0000

    Fix keycode problems for modified keys.
    There is a problem with keycode generation for non-trivial characters
    (i.e. those that have modifiers) because the event array generated is
    not simply a KeyDown/KeyUp combination; each modifier also gets a pair
    of events.
    
    Instead, scan through the events and send the first KeyDown that isn't
    a modifier key.  Also, don't send key events if nothing is changing
    (as happens with the final "commit").
    
    SendSythenticKeyEvent needed to be extended to accept modifier flags.
    
    BUG=118639
    
    Review URL: https://codereview.chromium.org/426063002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288179 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/javatests/src/org/chromium/content/browser/input/ImeTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/input/ImeTest.java
@@ -263,40 +263,51 @@ public class ImeTest extends ContentShellTestBase {
         waitAndVerifyEditableCallback(mConnection.mImeUpdateQueue, 6, "h\nllo ", 2, 2, -1, -1);
     }
 
+    private int getTypedKeycodeGuess(String before, String after) {
+        KeyEvent ev = ImeAdapter.getTypedKeyEventGuess(before, after);
+        if (ev == null) return -1;
+        return ev.getKeyCode();
+    }
+
     @SmallTest
     @Feature({"TextInput", "Main"})
-    public void testGuessedKeycodeFromTyping() throws Throwable {
-        assertEquals(0, ImeAdapter.getTypedKeycodeGuess(null, ""));
-        assertEquals(KeyEvent.KEYCODE_X, ImeAdapter.getTypedKeycodeGuess(null, "x"));
-        assertEquals(0, ImeAdapter.getTypedKeycodeGuess(null, "xyz"));
+    public void testGuessedKeyCodeFromTyping() throws Throwable {
+        assertEquals(-1, getTypedKeycodeGuess(null, ""));
+        assertEquals(KeyEvent.KEYCODE_X, getTypedKeycodeGuess(null, "x"));
+        assertEquals(-1, getTypedKeycodeGuess(null, "xyz"));
 
-        assertEquals(0, ImeAdapter.getTypedKeycodeGuess("abc", "abc"));
-        assertEquals(KeyEvent.KEYCODE_DEL, ImeAdapter.getTypedKeycodeGuess("abc", ""));
+        assertEquals(-1, getTypedKeycodeGuess("abc", "abc"));
+        assertEquals(KeyEvent.KEYCODE_DEL, getTypedKeycodeGuess("abc", ""));
 
-        assertEquals(KeyEvent.KEYCODE_H, ImeAdapter.getTypedKeycodeGuess("", "h"));
-        assertEquals(KeyEvent.KEYCODE_DEL, ImeAdapter.getTypedKeycodeGuess("h", ""));
-        assertEquals(KeyEvent.KEYCODE_E, ImeAdapter.getTypedKeycodeGuess("h", "he"));
-        assertEquals(KeyEvent.KEYCODE_L, ImeAdapter.getTypedKeycodeGuess("he", "hel"));
-        assertEquals(KeyEvent.KEYCODE_O, ImeAdapter.getTypedKeycodeGuess("hel", "helo"));
-        assertEquals(KeyEvent.KEYCODE_DEL, ImeAdapter.getTypedKeycodeGuess("helo", "hel"));
-        assertEquals(KeyEvent.KEYCODE_L, ImeAdapter.getTypedKeycodeGuess("hel", "hell"));
-        assertEquals(KeyEvent.KEYCODE_L, ImeAdapter.getTypedKeycodeGuess("hell", "helll"));
-        assertEquals(KeyEvent.KEYCODE_DEL, ImeAdapter.getTypedKeycodeGuess("helll", "hell"));
-        assertEquals(KeyEvent.KEYCODE_O, ImeAdapter.getTypedKeycodeGuess("hell", "hello"));
+        assertEquals(KeyEvent.KEYCODE_H, getTypedKeycodeGuess("", "h"));
+        assertEquals(KeyEvent.KEYCODE_DEL, getTypedKeycodeGuess("h", ""));
+        assertEquals(KeyEvent.KEYCODE_E, getTypedKeycodeGuess("h", "he"));
+        assertEquals(KeyEvent.KEYCODE_L, getTypedKeycodeGuess("he", "hel"));
+        assertEquals(KeyEvent.KEYCODE_O, getTypedKeycodeGuess("hel", "helo"));
+        assertEquals(KeyEvent.KEYCODE_DEL, getTypedKeycodeGuess("helo", "hel"));
+        assertEquals(KeyEvent.KEYCODE_L, getTypedKeycodeGuess("hel", "hell"));
+        assertEquals(KeyEvent.KEYCODE_L, getTypedKeycodeGuess("hell", "helll"));
+        assertEquals(KeyEvent.KEYCODE_DEL, getTypedKeycodeGuess("helll", "hell"));
+        assertEquals(KeyEvent.KEYCODE_O, getTypedKeycodeGuess("hell", "hello"));
 
-        assertEquals(KeyEvent.KEYCODE_X, ImeAdapter.getTypedKeycodeGuess("xxx", "xxxx"));
-        assertEquals(KeyEvent.KEYCODE_X, ImeAdapter.getTypedKeycodeGuess("xxx", "xxxxx"));
-        assertEquals(KeyEvent.KEYCODE_DEL, ImeAdapter.getTypedKeycodeGuess("xxx", "xx"));
-        assertEquals(KeyEvent.KEYCODE_DEL, ImeAdapter.getTypedKeycodeGuess("xxx", "x"));
+        assertEquals(KeyEvent.KEYCODE_X, getTypedKeycodeGuess("xxx", "xxxx"));
+        assertEquals(KeyEvent.KEYCODE_X, getTypedKeycodeGuess("xxx", "xxxxx"));
+        assertEquals(KeyEvent.KEYCODE_DEL, getTypedKeycodeGuess("xxx", "xx"));
+        assertEquals(KeyEvent.KEYCODE_DEL, getTypedKeycodeGuess("xxx", "x"));
 
-        assertEquals(KeyEvent.KEYCODE_Y, ImeAdapter.getTypedKeycodeGuess("xxx", "xxxy"));
-        assertEquals(KeyEvent.KEYCODE_Y, ImeAdapter.getTypedKeycodeGuess("xxx", "xxxxy"));
-        assertEquals(0, ImeAdapter.getTypedKeycodeGuess("xxx", "xy"));
-        assertEquals(0, ImeAdapter.getTypedKeycodeGuess("xxx", "y"));
+        assertEquals(KeyEvent.KEYCODE_Y, getTypedKeycodeGuess("xxx", "xxxy"));
+        assertEquals(KeyEvent.KEYCODE_Y, getTypedKeycodeGuess("xxx", "xxxxy"));
+        assertEquals(-1, getTypedKeycodeGuess("xxx", "xy"));
+        assertEquals(-1, getTypedKeycodeGuess("xxx", "y"));
 
-        assertEquals(0, ImeAdapter.getTypedKeycodeGuess("foo", "bar"));
-        assertEquals(0, ImeAdapter.getTypedKeycodeGuess("foo", "bars"));
-        assertEquals(0, ImeAdapter.getTypedKeycodeGuess("foo", "ba"));
+        assertEquals(-1, getTypedKeycodeGuess("foo", "bar"));
+        assertEquals(-1, getTypedKeycodeGuess("foo", "bars"));
+        assertEquals(-1, getTypedKeycodeGuess("foo", "ba"));
+
+        // Some characters also require modifiers so we have to check the full event.
+        KeyEvent ev = ImeAdapter.getTypedKeyEventGuess(null, "!");
+        assertEquals(KeyEvent.KEYCODE_1, ev.getKeyCode());
+        assertTrue(ev.isShiftPressed());
     }
 
     @SmallTest
@@ -316,7 +327,6 @@ public class ImeTest extends ContentShellTestBase {
         expectUpdateStateCall(mConnection);
         setComposingText(mConnection, "h", 1);
         assertEquals(KeyEvent.KEYCODE_H, mImeAdapter.mLastSyntheticKeyCode);
-        assertEquals("h", mConnection.getTextBeforeCursor(9, 0));
         assertUpdateStateCall(mConnection, 1000);
         assertEquals("h", mConnection.getTextBeforeCursor(9, 0));
 
@@ -324,7 +334,6 @@ public class ImeTest extends ContentShellTestBase {
         expectUpdateStateCall(mConnection);
         setComposingText(mConnection, "ho", 1);
         assertEquals(KeyEvent.KEYCODE_O, mImeAdapter.mLastSyntheticKeyCode);
-        assertEquals("ho", mConnection.getTextBeforeCursor(9, 0));
         assertUpdateStateCall(mConnection, 1000);
         assertEquals("ho", mConnection.getTextBeforeCursor(9, 0));
 
@@ -332,10 +341,10 @@ public class ImeTest extends ContentShellTestBase {
         expectUpdateStateCall(mConnection);
         setComposingText(mConnection, "h", 1);
         assertEquals(KeyEvent.KEYCODE_DEL, mImeAdapter.mLastSyntheticKeyCode);
-        assertEquals("h", mConnection.getTextBeforeCursor(9, 0));
         assertUpdateStateCall(mConnection, 1000);
-        assertEquals("h", mConnection.getTextBeforeCursor(9, 0));
         setComposingRegion(mConnection, 0, 1);  // DEL calls cancelComposition() then restarts
+        setComposingText(mConnection, "h", 1);
+        assertEquals("h", mConnection.getTextBeforeCursor(9, 0));
 
         // I
         setComposingText(mConnection, "hi", 1);
@@ -344,7 +353,7 @@ public class ImeTest extends ContentShellTestBase {
 
         // SPACE
         commitText(mConnection, "hi", 1);
-        assertEquals(0, mImeAdapter.mLastSyntheticKeyCode);
+        assertEquals(-1, mImeAdapter.mLastSyntheticKeyCode);
         commitText(mConnection, " ", 1);
         assertEquals(KeyEvent.KEYCODE_SPACE, mImeAdapter.mLastSyntheticKeyCode);
         assertEquals("hi ", mConnection.getTextBeforeCursor(9, 0));
@@ -366,7 +375,7 @@ public class ImeTest extends ContentShellTestBase {
         assertEquals("", mConnection.getTextBeforeCursor(9, 0));
 
         // DEL (on empty input)
-        deleteSurroundingText(mConnection, 1, 0);  // BS on empty still sends 1,0
+        deleteSurroundingText(mConnection, 1, 0);  // DEL on empty still sends 1,0
         assertEquals(KeyEvent.KEYCODE_DEL, mImeAdapter.mLastSyntheticKeyCode);
         assertEquals("", mConnection.getTextBeforeCursor(9, 0));
     }
@@ -448,7 +457,7 @@ public class ImeTest extends ContentShellTestBase {
         assertEquals("", mConnection.getTextBeforeCursor(9, 0));
 
         // DEL (on empty input)
-        deleteSurroundingText(mConnection, 1, 0);  // BS on empty still sends 1,0
+        deleteSurroundingText(mConnection, 1, 0);  // DEL on empty still sends 1,0
         assertEquals(KeyEvent.KEYCODE_DEL, mImeAdapter.mLastSyntheticKeyCode);
         assertEquals("", mConnection.getTextBeforeCursor(9, 0));
     }
