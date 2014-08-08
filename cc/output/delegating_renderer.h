76290df3083527d36bee9f25058acf381f23caae
dneto@chromium.org <dneto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-08 22:53:11 +0000

    LayerTreeHostImpl knows if it has an output surface. Surface loss is only detected via the callback.
    We can tolerate multiple calls to InitializeRenderer without
    needing the lost-surface callback in between.
    
    Remove Renderer::IsContextLost, but keep GLRenderer::IsContextLost.
    
    Remove ThreadProxy::CheckOutputSurfaceStatusOnImplThread
    
    BUG=392891
    
    Review URL: https://codereview.chromium.org/446973002
    
    Cr-Commit-Position: refs/heads/master@{#288457}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288457 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/output/delegating_renderer.h
+++ cc/output/delegating_renderer.h
@@ -38,8 +38,6 @@ class CC_EXPORT DelegatingRenderer : public Renderer {
   virtual void SwapBuffers(const CompositorFrameMetadata& metadata) OVERRIDE;
   virtual void ReceiveSwapBuffersAck(const CompositorFrameAck&) OVERRIDE;
 
-  virtual bool IsContextLost() OVERRIDE;
-
  private:
   DelegatingRenderer(RendererClient* client,
                      const LayerTreeSettings* settings,
--- cc/output/renderer.h
+++ cc/output/renderer.h
@@ -74,8 +74,6 @@ class CC_EXPORT Renderer {
   virtual void SwapBuffers(const CompositorFrameMetadata& metadata) = 0;
   virtual void ReceiveSwapBuffersAck(const CompositorFrameAck& ack) {}
 
-  virtual bool IsContextLost();
-
   bool visible() const { return visible_; }
   void SetVisible(bool visible);
 
