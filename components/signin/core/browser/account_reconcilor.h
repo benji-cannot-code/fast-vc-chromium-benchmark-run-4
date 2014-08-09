f48e53cf5a0364527579d0d98de7f7f886edd823
mlerman@chromium.org <mlerman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-09 02:26:01 +0000

    Show GAIA/cookie accounts on the signin-internals page.
    BUG=326577
    
    Review URL: https://codereview.chromium.org/421583002
    
    Cr-Commit-Position: refs/heads/master@{#288493}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288493 0039d316-1c4b-4281-b951-d872f2087c98

--- components/signin/core/browser/account_reconcilor.h
+++ components/signin/core/browser/account_reconcilor.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/scoped_vector.h"
 #include "components/keyed_service/core/keyed_service.h"
+#include "components/signin/core/browser/signin_client.h"
 #include "components/signin/core/browser/signin_manager.h"
 #include "google_apis/gaia/gaia_auth_consumer.h"
 #include "google_apis/gaia/google_service_auth_error.h"
@@ -259,6 +260,9 @@ class AccountReconcilor : public KeyedService,
   std::vector<std::pair<std::string, int> > add_to_chrome_;
 
   std::deque<GetAccountsFromCookieCallback> get_gaia_accounts_callbacks_;
+
+  scoped_ptr<SigninClient::CookieChangedCallbackList::Subscription>
+      cookie_changed_subscription_;
 
   DISALLOW_COPY_AND_ASSIGN(AccountReconcilor);
 };
