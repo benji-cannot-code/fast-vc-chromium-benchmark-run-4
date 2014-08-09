61e5c7fde329c4e661d81477db12cf29796d130a
ccameron@chromium.org <ccameron@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-09 10:29:47 +0000

    Add temporary instrumentation to debug ui::Compositor crashes
    Multiple crashes are being implicating problems with the ui::Compositor
    observer list. Inspection has not found any issues, so add explicit
    CHECKs to ensure that
    - observers be unregistered before their destruction
    - no NULL observers be added
    
    This instrumentation is to be removed once one or two Canary builds
    have identified or failed to identify the source of the problems.
    
    BUG=401630
    
    Review URL: https://codereview.chromium.org/449233002
    
    Cr-Commit-Position: refs/heads/master@{#288553}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288553 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/compositor/compositor.gyp
+++ ui/compositor/compositor.gyp
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'compositor.h',
         'compositor_animation_observer.h',
         'compositor_export.h',
+        'compositor_observer.cc',
         'compositor_observer.h',
         'compositor_vsync_manager.cc',
         'compositor_vsync_manager.h',
---
+++ ui/compositor/compositor_observer.cc
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/compositor/compositor_observer.h"
+
+#include "base/logging.h"
+
+namespace ui {
+
+#if defined(OS_MACOSX)
+// Debugging instrumentation for crbug.com/401630.
+// TODO(ccameron): remove this.
+CompositorObserver::CompositorObserver() : observing_count_(0) {}
+#else
+CompositorObserver::CompositorObserver() {}
+#endif
+
+CompositorObserver::~CompositorObserver() {
+#if defined(OS_MACOSX)
+  // Debugging instrumentation for crbug.com/401630.
+  // TODO(ccameron): remove this.
+  CHECK_EQ(observing_count_, 0);
+#endif
+}
+
+}
+
