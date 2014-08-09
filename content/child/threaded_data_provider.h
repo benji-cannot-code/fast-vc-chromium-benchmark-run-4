58df0ac1d0fef103d54c334c789e2bfdc8250a44
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-09 05:37:11 +0000

    Cleanup: Remove obsolete references to webkit_glue namespace.
    BUG=338338
    TEST=None
    R=groby@chromium.org,avi@chromium.org,sky@chromium.org
    
    Review URL: https://codereview.chromium.org/450093002
    
    Cr-Commit-Position: refs/heads/master@{#288513}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288513 0039d316-1c4b-4281-b951-d872f2087c98

--- content/child/threaded_data_provider.h
+++ content/child/threaded_data_provider.h
@@ -23,12 +23,7 @@ namespace IPC {
 class SyncChannel;
 }
 
-namespace webkit_glue {
-class WebThreadImpl;
-}
-
 namespace content {
-class ResourceDispatcher;
 class WebThreadImpl;
 
 class ThreadedDataProvider {
