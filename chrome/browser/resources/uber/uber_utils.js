384d37648e95a92690a633e50db11af2c4441d4e
vitalyp@chromium.org <vitalyp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-09 05:22:29 +0000

    Typecheck JS files for chrome://help before doing import transition
    BUG=393873
    R=dbeam@chromium.org
    
    Review URL: https://codereview.chromium.org/418663002
    
    Cr-Commit-Position: refs/heads/master@{#288504}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288504 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/uber/uber_utils.js
+++ chrome/browser/resources/uber/uber_utils.js
@@ -52,17 +52,19 @@ cr.define('uber', function() {
     }
 
     invokeMethodOnParent('adjustToScroll', scrollLeft);
-  };
+  }
 
   /**
    * Handles 'message' events on window.
    * @param {Event} e The message event.
    */
   function handleWindowMessage(e) {
+    e = /** @type {!MessageEvent.<!{method: string, params: *}>} */(e);
     if (e.data.method === 'frameSelected')
       handleFrameSelected();
     else if (e.data.method === 'mouseWheel')
-      handleMouseWheel(e.data.params);
+      handleMouseWheel(
+          /** @type {{deltaX: number, deltaY: number}} */(e.data.params));
     else if (e.data.method === 'popState')
       handlePopState(e.data.params.state, e.data.params.path);
   }
@@ -83,7 +85,8 @@ cr.define('uber', function() {
    * It differs for every platform and even initWebKitWheelEvent takes a
    * pixel amount instead of a wheel delta. So we just choose something
    * reasonable and hope no one notices the difference.
-   * @param {Object} params A structure that holds wheel deltas in X and Y.
+   * @param {{deltaX: number, deltaY: number}} params A structure that holds
+   *     wheel deltas in X and Y.
    */
   function handleMouseWheel(params) {
     window.scrollBy(-params.deltaX * 49 / 120, -params.deltaY * 49 / 120);
@@ -92,9 +95,12 @@ cr.define('uber', function() {
   /**
    * Called when the parent window restores some state saved by uber.pushState
    * or uber.replaceState. Simulates a popstate event.
+   * @param {PopStateEvent} state A state object for replaceState and pushState.
+   * @param {string} path The path the page navigated to.
+   * @suppress {checkTypes}
    */
   function handlePopState(state, path) {
-    history.replaceState(state, '', path);
+    window.history.replaceState(state, '', path);
     window.dispatchEvent(new PopStateEvent('popstate', {state: state}));
   }
 
@@ -109,8 +115,8 @@ cr.define('uber', function() {
    * Invokes a method on the parent window (UberPage). This is a convenience
    * method for API calls into the uber page.
    * @param {string} method The name of the method to invoke.
-   * @param {Object=} opt_params Optional property bag of parameters to pass to
-   *     the invoked method.
+   * @param {(Object|number)=} opt_params Optional property bag of parameters
+   *     to pass to the invoked method.
    * @private
    */
   function invokeMethodOnParent(method, opt_params) {
@@ -123,8 +129,8 @@ cr.define('uber', function() {
   /**
    * Invokes a method on the target window.
    * @param {string} method The name of the method to invoke.
-   * @param {Object=} opt_params Optional property bag of parameters to pass to
-   *     the invoked method.
+   * @param {(Object|number)=} opt_params Optional property bag of parameters
+   *     to pass to the invoked method.
    * @param {string=} opt_url The origin of the target window.
    * @private
    */
@@ -138,7 +144,6 @@ cr.define('uber', function() {
    * forward the information to the parent for it to manage history for us. This
    * is a replacement of history.replaceState and history.pushState.
    * @param {Object} state A state object for replaceState and pushState.
-   * @param {string} title The title of the page to replace.
    * @param {string} path The path the page navigated to.
    * @param {boolean} replace If true, navigate with replacement.
    * @private
--- ui/webui/resources/js/cr/ui/focus_manager.js
+++ ui/webui/resources/js/cr/ui/focus_manager.js
@@ -24,13 +24,13 @@ cr.define('cr.ui', function() {
     /**
      * Determines whether the |child| is a descendant of |parent| in the page's
      * DOM.
-     * @param {Element} parent The parent element to test.
-     * @param {Element} child The child element to test.
+     * @param {Node} parent The parent element to test.
+     * @param {Node} child The child element to test.
      * @return {boolean} True if |child| is a descendant of |parent|.
      * @private
      */
     isDescendantOf_: function(parent, child) {
-      return parent && !(parent === child) && parent.contains(child);
+      return !!parent && !(parent === child) && parent.contains(child);
     },
 
     /**
@@ -44,7 +44,7 @@ cr.define('cr.ui', function() {
 
     /**
      * Returns the elements on the page capable of receiving focus.
-     * @return {Array.Element} The focusable elements.
+     * @return {Array.<Element>} The focusable elements.
      */
     getFocusableElements_: function() {
       var focusableDiv = this.getFocusParent();
@@ -53,7 +53,9 @@ cr.define('cr.ui', function() {
       var treeWalker = document.createTreeWalker(
           focusableDiv,
           NodeFilter.SHOW_ELEMENT,
-          { acceptNode: function(node) {
+          /** @type {NodeFilter} */
+          ({
+            acceptNode: function(node) {
               var style = window.getComputedStyle(node);
               // Reject all hidden nodes. FILTER_REJECT also rejects these
               // nodes' children, so non-hidden elements that are descendants of
@@ -71,7 +73,7 @@ cr.define('cr.ui', function() {
               // Accept nodes that are non-hidden and focusable.
               return NodeFilter.FILTER_ACCEPT;
             }
-          },
+          }),
           false);
 
       var focusable = [];
@@ -88,7 +90,7 @@ cr.define('cr.ui', function() {
      * event. This differs from the native 'focus' event which is received by
      * an element outside the page first, followed by a 'focus' on an element
      * within the page after the FocusManager has intervened.
-     * @param {Element} element The element that has received focus.
+     * @param {EventTarget} element The element that has received focus.
      * @private
      */
     dispatchFocusEvent_: function(element) {
@@ -149,7 +151,8 @@ cr.define('cr.ui', function() {
     onDocumentFocus_: function(event) {
       // If the element being focused is a descendant of the currently visible
       // page, focus is valid.
-      if (this.isDescendantOf_(this.getFocusParent(), event.target)) {
+      var targetNode = /** @type {Node} */(event.target);
+      if (this.isDescendantOf_(this.getFocusParent(), targetNode)) {
         this.dispatchFocusEvent_(event.target);
         return;
       }
