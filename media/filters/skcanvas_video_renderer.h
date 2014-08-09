a3543fc27b52df40d650e2e35a324cbb1690312f
suderman@chromium.org <suderman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-09 05:52:38 +0000

    Added rotation operation to Paint for Canvas/WebGL copying. This corrects for wrong video orientation.
    BUG=47554
    
    Review URL: https://codereview.chromium.org/402133005
    
    Cr-Commit-Position: refs/heads/master@{#288518}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288518 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/skcanvas_video_renderer.h
+++ media/filters/skcanvas_video_renderer.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/time/time.h"
 #include "media/base/media_export.h"
+#include "media/base/video_rotation.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/gfx/rect.h"
 
@@ -31,7 +32,8 @@ class MEDIA_EXPORT SkCanvasVideoRenderer {
   void Paint(media::VideoFrame* video_frame,
              SkCanvas* canvas,
              const gfx::RectF& dest_rect,
-             uint8 alpha);
+             uint8 alpha,
+             VideoRotation video_rotation);
 
  private:
   // An RGB bitmap and corresponding timestamp of the previously converted
