089e866eaa8f2622e88768b3c201407bea4e4eb0
evy@chromium.org <evy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-09 10:56:11 +0000

    Fixed accessibility typo (mistyped accessiblity) in the code base.
    Review URL: https://codereview.chromium.org/459553002
    
    Cr-Commit-Position: refs/heads/master@{#288567}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288567 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/keyboard_overlay_accessibility_helper.js
+++ chrome/browser/resources/chromeos/keyboard_overlay_accessibility_helper.js
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// An object to implement keyboard overlay accessiblity.
+// An object to implement keyboard overlay accessibility.
 var KeyboardOverlayAccessibilityHelper = {
   // Returns true when ChromeVox is loaded and active, false otherwise.
   cvoxIsActive: function() {
