591e4939dd3fc6235aa1eb325a557c3141f5926d
nfullagar@chromium.org <nfullagar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-09 00:11:27 +0000

    Update SIMD version of life demo. - add benchmark option - add multi-thread support - convert from c to cpp (inc. ppapi) BUG=NONE TEST=this is a demo
    R=binji@chromium.org
    
    Review URL: https://codereview.chromium.org/451883002
    
    Cr-Commit-Position: refs/heads/master@{#288473}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288473 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/examples/demo/life_simd/example.dsc
+++ native_client_sdk/src/examples/demo/life_simd/example.dsc
@@ -3,17 +3,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   'TOOLS': ['pnacl'],
   'TARGETS': [
     {
-      'NAME' : 'life_simd',
+      'NAME' : 'life',
       'TYPE' : 'main',
       'SOURCES' : [
-        'life.c',
+        'life.cc',
       ],
       'DEPS': ['ppapi_simple', 'nacl_io'],
-      'LIBS': ['ppapi_simple', 'nacl_io', 'ppapi_cpp', 'ppapi', 'pthread']
+      'LIBS': ['ppapi_simple', 'nacl_io', 'sdk_util', 'ppapi_cpp', 'ppapi', 'pthread']
     }
+  ],
+  'DATA': [
+    'example.js'
   ],
   'DEST': 'examples/demo',
   'NAME': 'life_simd',
-  'TITLE': "Conway's Life (SIMD version)",
+  'TITLE': "Conway's Life",
   'GROUP': 'Demo'
 }
---
+++ native_client_sdk/src/examples/demo/life_simd/example.js
@@ -0,0 +1,52 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function moduleDidLoad() {
+}
+
+
+// Add event listeners after the NaCl module has loaded.  These listeners will
+// forward messages to the NaCl module via postMessage()
+function attachListeners() {
+  document.getElementById('benchmark').addEventListener('click',
+    function() {
+      common.naclModule.postMessage({'message' : 'run_benchmark'});
+      common.updateStatus('BENCHMARKING... (please wait)');
+    });
+  document.getElementById('simd').addEventListener('click',
+    function() {
+      var simd = document.getElementById('simd');
+      common.naclModule.postMessage({'message' : 'set_simd',
+          'value' : simd.checked});
+    });
+  document.getElementById('multithread').addEventListener('click',
+    function() {
+      var multithread = document.getElementById('multithread');
+      common.naclModule.postMessage({'message' : 'set_threading',
+          'value' : multithread.checked});
+    });
+  document.getElementById('large').addEventListener('click',
+    function() {
+      var large = document.getElementById('large');
+      var nacl = document.getElementById('nacl_module');
+      nacl.setAttribute('width', large.checked ? 1280 : 640);
+      nacl.setAttribute('height', large.checked ? 1024 : 640);
+    });
+}
+
+
+// Handle a message coming from the NaCl module.
+function handleMessage(message_event) {
+  if (message_event.data.message == 'benchmark_result') {
+    // benchmark result
+    var result = message_event.data.value;
+    console.log('Benchmark result:' + result);
+    result = (Math.round(result * 1000) / 1000).toFixed(3);
+    document.getElementById('result').textContent =
+      'Result: ' + result + ' seconds';
+    common.updateStatus('SUCCESS');
+  }
+}
+
