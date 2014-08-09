a59996daa0c76fde3ab54dcf1299f2b4af9d7588
rkc@chromium.org <rkc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-09 11:00:40 +0000

    Add audible support to the copresence API.
    This CL currently depends on 3 other CL's before it can land so it might be a
    good idea to hold off on the review till at least 2 of them have landed and
    the only files in the CL are truly the ones that this CL adds.
    
    R=ckehoe@chromium.org
    BUG=390393
    
    Review URL: https://codereview.chromium.org/453793002
    
    Cr-Commit-Position: refs/heads/master@{#288571}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288571 0039d316-1c4b-4281-b951-d872f2087c98

--- components/copresence/rpc/rpc_handler.h
+++ components/copresence/rpc/rpc_handler.h
@@ -109,6 +109,7 @@ class RpcHandler {
   // whispernet, setting the samples return callback to samples_callback.
   void AudioDirectiveListToWhispernetConnector(
       const std::string& token,
+      bool audible,
       const WhispernetClient::SamplesCallback& samples_callback);
 
   CopresenceClientDelegate* delegate_;  // Belongs to the caller.
