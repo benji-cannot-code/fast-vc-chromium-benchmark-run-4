6e94670d47300a668682ea72aefc7bc31ba8cb65
ehsan.akhgari@gmail.com <ehsan.akhgari@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-09 14:48:36 +0000

    base: Include intrin.h to get _ReturnAddress from clang
    This is similar to r228123, to fix another occurrence where
    _ReturnAddress is declared manually.
    
    BUG=395593
    R=cpu@chromium.org
    
    Review URL: https://codereview.chromium.org/403103006
    
    Cr-Commit-Position: refs/heads/master@{#288575}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288575 0039d316-1c4b-4281-b951-d872f2087c98

--- base/location.cc
+++ base/location.cc
@@ -6,10 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 
 #if defined(COMPILER_MSVC)
-// MSDN says to #include <intrin.h>, but that breaks the VS2005 build.
-extern "C" {
-  void* _ReturnAddress();
-}
+#include <intrin.h>
 #endif
 
 #include "base/location.h"
