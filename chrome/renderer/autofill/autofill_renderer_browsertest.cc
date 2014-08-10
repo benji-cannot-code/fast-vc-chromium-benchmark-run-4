a950fce54c8226de0779b4b9e973cd2be0e69760
sunil.ratnu@samsung.com <sunil.ratnu@samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-10 05:12:34 +0000

    Use single quotes in Layout tests as they don't need to be escaped
    Using single quotes instead of double quotes as single quotes don't need to be
    escaped. This helps in maintaining a consistency throughout Layout tests.
    
    R=isherman@chromium.org
    BUG=None
    
    Review URL: https://codereview.chromium.org/455653002
    
    Cr-Commit-Position: refs/heads/master@{#288618}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288618 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/autofill/autofill_renderer_browsertest.cc
+++ chrome/renderer/autofill/autofill_renderer_browsertest.cc
@@ -60,12 +60,12 @@ class AutofillRendererTest : public ChromeRenderViewTest {
 };
 
 TEST_F(AutofillRendererTest, SendForms) {
-  LoadHTML("<form method=\"POST\">"
-           "  <input type=\"text\" id=\"firstname\"/>"
-           "  <input type=\"text\" id=\"middlename\"/>"
-           "  <input type=\"text\" id=\"lastname\" autoComplete=\"off\"/>"
-           "  <input type=\"hidden\" id=\"email\"/>"
-           "  <select id=\"state\"/>"
+  LoadHTML("<form method='POST'>"
+           "  <input type='text' id='firstname'/>"
+           "  <input type='text' id='middlename'/>"
+           "  <input type='text' id='lastname' autoComplete='off'/>"
+           "  <input type='hidden' id='email'/>"
+           "  <select id='state'/>"
            "    <option>?</option>"
            "    <option>California</option>"
            "    <option>Texas</option>"
@@ -162,9 +162,9 @@ TEST_F(AutofillRendererTest, SendForms) {
 }
 
 TEST_F(AutofillRendererTest, EnsureNoFormSeenIfTooFewFields) {
-  LoadHTML("<form method=\"POST\">"
-           "  <input type=\"text\" id=\"firstname\"/>"
-           "  <input type=\"text\" id=\"middlename\"/>"
+  LoadHTML("<form method='POST'>"
+           "  <input type='text' id='firstname'/>"
+           "  <input type='text' id='middlename'/>"
            "</form>");
 
   // Verify that "FormsSeen" isn't sent, as there are too few fields.
@@ -178,10 +178,10 @@ TEST_F(AutofillRendererTest, EnsureNoFormSeenIfTooFewFields) {
 }
 
 TEST_F(AutofillRendererTest, ShowAutofillWarning) {
-  LoadHTML("<form method=\"POST\" autocomplete=\"Off\">"
-           "  <input id=\"firstname\" autocomplete=\"OFF\"/>"
-           "  <input id=\"middlename\"/>"
-           "  <input id=\"lastname\"/>"
+  LoadHTML("<form method='POST' autocomplete='Off'>"
+           "  <input id='firstname' autocomplete='OFF'/>"
+           "  <input id='middlename'/>"
+           "  <input id='lastname'/>"
            "</form>");
 
   // Verify that "QueryFormFieldAutofill" isn't sent prior to a user
@@ -230,7 +230,7 @@ TEST_F(AutofillRendererTest, DontCrashWhileAssociatingForms) {
            "<script id='script'>"
            "document.documentElement.appendChild(foo);"
            "newDoc = document.implementation.createDocument("
-           "    \"http://www.w3.org/1999/xhtml\", \"html\");"
+           "    'http://www.w3.org/1999/xhtml', 'html');"
            "foo.insertBefore(form, script);"
            "newDoc.adoptNode(foo);"
            "</script>");
@@ -254,7 +254,7 @@ class RequestAutocompleteRendererTest : public AutofillRendererTest {
 
     GURL url("data:text/html;charset=utf-8,"
              "<form><input autocomplete=cc-number></form>");
-    const char kDoubleIframeHtml[] = "<iframe id=subframe src=\"%s\"></iframe>"
+    const char kDoubleIframeHtml[] = "<iframe id=subframe src='%s'></iframe>"
                                      "<iframe id=sibling></iframe>";
     LoadHTML(base::StringPrintf(kDoubleIframeHtml, url.spec().c_str()).c_str());
 
