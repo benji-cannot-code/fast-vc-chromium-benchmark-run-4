b1d84988214ee04803473a97dbad7b620da50147
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-10 05:49:22 +0000

    Cleanup: content: Rename blink_platform_unittest.cc to blink_platform_impl_unittest.cc
    This is so it matches with blink_platform_impl.*, as it is its test
    driver for BlinkPlatformImpl.
    
    BUG=359243
    TEST=content_unittests
    R=avi@chromium.org
    
    Review URL: https://codereview.chromium.org/458743002
    
    Cr-Commit-Position: refs/heads/master@{#288619}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288619 0039d316-1c4b-4281-b951-d872f2087c98

--- content/child/blink_platform_impl_unittest.cc
+++ content/child/blink_platform_impl_unittest.cc
@@ -1,11 +1,12 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2013 The Chromium Authors. All rights reserved.
+// Copyright 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "content/child/blink_platform_impl.h"
+
 #include "base/run_loop.h"
 #include "base/time/time.h"
-#include "content/child/blink_platform_impl.h"
 #include "net/base/net_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/WebKit/public/platform/WebString.h"
--- content/content_tests.gypi
+++ content/content_tests.gypi
@@ -630,7 +630,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/web_contents/web_drag_source_mac_unittest.mm',
         'browser/webui/web_ui_data_source_unittest.cc',
         'browser/webui/web_ui_message_handler_unittest.cc',
-        'child/blink_platform_unittest.cc',
+        'child/blink_platform_impl_unittest.cc',
         'child/fileapi/webfilewriter_base_unittest.cc',
         'child/indexed_db/indexed_db_dispatcher_unittest.cc',
         'child/indexed_db/webidbcursor_impl_unittest.cc',
