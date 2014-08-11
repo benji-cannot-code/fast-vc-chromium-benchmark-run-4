f617b0bc711629db9f17ce913488036eb014b6a1
yoav@yoav.ws <yoav@yoav.ws@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-11 11:27:08 +0000

    ViewportChangeListener deregister and reregister on DOM changes.
    BUG=402263,402218
    
    Review URL: https://codereview.chromium.org/455333002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179937 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLImageElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLImageElement.cpp
@@ -347,6 +347,8 @@ Node::InsertionNotificationRequest HTMLImageElement::insertedInto(ContainerNode*
 {
     if (!m_formWasSetByParser || NodeTraversal::highestAncestorOrSelf(*insertionPoint) != NodeTraversal::highestAncestorOrSelf(*m_form.get()))
         resetFormOwner();
+    if (m_listener)
+        document().mediaQueryMatcher().addViewportListener(m_listener.get());
 
     bool imageWasModified = false;
     if (RuntimeEnabledFeatures::pictureEnabled()) {
@@ -369,6 +371,8 @@ void HTMLImageElement::removedFrom(ContainerNode* insertionPoint)
 {
     if (!m_form || NodeTraversal::highestAncestorOrSelf(*m_form.get()) != NodeTraversal::highestAncestorOrSelf(*this))
         resetFormOwner();
+    if (m_listener)
+        document().mediaQueryMatcher().removeViewportListener(m_listener.get());
     HTMLElement::removedFrom(insertionPoint);
 }
 
