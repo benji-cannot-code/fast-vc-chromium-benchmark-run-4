9e20e6179eb7bcbe1eaa4ca6fc8361f82fc081c0
amistry@chromium.org <amistry@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-11 09:16:52 +0000

    Rename the hotword flag to better reflect meaning, and move into Chrome.
    The old name didn't reflect the intended meaning, to enable new
    built-in "Ok Google" detection functionality. Also, we don't want to
    restrict this functionality to ChromeOS.
    
    BUG=397019
    
    Review URL: https://codereview.chromium.org/442183002
    
    Cr-Commit-Position: refs/heads/master@{#288677}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288677 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/hotword_private/hotword_private_api.cc
+++ chrome/browser/extensions/api/hotword_private/hotword_private_api.cc
@@ -12,10 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/search/hotword_client.h"
 #include "chrome/browser/search/hotword_service.h"
 #include "chrome/browser/search/hotword_service_factory.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
-#if defined(OS_CHROMEOS)
-#include "chromeos/chromeos_switches.h"
-#endif
 #include "extensions/browser/event_router.h"
 
 namespace extensions {
@@ -111,13 +109,9 @@ bool HotwordPrivateGetStatusFunction::RunSync() {
   result.enabled_set = prefs->HasPrefPath(prefs::kHotwordSearchEnabled);
   result.enabled = prefs->GetBoolean(prefs::kHotwordSearchEnabled);
   result.audio_logging_enabled = false;
-#if defined(OS_CHROMEOS)
   CommandLine* command_line = CommandLine::ForCurrentProcess();
   result.experimental_hotword_enabled = command_line->HasSwitch(
-      chromeos::switches::kEnableOkGoogleVoiceSearch);
-#else
-  result.experimental_hotword_enabled = false;
-#endif
+      switches::kEnableExperimentalHotwording);
   if (hotword_service)
     result.audio_logging_enabled = hotword_service->IsOptedIntoAudioLogging();
 
