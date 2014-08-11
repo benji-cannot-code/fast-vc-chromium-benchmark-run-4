fe76a34dc7f4e612a1896ad7d9742c1785e06e3f
jdduke@chromium.org <jdduke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-11 19:40:52 +0000

    [Android] Temporarily avoid asserting overscroll resource validity
    The overscroll effect has changed with Android L. Consequently, there are no
    guarantees that the previous overscroll resources identifiers will yield valid
    resources when running Android L.  This will soon be resolved with the updated
    overscroll effect, but until such time avoid related bitmap validity asserts.
    
    BUG=389744,326326
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/463593002
    
    Cr-Commit-Position: refs/heads/master@{#288766}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288766 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/android/system_ui_resource_manager_impl.cc
+++ content/browser/android/system_ui_resource_manager_impl.cc
@@ -32,8 +32,9 @@ class SystemUIResourceManagerImpl::Entry
   }
 
   void SetBitmap(const SkBitmap& bitmap) {
+    // TODO(jdduke): Verify validity of |bitmap| after resolving resource
+    // loading issues on Android L, crbug.com/389744.
     DCHECK(bitmap_.empty());
-    DCHECK(!bitmap.empty());
     DCHECK(!id_);
     bitmap_ = bitmap;
   }
