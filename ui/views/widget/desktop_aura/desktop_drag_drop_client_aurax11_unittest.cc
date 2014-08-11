2212bca54ff7b2f9f6cbddee6829e4b909a78c70
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-11 16:12:37 +0000

    Disable all the tests that are flaking more than 5% on Linux builders.
    Data from http://chromium-build-logs.appspot.com/flakiness_dashboard?master=chromium.linux.
    
    BUG=386891,396458,396455,396462,396464,396492,396491,396488,396484,396482,396480,180759,396477,126092,396473,396472,396468,396466
    R=viettrungluu@chromium.org
    TBR=ben
    
    Review URL: https://codereview.chromium.org/417453002
    
    Cr-Commit-Position: refs/heads/master@{#288722}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288722 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/desktop_aura/desktop_drag_drop_client_aurax11_unittest.cc
+++ ui/views/widget/desktop_aura/desktop_drag_drop_client_aurax11_unittest.cc
@@ -446,7 +446,8 @@ void BasicStep3(TestDragDropClient* client, XID toplevel) {
 
 }  // namespace
 
-TEST_F(DesktopDragDropClientAuraX11Test, Basic) {
+// http://crbug.com/396468
+TEST_F(DesktopDragDropClientAuraX11Test, DISABLED_Basic) {
   XID toplevel = 1;
 
   base::MessageLoop::current()->PostTask(FROM_HERE,
