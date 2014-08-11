97da6070f1d0a779d230bfada4a4a942e7718c55
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-11 19:11:47 +0000

    Disable SyncSchedulerTest.SuccessfulRetry since it's timing out flakly.
    BUG=402212
    TBR=rlarocque@chromium.org
    
    Review URL: https://codereview.chromium.org/465613002
    
    Cr-Commit-Position: refs/heads/master@{#288753}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288753 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/engine/sync_scheduler_unittest.cc
+++ sync/engine/sync_scheduler_unittest.cc
@@ -1312,7 +1312,8 @@ TEST_F(SyncSchedulerTest, PollFromCanaryAfterAuthError) {
   StopSyncScheduler();
 }
 
-TEST_F(SyncSchedulerTest, SuccessfulRetry) {
+// Times out: http://crbug.com/402212
+TEST_F(SyncSchedulerTest, DISABLED_SuccessfulRetry) {
   StartSyncScheduler(SyncScheduler::NORMAL_MODE);
 
   SyncShareTimes times;
