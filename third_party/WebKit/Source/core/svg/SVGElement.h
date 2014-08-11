52dcd16f1f0d3665b5c30fcb67efc99dea9e291e
pdr@chromium.org <pdr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-11 22:08:09 +0000

    Revert of [SVG2] Make transform, gradientTransform and patternTransform presentation attributes.
    Reason for revert:
    Caused 10-50% performance regressions on svg, see http://crbug.com/402133.
    
    Original issue's description:
    > [SVG2] Make transform, gradientTransform and patternTransform presentation attributes.
    >
    > Spec:
    > http://www.w3.org/TR/css3-transforms/#svg-transform
    > http://www.w3.org/TR/css3-transforms/#svg-syntax
    > http://www.w3.org/TR/css3-transforms/#svg-gradient-transform-pattern-transform
    >
    > BUG=369942
    >
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=179702
    
    TBR=dschulze@chromium.org,ed@opera.com
    NOTREECHECKS=true
    BUG=369942
    
    Review URL: https://codereview.chromium.org/459053002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179998 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGElement.h
+++ third_party/WebKit/Source/core/svg/SVGElement.h
@@ -192,8 +192,6 @@ public:
 
     static const AtomicString& eventParameterName();
 
-    bool getStyleTransform(AffineTransform& matrix) const;
-
 protected:
     SVGElement(const QualifiedName&, Document&, ConstructionType = CreateSVGElement);
 
