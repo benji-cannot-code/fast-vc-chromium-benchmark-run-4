4ea55cf48ea1bb5d697efc45895b79aad148dfcb
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-11 12:23:04 +0000

    Disable New Avatar Menu in unit tests where necessary.
    This makes the tests more robust to it getting enabled by default.
    
    BUG=400667
    
    Review URL: https://codereview.chromium.org/444653002
    
    Cr-Commit-Position: refs/heads/master@{#288695}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288695 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_list_desktop_unittest.cc
+++ chrome/browser/profiles/profile_list_desktop_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/command_line.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/metrics/field_trial.h"
 #include "base/strings/string16.h"
@@ -17,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profiles_state.h"
 #include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile_manager.h"
+#include "components/signin/core/common/profile_management_switches.h"
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "grit/generated_resources.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -286,7 +288,9 @@ TEST_F(ProfileListDesktopTest, ShowAvatarMenuInTrial) {
 #endif
 }
 
-TEST_F(ProfileListDesktopTest, DontShowAvatarMenu) {
+TEST_F(ProfileListDesktopTest, DontShowOldAvatarMenuForSingleProfile) {
+  switches::DisableNewAvatarMenuForTesting(CommandLine::ForCurrentProcess());
+
   manager()->CreateTestingProfile("Test 1");
 
   EXPECT_FALSE(AvatarMenu::ShouldShowAvatarMenu());
@@ -299,6 +303,18 @@ TEST_F(ProfileListDesktopTest, DontShowAvatarMenu) {
   manager()->CreateTestingProfile("Test 2");
 
   EXPECT_FALSE(AvatarMenu::ShouldShowAvatarMenu());
+}
+
+TEST_F(ProfileListDesktopTest, AlwaysShowNewAvatarMenu) {
+  // If multiprofile mode is not enabled then the menu is never shown.
+  if (!profiles::IsMultipleProfilesEnabled())
+    return;
+
+  switches::EnableNewAvatarMenuForTesting(CommandLine::ForCurrentProcess());
+
+  manager()->CreateTestingProfile("Test 1");
+
+  EXPECT_TRUE(AvatarMenu::ShouldShowAvatarMenu());
 }
 
 TEST_F(ProfileListDesktopTest, ShowAvatarMenu) {
