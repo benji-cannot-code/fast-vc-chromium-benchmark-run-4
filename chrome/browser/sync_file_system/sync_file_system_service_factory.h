940ee559a13db6c5fc6f4013bbc1f6fd8350459d
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-11 09:24:03 +0000

    [SyncFS] Add injection point of LocalFileSyncService to SyncFileSystemService for testing
    BUG=402356
    
    Review URL: https://codereview.chromium.org/461523003
    
    Cr-Commit-Position: refs/heads/master@{#288679}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288679 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/sync_file_system_service_factory.h
+++ chrome/browser/sync_file_system/sync_file_system_service_factory.h
@@ -15,6 +15,7 @@ class Profile;
 
 namespace sync_file_system {
 
+class LocalFileSyncService;
 class RemoteFileSyncService;
 class SyncFileSystemService;
 
@@ -24,11 +25,13 @@ class SyncFileSystemServiceFactory : public BrowserContextKeyedServiceFactory {
   static SyncFileSystemService* FindForProfile(Profile* profile);
   static SyncFileSystemServiceFactory* GetInstance();
 
-  // This overrides the remote service for testing.
+  // This overrides the local/remote service for testing.
   // For testing this must be called before GetForProfile is called.
   // Otherwise a new DriveFileSyncService is created for the new service.
   // Since we use scoped_ptr it's one-off and the instance is passed
   // to the newly created SyncFileSystemService.
+  void set_mock_local_file_service(
+      scoped_ptr<LocalFileSyncService> mock_local_service);
   void set_mock_remote_file_service(
       scoped_ptr<RemoteFileSyncService> mock_remote_service);
 
@@ -41,6 +44,7 @@ class SyncFileSystemServiceFactory : public BrowserContextKeyedServiceFactory {
   virtual KeyedService* BuildServiceInstanceFor(
       content::BrowserContext* context) const OVERRIDE;
 
+  mutable scoped_ptr<LocalFileSyncService> mock_local_file_service_;
   mutable scoped_ptr<RemoteFileSyncService> mock_remote_file_service_;
 };
 
