bc58406376455e35d495279f4a59f7f5a682c7b6
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-12 23:45:11 +0000

    Change GuestViewInternalCreateGuestFunction to create the guest view using the calling WebContents, not whatever the "associated" WebContents is.
    BUG=402318
    R=fsamuel@chromium.org, jamescook@chromium.org
    
    Review URL: https://codereview.chromium.org/458323002
    
    Cr-Commit-Position: refs/heads/master@{#289118}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@289118 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/platform_apps/web_view/background/background.js
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var pass = chrome.test.callbackPass;
+
+var WEBVIEW_SRC = "data:text/html,<body>One</body>";
+
+chrome.test.runTests([
+  // Tests that embedding <webview> inside a background page loads.
+  function inDOM() {
+    var webview = document.querySelector('webview');
+    webview.addEventListener('contentload', pass());
+    webview.setAttribute('src', WEBVIEW_SRC);
+  },
+
+  // Tests that creating and attaching a WebView element inside a background
+  // page loads.
+  function newWebView() {
+    var webview = new WebView();
+    webview.addEventListener('contentload', pass());
+    webview.src = WEBVIEW_SRC;
+    document.body.appendChild(webview);
+  }
+]);
