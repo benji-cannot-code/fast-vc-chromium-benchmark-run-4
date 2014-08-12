c7e4bf89e5569f211fa624dcf98afe0e35da33ca
eranm@chromium.org <eranm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-12 16:26:17 +0000

    Certificate Transparency: Use component updater for EV whitelist
    Fetches the list of EV certificate hashes using the component updater.
    Once this is in, we could gather data on how many EV certificates do
    not appear in any Certificate Transparency log.
    
    BUG=339128
    
    Review URL: https://codereview.chromium.org/465443002
    
    Cr-Commit-Position: refs/heads/master@{#289001}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@289001 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_browser_main.cc
+++ chrome/browser/chrome_browser_main.cc
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chrome_browser_main_extra_parts.h"
 #include "chrome/browser/component_updater/cld_component_installer.h"
 #include "chrome/browser/component_updater/component_updater_service.h"
+#include "chrome/browser/component_updater/ev_whitelist_component_installer.h"
 #include "chrome/browser/component_updater/flash_component_installer.h"
 #include "chrome/browser/component_updater/pnacl/pnacl_component_installer.h"
 #include "chrome/browser/component_updater/recovery_component_installer.h"
@@ -413,6 +414,12 @@ void RegisterComponentsForUpdate() {
     g_browser_process->crl_set_fetcher()->StartInitialLoad(cus, path);
 #endif
   }
+
+#if !defined(OS_ANDROID)
+  // Android does not currently have the EV indicator. No need to get the
+  // EV certs whitelist on Android, then.
+  RegisterEVWhitelistComponent(cus);
+#endif
 
 #if defined(OS_WIN)
   ExecutePendingSwReporter(cus, g_browser_process->local_state());
--- chrome/chrome_browser.gypi
+++ chrome/chrome_browser.gypi
@@ -295,6 +295,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'browser/component_updater/crx_downloader.h',
       'browser/component_updater/default_component_installer.cc',
       'browser/component_updater/default_component_installer.h',
+      'browser/component_updater/ev_whitelist_component_installer.cc',
+      'browser/component_updater/ev_whitelist_component_installer.h',
       'browser/component_updater/flash_component_installer.h',
       'browser/component_updater/pepper_flash_component_installer.cc',
       'browser/component_updater/pnacl/pnacl_component_installer.cc',
