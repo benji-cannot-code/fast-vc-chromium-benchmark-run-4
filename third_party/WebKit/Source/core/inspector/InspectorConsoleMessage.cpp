1f21a1d29163e0936ee4b2333599cc7e2b2446f2
kozyatinskiy@google.com <kozyatinskiy@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-12 10:15:47 +0000

    [DevTools] Removed ScriptState from createScriptCallTackForConsole
    In method createScriptCallTackFromConsole we can get current execution context in function body without passing it in arguments.
    
    Removed canGenerateCallStack argument from InspectorConsoleMessage cstor which uses the transmitted stack.
    
    Review URL: https://codereview.chromium.org/458253002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180057 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorConsoleMessage.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorConsoleMessage.cpp
@@ -76,7 +76,7 @@ InspectorConsoleMessage::InspectorConsoleMessage(bool canGenerateCallStack, Mess
     autogenerateMetadata(canGenerateCallStack, scriptState);
 }
 
-InspectorConsoleMessage::InspectorConsoleMessage(bool, MessageSource source, MessageType type, MessageLevel level, const String& message, PassRefPtrWillBeRawPtr<ScriptCallStack> callStack, unsigned long requestIdentifier)
+InspectorConsoleMessage::InspectorConsoleMessage(MessageSource source, MessageType type, MessageLevel level, const String& message, PassRefPtrWillBeRawPtr<ScriptCallStack> callStack, unsigned long requestIdentifier)
     : m_source(source)
     , m_type(type)
     , m_level(level)
@@ -123,7 +123,7 @@ void InspectorConsoleMessage::autogenerateMetadata(bool canGenerateCallStack, Sc
         return;
 
     if (scriptState)
-        m_callStack = createScriptCallStackForConsole(scriptState);
+        m_callStack = createScriptCallStackForConsole();
     else if (canGenerateCallStack)
         m_callStack = createScriptCallStack(ScriptCallStack::maxCallStackSizeToCapture, true);
     else
