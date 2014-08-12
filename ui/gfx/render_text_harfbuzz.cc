6d63bebece6ab011b0cb7ddd43a9f81ae93465a9
mostynb@opera.com <mostynb@opera.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-12 15:38:54 +0000

    include <limits> for std::numeric_limits
    CL 435583003 adds usage of std::numeric_limits without including the
    required header "limits" header.  Let's fix that.
    
    BUG=398712
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/454413002
    
    Cr-Commit-Position: refs/heads/master@{#288986}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288986 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/render_text_harfbuzz.cc
+++ ui/gfx/render_text_harfbuzz.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/gfx/render_text_harfbuzz.h"
 
+#include <limits>
 #include <map>
 
 #include "base/i18n/bidi_line_iterator.h"
