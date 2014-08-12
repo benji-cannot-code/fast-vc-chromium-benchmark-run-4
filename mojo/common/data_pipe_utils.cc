5356ec9d884e990ddd524de2f6ae6850f0a98587
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-12 14:19:52 +0000

    Adds some CHECKs to MessagePumpMojo
    The thread watcher appears to be kicking on when we're trying to
    remove a handle. Not sure why. This converts some DCHECKs to CHECKs
    and fixes handling of the deadline. I suspect none of this will help,
    but it's worth a shot.
    
    BUG=399769
    TEST=none
    R=darin@chromium.org
    
    Review URL: https://codereview.chromium.org/454433003
    
    Cr-Commit-Position: refs/heads/master@{#288949}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288949 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/common/data_pipe_utils.cc
+++ mojo/common/data_pipe_utils.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/scoped_file.h"
 #include "base/message_loop/message_loop.h"
 #include "base/task_runner_util.h"
-#include "mojo/common/handle_watcher.h"
 
 namespace mojo {
 namespace common {
