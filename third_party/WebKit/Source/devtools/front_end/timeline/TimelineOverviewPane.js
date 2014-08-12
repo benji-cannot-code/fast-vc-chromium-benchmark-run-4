4bf50b56307c38ba22ec809700f14936439ff298
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-12 08:39:36 +0000

    Update Timeline overview pane when recording is stopped in Frame mode
    BUG=None
    
    Review URL: https://codereview.chromium.org/467523002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180045 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/timeline/TimelineOverviewPane.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TimelineOverviewPane.js
@@ -88,8 +88,6 @@ WebInspector.TimelineOverviewPane.prototype = {
 
     update: function()
     {
-        delete this._refreshTimeout;
-
         if (this._model.isEmpty())
             this._overviewCalculator._setWindow(0, 1000);
         else
@@ -172,15 +170,6 @@ WebInspector.TimelineOverviewPane.prototype = {
         this._overviewGrid.setWindow(windowBoundaries.left, windowBoundaries.right);
         this._overviewGrid.setResizeEnabled(!!this._model.records().length);
         this._muteOnWindowChanged = false;
-    },
-
-    _scheduleRefresh: function()
-    {
-        if (this._refreshTimeout)
-            return;
-        if (!this.isShowing())
-            return;
-        this._refreshTimeout = setTimeout(this.update.bind(this), 300);
     },
 
     __proto__: WebInspector.VBox.prototype
