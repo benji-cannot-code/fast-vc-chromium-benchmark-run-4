db3b9bfd0cc97f2e3791eabb98e05a1aa478bcc2
posciak@chromium.org <posciak@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-12 05:47:41 +0000

    VAVEA: Turn VAAPI encoder flag into a kill switch.
    Flip the flag to enable VAVEA by default.
    
    Also update the CastStreamingApiTest to not require VP8, as we may be using
    H264 as well, but just assert on non-empty string for the codec. Do the
    same for the audio codec while we are here.
    
    
    TEST=encoding scenarios
    BUG=378962,398322
    
    Review URL: https://codereview.chromium.org/374553002
    
    Cr-Commit-Position: refs/heads/master@{#288920}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288920 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/chrome_restart_request.cc
+++ chrome/browser/chromeos/login/chrome_restart_request.cc
@@ -156,7 +156,7 @@ std::string DeriveCommandLine(const GURL& start_url,
     ::switches::kDisableWebRtcHWEncoding,
     ::switches::kEnableWebRtcHWVp8Encoding,
 #endif
-    ::switches::kEnableVaapiAcceleratedVideoEncode,
+    ::switches::kDisableVaapiAcceleratedVideoEncode,
 #if defined(USE_OZONE)
     ::switches::kOzonePlatform,
 #endif
--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1258,7 +1258,7 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kDisableDirectWrite,
 #endif
 #if defined(OS_CHROMEOS)
-    switches::kEnableVaapiAcceleratedVideoEncode,
+    switches::kDisableVaapiAcceleratedVideoEncode,
 #endif
   };
   renderer_cmd->CopySwitchesFrom(browser_cmd, kSwitchNames,
