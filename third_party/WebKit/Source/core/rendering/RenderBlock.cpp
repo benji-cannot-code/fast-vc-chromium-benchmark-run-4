d1d5184b6cfadfce18a8c5aecab40e872646bd26
rego@igalia.com <rego@igalia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-12 08:23:41 +0000

    [CSS Grid Layout] Ignore ::first-letter pseudo-element
    According to the spec the ::first-line pseudo-element do not apply to
    grid containers (neither to flexboxes).
    
    Modified RenderBlock::findFirstLetterBlock() to use isRenderBlockFlow()
    instead of isFlexibleBox(). This change has been already made before in
    several parts of the code, but there was one missing case here.
    
    Fix issue in RenderBlock::updateFirstLetter() that applies to both grids
    and flexboxes. Basically if the grid's or flexbox's container was
    defining the ::first-line pseudo-element and the grid or flexbox itself
    too, the value from the grid or flexbox was being applied to the items.
    Add the proper check to avoid this.
    
    Added two new tests for grid and modified flexbox test to cover the
    issue explained above.
    
    TEST=css3/flexbox/flexbox-ignore-container-firstLetter.html
    TEST=fast/css-grid-layout/grid-container-ignore-first-letter.html
    TEST=fast/css-grid-layout/grid-item-first-letter-valid.html
    
    BUG=395788
    
    Review URL: https://codereview.chromium.org/440233002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180043 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderBlock.cpp
+++ third_party/WebKit/Source/core/rendering/RenderBlock.cpp
@@ -3930,6 +3930,17 @@ int RenderBlock::lastLineBoxBaseline(LineDirectionMode lineDirection) const
     return -1;
 }
 
+static inline bool isRenderBlockFlowOrRenderButton(RenderObject* renderObject)
+{
+    // We include isRenderButton in this check because buttons are implemented
+    // using flex box but should still support first-line|first-letter.
+    // The flex box and grid specs require that flex box and grid do not
+    // support first-line|first-letter, though.
+    // FIXME: Remove when buttons are implemented with align-items instead
+    // of flex box.
+    return renderObject->isRenderBlockFlow() || renderObject->isRenderButton();
+}
+
 RenderBlock* RenderBlock::firstLineBlock() const
 {
     RenderBlock* firstLineBlock = const_cast<RenderBlock*>(this);
@@ -3939,15 +3950,9 @@ RenderBlock* RenderBlock::firstLineBlock() const
         if (hasPseudo)
             break;
         RenderObject* parentBlock = firstLineBlock->parent();
-        // We include isRenderButton in this check because buttons are
-        // implemented using flex box but should still support first-line. The
-        // flex box spec requires that flex box does not support first-line,
-        // though.
-        // FIXME: Remove when buttons are implemented with align-items instead
-        // of flexbox.
         if (firstLineBlock->isReplaced() || firstLineBlock->isFloating()
             || !parentBlock
-            || (!parentBlock->isRenderBlockFlow() && !parentBlock->isRenderButton()))
+            || !isRenderBlockFlowOrRenderButton(parentBlock))
             break;
         ASSERT_WITH_SECURITY_IMPLICATION(parentBlock->isRenderBlock());
         if (toRenderBlock(parentBlock)->firstChild() != firstLineBlock)
@@ -3993,21 +3998,15 @@ static inline RenderObject* findFirstLetterBlock(RenderBlock* start)
 {
     RenderObject* firstLetterBlock = start;
     while (true) {
-        // We include isRenderButton in these two checks because buttons are
-        // implemented using flex box but should still support first-letter.
-        // The flex box spec requires that flex box does not support
-        // first-letter, though.
-        // FIXME: Remove when buttons are implemented with align-items instead
-        // of flexbox.
         bool canHaveFirstLetterRenderer = firstLetterBlock->style()->hasPseudoStyle(FIRST_LETTER)
             && firstLetterBlock->canHaveGeneratedChildren()
-            && (!firstLetterBlock->isFlexibleBox() || firstLetterBlock->isRenderButton());
+            && isRenderBlockFlowOrRenderButton(firstLetterBlock);
         if (canHaveFirstLetterRenderer)
             return firstLetterBlock;
 
         RenderObject* parentBlock = firstLetterBlock->parent();
         if (firstLetterBlock->isReplaced() || !parentBlock
-            || (!parentBlock->isRenderBlockFlow() && !parentBlock->isRenderButton())) {
+            || !isRenderBlockFlowOrRenderButton(parentBlock)) {
             return 0;
         }
         ASSERT(parentBlock->isRenderBlock());
@@ -4190,7 +4189,7 @@ void RenderBlock::updateFirstLetter()
         }
     }
 
-    if (!currChild)
+    if (!currChild || !isRenderBlockFlowOrRenderButton(firstLetterBlock))
         return;
 
     // If the child already has style, then it has already been created, so we just want
