30c8c35d00d1ded275c54e8707f6b0bb31eda1cd
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-13 19:28:32 +0000

    WebLocalFrameImpl::printPage needs to ensure layout, style, and compositing are up-to-date
    Previously, we could try to paint without having our painting state up-to-date.
    The codepath for printing multiple pages updatesLayoutAndStyleForPainting. This
    CL makes the code path for printing single pages do the same.
    
    R=adamk@chromium.org
    
    Review URL: https://codereview.chromium.org/471623003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180200 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/FrameView.cpp
+++ third_party/WebKit/Source/core/frame/FrameView.cpp
@@ -2556,6 +2556,7 @@ void FrameView::paintContents(GraphicsContext* p, const IntRect& rect)
     }
 
     RELEASE_ASSERT(!needsLayout());
+    ASSERT(document->lifecycle().state() >= DocumentLifecycle::CompositingClean);
 
     TRACE_EVENT1(TRACE_DISABLED_BY_DEFAULT("devtools.timeline"), "Paint", "data", InspectorPaintEvent::data(renderView, rect, 0));
     TRACE_EVENT_INSTANT1(TRACE_DISABLED_BY_DEFAULT("devtools.timeline.stack"), "CallStack", "stack", InspectorCallStackEvent::currentCallStack());
