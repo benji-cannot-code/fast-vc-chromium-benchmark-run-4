cb10c0bb0eada26add1631702a33ab7173602f45
dtrainor@chromium.org <dtrainor@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-13 03:25:32 +0000

    Android Disable menu animations for low end devices
    BUG=403143
    
    Review URL: https://codereview.chromium.org/469583003
    
    Cr-Commit-Position: refs/heads/master@{#289183}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@289183 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/android/java/src/org/chromium/chrome/browser/appmenu/AppMenu.java
+++ chrome/android/java/src/org/chromium/chrome/browser/appmenu/AppMenu.java
@@ -25,6 +25,7 @@ import android.widget.ListPopupWindow;
 import android.widget.PopupWindow;
 import android.widget.PopupWindow.OnDismissListener;
 
+import org.chromium.base.SysUtils;
 import org.chromium.chrome.R;
 
 import java.util.ArrayList;
@@ -113,6 +114,9 @@ public class AppMenu implements OnItemClickListener, OnKeyListener {
             mPopup.setAnimationStyle(R.style.OverflowMenuAnim);
         }
 
+        // Turn off window animations for low end devices.
+        if (SysUtils.isLowEndDevice()) mPopup.setAnimationStyle(0);
+
         Rect bgPadding = new Rect();
         mPopup.getBackground().getPadding(bgPadding);
 
@@ -156,14 +160,17 @@ public class AppMenu implements OnItemClickListener, OnKeyListener {
             mPopup.getListView().setFadingEdgeLength(mVerticalFadeDistance);
         }
 
-        mPopup.getListView().addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
-            @Override
-            public void onLayoutChange(View v, int left, int top, int right, int bottom,
-                    int oldLeft, int oldTop, int oldRight, int oldBottom) {
-                mPopup.getListView().removeOnLayoutChangeListener(this);
-                runMenuItemEnterAnimations();
-            }
-        });
+        // Don't animate the menu items for low end devices.
+        if (!SysUtils.isLowEndDevice()) {
+            mPopup.getListView().addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+                @Override
+                public void onLayoutChange(View v, int left, int top, int right, int bottom,
+                        int oldLeft, int oldTop, int oldRight, int oldBottom) {
+                    mPopup.getListView().removeOnLayoutChangeListener(this);
+                    runMenuItemEnterAnimations();
+                }
+            });
+        }
     }
 
     private void setPopupOffset(ListPopupWindow popup, int screenRotation, Rect appRect) {
