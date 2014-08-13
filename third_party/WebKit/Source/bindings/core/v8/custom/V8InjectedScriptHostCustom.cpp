37aeb08a46beb1575d080e1c77bb00a6c1b361e3
aandrey@chromium.org <aandrey@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-13 12:18:10 +0000

    DevTools: Simplify isArrayLike() in injected script and extend InjectedScriptHost.type()
    R=yurys@chromium.org, yurys
    
    Review URL: https://codereview.chromium.org/464793002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180168 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/core/v8/custom/V8InjectedScriptHostCustom.cpp
+++ third_party/WebKit/Source/bindings/core/v8/custom/V8InjectedScriptHostCustom.cpp
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/ScriptValue.h"
 #include "bindings/core/v8/V8AbstractEventListener.h"
 #include "bindings/core/v8/V8Binding.h"
+#include "bindings/core/v8/V8DOMTokenList.h"
 #include "bindings/core/v8/V8EventTarget.h"
 #include "bindings/core/v8/V8HTMLAllCollection.h"
 #include "bindings/core/v8/V8HTMLCollection.h"
@@ -165,7 +166,7 @@ void V8InjectedScriptHost::typeMethodCustom(const v8::FunctionCallbackInfo<v8::V
         v8SetReturnValue(info, v8AtomicString(isolate, "string"));
         return;
     }
-    if (value->IsArray()) {
+    if (value->IsArray() || value->IsTypedArray()) {
         v8SetReturnValue(info, v8AtomicString(isolate, "array"));
         return;
     }
@@ -189,27 +190,10 @@ void V8InjectedScriptHost::typeMethodCustom(const v8::FunctionCallbackInfo<v8::V
         v8SetReturnValue(info, v8AtomicString(isolate, "node"));
         return;
     }
-    if (V8NodeList::hasInstance(value, isolate)) {
-        v8SetReturnValue(info, v8AtomicString(isolate, "array"));
-        return;
-    }
-    if (V8HTMLCollection::hasInstance(value, isolate)) {
-        v8SetReturnValue(info, v8AtomicString(isolate, "array"));
-        return;
-    }
-    if (V8Int8Array::hasInstance(value, isolate) || V8Int16Array::hasInstance(value, isolate) || V8Int32Array::hasInstance(value, isolate)) {
-        v8SetReturnValue(info, v8AtomicString(isolate, "array"));
-        return;
-    }
-    if (V8Uint8Array::hasInstance(value, isolate) || V8Uint16Array::hasInstance(value, isolate) || V8Uint32Array::hasInstance(value, isolate)) {
-        v8SetReturnValue(info, v8AtomicString(isolate, "array"));
-        return;
-    }
-    if (V8Float32Array::hasInstance(value, isolate) || V8Float64Array::hasInstance(value, isolate)) {
-        v8SetReturnValue(info, v8AtomicString(isolate, "array"));
-        return;
-    }
-    if (V8Uint8ClampedArray::hasInstance(value, isolate)) {
+    if (V8NodeList::hasInstance(value, isolate)
+        || V8DOMTokenList::hasInstance(value, isolate)
+        || V8HTMLCollection::hasInstance(value, isolate)
+        || V8HTMLAllCollection::hasInstance(value, isolate)) {
         v8SetReturnValue(info, v8AtomicString(isolate, "array"));
         return;
     }
