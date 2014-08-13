988cc38603f19de18aa446df5ebd011204f2d579
lushnikov@chromium.org <lushnikov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-13 20:44:47 +0000

    DevTools: [SSP] fix design glitches in "Insert New Rule"
    The patch does the following:
    - adds tooltip for the "Insert New Rule" button
    - repositions button via absolute positioning
    - does not show "via inspector" title in "Insert New Rule" if it's going
    to be added in actual stylesheet.
    
    R=pfeldman, vsevik
    BUG=402461
    
    Review URL: https://codereview.chromium.org/468973002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180204 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/elements/StylesSidebarPane.js
+++ third_party/WebKit/Source/devtools/front_end/elements/StylesSidebarPane.js
@@ -1139,6 +1139,7 @@ WebInspector.StylePropertiesSection = function(parentPane, styleRule, editable, 
 
     if (this.editable && this.rule) {
         var newRuleButton = closeBrace.createChild("div", "sidebar-pane-button-new-rule");
+        newRuleButton.title = WebInspector.UIString("Insert Style Rule");
         newRuleButton.addEventListener("click", this._onNewRuleClick.bind(this), false);
     }
 
@@ -1890,7 +1891,8 @@ WebInspector.ComputedStylePropertiesSection.prototype = {
  */
 WebInspector.BlankStylePropertiesSection = function(stylesPane, defaultSelectorText, styleSheetId, ruleLocation, insertAfterRule)
 {
-    WebInspector.StylePropertiesSection.call(this, stylesPane, {selectorText: defaultSelectorText, rule: {isViaInspector: true}}, true, false);
+    var styleSheetHeader = WebInspector.cssModel.styleSheetHeaderForId(styleSheetId);
+    WebInspector.StylePropertiesSection.call(this, stylesPane, {selectorText: defaultSelectorText, rule: {isViaInspector: styleSheetHeader.isViaInspector()}}, true, false);
     this._ruleLocation = ruleLocation;
     this._styleSheetId = styleSheetId;
     if (insertAfterRule)
