4c178e5e002bc64afc69365b9b70bb64650e0f78
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-13 16:57:25 +0000

    Cleanup client plugin handling code in the webapp.
    Previously the <embed> element was created in client_session.js , while
    it's owned by the ClientPlugin object. Moved plugin creation code where
    it belongs, to client_plugin.js .
    
    R=jamiewalch@chromium.org
    
    Review URL: https://codereview.chromium.org/469453003
    
    Cr-Commit-Position: refs/heads/master@{#289323}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@289323 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/webapp/client_plugin.js
+++ remoting/webapp/client_plugin.js
@@ -18,14 +18,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 var remoting = remoting || {};
 
 /**
- * @param {remoting.ViewerPlugin} plugin The plugin embed element.
+ * @param {Element} container The container for the embed element.
  * @param {function(string, string):boolean} onExtensionMessage The handler for
  *     protocol extension messages. Returns true if a message is recognized;
  *     false otherwise.
  * @constructor
  */
-remoting.ClientPlugin = function(plugin, onExtensionMessage) {
-  this.plugin = plugin;
+remoting.ClientPlugin = function(container, onExtensionMessage) {
+  this.plugin_ = /** @type {remoting.ViewerPlugin} */
+      document.createElement('embed');
+
+  this.plugin_.id = 'session-client-plugin';
+  if (remoting.settings.CLIENT_PLUGIN_TYPE == 'pnacl') {
+    this.plugin_.src = 'remoting_client_pnacl.nmf';
+    this.plugin_.type = 'application/x-pnacl';
+  } else if (remoting.settings.CLIENT_PLUGIN_TYPE == 'nacl') {
+    this.plugin_.src = 'remoting_client_nacl.nmf';
+    this.plugin_.type = 'application/x-nacl';
+  } else {
+    this.plugin_.src = 'about://none';
+    this.plugin_.type = 'application/vnd.chromium.remoting-viewer';
+  }
+
+  this.plugin_.width = 0;
+  this.plugin_.height = 0;
+  this.plugin_.tabIndex = 0;  // Required, otherwise focus() doesn't work.
+  container.appendChild(this.plugin_);
+
   this.onExtensionMessage_ = onExtensionMessage;
 
   this.desktopWidth = 0;
@@ -88,7 +107,7 @@ remoting.ClientPlugin = function(plugin, onExtensionMessage) {
   /** @type {remoting.ClientPlugin} */
   var that = this;
   /** @param {Event} event Message event from the plugin. */
-  this.plugin.addEventListener('message', function(event) {
+  this.plugin_.addEventListener('message', function(event) {
       that.handleMessage_(event.data);
     }, false);
 
@@ -381,14 +400,17 @@ remoting.ClientPlugin.prototype.handleMessageMethod_ = function(message) {
  * Deletes the plugin.
  */
 remoting.ClientPlugin.prototype.cleanup = function() {
-  this.plugin.parentNode.removeChild(this.plugin);
+  if (this.plugin_) {
+    this.plugin_.parentNode.removeChild(this.plugin_);
+    this.plugin_ = null;
+  }
 };
 
 /**
- * @return {HTMLEmbedElement} HTML element that correspods to the plugin.
+ * @return {HTMLEmbedElement} HTML element that corresponds to the plugin.
  */
 remoting.ClientPlugin.prototype.element = function() {
-  return this.plugin;
+  return this.plugin_;
 };
 
 /**
@@ -439,8 +461,8 @@ remoting.ClientPlugin.prototype.isInjectKeyEventSupported = function() {
  * @param {string} iq Incoming IQ stanza.
  */
 remoting.ClientPlugin.prototype.onIncomingIq = function(iq) {
-  if (this.plugin && this.plugin.postMessage) {
-    this.plugin.postMessage(JSON.stringify(
+  if (this.plugin_ && this.plugin_.postMessage) {
+    this.plugin_.postMessage(JSON.stringify(
         { method: 'incomingIq', data: { iq: iq } }));
   } else {
     // plugin.onIq may not be set after the plugin has been shut
@@ -476,9 +498,9 @@ remoting.ClientPlugin.prototype.connect = function(
   } else if (remoting.platformIsChromeOS()) {
     keyFilter = 'cros';
   }
-  this.plugin.postMessage(JSON.stringify(
+  this.plugin_.postMessage(JSON.stringify(
       { method: 'delegateLargeCursors', data: {} }));
-  this.plugin.postMessage(JSON.stringify(
+  this.plugin_.postMessage(JSON.stringify(
     { method: 'connect', data: {
         hostJid: hostJid,
         hostPublicKey: hostPublicKey,
@@ -498,7 +520,7 @@ remoting.ClientPlugin.prototype.connect = function(
  * Release all currently pressed keys.
  */
 remoting.ClientPlugin.prototype.releaseAllKeys = function() {
-  this.plugin.postMessage(JSON.stringify(
+  this.plugin_.postMessage(JSON.stringify(
       { method: 'releaseAllKeys', data: {} }));
 };
 
@@ -510,7 +532,7 @@ remoting.ClientPlugin.prototype.releaseAllKeys = function() {
  */
 remoting.ClientPlugin.prototype.injectKeyEvent =
     function(usbKeycode, pressed) {
-  this.plugin.postMessage(JSON.stringify(
+  this.plugin_.postMessage(JSON.stringify(
       { method: 'injectKeyEvent', data: {
           'usbKeycode': usbKeycode,
           'pressed': pressed}
@@ -525,7 +547,7 @@ remoting.ClientPlugin.prototype.injectKeyEvent =
  */
 remoting.ClientPlugin.prototype.remapKey =
     function(fromKeycode, toKeycode) {
-  this.plugin.postMessage(JSON.stringify(
+  this.plugin_.postMessage(JSON.stringify(
       { method: 'remapKey', data: {
           'fromKeycode': fromKeycode,
           'toKeycode': toKeycode}
@@ -539,7 +561,7 @@ remoting.ClientPlugin.prototype.remapKey =
  * @param {Boolean} trap True to enable trapping, False to disable.
  */
 remoting.ClientPlugin.prototype.trapKey = function(keycode, trap) {
-  this.plugin.postMessage(JSON.stringify(
+  this.plugin_.postMessage(JSON.stringify(
       { method: 'trapKey', data: {
           'keycode': keycode,
           'trap': trap}
@@ -565,7 +587,7 @@ remoting.ClientPlugin.prototype.sendClipboardItem =
     function(mimeType, item) {
   if (!this.hasFeature(remoting.ClientPlugin.Feature.SEND_CLIPBOARD_ITEM))
     return;
-  this.plugin.postMessage(JSON.stringify(
+  this.plugin_.postMessage(JSON.stringify(
       { method: 'sendClipboardItem',
         data: { mimeType: mimeType, item: item }}));
 };
@@ -581,7 +603,7 @@ remoting.ClientPlugin.prototype.notifyClientResolution =
     function(width, height, device_scale) {
   if (this.hasFeature(remoting.ClientPlugin.Feature.NOTIFY_CLIENT_RESOLUTION)) {
     var dpi = Math.floor(device_scale * 96);
-    this.plugin.postMessage(JSON.stringify(
+    this.plugin_.postMessage(JSON.stringify(
         { method: 'notifyClientResolution',
           data: { width: Math.floor(width * device_scale),
                   height: Math.floor(height * device_scale),
@@ -597,10 +619,10 @@ remoting.ClientPlugin.prototype.notifyClientResolution =
 remoting.ClientPlugin.prototype.pauseVideo =
     function(pause) {
   if (this.hasFeature(remoting.ClientPlugin.Feature.VIDEO_CONTROL)) {
-    this.plugin.postMessage(JSON.stringify(
+    this.plugin_.postMessage(JSON.stringify(
         { method: 'videoControl', data: { pause: pause }}));
   } else if (this.hasFeature(remoting.ClientPlugin.Feature.PAUSE_VIDEO)) {
-    this.plugin.postMessage(JSON.stringify(
+    this.plugin_.postMessage(JSON.stringify(
         { method: 'pauseVideo', data: { pause: pause }}));
   }
 };
@@ -615,7 +637,7 @@ remoting.ClientPlugin.prototype.pauseAudio =
   if (!this.hasFeature(remoting.ClientPlugin.Feature.PAUSE_AUDIO)) {
     return;
   }
-  this.plugin.postMessage(JSON.stringify(
+  this.plugin_.postMessage(JSON.stringify(
       { method: 'pauseAudio', data: { pause: pause }}));
 };
 
@@ -629,7 +651,7 @@ remoting.ClientPlugin.prototype.setLosslessEncode =
   if (!this.hasFeature(remoting.ClientPlugin.Feature.VIDEO_CONTROL)) {
     return;
   }
-  this.plugin.postMessage(JSON.stringify(
+  this.plugin_.postMessage(JSON.stringify(
       { method: 'videoControl', data: { losslessEncode: wantLossless }}));
 };
 
@@ -643,7 +665,7 @@ remoting.ClientPlugin.prototype.setLosslessColor =
   if (!this.hasFeature(remoting.ClientPlugin.Feature.VIDEO_CONTROL)) {
     return;
   }
-  this.plugin.postMessage(JSON.stringify(
+  this.plugin_.postMessage(JSON.stringify(
       { method: 'videoControl', data: { losslessColor: wantLossless }}));
 };
 
@@ -657,7 +679,7 @@ remoting.ClientPlugin.prototype.onPinFetched =
   if (!this.hasFeature(remoting.ClientPlugin.Feature.ASYNC_PIN)) {
     return;
   }
-  this.plugin.postMessage(JSON.stringify(
+  this.plugin_.postMessage(JSON.stringify(
       { method: 'onPinFetched', data: { pin: pin }}));
 };
 
@@ -669,7 +691,7 @@ remoting.ClientPlugin.prototype.useAsyncPinDialog =
   if (!this.hasFeature(remoting.ClientPlugin.Feature.ASYNC_PIN)) {
     return;
   }
-  this.plugin.postMessage(JSON.stringify(
+  this.plugin_.postMessage(JSON.stringify(
       { method: 'useAsyncPinDialog', data: {} }));
 };
 
@@ -681,7 +703,7 @@ remoting.ClientPlugin.prototype.useAsyncPinDialog =
  */
 remoting.ClientPlugin.prototype.onThirdPartyTokenFetched = function(
     token, sharedSecret) {
-  this.plugin.postMessage(JSON.stringify(
+  this.plugin_.postMessage(JSON.stringify(
     { method: 'onThirdPartyTokenFetched',
       data: { token: token, sharedSecret: sharedSecret}}));
 };
@@ -699,7 +721,7 @@ remoting.ClientPlugin.prototype.requestPairing =
     return;
   }
   this.onPairingComplete_ = onDone;
-  this.plugin.postMessage(JSON.stringify(
+  this.plugin_.postMessage(JSON.stringify(
       { method: 'requestPairing', data: { clientName: clientName } }));
 };
 
@@ -714,7 +736,7 @@ remoting.ClientPlugin.prototype.sendClientMessage =
   if (!this.hasFeature(remoting.ClientPlugin.Feature.EXTENSION_MESSAGE)) {
     return;
   }
-  this.plugin.postMessage(JSON.stringify(
+  this.plugin_.postMessage(JSON.stringify(
       { method: 'extensionMessage',
         data: { type: type, data: message } }));
 
@@ -731,7 +753,7 @@ remoting.ClientPlugin.prototype.enableMediaSourceRendering =
     return;
   }
   this.mediaSourceRenderer_ = mediaSourceRenderer;
-  this.plugin.postMessage(JSON.stringify(
+  this.plugin_.postMessage(JSON.stringify(
       { method: 'enableMediaSourceRendering', data: {} }));
 };
 
@@ -745,14 +767,14 @@ remoting.ClientPlugin.prototype.showPluginForClickToPlay_ = function() {
   if (!this.helloReceived_) {
     var width = 200;
     var height = 200;
-    this.plugin.style.width = width + 'px';
-    this.plugin.style.height = height + 'px';
+    this.plugin_.style.width = width + 'px';
+    this.plugin_.style.height = height + 'px';
     // Center the plugin just underneath the "Connnecting..." dialog.
     var dialog = document.getElementById('client-dialog');
     var dialogRect = dialog.getBoundingClientRect();
-    this.plugin.style.top = (dialogRect.bottom + 16) + 'px';
-    this.plugin.style.left = (window.innerWidth - width) / 2 + 'px';
-    this.plugin.style.position = 'fixed';
+    this.plugin_.style.top = (dialogRect.bottom + 16) + 'px';
+    this.plugin_.style.left = (window.innerWidth - width) / 2 + 'px';
+    this.plugin_.style.position = 'fixed';
   }
 };
 
@@ -761,9 +783,9 @@ remoting.ClientPlugin.prototype.showPluginForClickToPlay_ = function() {
  * @private
  */
 remoting.ClientPlugin.prototype.hidePluginForClickToPlay_ = function() {
-  this.plugin.style.width = '';
-  this.plugin.style.height = '';
-  this.plugin.style.top = '';
-  this.plugin.style.left = '';
-  this.plugin.style.position = '';
+  this.plugin_.style.width = '';
+  this.plugin_.style.height = '';
+  this.plugin_.style.top = '';
+  this.plugin_.style.left = '';
+  this.plugin_.style.position = '';
 };
