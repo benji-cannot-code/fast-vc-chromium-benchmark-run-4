35bf995d23f39ca1d617beaa7b3346a0d245a2de
a.sarkar.arun@gmail.com <a.sarkar.arun@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-13 22:22:03 +0000

    The objective of this test to assert text returned from a SelectAll call on WebContent is same as text from WebLocalFrame::ContentAsText.
    This is a patch that refers to the following CL
    https://codereview.chromium.org/342143004/
    
    Review URL: https://codereview.chromium.org/464613003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180208 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/tests/WebViewTest.cpp
+++ third_party/WebKit/Source/web/tests/WebViewTest.cpp
@@ -67,6 +67,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "public/web/WebFrameClient.h"
 #include "public/web/WebHitTestResult.h"
 #include "public/web/WebInputEvent.h"
+#include "public/web/WebScriptSource.h"
 #include "public/web/WebSettings.h"
 #include "public/web/WebViewClient.h"
 #include "public/web/WebWidget.h"
@@ -2139,6 +2140,20 @@ TEST_F(WebViewTest, FirstUserGestureObservedGestureTap)
 
     EXPECT_EQ(1, client.getUserGestureNotificationsCount());
     webView->setAutofillClient(0);
+}
+
+TEST_F(WebViewTest, CompareSelectAllToContentAsText)
+{
+    URLTestHelpers::registerMockedURLFromBaseURL(WebString::fromUTF8(m_baseURL.c_str()), WebString::fromUTF8("longpress_selection.html"));
+    WebView* webView = m_webViewHelper.initializeAndLoad(m_baseURL + "longpress_selection.html", true);
+
+    WebLocalFrameImpl* frame = toWebLocalFrameImpl(webView->mainFrame());
+    frame->executeScript(WebScriptSource(WebString::fromUTF8("document.execCommand('SelectAll', false, null)")));
+    std::string actual = frame->selectionAsText().utf8();
+
+    const int kMaxOutputCharacters = 1024;
+    std::string expected = frame->contentAsText(kMaxOutputCharacters).utf8();
+    EXPECT_EQ(expected, actual);
 }
 
 } // namespace
