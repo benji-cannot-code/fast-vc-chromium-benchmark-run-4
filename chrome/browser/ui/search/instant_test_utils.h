47760b42d325efa9925a5514deed6004496ceea1
mathp@chromium.org <mathp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-13 16:50:11 +0000

    [Local NTP] Fix LocalNTP tests, and re-enable them.
    BUG=267117
    TEST=LocalNTPTest (interactive_ui_tests)
    
    Review URL: https://codereview.chromium.org/464483002
    
    Cr-Commit-Position: refs/heads/master@{#289321}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@289321 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/search/instant_test_utils.h
+++ chrome/browser/ui/search/instant_test_utils.h
@@ -63,6 +63,8 @@ class InstantTestBase {
 
   const GURL& instant_url() const { return instant_url_; }
 
+  const GURL& ntp_url() const { return ntp_url_; }
+
   net::SpawnedTestServer& https_test_server() { return https_test_server_; }
 
   void KillInstantRenderView();
