e7f98e2f35ef4517cb27d7b02e4e861fe592dbaa
gunsch@chromium.org <gunsch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-13 09:56:08 +0000

    CastContentBrowserClient should not hold CastBrowserMainParts lifetime.
    R=lcwu@chromium.org,byungchul@chromium.org
    BUG=None
    
    Review URL: https://codereview.chromium.org/463563003
    
    Cr-Commit-Position: refs/heads/master@{#289241}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@289241 0039d316-1c4b-4281-b951-d872f2087c98

--- chromecast/shell/browser/cast_content_browser_client.h
+++ chromecast/shell/browser/cast_content_browser_client.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROMECAST_SHELL_BROWSER_CAST_CONTENT_BROWSER_CLIENT_H_
 
 #include "base/macros.h"
+#include "base/memory/scoped_ptr.h"
 #include "content/public/browser/content_browser_client.h"
 
 namespace chromecast {
@@ -70,8 +71,9 @@ class CastContentBrowserClient: public content::ContentBrowserClient {
       std::vector<content::FileDescriptorInfo>* mappings) OVERRIDE;
 
  private:
-  scoped_ptr<CastBrowserMainParts> shell_browser_main_parts_;
-
+  // Note: BrowserMainLoop holds ownership of CastBrowserMainParts after it is
+  // created.
+  CastBrowserMainParts* shell_browser_main_parts_;
   scoped_ptr<URLRequestContextFactory> url_request_context_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(CastContentBrowserClient);
