d087d7fd62dd40c11bd97e96d285870a307c55eb
dsinclair@chromium.org <dsinclair@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-13 16:29:31 +0000

    Rename repaint to paintInvalidation in rendering/style.
    This CL updates the usage of repaint to be paintInvalidation inside the
    various style/ classes.
    
    BUG=338691
    
    Review URL: https://codereview.chromium.org/470633002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180192 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/svg/RenderSVGRoot.cpp
+++ third_party/WebKit/Source/core/rendering/svg/RenderSVGRoot.cpp
@@ -282,7 +282,7 @@ void RenderSVGRoot::styleDidChange(StyleDifference diff, const RenderStyle* oldS
 {
     if (diff.needsFullLayout())
         setNeedsBoundariesUpdate();
-    if (diff.needsRepaint()) {
+    if (diff.needsPaintInvalidation()) {
         // Box decorations may have appeared/disappeared - recompute status.
         m_hasBoxDecorationBackground = calculateHasBoxDecorations();
     }
