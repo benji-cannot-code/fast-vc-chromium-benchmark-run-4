65fe0a3d2cb81b4c249fb4715354be3af54a253e
jackhou@chromium.org <jackhou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-13 10:25:04 +0000

    [Win Aero] Correctly set the minimum window size.
    This also fixes the value of [inner|outer]Bounds.[width|height]
    to account for the border added by
    AppWindowDesktopWindowTreeHostWin::GetClientAreaInsets
    
    BUG=394567, 395929
    
    Review URL: https://codereview.chromium.org/405323002
    
    Cr-Commit-Position: refs/heads/master@{#289257}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@289257 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/platform_apps/outer_bounds/test.js
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.test.sendMessage('Launched', function (response) {
+    var frameType = response == 'color' ? { color: '#ff0000' } : response;
+    chrome.app.window.create('main.html', {
+      frame: frameType,
+      outerBounds: {
+        left: 10,
+        top: 11,
+        width: 300,
+        height: 301,
+        minWidth: 200,
+        minHeight: 201,
+        maxWidth: 400,
+        maxHeight: 401
+      }
+    }, function () {
+      // Send this again as the test is waiting for the window to be ready.
+      chrome.test.sendMessage('Launched');
+    });
+  });
+});
