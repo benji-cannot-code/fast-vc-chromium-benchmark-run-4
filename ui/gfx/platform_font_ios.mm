e43f0edd9b22276cde8188e07e608bead2373aae
bartfab@chromium.org <bartfab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-14 10:10:17 +0000

    Revert 289403 "Update iOS deployment target to 7.0"
    This appears to have broken gfx_unittests on iOS:
    
    http://build.chromium.org/p/chromium.mac/builders/iOS%20Simulator%20%28dbg%29/builds/17689
    
    > Update iOS deployment target to 7.0
    > 
    > iOS 6 will no longer be supported.
    > 
    > Replaces deprecated sizeWithFont: with sizeWithAttributes: (adding a
    > ceilf since according to the internet, the former rounds up, and the
    > latter does not) to fix compilation.
    > 
    > BUG=402921
    > 
    > Review URL: https://codereview.chromium.org/463333002
    
    TBR=stuartmorgan@chromium.org
    
    Review URL: https://codereview.chromium.org/473883002
    
    Cr-Commit-Position: refs/heads/master@{#289510}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@289510 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/platform_font_ios.mm
+++ ui/gfx/platform_font_ios.mm
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
-#include <cmath>
-
 #include "base/basictypes.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/strings/utf_string_conversions.h"
@@ -113,8 +111,7 @@ void PlatformFontIOS::CalculateMetrics() {
   height_ = font.lineHeight;
   ascent_ = font.ascender;
   cap_height_ = font.capHeight;
-  NSDictionary* attributes = @{ NSFontAttributeName : font };
-  average_width_ = std::ceil([@"x" sizeWithAttributes:attributes].width);
+  average_width_ = [@"x" sizeWithFont:font].width;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
