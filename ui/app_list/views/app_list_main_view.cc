d70edeb33692782bc488ad8505fafd255b9053f3
tapted@chromium.org <tapted@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-14 11:21:52 +0000

    Fix app list DCHECKs getting hit when model updates occur during dragging
    The DCHECK from AppsGridView::UpdateDragFromItem() is changed
    to an early return. This is a valid codepath, because drag events
    can still arrive from the AppListItemView after a drag is cancelled,
    e.g., via Sync.
    
    AppsGridViewTest.MouseDragWithFolderDisabled is updated to
    include coverage for this codepath.
    
    A DCHECK was also hit in UpdateDragFromReparentItem(). This was not
    a valid code path. The problem was that the root grid view tried
    to cancel only its "part" of the drag, rather than forwarding the
    cancel request to the folder managing the drag.
    
    AppListMainViewTest.MouseDragItemOutOfFolderWithCancel is added to
    include coverage for this codepath.
    
    BUG=402784
    TEST=app_list_unittests
    
    Review URL: https://codereview.chromium.org/466693002
    
    Cr-Commit-Position: refs/heads/master@{#289525}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@289525 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/app_list/views/app_list_main_view.cc
+++ ui/app_list/views/app_list_main_view.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/app_list/app_list_view_delegate.h"
 #include "ui/app_list/pagination_model.h"
 #include "ui/app_list/search_box_model.h"
+#include "ui/app_list/views/app_list_folder_view.h"
 #include "ui/app_list/views/app_list_item_view.h"
 #include "ui/app_list/views/apps_container_view.h"
 #include "ui/app_list/views/apps_grid_view.h"
@@ -301,6 +302,13 @@ void AppListMainView::GetShortcutPathForApp(
     const std::string& app_id,
     const base::Callback<void(const base::FilePath&)>& callback) {
   delegate_->GetShortcutPathForApp(app_id, callback);
+}
+
+void AppListMainView::CancelDragInActiveFolder() {
+  contents_view_->apps_container_view()
+      ->app_list_folder_view()
+      ->items_grid_view()
+      ->EndDrag(true);
 }
 
 void AppListMainView::QueryChanged(SearchBoxView* sender) {
