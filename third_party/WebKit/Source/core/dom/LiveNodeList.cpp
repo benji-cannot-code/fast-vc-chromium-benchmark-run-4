af4189b411d67665da519bdeb4a86ce5479863bd
ch.dumez@samsung.com <ch.dumez@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-14 03:53:18 +0000

    Move matching Element traversal functions from LiveNodeListBase to ElementTraversal
    Move matching Element traversal functions from LiveNodeListBase to
    ElementTraversal and generalized them a bit so that they can be reused in the
    rest of the code base.
    
    LiveNodeListBase exists so that code can be shared between LiveNodeList and
    HTMLCollection. However, the ElementTraversal class is a better place to share
    DOM Tree traversal utility functions. It is quite common in the code base that
    we want to traverse Elements that match a specific rule so this will reduce
    code duplication once the rest of the code is ported to these new
    ElementTraversal functions.
    
    R=adamk@chromium.org, esprehn@chromium.org
    
    Review URL: https://codereview.chromium.org/468183002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180227 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/LiveNodeList.cpp
+++ third_party/WebKit/Source/core/dom/LiveNodeList.cpp
@@ -26,10 +26,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-static inline bool isMatchingElement(const LiveNodeList& nodeList, const Element& element)
-{
-    return nodeList.elementMatches(element);
-}
+namespace {
+
+class IsMatch {
+public:
+    IsMatch(const LiveNodeList& list)
+        : m_list(list)
+    { }
+
+    bool operator() (const Element& element) const
+    {
+        return m_list.elementMatches(element);
+    }
+
+private:
+    const LiveNodeList& m_list;
+};
+
+} // namespace
 
 Node* LiveNodeList::virtualOwnerNode() const
 {
@@ -53,22 +67,22 @@ Element* LiveNodeList::item(unsigned offset) const
 
 Element* LiveNodeList::traverseToFirstElement() const
 {
-    return firstMatchingElement(*this);
+    return ElementTraversal::firstWithin(rootNode(), IsMatch(*this));
 }
 
 Element* LiveNodeList::traverseToLastElement() const
 {
-    return lastMatchingElement(*this);
+    return ElementTraversal::lastWithin(rootNode(), IsMatch(*this));
 }
 
-Element* LiveNodeList::traverseForwardToOffset(unsigned offset, Element& currentNode, unsigned& currentOffset) const
+Element* LiveNodeList::traverseForwardToOffset(unsigned offset, Element& currentElement, unsigned& currentOffset) const
 {
-    return traverseMatchingElementsForwardToOffset(*this, offset, currentNode, currentOffset);
+    return traverseMatchingElementsForwardToOffset(currentElement, &rootNode(), offset, currentOffset, IsMatch(*this));
 }
 
-Element* LiveNodeList::traverseBackwardToOffset(unsigned offset, Element& currentNode, unsigned& currentOffset) const
+Element* LiveNodeList::traverseBackwardToOffset(unsigned offset, Element& currentElement, unsigned& currentOffset) const
 {
-    return traverseMatchingElementsBackwardToOffset(*this, offset, currentNode, currentOffset);
+    return traverseMatchingElementsBackwardToOffset(currentElement, &rootNode(), offset, currentOffset, IsMatch(*this));
 }
 
 void LiveNodeList::trace(Visitor* visitor)
