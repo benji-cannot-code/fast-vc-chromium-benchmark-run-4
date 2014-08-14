f06b3d9053b21e42313e5f0c58396cd5d80aae7d
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-14 06:47:06 +0000

    DevTools: keyboard navigation broken in the viewported Network panel
    BUG=403430
    R=eustas@chromium.org, lushnikov@chromium.org
    
    Review URL: https://codereview.chromium.org/468043004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180245 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ui/ViewportDataGrid.js
+++ third_party/WebKit/Source/devtools/front_end/ui/ViewportDataGrid.js
@@ -63,7 +63,8 @@ WebInspector.ViewportDataGrid.prototype = {
     /**
      * @protected
      */
-    scheduleUpdate: function() {
+    scheduleUpdate: function()
+    {
         if (this._updateScheduled)
             return;
         this._updateScheduled = true;
@@ -150,6 +151,28 @@ WebInspector.ViewportDataGrid.prototype = {
         this._visibleNodes = visibleNodes;
     },
 
+    /**
+     * @param {!WebInspector.ViewportDataGridNode} node
+     */
+    _revealViewportNode: function(node)
+    {
+        var nodes = this._rootNode.children;
+        var index = nodes.indexOf(node);
+        if (index === -1)
+            return;
+        var fromY = 0;
+        for (var i = 0; i < index; ++i)
+            fromY += nodes[i].nodeSelfHeight();
+        var toY = fromY + node.nodeSelfHeight();
+
+        var scrollTop = this._scrollContainer.scrollTop;
+        if (scrollTop > fromY)
+            scrollTop = fromY;
+        else if (scrollTop + this._scrollContainer.offsetHeight < toY)
+            scrollTop = toY - this._scrollContainer.offsetHeight;
+        this._scrollContainer.scrollTop = scrollTop;
+    },
+
     __proto__: WebInspector.DataGrid.prototype
 }
 
@@ -193,6 +216,7 @@ WebInspector.ViewportDataGridNode.prototype = {
      */
     insertChild: function(child, index)
     {
+        child.parent = this;
         child.dataGrid = this.dataGrid;
         this.children.splice(index, 0, child);
         child.recalculateSiblings(index);
@@ -259,6 +283,11 @@ WebInspector.ViewportDataGridNode.prototype = {
         this._element = null;
         return result;
      },
+
+    reveal: function()
+    {
+        this.dataGrid._revealViewportNode(this);
+    },
 
     __proto__: WebInspector.DataGridNode.prototype
 }
