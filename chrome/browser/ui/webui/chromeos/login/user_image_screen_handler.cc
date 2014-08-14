abaedab0ef622addb50f3b0849bc9c6efb33894a
dzhioev@chromium.org <dzhioev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-14 19:01:26 +0000

    [cleanup] Removed dead code from avatar selection screen.
    Params "user_id" and "profile_picture_enabled" were not used so I
    removed them and related code.
    Also one usage of GetProfileByUserUnsafe eliminated.
    
    BUG=361528
    TEST=browser_tests
    
    Review URL: https://codereview.chromium.org/466793002
    
    Cr-Commit-Position: refs/heads/master@{#289661}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@289661 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chromeos/login/user_image_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/user_image_screen_handler.cc
@@ -221,8 +221,4 @@ void UserImageScreenHandler::HideCurtain() {
   CallJS("hideCurtain");
 }
 
-void UserImageScreenHandler::SetProfilePictureEnabled(bool enabled) {
-  CallJS("setProfilePictureEnabled", enabled);
-}
-
 }  // namespace chromeos
--- chrome/browser/ui/webui/chromeos/login/user_image_screen_handler.h
+++ chrome/browser/ui/webui/chromeos/login/user_image_screen_handler.h
@@ -44,8 +44,6 @@ class UserImageScreenHandler : public UserImageScreenActor,
   virtual void SendProfileImage(const std::string& data_url) OVERRIDE;
   virtual void OnProfileImageAbsent() OVERRIDE;
 
-  virtual void SetProfilePictureEnabled(bool enabled) OVERRIDE;
-
   virtual void SetCameraPresent(bool enabled) OVERRIDE;
 
   virtual void HideCurtain() OVERRIDE;
