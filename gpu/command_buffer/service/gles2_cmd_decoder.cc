25e710beec2c912a92c6b3f5601b086427889841
oetuaho@nvidia.com <oetuaho@nvidia.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-14 15:20:56 +0000

    Consistently generate INVALID_VALUE on NaN line width
    GL drivers are allowed to generate errors when NaN is supplied to
    functions under GLES 3.0 section 2.1.1 or OpenGL 4.5 section 2.3.4.1.
    NVIDIA OpenGL driver generates an error when NaN is supplied to
    glLineWidth. Make this behavior consistent for Chromium, which removes
    the possibility of drawing with an undefined line width, and makes sure
    that errors don't leak from the GL driver.
    
    BUG=402847
    TEST=WebGL conformance tests on debug build with virtual contexts,
    gpu_unittests
    
    Review URL: https://codereview.chromium.org/469673003
    
    Cr-Commit-Position: refs/heads/master@{#289594}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@289594 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/gles2_cmd_decoder.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/debug/trace_event.h"
 #include "base/debug/trace_event_synthetic_delay.h"
+#include "base/float_util.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/numerics/safe_math.h"
 #include "base/strings/string_number_conversions.h"
