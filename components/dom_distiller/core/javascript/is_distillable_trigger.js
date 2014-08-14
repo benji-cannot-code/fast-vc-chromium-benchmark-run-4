e3f746142e16f6f6c614a4b43676610243743bec
smaslo@chromium.org <smaslo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-14 11:27:20 +0000

    Fixing small error with is_distilable_trigger.js
    Removing backslash that would cause JavaScript error.
    
    BUG=383630
    
    Review URL: https://codereview.chromium.org/474493003
    
    Cr-Commit-Position: refs/heads/master@{#289527}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@289527 0039d316-1c4b-4281-b951-d872f2087c98

--- components/dom_distiller/core/javascript/is_distillable_trigger.js
+++ components/dom_distiller/core/javascript/is_distillable_trigger.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 (function() {
   var elems = document.querySelectorAll(
-      'meta[property="og:type"],meta[name=\\"og:type\\"]');
+      'meta[property="og:type"],meta[name="og:type"]');
   for (var i in elems) {
     if (elems[i].content && elems[i].content.toUpperCase() == 'ARTICLE') {
       return true;
