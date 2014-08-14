58d2c8df2a26cfc73cf2d4a948a5a2d73df2b247
mvanouwerkerk@chromium.org <mvanouwerkerk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-14 19:47:17 +0000

    Rename various Geolocation bits for clarity and consistency.
    Review URL: https://codereview.chromium.org/477633002
    
    Cr-Commit-Position: refs/heads/master@{#289678}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@289678 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/geolocation/location_arbitrator_impl.cc
+++ content/browser/geolocation/location_arbitrator_impl.cc
@@ -29,9 +29,9 @@ const int64 LocationArbitratorImpl::kFixStaleTimeoutMilliseconds =
 
 LocationArbitratorImpl::LocationArbitratorImpl(
     const LocationUpdateCallback& callback)
-    : callback_(callback),
-      provider_callback_(
-          base::Bind(&LocationArbitratorImpl::LocationUpdateAvailable,
+    : arbitrator_update_callback_(callback),
+      provider_update_callback_(
+          base::Bind(&LocationArbitratorImpl::OnLocationUpdate,
                      base::Unretained(this))),
       position_provider_(NULL),
       is_permission_granted_(false),
@@ -117,15 +117,14 @@ void LocationArbitratorImpl::RegisterProvider(
     LocationProvider* provider) {
   if (!provider)
     return;
-  provider->SetUpdateCallback(provider_callback_);
+  provider->SetUpdateCallback(provider_update_callback_);
   if (is_permission_granted_)
     provider->OnPermissionGranted();
   providers_.push_back(provider);
 }
 
-void LocationArbitratorImpl::LocationUpdateAvailable(
-    const LocationProvider* provider,
-    const Geoposition& new_position) {
+void LocationArbitratorImpl::OnLocationUpdate(const LocationProvider* provider,
+                                              const Geoposition& new_position) {
   DCHECK(new_position.Validate() ||
          new_position.error_code != Geoposition::ERROR_CODE_NONE);
   if (!IsNewPositionBetter(position_, new_position,
@@ -133,7 +132,7 @@ void LocationArbitratorImpl::LocationUpdateAvailable(
     return;
   position_provider_ = provider;
   position_ = new_position;
-  callback_.Run(position_);
+  arbitrator_update_callback_.Run(position_);
 }
 
 AccessTokenStore* LocationArbitratorImpl::NewAccessTokenStore() {
--- content/browser/geolocation/network_location_request.cc
+++ content/browser/geolocation/network_location_request.cc
@@ -104,9 +104,7 @@ NetworkLocationRequest::NetworkLocationRequest(
     net::URLRequestContextGetter* context,
     const GURL& url,
     LocationResponseCallback callback)
-        : url_context_(context),
-          callback_(callback),
-          url_(url) {
+    : url_context_(context), location_response_callback_(callback), url_(url) {
 }
 
 NetworkLocationRequest::~NetworkLocationRequest() {
@@ -123,7 +121,7 @@ bool NetworkLocationRequest::MakeRequest(const base::string16& access_token,
     url_fetcher_.reset();
   }
   wifi_data_ = wifi_data;
-  timestamp_ = timestamp;
+  wifi_data_timestamp_ = timestamp;
 
   GURL request_url = FormRequestURL(url_);
   url_fetcher_.reset(net::URLFetcher::Create(
@@ -137,7 +135,7 @@ bool NetworkLocationRequest::MakeRequest(const base::string16& access_token,
       net::LOAD_DO_NOT_SAVE_COOKIES | net::LOAD_DO_NOT_SEND_COOKIES |
       net::LOAD_DO_NOT_SEND_AUTH_DATA);
 
-  start_time_ = base::TimeTicks::Now();
+  request_start_time_ = base::TimeTicks::Now();
   url_fetcher_->Start();
   return true;
 }
@@ -157,7 +155,7 @@ void NetworkLocationRequest::OnURLFetchComplete(
   GetLocationFromResponse(status.is_success(),
                           response_code,
                           data,
-                          timestamp_,
+                          wifi_data_timestamp_,
                           source->GetURL(),
                           &position,
                           &access_token);
@@ -166,7 +164,8 @@ void NetworkLocationRequest::OnURLFetchComplete(
   url_fetcher_.reset();
 
   if (!server_error) {
-    const base::TimeDelta request_time = base::TimeTicks::Now() - start_time_;
+    const base::TimeDelta request_time =
+        base::TimeTicks::Now() - request_start_time_;
 
     UMA_HISTOGRAM_CUSTOM_TIMES(
         "Net.Wifi.LbsLatency",
@@ -177,7 +176,8 @@ void NetworkLocationRequest::OnURLFetchComplete(
   }
 
   DVLOG(1) << "NetworkLocationRequest::OnURLFetchComplete() : run callback.";
-  callback_.Run(position, server_error, access_token, wifi_data_);
+  location_response_callback_.Run(
+      position, server_error, access_token, wifi_data_);
 }
 
 // Local functions.
