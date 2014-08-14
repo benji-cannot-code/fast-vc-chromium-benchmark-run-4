5cee2f8cfd6eece8a709c7f5730ec920dbe7326c
Gordana.Cmiljanovic@imgtec.com <Gordana.Cmiljanovic@imgtec.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-14 17:14:05 +0000

    Adding atomic ops for MIPS64.
    This change adds atomic operations for MIPS64.
    
    BUG=400684
    
    Review URL: https://codereview.chromium.org/450343002
    
    Cr-Commit-Position: refs/heads/master@{#289624}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@289624 0039d316-1c4b-4281-b951-d872f2087c98

--- build/build_config.h
+++ build/build_config.h
@@ -114,10 +114,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ARCH_CPU_32_BITS 1
 #define ARCH_CPU_LITTLE_ENDIAN 1
 #elif defined(__MIPSEL__)
+#if defined(__LP64__)
+#define ARCH_CPU_MIPS64_FAMILY 1
+#define ARCH_CPU_MIPS64EL 1
+#define ARCH_CPU_64_BITS 1
+#define ARCH_CPU_LITTLE_ENDIAN 1
+#else
 #define ARCH_CPU_MIPS_FAMILY 1
 #define ARCH_CPU_MIPSEL 1
 #define ARCH_CPU_32_BITS 1
 #define ARCH_CPU_LITTLE_ENDIAN 1
+#endif
 #else
 #error Please add support for your architecture in build/build_config.h
 #endif
