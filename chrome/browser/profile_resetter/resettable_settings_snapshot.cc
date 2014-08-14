f51439df2b748d0a030914ddb796d091e1362bdf
battre@chromium.org <battre@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-14 08:44:27 +0000

    Add show_home_button value to reset feedback reports
    BUG=235037
    
    Review URL: https://codereview.chromium.org/465393002
    
    Cr-Commit-Position: refs/heads/master@{#289494}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@289494 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profile_resetter/resettable_settings_snapshot.cc
+++ chrome/browser/profile_resetter/resettable_settings_snapshot.cc
@@ -39,6 +39,7 @@ const char kEnabledExtensions[] = "enabled_extensions";
 const char kHomepageIsNewTabPage[] = "homepage_is_ntp";
 const char kHomepagePath[] = "homepage";
 const char kShortcuts[] = "shortcuts";
+const char kShowHomeButton[] = "show_home_button";
 const char kStartupTypePath[] = "startup_type";
 const char kStartupURLPath[] = "startup_urls";
 
@@ -67,6 +68,7 @@ ResettableSettingsSnapshot::ResettableSettingsSnapshot(
   DCHECK(prefs);
   homepage_ = prefs->GetString(prefs::kHomePage);
   homepage_is_ntp_ = prefs->GetBoolean(prefs::kHomePageIsNewTabPage);
+  show_home_button_ = prefs->GetBoolean(prefs::kShowHomeButton);
 
   TemplateURLService* service =
       TemplateURLServiceFactory::GetForProfile(profile);
@@ -111,7 +113,8 @@ int ResettableSettingsSnapshot::FindDifferentFields(
     bit_mask |= STARTUP_MODE;
 
   if (homepage_is_ntp_ != snapshot.homepage_is_ntp_ ||
-      homepage_ != snapshot.homepage_)
+      homepage_ != snapshot.homepage_ ||
+      show_home_button_ != snapshot.show_home_button_)
     bit_mask |= HOMEPAGE;
 
   if (dse_url_ != snapshot.dse_url_)
@@ -174,6 +177,7 @@ std::string SerializeSettingsReport(const ResettableSettingsSnapshot& snapshot,
   if (field_mask & ResettableSettingsSnapshot::HOMEPAGE) {
     dict.SetString(kHomepagePath, snapshot.homepage());
     dict.SetBoolean(kHomepageIsNewTabPage, snapshot.homepage_is_ntp());
+    dict.SetBoolean(kShowHomeButton, snapshot.show_home_button());
   }
 
   if (field_mask & ResettableSettingsSnapshot::DSE_URL)
@@ -295,6 +299,14 @@ scoped_ptr<base::ListValue> GetReadableFeedbackForSnapshot(
   AddPair(list.get(),
           l10n_util::GetStringUTF16(IDS_RESET_PROFILE_SETTINGS_HOMEPAGE_IS_NTP),
           l10n_util::GetStringUTF16(is_ntp_message_id));
+
+  int show_home_button_id = snapshot.show_home_button() ?
+      IDS_RESET_PROFILE_SETTINGS_SHOW_HOME_BUTTON_TRUE :
+      IDS_RESET_PROFILE_SETTINGS_SHOW_HOME_BUTTON_FALSE;
+  AddPair(
+      list.get(),
+      l10n_util::GetStringUTF16(IDS_RESET_PROFILE_SETTINGS_SHOW_HOME_BUTTON),
+      l10n_util::GetStringUTF16(show_home_button_id));
 
   TemplateURLService* service =
       TemplateURLServiceFactory::GetForProfile(profile);
