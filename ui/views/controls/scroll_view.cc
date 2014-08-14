6cb7ae50305de6efca0d3227f912fe998d67145f
andresantoso@chromium.org <andresantoso@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-14 07:56:02 +0000

    For the Mac port of views, we need to draw the corner part to connect the vertical and horizontal tracks together. Update ScrollView to allow the native theme to paint the corner square when both horizontal and vertical scrollbars are showing. This is done by installing a view at the corner and have it call NativeTheme::Paint(..).
    BUG=401346
    
    Review URL: https://codereview.chromium.org/462373002
    
    Cr-Commit-Position: refs/heads/master@{#289483}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@289483 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/scroll_view.cc
+++ ui/views/controls/scroll_view.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "ui/events/event.h"
+#include "ui/gfx/canvas.h"
 #include "ui/native_theme/native_theme.h"
 #include "ui/views/border.h"
 #include "ui/views/controls/scrollbar/native_scroll_bar.h"
@@ -32,6 +33,23 @@ class ScrollViewWithBorder : public views::ScrollView {
 
  private:
   DISALLOW_COPY_AND_ASSIGN(ScrollViewWithBorder);
+};
+
+class ScrollCornerView : public views::View {
+ public:
+  ScrollCornerView() {}
+
+  virtual void OnPaint(gfx::Canvas* canvas) OVERRIDE {
+    ui::NativeTheme::ExtraParams ignored;
+    GetNativeTheme()->Paint(canvas->sk_canvas(),
+                            ui::NativeTheme::kScrollbarCorner,
+                            ui::NativeTheme::kNormal,
+                            GetLocalBounds(),
+                            ignored);
+  }
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(ScrollCornerView);
 };
 
 // Returns the position for the view so that it isn't scrolled off the visible
@@ -110,7 +128,7 @@ ScrollView::ScrollView()
       header_viewport_(new Viewport()),
       horiz_sb_(new NativeScrollBar(true)),
       vert_sb_(new NativeScrollBar(false)),
-      resize_corner_(NULL),
+      corner_view_(new ScrollCornerView()),
       min_height_(-1),
       max_height_(-1),
       hide_horizontal_scrollbar_(false) {
@@ -125,8 +143,7 @@ ScrollView::ScrollView()
   horiz_sb_->set_controller(this);
   vert_sb_->SetVisible(false);
   vert_sb_->set_controller(this);
-  if (resize_corner_)
-    resize_corner_->SetVisible(false);
+  corner_view_->SetVisible(false);
 }
 
 ScrollView::~ScrollView() {
@@ -134,9 +151,7 @@ ScrollView::~ScrollView() {
   // deleted.
   delete horiz_sb_;
   delete vert_sb_;
-
-  if (resize_corner_ && !resize_corner_->parent())
-    delete resize_corner_;
+  delete corner_view_;
 }
 
 // static
@@ -269,12 +284,11 @@ void ScrollView::Layout() {
                                 &horiz_sb_required,
                                 &vert_sb_required);
   }
-  bool resize_corner_required = resize_corner_ && horiz_sb_required &&
-                                vert_sb_required;
+  bool corner_view_required = horiz_sb_required && vert_sb_required;
   // Take action.
   SetControlVisibility(horiz_sb_, horiz_sb_required);
   SetControlVisibility(vert_sb_, vert_sb_required);
-  SetControlVisibility(resize_corner_, resize_corner_required);
+  SetControlVisibility(corner_view_, corner_view_required);
 
   // Non-default.
   if (horiz_sb_required) {
@@ -302,12 +316,12 @@ void ScrollView::Layout() {
                         vert_sb_width + width_offset,
                         viewport_bounds.bottom());
   }
-  if (resize_corner_required) {
+  if (corner_view_required) {
     // Show the resize corner.
-    resize_corner_->SetBounds(viewport_bounds.right(),
-                              viewport_bounds.bottom(),
-                              vert_sb_width,
-                              horiz_sb_height);
+    corner_view_->SetBounds(viewport_bounds.right(),
+                            viewport_bounds.bottom(),
+                            vert_sb_width,
+                            horiz_sb_height);
   }
 
   // Update to the real client size with the visible scrollbars.
