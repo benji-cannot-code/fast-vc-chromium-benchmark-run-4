d49c8b1ea6bd3dd590441bb844a543bf352a929a
noms@chromium.org <noms@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-15 19:05:25 +0000

    [Profiles] Downloading high res avatars should do nothing on non-desktop
    BUG=404066
    
    Review URL: https://codereview.chromium.org/476133002
    
    Cr-Commit-Position: refs/heads/master@{#289964}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@289964 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_info_cache_unittest.cc
+++ chrome/browser/profiles/profile_info_cache_unittest.cc
@@ -533,6 +533,8 @@ TEST_F(ProfileInfoCacheTest, AddStubProfile) {
     ASSERT_FALSE(names[i].empty());
 }
 
+// High res avatar downloading is only supported on desktop.
+#if !defined(OS_ANDROID) && !defined(OS_IOS) && !defined(OS_CHROMEOS)
 TEST_F(ProfileInfoCacheTest, DownloadHighResAvatarTest) {
   switches::EnableNewAvatarMenuForTesting(CommandLine::ForCurrentProcess());
 
@@ -586,3 +588,4 @@ TEST_F(ProfileInfoCacheTest, DownloadHighResAvatarTest) {
   EXPECT_TRUE(base::DeleteFile(icon_path, true));
   EXPECT_FALSE(base::PathExists(icon_path));
 }
+#endif
