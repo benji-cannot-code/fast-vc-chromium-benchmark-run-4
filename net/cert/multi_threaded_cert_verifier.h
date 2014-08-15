f30afdc534d7cfc31d8dc716aad3d615b2dfede7
hwennborg@google.com <hwennborg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-15 19:27:22 +0000

    Mark two inner classes in net/ exported
    The inner class needs to be exported in case any uses in inline methods
    or vtable of the outer class causes it to potentially be referenced across
    a shared library boundary.
    
    On non-Windows, the visibility attribute is already inherited by nested
    classes, but on Windows, they must be marked explicitly.
    
    BUG=82385
    R=thakis@chromium.org, willchan@chromium.org
    
    Review URL: https://codereview.chromium.org/476013002
    
    Cr-Commit-Position: refs/heads/master@{#289977}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@289977 0039d316-1c4b-4281-b951-d872f2087c98

--- net/cert/multi_threaded_cert_verifier.h
+++ net/cert/multi_threaded_cert_verifier.h
@@ -97,7 +97,7 @@ class NET_EXPORT_PRIVATE MultiThreadedCertVerifier
   };
 
   // CachedResult contains the result of a certificate verification.
-  struct CachedResult {
+  struct NET_EXPORT_PRIVATE CachedResult {
     CachedResult();
     ~CachedResult();
 
