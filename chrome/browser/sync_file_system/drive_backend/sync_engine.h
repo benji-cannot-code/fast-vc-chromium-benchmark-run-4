2f130ac10106dde00a1b87368457adf13f43fbc8
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-15 16:41:12 +0000

    [SyncFS] Add an injection point to DriveService creation at SyncEngine::Initialize
    Add an injection point for testing.
    
    BUG=402356
    
    Review URL: https://codereview.chromium.org/476053002
    
    Cr-Commit-Position: refs/heads/master@{#289918}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@289918 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_backend/sync_engine.h
+++ chrome/browser/sync_file_system/drive_backend/sync_engine.h
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/network_change_notifier.h"
 
 class ExtensionServiceInterface;
-class ProfileOAuth2TokenService;
+class OAuth2TokenService;
 
 namespace base {
 class SequencedTaskRunner;
@@ -65,6 +65,19 @@ class SyncEngine : public RemoteFileSyncService,
                    public SigninManagerBase::Observer {
  public:
   typedef RemoteFileSyncService::Observer SyncServiceObserver;
+
+  class DriveServiceFactory {
+   public:
+    DriveServiceFactory() {}
+    virtual ~DriveServiceFactory() {}
+    virtual scoped_ptr<drive::DriveServiceInterface> CreateDriveService(
+        OAuth2TokenService* oauth2_token_service,
+        net::URLRequestContextGetter* url_request_context_getter,
+        base::SequencedTaskRunner* blocking_task_runner);
+
+   private:
+    DISALLOW_COPY_AND_ASSIGN(DriveServiceFactory);
+  };
 
   static scoped_ptr<SyncEngine> CreateForBrowserContext(
       content::BrowserContext* context,
@@ -155,8 +168,9 @@ class SyncEngine : public RemoteFileSyncService,
              drive::DriveNotificationManager* notification_manager,
              ExtensionServiceInterface* extension_service,
              SigninManagerBase* signin_manager,
-             ProfileOAuth2TokenService* token_service,
+             OAuth2TokenService* token_service,
              net::URLRequestContextGetter* request_context,
+             scoped_ptr<DriveServiceFactory> drive_service_factory,
              leveldb::Env* env_override);
 
   // Called by WorkerObserver.
@@ -184,9 +198,11 @@ class SyncEngine : public RemoteFileSyncService,
   drive::DriveNotificationManager* notification_manager_;
   ExtensionServiceInterface* extension_service_;
   SigninManagerBase* signin_manager_;
-  ProfileOAuth2TokenService* token_service_;
+  OAuth2TokenService* token_service_;
 
   scoped_refptr<net::URLRequestContextGetter> request_context_;
+
+  scoped_ptr<DriveServiceFactory> drive_service_factory_;
 
   scoped_ptr<drive::DriveServiceInterface> drive_service_;
   scoped_ptr<DriveServiceWrapper> drive_service_wrapper_;
