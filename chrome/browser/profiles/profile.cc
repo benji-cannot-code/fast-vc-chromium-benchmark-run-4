1e39b62cd1ba24f6b36b72e0e7ee8d0e0265be80
noms@chromium.org <noms@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-15 21:48:44 +0000

    [Mac, Win] Only show the user manager tutorial if "See what's new" is clicked.
    - remove the profile preference that used to keep track of showing the
    tutorial any time the user manager was shown for a particular profile.
    
    - show the upgrade bubble for local profiles as well (so that local profiles
    have a chance to see the tutorial)
    
    - for the local profiles upgrade bubble, don't show the "Not you?" link as
    it doesn't really make sense.
    
    BUG=399679
    TBR=rpetterson@chromium.org
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=288817
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=289005
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=289636
    
    Review URL: https://codereview.chromium.org/454153002
    
    Cr-Commit-Position: refs/heads/master@{#290021}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290021 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile.cc
+++ chrome/browser/profiles/profile.cc
@@ -203,10 +203,6 @@ void Profile::RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
       prefs::kProfileAvatarTutorialShown,
       0,
       user_prefs::PrefRegistrySyncable::UNSYNCABLE_PREF);
-  registry->RegisterBooleanPref(
-      prefs::kProfileUserManagerTutorialShown,
-      false,
-      user_prefs::PrefRegistrySyncable::UNSYNCABLE_PREF);
 #endif
 }
 
