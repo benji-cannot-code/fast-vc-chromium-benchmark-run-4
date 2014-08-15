680676f2f3028b5c922b15a874901a45996dd3b6
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-15 07:36:36 +0000

    Make profiling lock global rather than per Target
    We should pause any actions that could affect performance if any of the targets is being profiled, not only current target.
    
    BUG=403684
    R=sergeyv@chromium.org
    
    Review URL: https://codereview.chromium.org/475803002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180336 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sdk/DOMModel.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/DOMModel.js
@@ -950,7 +950,7 @@ WebInspector.DOMModel = function(target) {
     this._highlighter = this._defaultHighlighter;
 
     if (WebInspector.experimentsSettings.disableAgentsWhenProfile.isEnabled())
-        target.profilingLock.addEventListener(WebInspector.Lock.Events.StateChanged, this._profilingStateChanged, this);
+        WebInspector.profilingLock.addEventListener(WebInspector.Lock.Events.StateChanged, this._profilingStateChanged, this);
 
     this._agent.enable();
 }
@@ -971,7 +971,7 @@ WebInspector.DOMModel.Events = {
 WebInspector.DOMModel.prototype = {
     _profilingStateChanged: function()
     {
-        if (this.target().profilingLock.isAcquired())
+        if (WebInspector.profilingLock.isAcquired())
             this._agent.disable();
         else
             this._agent.enable();
--- third_party/WebKit/Source/devtools/front_end/sdk/DebuggerModel.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/DebuggerModel.js
@@ -56,7 +56,7 @@ WebInspector.DebuggerModel = function(target)
     WebInspector.settings.pauseOnCaughtException.addChangeListener(this._pauseOnExceptionStateChanged, this);
 
     WebInspector.settings.enableAsyncStackTraces.addChangeListener(this._asyncStackTracesStateChanged, this);
-    target.profilingLock.addEventListener(WebInspector.Lock.Events.StateChanged, this._profilingStateChanged, this);
+    WebInspector.profilingLock.addEventListener(WebInspector.Lock.Events.StateChanged, this._profilingStateChanged, this);
 
     this.enableDebugger();
 
@@ -172,7 +172,7 @@ WebInspector.DebuggerModel.prototype = {
     _profilingStateChanged: function()
     {
         if (WebInspector.experimentsSettings.disableAgentsWhenProfile.isEnabled()) {
-            if (this.target().profilingLock.isAcquired())
+            if (WebInspector.profilingLock.isAcquired())
                 this.disableDebugger();
             else
                 this.enableDebugger();
@@ -183,7 +183,7 @@ WebInspector.DebuggerModel.prototype = {
     _asyncStackTracesStateChanged: function()
     {
         const maxAsyncStackChainDepth = 4;
-        var enabled = WebInspector.settings.enableAsyncStackTraces.get() && !this.target().profilingLock.isAcquired();
+        var enabled = WebInspector.settings.enableAsyncStackTraces.get() && !WebInspector.profilingLock.isAcquired();
         this._agent.setAsyncCallStackDepth(enabled ? maxAsyncStackChainDepth : 0);
     },
 
