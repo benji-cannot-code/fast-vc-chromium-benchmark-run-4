2ad650d684f60963f7de60d8ad2edcaa6dbf6f84
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-15 00:28:41 +0000

    Handle tries of results data in upload (fix _is_directory) for TestResultsServer
    The file handler was getting confused when we would upload a full_results.json
    that contained a "results" key that pointed to a trie of tests.
    
    Also fix some file permissions issues that the presubmit check complained about.
    
    R=ojan@chromium.org
    BUG=403663
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/475503005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180310 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/TestResultServer/model/jsonresults.py
+++ third_party/WebKit/Tools/TestResultServer/model/jsonresults.py
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+import collections
 import json
 import logging
 import re
@@ -111,7 +112,7 @@ FAILURE_TO_CHAR = {
 CHAR_TO_FAILURE = dict((value, key) for key, value in FAILURE_TO_CHAR.items())
 
 def _is_directory(subtree):
-    return RESULTS_KEY not in subtree
+    return RESULTS_KEY not in subtree or not isinstance(subtree[RESULTS_KEY], collections.Sequence)
 
 
 class JsonResults(object):
