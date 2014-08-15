41e901b15e6ff2cf12aca9b24761d8a5a7c42845
damienv@chromium.org <damienv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-15 18:25:57 +0000

    Mpeg2 TS - Fail when no valid timestamp in the ADTS parser.
    BUG=None
    
    Review URL: https://codereview.chromium.org/399433003
    
    Cr-Commit-Position: refs/heads/master@{#289944}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@289944 0039d316-1c4b-4281-b951-d872f2087c98

--- media/formats/mp2t/es_parser_adts.h
+++ media/formats/mp2t/es_parser_adts.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/time/time.h"
 #include "media/base/audio_decoder_config.h"
+#include "media/base/media_export.h"
 #include "media/formats/mp2t/es_parser.h"
 
 namespace media {
@@ -26,7 +27,7 @@ class StreamParserBuffer;
 namespace media {
 namespace mp2t {
 
-class EsParserAdts : public EsParser {
+class MEDIA_EXPORT EsParserAdts : public EsParser {
  public:
   typedef base::Callback<void(const AudioDecoderConfig&)> NewAudioConfigCB;
 
