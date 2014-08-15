c486b5b8c59867e1303421b344c1d5368fa37048
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-15 19:47:27 +0000

    athena: A simpler implementation of WindowListProvider.
    Introduce WindowListProviderImpl that simply works with the stacking order of
    child windows of the container. This changes the behaviour of repeated bezel
    swipes from the left edge to swipe between the last two active windows, which
    is the desired behaviour.
    
    BUG=403444
    R=mfomitchev@chromium.org, oshima@chromium.org
    
    Review URL: https://codereview.chromium.org/470083004
    
    Cr-Commit-Position: refs/heads/master@{#289984}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@289984 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ athena/wm/window_list_provider_impl.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "athena/wm/window_list_provider_impl.h"
+
+#include "ui/aura/window.h"
+
+namespace athena {
+
+WindowListProviderImpl::WindowListProviderImpl(aura::Window* container)
+    : container_(container) {
+  CHECK(container_);
+}
+
+WindowListProviderImpl::~WindowListProviderImpl() {
+}
+
+aura::Window::Windows WindowListProviderImpl::GetWindowList() const {
+  aura::Window::Windows list;
+  const aura::Window::Windows& container_children = container_->children();
+  for (aura::Window::Windows::const_iterator iter = container_children.begin();
+       iter != container_children.end();
+       ++iter) {
+    if ((*iter)->type() == ui::wm::WINDOW_TYPE_NORMAL)
+      list.push_back(*iter);
+  }
+  return list;
+}
+
+}  // namespace athena
---
+++ athena/wm/window_list_provider_impl.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ATHENA_WM_WINDOW_LIST_PROVIDER_IMPL_H_
+#define ATHENA_WM_WINDOW_LIST_PROVIDER_IMPL_H_
+
+#include "athena/wm/public/window_list_provider.h"
+
+namespace athena {
+
+// This implementation of the WindowListProviderImpl uses the same order as in
+// the container window's stacking order.
+class ATHENA_EXPORT WindowListProviderImpl : public WindowListProvider {
+ public:
+  explicit WindowListProviderImpl(aura::Window* container);
+  virtual ~WindowListProviderImpl();
+
+ private:
+  // WindowListProvider:
+  virtual aura::Window::Windows GetWindowList() const OVERRIDE;
+
+  aura::Window* container_;
+
+  DISALLOW_COPY_AND_ASSIGN(WindowListProviderImpl);
+};
+
+}  // namespace athena
+
+#endif  // ATHENA_WM_WINDOW_LIST_PROVIDER_IMPL_H_
