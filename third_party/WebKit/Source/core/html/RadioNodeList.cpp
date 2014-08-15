1452a16426cd2e0dce5d0c542c77e6830687a813
ch.dumez@samsung.com <ch.dumez@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-15 16:08:00 +0000

    Use tighter typing in RadioNodeList::elementMatches()
    Use tighter typing in RadioNodeList::elementMatches() to give C++11 compilers
    a chance to devirtualize the call to HTMLImageElement::formOwner().
    
    R=adamk@chromium.org
    
    Review URL: https://codereview.chromium.org/475613003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180369 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/RadioNodeList.cpp
+++ third_party/WebKit/Source/core/html/RadioNodeList.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/Element.h"
 #include "core/dom/NodeRareData.h"
 #include "core/html/HTMLFormElement.h"
+#include "core/html/HTMLImageElement.h"
 #include "core/html/HTMLInputElement.h"
 #include "core/html/HTMLObjectElement.h"
 
@@ -115,7 +116,7 @@ bool RadioNodeList::elementMatches(const Element& element) const
         if (!isHTMLImageElement(element))
             return false;
 
-        if (toHTMLElement(element).formOwner() != ownerNode())
+        if (toHTMLImageElement(element).formOwner() != ownerNode())
             return false;
 
         return matchesByIdOrName(element);
