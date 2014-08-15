d33e99d37d5bb0d31ef181cdcc2d5aef6423aac8
jeun@chromium.org <jeun@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-15 18:36:25 +0000

    [Findit] Plain objects to represent the returned result from running the algorithm,
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/421223003
    
    Cr-Commit-Position: refs/heads/master@{#289949}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@289949 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/findit/result.py
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+class Result(object):
+
+  def __init__(self, suspected_cl, revision_url, component_name, author,
+               reason, review_url, reviewers, line_content):
+    self.suspected_cl = suspected_cl
+    self.revision_url = revision_url
+    self.component_name = component_name
+    self.author = author
+    self.reason = reason
+    self.review_url = review_url
+    self.reviewers = reviewers
+    self.line_content = line_content
