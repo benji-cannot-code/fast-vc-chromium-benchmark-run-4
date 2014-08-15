41e226082d8c489e86dea376228717d820661d03
miu@chromium.org <miu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-15 21:30:58 +0000

    Revert of ChromeOS: should not show "Language changed" notification for certain languages. (patchset #6 of https://codereview.chromium.org/382973002/)
    Reason for revert:
    Link failure of browser_tests on ChromiumOS build bot:
    
    http://build.chromium.org/p/chromium.chromiumos/buildstatus?builder=Linux%20ChromiumOS%20Builder%20%28dbg%29&number=50192
    
    
    Original issue's description:
    > ChromeOS: should not show "Language changed" notification for certain languages.
    > 
    > BUG=317718
    > TEST=manually tested
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=289985
    
    NOTREECHECKS=true
    NOTRY=true
    TBR=alemate@chromium.org, nkostylev@chromium.org, jshin@chromium.org
    
    Review URL: https://codereview.chromium.org/471403005
    
    Cr-Commit-Position: refs/heads/master@{#290017}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290017 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/locale_change_guard.h
+++ chrome/browser/chromeos/locale_change_guard.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/system/locale/locale_observer.h"
 #include "base/compiler_specific.h"
-#include "base/gtest_prod_util.h"
 #include "base/lazy_instance.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
@@ -49,11 +48,6 @@ class LocaleChangeGuard : public content::NotificationObserver,
   void OnLogin();
 
  private:
-  FRIEND_TEST_ALL_PREFIXES(LocaleChangeGuardTest,
-                           ShowNotificationLocaleChanged);
-  FRIEND_TEST_ALL_PREFIXES(LocaleChangeGuardTest,
-                           ShowNotificationLocaleChangedList);
-
   void RevertLocaleChangeCallback(const base::ListValue* list);
   void Check();
 
@@ -61,13 +55,6 @@ class LocaleChangeGuard : public content::NotificationObserver,
   virtual void Observe(int type,
                        const content::NotificationSource& source,
                        const content::NotificationDetails& details) OVERRIDE;
-
-  // Returns true if we should notify user about automatic locale change.
-  static bool ShouldShowLocaleChangeNotification(const std::string& from_locale,
-                                                 const std::string& to_locale);
-
-  static const char* const* GetSkipShowNotificationLanguagesForTesting();
-  static size_t GetSkipShowNotificationLanguagesSizeForTesting();
 
   std::string from_locale_;
   std::string to_locale_;
