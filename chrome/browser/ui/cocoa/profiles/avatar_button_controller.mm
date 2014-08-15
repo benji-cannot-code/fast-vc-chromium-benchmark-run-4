a90230804d696d50c24d5cb031965437e46feca5
noms@chromium.org <noms@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-15 23:52:54 +0000

    [Mac] Add accessibility features to the new avatar menu
    BUG=401834
    
    Review URL: https://codereview.chromium.org/471703002
    
    Cr-Commit-Position: refs/heads/master@{#290055}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290055 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/profiles/avatar_button_controller.mm
+++ chrome/browser/ui/cocoa/profiles/avatar_button_controller.mm
@@ -14,9 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window.h"
 #import "chrome/browser/ui/cocoa/browser_window_controller.h"
 #include "components/signin/core/browser/signin_error_controller.h"
+#include "grit/generated_resources.h"
 #include "grit/theme_resources.h"
 #import "ui/base/cocoa/appkit_utils.h"
 #import "ui/base/cocoa/hover_image_button.h"
+#include "ui/base/l10n/l10n_util_mac.h"
 #include "ui/base/nine_image_painter_factory.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/image/image_skia_operations.h"
@@ -52,7 +54,7 @@ NSImage* GetImageFromResourceID(int resourceId) {
    base::scoped_nsobject<NSImage> authenticationErrorImage_;
 }
 - (void)setIsThemedWindow:(BOOL)isThemedWindow;
-- (void)setHasError:(BOOL)hasError;
+- (void)setHasError:(BOOL)hasError withTitle:(NSString*)title;
 
 @end
 
@@ -132,13 +134,19 @@ NSImage* GetImageFromResourceID(int resourceId) {
   isThemedWindow_ = isThemedWindow;
 }
 
-- (void)setHasError:(BOOL)hasError {
+- (void)setHasError:(BOOL)hasError withTitle:(NSString*)title {
   if (hasError) {
     authenticationErrorImage_.reset(
         [ui::ResourceBundle::GetSharedInstance().GetImageNamed(
             IDR_ICON_PROFILES_AVATAR_BUTTON_ERROR).ToNSImage() retain]);
+    [self accessibilitySetOverrideValue:l10n_util::GetNSStringF(
+        IDS_PROFILES_ACCOUNT_BUTTON_AUTH_ERROR_ACCESSIBLE_NAME,
+        base::SysNSStringToUTF16(title))
+                           forAttribute:NSAccessibilityTitleAttribute];
   } else {
     authenticationErrorImage_.reset();
+    [self accessibilitySetOverrideValue:title
+                          forAttribute:NSAccessibilityTitleAttribute];
   }
 }
 
@@ -173,8 +181,7 @@ NSImage* GetImageFromResourceID(int resourceId) {
         [[CustomThemeButtonCell alloc] initWithThemedWindow:isThemedWindow_]);
     SigninErrorController* errorController =
         profiles::GetSigninErrorController(browser->profile());
-    if (errorController)
-      [cell setHasError:errorController->HasError()];
+
     [button_ setCell:cell.get()];
     [self setView:button_];
 
@@ -190,6 +197,8 @@ NSImage* GetImageFromResourceID(int resourceId) {
     [button_ setAction:@selector(buttonClicked:)];
 
     [self updateAvatarButtonAndLayoutParent:NO];
+    if (errorController)
+      [cell setHasError:errorController->HasError() withTitle:[button_ title]];
 
     NSNotificationCenter* center = [NSNotificationCenter defaultCenter];
     [center addObserver:self
@@ -263,7 +272,7 @@ NSImage* GetImageFromResourceID(int resourceId) {
 }
 
 - (void)updateErrorStatus:(BOOL)hasError {
-  [[button_ cell] setHasError:hasError];
+  [[button_ cell] setHasError:hasError withTitle:[button_ title]];
   [self updateAvatarButtonAndLayoutParent:YES];
 }
 
--- chrome/browser/ui/cocoa/profiles/profile_chooser_controller.mm
+++ chrome/browser/ui/cocoa/profiles/profile_chooser_controller.mm
@@ -487,6 +487,32 @@ class ActiveProfileObserverBridge : public AvatarMenuObserver,
       // Hide the button until the image is hovered over.
       [changePhotoButton_ setHidden:YES];
     }
+
+    // Set the image cell's accessibility strings to be the same as the
+    // button's strings.
+    [[self cell] accessibilitySetOverrideValue:l10n_util::GetNSString(
+        editingAllowed ?
+        IDS_PROFILES_NEW_AVATAR_MENU_CHANGE_PHOTO_ACCESSIBLE_NAME :
+        IDS_PROFILES_NEW_AVATAR_MENU_PHOTO_ACCESSIBLE_NAME)
+                                  forAttribute:NSAccessibilityTitleAttribute];
+    [[self cell] accessibilitySetOverrideValue:
+        editingAllowed ? NSAccessibilityButtonRole : NSAccessibilityImageRole
+                                  forAttribute:NSAccessibilityRoleAttribute];
+    [[self cell] accessibilitySetOverrideValue:
+        NSAccessibilityRoleDescription(NSAccessibilityButtonRole, nil)
+            forAttribute:NSAccessibilityRoleDescriptionAttribute];
+
+    // The button and the cell should read the same thing.
+    [self accessibilitySetOverrideValue:l10n_util::GetNSString(
+        editingAllowed ?
+        IDS_PROFILES_NEW_AVATAR_MENU_CHANGE_PHOTO_ACCESSIBLE_NAME :
+        IDS_PROFILES_NEW_AVATAR_MENU_PHOTO_ACCESSIBLE_NAME)
+                                  forAttribute:NSAccessibilityTitleAttribute];
+    [self accessibilitySetOverrideValue:NSAccessibilityButtonRole
+                                  forAttribute:NSAccessibilityRoleAttribute];
+    [self accessibilitySetOverrideValue:
+        NSAccessibilityRoleDescription(NSAccessibilityButtonRole, nil)
+            forAttribute:NSAccessibilityRoleDescriptionAttribute];
   }
   return self;
 }
@@ -516,6 +542,28 @@ class ActiveProfileObserverBridge : public AvatarMenuObserver,
 
 - (void)mouseExited:(NSEvent*)event {
   [changePhotoButton_ setHidden:YES];
+}
+
+// Make sure the element is focusable for accessibility.
+- (BOOL)canBecomeKeyView {
+  return YES;
+}
+
+- (BOOL)accessibilityIsIgnored {
+  return NO;
+}
+
+- (NSArray*)accessibilityActionNames {
+  NSArray* parentActions = [super accessibilityActionNames];
+  return [parentActions arrayByAddingObject:NSAccessibilityPressAction];
+}
+
+- (void)accessibilityPerformAction:(NSString*)action {
+  if ([action isEqualToString:NSAccessibilityPressAction]) {
+    avatarMenu_->EditProfile(avatarMenu_->GetActiveProfileIndex());
+  }
+
+  [super accessibilityPerformAction:action];
 }
 
 - (TransparentBackgroundButton*)changePhotoButtonWithRect:(NSRect)rect {
@@ -609,6 +657,12 @@ class ActiveProfileObserverBridge : public AvatarMenuObserver,
       // Hide the textfield until the user clicks on the button.
       [profileNameTextField_ setHidden:YES];
     }
+
+    [[self cell] accessibilitySetOverrideValue:NSAccessibilityButtonRole
+                           forAttribute:NSAccessibilityRoleAttribute];
+    [[self cell] accessibilitySetOverrideValue:
+        NSAccessibilityRoleDescription(NSAccessibilityButtonRole, nil)
+          forAttribute:NSAccessibilityRoleDescriptionAttribute];
 
     [self setBordered:NO];
     [self setFont:[NSFont labelFontOfSize:kTitleFontSize]];
@@ -988,6 +1042,13 @@ class ActiveProfileObserverBridge : public AvatarMenuObserver,
             signed_in) {
       viewMode_ = profiles::BUBBLE_VIEW_MODE_ACCOUNT_MANAGEMENT;
     }
+
+    [window accessibilitySetOverrideValue:
+        l10n_util::GetNSString(IDS_PROFILES_NEW_AVATAR_MENU_ACCESSIBLE_NAME)
+                             forAttribute:NSAccessibilityTitleAttribute];
+    [window accessibilitySetOverrideValue:
+        l10n_util::GetNSString(IDS_PROFILES_NEW_AVATAR_MENU_ACCESSIBLE_NAME)
+                             forAttribute:NSAccessibilityHelpAttribute];
 
     [[self bubble] setAlignment:info_bubble::kAlignRightEdgeToAnchorEdge];
     [[self bubble] setArrowLocation:info_bubble::kNoArrow];
@@ -1402,6 +1463,11 @@ class ActiveProfileObserverBridge : public AvatarMenuObserver,
         [link setTarget:self];
         [link setAction:@selector(showAccountReauthenticationView:)];
         [link setTag:kPrimaryProfileTag];
+        [[link cell]
+            accessibilitySetOverrideValue:l10n_util::GetNSStringF(
+            IDS_PROFILES_ACCOUNT_BUTTON_AUTH_ERROR_ACCESSIBLE_NAME,
+            item.sync_state)
+                             forAttribute:NSAccessibilityTitleAttribute];
       } else {
         [link setEnabled:NO];
       }
