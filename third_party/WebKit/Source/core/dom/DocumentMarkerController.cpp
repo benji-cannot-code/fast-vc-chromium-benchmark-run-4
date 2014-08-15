76b5f3906a65995c49c497cfaad210280b846c1e
klemen.forstneric@gmail.com <klemen.forstneric@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-15 18:09:04 +0000

    Adding a word to dictionary should remove spelling markers.
    This patch adds code for removing spelling markers under words on 
    blink's side.
    
    BUG=3506
    
    Review URL: https://codereview.chromium.org/419563003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180382 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/DocumentMarkerController.cpp
+++ third_party/WebKit/Source/core/dom/DocumentMarkerController.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/NodeTraversal.h"
 #include "core/dom/Range.h"
 #include "core/dom/RenderedDocumentMarker.h"
+#include "core/dom/Text.h"
 #include "core/editing/TextIterator.h"
 #include "core/rendering/RenderObject.h"
 
@@ -40,6 +41,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace blink {
+
+MarkerRemoverPredicate::MarkerRemoverPredicate(const Vector<String>& words)
+    : m_words(words)
+{
+}
+
+bool MarkerRemoverPredicate::operator()(const DocumentMarker& documentMarker,
+    const Text& textNode) const {
+    unsigned start  = documentMarker.startOffset();
+    unsigned length = documentMarker.endOffset() - documentMarker.startOffset();
+
+    String markerText = textNode.data().substring(start, length);
+    return m_words.contains(markerText);
+}
 
 namespace {
 
@@ -493,6 +508,25 @@ void DocumentMarkerController::removeMarkers(Node* node, DocumentMarker::MarkerT
     MarkerMap::iterator iterator = m_markers.find(node);
     if (iterator != m_markers.end())
         removeMarkersFromList(iterator, markerTypes);
+}
+
+void DocumentMarkerController::removeMarkers(const MarkerRemoverPredicate& shouldRemoveMarker)
+{
+    for (MarkerMap::iterator i = m_markers.begin(); i != m_markers.end(); ++i) {
+        MarkerLists* markers = i->value.get();
+        for (size_t markerListIndex = 0; markerListIndex < DocumentMarker::MarkerTypeIndexesCount; ++markerListIndex) {
+            OwnPtrWillBeMember<MarkerList>& list = (*markers)[markerListIndex];
+
+            WillBeHeapVector<RawPtrWillBeMember<RenderedDocumentMarker> > markersToBeRemoved;
+            for (size_t j = 0; list.get() && j < list->size(); ++j) {
+                if (i->key->isTextNode() && shouldRemoveMarker(*list->at(j).get(), static_cast<const Text&>(*i->key)))
+                    markersToBeRemoved.append(list->at(j).get());
+            }
+
+            for (size_t j = 0; j < markersToBeRemoved.size(); ++j)
+                list->remove(list->find(markersToBeRemoved[j].get()));
+        }
+    }
 }
 
 void DocumentMarkerController::removeMarkers(DocumentMarker::MarkerTypes markerTypes)
--- third_party/WebKit/Source/core/frame/LocalFrame.h
+++ third_party/WebKit/Source/core/frame/LocalFrame.h
@@ -151,6 +151,8 @@ namespace blink {
 
         bool isURLAllowed(const KURL&) const;
 
+        void removeSpellingMarkersUnderWords(const Vector<String>& words);
+
     // ========
 
     private:
