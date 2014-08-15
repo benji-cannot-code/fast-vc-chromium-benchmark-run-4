81234f47038acc8e56a30d045c564382a93cdd50
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-15 05:40:49 +0000

    Replace redundant frame images with a color constant.
    The frame images are just a single color in all pixels. Using a constant
    makes it easier to support other colors for the frame, which we want to
    do for both packaged apps and service worker based apps.
    
    BUG=400958
    TEST=Make sure packaged app and hosted app frames on ChromeOS look like
    they did before this change.
    
    Review URL: https://codereview.chromium.org/447663002
    
    Cr-Commit-Position: refs/heads/master@{#289808}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@289808 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/frame/default_header_painter.h
+++ ash/frame/default_header_painter.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"  // OVERRIDE
 #include "base/gtest_prod_util.h"
 #include "base/memory/scoped_ptr.h"
+#include "third_party/skia/include/core/SkColor.h"
 #include "ui/gfx/animation/animation_delegate.h"
 
 namespace gfx {
@@ -75,11 +76,8 @@ class ASH_EXPORT DefaultHeaderPainter : public HeaderPainter,
   // Returns the bounds for the title.
   gfx::Rect GetTitleBounds() const;
 
-  // Returns the frame image to use when |frame_| is active.
-  gfx::ImageSkia* GetActiveFrameImage() const;
-
-  // Returns the frame image to use when |frame_| is inactive.
-  gfx::ImageSkia* GetInactiveFrameImage() const;
+  // Returns the frame color to use when |frame_| is inactive.
+  SkColor GetInactiveFrameColor() const;
 
   views::Widget* frame_;
   views::View* view_;
