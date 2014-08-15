0bdc70355922b23b7f4cbbf61567e7af1bbfe256
calamity@chromium.org <calamity@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-15 16:03:06 +0000

    Split Shell::ToggleAppList() into ShowAppList and DismissAppList.
    This CL is in preparation for passing a reason for showing the app list.
    This split is necessary to pass a reason only when showing and not when
    dismissing.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/460933002
    
    Cr-Commit-Position: refs/heads/master@{#289889}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@289889 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/ash/app_list/app_list_controller_ash.cc
+++ chrome/browser/ui/ash/app_list/app_list_controller_ash.cc
@@ -16,8 +16,7 @@ AppListControllerDelegateAsh::~AppListControllerDelegateAsh() {}
 
 void AppListControllerDelegateAsh::DismissView() {
   DCHECK(ash::Shell::HasInstance());
-  if (ash::Shell::GetInstance()->GetAppListTargetVisibility())
-    ash::Shell::GetInstance()->ToggleAppList(NULL);
+  ash::Shell::GetInstance()->DismissAppList();
 }
 
 gfx::NativeWindow AppListControllerDelegateAsh::GetAppListWindow() {
