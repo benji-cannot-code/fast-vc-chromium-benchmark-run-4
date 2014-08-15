43779b542cd5a4364abf2f676a7b2ee41a0d3184
pmonette@google.com <pmonette@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-15 18:15:27 +0000

    Moving all the incident reporting stuff to a common folder
    BUG=
    
    Review URL: https://codereview.chromium.org/470213002
    
    Cr-Commit-Position: refs/heads/master@{#289938}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@289938 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/safe_browsing/safe_browsing_service.h
+++ chrome/browser/safe_browsing/safe_browsing_service.h
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/observer_list.h"
 #include "base/sequenced_task_runner_helpers.h"
-#include "chrome/browser/safe_browsing/delayed_analysis_callback.h"
+#include "chrome/browser/safe_browsing/incident_reporting/delayed_analysis_callback.h"
 #include "chrome/browser/safe_browsing/safe_browsing_util.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/notification_observer.h"
