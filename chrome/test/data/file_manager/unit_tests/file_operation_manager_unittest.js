57d306f587714b179a0e821954bafaba5b7dac8d
fukino@chromium.org <fukino@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-15 16:21:35 +0000

    Combine consecutive entry-changed events into one.
    BUG=383232
    TEST=ran browser_tests, tested the performance manually.
    
    Review URL: https://codereview.chromium.org/463783002
    
    Cr-Commit-Position: refs/heads/master@{#289899}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@289899 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/file_manager/unit_tests/file_operation_manager_unittest.js
+++ chrome/test/data/file_manager/unit_tests/file_operation_manager_unittest.js
@@ -121,7 +121,7 @@ function waitForEvents(fileOperationManager) {
       if (event.reason === 'SUCCESS')
         fulfill(events);
     });
-    fileOperationManager.addEventListener('entry-changed', function(event) {
+    fileOperationManager.addEventListener('entries-changed', function(event) {
       events.push(event);
     });
     fileOperationManager.addEventListener('delete', function(event) {
@@ -278,9 +278,9 @@ function testCopy(callback) {
     assertEquals(10, lastEvent.status.totalBytes);
 
     assertTrue(events.some(function(event) {
-      return event.type === 'entry-changed' &&
+      return event.type === 'entries-changed' &&
           event.kind === util.EntryChangedKind.CREATED &&
-          event.entry.fullPath === '/test (1).txt';
+          event.entries[0].fullPath === '/test (1).txt';
     }));
 
     assertFalse(events.some(function(event) {
@@ -327,15 +327,15 @@ function testMove(callback) {
     assertEquals(1, lastEvent.status.totalBytes);
 
     assertTrue(events.some(function(event) {
-      return event.type === 'entry-changed' &&
+      return event.type === 'entries-changed' &&
           event.kind === util.EntryChangedKind.DELETED &&
-          event.entry.fullPath === '/test.txt';
+          event.entries[0].fullPath === '/test.txt';
     }));
 
     assertTrue(events.some(function(event) {
-      return event.type === 'entry-changed' &&
+      return event.type === 'entries-changed' &&
           event.kind === util.EntryChangedKind.CREATED &&
-          event.entry.fullPath === '/directory/test.txt';
+          event.entries[0].fullPath === '/directory/test.txt';
     }));
 
     assertFalse(events.some(function(event) {
@@ -423,8 +423,8 @@ function testZip(callback) {
     }));
 
     assertTrue(events.some(function(event) {
-      return event.type === 'entry-changed' &&
-          event.entry.fullPath === '/test.zip';
+      return event.type === 'entries-changed' &&
+          event.entries[0].fullPath === '/test.zip';
     }));
   }), callback);
 
