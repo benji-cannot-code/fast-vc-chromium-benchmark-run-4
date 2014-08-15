37fbd1839752f6ec3e193211495e10d20e69478a
gunsch@chromium.org <gunsch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-15 16:30:05 +0000

    Chromecast: pages default to having a black background.
    R=byungchul@chromium.org,lcwu@chromium.org,abarth@chromium.org
    BUG=400916
    
    Review URL: https://codereview.chromium.org/454113003
    
    Cr-Commit-Position: refs/heads/master@{#289907}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@289907 0039d316-1c4b-4281-b951-d872f2087c98

--- chromecast/shell/renderer/cast_content_renderer_client.h
+++ chromecast/shell/renderer/cast_content_renderer_client.h
@@ -18,6 +18,7 @@ class CastContentRendererClient : public content::ContentRendererClient {
 
   // ContentRendererClient implementation:
   virtual void RenderThreadStarted() OVERRIDE;
+  virtual void RenderViewCreated(content::RenderView* render_view) OVERRIDE;
   virtual void AddKeySystems(
       std::vector<content::KeySystemInfo>* key_systems) OVERRIDE;
 };
