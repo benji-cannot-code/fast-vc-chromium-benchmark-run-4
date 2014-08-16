a41d122369269a5a0902b25f8afcaba572ffa9fe
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-16 00:34:59 +0000

    Remove feedback debugging log.
    Removing as bug http://crbug.com/284662 is fixed.
    
    BUG=284662
    
    Review URL: https://codereview.chromium.org/469213008
    
    Cr-Commit-Position: refs/heads/master@{#290068}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290068 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/feedback_private/feedback_private_api.cc
+++ chrome/browser/extensions/api/feedback_private/feedback_private_api.cc
@@ -69,8 +69,6 @@ void FeedbackPrivateAPI::RequestFeedback(
     const std::string& description_template,
     const std::string& category_tag,
     const GURL& page_url) {
-  // TODO(rkc): Remove logging once crbug.com/284662 is closed.
-  LOG(WARNING) << "FEEDBACK_DEBUG: Feedback requested.";
   if (browser_context_ && EventRouter::Get(browser_context_)) {
     FeedbackInfo info;
     info.description = description_template;
@@ -89,8 +87,6 @@ void FeedbackPrivateAPI::RequestFeedback(
         feedback_private::OnFeedbackRequested::kEventName, args.Pass()));
     event->restrict_to_browser_context = browser_context_;
 
-    // TODO(rkc): Remove logging once crbug.com/284662 is closed.
-    LOG(WARNING) << "FEEDBACK_DEBUG: Dispatching onFeedbackRequested event.";
     EventRouter::Get(browser_context_)
         ->DispatchEventToExtension(kFeedbackExtensionId, event.Pass());
   }
@@ -136,8 +132,6 @@ bool FeedbackPrivateGetStringsFunction::RunSync() {
 }
 
 bool FeedbackPrivateGetUserEmailFunction::RunSync() {
-  // TODO(rkc): Remove logging once crbug.com/284662 is closed.
-  LOG(WARNING) << "FEEDBACK_DEBUG: User e-mail requested.";
   FeedbackService* service =
       FeedbackPrivateAPI::GetFactoryInstance()->Get(GetProfile())->GetService();
   DCHECK(service);
@@ -146,8 +140,6 @@ bool FeedbackPrivateGetUserEmailFunction::RunSync() {
 }
 
 bool FeedbackPrivateGetSystemInformationFunction::RunAsync() {
-  // TODO(rkc): Remove logging once crbug.com/284662 is closed.
-  LOG(WARNING) << "FEEDBACK_DEBUG: System information requested.";
   FeedbackService* service =
       FeedbackPrivateAPI::GetFactoryInstance()->Get(GetProfile())->GetService();
   DCHECK(service);
