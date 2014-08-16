56609aaa5406514529dfc4abbfab294292eb234b
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-16 03:05:32 +0000

    Remove webkitpy.thirdparty.mock, webkitpy.common.webkitunittest.
    More webkitpy cleanup.
    
    webkitunittest isn't needed once we required Python 2.7.
    
    mock.Mock() was really only being used in one file (and didn't need to be used
    there, either), and I'm not generally fan of the generic Mock approach
    it uses (preferring concrete mock objects or fakes).
    
    R=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/467133003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180404 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/tool/commands/queries_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/commands/queries_unittest.py
@@ -31,10 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import unittest
 
 from webkitpy.common.system.outputcapture import OutputCapture
-from webkitpy.common.system.outputcapture import OutputCapture
-from webkitpy.thirdparty.mock import Mock
 from webkitpy.layout_tests.port.test import TestPort
-from webkitpy.tool.commands.commandtest import CommandsTest
 from webkitpy.tool.commands.queries import *
 from webkitpy.tool.mocktool import MockTool, MockOptions
 
