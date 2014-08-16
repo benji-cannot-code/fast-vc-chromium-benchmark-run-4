d1a7114f5adc50faee1bb7f07ea349676fa2dc43
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-16 00:11:20 +0000

    Remove several unneeded modules from webkitpy.common.
    webkitpy.common.editdistance, webkitpy.common.lru_cache, and
    webkitpy.common.thread were no longer being used.
    
    webkitpy.common.newstringio could be (and should've been) replaced by
    io.StringIO() or io.BytesIO() as appropriate to indicate if you were
    dealing with text or bytes.
    
    R=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/479633002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180397 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/common/checksvnconfigfile.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/checksvnconfigfile.py
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 # This file is used by:
-# webkitpy/tool/steps/addsvnmimetypeforpng.py
 # webkitpy/style/checkers/png.py
 
 import os
