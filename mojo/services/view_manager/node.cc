2a4cb0c74f29137d0fdab6c02a9accaa09e41c7b
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-16 07:55:10 +0000

    Add some trivial window decorations & window closing back to the demo window manager
    R=sky@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/478833002
    
    Cr-Commit-Position: refs/heads/master@{#290120}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290120 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/services/view_manager/node.cc
+++ mojo/services/view_manager/node.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window_property.h"
 #include "ui/base/cursor/cursor.h"
 #include "ui/base/hit_test.h"
+#include "ui/compositor/layer.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/image/image_skia.h"
 #include "ui/gfx/native_widget_types.h"
