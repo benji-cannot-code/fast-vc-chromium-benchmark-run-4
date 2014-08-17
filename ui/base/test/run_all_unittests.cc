778e34006c35064fee4b32e06d5a16541cc9fa57
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-17 04:41:59 +0000

    ui: Move win gfx tests into gfx_unittests target.
    This is pretty straightforward, just moving the remaining win tests from
    ui_unittests.gyp to gfx_tests.gyp. gfx_unittests now has a
    TestSuite that setups everything we need to run it.
    
    BUG=331829
    TEST=ui_unittests, gfx_unittests
    TBR=ben@chromium.org,danakj@chromium.org
    
    Review URL: https://codereview.chromium.org/469003009
    
    Cr-Commit-Position: refs/heads/master@{#290173}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290173 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/test/run_all_unittests.cc
+++ ui/base/test/run_all_unittests.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/ui_base_paths.h"
-#include "ui/gfx/gfx_paths.h"
 
 #if defined(OS_ANDROID)
 #include "base/android/jni_android.h"
@@ -59,7 +58,6 @@ void UIBaseTestSuite::Initialize() {
 #endif
 
   ui::RegisterPathProvider();
-  gfx::RegisterPathProvider();
 
   base::FilePath exe_path;
   PathService::Get(base::DIR_EXE, &exe_path);
