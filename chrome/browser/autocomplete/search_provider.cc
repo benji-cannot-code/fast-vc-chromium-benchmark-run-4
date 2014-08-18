119a22dbe47b04f7add57b52cad1bb1271d5f13b
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-18 09:52:27 +0000

    Move string resources to omnibox_strings.grdp
    BUG=371538
    TEST=build
    
    Review URL: https://codereview.chromium.org/450243002
    
    Cr-Commit-Position: refs/heads/master@{#290230}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290230 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/search_provider.cc
+++ chrome/browser/autocomplete/search_provider.cc
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/search_engines/template_url_prepopulate_data.h"
 #include "components/search_engines/template_url_service.h"
 #include "components/variations/variations_http_header_provider.h"
-#include "grit/generated_resources.h"
+#include "grit/components_strings.h"
 #include "net/base/escape.h"
 #include "net/base/load_flags.h"
 #include "net/base/net_util.h"
