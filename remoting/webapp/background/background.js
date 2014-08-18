cbe1a42b7a079a1d7b00a37ac5a40f7cb5f21760
kelvinp@chromium.org <kelvinp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-18 23:23:44 +0000

    Move isAppsV2 check to base.js
    This CL moves the isAppsV2 check from remoting.js to base.js 
    so that it can be shared across the background page, confirm
    dialogs and the webapp.
    
    Review URL: https://codereview.chromium.org/484733002
    
    Cr-Commit-Position: refs/heads/master@{#290403}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290403 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/webapp/background/background.js
+++ remoting/webapp/background/background.js
@@ -8,15 +8,6 @@ var remoting = remoting || {};
 
 (function(){
 
-/** @return {boolean} */
-function isAppsV2() {
-  var manifest = chrome.runtime.getManifest();
-  if (manifest && manifest.app && manifest.app.background) {
-    return true;
-  }
-  return false;
-}
-
 /** @param {remoting.AppLauncher} appLauncher */
 function initializeAppV2(appLauncher) {
   /** @type {string} */
@@ -70,7 +61,7 @@ function initializeBackgroundService(appLauncher) {
 function main() {
   /** @type {remoting.AppLauncher} */
   var appLauncher = new remoting.V1AppLauncher();
-  if (isAppsV2()) {
+  if (base.isAppsV2()) {
     appLauncher = new remoting.V2AppLauncher();
     initializeAppV2(appLauncher);
   }
