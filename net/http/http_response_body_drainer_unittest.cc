51cbde92b8187960d5f8f02a21a4fda61d378f42
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-18 22:47:00 +0000

    Revert 290320 "Refactor pooling logic into a helper method"
    Leaks (see bug).
    
    Actually reverting the re-land this time.
    
    BUG=404833
    
    > Refactor pooling logic into a helper method
    > Disable pooling when there are cert errors.
    > Disable pooling when pinning does not match for the new host.
    > 
    > BUG=398925
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=289433
    > 
    > Review URL: https://codereview.chromium.org/425803014
    
    TBR=rch@chromium.org
    
    Review URL: https://codereview.chromium.org/485943004
    
    Cr-Commit-Position: refs/heads/master@{#290386}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290386 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_response_body_drainer_unittest.cc
+++ net/http/http_response_body_drainer_unittest.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_network_session.h"
 #include "net/http/http_server_properties_impl.h"
 #include "net/http/http_stream.h"
-#include "net/http/transport_security_state.h"
 #include "net/proxy/proxy_service.h"
 #include "net/ssl/ssl_config_service_defaults.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -223,7 +222,6 @@ class HttpResponseBodyDrainerTest : public testing::Test {
     params.proxy_service = proxy_service_.get();
     params.ssl_config_service = ssl_config_service_.get();
     params.http_server_properties = http_server_properties_->GetWeakPtr();
-    params.transport_security_state = new TransportSecurityState();
     return new HttpNetworkSession(params);
   }
 
--- net/socket/ssl_client_socket_pool_unittest.cc
+++ net/socket/ssl_client_socket_pool_unittest.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_request_headers.h"
 #include "net/http/http_response_headers.h"
 #include "net/http/http_server_properties_impl.h"
-#include "net/http/transport_security_state.h"
 #include "net/proxy/proxy_service.h"
 #include "net/socket/client_socket_handle.h"
 #include "net/socket/client_socket_pool_histograms.h"
@@ -194,7 +193,6 @@ class SSLClientSocketPoolTest
         http_server_properties_.GetWeakPtr();
     params.enable_spdy_compression = false;
     params.spdy_default_protocol = GetParam();
-    params.transport_security_state = new TransportSecurityState();
     return new HttpNetworkSession(params);
   }
 
@@ -1260,7 +1258,8 @@ TEST_P(SSLClientSocketPoolTest, NeedProxyAuth) {
   EXPECT_FALSE(tunnel_handle->socket()->IsConnected());
 }
 
-TEST_P(SSLClientSocketPoolTest, IPPooling) {
+// TODO(rch): re-enable this.
+TEST_P(SSLClientSocketPoolTest, DISABLED_IPPooling) {
   const int kTestPort = 80;
   struct TestHosts {
     std::string name;
