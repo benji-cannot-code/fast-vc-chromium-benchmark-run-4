ce2b46c94b03f3e656ea386a1cde37943e669d27
alemate@chromium.org <alemate@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-18 14:33:38 +0000

    ChromeOS: should not show "Language changed" notification for certain languages.
    BUG=317718
    TEST=manually tested
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=289985
    
    Review URL: https://codereview.chromium.org/382973002
    
    Cr-Commit-Position: refs/heads/master@{#290255}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290255 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/locale_change_guard.h
+++ chrome/browser/chromeos/locale_change_guard.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/system/locale/locale_observer.h"
 #include "base/compiler_specific.h"
+#include "base/gtest_prod_util.h"
 #include "base/lazy_instance.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
@@ -48,6 +49,11 @@ class LocaleChangeGuard : public content::NotificationObserver,
   void OnLogin();
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(LocaleChangeGuardTest,
+                           ShowNotificationLocaleChanged);
+  FRIEND_TEST_ALL_PREFIXES(LocaleChangeGuardTest,
+                           ShowNotificationLocaleChangedList);
+
   void RevertLocaleChangeCallback(const base::ListValue* list);
   void Check();
 
@@ -55,6 +61,13 @@ class LocaleChangeGuard : public content::NotificationObserver,
   virtual void Observe(int type,
                        const content::NotificationSource& source,
                        const content::NotificationDetails& details) OVERRIDE;
+
+  // Returns true if we should notify user about automatic locale change.
+  static bool ShouldShowLocaleChangeNotification(const std::string& from_locale,
+                                                 const std::string& to_locale);
+
+  static const char* const* GetSkipShowNotificationLanguagesForTesting();
+  static size_t GetSkipShowNotificationLanguagesSizeForTesting();
 
   std::string from_locale_;
   std::string to_locale_;
--- chrome/chrome_tests_unit.gypi
+++ chrome/chrome_tests_unit.gypi
@@ -737,6 +737,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/chromeos/input_method/input_method_util_unittest.cc',
         'browser/chromeos/kiosk_mode/kiosk_mode_idle_logout_unittest.cc',
         'browser/chromeos/kiosk_mode/kiosk_mode_settings_unittest.cc',
+        'browser/chromeos/locale_change_guard_unittest.cc',
         'browser/chromeos/login/auth/online_attempt_unittest.cc',
         'browser/chromeos/login/auth/parallel_authenticator_unittest.cc',
         'browser/chromeos/login/existing_user_controller_auto_login_unittest.cc',
