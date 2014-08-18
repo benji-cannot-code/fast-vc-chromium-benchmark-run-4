2adb9ee06bc590641f5295edc7bf7c65105c11ff
dgozman@chromium.org <dgozman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-18 17:32:51 +0000

    [DevTools] Change navigator.maxTouchPoints when emulating touch.
    BUG=403876
    
    Review URL: https://codereview.chromium.org/485563003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180476 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorPageAgent.h
+++ third_party/WebKit/Source/core/inspector/InspectorPageAgent.h
@@ -212,6 +212,7 @@ private:
     bool m_originalTouchEnabled;
     bool m_originalDeviceSupportsMouse;
     bool m_originalDeviceSupportsTouch;
+    int m_originalMaxTouchPoints;
 
     bool m_embedderTextAutosizingEnabled;
     double m_embedderFontScaleFactor;
