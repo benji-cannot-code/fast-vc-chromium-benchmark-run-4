652b5218dd8be7e252394d0af35f4c945b220f60
noel@chromium.org <noel@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-18 08:42:11 +0000

    ImageSource misc cleanup
    Remove unused includes, simplify and clarify a comment, format the
    frameBytesAtIndex() code like the rest of the code in this file.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/485623002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180434 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/ImageSource.cpp
+++ third_party/WebKit/Source/platform/graphics/ImageSource.cpp
@@ -31,8 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "platform/graphics/DeferredImageDecoder.h"
 #include "platform/image-decoders/ImageDecoder.h"
-#include "wtf/PassOwnPtr.h"
-#include "wtf/PassRefPtr.h"
 
 namespace blink {
 
@@ -58,10 +56,8 @@ bool ImageSource::initialized() const
 
 void ImageSource::setData(SharedBuffer& data, bool allDataReceived)
 {
-    // Make the decoder by sniffing the bytes.
-    // This method will examine the data and instantiate an instance of the appropriate decoder plugin.
-    // If insufficient bytes are available to determine the image type, no decoder plugin will be
-    // made.
+    // Create a decoder by sniffing the encoded data. If insufficient data bytes are available to
+    // determine the encoded image type, no decoder is created.
     if (!m_decoder)
         m_decoder = DeferredImageDecoder::create(data, m_alphaOption, m_gammaAndColorProfileOption);
 
@@ -167,9 +163,7 @@ bool ImageSource::frameIsCompleteAtIndex(size_t index) const
 
 unsigned ImageSource::frameBytesAtIndex(size_t index) const
 {
-    if (!m_decoder)
-        return 0;
-    return m_decoder->frameBytesAtIndex(index);
+    return m_decoder ? m_decoder->frameBytesAtIndex(index) : 0;
 }
 
 } // namespace blink
