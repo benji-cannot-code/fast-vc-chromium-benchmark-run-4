793721a4ccad800962c38c9891ac4d48ceb2eca3
zqiu@chromium.org <zqiu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-18 20:12:07 +0000

    Add wifi AP info to system profile metrics
    Report wifi AP info to UMA system profile information stats for connected APs. The data will not be collected for the APs that contain "_nomap" in their SSID. The stats will be display/analyze in the dashboard that will be created with future CLs.
    
    BUG=chromium:378892
    TEST=Use NET_LOG messages to verify the AP vendor information. Will
    also be verified in the future CLs.
    
    Review URL: https://codereview.chromium.org/328793002
    
    Cr-Commit-Position: refs/heads/master@{#290350}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290350 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/metrics/wifi_access_point_info_provider.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/metrics/wifi_access_point_info_provider.h"
+
+WifiAccessPointInfoProvider::WifiAccessPointInfo::WifiAccessPointInfo() {
+}
+
+WifiAccessPointInfoProvider::WifiAccessPointInfo::~WifiAccessPointInfo() {
+}
+
+WifiAccessPointInfoProvider::WifiAccessPointInfoProvider() {
+}
+
+WifiAccessPointInfoProvider::~WifiAccessPointInfoProvider() {
+}
+
+bool WifiAccessPointInfoProvider::GetInfo(WifiAccessPointInfo *info) {
+  return false;
+}
