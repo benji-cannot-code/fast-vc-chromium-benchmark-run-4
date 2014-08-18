bf56e47169e8e464c139037ef7827cb44820a9f4
halcanary@google.com <halcanary@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-18 19:43:28 +0000

    remove unncessary include
    Review URL: https://codereview.chromium.org/478383002
    
    Cr-Commit-Position: refs/heads/master@{#290338}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290338 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/ext/pixel_ref_utils_unittest.cc
+++ skia/ext/pixel_ref_utils_unittest.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "third_party/skia/include/core/SkCanvas.h"
-#include "third_party/skia/include/core/SkFlattenableBuffers.h"
 #include "third_party/skia/include/core/SkPictureRecorder.h"
 #include "third_party/skia/include/core/SkPixelRef.h"
 #include "third_party/skia/include/core/SkPoint.h"
