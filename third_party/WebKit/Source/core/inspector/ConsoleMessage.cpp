46330616dd532b926c16436c9bbff3337d4ae083
sigbjornf@opera.com <sigbjornf@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-19 10:55:24 +0000

    Oilpan: fix build after r180544.
    TBR=oilpan-reviews
    BUG=
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/488543002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180545 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/ConsoleMessage.cpp
+++ third_party/WebKit/Source/core/inspector/ConsoleMessage.cpp
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "core/inspector/ConsoleMessage.h"
 
-#include "bindings/core/v8/ScriptState.h"
+#include "bindings/core/v8/ScriptValue.h"
 #include "core/inspector/ScriptArguments.h"
 
 namespace blink {
@@ -88,12 +88,12 @@ void ConsoleMessage::setScriptState(ScriptState* scriptState)
         m_scriptState.clear();
 }
 
-PassRefPtr<ScriptArguments> ConsoleMessage::scriptArguments() const
+PassRefPtrWillBeRawPtr<ScriptArguments> ConsoleMessage::scriptArguments() const
 {
     return m_scriptArguments;
 }
 
-void ConsoleMessage::setScriptArguments(PassRefPtr<ScriptArguments> scriptArguments)
+void ConsoleMessage::setScriptArguments(PassRefPtrWillBeRawPtr<ScriptArguments> scriptArguments)
 {
     m_scriptArguments = scriptArguments;
 }
@@ -131,6 +131,7 @@ unsigned ConsoleMessage::columnNumber() const
 void ConsoleMessage::trace(Visitor* visitor)
 {
     visitor->trace(m_callStack);
+    visitor->trace(m_scriptArguments);
 }
 
 } // namespace blink
