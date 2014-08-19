8074cacb99509650c9e112a085aa95beb1f8aa51
kouhei@chromium.org <kouhei@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-19 01:52:55 +0000

    Sheriff-o-matic: Support LEAK failure type
    Before this patch, sheriff-o-matic didn't show any information for LEAK
    failures from "WebKit Linux Leak" builder. This patch changes it to
    display object leak log.
    
    BUG=404030
    win_blink_rel seems broken atm. Passing on all other trybots and the trybots don't seem to trigger the test anyway.
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/481833003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180509 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/GardeningServer/scripts/results.js
+++ third_party/WebKit/Tools/GardeningServer/scripts/results.js
@@ -32,6 +32,7 @@ var PASS = 'PASS';
 var TIMEOUT = 'TIMEOUT';
 var TEXT = 'TEXT';
 var CRASH = 'CRASH';
+var LEAK = 'LEAK';
 var IMAGE = 'IMAGE';
 var IMAGE_TEXT = 'IMAGE+TEXT';
 var AUDIO = 'AUDIO';
@@ -48,6 +49,7 @@ var kExpectedTextSuffix = '-expected.txt';
 var kActualTextSuffix = '-actual.txt';
 var kDiffTextSuffix = '-diff.txt';
 var kCrashLogSuffix = '-crash-log.txt';
+var kLeakLogSuffix = '-leak-log.txt';
 
 var kPreferredSuffixOrder = [
     kExpectedImageSuffix,
@@ -59,6 +61,7 @@ var kPreferredSuffixOrder = [
     kCrashLogSuffix,
     kExpectedAudioSuffix,
     kActualAudioSuffix,
+    kLeakLogSuffix,
 ];
 
 // Kinds of results.
@@ -113,6 +116,9 @@ function possibleSuffixListFor(failureTypeList)
             break;
         case CRASH:
             suffixList.push(kCrashLogSuffix);
+            break;
+        case LEAK:
+            suffixList.push(kLeakLogSuffix);
             break;
         case MISSING:
             pushImageSuffixes();
