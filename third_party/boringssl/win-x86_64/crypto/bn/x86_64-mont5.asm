8ecca449f3476a55845616e29cf046fd412d679c
davidben@chromium.org <davidben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-19 10:29:51 +0000

    Roll BoringSSL.
    This pulls in the following changes:
    
    d4178fd Remove AES-GCM cipher indices.
    b00eee1 Trim unused cipher indices and definitions.
    335d10d Remove Suite B mode.
    76d8abe Get SSL 3.0 server tests working.
    6fd297b Add initial DTLS tests.
    c10dde4 Only allow indefinite lengths for constructed types.
    82b7da2 Set SSL_SESSION.cipher when parsing.
    0f4746e Add SSL_SESSION_get_version.
    4dc016f Clarify a comment.
    b8b5478 Expose two, rather internal, BIGNUM functions.
    6601402 Add EVP_EncodedLength.
    e2c4d26 Recover BIO_number_{read|written}.
    1b96526 Merge tls1_* method hooks with the ssl3_* versions.
    5a593af Move configuration into a dedicated TestConfig struct.
    83c0bc9 Test-only DTLS implementation in runner.go.
    51e3283 Port dtls1_get_hello_verify to CBS.
    9174312 Update d1_clnt.c to use ssl_cipher_has_server_public_key.
    6491e8c Allow renewed tickets on session resumption in DTLS.
    09bd58d Replace some DTLS version checks with SSL_IS_DTLS.
    16c623b Fix of handling weak dh server keys.
    584d28c Use OPENSSL_free instead of free.
    f1df2b3 Add SSL_CIPHER_get_kx_name.
    8eaaa86 Add mode to disallow session creation.
    98e882e Clean up s23_srvr.c.
    f476d30 Fix err_test.
    03d31ed Add ERR_remove_thread_state (for Android).
    ded9358 Windows build fixes.
    d83f38c Recognise "i686" as an x86 CPU.
    90974e7 Fixed reporting wrong function to OPENSSL_PUT_ERROR.
    2cd6387 Use unified ARM assembly.
    1e7f8d7 Move SendV2ClientHello to handshake coverage tests.
    bed9aae Add RenewTicketOnResume tests.
    9821454 Add tests for CVE-2014-3511.
    7e2e6cf Add test coverage for TLS version negotiation.
    0fecacd Fix protocol downgrade bug in case of fragmented packets
    a8e3e0e Remove SSL_OP_TLS_ROLLBACK_BUG.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/479723002
    
    Cr-Commit-Position: refs/heads/master@{#290532}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290532 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/boringssl/win-x86_64/crypto/bn/x86_64-mont5.asm
+++ third_party/boringssl/win-x86_64/crypto/bn/x86_64-mont5.asm
@@ -58,7 +58,7 @@ $L$mul_body::
 	movq	xmm6,QWORD PTR[16+r10*8+rax]
 	movq	xmm7,QWORD PTR[24+r10*8+rax]
 
-	movq	xmm0,QWORD PTR[((-96))+r12]
+	movq	xmm0,QWORD PTR[(((-96)))+r12]
 	movq	xmm1,QWORD PTR[((-32))+r12]
 	pand	xmm0,xmm4
 	movq	xmm2,QWORD PTR[32+r12]
@@ -79,7 +79,7 @@ DB	102,72,15,126,195
 	xor	r14,r14
 	xor	r15,r15
 
-	movq	xmm0,QWORD PTR[((-96))+r12]
+	movq	xmm0,QWORD PTR[(((-96)))+r12]
 	movq	xmm1,QWORD PTR[((-32))+r12]
 	pand	xmm0,xmm4
 	movq	xmm2,QWORD PTR[32+r12]
@@ -159,7 +159,7 @@ $L$outer::
 	mov	rbp,r8
 	mov	r10,QWORD PTR[rsp]
 
-	movq	xmm0,QWORD PTR[((-96))+r12]
+	movq	xmm0,QWORD PTR[(((-96)))+r12]
 	movq	xmm1,QWORD PTR[((-32))+r12]
 	pand	xmm0,xmm4
 	movq	xmm2,QWORD PTR[32+r12]
@@ -391,7 +391,7 @@ mul4x_internal	PROC PRIVATE
 	movq	xmm7,QWORD PTR[24+r10*8+rax]
 	and	r11,7
 
-	movq	xmm0,QWORD PTR[((-96))+r12]
+	movq	xmm0,QWORD PTR[(((-96)))+r12]
 	lea	r14,QWORD PTR[256+r12]
 	movq	xmm1,QWORD PTR[((-32))+r12]
 	pand	xmm0,xmm4
@@ -589,7 +589,7 @@ $L$outer4x::
 	mov	rax,QWORD PTR[rcx]
 	adc	rdx,0
 
-	movq	xmm0,QWORD PTR[((-96))+r12]
+	movq	xmm0,QWORD PTR[(((-96)))+r12]
 	movq	xmm1,QWORD PTR[((-32))+r12]
 	pand	xmm0,xmm4
 	movq	xmm2,QWORD PTR[32+r12]
@@ -1869,7 +1869,7 @@ DB	00fh,029h,07ch,024h,010h
 	jmp	$L$gather
 ALIGN	16
 $L$gather::
-	movq	xmm0,QWORD PTR[((-128))+r8]
+	movq	xmm0,QWORD PTR[(((-128)))+r8]
 	movq	xmm1,QWORD PTR[((-64))+r8]
 	pand	xmm0,xmm4
 	movq	xmm2,QWORD PTR[r8]
