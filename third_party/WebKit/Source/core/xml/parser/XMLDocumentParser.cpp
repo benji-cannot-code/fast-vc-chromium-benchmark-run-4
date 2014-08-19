3674255aa1100ca6b08aa82e360eceeaba4d5a36
sigbjornf@opera.com <sigbjornf@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-19 18:51:32 +0000

    Oilpan: fix build after r180659.
    TBR=oilpan-reviews
    BUG=404107
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/487313003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180591 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/xml/parser/XMLDocumentParser.cpp
+++ third_party/WebKit/Source/core/xml/parser/XMLDocumentParser.cpp
@@ -443,7 +443,7 @@ void XMLDocumentParser::finish()
     // However, FrameLoader::stop calls DocumentParser::finish unconditionally.
 
     // flush may ending up executing arbitrary script, and possibly detach the parser.
-    RefPtr<XMLDocumentParser> protect(this);
+    RefPtrWillBeRawPtr<XMLDocumentParser> protect(this);
     flush();
     if (isDetached())
         return;
