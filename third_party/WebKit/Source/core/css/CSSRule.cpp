3b2e9bfbf1ad642b9b06c820c184e0bd08c55347
sigbjornf@opera.com <sigbjornf@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-19 21:17:11 +0000

    Oilpan: fix MSVC compilation after r180556.
    Have the "same-size" class inherit from the same classes as CSSRule, so
    as to have the MSVC-specific alignment constraints of the parent
    ScriptWrappableBase class apply in equal measure.
    
    TBR=oilpan-reviews@chromium.org,yukishiino@chromium.org
    BUG=235436
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/487923002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180601 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/CSSRule.cpp
+++ third_party/WebKit/Source/core/css/CSSRule.cpp
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-struct SameSizeAsCSSRule : public RefCountedWillBeGarbageCollectedFinalized<SameSizeAsCSSRule> {
+struct SameSizeAsCSSRule : public RefCountedWillBeGarbageCollectedFinalized<SameSizeAsCSSRule>, public ScriptWrappableBase {
     virtual ~SameSizeAsCSSRule();
     unsigned char bitfields;
     void* pointerUnion;
