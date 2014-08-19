7a23ab74a26a9f095671aa6d1c2fa2c3952ddb20
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-19 06:19:24 +0000

    Remove no-op uses of aura::client::DispatcherRunLoop
    BUG=None
    TEST=None
    
    Review URL: https://codereview.chromium.org/482513002
    
    Cr-Commit-Position: refs/heads/master@{#290508}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290508 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/first_run_dialog.cc
+++ chrome/browser/ui/views/first_run_dialog.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/first_run_dialog.h"
 
+#include "base/message_loop/message_loop.h"
+#include "base/run_loop.h"
 #include "chrome/browser/first_run/first_run.h"
 #include "chrome/browser/platform_util.h"
 #include "chrome/browser/shell_integration.h"
@@ -26,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/layout/layout_constants.h"
 #include "ui/views/widget/widget.h"
 #include "ui/views/window/dialog_delegate.h"
-#include "ui/wm/public/dispatcher_client.h"
 
 #if defined(GOOGLE_CHROME_BUILD)
 #include "base/prefs/pref_service.h"
@@ -58,16 +59,9 @@ bool FirstRunDialog::Show(Profile* profile) {
     FirstRunDialog* dialog = new FirstRunDialog(profile);
     views::DialogDelegate::CreateDialogWidget(dialog, NULL, NULL)->Show();
 
-    // Use the widget's window itself so that the message loop
-    // exists when the dialog is closed by some other means than
-    // |Accept|.
-    //
-    // This is the same trick used in simple_message_box_views.cc, minus the
-    // refcounting.
-    aura::Window* anchor = dialog->GetWidget()->GetNativeWindow();
-    aura::client::DispatcherClient* client =
-        aura::client::GetDispatcherClient(anchor->GetRootWindow());
-    aura::client::DispatcherRunLoop run_loop(client, NULL);
+    base::MessageLoopForUI* loop = base::MessageLoopForUI::current();
+    base::MessageLoopForUI::ScopedNestableTaskAllower allow_nested(loop);
+    base::RunLoop run_loop;
     dialog->quit_runloop_ = run_loop.QuitClosure();
     run_loop.Run();
     dialog_shown = true;
