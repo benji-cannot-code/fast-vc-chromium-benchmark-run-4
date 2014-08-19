9c3505665d9aab500f3b65a80d90b993bea16be4
jrummell@chromium.org <jrummell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-19 21:43:38 +0000

    Implement ClearKey message format as JSON.
    The EME-WD specification now describes the format of the license
    request provided to the application via the message attribute of
    the message event as being JSON formatted. This change updates
    the ClearKey implmentation to return the JSON formatted attribute.
    
    For backwards compatibility with the prefixed EME implementation,
    the old format (single key id as Uint8Array) is still used for
    any prefixed calls.
    
    BUG=358271
    TEST=Updated EME layout tests pass
    
    Review URL: https://codereview.chromium.org/427993002
    
    Cr-Commit-Position: refs/heads/master@{#290669}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290669 0039d316-1c4b-4281-b951-d872f2087c98

--- media/cdm/aes_decryptor_unittest.cc
+++ media/cdm/aes_decryptor_unittest.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/bind.h"
+#include "base/json/json_reader.h"
+#include "base/values.h"
 #include "media/base/cdm_promise.h"
 #include "media/base/decoder_buffer.h"
 #include "media/base/decrypt_config.h"
@@ -25,6 +27,11 @@ using ::testing::StrNe;
 
 MATCHER(IsEmpty, "") { return arg.empty(); }
 MATCHER(IsNotEmpty, "") { return !arg.empty(); }
+MATCHER(IsJSONDictionary, "") {
+  std::string result(arg.begin(), arg.end());
+  scoped_ptr<base::Value> root(base::JSONReader().ReadToValue(result));
+  return (root.get() && root->GetType() == base::Value::TYPE_DICTIONARY);
+}
 
 class GURL;
 
@@ -52,7 +59,8 @@ const char kKeyAsJWK[] =
     "      \"kid\": \"AAECAw\","
     "      \"k\": \"BAUGBwgJCgsMDQ4PEBESEw\""
     "    }"
-    "  ]"
+    "  ],"
+    "  \"type\": \"temporary\""
     "}";
 
 // Same kid as kKeyAsJWK, key to decrypt kEncryptedData2
@@ -288,7 +296,8 @@ class AesDecryptorTest : public testing::Test {
   std::string CreateSession(const std::vector<uint8>& key_id) {
     DCHECK(!key_id.empty());
     EXPECT_CALL(*this,
-                OnSessionMessage(IsNotEmpty(), key_id, GURL::EmptyGURL()));
+                OnSessionMessage(
+                    IsNotEmpty(), IsJSONDictionary(), GURL::EmptyGURL()));
     decryptor_.CreateSession(std::string(),
                              &key_id[0],
                              key_id.size(),
