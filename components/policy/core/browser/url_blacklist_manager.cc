427054515aae05fa7259016e3ddaa21d6d7bdfb1
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-19 10:38:14 +0000

    Bypass internal Chrome schemes when "*" is blacklisted.
    This change prevents Chrome from blocking internal URLs with the
    wildcard "*" filter in the URL blacklist. Such URLs can still be
    blocked with more specific filters.
    
    BUG=403336
    
    Review URL: https://codereview.chromium.org/478283003
    
    Cr-Commit-Position: refs/heads/master@{#290534}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290534 0039d316-1c4b-4281-b951-d872f2087c98

--- components/policy/core/browser/url_blacklist_manager.cc
+++ components/policy/core/browser/url_blacklist_manager.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/load_flags.h"
 #include "net/base/net_errors.h"
 #include "net/url_request/url_request.h"
+#include "url/url_constants.h"
 #include "url/url_parse.h"
 
 using url_matcher::URLMatcher;
@@ -35,7 +36,22 @@ namespace policy {
 
 namespace {
 
-const char kFileScheme[] = "file";
+// List of schemes of URLs that should not be blocked by the "*" wildcard in
+// the blacklist. Note that URLs with these schemes can still be blocked with
+// a more specific filter e.g. "chrome-extension://*".
+// The schemes are hardcoded here to avoid dependencies on //extensions and
+// //chrome.
+const char* kBypassBlacklistWildcardForSchemes[] = {
+  // For internal extension URLs e.g. the Bookmark Manager and the File
+  // Manager on Chrome OS.
+  "chrome-extension",
+
+  // NTP on Android.
+  "chrome-native",
+
+  // NTP on other platforms.
+  "chrome-search",
+};
 
 // Maximum filters per policy. Filters over this index are ignored.
 const size_t kMaxFiltersPerPolicy = 1000;
@@ -99,11 +115,27 @@ void ProcessQueryToConditions(
   }
 }
 
+bool BypassBlacklistWildcardForURL(const GURL& url) {
+  const std::string& scheme = url.scheme();
+  for (size_t i = 0; i < arraysize(kBypassBlacklistWildcardForSchemes); ++i) {
+    if (scheme == kBypassBlacklistWildcardForSchemes[i])
+      return true;
+  }
+  return false;
+}
+
 }  // namespace
 
 struct URLBlacklist::FilterComponents {
   FilterComponents() : port(0), match_subdomains(true), allow(true) {}
   ~FilterComponents() {}
+
+  // Returns true if |this| represents the "*" filter in the blacklist.
+  bool IsBlacklistWildcard() const {
+    return !allow && host.empty() && scheme.empty() && path.empty() &&
+           query.empty() && port == 0 && number_of_key_value_pairs == 0 &&
+           match_subdomains;
+  }
 
   std::string scheme;
   std::string host;
@@ -186,6 +218,12 @@ bool URLBlacklist::IsURLBlocked(const GURL& url) const {
   if (!max)
     return false;
 
+  // Some of the internal Chrome URLs are not affected by the "*" in the
+  // blacklist. Note that the "*" is the lowest priority filter possible, so
+  // any higher priority filter will be applied first.
+  if (max->IsBlacklistWildcard() && BypassBlacklistWildcardForURL(url))
+    return false;
+
   return !max->allow;
 }
 
@@ -204,12 +242,12 @@ bool URLBlacklist::FilterToComponents(SegmentURLCallback segment_url,
                                       std::string* query) {
   url::Parsed parsed;
 
-  if (segment_url(filter, &parsed) == kFileScheme) {
+  if (segment_url(filter, &parsed) == url::kFileScheme) {
     base::FilePath file_path;
     if (!net::FileURLToFilePath(GURL(filter), &file_path))
       return false;
 
-    *scheme = kFileScheme;
+    *scheme = url::kFileScheme;
     host->clear();
     *match_subdomains = true;
     *port = 0;
@@ -330,6 +368,10 @@ scoped_refptr<URLMatcherConditionSet> URLBlacklist::CreateConditionSet(
 // static
 bool URLBlacklist::FilterTakesPrecedence(const FilterComponents& lhs,
                                          const FilterComponents& rhs) {
+  // The "*" wildcard is the lowest priority filter.
+  if (rhs.IsBlacklistWildcard())
+    return true;
+
   if (lhs.match_subdomains && !rhs.match_subdomains)
     return false;
   if (!lhs.match_subdomains && rhs.match_subdomains)
