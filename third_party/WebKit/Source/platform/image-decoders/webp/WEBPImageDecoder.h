493859855963946d3c271462aeae30a37115d609
noel@chromium.org <noel@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-19 05:09:34 +0000

    Add clearColorTransform() routine to JPEG/PNG/WEBP decoders
    Resetting the color transform is required in two places in these
    decoders. Define a helper function and call it to do reset. 
     
    BUG=404639
    
    Review URL: https://codereview.chromium.org/480063003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180521 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/image-decoders/webp/WEBPImageDecoder.h
+++ third_party/WebKit/Source/platform/image-decoders/webp/WEBPImageDecoder.h
@@ -65,6 +65,7 @@ private:
 #if USE(QCMSLIB)
     qcms_transform* colorTransform() const { return m_transform; }
     void createColorTransform(const char* data, size_t);
+    void clearColorTransform();
     void readColorProfile();
 
     bool m_haveReadProfile;
