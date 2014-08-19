9be6b591201987c5335468a4def9ce9fb6429e2a
jamesr@chromium.org <jamesr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-19 00:45:31 +0000

    Mojo multiple command buffer support and sample
    TBR=danakj@chromium.org for DEPS file shuffle that moves a "+ui/gfx"
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=290157
    
    Review URL: https://codereview.chromium.org/451753003
    
    Cr-Commit-Position: refs/heads/master@{#290431}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290431 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/examples/pepper_container_app/pepper_container_app.cc
+++ mojo/examples/pepper_container_app/pepper_container_app.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/application/application_delegate.h"
 #include "mojo/public/cpp/application/application_impl.h"
 #include "mojo/public/cpp/system/core.h"
+#include "mojo/services/public/interfaces/gpu/gpu.mojom.h"
 #include "mojo/services/public/interfaces/native_viewport/native_viewport.mojom.h"
 #include "ppapi/c/pp_rect.h"
 #include "ppapi/shared_impl/proxy_lock.h"
@@ -36,6 +37,9 @@ class PepperContainerApp: public ApplicationDelegate,
     app->ConnectToService("mojo:mojo_native_viewport_service", &viewport_);
     viewport_.set_client(this);
 
+    // TODO(jamesr): Should be mojo:mojo_gpu_service
+    app->ConnectToService("mojo:mojo_native_viewport_service", &gpu_service_);
+
     RectPtr rect(Rect::New());
     rect->x = 10;
     rect->y = 10;
@@ -46,7 +50,8 @@ class PepperContainerApp: public ApplicationDelegate,
   }
 
   // NativeViewportClient implementation.
-  virtual void OnCreated() OVERRIDE {
+  virtual void OnCreated(uint64_t native_viewport_id) OVERRIDE {
+    native_viewport_id_ = native_viewport_id;
     ppapi::ProxyAutoLock lock;
 
     plugin_instance_ = plugin_module_->CreateInstance().Pass();
@@ -54,7 +59,7 @@ class PepperContainerApp: public ApplicationDelegate,
       plugin_instance_.reset();
   }
 
-  virtual void OnDestroyed(const mojo::Callback<void()>& callback) OVERRIDE {
+  virtual void OnDestroyed() OVERRIDE {
     ppapi::ProxyAutoLock lock;
 
     if (plugin_instance_) {
@@ -63,7 +68,6 @@ class PepperContainerApp: public ApplicationDelegate,
     }
 
     base::MessageLoop::current()->Quit();
-    callback.Run();
   }
 
   virtual void OnBoundsChanged(RectPtr bounds) OVERRIDE {
@@ -86,14 +90,20 @@ class PepperContainerApp: public ApplicationDelegate,
   // MojoPpapiGlobals::Delegate implementation.
   virtual ScopedMessagePipeHandle CreateGLES2Context() OVERRIDE {
     CommandBufferPtr command_buffer;
-    viewport_->CreateGLES2Context(Get(&command_buffer));
+    SizePtr size = Size::New();
+    size->width = 800;
+    size->width = 600;
+    gpu_service_->CreateOnscreenGLES2Context(
+        native_viewport_id_, size.Pass(), Get(&command_buffer));
     return command_buffer.PassMessagePipe();
   }
 
  private:
   MojoPpapiGlobals ppapi_globals_;
 
+  uint64_t native_viewport_id_;
   NativeViewportPtr viewport_;
+  GpuPtr gpu_service_;
   scoped_refptr<PluginModule> plugin_module_;
   scoped_ptr<PluginInstance> plugin_instance_;
 
--- mojo/mojo.gyp
+++ mojo/mojo.gyp
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'mojo_surfaces_app',
         'mojo_surfaces_app',
         'mojo_surfaces_child_app',
+        'mojo_surfaces_child_gl_app',
         'mojo_surfaces_lib',
         'mojo_surfaces_lib_unittests',
         'mojo_surfaces_service',
