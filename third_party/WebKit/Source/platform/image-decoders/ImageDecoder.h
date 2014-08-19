6eef292692b7185169ea38190d38a73cd9bae30d
noel@chromium.org <noel@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-19 08:42:27 +0000

    Fix include order ImageDecoder.h
    A pre-submit complained to me about include order in this file so
    alpha sort the includes to satisfy that pernickety tool.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/481353002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180535 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/image-decoders/ImageDecoder.h
+++ third_party/WebKit/Source/platform/image-decoders/ImageDecoder.h
@@ -37,8 +37,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "public/platform/Platform.h"
 #include "wtf/Assertions.h"
 #include "wtf/RefPtr.h"
-#include "wtf/text/WTFString.h"
 #include "wtf/Vector.h"
+#include "wtf/text/WTFString.h"
 
 #if USE(QCMSLIB)
 #include "qcms.h"
