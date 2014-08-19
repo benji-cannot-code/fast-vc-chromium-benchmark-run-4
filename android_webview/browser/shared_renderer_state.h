2218aa8f63cfe08f2d110069e3e6c01fa1f87726
boliu@chromium.org <boliu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-19 01:41:52 +0000

    aw: Reset parent draw constraints on tear down
    Without this, the same webview getting attached again but at a different
    part of the view tree would be getting stale and completely wrong parent
    draw constraints.
    
    A classic example of this is when webview going into full screen video
    mode.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/486833002
    
    Cr-Commit-Position: refs/heads/master@{#290450}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290450 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/browser/shared_renderer_state.h
+++ android_webview/browser/shared_renderer_state.h
@@ -58,6 +58,8 @@ class SharedRendererState {
   scoped_ptr<DrawGLInput> PassDrawGLInput();
 
   bool IsInsideHardwareRelease() const;
+  void UpdateDrawConstraints(
+      const ParentCompositorDrawConstraints& parent_draw_constraints);
   void PostExternalDrawConstraintsToChildCompositor(
       const ParentCompositorDrawConstraints& parent_draw_constraints);
 
