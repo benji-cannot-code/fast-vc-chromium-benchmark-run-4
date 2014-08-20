3a25cf14979a6bc6607d2b4845a6f7add9a3eb60
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-20 09:53:45 +0000

    Use scoped_ptr to pass settings in PrintJobWorker and PrinterQuery.
    BUG=374321
    TBR=noamsml
    
    Review URL: https://codereview.chromium.org/488913002
    
    Cr-Commit-Position: refs/heads/master@{#290799}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290799 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/printing/print_job_worker.cc
+++ chrome/browser/printing/print_job_worker.cc
@@ -117,7 +117,7 @@ void PrintJobWorker::GetSettings(
 }
 
 void PrintJobWorker::SetSettings(
-    const base::DictionaryValue* const new_settings) {
+    scoped_ptr<base::DictionaryValue> new_settings) {
   DCHECK(task_runner_->RunsTasksOnCurrentThread());
 
   BrowserThread::PostTask(
@@ -127,11 +127,11 @@ void PrintJobWorker::SetSettings(
                  make_scoped_refptr(owner_),
                  base::Bind(&PrintJobWorker::UpdatePrintSettings,
                             base::Unretained(this),
-                            base::Owned(new_settings))));
+                            base::Passed(&new_settings))));
 }
 
 void PrintJobWorker::UpdatePrintSettings(
-    const base::DictionaryValue* const new_settings) {
+    scoped_ptr<base::DictionaryValue> new_settings) {
   PrintingContext::Result result =
       printing_context_->UpdatePrintSettings(*new_settings);
   GetSettingsDone(result);
