37513fa2ecbaa71b195c67261261926da2373386
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-20 21:40:01 +0000

    Disable NetInternalsTest.netInternalsChromeOSViewStoreDebugLogs which is flaking on Windows.
    BUG=405700
    TBR=ygorshenin@chromium.org
    
    Review URL: https://codereview.chromium.org/492033002
    
    Cr-Commit-Position: refs/heads/master@{#290921}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290921 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/webui/net_internals/chromeos_view.js
+++ chrome/test/data/webui/net_internals/chromeos_view.js
@@ -48,7 +48,7 @@ ResultChecker.prototype = {
 };
 
 TEST_F('NetInternalsTest',
-       'netInternalsChromeOSViewStoreDebugLogs',
+       'DISABLED_netInternalsChromeOSViewStoreDebugLogs',
        function() {
   if (!cr.isChromeOS)
     testDone();
