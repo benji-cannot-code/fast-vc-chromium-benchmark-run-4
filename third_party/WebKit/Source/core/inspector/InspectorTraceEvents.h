7017a6fdd720833d834caec7ed68c17d18ac2811
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-20 10:19:19 +0000

    Revert of DevTools: add initial support for console.time/timeEnd in tracing based Timeline (patchset #3 of https://codereview.chromium.org/464063008/)
    Reason for revert:
    As per comment from nduca. I also don't understand why instrumentation is not reused.
    
    Original issue's description:
    > DevTools: add initial support for console.time/timeEnd in tracing based Timeline
    > 
    > console.time/timeEnd will result in instant trace events. The events will be shown as marker events, i.e. as markers on the Timeline overview.
    > 
    > BUG=361045
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=180635
    
    TBR=nduca@chromium.org,alph@chromium.org,loislo@chromium.org,yurys@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=361045
    
    Review URL: https://codereview.chromium.org/485893006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180640 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorTraceEvents.h
+++ third_party/WebKit/Source/core/inspector/InspectorTraceEvents.h
@@ -150,11 +150,6 @@ public:
     static PassRefPtr<TraceEvent::ConvertableToTraceFormat> data(ExecutionContext*, const String& message);
 };
 
-class InspectorConsoleTimeEvent {
-public:
-    static PassRefPtr<TraceEvent::ConvertableToTraceFormat> data(ExecutionContext*, const String& message);
-};
-
 } // namespace blink
 
 
--- third_party/WebKit/Source/devtools/front_end/timeline/TracingTimelineModel.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TracingTimelineModel.js
@@ -61,7 +61,6 @@ WebInspector.TracingTimelineModel.RecordType = {
 
     TimeStamp: "TimeStamp",
     ConsoleTime: "ConsoleTime",
-    ConsoleTimeEnd: "ConsoleTimeEnd",
 
     ResourceSendRequest: "ResourceSendRequest",
     ResourceReceiveResponse: "ResourceReceiveResponse",
