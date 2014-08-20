9a8fffa26160177947881daebe785648c6abfa7f
zmo@chromium.org <zmo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-20 18:26:50 +0000

    Add logging for ScriptedAnimationController.
    This is for debugging RAF based WebGL conformance tests random timeouts.
    
    BUG=393331
    TEST=bots
    NOTRY=true
    R=kbr@chromium.org,eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/484323002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180672 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/Logging.cpp
+++ third_party/WebKit/Source/platform/Logging.cpp
@@ -68,6 +68,7 @@ WTFLogChannel LogWebAudio =           { WTFLogChannelOff };
 WTFLogChannel LogCompositing =        { WTFLogChannelOff };
 WTFLogChannel LogGamepad =            { WTFLogChannelOff };
 
+WTFLogChannel LogScriptedAnimationController = { WTFLogChannelOff };
 
 WTFLogChannel* getChannelFromName(const String& channelName)
 {
@@ -148,6 +149,9 @@ WTFLogChannel* getChannelFromName(const String& channelName)
 
     if (equalIgnoringCase(channelName, String("Gamepad")))
         return &LogGamepad;
+
+    if (equalIgnoringCase(channelName, String("ScriptedAnimationController")))
+        return &LogScriptedAnimationController;
 
     return 0;
 }
