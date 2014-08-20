30423c23494b3a76886ee127af3d7a12d02d1c4f
tsepez@chromium.org <tsepez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-20 19:23:20 +0000

    Add a X-Requested-With header to URL requests for PPAPI Flash (only).
    This is a revival of Trung's patch in https://codereview.chromium.org/10886047/. 
    The difference is that we provide the header only for the Shockwave Flash
    plugin (plus a test plugin).
    
    BUG=145090
    
    Review URL: https://codereview.chromium.org/451923002
    
    Cr-Commit-Position: refs/heads/master@{#290887}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290887 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/pepper/plugin_module.h
+++ content/renderer/pepper/plugin_module.h
@@ -71,6 +71,7 @@ class CONTENT_EXPORT PluginModule : public base::RefCounted<PluginModule>,
   // all plugin modules. In practice it will be a global singleton that
   // tracks which modules are alive.
   PluginModule(const std::string& name,
+               const std::string& version,
                const base::FilePath& path,
                const ppapi::PpapiPermissions& perms);
 
@@ -135,6 +136,7 @@ class CONTENT_EXPORT PluginModule : public base::RefCounted<PluginModule>,
   PP_Module pp_module() const { return pp_module_; }
 
   const std::string& name() const { return name_; }
+  const std::string& version() const { return version_; }
   const base::FilePath& path() const { return path_; }
   const ppapi::PpapiPermissions& permissions() const { return permissions_; }
 
@@ -257,8 +259,9 @@ class CONTENT_EXPORT PluginModule : public base::RefCounted<PluginModule>,
   // presence of the host_dispatcher_wrapper_ value.
   PepperPluginInfo::EntryPoints entry_points_;
 
-  // The name and file location of the module.
+  // The name, version, and file location of the module.
   const std::string name_;
+  const std::string version_;
   const base::FilePath path_;
 
   ppapi::PpapiPermissions permissions_;
