3ae78a6166227950feab916876843da2f52ba315
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-20 07:31:58 +0000

    DevTools: eye dropper, only pick on mouse down.
    BUG=404375
    
    Review URL: https://codereview.chromium.org/485253002
    
    Cr-Commit-Position: refs/heads/master@{#290784}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290784 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/devtools/renderer_overrides_handler.cc
+++ content/browser/devtools/renderer_overrides_handler.cc
@@ -984,7 +984,8 @@ bool RendererOverridesHandler::HandleMouseEvent(
   if (color_picker_frame_.drawsNothing())
     return true;
 
-  if (event.button == blink::WebMouseEvent::ButtonLeft) {
+  if (event.button == blink::WebMouseEvent::ButtonLeft &&
+      event.type == blink::WebInputEvent::MouseDown) {
     if (last_cursor_x_ < 0 || last_cursor_x_ >= color_picker_frame_.width() ||
         last_cursor_y_ < 0 || last_cursor_y_ >= color_picker_frame_.height()) {
       return true;
@@ -1048,6 +1049,7 @@ void RendererOverridesHandler::UpdateColorPickerCursor() {
       kCursorSize * device_scale_factor,
       kCursorSize * device_scale_factor));
   canvas->scale(device_scale_factor, device_scale_factor);
+  canvas->translate(0.5f, 0.5f);
 
   SkPaint paint;
 
