16f53450145a8ad2707b7d9b061e192fc6bd0329
dconnelly@chromium.org <dconnelly@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-20 18:26:21 +0000

    Add ManagePasswordsBubblePendingViewController and unit tests.
    BUG=328847
    
    Review URL: https://codereview.chromium.org/432393002
    
    Cr-Commit-Position: refs/heads/master@{#290873}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290873 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/ui/cocoa/bubble_combobox.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+
+#ifndef CHROME_BROWSER_UI_COCOA_BUBBLE_COMBOBOX_H_
+#define CHROME_BROWSER_UI_COCOA_BUBBLE_COMBOBOX_H_
+
+#import <Cocoa/Cocoa.h>
+
+namespace ui {
+class ComboboxModel;
+}  // namespace ui
+
+// An NSPopUpButton that auto-populates from a ui::ComboboxModel.
+// By default it comes with a border, small font size, and small control size.
+@interface BubbleCombobox : NSPopUpButton
+// Does not take ownership nor store a pointer to |model|; it is used only for
+// population of the combobox.
+- (id)initWithFrame:(NSRect)frame
+          pullsDown:(BOOL)pullsDown
+              model:(ui::ComboboxModel*)model;
+@end
+
+#endif  // CHROME_BROWSER_UI_COCOA_BUBBLE_COMBOBOX_H_
--- chrome/browser/ui/views/passwords/manage_passwords_bubble_view.h
+++ chrome/browser/ui/views/passwords/manage_passwords_bubble_view.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/timer/timer.h"
 #include "chrome/browser/ui/passwords/manage_passwords_bubble.h"
-#include "chrome/browser/ui/views/passwords/save_password_refusal_combobox_model.h"
+#include "chrome/browser/ui/passwords/save_password_refusal_combobox_model.h"
 #include "ui/views/bubble/bubble_delegate.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/controls/combobox/combobox.h"
