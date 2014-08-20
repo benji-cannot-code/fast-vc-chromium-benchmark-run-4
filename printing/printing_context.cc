ddafde6a8d06704d40b9a3c1dcc764491db4fe1f
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-20 04:12:54 +0000

    Fixed return value in PrintingContext::OnError().
    TBR=noamsml
    BUG=374321
    
    Review URL: https://codereview.chromium.org/484373002
    
    Cr-Commit-Position: refs/heads/master@{#290759}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290759 0039d316-1c4b-4281-b951-d872f2087c98

--- printing/printing_context.cc
+++ printing/printing_context.cc
@@ -45,8 +45,9 @@ void PrintingContext::ResetSettings() {
 }
 
 PrintingContext::Result PrintingContext::OnError() {
+  Result result = abort_printing_ ? CANCEL : FAILED;
   ResetSettings();
-  return abort_printing_ ? CANCEL : FAILED;
+  return result;
 }
 
 PrintingContext::Result PrintingContext::UsePdfSettings() {
