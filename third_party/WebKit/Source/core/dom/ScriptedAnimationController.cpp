7f495f6683ecaa521f86f59d76be5575537b456e
zmo@chromium.org <zmo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-20 23:41:32 +0000

    Add logging for ScriptedAnimationController.
    This is for debugging RAF based WebGL conformance tests random timeouts.
    
    BUG=393331
    TEST=bots
    NOTRY=true
    R=kbr@chromium.org,eseidel@chromium.org
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=180672
    
    Review URL: https://codereview.chromium.org/484323002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180685 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/ScriptedAnimationController.cpp
+++ third_party/WebKit/Source/core/dom/ScriptedAnimationController.cpp
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/inspector/InspectorInstrumentation.h"
 #include "core/inspector/InspectorTraceEvents.h"
 #include "core/loader/DocumentLoader.h"
+#include "platform/Logging.h"
 
 namespace blink {
 
@@ -68,6 +69,7 @@ void ScriptedAnimationController::trace(Visitor* visitor)
 void ScriptedAnimationController::suspend()
 {
     ++m_suspendCount;
+    WTF_LOG(ScriptedAnimationController, "suspend: count = %d", m_suspendCount);
 }
 
 void ScriptedAnimationController::resume()
@@ -76,12 +78,14 @@ void ScriptedAnimationController::resume()
     // even when suspend hasn't (if a tab was created in the background).
     if (m_suspendCount > 0)
         --m_suspendCount;
+    WTF_LOG(ScriptedAnimationController, "resume: count = %d", m_suspendCount);
     scheduleAnimationIfNeeded();
 }
 
 ScriptedAnimationController::CallbackId ScriptedAnimationController::registerCallback(PassOwnPtr<RequestAnimationFrameCallback> callback)
 {
     ScriptedAnimationController::CallbackId id = ++m_nextCallbackId;
+    WTF_LOG(ScriptedAnimationController, "registerCallback: id = %d", id);
     callback->m_cancelled = false;
     callback->m_id = id;
     m_callbacks.append(callback);
@@ -97,6 +101,7 @@ ScriptedAnimationController::CallbackId ScriptedAnimationController::registerCal
 
 void ScriptedAnimationController::cancelCallback(CallbackId id)
 {
+    WTF_LOG(ScriptedAnimationController, "cancelCallback: id = %d", id);
     for (size_t i = 0; i < m_callbacks.size(); ++i) {
         if (m_callbacks[i]->m_id == id) {
             TRACE_EVENT_INSTANT1(TRACE_DISABLED_BY_DEFAULT("devtools.timeline"), "CancelAnimationFrame", "data", InspectorAnimationFrameEvent::data(m_document, id));
@@ -185,6 +190,11 @@ void ScriptedAnimationController::callMediaQueryListListeners()
 
 void ScriptedAnimationController::serviceScriptedAnimations(double monotonicTimeNow)
 {
+    WTF_LOG(ScriptedAnimationController, "serviceScriptedAnimations: #callbacks = %d, #events = %d, #mediaQueryListListeners = %d, count = %d",
+        static_cast<int>(m_callbacks.size()),
+        static_cast<int>(m_eventQueue.size()),
+        static_cast<int>(m_mediaQueryListListeners.size()),
+        m_suspendCount);
     if (!m_callbacks.size() && !m_eventQueue.size() && !m_mediaQueryListListeners.size())
         return;
 
@@ -202,6 +212,7 @@ void ScriptedAnimationController::serviceScriptedAnimations(double monotonicTime
 
 void ScriptedAnimationController::enqueueEvent(PassRefPtrWillBeRawPtr<Event> event)
 {
+    WTF_LOG(ScriptedAnimationController, "enqueueEvent");
     InspectorInstrumentation::didEnqueueEvent(event->target(), event.get());
     m_eventQueue.append(event);
     scheduleAnimationIfNeeded();
@@ -216,6 +227,7 @@ void ScriptedAnimationController::enqueuePerFrameEvent(PassRefPtrWillBeRawPtr<Ev
 
 void ScriptedAnimationController::enqueueMediaQueryChangeListeners(WillBeHeapVector<RefPtrWillBeMember<MediaQueryListListener> >& listeners)
 {
+    WTF_LOG(ScriptedAnimationController, "enqueueMediaQueryChangeListeners");
     for (size_t i = 0; i < listeners.size(); ++i) {
         m_mediaQueryListListeners.add(listeners[i]);
     }
@@ -224,6 +236,12 @@ void ScriptedAnimationController::enqueueMediaQueryChangeListeners(WillBeHeapVec
 
 void ScriptedAnimationController::scheduleAnimationIfNeeded()
 {
+    WTF_LOG(ScriptedAnimationController, "scheduleAnimationIfNeeded: document = %d, count = %d, #callbacks = %d, #events = %d, #mediaQueryListListeners =%d, frameView = %d",
+        m_document ? 1 : 0, m_suspendCount,
+        static_cast<int>(m_callbacks.size()),
+        static_cast<int>(m_eventQueue.size()),
+        static_cast<int>(m_mediaQueryListListeners.size()),
+        m_document && m_document->view() ? 1 : 0);
     if (!m_document)
         return;
 
