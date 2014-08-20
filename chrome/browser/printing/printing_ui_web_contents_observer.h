3b4d730cce4b36cc580d800085129eea3b9c21ed
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-20 07:33:47 +0000

    Delete PrintingUIWebContentsObserver on UI thread.
    BUG=374321
    TBR=noamsml
    
    Review URL: https://codereview.chromium.org/490763003
    
    Cr-Commit-Position: refs/heads/master@{#290785}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290785 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/printing/printing_ui_web_contents_observer.h
+++ chrome/browser/printing/printing_ui_web_contents_observer.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class PrintingUIWebContentsObserver : public content::WebContentsObserver {
  public:
   explicit PrintingUIWebContentsObserver(content::WebContents* web_contents);
+  virtual ~PrintingUIWebContentsObserver();
 
   // Return the parent NativeView of the observed WebContents.
   gfx::NativeView GetParentView();
