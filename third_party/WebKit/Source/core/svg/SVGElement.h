82357168099fa35dfd447368c28c25c4ccec8e8a
kouhei@chromium.org <kouhei@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-20 01:23:32 +0000

    remove SVGElement::isContextElement
    Original suggestion by rob.buis@samsung.com
    
    All SVGElements are context element as |className| is a SVGAnimatedString. This flag is redundant.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/488843003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180614 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGElement.h
+++ third_party/WebKit/Source/core/svg/SVGElement.h
@@ -149,9 +149,6 @@ public:
 
     void invalidateRelativeLengthClients(SubtreeLayoutScope* = 0);
 
-    bool isContextElement() const { return m_isContextElement; }
-    void setContextElement() { m_isContextElement = true; }
-
     void addToPropertyMap(PassRefPtr<SVGAnimatedPropertyBase>);
 
     SVGAnimatedString* className() { return m_className.get(); }
