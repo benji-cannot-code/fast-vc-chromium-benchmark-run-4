d72849286023f97991adc3b8e867e6df89fe92ad
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-20 23:39:50 +0000

    [Telemetry] Suppress red-herring error messages.
    These confuse sheriffs in the log because they are harmless and
    just mean that the browser closed the connection to the webserver
    aburptly.
    
    WPR has the same logic here:
    https://github.com/chromium/web-page-replay/blob/master/httpproxy.py
    
    This eliminates this stack:
    Traceback (most recent call last):
      File "/usr/lib/python2.7/SocketServer.py", line 593, in process_request_thread
        self.finish_request(request, client_address)
      File "/usr/lib/python2.7/SocketServer.py", line 334, in finish_request
        self.RequestHandlerClass(request, client_address, self)
      File "/usr/lib/python2.7/SocketServer.py", line 649, in __init__
        self.handle()
      File "/usr/lib/python2.7/BaseHTTPServer.py", line 342, in handle
        self.handle_one_request()
      File "/usr/lib/python2.7/BaseHTTPServer.py", line 310, in handle_one_request
        self.raw_requestline = self.rfile.readline(65537)
      File "/usr/lib/python2.7/socket.py", line 476, in readline
        data = self._sock.recv(self._rbufsize)
    error: [Errno 104] Connection reset by peer
    
    BUG=
    
    Review URL: https://codereview.chromium.org/386963006
    
    Cr-Commit-Position: refs/heads/master@{#290948}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290948 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/memory_cache_http_server.py
+++ tools/telemetry/telemetry/core/memory_cache_http_server.py
@@ -4,10 +4,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 import BaseHTTPServer
+import errno
 import gzip
 import mimetypes
 import os
 import SimpleHTTPServer
+import socket
 import SocketServer
 import StringIO
 import sys
@@ -24,6 +26,16 @@ class MemoryCacheHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
   protocol_version = 'HTTP/1.1'  # override BaseHTTPServer setting
   wbufsize = -1  # override StreamRequestHandler (a base class) setting
+
+  def handle(self):
+    try:
+      BaseHTTPServer.BaseHTTPRequestHandler.handle(self)
+    except socket.error, e:
+      # Connection reset errors happen all the time due to the browser closing
+      # without terminating the connection properly.  They can be safely
+      # ignored.
+      if e[0] != errno.ECONNRESET:
+        raise
 
   def do_GET(self):
     """Serve a GET request."""
