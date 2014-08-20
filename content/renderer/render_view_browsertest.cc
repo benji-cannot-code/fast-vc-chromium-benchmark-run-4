8eb3ece42ae794cb5c58382db89281eb76431cfb
creis@chromium.org <creis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-20 13:17:52 +0000

    Don't swap out the old RenderFrameHost until the new one commits.
    This is a major cleanup of cross-process navigation that delays running
    the old render frame's unload handler until the new render frame has
    committed.  As a result, there is no period of time when swapped out 
    RenderFrameHosts are visible.
    
    Most cross-process navigations are no longer paused in the network 
    stack (unless they require a transfer), and the WasSwappedOut IPC 
    message is removed.  The STATE_WAITING_FOR_COMMIT and 
    STATE_WAITING_FOR_UNLOAD_ACK states in RenderViewHostImpl 
    can also be removed.
    
    Overview doc with navigation timeline diagrams:
    https://docs.google.com/document/d/1nLUxn4nV5g0HVGf-n34us5kQ0-erE-7050lzpMSxa4Q/edit?usp=sharing
    
    BUG=402020
    TEST=Unload handlers still run on cross-process navigations.
    
    Review URL: https://codereview.chromium.org/464593003
    
    Cr-Commit-Position: refs/heads/master@{#290828}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290828 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_view_browsertest.cc
+++ content/renderer/render_view_browsertest.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/accessibility/renderer_accessibility_focus_only.h"
 #include "content/renderer/history_controller.h"
 #include "content/renderer/history_serialization.h"
+#include "content/renderer/render_process.h"
 #include "content/renderer/render_view_impl.h"
 #include "content/shell/browser/shell.h"
 #include "content/shell/browser/shell_browser_context.h"
@@ -497,6 +498,9 @@ TEST_F(RenderViewImplTest, DecideNavigationPolicyForWebUI) {
 TEST_F(RenderViewImplTest, SendSwapOutACK) {
   LoadHTML("<div>Page A</div>");
   int initial_page_id = view_page_id();
+
+  // Increment the ref count so that we don't exit when swapping out.
+  RenderProcess::current()->AddRefProcess();
 
   // Respond to a swap out request.
   view()->main_render_frame()->OnSwapOut(kProxyRoutingId);
--- content/renderer/render_widget.h
+++ content/renderer/render_widget.h
@@ -335,6 +335,10 @@ class CONTENT_EXPORT RenderWidget
   // active RenderWidgets.
   void SetSwappedOut(bool is_swapped_out);
 
+  // Allows the process to exit once the unload handler has finished, if there
+  // are no other active RenderWidgets.
+  void WasSwappedOut();
+
   void FlushPendingInputEventAck();
   void DoDeferredClose();
   void DoDeferredSetWindowRect(const blink::WebRect& pos);
@@ -372,7 +376,6 @@ class CONTENT_EXPORT RenderWidget
   virtual void OnWasHidden();
   virtual void OnWasShown(bool needs_repainting,
                           const ui::LatencyInfo& latency_info);
-  virtual void OnWasSwappedOut();
   void OnCreateVideoAck(int32 video_id);
   void OnUpdateVideoAck(int32 video_id);
   void OnRequestMoveAck();
