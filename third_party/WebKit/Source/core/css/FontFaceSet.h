040b84a56645f672405bb3166e1b48807785661a
jl@opera.com <jl@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-21 16:02:43 +0000

    Make some ScriptValue references const
    The functions FontFaceSet::forEach() and Headers::forEach() are called
    from the bindings layer, and by having non-const reference arguments, they
    somewhat restrict what we can do in the bindings layer.
    
    Since there is no reason for the arguments to be non-const references,
    just make them const.
    
    Review URL: https://codereview.chromium.org/478233003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180719 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/FontFaceSet.h
+++ third_party/WebKit/Source/core/css/FontFaceSet.h
@@ -81,7 +81,7 @@ public:
     void add(FontFace*, ExceptionState&);
     void clear();
     bool remove(FontFace*, ExceptionState&);
-    void forEach(PassOwnPtr<FontFaceSetForEachCallback>, ScriptValue& thisArg) const;
+    void forEach(PassOwnPtr<FontFaceSetForEachCallback>, const ScriptValue& thisArg) const;
     void forEach(PassOwnPtr<FontFaceSetForEachCallback>) const;
     bool has(FontFace*, ExceptionState&) const;
 
@@ -137,7 +137,7 @@ private:
     bool hasLoadedFonts() const { return !m_loadedFonts.isEmpty() || !m_failedFonts.isEmpty(); }
 
     bool inActiveDocumentContext() const;
-    void forEachInternal(PassOwnPtr<FontFaceSetForEachCallback>, ScriptValue* thisArg) const;
+    void forEachInternal(PassOwnPtr<FontFaceSetForEachCallback>, const ScriptValue* thisArg) const;
     void addToLoadingFonts(PassRefPtrWillBeRawPtr<FontFace>);
     void removeFromLoadingFonts(PassRefPtrWillBeRawPtr<FontFace>);
     void fireLoadingEvent();
