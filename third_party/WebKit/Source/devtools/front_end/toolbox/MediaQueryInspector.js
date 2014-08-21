511d3dcdda6d70ab7653bf3cf173c684f5f922f0
dgozman@chromium.org <dgozman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-21 22:20:50 +0000

    [DevTools] Show the number of media queries and disable MQI button.
    BUG=none
    
    Review URL: https://codereview.chromium.org/490163002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180743 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/toolbox/MediaQueryInspector.js
+++ third_party/WebKit/Source/devtools/front_end/toolbox/MediaQueryInspector.js
@@ -40,7 +40,8 @@ WebInspector.MediaQueryInspector.Section = {
 }
 
 WebInspector.MediaQueryInspector.Events = {
-    HeightUpdated: "HeightUpdated"
+    HeightUpdated: "HeightUpdated",
+    CountUpdated: "CountUpdated"
 }
 
 WebInspector.MediaQueryInspector.prototype = {
@@ -325,7 +326,7 @@ WebInspector.MediaQueryInspector.prototype = {
                 markers.push(lastMarker);
             }
         }
-        var heightChanges = this.element.children.length !== markers.length;
+        var countChanges = this.element.children.length !== markers.length;
 
         var scrollTop = this.element.scrollTop;
         this.element.removeChildren();
@@ -338,8 +339,10 @@ WebInspector.MediaQueryInspector.prototype = {
         }
         this.element.scrollTop = scrollTop;
         this.element.classList.toggle("media-inspector-view-empty", !this.element.children.length);
-        if (heightChanges)
+        if (countChanges) {
+            this.dispatchEventToListeners(WebInspector.MediaQueryInspector.Events.CountUpdated, markers.length);
             this.dispatchEventToListeners(WebInspector.MediaQueryInspector.Events.HeightUpdated);
+        }
     },
 
     /**
