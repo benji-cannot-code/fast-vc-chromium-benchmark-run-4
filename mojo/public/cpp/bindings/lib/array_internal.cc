bf9cff179f2f789f1f3706f3a5d55b35ecba3123
yzshen@chromium.org <yzshen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-21 13:51:30 +0000

    Mojo C++ bindings: better log message for serialization warnings.
    BUG=324170
    TEST=None
    
    Review URL: https://codereview.chromium.org/473793004
    
    Cr-Commit-Position: refs/heads/master@{#291058}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@291058 0039d316-1c4b-4281-b951-d872f2087c98

--- mojo/public/cpp/bindings/lib/array_internal.cc
+++ mojo/public/cpp/bindings/lib/array_internal.cc
@@ -5,8 +5,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "mojo/public/cpp/bindings/lib/array_internal.h"
 
+#include <sstream>
+
 namespace mojo {
 namespace internal {
+
+std::string MakeMessageWithArrayIndex(const char* message,
+                                      size_t size,
+                                      size_t index) {
+  std::ostringstream stream;
+  stream << message << ": array size - " << size << "; index - " << index;
+  return stream.str();
+}
+
+std::string MakeMessageWithExpectedArraySize(const char* message,
+                                             size_t size,
+                                             size_t expected_size) {
+  std::ostringstream stream;
+  stream << message << ": array size - " << size << "; expected size - "
+         << expected_size;
+  return stream.str();
+}
 
 ArrayDataTraits<bool>::BitRef::~BitRef() {
 }
--- mojo/public/cpp/bindings/lib/array_serialization.h
+++ mojo/public/cpp/bindings/lib/array_serialization.h
@@ -104,7 +104,10 @@ template <typename H> struct ArraySerializer<ScopedHandleBase<H>, H, true> {
       output->at(i) = input[i].release();  // Transfer ownership of the handle.
       MOJO_INTERNAL_DLOG_SERIALIZATION_WARNING(
           !element_is_nullable && !output->at(i).is_valid(),
-          VALIDATION_ERROR_UNEXPECTED_INVALID_HANDLE);
+          VALIDATION_ERROR_UNEXPECTED_INVALID_HANDLE,
+          MakeMessageWithArrayIndex(
+              "invalid handle in array expecting valid handles",
+              input.size(), i));
     }
   }
   static void DeserializeElements(
@@ -135,7 +138,9 @@ template <typename S> struct ArraySerializer<S, typename S::Data_*, true> {
       output->at(i) = element;
       MOJO_INTERNAL_DLOG_SERIALIZATION_WARNING(
           !element_is_nullable && !element,
-          VALIDATION_ERROR_UNEXPECTED_NULL_POINTER);
+          VALIDATION_ERROR_UNEXPECTED_NULL_POINTER,
+          MakeMessageWithArrayIndex(
+              "null in array expecting valid pointers", input.size(), i));
     }
   }
   static void DeserializeElements(
@@ -194,7 +199,9 @@ template <> struct ArraySerializer<String, String_Data*, false> {
       output->at(i) = element;
       MOJO_INTERNAL_DLOG_SERIALIZATION_WARNING(
           !element_is_nullable && !element,
-          VALIDATION_ERROR_UNEXPECTED_NULL_POINTER);
+          VALIDATION_ERROR_UNEXPECTED_NULL_POINTER,
+          MakeMessageWithArrayIndex(
+              "null in array expecting valid strings", input.size(), i));
     }
   }
   static void DeserializeElements(
@@ -223,7 +230,10 @@ inline void SerializeArray_(Array<E> input, internal::Buffer* buf,
     MOJO_INTERNAL_DLOG_SERIALIZATION_WARNING(
         ValidateParams::expected_num_elements != 0 &&
             input.size() != ValidateParams::expected_num_elements,
-        internal::VALIDATION_ERROR_UNEXPECTED_ARRAY_HEADER);
+        internal::VALIDATION_ERROR_UNEXPECTED_ARRAY_HEADER,
+        internal::MakeMessageWithExpectedArraySize(
+            "fixed-size array has wrong number of elements",
+            input.size(), ValidateParams::expected_num_elements));
 
     internal::Array_Data<F>* result =
         internal::Array_Data<F>::New(input.size(), buf);
