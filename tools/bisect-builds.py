b59550b688661334319d561f68b6a6178572a761
pshenoy@chromium.org <pshenoy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-21 18:41:23 +0000

    bisect-builds.py: Fix official build bisect for Windows platform.
    BUG=None
    NOTRY=True
    
    Review URL: https://codereview.chromium.org/498473002
    
    Cr-Commit-Position: refs/heads/master@{#291142}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@291142 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/bisect-builds.py
+++ tools/bisect-builds.py
@@ -159,6 +159,8 @@ class PathContext(object):
         self._binary_name = 'Google Chrome.app/Contents/MacOS/Google Chrome'
       elif self.platform == 'win':
         self._listing_platform_dir = 'win/'
+        self.archive_name = 'chrome-win.zip'
+        self._archive_extract_dir = 'chrome-win'
     else:
       if self.platform in ('linux', 'linux64', 'linux-arm'):
         self.archive_name = 'chrome-linux.zip'
@@ -446,7 +448,6 @@ class PathContext(object):
     revision_re = re.compile(r'(\d\d\.\d\.\d{4}\.\d+)')
     build_numbers = filter(lambda b: revision_re.search(b), build_numbers)
     final_list = []
-    i = 0
     parsed_build_numbers = [LooseVersion(x) for x in build_numbers]
     connection = httplib.HTTPConnection(GOOGLE_APIS_URL)
     for build_number in sorted(parsed_build_numbers):
@@ -456,7 +457,6 @@ class PathContext(object):
         continue
       path = ('/' + GS_BUCKET_NAME + '/' + str(build_number) + '/' +
               self._listing_platform_dir + self.archive_name)
-      i = i + 1
       connection.request('HEAD', path)
       response = connection.getresponse()
       if response.status == 200:
