ce8403f449ffc0baac6c7b7ce8a8223dc3fd94d8
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-21 13:21:34 +0000

    DevTools: add initial support for console.time/timeEnd in tracing based Timeline
    Existing async events with category blink.timeline are reused for DevTools needs. The events will be shown as marker events, i.e. as markers on the Timeline overview.
    
    BUG=361045
    R=caseq@chromium.org
    
    Review URL: https://codereview.chromium.org/497493002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180711 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sdk/TracingModel.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/TracingModel.js
@@ -71,6 +71,8 @@ WebInspector.TracingModel.MetadataEvent = {
 
 WebInspector.TracingModel.DevToolsMetadataEventCategory = "disabled-by-default-devtools.timeline";
 
+WebInspector.TracingModel.ConsoleEventCategory = "blink.console";
+
 WebInspector.TracingModel.FrameLifecycleEventCategory = "cc,devtools";
 
 WebInspector.TracingModel.DevToolsMetadataEvent = {
