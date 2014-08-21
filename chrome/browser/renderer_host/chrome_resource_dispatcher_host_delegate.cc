1603bfa99330d6704925ecc45b7cd5235ccd2089
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-21 14:59:42 +0000

    Only build various flash/pnacl/ppapi code when they are enabled.
    Review URL: https://codereview.chromium.org/471263003
    
    Cr-Commit-Position: refs/heads/master@{#291069}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@291069 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/chrome_resource_dispatcher_host_delegate.cc
+++ chrome/browser/renderer_host/chrome_resource_dispatcher_host_delegate.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/component_updater/component_updater_resource_throttle.h"
-#include "chrome/browser/component_updater/pnacl/pnacl_component_installer.h"
 #include "chrome/browser/content_settings/host_content_settings_map.h"
 #include "chrome/browser/download/download_request_limiter.h"
 #include "chrome/browser/download/download_resource_throttle.h"
@@ -53,6 +52,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/request_priority.h"
 #include "net/http/http_response_headers.h"
 #include "net/url_request/url_request.h"
+
+#if !defined(DISABLE_NACL)
+#include "chrome/browser/component_updater/pnacl/pnacl_component_installer.h"
+#endif
 
 #if defined(ENABLE_CONFIGURATION_POLICY)
 #include "components/policy/core/common/cloud/policy_header_io_helper.h"
@@ -224,6 +227,7 @@ void LaunchURL(const GURL& url, int render_process_id, int render_view_id) {
 }
 #endif  // !defined(OS_ANDROID)
 
+#if !defined(DISABLE_NACL)
 void AppendComponentUpdaterThrottles(
     net::URLRequest* request,
     content::ResourceContext* resource_context,
@@ -252,8 +256,9 @@ void AppendComponentUpdaterThrottles(
         component_updater::GetOnDemandResourceThrottle(cus, crx_id));
   }
 }
+#endif  // !defined(DISABLE_NACL)
 
-}  // end namespace
+}  // namespace
 
 ChromeResourceDispatcherHostDelegate::ChromeResourceDispatcherHostDelegate(
     prerender::PrerenderTracker* prerender_tracker)
@@ -403,12 +408,14 @@ void ChromeResourceDispatcherHostDelegate::RequestBeginning(
                                   resource_context,
                                   resource_type,
                                   throttles);
+#if defined(ENABLE_PLUGINS)
   if (!is_prerendering) {
     AppendComponentUpdaterThrottles(request,
                                     resource_context,
                                     resource_type,
                                     throttles);
   }
+#endif
 }
 
 void ChromeResourceDispatcherHostDelegate::DownloadStarting(
