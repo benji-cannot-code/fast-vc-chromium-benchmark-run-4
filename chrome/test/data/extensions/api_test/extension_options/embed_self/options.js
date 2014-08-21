cb5a5e2bf0fdfe58c6e2749632406a81e2138dd2
ericzeng@chromium.org <ericzeng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-21 23:28:04 +0000

    Allow links inside <extensionoptions> to be opened in new tabs
    Extension options pages often have links to external pages as well as
    internal extension pages. When embedded in an <extensionoptions> element
    open external links in a new tab, and internal links in the embedded
    view (unless specified otherwise).
    
    BUG=386838
    
    Review URL: https://codereview.chromium.org/471053003
    
    Cr-Commit-Position: refs/heads/master@{#291241}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@291241 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/api_test/extension_options/embed_self/options.js
+++ chrome/test/data/extensions/api_test/extension_options/embed_self/options.js
@@ -45,5 +45,14 @@ chrome.runtime.sendMessage('ready', function(command) {
           });
         });
       });
+
+      break;
+
+    case 'externalLinksOpenInNewTab':
+      var link = document.getElementById('link');
+      chrome.test.runWithUserGesture(function() {
+        link.click();
+        chrome.runtime.sendMessage('done');
+      }.bind(link));
   }
 });
