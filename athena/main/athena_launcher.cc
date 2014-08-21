82a4b7664885040fe83627e01bead9724f708aa3
skuhne@chromium.org <skuhne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-21 22:57:10 +0000

    Athena: Adding basic resource management framework (un-/re-loading) of V2 applications
    Functionality:
    
    The |AppRegistry| has for each running application an |AppActivityRegistry|.
    
    The |AppActivityRegistry| knows all activities associated with the application it represents.
    It can furthermore shut the app entirely down upon resource manager request. It will then create
    an |AppActivityProxy| for the overview mode which shows a placeholder for an unloaded app. This
    placeholder can then ask the |AppActivityRegistry| to restart the application again.
    
    A shutdown request for the application is only performed when all activities were marked for
    UNLOAD.
    
    If there were multiple activities upon shutdown for one app, the app has to take care of
    re-creating all windows and thus re-creating all activities. Since an activity match cannot
    be performed, the |AppActivityProxy| will only be shown once and it will show in the location
    of the most recently used activity of that app. If we later on find an app which really uses
    multiple windows and it is imperative to keep the history for all of them tact & the app is
    recreating them properly, (a lot of if's) we can revisit the single |AppActivityProxy| and
    try to address it in a cleaner way, but at this time that seems rather un-useful since it is
    not known if required.
    
    BUG=388085
    TEST=AppActivityTest.*
    
    Review URL: https://codereview.chromium.org/477523002
    
    Cr-Commit-Position: refs/heads/master@{#291221}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@291221 0039d316-1c4b-4281-b951-d872f2087c98

--- athena/main/athena_launcher.cc
+++ athena/main/athena_launcher.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "athena/activity/public/activity_factory.h"
 #include "athena/activity/public/activity_manager.h"
+#include "athena/content/public/app_registry.h"
 #include "athena/content/public/content_activity_factory.h"
 #include "athena/content/public/content_app_model_builder.h"
 #include "athena/home/public/home_card.h"
@@ -120,6 +121,7 @@ void StartAthenaEnv(aura::Window* root_window,
   athena::InputManager::Create()->OnRootWindowCreated(root_window);
   athena::ScreenManager::Create(delegate, root_window);
   athena::WindowManager::Create();
+  athena::AppRegistry::Create();
   SetupBackgroundImage();
 
   athena::ScreenManager::Get()->GetContext()->SetProperty(
@@ -152,6 +154,7 @@ void ShutdownAthena() {
   athena::ActivityFactory::Shutdown();
   athena::ActivityManager::Shutdown();
   athena::HomeCard::Shutdown();
+  athena::AppRegistry::ShutDown();
   athena::WindowManager::Shutdown();
   athena::ScreenManager::Shutdown();
   athena::InputManager::Shutdown();
