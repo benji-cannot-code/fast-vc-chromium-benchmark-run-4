862ba2136d953295455364249956f93fdf5acc75
jdduke@chromium.org <jdduke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-22 18:57:44 +0000

    Plumb selection edge points through the compositor
    Previously, a rect was used to convey selection bound edge geometry. This
    proved confusing and problematic, particularly for non-axis-aligned text.
    Instead, start using the bound edge points provided by Blink.
    
    This depends on the Blink change http://codereview.chromium.org/495673003/.
    
    BUG=405666
    
    Review URL: https://codereview.chromium.org/494823002
    
    Cr-Commit-Position: refs/heads/master@{#291460}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@291460 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/output/viewport_selection_bound.h
+++ cc/output/viewport_selection_bound.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "cc/base/cc_export.h"
 #include "cc/input/selection_bound_type.h"
-#include "ui/gfx/geometry/rect_f.h"
+#include "ui/gfx/geometry/point_f.h"
 
 namespace cc {
 
@@ -18,7 +18,8 @@ struct CC_EXPORT ViewportSelectionBound {
   ~ViewportSelectionBound();
 
   SelectionBoundType type;
-  gfx::RectF viewport_rect;
+  gfx::PointF edge_top;
+  gfx::PointF edge_bottom;
   bool visible;
 };
 
--- content/browser/renderer_host/input/touch_selection_controller.h
+++ content/browser/renderer_host/input/touch_selection_controller.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_RENDERER_HOST_INPUT_TOUCH_SELECTION_CONTROLLER_H_
 #define CONTENT_BROWSER_RENDERER_HOST_INPUT_TOUCH_SELECTION_CONTROLLER_H_
 
+#include "cc/output/viewport_selection_bound.h"
 #include "content/browser/renderer_host/input/selection_event_type.h"
 #include "content/browser/renderer_host/input/touch_handle.h"
 #include "content/common/content_export.h"
@@ -47,12 +48,8 @@ class CONTENT_EXPORT TouchSelectionController : public TouchHandleClient {
   // To be called when the selection bounds have changed.
   // Note that such updates will trigger handle updates only if preceded
   // by an appropriate call to allow automatic showing.
-  void OnSelectionBoundsChanged(const gfx::RectF& start_rect,
-                                TouchHandleOrientation start_orientation,
-                                bool start_visible,
-                                const gfx::RectF& end_rect,
-                                TouchHandleOrientation end_orientation,
-                                bool end_visible);
+  void OnSelectionBoundsChanged(const cc::ViewportSelectionBound& start,
+                                const cc::ViewportSelectionBound& end);
 
   // Allows touch-dragging of the handle.
   // Returns true iff the event was consumed, in which case the caller should
@@ -108,10 +105,10 @@ class CONTENT_EXPORT TouchSelectionController : public TouchHandleClient {
   void DeactivateSelection();
   void ResetCachedValuesIfInactive();
 
-  gfx::PointF GetStartPosition() const;
-  gfx::PointF GetEndPosition() const;
-  float GetStartLineHeight() const;
-  float GetEndLineHeight() const;
+  const gfx::PointF& GetStartPosition() const;
+  const gfx::PointF& GetEndPosition() const;
+  gfx::Vector2dF GetStartLineOffset() const;
+  gfx::Vector2dF GetEndLineOffset() const;
   bool GetStartVisible() const;
   bool GetEndVisible() const;
   TouchHandle::AnimationStyle GetAnimationStyle(bool was_active) const;
@@ -120,12 +117,10 @@ class CONTENT_EXPORT TouchSelectionController : public TouchHandleClient {
 
   InputEventType response_pending_input_event_;
 
-  gfx::RectF start_rect_;
+  cc::ViewportSelectionBound start_;
+  cc::ViewportSelectionBound end_;
   TouchHandleOrientation start_orientation_;
-  bool start_visible_;
-  gfx::RectF end_rect_;
   TouchHandleOrientation end_orientation_;
-  bool end_visible_;
 
   scoped_ptr<TouchHandle> insertion_handle_;
   bool is_insertion_active_;
