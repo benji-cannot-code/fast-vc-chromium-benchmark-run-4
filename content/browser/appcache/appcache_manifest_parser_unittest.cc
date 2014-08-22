5bff73cf63d4de8850ed198147094838d2a2721d
mlamouri@chromium.org <mlamouri@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-22 12:51:36 +0000

    Rename appcache/manifest_parser* to appcache/appcache_manifest_parser*.
    The manifest_parser.{h,cc} files are colliding with new files I'm
    creating for the Web Manifest feature. Appcache manifest files should
    be prefixed by "appcache".
    
    BUG=None
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/469413003
    
    Cr-Commit-Position: refs/heads/master@{#291388}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@291388 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/appcache/appcache_manifest_parser_unittest.cc
+++ content/browser/appcache/appcache_manifest_parser_unittest.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "content/browser/appcache/manifest_parser.h"
+#include "content/browser/appcache/appcache_manifest_parser.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
 
