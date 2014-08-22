459c75b23110a65d04e93bcd24f2ffce09b53106
dmazzoni@chromium.org <dmazzoni@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-22 22:42:55 +0000

    Fix accessibility crash when the same SVG remote image is referenced twice in the same document.
    BUG=383978
    
    Review URL: https://codereview.chromium.org/496403002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180798 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/accessibility/AXSVGRoot.h
+++ third_party/WebKit/Source/core/accessibility/AXSVGRoot.h
@@ -42,7 +42,7 @@ public:
     static PassRefPtr<AXSVGRoot> create(RenderObject*);
     virtual ~AXSVGRoot();
 
-    void setParent(AXObject* parent) { m_parent = parent; }
+    void setParent(AXObject*);
 
 private:
     AXObject* m_parent;
