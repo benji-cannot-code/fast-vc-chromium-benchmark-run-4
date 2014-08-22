8b776f29964ba5ff46be21990adbc77721667465
rileya@chromium.org <rileya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-22 00:59:11 +0000

    Reduce demuxer benchmark iterations.
    BUG=399002
    
    Review URL: https://codereview.chromium.org/495193002
    
    Cr-Commit-Position: refs/heads/master@{#291284}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@291284 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/demuxer_perftest.cc
+++ media/base/demuxer_perftest.cc
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
-static const int kBenchmarkIterations = 500;
+static const int kBenchmarkIterations = 100;
 
 class DemuxerHostImpl : public media::DemuxerHost {
  public:
