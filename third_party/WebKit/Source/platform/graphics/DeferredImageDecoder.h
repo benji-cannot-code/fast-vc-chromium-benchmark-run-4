b48e12f056e83d8f27751694ada10ecf275fb708
hclam@chromium.org <hclam@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-22 00:13:04 +0000

    Image decoding: Remove deprecated image caching code
    ImageDecodingStore was used to cache images for impl side painting on
    Android. Now impl side painting is using image cache in Skia.
    ImageDecodingStore is only used to cache decoders.
    
    Deleted 1k lines of code.
    
    Review URL: https://codereview.chromium.org/485833005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180746 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/DeferredImageDecoder.h
+++ third_party/WebKit/Source/platform/graphics/DeferredImageDecoder.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define DeferredImageDecoder_h
 
 #include "SkBitmap.h"
+#include "SkPixelRef.h"
 #include "platform/PlatformExport.h"
 #include "platform/geometry/IntSize.h"
 #include "platform/graphics/ImageFrameGenerator.h"
--- third_party/WebKit/Source/platform/graphics/ImageFrameGenerator.cpp
+++ third_party/WebKit/Source/platform/graphics/ImageFrameGenerator.cpp
@@ -74,7 +74,7 @@ ImageFrameGenerator::ImageFrameGenerator(const SkISize& fullSize, PassRefPtr<Sha
     : m_fullSize(fullSize)
     , m_isMultiFrame(isMultiFrame)
     , m_decodeFailedAndEmpty(false)
-    , m_decodeCount(ScaledImageFragment::FirstPartialImage)
+    , m_decodeCount(0)
 {
     setData(data.get(), allDataReceived);
 }
@@ -111,7 +111,7 @@ bool ImageFrameGenerator::decodeAndScale(const SkImageInfo& info, size_t index, 
     if (m_decodeFailedAndEmpty)
         return 0;
 
-    TRACE_EVENT2("blink", "ImageFrameGenerator::decodeAndScale", "generator", this, "decodeCount", static_cast<int>(m_decodeCount));
+    TRACE_EVENT2("blink", "ImageFrameGenerator::decodeAndScale", "generator", this, "decodeCount", m_decodeCount);
 
     m_externalAllocator = adoptPtr(new ExternalMemoryAllocator(info, pixels, rowBytes));
 
@@ -176,7 +176,7 @@ SkBitmap ImageFrameGenerator::tryToResumeDecode(const SkISize& scaledSize, size_
         else
             ImageDecodingStore::instance()->unlockDecoder(this, decoder);
     } else if (!removeDecoder) {
-        ImageDecodingStore::instance()->insertDecoder(this, decoderContainer.release(), false);
+        ImageDecodingStore::instance()->insertDecoder(this, decoderContainer.release());
     }
     return fullSizeImage;
 }
