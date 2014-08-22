ecad0ae7b57fb4662a67dd7ebe56c0ae97833724
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-22 21:39:09 +0000

    Mark some destructors as virtual.
    Since the classes have virtual functions.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/497213002
    
    Cr-Commit-Position: refs/heads/master@{#291510}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@291510 0039d316-1c4b-4281-b951-d872f2087c98

--- components/rappor/byte_vector_utils.h
+++ components/rappor/byte_vector_utils.h
@@ -44,7 +44,7 @@ class ByteVectorGenerator {
  public:
   explicit ByteVectorGenerator(size_t byte_count);
 
-  ~ByteVectorGenerator();
+  virtual ~ByteVectorGenerator();
 
   // Generates a random byte vector where the bits are independent random
   // variables which are true with the given |probability|.
@@ -81,7 +81,7 @@ class HmacByteVectorGenerator : public ByteVectorGenerator {
                           const std::string& entropy_input,
                           const std::string& personalization_string);
 
-  ~HmacByteVectorGenerator();
+  virtual ~HmacByteVectorGenerator();
 
   // Generates a random string suitable for passing to the constructor as
   // |entropy_input|.
