70c02c036910a74590c2cd460aee56e87c488e8e
hubbe@chromium.org <hubbe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-22 18:29:00 +0000

    Implement adaptive target delay extension
    Adds new call on cast_sender: SetTargetPlayoutDelay
    Caller is responsible for negotiating with the receiver if the extension is actually supported or not.
    
    BUG=405339
    
    Review URL: https://codereview.chromium.org/493823002
    
    Cr-Commit-Position: refs/heads/master@{#291453}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@291453 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ media/cast/net/rtp/rtp_defines.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+namespace media {
+namespace cast {
+
+static const uint16 kRtpHeaderLength = 12;
+static const uint16 kCastHeaderLength = 7;
+static const uint8 kRtpExtensionBitMask = 0x10;
+static const uint8 kCastKeyFrameBitMask = 0x80;
+static const uint8 kCastReferenceFrameIdBitMask = 0x40;
+static const uint8 kRtpMarkerBitMask = 0x80;
+static const uint8 kCastExtensionCountmask = 0x3f;
+
+// Cast RTP extensions.
+static const uint8 kCastRtpExtensionAdaptiveLatency = 1;
+
+}  // namespace cast
+}  // namespace media
