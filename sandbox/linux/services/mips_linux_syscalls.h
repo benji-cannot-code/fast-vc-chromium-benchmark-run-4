a525a6dbad6a192a2f9d686a1c060c44f9b988d9
leecam@chromium.org <leecam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-22 02:02:15 +0000

    sandbox: Update known syscalls to kernel 3.17
    Updated Arm, Mips, x86_32 and x86_64 syscall defines
    to match the latest kernel 3.17 at the time of this commit.
    
    BUG=406123
    TEST=sandbox_linux_unittests
    
    Review URL: https://codereview.chromium.org/493603003
    
    Cr-Commit-Position: refs/heads/master@{#291315}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@291315 0039d316-1c4b-4281-b951-d872f2087c98

--- sandbox/linux/services/mips_linux_syscalls.h
+++ sandbox/linux/services/mips_linux_syscalls.h
@@ -1410,4 +1410,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define __NR_finit_module (__NR_Linux + 348)
 #endif
 
+#if !defined(__NR_sched_setattr)
+#define __NR_sched_setattr (__NR_Linux + 349)
+#endif
+
+#if !defined(__NR_sched_getattr)
+#define __NR_sched_getattr (__NR_Linux + 350)
+#endif
+
+#if !defined(__NR_renameat2)
+#define __NR_renameat2 (__NR_Linux + 351)
+#endif
+
+#if !defined(__NR_seccomp)
+#define __NR_seccomp (__NR_Linux + 352)
+#endif
+
 #endif  // SANDBOX_LINUX_SERVICES_MIPS_LINUX_SYSCALLS_H_
