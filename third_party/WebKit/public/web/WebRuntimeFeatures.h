a02998b3a442a240a0e8230e222c048fc66421f7
heeyoun.lee@samsung.com <heeyoun.lee@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-22 09:10:44 +0000

    Remove SpeechSynthesis runtime flag (status=stable)
    BUG=402536
    
    Review URL: https://codereview.chromium.org/482273005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180763 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/web/WebRuntimeFeatures.h
+++ third_party/WebKit/public/web/WebRuntimeFeatures.h
@@ -101,8 +101,6 @@ public:
 
     BLINK_EXPORT static void enableSessionStorage(bool);
 
-    BLINK_EXPORT static void enableSpeechSynthesis(bool);
-
     BLINK_EXPORT static void enableTouch(bool);
 
     BLINK_EXPORT static void enableTouchIconLoading(bool);
