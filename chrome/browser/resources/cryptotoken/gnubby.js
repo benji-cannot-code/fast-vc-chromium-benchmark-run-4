d493205af350289a6ad9a3431c9591d98d480c43
arnarb@chromium.org <arnarb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-22 07:24:59 +0000

    Tolerate that CMD_SYNC does not exist (per U2F spec).
    BUG=405904
    R=mschilder,juanlang,xiyuan
    
    Review URL: https://codereview.chromium.org/495033002
    
    Cr-Commit-Position: refs/heads/master@{#291351}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@291351 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/cryptotoken/gnubby.js
+++ chrome/browser/resources/cryptotoken/gnubby.js
@@ -524,7 +524,10 @@ Gnubby.prototype.sync = function(cb) {
   }
 
   function sendInitSentinel() {
-    var cid = Gnubby.BROADCAST_CID;
+    var cid = self.cid;
+    if (cid == Gnubby.defaultChannelId_(self.gnubbyInstance, self.which)) {
+      cid = Gnubby.BROADCAST_CID;
+    }
     var cmd = GnubbyDevice.CMD_INIT;
     self.dev.queueCommand(cid, cmd, nonce);
   }
@@ -629,8 +632,7 @@ Gnubby.prototype.sync = function(cb) {
     completionAction = syncCompletionAction;
   }
 
-  if (Gnubby.gnubbies_.isSharedAccess(this.which) &&
-      this.cid == Gnubby.defaultChannelId_(this.gnubbyInstance, this.which)) {
+  if (Gnubby.gnubbies_.isSharedAccess(this.which)) {
     setInit();
   } else {
     setSync();
