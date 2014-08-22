de3126fabe2fd2588db4da9b43e3abe730affde9
jdduke@chromium.org <jdduke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-22 12:58:34 +0000

    Expose a pair of points for selection bound edge coordinates
    Using a rect to indicate the logical selection bound edge is confusing,
    particular if the selection bound is not axis aligned with respect to its
    compositing layer. Instead, expose a top and bottom coordinate pair for the
    logical edge of the selection bound.
    
    The corresponding Chromium change is https://codereview.chromium.org/494823002.
    
    BUG=405666
    
    Review URL: https://codereview.chromium.org/495673003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180773 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebSelectionBound.h
+++ third_party/WebKit/public/platform/WebSelectionBound.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WebSelectionBound_h
 #define WebSelectionBound_h
 
+#include "public/platform/WebPoint.h"
 #include "public/platform/WebRect.h"
 
 namespace blink {
@@ -33,7 +34,14 @@ struct WebSelectionBound {
 
     // The one-dimensional rect of the bound's edge in layer coordinates, not to
     // be confused with the selection region.
+    // FIXME: Remove when downstream code uses |edge{Top|Bottom}InLayer|,
+    // crbug.com/405666.
     WebRect edgeRectInLayer;
+
+    // The bottom and top coordinates of the edge (caret), in layer coordinates,
+    // that define the selection bound.
+    WebPoint edgeTopInLayer;
+    WebPoint edgeBottomInLayer;
 };
 
 } // namespace blink
