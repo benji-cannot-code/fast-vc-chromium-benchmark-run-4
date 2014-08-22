aee44dbc4c9248bedb8f38b6ba9b933e289fa9fc
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-22 21:05:32 +0000

    Disable flaky XForm tests on Win 64.
    BUG=406574
    TBR=wfh@chromium.org
    
    Review URL: https://codereview.chromium.org/475173006
    
    Cr-Commit-Position: refs/heads/master@{#291503}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@291503 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/transform_unittest.cc
+++ ui/gfx/transform_unittest.cc
@@ -736,7 +736,13 @@ TEST(XFormTest, BlendRotate) {
   }
 }
 
-TEST(XFormTest, BlendRotateFollowsShortestPath) {
+#if defined(_WIN64)
+// http://crbug.com/406574
+#define MAYBE_BlendRotateFollowsShortestPath DISABLED_BlendRotateFollowsShortestPath
+#else
+#define MAYBE_BlendRotateFollowsShortestPath BlendRotateFollowsShortestPath
+#endif
+TEST(XFormTest, MAYBE_BlendRotateFollowsShortestPath) {
   // Verify that we interpolate along the shortest path regardless of whether
   // this path crosses the 180-degree point.
   Vector3dF axes[] = {
@@ -803,7 +809,13 @@ TEST(XFormTest, CanBlend180DegreeRotation) {
   }
 }
 
-TEST(XFormTest, BlendScale) {
+#if defined(_WIN64)
+// http://crbug.com/406574
+#define MAYBE_BlendScale DISABLED_BlendScale
+#else
+#define MAYBE_BlendScale BlendScale
+#endif
+TEST(XFormTest, MAYBE_BlendScale) {
   Transform from;
   for (int i = -5; i < 15; ++i) {
     Transform to;
@@ -844,7 +856,13 @@ TEST(XFormTest, ExtrapolateSkew) {
   }
 }
 
-TEST(XFormTest, BlendPerspective) {
+#if defined(_WIN64)
+// http://crbug.com/406574
+#define MAYBE_BlendPerspective DISABLED_BlendPerspective
+#else
+#define MAYBE_BlendPerspective BlendPerspective
+#endif
+TEST(XFormTest, MAYBE_BlendPerspective) {
   Transform from;
   from.ApplyPerspectiveDepth(200);
   for (int i = -1; i < 3; ++i) {
@@ -1050,7 +1068,13 @@ TEST(XFormTest, VerifyBlendForSkewY) {
   EXPECT_ROW4_EQ(0.0f, 0.0f, 0.0f, 1.0f, to);
 }
 
-TEST(XFormTest, VerifyBlendForRotationAboutX) {
+#if defined(_WIN64)
+// http://crbug.com/406574
+#define MAYBE_VerifyBlendForRotationAboutX DISABLED_VerifyBlendForRotationAboutX
+#else
+#define MAYBE_VerifyBlendForRotationAboutX VerifyBlendForRotationAboutX
+#endif
+TEST(XFormTest, MAYBE_VerifyBlendForRotationAboutX) {
   // Even though.Blending uses quaternions, axis-aligned rotations should.
   // Blend the same with quaternions or Euler angles. So we can test
   // rotation.Blending by comparing against manually specified matrices from
@@ -1112,7 +1136,13 @@ TEST(XFormTest, VerifyBlendForRotationAboutX) {
   EXPECT_ROW4_EQ(0.0f, 0.0f, 0.0f, 1.0f, to);
 }
 
-TEST(XFormTest, VerifyBlendForRotationAboutY) {
+#if defined(_WIN64)
+// http://crbug.com/406574
+#define MAYBE_VerifyBlendForRotationAboutY DISABLED_VerifyBlendForRotationAboutY
+#else
+#define MAYBE_VerifyBlendForRotationAboutY VerifyBlendForRotationAboutY
+#endif
+TEST(XFormTest, MAYBE_VerifyBlendForRotationAboutY) {
   Transform from;
   from.RotateAbout(Vector3dF(0.0, 1.0, 0.0), 0.0);
 
@@ -1169,7 +1199,13 @@ TEST(XFormTest, VerifyBlendForRotationAboutY) {
   EXPECT_ROW4_EQ(0.0f, 0.0f, 0.0f, 1.0f, to);
 }
 
-TEST(XFormTest, VerifyBlendForRotationAboutZ) {
+#if defined(_WIN64)
+// http://crbug.com/406574
+#define MAYBE_VerifyBlendForRotationAboutZ DISABLED_VerifyBlendForRotationAboutZ
+#else
+#define MAYBE_VerifyBlendForRotationAboutZ VerifyBlendForRotationAboutZ
+#endif
+TEST(XFormTest, MAYBE_VerifyBlendForRotationAboutZ) {
   Transform from;
   from.RotateAbout(Vector3dF(0.0, 0.0, 1.0), 0.0);
 
