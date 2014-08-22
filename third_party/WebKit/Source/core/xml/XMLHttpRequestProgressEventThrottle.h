2d8355e8911644884902b72839b8ac3f9c7546e8
tyoshino@chromium.org <tyoshino@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-22 07:10:13 +0000

    Refactor XMLHttpRequestProgressEventThrottle for readability
    - Revise comments to distinguish events named "progress" and events
      using the ProgressEvent interface.
    - Rename deliverProgressEvent() to make it clear it's for scheduled one.
    - Add one ASSERT
    - Add FIXME about dispatchReadyStateChangeEvent()'s progress event
      coalescing
    - Explained terminology in the class comment
    - Use XMLHttpRequestProgressEvent for m_deferredProgressEvent
    
    BUG=none
    R=yhirano,sof
    
    Review URL: https://codereview.chromium.org/496443002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180760 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/xml/XMLHttpRequestProgressEventThrottle.h
+++ third_party/WebKit/Source/core/xml/XMLHttpRequestProgressEventThrottle.h
@@ -38,6 +38,7 @@ namespace blink {
 
 class Event;
 class EventTarget;
+class XMLHttpRequestProgressEvent;
 
 enum ProgressEventAction {
     DoNotFlushProgressEvent,
@@ -45,8 +46,17 @@ enum ProgressEventAction {
     FlushProgressEvent
 };
 
-// This implements the XHR2 progress event dispatching: "dispatch a progress event called progress
-// about every 50ms or for every byte received, whichever is least frequent".
+// This class implements the XHR2 ProgressEvent dispatching:
+//   "dispatch a progress event named progress about every 50ms or for every
+//   byte received, whichever is least frequent".
+//
+// readystatechange events also go through this class since ProgressEvents and
+// readystatechange events affect each other.
+//
+// In the comments for this class:
+// - "progress" event means an event named "progress"
+// - ProgressEvent means an event using the ProgressEvent interface defined in
+//   the spec.
 class XMLHttpRequestProgressEventThrottle FINAL : public TimerBase {
     DISALLOW_ALLOCATION();
 public:
@@ -62,6 +72,8 @@ public:
     // m_lengthComputable, m_loaded and m_total. They'll be used on next
     // fired() call.
     void dispatchProgressEvent(const AtomicString&, bool lengthComputable, unsigned long long loaded, unsigned long long total);
+    // Dispatches the given event after operation about the "progress" event
+    // depending on the value of the ProgressEventAction argument.
     void dispatchReadyStateChangeEvent(PassRefPtrWillBeRawPtr<Event>, ProgressEventAction = DoNotFlushProgressEvent);
 
     void suspend();
@@ -77,8 +89,12 @@ private:
 
     virtual void fired() OVERRIDE;
     void dispatchDeferredEvents(Timer<XMLHttpRequestProgressEventThrottle>*);
+    // If m_deferredProgressEvent is not null, push it to m_deferredEvents.
+    // Otherwise, do nothing. Returns true iff push happened.
     bool flushDeferredProgressEvent();
-    void deliverProgressEvent();
+    // Builds a "progress" event from m_lengthComputable, m_loaded and m_total
+    // and tries to dispatch it. If we're suspended, just queues the event.
+    void deliverScheduledProgressEvent();
 
     bool hasEventToDispatch() const;
 
@@ -88,12 +104,19 @@ private:
     // with the XMLHttpRequest object.
     RawPtrWillBeMember<EventTarget> m_target;
 
+    // These three variables represents the "progress" event scheduled by
+    // dispatchProgressEvent() to be dispatched by fired() on the next Timer
+    // event.
     bool m_lengthComputable;
     unsigned long long m_loaded;
     unsigned long long m_total;
 
     bool m_deferEvents;
-    RefPtrWillBeMember<Event> m_deferredProgressEvent;
+    // Holds a "progress" event deferred due to suspension.
+    //
+    // FIXME: Consider reusing m_lengthComputable, m_loaded and m_total than
+    // having this separate variable to remember a deferred "progress" event.
+    RefPtrWillBeMember<XMLHttpRequestProgressEvent> m_deferredProgressEvent;
     WillBeHeapVector<RefPtrWillBeMember<Event> > m_deferredEvents;
     Timer<XMLHttpRequestProgressEventThrottle> m_dispatchDeferredEventsTimer;
 };
