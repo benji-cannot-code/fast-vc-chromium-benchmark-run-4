f58c358df1e5af08566e5d22f9fb6ee10a49cc90
dgozman@chromium.org <dgozman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-22 10:45:33 +0000

    DevTools: add support for chrome-devtools://devtools/remote/open? URL.
    BUG=370025
    
    Review URL: https://codereview.chromium.org/489893002
    
    Cr-Commit-Position: refs/heads/master@{#291382}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@291382 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/devtools/devtools_ui_bindings.cc
+++ chrome/browser/devtools/devtools_ui_bindings.cc
@@ -795,7 +795,8 @@ void DevToolsUIBindings::SetDelegate(Delegate* delegate) {
 }
 
 void DevToolsUIBindings::AttachTo(content::DevToolsAgentHost* agent_host) {
-  DCHECK(!agent_host_);
+  if (agent_host_)
+    Detach();
   agent_host_ = agent_host;
   agent_host_->AttachClient(this);
 }
@@ -807,8 +808,8 @@ void DevToolsUIBindings::Reattach() {
 }
 
 void DevToolsUIBindings::Detach() {
-  DCHECK(agent_host_);
-  agent_host_->DetachClient();
+  if (agent_host_)
+    agent_host_->DetachClient();
   agent_host_ = NULL;
 }
 
