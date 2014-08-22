483862216d156bc7ec29c47f252fe4f81abdccf4
r.ghatage@samsung.com <r.ghatage@samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-22 16:36:10 +0000

    Hide PopupZoomer when the container view or window loses focus
    Resubmitting the patch 
    https://src.chromium.org/viewvc/chrome?view=rev&revision=291365
    which was reverted as it caused a FindBug failure due to not calling 
    the Super method in the setUp method in PopupZoomerTest class.
    
    Adding call to to super.setUp() to fix the warning.
    
    BUG=405477, 406272
    
    Review URL: https://codereview.chromium.org/497753003
    
    Cr-Commit-Position: refs/heads/master@{#291430}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@291430 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -522,9 +522,7 @@ public class ContentViewCore
                 new ImeAdapter.ImeAdapterDelegate() {
                     @Override
                     public void onImeEvent() {
-                        if (mPopupZoomer.isShowing()) {
-                            mPopupZoomer.hide(true);
-                        }
+                        mPopupZoomer.hide(true);
                         getContentViewClient().onImeEvent();
                         hideTextHandles();
                     }
@@ -753,6 +751,11 @@ public class ContentViewCore
             }
         };
         mPopupZoomer.setOnTapListener(listener);
+    }
+
+    @VisibleForTesting
+    public void setPopupZoomerForTest(PopupZoomer popupZoomer) {
+        mPopupZoomer = popupZoomer;
     }
 
     /**
@@ -1435,6 +1438,7 @@ public class ContentViewCore
         hidePastePopup();
         hideSelectPopup();
         hideTextHandles();
+        mPopupZoomer.hide(false);
     }
 
     public void hideSelectActionBar() {
@@ -1628,6 +1632,7 @@ public class ContentViewCore
             cancelRequestToScrollFocusedEditableNodeIntoView();
             hidePastePopup();
             hideTextHandles();
+            mPopupZoomer.hide(false);
         }
         if (mNativeContentViewCore != 0) nativeSetFocus(mNativeContentViewCore, gainFocus);
     }
