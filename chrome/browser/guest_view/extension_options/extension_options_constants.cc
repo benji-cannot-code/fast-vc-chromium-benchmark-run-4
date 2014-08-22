506e8df5d57b7361c5939a17ca13a191a10baae8
ericzeng@chromium.org <ericzeng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-22 01:05:50 +0000

    Implement smoother autosizing of the extension options overlay
     - Use element.animate to resize the overlay for embedded extension
       options. When an <extensionoptions> element is embedded in a WebUI
       overlay, if its size changes the overlay will smoothly expand to
       surround the element.
     - Add an API to <extensionoptions> to defer autosizing until its
       embedder is ready. This is used to 'pin' the element's size until
       the overlay is done expanding.
     - Move the <extensionoptions> off screen until the overlay is done
       expanding.
     - Prevent <extensionoptions> from shrinking after the initial sizing.
       This makes the look of the embedded options page more consistent.
    
    BUG=386842
    
    Review URL: https://codereview.chromium.org/480243003
    
    Cr-Commit-Position: refs/heads/master@{#291286}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@291286 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/guest_view/extension_options/extension_options_constants.cc
+++ chrome/browser/guest_view/extension_options/extension_options_constants.cc
@@ -18,7 +18,11 @@ const char kAttributeMinHeight[] = "minheight";
 const char kAttributeMinWidth[] = "minwidth";
 
 const char kExtensionId[] = "extensionId";
-const char kHeight[] = "height";
-const char kWidth[] = "width";
+
+const char kNewHeight[] = "newHeight";
+const char kNewWidth[] = "newWidth";
+const char kOldHeight[] = "oldHeight";
+const char kOldWidth[] = "oldWidth";
+
 
 }  // namespace extensionoptions
