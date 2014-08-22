83cb96cfd21ec0d73d1fe76c83ff5ab30a3e5574
felt@chromium.org <felt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-22 00:31:22 +0000

    Move the security interstitials into a single folder
    This moves the Safe Browsing and SSL interstitial HTML/JS into a single new
    folder: chrome/browser/resources/security_warnings/. It also moves some
    common code out of the SSL-related JS file and into the general interstitial JS
    file.
    
    BUG=402540
    
    Review URL: https://codereview.chromium.org/480393002
    
    Cr-Commit-Position: refs/heads/master@{#291270}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@291270 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/resources/security_warnings/ssl.js
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Should match SSLBlockingPageCommands in ssl_blocking_page.cc.
+var CMD_DONT_PROCEED = 0;
+var CMD_PROCEED = 1;
+var CMD_MORE = 2;
+var CMD_RELOAD = 3;
+var CMD_HELP = 4;
+var CMD_CLOCK = 5;
