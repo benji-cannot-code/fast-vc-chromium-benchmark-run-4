392224aff08aad06d9c127d6e5311a3c360fa208
penghuang@chromium.org <penghuang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-22 21:50:37 +0000

    [PPAPI] MediaStream: do not crash when the source frame size is changed.
    BUG=405871
    
    Review URL: https://codereview.chromium.org/495403002
    
    Cr-Commit-Position: refs/heads/master@{#291517}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@291517 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/pepper/pepper_media_stream_video_track_host.cc
+++ content/renderer/pepper/pepper_media_stream_video_track_host.cc
@@ -405,7 +405,6 @@ void PepperMediaStreamVideoTrackHost::OnVideoFrame(
     return;
   }
 
-  CHECK(frame->coded_size() == source_frame_size_) << "Frame size is changed";
   CHECK_EQ(ppformat, source_frame_format_) << "Frame format is changed.";
 
   gfx::Size size = GetTargetSize(source_frame_size_, plugin_frame_size_);
