bd01af767b589894796328e96164f1dad4d134dc
sigbjornf@opera.com <sigbjornf@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-22 05:01:36 +0000

    Fix spelling of provideNotificationPermissionClientTo()
    TBR=tkent
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/489373002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180755 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/notifications/NotificationPermissionClient.h
+++ third_party/WebKit/Source/modules/notifications/NotificationPermissionClient.h
@@ -29,7 +29,7 @@ public:
     static NotificationPermissionClient* from(ExecutionContext*);
 };
 
-void proviceNotificationPermissionClientTo(LocalFrame&, PassOwnPtrWillBeRawPtr<NotificationPermissionClient>);
+void provideNotificationPermissionClientTo(LocalFrame&, PassOwnPtrWillBeRawPtr<NotificationPermissionClient>);
 
 } // namespace blink
 
