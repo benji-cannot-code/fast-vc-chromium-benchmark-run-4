1d521eef90d5f7437e126524c72f198e86beb028
tyoshino@chromium.org <tyoshino@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-22 17:28:23 +0000

    Add some includes to web_url_loader_impl.
    BUG=none
    
    Review URL: https://codereview.chromium.org/488393002
    
    Cr-Commit-Position: refs/heads/master@{#291440}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@291440 0039d316-1c4b-4281-b951-d872f2087c98

--- content/child/web_url_loader_impl.h
+++ content/child/web_url_loader_impl.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/content_export.h"
 #include "third_party/WebKit/public/platform/WebURLLoader.h"
 
+class GURL;
+
 namespace content {
 
 class ResourceDispatcher;
