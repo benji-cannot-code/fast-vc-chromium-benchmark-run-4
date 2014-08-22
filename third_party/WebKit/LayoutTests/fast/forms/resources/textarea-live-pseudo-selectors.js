3807566f23f74a46de14a5dd73aed9c6af1c3864
sigbjornf@opera.com <sigbjornf@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-22 09:38:21 +0000

    Test tidying: use "escape" and "backspace" with eventSender.keyDown()
    R=keishi@chromium.org,yosin@chromium.org
    BUG=392517
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/484353004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180765 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/forms/resources/textarea-live-pseudo-selectors.js
+++ third_party/WebKit/LayoutTests/fast/forms/resources/textarea-live-pseudo-selectors.js
@@ -77,7 +77,7 @@ el = makeInvalid();
 el.focus();
 eventSender.keyDown('a');
 shouldBe(elBackground, 'validColor');
-eventSender.keyDown('\x08', []);
+eventSender.keyDown('backspace', []);
 shouldBe(elBackground, 'invalidColor');
 
 // --------------------------------
