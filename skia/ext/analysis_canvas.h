d8465aa424b6ab414a60d71201cf265e14f73a36
hendrikw@chromium.org <hendrikw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-22 23:24:24 +0000

    AnalysisCanvas is missing an override for virtual void onClipRegion
    BUG=405364
    
    Review URL: https://codereview.chromium.org/492943002
    
    Cr-Commit-Position: refs/heads/master@{#291548}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@291548 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/ext/analysis_canvas.h
+++ skia/ext/analysis_canvas.h
@@ -85,6 +85,8 @@ class SK_API AnalysisCanvas : public SkCanvas, public SkDrawPictureCallback {
   virtual void onClipPath(const SkPath& path,
                           SkRegion::Op op,
                           ClipEdgeStyle edge_style) OVERRIDE;
+  virtual void onClipRegion(const SkRegion& deviceRgn,
+                            SkRegion::Op op) OVERRIDE;
 
   virtual void onDrawText(const void* text,
                           size_t byteLength,
@@ -109,7 +111,9 @@ class SK_API AnalysisCanvas : public SkCanvas, public SkDrawPictureCallback {
                             const SkRRect& inner,
                             const SkPaint&) OVERRIDE;
 
-private:
+  void OnComplexClip();
+
+ private:
   typedef SkCanvas INHERITED;
 
   int saved_stack_size_;
