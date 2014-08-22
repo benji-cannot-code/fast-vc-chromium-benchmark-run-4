15504a0249bc9ba8ab92272a074594e2aaca7fb2
ericzeng@chromium.org <ericzeng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-22 23:32:18 +0000

    Handle closing web contents in ExtensionOptionsGuest
    Implement CloseContents for ExtensionOptionsGuest. CloseContents
    propagates a 'close' event up to the <extensionoptions> node.
    In WebUI, when the extension options overlay receives this event,
    it will close the overlay.
    
    Extension options pages that used window.close() to close their
    tab in the old options page UI will now close their embedder
    overlay.
    
    BUG=386842, 386838
    
    Review URL: https://codereview.chromium.org/490783002
    
    Cr-Commit-Position: refs/heads/master@{#291554}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@291554 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/resources/extensions/extension_options_events.js
+++ chrome/renderer/resources/extensions/extension_options_events.js
@@ -11,6 +11,10 @@ var CreateEvent = function(name) {
 };
 
 var EXTENSION_OPTIONS_EVENTS = {
+  'close': {
+    evt: CreateEvent('extensionOptionsInternal.onClose'),
+    fields: []
+  },
   'load': {
     evt: CreateEvent('extensionOptionsInternal.onLoad'),
     fields: []
--- chrome/test/data/extensions/api_test/extension_options/close_self/options.js
+++ chrome/test/data/extensions/api_test/extension_options/close_self/options.js
@@ -3,11 +3,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Internal API for the &lt;extensiontoptions&gt; tag
-namespace extensionOptionsInternal {
-  interface Events {
-    static void onClose();
-    static void onLoad();
-    static void onSizeChanged(long width, long height);
-  };
-};
+window.close();
