e4bad74758c7424784e14cada0bd70ff6ba6e675
kmadhusu <kmadhusu@chromium.org>
2014-08-23 15:09:51 -0700

    Have TabAndroid to implement the SearchTabHelperDelegate interface to provide the necessary functionality to SearchTabHelper.
    This will allow us to turn off search term replacement (by updating the location bar displayed url) when the underlying page doesn't support embedded search.
    
    Server side bug: b/17140979
    
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/488093003
    
    Cr-Commit-Position: refs/heads/master@{#291603}

--- chrome/browser/android/tab_android.h
+++ chrome/browser/android/tab_android.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/strings/string16.h"
 #include "chrome/browser/sync/glue/synced_tab_delegate_android.h"
+#include "chrome/browser/ui/search/search_tab_helper_delegate.h"
 #include "chrome/browser/ui/tab_contents/core_tab_helper_delegate.h"
 #include "chrome/browser/ui/toolbar/toolbar_model.h"
 #include "components/sessions/session_id.h"
@@ -43,6 +44,7 @@ class PrerenderManager;
 }
 
 class TabAndroid : public CoreTabHelperDelegate,
+                   public SearchTabHelperDelegate,
                    public content::NotificationObserver {
  public:
   enum TabLoadStatus {
@@ -111,6 +113,10 @@ class TabAndroid : public CoreTabHelperDelegate,
                                content::WebContents* new_contents,
                                bool did_start_load,
                                bool did_finish_load) OVERRIDE;
+
+  // Overridden from SearchTabHelperDelegate:
+  virtual void OnWebContentsInstantSupportDisabled(
+      const content::WebContents* web_contents) OVERRIDE;
 
   // NotificationObserver -----------------------------------------------------
   virtual void Observe(int type,
