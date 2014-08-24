68171f9cbe3d75f18fb69e6df89851c310498c5f
yurys@chromium.org <yurys@chromium.org>
2014-08-24 04:55:02 +0000

    DevTools: adopt existing Timeline tests for tracing based Timeline
    All existing timeline tests under inspector/timeline and http/tests/inspector are copied into inspector/tracing and http/tests/tracing correspondingly. All tests under .../inspector/timeline/tracing are moved to .../inspector/tracing too.
    
    In that folder the tests are run with "Timeline based on trace events" experiment on (see changes in InspectorView.js), while the old tests continue to use old Timeline implementation. We are going to keep both implementations and tests for them until we are sure that the new implementation is stable enough and can be moved out of experimental. At that point old implementation will be removed along with the old tests.
    
    BUG=398785
    
    Review URL: https://codereview.chromium.org/477153005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180815 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/inspector/timeline-test.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/timeline-test.js
@@ -20,11 +20,14 @@ InspectorTest.timelinePropertyFormatters = {
     lineNumber: "formatAsTypeName",
     columnNumber: "formatAsTypeName",
     frameId: "formatAsTypeName",
+    frame: "formatAsTypeName",
     encodedDataLength: "formatAsTypeName",
     identifier: "formatAsTypeName",
     clip: "formatAsTypeName",
     root: "formatAsTypeName",
     backendNodeId: "formatAsTypeName",
+    nodeId: "formatAsTypeName",
+    rootNode: "formatAsTypeName",
     networkTime: "formatAsTypeName",
     thread: "formatAsTypeName"
 };
@@ -252,6 +255,8 @@ InspectorTest.printTimelineRecordProperties = function(record)
     }
     if (record.children().length)
         object["children"] = [];
+    if (!record.data() && record instanceof WebInspector.TracingTimelineModel.TraceEventRecord)
+        object["data"] = record.traceEvent().args;
     InspectorTest.addObject(object, InspectorTest.timelinePropertyFormatters);
 };
 
---
+++ third_party/WebKit/LayoutTests/virtual/deferred/inspector/tracing/README.txt
@@ -0,0 +1,3 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# This suite runs the tests in LayoutTests/inspector/tracing/ with additional flags.
+# See the virtual_test_suites() method in Tools/Scripts/webkitpy/layout_tests/port/base.py.
