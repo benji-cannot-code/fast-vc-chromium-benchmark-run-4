331304760edf1aba2e99d29ed10a8213e65de01e
sullivan <sullivan@chromium.org>
2014-08-25 10:35:24 -0700

    Add support for codepen to form_based_credentials_background
    Fix google credentials support.
    
    Migrate to action_runner when possible.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/485743002
    
    Cr-Commit-Position: refs/heads/master@{#291705}

---
+++ tools/telemetry/telemetry/core/backends/codepen_credentials_backend_unittest.py
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+from telemetry.core.backends import form_based_credentials_backend_unittest_base
+from telemetry.core.backends import codepen_credentials_backend
+
+
+class TestCodePenCredentialsBackend(
+    form_based_credentials_backend_unittest_base.
+    FormBasedCredentialsBackendUnitTestBase):
+  def setUp(self):
+    self._credentials_type = 'codepen'
+
+  def testLoginUsingMock(self):
+    self._LoginUsingMock(
+        codepen_credentials_backend.CodePenCredentialsBackend(),
+        'https://codepen.io/login', 'login-email-field', 'login-password',
+        'login-login-form', 'document.querySelector(".login-area") === null')
