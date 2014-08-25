cba762bb9c61fe6e65d3d7fbf04a5d72ae0322c9
jeun <jeun@chromium.org>
2014-08-25 09:52:08 -0700

    [Findit]Bug fixing and implemented some feature requests.
    Fixed various bugs, including exception on stacktrace and unnamed variable in git parser.
    Changed the algorithm to use file_path only instead of file_name.
    Removed URL links from the result.
    Added commit message to the final result.
    Updated OWNERS.
    
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/478763003
    
    Cr-Commit-Position: refs/heads/master@{#291698}

--- tools/findit/result.py
+++ tools/findit/result.py
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class Result(object):
 
   def __init__(self, suspected_cl, revision_url, component_name, author,
-               reason, review_url, reviewers, line_content):
+               reason, review_url, reviewers, line_content, message):
     self.suspected_cl = suspected_cl
     self.revision_url = revision_url
     self.component_name = component_name
@@ -16,3 +16,4 @@ class Result(object):
     self.review_url = review_url
     self.reviewers = reviewers
     self.line_content = line_content
+    self.commit_message = message
