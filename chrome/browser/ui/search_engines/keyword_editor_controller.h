d35c64c596affcc08a4f7cfac8d975de152aa936
dgrogan <dgrogan@chromium.org>
2014-08-25 14:11:38 -0700

    Remove two unused prefs keys.
    kKeywordEditorWindowPlacement, kPreferencesWindowPlacement haven't been
    used in a few years.
    
    Review URL: https://codereview.chromium.org/501463003
    
    Cr-Commit-Position: refs/heads/master@{#291760}

--- chrome/browser/ui/search_engines/keyword_editor_controller.h
+++ chrome/browser/ui/search_engines/keyword_editor_controller.h
@@ -23,8 +23,6 @@ class KeywordEditorController {
   explicit KeywordEditorController(Profile* profile);
   ~KeywordEditorController();
 
-  static void RegisterPrefs(PrefRegistrySimple* registry);
-
   // Invoked when the user succesfully fills out the add keyword dialog.
   // Propagates the change to the TemplateURLService and updates the table
   // model.  Returns the index of the added URL.
