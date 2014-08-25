bed3f98b1af76c6fde7725392bfdf8c593da849d
dneto <dneto@chromium.org>
2014-08-25 16:27:20 -0700

    Remove cc:LayerTreeHostImpl::IsContextLost.
    The LTHI always notifies the scheduler of context loss.
    The single thread proxy no longer asks LTHI if the
    context is lost.  In-flight composite actions just go
    ahead and draw.
    
    BUG=402511
    
    Review URL: https://codereview.chromium.org/505713004
    
    Cr-Commit-Position: refs/heads/master@{#291783}

--- cc/trees/layer_tree_host_impl.cc
+++ cc/trees/layer_tree_host_impl.cc
@@ -257,7 +257,6 @@ LayerTreeHostImpl::LayerTreeHostImpl(
       rendering_stats_instrumentation_(rendering_stats_instrumentation),
       micro_benchmark_controller_(this),
       need_to_update_visible_tiles_before_draw_(false),
-      have_valid_output_surface_(false),
       shared_bitmap_manager_(manager),
       id_(id),
       transfer_buffer_memory_limit_(0u) {
@@ -1616,13 +1615,6 @@ void LayerTreeHostImpl::FinishAllRendering() {
     renderer_->Finish();
 }
 
-bool LayerTreeHostImpl::IsContextLost() {
-  DCHECK(proxy_->IsImplThread());
-  // To avoid races, rely only on the lost-surface callback.
-  // See crbug.com/392891.
-  return !have_valid_output_surface_;
-}
-
 void LayerTreeHostImpl::SetUseGpuRasterization(bool use_gpu) {
   if (use_gpu == use_gpu_rasterization_)
     return;
@@ -1695,16 +1687,9 @@ void LayerTreeHostImpl::UpdateInnerViewportContainerSize() {
 }
 
 void LayerTreeHostImpl::DidLoseOutputSurface() {
-  if (!have_valid_output_surface_)
-    return;
-  have_valid_output_surface_ = false;
   if (resource_provider_)
     resource_provider_->DidLoseOutputSurface();
-  // TODO(jamesr): The renderer_ check is needed to make some of the
-  // LayerTreeHostContextTest tests pass, but shouldn't be necessary (or
-  // important) in production. We should adjust the test to not need this.
-  if (renderer_)
-    client_->DidLoseOutputSurfaceOnImplThread();
+  client_->DidLoseOutputSurfaceOnImplThread();
 }
 
 bool LayerTreeHostImpl::HaveRootScrollLayer() const {
@@ -2063,7 +2048,6 @@ bool LayerTreeHostImpl::InitializeRenderer(
     return false;
 
   output_surface_ = output_surface.Pass();
-  have_valid_output_surface_ = true;
   resource_provider_ =
       ResourceProvider::Create(output_surface_.get(),
                                shared_bitmap_manager_,
