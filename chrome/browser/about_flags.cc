6dccf8cd55f33e8439db981113e8e133f03555d5
hirono <hirono@chromium.org>
2014-08-24 19:13:37 -0700

    Gallery.app: Remove old gallery from Files.app.
    BUG=None
    TEST=None
    
    Review URL: https://codereview.chromium.org/491223002
    
    Cr-Commit-Position: refs/heads/master@{#291633}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -426,14 +426,6 @@ const Experiment::Choice kEnableFileManagerMTPChoices[] = {
   { IDS_GENERIC_EXPERIMENT_CHOICE_DISABLED,
     chromeos::switches::kEnableFileManagerMTP, "false" }
 };
-
-const Experiment::Choice kEnableFileManagerNewGalleryChoices[] = {
-  { IDS_GENERIC_EXPERIMENT_CHOICE_DEFAULT, "", ""},
-  { IDS_GENERIC_EXPERIMENT_CHOICE_ENABLED,
-    chromeos::switches::kFileManagerEnableNewGallery, "true"},
-  { IDS_GENERIC_EXPERIMENT_CHOICE_DISABLED,
-    chromeos::switches::kFileManagerEnableNewGallery, "false"}
-};
 #endif
 
 const Experiment::Choice kEnableSettingsWindowChoices[] = {
@@ -994,13 +986,6 @@ const Experiment kExperiments[] = {
     IDS_FLAGS_DISABLE_BOOT_ANIMATION_DESCRIPTION,
     kOsCrOSOwnerOnly,
     SINGLE_VALUE_TYPE(chromeos::switches::kDisableBootAnimation),
-  },
-  {
-    "enable-new-gallery",
-    IDS_FLAGS_FILE_MANAGER_ENABLE_NEW_GALLERY_NAME,
-    IDS_FLAGS_FILE_MANAGER_ENABLE_NEW_GALLERY_DESCRIPTION,
-    kOsCrOS,
-    MULTI_VALUE_TYPE(kEnableFileManagerNewGalleryChoices)
   },
   {
     "enable-video-player-chromecast-support",
--- chrome/browser/chromeos/file_manager/file_tasks.cc
+++ chrome/browser/chromeos/file_manager/file_tasks.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "apps/launcher.h"
 #include "base/bind.h"
-#include "base/command_line.h"
 #include "base/prefs/pref_service.h"
 #include "base/prefs/scoped_user_pref_update.h"
 #include "base/strings/stringprintf.h"
@@ -27,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/extensions/api/file_browser_private.h"
 #include "chrome/common/extensions/extension_constants.h"
 #include "chrome/common/pref_names.h"
-#include "chromeos/chromeos_switches.h"
 #include "extensions/browser/extension_host.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/extension_system.h"
@@ -104,28 +102,6 @@ void KeepOnlyFileManagerInternalTasks(std::vector<FullTaskDescriptor>* tasks) {
       filtered.push_back((*tasks)[i]);
   }
   tasks->swap(filtered);
-}
-
-void ChooseSuitableGalleryHandler(std::vector<FullTaskDescriptor>* task_list) {
-  const bool disable_new_gallery =
-      base::CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
-          chromeos::switches::kFileManagerEnableNewGallery) == "false";
-  std::vector<FullTaskDescriptor>::iterator it = task_list->begin();
-  while (it != task_list->end()) {
-    if (disable_new_gallery) {
-      if (it->task_descriptor().app_id == kGalleryAppId)
-        it = task_list->erase(it);
-      else
-        ++it;
-    } else {
-      if (it->task_descriptor().app_id == kFileManagerAppId &&
-          it->task_descriptor().action_id == "gallery") {
-        it = task_list->erase(it);
-      } else {
-        ++it;
-      }
-    }
-  }
 }
 
 // Returns true if the given task is a handler by built-in apps like Files.app
@@ -505,7 +481,6 @@ void FindAllTypesOfTasks(
   if (ContainsGoogleDocument(path_mime_set))
     KeepOnlyFileManagerInternalTasks(result_list);
 
-  ChooseSuitableGalleryHandler(result_list);
   ChooseAndSetDefaultTask(*profile->GetPrefs(), path_mime_set, result_list);
 }
 
