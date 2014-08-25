e80cfeb43a1b91fbff3337aba5457ab530da14c7
lushnikov@chromium.org <lushnikov@chromium.org>
2014-08-25 16:02:15 +0000

    DevTools: [MQI] report active media queries from renderer
    Improve protocol to report an "active" flag for media queries.
    
    Layout Test: in order to avoid flakiness, the flag is dumped only for
    the media queries which are guaranteed to be satisfied/non-satisfied.
    
    BUG=400147
    R=vsevik, dgozman
    
    Review URL: https://codereview.chromium.org/492963006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180855 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/MediaQueryEvaluator.h
+++ third_party/WebKit/Source/core/css/MediaQueryEvaluator.h
@@ -34,11 +34,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 class LocalFrame;
+class MediaQuery;
 class MediaQueryExp;
 class MediaQueryResult;
 class MediaQuerySet;
-class RenderStyle;
 class MediaValues;
+class RenderStyle;
 
 typedef WillBeHeapVector<RefPtrWillBeMember<MediaQueryResult> > MediaQueryResultList;
 
@@ -58,30 +59,33 @@ class MediaQueryEvaluator {
 public:
     // Creates evaluator which evaluates only simple media queries
     // Evaluator returns true for "all", and returns value of \mediaFeatureResult
-    // for any media features
+    // for any media features.
 
     explicit MediaQueryEvaluator(bool mediaFeatureResult = false);
 
     // Creates evaluator which evaluates only simple media queries
     // Evaluator  returns true for acceptedMediaType and returns value of \mediafeatureResult
-    // for any media features
+    // for any media features.
 
     MediaQueryEvaluator(const char* acceptedMediaType, bool mediaFeatureResult = false);
 
-    // Creates evaluator which evaluates full media queries
+    // Creates evaluator which evaluates full media queries.
     explicit MediaQueryEvaluator(LocalFrame*);
 
-    // Creates evaluator which evaluates in a thread-safe manner a subset of media values
+    // Creates evaluator which evaluates in a thread-safe manner a subset of media values.
     explicit MediaQueryEvaluator(const MediaValues&);
 
     ~MediaQueryEvaluator();
 
     bool mediaTypeMatch(const String& mediaTypeToMatch) const;
 
-    // Evaluates a list of media queries
+    // Evaluates a list of media queries.
     bool eval(const MediaQuerySet*, MediaQueryResultList* = 0) const;
 
-    // Evaluates media query subexpression, ie "and (media-feature: value)" part
+    // Evaluates media query.
+    bool eval(const MediaQuery*, MediaQueryResultList*) const;
+
+    // Evaluates media query subexpression, ie "and (media-feature: value)" part.
     bool eval(const MediaQueryExp*) const;
 
 private:
