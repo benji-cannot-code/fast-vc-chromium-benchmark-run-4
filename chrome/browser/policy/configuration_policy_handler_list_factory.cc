4b5062871f8e8eb56dd85550149a9eb9ea1af958
hashimoto <hashimoto@chromium.org>
2014-08-25 01:19:15 -0700

    Merge apps/pref* to extensions/browser/pref*
    The pref's exclusive user AppWindow will move to extensions/browser
    
    Merge apps/pref_names.{cc,h} to extensions/browser/pref_names.{cc,h}
    Merge apps/prefs.{cc,h} to extensions/browser/extension_prefs.cc
    
    BUG=403726
    TBR=bartfab@chromium.org for include fix in configuration_policy_handler_list_factory.cc.
    
    Review URL: https://codereview.chromium.org/493003002
    
    Cr-Commit-Position: refs/heads/master@{#291650}

--- chrome/browser/policy/configuration_policy_handler_list_factory.cc
+++ chrome/browser/policy/configuration_policy_handler_list_factory.cc
@@ -49,10 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/download/download_dir_policy_handler.h"
 #endif
 
-#if !defined(OS_MACOSX) && !defined(OS_IOS)
-#include "apps/pref_names.h"
-#endif
-
 #if defined(ENABLE_EXTENSIONS)
 #include "chrome/browser/extensions/api/messaging/native_messaging_policy_handler.h"
 #include "chrome/browser/extensions/policy_handlers.h"
@@ -371,7 +367,7 @@ const PolicyToPreferenceMapEntry kSimplePolicyMap[] = {
     base::Value::TYPE_BOOLEAN },
 #if defined(ENABLE_EXTENSIONS)
   { key::kFullscreenAllowed,
-    apps::prefs::kAppFullscreenAllowed,
+    extensions::pref_names::kAppFullscreenAllowed,
     base::Value::TYPE_BOOLEAN },
 #endif  // defined(ENABLE_EXTENSIONS)
 #endif  // !defined(OS_MACOSX) && !defined(OS_IOS)
