808aaf057877a7dcaa84d33e91e42efe06f91cb5
rogerta <rogerta@chromium.org>
2014-08-25 08:59:42 -0700

    Create a new AccountTrackerService PKS based on the existing AccountTracker.
    This CL is one part of the effort to change chrome to use gaiaid instead of
    email address as the unique id for an account.
    
    BUG=341408
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=291263
    
    Review URL: https://codereview.chromium.org/425823002
    
    Cr-Commit-Position: refs/heads/master@{#291686}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sessions/session_service_factory.h"
 #include "chrome/browser/sessions/tab_restore_service_factory.h"
 #include "chrome/browser/signin/about_signin_internals_factory.h"
+#include "chrome/browser/signin/account_tracker_service_factory.h"
 #include "chrome/browser/signin/signin_manager_factory.h"
 #include "chrome/browser/sync/profile_sync_service_factory.h"
 #include "chrome/browser/themes/theme_service_factory.h"
@@ -150,6 +151,7 @@ EnsureBrowserContextKeyedServiceFactoriesBuilt() {
 #endif
 
   AboutSigninInternalsFactory::GetInstance();
+  AccountTrackerServiceFactory::GetInstance();
   autofill::PersonalDataManagerFactory::GetInstance();
 #if defined(ENABLE_BACKGROUND)
   BackgroundContentsServiceFactory::GetInstance();
