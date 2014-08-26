d6ee6cb24cc1114c5997beaaf11221a85c5fef5e
sammc <sammc@chromium.org>
2014-08-26 02:01:11 -0700

    Send less data in the DataSender test that timed out under valgrind.
    BUG=405531
    
    Review URL: https://codereview.chromium.org/499243002
    
    Cr-Commit-Position: refs/heads/master@{#291872}

--- extensions/test/data/data_sender_unittest.js
+++ extensions/test/data/data_sender_unittest.js
@@ -58,9 +58,9 @@ unittestBindings.exportTests([
 
   function testLargeSend() {
     createSender().then(test.callbackPass(function(sender) {
-      sender.send(generateData(BUFFER_SIZE * 100, '1234567890')).then(
+      sender.send(generateData(BUFFER_SIZE * 3, '123')).then(
           test.callbackPass(function(bytesSent) {
-        test.assertEq(BUFFER_SIZE * 100, bytesSent);
+        test.assertEq(BUFFER_SIZE * 3, bytesSent);
         sender.close();
       }));
     }));
@@ -68,7 +68,7 @@ unittestBindings.exportTests([
 
   function testSendError() {
     createSender().then(test.callbackPass(function(sender) {
-      sender.send(generateData(BUFFER_SIZE * 100, 'b')).catch(test.callbackPass(
+      sender.send(generateData(BUFFER_SIZE * 3, 'b')).catch(test.callbackPass(
           function(e) {
         test.assertEq(1, e.error);
         test.assertEq(0, e.bytesSent);
@@ -83,7 +83,7 @@ unittestBindings.exportTests([
 
   function testSendErrorPartialSuccess() {
     createSender().then(test.callbackPass(function(sender) {
-      sender.send(generateData(BUFFER_SIZE * 100, 'b')).catch(test.callbackPass(
+      sender.send(generateData(BUFFER_SIZE * 3, 'b')).catch(test.callbackPass(
           function(e) {
         test.assertEq(1, e.error);
         test.assertEq(5, e.bytesSent);
@@ -135,7 +135,7 @@ unittestBindings.exportTests([
 
   function testSendErrorInLargeSend() {
     createSender().then(test.callbackPass(function(sender) {
-      sender.send(generateData(BUFFER_SIZE * 100, '1234567890')).catch(
+      sender.send(generateData(BUFFER_SIZE * 3, '1234567890')).catch(
           test.callbackPass(function(e) {
         test.assertEq(1, e.error);
         test.assertEq(12, e.bytesSent);
@@ -154,7 +154,7 @@ unittestBindings.exportTests([
         test.assertEq(1, e.error);
         test.assertEq(2, e.bytesSent);
       }));
-      sender.send(generateData(BUFFER_SIZE * 100, '1234567890')).catch(
+      sender.send(generateData(BUFFER_SIZE * 3, '1234567890')).catch(
           test.callbackPass(function(e) {
         test.assertEq(1, e.error);
         test.assertEq(0, e.bytesSent);
