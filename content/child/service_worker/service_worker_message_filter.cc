7a801109d0dbfc896c15140f61c10b15fb0e3216
dcheng <dcheng@chromium.org>
2014-08-26 15:44:05 -0700

    Remove implicit conversions from scoped_refptr to T* in content/*/service_worker/
    This patch was generated by running the rewrite_scoped_refptr clang tool
    on a Linux build.
    
    BUG=110610
    
    Review URL: https://codereview.chromium.org/506093002
    
    Cr-Commit-Position: refs/heads/master@{#292011}

--- content/child/service_worker/service_worker_message_filter.cc
+++ content/child/service_worker/service_worker_message_filter.cc
@@ -83,7 +83,7 @@ void ServiceWorkerMessageFilter::OnStaleRegistered(
     int thread_id,
     int request_id,
     const ServiceWorkerRegistrationObjectInfo& info) {
-  SendRegistrationObjectDestroyed(thread_safe_sender_, info.handle_id);
+  SendRegistrationObjectDestroyed(thread_safe_sender_.get(), info.handle_id);
 }
 
 void ServiceWorkerMessageFilter::OnStaleSetVersionAttributes(
@@ -92,20 +92,21 @@ void ServiceWorkerMessageFilter::OnStaleSetVersionAttributes(
     int registration_handle_id,
     int changed_mask,
     const ServiceWorkerVersionAttributes& attributes) {
-  SendServiceWorkerObjectDestroyed(thread_safe_sender_,
+  SendServiceWorkerObjectDestroyed(thread_safe_sender_.get(),
                                    attributes.installing.handle_id);
-  SendServiceWorkerObjectDestroyed(thread_safe_sender_,
+  SendServiceWorkerObjectDestroyed(thread_safe_sender_.get(),
                                    attributes.waiting.handle_id);
-  SendServiceWorkerObjectDestroyed(thread_safe_sender_,
+  SendServiceWorkerObjectDestroyed(thread_safe_sender_.get(),
                                    attributes.active.handle_id);
-  SendRegistrationObjectDestroyed(thread_safe_sender_, registration_handle_id);
+  SendRegistrationObjectDestroyed(thread_safe_sender_.get(),
+                                  registration_handle_id);
 }
 
 void ServiceWorkerMessageFilter::OnStaleSetControllerServiceWorker(
     int thread_id,
     int provider_id,
     const ServiceWorkerObjectInfo& info) {
-  SendServiceWorkerObjectDestroyed(thread_safe_sender_, info.handle_id);
+  SendServiceWorkerObjectDestroyed(thread_safe_sender_.get(), info.handle_id);
 }
 
 }  // namespace content
