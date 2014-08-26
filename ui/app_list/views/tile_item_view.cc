51ce19445e7307432b838ab37bc25020cb582930
calamity <calamity@chromium.org>
2014-08-25 22:44:35 -0700

    Fix font rendering in experimental app list tiles.
    This CL fixes font rendering for experimental app list tiles by setting
    a background.
    
    BUG=406989
    
    Review URL: https://codereview.chromium.org/499983002
    
    Cr-Commit-Position: refs/heads/master@{#291849}

--- ui/app_list/views/tile_item_view.cc
+++ ui/app_list/views/tile_item_view.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/color_analysis.h"
 #include "ui/gfx/color_utils.h"
+#include "ui/views/background.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/layout/box_layout.h"
@@ -44,6 +45,8 @@ TileItemView::TileItemView()
   ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
   title_->SetAutoColorReadabilityEnabled(false);
   title_->SetEnabledColor(kGridTitleColor);
+  title_->set_background(views::Background::CreateSolidBackground(
+      app_list::kContentsBackgroundColor));
   title_->SetFontList(rb.GetFontList(kItemTextFontStyle));
   title_->SetHorizontalAlignment(gfx::ALIGN_CENTER);
 
