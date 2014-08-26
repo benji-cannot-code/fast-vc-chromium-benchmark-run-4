d4bf2c0e858573b21df25f500a87d5b2d18623ac
dhnishi <dhnishi@chromium.org>
2014-08-25 19:52:42 -0700

    Fix a very minor typo in ParseBlameInfo's docstring.
    TBR=stgao@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/501153004
    
    Cr-Commit-Position: refs/heads/master@{#291824}

--- tools/findit/repository_parser_interface.py
+++ tools/findit/repository_parser_interface.py
@@ -44,7 +44,7 @@ class ParserInterface(object):
     raise NotImplementedError()
 
   def ParseBlameInfo(self, component, file_path, line, revision):
-    """Parses blame imformation of the given file/line in revision.
+    """Parses blame information of the given file/line in revision.
 
     Args:
       component: The component this line is from.
