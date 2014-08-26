b116781c9ea5592d6833e57a88599d02ddb6853d
dcheng <dcheng@chromium.org>
2014-08-26 12:20:09 -0700

    Remove implicit conversions from scoped_refptr to T* in components/nacl/
    This patch was generated by running the rewrite_scoped_refptr clang tool
    on a Linux build.
    
    BUG=110610
    
    Review URL: https://codereview.chromium.org/501993003
    
    Cr-Commit-Position: refs/heads/master@{#291959}

--- components/nacl/browser/pnacl_host.cc
+++ components/nacl/browser/pnacl_host.cc
@@ -511,7 +511,7 @@ void PnaclHost::StoreTranslatedNexe(
   }
   pending_backend_operations_++;
   disk_cache_->StoreNexe(it->second.cache_key,
-                         buffer,
+                         buffer.get(),
                          base::Bind(&PnaclHost::OnTranslatedNexeStored,
                                     weak_factory_.GetWeakPtr(),
                                     it->first));
