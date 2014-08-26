694f55f20d15cc793a81a797369df0478762bbbc
dcheng <dcheng@chromium.org>
2014-08-26 12:11:31 -0700

    Minor scoped_refptr cleanup in cc::OutputSurface.
    Using const scoped_refptr<T>& is more efficient when passing a
    scoped_refptr<T> to a function or returning a scoped_refptr<T> from an
    accessor. It avoids the creation of a temporary scoped_refptr and
    reduces refcount churn.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/467183003
    
    Cr-Commit-Position: refs/heads/master@{#291955}

--- cc/output/gl_renderer_unittest.cc
+++ cc/output/gl_renderer_unittest.cc
@@ -1648,8 +1648,8 @@ class MockOutputSurfaceTest : public GLRendererTest {
 
   OutputSurfaceMockContext* Context() {
     return static_cast<OutputSurfaceMockContext*>(
-        static_cast<TestContextProvider*>(
-            output_surface_.context_provider().get())->TestContext3d());
+        static_cast<TestContextProvider*>(output_surface_.context_provider())
+            ->TestContext3d());
   }
 
   LayerTreeSettings settings_;
