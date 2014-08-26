182d39268d6df5ac421228baee9f53b1b80ae3fe
dmazzoni@chromium.org <dmazzoni@chromium.org>
2014-08-26 17:23:26 +0000

    Send accessibility notifications to the frame client, not view client.
    This is an incremental step to support out-of-process iframe accessibility.
    
    BUG=368298
    
    Review URL: https://codereview.chromium.org/468723005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180911 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/web/WebFrameClient.h
+++ third_party/WebKit/public/web/WebFrameClient.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WebFrameClient_h
 
 #include "../platform/WebColor.h"
+#include "WebAXObject.h"
 #include "WebDOMMessageEvent.h"
 #include "WebDataSource.h"
 #include "WebFrame.h"
@@ -560,6 +561,11 @@ public:
 
     // Access the embedder API for (client-based) screen orientation client .
     virtual WebScreenOrientationClient* webScreenOrientationClient() { return 0; }
+
+    // Accessibility -------------------------------------------------------
+
+    // Notifies embedder about an accessibility event.
+    virtual void postAccessibilityEvent(const WebAXObject&, WebAXEvent) { }
 
 protected:
     virtual ~WebFrameClient() { }
