97ef0aa121861d09e19c19cd00f44f316ca4767f
hshi <hshi@chromium.org>
2014-08-26 16:32:22 -0700

    Only allow webrtc HW offload for H264 encode for extension process.
    Add cmdline switch::kEnableWebRtcHWH264Encoding to control whether
    to report WebRtc HW H264 encoding capability.
    
    BUG=385941
    R=xiyuan@chromium.org,kalman@chromium.org,posicak@chromium.org,jamesr@chromium.org
    TBR=piman@chromium.org
    
    Review URL: https://codereview.chromium.org/453063002
    
    Cr-Commit-Position: refs/heads/master@{#292028}

--- chrome/browser/extensions/chrome_content_browser_client_extensions_part.cc
+++ chrome/browser/extensions/chrome_content_browser_client_extensions_part.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/site_instance.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/common/content_switches.h"
 #include "extensions/browser/extension_host.h"
 #include "extensions/browser/extension_message_filter.h"
 #include "extensions/browser/extension_registry.h"
@@ -527,8 +528,12 @@ void ChromeContentBrowserClientExtensionsPart::
   if (!process)
     return;
   DCHECK(profile);
-  if (ProcessMap::Get(profile)->Contains(process->GetID()))
+  if (ProcessMap::Get(profile)->Contains(process->GetID())) {
     command_line->AppendSwitch(switches::kExtensionProcess);
+#if defined(ENABLE_WEBRTC)
+    command_line->AppendSwitch(::switches::kEnableWebRtcHWH264Encoding);
+#endif
+  }
 }
 
 }  // namespace extensions
