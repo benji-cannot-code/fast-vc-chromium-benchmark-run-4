6b422cd128dd5d291245eb2155603341d78d444d
sanjoy.pal@samsung.com <sanjoy.pal@samsung.com>
2014-08-26 10:47:49 +0000

    Add support to populate custom context menu items.
    contextmenu attribute which enables web pages to specify custom
    context menu along with or in place of default platform context
    menu for a particular element. The value of the contextmenu
    attribute must be the ID of menu element with type popup.
    This CL enables ContextMenuController to populate custom context
    menu items specified by contextmenu attribute on any element.
    
    Specifications:
    http://www.whatwg.org/specs/web-apps/current-work/multipage/forms.html#context-menus
    
    BUG=87553
    
    Review URL: https://codereview.chromium.org/492753002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180896 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/core.gypi
+++ third_party/WebKit/Source/core/core.gypi
@@ -1426,6 +1426,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'page/ContextMenuProvider.h',
             'page/CreateWindow.cpp',
             'page/CreateWindow.h',
+            'page/CustomContextMenuProvider.cpp',
+            'page/CustomContextMenuProvider.h',
             'page/DOMWindowPagePopup.cpp',
             'page/DOMWindowPagePopup.h',
             'page/DragController.cpp',
---
+++ third_party/WebKit/Source/core/page/CustomContextMenuProvider.h
@@ -0,0 +1,46 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CustomContextMenuProvider_h
+#define CustomContextMenuProvider_h
+
+#include "core/page/ContextMenuProvider.h"
+#include "platform/ContextMenuItem.h"
+#include "platform/heap/Handle.h"
+
+namespace blink {
+
+class ContextMenu;
+class HTMLElement;
+class HTMLMenuElement;
+class HTMLMenuItemElement;
+
+class CustomContextMenuProvider FINAL : public ContextMenuProvider {
+public:
+    static PassRefPtr<CustomContextMenuProvider> create(HTMLMenuElement& menu, HTMLElement& subject)
+    {
+        return adoptRef(new CustomContextMenuProvider(menu, subject));
+    }
+
+private:
+    CustomContextMenuProvider(HTMLMenuElement&, HTMLElement&);
+    virtual ~CustomContextMenuProvider();
+
+    virtual void populateContextMenu(ContextMenu*) OVERRIDE;
+    virtual void contextMenuItemSelected(const ContextMenuItem*) OVERRIDE;
+    virtual void contextMenuCleared() OVERRIDE;
+    void populateContextMenuItems(const HTMLMenuElement&, ContextMenu&);
+    void appendSeparator(ContextMenu&);
+    void appendMenuItem(HTMLMenuItemElement*, ContextMenu&);
+    HTMLElement* menuItemAt(unsigned menuId);
+
+    RefPtrWillBePersistent<HTMLMenuElement> m_menu;
+    RefPtrWillBePersistent<HTMLElement> m_subjectElement;
+    WillBePersistentHeapVector<RefPtrWillBeMember<HTMLElement> > m_menuItems;
+};
+
+} // namespace blink
+
+#endif
