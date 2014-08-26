013379338b4fdca3470e0bcd50f37cf1cf57e8a0
dcheng <dcheng@chromium.org>
2014-08-26 15:16:24 -0700

    Remove implicit conversions from scoped_refptr to T* in chrome/browser/browsing_data/
    This patch was generated by running the rewrite_scoped_refptr clang tool
    on a Linux build.
    
    BUG=110610
    
    Review URL: https://codereview.chromium.org/503363003
    
    Cr-Commit-Position: refs/heads/master@{#291996}

--- chrome/browser/browsing_data/browsing_data_remover_unittest.cc
+++ chrome/browser/browsing_data/browsing_data_remover_unittest.cc
@@ -821,7 +821,7 @@ class BrowsingDataRemoverTest : public testing::Test,
   MockExtensionSpecialStoragePolicy* CreateMockPolicy() {
 #if defined(ENABLE_EXTENSIONS)
     mock_policy_ = new MockExtensionSpecialStoragePolicy;
-    return mock_policy_;
+    return mock_policy_.get();
 #else
     NOTREACHED();
     return NULL;
@@ -830,7 +830,7 @@ class BrowsingDataRemoverTest : public testing::Test,
 
   storage::SpecialStoragePolicy* mock_policy() {
 #if defined(ENABLE_EXTENSIONS)
-    return mock_policy_;
+    return mock_policy_.get();
 #else
     return NULL;
 #endif
