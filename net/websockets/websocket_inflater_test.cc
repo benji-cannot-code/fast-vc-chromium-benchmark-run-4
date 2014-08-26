94400d4cfe9e48b1129b15812c11e7871d7652f1
dcheng <dcheng@chromium.org>
2014-08-26 12:46:23 -0700

    Remove implicit conversions from scoped_refptr to T* in net/websockets/
    This patch was generated by running the rewrite_scoped_refptr clang tool
    on a Linux build.
    
    BUG=110610
    
    Review URL: https://codereview.chromium.org/474973004
    
    Cr-Commit-Position: refs/heads/master@{#291970}

--- net/websockets/websocket_inflater_test.cc
+++ net/websockets/websocket_inflater_test.cc
@@ -38,14 +38,14 @@ TEST(WebSocketInflaterTest, InflateHelloTakeOverContext) {
   ASSERT_TRUE(inflater.AddBytes("\xf2\x48\xcd\xc9\xc9\x07\x00", 7));
   ASSERT_TRUE(inflater.Finish());
   actual1 = inflater.GetOutput(inflater.CurrentOutputSize());
-  ASSERT_TRUE(actual1);
+  ASSERT_TRUE(actual1.get());
   EXPECT_EQ("Hello", ToString(actual1.get()));
   EXPECT_EQ(0u, inflater.CurrentOutputSize());
 
   ASSERT_TRUE(inflater.AddBytes("\xf2\x00\x11\x00\x00", 5));
   ASSERT_TRUE(inflater.Finish());
   actual2 = inflater.GetOutput(inflater.CurrentOutputSize());
-  ASSERT_TRUE(actual2);
+  ASSERT_TRUE(actual2.get());
   EXPECT_EQ("Hello", ToString(actual2.get()));
   EXPECT_EQ(0u, inflater.CurrentOutputSize());
 }
@@ -60,7 +60,7 @@ TEST(WebSocketInflaterTest, InflateHelloSmallCapacity) {
   for (size_t i = 0; i < 5; ++i) {
     ASSERT_EQ(1u, inflater.CurrentOutputSize());
     scoped_refptr<IOBufferWithSize> buffer = inflater.GetOutput(1);
-    ASSERT_TRUE(buffer);
+    ASSERT_TRUE(buffer.get());
     ASSERT_EQ(1, buffer->size());
     actual += ToString(buffer.get());
   }
@@ -77,7 +77,7 @@ TEST(WebSocketInflaterTest, InflateHelloSmallCapacityGetTotalOutput) {
   ASSERT_TRUE(inflater.Finish());
   ASSERT_EQ(1u, inflater.CurrentOutputSize());
   actual = inflater.GetOutput(1024);
-  EXPECT_EQ("Hello", ToString(actual));
+  EXPECT_EQ("Hello", ToString(actual.get()));
   EXPECT_EQ(0u, inflater.CurrentOutputSize());
 }
 
@@ -108,7 +108,7 @@ TEST(WebSocketInflaterTest, MultipleAddBytesCalls) {
   }
   ASSERT_TRUE(inflater.Finish());
   actual = inflater.GetOutput(5);
-  ASSERT_TRUE(actual);
+  ASSERT_TRUE(actual.get());
   EXPECT_EQ("Hello", ToString(actual.get()));
 }
 
@@ -120,7 +120,7 @@ TEST(WebSocketInflaterTest, Reset) {
   ASSERT_TRUE(inflater.AddBytes("\xf2\x48\xcd\xc9\xc9\x07\x00", 7));
   ASSERT_TRUE(inflater.Finish());
   actual1 = inflater.GetOutput(inflater.CurrentOutputSize());
-  ASSERT_TRUE(actual1);
+  ASSERT_TRUE(actual1.get());
   EXPECT_EQ("Hello", ToString(actual1.get()));
   EXPECT_EQ(0u, inflater.CurrentOutputSize());
 
@@ -132,7 +132,7 @@ TEST(WebSocketInflaterTest, Reset) {
   ASSERT_TRUE(inflater.AddBytes("\xf2\x48\xcd\xc9\xc9\x07\x00", 7));
   ASSERT_TRUE(inflater.Finish());
   actual2 = inflater.GetOutput(inflater.CurrentOutputSize());
-  ASSERT_TRUE(actual2);
+  ASSERT_TRUE(actual2.get());
   EXPECT_EQ("Hello", ToString(actual2.get()));
   EXPECT_EQ(0u, inflater.CurrentOutputSize());
 }
@@ -145,7 +145,7 @@ TEST(WebSocketInflaterTest, ResetAndLostContext) {
   ASSERT_TRUE(inflater.AddBytes("\xf2\x48\xcd\xc9\xc9\x07\x00", 7));
   ASSERT_TRUE(inflater.Finish());
   actual1 = inflater.GetOutput(inflater.CurrentOutputSize());
-  ASSERT_TRUE(actual1);
+  ASSERT_TRUE(actual1.get());
   EXPECT_EQ("Hello", ToString(actual1.get()));
   EXPECT_EQ(0u, inflater.CurrentOutputSize());
 
@@ -202,7 +202,7 @@ TEST(WebSocketInflaterTest, LargeRandomDeflateInflate) {
 
   compressed = deflater.GetOutput(deflater.CurrentOutputSize());
 
-  ASSERT_TRUE(compressed);
+  ASSERT_TRUE(compressed.get());
   ASSERT_EQ(0u, deflater.CurrentOutputSize());
 
   ASSERT_TRUE(inflater.AddBytes(compressed->data(), compressed->size()));
@@ -211,7 +211,7 @@ TEST(WebSocketInflaterTest, LargeRandomDeflateInflate) {
   while (inflater.CurrentOutputSize() > 0) {
     scoped_refptr<IOBufferWithSize> uncompressed =
         inflater.GetOutput(inflater.CurrentOutputSize());
-    ASSERT_TRUE(uncompressed);
+    ASSERT_TRUE(uncompressed.get());
     output.insert(output.end(),
                   uncompressed->data(),
                   uncompressed->data() + uncompressed->size());
