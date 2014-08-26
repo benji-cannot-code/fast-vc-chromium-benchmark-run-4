894de36e1b87cd7fb524499ba41eb9f61fc61dff
eroman <eroman@chromium.org>
2014-08-26 13:15:45 -0700

    Rename digest.json --> sha.json
    Review URL: https://codereview.chromium.org/508643003
    
    Cr-Commit-Position: refs/heads/master@{#291976}

--- content/child/webcrypto/test/sha_unittest.cc
+++ content/child/webcrypto/test/sha_unittest.cc
@@ -23,8 +23,7 @@ namespace {
 
 TEST(WebCryptoShaTest, DigestSampleSets) {
   scoped_ptr<base::ListValue> tests;
-  // TODO(eroman): rename to sha.json
-  ASSERT_TRUE(ReadJsonTestFileToList("digest.json", &tests));
+  ASSERT_TRUE(ReadJsonTestFileToList("sha.json", &tests));
 
   for (size_t test_index = 0; test_index < tests->GetSize(); ++test_index) {
     SCOPED_TRACE(test_index);
@@ -45,7 +44,7 @@ TEST(WebCryptoShaTest, DigestSampleSets) {
 
 TEST(WebCryptoShaTest, DigestSampleSetsInChunks) {
   scoped_ptr<base::ListValue> tests;
-  ASSERT_TRUE(ReadJsonTestFileToList("digest.json", &tests));
+  ASSERT_TRUE(ReadJsonTestFileToList("sha.json", &tests));
 
   for (size_t test_index = 0; test_index < tests->GetSize(); ++test_index) {
     SCOPED_TRACE(test_index);
