baead98c0564b6795a5acbc32f4ee7f490b42b22
dcheng <dcheng@chromium.org>
2014-08-26 15:53:01 -0700

    Remove implicit conversions from scoped_refptr to T* in content/browser/dom_storage/
    This patch was generated by running the rewrite_scoped_refptr clang tool
    on a Linux build.
    
    BUG=110610
    
    Review URL: https://codereview.chromium.org/506083002
    
    Cr-Commit-Position: refs/heads/master@{#292013}

--- content/browser/dom_storage/session_storage_namespace_impl.cc
+++ content/browser/dom_storage/session_storage_namespace_impl.cc
@@ -29,7 +29,7 @@ SessionStorageNamespaceImpl::SessionStorageNamespaceImpl(
 SessionStorageNamespaceImpl::SessionStorageNamespaceImpl(
     SessionStorageNamespaceImpl* master_session_storage_namespace)
     : session_(new DOMStorageSession(
-          master_session_storage_namespace->session_)) {
+          master_session_storage_namespace->session_.get())) {
 }
 
 
@@ -82,7 +82,8 @@ void SessionStorageNamespaceImpl::Merge(
     const MergeResultCallback& callback) {
   SessionStorageNamespaceImpl* other_impl =
       static_cast<SessionStorageNamespaceImpl*>(other);
-  session_->Merge(actually_merge, process_id, other_impl->session_, callback);
+  session_->Merge(
+      actually_merge, process_id, other_impl->session_.get(), callback);
 }
 
 bool SessionStorageNamespaceImpl::IsAliasOf(SessionStorageNamespace* other) {
