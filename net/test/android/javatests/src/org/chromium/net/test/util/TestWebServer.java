2934bf8cfa60b59fc20ac4df9c508b80ccead763
hush <hush@chromium.org>
2014-08-27 14:05:16 -0700

    Revert of Revert "TestWebServer needs to use a free port." (patchset #1 of https://codereview.chromium.org/514443002/)
    Reason for revert:
    I will land the following CL to fix the findbugs warning
    https://codereview.chromium.org/513583002/
    
    Then I will revert the revert.
    
    Original issue's description:
    > Revert "TestWebServer needs to use a free port."
    >
    > Causing Android Build (dbg) findbugs to fail.
    >
    > This reverts commit c4a3f88c4434e86057a8c7ae4e109b8be4138d8f.
    >
    > BUG=none
    > TBR=hush
    >
    > Committed to pending queue: https://chromium.googlesource.com/chromium/src/+/b055d91
    
    TBR=ajwong@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=none
    
    Review URL: https://codereview.chromium.org/512573002
    
    Cr-Commit-Position: refs/heads/master@{#292218}

--- net/test/android/javatests/src/org/chromium/net/test/util/TestWebServer.java
+++ net/test/android/javatests/src/org/chromium/net/test/util/TestWebServer.java
@@ -61,10 +61,8 @@ import javax.net.ssl.X509TrustManager;
  *
  * Based heavily on the CTSWebServer in Android.
  */
-public class TestWebServer {
+public final class TestWebServer {
     private static final String TAG = "TestWebServer";
-    private static final int SERVER_PORT = 4444;
-    private static final int SSL_SERVER_PORT = 4445;
 
     public static final String SHUTDOWN_PREFIX = "/shutdown";
 
@@ -109,19 +107,21 @@ public class TestWebServer {
     public TestWebServer(boolean ssl) throws Exception {
         mSsl = ssl;
         if (mSsl) {
+            mServerUri = "https:";
             if (sSecureInstance != null) {
                 sSecureInstance.shutdown();
             }
-            mServerUri = "https://localhost:" + SSL_SERVER_PORT;
         } else {
+            mServerUri = "http:";
             if (sInstance != null) {
                 sInstance.shutdown();
             }
-            mServerUri = "http://localhost:" + SERVER_PORT;
         }
+
         setInstance(this, mSsl);
         mServerThread = new ServerThread(this, mSsl);
         mServerThread.start();
+        mServerUri += "//localhost:" + mServerThread.mSocket.getLocalPort();
     }
 
     /**
@@ -553,10 +553,9 @@ public class TestWebServer {
                     if (mIsSsl) {
                         mSslContext = SSLContext.getInstance("TLS");
                         mSslContext.init(getKeyManagers(), null, null);
-                        mSocket = mSslContext.getServerSocketFactory().createServerSocket(
-                                SSL_SERVER_PORT);
+                        mSocket = mSslContext.getServerSocketFactory().createServerSocket(0);
                     } else {
-                        mSocket = new ServerSocket(SERVER_PORT);
+                        mSocket = new ServerSocket(0);
                     }
                     return;
                 } catch (IOException e) {
