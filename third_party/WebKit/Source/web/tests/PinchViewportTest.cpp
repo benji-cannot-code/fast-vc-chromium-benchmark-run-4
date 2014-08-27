94ad16421a0375c2c889db72ba4754f1cd2cb3ef
shreeram.k@samsung.com <shreeram.k@samsung.com>
2014-08-27 07:45:25 +0000

    Removing duplicate 'using namespace blink' in platform and web
    Review URL: https://codereview.chromium.org/499543002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180942 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/tests/PinchViewportTest.cpp
+++ third_party/WebKit/Source/web/tests/PinchViewportTest.cpp
@@ -69,7 +69,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 
 using namespace blink;
-using namespace blink;
 
 using ::testing::_;
 using ::testing::PrintToString;
--- third_party/WebKit/Source/web/tests/ProgrammaticScrollTest.cpp
+++ third_party/WebKit/Source/web/tests/ProgrammaticScrollTest.cpp
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <gtest/gtest.h>
 
 using namespace blink;
-using namespace blink;
 
 namespace {
 
