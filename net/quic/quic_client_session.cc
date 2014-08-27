d8b6893ef6aa8c6bf39349f39b902e4d47e16197
jri <jri@chromium.org>
2014-08-27 11:10:25 -0700

    Adding histogram to count number of RTTs for handshake when confirmation is required.
    BUG=
    
    Review URL: https://codereview.chromium.org/502243002
    
    Cr-Commit-Position: refs/heads/master@{#292182}

--- net/quic/quic_client_session.cc
+++ net/quic/quic_client_session.cc
@@ -243,6 +243,11 @@ QuicClientSession::~QuicClientSession() {
     } else {
       UMA_HISTOGRAM_CUSTOM_COUNTS("Net.QuicSession.ConnectRandomPortForHTTP",
                                   round_trip_handshakes, 0, 3, 4);
+      if (require_confirmation_) {
+        UMA_HISTOGRAM_CUSTOM_COUNTS(
+            "Net.QuicSession.ConnectRandomPortRequiringConfirmationForHTTP",
+            round_trip_handshakes, 0, 3, 4);
+      }
     }
   } else {
     if (port_selected) {
@@ -251,6 +256,11 @@ QuicClientSession::~QuicClientSession() {
     } else {
       UMA_HISTOGRAM_CUSTOM_COUNTS("Net.QuicSession.ConnectRandomPortForHTTPS",
                                   round_trip_handshakes, 0, 3, 4);
+      if (require_confirmation_) {
+        UMA_HISTOGRAM_CUSTOM_COUNTS(
+            "Net.QuicSession.ConnectRandomPortRequiringConfirmationForHTTPS",
+            round_trip_handshakes, 0, 3, 4);
+      }
     }
   }
   const QuicConnectionStats stats = connection()->GetStats();
