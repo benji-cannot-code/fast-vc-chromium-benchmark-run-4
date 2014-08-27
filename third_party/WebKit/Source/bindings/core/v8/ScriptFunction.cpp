benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 9f586eab90c9ba28810adf6aab595742c8402efc

Move WaitUntilObserver and RespondWithObserver to Oilpan's heap
This is a preparation for enabling Oilpan by default for serviceworkers/.

BUG=340522
TBR=tkent

Review URL: https://codereview.chromium.org/467973005

git-svn-id: svn://svn.chromium.org/blink/trunk@180902 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/modules/serviceworkers/RespondWithObserver.cpp
@@ -26,14 +26,14 @@ public:
         Rejected,
     };
 
-    static PassOwnPtr<ScriptFunction> create(PassRefPtr<RespondWithObserver> observer, ResolveType type)
+    static PassOwnPtr<ScriptFunction> create(RespondWithObserver* observer, ResolveType type)
     {
         ExecutionContext* executionContext = observer->executionContext();
         return adoptPtr(new ThenFunction(toIsolate(executionContext), observer, type));
     }
 
 private:
-    ThenFunction(v8::Isolate* isolate, PassRefPtr<RespondWithObserver> observer, ResolveType type)
+    ThenFunction(v8::Isolate* isolate, RespondWithObserver* observer, ResolveType type)
         : ScriptFunction(isolate)
         , m_observer(observer)
         , m_resolveType(type)
@@ -52,13 +52,13 @@ private:
         return value;
     }
 
-    RefPtr<RespondWithObserver> m_observer;
+    RefPtrWillBePersistent<RespondWithObserver> m_observer;
     ResolveType m_resolveType;
 };
 
-PassRefPtr<RespondWithObserver> RespondWithObserver::create(ExecutionContext* context, int eventID)
+PassRefPtrWillBeRawPtr<RespondWithObserver> RespondWithObserver::create(ExecutionContext* context, int eventID)
 {
-    return adoptRef(new RespondWithObserver(context, eventID));
+    return adoptRefWillBeNoop(new RespondWithObserver(context, eventID));
 }
 
 RespondWithObserver::~RespondWithObserver()

third_party/WebKit/Source/modules/serviceworkers/WaitUntilObserver.cpp
@@ -27,14 +27,14 @@ public:
         Rejected,
     };
 
-    static PassOwnPtr<ScriptFunction> create(PassRefPtr<WaitUntilObserver> observer, ResolveType type)
+    static PassOwnPtr<ScriptFunction> create(PassRefPtrWillBeRawPtr<WaitUntilObserver> observer, ResolveType type)
     {
         ExecutionContext* executionContext = observer->executionContext();
         return adoptPtr(new ThenFunction(toIsolate(executionContext), observer, type));
     }
 
 private:
-    ThenFunction(v8::Isolate* isolate, PassRefPtr<WaitUntilObserver> observer, ResolveType type)
+    ThenFunction(v8::Isolate* isolate, PassRefPtrWillBeRawPtr<WaitUntilObserver> observer, ResolveType type)
         : ScriptFunction(isolate)
         , m_observer(observer)
         , m_resolveType(type)
@@ -52,13 +52,13 @@ private:
         return value;
     }
 
-    RefPtr<WaitUntilObserver> m_observer;
+    RefPtrWillBePersistent<WaitUntilObserver> m_observer;
     ResolveType m_resolveType;
 };
 
-PassRefPtr<WaitUntilObserver> WaitUntilObserver::create(ExecutionContext* context, EventType type, int eventID)
+PassRefPtrWillBeRawPtr<WaitUntilObserver> WaitUntilObserver::create(ExecutionContext* context, EventType type, int eventID)
 {
-    return adoptRef(new WaitUntilObserver(context, type, eventID));
+    return adoptRefWillBeNoop(new WaitUntilObserver(context, type, eventID));
 }
 
 WaitUntilObserver::~WaitUntilObserver()

third_party/WebKit/Source/modules/serviceworkers/FetchEvent.cpp
@@ -17,7 +17,7 @@ PassRefPtrWillBeRawPtr<FetchEvent> FetchEvent::create()
     return adoptRefWillBeNoop(new FetchEvent());
 }
 
-PassRefPtrWillBeRawPtr<FetchEvent> FetchEvent::create(PassRefPtr<RespondWithObserver> observer, PassRefPtrWillBeRawPtr<Request> request)
+PassRefPtrWillBeRawPtr<FetchEvent> FetchEvent::create(PassRefPtrWillBeRawPtr<RespondWithObserver> observer, PassRefPtrWillBeRawPtr<Request> request)
 {
     return adoptRefWillBeNoop(new FetchEvent(observer, request));
 }
@@ -53,7 +53,7 @@ FetchEvent::FetchEvent()
     ScriptWrappable::init(this);
 }
 
-FetchEvent::FetchEvent(PassRefPtr<RespondWithObserver> observer, PassRefPtrWillBeRawPtr<Request> request)
+FetchEvent::FetchEvent(PassRefPtrWillBeRawPtr<RespondWithObserver> observer, PassRefPtrWillBeRawPtr<Request> request)
     : Event(EventTypeNames::fetch, /*canBubble=*/false, /*cancelable=*/true)
     , m_observer(observer)
     , m_request(request)
@@ -65,6 +65,7 @@ FetchEvent::FetchEvent(PassRefPtr<RespondWithObserver> observer, PassRefPtrWillB
 void FetchEvent::trace(Visitor* visitor)
 {
     visitor->trace(m_request);
+    visitor->trace(m_observer);
     Event::trace(visitor);
 }
 

third_party/WebKit/Source/modules/serviceworkers/FetchEvent.h
@@ -22,7 +22,7 @@ class RespondWithObserver;
 class FetchEvent FINAL : public Event {
 public:
     static PassRefPtrWillBeRawPtr<FetchEvent> create();
-    static PassRefPtrWillBeRawPtr<FetchEvent> create(PassRefPtr<RespondWithObserver>, PassRefPtrWillBeRawPtr<Request>);
+    static PassRefPtrWillBeRawPtr<FetchEvent> create(PassRefPtrWillBeRawPtr<RespondWithObserver>, PassRefPtrWillBeRawPtr<Request>);
 
     PassRefPtrWillBeRawPtr<Request> request() const;
     bool isReload() const;
@@ -37,10 +37,10 @@ public:
 
 protected:
     FetchEvent();
-    FetchEvent(PassRefPtr<RespondWithObserver>, PassRefPtrWillBeRawPtr<Request>);
+    FetchEvent(PassRefPtrWillBeRawPtr<RespondWithObserver>, PassRefPtrWillBeRawPtr<Request>);
 
 private:
-    RefPtr<RespondWithObserver> m_observer;
+    RefPtrWillBeMember<RespondWithObserver> m_observer;
     RefPtrWillBeMember<Request> m_request;
     bool m_isReload;
 };

third_party/WebKit/Source/modules/serviceworkers/InstallEvent.cpp
@@ -44,7 +44,7 @@ PassRefPtrWillBeRawPtr<InstallEvent> InstallEvent::create()
     return adoptRefWillBeNoop(new InstallEvent());
 }
 
-PassRefPtrWillBeRawPtr<InstallEvent> InstallEvent::create(const AtomicString& type, const EventInit& initializer, PassRefPtr<WaitUntilObserver> observer)
+PassRefPtrWillBeRawPtr<InstallEvent> InstallEvent::create(const AtomicString& type, const EventInit& initializer, PassRefPtrWillBeRawPtr<WaitUntilObserver> observer)
 {
     return adoptRefWillBeNoop(new InstallEvent(type, initializer, observer));
 }
@@ -74,7 +74,7 @@ InstallEvent::InstallEvent()
     ScriptWrappable::init(this);
 }
 
-InstallEvent::InstallEvent(const AtomicString& type, const EventInit& initializer, PassRefPtr<WaitUntilObserver> observer)
+InstallEvent::InstallEvent(const AtomicString& type, const EventInit& initializer, PassRefPtrWillBeRawPtr<WaitUntilObserver> observer)
     : InstallPhaseEvent(type, initializer, observer)
 {
     ScriptWrappable::init(this);

third_party/WebKit/Source/modules/serviceworkers/InstallEvent.h
@@ -43,7 +43,7 @@ class ExecutionContext;
 class InstallEvent FINAL : public InstallPhaseEvent {
 public:
     static PassRefPtrWillBeRawPtr<InstallEvent> create();
-    static PassRefPtrWillBeRawPtr<InstallEvent> create(const AtomicString& type, const EventInit&, PassRefPtr<WaitUntilObserver>);
+    static PassRefPtrWillBeRawPtr<InstallEvent> create(const AtomicString& type, const EventInit&, PassRefPtrWillBeRawPtr<WaitUntilObserver>);
 
     void replace();
 
@@ -55,7 +55,7 @@ public:
 
 private:
     InstallEvent();
-    InstallEvent(const AtomicString& type, const EventInit&, PassRefPtr<WaitUntilObserver>);
+    InstallEvent(const AtomicString& type, const EventInit&, PassRefPtrWillBeRawPtr<WaitUntilObserver>);
 };
 
 } // namespace blink

third_party/WebKit/Source/modules/serviceworkers/InstallPhaseEvent.cpp
@@ -42,7 +42,7 @@ PassRefPtrWillBeRawPtr<InstallPhaseEvent> InstallPhaseEvent::create()
     return adoptRefWillBeNoop(new InstallPhaseEvent());
 }
 
-PassRefPtrWillBeRawPtr<InstallPhaseEvent> InstallPhaseEvent::create(const AtomicString& type, const EventInit& eventInit, PassRefPtr<WaitUntilObserver> observer)
+PassRefPtrWillBeRawPtr<InstallPhaseEvent> InstallPhaseEvent::create(const AtomicString& type, const EventInit& eventInit, PassRefPtrWillBeRawPtr<WaitUntilObserver> observer)
 {
     return adoptRefWillBeNoop(new InstallPhaseEvent(type, eventInit, observer));
 }
@@ -61,7 +61,7 @@ InstallPhaseEvent::InstallPhaseEvent()
     ScriptWrappable::init(this);
 }
 
-InstallPhaseEvent::InstallPhaseEvent(const AtomicString& type, const EventInit& initializer, PassRefPtr<WaitUntilObserver> observer)
+InstallPhaseEvent::InstallPhaseEvent(const AtomicString& type, const EventInit& initializer, PassRefPtrWillBeRawPtr<WaitUntilObserver> observer)
     : Event(type, initializer)
     , m_observer(observer)
 {
@@ -75,6 +75,7 @@ const AtomicString& InstallPhaseEvent::interfaceName() const
 
 void InstallPhaseEvent::trace(Visitor* visitor)
 {
+    visitor->trace(m_observer);
     Event::trace(visitor);
 }
 

third_party/WebKit/Source/modules/serviceworkers/InstallPhaseEvent.h
@@ -42,7 +42,7 @@ class WaitUntilObserver;
 class InstallPhaseEvent : public Event {
 public:
     static PassRefPtrWillBeRawPtr<InstallPhaseEvent> create();
-    static PassRefPtrWillBeRawPtr<InstallPhaseEvent> create(const AtomicString& type, const EventInit&, PassRefPtr<WaitUntilObserver>);
+    static PassRefPtrWillBeRawPtr<InstallPhaseEvent> create(const AtomicString& type, const EventInit&, PassRefPtrWillBeRawPtr<WaitUntilObserver>);
 
     virtual ~InstallPhaseEvent();
 
@@ -53,9 +53,9 @@ public:
 
 protected:
     InstallPhaseEvent();
-    InstallPhaseEvent(const AtomicString& type, const EventInit&, PassRefPtr<WaitUntilObserver>);
+    InstallPhaseEvent(const AtomicString& type, const EventInit&, PassRefPtrWillBeRawPtr<WaitUntilObserver>);
 
-    RefPtr<WaitUntilObserver> m_observer;
+    RefPtrWillBeMember<WaitUntilObserver> m_observer;
 };
 
 } // namespace blink

commit 4c14ca5bbfdfc0000f33728ec5f4a925f19c6961

Oilpan: Move ScriptFunction to oilpan's heap
This CL fixes crashes in oilpan builds introduced by r180902.

The problem was that RespondWithObserver::ThenFunction and WaitUntilObserver::ThenFunction
hold a persistent handle, and the persistent handle remains until the end of the worker thread
execution if the ThenFunction is not resolved nor rejected by user's script.
Then the final Oilpan's GC his the empty ASSERT.

This CL fixes the issue by moving the ThenFunction to oilpan's heap and replaces
the persitent handle with a Member.

BUG=340522

Review URL: https://codereview.chromium.org/490103005

git-svn-id: svn://svn.chromium.org/blink/trunk@180952 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/bindings/core/v8/ScriptFunction.cpp
@@ -22,7 +22,7 @@ void ScriptFunction::callCallback(const v8::FunctionCallbackInfo<v8::Value>& arg
     v8SetReturnValue(args, result.v8Value());
 }
 
-v8::Handle<v8::Function> ScriptFunction::adoptByGarbageCollector(PassOwnPtr<ScriptFunction> function)
+v8::Handle<v8::Function> ScriptFunction::adoptByGarbageCollector(PassOwnPtrWillBeRawPtr<ScriptFunction> function)
 {
     if (!function)
         return v8::Handle<v8::Function>();

third_party/WebKit/Source/bindings/core/v8/ScriptFunction.h
@@ -34,15 +34,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "bindings/core/v8/ScriptValue.h"
 #include "bindings/core/v8/V8GarbageCollected.h"
-#include "wtf/RefCounted.h"
+#include "platform/heap/Handle.h"
 #include <v8.h>
 
 namespace blink {
 
-class ScriptFunction : public V8GarbageCollected<ScriptFunction> {
+class ScriptFunction : public NoBaseWillBeGarbageCollectedFinalized<ScriptFunction>, public V8GarbageCollected<ScriptFunction> {
 public:
     virtual ~ScriptFunction() { }
-    static v8::Handle<v8::Function> adoptByGarbageCollector(PassOwnPtr<ScriptFunction>);
+    static v8::Handle<v8::Function> adoptByGarbageCollector(PassOwnPtrWillBeRawPtr<ScriptFunction>);
+
+    virtual void trace(Visitor*) { }
 
 protected:
     ScriptFunction(v8::Isolate* isolate) : V8GarbageCollected<ScriptFunction>(isolate) { }

third_party/WebKit/Source/bindings/core/v8/ScriptPromise.cpp
@@ -101,7 +101,7 @@ ScriptPromise::ScriptPromise(ScriptState* scriptState, v8::Handle<v8::Value> val
     m_promise = ScriptValue(scriptState, value);
 }
 
-ScriptPromise ScriptPromise::then(PassOwnPtr<ScriptFunction> onFulfilled, PassOwnPtr<ScriptFunction> onRejected)
+ScriptPromise ScriptPromise::then(PassOwnPtrWillBeRawPtr<ScriptFunction> onFulfilled, PassOwnPtrWillBeRawPtr<ScriptFunction> onRejected)
 {
     if (m_promise.isEmpty())
         return ScriptPromise();

third_party/WebKit/Source/bindings/core/v8/ScriptPromise.h
@@ -61,7 +61,7 @@ public:
     // If |promise| is not a Promise object, throws a v8 TypeError.
     ScriptPromise(ScriptState*, v8::Handle<v8::Value> promise);
 
-    ScriptPromise then(PassOwnPtr<ScriptFunction> onFulfilled, PassOwnPtr<ScriptFunction> onRejected = PassOwnPtr<ScriptFunction>());
+    ScriptPromise then(PassOwnPtrWillBeRawPtr<ScriptFunction> onFulfilled, PassOwnPtrWillBeRawPtr<ScriptFunction> onRejected = PassOwnPtrWillBeRawPtr<ScriptFunction>());
 
     bool isObject() const
     {

third_party/WebKit/Source/modules/serviceworkers/RespondWithObserver.cpp
@@ -26,10 +26,16 @@ public:
         Rejected,
     };
 
-    static PassOwnPtr<ScriptFunction> create(RespondWithObserver* observer, ResolveType type)
+    static PassOwnPtrWillBeRawPtr<ScriptFunction> create(RespondWithObserver* observer, ResolveType type)
     {
         ExecutionContext* executionContext = observer->executionContext();
-        return adoptPtr(new ThenFunction(toIsolate(executionContext), observer, type));
+        return adoptPtrWillBeNoop(new ThenFunction(toIsolate(executionContext), observer, type));
+    }
+
+    virtual void trace(Visitor* visitor) OVERRIDE
+    {
+        visitor->trace(m_observer);
+        ScriptFunction::trace(visitor);
     }
 
 private:
@@ -52,7 +58,7 @@ private:
         return value;
     }
 
-    RefPtrWillBePersistent<RespondWithObserver> m_observer;
+    RefPtrWillBeMember<RespondWithObserver> m_observer;
     ResolveType m_resolveType;
 };
 

third_party/WebKit/Source/modules/serviceworkers/ServiceWorker.cpp
@@ -48,11 +48,18 @@ namespace blink {
 
 class ServiceWorker::ThenFunction FINAL : public ScriptFunction {
 public:
-    static PassOwnPtr<ScriptFunction> create(PassRefPtrWillBeRawPtr<ServiceWorker> observer)
+    static PassOwnPtrWillBeRawPtr<ScriptFunction> create(PassRefPtrWillBeRawPtr<ServiceWorker> observer)
     {
         ExecutionContext* executionContext = observer->executionContext();
-        return adoptPtr(new ThenFunction(toIsolate(executionContext), observer));
+        return adoptPtrWillBeNoop(new ThenFunction(toIsolate(executionContext), observer));
     }
+
+    virtual void trace(Visitor* visitor) OVERRIDE
+    {
+        visitor->trace(m_observer);
+        ScriptFunction::trace(visitor);
+    }
+
 private:
     ThenFunction(v8::Isolate* isolate, PassRefPtrWillBeRawPtr<ServiceWorker> observer)
         : ScriptFunction(isolate)
@@ -66,7 +73,7 @@ private:
         return value;
     }
 
-    RefPtrWillBePersistent<ServiceWorker> m_observer;
+    RefPtrWillBeMember<ServiceWorker> m_observer;
 };
 
 const AtomicString& ServiceWorker::interfaceName() const

third_party/WebKit/Source/modules/serviceworkers/WaitUntilObserver.cpp
@@ -27,10 +27,16 @@ public:
         Rejected,
     };
 
-    static PassOwnPtr<ScriptFunction> create(PassRefPtrWillBeRawPtr<WaitUntilObserver> observer, ResolveType type)
+    static PassOwnPtrWillBeRawPtr<ScriptFunction> create(PassRefPtrWillBeRawPtr<WaitUntilObserver> observer, ResolveType type)
     {
         ExecutionContext* executionContext = observer->executionContext();
-        return adoptPtr(new ThenFunction(toIsolate(executionContext), observer, type));
+        return adoptPtrWillBeNoop(new ThenFunction(toIsolate(executionContext), observer, type));
+    }
+
+    virtual void trace(Visitor* visitor) OVERRIDE
+    {
+        visitor->trace(m_observer);
+        ScriptFunction::trace(visitor);
     }
 
 private:
@@ -52,7 +58,7 @@ private:
         return value;
     }
 
-    RefPtrWillBePersistent<WaitUntilObserver> m_observer;
+    RefPtrWillBeMember<WaitUntilObserver> m_observer;
     ResolveType m_resolveType;
 };
 

third_party/WebKit/Source/wtf/RawPtr.h
@@ -90,6 +90,12 @@ public:
         m_ptr = 0;
         return ptr;
     }
+    T* leakPtr()
+    {
+        T* ptr = m_ptr;
+        m_ptr = 0;
+        return ptr;
+    }
 
     template<typename U>
     RawPtr& operator=(U* ptr)

third_party/WebKit/Source/bindings/core/v8/ScriptPromisePropertyTest.cpp
@@ -30,7 +30,7 @@ using namespace blink;
 
 namespace {
 
-class NotReached : public ScriptFunction {
+class NotReached FINAL : public ScriptFunction {
 public:
     NotReached() : ScriptFunction(v8::Isolate::GetCurrent()) { }
 
@@ -44,7 +44,7 @@ ScriptValue NotReached::call(ScriptValue)
     return ScriptValue();
 }
 
-class StubFunction : public ScriptFunction {
+class StubFunction FINAL : public ScriptFunction {
 public:
     StubFunction(ScriptValue&, size_t& callCount);
 
@@ -142,8 +142,8 @@ public:
 
     void gc() { V8GCController::collectGarbage(v8::Isolate::GetCurrent()); }
 
-    PassOwnPtr<ScriptFunction> notReached() { return adoptPtr(new NotReached()); }
-    PassOwnPtr<ScriptFunction> stub(ScriptValue& value, size_t& callCount) { return adoptPtr(new StubFunction(value, callCount)); }
+    PassOwnPtrWillBeRawPtr<ScriptFunction> notReached() { return adoptPtrWillBeNoop(new NotReached()); }
+    PassOwnPtrWillBeRawPtr<ScriptFunction> stub(ScriptValue& value, size_t& callCount) { return adoptPtrWillBeNoop(new StubFunction(value, callCount)); }
 
     template <typename T>
     ScriptValue wrap(DOMWrapperWorld& world, const T& value)

third_party/WebKit/Source/bindings/core/v8/ScriptPromiseResolverTest.cpp
@@ -22,11 +22,11 @@ namespace {
 
 void callback(const v8::FunctionCallbackInfo<v8::Value>& info) { }
 
-class Function : public ScriptFunction {
+class Function FINAL : public ScriptFunction {
 public:
-    static PassOwnPtr<Function> create(v8::Isolate* isolate, String* value)
+    static PassOwnPtrWillBeRawPtr<Function> create(v8::Isolate* isolate, String* value)
     {
-        return adoptPtr(new Function(isolate, value));
+        return adoptPtrWillBeNoop(new Function(isolate, value));
     }
 
     virtual ScriptValue call(ScriptValue value) OVERRIDE

third_party/WebKit/Source/bindings/core/v8/ScriptPromiseTest.cpp
@@ -47,11 +47,11 @@ namespace {
 
 void callback(const v8::FunctionCallbackInfo<v8::Value>& info) { }
 
-class Function : public ScriptFunction {
+class Function FINAL : public ScriptFunction {
 public:
-    static PassOwnPtr<Function> create(v8::Isolate* isolate, String* value)
+    static PassOwnPtrWillBeRawPtr<Function> create(v8::Isolate* isolate, String* value)
     {
-        return adoptPtr(new Function(isolate, value));
+        return adoptPtrWillBeNoop(new Function(isolate, value));
     }
 
     virtual ScriptValue call(ScriptValue value) OVERRIDE

third_party/WebKit/Source/core/streams/ReadableStreamTest.cpp
@@ -30,11 +30,11 @@ namespace {
 typedef ::testing::StrictMock<::testing::MockFunction<void(int)> > Checkpoint;
 typedef ReadableStreamImpl<ReadableStreamChunkTypeTraits<String> > StringStream;
 
-class StringCapturingFunction : public ScriptFunction {
+class StringCapturingFunction FINAL : public ScriptFunction {
 public:
-    static PassOwnPtr<StringCapturingFunction> create(v8::Isolate* isolate, String* value)
+    static PassOwnPtrWillBeRawPtr<StringCapturingFunction> create(v8::Isolate* isolate, String* value)
     {
-        return adoptPtr(new StringCapturingFunction(isolate, value));
+        return adoptPtrWillBeNoop(new StringCapturingFunction(isolate, value));
     }
 
     virtual ScriptValue call(ScriptValue value) OVERRIDE
@@ -92,7 +92,7 @@ public:
     ScriptState* scriptState() { return ScriptState::forMainWorld(m_page->document().frame()); }
     v8::Isolate* isolate() { return scriptState()->isolate(); }
 
-    PassOwnPtr<StringCapturingFunction> createCaptor(String* value)
+    PassOwnPtrWillBeRawPtr<StringCapturingFunction> createCaptor(String* value)
     {
         return StringCapturingFunction::create(isolate(), value);
     }

third_party/WebKit/Source/core/testing/Internals.cpp
@@ -2060,11 +2060,11 @@ void Internals::setShouldRevealPassword(Element* element, bool reveal, Exception
 
 namespace {
 
-class AddOneFunction : public ScriptFunction {
+class AddOneFunction FINAL : public ScriptFunction {
 public:
-    static PassOwnPtr<ScriptFunction> create(ExecutionContext* context)
+    static PassOwnPtrWillBeRawPtr<ScriptFunction> create(ExecutionContext* context)
     {
-        return adoptPtr(new AddOneFunction(toIsolate(context)));
+        return adoptPtrWillBeNoop(new AddOneFunction(toIsolate(context)));
     }
 
 private:

third_party/WebKit/Source/modules/serviceworkers/ServiceWorkerContainerTest.cpp
@@ -41,9 +41,9 @@ public:
 
     // The returned ScriptFunction can outlive the StubScriptFunction,
     // but it should not be called after the StubScriptFunction dies.
-    PassOwnPtr<ScriptFunction> function(v8::Isolate* isolate)
+    PassOwnPtrWillBeRawPtr<ScriptFunction> function(v8::Isolate* isolate)
     {
-        return adoptPtr(new ScriptFunctionImpl(isolate, *this));
+        return adoptPtrWillBeNoop(new ScriptFunctionImpl(isolate, *this));
     }
 
     size_t callCount() { return m_callCount; }
@@ -53,7 +53,7 @@ private:
     size_t m_callCount;
     ScriptValue m_arg;
 
-    class ScriptFunctionImpl : public ScriptFunction {
+    class ScriptFunctionImpl FINAL : public ScriptFunction {
     public:
         ScriptFunctionImpl(v8::Isolate* isolate, StubScriptFunction& owner)
             : ScriptFunction(isolate)