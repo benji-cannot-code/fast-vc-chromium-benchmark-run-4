de15edb17660f68cfdc7334fec84c994d60e3870
vitalybuka <vitalybuka@chromium.org>
2014-08-27 12:00:53 -0700

    Check Firewall state only initializing mDns from PrintPreview.
    BUG=366408
    
    Review URL: https://codereview.chromium.org/504223005
    
    Cr-Commit-Position: refs/heads/master@{#292200}

--- chrome/browser/local_discovery/service_discovery_shared_client.h
+++ chrome/browser/local_discovery/service_discovery_shared_client.h
@@ -16,6 +16,10 @@ class ServiceDiscoverySharedClient
  public:
   static scoped_refptr<ServiceDiscoverySharedClient> GetInstance();
 
+  typedef base::Callback<void(scoped_refptr<ServiceDiscoverySharedClient>)>
+      GetInstanceCallback;
+  static void GetInstanceWithoutAlert(const GetInstanceCallback& callback);
+
  protected:
   ServiceDiscoverySharedClient();
   virtual ~ServiceDiscoverySharedClient();
