d2bd592398cedb35441bab688228ac3827081fa4
chrishtr@chromium.org <chrishtr@chromium.org>
2014-08-27 19:23:36 +0000

    Make the compositing assert disabler for paint invalidation more targeted.
    BUG=360286
    
    Review URL: https://codereview.chromium.org/506553004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180997 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLCanvasElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLCanvasElement.cpp
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/html/canvas/WebGLContextEvent.h"
 #include "core/html/canvas/WebGLRenderingContext.h"
 #include "core/rendering/RenderHTMLCanvas.h"
+#include "core/rendering/RenderLayer.h"
 #include "platform/MIMETypeRegistry.h"
 #include "platform/RuntimeEnabledFeatures.h"
 #include "platform/graphics/Canvas2DImageBufferSurface.h"
@@ -213,6 +214,9 @@ void HTMLCanvasElement::didFinalizeFrame()
     m_dirtyRect.intersect(srcRect);
     if (RenderBox* ro = renderBox()) {
         FloatRect mappedDirtyRect = mapRect(m_dirtyRect, srcRect, ro->contentBoxRect());
+        // For querying RenderLayer::compositingState()
+        // FIXME: is this invalidation using the correct compositing state?
+        DisableCompositingQueryAsserts disabler;
         ro->invalidatePaintRectangle(enclosingIntRect(mappedDirtyRect));
     }
     notifyObserversCanvasChanged(m_dirtyRect);
