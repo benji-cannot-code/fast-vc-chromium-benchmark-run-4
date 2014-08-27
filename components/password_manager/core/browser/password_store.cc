7f773ae7c3e133e7541c559c6216e2399cc7e977
vasilii <vasilii@chromium.org>
2014-08-27 04:56:18 -0700

    Track empty username and password in PasswordStore.
    Having both username and password empty is incorrect. We should find out where they come from.
    
    BUG=404012
    
    Review URL: https://codereview.chromium.org/514573002
    
    Cr-Commit-Position: refs/heads/master@{#292135}

--- components/password_manager/core/browser/password_store.cc
+++ components/password_manager/core/browser/password_store.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/password_store.h"
 
 #include "base/bind.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/message_loop/message_loop.h"
 #include "base/message_loop/message_loop_proxy.h"
@@ -34,6 +35,12 @@ void MaybeCallConsumerCallback(base::WeakPtr<PasswordStoreConsumer> consumer,
     consumer->OnGetPasswordStoreResults(*result);
   else
     STLDeleteElements(result.get());
+}
+
+// http://crbug.com/404012. Let's see where the empty fields come from.
+void CheckForEmptyUsernameAndPassword(const PasswordForm& form) {
+  if (form.username_value.empty() && form.password_value.empty())
+    base::debug::DumpWithoutCrashing();
 }
 
 }  // namespace
@@ -89,12 +96,14 @@ bool PasswordStore::Init(const syncer::SyncableService::StartSyncFlare& flare,
 }
 
 void PasswordStore::AddLogin(const PasswordForm& form) {
+  CheckForEmptyUsernameAndPassword(form);
   ScheduleTask(
       base::Bind(&PasswordStore::WrapModificationTask, this,
                  base::Bind(&PasswordStore::AddLoginImpl, this, form)));
 }
 
 void PasswordStore::UpdateLogin(const PasswordForm& form) {
+  CheckForEmptyUsernameAndPassword(form);
   ScheduleTask(
       base::Bind(&PasswordStore::WrapModificationTask, this,
                  base::Bind(&PasswordStore::UpdateLoginImpl, this, form)));
