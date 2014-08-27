70562263b8c847f3069d6253f0b4699980b65387
sergeyv@chromium.org <sergeyv@chromium.org>
2014-08-27 09:16:39 +0000

    DevTools: Fix duplication of context menu items in elements panel with workers
    BUG=407071
    
    Review URL: https://codereview.chromium.org/504143002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180950 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/elements/ElementsTreeOutline.js
+++ third_party/WebKit/Source/devtools/front_end/elements/ElementsTreeOutline.js
@@ -772,7 +772,7 @@ WebInspector.ElementsTreeOutline.prototype = {
     _contextMenuEventFired: function(event)
     {
         var treeElement = this._treeElementFromEvent(event);
-        if (!treeElement)
+        if (!treeElement || treeElement.treeOutline !== this)
             return;
 
         var contextMenu = new WebInspector.ContextMenu(event);
@@ -783,7 +783,7 @@ WebInspector.ElementsTreeOutline.prototype = {
     populateContextMenu: function(contextMenu, event)
     {
         var treeElement = this._treeElementFromEvent(event);
-        if (!treeElement)
+        if (!treeElement || treeElement.treeOutline !== this)
             return;
 
         var isPseudoElement = !!treeElement._node.pseudoType();
