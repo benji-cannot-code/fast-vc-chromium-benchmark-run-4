db7cf6b4e47fce978db0523be00c7ed5e5eec98d
kochi@chromium.org <kochi@chromium.org>
2014-08-27 03:35:30 +0000

    Handle CSS stylesheet order properly in Shadow DOM
    TreeBoundaryCrossingRules did not take the order of
    declaration appearing in separate CSSStyleSheets into
    account and therefore failed sorting of the rules.
    
    Passing CSSStyleSheet's index to TreeBoundaryCrossingRules
    so rules can properly sorted.
    
    BUG=396585
    TEST=layout test fast/dom/shadow/stylesheets-order-in-shadow-dom
    
    Review URL: https://codereview.chromium.org/488033002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180932 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/TreeBoundaryCrossingRules.h
+++ third_party/WebKit/Source/core/css/TreeBoundaryCrossingRules.h
@@ -41,7 +41,7 @@ class RuleFeatureSet;
 class TreeBoundaryCrossingRules FINAL {
     DISALLOW_ALLOCATION();
 public:
-    void addTreeBoundaryCrossingRules(const RuleSet&, ContainerNode& scope, CSSStyleSheet*);
+    void addTreeBoundaryCrossingRules(const RuleSet&, CSSStyleSheet*, unsigned sheetIndex, ContainerNode&);
 
     void reset(const ContainerNode* scopingNode);
     void collectFeaturesTo(RuleFeatureSet&);
@@ -51,7 +51,25 @@ public:
 
 private:
     size_t size() const { return m_scopingNodes.size(); }
-    typedef WillBeHeapVector<std::pair<CSSStyleSheet*, OwnPtrWillBeMember<RuleSet> > > CSSStyleSheetRuleSubSet;
+    class RuleSubSet {
+    public:
+        static PassOwnPtrWillBeRawPtr<RuleSubSet> create(CSSStyleSheet* sheet, unsigned index, PassOwnPtrWillBeRawPtr<RuleSet> rules)
+        {
+            return adoptPtrWillBeNoop(new RuleSubSet(sheet, index, rules));
+        }
+        CSSStyleSheet* parentStyleSheet;
+        unsigned parentIndex;
+        OwnPtr<RuleSet> ruleSet;
+
+    private:
+        RuleSubSet(CSSStyleSheet* sheet, unsigned index, PassOwnPtrWillBeRawPtr<RuleSet> rules)
+            : parentStyleSheet(sheet)
+            , parentIndex(index)
+            , ruleSet(rules)
+        {
+        }
+    };
+    typedef WillBeHeapVector<OwnPtrWillBeMember<RuleSubSet> > CSSStyleSheetRuleSubSet;
     void collectFeaturesFromRuleSubSet(CSSStyleSheetRuleSubSet*, RuleFeatureSet&);
 
     DocumentOrderedList m_scopingNodes;
