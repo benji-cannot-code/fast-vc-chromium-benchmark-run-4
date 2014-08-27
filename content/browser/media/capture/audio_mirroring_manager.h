ca981818ff73d235816f2d379339b2914a4ce7ac
miu <miu@chromium.org>
2014-08-27 16:03:37 -0700

    AudioMirroringManager becomes a global LazyInstance.
    While attempting to resolve a flaky browser_test, it became clear that
    AudioMirroringManager was not outliving its use.  In the original change
    that introduced this class, it was instantiated and owned by
    BrowserMainLoop as a matter of convenience.  However, with some
    debugging, it's clear that it must outlive objects that can outlive
    BrowserMainLoop (e.g., WebContentsAudioInputStream).
    
    Side notes: I've checked the feasibility of other solutions, confirming
    that the shutdown of AudioManager does NOT guarantee complete teardown
    of an AudioInputStream, so it's not sufficient to simply change the
    destruction order of the objects in BrowserMainLoop to resolve this
    problem.  As AudioMirroringManager provides a browser-wide service and
    owns no objects, it seems reasonable for it to exist as a global
    LazyInstance.
    
    BUG=396413
    
    Review URL: https://codereview.chromium.org/499483003
    
    Cr-Commit-Position: refs/heads/master@{#292258}

--- content/browser/media/capture/audio_mirroring_manager.h
+++ content/browser/media/capture/audio_mirroring_manager.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // AudioMirroringManager is a singleton object that maintains a set of active
 // audio mirroring destinations and auto-connects/disconnects audio streams
 // to/from those destinations.  It is meant to be used exclusively on the IO
-// BrowserThread.
+// thread.
 //
 // How it works:
 //
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/basictypes.h"
+#include "base/threading/thread_checker.h"
 #include "content/common/content_export.h"
 #include "media/audio/audio_source_diverter.h"
 
@@ -61,9 +62,12 @@ class CONTENT_EXPORT AudioMirroringManager {
     virtual ~MirroringDestination() {}
   };
 
+  // Note: Use GetInstance() for non-test code.
   AudioMirroringManager();
-
   virtual ~AudioMirroringManager();
+
+  // Returns the global instance.
+  static AudioMirroringManager* GetInstance();
 
   // Add/Remove a diverter for an audio stream with a known RenderView target
   // (represented by |render_process_id| + |render_view_id|).  Multiple
@@ -100,6 +104,9 @@ class CONTENT_EXPORT AudioMirroringManager {
 
   // Currently-active mirroring sessions.
   SessionMap sessions_;
+
+  // Used to check that all AudioMirroringManager code runs on the same thread.
+  base::ThreadChecker thread_checker_;
 
   DISALLOW_COPY_AND_ASSIGN(AudioMirroringManager);
 };
