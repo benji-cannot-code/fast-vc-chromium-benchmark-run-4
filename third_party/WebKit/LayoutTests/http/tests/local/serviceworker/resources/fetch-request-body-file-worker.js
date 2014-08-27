55bcfeae7427ba2491953b162c7fc9c975ea3bb8
horo@chromium.org <horo@chromium.org>
2014-08-27 08:10:40 +0000

    [ServiceWorker] Add test for FetchEvent's body with a local file.
    This test uses eventSender.beginDragWithFiles() in JS to upload the local file.
    So we have to locate the files under LayoutTests/http/tests/local/.
    
    BUG=402387
    
    Review URL: https://codereview.chromium.org/491203002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180945 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/local/serviceworker/resources/fetch-request-body-file-worker.js
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+self.addEventListener('fetch', function(event) {
+    event.respondWith(new Promise(function(resolve) {
+        var headers = [];
+        event.request.headers.forEach(function(value, key) {
+            headers.push([key, value]);
+          });
+        event.request.body.asText()
+          .then(function(result) {
+              resolve(new Response(JSON.stringify({
+                  method: event.request.method,
+                  headers: headers,
+                  body: result
+                })));
+            })
+      }));
+  });
