e8c0ac839df7dfd785145ccd9e80670018cb588e
dcheng <dcheng@chromium.org>
2014-08-26 23:58:55 -0700

    Remove implicit conversions from scoped_refptr to T* in content/browser/devtools/
    This patch was generated by running the rewrite_scoped_refptr clang tool
    on a Linux build.
    
    BUG=110610
    
    Review URL: https://codereview.chromium.org/502203002
    
    Cr-Commit-Position: refs/heads/master@{#292112}

--- content/browser/devtools/render_view_devtools_agent_host.cc
+++ content/browser/devtools/render_view_devtools_agent_host.cc
@@ -139,7 +139,7 @@ void RenderViewDevToolsAgentHost::DispatchProtocolMessage(
   scoped_refptr<DevToolsProtocol::Command> command =
       DevToolsProtocol::ParseCommand(message_dict.get(), &error_message);
 
-  if (command) {
+  if (command.get()) {
     scoped_refptr<DevToolsProtocol::Response> overridden_response;
 
     DevToolsManagerDelegate* delegate =
@@ -151,13 +151,13 @@ void RenderViewDevToolsAgentHost::DispatchProtocolMessage(
         overridden_response = DevToolsProtocol::ParseResponse(
             overridden_response_value.get());
     }
-    if (!overridden_response)
+    if (!overridden_response.get())
       overridden_response = overrides_handler_->HandleCommand(command);
-    if (!overridden_response)
+    if (!overridden_response.get())
       overridden_response = tracing_handler_->HandleCommand(command);
-    if (!overridden_response)
+    if (!overridden_response.get())
       overridden_response = power_handler_->HandleCommand(command);
-    if (overridden_response) {
+    if (overridden_response.get()) {
       if (!overridden_response->is_async_promise())
         OnDispatchOnInspectorFrontend(overridden_response->Serialize());
       return;
@@ -479,7 +479,7 @@ void RenderViewDevToolsAgentHost::OnDispatchOnInspectorFrontend(
   scoped_refptr<DevToolsProtocol::Notification> notification =
       DevToolsProtocol::ParseNotification(message);
 
-  if (notification) {
+  if (notification.get()) {
     tracing_handler_->HandleNotification(notification);
   }
   SendMessageToClient(message);
--- content/browser/devtools/renderer_overrides_handler.cc
+++ content/browser/devtools/renderer_overrides_handler.cc
@@ -193,14 +193,14 @@ void RendererOverridesHandler::OnSwapCompositorFrame(
   last_compositor_frame_metadata_ = frame_metadata;
   has_last_compositor_frame_metadata_ = true;
 
-  if (screencast_command_)
+  if (screencast_command_.get())
     InnerSwapCompositorFrame();
   if (color_picker_enabled_)
     UpdateColorPickerFrame();
 }
 
 void RendererOverridesHandler::OnVisibilityChanged(bool visible) {
-  if (!screencast_command_)
+  if (!screencast_command_.get())
     return;
   NotifyScreencastVisibility(visible);
 }
@@ -1171,7 +1171,7 @@ void RendererOverridesHandler::UpdateColorPickerCursor() {
 scoped_refptr<DevToolsProtocol::Response>
 RendererOverridesHandler::InputEmulateTouchFromMouseEvent(
     scoped_refptr<DevToolsProtocol::Command> command) {
-  if (!screencast_command_)
+  if (!screencast_command_.get())
     return command->InternalErrorResponse("Screencast should be turned on");
 
   base::DictionaryValue* params = command->params();
@@ -1300,7 +1300,7 @@ RendererOverridesHandler::InputEmulateTouchFromMouseEvent(
 void RendererOverridesHandler::UpdateTouchEventEmulationState() {
   if (!host_)
     return;
-  bool enabled = touch_emulation_enabled_ || screencast_command_;
+  bool enabled = touch_emulation_enabled_ || screencast_command_.get();
   host_->SetTouchEventEmulationEnabled(enabled);
   WebContentsImpl* web_contents = static_cast<WebContentsImpl*>(
       WebContents::FromRenderViewHost(host_));
