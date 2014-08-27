3edd7b13435ce4da0dff6b4db8c877d812ffa852
dbeam <dbeam@chromium.org>
2014-08-27 14:30:26 -0700

    Revert of Revert "Typecheck JS files for chrome://extensions" (patchset #1 of https://codereview.chromium.org/508283002/)
    Reason for revert:
    fixed check_licenses issues
    
    Original issue's description:
    > Revert "Typecheck JS files for chrome://extensions"
    >
    > This reverts commit 405253b7ee22ecb318d3c1ca9187e1e7259e704f.
    >
    > Caused a liscene failure on android bot:
    > http://build.chromium.org/p/chromium.linux/builders/Android%20Builder%20%28dbg%29/builds/64664/steps/check_licenses/logs/stdio
    >
    > Committed to pending queue: https://chromium.googlesource.com/chromium/src/+/87b3206
    
    TBR=vitalyp@chromium.org,eroman@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/512003002
    
    Cr-Commit-Position: refs/heads/master@{#292229}

--- ui/webui/resources/js/cr/ui/drag_wrapper.js
+++ ui/webui/resources/js/cr/ui/drag_wrapper.js
@@ -17,6 +17,7 @@ cr.define('cr.ui', function() {
    *   doDragLeave
    *   doDragOver
    *   doDrop
+   * @constructor
    */
   function DragWrapper(target, handler) {
     this.initialize(target, handler);
--- ui/webui/resources/js/cr/ui/list_item.js
+++ ui/webui/resources/js/cr/ui/list_item.js
@@ -7,7 +7,6 @@ cr.define('cr.ui', function() {
 
   /**
    * Creates a new list item element.
-   * @param {string} opt_label The text label for the item.
    * @constructor
    * @extends {HTMLLIElement}
    */
@@ -50,7 +49,6 @@ cr.define('cr.ui', function() {
   /**
    * Whether the item is selected. Setting this does not update the underlying
    * selection model. This is only used for display purpose.
-   * @type {boolean}
    */
   cr.defineProperty(ListItem, 'selected', cr.PropertyKind.BOOL_ATTR,
                     function() {
@@ -60,13 +58,12 @@ cr.define('cr.ui', function() {
   /**
    * Whether the item is the lead in a selection. Setting this does not update
    * the underlying selection model. This is only used for display purpose.
-   * @type {boolean}
    */
   cr.defineProperty(ListItem, 'lead', cr.PropertyKind.BOOL_ATTR);
 
   /**
    * This item's index in the containing list.
-   * @type {number}
+   * type {number}
    */
   cr.defineProperty(ListItem, 'listIndex');
 
