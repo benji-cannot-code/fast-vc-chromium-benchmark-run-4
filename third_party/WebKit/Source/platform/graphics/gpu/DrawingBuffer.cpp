b23289c7e6298ed8182b64feb7325cdd72b4b094
dongseong.hwang@intel.com <dongseong.hwang@intel.com>
2014-08-27 11:20:57 +0000

    Re-land "WebGL: Free temporary GPU resources held by inactive or hidden WebGL."
    r177854 reverted r177827, the given commit, because it seemed to cause gpu bots crash.
    However, r177833 actually caused the crash and r177827 was innocent.
    
    > WebGL: Free temporary GPU resources held by inactive or hidden WebGL.
    >
    > This CL applies Canvas 2D change in r165966 to WebGL
    >
    > TEST=fast/canvas/webgl/webgl-composite-modes-tabswitching.html,
    > DrawingBufferTest.verifySetIsHiddenProperlyAffectsMailboxes
    >
    > BUG=391400
    >
    > Review URL: https://codereview.chromium.org/251023004
    >
    > git-svn-id: svn://svn.chromium.org/blink/trunk@177827 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    BUG=391400
    
    Review URL: https://codereview.chromium.org/450223002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180964 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/gpu/DrawingBuffer.cpp
+++ third_party/WebKit/Source/platform/graphics/gpu/DrawingBuffer.cpp
@@ -149,6 +149,7 @@ DrawingBuffer::DrawingBuffer(PassOwnPtr<WebGraphicsContext3D> context,
     , m_sampleCount(0)
     , m_packAlignment(4)
     , m_destructionInProgress(false)
+    , m_isHidden(false)
     , m_contextEvictionManager(contextEvictionManager)
 {
     // Used by browser tests to detect the use of a DrawingBuffer.
@@ -191,8 +192,26 @@ WebGraphicsContext3D* DrawingBuffer::context()
     return m_context.get();
 }
 
+void DrawingBuffer::setIsHidden(bool hidden)
+{
+    if (m_isHidden == hidden)
+        return;
+    m_isHidden = hidden;
+    if (m_isHidden)
+        freeRecycledMailboxes();
+}
+
+void DrawingBuffer::freeRecycledMailboxes()
+{
+    if (m_recycledMailboxQueue.isEmpty())
+        return;
+    while (!m_recycledMailboxQueue.isEmpty())
+        deleteMailbox(m_recycledMailboxQueue.takeLast());
+}
+
 bool DrawingBuffer::prepareMailbox(WebExternalTextureMailbox* outMailbox, WebExternalBitmap* bitmap)
 {
+    ASSERT(!m_isHidden);
     if (!m_contentsChanged)
         return false;
 
@@ -278,8 +297,8 @@ bool DrawingBuffer::prepareMailbox(WebExternalTextureMailbox* outMailbox, WebExt
 
 void DrawingBuffer::mailboxReleased(const WebExternalTextureMailbox& mailbox, bool lostResource)
 {
-    if (m_destructionInProgress || m_context->isContextLost() || lostResource) {
-        mailboxReleasedWhileDestructionInProgress(mailbox);
+    if (m_destructionInProgress || m_context->isContextLost() || lostResource || m_isHidden) {
+        mailboxReleasedWithoutRecycling(mailbox);
         return;
     }
 
@@ -296,7 +315,7 @@ void DrawingBuffer::mailboxReleased(const WebExternalTextureMailbox& mailbox, bo
     ASSERT_NOT_REACHED();
 }
 
-void DrawingBuffer::mailboxReleasedWhileDestructionInProgress(const WebExternalTextureMailbox& mailbox)
+void DrawingBuffer::mailboxReleasedWithoutRecycling(const WebExternalTextureMailbox& mailbox)
 {
     ASSERT(m_textureMailboxes.size());
     m_context->makeContextCurrent();
--- third_party/WebKit/Source/platform/graphics/gpu/DrawingBuffer.h
+++ third_party/WebKit/Source/platform/graphics/gpu/DrawingBuffer.h
@@ -141,6 +141,7 @@ public:
     void markContentsChanged();
     void markLayerComposited();
     bool layerComposited() const;
+    void setIsHidden(bool);
 
     WebLayer* platformLayer();
     void paintCompositedResultsToCanvas(ImageBuffer*);
@@ -177,7 +178,7 @@ protected: // For unittests
     bool initialize(const IntSize&);
 
 private:
-    void mailboxReleasedWhileDestructionInProgress(const WebExternalTextureMailbox&);
+    void mailboxReleasedWithoutRecycling(const WebExternalTextureMailbox&);
 
     unsigned createColorTexture();
     // Create the depth/stencil and multisample buffers, if needed.
@@ -194,6 +195,7 @@ private:
     PassRefPtr<MailboxInfo> recycledMailbox();
     PassRefPtr<MailboxInfo> createNewMailbox(const TextureInfo&);
     void deleteMailbox(const WebExternalTextureMailbox&);
+    void freeRecycledMailboxes();
 
     // Updates the current size of the buffer, ensuring that s_currentResourceUsePixels is updated.
     void setSize(const IntSize& size);
@@ -278,6 +280,7 @@ private:
     int m_sampleCount;
     int m_packAlignment;
     bool m_destructionInProgress;
+    bool m_isHidden;
 
     OwnPtr<WebExternalTextureLayer> m_layer;
 
