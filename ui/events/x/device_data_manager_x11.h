875b0a6162d9bf674436ff0d22762190d2f5aa5c
sky <sky@chromium.org>
2014-08-27 19:52:44 -0700

    Remove include of ui/events/event.h from device_data_manager
    device_data_manager shouldn't include event.h as they are in different
    targets and we don't want event_base depending upon event.
    
    BUG=none
    TEST=none
    R=sadrul@chromium.org
    
    Review URL: https://codereview.chromium.org/512553003
    
    Cr-Commit-Position: refs/heads/master@{#292301}

--- ui/events/x/device_data_manager_x11.h
+++ ui/events/x/device_data_manager_x11.h
@@ -24,9 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/event_types.h"
 #include "base/memory/scoped_ptr.h"
 #include "ui/events/device_data_manager.h"
-#include "ui/events/event.h"
 #include "ui/events/event_constants.h"
 #include "ui/events/events_base_export.h"
+#include "ui/events/keycodes/keyboard_codes.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/x/x11_atom_cache.h"
 
--- ui/events/x/events_x.cc
+++ ui/events/x/events_x.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/memory/singleton.h"
+#include "ui/events/event.h"
 #include "ui/events/event_utils.h"
 #include "ui/events/keycodes/keyboard_code_conversion_x.h"
 #include "ui/events/x/device_data_manager_x11.h"
