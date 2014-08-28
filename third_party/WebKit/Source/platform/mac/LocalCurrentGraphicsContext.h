12e4b951572f8ffb51d0cf6a13538fe8e1afb8d3
sungmann.cho@navercorp.com <sungmann.cho@navercorp.com>
2014-08-28 03:07:36 +0000

    Fix a build error on the Mac OS with the case-sensitive file system.
    On Mac OS with HFS+, ToT currently breaks at:
    In file included from ../../third_party/WebKit/Source/platform/mac/LocalCurrentGraphicsContext.mm:21:
    ../../third_party/WebKit/Source/platform/mac/LocalCurrentGraphicsContext.h:20:10: fatal error: 'platform/Geometry/IntRect.h' file not found
    
    ".../Geometry/..." should be changed to lowercase.
    
    Review URL: https://codereview.chromium.org/513013002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181008 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/mac/LocalCurrentGraphicsContext.h
+++ third_party/WebKit/Source/platform/mac/LocalCurrentGraphicsContext.h
@@ -18,8 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Boston, MA 02110-1301, USA.
  */
 
-#include "platform/Geometry/IntRect.h"
 #include "platform/PlatformExport.h"
+#include "platform/geometry/IntRect.h"
 #include "skia/ext/skia_utils_mac.h"
 #include "wtf/Noncopyable.h"
 
