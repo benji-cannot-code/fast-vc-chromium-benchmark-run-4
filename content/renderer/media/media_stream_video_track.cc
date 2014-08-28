9426e1e444af5d695a3ebf1eee94c43646c52955
dcheng <dcheng@chromium.org>
2014-08-28 15:55:14 -0700

    Manual fixups in media code for scoped_refptr operator T* removal.
    BUG=110610
    
    Review URL: https://codereview.chromium.org/515093003
    
    Cr-Commit-Position: refs/heads/master@{#292487}

--- content/renderer/media/media_stream_video_track.cc
+++ content/renderer/media/media_stream_video_track.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
+#include "third_party/libjingle/source/talk/app/webrtc/mediastreaminterface.h"
 
 namespace content {
 
--- content/renderer/media/video_frame_compositor_unittest.cc
+++ content/renderer/media/video_frame_compositor_unittest.cc
@@ -70,7 +70,7 @@ class VideoFrameCompositorTest : public testing::Test,
 };
 
 TEST_F(VideoFrameCompositorTest, InitialValues) {
-  EXPECT_FALSE(compositor()->GetCurrentFrame());
+  EXPECT_FALSE(compositor()->GetCurrentFrame().get());
 }
 
 TEST_F(VideoFrameCompositorTest, UpdateCurrentFrame) {
