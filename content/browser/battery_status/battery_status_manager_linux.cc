026edbddf8daf78240cc6d3162c8c6eceef6134c
timvolodine <timvolodine@chromium.org>
2014-08-28 09:39:32 -0700

    Battery Status API: add UMA logging for Linux.
    Adds UMA logging to track how many batteries are reported
    by the system at the start of Battery Status API.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/480113003
    
    Cr-Commit-Position: refs/heads/master@{#292398}

--- content/browser/battery_status/battery_status_manager_linux.cc
+++ content/browser/battery_status/battery_status_manager_linux.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/battery_status/battery_status_manager_linux.h"
 
 #include "base/macros.h"
+#include "base/metrics/histogram.h"
 #include "base/threading/thread.h"
 #include "base/values.h"
 #include "content/browser/battery_status/battery_status_manager.h"
@@ -97,6 +98,11 @@ scoped_ptr<PathsVector> GetPowerSourcesPaths(dbus::ObjectProxy* proxy) {
   return paths.Pass();;
 }
 
+void UpdateNumberBatteriesHistogram(int count) {
+  UMA_HISTOGRAM_CUSTOM_COUNTS(
+      "BatteryStatus.NumberBatteriesLinux", count, 1, 5, 6);
+}
+
 // Class that represents a dedicated thread which communicates with DBus to
 // obtain battery information and receives battery change notifications.
 class BatteryStatusNotificationThread : public base::Thread {
@@ -132,6 +138,7 @@ class BatteryStatusNotificationThread : public base::Thread {
         system_bus_->GetObjectProxy(kUPowerServiceName,
                                     dbus::ObjectPath(kUPowerPath));
     scoped_ptr<PathsVector> device_paths = GetPowerSourcesPaths(power_proxy);
+    int num_batteries = 0;
 
     for (size_t i = 0; i < device_paths->size(); ++i) {
       const dbus::ObjectPath& device_path = device_paths->at(i);
@@ -158,13 +165,15 @@ class BatteryStatusNotificationThread : public base::Thread {
         // TODO(timvolodine): add support for multiple batteries. Currently we
         // only collect information from the first battery we encounter
         // (crbug.com/400780).
-        // TODO(timvolodine): add UMA logging for this case.
         LOG(WARNING) << "multiple batteries found, "
                      << "using status data of the first battery only.";
       } else {
         battery_proxy_ = device_proxy;
       }
+      num_batteries++;
     }
+
+    UpdateNumberBatteriesHistogram(num_batteries);
 
     if (!battery_proxy_) {
       callback_.Run(blink::WebBatteryStatus());
