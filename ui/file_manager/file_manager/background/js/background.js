12e29ac7637e7f90188c81414bbe3f8e2d7d43e1
iseki <iseki@chromium.org>
2014-08-28 01:20:18 -0700

    Expand minimum size of the window to avoid cut off context menu
    BUG=259656
    TEST=manually
    
    Review URL: https://codereview.chromium.org/510713002
    
    Cr-Commit-Position: refs/heads/master@{#292348}

--- ui/file_manager/file_manager/background/js/background.js
+++ ui/file_manager/file_manager/background/js/background.js
@@ -615,7 +615,7 @@ var FILE_MANAGER_WINDOW_CREATE_OPTIONS = Object.freeze({
     height: Math.round(window.screen.availHeight * 0.8)
   }),
   minWidth: 480,
-  minHeight: 240,
+  minHeight: 300,
   hidden: true
 });
 
