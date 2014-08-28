56da3a0ca4319c5644974a9c5f37d800de9f2544
dcheng <dcheng@chromium.org>
2014-08-28 11:56:33 -0700

    Manual fixups in compositor code scoped_refptr operator T* removal.
    BUG=110610
    
    Review URL: https://codereview.chromium.org/514123002
    
    Cr-Commit-Position: refs/heads/master@{#292432}

--- cc/resources/tile_manager.h
+++ cc/resources/tile_manager.h
@@ -160,7 +160,7 @@ class CC_EXPORT TileManager : public RasterizerClient,
 
  protected:
   TileManager(TileManagerClient* client,
-              base::SequencedTaskRunner* task_runner,
+              const scoped_refptr<base::SequencedTaskRunner>& task_runner,
               ResourcePool* resource_pool,
               Rasterizer* rasterizer,
               RenderingStatsInstrumentation* rendering_stats_instrumentation);
