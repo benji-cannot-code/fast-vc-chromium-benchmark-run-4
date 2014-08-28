89a9580a818fcd3e915d4d815ac5e779428e9f84
fsamuel <fsamuel@chromium.org>
2014-08-28 09:01:48 -0700

    ExtensionOptionsGuest: Fixed spacing
    BUG=none
    TBR=fsamuel@chromium.org
    
    Review URL: https://codereview.chromium.org/517593002
    
    Cr-Commit-Position: refs/heads/master@{#292393}

--- chrome/browser/guest_view/extension_options/extension_options_guest.cc
+++ chrome/browser/guest_view/extension_options/extension_options_guest.cc
@@ -133,7 +133,7 @@ const char* ExtensionOptionsGuest::GetAPINamespace() const {
 }
 
 int ExtensionOptionsGuest::GetTaskPrefix() const {
- return IDS_EXTENSION_TASK_MANAGER_EXTENSIONOPTIONS_TAG_PREFIX;
+  return IDS_EXTENSION_TASK_MANAGER_EXTENSIONOPTIONS_TAG_PREFIX;
 }
 
 content::WebContents* ExtensionOptionsGuest::GetAssociatedWebContents() const {
