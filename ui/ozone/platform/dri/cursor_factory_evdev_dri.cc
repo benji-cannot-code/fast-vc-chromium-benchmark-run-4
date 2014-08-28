e07049bb56a8a509961a14d9505f2689c97f9c67
spang <spang@chromium.org>
2014-08-27 17:50:49 -0700

    ozone: evdev: gestures: Disable click when the cursor is hidden
    This is hacky, and a stopgap while we wait on property plumbing.
    
    BUG=none
    TEST=manual on link_freon
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/514943003
    
    Cr-Commit-Position: refs/heads/master@{#292289}

--- ui/ozone/platform/dri/cursor_factory_evdev_dri.cc
+++ ui/ozone/platform/dri/cursor_factory_evdev_dri.cc
@@ -59,6 +59,8 @@ gfx::AcceleratedWidget CursorFactoryEvdevDri::GetCursorWindow() {
   return cursor_window_;
 }
 
+bool CursorFactoryEvdevDri::IsCursorVisible() { return cursor_; }
+
 gfx::PointF CursorFactoryEvdevDri::location() { return cursor_location_; }
 
 gfx::Point CursorFactoryEvdevDri::bitmap_location() {
