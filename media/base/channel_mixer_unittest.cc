f4b2ca6fb52fb59d3f912125eef0623a6013997b
xians <xians@chromium.org>
2014-08-28 14:10:15 -0700

    Revert of Revert of Remove the last piece of deprecated synchronous IO code. (patchset #1 of https://codereview.chromium.org/509893002/)
    Reason for revert:
    https://codereview.chromium.org/510893003/ fixed the ledger bot, so I am going to revert this revert CL.
    
    Original issue's description:
    > Revert of Remove the last piece of deprecated synchronous IO code. (patchset #5 of https://codereview.chromium.org/460373002/)
    >
    > Reason for revert:
    > Build failure on Google Chrome ChromeOS:
    >
    > http://build.chromium.org/p/chromium.chrome/buildstatus?builder=Google%20Chrome%20ChromeOS&number=71164
    >
    > Original issue's description:
    > > Remove the last piece of deprecated synchronous IO code.
    > >
    > > BUG=337096
    > > TEST=bots
    > >
    > > Committed: https://chromium.googlesource.com/chromium/src/+/c4a64ffdd0d511c66d774341fa0a318af7911193
    >
    > TBR=dalecurtis@chromium.org,ajm@chromium.org,rkc@chromium.org,avi@chromium.org,palmer@chromium.org,xians@chromium.org
    > NOTREECHECKS=true
    > BUG=337096
    >
    > Committed: https://chromium.googlesource.com/chromium/src/+/3e8c19c89c16fe8a7c2dfe0cae2990cd4a004339
    
    TBR=dalecurtis@chromium.org,ajm@chromium.org,rkc@chromium.org,avi@chromium.org,palmer@chromium.org,tnagel@chromium.org
    NOTREECHECKS=true
    BUG=337096
    
    Review URL: https://codereview.chromium.org/518433002
    
    Cr-Commit-Position: refs/heads/master@{#292463}

--- media/base/channel_mixer_unittest.cc
+++ media/base/channel_mixer_unittest.cc
@@ -108,7 +108,6 @@ TEST_P(ChannelMixerTest, Mixing) {
                               input_layout == CHANNEL_LAYOUT_DISCRETE ?
                                   input_channels :
                                   ChannelLayoutToChannelCount(input_layout),
-                              0,
                               AudioParameters::kAudioCDSampleRate, 16,
                               kFrames,
                               AudioParameters::NO_EFFECTS);
@@ -119,9 +118,8 @@ TEST_P(ChannelMixerTest, Mixing) {
   AudioParameters output_audio(AudioParameters::AUDIO_PCM_LINEAR,
                                output_layout,
                                output_layout == CHANNEL_LAYOUT_DISCRETE ?
-                                  output_channels :
-                                  ChannelLayoutToChannelCount(output_layout),
-                               0,
+                                   output_channels :
+                                   ChannelLayoutToChannelCount(output_layout),
                                AudioParameters::kAudioCDSampleRate, 16,
                                kFrames,
                                AudioParameters::NO_EFFECTS);
