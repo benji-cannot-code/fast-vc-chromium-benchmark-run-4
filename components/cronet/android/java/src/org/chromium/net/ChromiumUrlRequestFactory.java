27bf357d14d03d28ee0e9b78776d6957f421249a
aurimas <aurimas@chromium.org>
2014-08-28 09:15:22 -0700

    Introduce @CalledByReflection annotation.
    BUG=163393
    
    Review URL: https://codereview.chromium.org/509753002
    
    Cr-Commit-Position: refs/heads/master@{#292394}

--- components/cronet/android/java/src/org/chromium/net/ChromiumUrlRequestFactory.java
+++ components/cronet/android/java/src/org/chromium/net/ChromiumUrlRequestFactory.java
@@ -8,6 +8,8 @@ package org.chromium.net;
 import android.content.Context;
 import android.os.Build;
 
+import org.chromium.base.UsedByReflection;
+
 import java.nio.channels.WritableByteChannel;
 import java.util.Map;
 
