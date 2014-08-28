362cd009db30ea5fc529997271cd649200e34704
jmadill <jmadill@chromium.org>
2014-08-28 11:19:25 -0700

    Fix broken path in GLES2 CTS.
    AppendSwitch was casting all directories to lower case, causing the tests
    to early-out and return as always passed even before they create the test
    window.
    
    BUG=408251
    
    Review URL: https://codereview.chromium.org/500133005
    
    Cr-Commit-Position: refs/heads/master@{#292413}

--- gpu/gles2_conform_support/gles2_conform_test.cc
+++ gpu/gles2_conform_support/gles2_conform_test.cc
@@ -65,7 +65,7 @@ bool RunGLES2ConformTest(const char* path) {
       "gles2_conform_test_windowless")));
 
   CommandLine cmdline(program);
-  cmdline.AppendSwitch(std::string("-run=") + path);
+  cmdline.AppendArg(std::string("-run=") + path);
 
   std::string output;
   bool success = base::GetAppOutput(cmdline, &output);
