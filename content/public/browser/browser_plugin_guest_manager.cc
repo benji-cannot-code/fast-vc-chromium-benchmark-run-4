be2537805fb819f613c7283c258c977ad106dd2d
fsamuel <fsamuel@chromium.org>
2014-08-27 21:25:46 -0700

    Cleanup BrowserPluginGuestManager
    Remove some unnecessary includes, typedefs, and forward declarations.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/490923006
    
    Cr-Commit-Position: refs/heads/master@{#292318}

--- content/public/browser/browser_plugin_guest_manager.cc
+++ content/public/browser/browser_plugin_guest_manager.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/public/browser/browser_plugin_guest_manager.h"
 
-#include "base/values.h"
-
 namespace content {
 
 WebContents* BrowserPluginGuestManager::GetGuestByInstanceID(
--- content/public/browser/browser_plugin_guest_manager.h
+++ content/public/browser/browser_plugin_guest_manager.h
@@ -6,20 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_PUBLIC_BROWSER_BROWSER_PLUGIN_GUEST_MANAGER_H_
 #define CONTENT_PUBLIC_BROWSER_BROWSER_PLUGIN_GUEST_MANAGER_H_
 
-#include <string>
-
 #include "base/callback.h"
 #include "content/common/content_export.h"
 
-class GURL;
-
-namespace base {
-class DictionaryValue;
-}  // namespace base
-
 namespace content {
 
-class SiteInstance;
 class WebContents;
 
 // A BrowserPluginGuestManager offloads guest management and routing
@@ -28,7 +19,6 @@ class CONTENT_EXPORT BrowserPluginGuestManager {
  public:
   virtual ~BrowserPluginGuestManager() {}
 
-  typedef base::Callback<void(WebContents*)> GuestByInstanceIDCallback;
   // Requests a guest WebContents associated with the provided
   // |browser_plugin_instance_id|.
   // Returns the guest associated with the provided ID if one exists.
