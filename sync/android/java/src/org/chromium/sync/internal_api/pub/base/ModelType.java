1cd76f10711b58a066c3186a628f2067667b7b30
bauerb <bauerb@chromium.org>
2014-08-28 13:30:44 -0700

    Use name() instead of toString() and fix comparison order in ModelType.
    BUG=none
    
    Review URL: https://codereview.chromium.org/511253002
    
    Cr-Commit-Position: refs/heads/master@{#292451}

--- sync/android/java/src/org/chromium/sync/internal_api/pub/base/ModelType.java
+++ sync/android/java/src/org/chromium/sync/internal_api/pub/base/ModelType.java
@@ -91,7 +91,7 @@ public enum ModelType {
     private final boolean mNonInvalidationType;
 
     ModelType(String modelType, boolean nonInvalidationType) {
-        assert nonInvalidationType || modelType.equals(toString());
+        assert nonInvalidationType || name().equals(modelType);
         mModelType = modelType;
         mNonInvalidationType = nonInvalidationType;
     }
