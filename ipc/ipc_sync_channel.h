5a87e835eef9398ae2d6f97df14b430827ec0073
dcheng <dcheng@chromium.org>
2014-08-28 09:59:29 -0700

    Manual fixups for scoped_refptr operator T* removal in ipc/
    BUG=110610
    
    Review URL: https://codereview.chromium.org/512153002
    
    Cr-Commit-Position: refs/heads/master@{#292404}

--- ipc/ipc_sync_channel.h
+++ ipc/ipc_sync_channel.h
@@ -73,14 +73,14 @@ class IPC_EXPORT SyncChannel : public ChannelProxy {
       const IPC::ChannelHandle& channel_handle,
       IPC::Channel::Mode mode,
       Listener* listener,
-      base::SingleThreadTaskRunner* ipc_task_runner,
+      const scoped_refptr<base::SingleThreadTaskRunner>& ipc_task_runner,
       bool create_pipe_now,
       base::WaitableEvent* shutdown_event);
 
   static scoped_ptr<SyncChannel> Create(
       scoped_ptr<ChannelFactory> factory,
       Listener* listener,
-      base::SingleThreadTaskRunner* ipc_task_runner,
+      const scoped_refptr<base::SingleThreadTaskRunner>& ipc_task_runner,
       bool create_pipe_now,
       base::WaitableEvent* shutdown_event);
 
@@ -89,7 +89,7 @@ class IPC_EXPORT SyncChannel : public ChannelProxy {
   // added before any messages are sent or received.
   static scoped_ptr<SyncChannel> Create(
       Listener* listener,
-      base::SingleThreadTaskRunner* ipc_task_runner,
+      const scoped_refptr<base::SingleThreadTaskRunner>& ipc_task_runner,
       base::WaitableEvent* shutdown_event);
 
   virtual ~SyncChannel();
@@ -122,9 +122,10 @@ class IPC_EXPORT SyncChannel : public ChannelProxy {
   // ChannelProxy::Context for more information.
   class SyncContext : public Context {
    public:
-    SyncContext(Listener* listener,
-                base::SingleThreadTaskRunner* ipc_task_runner,
-                base::WaitableEvent* shutdown_event);
+    SyncContext(
+        Listener* listener,
+        const scoped_refptr<base::SingleThreadTaskRunner>& ipc_task_runner,
+        base::WaitableEvent* shutdown_event);
 
     // Adds information about an outgoing sync message to the context so that
     // we know how to deserialize the reply.
@@ -200,9 +201,10 @@ class IPC_EXPORT SyncChannel : public ChannelProxy {
   };
 
  private:
-  SyncChannel(Listener* listener,
-              base::SingleThreadTaskRunner* ipc_task_runner,
-              base::WaitableEvent* shutdown_event);
+  SyncChannel(
+      Listener* listener,
+      const scoped_refptr<base::SingleThreadTaskRunner>& ipc_task_runner,
+      base::WaitableEvent* shutdown_event);
 
   void OnWaitableEventSignaled(base::WaitableEvent* arg);
 
