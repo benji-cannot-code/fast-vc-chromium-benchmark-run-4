4374660c6fb13728ae265c6c7ab66d1868c543a3
Jun Mukai <mukai@chromium.org>
2014-08-28 10:10:15 -0700

    Fix resources grit paths.
    BUG=None
    R=oshima@chromium.org
    TBR=oshima@chromium.org
    TEST=compile succeeds
    
    Review URL: https://codereview.chromium.org/508283003
    
    Cr-Commit-Position: refs/heads/master@{#292409}

--- athena/main/placeholder.cc
+++ athena/main/placeholder.cc
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "athena/activity/public/activity_factory.h"
 #include "athena/activity/public/activity_manager.h"
+#include "athena/resources/grit/athena_resources.h"
 #include "athena/screen/public/screen_manager.h"
-#include "grit/athena_resources.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/image/image_skia.h"
 
