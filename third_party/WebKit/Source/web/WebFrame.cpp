1c060785d3b347820d0d0f088d527e7795cec674
zeeshanq@chromium.org <zeeshanq@chromium.org>
2014-08-29 05:01:17 +0000

    A recent CL to add reader mode for clank started triggering this codepath on load and was interfering with how the virtual keyboard handles programmatic focus() calls. This rogue UGI seems to have been added to get pyauto tests to pass, we should be able to only set this for tests.
    This is part 1 of 3, will be followed by:
    https://codereview.chromium.org/518583003/
    
    BUG=408426,407643
    
    Review URL: https://codereview.chromium.org/516753002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181075 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebFrame.cpp
+++ third_party/WebKit/Source/web/WebFrame.cpp
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/frame/RemoteFrame.h"
 #include "core/html/HTMLFrameOwnerElement.h"
+#include "platform/UserGestureIndicator.h"
 #include "web/OpenedFrameTracker.h"
 #include "web/WebLocalFrameImpl.h"
 #include "web/WebRemoteFrameImpl.h"
@@ -74,6 +75,16 @@ void WebFrame::swap(WebFrame* frame)
     } else {
         toWebRemoteFrameImpl(frame)->initializeCoreFrame(oldFrame->host(), owner, oldFrame->tree().name());
     }
+}
+
+v8::Handle<v8::Value> WebFrame::executeScriptAndReturnValueForTests(const WebScriptSource& source)
+{
+    // FIXME: This fake UserGestureIndicator is required for a bunch of browser
+    // tests to pass. We should update the tests to simulate input and get rid
+    // of this.
+    // http://code.google.com/p/chromium/issues/detail?id=86397
+    UserGestureIndicator gestureIndicator(DefinitelyProcessingNewUserGesture);
+    return executeScriptAndReturnValue(source);
 }
 
 WebFrame* WebFrame::opener() const
