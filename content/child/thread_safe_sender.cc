f506e6af940e5edee7ec362a0a09d42bc83e946f
dcheng <dcheng@chromium.org>
2014-08-28 18:18:40 -0700

    Manual fixups in content/child for scoped_refptr operator T* removal.
    BUG=110610
    
    Review URL: https://codereview.chromium.org/511183002
    
    Cr-Commit-Position: refs/heads/master@{#292533}

--- content/child/thread_safe_sender.cc
+++ content/child/thread_safe_sender.cc
@@ -11,8 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-ThreadSafeSender::ThreadSafeSender(base::MessageLoopProxy* main_loop,
-                                   IPC::SyncMessageFilter* sync_filter)
+ThreadSafeSender::ThreadSafeSender(
+    const scoped_refptr<base::MessageLoopProxy>& main_loop,
+    const scoped_refptr<IPC::SyncMessageFilter>& sync_filter)
     : main_loop_(main_loop), sync_filter_(sync_filter) {
 }
 
--- content/child/webmessageportchannel_impl.cc
+++ content/child/webmessageportchannel_impl.cc
@@ -21,7 +21,7 @@ using blink::WebString;
 namespace content {
 
 WebMessagePortChannelImpl::WebMessagePortChannelImpl(
-    base::MessageLoopProxy* child_thread_loop)
+    const scoped_refptr<base::MessageLoopProxy>& child_thread_loop)
     : client_(NULL),
       route_id_(MSG_ROUTING_NONE),
       message_port_id_(MSG_ROUTING_NONE),
@@ -33,7 +33,7 @@ WebMessagePortChannelImpl::WebMessagePortChannelImpl(
 WebMessagePortChannelImpl::WebMessagePortChannelImpl(
     int route_id,
     int message_port_id,
-    base::MessageLoopProxy* child_thread_loop)
+    const scoped_refptr<base::MessageLoopProxy>& child_thread_loop)
     : client_(NULL),
       route_id_(route_id),
       message_port_id_(message_port_id),
@@ -62,7 +62,7 @@ WebMessagePortChannelImpl::~WebMessagePortChannelImpl() {
 
 // static
 void WebMessagePortChannelImpl::CreatePair(
-    base::MessageLoopProxy* child_thread_loop,
+    const scoped_refptr<base::MessageLoopProxy>& child_thread_loop,
     blink::WebMessagePortChannel** channel1,
     blink::WebMessagePortChannel** channel2) {
   WebMessagePortChannelImpl* impl1 =
