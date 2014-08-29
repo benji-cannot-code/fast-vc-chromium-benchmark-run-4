96bc6bbd429cf6df846a897ce92562e3e0ed620a
haraken@chromium.org <haraken@chromium.org>
2014-08-29 09:58:07 +0000

    Implement DOMWrapperWorld::DOMObjectHolder
    We sometimes need to keep a DOM object X alive while a V8 object Y is alive.
    If Y is a wrapper of X and we want to create only one wrapper for X,
    we can just use DOMDataStore. However, in some rare cases this condition
    doesn't hold, and in that case we need a separate mechanism.
    
    InjectedScriptManager is one example, and it implements manual lifetime
    management using InjectedScriptManager::CallbackData.
    
    This CL generalizes the mechanism and move it to DOMWrapperWorld::DOMObjectHolder.
    This is because I want to use the mechanism for ThenFunctions in serviceworkers/.
    
    The semantics of DOMWrapperWorld::DOMObjectHolder is as follows:
    
    - By calling DOMObjectHolder::create(T* object, Handle<Object> wrapper),
    you can create a holder that keeps the |object| alive as long as
    (1) the |wrapper| is alive or (2) the DOMWrapperWorld which the |wrapper| belongs to
    is disposed.
    
    - By calling DOMWrapperWorld::registerDOMObjectHolder(DOMObjectHolderBase* holder),
    you can ask the DOMWrapperWorld to manage the lifetime of the holder.
    
    BUG=340522
    
    Review URL: https://codereview.chromium.org/512703007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181085 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/core/v8/custom/V8InjectedScriptManager.cpp
+++ third_party/WebKit/Source/bindings/core/v8/custom/V8InjectedScriptManager.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/inspector/InjectedScriptManager.h"
 
 #include "bindings/core/v8/BindingSecurity.h"
+#include "bindings/core/v8/DOMWrapperWorld.h"
 #include "bindings/core/v8/ScopedPersistent.h"
 #include "bindings/core/v8/ScriptDebugServer.h"
 #include "bindings/core/v8/ScriptValue.h"
@@ -47,21 +48,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-InjectedScriptManager::CallbackData* InjectedScriptManager::createCallbackData(InjectedScriptManager* injectedScriptManager)
-{
-    OwnPtr<InjectedScriptManager::CallbackData> callbackData = adoptPtr(new InjectedScriptManager::CallbackData());
-    InjectedScriptManager::CallbackData* callbackDataPtr = callbackData.get();
-    callbackData->injectedScriptManager = injectedScriptManager;
-    m_callbackDataSet.add(callbackData.release());
-    return callbackDataPtr;
-}
-
-void InjectedScriptManager::removeCallbackData(InjectedScriptManager::CallbackData* callbackData)
-{
-    ASSERT(m_callbackDataSet.contains(callbackData));
-    m_callbackDataSet.remove(callbackData);
-}
-
 static v8::Local<v8::Object> createInjectedScriptHostV8Wrapper(PassRefPtrWillBeRawPtr<InjectedScriptHost> host, InjectedScriptManager* injectedScriptManager, v8::Handle<v8::Object> creationContext, v8::Isolate* isolate)
 {
     ASSERT(host);
@@ -70,12 +56,7 @@ static v8::Local<v8::Object> createInjectedScriptHostV8Wrapper(PassRefPtrWillBeR
     if (UNLIKELY(wrapper.IsEmpty()))
         return wrapper;
 
-    // Create a weak reference to the v8 wrapper of InspectorBackend to deref
-    // InspectorBackend when the wrapper is garbage collected.
-    InjectedScriptManager::CallbackData* callbackData = injectedScriptManager->createCallbackData(injectedScriptManager);
-    callbackData->host = host.get();
-    callbackData->handle.set(isolate, wrapper);
-    callbackData->handle.setWeak(callbackData, &InjectedScriptManager::setWeakCallback);
+    DOMWrapperWorld::current(isolate).registerDOMObjectHolder(isolate, host.get(), wrapper);
 
 #if ENABLE(OILPAN)
     V8DOMWrapper::setNativeInfoWithPersistentHandle(wrapper, &V8InjectedScriptHost::wrapperTypeInfo, V8InjectedScriptHost::toInternalPointer(host), &callbackData->host);
@@ -126,12 +107,6 @@ bool InjectedScriptManager::canAccessInspectedWindow(ScriptState* scriptState)
     LocalFrame* frame = V8Window::toNative(holder)->frame();
 
     return BindingSecurity::shouldAllowAccessToFrame(scriptState->isolate(), frame, DoNotReportSecurityError);
-}
-
-void InjectedScriptManager::setWeakCallback(const v8::WeakCallbackData<v8::Object, InjectedScriptManager::CallbackData>& data)
-{
-    InjectedScriptManager::CallbackData* callbackData = data.GetParameter();
-    callbackData->injectedScriptManager->removeCallbackData(callbackData);
 }
 
 } // namespace blink
