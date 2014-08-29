adc625b609f811c04a5173cc72ebddc26717d563
hush <hush@chromium.org>
2014-08-28 18:34:26 -0700

    Re-enalbe "testSetGetBackgroundColor"
    Background color issue should already be fixed by
    crbug.com/391462
    
    BUG=384559
    
    Review URL: https://codereview.chromium.org/517153002
    
    Cr-Commit-Position: refs/heads/master@{#292540}

--- android_webview/javatests/src/org/chromium/android_webview/test/AwContentsRenderTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwContentsRenderTest.java
@@ -11,7 +11,6 @@ import android.test.suitebuilder.annotation.SmallTest;
 
 import org.chromium.android_webview.AwContents;
 import org.chromium.base.ThreadUtils;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 
 import java.util.concurrent.Callable;
@@ -69,9 +68,7 @@ public class AwContentsRenderTest extends AwTestBase {
     /*
     @SmallTest
     @Feature({"AndroidWebView"})
-    crbug.com/384559
     */
-    @DisabledTest
     public void testSetGetBackgroundColor() throws Throwable {
         setBackgroundColorOnUiThread(Color.MAGENTA);
         pollForBackgroundColor(Color.MAGENTA);
@@ -87,7 +84,7 @@ public class AwContentsRenderTest extends AwTestBase {
 
         loadUrlSync(mAwContents, mContentsClient.getOnPageFinishedHelper(),
             "data:text/html,<html><head><style>body {background-color:#227788}</style></head>" +
-            "<body><br>HelloWorld</body></html>");
+            "<body></body></html>");
         pollForBackgroundColor(Color.rgb(0x22, 0x77, 0x88));
 
         // Changing the base background should not override CSS background.
