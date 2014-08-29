01d6aef6b954cf02bf2cccbd63250f05b1a09fd6
meacer <meacer@chromium.org>
2014-08-28 18:54:50 -0700

    Add a separate warning message for topSites permission.
    The current warning is the generic "Read your browsing history"
    warning. The new warning for topSites will be suppressed by
    read only and read/write history warnings, and all hosts warning.
    
    BUG=404334
    
    Review URL: https://codereview.chromium.org/482953003
    
    Cr-Commit-Position: refs/heads/master@{#292545}

--- extensions/common/permissions/permission_message.h
+++ extensions/common/permissions/permission_message.h
@@ -89,6 +89,7 @@ class PermissionMessage {
     kIdentityEmail,
     kExperienceSamplingPrivate,
     kCopresence,
+    kTopSites,
     kEnumBoundary,
   };
   COMPILE_ASSERT(PermissionMessage::kNone > PermissionMessage::kUnknown,
