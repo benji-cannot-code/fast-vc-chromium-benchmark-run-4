a9ed7f8333db693c226be6d65b2f1e8cd93e5390
jvoung <jvoung@chromium.org>
2014-08-28 17:35:10 -0700

    Revert of Set RequestContextObject for PNaCl pexe fetches, to fix On-Demand update. (patchset #5 of https://codereview.chromium.org/471233003/)
    Reason for revert:
    Test is failing on a Blink bot on ChromeOS
    
    Original issue's description:
    > Set RequestContextObject for PNaCl pexe fetches, to fix On-Demand update.
    >
    > Add more checks to test to detect that the resource type of
    > the URL request is indeed OBJECT.
    > Would like to get this merged to the M38 branch.
    >
    > BUG=401755
    >
    > Committed: https://chromium.googlesource.com/chromium/src/+/bfc3e122d6721ccc93f0f8b19dac4a3c2a0c947d
    
    TBR=teravest@chromium.org,ncbray@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=401755
    
    Review URL: https://codereview.chromium.org/524483002
    
    Cr-Commit-Position: refs/heads/master@{#292522}

--- chrome/test/nacl/pnacl_header_test.h
+++ chrome/test/nacl/pnacl_header_test.h
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/memory/scoped_ptr.h"
 #include "chrome/test/base/in_process_browser_test.h"
-#include "content/public/browser/resource_dispatcher_host_delegate.h"
-#include "content/public/common/resource_type.h"
 
 namespace base {
 class FilePath;
@@ -22,30 +20,6 @@ struct HttpRequest;
 class HttpResponse;
 }
 }
-
-using content::ResourceDispatcherHostDelegate;
-
-class TestDispatcherHostDelegate : public ResourceDispatcherHostDelegate {
- public:
-  explicit TestDispatcherHostDelegate()
-      : ResourceDispatcherHostDelegate(), found_pnacl_header_(false) {}
-
-  virtual ~TestDispatcherHostDelegate() {}
-
-  virtual void RequestBeginning(
-      net::URLRequest* request,
-      content::ResourceContext* resource_context,
-      content::AppCacheService* appcache_service,
-      content::ResourceType resource_type,
-      ScopedVector<content::ResourceThrottle>* throttles) OVERRIDE;
-
-  bool found_pnacl_header() const { return found_pnacl_header_; }
-
- private:
-  bool found_pnacl_header_;
-
-  DISALLOW_COPY_AND_ASSIGN(TestDispatcherHostDelegate);
-};
 
 class PnaclHeaderTest : public InProcessBrowserTest {
  public:
@@ -67,7 +41,6 @@ class PnaclHeaderTest : public InProcessBrowserTest {
 
   int noncors_loads_;
   int cors_loads_;
-  TestDispatcherHostDelegate test_delegate_;
   DISALLOW_COPY_AND_ASSIGN(PnaclHeaderTest);
 };
 
