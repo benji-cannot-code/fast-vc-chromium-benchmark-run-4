437efedcc8f4d4237cbf0137dfb0b3c24281a04e
yhirano <yhirano@chromium.org>
2014-08-28 19:37:23 -0700

    [MIDI] Make android MIDI port id unique
    MIDI port id must be unique, as specified in the spec.
    
    BUG=341423
    
    Review URL: https://codereview.chromium.org/516893002
    
    Cr-Commit-Position: refs/heads/master@{#292553}

--- media/midi/midi_manager_usb.cc
+++ media/midi/midi_manager_usb.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/message_loop/message_loop.h"
+#include "base/strings/stringprintf.h"
 #include "media/midi/usb_midi_descriptor_parser.h"
 #include "media/midi/usb_midi_device.h"
 #include "media/midi/usb_midi_input_stream.h"
@@ -95,12 +96,24 @@ void MidiManagerUsb::OnEnumerateDevicesDone(bool result,
       if (jacks[j].direction() == UsbMidiJack::DIRECTION_OUT) {
         output_streams_.push_back(new UsbMidiOutputStream(jacks[j]));
         // TODO(yhirano): Set appropriate properties.
-        AddOutputPort(MidiPortInfo());
+        // TODO(yhiran): Port ID should contain product ID / vendor ID.
+        // Port ID must be unique in a MIDI manager. This (and the below) ID
+        // setting is sufficiently unique although there is no user-friendly
+        // meaning.
+        MidiPortInfo port;
+        port.id = base::StringPrintf("port-%ld-%ld",
+                                     static_cast<long>(i),
+                                     static_cast<long>(j));
+        AddOutputPort(port);
       } else {
         DCHECK_EQ(jacks[j].direction(), UsbMidiJack::DIRECTION_IN);
         input_jacks.push_back(jacks[j]);
         // TODO(yhirano): Set appropriate properties.
-        AddInputPort(MidiPortInfo());
+        MidiPortInfo port;
+        port.id = base::StringPrintf("port-%ld-%ld",
+                                     static_cast<long>(i),
+                                     static_cast<long>(j));
+        AddInputPort(port);
       }
     }
     input_stream_.reset(new UsbMidiInputStream(input_jacks, this));
