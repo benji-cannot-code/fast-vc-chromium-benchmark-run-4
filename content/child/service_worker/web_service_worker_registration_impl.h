95a2a8fa27a39247513bfbc98efb7739e17654dc
nhiroki <nhiroki@chromium.org>
2014-08-28 18:22:05 -0700

    ServiceWorker: Queue tasks until the registration proxy becomes ready
    In [[Install]] algo. of the latest spec, register() resolves a promise
    with a registration object after the worker enters the installation
    process. This means the registration needs to set its .installing field
    before the register promise is resolved.
    
    But, before this patch, the registration cannot do so before resolving
    the register promise because its proxy is set when the registration is
    passed to the blink.
    
    This change makes the registration queue notifications of worker version
    and 'updatefound'. Once the proxy becomes ready, the registration handles
    them.
    
    Spec: https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#installation-algorithm
    
    BUG=399533,406240
    TEST=content_unittests --gtest_filter=ServiceWorker*
    TEST=run_webkit_tests.py http/tests/serviceworker
    
    Review URL: https://codereview.chromium.org/516873002
    
    Cr-Commit-Position: refs/heads/master@{#292535}

--- content/child/service_worker/web_service_worker_registration_impl.h
+++ content/child/service_worker/web_service_worker_registration_impl.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_CHILD_SERVICE_WORKER_WEB_SERVICE_WORKER_REGISTRATION_IMPL_H_
 #define CONTENT_CHILD_SERVICE_WORKER_WEB_SERVICE_WORKER_REGISTRATION_IMPL_H_
 
+#include <vector>
+
 #include "base/compiler_specific.h"
 #include "third_party/WebKit/public/platform/WebServiceWorkerRegistration.h"
 
@@ -39,8 +41,27 @@ class WebServiceWorkerRegistrationImpl
   virtual blink::WebURL scope() const;
 
  private:
+  enum QueuedTaskType {
+    INSTALLING,
+    WAITING,
+    ACTIVE,
+    UPDATE_FOUND,
+  };
+
+  struct QueuedTask {
+    QueuedTask(QueuedTaskType type,
+               blink::WebServiceWorker* worker);
+    QueuedTaskType type;
+    blink::WebServiceWorker* worker;
+  };
+
+  void RunQueuedTasks();
+  void ClearQueuedTasks();
+
   scoped_ptr<ServiceWorkerRegistrationHandleReference> handle_ref_;
   blink::WebServiceWorkerRegistrationProxy* proxy_;
+
+  std::vector<QueuedTask> queued_tasks_;
 
   DISALLOW_COPY_AND_ASSIGN(WebServiceWorkerRegistrationImpl);
 };
