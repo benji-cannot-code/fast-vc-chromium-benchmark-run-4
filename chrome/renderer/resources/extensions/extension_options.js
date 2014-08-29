de71acc738b358f6c80b8ef7ee7ab795d473eadd
ericzeng <ericzeng@chromium.org>
2014-08-29 07:32:06 -0700

    Fix ExtensionOptionsGuest style and minor bug
    - Reorganize methods in cc file so they match up with the header
    - In SetUpAutoSize, use the autosize flag parameter read in from
      the embedder to enable/disable autosize instead of passing true
    
    BUG=386838
    
    Review URL: https://codereview.chromium.org/510183002
    
    Cr-Commit-Position: refs/heads/master@{#292629}

--- chrome/renderer/resources/extensions/extension_options.js
+++ chrome/renderer/resources/extensions/extension_options.js
@@ -54,7 +54,7 @@ ExtensionOptionsInternal.prototype.attachWindow = function() {
       this.internalInstanceId,
       this.guestInstanceId,
       {
-        'autosize': this.autosize,
+        'autosize': this.extensionoptionsNode.hasAttribute('autosize'),
         'instanceId': this.viewInstanceId,
         'maxheight': parseInt(this.maxheight || 0),
         'maxwidth': parseInt(this.maxwidth || 0),
