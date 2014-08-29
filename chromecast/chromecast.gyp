183e0f81972f3639ca1d537d3739e134f88c0db6
gunsch <gunsch@chromium.org>
2014-08-29 15:28:44 -0700

    Chromecast fix: MetricsStateManager segfaults if callbacks are empty.
    R=lcwu@chromium.org,asvitkine@chromium.org
    BUG=None
    
    Review URL: https://codereview.chromium.org/522853002
    
    Cr-Commit-Position: refs/heads/master@{#292691}

--- chromecast/chromecast.gyp
+++ chromecast/chromecast.gyp
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'target_name': 'cast_metrics',
       'type': '<(component)',
       'dependencies': [
+        'cast_common',
         '../components/components.gyp:component_metrics_proto',
         '../components/components.gyp:metrics',
         '../components/components.gyp:metrics_net',
@@ -69,6 +70,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         }],
       ],
     },
+    {
+      'target_name': 'cast_metrics_unittests',
+      'type': '<(gtest_target_type)',
+      'dependencies': [
+        'cast_metrics',
+        '../base/base.gyp:base_prefs_test_support',
+        '../base/base.gyp:run_all_unittests',
+        '../base/base.gyp:test_support_base',
+        '../components/components.gyp:component_metrics_proto',
+        '../testing/gtest.gyp:gtest',
+      ],
+      'sources': [
+        'metrics/cast_metrics_service_client_unittest.cc',
+      ],
+    },  # end of target 'cast_metrics_unittests'
     {
       'target_name': 'cast_service',
       'type': '<(component)',
--- chromecast/metrics/cast_metrics_service_client.cc
+++ chromecast/metrics/cast_metrics_service_client.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/rtl.h"
 #include "chromecast/common/chromecast_config.h"
 #include "chromecast/metrics/platform_metrics_providers.h"
+#include "components/metrics/client_info.h"
 #include "components/metrics/metrics_provider.h"
 #include "components/metrics/metrics_service.h"
 #include "components/metrics/metrics_state_manager.h"
@@ -15,6 +16,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromecast {
 namespace metrics {
+
+namespace {
+
+void StoreClientInfo(const ::metrics::ClientInfo& client_info) {
+}
+
+scoped_ptr<::metrics::ClientInfo> LoadClientInfo() {
+  return scoped_ptr<::metrics::ClientInfo>();
+}
+
+}  // namespace
 
 // static
 CastMetricsServiceClient* CastMetricsServiceClient::Create(
@@ -93,12 +105,12 @@ CastMetricsServiceClient::CastMetricsServiceClient(
           pref_service,
           base::Bind(&CastMetricsServiceClient::IsReportingEnabled,
                      base::Unretained(this)),
-                     ::metrics::MetricsStateManager::StoreClientInfoCallback(),
-                     ::metrics::MetricsStateManager::LoadClientInfoCallback())),
+                     base::Bind(&StoreClientInfo),
+                     base::Bind(&LoadClientInfo))),
       metrics_service_(new ::metrics::MetricsService(
           metrics_state_manager_.get(),
           this,
-          ChromecastConfig::GetInstance()->pref_service())),
+          pref_service)),
       request_context_(request_context) {
   // Always create a client id as it may also be used by crash reporting,
   // (indirectly) included in feedback, and can be queried during setup.
