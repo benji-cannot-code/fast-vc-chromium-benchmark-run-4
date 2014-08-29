ffa303e1e635946f175d7c5fbcc4162789f20dde
dcheng <dcheng@chromium.org>
2014-08-29 06:34:04 -0700

    Manual fixups in drive code for scoped_refptr operator T* removal.
    BUG=110610
    
    Review URL: https://codereview.chromium.org/511323002
    
    Cr-Commit-Position: refs/heads/master@{#292620}

--- google_apis/drive/request_sender_unittest.cc
+++ google_apis/drive/request_sender_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "google_apis/drive/request_sender.h"
 
+#include "base/sequenced_task_runner.h"
 #include "base/strings/string_number_conversions.h"
 #include "google_apis/drive/base_requests.h"
 #include "google_apis/drive/dummy_auth_service.h"
