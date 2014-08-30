24c5cb95f151693828cdd0f63b76b879fd8f5534
gunsch <gunsch@chromium.org>
2014-08-30 12:20:23 -0700

    Chromecast buildfix: forgot an include.
    Was necessary for https://codereview.chromium.org/517213005/.
    
    R=lcwu@chromium.org,damienv@chromium.org
    BUG=None
    
    Review URL: https://codereview.chromium.org/526693002
    
    Cr-Commit-Position: refs/heads/master@{#292804}

--- chromecast/shell/browser/cast_content_browser_client.cc
+++ chromecast/shell/browser/cast_content_browser_client.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromecast/shell/browser/cast_content_browser_client.h"
 
 #include "base/command_line.h"
+#include "base/i18n/rtl.h"
 #include "chromecast/shell/browser/cast_browser_context.h"
 #include "chromecast/shell/browser/cast_browser_main_parts.h"
 #include "chromecast/shell/browser/cast_browser_process.h"
