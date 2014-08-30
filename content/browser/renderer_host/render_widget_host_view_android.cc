601ed73d9c69cff11f9022c34c1992e27a7c05e7
jdduke <jdduke@chromium.org>
2014-08-29 18:34:50 -0700

    Use platform slop/timeout values for selection handle tap detection
    Currently, selection handle taps are detected using a basic, hard-coded
    timeout threshold. Update this logic to use the platform tap timeout and
    scroll slop values, ensuring tap detection consistentcy with the
    platform.
    
    BUG=394093,407963
    
    Review URL: https://codereview.chromium.org/516573003
    
    Cr-Commit-Position: refs/heads/master@{#292748}

--- content/browser/renderer_host/render_widget_host_view_android.cc
+++ content/browser/renderer_host/render_widget_host_view_android.cc
@@ -73,6 +73,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/gesture_detection/motion_event.h"
 #include "ui/gfx/android/device_display_info.h"
 #include "ui/gfx/android/java_bitmap.h"
+#include "ui/gfx/android/view_configuration.h"
 #include "ui/gfx/display.h"
 #include "ui/gfx/screen.h"
 #include "ui/gfx/size_conversions.h"
@@ -194,6 +195,19 @@ scoped_ptr<OverscrollGlow> CreateOverscrollEffect(
       new OverscrollGlow(base::Bind(&CreateEdgeEffect,
                                     system_resource_manager,
                                     content_view_core->GetDpiScale())));
+}
+
+scoped_ptr<TouchSelectionController> CreateSelectionController(
+    TouchSelectionControllerClient* client,
+    ContentViewCore* content_view_core) {
+  DCHECK(client);
+  DCHECK(content_view_core);
+  int tap_timeout_ms = gfx::ViewConfiguration::GetTapTimeoutInMs();
+  int touch_slop_pixels = gfx::ViewConfiguration::GetTouchSlopInPixels();
+  return make_scoped_ptr(new TouchSelectionController(
+      client,
+      base::TimeDelta::FromMilliseconds(tap_timeout_ms),
+      touch_slop_pixels / content_view_core->GetDpiScale()));
 }
 
 ui::GestureProvider::Config CreateGestureProviderConfig() {
@@ -1636,7 +1650,7 @@ void RenderWidgetHostViewAndroid::SetContentViewCore(
     WasResized();
 
   if (!selection_controller_)
-    selection_controller_.reset(new TouchSelectionController(this));
+    selection_controller_ = CreateSelectionController(this, content_view_core_);
 
   if (overscroll_effect_enabled_ && !overscroll_effect_ &&
       content_view_core_->GetWindowAndroid()->GetCompositor())
