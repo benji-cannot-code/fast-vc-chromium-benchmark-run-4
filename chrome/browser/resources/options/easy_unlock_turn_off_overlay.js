9f33e07ca9a48d0e007b9138c219472dbdf5f294
xiyuan <xiyuan@chromium.org>
2014-08-31 01:41:34 -0700

    Revert of EasyUnlock: Work around the ghost overlay from CSS animation. (patchset #3 id:40001 of https://codereview.chromium.org/487373002/)
    Reason for revert:
    No longer needed since underlying blink issue http://crbug.com/405250 is fixed.
    
    Original issue's description:
    > EasyUnlock: Work around the ghost overlay from CSS animation.
    >
    > BUG=404779
    >
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=290743
    
    TBR=tbarzic@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=404779
    
    Review URL: https://codereview.chromium.org/521343002
    
    Cr-Commit-Position: refs/heads/master@{#292816}

--- chrome/browser/resources/options/easy_unlock_turn_off_overlay.js
+++ chrome/browser/resources/options/easy_unlock_turn_off_overlay.js
@@ -93,13 +93,6 @@ cr.define('options', function() {
       chrome.send('easyUnlockTurnOffOverlayDismissed');
     },
 
-    /** @override */
-    handleCancel: function() {
-      // Make sure keyboard dismiss uses the word around as well.
-      // TODO(xiyuan): Remove the workaround. See http://crbug.com/405250
-      EasyUnlockTurnOffOverlay.dismiss();
-    },
-
     /**
      * Returns the button strip element.
      * @return {HTMLDivElement} The container div of action buttons.
@@ -183,12 +176,7 @@ cr.define('options', function() {
    * Closes the Easy unlock turn off overlay.
    */
   EasyUnlockTurnOffOverlay.dismiss = function() {
-    // TODO(xiyuan): Remove the workaround and call closeOverly directly.
-    // See http://crbug.com/405250
-    $('easy-unlock-turn-off-spinner').hidden = true;
-    window.setTimeout(function() {
-      PageManager.closeOverlay();
-    }, 100);
+    PageManager.closeOverlay();
   };
 
   /**
