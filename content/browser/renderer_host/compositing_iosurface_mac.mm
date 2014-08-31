b9e3dfd460ed3f510c8dbe4037b6df117ad1e7d4
shouqun <shouqun@chromium.org>
2014-08-30 18:34:45 -0700

    Fix build error with XCode6/OSX 10.9 SDK
    OpenGL/gl.h is required to be included to make the build pass.
    
    BUG=
    TEST=
    
    Review URL: https://codereview.chromium.org/522533002
    
    Cr-Commit-Position: refs/heads/master@{#292810}

--- content/browser/renderer_host/compositing_iosurface_mac.mm
+++ content/browser/renderer_host/compositing_iosurface_mac.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <OpenGL/CGLIOSurface.h>
 #include <OpenGL/CGLRenderers.h>
 #include <OpenGL/OpenGL.h>
+#include <OpenGL/gl.h>
 
 #include "base/bind.h"
 #include "base/bind_helpers.h"
