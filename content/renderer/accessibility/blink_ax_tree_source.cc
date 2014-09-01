47bf5770863962c50982098e605002d693b606ef
tkent <tkent@chromium.org>
2014-08-31 23:39:10 -0700

    AX: Remove WebLocalFrame dependency from BlinkAXTreeSource.
    This is a preparation to support AXObject in PagePopup.
    PagePopup doesn't have WebLocalFrame, and it's difficult to provide it. We can
    remove WebLocalFrame dependency of BlinkAXTreeSource by using
    WebDocument::scrollOffset, minimumScrollOffset, and maximumScrollOffset.
    
    This CL has no functionality changes.
    
    BUG=123896
    
    Review URL: https://codereview.chromium.org/510123003
    
    Cr-Commit-Position: refs/heads/master@{#292839}

--- content/renderer/accessibility/blink_ax_tree_source.cc
+++ content/renderer/accessibility/blink_ax_tree_source.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/web/WebElement.h"
 #include "third_party/WebKit/public/web/WebFormControlElement.h"
 #include "third_party/WebKit/public/web/WebInputElement.h"
-#include "third_party/WebKit/public/web/WebLocalFrame.h"
 #include "third_party/WebKit/public/web/WebNode.h"
 #include "third_party/WebKit/public/web/WebView.h"
 
@@ -35,7 +34,6 @@ using blink::WebDocument;
 using blink::WebDocumentType;
 using blink::WebElement;
 using blink::WebFrame;
-using blink::WebLocalFrame;
 using blink::WebNode;
 using blink::WebVector;
 using blink::WebView;
@@ -440,15 +438,15 @@ void BlinkAXTreeSource::SerializeNode(blink::WebAXObject src,
                               UTF16ToUTF8(doctype.name()));
     }
 
-    const gfx::Size& scroll_offset = document.frame()->scrollOffset();
+    const gfx::Size& scroll_offset = document.scrollOffset();
     dst->AddIntAttribute(ui::AX_ATTR_SCROLL_X, scroll_offset.width());
     dst->AddIntAttribute(ui::AX_ATTR_SCROLL_Y, scroll_offset.height());
 
-    const gfx::Size& min_offset = document.frame()->minimumScrollOffset();
+    const gfx::Size& min_offset = document.minimumScrollOffset();
     dst->AddIntAttribute(ui::AX_ATTR_SCROLL_X_MIN, min_offset.width());
     dst->AddIntAttribute(ui::AX_ATTR_SCROLL_Y_MIN, min_offset.height());
 
-    const gfx::Size& max_offset = document.frame()->maximumScrollOffset();
+    const gfx::Size& max_offset = document.maximumScrollOffset();
     dst->AddIntAttribute(ui::AX_ATTR_SCROLL_X_MAX, max_offset.width());
     dst->AddIntAttribute(ui::AX_ATTR_SCROLL_Y_MAX, max_offset.height());
   }
