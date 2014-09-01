a03f2b2322c1396b419e91d904d2adead4955b93
haraken@chromium.org <haraken@chromium.org>
2014-09-01 08:44:19 +0000

    Oilpan: Move RTCOfferOptions to oilpan's heap
    This is one of leftovers when we shipped Oilpan for mediastream/.
    
    BUG=340522
    
    Review URL: https://codereview.chromium.org/510673003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181162 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/exported/WebRTCOfferOptions.cpp
+++ third_party/WebKit/Source/platform/exported/WebRTCOfferOptions.cpp
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-WebRTCOfferOptions::WebRTCOfferOptions(const PassRefPtr<RTCOfferOptions>& options)
+WebRTCOfferOptions::WebRTCOfferOptions(RTCOfferOptions* options)
     : m_private(options)
 {
 }
