ccffeee90fa0607e446a68a65b639d56ad81daac
dongseong.hwang@intel.com <dongseong.hwang@intel.com>
2014-09-01 10:16:51 +0000

    Canvas: composite video on canvas correctly.
    Currently, 2D Canvas composites a video as only source-over,
    because Blink doesn't pass composite information to chromium
    when calling platform video painting API.
    This CL fixes it.
    
    TEST=fast/canvas/canvas-drawImage-video.html
    BUG=407079
    
    Review URL: https://codereview.chromium.org/443873005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181175 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebMediaPlayer.h
+++ third_party/WebKit/public/platform/WebMediaPlayer.h
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebMediaSource.h"
 #include "WebString.h"
 #include "WebTimeRange.h"
+#include "third_party/skia/include/core/SkXfermode.h"
 
 namespace blink {
 
@@ -113,8 +114,6 @@ public:
     virtual WebTimeRanges buffered() const = 0;
     virtual double maxTimeSeekable() const = 0;
 
-    virtual void paint(WebCanvas*, const WebRect&, unsigned char alpha) = 0;
-
     // True if the loaded media has a playable video/audio track.
     virtual bool hasVideo() const = 0;
     virtual bool hasAudio() const = 0;
@@ -145,6 +144,7 @@ public:
     virtual unsigned audioDecodedByteCount() const = 0;
     virtual unsigned videoDecodedByteCount() const = 0;
 
+    virtual void paint(WebCanvas*, const WebRect&, unsigned char alpha, SkXfermode::Mode) = 0;
     // Do a GPU-GPU textures copy if possible.
     virtual bool copyVideoTextureToPlatformTexture(WebGraphicsContext3D*, unsigned texture, unsigned level, unsigned internalFormat, unsigned type, bool premultiplyAlpha, bool flipY) { return false; }
 
