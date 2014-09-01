978c0eb2bcfd62b75142d088e83298a7d62c4021
qsr <qsr@chromium.org>
2014-09-01 02:27:03 -0700

    Add test data for mojo instrumentation tests.
    R=navabi@chromium.org
    
    Review URL: https://codereview.chromium.org/517983002
    
    Cr-Commit-Position: refs/heads/master@{#292854}

--- build/android/buildbot/bb_device_steps.py
+++ build/android/buildbot/bb_device_steps.py
@@ -215,7 +215,7 @@ def RunMojoTests(options):
            None,
            'org.chromium.mojo.tests',
            'MojoTest',
-           None)
+           'bindings:mojo/public/interfaces/bindings/tests/data')
   RunInstrumentationSuite(options, test)
 
 
