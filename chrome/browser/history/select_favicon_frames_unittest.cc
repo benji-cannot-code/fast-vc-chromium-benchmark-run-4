4285b0d53cf74e53ab496082849ea22167bad952
pkotwicz <pkotwicz@chromium.org>
2014-09-02 12:35:23 -0700

    Fix SelectFaviconFramesTest on Nexus 5
    BUG=407561
    TEST=Tests pass in Nexus 5
    
    Review URL: https://codereview.chromium.org/521393002
    
    Cr-Commit-Position: refs/heads/master@{#292977}

--- chrome/browser/history/select_favicon_frames_unittest.cc
+++ chrome/browser/history/select_favicon_frames_unittest.cc
@@ -198,6 +198,8 @@ TEST(SelectFaviconFramesTest, _16From16_Scale2x_32_From_32) {
 }
 
 TEST(SelectFaviconFramesTest, ExactMatchBetterThanLargeBitmap) {
+  SCOPED_FAVICON_SCALE_FACTOR_1XAND2X;
+
   float score1;
   vector<SkBitmap> bitmaps1;
   bitmaps1.push_back(MakeBitmap(SK_ColorGREEN, 48, 48));
@@ -216,6 +218,8 @@ TEST(SelectFaviconFramesTest, ExactMatchBetterThanLargeBitmap) {
 }
 
 TEST(SelectFaviconFramesTest, UpsampleABitBetterThanHugeBitmap) {
+  SCOPED_FAVICON_SCALE_FACTOR_1XAND2X;
+
   float score1;
   vector<SkBitmap> bitmaps1;
   bitmaps1.push_back(MakeBitmap(SK_ColorGREEN, 128, 128));
@@ -246,6 +250,8 @@ TEST(SelectFaviconFramesTest, UpsampleABitBetterThanHugeBitmap) {
 }
 
 TEST(SelectFaviconFramesTest, DownsamplingBetterThanUpsampling) {
+  SCOPED_FAVICON_SCALE_FACTOR_1XAND2X;
+
   float score1;
   vector<SkBitmap> bitmaps1;
   bitmaps1.push_back(MakeBitmap(SK_ColorGREEN, 8, 8));
@@ -262,6 +268,8 @@ TEST(SelectFaviconFramesTest, DownsamplingBetterThanUpsampling) {
 }
 
 TEST(SelectFaviconFramesTest, DownsamplingLessIsBetter) {
+  SCOPED_FAVICON_SCALE_FACTOR_1XAND2X;
+
   float score1;
   vector<SkBitmap> bitmaps1;
   bitmaps1.push_back(MakeBitmap(SK_ColorGREEN, 34, 34));
@@ -278,6 +286,8 @@ TEST(SelectFaviconFramesTest, DownsamplingLessIsBetter) {
 }
 
 TEST(SelectFaviconFramesTest, UpsamplingLessIsBetter) {
+  SCOPED_FAVICON_SCALE_FACTOR_1XAND2X;
+
   float score1;
   vector<SkBitmap> bitmaps1;
   bitmaps1.push_back(MakeBitmap(SK_ColorGREEN, 8, 8));
@@ -296,6 +306,8 @@ TEST(SelectFaviconFramesTest, UpsamplingLessIsBetter) {
 // Test that the score is determined by the |original_sizes| parameter, not the
 // |bitmaps| parameter to SelectFaviconFrames().
 TEST(SelectFaviconFramesTest, ScoreDeterminedByOriginalSizes) {
+  SCOPED_FAVICON_SCALE_FACTOR_1XAND2X;
+
   vector<SkBitmap> bitmaps1;
   bitmaps1.push_back(MakeBitmap(SK_ColorGREEN, 16, 16));
   vector<gfx::Size> sizes1;
