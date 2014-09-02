4e65117b2ce6f0c5d9502415bca81871369d0f5a
Paweł Hajdan, Jr <phajdan.jr@chromium.org>
2014-09-02 09:58:12 +0200

    Explicitly #include <algorithm> in media/cast/logging/encoding_event_subscriber.cc
    This is needed for std::sort.
    
    I detected the issue when building against system protobuf-2.6.0.
    Likely some header is no longer implicitly/indirectly #included.
    
    BUG=none
    R=miu@chromium.org
    
    Review URL: https://codereview.chromium.org/528743002
    
    Cr-Commit-Position: refs/heads/master@{#292903}

--- media/cast/logging/encoding_event_subscriber.cc
+++ media/cast/logging/encoding_event_subscriber.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/cast/logging/encoding_event_subscriber.h"
 
+#include <algorithm>
 #include <cstring>
 #include <utility>
 
