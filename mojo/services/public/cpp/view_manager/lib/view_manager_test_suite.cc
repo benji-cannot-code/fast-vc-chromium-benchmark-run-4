70dfaa554a8afc5c0fce15228d5e9d93d16e1b08
sky <sky@chromium.org>
2014-09-02 08:13:57 -0700

    Fixes to get view_manager_lib_unittests not crash
    . ICU may be initialized twice: once by the test suite, secondly by
      the app.
    . window_mamanger_app_.host_ may be deleted by the time we get to the
      destructor to try and remove something. Add check for this.
    
    BUG=none
    TEST=none
    R=viettrungluu@chromium.org
    
    Review URL: https://codereview.chromium.org/516523004
    
    Cr-Commit-Position: refs/heads/master@{#292929}

--- mojo/services/public/cpp/view_manager/lib/view_manager_test_suite.cc
+++ mojo/services/public/cpp/view_manager/lib/view_manager_test_suite.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "mojo/services/public/cpp/view_manager/lib/view_manager_test_suite.h"
 
+#include "base/i18n/icu_util.h"
 #include "ui/gl/gl_surface.h"
 
 #if defined(USE_X11)
@@ -28,6 +29,9 @@ void ViewManagerTestSuite::Initialize() {
 
   base::TestSuite::Initialize();
   gfx::GLSurface::InitializeOneOffForTests();
+
+  // base::TestSuite and ViewsInit both try to load icu. That's ok for tests.
+  base::i18n::AllowMultipleInitializeCallsForTesting();
 }
 
 }  // namespace mojo
