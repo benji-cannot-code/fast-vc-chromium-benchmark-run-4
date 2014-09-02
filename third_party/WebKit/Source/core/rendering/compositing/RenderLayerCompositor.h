73a5bb0435eda57615ef29c5e9cdfbdeaabccb99
kouhei@chromium.org <kouhei@chromium.org>
2014-09-02 20:12:18 +0000

    Remove dead code in RenderLayerCompositor.h
    The method implementation does not exist, and the functionality now lives in |CompositingRequirementsUpdater|.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/522173005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181254 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/compositing/RenderLayerCompositor.h
+++ third_party/WebKit/Source/core/rendering/compositing/RenderLayerCompositor.h
@@ -174,8 +174,6 @@ public:
     bool inOverlayFullscreenVideo() const { return m_inOverlayFullscreenVideo; }
 
 private:
-    class OverlapMap;
-
 #if ENABLE(ASSERT)
     void assertNoUnresolvedDirtyBits();
 #endif
@@ -191,8 +189,6 @@ private:
 
     void updateWithoutAcceleratedCompositing(CompositingUpdateType);
     void updateIfNeeded();
-
-    void computeCompositingRequirements(RenderLayer* ancestorLayer, RenderLayer*, OverlapMap&, struct CompositingRecursionData&, bool& descendantHas3DTransform, Vector<RenderLayer*>& unclippedDescendants, IntRect& absoluteDecendantBoundingBox);
 
     bool hasAnyAdditionalCompositedLayers(const RenderLayer* rootLayer) const;
 
