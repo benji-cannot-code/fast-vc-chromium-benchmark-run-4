8e1aaf043e6b0928f85ee38536e2e3876ba27bfb
dtrainor <dtrainor@chromium.org>
2014-09-02 11:17:43 -0700

    [Android] Stop all media on undoable close.
    -Stop all media playback on an undoable tab close
    
    BUG=388600
    
    Review URL: https://codereview.chromium.org/493253002
    
    Cr-Commit-Position: refs/heads/master@{#292959}

--- content/browser/web_contents/web_contents_android.cc
+++ content/browser/web_contents/web_contents_android.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "content/browser/android/interstitial_page_delegate_android.h"
 #include "content/browser/frame_host/interstitial_page_impl.h"
+#include "content/browser/media/android/browser_media_player_manager.h"
 #include "content/browser/media/media_web_contents_observer.h"
 #include "content/browser/renderer_host/render_view_host_impl.h"
 #include "content/browser/web_contents/web_contents_impl.h"
@@ -183,6 +184,21 @@ void WebContentsAndroid::OnHide(JNIEnv* env, jobject obj) {
 
 void WebContentsAndroid::OnShow(JNIEnv* env, jobject obj) {
   web_contents_->WasShown();
+}
+
+void WebContentsAndroid::ReleaseMediaPlayers(JNIEnv* env, jobject jobj) {
+#if defined(ENABLE_BROWSER_CDMS)
+  RenderViewHostImpl* rvhi = static_cast<RenderViewHostImpl*>(
+      web_contents_->GetRenderViewHost());
+  if (!rvhi || !rvhi->GetMainFrame())
+    return;
+
+  BrowserMediaPlayerManager* manager =
+      rvhi->media_web_contents_observer()->GetMediaPlayerManager(
+          rvhi->GetMainFrame());
+  if (manager)
+    manager->ReleaseAllMediaPlayers();
+#endif // defined(ENABLE_BROWSER_CDMS)
 }
 
 void WebContentsAndroid::PauseVideo() {
