1d64d63093b51ecf20b51d995e39007e125e022a
Scott Violet <sky@chromium.org>
2014-09-02 13:37:45 -0700

    Fixes compile error
    Without this I get compile errors in motion_event_web.cc because M_PI
    is undeclared.
    
    BUG=409940
    TEST=none
    R=scottmg@chromium.org
    
    Review URL: https://codereview.chromium.org/533863002
    
    Cr-Commit-Position: refs/heads/master@{#292991}

--- build/precompile.h
+++ build/precompile.h
@@ -21,6 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #define BUILD_PRECOMPILE_H_
 
+#define _USE_MATH_DEFINES
+
 // The Windows header needs to come before almost all the other
 // Windows-specific headers.
 #include <Windows.h>
