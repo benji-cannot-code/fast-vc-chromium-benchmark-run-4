9b81a416f26179dcfcb1c2e74a7bff76fa00906d
fsamuel <fsamuel@chromium.org>
2014-09-02 10:54:56 -0700

    Implement RenderWidgetHostViewAndroid::GetLastScrollOffset
    Apparently this code was being hit on Android. This CL implements the scroll
    offset the same way it's implemented on other platforms.
    
    BUG=409792
    NOTRY=true
    TBR=aelias@chromium.org
    
    Review URL: https://codereview.chromium.org/533483004
    
    Cr-Commit-Position: refs/heads/master@{#292956}

--- content/browser/renderer_host/render_widget_host_view_android.cc
+++ content/browser/renderer_host/render_widget_host_view_android.cc
@@ -428,8 +428,7 @@ bool RenderWidgetHostViewAndroid::HasValidFrame() const {
 }
 
 gfx::Vector2dF RenderWidgetHostViewAndroid::GetLastScrollOffset() const {
-  NOTREACHED();
-  return gfx::Vector2dF();
+  return last_scroll_offset_;
 }
 
 gfx::NativeView RenderWidgetHostViewAndroid::GetNativeView() const {
@@ -1035,6 +1034,7 @@ void RenderWidgetHostViewAndroid::ComputeContentsSize(
 void RenderWidgetHostViewAndroid::InternalSwapCompositorFrame(
     uint32 output_surface_id,
     scoped_ptr<cc::CompositorFrame> frame) {
+  last_scroll_offset_ = frame->metadata.root_scroll_offset;
   if (!frame->delegated_frame_data) {
     LOG(ERROR) << "Non-delegated renderer path no longer supported";
     return;
