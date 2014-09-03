805adc9376459bccb775a46f491589eb32832092
andresantoso <andresantoso@chromium.org>
2014-09-03 09:28:30 -0700

    Revert of [fsp] Enable the File System Provider API for extensions. (patchset #2 id:20001 of https://codereview.chromium.org/527663002/)
    Reason for revert:
    Speculative revert, failed ChromiumOS browser_tests.
    
    [1422/1422] FileSystemProviderApiTest.Extension (11555 ms)
    1 test failed:
        FileSystemProviderApiTest.Extension
    
    http://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Tests%20%28dbg%29%282%29/builds/11812
    
    Original issue's description:
    > [fsp] Enable the File System Provider API for extensions.
    >
    > Previously, only apps were allowed to use this api, however most clients will
    > be actually written as extensions.
    >
    > TEST=browser_tests: *FileSystemProvider*Extension*
    > BUG=409487
    >
    > Committed: https://chromium.googlesource.com/chromium/src/+/4acb869459901c6fa627473bb15b8a0f6190cc11
    
    TBR=benwells@chromium.org,kalman@chromium.org,mtomasz@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=409487
    
    Review URL: https://codereview.chromium.org/537603002
    
    Cr-Commit-Position: refs/heads/master@{#293148}

--- chrome/browser/chromeos/extensions/file_system_provider/file_system_provider_apitest.cc
+++ chrome/browser/chromeos/extensions/file_system_provider/file_system_provider_apitest.cc
@@ -121,10 +121,4 @@ IN_PROC_BROWSER_TEST_F(FileSystemProviderApiTest, WriteFile) {
       << message_;
 }
 
-IN_PROC_BROWSER_TEST_F(FileSystemProviderApiTest, Extension) {
-  ASSERT_TRUE(RunPlatformAppTestWithFlags("file_system_provider/extension",
-                                          kFlagLoadAsComponent))
-      << message_;
-}
-
 }  // namespace extensions
