f035d54c0b75581d8e600e6f3b9b3cca6106a942
estade <estade@chromium.org>
2014-09-02 18:25:07 -0700

    Focus the first input when adding a new Autofill cc or address in chrome://settings
    It was mentioned that this was desirable behavior for desktop during the UX review for the linked bug.
    
    BUG=407209
    
    Review URL: https://codereview.chromium.org/516403003
    
    Cr-Commit-Position: refs/heads/master@{#293040}

--- chrome/browser/resources/options/autofill_options.js
+++ chrome/browser/resources/options/autofill_options.js
@@ -105,6 +105,7 @@ cr.define('options', function() {
       var title = loadTimeData.getString('addAddressTitle');
       AutofillEditAddressOverlay.setTitle(title);
       PageManager.showPageByName('autofillEditAddress');
+      AutofillEditAddressOverlay.prepForNewAddress();
     },
 
     /**
@@ -116,6 +117,7 @@ cr.define('options', function() {
       var title = loadTimeData.getString('addCreditCardTitle');
       AutofillEditCreditCardOverlay.setTitle(title);
       PageManager.showPageByName('autofillEditCreditCard');
+      AutofillEditCreditCardOverlay.prepForNewCard();
     },
 
     /**
@@ -182,8 +184,8 @@ cr.define('options', function() {
 
     /**
      * Shows the 'Edit credit card' overlay, using the data in |credit_card| to
-     * fill the input fields. |address| is a list with one item, an associative
-     * array that contains the credit card data.
+     * fill the input fields. |creditCard| is a list with one item, an
+     * associative array that contains the credit card data.
      * @private
      */
     showEditCreditCardOverlay_: function(creditCard) {
