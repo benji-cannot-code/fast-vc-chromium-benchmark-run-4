4e30a1de89955393089361b558dfa1cff0eec181
eakuefner <eakuefner@chromium.org>
2014-09-03 00:03:46 -0700

    [Telemetry] Add option to output Chart JSON summary.
    This CL adds an option --chartjson to the test runner to automatically perform
    summarization to the specialized Chart JSON format that the perf dashboard has
    been modified to accept. Passing --chartjson is mutually exclusive with the --output-formatter
    option.
    
    Review URL: https://codereview.chromium.org/531973002
    
    Cr-Commit-Position: refs/heads/master@{#293089}

--- tools/telemetry/telemetry/benchmark.py
+++ tools/telemetry/telemetry/benchmark.py
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import json
 import logging
 import optparse
 import os
@@ -18,6 +19,7 @@ from telemetry.page import page_runner
 from telemetry.page import page_set
 from telemetry.page import page_test
 from telemetry.page import test_expectations
+from telemetry.results import chart_json
 from telemetry.results import results_options
 from telemetry.util import cloud_storage
 
@@ -90,13 +92,20 @@ class Benchmark(command_line.Command):
 
     self._DownloadGeneratedProfileArchive(finder_options)
 
-    results = results_options.CreateResults(self.GetMetadata(), finder_options)
+    benchmark_metadata = self.GetMetadata()
+    results = results_options.CreateResults(benchmark_metadata, finder_options)
     try:
       page_runner.Run(pt, ps, expectations, finder_options, results)
     except page_test.TestNotSupportedOnPlatformFailure as failure:
       logging.warning(str(failure))
 
-    results.PrintSummary()
+    if finder_options.chartjson:
+      print json.dumps(chart_json.ResultsAsChartDict(
+          benchmark_metadata,
+          results.all_page_specific_values,
+          results.all_summary_values))
+    else:
+      results.PrintSummary()
     return len(results.failures)
 
   def _DownloadGeneratedProfileArchive(self, options):
