de5733a06cc1b0525573db70bce43d37f8bbf29b
ajith.v <ajith.v@samsung.com>
2014-09-03 16:41:40 -0700

    Adding Paste Popup behavior Unit Test cases.
    Currently there is not enough unit test cases available to cover
    the functionality of Paste Popup menu behavior. This patch covers
    essential unit test cases for the same.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/534343002
    
    Cr-Commit-Position: refs/heads/master@{#293222}

--- content/public/android/javatests/src/org/chromium/content/browser/ContentViewCoreSelectionTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/ContentViewCoreSelectionTest.java
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.content.browser;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.test.suitebuilder.annotation.SmallTest;
 
 import org.chromium.base.test.util.Feature;
@@ -43,6 +46,7 @@ public class ContentViewCoreSelectionTest extends ContentShellTestBase {
         mContentViewCore = getContentViewCore();
         assertWaitForPageScaleFactorMatch(1.1f);
         assertWaitForSelectActionBarStatus(false);
+        assertWaitForPastePopupStatus(false);
     }
 
     @SmallTest
@@ -92,6 +96,74 @@ public class ContentViewCoreSelectionTest extends ContentShellTestBase {
         assertWaitForSelectActionBarStatus(true);
         DOMUtils.longPressNode(this, mContentViewCore, "empty_textarea");
         assertWaitForSelectActionBarStatus(false);
+    }
+
+    @SmallTest
+    @Feature({"TextInput"})
+    public void testPastePopupNotShownOnLongPressingNonEmptyInput() throws Throwable {
+        copyStringToClipboard();
+        DOMUtils.longPressNode(this, mContentViewCore, "empty_input_text");
+        assertWaitForPastePopupStatus(true);
+        DOMUtils.longPressNode(this, mContentViewCore, "input_text");
+        assertWaitForSelectActionBarStatus(true);
+        assertWaitForPastePopupStatus(false);
+    }
+
+    @SmallTest
+    @Feature({"TextInput"})
+    public void testPastePopupClearedOnTappingEmptyInput() throws Throwable {
+        copyStringToClipboard();
+        DOMUtils.longPressNode(this, mContentViewCore, "empty_input_text");
+        assertWaitForPastePopupStatus(true);
+        DOMUtils.clickNode(this, mContentViewCore, "empty_input_text");
+        assertWaitForPastePopupStatus(false);
+    }
+
+    @SmallTest
+    @Feature({"TextInput"})
+    public void testPastePopupClearedOnTappingNonEmptyInput() throws Throwable {
+        copyStringToClipboard();
+        DOMUtils.longPressNode(this, mContentViewCore, "empty_input_text");
+        assertWaitForPastePopupStatus(true);
+        DOMUtils.clickNode(this, mContentViewCore, "input_text");
+        assertWaitForPastePopupStatus(false);
+    }
+
+    @SmallTest
+    @Feature({"TextInput"})
+    public void testPastePopupClearedOnTappingOutsideInput() throws Throwable {
+        copyStringToClipboard();
+        DOMUtils.longPressNode(this, mContentViewCore, "empty_input_text");
+        assertWaitForPastePopupStatus(true);
+        DOMUtils.clickNode(this, mContentViewCore, "plain_text_2");
+        assertWaitForPastePopupStatus(false);
+    }
+
+    @SmallTest
+    @Feature({"TextInput"})
+    public void testPastePopupClearedOnLongPressingOutsideInput() throws Throwable {
+        copyStringToClipboard();
+        DOMUtils.longPressNode(this, mContentViewCore, "empty_input_text");
+        assertWaitForPastePopupStatus(true);
+        DOMUtils.longPressNode(this, mContentViewCore, "plain_text_2");
+        assertWaitForPastePopupStatus(false);
+    }
+
+    private void copyStringToClipboard() {
+        ClipboardManager clipboardManager =
+                (ClipboardManager) getActivity().getSystemService(
+                        Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("test", "Text to copy");
+                clipboardManager.setPrimaryClip(clip);
+    }
+
+    private void assertWaitForPastePopupStatus(final boolean show) throws InterruptedException {
+        assertTrue(CriteriaHelper.pollForCriteria(new Criteria() {
+            @Override
+            public boolean isSatisfied() {
+                return show == mContentViewCore.getPastePopupForTest().isShowing();
+            }
+        }));
     }
 
     private void assertWaitForSelectActionBarStatus(
