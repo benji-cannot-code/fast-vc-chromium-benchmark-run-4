49ca2421f00662b2e541785f1b081a7711ed81e7
kbr@chromium.org <kbr@chromium.org>
2014-09-03 21:45:34 +0000

    Log ScheduledAction early-outs.
    BUG=407976
    
    Review URL: https://codereview.chromium.org/536093002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181341 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/DOMTimer.cpp
+++ third_party/WebKit/Source/core/frame/DOMTimer.cpp
@@ -138,7 +138,7 @@ void DOMTimer::fired()
                 augmentRepeatInterval(minimumInterval - repeatInterval());
         }
 
-        WTF_LOG(Timers, "DOMTimer::fired: m_timeoutID = %d, repeatInterval = %f", m_timeoutID, repeatInterval());
+        WTF_LOG(Timers, "DOMTimer::fired: m_timeoutID = %d, repeatInterval = %f, m_action = %p", m_timeoutID, repeatInterval(), m_action.get());
 
         // No access to member variables after this point, it can delete the timer.
         m_action->execute(context);
@@ -148,7 +148,7 @@ void DOMTimer::fired()
         return;
     }
 
-    WTF_LOG(Timers, "DOMTimer::fired: m_timeoutID = %d, one-shot", m_timeoutID);
+    WTF_LOG(Timers, "DOMTimer::fired: m_timeoutID = %d, one-shot, m_action = %p", m_timeoutID, m_action.get());
 
     // Delete timer before executing the action for one-shot timers.
     OwnPtr<ScheduledAction> action = m_action.release();
