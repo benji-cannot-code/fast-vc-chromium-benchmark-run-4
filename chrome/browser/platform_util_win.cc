55d709c24a613bc24aa03272584d6b941a3afe20
dcheng <dcheng@chromium.org>
2014-09-03 10:14:37 -0700

    content/ fixups for scoped_refptr operator T* removal.
    BUG=110610
    TBR=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/532833003
    
    Cr-Commit-Position: refs/heads/master@{#293159}

--- chrome/browser/platform_util_win.cc
+++ chrome/browser/platform_util_win.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_utility_messages.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/utility_process_host.h"
+#include "content/public/browser/utility_process_host_client.h"
 #include "ui/base/win/shell.h"
 #include "ui/gfx/native_widget_types.h"
 #include "url/gurl.h"
--- content/browser/utility_process_host_impl.cc
+++ content/browser/utility_process_host_impl.cc
@@ -87,8 +87,8 @@ class UtilitySandboxedProcessLauncherDelegate
 UtilityMainThreadFactoryFunction g_utility_main_thread_factory = NULL;
 
 UtilityProcessHost* UtilityProcessHost::Create(
-    UtilityProcessHostClient* client,
-    base::SequencedTaskRunner* client_task_runner) {
+    const scoped_refptr<UtilityProcessHostClient>& client,
+    const scoped_refptr<base::SequencedTaskRunner>& client_task_runner) {
   return new UtilityProcessHostImpl(client, client_task_runner);
 }
 
@@ -98,8 +98,8 @@ void UtilityProcessHostImpl::RegisterUtilityMainThreadFactory(
 }
 
 UtilityProcessHostImpl::UtilityProcessHostImpl(
-    UtilityProcessHostClient* client,
-    base::SequencedTaskRunner* client_task_runner)
+    const scoped_refptr<UtilityProcessHostClient>& client,
+    const scoped_refptr<base::SequencedTaskRunner>& client_task_runner)
     : client_(client),
       client_task_runner_(client_task_runner),
       is_batch_mode_(false),
