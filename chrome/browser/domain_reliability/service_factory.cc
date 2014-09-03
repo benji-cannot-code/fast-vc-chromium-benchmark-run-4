39e33e6567d016c3a8ebd5c0fb60f7447ec0bb17
ttuttle <ttuttle@chromium.org>
2014-09-03 16:56:15 -0700

    Domain Reliability: Don't create Service if metrics reporting disabled
    Given that the metrics reporting pref is labelled in the UI as requiring
    a restart to apply, this is a much simpler way of gating Domain
    Reliability uploads on it.
    
    We simply don't create the Service (so we don't collect data and upload
    it) if the pref is false.
    
    This will need to be revisited when we also start using the Monitor for
    JavaScript hooks (e.g. the Navigation Error Logging API).
    
    BUG=407170
    
    Review URL: https://codereview.chromium.org/525303004
    
    Cr-Commit-Position: refs/heads/master@{#293226}

--- chrome/browser/domain_reliability/service_factory.cc
+++ chrome/browser/domain_reliability/service_factory.cc
@@ -7,7 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/metrics/field_trial.h"
+#include "base/prefs/pref_service.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/common/chrome_switches.h"
+#include "chrome/common/pref_names.h"
 #include "components/domain_reliability/service.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "content/public/browser/browser_context.h"
@@ -56,6 +59,11 @@ KeyedService* DomainReliabilityServiceFactory::BuildServiceInstanceFor(
     content::BrowserContext* context) const {
   if (!IsDomainReliabilityMonitoringEnabled())
     return NULL;
+
+  if (!g_browser_process->local_state()->GetBoolean(
+          prefs::kMetricsReportingEnabled)) {
+    return NULL;
+  }
 
   return DomainReliabilityService::Create(
       kDomainReliabilityUploadReporterString);
