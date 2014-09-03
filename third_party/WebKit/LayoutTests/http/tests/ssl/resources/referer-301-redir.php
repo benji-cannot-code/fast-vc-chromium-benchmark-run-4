12959276fdd01a5fdda3c95eaedfd2dffcc4e76e
mkwst@chromium.org <mkwst@chromium.org>
2014-09-03 04:59:14 +0000

    Layout Tests nested under an 'https' directory load over HTTPS directly.
    Currently, we special-case tests under 'http/tests/ssl' to load directly
    over HTTPS rather than HTTP. This allows us to avoid a 'window.location'
    hop in the test itself, and is a generally useful ability when testing
    behavior of features when behavior differs between HTTPS and HTTP
    origins.
    
    We're not using this useful ability, however: there are three tests in
    that directory, two of which replicate tests under
    'http/tests/security' and 'http/tests/navigation'.
    
    This patch changes the special-case to include all tests with an
    'https' directory in their paths, which will allow us to put the HTTP
    and HTTPS versions of a feature's tests right next to each other.
    
    This change was (briefly) discussed on blink-dev at
    https://groups.google.com/a/chromium.org/d/msg/blink-dev/MqQFEjr43UQ/6ik0_OaFh1cJ
    
    Review URL: https://codereview.chromium.org/527283002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181281 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/ssl/resources/referer-301-redir.php
+++
@@ -1,6 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-<?php
-header("HTTP/1.1 301 Moved Permanently");
-header("Location: http://127.0.0.1:8000/ssl/resources/no-http-referer.cgi");
-header("Cache-Control: no-cache,no-store");
-?>
