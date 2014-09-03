7c34cd6d866035b1e6d21b1bebc6435eedb96277
nyquist <nyquist@chromium.org>
2014-09-02 17:08:20 -0700

    Revert of Add support for Java nano protocol buffers for Android. (patchset #4 id:60001 of https://codereview.chromium.org/511283003/)
    Reason for revert:
    Breaks some workflows and also should get new quota.
    
    Original issue's description:
    > Add support for Java nano protocol buffers for Android.
    >
    > This CL adds a new dependency on the protocol buffer compiler
    > from the android source tree, since this compiler supports
    > generating Java files using the nano runtime.
    >
    > The initial version of this dependency is 2.2.0a, but checked
    > out as what the Android 4.4.4 Release 2.0.1 tag points to.
    >
    > This CL adds a new protoc binary (for compiling protos) that supports
    > this, and also adds a Java library with the runtime.
    >
    > To simplify use of this, it also updates build/protoc_java.gypi to
    > support generating nano protos by specifying an optional
    > proto_runtime argument. The argument defaults to 'lite' which does
    > the same thing as before this change, and setting it to 'nano'
    > generates the new style Java files.
    >
    > The plan is to quickly deprecate the 'lite' runtime for Java, since
    > it is too big and uses too many methods.
    >
    > BUG=377891
    >
    > Committed: https://chromium.googlesource.com/chromium/src/+/d8ae0a79834531e41912de44b9e212ffa7eb2785
    
    TBR=cjhopman@chromium.org,darin@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=377891
    
    Review URL: https://codereview.chromium.org/534703004
    
    Cr-Commit-Position: refs/heads/master@{#293028}

--- build/protoc_java.py
+++ build/protoc_java.py
@@ -4,11 +4,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Generate java source files from protobuf files.
+"""Generate java source files from protobufs
 
 Usage:
-    protoc_java.py {protoc} {proto_path} {java_out} {proto_runtime} \
-        {stamp_file} {proto_files}
+    protoc_java.py {protoc} {proto_path} {java_out} {stamp_file} {proto_files}
 
 This is a helper file for the genproto_java action in protoc_java.gypi.
 
@@ -17,8 +16,6 @@ It performs the following steps:
 2. Creates source directory.
 3. Generates Java files using protoc.
 4. Creates a new stamp file.
-
-proto_runtime must be one of 'nano' and 'lite'.
 """
 
 import os
@@ -27,36 +24,27 @@ import subprocess
 import sys
 
 def main(argv):
-  if len(argv) < 6:
+  if len(argv) < 5:
     usage()
     return 1
 
-  protoc_path, proto_path, java_out, proto_runtime, stamp_file = argv[1:6]
-  proto_files = argv[6:]
+  protoc_path, proto_path, java_out, stamp_file = argv[1:5]
+  proto_files = argv[5:]
 
-  # Delete all old sources.
+  # Delete all old sources
   if os.path.exists(java_out):
     shutil.rmtree(java_out)
 
-  # Create source directory.
+  # Create source directory
   os.makedirs(java_out)
 
-  # Figure out which runtime to use.
-  if proto_runtime == 'nano':
-    out_arg = '--javanano_out=optional_field_style=reftypes,' + \
-        'store_unknown_fields=true:' + java_out
-  elif proto_runtime == 'lite':
-    out_arg = '--java_out=' + java_out
-  else:
-    usage()
-    return 1
-
-  # Generate Java files using protoc.
+  # Generate Java files using protoc
   ret = subprocess.call(
-      [protoc_path, '--proto_path', proto_path, out_arg] + proto_files)
+      [protoc_path, '--proto_path', proto_path, '--java_out', java_out]
+      + proto_files)
 
   if ret == 0:
-    # Create a new stamp file.
+    # Create a new stamp file
     with file(stamp_file, 'a'):
       os.utime(stamp_file, None)
 
