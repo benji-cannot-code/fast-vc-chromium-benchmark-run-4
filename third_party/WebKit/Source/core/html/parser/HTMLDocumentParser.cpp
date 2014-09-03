cda7eda17e37ba90fec64c41f581f7b62aa7b19a
kouhei@chromium.org <kouhei@chromium.org>
2014-09-03 22:35:56 +0000

    Progress HTMLDocumentParser state on non-interrupted cases
    Before this patch, HTMLDocumentParser never progressed to StoppedState
    but stuck in StoppingState when parsing finished without being interrupted.
    This patch adds a call to |DocumentParser::stopParsing()| to ensure its
    |m_state| progress to StoppedState.
    
    BUG=409461
    
    Review URL: https://codereview.chromium.org/532343002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181346 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/parser/HTMLDocumentParser.cpp
+++ third_party/WebKit/Source/core/html/parser/HTMLDocumentParser.cpp
@@ -829,6 +829,8 @@ void HTMLDocumentParser::end()
 
     // Informs the the rest of WebCore that parsing is really finished (and deletes this).
     m_treeBuilder->finished();
+
+    DocumentParser::stopParsing();
 }
 
 void HTMLDocumentParser::attemptToRunDeferredScriptsAndEnd()
