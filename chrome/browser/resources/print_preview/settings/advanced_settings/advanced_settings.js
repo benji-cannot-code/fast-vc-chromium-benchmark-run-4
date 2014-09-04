8e05e6bda26ea6b4eb180bc65545cecbf36733eb
alekseys <alekseys@chromium.org>
2014-09-03 22:25:18 -0700

    Export vendor capabilities to the CJT ticket.
    BUG=397741
    
    Review URL: https://codereview.chromium.org/540563002
    
    Cr-Commit-Position: refs/heads/master@{#293265}

--- chrome/browser/resources/print_preview/settings/advanced_settings/advanced_settings.js
+++ chrome/browser/resources/print_preview/settings/advanced_settings/advanced_settings.js
@@ -67,6 +67,11 @@ cr.define('print_preview', function() {
           this.cancel.bind(this));
 
       this.tracker.add(
+          this.getChildElement('#done-button'),
+          'click',
+          this.onApplySettings_.bind(this));
+
+      this.tracker.add(
           this.searchBox_,
           print_preview.SearchBox.EventType.SEARCH,
           this.onSearch_.bind(this));
@@ -94,6 +99,14 @@ cr.define('print_preview', function() {
     onCancelInternal: function() {
       this.metrics_.record(print_preview.Metrics.PrintSettingsUiBucket.
           ADVANCED_SETTINGS_DIALOG_CANCELED);
+    },
+
+    /** @override */
+    onEnterPressedInternal: function() {
+      var doneButton = this.getChildElement('#done-button');
+      if (!doneButton.disabled)
+        doneButton.click();
+      return !doneButton.disabled;
     },
 
     /**
@@ -137,11 +150,7 @@ cr.define('print_preview', function() {
       }.bind(this));
       this.items_ = [];
 
-      var vendorCapabilities =
-          this.destination_ &&
-          this.destination_.capabilities &&
-          this.destination_.capabilities.printer &&
-          this.destination_.capabilities.printer.vendor_capability;
+      var vendorCapabilities = this.printTicketStore_.vendorItems.capability;
       if (!vendorCapabilities)
         return;
 
@@ -166,6 +175,22 @@ cr.define('print_preview', function() {
      */
     onSearch_: function(evt) {
       this.filterLists_(evt.query);
+    },
+
+    /**
+     * Called when current settings selection need to be stored in the ticket.
+     * @private
+     */
+    onApplySettings_: function(evt) {
+      this.setIsVisible(false);
+
+      var values = {};
+      this.items_.forEach(function(item) {
+        if (item.isModified())
+          values[item.id] = item.selectedValue;
+      }.bind(this));
+
+      this.printTicketStore_.vendorItems.updateValue(values);
     }
   };
 
