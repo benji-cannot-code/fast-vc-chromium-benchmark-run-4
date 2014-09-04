db8a193cb52784071ff207c8ed2bfc1757e6b2d0
junov@chromium.org <junov@chromium.org>
2014-09-04 21:28:15 +0000

    Partial revert of r178392
    An unnecessary modification that was made in Blink r178392 was causing
    a regression in the rendering of the inspector timeline.  Pulling it out
    
    BUG=408080
    R=kbr@chromium.org
    
    Review URL: https://codereview.chromium.org/541783002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181400 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/compositing/CompositedLayerMapping.cpp
+++ third_party/WebKit/Source/core/rendering/compositing/CompositedLayerMapping.cpp
@@ -1174,6 +1174,7 @@ void CompositedLayerMapping::updateDrawsContent()
             Color bgColor(Color::transparent);
             if (contentLayerSupportsDirectBackgroundComposition(renderer())) {
                 bgColor = rendererBackgroundColor();
+                hasPaintedContent = false;
             }
             contentLayer->setBackgroundColor(bgColor.rgb());
         }
