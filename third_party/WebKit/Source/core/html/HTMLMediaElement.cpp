6750e3bac9d1d9c7e560c464d5a4ae3ba6676a7c
fs@opera.com <fs@opera.com>
2014-09-04 23:13:30 +0000

    Make markCaptionAndSubtitleTracksAsUnconfigured() do what the box say
    Sink the call to configureTextTracks() out of
    markCaptionAndSubtitleTracksAsUnconfigured() - this means the latter does
    what it advertises and nothing more.
    This also allows tightening the scope for when m_processingPreferenceChange
    is set to true.
    
    Review URL: https://codereview.chromium.org/541593004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181416 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLMediaElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLMediaElement.cpp
@@ -3550,8 +3550,9 @@ void HTMLMediaElement::setClosedCaptionsVisible(bool closedCaptionVisible)
 
     m_closedCaptionsVisible = closedCaptionVisible;
 
-    m_processingPreferenceChange = true;
     markCaptionAndSubtitleTracksAsUnconfigured();
+    m_processingPreferenceChange = true;
+    configureTextTracks();
     m_processingPreferenceChange = false;
 
     updateTextTrackDisplay();
@@ -3695,7 +3696,6 @@ void HTMLMediaElement::markCaptionAndSubtitleTracksAsUnconfigured()
         if (kind == TextTrack::subtitlesKeyword() || kind == TextTrack::captionsKeyword())
             textTrack->setHasBeenConfigured(false);
     }
-    configureTextTracks();
 }
 
 void* HTMLMediaElement::preDispatchEventHandler(Event* event)
