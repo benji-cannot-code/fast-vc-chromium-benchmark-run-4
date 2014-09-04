6b407ee159f6a2220517e06d8594d8322471c8bf
davidben <davidben@chromium.org>
2014-09-04 07:14:32 -0700

    Enable Certificate Transparency in the OpenSSL port.
    Also add tests that assert OCSP responses and SCT extensions are returned in
    the expected format.
    
    BUG=408687
    TEST=open https://embed.ct.digicert.com/ in Chrome for Android and
         tap the lock icon. Popup should say "The identity of this
         [...] and is publicly auditable."
    
    Review URL: https://codereview.chromium.org/517083002
    
    Cr-Commit-Position: refs/heads/master@{#293295}

--- net/socket/ssl_client_socket_openssl.h
+++ net/socket/ssl_client_socket_openssl.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/completion_callback.h"
 #include "net/base/io_buffer.h"
 #include "net/cert/cert_verify_result.h"
+#include "net/cert/ct_verify_result.h"
 #include "net/socket/client_socket_handle.h"
 #include "net/socket/ssl_client_socket.h"
 #include "net/ssl/channel_id_service.h"
@@ -35,6 +36,7 @@ typedef struct x509_store_ctx_st X509_STORE_CTX;
 namespace net {
 
 class CertVerifier;
+class CTVerifier;
 class SingleRequestCertVerifier;
 class SSLCertRequestInfo;
 class SSLInfo;
@@ -122,6 +124,7 @@ class SSLClientSocketOpenSSL : public SSLClientSocket {
   int DoVerifyCertComplete(int result);
   void DoConnectCallback(int result);
   X509Certificate* UpdateServerCert();
+  void VerifyCT();
 
   void OnHandshakeIOComplete(int result);
   void OnSendComplete(int result);
@@ -173,6 +176,13 @@ class SSLClientSocketOpenSSL : public SSLClientSocket {
   static void InfoCallback(const SSL* ssl, int type, int val);
 
   void CheckIfHandshakeFinished();
+
+  // Adds the SignedCertificateTimestamps from ct_verify_result_ to |ssl_info|.
+  // SCTs are held in three separate vectors in ct_verify_result, each
+  // vetor representing a particular verification state, this method associates
+  // each of the SCTs with the corresponding SCTVerifyStatus as it adds it to
+  // the |ssl_info|.signed_certificate_timestamps list.
+  void AddSCTInfoToSSLInfo(SSLInfo* ssl_info) const;
 
   bool transport_send_busy_;
   bool transport_recv_busy_;
@@ -232,6 +242,10 @@ class SSLClientSocketOpenSSL : public SSLClientSocket {
   CertVerifier* const cert_verifier_;
   scoped_ptr<SingleRequestCertVerifier> verifier_;
   base::TimeTicks start_cert_verification_time_;
+
+  // Certificate Transparency: Verifier and result holder.
+  ct::CTVerifyResult ct_verify_result_;
+  CTVerifier* cert_transparency_verifier_;
 
   // The service for retrieving Channel ID keys.  May be NULL.
   ChannelIDService* channel_id_service_;
