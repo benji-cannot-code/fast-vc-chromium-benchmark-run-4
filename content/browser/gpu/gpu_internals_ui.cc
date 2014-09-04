2958600490516d3dd31195e3ca130ee917840435
zmo <zmo@chromium.org>
2014-09-04 12:20:38 -0700

    Add WindowManager to about:gpu page on Linux/X11
    BUG=410560
    TEST=about:gpu page
    R=piman@chromium.org,davemoore@chromium.org,sadrul@chromium.org,derat@chromium.org
    
    Review URL: https://codereview.chromium.org/540643002
    
    Cr-Commit-Position: refs/heads/master@{#293322}

--- content/browser/gpu/gpu_internals_ui.cc
+++ content/browser/gpu/gpu_internals_ui.cc
@@ -35,6 +35,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/win/shell.h"
 #endif
 
+#if defined(OS_LINUX) && defined(USE_X11)
+#include "ui/base/x/x11_util.h"
+#endif
+
 namespace content {
 namespace {
 
@@ -162,6 +166,10 @@ base::DictionaryValue* GpuInfoAsDictionaryValue() {
                                              gpu_info.gl_ws_version));
   basic_info->Append(NewDescriptionValuePair("Window system binding extensions",
                                              gpu_info.gl_ws_extensions));
+#if defined(OS_LINUX) && defined(USE_X11)
+  basic_info->Append(NewDescriptionValuePair("Window manager",
+                                             ui::GuessWindowManagerName()));
+#endif
   std::string direct_rendering = gpu_info.direct_rendering ? "Yes" : "No";
   basic_info->Append(
       NewDescriptionValuePair("Direct rendering", direct_rendering));
