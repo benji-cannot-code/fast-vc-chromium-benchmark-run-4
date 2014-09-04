5f4c49546ca8ea39c22f9c54e1b7aac57c090295
dcheng <dcheng@chromium.org>
2014-09-03 21:22:38 -0700

    Fixups in components/ for scoped_refptr operator T* removal.
    BUG=110610
    TBR=rpaquay@chromium.org
    
    Review URL: https://codereview.chromium.org/536623003
    
    Cr-Commit-Position: refs/heads/master@{#293255}

--- components/feedback/feedback_uploader.h
+++ components/feedback/feedback_uploader.h
@@ -46,7 +46,8 @@ class FeedbackUploader : public base::SupportsWeakPtr<FeedbackUploader> {
   friend class FeedbackUploaderTest;
 
   struct ReportsUploadTimeComparator {
-    bool operator()(FeedbackReport* a, FeedbackReport* b) const;
+    bool operator()(const scoped_refptr<FeedbackReport>& a,
+                    const scoped_refptr<FeedbackReport>& b) const;
   };
 
   void Init();
