2a4e860a0c72aedb231d215b23f3496027c36164
jbauman <jbauman@chromium.org>
2014-09-04 10:22:49 -0700

    Add LatencyInfo support for Surfaces.
    BUG=
    
    Review URL: https://codereview.chromium.org/539783002
    
    Cr-Commit-Position: refs/heads/master@{#293314}

--- cc/surfaces/surface.h
+++ cc/surfaces/surface.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CC_SURFACES_SURFACE_H_
 #define CC_SURFACES_SURFACE_H_
 
+#include <vector>
+
 #include "base/callback.h"
 #include "base/containers/hash_tables.h"
 #include "base/macros.h"
@@ -13,6 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/surfaces/surface_id.h"
 #include "cc/surfaces/surfaces_export.h"
 #include "ui/gfx/size.h"
+
+namespace ui {
+struct LatencyInfo;
+}
 
 namespace cc {
 class CompositorFrame;
@@ -38,6 +44,7 @@ class CC_SURFACES_EXPORT Surface {
   // Returns a number that increments by 1 every time a new frame is enqueued.
   int frame_index() const { return frame_index_; }
 
+  void TakeLatencyInfo(std::vector<ui::LatencyInfo>* latency_info);
   void RunDrawCallbacks();
 
   SurfaceFactory* factory() { return factory_; }
