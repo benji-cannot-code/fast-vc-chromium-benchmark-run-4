d83e0a32c73f81dc50358a1c3987b578af9c6ced
Andrew Scherkus <scherkus@chromium.org>
2014-09-04 14:43:39 -0700

    Have AudioRendererImpl advance time until it's told to stop.
    This removes the hacky fall-back-to-interpolated-time code when audio
    ends before video. Considering we're still getting audio render
    callbacks during this time, we can simply account for how much
    silence we've written to compute the media time.
    
    More importantly, this brings AudioRendererImpl's TimeSource
    implementation in line with how WallClockTimeSource operates and gets
    us one step closer to removing TimeDeltaInterpolator entirely.
    
    BUG=370634
    R=dalecurtis@chromium.org, xhwang@chromium.org
    
    Review URL: https://codereview.chromium.org/518613002
    
    Cr-Commit-Position: refs/heads/master@{#293343}

--- media/filters/renderer_impl.cc
+++ media/filters/renderer_impl.cc
@@ -502,14 +502,6 @@ void RendererImpl::OnAudioRendererEnded() {
   DCHECK(!audio_ended_);
   audio_ended_ = true;
 
-  // Start clock since there is no more audio to trigger clock updates.
-  {
-    base::TimeDelta duration = get_duration_cb_.Run();
-    base::AutoLock auto_lock(interpolator_lock_);
-    interpolator_->SetUpperBound(duration);
-    StartClockIfWaitingForTimeUpdate_Locked();
-  }
-
   RunEndedCallbackIfNeeded();
 }
 
@@ -537,10 +529,8 @@ void RendererImpl::RunEndedCallbackIfNeeded() {
     return;
 
   {
-    base::TimeDelta duration = get_duration_cb_.Run();
     base::AutoLock auto_lock(interpolator_lock_);
     PauseClockAndStopTicking_Locked();
-    interpolator_->SetBounds(duration, duration);
   }
 
   ended_cb_.Run();
