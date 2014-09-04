2446b27d60c5a826c9b59bc03e33285db4488b0d
Justin TerAvest <teravest@chromium.org>
2014-09-04 08:41:18 -0600

    NaCl: Detect plugin crashes via EOF on Chromium IPC.
    This change uses the OnChannelError() callback in TrustedPluginChannel to
    detect that the channel between the renderer and NaCl processes has closed.
    Previously, closure of the SRPC channel between those two processes was used to
    indicate a plugin crash.
    
    I wasn't sure at first that OnChannelError() would be called, but it appears to
    be called reliably in the testing that I've done.
    
    This change removes a call to ReportCrash() in ServiceRuntime::ReapLogs() since
    ReportCrash() has become a no-op.
    
    This change reenables the NaClBrowserTest*.Crash tests on Linux to ensure that
    there's good coverage-- they seem to pass.
    
    TEST=NaClBrowserTest*.Crash
    BUG=366334,401105
    R=mseaborn@chromium.org
    
    Review URL: https://codereview.chromium.org/512323002
    
    Cr-Commit-Position: refs/heads/master@{#293297}

--- ppapi/c/private/ppb_nacl_private.h
+++ ppapi/c/private/ppb_nacl_private.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * found in the LICENSE file.
  */
 
-/* From private/ppb_nacl_private.idl modified Wed Aug 27 13:55:13 2014. */
+/* From private/ppb_nacl_private.idl modified Thu Sep  4 07:46:02 2014. */
 
 #ifndef PPAPI_C_PRIVATE_PPB_NACL_PRIVATE_H_
 #define PPAPI_C_PRIVATE_PPB_NACL_PRIVATE_H_
@@ -305,8 +305,6 @@ struct PPB_NaCl_Private_1_0 {
                           const char* error_message);
   /* Reports that loading a nexe was aborted. */
   void (*ReportLoadAbort)(PP_Instance instance);
-  /* Reports that the nexe has crashed. */
-  void (*NexeDidCrash)(PP_Instance instance);
   /* Performs internal setup when an instance is created. */
   void (*InstanceCreated)(PP_Instance instance);
   /* Performs internal cleanup when an instance is destroyed. */
