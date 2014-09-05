0b6c88339a3acc8ba0f2a2eb8f689b8d810a11e5
oshima <oshima@chromium.org>
2014-09-04 18:55:37 -0700

    Use stored render_frame_id_ instead of using GetRenderFrameHost(), as it may return NULL
    BUG=402028
    R=lazyboy@chromium.org
    
    Review URL: https://codereview.chromium.org/541753003
    
    Cr-Commit-Position: refs/heads/master@{#293398}

--- components/renderer_context_menu/render_view_context_menu_base.h
+++ components/renderer_context_menu/render_view_context_menu_base.h
@@ -167,6 +167,9 @@ class RenderViewContextMenuBase : public ui::SimpleMenuModel::Delegate,
 
   ui::SimpleMenuModel menu_model_;
 
+  // Renderer's frame id.
+  int render_frame_id_;
+
   // Our observers.
   mutable ObserverList<RenderViewContextMenuObserver> observers_;
 
@@ -181,7 +184,6 @@ class RenderViewContextMenuBase : public ui::SimpleMenuModel::Delegate,
 
   // The RenderFrameHost's IDs.
   int render_process_id_;
-  int render_frame_id_;
 
   scoped_ptr<ToolkitDelegate> toolkit_delegate_;
 
