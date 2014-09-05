3c1c01b9e2ee23b2f7b5934802f100c907d7405b
estade <estade@chromium.org>
2014-09-04 19:16:05 -0700

    Remove more label for= use.
    BUG=409938
    
    Review URL: https://codereview.chromium.org/541533002
    
    Cr-Commit-Position: refs/heads/master@{#293403}

--- chrome/browser/resources/options/browser_options.js
+++ chrome/browser/resources/options/browser_options.js
@@ -317,11 +317,6 @@ cr.define('options', function() {
           chrome.send('coreOptionsUserMetricsAction',
               ['Options_ManageAccounts']);
         };
-
-        document.querySelector(
-            '#enable-screen-lock + span > .controlled-setting-indicator').
-            setAttribute('textshared',
-                         loadTimeData.getString('screenLockShared'));
       } else {
         $('import-data').onclick = function(event) {
           ImportDataOverlay.show();
