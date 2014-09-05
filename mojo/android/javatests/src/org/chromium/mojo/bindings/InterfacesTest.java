70d1db05dbdae9ae60fa81fb4df10be12b0141f7
qsr <qsr@chromium.org>
2014-09-05 03:47:00 -0700

    mojo: Java bindings: Add name to the manager.
    The name is used to retrieve a service from a ServiceProvider.
    
    R=ppi@chromium.org
    
    Review URL: https://codereview.chromium.org/540913003
    
    Cr-Commit-Position: refs/heads/master@{#293499}

--- mojo/android/javatests/src/org/chromium/mojo/bindings/InterfacesTest.java
+++ mojo/android/javatests/src/org/chromium/mojo/bindings/InterfacesTest.java
@@ -278,6 +278,11 @@ public class InterfacesTest extends MojoTestCase {
     }
 
     @SmallTest
+    public void testName() {
+        assertEquals("sample::NamedObject", NamedObject.MANAGER.getName());
+    }
+
+    @SmallTest
     public void testProxyAndStub() {
         MockNamedObjectImpl impl = new MockNamedObjectImpl();
         NamedObject.Proxy proxy =
