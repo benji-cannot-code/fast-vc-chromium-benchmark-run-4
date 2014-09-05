dac376406a2f9e994eea9e42d0f9f3328d51249a
scherkus <scherkus@chromium.org>
2014-09-05 13:58:05 -0700

    Protect access to Pipeline::renderer_ with a lock.
    During teardown it's possible to race calling Renderer::GetMediaTime()
    on the main thread with ~Renderer() on the media thread.
    
    This was introduced in be9da705e341863169faeff532c24c568fad2852 by
    moving the TimeDeltaInterpolator (which used to never get destroyed)
    into Renderer (which does get destroyed during error/stop).
    
    BUG=411320
    
    Review URL: https://codereview.chromium.org/545943004
    
    Cr-Commit-Position: refs/heads/master@{#293577}

--- media/base/pipeline.cc
+++ media/base/pipeline.cc
@@ -159,12 +159,11 @@ void Pipeline::SetVolume(float volume) {
 }
 
 TimeDelta Pipeline::GetMediaTime() const {
+  base::AutoLock auto_lock(lock_);
   if (!renderer_)
     return TimeDelta();
 
   TimeDelta media_time = renderer_->GetMediaTime();
-
-  base::AutoLock auto_lock(lock_);
   return std::min(media_time, duration_);
 }
 
@@ -407,7 +406,10 @@ void Pipeline::DoStop(const PipelineStatusCB& done_cb) {
   DCHECK(task_runner_->BelongsToCurrentThread());
   DCHECK(!pending_callbacks_.get());
 
-  renderer_.reset();
+  {
+    base::AutoLock auto_lock(lock_);
+    renderer_.reset();
+  }
   text_renderer_.reset();
 
   if (demuxer_) {
@@ -671,7 +673,10 @@ void Pipeline::InitializeRenderer(const base::Closure& done_cb) {
 
   if (!demuxer_->GetStream(DemuxerStream::AUDIO) &&
       !demuxer_->GetStream(DemuxerStream::VIDEO)) {
-    renderer_.reset();
+    {
+      base::AutoLock auto_lock(lock_);
+      renderer_.reset();
+    }
     OnError(PIPELINE_ERROR_COULD_NOT_RENDER);
     return;
   }
