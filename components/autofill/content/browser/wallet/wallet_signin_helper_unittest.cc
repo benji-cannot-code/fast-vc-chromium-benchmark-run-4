0a6f35e05a887fbb8a29a9ebb0a0f034a856d500
bartfab <bartfab@chromium.org>
2014-09-05 08:07:28 -0700

    Fix copying of SAML IdP cookies on subsequent logins
    The code that copies cookies set by a SAML IdP on subsequent logins is
    wrong as it uses a method which modifies the cookie being copied by
    prepending a dot to the domain name. This CL fixes the problem by
    switching to the method that is used during initial login. The method
    is also renamed to make it clearer that it may be used on subsequent
    logins too.
    
    BUG=410416
    TEST=unit_tests updated to detect this problem
    
    TBR=dbeam (wallet_signin_helper_unittest.cc)
    
    Review URL: https://codereview.chromium.org/538543003
    
    Cr-Commit-Position: refs/heads/master@{#293524}

--- components/autofill/content/browser/wallet/wallet_signin_helper_unittest.cc
+++ components/autofill/content/browser/wallet/wallet_signin_helper_unittest.cc
@@ -145,7 +145,7 @@ TEST_F(WalletSigninHelperTest, GetWalletCookieValueWhenPresent) {
 
   net::CookieList cookie_list;
   cookie_list.push_back(*cookie);
-  cookie_monster->InitializeFrom(cookie_list);
+  cookie_monster->ImportCookies(cookie_list);
   request_context_->GetURLRequestContext()
       ->set_cookie_store(cookie_monster);
   signin_helper_->StartWalletCookieValueFetch();
@@ -167,7 +167,7 @@ TEST_F(WalletSigninHelperTest, GetWalletCookieValueWhenMissing) {
 
   net::CookieList cookie_list;
   cookie_list.push_back(*cookie);
-  cookie_monster->InitializeFrom(cookie_list);
+  cookie_monster->ImportCookies(cookie_list);
   request_context_->GetURLRequestContext()
       ->set_cookie_store(cookie_monster);
   signin_helper_->StartWalletCookieValueFetch();
--- net/cookies/cookie_monster.cc
+++ net/cookies/cookie_monster.cc
@@ -1102,7 +1102,7 @@ bool CookieMonster::SetCookieWithDetails(const GURL& url,
   return SetCanonicalCookie(&cc, creation_time, options);
 }
 
-bool CookieMonster::InitializeFrom(const CookieList& list) {
+bool CookieMonster::ImportCookies(const CookieList& list) {
   base::AutoLock autolock(lock_);
   InitIfNecessary();
   for (net::CookieList::const_iterator iter = list.begin();
