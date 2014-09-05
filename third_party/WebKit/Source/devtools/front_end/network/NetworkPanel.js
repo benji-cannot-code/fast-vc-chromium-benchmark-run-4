b05897e3bfcd2105d0d107901d5a228f713f15c8
eustas@chromium.org <eustas@chromium.org>
2014-09-05 09:09:05 +0000

    DevTools: NetworkPanel: Add scheme auto-filter.
    BUG=410988
    
    Review URL: https://codereview.chromium.org/541913004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181455 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/network/NetworkPanel.js
+++ third_party/WebKit/Source/devtools/front_end/network/NetworkPanel.js
@@ -102,6 +102,7 @@ WebInspector.NetworkLogView.FilterType = {
     HasResponseHeader: "HasResponseHeader",
     Method: "Method",
     MimeType: "MimeType",
+    Scheme: "Scheme",
     SetCookieDomain: "SetCookieDomain",
     SetCookieName: "SetCookieName",
     SetCookieValue: "SetCookieValue",
@@ -909,6 +910,7 @@ WebInspector.NetworkLogView.prototype = {
         this._suggestionBuilder.addItem(WebInspector.NetworkLogView.FilterType.Domain, request.domain);
         this._suggestionBuilder.addItem(WebInspector.NetworkLogView.FilterType.Method, request.requestMethod);
         this._suggestionBuilder.addItem(WebInspector.NetworkLogView.FilterType.MimeType, request.mimeType);
+        this._suggestionBuilder.addItem(WebInspector.NetworkLogView.FilterType.Scheme, "" + request.scheme);
         this._suggestionBuilder.addItem(WebInspector.NetworkLogView.FilterType.StatusCode, "" + request.statusCode);
 
         var responseHeaders = request.responseHeaders;
@@ -1487,6 +1489,9 @@ WebInspector.NetworkLogView.prototype = {
         case WebInspector.NetworkLogView.FilterType.MimeType:
             return WebInspector.NetworkLogView._requestMimeTypeFilter.bind(null, value);
 
+        case WebInspector.NetworkLogView.FilterType.Scheme:
+            return WebInspector.NetworkLogView._requestSchemeFilter.bind(null, value);
+
         case WebInspector.NetworkLogView.FilterType.SetCookieDomain:
             return WebInspector.NetworkLogView._requestSetCookieDomainFilter.bind(null, value);
 
@@ -1715,6 +1720,16 @@ WebInspector.NetworkLogView._requestMethodFilter = function(value, request)
 WebInspector.NetworkLogView._requestMimeTypeFilter = function(value, request)
 {
     return request.mimeType === value;
+}
+
+/**
+ * @param {string} value
+ * @param {!WebInspector.NetworkRequest} request
+ * @return {boolean}
+ */
+WebInspector.NetworkLogView._requestSchemeFilter = function(value, request)
+{
+    return request.scheme === value;
 }
 
 /**
