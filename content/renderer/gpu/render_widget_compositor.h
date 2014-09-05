11620771f286910be355a13b91efcde9fb0addac
skyostil <skyostil@chromium.org>
2014-09-05 01:22:23 -0700

    Pass more frame timing information to Blink
    This patch conveys more information about frame timings to Blink.
    Specifically it adds the current deadline by which work should be
    finished as well as the expected interval between frames. The
    Blink scheduler will use this information to schedule lower priority
    work.
    
    We also notify Blink when a committed frame actually got composited,
    which means the current frame is done and lower priority work can be
    started immediately (i.e., before the scheduled deadline).
    
    BUG=391005
    
    Review URL: https://codereview.chromium.org/521613002
    
    Cr-Commit-Position: refs/heads/master@{#293476}

--- content/renderer/gpu/render_widget_compositor.h
+++ content/renderer/gpu/render_widget_compositor.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/trees/layer_tree_host_client.h"
 #include "cc/trees/layer_tree_host_single_thread_client.h"
 #include "cc/trees/layer_tree_settings.h"
+#include "content/common/content_export.h"
 #include "third_party/WebKit/public/platform/WebLayerTreeView.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/gfx/rect.h"
@@ -33,9 +34,10 @@ class LayerTreeHost;
 namespace content {
 class RenderWidget;
 
-class RenderWidgetCompositor : public blink::WebLayerTreeView,
-                               public cc::LayerTreeHostClient,
-                               public cc::LayerTreeHostSingleThreadClient {
+class CONTENT_EXPORT RenderWidgetCompositor
+    : NON_EXPORTED_BASE(public blink::WebLayerTreeView),
+      NON_EXPORTED_BASE(public cc::LayerTreeHostClient),
+      NON_EXPORTED_BASE(public cc::LayerTreeHostSingleThreadClient) {
  public:
   // Attempt to construct and initialize a compositor instance for the widget
   // with the given settings. Returns NULL if initialization fails.
