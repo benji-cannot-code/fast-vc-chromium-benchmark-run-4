4ff4705f7cb479f4f3b7fabb670eef709416c0d1
vollick@chromium.org <vollick@chromium.org>
2014-09-05 23:05:15 +0000

    Opt out of composited scrolling with border radius.
    This currently causes correctness and memory issues due to the way the
    compositor handles masking out the corners. Let's punt on this for now.
    
    BUG=409984,365828
    
    Review URL: https://codereview.chromium.org/544183002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181484 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderLayerScrollableArea.cpp
+++ third_party/WebKit/Source/core/rendering/RenderLayerScrollableArea.cpp
@@ -1462,7 +1462,8 @@ static bool layerNeedsCompositedScrolling(const RenderLayer* layer)
     return layer->scrollsOverflow()
         && layer->compositor()->preferCompositingToLCDTextEnabled()
         && !layer->hasDescendantWithClipPath()
-        && !layer->hasAncestorWithClipPath();
+        && !layer->hasAncestorWithClipPath()
+        && !layer->renderer()->style()->hasBorderRadius();
 }
 
 void RenderLayerScrollableArea::updateNeedsCompositedScrolling()
