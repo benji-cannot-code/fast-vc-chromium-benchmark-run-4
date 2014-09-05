bc3598855cfb73125a5c96055af1c97443afd9ab
keishi@chromium.org <keishi@chromium.org>
2014-09-05 05:31:37 +0000

    Oilpan: Prepare moving InspectorResourceContentLoader to Oilpan
    BUG=340522
    
    Review URL: https://codereview.chromium.org/527183002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181437 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorPageAgent.h
+++ third_party/WebKit/Source/core/inspector/InspectorPageAgent.h
@@ -151,6 +151,7 @@ public:
     virtual void setFrontend(InspectorFrontend*) OVERRIDE;
     virtual void clearFrontend() OVERRIDE;
     virtual void restore() OVERRIDE;
+    virtual void discardAgent() OVERRIDE;
 
     // Cross-agents API
     Page* page() { return m_page; }
@@ -220,7 +221,7 @@ private:
     double m_embedderFontScaleFactor;
     bool m_embedderPreferCompositingToLCDTextEnabled;
 
-    OwnPtr<InspectorResourceContentLoader> m_inspectorResourceContentLoader;
+    OwnPtrWillBeMember<InspectorResourceContentLoader> m_inspectorResourceContentLoader;
     HashMap<String, String> m_editedResourceContent;
 };
 
