99a6145bfe159c1c64f13d53d1994f431a1e000a
zerny@chromium.org <zerny@chromium.org>
2014-09-05 08:22:27 +0000

    Oilpan: fix android build after r180901
    TBR=keishi@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/545133002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181453 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/forms/BaseChooserOnlyDateAndTimeInputType.h
+++ third_party/WebKit/Source/core/html/forms/BaseChooserOnlyDateAndTimeInputType.h
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class BaseChooserOnlyDateAndTimeInputType : public BaseDateAndTimeInputType, public DateTimeChooserClient {
-    WILL_BE_USING_GARBAGE_COLLECTED_MIXIN(BaseChooserOnlyDateAndTimeInputType);
 protected:
     BaseChooserOnlyDateAndTimeInputType(HTMLInputElement& element) : BaseDateAndTimeInputType(element) { }
     virtual ~BaseChooserOnlyDateAndTimeInputType();
--- third_party/WebKit/Source/web/ExternalDateTimeChooser.cpp
+++ third_party/WebKit/Source/web/ExternalDateTimeChooser.cpp
@@ -64,7 +64,7 @@ private:
         delete this;
     }
 
-    RefPtrWillBePersistent<ExternalDateTimeChooser> m_chooser;
+    RefPtr<ExternalDateTimeChooser> m_chooser;
 };
 
 ExternalDateTimeChooser::~ExternalDateTimeChooser()
