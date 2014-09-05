631c815ca218b70505aa06bfa79ba2c95644c336
vitalybuka <vitalybuka@chromium.org>
2014-09-04 18:24:29 -0700

    Use SetAlternateDesktop(false) for service process. Service process may run as windows service and it fails to create a window station.
    BUG=410237
    
    Review URL: https://codereview.chromium.org/534413002
    
    Cr-Commit-Position: refs/heads/master@{#293391}

--- chrome/service/service_utility_process_host.cc
+++ chrome/service/service_utility_process_host.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/sandbox_init.h"
 #include "content/public/common/sandboxed_process_launcher_delegate.h"
 #include "printing/emf_win.h"
+#include "sandbox/win/src/sandbox_policy_base.h"
 
 namespace {
 
@@ -48,6 +49,13 @@ class ServiceSandboxedProcessLauncherDelegate
   virtual void PreSandbox(bool* disable_default_policy,
                           base::FilePath* exposed_dir) OVERRIDE {
     *exposed_dir = exposed_dir_;
+  }
+
+  virtual void PreSpawnTarget(sandbox::TargetPolicy* policy,
+                              bool* success) OVERRIDE {
+    // Service process may run as windows service and it fails to create a
+    // window station.
+    policy->SetAlternateDesktop(false);
   }
 
  private:
