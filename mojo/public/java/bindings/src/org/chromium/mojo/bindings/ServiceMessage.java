d6c126201c847b56935e9aeb7da060414344f58d
qsr <qsr@chromium.org>
2014-09-05 02:55:16 -0700

    mojo: java bindings rename Message subclasses.
    R=ppi@chromium.org
    
    Review URL: https://codereview.chromium.org/526303003
    
    Cr-Commit-Position: refs/heads/master@{#293485}

--- mojo/public/java/bindings/src/org/chromium/mojo/bindings/ServiceMessage.java
+++ mojo/public/java/bindings/src/org/chromium/mojo/bindings/ServiceMessage.java
@@ -5,19 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.mojo.bindings;
 
-import org.chromium.mojo.system.Handle;
-
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
-import java.util.List;
 
 /**
  * Represents a {@link Message} which contains a {@link MessageHeader}. Deals with parsing the
  * {@link MessageHeader} for a message.
  */
-public class MessageWithHeader implements Message {
+public class ServiceMessage extends Message {
 
-    private final Message mBaseMessage;
     private final MessageHeader mHeader;
     private Message mPayload;
 
@@ -25,9 +21,9 @@ public class MessageWithHeader implements Message {
      * Reinterpret the given |message| as a message with the given |header|. The |message| must
      * contain the |header| as the start of its raw data.
      */
-    public MessageWithHeader(Message baseMessage, MessageHeader header) {
+    public ServiceMessage(Message baseMessage, MessageHeader header) {
+        super(baseMessage.getData(), baseMessage.getHandles());
         assert header.equals(new org.chromium.mojo.bindings.MessageHeader(baseMessage));
-        this.mBaseMessage = baseMessage;
         this.mHeader = header;
     }
 
@@ -35,31 +31,15 @@ public class MessageWithHeader implements Message {
      * Reinterpret the given |message| as a message with a header. The |message| must contain a
      * header as the start of it's raw data, which will be parsed by this constructor.
      */
-    MessageWithHeader(Message baseMessage) {
+    ServiceMessage(Message baseMessage) {
         this(baseMessage, new org.chromium.mojo.bindings.MessageHeader(baseMessage));
     }
 
     /**
-     * @see Message#getData()
+     * @see Message#asServiceMessage()
      */
     @Override
-    public ByteBuffer getData() {
-        return mBaseMessage.getData();
-    }
-
-    /**
-     * @see Message#getHandles()
-     */
-    @Override
-    public List<? extends Handle> getHandles() {
-        return mBaseMessage.getHandles();
-    }
-
-    /**
-     * @see Message#asMojoMessage()
-     */
-    @Override
-    public MessageWithHeader asMojoMessage() {
+    public ServiceMessage asServiceMessage() {
         return this;
     }
 
@@ -76,10 +56,10 @@ public class MessageWithHeader implements Message {
      */
     public Message getPayload() {
         if (mPayload == null) {
-            ByteBuffer truncatedBuffer = ((ByteBuffer) mBaseMessage.getData().position(
-                    getHeader().getSize())).slice();
+            ByteBuffer truncatedBuffer =
+                    ((ByteBuffer) getData().position(getHeader().getSize())).slice();
             truncatedBuffer.order(ByteOrder.nativeOrder());
-            mPayload = new SimpleMessage(truncatedBuffer, mBaseMessage.getHandles());
+            mPayload = new Message(truncatedBuffer, getHandles());
         }
         return mPayload;
     }
@@ -88,7 +68,7 @@ public class MessageWithHeader implements Message {
      * Set the request identifier on the message.
      */
     void setRequestId(long requestId) {
-        mHeader.setRequestId(mBaseMessage.getData(), requestId);
+        mHeader.setRequestId(getData(), requestId);
     }
 
 }
