076ec79acb2c96b4f2bd12aa9a516fabd343fde1
Sadrul Habib Chowdhury <sadrul@chromium.org>
2014-09-05 21:19:39 -0400

    athena: Make sure the window captions update correctly for theme-color.
    Make sure the Activity is updated when the theme-color changes. Also, update the
    background color from UpdateWindowIcon() override, instead of from UpdateWindowTitle(),
    since the latter doesn't get called if the title didn't actually change, and so
    just changing the color doesn't actually update the view.
    
    BUG=none
    R=oshima@chromium.org
    
    Review URL: https://codereview.chromium.org/549593002
    
    Cr-Commit-Position: refs/heads/master@{#293600}

--- athena/content/web_activity.cc
+++ athena/content/web_activity.cc
@@ -478,6 +478,7 @@ void WebActivity::DidUpdateFaviconURL(
 
 void WebActivity::DidChangeThemeColor(SkColor theme_color) {
   title_color_ = theme_color;
+  ActivityManager::Get()->UpdateActivity(this);
 }
 
 void WebActivity::MakeVisible() {
