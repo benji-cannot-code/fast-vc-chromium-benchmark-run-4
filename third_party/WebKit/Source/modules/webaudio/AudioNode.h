f8cc4cfe9e02e30b72ad0d2ddd27bd761b874d09
rtoy@chromium.org <rtoy@chromium.org>
2014-09-08 19:55:49 +0000

    Defer changes to the channel count mode to the pre or post-render phase
    In some situations, the channel count mode is updated just before the
    audio rendering starts. During audio processing, the channel count
    mode is used to set the number of channels for processing.  However,
    for AudioBasicProcessor's, the number of kernels has not yet been
    updated to the correct number of channels, so the incorrect number of
    channels is accessed.
    
    We delay the setting of the channel count mode until the pre or post
    rendering phase of the audio thread where it is safe to change things
    since the audio graph is not being pulled.
    
    BUG=407489
    
    Review URL: https://codereview.chromium.org/543073004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181573 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/webaudio/AudioNode.h
+++ third_party/WebKit/Source/modules/webaudio/AudioNode.h
@@ -184,6 +184,8 @@ public:
     virtual const AtomicString& interfaceName() const OVERRIDE FINAL;
     virtual ExecutionContext* executionContext() const OVERRIDE FINAL;
 
+    void updateChannelCountMode();
+
     virtual void trace(Visitor*) OVERRIDE;
 
 protected:
@@ -220,6 +222,9 @@ private:
 #endif
     static unsigned s_instanceCount;
 
+    // The new channel count mode that will be used to set the actual mode in the pre or post
+    // rendering phase.
+    ChannelCountMode m_newChannelCountMode;
 protected:
     unsigned m_channelCount;
     ChannelCountMode m_channelCountMode;
