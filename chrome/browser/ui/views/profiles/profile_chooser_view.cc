ba02c2f357a0f1b83138869420bcccd7591d577f
noms <noms@chromium.org>
2014-09-08 12:25:05 -0700

    Don't focus a profile by default in the User Manager
    When switching users, we used to focus the profile that initiated the
    action. This causes weird skip-a-roos if you try to tab to a different
    pod before the forced focus completes, so don't focus any pods by default.
    
    BUG=410718
    TEST=Start Chrome with --enable-new-menu. Click on "Switch Person". The
    User Manager should come up, no pods should be focused, and if you try
    to left arrow or right arrow really fast, the pods shouldn't jump focus.
    
    Review URL: https://codereview.chromium.org/546953002
    
    Cr-Commit-Position: refs/heads/master@{#293767}

--- chrome/browser/ui/views/profiles/profile_chooser_view.cc
+++ chrome/browser/ui/views/profiles/profile_chooser_view.cc
@@ -711,7 +711,7 @@ void ProfileChooserView::ButtonPressed(views::Button* sender,
     if (browser_->profile()->IsGuestSession())
       profiles::CloseGuestProfileWindows();
     else
-      chrome::ShowUserManager(browser_->profile()->GetPath());
+      chrome::ShowUserManager(base::FilePath());
     PostActionPerformed(ProfileMetrics::PROFILE_DESKTOP_MENU_OPEN_USER_MANAGER);
   } else if (sender == go_incognito_button_) {
     DCHECK(ShouldShowGoIncognito());
@@ -759,7 +759,7 @@ void ProfileChooserView::ButtonPressed(views::Button* sender,
   } else if (sender == add_person_button_) {
     ProfileMetrics::LogProfileNewAvatarMenuNotYou(
         ProfileMetrics::PROFILE_AVATAR_MENU_NOT_YOU_ADD_PERSON);
-    chrome::ShowUserManager(browser_->profile()->GetPath());
+    chrome::ShowUserManager(base::FilePath());
   } else if (sender == disconnect_button_) {
     ProfileMetrics::LogProfileNewAvatarMenuNotYou(
         ProfileMetrics::PROFILE_AVATAR_MENU_NOT_YOU_DISCONNECT);
