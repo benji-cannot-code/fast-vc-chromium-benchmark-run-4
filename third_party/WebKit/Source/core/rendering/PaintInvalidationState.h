5b86e1a6a0b416e1468149079f01327e8b3c2230
wangxianzhu@chromium.org <wangxianzhu@chromium.org>
2014-09-08 22:09:30 +0000

    Remove unused PaintInvalidationState::renderer()
    This might be left from historical versions. Remove.
    
    Review URL: https://codereview.chromium.org/550363003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181580 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/PaintInvalidationState.h
+++ third_party/WebKit/Source/core/rendering/PaintInvalidationState.h
@@ -35,7 +35,6 @@ public:
     void setForceCheckForPaintInvalidation() { m_forceCheckForPaintInvalidation = true; }
 
     const RenderLayerModelObject& paintInvalidationContainer() const { return m_paintInvalidationContainer; }
-    const RenderObject& renderer() const { return m_renderer; }
 
     bool canMapToContainer(const RenderLayerModelObject* container) const
     {
@@ -56,8 +55,6 @@ private:
     LayoutSize m_paintOffset;
 
     const RenderLayerModelObject& m_paintInvalidationContainer;
-
-    const RenderObject& m_renderer;
 };
 
 } // namespace blink
