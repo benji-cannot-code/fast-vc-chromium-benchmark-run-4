c1e169cc4ed7ecdbb6b49731e1cd23647dc15042
Brett Wilson <brettw@chromium.org>
2014-09-08 16:14:44 -0700

    Allow GN template collisions if they're the same.
    This makes templates behave like values when merging and duplicates are found.
    If the values are the same, allow them to collide. This happens when importing
    if multiple files import the same .gni file.
    
    R=scottmg@chromium.org
    
    Review URL: https://codereview.chromium.org/547063004
    
    Cr-Commit-Position: refs/heads/master@{#293809}

--- tools/gn/scope_unittest.cc
+++ tools/gn/scope_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "tools/gn/input_file.h"
 #include "tools/gn/parse_tree.h"
 #include "tools/gn/scope.h"
+#include "tools/gn/template.h"
 #include "tools/gn/test_with_scope.h"
 
 namespace {
@@ -35,10 +36,19 @@ TEST(Scope, NonRecursiveMergeTo) {
   LiteralNode assignment;
   assignment.set_value(assignment_token);
 
+  // Add some values to the scope.
   Value old_value(&assignment, "hello");
   setup.scope()->SetValue("v", old_value, &assignment);
   base::StringPiece private_var_name("_private");
   setup.scope()->SetValue(private_var_name, old_value, &assignment);
+
+  // Add some templates to the scope.
+  FunctionCallNode templ_definition;
+  scoped_refptr<Template> templ(new Template(setup.scope(), &templ_definition));
+  setup.scope()->AddTemplate("templ", templ.get());
+  scoped_refptr<Template> private_templ(
+      new Template(setup.scope(), &templ_definition));
+  setup.scope()->AddTemplate("_templ", private_templ.get());
 
   // Detect collisions of values' values.
   {
@@ -50,6 +60,20 @@ TEST(Scope, NonRecursiveMergeTo) {
     EXPECT_FALSE(setup.scope()->NonRecursiveMergeTo(
         &new_scope, Scope::MergeOptions(),
         &assignment, "error", &err));
+    EXPECT_TRUE(err.has_error());
+  }
+
+  // Template name collisions.
+  {
+    Scope new_scope(setup.settings());
+
+    scoped_refptr<Template> new_templ(
+        new Template(&new_scope, &templ_definition));
+    new_scope.AddTemplate("templ", new_templ.get());
+
+    Err err;
+    EXPECT_FALSE(setup.scope()->NonRecursiveMergeTo(
+        &new_scope, Scope::MergeOptions(), &assignment, "error", &err));
     EXPECT_TRUE(err.has_error());
   }
 
@@ -71,6 +95,26 @@ TEST(Scope, NonRecursiveMergeTo) {
     EXPECT_TRUE(old_value == *found_value);
   }
 
+  // Clobber flag for templates.
+  {
+    Scope new_scope(setup.settings());
+
+    scoped_refptr<Template> new_templ(
+        new Template(&new_scope, &templ_definition));
+    new_scope.AddTemplate("templ", new_templ.get());
+    Scope::MergeOptions options;
+    options.clobber_existing = true;
+
+    Err err;
+    EXPECT_TRUE(setup.scope()->NonRecursiveMergeTo(
+        &new_scope, options, &assignment, "error", &err));
+    EXPECT_FALSE(err.has_error());
+
+    const Template* found_value = new_scope.GetTemplate("templ");
+    ASSERT_TRUE(found_value);
+    EXPECT_TRUE(templ.get() == found_value);
+  }
+
   // Don't flag values that technically collide but have the same value.
   {
     Scope new_scope(setup.settings());
@@ -78,14 +122,26 @@ TEST(Scope, NonRecursiveMergeTo) {
     new_scope.SetValue("v", new_value, &assignment);
 
     Err err;
-    Scope::MergeOptions options;
-    options.clobber_existing = true;
     EXPECT_TRUE(setup.scope()->NonRecursiveMergeTo(
-        &new_scope, options, &assignment, "error", &err));
+        &new_scope, Scope::MergeOptions(), &assignment, "error", &err));
     EXPECT_FALSE(err.has_error());
   }
 
-  // Copy private values.
+  // Templates that technically collide but are the same.
+  {
+    Scope new_scope(setup.settings());
+
+    scoped_refptr<Template> new_templ(
+        new Template(&new_scope, &templ_definition));
+    new_scope.AddTemplate("templ", templ.get());
+
+    Err err;
+    EXPECT_TRUE(setup.scope()->NonRecursiveMergeTo(
+        &new_scope, Scope::MergeOptions(), &assignment, "error", &err));
+    EXPECT_FALSE(err.has_error());
+  }
+
+  // Copy private values and templates.
   {
     Scope new_scope(setup.settings());
 
@@ -94,9 +150,10 @@ TEST(Scope, NonRecursiveMergeTo) {
         &new_scope, Scope::MergeOptions(), &assignment, "error", &err));
     EXPECT_FALSE(err.has_error());
     EXPECT_TRUE(new_scope.GetValue(private_var_name));
+    EXPECT_TRUE(new_scope.GetTemplate("_templ"));
   }
 
-  // Skip private values.
+  // Skip private values and templates.
   {
     Scope new_scope(setup.settings());
 
@@ -107,6 +164,7 @@ TEST(Scope, NonRecursiveMergeTo) {
         &new_scope, options, &assignment, "error", &err));
     EXPECT_FALSE(err.has_error());
     EXPECT_FALSE(new_scope.GetValue(private_var_name));
+    EXPECT_FALSE(new_scope.GetTemplate("_templ"));
   }
 
   // Don't mark used.
