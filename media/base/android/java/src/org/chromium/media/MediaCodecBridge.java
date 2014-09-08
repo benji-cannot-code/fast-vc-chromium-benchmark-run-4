f50822896a26d80e1f5e775872015df1e34630b0
qinmin <qinmin@chromium.org>
2014-09-08 15:40:02 -0700

    Suppress build time warnings
    BUG=411360
    
    Review URL: https://codereview.chromium.org/550503002
    
    Cr-Commit-Position: refs/heads/master@{#293799}

--- media/base/android/java/src/org/chromium/media/MediaCodecBridge.java
+++ media/base/android/java/src/org/chromium/media/MediaCodecBridge.java
@@ -154,6 +154,7 @@ class MediaCodecBridge {
     /**
      * Get a list of supported android codec mimes.
      */
+    @SuppressWarnings("deprecation")
     @CalledByNative
     private static CodecInfo[] getCodecsInfo() {
         // Return the first (highest-priority) codec for each MIME type.
@@ -181,6 +182,7 @@ class MediaCodecBridge {
         return codecInfos.toArray(new CodecInfo[codecInfos.size()]);
     }
 
+    @SuppressWarnings("deprecation")
     private static String getDecoderNameForMime(String mime) {
         int count = MediaCodecList.getCodecCount();
         for (int i = 0; i < count; ++i) {
@@ -269,6 +271,7 @@ class MediaCodecBridge {
         }
     }
 
+    @SuppressWarnings("deprecation")
     @CalledByNative
     private boolean start() {
         try {
@@ -363,6 +366,7 @@ class MediaCodecBridge {
         return mOutputBuffers != null ? mOutputBuffers[0].capacity() : -1;
     }
 
+    @SuppressWarnings("deprecation")
     @CalledByNative
     private boolean getOutputBuffers() {
         try {
@@ -436,6 +440,7 @@ class MediaCodecBridge {
         }
     }
 
+    @SuppressWarnings("deprecation")
     @CalledByNative
     private DequeueOutputResult dequeueOutputBuffer(long timeoutUs) {
         MediaCodec.BufferInfo info = new MediaCodec.BufferInfo();
@@ -611,6 +616,7 @@ class MediaCodecBridge {
         return mAudioTrack.getPlaybackHeadPosition();
     }
 
+    @SuppressWarnings("deprecation")
     @CalledByNative
     private void setVolume(double volume) {
         if (mAudioTrack != null) {
