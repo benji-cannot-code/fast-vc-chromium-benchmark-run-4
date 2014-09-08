11ff2bffb16ad8a36510d851a557f2c07612b040
scottmg@chromium.org <scottmg@chromium.org>
2014-09-08 23:19:55 +0000

    Remove unnecessary 'const's
    Noticed here https://codereview.chromium.org/538333002/ .
    Something's wrong between gn/gyp because this doesn't happen
    in gyp, but remove these consts anyway as they're unnecessary.
    
    R=thakis@chromium.org
    BUG=335824
    
    Review URL: https://codereview.chromium.org/551863004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181584 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/GraphicsContext.cpp
+++ third_party/WebKit/Source/platform/graphics/GraphicsContext.cpp
@@ -1902,7 +1902,7 @@ void GraphicsContext::draw1xMarker(SkBitmap* bitmap, int index)
     }
 }
 
-const SkPMColor GraphicsContext::lineColors(int index)
+SkPMColor GraphicsContext::lineColors(int index)
 {
     static const SkPMColor colors[] = {
         SkPreMultiplyARGB(0xFF, 0xFF, 0x00, 0x00), // Opaque red.
@@ -1912,7 +1912,7 @@ const SkPMColor GraphicsContext::lineColors(int index)
     return colors[index];
 }
 
-const SkPMColor GraphicsContext::antiColors1(int index)
+SkPMColor GraphicsContext::antiColors1(int index)
 {
     static const SkPMColor colors[] = {
         SkPreMultiplyARGB(0xB0, 0xFF, 0x00, 0x00), // Semitransparent red.
@@ -1922,7 +1922,7 @@ const SkPMColor GraphicsContext::antiColors1(int index)
     return colors[index];
 }
 
-const SkPMColor GraphicsContext::antiColors2(int index)
+SkPMColor GraphicsContext::antiColors2(int index)
 {
     static const SkPMColor colors[] = {
         SkPreMultiplyARGB(0x60, 0xFF, 0x00, 0x00), // More transparent red
