f03ba63b7828120d36c675882612a0594167d9eb
lcwu <lcwu@chromium.org>
2014-09-08 13:29:35 -0700

    Fix the uses of T* conversion operator from scoped_refptr<T> which is now removed.
    Please see https://codereview.chromium.org/510323002 for reference.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/546243002
    
    Cr-Commit-Position: refs/heads/master@{#293778}

--- chromecast/shell/browser/url_request_context_factory.cc
+++ chromecast/shell/browser/url_request_context_factory.cc
@@ -151,7 +151,8 @@ net::URLRequestContextGetter* URLRequestContextFactory::CreateMainGetter(
     content::BrowserContext* browser_context,
     content::ProtocolHandlerMap* protocol_handlers,
     content::URLRequestInterceptorScopedVector request_interceptors) {
-  DCHECK(!main_getter_) << "Main URLRequestContextGetter already initialized";
+  DCHECK(!main_getter_.get())
+      << "Main URLRequestContextGetter already initialized";
   main_getter_ = new MainURLRequestContextGetter(this,
                                                  browser_context,
                                                  protocol_handlers,
@@ -160,19 +161,19 @@ net::URLRequestContextGetter* URLRequestContextFactory::CreateMainGetter(
 }
 
 net::URLRequestContextGetter* URLRequestContextFactory::GetMainGetter() {
-  CHECK(main_getter_);
+  CHECK(main_getter_.get());
   return main_getter_.get();
 }
 
 net::URLRequestContextGetter* URLRequestContextFactory::GetSystemGetter() {
-  if (!system_getter_) {
+  if (!system_getter_.get()) {
     system_getter_ = new URLRequestContextGetter(this, false);
   }
   return system_getter_.get();
 }
 
 net::URLRequestContextGetter* URLRequestContextFactory::GetMediaGetter() {
-  if (!media_getter_) {
+  if (!media_getter_.get()) {
     media_getter_ = new URLRequestContextGetter(this, true);
   }
   return media_getter_.get();
@@ -204,8 +205,8 @@ void URLRequestContextFactory::InitializeSystemContextDependencies() {
   proxy_service_.reset(net::ProxyService::CreateUsingSystemProxyResolver(
       net::ProxyService::CreateSystemProxyConfigService(
           content::BrowserThread::GetMessageLoopProxyForThread(
-              content::BrowserThread::IO).get(),
-          content::BrowserThread::UnsafeGetMessageLoopForThread(
+              content::BrowserThread::IO),
+          content::BrowserThread::GetMessageLoopProxyForThread(
               content::BrowserThread::FILE)),
       0,
       NULL));
@@ -313,7 +314,7 @@ net::URLRequestContext* URLRequestContextFactory::CreateSystemRequestContext() {
 
 net::URLRequestContext* URLRequestContextFactory::CreateMediaRequestContext() {
   DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::IO));
-  DCHECK(main_getter_)
+  DCHECK(main_getter_.get())
       << "Getting MediaRequestContext before MainRequestContext";
   net::URLRequestContext* main_context = main_getter_->GetURLRequestContext();
 
