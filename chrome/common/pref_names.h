7ceb128a4f8891b580cc70d2e9e03edee0cbd865
dhnishi <dhnishi@chromium.org>
2014-09-09 10:15:42 -0700

    Remove Google Talk plugin whitelisting now that b/11355273 is fixed.
    BUG=115472
    
    Review URL: https://codereview.chromium.org/551093003
    
    Cr-Commit-Position: refs/heads/master@{#293959}

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -326,7 +326,6 @@ extern const char kDefaultContentSettings[];
 extern const char kContentSettingsClearOnExitMigrated[];
 extern const char kContentSettingsVersion[];
 extern const char kContentSettingsPatternPairs[];
-extern const char kContentSettingsDefaultWhitelistVersion[];
 extern const char kContentSettingsPluginWhitelist[];
 extern const char kBlockThirdPartyCookies[];
 extern const char kClearSiteDataOnExit[];
