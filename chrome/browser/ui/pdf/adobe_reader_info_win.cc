0f2887abfe5cc1345bf0700345ea5d6a03030a5b
thestig <thestig@chromium.org>
2014-09-09 00:13:07 -0700

    Update latest Adobe Reader version to 11.0.8.4.
    BUG=412047
    
    Review URL: https://codereview.chromium.org/555523005
    
    Cr-Commit-Position: refs/heads/master@{#293883}

--- chrome/browser/ui/pdf/adobe_reader_info_win.cc
+++ chrome/browser/ui/pdf/adobe_reader_info_win.cc
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // Hardcoded value for the secure version of Acrobat Reader.
-const char kSecureVersion[] = "11.0.7.79";
+const char kSecureVersion[] = "11.0.8.4";
 
 const char kAdobeReaderIdentifier[] = "adobe-reader";
 const char kPdfMimeType[] = "application/pdf";
