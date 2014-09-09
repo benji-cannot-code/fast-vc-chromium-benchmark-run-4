4af5c894c56bf18739b572bf55d03309c3d30d1f
derat <derat@chromium.org>
2014-09-08 23:22:59 -0700

    linux: Don't lower windows on middle-click on KDE 4.
    KDE releases since 4.4 have had activated a "tab-grouping"
    feature when users middle-click on window titlebars. Make
    Chrome be slightly less jarring by doing nothing in response
    to middle-clicks on KDE4 instead of lowering windows.
    
    BUG=36586
    
    Review URL: https://codereview.chromium.org/557503002
    
    Cr-Commit-Position: refs/heads/master@{#293870}

--- chrome/browser/ui/libgtk2ui/gtk2_ui.cc
+++ chrome/browser/ui/libgtk2ui/gtk2_ui.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/rtl.h"
 #include "base/logging.h"
 #include "base/nix/mime_util_xdg.h"
+#include "base/nix/xdg_util.h"
 #include "base/stl_util.h"
 #include "base/strings/stringprintf.h"
 #include "chrome/browser/themes/theme_properties.h"
@@ -379,9 +380,23 @@ gfx::FontRenderParams GetGtkFontRenderParams() {
   return params;
 }
 
+views::LinuxUI::NonClientMiddleClickAction GetDefaultMiddleClickAction() {
+  scoped_ptr<base::Environment> env(base::Environment::Create());
+  switch (base::nix::GetDesktopEnvironment(env.get())) {
+    case base::nix::DESKTOP_ENVIRONMENT_KDE4:
+      // Starting with KDE 4.4, windows' titlebars can be dragged with the
+      // middle mouse button to create tab groups. We don't support that in
+      // Chrome, but at least avoid lowering windows in response to middle
+      // clicks to avoid surprising users who expect the KDE behavior.
+      return views::LinuxUI::MIDDLE_CLICK_ACTION_NONE;
+    default:
+      return views::LinuxUI::MIDDLE_CLICK_ACTION_LOWER;
+  }
+}
+
 }  // namespace
 
-Gtk2UI::Gtk2UI() : middle_click_action_(MIDDLE_CLICK_ACTION_LOWER) {
+Gtk2UI::Gtk2UI() : middle_click_action_(GetDefaultMiddleClickAction()) {
   GtkInitFromCommandLine(*CommandLine::ForCurrentProcess());
 }
 
