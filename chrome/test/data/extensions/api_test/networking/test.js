f1a4a5a97cf28c49b3963eaeb00f51fe3c71f597
pneubeck <pneubeck@chromium.org>
2014-09-09 06:16:13 -0700

    ONC: Remove augmentation of unmanaged network properties.
    Before read-only properties which cannot be set by the user or policy, were explicitly marked as 'unmanaged' in the augmented dictionary returned by getManagedProperties.
    
    Now, instead these properties are returned as plain values, which simplifies the consumer side, especially of Array typed properties like IPConfigs (compare this with SavedIPConfig).
    
    BUG=410877
    
    Review URL: https://codereview.chromium.org/551753002
    
    Cr-Commit-Position: refs/heads/master@{#293931}

--- chrome/test/data/extensions/api_test/networking/test.js
+++ chrome/test/data/extensions/api_test/networking/test.js
@@ -317,14 +317,8 @@ var availableTests = [
       "stub_wifi2",
       callbackPass(function(result) {
         assertEq({
-                   "Connectable": {
-                     "Active": true,
-                     "Effective": "Unmanaged"
-                   },
-                   "ConnectionState": {
-                     "Active": "NotConnected",
-                     "Effective": "Unmanaged"
-                   },
+                   "Connectable": true,
+                   "ConnectionState": "NotConnected",
                    "GUID": "stub_wifi2",
                    "Name": {
                      "Active": "wifi2_PSK",
@@ -341,14 +335,8 @@ var availableTests = [
                        "Active": false,
                        "UserEditable": true
                      },
-                     "Frequency" : {
-                       "Active": 5000,
-                       "Effective": "Unmanaged"
-                     },
-                     "FrequencyList" : {
-                       "Active": [2400, 5000],
-                       "Effective": "Unmanaged"
-                     },
+                     "Frequency" : 5000,
+                     "FrequencyList" : [2400, 5000],
                      "Passphrase": {
                        "Effective": "UserSetting",
                        "UserEditable": true,
@@ -364,10 +352,7 @@ var availableTests = [
                        "Effective": "UserPolicy",
                        "UserPolicy": "WPA-PSK"
                      },
-                     "SignalStrength": {
-                       "Active": 80,
-                       "Effective": "Unmanaged"
-                     }
+                     "SignalStrength": 80,
                    }
                  }, result);
       }));
--- components/onc/onc_constants.cc
+++ components/onc/onc_constants.cc
@@ -10,7 +10,6 @@ namespace onc {
 
 const char kAugmentationActiveSetting[] = "Active";
 const char kAugmentationEffectiveSetting[] = "Effective";
-const char kAugmentationUnmanaged[] = "Unmanaged";
 const char kAugmentationUserPolicy[] = "UserPolicy";
 const char kAugmentationDevicePolicy[] = "DevicePolicy";
 const char kAugmentationUserSetting[] = "UserSetting";
