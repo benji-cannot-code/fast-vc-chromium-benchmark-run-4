0c61d947db0b4d42e5f3208e9881857f5d6d791c
scottmg <scottmg@chromium.org>
2014-09-09 06:14:49 -0700

    gn/linux: warnings as errors, turn on Wextra
    Also remove a few redundant consts that result in warnings like:
    
    ../../content/common/cursors/webcursor_aurax11.cc:20:1: error: 'const' type qualifier on return type has no effect [-Werror,-Wignored-qualifiers]
    const ui::PlatformCursor WebCursor::GetPlatformCursor() {
    ^~~~~~
    
    TBR=davemoore@chromium.org,sadrul@chromium.org
    R=brettw@chromium.org, thakis@chromium.org,rch@chromium.org
    BUG=393046, 335824
    
    Review URL: https://codereview.chromium.org/538333002
    
    Cr-Commit-Position: refs/heads/master@{#293930}

--- content/common/cursors/webcursor_aurawin.cc
+++ content/common/cursors/webcursor_aurawin.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-const ui::PlatformCursor WebCursor::GetPlatformCursor() {
+ui::PlatformCursor WebCursor::GetPlatformCursor() {
   if (!IsCustom())
     return LoadCursor(NULL, IDC_ARROW);
 
--- content/common/cursors/webcursor_aurax11.cc
+++ content/common/cursors/webcursor_aurax11.cc
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-const ui::PlatformCursor WebCursor::GetPlatformCursor() {
+ui::PlatformCursor WebCursor::GetPlatformCursor() {
   if (platform_cursor_)
     return platform_cursor_;
 
