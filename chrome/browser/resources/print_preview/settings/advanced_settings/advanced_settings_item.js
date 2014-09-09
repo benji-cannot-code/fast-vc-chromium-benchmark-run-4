303d94001e4b06ab129f3127a48e9aa14a9176a3
alekseys <alekseys@chromium.org>
2014-09-08 18:00:17 -0700

    Add search hint bubbles to Print Preview advanced options controls.
    BUG=397741
    
    Review URL: https://codereview.chromium.org/542363002
    
    Cr-Commit-Position: refs/heads/master@{#293827}

--- chrome/browser/resources/print_preview/settings/advanced_settings/advanced_settings_item.js
+++ chrome/browser/resources/print_preview/settings/advanced_settings/advanced_settings_item.js
@@ -110,6 +110,10 @@ cr.define('print_preview', function() {
       this.renderCapability_();
     },
 
+    get searchBubbleShown() {
+      return getIsVisible(this.getElement()) && !!this.searchBubble_;
+    },
+
     /**
      * @return {HTMLSelectElement} Select element.
      * @private
@@ -156,7 +160,7 @@ cr.define('print_preview', function() {
       var textContent = this.capability_.display_name;
       var nameMatches = this.query_ ? !!textContent.match(this.query_) : true;
       var optionMatches = null;
-      if (false && this.query_) {
+      if (this.query_) {
         if (this.capability_.type == 'SELECT') {
           this.capability_.select_cap.option.some(function(option) {
             optionMatches = (option.display_name || '').match(this.query_);
@@ -187,7 +191,6 @@ cr.define('print_preview', function() {
       nameEl.title = textContent;
 
       if (optionMatches) {
-        window.console.log(optionMatches[0]);
         var element =
             this.capability_.type == 'SELECT' ? this.select_ : this.text_;
         if (!this.searchBubble_) {
