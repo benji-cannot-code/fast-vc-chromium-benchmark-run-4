5b41c8b2ee111eb1d28cb1357b4dc12a8870f1ea
hubbe <hubbe@chromium.org>
2014-09-08 19:31:47 -0700

    Re-Re-Enable CastV2PerformanceTest
    As far as I know, all outstanding issues have been resolved in
    separate CLs. Time to re-re-enable the test.
    
    BUG=356842
    
    Review URL: https://codereview.chromium.org/551153002
    
    Cr-Commit-Position: refs/heads/master@{#293839}

--- chrome/browser/extensions/api/cast_streaming/performance_test.cc
+++ chrome/browser/extensions/api/cast_streaming/performance_test.cc
@@ -662,8 +662,7 @@ class CastV2PerformanceTest
 
 }  // namespace
 
-// crbug.com/356842
-IN_PROC_BROWSER_TEST_P(CastV2PerformanceTest, DISABLED_Performance) {
+IN_PROC_BROWSER_TEST_P(CastV2PerformanceTest, Performance) {
   RunTest("CastV2Performance");
 }
 
