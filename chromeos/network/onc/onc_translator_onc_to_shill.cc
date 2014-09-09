c9f9b9eacd39b42445c6bb9ef265a31e7052a208
pneubeck <pneubeck@chromium.org>
2014-09-09 03:46:01 -0700

    ONC: Fix Static-/Saved-/IPConfig.
    Before commit 41ec57e48ebd467c7a3b169a5598fb8038f54232,
    IPConfigs was the field in ONC meant for configuring ip configuration of a network.
    This was changed to:
    - IPConfigs is the set of currently active ip configurations (it can be more than one if IPv4 and IPv6 are coexisting in parallel) reported by ChromeOS
    - StaticIPConfig (note that it's a single configuration, not multiple as before) is the writable field that the user or policy can set
    - SavedIPConfig is the configuration received via DHCP and reported by ChromeOS.
    
    This change updates the different ONC functions to these new field definitions.
    
    Effectively this also allows setting the IPConfig of a network through ONC import, the networkingPrivate API, and through policy (there's no server support though).
    
    BUG=410877
    
    Review URL: https://codereview.chromium.org/540333002
    
    Cr-Commit-Position: refs/heads/master@{#293915}

--- chromeos/network/onc/onc_translator_onc_to_shill.cc
+++ chromeos/network/onc/onc_translator_onc_to_shill.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_reader.h"
 #include "base/json/json_writer.h"
 #include "base/logging.h"
+#include "base/strings/string_util.h"
 #include "base/values.h"
 #include "chromeos/network/onc/onc_signature.h"
 #include "chromeos/network/onc/onc_translation_tables.h"
@@ -27,6 +28,17 @@ namespace chromeos {
 namespace onc {
 
 namespace {
+
+bool ConvertListValueToStringVector(const base::ListValue& string_list,
+                                    std::vector<std::string>* result) {
+  for (size_t i = 0; i < string_list.GetSize(); ++i) {
+    std::string str;
+    if (!string_list.GetString(i, &str))
+      return false;
+    result->push_back(str);
+  }
+  return true;
+}
 
 scoped_ptr<base::StringValue> ConvertValueToString(const base::Value& value) {
   std::string str;
@@ -60,6 +72,7 @@ class LocalTranslator {
   void TranslateVPN();
   void TranslateWiFi();
   void TranslateEAP();
+  void TranslateStaticIPConfig();
   void TranslateNetworkConfiguration();
 
   // Copies all entries from |onc_object_| to |shill_dictionary_| for which a
@@ -90,6 +103,8 @@ class LocalTranslator {
 void LocalTranslator::TranslateFields() {
   if (onc_signature_ == &kNetworkConfigurationSignature)
     TranslateNetworkConfiguration();
+  else if (onc_signature_ == &kStaticIPConfigSignature)
+    TranslateStaticIPConfig();
   else if (onc_signature_ == &kEthernetSignature)
     TranslateEthernet();
   else if (onc_signature_ == &kVPNSignature)
@@ -116,6 +131,21 @@ void LocalTranslator::TranslateEthernet() {
     shill_type = shill::kTypeEthernetEap;
   shill_dictionary_->SetStringWithoutPathExpansion(shill::kTypeProperty,
                                                    shill_type);
+
+  CopyFieldsAccordingToSignature();
+}
+
+
+void LocalTranslator::TranslateStaticIPConfig() {
+  const base::ListValue* onc_nameservers = NULL;
+  if (onc_object_->GetListWithoutPathExpansion(::onc::ipconfig::kNameServers,
+                                               &onc_nameservers)) {
+    std::vector<std::string> onc_nameservers_vector;
+    ConvertListValueToStringVector(*onc_nameservers, &onc_nameservers_vector);
+    std::string shill_nameservers = JoinString(onc_nameservers_vector, ',');
+    shill_dictionary_->SetStringWithoutPathExpansion(
+        shill::kStaticIPNameServersProperty, shill_nameservers);
+  }
 
   CopyFieldsAccordingToSignature();
 }
