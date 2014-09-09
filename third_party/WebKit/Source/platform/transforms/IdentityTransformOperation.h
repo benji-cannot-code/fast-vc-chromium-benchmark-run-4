7dba23a8ad43fd54be38a1f3badd9b013de8a788
timloh@chromium.org <timloh@chromium.org>
2014-09-09 07:40:24 +0000

    Remove unused TransformOperation::isIdentity
    Review URL: https://codereview.chromium.org/551183003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181613 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/transforms/IdentityTransformOperation.h
+++ third_party/WebKit/Source/platform/transforms/IdentityTransformOperation.h
@@ -43,7 +43,6 @@ public:
     }
 
 private:
-    virtual bool isIdentity() const OVERRIDE FINAL { return true; }
     virtual OperationType type() const OVERRIDE { return Identity; }
 
     virtual bool operator==(const TransformOperation& o) const OVERRIDE
