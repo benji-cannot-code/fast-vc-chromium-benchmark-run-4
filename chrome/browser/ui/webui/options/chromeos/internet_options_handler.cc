a57d28dc4ed31859b72ea6cc0119575162700cc8
stevenjb <stevenjb@chromium.org>
2014-09-09 01:15:40 -0700

    Some JS cleanup, including proper ownership of ONC properties.
    This is in preparation for eliminating the remaining non ONC properties
    from the 'data' object passed from Chrome.
    
    BUG=279351
    
    Review URL: https://codereview.chromium.org/547703004
    
    Cr-Commit-Position: refs/heads/master@{#293895}

--- chrome/browser/ui/webui/options/chromeos/internet_options_handler.cc
+++ chrome/browser/ui/webui/options/chromeos/internet_options_handler.cc
@@ -91,7 +91,6 @@ const char kUpdateCarrierFunction[] =
     "options.internet.DetailsInternetPage.updateCarrier";
 
 // These are used to register message handlers with JavaScript.
-const char kBuyDataPlanMessage[] = "buyDataPlan";
 const char kChangePinMessage[] = "changePin";
 const char kDisableCellularMessage[] = "disableCellular";
 const char kDisableWifiMessage[] = "disableWifi";
@@ -460,9 +459,6 @@ void InternetOptionsHandler::RegisterMessages() {
   web_ui()->RegisterMessageCallback(kDisableWimaxMessage,
       base::Bind(&InternetOptionsHandler::DisableWimaxCallback,
                  base::Unretained(this)));
-  web_ui()->RegisterMessageCallback(kBuyDataPlanMessage,
-      base::Bind(&InternetOptionsHandler::BuyDataPlanCallback,
-                 base::Unretained(this)));
   web_ui()->RegisterMessageCallback(kShowMorePlanInfoMessage,
       base::Bind(&InternetOptionsHandler::ShowMorePlanInfoCallback,
                  base::Unretained(this)));
@@ -545,17 +541,6 @@ void InternetOptionsHandler::DisableWimaxCallback(const base::ListValue* args) {
 
 void InternetOptionsHandler::ShowMorePlanInfoCallback(
     const base::ListValue* args) {
-  if (!web_ui())
-    return;
-  std::string service_path;
-  if (args->GetSize() != 1 || !args->GetString(0, &service_path)) {
-    NOTREACHED();
-    return;
-  }
-  ash::network_connect::ShowMobileSetup(service_path);
-}
-
-void InternetOptionsHandler::BuyDataPlanCallback(const base::ListValue* args) {
   if (!web_ui())
     return;
   std::string service_path;
