b0fab0289f9396e42c310c8b8acf9b15ca427710
msw <msw@chromium.org>
2014-09-09 14:33:57 -0700

    Add textfield internal padding from FocusableBorder.
    Add 3px of internal padding for textfield/combobox content.
    Remove corresponding unused padding from FocusableBorder.
    This better accommodates excessively tall fallback font text.
    Update Omnibox, Find Bar, App List, and CookieInfoView.
    
    See before/after pics at http://crbug.com/404999#c51
    
    BUG=404999
    TEST=Bookmark bubble/dialog, find bar, etc. don't clip most tall fallback text.
    R=sky@chromium.org,pkasting@chromium.org
    
    Review URL: https://codereview.chromium.org/516943003
    
    Cr-Commit-Position: refs/heads/master@{#294016}

--- chrome/browser/ui/views/location_bar/location_bar_view.cc
+++ chrome/browser/ui/views/location_bar/location_bar_view.cc
@@ -601,7 +601,6 @@ gfx::Size LocationBarView::GetPreferredSize() const {
     leading_width +=
         kItemPadding + location_icon_view_->GetMinimumSize().width();
   }
-  leading_width += kItemPadding - GetEditLeadingInternalSpace();
 
   // Compute width of omnibox-trailing content.
   int trailing_width = search_button_->visible() ?
@@ -613,7 +612,7 @@ gfx::Size LocationBarView::GetPreferredSize() const {
       IncrementalMinimumWidth(manage_passwords_icon_view_) +
       IncrementalMinimumWidth(zoom_view_) +
       IncrementalMinimumWidth(generated_credit_card_view_) +
-      IncrementalMinimumWidth(mic_search_view_) + kItemPadding;
+      IncrementalMinimumWidth(mic_search_view_);
   for (PageActionViews::const_iterator i(page_action_views_.begin());
        i != page_action_views_.end(); ++i)
     trailing_width += IncrementalMinimumWidth((*i));
@@ -621,8 +620,8 @@ gfx::Size LocationBarView::GetPreferredSize() const {
        i != content_setting_views_.end(); ++i)
     trailing_width += IncrementalMinimumWidth((*i));
 
-  min_size.set_width(
-      leading_width + omnibox_view_->GetMinimumSize().width() + trailing_width);
+  min_size.set_width(leading_width + omnibox_view_->GetMinimumSize().width() +
+      2 * kItemPadding - omnibox_view_->GetInsets().width() + trailing_width);
   return min_size;
 }
 
@@ -638,9 +637,11 @@ void LocationBarView::Layout() {
 
   LocationBarLayout leading_decorations(
       LocationBarLayout::LEFT_EDGE,
-      kItemPadding - GetEditLeadingInternalSpace());
-  LocationBarLayout trailing_decorations(LocationBarLayout::RIGHT_EDGE,
-                                         kItemPadding);
+      kItemPadding - omnibox_view_->GetInsets().left() -
+          GetEditLeadingInternalSpace());
+  LocationBarLayout trailing_decorations(
+      LocationBarLayout::RIGHT_EDGE,
+      kItemPadding - omnibox_view_->GetInsets().right());
 
   const int origin_chip_preferred_width =
       origin_chip_view_->GetPreferredSize().width();
@@ -933,9 +934,8 @@ void LocationBarView::Layout() {
     }
   }
 
-  const gfx::Insets insets = omnibox_view_->GetInsets();
-  omnibox_view_->SetBorder(views::Border::CreateEmptyBorder(
-      insets.top(), insets.left(), insets.bottom(), omnibox_view_margin));
+  omnibox_view_->SetBorder(
+      views::Border::CreateEmptyBorder(0, 0, 0, omnibox_view_margin));
 
   // Layout |ime_inline_autocomplete_view_| next to the user input.
   if (ime_inline_autocomplete_view_->visible()) {
--- ui/views/controls/focusable_border.h
+++ ui/views/controls/focusable_border.h
@@ -22,6 +22,7 @@ namespace views {
 class VIEWS_EXPORT FocusableBorder : public Border {
  public:
   FocusableBorder();
+  virtual ~FocusableBorder();
 
   // Sets the insets of the border.
   void SetInsets(int top, int left, int bottom, int right);
