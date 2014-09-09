2648edcfc27d674ccde9a0a2508c786369e23d95
nyquist <nyquist@chromium.org>
2014-09-08 18:17:09 -0700

    [android] Proguard out most of guava
    This CL removes most of guava, except a few classes and annotations.
    
    Also updates the rest of Chrome to ensure only those classes are in use.
    
    Thirdly, it adds an OWNERS file to third_party/guava.
    
    BUG=163393
    TBR=brettw@chromium.org
    
    Review URL: https://codereview.chromium.org/546973003
    
    Cr-Commit-Position: refs/heads/master@{#293828}

--- chrome/android/java/src/org/chromium/chrome/browser/sync/ProfileSyncService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/sync/ProfileSyncService.java
@@ -8,8 +8,6 @@ package org.chromium.chrome.browser.sync;
 import android.content.Context;
 import android.util.Log;
 
-import com.google.common.base.Joiner;
-
 import org.chromium.base.CalledByNative;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.VisibleForTesting;
@@ -18,6 +16,7 @@ import org.chromium.sync.internal_api.pub.SyncDecryptionPassphraseType;
 import org.chromium.sync.internal_api.pub.base.ModelType;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.SortedSet;
@@ -534,7 +533,16 @@ public class ProfileSyncService {
         for (ModelType type : filteredTypes) {
             set.add(type.toString());
         }
-        return Joiner.on(", ").join(set);
+        StringBuilder sb = new StringBuilder();
+        Iterator<String> it = set.iterator();
+        if (it.hasNext()) {
+            sb.append(it.next());
+            while (it.hasNext()) {
+                sb.append(", ");
+                sb.append(it.next());
+            }
+        }
+        return sb.toString();
     }
 
     // Native methods
