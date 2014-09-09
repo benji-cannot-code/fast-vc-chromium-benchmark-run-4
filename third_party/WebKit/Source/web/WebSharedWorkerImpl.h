5ea807ed2fae9e17d2bcd27992ee2195dfedfe8c
vsevik@chromium.org <vsevik@chromium.org>
2014-09-09 20:45:02 +0000

    DevTools: Switch shared workers inspection on to the main thread.
    BUG=404355
    
    Review URL: https://codereview.chromium.org/534293003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181674 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebSharedWorkerImpl.h
+++ third_party/WebKit/Source/web/WebSharedWorkerImpl.h
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/workers/WorkerScriptLoaderClient.h"
 #include "core/workers/WorkerThread.h"
 #include "public/web/WebContentSecurityPolicy.h"
+#include "public/web/WebDevToolsAgentClient.h"
 #include "public/web/WebFrameClient.h"
 #include "public/web/WebSharedWorkerClient.h"
 #include "wtf/PassOwnPtr.h"
@@ -69,7 +70,8 @@ class WebSharedWorkerImpl FINAL
     : public WorkerReportingProxy
     , public WorkerLoaderProxy
     , public WebFrameClient
-    , public WebSharedWorker {
+    , public WebSharedWorker
+    , public WebDevToolsAgentClient {
 public:
     explicit WebSharedWorkerImpl(WebSharedWorkerClient*);
 
@@ -92,6 +94,11 @@ public:
     virtual WebApplicationCacheHost* createApplicationCacheHost(WebLocalFrame*, WebApplicationCacheHostClient*) OVERRIDE;
     virtual void didFinishDocumentLoad(WebLocalFrame*) OVERRIDE;
 
+    // WebDevToolsAgentClient overrides.
+    virtual void sendMessageToInspectorFrontend(const WebString&) OVERRIDE;
+    virtual void saveAgentRuntimeState(const WebString&) OVERRIDE;
+    virtual void resumeStartup() OVERRIDE;
+
     // WebSharedWorker methods:
     virtual void startWorkerContext(const WebURL&, const WebString& name, const WebString& contentSecurityPolicy, WebContentSecurityPolicyType) OVERRIDE;
     virtual void connect(WebMessagePortChannel*) OVERRIDE;
@@ -100,11 +107,7 @@ public:
 
     virtual void pauseWorkerContextOnStart() OVERRIDE;
     virtual void resumeWorkerContext() OVERRIDE;
-    // FIXME: Remove this once chromium uses the one that receives hostId as a parameter.
-    virtual void attachDevTools() OVERRIDE;
     virtual void attachDevTools(const WebString& hostId) OVERRIDE;
-    // FIXME: Remove this once chromium uses the one that receives hostId as a parameter.
-    virtual void reattachDevTools(const WebString& savedState) OVERRIDE;
     virtual void reattachDevTools(const WebString& hostId, const WebString& savedState) OVERRIDE;
     virtual void detachDevTools() OVERRIDE;
     virtual void dispatchDevToolsMessage(const WebString&) OVERRIDE;
@@ -125,6 +128,7 @@ private:
     // Creates the shadow loader used for worker network requests.
     void initializeLoader(const WebURL&);
 
+    void startScriptLoader(WebLocalFrame*);
     void didReceiveScriptLoaderResponse();
     void onScriptLoaderFinished();
 
@@ -154,7 +158,7 @@ private:
     WeakPtr<WebSharedWorkerClient> m_clientWeakPtr;
 
     bool m_pauseWorkerContextOnStart;
-    bool m_attachDevToolsOnStart;
+    bool m_isPausedOnStart;
 
     // Kept around only while main script loading is ongoing.
     OwnPtr<Loader> m_mainScriptLoader;
