cb5b609ab2487e55093cb8dbc5bdb9f239dd071c
haraken@chromium.org <haraken@chromium.org>
2014-09-09 07:48:56 +0000

    Oilpan: Expose Heap::collectGarbage to the web layer
    Heap::collectGarbage is needed in media_stream_impl_unittest.
    
    BUG=340522
    
    Review URL: https://codereview.chromium.org/543603003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181615 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebHeap.cpp
+++ third_party/WebKit/Source/web/WebHeap.cpp
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "public/web/WebHeap.h"
 
-#include "platform/heap/ThreadState.h"
+#include "platform/heap/Handle.h"
 
 namespace blink {
 
@@ -44,6 +44,16 @@ WebHeap::SafePointScope::SafePointScope()
 WebHeap::SafePointScope::~SafePointScope()
 {
     ThreadState::current()->leaveSafePoint();
+}
+
+void WebHeap::collectGarbageForTesting()
+{
+    Heap::collectGarbage(ThreadState::HeapPointersOnStack);
+}
+
+void WebHeap::collectAllGarbageForTesting()
+{
+    Heap::collectAllGarbage();
 }
 
 } // namespace blink
--- third_party/WebKit/public/web/WebHeap.h
+++ third_party/WebKit/public/web/WebHeap.h
@@ -48,6 +48,11 @@ public:
         BLINK_EXPORT SafePointScope();
         BLINK_EXPORT ~SafePointScope();
     };
+
+    // These APIs are only for testing purposes and should not be used
+    // outside of tests.
+    BLINK_EXPORT static void collectGarbageForTesting();
+    BLINK_EXPORT static void collectAllGarbageForTesting();
 };
 
 } // namespace blink
