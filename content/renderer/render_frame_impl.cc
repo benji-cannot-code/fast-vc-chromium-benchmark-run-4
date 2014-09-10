a7e8774df32a799f398992e76cb101b337410d44
horo <horo@chromium.org>
2014-09-10 02:35:29 -0700

    [ServiceWorker] Move skip_service_worker flag from RequestExtraData to RequestInfo.
    skip_service_worker flag was introduced in https://codereview.chromium.org/527713002/.
    But setting this flag of RequestExtraData in RenderFrameImpl::willSendRequest() is looks weird.
    This flag should be in RequestInfo and should be set in WebURLLoaderImpl::Context::Start.
    
    BUG=402093
    
    Review URL: https://codereview.chromium.org/547213002
    
    Cr-Commit-Position: refs/heads/master@{#294151}

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -2769,7 +2769,6 @@ void RenderFrameImpl::willSendRequest(
   extra_data->set_transferred_request_request_id(
       navigation_state->transferred_request_request_id());
   extra_data->set_service_worker_provider_id(provider_id);
-  extra_data->set_skip_service_worker(request.skipServiceWorker());
   request.setExtraData(extra_data);
 
   DocumentState* top_document_state =
