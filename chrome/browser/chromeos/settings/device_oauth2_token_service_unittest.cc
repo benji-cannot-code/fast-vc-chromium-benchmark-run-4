454fda5e88f64b56e9102e19c5483ddf517c85e2
ygorshenin <ygorshenin@chromium.org>
2014-09-10 04:47:50 -0700

    BlockingPool is flushed in DeviceOAuth2TokenServiceTest to prevent memory leaks.
    BUG=381584
    TEST=unit_tests:DeviceOAuth2TokenServiceTest.*
    
    Review URL: https://codereview.chromium.org/552363002
    
    Cr-Commit-Position: refs/heads/master@{#294158}

--- chrome/browser/chromeos/settings/device_oauth2_token_service_unittest.cc
+++ chrome/browser/chromeos/settings/device_oauth2_token_service_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_loop.h"
 #include "base/prefs/testing_pref_service.h"
 #include "base/run_loop.h"
+#include "base/threading/sequenced_worker_pool.h"
 #include "chrome/browser/chromeos/policy/device_policy_builder.h"
 #include "chrome/browser/chromeos/settings/cros_settings.h"
 #include "chrome/browser/chromeos/settings/device_settings_service.h"
@@ -98,6 +99,7 @@ class DeviceOAuth2TokenServiceTest : public testing::Test {
   virtual void TearDown() OVERRIDE {
     CrosSettings::Shutdown();
     TestingBrowserProcess::GetGlobal()->SetBrowserPolicyConnector(NULL);
+    content::BrowserThread::GetBlockingPool()->FlushForTesting();
     DeviceSettingsService::Get()->UnsetSessionManager();
     DeviceSettingsService::Shutdown();
     SystemSaltGetter::Shutdown();
