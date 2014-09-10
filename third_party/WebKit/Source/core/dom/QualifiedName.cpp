cdd9e05cc6cf4dfda9a751f6268f3b13ea4dcb10
rob.buis@samsung.com <rob.buis@samsung.com>
2014-09-10 23:23:51 +0000

    Shrink QualifiedNameImpl by 8 bytes on 64-bit platforms
    Review URL: https://codereview.chromium.org/560613002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181773 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/QualifiedName.cpp
+++ third_party/WebKit/Source/core/dom/QualifiedName.cpp
@@ -35,6 +35,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+struct SameSizeAsQualifiedNameImpl : public RefCounted<SameSizeAsQualifiedNameImpl> {
+    unsigned bitfield;
+    void* pointers[4];
+};
+
+COMPILE_ASSERT(sizeof(QualifiedName::QualifiedNameImpl) == sizeof(SameSizeAsQualifiedNameImpl), qualified_name_impl_should_stay_small);
+
 static const int staticQualifiedNamesCount = HTMLNames::HTMLTagsCount + HTMLNames::HTMLAttrsCount
     + MathMLNames::MathMLTagsCount + MathMLNames::MathMLAttrsCount
     + SVGNames::SVGTagsCount + SVGNames::SVGAttrsCount
