60d5b2ccc235d68f81387c35ddfe7989bbc7d0db
lazyboy <lazyboy@chromium.org>
2014-09-09 18:21:15 -0700

    nit: Remove all unused includes in BrowserPluginBindings.
    There seems to be a lot of them.
    
    BUG=None
    Test=None, code cleanup, no visible change.
    
    Review URL: https://codereview.chromium.org/554333003
    
    Cr-Commit-Position: refs/heads/master@{#294061}

--- content/renderer/browser_plugin/browser_plugin_bindings.h
+++ content/renderer/browser_plugin/browser_plugin_bindings.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/scoped_vector.h"
 #include "base/memory/weak_ptr.h"
-#include "ppapi/shared_impl/resource.h"
 #include "third_party/npapi/bindings/npruntime.h"
 
 namespace content {
