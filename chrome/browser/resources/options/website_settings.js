915b730ea7c42802ed82859cd17226569c44a3d1
dhnishi <dhnishi@chromium.org>
2014-09-10 11:03:48 -0700

    Allow the content setting or resource type to be passed into the Website Settings page by a hash.
    Review URL: https://codereview.chromium.org/547753004
    
    Cr-Commit-Position: refs/heads/master@{#294189}

--- chrome/browser/resources/options/website_settings.js
+++ chrome/browser/resources/options/website_settings.js
@@ -50,12 +50,7 @@ cr.define('options', function() {
       $('resourceType').onchange = function(event) {
         var target = event.target;
         assert(target.tagName == 'SELECT');
-        if (target.value == 'storage')
-          chrome.send('updateLocalStorage');
-        else if (target.value == 'battery')
-          chrome.send('updateBatteryUsage');
-        else
-          chrome.send('updateOrigins', [target.value]);
+        WebsiteSettingsManager.getInstance().updatePage_(target.value);
       };
 
       var searchBox = $('website-settings-search-box');
@@ -68,7 +63,22 @@ cr.define('options', function() {
       };
 
       this.createOriginsList_();
-      chrome.send('updateOrigins', ['geolocation']);
+      this.updatePage_('geolocation');
+    },
+
+    /**
+     * Called after the page has been shown. Show the content settings or
+     * resource auditing for the location's hash.
+     */
+    didShowPage: function() {
+      var hash = location.hash;
+      if (hash)
+        hash = hash.slice(1);
+      else
+        hash = 'geolocation';
+      this.updatePage_(hash);
+
+      $('resourceType').value = hash;
     },
 
     /**
@@ -159,6 +169,20 @@ cr.define('options', function() {
       this.queryDelayTimerId_ = window.setTimeout(this.searchOrigins.bind(this),
                                                   160);
     },
+
+    /**
+     * Updates the page with the given content setting or resource name's
+     * information.
+     * @param {string} typeName The name of the content setting or resource.
+     */
+    updatePage_: function(typeName) {
+      if (typeName == 'storage')
+        chrome.send('updateLocalStorage');
+      else if (typeName == 'battery')
+        chrome.send('updateBatteryUsage');
+      else
+        chrome.send('updateOrigins', [typeName]);
+    }
   };
 
   WebsiteSettingsManager.populateOrigins = function(allowedDict, blockedDict) {
