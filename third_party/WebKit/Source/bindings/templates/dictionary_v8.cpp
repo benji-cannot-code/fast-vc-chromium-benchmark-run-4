05163f3671112a41959eb8904f8d7ad6d6cf2a49
jl@opera.com <jl@opera.com>
2014-09-10 15:47:33 +0000

    In generated dictionary toImpl(), throw all exceptions via ExceptionState
    This allows us to simplify callers by not having a v8::TryCatch there, and
    instead only check the ExceptionState object.
    
    Review URL: https://codereview.chromium.org/556273003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181738 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/templates/dictionary_v8.cpp
+++ third_party/WebKit/Source/bindings/templates/dictionary_v8.cpp
@@ -20,6 +20,9 @@ namespace blink {
     // FIXME: Do not use Dictionary and DictionaryHelper
     // https://crbug.com/321462
     Dictionary dictionary(v8Value, isolate);
+    // FIXME: Remove this v8::TryCatch once the code is switched from
+    // Dictionary/DictionaryHelper to something that uses ExceptionState.
+    v8::TryCatch block;
     {% for member in members %}
     {{member.cpp_type}} {{member.name}};
     if (DictionaryHelper::getWithUndefinedOrNullCheck(dictionary, "{{member.name}}", {{member.name}})) {
@@ -31,6 +34,9 @@ namespace blink {
         }
     {% endif %}
         impl->{{member.setter_name}}({{member.name}});
+    } else if (block.HasCaught()) {
+        exceptionState.rethrowV8Exception(block.Exception());
+        return 0;
     }
     {% endfor %}
     return impl;
