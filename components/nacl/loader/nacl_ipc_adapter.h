f96ccbddca894f79280ec460362af3ae3329839b
Justin TerAvest <teravest@chromium.org>
2014-09-10 13:21:56 -0600

    Pepper: Stop using SRPC for irt_open_resource().
    This registers an IRT interface in Chromium instead of using the one provided
    by NaCl. This reuses the ManifestServiceChannel used for providing
    irt_open_resource() in non-SFI mode.
    
    In this change, the Chromium implementation of NACL_IRT_RESOURCE_OPEN_v0_1
    takes precedence over the one supplied by NaCl (which is SRPC-based).
    
    The SRPC-based codepath in service_runtime.cc needs to be kept because the
    PNaCl translator doesn't have the IRT available yet. I've added a check to
    enforce that's the only user of that codepath.
    
    BUG=394130
    TEST=Manually tested with a file token that didn't resolve with a local patch
    that forced GetFilePath to fail in nacl_process_host.cc and confirmed
    that URLLoader* still passed.
    CQ_EXTRA_TRYBOTS=tryserver.chromium.linux:linux_rel_precise32
    R=dmichael@chromium.org, mseaborn@chromium.org, tsepez@chromium.org
    
    Review URL: https://codereview.chromium.org/418423002
    
    Cr-Commit-Position: refs/heads/master@{#294208}

--- components/nacl/loader/nacl_ipc_adapter.h
+++ components/nacl/loader/nacl_ipc_adapter.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/basictypes.h"
+#include "base/callback.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/scoped_vector.h"
@@ -21,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/lock.h"
 #include "base/task_runner.h"
 #include "ipc/ipc_listener.h"
+#include "ipc/ipc_platform_file.h"
 #include "ppapi/c/pp_stdint.h"
 #include "ppapi/proxy/nacl_message_scanner.h"
 
@@ -113,6 +115,22 @@ class NaClIPCAdapter : public base::RefCountedThreadSafe<NaClIPCAdapter>,
   virtual void OnChannelConnected(int32 peer_pid) OVERRIDE;
   virtual void OnChannelError() OVERRIDE;
 
+  typedef base::Callback<void(IPC::PlatformFileForTransit, base::FilePath)>
+      ResolveFileTokenReplyCallback;
+
+  typedef base::Callback<void(uint64_t,  // file_token_lo
+                              uint64_t,  // file_token_hi
+                              ResolveFileTokenReplyCallback)>
+      ResolveFileTokenCallback;
+
+  // Sets a callback to be invoked for resolving file tokens received from the
+  // renderer. When the file token is resolved, the
+  // ResolveFileTokenReplyCallback passed inside the ResolveFileTokenCallback
+  // will be invoked.
+  void set_resolve_file_token_callback(ResolveFileTokenCallback cb) {
+    resolve_file_token_cb_ = cb;
+  }
+
  private:
   friend class base::RefCountedThreadSafe<NaClIPCAdapter>;
 
@@ -159,6 +177,12 @@ class NaClIPCAdapter : public base::RefCountedThreadSafe<NaClIPCAdapter>,
 
   virtual ~NaClIPCAdapter();
 
+  void OnFileTokenResolved(const IPC::Message& orig_msg,
+                           IPC::PlatformFileForTransit ipc_fd,
+                           base::FilePath file_path);
+
+  bool RewriteMessage(const IPC::Message& msg, uint32_t type);
+
   // Returns 0 if nothing is waiting.
   int LockedReceive(NaClImcTypedMsgHdr* msg);
 
@@ -183,6 +207,8 @@ class NaClIPCAdapter : public base::RefCountedThreadSafe<NaClIPCAdapter>,
   base::ConditionVariable cond_var_;
 
   scoped_refptr<base::TaskRunner> task_runner_;
+
+  ResolveFileTokenCallback resolve_file_token_cb_;
 
   // To be accessed inside of lock_ only.
   LockedData locked_data_;
--- ppapi/nacl_irt/manifest_service.h
+++ ppapi/nacl_irt/manifest_service.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/synchronization/lock.h"
 
 namespace base {
 class MessageLoopProxy;
@@ -36,6 +37,8 @@ class ManifestService {
  private:
   scoped_ptr<IPC::ChannelProxy> channel_;
   scoped_refptr<IPC::SyncMessageFilter> filter_;
+
+  base::Lock open_resource_lock_;
 
   DISALLOW_COPY_AND_ASSIGN(ManifestService);
 };
