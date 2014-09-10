ce7051d660747fc5a789a45dbe43e22f77df81a2
yfriedman <yfriedman@chromium.org>
2014-09-09 20:24:31 -0700

    Add UMA metric for "View Original" link clicks in distilled page viewer.
    Review URL: https://codereview.chromium.org/502653002
    
    Cr-Commit-Position: refs/heads/master@{#294089}

--- components/dom_distiller/content/dom_distiller_viewer_source.cc
+++ components/dom_distiller/content/dom_distiller_viewer_source.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted_memory.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/message_loop/message_loop.h"
+#include "base/metrics/user_metrics.h"
 #include "base/strings/utf_string_conversions.h"
 #include "components/dom_distiller/core/distilled_page_prefs.h"
 #include "components/dom_distiller/core/dom_distiller_service.h"
@@ -22,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_view_host.h"
+#include "content/public/browser/user_metrics.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "net/base/url_util.h"
@@ -283,6 +285,11 @@ void DomDistillerViewerSource::StartDataRequest(
   if (kViewerJsPath == path) {
     std::string js = viewer::GetJavaScript();
     callback.Run(base::RefCountedString::TakeString(&js));
+    return;
+  }
+  if (kViewerViewOriginalPath == path) {
+    content::RecordAction(base::UserMetricsAction("DomDistiller_ViewOriginal"));
+    callback.Run(NULL);
     return;
   }
   content::WebContents* web_contents =
