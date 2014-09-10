1a8a59a2c2f8cce341d14973b652eb6059b51a95
ed@opera.com <ed@opera.com>
2014-09-10 14:23:19 +0000

    [SVG2] Add support for marker orient="auto-start-reverse".
    Spec: https://svgwg.org/svg2-draft/painting.html#OrientAttribute
    
    BUG=367748
    
    Review URL: https://codereview.chromium.org/539833004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181732 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGAngle.h
+++ third_party/WebKit/Source/core/svg/SVGAngle.h
@@ -34,9 +34,11 @@ class SVGAngleTearOff;
 enum SVGMarkerOrientType {
     SVGMarkerOrientUnknown = 0,
     SVGMarkerOrientAuto,
-    SVGMarkerOrientAngle
+    SVGMarkerOrientAngle,
+    SVGMarkerOrientAutoStartReverse
 };
 template<> const SVGEnumerationStringEntries& getStaticStringEntries<SVGMarkerOrientType>();
+template<> unsigned short getMaxExposedEnumValue<SVGMarkerOrientType>();
 
 class SVGMarkerOrientEnumeration : public SVGEnumeration<SVGMarkerOrientType> {
 public:
