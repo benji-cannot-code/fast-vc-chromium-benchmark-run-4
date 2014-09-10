04c3a3a62b37b913a3117929fdc1caf4db94a018
japhet <japhet@chromium.org>
2014-09-09 20:06:46 -0700

    Give the layout test harness better information to determine when to finish.
    Currently, blink's FrameLoader.cpp contains a complex, hacky function to
    determine when to inform the rest of chromium that loading is complete. This
    complexity is only necessary to support layout tests. This puts the requisite
    logic in the layout test harness. This logic is behind a temporary flag, in
    order to permit both blink and chromium to flip to the new definition of load
    completion simultaneously.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/550343002
    
    Cr-Commit-Position: refs/heads/master@{#294085}

--- content/shell/renderer/test_runner/web_test_proxy.cc
+++ content/shell/renderer/test_runner/web_test_proxy.cc
@@ -928,7 +928,7 @@ bool WebTestProxyBase::DidFailProvisionalLoad(blink::WebLocalFrame* frame,
     PrintFrameDescription(delegate_, frame);
     delegate_->printMessage(" - didFailProvisionalLoadWithError\n");
   }
-  LocationChangeDone(frame);
+  CheckDone(frame, MainResourceLoadFailed);
   return !frame->provisionalDataSource();
 }
 
@@ -993,7 +993,7 @@ void WebTestProxyBase::DidFailLoad(blink::WebLocalFrame* frame,
     PrintFrameDescription(delegate_, frame);
     delegate_->printMessage(" - didFailLoadWithError\n");
   }
-  LocationChangeDone(frame);
+  CheckDone(frame, MainResourceLoadFailed);
 }
 
 void WebTestProxyBase::DidFinishLoad(blink::WebLocalFrame* frame) {
@@ -1001,7 +1001,7 @@ void WebTestProxyBase::DidFinishLoad(blink::WebLocalFrame* frame) {
     PrintFrameDescription(delegate_, frame);
     delegate_->printMessage(" - didFinishLoadForFrame\n");
   }
-  LocationChangeDone(frame);
+  CheckDone(frame, LoadFinished);
 }
 
 void WebTestProxyBase::DidDetectXSS(blink::WebLocalFrame* frame,
@@ -1161,6 +1161,9 @@ void WebTestProxyBase::DidFinishResourceLoad(blink::WebLocalFrame* frame,
     delegate_->printMessage(" - didFinishLoading\n");
   }
   resource_identifier_map_.erase(identifier);
+#if !defined(ENABLE_LOAD_COMPLETION_HACKS)
+  CheckDone(frame, ResourceLoadCompleted);
+#endif
 }
 
 void WebTestProxyBase::DidAddMessageToConsole(
@@ -1205,9 +1208,21 @@ void WebTestProxyBase::DidAddMessageToConsole(
   delegate_->printMessage(std::string("\n"));
 }
 
-void WebTestProxyBase::LocationChangeDone(blink::WebFrame* frame) {
+void WebTestProxyBase::CheckDone(blink::WebLocalFrame* frame,
+                                 CheckDoneReason reason) {
   if (frame != test_interfaces_->GetTestRunner()->topLoadingFrame())
     return;
+
+#if !defined(ENABLE_LOAD_COMPLETION_HACKS)
+  // Quirk for MHTML prematurely completing on resource load completion.
+  std::string mime_type = frame->dataSource()->response().mimeType().utf8();
+  if (reason == ResourceLoadCompleted && mime_type == "multipart/related")
+    return;
+
+  if (reason != MainResourceLoadFailed &&
+      (frame->isResourceLoadInProgress() || frame->isLoading()))
+    return;
+#endif
   test_interfaces_->GetTestRunner()->setTopLoadingFrame(frame, true);
 }
 
