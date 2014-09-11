1f00795468ef725de331c8050d7bf8b9dfdaef60
jitendra.ks <jitendra.ks@samsung.com>
2014-09-10 22:34:41 -0700

    Remove IDR_STATUS_TRAY_ICON_PRESSED
    IDR_STATUS_TRAY_ICON_PRESSED is only used in unit testcases. So we remove
    this so that it should not endup in pak files.
    
    BUG=367339
    
    Review URL: https://codereview.chromium.org/464163003
    
    Cr-Commit-Position: refs/heads/master@{#294324}

--- chrome/browser/status_icons/status_tray_unittest.cc
+++ chrome/browser/status_icons/status_tray_unittest.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class MockStatusIcon : public StatusIcon {
   virtual void SetImage(const gfx::ImageSkia& image) OVERRIDE {}
-  virtual void SetPressedImage(const gfx::ImageSkia& image) OVERRIDE {}
   virtual void SetToolTip(const base::string16& tool_tip) OVERRIDE {}
   virtual void DisplayBalloon(const gfx::ImageSkia& icon,
                               const base::string16& title,
