d042c20f88d25d52394f8f97685b4cc6a43b6a87
jkarlin <jkarlin@chromium.org>
2014-09-10 20:30:06 -0700

    Fix ServiceWorkerCache::Keys() unittest memory leak.
    I thought that this was fixed (Linux Valgrind had passed the content test) but it turns out that the failure is flaky so I need to run it a few times to be sure.  Fix again and run the tests a few times to make sure they work.
    
    If after submitting it's still not fixed I'll suppress it and fix it instead of reverting.
    
    Reverted CL: https://codereview.chromium.org/543093003
    
    BUG=392621
    
    Review URL: https://codereview.chromium.org/559003002
    
    Cr-Commit-Position: refs/heads/master@{#294313}

--- content/browser/service_worker/service_worker_cache_storage_manager_unittest.cc
+++ content/browser/service_worker/service_worker_cache_storage_manager_unittest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "content/public/test/test_browser_context.h"
 #include "content/public/test/test_browser_thread_bundle.h"
+#include "net/disk_cache/simple/simple_backend_impl.h"
 #include "net/url_request/url_request_context_getter.h"
 #include "storage/browser/blob/blob_storage_context.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -49,6 +50,16 @@ class ServiceWorkerCacheStorageManagerTest : public testing::Test {
 
     cache_manager_->SetBlobParametersForCache(
         url_request_context, blob_storage_context->context()->AsWeakPtr());
+  }
+
+  virtual void TearDown() OVERRIDE {
+    base::RunLoop().RunUntilIdle();
+    disk_cache::SimpleBackendImpl::FlushWorkerPoolForTesting();
+    base::RunLoop().RunUntilIdle();
+    cache_manager_.reset();
+    base::RunLoop().RunUntilIdle();
+    disk_cache::SimpleBackendImpl::FlushWorkerPoolForTesting();
+    base::RunLoop().RunUntilIdle();
   }
 
   virtual bool MemoryOnly() { return false; }
