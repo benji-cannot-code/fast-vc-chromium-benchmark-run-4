f66825d68146cb2b81d25cf04bdbd190b00675ef
michaeln <michaeln@chromium.org>
2014-09-11 13:25:26 -0700

    Better coordinate what's responsible for associating a registration to a document. During navigation, its the ServiceWorkerControlleeRequestHandler responsibility, but the ServiceWorkerRegisterJob takes over there after.
    Review URL: https://codereview.chromium.org/558873003
    
    Cr-Commit-Position: refs/heads/master@{#294457}

--- content/browser/service_worker/service_worker_provider_host.cc
+++ content/browser/service_worker/service_worker_provider_host.cc
@@ -29,7 +29,8 @@ ServiceWorkerProviderHost::ServiceWorkerProviderHost(
     : process_id_(process_id),
       provider_id_(provider_id),
       context_(context),
-      dispatcher_host_(dispatcher_host) {
+      dispatcher_host_(dispatcher_host),
+      allow_association_(true) {
 }
 
 ServiceWorkerProviderHost::~ServiceWorkerProviderHost() {
@@ -184,7 +185,7 @@ bool ServiceWorkerProviderHost::CanAssociateRegistration(
     return false;
   if (running_hosted_version_.get())
     return false;
-  if (!registration || associated_registration_.get())
+  if (!registration || associated_registration_.get() || !allow_association_)
     return false;
   return true;
 }
