785e50ae30dee57e839ef6ef63a92ee5b02c021e
loislo <loislo@chromium.org>
2014-09-11 03:58:11 -0700

    Gardening: Revert "Use AudioStreamMonitor to control power save blocking."
    This reverts commit 484d29dcb45ca362fc9049c37dd3a51e11492f14.
    
    This patch made media/media-controller-playbackrate.html test crash on DEBUG.
    
    it crashes at content/browser/web_contents/web_contents_impl.cc:4296 on DCHECK
    Looks like AddMediaPlayerEntry was called twice for the same player_cookie
    
    It is not clear how it happens due to async nature of the notification.
    The author is sleeping at the moment, so I decided to roll back the patch.
    
    BUG=
    TBR=dalecurtis@chromium.org
    
    Review URL: https://codereview.chromium.org/562863002
    
    Cr-Commit-Position: refs/heads/master@{#294370}

--- media/audio/audio_output_controller.cc
+++ media/audio/audio_output_controller.cc
@@ -18,6 +18,12 @@ using base::TimeDelta;
 
 namespace media {
 
+#if defined(AUDIO_POWER_MONITORING)
+// Time constant for AudioPowerMonitor.  See AudioPowerMonitor ctor comments for
+// semantics.  This value was arbitrarily chosen, but seems to work well.
+static const int kPowerMeasurementTimeConstantMillis = 10;
+#endif
+
 AudioOutputController::AudioOutputController(
     AudioManager* audio_manager,
     EventHandler* handler,
@@ -34,9 +40,11 @@ AudioOutputController::AudioOutputController(
       state_(kEmpty),
       sync_reader_(sync_reader),
       message_loop_(audio_manager->GetTaskRunner()),
+#if defined(AUDIO_POWER_MONITORING)
       power_monitor_(
           params.sample_rate(),
           TimeDelta::FromMilliseconds(kPowerMeasurementTimeConstantMillis)),
+#endif
       on_more_io_data_called_(0) {
   DCHECK(audio_manager);
   DCHECK(handler_);
@@ -193,9 +201,11 @@ void AudioOutputController::StopStream() {
     wedge_timer_.reset();
     stream_->Stop();
 
+#if defined(AUDIO_POWER_MONITORING)
     // A stopped stream is silent, and power_montior_.Scan() is no longer being
     // called; so we must reset the power monitor.
     power_monitor_.Reset();
+#endif
 
     state_ = kPaused;
   }
@@ -297,8 +307,9 @@ int AudioOutputController::OnMoreData(AudioBus* dest,
   sync_reader_->UpdatePendingBytes(
       buffers_state.total_bytes() + frames * params_.GetBytesPerFrame());
 
-  if (will_monitor_audio_levels())
-    power_monitor_.Scan(*dest, frames);
+#if defined(AUDIO_POWER_MONITORING)
+  power_monitor_.Scan(*dest, frames);
+#endif
 
   return frames;
 }
@@ -402,8 +413,12 @@ void AudioOutputController::DoStopDiverting() {
 }
 
 std::pair<float, bool> AudioOutputController::ReadCurrentPowerAndClip() {
-  DCHECK(will_monitor_audio_levels());
+#if defined(AUDIO_POWER_MONITORING)
   return power_monitor_.ReadCurrentPowerAndClip();
+#else
+  NOTREACHED();
+  return std::make_pair(AudioPowerMonitor::zero_power(), false);
+#endif
 }
 
 void AudioOutputController::WedgeCheck() {
