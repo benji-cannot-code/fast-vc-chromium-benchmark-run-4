49e54210abc4ef62d2c810078e1d5445983bd26b
sammc <sammc@chromium.org>
2014-09-10 17:52:33 -0700

    Make some fields non-nullable in serial.mojom and data_stream.mojom.
    This also removes some null checks for the now non-nullable fields.
    
    BUG=407683
    
    Review URL: https://codereview.chromium.org/562563002
    
    Cr-Commit-Position: refs/heads/master@{#294277}

--- device/serial/data_sink_receiver.cc
+++ device/serial/data_sink_receiver.cc
@@ -90,7 +90,7 @@ DataSinkReceiver::~DataSinkReceiver() {
 }
 
 void DataSinkReceiver::Init(mojo::ScopedDataPipeConsumerHandle handle) {
-  if (!handle.is_valid() || handle_.is_valid()) {
+  if (handle_.is_valid()) {
     DispatchFatalError();
     return;
   }
