e3d0b19180f23f501273b80c0fcd10f934fb9d96
mkwst@chromium.org <mkwst@chromium.org>
2014-09-11 12:57:28 +0000

    CSP: Move parsing a document's CSP to DocumentLoader::responseReceived.
    This patch moves CSP generation out of FrameLoader and into
    DocumentLoader in order to ensure that the 'frame-ancestors' directive
    can properly block loading a document. The current implementation checks
    too late in the loading process.
    
    The new implementation works, with the caveat that it breaks both
    console messages and violation reports. Since we have no
    ExecutionContext, we have nothing to dump console messages into. This
    work will be covered in https://crbug.com/412725
    
    BUG=411889,411600,357462
    
    Review URL: https://codereview.chromium.org/563473002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181814 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/DocumentLoader.h
+++ third_party/WebKit/Source/core/loader/DocumentLoader.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/fetch/RawResource.h"
 #include "core/fetch/ResourceLoaderOptions.h"
 #include "core/fetch/ResourcePtr.h"
+#include "core/frame/csp/ContentSecurityPolicy.h"
 #include "core/loader/DocumentLoadTiming.h"
 #include "core/loader/DocumentWriter.h"
 #include "core/loader/NavigationAction.h"
@@ -137,6 +138,8 @@ namespace blink {
         void clearRedirectChain();
         void appendRedirect(const KURL&);
 
+        PassRefPtr<ContentSecurityPolicy> releaseContentSecurityPolicy() { return m_contentSecurityPolicy.release(); }
+
     protected:
         DocumentLoader(LocalFrame*, const ResourceRequest&, const SubstituteData&);
 
@@ -166,6 +169,7 @@ namespace blink {
         void willSendRequest(ResourceRequest&, const ResourceResponse&);
         void finishedLoading(double finishTime);
         void mainReceivedError(const ResourceError&);
+        void cancelLoadAfterXFrameOptionsOrCSPDenied(const ResourceResponse&);
         virtual void redirectReceived(Resource*, ResourceRequest&, const ResourceResponse&) OVERRIDE FINAL;
         virtual void updateRequest(Resource*, const ResourceRequest&) OVERRIDE FINAL;
         virtual void responseReceived(Resource*, const ResourceResponse&) OVERRIDE FINAL;
@@ -221,6 +225,8 @@ namespace blink {
 
         friend class ApplicationCacheHost;  // for substitute resource delivery
         OwnPtrWillBePersistent<ApplicationCacheHost> m_applicationCacheHost;
+
+        RefPtr<ContentSecurityPolicy> m_contentSecurityPolicy;
     };
 }
 
