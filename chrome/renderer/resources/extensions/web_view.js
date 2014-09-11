4f5f7bd6c5e3cb28cf46537b727a1dde92f32ffc
lfg <lfg@chromium.org>
2014-09-11 11:06:38 -0700

    Moving webview cleardata function.
    BUG=352293
    
    Review URL: https://codereview.chromium.org/567643002
    
    Cr-Commit-Position: refs/heads/master@{#294420}

--- chrome/renderer/resources/extensions/web_view.js
+++ chrome/renderer/resources/extensions/web_view.js
@@ -240,7 +240,7 @@ WebViewInternal.prototype.clearData = function() {
     return;
   }
   var args = $Array.concat([this.guestInstanceId], $Array.slice(arguments));
-  $Function.apply(ChromeWebView.clearData, null, args);
+  $Function.apply(WebView.clearData, null, args);
 };
 
 /**
