bb679346cac2cb7307c30ac37e756ed14b0bfb9b
kpschoedel <kpschoedel@chromium.org>
2014-09-11 15:14:44 -0700

    Disable XI2 events on mirror window
    BUG=406064
    
    Review URL: https://codereview.chromium.org/558503005
    
    Cr-Commit-Position: refs/heads/master@{#294477}

--- ash/display/mirror_window_controller.cc
+++ ash/display/mirror_window_controller.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(USE_X11)
 #include <X11/Xlib.h>
+#include <X11/extensions/XInput2.h>
 
 // Xlib.h defines RootWindow.
 #undef RootWindow
@@ -46,6 +47,12 @@ void DisableInput(XID window) {
   long event_mask = ExposureMask | VisibilityChangeMask |
       StructureNotifyMask | PropertyChangeMask;
   XSelectInput(gfx::GetXDisplay(), window, event_mask);
+  unsigned char mask[XIMaskLen(XI_LASTEVENT)] = {0};
+  XIEventMask evmask;
+  evmask.deviceid = XIAllDevices;
+  evmask.mask_len = sizeof(mask);
+  evmask.mask = mask;
+  XISelectEvents(gfx::GetXDisplay(), window, &evmask, 1);
 }
 #endif
 
