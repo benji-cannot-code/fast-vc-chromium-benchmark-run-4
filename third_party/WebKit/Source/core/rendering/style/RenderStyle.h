7fe9182de4febf73fd5350026ac9b3835e1d9bae
rune@opera.com <rune@opera.com>
2014-09-11 08:26:54 +0000

    Removed stray declared method.
    Left-over from the removal of diffNeedsRecompositeLayer [1].
    
    [1] https://codereview.chromium.org/360103008
    
    R=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/565583002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181801 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/style/RenderStyle.h
+++ third_party/WebKit/Source/core/rendering/style/RenderStyle.h
@@ -1816,7 +1816,6 @@ private:
     bool diffNeedsFullLayout(const RenderStyle& other) const;
     bool diffNeedsPaintInvalidationLayer(const RenderStyle& other) const;
     bool diffNeedsPaintInvalidationObject(const RenderStyle& other) const;
-    bool diffNeedsRecompositeLayer(const RenderStyle& other) const;
     void updatePropertySpecificDifferences(const RenderStyle& other, StyleDifference&) const;
 };
 
