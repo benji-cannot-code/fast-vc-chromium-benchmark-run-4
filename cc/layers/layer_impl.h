d0d3eb118cd8a1409ea73a50bfd6458b866a73e8
vmpstr <vmpstr@chromium.org>
2014-09-10 17:58:37 -0700

    Reland of: cc: Add occlusion checker as a fixed view of occlusion tracker.
    This patch adds an occlusion checker to get the state of
    occlusion tracker for permanent checks. That is, occlusion
    checker does not have to be queried during a layer walk.
    
    BUG=410932
    R=danakj
    
    Committed: https://crrev.com/1af780529157c94a9e0f05bcf478543b85714f6b
    Cr-Commit-Position: refs/heads/master@{#294205}
    
    Review URL: https://codereview.chromium.org/547723002
    
    Cr-Commit-Position: refs/heads/master@{#294280}

--- cc/layers/layer_impl.h
+++ cc/layers/layer_impl.h
@@ -50,6 +50,7 @@ namespace cc {
 class LayerTreeHostImpl;
 class LayerTreeImpl;
 class MicroBenchmarkImpl;
+class Occlusion;
 template <typename LayerType>
 class OcclusionTracker;
 class RenderPass;
@@ -205,8 +206,7 @@ class CC_EXPORT LayerImpl : public LayerAnimationValueObserver,
   virtual RenderPassId FirstContributingRenderPassId() const;
   virtual RenderPassId NextContributingRenderPassId(RenderPassId id) const;
 
-  virtual void UpdateTiles(
-      const OcclusionTracker<LayerImpl>* occlusion_tracker) {}
+  virtual void UpdateTiles(const Occlusion& occlusion_in_layer_space) {}
   virtual void NotifyTileStateChanged(const Tile* tile) {}
 
   virtual ScrollbarLayerImplBase* ToScrollbarLayer();
--- cc/resources/picture_layer_tiling.h
+++ cc/resources/picture_layer_tiling.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/base/tiling_data.h"
 #include "cc/resources/tile.h"
 #include "cc/resources/tile_priority.h"
+#include "cc/trees/occlusion.h"
 #include "ui/gfx/rect.h"
 
 namespace base {
@@ -28,8 +29,6 @@ class TracedValue;
 
 namespace cc {
 
-template <typename LayerType>
-class OcclusionTracker;
 class PictureLayerTiling;
 class PicturePileImpl;
 
@@ -238,14 +237,11 @@ class CC_EXPORT PictureLayerTiling {
 
   void Reset();
 
-  void UpdateTilePriorities(
-      WhichTree tree,
-      const gfx::Rect& visible_layer_rect,
-      float ideal_contents_scale,
-      double current_frame_time_in_seconds,
-      const OcclusionTracker<LayerImpl>* occlusion_tracker,
-      const LayerImpl* render_target,
-      const gfx::Transform& draw_transform);
+  void UpdateTilePriorities(WhichTree tree,
+                            const gfx::Rect& visible_layer_rect,
+                            float ideal_contents_scale,
+                            double current_frame_time_in_seconds,
+                            const Occlusion& occlusion_in_layer_space);
 
   // Copies the src_tree priority into the dst_tree priority for all tiles.
   // The src_tree priority is reset to the lowest priority possible.  This
