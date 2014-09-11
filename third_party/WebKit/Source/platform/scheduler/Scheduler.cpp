03f75544e2ced2a21fa0a0d22b2aa8c520739692
alexclarke@chromium.org <alexclarke@chromium.org>
2014-09-11 13:15:25 +0000

    Adds the concept of Policies to the Blink Scheduler
    In Normal mode (the default), there's virtually no 
    prioritization of compositor events, and
    shouldYieldForHighPriorityWork always returns false. In 
    CompositorPriority mode, there *is* prioritization of 
    compositor tasks and shouldYieldForHighPriorityWork will 
    return false if there are pending high priority events.
    
    Currently there is no way for client code to set the policy
    but that will be introduced in a follow up CL.
    
    Also adds some disabled by default tracing in
    "blink.scheduler" to track the number of pending high
    priority tasks and which policy mode the scheduler is in.
    
    BUG=391005,411520
    
    Review URL: https://codereview.chromium.org/559973003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181818 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/scheduler/Scheduler.cpp
+++ third_party/WebKit/Source/platform/scheduler/Scheduler.cpp
@@ -19,6 +19,9 @@ namespace blink {
 
 namespace {
 
+// The time we should stay in CompositorPriority mode for, after a touch event.
+double kLowSchedulerPolicyAfterTouchTimeSeconds = 0.1;
+
 // Can be created from any thread.
 // Note if the scheduler gets shutdown, this may be run after.
 class MainThreadIdleTaskAdapter : public WebThread::Task {
@@ -64,6 +67,8 @@ public:
         ASSERT(scheduler);
         if (!scheduler)
             return;
+        // NOTE we must unconditionally execute high priority tasks here, since if we're not in CompositorPriority
+        // mode, then this is the only place where high priority tasks will be executed.
         scheduler->swapQueuesRunPendingTasksAndAllowHighPriorityTaskRunnerPosting();
     }
 };
@@ -87,7 +92,7 @@ public:
         // FIXME: This check should't be necessary, tasks should not outlive blink.
         ASSERT(scheduler);
         if (scheduler)
-            Scheduler::shared()->swapQueuesAndRunPendingTasks();
+            Scheduler::shared()->runPendingHighPriorityTasksIfInCompositorPriority();
         m_task.run();
     }
 
@@ -115,8 +120,10 @@ Scheduler* Scheduler::shared()
 Scheduler::Scheduler()
     : m_sharedTimerFunction(nullptr)
     , m_mainThread(blink::Platform::current()->currentThread())
+    , m_compositorPriorityPolicyEndTimeSeconds(0)
     , m_highPriorityTaskCount(0)
     , m_highPriorityTaskRunnerPosted(false)
+    , m_schedulerPolicy(Normal)
 {
 }
 
@@ -154,6 +161,7 @@ void Scheduler::postInputTask(const TraceLocation& location, const Task& task)
     m_pendingHighPriorityTasks.append(TracedTask(task, location));
     atomicIncrement(&m_highPriorityTaskCount);
     maybePostMainThreadPendingHighPriorityTaskRunner();
+    TRACE_COUNTER1(TRACE_DISABLED_BY_DEFAULT("blink.scheduler"), "PendingHighPriorityTasks", m_highPriorityTaskCount);
 }
 
 void Scheduler::postCompositorTask(const TraceLocation& location, const Task& task)
@@ -162,6 +170,7 @@ void Scheduler::postCompositorTask(const TraceLocation& location, const Task& ta
     m_pendingHighPriorityTasks.append(TracedTask(task, location));
     atomicIncrement(&m_highPriorityTaskCount);
     maybePostMainThreadPendingHighPriorityTaskRunner();
+    TRACE_COUNTER1(TRACE_DISABLED_BY_DEFAULT("blink.scheduler"), "PendingHighPriorityTasks", m_highPriorityTaskCount);
 }
 
 void Scheduler::maybePostMainThreadPendingHighPriorityTaskRunner()
@@ -183,13 +192,22 @@ void Scheduler::tickSharedTimer()
     TRACE_EVENT0("blink", "Scheduler::tickSharedTimer");
 
     // Run any high priority tasks that are queued up, otherwise the blink timers will yield immediately.
-    bool workDone = swapQueuesAndRunPendingTasks();
+    bool workDone = runPendingHighPriorityTasksIfInCompositorPriority();
     m_sharedTimerFunction();
 
     // The blink timers may have just yielded, so run any high priority tasks that where queued up
     // while the blink timers were executing.
     if (!workDone)
-        swapQueuesAndRunPendingTasks();
+        runPendingHighPriorityTasksIfInCompositorPriority();
+}
+
+bool Scheduler::runPendingHighPriorityTasksIfInCompositorPriority()
+{
+    ASSERT(isMainThread());
+    if (schedulerPolicy() != CompositorPriority)
+        return false;
+
+    return swapQueuesAndRunPendingTasks();
 }
 
 bool Scheduler::swapQueuesAndRunPendingTasks()
@@ -200,6 +218,7 @@ bool Scheduler::swapQueuesAndRunPendingTasks()
     // One the buffers have been swapped we can safely access the returned deque without having to lock.
     m_pendingTasksMutex.lock();
     Deque<TracedTask>& highPriorityTasks = m_pendingHighPriorityTasks.swapBuffers();
+    maybeEnterNormalSchedulerPolicy();
     m_pendingTasksMutex.unlock();
     return executeHighPriorityTasks(highPriorityTasks);
 }
@@ -213,8 +232,19 @@ void Scheduler::swapQueuesRunPendingTasksAndAllowHighPriorityTaskRunnerPosting()
     m_pendingTasksMutex.lock();
     Deque<TracedTask>& highPriorityTasks = m_pendingHighPriorityTasks.swapBuffers();
     m_highPriorityTaskRunnerPosted = false;
+    maybeEnterNormalSchedulerPolicy();
     m_pendingTasksMutex.unlock();
     executeHighPriorityTasks(highPriorityTasks);
+}
+
+void Scheduler::maybeEnterNormalSchedulerPolicy()
+{
+    ASSERT(isMainThread());
+    ASSERT(m_pendingTasksMutex.locked());
+
+    // Go back to the normal scheduler policy if enough time has elapsed.
+    if (schedulerPolicy() == CompositorPriority && Platform::current()->monotonicallyIncreasingTime() > m_compositorPriorityPolicyEndTimeSeconds)
+        enterSchedulerPolicyLocked(Normal);
 }
 
 bool Scheduler::executeHighPriorityTasks(Deque<TracedTask>& highPriorityTasks)
@@ -228,6 +258,7 @@ bool Scheduler::executeHighPriorityTasks(Deque<TracedTask>& highPriorityTasks)
 
     int highPriorityTaskCount = atomicSubtract(&m_highPriorityTaskCount, highPriorityTasksExecuted);
     ASSERT_UNUSED(highPriorityTaskCount, highPriorityTaskCount >= 0);
+    TRACE_COUNTER1(TRACE_DISABLED_BY_DEFAULT("blink.scheduler"), "PendingHighPriorityTasks", m_highPriorityTaskCount);
     return highPriorityTasksExecuted > 0;
 }
 
@@ -254,6 +285,10 @@ void Scheduler::stopSharedTimer()
 
 bool Scheduler::shouldYieldForHighPriorityWork() const
 {
+    // It's only worthwhile yielding in CompositorPriority mode.
+    if (schedulerPolicy() != CompositorPriority)
+        return false;
+
     return hasPendingHighPriorityWork();
 }
 
@@ -265,6 +300,31 @@ bool Scheduler::hasPendingHighPriorityWork() const
     // should be cheaper.
     // NOTE it's possible the barrier read is overkill here, since delayed yielding isn't a big deal.
     return acquireLoad(&m_highPriorityTaskCount) != 0;
+}
+
+Scheduler::SchedulerPolicy Scheduler::schedulerPolicy() const
+{
+    ASSERT(isMainThread());
+    // It's important not to miss the transition from normal to low latency mode, otherwise we're likely to
+    // delay the processing of input tasks. Since that transition is triggered by a different thread, we
+    // need either a lock or a memory barrier, and the memory barrier is probably cheaper.
+    return static_cast<SchedulerPolicy>(acquireLoad(&m_schedulerPolicy));
+}
+
+void Scheduler::enterSchedulerPolicy(SchedulerPolicy schedulerPolicy)
+{
+    Locker<Mutex> lock(m_pendingTasksMutex);
+    enterSchedulerPolicyLocked(schedulerPolicy);
+}
+
+void Scheduler::enterSchedulerPolicyLocked(SchedulerPolicy schedulerPolicy)
+{
+    ASSERT(m_pendingTasksMutex.locked());
+    if (schedulerPolicy == CompositorPriority)
+        m_compositorPriorityPolicyEndTimeSeconds = Platform::current()->monotonicallyIncreasingTime() + kLowSchedulerPolicyAfterTouchTimeSeconds;
+
+    releaseStore(&m_schedulerPolicy, schedulerPolicy);
+    TRACE_COUNTER1(TRACE_DISABLED_BY_DEFAULT("blink.scheduler"), "SchedulerPolicy", schedulerPolicy);
 }
 
 void Scheduler::TracedTask::run()
