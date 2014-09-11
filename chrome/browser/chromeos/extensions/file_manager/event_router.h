8d141559dd956c710b876f5c6c1345f3f2d17b28
iseki <iseki@chromium.org>
2014-09-11 00:29:05 -0700

    Add notification and observer to copy the file between other users.
    * NotificationRegister and NotificationObserver is need to collect
      file information.
    
    * This CL is related to https://codereview.chromium.org/530553006
    
    BUG=412691
    
    Review URL: https://codereview.chromium.org/566433004
    
    Cr-Commit-Position: refs/heads/master@{#294346}

--- chrome/browser/chromeos/extensions/file_manager/event_router.h
+++ chrome/browser/chromeos/extensions/file_manager/event_router.h
@@ -53,7 +53,8 @@ class EventRouter : public chromeos::NetworkStateHandlerObserver,
                     public drive::FileSystemObserver,
                     public drive::JobListObserver,
                     public drive::DriveServiceObserver,
-                    public VolumeManagerObserver {
+                    public VolumeManagerObserver,
+                    public content::NotificationObserver {
  public:
   explicit EventRouter(Profile* profile);
   virtual ~EventRouter();
@@ -127,6 +128,11 @@ class EventRouter : public chromeos::NetworkStateHandlerObserver,
   virtual void OnFormatCompleted(
       const std::string& device_path, bool success) OVERRIDE;
 
+  // content::NotificationObserver overrides.
+  virtual void Observe(int type,
+                       const content::NotificationSource& source,
+                       const content::NotificationDetails& details) OVERRIDE;
+
  private:
   typedef std::map<base::FilePath, FileWatcher*> WatcherMap;
 
@@ -197,6 +203,8 @@ class EventRouter : public chromeos::NetworkStateHandlerObserver,
   WatcherMap file_watchers_;
   scoped_ptr<PrefChangeRegistrar> pref_change_registrar_;
   Profile* profile_;
+
+  content::NotificationRegistrar notification_registrar_;
 
   scoped_ptr<DeviceEventRouter> device_event_router_;
 
