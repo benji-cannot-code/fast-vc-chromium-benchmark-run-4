b794628e4a194b4eb2d75dcfb82314ddc88cb0e9
orglofch <orglofch@chromium.org>
2014-09-10 18:56:17 -0700

    cc:: Add LatencyQuery logic back into OutputSurface.
    Flickering no longer evident on Note 3 Device.
    
    BUG=317928
    
    Review URL: https://codereview.chromium.org/560123002
    
    Cr-Commit-Position: refs/heads/master@{#294292}

--- cc/output/output_surface.h
+++ cc/output/output_surface.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "cc/base/cc_export.h"
+#include "cc/base/rolling_time_delta_history.h"
 #include "cc/output/context_provider.h"
 #include "cc/output/overlay_candidate_validator.h"
 #include "cc/output/software_output_device.h"
@@ -128,6 +129,10 @@ class CC_EXPORT OutputSurface {
 
   bool HasClient() { return !!client_; }
 
+  // Returns an estimate of the current GPU latency. When only a software
+  // device is present, returns 0.
+  base::TimeDelta GpuLatencyEstimate();
+
   // Get the class capable of informing cc of hardware overlay capability.
   OverlayCandidateValidator* overlay_candidate_validator() const {
     return overlay_candidate_validator_.get();
@@ -170,10 +175,15 @@ class CC_EXPORT OutputSurface {
   void SetUpContext3d();
   void ResetContext3d();
   void SetMemoryPolicy(const ManagedMemoryPolicy& policy);
+  void UpdateAndMeasureGpuLatency();
 
   bool external_stencil_test_enabled_;
 
   base::WeakPtrFactory<OutputSurface> weak_ptr_factory_;
+
+  std::deque<unsigned> available_gpu_latency_query_ids_;
+  std::deque<unsigned> pending_gpu_latency_query_ids_;
+  RollingTimeDeltaHistory gpu_latency_history_;
 
   DISALLOW_COPY_AND_ASSIGN(OutputSurface);
 };
