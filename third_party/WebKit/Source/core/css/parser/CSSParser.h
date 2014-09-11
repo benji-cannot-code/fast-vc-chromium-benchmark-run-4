890b2b9bd53537002b255b2367d5bc5d995dd029
sigbjornf@opera.com <sigbjornf@opera.com>
2014-09-11 22:24:34 +0000

    Oilpan: fix build after r181825.
    TBR=oilpan-reviews
    BUG=413341
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/563923003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181852 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/parser/CSSParser.h
+++ third_party/WebKit/Source/core/css/parser/CSSParser.h
@@ -34,8 +34,8 @@ public:
 
     static bool parseSupportsCondition(const String&);
 
-    static PassRefPtr<CSSValueList> parseFontFaceValue(const AtomicString&);
-    static PassRefPtr<CSSValue> parseAnimationTimingFunctionValue(const String&);
+    static PassRefPtrWillBeRawPtr<CSSValueList> parseFontFaceValue(const AtomicString&);
+    static PassRefPtrWillBeRawPtr<CSSValue> parseAnimationTimingFunctionValue(const String&);
 
     static bool parseColor(RGBA32& color, const String&, bool strict = false);
     static bool parseSystemColor(RGBA32& color, const String&);
@@ -50,4 +50,3 @@ CSSPropertyID cssPropertyID(const String&);
 } // namespace blink
 
 #endif // CSSParser_h
-
