050a1195b4894a50b9b4b2232f1111f05e981c29
xhwang <xhwang@chromium.org>
2014-09-12 14:28:40 -0700

    Whitelist Widevine CDM for plugin UMA on all platforms.
    Previously this is only whitelisted on Linux/ChromeOS. This CL relax this to
    all platforms.
    
    BUG=413812
    TEST=Tested on Windows.
    
    Review URL: https://codereview.chromium.org/567913002
    
    Cr-Commit-Position: refs/heads/master@{#294663}

--- chrome/renderer/pepper/pepper_uma_host.cc
+++ chrome/renderer/pepper/pepper_uma_host.cc
@@ -22,6 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/host/ppapi_host.h"
 #include "ppapi/proxy/ppapi_messages.h"
 
+#include "widevine_cdm_version.h"  // In SHARED_INTERMEDIATE_DIR.
+
 namespace {
 
 const char* const kPredefinedAllowedUMAOrigins[] = {
@@ -34,8 +36,11 @@ const char* const kWhitelistedHistogramPrefixes[] = {
 };
 
 const char* const kWhitelistedPluginBaseNames[] = {
-    "libwidevinecdmadapter.so",  // see http://crbug.com/368743
-    "libpdf.so"                  // see http://crbug.com/405305
+#if defined(WIDEVINE_CDM_AVAILABLE) && defined(ENABLE_PEPPER_CDMS)
+    kWidevineCdmAdapterFileName,  // see http://crbug.com/368743
+                                  // and http://crbug.com/410630
+#endif
+    "libpdf.so"                   // see http://crbug.com/405305
 };
 
 std::string HashPrefix(const std::string& histogram) {
