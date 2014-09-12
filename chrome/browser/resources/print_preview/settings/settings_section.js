5f2e426b09ae6f0e78b449b318dcbfc2545d2aed
alekseys <alekseys@chromium.org>
2014-09-12 14:03:45 -0700

    Make Print Preview elements transitions smoother.
    BUG=397741
    
    Review URL: https://codereview.chromium.org/565213005
    
    Cr-Commit-Position: refs/heads/master@{#294656}

--- chrome/browser/resources/print_preview/settings/settings_section.js
+++ chrome/browser/resources/print_preview/settings/settings_section.js
@@ -25,7 +25,7 @@ cr.define('print_preview', function() {
      * Whether content of this section should be collapsed or not.
      * @private {boolean}
      */
-    this.collapseContent_ = false;
+    this.collapseContent_ = true;
   };
 
   /**
@@ -69,10 +69,12 @@ cr.define('print_preview', function() {
     /**
      * @param {boolean} collapseContent Whether the content of this section
      *     should be collapsed, even if this section is available.
+     * @param {boolean} noAnimation Whether section visibility transition
+     *     should not be animated.
      */
-    set collapseContent(collapseContent) {
+    setCollapseContent: function(collapseContent, noAnimation) {
       this.collapseContent_ = collapseContent && this.hasCollapsibleContent();
-      this.updateUiStateInternal();
+      this.updateUiStateInternal(noAnimation);
     },
 
     /** @override */
@@ -85,17 +87,19 @@ cr.define('print_preview', function() {
 
     /**
      * Updates the component appearance according to the current state.
+     * @param {boolean=} opt_noAnimation Whether section visibility transition
+     *     should not be animated.
      * @protected
      */
-    updateUiStateInternal: function() {
+    updateUiStateInternal: function(opt_noAnimation) {
       var hasCollapsibleContent = this.hasCollapsibleContent();
       var changed = this.hasCollapsibleContentCached_ != hasCollapsibleContent;
       this.hasCollapsibleContentCached_ = hasCollapsibleContent;
 
       if (this.isSectionVisibleInternal())
-        fadeInOption(this.getElement());
+        fadeInOption(this.getElement(), opt_noAnimation);
       else
-        fadeOutOption(this.getElement());
+        fadeOutOption(this.getElement(), opt_noAnimation);
 
       if (changed) {
         cr.dispatchSimpleEvent(
