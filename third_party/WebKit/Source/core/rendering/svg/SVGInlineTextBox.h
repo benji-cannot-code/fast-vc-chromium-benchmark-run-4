b459fe57f81c66bc12da3bb848a83f87c534d230
fs@opera.com <fs@opera.com>
2014-09-12 14:07:19 +0000

    Move transient/temporary RenderSVGResource state out of SVGInlineTextBox
    The SVGInlineTextBox member variable |m_paintingResource| is only set
    temporarily within paintTextWithShadows and paintDecorationWithStyle, and
    is only accessed from the methods that do setup and teardown of it. This
    makes it possible to remove it from SVGInlineTextBox and instead manage it
    in through a variable on the stack.
    The lifetime management is kept manual/the same as previously. The
    interaction with TextRun::RenderingContext is changed to be passed as a
    parameter to the added "scope" object in order to simplify the interface
    a bit.
    
    Review URL: https://codereview.chromium.org/568723002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181904 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/svg/SVGInlineTextBox.h
+++ third_party/WebKit/Source/core/rendering/svg/SVGInlineTextBox.h
@@ -66,14 +66,6 @@ public:
 private:
     TextRun constructTextRun(RenderStyle*, const SVGTextFragment&) const;
 
-    bool acquirePaintingResource(GraphicsContext*&, float scalingFactor, RenderObject*,
-        RenderStyle*, RenderSVGResourceModeFlags);
-    void releasePaintingResource(GraphicsContext*&, const Path*, RenderSVGResourceModeFlags);
-
-    bool prepareGraphicsContextForTextPainting(GraphicsContext*&, float scalingFactor, TextRun&,
-        RenderStyle*, RenderSVGResourceModeFlags);
-    void restoreGraphicsContextAfterTextPainting(GraphicsContext*&, TextRun&, RenderSVGResourceModeFlags);
-
     void paintDecoration(GraphicsContext*, TextDecoration, const SVGTextFragment&);
     void paintDecorationWithStyle(GraphicsContext*, TextDecoration, const SVGTextFragment&,
         RenderObject* decorationRenderer, RenderSVGResourceModeFlags);
@@ -90,7 +82,6 @@ private:
 private:
     float m_logicalHeight;
     bool m_startsNewTextChunk : 1;
-    RenderSVGResource* m_paintingResource;
     Vector<SVGTextFragment> m_textFragments;
 };
 
