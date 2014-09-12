4719a7cb26c0b43de5d173c4d2346bfbcb69c0c0
ericzeng <ericzeng@chromium.org>
2014-09-12 14:36:15 -0700

    Embedded Extension Options: remove flags and trunk channel restrictions
    Remove flags and change trunk channel restrictions to dev in preparation
    for launch.
    
    BUG=386830
    
    Review URL: https://codereview.chromium.org/564993002
    
    Cr-Commit-Position: refs/heads/master@{#294666}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -1334,7 +1334,6 @@ void ChromeContentBrowserClient::AppendExtraCommandLineSwitches(
       extensions::switches::kAllowLegacyExtensionManifests,
       extensions::switches::kEnableAppView,
       extensions::switches::kEnableAppWindowControls,
-      extensions::switches::kEnableEmbeddedExtensionOptions,
       extensions::switches::kEnableExperimentalExtensionApis,
       extensions::switches::kEnableScriptsRequireAction,
       extensions::switches::kExtensionsOnChromeURLs,
