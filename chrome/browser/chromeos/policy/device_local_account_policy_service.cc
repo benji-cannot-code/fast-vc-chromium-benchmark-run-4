a757ea6572fdae0d5dc70f3accf12d3d5dffc520
dcheng <dcheng@chromium.org>
2014-09-11 19:16:27 -0700

    Remove implicit conversions from scoped_refptr to T* in c/b/chromeos/policy
    This patch was generated by running the rewrite_scoped_refptr clang tool
    on a ChromeOS build.
    
    BUG=110610
    
    Review URL: https://codereview.chromium.org/549313004
    
    Cr-Commit-Position: refs/heads/master@{#294526}

--- chrome/browser/chromeos/policy/device_local_account_policy_service.cc
+++ chrome/browser/chromeos/policy/device_local_account_policy_service.cc
@@ -275,7 +275,7 @@ DeviceLocalAccountPolicyService::DeviceLocalAccountPolicyService(
 }
 
 DeviceLocalAccountPolicyService::~DeviceLocalAccountPolicyService() {
-  DCHECK(!request_context_);
+  DCHECK(!request_context_.get());
   DCHECK(policy_brokers_.empty());
 }
 
