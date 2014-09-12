9f4e10429e15ee7747c51f692da24a05cde8555a
erikchen <erikchen@chromium.org>
2014-09-12 12:40:53 -0700

    mac: Use immersive fullscreen for html5 style fullscreen.
    This accidentally changed during the large fullscreen refactor. The method
    enterPresentationModeForURL:* was previously used to enter both immersive
    fullscreen and AppKit Fullscreen. Now it is only used to enter AppKit
    Fullscreen.
    
    BUG=406656
    
    Review URL: https://codereview.chromium.org/565803004
    
    Cr-Commit-Position: refs/heads/master@{#294636}

--- chrome/browser/ui/cocoa/browser_window_controller.h
+++ chrome/browser/ui/cocoa/browser_window_controller.h
@@ -496,7 +496,7 @@ class Command;
 - (void)enterPresentationModeForURL:(const GURL&)url
                          bubbleType:(FullscreenExitBubbleType)bubbleType;
 
-// Tries to use AppKit Fullscreen, falls back to Immersive Fullscreen.
+// Enters Immersive Fullscreen for the given URL.
 - (void)enterHTML5FullscreenForURL:(const GURL&)url
                         bubbleType:(FullscreenExitBubbleType)bubbleType;
 
