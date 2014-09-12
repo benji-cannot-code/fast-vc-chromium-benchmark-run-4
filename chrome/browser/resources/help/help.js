50637aca8455023b8edb36d9a153d77f2d9a12ba
davidben <davidben@chromium.org>
2014-09-12 11:09:46 -0700

    Settings: manage location.hash explicitly on a Page.
    Maintain the current hash associated each Page's state and use that rather than
    querying location.hash directly. This fixes several long-standing issues with
    the settings search page and the back/forward list:
    
    - Opening an overlay from a search page no longer keeps the fragment in the
      URL.
    - Closing an overlay no longer loses the search in the URL.
    - Navigating back from one search page to another actually works.
    - Navigating back from the search page doesn't clear the search box.
    
    Add tests for this behavior.
    
    BUG=410204
    
    Review URL: https://codereview.chromium.org/555163007
    
    Cr-Commit-Position: refs/heads/master@{#294614}

--- chrome/browser/resources/help/help.js
+++ chrome/browser/resources/help/help.js
@@ -48,6 +48,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
    */
   window.onpopstate = function(e) {
     var pageName = PageManager.getPageNameFromPath();
-    PageManager.setState(pageName, e.state);
+    PageManager.setState(pageName, location.hash, e.state);
   };
 })();
