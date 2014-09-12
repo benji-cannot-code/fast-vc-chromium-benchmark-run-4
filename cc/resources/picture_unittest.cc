7d400aad1b83237a35c7dd77852c747abb8cbb77
danakj <danakj@chromium.org>
2014-09-12 14:29:51 -0700

    cc: Remove opaque rect from cc::Picture.
    We don't do anything with this rect so remove it.
    
    R=nduca@chromium.org, vmpstr
    BUG=413479
    
    Review URL: https://codereview.chromium.org/563963002
    
    Cr-Commit-Position: refs/heads/master@{#294664}

--- cc/resources/picture_unittest.cc
+++ cc/resources/picture_unittest.cc
@@ -74,8 +74,6 @@ TEST(PictureTest, AsBase64String) {
 
     EXPECT_EQ(one_rect_picture->LayerRect(),
               one_rect_picture_check->LayerRect());
-    EXPECT_EQ(one_rect_picture->OpaqueRect(),
-              one_rect_picture_check->OpaqueRect());
     EXPECT_EQ(0, memcmp(one_rect_buffer, one_rect_buffer_check, 4 * 100 * 100));
   }
 
@@ -105,8 +103,6 @@ TEST(PictureTest, AsBase64String) {
 
     EXPECT_EQ(two_rect_picture->LayerRect(),
               two_rect_picture_check->LayerRect());
-    EXPECT_EQ(two_rect_picture->OpaqueRect(),
-              two_rect_picture_check->OpaqueRect());
     EXPECT_EQ(0, memcmp(two_rect_buffer, two_rect_buffer_check, 4 * 100 * 100));
   }
 }
@@ -439,8 +435,6 @@ TEST(PictureTest, CreateFromSkpValue) {
 
   EXPECT_EQ(100, one_rect_picture_check->LayerRect().width());
   EXPECT_EQ(200, one_rect_picture_check->LayerRect().height());
-  EXPECT_EQ(100, one_rect_picture_check->OpaqueRect().width());
-  EXPECT_EQ(200, one_rect_picture_check->OpaqueRect().height());
 }
 
 TEST(PictureTest, RecordingModes) {
