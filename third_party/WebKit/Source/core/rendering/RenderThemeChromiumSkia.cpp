93a6fce09b0c27afcb5788c60d7c3a20c2ef3178
jianli@chromium.org <jianli@chromium.org>
2014-09-12 22:02:48 +0000

    Revert of Move the user agent styles sheets to blink_resources.grd (Part 3) (patchset #10 id:260001 of https://codereview.chromium.org/438843004/)
    Reason for revert:
    Speculative revert for breaking quite a few browser_tests and content_browsertests across multiple platforms:
    https://build.chromium.org/p/chromium.webkit/builders/Mac10.6%20Tests/builds/26427
    http://build.chromium.org/p/chromium.webkit/builders/Mac10.8%20Tests/builds/9662
    http://build.chromium.org/p/chromium.webkit/builders/Win7%20Tests/builds/5566
    
    
    Original issue's description:
    > Move the user agent styles sheets to blink_resources.grd (Part 3)
    > 
    > Blink should make use of blink_resources.grd for the inline resources of user agent stylesheets.
    > This removes the dependency upon using make-file-arrays.py which embeds these resources as strings. 
    > 
    > Also the .rodata section of libblink_web (in component build mode) is reduced by ~33kb.
    > 
    > Part 1: https://codereview.chromium.org/436843004/
    > Part 2: https://codereview.chromium.org/422023008
    > Part 2 Unit Test support: https://codereview.chromium.org/456413002/
    > 
    > BUG=312586
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=181917
    
    TBR=eseidel@chromium.org,abarth@chromium.org,jochen@chromium.org,vivekg@chromium.org,vivek.vg@samsung.com
    NOTREECHECKS=true
    NOTRY=true
    BUG=312586
    
    Review URL: https://codereview.chromium.org/567073004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181938 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderThemeChromiumSkia.cpp
+++ third_party/WebKit/Source/core/rendering/RenderThemeChromiumSkia.cpp
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "core/rendering/RenderThemeChromiumSkia.h"
 
+#include "core/UserAgentStyleSheets.h"
 #include "core/rendering/PaintInfo.h"
 #include "core/rendering/RenderBox.h"
 #include "core/rendering/RenderMediaControls.h"
@@ -32,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/rendering/RenderProgress.h"
 #include "core/rendering/RenderThemeChromiumFontProvider.h"
 #include "platform/LayoutTestSupport.h"
-#include "platform/PlatformResourceLoader.h"
 #include "platform/graphics/GraphicsContext.h"
 #include "platform/graphics/Image.h"
 #include "platform/scroll/ScrollbarTheme.h"
@@ -71,14 +71,14 @@ RenderThemeChromiumSkia::~RenderThemeChromiumSkia()
 String RenderThemeChromiumSkia::extraDefaultStyleSheet()
 {
     return RenderTheme::extraDefaultStyleSheet() +
-        loadResourceAsASCIIString("themeWin.css") +
-        loadResourceAsASCIIString("themeChromiumSkia.css") +
-        loadResourceAsASCIIString("themeChromium.css");
+        String(themeWinCss, sizeof(themeWinCss)) +
+        String(themeChromiumSkiaCss, sizeof(themeChromiumSkiaCss)) +
+        String(themeChromiumCss, sizeof(themeChromiumCss));
 }
 
 String RenderThemeChromiumSkia::extraQuirksStyleSheet()
 {
-    return loadResourceAsASCIIString("themeWinQuirks.css");
+    return String(themeWinQuirksCss, sizeof(themeWinQuirksCss));
 }
 
 bool RenderThemeChromiumSkia::supportsHover(const RenderStyle* style) const
--- third_party/WebKit/Source/web/tests/WebFrameTest.cpp
+++ third_party/WebKit/Source/web/tests/WebFrameTest.cpp
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "SkBitmap.h"
 #include "SkCanvas.h"
+#include "core/UserAgentStyleSheets.h"
 #include "core/clipboard/DataTransfer.h"
 #include "core/css/StyleSheetContents.h"
 #include "core/css/resolver/StyleResolver.h"
@@ -65,7 +66,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/rendering/compositing/RenderLayerCompositor.h"
 #include "core/testing/URLTestHelpers.h"
 #include "platform/DragImage.h"
-#include "platform/PlatformResourceLoader.h"
 #include "platform/RuntimeEnabledFeatures.h"
 #include "platform/UserGestureIndicator.h"
 #include "platform/geometry/FloatRect.h"
@@ -153,7 +153,7 @@ protected:
     void applyViewportStyleOverride(FrameTestHelpers::WebViewHelper* webViewHelper)
     {
         RefPtrWillBeRawPtr<StyleSheetContents> styleSheet = StyleSheetContents::create(CSSParserContext(UASheetMode, 0));
-        styleSheet->parseString(loadResourceAsASCIIString("viewportAndroid.css"));
+        styleSheet->parseString(String(blink::viewportAndroidCss, sizeof(blink::viewportAndroidCss)));
         OwnPtrWillBeRawPtr<RuleSet> ruleSet = RuleSet::create();
         ruleSet->addRulesFromSheet(styleSheet.get(), MediaQueryEvaluator("screen"));
 
