81f7111b5a3f88216949a2050db127f213568aa9
scherkus <scherkus@chromium.org>
2014-09-12 15:39:29 -0700

    Sprinkle some DVLOGs in media::WallClockTimeSource.
    Matches what we have in AudioRendererImpl, which is our only other
    TimeSource implementation.
    
    TBR=xhwang
    
    Review URL: https://codereview.chromium.org/551273004
    
    Cr-Commit-Position: refs/heads/master@{#294684}

--- media/base/wall_clock_time_source.cc
+++ media/base/wall_clock_time_source.cc
@@ -20,6 +20,7 @@ WallClockTimeSource::~WallClockTimeSource() {
 }
 
 void WallClockTimeSource::StartTicking() {
+  DVLOG(1) << __FUNCTION__;
   base::AutoLock auto_lock(lock_);
   DCHECK(!ticking_);
   ticking_ = true;
@@ -27,6 +28,7 @@ void WallClockTimeSource::StartTicking() {
 }
 
 void WallClockTimeSource::StopTicking() {
+  DVLOG(1) << __FUNCTION__;
   base::AutoLock auto_lock(lock_);
   DCHECK(ticking_);
   base_time_ = CurrentMediaTime_Locked();
@@ -35,6 +37,7 @@ void WallClockTimeSource::StopTicking() {
 }
 
 void WallClockTimeSource::SetPlaybackRate(float playback_rate) {
+  DVLOG(1) << __FUNCTION__ << "(" << playback_rate << ")";
   base::AutoLock auto_lock(lock_);
   // Estimate current media time using old rate to use as a new base time for
   // the new rate.
@@ -47,6 +50,7 @@ void WallClockTimeSource::SetPlaybackRate(float playback_rate) {
 }
 
 void WallClockTimeSource::SetMediaTime(base::TimeDelta time) {
+  DVLOG(1) << __FUNCTION__ << "(" << time.InMicroseconds() << ")";
   base::AutoLock auto_lock(lock_);
   CHECK(!ticking_);
   base_time_ = time;
