e81aff110c92e4b504e25318cb3f5d00af68c2b7
qinmin <qinmin@chromium.org>
2014-09-11 20:46:33 -0700

    Revert 282243 "Fix an issue that android Download Manager doesn'..."
    > Fix an issue that android Download Manager doesn't handle OMA files correctly
    >
    > For OMA files, it looks like that android download manager cannot correctly download it.
    > Some of the files cannot be downloaded, and some of the files downloaded has different binary content as the ones downloaed on chrome desktop.
    > Bypass android download manager for OMA mime types.
    >
    > BUG=382698
    >
    > Review URL: https://codereview.chromium.org/331143010
    
    TBR=tedchoc@chromium.org
    BUG=382698
    
    Review URL: https://codereview.chromium.org/567733002
    
    Cr-Commit-Position: refs/heads/master@{#294538}

--- chrome/browser/android/intercept_download_resource_throttle.cc
+++ chrome/browser/android/intercept_download_resource_throttle.cc
@@ -14,9 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chrome {
 
-static const char kOmaDrmContentMime[]  = "application/vnd.oma.drm.content";
-static const char kOmaDrmMessageMime[]  = "application/vnd.oma.drm.message";
-
 InterceptDownloadResourceThrottle::InterceptDownloadResourceThrottle(
     net::URLRequest* request,
     int render_process_id,
@@ -70,13 +67,6 @@ void InterceptDownloadResourceThrottle::ProcessDownloadRequest() {
     return;
 #endif
   }
-
-  // For OMA DRM downloads, Android Download Manager doesn't handle them
-  // correctly. Use chromium network stack instead. http://crbug.com/382698.
-  std::string mime;
-  request_->GetMimeType(&mime);
-  if (!mime.compare(kOmaDrmContentMime) || !mime.compare(kOmaDrmMessageMime))
-    return;
 
   content::DownloadControllerAndroid::Get()->CreateGETDownload(
       render_process_id_, render_view_id_, request_id_);
