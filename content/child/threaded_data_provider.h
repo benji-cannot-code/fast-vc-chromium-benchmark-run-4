85f85080a070e1626a15142439fc52040499edfd
mohan.reddy <mohan.reddy@samsung.com>
2014-09-12 02:50:38 -0700

    Refactoring the weak_ptr_factory order in src/content/child
    Changing in the intialization order of WeakPtrFactory such that all
    member variables should appear before the WeakPtrFactory to ensure
    that any WeakPtrs to Controller are invalidated before its members
    variable's destructors are executed, rendering them invalid.
    
    BUG=303818
    
    Review URL: https://codereview.chromium.org/567873003
    
    Cr-Commit-Position: refs/heads/master@{#294566}

--- content/child/threaded_data_provider.h
+++ content/child/threaded_data_provider.h
@@ -57,8 +57,6 @@ class ThreadedDataProvider {
   int shm_size_;
   scoped_ptr<base::WeakPtrFactory<ThreadedDataProvider> >
       background_thread_weak_factory_;
-  base::WeakPtrFactory<ThreadedDataProvider>
-      main_thread_weak_factory_;
   WebThreadImpl& background_thread_;
   IPC::SyncChannel* ipc_channel_;
   blink::WebThreadedDataReceiver* threaded_data_receiver_;
@@ -70,6 +68,9 @@ class ThreadedDataProvider {
     int length;
   };
   std::vector<QueuedSharedMemoryData> queued_data_;
+
+  base::WeakPtrFactory<ThreadedDataProvider>
+      main_thread_weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(ThreadedDataProvider);
 };
