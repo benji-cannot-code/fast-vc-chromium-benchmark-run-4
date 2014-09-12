01b79a811c1419b4d33c7e56ce9c4295b1e1d8ea
kulkarni.a <kulkarni.a@samsung.com>
2014-09-12 02:54:56 -0700

    android_webview  : Class member variable WeakPtrFactory in proper order.
    Maintaing the proper WeakPtrFactory destruction order to avoid the runtime
    issue like unrleased memory,etc...
    WeakPtrFactory should remain the last member so it'll be destroyed and
    invalidate its weak pointers before any other members are destroyed.
    
    BUG=303818
    
    Review URL: https://codereview.chromium.org/509453002
    
    Cr-Commit-Position: refs/heads/master@{#294569}

--- android_webview/browser/shared_renderer_state.h
+++ android_webview/browser/shared_renderer_state.h
@@ -80,7 +80,6 @@ class SharedRendererState {
 
   scoped_refptr<base::MessageLoopProxy> ui_loop_;
   BrowserViewRendererClient* client_on_ui_;
-  base::WeakPtrFactory<SharedRendererState> weak_factory_on_ui_thread_;
   base::WeakPtr<SharedRendererState> ui_thread_weak_ptr_;
   base::CancelableClosure request_draw_gl_cancelable_closure_;
 
@@ -91,6 +90,8 @@ class SharedRendererState {
   ParentCompositorDrawConstraints parent_draw_constraints_;
   cc::ReturnedResourceArray returned_resources_;
   base::Closure request_draw_gl_closure_;
+
+  base::WeakPtrFactory<SharedRendererState> weak_factory_on_ui_thread_;
 
   DISALLOW_COPY_AND_ASSIGN(SharedRendererState);
 };
