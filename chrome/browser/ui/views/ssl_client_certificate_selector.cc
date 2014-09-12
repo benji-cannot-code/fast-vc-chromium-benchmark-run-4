8ed2d9404cea39fcd7f38904cbbd572729657a8e
davidben <davidben@chromium.org>
2014-09-11 20:32:41 -0700

    Fix Linux OpenSSL build because of scoped_refptr cast.
    The clang rewriter must not have caught this since it's #ifdef'd.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/563973002
    
    Cr-Commit-Position: refs/heads/master@{#294536}

--- chrome/browser/ui/views/ssl_client_certificate_selector.cc
+++ chrome/browser/ui/views/ssl_client_certificate_selector.cc
@@ -205,7 +205,7 @@ bool SSLClientCertificateSelector::Accept() {
                    base::Unretained(this),
                    cert));
 #else
-    Unlocked(cert);
+    Unlocked(cert.get());
 #endif
     return false;  // Unlocked() will close the dialog.
   }
