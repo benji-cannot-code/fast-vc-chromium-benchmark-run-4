9055fea2e37012e00e1719195fad6def10a7b3b1
dmazzoni <dmazzoni@chromium.org>
2014-09-12 18:17:13 -0700

    Add private extension API to move the accessibility focus ring.
    BUG=314889
    
    Review URL: https://codereview.chromium.org/560983002
    
    Cr-Commit-Position: refs/heads/master@{#294719}

--- chrome/browser/accessibility/accessibility_extension_api_constants.cc
+++ chrome/browser/accessibility/accessibility_extension_api_constants.cc
@@ -43,4 +43,7 @@ const char kTypeTree[] = "tree";
 const char kTypeTreeItem[] = "treeitem";
 const char kTypeWindow[] = "window";
 
+// Errors.
+const char kErrorNotSupported[] = "This API is not supported on this platform.";
+
 }  // namespace extension_accessibility_api_constants
