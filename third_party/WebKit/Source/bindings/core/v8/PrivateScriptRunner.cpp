708cfd972c70d77c10dd799995c9e7caa82a6c4e
loislo@chromium.org <loislo@chromium.org>
2014-09-13 17:54:59 +0000

    Revert "[blink-in-js] Migrate resources required for blink-in-js to grd - part 3"
    This reverts commit 0ad77c5152a930f08e04a2db05ad5331c711333f.
    
    It breaks compilation on Linux GN (dbg) which is tree closer.
    http://build.chromium.org/p/chromium.linux/builders/Linux%20GN%20(dbg)
    
    BUG=312586
    TBR= dpranke@chromium.org, haraken@chromium.org, jochen@chromium.org
    
    Review URL: https://codereview.chromium.org/557543004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181957 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/core/v8/PrivateScriptRunner.cpp
+++ third_party/WebKit/Source/bindings/core/v8/PrivateScriptRunner.cpp
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/PrivateScriptSourcesForTesting.h"
 #endif
 #include "core/dom/ExceptionCode.h"
-#include "platform/PlatformResourceLoader.h"
 
 namespace blink {
 
@@ -41,10 +40,10 @@ static void dumpV8Message(v8::Handle<v8::Message> message)
     fprintf(stderr, "%s (line %d): %s\n", fileName.utf8().data(), lineNumber, toCoreString(errorMessage).utf8().data());
 }
 
-static v8::Handle<v8::Value> compileAndRunPrivateScript(v8::Isolate* isolate, String scriptClassName, const char* source, size_t size)
+static v8::Handle<v8::Value> compileAndRunPrivateScript(v8::Isolate* isolate, String scriptClassName, const unsigned char* source, size_t size)
 {
     v8::TryCatch block;
-    String sourceString(source, size);
+    String sourceString(reinterpret_cast<const char*>(source), size);
     String fileName = scriptClassName + ".js";
     v8::Handle<v8::Script> script = V8ScriptRunner::compileScript(v8String(isolate, sourceString), fileName, TextPosition::minimumPosition(), 0, isolate, NotSharableCrossOrigin, V8CacheOptionsOff);
     if (block.HasCaught()) {
@@ -84,8 +83,7 @@ static void installPrivateScript(v8::Isolate* isolate, String className)
     // by make_private_script_source.py.
     for (size_t index = 0; index < WTF_ARRAY_LENGTH(kPrivateScriptSources); index++) {
         if (className == kPrivateScriptSources[index].className) {
-            String resourceData = loadResourceAsASCIIString(kPrivateScriptSources[index].resourceFile);
-            compileAndRunPrivateScript(isolate, kPrivateScriptSources[index].scriptClassName, resourceData.utf8().data(), resourceData.length());
+            compileAndRunPrivateScript(isolate, kPrivateScriptSources[index].scriptClassName, kPrivateScriptSources[index].source, kPrivateScriptSources[index].size);
             compiledScriptCount++;
         }
     }
@@ -110,8 +108,7 @@ static v8::Handle<v8::Value> installPrivateScriptRunner(v8::Isolate* isolate)
         fprintf(stderr, "Private script error: Target source code was not found. (Class name = %s)\n", className.utf8().data());
         RELEASE_ASSERT_NOT_REACHED();
     }
-    String resourceData = loadResourceAsASCIIString(kPrivateScriptSources[index].resourceFile);
-    return compileAndRunPrivateScript(isolate, className, resourceData.utf8().data(), resourceData.length());
+    return compileAndRunPrivateScript(isolate, className, kPrivateScriptSources[index].source, kPrivateScriptSources[index].size);
 }
 
 static v8::Handle<v8::Object> classObjectOfPrivateScript(ScriptState* scriptState, String className)
