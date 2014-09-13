473b739bb26b2de2010272bdb9b243d2fa5e1407
ksakamoto@chromium.org <ksakamoto@chromium.org>
2014-09-13 06:13:13 +0000

    ServiceWorker: Implement navigator.serviceWorker.getRegistration [3/3]
    This completes the implementation of ServiceWorkerContainer#getRegistration().
    
    Spec: https://slightlyoff.github.io/ServiceWorker/spec/service_worker/#navigator-service-worker-getRegistration
    
    [1/3] https://codereview.chromium.org/553983010/
    [2/3] https://codereview.chromium.org/535753002/
    
    BUG=404951
    TEST=LayoutTests/http/tests/serviceworker/getregistration.html,webkit_unit_tests
    
    Review URL: https://codereview.chromium.org/540823003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181952 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/serviceworkers/ServiceWorkerContainer.h
+++ third_party/WebKit/Source/modules/serviceworkers/ServiceWorkerContainer.h
@@ -72,6 +72,7 @@ public:
 
     ScriptPromise registerServiceWorker(ScriptState*, const String& pattern, const Dictionary&);
     ScriptPromise unregisterServiceWorker(ScriptState*, const String& scope);
+    ScriptPromise getRegistration(ScriptState*, const String& documentURL);
 
     // WebServiceWorkerProviderClient overrides.
     virtual void setController(WebServiceWorker*) OVERRIDE;
