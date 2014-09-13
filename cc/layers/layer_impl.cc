e29e26b2fd71a737781ec3006d8470b6a53cf16b
danakj <danakj@chromium.org>
2014-09-12 19:56:43 -0700

    cc: Add some useful layer data to traces.
    These traces were really helpful in tracking out why things were getting
    prioritized wrong.
    
    BUG=410625
    
    Review URL: https://codereview.chromium.org/568183002
    
    Cr-Commit-Position: refs/heads/master@{#294728}

--- cc/layers/layer_impl.cc
+++ cc/layers/layer_impl.cc
@@ -1416,6 +1416,8 @@ void LayerImpl::AsValueInto(base::debug::TracedValue* state) const {
   MathUtil::AddToTracedValue(bounds_, state);
   state->EndDictionary();
 
+  state->SetDouble("opacity", opacity());
+
   state->BeginArray("position");
   MathUtil::AddToTracedValue(position_, state);
   state->EndArray();
