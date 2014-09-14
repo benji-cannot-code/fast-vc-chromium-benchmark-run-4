3e297a6556e340251f4f3298db01cb525a56dc6a
John Abd-El-Malek <jam@chromium.org>
2014-09-13 23:07:31 -0700

    Revert "Adding DumpAccessibilityTree Tests (8 of 20)"
    This reverts commit 711d923e3b9dd778e482f991f280e6dd937d1abf.
    
    flaky a bunch of times. as an example, see
    http://build.chromium.org/p/chromium.win/builders/Win7%20Tests%20%28dbg%29%282%29/builds/22538/steps/content_browsertests/logs/AccessibilityFrameset
    DumpAccessibilityTreeTest.AccessibilityFrameset (run #1):
    [ RUN      ] DumpAccessibilityTreeTest.AccessibilityFrameset
    Testing: E:\b\build\slave\Win7_Tests__dbg__2_\build\src\content\test\data\accessibility\frameset.html
    c:\b\build\slave\win_builder__dbg_\build\src\content\browser\accessibility\dump_accessibility_tree_browsertest.cc(252): error: Value of: is_different
      Actual: true
    Expected: false
    [3080:2208:0913/171203:4867433:WARNING:channel.cc(211)] WriteMessage() while shutting down
    * Line Expected
    - ---- --------
         1 ROLE_SYSTEM_DOCUMENT READONLY FOCUSABLE
         2     ROLE_SYSTEM_CLIENT FOCUSABLE
         3         IA2_ROLE_SCROLL_PANE
         4             ROLE_SYSTEM_DOCUMENT READONLY FOCUSABLE
         5                 IA2_ROLE_PARAGRAPH
         6                     ROLE_SYSTEM_STATICTEXT name='My favorite browser is '
         7                     ROLE_SYSTEM_STATICTEXT name='ABC'
         8                     ROLE_SYSTEM_STATICTEXT name='Chrome'
         9                     ROLE_SYSTEM_STATICTEXT name='!'
        10     ROLE_SYSTEM_CLIENT FOCUSABLE
        11         IA2_ROLE_SCROLL_PANE
        12             ROLE_SYSTEM_DOCUMENT READONLY FOCUSABLE
    *   13                 IA2_ROLE_PARAGRAPH
        14                     ROLE_SYSTEM_STATICTEXT name='This test is to check '
        15                     ROLE_SYSTEM_STATICTEXT name='mark tag'
        16                     ROLE_SYSTEM_STATICTEXT name='.'
        17 \u003C-- End-of-file -->
    
    Actual
    ------
    ROLE_SYSTEM_DOCUMENT READONLY FOCUSABLE
        ROLE_SYSTEM_CLIENT FOCUSABLE
            IA2_ROLE_SCROLL_PANE
                ROLE_SYSTEM_DOCUMENT READONLY FOCUSABLE
                    IA2_ROLE_PARAGRAPH
                        ROLE_SYSTEM_STATICTEXT name='My favorite browser is '
                        ROLE_SYSTEM_STATICTEXT name='ABC'
                        ROLE_SYSTEM_STATICTEXT name='Chrome'
                        ROLE_SYSTEM_STATICTEXT name='!'
        ROLE_SYSTEM_CLIENT FOCUSABLE
            IA2_ROLE_SCROLL_PANE
                ROLE_SYSTEM_DOCUMENT READONLY FOCUSABLE
    
    [  FAILED  ] DumpAccessibilityTreeTest.AccessibilityFrameset, where TypeParam =  and GetParam() =  (1903 ms)
    
    BUG=
    
    Review URL: https://codereview.chromium.org/567223002
    
    Cr-Commit-Position: refs/heads/master@{#294764}

--- content/browser/accessibility/dump_accessibility_tree_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_tree_browsertest.cc
@@ -320,10 +320,6 @@ IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest,
   RunTest(FILE_PATH_LITERAL("aria-application.html"));
 }
 
-IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityAriaAtomic) {
-  RunTest(FILE_PATH_LITERAL("aria-atomic.html"));
-}
-
 IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest,
                        AccessibilityAriaAutocomplete) {
   RunTest(FILE_PATH_LITERAL("aria-autocomplete.html"));
@@ -400,10 +396,6 @@ IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest,
   RunTest(FILE_PATH_LITERAL("aria-progressbar.html"));
 }
 
-IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityAriaRow) {
-  RunTest(FILE_PATH_LITERAL("aria-row.html"));
-}
-
 IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest,
                        AccessibilityAriaReadonly) {
   RunTest(FILE_PATH_LITERAL("aria-readonly.html"));
@@ -450,10 +442,6 @@ IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityBdo) {
   RunTest(FILE_PATH_LITERAL("bdo.html"));
 }
 
-IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityBody) {
-  RunTest(FILE_PATH_LITERAL("body.html"));
-}
-
 IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityBR) {
   RunTest(FILE_PATH_LITERAL("br.html"));
 }
@@ -464,10 +452,6 @@ IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityButtonNameCalc) {
 
 IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityCanvas) {
   RunTest(FILE_PATH_LITERAL("canvas.html"));
-}
-
-IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityCaption) {
-  RunTest(FILE_PATH_LITERAL("caption.html"));
 }
 
 IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest,
@@ -512,20 +496,12 @@ IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityForm) {
   RunTest(FILE_PATH_LITERAL("form.html"));
 }
 
-IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityFrameset) {
-  RunTest(FILE_PATH_LITERAL("frameset.html"));
-}
-
 IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityHeading) {
   RunTest(FILE_PATH_LITERAL("heading.html"));
 }
 
 IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityHR) {
   RunTest(FILE_PATH_LITERAL("hr.html"));
-}
-
-IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityI) {
-  RunTest(FILE_PATH_LITERAL("i.html"));
 }
 
 IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest,
@@ -567,10 +543,6 @@ IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest,
   RunTest(FILE_PATH_LITERAL("input-types.html"));
 }
 
-IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityIns) {
-  RunTest(FILE_PATH_LITERAL("ins.html"));
-}
-
 IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityLabel) {
   RunTest(FILE_PATH_LITERAL("label.html"));
 }
@@ -579,16 +551,8 @@ IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityLandmark) {
   RunTest(FILE_PATH_LITERAL("landmark.html"));
 }
 
-IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityLegend) {
-  RunTest(FILE_PATH_LITERAL("legend.html"));
-}
-
 IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityListMarkers) {
   RunTest(FILE_PATH_LITERAL("list-markers.html"));
-}
-
-IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityMark) {
-  RunTest(FILE_PATH_LITERAL("mark.html"));
 }
 
 IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest,
@@ -623,10 +587,6 @@ IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest,
 
 IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityOl) {
   RunTest(FILE_PATH_LITERAL("ol.html"));
-}
-
-IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityObject) {
-  RunTest(FILE_PATH_LITERAL("object.html"));
 }
 
 IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest,
