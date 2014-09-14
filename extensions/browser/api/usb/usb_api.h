b9dd890c5594c02c15bd4bd0233f593154c75d20
reillyg <reillyg@chromium.org>
2014-09-13 22:19:25 -0700

    Add chrome.usb.getConfiguration and expose extra descriptors.
    Adds a new API function, chrome.usb.getConfiguration, that returns all
    the data from the device's configuration descriptor instead of just the
    interfaces as chrome.usb.listInterfaces does. Each level of the
    descriptor hierarchy also gets an "extra_data" field which contains all
    of the miscellaneous descriptors assocated with a configuration,
    interface or endpoint.
    
    BUG=382908
    
    Review URL: https://codereview.chromium.org/558373002
    
    Cr-Commit-Position: refs/heads/master@{#294758}

--- extensions/browser/api/usb/usb_api.h
+++ extensions/browser/api/usb/usb_api.h
@@ -145,6 +145,22 @@ class UsbOpenDeviceFunction : public UsbAsyncApiFunction {
   scoped_ptr<extensions::core_api::usb::OpenDevice::Params> parameters_;
 };
 
+class UsbGetConfigurationFunction : public UsbAsyncApiFunction {
+ public:
+  DECLARE_EXTENSION_FUNCTION("usb.getConfiguration", USB_GETCONFIGURATION)
+
+  UsbGetConfigurationFunction();
+
+ protected:
+  virtual ~UsbGetConfigurationFunction();
+
+  virtual bool Prepare() OVERRIDE;
+  virtual void AsyncWorkStart() OVERRIDE;
+
+ private:
+  scoped_ptr<extensions::core_api::usb::GetConfiguration::Params> parameters_;
+};
+
 class UsbListInterfacesFunction : public UsbAsyncApiFunction {
  public:
   DECLARE_EXTENSION_FUNCTION("usb.listInterfaces", USB_LISTINTERFACES)
@@ -158,17 +174,6 @@ class UsbListInterfacesFunction : public UsbAsyncApiFunction {
   virtual void AsyncWorkStart() OVERRIDE;
 
  private:
-  bool ConvertDirectionSafely(const device::UsbEndpointDirection& input,
-                              extensions::core_api::usb::Direction* output);
-  bool ConvertSynchronizationTypeSafely(
-      const device::UsbSynchronizationType& input,
-      extensions::core_api::usb::SynchronizationType* output);
-  bool ConvertTransferTypeSafely(
-      const device::UsbTransferType& input,
-      extensions::core_api::usb::TransferType* output);
-  bool ConvertUsageTypeSafely(const device::UsbUsageType& input,
-                              extensions::core_api::usb::UsageType* output);
-
   scoped_ptr<extensions::core_api::usb::ListInterfaces::Params> parameters_;
 };
 
