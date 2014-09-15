f22b43dba1b333dde58b4ef8db24c9fc15f181ad
aandrey@chromium.org <aandrey@chromium.org>
2014-09-15 12:02:13 +0000

    DevTools: Show preview in console of ES6 Map, Set, WeakMap and WeakSet entries.
    BUG=376196
    R=yurys, vsevik
    
    Review URL: https://codereview.chromium.org/512003003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181972 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/components/ObjectPropertiesSection.js
+++ third_party/WebKit/Source/devtools/front_end/components/ObjectPropertiesSection.js
@@ -579,7 +579,7 @@ WebInspector.ObjectPropertyTreeElement.populateWithProperties = function(treeEle
 }
 
 /**
- * @param {!WebInspector.RemoteObject} object
+ * @param {?WebInspector.RemoteObject} object
  * @param {!Array.<string>} propertyPath
  * @param {function(?WebInspector.RemoteObject, boolean=)} callback
  * @return {!Element}
@@ -587,8 +587,11 @@ WebInspector.ObjectPropertyTreeElement.populateWithProperties = function(treeEle
 WebInspector.ObjectPropertyTreeElement.createRemoteObjectAccessorPropertySpan = function(object, propertyPath, callback)
 {
     var rootElement = document.createElement("span");
-    var element = rootElement.createChild("span", "properties-calculate-value-button");
+    var element = rootElement.createChild("span");
     element.textContent = WebInspector.UIString("(...)");
+    if (!object)
+        return rootElement;
+    element.classList.add("properties-calculate-value-button");
     element.title = WebInspector.UIString("Invoke property getter");
     element.addEventListener("click", onInvokeGetterClick, false);
 
@@ -722,10 +725,10 @@ WebInspector.CollectionEntriesMainTreeElement.prototype = {
             for (var i = 0; i < entries.length; ++i) {
                 var entry = entries[i];
                 if (entry.key) {
-                    entriesLocalObject.push({
+                    entriesLocalObject.push(new WebInspector.MapEntryLocalJSONObject({
                         key: runtimeModel.createRemoteObject(entry.key),
                         value: runtimeModel.createRemoteObject(entry.value)
-                    });
+                    }));
                 } else {
                     entriesLocalObject.push(runtimeModel.createRemoteObject(entry.value));
                 }
