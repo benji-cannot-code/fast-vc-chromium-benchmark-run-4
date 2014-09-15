54193c96ebc26457645494ddb4e88fe8fe459ba8
noel <noel@chromium.org>
2014-09-14 23:41:01 -0700

    Revert of [Mac] Implement a new WebSandboxSupport method to get the display's color space. (patchset #1 id:1 of https://codereview.chromium.org/549213004/)
    Reason for revert:
    Not needed, used https://codereview.chromium.org/554033002 instead.
    
    Original issue's description:
    > [Mac] Implement a new WebSandboxSupport method to get the display's color space.
    >
    > This new interface is defined at https://codereview.chromium.org/554033002/.
    >
    > BUG=397642,306348
    > R=avi@chromium.org
    >
    > Committed: https://crrev.com/b6ae4d0ecc983a71a32ab1480a70e065c718c195
    > Cr-Commit-Position: refs/heads/master@{#293852}
    
    TBR=avi@chromium.org,rsesek@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=397642,306348
    
    Review URL: https://codereview.chromium.org/563943003
    
    Cr-Commit-Position: refs/heads/master@{#294786}

--- content/renderer/renderer_webkitplatformsupport_impl.cc
+++ content/renderer/renderer_webkitplatformsupport_impl.cc
@@ -85,7 +85,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(OS_MACOSX)
-#include "base/mac/mac_util.h"
 #include "content/common/mac/font_descriptor.h"
 #include "content/common/mac/font_loader.h"
 #include "content/renderer/webscrollbarbehavior_impl_mac.h"
@@ -204,7 +203,6 @@ class RendererWebKitPlatformSupportImpl::SandboxSupport
       NSFont* src_font,
       CGFontRef* container,
       uint32* font_id);
-  virtual CGColorSpaceRef displayColorSpace();
 #elif defined(OS_POSIX)
   virtual void getFallbackFontForCharacter(
       blink::WebUChar32 character,
@@ -574,11 +572,6 @@ bool RendererWebKitPlatformSupportImpl::SandboxSupport::loadFont(
   // activated, don't reactivate it here - crbug.com/72727 .
 
   return FontLoader::CGFontRefFromBuffer(font_data, font_data_size, out);
-}
-
-CGColorSpaceRef
-RendererWebKitPlatformSupportImpl::SandboxSupport::displayColorSpace() {
-  return base::mac::GetSystemColorSpace();
 }
 
 #elif defined(OS_ANDROID)
