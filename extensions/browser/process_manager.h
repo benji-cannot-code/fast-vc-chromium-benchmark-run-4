9ca4430bd0b476d3c9a6519748e0e3503ffa7861
glider <glider@chromium.org>
2014-09-15 05:04:30 -0700

    Revert of Remove deprecated extension notification from ProcessManager. (patchset #2 id:20001 of https://codereview.chromium.org/566863002/)
    Reason for revert:
    One of the tests crashes with a UAF: http://crbug.com/414245
    
    Original issue's description:
    > Remove deprecated extension notification from ProcessManager.
    >
    > This patch used EntensionRegistryObserver instead of DEPRECATED extension
    > NOTIFICATION_EXTENSION_UNLOADED_DEPRECATED removed now.
    >
    > BUG=411568
    >
    > Committed: https://crrev.com/1bd52ffda33ffc5614dab8e3f3d677f0a528e763
    > Cr-Commit-Position: refs/heads/master@{#294799}
    
    TBR=bauerb@chromium.org,rockot@chromium.org,limasdf@gmail.com,jitendra.ks@samsung.com
    NOTREECHECKS=true
    NOTRY=true
    BUG=411568
    
    Review URL: https://codereview.chromium.org/570073002
    
    Cr-Commit-Position: refs/heads/master@{#294806}

--- extensions/browser/process_manager.h
+++ extensions/browser/process_manager.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
-#include "extensions/browser/extension_registry_observer.h"
 #include "extensions/common/view_type.h"
 
 class GURL;
@@ -42,8 +41,7 @@ class ProcessManagerObserver;
 // Manages dynamic state of running Chromium extensions. There is one instance
 // of this class per Profile. OTR Profiles have a separate instance that keeps
 // track of split-mode extensions only.
-class ProcessManager : public content::NotificationObserver,
-                       public extensions::ExtensionRegistryObserver {
+class ProcessManager : public content::NotificationObserver {
  public:
   typedef std::set<extensions::ExtensionHost*> ExtensionHostSet;
   typedef ExtensionHostSet::const_iterator const_iterator;
@@ -180,16 +178,6 @@ class ProcessManager : public content::NotificationObserver,
   virtual void Observe(int type,
                        const content::NotificationSource& source,
                        const content::NotificationDetails& details) OVERRIDE;
-
-  // extensions::ExtensionRegistryObserver:
-  virtual void OnExtensionLoaded(
-      content::BrowserContext* browser_context,
-      const extensions::Extension* extension) OVERRIDE;
-
-  virtual void OnExtensionUnloaded(
-      content::BrowserContext* browser_context,
-      const extensions::Extension* extension,
-      extensions::UnloadedExtensionInfo::Reason reason) OVERRIDE;
 
   content::NotificationRegistrar registrar_;
 
