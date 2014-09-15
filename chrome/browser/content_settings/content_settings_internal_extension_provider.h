d97a439858fdfe5a43af2df66e8f97c1a39c180b
limasdf <limasdf@gmail.com>
2014-09-14 17:59:41 -0700

    Revert of Remove deprecated extension notification from content_settings (patchset #6 id:240001 of https://codereview.chromium.org/276573011/)
    Reason for revert:
    Becuase this CL seems to break linux asan test
    http://build.chromium.org/p/tryserver.chromium.linux/builders/linux_chromium_asan_rel/builds/882
    
    Original issue's description:
    > remove deprecated extension notification from content_settings.
    >
    > Use ExtensionRegistryObserver instead.
    >
    > R=bauerb@chromium.org
    > BUG=354046, 354458
    > TEST=unit_tests
    >
    > Committed: https://crrev.com/76ad58487578183de69a8caeb48b8a22dccad652
    > Cr-Commit-Position: refs/heads/master@{#294768}
    
    TBR=bauerb@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=354046, 354458
    
    Review URL: https://codereview.chromium.org/553263004
    
    Cr-Commit-Position: refs/heads/master@{#294773}

--- chrome/browser/content_settings/content_settings_internal_extension_provider.h
+++ chrome/browser/content_settings/content_settings_internal_extension_provider.h
@@ -13,21 +13,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/common/content_settings.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
-#include "extensions/browser/extension_registry_observer.h"
 
 class ExtensionService;
 
 namespace extensions {
 class Extension;
-class ExtensionRegistry;
 }
 
 namespace content_settings {
 
 // A content settings provider which disables certain plugins for platform apps.
 class InternalExtensionProvider : public ObservableProvider,
-                                  public content::NotificationObserver,
-                                  public extensions::ExtensionRegistryObserver {
+                            public content::NotificationObserver {
  public:
   explicit InternalExtensionProvider(ExtensionService* extension_service);
 
@@ -55,16 +52,6 @@ class InternalExtensionProvider : public ObservableProvider,
   virtual void Observe(int type,
                        const content::NotificationSource& source,
                        const content::NotificationDetails& details) OVERRIDE;
-
-  // extensions::ExtensionRegistryObserver implementation.
-  virtual void OnExtensionLoaded(
-      content::BrowserContext* browser_context,
-      const extensions::Extension* extension) OVERRIDE;
-  virtual void OnExtensionUnloaded(
-      content::BrowserContext* browser_context,
-      const extensions::Extension* extension,
-      extensions::UnloadedExtensionInfo::Reason reason) OVERRIDE;
-
  private:
   void SetContentSettingForExtension(const extensions::Extension* extension,
                                      ContentSetting setting);
@@ -78,8 +65,6 @@ class InternalExtensionProvider : public ObservableProvider,
   // Used around accesses to the |value_map_| list to guarantee thread safety.
   mutable base::Lock lock_;
   scoped_ptr<content::NotificationRegistrar> registrar_;
-
-  extensions::ExtensionRegistry* extension_registry_;  // Not owned.
 
   DISALLOW_COPY_AND_ASSIGN(InternalExtensionProvider);
 };
