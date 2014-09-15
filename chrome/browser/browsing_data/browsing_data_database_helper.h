e8068d269f011ed7cc0923eba5741698bfda7860
vabr <vabr@chromium.org>
2014-09-15 09:12:15 -0700

    Remove Clone() from CannedBrowsingData*Helper classes
    The Clone() method was added in https://codereview.chromium.org/6670033, but is no longer called anywhere. This CL removes that dead code.
    
    BUG=384873
    
    Review URL: https://codereview.chromium.org/546663003
    
    Cr-Commit-Position: refs/heads/master@{#294834}

--- chrome/browser/browsing_data/browsing_data_database_helper.h
+++ chrome/browser/browsing_data/browsing_data_database_helper.h
@@ -116,11 +116,6 @@ class CannedBrowsingDataDatabaseHelper : public BrowsingDataDatabaseHelper {
 
   explicit CannedBrowsingDataDatabaseHelper(Profile* profile);
 
-  // Return a copy of the database helper. Only one consumer can use the
-  // StartFetching method at a time, so we need to create a copy of the helper
-  // everytime we instantiate a cookies tree model for it.
-  CannedBrowsingDataDatabaseHelper* Clone();
-
   // Add a database to the set of canned databases that is returned by this
   // helper.
   void AddDatabase(const GURL& origin,
@@ -150,8 +145,6 @@ class CannedBrowsingDataDatabaseHelper : public BrowsingDataDatabaseHelper {
   virtual ~CannedBrowsingDataDatabaseHelper();
 
   std::set<PendingDatabaseInfo> pending_database_info_;
-
-  Profile* profile_;
 
   DISALLOW_COPY_AND_ASSIGN(CannedBrowsingDataDatabaseHelper);
 };
