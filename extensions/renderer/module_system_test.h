822ac84cc5c404c365baa672c906d25c87c184b9
sammc <sammc@chromium.org>
2014-09-14 19:47:31 -0700

    Fix some leaks and failures under valgrind in JS extensions unit tests.
    This change:
    - Adds running the GC until the v8 heap size reaches steady state during
      ModuleSystemTest::TearDown. This is necessary to ensure that
      gin::Wrappable objects are freed, as finalizers for JS objects aren't
      run during isolate shut down.
    - Removes a racy check that failed under valgrind.
    
    BUG=389016,406487
    
    Review URL: https://codereview.chromium.org/502893002
    
    Cr-Commit-Position: refs/heads/master@{#294779}

--- extensions/renderer/module_system_test.h
+++ extensions/renderer/module_system_test.h
@@ -82,6 +82,7 @@ class ModuleSystemTest : public testing::Test {
   ModuleSystemTest();
   virtual ~ModuleSystemTest();
 
+  virtual void SetUp() OVERRIDE;
   virtual void TearDown() OVERRIDE;
 
  protected:
