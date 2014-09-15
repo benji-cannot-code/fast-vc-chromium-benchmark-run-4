686dc976fc706117ad63923a257c36f6e5458d99
noel@chromium.org <noel@chromium.org>
2014-09-15 14:16:13 +0000

    Move screenColorProfile from Platform to ImageDecoder
    Platform is the wrong place for screenColorprofile, where it was
    the only method there that did not know its Widget. ImageDecoder
    is the only caller currently, move it there.
    
    No change in behavior, covered by existing layout tests.
    
    BUG=369787
    
    Review URL: https://codereview.chromium.org/570023002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181978 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/PlatformScreen.h
+++ third_party/WebKit/Source/platform/PlatformScreen.h
@@ -32,8 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/Forward.h"
 #include "wtf/RefPtr.h"
 
-typedef WTF::Vector<char> ColorProfile;
-
 namespace blink {
 
 class FloatRect;
@@ -45,7 +43,6 @@ PLATFORM_EXPORT bool screenIsMonochrome(Widget*);
 
 PLATFORM_EXPORT FloatRect screenRect(Widget*);
 PLATFORM_EXPORT FloatRect screenAvailableRect(Widget*);
-PLATFORM_EXPORT void screenColorProfile(ColorProfile&);
 
 PLATFORM_EXPORT uint16_t screenOrientationAngle(Widget*);
 PLATFORM_EXPORT WebScreenOrientationType screenOrientationType(Widget*);
--- third_party/WebKit/Source/platform/image-decoders/ImageDecoder.h
+++ third_party/WebKit/Source/platform/image-decoders/ImageDecoder.h
@@ -44,6 +44,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "qcms.h"
 #endif
 
+typedef Vector<char> ColorProfile;
+
 namespace blink {
 
 // ImagePlanes can be used to decode color components into provided buffers instead of using an ImageFrame.
@@ -197,16 +199,15 @@ public:
         OutputDeviceProfile()
             : m_outputDeviceProfile(0)
         {
-            // FIXME: Add optional ICCv4 support.
-            // FIXME: add support for multiple monitors.
-            ColorProfile profile;
-            screenColorProfile(profile);
+            ColorProfile profile = screenColorProfile();
             if (!profile.isEmpty())
                 m_outputDeviceProfile = qcms_profile_from_memory(profile.data(), profile.size());
+
             if (m_outputDeviceProfile && qcms_profile_is_bogus(m_outputDeviceProfile)) {
                 qcms_profile_release(m_outputDeviceProfile);
                 m_outputDeviceProfile = 0;
             }
+
             if (!m_outputDeviceProfile)
                 m_outputDeviceProfile = qcms_profile_sRGB();
             if (m_outputDeviceProfile)
@@ -216,6 +217,17 @@ public:
         qcms_profile* profile() const { return m_outputDeviceProfile; }
 
     private:
+        static ColorProfile screenColorProfile()
+        {
+            // FIXME: Add optional ICCv4 support and support for multiple monitors.
+            WebVector<char> profile;
+            Platform::current()->screenColorProfile(&profile);
+
+            ColorProfile colorProfile;
+            colorProfile.append(profile.data(), profile.size());
+            return colorProfile;
+        }
+
         qcms_profile* m_outputDeviceProfile;
     };
 
