ecd0db332280d125fed2ee4f92d6ffa7362b5302
mohan.reddy <mohan.reddy@samsung.com>
2014-09-15 11:58:41 -0700

    Refactoring the order of the weak_ptr_factory in chrome/renderer folder
    Changing in the intialization order of WeakPtrFactory such that all
    member variables should appear before the WeakPtrFactory to ensure
    that any WeakPtrs to Controller are invalidated before its members
    variable's destructors are executed, rendering them invalid.
    
    BUG=303818
    
    Review URL: https://codereview.chromium.org/569233002
    
    Cr-Commit-Position: refs/heads/master@{#294861}

--- chrome/renderer/extensions/cast_streaming_native_handler.h
+++ chrome/renderer/extensions/cast_streaming_native_handler.h
@@ -86,8 +86,6 @@ class CastStreamingNativeHandler : public ObjectBackedNativeHandler {
   typedef std::map<int, linked_ptr<CastUdpTransport> > UdpTransportMap;
   UdpTransportMap udp_transport_map_;
 
-  base::WeakPtrFactory<CastStreamingNativeHandler> weak_factory_;
-
   extensions::ScopedPersistent<v8::Function> create_callback_;
 
   typedef std::map<int,
@@ -95,6 +93,8 @@ class CastStreamingNativeHandler : public ObjectBackedNativeHandler {
       RtpStreamCallbackMap;
   RtpStreamCallbackMap get_raw_events_callbacks_;
   RtpStreamCallbackMap get_stats_callbacks_;
+
+  base::WeakPtrFactory<CastStreamingNativeHandler> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(CastStreamingNativeHandler);
 };
