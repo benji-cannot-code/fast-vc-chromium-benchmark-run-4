9213105c8893fdd50875692a3b3f1bd6c799c657
xunjieli <xunjieli@chromium.org>
2014-09-16 10:13:18 -0700

    This CL adds a NetLog observer, and uses the newly added NESTABLE_ASYNC APIs (https://codereview.chromium.org/536503002/) to log NetLog entries in Tracing. This CL is a part of the effort to get NetLog data into Tracing.
    Design Doc:
    https://docs.google.com/document/d/1Z2uqj59UEts5IiXX78mkdU4kd6e7kE3JUKPoDK97bVs/edit?usp=sharing
    
    BUG=399701
    
    Review URL: https://codereview.chromium.org/468083004
    
    Cr-Commit-Position: refs/heads/master@{#295089}

--- chrome/browser/net/chrome_net_log.h
+++ chrome/browser/net/chrome_net_log.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace net {
 class NetLogLogger;
+class TraceNetLogObserver;
 }
 
 class NetLogTempFile;
@@ -32,6 +33,8 @@ class ChromeNetLog : public net::NetLog {
  private:
   scoped_ptr<net::NetLogLogger> net_log_logger_;
   scoped_ptr<NetLogTempFile> net_log_temp_file_;
+
+  scoped_ptr<net::TraceNetLogObserver> trace_net_log_observer_;
 
   DISALLOW_COPY_AND_ASSIGN(ChromeNetLog);
 };
