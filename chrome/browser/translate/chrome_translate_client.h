fbbc1c3ef7d514e8a9fb83463fa62f0086dd4dfe
droger <droger@chromium.org>
2014-09-16 08:37:41 -0700

    Move Translate browser-side IPC handling to the component.
    This CL moves a bit of translate logic from chrome/ to the translate
    component.
    
    Some IPC messages are still forwarded to chrome (through
    ContentTranslateDriver::Observer) because of translate notifications.
    This is temporary though, because once translate notifications are
    removed, ChromeTranslateClient will no longer need to be an Observer.
    
    Another motivation for this change is that the renderer-side of translate
    IPC (TranslateHelper) is moving to the component (in another CL),
    and thus the browser-side has to move too for consistency (this CL).
    
    BUG=335087
    TBR=msw
    
    Review URL: https://codereview.chromium.org/564743002
    
    Cr-Commit-Position: refs/heads/master@{#295073}

--- chrome/browser/translate/chrome_translate_client.h
+++ chrome/browser/translate/chrome_translate_client.h
@@ -18,10 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
 
-namespace base {
-class File;
-}  // namespace base
-
 namespace content {
 class BrowserContext;
 class WebContents;
@@ -34,7 +30,6 @@ class ScopedCLDDynamicDataHarness;
 class PrefService;
 
 namespace translate {
-struct LanguageDetectionDetails;
 class LanguageState;
 class TranslateAcceptLanguages;
 class TranslatePrefs;
@@ -43,6 +38,7 @@ class TranslateManager;
 
 class ChromeTranslateClient
     : public translate::TranslateClient,
+      public translate::ContentTranslateDriver::Observer,
       public content::WebContentsObserver,
       public content::WebContentsUserData<ChromeTranslateClient> {
  public:
@@ -101,6 +97,14 @@ class ChromeTranslateClient
   virtual void ShowReportLanguageDetectionErrorUI(
       const GURL& report_url) OVERRIDE;
 
+  // ContentTranslateDriver::Observer implementation.
+  virtual void OnLanguageDetermined(
+      const translate::LanguageDetectionDetails& details) OVERRIDE;
+  virtual void OnPageTranslated(
+      const std::string& original_lang,
+      const std::string& translated_lang,
+      translate::TranslateErrors::Type error_type) OVERRIDE;
+
  private:
   explicit ChromeTranslateClient(content::WebContents* web_contents);
   friend class content::WebContentsUserData<ChromeTranslateClient>;
@@ -108,14 +112,6 @@ class ChromeTranslateClient
   // content::WebContentsObserver implementation.
   virtual bool OnMessageReceived(const IPC::Message& message) OVERRIDE;
   virtual void WebContentsDestroyed() OVERRIDE;
-
-  // IPC handlers.
-  void OnTranslateAssignedSequenceNumber(int page_seq_no);
-  void OnLanguageDetermined(const translate::LanguageDetectionDetails& details,
-                            bool page_needs_translation);
-  void OnPageTranslated(const std::string& original_lang,
-                        const std::string& translated_lang,
-                        translate::TranslateErrors::Type error_type);
 
   // Shows the translate bubble.
   void ShowBubble(translate::TranslateStep step,
