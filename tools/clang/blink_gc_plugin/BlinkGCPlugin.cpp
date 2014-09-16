db2ef609cb95e4cb3f1e8787dc8a6b429757b9eb
Hans Wennborg <hans@chromium.org>
2014-09-16 10:58:09 -0700

    Roll Clang 214024:216630 (+216684) and switch to CMake (try 2)
    This updates Chromium's clang version to r216630 with
    r216684 cherry-picked to fix an ASan issue.
    
    It also changes the build script for Clang to use CMake
    instead of Autoconf. The ASan team say this configuration
    is better tested, and it also makes us consistent with
    the Windows Clang build which already uses CMake.
    
    BUG=400849
    R=thakis@chromium.org
    TBR=brettw
    
    Review URL: https://codereview.chromium.org/578623002
    
    Cr-Commit-Position: refs/heads/master@{#295099}

--- tools/clang/blink_gc_plugin/BlinkGCPlugin.cpp
+++ tools/clang/blink_gc_plugin/BlinkGCPlugin.cpp
@@ -866,7 +866,7 @@ class BlinkGCPluginConsumer : public ASTConsumer {
     visitor.TraverseDecl(context.getTranslationUnitDecl());
 
     if (options_.dump_graph) {
-      string err;
+      std::error_code err;
       // TODO: Make createDefaultOutputFile or a shorter createOutputFile work.
       json_ = JsonWriter::from(instance_.createOutputFile(
           "",                                      // OutputPath
@@ -879,7 +879,7 @@ class BlinkGCPluginConsumer : public ASTConsumer {
           false,                                   // CreateMissingDirectories
           0,                                       // ResultPathName
           0));                                     // TempPathName
-      if (err.empty() && json_) {
+      if (!err && json_) {
         json_->OpenList();
       } else {
         json_ = 0;
@@ -1845,9 +1845,10 @@ class BlinkGCPluginAction : public PluginASTAction {
 
  protected:
   // Overridden from PluginASTAction:
-  virtual ASTConsumer* CreateASTConsumer(CompilerInstance& instance,
-                                         llvm::StringRef ref) {
-    return new BlinkGCPluginConsumer(instance, options_);
+  virtual std::unique_ptr<ASTConsumer> CreateASTConsumer(
+      CompilerInstance& instance,
+      llvm::StringRef ref) {
+    return llvm::make_unique<BlinkGCPluginConsumer>(instance, options_);
   }
 
   virtual bool ParseArgs(const CompilerInstance& instance,
