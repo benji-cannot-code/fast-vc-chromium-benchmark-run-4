80242895233dc7bd7230882a9db8b2f4f381ce50
msw <msw@chromium.org>
2014-09-15 18:02:20 -0700

    Avoid repeatedly deriving fonts in the LabelButton ctor.
    Create LazyInstances for the default LabelButton FontLists.
    Avoid creating/deriving default fonts for each new button.
    (the bookmark bar can create unbounded button instances)
    (repeated derivation is slow and exhausts GDI handles)
    
    BUG=411716
    TEST=Profiles with tons of bookmarks do not crash.
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/544173007
    
    Cr-Commit-Position: refs/heads/master@{#294955}

--- ui/views/controls/button/label_button.cc
+++ ui/views/controls/button/label_button.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/views/controls/button/label_button.h"
 
+#include "base/lazy_instance.h"
 #include "base/logging.h"
 #include "ui/gfx/animation/throb_animation.h"
 #include "ui/gfx/canvas.h"
@@ -31,6 +32,23 @@ const SkColor kStyleButtonTextColor = SK_ColorBLACK;
 const SkColor kStyleButtonShadowColor = SK_ColorWHITE;
 #endif
 
+const gfx::FontList& GetDefaultNormalFontList() {
+  static base::LazyInstance<gfx::FontList>::Leaky font_list =
+      LAZY_INSTANCE_INITIALIZER;
+  return font_list.Get();
+}
+
+const gfx::FontList& GetDefaultBoldFontList() {
+  static base::LazyInstance<gfx::FontList>::Leaky font_list =
+      LAZY_INSTANCE_INITIALIZER;
+  if ((font_list.Get().GetFontStyle() & gfx::Font::BOLD) == 0) {
+    font_list.Get() = font_list.Get().
+        DeriveWithStyle(font_list.Get().GetFontStyle() | gfx::Font::BOLD);
+    DCHECK_NE(font_list.Get().GetFontStyle() & gfx::Font::BOLD, 0);
+  }
+  return font_list.Get();
+}
+
 }  // namespace
 
 namespace views {
@@ -45,6 +63,8 @@ LabelButton::LabelButton(ButtonListener* listener, const base::string16& text)
     : CustomButton(listener),
       image_(new ImageView()),
       label_(new Label()),
+      cached_normal_font_list_(GetDefaultNormalFontList()),
+      cached_bold_font_list_(GetDefaultBoldFontList()),
       button_state_images_(),
       button_state_colors_(),
       explicitly_set_colors_(),
@@ -54,12 +74,12 @@ LabelButton::LabelButton(ButtonListener* listener, const base::string16& text)
       image_label_spacing_(kSpacing) {
   SetAnimationDuration(kHoverAnimationDurationMs);
   SetText(text);
-  SetFontList(gfx::FontList());
 
   AddChildView(image_);
   image_->set_interactive(false);
 
   AddChildView(label_);
+  label_->SetFontList(cached_normal_font_list_);
   label_->SetAutoColorReadabilityEnabled(false);
   label_->SetHorizontalAlignment(gfx::ALIGN_LEFT);
 
