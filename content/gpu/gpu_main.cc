4d32dcfc9fbdc9b8ef031ee09075b6cda580c796
tonyg <tonyg@chromium.org>
2014-09-15 19:14:43 -0700

    Allow maximum timer coalescing of the GPU watchdog thread.
    The exact wakeup times are don't-cares as long as the interval is regular.
    
    BUG=411488
    
    Review URL: https://codereview.chromium.org/567133002
    
    Cr-Commit-Position: refs/heads/master@{#294978}

--- content/gpu/gpu_main.cc
+++ content/gpu/gpu_main.cc
@@ -207,7 +207,9 @@ int GpuMain(const MainFunctionParams& parameters) {
   // consuming has completed, otherwise the process is liable to be aborted.
   if (enable_watchdog && !delayed_watchdog_enable) {
     watchdog_thread = new GpuWatchdogThread(kGpuTimeout);
-    watchdog_thread->Start();
+    base::Thread::Options options;
+    options.timer_slack = base::TIMER_SLACK_MAXIMUM;
+    watchdog_thread->StartWithOptions(options);
   }
 
   gpu::GPUInfo gpu_info;
@@ -301,7 +303,9 @@ int GpuMain(const MainFunctionParams& parameters) {
 
     if (enable_watchdog && delayed_watchdog_enable) {
       watchdog_thread = new GpuWatchdogThread(kGpuTimeout);
-      watchdog_thread->Start();
+      base::Thread::Options options;
+      options.timer_slack = base::TIMER_SLACK_MAXIMUM;
+      watchdog_thread->StartWithOptions(options);
     }
 
     // OSMesa is expected to run very slowly, so disable the watchdog in that
@@ -496,7 +500,9 @@ bool StartSandboxLinux(const gpu::GPUInfo& gpu_info,
   // with only one thread.
   res = LinuxSandbox::InitializeSandbox();
   if (watchdog_thread) {
-    watchdog_thread->Start();
+    base::Thread::Options options;
+    options.timer_slack = base::TIMER_SLACK_MAXIMUM;
+    watchdog_thread->StartWithOptions(options);
   }
 
   return res;
