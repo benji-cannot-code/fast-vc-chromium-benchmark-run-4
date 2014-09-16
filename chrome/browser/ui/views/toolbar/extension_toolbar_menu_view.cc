d4c70140db20f40e6d0f6562f1b9b72a14f711d2
rdevlin.cronin <rdevlin.cronin@chromium.org>
2014-09-16 16:57:23 -0700

    Allow the user to drag an extension to the overflow menu, even when its empty
    We need to show an empty bar for the user to drag extension actions into for
    overflow when there are none currently overflowed. Obviously, this empty bar
    should only be displayed when the user is dragging an extension action.
    
    BUG=414386
    
    Review URL: https://codereview.chromium.org/556293003
    
    Cr-Commit-Position: refs/heads/master@{#295179}

--- chrome/browser/ui/views/toolbar/extension_toolbar_menu_view.cc
+++ chrome/browser/ui/views/toolbar/extension_toolbar_menu_view.cc
@@ -45,6 +45,9 @@ ExtensionToolbarMenuView::ExtensionToolbarMenuView(Browser* browser,
       main);
   container_->Init();
   AddChildView(container_);
+  // We Layout() the container here so that we know the number of actions
+  // that will be visible in ShouldShow().
+  container_->Layout();
 
   // If we were opened for a drop command, we have to wait for the drop to
   // finish so we can close the wrench menu.
@@ -55,6 +58,11 @@ ExtensionToolbarMenuView::ExtensionToolbarMenuView(Browser* browser,
 }
 
 ExtensionToolbarMenuView::~ExtensionToolbarMenuView() {
+}
+
+bool ExtensionToolbarMenuView::ShouldShow() {
+  return wrench_menu_->for_drop() ||
+      container_->VisibleBrowserActionsAfterAnimation();
 }
 
 gfx::Size ExtensionToolbarMenuView::GetPreferredSize() const {
