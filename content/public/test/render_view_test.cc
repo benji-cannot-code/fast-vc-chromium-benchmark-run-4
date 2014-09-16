bb677ac41af37317dc762f90b61708d5a701065c
vivek.vg <vivek.vg@samsung.com>
2014-09-16 08:24:32 -0700

    Utilize TestContentClient in RenderViewTest as resources are being migrated to using grd system.
    Blink is about to use blink_resources.grd for the inline resources of user agent stylesheets.
    This removes the dependency upon using make-file-arrays.py which embeds these resources as strings.
    
    This patch instantiates TestContentClient which does all the plumbing required for various test targets in content.
    As the resources are being moved into blink_resources.grd, this generates blink_resources.pak.
    This pak doesn't exists as a separate pak file but its repacked into "content_shell.pak".
    
    This blink side changes were reverted here https://codereview.chromium.org/567073004/
    
    The reason for reverting: Breaking quite a few browser_tests and content_browsertests across multiple platforms.
    
    BUG=312586
    
    Review URL: https://codereview.chromium.org/567153002
    
    Cr-Commit-Position: refs/heads/master@{#295071}

--- content/public/test/render_view_test.cc
+++ content/public/test/render_view_test.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/renderer_main_platform_delegate.h"
 #include "content/renderer/renderer_webkitplatformsupport_impl.h"
 #include "content/test/mock_render_process.h"
+#include "content/test/test_content_client.h"
 #include "third_party/WebKit/public/platform/WebScreenInfo.h"
 #include "third_party/WebKit/public/platform/WebURLRequest.h"
 #include "third_party/WebKit/public/web/WebHistoryItem.h"
@@ -392,7 +393,7 @@ blink::WebWidget* RenderViewTest::GetWebWidget() {
 
 
 ContentClient* RenderViewTest::CreateContentClient() {
-  return new ContentClient;
+  return new TestContentClient;
 }
 
 ContentBrowserClient* RenderViewTest::CreateContentBrowserClient() {
