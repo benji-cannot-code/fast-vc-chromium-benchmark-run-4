49e5669aefa1d7ab88ca257116499d3260c401c0
zmo <zmo@chromium.org>
2014-09-15 17:14:03 -0700

    Clean up logging for debugging RAF related flakiness in WebGL conformance tests.
    We figured out which part caused the problom, so these logs are no longer needed.
    
    BUG=393331
    TEST=bots
    TBR=kbr@chromium.org
    
    Review URL: https://codereview.chromium.org/551803006
    
    Cr-Commit-Position: refs/heads/master@{#294937}

--- cc/trees/thread_proxy.cc
+++ cc/trees/thread_proxy.cc
@@ -258,8 +258,6 @@ void ThreadProxy::SetRendererCapabilitiesMainThreadCopy(
 
 void ThreadProxy::SendCommitRequestToImplThreadIfNeeded() {
   DCHECK(IsMainThread());
-  VLOG(2) << "ThreadProxy::SendCommitRequestToImplThreadIfNeeded: "
-          << "already_sent = " << main().commit_request_sent_to_impl_thread;
   if (main().commit_request_sent_to_impl_thread)
     return;
   main().commit_request_sent_to_impl_thread = true;
@@ -277,8 +275,6 @@ const RendererCapabilities& ThreadProxy::GetRendererCapabilities() const {
 
 void ThreadProxy::SetNeedsAnimate() {
   DCHECK(IsMainThread());
-  VLOG(2) << "ThreadProxy::SetNeedsAnimate: already_requested = "
-          << main().animate_requested;
   if (main().animate_requested)
     return;
 
@@ -385,7 +381,6 @@ void ThreadProxy::NotifyReadyToActivate() {
 
 void ThreadProxy::SetNeedsCommitOnImplThread() {
   TRACE_EVENT0("cc", "ThreadProxy::SetNeedsCommitOnImplThread");
-  VLOG(2) << "ThreadProxy::SetNeedsCommitOnImplThread";
   DCHECK(IsImplThread());
   impl().scheduler->SetNeedsCommit();
 }
@@ -684,7 +679,6 @@ void ThreadProxy::FinishAllRenderingOnImplThread(CompletionEvent* completion) {
 }
 
 void ThreadProxy::ScheduledActionSendBeginMainFrame() {
-  VLOG(2) << "ThreadProxy::ScheduledActionSendBeginMainFrame";
   unsigned int begin_frame_id = nextBeginFrameId++;
   benchmark_instrumentation::ScopedBeginFrameTask begin_frame_task(
       benchmark_instrumentation::kSendBeginFrame, begin_frame_id);
@@ -723,14 +717,11 @@ void ThreadProxy::BeginMainFrame(
   TRACE_EVENT_SYNTHETIC_DELAY_BEGIN("cc.BeginMainFrame");
   DCHECK(IsMainThread());
 
-  VLOG(2) << "ThreadProxy::BeginMainFrame - BEGIN";
-
   if (main().defer_commits) {
     main().pending_deferred_commit = begin_main_frame_state.Pass();
     layer_tree_host()->DidDeferCommit();
     TRACE_EVENT_INSTANT0(
         "cc", "EarlyOut_DeferCommits", TRACE_EVENT_SCOPE_THREAD);
-    VLOG(2) << "ThreadProxy::BeginMainFrame: EarlyOut_DeferCommits";
     return;
   }
 
@@ -745,7 +736,6 @@ void ThreadProxy::BeginMainFrame(
 
   if (!layer_tree_host()->visible()) {
     TRACE_EVENT_INSTANT0("cc", "EarlyOut_NotVisible", TRACE_EVENT_SCOPE_THREAD);
-    VLOG(2) << "ThreadProxy::BeginMainFrame: EarlyOut_NotVisible";
     bool did_handle = false;
     Proxy::ImplThreadTaskRunner()->PostTask(
         FROM_HERE,
@@ -758,7 +748,6 @@ void ThreadProxy::BeginMainFrame(
   if (layer_tree_host()->output_surface_lost()) {
     TRACE_EVENT_INSTANT0(
         "cc", "EarlyOut_OutputSurfaceLost", TRACE_EVENT_SCOPE_THREAD);
-    VLOG(2) << "ThreadProxy::BeginMainFrame: EarlyOut_OutputSurfaceLost";
     bool did_handle = false;
     Proxy::ImplThreadTaskRunner()->PostTask(
         FROM_HERE,
@@ -778,7 +767,6 @@ void ThreadProxy::BeginMainFrame(
   // callbacks will trigger another frame.
   main().commit_requested = true;
   main().commit_request_sent_to_impl_thread = true;
-  VLOG(2) << "ThreadProxy::BeginMainFrame: Commit requests silenced.";
 
   layer_tree_host()->ApplyScrollAndScale(
       begin_main_frame_state->scroll_info.get());
@@ -833,16 +821,12 @@ void ThreadProxy::BeginMainFrame(
   // thread.
   if (main().animate_requested) {
     // Forces SetNeedsAnimate to consider posting a commit task.
-    VLOG(2) << "ThreadProxy::BeginMainFrame: RAF requested.";
     main().animate_requested = false;
     SetNeedsAnimate();
-  } else {
-    VLOG(2) << "ThreadProxy::BeginMainFrame: RAF NOT requested.";
   }
 
   if (!updated && can_cancel_this_commit) {
     TRACE_EVENT_INSTANT0("cc", "EarlyOut_NoUpdates", TRACE_EVENT_SCOPE_THREAD);
-    VLOG(2) << "ThreadProxy::BeginMainFrame: EarlyOut_NoUpdates";
     bool did_handle = true;
     Proxy::ImplThreadTaskRunner()->PostTask(
         FROM_HERE,
