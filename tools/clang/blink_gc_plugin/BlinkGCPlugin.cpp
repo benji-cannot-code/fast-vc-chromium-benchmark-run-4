77bed32b79b210434693b7c815e0bcfda7153bc8
Dirk Pranke <dpranke@chromium.org>
2014-09-16 12:50:56 -0700

    Revert "Roll Clang 214024:216630 (+216684) and switch to CMake (try 2)"
    This reverts commit 3853ffa861dc43537f23c21a3fc4104c6f08c3a2.
    
    The change broke the Mac10.6 Tests bot on the chromium.webkit waterfall.
    
    TBR=hwennborg@chromium.org, thakis@chromium.org
    BUG=400849
    
    Review URL: https://codereview.chromium.org/572413002
    
    Cr-Commit-Position: refs/heads/master@{#295132}

--- tools/clang/blink_gc_plugin/BlinkGCPlugin.cpp
+++ tools/clang/blink_gc_plugin/BlinkGCPlugin.cpp
@@ -866,7 +866,7 @@ class BlinkGCPluginConsumer : public ASTConsumer {
     visitor.TraverseDecl(context.getTranslationUnitDecl());
 
     if (options_.dump_graph) {
-      std::error_code err;
+      string err;
       // TODO: Make createDefaultOutputFile or a shorter createOutputFile work.
       json_ = JsonWriter::from(instance_.createOutputFile(
           "",                                      // OutputPath
@@ -879,7 +879,7 @@ class BlinkGCPluginConsumer : public ASTConsumer {
           false,                                   // CreateMissingDirectories
           0,                                       // ResultPathName
           0));                                     // TempPathName
-      if (!err && json_) {
+      if (err.empty() && json_) {
         json_->OpenList();
       } else {
         json_ = 0;
@@ -1845,10 +1845,9 @@ class BlinkGCPluginAction : public PluginASTAction {
 
  protected:
   // Overridden from PluginASTAction:
-  virtual std::unique_ptr<ASTConsumer> CreateASTConsumer(
-      CompilerInstance& instance,
-      llvm::StringRef ref) {
-    return llvm::make_unique<BlinkGCPluginConsumer>(instance, options_);
+  virtual ASTConsumer* CreateASTConsumer(CompilerInstance& instance,
+                                         llvm::StringRef ref) {
+    return new BlinkGCPluginConsumer(instance, options_);
   }
 
   virtual bool ParseArgs(const CompilerInstance& instance,
