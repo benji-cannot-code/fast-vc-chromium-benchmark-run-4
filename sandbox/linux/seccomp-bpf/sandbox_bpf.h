7ba33debfd558b279645db38525639308ef7db18
mdempsky <mdempsky@chromium.org>
2014-09-16 12:15:48 -0700

    Decouple Trap from ErrorCode
    This CL makes a few changes in the direction of removing dependencies
    on ErrorCode by pushing them up a layer:
    
    - Trap now simply uses TrapKeys directly to store trap function and
    metadata instead of needlessly encoding them as ErrorCode.
    
    - MakeTrap returns a bare trap ID and it's instead the caller's
    responsibility to encode in an ErrorCode if needed, which is now
    handled by ErrorCode's trap constructor.
    
    - Also, ErrorCodeFromTrapId() is replaced by IsSafeTrapId() to answer
    the one question that SandboxBPF was interested in knowing about
    existing traps.
    
    - Change a few SandboxBPF trap-constructing functions into static
    functions since they don't depend on any SandboxBPF instance member
    variables.
    
    BUG=414363
    
    Review URL: https://codereview.chromium.org/572753002
    
    Cr-Commit-Position: refs/heads/master@{#295121}

--- sandbox/linux/seccomp-bpf/sandbox_bpf.h
+++ sandbox/linux/seccomp-bpf/sandbox_bpf.h
@@ -22,23 +22,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sandbox/linux/seccomp-bpf/die.h"
 #include "sandbox/linux/seccomp-bpf/errorcode.h"
 #include "sandbox/linux/seccomp-bpf/linux_seccomp.h"
+#include "sandbox/linux/seccomp-bpf/trap.h"
 #include "sandbox/sandbox_export.h"
 
 namespace sandbox {
-
-// This must match the kernel's seccomp_data structure.
-struct arch_seccomp_data {
-  int nr;
-  uint32_t arch;
-  uint64_t instruction_pointer;
-  uint64_t args[6];
-};
-
-struct arch_sigsys {
-  void* ip;
-  int nr;
-  unsigned int arch;
-};
 
 class CodeGen;
 class SandboxBPFPolicy;
@@ -117,7 +104,7 @@ class SANDBOX_EXPORT SandboxBPF {
   // The "aux" field can carry a pointer to arbitrary data. See EvaluateSyscall
   // for a description of how to pass data from SetSandboxPolicy() to a Trap()
   // handler.
-  ErrorCode Trap(Trap::TrapFnc fnc, const void* aux);
+  static ErrorCode Trap(Trap::TrapFnc fnc, const void* aux);
 
   // Calls a user-space trap handler and disables all sandboxing for system
   // calls made from this trap handler.
@@ -129,7 +116,7 @@ class SANDBOX_EXPORT SandboxBPF {
   //   very useful to diagnose code that is incompatible with the sandbox.
   //   If even a single system call returns "UnsafeTrap", the security of
   //   entire sandbox should be considered compromised.
-  ErrorCode UnsafeTrap(Trap::TrapFnc fnc, const void* aux);
+  static ErrorCode UnsafeTrap(Trap::TrapFnc fnc, const void* aux);
 
   // UnsafeTraps require some syscalls to always be allowed.
   // This helper function returns true for these calls.
@@ -171,7 +158,7 @@ class SANDBOX_EXPORT SandboxBPF {
                  const ErrorCode& failed);
 
   // Kill the program and print an error message.
-  ErrorCode Kill(const char* msg);
+  static ErrorCode Kill(const char* msg);
 
   // This is the main public entry point. It finds all system calls that
   // need rewriting, sets up the resources needed by the sandbox, and
@@ -201,7 +188,7 @@ class SANDBOX_EXPORT SandboxBPF {
   // Returns the fatal ErrorCode that is used to indicate that somebody
   // attempted to pass a 64bit value in a 32bit system call argument.
   // This method is primarily needed for testing purposes.
-  ErrorCode Unexpected64bitArgument();
+  static ErrorCode Unexpected64bitArgument();
 
  private:
   friend class CodeGen;
