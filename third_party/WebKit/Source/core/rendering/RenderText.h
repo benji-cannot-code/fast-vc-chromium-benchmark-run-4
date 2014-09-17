975e8537315591fea28dc4c84d7f748cb3deba05
wangxianzhu@chromium.org <wangxianzhu@chromium.org>
2014-09-17 00:58:58 +0000

    Revert "Let parent handle invalidation of RenderText" and its auto-rebaseline
    The original change caused failure of some chromium browser tests.
    
    > Let parent handle invalidation of RenderText
    >
    > Instead of letting invalidateTreeIfNeeded() work for RenderText,
    > this CL let RenderText's parent handle invalidation of RenderText
    > during invalidateTreeIfNeeded().
    >
    > This avoids the complexity of implementing a correct and efficient
    > RenderText::clippedOverflowRectForPaintInvalidation(), and I believe
    > the performance is also better than full-functional RenderText
    > invalidation.
    >
    > BUG=394133, 409068
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=182069
    
    BUG=394133, 409068
    TBR=wangxianzhu@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/577863002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182128 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderText.h
+++ third_party/WebKit/Source/core/rendering/RenderText.h
@@ -120,6 +120,9 @@ public:
     LayoutUnit marginLeft() const { return minimumValueForLength(style()->marginLeft(), 0); }
     LayoutUnit marginRight() const { return minimumValueForLength(style()->marginRight(), 0); }
 
+    virtual LayoutRect clippedOverflowRectForPaintInvalidation(const RenderLayerModelObject* paintInvalidationContainer, const PaintInvalidationState* = 0) const OVERRIDE FINAL;
+    virtual const RenderLayerModelObject* containerForPaintInvalidation() const OVERRIDE;
+
     InlineTextBox* firstTextBox() const { return m_firstTextBox; }
     InlineTextBox* lastTextBox() const { return m_lastTextBox; }
 
@@ -160,6 +163,10 @@ protected:
     virtual InlineTextBox* createTextBox(); // Subclassed by SVG.
 
 private:
+    // RenderText objects don't know how to invalidate paint for themselves, since they don't know how to compute their bounds. Instead, they
+    // invalidate paint for the containing block or layer, as computed by this method.
+    const RenderLayerModelObject* containingObjectForPaintInvalidation() const;
+
     void computePreferredLogicalWidths(float leadWidth);
     void computePreferredLogicalWidths(float leadWidth, HashSet<const SimpleFontData*>& fallbackFonts, GlyphOverflow&);
 
@@ -183,8 +190,7 @@ private:
 
     bool isText() const WTF_DELETED_FUNCTION; // This will catch anyone doing an unnecessary check.
 
-    virtual LayoutRect clippedOverflowRectForPaintInvalidation(const RenderLayerModelObject* paintInvalidationContainer, const PaintInvalidationState* = 0) const OVERRIDE FINAL { ASSERT_NOT_REACHED(); return LayoutRect(); }
-    // The parent invalidates for RenderText, so RenderText does nothing.
+    // FIXME: This is temporarily empty. Will let it actually work in following steps.
     virtual InvalidationReason invalidatePaintIfNeeded(const PaintInvalidationState&, const RenderLayerModelObject&) OVERRIDE FINAL { return InvalidationNone; }
 
     // We put the bitfield first to minimize padding on 64-bit.
