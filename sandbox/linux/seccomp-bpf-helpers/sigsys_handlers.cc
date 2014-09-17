a343029dc92af37934ffa87aaaf0179e7e8a47c5
Gordana.Cmiljanovic <Gordana.Cmiljanovic@imgtec.com>
2014-09-17 10:23:39 -0700

    [MIPS] Fix seccomp-bpf compile error for MIPS
    This change fixes the seccomp-bpf compile error
    introduced by change:
    https://chromium.googlesource.com/chromium/src/+/ab2d46af89c657ab4fd01ab00de1ba2a6ad73f8d
    
    BUG=408845
    
    Review URL: https://codereview.chromium.org/569333003
    
    Cr-Commit-Position: refs/heads/master@{#295291}

--- sandbox/linux/seccomp-bpf-helpers/sigsys_handlers.cc
+++ sandbox/linux/seccomp-bpf-helpers/sigsys_handlers.cc
@@ -14,6 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "sandbox/linux/seccomp-bpf/sandbox_bpf.h"
 
+#if defined(__mips__)
+// __NR_Linux, is defined in <asm/unistd.h>.
+#include <asm/unistd.h>
+#endif
+
 #define SECCOMP_MESSAGE_COMMON_CONTENT "seccomp-bpf failure"
 #define SECCOMP_MESSAGE_CLONE_CONTENT "clone() failure"
 #define SECCOMP_MESSAGE_PRCTL_CONTENT "prctl() failure"
