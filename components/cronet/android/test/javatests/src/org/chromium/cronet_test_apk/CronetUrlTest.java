b98b4de0b6b14bc92d4530eef22c2da87e27c220
mef <mef@chromium.org>
2014-09-17 14:04:14 -0700

    Allow any HTTP verb in Cronet HttpUrlRequest.
    BUG=390267
    
    Review URL: https://codereview.chromium.org/562193003
    
    Cr-Commit-Position: refs/heads/master@{#295342}

--- components/cronet/android/test/javatests/src/org/chromium/cronet_test_apk/CronetUrlTest.java
+++ components/cronet/android/test/javatests/src/org/chromium/cronet_test_apk/CronetUrlTest.java
@@ -100,6 +100,7 @@ public class CronetUrlTest extends CronetTestBase {
         ConditionVariable mComplete = new ConditionVariable();
         public String negotiatedProtocol;
         public int httpStatusCode = 0;
+        public byte httpResponseData[];
 
         public SimpleHttpUrlRequestListener() {
         }
@@ -112,6 +113,7 @@ public class CronetUrlTest extends CronetTestBase {
 
         @Override
         public void onRequestComplete(HttpUrlRequest request) {
+            httpResponseData = request.getResponseAsBytes();
             mComplete.open();
         }
 
@@ -156,7 +158,8 @@ public class CronetUrlTest extends CronetTestBase {
         listener.blockForComplete();
         assertTrue(request.isCanceled());
         assertNotNull(request.getException());
-        assertEquals(listener.THROW_TAG, request.getException().getCause().getMessage());
+        assertEquals(listener.THROW_TAG,
+                     request.getException().getCause().getMessage());
     }
 
     @SmallTest
@@ -252,5 +255,29 @@ public class CronetUrlTest extends CronetTestBase {
         // Make sure that the URL is set as expected.
         assertEquals(URL, activity.getUrl());
         assertEquals(200, activity.getHttpStatusCode());
+    }
+
+    @SmallTest
+    @Feature({"Cronet"})
+    public void testRequestHead() throws Exception {
+        CronetTestActivity activity = launchCronetTestAppWithUrl(URL);
+
+        // Make sure the activity was created as expected.
+        waitForActiveShellToBeDoneLoading();
+
+        HashMap<String, String> headers = new HashMap<String, String>();
+        SimpleHttpUrlRequestListener listener =
+                new SimpleHttpUrlRequestListener();
+
+        // Create request.
+        HttpUrlRequest request = activity.mChromiumRequestFactory.createRequest(
+                URL, HttpUrlRequest.REQUEST_PRIORITY_MEDIUM, headers, listener);
+        request.setHttpMethod("HEAD");
+        request.start();
+        listener.blockForComplete();
+        assertEquals(200, listener.httpStatusCode);
+        // HEAD requests do not get any response data and Content-Length must be
+        // ignored.
+        assertEquals(0, listener.httpResponseData.length);
     }
 }
