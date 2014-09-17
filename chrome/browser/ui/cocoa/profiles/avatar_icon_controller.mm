6bf0718ce17a5cec4fd57420164a04a06c0b77c1
andresantoso <andresantoso@chromium.org>
2014-09-17 14:46:15 -0700

    Mac: Fix tab layout when showing/hiding the avatar button.
    When the old style avatar button gets shown/hidden, the fullscreen button's
    position will move accordingly. This affects the amount of space available for
    laying out the tabs. This fix ensure that tab layout is called after the
    fullscreen button has moved to its new position, not before.
    
    BUG=412246
    
    Review URL: https://codereview.chromium.org/579583002
    
    Cr-Commit-Position: refs/heads/master@{#295351}

--- chrome/browser/ui/cocoa/profiles/avatar_icon_controller.mm
+++ chrome/browser/ui/cocoa/profiles/avatar_icon_controller.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "chrome/browser/ui/cocoa/profiles/avatar_icon_controller.h"
 
+#include "base/mac/foundation_util.h"
 #include "base/strings/sys_string_conversions.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
@@ -220,14 +221,12 @@ const CGFloat kAvatarLabelRightSpacing = 2;
   if (browser_->profile()->IsOffTheRecord())
     return;
 
-  NSWindowController* wc =
-      [browser_->window()->GetNativeWindow() windowController];
-  if (![wc isKindOfClass:[BrowserWindowController class]])
+  BrowserWindowController* wc = base::mac::ObjCCast<BrowserWindowController>(
+      [browser_->window()->GetNativeWindow() windowController]);
+  if (!wc)
     return;
 
-  [self.view setHidden:
-      ![static_cast<BrowserWindowController*>(wc) shouldShowAvatar]];
-  [static_cast<BrowserWindowController*>(wc) layoutSubviews];
+  [self.view setHidden:![wc shouldShowAvatar]];
 
   // If the avatar is being added or removed, then the Lion fullscreen button
   // needs to be adjusted. Since the fullscreen button is positioned by
@@ -237,6 +236,10 @@ const CGFloat kAvatarLabelRightSpacing = 2;
   NSView* themeFrame = [[[wc window] contentView] superview];
   if ([themeFrame respondsToSelector:@selector(_tileTitlebarAndRedisplay:)])
     [themeFrame _tileTitlebarAndRedisplay:YES];
+
+  // This needs to be called after the fullscreen button is positioned, because
+  // TabStripController's rightIndentForControls depends on it.
+  [wc layoutSubviews];
 }
 
 @end
