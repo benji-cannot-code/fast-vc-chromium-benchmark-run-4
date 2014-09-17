0a8a87e53c6991860b591656c94332134c6f1a50
qinmin <qinmin@chromium.org>
2014-09-17 12:21:53 -0700

    Fix crashing MediaSourcePlayerTest on nexus 5
    If a surface is released while MediaCodec is still decoding the data, it will raise a CHECK assertion failure in android media framework in Kitkat
    Make some of the affected tests wait until MediaCodec finishes decoding
    
    BUG=411499
    
    Review URL: https://codereview.chromium.org/572403002
    
    Cr-Commit-Position: refs/heads/master@{#295320}

--- media/base/android/media_source_player_unittest.cc
+++ media/base/android/media_source_player_unittest.cc
@@ -884,6 +884,7 @@ TEST_F(MediaSourcePlayerTest, StartVideoCodecWithValidSurface) {
   // Send the first input chunk and verify that decoder will be created.
   player_.OnDemuxerDataAvailable(CreateReadFromDemuxerAckForVideo());
   EXPECT_TRUE(GetMediaCodecBridge(false));
+  WaitForVideoDecodeDone();
 }
 
 TEST_F(MediaSourcePlayerTest, StartVideoCodecWithInvalidSurface) {
@@ -943,6 +944,7 @@ TEST_F(MediaSourcePlayerTest, SetSurfaceWhileSeeking) {
   // was not a browser seek request.
   EXPECT_EQ(1, demuxer_->num_seek_requests());
   EXPECT_EQ(0, demuxer_->num_browser_seek_requests());
+  WaitForVideoDecodeDone();
 }
 
 TEST_F(MediaSourcePlayerTest, ChangeMultipleSurfaceWhileDecoding) {
@@ -983,6 +985,7 @@ TEST_F(MediaSourcePlayerTest, ChangeMultipleSurfaceWhileDecoding) {
 
   player_.OnDemuxerDataAvailable(CreateReadFromDemuxerAckForVideo());
   EXPECT_TRUE(GetMediaCodecBridge(false));
+  WaitForVideoDecodeDone();
 }
 
 TEST_F(MediaSourcePlayerTest, SetEmptySurfaceAndStarveWhileDecoding) {
@@ -1015,6 +1018,7 @@ TEST_F(MediaSourcePlayerTest, SetEmptySurfaceAndStarveWhileDecoding) {
   // Playback resumes once a non-empty surface is passed.
   CreateNextTextureAndSetVideoSurface();
   EXPECT_EQ(1, demuxer_->num_browser_seek_requests());
+  WaitForVideoDecodeDone();
 }
 
 TEST_F(MediaSourcePlayerTest, ReleaseVideoDecoderResourcesWhileDecoding) {
@@ -1097,6 +1101,7 @@ TEST_F(MediaSourcePlayerTest, VideoOnlyStartAfterSeekFinish) {
   // Decoder is created after data is received.
   player_.OnDemuxerDataAvailable(CreateReadFromDemuxerAckForVideo());
   EXPECT_TRUE(GetMediaCodecBridge(false));
+  WaitForVideoDecodeDone();
 }
 
 TEST_F(MediaSourcePlayerTest, StartImmediatelyAfterPause) {
@@ -1155,6 +1160,7 @@ TEST_F(MediaSourcePlayerTest, DecoderJobsCannotStartWithoutAudio) {
 
   // No seeks should have occurred.
   EXPECT_EQ(0, demuxer_->num_seek_requests());
+  WaitForVideoDecodeDone();
 }
 
 TEST_F(MediaSourcePlayerTest, StartTimeTicksResetAfterDecoderUnderruns) {
@@ -1461,6 +1467,7 @@ TEST_F(MediaSourcePlayerTest, BrowserSeek_RegularSeekPendsBrowserSeekDone) {
   EXPECT_EQ(2, demuxer_->num_seek_requests());
   player_.OnDemuxerDataAvailable(CreateReadFromDemuxerAckForVideo());
   EXPECT_TRUE(GetMediaCodecBridge(false));
+  WaitForVideoDecodeDone();
 }
 
 TEST_F(MediaSourcePlayerTest, BrowserSeek_InitialReleaseAndStart) {
@@ -1485,6 +1492,7 @@ TEST_F(MediaSourcePlayerTest, BrowserSeek_InitialReleaseAndStart) {
   player_.OnDemuxerDataAvailable(CreateReadFromDemuxerAckForVideo());
   EXPECT_EQ(0, demuxer_->num_browser_seek_requests());
   EXPECT_EQ(2, demuxer_->num_data_requests());
+  WaitForVideoDecodeDone();
 }
 
 TEST_F(MediaSourcePlayerTest, BrowserSeek_MidStreamReleaseAndStart) {
@@ -2133,8 +2141,8 @@ TEST_F(MediaSourcePlayerTest, BrowserSeek_ThenReleaseThenStart) {
 
   // Decoder will be created once data is received.
   player_.OnDemuxerDataAvailable(CreateReadFromDemuxerAckForVideo());
-  while (!GetMediaCodecBridge(false))
-    message_loop_.RunUntilIdle();
+  EXPECT_TRUE(GetMediaCodecBridge(false));
+  WaitForVideoDecodeDone();
 }
 
 // TODO(xhwang): Once we add tests to cover DrmBridge, update this test to
