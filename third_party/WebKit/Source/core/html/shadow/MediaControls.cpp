c9662db135c3ed743d3df11182609ca129087b77
rob.buis@samsung.com <rob.buis@samsung.com>
2014-09-17 02:12:32 +0000

    Use ConstructFromLiteral instead of implicit conversion
    Since we are supposed to end up with an AtomicString, don't rely
    on implicit conversion, but provide it as static local variable
    while using ConstructFromLiteral to avoid using strlen.
    
    Review URL: https://codereview.chromium.org/566373002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182133 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/shadow/MediaControls.cpp
+++ third_party/WebKit/Source/core/html/shadow/MediaControls.cpp
@@ -492,7 +492,7 @@ void MediaControls::resetHideMediaControlsTimer()
 
 const AtomicString& MediaControls::shadowPseudoId() const
 {
-    DEFINE_STATIC_LOCAL(AtomicString, id, ("-webkit-media-controls"));
+    DEFINE_STATIC_LOCAL(AtomicString, id, ("-webkit-media-controls", AtomicString::ConstructFromLiteral));
     return id;
 }
 
