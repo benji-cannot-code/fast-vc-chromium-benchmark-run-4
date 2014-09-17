da9588591070f1a34c3b0d98d79f7d609c918465
reveman <reveman@chromium.org>
2014-09-17 10:19:58 -0700

    cc: Return a NullCanvas instead of NULL in context lost situations.
    This reduces the difference in code paths between normal usage and
    context lost situations.
    
    Also moves the save()/restore() pair required for GPU raster
    into the RasterBuffer class so it can be avoided when unnecessary.
    
    BUG=406404
    
    Review URL: https://codereview.chromium.org/578843002
    
    Cr-Commit-Position: refs/heads/master@{#295290}

--- cc/resources/resource_provider.cc
+++ cc/resources/resource_provider.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/khronos/GLES2/gl2ext.h"
 #include "third_party/skia/include/core/SkSurface.h"
 #include "third_party/skia/include/gpu/GrContext.h"
+#include "third_party/skia/include/utils/SkNullCanvas.h"
 #include "ui/gfx/frame_time.h"
 #include "ui/gfx/rect.h"
 #include "ui/gfx/vector2d.h"
@@ -455,8 +456,11 @@ skia::RefPtr<SkCanvas> ResourceProvider::GpuRasterBuffer::AcquireSkCanvas() {
   TRACE_EVENT0(TRACE_DISABLED_BY_DEFAULT("cc.debug"),
                "ResourceProvider::GpuRasterBuffer::AcquireSkCanvas");
 
-  return surface_ ? skia::SharePtr(surface_->getCanvas())
-                  : skia::RefPtr<SkCanvas>();
+  skia::RefPtr<SkCanvas> canvas = surface_
+                                      ? skia::SharePtr(surface_->getCanvas())
+                                      : skia::AdoptRef(SkCreateNullCanvas());
+  canvas->save();
+  return canvas;
 }
 
 void ResourceProvider::GpuRasterBuffer::ReleaseSkCanvas(
@@ -464,6 +468,8 @@ void ResourceProvider::GpuRasterBuffer::ReleaseSkCanvas(
   // Note that this function is called from a worker thread.
   TRACE_EVENT0(TRACE_DISABLED_BY_DEFAULT("cc.debug"),
                "ResourceProvider::GpuRasterBuffer::ReleaseSkCanvas");
+
+  canvas->restore();
 }
 
 ResourceProvider::ImageRasterBuffer::ImageRasterBuffer(
@@ -507,7 +513,7 @@ skia::RefPtr<SkCanvas> ResourceProvider::ImageRasterBuffer::AcquireSkCanvas() {
                "ResourceProvider::ImageRasterBuffer::AcquireSkCanvas");
 
   if (!mapped_buffer_)
-    return skia::RefPtr<SkCanvas>();
+    return skia::AdoptRef(SkCreateNullCanvas());
 
   MakeBitmap(&raster_bitmap_,
              mapped_buffer_,
@@ -571,7 +577,7 @@ skia::RefPtr<SkCanvas> ResourceProvider::PixelRasterBuffer::AcquireSkCanvas() {
                "ResourceProvider::PixelRasterBuffer::AcquireSkCanvas");
 
   if (!mapped_buffer_)
-    return skia::RefPtr<SkCanvas>();
+    return skia::AdoptRef(SkCreateNullCanvas());
 
   MakeBitmap(&raster_bitmap_,
              mapped_buffer_,
