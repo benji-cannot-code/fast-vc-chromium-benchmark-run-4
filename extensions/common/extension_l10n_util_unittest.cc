e8e75b1f24fa8519c213039c72c18882e3d62c7e
jamescook <jamescook@chromium.org>
2014-09-18 16:43:56 -0700

    Move the last extensions/common unit tests to extensions_unittests
    They don't need to run in Chrome's unit_tests suite any more.
    
    * Move ExtensionL10UtilTest
    * Move FileUtilTest
    * Keep browser action and page action tests in Chrome, since those are
      Chrome-specific concepts
    
    This CL depends on https://codereview.chromium.org/565423003/ for cleanup
    of _manifest_features.json
    
    BUG=397165
    TEST=extensions_unittests, unit_tests
    
    Review URL: https://codereview.chromium.org/562273006
    
    Cr-Commit-Position: refs/heads/master@{#295605}

--- extensions/common/extension_l10n_util_unittest.cc
+++ extensions/common/extension_l10n_util_unittest.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "extensions/common/extension_l10n_util.h"
+
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
@@ -11,22 +13,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
-#include "chrome/common/chrome_paths.h"
 #include "extensions/common/constants.h"
 #include "extensions/common/error_utils.h"
-#include "extensions/common/extension_l10n_util.h"
+#include "extensions/common/extension_paths.h"
 #include "extensions/common/manifest_constants.h"
 #include "extensions/common/message_bundle.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/l10n/l10n_util.h"
 
-using extensions::kLocaleFolder;
-using extensions::kMessagesFilename;
-using extensions::MessageBundle;
+namespace extensions {
 
-namespace errors = extensions::manifest_errors;
-namespace keys = extensions::manifest_keys;
+namespace errors = manifest_errors;
+namespace keys = manifest_keys;
 
 namespace {
 
@@ -109,13 +108,9 @@ TEST(ExtensionL10nUtil, GetValidLocalesWithUnsupportedLocale) {
 
 TEST(ExtensionL10nUtil, GetValidLocalesWithValidLocalesAndMessagesFile) {
   base::FilePath install_dir;
-  ASSERT_TRUE(PathService::Get(chrome::DIR_TEST_DATA, &install_dir));
-  install_dir = install_dir.AppendASCII("extensions")
-                    .AppendASCII("good")
-                    .AppendASCII("Extensions")
-                    .AppendASCII("behllobkkfkfnphdnhnkndlbkcpglgmj")
-                    .AppendASCII("1.0.0.0")
-                    .Append(kLocaleFolder);
+  ASSERT_TRUE(PathService::Get(DIR_TEST_DATA, &install_dir));
+  install_dir =
+      install_dir.AppendASCII("extension_with_locales").Append(kLocaleFolder);
 
   std::string error;
   std::set<std::string> locales;
@@ -129,13 +124,9 @@ TEST(ExtensionL10nUtil, GetValidLocalesWithValidLocalesAndMessagesFile) {
 
 TEST(ExtensionL10nUtil, LoadMessageCatalogsValidFallback) {
   base::FilePath install_dir;
-  ASSERT_TRUE(PathService::Get(chrome::DIR_TEST_DATA, &install_dir));
-  install_dir = install_dir.AppendASCII("extensions")
-                    .AppendASCII("good")
-                    .AppendASCII("Extensions")
-                    .AppendASCII("behllobkkfkfnphdnhnkndlbkcpglgmj")
-                    .AppendASCII("1.0.0.0")
-                    .Append(kLocaleFolder);
+  ASSERT_TRUE(PathService::Get(DIR_TEST_DATA, &install_dir));
+  install_dir =
+      install_dir.AppendASCII("extension_with_locales").Append(kLocaleFolder);
 
   std::string error;
   std::set<std::string> locales;
@@ -186,7 +177,7 @@ TEST(ExtensionL10nUtil, LoadMessageCatalogsBadJSONFormat) {
   std::string error;
   EXPECT_TRUE(NULL == extension_l10n_util::LoadMessageCatalogs(
                           src_path, "en_US", "sr", valid_locales, &error));
-  EXPECT_EQ(extensions::ErrorUtils::FormatErrorMessage(
+  EXPECT_EQ(ErrorUtils::FormatErrorMessage(
                 errors::kLocalesInvalidLocale,
                 base::UTF16ToUTF8(messages_file.LossyDisplayName()),
                 "Line: 1, column: 10, Unexpected token."),
@@ -661,3 +652,4 @@ TEST(ExtensionL10nUtil, GetAllFallbackLocales) {
 }
 
 }  // namespace
+}  // namespace extensions
