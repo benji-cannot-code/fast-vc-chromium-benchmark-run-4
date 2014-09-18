e4dc72d7ea6b5abee044e4ef42325a9a8147cdd7
estade <estade@chromium.org>
2014-09-17 17:48:52 -0700

    Fix SQLite query to order credit cards and profiles by modification date.
    BUG=412968
    TEST=all those tests that Scott says would soon be flaky
    
    Review URL: https://codereview.chromium.org/567743004
    
    Cr-Commit-Position: refs/heads/master@{#295388}

--- components/autofill/core/browser/webdata/autofill_table.cc
+++ components/autofill/core/browser/webdata/autofill_table.cc
@@ -930,7 +930,8 @@ bool AutofillTable::GetAutofillProfiles(
 
   sql::Statement s(db_->GetUniqueStatement(
       "SELECT guid "
-      "FROM autofill_profiles"));
+      "FROM autofill_profiles "
+      "ORDER BY date_modified, guid"));
 
   while (s.Step()) {
     std::string guid = s.ColumnString(0);
@@ -1068,7 +1069,8 @@ bool AutofillTable::GetCreditCards(
 
   sql::Statement s(db_->GetUniqueStatement(
       "SELECT guid "
-      "FROM credit_cards"));
+      "FROM credit_cards "
+      "ORDER BY date_modified, guid"));
 
   while (s.Step()) {
     std::string guid = s.ColumnString(0);
