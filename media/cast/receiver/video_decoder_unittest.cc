347ed57efba77f67d463647594e1355ec00155cf
miu <miu@chromium.org>
2014-09-18 10:55:18 -0700

    [Cast] Fix referenced_frame_ids emitted by Vp8Encoder, with massive test clean-up.
    The referenced_frame_ids set by Vp8Encoder were wrong in two ways:
    1) A recent change introduced an off-by-one error.  2) Math was being
    done with results stored in a uint8 variable, which was causing the
    upper 24 bits to be stripped.
    
    Added VideoEncoderImplTest.GeneratesKeyFrameThenOnlyDeltaFrames test to
    prevent future regressions.  In addition, a lot of unit test code clean-up
    was done to beef-up existing testing while also removing useless tests.
    
    Review URL: https://codereview.chromium.org/581903002
    
    Cr-Commit-Position: refs/heads/master@{#295493}

--- media/cast/receiver/video_decoder_unittest.cc
+++ media/cast/receiver/video_decoder_unittest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/cast/cast_config.h"
 #include "media/cast/receiver/video_decoder.h"
 #include "media/cast/sender/vp8_encoder.h"
+#include "media/cast/test/utility/default_config.h"
 #include "media/cast/test/utility/standalone_cast_environment.h"
 #include "media/cast/test/utility/video_utility.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -27,7 +28,7 @@ const int kHeight = 240;
 const int kFrameRate = 10;
 
 VideoSenderConfig GetVideoSenderConfigForTest() {
-  VideoSenderConfig config;
+  VideoSenderConfig config = GetDefaultVideoSenderConfig();
   config.width = kWidth;
   config.height = kHeight;
   config.max_frame_rate = kFrameRate;
@@ -84,7 +85,12 @@ class VideoDecoderTest : public ::testing::TestWithParam<Codec> {
     // Test only supports VP8, currently.
     CHECK_EQ(CODEC_VIDEO_VP8, GetParam());
     vp8_encoder_.Encode(video_frame, encoded_frame.get());
+    // Rewrite frame IDs for testing purposes.
     encoded_frame->frame_id = last_frame_id_ + 1 + num_dropped_frames;
+    if (last_frame_id_ == 0)
+      encoded_frame->referenced_frame_id = encoded_frame->frame_id;
+    else
+      encoded_frame->referenced_frame_id = encoded_frame->frame_id - 1;
     last_frame_id_ = encoded_frame->frame_id;
 
     {
