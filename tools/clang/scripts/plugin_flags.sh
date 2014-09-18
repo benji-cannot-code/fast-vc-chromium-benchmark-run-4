94407fe20b045771fa6b889c6a8ce9566ebec775
mostynb <mostynb@opera.com>
2014-09-18 03:14:39 -0700

    use absolute path to clang plugins
    clang + icecc has problems finding libFindBadConstructs.so, probably
    due to a path bug in icecc.
    
    We can however workaround this bug by using the canonical absolute path
    instead of an absolute path that includes redundant directories (ie
    remove "tools/clang/scripts/../../../" from the middle).  This has the
    benefit of simplifying and shortening clang compile commands.
    
    BUG=409571
    
    Review URL: https://codereview.chromium.org/525733005
    
    Cr-Commit-Position: refs/heads/master@{#295446}

--- tools/clang/scripts/plugin_flags.sh
+++ tools/clang/scripts/plugin_flags.sh
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # clang_use_chrome_plugins. The flags are stored in a script so that they can
 # be changed on the bots without requiring a master restart.
 
-THIS_ABS_DIR=$(cd $(dirname $0) && echo $PWD)
-CLANG_LIB_PATH=$THIS_ABS_DIR/../../../third_party/llvm-build/Release+Asserts/lib
+SRC_ABS_DIR=$(cd $(dirname $0)/../../.. && echo $PWD)
+CLANG_LIB_PATH=$SRC_ABS_DIR/third_party/llvm-build/Release+Asserts/lib
 
 if uname -s | grep -q Darwin; then
   LIBSUFFIX=dylib
