435edf1916a41ddffe806cbc073c5e7d4cc62171
vasilii <vasilii@chromium.org>
2014-09-18 03:08:14 -0700

    Collect statistics on importing empty usernames and passwords from other browsers.
    BUG=404012
    
    Review URL: https://codereview.chromium.org/579813002
    
    Cr-Commit-Position: refs/heads/master@{#295442}

--- chrome/browser/importer/in_process_importer_bridge.cc
+++ chrome/browser/importer/in_process_importer_bridge.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/importer/in_process_importer_bridge.h"
 
 #include "base/bind.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/files/file_util.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
@@ -61,6 +62,15 @@ history::VisitSource ConvertImporterVisitSourceToHistoryVisitSource(
   }
   NOTREACHED();
   return history::SOURCE_SYNCED;
+}
+
+// http://crbug.com/404012. Let's see where the empty fields come from.
+void CheckForEmptyUsernameAndPassword(const autofill::PasswordForm& form) {
+  if (form.username_value.empty() &&
+      form.password_value.empty() &&
+      !form.blacklisted_by_user) {
+    base::debug::DumpWithoutCrashing();
+  }
 }
 
 }  // namespace
@@ -252,6 +262,7 @@ void InProcessImporterBridge::SetFirefoxSearchEnginesXMLData(
 
 void InProcessImporterBridge::SetPasswordForm(
     const autofill::PasswordForm& form) {
+  CheckForEmptyUsernameAndPassword(form);
   BrowserThread::PostTask(
       BrowserThread::UI, FROM_HERE,
       base::Bind(&ProfileWriter::AddPasswordForm, writer_, form));
