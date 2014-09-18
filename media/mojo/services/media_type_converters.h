47749d3d60df2993085653e95d0a867164fc87ca
tim <tim@chromium.org>
2014-09-18 15:10:14 -0700

    media: scaffolding and plumbing for MojoRenderer{Impl, Service}
    * Add skeleton client-side and service-side MediaRenderer pieces with basic plumbing and connections but no real logic.
    * model a media::DemuxerStream in a mojom to clean up MediaRenderer interface and line-up with RendererImpl expectations.
    * add DecoderConfig plumbing support
    * add a basic unittest verifying things do actually hook up
    * consolidate around 'media_mojo_' naming in media.gyp and add media_mojo target to build all this stuff.
    
    BUG=410451
    
    Review URL: https://codereview.chromium.org/551963004
    
    Cr-Commit-Position: refs/heads/master@{#295579}

--- media/mojo/services/media_type_converters.h
+++ media/mojo/services/media_type_converters.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/mojo/interfaces/media_types.mojom.h"
 
 namespace media {
+class AudioDecoderConfig;
 class DecoderBuffer;
 }
 
@@ -26,6 +27,15 @@ struct TypeConverter<scoped_refptr<media::DecoderBuffer>,
                      MediaDecoderBufferPtr> {
   static scoped_refptr<media::DecoderBuffer> Convert(
       const MediaDecoderBufferPtr& input);
+};
+
+template <>
+struct TypeConverter<AudioDecoderConfigPtr, media::AudioDecoderConfig> {
+  static AudioDecoderConfigPtr Convert(const media::AudioDecoderConfig& input);
+};
+template <>
+struct TypeConverter<media::AudioDecoderConfig, AudioDecoderConfigPtr> {
+  static media::AudioDecoderConfig Convert(const AudioDecoderConfigPtr& input);
 };
 
 }  // namespace mojo
