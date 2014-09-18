e5136037be21386d88a504ab8ac9e9d703a032f4
cylee <cylee@chromium.org>
2014-09-18 12:12:40 -0700

    Remove flag --disable-genius-app since it's been public launched for several milestones
    BUG=chromium:350245
    
    Review URL: https://codereview.chromium.org/581583003
    
    Cr-Commit-Position: refs/heads/master@{#295517}

--- chrome/browser/ui/chrome_pages.cc
+++ chrome/browser/ui/chrome_pages.cc
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_CHROMEOS)
 #include "chrome/browser/chromeos/genius_app/app_id.h"
-#include "chromeos/chromeos_switches.h"
 #include "extensions/browser/extension_registry.h"
 #endif
 
@@ -77,16 +76,12 @@ void ShowHelpImpl(Browser* browser,
                   HelpSource source) {
   content::RecordAction(UserMetricsAction("ShowHelpTab"));
 #if defined(OS_CHROMEOS) && defined(OFFICIAL_BUILD)
-  const CommandLine* command_line = CommandLine::ForCurrentProcess();
-  if (!command_line->HasSwitch(chromeos::switches::kDisableGeniusApp)) {
-    const extensions::Extension* extension =
-        extensions::ExtensionRegistry::Get(profile)->GetExtensionById(
-            genius_app::kGeniusAppId,
-            extensions::ExtensionRegistry::EVERYTHING);
-    OpenApplication(AppLaunchParams(profile, extension, 0, host_desktop_type));
-    return;
-  }
-#endif
+  const extensions::Extension* extension =
+      extensions::ExtensionRegistry::Get(profile)->GetExtensionById(
+          genius_app::kGeniusAppId,
+          extensions::ExtensionRegistry::EVERYTHING);
+  OpenApplication(AppLaunchParams(profile, extension, 0, host_desktop_type));
+#else
   GURL url;
   switch (source) {
     case HELP_SOURCE_KEYBOARD:
@@ -108,6 +103,7 @@ void ShowHelpImpl(Browser* browser,
     browser = displayer->browser();
   }
   ShowSingletonTab(browser, url);
+#endif
 }
 
 }  // namespace
