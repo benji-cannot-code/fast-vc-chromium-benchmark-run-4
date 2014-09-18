b16f0901ef8dcfda90beb00c9269f2ce1ddd574c
qsr <qsr@chromium.org>
2014-09-18 04:07:00 -0700

    mojo: Add a runloop utility in python
    This is necessary to be able to use async operation on handles.
    
    This is a reland of https://codereview.chromium.org/552783004 after the
    build dependency issue has been fixed.
    
    R=sdefresne@chromium.org
    BUG=415485
    
    Review URL: https://codereview.chromium.org/577193002
    
    Cr-Commit-Position: refs/heads/master@{#295451}

---
+++ mojo/public/python/src/python_system_helper.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MOJO_PUBLIC_PYTHON_SRC_PYTHON_SYSTEM_HELPER_H_
+#define MOJO_PUBLIC_PYTHON_SRC_PYTHON_SYSTEM_HELPER_H_
+
+#include "Python.h"
+
+#include "mojo/public/cpp/bindings/callback.h"
+
+namespace mojo {
+
+// Create a mojo::Closure from a callable python object.
+mojo::Closure BuildClosure(PyObject* callable);
+
+// Initalize mojo::RunLoop
+void InitRunLoop();
+
+}  // namespace mojo
+
+#endif  // MOJO_PUBLIC_PYTHON_SRC_PYTHON_SYSTEM_HELPER_H_
