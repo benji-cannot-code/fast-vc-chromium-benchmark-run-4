71a91e20e77abef447e61bd34b8cfb1bf6bde010
mlamouri <mlamouri@chromium.org>
2014-09-18 03:33:10 -0700

    Add support for 'orientation' in Manifest.
    BUG=366145
    
    Review URL: https://codereview.chromium.org/576073004
    
    Cr-Commit-Position: refs/heads/master@{#295449}

--- content/public/common/manifest.h
+++ content/public/common/manifest.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/nullable_string16.h"
 #include "content/common/content_export.h"
+#include "third_party/WebKit/public/platform/WebScreenOrientationLockType.h"
 #include "url/gurl.h"
 
 namespace content {
@@ -43,6 +44,10 @@ struct CONTENT_EXPORT Manifest {
   // Set to DISPLAY_MODE_UNSPECIFIED if the parsing failed or the field was not
   // present.
   DisplayMode display;
+
+  // Set to blink::WebScreenOrientationLockDefault if the parsing failed or the
+  // field was not present.
+  blink::WebScreenOrientationLockType orientation;
 
   // Maximum length for all the strings inside the Manifest when it is sent over
   // IPC. The renderer process should truncate the strings before sending the
