12d4ab069c432645aa7af3b167cde734590af42d
ager@chromium.org <ager@chromium.org>
2014-09-18 08:22:19 +0000

    Removal of more temporary Range objects.
    The DOM/textarea-edit benchmark now creates no temporary ranges at all.
    
    R=yosin@chromium.org
    BUG=388681
    
    Review URL: https://codereview.chromium.org/576073003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182224 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/TextCheckingHelper.h
+++ third_party/WebKit/Source/core/editing/TextCheckingHelper.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef TextCheckingHelper_h
 #define TextCheckingHelper_h
 
+#include "core/dom/Position.h"
 #include "platform/heap/Handle.h"
 #include "platform/text/TextChecking.h"
 #include "wtf/text/WTFString.h"
@@ -31,7 +32,6 @@ namespace blink {
 class ExceptionState;
 class LocalFrame;
 class Range;
-class Position;
 class SpellCheckerClient;
 class TextCheckerClient;
 struct TextCheckingResult;
@@ -83,7 +83,7 @@ class TextCheckingHelper {
     WTF_MAKE_NONCOPYABLE(TextCheckingHelper);
     STACK_ALLOCATED();
 public:
-    TextCheckingHelper(SpellCheckerClient&, PassRefPtrWillBeRawPtr<Range>);
+    TextCheckingHelper(SpellCheckerClient&, const Position& start, const Position& end);
     ~TextCheckingHelper();
 
     String findFirstMisspelling(int& firstMisspellingOffset, bool markAll, RefPtrWillBeRawPtr<Range>& firstMisspellingRange);
@@ -94,7 +94,8 @@ public:
 
 private:
     SpellCheckerClient* m_client;
-    RefPtrWillBeMember<Range> m_range;
+    Position m_start;
+    Position m_end;
 
     int findFirstGrammarDetail(const Vector<GrammarDetail>& grammarDetails, int badGrammarPhraseLocation, int startOffset, int endOffset, bool markAll) const;
     bool unifiedTextCheckerEnabled() const;
