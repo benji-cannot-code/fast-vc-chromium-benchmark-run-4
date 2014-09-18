708a0ee4aec9aab98955dc8559d613c3d7fb346e
shimazu@chromium.org <shimazu@chromium.org>
2014-09-18 10:46:26 +0000

    Add ServiceWorker timing information on the popup panel in DevTools's Network tab (1/2)
    2nd patch: https://codereview.chromium.org/515753003/
    
    BUG=401389
    TEST=N/A
    
    Review URL: https://codereview.chromium.org/515583005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182231 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/network/ResourceLoadTiming.h
+++ third_party/WebKit/Source/platform/network/ResourceLoadTiming.h
@@ -50,6 +50,9 @@ public:
         timing->dnsEnd = dnsEnd;
         timing->connectStart = connectStart;
         timing->connectEnd = connectEnd;
+        timing->serviceWorkerFetchStart = serviceWorkerFetchStart;
+        timing->serviceWorkerFetchReady = serviceWorkerFetchReady;
+        timing->serviceWorkerFetchEnd = serviceWorkerFetchEnd;
         timing->sendStart = sendStart;
         timing->sendEnd = sendEnd;
         timing->receiveHeadersEnd = receiveHeadersEnd;
@@ -67,6 +70,9 @@ public:
             && dnsEnd == other.dnsEnd
             && connectStart == other.connectStart
             && connectEnd == other.connectEnd
+            && serviceWorkerFetchStart == other.serviceWorkerFetchStart
+            && serviceWorkerFetchReady == other.serviceWorkerFetchReady
+            && serviceWorkerFetchEnd == other.serviceWorkerFetchEnd
             && sendStart == other.sendStart
             && sendEnd == other.sendEnd
             && receiveHeadersEnd == other.receiveHeadersEnd
@@ -91,6 +97,9 @@ public:
     double dnsEnd;
     double connectStart;
     double connectEnd;
+    double serviceWorkerFetchStart;
+    double serviceWorkerFetchReady;
+    double serviceWorkerFetchEnd;
     double sendStart;
     double sendEnd;
     double receiveHeadersEnd;
@@ -108,6 +117,9 @@ private:
         , dnsEnd(0)
         , connectStart(0)
         , connectEnd(0)
+        , serviceWorkerFetchStart(0)
+        , serviceWorkerFetchReady(0)
+        , serviceWorkerFetchEnd(0)
         , sendStart(0)
         , sendEnd(0)
         , receiveHeadersEnd(0)
