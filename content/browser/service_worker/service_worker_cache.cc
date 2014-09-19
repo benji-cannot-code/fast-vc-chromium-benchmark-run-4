3486521185c0777fd08fa562db53b9c77d948726
gavinp <gavinp@chromium.org>
2014-09-18 21:20:28 -0700

    Remove void** from disk_cache interface.
    Enumeration and iteration were passing around void**. With this CL, we
    instead use an Iterator object.
    
    R=clamy@chromium.org,jkarlin@chromium.org,jsbell@chromium.org
    BUG=413644
    
    Review URL: https://codereview.chromium.org/542733002
    
    Cr-Commit-Position: refs/heads/master@{#295659}

--- content/browser/service_worker/service_worker_cache.cc
+++ content/browser/service_worker/service_worker_cache.cc
@@ -598,7 +598,6 @@ struct ServiceWorkerCache::KeysContext {
       : original_callback(callback),
         cache(cache),
         out_keys(new ServiceWorkerCache::Requests()),
-        backend_iterator(NULL),
         enumerated_entry(NULL) {}
 
   ~KeysContext() {
@@ -606,8 +605,6 @@ struct ServiceWorkerCache::KeysContext {
       entries[i]->Close();
     if (enumerated_entry)
       enumerated_entry->Close();
-    if (cache && backend_iterator && cache->backend_)
-      cache->backend_->EndEnumeration(&backend_iterator);
   }
 
   // The callback passed to the Keys() function.
@@ -623,7 +620,7 @@ struct ServiceWorkerCache::KeysContext {
   scoped_ptr<ServiceWorkerCache::Requests> out_keys;
 
   // Used for enumerating cache entries.
-  void* backend_iterator;
+  scoped_ptr<disk_cache::Backend::Iterator> backend_iterator;
   disk_cache::Entry* enumerated_entry;
 };
 
@@ -775,14 +772,14 @@ void ServiceWorkerCache::Keys(const RequestsCallback& callback) {
   scoped_ptr<KeysContext> keys_context(
       new KeysContext(callback, weak_ptr_factory_.GetWeakPtr()));
 
-  void** backend_iterator = &keys_context->backend_iterator;
+  keys_context->backend_iterator = backend_->CreateIterator();
+  disk_cache::Backend::Iterator& iterator = *keys_context->backend_iterator;
   disk_cache::Entry** enumerated_entry = &keys_context->enumerated_entry;
 
   net::CompletionCallback open_entry_callback =
       base::Bind(KeysDidOpenNextEntry, base::Passed(keys_context.Pass()));
 
-  int rv = backend_->OpenNextEntry(
-      backend_iterator, enumerated_entry, open_entry_callback);
+  int rv = iterator.OpenNextEntry(enumerated_entry, open_entry_callback);
 
   if (rv != net::ERR_IO_PENDING)
     open_entry_callback.Run(rv);
@@ -865,14 +862,12 @@ void ServiceWorkerCache::KeysDidOpenNextEntry(
   keys_context->enumerated_entry = NULL;
 
   // Enumerate the next entry.
-  void** backend_iterator = &keys_context->backend_iterator;
+  disk_cache::Backend::Iterator& iterator = *keys_context->backend_iterator;
   disk_cache::Entry** enumerated_entry = &keys_context->enumerated_entry;
-
   net::CompletionCallback open_entry_callback =
       base::Bind(KeysDidOpenNextEntry, base::Passed(keys_context.Pass()));
 
-  rv = cache->backend_->OpenNextEntry(
-      backend_iterator, enumerated_entry, open_entry_callback);
+  rv = iterator.OpenNextEntry(enumerated_entry, open_entry_callback);
 
   if (rv != net::ERR_IO_PENDING)
     open_entry_callback.Run(rv);
--- net/disk_cache/memory/mem_backend_impl.h
+++ net/disk_cache/memory/mem_backend_impl.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/containers/hash_tables.h"
+#include "base/memory/weak_ptr.h"
 #include "net/disk_cache/disk_cache.h"
 #include "net/disk_cache/memory/mem_rankings.h"
 
@@ -77,14 +78,15 @@ class NET_EXPORT_PRIVATE MemBackendImpl : public Backend {
                                  const CompletionCallback& callback) OVERRIDE;
   virtual int DoomEntriesSince(base::Time initial_time,
                                const CompletionCallback& callback) OVERRIDE;
-  virtual int OpenNextEntry(void** iter, Entry** next_entry,
-                            const CompletionCallback& callback) OVERRIDE;
-  virtual void EndEnumeration(void** iter) OVERRIDE;
+  virtual scoped_ptr<Iterator> CreateIterator() OVERRIDE;
   virtual void GetStats(
       std::vector<std::pair<std::string, std::string> >* stats) OVERRIDE {}
   virtual void OnExternalCacheHit(const std::string& key) OVERRIDE;
 
  private:
+  class MemIterator;
+  friend class MemIterator;
+
   typedef base::hash_map<std::string, MemEntryImpl*> EntryMap;
 
   // Old Backend interface.
@@ -95,7 +97,6 @@ class NET_EXPORT_PRIVATE MemBackendImpl : public Backend {
   bool DoomEntriesBetween(const base::Time initial_time,
                           const base::Time end_time);
   bool DoomEntriesSince(const base::Time initial_time);
-  bool OpenNextEntry(void** iter, Entry** next_entry);
 
   // Deletes entries from the cache until the current size is below the limit.
   // If empty is true, the whole cache will be trimmed, regardless of being in
@@ -112,6 +113,8 @@ class NET_EXPORT_PRIVATE MemBackendImpl : public Backend {
   int32 current_size_;
 
   net::NetLog* net_log_;
+
+  base::WeakPtrFactory<MemBackendImpl> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(MemBackendImpl);
 };
