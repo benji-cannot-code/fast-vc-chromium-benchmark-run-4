dc836f6b8aa76027c89b79507dd3b64009e5cae6
mlamouri <mlamouri@chromium.org>
2014-09-19 06:13:31 -0700

    Explicitely unlock screen orientation when navigating.
    BUG=162827
    
    Review URL: https://codereview.chromium.org/582373003
    
    Cr-Commit-Position: refs/heads/master@{#295692}

--- content/browser/screen_orientation/screen_orientation_dispatcher_host_impl.cc
+++ content/browser/screen_orientation/screen_orientation_dispatcher_host_impl.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/screen_orientation/screen_orientation_dispatcher_host_impl.h"
 
 #include "content/common/screen_orientation_messages.h"
+#include "content/public/browser/navigation_details.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/render_view_host.h"
@@ -54,6 +55,13 @@ bool ScreenOrientationDispatcherHostImpl::OnMessageReceived(
   IPC_END_MESSAGE_MAP()
 
   return handled;
+}
+
+void ScreenOrientationDispatcherHostImpl::DidNavigateMainFrame(
+    const LoadCommittedDetails& details, const FrameNavigateParams& params) {
+  if (!provider_ || details.is_in_page)
+    return;
+  provider_->UnlockOrientation();
 }
 
 RenderFrameHost*
