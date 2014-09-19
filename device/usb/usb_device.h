6f53d572809f8d80974a907a0af5223eaf36a039
reillyg <reillyg@chromium.org>
2014-09-19 08:00:01 -0700

    Add a service to track devices selected by the user.
    apps::SavedDevicesService tracks USB devices that have been selected by
    the user in the context of a given extension. Devices that can be
    identified accurately after they have been reconnected because they have
    a serial number are written out to ExtensionPrefs. All others are only
    remembered until they are disconnected.
    
    A new OnDisconnect observer function has been added to UsbDevice to
    enable this monitoring.
    
    BUG=346953
    
    Review URL: https://codereview.chromium.org/580963002
    
    Cr-Commit-Position: refs/heads/master@{#295708}

--- device/usb/usb_device.h
+++ device/usb/usb_device.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/callback.h"
 #include "base/memory/ref_counted.h"
+#include "base/observer_list.h"
 
 namespace device {
 
@@ -20,6 +21,11 @@ struct UsbConfigDescriptor;
 // UsbDeviceHandle must be created from Open() method.
 class UsbDevice : public base::RefCountedThreadSafe<UsbDevice> {
  public:
+  class Observer {
+   public:
+    virtual void OnDisconnect(scoped_refptr<UsbDevice> device) = 0;
+  };
+
   // Accessors to basic information.
   uint16 vendor_id() const { return vendor_id_; }
   uint16 product_id() const { return product_id_; }
@@ -49,11 +55,14 @@ class UsbDevice : public base::RefCountedThreadSafe<UsbDevice> {
   // Blocking method. Must be called on FILE thread.
   virtual const UsbConfigDescriptor& GetConfiguration() = 0;
 
- protected:
-  UsbDevice(uint16 vendor_id, uint16 product_id, uint32 unique_id)
-      : vendor_id_(vendor_id), product_id_(product_id), unique_id_(unique_id) {}
+  void AddObserver(Observer* obs) { observer_list_.AddObserver(obs); }
+  void RemoveObserver(Observer* obs) { observer_list_.RemoveObserver(obs); }
 
-  virtual ~UsbDevice() {}
+ protected:
+  UsbDevice(uint16 vendor_id, uint16 product_id, uint32 unique_id);
+  virtual ~UsbDevice();
+
+  void NotifyDisconnect();
 
  private:
   friend class base::RefCountedThreadSafe<UsbDevice>;
@@ -61,6 +70,8 @@ class UsbDevice : public base::RefCountedThreadSafe<UsbDevice> {
   const uint16 vendor_id_;
   const uint16 product_id_;
   const uint32 unique_id_;
+
+  ObserverList<Observer> observer_list_;
 
   DISALLOW_COPY_AND_ASSIGN(UsbDevice);
 };
