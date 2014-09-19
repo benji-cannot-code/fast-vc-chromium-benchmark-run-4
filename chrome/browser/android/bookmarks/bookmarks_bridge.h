061598ba6de1f77a4c6700960d56a7f6caef2b14
ianwen <ianwen@chromium.org>
2014-09-18 18:00:26 -0700

    Instrumental test for BookmarksBridge. It currently tests these functionalities: addBookmark, addFolder and getAllFoldersWithDepths.
    BUG=411545
    
    Review URL: https://codereview.chromium.org/550543003
    
    Cr-Commit-Position: refs/heads/master@{#295627}

--- chrome/browser/android/bookmarks/bookmarks_bridge.h
+++ chrome/browser/android/bookmarks/bookmarks_bridge.h
@@ -32,6 +32,8 @@ class BookmarksBridge : public BaseBookmarkModelObserver,
 
   bool IsDoingExtensiveChanges(JNIEnv* env, jobject obj);
 
+  void LoadEmptyPartnerBookmarkShimForTesting(JNIEnv* env, jobject obj);
+
   base::android::ScopedJavaLocalRef<jobject> GetBookmarkByID(
       JNIEnv* env,
       jobject obj,
@@ -62,6 +64,12 @@ class BookmarksBridge : public BaseBookmarkModelObserver,
 
   base::android::ScopedJavaLocalRef<jobject> GetMobileFolderId(JNIEnv* env,
                                                                jobject obj);
+
+  base::android::ScopedJavaLocalRef<jobject> GetOtherFolderId(JNIEnv* env,
+                                                              jobject obj);
+
+  base::android::ScopedJavaLocalRef<jobject> GetDesktopFolderId(JNIEnv* env,
+                                                                jobject obj);
 
   void GetChildIDs(JNIEnv* env,
                    jobject obj,
@@ -101,15 +109,27 @@ class BookmarksBridge : public BaseBookmarkModelObserver,
                                  jobject j_callback_obj,
                                  jobject j_result_obj);
 
-  void DeleteBookmark(JNIEnv* env,
-                      jobject obj,
-                      jobject j_bookmark_id_obj);
+  base::android::ScopedJavaLocalRef<jobject> AddFolder(JNIEnv* env,
+                                                       jobject obj,
+                                                       jobject j_parent_id_obj,
+                                                       jint index,
+                                                       jstring j_title);
+
+  void DeleteBookmark(JNIEnv* env, jobject obj, jobject j_bookmark_id_obj);
 
   void MoveBookmark(JNIEnv* env,
                     jobject obj,
                     jobject j_bookmark_id_obj,
                     jobject j_parent_id_obj,
                     jint index);
+
+  base::android::ScopedJavaLocalRef<jobject> AddBookmark(
+      JNIEnv* env,
+      jobject obj,
+      jobject j_parent_id_obj,
+      jint index,
+      jstring j_title,
+      jstring j_url);
 
  private:
   virtual ~BookmarksBridge();
