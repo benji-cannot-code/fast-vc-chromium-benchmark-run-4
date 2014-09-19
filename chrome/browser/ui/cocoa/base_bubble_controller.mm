92edd78f0cb6208ab951de650699b7d036608446
tapted <tapted@chromium.org>
2014-09-18 17:35:28 -0700

    Mac: Give bubble windows an initial position before showing them.
    Otherwise initialization that occurs offscreen can load incorrect screen
    metric information. E.g. a RenderView will assume it is on the primary
    display when loaded in a bubble, which results in it being created with
    an incorrect scale factor in some screen setups.
    
    This CL adds a call to -[BaseBubbleController setAnchorPosition] in the
    non-NIB initializer to give the window a good estimate of the position
    it will eventually be shown at.
    
    BUG=324748
    
    Review URL: https://codereview.chromium.org/573143002
    
    Cr-Commit-Position: refs/heads/master@{#295625}

--- chrome/browser/ui/cocoa/base_bubble_controller.mm
+++ chrome/browser/ui/cocoa/base_bubble_controller.mm
@@ -73,7 +73,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   DCHECK(theWindow);
   if ((self = [super initWithWindow:theWindow])) {
     parentWindow_ = parentWindow;
-    anchor_ = anchoredAt;
     shouldOpenAsKeyWindow_ = YES;
     shouldCloseOnResignKey_ = YES;
 
@@ -87,6 +86,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     [self registerForNotifications];
     [self awakeFromNib];
+    [self setAnchorPoint:anchoredAt];
   }
   return self;
 }
