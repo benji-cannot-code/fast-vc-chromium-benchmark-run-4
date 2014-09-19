d97a46b3af31e18bf38fa9bbaf5d2f172a118d99
lanwei <lanwei@chromium.org>
2014-09-19 07:52:45 -0700

    Clean up GestureEventDetails constructors and fix unit tests.
    Reland the code change for Issue 573963005: Clean up GestureEventDetails
    constructors and fix unit tests:
    https://codereview.chromium.org/573963005/.
    
    The code does not change, just merge two commits into one.
    
    BUG=350942
    TBR=tdresser@chromium.org,jdduke@chromium.org,sadrul@chromium.org,sky@chromium.org
    
    Review URL: https://codereview.chromium.org/581963004
    
    Cr-Commit-Position: refs/heads/master@{#295706}

--- ui/views/widget/root_view_unittest.cc
+++ ui/views/widget/root_view_unittest.cc
@@ -207,14 +207,11 @@ TEST_F(RootViewTest, ContextMenuFromLongPress) {
       5,
       0,
       base::TimeDelta(),
-      ui::GestureEventDetails(ui::ET_GESTURE_LONG_PRESS, 0, 0));
+      ui::GestureEventDetails(ui::ET_GESTURE_LONG_PRESS));
   ui::EventDispatchDetails details = root_view->OnEventFromSource(&long_press1);
 
-  ui::GestureEvent end1(5,
-                        5,
-                        0,
-                        base::TimeDelta(),
-                        ui::GestureEventDetails(ui::ET_GESTURE_END, 0, 0));
+  ui::GestureEvent end1(
+      5, 5, 0, base::TimeDelta(), ui::GestureEventDetails(ui::ET_GESTURE_END));
   details = root_view->OnEventFromSource(&end1);
 
   EXPECT_FALSE(details.target_destroyed);
@@ -229,14 +226,11 @@ TEST_F(RootViewTest, ContextMenuFromLongPress) {
       5,
       0,
       base::TimeDelta(),
-      ui::GestureEventDetails(ui::ET_GESTURE_LONG_PRESS, 0, 0));
+      ui::GestureEventDetails(ui::ET_GESTURE_LONG_PRESS));
   details = root_view->OnEventFromSource(&long_press2);
 
-  ui::GestureEvent end2(25,
-                        5,
-                        0,
-                        base::TimeDelta(),
-                        ui::GestureEventDetails(ui::ET_GESTURE_END, 0, 0));
+  ui::GestureEvent end2(
+      25, 5, 0, base::TimeDelta(), ui::GestureEventDetails(ui::ET_GESTURE_END));
   details = root_view->OnEventFromSource(&end2);
 
   EXPECT_FALSE(details.target_destroyed);
@@ -251,14 +245,11 @@ TEST_F(RootViewTest, ContextMenuFromLongPress) {
       50,
       0,
       base::TimeDelta(),
-      ui::GestureEventDetails(ui::ET_GESTURE_LONG_PRESS, 0, 0));
+      ui::GestureEventDetails(ui::ET_GESTURE_LONG_PRESS));
   details = root_view->OnEventFromSource(&long_press3);
 
-  ui::GestureEvent end3(25,
-                        5,
-                        0,
-                        base::TimeDelta(),
-                        ui::GestureEventDetails(ui::ET_GESTURE_END, 0, 0));
+  ui::GestureEvent end3(
+      25, 5, 0, base::TimeDelta(), ui::GestureEventDetails(ui::ET_GESTURE_END));
   details = root_view->OnEventFromSource(&end3);
 
   EXPECT_FALSE(details.target_destroyed);
@@ -301,14 +292,11 @@ TEST_F(RootViewTest, ContextMenuFromLongPressOnDisabledView) {
       5,
       0,
       base::TimeDelta(),
-      ui::GestureEventDetails(ui::ET_GESTURE_LONG_PRESS, 0, 0));
+      ui::GestureEventDetails(ui::ET_GESTURE_LONG_PRESS));
   ui::EventDispatchDetails details = root_view->OnEventFromSource(&long_press1);
 
-  ui::GestureEvent end1(5,
-                        5,
-                        0,
-                        base::TimeDelta(),
-                        ui::GestureEventDetails(ui::ET_GESTURE_END, 0, 0));
+  ui::GestureEvent end1(
+      5, 5, 0, base::TimeDelta(), ui::GestureEventDetails(ui::ET_GESTURE_END));
   details = root_view->OnEventFromSource(&end1);
 
   EXPECT_FALSE(details.target_destroyed);
@@ -323,14 +311,11 @@ TEST_F(RootViewTest, ContextMenuFromLongPressOnDisabledView) {
       5,
       0,
       base::TimeDelta(),
-      ui::GestureEventDetails(ui::ET_GESTURE_LONG_PRESS, 0, 0));
+      ui::GestureEventDetails(ui::ET_GESTURE_LONG_PRESS));
   details = root_view->OnEventFromSource(&long_press2);
 
-  ui::GestureEvent end2(25,
-                        5,
-                        0,
-                        base::TimeDelta(),
-                        ui::GestureEventDetails(ui::ET_GESTURE_END, 0, 0));
+  ui::GestureEvent end2(
+      25, 5, 0, base::TimeDelta(), ui::GestureEventDetails(ui::ET_GESTURE_END));
   details = root_view->OnEventFromSource(&end2);
 
   EXPECT_FALSE(details.target_destroyed);
@@ -345,14 +330,11 @@ TEST_F(RootViewTest, ContextMenuFromLongPressOnDisabledView) {
       50,
       0,
       base::TimeDelta(),
-      ui::GestureEventDetails(ui::ET_GESTURE_LONG_PRESS, 0, 0));
+      ui::GestureEventDetails(ui::ET_GESTURE_LONG_PRESS));
   details = root_view->OnEventFromSource(&long_press3);
 
-  ui::GestureEvent end3(25,
-                        5,
-                        0,
-                        base::TimeDelta(),
-                        ui::GestureEventDetails(ui::ET_GESTURE_END, 0, 0));
+  ui::GestureEvent end3(
+      25, 5, 0, base::TimeDelta(), ui::GestureEventDetails(ui::ET_GESTURE_END));
   details = root_view->OnEventFromSource(&end3);
 
   EXPECT_FALSE(details.target_destroyed);
