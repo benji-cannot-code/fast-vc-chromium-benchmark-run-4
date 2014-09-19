93e71a68d015c281b82c446af39a02b4d7c52941
yosin@chromium.org <yosin@chromium.org>
2014-09-19 09:51:11 +0000

    Create a dummy resource for DocumentExecCommand.js
    This prepares for the landing of
    http://crrev.com/530663002
    
    which now requires a three-sided change due to changes in how Blink
    accesses private script resources.
    
    Part 2: http://crrev.com/588473003
    
    BUG=408887
    TEST=n/a; no code changes
    
    Review URL: https://codereview.chromium.org/583753003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182312 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/Source/core/editing/js/DocumentExecCommand.js
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/**
+ * This is a placeholder to create the
+ * IDR_PRIVATE_SCRIPT_DOCUMENTEXECCOMMAND_JS resource.
+ *
+ * It will be replaced by a complete file in:
+ *   https://codereview.chromium.org/530663002/
+ */
