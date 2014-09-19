f760a641dc7ea3f2d8269448957e4017182e6bd3
iseki <iseki@chromium.org>
2014-09-18 18:38:22 -0700

    Modify the action of cancel button in syncing status to cancel all jobs.
    * Previous behavior is to cancel only one item.
    * After Issue 400636, this behavior doesn't make sense because only the
      number is decreased.
    
    BUG=414610
    TEST=manually
    1. Copy the file from Download to Drive.
    2. Push the button in syncing status.
    3. Confirm all jobs are canceled.
    
    Review URL: https://codereview.chromium.org/576063002
    
    Cr-Commit-Position: refs/heads/master@{#295638}

--- ui/file_manager/file_manager/background/js/drive_sync_handler.js
+++ ui/file_manager/file_manager/background/js/drive_sync_handler.js
@@ -153,7 +153,8 @@ DriveSyncHandler.prototype.removeItem_ = function(status) {
  * @private
  */
 DriveSyncHandler.prototype.requestCancel_ = function(entry) {
-  chrome.fileManagerPrivate.cancelFileTransfers([entry.toURL()], function() {});
+  // Cancel all jobs.
+  chrome.fileManagerPrivate.cancelFileTransfers(function() {});
 };
 
 /**
