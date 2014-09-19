e3e20794971411bfa2c1c4184489a9437046719b
kn.darshini <kn.darshini@samsung.com>
2014-09-18 20:27:33 -0700

    Removing onBookmarkChanged notification to application on history change and added proper history change notification on history data change.
    BUG=None
    
    Review URL: https://codereview.chromium.org/579753002
    
    Cr-Commit-Position: refs/heads/master@{#295651}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeBrowserProvider.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeBrowserProvider.java
@@ -1046,6 +1046,11 @@ public class ChromeBrowserProvider extends ContentProvider {
     }
 
     @CalledByNative
+    private void onHistoryChanged() {
+        notifyChange(buildAPIContentUri(getContext(), HISTORY_PATH));
+    }
+
+    @CalledByNative
     private void onSearchTermChanged() {
         notifyChange(buildAPIContentUri(getContext(), SEARCHES_PATH));
     }
