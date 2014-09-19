3d1a13c1e9a0c33272f4ac9dff476542179671b0
rfevang <rfevang@chromium.org>
2014-09-19 15:06:24 -0700

    Only set remote id during url node creation.
    Folders should only have their id set by the server, and the clients
    should only set the remote id on clips they created themselves.
    
    Additionally, EnhancedBookmarkModel now monitors the remote id field for bookmarks, and initiates a de-duping protocol whenever two (or more) nodes with the same id are detected.
    BUG=413876
    
    Review URL: https://codereview.chromium.org/563363002
    
    Cr-Commit-Position: refs/heads/master@{#295790}

--- components/enhanced_bookmarks.gypi
+++ components/enhanced_bookmarks.gypi
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'enhanced_bookmarks/bookmark_server_service.h',
         'enhanced_bookmarks/enhanced_bookmark_model.cc',
         'enhanced_bookmarks/enhanced_bookmark_model.h',
+        'enhanced_bookmarks/enhanced_bookmark_model_observer.h',
         'enhanced_bookmarks/enhanced_bookmark_utils.cc',
         'enhanced_bookmarks/enhanced_bookmark_utils.h',
         'enhanced_bookmarks/image_store.cc',
--- components/enhanced_bookmarks/bookmark_server_search_service.cc
+++ components/enhanced_bookmarks/bookmark_server_search_service.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/enhanced_bookmarks/bookmark_server_search_service.h"
 
-#include "components/bookmarks/browser/bookmark_model.h"
 #include "components/enhanced_bookmarks/enhanced_bookmark_utils.h"
 #include "components/enhanced_bookmarks/proto/search.pb.h"
 #include "net/base/url_util.h"
@@ -22,11 +21,11 @@ BookmarkServerSearchService::BookmarkServerSearchService(
     scoped_refptr<net::URLRequestContextGetter> request_context_getter,
     ProfileOAuth2TokenService* token_service,
     SigninManagerBase* signin_manager,
-    BookmarkModel* bookmark_model)
+    EnhancedBookmarkModel* enhanced_bookmark_model)
     : BookmarkServerService(request_context_getter,
                             token_service,
                             signin_manager,
-                            bookmark_model) {
+                            enhanced_bookmark_model) {
 }
 
 BookmarkServerSearchService::~BookmarkServerSearchService() {
@@ -100,17 +99,19 @@ void BookmarkServerSearchService::CleanAfterFailure() {
   searches_.clear();
 }
 
-void BookmarkServerSearchService::BookmarkNodeAdded(BookmarkModel* model,
-                                                    const BookmarkNode* parent,
-                                                    int index) {
-  BookmarkServerService::BookmarkNodeAdded(model, parent, index);
+void BookmarkServerSearchService::EnhancedBookmarkAdded(
+    const BookmarkNode* node) {
   searches_.clear();
 }
 
-void BookmarkServerSearchService::BookmarkMetaInfoChanged(
-    BookmarkModel* model,
-    const BookmarkNode* node) {
-  BookmarkServerService::BookmarkMetaInfoChanged(model, node);
+void BookmarkServerSearchService::EnhancedBookmarkAllUserNodesRemoved() {
+  searches_.clear();
+}
+
+void BookmarkServerSearchService::EnhancedBookmarkRemoteIdChanged(
+    const BookmarkNode* node,
+    const std::string& old_remote_id,
+    const std::string& remote_id) {
   searches_.clear();
 }
 }  // namespace enhanced_bookmarks
