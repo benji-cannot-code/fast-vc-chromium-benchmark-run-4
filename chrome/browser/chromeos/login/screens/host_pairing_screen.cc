58d2ed9a21a71610594cae0d3796dfa16a45aff1
dzhioev <dzhioev@chromium.org>
2014-09-19 10:09:18 -0700

    Host pairing OOBE starts at the right time.
    The following behaviour implmented:
    * If "new-remora-oobe" switch is not set, OOBE starts with with the regular
      "remora" UI. At the same time wizard controller we start to listen for
      incoming connection from shark controller. If connection is established, we
      switch UI to the new one.
    * Otherwise, we launch the new UI from the beginning of OOBE.
    
    Several small issues fixed as well.
    
    BUG=405150
    TEST=manually
    
    Review URL: https://codereview.chromium.org/528803002
    
    Cr-Commit-Position: refs/heads/master@{#295721}

--- chrome/browser/chromeos/login/screens/host_pairing_screen.cc
+++ chrome/browser/chromeos/login/screens/host_pairing_screen.cc
@@ -47,7 +47,7 @@ void HostPairingScreen::PrepareToShow() {
 void HostPairingScreen::Show() {
   if (actor_)
     actor_->Show();
-  controller_->StartPairing();
+  PairingStageChanged(controller_->GetCurrentStage());
 }
 
 void HostPairingScreen::Hide() {
@@ -60,8 +60,6 @@ std::string HostPairingScreen::GetName() const {
 }
 
 void HostPairingScreen::PairingStageChanged(Stage new_stage) {
-  DCHECK(new_stage != current_stage_);
-
   std::string desired_page;
   switch (new_stage) {
     case HostPairingController::STAGE_NONE:
