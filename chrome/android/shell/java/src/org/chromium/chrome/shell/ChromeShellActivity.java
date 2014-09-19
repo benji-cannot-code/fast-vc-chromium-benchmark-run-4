ccab628100a0f0b07a8c9cd73106672f1fa6acb4
ankit2.kumar <ankit2.kumar@samsung.com>
2014-09-18 21:33:43 -0700

    [Chrome Shell]Added code to regain focus on selection of any item from options menu.
    Currently if some option is selected from options menu then in that
    case content view does not gain focus in operations(like prev,next ).
    Added code to handle this.
    
    BUG=414219
    
    Review URL: https://codereview.chromium.org/573653003
    
    Cr-Commit-Position: refs/heads/master@{#295661}

--- chrome/android/shell/java/src/org/chromium/chrome/shell/ChromeShellActivity.java
+++ chrome/android/shell/java/src/org/chromium/chrome/shell/ChromeShellActivity.java
@@ -14,6 +14,7 @@ import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
 import org.chromium.base.ApiCompatibilityUtils;
@@ -292,6 +293,12 @@ public class ChromeShellActivity extends Activity implements AppMenuPropertiesDe
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         ChromeShellTab activeTab = getActiveTab();
+        if (activeTab != null) {
+            ViewGroup containerView = activeTab.getContentViewCore().getContainerView();
+            if (containerView.isFocusable() && containerView.isFocusableInTouchMode()) {
+                containerView.requestFocus();
+            }
+        }
         switch (item.getItemId()) {
             case R.id.signin:
                 if (ChromeSigninController.get(this).isSignedIn()) {
