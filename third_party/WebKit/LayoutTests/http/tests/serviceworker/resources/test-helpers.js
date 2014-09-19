240da250721fe31dcf7fdf5f1ee4597f17667a3b
ksakamoto@chromium.org <ksakamoto@chromium.org>
2014-09-19 05:37:33 +0000

    Use getRegistration in service_worker_unregister_and_register
    Now it can be used with a worker that doesn't install successfully.
    
    BUG=404064
    
    Review URL: https://codereview.chromium.org/587573002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182301 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/serviceworker/resources/test-helpers.js
+++ third_party/WebKit/LayoutTests/http/tests/serviceworker/resources/test-helpers.js
@@ -1,19 +1,12 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Adapter for testharness.js-style tests with Service Workers
 
-// Can only be used with a worker that installs successfully, since it
-// first registers to acquire a ServiceWorkerRegistration object to
-// unregister.
-// FIXME: Use getRegistration() when implemented.
 function service_worker_unregister_and_register(test, url, scope) {
   if (!scope || scope.length == 0)
     return Promise.reject(new Error('tests must define a scope'));
 
   var options = { scope: scope };
-  return navigator.serviceWorker.register(url, options)
-      .then(function(registration) {
-          return registration.unregister();
-        })
+  return service_worker_unregister(test, scope)
       .then(function() {
           return navigator.serviceWorker.register(url, options);
         })
