9d20d353db7fe16e62d19be768a65ac21be55949
dnicoara <dnicoara@chromium.org>
2014-09-19 07:26:59 -0700

    Reland of: Remove deprecated cursor event filter for Ozone
    Initial code review: https://codereview.chromium.org/580553002/
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/575143002
    
    Cr-Commit-Position: refs/heads/master@{#295701}

--- ash/display/mouse_cursor_event_filter.cc
+++ ash/display/mouse_cursor_event_filter.cc
@@ -153,7 +153,6 @@ void MouseCursorEventFilter::OnDisplaysInitialized() {
   OnDisplayConfigurationChanged();
 }
 
-#if !defined(USE_OZONE)
 void MouseCursorEventFilter::OnDisplayConfigurationChanged() {
   // Extra check for |num_connected_displays()| is for SystemDisplayApiTest
   // that injects MockScreen.
@@ -175,7 +174,6 @@ void MouseCursorEventFilter::OnDisplayConfigurationChanged() {
   else
     UpdateVerticalEdgeBounds();
 }
-#endif
 
 void MouseCursorEventFilter::OnMouseEvent(ui::MouseEvent* event) {
   aura::Window* target = static_cast<aura::Window*>(event->target());
@@ -225,7 +223,6 @@ void MouseCursorEventFilter::MoveCursorTo(aura::Window* root,
   root->GetHost()->MoveCursorToHostLocation(point_in_host);
 }
 
-#if !defined(USE_OZONE)
 bool MouseCursorEventFilter::WarpMouseCursorIfNecessary(ui::MouseEvent* event) {
   if (!event->HasNativeEvent())
     return false;
@@ -233,8 +230,16 @@ bool MouseCursorEventFilter::WarpMouseCursorIfNecessary(ui::MouseEvent* event) {
   gfx::Point point_in_native =
       ui::EventSystemLocationFromNative(event->native_event());
 
-  gfx::Point point_in_screen = event->location();
   aura::Window* target = static_cast<aura::Window*>(event->target());
+#if defined(USE_OZONE)
+  // TODO(dnicoara): crbug.com/415680 Move cursor warping into Ozone once Ozone
+  // has access to the logical display layout.
+  // Native events in Ozone are in the native window coordinate system. We need
+  // to translate them to get the global position.
+  point_in_native.Offset(target->GetHost()->GetBounds().x(),
+                         target->GetHost()->GetBounds().y());
+#endif
+  gfx::Point point_in_screen = event->location();
   ::wm::ConvertPointToScreen(target, &point_in_screen);
 
   return WarpMouseCursorInNativeCoords(point_in_native, point_in_screen);
@@ -264,7 +269,6 @@ bool MouseCursorEventFilter::WarpMouseCursorInNativeCoords(
 
   return true;
 }
-#endif
 
 void MouseCursorEventFilter::UpdateHorizontalEdgeBounds() {
   bool from_primary = Shell::GetPrimaryRootWindow() == drag_source_root_;
@@ -375,7 +379,6 @@ void MouseCursorEventFilter::GetSrcAndDstRootWindows(aura::Window** src_root,
   *dst_root = root_windows[0] == *src_root ? root_windows[1] : root_windows[0];
 }
 
-#if !defined(USE_OZONE)
 bool MouseCursorEventFilter::WarpMouseCursorIfNecessaryForTest(
     aura::Window* target_root,
     const gfx::Point& point_in_screen) {
@@ -384,6 +387,5 @@ bool MouseCursorEventFilter::WarpMouseCursorIfNecessaryForTest(
   target_root->GetHost()->ConvertPointToNativeScreen(&native);
   return WarpMouseCursorInNativeCoords(native, point_in_screen);
 }
-#endif
 
 }  // namespace ash
