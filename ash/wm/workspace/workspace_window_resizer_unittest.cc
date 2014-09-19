f3021587cb1f3d6c178a0caf70e91e90e93456ff
jackhou <jackhou@chromium.org>
2014-09-18 20:56:45 -0700

    [Ash] Only snap windows that can maximize.
    Previously, windows that have only one of maxWidth or maxHeight defined
    could not be maximized by the user, but they would still snap when
    dragged to the edge of the screen.
    
    This changes WindowState::CanSnap to use CanMaximize which is false
    when there is a maxWidth or maxHeight, or when the window is not
    resizable.
    
    BUG=412610, 408737
    
    Review URL: https://codereview.chromium.org/557693002
    
    Cr-Commit-Position: refs/heads/master@{#295657}

--- ash/wm/workspace/workspace_window_resizer_unittest.cc
+++ ash/wm/workspace/workspace_window_resizer_unittest.cc
@@ -557,6 +557,7 @@ TEST_F(WorkspaceWindowResizerTest, Edge) {
   // http://crbug.com/292238.
   // Window is wide enough not to get docked right away.
   window_->SetBounds(gfx::Rect(20, 30, 400, 60));
+  window_->SetProperty(aura::client::kCanMaximizeKey, true);
   wm::WindowState* window_state = wm::GetWindowState(window_.get());
 
   {
@@ -1416,6 +1417,7 @@ TEST_F(WorkspaceWindowResizerTest, MagneticallyResize_LEFT) {
 // Test that the user user moved window flag is getting properly set.
 TEST_F(WorkspaceWindowResizerTest, CheckUserWindowManagedFlags) {
   window_->SetBounds(gfx::Rect( 0,  50, 400, 200));
+  window_->SetProperty(aura::client::kCanMaximizeKey, true);
 
   std::vector<aura::Window*> no_attached_windows;
   // Check that an abort doesn't change anything.
