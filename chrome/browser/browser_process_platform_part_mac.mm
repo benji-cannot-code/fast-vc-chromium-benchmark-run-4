d407275dece874a80b6c0d44c4c4bf4dd63d515d
dcheng <dcheng@chromium.org>
2014-09-19 20:22:09 -0700

    Remove implicit conversions from scoped_refptr to T* in chrome/
    This patch was generated by running the rewrite_scoped_refptr clang tool
    on a Mac build.
    
    BUG=110610
    
    Review URL: https://codereview.chromium.org/591493002
    
    Cr-Commit-Position: refs/heads/master@{#295854}

--- chrome/browser/browser_process_platform_part_mac.mm
+++ chrome/browser/browser_process_platform_part_mac.mm
@@ -27,7 +27,7 @@ void BrowserProcessPlatformPart::AttemptExit() {
 void BrowserProcessPlatformPart::PreMainMessageLoopRun() {
   // AppShimHostManager can not simply be reset, otherwise destroying the old
   // domain socket will cause the just-created socket to be unlinked.
-  DCHECK(!app_shim_host_manager_);
+  DCHECK(!app_shim_host_manager_.get());
   app_shim_host_manager_ = new AppShimHostManager;
 }
 
