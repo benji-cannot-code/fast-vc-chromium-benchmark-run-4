dc219bfa4adb26c04516a916d357948bd6558272
sigbjornf@opera.com <sigbjornf@opera.com>
2014-09-20 17:34:51 +0000

    Oilpan: remove unused support for removing FrameDestructionObservers.
    Remove ability to explicitly remove a FrameDestructionObserver from a
    LocalFrame; this is taken care of by weak references instead.
    
    Also make FrameDestructionObserver::observeFrame() private; this will ease
    later removal once we can assume Oilpan is enabled-by-default for all
    observer implementations.
    
    R=haraken
    BUG=340522
    
    Review URL: https://codereview.chromium.org/584403002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182370 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/LocalFrame.cpp
+++ third_party/WebKit/Source/core/frame/LocalFrame.cpp
@@ -309,10 +309,12 @@ void LocalFrame::addDestructionObserver(FrameDestructionObserver* observer)
     m_destructionObservers.add(observer);
 }
 
+#if !ENABLE(OILPAN)
 void LocalFrame::removeDestructionObserver(FrameDestructionObserver* observer)
 {
     m_destructionObservers.remove(observer);
 }
+#endif
 
 void LocalFrame::willDetachFrameHost()
 {
