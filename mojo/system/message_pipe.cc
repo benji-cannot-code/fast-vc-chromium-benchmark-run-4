c7979c3435db68f74bfa4bdf87970ea75814eeed
Viet-Trung Luu <viettrungluu@chromium.org>
2014-09-19 17:05:47 -0700

    Mojo: Make MessagePipe::ConvertLocalToProxy() create the ChannelEndpoint.
    This will be needed to move the "paused message queue" into the
    ChannelEndpoint, since transferring messages from the
    LocalMessagePipeEndpoint needs to happen "atomically" with its
    conversion to a ProxyMessagePipeEndpoint. (Before the MessagePipe lock
    is released, the ProxyMessagePipeEndpoint needs to have a place to queue
    messages, and any already-queued messages must be at the front of that
    queue.)
    
    R=darin@chromium.org
    
    Review URL: https://codereview.chromium.org/585093002
    
    Cr-Commit-Position: refs/heads/master@{#295822}

--- mojo/system/message_pipe.cc
+++ mojo/system/message_pipe.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/system/message_pipe.h"
 
 #include "base/logging.h"
+#include "mojo/system/channel_endpoint.h"
 #include "mojo/system/local_message_pipe_endpoint.h"
 #include "mojo/system/message_in_transit.h"
 #include "mojo/system/message_pipe_dispatcher.h"
@@ -146,7 +147,7 @@ void MessagePipe::RemoveWaiter(unsigned port,
   endpoints_[port]->RemoveWaiter(waiter, signals_state);
 }
 
-void MessagePipe::ConvertLocalToProxy(unsigned port) {
+scoped_refptr<ChannelEndpoint> MessagePipe::ConvertLocalToProxy(unsigned port) {
   DCHECK(port == 0 || port == 1);
 
   base::AutoLock locker(lock_);
@@ -170,6 +171,8 @@ void MessagePipe::ConvertLocalToProxy(unsigned port) {
           static_cast<LocalMessagePipeEndpoint*>(endpoints_[port].get()),
           is_peer_open));
   endpoints_[port].swap(replacement_endpoint);
+
+  return make_scoped_refptr(new ChannelEndpoint(this, port));
 }
 
 MojoResult MessagePipe::EnqueueMessage(unsigned port,
