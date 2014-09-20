a45afc05e8a64fff302a0d5c8a7551cc65293f4c
hendrikw <hendrikw@chromium.org>
2014-09-20 06:14:25 -0700

    This is an optimization that uses solid color detection to prevent PictureLayerImpl from creating tiles if the entire layer is solid.
    If the entire layer is solid, PictureLayerImpl will now draw
    solid quads using a method provided in SolidColorLayerImpl.
    
    The is_solid_color_ and solid_color members have been moved
    from PicturePile To BasePicturePile so that the properties
    are copied between PicturePile and PicturePileImpl.
    
    BUG=396908
    
    Review URL: https://codereview.chromium.org/519583003
    
    Cr-Commit-Position: refs/heads/master@{#295860}

--- cc/resources/picture_pile.h
+++ cc/resources/picture_pile.h
@@ -52,9 +52,6 @@ class CC_EXPORT PicturePile : public PicturePileBase {
     is_suitable_for_gpu_rasterization_ = false;
   }
 
-  bool IsSolidColor() const { return is_solid_color_; }
-  SkColor GetSolidColor() const { return solid_color_; }
-
  protected:
   virtual ~PicturePile();
 
@@ -64,8 +61,6 @@ class CC_EXPORT PicturePile : public PicturePileBase {
   void DetermineIfSolidColor();
 
   bool is_suitable_for_gpu_rasterization_;
-  bool is_solid_color_;
-  SkColor solid_color_;
 
   DISALLOW_COPY_AND_ASSIGN(PicturePile);
 };
