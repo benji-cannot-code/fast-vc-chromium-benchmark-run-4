a5e5587f57784bf2f28e37ae13664cbb0679e0fd
Thiago Farina <tfarina@chromium.org>
2014-09-19 23:24:58 -0300

    gn: Fix some typos in unittests.
    SubstitutionPatterm -> SubstitutionPattern
    SubstutitionWriter -> SubstitutionWriter
    
    BUG=None
    TEST=ninja -C out/Debug gn_unittests - compare the output before and
    after this change.
    TBR=brettw@chromium.org
    
    Review URL: https://codereview.chromium.org/590683002
    
    Cr-Commit-Position: refs/heads/master@{#295846}

--- tools/gn/substitution_writer_unittest.cc
+++ tools/gn/substitution_writer_unittest.cc
@@ -60,7 +60,7 @@ TEST(SubstitutionWriter, ApplyPatternToSourceAsOutputFile) {
   ASSERT_EQ("gen/foo/bar/myfile.tmp", result.value());
 }
 
-TEST(SubstutitionWriter, WriteNinjaVariablesForSource) {
+TEST(SubstitutionWriter, WriteNinjaVariablesForSource) {
   TestWithScope setup;
 
   std::vector<SubstitutionType> types;
@@ -102,7 +102,7 @@ TEST(SubstitutionWriter, WriteWithNinjaVariables) {
       out.str());
 }
 
-TEST(SubstutitionWriter, SourceSubstitutions) {
+TEST(SubstitutionWriter, SourceSubstitutions) {
   TestWithScope setup;
 
   // Call to get substitutions relative to the build dir.
