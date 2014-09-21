b6e5ca275df4f6201c32749967f8b03c1a990da5
bengr <bengr@chromium.org>
2014-09-21 10:43:51 -0700

    Use chromium version for data reduction proxy version
    Provide a method to get the chromium version for use as the
    data reduction proxy version.
    
    BUG=410127
    
    Review URL: https://codereview.chromium.org/533003002
    
    Cr-Commit-Position: refs/heads/master@{#295906}

--- chrome/browser/profiles/profile_impl_io_data.cc
+++ chrome/browser/profiles/profile_impl_io_data.cc
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_switches.h"
-#include "chrome/common/chrome_version_info.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
 #include "components/data_reduction_proxy/browser/data_reduction_proxy_statistics_prefs.h"
@@ -444,7 +443,6 @@ void ProfileImplIOData::InitializeInternal(
   data_reduction_proxy_auth_request_handler_.reset(
       new data_reduction_proxy::DataReductionProxyAuthRequestHandler(
           DataReductionProxyChromeSettings::GetClient(),
-          chrome::VersionInfo().Version(),
           data_reduction_proxy_params_.get(),
           BrowserThread::GetMessageLoopProxyForThread(BrowserThread::IO)));
   data_reduction_proxy_usage_stats_.reset(
