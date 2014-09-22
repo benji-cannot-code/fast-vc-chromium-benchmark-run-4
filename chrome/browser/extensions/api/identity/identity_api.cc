8cc4e852dab8ed8c075706986ab098c7f2d953b0
courage <courage@chromium.org>
2014-09-22 15:35:02 -0700

    Fix crash in tracing for chrome.identity.getAuthToken
    A trace statement in the IdentityGetAuthTokenFunction constructor was
    trying to use the app ID, which is not available until later. Moving
    the statement to RunAsync fixes the crash.
    
    BUG=411396
    
    Review URL: https://codereview.chromium.org/588343002
    
    Cr-Commit-Position: refs/heads/master@{#296073}

--- chrome/browser/extensions/api/identity/identity_api.cc
+++ chrome/browser/extensions/api/identity/identity_api.cc
@@ -289,11 +289,6 @@ IdentityGetAuthTokenFunction::IdentityGetAuthTokenFunction()
     : OAuth2TokenService::Consumer("extensions_identity_api"),
       should_prompt_for_scopes_(false),
       should_prompt_for_signin_(false) {
-  TRACE_EVENT_ASYNC_BEGIN1("identity",
-                           "IdentityGetAuthTokenFunction",
-                           this,
-                           "extension",
-                           extension()->id());
 }
 
 IdentityGetAuthTokenFunction::~IdentityGetAuthTokenFunction() {
@@ -301,6 +296,12 @@ IdentityGetAuthTokenFunction::~IdentityGetAuthTokenFunction() {
 }
 
 bool IdentityGetAuthTokenFunction::RunAsync() {
+  TRACE_EVENT_ASYNC_BEGIN1("identity",
+                           "IdentityGetAuthTokenFunction",
+                           this,
+                           "extension",
+                           extension()->id());
+
   if (GetProfile()->IsOffTheRecord()) {
     error_ = identity_constants::kOffTheRecord;
     return false;
