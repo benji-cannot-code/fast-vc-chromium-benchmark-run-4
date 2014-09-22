489fc931305a0fe999063e6f8e34a27a87dc70ae
Brett Wilson <brettw@chromium.org>
2014-09-22 09:31:07 -0700

    GN: Fix dependency output file handling.
    A copy and paste error meant it used the link output file for dependency tracking. This is valid but inefficient, which was why it wasn't caught. Added a new test for this case
    
    R=scottmg@chromium.org
    
    Review URL: https://codereview.chromium.org/583363003
    
    Cr-Commit-Position: refs/heads/master@{#295985}

--- tools/gn/target.cc
+++ tools/gn/target.cc
@@ -323,7 +323,7 @@ void Target::FillOutputFiles() {
         if (!tool->depend_output().empty()) {
           dependency_output_file_ =
               SubstitutionWriter::ApplyPatternToLinkerAsOutputFile(
-                  this, tool, tool->link_output());
+                  this, tool, tool->depend_output());
         }
       }
       break;
