877fcce109e7f61dbcc86f4addcdcfb5b6ab1c4c
lushnikov@chromium.org <lushnikov@chromium.org>
2014-09-22 14:15:05 +0000

    DevTools: highlight network project files with respect to canonical mime type
    Currently, we rely on uiSourceCode extension to figure out an appropriate
    highlighter. This turns out to be wrong approach for the files originating
    from network project - they should be highlighted with respect to the
    mimeType in their HTTP headers.
    
    BUG=411863
    R=vsevik, pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/574043002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182417 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/inspector/sources/ui-source-code-highlight.php
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<html>
+<head>
+<script src="/inspector/inspector-test.js"></script>
+<script>
+
+function test()
+{
+    var uiSourceCodes = WebInspector.workspace.uiSourceCodes();
+
+    for (var i = 0; i < uiSourceCodes.length; ++i) {
+        var uiSourceCode = uiSourceCodes[i];
+        if (!/.php$/.test(uiSourceCode.originURL()))
+            continue;
+        if (uiSourceCode.project().type() !== WebInspector.projectTypes.Network)
+            continue;
+        InspectorTest.addResult("Highlight mimeType: " + uiSourceCode.highlighterType());
+        InspectorTest.completeTest();
+        return;
+    }
+
+    InspectorTest.addResult("Failed to find source code with .php extension.");
+    InspectorTest.completeTest();
+}
+
+</script>
+</head>
+
+<body onload="runTest()">
+<p>
+Tests that network-loaded UISourceCodes are highlighted according to their HTTP header mime type
+instead of their extension. crbug.com/411863
+</p>
+</body>
+</html>
