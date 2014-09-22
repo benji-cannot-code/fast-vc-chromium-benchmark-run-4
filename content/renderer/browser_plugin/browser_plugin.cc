6dda564e5500507615c68c4ee373c28fc1419298
fsamuel <fsamuel@chromium.org>
2014-09-22 13:32:57 -0700

    Browser Plugin: Minor cleanup
    BUG=none
    
    Review URL: https://codereview.chromium.org/594623002
    
    Cr-Commit-Position: refs/heads/master@{#296043}

--- content/renderer/browser_plugin/browser_plugin.cc
+++ content/renderer/browser_plugin/browser_plugin.cc
@@ -26,14 +26,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/web/WebElement.h"
 #include "third_party/WebKit/public/web/WebInputEvent.h"
 #include "third_party/WebKit/public/web/WebPluginContainer.h"
-#include "third_party/WebKit/public/web/WebPluginParams.h"
 #include "third_party/WebKit/public/web/WebView.h"
 #include "third_party/skia/include/core/SkCanvas.h"
 #include "ui/events/keycodes/keyboard_codes.h"
 
 using blink::WebCanvas;
 using blink::WebPluginContainer;
-using blink::WebPluginParams;
 using blink::WebPoint;
 using blink::WebRect;
 using blink::WebURL;
