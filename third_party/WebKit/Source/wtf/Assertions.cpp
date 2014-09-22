2c648b8ec89b62976bee55f007333cf8f58b1cf1
arurajku@cisco.com <arurajku@cisco.com>
2014-09-22 11:50:31 +0000

    Remove extern "C" from Assertions.h, it is no longer useful.
    It is only included in C++ sources.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/583323002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182410 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/Assertions.cpp
+++ third_party/WebKit/Source/wtf/Assertions.cpp
@@ -76,8 +76,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <android/log.h>
 #endif
 
-extern "C" {
-
 WTF_ATTRIBUTE_PRINTF(1, 0)
 static void vprintf_stderr_common(const char* format, va_list args)
 {
@@ -399,4 +397,3 @@ void WTFLogAlways(const char* format, ...)
     va_end(args);
 }
 
-} // extern "C"
