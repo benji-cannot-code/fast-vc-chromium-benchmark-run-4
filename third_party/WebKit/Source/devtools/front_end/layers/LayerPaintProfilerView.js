bd08f4b6ce9d831ddbeb8fea9126c9c4c691c4d9
caseq@chromium.org <caseq@chromium.org>
2014-09-22 09:04:57 +0000

    DevTools Paint Profiler: display progress banner while profiling snapshot
    - clear command log and duration bars while loading new snapshot;
    - display "Profiling..." in overview bars window while profiling the snapshot.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/583953002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182400 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/layers/LayerPaintProfilerView.js
+++ third_party/WebKit/Source/devtools/front_end/layers/LayerPaintProfilerView.js
@@ -27,6 +27,8 @@ WebInspector.LayerPaintProfilerView.prototype = {
      */
     profileLayer: function(layer)
     {
+        this._logTreeView.setCommandLog(null, []);
+        this._paintProfilerView.setSnapshotAndLog(null, []);
         layer.requestSnapshot(onSnapshotDone.bind(this));
 
         /**
--- third_party/WebKit/Source/devtools/front_end/timeline/PaintProfilerView.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/PaintProfilerView.js
@@ -39,6 +39,8 @@ WebInspector.PaintProfilerView = function(showImageCallback)
     WebInspector.HBox.call(this);
     this.element.classList.add("paint-profiler-overview", "hbox");
     this._canvasContainer = this.element.createChild("div", "paint-profiler-canvas-container");
+    this._progressBanner = this.element.createChild("div", "fill progress-banner hidden");
+    this._progressBanner.textContent = WebInspector.UIString("Profiling\u2026");
     this._pieChart = new WebInspector.PieChart(55, this._formatPieChartTime.bind(this));
     this.element.createChild("div", "paint-profiler-pie-chart").appendChild(this._pieChart.element);
 
@@ -82,6 +84,7 @@ WebInspector.PaintProfilerView.prototype = {
             this._update();
             return;
         }
+        this._progressBanner.classList.remove("hidden");
         snapshot.requestImage(null, null, 1, this._showImageCallback);
         snapshot.profile(onProfileDone.bind(this));
         /**
@@ -90,6 +93,7 @@ WebInspector.PaintProfilerView.prototype = {
          */
         function onProfileDone(profiles)
         {
+            this._progressBanner.classList.add("hidden");
             this._profiles = profiles;
             this._update();
         }
@@ -284,9 +288,11 @@ WebInspector.PaintProfilerCommandLogView.prototype = {
      */
     updateWindow: function(stepLeft, stepRight)
     {
+        this.sidebarTree.removeChildren();
+        if (!this._log)
+            return;
         stepLeft = stepLeft || 0;
         stepRight = stepRight || this._log.length - 1;
-        this.sidebarTree.removeChildren();
         for (var i = stepLeft; i <= stepRight; ++i)
             this._appendLogItem(this.sidebarTree, this._log[i]);
     },
