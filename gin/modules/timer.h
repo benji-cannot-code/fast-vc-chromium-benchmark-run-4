e2b263f6d3b6364d31e8d3fef16873cae4f022ce
kulkarni.a <kulkarni.a@samsung.com>
2014-09-22 03:44:48 -0700

    Added the "DISALLOW_COPY_AND_ASSIGN(Timer);"  for Timer class in gin/module/timer.h.
    BUG=
    
    Review URL: https://codereview.chromium.org/587713002
    
    Cr-Commit-Position: refs/heads/master@{#295948}

--- gin/modules/timer.h
+++ gin/modules/timer.h
@@ -43,6 +43,7 @@ class GIN_EXPORT Timer : public Wrappable<Timer> {
   base::WeakPtr<gin::Runner> runner_;
   base::WeakPtrFactory<Timer> weak_factory_;
 
+  DISALLOW_COPY_AND_ASSIGN(Timer);
 };
 
 
