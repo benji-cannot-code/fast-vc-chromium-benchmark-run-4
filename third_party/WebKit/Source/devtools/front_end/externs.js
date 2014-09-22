84a0d58720575d4fd71d36e5dc178dba39ba11de
lushnikov@chromium.org <lushnikov@chromium.org>
2014-09-22 15:58:13 +0000

    DevTools: [CodeMirror] do not insert pair of quotes inside string literal
    The patch implements a feature-request to not insert a pair of matching
    quotes if the cursor is inside a string token.
    
    R=apavlov, vsevik@chromium.org
    BUG=411238
    
    Review URL: https://codereview.chromium.org/590113003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182431 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/externs.js
+++ third_party/WebKit/Source/devtools/front_end/externs.js
@@ -361,7 +361,11 @@ CodeMirror.prototype = {
     removeOverlay: function(spec) { },
     /** @param {*=} origin */
     replaceRange: function(code, from, to, origin) { },
-    replaceSelection: function(code, collapse, origin) { },
+    /**
+     * @param {string} replacement
+     * @param {string=} select
+     */
+    replaceSelection: function(replacement, select) { },
     /**
      * @param {!Array.<string>} textPerSelection
      */
