7e975bf94832f2b14fb64854b0857e2e6b44b99d
mattm <mattm@chromium.org>
2014-09-22 13:30:46 -0700

    Safebrowsing: Fix comments pointing to internal URL for API docs.
    BUG=
    
    Review URL: https://codereview.chromium.org/588803002
    
    Cr-Commit-Position: refs/heads/master@{#296041}

--- chrome/browser/safe_browsing/protocol_parser.h
+++ chrome/browser/safe_browsing/protocol_parser.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_SAFE_BROWSING_PROTOCOL_PARSER_H_
 
 // Parsers and formatters for SafeBrowsing v3.0 protocol:
-// https://devsite.googleplex.com/safe-browsing/developers_guide_v3
+// https://developers.google.com/safe-browsing/developers_guide_v3
 //
 // The quoted references are with respect to that document.
 
