14b4a3defdb05b4a6d23abaef3bd1fa4969ffd08
vitalybuka <vitalybuka@chromium.org>
2014-09-22 16:42:30 -0700

    Enqueue mDns requests if send on socket in progress.
    BUG=415753
    
    Review URL: https://codereview.chromium.org/581813004
    
    Cr-Commit-Position: refs/heads/master@{#296094}

--- net/dns/mdns_client_impl.h
+++ net/dns/mdns_client_impl.h
@@ -53,7 +53,7 @@ class NET_EXPORT_PRIVATE MDnsConnection {
 
   // Both methods return true if at least one of the socket handlers succeeded.
   bool Init(MDnsSocketFactory* socket_factory);
-  bool Send(IOBuffer* buffer, unsigned size);
+  void Send(const scoped_refptr<IOBuffer>& buffer, unsigned size);
 
  private:
   class SocketHandler {
@@ -63,7 +63,7 @@ class NET_EXPORT_PRIVATE MDnsConnection {
     ~SocketHandler();
 
     int Start();
-    int Send(IOBuffer* buffer, unsigned size);
+    void Send(const scoped_refptr<IOBuffer>& buffer, unsigned size);
 
    private:
     int DoLoop(int rv);
@@ -77,6 +77,8 @@ class NET_EXPORT_PRIVATE MDnsConnection {
     IPEndPoint recv_addr_;
     DnsResponse response_;
     IPEndPoint multicast_addr_;
+    bool send_in_progress_;
+    std::queue<std::pair<scoped_refptr<IOBuffer>, unsigned> > send_queue_;
 
     DISALLOW_COPY_AND_ASSIGN(SocketHandler);
   };
@@ -86,12 +88,15 @@ class NET_EXPORT_PRIVATE MDnsConnection {
                           const IPEndPoint& recv_addr,
                           int bytes_read);
 
-  void OnError(SocketHandler* loop, int error);
+  void PostOnError(SocketHandler* loop, int rv);
+  void OnError(int rv);
 
   // Only socket handlers which successfully bound and started are kept.
   ScopedVector<SocketHandler> socket_handlers_;
 
   Delegate* delegate_;
+
+  base::WeakPtrFactory<MDnsConnection> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(MDnsConnection);
 };
