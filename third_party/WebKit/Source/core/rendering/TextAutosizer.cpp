47f93bbb3b151e4c553adcd43501531d4cb130ea
pdr@chromium.org <pdr@chromium.org>
2014-09-23 21:43:45 +0000

    [TextAutosizer] Make it clear that maxClusterWidthProvider is logically const
    This patch makes maxClusterWidthProvider const which is not obvious from
    a quick scan of the code. As broken as const is in C++, I hope this will
    help folks understand this area in the future.
    
    R=skobes
    
    Review URL: https://codereview.chromium.org/595683004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182518 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/TextAutosizer.cpp
+++ third_party/WebKit/Source/core/rendering/TextAutosizer.cpp
@@ -567,7 +567,7 @@ void TextAutosizer::setAllTextNeedsLayout()
     }
 }
 
-TextAutosizer::BlockFlags TextAutosizer::classifyBlock(const RenderObject* renderer, BlockFlags mask)
+TextAutosizer::BlockFlags TextAutosizer::classifyBlock(const RenderObject* renderer, BlockFlags mask) const
 {
     if (!renderer->isRenderBlock())
         return 0;
@@ -790,7 +790,7 @@ float TextAutosizer::superclusterMultiplier(Cluster* cluster)
     return supercluster->m_multiplier;
 }
 
-const RenderBlock* TextAutosizer::clusterWidthProvider(const RenderBlock* root)
+const RenderBlock* TextAutosizer::clusterWidthProvider(const RenderBlock* root) const
 {
     if (root->isTable() || root->isTableCell())
         return root;
@@ -798,7 +798,7 @@ const RenderBlock* TextAutosizer::clusterWidthProvider(const RenderBlock* root)
     return deepestBlockContainingAllText(root);
 }
 
-const RenderBlock* TextAutosizer::maxClusterWidthProvider(const Supercluster* supercluster, const RenderBlock* currentRoot)
+const RenderBlock* TextAutosizer::maxClusterWidthProvider(const Supercluster* supercluster, const RenderBlock* currentRoot) const
 {
     const RenderBlock* result = clusterWidthProvider(currentRoot);
     float maxWidth = widthFromBlock(result);
@@ -818,7 +818,7 @@ const RenderBlock* TextAutosizer::maxClusterWidthProvider(const Supercluster* su
     return result;
 }
 
-float TextAutosizer::widthFromBlock(const RenderBlock* block)
+float TextAutosizer::widthFromBlock(const RenderBlock* block) const
 {
     RELEASE_ASSERT(block);
     RELEASE_ASSERT(block->style());
@@ -874,7 +874,7 @@ const RenderBlock* TextAutosizer::deepestBlockContainingAllText(Cluster* cluster
 }
 
 // FIXME: Refactor this to look more like TextAutosizer::deepestCommonAncestor.
-const RenderBlock* TextAutosizer::deepestBlockContainingAllText(const RenderBlock* root)
+const RenderBlock* TextAutosizer::deepestBlockContainingAllText(const RenderBlock* root) const
 {
     size_t firstDepth = 0;
     const RenderObject* firstTextLeaf = findTextLeaf(root, firstDepth, First);
@@ -918,7 +918,7 @@ const RenderBlock* TextAutosizer::deepestBlockContainingAllText(const RenderBloc
     return containingBlock;
 }
 
-const RenderObject* TextAutosizer::findTextLeaf(const RenderObject* parent, size_t& depth, TextLeafSearch firstOrLast)
+const RenderObject* TextAutosizer::findTextLeaf(const RenderObject* parent, size_t& depth, TextLeafSearch firstOrLast) const
 {
     // List items are treated as text due to the marker.
     // The actual renderer for the marker (RenderListMarker) may not be in the tree yet since it is added during layout.
