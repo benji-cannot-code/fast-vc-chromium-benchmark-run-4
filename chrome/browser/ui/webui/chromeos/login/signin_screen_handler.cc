cb1352f2af0126616481c25eff3c0869d6f82317
dpolukhin <dpolukhin@chromium.org>
2014-09-23 05:51:13 -0700

    Fix crash on login screen for Athena Chrome
    BUG=none
    TEST=manual
    
    Review URL: https://codereview.chromium.org/597523002
    
    Cr-Commit-Position: refs/heads/master@{#296170}

--- chrome/browser/ui/webui/chromeos/login/signin_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/signin_screen_handler.cc
@@ -1327,7 +1327,10 @@ void SigninScreenHandler::HandleUpdateOfflineLogin(bool offline_login_active) {
 
 void SigninScreenHandler::HandleFocusPod(const std::string& user_id) {
   SetUserInputMethod(user_id);
+#if !defined(USE_ATHENA)
+  // TODO(dpolukhin):  crbug.com/408734.
   WallpaperManager::Get()->SetUserWallpaperDelayed(user_id);
+#endif
   ScreenlockBridge::Get()->SetFocusedUser(user_id);
 }
 
