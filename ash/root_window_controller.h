f5525b45c34ca00c6d52d8ee337a7ab0579546e4
bshe <bshe@chromium.org>
2014-09-23 14:18:38 -0700

    Remove more keyboard usability related code
    BUG=414766
    
    Review URL: https://codereview.chromium.org/587243004
    
    Cr-Commit-Position: refs/heads/master@{#296250}

--- ash/root_window_controller.h
+++ ash/root_window_controller.h
@@ -243,8 +243,7 @@ class ASH_EXPORT RootWindowController : public ShellObserver {
   explicit RootWindowController(AshWindowTreeHost* host);
   enum RootWindowType {
     PRIMARY,
-    SECONDARY,
-    VIRTUAL_KEYBOARD
+    SECONDARY
   };
 
   // Initializes the RootWindowController.  |is_primary| is true if
