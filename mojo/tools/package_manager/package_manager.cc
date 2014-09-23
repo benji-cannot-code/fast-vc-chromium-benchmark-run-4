4f6ce5f6ad8d2ee7578c9348591cbd9ddb41979c
Brett Wilson <brettw@chromium.org>
2014-09-23 16:12:32 -0700

    Add mojo package manager skeleton.
    This application downloads a URL to a temp file, unzips it, and then cleans everything up.
    
    IN a future pass, we'll extract a manifest and contents of the module.
    
    BUG=
    R=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/596913002
    
    Cr-Commit-Position: refs/heads/master@{#296294}

---
+++ mojo/tools/package_manager/package_manager.cc
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "mojo/application/application_runner_chromium.h"
+#include "mojo/public/c/system/main.h"
+#include "mojo/tools/package_manager/package_manager_application.h"
+
+MojoResult MojoMain(MojoHandle shell_handle) {
+  mojo::ApplicationRunnerChromium runner(new mojo::PackageManagerApplication);
+  return runner.Run(shell_handle);
+}
