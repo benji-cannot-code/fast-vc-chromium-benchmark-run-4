89f9b2790d76d71ec2d3327f86ef269978b98c4c
chrishtr@chromium.org <chrishtr@chromium.org>
2014-09-23 06:35:49 +0000

    Move painting code from RenderDetailsMarker into DetailsMarkerPainter.
    BUG=412088
    
    Review URL: https://codereview.chromium.org/596563003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182469 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderDetailsMarker.h
+++ third_party/WebKit/Source/core/rendering/RenderDetailsMarker.h
@@ -40,8 +40,6 @@ private:
     virtual void paint(PaintInfo&, const LayoutPoint&) OVERRIDE;
 
     bool isOpen() const;
-    Path getCanonicalPath() const;
-    Path getPath(const LayoutPoint& origin) const;
 };
 
 DEFINE_RENDER_OBJECT_TYPE_CASTS(RenderDetailsMarker, isDetailsMarker());
