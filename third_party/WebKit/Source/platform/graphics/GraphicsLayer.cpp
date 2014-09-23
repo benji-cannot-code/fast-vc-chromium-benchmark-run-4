b43ad450c967634492178e7263969c455f8e95db
vivek.vg@samsung.com <vivek.vg@samsung.com>
2014-09-23 11:02:39 +0000

    [blink] Pass SkBitmap by reference in WebImageLayer::setBitmap
    Although passing SkBitmap by value doesn't copy the underlying pixels, it indeed involves
    memcpy (~64 bytes to be precise) to replicate the settings from the other bitmap in the copy constructor.
    Along with this, it has to do some book keeping with respect to reference counts, updating pixel lock count
    etc.
    
    This overhead, albeit minor, can be avoided by passing SkBitmap as reference.
    
    R=abarth@chromium.org, eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/583733002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182486 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/GraphicsLayer.cpp
+++ third_party/WebKit/Source/platform/graphics/GraphicsLayer.cpp
@@ -931,7 +931,7 @@ void GraphicsLayer::setContentsToImage(Image* image)
             m_imageLayer = adoptPtr(Platform::current()->compositorSupport()->createImageLayer());
             registerContentsLayer(m_imageLayer->layer());
         }
-        m_imageLayer->setBitmap(nativeImage->bitmap());
+        m_imageLayer->setImageBitmap(nativeImage->bitmap());
         m_imageLayer->layer()->setOpaque(image->currentFrameKnownToBeOpaque());
         updateContentsRect();
     } else {
