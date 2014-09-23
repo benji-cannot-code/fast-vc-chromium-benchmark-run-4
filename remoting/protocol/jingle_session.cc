07d934b32348139be879eda649f2e6e0d4502122
sergeyu <sergeyu@chromium.org>
2014-09-23 13:44:53 -0700

    Fix side-effect in DCHECK that breaks another DCHECK in JingleSession.
    DCHECK(!channels_[name]) adds NULL channel with |name| into the
    map, which was breaking DCHECK in JingleSession destructor. Replaced
    with another DCHECK that doesn't have side-effects. Added unittest
    to catch this problem.
    Also removed no-op line from ~JingleSession().
    
    Review URL: https://codereview.chromium.org/594743002
    
    Cr-Commit-Position: refs/heads/master@{#296224}

--- remoting/protocol/jingle_session.cc
+++ remoting/protocol/jingle_session.cc
@@ -86,7 +86,6 @@ JingleSession::~JingleSession() {
   STLDeleteContainerPointers(transport_info_requests_.begin(),
                              transport_info_requests_.end());
 
-  channel_multiplexer_.reset();
   DCHECK(channels_.empty());
 
   session_manager_->SessionDestroyed(this);
@@ -275,7 +274,7 @@ void JingleSession::CancelChannelCreation(const std::string& name) {
   if (it != channels_.end()) {
     DCHECK(!it->second->is_connected());
     delete it->second;
-    DCHECK(!channels_[name]);
+    DCHECK(channels_.find(name) == channels_.end());
   }
 }
 
