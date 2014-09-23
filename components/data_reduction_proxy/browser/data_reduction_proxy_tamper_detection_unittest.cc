259438405d9805c4e98d2dede3c1f729314ca476
sclittle <sclittle@chromium.org>
2014-09-23 16:22:00 -0700

    Adds UMA to measure when the data reduction proxy via header is missing
    Keep track of the situations when Chrome expects the data reduction
    proxy via header to be present in a response, but the data reduction
    proxy via header is missing.
    
    BUG=412888
    
    Review URL: https://codereview.chromium.org/577343002
    
    Cr-Commit-Position: refs/heads/master@{#296297}

--- components/data_reduction_proxy/browser/data_reduction_proxy_tamper_detection_unittest.cc
+++ components/data_reduction_proxy/browser/data_reduction_proxy_tamper_detection_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
 #include "components/data_reduction_proxy/common/data_reduction_proxy_headers.h"
+#include "components/data_reduction_proxy/common/data_reduction_proxy_headers_test_utils.h"
 #include "net/http/http_response_headers.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -25,12 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace {
-
-void HeadersToRaw(std::string* headers) {
-  std::replace(headers->begin(), headers->end(), '\n', '\0');
-  if (!headers->empty())
-    *headers += '\0';
-}
 
 // Calcuates MD5 hash value for a string and then base64 encode it. Testcases
 // contain expected fingerprint in plain text, which needs to be encoded before
--- components/data_reduction_proxy/browser/data_reduction_proxy_usage_stats.h
+++ components/data_reduction_proxy/browser/data_reduction_proxy_usage_stats.h
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request.h"
 
 namespace net {
+class HttpResponseHeaders;
+class ProxyConfig;
 class ProxyServer;
 }
 
@@ -33,6 +35,13 @@ class DataReductionProxyUsageStats
       bool bypass_all,
       const net::ProxyServer& proxy_server,
       DataReductionProxyBypassType bypass_type);
+
+  // For the given response |headers| that are expected to include the data
+  // reduction proxy via header, records response code UMA if the data reduction
+  // proxy via header is not present.
+  static void DetectAndRecordMissingViaHeaderResponseCode(
+      bool is_primary,
+      const net::HttpResponseHeaders* headers);
 
   // MessageLoopProxy instance is owned by io_thread. |params| outlives
   // this class instance.
@@ -57,12 +66,10 @@ class DataReductionProxyUsageStats
   // cause the current bypass.
   void SetBypassType(DataReductionProxyBypassType type);
 
-  // Given |data_reduction_proxy_enabled|, a |request|, and the
-  // |data_reduction_proxy_config| records the number of bypassed bytes for that
-  // |request| into UMAs based on bypass type. |data_reduction_proxy_enabled|
-  // tells us the state of the kDataReductionProxyEnabled preference.
-  void RecordBypassedBytesHistograms(
-      net::URLRequest& request,
+  // Records all the data reduction proxy bytes-related histograms for the
+  // completed URLRequest |request|.
+  void RecordBytesHistograms(
+      net::URLRequest* request,
       const BooleanPrefMember& data_reduction_proxy_enabled,
       const net::ProxyConfig& data_reduction_proxy_config);
 
@@ -72,6 +79,10 @@ class DataReductionProxyUsageStats
                        int net_error);
 
  private:
+  friend class DataReductionProxyUsageStatsTest;
+  FRIEND_TEST_ALL_PREFIXES(DataReductionProxyUsageStatsTest,
+                           RecordMissingViaHeaderBytes);
+
   enum BypassedBytesType {
     NOT_BYPASSED = 0,         /* Not bypassed. */
     SSL,                      /* Bypass due to SSL. */
@@ -82,6 +93,20 @@ class DataReductionProxyUsageStats
     NETWORK_ERROR,            /* Network error. */
     BYPASSED_BYTES_TYPE_MAX   /* This must always be last.*/
   };
+
+  // Given |data_reduction_proxy_enabled|, a |request|, and the
+  // |data_reduction_proxy_config| records the number of bypassed bytes for that
+  // |request| into UMAs based on bypass type. |data_reduction_proxy_enabled|
+  // tells us the state of the kDataReductionProxyEnabled preference.
+  void RecordBypassedBytesHistograms(
+      net::URLRequest* request,
+      const BooleanPrefMember& data_reduction_proxy_enabled,
+      const net::ProxyConfig& data_reduction_proxy_config);
+
+  // Records UMA of the number of response bytes of responses that are expected
+  // to have the data reduction proxy via header, but where the data reduction
+  // proxy via header is not present.
+  void RecordMissingViaHeaderBytes(net::URLRequest* request);
 
   // NetworkChangeNotifier::NetworkChangeObserver:
   virtual void OnNetworkChanged(
