d27189137386b14744a8deed3663931331b63986
erikchen <erikchen@chromium.org>
2014-09-22 17:53:43 -0700

    mac: Only show the access Address Book prompt a fixed number of times.
    Once the Address Book prompt has been shown on 5 times (repeatedly being shown
    on the same form doesn't count), don't show it ever again.
    
    This CL also re-enables the access Address Book prompt, which was disabled due
    to bugs in the implementation.
    
    BUG=414359
    
    Review URL: https://codereview.chromium.org/578383002
    
    Cr-Commit-Position: refs/heads/master@{#296114}

--- components/autofill/core/browser/personal_data_manager_mac.mm
+++ components/autofill/core/browser/personal_data_manager_mac.mm
@@ -32,6 +32,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace autofill {
 namespace {
 
+// The maximum number of instances when the access Address Book prompt should
+// be shown.
+int kMaxTimesToShowMacAddressBook = 5;
+
 // There is an uncommon sequence of events that causes the Address Book
 // permissions dialog to appear more than once for a given install of Chrome.
 //  1. Chrome has previously presented the Address Book permissions dialog.
@@ -55,9 +59,8 @@ static bool g_binary_changed = false;
 
 const char kAddressBookOrigin[] = "OS X Address Book";
 
-// Whether Chrome has prompted the user for permission to access the user's
-// address book.
-bool HasPromptedForAccessToAddressBook(PrefService* pref_service) {
+// Whether Chrome has attempted to access the Mac Address Book.
+bool HasQueriedMacAddressBook(PrefService* pref_service) {
   return pref_service->GetBoolean(prefs::kAutofillMacAddressBookQueried);
 }
 
@@ -75,7 +78,7 @@ void RecordAccessSkipped(bool skipped) {
 }
 
 ABAddressBook* GetAddressBook(PrefService* pref_service) {
-  bool first_access = !HasPromptedForAccessToAddressBook(pref_service);
+  bool first_access = !HasQueriedMacAddressBook(pref_service);
 
   // +[ABAddressBook sharedAddressBook] throws an exception internally in
   // circumstances that aren't clear. The exceptions are only observed in crash
@@ -372,7 +375,10 @@ bool PersonalDataManager::ShouldShowAccessAddressBookSuggestion(
   if (!enabled_pref_->GetValue())
     return false;
 
-  if (HasPromptedForAccessToAddressBook(pref_service_))
+  if (HasQueriedMacAddressBook(pref_service_))
+    return false;
+
+  if (AccessAddressBookPromptCount() >= kMaxTimesToShowMacAddressBook)
     return false;
 
   switch (type.group()) {
@@ -393,6 +399,15 @@ bool PersonalDataManager::ShouldShowAccessAddressBookSuggestion(
   }
 
   return false;
+}
+
+void PersonalDataManager::ShowedAccessAddressBookPrompt() {
+  pref_service_->SetInteger(prefs::kAutofillMacAddressBookShowedCount,
+                            AccessAddressBookPromptCount() + 1);
+}
+
+int PersonalDataManager::AccessAddressBookPromptCount() {
+  return pref_service_->GetInteger(prefs::kAutofillMacAddressBookShowedCount);
 }
 
 void PersonalDataManager::BinaryChanging() {
