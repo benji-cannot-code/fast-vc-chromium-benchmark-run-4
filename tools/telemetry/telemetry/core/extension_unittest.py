efe216251637821a9cc69e076d7991c378792e84
wuhu <wuhu@google.com>
2014-09-22 17:33:55 -0700

    Removing browser.Start() and instead starts the browser at the point of creation.
    BUG=412126
    
    Committed: https://crrev.com/01c68e8765254d9971296b291ff718b4f6af3004
    Cr-Commit-Position: refs/heads/master@{#295591}
    
    Review URL: https://codereview.chromium.org/565803002
    
    Cr-Commit-Position: refs/heads/master@{#296107}

--- tools/telemetry/telemetry/core/extension_unittest.py
+++ tools/telemetry/telemetry/core/extension_unittest.py
@@ -33,7 +33,6 @@ class ExtensionTest(unittest.TestCase):
       # May not find a browser that supports extensions.
       return False
     self._browser = browser_to_create.Create()
-    self._browser.Start()
     self._extension = self._browser.extensions[load_extension]
     self._extension_id = load_extension.extension_id
     self.assertTrue(self._extension)
@@ -125,7 +124,6 @@ class MultipleExtensionTest(unittest.TestCase):
     # May not find a browser that supports extensions.
     if browser_to_create:
       self._browser = browser_to_create.Create()
-      self._browser.Start()
 
   def tearDown(self):
     if self._browser:
