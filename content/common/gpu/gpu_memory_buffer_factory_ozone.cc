3fc776e7cad1f74583f2bc19dc7af1edf0bc7ee6
reveman <reveman@chromium.org>
2014-09-23 07:32:26 -0700

    content: Add missing "_" suffix to member variable.
    This adds a missing "_" suffix to |ozone_buffer_factory| member
    variable in ozone implementation of GpuMemoryBufferFactory interface.
    
    BUG=
    TBR=alexst@chromium.org
    
    Review URL: https://codereview.chromium.org/594043004
    
    Cr-Commit-Position: refs/heads/master@{#296181}

--- content/common/gpu/gpu_memory_buffer_factory_ozone.cc
+++ content/common/gpu/gpu_memory_buffer_factory_ozone.cc
@@ -23,7 +23,7 @@ class GpuMemoryBufferFactoryImpl : public GpuMemoryBufferFactory {
       unsigned usage) OVERRIDE {
     switch (handle.type) {
       case gfx::OZONE_NATIVE_BUFFER:
-        return ozone_buffer_factory.CreateGpuMemoryBuffer(
+        return ozone_buffer_factory_.CreateGpuMemoryBuffer(
                    handle.global_id, size, internalformat, usage)
                    ? handle
                    : gfx::GpuMemoryBufferHandle();
@@ -36,7 +36,7 @@ class GpuMemoryBufferFactoryImpl : public GpuMemoryBufferFactory {
       const gfx::GpuMemoryBufferHandle& handle) OVERRIDE {
     switch (handle.type) {
       case gfx::OZONE_NATIVE_BUFFER:
-        ozone_buffer_factory.DestroyGpuMemoryBuffer(handle.global_id);
+        ozone_buffer_factory_.DestroyGpuMemoryBuffer(handle.global_id);
         break;
       default:
         NOTREACHED();
@@ -62,7 +62,7 @@ class GpuMemoryBufferFactoryImpl : public GpuMemoryBufferFactory {
         if (handle.global_id.secondary_id != client_id)
           return scoped_refptr<gfx::GLImage>();
 
-        return ozone_buffer_factory.CreateImageForGpuMemoryBuffer(
+        return ozone_buffer_factory_.CreateImageForGpuMemoryBuffer(
             handle.global_id, size, internalformat);
       default:
         NOTREACHED();
@@ -71,7 +71,7 @@ class GpuMemoryBufferFactoryImpl : public GpuMemoryBufferFactory {
   }
 
  private:
-  ui::GpuMemoryBufferFactoryOzoneNativeBuffer ozone_buffer_factory;
+  ui::GpuMemoryBufferFactoryOzoneNativeBuffer ozone_buffer_factory_;
 };
 
 }  // namespace
