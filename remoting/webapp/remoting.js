84283802c04c188ad3158cf604d86b63413e3ddd
jamiewalch <jamiewalch@chromium.org>
2014-09-23 15:29:35 -0700

    Move ownership of the tool-bar's menu buttons into remoting.Toolbar.
    This CL also reinstates the previous behaviour of MenuButton whereby a
    second click on the button hides the menu, which is made possible by a
    saner ownership model.
    
    BUG=415410
    
    Review URL: https://codereview.chromium.org/594503004
    
    Cr-Commit-Position: refs/heads/master@{#296284}

--- remoting/webapp/remoting.js
+++ remoting/webapp/remoting.js
@@ -62,6 +62,7 @@ remoting.init = function() {
     remoting.fullscreen = new remoting.FullscreenAppsV2();
     remoting.windowFrame = new remoting.WindowFrame(
         document.getElementById('title-bar'));
+    remoting.optionsMenu = remoting.windowFrame.createOptionsMenu();
   } else {
     remoting.oauth2 = new remoting.OAuth2();
     if (!remoting.oauth2.isAuthenticated()) {
@@ -69,6 +70,9 @@ remoting.init = function() {
     }
     remoting.identity = remoting.oauth2;
     remoting.fullscreen = new remoting.FullscreenAppsV1();
+    remoting.toolbar = new remoting.Toolbar(
+        document.getElementById('session-toolbar'));
+    remoting.optionsMenu = remoting.toolbar.createOptionsMenu();
   }
   remoting.stats = new remoting.ConnectionStats(
       document.getElementById('statistics'));
@@ -79,8 +83,6 @@ remoting.init = function() {
       document.getElementById('host-list-error-message'),
       document.getElementById('host-list-refresh-failed-button'),
       document.getElementById('host-list-loading-indicator'));
-  remoting.toolbar = new remoting.Toolbar(
-      document.getElementById('session-toolbar'));
   remoting.clipboard = new remoting.Clipboard();
   var sandbox = /** @type {HTMLIFrameElement} */
       document.getElementById('wcs-sandbox');
