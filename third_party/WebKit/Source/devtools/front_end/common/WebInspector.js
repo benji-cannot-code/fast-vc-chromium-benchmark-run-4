e1c36254264ae5b0ecd6c14b3981efde84a9b533
apavlov@chromium.org <apavlov@chromium.org>
2014-09-23 12:42:14 +0000

    DevTools: Move query param retrieval onto Runtime
    R=aandrey, lushnikov, sergeyv
    BUG=391566
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/593043002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182491 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/common/WebInspector.js
+++ third_party/WebKit/Source/devtools/front_end/common/WebInspector.js
@@ -5,39 +5,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * found in the LICENSE file.
  */
 
-self.WebInspector = {
-    _queryParamsObject: {}
-}
-
-/**
- * @param {string} name
- * @return {?string}
- */
-WebInspector.queryParam = function(name)
-{
-    return WebInspector._queryParamsObject.hasOwnProperty(name) ? WebInspector._queryParamsObject[name] : null;
-}
-
-{(function parseQueryParameters()
-{
-    var queryParams = location.search;
-    if (!queryParams)
-        return;
-    var params = queryParams.substring(1).split("&");
-    for (var i = 0; i < params.length; ++i) {
-        var pair = params[i].split("=");
-        WebInspector._queryParamsObject[pair[0]] = pair[1];
-    }
-
-    // Patch settings from the URL param (for tests).
-    var settingsParam = WebInspector.queryParam("settings");
-    if (settingsParam) {
-        try {
-            var settings = JSON.parse(window.decodeURI(settingsParam));
-            for (var key in settings)
-                window.localStorage[key] = settings[key];
-        } catch(e) {
-            // Ignore malformed settings.
-        }
-    }
-})();}
+self.WebInspector = {}
