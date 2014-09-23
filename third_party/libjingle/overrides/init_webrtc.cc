8bf9bbd6682200bf3a3fabcdd3f2b607b80310c7
xians <xians@chromium.org>
2014-09-23 08:32:23 -0700

    Revert of Fix the way how we create webrtc::AudioProcessing in Chrome (patchset #5 id:100001 of https://codereview.chromium.org/588523002/)
    Reason for revert:
    It broke some internal webrtc bots, revert it for now and will reland it after fixing the problems.
    
    http://chromegw.corp.google.com/i/internal.chromium.webrtc/builders/Mac%20Tester/builds/22092
    
    Original issue's description:
    > Fix the way how we create webrtc::AudioProcessing in Chrome.
    >
    > BUG=415935
    > TEST=all webrtc tests in all bots + manual test to verify the agc loggings exist.
    >
    > Committed: https://crrev.com/a5e9fc62b7bf25931ffe6153cc738098d8119c28
    > Cr-Commit-Position: refs/heads/master@{#295990}
    
    TBR=tommi@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=415935
    
    Review URL: https://codereview.chromium.org/594883002
    
    Cr-Commit-Position: refs/heads/master@{#296191}

--- third_party/libjingle/overrides/init_webrtc.cc
+++ third_party/libjingle/overrides/init_webrtc.cc
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/field_trial.h"
 #include "base/native_library.h"
 #include "base/path_service.h"
-#include "third_party/webrtc/common.h"
-#include "third_party/webrtc/modules/audio_processing/include/audio_processing.h"
 #include "webrtc/base/basictypes.h"
 #include "webrtc/base/logging.h"
 
@@ -56,13 +54,6 @@ bool InitializeWebRtcModule() {
   return true;
 }
 
-webrtc::AudioProcessing* CreateWebRtcAudioProcessing(
-    const webrtc::Config& config) {
-  // libpeerconnection is being compiled as a static lib, use
-  // webrtc::AudioProcessing directly.
-  return webrtc::AudioProcessing::Create(config);
-}
-
 #else  // !LIBPEERCONNECTION_LIB
 
 // When being compiled as a shared library, we need to bridge the gap between
@@ -72,7 +63,6 @@ webrtc::AudioProcessing* CreateWebRtcAudioProcessing(
 // Global function pointers to the factory functions in the shared library.
 CreateWebRtcMediaEngineFunction g_create_webrtc_media_engine = NULL;
 DestroyWebRtcMediaEngineFunction g_destroy_webrtc_media_engine = NULL;
-CreateWebRtcAudioProcessingFunction g_create_webrtc_audio_processing = NULL;
 
 // Returns the full or relative path to the libpeerconnection module depending
 // on what platform we're on.
@@ -146,8 +136,7 @@ bool InitializeWebRtcModule() {
                                    &AddTraceEvent,
                                    &g_create_webrtc_media_engine,
                                    &g_destroy_webrtc_media_engine,
-                                   &init_diagnostic_logging,
-                                   &g_create_webrtc_audio_processing);
+                                   &init_diagnostic_logging);
 
   if (init_ok)
     rtc::SetExtraLoggingInit(init_diagnostic_logging);
@@ -170,14 +159,6 @@ cricket::MediaEngineInterface* CreateWebRtcMediaEngine(
 
 void DestroyWebRtcMediaEngine(cricket::MediaEngineInterface* media_engine) {
   g_destroy_webrtc_media_engine(media_engine);
-}
-
-webrtc::AudioProcessing* CreateWebRtcAudioProcessing(
-    const webrtc::Config& config) {
-  // The same as CreateWebRtcMediaEngine(), we call InitializeWebRtcModule here
-  // for convenience of tests.
-  InitializeWebRtcModule();
-  return g_create_webrtc_audio_processing(config);
 }
 
 #endif  // LIBPEERCONNECTION_LIB
--- third_party/libjingle/overrides/init_webrtc.h
+++ third_party/libjingle/overrides/init_webrtc.h
@@ -24,8 +24,6 @@ class WebRtcVideoEncoderFactory;
 
 namespace webrtc {
 class AudioDeviceModule;
-class AudioProcessing;
-class Config;
 }  // namespace webrtc
 
 typedef std::string (*FieldTrialFindFullName)(const std::string& trial_name);
@@ -41,9 +39,6 @@ typedef void (*DestroyWebRtcMediaEngineFunction)(
 
 typedef void (*InitDiagnosticLoggingDelegateFunctionFunction)(
     void (*DelegateFunction)(const std::string&));
-
-typedef webrtc::AudioProcessing* (*CreateWebRtcAudioProcessingFunction)(
-    const webrtc::Config& config);
 
 // A typedef for the main initialize function in libpeerconnection.
 // This will initialize logging in the module with the proper arguments
@@ -62,8 +57,7 @@ typedef bool (*InitializeModuleFunction)(
     webrtc::AddTraceEventPtr trace_add_trace_event,
     CreateWebRtcMediaEngineFunction* create_media_engine,
     DestroyWebRtcMediaEngineFunction* destroy_media_engine,
-    InitDiagnosticLoggingDelegateFunctionFunction* init_diagnostic_logging,
-    CreateWebRtcAudioProcessingFunction* create_audio_processing);
+    InitDiagnosticLoggingDelegateFunctionFunction* init_diagnostic_logging);
 
 #if !defined(LIBPEERCONNECTION_IMPLEMENTATION)
 // Load and initialize the shared WebRTC module (libpeerconnection).
@@ -72,11 +66,6 @@ typedef bool (*InitializeModuleFunction)(
 // If not called explicitly, this function will still be called from the main
 // CreateWebRtcMediaEngine factory function the first time it is called.
 bool InitializeWebRtcModule();
-
-// Return a webrtc::AudioProcessing object.
-webrtc::AudioProcessing* CreateWebRtcAudioProcessing(
-    const webrtc::Config& config);
-
 #endif
 
 #endif // THIRD_PARTY_LIBJINGLE_OVERRIDES_INIT_WEBRTC_H_
