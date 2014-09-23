0dbbc03ddb0e8246b7b7253d4731aa238a72b5f8
vivek.vg@samsung.com <vivek.vg@samsung.com>
2014-09-23 18:10:15 +0000

    Remove inclusion of WebImageLayer.h from ImageLayerChromiumTest.cpp
    This is unused header in the given test.
    
    Review URL: https://codereview.chromium.org/592383003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182513 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/ImageLayerChromiumTest.cpp
+++ third_party/WebKit/Source/platform/graphics/ImageLayerChromiumTest.cpp
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "platform/graphics/GraphicsLayer.h"
 #include "platform/graphics/skia/NativeImageSkia.h"
-#include "public/platform/WebImageLayer.h"
 #include "wtf/PassOwnPtr.h"
 
 #include <gtest/gtest.h>
