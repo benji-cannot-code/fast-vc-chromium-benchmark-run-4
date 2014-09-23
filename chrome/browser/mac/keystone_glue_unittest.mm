9db116aa079c12bfd331679438c1aac497c9ca06
Mark Mentovai <mark@chromium.org>
2014-09-22 22:27:34 -0400

    Update KeystoneRegistration.framework to 1.2.0.7671.
    The big change in this version is that an installation attempt initiated
    by the application can show up as ondemand on the server. This will
    allow us to provide throttled Mac downloads (such as a "5% push") with
    correct behavior.
    
    A previous registration framework, 1.2.0.6782, intended to fix this bug
    was backed out (via DEPS) because it was not compatible with older
    installed Keystones. This version contains a fix for that problem.
    
    Another previous registration framework, 1.2.0.7407, was backed out
    because it contained bugs in its installer.
    
    Previous commit: r245361, https://codereview.chromium.org/141193002
    Backed out: r245603, https://codereview.chromium.org/141583005
    Tried again: r261532, https://codereview.chromium.org/212703006
    Backed out: r262037, https://codereview.chromium.org/227173002
    
    BUG=35495
    R=oemilyo@chromium.org
    
    Review URL: https://codereview.chromium.org/577353006
    
    Cr-Commit-Position: refs/heads/master@{#296130}

--- chrome/browser/mac/keystone_glue_unittest.mm
+++ chrome/browser/mac/keystone_glue_unittest.mm
@@ -36,10 +36,11 @@ namespace ksr = keystone_registration;
   return NO;
 }
 
-- (void)setActive {
+- (BOOL)setActive {
+  return NO;
 }
 
-- (void)checkForUpdate {
+- (void)checkForUpdateWasUserInitiated:(BOOL)userInitiated {
 }
 
 - (void)startUpdate {
@@ -56,7 +57,7 @@ namespace ksr = keystone_registration;
 
 // Send the notifications that a real KeystoneGlue object would send.
 
-- (void)checkForUpdate {
+- (void)checkForUpdateWasUserInitiated:(BOOL)userInitiated {
   NSNumber* yesNumber = [NSNumber numberWithBool:YES];
   NSString* statusKey = @"Status";
   NSDictionary* dictionary = [NSDictionary dictionaryWithObject:yesNumber
