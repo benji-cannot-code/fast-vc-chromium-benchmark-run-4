3b10408f0461e2b509b6e8eed49ff093d28acba5
Scott Violet <sky@chromium.org>
2014-09-23 14:24:43 -0700

    Fix bad scaling in TiledLayer
    On the windows side (and likely chromeos for some devices) we can end
    up in a situation where the pixel bounds is not an even multiple of the
    scale factor. This means when TiledLayer calculates the scale it ends
    up off.
    
    BUG=416655,416775
    TEST=added test coverage
    R=danakj@chromium.org
    
    Review URL: https://codereview.chromium.org/567743003
    
    Cr-Commit-Position: refs/heads/master@{#296252}

--- cc/test/fake_content_layer.cc
+++ cc/test/fake_content_layer.cc
@@ -13,7 +13,7 @@ namespace cc {
 
 class FakeContentLayerUpdater : public ContentLayerUpdater {
  public:
-  using ContentLayerUpdater::content_rect;
+  using ContentLayerUpdater::paint_rect;
 
  private:
   virtual ~FakeContentLayerUpdater() {}
@@ -44,8 +44,7 @@ bool FakeContentLayer::Update(ResourceUpdateQueue* queue,
 }
 
 gfx::Rect FakeContentLayer::LastPaintRect() const {
-  return (static_cast<FakeContentLayerUpdater*>
-          (Updater()))->content_rect();
+  return (static_cast<FakeContentLayerUpdater*>(Updater()))->paint_rect();
 }
 
 void FakeContentLayer::PushPropertiesTo(LayerImpl* layer) {
