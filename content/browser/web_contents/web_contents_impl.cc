55de4139dd8638154cbc3d1f3fb431ee9e8660fd
avi <avi@chromium.org>
2014-09-24 09:42:51 -0700

    Remove WebContentsObserver::ProvisionalChangeToMainFrameUrl and all the code supporting it.
    BUG=78512, 407376
    TEST=no change
    
    Review URL: https://codereview.chromium.org/596543002
    
    Cr-Commit-Position: refs/heads/master@{#296439}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -2495,14 +2495,6 @@ void WebContentsImpl::DidStartProvisionalLoad(
       observers_,
       DidStartProvisionalLoadForFrame(
           render_frame_host, validated_url, is_error_page, is_iframe_srcdoc));
-
-  if (!render_frame_host->GetParent()) {
-    FOR_EACH_OBSERVER(
-        WebContentsObserver,
-        observers_,
-        ProvisionalChangeToMainFrameUrl(validated_url,
-                                        render_frame_host));
-  }
 }
 
 void WebContentsImpl::DidStartNavigationTransition(
@@ -2584,17 +2576,6 @@ bool WebContentsImpl::ShouldPreserveAbortedURLs() {
   if (!delegate_)
     return false;
   return delegate_->ShouldPreserveAbortedURLs(this);
-}
-
-void WebContentsImpl::DidRedirectProvisionalLoad(
-    RenderFrameHostImpl* render_frame_host,
-    const GURL& validated_target_url) {
-  // Notify observers about the provisional change in the main frame URL.
-  FOR_EACH_OBSERVER(
-      WebContentsObserver,
-      observers_,
-      ProvisionalChangeToMainFrameUrl(validated_target_url,
-                                      render_frame_host));
 }
 
 void WebContentsImpl::DidCommitProvisionalLoad(
