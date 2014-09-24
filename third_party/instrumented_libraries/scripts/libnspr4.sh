884af62f48d95af6ed3dd7d49a02fb610c1644b2
earthdok <earthdok@chromium.org>
2014-09-24 11:11:59 -0700

    Re-land r296395: "Instrumented libraries: update the libnspr4 build script."
    There has been a security update in gPrecise which changed the paths somewhat.
    The previous version of this CL was reverted as it broke on vanilla Precise.
    This version fixes that.
    
    BUG=313751
    TBR=glider@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/602823002
    
    Cr-Commit-Position: refs/heads/master@{#296466}

--- third_party/instrumented_libraries/scripts/libnspr4.sh
+++ third_party/instrumented_libraries/scripts/libnspr4.sh
@@ -6,4 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 # This script does some preparations before build of instrumented libnspr4.
 
-mv mozilla/nsprpub/* .
+if [ -d nspr ]
+then
+  mv nspr/* .
+elif [ -d mozilla/nsprpub ]
+then
+  mv mozilla/nsprpub/* .
+else
+  echo "libnspr4.sh: package has unexpected directory structure. Please update this script."
+  return 1
+fi
