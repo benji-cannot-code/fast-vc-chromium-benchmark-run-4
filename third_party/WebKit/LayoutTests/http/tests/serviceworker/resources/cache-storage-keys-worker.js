563f33c44bb928493c4dab0e8adfcb024f099c84
asanka@chromium.org <asanka@chromium.org>
2014-09-24 21:27:28 +0000

    [ServiceWorker] CacheStorage tests.
    These tests exercise the following methods of CacheStorage:
      * get()
      * has()
      * create()
      * delete()
      * keys()
    
    The editors draft is at:
    https://slightlyoff.github.io/ServiceWorker/spec/service_worker/#cache-storage
    
    BUG=374802
    
    Review URL: https://codereview.chromium.org/430993002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182633 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/serviceworker/resources/cache-storage-keys-worker.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+importScripts('worker-test-harness.js');
+
+var test_cache_list =
+  ['', 'example', 'Another cache name', 'A', 'a', 'ex ample'];
+
+promise_test(function(test) {
+    return self.caches.keys()
+      .then(function(keys) {
+          assert_true(Array.isArray(keys),
+                      'CacheStorage.keys should return an Array.');
+          return Promise.all(keys.map(function(key) {
+              return self.caches.delete(key);
+            }));
+        })
+      .then(function() {
+          return Promise.all(test_cache_list.map(function(key) {
+              return self.caches.create(key);
+            }));
+        })
+
+      .then(function() { return self.caches.keys(); })
+      .then(function(keys) {
+          assert_true(Array.isArray(keys),
+                      'CacheStorage.keys should return an Array.');
+          assert_array_equals(keys,
+                              test_cache_list,
+                              'CacheStorage.keys should only return ' +
+                              'existing caches.');
+        });
+  }, 'CacheStorage keys');
--- third_party/WebKit/LayoutTests/http/tests/serviceworker/resources/test-helpers.js
+++ third_party/WebKit/LayoutTests/http/tests/serviceworker/resources/test-helpers.js
@@ -200,7 +200,7 @@ function wait_for_activated(test, registration) {
   };
 
   function service_worker_test(url, description) {
-    var scope = window.location.origin + '/service-worker-scope/' +
+    var scope = window.location.origin + '/service-worker-scope' +
       window.location.pathname;
 
     var test = async_test(description);
