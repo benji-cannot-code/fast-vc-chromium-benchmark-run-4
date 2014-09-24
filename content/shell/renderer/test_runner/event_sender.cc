e879c4e13a661cfe6e69262e0415bfd149f0970e
mustaq <mustaq@chromium.org>
2014-09-24 13:21:43 -0700

    Added checks for missing arguments in event_sender.cc. Also simplified the uses of x & y in EventSender::GestureEvent.
    BUG=415970
    
    Review URL: https://codereview.chromium.org/584203003
    
    Cr-Commit-Position: refs/heads/master@{#296505}

--- content/shell/renderer/test_runner/event_sender.cc
+++ content/shell/renderer/test_runner/event_sender.cc
@@ -1641,8 +1641,10 @@ void EventSender::BeginDragWithFiles(const std::vector<std::string>& files) {
 void EventSender::AddTouchPoint(gin::Arguments* args) {
   double x;
   double y;
-  args->GetNext(&x);
-  args->GetNext(&y);
+  if (!args->GetNext(&x) || !args->GetNext(&y)) {
+    args->ThrowError();
+    return;
+  }
 
   WebTouchPoint touch_point;
   touch_point.state = WebTouchPoint::StatePressed;
@@ -1762,8 +1764,10 @@ void EventSender::MouseMoveTo(gin::Arguments* args) {
 
   double x;
   double y;
-  args->GetNext(&x);
-  args->GetNext(&y);
+  if (!args->GetNext(&x) || !args->GetNext(&y)) {
+    args->ThrowError();
+    return;
+  }
   WebPoint mouse_pos(static_cast<int>(x), static_cast<int>(y));
 
   int modifiers = 0;
@@ -1925,9 +1929,10 @@ void EventSender::GestureEvent(WebInputEvent::Type type,
                                gin::Arguments* args) {
   double x;
   double y;
-  args->GetNext(&x);
-  args->GetNext(&y);
-  WebPoint point(x, y);
+  if (!args->GetNext(&x) || !args->GetNext(&y)) {
+    args->ThrowError();
+    return;
+  }
 
   WebGestureEvent event;
   event.type = type;
@@ -1945,7 +1950,7 @@ void EventSender::GestureEvent(WebInputEvent::Type type,
           current_gesture_location_.y + event.data.scrollUpdate.deltaY;
       break;
     case WebInputEvent::GestureScrollBegin:
-      current_gesture_location_ = WebPoint(point.x, point.y);
+      current_gesture_location_ = WebPoint(x, y);
       event.x = current_gesture_location_.x;
       event.y = current_gesture_location_.y;
       break;
@@ -1980,8 +1985,8 @@ void EventSender::GestureEvent(WebInputEvent::Type type,
       event.data.tap.tapCount = tap_count;
       event.data.tap.width = width;
       event.data.tap.height = height;
-      event.x = point.x;
-      event.y = point.y;
+      event.x = x;
+      event.y = y;
       break;
     }
     case WebInputEvent::GestureTapUnconfirmed:
@@ -1995,8 +2000,8 @@ void EventSender::GestureEvent(WebInputEvent::Type type,
       } else {
         event.data.tap.tapCount = 1;
       }
-      event.x = point.x;
-      event.y = point.y;
+      event.x = x;
+      event.y = y;
       break;
     case WebInputEvent::GestureTapDown:
     {
@@ -2014,8 +2019,8 @@ void EventSender::GestureEvent(WebInputEvent::Type type,
           return;
         }
       }
-      event.x = point.x;
-      event.y = point.y;
+      event.x = x;
+      event.y = y;
       event.data.tapDown.width = width;
       event.data.tapDown.height = height;
       break;
@@ -2036,19 +2041,19 @@ void EventSender::GestureEvent(WebInputEvent::Type type,
           }
         }
       }
-      event.x = point.x;
-      event.y = point.y;
+      event.x = x;
+      event.y = y;
       event.data.showPress.width = width;
       event.data.showPress.height = height;
       break;
     }
     case WebInputEvent::GestureTapCancel:
-      event.x = point.x;
-      event.y = point.y;
+      event.x = x;
+      event.y = y;
       break;
     case WebInputEvent::GestureLongPress:
-      event.x = point.x;
-      event.y = point.y;
+      event.x = x;
+      event.y = y;
       if (!args->PeekNext().IsEmpty()) {
         float width;
         if (!args->GetNext(&width)) {
@@ -2067,8 +2072,8 @@ void EventSender::GestureEvent(WebInputEvent::Type type,
       }
       break;
     case WebInputEvent::GestureLongTap:
-      event.x = point.x;
-      event.y = point.y;
+      event.x = x;
+      event.y = y;
       if (!args->PeekNext().IsEmpty()) {
         float width;
         if (!args->GetNext(&width)) {
@@ -2087,8 +2092,8 @@ void EventSender::GestureEvent(WebInputEvent::Type type,
       }
       break;
     case WebInputEvent::GestureTwoFingerTap:
-      event.x = point.x;
-      event.y = point.y;
+      event.x = x;
+      event.y = y;
       if (!args->PeekNext().IsEmpty()) {
         float first_finger_width;
         if (!args->GetNext(&first_finger_width)) {
@@ -2124,7 +2129,7 @@ void EventSender::GestureEvent(WebInputEvent::Type type,
     WebMouseEvent mouse_event;
     InitMouseEvent(WebInputEvent::MouseDown,
                    pressed_button_,
-                   point,
+                   WebPoint(x, y),
                    GetCurrentEventTimeSec(),
                    click_count_,
                    0,
