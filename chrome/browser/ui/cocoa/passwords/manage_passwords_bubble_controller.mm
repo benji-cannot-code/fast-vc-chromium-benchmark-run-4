ed22a7e9a3ad3de48db232a0c0f45ad641a801ea
dconnelly <dconnelly@chromium.org>
2014-09-24 05:53:01 -0700

    Fix Mac password bubble crash due to the "nope" menu hanging around.
    By default open menus in Cocoa exist until the user makes a choice, but
    since the bubble and its backing controller are owned and managed by
    cross-platform code, this model doesn't make sense. Hiding the menu
    programmatically matches the Views implementation and fixes the crash.
    
    Since triggering the NSPopUpButton's menu with |performClick:| spins
    a run loop waiting for user input, getting a test working was extremely
    difficult. The solution is to refactor BubbleCombobox to subclass
    MenuButton instead of NSPopUpButton; http://crbug.com/415088 created
    to track this work, which will then support a test for this bug.
    
    BUG=413267
    
    Review URL: https://codereview.chromium.org/563093003
    
    Cr-Commit-Position: refs/heads/master@{#296398}

--- chrome/browser/ui/cocoa/passwords/manage_passwords_bubble_controller.mm
+++ chrome/browser/ui/cocoa/passwords/manage_passwords_bubble_controller.mm
@@ -43,6 +43,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [super showWindow:sender];
 }
 
+- (void)close {
+  [currentController_ bubbleWillDisappear];
+  [super close];
+}
+
 - (void)updateState {
   // Find the next view controller.
   currentController_.reset();
