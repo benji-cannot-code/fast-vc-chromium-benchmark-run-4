a442d2668b2d8d7bbc62e30675afe3b6351ba2bd
pfeldman@chromium.org <pfeldman@chromium.org>
2014-09-24 17:41:25 +0000

    DevTools: properly inject elements panel context menu items.
    BUG=417275
    
    Review URL: https://codereview.chromium.org/603643002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182620 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/elements/ElementsPanel.js
+++ third_party/WebKit/Source/devtools/front_end/elements/ElementsPanel.js
@@ -58,7 +58,6 @@ WebInspector.ElementsPanel = function()
         this.contentElement.classList.add("nowrap");
     WebInspector.settings.domWordWrap.addChangeListener(this._domWordWrapSettingChanged.bind(this));
 
-    this.contentElement.addEventListener("contextmenu", this._contextMenuEventFired.bind(this), true);
     this._splitView.sidebarElement().addEventListener("contextmenu", this._sidebarContextMenuEventFired.bind(this), false);
 
     var crumbsContainer = stackElement.createChild("div");
@@ -131,7 +130,7 @@ WebInspector.ElementsPanel.prototype = {
      */
     targetAdded: function(target)
     {
-        var treeOutline = new WebInspector.ElementsTreeOutline(target, true, true, this._populateContextMenu.bind(this), this._setPseudoClassForNode.bind(this));
+        var treeOutline = new WebInspector.ElementsTreeOutline(target, true, true, this._setPseudoClassForNode.bind(this));
         treeOutline.wireToDOMModel();
         treeOutline.addEventListener(WebInspector.ElementsTreeOutline.Events.SelectedNodeChanged, this._selectedNodeChanged, this);
         treeOutline.addEventListener(WebInspector.ElementsTreeOutline.Events.NodePicked, this._onNodePicked, this);
@@ -452,16 +451,6 @@ WebInspector.ElementsPanel.prototype = {
         }
     },
 
-    _contextMenuEventFired: function(event)
-    {
-        if (this.sidebarPanes.styles.isEditingSelector())
-            return;
-        var contextMenu = new WebInspector.ContextMenu(event);
-        for (var i = 0; i < this._treeOutlines.length; ++i)
-            this._treeOutlines[i].populateContextMenu(contextMenu, event);
-        contextMenu.show();
-    },
-
     _domWordWrapSettingChanged: function(event)
     {
         if (event.data)
@@ -484,14 +473,6 @@ WebInspector.ElementsPanel.prototype = {
         this._searchableView.cancelSearch();
         WebInspector.inspectorView.setCurrentPanel(this);
         this.selectDOMNode(node, true);
-    },
-
-    _populateContextMenu: function(contextMenu, node)
-    {
-        // Add debbuging-related actions
-        contextMenu.appendSeparator();
-        var pane = WebInspector.domBreakpointsSidebarPane;
-        pane.populateNodeContextMenu(node, contextMenu);
     },
 
     /**
@@ -1293,10 +1274,18 @@ WebInspector.ElementsPanel.prototype = {
             && !(object instanceof WebInspector.DeferredDOMNode)) {
             return;
         }
+
+        // Add debbuging-related actions
+        if (object instanceof WebInspector.DOMNode) {
+            contextMenu.appendSeparator();
+            WebInspector.domBreakpointsSidebarPane.populateNodeContextMenu(object, contextMenu);
+        }
+
         // Skip adding "Reveal..." menu item for our own tree outline.
         if (this.element.isAncestor(/** @type {!Node} */ (event.target)))
             return;
         var commandCallback = WebInspector.Revealer.reveal.bind(WebInspector.Revealer, object);
+
         contextMenu.appendItem(WebInspector.useLowerCaseMenuTitles() ? "Reveal in Elements panel" : "Reveal in Elements Panel", commandCallback);
     },
 
