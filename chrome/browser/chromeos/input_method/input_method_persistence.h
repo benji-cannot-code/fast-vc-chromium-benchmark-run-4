810bc4dd01e8f4b5dc410cfb177346b56cf0ac53
alemate <alemate@chromium.org>
2014-09-24 07:31:34 -0700

    ChromeOS: "Add New User" screen should enable all hardware keyboards.
    "Add New User" screen should have enabled all hardware
    keyboards, owner and system input methods.
    
    BUG=365299
    TEST=manual
    
    Committed: https://crrev.com/a5a81ef618a26e513a4ae0f2b4b0e77f8224e946
    Cr-Commit-Position: refs/heads/master@{#296199}
    
    Review URL: https://codereview.chromium.org/484353005
    
    Cr-Commit-Position: refs/heads/master@{#296408}

--- chrome/browser/chromeos/input_method/input_method_persistence.h
+++ chrome/browser/chromeos/input_method/input_method_persistence.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "chromeos/ime/input_method_manager.h"
 
+class PrefService;
+
 namespace chromeos {
 namespace input_method {
 
@@ -39,6 +41,10 @@ class InputMethodPersistence : public InputMethodManager::Observer {
   InputMethodManager::UISessionState ui_session_;
   DISALLOW_COPY_AND_ASSIGN(InputMethodPersistence);
 };
+
+void SetUserLRUInputMethodPreferenceForTesting(const std::string& username,
+                                               const std::string& input_method,
+                                               PrefService* local_state);
 
 }  // namespace input_method
 }  // namespace chromeos
