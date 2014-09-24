453f2cf838fb25b33ac6fa9a74d765fdaa5c173f
hclam <hclam@chromium.org>
2014-09-23 17:05:46 -0700

    Cast: Reduce the size of stats
    Stats generated by cast streaming API contains some redundancy stuff
    like "bucket" and "count".
    
    Also remove 0 buckets in the stats to reduce size.
    
    Review URL: https://codereview.chromium.org/582253002
    
    Cr-Commit-Position: refs/heads/master@{#296309}

--- media/cast/logging/stats_event_subscriber.cc
+++ media/cast/logging/stats_event_subscriber.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <cmath>
+
 #include "media/cast/logging/stats_event_subscriber.h"
 
 #include "base/format_macros.h"
@@ -66,27 +68,30 @@ StatsEventSubscriber::SimpleHistogram::GetHistogram() const {
 
   scoped_ptr<base::DictionaryValue> bucket(new base::DictionaryValue);
 
-  bucket->SetString("bucket", base::StringPrintf("< %" PRId64, min_));
-  bucket->SetInteger("count", buckets_.front());
-  histo->Append(bucket.release());
-
-  for (size_t i = 1; i < buckets_.size() - 1; i++) {
-    bucket.reset(new base::DictionaryValue);
-
-    int64 lower = min_ + (i - 1) * width_;
-    int64 upper = lower + width_ - 1;
-    bucket->SetString(
-        "bucket", base::StringPrintf("%" PRId64 " - %" PRId64, lower, upper));
-    bucket->SetInteger("count", buckets_[i]);
+  if (buckets_.front()) {
+    bucket->SetInteger(base::StringPrintf("<%" PRId64, min_),
+                       buckets_.front());
     histo->Append(bucket.release());
   }
 
-  bucket.reset(new base::DictionaryValue);
+  for (size_t i = 1; i < buckets_.size() - 1; i++) {
+    if (!buckets_[i])
+      continue;
+    bucket.reset(new base::DictionaryValue);
+    int64 lower = min_ + (i - 1) * width_;
+    int64 upper = lower + width_ - 1;
+    bucket->SetInteger(
+        base::StringPrintf("%" PRId64 "-%" PRId64, lower, upper),
+        buckets_[i]);
+    histo->Append(bucket.release());
+  }
 
-  bucket->SetString("bucket", base::StringPrintf(">= %" PRId64, max_));
-  bucket->SetInteger("count", buckets_.back());
-  histo->Append(bucket.release());
-
+  if (buckets_.back()) {
+    bucket.reset(new base::DictionaryValue);
+    bucket->SetInteger(base::StringPrintf(">=%" PRId64, max_),
+                       buckets_.back());
+    histo->Append(bucket.release());
+  }
   return histo.Pass();
 }
 
@@ -218,7 +223,9 @@ scoped_ptr<base::DictionaryValue> StatsEventSubscriber::GetStats() const {
   scoped_ptr<base::DictionaryValue> stats(new base::DictionaryValue);
   for (StatsMap::const_iterator it = stats_map.begin(); it != stats_map.end();
        ++it) {
-    stats->SetDouble(CastStatToString(it->first), it->second);
+    // Round to 3 digits after the decimal point.
+    stats->SetDouble(CastStatToString(it->first),
+                     round(it->second * 1000.0) / 1000.0);
   }
   for (HistogramMap::const_iterator it = histograms_.begin();
        it != histograms_.end();
