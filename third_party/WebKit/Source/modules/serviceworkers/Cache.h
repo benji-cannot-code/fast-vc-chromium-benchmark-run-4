7f7c70e0e455f0a24969b3ce8217070f16e38bc8
gavinp@chromium.org <gavinp@chromium.org>
2014-09-24 15:01:25 +0000

    Introducing the WebServiceWorkerCache.
    This CL provides the new WebServiceWorkerCache object, which connects the
    ServiceWorker Cache between blink and content. There's two notable exceptions
    in the implementation; the various Cache.add() and Cache.addAll() methods are
    not implemented (they should do fetches in blink and provide the responses to
    this API through the batch call), and Response bodies via blobs is not implemented.
    
    R=dominicc@chromium.org,falken@chromium.org,jkarlin@chromium.org
    BUG=399178
    
    Review URL: https://codereview.chromium.org/433793002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182604 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/serviceworkers/Cache.h
+++ third_party/WebKit/Source/modules/serviceworkers/Cache.h
@@ -9,12 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/ScriptPromise.h"
 #include "bindings/core/v8/ScriptWrappable.h"
 #include "core/dom/DOMException.h"
+#include "public/platform/WebServiceWorkerCache.h"
 #include "public/platform/WebServiceWorkerCacheError.h"
 #include "wtf/Forward.h"
 #include "wtf/Noncopyable.h"
+#include "wtf/OwnPtr.h"
 #include "wtf/Vector.h"
 #include "wtf/text/WTFString.h"
-#include <v8.h>
 
 namespace blink {
 
@@ -22,9 +23,8 @@ class Dictionary;
 class Response;
 class Request;
 class ScriptState;
-class WebServiceWorkerCache;
 
-class Cache FINAL : public GarbageCollected<Cache>, public ScriptWrappable {
+class Cache FINAL : public GarbageCollectedFinalized<Cache>, public ScriptWrappable {
     DEFINE_WRAPPERTYPEINFO();
     WTF_MAKE_NONCOPYABLE(Cache);
 public:
@@ -46,14 +46,23 @@ public:
     ScriptPromise keys(ScriptState*, Request*, const Dictionary& queryParams);
     ScriptPromise keys(ScriptState*, const String&, const Dictionary& queryParams);
 
-    void trace(Visitor*) { }
-
     static PassRefPtrWillBeRawPtr<DOMException> domExceptionForCacheError(WebServiceWorkerCacheError);
 
-private:
-    explicit Cache(WebServiceWorkerCache* webCache);
+    void trace(Visitor*) { }
 
-    WebServiceWorkerCache const* ALLOW_UNUSED m_webCache;
+private:
+    explicit Cache(WebServiceWorkerCache*);
+
+    ScriptPromise matchImpl(ScriptState*, Request*, const Dictionary& queryParams);
+    ScriptPromise matchAllImpl(ScriptState*, Request*, const Dictionary& queryParams);
+    ScriptPromise addImpl(ScriptState*, Request*);
+    ScriptPromise addAllImpl(ScriptState*, Vector<Request*>);
+    ScriptPromise deleteImpl(ScriptState*, Request*, const Dictionary& queryParams);
+    ScriptPromise putImpl(ScriptState*, Request*, Response*);
+    ScriptPromise keysImpl(ScriptState*);
+    ScriptPromise keysImpl(ScriptState*, Request*, const Dictionary& queryParams);
+
+    OwnPtr<WebServiceWorkerCache> m_webCache;
 };
 
 } // namespace blink
