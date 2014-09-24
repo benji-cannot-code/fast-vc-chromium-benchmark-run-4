2cdb9bffbb7c8df95025462c466a8494761091cd
mathiash@opera.com <mathiash@opera.com>
2014-09-24 15:09:06 +0000

    Add WTF::String::emptyString16Bit.
    This makes sure WTF::String::make16BitFrom8BitSource returns a 16-bit string,
    which some callers count on even if the source string happens to be empty.
    
    BUG=412032
    
    Review URL: https://codereview.chromium.org/587173005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182606 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/text/WTFString.h
+++ third_party/WebKit/Source/wtf/text/WTFString.h
@@ -656,6 +656,7 @@ template<> struct DefaultHash<String> {
 
 // Shared global empty string.
 WTF_EXPORT const String& emptyString();
+WTF_EXPORT const String& emptyString16Bit();
 WTF_EXPORT extern const String& xmlnsWithColon;
 
 } // namespace WTF
@@ -668,6 +669,7 @@ using WTF::StrictUTF8Conversion;
 using WTF::StrictUTF8ConversionReplacingUnpairedSurrogatesWithFFFD;
 using WTF::String;
 using WTF::emptyString;
+using WTF::emptyString16Bit;
 using WTF::append;
 using WTF::appendNumber;
 using WTF::charactersAreAllASCII;
