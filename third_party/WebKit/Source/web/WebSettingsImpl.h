593d868af2ab91c41ae2da61b0ced43e565c88d7
jdduke@chromium.org <jdduke@chromium.org>
2014-09-24 16:28:21 +0000

    Add a setting to disable threaded scrolling
    Add a web setting for disabling threaded scrolling. The
    ScrollingCoordinator enforces this by tagging all layers as main-thread
    scroll targets.
    
    Such a setting is useful for local testing and potentially for perf
    instrumentation to track and improve main-thread driven scrolling. Note
    also that this is but a temporary solution as other synchronous
    scrolling triggers are explored.
    
    The Chromium flag to trigger this path is added in
    https://codereview.chromium.org/571963003/.
    
    BUG=414323,410974
    
    Review URL: https://codereview.chromium.org/564043003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182611 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebSettingsImpl.h
+++ third_party/WebKit/Source/web/WebSettingsImpl.h
@@ -155,6 +155,7 @@ public:
     virtual void setTextAreasAreResizable(bool) OVERRIDE;
     virtual void setTextAutosizingEnabled(bool) OVERRIDE;
     virtual void setAccessibilityFontScaleFactor(float) OVERRIDE;
+    virtual void setThreadedScrollingEnabled(bool) OVERRIDE;
     virtual void setTouchDragDropEnabled(bool) OVERRIDE;
     virtual void setTouchEditingEnabled(bool) OVERRIDE;
     virtual void setUnifiedTextCheckerEnabled(bool) OVERRIDE;
