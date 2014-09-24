49978840ccd4bab205e99526358a88a6cf205d73
ianwen <ianwen@chromium.org>
2014-09-24 15:54:55 -0700

    Migrate EnhancedBookmarkBridge from component folder to chrome/browser folder.
    EnhancedBookmarkBridge is only used in android platform and should be
    moved to chrome/browser folder, bacause most bookmark services going
    through this bridge requires profile.
    
    BUG=415774
    
    Review URL: https://codereview.chromium.org/597253003
    
    Cr-Commit-Position: refs/heads/master@{#296547}

--- chrome/browser/android/chrome_jni_registrar.cc
+++ chrome/browser/android/chrome_jni_registrar.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/content_view_util.h"
 #include "chrome/browser/android/dev_tools_server.h"
 #include "chrome/browser/android/dom_distiller/feedback_reporter_android.h"
+#include "chrome/browser/android/enhanced_bookmarks/enhanced_bookmarks_bridge.h"
 #include "chrome/browser/android/favicon_helper.h"
 #include "chrome/browser/android/foreign_session_helper.h"
 #include "chrome/browser/android/intent_helper.h"
@@ -75,7 +76,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/android/website_settings_popup_android.h"
 #include "components/bookmarks/common/android/component_jni_registrar.h"
 #include "components/dom_distiller/android/component_jni_registrar.h"
-#include "components/enhanced_bookmarks/android/component_jni_registrar.h"
 #include "components/gcm_driver/android/component_jni_registrar.h"
 #include "components/navigation_interception/component_jni_registrar.h"
 #include "components/variations/android/component_jni_registrar.h"
@@ -94,8 +94,6 @@ static base::android::RegistrationMethod kChromeRegisteredMethods[] = {
   // Register JNI for components we depend on.
   { "Bookmarks", bookmarks::android::RegisterBookmarks },
   { "DomDistiller", dom_distiller::android::RegisterDomDistiller },
-  { "EnhancedBookmarks",
-    enhanced_bookmarks::android::RegisterEnhancedBookmarks },
   { "GCMDriver", gcm::android::RegisterGCMDriverJni },
   { "NavigationInterception",
     navigation_interception::RegisterNavigationInterceptionJni },
@@ -140,6 +138,8 @@ static base::android::RegistrationMethod kChromeRegisteredMethods[] = {
   { "DomDistillerServiceFactory",
     dom_distiller::android::DomDistillerServiceFactoryAndroid::Register },
   { "DomDistillerTabUtils", RegisterDomDistillerTabUtils },
+  { "EnhancedBookmarksBridge",
+    enhanced_bookmarks::android::RegisterEnhancedBookmarksBridge},
   { "ExternalPrerenderRequestHandler",
       prerender::ExternalPrerenderHandlerAndroid::
       RegisterExternalPrerenderHandlerAndroid },
--- chrome/browser/android/enhanced_bookmarks/enhanced_bookmarks_bridge.h
+++ chrome/browser/android/enhanced_bookmarks/enhanced_bookmarks_bridge.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef COMPONENTS_ENHANCED_BOOKMARKS_ANDROID_ENHANCED_BOOKMARKS_BRIDGE_H_
-#define COMPONENTS_ENHANCED_BOOKMARKS_ANDROID_ENHANCED_BOOKMARKS_BRIDGE_H_
+#ifndef CHROME_BROWSER_ANDROID_ENHANCED_BOOKMARKS_ENHANCED_BOOKMARKS_BRIDGE_H_
+#define CHROME_BROWSER_ANDROID_ENHANCED_BOOKMARKS_ENHANCED_BOOKMARKS_BRIDGE_H_
 
 #include "base/android/jni_android.h"
 
@@ -41,4 +41,4 @@ bool RegisterEnhancedBookmarksBridge(JNIEnv* env);
 }  // namespace android
 }  // namespace enhanced_bookmarks
 
-#endif  // COMPONENTS_ENHANCED_BOOKMARKS_ANDROID_ENHANCED_BOOKMARKS_BRIDGE_H_
+#endif  // CHROME_BROWSER_ANDROID_ENHANCED_BOOKMARKS_ENHANCED_BOOKMARKS_BRIDGE_H_
