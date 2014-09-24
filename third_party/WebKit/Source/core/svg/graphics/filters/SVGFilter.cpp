4e5f2a8bc5f42aefb9a37935bb5b20ea25822cb4
senorblanco@chromium.org <senorblanco@chromium.org>
2014-09-24 00:13:26 +0000

    First pass at ripping out non-deferred SVG filter path.
    There will be more to come, especially once the CSS deferred
    path has proven itself and stuck and we can remove big
    chunks of FilterEffect.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/500233002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182529 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/graphics/filters/SVGFilter.cpp
+++ third_party/WebKit/Source/core/svg/graphics/filters/SVGFilter.cpp
@@ -26,8 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-SVGFilter::SVGFilter(const AffineTransform& absoluteTransform, const IntRect& absoluteSourceDrawingRegion, const FloatRect& targetBoundingBox, const FloatRect& filterRegion, bool effectBBoxMode)
-    : Filter(absoluteTransform)
+SVGFilter::SVGFilter(const IntRect& absoluteSourceDrawingRegion, const FloatRect& targetBoundingBox, const FloatRect& filterRegion, bool effectBBoxMode)
+    : Filter(AffineTransform())
     , m_absoluteSourceDrawingRegion(absoluteSourceDrawingRegion)
     , m_targetBoundingBox(targetBoundingBox)
     , m_effectBBoxMode(effectBBoxMode)
@@ -58,9 +58,9 @@ FloatPoint3D SVGFilter::resolve3dPoint(const FloatPoint3D& point) const
         point.z() * sqrtf(m_targetBoundingBox.size().diagonalLengthSquared() / 2));
 }
 
-PassRefPtr<SVGFilter> SVGFilter::create(const AffineTransform& absoluteTransform, const IntRect& absoluteSourceDrawingRegion, const FloatRect& targetBoundingBox, const FloatRect& filterRegion, bool effectBBoxMode)
+PassRefPtr<SVGFilter> SVGFilter::create(const IntRect& absoluteSourceDrawingRegion, const FloatRect& targetBoundingBox, const FloatRect& filterRegion, bool effectBBoxMode)
 {
-    return adoptRef(new SVGFilter(absoluteTransform, absoluteSourceDrawingRegion, targetBoundingBox, filterRegion, effectBBoxMode));
+    return adoptRef(new SVGFilter(absoluteSourceDrawingRegion, targetBoundingBox, filterRegion, effectBBoxMode));
 }
 
 } // namespace blink
