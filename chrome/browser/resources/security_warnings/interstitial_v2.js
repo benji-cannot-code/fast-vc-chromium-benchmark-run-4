d87840b31520224087997e1b4bc5668f2dd2d2d7
felt <felt@chromium.org>
2014-09-23 21:49:01 -0700

    Add debugging information to the SSL blocking page
    If you click on the SSL interstitial's error code, the following is
    now shown:
    * subject
    * issuer
    * validity end date
    * current date
    * pem encoded chain
    
    This CL adds a placeholder for debugging information to the security
    interstitial. It's intentionally meant to be flexible, so that it could
    support other debugging information in the future from other warnings.
    
    BUG=413838
    
    Review URL: https://codereview.chromium.org/584463005
    
    Cr-Commit-Position: refs/heads/master@{#296349}

--- chrome/browser/resources/security_warnings/interstitial_v2.js
+++ chrome/browser/resources/security_warnings/interstitial_v2.js
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 var expandedDetails = false;
 var keyPressState = 0;
 
-/*
+/**
  * A convenience method for sending commands to the parent page.
  * @param {string} cmd  The command to send.
  */
@@ -18,7 +18,7 @@ function sendCommand(cmd) {
   window.domAutomationController.send(cmd);
 }
 
-/*
+/**
  * This allows errors to be skippped by typing "danger" into the page.
  * @param {string} e The key that was just pressed.
  */
@@ -33,6 +33,35 @@ function handleKeypress(e) {
   } else {
     keyPressState = 0;
   }
+}
+
+/**
+ * This appends a piece of debugging information to the end of the warning.
+ * When complete, the caller must also make the debugging div
+ * (error-debugging-info) visible.
+ * @param {string} title  The name of this debugging field.
+ * @param {string} value  The value of the debugging field.
+ */
+function appendDebuggingField(title, value) {
+  // The values input here are not trusted. Never use innerHTML on these
+  // values!
+  var spanTitle = document.createElement('span');
+  spanTitle.classList.add('debugging-title');
+  spanTitle.innerText = title + ': ';
+
+  var spanValue = document.createElement('span');
+  spanValue.classList.add('debugging-value');
+  spanValue.innerText = value;
+
+  var pElem = document.createElement('p');
+  pElem.classList.add('debugging-content');
+  pElem.appendChild(spanTitle);
+  pElem.appendChild(spanValue);
+  $('error-debugging-info').appendChild(pElem);
+}
+
+function toggleDebuggingInfo() {
+  $('error-debugging-info').classList.toggle('hidden');
 }
 
 function setupEvents() {
@@ -98,6 +127,7 @@ function setupEvents() {
 
   preventDefaultOnPoundLinkClicks();
   setupCheckbox();
+  setupSSLDebuggingInfo();
   document.addEventListener('keypress', handleKeypress);
 }
 
