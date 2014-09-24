a4454d824453686536eb86313e0882d36194e160
gcasto <gcasto@chromium.org>
2014-09-24 12:11:09 -0700

    [Password Generation] Change links in the bubble to point to the local manager
    BUG=413846
    
    Review URL: https://codereview.chromium.org/583883003
    
    Cr-Commit-Position: refs/heads/master@{#296482}

--- chrome/browser/ui/cocoa/passwords/manage_passwords_bubble_confirmation_view_controller.mm
+++ chrome/browser/ui/cocoa/passwords/manage_passwords_bubble_confirmation_view_controller.mm
@@ -42,7 +42,7 @@ using namespace password_manager::mac::ui;
 - (BOOL)textView:(NSTextView*)textView
    clickedOnLink:(id)link
          atIndex:(NSUInteger)charIndex {
-  model_->OnRemoteManageLinkClicked();
+  model_->OnManageLinkClicked();
   [delegate_ viewShouldDismiss];
   return YES;
 }
--- chrome/browser/ui/passwords/manage_passwords_ui_controller_mock.cc
+++ chrome/browser/ui/passwords/manage_passwords_ui_controller_mock.cc
@@ -11,7 +11,6 @@ ManagePasswordsUIControllerMock::ManagePasswordsUIControllerMock(
     content::WebContents* contents)
     : ManagePasswordsUIController(contents),
       navigated_to_settings_page_(false),
-      navigated_to_account_central_management_page_(false),
       saved_password_(false),
       never_saved_password_(false) {
   contents->SetUserData(UserDataKey(), this);
@@ -23,10 +22,6 @@ ManagePasswordsUIControllerMock::
 void ManagePasswordsUIControllerMock::
     NavigateToPasswordManagerSettingsPage() {
   navigated_to_settings_page_ = true;
-}
-
-void ManagePasswordsUIControllerMock::NavigateToAccountCentralManagementPage() {
-  navigated_to_account_central_management_page_ = true;
 }
 
 const autofill::PasswordForm&
