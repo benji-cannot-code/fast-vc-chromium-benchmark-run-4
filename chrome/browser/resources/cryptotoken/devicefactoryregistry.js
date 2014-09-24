23cfe37d93f4c4646d57bbc28569d71d14ff9ccb
arnarb <arnarb@chromium.org>
2014-09-23 19:59:23 -0700

    Update cryptotoken to 0.8.63
     - Request batch attestation instead of individual
     - Updates from U2F API revisions
     - Enumerate HID devieces based on usage page instead of vid/pid
     - Handle wrong keyHandle length and init messages
     - Smaller bug fixes and refactoring
    
    BUG=416998
    R=xiyuan,juanlang
    
    Review URL: https://codereview.chromium.org/596083002
    
    Cr-Commit-Position: refs/heads/master@{#296334}

--- chrome/browser/resources/cryptotoken/devicefactoryregistry.js
+++ chrome/browser/resources/cryptotoken/devicefactoryregistry.js
@@ -12,13 +12,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /**
  * @param {!GnubbyFactory} gnubbyFactory A Gnubby factory.
  * @param {!CountdownFactory} countdownFactory A countdown timer factory.
+ * @param {!IndividualAttestation} individualAttestation An individual
+ *     attestation implementation.
  * @constructor
  */
-function DeviceFactoryRegistry(gnubbyFactory, countdownFactory) {
+function DeviceFactoryRegistry(gnubbyFactory, countdownFactory,
+    individualAttestation) {
   /** @private {!GnubbyFactory} */
   this.gnubbyFactory_ = gnubbyFactory;
   /** @private {!CountdownFactory} */
   this.countdownFactory_ = countdownFactory;
+  /** @private {!IndividualAttestation} */
+  this.individualAttestation_ = individualAttestation;
 }
 
 /** @return {!GnubbyFactory} A Gnubby factory. */
@@ -29,4 +34,10 @@ DeviceFactoryRegistry.prototype.getGnubbyFactory = function() {
 /** @return {!CountdownFactory} A countdown factory. */
 DeviceFactoryRegistry.prototype.getCountdownFactory = function() {
   return this.countdownFactory_;
+};
+
+/** @return {!IndividualAttestation} An individual attestation implementation.
+ */
+DeviceFactoryRegistry.prototype.getIndividualAttestation = function() {
+  return this.individualAttestation_;
 };
