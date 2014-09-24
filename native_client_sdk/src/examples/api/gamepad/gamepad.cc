cd6fa4530a22aba765bf66b634f9591e0194e998
Ben Smith <binji@chromium.org>
2014-09-24 14:25:46 -0700

    [NaCl SDK] Fix gamepad example for msvc 2013
    std::{min,max} requires the algorithm.h header.
    
    BUG=none
    R=bradnelson@google.com, bradnelson@chromium.org
    
    Review URL: https://codereview.chromium.org/597283002
    
    Cr-Commit-Position: refs/heads/master@{#296523}

--- native_client_sdk/src/examples/api/gamepad/gamepad.cc
+++ native_client_sdk/src/examples/api/gamepad/gamepad.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <algorithm>
 #include <cassert>
 
 #include "ppapi/c/ppb_gamepad.h"
