cc609729c52cb15fb6a7fb9651d20cfe3d50c0b7
earthdok <earthdok@chromium.org>
2014-09-24 05:42:07 -0700

    Instrumented libraries: update the libnspr4 build script.
    There has been a security update in Precise which changed the paths somewhat.
    
    BUG=313751
    TBR=glider@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/600793002
    
    Cr-Commit-Position: refs/heads/master@{#296395}

--- third_party/instrumented_libraries/scripts/libnspr4.sh
+++ third_party/instrumented_libraries/scripts/libnspr4.sh
@@ -6,4 +6,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 # This script does some preparations before build of instrumented libnspr4.
 
-mv mozilla/nsprpub/* .
+mv nspr/* .
