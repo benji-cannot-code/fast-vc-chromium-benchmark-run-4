6139b25cdf074082cb7bcee13e660d879cbd0b74
anand.ratn <anand.ratn@samsung.com>
2014-09-24 02:15:02 -0700

    Use base::StringPairs where appropriate from src/extensions
    Becuase base/strings/string_split.h defines:
    typedef std::vector<std::pair<std::string, std::string> > StringPairs;
    
    BUG=412250
    
    Review URL: https://codereview.chromium.org/594133003
    
    Cr-Commit-Position: refs/heads/master@{#296369}

--- extensions/common/permissions/permission_message_util.cc
+++ extensions/common/permissions/permission_message_util.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/permissions/permission_message_util.h"
 
 #include "base/strings/string_number_conversions.h"
+#include "base/strings/string_split.h"
 #include "base/strings/utf_string_conversions.h"
 #include "extensions/common/permissions/permission_message.h"
 #include "extensions/common/permissions/permission_set.h"
