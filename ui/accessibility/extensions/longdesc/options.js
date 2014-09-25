f305314d38e3cc625ba33760fbbda725d3087023
Dominic Mazzoni <dmazzoni@chromium.org>
2014-09-25 10:44:11 -0700

    Initial checkin of accessibility extensions.
    We still need to clean up style violations and add i18n to all of these.
    
    BUG=416983
    R=plundblad@chromium.org
    
    Review URL: https://codereview.chromium.org/593293002
    
    Cr-Commit-Position: refs/heads/master@{#296737}

---
+++ ui/accessibility/extensions/longdesc/options.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/* Copyright (c) 2014 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file. */
+
+// Saves options to chrome.storage
+function save_options() {
+  var border = document.getElementById('border').checked;
+  chrome.storage.sync.set({
+    addBorder: border
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  // Use default value addBOrder = false.
+  chrome.storage.sync.get("addBorder", function(item) {
+    document.getElementById('border').checked = item.addBorder;
+  });
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('border').addEventListener('click', save_options);
