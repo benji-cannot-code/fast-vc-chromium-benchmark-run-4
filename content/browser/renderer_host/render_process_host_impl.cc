9a5da3bdc56be842ce9e851dceb1c896ca806c77
mohan.reddy <mohan.reddy@samsung.com>
2014-09-24 22:34:47 -0700

    Removed the ScriptedSpeech content side.
    Removal the corresponding changes in blink are applied in the following patch
    https://codereview.chromium.org/599863002/
    
    BUG=402536
    
    Review URL: https://codereview.chromium.org/598923002
    
    Cr-Commit-Position: refs/heads/master@{#296655}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1233,7 +1233,6 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
 #if defined(OS_ANDROID)
     switches::kDisableGestureRequirementForMediaPlayback,
     switches::kDisableWebRTC,
-    switches::kEnableSpeechRecognition,
     switches::kMediaDrmEnableNonCompositing,
     switches::kNetworkCountryIso,
     switches::kDisableWebAudio,
