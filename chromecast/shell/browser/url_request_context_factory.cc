fdea7d619d79514bd66f551d66332248a64b1cb2
gunsch <gunsch@chromium.org>
2014-09-24 17:06:03 -0700

    Chromecast: allow local file access for Android.
    R=byungchul@chromium.org,lcwu@chromium.org
    BUG=332744
    
    Review URL: https://codereview.chromium.org/595113002
    
    Cr-Commit-Position: refs/heads/master@{#296581}

--- chromecast/shell/browser/url_request_context_factory.cc
+++ chromecast/shell/browser/url_request_context_factory.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/ssl/default_channel_id_store.h"
 #include "net/ssl/ssl_config_service_defaults.h"
 #include "net/url_request/data_protocol_handler.h"
+#include "net/url_request/file_protocol_handler.h"
 #include "net/url_request/url_request_context.h"
 #include "net/url_request/url_request_context_getter.h"
 #include "net/url_request/url_request_intercepting_job_factory.h"
@@ -241,6 +242,15 @@ void URLRequestContextFactory::InitializeMainContextDependencies(
       url::kDataScheme,
       new net::DataProtocolHandler);
   DCHECK(set_protocol);
+#if defined(OS_ANDROID)
+  set_protocol = job_factory->SetProtocolHandler(
+      url::kFileScheme,
+      new net::FileProtocolHandler(
+          content::BrowserThread::GetBlockingPool()->
+              GetTaskRunnerWithShutdownBehavior(
+                  base::SequencedWorkerPool::SKIP_ON_SHUTDOWN)));
+  DCHECK(set_protocol);
+#endif  // defined(OS_ANDROID)
 
   // Set up interceptors in the reverse order.
   scoped_ptr<net::URLRequestJobFactory> top_job_factory =
