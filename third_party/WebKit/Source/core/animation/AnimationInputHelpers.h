9e1d12a80b94116b09a3dc8191bc5ccb702e130a
alancutter@chromium.org <alancutter@chromium.org>
2014-09-25 08:50:03 +0000

    Web Animations API: Support dashes in keyframe properties
    This change allows element.animate() keyframes to accept CSS properties
    in their "dashed" form as well as camel cased.
    Example:
    element.animate([
      {backgroundColor: 'red'},
      {'background-color': 'green'},
    ], 1000);
    
    Relevant specs:
    http://w3c.github.io/web-animations/#dictdef-keyframe
    http://dev.w3.org/csswg/cssom/#idl-attribute-to-css-property
    
    
    BUG=417594
    
    Review URL: https://codereview.chromium.org/606563002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182658 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/AnimationInputHelpers.h
+++ third_party/WebKit/Source/core/animation/AnimationInputHelpers.h
@@ -15,7 +15,7 @@ class TimingFunction;
 
 class AnimationInputHelpers {
 public:
-    static CSSPropertyID camelCaseCSSPropertyNameToID(const String&);
+    static CSSPropertyID keyframeAttributeToCSSPropertyID(const String&);
     static PassRefPtr<TimingFunction> parseTimingFunction(const String&);
 };
 
