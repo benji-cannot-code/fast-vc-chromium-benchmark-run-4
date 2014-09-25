fddca08ef499d16ab4fb936f7f90bc2f9ff4d483
Dominic Mazzoni <dmazzoni@chromium.org>
2014-09-25 15:02:34 -0700

    Move accessibility extension translations to grd file.
    We still need to finish updating the extensions to use these translations.
    
    BUG=416983
    TBR=plundblad
    
    Review URL: https://codereview.chromium.org/610453002
    
    Cr-Commit-Position: refs/heads/master@{#296803}

--- ui/accessibility/extensions/longdesc/background.js
+++ ui/accessibility/extensions/longdesc/background.js
@@ -27,7 +27,7 @@ chrome.windows.getAll({'populate': true}, function(windows) {
  * Add context menu item when the extension is installed.
  */
 chrome.contextMenus.create({
-    "title": "More information...",
+    "title": chrome.i18n.getMessage('openLongDescription'),
     "contexts": ["all"],
     "id": "moreInfo",
     "onclick": contextMenuClicked,
