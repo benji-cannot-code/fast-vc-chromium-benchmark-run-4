206a2cf5fc6c0be5e2bc322049b6c418bc038cda
mad <mad@chromium.org>
2014-09-25 12:26:46 -0700

    Revert of Remove unneeded Friend from chrome_metrics_service_accessor (patchset #1 id:20001 of https://codereview.chromium.org/601243002/)
    Reason for revert:
    We need to bring this back since the usage of it is coming back in an upcoming patch.
    
    Sorry about that...
    
    BYE
    MAD
    
    Original issue's description:
    > Remove unneeded Friend from chrome_metrics_service_accessor
    >
    > BUG=
    >
    > Committed: https://crrev.com/ca032c3aed93e5afd7c32da76147f36a72dc4341
    > Cr-Commit-Position: refs/heads/master@{#296722}
    
    TBR=asvitkine@chromium.org,csharp@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=
    
    Review URL: https://codereview.chromium.org/603173003
    
    Cr-Commit-Position: refs/heads/master@{#296763}

--- chrome/browser/metrics/chrome_metrics_service_accessor.h
+++ chrome/browser/metrics/chrome_metrics_service_accessor.h
@@ -15,11 +15,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/metrics/metrics_service_accessor.h"
 
 class ChromeExtensionDownloaderFactory;
+class PrefService;
 class Profile;
 
 namespace {
 class CrashesDOMHandler;
 class FlashDOMHandler;
+}
+
+namespace component_updater {
+class ComponentUpdateService;
+void RegisterSwReporterComponent(ComponentUpdateService* cus,
+                                 PrefService* prefs);
 }
 
 namespace extensions {
@@ -43,6 +50,9 @@ class BrowserOptionsHandler;
 // as a 'friend' below.
 class ChromeMetricsServiceAccessor : public MetricsServiceAccessor {
  private:
+  friend void component_updater::RegisterSwReporterComponent(
+      component_updater::ComponentUpdateService* cus,
+      PrefService* prefs);
   friend bool prerender::IsOmniboxEnabled(Profile* profile);
   friend class ChromeExtensionDownloaderFactory;
   friend class ChromeRenderMessageFilter;
