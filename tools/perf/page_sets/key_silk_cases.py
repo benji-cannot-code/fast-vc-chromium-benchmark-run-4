61c61e2365a58b3b006edf78e75f0684dc362289
ajuma <ajuma@chromium.org>
2014-09-25 13:33:06 -0700

    Add text size animation to silk page set
    BUG=368420
    
    Review URL: https://codereview.chromium.org/604003002
    
    Cr-Commit-Position: refs/heads/master@{#296783}

--- tools/perf/page_sets/key_silk_cases.py
+++ tools/perf/page_sets/key_silk_cases.py
@@ -634,6 +634,21 @@ class UpdateHistoryState(KeySilkCasesPage):
     interaction.End()
 
 
+class TextSizeAnimation(KeySilkCasesPage):
+
+  """ Why: Scale animation with text. """
+
+  def __init__(self, page_set):
+    super(TextSizeAnimation, self).__init__(
+      url='http://jsbin.com/gikex/2/quiet',
+      page_set=page_set)
+
+    self.gpu_raster = True
+
+  def RunSmoothness(self, action_runner):
+    action_runner.Wait(4)
+
+
 class KeySilkCasesPageSet(page_set_module.PageSet):
 
   """ Pages hand-picked for project Silk. """
@@ -675,3 +690,4 @@ class KeySilkCasesPageSet(page_set_module.PageSet):
     self.AddPage(Page26(self))
     self.AddPage(SVGIconRaster(self))
     self.AddPage(UpdateHistoryState(self))
+    self.AddPage(TextSizeAnimation(self))
