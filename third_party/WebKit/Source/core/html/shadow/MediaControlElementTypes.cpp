486cf3070148fd134cb3c3062caaa2c344c9154d
philipj@opera.com <philipj@opera.com>
2014-09-25 09:43:19 +0000

    Let toParentMediaElement return null for the media element itself
    An element isn't its own parent, and actual usage doesn't require it.
    
    TEST=LayoutTests/media/video-webkit-appearance.html
    
    Without the fix, this test hits the ASSERT_WITH_SECURITY_IMPLICATION
    in mediaControlElementType(), via paintMediaPlayButton().
    
    BUG=414100
    R=fs@opera.com
    
    Review URL: https://codereview.chromium.org/598013002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182664 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/shadow/MediaControlElementTypes.cpp
+++ third_party/WebKit/Source/core/html/shadow/MediaControlElementTypes.cpp
@@ -52,7 +52,7 @@ HTMLMediaElement* toParentMediaElement(Node* node)
         return 0;
     Node* mediaNode = node->shadowHost();
     if (!mediaNode)
-        mediaNode = node;
+        return 0;
     if (!isHTMLMediaElement(mediaNode))
         return 0;
 
