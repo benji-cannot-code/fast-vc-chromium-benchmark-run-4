78ba2ec3758ac05e875e8c3b2c5763dd48fed00d
vkuzkokov <vkuzkokov@chromium.org>
2014-09-25 09:44:24 -0700

    DevTools: Protocol handler generator for content
    BUG=405566
    
    Review URL: https://codereview.chromium.org/508973003
    
    Cr-Commit-Position: refs/heads/master@{#296723}

---
+++ content/browser/devtools/protocol/inspector_handler.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_DEVTOOLS_PROTOCOL_INSPECTOR_HANDLER_H_
+#define CONTENT_BROWSER_DEVTOOLS_PROTOCOL_INSPECTOR_HANDLER_H_
+
+#include "content/browser/devtools/protocol/devtools_protocol_handler_impl.h"
+
+namespace content {
+namespace devtools {
+namespace inspector {
+
+class InspectorHandler {
+ public:
+  InspectorHandler();
+  virtual ~InspectorHandler();
+
+  void SetClient(scoped_ptr<Client> client);
+
+ private:
+  scoped_ptr<Client> client_;
+
+  DISALLOW_COPY_AND_ASSIGN(InspectorHandler);
+};
+
+}  // namespace inspector
+}  // namespace devtools
+}  // namespace content
+
+#endif  // CONTENT_BROWSER_DEVTOOLS_PROTOCOL_INSPECTOR_HANDLER_H_
