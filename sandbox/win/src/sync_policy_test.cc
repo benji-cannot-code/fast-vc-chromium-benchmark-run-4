fc95aea289f3a2bf5993fcba7d89ef8f058d9df9
rvargas <rvargas@chromium.org>
2014-09-24 17:02:04 -0700

    Remove implicit HANDLE conversions from sandbox.
    BUG=416722
    R=wfh@chromium.org
    
    Review URL: https://codereview.chromium.org/606443002
    
    Cr-Commit-Position: refs/heads/master@{#296580}

--- sandbox/win/src/sync_policy_test.cc
+++ sandbox/win/src/sync_policy_test.cc
@@ -26,7 +26,7 @@ SBOX_TESTS_COMMAND int Event_Open(int argc, wchar_t **argv) {
       desired_access, FALSE, argv[1]));
   DWORD error_open = ::GetLastError();
 
-  if (event_open.Get())
+  if (event_open.IsValid())
     return SBOX_TEST_SUCCEEDED;
 
   if (ERROR_ACCESS_DENIED == error_open ||
@@ -58,7 +58,7 @@ SBOX_TESTS_COMMAND int Event_CreateOpen(int argc, wchar_t **argv) {
   if (event_name)
     event_open.Set(::OpenEvent(EVENT_ALL_ACCESS, FALSE, event_name));
 
-  if (event_create.Get()) {
+  if (event_create.IsValid()) {
     DWORD wait = ::WaitForSingleObject(event_create.Get(), 0);
     if (initial_state && WAIT_OBJECT_0 != wait)
       return SBOX_TEST_FAILED;
@@ -69,10 +69,11 @@ SBOX_TESTS_COMMAND int Event_CreateOpen(int argc, wchar_t **argv) {
 
   if (event_name) {
     // Both event_open and event_create have to be valid.
-    if (event_open.Get() && event_create)
+    if (event_open.IsValid() && event_create.IsValid())
       return SBOX_TEST_SUCCEEDED;
 
-    if (event_open.Get() && !event_create || !event_open.Get() && event_create)
+    if (event_open.IsValid() && !event_create.IsValid() ||
+        !event_open.IsValid() && event_create.IsValid())
       return SBOX_TEST_FAILED;
   } else {
     // Only event_create has to be valid.
