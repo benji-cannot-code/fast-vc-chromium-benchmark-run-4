2c56cd5e1b146df59d3f24b89b56dbcb2c3fae98
perkj@chromium.org <perkj@chromium.org>
2014-09-25 06:41:32 +0000

    Make sure the last remote track can be removed from a MediaStream generated from a PeerConnection.
    This cl change MediaStream.cpp to allow removing a remote track from a MediaStream even if there are no live tracks.
    The problem is that if the implementor of the remote track has set the last tracks readyState to ended, the last track can't be removed from the MediaStream.
    
    BUG=417245
    TEST= See bug report.
    
    Review URL: https://codereview.chromium.org/600823002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182651 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/mediastream/MediaStream.cpp
+++ third_party/WebKit/Source/modules/mediastream/MediaStream.cpp
@@ -329,7 +329,7 @@ void MediaStream::addRemoteTrack(MediaStreamComponent* component)
 
 void MediaStream::removeRemoteTrack(MediaStreamComponent* component)
 {
-    if (ended())
+    if (m_stopped)
         return;
 
     MediaStreamTrackVector* tracks = 0;
