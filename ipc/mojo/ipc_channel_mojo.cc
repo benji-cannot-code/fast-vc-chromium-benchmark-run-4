b7e2d7a98e7e8e7754ef2756d866b93a217d9c85
morrita <morrita@chromium.org>
2014-09-25 16:12:16 -0700

    Remove IPC::Channel::TakePipeHandle()
    It doesn't work, and it is no longer used.
    
    BUG=416208
    R=cpu@chromium.org, jam@chromium.org
    
    Review URL: https://codereview.chromium.org/601563002
    
    Cr-Commit-Position: refs/heads/master@{#296820}

--- ipc/mojo/ipc_channel_mojo.cc
+++ ipc/mojo/ipc_channel_mojo.cc
@@ -192,11 +192,6 @@ base::ProcessId ChannelMojo::GetSelfPID() const {
   return base::GetCurrentProcId();
 }
 
-ChannelHandle ChannelMojo::TakePipeHandle() {
-  NOTREACHED();
-  return ChannelHandle();
-}
-
 void ChannelMojo::OnClientLaunched(base::ProcessHandle handle) {
   bootstrap_->OnClientLaunched(handle);
 }
