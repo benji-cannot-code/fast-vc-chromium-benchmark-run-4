90fa76accfdefc814b3855e3c5d792908625ae77
mkosiba <mkosiba@chromium.org>
2014-09-25 05:13:24 -0700

    [android_webview] Android.mk file cleanup.
    We don't use the android_webview_java target anymore, get rid of it
    and the resource_map entries.
    Also, get rid of the res_hack folder since we now have a real resources
    folder in our source tree.
    
    BUG=None
    TEST=compile on android_aosp bot
    TBR=miguelg@chromium.org
    
    Review URL: https://codereview.chromium.org/540993002
    
    Cr-Commit-Position: refs/heads/master@{#296695}

--- android_webview/build/resources_config.mk
+++ android_webview/build/resources_config.mk
@@ -10,14 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 android_webview_manifest_file := $(call my-dir)/AndroidManifest.xml
 
 # Resources.
-# The res_hack folder is necessary to defeat a build system "optimization" which
-# ends up skipping running aapt if there are no resource files in any of the
-# resources dirs. Unfortunately, because all of our resources are generated at
-# build time and because this check is performed when processing the Makefile
-# it tests positive when building from clean resulting in a build failure.
-# We defeat the optimization by including an empty values.xml file in the list.
 android_webview_resources_dirs := \
-    $(call my-dir)/res_hack \
     $(call my-dir)/../java/res \
     $(call intermediates-dir-for,GYP,shared)/android_webview_jarjar_content_resources/jarjar_res \
     $(call intermediates-dir-for,GYP,shared)/android_webview_jarjar_ui_resources/jarjar_res \
