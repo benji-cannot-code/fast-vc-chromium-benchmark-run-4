f6f3008ce476f20d5c2e7c33556ac1575b1b8af2
alexmos <alexmos@chromium.org>
2014-09-26 14:02:51 -0700

    Fix RenderProcessHostUnitTest.RendererProcessLimit to pass with --site-per-process.
    The --site-per-process mode does not allow renderer process reuse, so this test shouldn't run under that flag.
    
    BUG=417518
    
    Review URL: https://codereview.chromium.org/600993002
    
    Cr-Commit-Position: refs/heads/master@{#297029}

--- content/browser/renderer_host/render_process_host_unittest.cc
+++ content/browser/renderer_host/render_process_host_unittest.cc
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <limits>
 
+#include "base/command_line.h"
 #include "content/public/common/content_constants.h"
+#include "content/public/common/content_switches.h"
 #include "content/public/test/mock_render_process_host.h"
 #include "content/test/test_render_view_host.h"
 
@@ -32,6 +34,15 @@ TEST_F(RenderProcessHostUnitTest, GuestsAreNotSuitableHosts) {
 
 #if !defined(OS_ANDROID)
 TEST_F(RenderProcessHostUnitTest, RendererProcessLimit) {
+  // This test shouldn't run with --site-per-process or
+  // --enable-strict-site-isolation modes, since they don't allow renderer
+  // process reuse, which this test explicitly exercises.
+  const base::CommandLine& command_line =
+      *base::CommandLine::ForCurrentProcess();
+  if (command_line.HasSwitch(switches::kSitePerProcess) ||
+      command_line.HasSwitch(switches::kEnableStrictSiteIsolation))
+    return;
+
   // Disable any overrides.
   RenderProcessHostImpl::SetMaxRendererProcessCount(0);
 
