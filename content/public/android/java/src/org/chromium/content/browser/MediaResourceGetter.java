88a6550d4ba7d3381402cf1f6bd4297295f80bf9
wajahat.s <wajahat.s@samsung.com>
2014-09-25 22:41:11 -0700

    Suppress lint warning "SdCardPath" for getRawAcceptableDirectories()
    The following lint warnings are issued every time when chrome/content
    shell is built
    
    ../../../tmp/tmp6VcHbX/0/MediaResourceGetter.java:365 Do not hardcode "/sdcard/";
     use Environment.getExternalStorageDirectory().getPath() instead: SdCardPath [warning]
            result.add("/mnt/sdcard/");
                       ~~~~~~~~~~~~~~
    ../../../tmp/tmp6VcHbX/0/MediaResourceGetter.java:366 Do not hardcode "/sdcard/";
     use Environment.getExternalStorageDirectory().getPath() instead: SdCardPath [warning]
            result.add("/sdcard/");
                       ~~~~~~~~~~
    ../../../tmp/tmp6VcHbX/0/MediaResourceGetter.java:367 Do not hardcode "/data/";
     use Context.getFilesDir().getPath() instead: SdCardPath [warning]
            result.add("/data/data/" + context.getPackageName() + "/cache/");
    
    As these hardcoded values are required and the warnings here are false positives
    which are more explained in https://codereview.chromium.org/561743003/#msg5
    
    BUG=None.
    
    Review URL: https://codereview.chromium.org/597283007
    
    Cr-Commit-Position: refs/heads/master@{#296891}

--- content/public/android/java/src/org/chromium/content/browser/MediaResourceGetter.java
+++ content/public/android/java/src/org/chromium/content/browser/MediaResourceGetter.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.content.browser;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.media.MediaMetadataRetriever;
@@ -361,6 +362,7 @@ class MediaResourceGetter {
         return info.getType();
     }
 
+    @SuppressLint("SdCardPath")
     private List<String> getRawAcceptableDirectories(Context context) {
         List<String> result = new ArrayList<String>();
         result.add("/mnt/sdcard/");
