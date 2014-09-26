c698bc58dbdb733c837b682f37d1dcbb1bd61ca0
qyearsley <qyearsley@chromium.org>
2014-09-25 18:19:34 -0700

    Move bisect-perf-regression.py into auto_bisect directory.
    The purpose of this is to continue gathering all the related code together, and to make it easier to run pylint/tests for the main module which still contains most of the code.
    
    After doing this, pylint and the tests caught a couple of things -- one unused variable name, and after the script changed directory, some of the paths had to be modified as well.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/564663002
    
    Cr-Commit-Position: refs/heads/master@{#296847}

--- tools/auto_bisect/source_control.py
+++ tools/auto_bisect/source_control.py
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import os
 
-from . import bisect_utils
+import bisect_utils
 
 CROS_VERSION_PATTERN = 'new version number from %s'
 
