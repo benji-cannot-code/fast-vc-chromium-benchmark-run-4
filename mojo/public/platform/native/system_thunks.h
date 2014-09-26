67c5121c637f1566aa75ddb889c51c11382385bd
tburkard <tburkard@chromium.org>
2014-09-26 14:24:30 -0700

    Add Go build support for GN. Provide a skeleton of the mojo system API, as well as a skeleton test. R=qsr@chromium.org, brettw@chromium.org, viettrungluu@chromium.org
    Review URL: https://codereview.chromium.org/556813003
    
    Cr-Commit-Position: refs/heads/master@{#297038}

--- mojo/public/platform/native/system_thunks.h
+++ mojo/public/platform/native/system_thunks.h
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// Note: This header should be compilable as C.
+
 #ifndef MOJO_PUBLIC_PLATFORM_NATIVE_SYSTEM_THUNKS_H_
 #define MOJO_PUBLIC_PLATFORM_NATIVE_SYSTEM_THUNKS_H_
 
@@ -44,9 +46,10 @@ struct MojoSystemThunks {
                          const MojoHandleSignals* signals,
                          uint32_t num_handles,
                          MojoDeadline deadline);
-  MojoResult (*CreateMessagePipe)(const MojoCreateMessagePipeOptions* options,
-                                  MojoHandle* message_pipe_handle0,
-                                  MojoHandle* message_pipe_handle1);
+  MojoResult (*CreateMessagePipe)(
+      const struct MojoCreateMessagePipeOptions* options,
+      MojoHandle* message_pipe_handle0,
+      MojoHandle* message_pipe_handle1);
   MojoResult (*WriteMessage)(MojoHandle message_pipe_handle,
                              const void* bytes,
                              uint32_t num_bytes,
@@ -59,7 +62,7 @@ struct MojoSystemThunks {
                             MojoHandle* handles,
                             uint32_t* num_handles,
                             MojoReadMessageFlags flags);
-  MojoResult (*CreateDataPipe)(const MojoCreateDataPipeOptions* options,
+  MojoResult (*CreateDataPipe)(const struct MojoCreateDataPipeOptions* options,
                                MojoHandle* data_pipe_producer_handle,
                                MojoHandle* data_pipe_consumer_handle);
   MojoResult (*WriteData)(MojoHandle data_pipe_producer_handle,
@@ -83,12 +86,12 @@ struct MojoSystemThunks {
   MojoResult (*EndReadData)(MojoHandle data_pipe_consumer_handle,
                             uint32_t num_elements_read);
   MojoResult (*CreateSharedBuffer)(
-      const MojoCreateSharedBufferOptions* options,
+      const struct MojoCreateSharedBufferOptions* options,
       uint64_t num_bytes,
       MojoHandle* shared_buffer_handle);
   MojoResult (*DuplicateBufferHandle)(
       MojoHandle buffer_handle,
-      const MojoDuplicateBufferHandleOptions* options,
+      const struct MojoDuplicateBufferHandleOptions* options,
       MojoHandle* new_buffer_handle);
   MojoResult (*MapBuffer)(MojoHandle buffer_handle,
                           uint64_t offset,
@@ -99,6 +102,8 @@ struct MojoSystemThunks {
 };
 #pragma pack(pop)
 
+
+#ifdef __cplusplus
 // Intended to be called from the embedder. Returns a |MojoCore| initialized
 // to contain pointers to each of the embedder's MojoCore functions.
 inline MojoSystemThunks MojoMakeSystemThunks() {
@@ -125,6 +130,8 @@ inline MojoSystemThunks MojoMakeSystemThunks() {
   };
   return system_thunks;
 }
+#endif
+
 
 // Use this type for the function found by dynamically discovering it in
 // a DSO linked with mojo_system. For example:
@@ -133,6 +140,7 @@ inline MojoSystemThunks MojoMakeSystemThunks() {
 //         "MojoSetSystemThunks"));
 // The expected size of |system_thunks} is returned.
 // The contents of |system_thunks| are copied.
-typedef size_t (*MojoSetSystemThunksFn)(const MojoSystemThunks* system_thunks);
+typedef size_t (*MojoSetSystemThunksFn)(
+    const struct MojoSystemThunks* system_thunks);
 
 #endif  // MOJO_PUBLIC_PLATFORM_NATIVE_SYSTEM_THUNKS_H_
