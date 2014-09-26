bc6a91799f36cf33c4c6963507fba4bf6c1a9efa
viettrungluu <viettrungluu@chromium.org>
2014-09-25 18:19:53 -0700

    Mojo: COMPILE_ASSERT -> static_assert in mojo/system and mojo/embedder.
    R=jamesr@chromium.org
    
    Review URL: https://codereview.chromium.org/603983003
    
    Cr-Commit-Position: refs/heads/master@{#296848}

--- mojo/system/channel.cc
+++ mojo/system/channel.cc
@@ -19,9 +19,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace mojo {
 namespace system {
 
-COMPILE_ASSERT(Channel::kBootstrapEndpointId !=
-                   MessageInTransit::kInvalidEndpointId,
-               kBootstrapEndpointId_is_invalid);
+static_assert(Channel::kBootstrapEndpointId !=
+                  MessageInTransit::kInvalidEndpointId,
+              "kBootstrapEndpointId is invalid");
 
 STATIC_CONST_MEMBER_DEFINITION const MessageInTransit::EndpointId
     Channel::kBootstrapEndpointId;
