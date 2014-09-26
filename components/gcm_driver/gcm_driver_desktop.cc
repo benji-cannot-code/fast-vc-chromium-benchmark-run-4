ad2deff714756b7c6e449ad1efbc232405f7bbc0
jianli <jianli@chromium.org>
2014-09-26 14:50:28 -0700

    Add a new UMA to GCM
    This is used to find out the GCM usage from non-signed-in users.
    
    BUG=384041
    TEST=none
    
    Review URL: https://codereview.chromium.org/605363002
    
    Cr-Commit-Position: refs/heads/master@{#297045}

--- components/gcm_driver/gcm_driver_desktop.cc
+++ components/gcm_driver/gcm_driver_desktop.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/location.h"
 #include "base/logging.h"
+#include "base/metrics/histogram.h"
 #include "base/sequenced_task_runner.h"
 #include "base/threading/sequenced_worker_pool.h"
 #include "components/gcm_driver/gcm_app_handler.h"
@@ -645,6 +646,8 @@ GCMClient::Result GCMDriverDesktop::EnsureStarted() {
   // users.
   if (GCMDriver::IsAllowedForAllUsers())
     gcm_channel_status_syncer_->EnsureStarted();
+
+  UMA_HISTOGRAM_BOOLEAN("GCM.UserSignedIn", signed_in_);
 
   // Note that we need to pass weak pointer again since the existing weak
   // pointer in IOWorker might have been invalidated when check-out occurs.
