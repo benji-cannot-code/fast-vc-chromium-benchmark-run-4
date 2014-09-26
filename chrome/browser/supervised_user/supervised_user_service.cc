d895ae202bd310bdfd458f05b176d5f1f888c7ce
zea <zea@chromium.org>
2014-09-26 10:24:48 -0700

    [Sync] Fix sync backup for supervised users
    sync_initialized() does not account for backup/rollback modes. For now we
    just manually check the backend mode. In the future the PSS will expose
    a better method for denoting when its safe to reconfigure.
    
    BUG=417400
    
    Review URL: https://codereview.chromium.org/605483002
    
    Cr-Commit-Position: refs/heads/master@{#296968}

--- chrome/browser/supervised_user/supervised_user_service.cc
+++ chrome/browser/supervised_user/supervised_user_service.cc
@@ -414,7 +414,8 @@ syncer::ModelTypeSet SupervisedUserService::GetPreferredDataTypes() const {
 void SupervisedUserService::OnStateChanged() {
   ProfileSyncService* service =
       ProfileSyncServiceFactory::GetForProfile(profile_);
-  if (waiting_for_sync_initialization_ && service->sync_initialized()) {
+  if (waiting_for_sync_initialization_ && service->sync_initialized() &&
+      service->backend_mode() == ProfileSyncService::SYNC) {
     waiting_for_sync_initialization_ = false;
     service->RemoveObserver(this);
     FinishSetupSync();
@@ -445,7 +446,8 @@ void SupervisedUserService::FinishSetupSyncWhenReady() {
   // Continue in FinishSetupSync() once the Sync backend has been initialized.
   ProfileSyncService* service =
       ProfileSyncServiceFactory::GetForProfile(profile_);
-  if (service->sync_initialized()) {
+  if (service->sync_initialized() &&
+      service->backend_mode() == ProfileSyncService::SYNC) {
     FinishSetupSync();
   } else {
     service->AddObserver(this);
@@ -456,7 +458,8 @@ void SupervisedUserService::FinishSetupSyncWhenReady() {
 void SupervisedUserService::FinishSetupSync() {
   ProfileSyncService* service =
       ProfileSyncServiceFactory::GetForProfile(profile_);
-  DCHECK(service->sync_initialized());
+  DCHECK(service->sync_initialized() &&
+         service->backend_mode() == ProfileSyncService::SYNC);
 
   // Sync nothing (except types which are set via GetPreferredDataTypes).
   bool sync_everything = false;
