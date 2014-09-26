2b203814a172e90544be14f0147954df7daaa9a4
rob.buis@samsung.com <rob.buis@samsung.com>
2014-09-26 02:10:21 +0000

    Remove QualifiedName::hasOneRef
    This is no longer needed.
    
    Review URL: https://codereview.chromium.org/608593004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182721 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/QualifiedName.h
+++ third_party/WebKit/Source/core/dom/QualifiedName.h
@@ -56,11 +56,6 @@ public:
 
         unsigned computeHash() const;
 
-        bool hasOneRef() const
-        {
-            return m_isStatic || RefCounted<QualifiedNameImpl>::hasOneRef();
-        }
-
         void ref()
         {
             if (m_isStatic)
