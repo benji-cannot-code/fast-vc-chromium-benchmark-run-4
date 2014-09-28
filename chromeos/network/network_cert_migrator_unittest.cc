84be731bd2d69f5dd24a59bac85ea28c25fb7604
John Abd-El-Malek <jam@chromium.org>
2014-09-27 18:46:25 -0700

    Fix the condition when disabling CertLoaderTest and NetworkCertMigratorTest.
    BUG=418369
    TBR=zelidrag@chromium.org
    
    Review URL: https://codereview.chromium.org/607313002
    
    Cr-Commit-Position: refs/heads/master@{#297124}

--- chromeos/network/network_cert_migrator_unittest.cc
+++ chromeos/network/network_cert_migrator_unittest.cc
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/cros_system_api/dbus/service_constants.h"
 
 // http://crbug.com/418369
-#ifndef NDEBUG
+#ifdef NDEBUG
 
 namespace chromeos {
 
