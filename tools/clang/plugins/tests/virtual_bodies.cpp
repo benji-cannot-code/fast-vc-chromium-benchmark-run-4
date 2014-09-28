4bc6b5e12f77ca562e82239b3adf854f1fc3ec9e
dcheng <dcheng@chromium.org>
2014-09-28 12:27:54 -0700

    Update plugin to handle new style rules for virtual specifiers.
    Implements several new checks in the plugin, gated behind a flag:
    - Only one of {virtual,override,final} should be ever used.
    - Destructors must also be annotated correctly.
    - A virtual final method that doesn't override anything should be
      devirtualized.
    
    The test harness for the Chrome plugin has also been updated to stop
    it from littering the source tree with object files, and to make the
    golden files easier to update.
    
    BUG=417463
    
    Review URL: https://codereview.chromium.org/597863002
    
    Cr-Commit-Position: refs/heads/master@{#297129}

--- tools/clang/plugins/tests/virtual_bodies.cpp
+++ tools/clang/plugins/tests/virtual_bodies.cpp
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "virtual_methods.h"
+#include "virtual_bodies.h"
 
 // Shouldn't warn about method usage in the implementation file.
 class VirtualMethodsInImplementation {
@@ -27,9 +27,12 @@ class ConcreteVirtualMethodsInImplementation
 };
 
 // Fill in the implementations
-void VirtualMethodsInHeaders::MethodHasNoArguments() {}
-void WarnOnMissingVirtual::MethodHasNoArguments() {}
-void VirtualMethodsInImplementation::MethodHasNoArguments() {}
+void VirtualMethodsInHeaders::MethodHasNoArguments() {
+}
+void WarnOnMissingVirtual::MethodHasNoArguments() {
+}
+void VirtualMethodsInImplementation::MethodHasNoArguments() {
+}
 
 int main() {
   ConcreteVirtualMethodsInHeaders one;
--- tools/clang/plugins/tests/virtual_bodies.h
+++ tools/clang/plugins/tests/virtual_bodies.h
@@ -33,6 +33,7 @@ class VirtualMethodsInHeadersTesting : public VirtualMethodsInHeaders {
  public:
   // Don't complain about no virtual testing methods.
   void MethodHasNoArguments();
+
  private:
   testing::TestStruct tester_;
 };
