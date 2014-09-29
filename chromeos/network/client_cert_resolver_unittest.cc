15183487812ee06cde586eaa3d6903a6fc93664d
John Abd-El-Malek <jam@chromium.org>
2014-09-29 07:54:26 -0700

    Disable ClientCertResolverTest tests in debug builds as they time out.
    BUG=418369
    TBR=zelidrag@chromium.org
    
    Review URL: https://codereview.chromium.org/607413004
    
    Cr-Commit-Position: refs/heads/master@{#297172}

--- chromeos/network/client_cert_resolver_unittest.cc
+++ chromeos/network/client_cert_resolver_unittest.cc
@@ -35,6 +35,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/cros_system_api/dbus/service_constants.h"
 
+// http://crbug.com/418369
+#ifdef NDEBUG
+
 namespace chromeos {
 
 namespace {
@@ -319,3 +322,5 @@ TEST_F(ClientCertResolverTest, ResolveAfterPolicyApplication) {
 }
 
 }  // namespace chromeos
+
+#endif
