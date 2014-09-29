e719cc2c12096475b2e7d4bc19f4e519e77c4d37
acolwell <acolwell@chromium.org>
2014-09-29 11:53:53 -0700

    Remove AudioBuffersState class
    The AudioBuffersState object doesn't appear to be necessary anymore.
    Most code either completely ignores this information or doesn't actually
    care about the difference between pending_bytes and hardware_delay_bytes.
    Also usually only one of the 2 fields was actually being used at a time.
    This change removes the class and simply uses an int that represent the
    total number of delay bytes.
    
    BUG=125685
    
    Review URL: https://codereview.chromium.org/481193003
    
    Cr-Commit-Position: refs/heads/master@{#297220}

--- content/browser/renderer_host/media/audio_sync_reader.cc
+++ content/browser/renderer_host/media/audio_sync_reader.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "content/browser/renderer_host/media/media_stream_manager.h"
 #include "content/public/common/content_switches.h"
-#include "media/audio/audio_buffers_state.h"
 #include "media/audio/audio_parameters.h"
 
 using media::AudioBus;
