f6000a4838cff3b2500c56535554cc458e158369
reillyg <reillyg@chromium.org>
2014-09-29 12:27:54 -0700

    Rewrite apps::SavedDevicesService as extensions::DevicePermissionsManager.
    This service doesn't need any Chrome dependencies after all and so it
    can move into //extensions. This will make it possible for the USB
    extensions API code to directly depend on it. In the process I have
    rewritten the service with a couple improvements:
    
    * C++11 for-each loops are used instead of explicit iterators.
    * The DevicePermissions object is no longer an inner class (so it can
      be forward declared) and it is explicitly copied when requested so
      that the ownership of data on the FILE and UI threads is clear.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/606503002
    
    Cr-Commit-Position: refs/heads/master@{#297230}

--- chrome/browser/extensions/api/device_permissions_manager_unittest.cc
+++ chrome/browser/extensions/api/device_permissions_manager_unittest.cc
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "apps/saved_devices_service.h"
 #include "base/run_loop.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/values_test_util.h"
@@ -11,19 +10,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/testing_profile.h"
 #include "device/usb/usb_device.h"
 #include "device/usb/usb_device_handle.h"
+#include "extensions/browser/api/device_permissions_manager.h"
 #include "extensions/browser/extension_prefs.h"
 #include "extensions/common/extension.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-namespace apps {
+namespace extensions {
 
 namespace {
 
 using device::UsbDevice;
 using device::UsbDeviceHandle;
-using device::UsbEndpointDirection;
-using device::UsbTransferCallback;
 using testing::Return;
 
 class MockUsbDevice : public UsbDevice {
@@ -40,12 +38,12 @@ class MockUsbDevice : public UsbDevice {
   MOCK_METHOD1(GetManufacturer, bool(base::string16*));
   MOCK_METHOD1(GetProduct, bool(base::string16*));
 
-  bool GetSerialNumber(base::string16* serial) OVERRIDE {
+  virtual bool GetSerialNumber(base::string16* serial_number) OVERRIDE {
     if (serial_number_.empty()) {
       return false;
     }
 
-    *serial = base::UTF8ToUTF16(serial_number_);
+    *serial_number = base::UTF8ToUTF16(serial_number_);
     return true;
   }
 
@@ -56,9 +54,10 @@ class MockUsbDevice : public UsbDevice {
 
   const std::string serial_number_;
 };
-}
 
-class SavedDevicesServiceTest : public testing::Test {
+}  // namespace
+
+class DevicePermissionsManagerTest : public testing::Test {
  protected:
   virtual void SetUp() OVERRIDE {
     testing::Test::SetUp();
@@ -74,7 +73,6 @@ class SavedDevicesServiceTest : public testing::Test {
                                         "    \"usb\""
                                         "  ]"
                                         "}"));
-    service_ = SavedDevicesService::Get(env_.profile());
     device0 = new MockUsbDevice("ABCDE", 0);
     device1 = new MockUsbDevice("", 1);
     device2 = new MockUsbDevice("12345", 2);
@@ -83,53 +81,49 @@ class SavedDevicesServiceTest : public testing::Test {
 
   extensions::TestExtensionEnvironment env_;
   const extensions::Extension* extension_;
-  SavedDevicesService* service_;
   scoped_refptr<MockUsbDevice> device0;
   scoped_refptr<MockUsbDevice> device1;
   scoped_refptr<MockUsbDevice> device2;
   scoped_refptr<MockUsbDevice> device3;
 };
 
-TEST_F(SavedDevicesServiceTest, RegisterDevices) {
-  SavedDevicesService::SavedDevices* saved_devices =
-      service_->GetOrInsert(extension_->id());
+TEST_F(DevicePermissionsManagerTest, RegisterDevices) {
+  DevicePermissionsManager* manager =
+      DevicePermissionsManager::Get(env_.profile());
+  manager->AllowUsbDevice(
+      extension_->id(), device0, base::ASCIIToUTF16("ABCDE"));
+  manager->AllowUsbDevice(extension_->id(), device1, base::string16());
 
-  base::string16 serial_number(base::ASCIIToUTF16("ABCDE"));
-  saved_devices->RegisterDevice(device0, &serial_number);
-  saved_devices->RegisterDevice(device1, NULL);
+  scoped_ptr<DevicePermissions> device_permissions =
+      manager->GetForExtension(extension_->id());
+  ASSERT_TRUE(device_permissions->CheckUsbDevice(device0));
+  ASSERT_TRUE(device_permissions->CheckUsbDevice(device1));
+  ASSERT_FALSE(device_permissions->CheckUsbDevice(device2));
+  ASSERT_FALSE(device_permissions->CheckUsbDevice(device3));
 
-  // This is necessary as writing out registered devices happens in a task on
-  // the UI thread.
-  base::RunLoop run_loop;
-  run_loop.RunUntilIdle();
-
-  ASSERT_TRUE(saved_devices->IsRegistered(device0));
-  ASSERT_TRUE(saved_devices->IsRegistered(device1));
-  ASSERT_FALSE(saved_devices->IsRegistered(device2));
-  ASSERT_FALSE(saved_devices->IsRegistered(device3));
-
-  std::vector<SavedDeviceEntry> device_entries =
-      service_->GetAllDevices(extension_->id());
-  ASSERT_EQ(1U, device_entries.size());
-  ASSERT_EQ(base::ASCIIToUTF16("ABCDE"), device_entries[0].serial_number);
+  std::vector<base::string16> device_messages =
+      manager->GetPermissionMessageStrings(extension_->id());
+  ASSERT_EQ(1U, device_messages.size());
+  ASSERT_NE(device_messages[0].find(base::ASCIIToUTF16("ABCDE")),
+            base::string16::npos);
 
   device1->NotifyDisconnect();
 
-  ASSERT_TRUE(saved_devices->IsRegistered(device0));
-  ASSERT_FALSE(saved_devices->IsRegistered(device1));
-  ASSERT_FALSE(saved_devices->IsRegistered(device2));
-  ASSERT_FALSE(saved_devices->IsRegistered(device3));
+  device_permissions = manager->GetForExtension(extension_->id());
+  ASSERT_TRUE(device_permissions->CheckUsbDevice(device0));
+  ASSERT_FALSE(device_permissions->CheckUsbDevice(device1));
+  ASSERT_FALSE(device_permissions->CheckUsbDevice(device2));
+  ASSERT_FALSE(device_permissions->CheckUsbDevice(device3));
 
-  service_->Clear(extension_->id());
+  manager->Clear(extension_->id());
 
-  // App is normally restarted, clearing its reference to the SavedDevices.
-  saved_devices = service_->GetOrInsert(extension_->id());
-  ASSERT_FALSE(saved_devices->IsRegistered(device0));
-  device_entries = service_->GetAllDevices(extension_->id());
-  ASSERT_EQ(0U, device_entries.size());
+  device_permissions = manager->GetForExtension(extension_->id());
+  ASSERT_FALSE(device_permissions->CheckUsbDevice(device0));
+  device_messages = manager->GetPermissionMessageStrings(extension_->id());
+  ASSERT_EQ(0U, device_messages.size());
 }
 
-TEST_F(SavedDevicesServiceTest, LoadPrefs) {
+TEST_F(DevicePermissionsManagerTest, LoadPrefs) {
   scoped_ptr<base::Value> prefs_value = base::test::ParseJson(
       "["
       "  {"
@@ -142,12 +136,14 @@ TEST_F(SavedDevicesServiceTest, LoadPrefs) {
   env_.GetExtensionPrefs()->UpdateExtensionPref(
       extension_->id(), "devices", prefs_value.release());
 
-  SavedDevicesService::SavedDevices* saved_devices =
-      service_->GetOrInsert(extension_->id());
-  ASSERT_TRUE(saved_devices->IsRegistered(device0));
-  ASSERT_FALSE(saved_devices->IsRegistered(device1));
-  ASSERT_FALSE(saved_devices->IsRegistered(device2));
-  ASSERT_FALSE(saved_devices->IsRegistered(device3));
+  DevicePermissionsManager* manager =
+      DevicePermissionsManager::Get(env_.profile());
+  scoped_ptr<DevicePermissions> device_permissions =
+      manager->GetForExtension(extension_->id());
+  ASSERT_TRUE(device_permissions->CheckUsbDevice(device0));
+  ASSERT_FALSE(device_permissions->CheckUsbDevice(device1));
+  ASSERT_FALSE(device_permissions->CheckUsbDevice(device2));
+  ASSERT_FALSE(device_permissions->CheckUsbDevice(device3));
 }
 
-}  // namespace apps
+}  // namespace extensions
--- chrome/chrome_tests_unit.gypi
+++ chrome/chrome_tests_unit.gypi
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 {
   'variables': {
     'chrome_unit_tests_sources': [
-      '../apps/saved_devices_service_unittest.cc',
       '../apps/saved_files_service_unittest.cc',
       '../components/autofill/content/renderer/test_password_autofill_agent.cc',
       '../components/autofill/content/renderer/test_password_autofill_agent.h',
@@ -318,6 +317,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'browser/extensions/api/declarative_content/chrome_content_rules_registry_unittest.cc',
       'browser/extensions/api/declarative_webrequest/webrequest_action_unittest.cc',
       'browser/extensions/api/declarative_webrequest/webrequest_rules_registry_unittest.cc',
+      'browser/extensions/api/device_permissions_manager_unittest.cc',
       'browser/extensions/api/dial/dial_device_data_unittest.cc',
       'browser/extensions/api/dial/dial_registry_unittest.cc',
       'browser/extensions/api/dial/dial_service_unittest.cc',
