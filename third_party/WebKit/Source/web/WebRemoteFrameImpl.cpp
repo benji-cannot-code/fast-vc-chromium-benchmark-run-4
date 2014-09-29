81246538b87203939e5502b0c15dc0c67647805a
sigbjornf@opera.com <sigbjornf@opera.com>
2014-09-29 09:34:53 +0000

    Remove unused WebRemoteFrameImpl::viewImpl() helper.
    Added by r182337; an, as it turned out, unused helper that wasn't removed
    there.
    
    R=haraken,jochen
    BUG=340522
    
    Review URL: https://codereview.chromium.org/606663002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182818 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebRemoteFrameImpl.cpp
+++ third_party/WebKit/Source/web/WebRemoteFrameImpl.cpp
@@ -284,13 +284,6 @@ WebView* WebRemoteFrameImpl::view() const
     return WebViewImpl::fromPage(frame()->page());
 }
 
-WebViewImpl* WebRemoteFrameImpl::viewImpl() const
-{
-    if (!frame())
-        return 0;
-    return WebViewImpl::fromPage(frame()->page());
-}
-
 void WebRemoteFrameImpl::removeChild(WebFrame* frame)
 {
     WebFrame::removeChild(frame);
--- third_party/WebKit/Source/web/WebRemoteFrameImpl.h
+++ third_party/WebKit/Source/web/WebRemoteFrameImpl.h
@@ -193,8 +193,6 @@ private:
     RefPtrWillBeMember<RemoteFrame> m_frame;
     WebRemoteFrameClient* m_client;
 
-    WebViewImpl* viewImpl() const;
-
     WillBeHeapHashMap<WebFrame*, OwnPtrWillBeMember<FrameOwner> > m_ownersForChildren;
 
 #if ENABLE(OILPAN)
