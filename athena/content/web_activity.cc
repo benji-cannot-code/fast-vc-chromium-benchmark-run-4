65e5297716e5d725869d3431e6d296dd904ef461
skuhne <skuhne@chromium.org>
2014-09-29 09:51:52 -0700

    Fixing issues with new activity image readback
    While writing the unit tests I have found problems and fixed them. Since I am not sure when the unit tests will land however (they come with the new Athena Browser test framework) I created this patch to get a workable version for Sadrul's Overview mode work.
    
    BUG=408837
    TEST=tests are in different CL which introduces Browser Tests for Athena
    
    Review URL: https://codereview.chromium.org/611773002
    
    Cr-Commit-Position: refs/heads/master@{#297193}

--- athena/content/web_activity.cc
+++ athena/content/web_activity.cc
@@ -559,7 +559,7 @@ views::Widget* WebActivity::CreateWidget() {
 
 gfx::ImageSkia WebActivity::GetOverviewModeImage() {
   if (content_proxy_.get())
-    content_proxy_->GetContentImage();
+    return content_proxy_->GetContentImage();
   return gfx::ImageSkia();
 }
 
