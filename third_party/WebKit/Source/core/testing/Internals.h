ca9078ba4402a5fff46a35f2866ee654673a7caa
hayato@chromium.org <hayato@chromium.org>
2014-09-30 02:17:28 +0000

    Prevents a node in user agent shadow trees from be drag-and-dropped from outside of shadow trees.
    r178285 made drag and drop work for a node in shadow trees, however,
    it turned out that built-in elements, such as <input> elements, have a custom drag and drop behavior.
    This patch reverts the drag-and-drop capability only for a node in user agent shadow trees.
    
    BUG=417170
    TEST=new layout test and manual test for the case reported in the bug
    
    Review URL: https://codereview.chromium.org/608683002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182880 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/testing/Internals.h
+++ third_party/WebKit/Source/core/testing/Internals.h
@@ -96,6 +96,8 @@ public:
 
     PassRefPtrWillBeRawPtr<CSSStyleDeclaration> computedStyleIncludingVisitedInfo(Node*) const;
 
+    PassRefPtrWillBeRawPtr<ShadowRoot> createUserAgentShadowRoot(Element* host);
+
     ShadowRoot* shadowRoot(Element* host);
     ShadowRoot* youngestShadowRoot(Element* host);
     ShadowRoot* oldestShadowRoot(Element* host);
