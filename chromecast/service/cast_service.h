bb829035c43e9f191d92f7168d891f035a249bd6
gunsch <gunsch@chromium.org>
2014-09-29 23:43:12 -0700

    Chromecast: removes CastNetworkDelegate from CastService::Create.
    R=lcwu@chromium.org,maclellant@chromium.org
    BUG=None
    
    Review URL: https://codereview.chromium.org/613093002
    
    Cr-Commit-Position: refs/heads/master@{#297371}

--- chromecast/service/cast_service.h
+++ chromecast/service/cast_service.h
@@ -23,9 +23,6 @@ class URLRequestContextGetter;
 }
 
 namespace chromecast {
-namespace shell {
-class CastNetworkDelegate;
-}
 
 class CastService {
  public:
@@ -42,7 +39,6 @@ class CastService {
   static CastService* Create(
       content::BrowserContext* browser_context,
       net::URLRequestContextGetter* request_context_getter,
-      shell::CastNetworkDelegate* network_delegate,
       const OptInStatsChangedCallback& opt_in_stats_callback);
 
   virtual ~CastService();
