2b306c9f3ebeb78f7b7846f3c47060ef11a02283
lfg <lfg@chromium.org>
2014-09-30 16:16:18 -0700

    Porting more tests to app_shell_browsertests.
    The tests included in this CL are:
    TestExecuteScriptIsAbortedWhenWebViewSourceIsChanged
    TestFindAPI
    TestFindAPI_findupdate
    TestGetProcessId
    TestHiddenBeforeNavigation
    TestInlineScriptFromAccessibleResources
    TestInvalidChromeExtensionURL
    
    BUG=352293
    
    Review URL: https://codereview.chromium.org/615623002
    
    Cr-Commit-Position: refs/heads/master@{#297537}

---
+++ extensions/test/data/web_view/apitest/inject_hidden_test.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+window.addEventListener('message', function(e) {
+  var response = '';
+  var data = JSON.parse(e.data);
+
+  if (data[0] == 'visibilityState-request') {
+    response = document.visibilityState;
+  } else {
+    response = 'FAILED';
+  }
+
+  e.source.postMessage(
+      JSON.stringify(['visibilityState-response', response]), '*');
+});
