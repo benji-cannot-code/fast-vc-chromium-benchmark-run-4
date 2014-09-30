e37d4be0884ebf29202b8aed819416e7728d2396
hjd <hjd@chromium.org>
2014-09-30 11:58:53 -0700

    Fix the CommandLine for the WebView
    The CookieManager startup path needs access to the CommandLine
    before initialization.
    
    BUG=331424
    
    Review URL: https://codereview.chromium.org/617883003
    
    Cr-Commit-Position: refs/heads/master@{#297469}

--- android_webview/java/src/org/chromium/android_webview/AwBrowserProcess.java
+++ android_webview/java/src/org/chromium/android_webview/AwBrowserProcess.java
@@ -36,6 +36,10 @@ public abstract class AwBrowserProcess {
         } catch (ProcessInitException e) {
             throw new RuntimeException("Cannot load WebView", e);
         }
+        // Switch the command line implementation from Java to native.
+        // It's okay for the WebView to do this before initialization because we have
+        // setup the JNI bindings by this point.
+        LibraryLoader.switchCommandLineForWebView();
     }
 
     /**
