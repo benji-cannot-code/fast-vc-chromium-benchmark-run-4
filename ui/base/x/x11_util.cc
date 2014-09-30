ec5ce62c039bb677098a57671b8dde33076b830d
pkotwicz <pkotwicz@chromium.org>
2014-09-29 17:06:24 -0700

    Do not consider the wmii window manager as supporting _NET_ACTIVE_WINDOW. In particular, wmii indicates that it supports EWMH via _NET_SUPPORTING_WM_CHECK and _NET_ACTIVE_WINDOW via _NET_SUPPORTED but does not ever set the _NET_ACTIVE_WINDOW property. This is a known bug with wmii (https://code.google.com/p/wmii/issues/detail?id=266)
    BUG=417767
    TEST=Manual, see bug
    
    Review URL: https://codereview.chromium.org/602183003
    
    Cr-Commit-Position: refs/heads/master@{#297313}

--- ui/base/x/x11_util.cc
+++ ui/base/x/x11_util.cc
@@ -1033,7 +1033,8 @@ bool GetCustomFramePrefDefault() {
       wm == WM_NOTION ||
       wm == WM_QTILE ||
       wm == WM_RATPOISON ||
-      wm == WM_STUMPWM)
+      wm == WM_STUMPWM ||
+      wm == WM_WMII)
     return false;
 
   // Handle a few more window managers that don't get along well with custom
@@ -1260,6 +1261,8 @@ WindowManagerName GuessWindowManager() {
       return WM_RATPOISON;
     if (name == "stumpwm")
       return WM_STUMPWM;
+    if (name == "wmii")
+      return WM_WMII;
     if (name == "Xfwm4")
       return WM_XFWM4;
   }
