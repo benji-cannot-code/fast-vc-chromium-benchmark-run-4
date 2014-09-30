ebb3b9f9e7b7b1875c68605ab6231ae824e2c499
tapted <tapted@chromium.org>
2014-09-29 21:19:06 -0700

    Remove #ifdefs around ShowLinkDisambiguationPopup
    Current efforts are aimed at harmonizing our UI code. #ifdefs work
    against this and have a lot of downsides besides.
    
    This is a follow-up to http://crrev.com/351683002 -- a long-running CL
    which added ShowLinkDisambiguationPopup to Aura -- and
    http://crrev.com/596373002 which fixed some build problems with that.
    However, toolkit_views=1 use_aura=0 which is in use on Mac still has
    problems.
    
    This CL adds default method definitions to WebContentsViewDelegate and
    RenderWidgetHostViewBase. Essentially moving a NOTIMPLEMENTED() guarded
    by #ifdefs into a base class method, and removing some unnecessary
    boilerplate.
    
    BUG=385249, 399191
    
    Review URL: https://codereview.chromium.org/610893003
    
    Cr-Commit-Position: refs/heads/master@{#297360}

--- content/public/browser/web_contents_view_delegate.h
+++ content/public/browser/web_contents_view_delegate.h
@@ -10,9 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Cocoa/Cocoa.h>
 #endif
 
-#include "base/callback.h"
+#include "base/callback_forward.h"
 #include "content/common/content_export.h"
-#include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/native_widget_types.h"
 
 #if defined(__OBJC__)
@@ -20,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace gfx {
+class Rect;
 class Size;
 }
 
@@ -56,7 +56,6 @@ class CONTENT_EXPORT WebContentsViewDelegate {
   virtual void TakeFocus(bool reverse);
   virtual void SizeChanged(const gfx::Size& size);
 
-#if defined(TOOLKIT_VIEWS) || defined(USE_AURA)
   // Shows a popup window containing the |zoomed_bitmap| of web content with
   // more than one link, allowing the user to more easily select which link
   // they were trying to touch. |target_rect| is the rectangle in DIPs in the
@@ -69,12 +68,11 @@ class CONTENT_EXPORT WebContentsViewDelegate {
       const SkBitmap& zoomed_bitmap,
       const gfx::NativeView content,
       const base::Callback<void(ui::GestureEvent*)>& gesture_cb,
-      const base::Callback<void(ui::MouseEvent*)>& mouse_cb) = 0;
+      const base::Callback<void(ui::MouseEvent*)>& mouse_cb);
 
   // Hides the link disambiguation popup window if it is showing, otherwise does
   // nothing.
-  virtual void HideDisambiguationPopup() = 0;
-#endif
+  virtual void HideDisambiguationPopup();
 
   // Returns a newly-created delegate for the RenderWidgetHostViewMac, to handle
   // events on the responder chain.
