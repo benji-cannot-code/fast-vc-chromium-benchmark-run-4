e16667adfdd041f2c83a8cd1f8cd2224423cc095
caseq@chromium.org <caseq@chromium.org>
2014-09-30 19:55:42 +0000

    DevTools: use MainFrameNavigated, not DocumentUpdated to re-enable Tracing agent to avoid races with DOMModel loading document.
    BUG=
    
    Review URL: https://codereview.chromium.org/618803002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182969 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sdk/LayerTreeModel.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/LayerTreeModel.js
@@ -51,7 +51,7 @@ WebInspector.LayerTreeModel = function(target)
 {
     WebInspector.SDKModel.call(this, WebInspector.LayerTreeModel, target);
     target.registerLayerTreeDispatcher(new WebInspector.LayerTreeDispatcher(this));
-    target.domModel.addEventListener(WebInspector.DOMModel.Events.DocumentUpdated, this._onDocumentUpdated, this);
+    WebInspector.targetManager.addEventListener(WebInspector.TargetManager.Events.MainFrameNavigated, this._onMainFrameNavigated, this);
     /** @type {?WebInspector.LayerTreeBase} */
     this._layerTree = null;
 }
@@ -83,6 +83,11 @@ WebInspector.LayerTreeModel.prototype = {
         if (this._enabled)
             return;
         this._enabled = true;
+        this._forceEnable();
+    },
+
+    _forceEnable: function()
+    {
         this._layerTree = new WebInspector.AgentLayerTree(this.target());
         this._lastPaintRectByLayerId = {};
         this.target().layerTreeAgent().enable();
@@ -151,12 +156,10 @@ WebInspector.LayerTreeModel.prototype = {
         this.dispatchEventToListeners(WebInspector.LayerTreeModel.Events.LayerPainted, layer);
     },
 
-    _onDocumentUpdated: function()
+    _onMainFrameNavigated: function()
     {
-        if (!this._enabled)
-            return;
-        this.disable();
-        this.enable();
+        if (this._enabled)
+            this._forceEnable();
     },
 
     __proto__: WebInspector.SDKModel.prototype
