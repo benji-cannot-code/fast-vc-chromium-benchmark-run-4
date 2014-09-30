bbaf07dfd803fdac678acb019d8aa2489d3eaa6e
sbc <sbc@chromium.org>
2014-09-30 09:46:09 -0700

    [NaCl SDK] Update naclports revision used to build gonacl.com examples
    This fixed the build of lua and bullet which was
    broken by the recent change in PNaCl toolchain
    layout.
    
    Review URL: https://codereview.chromium.org/613233002
    
    Cr-Commit-Position: refs/heads/master@{#297436}

--- native_client_sdk/src/gonacl_appengine/src/lua/build.sh
+++ native_client_sdk/src/gonacl_appengine/src/lua/build.sh
@@ -6,14 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 set -o nounset
 set -o errexit
-set -o xtrace
 
 SCRIPT_DIR="$(cd $(dirname $0) && pwd)"
 cd ${SCRIPT_DIR}
 
 OUT_DIR=out
-NACLPORTS_URL=http://naclports.googlecode.com/svn/trunk/src
-NACLPORTS_REV=1290
+NACLPORTS_URL=https://chromium.googlesource.com/external/naclports.git
+NACLPORTS_REV=99f2417ab1a397735b20ccceaa58ffcc166f3fb1
 NACLPORTS_DIR=${OUT_DIR}/naclports
 
 if [ -z "${NACL_SDK_ROOT:-}" ]; then
@@ -38,24 +37,11 @@ LogExecute() {
   $*
 }
 
-Clone() {
-  local url=$1
-  local dir=$2
-  local sha=$3
-  if [ ! -d $dir ]; then
-    LogExecute git clone $url $dir
-  else
-    pushd $dir
-    LogExecute git fetch origin
-    popd
-  fi
-
-  pushd $dir
-  LogExecute git checkout $sha
-  popd
-}
-
 Banner Cloning naclports
+if [ -d ${NACLPORTS_DIR} -a ! -d ${NACLPORTS_DIR}/src/.git ]; then
+  rm -rf ${NACLPORTS_DIR}
+fi
+
 if [ ! -d ${NACLPORTS_DIR} ]; then
   mkdir -p ${NACLPORTS_DIR}
   pushd ${NACLPORTS_DIR}
