f880c648c1efc4a260a56b466bb3b0911f1091b7
wuchengli <wuchengli@chromium.org>
2014-09-30 10:39:56 -0700

    Add kEnableWebRtcHWVp8Encoding to GPU process.
    VideoEncodeAccelerator::GetSupportedProfiles now runs in GPU
    process because the profiles were added to GPUInfo. Add
    kEnableWebRtcHWVp8Encoding to GPU process to fix the missing
    encode profile.
    
    BUG=418762
    TEST=Run apprtc loopback and check the histogram.
    
    Review URL: https://codereview.chromium.org/618633002
    
    Cr-Commit-Position: refs/heads/master@{#297445}

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -81,6 +81,7 @@ static const char* const kSwitchNames[] = {
   switches::kDisableSeccompFilterSandbox,
 #if defined(ENABLE_WEBRTC)
   switches::kDisableWebRtcHWEncoding,
+  switches::kEnableWebRtcHWVp8Encoding,
 #endif
   switches::kEnableLogging,
   switches::kEnableShareGroupAsyncTextureUpload,
