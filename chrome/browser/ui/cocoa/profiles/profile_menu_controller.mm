3997048fd4841f7508f2e7a2a7de48b19b672942
noms <noms@chromium.org>
2014-09-30 07:53:52 -0700

    Remove unused User/Users strings from the profiles UI
    The strings stopped being used in https://codereview.chromium.org/611033002/
    
    XIB changes: use a different string ID for the profiles menu in the menu bar.
    
    BUG=405988
    
    Review URL: https://codereview.chromium.org/603023004
    
    Cr-Commit-Position: refs/heads/master@{#297422}

--- chrome/browser/ui/cocoa/profiles/profile_menu_controller.mm
+++ chrome/browser/ui/cocoa/profiles/profile_menu_controller.mm
@@ -71,7 +71,7 @@ class Observer : public chrome::BrowserListObserver,
     mainMenuItem_ = item;
 
     base::scoped_nsobject<NSMenu> menu([[NSMenu alloc] initWithTitle:
-        l10n_util::GetNSStringWithFixup(IDS_NEW_PROFILES_OPTIONS_GROUP_NAME)]);
+        l10n_util::GetNSStringWithFixup(IDS_PROFILES_OPTIONS_GROUP_NAME)]);
     [mainMenuItem_ setSubmenu:menu];
 
     // This object will be constructed as part of nib loading, which happens
@@ -112,7 +112,7 @@ class Observer : public chrome::BrowserListObserver,
 
   if (dock) {
     NSString* headerName =
-        l10n_util::GetNSStringWithFixup(IDS_NEW_PROFILES_OPTIONS_GROUP_NAME);
+        l10n_util::GetNSStringWithFixup(IDS_PROFILES_OPTIONS_GROUP_NAME);
     base::scoped_nsobject<NSMenuItem> header(
         [[NSMenuItem alloc] initWithTitle:headerName
                                    action:NULL
@@ -198,7 +198,7 @@ class Observer : public chrome::BrowserListObserver,
 
   [[self menu] addItem:[NSMenuItem separatorItem]];
   item = [self createItemWithTitle:l10n_util::GetNSStringWithFixup(
-      IDS_NEW_PROFILES_CREATE_NEW_PROFILE_OPTION)
+      IDS_PROFILES_CREATE_NEW_PROFILE_OPTION)
                             action:@selector(newProfile:)];
   [[self menu] addItem:item];
 
