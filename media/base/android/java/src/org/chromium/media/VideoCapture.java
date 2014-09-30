197fda27b8843ea0486bdd2054863f5652f1abd9
mcasas <mcasas@chromium.org>
2014-09-30 01:21:35 -0700

    Android Video Capture: Removed references to Android.Hardware.Camera from the factory and cleanup
    As part of the masterplan of the bug, this CL
    removes the explicit android.hardware.Camera
    classes manipulations, asking instead classes
    VideoCapture{,Android,Tango} for this info.
    
    Some other minor cleanup goes along with it:
    VideoCaptureFactory.CamParams is only used inside
    VideoCaptureTango and to retrieve the name of the
    special Tango camera, that's refactored to hide the
    CamParams inside VideoCaptureTango.
    
    Other methods are further simplified in the
    VideoCaptureFactory so their use from C++
    is more evident.
    
    BUG=418052
    
    Review URL: https://codereview.chromium.org/602063002
    
    Cr-Commit-Position: refs/heads/master@{#297380}

--- media/base/android/java/src/org/chromium/media/VideoCapture.java
+++ media/base/android/java/src/org/chromium/media/VideoCapture.java
@@ -78,6 +78,18 @@ public abstract class VideoCapture implements android.hardware.Camera.PreviewCal
     protected int mDeviceOrientation;
     private static final String TAG = "VideoCapture";
 
+    static android.hardware.Camera.CameraInfo getCameraInfo(int id) {
+        android.hardware.Camera.CameraInfo cameraInfo =
+                new android.hardware.Camera.CameraInfo();
+        try {
+            android.hardware.Camera.getCameraInfo(id, cameraInfo);
+        } catch (RuntimeException ex) {
+            Log.e(TAG, "getCameraInfo: Camera.getCameraInfo: " + ex);
+            return null;
+        }
+        return cameraInfo;
+    }
+
     VideoCapture(Context context,
                  int id,
                  long nativeVideoCaptureDeviceAndroid) {
@@ -97,7 +109,7 @@ public abstract class VideoCapture implements android.hardware.Camera.PreviewCal
             return false;
         }
 
-        android.hardware.Camera.CameraInfo cameraInfo = getCameraInfo(mId);
+        android.hardware.Camera.CameraInfo cameraInfo = VideoCapture.getCameraInfo(mId);
         if (cameraInfo == null) {
             mCamera.release();
             mCamera = null;
@@ -369,14 +381,4 @@ public abstract class VideoCapture implements android.hardware.Camera.PreviewCal
         return parameters;
     }
 
-    private android.hardware.Camera.CameraInfo getCameraInfo(int id) {
-        android.hardware.Camera.CameraInfo cameraInfo = new android.hardware.Camera.CameraInfo();
-        try {
-            android.hardware.Camera.getCameraInfo(id, cameraInfo);
-        } catch (RuntimeException ex) {
-            Log.e(TAG, "getCameraInfo: android.hardware.Camera.getCameraInfo: " + ex);
-            return null;
-        }
-        return cameraInfo;
-    }
 }
