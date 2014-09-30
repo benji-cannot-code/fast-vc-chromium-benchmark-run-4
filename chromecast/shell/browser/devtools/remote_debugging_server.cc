8b420d85e305b2f0acf7becc11f3b7e725f4201e
gunsch <gunsch@chromium.org>
2014-09-30 09:26:11 -0700

    Chromecast: Android devtools needs remote frontend URL.
    If not, this code blocks it from loading:
    https://code.google.com/p/chromium/codesearch#chromium/src/chrome/browser/ui/webui/devtools_ui.cc&sq=package:chromium&type=cs&rcl=1411534708&l=343
    
    R=dgozman@chromium.org,lcwu@chromium.org
    BUG=400876
    
    Review URL: https://codereview.chromium.org/607973004
    
    Cr-Commit-Position: refs/heads/master@{#297433}

--- chromecast/shell/browser/devtools/remote_debugging_server.cc
+++ chromecast/shell/browser/devtools/remote_debugging_server.cc
@@ -30,6 +30,10 @@ namespace shell {
 
 namespace {
 
+#if defined(OS_ANDROID)
+const char kFrontEndURL[] =
+    "http://chrome-devtools-frontend.appspot.com/serve_rev/%s/devtools.html";
+#endif  // defined(OS_ANDROID)
 const int kDefaultRemoteDebuggingPort = 9222;
 
 #if defined(OS_ANDROID)
@@ -86,6 +90,14 @@ CreateSocketFactory(int port) {
 #endif
 }
 
+std::string GetFrontendUrl() {
+#if defined(OS_ANDROID)
+  return base::StringPrintf(kFrontEndURL, content::GetWebKitRevision().c_str());
+#else
+  return std::string();
+#endif  // defined(OS_ANDROID)
+}
+
 }  // namespace
 
 RemoteDebuggingServer::RemoteDebuggingServer()
@@ -133,7 +145,7 @@ void RemoteDebuggingServer::OnPortChanged() {
   if (port_ > 0) {
     devtools_http_handler_ = content::DevToolsHttpHandler::Start(
         CreateSocketFactory(port_),
-        std::string(),
+        GetFrontendUrl(),
         new CastDevToolsDelegate(),
         base::FilePath());
     LOG(INFO) << "Devtools started: port=" << port_;
