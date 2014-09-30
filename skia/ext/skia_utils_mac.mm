26dbb73fdcaa4c913cf909bdccb69f6c5b50374b
ccameron <ccameron@chromium.org>
2014-09-30 12:59:16 -0700

    Fix pixeldated HiDPI Mac widgets with impl-side painting
    Allow SkiaBitLockers to specify that they should record a bitmap at
    a specified scale. Callers that know that they will be replayed at a
    given scale factor can specify the final scale during record.
    
    BUG=408557
    
    Review URL: https://codereview.chromium.org/611253004
    
    Cr-Commit-Position: refs/heads/master@{#297485}

--- skia/ext/skia_utils_mac.mm
+++ skia/ext/skia_utils_mac.mm
@@ -270,14 +270,18 @@ SkiaBitLocker::SkiaBitLocker(SkCanvas* canvas)
     : canvas_(canvas),
       userClipRectSpecified_(false),
       cgContext_(0),
+      bitmapScaleFactor_(1),
       useDeviceBits_(false),
       bitmapIsDummy_(false) {
 }
 
-SkiaBitLocker::SkiaBitLocker(SkCanvas* canvas, const SkIRect& userClipRect)
+SkiaBitLocker::SkiaBitLocker(SkCanvas* canvas,
+                             const SkIRect& userClipRect,
+                             SkScalar bitmapScaleFactor)
     : canvas_(canvas),
       userClipRectSpecified_(true),
       cgContext_(0),
+      bitmapScaleFactor_(bitmapScaleFactor),
       useDeviceBits_(false),
       bitmapIsDummy_(false) {
   canvas_->save();
@@ -383,8 +387,10 @@ void SkiaBitLocker::releaseIfNeeded() {
       return;
     canvas_->save();
     canvas_->concat(inverse);
-    canvas_->drawBitmap(subset, bounds.x() + bitmapOffset_.x(),
-                                bounds.y() + bitmapOffset_.y());
+    canvas_->translate(bounds.x() + bitmapOffset_.x(),
+                       bounds.y() + bitmapOffset_.y());
+    canvas_->scale(1.f / bitmapScaleFactor_, 1.f / bitmapScaleFactor_);
+    canvas_->drawBitmap(subset, 0, 0);
     canvas_->restore();
   }
   CGContextRelease(cgContext_);
@@ -432,7 +438,8 @@ CGContextRef SkiaBitLocker::cgContext() {
     bitmap_.lockPixels();
   } else {
     bool result = bitmap_.tryAllocN32Pixels(
-        clip_bounds.width(), clip_bounds.height());
+        SkScalarCeilToInt(bitmapScaleFactor_ * clip_bounds.width()),
+        SkScalarCeilToInt(bitmapScaleFactor_ * clip_bounds.height()));
     DCHECK(result);
     if (!result)
       return 0;
@@ -448,7 +455,7 @@ CGContextRef SkiaBitLocker::cgContext() {
   SkMatrix matrix = canvas_->getTotalMatrix();
   matrix.postTranslate(-SkIntToScalar(bitmapOffset_.x()),
                        -SkIntToScalar(bitmapOffset_.y()));
-  matrix.postScale(1, -1);
+  matrix.postScale(bitmapScaleFactor_, -bitmapScaleFactor_);
   matrix.postTranslate(0, SkIntToScalar(bitmap_.height()));
 
   CGContextConcatCTM(cgContext_, SkMatrixToCGAffineTransform(matrix));
