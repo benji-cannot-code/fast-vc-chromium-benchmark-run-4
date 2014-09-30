c443a2a0f04416740c9d2c0cd794fd5ec47f6861
flackr <flackr@chromium.org>
2014-09-30 11:19:00 -0700

    Defer maximize mode bounds updates until after exiting overview.
    When we enter overview the shelf is revealed which causes a workspace bounds
    change. Normally the maximize mode window manager would update all active
    windows to have the new bounds but this effectively undoes the transformation
    applied to the window to show it in overview. As such, we defer updating the
    bounds until we exit overview mode.
    
    BUG=401664
    TEST=WindowSelectorTest.FullscreenWindowMaximizeMode, WindowAnimationsTest.CrossFadeToBoundsFromTransform
    TEST=Repro steps in http://crbug.com/401664
    
    Review URL: https://codereview.chromium.org/588193003
    
    Cr-Commit-Position: refs/heads/master@{#297455}

--- ash/wm/maximize_mode/maximize_mode_window_manager.cc
+++ ash/wm/maximize_mode/maximize_mode_window_manager.cc
@@ -82,6 +82,7 @@ void MaximizeModeWindowManager::OnOverviewModeStarting() {
     return;
 
   EnableBackdropBehindTopWindowOnEachDisplay(false);
+  SetDeferBoundsUpdates(true);
   backdrops_hidden_ = true;
 }
 
@@ -91,6 +92,7 @@ void MaximizeModeWindowManager::OnOverviewModeEnding() {
 
   backdrops_hidden_ = false;
   EnableBackdropBehindTopWindowOnEachDisplay(true);
+  SetDeferBoundsUpdates(false);
 }
 
 void MaximizeModeWindowManager::OnWindowDestroying(aura::Window* window) {
@@ -193,6 +195,15 @@ void MaximizeModeWindowManager::MaximizeAllWindows() {
 void MaximizeModeWindowManager::RestoreAllWindows() {
   while (window_state_map_.size())
     ForgetWindow(window_state_map_.begin()->first);
+}
+
+void MaximizeModeWindowManager::SetDeferBoundsUpdates(
+    bool defer_bounds_updates) {
+  for (WindowToState::iterator it = window_state_map_.begin();
+       it != window_state_map_.end();
+       ++it) {
+    it->second->SetDeferBoundsUpdates(defer_bounds_updates);
+  }
 }
 
 void MaximizeModeWindowManager::MaximizeAndTrackWindow(
