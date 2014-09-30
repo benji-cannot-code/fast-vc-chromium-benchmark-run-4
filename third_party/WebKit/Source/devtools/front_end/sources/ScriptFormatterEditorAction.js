2a1cbfc39b1e26f3bd80560d59254154849c4558
vsevik@chromium.org <vsevik@chromium.org>
2014-09-30 10:07:12 +0000

    DevTools: Use correct originURL for pretty-printed scripts.
    BUG=402245
    R=sergeyv, lushnikov
    
    Review URL: https://codereview.chromium.org/537503002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182902 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sources/ScriptFormatterEditorAction.js
+++ third_party/WebKit/Source/devtools/front_end/sources/ScriptFormatterEditorAction.js
@@ -122,7 +122,7 @@ WebInspector.FormatterProjectDelegate.prototype = {
     _addFormatted: function(name, sourceURL, contentType, content)
     {
         var contentProvider = new WebInspector.StaticContentProvider(contentType, content);
-        return this.addContentProvider(sourceURL, name + ":formatted", "deobfuscated:" + sourceURL, contentProvider);
+        return this.addContentProvider(sourceURL, name + ":formatted", sourceURL, "deobfuscated:" + sourceURL, contentProvider);
     },
 
     /**
