f3d97f9ae7e66f3579a5c4f09b9284efd9ba6364
shuchen <shuchen@chromium.org>
2014-09-29 20:50:03 -0700

    Move all white-listed input method indicator text from InputMethodUtil to manifests of IME extensions.
    BUG=232260
    TEST=Verified on linux_chromeos.
    
    Review URL: https://codereview.chromium.org/608183002
    
    Cr-Commit-Position: refs/heads/master@{#297357}

--- chromeos/ime/input_method_descriptor.cc
+++ chromeos/ime/input_method_descriptor.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/strings/string_split.h"
+#include "base/strings/string_util.h"
+#include "chromeos/ime/extension_ime_util.h"
 #include "url/gurl.h"
 
 namespace chromeos {
@@ -37,6 +39,18 @@ std::string InputMethodDescriptor::GetPreferredKeyboardLayout() const {
   // TODO(nona): Investigate better way to guess the preferred layout
   //             http://crbug.com/170601.
   return keyboard_layouts_.empty() ? "us" : keyboard_layouts_[0];
+}
+
+std::string InputMethodDescriptor::GetIndicator() const {
+  // If indicator is empty, use the first two character in its preferred
+  // keyboard layout or language code.
+  if (indicator_.empty()) {
+    if (extension_ime_util::IsKeyboardLayoutExtension(id_))
+      return StringToUpperASCII(GetPreferredKeyboardLayout().substr(0, 2));
+    DCHECK(language_codes_.size() > 0);
+    return StringToUpperASCII(language_codes_[0].substr(0, 2));
+  }
+  return indicator_;
 }
 
 InputMethodDescriptor::InputMethodDescriptor() {
