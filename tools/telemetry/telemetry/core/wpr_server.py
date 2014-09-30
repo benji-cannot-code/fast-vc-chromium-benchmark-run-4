b3661cbd9cfe09f383df12a7df3458d07ecd234a
slamm <slamm@chromium.org>
2014-09-30 13:29:38 -0700

    Add wpr_modes.WPR_APPEND.
    BUG=404771
    
    Review URL: https://codereview.chromium.org/616923002
    
    Cr-Commit-Position: refs/heads/master@{#297491}

--- tools/telemetry/telemetry/core/wpr_server.py
+++ tools/telemetry/telemetry/core/wpr_server.py
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 from telemetry.core import forwarders
 from telemetry.core import webpagereplay
+from telemetry.core import wpr_modes
 
 
 # TODO(tonyg): Move webpagereplay.py's guts into this class and
@@ -12,20 +13,17 @@ from telemetry.core import webpagereplay
 
 
 class ReplayServer(object):
-  def __init__(self, browser_backend, path, is_record_mode, is_append_mode,
+  def __init__(self, browser_backend, path, wpr_mode,
                make_javascript_deterministic):
     self._browser_backend = browser_backend
     self._forwarder = None
     self._web_page_replay = None
-    self._is_record_mode = is_record_mode
-    self._is_append_mode = is_append_mode
 
     wpr_args = browser_backend.browser_options.extra_wpr_args
-    if self._is_record_mode:
-      if self._is_append_mode:
-        wpr_args.append('--append')
-      else:
-        wpr_args.append('--record')
+    if wpr_mode == wpr_modes.WPR_APPEND:
+      wpr_args.append('--append')
+    elif wpr_mode == wpr_modes.WPR_RECORD:
+      wpr_args.append('--record')
     if not make_javascript_deterministic:
       wpr_args.append('--inject_scripts=')
     browser_backend.AddReplayServerOptions(wpr_args)
