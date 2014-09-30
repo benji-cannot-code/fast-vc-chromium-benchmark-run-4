1b13ab8cea1b06ba41640ae6313271c6d1b8d802
shreeram.k@samsung.com <shreeram.k@samsung.com>
2014-09-30 11:30:44 +0000

    Expose IA and IA2 role and state for HTML input type attributes related to date and time.(re-land)
    For input type date, datetime and datetime-local, time, month, week - IA and IA2 role and states
    are not exposed properly.
    
    BUG=418486
    
    Review URL: https://codereview.chromium.org/615773002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182911 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/accessibility/AXNodeObject.cpp
+++ third_party/WebKit/Source/core/accessibility/AXNodeObject.cpp
@@ -213,6 +213,13 @@ AccessibilityRole AXNodeObject::determineAccessibilityRole()
                 return CheckBoxMenuItemRole;
             return CheckBoxRole;
         }
+        if (type == InputTypeNames::date)
+            return DateRole;
+        if (type == InputTypeNames::datetime
+            || type == InputTypeNames::datetime_local
+            || type == InputTypeNames::month
+            || type == InputTypeNames::week)
+            return DateTimeRole;
         if (type == InputTypeNames::radio)
             return RadioButtonRole;
         if (input.isTextButton())
@@ -221,6 +228,8 @@ AccessibilityRole AXNodeObject::determineAccessibilityRole()
             return SliderRole;
         if (type == InputTypeNames::color)
             return ColorWellRole;
+        if (type == InputTypeNames::time)
+            return TimeRole;
         return TextFieldRole;
     }
     if (isHTMLSelectElement(*node())) {
