15dc57a790107824181627cf3450e3e21729f938
jdarpinian <jdarpinian@chromium.org>
2014-09-29 18:36:52 -0700

    Increase device orientation event frequency to 60 Hz to match requestAnimationFrame rate on most hardware. Change constants to microseconds for increased precision.
    BUG=413327
    
    Review URL: https://codereview.chromium.org/604483003
    
    Cr-Commit-Position: refs/heads/master@{#297332}

--- content/browser/device_sensors/inertial_sensor_consts.h
+++ content/browser/device_sensors/inertial_sensor_consts.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_DEVICE_SENSORS_INERTIAL_SENSOR_CONSTS_H_
 #define CONTENT_BROWSER_DEVICE_SENSORS_INERTIAL_SENSOR_CONSTS_H_
 
+#include "base/time/time.h"
+
 namespace content {
 
 // Constants related to the Device {Motion|Orientation|Light} APIs.
@@ -16,14 +18,19 @@ enum ConsumerType {
   CONSUMER_TYPE_LIGHT = 1 << 2,
 };
 
-// Specifies the minimal interval between subsequent sensor data updates.
+// Specifies the sampling rate for sensor data updates.
 // Note that when changing this value it is desirable to have an adequate
-// matching value |DeviceSensorEventPump::kDefaultPumpDelayMillis| in
-// content/renderer/device_sensors/device_sensor_event_pump.cc.
-const int kInertialSensorIntervalMillis = 50;
-// Corresponding |kDefaultLightPumpDelayMillis| is in
+// matching value |DeviceSensorEventPump::kDefaultPumpFrequencyHz| in
+// content/renderer/device_orientation/device_sensor_event_pump.cc.
+const int kInertialSensorSamplingRateHz = 60;
+const int kInertialSensorIntervalMicroseconds =
+    base::Time::kMicrosecondsPerSecond / kInertialSensorSamplingRateHz;
+
+// Corresponding |kDefaultLightPumpFrequencyHz| is in
 // content/renderer/device_sensors/device_light_event_pump.cc.
-const int kLightSensorIntervalMillis = 200;
+const int kLightSensorSamplingRateHz = 5;
+const int kLightSensorIntervalMicroseconds =
+    base::Time::kMicrosecondsPerSecond / kLightSensorSamplingRateHz;
 
 }  // namespace content
 
--- content/renderer/device_sensors/device_light_event_pump.cc
+++ content/renderer/device_sensors/device_light_event_pump.cc
@@ -5,13 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/renderer/device_sensors/device_light_event_pump.h"
 
+#include "base/time/time.h"
 #include "content/common/device_sensors/device_light_messages.h"
 #include "content/public/renderer/render_thread.h"
 #include "third_party/WebKit/public/platform/WebDeviceLightListener.h"
 
 namespace {
-// Default delay between subsequent firing of DeviceLight events.
-const int kDefaultLightPumpDelayMillis = 200;
+// Default rate for firing of DeviceLight events.
+const int kDefaultLightPumpFrequencyHz = 5;
+const int kDefaultLightPumpDelayMicroseconds =
+    base::Time::kMicrosecondsPerSecond / kDefaultLightPumpFrequencyHz;
 }  // namespace
 
 namespace content {
@@ -19,7 +22,7 @@ namespace content {
 DeviceLightEventPump::DeviceLightEventPump(RenderThread* thread)
     : DeviceSensorEventPump<blink::WebDeviceLightListener>(thread),
       last_seen_data_(-1) {
-  pump_delay_millis_ = kDefaultLightPumpDelayMillis;
+  pump_delay_microseconds_ = kDefaultLightPumpDelayMicroseconds;
 }
 
 DeviceLightEventPump::~DeviceLightEventPump() {
