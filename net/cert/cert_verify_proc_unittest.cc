57ac3805346344d5497ceb7ac4cddab5ea572341
rsleevi <rsleevi@chromium.org>
2014-09-29 18:28:01 -0700

    Mark SHA-1 as deprecated
    BUG=401365
    
    Review URL: https://codereview.chromium.org/508823009
    
    Cr-Commit-Position: refs/heads/master@{#297331}

--- net/cert/cert_verify_proc_unittest.cc
+++ net/cert/cert_verify_proc_unittest.cc
@@ -645,7 +645,7 @@ TEST_F(CertVerifyProcTest, TestKnownRoot) {
                      empty_cert_list_,
                      &verify_result);
   EXPECT_EQ(OK, error);
-  EXPECT_EQ(0U, verify_result.cert_status);
+  EXPECT_EQ(CERT_STATUS_SHA1_SIGNATURE_PRESENT, verify_result.cert_status);
   EXPECT_TRUE(verify_result.is_issued_by_known_root);
 }
 
@@ -679,7 +679,7 @@ TEST_F(CertVerifyProcTest, PublicKeyHashes) {
                      empty_cert_list_,
                      &verify_result);
   EXPECT_EQ(OK, error);
-  EXPECT_EQ(0U, verify_result.cert_status);
+  EXPECT_EQ(CERT_STATUS_SHA1_SIGNATURE_PRESENT, verify_result.cert_status);
   ASSERT_LE(2U, verify_result.public_key_hashes.size());
 
   HashValueVector sha1_hashes;
@@ -1077,7 +1077,7 @@ TEST_F(CertVerifyProcTest, CybertrustGTERoot) {
                      empty_cert_list_,
                      &verify_result);
   EXPECT_EQ(OK, error);
-  EXPECT_EQ(0U, verify_result.cert_status);
+  EXPECT_EQ(CERT_STATUS_SHA1_SIGNATURE_PRESENT, verify_result.cert_status);
 
   // Attempt to verify with the first known cross-certified intermediate
   // provided.
@@ -1100,7 +1100,7 @@ TEST_F(CertVerifyProcTest, CybertrustGTERoot) {
                  empty_cert_list_,
                  &verify_result);
   EXPECT_EQ(OK, error);
-  EXPECT_EQ(0U, verify_result.cert_status);
+  EXPECT_EQ(CERT_STATUS_SHA1_SIGNATURE_PRESENT, verify_result.cert_status);
 
   // Attempt to verify with the second known cross-certified intermediate
   // provided.
@@ -1123,7 +1123,7 @@ TEST_F(CertVerifyProcTest, CybertrustGTERoot) {
                  empty_cert_list_,
                  &verify_result);
   EXPECT_EQ(OK, error);
-  EXPECT_EQ(0U, verify_result.cert_status);
+  EXPECT_EQ(CERT_STATUS_SHA1_SIGNATURE_PRESENT, verify_result.cert_status);
 
   // Attempt to verify when both a cross-certified intermediate AND
   // the legacy GTE root are provided.
@@ -1143,7 +1143,7 @@ TEST_F(CertVerifyProcTest, CybertrustGTERoot) {
                  empty_cert_list_,
                  &verify_result);
   EXPECT_EQ(OK, error);
-  EXPECT_EQ(0U, verify_result.cert_status);
+  EXPECT_EQ(CERT_STATUS_SHA1_SIGNATURE_PRESENT, verify_result.cert_status);
 
   TestRootCerts::GetInstance()->Clear();
   EXPECT_TRUE(TestRootCerts::GetInstance()->IsEmpty());
@@ -1241,7 +1241,7 @@ TEST_F(CertVerifyProcTest, CRLSetLeafSerial) {
                      empty_cert_list_,
                      &verify_result);
   EXPECT_EQ(OK, error);
-  EXPECT_EQ(0U, verify_result.cert_status);
+  EXPECT_EQ(CERT_STATUS_SHA1_SIGNATURE_PRESENT, verify_result.cert_status);
 
   // Test revocation by serial number of a certificate not under the root.
   scoped_refptr<CRLSet> crl_set;
