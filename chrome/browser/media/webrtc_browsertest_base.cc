ded52a720e14d1c3be79646599df95ee0a0fe905
phoglund <phoglund@chromium.org>
2014-10-01 09:41:45 -0700

    No longer requiring pixel output for WebRTC tests.
    We used to require pixel output for our canvas-scraping, but that
    appears to no longer be required. This speeds up the test 3x on my
    win machine, from 30 seconds to about 10.
    
    The content_browsertests on ChromeOS will still enable pixel
    output since it seems the requirements there are different - video
    tags will not work unless that is enabled, it seems. I think
    that's fine since our original problem was with the browser_tests
    anyway, and I think the content_browsertests are probably fast
    enough on ChromeOS anyway (whereas the browser_tests weren't).
    
    BUG=396505
    
    Review URL: https://codereview.chromium.org/611953004
    
    Cr-Commit-Position: refs/heads/master@{#297654}

--- chrome/browser/media/webrtc_browsertest_base.cc
+++ chrome/browser/media/webrtc_browsertest_base.cc
@@ -83,8 +83,6 @@ WebRtcTestBase::WebRtcTestBase(): detect_errors_in_javascript_(false) {
   // set operation is idempotent.
   logging::SetLogMessageHandler(&JavascriptErrorDetectingLogHandler);
   hit_javascript_errors_.Get() = false;
-
-  EnablePixelOutput();
 }
 
 WebRtcTestBase::~WebRtcTestBase() {
