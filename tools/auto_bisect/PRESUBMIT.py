8ac66777d847bf7020e3c64c4b40de7c5840641a
qyearsley <qyearsley@chromium.org>
2014-10-01 10:29:03 -0700

    Add a module to fetch builds from different types of builders.
    This module and its contents will replace the following functions and methods
    in bisect-perf-regression.py:
      Most of DownloadCurrentBuild
      GetBuildArchiveForRevision
      GetRemoteBuildPath
      GetGSRootFolderName
      GetZipFileName
      FetchFromCloudStorage
      ExtractZip
      MaybeMakeDirectory
    
    In this module, we want to be able to support downloading from different
    places with possibly different naming conventions in a clean way.
    
    As-is, this module works for downloading and extracting full builds that are
    archived by the builders on the main waterfall. Example invocation:
    
    $ python fetch_build.py full 9f11d3bc7c31cc4c10efa15e4bfddf12705908ed outdir
    
    BUG=402669
    
    Review URL: https://codereview.chromium.org/548233002
    
    Cr-Commit-Position: refs/heads/master@{#297669}

--- tools/auto_bisect/PRESUBMIT.py
+++ tools/auto_bisect/PRESUBMIT.py
@@ -19,6 +19,7 @@ CONFIG_FILES = [
     'run-perf-test.cfg'
 ]
 
+
 def CheckChangeOnUpload(input_api, output_api):
   return _CommonChecks(input_api, output_api)
 
@@ -89,6 +90,10 @@ def _RunPyLint(input_api, output_api):
   """Runs unit tests for auto-bisect."""
   telemetry_path = os.path.join(
       input_api.PresubmitLocalPath(), os.path.pardir, 'telemetry')
+  mock_path = os.path.join(
+      input_api.PresubmitLocalPath(),os.path.pardir, os.path.pardir,
+      'third_party', 'pymock')
   tests = input_api.canned_checks.GetPylint(
-      input_api, output_api, extra_paths_list=[telemetry_path])
+      input_api, output_api, extra_paths_list=[telemetry_path, mock_path])
   return input_api.RunTests(tests)
+
