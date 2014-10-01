2a58ca908322eca7f3f188ce331ac12f4e36cee3
lushnikov@chromium.org <lushnikov@chromium.org>
2014-10-01 10:48:47 +0000

    DevTools: fix flakiness of some inspector/elements/styles tests
    The flakiness was implied by interference of throttled process applyStyleText
    and committing of a new style rule. Helper method relied on
    InspectorTest.runAfterPendingDispatches method, which in this case turned
    out to be invalid.
    
    BUG=416846, 418483
    R=pfeldman, apavlov
    
    Review URL: https://codereview.chromium.org/617473002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183047 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/inspector/elements-test.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/elements-test.js
@@ -757,7 +757,7 @@ function onBlankSection(selector, callback)
     if (typeof selector === "string")
         section._selectorElement.textContent = selector;
     section._selectorElement.dispatchEvent(InspectorTest.createKeyEvent("Enter"));
-    InspectorTest.runAfterPendingDispatches(callback.bind(null, section));
+    InspectorTest.addSniffer(WebInspector.BlankStylePropertiesSection.prototype, "_finishedAddingRuleForTest", callback.bind(null, section));
 }
 
 InspectorTest.dumpInspectorHighlight = function(node, callback)
