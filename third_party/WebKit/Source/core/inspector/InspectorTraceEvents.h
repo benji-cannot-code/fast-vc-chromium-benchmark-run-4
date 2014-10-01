d3768e35ec9625d7e02baccb1554485c21776400
kouhei@chromium.org <kouhei@chromium.org>
2014-10-01 13:23:37 +0000

    [Invalidation Tracking] add TraceEvents for setNeedsStyleRecalc.
    Depends on http://crrev.com/547823002 .
    
    This CL introduces a devtools.timeline TraceEvent for Node::setNeedsStyleRecalc.
    
    The TraceEvent is going to be used in devtools to track style invalidation reasons.
    Please see the bug for more info.
    
    BUG=410701
    
    Review URL: https://codereview.chromium.org/600733005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183059 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorTraceEvents.h
+++ third_party/WebKit/Source/core/inspector/InspectorTraceEvents.h
@@ -21,13 +21,15 @@ class GraphicsLayer;
 class KURL;
 class LayoutRect;
 class LocalFrame;
+class Node;
 class RenderImage;
 class RenderLayer;
 class RenderObject;
 class ResourceRequest;
 class ResourceResponse;
-class ScriptSourceCode;
 class ScriptCallStack;
+class ScriptSourceCode;
+class StyleChangeReasonForTracing;
 class WorkerThread;
 class XMLHttpRequest;
 
@@ -35,6 +37,11 @@ class InspectorLayoutEvent {
 public:
     static PassRefPtr<TraceEvent::ConvertableToTraceFormat> beginData(FrameView*);
     static PassRefPtr<TraceEvent::ConvertableToTraceFormat> endData(RenderObject* rootForThisLayout);
+};
+
+class InspectorStyleRecalcInvalidationTrackingEvent {
+public:
+    static PassRefPtr<TraceEvent::ConvertableToTraceFormat> data(Node*, const StyleChangeReasonForTracing&);
 };
 
 class InspectorLayoutInvalidationTrackingEvent {
