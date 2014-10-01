ea77654dc45f1edc3d0ddf5ce10fe37cd7bd437e
yurys@chromium.org <yurys@chromium.org>
2014-10-01 11:59:50 +0000

    DevTools: show aggregated stats for async trace events in Timeline
    When async trace event is selected its details section is populated with aggregated stats for corresponding range. Self time of async event is always 0.
    
    Each async event with phase "S" (i.e. begin) now has endTime as well. End time is equal to the timestamp of corresponding "F" (i.e. end) event.
    
    BUG=405502
    
    Review URL: https://codereview.chromium.org/619613006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183052 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/timeline/TracingModel.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TracingModel.js
@@ -366,6 +366,22 @@ WebInspector.TracingModel.Event.prototype = {
     },
 
     /**
+     * @return {boolean}
+     */
+    isAsync: function()
+    {
+        var phase = WebInspector.TracingModel.Phase;
+        switch (this.phase) {
+        case phase.AsyncBegin:
+        case phase.AsyncEnd:
+        case phase.AsyncStepInto:
+        case phase.AsyncStepPast:
+            return true;
+        }
+        return false;
+    },
+
+    /**
      * @param {!WebInspector.TracingManager.EventPayload} payload
      */
     _complete: function(payload)
@@ -596,9 +612,9 @@ WebInspector.TracingModel.Process.prototype = {
     },
 
     /**
-     * @param {!number} lastEventTime
+     * @param {!number} lastEventTimeMs
      */
-    _tracingComplete: function(lastEventTime)
+    _tracingComplete: function(lastEventTimeMs)
     {
         /**
          * @param {!WebInspector.TracingManager.EventPayload} a
@@ -614,8 +630,9 @@ WebInspector.TracingModel.Process.prototype = {
             if (!steps)
                 continue;
             var startEvent = steps[0];
-            var syntheticEndEvent = new WebInspector.TracingModel.Event(startEvent.category, startEvent.name, WebInspector.TracingModel.Phase.AsyncEnd, lastEventTime, startEvent.thread);
+            var syntheticEndEvent = new WebInspector.TracingModel.Event(startEvent.category, startEvent.name, WebInspector.TracingModel.Phase.AsyncEnd, lastEventTimeMs, startEvent.thread);
             steps.push(syntheticEndEvent);
+            startEvent.setEndTime(lastEventTimeMs)
         }
         this._asyncEvents = [];
         this._openAsyncEvents = [];
@@ -649,6 +666,7 @@ WebInspector.TracingModel.Process.prototype = {
         var newEvent = WebInspector.TracingModel.Event.fromPayload(payload, thread);
         if (payload.ph === phase.AsyncEnd) {
             steps.push(newEvent);
+            steps[0].setEndTime(timestamp);
             delete this._openAsyncEvents[key];
         } else if (payload.ph === phase.AsyncStepInto || payload.ph === phase.AsyncStepPast) {
             var lastPhase = steps.peekLast().phase;
