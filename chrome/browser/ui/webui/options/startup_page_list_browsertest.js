7a79a1321996f9091840b8953a8be5a1f2063916
Julian Pastarmov <pastarmovj@chromium.org>
2014-10-01 16:04:35 +0200

    Disabled broken, tests.
    BUG=419370
    TEST=bots are green again.
    TBR=pastarmovj@chromium.org
    TBR=vitalyp@chromium.org
    
    Review URL: https://codereview.chromium.org/614373002
    
    Cr-Commit-Position: refs/heads/master@{#297639}

--- chrome/browser/ui/webui/options/startup_page_list_browsertest.js
+++ chrome/browser/ui/webui/options/startup_page_list_browsertest.js
@@ -123,7 +123,9 @@ function createMouseEvent(type) {
   return event;
 }
 
-TEST_F('StartupPageListWebUITest', 'testDropFromOutsideSource', function() {
+// Disabled due to: crbug.com/419370
+TEST_F('StartupPageListWebUITest', 'DISABLED_testDropFromOutsideSource',
+       function() {
   /** @const */ var NEW_PAGE = 'http://google.com';
 
   var mockDropEvent = createMouseEvent('drop');
@@ -136,7 +138,8 @@ TEST_F('StartupPageListWebUITest', 'testDropFromOutsideSource', function() {
   expectTrue(mockDropEvent.defaultPrevented);
 });
 
-TEST_F('StartupPageListWebUITest', 'testDropToReorder', function() {
+// Disabled due to: crbug.com/419370
+TEST_F('StartupPageListWebUITest', 'DISABLED_testDropToReorder', function() {
   // TODO(dbeam): mock4js doesn't handle complex arguments well. Fix this.
   this.mockHandler.expects(once()).dragDropStartupPage([0, [1].join()]);
 
