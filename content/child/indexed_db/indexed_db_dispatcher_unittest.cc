3172e0c36215035217b117fa27d9b2bd54222680
jsbell <jsbell@chromium.org>
2014-10-01 11:32:50 -0700

    Fix unit test memory leaks following blink r183043
    Garbage collection was enabled on the blink side for DOMError objects,
    which were created by the IndexedDBDispatcher tests. Add in the
    appropriate GC calls at test tear-down.
    
    BUG=419418
    R=haraken@chromium.org,cmumford@chromium.org
    
    Review URL: https://codereview.chromium.org/621683003
    
    Cr-Commit-Position: refs/heads/master@{#297677}

--- content/child/indexed_db/indexed_db_dispatcher_unittest.cc
+++ content/child/indexed_db/indexed_db_dispatcher_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/platform/WebBlobInfo.h"
 #include "third_party/WebKit/public/platform/WebData.h"
 #include "third_party/WebKit/public/platform/WebIDBCallbacks.h"
+#include "third_party/WebKit/public/web/WebHeap.h"
 
 using blink::WebBlobInfo;
 using blink::WebData;
@@ -67,6 +68,10 @@ class IndexedDBDispatcherTest : public testing::Test {
         sync_message_filter_(new IPC::SyncMessageFilter(NULL)),
         thread_safe_sender_(new ThreadSafeSender(message_loop_proxy_.get(),
                                                  sync_message_filter_.get())) {}
+
+  virtual void TearDown() override {
+    blink::WebHeap::collectAllGarbageForTesting();
+  }
 
  protected:
   scoped_refptr<base::MessageLoopProxy> message_loop_proxy_;
