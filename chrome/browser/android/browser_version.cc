0d9e8de05cf8b7414424b6c20d4c01358c27a269
newt <newt@chromium.org>
2014-09-30 17:05:49 -0700

    Remove ToS strings on Android.
    The terms of service are now loaded from a URL instead of being packaged
    in the APK.
    
    BUG=416002
    
    Review URL: https://codereview.chromium.org/619773002
    
    Cr-Commit-Position: refs/heads/master@{#297554}

--- chrome/browser/android/browser_version.cc
+++ chrome/browser/android/browser_version.cc
@@ -5,21 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/android/browser_version.h"
 
-#include "base/android/jni_string.h"
 #include "chrome/common/chrome_version_info.h"
-#include "chrome/grit/locale_settings.h"
 #include "jni/BrowserVersion_jni.h"
-#include "ui/base/l10n/l10n_util.h"
-
-using base::android::ConvertUTF8ToJavaString;
 
 static jboolean IsOfficialBuild(JNIEnv* env, jclass /* clazz */) {
   return chrome::VersionInfo().IsOfficialBuild();
-}
-
-static jstring GetTermsOfServiceHtml(JNIEnv* env, jclass /* clazz */) {
-  return ConvertUTF8ToJavaString(env,
-      l10n_util::GetStringUTF8(IDS_TERMS_HTML)).Release();
 }
 
 bool RegisterBrowserVersion(JNIEnv* env) {
