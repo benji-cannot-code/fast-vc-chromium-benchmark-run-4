e3cdddfd993129e17f4a740d6cce5b9d83d3d3c5
dkrahn <dkrahn@chromium.org>
2014-10-01 11:51:34 -0700

    PlatformVerification: Added a check for attestation availability.
    Currently if a system is not capable of enrolling for attestation or is
    not yet ready to enroll for attestation the flow proceeds to the user
    consent dialog anyways. Since user consent is always presented when the
    device has not been enrolled the user sees the dialog every time a new
    platform verification is initiated (e.g. every video).
    
    This change detects this condition and aborts the verification before
    the dialog is shown. Metrics have also been added.
    
    BUG=chromium-os-partner:31591, chromium-os-partner:27593
    TEST=unit, manual
    
    Review URL: https://codereview.chromium.org/567723003
    
    Cr-Commit-Position: refs/heads/master@{#297681}

--- chrome/browser/chromeos/attestation/platform_verification_flow.cc
+++ chrome/browser/chromeos/attestation/platform_verification_flow.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/logging.h"
 #include "base/message_loop/message_loop.h"
+#include "base/metrics/histogram.h"
 #include "base/prefs/pref_service.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
@@ -38,6 +39,11 @@ namespace {
 
 const char kDefaultHttpsPort[] = "443";
 const int kTimeoutInSeconds = 8;
+const char kAttestationResultHistogram[] =
+    "ChromeOS.PlatformVerification.Result";
+const char kAttestationAvailableHistogram[] =
+    "ChromeOS.PlatformVerification.Available";
+const int kAttestationResultHistogramMax = 10;
 
 // A callback method to handle DBus errors.
 void DBusCallback(const base::Callback<void(bool)>& on_success,
@@ -57,6 +63,8 @@ void ReportError(
     const chromeos::attestation::PlatformVerificationFlow::ChallengeCallback&
         callback,
     chromeos::attestation::PlatformVerificationFlow::Result error) {
+  UMA_HISTOGRAM_ENUMERATION(kAttestationResultHistogram, error,
+                            kAttestationResultHistogramMax);
   callback.Run(error, std::string(), std::string(), std::string());
 }
 }  // namespace
@@ -183,10 +191,26 @@ void PlatformVerificationFlow::ChallengePlatformKey(
     return;
   }
   ChallengeContext context(web_contents, service_id, challenge, callback);
+  // Check if the device has been prepared to use attestation.
+  BoolDBusMethodCallback dbus_callback = base::Bind(
+      &DBusCallback,
+      base::Bind(&PlatformVerificationFlow::CheckEnrollment, this, context),
+      base::Bind(&ReportError, callback, INTERNAL_ERROR));
+  cryptohome_client_->TpmAttestationIsPrepared(dbus_callback);
+}
+
+void PlatformVerificationFlow::CheckEnrollment(const ChallengeContext& context,
+                                               bool attestation_prepared) {
+  UMA_HISTOGRAM_BOOLEAN(kAttestationAvailableHistogram, attestation_prepared);
+  if (!attestation_prepared) {
+    // This device is not currently able to use attestation features.
+    ReportError(context.callback, PLATFORM_NOT_VERIFIED);
+    return;
+  }
   BoolDBusMethodCallback dbus_callback = base::Bind(
       &DBusCallback,
       base::Bind(&PlatformVerificationFlow::CheckConsent, this, context),
-      base::Bind(&ReportError, callback, INTERNAL_ERROR));
+      base::Bind(&ReportError, context.callback, INTERNAL_ERROR));
   cryptohome_client_->TpmAttestationIsEnrolled(dbus_callback);
 }
 
@@ -353,6 +377,8 @@ void PlatformVerificationFlow::OnChallengeReady(
     return;
   }
   VLOG(1) << "Platform verification successful.";
+  UMA_HISTOGRAM_ENUMERATION(kAttestationResultHistogram, SUCCESS,
+                            kAttestationResultHistogramMax);
   context.callback.Run(SUCCESS,
                        signed_data_pb.data(),
                        signed_data_pb.signature(),
