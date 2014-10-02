523c70307cf3da630eb77f117bd926cfde2c3dfe
rtenneti <rtenneti@chromium.org>
2014-10-01 20:15:31 -0700

    QUIC - track the open and close states of disk cache entries of QuicServerInfo.
    BUG=417835
    R=rch@chromium.org, asvitkine@chromium.org
    
    Review URL: https://codereview.chromium.org/618083003
    
    Cr-Commit-Position: refs/heads/master@{#297776}

--- net/http/disk_cache_based_quic_server_info.cc
+++ net/http/disk_cache_based_quic_server_info.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/callback.h"
 #include "base/logging.h"
+#include "base/metrics/histogram.h"
 #include "net/base/completion_callback.h"
 #include "net/base/io_buffer.h"
 #include "net/base/net_errors.h"
@@ -16,6 +17,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/quic/quic_server_id.h"
 
 namespace net {
+
+// Histogram for tracking down the state of disk_cache::Entry.
+enum DiskCacheEntryState {
+  DISK_CACHE_ENTRY_OPENED = 0,
+  DISK_CACHE_ENTRY_CLOSED = 1,
+  DISK_CACHE_ENTRY_NUM_STATES = 2,
+};
+
+void RecordDiskCacheEntryState(DiskCacheEntryState entry_state) {
+  UMA_HISTOGRAM_ENUMERATION("Net.QuicDiskCache.EntryState", entry_state,
+                            DISK_CACHE_ENTRY_NUM_STATES);
+}
 
 // Some APIs inside disk_cache take a handle that the caller must keep alive
 // until the API has finished its asynchronous execution.
@@ -120,8 +133,10 @@ void DiskCacheBasedQuicServerInfo::Persist() {
 
 DiskCacheBasedQuicServerInfo::~DiskCacheBasedQuicServerInfo() {
   DCHECK(user_callback_.is_null());
-  if (entry_)
+  if (entry_) {
     entry_->Close();
+    RecordDiskCacheEntryState(DISK_CACHE_ENTRY_CLOSED);
+  }
 }
 
 std::string DiskCacheBasedQuicServerInfo::key() const {
@@ -202,6 +217,7 @@ int DiskCacheBasedQuicServerInfo::DoOpenComplete(int rv) {
     entry_ = data_shim_->entry;
     state_ = READ;
     found_entry_ = true;
+    RecordDiskCacheEntryState(DISK_CACHE_ENTRY_OPENED);
   } else {
     state_ = WAIT_FOR_DATA_READY_DONE;
   }
@@ -229,6 +245,7 @@ int DiskCacheBasedQuicServerInfo::DoCreateOrOpenComplete(int rv) {
     if (!entry_) {
       entry_ = data_shim_->entry;
       found_entry_ = true;
+      RecordDiskCacheEntryState(DISK_CACHE_ENTRY_OPENED);
     }
     DCHECK(entry_);
     state_ = WRITE;
@@ -290,16 +307,20 @@ int DiskCacheBasedQuicServerInfo::DoWaitForDataReadyDone() {
   ready_ = true;
   // We close the entry because, if we shutdown before ::Persist is called,
   // then we might leak a cache reference, which causes a DCHECK on shutdown.
-  if (entry_)
+  if (entry_) {
     entry_->Close();
+    RecordDiskCacheEntryState(DISK_CACHE_ENTRY_CLOSED);
+  }
   entry_ = NULL;
   Parse(data_);
   return OK;
 }
 
 int DiskCacheBasedQuicServerInfo::DoSetDone() {
-  if (entry_)
+  if (entry_) {
     entry_->Close();
+    RecordDiskCacheEntryState(DISK_CACHE_ENTRY_CLOSED);
+  }
   entry_ = NULL;
   new_data_.clear();
   state_ = NONE;
