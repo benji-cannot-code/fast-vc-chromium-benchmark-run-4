ba7f83e1b7904488321050b8cbdee65a62fe8b35
skyostil <skyostil@chromium.org>
2014-10-02 03:08:56 -0700

    Revert of Revert of Use the new java_cpp_enum rule in content. (patchset #1 id:1 of https://codereview.chromium.org/625543002/)
    Reason for revert:
    Oops, wasn't supposed to revert this after all.
    
    Original issue's description:
    > Revert of Use the new java_cpp_enum rule in content. (patchset #8 id:140001 of https://codereview.chromium.org/615893003/)
    >
    > Reason for revert:
    > Fails to compile on several bots:
    >
    > https://chromegw.corp.google.com/i/clank.tot/builders/clang-clankium-tot-builder/builds/56800/steps/compile/logs/stdio
    >
    > First error:
    > FAILED: cd ../../clank/native/framework; python <snip ...>
    > ../../../clank/java/apps/chrome/src/com/google/android/apps/chrome/webapps/FullScreenActivityTab.java:28: error: cannot find symbol
    > import org.chromium.content.common.TopControlsState;
    >                                   ^
    >   symbol:   class TopControlsState
    >   location: package org.chromium.content.common
    >
    > Original issue's description:
    > > Use the new java_cpp_enum rule in content.
    > >
    > > This moves most of the generated Java enums under content/ to use the
    > > java_cpp_enum rule removing the need for keeping the enums in
    > > separate list files and the need for the .template files.
    > >
    > > BUG=405532, 351558
    > >
    > > Committed: https://crrev.com/7d5f0a581ba2f0e79b09ecbf6127ad453a861a96
    > > Cr-Commit-Position: refs/heads/master@{#297789}
    >
    > TBR=jam@chromium.org,yfriedman@chromium.org,scottmg@chromium.org,jdduke@chromium.org,brettw@chromium.org,mkosiba@chromium.org
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=405532, 351558
    >
    > Committed: https://crrev.com/949b6e5050c7a2ee2a894799db19ec325ba911ef
    > Cr-Commit-Position: refs/heads/master@{#297793}
    
    TBR=jam@chromium.org,yfriedman@chromium.org,scottmg@chromium.org,jdduke@chromium.org,brettw@chromium.org,mkosiba@chromium.org,perezju@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=405532, 351558
    
    Review URL: https://codereview.chromium.org/617103007
    
    Cr-Commit-Position: refs/heads/master@{#297794}

--- content/browser/renderer_host/input/selection_event_type.h
+++ content/browser/renderer_host/input/selection_event_type.h
@@ -8,10 +8,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
+// This file contains a list of events relating to selection and insertion, used
+// for notifying Java when the renderer selection has changed.
+//
+// A Java counterpart will be generated for this enum.
+// GENERATED_JAVA_ENUM_PACKAGE: org.chromium.content.browser.input
 enum SelectionEventType {
-#define DEFINE_SELECTION_EVENT_TYPE(name, value) name = value,
-#include "content/browser/renderer_host/input/selection_event_type_list.h"
-#undef DEFINE_SELECTION_EVENT_TYPE
+  SELECTION_SHOWN,
+  SELECTION_CLEARED,
+  SELECTION_DRAG_STARTED,
+  SELECTION_DRAG_STOPPED,
+  INSERTION_SHOWN,
+  INSERTION_MOVED,
+  INSERTION_TAPPED,
+  INSERTION_CLEARED,
+  INSERTION_DRAG_STARTED,
 };
 
 }  // namespace content
