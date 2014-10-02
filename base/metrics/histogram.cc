c6b6b0cc820b8100d745467be678efa8ded72e51
pkasting <pkasting@chromium.org>
2014-10-01 20:01:04 -0700

    Add Read/WriteSizeT() functions to the pickle layer, plus one consumer.
    This eliminates the need for callers to do explicit conversions, and also
    ensures callers don't try to implement pickling of a size_t using a 32-bit type,
    leading to truncation on 64-bit targets.  The pickle layer will ensure 64-bit
    types are always used.
    
    I'll be changing other callsites to use this in future patches.
    
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/601563003
    
    Cr-Commit-Position: refs/heads/master@{#297774}

--- base/metrics/histogram.cc
+++ base/metrics/histogram.cc
@@ -38,13 +38,13 @@ bool ReadHistogramArguments(PickleIterator* iter,
                             int* flags,
                             int* declared_min,
                             int* declared_max,
-                            uint64* bucket_count,
+                            size_t* bucket_count,
                             uint32* range_checksum) {
   if (!iter->ReadString(histogram_name) ||
       !iter->ReadInt(flags) ||
       !iter->ReadInt(declared_min) ||
       !iter->ReadInt(declared_max) ||
-      !iter->ReadUInt64(bucket_count) ||
+      !iter->ReadSizeT(bucket_count) ||
       !iter->ReadUInt32(range_checksum)) {
     DLOG(ERROR) << "Pickle error decoding Histogram: " << *histogram_name;
     return false;
@@ -298,7 +298,7 @@ bool Histogram::SerializeInfoImpl(Pickle* pickle) const {
       pickle->WriteInt(flags()) &&
       pickle->WriteInt(declared_min()) &&
       pickle->WriteInt(declared_max()) &&
-      pickle->WriteUInt64(bucket_count()) &&
+      pickle->WriteSizeT(bucket_count()) &&
       pickle->WriteUInt32(bucket_ranges()->checksum());
 }
 
@@ -348,7 +348,7 @@ HistogramBase* Histogram::DeserializeInfoImpl(PickleIterator* iter) {
   int flags;
   int declared_min;
   int declared_max;
-  uint64 bucket_count;
+  size_t bucket_count;
   uint32 range_checksum;
 
   if (!ReadHistogramArguments(iter, &histogram_name, &flags, &declared_min,
@@ -637,7 +637,7 @@ HistogramBase* LinearHistogram::DeserializeInfoImpl(PickleIterator* iter) {
   int flags;
   int declared_min;
   int declared_max;
-  uint64 bucket_count;
+  size_t bucket_count;
   uint32 range_checksum;
 
   if (!ReadHistogramArguments(iter, &histogram_name, &flags, &declared_min,
@@ -692,7 +692,7 @@ HistogramBase* BooleanHistogram::DeserializeInfoImpl(PickleIterator* iter) {
   int flags;
   int declared_min;
   int declared_max;
-  uint64 bucket_count;
+  size_t bucket_count;
   uint32 range_checksum;
 
   if (!ReadHistogramArguments(iter, &histogram_name, &flags, &declared_min,
@@ -787,7 +787,7 @@ HistogramBase* CustomHistogram::DeserializeInfoImpl(PickleIterator* iter) {
   int flags;
   int declared_min;
   int declared_max;
-  uint64 bucket_count;
+  size_t bucket_count;
   uint32 range_checksum;
 
   if (!ReadHistogramArguments(iter, &histogram_name, &flags, &declared_min,
