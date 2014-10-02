bf64794efa50e238d37d170ca439984f6e59f9f5
hjd <hjd@chromium.org>
2014-10-02 07:23:39 -0700

    Turn off telemetry tests which crash on WebView
    To move the WebView telemetry bot off hard coding
    which tests to use and start getting the list of tests
    automatically we need to label which tests don't work
    on the WebView.
    
    Bug tracking enabling them here: crbug.com/419687
    
    BUG=391488, 419687
    
    Review URL: https://codereview.chromium.org/619903003
    
    Cr-Commit-Position: refs/heads/master@{#297827}

--- tools/perf/benchmarks/blink_perf.py
+++ tools/perf/benchmarks/blink_perf.py
@@ -125,6 +125,7 @@ class BlinkPerfCSS(benchmark.Benchmark):
     return _CreatePageSetFromPath(path, SKIPPED_FILE)
 
 
+@benchmark.Disabled('android-webview') # crbug.com/419692
 class BlinkPerfCanvas(benchmark.Benchmark):
   tag = 'canvas'
   test = _BlinkPerfMeasurement
@@ -206,6 +207,7 @@ class BlinkPerfShadowDOM(benchmark.Benchmark):
     return _CreatePageSetFromPath(path, SKIPPED_FILE)
 
 
+@benchmark.Disabled('android-webview') # crbug.com/419690
 class BlinkPerfXMLHttpRequest(benchmark.Benchmark):
   tag = 'xml_http_request'
   test = _BlinkPerfMeasurement
