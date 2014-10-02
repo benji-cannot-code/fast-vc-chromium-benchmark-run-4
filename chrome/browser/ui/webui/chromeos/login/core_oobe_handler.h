0e4e4ed173ad80cb95a722570410fe4770b9f5c2
nkostylev <nkostylev@chromium.org>
2014-10-02 08:46:04 -0700

    [athena] Various fixes for out-of-box/login
    * Add unused parameter for toggling spoken feedback handler
    * For Athena disable magnification, use proper shutdown API
    
    BUG=419708
    
    Review URL: https://codereview.chromium.org/626503002
    
    Cr-Commit-Position: refs/heads/master@{#297842}

--- chrome/browser/ui/webui/chromeos/login/core_oobe_handler.h
+++ chrome/browser/ui/webui/chromeos/login/core_oobe_handler.h
@@ -93,7 +93,7 @@ class CoreOobeHandler : public BaseScreenHandler,
   void HandleEnableHighContrast(bool enabled);
   void HandleEnableVirtualKeyboard(bool enabled);
   void HandleEnableScreenMagnifier(bool enabled);
-  void HandleEnableSpokenFeedback();
+  void HandleEnableSpokenFeedback(bool /* enabled */);
   void HandleInitialized();
   void HandleSkipUpdateEnrollAfterEula();
   void HandleUpdateCurrentScreen(const std::string& screen);
