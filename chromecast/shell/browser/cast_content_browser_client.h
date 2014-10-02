82ed152b4d0866d1511b4bf649ef44e3c96c05ef
gunsch <gunsch@chromium.org>
2014-10-02 13:54:11 -0700

    Chromecast: device identification for whitelisted apps and servers.
    R=dougsteed@chromium.org,lcwu@chromium.org
    BUG=336640
    
    Review URL: https://codereview.chromium.org/618863002
    
    Cr-Commit-Position: refs/heads/master@{#297898}

--- chromecast/shell/browser/cast_content_browser_client.h
+++ chromecast/shell/browser/cast_content_browser_client.h
@@ -51,6 +51,12 @@ class CastContentBrowserClient: public content::ContentBrowserClient {
       bool expired_previous_decision,
       const base::Callback<void(bool)>& callback,
       content::CertificateRequestResultType* result) OVERRIDE;
+  virtual void SelectClientCertificate(
+      int render_process_id,
+      int render_frame_id,
+      const net::HttpNetworkSession* network_session,
+      net::SSLCertRequestInfo* cert_request_info,
+      const base::Callback<void(net::X509Certificate*)>& callback) OVERRIDE;
   virtual bool CanCreateWindow(
       const GURL& opener_url,
       const GURL& opener_top_level_frame_url,
@@ -74,6 +80,9 @@ class CastContentBrowserClient: public content::ContentBrowserClient {
       content::FileDescriptorInfo* mappings) OVERRIDE;
 
  private:
+  net::X509Certificate* SelectClientCertificateOnIOThread(
+      GURL requesting_url);
+
   scoped_ptr<URLRequestContextFactory> url_request_context_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(CastContentBrowserClient);
