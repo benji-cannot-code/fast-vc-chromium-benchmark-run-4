90d4c7ec55ef66b0a29cb7e0ae264e5c854075f9
yusufo <yusufo@chromium.org>
2014-10-02 11:28:43 -0700

    Make hasCookieStoreLoaded visible for testing
    We need to start using this method for testing purposes downstream
    but without the annotations it is getting stripped our by proguard.
    
    BUG=372774
    
    Review URL: https://codereview.chromium.org/615843004
    
    Cr-Commit-Position: refs/heads/master@{#297867}

--- chrome/android/javatests/src/org/chromium/chrome/browser/prerender/ExternalPrerenderRequestTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/prerender/ExternalPrerenderRequestTest.java
@@ -15,6 +15,8 @@ import org.chromium.chrome.browser.ContentViewUtil;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.shell.ChromeShellTestBase;
 import org.chromium.chrome.test.util.TestHttpServerClient;
+import org.chromium.content.browser.test.util.Criteria;
+import org.chromium.content.browser.test.util.CriteriaHelper;
 
 import java.util.concurrent.Callable;
 
@@ -49,8 +51,18 @@ public class ExternalPrerenderRequestTest extends ChromeShellTestBase {
             }
         };
         mProfile = ThreadUtils.runOnUiThreadBlocking(profileCallable);
-        while (!ExternalPrerenderHandler.hasCookieStoreLoaded(mProfile))
-            Thread.sleep(CHECK_COOKIE_STORE_FREQUENCY_MS);
+        final Callable<Boolean> cookieStoreCallable = new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                return ExternalPrerenderHandler.checkCookieStoreLoadedForTesting(mProfile);
+            }
+        };
+        assertTrue(CriteriaHelper.pollForCriteria(new Criteria() {
+            @Override
+            public boolean isSatisfied() {
+                return ThreadUtils.runOnUiThreadBlockingNoException(cookieStoreCallable);
+            }
+        }, CHECK_COOKIE_STORE_FREQUENCY_MS, 5000));
     }
 
     @MediumTest
