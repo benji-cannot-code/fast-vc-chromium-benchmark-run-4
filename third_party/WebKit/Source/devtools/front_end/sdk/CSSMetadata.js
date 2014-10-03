7ff71dd1d99b32c41ce7d74f0e1f998171241f86
lushnikov@chromium.org <lushnikov@chromium.org>
2014-10-03 14:41:14 +0000

    DevTools: [CSS] do not suggest box-sizing: padding-box value
    The "padding-box" value for box-sizing property is not currently supported
    in chrome, so it should not be suggested in styles sidebar pane.
    
    BUG=419900
    R=apavlov, vsevik
    
    Review URL: https://codereview.chromium.org/620323002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183195 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sdk/CSSMetadata.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/CSSMetadata.js
@@ -529,7 +529,7 @@ WebInspector.CSSMetadata._propertyDataMap = {
         "from-image", "snap"
     ] },
     "box-sizing": { values: [
-        "content-box", "padding-box", "border-box"
+        "content-box", "border-box"
     ] },
     "clip": { values: [
         "auto"
