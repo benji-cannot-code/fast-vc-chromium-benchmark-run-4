5637091b343774dd33280ab54e94d5529480eab2
lfg <lfg@chromium.org>
2014-10-03 13:17:15 -0700

    Revert of Enabling two tests that were disabled in app_shell_browsertests. (patchset #1 id:1 of https://codereview.chromium.org/623193002/)
    Reason for revert:
    The RenderProcessHost may not be destroyed because the message to destroy it may be queued in the message loop after the test pass message.
    
    Original issue's description:
    > Enabling two tests that were disabled in app_shell_browsertests.
    >
    > We can enable those tests now that crbug.com/419020 is fixed.
    >
    > BUG=352293
    >
    > Committed: https://crrev.com/9b10e4b077ee7106ab57d4728c5199a03638701a
    > Cr-Commit-Position: refs/heads/master@{#298062}
    
    TBR=fsamuel@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=352293
    
    Review URL: https://codereview.chromium.org/630453002
    
    Cr-Commit-Position: refs/heads/master@{#298086}

--- extensions/browser/guest_view/web_view/web_view_apitest.cc
+++ extensions/browser/guest_view/web_view/web_view_apitest.cc
@@ -198,7 +198,10 @@ IN_PROC_BROWSER_TEST_F(WebViewAPITest, TestDisplayNoneWebviewLoad) {
   RunTest("testDisplayNoneWebviewLoad", "web_view/apitest");
 }
 
-IN_PROC_BROWSER_TEST_F(WebViewAPITest, TestDisplayNoneWebviewRemoveChild) {
+// TODO(lfg): Re-enable this test once the fix for webview leaking
+// RenderProcessHost has landed. See http://crbug.com/419020 .
+IN_PROC_BROWSER_TEST_F(WebViewAPITest,
+                       DISABLED_TestDisplayNoneWebviewRemoveChild) {
   RunTest("testDisplayNoneWebviewRemoveChild", "web_view/apitest");
 }
 
@@ -322,7 +325,10 @@ IN_PROC_BROWSER_TEST_F(WebViewAPITest, TestRemoveSrcAttribute) {
   RunTest("testRemoveSrcAttribute", "web_view/apitest");
 }
 
-IN_PROC_BROWSER_TEST_F(WebViewAPITest, TestRemoveWebviewAfterNavigation) {
+// TODO(lfg): Re-enable this test once the fix for webview leaking
+// RenderProcessHost has landed. See http://crbug.com/419020 .
+IN_PROC_BROWSER_TEST_F(WebViewAPITest,
+                       DISABLED_TestRemoveWebviewAfterNavigation) {
   RunTest("testRemoveWebviewAfterNavigation", "web_view/apitest");
 }
 
