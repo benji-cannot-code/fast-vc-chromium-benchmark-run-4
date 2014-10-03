c719c30a512e8bff738b4433d63b89dd5f220d71
scottmg <scottmg@chromium.org>
2014-10-03 10:51:58 -0700

    gn format: support --in-place
    This is helpful for editor integration. e.g. in Vim:
    
    au BufRead,BufNewFile,BufEnter *.gn map <silent> <F1> :w<cr>:silent! exe "!/path/to/gn/gn.exe format --in-place " . expand('%:p')<cr>:e<cr>
    
    to reformat the current file.
    
    R=brettw@chromium.org
    BUG=348474
    
    Review URL: https://codereview.chromium.org/614683006
    
    Cr-Commit-Position: refs/heads/master@{#298058}

--- tools/gn/command_format.cc
+++ tools/gn/command_format.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sstream>
 
 #include "base/command_line.h"
+#include "base/file_util.h"
 #include "base/strings/string_split.h"
 #include "tools/gn/commands.h"
 #include "tools/gn/filesystem_utils.h"
@@ -553,8 +554,20 @@ int RunFormat(const std::vector<std::string>& args) {
   if (FormatFileToString(&setup, file, dump_tree, &output_string)) {
     bool in_place =
         base::CommandLine::ForCurrentProcess()->HasSwitch(kSwitchInPlace);
-    CHECK(!in_place) << "TODO(scottmg)";
-    printf("%s", output_string.c_str());
+    if (in_place) {
+      base::FilePath to_write = setup.build_settings().GetFullPath(file);
+      printf("Writing formatted to '%s'.\n", to_write.AsUTF8Unsafe().c_str());
+      if (base::WriteFile(to_write,
+                          output_string.data(),
+                          static_cast<int>(output_string.size())) == -1) {
+        Err(Location(),
+            std::string("Failed to write formatted output back to \"") +
+                to_write.AsUTF8Unsafe() + std::string("\".")).PrintToStdout();
+        return 1;
+      }
+    } else {
+      printf("%s", output_string.c_str());
+    }
   }
 
   return 0;
