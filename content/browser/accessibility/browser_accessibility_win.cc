e79232914b3f53fd57d61c199fa39aa69f538444
je_julie.kim <je_julie.kim@samsung.com>
2014-10-03 00:13:59 -0700

    Add AX attribute conversion for input type
    Exposed input type for tel, search, url and email on mac and windows.
    The Related CL:
    https://codereview.chromium.org/596393004/
    
    BUG=323161
    
    Review URL: https://codereview.chromium.org/559343002
    
    Cr-Commit-Position: refs/heads/master@{#298008}

--- content/browser/accessibility/browser_accessibility_win.cc
+++ content/browser/accessibility/browser_accessibility_win.cc
@@ -2910,6 +2910,7 @@ void BrowserAccessibilityWin::OnDataChanged() {
 
   // Expose the "display" and "tag" attributes.
   StringAttributeToIA2(ui::AX_ATTR_DISPLAY, "display");
+  StringAttributeToIA2(ui::AX_ATTR_TEXT_INPUT_TYPE, "text-input-type");
   StringAttributeToIA2(ui::AX_ATTR_HTML_TAG, "tag");
   StringAttributeToIA2(ui::AX_ATTR_ROLE, "xml-roles");
 
