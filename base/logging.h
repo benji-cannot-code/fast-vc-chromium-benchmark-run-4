251bbeb549243d9d4671c883794b314876e3d4aa
mdempsky <mdempsky@chromium.org>
2014-10-02 17:02:58 -0700

    Enable "extern template" under MSVC
    Review URL: https://codereview.chromium.org/613273005
    
    Cr-Commit-Position: refs/heads/master@{#297955}

--- base/logging.h
+++ base/logging.h
@@ -484,8 +484,6 @@ std::string* MakeCheckOpString(const t1& v1, const t2& v2, const char* names) {
   return msg;
 }
 
-// MSVC doesn't like complex extern templates and DLLs.
-#if !defined(COMPILER_MSVC)
 // Commonly used instantiations of MakeCheckOpString<>. Explicitly instantiated
 // in logging.cc.
 extern template BASE_EXPORT std::string* MakeCheckOpString<int, int>(
@@ -502,7 +500,6 @@ std::string* MakeCheckOpString<unsigned int, unsigned long>(
 extern template BASE_EXPORT
 std::string* MakeCheckOpString<std::string, std::string>(
     const std::string&, const std::string&, const char* name);
-#endif
 
 // Helper functions for CHECK_OP macro.
 // The (int, int) specialization works around the issue that the compiler
