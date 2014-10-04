7a3ebd681438a0a527e0ab501a7f72318a4bd0c0
tbarzic <tbarzic@chromium.org>
2014-10-03 18:28:15 -0700

    Revert "Clear user session flags before restarting Chrome for guest mode"
    Breaks login_CryptohomeIncognito on cros tests
    
    BUG=420258
    TBR=ihf@chromium.org
    
    This reverts commit 6d934345e2030aed1e6d831fad0c6b859c1e9600.
    
    > Clear user session flags before restarting Chrome for guest mode
    >
    > This avoids early restart to apply guest profile user flags
    >  if device owner has some flags set.
    >
    >  BUG=411049
    >  TEST=
    >  1. Login as the device owner and set some flags in about::flags
    >  2. Log out and enter guest mode
    >  3.1. Press alt + shift + I -> Feedback report dialog should be launched
    >  3.2. In chrome://help click "Report an issue" -> Feedback report dialog
    >  should be launched
    >
    >  Review URL: https://codereview.chromium.org/611383007
    >
    >  Cr-Commit-Position: refs/heads/master@{#297920}
    >
    
    Review URL: https://codereview.chromium.org/628813002
    
    Cr-Commit-Position: refs/heads/master@{#298155}

--- chrome/browser/chromeos/login/login_utils.cc
+++ chrome/browser/chromeos/login/login_utils.cc
@@ -79,7 +79,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/session_manager_client.h"
 #include "chromeos/login/auth/user_context.h"
-#include "chromeos/login/user_names.h"
 #include "chromeos/settings/cros_settings_names.h"
 #include "components/signin/core/browser/signin_manager.h"
 #include "components/user_manager/user.h"
@@ -422,16 +421,6 @@ void LoginUtilsImpl::CompleteOffTheRecordLogin(const GURL& start_url) {
                                  StartupUtils::IsOobeCompleted(),
                                  browser_command_line,
                                  &command_line);
-
-  // This makes sure that Chrome restarts with no per-session flags. The guest
-  // profile will always have empty set of per-session flags. If this is not
-  // done and device owner has some per-session flags, when Chrome is relaunched
-  // the guest profile session flags will not match the current command line and
-  // another restart will be attempted in order to reset the user flags for the
-  // guest user.
-  DBusThreadManager::Get()->GetSessionManagerClient()->SetFlagsForUser(
-      chromeos::login::kGuestUserName,
-      CommandLine::StringVector());
 
   RestartChrome(cmd_line_str);
 }
