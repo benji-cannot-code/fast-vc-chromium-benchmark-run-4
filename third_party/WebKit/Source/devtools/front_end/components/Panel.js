a7e18d5611a86796dbec56e80f3cfa140b42398b
pfeldman@chromium.org <pfeldman@chromium.org>
2014-10-05 10:46:20 +0000

    Reapply "DevTools: migrate extensible tabbed pane to promise-based instantiation.", "DevTools: load panel modules lazily." reverted as r183202.
    TBR=apavlov
    BUG=420087
    
    Review URL: https://codereview.chromium.org/630703002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183236 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/components/Panel.js
+++ third_party/WebKit/Source/devtools/front_end/components/Panel.js
@@ -209,7 +209,7 @@ WebInspector.PanelDescriptor.prototype = {
     title: function() {},
 
     /**
-     * @return {!WebInspector.Panel}
+     * @return {!Promise.<!WebInspector.Panel>}
      */
     panel: function() {}
 }
@@ -258,10 +258,19 @@ WebInspector.RuntimeExtensionPanelDescriptor.prototype = {
     },
 
     /**
-     * @return {!WebInspector.Panel}
+     * @return {!Promise.<!WebInspector.Panel>}
      */
     panel: function()
     {
-        return /** @type {!WebInspector.PanelFactory} */ (this._extension.instance()).createPanel();
+        return this._extension.instancePromise().then(createPanel);
+
+        /**
+         * @param {!Object} panelFactory
+         * @return {!WebInspector.Panel}
+         */
+        function createPanel(panelFactory)
+        {
+            return /** @type {!WebInspector.PanelFactory} */ (panelFactory).createPanel();
+        }
     }
 }
