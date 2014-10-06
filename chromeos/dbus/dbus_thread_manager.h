0e014273beb318239567a410a9e5af82f586bf3a
benchan <benchan@chromium.org>
2014-10-05 23:03:26 -0700

    Minor clean up of DBusClient / DBusThreadManager code.
    TEST=chromeos_unittests
    
    Review URL: https://codereview.chromium.org/602693003
    
    Cr-Commit-Position: refs/heads/master@{#298198}

--- chromeos/dbus/dbus_thread_manager.h
+++ chromeos/dbus/dbus_thread_manager.h
@@ -36,8 +36,6 @@ class BluetoothProfileManagerClient;
 class CrasAudioClient;
 class CrosDisksClient;
 class CryptohomeClient;
-class DBusClient;
-class DBusThreadManager;
 class DBusThreadManagerSetter;
 class DebugDaemonClient;
 class EasyUnlockClient;
