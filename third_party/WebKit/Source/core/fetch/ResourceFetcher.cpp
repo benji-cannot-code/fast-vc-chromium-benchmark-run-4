76949253757bc537496329a06b12134e27631603
mkwst@chromium.org <mkwst@chromium.org>
2014-10-06 13:33:28 +0000

    Mixed Content: Extract frame processing from ::shouldBlockFetch.
    In order to break MixedContentChecker's dependency on LocalFrame, we
    need to replace the existing checking mechanisms with static calls. This
    patch extracts some of the logic from ::shouldBlockFetch that we'll need
    to reuse for blocking WebSockets and form actions in future CLs.
    
    ------------------------------------------------------------------------
    This was originally landed in https://codereview.chromium.org/566533003,
    but reverted due to hard-to-track-down ChromeOS issues. I'll re-land it
    in parts to narrow down the bits that caused problems.
    ------------------------------------------------------------------------
    
    BUG=411184
    
    Review URL: https://codereview.chromium.org/623683002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183268 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/fetch/ResourceFetcher.cpp
+++ third_party/WebKit/Source/core/fetch/ResourceFetcher.cpp
@@ -57,6 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/loader/DocumentLoader.h"
 #include "core/loader/FrameLoader.h"
 #include "core/loader/FrameLoaderClient.h"
+#include "core/loader/MixedContentChecker.h"
 #include "core/loader/PingLoader.h"
 #include "core/loader/SubstituteData.h"
 #include "core/loader/UniqueIdentifier.h"
--- third_party/WebKit/Source/core/loader/MixedContentChecker.h
+++ third_party/WebKit/Source/core/loader/MixedContentChecker.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define MixedContentChecker_h
 
 #include "platform/heap/Handle.h"
+#include "platform/network/ResourceRequest.h"
 #include "public/platform/WebURLRequest.h"
 #include "wtf/text/WTFString.h"
 
@@ -49,7 +50,11 @@ class MixedContentChecker FINAL {
 public:
     explicit MixedContentChecker(LocalFrame*);
 
-    static bool shouldBlockFetch(LocalFrame*, const ResourceRequest&, const KURL&);
+    static bool shouldBlockFetch(LocalFrame* frame, const ResourceRequest& request, const KURL& url)
+    {
+        return shouldBlockFetch(frame, request.requestContext(), request.frameType(), url);
+    }
+    static bool shouldBlockFetch(LocalFrame*, WebURLRequest::RequestContext, WebURLRequest::FrameType, const KURL&);
 
     bool canDisplayInsecureContent(SecurityOrigin* securityOrigin, const KURL& url) const
     {
@@ -84,6 +89,8 @@ private:
         ContextTypeShouldBeBlockable,
         ContextTypeBlockableUnlessLax
     };
+
+    static LocalFrame* inWhichFrameIsThisContentMixed(LocalFrame*, WebURLRequest::RequestContext, WebURLRequest::FrameType, const KURL&);
 
     static ContextType contextTypeFromContext(WebURLRequest::RequestContext);
     static const char* typeNameFromContext(WebURLRequest::RequestContext);
