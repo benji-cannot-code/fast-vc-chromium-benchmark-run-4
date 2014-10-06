7063b2aaba0c1863c21d87f9500628d611cacd93
zerny@chromium.org <zerny@chromium.org>
2014-10-06 10:36:15 +0000

    Oilpan: Replace the positive heap-contains cache with a binary search tree of memory regions.
    R=ager@chromium.org, wibling@chromium.org
    BUG=419760, 420086
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=183139
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=183184
    
    Review URL: https://codereview.chromium.org/616483002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183259 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/heap/ThreadState.h
+++ third_party/WebKit/Source/platform/heap/ThreadState.h
@@ -50,7 +50,6 @@ class BaseHeap;
 class BaseHeapPage;
 class FinalizedHeapObjectHeader;
 struct GCInfo;
-class HeapContainsCache;
 class HeapObjectHeader;
 class PageMemory;
 class PersistentNode;
@@ -60,6 +59,7 @@ class SafePointBarrier;
 class SafePointAwareMutexLocker;
 template<typename Header> class ThreadHeap;
 class CallbackStack;
+class PageMemoryRegion;
 
 typedef uint8_t* Address;
 
@@ -459,6 +459,7 @@ public:
     // sweeping?
     bool isSweepInProgress() const { return m_sweepInProgress; }
 
+    void prepareRegionTree();
     void prepareForGC();
 
     // Safepoint related functionality.
@@ -592,7 +593,6 @@ public:
     // Infrastructure to determine if an address is within one of the
     // address ranges for the Blink heap. If the address is in the Blink
     // heap the containing heap page is returned.
-    HeapContainsCache* heapContainsCache() { return m_heapContainsCache.get(); }
     BaseHeapPage* contains(Address address) { return heapPageFromAddress(address); }
     BaseHeapPage* contains(void* pointer) { return contains(reinterpret_cast<Address>(pointer)); }
     BaseHeapPage* contains(const void* pointer) { return contains(const_cast<void*>(pointer)); }
@@ -622,10 +622,6 @@ public:
 
     // Visit all persistents allocated on this thread.
     void visitPersistents(Visitor*);
-
-    // Checks a given address and if a pointer into the oilpan heap marks
-    // the object to which it points.
-    bool checkAndMarkPointer(Visitor*, Address);
 
 #if ENABLE(GC_PROFILE_MARKING)
     const GCInfo* findGCInfo(Address);
@@ -697,6 +693,8 @@ public:
     }
 
     Mutex& sweepMutex() { return m_sweepMutex; }
+
+    Vector<PageMemoryRegion*>& allocatedRegionsSinceLastGC() { return m_allocatedRegionsSinceLastGC; }
 
 private:
     explicit ThreadState();
@@ -773,7 +771,6 @@ private:
     size_t m_noAllocationCount;
     bool m_inGC;
     BaseHeap* m_heaps[NumberOfHeaps];
-    OwnPtr<HeapContainsCache> m_heapContainsCache;
     HeapStats m_stats;
     HeapStats m_statsAfterLastGC;
 
@@ -793,6 +790,8 @@ private:
 #if defined(ADDRESS_SANITIZER)
     void* m_asanFakeStack;
 #endif
+
+    Vector<PageMemoryRegion*> m_allocatedRegionsSinceLastGC;
 };
 
 template<ThreadAffinity affinity> class ThreadStateFor;
@@ -893,13 +892,7 @@ public:
     ThreadState* threadState() const { return m_threadState; }
     const GCInfo* gcInfo() { return m_gcInfo; }
     virtual bool isLargeObject() { return false; }
-    virtual void markOrphaned()
-    {
-        m_threadState = 0;
-        m_gcInfo = 0;
-        m_terminating = false;
-        m_tracedAfterOrphaned = false;
-    }
+    virtual void markOrphaned();
     bool orphaned() { return !m_threadState; }
     bool terminating() { return m_terminating; }
     void setTerminating() { m_terminating = true; }
