e3371573fbd8dd2c3ddf8c65af64efdf2c78e803
anand.ratn <anand.ratn@samsung.com>
2014-10-06 16:06:32 -0700

    Replace OVERRIDE and FINAL with override and final in content/app/[a-s]*
    BUG=417463
    
    Review URL: https://codereview.chromium.org/625263003
    
    Cr-Commit-Position: refs/heads/master@{#298305}

--- content/app/android/child_process_service.cc
+++ content/app/android/child_process_service.cc
@@ -54,7 +54,7 @@ class SurfaceTexturePeerChildImpl : public SurfaceTexturePeer,
       base::ProcessHandle pid,
       scoped_refptr<gfx::SurfaceTexture> surface_texture,
       int primary_id,
-      int secondary_id) OVERRIDE {
+      int secondary_id) override {
     JNIEnv* env = base::android::AttachCurrentThread();
     content::Java_ChildProcessService_establishSurfaceTexturePeer(
         env, service_.obj(), pid,
@@ -64,7 +64,7 @@ class SurfaceTexturePeerChildImpl : public SurfaceTexturePeer,
   }
 
   // Overridden from GpuSurfaceLookup:
-  virtual gfx::AcceleratedWidget AcquireNativeWidget(int surface_id) OVERRIDE {
+  virtual gfx::AcceleratedWidget AcquireNativeWidget(int surface_id) override {
     JNIEnv* env = base::android::AttachCurrentThread();
     gfx::ScopedJavaSurface surface(
         content::Java_ChildProcessService_getViewSurface(
@@ -86,7 +86,7 @@ class SurfaceTexturePeerChildImpl : public SurfaceTexturePeer,
   // Overridden from SurfaceTextureLookup:
   virtual gfx::AcceleratedWidget AcquireNativeWidget(int primary_id,
                                                      int secondary_id)
-      OVERRIDE {
+      override {
     JNIEnv* env = base::android::AttachCurrentThread();
     gfx::ScopedJavaSurface surface(
         content::Java_ChildProcessService_getSurfaceTextureSurface(
--- content/app/content_main_runner.cc
+++ content/app/content_main_runner.cc
@@ -481,7 +481,7 @@ class ContentMainRunnerImpl : public ContentMainRunner {
   }
 #endif
 
-  virtual int Initialize(const ContentMainParams& params) OVERRIDE {
+  virtual int Initialize(const ContentMainParams& params) override {
     ui_task_ = params.ui_task;
 
 #if defined(OS_WIN)
@@ -750,7 +750,7 @@ class ContentMainRunnerImpl : public ContentMainRunner {
     return -1;
   }
 
-  virtual int Run() OVERRIDE {
+  virtual int Run() override {
     DCHECK(is_initialized_);
     DCHECK(!is_shutdown_);
     const base::CommandLine& command_line =
@@ -773,7 +773,7 @@ class ContentMainRunnerImpl : public ContentMainRunner {
 #endif
   }
 
-  virtual void Shutdown() OVERRIDE {
+  virtual void Shutdown() override {
     DCHECK(is_initialized_);
     DCHECK(!is_shutdown_);
 
