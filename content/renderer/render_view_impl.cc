947da867130cd33981246f2b91561adf1edf3d8e
jdduke <jdduke@chromium.org>
2014-10-06 21:19:08 -0700

    Only send ViewHostMsg_FocusedNodeTouched on Windows
    This message is only handled on Windows, and should only be sent on
    Windows. The longer term solution will involve removing it entirely,
    but in the meantime disable its dispatch on all other platforms.
    
    BUG=420130
    
    Review URL: https://codereview.chromium.org/625013002
    
    Cr-Commit-Position: refs/heads/master@{#298368}

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -2108,6 +2108,9 @@ void RenderViewImpl::didHandleGestureEvent(
         RenderViewObserver, observers_, DidHandleGestureEvent(event));
   }
 
+  // TODO(ananta): Piggyback off existing IPCs to communicate this information,
+  // crbug/420130.
+#if defined(OS_WIN)
   if (event.type != blink::WebGestureEvent::GestureTap)
     return;
 
@@ -2118,6 +2121,7 @@ void RenderViewImpl::didHandleGestureEvent(
 
   Send(new ViewHostMsg_FocusedNodeTouched(
       routing_id(), text_input_type != blink::WebTextInputTypeNone));
+#endif
 }
 
 void RenderViewImpl::initializeLayerTreeView() {
