f0899e3c5065fb6b54bb031ca701ca9a018db7ca
r.c.ladan <r.c.ladan@gmail.com>
2014-10-06 22:07:59 -0700

    Fix deprecation warnings about including <gssapi.h> instead of <gssapi/gssapi.h> on FreeBSD.
    While here remove redundant blank line found by 'git-cl lint'
    
    BUG=418985
    
    Review URL: https://codereview.chromium.org/617743003
    
    Cr-Commit-Position: refs/heads/master@{#298378}

--- net/http/http_auth_gssapi_posix.h
+++ net/http/http_auth_gssapi_posix.h
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_9
 // Including gssapi.h directly is deprecated in the 10.9 SDK.
 #include <GSS/gssapi.h>
+#elif defined(OS_FREEBSD)
+#include <gssapi/gssapi.h>
 #else
 #include <gssapi.h>
 #endif
@@ -100,7 +102,6 @@ class NET_EXPORT_PRIVATE GSSAPILibrary {
       OM_uint32* ctx_flags,
       int* locally_initiated,
       int* open) = 0;
-
 };
 
 // GSSAPISharedLibrary class is defined here so that unit tests can access it.
--- net/http/mock_gssapi_library_posix.h
+++ net/http/mock_gssapi_library_posix.h
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_9
 // Including gssapi.h directly is deprecated in the 10.9 SDK.
 #include <GSS/gssapi.h>
+#elif defined(OS_FREEBSD)
+#include <gssapi/gssapi.h>
 #else
 #include <gssapi.h>
 #endif
