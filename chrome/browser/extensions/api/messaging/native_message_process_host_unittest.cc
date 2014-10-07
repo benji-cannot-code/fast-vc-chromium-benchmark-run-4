1f9056104c80239986da1a86e572d42beddc7c04
kelvinp <kelvinp@chromium.org>
2014-10-06 21:17:53 -0700

    Remote Assistance on Chrome OS Part III - NativeMessageHost
    This CL extracts the NativeMessageHost interface from the NativeMessageProcessHost
    A NativeMessageHost hosts a native component, which can be
    running in the same process as chrome or in a separate
    process.
    
    Review URL: https://codereview.chromium.org/591463003
    
    Cr-Commit-Position: refs/heads/master@{#298366}

--- chrome/browser/extensions/api/messaging/native_message_process_host_unittest.cc
+++ chrome/browser/extensions/api/messaging/native_message_process_host_unittest.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/scoped_temp_dir.h"
 #include "base/json/json_reader.h"
 #include "base/memory/scoped_ptr.h"
-#include "base/memory/weak_ptr.h"
 #include "base/message_loop/message_loop.h"
 #include "base/rand_util.h"
 #include "base/run_loop.h"
@@ -94,7 +93,7 @@ class FakeLauncher : public NativeProcessLauncher {
 };
 
 class NativeMessagingTest : public ::testing::Test,
-                            public NativeMessageProcessHost::Client,
+                            public NativeMessageHost::Client,
                             public base::SupportsWeakPtr<NativeMessagingTest> {
  protected:
   NativeMessagingTest()
@@ -107,16 +106,14 @@ class NativeMessagingTest : public ::testing::Test,
   }
 
   virtual void TearDown() override {
-    if (native_message_process_host_.get()) {
-      BrowserThread::DeleteSoon(BrowserThread::IO, FROM_HERE,
-                                native_message_process_host_.release());
+    if (native_message_host_.get()) {
+      BrowserThread::DeleteSoon(
+          BrowserThread::IO, FROM_HERE, native_message_host_.release());
     }
     base::RunLoop().RunUntilIdle();
   }
 
-  virtual void PostMessageFromNativeProcess(
-      int port_id,
-      const std::string& message) override  {
+  virtual void PostMessageFromNativeHost(const std::string& message) override {
     last_message_ = message;
 
     // Parse the message.
@@ -134,8 +131,7 @@ class NativeMessagingTest : public ::testing::Test,
       run_loop_->Quit();
   }
 
-  virtual void CloseChannel(int port_id,
-                            const std::string& error_message) override {
+  virtual void CloseChannel(const std::string& error_message) override {
     channel_closed_ = true;
     if (run_loop_)
       run_loop_->Quit();
@@ -165,7 +161,7 @@ class NativeMessagingTest : public ::testing::Test,
   base::ScopedTempDir temp_dir_;
   // Force the channel to be dev.
   ScopedCurrentChannel current_channel_;
-  scoped_ptr<NativeMessageProcessHost> native_message_process_host_;
+  scoped_ptr<NativeMessageHost> native_message_host_;
   scoped_ptr<base::RunLoop> run_loop_;
   content::TestBrowserThreadBundle thread_bundle_;
   std::string last_message_;
@@ -181,15 +177,19 @@ TEST_F(NativeMessagingTest, SingleSendMessageRead) {
 
   scoped_ptr<NativeProcessLauncher> launcher =
       FakeLauncher::Create(temp_input_file, temp_output_file).Pass();
-  native_message_process_host_ = NativeMessageProcessHost::CreateWithLauncher(
-      AsWeakPtr(), ScopedTestNativeMessagingHost::kExtensionId, "empty_app.py",
-      0, launcher.Pass());
-  ASSERT_TRUE(native_message_process_host_.get());
+  native_message_host_ = NativeMessageProcessHost::CreateWithLauncher(
+      ScopedTestNativeMessagingHost::kExtensionId,
+      "empty_app.py",
+      launcher.Pass());
+  native_message_host_->Start(this);
+  ASSERT_TRUE(native_message_host_.get());
   run_loop_.reset(new base::RunLoop());
   run_loop_->RunUntilIdle();
 
   if (last_message_.empty()) {
     run_loop_.reset(new base::RunLoop());
+    scoped_ptr<NativeMessageProcessHost> native_message_process_host_(
+        static_cast<NativeMessageProcessHost*>(native_message_host_.release()));
     native_message_process_host_->ReadNowForTesting();
     run_loop_->Run();
   }
@@ -227,13 +227,15 @@ TEST_F(NativeMessagingTest, SingleSendMessageWrite) {
   scoped_ptr<NativeProcessLauncher> launcher =
       FakeLauncher::CreateWithPipeInput(read_file.Pass(),
                                         temp_output_file).Pass();
-  native_message_process_host_ = NativeMessageProcessHost::CreateWithLauncher(
-      AsWeakPtr(), ScopedTestNativeMessagingHost::kExtensionId, "empty_app.py",
-      0, launcher.Pass());
-  ASSERT_TRUE(native_message_process_host_.get());
+  native_message_host_ = NativeMessageProcessHost::CreateWithLauncher(
+      ScopedTestNativeMessagingHost::kExtensionId,
+      "empty_app.py",
+      launcher.Pass());
+  native_message_host_->Start(this);
+  ASSERT_TRUE(native_message_host_.get());
   base::RunLoop().RunUntilIdle();
 
-  native_message_process_host_->Send(kTestMessage);
+  native_message_host_->OnMessage(kTestMessage);
   base::RunLoop().RunUntilIdle();
 
   std::string output;
@@ -253,13 +255,17 @@ TEST_F(NativeMessagingTest, SingleSendMessageWrite) {
 TEST_F(NativeMessagingTest, EchoConnect) {
   ScopedTestNativeMessagingHost test_host;
   ASSERT_NO_FATAL_FAILURE(test_host.RegisterTestHost(false));
+  std::string error_message;
+  native_message_host_ = NativeMessageProcessHost::Create(
+      NULL,
+      ScopedTestNativeMessagingHost::kExtensionId,
+      ScopedTestNativeMessagingHost::kHostName,
+      false,
+      &error_message);
+  native_message_host_->Start(this);
+  ASSERT_TRUE(native_message_host_.get());
 
-  native_message_process_host_ = NativeMessageProcessHost::Create(
-      NULL, AsWeakPtr(), ScopedTestNativeMessagingHost::kExtensionId,
-      ScopedTestNativeMessagingHost::kHostName, 0, false);
-  ASSERT_TRUE(native_message_process_host_.get());
-
-  native_message_process_host_->Send("{\"text\": \"Hello.\"}");
+  native_message_host_->OnMessage("{\"text\": \"Hello.\"}");
   run_loop_.reset(new base::RunLoop());
   run_loop_->Run();
   ASSERT_FALSE(last_message_.empty());
@@ -277,7 +283,7 @@ TEST_F(NativeMessagingTest, EchoConnect) {
   EXPECT_TRUE(last_message_parsed_->GetString("caller_url", &url));
   EXPECT_EQ(expected_url, url);
 
-  native_message_process_host_->Send("{\"foo\": \"bar\"}");
+  native_message_host_->OnMessage("{\"foo\": \"bar\"}");
   run_loop_.reset(new base::RunLoop());
   run_loop_->Run();
   EXPECT_TRUE(last_message_parsed_->GetInteger("id", &id));
@@ -292,12 +298,17 @@ TEST_F(NativeMessagingTest, UserLevel) {
   ScopedTestNativeMessagingHost test_host;
   ASSERT_NO_FATAL_FAILURE(test_host.RegisterTestHost(true));
 
-  native_message_process_host_ = NativeMessageProcessHost::Create(
-      NULL, AsWeakPtr(), ScopedTestNativeMessagingHost::kExtensionId,
-      ScopedTestNativeMessagingHost::kHostName, 0, true);
-  ASSERT_TRUE(native_message_process_host_.get());
+  std::string error_message;
+  native_message_host_ = NativeMessageProcessHost::Create(
+      NULL,
+      ScopedTestNativeMessagingHost::kExtensionId,
+      ScopedTestNativeMessagingHost::kHostName,
+      true,
+      &error_message);
+  native_message_host_->Start(this);
+  ASSERT_TRUE(native_message_host_.get());
 
-  native_message_process_host_->Send("{\"text\": \"Hello.\"}");
+  native_message_host_->OnMessage("{\"text\": \"Hello.\"}");
   run_loop_.reset(new base::RunLoop());
   run_loop_->Run();
   ASSERT_FALSE(last_message_.empty());
@@ -308,10 +319,15 @@ TEST_F(NativeMessagingTest, DisallowUserLevel) {
   ScopedTestNativeMessagingHost test_host;
   ASSERT_NO_FATAL_FAILURE(test_host.RegisterTestHost(true));
 
-  native_message_process_host_ = NativeMessageProcessHost::Create(
-      NULL, AsWeakPtr(), ScopedTestNativeMessagingHost::kExtensionId,
-      ScopedTestNativeMessagingHost::kHostName, 0, false);
-  ASSERT_TRUE(native_message_process_host_.get());
+  std::string error_message;
+  native_message_host_ = NativeMessageProcessHost::Create(
+      NULL,
+      ScopedTestNativeMessagingHost::kExtensionId,
+      ScopedTestNativeMessagingHost::kHostName,
+      false,
+      &error_message);
+  native_message_host_->Start(this);
+  ASSERT_TRUE(native_message_host_.get());
   run_loop_.reset(new base::RunLoop());
   run_loop_->Run();
 
--- extensions/common/switches.h
+++ extensions/common/switches.h
@@ -26,6 +26,7 @@ extern const char kEnableExtensionActionRedesign[];
 extern const char kEnableExtensionInfoDialog[];
 extern const char kEnableMimeHandlerView[];
 extern const char kEnableOverrideBookmarksUI[];
+extern const char kEnableRemoteAssistance[];
 extern const char kErrorConsole[];
 extern const char kExtensionActionRedesign[];
 extern const char kExtensionProcess[];
