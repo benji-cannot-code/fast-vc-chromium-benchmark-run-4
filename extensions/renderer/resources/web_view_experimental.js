584833521528eef8664084d0480f89ea2ae5417a
paulmeyer <paulmeyer@chromium.org>
2014-10-06 20:33:52 -0700

    Added/fixed/improved documentation in web_view.js and web_view_experimental.js.
    Review URL: https://codereview.chromium.org/633633003
    
    Cr-Commit-Position: refs/heads/master@{#298352}

--- extensions/renderer/resources/web_view_experimental.js
+++ extensions/renderer/resources/web_view_experimental.js
@@ -12,16 +12,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 var WebViewInternal = require('webView').WebViewInternal;
 var WebView = require('webViewInternal').WebView;
 
+// Returns a map of experimental <webview> DOM event names to their associated
+// extension event descriptor objects. See |WEB_VIEW_EVENTS| in
+// web_view_events.js for more information and details on how this map should
+// be formatted.
 WebViewInternal.prototype.maybeGetExperimentalEvents = function() {
   return {};
 };
 
-/** @private */
+// Captures the visible content within the webview.
 WebViewInternal.prototype.captureVisibleRegion = function(spec, callback) {
   WebView.captureVisibleRegion(this.guestInstanceId, spec, callback);
 };
 
-/** @private */
+// Loads a data URL with a specified base URL used for relative links.
+// Optionally, a virtual URL can be provided to be shown to the user instead
+// of the data URL.
 WebViewInternal.prototype.loadDataWithBaseUrl = function(
     dataUrl, baseUrl, virtualUrl) {
   if (!this.guestInstanceId) {
@@ -38,6 +44,7 @@ WebViewInternal.prototype.loadDataWithBaseUrl = function(
       });
 };
 
+// Registers the experimantal WebVIew API when available.
 WebViewInternal.maybeRegisterExperimentalAPIs = function(proto) {
   proto.captureVisibleRegion = function(spec, callback) {
     privates(this).internal.captureVisibleRegion(spec, callback);
