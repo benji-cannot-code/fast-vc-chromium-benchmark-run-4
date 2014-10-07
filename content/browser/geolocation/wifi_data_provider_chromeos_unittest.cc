154db51b25c5d9b71ead4f6830eb5e6567a7c349
mohan.reddy <mohan.reddy@samsung.com>
2014-10-07 01:49:33 -0700

    Replacing the OVERRIDE with override and FINAL with final in content/browser/geolocation
    This step is a giant search and replace for OVERRIDE and FINAL to
    replace them with their lowercase versions.
    
    BUG=417463
    
    Review URL: https://codereview.chromium.org/631053003
    
    Cr-Commit-Position: refs/heads/master@{#298429}

--- content/browser/geolocation/wifi_data_provider_chromeos_unittest.cc
+++ content/browser/geolocation/wifi_data_provider_chromeos_unittest.cc
@@ -20,7 +20,7 @@ class GeolocationChromeOsWifiDataProviderTest : public testing::Test {
   GeolocationChromeOsWifiDataProviderTest() {
   }
 
-  virtual void SetUp() OVERRIDE {
+  virtual void SetUp() override {
     chromeos::DBusThreadManager::Initialize();
     chromeos::NetworkHandler::Initialize();
     manager_client_ =
@@ -30,7 +30,7 @@ class GeolocationChromeOsWifiDataProviderTest : public testing::Test {
     message_loop_.RunUntilIdle();
   }
 
-  virtual void TearDown() OVERRIDE {
+  virtual void TearDown() override {
     provider_ = NULL;
     chromeos::NetworkHandler::Shutdown();
     chromeos::DBusThreadManager::Shutdown();
