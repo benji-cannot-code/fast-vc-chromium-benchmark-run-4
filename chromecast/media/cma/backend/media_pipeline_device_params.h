ecb32aeff9a4cfc14b3e03713c7e2edd0a6ac699
gunsch <gunsch@chromium.org>
2014-10-06 20:29:13 -0700

    Chromecast: adds interfaces for hardware layer of CMA pipeline.
    R=damienv@chromium.org,lcwu@chromium.org
    BUG=408189
    
    Review URL: https://codereview.chromium.org/622853002
    
    Cr-Commit-Position: refs/heads/master@{#298350}

---
+++ chromecast/media/cma/backend/media_pipeline_device_params.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMECAST_MEDIA_CMA_BACKEND_MEDIA_PIPELINE_DEVICE_PARAMS_H_
+#define CHROMECAST_MEDIA_CMA_BACKEND_MEDIA_PIPELINE_DEVICE_PARAMS_H_
+
+#include "base/macros.h"
+
+namespace chromecast {
+namespace media {
+
+class MediaPipelineDeviceParams {
+ public:
+  MediaPipelineDeviceParams();
+  ~MediaPipelineDeviceParams();
+
+  // When set to true, synchronization is disabled and audio/video frames are
+  // rendered "right away":
+  // - for audio, frames are still rendered based on the sampling frequency
+  // - for video, frames are rendered as soon as available at the output of
+  //   the video decoder.
+  //   The assumption is that no B frames are used when synchronization is
+  //   disabled, otherwise B frames would always be skipped.
+  bool disable_synchronization;
+};
+
+}  // namespace media
+}  // namespace chromecast
+
+#endif  // CHROMECAST_MEDIA_CMA_BACKEND_MEDIA_PIPELINE_DEVICE_PARAMS_H_
