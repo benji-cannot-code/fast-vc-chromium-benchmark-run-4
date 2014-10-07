520fc3ad8b2ffb68f906b12a61147c7ba3db6422
qsr <qsr@chromium.org>
2014-10-07 06:36:24 -0700

    mojo: Use a MessagePumpMojo in java tests
    This change allows to use RunUntilIdle() instead of a timeout most of
    the time.
    
    R=ppi@chromium.org
    
    Review URL: https://codereview.chromium.org/621793002
    
    Cr-Commit-Position: refs/heads/master@{#298466}

--- mojo/android/javatests/src/org/chromium/mojo/bindings/ConnectorTest.java
+++ mojo/android/javatests/src/org/chromium/mojo/bindings/ConnectorTest.java
@@ -25,8 +25,6 @@ import java.util.ArrayList;
  */
 public class ConnectorTest extends MojoTestCase {
 
-    private static final long RUN_LOOP_TIMEOUT_MS = 25;
-
     private static final int DATA_LENGTH = 1024;
 
     private MessagePipeHandle mHandle;
@@ -88,7 +86,7 @@ public class ConnectorTest extends MojoTestCase {
     public void testReceivingMessage() {
         mHandle.writeMessage(mTestMessage.getData(), new ArrayList<Handle>(),
                 MessagePipeHandle.WriteFlags.NONE);
-        nativeRunLoop(RUN_LOOP_TIMEOUT_MS);
+        runLoopUntilIdle();
         assertNull(mErrorHandler.getLastMojoException());
         assertEquals(1, mReceiver.messages.size());
         Message received = mReceiver.messages.get(0);
@@ -102,7 +100,7 @@ public class ConnectorTest extends MojoTestCase {
     @SmallTest
     public void testErrors() {
         mHandle.close();
-        nativeRunLoop(RUN_LOOP_TIMEOUT_MS);
+        runLoopUntilIdle();
         assertNotNull(mErrorHandler.getLastMojoException());
         assertEquals(MojoResult.FAILED_PRECONDITION,
                 mErrorHandler.getLastMojoException().getMojoResult());
--- mojo/android/javatests/src/org/chromium/mojo/bindings/RouterTest.java
+++ mojo/android/javatests/src/org/chromium/mojo/bindings/RouterTest.java
@@ -25,8 +25,6 @@ import java.util.ArrayList;
  */
 public class RouterTest extends MojoTestCase {
 
-    private static final long RUN_LOOP_TIMEOUT_MS = 25;
-
     private MessagePipeHandle mHandle;
     private Router mRouter;
     private RecordingMessageReceiverWithResponder mReceiver;
@@ -83,7 +81,7 @@ public class RouterTest extends MojoTestCase {
         Message responseMessage = encoder.getMessage();
         mHandle.writeMessage(responseMessage.getData(), new ArrayList<Handle>(),
                 MessagePipeHandle.WriteFlags.NONE);
-        nativeRunLoop(RUN_LOOP_TIMEOUT_MS);
+        runLoopUntilIdle();
 
         assertEquals(1, mReceiver.messages.size());
         ServiceMessage receivedResponseMessage = mReceiver.messages.get(0).asServiceMessage();
@@ -109,7 +107,7 @@ public class RouterTest extends MojoTestCase {
         Message headerMessage = encoder.getMessage();
         mHandle.writeMessage(headerMessage.getData(), new ArrayList<Handle>(),
                 MessagePipeHandle.WriteFlags.NONE);
-        nativeRunLoop(RUN_LOOP_TIMEOUT_MS);
+        runLoopUntilIdle();
 
         assertEquals(1, mReceiver.messagesWithReceivers.size());
         Pair<Message, MessageReceiver> receivedMessage =
