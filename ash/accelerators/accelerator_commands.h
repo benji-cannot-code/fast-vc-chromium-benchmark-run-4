97c4985ef21c58895d99200e08612f98158dd5d0
kpschoedel <kpschoedel@chromium.org>
2014-10-07 11:32:41 -0700

    Allow the Ash shortcut Ctrl+Alt+P to work in browser windows.
    BUG=416935
    
    Review URL: https://codereview.chromium.org/620423002
    
    Cr-Commit-Position: refs/heads/master@{#298528}

--- ash/accelerators/accelerator_commands.h
+++ ash/accelerators/accelerator_commands.h
@@ -26,6 +26,9 @@ ASH_EXPORT void ToggleMaximized();
 // by WindowStateDelegate::ToggleFullscreen().
 ASH_EXPORT void ToggleFullscreen();
 
+// Toggles touch HUD.
+ASH_EXPORT void ToggleTouchHudProjection();
+
 }  // namespace accelerators
 }  // namespace ash
 
--- ash/accelerators/accelerator_controller.cc
+++ ash/accelerators/accelerator_controller.cc
@@ -627,13 +627,6 @@ bool HandleTouchHudModeChange() {
   return false;
 }
 
-bool HandleTouchHudProjectToggle() {
-  base::RecordAction(UserMetricsAction("Accel_Touch_Hud_Clear"));
-  bool enabled = Shell::GetInstance()->is_touch_hud_projection_enabled();
-  Shell::GetInstance()->SetTouchHudProjectionEnabled(!enabled);
-  return true;
-}
-
 bool HandleDisableCapsLock(ui::KeyboardCode key_code,
                            ui::EventType previous_event_type,
                            ui::KeyboardCode previous_key_code) {
@@ -943,7 +936,8 @@ bool AcceleratorController::PerformAction(int action,
     case TOUCH_HUD_MODE_CHANGE:
       return HandleTouchHudModeChange();
     case TOUCH_HUD_PROJECTION_TOGGLE:
-      return HandleTouchHudProjectToggle();
+      accelerators::ToggleTouchHudProjection();
+      return true;
     case DISABLE_GPU_WATCHDOG:
       Shell::GetInstance()->gpu_support()->DisableGpuWatchdog();
       return true;
