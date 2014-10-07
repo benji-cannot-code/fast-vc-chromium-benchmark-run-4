95674e7d9e755bcd40da377f756e317893a8929e
kelvinp <kelvinp@chromium.org>
2014-10-07 14:49:39 -0700

    Remote Assistance on Chrome OS Part III - NativeMessageHost
    This CL extracts the NativeMessageHost interface from the NativeMessageProcessHost
    A NativeMessageHost hosts a native component, which can be
    running in the same process as chrome or in a separate
    process.
    
    Committed: https://crrev.com/ba722a2a8ddf967dcadb483ea32d5ad31fc4bdf6
    Cr-Commit-Position: refs/heads/master@{#298366}
    
    Review URL: https://codereview.chromium.org/591463003
    
    Cr-Commit-Position: refs/heads/master@{#298579}

--- chrome/browser/policy/policy_browsertest.cc
+++ chrome/browser/policy/policy_browsertest.cc
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/content_settings/tab_specific_content_settings.h"
 #include "chrome/browser/devtools/devtools_window_testing.h"
 #include "chrome/browser/download/download_prefs.h"
-#include "chrome/browser/extensions/api/messaging/native_message_process_host.h"
+#include "chrome/browser/extensions/api/messaging/message_service.h"
 #include "chrome/browser/extensions/crx_installer.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/unpacked_installer.h"
@@ -3172,9 +3172,9 @@ IN_PROC_BROWSER_TEST_F(PolicyTest, NativeMessagingBlacklistSelective) {
   UpdateProviderPolicy(policies);
 
   PrefService* prefs = browser()->profile()->GetPrefs();
-  EXPECT_FALSE(extensions::NativeMessageProcessHost::IsHostAllowed(
+  EXPECT_FALSE(extensions::MessageService::IsNativeMessagingHostAllowed(
       prefs, "host.name"));
-  EXPECT_TRUE(extensions::NativeMessageProcessHost::IsHostAllowed(
+  EXPECT_TRUE(extensions::MessageService::IsNativeMessagingHostAllowed(
       prefs, "other.host.name"));
 }
 
@@ -3187,9 +3187,9 @@ IN_PROC_BROWSER_TEST_F(PolicyTest, NativeMessagingBlacklistWildcard) {
   UpdateProviderPolicy(policies);
 
   PrefService* prefs = browser()->profile()->GetPrefs();
-  EXPECT_FALSE(extensions::NativeMessageProcessHost::IsHostAllowed(
+  EXPECT_FALSE(extensions::MessageService::IsNativeMessagingHostAllowed(
       prefs, "host.name"));
-  EXPECT_FALSE(extensions::NativeMessageProcessHost::IsHostAllowed(
+  EXPECT_FALSE(extensions::MessageService::IsNativeMessagingHostAllowed(
       prefs, "other.host.name"));
 }
 
@@ -3206,9 +3206,9 @@ IN_PROC_BROWSER_TEST_F(PolicyTest, NativeMessagingWhitelist) {
   UpdateProviderPolicy(policies);
 
   PrefService* prefs = browser()->profile()->GetPrefs();
-  EXPECT_TRUE(extensions::NativeMessageProcessHost::IsHostAllowed(
+  EXPECT_TRUE(extensions::MessageService::IsNativeMessagingHostAllowed(
       prefs, "host.name"));
-  EXPECT_FALSE(extensions::NativeMessageProcessHost::IsHostAllowed(
+  EXPECT_FALSE(extensions::MessageService::IsNativeMessagingHostAllowed(
       prefs, "other.host.name"));
 }
 
