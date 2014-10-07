4c68d6ff5633dbbb87cee1ed25fcb6d5f4e983e7
anujk.sharma <anujk.sharma@samsung.com>
2014-10-07 00:29:42 -0700

    Replacing the OVERRIDE with override and FINAL with final in /src/printing
    This step is a giant search and replace for OVERRIDE and FINAL to
    replace them with their lowercase versions.
    
    BUG=417463
    
    Review URL: https://codereview.chromium.org/628133002
    
    Cr-Commit-Position: refs/heads/master@{#298414}

--- printing/printing_context_system_dialog_win.h
+++ printing/printing_context_system_dialog_win.h
@@ -26,8 +26,8 @@ class PRINTING_EXPORT PrintingContextSytemDialogWin
   virtual void AskUserForSettings(
       int max_pages,
       bool has_selection,
-      const PrintSettingsCallback& callback) OVERRIDE;
-  virtual void Cancel() OVERRIDE;
+      const PrintSettingsCallback& callback) override;
+  virtual void Cancel() override;
 
  private:
   friend class MockPrintingContextWin;
