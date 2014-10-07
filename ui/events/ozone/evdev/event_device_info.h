923328e70970c2c825e35f57959044454cfbfcf4
spang <spang@chromium.org>
2014-10-07 13:07:40 -0700

    ozone: evdev: Factor macros into event_device_util.h
    Prep for using them inside libgestures_glue.
    
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/631963002
    
    Cr-Commit-Position: refs/heads/master@{#298553}

--- ui/events/ozone/evdev/event_device_info.h
+++ ui/events/ozone/evdev/event_device_info.h
@@ -10,10 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <linux/input.h>
 
 #include "base/basictypes.h"
+#include "ui/events/ozone/evdev/event_device_util.h"
 #include "ui/events/ozone/evdev/events_ozone_evdev_export.h"
-
-#define EVDEV_LONG_BITS (CHAR_BIT * sizeof(long))
-#define EVDEV_BITS_TO_LONGS(x) (((x) + EVDEV_LONG_BITS - 1) / EVDEV_LONG_BITS)
 
 namespace ui {
 
---
+++ ui/events/ozone/evdev/event_device_util.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_EVENTS_OZONE_EVDEV_EVENT_DEVICE_UTIL_H_
+#define UI_EVENTS_OZONE_EVDEV_EVENT_DEVICE_UTIL_H_
+
+#include <limits.h>
+
+namespace ui {
+
+#define EVDEV_LONG_BITS (CHAR_BIT * sizeof(long))
+#define EVDEV_BITS_TO_LONGS(x) (((x) + EVDEV_LONG_BITS - 1) / EVDEV_LONG_BITS)
+
+static inline int EvdevBitIsSet(const unsigned long* data, int bit) {
+  return data[bit / EVDEV_LONG_BITS] & (1UL << (bit % EVDEV_LONG_BITS));
+}
+
+}  // namespace ui
+
+#endif  // UI_EVENTS_OZONE_EVDEV_EVENT_DEVICE_UTIL_H_
