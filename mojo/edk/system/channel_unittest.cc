5f035ae42c46d97879055db4c2350db3cb161c43
Viet-Trung Luu <viettrungluu@chromium.org>
2014-10-07 13:57:55 -0700

    Mojo: Make ChannelEndpointId a struct.
    (This is another step to locally allocating remote endpoint IDs.)
    
    R=brettw@chromium.org
    
    Review URL: https://codereview.chromium.org/634973002
    
    Cr-Commit-Position: refs/heads/master@{#298568}

--- mojo/edk/system/channel_unittest.cc
+++ mojo/edk/system/channel_unittest.cc
@@ -200,7 +200,7 @@ TEST_F(ChannelTest, CloseBeforeRun) {
       MessagePipe::CreateLocalProxy(&channel_endpoint));
 
   ChannelEndpointId local_id = channel()->AttachEndpoint(channel_endpoint);
-  EXPECT_EQ(kBootstrapChannelEndpointId, local_id);
+  EXPECT_EQ(ChannelEndpointId::GetBootstrap(), local_id);
 
   mp->Close(0);
 
@@ -210,7 +210,7 @@ TEST_F(ChannelTest, CloseBeforeRun) {
   // |AttachEndpoint()| to indicate whether |Run...()| will necessarily be
   // called or not. (Then, in the case that it may not be called, this will
   // return false.)
-  channel()->RunEndpoint(channel_endpoint, kBootstrapChannelEndpointId);
+  channel()->RunEndpoint(channel_endpoint, ChannelEndpointId::GetBootstrap());
 
   io_thread()->PostTaskAndWait(
       FROM_HERE,
@@ -238,14 +238,14 @@ TEST_F(ChannelTest, ShutdownAfterAttach) {
       MessagePipe::CreateLocalProxy(&channel_endpoint));
 
   ChannelEndpointId local_id = channel()->AttachEndpoint(channel_endpoint);
-  EXPECT_EQ(kBootstrapChannelEndpointId, local_id);
+  EXPECT_EQ(ChannelEndpointId::GetBootstrap(), local_id);
 
   // TODO(vtl): Currently, we always "expect" a |RunMessagePipeEndpoint()| after
   // an |AttachEndpoint()| (which is actually incorrect). We need to refactor
   // |AttachEndpoint()| to indicate whether |Run...()| will necessarily be
   // called or not. (Then, in the case that it may not be called, we should test
   // a |Shutdown()| without the |Run...()|.)
-  channel()->RunEndpoint(channel_endpoint, kBootstrapChannelEndpointId);
+  channel()->RunEndpoint(channel_endpoint, ChannelEndpointId::GetBootstrap());
 
   Waiter waiter;
   waiter.Init();
@@ -289,9 +289,9 @@ TEST_F(ChannelTest, WaitAfterAttachRunAndShutdown) {
       MessagePipe::CreateLocalProxy(&channel_endpoint));
 
   ChannelEndpointId local_id = channel()->AttachEndpoint(channel_endpoint);
-  EXPECT_EQ(kBootstrapChannelEndpointId, local_id);
+  EXPECT_EQ(ChannelEndpointId::GetBootstrap(), local_id);
 
-  channel()->RunEndpoint(channel_endpoint, kBootstrapChannelEndpointId);
+  channel()->RunEndpoint(channel_endpoint, ChannelEndpointId::GetBootstrap());
 
   io_thread()->PostTaskAndWait(
       FROM_HERE,
