05bcda4096d1b3ab1b8c251cd2f3572331b0d9ff
jianli <jianli@chromium.org>
2014-10-06 20:03:53 -0700

    Keep the GCM data intact when the user signs out of the profile
    BUG=419168
    TEST=tests updated
    
    Review URL: https://codereview.chromium.org/617003005
    
    Cr-Commit-Position: refs/heads/master@{#298343}

--- components/gcm_driver/gcm_driver_android.h
+++ components/gcm_driver/gcm_driver_android.h
@@ -46,6 +46,7 @@ class GCMDriverAndroid : public GCMDriver {
 
   // GCMDriver implementation:
   virtual void OnSignedIn() override;
+  virtual void OnSignedOut() override;
   virtual void Purge() override;
   virtual void Enable() override;
   virtual void AddConnectionObserver(GCMConnectionObserver* observer) override;
