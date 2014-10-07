84d04d15ed9d2c1cc02104e31a117722356b7637
weiliangc <weiliangc@chromium.org>
2014-10-06 17:04:40 -0700

    Clean the usage of QuadList (Const)Iterator
    Use C++11 feature such as auto and range based for loop to replace
    current usage of QuadList Iterators.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/615253009
    
    Cr-Commit-Position: refs/heads/master@{#298311}

--- cc/quads/list_container.cc
+++ cc/quads/list_container.cc
@@ -280,7 +280,7 @@ void ListContainer<BaseElementType>::EraseAndInvalidateAllPointers(
 
 template <typename BaseElementType>
 typename ListContainer<BaseElementType>::ConstReverseIterator
-ListContainer<BaseElementType>::rbegin() const {
+ListContainer<BaseElementType>::crbegin() const {
   if (data_->IsEmpty())
     return ConstReverseIterator(data_.get(), 0, NULL, 0);
 
@@ -291,8 +291,20 @@ ListContainer<BaseElementType>::rbegin() const {
 
 template <typename BaseElementType>
 typename ListContainer<BaseElementType>::ConstReverseIterator
-ListContainer<BaseElementType>::rend() const {
+ListContainer<BaseElementType>::crend() const {
   return ConstReverseIterator(data_.get(), 0, NULL, size());
+}
+
+template <typename BaseElementType>
+typename ListContainer<BaseElementType>::ConstReverseIterator
+ListContainer<BaseElementType>::rbegin() const {
+  return crbegin();
+}
+
+template <typename BaseElementType>
+typename ListContainer<BaseElementType>::ConstReverseIterator
+ListContainer<BaseElementType>::rend() const {
+  return crend();
 }
 
 template <typename BaseElementType>
@@ -314,7 +326,7 @@ ListContainer<BaseElementType>::rend() {
 
 template <typename BaseElementType>
 typename ListContainer<BaseElementType>::ConstIterator
-ListContainer<BaseElementType>::begin() const {
+ListContainer<BaseElementType>::cbegin() const {
   if (data_->IsEmpty())
     return ConstIterator(data_.get(), 0, NULL, 0);
 
@@ -323,12 +335,24 @@ ListContainer<BaseElementType>::begin() const {
 
 template <typename BaseElementType>
 typename ListContainer<BaseElementType>::ConstIterator
-ListContainer<BaseElementType>::end() const {
+ListContainer<BaseElementType>::cend() const {
   if (data_->IsEmpty())
     return ConstIterator(data_.get(), 0, NULL, size());
 
   size_t last_id = data_->list_count() - 1;
   return ConstIterator(data_.get(), last_id, NULL, size());
+}
+
+template <typename BaseElementType>
+typename ListContainer<BaseElementType>::ConstIterator
+ListContainer<BaseElementType>::begin() const {
+  return cbegin();
+}
+
+template <typename BaseElementType>
+typename ListContainer<BaseElementType>::ConstIterator
+ListContainer<BaseElementType>::end() const {
+  return cend();
 }
 
 template <typename BaseElementType>
