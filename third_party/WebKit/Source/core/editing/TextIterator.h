5f751eaba443a87951b948a5dac8090cb0296a4c
c.shu@samsung.com <c.shu@samsung.com>
2014-10-07 15:47:14 +0000

    In text iterator, the SearchBuffer is reset when the iterator passes through a 0-length boundary, such as IMG element. This causes failure to find texts containing parts from both before and after the image.
    BUG=419524
    
    Review URL: https://codereview.chromium.org/624483002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183345 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/TextIterator.h
+++ third_party/WebKit/Source/core/editing/TextIterator.h
@@ -47,7 +47,8 @@ enum TextIteratorBehavior {
     TextIteratorStopsOnFormControls = 1 << 4,
     TextIteratorEmitsImageAltText = 1 << 5,
     TextIteratorEntersAuthorShadowRoots = 1 << 6,
-    TextIteratorEmitsObjectReplacementCharacter = 1 << 7
+    TextIteratorEmitsObjectReplacementCharacter = 1 << 7,
+    TextIteratorDoesNotBreakAtReplacedElement = 1 << 8
 };
 typedef unsigned TextIteratorBehaviorFlags;
 
@@ -119,6 +120,8 @@ public:
     int endOffset() const;
     Position startPosition() const;
     Position endPosition() const;
+
+    bool breaksAtReplacedElement() { return m_breaksAtReplacedElement; }
 
     // Computes the length of the given range using a text iterator. The default
     // iteration behavior is to always emit object replacement characters for
@@ -229,6 +232,8 @@ private:
     bool m_entersAuthorShadowRoots;
 
     bool m_emitsObjectReplacementCharacter;
+
+    bool m_breaksAtReplacedElement;
 };
 
 // Iterates through the DOM range, returning all the text, and 0-length boundaries
