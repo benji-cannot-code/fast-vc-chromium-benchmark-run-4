f7969b83c68107dde69cffab3dc7b65abcff769e
heeyoun.lee@samsung.com <heeyoun.lee@samsung.com>
2014-10-07 17:22:36 +0000

    Remove Gamepad runtime flag (status=stable) for blink
    BUG=402536
    
    Review URL: https://codereview.chromium.org/582363002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183354 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/gamepad/NavigatorGamepad.cpp
+++ third_party/WebKit/Source/modules/gamepad/NavigatorGamepad.cpp
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "modules/gamepad/GamepadEvent.h"
 #include "modules/gamepad/GamepadList.h"
 #include "modules/gamepad/WebKitGamepadList.h"
-#include "platform/RuntimeEnabledFeatures.h"
 
 namespace blink {
 
@@ -227,7 +226,7 @@ static bool isGamepadEvent(const AtomicString& eventType)
 
 void NavigatorGamepad::didAddEventListener(LocalDOMWindow*, const AtomicString& eventType)
 {
-    if (RuntimeEnabledFeatures::gamepadEnabled() && isGamepadEvent(eventType)) {
+    if (isGamepadEvent(eventType)) {
         if (page() && page()->visibilityState() == PageVisibilityStateVisible)
             startUpdating();
         m_hasEventListener = true;
