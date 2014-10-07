38394dd9a474e7113db637939346f0790733a15d
viettrungluu <viettrungluu@chromium.org>
2014-10-06 19:38:10 -0700

    Mojo: Change MOJO_COMPILE_ASSERT -> static_assert in mojo/.
    Except under mojo/public/c/system, where we rename MOJO_COMPILE_ASSERT
    -> MOJO_STATIC_ASSERT and keep it around (but always use static_assert
    when compiling as C++). (We need to keep some macro around, since we
    want static assertions in headers that may be compiled as C.)
    
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/632743003
    
    Cr-Commit-Position: refs/heads/master@{#298337}

--- mojo/public/cpp/bindings/tests/validation_test_input_parser.cc
+++ mojo/public/cpp/bindings/tests/validation_test_input_parser.cc
@@ -276,7 +276,7 @@ bool ValidationTestInputParser::ParseSignedInteger(
 
 bool ValidationTestInputParser::ParseFloat(
     const DataType& type, const std::string& value_string) {
-  MOJO_COMPILE_ASSERT(sizeof(float) == 4, float_size_is_not_4);
+  static_assert(sizeof(float) == 4, "sizeof(float) is not 4");
 
   float value;
   if (sscanf(value_string.c_str(), "%f", &value) != 1)
@@ -288,7 +288,7 @@ bool ValidationTestInputParser::ParseFloat(
 
 bool ValidationTestInputParser::ParseDouble(const DataType& type,
                                             const std::string& value_string) {
-  MOJO_COMPILE_ASSERT(sizeof(double) == 8, double_size_is_not_8);
+  static_assert(sizeof(double) == 8, "sizeof(double) is not 8");
 
   double value;
   if (sscanf(value_string.c_str(), "%lf", &value) != 1)
