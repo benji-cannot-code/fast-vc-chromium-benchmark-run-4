181fcef2c6f3caccafe33a029243f332eb9d119f
wangxianzhu@chromium.org <wangxianzhu@chromium.org>
2014-10-07 18:25:52 +0000

    Allow RenderObject::renderName() to be called before style is set
    Sometimes we may want to print renderName() or showRenderTree()
    in the first setStyle() during debugging a renderer process.
    For now they will crash the debugged renderer process and we must
    reload the page and reattach gdb to the new renderer process.
    
    Add a check in RenderBlock, RenderInline and
    RenderDeprecatedFlexiblebox's renderName() to allow it to be 
    called before style is set.
    
    TEST=RenderBlockTest.RenderNameBeforeSetStyle
    TEST=RenderInlineTest.RenderNameBeforeSetStyle
    
    Review URL: https://codereview.chromium.org/621943002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183356 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/core.gypi
+++ third_party/WebKit/Source/core/core.gypi
@@ -3583,6 +3583,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'loader/MixedContentCheckerTest.cpp',
             'page/NetworkStateNotifierTest.cpp',
             'page/PrintContextTest.cpp',
+            'rendering/RenderBlockTest.cpp',
+            'rendering/RenderInlineTest.cpp',
             'rendering/RenderOverflowTest.cpp',
             'rendering/RenderPartTest.cpp',
             'rendering/RenderTableCellTest.cpp',
---
+++ third_party/WebKit/Source/core/rendering/RenderBlockTest.cpp
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "config.h"
+#include "core/rendering/RenderBlock.h"
+
+#include "core/rendering/RenderBlockFlow.h"
+#include "core/rendering/RenderingTestHelper.h"
+#include <gtest/gtest.h>
+
+namespace blink {
+
+class RenderBlockTest : public RenderingTest {
+};
+
+TEST_F(RenderBlockTest, RenderNameCalledWithNullStyle)
+{
+    RenderObject* obj = RenderBlockFlow::createAnonymous(&document());
+    EXPECT_FALSE(obj->style());
+    EXPECT_STREQ("RenderBlock (generated)", obj->renderName());
+    obj->destroy();
+}
+
+}
