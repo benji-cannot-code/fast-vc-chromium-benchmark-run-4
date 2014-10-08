18c753786a7203ea34795f004c5045aa39fe5a91
yoav@yoav.ws <yoav@yoav.ws>
2014-10-08 09:35:36 +0000

    Enable float source size
    The source-size in the sizes parsing algorithm was wrongfully returned as unsigned. This CL fixes that to enable floats there.
    
    BUG=418905
    
    Review URL: https://codereview.chromium.org/633643003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183401 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/parser/SizesAttributeParserTest.cpp
+++ third_party/WebKit/Source/core/css/parser/SizesAttributeParserTest.cpp
@@ -15,7 +15,7 @@ namespace blink {
 
 typedef struct {
     const char* input;
-    const unsigned effectiveSize;
+    const float effectiveSize;
     const bool viewportDependant;
 } TestCase;
 
@@ -69,6 +69,7 @@ TEST(SizesAttributeParserTest, Basic)
         {"{{},1px", 500, true},
         {"[[],1px", 500, true},
         {"x(x(),1px", 500, true},
+        {"(max-width: 3000px) 50.5px, 40vw", 50.5, false},
         {0, 0, false} // Do not remove the terminator line.
     };
 
