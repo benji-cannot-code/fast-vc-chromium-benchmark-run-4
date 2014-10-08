ab6ee8a4d17666a77e66c6963db6b9c0da50d143
dalecurtis@chromium.org <dalecurtis@chromium.org>
2014-10-08 02:52:17 +0000

    Add layout tests for seeks to zero on streaming sources.
    Adds testing for the Chrome side patch:
    https://codereview.chromium.org/621573002
    
    BUG=412562
    TEST=all tests!
    
    Review URL: https://codereview.chromium.org/615193004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183373 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/media/resources/load-video.php
+++ third_party/WebKit/LayoutTests/http/tests/media/resources/load-video.php
@@ -3,10 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     $fileName = $_GET["name"];
     $type = $_GET["type"];
+    $norange = $_GET["norange"];
 
     $_GET = array();
     $_GET['name'] = $fileName;
     $_GET['type'] = $type;
-    @include("./serve-video.php"); 
+    $_GET['norange'] = $norange;
+    @include("./serve-video.php");
 
 ?>
