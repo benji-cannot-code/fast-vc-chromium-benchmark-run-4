732706eac8e2c31b86a828245d96c66335407e47
dtrainor <dtrainor@chromium.org>
2014-10-08 11:05:24 -0700

    Fix reinterpret_cast issues
    It looks like these places are using incorrect casting (reinterpret_cast instead of
    static_cast) for GetUserData.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/629503004
    
    Cr-Commit-Position: refs/heads/master@{#298743}

--- content/browser/service_worker/service_worker_request_handler.cc
+++ content/browser/service_worker/service_worker_request_handler.cc
@@ -104,7 +104,7 @@ void ServiceWorkerRequestHandler::InitializeHandler(
 
 ServiceWorkerRequestHandler* ServiceWorkerRequestHandler::GetHandler(
     net::URLRequest* request) {
-  return reinterpret_cast<ServiceWorkerRequestHandler*>(
+  return static_cast<ServiceWorkerRequestHandler*>(
       request->GetUserData(&kUserDataKey));
 }
 
