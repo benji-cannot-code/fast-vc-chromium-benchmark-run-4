2ed2003104c058582a1433344fddf60b3ce49369
chrishtr@chromium.org <chrishtr@chromium.org>
2014-10-08 06:35:41 +0000

    Move painting code from RenderFileUploadControl to FileUploadControlPainter.
    BUG=412088
    
    Review URL: https://codereview.chromium.org/634013002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183388 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/Source/core/paint/FileUploadControlPainter.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef FileUploadControlPainter_h
+#define FileUploadControlPainter_h
+
+namespace blink {
+
+struct PaintInfo;
+class LayoutPoint;
+class RenderFileUploadControl;
+
+class FileUploadControlPainter {
+public:
+    FileUploadControlPainter(RenderFileUploadControl& renderFileUploadControl) : m_renderFileUploadControl(renderFileUploadControl) { }
+
+    void paintObject(PaintInfo&, const LayoutPoint&);
+
+private:
+    RenderFileUploadControl& m_renderFileUploadControl;
+};
+
+} // namespace blink
+
+#endif // FileUploadControlPainter_h
--- third_party/WebKit/Source/core/rendering/RenderFileUploadControl.h
+++ third_party/WebKit/Source/core/rendering/RenderFileUploadControl.h
@@ -42,6 +42,11 @@ public:
     String buttonValue();
     String fileTextValue() const;
 
+    HTMLInputElement* uploadButton() const;
+    int uploadButtonWidth();
+
+    static const int afterButtonSpacing = 4;
+
 private:
     virtual const char* renderName() const OVERRIDE { return "RenderFileUploadControl"; }
 
@@ -53,8 +58,6 @@ private:
     int maxFilenameWidth() const;
 
     virtual PositionWithAffinity positionForPoint(const LayoutPoint&) OVERRIDE;
-
-    HTMLInputElement* uploadButton() const;
 
     bool m_canReceiveDroppedFiles;
 };
