776ec759386cdd3465e6a13a94ce288e5110fb7b
rune@opera.com <rune@opera.com>
2014-10-08 15:07:40 +0000

    Use C++11 range based iteration for invalidation sets.
    Shortens down the source and improves readability.
    Also removed a left-over include.
    
    R=chrishtr@chromium.org
    
    Review URL: https://codereview.chromium.org/633343002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183416 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/RuleFeature.cpp
+++ third_party/WebKit/Source/core/css/RuleFeature.cpp
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/css/invalidation/DescendantInvalidationSet.h"
 #include "core/dom/Element.h"
 #include "core/dom/Node.h"
-#include "platform/RuntimeEnabledFeatures.h"
 #include "wtf/BitVector.h"
 
 namespace blink {
@@ -307,10 +306,10 @@ void RuleFeatureSet::addFeaturesToInvalidationSets(const CSSSelector& selector, 
                     invalidationSet->addId(features.id);
                 if (!features.tagName.isEmpty())
                     invalidationSet->addTagName(features.tagName);
-                for (Vector<AtomicString>::const_iterator it = features.classes.begin(); it != features.classes.end(); ++it)
-                    invalidationSet->addClass(*it);
-                for (Vector<AtomicString>::const_iterator it = features.attributes.begin(); it != features.attributes.end(); ++it)
-                    invalidationSet->addAttribute(*it);
+                for (const auto& className : features.classes)
+                    invalidationSet->addClass(className);
+                for (const auto& attribute : features.attributes)
+                    invalidationSet->addAttribute(attribute);
                 if (features.customPseudoElement)
                     invalidationSet->setCustomPseudoInvalid();
             }
@@ -450,14 +449,14 @@ void RuleFeatureSet::FeatureMetadata::clear()
 
 void RuleFeatureSet::add(const RuleFeatureSet& other)
 {
-    for (InvalidationSetMap::const_iterator it = other.m_classInvalidationSets.begin(); it != other.m_classInvalidationSets.end(); ++it)
-        ensureClassInvalidationSet(it->key).combine(*it->value);
-    for (InvalidationSetMap::const_iterator it = other.m_attributeInvalidationSets.begin(); it != other.m_attributeInvalidationSets.end(); ++it)
-        ensureAttributeInvalidationSet(it->key).combine(*it->value);
-    for (InvalidationSetMap::const_iterator it = other.m_idInvalidationSets.begin(); it != other.m_idInvalidationSets.end(); ++it)
-        ensureIdInvalidationSet(it->key).combine(*it->value);
-    for (PseudoTypeInvalidationSetMap::const_iterator it = other.m_pseudoInvalidationSets.begin(); it != other.m_pseudoInvalidationSets.end(); ++it)
-        ensurePseudoInvalidationSet(static_cast<CSSSelector::PseudoType>(it->key)).combine(*it->value);
+    for (const auto& invalidationSet : other.m_classInvalidationSets)
+        ensureClassInvalidationSet(invalidationSet.key).combine(*invalidationSet.value);
+    for (const auto& invalidationSet : other.m_attributeInvalidationSets)
+        ensureAttributeInvalidationSet(invalidationSet.key).combine(*invalidationSet.value);
+    for (const auto& invalidationSet : other.m_idInvalidationSets)
+        ensureIdInvalidationSet(invalidationSet.key).combine(*invalidationSet.value);
+    for (const auto& invalidationSet : other.m_pseudoInvalidationSets)
+        ensurePseudoInvalidationSet(static_cast<CSSSelector::PseudoType>(invalidationSet.key)).combine(*invalidationSet.value);
 
     m_metadata.add(other.m_metadata);
 
