3ccf7d2a179c07ea5fdaa73c1c34be37f1f20aff
anand.ratn <anand.ratn@samsung.com>
2014-10-08 22:55:01 -0700

    Replace OVERRIDE and FINAL with override and final in src/apps
    BUG=417463
    
    Review URL: https://codereview.chromium.org/641963002
    
    Cr-Commit-Position: refs/heads/master@{#298808}

--- apps/app_restore_service_factory.h
+++ apps/app_restore_service_factory.h
@@ -32,8 +32,8 @@ class AppRestoreServiceFactory : public BrowserContextKeyedServiceFactory {
 
   // BrowserContextKeyedServiceFactory:
   virtual KeyedService* BuildServiceInstanceFor(
-      content::BrowserContext* profile) const OVERRIDE;
-  virtual bool ServiceIsCreatedWithBrowserContext() const OVERRIDE;
+      content::BrowserContext* profile) const override;
+  virtual bool ServiceIsCreatedWithBrowserContext() const override;
 };
 
 }  // namespace apps
