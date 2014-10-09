48c8078c8f810bec16d5e09ee92a85f387e2a150
qinmin <qinmin@chromium.org>
2014-10-09 16:18:09 -0700

    Preroll decoders when video starts playing
    When video starts playing, HTMLMediaElement no longer issues a seek(0) call.
    As a result, decoders needs to be prerolled so that video and audio will start outputting frames at the same time.
    This change also fixes an issue that when EOS is reached, the decoder does not clean up the prerolling state
    
    internal b/17812723
    
    BUG=
    
    Review URL: https://codereview.chromium.org/638233002
    
    Cr-Commit-Position: refs/heads/master@{#299006}

--- media/base/android/media_source_player_unittest.cc
+++ media/base/android/media_source_player_unittest.cc
@@ -356,7 +356,10 @@ class MediaSourcePlayerTest : public testing::Test {
                 new_current_time.InMilliseconds());
       current_time = new_current_time;
       if (manager_.timestamp_updated()) {
-        EXPECT_LT(start_timestamp.InMillisecondsF(),
+        // TODO(qinmin): the current time is from the decoder thread and it does
+        // not take the delay from posting the task into consideration.
+        // http://crbug.com/421616.
+        EXPECT_LE(start_timestamp.InMillisecondsF(),
                   new_current_time.InMillisecondsF());
         return;
       }
@@ -1179,9 +1182,9 @@ TEST_F(MediaSourcePlayerTest, StartTimeTicksResetAfterDecoderUnderruns) {
 
   DecodeAudioDataUntilOutputBecomesAvailable();
 
-  // The decoder job should finish and a new request will be sent.
-  base::TimeTicks previous = StartTimeTicks();
+  // The decoder job should finish prerolling and start prefetching.
   player_.OnDemuxerDataAvailable(CreateReadFromDemuxerAckForAudio(3));
+  base::TimeTicks previous = StartTimeTicks();
 
   // Let the decoder starve.
   TriggerPlayerStarvation();
@@ -1739,6 +1742,12 @@ TEST_F(MediaSourcePlayerTest, SimultaneousAudioVideoConfigChange) {
   EnableAdaptiveVideoPlayback(false);
   WaitForAudioVideoDecodeDone();
 
+  EXPECT_TRUE(IsPrerolling(true));
+  EXPECT_TRUE(IsPrerolling(false));
+  PrerollDecoderToTime(true, base::TimeDelta(), base::TimeDelta(), true);
+  PrerollDecoderToTime(false, base::TimeDelta(), base::TimeDelta(), false);
+  int expected_num_data_requests = demuxer_->num_data_requests();
+
   // Simulate audio |kConfigChanged| prefetched as standalone access unit.
   DemuxerConfigs audio_configs = CreateAudioDemuxerConfigs(kCodecVorbis, true);
   player_.OnDemuxerDataAvailable(
@@ -1748,7 +1757,7 @@ TEST_F(MediaSourcePlayerTest, SimultaneousAudioVideoConfigChange) {
   player_.OnDemuxerDataAvailable(
       CreateReadFromDemuxerAckWithConfigChanged(
           false, 0, CreateVideoDemuxerConfigs(true)));
-  EXPECT_EQ(6, demuxer_->num_data_requests());
+  EXPECT_EQ(expected_num_data_requests + 2, demuxer_->num_data_requests());
   EXPECT_TRUE(IsDrainingDecoder(true));
   EXPECT_TRUE(IsDrainingDecoder(false));
 
