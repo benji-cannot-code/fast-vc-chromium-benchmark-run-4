3a2873ed12dc7536e2b5ec424e8d91a40d76f7d2
hshi@chromium.org <hshi@chromium.org>
2014-10-09 06:39:51 +0000

    Revert "Mixed Content: Use a static method to check form actions."
    This reverts commit a26c8b8f48571a58267fc527d363c53c893bbd3a.
    
    Revert "Mixed Content: Extract frame processing from ::shouldBlockFetch."
    
    This reverts commit 828cf97a2a2dfe6316bb2d510ef0bd0061c4046f.
    
    Reason for revert: see https://code.google.com/p/chromium/issues/detail?id=421550
    
    BUG=421550
    BUG=411184
    TBR=mkwst@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/638253002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183440 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/MixedContentChecker.h
+++ third_party/WebKit/Source/core/loader/MixedContentChecker.h
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define MixedContentChecker_h
 
 #include "platform/heap/Handle.h"
-#include "platform/network/ResourceRequest.h"
 #include "public/platform/WebURLRequest.h"
 #include "wtf/text/WTFString.h"
 
@@ -50,12 +49,7 @@ class MixedContentChecker final {
 public:
     explicit MixedContentChecker(LocalFrame*);
 
-    static bool shouldBlockFetch(LocalFrame* frame, const ResourceRequest& request, const KURL& url)
-    {
-        return shouldBlockFetch(frame, request.requestContext(), request.frameType(), url);
-    }
-    static bool shouldBlockFetch(LocalFrame*, WebURLRequest::RequestContext, WebURLRequest::FrameType, const KURL&);
-    static bool checkFormAction(LocalFrame*, const KURL&);
+    static bool shouldBlockFetch(LocalFrame*, const ResourceRequest&, const KURL&);
 
     bool canDisplayInsecureContent(SecurityOrigin* securityOrigin, const KURL& url) const
     {
@@ -67,6 +61,7 @@ public:
         return canRunInsecureContentInternal(securityOrigin, url, MixedContentChecker::Execution);
     }
 
+    bool canSubmitToInsecureForm(SecurityOrigin*, const KURL&) const;
     bool canConnectInsecureWebSocket(SecurityOrigin*, const KURL&) const;
     bool canFrameInsecureContent(SecurityOrigin*, const KURL&) const;
     static bool isMixedContent(SecurityOrigin*, const KURL&);
@@ -89,8 +84,6 @@ private:
         ContextTypeShouldBeBlockable,
         ContextTypeBlockableUnlessLax
     };
-
-    static LocalFrame* inWhichFrameIsThisContentMixed(LocalFrame*, WebURLRequest::RequestContext, WebURLRequest::FrameType, const KURL&);
 
     static ContextType contextTypeFromContext(WebURLRequest::RequestContext);
     static const char* typeNameFromContext(WebURLRequest::RequestContext);
