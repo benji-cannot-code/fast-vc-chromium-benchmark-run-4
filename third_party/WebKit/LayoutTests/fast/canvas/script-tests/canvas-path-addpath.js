ecd02e086cccab7c20ff06eeb36364a580943ea1
piotaixr@chromium.org <piotaixr@chromium.org>
2014-10-09 18:53:15 +0000

    Reenable test marked as flaky
    Unable to repro locally and disabling too old: no information about flakiness.
    
    BUG=368531
    
    Review URL: https://codereview.chromium.org/632353004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183483 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/canvas/script-tests/canvas-path-addpath.js
+++ third_party/WebKit/LayoutTests/fast/canvas/script-tests/canvas-path-addpath.js
@@ -3,7 +3,7 @@ description("Test addPath() method.");
 var canvas = document.createElement('canvas');
 var ctx = canvas.getContext('2d');
 
-debug("Test addPath() with transform as identity matrix.")
+debug("Test addPath() with transform as identity matrix.");
 ctx.clearRect(0, 0, canvas.width, canvas.height);
 ctx.beginPath();
 var p1 = new Path2D();
@@ -22,7 +22,7 @@ shouldBe("imgdata[6]", "0");
 shouldBe("imgdata[7]", "255");
 debug("");
 
-debug("Test addPath() with transform as translate(100, -100).")
+debug("Test addPath() with transform as translate(100, -100).");
 ctx.clearRect(0, 0, canvas.width, canvas.height);
 ctx.beginPath();
 var p3 = new Path2D();
@@ -43,7 +43,7 @@ shouldBe("imgdata[6]", "0");
 shouldBe("imgdata[7]", "255");
 debug("");
 
-debug("Test addPath() with non-invertible transform.")
+debug("Test addPath() with non-invertible transform.");
 ctx.clearRect(0, 0, canvas.width, canvas.height);
 ctx.beginPath();
 var p5 = new Path2D();
@@ -64,7 +64,7 @@ shouldBe("imgdata[6]", "0");
 shouldNotBe("imgdata[7]", "255");
 debug("");
 
-debug("Test addPath() with transform as null or invalid type.")
+debug("Test addPath() with transform as null or invalid type.");
 ctx.clearRect(0, 0, canvas.width, canvas.height);
 ctx.beginPath();
 var p7 = new Path2D();
@@ -84,7 +84,7 @@ shouldBe("imgdata[6]", "0");
 shouldBe("imgdata[7]", "255");
 debug("");
 
-debug("Test addPath() with transform omitted.")
+debug("Test addPath() with transform omitted.");
 ctx.clearRect(0, 0, canvas.width, canvas.height);
 ctx.beginPath();
 var p9 = new Path2D();
