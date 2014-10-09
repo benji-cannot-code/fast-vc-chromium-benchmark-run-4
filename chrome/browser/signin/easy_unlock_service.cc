3e285e7c6528ead888895a3a9d62260b374f4952
xiyuan <xiyuan@chromium.org>
2014-10-09 10:00:36 -0700

    easy-signin: Add a finch config to control it.
    BUG=419883
    
    Review URL: https://codereview.chromium.org/639913002
    
    Cr-Commit-Position: refs/heads/master@{#298912}

--- chrome/browser/signin/easy_unlock_service.cc
+++ chrome/browser/signin/easy_unlock_service.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/command_line.h"
 #include "base/logging.h"
+#include "base/metrics/field_trial.h"
 #include "base/prefs/pref_registry_simple.h"
 #include "base/prefs/pref_service.h"
 #include "base/prefs/scoped_user_pref_update.h"
@@ -38,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/easy_unlock/easy_unlock_key_manager.h"
 #include "chrome/browser/chromeos/login/session/user_session_manager.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
+#include "chromeos/chromeos_switches.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/power_manager_client.h"
 #endif
@@ -73,6 +75,23 @@ EasyUnlockService* EasyUnlockService::GetForUser(
   return EasyUnlockService::Get(profile);
 #else
   return NULL;
+#endif
+}
+
+// static
+bool EasyUnlockService::IsSignInEnabled() {
+#if defined(OS_CHROMEOS)
+  const std::string group_name =
+      base::FieldTrialList::FindFullName("EasySignIn");
+
+  if (CommandLine::ForCurrentProcess()->HasSwitch(
+          chromeos::switches::kDisableEasySignin)) {
+    return false;
+  }
+
+  return group_name == "Enable";
+#else
+  return false;
 #endif
 }
 
