0110e55d18312b1b792f12cbac6be162b3bce225
anujk.sharma <anujk.sharma@samsung.com>
2014-10-09 10:06:51 -0700

    [components] Apply automatic range checks to content enum types across IPC
    This is part of a long-running background task to remove the remaining
    uses of the unchecked IPC_ENUM_TRAITS() macro.
    
    BUG= 246708
    
    Review URL: https://codereview.chromium.org/646543003
    
    Cr-Commit-Position: refs/heads/master@{#298915}

--- components/translate/content/common/translate_messages.h
+++ components/translate/content/common/translate_messages.h
@@ -13,7 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #define IPC_MESSAGE_START TranslateMsgStart
 
-IPC_ENUM_TRAITS(translate::TranslateErrors::Type)
+IPC_ENUM_TRAITS_MAX_VALUE(translate::TranslateErrors::Type,
+                          translate::TranslateErrors::TYPE_LAST)
 
 IPC_STRUCT_TRAITS_BEGIN(translate::LanguageDetectionDetails)
   IPC_STRUCT_TRAITS_MEMBER(time)
