1595af65f236ceac21fa65459e9007c306e509a2
elijahtaylor <elijahtaylor@chromium.org>
2014-10-08 21:51:41 -0700

    Remove "resources" subkey and functionality for Shared Modules
    BUG=419936
    
    Review URL: https://codereview.chromium.org/625713002
    
    Cr-Commit-Position: refs/heads/master@{#298790}

--- chrome/test/data/extensions/api_test/shared_module/import_pass/check.js
+++ chrome/test/data/extensions/api_test/shared_module/import_pass/check.js
@@ -3,9 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-if (typeof fail_exported != 'undefined')
-  chrome.test.notifyFail('fail.js was unintentionally exported.');
-
 if (typeof pass_exported == 'undefined')
   chrome.test.notifyFail('pass.js was not exported correctly.');
 
