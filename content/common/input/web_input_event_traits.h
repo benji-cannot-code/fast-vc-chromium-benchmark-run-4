9e38cead2a0e358711fa2f3e26854758c4832b9d
jdduke <jdduke@chromium.org>
2014-10-09 11:49:33 -0700

    Allow small tolerance when validating MotionEvent orientation
    Validating MotionEvent orientation values should account for minor error
    during conversion. Add a small tolerance while also separating the range
    DCHECK to make any future errors immediately clear to the user. Also
    add a basic MotionEvent to WebTouchEvent conversion test and sanitize
    unreasonably large orientation values on Android.
    
    BUG=421202
    
    Review URL: https://codereview.chromium.org/631373003
    
    Cr-Commit-Position: refs/heads/master@{#298945}

--- content/common/input/web_input_event_traits.h
+++ content/common/input/web_input_event_traits.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_COMMON_INPUT_WEB_INPUT_EVENT_TRAITS_H_
 #define CONTENT_COMMON_INPUT_WEB_INPUT_EVENT_TRAITS_H_
 
+#include <string>
+
 #include "base/basictypes.h"
 #include "content/common/input/scoped_web_input_event.h"
 #include "third_party/WebKit/public/web/WebInputEvent.h"
@@ -16,6 +18,7 @@ namespace content {
 class CONTENT_EXPORT WebInputEventTraits {
  public:
   static const char* GetName(blink::WebInputEvent::Type type);
+  static std::string ToString(const blink::WebInputEvent& event);
   static size_t GetSize(blink::WebInputEvent::Type type);
   static ScopedWebInputEvent Clone(const blink::WebInputEvent& event);
   static void Delete(blink::WebInputEvent* event);
--- content/common/input/web_input_event_traits_unittest.cc
+++ content/common/input/web_input_event_traits_unittest.cc
@@ -12,6 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using blink::WebGestureEvent;
 using blink::WebInputEvent;
+using blink::WebKeyboardEvent;
+using blink::WebMouseEvent;
+using blink::WebMouseWheelEvent;
 using blink::WebTouchEvent;
 using blink::WebTouchPoint;
 using std::numeric_limits;
@@ -158,6 +161,28 @@ TEST_F(WebInputEventTraitsTest, PinchEventCoalescing) {
   EXPECT_TRUE(WebInputEventTraits::CanCoalesce(pinch0, pinch1));
   WebInputEventTraits::Coalesce(pinch0, &pinch1);
   EXPECT_EQ(numeric_limits<float>::max(), pinch1.data.pinchUpdate.scale);
+}
+
+// Very basic smoke test to ensure stringification doesn't explode.
+TEST_F(WebInputEventTraitsTest, ToString) {
+  WebKeyboardEvent key;
+  key.type = WebInputEvent::RawKeyDown;
+  EXPECT_FALSE(WebInputEventTraits::ToString(key).empty());
+
+  WebMouseEvent mouse;
+  mouse.type = WebInputEvent::MouseMove;
+  EXPECT_FALSE(WebInputEventTraits::ToString(mouse).empty());
+
+  WebMouseEvent mouse_wheel;
+  mouse_wheel.type = WebInputEvent::MouseWheel;
+  EXPECT_FALSE(WebInputEventTraits::ToString(mouse_wheel).empty());
+
+  WebGestureEvent gesture =
+      CreateGesture(WebInputEvent::GesturePinchBegin, 1, 1);
+  EXPECT_FALSE(WebInputEventTraits::ToString(gesture).empty());
+
+  WebTouchEvent touch = CreateTouch(WebInputEvent::TouchStart);
+  EXPECT_FALSE(WebInputEventTraits::ToString(touch).empty());
 }
 
 }  // namespace
