0a007dd01fcbae8d98d4d515c256f8b974b59e6b
mlamouri <mlamouri@chromium.org>
2014-10-09 14:51:37 -0700

    [WebMIDI] Remove not needed CancelPermission IPC message.
    BUG=421085
    
    Review URL: https://codereview.chromium.org/642623005
    
    Cr-Commit-Position: refs/heads/master@{#298986}

--- content/browser/media/midi_dispatcher_host.h
+++ content/browser/media/midi_dispatcher_host.h
@@ -23,6 +23,10 @@ class MidiDispatcherHost : public WebContentsObserver {
   virtual ~MidiDispatcherHost();
 
   // WebContentsObserver implementation.
+  virtual void RenderFrameDeleted(RenderFrameHost* render_frame_host) override;
+  virtual void DidNavigateAnyFrame(RenderFrameHost* render_frame_host,
+                                   const LoadCommittedDetails& details,
+                                   const FrameNavigateParams& params) override;
   virtual bool OnMessageReceived(const IPC::Message& message,
                                  RenderFrameHost* render_frame_host) override;
 
@@ -38,6 +42,7 @@ class MidiDispatcherHost : public WebContentsObserver {
                                  int render_frame_id,
                                  int bridge_id,
                                  bool is_allowed);
+  void CancelPermissionRequestsForFrame(RenderFrameHost* render_frame_host);
 
   struct PendingPermission {
     PendingPermission(int render_process_id,
