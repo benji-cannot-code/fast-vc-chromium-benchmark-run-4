cf019936c9310c61bc9d452c0ac4098dcea5a9bb
kalman <kalman@chromium.org>
2014-10-08 21:52:52 -0700

    Docserver: cache the compiled JSON files. This cuts down preview.py render runtime to a third.
    R=rockot@chromium.org
    
    Review URL: https://codereview.chromium.org/637253002
    
    Cr-Commit-Position: refs/heads/master@{#298792}

--- chrome/common/extensions/docs/server2/compiled_file_system.py
+++ chrome/common/extensions/docs/server2/compiled_file_system.py
@@ -122,7 +122,7 @@ class CompiledFileSystem(object):
       '''
       return self.Create(file_system,
                          Cache(SingleFile(lambda _, data:
-                             json_parse.Parse(ToUnicode(data)))),
+                                          json_parse.Parse(ToUnicode(data)))),
                          CompiledFileSystem,
                          category='json')
 
@@ -132,7 +132,8 @@ class CompiledFileSystem(object):
       '''
       return self.Create(
           file_system,
-          SingleFile(lambda path, text: Motemplate(ToUnicode(text), name=path)),
+          Cache(SingleFile(lambda path, text:
+                           Motemplate(ToUnicode(text), name=path))),
           CompiledFileSystem)
 
     @memoize
