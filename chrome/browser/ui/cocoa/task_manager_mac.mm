208130efdb5271e98523266e28306295011a114b
mlerman <mlerman@chromium.org>
2014-10-09 07:08:37 -0700

    Locking a profile closes the Task Manager.
    BUG=414686
    TEST=Ensure --new-profile-management is enabled. Have one signed in profile. Open the Chrome Task Manager. Lock your profile - the task manager should close.
    
    Review URL: https://codereview.chromium.org/630193002
    
    Cr-Commit-Position: refs/heads/master@{#298871}

--- chrome/browser/ui/cocoa/task_manager_mac.mm
+++ chrome/browser/ui/cocoa/task_manager_mac.mm
@@ -540,11 +540,21 @@ void TaskManagerMac::Show() {
   instance_->model_->StartUpdating();
 }
 
+// static
+void TaskManagerMac::Hide() {
+  if (instance_)
+    [instance_->window_controller_ close];
+}
+
 namespace chrome {
 
 // Declared in browser_dialogs.h.
 void ShowTaskManager(Browser* browser) {
   TaskManagerMac::Show();
+}
+
+void HideTaskManager() {
+  TaskManagerMac::Hide();
 }
 
 }  // namespace chrome
