be5bf5e4a927998bbeaa54cbe48e747646b68e9a
sdefresne <sdefresne@chromium.org>
2014-10-09 03:08:28 -0700

    Fix includes for refcounted_browser_context_keyed_service_factory.h
    Remove unnecessary includes and add missing ones.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/637853003
    
    Cr-Commit-Position: refs/heads/master@{#298835}

--- components/keyed_service/content/refcounted_browser_context_keyed_service_factory.h
+++ components/keyed_service/content/refcounted_browser_context_keyed_service_factory.h
@@ -6,10 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_KEYED_SERVICE_CONTENT_REFCOUNTED_BROWSER_CONTEXT_KEYED_SERVICE_FACTORY_H_
 #define COMPONENTS_KEYED_SERVICE_CONTENT_REFCOUNTED_BROWSER_CONTEXT_KEYED_SERVICE_FACTORY_H_
 
+#include <map>
+
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
 #include "base/memory/ref_counted.h"
-#include "components/keyed_service/content/browser_context_keyed_service_factory.h"
+#include "components/keyed_service/content/browser_context_keyed_base_factory.h"
 #include "components/keyed_service/core/keyed_service_export.h"
 #include "components/keyed_service/core/refcounted_keyed_service.h"
 
