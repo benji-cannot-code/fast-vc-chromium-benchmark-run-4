8f7296997e1b76734ee9f412d1b6b1961dd12fed
aurimas <aurimas@chromium.org>
2014-10-08 22:02:43 -0700

    Add FindInPageBridge
    First CL to upstream FindInPage functionality.
    
    BUG=332413
    
    Review URL: https://codereview.chromium.org/633743002
    
    Cr-Commit-Position: refs/heads/master@{#298801}

--- chrome/browser/android/chrome_jni_registrar.cc
+++ chrome/browser/android/chrome_jni_registrar.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/dom_distiller/feedback_reporter_android.h"
 #include "chrome/browser/android/enhanced_bookmarks/enhanced_bookmarks_bridge.h"
 #include "chrome/browser/android/favicon_helper.h"
+#include "chrome/browser/android/find_in_page/find_in_page_bridge.h"
 #include "chrome/browser/android/foreign_session_helper.h"
 #include "chrome/browser/android/intent_helper.h"
 #include "chrome/browser/android/logo_bridge.h"
@@ -145,6 +146,7 @@ static base::android::RegistrationMethod kChromeRegisteredMethods[] = {
       RegisterExternalPrerenderHandlerAndroid },
   { "FaviconHelper", FaviconHelper::RegisterFaviconHelper },
   { "FeedbackReporter", dom_distiller::android::RegisterFeedbackReporter },
+  { "FindInPageBridge", FindInPageBridge::RegisterFindInPageBridge },
   { "FontSizePrefsAndroid", FontSizePrefsAndroid::Register },
   { "ForeignSessionHelper",
     ForeignSessionHelper::RegisterForeignSessionHelper },
