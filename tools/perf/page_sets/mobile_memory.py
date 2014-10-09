18725941f1fc9540d1e3c85769471d662fb802ae
nednguyen <nednguyen@google.com>
2014-10-09 08:59:22 -0700

    [Telemetry] Replace RunStressMemorey with RunPageInteraction
    BUG=418375
    
    Review URL: https://codereview.chromium.org/632013002
    
    Cr-Commit-Position: refs/heads/master@{#298883}

--- tools/perf/page_sets/mobile_memory.py
+++ tools/perf/page_sets/mobile_memory.py
@@ -33,7 +33,7 @@ class GmailPage(MobileMemoryPage):
     action_runner.Wait(15)
     action_runner.ForceGarbageCollection()
 
-  def RunStressMemory(self, action_runner):
+  def RunPageInteractions(self, action_runner):
     for _ in xrange(3):
       self.ReloadAndGc(action_runner)
 
@@ -47,7 +47,7 @@ class GoogleSearchPage(MobileMemoryPage):
         url='https://www.google.com/search?site=&tbm=isch&q=google',
         page_set=page_set)
 
-  def RunStressMemory(self, action_runner):
+  def RunPageInteractions(self, action_runner):
     interaction = action_runner.BeginGestureInteraction(
         'ScrollAction', is_smooth=True)
     action_runner.ScrollPage()
@@ -76,7 +76,7 @@ class ScrollPage(MobileMemoryPage):
   def __init__(self, url, page_set):
     super(ScrollPage, self).__init__(url=url, page_set=page_set)
 
-  def RunStressMemory(self, action_runner):
+  def RunPageInteractions(self, action_runner):
     interaction = action_runner.BeginGestureInteraction(
         'ScrollAction', is_smooth=True)
     action_runner.ScrollPage()
