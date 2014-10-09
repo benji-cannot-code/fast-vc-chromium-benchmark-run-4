f77344773ce3b2a2feb4f276fc89402fdd9816a1
hansmuller <hansmuller@chromium.org>
2014-10-09 11:54:59 -0700

    mojo_shell --help
    Print a brief message that itemizes most of the command-line options and offers a few hints about how they are to be used:
    
    % mojo_shell --help
    Launch Mojo applications.
    Usage: mojo_shell [--args-for=<mojo-app>] [--content-handlers=<handlers>] [--enable-external-applications] [--disable-cache] [--enable-multiprocess] [--origin=<url-lib-path>] <mojo-app> ...
    
    A <mojo-app> is a Mojo URL or a Mojo URL and arguments within quotes.
    Example: mojo_shell "mojo://mojo_js_standalone test.js".
    <url-lib-path> is searched for shared libraries named by mojo URLs.
    The value of <handlers> is a comma separated list like:
    text/html,mojo://mojo_html_viewer,application/javascript,mojo://mojo_js_content_handler
    
    BUG=420743
    
    Review URL: https://codereview.chromium.org/630233002
    
    Cr-Commit-Position: refs/heads/master@{#298948}

--- mojo/shell/switches.h
+++ mojo/shell/switches.h
@@ -9,8 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace switches {
 
 // All switches in alphabetical order. The switches should be documented
-// alongside the definition of their values in the .cc file.
+// alongside the definition of their values in the .cc file and, as needed,
+// in mojo_main's Usage() function.
 extern const char kArgsFor[];
+extern const char kHelp[];
 extern const char kChildProcessType[];
 extern const char kContentHandlers[];
 extern const char kDisableCache[];
