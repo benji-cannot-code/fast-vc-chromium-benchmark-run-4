e76a8d8cd3c8dd173290481d27195b80f5fcf341
vadimt <vadimt@chromium.org>
2014-10-10 14:52:15 -0700

    Mechanical change that adds instrumentation required to locate the source of jankiness (i.e. a long-running fragment of code executed as a part of the task that causes jank) in the code. See the bug for details on what kind of jank we are after. A number of similar CLs were landed, and none of them caused issues. The code of the instrumentation is highly optimized and is not expected to affect performance. The code simply creates a diagnostic task which is identical to ones created by PostTask or IPC message handlers. Landing as TBR since this is a mechanical, safe and temporary change.
    BUG=422460
    TBR=isherman@chromium.org, caitkp@chromium.org
    
    Review URL: https://codereview.chromium.org/647123002
    
    Cr-Commit-Position: refs/heads/master@{#299198}

--- components/autofill/core/browser/autocomplete_history_manager.cc
+++ components/autofill/core/browser/autocomplete_history_manager.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/prefs/pref_service.h"
+#include "base/profiler/scoped_profile.h"
 #include "base/strings/string16.h"
 #include "base/strings/utf_string_conversions.h"
 #include "components/autofill/core/browser/autofill_client.h"
@@ -54,6 +55,11 @@ AutocompleteHistoryManager::~AutocompleteHistoryManager() {
 void AutocompleteHistoryManager::OnWebDataServiceRequestDone(
     WebDataServiceBase::Handle h,
     const WDTypedResult* result) {
+  // TODO(vadimt): Remove ScopedProfile below once crbug.com/422460 is fixed.
+  tracked_objects::ScopedProfile tracking_profile(
+      FROM_HERE_WITH_EXPLICIT_FUNCTION(
+          "422460 AutocompleteHistoryManager::OnWebDataServiceRequestDone"));
+
   DCHECK(pending_query_handle_);
   pending_query_handle_ = 0;
 
