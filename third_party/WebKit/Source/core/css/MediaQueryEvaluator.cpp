3da26347d6444160b05f09ab747a40cc8fd1cc4c
yoav@yoav.ws <yoav@yoav.ws>
2014-10-10 22:07:26 +0000

    Floating point MQs should not match.
    Floating point values inside MQs should not be round down/up
    to match the integer dimensions of the viewport. That is in order to align our behavior with the way Gecko are doing that.
    This CL fixes that issue.
    
    BUG=422234
    
    Review URL: https://codereview.chromium.org/644813005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183563 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/MediaQueryEvaluator.cpp
+++ third_party/WebKit/Source/core/css/MediaQueryEvaluator.cpp
@@ -327,7 +327,7 @@ static bool gridMediaFeatureEval(const MediaQueryExpValue& value, MediaFeaturePr
     return false;
 }
 
-static bool computeLength(const MediaQueryExpValue& value, const MediaValues& mediaValues, int& result)
+static bool computeLength(const MediaQueryExpValue& value, const MediaValues& mediaValues, double& result)
 {
     if (!value.isValue)
         return false;
@@ -342,12 +342,17 @@ static bool computeLength(const MediaQueryExpValue& value, const MediaValues& me
     return false;
 }
 
+static bool computeLengthAndCompare(const MediaQueryExpValue& value, MediaFeaturePrefix op, const MediaValues& mediaValues, double compareToValue)
+{
+    double length;
+    return computeLength(value, mediaValues, length) && compareValue(compareToValue, length, op);
+}
+
 static bool deviceHeightMediaFeatureEval(const MediaQueryExpValue& value, MediaFeaturePrefix op, const MediaValues& mediaValues)
 {
-    if (value.isValid()) {
-        int length;
-        return computeLength(value, mediaValues, length) && compareValue(static_cast<int>(mediaValues.deviceHeight()), length, op);
-    }
+    if (value.isValid())
+        return computeLengthAndCompare(value, op, mediaValues, mediaValues.deviceHeight());
+
     // ({,min-,max-}device-height)
     // assume if we have a device, assume non-zero
     return true;
@@ -355,10 +360,9 @@ static bool deviceHeightMediaFeatureEval(const MediaQueryExpValue& value, MediaF
 
 static bool deviceWidthMediaFeatureEval(const MediaQueryExpValue& value, MediaFeaturePrefix op, const MediaValues& mediaValues)
 {
-    if (value.isValid()) {
-        int length;
-        return computeLength(value, mediaValues, length) && compareValue(static_cast<int>(mediaValues.deviceWidth()), length, op);
-    }
+    if (value.isValid())
+        return computeLengthAndCompare(value, op, mediaValues, mediaValues.deviceWidth());
+
     // ({,min-,max-}device-width)
     // assume if we have a device, assume non-zero
     return true;
@@ -367,10 +371,8 @@ static bool deviceWidthMediaFeatureEval(const MediaQueryExpValue& value, MediaFe
 static bool heightMediaFeatureEval(const MediaQueryExpValue& value, MediaFeaturePrefix op, const MediaValues& mediaValues)
 {
     int height = mediaValues.viewportHeight();
-    if (value.isValid()) {
-        int length;
-        return computeLength(value, mediaValues, length) && compareValue(height, length, op);
-    }
+    if (value.isValid())
+        return computeLengthAndCompare(value, op, mediaValues, height);
 
     return height;
 }
@@ -378,10 +380,8 @@ static bool heightMediaFeatureEval(const MediaQueryExpValue& value, MediaFeature
 static bool widthMediaFeatureEval(const MediaQueryExpValue& value, MediaFeaturePrefix op, const MediaValues& mediaValues)
 {
     int width = mediaValues.viewportWidth();
-    if (value.isValid()) {
-        int length;
-        return computeLength(value, mediaValues, length) && compareValue(width, length, op);
-    }
+    if (value.isValid())
+        return computeLengthAndCompare(value, op, mediaValues, width);
 
     return width;
 }
