db1eefc00d2682596c8bf64e9647344b2f8b5656
perkj <perkj@chromium.org>
2014-10-09 23:53:44 -0700

    Updated the MockWebRtcPeerConnectionHandler object used in blink Layouttests. This cl change the mock to create remote mediastreams based on the mediastreams that have been added as local mediastreams. The remote streams are updated once RTCPeerConnection::setRemoteDescription is called. The purpose is to be able to test remote mediastreams in blink layouttests.
    BUG=417245
    
    Review URL: https://codereview.chromium.org/597283006
    
    Cr-Commit-Position: refs/heads/master@{#299069}

--- content/shell/renderer/test_runner/mock_webrtc_peer_connection_handler.h
+++ content/shell/renderer/test_runner/mock_webrtc_peer_connection_handler.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_SHELL_RENDERER_TEST_RUNNER_MOCKWEBRTCPEERCONNECTIONHANDLER_H_
 #define CONTENT_SHELL_RENDERER_TEST_RUNNER_MOCKWEBRTCPEERCONNECTIONHANDLER_H_
 
+#include <map>
+
 #include "base/basictypes.h"
 #include "content/shell/renderer/test_runner/web_task.h"
 #include "third_party/WebKit/public/platform/WebRTCPeerConnectionHandler.h"
@@ -27,6 +29,7 @@ class MockWebRTCPeerConnectionHandler
   MockWebRTCPeerConnectionHandler(
       blink::WebRTCPeerConnectionHandlerClient* client,
       TestInterfaces* interfaces);
+  virtual ~MockWebRTCPeerConnectionHandler();
 
   // WebRTCPeerConnectionHandler related methods
   virtual bool initialize(
@@ -76,6 +79,12 @@ class MockWebRTCPeerConnectionHandler
  private:
   MockWebRTCPeerConnectionHandler();
 
+  // UpdateRemoteStreams uses the collection of |local_streams_| to create
+  // remote MediaStreams with the same number of tracks and notifies |client_|
+  // about added and removed streams. It's triggered when setRemoteDescription
+  // is called.
+  void UpdateRemoteStreams();
+
   blink::WebRTCPeerConnectionHandlerClient* client_;
   bool stopped_;
   WebTaskList task_list_;
@@ -83,6 +92,9 @@ class MockWebRTCPeerConnectionHandler
   blink::WebRTCSessionDescription remote_description_;
   int stream_count_;
   TestInterfaces* interfaces_;
+  typedef std::map<std::string, blink::WebMediaStream> StreamMap;
+  StreamMap local_streams_;
+  StreamMap remote_streams_;
 
   DISALLOW_COPY_AND_ASSIGN(MockWebRTCPeerConnectionHandler);
 };
