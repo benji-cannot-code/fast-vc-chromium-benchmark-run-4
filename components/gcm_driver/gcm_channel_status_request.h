d9337747afb25d6e71284b45b9642981531c90d7
fgorski <fgorski@chromium.org>
2014-10-10 14:04:18 -0700

    [GCM] Updates to GCMChannelStatusSyncer - UserAgent and server URL.
    BUG=421208
    R=zea@chromium.org
    
    Review URL: https://codereview.chromium.org/635073002
    
    Cr-Commit-Position: refs/heads/master@{#299185}

--- components/gcm_driver/gcm_channel_status_request.h
+++ components/gcm_driver/gcm_channel_status_request.h
@@ -32,6 +32,8 @@ class GCMChannelStatusRequest : public net::URLFetcherDelegate {
 
   GCMChannelStatusRequest(
       const scoped_refptr<net::URLRequestContextGetter>& request_context_getter,
+      const std::string& channel_status_request_url,
+      const std::string& user_agent,
       const GCMChannelStatusRequestCallback& callback);
   virtual ~GCMChannelStatusRequest();
 
@@ -48,6 +50,8 @@ class GCMChannelStatusRequest : public net::URLFetcherDelegate {
   void RetryWithBackoff(bool update_backoff);
 
   scoped_refptr<net::URLRequestContextGetter> request_context_getter_;
+  const std::string channel_status_request_url_;
+  const std::string user_agent_;
   GCMChannelStatusRequestCallback callback_;
   scoped_ptr<net::URLFetcher> url_fetcher_;
   net::BackoffEntry backoff_entry_;
