ddbd461edb2d7d1d97cbf89dce20fca676998eef
mef <mef@chromium.org>
2014-10-10 09:32:44 -0700

    Fix compilation error in url_request_adapter.cc.
    BUG=412942
    TBR=mmenke@chromium.org
    
    Review URL: https://codereview.chromium.org/643963002
    
    Cr-Commit-Position: refs/heads/master@{#299125}

--- components/cronet/android/url_request_adapter.cc
+++ components/cronet/android/url_request_adapter.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "components/cronet/android/url_request_context_adapter.h"
 #include "components/cronet/android/wrapped_channel_upload_element_reader.h"
-#include "net/base/element_upload_data_stream.h"
+#include "net/base/elements_upload_data_stream.h"
 #include "net/base/load_flags.h"
 #include "net/base/upload_bytes_element_reader.h"
 #include "net/http/http_status_code.h"
