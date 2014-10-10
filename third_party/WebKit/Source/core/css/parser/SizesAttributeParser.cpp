ee793a101b005ae749dd9b1cb485e9dbe7e90d75
kochi@chromium.org <kochi@chromium.org>
2014-10-10 04:45:38 +0000

    Use C++11 range-based loop in css/parser
    BUG=none
    TEST=no layout test failures
    
    Review URL: https://codereview.chromium.org/646483002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183510 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/parser/SizesAttributeParser.cpp
+++ third_party/WebKit/Source/core/css/parser/SizesAttributeParser.cpp
@@ -124,13 +124,13 @@ bool SizesAttributeParser::parse(Vector<CSSParserToken>& tokens)
     CSSParserTokenIterator startToken = tokens.begin();
     CSSParserTokenIterator endToken;
     // Split on a comma token, and send the result tokens to be parsed as (media-condition, length) pairs
-    for (CSSParserTokenIterator token = tokens.begin(); token != tokens.end(); ++token) {
-        m_blockWatcher.handleToken(*token);
-        if (token->type() == CommaToken && !m_blockWatcher.blockLevel()) {
-            endToken = token;
+    for (auto& token : tokens) {
+        m_blockWatcher.handleToken(token);
+        if (token.type() == CommaToken && !m_blockWatcher.blockLevel()) {
+            endToken = &token;
             if (parseMediaConditionAndLength(startToken, endToken))
                 return true;
-            startToken = token;
+            startToken = &token;
             ++startToken;
         }
     }
@@ -153,4 +153,3 @@ unsigned SizesAttributeParser::effectiveSizeDefaultValue()
 }
 
 } // namespace
-
