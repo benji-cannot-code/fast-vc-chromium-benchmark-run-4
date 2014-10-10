cb3db83de84c587791524b195129145810383321
jkarlin@chromium.org <jkarlin@chromium.org>
2014-10-10 00:39:33 +0000

    Let the client (browser) set blobs in WebServiceWorkerResponse.
    This lets ServiceWorkerCache return a response to Blink with a blob for the body.
    
    This CL is a dependency of: https://codereview.chromium.org/622833002/
    
    BUG=419417
    
    Review URL: https://codereview.chromium.org/623753002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183500 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/serviceworkers/Response.cpp
+++ third_party/WebKit/Source/modules/serviceworkers/Response.cpp
@@ -35,6 +35,8 @@ FetchResponseData* createFetchResponseDataFromWebResponse(const WebServiceWorker
         response->headerList()->append(i->key, i->value);
     }
 
+    response->setBlobDataHandle(webResponse.blobDataHandle());
+
     // Filter the response according to |webResponse|'s ResponseType.
     switch (webResponse.responseType()) {
     case WebServiceWorkerResponseTypeBasic:
@@ -161,7 +163,6 @@ Response* Response::create(ExecutionContext* context, FetchResponseData* respons
 
 Response* Response::create(ExecutionContext* context, const WebServiceWorkerResponse& webResponse)
 {
-    // FIXME: Handle response body data.
     FetchResponseData* responseData = createFetchResponseDataFromWebResponse(webResponse);
     Response* r = new Response(context, responseData);
     r->suspendIfNeeded();
