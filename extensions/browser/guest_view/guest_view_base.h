dbfbd8e97bf4a02b78d3b360087933c8fc3c6eeb
lfg <lfg@chromium.org>
2014-10-10 08:18:56 -0700

    Remove the RenderProcessHost observer and attach the WebContentsObserver earlier to the GuestView.
    This fixes an issue where if the embedder WebContents is destroyed during a GuestView creation we may end up leaking the guest WebContents.
    
    BUG=419020
    
    Committed: https://crrev.com/52fce9455ffd688ef3752a816192f9e76555e7d1
    Cr-Commit-Position: refs/heads/master@{#298934}
    
    Review URL: https://codereview.chromium.org/642573002
    
    Cr-Commit-Position: refs/heads/master@{#299117}

--- extensions/browser/guest_view/guest_view_base.h
+++ extensions/browser/guest_view/guest_view_base.h
@@ -28,7 +28,6 @@ namespace extensions {
 // WebContents. At that point, its lifetime is restricted in scope to the
 // lifetime of its embedder WebContents.
 class GuestViewBase : public content::BrowserPluginGuestDelegate,
-                      public content::RenderProcessHostObserver,
                       public content::WebContentsDelegate,
                       public content::WebContentsObserver {
  public:
@@ -172,10 +171,9 @@ class GuestViewBase : public content::BrowserPluginGuestDelegate,
             const base::DictionaryValue& create_params,
             const WebContentsCreatedCallback& callback);
 
-  void InitWithWebContents(
-      const std::string& embedder_extension_id,
-      int embedder_render_process_id,
-      content::WebContents* guest_web_contents);
+  void InitWithWebContents(const std::string& embedder_extension_id,
+                           content::WebContents* embedder_web_contents,
+                           content::WebContents* guest_web_contents);
 
   bool IsViewType(const char* const view_type) const {
     return !strcmp(GetViewType(), view_type);
@@ -230,12 +228,6 @@ class GuestViewBase : public content::BrowserPluginGuestDelegate,
   void SetAttachParams(const base::DictionaryValue& params);
   void SetOpener(GuestViewBase* opener);
 
-  // RenderProcessHostObserver implementation
-  virtual void RenderProcessExited(content::RenderProcessHost* host,
-                                   base::ProcessHandle handle,
-                                   base::TerminationStatus status,
-                                   int exit_code) override;
-
   // BrowserPluginGuestDelegate implementation.
   virtual void DidAttach(int guest_proxy_routing_id) override final;
   virtual void ElementSizeChanged(const gfx::Size& old_size,
@@ -258,12 +250,12 @@ class GuestViewBase : public content::BrowserPluginGuestDelegate,
   virtual ~GuestViewBase();
 
  private:
-  class EmbedderWebContentsObserver;
+  class EmbedderLifetimeObserver;
 
   void SendQueuedEvents();
 
   void CompleteInit(const std::string& embedder_extension_id,
-                    int embedder_render_process_id,
+                    content::WebContents* embedder_web_contents,
                     const WebContentsCreatedCallback& callback,
                     content::WebContents* guest_web_contents);
 
@@ -321,7 +313,7 @@ class GuestViewBase : public content::BrowserPluginGuestDelegate,
   // guests that are created from this guest.
   scoped_ptr<base::DictionaryValue> attach_params_;
 
-  scoped_ptr<EmbedderWebContentsObserver> embedder_web_contents_observer_;
+  scoped_ptr<EmbedderLifetimeObserver> embedder_web_contents_observer_;
 
   // The size of the container element.
   gfx::Size element_size_;
