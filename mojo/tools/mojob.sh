ba7b44f167ec747f52fe1106ee375850456d9202
qsr <qsr@chromium.org>
2014-10-10 09:29:44 -0700

    mojo: Add python bindings tests to mojob.sh
    R=viettrungluu@chromium.org
    
    Review URL: https://codereview.chromium.org/639713002
    
    Cr-Commit-Position: refs/heads/master@{#299124}

--- mojo/tools/mojob.sh
+++ mojo/tools/mojob.sh
@@ -20,7 +20,7 @@ command should be one of:
   build - Build.
   test - Run unit tests (does not build).
   perftest - Run perf tests (does not build).
-  pytest - Run Python unit tests.
+  pytest - Run Python unit tests (does not build).
   gyp - Run gyp for mojo (does not sync).
   gypall - Run gyp for all of chromium (does not sync).
   sync - Sync using gclient (does not run gyp).
@@ -67,7 +67,14 @@ do_perftests() {
 }
 
 do_pytests() {
+  echo "Running python tests in out/$1 ..."
   python mojo/tools/run_mojo_python_tests.py || exit 1
+  # TODO(qsr) Remove this test when the component build is not supported
+  # anymore.
+  if [ -f "out/$1/python/mojo/system.so" ]; then
+    python mojo/tools/run_mojo_python_bindings_tests.py \
+        "--build-dir=out/$1" || exit 1
+  fi
 }
 
 do_gyp() {
@@ -181,7 +188,8 @@ for arg in "$@"; do
       should_do_Release && do_perftests Release
       ;;
     pytest)
-      do_pytests
+      should_do_Debug && do_pytests Debug
+      should_do_Release && do_pytests Release
       ;;
     gyp)
       set_goma_dir_if_necessary
@@ -237,3 +245,5 @@ for arg in "$@"; do
       ;;
   esac
 done
+
+exit 0
