25d19e947ed464ab09cf8241b10149d27ecf91a5
mek <mek@chromium.org>
2014-10-10 13:16:33 -0700

    Basic implementation of GeofencingManager class.
    This class is the single entry point for all Geofencing related API calls, and will be responsible for storing all geofence registrations, and registering them with the underlying platform specific GeofencingProvider.
    
    BUG=383125
    
    Review URL: https://codereview.chromium.org/586163003
    
    Cr-Commit-Position: refs/heads/master@{#299172}

--- content/browser/geofencing/geofencing_dispatcher_host.h
+++ content/browser/geofencing/geofencing_dispatcher_host.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_GEOFENCING_GEOFENCING_DISPATCHER_HOST_H_
 #define CONTENT_BROWSER_GEOFENCING_GEOFENCING_DISPATCHER_HOST_H_
 
+#include "content/common/geofencing_status.h"
 #include "content/public/browser/browser_message_filter.h"
 
 namespace blink {
@@ -14,9 +15,11 @@ struct WebCircularGeofencingRegion;
 
 namespace content {
 
+class BrowserContext;
+
 class GeofencingDispatcherHost : public BrowserMessageFilter {
  public:
-  GeofencingDispatcherHost();
+  explicit GeofencingDispatcherHost(BrowserContext* browser_context);
 
  private:
   virtual ~GeofencingDispatcherHost();
@@ -32,6 +35,16 @@ class GeofencingDispatcherHost : public BrowserMessageFilter {
                           int request_id,
                           const std::string& region_id);
   void OnGetRegisteredRegions(int thread_id, int request_id);
+
+  void RegisterRegionCompleted(int thread_id,
+                               int request_id,
+                               GeofencingStatus result);
+  void UnregisterRegionCompleted(int thread_id,
+                                 int request_id,
+                                 GeofencingStatus result);
+
+  BrowserContext* browser_context_;
+  base::WeakPtrFactory<GeofencingDispatcherHost> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(GeofencingDispatcherHost);
 };
