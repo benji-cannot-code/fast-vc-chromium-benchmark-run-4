02a519fc9e504e44fae4d6f680cbe6beba849819
tfarina <tfarina@chromium.org>
2014-10-09 20:11:29 -0700

    Move run_webkit_tests.* script into their new home.
    As we are removing webkit/ glue, we need a new home for these scripts.
    
    BUG=338338
    TEST=None
    R=dpranke@chromium.org
    
    Review URL: https://codereview.chromium.org/629083002
    
    Cr-Commit-Position: refs/heads/master@{#299051}

--- blink/tools/run_layout_tests.sh
+++ blink/tools/run_layout_tests.sh
@@ -8,9 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 exec_dir=$(dirname $0)
 
 if [ "$OSTYPE" = "cygwin" ]; then
-  SCRIPT=$(cygpath -wa "$exec_dir/run_webkit_tests.py")
+  SCRIPT=$(cygpath -wa "$exec_dir/run_layout_tests.py")
 else
-  SCRIPT="$exec_dir/run_webkit_tests.py"
+  SCRIPT="$exec_dir/run_layout_tests.py"
 fi
 
 PYTHON_PROG=python
