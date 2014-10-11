9f5fa0ec27dca6c16950e2431d19744f35f7ee3a
hanxi <hanxi@chromium.org>
2014-10-11 03:59:54 -0700

    Adding more webview browser tests to app_shell_browsertests.
    This CL includes the following tests:
    - WebViewAPITest.AcceptTouchEvents
    - WebViewAPITest.EmbedderVisibilityChanged
    - WebViewAPITest.GuestVisibilityChanged
    - WebViewAPITest.ReloadEmbedder
    
    BUG=352293
    
    Review URL: https://codereview.chromium.org/639693005
    
    Cr-Commit-Position: refs/heads/master@{#299243}

---
+++ extensions/test/data/web_view/accept_touch_events/guest.js
@@ -0,0 +1,52 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var LOG = function(msg) {
+  window.console.log(msg);
+};
+
+var touchDiv;
+var embedder;
+
+function init() {
+  touchDiv = document.createElement('div');
+  touchDiv.innerText = 'With touch';
+  document.body.appendChild(touchDiv);
+}
+
+function handler() {}
+
+function onAppCommand(command) {
+  LOG('onAppCommand, command = ' + command);
+  switch (command) {
+    case 'install-touch-handler':
+      touchDiv.addEventListener('touchstart', handler);
+      sendMessageToEmbedder('installed-touch-handler');
+      break;
+    case 'uninstall-touch-handler':
+      touchDiv.removeEventListener('touchstart', handler);
+      sendMessageToEmbedder('uninstalled-touch-handler');
+      break;
+  }
+};
+
+function sendMessageToEmbedder(message) {
+  if (!embedder) {
+     LOG('no embedder channel to send postMessage');
+     return;
+  }
+
+  embedder.postMessage(JSON.stringify([message]), '*');
+}
+
+window.addEventListener('message', function(e) {
+  embedder = e.source;
+  var data = JSON.parse(e.data);
+  if (data[0] == 'connect') {
+    sendMessageToEmbedder('connected');
+  }
+});
+
+window.onload = init;
