62247a91e0a48008897bd886f9bd5de2c4c9e77a
kouhei@chromium.org <kouhei@chromium.org>
2014-10-11 07:38:44 +0000

    Disable async main thread HTML parsing
    This CL depends on CL: https://codereview.chromium.org/646913002/
    
    This CL disables async main thread HTML parsing code path by:
    - forcing HTMLParserOptions::useThreading to be true
    - making all pumpTokenizer calls ForceSynchronous
    
    HTMLDocumentParser has three parsing modes:
    - Synchronous main thread parsing
    - Asynchronous main thread parsing
    - Asynchronous background thread parsing
    
    Asynchronous main thread parsing is a historic feature before
    asynchronous background parsing. Now all use cases that required
    asynchronous main thread parsing are gone, we can disable
    the code path so that we only have two parsing modes.
    
    The HTMLParserOptions::useThreading had comment about where the
    flag was used to force synchronous parsing behavior, but it is
    obsolete as of now:
    - The about:blank url itself does not use HTMLDocumentParser.
    - javascript: urls use about:blank url, but synchronous behavior
      is not controlled by this useThreading flag, but call to
      pinToMainThread from DocumentLoader::replaceDocumentWhileExecutingJavaScriptURL.
    - core/inspector/DOMPatchSupport also explicitly call
      pinToMainThread to force synchronous parsing.
    
    This CL also removes the last asynchronous main thread parsing
    case where:
    - document.write caused recursive document.write via script, and
    - the suspended document.write was resumed asynchronously.
    However I think this behavior was a bug, as the suspended
    document.write should have resumed synchronously.
    To fix this, HTMLDocumentParser::resumeParsingAfterScriptExecution
    is changed to resume parsing synchronously when background thread
    doesn't exist.
    This codepath is tested on fast/loader/external-script-URL-location.html
    
    TEST=fast/loader/external-script-URL-location.html
    BUG=421289
    
    Review URL: https://codereview.chromium.org/644573002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183572 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/parser/HTMLDocumentParser.cpp
+++ third_party/WebKit/Source/core/html/parser/HTMLDocumentParser.cpp
@@ -256,6 +256,8 @@ bool HTMLDocumentParser::processingData() const
 
 void HTMLDocumentParser::pumpTokenizerIfPossible(SynchronousMode mode)
 {
+    ASSERT(mode == ForceSynchronous);
+
     if (isStopped())
         return;
     if (isWaitingForScripts())
@@ -263,7 +265,6 @@ void HTMLDocumentParser::pumpTokenizerIfPossible(SynchronousMode mode)
 
     // Once a resume is scheduled, HTMLParserScheduler controls when we next pump.
     if (isScheduledForResume()) {
-        ASSERT(mode == AllowYield);
         return;
     }
 
@@ -279,19 +280,12 @@ bool HTMLDocumentParser::isScheduledForResume() const
 void HTMLDocumentParser::resumeParsingAfterYield()
 {
     ASSERT(!m_isPinnedToMainThread);
-    // pumpTokenizer can cause this parser to be detached from the Document,
+    ASSERT(m_haveBackgroundParser);
+
+    // pumpPendingSpeculations can cause this parser to be detached from the Document,
     // but we need to ensure it isn't deleted yet.
     RefPtrWillBeRawPtr<HTMLDocumentParser> protect(this);
-
-    if (m_haveBackgroundParser) {
-        pumpPendingSpeculations();
-        return;
-    }
-
-    // We should never be here unless we can pump immediately.  Call pumpTokenizer()
-    // directly so that ASSERTS will fire if we're wrong.
-    pumpTokenizer(AllowYield);
-    endIfDelayed();
+    pumpPendingSpeculations();
 }
 
 void HTMLDocumentParser::runScriptsForPausedTreeBuilder()
@@ -590,7 +584,7 @@ void HTMLDocumentParser::pumpTokenizer(SynchronousMode mode)
 #endif
     ASSERT(m_tokenizer);
     ASSERT(m_token);
-    ASSERT(!m_haveBackgroundParser || mode == ForceSynchronous);
+    ASSERT(mode == ForceSynchronous);
 
     PumpSession session(m_pumpSessionNestingLevel, contextForParsingSession());
 
@@ -815,10 +809,8 @@ void HTMLDocumentParser::append(PassRefPtr<StringImpl> inputSource)
     // javascript: url handling is one such caller.
     // FIXME: This is gross, and we should separate the concept of synchronous parsing
     // from insert() so that only document.write() uses insert.
-    if (m_isPinnedToMainThread)
-        pumpTokenizerIfPossible(ForceSynchronous);
-    else
-        pumpTokenizerIfPossible(AllowYield);
+    ASSERT(m_isPinnedToMainThread);
+    pumpTokenizerIfPossible(ForceSynchronous);
 
     endIfDelayed();
 }
@@ -972,7 +964,7 @@ void HTMLDocumentParser::resumeParsingAfterScriptExecution()
     }
 
     m_insertionPreloadScanner.clear();
-    pumpTokenizerIfPossible(AllowYield);
+    pumpTokenizerIfPossible(ForceSynchronous);
     endIfDelayed();
 }
 
