6e42b760b6480eaf5652f1b9b0c20ebe0efa01ea
ernstm <ernstm@chromium.org>
2014-10-10 17:31:43 -0700

    telemetry: Remove 7 obsolete rendering benchmarks.
    R=tonyg@chromium.org,nduca@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/643273002
    
    Cr-Commit-Position: refs/heads/master@{#299220}

--- tools/perf/benchmarks/silk_flags.py
+++ tools/perf/benchmarks/silk_flags.py
@@ -3,10 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-def CustomizeBrowserOptionsForFastPath(options):
-  """Enables flags needed for bleeding edge rendering fast paths."""
-  options.AppendExtraBrowserArgs('--enable-bleeding-edge-rendering-fast-paths')
-
 def CustomizeBrowserOptionsForSoftwareRasterization(options):
   """Enables flags needed for forced software rasterization."""
   options.AppendExtraBrowserArgs('--disable-gpu-rasterization')
