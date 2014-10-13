21c1d99ded62c436e76cae31c69ffa2367012f01
sievers <sievers@chromium.org>
2014-10-13 14:22:19 -0700

    Android: Blacklist async_readpixels on VMWare/Mesa (emulator)
    BUG=340882
    
    Review URL: https://codereview.chromium.org/648333002
    
    Cr-Commit-Position: refs/heads/master@{#299364}

--- gpu/config/gpu_driver_bug_list_json.cc
+++ gpu/config/gpu_driver_bug_list_json.cc
@@ -20,7 +20,7 @@ const char kGpuDriverBugListJson[] = LONG_STRING_CONST(
 {
   "name": "gpu driver bug list",
   // Please update the version number whenever you change this file.
-  "version": "7.6",
+  "version": "7.7",
   "entries": [
     {
       "id": 1,
@@ -1028,6 +1028,24 @@ LONG_STRING_CONST(
       },
       "features": [
         "disable_d3d11"
+      ]
+    },
+    {
+      "id": 93,
+      "description": "The GL implementation on the Android emulator has problems with PBOs.",
+      "cr_bugs": [340882],
+      "os": {
+        "type": "android"
+      },
+      "gl_vendor": "VMware.*",
+      "gl_renderer": "Gallium.*",
+      "gl_type": "gles",
+      "gl_version": {
+        "op": "=",
+        "value": "3.0"
+      },
+      "features": [
+        "disable_async_readpixels"
       ]
     }
   ]
