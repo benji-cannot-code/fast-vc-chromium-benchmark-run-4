62f0675f3fb2a2bb5e99063d46529a2e0993780f
pkasting <pkasting@chromium.org>
2014-10-13 12:48:53 -0700

    Change overscroll functions to pass floats.
    The source values for these are floats, and the lone nontrivial consumer
    (athena) wants floats.  So avoid truncating to int in the meantime.
    
    BUG=81439
    TEST=none
    
    Review URL: https://codereview.chromium.org/645223003
    
    Cr-Commit-Position: refs/heads/master@{#299347}

--- content/browser/web_contents/web_contents_view_aura_browsertest.cc
+++ content/browser/web_contents/web_contents_view_aura_browsertest.cc
@@ -78,7 +78,7 @@ class VerticalOverscrollTracker : public content::WebContentsDelegate {
     return true;
   }
 
-  virtual void OverscrollUpdate(int delta_y) override {
+  virtual void OverscrollUpdate(float delta_y) override {
     ++count_;
   }
 
