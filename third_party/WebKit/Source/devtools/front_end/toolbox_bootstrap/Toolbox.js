dee794508b96cabe9111c14cb62915196180c1d9
apavlov@chromium.org <apavlov@chromium.org>
2014-10-13 12:10:47 +0000

    DevTools: Fix "toolbox" app startup in the debug_devtools mode
    This was broken by the async module loading.
    
    R=pfeldman@chromium.org,dgozman
    
    Review URL: https://codereview.chromium.org/648153002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183599 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/toolbox_bootstrap/Toolbox.js
+++ third_party/WebKit/Source/devtools/front_end/toolbox_bootstrap/Toolbox.js
@@ -3,13 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-function windowLoaded()
-{
-    window.removeEventListener("DOMContentLoaded", windowLoaded, false);
-    new WebInspector.Toolbox();
-}
-window.addEventListener("DOMContentLoaded", windowLoaded, false);
-
 /**
  * @constructor
  */
@@ -41,3 +34,14 @@ WebInspector.Toolbox = function()
 // FIXME: This stub is invoked from the backend and should be removed
 // once we migrate to the "pull" model for extensions retrieval.
 WebInspector.addExtensions = function() {}
+
+function windowLoaded()
+{
+    window.removeEventListener("DOMContentLoaded", windowLoaded, false);
+    new WebInspector.Toolbox();
+}
+
+if (document.readyState === "complete")
+    new WebInspector.Toolbox();
+else
+    window.addEventListener("DOMContentLoaded", windowLoaded, false);
