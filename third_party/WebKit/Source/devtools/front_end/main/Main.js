17584e79786f733d663607736bf51211a4145948
apavlov@chromium.org <apavlov@chromium.org>
2014-10-13 12:17:33 +0000

    DevTools: Remove InspectorFrontendAPIImpl.prototype.loadCompleted()
    https://crrev.com/6bffde4d87f7a05e5a871cd48366d56b8b83e950 introduces
    dispatching of messages on InspectorFrontendAPI post the
    IFH.loadCompleted() call only, hence the IFAPI.loadCompleted() call
    is no longer required.
    
    R=pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/644493003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183601 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/main/Main.js
+++ third_party/WebKit/Source/devtools/front_end/main/Main.js
@@ -254,9 +254,6 @@ WebInspector.Main.prototype = {
 
         WebInspector.extensionServerProxy.setFrontendReady();
 
-        // FIXME: Remove once the upstream counterpart has landed.
-        InspectorFrontendAPI.loadCompleted();
-
         InspectorFrontendHost.loadCompleted();
 
         // Give UI cycles to repaint, then proceed with creating connection.
