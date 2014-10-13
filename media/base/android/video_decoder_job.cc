2b50e64d75208de04a7bacca640a53fdb35c7209
qinmin <qinmin@chromium.org>
2014-10-13 12:09:35 -0700

    Send metadata change to renderer after decoder is drained
    MediaSourcePlayer sends a metadata change to renderer whenever it receives a config change.
    However, the decoder might still be in the draining process when this happens.
    To fix this, we should wait for a OUTPUT_FORMAT_CHANGED status before sending out the IPC.
    
    BUG=381975
    
    Review URL: https://codereview.chromium.org/623363002
    
    Cr-Commit-Position: refs/heads/master@{#299338}

--- media/base/android/video_decoder_job.cc
+++ media/base/android/video_decoder_job.cc
@@ -34,8 +34,10 @@ VideoDecoderJob::VideoDecoderJob(
                       request_data_cb,
                       on_demuxer_config_changed_cb),
       video_codec_(kUnknownVideoCodec),
-      width_(0),
-      height_(0),
+      config_width_(0),
+      config_height_(0),
+      output_width_(0),
+      output_height_(0),
       request_resources_cb_(request_resources_cb),
       next_video_data_is_iframe_(true) {
 }
@@ -70,6 +72,17 @@ void VideoDecoderJob::ReleaseDecoderResources() {
   surface_ = gfx::ScopedJavaSurface();
 }
 
+void VideoDecoderJob::SetDemuxerConfigs(const DemuxerConfigs& configs) {
+  video_codec_ = configs.video_codec;
+  config_width_ = configs.video_size.width();
+  config_height_ = configs.video_size.height();
+  set_is_content_encrypted(configs.is_video_encrypted);
+  if (!media_codec_bridge_) {
+    output_width_ = config_width_;
+    output_height_ = config_height_;
+  }
+}
+
 void VideoDecoderJob::ReleaseOutputBuffer(
     int output_buffer_index,
     size_t size,
@@ -82,13 +95,6 @@ void VideoDecoderJob::ReleaseOutputBuffer(
 
 bool VideoDecoderJob::ComputeTimeToRender() const {
   return true;
-}
-
-void VideoDecoderJob::UpdateDemuxerConfigs(const DemuxerConfigs& configs) {
-  video_codec_ = configs.video_codec;
-  width_ = configs.video_size.width();
-  height_ = configs.video_size.height();
-  set_is_content_encrypted(configs.is_video_encrypted);
 }
 
 bool VideoDecoderJob::IsCodecReconfigureNeeded(
@@ -115,8 +121,8 @@ bool VideoDecoderJob::AreDemuxerConfigsChanged(
     const DemuxerConfigs& configs) const {
   return video_codec_ != configs.video_codec ||
       is_content_encrypted() != configs.is_video_encrypted ||
-      width_ != configs.video_size.width() ||
-      height_ != configs.video_size.height();
+      config_width_ != configs.video_size.width() ||
+      config_height_ != configs.video_size.height();
 }
 
 bool VideoDecoderJob::CreateMediaCodecBridgeInternal() {
@@ -134,7 +140,7 @@ bool VideoDecoderJob::CreateMediaCodecBridgeInternal() {
       drm_bridge()->IsProtectedSurfaceRequired();
 
   media_codec_bridge_.reset(VideoCodecBridge::CreateDecoder(
-      video_codec_, is_secure, gfx::Size(width_, height_),
+      video_codec_, is_secure, gfx::Size(config_width_, config_height_),
       surface_.j_surface().obj(), GetMediaCrypto().obj()));
 
   if (!media_codec_bridge_)
@@ -146,6 +152,16 @@ bool VideoDecoderJob::CreateMediaCodecBridgeInternal() {
 
 void VideoDecoderJob::CurrentDataConsumed(bool is_config_change) {
   next_video_data_is_iframe_ = is_config_change;
+}
+
+bool VideoDecoderJob::UpdateOutputFormat() {
+  if (!media_codec_bridge_)
+    return false;
+  int prev_output_width = output_width_;
+  int prev_output_height = output_height_;
+  media_codec_bridge_->GetOutputFormat(&output_width_, &output_height_);
+  return (output_width_ != prev_output_width) ||
+      (output_height_ != prev_output_height);
 }
 
 bool VideoDecoderJob::IsProtectedSurfaceRequired() {
