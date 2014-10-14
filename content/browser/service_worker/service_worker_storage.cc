329bdf7233f5ace71124809af068dc295ca5fa33
shimazu <shimazu@chromium.org>
2014-10-14 04:00:54 -0700

    ServiceWorker: Move TRACE_EVENT to avoid early return and mismatching
    BUG=N/A
    TEST=N/A
    
    Review URL: https://codereview.chromium.org/636253004
    
    Cr-Commit-Position: refs/heads/master@{#299458}

--- content/browser/service_worker/service_worker_storage.cc
+++ content/browser/service_worker/service_worker_storage.cc
@@ -253,11 +253,6 @@ void ServiceWorkerStorage::FindRegistrationForDocument(
     const GURL& document_url,
     const FindRegistrationCallback& callback) {
   DCHECK(!document_url.has_ref());
-  TRACE_EVENT_ASYNC_BEGIN1(
-      "ServiceWorker",
-      "ServiceWorkerStorage::FindRegistrationForDocument",
-      base::Hash(document_url.spec()),
-      "URL", document_url.spec());
   if (!LazyInitialize(base::Bind(
           &ServiceWorkerStorage::FindRegistrationForDocument,
           weak_factory_.GetWeakPtr(), document_url, callback))) {
@@ -265,6 +260,11 @@ void ServiceWorkerStorage::FindRegistrationForDocument(
       CompleteFindNow(scoped_refptr<ServiceWorkerRegistration>(),
                       SERVICE_WORKER_ERROR_FAILED, callback);
     }
+    TRACE_EVENT_INSTANT1(
+        "ServiceWorker",
+        "ServiceWorkerStorage::FindRegistrationForDocument:LazyInitialize",
+        TRACE_EVENT_SCOPE_THREAD,
+        "URL", document_url.spec());
     return;
   }
   DCHECK_EQ(INITIALIZED, state_);
@@ -274,14 +274,28 @@ void ServiceWorkerStorage::FindRegistrationForDocument(
     // Look for something currently being installed.
     scoped_refptr<ServiceWorkerRegistration> installing_registration =
         FindInstallingRegistrationForDocument(document_url);
+    ServiceWorkerStatusCode status = installing_registration.get() ?
+        SERVICE_WORKER_OK : SERVICE_WORKER_ERROR_NOT_FOUND;
+    TRACE_EVENT_INSTANT2(
+        "ServiceWorker",
+        "ServiceWorkerStorage::FindRegistrationForDocument:CheckInstalling",
+        TRACE_EVENT_SCOPE_THREAD,
+        "URL", document_url.spec(),
+        "Status", (status == SERVICE_WORKER_OK) ? "Found" : "Not Found");
     CompleteFindNow(installing_registration,
-                    installing_registration.get()
-                        ? SERVICE_WORKER_OK
-                        : SERVICE_WORKER_ERROR_NOT_FOUND,
+                    status,
                     callback);
     return;
   }
 
+  // To connect this TRACE_EVENT with the callback, TimeTicks is used for
+  // callback id.
+  int64 callback_id = base::TimeTicks::Now().ToInternalValue();
+  TRACE_EVENT_ASYNC_BEGIN1(
+      "ServiceWorker",
+      "ServiceWorkerStorage::FindRegistrationForDocument",
+      callback_id,
+      "URL", document_url.spec());
   database_task_runner_->PostTask(
       FROM_HERE,
       base::Bind(
@@ -290,7 +304,10 @@ void ServiceWorkerStorage::FindRegistrationForDocument(
           base::MessageLoopProxy::current(),
           document_url,
           base::Bind(&ServiceWorkerStorage::DidFindRegistrationForDocument,
-                     weak_factory_.GetWeakPtr(), document_url, callback)));
+                     weak_factory_.GetWeakPtr(),
+                     document_url,
+                     callback,
+                     callback_id)));
 }
 
 void ServiceWorkerStorage::FindRegistrationForPattern(
@@ -770,6 +787,7 @@ void ServiceWorkerStorage::DidReadInitialData(
 void ServiceWorkerStorage::DidFindRegistrationForDocument(
     const GURL& document_url,
     const FindRegistrationCallback& callback,
+    int64 callback_id,
     const ServiceWorkerDatabase::RegistrationData& data,
     const ResourceList& resources,
     ServiceWorkerDatabase::Status status) {
@@ -778,7 +796,7 @@ void ServiceWorkerStorage::DidFindRegistrationForDocument(
     TRACE_EVENT_ASYNC_END1(
         "ServiceWorker",
         "ServiceWorkerStorage::FindRegistrationForDocument",
-        base::Hash(document_url.spec()),
+        callback_id,
         "Status", "OK");
     return;
   }
@@ -793,7 +811,7 @@ void ServiceWorkerStorage::DidFindRegistrationForDocument(
     TRACE_EVENT_ASYNC_END1(
         "ServiceWorker",
         "ServiceWorkerStorage::FindRegistrationForDocument",
-        base::Hash(document_url.spec()),
+        callback_id,
         "Status", status);
     return;
   }
@@ -804,7 +822,7 @@ void ServiceWorkerStorage::DidFindRegistrationForDocument(
   TRACE_EVENT_ASYNC_END1(
       "ServiceWorker",
       "ServiceWorkerStorage::FindRegistrationForDocument",
-      base::Hash(document_url.spec()),
+      callback_id,
       "Status", status);
 }
 
