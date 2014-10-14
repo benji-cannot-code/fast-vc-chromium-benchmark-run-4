bbcc1a6ced39870dd789afaf9fa2e1671a4f0fe4
estade <estade@chromium.org>
2014-10-13 18:12:05 -0700

    Add test for requestAutocomplete and readonly attribute.
    BUG=231160
    
    Review URL: https://codereview.chromium.org/650293002
    
    Cr-Commit-Position: refs/heads/master@{#299411}

--- chrome/browser/ui/autofill/autofill_dialog_controller_impl.h
+++ chrome/browser/ui/autofill/autofill_dialog_controller_impl.h
@@ -333,6 +333,8 @@ class AutofillDialogControllerImpl
                            CorrectCountryFromInputs);
   FRIEND_TEST_ALL_PREFIXES(AutofillDialogControllerTest,
                            TransactionAmount);
+  FRIEND_TEST_ALL_PREFIXES(AutofillDialogControllerTest,
+                           TransactionAmountReadonly);
 
   // Initializes or updates |suggested_cc_| et al.
   void SuggestionsUpdated();
