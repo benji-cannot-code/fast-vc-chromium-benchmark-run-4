9609a9defce36de5773d4a0ba0863463d02db388
haraken@chromium.org <haraken@chromium.org>
2014-10-14 03:01:57 +0000

    Readd a UseCounter for HTMLMarqueeElement
    I misremoved the UseCounter in the process of moving marquee implementation
    to JS. This CL readds the UseCounter.
    
    Review URL: https://codereview.chromium.org/653753003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183635 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLMarqueeElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLMarqueeElement.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/V8HTMLMarqueeElement.h"
 #include "core/HTMLNames.h"
 #include "core/dom/Document.h"
+#include "core/frame/UseCounter.h"
 
 namespace blink {
 
@@ -36,6 +37,7 @@ inline HTMLMarqueeElement::HTMLMarqueeElement(Document& document)
 {
     v8::Handle<v8::Value> classObject = PrivateScriptRunner::installClassIfNeeded(document.frame(), "HTMLMarqueeElement");
     RELEASE_ASSERT(!classObject.IsEmpty());
+    UseCounter::count(document, UseCounter::HTMLMarqueeElement);
 }
 
 PassRefPtrWillBeRawPtr<HTMLMarqueeElement> HTMLMarqueeElement::create(Document& document)
