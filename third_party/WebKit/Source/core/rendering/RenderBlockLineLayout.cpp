7b5841404b554abf53286da8f3a6eec8c5bd37c9
wangxianzhu@chromium.org <wangxianzhu@chromium.org>
2014-10-14 16:44:26 +0000

    Remove RenderObject::checkForPaintInvalidation()
    It was used during layout to avoid unnecessary local invalidations.
    However, with RACUN we set flag instead of issuing invalidation during
    layout, making the local invalidation optimization unnecessary.
    
    Also the name is confusing with shouldCheckForPaintInvalidation().
    
    Review URL: https://codereview.chromium.org/647903002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183679 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderBlockLineLayout.cpp
+++ third_party/WebKit/Source/core/rendering/RenderBlockLineLayout.cpp
@@ -1071,9 +1071,8 @@ void RenderBlockFlow::markDirtyFloatsForPaintInvalidation(Vector<FloatWithRect>&
     for (size_t i = 0; i < floatCount; ++i) {
         if (!floats[i].everHadLayout) {
             RenderBox* f = floats[i].object;
-            if (!f->x() && !f->y() && f->checkForPaintInvalidation()) {
+            if (!f->x() && !f->y())
                 f->setShouldDoFullPaintInvalidation();
-            }
         }
     }
 }
