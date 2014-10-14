0b9f9c2e2b8c79de1d0ac87cc727d1928a73e361
zea <zea@chromium.org>
2014-10-14 14:06:20 -0700

    [Sync] fix (again) supervised user sync setup
    The recent refactor of SyncActive state regressed the supervised user sync
    setup logic. The supervised user service is special in that it is actually
    interacting with sync before sync has fully started up, and therefore needs to
    pay attention to backend_initialized, not SyncActive.
    
    BUG=422508
    
    Review URL: https://codereview.chromium.org/647053004
    
    Cr-Commit-Position: refs/heads/master@{#299547}

--- chrome/browser/supervised_user/supervised_user_service.cc
+++ chrome/browser/supervised_user/supervised_user_service.cc
@@ -425,7 +425,8 @@ bool SupervisedUserService::IncludesSyncSessionsType() const {
 void SupervisedUserService::OnStateChanged() {
   ProfileSyncService* service =
       ProfileSyncServiceFactory::GetForProfile(profile_);
-  if (waiting_for_sync_initialization_ && service->SyncActive()) {
+  if (waiting_for_sync_initialization_ && service->backend_initialized() &&
+      service->backend_mode() == ProfileSyncService::SYNC) {
     waiting_for_sync_initialization_ = false;
     service->RemoveObserver(this);
     FinishSetupSync();
@@ -456,7 +457,8 @@ void SupervisedUserService::FinishSetupSyncWhenReady() {
   // Continue in FinishSetupSync() once the Sync backend has been initialized.
   ProfileSyncService* service =
       ProfileSyncServiceFactory::GetForProfile(profile_);
-  if (service->SyncActive()) {
+  if (service->backend_initialized() &&
+      service->backend_mode() == ProfileSyncService::SYNC) {
     FinishSetupSync();
   } else {
     service->AddObserver(this);
@@ -467,7 +469,8 @@ void SupervisedUserService::FinishSetupSyncWhenReady() {
 void SupervisedUserService::FinishSetupSync() {
   ProfileSyncService* service =
       ProfileSyncServiceFactory::GetForProfile(profile_);
-  DCHECK(service->SyncActive());
+  DCHECK(service->backend_initialized());
+  DCHECK(service->backend_mode() == ProfileSyncService::SYNC);
 
   // Sync nothing (except types which are set via GetPreferredDataTypes).
   bool sync_everything = false;
