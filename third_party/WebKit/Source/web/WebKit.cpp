fb574515529ece3c2e0e511c24d1845ea92e5c10
aandrey@chromium.org <aandrey@chromium.org>
2014-10-14 09:47:52 +0000

    Catch uncaught promise rejections from V8 and log to console.
    Print error messages to DevTools console for rejected Promises that are unhandled by the platform end-of-turn.
    
    BUG=393913
    R=pfeldman, yurys
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=183464
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=183598
    
    Review URL: https://codereview.chromium.org/625943002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183659 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebKit.cpp
+++ third_party/WebKit/Source/web/WebKit.cpp
@@ -79,6 +79,7 @@ public:
     {
         Microtask::performCheckpoint();
         V8GCController::reportDOMMemoryUsageToV8(mainThreadIsolate());
+        V8Initializer::reportRejectedPromises();
     }
 };
 
