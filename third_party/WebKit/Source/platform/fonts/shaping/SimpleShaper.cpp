c18ab82c26eda176fe98620d6e208577e7ee02e0
eae@chromium.org <eae@chromium.org>
2014-10-14 18:03:48 +0000

    Handle missing glyphs when adjusting spacing in SimpleShaper
    Handle the case where the space glyph is used instead of a missing zero-
    width-space, soft-hypen or non-printable space glyph when adjusting the
    spacing in SimpleShaper::advanceInternal.
    
    TEST=fast/text/soft-hyphen-simple-text.html
    BUG=422050
    R=pdr@chromium.org
    
    Review URL: https://codereview.chromium.org/648293003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183685 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/fonts/shaping/SimpleShaper.cpp
+++ third_party/WebKit/Source/platform/fonts/shaping/SimpleShaper.cpp
@@ -191,10 +191,12 @@ unsigned SimpleShaper::advanceInternal(TextIterator& textIterator, GlyphBuffer* 
         // Some fonts do not have a glyph for zero-width-space,
         // in that case use the space character and override the width.
         float width;
+        bool spaceUsedAsZeroWidthSpace = false;
         if (!glyphData.glyph && Character::treatAsZeroWidthSpaceInComplexScript(charData.character)) {
             charData.character = space;
             glyphData = glyphDataForCharacter(charData);
             width = 0;
+            spaceUsedAsZeroWidthSpace = true;
         } else {
             width = characterWidth(charData.character, glyphData);
         }
@@ -208,7 +210,7 @@ unsigned SimpleShaper::advanceInternal(TextIterator& textIterator, GlyphBuffer* 
             cacheFallbackFont(fontData, primaryFont);
         }
 
-        if (hasExtraSpacing)
+        if (hasExtraSpacing && !spaceUsedAsZeroWidthSpace)
             width = adjustSpacing(width, charData, *fontData, glyphBuffer);
 
         if (m_bounds)
