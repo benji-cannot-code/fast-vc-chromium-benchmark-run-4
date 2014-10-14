15370a36fddf2d2d8118857fdc327ccc7a992da7
leviw@chromium.org <leviw@chromium.org>
2014-10-14 00:59:07 +0000

    Fix slimming paint clipping and add a debug fill color
    The Slimming Paint codepath wasn't properly adding paint offsets
    to the applied clip rects, causing only a fraction of text to be
    drawn. Also adding a debug red background to make it more obvious
    when we're failing to paint. Cribbed from FrameView's old paint
    code.
    
    BUG=423071
    
    Review URL: https://codereview.chromium.org/654713003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183634 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/paint/ViewDisplayList.cpp
+++ third_party/WebKit/Source/core/paint/ViewDisplayList.cpp
@@ -38,7 +38,7 @@ const PaintCommandList& ViewDisplayList::paintCommandList()
     ASSERT(RuntimeEnabledFeatures::slimmingPaintEnabled());
 
     updatePaintCommandList();
-    return m_paintList;
+    return m_newPaints;
 }
 
 void ViewDisplayList::add(WTF::PassOwnPtr<AtomicPaintChunk> atomicPaintChunk)
--- third_party/WebKit/Source/core/rendering/compositing/CompositedLayerMapping.cpp
+++ third_party/WebKit/Source/core/rendering/compositing/CompositedLayerMapping.cpp
@@ -2135,6 +2135,11 @@ void CompositedLayerMapping::doPaintTask(const GraphicsLayerPaintInfo& paintInfo
         LayerPainter(*paintInfo.renderLayer).paintLayerContents(context, paintingInfo, paintLayerFlags);
 
         if (RuntimeEnabledFeatures::slimmingPaintEnabled()) {
+
+#ifndef NDEBUG
+            context->fillRect(dirtyRect, Color(0xFF, 0, 0));
+#endif
+
             if (RenderView* view = paintInfo.renderLayer->renderer()->view()) {
                 const PaintCommandList& paintCommandList = view->viewDisplayList().paintCommandList();
                 for (PaintCommandList::const_iterator it = paintCommandList.begin(); it != paintCommandList.end(); ++it)
