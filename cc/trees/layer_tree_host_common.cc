1b0ba071e59465115b628e105651b02874fc5cde
aelias <aelias@chromium.org>
2014-10-13 20:51:02 -0700

    Don't set sent_top_controls_delta when root layer doesn't exist.
    Soon after CC initialization when the layer tree doesn't exist yet, the
    top controls delta can still be set.  In that case, an early return in
    LTH::ApplyScrollAndScale doesn't forward the value to Blink -- the
    "sent_" value is misleading here, and this causes the delta to get
    clobbered on the next commit.  Avoid this scenario by matching the
    LTH-side early-return in LTHI.
    
    NOTRY=true
    BUG=423139
    
    Review URL: https://codereview.chromium.org/647753004
    
    Cr-Commit-Position: refs/heads/master@{#299426}

--- cc/trees/layer_tree_host_common.cc
+++ cc/trees/layer_tree_host_common.cc
@@ -22,7 +22,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cc {
 
-ScrollAndScaleSet::ScrollAndScaleSet() {}
+ScrollAndScaleSet::ScrollAndScaleSet()
+    : page_scale_delta(1.f), top_controls_delta(0.f) {
+}
 
 ScrollAndScaleSet::~ScrollAndScaleSet() {}
 
--- cc/trees/layer_tree_host_impl_unittest.cc
+++ cc/trees/layer_tree_host_impl_unittest.cc
@@ -2267,8 +2267,6 @@ class LayerTreeHostImplTopControlsTest : public LayerTreeHostImplTest {
   }
 
   void SetupTopControlsAndScrollLayer() {
-    CreateHostImpl(settings_, CreateOutputSurface());
-
     scoped_ptr<LayerImpl> root =
         LayerImpl::Create(host_impl_->active_tree(), 1);
     scoped_ptr<LayerImpl> root_clip =
@@ -2291,8 +2289,6 @@ class LayerTreeHostImplTopControlsTest : public LayerTreeHostImplTest {
     host_impl_->SetViewportSize(viewport_size_);
     host_impl_->SetTopControlsLayoutHeight(
         settings_.top_controls_height);
-    LayerImpl* root_clip_ptr = host_impl_->active_tree()->root_layer();
-    EXPECT_EQ(clip_size_, root_clip_ptr->bounds());
 
     host_impl_->CreatePendingTree();
     root =
@@ -2317,8 +2313,6 @@ class LayerTreeHostImplTopControlsTest : public LayerTreeHostImplTest {
     host_impl_->SetViewportSize(viewport_size_);
     host_impl_->sync_tree()->set_top_controls_layout_height(
         settings_.top_controls_height);
-    root_clip_ptr = host_impl_->sync_tree()->root_layer();
-    EXPECT_EQ(clip_size_, root_clip_ptr->bounds());
   }
 
   void SetupTopControlsAndScrollLayerWithVirtualViewport(
@@ -2385,7 +2379,30 @@ class LayerTreeHostImplTopControlsTest : public LayerTreeHostImplTest {
   LayerTreeSettings settings_;
 };  // class LayerTreeHostImplTopControlsTest
 
+TEST_F(LayerTreeHostImplTopControlsTest,
+       TopControlsDeltaOnlySentWithRootLayer) {
+  CreateHostImpl(settings_, CreateOutputSurface());
+
+  host_impl_->active_tree()->set_top_controls_delta(-20.f);
+
+  // Because LTH::ApplyScrollAndScale doesn't know what to do with a scroll
+  // delta packet when the root layer doesn't exist yet, make sure not to set
+  // sent_top_controls_delta either to avoid the delta getting clobbered on the
+  // next commit.
+  scoped_ptr<ScrollAndScaleSet> scroll_info = host_impl_->ProcessScrollDeltas();
+  EXPECT_EQ(scroll_info->top_controls_delta, 0.f);
+  EXPECT_EQ(host_impl_->active_tree()->sent_top_controls_delta(), 0.f);
+
+  SetupTopControlsAndScrollLayer();
+
+  // After the root layer exists, it should be set normally.
+  scroll_info = host_impl_->ProcessScrollDeltas();
+  EXPECT_EQ(scroll_info->top_controls_delta, -20.f);
+  EXPECT_EQ(host_impl_->active_tree()->sent_top_controls_delta(), -20.f);
+}
+
 TEST_F(LayerTreeHostImplTopControlsTest, ScrollTopControlsByFractionalAmount) {
+  CreateHostImpl(settings_, CreateOutputSurface());
   SetupTopControlsAndScrollLayer();
   DrawFrame();
 
@@ -2409,6 +2426,7 @@ TEST_F(LayerTreeHostImplTopControlsTest, ScrollTopControlsByFractionalAmount) {
 }
 
 TEST_F(LayerTreeHostImplTopControlsTest, ScrollTopControlsWithPageScale) {
+  CreateHostImpl(settings_, CreateOutputSurface());
   SetupTopControlsAndScrollLayer();
   DrawFrame();
 
@@ -2442,6 +2460,7 @@ TEST_F(LayerTreeHostImplTopControlsTest, ScrollTopControlsWithPageScale) {
 // Ensure setting the top controls position explicitly using the setters on the
 // TreeImpl correctly affects the top controls manager and viewport bounds.
 TEST_F(LayerTreeHostImplTopControlsTest, PositionTopControlsExplicitly) {
+  CreateHostImpl(settings_, CreateOutputSurface());
   SetupTopControlsAndScrollLayer();
   DrawFrame();
 
@@ -2465,6 +2484,7 @@ TEST_F(LayerTreeHostImplTopControlsTest, PositionTopControlsExplicitly) {
 // applied on sync tree activation. The total top controls offset shouldn't
 // change after the activation.
 TEST_F(LayerTreeHostImplTopControlsTest, ApplyDeltaOnTreeActivation) {
+  CreateHostImpl(settings_, CreateOutputSurface());
   SetupTopControlsAndScrollLayer();
   DrawFrame();
 
@@ -2499,6 +2519,7 @@ TEST_F(LayerTreeHostImplTopControlsTest, ApplyDeltaOnTreeActivation) {
 // height is the amount that the inner viewport container was shrunk outside
 // the compositor to accommodate the top controls.
 TEST_F(LayerTreeHostImplTopControlsTest, TopControlsLayoutHeightChanged) {
+  CreateHostImpl(settings_, CreateOutputSurface());
   SetupTopControlsAndScrollLayer();
   DrawFrame();
 
@@ -2690,6 +2711,7 @@ TEST_F(LayerTreeHostImplTopControlsTest, TopControlsScrollOuterViewport) {
 
 TEST_F(LayerTreeHostImplTopControlsTest,
        ScrollNonScrollableRootWithTopControls) {
+  CreateHostImpl(settings_, CreateOutputSurface());
   SetupTopControlsAndScrollLayer();
   DrawFrame();
 
