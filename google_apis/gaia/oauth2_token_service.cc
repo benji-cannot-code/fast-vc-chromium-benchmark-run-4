085eb8940d7a3b10502866e9fe4d0f173d11c061
vadimt <vadimt@chromium.org>
2014-10-14 15:38:55 -0700

    Adding instrumentation to locate the source of jankiness.
    Mechanical change that adds instrumentation required to locate the source of jankiness (i.e. a long-running fragment of code executed as a part of the task that causes jank) in the code. See the bug for details on what kind of jank we are after.
    A number of similar CLs were landed, and none of them caused issues. The code of the instrumentation is highly optimized and is not expected to affect performance. The code simply creates a diagnostic task which is identical to ones created by PostTask or IPC message handlers.
    Landing as TBR since this is a mechanical, safe and temporary change.
    
    BUG=422460
    TBR=rogerta@chromium.org
    
    Review URL: https://codereview.chromium.org/653993004
    
    Cr-Commit-Position: refs/heads/master@{#299570}

--- google_apis/gaia/oauth2_token_service.cc
+++ google_apis/gaia/oauth2_token_service.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/memory/weak_ptr.h"
 #include "base/message_loop/message_loop.h"
+#include "base/profiler/scoped_profile.h"
 #include "base/rand_util.h"
 #include "base/stl_util.h"
 #include "base/time/time.h"
@@ -758,6 +759,11 @@ void OAuth2TokenService::CancelFetchers(
 
 void OAuth2TokenService::FireRefreshTokenAvailable(
     const std::string& account_id) {
+  // TODO(vadimt): Remove ScopedProfile below once crbug.com/422460 is fixed.
+  tracked_objects::ScopedProfile tracking_profile(
+      FROM_HERE_WITH_EXPLICIT_FUNCTION(
+          "422460 OAuth2TokenService::FireRefreshTokenAvailable"));
+
   FOR_EACH_OBSERVER(Observer, observer_list_,
                     OnRefreshTokenAvailable(account_id));
 }
@@ -769,6 +775,11 @@ void OAuth2TokenService::FireRefreshTokenRevoked(
 }
 
 void OAuth2TokenService::FireRefreshTokensLoaded() {
+  // TODO(vadimt): Remove ScopedProfile below once crbug.com/422460 is fixed.
+  tracked_objects::ScopedProfile tracking_profile(
+      FROM_HERE_WITH_EXPLICIT_FUNCTION(
+          "422460 OAuth2TokenService::FireRefreshTokensLoaded"));
+
   FOR_EACH_OBSERVER(Observer, observer_list_, OnRefreshTokensLoaded());
 }
 
