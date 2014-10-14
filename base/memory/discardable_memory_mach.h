9f99b4d6b63ee0813a759793c5a124235171ac6b
reveman <reveman@chromium.org>
2014-10-14 12:44:24 -0700

    base: Rename and move Mach implementation of discardable memory to a separate file.
    This will hopefully reduce confusion about the N-M mapping between
    platforms and discardable memory implementations by not having an
    implementation named after a platform.
    
    This also moves the Mach implementation of discardable memory
    used on mac to a separate file for consistency with other platforms
    and discardable memory implementations.
    
    BUG=422953
    
    Review URL: https://codereview.chromium.org/649233002
    
    Cr-Commit-Position: refs/heads/master@{#299528}

---
+++ base/memory/discardable_memory_mach.h
@@ -0,0 +1,49 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_MEMORY_DISCARDABLE_MEMORY_MACH_H_
+#define BASE_MEMORY_DISCARDABLE_MEMORY_MACH_H_
+
+#include "base/memory/discardable_memory.h"
+
+#include "base/mac/scoped_mach_vm.h"
+#include "base/memory/discardable_memory_manager.h"
+
+namespace base {
+namespace internal {
+
+class DiscardableMemoryMach
+    : public DiscardableMemory,
+      public internal::DiscardableMemoryManagerAllocation {
+ public:
+  explicit DiscardableMemoryMach(size_t bytes);
+  virtual ~DiscardableMemoryMach();
+
+  static void PurgeForTesting();
+
+  bool Initialize();
+
+  // Overridden from DiscardableMemory:
+  virtual DiscardableMemoryLockStatus Lock() override;
+  virtual void Unlock() override;
+  virtual void* Memory() const override;
+
+  // Overridden from internal::DiscardableMemoryManagerAllocation:
+  virtual bool AllocateAndAcquireLock() override;
+  virtual void ReleaseLock() override;
+  virtual void Purge() override;
+
+ private:
+  mac::ScopedMachVM memory_;
+  const size_t bytes_;
+  bool is_locked_;
+
+  DISALLOW_COPY_AND_ASSIGN(DiscardableMemoryMach);
+};
+
+}  // namespace internal
+}  // namespace base
+
+#endif  // BASE_MEMORY_DISCARDABLE_MEMORY_MACH_H_
