84fb64290b99d74d6e29f040f586bcaa3acf29bb
dgozman@chromium.org <dgozman@chromium.org>
2014-10-14 13:08:19 +0000

    [DevTools] Inject element into toolbox window, do not load modules there.
    BUG=none
    
    Review URL: https://codereview.chromium.org/653613002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183669 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/host/InspectorFrontendHost.js
+++ third_party/WebKit/Source/devtools/front_end/host/InspectorFrontendHost.js
@@ -32,6 +32,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /** @interface */
 function InspectorFrontendHostAPI()
 {
+    /**
+     * @type {!WebInspector.EventTarget}
+     */
+    this.events;
 }
 
 /** @typedef {{type:string, id:(number|undefined),
@@ -273,8 +277,9 @@ InspectorFrontendHostAPI.prototype = {
      * @param {number} x
      * @param {number} y
      * @param {!Array.<!InspectorFrontendHostAPI.ContextMenuDescriptor>} items
+     * @param {!Window} window
      */
-    showContextMenuAtPoint: function(x, y, items) { },
+    showContextMenuAtPoint: function(x, y, items, window) { },
 
     /**
      * @return {boolean}
@@ -587,8 +592,9 @@ WebInspector.InspectorFrontendHostStub.prototype = {
      * @param {number} x
      * @param {number} y
      * @param {!Array.<!InspectorFrontendHostAPI.ContextMenuDescriptor>} items
+     * @param {!Window} window
      */
-    showContextMenuAtPoint: function(x, y, items)
+    showContextMenuAtPoint: function(x, y, items, window)
     {
         throw "Soft context menu should be used";
     },
