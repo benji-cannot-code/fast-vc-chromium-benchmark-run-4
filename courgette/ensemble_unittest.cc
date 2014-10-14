d5f25d7315b0ef5d55c2f2af5086c5a7bbb1862b
wfh <wfh@chromium.org>
2014-10-13 23:12:07 -0700

    Re-enable ensemble tests on Windows Release builds.
    This is only taking a long time on Debug, because of the additional iterator
    validators running on all the stl objects being used.
    
    BUG=101614
    
    Review URL: https://codereview.chromium.org/647093002
    
    Cr-Commit-Position: refs/heads/master@{#299437}

--- courgette/ensemble_unittest.cc
+++ courgette/ensemble_unittest.cc
@@ -7,6 +7,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "courgette/courgette.h"
 #include "courgette/streams.h"
 
+#if defined(OS_WIN) && !defined(NDEBUG)
+// Ensemble tests still take too long on Debug Windows so disabling for now
+// TODO(dgarrett) http://code.google.com/p/chromium/issues/detail?id=101614
+#define MAYBE_PE DISABLED_PE
+#define MAYBE_PE64 DISABLED_PE64
+#define MAYBE_Elf32 DISABLED_Elf32
+#else
+#define MAYBE_PE PE
+#define MAYBE_PE64 PE64
+#define MAYBE_Elf32 Elf32
+#endif
+
 class EnsembleTest : public BaseTest {
  public:
 
@@ -104,17 +116,14 @@ void EnsembleTest::Pe64Ensemble() const {
   TestEnsemble(src_bytes, tgt_bytes);
 }
 
-// Ensemble tests still take too long on Windows so disabling for now
-// TODO(dgarrett) http://code.google.com/p/chromium/issues/detail?id=101614
-
-TEST_F(EnsembleTest, DISABLED_PE) {
+TEST_F(EnsembleTest, MAYBE_PE) {
   PeEnsemble();
 }
 
-TEST_F(EnsembleTest, DISABLED_PE64) {
+TEST_F(EnsembleTest, MAYBE_PE64) {
   Pe64Ensemble();
 }
 
-TEST_F(EnsembleTest, DISABLED_Elf32) {
+TEST_F(EnsembleTest, MAYBE_Elf32) {
   Elf32Ensemble();
 }
