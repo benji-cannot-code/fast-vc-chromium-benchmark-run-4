b558659306fcccee9a3ff48dbb48c77983d1add2
sclittle <sclittle@chromium.org>
2014-10-14 16:21:11 -0700

    Added integration test for 4xx response with missing via header
    Added a data reduction proxy telemetry test for a 4xx response that's
    missing the data reduction proxy via header.
    
    BUG=422580
    
    Review URL: https://codereview.chromium.org/652883002
    
    Cr-Commit-Position: refs/heads/master@{#299584}

--- tools/chrome_proxy/integration_tests/chrome_proxy_measurements.py
+++ tools/chrome_proxy/integration_tests/chrome_proxy_measurements.py
@@ -221,7 +221,8 @@ class ChromeProxyHTTPFallbackViaHeader(ChromeProxyValidation):
   """
 
   def __init__(self):
-    super(ChromeProxyHTTPFallbackViaHeader, self).__init__()
+    super(ChromeProxyHTTPFallbackViaHeader, self).__init__(
+        restart_after_each_page=True)
 
   def CustomizeBrowserOptions(self, options):
     super(ChromeProxyHTTPFallbackViaHeader,
