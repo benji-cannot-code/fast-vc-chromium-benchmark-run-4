10a18c30b9eba55956a64e93d3cdaa2d5ef5d1c9
jrummell@chromium.org <jrummell@chromium.org>
2014-10-14 18:46:15 +0000

    Implement MediaKeys.setServerCertificate()
    Add code and tests for setServerCertificate().
    
    BUG=358271
    TEST=new EME tests pass
    
    Review URL: https://codereview.chromium.org/563083002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183692 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/encryptedmedia/MediaKeys.h
+++ third_party/WebKit/Source/modules/encryptedmedia/MediaKeys.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/ScriptPromise.h"
 #include "bindings/core/v8/ScriptWrappable.h"
 #include "core/dom/ContextLifecycleObserver.h"
+#include "platform/Timer.h"
 #include "wtf/Forward.h"
 #include "wtf/text/WTFString.h"
 
@@ -54,6 +55,9 @@ public:
 
     MediaKeySession* createSession(ScriptState*, const String& sessionType);
 
+    ScriptPromise setServerCertificate(ScriptState*, ArrayBuffer* serverCertificate);
+    ScriptPromise setServerCertificate(ScriptState*, ArrayBufferView* serverCertificate);
+
     static bool isTypeSupported(const String& keySystem, const String& contentType);
 
     blink::WebContentDecryptionModule* contentDecryptionModule();
@@ -64,11 +68,19 @@ public:
     virtual void contextDestroyed() override;
 
 private:
+    class PendingAction;
     friend class MediaKeysInitializer;
     MediaKeys(ExecutionContext*, const String& keySystem, PassOwnPtr<blink::WebContentDecryptionModule>);
 
+    ScriptPromise setServerCertificateInternal(ScriptState*, PassRefPtr<ArrayBuffer> initData);
+
+    void timerFired(Timer<MediaKeys>*);
+
     const String m_keySystem;
     OwnPtr<blink::WebContentDecryptionModule> m_cdm;
+
+    HeapDeque<Member<PendingAction> > m_pendingActions;
+    Timer<MediaKeys> m_timer;
 };
 
 } // namespace blink
