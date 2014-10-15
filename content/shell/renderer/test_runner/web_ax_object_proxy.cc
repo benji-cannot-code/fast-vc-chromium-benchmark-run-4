058f65c0c36f42548683b4bba3b2a0f4537c3211
shreeram.k <shreeram.k@samsung.com>
2014-10-15 05:00:58 -0700

    Expose IA and IA2 role and state for HTML input type attributes related to date and time.
    For input type date, datetime and datetime-local, time, month, week - IA and IA2 role and states
    are not exposed properly.
    
    This is chromium side CL for this blink side CL https://codereview.chromium.org/615773002
    
    BUG=418486, 170586, 170575
    
    Review URL: https://codereview.chromium.org/616473002
    
    Cr-Commit-Position: refs/heads/master@{#299677}

--- content/shell/renderer/test_runner/web_ax_object_proxy.cc
+++ content/shell/renderer/test_runner/web_ax_object_proxy.cc
@@ -59,6 +59,10 @@ std::string RoleToString(blink::WebAXRole role)
       return result.append("Complementary");
     case blink::WebAXRoleContentInfo:
       return result.append("ContentInfo");
+    case blink::WebAXRoleDate:
+      return result.append("DateField");
+    case blink::WebAXRoleDateTime:
+      return result.append("DateTimeField");
     case blink::WebAXRoleDefinition:
       return result.append("Definition");
     case blink::WebAXRoleDescriptionListDetail:
@@ -237,6 +241,8 @@ std::string RoleToString(blink::WebAXRole role)
       return result.append("TextArea");
     case blink::WebAXRoleTextField:
       return result.append("TextField");
+    case blink::WebAXRoleTime:
+      return result.append("Time");
     case blink::WebAXRoleTimer:
       return result.append("Timer");
     case blink::WebAXRoleToggleButton:
