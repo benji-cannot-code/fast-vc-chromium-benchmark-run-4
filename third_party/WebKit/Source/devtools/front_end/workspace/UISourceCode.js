e4490f92cf15855d459ed2cb338ed71095a0b925
vsevik@chromium.org <vsevik@chromium.org>
2014-10-15 14:13:04 +0000

    DevTools: Fix unsaved committed changes UI to match saved state when saving with file manager.
    BUG=167285
    
    Review URL: https://codereview.chromium.org/659433002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183751 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/workspace/UISourceCode.js
+++ third_party/WebKit/Source/devtools/front_end/workspace/UISourceCode.js
@@ -364,9 +364,9 @@ WebInspector.UISourceCode.prototype = {
          */
         function callback(accepted)
         {
-            if (!accepted)
-                return;
-            this._savedWithFileManager = true;
+            this._savedWithFileManager = accepted;
+            if (accepted)
+                this._hasCommittedChanges = false;
             this.dispatchEventToListeners(WebInspector.UISourceCode.Events.SavedStateUpdated);
         }
     },
