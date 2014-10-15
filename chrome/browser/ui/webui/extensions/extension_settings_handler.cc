6ec1643184463e2563a089b1d0086aba8f3ef1d5
kalman <kalman@chromium.org>
2014-10-15 13:08:17 -0700

    Set an href for the extension options pages link to get the correct mouse over behaviour.
    BUG=409319
    R=mek@chromium.org
    
    Review URL: https://codereview.chromium.org/657143002
    
    Cr-Commit-Position: refs/heads/master@{#299749}

--- chrome/browser/ui/webui/extensions/extension_settings_handler.cc
+++ chrome/browser/ui/webui/extensions/extension_settings_handler.cc
@@ -288,6 +288,8 @@ base::DictionaryValue* ExtensionSettingsHandler::CreateExtensionDetailValue(
       ManifestURL::SpecifiedHomepageURL(extension));
   extension_data->SetBoolean("optionsOpenInTab",
                              OptionsPageInfo::ShouldOpenInTab(extension));
+  extension_data->SetString("optionsPageHref",
+                            OptionsPageInfo::GetOptionsPage(extension).spec());
 
   // Add dependent extensions.
   base::ListValue* dependents_list = new base::ListValue;
