252177dbcbbbf28779790312885531f1befc831e
xunjieli <xunjieli@chromium.org>
2014-10-15 10:02:08 -0700

    Removed virtual modifier from NetLogObserver constructor
    BUG=
    
    Review URL: https://codereview.chromium.org/653413003
    
    Cr-Commit-Position: refs/heads/master@{#299697}

--- components/cronet/android/url_request_context_adapter.h
+++ components/cronet/android/url_request_context_adapter.h
@@ -35,7 +35,7 @@ typedef base::Callback<void(void)> RunAfterContextInitTask;
 // Implementation of the Chromium NetLog observer interface.
 class NetLogObserver : public net::NetLog::ThreadSafeObserver {
  public:
-  virtual NetLogObserver() {}
+  NetLogObserver() {}
 
   virtual ~NetLogObserver() {}
 
