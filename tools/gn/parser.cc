45ca5260b5dea0c2076e96c81da017bb5291c1a2
dcheng <dcheng@chromium.org>
2014-10-15 13:06:11 -0700

    Use scoped_ptr::Pass instead of scoped_ptr::PassAs<T>.
    BUG=423621
    
    Review URL: https://codereview.chromium.org/656093004
    
    Cr-Commit-Position: refs/heads/master@{#299748}

--- tools/gn/parser.cc
+++ tools/gn/parser.cc
@@ -107,7 +107,7 @@ Parser::~Parser() {
 scoped_ptr<ParseNode> Parser::Parse(const std::vector<Token>& tokens,
                                     Err* err) {
   Parser p(tokens, err);
-  return p.ParseFile().PassAs<ParseNode>();
+  return p.ParseFile();
 }
 
 // static
@@ -226,7 +226,7 @@ scoped_ptr<ParseNode> Parser::ParseExpression(int precedence) {
 }
 
 scoped_ptr<ParseNode> Parser::Literal(Token token) {
-  return scoped_ptr<ParseNode>(new LiteralNode(token)).Pass();
+  return make_scoped_ptr(new LiteralNode(token));
 }
 
 scoped_ptr<ParseNode> Parser::Name(Token token) {
@@ -236,7 +236,7 @@ scoped_ptr<ParseNode> Parser::Name(Token token) {
 scoped_ptr<ParseNode> Parser::BlockComment(Token token) {
   scoped_ptr<BlockCommentNode> comment(new BlockCommentNode());
   comment->set_comment(token);
-  return comment.PassAs<ParseNode>();
+  return comment.Pass();
 }
 
 scoped_ptr<ParseNode> Parser::Group(Token token) {
@@ -254,7 +254,7 @@ scoped_ptr<ParseNode> Parser::Not(Token token) {
   scoped_ptr<UnaryOpNode> unary_op(new UnaryOpNode);
   unary_op->set_op(token);
   unary_op->set_operand(expr.Pass());
-  return unary_op.PassAs<ParseNode>();
+  return unary_op.Pass();
 }
 
 scoped_ptr<ParseNode> Parser::List(Token node) {
@@ -278,7 +278,7 @@ scoped_ptr<ParseNode> Parser::BinaryOperator(scoped_ptr<ParseNode> left,
   binary_op->set_op(token);
   binary_op->set_left(left.Pass());
   binary_op->set_right(right.Pass());
-  return binary_op.PassAs<ParseNode>();
+  return binary_op.Pass();
 }
 
 scoped_ptr<ParseNode> Parser::IdentifierOrCall(scoped_ptr<ParseNode> left,
@@ -317,7 +317,7 @@ scoped_ptr<ParseNode> Parser::IdentifierOrCall(scoped_ptr<ParseNode> left,
   func_call->set_args(list.Pass());
   if (block)
     func_call->set_block(block.Pass());
-  return func_call.PassAs<ParseNode>();
+  return func_call.Pass();
 }
 
 scoped_ptr<ParseNode> Parser::Assignment(scoped_ptr<ParseNode> left,
@@ -331,7 +331,7 @@ scoped_ptr<ParseNode> Parser::Assignment(scoped_ptr<ParseNode> left,
   assign->set_op(token);
   assign->set_left(left.Pass());
   assign->set_right(value.Pass());
-  return assign.PassAs<ParseNode>();
+  return assign.Pass();
 }
 
 scoped_ptr<ParseNode> Parser::Subscript(scoped_ptr<ParseNode> left,
@@ -350,7 +350,7 @@ scoped_ptr<ParseNode> Parser::Subscript(scoped_ptr<ParseNode> left,
   scoped_ptr<AccessorNode> accessor(new AccessorNode);
   accessor->set_base(left->AsIdentifier()->value());
   accessor->set_index(value.Pass());
-  return accessor.PassAs<ParseNode>();
+  return accessor.Pass();
 }
 
 scoped_ptr<ParseNode> Parser::DotOperator(scoped_ptr<ParseNode> left,
@@ -374,7 +374,7 @@ scoped_ptr<ParseNode> Parser::DotOperator(scoped_ptr<ParseNode> left,
   accessor->set_base(left->AsIdentifier()->value());
   accessor->set_member(scoped_ptr<IdentifierNode>(
       static_cast<IdentifierNode*>(right.release())));
-  return accessor.PassAs<ParseNode>();
+  return accessor.Pass();
 }
 
 // Does not Consume the start or end token.
@@ -443,12 +443,12 @@ scoped_ptr<ParseNode> Parser::ParseFile() {
   // ignorant of them.
   AssignComments(file.get());
 
-  return file.PassAs<ParseNode>();
+  return file.Pass();
 }
 
 scoped_ptr<ParseNode> Parser::ParseStatement() {
   if (LookAhead(Token::LEFT_BRACE)) {
-    return ParseBlock().PassAs<ParseNode>();
+    return ParseBlock();
   } else if (LookAhead(Token::IF)) {
     return ParseCondition();
   } else if (LookAhead(Token::BLOCK_COMMENT)) {
@@ -504,7 +504,7 @@ scoped_ptr<ParseNode> Parser::ParseCondition() {
     condition->set_if_false(ParseStatement().Pass());
   if (has_error())
     return scoped_ptr<ParseNode>();
-  return condition.PassAs<ParseNode>();
+  return condition.Pass();
 }
 
 void Parser::TraverseOrder(const ParseNode* root,
