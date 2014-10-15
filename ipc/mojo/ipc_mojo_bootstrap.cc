810d9c0c107592c68fd2e219d86cdf25b1613903
hans <hans@chromium.org>
2014-10-15 11:14:34 -0700

    ipc_mojo_bootstrap.cc: don't export classes in anonymous namespace
    It doesn't make sense since the classes cannot be referred to outside the
    translation unit, and Clang on Windows gets upset about putting dllexport
    on symbols with internal linkage.
    
    BUG=82385
    
    Review URL: https://codereview.chromium.org/655983002
    
    Cr-Commit-Position: refs/heads/master@{#299718}

--- ipc/mojo/ipc_mojo_bootstrap.cc
+++ ipc/mojo/ipc_mojo_bootstrap.cc
@@ -17,7 +17,7 @@ namespace {
 
 // MojoBootstrap for the server process. You should create the instance
 // using MojoBootstrap::Create().
-class IPC_MOJO_EXPORT MojoServerBootstrap : public MojoBootstrap {
+class MojoServerBootstrap : public MojoBootstrap {
  public:
   MojoServerBootstrap();
 
@@ -100,7 +100,7 @@ bool MojoServerBootstrap::OnMessageReceived(const Message&) {
 
 // MojoBootstrap for client processes. You should create the instance
 // using MojoBootstrap::Create().
-class IPC_MOJO_EXPORT MojoClientBootstrap : public MojoBootstrap {
+class MojoClientBootstrap : public MojoBootstrap {
  public:
   MojoClientBootstrap();
 
