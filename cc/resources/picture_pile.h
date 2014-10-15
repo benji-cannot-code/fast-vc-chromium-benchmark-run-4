b81db82d06f41379aa21cb810dc527593ab62508
danakj <danakj@chromium.org>
2014-10-15 13:11:52 -0700

    cc: Correct expansion of invalidation for tiles outside of interest rect
    Previously we subtracted the interest_rect_over_tiles from the
    invalid_rect when looking to see what invalidation would not be recorded
    but this computation is wrong.
    
    Instead, we need to expand the invalidation to the bounds of the tiles
    it touches (including tiles it touches on borders). Then we can subtract
    the interest rect expanded to the bounds of tiles it touches (including
    tiles it touches on borders). This makes each expansion match the
    iteration of the given rect where we include borders.
    
    If a tile is touched only on the border by invalidation, but the tile is
    not touched even on a border by the interest rect, we want to expand the
    invalidation to cover that tile.
    
    R=enne, vmpstr
    BUG=421729
    
    Review URL: https://codereview.chromium.org/657913002
    
    Cr-Commit-Position: refs/heads/master@{#299750}

--- cc/resources/picture_pile.h
+++ cc/resources/picture_pile.h
@@ -52,6 +52,8 @@ class CC_EXPORT PicturePile : public PicturePileBase {
     is_suitable_for_gpu_rasterization_ = false;
   }
 
+  void SetPixelRecordDistanceForTesting(int d) { pixel_record_distance_ = d; }
+
  protected:
   virtual ~PicturePile();
 
@@ -61,6 +63,7 @@ class CC_EXPORT PicturePile : public PicturePileBase {
   void DetermineIfSolidColor();
 
   bool is_suitable_for_gpu_rasterization_;
+  int pixel_record_distance_;
 
   DISALLOW_COPY_AND_ASSIGN(PicturePile);
 };
