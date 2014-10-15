ed242012525f783f36caece5d11f671405a1f020
vadimt <vadimt@chromium.org>
2014-10-14 19:35:15 -0700

    Adding instrumentation to locate the source of jankiness.
    Mechanical change that adds instrumentation required to locate the source of jankiness (i.e. a long-running fragment of code executed as a part of the task that causes jank) in the code. See the bug for details on what kind of jank we are after.
    A number of similar CLs were landed, and none of them caused issues. The code of the instrumentation is highly optimized and is not expected to affect performance. The code simply creates a diagnostic task which is identical to ones created by PostTask or IPC message handlers.
    Landing as TBR since this is a mechanical, safe and temporary change.
    
    Specifically, this CL instruments most popular callbacks invoked from InFlightBackendIO::OnOperationComplete.
    
    BUG=422516
    TBR=ttuttle@chromium.org, sievers@chromium.org
    
    Review URL: https://codereview.chromium.org/654143002
    
    Cr-Commit-Position: refs/heads/master@{#299634}

--- content/browser/gpu/shader_disk_cache.cc
+++ content/browser/gpu/shader_disk_cache.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/gpu/shader_disk_cache.h"
 
+#include "base/profiler/scoped_profile.h"
 #include "base/threading/thread_checker.h"
 #include "content/browser/gpu/gpu_process_host.h"
 #include "content/public/browser/browser_thread.h"
@@ -260,6 +261,11 @@ void ShaderDiskReadHelper::LoadCache() {
 }
 
 void ShaderDiskReadHelper::OnOpComplete(int rv) {
+  // TODO(vadimt): Remove ScopedProfile below once crbug.com/422516 is fixed.
+  tracked_objects::ScopedProfile tracking_profile(
+      FROM_HERE_WITH_EXPLICIT_FUNCTION(
+          "422516 ShaderDiskReadHelper::OnOpComplete"));
+
   DCHECK(CalledOnValidThread());
   if (!cache_.get())
     return;
--- net/http/http_cache_transaction.cc
+++ net/http/http_cache_transaction.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/field_trial.h"
 #include "base/metrics/histogram.h"
 #include "base/metrics/sparse_histogram.h"
+#include "base/profiler/scoped_profile.h"
 #include "base/rand_util.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_piece.h"
@@ -2945,6 +2946,10 @@ void HttpCache::Transaction::RecordHistograms() {
 }
 
 void HttpCache::Transaction::OnIOComplete(int result) {
+  // TODO(vadimt): Remove ScopedProfile below once crbug.com/422516 is fixed.
+  tracked_objects::ScopedProfile tracking_profile(
+      FROM_HERE_WITH_EXPLICIT_FUNCTION("422516 Transaction::OnIOComplete"));
+
   DoLoop(result);
 }
 
