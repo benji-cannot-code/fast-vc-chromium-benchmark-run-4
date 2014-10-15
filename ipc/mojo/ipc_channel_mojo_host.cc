3a4a2c5882df6a86d0e3a306dc81fd2ccfa1d1bd
anujk.sharma <anujk.sharma@samsung.com>
2014-10-15 00:20:06 -0700

    Fix WeakPtrFactory member placement
    Changing in the intialization order of WeakPtrFactory in src/ipc module
    such that all member variables should appear before the WeakPtrFactory
    to ensure that any WeakPtrs to Controller are invalidated before its members
    variable's destructors are executed, rendering them invalid.
    
    BUG=303818
    
    Review URL: https://codereview.chromium.org/645123004
    
    Cr-Commit-Position: refs/heads/master@{#299660}

--- ipc/mojo/ipc_channel_mojo_host.cc
+++ ipc/mojo/ipc_channel_mojo_host.cc
@@ -32,8 +32,8 @@ class ChannelMojoHost::ChannelDelegate : public ChannelMojo::Delegate {
 
  private:
   scoped_refptr<base::TaskRunner> io_task_runner_;
-  base::WeakPtrFactory<ChannelDelegate> weak_factory_;
   base::WeakPtr<ChannelMojo> channel_;
+  base::WeakPtrFactory<ChannelDelegate> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(ChannelDelegate);
 };
@@ -85,9 +85,9 @@ void ChannelMojoHost::ChannelDelegate::DeleteThisSoon() {
 //
 
 ChannelMojoHost::ChannelMojoHost(scoped_refptr<base::TaskRunner> io_task_runner)
-    : weak_factory_(this),
-      io_task_runner_(io_task_runner),
-      channel_delegate_(new ChannelDelegate(io_task_runner)) {
+    : io_task_runner_(io_task_runner),
+      channel_delegate_(new ChannelDelegate(io_task_runner)),
+      weak_factory_(this) {
 }
 
 ChannelMojoHost::~ChannelMojoHost() {
