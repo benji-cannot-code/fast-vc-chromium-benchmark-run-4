4ec362f0e7b7089f76d30151df4942adcf997f6a
dtseng <dtseng@chromium.org>
2014-10-15 10:13:47 -0700

    Provide a base cursor class for ChromeVox Next.
    TEST=chromevox_tests --gtest_filter=CursorsTest.*
    
    Review URL: https://codereview.chromium.org/650563002
    
    Cr-Commit-Position: refs/heads/master@{#299701}

--- chrome/browser/resources/chromeos/chromevox/cvox2/background/loader.js
+++ chrome/browser/resources/chromeos/chromevox/cvox2/background/loader.js
@@ -7,4 +7,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview Loader for the background page.
  */
 
-goog.require('cvox2.Background');
+goog.require('Background');
