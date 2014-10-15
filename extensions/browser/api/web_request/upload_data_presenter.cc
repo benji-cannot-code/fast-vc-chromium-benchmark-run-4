772af9ea3664a645106a69ddcd16a7a506ee526a
dcheng <dcheng@chromium.org>
2014-10-15 10:50:02 -0700

    Use scoped_ptr::Pass instead of scoped_ptr::PassAs<T>.
    BUG=423621
    
    Review URL: https://codereview.chromium.org/658673002
    
    Cr-Commit-Position: refs/heads/master@{#299710}

--- extensions/browser/api/web_request/upload_data_presenter.cc
+++ extensions/browser/api/web_request/upload_data_presenter.cc
@@ -82,9 +82,9 @@ bool RawDataPresenter::Succeeded() {
 
 scoped_ptr<base::Value> RawDataPresenter::Result() {
   if (!success_)
-    return scoped_ptr<base::Value>();
+    return nullptr;
 
-  return list_.PassAs<base::Value>();
+  return list_.Pass();
 }
 
 void RawDataPresenter::FeedNextBytes(const char* bytes, size_t size) {
@@ -137,9 +137,9 @@ bool ParsedDataPresenter::Succeeded() {
 
 scoped_ptr<base::Value> ParsedDataPresenter::Result() {
   if (!success_)
-    return scoped_ptr<base::Value>();
+    return nullptr;
 
-  return dictionary_.PassAs<base::Value>();
+  return dictionary_.Pass();
 }
 
 // static
--- extensions/browser/guest_view/web_view/web_view_apitest.cc
+++ extensions/browser/guest_view/web_view/web_view_apitest.cc
@@ -70,7 +70,7 @@ static scoped_ptr<net::test_server::HttpResponse> UserAgentResponseHandler(
       new net::test_server::BasicHttpResponse);
   http_response->set_code(net::HTTP_MOVED_PERMANENTLY);
   http_response->AddCustomHeader("Location", redirect_target.spec());
-  return http_response.PassAs<net::test_server::HttpResponse>();
+  return http_response.Pass();
 }
 
 class WebContentsHiddenObserver : public content::WebContentsObserver {
@@ -109,7 +109,7 @@ scoped_ptr<net::test_server::HttpResponse> RedirectResponseHandler(
       new net::test_server::BasicHttpResponse);
   http_response->set_code(net::HTTP_MOVED_PERMANENTLY);
   http_response->AddCustomHeader("Location", redirect_target.spec());
-  return http_response.PassAs<net::test_server::HttpResponse>();
+  return http_response.Pass();
 }
 
 // Handles |request| by serving an empty response.
