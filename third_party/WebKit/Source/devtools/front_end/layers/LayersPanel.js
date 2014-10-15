ed4bb4eb12757d25e59449a0b80aa3865c58d6aa
caseq@chromium.org <caseq@chromium.org>
2014-10-15 17:53:54 +0000

    DevTools: move non panel-specific CSS from layerPanel.css to timelinePanel.css
    ... so we can match the direction of code dependency (Layers panel currently
    depends on Timeline panel to show shared layer views).
    
    BUG=
    
    Review URL: https://codereview.chromium.org/658793002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183758 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/layers/LayersPanel.js
+++ third_party/WebKit/Source/devtools/front_end/layers/LayersPanel.js
@@ -38,6 +38,7 @@ WebInspector.LayersPanel = function()
 {
     WebInspector.PanelWithSidebarTree.call(this, "layers", 225);
     this.registerRequiredCSS("layersPanel.css");
+    this.registerRequiredCSS("timelinePanel.css");
     this._target = null;
 
     this.sidebarElement().classList.add("outline-disclosure");
