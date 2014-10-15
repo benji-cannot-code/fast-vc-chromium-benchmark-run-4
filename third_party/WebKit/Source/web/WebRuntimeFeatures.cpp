f5dd831d8c53be8a6e74f7e7882a630c2a40af41
noel@chromium.org <noel@chromium.org>
2014-10-15 19:12:10 +0000

    Add WebRuntimeFeatures::enableImageColorProfiles
    Add a run-time feature enableImageColorProfiles, default off, to be
    plumbed from a Chromium-side commandline flag:
    
     https://codereview.chromium.org/658683003
    
    BUG=423670, 369787
    
    Review URL: https://codereview.chromium.org/646033003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183762 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebRuntimeFeatures.cpp
+++ third_party/WebKit/Source/web/WebRuntimeFeatures.cpp
@@ -65,7 +65,6 @@ void WebRuntimeFeatures::enableApplicationCache(bool enable)
     RuntimeEnabledFeatures::setApplicationCacheEnabled(enable);
 }
 
-
 void WebRuntimeFeatures::enableCompositedSelectionUpdate(bool enable)
 {
     RuntimeEnabledFeatures::setCompositedSelectionUpdateEnabled(enable);
@@ -124,6 +123,11 @@ void WebRuntimeFeatures::enableFastMobileScrolling(bool enable)
 void WebRuntimeFeatures::enableFileSystem(bool enable)
 {
     RuntimeEnabledFeatures::setFileSystemEnabled(enable);
+}
+
+void WebRuntimeFeatures::enableImageColorProfiles(bool enable)
+{
+    RuntimeEnabledFeatures::setImageColorProfilesEnabled(enable);
 }
 
 void WebRuntimeFeatures::enableLocalStorage(bool enable)
