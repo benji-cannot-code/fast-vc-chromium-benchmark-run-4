03289ebed253f285c0f18e32b8bfa6fe0cfa18dd
alemate <alemate@chromium.org>
2014-10-16 06:07:52 -0700

    Revert of Revert of ChromeOS NetworkScreenHandler should not call CheckAndResolveLocale on UI thread. (patchs… (patchset #1 id:1 of https://codereview.chromium.org/653123002/)
    Reason for revert:
    The speculative revert didn't help.
    
    Original issue's description:
    > Revert of ChromeOS NetworkScreenHandler should not call CheckAndResolveLocale on UI thread. (patchset #17 id:320001 of https://codereview.chromium.org/620563002/)
    >
    > Reason for revert:
    > Speculatively reverting for https://crbug.com/422703
    >
    > Original issue's description:
    > > ChromeOS NetworkScreenHandler should not call CheckAndResolveLocale on UI thread.
    > >
    > > BUG=419030
    > >
    > > Committed: https://crrev.com/d52dffddbf0abe5ca55509a9cdedcd9b0674bb1c
    > > Cr-Commit-Position: refs/heads/master@{#298860}
    >
    > TBR=dzhioev@chromium.org,nkostylev@chromium.org
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=422703
    >
    > Committed: https://crrev.com/f09329c436f4929c1f9ffd59e49aa4aef441338b
    > Cr-Commit-Position: refs/heads/master@{#299480}
    
    TBR=dzhioev@chromium.org,nkostylev@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=422703
    
    Review URL: https://codereview.chromium.org/658903004
    
    Cr-Commit-Position: refs/heads/master@{#299883}

--- chrome/browser/ui/webui/chromeos/login/network_screen_handler.h
+++ chrome/browser/ui/webui/chromeos/login/network_screen_handler.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/memory/scoped_ptr.h"
+#include "chrome/browser/chromeos/base/locale_util.h"
 #include "chrome/browser/chromeos/login/screens/network_screen_actor.h"
 #include "chrome/browser/chromeos/settings/cros_settings.h"
 #include "chrome/browser/ui/webui/chromeos/login/base_screen_handler.h"
@@ -21,8 +22,7 @@ namespace chromeos {
 
 class CoreOobeActor;
 class IdleDetector;
-
-struct NetworkScreenHandlerOnLanguageChangedCallbackData;
+class InputEventsBlocker;
 
 // WebUI implementation of NetworkScreenActor. It is used to interact with
 // the welcome screen (part of the page) of the OOBE.
@@ -56,10 +56,10 @@ class NetworkScreenHandler : public NetworkScreenActor,
   virtual void InputMethodChanged(input_method::InputMethodManager* manager,
                                   bool show_message) override;
 
+ private:
   // Reloads localized contents.
   void ReloadLocalizedContent();
 
- private:
   // Handles moving off the screen.
   void HandleOnExit();
 
@@ -67,11 +67,9 @@ class NetworkScreenHandler : public NetworkScreenActor,
   void HandleOnLanguageChanged(const std::string& locale);
 
   // Async callback after ReloadResourceBundle(locale) completed.
-  static void OnLanguageChangedCallback(
-      scoped_ptr<NetworkScreenHandlerOnLanguageChangedCallbackData> context,
-      const std::string& requested_locale,
-      const std::string& loaded_locale,
-      const bool success);
+  void OnLanguageChangedCallback(
+      const InputEventsBlocker* input_events_blocker,
+      const locale_util::LanguageSwitchResult& result);
 
   // Handles change of the input method.
   void HandleOnInputMethodChanged(const std::string& id);
@@ -84,6 +82,15 @@ class NetworkScreenHandler : public NetworkScreenActor,
 
   // Returns available timezones. Caller gets the ownership.
   static base::ListValue* GetTimezoneList();
+
+  // Starts resolving language list on BlockingPool.
+  void ScheduleResolveLanguageList(
+      scoped_ptr<locale_util::LanguageSwitchResult> language_switch_result);
+
+  // Callback for chromeos::ResolveUILanguageList() (from l10n_util).
+  void OnLanguageListResolved(scoped_ptr<base::ListValue> new_language_list,
+                              std::string new_language_list_locale,
+                              std::string new_selected_language);
 
   NetworkScreenActor::Delegate* screen_;
   CoreOobeActor* core_oobe_actor_;
@@ -100,6 +107,11 @@ class NetworkScreenHandler : public NetworkScreenActor,
 
   // The exact language code selected by user in the menu.
   std::string selected_language_code_;
+
+  // Creation of language list happens on Blocking Pool, so we cache resolved
+  // data.
+  std::string language_list_locale_;
+  scoped_ptr<base::ListValue> language_list_;
 
   base::WeakPtrFactory<NetworkScreenHandler> weak_ptr_factory_;
 
