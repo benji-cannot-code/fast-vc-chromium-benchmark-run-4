593de3a52e1ea1135f33e74c295ef96f43de9dc7
sigbjornf@opera.com <sigbjornf@opera.com>
2014-10-16 13:24:49 +0000

    Oilpan: fix build after r183810.
    R=haraken
    BUG=393913
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/662633002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183814 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/core/v8/V8Initializer.cpp
+++ third_party/WebKit/Source/bindings/core/v8/V8Initializer.cpp
@@ -162,6 +162,7 @@ static void messageHandlerInMainThread(v8::Handle<v8::Message> message, v8::Hand
 namespace {
 
 class PromiseRejectMessage {
+    ALLOW_ONLY_INLINE_ALLOCATION();
 public:
     PromiseRejectMessage(const ScriptValue& promise, PassRefPtrWillBeRawPtr<ScriptCallStack> callStack)
         : m_promise(promise)
@@ -171,6 +172,11 @@ public:
 
     const ScriptValue m_promise;
     const RefPtrWillBeMember<ScriptCallStack> m_callStack;
+
+    void trace(Visitor* visitor)
+    {
+        visitor->trace(m_callStack);
+    }
 };
 
 } // namespace
