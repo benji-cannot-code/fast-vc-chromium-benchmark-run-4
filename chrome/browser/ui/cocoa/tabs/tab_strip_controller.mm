e1ab79bdf2ec62e6a57ef3e717209fab575c7b95
reillyg <reillyg@chromium.org>
2014-10-15 17:22:49 -0700

    Nasty hack to pick the right origin for a sheet above an app window.
    In r546e2e6931 GetSheetParentBoundsForParentView was added to work around
    the presense of dev tools in the view hierarchy when calculating the center
    of the web contents view. In an app window there is no dev tools view so
    this patch falls back to the parent view's normal bounds.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/659823003
    
    Cr-Commit-Position: refs/heads/master@{#299790}

--- chrome/browser/ui/cocoa/tabs/tab_strip_controller.mm
+++ chrome/browser/ui/cocoa/tabs/tab_strip_controller.mm
@@ -2315,5 +2315,9 @@ NSRect GetSheetParentBoundsForParentView(NSView* view) {
   // the devtools view is always in the hierarchy even if it is not open or it
   // is detached.
   NSView* devtools_view = [[[view superview] superview] superview];
-  return [devtools_view convertRect:[devtools_view bounds] toView:nil];
+  if (devtools_view) {
+    return [devtools_view convertRect:[devtools_view bounds] toView:nil];
+  } else {
+    return [view convertRect:[view bounds] toView:nil];
+  }
 }
