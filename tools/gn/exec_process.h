aac6b3d9249341b6d559979cbe84b8392ac3af27
cjhopman <cjhopman@chromium.org>
2014-10-16 12:19:40 -0700

    GN: Capture and print stderr from exec_script
    This moves ExecProcess to exec_process.h/cc and adds capturing
    of stderr on linux.
    
    Adds unittests for ExecProcess.
    
    Windows is not supported.
    
    Review URL: https://codereview.chromium.org/646013003
    
    Cr-Commit-Position: refs/heads/master@{#299951}

---
+++ tools/gn/exec_process.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <string>
+
+namespace base {
+class CommandLine;
+class FilePath;
+}
+
+namespace internal {
+bool ExecProcess(const base::CommandLine& cmdline,
+                 const base::FilePath& startup_dir,
+                 std::string* std_out,
+                 std::string* std_err,
+                 int* exit_code);
+}  // namespace internal
