81a0d290d90a3ced84b80ac98f36845232f54b2b
Brett Wilson <brettw@chromium.org>
2014-10-16 16:12:08 -0700

    Add TCP socket mojo implementation.
    This is a very preliminary pass and not tested very well. Open issues:
    
    It seems there is no way to wait on a mojo data pipe without using chromium message loops, which I was reluctant to do in my example. We'll need this before doing any nontrivial sending or receiving, including testing of the infrastructure.
    
    We need to be able to pass the net work type (IPv4 or v6) to the connect function that bypasses bind. I removed the code here and documented that it doesn't work yet.
    
    R=yzshen@chromium.org
    
    Review URL: https://codereview.chromium.org/657113002
    
    Cr-Commit-Position: refs/heads/master@{#300003}

--- mojo/services/network/network_service_impl.cc
+++ mojo/services/network/network_service_impl.cc
@@ -15,19 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace mojo {
 
-namespace {
-
-// Allows a Callback<NetworkErrorPtr, NetAddressPtr> to be called by a
-// Callback<NetworkErrorPtr> when the address is already known.
-void BoundAddressCallbackAdapter(
-    const Callback<void(NetworkErrorPtr, NetAddressPtr)>& callback,
-    NetAddressPtr address,
-    NetworkErrorPtr err) {
-  callback.Run(err.Pass(), address.Pass());
-}
-
-}  // namespace
-
 NetworkServiceImpl::NetworkServiceImpl(ApplicationConnection* connection,
                                        NetworkContext* context)
     : context_(context),
@@ -71,19 +58,10 @@ void NetworkServiceImpl::CreateTCPConnectedSocket(
     ScopedDataPipeProducerHandle receive_stream,
     InterfaceRequest<TCPConnectedSocket> client_socket,
     const Callback<void(NetworkErrorPtr, NetAddressPtr)>& callback) {
-  TCPBoundSocketImpl bound_socket;
-  int net_error = bound_socket.Bind(NetAddressPtr());
-  if (net_error != net::OK) {
-    callback.Run(MakeNetworkError(net_error), NetAddressPtr());
-    return;
-  }
-
-  bound_socket.Connect(
-      remote_address.Pass(), send_stream.Pass(), receive_stream.Pass(),
-      client_socket.Pass(),
-      base::Bind(&BoundAddressCallbackAdapter,
-                 callback,
-                 base::Passed(bound_socket.GetLocalAddress().Pass())));
+  // TODO(brettw) implement this. We need to know what type of socket to use
+  // so we can create the right one (i.e. to pass to TCPSocket::Open) before
+  // doing the connect.
+  callback.Run(MakeNetworkError(net::ERR_NOT_IMPLEMENTED), NetAddressPtr());
 }
 
 void NetworkServiceImpl::CreateUDPSocket(InterfaceRequest<UDPSocket> socket) {
