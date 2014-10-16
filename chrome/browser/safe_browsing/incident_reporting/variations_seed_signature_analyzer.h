0184c32f0db77a88d00c113ed48edc215288222c
georgesak <georgesak@chromium.org>
2014-10-16 13:15:18 -0700

    Added incident report for variations seed signature mismatch.
    - Keep record of a bad signature in VariationsSeedStore::LoadSeed.
    - Register VerifyVariationsSeedSignature in SafeBrowsingService::RegisterAllDelayedAnalysis to send an incident report, if a bad signature is encountered.
    
    BUG=423467
    
    Review URL: https://codereview.chromium.org/646733002
    
    Cr-Commit-Position: refs/heads/master@{#299964}

---
+++ chrome/browser/safe_browsing/incident_reporting/variations_seed_signature_analyzer.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_SAFE_BROWSING_INCIDENT_REPORTING_VARIATIONS_SEED_SIGNATURE_ANALYZER_H_
+#define CHROME_BROWSER_SAFE_BROWSING_INCIDENT_REPORTING_VARIATIONS_SEED_SIGNATURE_ANALYZER_H_
+
+#include "base/strings/string16.h"
+#include "chrome/browser/safe_browsing/incident_reporting/add_incident_callback.h"
+
+namespace safe_browsing {
+
+// Registers a process-wide analysis with the incident reporting service that
+// will verify the variations seed signature.
+void RegisterVariationsSeedSignatureAnalysis();
+
+// Callback to pass to the incident reporting service. The incident reporting
+// service will verify if the variations seed signature is invalid.
+void VerifyVariationsSeedSignature(const AddIncidentCallback& callback);
+
+}  // namespace safe_browsing
+
+#endif  // CHROME_BROWSER_SAFE_BROWSING_INCIDENT_REPORTING_VARIATIONS_SEED_SIGNATURE_ANALYZER_H_
