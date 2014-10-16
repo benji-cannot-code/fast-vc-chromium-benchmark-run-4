0b45549eb5c4cad5ba0fdb69922dfde4686b31c9
adamk@chromium.org <adamk@chromium.org>
2014-10-16 16:27:29 +0000

    Fix crash in MutationObserver::canDeliver due to NULL ExecutionContext
    Rather than NULL-checking it directly, I've instead plumbed through
    ActiveDOMCallback::canInvokeCallback(), which handles all the logic
    MutationObserver was doing previously and also handles the NULL check.
    
    The test case is crazy-looking because it was generated by ClusterFuzz.
    I've simplified it as much as possible.
    
    BUG=410755
    
    Review URL: https://codereview.chromium.org/657983002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183827 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/MutationObserver.cpp
+++ third_party/WebKit/Source/core/dom/MutationObserver.cpp
@@ -210,7 +210,7 @@ WillBeHeapHashSet<RawPtrWillBeMember<Node> > MutationObserver::getObservedNodes(
 
 bool MutationObserver::canDeliver()
 {
-    return !m_callback->executionContext()->activeDOMObjectsAreSuspended();
+    return m_callback->canInvokeCallback();
 }
 
 void MutationObserver::deliver()
