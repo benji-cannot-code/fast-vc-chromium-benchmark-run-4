eef4ca9b50dea6d050e239cfff0f410870671576
ernstm <ernstm@chromium.org>
2014-10-16 13:28:08 -0700

    telemetry: remove run_measurment script.
    R=dtu@chromium.org,nednguyen@chromium.org,tonyg@chromium.org
    BUG=423563
    
    Review URL: https://codereview.chromium.org/655563007
    
    Cr-Commit-Position: refs/heads/master@{#299968}

--- tools/telemetry/build/generate_telemetry_harness.sh
+++ tools/telemetry/build/generate_telemetry_harness.sh
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 src/tools/telemetry/find_dependencies \
   src/tools/perf/run_benchmark \
-  src/tools/perf/run_measurement \
   src/tools/perf/record_wpr \
   src/content/test/gpu/run_gpu_test.py \
   --exclude=*/third_party/trace-viewer/test_data/* \
--- tools/telemetry/telemetry/core/backends/remote/trybot_browser_finder_unittest.py
+++ tools/telemetry/telemetry/core/backends/remote/trybot_browser_finder_unittest.py
@@ -205,7 +205,7 @@ class TrybotBrowserFinderTest(unittest.TestCase):
         'trybot-android-nexus4', 'somebranch', 'tools/run-perf-test.cfg')
     self.assertEquals(
         ('config = {\n'
-         '  "command": "./tools/perf/run_measurement '
+         '  "command": "./tools/perf/run_benchmark '
          '--browser=android-chrome-shell sunspider",\n'
          '  "max_time_minutes": "120",\n'
          '  "repeat_count": "1",\n'
@@ -217,7 +217,7 @@ class TrybotBrowserFinderTest(unittest.TestCase):
         'trybot-mac-10-9', 'currentwork', 'tools/run-perf-test.cfg')
     self.assertEquals(
         ('config = {\n'
-         '  "command": "./tools/perf/run_measurement '
+         '  "command": "./tools/perf/run_benchmark '
          '--browser=release sunspider",\n'
          '  "max_time_minutes": "120",\n'
          '  "repeat_count": "1",\n'
@@ -229,10 +229,9 @@ class TrybotBrowserFinderTest(unittest.TestCase):
         'trybot-mac-10-9', 'blinkbranch', 'Tools/run-perf-test.cfg', True)
     self.assertEquals(
         ('config = {\n'
-         '  "command": "./tools/perf/run_measurement '
+         '  "command": "./tools/perf/run_benchmark '
          '--browser=release sunspider",\n'
          '  "max_time_minutes": "120",\n'
          '  "repeat_count": "1",\n'
          '  "truncate_percent": "0"\n'
          '}'), config)
-
