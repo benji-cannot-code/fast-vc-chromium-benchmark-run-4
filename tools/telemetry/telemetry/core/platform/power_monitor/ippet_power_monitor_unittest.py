dc2f9a28c5fff9ce6d1318793eba996c1985ac6b
John Abd-El-Malek <jam@chromium.org>
2014-10-15 20:37:19 -0700

    Disable IppetPowerMonitorTest.testFindOrInstallIppet which is failing on new trybots.
    BUG=424027
    TBR=dtu@chromium.org
    
    Review URL: https://codereview.chromium.org/643763005
    
    Cr-Commit-Position: refs/heads/master@{#299833}

--- tools/telemetry/telemetry/core/platform/power_monitor/ippet_power_monitor_unittest.py
+++ tools/telemetry/telemetry/core/platform/power_monitor/ippet_power_monitor_unittest.py
@@ -12,7 +12,7 @@ from telemetry.core.platform.power_monitor import ippet_power_monitor
 
 
 class IppetPowerMonitorTest(unittest.TestCase):
-  @decorators.Enabled('win')
+  @decorators.Disabled
   def testFindOrInstallIppet(self):
     self.assertTrue(ippet_power_monitor.IppetPath())
 
