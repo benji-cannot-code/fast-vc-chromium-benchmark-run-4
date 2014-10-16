1220c60ad63816ce15bc471528ae3cef5d2d5223
dstockwell@chromium.org <dstockwell@chromium.org>
2014-10-16 04:59:52 +0000

    Web Animations: Overhaul and trace play state in AnimationPlayer
    Adds an update scope and instruments call sites which require an up-to-date
    current time or modify the play state. Tracing now displays spans for the pending
    and running phases.
    
    BUG=351446
    
    Review URL: https://codereview.chromium.org/638313002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183780 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/AnimationPlayer.h
+++ third_party/WebKit/Source/core/animation/AnimationPlayer.h
@@ -75,20 +75,20 @@ public:
     double currentTime();
     void setCurrentTime(double newCurrentTime);
 
-    double calculateCurrentTime() const;
-    double currentTimeInternal();
-    void setCurrentTimeInternal(double newCurrentTime, TimingUpdateReason = TimingUpdateOnDemand);
+    double currentTimeInternal() const;
 
+    void setCurrentTimeInternal(double newCurrentTime, TimingUpdateReason = TimingUpdateOnDemand);
     bool paused() const { return m_paused && !m_isPausedForTesting; }
-    String playState();
-    AnimationPlayState playStateInternal();
+    static const char* playStateString(AnimationPlayState);
+    String playState() { return playStateString(playStateInternal()); }
+    AnimationPlayState playStateInternal() const;
 
     void pause();
     void play();
     void reverse();
     void finish(ExceptionState&);
-    bool finished() { return !m_idle && limited(currentTimeInternal()); }
-    bool playing() { return !(finished() || m_paused || m_isPausedForTesting || m_idle); }
+    bool finished() const { return m_playState != Idle && limited(currentTimeInternal()); }
+    bool playing() const { return !(playStateInternal() == Idle || finished() || m_paused || m_isPausedForTesting); }
     // FIXME: Resolve whether finished() should just return the flag, and
     // remove this method.
     bool finishedInternal() const { return m_finished; }
@@ -137,6 +137,7 @@ public:
     bool hasActiveAnimationsOnCompositor();
     void setCompositorPending(bool sourceChanged = false);
     void notifyCompositorStartTime(double timelineTime);
+    void notifyStartTime(double timelineTime);
 
 
     void preCommit(bool startOnCompositor);
@@ -161,16 +162,21 @@ public:
 
 private:
     AnimationPlayer(ExecutionContext*, AnimationTimeline&, AnimationNode*);
+
     double sourceEnd() const;
     bool limited(double currentTime) const;
-    void setPlaybackRateInternal(double);
-    void updateCurrentTimingState(TimingUpdateReason);
+
+    AnimationPlayState calculatePlayState();
+    double calculateCurrentTime() const;
+
     void unpauseInternal();
     void uncancel();
-    void setFinished(bool);
+    void setPlaybackRateInternal(double);
+    void updateCurrentTimingState(TimingUpdateReason);
 
+
+    AnimationPlayState m_playState;
     double m_playbackRate;
-
     double m_startTime;
     double m_holdTime;
 
@@ -216,13 +222,29 @@ private:
         CompositorAction pendingAction;
     };
 
+    enum CompositorPendingChange {
+        SetCompositorPending,
+        SetCompositorPendingWithSourceChanged,
+        DoNotSetCompositorPending,
+    };
+
+    class PlayStateUpdateScope {
+        STACK_ALLOCATED();
+    public:
+        PlayStateUpdateScope(AnimationPlayer&, TimingUpdateReason, CompositorPendingChange = SetCompositorPending);
+        ~PlayStateUpdateScope();
+    private:
+        AnimationPlayer& m_player;
+        AnimationPlayState m_initial;
+        CompositorPendingChange m_compositorPendingChange;
+    };
+
     // This mirrors the known compositor state. It is created when a compositor
     // animation is started. Updated once the start time is known and each time
     // modifications are pushed to the compositor.
     OwnPtr<CompositorState> m_compositorState;
     bool m_compositorPending;
     bool m_currentTimePending;
-    bool m_idle;
 };
 
 } // namespace blink
