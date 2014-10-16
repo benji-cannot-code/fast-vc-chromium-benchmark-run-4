62e94abab9adce1921496d83b41fc79c0ad49e34
noms <noms@chromium.org>
2014-10-16 14:51:59 -0700

    [Mac] The profile name field shouldn't be multi line because that's silly.
    BUG=424147
    TEST=Start Chrome with --enable-new-avatar-menu. In the avatar menu, edit
    the profile name and try to press Alt+Enter to get a new line. This
    shouldn't work.
    
    Review URL: https://codereview.chromium.org/638133004
    
    Cr-Commit-Position: refs/heads/master@{#299981}

--- chrome/browser/ui/cocoa/profiles/profile_chooser_controller.mm
+++ chrome/browser/ui/cocoa/profiles/profile_chooser_controller.mm
@@ -643,6 +643,7 @@ class ActiveProfileObserverBridge : public AvatarMenuObserver,
       [[profileNameTextField_ cell] setWraps:NO];
       [[profileNameTextField_ cell] setLineBreakMode:
           NSLineBreakByTruncatingTail];
+      [[profileNameTextField_ cell] setUsesSingleLineMode:YES];
       [self addSubview:profileNameTextField_];
       [profileNameTextField_ setTarget:self];
       [profileNameTextField_ setAction:@selector(saveProfileName:)];
