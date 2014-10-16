bf1c282026e5fc0365d2df76d68d3ef524322cbd
dzhioev <dzhioev@chromium.org>
2014-10-16 16:59:12 -0700

    Implemented UI for the meetings controller out-of-box.
    Implemented all the stages of the controller out-of-box.
    Known issues:
    * Most of the stages are not shown now during the real out-of-box, because C++
      part is not ready yet.
    * Some transitions look bad/don't work.
    * Spinner is wrong.
    
    BUG=375191
    TEST=manually
    
    Review URL: https://codereview.chromium.org/642313002
    
    Cr-Commit-Position: refs/heads/master@{#300017}

--- chrome/browser/resources/chromeos/login/oobe.js
+++ chrome/browser/resources/chromeos/login/oobe.js
@@ -15,8 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* @const */ var WAIT_FOR_POLYMER = true;
 
 <include src="login_common.js">
-<include src="oobe-screen.js">
-<include src="html-echo.js">
 <include src="oobe_screen_eula.js">
 <include src="oobe_screen_network.js">
 <include src="oobe_screen_hid_detection.js">
