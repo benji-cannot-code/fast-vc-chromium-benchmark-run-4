eceaeb5b161f0525959d7995c423c0bef7ee419f
rnephew <rnephew@chromium.org>
2014-10-17 12:51:32 -0700

    Add android acceptance test to telemetry.
    This telemetry test closely mimics the android acceptance test for power.
    It uses the same websites; sits on them for 20 seconds, scrolls down, sits 20 more seconds.
    
    BUG=421569
    
    Review URL: https://codereview.chromium.org/654263005
    
    Cr-Commit-Position: refs/heads/master@{#300147}

---
+++ tools/perf/benchmarks/power.py
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from measurements import power
+import page_sets
+from telemetry import benchmark
+
+
+@benchmark.Enabled('android')
+class PowerAndroidAcceptance(benchmark.Benchmark):
+
+  """Android power acceptance test."""
+
+  test = power.Power
+  page_set = page_sets.AndroidAcceptancePageSet
