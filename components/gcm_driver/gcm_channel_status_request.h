f076a3198ea7799651f2293a524b936f15378e1a
jianli <jianli@chromium.org>
2014-10-17 14:03:57 -0700

    [GCM] DCHECK should not be used since it will skip the code inside
    BUG=424697
    TEST=Test added
    
    Review URL: https://codereview.chromium.org/659393003
    
    Cr-Commit-Position: refs/heads/master@{#300161}

--- components/gcm_driver/gcm_channel_status_request.h
+++ components/gcm_driver/gcm_channel_status_request.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/compiler_specific.h"
+#include "base/gtest_prod_util.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
@@ -50,6 +51,8 @@ class GCMChannelStatusRequest : public net::URLFetcherDelegate {
   static int min_poll_interval_seconds();
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(GCMChannelStatusRequestTest, RequestData);
+
   // Overridden from URLFetcherDelegate:
   virtual void OnURLFetchComplete(const net::URLFetcher* source) override;
 
