59ec5bf9a21edf44ef8c37af969d7981c55e5304
jkarlin <jkarlin@chromium.org>
2014-10-16 19:38:01 -0700

    [ServiceWorkerCacheStorage] Add support for new Open method.
    Create/Get have been replaced by Open which will first attempt to Get() followed by Create() if Get() failed.
    
    CLs in this three-legged-patch:
    
    *1. Add open to browser - https://codereview.chromium.org/664433003/
    2. Add open (remove get/create) to Blink - https://codereview.chromium.org/638023003
    3. Remove get/create from browser - https://codereview.chromium.org/663503002/
    
    Note that the three CLs have been tested together and pass tests locally.
    
    BUG=423942
    
    Review URL: https://codereview.chromium.org/664433003
    
    Cr-Commit-Position: refs/heads/master@{#300047}

--- content/renderer/service_worker/service_worker_cache_storage_dispatcher.h
+++ content/renderer/service_worker/service_worker_cache_storage_dispatcher.h
@@ -42,6 +42,7 @@ class ServiceWorkerCacheStorageDispatcher
   void OnCacheStorageGetSuccess(int request_id, int cache_id);
   void OnCacheStorageHasSuccess(int request_id);
   void OnCacheStorageCreateSuccess(int request_id, int cache_id);
+  void OnCacheStorageOpenSuccess(int request_id, int cache_id);
   void OnCacheStorageDeleteSuccess(int request_id);
   void OnCacheStorageKeysSuccess(int request_id,
                                  const std::vector<base::string16>& keys);
@@ -52,6 +53,8 @@ class ServiceWorkerCacheStorageDispatcher
                               blink::WebServiceWorkerCacheError reason);
   void OnCacheStorageCreateError(int request_id,
                                  blink::WebServiceWorkerCacheError reason);
+  void OnCacheStorageOpenError(int request_id,
+                               blink::WebServiceWorkerCacheError reason);
   void OnCacheStorageDeleteError(int request_id,
                                  blink::WebServiceWorkerCacheError reason);
   void OnCacheStorageKeysError(int request_id,
@@ -85,6 +88,8 @@ class ServiceWorkerCacheStorageDispatcher
                            const blink::WebString& cacheName);
   virtual void dispatchCreate(CacheStorageWithCacheCallbacks* callbacks,
                               const blink::WebString& cacheName);
+  virtual void dispatchOpen(CacheStorageWithCacheCallbacks* callbacks,
+                            const blink::WebString& cacheName);
   virtual void dispatchDelete(CacheStorageCallbacks* callbacks,
                               const blink::WebString& cacheName);
   virtual void dispatchKeys(CacheStorageKeysCallbacks* callbacks);
@@ -143,6 +148,7 @@ class ServiceWorkerCacheStorageDispatcher
   WithCacheCallbacksMap get_callbacks_;
   CallbacksMap has_callbacks_;
   WithCacheCallbacksMap create_callbacks_;
+  WithCacheCallbacksMap open_callbacks_;
   CallbacksMap delete_callbacks_;
   KeysCallbacksMap keys_callbacks_;
 
