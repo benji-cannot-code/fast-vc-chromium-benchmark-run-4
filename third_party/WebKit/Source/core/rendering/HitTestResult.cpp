d8ce06c5039ed1c1f93c8077dabc4b26195fe8ad
ajith.v@samsung.com <ajith.v@samsung.com>
2014-10-17 10:12:37 +0000

    Paste Popup showing is blocked for ReadOnly/Disabled input and textarea elements.
    Editable element check is not taken care of disabled and readonly elements
    after getting the hittest result. Hence causing this issue. In this patch we
    are checking whether the element is readonly or disabled and not allowing
    paste popup to show.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/246203004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183874 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/HitTestResult.cpp
+++ third_party/WebKit/Source/core/rendering/HitTestResult.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/html/HTMLImageElement.h"
 #include "core/html/HTMLInputElement.h"
 #include "core/html/HTMLMediaElement.h"
+#include "core/html/HTMLTextAreaElement.h"
 #include "core/html/parser/HTMLParserIdioms.h"
 #include "core/page/FrameTree.h"
 #include "core/rendering/RenderImage.h"
@@ -372,10 +373,12 @@ bool HitTestResult::isContentEditable() const
         return false;
 
     if (isHTMLTextAreaElement(*m_innerNonSharedNode))
-        return true;
+        return !toHTMLTextAreaElement(*m_innerNonSharedNode).isDisabledOrReadOnly();
 
-    if (isHTMLInputElement(*m_innerNonSharedNode))
-        return toHTMLInputElement(*m_innerNonSharedNode).isTextField();
+    if (isHTMLInputElement(*m_innerNonSharedNode)) {
+        HTMLInputElement& inputElement = toHTMLInputElement(*m_innerNonSharedNode);
+        return !inputElement.isDisabledOrReadOnly() && inputElement.isTextField();
+    }
 
     return m_innerNonSharedNode->hasEditableStyle();
 }
