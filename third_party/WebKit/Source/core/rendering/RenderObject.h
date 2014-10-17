87743de863e4af1d401f996e87620ca3a05b57e9
wangxianzhu@chromium.org <wangxianzhu@chromium.org>
2014-10-17 18:15:41 +0000

    Ensure positioned descendants are invalidated if needed
    When a render object changes location, we should force paint
    invalidation checking for all descendants on the same backing,
    including positioned ones. However, previously we only force
    invalidation checking for non-positioned descendants.
    
    Now let RenderBlock override
    RenderObject::invalidatePaintOfSubtreesIfNeeded() so that it
    can use the childPaintInvalidationState, which has been forced
    checking of invaldation, for the positioned descendants.
    
    BUG=422812
    TEST=fast/repaint/positioned-great-grandparent-change-location.html
    
    Review URL: https://codereview.chromium.org/662483002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183910 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderObject.h
+++ third_party/WebKit/Source/core/rendering/RenderObject.h
@@ -1176,7 +1176,7 @@ protected:
     }
 #endif
 
-    void invalidatePaintOfSubtreesIfNeeded(const PaintInvalidationState&);
+    virtual void invalidatePaintOfSubtreesIfNeeded(const PaintInvalidationState& childPaintInvalidationState);
     virtual PaintInvalidationReason invalidatePaintIfNeeded(const PaintInvalidationState&, const RenderLayerModelObject& paintInvalidationContainer);
 
 private:
