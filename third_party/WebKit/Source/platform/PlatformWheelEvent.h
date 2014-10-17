0ee896bf77737d565343931682dde247e9601a93
ccameron@chromium.org <ccameron@chromium.org>
2014-10-17 02:21:26 +0000

    Remove redundant Mac event structures
    The scrollCount field is unused.
    
    The unacceleratedScrollingDeltaX/Y field always equal deltaX/Y (by
    inspection, then by adding an assert and testing), so use those
    directly.
    
    BUG=133097
    
    Review URL: https://codereview.chromium.org/658953003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183851 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/PlatformWheelEvent.h
+++ third_party/WebKit/Source/platform/PlatformWheelEvent.h
@@ -67,9 +67,6 @@ public:
 #if OS(MACOSX)
         , m_phase(PlatformWheelEventPhaseNone)
         , m_momentumPhase(PlatformWheelEventPhaseNone)
-        , m_scrollCount(0)
-        , m_unacceleratedScrollingDeltaX(0)
-        , m_unacceleratedScrollingDeltaY(0)
         , m_canRubberbandLeft(true)
         , m_canRubberbandRight(true)
 #endif
@@ -89,9 +86,6 @@ public:
 #if OS(MACOSX)
         , m_phase(PlatformWheelEventPhaseNone)
         , m_momentumPhase(PlatformWheelEventPhaseNone)
-        , m_scrollCount(0)
-        , m_unacceleratedScrollingDeltaX(0)
-        , m_unacceleratedScrollingDeltaY(0)
         , m_canRubberbandLeft(true)
         , m_canRubberbandRight(true)
 #endif
@@ -114,9 +108,6 @@ public:
 #if OS(MACOSX)
     PlatformWheelEventPhase phase() const { return m_phase; }
     PlatformWheelEventPhase momentumPhase() const { return m_momentumPhase; }
-    unsigned scrollCount() const { return m_scrollCount; }
-    float unacceleratedScrollingDeltaX() const { return m_unacceleratedScrollingDeltaX; }
-    float unacceleratedScrollingDeltaY() const { return m_unacceleratedScrollingDeltaY; }
     bool useLatchedEventNode() const { return m_momentumPhase == PlatformWheelEventPhaseBegan || m_momentumPhase == PlatformWheelEventPhaseChanged; }
     bool canRubberbandLeft() const { return m_canRubberbandLeft; }
     bool canRubberbandRight() const { return m_canRubberbandRight; }
@@ -136,9 +127,6 @@ protected:
 #if OS(MACOSX)
     PlatformWheelEventPhase m_phase;
     PlatformWheelEventPhase m_momentumPhase;
-    unsigned m_scrollCount;
-    float m_unacceleratedScrollingDeltaX;
-    float m_unacceleratedScrollingDeltaY;
     bool m_canRubberbandLeft;
     bool m_canRubberbandRight;
 #endif
