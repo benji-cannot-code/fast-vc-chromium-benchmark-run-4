b4ae5bde794e4be4d6fb2e18b7e0d4bf81e36f9e
alemate <alemate@chromium.org>
2014-10-16 17:15:55 -0700

    Revert of Revert of Revert of ChromeOS NetworkScreenHandler should not call CheckAndResolveLocale on UI thread. (patchs… (patchset #1 id:1 of https://codereview.chromium.org/658903004/)
    Reason for revert:
    Speculatively reverting (second time) for https://crbug.com/422703 .
    
    Original issue's description:
    > Revert of Revert of ChromeOS NetworkScreenHandler should not call CheckAndResolveLocale on UI thread. (patchs… (patchset #1 id:1 of https://codereview.chromium.org/653123002/)
    >
    > Reason for revert:
    > The speculative revert didn't help.
    >
    > Original issue's description:
    > > Revert of ChromeOS NetworkScreenHandler should not call CheckAndResolveLocale on UI thread. (patchset #17 id:320001 of https://codereview.chromium.org/620563002/)
    > >
    > > Reason for revert:
    > > Speculatively reverting for https://crbug.com/422703
    > >
    > > Original issue's description:
    > > > ChromeOS NetworkScreenHandler should not call CheckAndResolveLocale on UI thread.
    > > >
    > > > BUG=419030
    > > >
    > > > Committed: https://crrev.com/d52dffddbf0abe5ca55509a9cdedcd9b0674bb1c
    > > > Cr-Commit-Position: refs/heads/master@{#298860}
    > >
    > > TBR=dzhioev@chromium.org,nkostylev@chromium.org
    > > NOTREECHECKS=true
    > > NOTRY=true
    > > BUG=422703
    > >
    > > Committed: https://crrev.com/f09329c436f4929c1f9ffd59e49aa4aef441338b
    > > Cr-Commit-Position: refs/heads/master@{#299480}
    >
    > TBR=dzhioev@chromium.org,nkostylev@chromium.org
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=422703
    >
    > Committed: https://crrev.com/f4af00363b57344b5d024791b751d966a34b0eb8
    > Cr-Commit-Position: refs/heads/master@{#299883}
    
    TBR=dzhioev@chromium.org,nkostylev@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=422703
    
    Review URL: https://codereview.chromium.org/664503003
    
    Cr-Commit-Position: refs/heads/master@{#300022}

--- chrome/browser/chromeos/chrome_browser_main_chromeos.cc
+++ chrome/browser/chromeos/chrome_browser_main_chromeos.cc
@@ -453,7 +453,9 @@ class GuestLanguageSetCallbackData {
 
   // Must match SwitchLanguageCallback type.
   static void Callback(const scoped_ptr<GuestLanguageSetCallbackData>& self,
-                       const locale_util::LanguageSwitchResult& result);
+                       const std::string& locale,
+                       const std::string& loaded_locale,
+                       bool success);
 
   Profile* profile;
 };
@@ -461,7 +463,9 @@ class GuestLanguageSetCallbackData {
 // static
 void GuestLanguageSetCallbackData::Callback(
     const scoped_ptr<GuestLanguageSetCallbackData>& self,
-    const locale_util::LanguageSwitchResult& result) {
+    const std::string& locale,
+    const std::string& loaded_locale,
+    bool success) {
   input_method::InputMethodManager* manager =
       input_method::InputMethodManager::Get();
   scoped_refptr<input_method::InputMethodManager::State> ime_state =
@@ -477,7 +481,7 @@ void GuestLanguageSetCallbackData::Callback(
   // Second, enable locale based input methods.
   const std::string locale_default_input_method =
       manager->GetInputMethodUtil()->GetLanguageDefaultInputMethodId(
-          result.loaded_locale);
+          loaded_locale);
   if (!locale_default_input_method.empty()) {
     PrefService* user_prefs = self->profile->GetPrefs();
     user_prefs->SetString(prefs::kLanguagePreviousInputMethod,
@@ -495,12 +499,13 @@ void GuestLanguageSetCallbackData::Callback(
 void SetGuestLocale(Profile* const profile) {
   scoped_ptr<GuestLanguageSetCallbackData> data(
       new GuestLanguageSetCallbackData(profile));
-  locale_util::SwitchLanguageCallback callback(base::Bind(
-      &GuestLanguageSetCallbackData::Callback, base::Passed(data.Pass())));
+  scoped_ptr<locale_util::SwitchLanguageCallback> callback(
+      new locale_util::SwitchLanguageCallback(base::Bind(
+          &GuestLanguageSetCallbackData::Callback, base::Passed(data.Pass()))));
   user_manager::User* const user =
       ProfileHelper::Get()->GetUserByProfile(profile);
   UserSessionManager::GetInstance()->RespectLocalePreference(
-      profile, user, callback);
+      profile, user, callback.Pass());
 }
 
 void ChromeBrowserMainPartsChromeos::PostProfileInit() {
