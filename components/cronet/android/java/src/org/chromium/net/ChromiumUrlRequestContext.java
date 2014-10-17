7b825d88abc8e176c452215c35ed3b121cd7f607
xunjieli <xunjieli@chromium.org>
2014-10-17 05:58:28 -0700

    Setup ProxyConfigServiceAndroid in Cronet
    BUG=411377
    
    Review URL: https://codereview.chromium.org/624443003
    
    Cr-Commit-Position: refs/heads/master@{#300094}

--- components/cronet/android/java/src/org/chromium/net/ChromiumUrlRequestContext.java
+++ components/cronet/android/java/src/org/chromium/net/ChromiumUrlRequestContext.java
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.net;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
 import android.os.Process;
 import android.util.Log;
 
@@ -29,7 +31,6 @@ public class ChromiumUrlRequestContext {
 
     /**
      * Constructor.
-     *
      */
     protected ChromiumUrlRequestContext(Context context, String userAgent,
             String config) {
@@ -38,6 +39,17 @@ public class ChromiumUrlRequestContext {
         if (mChromiumUrlRequestContextAdapter == 0) {
             throw new NullPointerException("Context Adapter creation failed");
         }
+        // Post a task to UI thread to init native Chromium URLRequestContext.
+        // TODO(xunjieli): This constructor is not supposed to be invoked on
+        // the main thread. Consider making the following code into a blocking
+        // API to handle the case where we are already on main thread.
+        Runnable task = new Runnable() {
+            public void run() {
+                nativeInitRequestContextOnMainThread(
+                        mChromiumUrlRequestContextAdapter);
+            }
+        };
+        new Handler(Looper.getMainLooper()).post(task);
     }
 
     /**
@@ -130,4 +142,7 @@ public class ChromiumUrlRequestContext {
             long chromiumUrlRequestContextAdapter, String fileName);
 
     private native void nativeStopNetLog(long chromiumUrlRequestContextAdapter);
+
+    private native void nativeInitRequestContextOnMainThread(
+            long chromiumUrlRequestContextAdapter);
 }
--- components/cronet/android/test/src/org/chromium/cronet_test_apk/CronetTestActivity.java
+++ components/cronet/android/test/src/org/chromium/cronet_test_apk/CronetTestActivity.java
@@ -13,7 +13,6 @@ import android.os.Environment;
 import android.util.Log;
 
 import org.chromium.base.PathUtils;
-import org.chromium.net.ChromiumUrlRequestFactory;
 import org.chromium.net.HttpUrlRequest;
 import org.chromium.net.HttpUrlRequestFactory;
 import org.chromium.net.HttpUrlRequestFactoryConfig;
@@ -37,8 +36,8 @@ public class CronetTestActivity extends Activity {
     public static final String COMMAND_LINE_ARGS_KEY = "commandLineArgs";
     public static final String POST_DATA_KEY = "postData";
     public static final String CONFIG_KEY = "config";
+    public static final String SKIP_FACTORY_INIT_KEY = "skipFactoryInit";
 
-    ChromiumUrlRequestFactory mChromiumRequestFactory;
     HttpUrlRequestFactory mRequestFactory;
 
     String mUrl;
@@ -79,6 +78,23 @@ public class CronetTestActivity extends Activity {
             return;
         }
 
+        String skipInitString = getCommandLineArg(SKIP_FACTORY_INIT_KEY);
+        if (skipInitString != null) {
+            return;
+        }
+        mRequestFactory = initRequestFactory();
+        if (mRequestFactory == null) {
+            return;
+        }
+
+        String appUrl = getUrlFromIntent(getIntent());
+        if (appUrl != null) {
+            startWithURL(appUrl);
+        }
+    }
+
+    // Helper function to initialize request factory. Also used in testing.
+    public HttpUrlRequestFactory initRequestFactory() {
         HttpUrlRequestFactoryConfig config = new HttpUrlRequestFactoryConfig();
         config.enableHttpCache(HttpUrlRequestFactoryConfig.HttpCache.IN_MEMORY,
                                100 * 1024)
@@ -94,23 +110,15 @@ public class CronetTestActivity extends Activity {
             } catch (org.json.JSONException e) {
                 Log.e(TAG, "Invalid Config.", e);
                 finish();
-                return;
+                return null;
             }
         }
 
         // Setting this here so it isn't overridden on the command line
         config.setLibraryName("cronet_tests");
 
-        mRequestFactory = HttpUrlRequestFactory.createFactory(
-                getApplicationContext(), config);
-
-        mChromiumRequestFactory = new ChromiumUrlRequestFactory(
-                getApplicationContext(), config);
-
-        String appUrl = getUrlFromIntent(getIntent());
-        if (appUrl != null) {
-            startWithURL(appUrl);
-        }
+        return HttpUrlRequestFactory.createFactory(getApplicationContext(),
+                                                   config);
     }
 
     private boolean loadTestFiles() {
@@ -218,12 +226,12 @@ public class CronetTestActivity extends Activity {
     }
 
     public void startNetLog() {
-        mChromiumRequestFactory.getRequestContext().startNetLogToFile(
+        mRequestFactory.startNetLogToFile(
                 Environment.getExternalStorageDirectory().getPath() +
                         "/cronet_sample_netlog.json");
     }
 
     public void stopNetLog() {
-        mChromiumRequestFactory.getRequestContext().stopNetLog();
+        mRequestFactory.stopNetLog();
     }
 }
