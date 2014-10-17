de5961fad38fe5dab2af0e329067d29abeebe256
eae@chromium.org <eae@chromium.org>
2014-10-17 22:10:58 +0000

    ASSERT that characterIndexForXPosition has a GlyphToCharacter vector
    ASSERT that the m_glyphToCharacterIndexes vector is available in the
    characterIndexForXPosition method.
    
    BUG=423649
    R=jln@chromium.org
    
    Review URL: https://codereview.chromium.org/666613002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183930 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/fonts/shaping/HarfBuzzShaper.cpp
+++ third_party/WebKit/Source/platform/fonts/shaping/HarfBuzzShaper.cpp
@@ -318,6 +318,7 @@ int HarfBuzzShaper::HarfBuzzRun::characterIndexForXPosition(float targetX)
 float HarfBuzzShaper::HarfBuzzRun::xPositionForOffset(unsigned offset)
 {
     ASSERT(offset < m_numCharacters);
+    ASSERT(hasGlyphToCharacterIndexes());
     unsigned glyphIndex = 0;
     float position = 0;
     if (rtl()) {
