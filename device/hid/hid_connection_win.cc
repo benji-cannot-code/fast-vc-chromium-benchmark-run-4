31a3a06cf2485c786b0a7629ecaafbd213f5507f
pkasting <pkasting@chromium.org>
2014-10-17 19:13:01 -0700

    Fix type truncation warnings in HID code.
    GetShortData() and IOHIDElementGetUsage() both return uint32s, so raise the size
    of various containing types to uint32.
    
    I tried to look in the USB HID spec to see if any of the affected fields here
    were limited to e.g. <2^16, meaning that it'd be safe to just cast down to the
    existing uint16s instead, but I couldn't find any limits.
    
    BUG=81439
    TEST=none
    
    Review URL: https://codereview.chromium.org/656583003
    
    Cr-Commit-Position: refs/heads/master@{#300201}

--- device/hid/hid_connection_win.cc
+++ device/hid/hid_connection_win.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/files/file.h"
 #include "base/message_loop/message_loop.h"
+#include "base/numerics/safe_conversions.h"
 #include "base/profiler/scoped_profile.h"
 #include "base/win/object_watcher.h"
 
@@ -136,8 +137,8 @@ void HidConnectionWin::PlatformRead(
     const HidConnection::ReadCallback& callback) {
   // Windows will always include the report ID (including zero if report IDs
   // are not in use) in the buffer.
-  scoped_refptr<net::IOBufferWithSize> buffer =
-      new net::IOBufferWithSize(device_info().max_input_report_size + 1);
+  scoped_refptr<net::IOBufferWithSize> buffer = new net::IOBufferWithSize(
+      base::checked_cast<int>(device_info().max_input_report_size + 1));
   scoped_refptr<PendingHidTransfer> transfer(new PendingHidTransfer(
       buffer,
       base::Bind(&HidConnectionWin::OnReadComplete, this, buffer, callback)));
@@ -168,8 +169,8 @@ void HidConnectionWin::PlatformWrite(scoped_refptr<net::IOBuffer> buffer,
 void HidConnectionWin::PlatformGetFeatureReport(uint8_t report_id,
                                                 const ReadCallback& callback) {
   // The first byte of the destination buffer is the report ID being requested.
-  scoped_refptr<net::IOBufferWithSize> buffer =
-      new net::IOBufferWithSize(device_info().max_feature_report_size + 1);
+  scoped_refptr<net::IOBufferWithSize> buffer = new net::IOBufferWithSize(
+      base::checked_cast<int>(device_info().max_feature_report_size + 1));
   buffer->data()[0] = report_id;
 
   scoped_refptr<PendingHidTransfer> transfer(new PendingHidTransfer(
