dc373c49a9c0228d54f91f2514886e4d62e8ba78
kareng <kareng@google.com>
2014-10-18 22:16:46 -0700

    Revert of [ServiceWorkerCache] Implement storage::QuotaClient (patchset #12 id:320001 of https://codereview.chromium.org/651983002/)
    Reason for revert:
    bug 424923, top crash in canary.
    
    Original issue's description:
    > [ServiceWorkerCache] Implement storage::QuotaClient
    >
    > Adds a QuotaClient for the ServiceWorkerCache system.
    >
    > * Calls to the QuotaManagerProxy like (NotifyStorageAccessed/StorageModified/OriginInUse/etc..) will be in an upcoming CL.
    >
    > * The ServiceWorkerCacheQuotaClient is registered by the ServiceWorkerCacheStorageManager.  The QuotaClient holds a WeakPtr to the ServiceWorkerCacheStorageManager since the manager is destroyed before the QuotaClient.
    >
    > * Added a number of unittests.  They fit better into the StorageManager tests file than in their own file as they rely on a lot of the StorageManager infrastructure.
    >
    > * In order to support QuotaClient the ServiceWorkerCacheStorageManager needs to be able to list the origins it supports. This data is not currently stored so an "origin" member has been added to the CacheStorage index protobuf.  We won't report the origins of existing caches (if any even exist at this point) until their index is next written.
    >
    > BUG=420159
    >
    > Committed: https://crrev.com/22fcbd3c898136dfe839de3ca5cb0633c17bb163
    > Cr-Commit-Position: refs/heads/master@{#300187}
    
    TBR=michaeln@chromium.org,falken@chromium.org,jkarlin@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=420159
    
    Review URL: https://codereview.chromium.org/651433005
    
    Cr-Commit-Position: refs/heads/master@{#300224}

--- content/browser/service_worker/service_worker_cache_storage.h
+++ content/browser/service_worker/service_worker_cache_storage.h
@@ -50,18 +50,13 @@ class CONTENT_EXPORT ServiceWorkerCacheStorage {
   typedef base::Callback<void(const StringVector&, CacheStorageError)>
       StringsAndErrorCallback;
 
-  static const char kIndexFileName[];
-
   ServiceWorkerCacheStorage(
       const base::FilePath& origin_path,
       bool memory_only,
       base::SequencedTaskRunner* cache_task_runner,
       net::URLRequestContext* request_context,
-      base::WeakPtr<storage::BlobStorageContext> blob_context,
-      const GURL& origin);
+      base::WeakPtr<storage::BlobStorageContext> blob_context);
 
-  // Any unfinished asynchronous operations may not complete or call their
-  // callbacks.
   virtual ~ServiceWorkerCacheStorage();
 
   // Create a ServiceWorkerCache if it doesn't already exist and call the
@@ -93,8 +88,6 @@ class CONTENT_EXPORT ServiceWorkerCacheStorage {
   void EnumerateCaches(const StringsAndErrorCallback& callback);
 
   // TODO(jkarlin): Add match() function.
-
-  void CloseAllCaches();
 
  private:
   class MemoryLoader;
