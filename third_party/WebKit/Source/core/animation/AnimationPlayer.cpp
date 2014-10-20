09c34261f93e9b57d717ed28932252111d43b15d
dstockwell@chromium.org <dstockwell@chromium.org>
2014-10-20 01:50:33 +0000

    Web Animations: Compositor timeOffset should not be scaled
    This also seems to indicate that it's not yet possible to composite an
    animation with a negative playback rate and infinite duration.
    
    BUG=422432
    
    Review URL: https://codereview.chromium.org/667633003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183957 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/AnimationPlayer.cpp
+++ third_party/WebKit/Source/core/animation/AnimationPlayer.cpp
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/events/AnimationPlayerEvent.h"
 #include "core/frame/UseCounter.h"
 #include "platform/TraceEvent.h"
+#include "wtf/MathExtras.h"
 
 namespace blink {
 
@@ -598,7 +599,7 @@ void AnimationPlayer::setOutdated()
 
 bool AnimationPlayer::canStartAnimationOnCompositor()
 {
-    if (m_playbackRate == 0)
+    if (m_playbackRate == 0 || (std::isinf(sourceEnd()) && m_playbackRate < 0))
         return false;
 
     return m_timeline && m_content && m_content->isAnimation() && playing();
@@ -613,6 +614,7 @@ bool AnimationPlayer::maybeStartAnimationOnCompositor()
     double timeOffset = 0;
     if (std::isnan(startTime)) {
         timeOffset = m_playbackRate < 0 ? sourceEnd() - currentTimeInternal() : currentTimeInternal();
+        timeOffset = timeOffset / fabs(m_playbackRate);
     }
     return toAnimation(m_content.get())->maybeStartAnimationOnCompositor(startTime, timeOffset, m_playbackRate);
 }
