ff617a62f1a60efa486b452b6f2f8df840394dd3
sigbjornf <sigbjornf@opera.com>
2014-10-20 05:59:30 -0700

    Blink GC plugin: Gracefully handle cyclic part objects.
    When checking a class if it or its part objects don't define GC roots,
    avoid looping on part objects that are cyclic. i.e., the part object
    containing a collection that refers back to itself.
    
    R=haraken
    BUG=424962
    
    Review URL: https://codereview.chromium.org/666473003
    
    Cr-Commit-Position: refs/heads/master@{#300256}

--- tools/clang/blink_gc_plugin/BlinkGCPlugin.cpp
+++ tools/clang/blink_gc_plugin/BlinkGCPlugin.cpp
@@ -573,6 +573,7 @@ class CheckTraceVisitor : public RecursiveASTVisitor<CheckTraceVisitor> {
 class CheckGCRootsVisitor : public RecursiveEdgeVisitor {
  public:
   typedef std::vector<FieldPoint*> RootPath;
+  typedef std::set<RecordInfo*> VisitingSet;
   typedef std::vector<RootPath> Errors;
 
   CheckGCRootsVisitor() {}
@@ -595,6 +596,11 @@ class CheckGCRootsVisitor : public RecursiveEdgeVisitor {
     if (edge->value()->record()->isUnion())
       return;
 
+    // Prevent infinite regress for cyclic part objects.
+    if (visiting_set_.find(edge->value()) != visiting_set_.end())
+      return;
+
+    visiting_set_.insert(edge->value());
     // If the value is a part object, then continue checking for roots.
     for (Context::iterator it = context().begin();
          it != context().end();
@@ -603,6 +609,7 @@ class CheckGCRootsVisitor : public RecursiveEdgeVisitor {
         return;
     }
     ContainsGCRoots(edge->value());
+    visiting_set_.erase(edge->value());
   }
 
   void VisitPersistent(Persistent* edge) override {
@@ -616,6 +623,7 @@ class CheckGCRootsVisitor : public RecursiveEdgeVisitor {
 
  protected:
   RootPath current_;
+  VisitingSet visiting_set_;
   Errors gc_roots_;
 };
 
--- tools/clang/blink_gc_plugin/tests/fields_require_tracing.h
+++ tools/clang/blink_gc_plugin/tests/fields_require_tracing.h
@@ -11,6 +11,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class HeapObject;
+class PartObject;
+
+class PartBObject {
+    DISALLOW_ALLOCATION();
+public:
+    void trace(Visitor*);
+private:
+    HeapHashSet<PartBObject> m_set;
+    HeapVector<PartBObject> m_vector;
+};
 
 class PartObject {
     DISALLOW_ALLOCATION();
@@ -20,6 +30,8 @@ private:
     Member<HeapObject> m_obj1;
     Member<HeapObject> m_obj2;
     Member<HeapObject> m_obj3;
+
+    HeapVector<PartBObject> m_parts;
 };
 
 class HeapObject : public GarbageCollected<HeapObject> {
