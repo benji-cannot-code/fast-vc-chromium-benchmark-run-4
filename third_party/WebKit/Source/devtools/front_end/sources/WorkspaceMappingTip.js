511908f85d4173e4203519b65d68afe9ff67b2c4
pfeldman@chromium.org <pfeldman@chromium.org>
2014-10-20 11:48:35 +0000

    DevTools: do not suggest mapping workspace to itself, remove workspace mapping tip from behind experiments.
    Review URL: https://codereview.chromium.org/664713002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183973 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sources/WorkspaceMappingTip.js
+++ third_party/WebKit/Source/devtools/front_end/sources/WorkspaceMappingTip.js
@@ -27,11 +27,9 @@ WebInspector.WorkspaceMappingTip.prototype = {
     _editorSelected: function(event)
     {
         var uiSourceCode = /** @type {!WebInspector.UISourceCode} */ (event.data);
-        if (Runtime.experiments.isEnabled("suggestUsingWorkspace")) {
-            if (this._editorSelectedTimer)
-                clearTimeout(this._editorSelectedTimer);
-            this._editorSelectedTimer = setTimeout(this._updateSuggestedMappingInfobar.bind(this, uiSourceCode), 3000);
-        }
+        if (this._editorSelectedTimer)
+            clearTimeout(this._editorSelectedTimer);
+        this._editorSelectedTimer = setTimeout(this._updateSuggestedMappingInfobar.bind(this, uiSourceCode), 3000);
     },
 
     /**
@@ -53,7 +51,7 @@ WebInspector.WorkspaceMappingTip.prototype = {
                 return;
         }
 
-        var networkProjects = this._workspace.projectsForType(WebInspector.projectTypes.FileSystem);
+        var networkProjects = this._workspace.projectsForType(WebInspector.projectTypes.Network);
         networkProjects = networkProjects.concat(this._workspace.projectsForType(WebInspector.projectTypes.ContentScripts));
         for (var i = 0; i < networkProjects.length; ++i) {
             var name = uiSourceCode.name();
