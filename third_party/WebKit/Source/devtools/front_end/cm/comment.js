58aa4f45244118e0c66410a5b93f299233e7cc7b
lushnikov@chromium.org <lushnikov@chromium.org>
2014-10-20 17:22:44 +0000

    DevTools: [CodeMirror] roll comment plugin to @7dfbf42
    BUG=414302
    R=vsevik
    
    Review URL: https://codereview.chromium.org/658563005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183998 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/cm/comment.js
+++ third_party/WebKit/Source/devtools/front_end/cm/comment.js
@@ -110,7 +110,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   CodeMirror.defineExtension("uncomment", function(from, to, options) {
     if (!options) options = noOptions;
     var self = this, mode = self.getModeAt(from);
-    var end = Math.min(to.line, self.lastLine()), start = Math.min(from.line, end);
+    var end = Math.min(to.ch != 0 || to.line == from.line ? to.line : to.line - 1, self.lastLine()), start = Math.min(from.line, end);
 
     // Try finding line comments
     var lineString = options.lineComment || mode.lineComment, lines = [];
