aa904211ac18172068e19c098d8da2be518de4e4
vadimt <vadimt@chromium.org>
2014-10-20 15:40:15 -0700

    Adding instrumentation to locate the source of jankiness
    Mechanical change that adds instrumentation required to locate the source of jankiness (i.e. a long-running fragment of code executed as a part of the task that causes jank) in the code. See the bug for details on what kind of jank we are after.
    A number of similar CLs were landed, and none of them caused issues. The code of the instrumentation is highly optimized and is not expected to affect performance. The code simply creates a diagnostic task which is identical to ones created by PostTask or IPC message handlers.
    Landing as TBR since this is a mechanical, safe and temporary change.
    
    This CL instruments most popular OnRefreshTokenAvailable overrides.
    
    BUG=422460
    TBR=jam@chromium.org, zelidrag@chromium.org, erikwright@chromium.org
    
    Review URL: https://codereview.chromium.org/643183003
    
    Cr-Commit-Position: refs/heads/master@{#300361}

--- chrome/browser/sync/profile_sync_service.cc
+++ chrome/browser/sync/profile_sync_service.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/message_loop/message_loop.h"
 #include "base/metrics/histogram.h"
+#include "base/profiler/scoped_profile.h"
 #include "base/strings/string16.h"
 #include "base/strings/stringprintf.h"
 #include "base/threading/thread_restrictions.h"
@@ -756,6 +757,11 @@ void ProfileSyncService::OnGetTokenFailure(
 
 void ProfileSyncService::OnRefreshTokenAvailable(
     const std::string& account_id) {
+  // TODO(vadimt): Remove ScopedProfile below once crbug.com/422460 is fixed.
+  tracked_objects::ScopedProfile tracking_profile(
+      FROM_HERE_WITH_EXPLICIT_FUNCTION(
+          "422460 ProfileSyncService::OnRefreshTokenAvailable"));
+
   if (account_id == signin_->GetAccountIdToUse())
     OnRefreshTokensLoaded();
 }
--- google_apis/gaia/account_tracker.cc
+++ google_apis/gaia/account_tracker.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/debug/trace_event.h"
 #include "base/logging.h"
+#include "base/profiler/scoped_profile.h"
 #include "base/stl_util.h"
 #include "net/url_request/url_request_context_getter.h"
 
@@ -85,6 +86,11 @@ AccountIds AccountTracker::FindAccountIdsByGaiaId(const std::string& gaia_id) {
 }
 
 void AccountTracker::OnRefreshTokenAvailable(const std::string& account_id) {
+  // TODO(vadimt): Remove ScopedProfile below once crbug.com/422460 is fixed.
+  tracked_objects::ScopedProfile tracking_profile(
+      FROM_HERE_WITH_EXPLICIT_FUNCTION(
+          "422460 AccountTracker::OnRefreshTokenAvailable"));
+
   TRACE_EVENT1("identity",
                "AccountTracker::OnRefreshTokenAvailable",
                "account_key",
