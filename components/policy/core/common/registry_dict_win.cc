a720e32b2c03dbf9654340dd8f81fa01fc48cfda
dcheng <dcheng@chromium.org>
2014-10-20 18:03:33 -0700

    Use scoped_ptr::Pass instead of scoped_ptr::PassAs<T>.
    Cleanup to make future migration to std::move() easier.
    
    BUG=423621
    
    Review URL: https://codereview.chromium.org/665663002
    
    Cr-Commit-Position: refs/heads/master@{#300392}

--- components/policy/core/common/registry_dict_win.cc
+++ components/policy/core/common/registry_dict_win.cc
@@ -54,9 +54,9 @@ scoped_ptr<base::Value> ConvertValue(const base::Value& value,
         if (converted)
           result->Append(converted.release());
       }
-      return result.PassAs<base::Value>();
+      return result.Pass();
     }
-    return make_scoped_ptr(value.DeepCopy()).Pass();
+    return make_scoped_ptr(value.DeepCopy());
   }
 
   // Else, do some conversions to map windows registry data types to JSON types.
@@ -109,7 +109,7 @@ scoped_ptr<base::Value> ConvertValue(const base::Value& value,
           if (converted)
             result->Append(converted.release());
         }
-        return result.PassAs<base::Value>();
+        return result.Pass();
       }
       // Fall through in order to accept lists encoded as JSON strings.
     }
