a317d29d775778f4b9b64ebf2bafb76d8d28413a
yoav@yoav.ws <yoav@yoav.ws>
2014-10-21 11:40:34 +0000

    Add assert on frameView::wasViewportResized
    While going over the code, I found that renderView()->style() is used without a null check.
    I'm adding an assert to clarify that we're assuming it should be there when renderView() is.
    
    Review URL: https://codereview.chromium.org/663203003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184075 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/FrameView.cpp
+++ third_party/WebKit/Source/core/frame/FrameView.cpp
@@ -2025,6 +2025,7 @@ bool FrameView::wasViewportResized()
     RenderView* renderView = this->renderView();
     if (!renderView)
         return false;
+    ASSERT(renderView->style());
     return (layoutSize(IncludeScrollbars) != m_lastViewportSize || renderView->style()->zoom() != m_lastZoomFactor);
 }
 
