a401274cbe3d70a42976b45c02ef23123869a98b
sergeyv@chromium.org <sergeyv@chromium.org>
2014-10-21 10:04:48 +0000

    DevTools: Use script mappings to linkify console messages
    BUG=357958
    
    Review URL: https://codereview.chromium.org/671453002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184066 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/console/ConsoleViewMessage.js
+++ third_party/WebKit/Source/devtools/front_end/console/ConsoleViewMessage.js
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @constructor
  * @implements {WebInspector.ViewportElement}
  * @param {!WebInspector.ConsoleMessage} consoleMessage
- * @param {?WebInspector.Linkifier} linkifier
+ * @param {!WebInspector.Linkifier} linkifier
  * @param {number} nestingLevel
  */
 WebInspector.ConsoleViewMessage = function(consoleMessage, linkifier, nestingLevel)
@@ -261,9 +261,8 @@ WebInspector.ConsoleViewMessage.prototype = {
      */
     _linkifyLocation: function(url, lineNumber, columnNumber)
     {
-        console.assert(this._linkifier);
         var target = this._target();
-        if (!this._linkifier || !target)
+        if (!target)
             return null;
         // FIXME(62725): stack trace line/column numbers are one-based.
         lineNumber = lineNumber ? lineNumber - 1 : 0;
@@ -283,7 +282,6 @@ WebInspector.ConsoleViewMessage.prototype = {
      */
     _linkifyCallFrame: function(callFrame)
     {
-        console.assert(this._linkifier);
         var target = this._target();
         if (!this._linkifier)
             return null;
@@ -300,9 +298,8 @@ WebInspector.ConsoleViewMessage.prototype = {
      */
     _linkifyScriptId: function(scriptId, url, lineNumber, columnNumber)
     {
-        console.assert(this._linkifier);
         var target = this._target();
-        if (!this._linkifier || !target)
+        if (!target)
             return null;
         // FIXME(62725): stack trace line/column numbers are one-based.
         lineNumber = lineNumber ? lineNumber - 1 : 0;
@@ -390,7 +387,7 @@ WebInspector.ConsoleViewMessage.prototype = {
         for (var i = 0; i < parameters.length; ++i) {
             // Inline strings when formatting.
             if (shouldFormatMessage && parameters[i].type === "string")
-                formattedResult.appendChild(WebInspector.linkifyStringAsFragment(parameters[i].description));
+                formattedResult.appendChild(this._linkifier.linkifyStringAsFragment(this._target(), parameters[i].description));
             else
                 formattedResult.appendChild(this._formatParameter(parameters[i], false, true));
             if (i < parameters.length - 1)
@@ -712,7 +709,7 @@ WebInspector.ConsoleViewMessage.prototype = {
     {
         var span = createElement("span");
         span.className = "console-formatted-string source-code";
-        span.appendChild(WebInspector.linkifyStringAsFragment(output.description || ""));
+        span.appendChild(this._linkifier.linkifyStringAsFragment(this._target(), output.description || ""));
 
         // Make black quotes.
         elem.classList.remove("console-formatted-string");
@@ -914,12 +911,15 @@ WebInspector.ConsoleViewMessage.prototype = {
 
         formatters._ = bypassFormatter;
 
+        /**
+         * @this {WebInspector.ConsoleViewMessage}
+         */
         function append(a, b)
         {
             if (b instanceof Node)
                 a.appendChild(b);
             else if (typeof b !== "undefined") {
-                var toAppend = WebInspector.linkifyStringAsFragment(String(b));
+                var toAppend = this._linkifier.linkifyStringAsFragment(this._target(), String(b));
                 if (currentStyle) {
                     var wrapper = createElement('span');
                     for (var key in currentStyle)
@@ -933,7 +933,7 @@ WebInspector.ConsoleViewMessage.prototype = {
         }
 
         // String.format does treat formattedResult like a Builder, result is an object.
-        return String.format(format, parameters, formatters, formattedResult, append);
+        return String.format(format, parameters, formatters, formattedResult, append.bind(this));
     },
 
     clearHighlight: function()
@@ -1271,7 +1271,7 @@ WebInspector.ConsoleViewMessage.prototype = {
  * @constructor
  * @extends {WebInspector.ConsoleViewMessage}
  * @param {!WebInspector.ConsoleMessage} consoleMessage
- * @param {?WebInspector.Linkifier} linkifier
+ * @param {!WebInspector.Linkifier} linkifier
  * @param {number} nestingLevel
  */
 WebInspector.ConsoleGroupViewMessage = function(consoleMessage, linkifier, nestingLevel)
