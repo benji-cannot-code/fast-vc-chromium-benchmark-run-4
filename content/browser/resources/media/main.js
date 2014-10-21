91a2e4e3a308abc8d3f7a01d862f24569a6fa912
burnik <burnik@chromium.org>
2014-10-21 15:35:01 -0700

    Video Capture Capabilities on chrome://media-internals page are updated with:
    - Layout adjustments
    - Parsed capture formats (resolution, fps, [pixel format])
    - String representation of capture API types
    - Unit test checking all capture API types and video format strings are up to date
    
    Preview screenshot (linux): http://goo.gl/QTPUhj
    
    BUG=425029, 344882
    
    Review URL: https://codereview.chromium.org/637953008
    
    Cr-Commit-Position: refs/heads/master@{#300561}

--- content/browser/resources/media/main.js
+++ content/browser/resources/media/main.js
@@ -48,7 +48,6 @@ var media = (function() {
 
   media.onReceiveVideoCaptureCapabilities = function(videoCaptureCapabilities) {
     manager.updateVideoCaptureCapabilities(videoCaptureCapabilities)
-    console.log("Video capabilities", videoCaptureCapabilities);
   }
 
   media.onReceiveConstants = function(constants) {
