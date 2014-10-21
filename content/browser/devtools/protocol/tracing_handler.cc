0bf990dc83e8ad3d16572b81cf3aebc89c8249b3
dgozman <dgozman@chromium.org>
2014-10-21 05:43:02 -0700

    [DevTools] Add implementation for Tracing.start with optional parameters.
    BUG=405566
    TBR=pfeldman
    
    Review URL: https://codereview.chromium.org/663373002
    
    Cr-Commit-Position: refs/heads/master@{#300477}

--- content/browser/devtools/protocol/tracing_handler.cc
+++ content/browser/devtools/protocol/tracing_handler.cc
@@ -21,12 +21,6 @@ void TracingHandler::SetClient(scoped_ptr<Client> client) {
   client_.swap(client);
 }
 
-Response TracingHandler::Start(const std::string& categories,
-                               const std::string& options,
-                               const double* buffer_usage_reporting_interval) {
-  return Response::FallThrough();
-}
-
 scoped_refptr<DevToolsProtocol::Response> TracingHandler::Start(
     const std::string& categories,
     const std::string& options,
@@ -35,8 +29,12 @@ scoped_refptr<DevToolsProtocol::Response> TracingHandler::Start(
   return NULL;
 }
 
-Response TracingHandler::End() {
-  return Response::FallThrough();
+scoped_refptr<DevToolsProtocol::Response> TracingHandler::Start(
+    const std::string* categories,
+    const std::string* options,
+    const double* buffer_usage_reporting_interval,
+    scoped_refptr<DevToolsProtocol::Command> command) {
+  return NULL;
 }
 
 scoped_refptr<DevToolsProtocol::Response> TracingHandler::End(
