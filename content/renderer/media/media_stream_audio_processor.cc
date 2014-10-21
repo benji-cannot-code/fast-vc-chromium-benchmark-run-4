efea065a7801d6549b9546a742cb33ebb7f404ca
xians <xians@chromium.org>
2014-10-21 10:16:34 -0700

    Reland 597923002: Fix the way how we create webrtc::AudioProcessing in Chrome.
    The original review thread is in https://codereview.chromium.org/588523002/
    
    Fix the way how we create webrtc::AudioProcessing in Chrome.
    
    TBR=tommi@chromium.org,maruel@chromium.org
    
    BUG=415935
    TEST=all webrtc tests in all bots + manual test to verify the agc loggings exist.
    
    Review URL: https://codereview.chromium.org/663413002
    
    Cr-Commit-Position: refs/heads/master@{#300509}

--- content/renderer/media/media_stream_audio_processor.cc
+++ content/renderer/media/media_stream_audio_processor.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/audio_fifo.h"
 #include "media/base/channel_layout.h"
 #include "third_party/WebKit/public/platform/WebMediaConstraints.h"
+#include "third_party/libjingle/overrides/init_webrtc.h"
 #include "third_party/libjingle/source/talk/app/webrtc/mediaconstraintsinterface.h"
 #include "third_party/webrtc/modules/audio_processing/typing_detection.h"
 
@@ -452,7 +453,7 @@ void MediaStreamAudioProcessor::InitializeAudioProcessingModule(
 #endif
 
   // Create and configure the webrtc::AudioProcessing.
-  audio_processing_.reset(webrtc::AudioProcessing::Create(config));
+  audio_processing_.reset(CreateWebRtcAudioProcessing(config));
 
   // Enable the audio processing components.
   if (echo_cancellation) {
