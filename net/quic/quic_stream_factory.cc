9e659a85f312a9f2dc99539ed25535f066a5c614
rtenneti <rtenneti@chromium.org>
2014-10-21 13:33:17 -0700

    QUIC - Added Net.QuicServerInfo.DiskCacheWaitForDataReadyTime to measure actual time QuicStreamFactory state machine was waiting to load QUIC server information from disk cache.
    Retired Net.QuicServerInfo.DiskCacheReadTime.
    
    R=asvitkine@chromium.org, rch@chromium.org
    
    Review URL: https://codereview.chromium.org/667683004
    
    Cr-Commit-Position: refs/heads/master@{#300549}

--- net/quic/quic_stream_factory.cc
+++ net/quic/quic_stream_factory.cc
@@ -313,7 +313,6 @@ void QuicStreamFactory::Job::OnIOComplete(int rv) {
 int QuicStreamFactory::Job::DoResolveHost() {
   // Start loading the data now, and wait for it after we resolve the host.
   if (server_info_) {
-    disk_cache_load_start_time_ = base::TimeTicks::Now();
     server_info_->Start();
   }
 
@@ -352,6 +351,7 @@ int QuicStreamFactory::Job::DoLoadServerInfo() {
   if (!server_info_)
     return OK;
 
+  disk_cache_load_start_time_ = base::TimeTicks::Now();
   return server_info_->WaitForDataReady(
       base::Bind(&QuicStreamFactory::Job::OnIOComplete,
                  weak_factory_.GetWeakPtr()));
@@ -359,7 +359,7 @@ int QuicStreamFactory::Job::DoLoadServerInfo() {
 
 int QuicStreamFactory::Job::DoLoadServerInfoComplete(int rv) {
   if (server_info_) {
-    UMA_HISTOGRAM_TIMES("Net.QuicServerInfo.DiskCacheReadTime",
+    UMA_HISTOGRAM_TIMES("Net.QuicServerInfo.DiskCacheWaitForDataReadyTime",
                         base::TimeTicks::Now() - disk_cache_load_start_time_);
   }
 
