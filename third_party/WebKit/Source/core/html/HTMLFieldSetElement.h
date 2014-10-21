a18813276ea028e0e388aabb20d5b14e32996d32
bartekn@chromium.org <bartekn@chromium.org>
2014-10-21 07:15:19 +0000

    Implement :valid and :invalid pseudoclass for <fieldset>
    BUG=424028
    TEST=Added layout test: fieldset-pseudo-valid-style.html
    
    Review URL: https://codereview.chromium.org/646273004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184047 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLFieldSetElement.h
+++ third_party/WebKit/Source/core/html/HTMLFieldSetElement.h
@@ -43,6 +43,8 @@ public:
 
     const FormAssociatedElement::List& associatedElements() const;
 
+    void setNeedsValidityCheck();
+
 protected:
     virtual void disabledAttributeChanged() override;
 
@@ -54,6 +56,8 @@ private:
     virtual RenderObject* createRenderer(RenderStyle*) override;
     virtual const AtomicString& formControlType() const override;
     virtual bool recalcWillValidate() const override { return false; }
+    virtual bool matchesValidityPseudoClasses() const override final;
+    virtual bool isValidElement() override final;
     virtual void childrenChanged(const ChildrenChange&) override;
     virtual bool areAuthorShadowsAllowed() const override { return false; }
 
--- third_party/WebKit/Source/core/html/HTMLFormControlElement.h
+++ third_party/WebKit/Source/core/html/HTMLFormControlElement.h
@@ -158,8 +158,8 @@ private:
     virtual short tabIndex() const override final;
 
     virtual bool isDefaultButtonForForm() const override final;
-    virtual bool isValidElement() override final;
-    virtual bool matchesValidityPseudoClasses() const override final;
+    virtual bool isValidElement() override;
+    virtual bool matchesValidityPseudoClasses() const override;
     void updateAncestorDisabledState() const;
 
     bool isValidationMessageVisible() const;
@@ -167,6 +167,8 @@ private:
 
     // Requests validity recalc for the form owner, if one exists.
     void formOwnerSetNeedsValidityCheck();
+    // Requests validity recalc for all ancestor fieldsets, if exist.
+    void fieldSetAncestorsSetNeedsValidityCheck(Node*);
 
     bool m_disabled : 1;
     bool m_isAutofilled : 1;
