d2bb990248166a12217e50b8973f7e33ae24a532
pkasting <pkasting@chromium.org>
2014-10-21 16:53:15 -0700

    Type conversion fixes, media/ edition.
    This is mostly to fix MSVC warnings about possible value truncation.
    
    BUG=81439
    TEST=none
    
    Review URL: https://codereview.chromium.org/638123004
    
    Cr-Commit-Position: refs/heads/master@{#300571}

--- media/cast/logging/log_serializer.cc
+++ media/cast/logging/log_serializer.cc
@@ -48,7 +48,7 @@ bool DoSerializeEvents(const LogMetadata& metadata,
 
   int proto_size = metadata.ByteSize();
   DCHECK(proto_size <= kMaxSerializedProtoBytes);
-  if (!writer.WriteU16(proto_size))
+  if (!writer.WriteU16(static_cast<uint16>(proto_size)))
     return false;
   if (!metadata.SerializeToArray(writer.ptr(), writer.remaining()))
     return false;
@@ -74,7 +74,7 @@ bool DoSerializeEvents(const LogMetadata& metadata,
     DCHECK(proto_size <= kMaxSerializedProtoBytes);
 
     // Write size of the proto, then write the proto.
-    if (!writer.WriteU16(proto_size))
+    if (!writer.WriteU16(static_cast<uint16>(proto_size)))
       return false;
     if (!frame_event.SerializeToArray(writer.ptr(), writer.remaining()))
       return false;
@@ -98,7 +98,7 @@ bool DoSerializeEvents(const LogMetadata& metadata,
     DCHECK(proto_size <= kMaxSerializedProtoBytes);
 
     // Write size of the proto, then write the proto.
-    if (!writer.WriteU16(proto_size))
+    if (!writer.WriteU16(static_cast<uint16>(proto_size)))
       return false;
     if (!packet_event.SerializeToArray(writer.ptr(), writer.remaining()))
       return false;
