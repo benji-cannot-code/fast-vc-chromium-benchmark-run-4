005af95731c4081c47cfb08d780ada5f552c0720
boliu <boliu@chromium.org>
2014-10-21 16:23:57 -0700

    aw: Ensure invalidation when pipeline is stalled
    * If CommitFrame is skipped to to stall, ensure we invalidate.
    * Make sure force_invalidate does not get cancelled by a
      pending fallback tick.
    * Allow hardware initialization even when visible rect is
      empty, so fallback tick is always in the right mode.
    * Fix visible rect empty checks.
    
    BUG=425372
    
    Review URL: https://codereview.chromium.org/654403006
    
    Cr-Commit-Position: refs/heads/master@{#300566}

--- android_webview/browser/shared_renderer_state.h
+++ android_webview/browser/shared_renderer_state.h
@@ -50,6 +50,7 @@ class SharedRendererState {
       const ParentCompositorDrawConstraints& parent_draw_constraints);
   void PostExternalDrawConstraintsToChildCompositor(
       const ParentCompositorDrawConstraints& parent_draw_constraints);
+  void DidSkipCommitFrame();
 
   const ParentCompositorDrawConstraints ParentDrawConstraints() const;
 
@@ -68,6 +69,7 @@ class SharedRendererState {
   void ResetRequestDrawGLCallback();
   void ClientRequestDrawGLOnUIThread();
   void UpdateParentDrawConstraintsOnUIThread();
+  void DidSkipCommitFrameOnUIThread();
   void SetInsideHardwareRelease(bool inside);
 
   scoped_refptr<base::MessageLoopProxy> ui_loop_;
