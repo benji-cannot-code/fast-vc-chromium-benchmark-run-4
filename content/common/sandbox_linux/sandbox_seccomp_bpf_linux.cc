e9a92cb9865dd3b49e605aacf78fe7fc6c02e6e1
dongseong.hwang <dongseong.hwang@intel.com>
2014-10-21 03:29:19 -0700

    Fix crash with --use-gl=egl on Linux.
    eglCreateWindowSurface() in mesa egl driver needs mincore() system call, so
    allow gpu process to call mincore().
    
    BUG=423674
    
    Review URL: https://codereview.chromium.org/656023003
    
    Cr-Commit-Position: refs/heads/master@{#300445}

--- content/common/sandbox_linux/sandbox_seccomp_bpf_linux.cc
+++ content/common/sandbox_linux/sandbox_seccomp_bpf_linux.cc
@@ -35,6 +35,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sandbox/linux/seccomp-bpf/sandbox_bpf.h"
 #include "sandbox/linux/services/linux_syscalls.h"
 
+#if !defined(IN_NACL_HELPER)
+#include "ui/gl/gl_switches.h"
+#endif
+
 using sandbox::BaselinePolicy;
 using sandbox::SandboxBPF;
 using sandbox::SyscallSets;
@@ -168,7 +172,11 @@ scoped_ptr<SandboxBPFBasePolicy> GetGpuProcessSandbox() {
     return scoped_ptr<SandboxBPFBasePolicy>(
         new CrosArmGpuProcessPolicy(allow_sysv_shm));
   } else {
-    return scoped_ptr<SandboxBPFBasePolicy>(new GpuProcessPolicy);
+    bool allow_mincore = command_line.HasSwitch(switches::kUseGL) &&
+                         command_line.GetSwitchValueASCII(switches::kUseGL) ==
+                             gfx::kGLImplementationEGLName;
+    return scoped_ptr<SandboxBPFBasePolicy>(
+        new GpuProcessPolicy(allow_mincore));
   }
 }
 
