a794d05f6f41801e87e3861be746d7c241b504f8
yurys@chromium.org <yurys@chromium.org>
2014-10-21 08:29:05 +0000

    DevTools: [timeline] make frame mode & flame chart the default timeline UI mode
    BUG=424505
    
    Review URL: https://codereview.chromium.org/658343002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184052 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/timeline/TimelinePanel.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TimelinePanel.js
@@ -83,8 +83,8 @@ WebInspector.TimelinePanel = function()
     /** @type {!Array.<!WebInspector.TimelineModeView>} */
     this._currentViews = [];
 
-    this._overviewModeSetting = WebInspector.settings.createSetting("timelineOverviewMode", WebInspector.TimelinePanel.OverviewMode.Events);
-    this._flameChartEnabledSetting = WebInspector.settings.createSetting("timelineFlameChartEnabled", false);
+    this._overviewModeSetting = WebInspector.settings.createSetting("timelineOverviewMode", Runtime.experiments.isEnabled("timelineOnTraceEvents") ? WebInspector.TimelinePanel.OverviewMode.Frames : WebInspector.TimelinePanel.OverviewMode.Events);
+    this._flameChartEnabledSetting = WebInspector.settings.createSetting("timelineFlameChartEnabled", true);
     this._createStatusBarItems();
 
     var topPaneElement = this.element.createChild("div", "hbox");
@@ -340,11 +340,11 @@ WebInspector.TimelinePanel.prototype = {
         panelStatusBarElement.appendChild(framesToggleButton.element);
 
         if (Runtime.experiments.isEnabled("timelineOnTraceEvents")) {
-            var flameChartToggleButton = new WebInspector.StatusBarButton(WebInspector.UIString("Flame chart view. (Use WASD or time selection to navigate)"), "timeline-flame-chart-status-bar-item");
-            flameChartToggleButton.toggled = this._flameChartEnabledSetting.get();
-            flameChartToggleButton.addEventListener("click", this._flameChartEnabledChanged.bind(this, flameChartToggleButton));
-            this._statusBarButtons.push(flameChartToggleButton);
-            panelStatusBarElement.appendChild(flameChartToggleButton.element);
+            this._flameChartToggleButton = new WebInspector.StatusBarButton(WebInspector.UIString("Flame chart view. (Use WASD or time selection to navigate)"), "timeline-flame-chart-status-bar-item");
+            this._flameChartToggleButton.toggled = this._flameChartEnabledSetting.get();
+            this._flameChartToggleButton.addEventListener("click", this._flameChartEnabledChanged.bind(this, this._flameChartToggleButton));
+            this._statusBarButtons.push(this._flameChartToggleButton);
+            panelStatusBarElement.appendChild(this._flameChartToggleButton.element);
         }
 
         this._captureCausesSetting = WebInspector.settings.createSetting("timelineCaptureCauses", true);
@@ -574,15 +574,12 @@ WebInspector.TimelinePanel.prototype = {
         this._onModeChanged();
     },
 
-    /**
-     * @param {!WebInspector.StatusBarButton} button
-     */
-    _flameChartEnabledChanged: function(button)
+    _flameChartEnabledChanged: function()
     {
         var oldValue = this._flameChartEnabledSetting.get();
         var newValue = !oldValue;
         this._flameChartEnabledSetting.set(newValue);
-        button.toggled = newValue;
+        this._flameChartToggleButton.toggled = newValue;
         this._onModeChanged();
     },
 
