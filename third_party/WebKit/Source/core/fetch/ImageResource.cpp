2f8e4836bb4e132b233a05c875fd103ab6bc3a3f
horo@chromium.org <horo@chromium.org>
2014-10-21 09:48:26 +0000

    [ServiceWorker] Don't taint the CORS response image from the ServiceWorker
    This change depends on https://codereview.chromium.org/653373002/ and https://codereview.chromium.org/659573006/
    
    
    BUG=424473
    TEST=http/tests/serviceworker/resources/fetch-canvas-tainting-iframe.html
    
    Review URL: https://codereview.chromium.org/658703002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184063 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/fetch/ImageResource.cpp
+++ third_party/WebKit/Source/core/fetch/ImageResource.cpp
@@ -488,6 +488,8 @@ bool ImageResource::currentFrameKnownToBeOpaque(const RenderObject* renderer)
 
 bool ImageResource::isAccessAllowed(SecurityOrigin* securityOrigin)
 {
+    if (response().wasFetchedViaServiceWorker())
+        return response().serviceWorkerResponseType() != WebServiceWorkerResponseTypeOpaque;
     if (!image()->currentFrameHasSingleSecurityOrigin())
         return false;
     if (passesAccessControlCheck(securityOrigin))
