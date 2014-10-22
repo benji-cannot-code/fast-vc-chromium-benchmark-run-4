702f4a0dee6bf6c296afd564462fdbe80a28f790
thestig <thestig@chromium.org>
2014-10-22 14:55:47 -0700

    Cleanup: Remove unused SupervisedUserSiteList::CopyThumbnailUrl.
    BUG=424746
    
    Review URL: https://codereview.chromium.org/672693002
    
    Cr-Commit-Position: refs/heads/master@{#300770}

--- chrome/browser/supervised_user/supervised_user_site_list.h
+++ chrome/browser/supervised_user/supervised_user_site_list.h
@@ -53,8 +53,7 @@ class SupervisedUserSiteList {
     std::vector<std::string> hostname_hashes;
   };
 
-  SupervisedUserSiteList(const std::string& extension_id,
-                         const base::FilePath& path);
+  explicit SupervisedUserSiteList(const base::FilePath& path);
   ~SupervisedUserSiteList();
 
   // Creates a copy of the site list.
@@ -71,10 +70,7 @@ class SupervisedUserSiteList {
 
  private:
   bool LazyLoad();
-  void CopyThumbnailUrl(const base::DictionaryValue* source,
-                        base::DictionaryValue* dest);
 
-  std::string extension_id_;
   base::FilePath path_;
   scoped_ptr<base::DictionaryValue> categories_;
   scoped_ptr<base::ListValue> sites_;
