e6290bed6f696eab2e47a4c7132b80d0579b2311
sgurun <sgurun@chromium.org>
2014-10-21 18:56:22 -0700

    Move PostMessageToFrame to WebContentsAndroid
    Move PostMessageToFrame method from ContentViewCore to WebContentsAndroid
    which is a more suitable location.
    
    BUG=393291
    
    Review URL: https://codereview.chromium.org/649933006
    
    Cr-Commit-Position: refs/heads/master@{#300615}

--- content/public/android/java/src/org/chromium/content/browser/webcontents/WebContentsImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/webcontents/WebContentsImpl.java
@@ -228,6 +228,13 @@ import org.chromium.content_public.browser.WebContents;
         callback.handleJavaScriptResult(jsonResult);
     }
 
+    @Override
+    public void postMessageToFrame(String frameName, String message,
+            String sourceOrigin, String targetOrigin) {
+        nativePostMessageToFrame(mNativeWebContentsAndroid, frameName, message, sourceOrigin,
+                targetOrigin);
+    }
+
     private native String nativeGetTitle(long nativeWebContentsAndroid);
     private native String nativeGetVisibleURL(long nativeWebContentsAndroid);
     private native void nativeStop(long nativeWebContentsAndroid);
@@ -259,4 +266,6 @@ import org.chromium.content_public.browser.WebContents;
             String cssSelector);
     private native void nativeEvaluateJavaScript(long nativeWebContentsAndroid,
             String script, JavaScriptCallback callback);
+    private native void nativePostMessageToFrame(long nativeWebContentsAndroid, String frameId,
+            String message, String sourceOrigin, String targetOrigin);
 }
