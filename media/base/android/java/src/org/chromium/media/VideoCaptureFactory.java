ae09f37d03fa2d41231d0314c75c27e5e20ccf65
qinmin <qinmin@chromium.org>
2014-10-21 17:11:48 -0700

    Fix a crash when cameraInfo is NULL
    It is possible that cameraInfo returns NULL.
    However, the NULL case is not well handled and it will cause crashes.
    This change also removed the unncessary JNI call to convert an int to string
    
    BUG=424775
    
    Review URL: https://codereview.chromium.org/667433003
    
    Cr-Commit-Position: refs/heads/master@{#300578}

--- media/base/android/java/src/org/chromium/media/VideoCaptureFactory.java
+++ media/base/android/java/src/org/chromium/media/VideoCaptureFactory.java
@@ -109,11 +109,6 @@ class VideoCaptureFactory {
     }
 
     @CalledByNative
-    static String getDeviceId(int id) {
-        return Integer.toString(id);
-    }
-
-    @CalledByNative
     static VideoCapture.CaptureFormat[] getDeviceSupportedFormats(int id) {
         return ChromiumCameraInfo.isSpecialCamera(id) ?
                 VideoCaptureTango.getDeviceSupportedFormats(
