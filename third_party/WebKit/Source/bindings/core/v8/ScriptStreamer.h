2fc1660466c5bbab4cbb3d52236dc1725d7dedf2
marja@chromium.org <marja@chromium.org>
2014-10-22 17:59:49 +0000

    Script streaming: fix small script recognition heuristic.
    It can happen that the first data chunk for a large script is small. To handle
    this, we don't give up with streaming immediately when we receive a small data
    chunk, but simply delay the decision to start the streaming.
    
    See crbug.com/425475 for discussion about when this can happen.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/655263003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184201 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/core/v8/ScriptStreamer.h
+++ third_party/WebKit/Source/bindings/core/v8/ScriptStreamer.h
@@ -85,9 +85,9 @@ public:
     // has processed it.
     void streamingComplete();
 
-    static void removeSmallScriptThresholdForTesting()
+    static void setSmallScriptThresholdForTesting(size_t threshold)
     {
-        kSmallScriptThreshold = 0;
+        kSmallScriptThreshold = threshold;
     }
 
     static size_t smallScriptThreshold() { return kSmallScriptThreshold; }
@@ -120,7 +120,8 @@ private:
     WTF::OwnPtr<v8::ScriptCompiler::ScriptStreamingTask> m_task;
     bool m_loadingFinished; // Whether loading from the network is done.
     bool m_parsingFinished; // Whether the V8 side processing is done.
-    bool m_firstDataChunkReceived;
+    // Whether we have received enough data to start the streaming.
+    bool m_haveEnoughDataForStreaming;
 
     // Whether the script source code should be retrieved from the Resource
     // instead of the ScriptStreamer.
