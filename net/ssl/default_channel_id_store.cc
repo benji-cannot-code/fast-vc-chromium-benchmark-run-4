8c57e824e2b1c07a25dba558c64c344cc2a1820a
vadimt <vadimt@chromium.org>
2014-10-21 18:57:26 -0700

    Adding instrumentation to locate the source of jankiness.
    Mechanical change that adds instrumentation required to locate the source of jankiness (i.e. a long-running fragment of code executed as a part of the task that causes jank) in the code. See the bug for details on what kind of jank we are after.
    A number of similar CLs were landed, and none of them caused issues. They've helped to find and fix janky code. The code of the instrumentation is highly optimized and is not expected to affect performance. The code simply creates a diagnostic task which is identical to ones created by PostTask or IPC message handlers.
    Landing as TBR since this is a mechanical, safe and temporary change.
    
    BUG=425814
    TBR=rdsmith@chromium.org
    
    Review URL: https://codereview.chromium.org/673513002
    
    Cr-Commit-Position: refs/heads/master@{#300616}

--- net/ssl/default_channel_id_store.cc
+++ net/ssl/default_channel_id_store.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/message_loop/message_loop.h"
 #include "base/metrics/histogram.h"
+#include "base/profiler/scoped_profile.h"
 #include "net/base/net_errors.h"
 
 namespace net {
@@ -62,6 +63,11 @@ DefaultChannelIDStore::GetChannelIDTask::~GetChannelIDTask() {
 
 void DefaultChannelIDStore::GetChannelIDTask::Run(
     DefaultChannelIDStore* store) {
+  // TODO(vadimt): Remove ScopedProfile below once crbug.com/425814 is fixed.
+  tracked_objects::ScopedProfile tracking_profile(
+      FROM_HERE_WITH_EXPLICIT_FUNCTION(
+          "425814 DefaultChannelIDStore::GetChannelIDTask::Run"));
+
   base::Time expiration_time;
   std::string private_key_result;
   std::string cert_result;
@@ -113,6 +119,11 @@ DefaultChannelIDStore::SetChannelIDTask::~SetChannelIDTask() {
 
 void DefaultChannelIDStore::SetChannelIDTask::Run(
     DefaultChannelIDStore* store) {
+  // TODO(vadimt): Remove ScopedProfile below once crbug.com/425814 is fixed.
+  tracked_objects::ScopedProfile tracking_profile(
+      FROM_HERE_WITH_EXPLICIT_FUNCTION(
+          "425814 DefaultChannelIDStore::SetChannelIDTask::Run"));
+
   store->SyncSetChannelID(server_identifier_, creation_time_,
                           expiration_time_, private_key_, cert_);
 }
@@ -146,6 +157,11 @@ DefaultChannelIDStore::DeleteChannelIDTask::
 
 void DefaultChannelIDStore::DeleteChannelIDTask::Run(
     DefaultChannelIDStore* store) {
+  // TODO(vadimt): Remove ScopedProfile below once crbug.com/425814 is fixed.
+  tracked_objects::ScopedProfile tracking_profile(
+      FROM_HERE_WITH_EXPLICIT_FUNCTION(
+          "425814 DefaultChannelIDStore::DeleteChannelIDTask::Run"));
+
   store->SyncDeleteChannelID(server_identifier_);
 
   InvokeCallback(callback_);
@@ -184,6 +200,11 @@ DefaultChannelIDStore::DeleteAllCreatedBetweenTask::
 
 void DefaultChannelIDStore::DeleteAllCreatedBetweenTask::Run(
     DefaultChannelIDStore* store) {
+  // TODO(vadimt): Remove ScopedProfile below once crbug.com/425814 is fixed.
+  tracked_objects::ScopedProfile tracking_profile(
+      FROM_HERE_WITH_EXPLICIT_FUNCTION(
+          "425814 DefaultChannelIDStore::DeleteAllCreatedBetweenTask::Run"));
+
   store->SyncDeleteAllCreatedBetween(delete_begin_, delete_end_);
 
   InvokeCallback(callback_);
@@ -214,6 +235,11 @@ DefaultChannelIDStore::GetAllChannelIDsTask::
 
 void DefaultChannelIDStore::GetAllChannelIDsTask::Run(
     DefaultChannelIDStore* store) {
+  // TODO(vadimt): Remove ScopedProfile below once crbug.com/425814 is fixed.
+  tracked_objects::ScopedProfile tracking_profile(
+      FROM_HERE_WITH_EXPLICIT_FUNCTION(
+          "425814 DefaultChannelIDStore::GetAllChannelIDsTask::Run"));
+
   ChannelIDList cert_list;
   store->SyncGetAllChannelIDs(&cert_list);
 
