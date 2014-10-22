9a3a1087dd56aa8994c36df79e480c8667fe4b59
lushnikov@chromium.org <lushnikov@chromium.org>
2014-10-22 06:54:32 +0000

    DevTools: add "copy value" to watch expression context menu
    BUG=312963
    R=vsevik
    
    Review URL: https://codereview.chromium.org/665273002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184147 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sources/WatchExpressionsSidebarPane.js
+++ third_party/WebKit/Source/devtools/front_end/sources/WatchExpressionsSidebarPane.js
@@ -436,6 +436,8 @@ WebInspector.WatchExpressionTreeElement.prototype = {
         }
         if (this.treeOutline.section.watchExpressions.length > 1)
             contextMenu.appendItem(WebInspector.UIString(WebInspector.useLowerCaseMenuTitles() ? "Delete all watch expressions" : "Delete All Watch Expressions"), this._deleteAllButtonClicked.bind(this));
+        if (!this.isEditing() && (this.property.value.type === "number" || this.property.value.type === "string"))
+            contextMenu.appendItem(WebInspector.UIString(WebInspector.useLowerCaseMenuTitles() ? "Copy value" : "Copy Value"), this._copyValueButtonClicked.bind(this));
     },
 
     _contextMenu: function(event)
@@ -453,6 +455,11 @@ WebInspector.WatchExpressionTreeElement.prototype = {
     _deleteButtonClicked: function()
     {
         this.treeOutline.section.updateExpression(this, null);
+    },
+
+    _copyValueButtonClicked: function()
+    {
+        InspectorFrontendHost.copyText(this.valueElement.textContent);
     },
 
     /**
