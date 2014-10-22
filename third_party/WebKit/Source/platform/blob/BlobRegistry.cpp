a97f051abe67e9ef09eb475df720e439c92eefc6
tyoshino@chromium.org <tyoshino@chromium.org>
2014-10-22 04:25:14 +0000

    [WebBlobRegistry] Use char* than WebThreadSafeData
    We don't need to wrap the data with WebThreadSafeData here. Just use a
    pair of const char* and size_t.
    
    This is Blink side CL.
    
    Chromium side CL: https://codereview.chromium.org/610333002/
    
    R=yhirano,tkent
    BUG=417093
    
    Review URL: https://codereview.chromium.org/611923002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184141 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/blob/BlobRegistry.cpp
+++ third_party/WebKit/Source/platform/blob/BlobRegistry.cpp
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "public/platform/WebBlobData.h"
 #include "public/platform/WebBlobRegistry.h"
 #include "public/platform/WebString.h"
-#include "public/platform/WebThreadSafeData.h"
 #include "wtf/Assertions.h"
 #include "wtf/HashMap.h"
 #include "wtf/MainThread.h"
@@ -198,18 +197,16 @@ static void addDataToStreamTask(void* context)
 {
     OwnPtr<BlobRegistryContext> blobRegistryContext = adoptPtr(static_cast<BlobRegistryContext*>(context));
     if (WebBlobRegistry* registry = blobRegistry()) {
-        WebThreadSafeData webThreadSafeData(blobRegistryContext->streamData);
-        registry->addDataToStream(blobRegistryContext->url, webThreadSafeData);
+        RefPtr<RawData> data(blobRegistryContext->streamData);
+        registry->addDataToStream(blobRegistryContext->url, data->data(), data->length());
     }
 }
 
 void BlobRegistry::addDataToStream(const KURL& url, PassRefPtr<RawData> streamData)
 {
     if (isMainThread()) {
-        if (WebBlobRegistry* registry = blobRegistry()) {
-            WebThreadSafeData webThreadSafeData(streamData);
-            registry->addDataToStream(url, webThreadSafeData);
-        }
+        if (WebBlobRegistry* registry = blobRegistry())
+            registry->addDataToStream(url, streamData->data(), streamData->length());
     } else {
         OwnPtr<BlobRegistryContext> context = adoptPtr(new BlobRegistryContext(url, streamData));
         callOnMainThread(&addDataToStreamTask, context.leakPtr());
