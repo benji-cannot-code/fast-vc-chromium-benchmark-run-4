f40615b51527d0a8d737d8b554d4b578b0fefc50
phoglund <phoglund@chromium.org>
2014-10-22 01:54:13 -0700

    Relanding WebRTC real webcam browsertest for Android.
    Over the original patch that got reverted, this takes into account that
    the android device can be in a different orientation. If the nexus
    device happens to be in portrait mode in the lab, it will report a
    480x640 resolution rather than 640x480, so this reland accounts for
    that.
    
    The main purpose of this test is to test the capture path on Android,
    i.e. that we can acquire the device's built-in webcam. If we happen to
    run on a device without a camera, we'll quietly succeed the test.
    
    BUG=323921
    R=tommi
    
    Review URL: https://codereview.chromium.org/663793003
    
    Cr-Commit-Position: refs/heads/master@{#300655}

--- content/test/webrtc_content_browsertest_base.cc
+++ content/test/webrtc_content_browsertest_base.cc
@@ -25,12 +25,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 void WebRtcContentBrowserTest::SetUpCommandLine(CommandLine* command_line) {
-  // We need fake devices in this test since we want to run on naked VMs. We
-  // assume these switches are set by default in content_browsertests.
-  ASSERT_TRUE(CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kUseFakeDeviceForMediaStream));
+  // Assume this is set by the content test launcher.
   ASSERT_TRUE(CommandLine::ForCurrentProcess()->HasSwitch(
       switches::kUseFakeUIForMediaStream));
+  ASSERT_TRUE(CommandLine::ForCurrentProcess()->HasSwitch(
+      switches::kUseFakeDeviceForMediaStream));
 
   // Always include loopback interface in network list, in case the test device
   // doesn't have other interfaces available.
@@ -39,6 +38,7 @@ void WebRtcContentBrowserTest::SetUpCommandLine(CommandLine* command_line) {
 }
 
 void WebRtcContentBrowserTest::SetUp() {
+  // We need pixel output when we dig pixels out of video tags for verification.
   EnablePixelOutput();
 #if defined(OS_CHROMEOS)
     chromeos::CrasAudioHandler::InitializeForTesting();
