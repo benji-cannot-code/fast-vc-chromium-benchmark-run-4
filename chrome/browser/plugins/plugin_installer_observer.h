9cb677666dc3907dbd50ea323f88877c8e7450b9
dcheng <dcheng@chromium.org>
2014-10-22 11:47:42 -0700

    Standardize usage of virtual/override/final in chrome/browser/plugins
    The Google C++ style guide states:
    
      Explicitly annotate overrides of virtual functions or virtual
      destructors with an override or (less frequently) final specifier.
      Older (pre-C++11) code will use the virtual keyword as an inferior
      alternative annotation. For clarity, use exactly one of override,
      final, or virtual when declaring an override.
    
    To better conform to these guidelines, the following constructs have
    been rewritten:
    
    - if a base class has a virtual destructor, then:
        virtual ~Foo();                   ->  ~Foo() override;
    - virtual void Foo() override;        ->  void Foo() override;
    - virtual void Foo() override final;  ->  void Foo() final;
    
    This patch was automatically generated. The clang plugin can generate
    fixit hints, which are suggested edits when it is 100% sure it knows how
    to fix a problem. The hints from the clang plugin were applied to the
    source tree using the tool in https://codereview.chromium.org/598073004.
    
    BUG=417463
    R=bauerb@chromium.org
    
    Review URL: https://codereview.chromium.org/649683010
    
    Cr-Commit-Position: refs/heads/master@{#300722}

--- chrome/browser/plugins/plugin_installer_observer.h
+++ chrome/browser/plugins/plugin_installer_observer.h
@@ -36,7 +36,7 @@ class PluginInstallerObserver {
 class WeakPluginInstallerObserver : public PluginInstallerObserver {
  public:
   explicit WeakPluginInstallerObserver(PluginInstaller* installer);
-  virtual ~WeakPluginInstallerObserver();
+  ~WeakPluginInstallerObserver() override;
 
  private:
   friend class PluginInstaller;
