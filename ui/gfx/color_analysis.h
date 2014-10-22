4ed36e5978cf832d0a96fdcd56f0f6c73578cec3
tfarina <tfarina@chromium.org>
2014-10-21 21:16:33 -0700

    Cleanup: Update paths to matrix3_f.h includes.
    BUG=395370
    TEST=None
    TBR=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/636283007
    
    Cr-Commit-Position: refs/heads/master@{#300630}

--- ui/gfx/color_analysis.h
+++ ui/gfx/color_analysis.h
@@ -11,8 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/memory/ref_counted_memory.h"
 #include "third_party/skia/include/core/SkColor.h"
+#include "ui/gfx/geometry/matrix3_f.h"
 #include "ui/gfx/gfx_export.h"
-#include "ui/gfx/matrix3_f.h"
 
 class SkBitmap;
 
