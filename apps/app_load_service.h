bdbf58f6e6ea418db554276d8b81551c0766c34e
dcheng <dcheng@chromium.org>
2014-10-21 18:46:08 -0700

    Standardize usage of virtual/override/final in apps/
    The Google C++ style guide states:
    
      Explicitly annotate overrides of virtual functions or virtual
      destructors with an override or (less frequently) final specifier.
      Older (pre-C++11) code will use the virtual keyword as an inferior
      alternative annotation. For clarity, use exactly one of override,
      final, or virtual when declaring an override.
    
    To better conform to these guidelines, the following constructs have
    been rewritten:
    
    - if a base class has a virtual destructor, then:
        virtual ~Foo();                   ->  ~Foo() override;
    - virtual void Foo() override;        ->  void Foo() override;
    - virtual void Foo() override final;  ->  void Foo() final;
    
    This patch was automatically generated. The clang plugin can generate
    fixit hints, which are suggested edits when it is 100% sure it knows how
    to fix a problem. The hints from the clang plugin were applied to the
    source tree using the tool in https://codereview.chromium.org/598073004.
    
    BUG=417463
    R=scheib@chromium.org
    
    Review URL: https://codereview.chromium.org/666213002
    
    Cr-Commit-Position: refs/heads/master@{#300609}

--- apps/app_load_service.h
+++ apps/app_load_service.h
@@ -45,7 +45,7 @@ class AppLoadService : public KeyedService,
   };
 
   explicit AppLoadService(Profile* profile);
-  virtual ~AppLoadService();
+  ~AppLoadService() override;
 
   // Reload the application with the given id and then send it the OnRestarted
   // event.
@@ -66,12 +66,12 @@ class AppLoadService : public KeyedService,
 
  private:
   // content::NotificationObserver.
-  virtual void Observe(int type,
-                       const content::NotificationSource& source,
-                       const content::NotificationDetails& details) override;
+  void Observe(int type,
+               const content::NotificationSource& source,
+               const content::NotificationDetails& details) override;
 
   // extensions::ExtensionRegistryObserver.
-  virtual void OnExtensionUnloaded(
+  void OnExtensionUnloaded(
       content::BrowserContext* browser_context,
       const extensions::Extension* extension,
       extensions::UnloadedExtensionInfo::Reason reason) override;
--- apps/app_restore_service_factory.h
+++ apps/app_restore_service_factory.h
@@ -28,12 +28,12 @@ class AppRestoreServiceFactory : public BrowserContextKeyedServiceFactory {
   friend struct DefaultSingletonTraits<AppRestoreServiceFactory>;
 
   AppRestoreServiceFactory();
-  virtual ~AppRestoreServiceFactory();
+  ~AppRestoreServiceFactory() override;
 
   // BrowserContextKeyedServiceFactory:
-  virtual KeyedService* BuildServiceInstanceFor(
+  KeyedService* BuildServiceInstanceFor(
       content::BrowserContext* profile) const override;
-  virtual bool ServiceIsCreatedWithBrowserContext() const override;
+  bool ServiceIsCreatedWithBrowserContext() const override;
 };
 
 }  // namespace apps
