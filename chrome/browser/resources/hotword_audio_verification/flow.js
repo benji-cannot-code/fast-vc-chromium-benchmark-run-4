37cc9590e6ec1d140d8ae842d30be62122ecb973
kcarattini <kcarattini@chromium.org>
2014-10-21 18:59:54 -0700

    Adds the page for hotword-only to the opt-in flow.
    Updates the UI button text to reflect the latest mocks.
    
    BUG=390086
    
    Review URL: https://codereview.chromium.org/669603007
    
    Cr-Commit-Position: refs/heads/master@{#300618}

--- chrome/browser/resources/hotword_audio_verification/flow.js
+++ chrome/browser/resources/hotword_audio_verification/flow.js
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     // TODO(kcarattini): Remove the first flow, since we will not be
     // managing the Audio History Setting in Chrome anymore.
     [AUDIO_HISTORY_START],
-    [HOTWORD_ONLY_START, SPEECH_TRAINING, FINISHED],
+    [HOTWORD_ONLY_START, FINISHED],
     [HOTWORD_AUDIO_HISTORY, SPEECH_TRAINING, FINISHED],
     [SPEECH_TRAINING, FINISHED]
   ];
