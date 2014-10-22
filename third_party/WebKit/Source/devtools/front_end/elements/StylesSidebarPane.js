38adcda5e18e83759f3e1fa0f69e0e6f304a1ee1
lushnikov@chromium.org <lushnikov@chromium.org>
2014-10-22 10:52:00 +0000

    DevTools: [SSP] do not rebuild styles when sidebar pane is not showing
    BUG=425523
    R=pfeldman, vsevik
    
    Review URL: https://codereview.chromium.org/666163003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184162 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/elements/StylesSidebarPane.js
+++ third_party/WebKit/Source/devtools/front_end/elements/StylesSidebarPane.js
@@ -548,7 +548,7 @@ WebInspector.StylesSidebarPane.prototype = {
 
     _styleSheetOrMediaQueryResultChanged: function()
     {
-        if (this._userOperation || this._isEditingStyle)
+        if (this._userOperation || this._isEditingStyle || !this.isShowing())
             return;
 
         this._rebuildUpdate();
