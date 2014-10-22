266c670f55028dfee9af89702e032adbf1038a98
mseaborn <mseaborn@chromium.org>
2014-10-21 18:08:46 -0700

    NaCl: Ensure that sysconf() is declared: add missing #include of <unistd.h>
    This is necessary to roll in a new NaCl revision after I committed a
    NaCl-side change which cleans up the service_runtime/include/ headers.
    
    BUG= https://code.google.com/p/nativeclient/issues/detail?id=3909
    TEST= build
    
    Review URL: https://codereview.chromium.org/651643007
    
    Cr-Commit-Position: refs/heads/master@{#300593}

--- ppapi/nacl_irt/irt_ppapi.cc
+++ ppapi/nacl_irt/irt_ppapi.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <unistd.h>
+
 #include "native_client/src/public/irt_core.h"
 #include "native_client/src/trusted/service_runtime/include/sys/unistd.h"
 #include "native_client/src/untrusted/irt/irt.h"
