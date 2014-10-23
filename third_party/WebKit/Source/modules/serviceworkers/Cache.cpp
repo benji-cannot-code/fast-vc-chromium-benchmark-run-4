0f374098efd1dbb86024f4594d9e1fda86b23f66
jsbell@chromium.org <jsbell@chromium.org>
2014-10-23 23:00:07 +0000

    Service Workers: Cache.put() should resolve to undefined
    Spec was updated with the determination that add(), addAll() and put()
    should resolve to `undefined`, not the response(s). Only put() is
    currently implemented; update the resolution type.
    
    BUG=425469
    R=jkarlin@chromium.org
    
    Review URL: https://codereview.chromium.org/658743003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184307 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/serviceworkers/Cache.cpp
+++ third_party/WebKit/Source/modules/serviceworkers/Cache.cpp
@@ -79,16 +79,16 @@ protected:
 };
 
 // FIXME: Consider using CallbackPromiseAdapter.
-class CacheWithOneResponseCallbacks : public CacheWithResponsesCallbacks {
-    WTF_MAKE_NONCOPYABLE(CacheWithOneResponseCallbacks);
+class CacheAddOrPutCallbacks : public CacheWithResponsesCallbacks {
+    WTF_MAKE_NONCOPYABLE(CacheAddOrPutCallbacks);
 public:
-    CacheWithOneResponseCallbacks(PassRefPtr<ScriptPromiseResolver> resolver)
+    CacheAddOrPutCallbacks(PassRefPtr<ScriptPromiseResolver> resolver)
         : CacheWithResponsesCallbacks(resolver) { }
 
     virtual void onSuccess(WebVector<WebServiceWorkerResponse>* webResponses) override
     {
-        ASSERT(webResponses->size() == 1);
-        m_resolver->resolve(Response::create(m_resolver->scriptState()->executionContext(), (*webResponses)[0]));
+        // FIXME: Since response is ignored, consider simplifying public API.
+        m_resolver->resolve();
         m_resolver.clear();
     }
 };
@@ -102,6 +102,7 @@ public:
 
     virtual void onSuccess(WebVector<WebServiceWorkerResponse>* webResponses) override
     {
+        // FIXME: Since response is ignored, consider simplifying public API.
         m_resolver->resolve(true);
         m_resolver.clear();
     }
@@ -379,7 +380,7 @@ ScriptPromise Cache::putImpl(ScriptState* scriptState, Request* request, Respons
 
     RefPtr<ScriptPromiseResolver> resolver = ScriptPromiseResolver::create(scriptState);
     const ScriptPromise promise = resolver->promise();
-    m_webCache->dispatchBatch(new CacheWithOneResponseCallbacks(resolver), batchOperations);
+    m_webCache->dispatchBatch(new CacheAddOrPutCallbacks(resolver), batchOperations);
     return promise;
 }
 
