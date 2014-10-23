9f339795dfd8d454760ed4e92fe246232724a12c
xiyuan <xiyuan@chromium.org>
2014-10-23 11:33:43 -0700

    kiosk: Defer usb update UI until a manifest is found.
    - Don't show usb update ui if there is no manifest file;
    - Still show invalid manifest error;
    - Defer in-progress message until a valid manifest is found;
    
    BUG=426278
    
    Review URL: https://codereview.chromium.org/676803003
    
    Cr-Commit-Position: refs/heads/master@{#300920}

--- chrome/browser/chromeos/app_mode/kiosk_external_updater.cc
+++ chrome/browser/chromeos/app_mode/kiosk_external_updater.cc
@@ -141,10 +141,6 @@ void KioskExternalUpdater::OnMountEvent(
       return;
     }
 
-    NotifyKioskUpdateProgress(
-        ui::ResourceBundle::GetSharedInstance().GetLocalizedString(
-            IDS_KIOSK_EXTERNAL_UPDATE_IN_PROGRESS));
-
     base::DictionaryValue* parsed_manifest = new base::DictionaryValue();
     ExternalUpdateErrorCode* parsing_error = new ExternalUpdateErrorCode;
     backend_task_runner_->PostTaskAndReply(
@@ -240,9 +236,6 @@ void KioskExternalUpdater::ProcessParsedManifest(
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 
   if (*parsing_error == ERROR_NO_MANIFEST) {
-    NotifyKioskUpdateProgress(
-        ui::ResourceBundle::GetSharedInstance().GetLocalizedString(
-            IDS_KIOSK_EXTERNAL_UPDATE_NO_MANIFEST));
     KioskAppManager::Get()->OnKioskAppExternalUpdateComplete(false);
     return;
   } else if (*parsing_error == ERROR_INVALID_MANIFEST) {
@@ -252,6 +245,10 @@ void KioskExternalUpdater::ProcessParsedManifest(
     KioskAppManager::Get()->OnKioskAppExternalUpdateComplete(false);
     return;
   }
+
+  NotifyKioskUpdateProgress(
+      ui::ResourceBundle::GetSharedInstance().GetLocalizedString(
+          IDS_KIOSK_EXTERNAL_UPDATE_IN_PROGRESS));
 
   external_update_path_ = external_update_dir;
   for (base::DictionaryValue::Iterator it(*parsed_manifest); !it.IsAtEnd();
