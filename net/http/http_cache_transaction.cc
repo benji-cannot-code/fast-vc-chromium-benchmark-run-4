db34a4db14c160600b55f2ec6e8639ff7c54eeed
rvargas <rvargas@chromium.org>
2014-10-23 14:24:56 -0700

    Http cache: Fix DCHECK with POSTs and a disabled cache.
    Make sure that we don't update the cache after a POST if the cache is
    disabled.
    
    BUG=426059
    TEST=net_unittests
    R=derekjchow@chromium.org, davidben@chromium.org
    
    Review URL: https://codereview.chromium.org/672773002
    
    Cr-Commit-Position: refs/heads/master@{#300950}

--- net/http/http_cache_transaction.cc
+++ net/http/http_cache_transaction.cc
@@ -1209,6 +1209,7 @@ int HttpCache::Transaction::DoSuccessfulSendRequest() {
     UpdateTransactionPattern(PATTERN_ENTRY_NOT_CACHED);
   }
 
+  // Invalidate any cached GET with a successful PUT or DELETE.
   if (mode_ == WRITE &&
       (request_->method == "PUT" || request_->method == "DELETE")) {
     if (NonErrorResponse(new_response->headers->response_code())) {
@@ -1220,7 +1221,9 @@ int HttpCache::Transaction::DoSuccessfulSendRequest() {
     mode_ = NONE;
   }
 
-  if (request_->method == "POST" &&
+  // Invalidate any cached GET with a successful POST.
+  if (!(effective_load_flags_ & LOAD_DISABLE_CACHE) &&
+      request_->method == "POST" &&
       NonErrorResponse(new_response->headers->response_code())) {
     cache_->DoomMainEntryForUrl(request_->url);
   }
