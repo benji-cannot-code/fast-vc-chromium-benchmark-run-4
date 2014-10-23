0f2a0e1138b755b8db52d117109006c0e24a43a3
sigbjornf@opera.com <sigbjornf@opera.com>
2014-10-23 17:18:37 +0000

    Only dispose proxy-less WebServiceWorkerRegistration objects.
    If a service worker registration is passed along to a resolver that's
    in a detached/stopping state, the WebServiceWorkerRegistration is
    simply disposed of.
    
    Have that dispose step only delete the object if the registration object
    isn't attached to a proxy already. If it is, it is responsible for
    releasing the resource.
    
    R=haraken
    BUG=426321
    
    Review URL: https://codereview.chromium.org/669423002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184288 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/serviceworkers/ServiceWorkerRegistration.cpp
+++ third_party/WebKit/Source/modules/serviceworkers/ServiceWorkerRegistration.cpp
@@ -91,7 +91,8 @@ ServiceWorkerRegistration* ServiceWorkerRegistration::take(ScriptPromiseResolver
 
 void ServiceWorkerRegistration::dispose(WebType* registration)
 {
-    delete registration;
+    if (registration && !registration->proxy())
+        delete registration;
 }
 
 String ServiceWorkerRegistration::scope() const
