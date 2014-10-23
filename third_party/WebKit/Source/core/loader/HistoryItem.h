7343f5abdb2c6329ecee6c0db983092e34300e22
sigbjornf@opera.com <sigbjornf@opera.com>
2014-10-23 02:10:11 +0000

    Oilpan: move HistoryItem to the heap.
    Doing so removes a FrameLoader-reachable Persistent reference to
    a DocumentState.
    
    R=
    BUG=340522
    
    Review URL: https://codereview.chromium.org/639023004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184228 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/HistoryItem.h
+++ third_party/WebKit/Source/core/loader/HistoryItem.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/SerializedScriptValue.h"
 #include "platform/geometry/FloatPoint.h"
 #include "platform/geometry/IntPoint.h"
+#include "platform/heap/Handle.h"
 #include "platform/weborigin/Referrer.h"
 #include "wtf/RefCounted.h"
 #include "wtf/text/WTFString.h"
@@ -40,15 +41,15 @@ namespace blink {
 class Document;
 class DocumentState;
 class FormData;
-class HistoryItem;
 class KURL;
 class ResourceRequest;
 
-typedef Vector<RefPtr<HistoryItem> > HistoryItemVector;
-
-class HistoryItem : public RefCounted<HistoryItem> {
+class HistoryItem final : public RefCountedWillBeGarbageCollectedFinalized<HistoryItem> {
 public:
-    static PassRefPtr<HistoryItem> create() { return adoptRef(new HistoryItem); }
+    static PassRefPtrWillBeRawPtr<HistoryItem> create()
+    {
+        return adoptRefWillBeNoop(new HistoryItem);
+    }
     ~HistoryItem();
 
     // Used when the frame this item represents was navigated to a different
@@ -103,6 +104,8 @@ public:
 
     bool isCurrentDocument(Document*) const;
 
+    void trace(Visitor*);
+
 private:
     HistoryItem();
 
@@ -114,7 +117,7 @@ private:
     IntPoint m_scrollPoint;
     float m_pageScaleFactor;
     Vector<String> m_documentStateVector;
-    RefPtrWillBePersistent<DocumentState> m_documentState;
+    RefPtrWillBeMember<DocumentState> m_documentState;
 
     // If two HistoryItems have the same item sequence number, then they are
     // clones of one another. Traversing history from one such HistoryItem to
--- third_party/WebKit/Source/web/tests/WebFrameTest.cpp
+++ third_party/WebKit/Source/web/tests/WebFrameTest.cpp
@@ -5562,7 +5562,7 @@ TEST_F(WebFrameTest, BackToReload)
     webViewHelper.initializeAndLoad(m_baseURL + "fragment_middle_click.html", true);
     WebFrame* frame = webViewHelper.webView()->mainFrame();
     const FrameLoader& mainFrameLoader = webViewHelper.webViewImpl()->mainFrameImpl()->frame()->loader();
-    RefPtr<HistoryItem> firstItem = mainFrameLoader.currentItem();
+    RefPtrWillBePersistent<HistoryItem> firstItem = mainFrameLoader.currentItem();
     EXPECT_TRUE(firstItem);
 
     registerMockedHttpURLLoad("white-1x1.png");
@@ -5627,7 +5627,7 @@ TEST_F(WebFrameTest, LoadHistoryItemReload)
     webViewHelper.initializeAndLoad(m_baseURL + "fragment_middle_click.html", true);
     WebFrame* frame = webViewHelper.webView()->mainFrame();
     const FrameLoader& mainFrameLoader = webViewHelper.webViewImpl()->mainFrameImpl()->frame()->loader();
-    RefPtr<HistoryItem> firstItem = mainFrameLoader.currentItem();
+    RefPtrWillBePersistent<HistoryItem> firstItem = mainFrameLoader.currentItem();
     EXPECT_TRUE(firstItem);
 
     registerMockedHttpURLLoad("white-1x1.png");
@@ -5892,7 +5892,7 @@ TEST_F(WebFrameTest, SameDocumentHistoryNavigationCommitType)
     TestDidNavigateCommitTypeWebFrameClient client;
     FrameTestHelpers::WebViewHelper webViewHelper;
     WebViewImpl* webViewImpl = webViewHelper.initializeAndLoad(m_baseURL + "push_state.html", true, &client);
-    RefPtr<HistoryItem> item = toLocalFrame(webViewImpl->page()->mainFrame())->loader().currentItem();
+    RefPtrWillBePersistent<HistoryItem> item = toLocalFrame(webViewImpl->page()->mainFrame())->loader().currentItem();
     runPendingTasks();
 
     toLocalFrame(webViewImpl->page()->mainFrame())->loader().loadHistoryItem(item.get(), FrameLoadTypeBackForward, HistorySameDocumentLoad);
