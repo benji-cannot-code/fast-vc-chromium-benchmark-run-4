989e53207fd1ba5b506c35cc2773856e717bf83d
tasak@google.com <tasak@google.com>
2014-10-23 06:17:08 +0000

    bindings: removed unused includes from WorkerScriptController.cpp
    BUG=358074
    
    Review URL: https://codereview.chromium.org/662063004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184236 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/core/v8/WorkerScriptController.cpp
+++ third_party/WebKit/Source/bindings/core/v8/WorkerScriptController.cpp
@@ -44,7 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/V8SharedWorkerGlobalScope.h"
 #include "bindings/core/v8/V8WorkerGlobalScope.h"
 #include "bindings/core/v8/WrapperTypeInfo.h"
-#include "bindings/modules/v8/V8ServiceWorkerGlobalScope.h"
 #include "core/events/ErrorEvent.h"
 #include "core/frame/DOMTimer.h"
 #include "core/inspector/ScriptCallStack.h"
