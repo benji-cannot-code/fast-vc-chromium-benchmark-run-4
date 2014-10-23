7beaaad3aa7d10b4dd75b7b54bfa6f717d6ab36e
fs@opera.com <fs@opera.com>
2014-10-23 08:40:56 +0000

    Decouple content transform fiddling from SVGRenderingContext::renderSubtree
    The "content transform" is essentially a property of the call-site, and
    is hence invariant for a sequence of calls to renderSubtree.
    This means we ought to be able to save-mutate-restore (SMR) once per
    call-site, and not per call to renderSubtree. Achieve this by moving the
    SMR to a helper SubtreeContentTransformScope, and then instantiate objects
    where needed.
    This means the transform-logistics can be cleaned up a bit for some of the
    callers.
    
    BUG=420022
    
    Review URL: https://codereview.chromium.org/666403002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184245 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/svg/RenderSVGResourceClipper.cpp
+++ third_party/WebKit/Source/core/rendering/svg/RenderSVGResourceClipper.cpp
@@ -230,15 +230,16 @@ void RenderSVGResourceClipper::drawClipMaskContent(GraphicsContext* context, con
         context->concatCTM(contentTransformation);
     }
 
-    if (!m_clipContentDisplayList)
-        createDisplayList(context, contentTransformation);
+    if (!m_clipContentDisplayList) {
+        SubtreeContentTransformScope contentTransformScope(contentTransformation);
+        createDisplayList(context);
+    }
 
     ASSERT(m_clipContentDisplayList);
     context->drawDisplayList(m_clipContentDisplayList.get());
 }
 
-void RenderSVGResourceClipper::createDisplayList(GraphicsContext* context,
-    const AffineTransform& contentTransformation)
+void RenderSVGResourceClipper::createDisplayList(GraphicsContext* context)
 {
     ASSERT(context);
     ASSERT(frame());
@@ -286,7 +287,7 @@ void RenderSVGResourceClipper::createDisplayList(GraphicsContext* context,
         if (isUseElement)
             renderer = childElement->renderer();
 
-        SVGRenderingContext::renderSubtree(context, renderer, contentTransformation);
+        SVGRenderingContext::renderSubtree(context, renderer);
     }
 
     frame()->view()->setPaintBehavior(oldBehavior);
--- third_party/WebKit/Source/core/rendering/svg/SVGRenderingContext.h
+++ third_party/WebKit/Source/core/rendering/svg/SVGRenderingContext.h
@@ -29,14 +29,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/rendering/PaintInfo.h"
 #include "core/rendering/svg/RenderSVGResourceClipper.h"
 #include "platform/graphics/ImageBuffer.h"
+#include "platform/transforms/AffineTransform.h"
 
 namespace blink {
 
-class AffineTransform;
 class RenderObject;
 class FloatRect;
 class RenderSVGResourceFilter;
 class RenderSVGResourceMasker;
+
+class SubtreeContentTransformScope {
+public:
+    SubtreeContentTransformScope(const AffineTransform&);
+    ~SubtreeContentTransformScope();
+
+private:
+    AffineTransform m_savedContentTransformation;
+};
 
 // SVGRenderingContext
 class SVGRenderingContext {
@@ -74,7 +83,7 @@ public:
     void prepareToRenderSVGContent(RenderObject*, PaintInfo&);
     bool isRenderingPrepared() const { return m_renderingFlags & RenderingPrepared; }
 
-    static void renderSubtree(GraphicsContext*, RenderObject*, const AffineTransform&);
+    static void renderSubtree(GraphicsContext*, RenderObject*);
 
     static float calculateScreenFontSizeScalingFactor(const RenderObject*);
     static void calculateDeviceSpaceTransformation(const RenderObject*, AffineTransform& absoluteTransform);
