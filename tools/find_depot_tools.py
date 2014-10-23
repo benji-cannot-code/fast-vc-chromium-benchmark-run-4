ea4df3ad670ce1dc4d93440c007fdbfb336eb9c3
jamesr <jamesr@chromium.org>
2014-10-22 17:02:37 -0700

    Look for gclient.py in find_depot_tools
    Many (but not all!) of our bots have a directory early in the PATH that
    contains nothing but a script called 'gclient' and have the real
    depot_tools directory later on in the path. This looks for gclient.py,
    which is in the real depot_tools directory.
    
    Review URL: https://codereview.chromium.org/652893005
    
    Cr-Commit-Position: refs/heads/master@{#300802}

--- tools/find_depot_tools.py
+++ tools/find_depot_tools.py
@@ -13,7 +13,7 @@ import sys
 
 
 def IsRealDepotTools(path):
-  return os.path.isfile(os.path.join(path, 'gclient'))
+  return os.path.isfile(os.path.join(path, 'gclient.py'))
 
 
 def add_depot_tools_to_path():
