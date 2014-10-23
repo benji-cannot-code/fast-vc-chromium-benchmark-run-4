8f075c064cc2594c0321355947057499c8bc786b
yhirano@chromium.org <yhirano@chromium.org>
2014-10-23 17:52:24 +0000

    An API returning a Promise should not throw an exceptions [overload version]
    [1] made such APIs return rejected Promises instead of throwing exceptions,
    but overloaded functions were not covered. This CL fixes the problem.
    
    1: https://src.chromium.org/viewvc/blink?view=rev&revision=171450
    
    BUG=359586
    
    Review URL: https://codereview.chromium.org/675693002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184291 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/testing/Internals.cpp
+++ third_party/WebKit/Source/core/testing/Internals.cpp
@@ -2180,6 +2180,21 @@ ScriptPromise Internals::promiseCheckRange(ScriptState* scriptState, long arg1)
     return ScriptPromise::cast(scriptState, v8String(scriptState->isolate(), "done"));
 }
 
+ScriptPromise Internals::promiseCheckOverload(ScriptState* scriptState, Location*)
+{
+    return ScriptPromise::cast(scriptState, v8String(scriptState->isolate(), "done"));
+}
+
+ScriptPromise Internals::promiseCheckOverload(ScriptState* scriptState, Document*)
+{
+    return ScriptPromise::cast(scriptState, v8String(scriptState->isolate(), "done"));
+}
+
+ScriptPromise Internals::promiseCheckOverload(ScriptState* scriptState, Location*, long, long)
+{
+    return ScriptPromise::cast(scriptState, v8String(scriptState->isolate(), "done"));
+}
+
 void Internals::trace(Visitor* visitor)
 {
     visitor->trace(m_runtimeFlags);
