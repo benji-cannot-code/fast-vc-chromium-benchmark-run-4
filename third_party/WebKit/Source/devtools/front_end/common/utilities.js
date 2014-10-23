122618ac6f93f2cb1337be86f6fac2ce577c88eb
kozyatinskiy@google.com <kozyatinskiy@google.com>
2014-10-23 16:20:58 +0000

    [DevTools] Report error in debug mode from promise with stack
    R=vsevik@chromium.org
    
    Review URL: https://codereview.chromium.org/645513004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184282 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/common/utilities.js
+++ third_party/WebKit/Source/devtools/front_end/common/utilities.js
@@ -1750,6 +1750,6 @@ function suppressUnused(value)
  */
 self.setImmediate = function(callback)
 {
-    Promise.resolve().then(callback).catch(console.error.bind(console));
+    Promise.resolve().then(callback).done();
     return 0;
 }
--- third_party/WebKit/Source/devtools/front_end/externs.js
+++ third_party/WebKit/Source/devtools/front_end/externs.js
@@ -467,7 +467,7 @@ ErrorEvent.prototype.message;
 window.dispatchStandaloneTestRunnerMessages;
 
 /**
- * @param {function()} onCatch
+ * @param {function(*)} onCatch
  */
 Promise.prototype.catch = function(onCatch) {};
 
