649f209e57630667ce7304c46c50602a987f3479
pkotwicz <pkotwicz@chromium.org>
2014-10-24 08:53:57 -0700

    Close the select box when the parent RWHVA loses focus
    BUG=422264,414550
    TEST=RenderWidgetHostViewAuraTest.PopupClosesWhenParentLosesFocus
    
    Review URL: https://codereview.chromium.org/652793003
    
    Cr-Commit-Position: refs/heads/master@{#301108}

--- content/browser/renderer_host/render_widget_host_view_aura.h
+++ content/browser/renderer_host/render_widget_host_view_aura.h
@@ -419,6 +419,9 @@ class CONTENT_EXPORT RenderWidgetHostViewAura
 
   ui::InputMethod* GetInputMethod() const;
 
+  // Sends shutdown request.
+  void Shutdown();
+
   // Returns whether the widget needs an input grab to work properly.
   bool NeedsInputGrab();
 
