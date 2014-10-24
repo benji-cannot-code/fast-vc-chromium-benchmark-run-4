ddb2b97f59cbd437021eaffa3fc76f698636505c
kjellander <kjellander@chromium.org>
2014-10-23 23:17:34 -0700

    Revert of Specifies transparent background for the label in TileItemView. (patchset #2 id:10002 of https://codereview.chromium.org/655823005/)
    Reason for revert:
    Introduces static initializer, see https://build.chromium.org/p/chromium/builders/Linux%20x64/builds/72389
    
    Original issue's description:
    > Specifies transparent background for the label in TileItemView.
    >
    > We may not use single background when it's in Athena (it's not
    > used yet though).
    >
    > BUG=425717
    > R=xiyuan@chromium.org
    > TEST=manually
    >
    > Committed: https://crrev.com/3b74c2c950e3681a94601f61d48a6ff0e25f06f8
    > Cr-Commit-Position: refs/heads/master@{#301007}
    
    TBR=xiyuan@chromium.org,mukai@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=425717
    
    Review URL: https://codereview.chromium.org/674083002
    
    Cr-Commit-Position: refs/heads/master@{#301066}

--- ui/app_list/views/tile_item_view.cc
+++ ui/app_list/views/tile_item_view.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/app_list/app_list_view_delegate.h"
 #include "ui/app_list/search_result.h"
 #include "ui/app_list/views/app_list_main_view.h"
-#include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/color_analysis.h"
 #include "ui/gfx/color_utils.h"
@@ -25,14 +24,6 @@ namespace {
 
 const int kTileSize = 90;
 const int kTileHorizontalPadding = 10;
-
-// In Windows, transparent background color will cause ugly text rendering,
-// therefore kContentsBackgroundColor should be used. See crbug.com/406989
-#if defined(OS_CHROMEOS)
-const SkColor kTitleBackgroundColor = SK_ColorTRANSPARENT;
-#else
-const SkColor kTitleBackgroundColor = app_list::kContentsBackgroundColor;
-#endif
 
 }  // namespace
 
@@ -55,7 +46,7 @@ TileItemView::TileItemView()
   title_->SetAutoColorReadabilityEnabled(false);
   title_->SetEnabledColor(kGridTitleColor);
   title_->set_background(views::Background::CreateSolidBackground(
-      kTitleBackgroundColor));
+      app_list::kContentsBackgroundColor));
   title_->SetFontList(rb.GetFontList(kItemTextFontStyle));
   title_->SetHorizontalAlignment(gfx::ALIGN_CENTER);
 
