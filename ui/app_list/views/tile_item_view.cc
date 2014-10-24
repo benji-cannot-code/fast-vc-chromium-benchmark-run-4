d1fab44364e247c19ce99bbf93bd4e4ef9468186
mukai <mukai@chromium.org>
2014-10-24 12:23:32 -0700

    Reland: Specifies transparent background for the label in TileItemView.
    We may not use single background when it's in Athena (it's not
    used yet though).
    
    The CL was reverted due to obscurely introducing a static initializer;
    the title background color refers to another global variable in another
    file so that it is uninitialized data section at the compilation time.
    
    BUG=425717
    R=xiyuan@chromium.org
    TEST=tools/linux/dump-static-initializer.py ./out/Release/chrome
    
    Review URL: https://codereview.chromium.org/673253004
    
    Cr-Commit-Position: refs/heads/master@{#301168}

--- ui/app_list/views/tile_item_view.cc
+++ ui/app_list/views/tile_item_view.cc
@@ -46,7 +46,7 @@ TileItemView::TileItemView()
   title_->SetAutoColorReadabilityEnabled(false);
   title_->SetEnabledColor(kGridTitleColor);
   title_->set_background(views::Background::CreateSolidBackground(
-      app_list::kContentsBackgroundColor));
+      kLabelBackgroundColor));
   title_->SetFontList(rb.GetFontList(kItemTextFontStyle));
   title_->SetHorizontalAlignment(gfx::ALIGN_CENTER);
 
