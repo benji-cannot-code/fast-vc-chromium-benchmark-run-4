cadcc280a5623b7ad9a9e29a78acc9fbd00b3635
fmalita@chromium.org <fmalita@chromium.org>
2014-10-24 00:35:05 +0000

    Use the stateless GC::fillRect variant when possible.
    This reduces deferred paint save pressure and allows us to remove a
    couple of GC state savers.
    
    R=schenney@chromium.org,senorblanco@chromium.org
    TBR=esprehn@chromium.org
    BUG=424655
    
    Review URL: https://codereview.chromium.org/671813002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184318 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderTheme.cpp
+++ third_party/WebKit/Source/core/rendering/RenderTheme.cpp
@@ -876,8 +876,6 @@ void RenderTheme::paintSliderTicks(RenderObject* o, const PaintInfo& paintInfo, 
         tickRegionWidth = trackBounds.height() - thumbSize.width();
     }
     RefPtrWillBeRawPtr<HTMLDataListOptionsCollection> options = dataList->options();
-    GraphicsContextStateSaver stateSaver(*paintInfo.context);
-    paintInfo.context->setFillColor(o->resolveColor(CSSPropertyColor));
     for (unsigned i = 0; HTMLOptionElement* optionElement = options->item(i); i++) {
         String value = optionElement->value();
         if (!input->isValidValue(value))
@@ -890,7 +888,7 @@ void RenderTheme::paintSliderTicks(RenderObject* o, const PaintInfo& paintInfo, 
             tickRect.setX(tickPosition);
         else
             tickRect.setY(tickPosition);
-        paintInfo.context->fillRect(tickRect);
+        paintInfo.context->fillRect(tickRect, o->resolveColor(CSSPropertyColor));
     }
 }
 
