03054448ce133f2d0dd331c88836d277a370b185
Takayoshi Kochi <kochi@chromium.org>
2014-10-24 11:08:03 +0900

    Revert "Enabled the App Info dialog on the Extensions page by default"
    This reverts commit a867ca2b5e42207b65e9d07702138ff79bed0bb0.
    
    This CL forgot to change
    chrome/browser/ui/webui/extensions/extension_settings_handler.cc
    which resulted in compilation errors.
    
    E.g.
    http://build.chromium.org/p/chromium.webkit/builders/Linux%20ChromiumOS%20Full/builds/6096
    
    BUG=426717
    TBR=sashab@chromium.org, thestig@chromium.org, asvitkine@chromium.org, benwells@chromium.org, mpearson@chromium.org
    
    Review URL: https://codereview.chromium.org/650033006
    
    Cr-Commit-Position: refs/heads/master@{#301030}

--- chrome/browser/ui/webui/extensions/extension_settings_handler.cc
+++ chrome/browser/ui/webui/extensions/extension_settings_handler.cc
@@ -109,22 +109,9 @@ using content::RenderViewHost;
 using content::WebContents;
 
 namespace {
-
 const char kAppsDeveloperToolsExtensionId[] =
     "ohmmkhmmmpcnpikjeljgnaoabkaalbgc";
-
-// Returns true if the extensions page should display the new-style extension
-// info dialog. If false, display the old permissions dialog.
-bool ShouldDisplayExtensionInfoDialog() {
-#if defined(OS_MACOSX)
-  return false;
-#else
-  return !base::CommandLine::ForCurrentProcess()->HasSwitch(
-      extensions::switches::kDisableExtensionInfoDialog);
-#endif
 }
-
-}  // namespace
 
 namespace extensions {
 
@@ -554,7 +541,8 @@ void ExtensionSettingsHandler::GetLocalizedValues(
       l10n_util::GetStringUTF16(IDS_EXTENSIONS_RELOAD_UNPACKED));
   source->AddString("extensionSettingsOptions",
       l10n_util::GetStringUTF16(IDS_EXTENSIONS_OPTIONS_LINK));
-  if (ShouldDisplayExtensionInfoDialog()) {
+  if (CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kEnableExtensionInfoDialog)) {
     source->AddString("extensionSettingsPermissions",
                       l10n_util::GetStringUTF16(IDS_EXTENSIONS_INFO_LINK));
   } else {
@@ -1215,7 +1203,8 @@ void ExtensionSettingsHandler::HandlePermissionsMessage(
 
   // Show the new-style extensions dialog when the flag is set. The flag cannot
   // be set on Mac platforms.
-  if (ShouldDisplayExtensionInfoDialog()) {
+  if (CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kEnableExtensionInfoDialog)) {
     UMA_HISTOGRAM_ENUMERATION("Apps.AppInfoDialog.Launches",
                               AppInfoLaunchSource::FROM_EXTENSIONS_PAGE,
                               AppInfoLaunchSource::NUM_LAUNCH_SOURCES);
