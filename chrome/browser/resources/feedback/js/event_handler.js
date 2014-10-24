afb79673aec391c0afa59a2a2362d201bc393be5
joth <joth@chromium.org>
2014-10-23 21:54:28 -0700

    Add GLS to the feedback whitelist
    BUG=426600
    
    Review URL: https://codereview.chromium.org/645703004
    
    Cr-Commit-Position: refs/heads/master@{#301049}

--- chrome/browser/resources/feedback/js/event_handler.js
+++ chrome/browser/resources/feedback/js/event_handler.js
@@ -47,6 +47,8 @@ var whitelistedExtensionIds = [
   'ljclpkphhpbpinifbeabbhlfddcpfdde', // Hangouts Extension
   'nckgahadagoaajjgafhacjanaoiihapd', // Hangouts Extension
   'knipolnnllmklapflnccelgolnpehhpl', // Hangouts Extension
+  'dogkdgiahcdchbabhdmpbhlfoddjined', // GLS nightly
+  'khkjfddibboofomnlkndfedpoccieiee', // GLS stable
 ];
 
 /**
