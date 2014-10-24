51edaa92e46d05be469e13cc384f7c48c254b19d
sashab <sashab@chromium.org>
2014-10-23 18:22:59 -0700

    Hide second 'Details' link on extensions page if App Info dialog used
    Hide the second 'Details' link on extensions page if the Extension
    Info Dialog is enabled. Screenshots on bug.
    
    BUG=395495
    
    Review URL: https://codereview.chromium.org/664473005
    
    Cr-Commit-Position: refs/heads/master@{#301017}

--- chrome/browser/resources/extensions/extension_list.js
+++ chrome/browser/resources/extensions/extension_list.js
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *            dependentExtensions: Array,
  *            description: string,
  *            detailsUrl: string,
+ *            enableExtensionInfoDialog: boolean,
  *            enable_show_button: boolean,
  *            enabled: boolean,
  *            enabledIncognito: boolean,
@@ -309,7 +310,7 @@ cr.define('options', function() {
       });
 
       // The 'View in Web Store/View Web Site' link.
-      if (extension.homepageUrl) {
+      if (extension.homepageUrl && !extension.enableExtensionInfoDialog) {
         var siteLink = node.querySelector('.site-link');
         siteLink.href = extension.homepageUrl;
         siteLink.textContent = loadTimeData.getString(
