2f46f3bd83a8cce7b541143e767ef80b23386c81
tfarina <tfarina@chromium.org>
2014-10-23 20:56:33 -0700

    content: Cleanup, remove unused SkPorterDuff.h include.
    This skia API is marked as deprecated, so we need to remove all usages
    of it, to be able to remove it in Skia.
    
    BUG=None
    TEST=None
    TBR=skyostil@chromium.org
    
    Review URL: https://codereview.chromium.org/669303005
    
    Cr-Commit-Position: refs/heads/master@{#301045}

--- content/browser/android/system_ui_resource_manager_impl.cc
+++ content/browser/android/system_ui_resource_manager_impl.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "third_party/skia/include/core/SkCanvas.h"
 #include "third_party/skia/include/core/SkPaint.h"
-#include "third_party/skia/include/effects/SkPorterDuff.h"
 #include "ui/gfx/android/java_bitmap.h"
 #include "ui/gfx/screen.h"
 
