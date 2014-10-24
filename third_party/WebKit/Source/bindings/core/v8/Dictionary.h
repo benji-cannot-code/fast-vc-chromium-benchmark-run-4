89080e18afa154fe1afbf4f54b02d8a988964888
shans@chromium.org <shans@chromium.org>
2014-10-24 12:02:57 +0000

    WebIDL Dictionaries should access prototype chains when searching for properties. This change adds getPropertyNames to the v8 Dictionary class.
    Web Animations Keyframes should make use of prototype chains when looking for CSS properties to animate. This change makes use of getPropertyNames to ensure this happens.
    
    Review URL: https://codereview.chromium.org/661453004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184350 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/core/v8/Dictionary.h
+++ third_party/WebKit/Source/bindings/core/v8/Dictionary.h
@@ -126,6 +126,7 @@ public:
 
     bool getOwnPropertiesAsStringHashMap(HashMap<String, String>&) const;
     bool getOwnPropertyNames(Vector<String>&) const;
+    bool getPropertyNames(Vector<String>&) const;
 
     bool getWithUndefinedOrNullCheck(const String&, String&) const;
     bool getWithUndefinedOrNullCheck(const String&, RefPtrWillBeMember<Element>&) const;
