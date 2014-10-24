672495e69378fae66b73d1bc9b972a7dba704de6
dmazzoni@chromium.org <dmazzoni@chromium.org>
2014-10-24 06:50:58 +0000

    Get rid of AXComputedObjectAttributeCache
    We added AXComputedObjectAttributeCache to WebKit because we wanted to
    cache some expensive computations in the AX tree, but none of the other
    WebKit ports wanted it.
    
    A much simpler solution is to just store the cached value inside the
    AXObject. To determine if the value is out of date, I added a
    modification count that increments whenever there's a layout or DOM
    mutation. We can reuse the same modification count for more cached
    attributes.
    
    BUG=425879
    
    Review URL: https://codereview.chromium.org/670093002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184331 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/accessibility/AXObjectCacheImpl.cpp
+++ third_party/WebKit/Source/core/accessibility/AXObjectCacheImpl.cpp
@@ -89,34 +89,11 @@ AXObjectCache* AXObjectCache::create(Document& document)
     return new AXObjectCacheImpl(document);
 }
 
-AXObjectInclusion AXComputedObjectAttributeCache::getIgnored(AXID id) const
-{
-    HashMap<AXID, CachedAXObjectAttributes>::const_iterator it = m_idMapping.find(id);
-    return it != m_idMapping.end() ? it->value.ignored : DefaultBehavior;
-}
-
-void AXComputedObjectAttributeCache::setIgnored(AXID id, AXObjectInclusion inclusion)
-{
-    HashMap<AXID, CachedAXObjectAttributes>::iterator it = m_idMapping.find(id);
-    if (it != m_idMapping.end()) {
-        it->value.ignored = inclusion;
-    } else {
-        CachedAXObjectAttributes attributes;
-        attributes.ignored = inclusion;
-        m_idMapping.set(id, attributes);
-    }
-}
-
-void AXComputedObjectAttributeCache::clear()
-{
-    m_idMapping.clear();
-}
-
 AXObjectCacheImpl::AXObjectCacheImpl(Document& document)
     : m_document(document)
+    , m_modificationCount(0)
     , m_notificationPostTimer(this, &AXObjectCacheImpl::notificationPostTimerFired)
 {
-    m_computedObjectAttributeCache = AXComputedObjectAttributeCache::create();
 }
 
 AXObjectCacheImpl::~AXObjectCacheImpl()
@@ -759,7 +736,7 @@ void AXObjectCacheImpl::postNotification(RenderObject* renderer, AXNotification 
     if (!renderer)
         return;
 
-    m_computedObjectAttributeCache->clear();
+    m_modificationCount++;
 
     // Get an accessibility object that already exists. One should not be created here
     // because a render update may be in progress and creating an AX object can re-trigger a layout
@@ -780,7 +757,7 @@ void AXObjectCacheImpl::postNotification(Node* node, AXNotification notification
     if (!node)
         return;
 
-    m_computedObjectAttributeCache->clear();
+    m_modificationCount++;
 
     // Get an accessibility object that already exists. One should not be created here
     // because a render update may be in progress and creating an AX object can re-trigger a layout
@@ -798,7 +775,7 @@ void AXObjectCacheImpl::postNotification(Node* node, AXNotification notification
 
 void AXObjectCacheImpl::postNotification(AXObject* object, Document* document, AXNotification notification, bool postToElement, PostType postType)
 {
-    m_computedObjectAttributeCache->clear();
+    m_modificationCount++;
 
     if (object && !postToElement)
         object = object->observableObject();
@@ -844,7 +821,7 @@ void AXObjectCacheImpl::handleScrollbarUpdate(FrameView* view)
 
     // We don't want to create a scroll view from this method, only update an existing one.
     if (AXObject* scrollViewObject = get(view)) {
-        m_computedObjectAttributeCache->clear();
+        m_modificationCount++;
         scrollViewObject->updateChildrenIfNecessary();
     }
 }
@@ -854,7 +831,7 @@ void AXObjectCacheImpl::handleLayoutComplete(RenderObject* renderer)
     if (!renderer)
         return;
 
-    m_computedObjectAttributeCache->clear();
+    m_modificationCount++;
 
     // Create the AXObject if it didn't yet exist - that's always safe at the end of a layout, and it
     // allows an AX notification to be sent when a page has its first layout, rather than when the
@@ -879,7 +856,7 @@ void AXObjectCacheImpl::handleAriaRoleChanged(Node* node)
 {
     if (AXObject* obj = getOrCreate(node)) {
         obj->updateAccessibilityRole();
-        m_computedObjectAttributeCache->clear();
+        m_modificationCount++;
         obj->notifyIfIgnoredValueChanged();
     }
 }
