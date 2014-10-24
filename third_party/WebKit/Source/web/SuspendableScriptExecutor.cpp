0052c2fb40fa6075f354051b5a18847e3ccaba8b
kozyatinskiy@google.com <kozyatinskiy@google.com>
2014-10-24 09:55:45 +0000

    Added SuspendableScriptExecutor::createAndRun to replace ctor+run
    R=vsevik@chromium.org,aandrey@chromium.org,pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/649183010
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184344 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/SuspendableScriptExecutor.cpp
+++ third_party/WebKit/Source/web/SuspendableScriptExecutor.cpp
@@ -15,6 +15,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+void SuspendableScriptExecutor::createAndRun(LocalFrame* frame, int worldID, const Vector<ScriptSourceCode>& sources, int extensionGroup, bool userGesture, WebScriptExecutionCallback* callback)
+{
+    SuspendableScriptExecutor* executor = new SuspendableScriptExecutor(frame, worldID, sources, extensionGroup, userGesture, callback);
+    executor->run();
+}
+
+void SuspendableScriptExecutor::resume()
+{
+    executeAndDestroySelf();
+}
+
+void SuspendableScriptExecutor::contextDestroyed()
+{
+    // this method can only be called if the script was not called in run()
+    // and context remained suspend (method resume has never called)
+    ActiveDOMObject::contextDestroyed();
+    m_callback->completed(Vector<v8::Local<v8::Value> >());
+    delete this;
+}
+
 SuspendableScriptExecutor::SuspendableScriptExecutor(LocalFrame* frame, int worldID, const Vector<ScriptSourceCode>& sources, int extensionGroup, bool userGesture, WebScriptExecutionCallback* callback)
     : ActiveDOMObject(frame->document())
     , m_frame(frame)
@@ -37,20 +57,6 @@ void SuspendableScriptExecutor::run()
     ASSERT(context);
     if (context && !context->activeDOMObjectsAreSuspended())
         executeAndDestroySelf();
-}
-
-void SuspendableScriptExecutor::resume()
-{
-    executeAndDestroySelf();
-}
-
-void SuspendableScriptExecutor::contextDestroyed()
-{
-    // this method can only be called if the script was not called in run()
-    // and context remained suspend (method resume has never called)
-    ActiveDOMObject::contextDestroyed();
-    m_callback->completed(Vector<v8::Local<v8::Value> >());
-    delete this;
 }
 
 void SuspendableScriptExecutor::executeAndDestroySelf()
