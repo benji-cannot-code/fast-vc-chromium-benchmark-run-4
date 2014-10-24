fa71a1c4d9fc71157a78258fc6d5a6bff459a09b
kundaji <kundaji@chromium.org>
2014-10-23 17:06:51 -0700

    Activate data reduction proxy after profile initialization is complete.
    When we activate data reduction proxy, a request is made to the prober
    URL. If this request is made before
    ProfileManager::DoFinalInitForServices() completes, it causes a crash
    because HostContentSettingsMap is not yet initialized.
    
    BUG=424260
    
    Review URL: https://codereview.chromium.org/665703002
    
    Cr-Commit-Position: refs/heads/master@{#300995}

--- chrome/browser/net/spdyproxy/data_reduction_proxy_chrome_settings_factory.h
+++ chrome/browser/net/spdyproxy/data_reduction_proxy_chrome_settings_factory.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_NET_SPDYPROXY_DATA_REDUCTION_PROXY_CHROME_SETTINGS_FACTORY_H_
 #define CHROME_BROWSER_NET_SPDYPROXY_DATA_REDUCTION_PROXY_CHROME_SETTINGS_FACTORY_H_
 
-#include "chrome/browser/net/spdyproxy/data_reduction_proxy_chrome_settings_factory.h"
-
 #include "base/compiler_specific.h"
 #include "base/memory/singleton.h"
 #include "components/data_reduction_proxy/core/browser/data_reduction_proxy_params.h"
