c1f4533fc774b9b64287c348544d0afac82465a3
dbeam <dbeam@chromium.org>
2014-10-23 20:54:07 -0700

    zoom bubble: Close if anchor is clicked while bubble is showing.
    R=msw@chromium.org
    BUG=366092
    
    Review URL: https://codereview.chromium.org/420533002
    
    Cr-Commit-Position: refs/heads/master@{#301043}

--- chrome/browser/ui/views/location_bar/zoom_bubble_view.h
+++ chrome/browser/ui/views/location_bar/zoom_bubble_view.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_LOCATION_BAR_ZOOM_BUBBLE_VIEW_H_
 
 #include "base/basictypes.h"
+#include "base/gtest_prod_util.h"
 #include "base/timer/timer.h"
 #include "chrome/browser/ui/views/frame/immersive_mode_controller.h"
 #include "content/public/browser/notification_observer.h"
@@ -51,6 +52,8 @@ class ZoomBubbleView : public views::BubbleDelegateView,
   static const ZoomBubbleView* GetZoomBubbleForTest();
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(ZoomBubbleBrowserTest, ImmersiveFullscreen);
+
   // Stores information about the extension that initiated the zoom change, if
   // any.
   struct ZoomBubbleExtensionInfo {
