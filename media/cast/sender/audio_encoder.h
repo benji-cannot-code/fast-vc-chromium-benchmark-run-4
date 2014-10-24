5065e6590f06fd745cf5e53a5de23c5649f5b434
jfroy <jfroy@chromium.org>
2014-10-23 18:53:53 -0700

    [cast] AAC encoder for OS X and iOS based on AudioConverter and AudioFile
    BUG=417859
    
    Review URL: https://codereview.chromium.org/601413003
    
    Cr-Commit-Position: refs/heads/master@{#301026}

--- media/cast/sender/audio_encoder.h
+++ media/cast/sender/audio_encoder.h
@@ -46,6 +46,7 @@ class AudioEncoder {
   class ImplBase;
   class OpusImpl;
   class Pcm16Impl;
+  class AppleAacImpl;
 
   const scoped_refptr<CastEnvironment> cast_environment_;
   scoped_refptr<ImplBase> impl_;
