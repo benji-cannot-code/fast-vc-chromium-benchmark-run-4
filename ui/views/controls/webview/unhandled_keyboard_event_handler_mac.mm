e82fe7c1349994d3026dc94bd52e701af2f90c36
andresantoso <andresantoso@chromium.org>
2014-10-24 14:36:44 -0700

    MacViews: UnhandledKeyboardEventHandler stub
    Fix for missing UnhandledKeyboardEventHandler::HandleNativeKeyboardEvent on
    Mac, in preparation for compiling chrome/browser/ui/views for MacViews.
    Consolidate duplicated linux/windows code to be shared code and stub out
    the Mac implementation.
    
    BUG=425229
    
    Review URL: https://codereview.chromium.org/679743002
    
    Cr-Commit-Position: refs/heads/master@{#301198}

--- ui/views/controls/webview/unhandled_keyboard_event_handler_mac.mm
+++ ui/views/controls/webview/unhandled_keyboard_event_handler_mac.mm
@@ -1,11 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "ui/views/controls/webview/unhandled_keyboard_event_handler.h"
-
-#include "ui/events/event.h"
 
 namespace views {
 
@@ -13,10 +11,7 @@ namespace views {
 void UnhandledKeyboardEventHandler::HandleNativeKeyboardEvent(
     gfx::NativeEvent event,
     FocusManager* focus_manager) {
-  // Any unhandled keyboard/character messages should be defproced.
-  // This allows stuff like F10, etc to work correctly.
-  const MSG& message(event->native_event());
-  DefWindowProc(message.hwnd, message.message, message.wParam, message.lParam);
+  NOTIMPLEMENTED();
 }
 
 }  // namespace views
