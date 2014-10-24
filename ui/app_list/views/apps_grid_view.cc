01f2761c5de3b242e957df89b9c144d172281ab7
calamity <calamity@chromium.org>
2014-10-24 00:52:17 -0700

    Remove 1 pixel padding around app list main view.
    This CL removes the 1 pixel padding around the app list main view. This
    subtly changes the layout of the app list by changing the padding of
    certain elements.
    
    The dimensions of the app list window are still the same.
    
    This is being done in preparation for the new experimental app list
    layout.
    
    BUG=425444
    
    Review URL: https://codereview.chromium.org/676513003
    
    Cr-Commit-Position: refs/heads/master@{#301073}

--- ui/app_list/views/apps_grid_view.cc
+++ ui/app_list/views/apps_grid_view.cc
@@ -61,8 +61,9 @@ namespace {
 const int kDragBufferPx = 20;
 
 // Padding space in pixels for fixed layout.
-const int kLeftRightPadding = 23;
 const int kTopPadding = 1;
+const int kBottomPadding = 1;
+const int kLeftRightPadding = 24;
 
 // Padding space in pixels between pages.
 const int kPagePadding = 40;
@@ -72,7 +73,7 @@ const int kPreferredTileWidth = 88;
 const int kPreferredTileHeight = 98;
 
 const int kExperimentalPreferredTileWidth = 90;
-const int kExperimentalPrefferedTileHeight = 90;
+const int kExperimentalPreferredTileHeight = 90;
 
 // Padding on each side of a tile.
 const int kExperimentalTileLeftRightPadding = 15;
@@ -107,7 +108,7 @@ const int kFolderDroppingCircleRadius = 39;
 gfx::Size GetTileViewSize() {
   return switches::IsExperimentalAppListEnabled()
              ? gfx::Size(kExperimentalPreferredTileWidth,
-                         kExperimentalPrefferedTileHeight)
+                         kExperimentalPreferredTileHeight)
              : gfx::Size(kPreferredTileWidth, kPreferredTileHeight);
 }
 
@@ -425,11 +426,15 @@ void AppsGridView::SetLayout(int cols, int rows_per_page) {
   cols_ = cols;
   rows_per_page_ = rows_per_page;
 
-  SetBorder(views::Border::CreateEmptyBorder(
-      switches::IsExperimentalAppListEnabled() ? 0 : kTopPadding,
-      kLeftRightPadding,
-      0,
-      kLeftRightPadding));
+  if (switches::IsExperimentalAppListEnabled()) {
+    SetBorder(views::Border::CreateEmptyBorder(0,
+                                               kExperimentalWindowPadding,
+                                               kBottomPadding,
+                                               kExperimentalWindowPadding));
+  } else {
+    SetBorder(views::Border::CreateEmptyBorder(
+        kTopPadding, kLeftRightPadding, kBottomPadding, kLeftRightPadding));
+  }
 }
 
 void AppsGridView::ResetForShowApps() {
