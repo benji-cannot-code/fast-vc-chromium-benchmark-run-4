89a5dcdbb408ddd790bdd3ef60b6d03c9822f511
rlp <rlp@chromium.org>
2014-10-24 15:53:38 -0700

    [Hotword] Don't sync per-device hotword prefs.
    BUG=426655
    
    Review URL: https://codereview.chromium.org/649213003
    
    Cr-Commit-Position: refs/heads/master@{#301216}

--- chrome/browser/search/hotword_service_factory.cc
+++ chrome/browser/search/hotword_service_factory.cc
@@ -95,9 +95,6 @@ void HotwordServiceFactory::UpdateMicrophoneState() {
 
 void HotwordServiceFactory::RegisterProfilePrefs(
     user_prefs::PrefRegistrySyncable* prefs) {
-  prefs->RegisterBooleanPref(prefs::kHotwordSearchEnabled,
-                             false,
-                             user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
   // Although this is default true, users will not send back information to
   // Google unless they have opted-in to Hotwording at which point they must
   // also confirm that they wish this preference to be true or opt out of it.
@@ -107,9 +104,13 @@ void HotwordServiceFactory::RegisterProfilePrefs(
   prefs->RegisterStringPref(prefs::kHotwordPreviousLanguage,
                             std::string(),
                             user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
+  // Per-device settings (do not sync).
+  prefs->RegisterBooleanPref(prefs::kHotwordSearchEnabled,
+                             false,
+                             user_prefs::PrefRegistrySyncable::UNSYNCABLE_PREF);
   prefs->RegisterBooleanPref(prefs::kHotwordAlwaysOnSearchEnabled,
                              false,
-                             user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
+                             user_prefs::PrefRegistrySyncable::UNSYNCABLE_PREF);
 }
 
 KeyedService* HotwordServiceFactory::BuildServiceInstanceFor(
