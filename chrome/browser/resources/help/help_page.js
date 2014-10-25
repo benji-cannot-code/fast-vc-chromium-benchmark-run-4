8a63063939630aed30bac35a752036ed45b89f65
dbeam <dbeam@chromium.org>
2014-10-24 17:26:13 -0700

    closure: fix compile error in setTimeout() call.
    TBR=michaelpg@chromium.org
    BUG=none
    
    Review URL: https://codereview.chromium.org/675303003
    
    Cr-Commit-Position: refs/heads/master@{#301243}

--- chrome/browser/resources/help/help_page.js
+++ chrome/browser/resources/help/help_page.js
@@ -161,7 +161,7 @@ cr.define('help', function() {
       var logo = $('product-logo');
       logo.onclick = function(e) {
         logo.classList.remove('spin');
-        setTimeout(function() { logo.classList.add('spin'); });
+        setTimeout(function() { logo.classList.add('spin'); }, 0);
       };
 
       // Attempt to update.
