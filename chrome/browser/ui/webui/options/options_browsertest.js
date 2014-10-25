cf37588afc02f901efcbb7b5b4296a063588f2d2
hcarmona <hcarmona@chromium.org>
2014-10-24 19:21:06 -0700

    Enable a11y audit for chrome://settings
    Enabled the tests and fixed failure. Failure was due to transition obscuring elements in the UI.
    
    BUG=311866
    
    Review URL: https://codereview.chromium.org/646853004
    
    Cr-Commit-Position: refs/heads/master@{#301266}

--- chrome/browser/ui/webui/options/options_browsertest.js
+++ chrome/browser/ui/webui/options/options_browsertest.js
@@ -47,6 +47,9 @@ OptionsWebUITest.prototype = {
   __proto__: testing.Test.prototype,
 
   /** @override */
+  runAccessibilityChecks: true,
+
+  /** @override */
   accessibilityIssuesAreErrors: true,
 
   /** @override */
@@ -381,6 +384,12 @@ OptionsWebUIExtendedTest.prototype = {
   isAsync: true,
 
   /** @override */
+  runAccessibilityChecks: true,
+
+  /** @override */
+  accessibilityIssuesAreErrors: true,
+
+  /** @override */
   setUp: function() {
       // user-image-stream is a streaming video element used for capturing a
       // user image during OOBE.
@@ -709,7 +718,12 @@ TEST_F('OptionsWebUIExtendedTest', 'CloseOverlayWithHashes', function() {
       this.verifyHistory_(
           ['', 'search#1', 'languages#2', 'addLanguage#3', 'languages#2',
            'search#1'],
-          testDone);
+          function() {
+            document.addEventListener('webkitTransitionEnd', function f(e) {
+              document.removeEventListener('webkitTransitionEnd', f);
+              testDone();
+            });
+          });
     }.bind(this));
   }.bind(this));
 });
