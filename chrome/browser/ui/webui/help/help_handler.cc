a585d78aac18ad933e29114fa26a725d56661719
michaelpg <michaelpg@chromium.org>
2014-10-24 17:57:40 -0700

    Show product label in About UI
    This uses UserImageLoader instead of copying most of its functionality. As a
    follow-up, we should generalize this class to not depend on user_manager.
    
    Instead of checking if the file exists in HelpHander before telling the
    JavaScript where to point the <img> -- which delays the appearance of the image
    -- we just set <img src="chrome://foo"> and a network error is logged if the
    image does not exist.
    
    See https://crrev.com/642563003 for more details.
    
    BUG=427109
    TBR=dbeam@chromium.org
    
    Review URL: https://codereview.chromium.org/675273006
    
    Cr-Commit-Position: refs/heads/master@{#301254}

--- chrome/browser/ui/webui/help/help_handler.cc
+++ chrome/browser/ui/webui/help/help_handler.cc
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/policy/browser_policy_connector_chromeos.h"
 #include "chrome/browser/chromeos/settings/cros_settings.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/webui/chromeos/image_source.h"
 #include "chrome/browser/ui/webui/help/help_utils_chromeos.h"
 #include "chrome/browser/ui/webui/help/version_updater_chromeos.h"
 #include "chromeos/chromeos_switches.h"
@@ -293,6 +294,12 @@ void HelpHandler::RegisterMessages() {
 #if defined(OS_MACOSX)
   web_ui()->RegisterMessageCallback("promoteUpdater",
       base::Bind(&HelpHandler::PromoteUpdater, base::Unretained(this)));
+#endif
+
+#if defined(OS_CHROMEOS)
+  // Handler for the product label image, which will be shown if available.
+  content::URLDataSource::Add(Profile::FromWebUI(web_ui()),
+                              new chromeos::ImageSource());
 #endif
 }
 
