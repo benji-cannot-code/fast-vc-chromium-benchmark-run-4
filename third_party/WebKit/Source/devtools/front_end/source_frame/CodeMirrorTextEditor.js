6c234bcca7ca08dce7f7e0f9a264368d1b98ecf5
vsevik@chromium.org <vsevik@chromium.org>
2014-10-27 14:45:44 +0000

    Revert of DevTools: [CodeMirrro] defer autocomplete controller initialization (patchset #2 id:20001 of https://codereview.chromium.org/670163002/)
    Reason for revert:
    Breaks autocomplete (only ctrl+space works)
    
    Original issue's description:
    > DevTools: [CodeMirrro] defer autocomplete controller initialization
    > 
    > Autocompletion controller intialization makes almost half of the time
    > of SourceFrame initialization.
    > 
    > In order to render editor as fast as possible, this patch defers
    > autocompletion initialization until the first need.
    > 
    > BUG=425550
    > R=vsevik
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=184263
    
    TBR=lushnikov@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=425550
    
    Review URL: https://codereview.chromium.org/676393003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184451 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/source_frame/CodeMirrorTextEditor.js
+++ third_party/WebKit/Source/devtools/front_end/source_frame/CodeMirrorTextEditor.js
@@ -1991,12 +1991,17 @@ WebInspector.CodeMirrorTextEditor.AutocompleteController = function(textEditor, 
     this._changes = this._changes.bind(this);
     this._beforeChange = this._beforeChange.bind(this);
     this._blur = this._blur.bind(this);
+    this._codeMirror.on("scroll", this._onScroll);
+    this._codeMirror.on("cursorActivity", this._onCursorActivity);
+    this._codeMirror.on("changes", this._changes);
+    this._codeMirror.on("beforeChange", this._beforeChange);
+    this._codeMirror.on("blur", this._blur);
 
     this._additionalWordChars = WebInspector.CodeMirrorTextEditor._NoAdditionalWordChars;
     this._enabled = true;
 
     this._dictionary = dictionary;
-    this._initialized = false;
+    this._addTextToCompletionDictionary(this._textEditor.text());
 }
 
 WebInspector.CodeMirrorTextEditor.AutocompleteController.Dummy = new WebInspector.CodeMirrorTextEditor.DummyAutocompleteController();
@@ -2004,23 +2009,8 @@ WebInspector.CodeMirrorTextEditor._NoAdditionalWordChars = {};
 WebInspector.CodeMirrorTextEditor._CSSAdditionalWordChars = { ".": true, "-": true };
 
 WebInspector.CodeMirrorTextEditor.AutocompleteController.prototype = {
-    _initializeIfNeeded: function()
-    {
-        if (this._initialized)
-            return;
-        this._initialized = true;
-        this._codeMirror.on("scroll", this._onScroll);
-        this._codeMirror.on("cursorActivity", this._onCursorActivity);
-        this._codeMirror.on("changes", this._changes);
-        this._codeMirror.on("beforeChange", this._beforeChange);
-        this._codeMirror.on("blur", this._blur);
-        this._addTextToCompletionDictionary(this._textEditor.text());
-    },
-
     dispose: function()
     {
-        if (!this._initialized)
-            return;
         this._codeMirror.off("scroll", this._onScroll);
         this._codeMirror.off("cursorActivity", this._onCursorActivity);
         this._codeMirror.off("changes", this._changes);
@@ -2183,7 +2173,6 @@ WebInspector.CodeMirrorTextEditor.AutocompleteController.prototype = {
 
     autocomplete: function()
     {
-        this._initializeIfNeeded();
         var dictionary = this._dictionary;
         if (this._codeMirror.somethingSelected()) {
             this.finishAutocomplete();
