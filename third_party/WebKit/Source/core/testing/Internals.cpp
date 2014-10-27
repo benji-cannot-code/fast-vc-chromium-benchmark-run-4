6d923926e70312cb176e282bb696046d6a6f1f3f
yukishiino@chromium.org <yukishiino@chromium.org>
2014-10-27 10:36:37 +0000

    Fixes look up of the best clickable node.
    There is a case that EventHandler::bestClickableNodeForHitTestResult
    and Internals::touchNodeAdjustedToBestClickableNode return an
    uninitialized pointer.  In that case, we must return null instead.
    
    This CL fixes the issue and returns null of type Node* as expected.
    
    Zero-initializations in Internals.cpp are not mandatory, but I think
    it's better to have them just in case and as a good coding practice
    (never leave pointers uninitialized).
    
    Note that I cannot reproduce the issue 426760, so cannot confirm the
    fix, too.  But I think this is the cause of the issue.
    
    BUG=426760
    
    Review URL: https://codereview.chromium.org/675403003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184439 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/testing/Internals.cpp
+++ third_party/WebKit/Source/core/testing/Internals.cpp
@@ -1019,7 +1019,7 @@ DOMPoint* Internals::touchPositionAdjustedToBestClickableNode(long x, long y, lo
     IntPoint hitTestPoint = document->frame()->view()->windowToContents(point);
     HitTestResult result = eventHandler.hitTestResultAtPoint(hitTestPoint, HitTestRequest::ReadOnly | HitTestRequest::Active, radius);
 
-    Node* targetNode;
+    Node* targetNode = 0;
     IntPoint adjustedPoint;
 
     bool foundNode = eventHandler.bestClickableNodeForHitTestResult(result, adjustedPoint, targetNode);
@@ -1046,7 +1046,7 @@ Node* Internals::touchNodeAdjustedToBestClickableNode(long x, long y, long width
     IntPoint hitTestPoint = document->frame()->view()->windowToContents(point);
     HitTestResult result = eventHandler.hitTestResultAtPoint(hitTestPoint, HitTestRequest::ReadOnly | HitTestRequest::Active, radius);
 
-    Node* targetNode;
+    Node* targetNode = 0;
     IntPoint adjustedPoint;
     document->frame()->eventHandler().bestClickableNodeForHitTestResult(result, adjustedPoint, targetNode);
     return targetNode;
@@ -1115,7 +1115,7 @@ PassRefPtrWillBeRawPtr<ClientRect> Internals::bestZoomableAreaForTouchPoint(long
     IntSize radius(width / 2, height / 2);
     IntPoint point(x + radius.width(), y + radius.height());
 
-    Node* targetNode;
+    Node* targetNode = 0;
     IntRect zoomableArea;
     bool foundNode = document->frame()->eventHandler().bestZoomableAreaForTouchPoint(point, radius, zoomableArea, targetNode);
     if (foundNode)
