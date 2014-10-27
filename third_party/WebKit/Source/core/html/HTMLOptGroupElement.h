6224e18336b263b2e3fc8cf8ff42c7499d775214
keishi@chromium.org <keishi@chromium.org>
2014-10-27 07:00:10 +0000

    Fix <select> listbox height when there is :before pseudo element
    BUG=423761
    
    Review URL: https://codereview.chromium.org/660693002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184428 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLOptGroupElement.h
+++ third_party/WebKit/Source/core/html/HTMLOptGroupElement.h
@@ -41,6 +41,7 @@ public:
     HTMLSelectElement* ownerSelectElement() const;
 
     String groupLabelText() const;
+    HTMLDivElement& optGroupLabelElement() const;
 
 private:
     explicit HTMLOptGroupElement(Document&);
@@ -60,7 +61,6 @@ private:
 
     void updateGroupLabel();
     void recalcSelectOptions();
-    HTMLDivElement& optGroupLabelElement() const;
 
     RefPtr<RenderStyle> m_style;
 };
