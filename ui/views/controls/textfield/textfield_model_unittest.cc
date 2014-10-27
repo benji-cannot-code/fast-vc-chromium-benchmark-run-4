5f2c0e56363ca63452a0f06e1565d63d360916cf
dcheng <dcheng@chromium.org>
2014-10-27 14:55:30 -0700

    Standardize usage of virtual/override/final specifiers.
    The Google C++ style guide states:
    
      Explicitly annotate overrides of virtual functions or virtual
      destructors with an override or (less frequently) final specifier.
      Older (pre-C++11) code will use the virtual keyword as an inferior
      alternative annotation. For clarity, use exactly one of override,
      final, or virtual when declaring an override.
    
    To better conform to these guidelines, the following constructs have
    been rewritten:
    
    - if a base class has a virtual destructor, then:
        virtual ~Foo();                   ->  ~Foo() override;
    - virtual void Foo() override;        ->  void Foo() override;
    - virtual void Foo() override final;  ->  void Foo() final;
    
    This patch was automatically generated. The clang plugin can generate
    fixit hints, which are suggested edits when it is 100% sure it knows how
    to fix a problem. The hints from the clang plugin were applied to the
    source tree using the tool in https://codereview.chromium.org/598073004.
    
    BUG=417463
    R=msw@chromium.org
    
    Review URL: https://codereview.chromium.org/680023003
    
    Cr-Commit-Position: refs/heads/master@{#301456}

--- ui/views/controls/textfield/textfield_model_unittest.cc
+++ ui/views/controls/textfield/textfield_model_unittest.cc
@@ -51,7 +51,7 @@ class TextfieldModelTest : public ViewsTestBase,
         composition_text_confirmed_or_cleared_(false) {
   }
 
-  virtual void OnCompositionTextConfirmedOrCleared() override {
+  void OnCompositionTextConfirmedOrCleared() override {
     composition_text_confirmed_or_cleared_ = true;
   }
 
--- ui/views/controls/textfield/textfield_unittest.cc
+++ ui/views/controls/textfield/textfield_unittest.cc
@@ -65,7 +65,7 @@ class TestTextfield : public views::Textfield {
        key_received_(false),
        weak_ptr_factory_(this) {}
 
-  virtual bool OnKeyPressed(const ui::KeyEvent& e) override {
+  bool OnKeyPressed(const ui::KeyEvent& e) override {
     key_received_ = true;
 
     // Since OnKeyPressed() might destroy |this|, get a weak pointer and
@@ -81,7 +81,7 @@ class TestTextfield : public views::Textfield {
     return key_handled_;
   }
 
-  virtual bool OnKeyReleased(const ui::KeyEvent& e) override {
+  bool OnKeyReleased(const ui::KeyEvent& e) override {
     key_received_ = true;
     key_handled_ = views::Textfield::OnKeyReleased(e);
     return key_handled_;
@@ -123,8 +123,8 @@ class TextfieldDestroyerController : public views::TextfieldController {
   views::Textfield* target() { return target_.get(); }
 
   // views::TextfieldController:
-  virtual bool HandleKeyEvent(views::Textfield* sender,
-                              const ui::KeyEvent& key_event) override {
+  bool HandleKeyEvent(views::Textfield* sender,
+                      const ui::KeyEvent& key_event) override {
     target_.reset();
     return false;
   }
@@ -160,11 +160,9 @@ class TextfieldTest : public ViewsTestBase, public TextfieldController {
   }
 
   // ::testing::Test:
-  virtual void SetUp() {
-    ViewsTestBase::SetUp();
-  }
+  void SetUp() override { ViewsTestBase::SetUp(); }
 
-  virtual void TearDown() {
+  void TearDown() override {
     if (widget_)
       widget_->Close();
     ViewsTestBase::TearDown();
@@ -177,23 +175,23 @@ class TextfieldTest : public ViewsTestBase, public TextfieldController {
   }
 
   // TextfieldController:
-  virtual void ContentsChanged(Textfield* sender,
-                               const base::string16& new_contents) override {
+  void ContentsChanged(Textfield* sender,
+                       const base::string16& new_contents) override {
     // Paste calls TextfieldController::ContentsChanged() explicitly even if the
     // paste action did not change the content. So |new_contents| may match
     // |last_contents_|. For more info, see http://crbug.com/79002
     last_contents_ = new_contents;
   }
 
-  virtual void OnBeforeUserAction(Textfield* sender) override {
+  void OnBeforeUserAction(Textfield* sender) override {
     ++on_before_user_action_;
   }
 
-  virtual void OnAfterUserAction(Textfield* sender) override {
+  void OnAfterUserAction(Textfield* sender) override {
     ++on_after_user_action_;
   }
 
-  virtual void OnAfterCutOrCopy(ui::ClipboardType clipboard_type) override {
+  void OnAfterCutOrCopy(ui::ClipboardType clipboard_type) override {
     copied_to_clipboard_ = clipboard_type;
   }
 
@@ -629,9 +627,9 @@ TEST_F(TextfieldTest, OnKeyPressBinding) {
   class TestDelegate : public ui::TextEditKeyBindingsDelegateAuraLinux {
    public:
     TestDelegate() {}
-    virtual ~TestDelegate() {}
+    ~TestDelegate() override {}
 
-    virtual bool MatchEvent(
+    bool MatchEvent(
         const ui::Event& event,
         std::vector<ui::TextEditCommandAuraLinux>* commands) override {
       return false;
