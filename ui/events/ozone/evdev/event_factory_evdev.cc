c2e47f634b67f3af651decbc941b3063b252dec2
Antoine Labour <piman@chromium.org>
2014-10-27 16:13:29 -0700

    Revert "ozone: evdev: Handle mouse events"
    This reverts commit e6e890a7ead1cca18d7f9fa9c75d9765c04e6c1a.
    
    Causes compile failures:
    http://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Ozone%20Builder/builds/5856/steps/compile/logs/stdio
    
    TBR=spang
    BUG=
    
    Review URL: https://codereview.chromium.org/682753005
    
    Cr-Commit-Position: refs/heads/master@{#301474}

--- ui/events/ozone/evdev/event_factory_evdev.cc
+++ ui/events/ozone/evdev/event_factory_evdev.cc
@@ -17,8 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/ozone/device/device_event.h"
 #include "ui/events/ozone/device/device_manager.h"
 #include "ui/events/ozone/evdev/cursor_delegate_evdev.h"
-#include "ui/events/ozone/evdev/event_converter_evdev_impl.h"
 #include "ui/events/ozone/evdev/event_device_info.h"
+#include "ui/events/ozone/evdev/key_event_converter_evdev.h"
 #include "ui/events/ozone/evdev/touch_event_converter_evdev.h"
 
 #if defined(USE_EVDEV_GESTURES)
@@ -95,15 +95,9 @@ scoped_ptr<EventConverterEvdev> CreateConverter(
     return make_scoped_ptr<EventConverterEvdev>(new TouchEventConverterEvdev(
         fd, params.path, params.id, devinfo, params.dispatch_callback));
 
-  // Everything else: use EventConverterEvdevImpl.
-  return make_scoped_ptr<EventConverterEvdevImpl>(
-      new EventConverterEvdevImpl(fd,
-                                  params.path,
-                                  params.id,
-                                  params.modifiers,
-                                  params.cursor,
-                                  params.keyboard,
-                                  params.dispatch_callback));
+  // Everything else: use KeyEventConverterEvdev.
+  return make_scoped_ptr<EventConverterEvdev>(
+      new KeyEventConverterEvdev(fd, params.path, params.id, params.keyboard));
 }
 
 // Open an input device. Opening may put the calling thread to sleep, and
