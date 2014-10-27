ad4a3a813aa65b3a84b2f4e537a1e2f73ada4662
yukawa <yukawa@chromium.org>
2014-10-27 10:09:30 -0700

    Plumb composition character bounds for Android 5.0
    This is a groundwork to support CursorAnchorInfo API for Android 5.0.  No user visible change is intended with this CL.
    
    With this CL, ImeCompositionRangeChanged event will be routed from the renderer to the browser in Android 5.0 and above. The underlying functionality has been widely used in desktop OSes for years. There should be no performance impact for existing in Android 4.4 and prior version. Performance impact on Android 5.0 devices will be tracked as Issue 427090. Actual plumbing from native to Java layer will be handled in subsequent CLs.
    
    BUG=424866, 427090
    TEST=Manually done on Nexus 7 Build/LPX13D
    
    Review URL: https://codereview.chromium.org/671503005
    
    Cr-Commit-Position: refs/heads/master@{#301384}

--- content/renderer/render_widget.cc
+++ content/renderer/render_widget.cc
@@ -78,6 +78,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_ANDROID)
 #include <android/keycodes.h>
+#include "base/android/build_info.h"
 #include "content/renderer/android/synchronous_compositor_factory.h"
 #endif
 
@@ -1539,7 +1540,7 @@ void RenderWidget::OnImeSetComposition(
     // sure we are in a consistent state.
     Send(new InputHostMsg_ImeCancelComposition(routing_id()));
   }
-#if defined(OS_MACOSX) || defined(USE_AURA)
+#if defined(OS_MACOSX) || defined(USE_AURA) || defined(OS_ANDROID)
   UpdateCompositionInfo(true);
 #endif
 }
@@ -1558,7 +1559,7 @@ void RenderWidget::OnImeConfirmComposition(const base::string16& text,
   else
     webwidget_->confirmComposition(WebWidget::DoNotKeepSelection);
   handling_input_event_ = false;
-#if defined(OS_MACOSX) || defined(USE_AURA)
+#if defined(OS_MACOSX) || defined(USE_AURA) || defined(OS_ANDROID)
   UpdateCompositionInfo(true);
 #endif
 }
@@ -1890,7 +1891,7 @@ void RenderWidget::UpdateSelectionBounds() {
     }
   }
 
-#if defined(OS_MACOSX) || defined(USE_AURA)
+#if defined(OS_MACOSX) || defined(USE_AURA) || defined(OS_ANDROID)
   UpdateCompositionInfo(false);
 #endif
 }
@@ -1945,8 +1946,15 @@ ui::TextInputType RenderWidget::GetTextInputType() {
   return ui::TEXT_INPUT_TYPE_NONE;
 }
 
-#if defined(OS_MACOSX) || defined(USE_AURA)
+#if defined(OS_MACOSX) || defined(USE_AURA) || defined(OS_ANDROID)
 void RenderWidget::UpdateCompositionInfo(bool should_update_range) {
+#if defined(OS_ANDROID)
+  // Sending composition info makes sense only in Lollipop (API level 21)
+  // and above due to the API availability.
+  if (base::android::BuildInfo::GetInstance()->sdk_int() < 21)
+    return;
+#endif
+
   gfx::Range range = gfx::Range();
   if (should_update_range) {
     GetCompositionRange(&range);
@@ -2038,7 +2046,7 @@ void RenderWidget::resetInputMethod() {
       Send(new InputHostMsg_ImeCancelComposition(routing_id()));
   }
 
-#if defined(OS_MACOSX) || defined(USE_AURA)
+#if defined(OS_MACOSX) || defined(USE_AURA) || defined(OS_ANDROID)
   UpdateCompositionInfo(true);
 #endif
 }
