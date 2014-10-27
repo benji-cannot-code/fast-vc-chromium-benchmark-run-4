beb5358391c5516a18ed24c7edc3812306a1d05d
vmpstr <vmpstr@chromium.org>
2014-10-27 13:36:52 -0700

    cc: Make picture pile base thread safe.
    This patch ensures that picture pile base is thread safe, since it is
    not guaranteed that the ref is always adjusted on the same thread. In
    particular, the worker thread (which refs picture pile on the cc thread)
    might deref it (and delete it) on the worker thread.
    
    R=reveman
    
    Review URL: https://codereview.chromium.org/683543002
    
    Cr-Commit-Position: refs/heads/master@{#301434}

--- cc/resources/picture_pile_base.h
+++ cc/resources/picture_pile_base.h
@@ -27,7 +27,8 @@ class Value;
 
 namespace cc {
 
-class CC_EXPORT PicturePileBase : public base::RefCounted<PicturePileBase> {
+class CC_EXPORT PicturePileBase
+    : public base::RefCountedThreadSafe<PicturePileBase> {
  public:
   PicturePileBase();
   explicit PicturePileBase(const PicturePileBase* other);
@@ -137,7 +138,7 @@ class CC_EXPORT PicturePileBase : public base::RefCounted<PicturePileBase> {
  private:
   void SetBufferPixels(int buffer_pixels);
 
-  friend class base::RefCounted<PicturePileBase>;
+  friend class base::RefCountedThreadSafe<PicturePileBase>;
   DISALLOW_COPY_AND_ASSIGN(PicturePileBase);
 };
 
