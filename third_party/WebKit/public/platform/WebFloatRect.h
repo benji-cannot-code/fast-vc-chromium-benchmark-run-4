c63289fd3ed9437118e29a60e2d783213eb079d4
tfarina@chromium.org <tfarina@chromium.org>
2014-10-27 01:42:03 +0000

    Cleanup: Update the path to geometry headers.
    The geometry headers were moved from ui/gfx/ to ui/gfx/geometry, so we
    are updating the include paths to fix this.
    
    BUG=395370
    TEST=None
    R=tkent@chromium.org
    
    Review URL: https://codereview.chromium.org/678593003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184418 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebFloatRect.h
+++ third_party/WebKit/public/platform/WebFloatRect.h
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #else
 #include <algorithm>
 #include <cmath>
-#include <ui/gfx/rect_f.h>
+#include <ui/gfx/geometry/rect_f.h>
 #endif
 
 namespace blink {
--- third_party/WebKit/public/platform/WebFloatSize.h
+++ third_party/WebKit/public/platform/WebFloatSize.h
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #else
 #include <cmath>
 #include <ui/gfx/geometry/size_f.h>
-#include <ui/gfx/vector2d_f.h>
+#include <ui/gfx/geometry/vector2d_f.h>
 #endif
 
 namespace blink {
