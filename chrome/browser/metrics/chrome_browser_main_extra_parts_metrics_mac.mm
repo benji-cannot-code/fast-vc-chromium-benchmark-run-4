0a0ce35c818b013dfd355c1d3cc2684cf16d0249
erikchen <erikchen@chromium.org>
2014-10-27 16:38:55 -0700

    mac: Update metrics for Fullscreen styles.
    I removed the metric "OSX.Fullscreen.Enter" because the confusion it was
    causing outweighted the additional information it provided.
    
    It has been replaced by 3 metrics:
      - OSX.Fullscreen.Enter.Style
      - OSX.Fullscreen.Enter.WindowLocation
      - OSX.Settings.ScreensHaveSeparateSpaces
    
    BUG=414527
    
    Review URL: https://codereview.chromium.org/640053007
    
    Cr-Commit-Position: refs/heads/master@{#301484}

---
+++ chrome/browser/metrics/chrome_browser_main_extra_parts_metrics_mac.mm
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.h"
+
+#include "base/mac/sdk_forward_declarations.h"
+#include "base/metrics/histogram.h"
+
+namespace {
+
+// The possible values of the setting "Screens Have Separate Spaces".
+enum ScreenSpacesConfiguration {
+  SCREENS_CANNOT_HAVE_SEPARATE_SPACES = 0,
+  SCREENS_HAVE_SEPARATE_SPACES = 1,
+  SCREENS_HAVE_SHARED_SPACES = 2,
+  SCREEN_SPACES_CONFIGURATION_COUNT = 3
+};
+
+}  // namespace
+
+void ChromeBrowserMainExtraPartsMetrics::RecordMacMetrics() {
+  ScreenSpacesConfiguration separate_spaces =
+      SCREENS_CANNOT_HAVE_SEPARATE_SPACES;
+
+  if ([NSScreen respondsToSelector:@selector(screensHaveSeparateSpaces)]) {
+    BOOL screens_have_separate_spaces = [NSScreen screensHaveSeparateSpaces];
+    separate_spaces = screens_have_separate_spaces
+                          ? SCREENS_HAVE_SEPARATE_SPACES
+                          : SCREENS_HAVE_SHARED_SPACES;
+  }
+  UMA_HISTOGRAM_ENUMERATION("OSX.Settings.ScreensHaveSeparateSpaces",
+                            separate_spaces,
+                            SCREEN_SPACES_CONFIGURATION_COUNT);
+}
