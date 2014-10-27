c8e020c2c335bf62e26ae6e5c51955ffa51842dc
garykac <garykac@chromium.org>
2014-10-27 10:16:22 -0700

    [Chromoting] Update size to use inner/outer bounds
    BUG=
    
    Review URL: https://codereview.chromium.org/654393005
    
    Cr-Commit-Position: refs/heads/master@{#301388}

--- remoting/webapp/js_proto/chrome_proto.js
+++ remoting/webapp/js_proto/chrome_proto.js
@@ -365,6 +365,10 @@ var AppWindow = function() {
   this.onFullscreened = null;
   /** @type {string} */
   this.id = '';
+  /** @type {Bounds} */
+  this.outerBounds = null;
+  /** @type {Bounds} */
+  this.innerBounds = null;
 };
 
 AppWindow.prototype.close = function() {};
@@ -398,6 +402,18 @@ var LaunchData = function() {
   this.id = '';
   /** @type {Array.<{type: string, entry: FileEntry}>} */
   this.items = [];
+};
+
+/** @constructor */
+function Bounds() {
+  /** @type {number} */
+  this.left = 0;
+  /** @type {number} */
+  this.top = 0;
+  /** @type {number} */
+  this.width = 0;
+  /** @type {number} */
+  this.height = 0;
 };
 
 /** @constructor */
