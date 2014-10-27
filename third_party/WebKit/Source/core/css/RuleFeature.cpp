b1a3e762dced92d3bf555b902e8095be3096119b
rune@opera.com <rune@opera.com>
2014-10-27 10:35:23 +0000

    Custom pseudo elements do not require sub-tree recalcs.
    Custom pseudo elements are already invalidation set features and handled
    in invalidationSetModeForSelector and extractInvalidationSetFeature.
    
    TEST=fast/css/invalidation/targeted-class-custom-pseudo.html
    
    R=chrishtr@chromium.org,esprehn@chromium.org
    
    Review URL: https://codereview.chromium.org/669643003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184438 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/RuleFeature.cpp
+++ third_party/WebKit/Source/core/css/RuleFeature.cpp
@@ -123,6 +123,7 @@ static bool supportsInvalidation(CSSSelector::PseudoType type)
     case CSSSelector::PseudoSelection:
     case CSSSelector::PseudoInRange:
     case CSSSelector::PseudoOutOfRange:
+    case CSSSelector::PseudoWebKitCustomElement:
     case CSSSelector::PseudoUnresolved:
     case CSSSelector::PseudoContent:
     case CSSSelector::PseudoHost:
@@ -171,7 +172,6 @@ static bool requiresSubtreeInvalidation(const CSSSelector& selector)
     case CSSSelector::PseudoFullScreen:
     case CSSSelector::PseudoFullScreenDocument:
     case CSSSelector::PseudoFullScreenAncestor:
-    case CSSSelector::PseudoWebKitCustomElement:
     case CSSSelector::PseudoCue:
     case CSSSelector::PseudoFutureCue:
     case CSSSelector::PseudoPastCue:
