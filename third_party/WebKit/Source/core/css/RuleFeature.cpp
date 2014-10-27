36488cfe3c8b167162d6710db6836fed544f419c
rune@opera.com <rune@opera.com>
2014-10-27 12:18:08 +0000

    Avoid sub-tree style recalc for ::-webkit-resizer.
    The presence of the ::-webkit-resizer pseudo element should not trigger a
    sub-tree style recalc from style invalidation.
    
    This CL depends on https://codereview.chromium.org/639293008/
    
    R=esprehn@chromium.org,chrishtr@chromium.org
    BUG=425557
    
    Review URL: https://codereview.chromium.org/663313004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184442 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/RuleFeature.cpp
+++ third_party/WebKit/Source/core/css/RuleFeature.cpp
@@ -111,6 +111,7 @@ static bool supportsInvalidation(CSSSelector::PseudoType type)
     case CSSSelector::PseudoBackdrop:
     case CSSSelector::PseudoLang:
     case CSSSelector::PseudoNot:
+    case CSSSelector::PseudoResizer:
     case CSSSelector::PseudoRoot:
     case CSSSelector::PseudoScope:
     case CSSSelector::PseudoScrollbar:
@@ -161,7 +162,6 @@ static bool requiresSubtreeInvalidation(const CSSSelector& selector)
     case CSSSelector::PseudoFirstLine:
     case CSSSelector::PseudoFirstLetter:
     case CSSSelector::PseudoFullPageMedia:
-    case CSSSelector::PseudoResizer:
     case CSSSelector::PseudoCornerPresent:
     case CSSSelector::PseudoDecrement:
     case CSSSelector::PseudoIncrement:
