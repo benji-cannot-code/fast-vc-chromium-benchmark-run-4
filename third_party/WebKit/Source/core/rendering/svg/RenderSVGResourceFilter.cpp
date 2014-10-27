5d160e7c3b779ee2a7519fe44a0c7ff5437235c5
fs@opera.com <fs@opera.com>
2014-10-27 15:22:28 +0000

    Drop RenderStyle-arg. from RenderSVGResource{Filter,Masker}::prepareEffect
    It's unused in both cases.
    
    BUG=420022
    
    Review URL: https://codereview.chromium.org/674373004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184457 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/svg/RenderSVGResourceFilter.cpp
+++ third_party/WebKit/Source/core/rendering/svg/RenderSVGResourceFilter.cpp
@@ -185,7 +185,7 @@ static void drawDeferredFilter(GraphicsContext* context, FilterData* filterData,
     context->restore();
 }
 
-bool RenderSVGResourceFilter::prepareEffect(RenderObject* object, RenderStyle*, GraphicsContext*& context)
+bool RenderSVGResourceFilter::prepareEffect(RenderObject* object, GraphicsContext*& context)
 {
     ASSERT(object);
     ASSERT(context);
