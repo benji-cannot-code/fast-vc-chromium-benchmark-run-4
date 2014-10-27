37d204b77b1aaec509f0e81fa88c53548275a261
Antoine Labour <piman@chromium.org>
2014-10-27 12:04:36 -0700

    Fix windows builders when SPDY_PROXY_AUTH_VALUE is set
    TBR=kundaji
    
    BUG=427593
    
    Review URL: https://codereview.chromium.org/680103004
    
    Cr-Commit-Position: refs/heads/master@{#301403}

--- google_apis/google_api_keys.cc
+++ google_apis/google_api_keys.cc
@@ -184,8 +184,9 @@ class APIKeyCache {
   std::string GetSpdyProxyAuthValue() {
 #if defined(SPDY_PROXY_AUTH_VALUE)
     return SPDY_PROXY_AUTH_VALUE;
-#endif
+#else
     return std::string();
+#endif
   }
 
  private:
