5484eba2a46fc0b7891016ce6b78e555aa56434e
estade <estade@chromium.org>
2014-10-28 00:25:45 -0700

    Autofill - Add Kohls test case
    "Address lookup" messes up the address parsing.
    The card type <select> also fails for some reason.
    
    TBR=isherman@chromium.org
    BUG=427622
    
    Review URL: https://codereview.chromium.org/685493002
    
    Cr-Commit-Position: refs/heads/master@{#301587}

--- chrome/browser/autofill/form_structure_browsertest.cc
+++ chrome/browser/autofill/form_structure_browsertest.cc
@@ -282,4 +282,13 @@ IN_PROC_BROWSER_TEST_F(FormStructureBrowserTest,
                     kFileNamePattern);
 }
 
+IN_PROC_BROWSER_TEST_F(FormStructureBrowserTest,
+    MAYBE_DataDrivenHeuristics(22)) {
+  const base::FilePath::CharType kFileNamePattern[] =
+      FILE_PATH_LITERAL("22_*.html");
+  RunDataDrivenTest(GetInputDirectory(kTestName),
+                    GetOutputDirectory(kTestName),
+                    kFileNamePattern);
+}
+
 }  // namespace autofill
