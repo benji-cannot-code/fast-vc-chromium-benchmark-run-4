757ffb7d7ed796f01c8bda6ef42a25938119a600
magjed <magjed@chromium.org>
2014-10-28 08:18:23 -0700

    Revert of: https://codereview.chromium.org/631903003
    The reason for reverting is that this CL is not compatible with: https://codereview.chromium.org/653873003/
    
    Issue 653873003 will also free up processing time on the main render thread, making this CL unnecessary.
    
    TBR=bbudge@chromium.org
    BUG=421554
    
    Review URL: https://codereview.chromium.org/685683002
    
    Cr-Commit-Position: refs/heads/master@{#301634}

--- content/renderer/pepper/pepper_video_destination_host.h
+++ content/renderer/pepper/pepper_video_destination_host.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_RENDERER_PEPPER_PEPPER_VIDEO_DESTINATION_HOST_H_
 #define CONTENT_RENDERER_PEPPER_PEPPER_VIDEO_DESTINATION_HOST_H_
 
-#include <string>
-
 #include "base/compiler_specific.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
@@ -43,7 +41,7 @@ class CONTENT_EXPORT PepperVideoDestinationHost
 
   RendererPpapiHost* renderer_ppapi_host_;
 
-  VideoDestinationHandler::FrameWriterCallback frame_writer_;
+  scoped_ptr<FrameWriterInterface> frame_writer_;
 
   base::WeakPtrFactory<PepperVideoDestinationHost> weak_factory_;
 
