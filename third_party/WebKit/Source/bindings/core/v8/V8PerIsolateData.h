6b120eebc883735145e9e2c06214e1c34691545c
yurys@chromium.org <yurys@chromium.org>
2014-10-28 15:38:48 +0000

    Fix reporting of simultaneous uncaught exceptions in workers
    Static flag isReportingException in messageHandlerInWorker was replaced with a per isolate one. The test was failing consisitently on my machine before the fix and now it passes.
    
    BUG=425960
    
    Review URL: https://codereview.chromium.org/684763003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184532 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/core/v8/V8PerIsolateData.h
+++ third_party/WebKit/Source/bindings/core/v8/V8PerIsolateData.h
@@ -76,6 +76,9 @@ public:
     bool isHandlingRecursionLevelError() const { return m_isHandlingRecursionLevelError; }
     void setIsHandlingRecursionLevelError(bool value) { m_isHandlingRecursionLevelError = value; }
 
+    bool isReportingException() const { return m_isReportingException; }
+    void setReportingException(bool value) { m_isReportingException = value; }
+
     bool performingMicrotaskCheckpoint() const { return m_performingMicrotaskCheckpoint; }
     void setPerformingMicrotaskCheckpoint(bool performingMicrotaskCheckpoint) { m_performingMicrotaskCheckpoint = performingMicrotaskCheckpoint; }
 
@@ -128,6 +131,7 @@ private:
 
     int m_recursionLevel;
     bool m_isHandlingRecursionLevelError;
+    bool m_isReportingException;
 
 #if ENABLE(ASSERT)
     int m_internalScriptRecursionLevel;
