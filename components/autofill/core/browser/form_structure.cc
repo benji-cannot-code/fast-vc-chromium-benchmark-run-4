c0d3951ade45f1582fb27d88bf7bbaf7ead08cf3
gcasto <gcasto@chromium.org>
2014-10-27 17:45:43 -0700

    [Password Generation] Always query password forms via Autofill
    For other fields, we assume that if the author has specified autocomplete
    attributes we should respect all their choices, including not trying to
    determine field autocomplete values for fields that the author didn't specify.
    
    This doesn't apply for password fields for two reasons. First, there is no way
    for an author to specify generation for a particular field. Second, since
    generation isn't a specced autocomplete value, the password field can reasonably
    be set (e.g. "autocomplete=new-password") and also have generation enabled.
    
    BUG=424793
    
    Review URL: https://codereview.chromium.org/659793005
    
    Cr-Commit-Position: refs/heads/master@{#301521}

--- components/autofill/core/browser/form_structure.cc
+++ components/autofill/core/browser/form_structure.cc
@@ -353,7 +353,8 @@ FormStructure::FormStructure(const FormData& form)
       autofill_count_(0),
       active_field_count_(0),
       upload_required_(USE_UPLOAD_RATES),
-      has_author_specified_types_(false) {
+      has_author_specified_types_(false),
+      has_password_field_(false) {
   // Copy the form fields.
   std::map<base::string16, size_t> unique_names;
   for (std::vector<FormFieldData>::const_iterator field =
@@ -367,6 +368,9 @@ FormStructure::FormStructure(const FormData& form)
 
       ++active_field_count_;
     }
+
+    if (field->form_control_type == "password")
+      has_password_field_ = true;
 
     // Generate a unique name for this field by appending a counter to the name.
     // Make sure to prepend the counter with a non-numeric digit so that we are
@@ -580,20 +584,25 @@ void FormStructure::ParseQueryResponse(
       if (current_info == field_infos.end())
         break;
 
-      // UNKNOWN_TYPE is reserved for use by the client.
-      DCHECK_NE(current_info->field_type, UNKNOWN_TYPE);
+      // If |form->has_author_specified_types| only password fields should be
+      // updated.
+      if (!form->has_author_specified_types_ ||
+          (*field)->form_control_type == "password") {
+        // UNKNOWN_TYPE is reserved for use by the client.
+        DCHECK_NE(current_info->field_type, UNKNOWN_TYPE);
 
-      ServerFieldType heuristic_type = (*field)->heuristic_type();
-      if (heuristic_type != UNKNOWN_TYPE)
-        heuristics_detected_fillable_field = true;
+        ServerFieldType heuristic_type = (*field)->heuristic_type();
+        if (heuristic_type != UNKNOWN_TYPE)
+          heuristics_detected_fillable_field = true;
 
-      (*field)->set_server_type(current_info->field_type);
-      if (heuristic_type != (*field)->Type().GetStorableType())
-        query_response_overrode_heuristics = true;
+        (*field)->set_server_type(current_info->field_type);
+        if (heuristic_type != (*field)->Type().GetStorableType())
+          query_response_overrode_heuristics = true;
 
-      // Copy default value into the field if available.
-      if (!current_info->default_value.empty())
-        (*field)->set_default_value(current_info->default_value);
+        // Copy default value into the field if available.
+        if (!current_info->default_value.empty())
+          (*field)->set_default_value(current_info->default_value);
+      }
 
       ++current_info;
     }
@@ -707,7 +716,8 @@ bool FormStructure::ShouldBeParsed() const {
 }
 
 bool FormStructure::ShouldBeCrowdsourced() const {
-  return !has_author_specified_types_ && ShouldBeParsed();
+  return (has_password_field_ || !has_author_specified_types_) &&
+      ShouldBeParsed();
 }
 
 void FormStructure::UpdateFromCache(const FormStructure& cached_form) {
