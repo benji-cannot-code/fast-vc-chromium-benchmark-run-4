7230c3311f8a7314e41bf33cb33baebdaff4455a
vadimt <vadimt@chromium.org>
2014-10-27 17:13:32 -0700

    Adding instrumentation to locate the source of jankiness.
    Mechanical change that adds instrumentation required to locate the source of jankiness (i.e. a long-running fragment of code executed as a part of the task that causes jank) in the code. See the bug for details on what kind of jank we are after.
    A number of similar CLs were landed, and none of them caused issues. They've helped to find and fix janky code. The code of the instrumentation is highly optimized and is not expected to affect performance. The code simply creates a diagnostic task which is identical to ones created by PostTask or IPC message handlers.
    Landing as TBR since this is a mechanical, safe and temporary change.
    
    This CL instruments code invoked from HttpStreamParser::OnIOComplete's callback, which is responsible for 16.2 jph
    
    BUG=424359
    TBR=mmenke@chromium.org
    
    Review URL: https://codereview.chromium.org/675273004
    
    Cr-Commit-Position: refs/heads/master@{#301504}

--- net/http/http_network_transaction.cc
+++ net/http/http_network_transaction.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/field_trial.h"
 #include "base/metrics/histogram.h"
 #include "base/metrics/stats_counters.h"
+#include "base/profiler/scoped_profile.h"
 #include "base/stl_util.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
@@ -598,7 +599,18 @@ void HttpNetworkTransaction::DoCallback(int rv) {
 }
 
 void HttpNetworkTransaction::OnIOComplete(int result) {
+  // TODO(vadimt): Remove ScopedProfile below once crbug.com/424359 is fixed.
+  tracked_objects::ScopedProfile tracking_profile1(
+      FROM_HERE_WITH_EXPLICIT_FUNCTION(
+          "424359 HttpNetworkTransaction::OnIOComplete 1"));
+
   int rv = DoLoop(result);
+
+  // TODO(vadimt): Remove ScopedProfile below once crbug.com/424359 is fixed.
+  tracked_objects::ScopedProfile tracking_profile2(
+      FROM_HERE_WITH_EXPLICIT_FUNCTION(
+          "424359 HttpNetworkTransaction::OnIOComplete 2"));
+
   if (rv != ERR_IO_PENDING)
     DoCallback(rv);
 }
