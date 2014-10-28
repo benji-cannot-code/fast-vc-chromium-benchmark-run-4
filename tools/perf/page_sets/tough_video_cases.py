c2b7b6eb7f4f2a26b1e6b640ef332f8bfe74d264
nednguyen <nednguyen@google.com>
2014-10-27 19:22:39 -0700

    [Telemetry] Add labels attribute to page as the new way to set label
    BUG=418375
    
    Review URL: https://codereview.chromium.org/686623002
    
    Cr-Commit-Position: refs/heads/master@{#301552}

--- tools/perf/page_sets/tough_video_cases.py
+++ tools/perf/page_sets/tough_video_cases.py
@@ -8,8 +8,9 @@ from telemetry.page import page_set as page_set_module
 
 class ToughVideoCasesPage(page_module.Page):
 
-  def __init__(self, url, page_set):
-    super(ToughVideoCasesPage, self).__init__(url=url, page_set=page_set)
+  def __init__(self, url, page_set, labels=None):
+    super(ToughVideoCasesPage, self).__init__(
+        url=url, page_set=page_set, labels=labels)
 
   def LoopMixedAudio(self, action_runner):
     action_runner.PlayMedia(selector='#background_audio',
@@ -81,10 +82,9 @@ class Page4(ToughVideoCasesPage):
   def __init__(self, page_set):
     super(Page4, self).__init__(
       url='file://tough_video_cases/video.html?src=crowd1080.webm',
-      page_set=page_set)
+      page_set=page_set, labels=['is_50fps'])
 
     self.add_browser_metrics = True
-    self.is_50fps = True
 
   def RunPageInteractions(self, action_runner):
     self.PlayAction(action_runner)
@@ -95,11 +95,9 @@ class Page5(ToughVideoCasesPage):
   def __init__(self, page_set):
     super(Page5, self).__init__(
       url='file://tough_video_cases/video.html?src=crowd2160.ogv',
-      page_set=page_set)
+      page_set=page_set, labels=['is_4k', 'is_50fps'])
 
     self.add_browser_metrics = True
-    self.is_4k = True
-    self.is_50fps = True
 
   def RunPageInteractions(self, action_runner):
     self.PlayAction(action_runner)
@@ -110,11 +108,9 @@ class Page6(ToughVideoCasesPage):
   def __init__(self, page_set):
     super(Page6, self).__init__(
       url='file://tough_video_cases/video.html?src=crowd2160.webm',
-      page_set=page_set)
+      page_set=page_set, labels=['is_4k', 'is_50fps'])
 
     self.add_browser_metrics = True
-    self.is_4k = True
-    self.is_50fps = True
 
   def RunPageInteractions(self, action_runner):
     self.PlayAction(action_runner)
@@ -177,10 +173,9 @@ class Page11(ToughVideoCasesPage):
   def __init__(self, page_set):
     super(Page11, self).__init__(
       url='file://tough_video_cases/video.html?src=crowd1080.mp4',
-      page_set=page_set)
+      page_set=page_set, labels=['is_50fps'])
 
     self.add_browser_metrics = True
-    self.is_50fps = True
 
   def RunPageInteractions(self, action_runner):
     self.PlayAction(action_runner)
@@ -191,11 +186,9 @@ class Page12(ToughVideoCasesPage):
   def __init__(self, page_set):
     super(Page12, self).__init__(
       url='file://tough_video_cases/video.html?src=crowd2160.mp4',
-      page_set=page_set)
+      page_set=page_set, labels=['is_4k', 'is_50fps'])
 
     self.add_browser_metrics = True
-    self.is_4k = True
-    self.is_50fps = True
 
   def RunPageInteractions(self, action_runner):
     self.PlayAction(action_runner)
@@ -245,9 +238,8 @@ class Page16(ToughVideoCasesPage):
   def __init__(self, page_set):
     super(Page16, self).__init__(
       url='file://tough_video_cases/video.html?src=garden2_10s.webm',
-      page_set=page_set)
+      page_set=page_set, labels=['is_4k'])
 
-    self.is_4k = True
     self.add_browser_metrics = True
 
   def RunPageInteractions(self, action_runner):
@@ -259,9 +251,8 @@ class Page17(ToughVideoCasesPage):
   def __init__(self, page_set):
     super(Page17, self).__init__(
       url='file://tough_video_cases/video.html?src=garden2_10s.mp4',
-      page_set=page_set)
+      page_set=page_set, labels=['is_4k'])
 
-    self.is_4k = True
     self.add_browser_metrics = True
 
   def RunPageInteractions(self, action_runner):
@@ -273,9 +264,8 @@ class Page18(ToughVideoCasesPage):
   def __init__(self, page_set):
     super(Page18, self).__init__(
       url='file://tough_video_cases/video.html?src=garden2_10s.ogv',
-      page_set=page_set)
+      page_set=page_set, labels=['is_4k'])
 
-    self.is_4k = True
     self.add_browser_metrics = True
 
   def RunPageInteractions(self, action_runner):
@@ -365,10 +355,9 @@ class Page25(ToughVideoCasesPage):
   def __init__(self, page_set):
     super(Page25, self).__init__(
       url='file://tough_video_cases/video.html?src=garden2_10s.webm',
-      page_set=page_set)
+      page_set=page_set, labels=['is_4k'])
 
     self.skip_basic_metrics = True
-    self.is_4k = True
 
   def RunPageInteractions(self, action_runner):
     self.SeekBeforeAndAfterPlayhead(action_runner)
@@ -379,10 +368,9 @@ class Page26(ToughVideoCasesPage):
   def __init__(self, page_set):
     super(Page26, self).__init__(
       url='file://tough_video_cases/video.html?src=garden2_10s.mp4',
-      page_set=page_set)
+      page_set=page_set, labels=['is_4k'])
 
     self.skip_basic_metrics = True
-    self.is_4k = True
 
   def RunPageInteractions(self, action_runner):
     self.SeekBeforeAndAfterPlayhead(action_runner)
@@ -393,10 +381,9 @@ class Page27(ToughVideoCasesPage):
   def __init__(self, page_set):
     super(Page27, self).__init__(
       url='file://tough_video_cases/video.html?src=garden2_10s.ogv',
-      page_set=page_set)
+      page_set=page_set, labels=['is_4k'])
 
     self.skip_basic_metrics = True
-    self.is_4k = True
 
   def RunPageInteractions(self, action_runner):
     self.SeekBeforeAndAfterPlayhead(action_runner)
