6e5055d628f1087fb70b2af2c86a71d1615c8cc3
kouhei@chromium.org <kouhei@chromium.org>
2014-10-28 08:20:17 +0000

    Uninline PendingScript
    - PendingScirpt has a RefPtr to ScriptStreamer, but ScriptStreamer is not
      included from PendingScript.
    - PendingScript has a inline Ctor and assignment operator=, which requires
      complete ScriptStreamer definition.
    
    These cause implementations including only PendingScript.h to fail.
    
    However, we can't naively include ScriptStreamer.h in PendingScript.h, as
    ScriptStreamer declaration depends on PendingScript.
    
    This CL tries to fix this issue by uninlining the methods which touches
    m_streamer implicitly/explicitly.
    
    BUG=356292, 421300
    
    Review URL: https://codereview.chromium.org/686683002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184498 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/PendingScript.cpp
+++ third_party/WebKit/Source/core/dom/PendingScript.cpp
@@ -34,8 +34,44 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+PendingScript::PendingScript()
+    : m_watchingForLoad(false)
+    , m_startingPosition(TextPosition::belowRangePosition())
+{
+}
+
+PendingScript::PendingScript(Element* element, ScriptResource* resource)
+    : m_watchingForLoad(false)
+    , m_element(element)
+{
+    setScriptResource(resource);
+}
+
+PendingScript::PendingScript(const PendingScript& other)
+    : ResourceOwner(other)
+    , m_watchingForLoad(other.m_watchingForLoad)
+    , m_element(other.m_element)
+    , m_startingPosition(other.m_startingPosition)
+    , m_streamer(other.m_streamer)
+{
+    setScriptResource(other.resource());
+}
+
 PendingScript::~PendingScript()
 {
+}
+
+PendingScript& PendingScript::operator=(const PendingScript& other)
+{
+    if (this == &other)
+        return *this;
+
+    m_watchingForLoad = other.m_watchingForLoad;
+    m_element = other.m_element;
+    m_startingPosition = other.m_startingPosition;
+    m_streamer = other.m_streamer;
+    this->ResourceOwner<ScriptResource, ScriptResourceClient>::operator=(other);
+    return *this;
 }
 
 void PendingScript::watchForLoad(ScriptResourceClient* client)
@@ -111,6 +147,13 @@ ScriptSourceCode PendingScript::getSource(const KURL& documentURL, bool& errorOc
     }
     errorOccurred = false;
     return ScriptSourceCode(m_element->textContent(), documentURL, startingPosition());
+}
+
+void PendingScript::setStreamer(PassRefPtr<ScriptStreamer> streamer)
+{
+    ASSERT(!m_streamer);
+    ASSERT(!m_watchingForLoad);
+    m_streamer = streamer;
 }
 
 bool PendingScript::isReady() const
