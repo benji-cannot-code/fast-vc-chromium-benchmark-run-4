61973304ec0e0da7137ed484e746336803fc4435
sammc <sammc@chromium.org>
2014-10-27 18:15:33 -0700

    Fix a bug in data_sender.js.
    This fixes a bug where data sends that had been fully written to the
    data pipe were not considered when deciding whether to cancel (or wait
    for an in-progress cancel) during DataSender serialization.
    
    Review URL: https://codereview.chromium.org/678223002
    
    Cr-Commit-Position: refs/heads/master@{#301537}

--- extensions/renderer/resources/data_sender.js
+++ extensions/renderer/resources/data_sender.js
@@ -297,7 +297,7 @@ define('data_sender', [
       return Promise.resolve(null);
 
     var readyToSerialize = Promise.resolve();
-    if (this.pendingSends_.length) {
+    if (this.pendingSends_.length || this.sendsAwaitingAck_.length) {
       if (this.pendingCancel_)
         readyToSerialize = this.cancelPromise_;
       else
