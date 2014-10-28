65fe4185a30576348ef0ca35674531169b80486f
nhiroki@chromium.org <nhiroki@chromium.org>
2014-10-28 13:48:28 +0000

    ServiceWorker: Remove the obsolete "parsed" state [3/3]
    Spec: https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-state
    
    [1] Blink: https://codereview.chromium.org/678253002/
    [2] Chromium: https://codereview.chromium.org/685503004/
    [3] Blink: THIS PATCH
    
    BUG=424081
    TEST=run-webkit-tests --debug http/tests/serviceworker/
    
    Review URL: https://codereview.chromium.org/683733002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184520 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebServiceWorkerState.h
+++ third_party/WebKit/public/platform/WebServiceWorkerState.h
@@ -10,7 +10,6 @@ namespace blink {
 
 enum WebServiceWorkerState {
     WebServiceWorkerStateUnknown,
-    WebServiceWorkerStateParsed,
     WebServiceWorkerStateInstalling,
     WebServiceWorkerStateInstalled,
     WebServiceWorkerStateActivating,
