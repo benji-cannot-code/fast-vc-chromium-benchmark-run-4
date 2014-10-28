f3fcc1dcefc938af1eccb8300909367c978b2016
Hans Wennborg <hans@chromium.org>
2014-10-27 18:06:01 -0700

    Don't dllexport anonymous class GestureConfigurationAura
    BUG=82385
    R=jdduke@chromium.org
    
    Review URL: https://codereview.chromium.org/684643003
    
    Cr-Commit-Position: refs/heads/master@{#301529}

--- ui/events/gesture_detection/gesture_configuration_aura.cc
+++ ui/events/gesture_detection/gesture_configuration_aura.cc
@@ -11,8 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ui {
 namespace {
 
-class GESTURE_DETECTION_EXPORT GestureConfigurationAura
-    : public GestureConfiguration {
+class GestureConfigurationAura : public GestureConfiguration {
  public:
   ~GestureConfigurationAura() override {
   }
