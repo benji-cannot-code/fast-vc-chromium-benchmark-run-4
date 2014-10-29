6c5476826b4760b9d1e953767fb695adf5911498
dcheng <dcheng@chromium.org>
2014-10-28 17:16:39 -0700

    Standardize usage of virtual/override/final specifiers.
    The Google C++ style guide states:
    
      Explicitly annotate overrides of virtual functions or virtual
      destructors with an override or (less frequently) final specifier.
      Older (pre-C++11) code will use the virtual keyword as an inferior
      alternative annotation. For clarity, use exactly one of override,
      final, or virtual when declaring an override.
    
    To better conform to these guidelines, the following constructs have
    been rewritten:
    
    - if a base class has a virtual destructor, then:
        virtual ~Foo();                   ->  ~Foo() override;
    - virtual void Foo() override;        ->  void Foo() override;
    - virtual void Foo() override final;  ->  void Foo() final;
    
    This patch was automatically generated. The clang plugin can generate
    fixit hints, which are suggested edits when it is 100% sure it knows how
    to fix a problem. The hints from the clang plugin were applied to the
    source tree using the tool in https://codereview.chromium.org/598073004.
    
    BUG=417463
    R=vitalybuka@chromium.org
    
    Review URL: https://codereview.chromium.org/679233003
    
    Cr-Commit-Position: refs/heads/master@{#301741}

--- chrome/browser/local_discovery/service_discovery_client_mdns.cc
+++ chrome/browser/local_discovery/service_discovery_client_mdns.cc
@@ -112,7 +112,7 @@ class SocketFactory : public net::MDnsSocketFactory {
       : interfaces_(interfaces) {}
 
   // net::MDnsSocketFactory implementation:
-  virtual void CreateSockets(
+  void CreateSockets(
       ScopedVector<net::DatagramServerSocket>* sockets) override {
     for (size_t i = 0; i < interfaces_.size(); ++i) {
       DCHECK(interfaces_[i].second == net::ADDRESS_FAMILY_IPV4 ||
@@ -188,14 +188,14 @@ class ServiceWatcherProxy : public ProxyBase<ServiceWatcher> {
   }
 
   // ServiceWatcher methods.
-  virtual void Start() override {
+  void Start() override {
     if (implementation()) {
       PostToMdnsThread(base::Bind(&ServiceWatcher::Start,
                                   base::Unretained(implementation())));
     }
   }
 
-  virtual void DiscoverNewServices(bool force_update) override {
+  void DiscoverNewServices(bool force_update) override {
     if (implementation()) {
       PostToMdnsThread(base::Bind(&ServiceWatcher::DiscoverNewServices,
                                   base::Unretained(implementation()),
@@ -203,8 +203,7 @@ class ServiceWatcherProxy : public ProxyBase<ServiceWatcher> {
     }
   }
 
-  virtual void SetActivelyRefreshServices(
-      bool actively_refresh_services) override {
+  void SetActivelyRefreshServices(bool actively_refresh_services) override {
     if (implementation()) {
       PostToMdnsThread(base::Bind(&ServiceWatcher::SetActivelyRefreshServices,
                                   base::Unretained(implementation()),
@@ -212,11 +211,9 @@ class ServiceWatcherProxy : public ProxyBase<ServiceWatcher> {
     }
   }
 
-  virtual std::string GetServiceType() const override {
-    return service_type_;
-  }
+  std::string GetServiceType() const override { return service_type_; }
 
-  virtual void OnNewMdnsReady() override {
+  void OnNewMdnsReady() override {
     ProxyBase<ServiceWatcher>::OnNewMdnsReady();
     if (!implementation())
       callback_.Run(ServiceWatcher::UPDATE_INVALIDATED, "");
@@ -251,16 +248,14 @@ class ServiceResolverProxy : public ProxyBase<ServiceResolver> {
   }
 
   // ServiceResolver methods.
-  virtual void StartResolving() override {
+  void StartResolving() override {
     if (implementation()) {
       PostToMdnsThread(base::Bind(&ServiceResolver::StartResolving,
                                   base::Unretained(implementation())));
     }
   };
 
-  virtual std::string GetName() const override {
-    return service_name_;
-  }
+  std::string GetName() const override { return service_name_; }
 
  private:
   static void OnCallback(
@@ -295,7 +290,7 @@ class LocalDomainResolverProxy : public ProxyBase<LocalDomainResolver> {
   }
 
   // LocalDomainResolver methods.
-  virtual void Start() override {
+  void Start() override {
     if (implementation()) {
       PostToMdnsThread(base::Bind(&LocalDomainResolver::Start,
                                   base::Unretained(implementation())));
