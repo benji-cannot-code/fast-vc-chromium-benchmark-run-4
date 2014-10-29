63318d7e78faceeaee24141f0525e22019d6b153
yfriedman <yfriedman@chromium.org>
2014-10-28 17:28:49 -0700

    Remove TestTouchUtils.clickById.
    It's inherently a bad API as it relies on TouchUtils.clickView which
    simulates clicks on the screen. That approach can lead to INJECT_EVENTS
    issues if the screen turns off or a view pops in front of chrome.  The
    alternative is to use event propagation to a specific view such as in
    TouchCommon.singleClickView
    
    BUG=427031
    
    Review URL: https://codereview.chromium.org/678423004
    
    Cr-Commit-Position: refs/heads/master@{#301744}

--- content/public/test/android/javatests/src/org/chromium/content/browser/test/util/TestTouchUtils.java
+++ content/public/test/android/javatests/src/org/chromium/content/browser/test/util/TestTouchUtils.java
@@ -5,10 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.content.browser.test.util;
 
-import android.app.Activity;
 import android.app.Instrumentation;
 import android.os.SystemClock;
-import android.test.InstrumentationTestCase;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
@@ -210,20 +208,5 @@ public class TestTouchUtils extends android.test.TouchUtils {
                 v.performClick();
             }
         });
-    }
-
-    /**
-     * Clicks on specified view in the given {@link Activity}.
-     *
-     * @param test The test case using this utility.
-     * @param activity Activity containing the view.
-     * @param id The view to be clicked.
-     * @return {@code true} if a view with the given id exists.
-     */
-    public static boolean clickById(InstrumentationTestCase test, Activity activity, int id) {
-        View v = activity.findViewById(id);
-        if (v == null) return false;
-        clickView(test, v);
-        return true;
     }
 }
