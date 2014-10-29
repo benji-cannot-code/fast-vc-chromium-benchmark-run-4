e798310d7849dcd662a5e587e018a49dcfe84555
wolenetz <wolenetz@chromium.org>
2014-10-29 11:57:40 -0700

    Add --goma_threads option to bisection tools
    Lets user of run-bisect-manual-test.py specify a non-default value for
    number of goma threads, and hooks this into Builder used by
    bisect_perf_regression.py.
    
    Example usage:
    python tools/run-bisect-manual-test.py --browser=release -b 100 -g 50 -w ../../tmp --goma_threads 50
    
    R=qyearsley@chromium.org
    TEST=script passes through specified value that defaults to 64
    Bug=none
    
    Review URL: https://codereview.chromium.org/679353002
    
    Cr-Commit-Position: refs/heads/master@{#301896}

--- tools/auto_bisect/builder.py
+++ tools/auto_bisect/builder.py
@@ -125,7 +125,7 @@ class DesktopBuilder(Builder):
 
     threads = None
     if opts.use_goma:
-      threads = 64
+      threads = opts.goma_threads
 
     build_success = False
     if opts.build_preference == 'make':
@@ -165,7 +165,7 @@ class AndroidBuilder(Builder):
     """
     threads = None
     if opts.use_goma:
-      threads = 64
+      threads = opts.goma_threads
 
     build_success = False
     if opts.build_preference == 'ninja':
