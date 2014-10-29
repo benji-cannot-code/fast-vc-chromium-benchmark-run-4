f95176d8bfcbc86e16768a92a3573f5292462981
yawano <yawano@chromium.org>
2014-10-28 19:33:58 -0700

    Reland "Fixed memory leak of Changed api to notify when watched directory is deleted"
    This is reland of Issue 658013002: Changed api to notify when watched directory is deleted.
    
    Original Description:
    Changed api to notify when watched directory is deleted.
    
    BUG=423257
    TEST=out/Release/browser_tests --gtest_filter=FileManagerPrivateApiTest.OnFileChanged
    TBR=mtomasz,yoshiki
    
    Review URL: https://codereview.chromium.org/679383002
    
    Cr-Commit-Position: refs/heads/master@{#301776}

--- chrome/browser/chromeos/extensions/file_manager/event_router.h
+++ chrome/browser/chromeos/extensions/file_manager/event_router.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 #include <string>
+#include <vector>
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
@@ -58,6 +59,12 @@ class EventRouter : public KeyedService,
                     public VolumeManagerObserver,
                     public content::NotificationObserver {
  public:
+  typedef base::Callback<void(const base::FilePath& virtual_path,
+                              const drive::FileChange* list,
+                              bool got_error,
+                              const std::vector<std::string>& extension_ids)>
+      DispatchDirectoryChangeEventImplCallback;
+
   explicit EventRouter(Profile* profile);
   virtual ~EventRouter();
 
@@ -133,6 +140,10 @@ class EventRouter : public KeyedService,
                        const content::NotificationSource& source,
                        const content::NotificationDetails& details) override;
 
+  // Set custom dispatch directory change event implementation for testing.
+  void SetDispatchDirectoryChangeEventImplForTesting(
+      const DispatchDirectoryChangeEventImplCallback& callback);
+
  private:
   typedef std::map<base::FilePath, FileWatcher*> WatcherMap;
 
@@ -149,6 +160,13 @@ class EventRouter : public KeyedService,
 
   // Sends directory change event.
   void DispatchDirectoryChangeEvent(
+      const base::FilePath& path,
+      const drive::FileChange* list,
+      bool got_error,
+      const std::vector<std::string>& extension_ids);
+
+  // Default implementation of DispatchDirectoryChangeEvent.
+  void DispatchDirectoryChangeEventImpl(
       const base::FilePath& path,
       const drive::FileChange* list,
       bool got_error,
@@ -211,9 +229,13 @@ class EventRouter : public KeyedService,
 
   scoped_ptr<DeviceEventRouter> device_event_router_;
 
+  DispatchDirectoryChangeEventImplCallback
+      dispatch_directory_change_event_impl_;
+
   // Note: This should remain the last member so it'll be destroyed and
   // invalidate the weak pointers before any other members are destroyed.
   base::WeakPtrFactory<EventRouter> weak_factory_;
+
   DISALLOW_COPY_AND_ASSIGN(EventRouter);
 };
 
