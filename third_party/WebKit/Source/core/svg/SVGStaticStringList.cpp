2a6b6360354a9bedf425856ce8a3ee2540ad48fc
rhodovan.u-szeged@partner.samsung.com <rhodovan.u-szeged@partner.samsung.com>
2014-10-29 06:45:27 +0000

    Initialize SVGStaticStringLists with correct animated type.
    Until now, SVGStaticStringLists were initialized with
    AnimatedStringList animated type. This was the reason why SVG
    test attributes - with SVGStaticStringList type - were considered
    as valid animatable attributes and why we ran into a crash later.
    This patch initializes SVGStaticStringLists with AnimatedUnknown
    type to avoid any attempts to animate them later.
    
    R=fs@opera.com,kouhei@chromium.org
    BUG=427866
    
    Review URL: https://codereview.chromium.org/684833002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184567 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGStaticStringList.cpp
+++ third_party/WebKit/Source/core/svg/SVGStaticStringList.cpp
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 SVGStaticStringList::SVGStaticStringList(SVGElement* contextElement, const QualifiedName& attributeName)
-    : SVGAnimatedPropertyBase(AnimatedStringList, contextElement, attributeName)
+    : SVGAnimatedPropertyBase(AnimatedUnknown, contextElement, attributeName)
     , m_value(SVGStringList::create())
 {
     ASSERT(contextElement);
