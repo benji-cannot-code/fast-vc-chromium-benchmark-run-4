270cf532ed63f43f7bbeb35c03fb0f7e00824a8a
falken <falken@chromium.org>
2014-10-29 00:03:41 -0700

    Shutdown Service Workers before the profile is destroyed
    This fixes a crash when closing an incognito browser window that used
    Service Workers.
    
    Service Workers keep render process hosts alive. Service Workers are
    shutdown during the destruction of a profile (in ~StoragePartitionImpl).
    If the shutdown causes render process hosts to die, ExtensionService
    (and possibly other clients) receive a
    NOTIFICATION_RENDERER_PROCESS_TERMINATED and try to access the
    half-destroyed profile, causing a crash.
    
    We don't see this in a non-incognito window since the main profile is
    not destroyed until after browser shutdown, and we had special code to
    shutdown Service Workers there.
    
    This patch changes the special Service Worker shutdown code to happen
    before a profile is destroyed.
    
    BUG=419290
    
    Review URL: https://codereview.chromium.org/653633004
    
    Cr-Commit-Position: refs/heads/master@{#301800}

--- content/public/browser/service_worker_context.h
+++ content/public/browser/service_worker_context.h
@@ -13,8 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-// Represents the per-StoragePartition ServiceWorker data.  Must be used from
-// the IO thread.
+// Represents the per-StoragePartition ServiceWorker data.
 class ServiceWorkerContext {
  public:
   // https://rawgithub.com/slightlyoff/ServiceWorker/master/spec/service_worker/index.html#url-scope:
@@ -53,11 +52,6 @@ class ServiceWorkerContext {
                                        const ResultCallback& callback) = 0;
 
   // TODO(jyasskin): Provide a way to SendMessage to a Scope.
-
-  // Synchronously releases all of the RenderProcessHosts that have Service
-  // Workers running inside them, and prevents any new Service Worker instances
-  // from starting up.
-  virtual void Terminate() = 0;
 
   // Methods used in response to browsing data and quota manager requests.
   virtual void GetAllOriginsInfo(const GetUsageInfoCallback& callback) = 0;
