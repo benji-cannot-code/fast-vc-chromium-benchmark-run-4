a1666cf94a50d569d99f545aa1d0e5554a848b76
mukai <mukai@chromium.org>
2014-10-28 23:02:17 -0700

    Exclude BrowserThread if disable_nacl is specified.
    BUG=None
    TBR=jamescook@chromium.org
    TEST=manually
    
    Review URL: https://codereview.chromium.org/687903002
    
    Cr-Commit-Position: refs/heads/master@{#301791}

--- extensions/shell/browser/shell_browser_main_parts.cc
+++ extensions/shell/browser/shell_browser_main_parts.cc
@@ -56,7 +56,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using base::CommandLine;
 using content::BrowserContext;
+
+#if !defined(DISABLE_NACL)
 using content::BrowserThread;
+#endif
 
 namespace extensions {
 
