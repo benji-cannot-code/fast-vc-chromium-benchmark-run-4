54b7db7e46b1e18ae32dccfa7d49a43d129097e7
bauerb <bauerb@chromium.org>
2014-10-29 04:05:40 -0700

    Include given name in the profile information returned by the ProfileDownloader on Android.
    BUG=409803
    
    Review URL: https://codereview.chromium.org/679673003
    
    Cr-Commit-Position: refs/heads/master@{#301812}

--- chrome/browser/android/profiles/profile_downloader_android.h
+++ chrome/browser/android/profiles/profile_downloader_android.h
@@ -8,26 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <jni.h>
 
-#include <string>
-
-#include "base/strings/string16.h"
-
-class SkBitmap;
-
-// Android wrapper of the ProfileDownloader which provides access from the Java
-// layer. This class should only be accessed from the UI thread.
-class ProfileDownloaderAndroid {
- public:
-  // Registers the ProfileDownloaderAndroid's native methods through JNI.
-  static bool Register(JNIEnv* env);
-
-  static void OnProfileDownloadSuccess(
-      const std::string& account_id_,
-      const base::string16& full_name,
-      const SkBitmap& bitmap);
-
- private:
-  DISALLOW_COPY_AND_ASSIGN(ProfileDownloaderAndroid);
-};
+// Registers the ProfileDownloader's native methods through JNI.
+bool RegisterProfileDownloader(JNIEnv* env);
 
 #endif  // CHROME_BROWSER_ANDROID_PROFILES_PROFILE_DOWNLOADER_ANDROID_H_
