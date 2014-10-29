78026f842ddcc6ae65e14224e9f02416b35b1588
blundell <blundell@chromium.org>
2014-10-29 06:19:57 -0700

    Partially convert geolocation IPC to Mojo.
    This CL converts the non-permissions-related geolocation IPC to Mojo. The Mojo
    GeolocationService interface allows clients to observe location updates. In
    //content, the service and its client connect on a per-frame basis, eliminating
    the need for the tracking of multiple frames that GeolocationDispatcherHost had
    previously been doing. To handle the fact that geolocation updates can be
    paused and resumed at per-WebContents granularity, we introduce a
    GeolocationServiceContext class, which is used to scope the granularity of
    pauses and resumes.
    
    Currently, GeolocationDispatcherHost still handles permissions-related
    geolocation IPC. This IPC will be moved to Mojo once there is resolution on
    what the right model for handling permissions there is.
    
    BUG=420497
    TEST=Go to maps.google.com, allow location tracking, and check that your
    location is correctly pinpointed on the map.
    
    Committed: https://crrev.com/28e88081438dc08b06d5f05cfdd980c407db1638
    Cr-Commit-Position: refs/heads/master@{#301604}
    
    Review URL: https://codereview.chromium.org/628773003
    
    Cr-Commit-Position: refs/heads/master@{#301824}

--- content/browser/frame_host/render_frame_host_delegate.cc
+++ content/browser/frame_host/render_frame_host_delegate.cc
@@ -64,6 +64,11 @@ RenderFrameHost* RenderFrameHostDelegate::GetGuestByInstanceID(
   return NULL;
 }
 
+GeolocationServiceContext*
+RenderFrameHostDelegate::GetGeolocationServiceContext() {
+  return NULL;
+}
+
 #if defined(OS_WIN)
 gfx::NativeViewAccessible
     RenderFrameHostDelegate::GetParentNativeViewAccessible() {
