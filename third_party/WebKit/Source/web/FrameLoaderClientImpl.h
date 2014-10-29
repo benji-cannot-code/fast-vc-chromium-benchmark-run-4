7e67fb67c18ee4caee5557e88f24740798515417
nhiroki@chromium.org <nhiroki@chromium.org>
2014-10-29 05:15:03 +0000

    ServiceWorker: Make a way to retrieve a service worker version id (Blink)
    This is used to make a key of MemoryCache:
    https://codereview.chromium.org/640463003/
    
    Chromium-side: https://codereview.chromium.org/669303003/
    
    BUG=423219
    TEST=n/a
    
    Review URL: https://codereview.chromium.org/650033007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184561 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/FrameLoaderClientImpl.h
+++ third_party/WebKit/Source/web/FrameLoaderClientImpl.h
@@ -163,6 +163,7 @@ public:
 
     virtual PassOwnPtr<WebServiceWorkerProvider> createServiceWorkerProvider() override;
     virtual bool isControlledByServiceWorker(DocumentLoader&) override;
+    virtual int64_t serviceWorkerID(DocumentLoader&) override;
     virtual SharedWorkerRepositoryClient* sharedWorkerRepositoryClient() override;
 
     virtual PassOwnPtr<WebApplicationCacheHost> createApplicationCacheHost(WebApplicationCacheHostClient*) override;
