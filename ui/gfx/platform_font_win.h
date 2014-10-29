a8c8a90ae6a7c3614f4721d9ef519bf9c21046c8
ckocagil <ckocagil@chromium.org>
2014-10-29 13:47:33 -0700

    RenderTextHarfBuzz: Try fallback fonts of the Uniscribe font while shaping
    This CL also fixes a Linux issue where a fallback font could be skipped.
    
    BUG=426052
    R=msw
    
    Review URL: https://codereview.chromium.org/674233003
    
    Cr-Commit-Position: refs/heads/master@{#301920}

--- ui/gfx/platform_font_win.h
+++ ui/gfx/platform_font_win.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/compiler_specific.h"
+#include "base/gtest_prod_util.h"
 #include "base/memory/ref_counted.h"
 #include "ui/gfx/gfx_export.h"
 #include "ui/gfx/platform_font.h"
@@ -68,6 +69,8 @@ class GFX_EXPORT PlatformFontWin : public PlatformFont {
   virtual NativeFont GetNativeFont() const override;
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(RenderTextTest, HarfBuzz_UniscribeFallback);
+
   virtual ~PlatformFontWin() {}
 
   // Chrome text drawing bottoms out in the Windows GDI functions that take an
@@ -106,6 +109,7 @@ class GFX_EXPORT PlatformFontWin : public PlatformFont {
 
    private:
     friend class base::RefCounted<HFontRef>;
+    FRIEND_TEST_ALL_PREFIXES(RenderTextTest, HarfBuzz_UniscribeFallback);
 
     ~HFontRef();
 
