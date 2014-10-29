8bab2f452cc7817ef7f6e06b3cb40853b4d360ef
dcheng <dcheng@chromium.org>
2014-10-29 16:39:54 -0700

    Standardize usage of virtual/override/final specifiers.
    The Google C++ style guide states:
    
      Explicitly annotate overrides of virtual functions or virtual
      destructors with an override or (less frequently) final specifier.
      Older (pre-C++11) code will use the virtual keyword as an inferior
      alternative annotation. For clarity, use exactly one of override,
      final, or virtual when declaring an override.
    
    To better conform to these guidelines, the following constructs have
    been rewritten:
    
    - if a base class has a virtual destructor, then:
        virtual ~Foo();                   ->  ~Foo() override;
    - virtual void Foo() override;        ->  void Foo() override;
    - virtual void Foo() override final;  ->  void Foo() final;
    
    This patch was automatically generated. The clang plugin can generate
    fixit hints, which are suggested edits when it is 100% sure it knows how
    to fix a problem. The hints from the clang plugin were applied to the
    source tree using the tool in https://codereview.chromium.org/598073004.
    
    BUG=417463
    R=jhawkins@chromium.org
    
    Review URL: https://codereview.chromium.org/681893002
    
    Cr-Commit-Position: refs/heads/master@{#301980}

--- chrome/browser/ui/views/infobars/infobar_background.h
+++ chrome/browser/ui/views/infobars/infobar_background.h
@@ -13,13 +13,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class InfoBarBackground : public views::Background {
  public:
   explicit InfoBarBackground(infobars::InfoBarDelegate::Type infobar_type);
-  virtual ~InfoBarBackground();
+  ~InfoBarBackground() override;
 
   void set_separator_color(SkColor color) { separator_color_ = color; }
 
  private:
   // views::Background:
-  virtual void Paint(gfx::Canvas* canvas, views::View* view) const override;
+  void Paint(gfx::Canvas* canvas, views::View* view) const override;
 
   SkColor separator_color_;
   SkColor top_color_;
