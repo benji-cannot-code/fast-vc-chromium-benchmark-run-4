55db709fc5d88b235882fcfdd122838f76ad68e1
sigbjornf@opera.com <sigbjornf@opera.com>
2014-10-29 20:07:11 +0000

    Oilpan: fix build after r184599.
    TBR=oilpan-reviews,haraken
    BUG=425186
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/691613002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184601 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/encryptedmedia/NavigatorRequestMediaKeySystemAccess.h
+++ third_party/WebKit/Source/modules/encryptedmedia/NavigatorRequestMediaKeySystemAccess.h
@@ -17,7 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class NavigatorRequestMediaKeySystemAccess final : public NoBaseWillBeGarbageCollected<NavigatorRequestMediaKeySystemAccess>, public WillBeHeapSupplement<Navigator> {
+class NavigatorRequestMediaKeySystemAccess final : public NoBaseWillBeGarbageCollectedFinalized<NavigatorRequestMediaKeySystemAccess>, public WillBeHeapSupplement<Navigator> {
+    WILL_BE_USING_GARBAGE_COLLECTED_MIXIN(NavigatorRequestMediaKeySystemAccess);
 public:
     virtual ~NavigatorRequestMediaKeySystemAccess();
 
