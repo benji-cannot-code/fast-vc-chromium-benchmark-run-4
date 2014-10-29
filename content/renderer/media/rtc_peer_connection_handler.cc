603fbaf9546401443217a486325e524ed10e63a0
csharp <csharp@chromium.org>
2014-10-29 09:01:14 -0700

    Revert of Refactor RtcDataChannelHandler. (patchset #2 id:20001 of https://codereview.chromium.org/643693007/)
    Reason for revert:
    Broke some webrtc tests on win7
    
    Original issue's description:
    > Refactor RtcDataChannelHandler.
    > This change splits the WebRtc observer functionality of RtcDataChannelHandler
    > from the WebKit related functionality. The former belongs with libjingle's
    > signaling thread and the latter with the main thread. As is, these threads
    > are one and the same, but this will change later this week.
    >
    > BUG=369796
    >
    > Committed: https://crrev.com/832aa011fa8f98972f04468216dd2fbbd3e7ca42
    > Cr-Commit-Position: refs/heads/master@{#301831}
    
    TBR=perkj@chromium.org,tommi@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=369796
    
    Review URL: https://codereview.chromium.org/686183002
    
    Cr-Commit-Position: refs/heads/master@{#301846}

--- content/renderer/media/rtc_peer_connection_handler.cc
+++ content/renderer/media/rtc_peer_connection_handler.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram.h"
 #include "base/stl_util.h"
 #include "base/strings/utf_string_conversions.h"
-#include "base/thread_task_runner_handle.h"
 #include "content/public/common/content_switches.h"
 #include "content/renderer/media/media_stream_track.h"
 #include "content/renderer/media/peer_connection_tracker.h"
@@ -831,8 +830,7 @@ blink::WebRTCDataChannelHandler* RTCPeerConnectionHandler::createDataChannel(
 
   ++num_data_channels_created_;
 
-  return new RtcDataChannelHandler(base::ThreadTaskRunnerHandle::Get(),
-                                   webrtc_channel);
+  return new RtcDataChannelHandler(webrtc_channel);
 }
 
 blink::WebRTCDTMFSenderHandler* RTCPeerConnectionHandler::createDTMFSender(
@@ -1029,8 +1027,7 @@ void RTCPeerConnectionHandler::OnDataChannel(
 
   DVLOG(1) << "RTCPeerConnectionHandler::OnDataChannel "
            << data_channel->label();
-  client_->didAddRemoteDataChannel(new RtcDataChannelHandler(
-      base::ThreadTaskRunnerHandle::Get(), data_channel));
+  client_->didAddRemoteDataChannel(new RtcDataChannelHandler(data_channel));
 }
 
 void RTCPeerConnectionHandler::OnRenegotiationNeeded() {
