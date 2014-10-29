94c6649918608f3c4136c4d9534194e544549691
yukawa <yukawa@chromium.org>
2014-10-29 02:17:15 -0700

    Plumb composition character bounds from C++ to Java
    This is a follow up CL of crrev.com/671503005.
    
    This CL makes a plumbing from the C++ layer to Java layer so that
    composition character bounds can be passed to new Android 5.0 IME APIs
    in subsequent CLs.
    
    As discussed in the previous CL, that callback is called in Android 5.0
    and later. Hence no performance impact is intended in KitKat and prior.
    
    All-in-one CL is available here: http://crrev.com/643193003
    
    BUG=424866
    TEST=Manually done on Nexus 7 Build/LPX13D
    
    Review URL: https://codereview.chromium.org/679223002
    
    Cr-Commit-Position: refs/heads/master@{#301808}

--- content/browser/renderer_host/ime_adapter_android.h
+++ content/browser/renderer_host/ime_adapter_android.h
@@ -8,7 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <jni.h>
 
+#include <vector>
+
 #include "base/android/jni_weak_ref.h"
+#include "ui/gfx/geometry/rect.h"
 
 namespace content {
 
@@ -65,6 +68,7 @@ class ImeAdapterAndroid {
   // Called from native -> java
   void CancelComposition();
   void FocusedNodeChanged(bool is_editable_node);
+  void SetCharacterBounds(const std::vector<gfx::Rect>& rects);
 
  private:
   RenderWidgetHostImpl* GetRenderWidgetHostImpl();
--- content/public/android/java/src/org/chromium/content/browser/input/ImeAdapter.java
+++ content/public/android/java/src/org/chromium/content/browser/input/ImeAdapter.java
@@ -699,6 +699,11 @@ public class ImeAdapter {
     }
 
     @CalledByNative
+    private void setCharacterBounds(float[] characterBounds) {
+        // TODO(yukawa): Implement this.
+    }
+
+    @CalledByNative
     void detach() {
         if (mDismissInput != null) {
             mHandler.removeCallbacks(mDismissInput);
