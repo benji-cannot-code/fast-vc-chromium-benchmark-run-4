ed66c607883d57d56383013f82b8f3d283696602
reveman <reveman@chromium.org>
2014-10-29 08:37:15 -0700

    Revert of cc: Make Overlay unit tests work when sync query extension is available. (patchset #1 id:1 of https://codereview.chromium.org/669233003/)
    Reason for revert:
    Not properly reviewed.
    
    Original issue's description:
    > cc: Make Overlay unit tests work when sync query extension is available.
    >
    > GLRenderer expects a BeginDrawingFrame before each
    > FinishDrawingFrame call.
    >
    > This also makes sure GLRenderer::BeginDrawingFrame doesn't
    > early out before creating the read lock fence.
    >
    > BUG=428071
    > TBR=danakj
    > TEST=cc_unittests --gtest_filter=GLRendererWithOverlaysTest.ResourcesExportedAndReturned
    >
    > Committed: https://crrev.com/c9260a0912dfe55fb38f5d85f954a021d9a8d626
    > Cr-Commit-Position: refs/heads/master@{#301775}
    
    TBR=danakj@chromium.org,alexst@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=428071
    
    Review URL: https://codereview.chromium.org/684133005
    
    Cr-Commit-Position: refs/heads/master@{#301840}

--- cc/output/gl_renderer.cc
+++ cc/output/gl_renderer.cc
@@ -472,6 +472,9 @@ static ResourceProvider::ResourceId WaitOnResourceSyncPoints(
 }
 
 void GLRenderer::BeginDrawingFrame(DrawingFrame* frame) {
+  if (frame->device_viewport_rect.IsEmpty())
+    return;
+
   TRACE_EVENT0("cc", "GLRenderer::BeginDrawingFrame");
 
   scoped_refptr<ResourceProvider::Fence> read_lock_fence;
@@ -501,9 +504,6 @@ void GLRenderer::BeginDrawingFrame(DrawingFrame* frame) {
     read_lock_fence = make_scoped_refptr(new FallbackFence(gl_));
   }
   resource_provider_->SetReadLockFence(read_lock_fence.get());
-
-  if (frame->device_viewport_rect.IsEmpty())
-    return;
 
   // Insert WaitSyncPointCHROMIUM on quad resources prior to drawing the frame,
   // so that drawing can proceed without GL context switching interruptions.
