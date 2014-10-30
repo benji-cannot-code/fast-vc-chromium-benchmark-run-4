6d6c4cf5b9fdd65eb66b413efe771e5852942c41
hashimoto <hashimoto@chromium.org>
2014-10-29 21:08:28 -0700

    Set AppActivityProxy window name for better Ctrl+Shift+Alt+W output
    To call GetWindow(), make overridden methods of AppActivityProxy public.
    (this should be OK as AppActivity is doing the same thing and these methods become public with static_cast<Activity*>).
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/659493007
    
    Cr-Commit-Position: refs/heads/master@{#302028}

--- athena/content/app_activity_proxy.cc
+++ athena/content/app_activity_proxy.cc
@@ -25,10 +25,6 @@ AppActivityProxy::AppActivityProxy(AppActivity* replaced_activity,
     restart_called_(false) {
 }
 
-AppActivityProxy::~AppActivityProxy() {
-  app_activity_registry_->ProxyDestroyed(this);
-}
-
 ActivityViewModel* AppActivityProxy::GetActivityViewModel() {
   return this;
 }
@@ -109,6 +105,10 @@ void AppActivityProxy::PrepareContentsForOverview() {
 }
 
 void AppActivityProxy::ResetContentsView() {
+}
+
+AppActivityProxy::~AppActivityProxy() {
+  app_activity_registry_->ProxyDestroyed(this);
 }
 
 }  // namespace athena
