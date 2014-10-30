d859112478797c33ae3fea1eaf8e3c56963dc43d
dongseong.hwang@intel.com <dongseong.hwang@intel.com>
2014-10-30 06:20:01 +0000

    WebGL: remove redundant contentChanged callback when resizing.
    HTMLCanvasElement::reset() already notifies it, so
    WebGLRenderingContextBase::reshape() doesn't need to do it.
    
    Review URL: https://codereview.chromium.org/666393002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184619 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/canvas/WebGLRenderingContextBase.h
+++ third_party/WebKit/Source/core/html/canvas/WebGLRenderingContextBase.h
@@ -456,7 +456,6 @@ protected:
     bool m_restoreAllowed;
     Timer<WebGLRenderingContextBase> m_restoreTimer;
 
-    bool m_needsUpdate;
     bool m_markedCanvasDirty;
     WillBeHeapHashSet<RawPtrWillBeWeakMember<WebGLContextObject> > m_contextObjects;
 
