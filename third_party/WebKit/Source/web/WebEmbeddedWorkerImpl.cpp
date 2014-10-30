b3f2241105af8f42407274680460346a7fde7671
ksakamoto@chromium.org <ksakamoto@chromium.org>
2014-10-30 13:08:29 +0000

    ServiceWorker: Fix data race in shutdown sequence
    To avoid Worker thread's termination code touching released objects
    owned by WebEmbeddedWorkerImpl, join the worker thread in the destructor
    of WebEmbeddedWorkerImpl.
    
    BUG=426332
    
    Review URL: https://codereview.chromium.org/684223004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184647 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebEmbeddedWorkerImpl.cpp
+++ third_party/WebKit/Source/web/WebEmbeddedWorkerImpl.cpp
@@ -52,6 +52,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/network/ContentSecurityPolicyParsers.h"
 #include "public/platform/Platform.h"
 #include "public/platform/WebURLRequest.h"
+#include "public/platform/WebWaitableEvent.h"
 #include "public/web/WebDevToolsAgent.h"
 #include "public/web/WebServiceWorkerContextClient.h"
 #include "public/web/WebServiceWorkerNetworkProvider.h"
@@ -172,6 +173,11 @@ WebEmbeddedWorkerImpl::WebEmbeddedWorkerImpl(
 
 WebEmbeddedWorkerImpl::~WebEmbeddedWorkerImpl()
 {
+    if (m_workerThread) {
+        ASSERT(m_workerThread->terminated());
+        m_workerThread->terminationEvent()->wait();
+    }
+
     ASSERT(runningWorkerInstances().contains(this));
     runningWorkerInstances().remove(this);
     ASSERT(m_webView);
