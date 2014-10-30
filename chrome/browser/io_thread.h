8d79795b38222ba8e1d65d4b744758cb86dd096c
bnc <bnc@chromium.org>
2014-10-30 15:29:45 -0700

    Add Spdy4Experiment group.
    Add Spdy4Experiment group which can be kept at the same size as Spdy4Control,
    while Spdy4Enabled can be arbitrary.  This allows enabling SPDY/4 on more than
    50% while having a group the same size as Spdy4Control for convenient
    comparison.
    
    BUG=428690
    
    Review URL: https://codereview.chromium.org/689923002
    
    Cr-Commit-Position: refs/heads/master@{#302171}

--- chrome/browser/io_thread.h
+++ chrome/browser/io_thread.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/prefs/pref_member.h"
+#include "base/strings/string_piece.h"
 #include "base/time/time.h"
 #include "chrome/browser/net/chrome_network_delegate.h"
 #include "chrome/browser/net/ssl_config_service_manager.h"
@@ -290,7 +291,7 @@ class IOThread : public content::BrowserThreadDelegate {
 
   // Configures available SPDY protocol versions from the given trial.
   // Used only if no command-line configuration was present.
-  static void ConfigureSpdyFromTrial(const std::string& spdy_trial_group,
+  static void ConfigureSpdyFromTrial(base::StringPiece spdy_trial_group,
                                      Globals* globals);
 
   // Global state must be initialized on the IO thread, then this
