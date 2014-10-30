e42c45b0380d61e316633674644c859a1c32fdc6
mstensho@opera.com <mstensho@opera.com>
2014-10-30 21:37:27 +0000

    [New Multicolumn] boundingBoxForCompositingOverlapTest() needs to use fragmentsBoundingBox().
    Bounding boxes are visual, so we need to translate a flow thread
    rectangle to the union of each visual fragment in each column that the
    layer occurs. This means that flippedLogicalBoundingBox() won't do. Nor
    will physicalBoundingBox(), which, despite its name, isn't "physical"
    enough for multicol. In our case, flippedLogicalBoundingBox() and
    physicalBoundingBox() would behave identically anyway, since the
    ancestor layer is the same as this layer (and physicalBoundingBox()
    only adds the offset from this layer to the ancestor).
    
    BUG=359877
    R=jchaffraix@chromium.org
    
    Review URL: https://codereview.chromium.org/655003005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184670 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderLayer.cpp
+++ third_party/WebKit/Source/core/rendering/RenderLayer.cpp
@@ -2284,7 +2284,7 @@ LayoutRect RenderLayer::fragmentsBoundingBox(const RenderLayer* ancestorLayer) c
 
 LayoutRect RenderLayer::boundingBoxForCompositingOverlapTest() const
 {
-    return overlapBoundsIncludeChildren() ? boundingBoxForCompositing() : flippedLogicalBoundingBox(logicalBoundingBox(), renderer());
+    return overlapBoundsIncludeChildren() ? boundingBoxForCompositing() : fragmentsBoundingBox(this);
 }
 
 static void expandRectForReflectionAndStackingChildren(const RenderLayer* ancestorLayer, RenderLayer::CalculateBoundsOptions options, LayoutRect& result)
