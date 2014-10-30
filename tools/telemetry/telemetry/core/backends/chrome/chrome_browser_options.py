31baaf035a2a2f28c604fdb5be8a8c6ad36e0a18
achuith <achuith@chromium.org>
2014-10-29 20:13:02 -0700

    Some autotests need access to default apps (such as camera).
    BUG=428568
    TEST=manual
    
    Review URL: https://codereview.chromium.org/691673002
    
    Cr-Commit-Position: refs/heads/master@{#302025}

--- tools/telemetry/telemetry/core/backends/chrome/chrome_browser_options.py
+++ tools/telemetry/telemetry/core/backends/chrome/chrome_browser_options.py
@@ -37,6 +37,8 @@ class CrosBrowserOptions(ChromeBrowserOptions):
     self.clear_enterprise_policy = True
     # Disable GAIA/enterprise services.
     self.disable_gaia_services = True
+    # Disable default apps.
+    self.disable_default_apps = True
 
     self.auto_login = True
     self.gaia_login = False
