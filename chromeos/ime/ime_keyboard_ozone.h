7d7ab974587cdc9145388d0d357f599d55f681a2
fengyuan <fengyuan@chromium.org>
2014-10-30 02:19:19 -0700

    Adds ozone support for ime keyboard.
    BUG=342336
    
    Review URL: https://codereview.chromium.org/683773002
    
    Cr-Commit-Position: refs/heads/master@{#302057}

--- chromeos/ime/ime_keyboard_ozone.h
+++ chromeos/ime/ime_keyboard_ozone.h
@@ -16,10 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 namespace input_method {
 
-class CHROMEOS_EXPORT FakeImeKeyboard : public ImeKeyboard {
+class ImeKeyboardOzone : public ImeKeyboard {
  public:
-  FakeImeKeyboard();
-  virtual ~FakeImeKeyboard();
+  ImeKeyboardOzone();
+  virtual ~ImeKeyboardOzone();
 
   virtual bool SetCurrentKeyboardLayoutByName(const std::string& layout_name)
       override;
@@ -28,16 +28,11 @@ class CHROMEOS_EXPORT FakeImeKeyboard : public ImeKeyboard {
   virtual bool ReapplyCurrentKeyboardLayout() override;
   virtual void ReapplyCurrentModifierLockStatus() override;
   virtual void DisableNumLock() override;
-  virtual bool IsISOLevel5ShiftAvailable() const override;
-  virtual bool IsAltGrAvailable() const override;
-
-  int set_current_keyboard_layout_by_name_count_;
-  AutoRepeatRate last_auto_repeat_rate_;
-  // TODO(yusukes): Add more variables for counting the numbers of the API calls
-  bool auto_repeat_is_enabled_;
+  virtual void SetCapsLockEnabled(bool enable_caps_lock) override;
+  virtual bool CapsLockIsEnabled() override;
 
  private:
-  DISALLOW_COPY_AND_ASSIGN(FakeImeKeyboard);
+  DISALLOW_COPY_AND_ASSIGN(ImeKeyboardOzone);
 };
 
 }  // namespace input_method
