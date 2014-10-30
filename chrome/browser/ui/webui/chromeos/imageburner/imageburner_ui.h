2187989ff3df17ba72be47686f6a9d1f3d74d901
satorux <satorux@chromium.org>
2014-10-29 23:12:29 -0700

    chromeos: Remove legacy code for chrome://imageburner
    chrome://imageburner was replaced by the new recovery tool
    http://google.com/chromeos/recovery.
    
    With this change, chrome://imageburner just redirects to
    the new recovery tool page.
    
    Note that the D-Bus client code (ImageBurnerClient) is not removed
    because it's used in imageWriterPrivate API, used in the new
    recovery tool.
    
    BUG=344279
    TEST=open chrome://imageburner and you are redirected to http://google.com/chromeos/recovery
    
    Review URL: https://codereview.chromium.org/679633002
    
    Cr-Commit-Position: refs/heads/master@{#302042}

--- chrome/browser/ui/webui/chromeos/imageburner/imageburner_ui.h
+++ chrome/browser/ui/webui/chromeos/imageburner/imageburner_ui.h
@@ -2,6 +2,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+//
+// TODO(crbug.com/427785): Remove this file after 2015-10-31.
 
 #ifndef CHROME_BROWSER_UI_WEBUI_CHROMEOS_IMAGEBURNER_IMAGEBURNER_UI_H_
 #define CHROME_BROWSER_UI_WEBUI_CHROMEOS_IMAGEBURNER_IMAGEBURNER_UI_H_
@@ -17,4 +19,3 @@ class ImageBurnUI : public content::WebUIController {
 };
 
 #endif  // CHROME_BROWSER_UI_WEBUI_CHROMEOS_IMAGEBURNER_IMAGEBURNER_UI_H_
-
