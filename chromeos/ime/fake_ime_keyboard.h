011fe486d6131261267f2db29c75a21e320867db
yurys <yurys@chromium.org>
2014-10-30 03:13:19 -0700

    Revert of Adds ozone support for ime keyboard. (patchset #7 id:120001 of https://codereview.chromium.org/683773002/)
    Reason for revert:
    Change doesn't compile on Linux ChromiumOS Ozone Builder
    
    http://build.chromium.org/p/chromium.chromiumos/buildstatus?builder=Linux%20ChromiumOS%20Ozone%20Builder&number=6030
    
    Original issue's description:
    > Adds ozone support for ime keyboard.
    >
    > BUG=342336
    >
    > Committed: https://crrev.com/8be55caf3d38bba7645de143f7e6b8b655fdd36c
    > Cr-Commit-Position: refs/heads/master@{#302057}
    
    TBR=shuchen@chromium.org,fengyuan@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=342336
    
    Review URL: https://codereview.chromium.org/689873004
    
    Cr-Commit-Position: refs/heads/master@{#302062}

--- chromeos/ime/fake_ime_keyboard.h
+++ chromeos/ime/fake_ime_keyboard.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/compiler_specific.h"
+#include "base/observer_list.h"
 #include "chromeos/chromeos_export.h"
 
 namespace chromeos {
@@ -21,22 +22,30 @@ class CHROMEOS_EXPORT FakeImeKeyboard : public ImeKeyboard {
   FakeImeKeyboard();
   virtual ~FakeImeKeyboard();
 
+  virtual void AddObserver(Observer* observer) override;
+  virtual void RemoveObserver(Observer* observer) override;
   virtual bool SetCurrentKeyboardLayoutByName(const std::string& layout_name)
       override;
-  virtual bool SetAutoRepeatRate(const AutoRepeatRate& rate) override;
-  virtual bool SetAutoRepeatEnabled(bool enabled) override;
   virtual bool ReapplyCurrentKeyboardLayout() override;
   virtual void ReapplyCurrentModifierLockStatus() override;
   virtual void DisableNumLock() override;
+  virtual void SetCapsLockEnabled(bool enable_caps_lock) override;
+  virtual bool CapsLockIsEnabled() override;
   virtual bool IsISOLevel5ShiftAvailable() const override;
   virtual bool IsAltGrAvailable() const override;
+  virtual bool SetAutoRepeatEnabled(bool enabled) override;
+  virtual bool SetAutoRepeatRate(const AutoRepeatRate& rate) override;
 
   int set_current_keyboard_layout_by_name_count_;
+  std::string last_layout_;
+  bool caps_lock_is_enabled_;
+  bool auto_repeat_is_enabled_;
   AutoRepeatRate last_auto_repeat_rate_;
   // TODO(yusukes): Add more variables for counting the numbers of the API calls
-  bool auto_repeat_is_enabled_;
 
  private:
+  ObserverList<Observer> observers_;
+
   DISALLOW_COPY_AND_ASSIGN(FakeImeKeyboard);
 };
 
