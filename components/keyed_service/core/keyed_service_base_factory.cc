21ddba37c98960e385fb14fee5e4e5d6c1c48e25
sdefresne <sdefresne@chromium.org>
2014-10-30 01:52:27 -0700

    Refactor (Refcounted)BrowserContextKeyedServiceFactory
    Introduce (Refcounted)KeyedServiceFactory classes that deal with the
    lifetime management of (Refcounted)KeyedService. Port the classes
    (Refcounted)BrowserContextKeyedServiceFactory to inherit from them.
    
    BUG=419366
    
    Review URL: https://codereview.chromium.org/654753010
    
    Cr-Commit-Position: refs/heads/master@{#302054}

--- components/keyed_service/core/keyed_service_base_factory.cc
+++ components/keyed_service/core/keyed_service_base_factory.cc
@@ -8,9 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/prefs/pref_service.h"
 #include "base/supports_user_data.h"
 #include "components/keyed_service/core/dependency_manager.h"
-// #include "components/pref_registry/pref_registry_syncable.h"
-// #include "components/user_prefs/user_prefs.h"
-// #include "content/public/browser/browser_context.h"
 
 void KeyedServiceBaseFactory::RegisterUserPrefsOnContextForTest(
     base::SupportsUserData* context) {
