64b26f04f0b946d094a61870506391a83c80582e
hirono <hirono@chromium.org>
2014-10-30 19:44:38 -0700

    Remove component apps that should not used in the kiosk-mode.
    BUG=427114
    TEST=None
    
    Review URL: https://codereview.chromium.org/687903004
    
    Cr-Commit-Position: refs/heads/master@{#302214}

--- chrome/browser/extensions/component_loader.cc
+++ chrome/browser/extensions/component_loader.cc
@@ -498,10 +498,7 @@ void ComponentLoader::AddDefaultComponentExtensionsForKioskMode(
     return;
 
   // Component extensions needed for kiosk apps.
-  AddVideoPlayerExtension();
-  AddAudioPlayerExtension();
   AddFileManagerExtension();
-  AddGalleryExtension();
 
   // Add virtual keyboard.
   AddKeyboardApp();
