3d8f01a19320f893b45bced8536c5b681c9f8d66
wjmaclean <wjmaclean@chromium.org>
2014-10-31 15:00:13 -0700

    Revert of WebView Guest should use ZOOM_MODE_ISOLATED. (patchset #2 id:100001 of https://codereview.chromium.org/645153002/)
    Reason for revert:
    This CL seems to be at the root of several other issues, including https://code.google.com/p/chromium/issues/detail?id=429004.
    
    In particular, it seems to be allowing re-entrancy into ZoomController that was unexpected due to the timing with which OnGuestReady() is called.
    
    Original issue's description:
    > WebView Guest should use ZOOM_MODE_ISOLATED.
    >
    > In order that changes to zoom levels in a WebView Guest don't affect
    > the embedder, set the WebView Guest's WebContents to use
    > ZOOM_MODE_ISOLATED.
    >
    > BUG=418605
    >
    > Committed: https://crrev.com/f71a5dc79473cc74990d1e288c66ac282c2926a1
    > Cr-Commit-Position: refs/heads/master@{#299493}
    
    TBR=fsamuel@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=418605
    
    Review URL: https://codereview.chromium.org/690373002
    
    Cr-Commit-Position: refs/heads/master@{#302332}

--- chrome/browser/guest_view/web_view/chrome_web_view_guest_delegate.h
+++ chrome/browser/guest_view/web_view/chrome_web_view_guest_delegate.h
@@ -38,7 +38,6 @@ class ChromeWebViewGuestDelegate : public WebViewGuestDelegate,
   void OnDidInitialize() override;
   void OnDocumentLoadedInFrame(
       content::RenderFrameHost* render_frame_host) override;
-  void OnGuestReady() override;
   void OnEmbedderWillBeDestroyed() override;
   void OnGuestDestroyed() override;
   void OnSetZoom(double zoom_factor) override;
