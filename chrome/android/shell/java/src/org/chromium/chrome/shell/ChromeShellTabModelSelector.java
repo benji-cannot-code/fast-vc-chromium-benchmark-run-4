5365eb329b82766092dd06a65289e47b5941f826
ankit2.kumar <ankit2.kumar@samsung.com>
2014-10-30 21:51:00 -0700

    Tab manager view should hide on press of back key
    Currently when tab manager is shown and user presses back key
    then instead of hiding tab manager page is navigated to previous
    entry. If there is nothing to navigate then application is exited.
    Added code to hide tab manager view if back key is pressed and tab
    manager view is on.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/689583002
    
    Cr-Commit-Position: refs/heads/master@{#302222}

--- chrome/android/shell/java/src/org/chromium/chrome/shell/ChromeShellTabModelSelector.java
+++ chrome/android/shell/java/src/org/chromium/chrome/shell/ChromeShellTabModelSelector.java
@@ -138,7 +138,7 @@ class ChromeShellTabModelSelector extends TabModelSelectorBase {
         }
     }
 
-    private boolean isTabSwitcherVisible() {
+    public boolean isTabSwitcherVisible() {
         return mTabModelWrapper != null && mTabModelWrapper.getParent() == mParent;
     }
 }
