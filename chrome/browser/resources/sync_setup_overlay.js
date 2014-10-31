0c1c21d1ca530eefd2d60103d1b87f91bb15bd5d
dbeam <dbeam@chromium.org>
2014-10-31 14:45:21 -0700

    Fix closure compile.
    ## /mnt/data/b/build/slave/Closure_Compilation_Linux/build/src/chrome/browser/resources/sync_setup_overlay.js:445: ERROR - Property wifiCredentialsSynced never defined on args
    ##         $('wifi-credentials-checkbox').checked = args.wifiCredentialsSynced;
    ##                                                  ^
    ##
    ## /mnt/data/b/build/slave/Closure_Compilation_Linux/build/src/chrome/browser/resources/sync_setup_overlay.js:447: ERROR - Property wifiCredentialsSynced never defined on args
    ##             args.wifiCredentialsSynced;
    ##             ^
    ##
    ## /mnt/data/b/build/slave/Closure_Compilation_Linux/build/src/chrome/browser/resources/sync_setup_overlay.js:449: ERROR - Property wifiCredentialsEnforced never defined on args
    ##             args.wifiCredentialsEnforced;
    ##             ^
    
    BUG=none
    TEST=green closure bot
    TBR=quiche@chromium.org
    NOTRY=true
    NOTREECHECKS=true
    
    Review URL: https://codereview.chromium.org/696013002
    
    Cr-Commit-Position: refs/heads/master@{#302329}

--- chrome/browser/resources/sync_setup_overlay.js
+++ chrome/browser/resources/sync_setup_overlay.js
@@ -42,7 +42,9 @@ cr.exportPath('options');
  *             typedUrlsEnforced: boolean,
  *             typedUrlsRegistered: boolean,
  *             typedUrlsSynced: boolean,
- *             usePassphrase: boolean}}
+ *             usePassphrase: boolean,
+ *             wifiCredentialsEnforced: (boolean|undefined),
+ *             wifiCredentialsSynced: (boolean|undefined)}}
  */
 var SyncConfig;
 
