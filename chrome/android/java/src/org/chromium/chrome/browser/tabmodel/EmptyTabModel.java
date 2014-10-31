a9c30524987862d0dd8ccd4a1be56ac30fa8a27c
twellington <twellington@chromium.org>
2014-10-31 11:56:17 -0700

    Add TabModel#closeAllTabs(boolean allowDelegation, boolean uponExit) and TabModelSelector#closeAllTabs(boolean uponExit)
    BUG=428084
    
    Review URL: https://codereview.chromium.org/689693002
    
    Cr-Commit-Position: refs/heads/master@{#302293}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/EmptyTabModel.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/EmptyTabModel.java
@@ -59,7 +59,7 @@ public class EmptyTabModel implements TabModel {
     }
 
     @Override
-    public void closeAllTabs(boolean allowDelegation) {
+    public void closeAllTabs(boolean allowDelegation, boolean uponExit) {
     }
 
     @Override
