f6fec60f4bd48548cb8d6d57f1fe29a7c3a10813
ajith.v@samsung.com <ajith.v@samsung.com>
2014-10-31 18:37:45 +0000

    Text selection should happen on Disabled and Readonly elements when do long press on those elements in Android for allowing user to Copy.
    HitTest is coming as the layered parent node, so text selection is
    not happening. This change takes care of processing text selection in
    case of disabled/readonly elements.
    
    Review URL: https://codereview.chromium.org/309553007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184746 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/EventHandler.cpp
+++ third_party/WebKit/Source/core/page/EventHandler.cpp
@@ -2323,7 +2323,11 @@ bool EventHandler::handleGestureLongPress(const GestureEventWithHitTestResults& 
         IntPoint hitTestPoint = m_frame->view()->windowToContents(gestureEvent.position());
         HitTestResult result = hitTestResultAtPoint(hitTestPoint);
         Node* innerNode = result.innerNode();
-        if (!result.isLiveLink() && innerNode && (innerNode->isContentEditable() || innerNode->isTextNode())) {
+        if (!result.isLiveLink() && innerNode && (innerNode->isContentEditable() || innerNode->isTextNode()
+#if OS(ANDROID)
+            || innerNode->canStartSelection()
+#endif
+            )) {
             selectClosestWordFromHitTestResult(result, DontAppendTrailingWhitespace);
             if (m_frame->selection().isRange()) {
                 focusDocumentView();
