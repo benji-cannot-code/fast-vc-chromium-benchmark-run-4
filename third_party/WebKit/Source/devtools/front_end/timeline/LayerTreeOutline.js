40099d7118ecd1edb9eb024102c898f8f261ec97
caseq@chromium.org <caseq@chromium.org>
2014-10-31 12:55:22 +0000

    DevTools: fix logic of determining content root for trace layers
    We can't rely on draws_content property of layers we get from
    tracing, as it may be true for non-content layers. Let's also
    check the owner node. However, since there may be cases where
    we lack the owner node info, be prepared to draw the entire
    layer tree in case we failed to locate content root.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/687963002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184720 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/timeline/LayerTreeOutline.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/LayerTreeOutline.js
@@ -87,6 +87,7 @@ WebInspector.LayerTreeOutline.prototype = {
     update: function(layerTree)
     {
         var seenLayers = new Map();
+        var root = layerTree && (layerTree.contentRoot() || layerTree.root());
 
         /**
          * @param {!WebInspector.Layer} layer
@@ -98,7 +99,7 @@ WebInspector.LayerTreeOutline.prototype = {
                 console.assert(false, "Duplicate layer: " + layer.id());
             seenLayers.set(layer, true);
             var node = this._treeOutline.getCachedTreeElement(layer);
-            var parent = layer === layerTree.contentRoot() ? this._treeOutline : this._treeOutline.getCachedTreeElement(layer.parent());
+            var parent = layer === root ? this._treeOutline : this._treeOutline.getCachedTreeElement(layer.parent());
             if (!parent) {
                 console.assert(false, "Parent is not in the tree");
                 return;
@@ -114,8 +115,8 @@ WebInspector.LayerTreeOutline.prototype = {
                 node._update();
             }
         }
-        if (layerTree && layerTree.contentRoot())
-            layerTree.forEachLayer(updateLayer.bind(this), layerTree.contentRoot());
+        if (root)
+            layerTree.forEachLayer(updateLayer.bind(this), root);
         // Cleanup layers that don't exist anymore from tree.
         for (var node = /** @type {!TreeElement|!TreeOutline|null} */ (this._treeOutline.children[0]); node && !node.root;) {
             if (seenLayers.get(node.representedObject)) {
