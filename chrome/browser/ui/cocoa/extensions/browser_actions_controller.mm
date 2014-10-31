4947798a57d55db245425a5e82c586e805495daa
malcolm.2.wang <malcolm.2.wang@gmail.com>
2014-10-30 18:51:29 -0700

    To ensure that the location bar will not reach the minimum width.
    Fix the width of location bar will be less than the minimum width, when
    dragging the browserActionsContainer to the left.
    
    Before the browserActionsContainer will translate on x-axis, sending a
    notification to see whether it will be allowed. This will give the
    toolbar controller a chance to check if the width of location bar will
    reach the minimum by dragging the browserActionsContainer.
    
    BUG=329842
    TEST=manually tested.
    
    Review URL: https://codereview.chromium.org/636283004
    
    Cr-Commit-Position: refs/heads/master@{#302209}

--- chrome/browser/ui/cocoa/extensions/browser_actions_controller.mm
+++ chrome/browser/ui/cocoa/extensions/browser_actions_controller.mm
@@ -120,6 +120,10 @@ const CGFloat kBrowserActionBubbleYOffset = 3.0;
 // toolbar know that the drag has finished.
 - (void)containerDragFinished:(NSNotification*)notification;
 
+// Sends a notification for the toolbar to determine whether the container can
+// translate with a delta on x-axis.
+- (void)containerWillTranslateOnX:(NSNotification*)notification;
+
 // Adjusts the position of the surrounding action buttons depending on where the
 // button is within the container.
 - (void)actionButtonDragging:(NSNotification*)notification;
@@ -305,6 +309,11 @@ class ExtensionServiceObserverBridge
         addObserver:self
            selector:@selector(containerDragFinished:)
                name:kBrowserActionGrippyDragFinishedNotification
+             object:containerView_];
+    [[NSNotificationCenter defaultCenter]
+        addObserver:self
+           selector:@selector(containerWillTranslateOnX:)
+               name:kBrowserActionGrippyWillDragNotification
              object:containerView_];
     // Listen for a finished drag from any button to make sure each open window
     // stays in sync.
@@ -669,6 +678,13 @@ class ExtensionServiceObserverBridge
   [[NSNotificationCenter defaultCenter]
       postNotificationName:kBrowserActionGrippyDragFinishedNotification
                     object:self];
+}
+
+- (void)containerWillTranslateOnX:(NSNotification*)notification {
+  [[NSNotificationCenter defaultCenter]
+      postNotificationName:kBrowserActionGrippyWillDragNotification
+                    object:self
+                  userInfo:notification.userInfo];
 }
 
 - (void)actionButtonDragging:(NSNotification*)notification {
