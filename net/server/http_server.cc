3a79fcf6f2e97f43ffd85e76bffc400fd0436c5e
byungchul <byungchul@chromium.org>
2014-10-31 12:38:09 -0700

    Start accepting http connections in next run loop.
    Devtools is not ready to get callbacks if accept returns connections
    synchronously.
    
    BUG=428389
    
    Review URL: https://codereview.chromium.org/683733006
    
    Cr-Commit-Position: refs/heads/master@{#302309}

--- net/server/http_server.cc
+++ net/server/http_server.cc
@@ -5,8 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/server/http_server.h"
 
+#include "base/bind.h"
 #include "base/compiler_specific.h"
+#include "base/location.h"
 #include "base/logging.h"
+#include "base/message_loop/message_loop_proxy.h"
 #include "base/stl_util.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
@@ -31,7 +34,11 @@ HttpServer::HttpServer(scoped_ptr<ServerSocket> server_socket,
       last_id_(0),
       weak_ptr_factory_(this) {
   DCHECK(server_socket_);
-  DoAcceptLoop();
+  // Start accepting connections in next run loop in case when delegate is not
+  // ready to get callbacks.
+  base::MessageLoopProxy::current()->PostTask(
+      FROM_HERE,
+      base::Bind(&HttpServer::DoAcceptLoop, weak_ptr_factory_.GetWeakPtr()));
 }
 
 HttpServer::~HttpServer() {
