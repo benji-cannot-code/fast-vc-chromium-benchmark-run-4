c8c9faf7fbfbeeeea10d965c5b663f2d2e6d95cd
yawano <yawano@chromium.org>
2014-10-30 23:18:16 -0700

    Add an unittest for background/volume_manager.js
    BUG=315436
    TEST=out/Release/browser_tests --gtest_filter=FileManagerJsTest.*
    
    Review URL: https://codereview.chromium.org/663513003
    
    Cr-Commit-Position: refs/heads/master@{#302230}

--- chrome/test/data/file_manager/unit_tests/import_history_unittest.js
+++ chrome/test/data/file_manager/unit_tests/import_history_unittest.js
@@ -22,7 +22,7 @@ var GOOGLE_DRIVE = 'Google Drive';
  */
 var SPACE_CLOUD = 'Space Cloud';
 
-/** @type {!TestFileSystem|undefined} */
+/** @type {!MockFileSystem|undefined} */
 var testFileSystem;
 
 /** @type {!MockFileEntry|undefined} */
@@ -36,7 +36,7 @@ var historyLoader;
 
 // Set up the test components.
 function setUp() {
-  testFileSystem = new TestFileSystem('abc-123');
+  testFileSystem = new MockFileSystem('abc-123', 'filesystem:abc-123');
   testFileEntry = new MockFileEntry(
       testFileSystem,
       FILE_PATH, {
