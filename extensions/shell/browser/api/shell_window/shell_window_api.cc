c6617d91906dbca36199af761ca3d115fefb2ae1
zork <zork@chromium.org>
2014-10-31 15:33:21 -0700

    Add idl for managing window visibility in app_shell
    BUG=424650
    
    Review URL: https://codereview.chromium.org/686983002
    
    Cr-Commit-Position: refs/heads/master@{#302338}

---
+++ extensions/shell/browser/api/shell_window/shell_window_api.cc
@@ -0,0 +1,53 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "extensions/shell/browser/api/shell_window/shell_window_api.h"
+
+#include "base/logging.h"
+#include "extensions/shell/common/api/shell_window.h"
+
+namespace shell_window = extensions::shell::api::shell_window;
+
+namespace extensions {
+
+ShellWindowRequestHideFunction::ShellWindowRequestHideFunction() {
+}
+
+ShellWindowRequestHideFunction::~ShellWindowRequestHideFunction() {
+}
+
+ExtensionFunction::ResponseAction ShellWindowRequestHideFunction::Run() {
+  // TODO(zork): Implement this.
+  return RespondNow(NoArguments());
+}
+
+ShellWindowRequestShowFunction::ShellWindowRequestShowFunction() {
+}
+
+ShellWindowRequestShowFunction::~ShellWindowRequestShowFunction() {
+}
+
+ExtensionFunction::ResponseAction ShellWindowRequestShowFunction::Run() {
+  // TODO(zork): Implement this.
+  return RespondNow(NoArguments());
+}
+
+ShellWindowShowAppFunction::ShellWindowShowAppFunction() {
+}
+
+ShellWindowShowAppFunction::~ShellWindowShowAppFunction() {
+}
+
+ExtensionFunction::ResponseAction ShellWindowShowAppFunction::Run() {
+  // Get parameters
+  scoped_ptr<shell_window::ShowApp::Params> params(
+      shell_window::ShowApp::Params::Create(*args_));
+  EXTENSION_FUNCTION_VALIDATE(params.get());
+
+  // TODO(zork): Implement this.
+  return RespondNow(NoArguments());
+}
+
+}  // namespace extensions
