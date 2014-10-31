76c6e6d38fc7a2986377b08522b7399d71f1383e
gusfernandez <gusfernandez@chromium.org>
2014-10-30 17:02:46 -0700

    Fix an Aura crash several minutes into V2Mirroring
    Since we deleted the gfx::screen each time we closed an app,
    A query to the screen size during metrics gathering was causing
    a check failure.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/691023003
    
    Cr-Commit-Position: refs/heads/master@{#302194}

--- chromecast/browser/cast_browser_main_parts.cc
+++ chromecast/browser/cast_browser_main_parts.cc
@@ -53,6 +53,11 @@ DefaultCommandLineSwitch g_default_switches[] = {
   { switches::kDisablePlugins, "" },
   // Always enable HTMLMediaElement logs.
   { switches::kBlinkPlatformLogChannels, "Media"},
+#if defined(OS_LINUX) && defined(ARCH_CPU_X86_FAMILY)
+  // This is needed for now to enable the egltest Ozone platform to work with
+  // current Linux/NVidia OpenGL drivers.
+  { switches::kIgnoreGpuBlacklist, ""},
+#endif
   { NULL, NULL },  // Termination
 };
 
