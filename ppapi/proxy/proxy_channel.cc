1bb1822125a5ffb91ef042d65fb7527289d02f43
kareng <kareng@google.com>
2014-11-01 12:06:16 -0700

    Revert of Hold the Pepper proxy lock for all Pepper GL calls. (patchset #3 id:40001 of https://codereview.chromium.org/692683003/)
    Reason for revert:
    causing huge crashes in canary. bug=429446
    
    Original issue's description:
    > Hold the Pepper proxy lock for all Pepper GL calls.
    >
    > BUG=418651
    >
    > Committed: https://crrev.com/057f7a69dd40c93550b375b4287dba70d719316a
    > Cr-Commit-Position: refs/heads/master@{#302303}
    
    TBR=dmichael@chromium.org,dmichael@google.com,penghuang@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=418651
    
    Review URL: https://codereview.chromium.org/698463006
    
    Cr-Commit-Position: refs/heads/master@{#302391}

--- ppapi/proxy/proxy_channel.cc
+++ ppapi/proxy/proxy_channel.cc
@@ -86,9 +86,5 @@ bool ProxyChannel::Send(IPC::Message* msg) {
   return false;
 }
 
-bool ProxyChannel::SendAndStayLocked(IPC::Message* msg) {
-  return Send(msg);
-}
-
 }  // namespace proxy
 }  // namespace ppapi
