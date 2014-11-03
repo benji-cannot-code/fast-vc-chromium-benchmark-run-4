582218e3c9071513406b79e6c374062f32aed41f
dgn <dgn@chromium.org>
2014-11-03 03:03:33 -0800

    Fix getters for the printing preferences
    The getter for the printing.enabled preference was improperly
    testing for whether the property was managed. The value of the
    property and the managed status are now obtainable through separate
    functions
    
    BUG=423886
    
    Review URL: https://codereview.chromium.org/695773004
    
    Cr-Commit-Position: refs/heads/master@{#302420}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/PrefServiceBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/PrefServiceBridge.java
@@ -534,6 +534,13 @@ public final class PrefServiceBridge {
     }
 
     /**
+     * @return Whether printing is managed by policy.
+     */
+    public boolean isPrintingManaged() {
+        return nativeGetPrintingManaged();
+    }
+
+    /**
      * Adds/Edit a popup exception
      *
      * @param pattern attribute for the popup exception pattern
@@ -652,6 +659,7 @@ public final class PrefServiceBridge {
     private native boolean nativeGetIncognitoModeEnabled();
     private native boolean nativeGetIncognitoModeManaged();
     private native boolean nativeGetPrintingEnabled();
+    private native boolean nativeGetPrintingManaged();
     private native boolean nativeGetForceSafeSearch();
     private native void nativeSetTranslateEnabled(boolean enabled);
     private native void nativeResetTranslateDefaults();
