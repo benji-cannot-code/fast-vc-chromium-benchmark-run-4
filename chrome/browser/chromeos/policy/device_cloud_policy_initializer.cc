8e1cbd09777c5a3e9dbed6005cc7a54044444d42
bartfab <bartfab@chromium.org>
2014-11-03 06:13:45 -0800

    Add RESTORE_MODE_DISABLED to RestoreMode
    This CL changes the way that device disabling is expressed in the
    DeviceStateRetrievalResponse proto. Instead of being implied when
    the |device_state| field is present, it is now explictly set as a
    |restore_mode|.
    
    BUG=425574
    TEST=Updated unit and browser tests
    
    Review URL: https://codereview.chromium.org/690643002
    
    Cr-Commit-Position: refs/heads/master@{#302426}

--- chrome/browser/chromeos/policy/device_cloud_policy_initializer.cc
+++ chrome/browser/chromeos/policy/device_cloud_policy_initializer.cc
@@ -136,9 +136,9 @@ void DeviceCloudPolicyInitializer::StartEnrollment(
 }
 
 bool DeviceCloudPolicyInitializer::ShouldAutoStartEnrollment() const {
-  std::string restore_mode = GetRestoreMode();
-  if (restore_mode == kDeviceStateRestoreModeReEnrollmentRequested ||
-      restore_mode == kDeviceStateRestoreModeReEnrollmentEnforced) {
+  const RestoreMode restore_mode = GetRestoreMode();
+  if (restore_mode == RESTORE_MODE_REENROLLMENT_REQUESTED ||
+      restore_mode == RESTORE_MODE_REENROLLMENT_ENFORCED) {
     return true;
   }
 
@@ -164,7 +164,7 @@ std::string DeviceCloudPolicyInitializer::GetEnrollmentRecoveryDomain() const {
 }
 
 bool DeviceCloudPolicyInitializer::CanExitEnrollment() const {
-  if (GetRestoreMode() == kDeviceStateRestoreModeReEnrollmentEnforced)
+  if (GetRestoreMode() == RESTORE_MODE_REENROLLMENT_ENFORCED)
     return false;
 
   if (local_state_->HasPrefPath(prefs::kDeviceEnrollmentCanExit))
@@ -253,14 +253,6 @@ void DeviceCloudPolicyInitializer::StartConnection(
     on_connected_callback_.Run();
     on_connected_callback_.Reset();
   }
-}
-
-std::string DeviceCloudPolicyInitializer::GetRestoreMode() const {
-  const base::DictionaryValue* device_state_dict =
-      local_state_->GetDictionary(prefs::kServerBackedDeviceState);
-  std::string restore_mode;
-  device_state_dict->GetString(kDeviceStateRestoreMode, &restore_mode);
-  return restore_mode;
 }
 
 }  // namespace policy
