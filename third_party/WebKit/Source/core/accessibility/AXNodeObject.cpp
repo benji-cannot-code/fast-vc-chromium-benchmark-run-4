65f2f2429e19c24a8dfb382937397b58b48d342a
sungmann.cho@navercorp.com <sungmann.cho@navercorp.com>
2014-11-03 08:02:27 +0000

    Fix some minor typos related to the word "accessibility".
    Review URL: https://codereview.chromium.org/697913002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184784 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/accessibility/AXNodeObject.cpp
+++ third_party/WebKit/Source/core/accessibility/AXNodeObject.cpp
@@ -1120,7 +1120,7 @@ AccessibilityRole AXNodeObject::ariaRoleAttribute() const
 
 // When building the textUnderElement for an object, determine whether or not
 // we should include the inner text of this given descendant object or skip it.
-static bool shouldUseAccessiblityObjectInnerText(AXObject* obj)
+static bool shouldUseAccessibilityObjectInnerText(AXObject* obj)
 {
     // Consider this hypothetical example:
     // <div tabindex=0>
@@ -1162,7 +1162,7 @@ String AXNodeObject::textUnderElement() const
 
     StringBuilder builder;
     for (AXObject* child = firstChild(); child; child = child->nextSibling()) {
-        if (!shouldUseAccessiblityObjectInnerText(child))
+        if (!shouldUseAccessibilityObjectInnerText(child))
             continue;
 
         if (child->isAXNodeObject()) {
