7264cf24c8a107ce0d6eaf2499650c5629c68d30
rsadam <rsadam@chromium.org>
2014-11-03 12:50:52 -0800

    Update the keyboard before notifying observers that it has changed. Observers notified of this change that query the keyboard currently receive inconsistent information.
    BUG=429699
    
    Review URL: https://codereview.chromium.org/700573002
    
    Cr-Commit-Position: refs/heads/master@{#302476}

--- chrome/browser/chromeos/accessibility/accessibility_manager.cc
+++ chrome/browser/chromeos/accessibility/accessibility_manager.cc
@@ -803,13 +803,6 @@ void AccessibilityManager::UpdateVirtualKeyboardFromPref() {
     return;
   virtual_keyboard_enabled_ = enabled;
 
-  AccessibilityStatusEventDetails details(
-      ACCESSIBILITY_TOGGLE_VIRTUAL_KEYBOARD,
-      enabled,
-      ui::A11Y_NOTIFICATION_NONE);
-
-  NotifyAccessibilityStatusChanged(details);
-
 #if defined(USE_ASH) && !defined(USE_ATHENA)
   keyboard::SetAccessibilityKeyboardEnabled(enabled);
   // Note that there are two versions of the on-screen keyboard. A full layout
@@ -824,6 +817,12 @@ void AccessibilityManager::UpdateVirtualKeyboardFromPref() {
   else
     ash::Shell::GetInstance()->DeactivateKeyboard();
 #endif
+
+  AccessibilityStatusEventDetails details(
+      ACCESSIBILITY_TOGGLE_VIRTUAL_KEYBOARD,
+      enabled,
+      ui::A11Y_NOTIFICATION_NONE);
+  NotifyAccessibilityStatusChanged(details);
 }
 
 bool AccessibilityManager::IsBrailleDisplayConnected() const {
