7f8793e54e9289c2b4d8f694d67a89cf8d9838c1
morrita@chromium.org <morrita@chromium.org>
2014-11-03 21:53:40 +0000

    Revert of The name attribute can be from contents for listitem role (patchset #1 id:1 of https://codereview.chromium.org/696953002/)
    Reason for revert:
    This seems breaking AccessibilityAriaList in content_browsertests on Mac.
    http://build.chromium.org/p/chromium.webkit/builders/Mac10.6%20Tests/builds/27219
    http://test-results.appspot.com/dashboards/flakiness_dashboard.html#master=chromium.webkit&testType=content_browsertests&tests=DumpAccessibilityTreeTest.AccessibilityAriaList
    
    
    Original issue's description:
    > The name attribute can be from contents for listitem role
    > according to http://www.w3.org/WAI/PF/aria/roles#listitem.
    > 
    > BUG=347865
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=184795
    
    TBR=dmazzoni@chromium.org,je_julie.kim@samsung.com
    NOTREECHECKS=true
    NOTRY=true
    BUG=347865
    
    Review URL: https://codereview.chromium.org/703473002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184802 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/accessibility/AXNodeObject.cpp
+++ third_party/WebKit/Source/core/accessibility/AXNodeObject.cpp
@@ -1240,7 +1240,6 @@ String AXNodeObject::title() const
     case ToggleButtonRole:
     case CheckBoxRole:
     case ListBoxOptionRole:
-    case ListItemRole:
     case MenuButtonRole:
     case MenuItemRole:
     case MenuItemCheckBoxRole:
