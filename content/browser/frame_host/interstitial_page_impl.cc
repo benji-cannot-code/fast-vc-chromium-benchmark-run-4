e8de6e5216879c7b2934d03c6bf82a8b4ebb58e2
nick <nick@chromium.org>
2014-11-03 12:31:40 -0800

    Devirtualize InterstitialPageImpl::CreateRenderViewHost
    This isn't overridden by any tests, so make it a private method.
    
    BUG=304341
    
    Review URL: https://codereview.chromium.org/697163002
    
    Cr-Commit-Position: refs/heads/master@{#302473}

--- content/browser/frame_host/interstitial_page_impl.cc
+++ content/browser/frame_host/interstitial_page_impl.cc
@@ -245,7 +245,7 @@ void InterstitialPageImpl::Show() {
   }
 
   DCHECK(!render_view_host_);
-  render_view_host_ = static_cast<RenderViewHostImpl*>(CreateRenderViewHost());
+  render_view_host_ = CreateRenderViewHost();
   render_view_host_->AttachToFrameTree();
   CreateWebContentsView();
 
@@ -554,7 +554,7 @@ WebContents* InterstitialPageImpl::web_contents() const {
   return web_contents_;
 }
 
-RenderViewHost* InterstitialPageImpl::CreateRenderViewHost() {
+RenderViewHostImpl* InterstitialPageImpl::CreateRenderViewHost() {
   if (!enabled())
     return NULL;
 
