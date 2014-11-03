69e45b2c645eba6759d97bbed32b7f26868d10b9
dcheng <dcheng@chromium.org>
2014-11-03 13:40:57 -0800

    Standardize usage of virtual/override/final specifiers.
    The Google C++ style guide states:
    
      Explicitly annotate overrides of virtual functions or virtual
      destructors with an override or (less frequently) final specifier.
      Older (pre-C++11) code will use the virtual keyword as an inferior
      alternative annotation. For clarity, use exactly one of override,
      final, or virtual when declaring an override.
    
    To better conform to these guidelines, the following constructs have
    been rewritten:
    
    - if a base class has a virtual destructor, then:
        virtual ~Foo();                   ->  ~Foo() override;
    - virtual void Foo() override;        ->  void Foo() override;
    - virtual void Foo() override final;  ->  void Foo() final;
    
    This patch was automatically generated. The clang plugin can generate
    fixit hints, which are suggested edits when it is 100% sure it knows how
    to fix a problem. The hints from the clang plugin were applied to the
    source tree using the tool in https://codereview.chromium.org/598073004.
    
    BUG=417463
    R=plundblad@chromium.org
    
    Review URL: https://codereview.chromium.org/683583003
    
    Cr-Commit-Position: refs/heads/master@{#302488}

--- chrome/browser/ui/views/accessibility/accessibility_event_router_views_unittest.cc
+++ chrome/browser/ui/views/accessibility/accessibility_event_router_views_unittest.cc
@@ -45,11 +45,11 @@ using base::ASCIIToUTF16;
 class AccessibilityViewsDelegate : public views::TestViewsDelegate {
  public:
   AccessibilityViewsDelegate() {}
-  virtual ~AccessibilityViewsDelegate() {}
+  ~AccessibilityViewsDelegate() override {}
 
   // Overridden from views::TestViewsDelegate:
-  virtual void NotifyAccessibilityEvent(
-      views::View* view, ui::AXEvent event_type) override {
+  void NotifyAccessibilityEvent(views::View* view,
+                                ui::AXEvent event_type) override {
     AccessibilityEventRouterViews::GetInstance()->HandleAccessibilityEvent(
         view, event_type);
   }
@@ -64,12 +64,12 @@ class AccessibilityWindowDelegate : public views::WidgetDelegate {
       : contents_(contents) { }
 
   // Overridden from views::WidgetDelegate:
-  virtual void DeleteDelegate() override { delete this; }
-  virtual views::View* GetContentsView() override { return contents_; }
-  virtual const views::Widget* GetWidget() const override {
+  void DeleteDelegate() override { delete this; }
+  views::View* GetContentsView() override { return contents_; }
+  const views::Widget* GetWidget() const override {
     return contents_->GetWidget();
   }
-  virtual views::Widget* GetWidget() override { return contents_->GetWidget(); }
+  views::Widget* GetWidget() override { return contents_->GetWidget(); }
 
  private:
   views::View* contents_;
@@ -85,7 +85,7 @@ class ViewWithNameAndRole : public views::View {
         role_(role) {
   }
 
-  virtual void GetAccessibleState(ui::AXViewState* state) override {
+  void GetAccessibleState(ui::AXViewState* state) override {
     views::View::GetAccessibleState(state);
     state->name = name_;
     state->role = role_;
@@ -105,7 +105,7 @@ class AccessibilityEventRouterViewsTest
   AccessibilityEventRouterViewsTest() : control_event_count_(0) {
   }
 
-  virtual void SetUp() {
+  void SetUp() override {
 #if defined(OS_WIN)
     ole_initializer_.reset(new ui::ScopedOleInitializer());
 #endif
@@ -123,7 +123,7 @@ class AccessibilityEventRouterViewsTest
     EnableAccessibilityAndListenToFocusNotifications();
   }
 
-  virtual void TearDown() {
+  void TearDown() override {
     ClearCallback();
 #if defined(USE_AURA)
     aura_test_helper_->TearDown();
@@ -463,7 +463,7 @@ class SimpleMenuDelegate : public ui::SimpleMenuModel::Delegate {
   };
 
   SimpleMenuDelegate() {}
-  virtual ~SimpleMenuDelegate() {}
+  ~SimpleMenuDelegate() override {}
 
   views::MenuItemView* BuildMenu() {
     menu_model_.reset(new ui::SimpleMenuModel(this));
@@ -480,26 +480,20 @@ class SimpleMenuDelegate : public ui::SimpleMenuModel::Delegate {
     return menu_view;
   }
 
-  virtual bool IsCommandIdChecked(int command_id) const override {
-    return false;
-  }
+  bool IsCommandIdChecked(int command_id) const override { return false; }
 
-  virtual bool IsCommandIdEnabled(int command_id) const override {
-    return true;
-  }
+  bool IsCommandIdEnabled(int command_id) const override { return true; }
 
-  virtual bool IsCommandIdVisible(int command_id) const override {
+  bool IsCommandIdVisible(int command_id) const override {
     return command_id != IDC_MENU_INVISIBLE;
   }
 
-  virtual bool GetAcceleratorForCommandId(
-      int command_id,
-      ui::Accelerator* accelerator) override {
+  bool GetAcceleratorForCommandId(int command_id,
+                                  ui::Accelerator* accelerator) override {
     return false;
   }
 
-  virtual void ExecuteCommand(int command_id, int event_flags) override {
-  }
+  void ExecuteCommand(int command_id, int event_flags) override {}
 
  private:
   scoped_ptr<ui::SimpleMenuModel> menu_model_;
