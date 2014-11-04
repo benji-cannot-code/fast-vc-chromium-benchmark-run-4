5b456d0120014adb8ad8f649e8ce8ed0e7f1498b
serya <serya@chromium.org>
2014-11-04 08:34:02 -0800

    Refactoring DevTools bridge tests: moving interface adapters to SignalingReceiverProxy.
    Benefits of this refactoring are:
    1. Removed a redundant adapter.
    2. Removed an unnecessary interface conversion (A->B->A).
    3. Removed from ClientSessionTestingHost references to unrelated class LocalSessionBridge.
    4. Reduced number of public classes.
    
    BUG=383418
    
    Review URL: https://codereview.chromium.org/700833002
    
    Cr-Commit-Position: refs/heads/master@{#302615}

--- components/devtools_bridge/test/android/javatests/src/org/chromium/components/devtools_bridge/ClientSessionTestingHost.java
+++ components/devtools_bridge/test/android/javatests/src/org/chromium/components/devtools_bridge/ClientSessionTestingHost.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.components.devtools_bridge;
 
 import java.io.IOException;
-import java.util.List;
 
 /**
  * Helper class which handles a client session in tests. Having direct reference to
@@ -14,11 +13,12 @@ import java.util.List;
  * between them to satisfy theading requirements.
  */
 public class ClientSessionTestingHost {
+    private static final String SESSION_ID = "ID";
+
     private final SignalingReceiver mTarget;
     private final SessionBase.Executor mTargetExecutor;
     private final LocalSessionBridge.ThreadedExecutor mClientExecutor;
     private final String mSessionId;
-    private int mDelayMs = 10;
     private final ClientSession mClientSession;
 
     public ClientSessionTestingHost(
@@ -31,10 +31,14 @@ public class ClientSessionTestingHost {
         mClientExecutor = new LocalSessionBridge.ThreadedExecutor();
 
         mSessionId = sessionId;
+
+        SignalingReceiverProxy proxy = new SignalingReceiverProxy(
+                mTargetExecutor, mClientExecutor, target, 0);
+
         mClientSession = new ClientSession(
                 factory,
                 mClientExecutor,
-                new TargetAdaptor().createProxy(),
+                proxy.asServerSession(SESSION_ID),
                 clientSocketName);
     }
 
@@ -54,40 +58,5 @@ public class ClientSessionTestingHost {
                 mClientSession.start(new RTCConfiguration());
             }
         });
-    }
-
-    /**
-     * Adapts ServerSessionInterface to DevToolsBridgeServer. Lives on mServerExecutor.
-     */
-    private class TargetAdaptor implements SessionBase.ServerSessionInterface {
-        /**
-         * Creates proxy that to safely use on mClientExecutor.
-         */
-        public LocalSessionBridge.ServerSessionProxy createProxy() {
-            LocalSessionBridge.ServerSessionProxy proxy =
-                    new LocalSessionBridge.ServerSessionProxy(
-                            mTargetExecutor, mClientExecutor, this, mDelayMs);
-            assert proxy.clientExecutor() == mClientExecutor;
-            assert proxy.serverExecutor() == mTargetExecutor;
-            return proxy;
-        }
-
-        @Override
-        public void startSession(RTCConfiguration config,
-                                 String offer,
-                                 SessionBase.NegotiationCallback callback) {
-            mTarget.startSession(mSessionId, config, offer, callback);
-        }
-
-        @Override
-        public void renegotiate(String offer, SessionBase.NegotiationCallback callback) {
-            mTarget.renegotiate(mSessionId, offer, callback);
-        }
-
-        @Override
-        public void iceExchange(List<String> clientCandidates,
-                                SessionBase.IceExchangeCallback callback) {
-            mTarget.iceExchange(mSessionId, clientCandidates, callback);
-        }
     }
 }
