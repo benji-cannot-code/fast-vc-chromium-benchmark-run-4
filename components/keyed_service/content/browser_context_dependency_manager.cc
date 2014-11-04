d722fd59424de11b77796d6b94013c82f03de3e4
sdefresne <sdefresne@chromium.org>
2014-11-04 10:49:35 -0800

    Cleanup #include in //components/keyed_service/content
    Remove unnecessary #includes from //components/keyed_service/content
    after recent refactoring.
    
    BUG=419366
    
    Review URL: https://codereview.chromium.org/701463003
    
    Cr-Commit-Position: refs/heads/master@{#302631}

--- components/keyed_service/content/browser_context_dependency_manager.cc
+++ components/keyed_service/content/browser_context_dependency_manager.cc
@@ -5,10 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 
-#include "base/bind.h"
 #include "base/debug/trace_event.h"
 #include "base/memory/singleton.h"
-#include "components/keyed_service/content/browser_context_keyed_base_factory.h"
 #include "content/public/browser/browser_context.h"
 
 #ifndef NDEBUG
