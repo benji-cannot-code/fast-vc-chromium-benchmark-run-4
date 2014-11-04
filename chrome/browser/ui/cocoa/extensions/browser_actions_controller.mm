d15099821513ec021068de411cedc723e1eaa721
benwells <benwells@chromium.org>
2014-11-03 19:03:18 -0800

    Revert of Make extensions that desire to act pop out if in overflow (patchset #6 id:200001 of https://codereview.chromium.org/675023002/)
    Reason for revert:
    Suspect this patch of causing errors on linux valgrind for LocationBarControllerUnitTest.NavigationClearsState.
    
    http://build.chromium.org/p/chromium.memory.fyi/builders/Linux%20Tests%20%28valgrind%29%285%29/builds/31337
    
    Sample valgrind output:
    Suppression (error hash=#606630BA25518095#):
    For more info on using suppressions see http://dev.chromium.org/developers/tree-sheriffs/sheriff-details-chromium/memory-sheriff#TOC-Suppressing-memory-reports
    {
    <insert_a_suppression_name_here>
    Memcheck:Unaddressable
    fun:_ZN16ExtensionService21NotifyExtensionLoadedEPKN10extensions9ExtensionE
    fun:_ZN16ExtensionService12AddExtensionEPKN10extensions9ExtensionE
    fun:_ZN10extensions12_GLOBAL__N_129LocationBarControllerUnitTest12AddExtensionEbRKSs
    fun:_ZN10extensions12_GLOBAL__N_156LocationBarControllerUnitTest_NavigationClearsState_Test8TestBodyEv
    }
    
    Original issue's description:
    > Make extensions that desire to act pop out if in overflow
    >
    > If an extension desires to act, it should pop itself out of
    > the overflow menu.  There should also be a visual queue for
    > extensions that are already visible, but what exactly that
    > should be is still being discussed.
    >
    > BUG=417441
    >
    > Committed: https://crrev.com/d604171517135387ca3b4c33d7f1774c8d2d38b0
    > Cr-Commit-Position: refs/heads/master@{#302511}
    
    TBR=finnur@chromium.org,pkasting@chromium.org,rdevlin.cronin@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=417441
    
    Review URL: https://codereview.chromium.org/700453003
    
    Cr-Commit-Position: refs/heads/master@{#302563}

--- chrome/browser/ui/cocoa/extensions/browser_actions_controller.mm
+++ chrome/browser/ui/cocoa/extensions/browser_actions_controller.mm
@@ -246,10 +246,6 @@ class ExtensionServiceObserverBridge
 
   void ToolbarVisibleCountChanged() override {}
 
-  void OnToolbarReorderNecessary(content::WebContents* web_contents) override {
-    // TODO(devlin): Implement on mac.
-  }
-
   void ToolbarHighlightModeChanged(bool is_highlighting) override {}
 
   Browser* GetBrowser() override { return browser_; }
@@ -360,7 +356,9 @@ class ExtensionServiceObserverBridge
 }
 
 - (void)resizeContainerAndAnimate:(BOOL)animate {
-  int iconCount = toolbarModel_->visible_icon_count();
+  int iconCount = toolbarModel_->GetVisibleIconCount();
+  if (iconCount < 0)  // If no buttons are hidden.
+    iconCount = [self buttonCount];
 
   [containerView_ resizeToWidth:[self containerWidthWithButtonCount:iconCount]
                         animate:animate];
@@ -380,8 +378,9 @@ class ExtensionServiceObserverBridge
   if (!toolbarModel_)
     return 0;
 
-  int savedButtonCount = toolbarModel_->visible_icon_count();
-  if (static_cast<NSUInteger>(savedButtonCount) > [self buttonCount])
+  int savedButtonCount = toolbarModel_->GetVisibleIconCount();
+  if (savedButtonCount < 0 ||  // all icons are visible
+      static_cast<NSUInteger>(savedButtonCount) > [self buttonCount])
     savedButtonCount = [self buttonCount];
   return [self containerWidthWithButtonCount:savedButtonCount];
 }
