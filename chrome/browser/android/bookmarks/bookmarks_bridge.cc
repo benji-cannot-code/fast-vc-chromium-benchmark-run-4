d02ad2ec0d2877eb230b76a5cfb09f2484f74d05
kkimlabs <kkimlabs@chromium.org>
2014-11-04 11:37:57 -0800

    [Android] Change bookmark search to use prefix matching.
    BUG=415774
    
    Review URL: https://codereview.chromium.org/699963002
    
    Cr-Commit-Position: refs/heads/master@{#302643}

--- chrome/browser/android/bookmarks/bookmarks_bridge.cc
+++ chrome/browser/android/bookmarks/bookmarks_bridge.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/bookmarks/browser/bookmark_utils.h"
 #include "components/bookmarks/browser/scoped_group_bookmark_actions.h"
 #include "components/bookmarks/common/android/bookmark_type.h"
+#include "components/query_parser/query_parser.h"
 #include "components/signin/core/browser/signin_manager.h"
 #include "content/public/browser/browser_thread.h"
 #include "jni/BookmarksBridge_jni.h"
@@ -666,6 +667,7 @@ void BookmarksBridge::SearchBookmarks(JNIEnv* env,
   bookmark_model_->GetBookmarksMatching(
       base::android::ConvertJavaStringToUTF16(env, j_query),
       max_results,
+      query_parser::MatchingAlgorithm::ALWAYS_PREFIX_SEARCH,
       &results);
   for (const bookmarks::BookmarkMatch& match : results) {
     const BookmarkNode* node = match.node;
