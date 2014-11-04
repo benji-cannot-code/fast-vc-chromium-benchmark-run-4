242832a9d03412ede6ab894f2994f34c8d1f1a95
pkotwicz <pkotwicz@chromium.org>
2014-11-03 21:59:42 -0800

    Use a stub implementation of GlobalShortcutListener for Athena
    BUG=426561
    TEST=None
    
    Review URL: https://codereview.chromium.org/697803002
    
    Cr-Commit-Position: refs/heads/master@{#302578}

---
+++ chrome/browser/extensions/global_shortcut_listener_athena.cc
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/logging.h"
+#include "chrome/browser/extensions/global_shortcut_listener_athena.h"
+
+namespace extensions {
+
+// static
+GlobalShortcutListener* GlobalShortcutListener::GetInstance() {
+  static GlobalShortcutListenerAthena* instance =
+      new GlobalShortcutListenerAthena();
+  return instance;
+}
+
+GlobalShortcutListenerAthena::GlobalShortcutListenerAthena() {
+  // TODO: Compile out the CommandService on Athena. crbug.com/429398
+}
+
+GlobalShortcutListenerAthena::~GlobalShortcutListenerAthena() {
+}
+
+void GlobalShortcutListenerAthena::StartListening() {
+  NOTIMPLEMENTED();
+}
+
+void GlobalShortcutListenerAthena::StopListening() {
+  NOTIMPLEMENTED();
+}
+
+bool GlobalShortcutListenerAthena::RegisterAcceleratorImpl(
+    const ui::Accelerator& accelerator) {
+  NOTIMPLEMENTED();
+  return false;
+}
+
+void GlobalShortcutListenerAthena::UnregisterAcceleratorImpl(
+    const ui::Accelerator& accelerator) {
+  NOTIMPLEMENTED();
+}
+
+}  // namespace extensions
---
+++ chrome/browser/extensions/global_shortcut_listener_athena.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_EXTENSIONS_GLOBAL_SHORTCUT_LISTENER_ATHENA_H_
+#define CHROME_BROWSER_EXTENSIONS_GLOBAL_SHORTCUT_LISTENER_ATHENA_H_
+
+#include "chrome/browser/extensions/global_shortcut_listener.h"
+
+namespace extensions {
+
+// Athena implementation of GlobalShortcutListener.
+class GlobalShortcutListenerAthena : public GlobalShortcutListener {
+ public:
+  GlobalShortcutListenerAthena();
+  ~GlobalShortcutListenerAthena() override;
+
+ private:
+  // GlobalShortcutListener:
+  void StartListening() override;
+  void StopListening() override;
+  bool RegisterAcceleratorImpl(const ui::Accelerator& accelerator) override;
+  void UnregisterAcceleratorImpl(const ui::Accelerator& accelerator) override;
+
+  DISALLOW_COPY_AND_ASSIGN(GlobalShortcutListenerAthena);
+};
+
+}  // namespace extensions
+
+#endif  // CHROME_BROWSER_EXTENSIONS_GLOBAL_SHORTCUT_LISTENER_ATHENA_H_
