3e8ee6eb931f5dca6deca570a037484e03f30c72
divya.bansal <divya.bansal@samsung.com>
2014-11-04 02:12:56 -0800

    Load any URL using Go button in chrome shell.
    Currently any other link cannot be opened using
    Go button in the soft keyboard unless it is in right
    format with http://.These changes are to fix the format
    of URL in order to open it with Go button.
    
    BUG=429100
    
    Review URL: https://codereview.chromium.org/693133002
    
    Cr-Commit-Position: refs/heads/master@{#302595}

--- chrome/android/shell/java/src/org/chromium/chrome/shell/ChromeShellToolbar.java
+++ chrome/android/shell/java/src/org/chromium/chrome/shell/ChromeShellToolbar.java
@@ -26,6 +26,7 @@ import org.chromium.base.CommandLine;
 import org.chromium.chrome.browser.EmptyTabObserver;
 import org.chromium.chrome.browser.Tab;
 import org.chromium.chrome.browser.TabObserver;
+import org.chromium.chrome.browser.UrlUtilities;
 import org.chromium.chrome.browser.appmenu.AppMenuButtonHelper;
 import org.chromium.chrome.browser.appmenu.AppMenuHandler;
 import org.chromium.chrome.shell.omnibox.SuggestionPopup;
@@ -165,7 +166,8 @@ public class ChromeShellToolbar extends LinearLayout {
 
                 // This will set |mTab| by calling showTab().
                 // TODO(aurimas): Factor out initial tab creation to the activity level.
-                Tab tab = mTabManager.openUrl(mUrlTextView.getText().toString());
+                Tab tab = mTabManager.openUrl(
+                        UrlUtilities.fixupUrl(mUrlTextView.getText().toString()));
                 mUrlTextView.clearFocus();
                 setKeyboardVisibilityForUrl(false);
                 tab.getView().requestFocus();
