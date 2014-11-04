85b4cc50dd00d5a35c9292585eab9a2a43316563
tzik <tzik@chromium.org>
2014-11-04 00:44:27 -0800

    Revert of Add --root-layer-scrolls. (patchset #2 id:20001 of https://codereview.chromium.org/662973003/)
    Reason for revert:
    This CL causes build failure:
    http://build.chromium.org/p/chromium.chromiumos/buildstatus?builder=Linux%20ChromiumOS%20Builder%20%28dbg%29&number=54070
    
    The error log is:
    FAILED: /b/build/goma/gomacc ../../third_party/llvm-build/Release+Asserts/bin/clang++ (snip) -o interactive_ui_tests (snip)
    obj/chrome/libbrowser_chromeos.a(obj/chrome/browser/chromeos/login/browser_chromeos.chrome_restart_request.o):../../chrome/browser/chromeos/login/chrome_restart_request.cc:chromeos::(anonymous namespace)::DeriveCommandLine(GURL const&, base::CommandLine const&, base::DictionaryValue const&, base::CommandLine*)::kForwardSwitches:error: undefined reference to 'switches::kRootLayerScrolls'
    clang:error: linker command failed with exit code 1 (use -v to see invocation)
    ninja: build stopped: subcommand failed.
    
    Original issue's description:
    > Add --root-layer-scrolls.
    >
    > BUG=417782
    >
    > Committed: https://crrev.com/2eeec6853a1579d09754acfa9961e4dc36aa9264
    > Cr-Commit-Position: refs/heads/master@{#302588}
    
    TBR=vollick@chromium.org,jam@chromium.org,skobes@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=417782
    
    Review URL: https://codereview.chromium.org/695613005
    
    Cr-Commit-Position: refs/heads/master@{#302590}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1176,7 +1176,6 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kRegisterPepperPlugins,
     switches::kRendererAssertTest,
     switches::kRendererStartupDialog,
-    switches::kRootLayerScrolls,
     switches::kShowPaintRects,
     switches::kSitePerProcess,
     switches::kStatsCollectionController,
