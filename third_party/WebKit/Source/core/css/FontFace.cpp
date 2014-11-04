aec76415fe5412896ea309b163f896f0dd6f0bcf
bashi@chromium.org <bashi@chromium.org>
2014-11-04 09:49:12 +0000

    IDL: ArrayBuffer and ArrayBufferView support for union types
    (ArrayBuffer or ArrayBufferView) is a common union type usage. For example,
    CSS font loading[1] and fetch API[2] use it. Implement and use them for
    FontFace constructor.
    
    Change for FontFace is covered by fast/css/fontface-arraybuffer.html
    
    This CL also fixed two minor bugs:
    - Collect union types from constructors (we didn't before)
    - We need to use 'implemented_as' for forward class declarations, instead of
      'name'.
    
    [1] http://dev.w3.org/csswg/css-font-loading/#fontface-interface
    [2] https://fetch.spec.whatwg.org/#body-mixin
    
    BUG=240176
    
    Review URL: https://codereview.chromium.org/699713003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184824 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/FontFace.cpp
+++ third_party/WebKit/Source/core/css/FontFace.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "bindings/core/v8/ExceptionState.h"
 #include "bindings/core/v8/ScriptState.h"
+#include "bindings/core/v8/UnionTypesCore.h"
 #include "core/CSSValueKeywords.h"
 #include "core/css/BinaryDataFontFaceSource.h"
 #include "core/css/CSSFontFace.h"
@@ -67,6 +68,18 @@ static PassRefPtrWillBeRawPtr<CSSValue> parseCSSValue(const Document* document, 
 {
     CSSParserContext context(*document, UseCounter::getFrom(document));
     return CSSParser::parseSingleValue(propertyID, s, context);
+}
+
+PassRefPtrWillBeRawPtr<FontFace> FontFace::create(ExecutionContext* context, const AtomicString& family, StringOrArrayBufferOrArrayBufferView& source, const FontFaceDescriptors& descriptors)
+{
+    if (source.isString())
+        return create(context, family, source.getAsString(), descriptors);
+    if (source.isArrayBuffer())
+        return create(context, family, source.getAsArrayBuffer(), descriptors);
+    if (source.isArrayBufferView())
+        return create(context, family, source.getAsArrayBufferView(), descriptors);
+    ASSERT_NOT_REACHED();
+    return nullptr;
 }
 
 PassRefPtrWillBeRawPtr<FontFace> FontFace::create(ExecutionContext* context, const AtomicString& family, const String& source, const FontFaceDescriptors& descriptors)
