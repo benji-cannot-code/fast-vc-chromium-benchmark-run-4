c14a6531c2f7d5c3f21e82894109bbb271ed161c
Paweł Hajdan, Jr <phajdan.jr@chromium.org>
2014-11-05 16:04:49 +0100

    Clean up PRESUBMIT.py - remove references to removed clang trybots
    BUG=none
    R=maruel@chromium.org
    
    Review URL: https://codereview.chromium.org/683733009
    
    Cr-Commit-Position: refs/heads/master@{#302800}

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -1715,7 +1715,6 @@ def GetPreferredTryMasters(project, change):
       'ios_rel_device',
       'ios_rel_device_ninja',
       'linux_chromium_chromeos_rel',
-      'linux_chromium_clang_dbg',
       'linux_chromium_gn_dbg',
       'linux_chromium_gn_rel',
       'linux_chromium_rel_ng',
@@ -1735,7 +1734,6 @@ def GetPreferredTryMasters(project, change):
   if any(re.search(r'[\\\/_](aura|chromeos)', f) for f in files):
     builders.extend([
         'linux_chromeos_asan',
-        'linux_chromium_chromeos_clang_dbg'
     ])
 
   # If there are gyp changes to base, build, or chromeos, run a full cros build
