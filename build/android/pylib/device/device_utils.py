7b18973eadc06df891bb14440e1685938221be28
perezju <perezju@chromium.org>
2014-11-05 11:56:24 -0800

    Make IsOnline return False on device errors
    BUG=430262
    
    Review URL: https://codereview.chromium.org/703933002
    
    Cr-Commit-Position: refs/heads/master@{#302853}

--- build/android/pylib/device/device_utils.py
+++ build/android/pylib/device/device_utils.py
@@ -110,7 +110,11 @@ class DeviceUtils(object):
     Raises:
       CommandTimeoutError on timeout.
     """
-    return self.adb.GetState() == 'device'
+    try:
+      return self.adb.GetState() == 'device'
+    except device_errors.BaseError as exc:
+      logging.info('Failed to get state: %s', exc)
+      return False
 
   @decorators.WithTimeoutAndRetriesFromInstance()
   def HasRoot(self, timeout=None, retries=None):
@@ -240,7 +244,7 @@ class DeviceUtils(object):
     """
     def sd_card_ready():
       return self.RunShellCommand(['ls', self.GetExternalStoragePath()],
-                                    single_line=True, check_return=True)
+                                  check_return=True)
 
     def pm_ready():
       try:
