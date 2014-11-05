ca00de9576baf4fc22dbf89f03711801ad16f432
tbarzic <tbarzic@chromium.org>
2014-11-05 09:32:49 -0800

    Easy Unlock Remove en-US guards for strings that were merged to M39
    These should not be needed anymore.
    
    BUG=NONE
    TEST=None
    
    Review URL: https://codereview.chromium.org/703803002
    
    Cr-Commit-Position: refs/heads/master@{#302824}

--- chrome/browser/signin/easy_unlock_screenlock_state_handler_unittest.cc
+++ chrome/browser/signin/easy_unlock_screenlock_state_handler_unittest.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/signin/easy_unlock_service.h"
 #include "chrome/browser/signin/screenlock_bridge.h"
 #include "chrome/grit/generated_resources.h"
-#include "chrome/test/base/testing_browser_process.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/l10n/l10n_util.h"
 
@@ -199,8 +198,6 @@ class EasyUnlockScreenlockStateHandlerTest : public testing::Test {
   ~EasyUnlockScreenlockStateHandlerTest() override {}
 
   void SetUp() override {
-    TestingBrowserProcess::GetGlobal()->SetApplicationLocale("en-US");
-
     // Create and inject fake lock handler to the screenlock bridge.
     lock_handler_.reset(new TestLockHandler(user_email_));
     ScreenlockBridge* screenlock_bridge = ScreenlockBridge::Get();
