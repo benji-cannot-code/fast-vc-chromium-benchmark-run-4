9b742032960faedaaa2ec8284d7999300747711a
dongseong.hwang <dongseong.hwang@intel.com>
2014-11-05 00:59:41 -0800

    Check if there is error on waiting for GL Fence.
    DCHECK can catch a potential driver bug.
    
    Review URL: https://codereview.chromium.org/685003007
    
    Cr-Commit-Position: refs/heads/master@{#302769}

--- ui/gl/gl_fence_egl.cc
+++ ui/gl/gl_fence_egl.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/gl/gl_fence_egl.h"
 
+#include "ui/gl/egl_util.h"
 #include "ui/gl/gl_bindings.h"
 #include "ui/gl/gl_context.h"
 
@@ -25,6 +26,8 @@ bool GLFenceEGL::HasCompleted() {
   EGLint value = 0;
   if (eglGetSyncAttribKHR(display_, sync_, EGL_SYNC_STATUS_KHR, &value) !=
       EGL_TRUE) {
+    LOG(ERROR) << "Failed to get EGLSync attribute. error code:"
+               << eglGetError();
     return true;
   }
 
@@ -36,7 +39,12 @@ void GLFenceEGL::ClientWait() {
   if (!flush_event_.get() || flush_event_->IsSignaled()) {
     EGLint flags = 0;
     EGLTimeKHR time = EGL_FOREVER_KHR;
-    eglClientWaitSyncKHR(display_, sync_, flags, time);
+    EGLint result = eglClientWaitSyncKHR(display_, sync_, flags, time);
+    DCHECK_NE(EGL_TIMEOUT_EXPIRED_KHR, result);
+    if (result == EGL_FALSE) {
+      LOG(FATAL) << "Failed to wait for EGLSync. error:"
+                 << ui::GetLastEGLErrorString();
+    }
   } else {
     LOG(ERROR) << "Trying to wait for uncommitted fence. Skipping...";
   }
@@ -49,7 +57,10 @@ void GLFenceEGL::ServerWait() {
   }
   if (!flush_event_.get() || flush_event_->IsSignaled()) {
     EGLint flags = 0;
-    eglWaitSyncKHR(display_, sync_, flags);
+    if (eglWaitSyncKHR(display_, sync_, flags) == EGL_FALSE) {
+      LOG(FATAL) << "Failed to wait for EGLSync. error:"
+                 << ui::GetLastEGLErrorString();
+    }
   } else {
     LOG(ERROR) << "Trying to wait for uncommitted fence. Skipping...";
   }
