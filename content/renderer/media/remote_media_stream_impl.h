073cc67c52493203dfa7e4d2b1c8062552e02baf
tommi <tommi@chromium.org>
2014-11-05 12:07:12 -0800

    Separate unregistration from destruction of webrtc observers. This is to avoid a situation where we addref on the signaling thread while destructing and unregistering from another thread. Unregistration has to happen before the object can be released.
    BUG=378065
    TBR=perkj
    
    Review URL: https://codereview.chromium.org/706613002
    
    Cr-Commit-Position: refs/heads/master@{#302854}

--- content/renderer/media/remote_media_stream_impl.h
+++ content/renderer/media/remote_media_stream_impl.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/single_thread_task_runner.h"
+#include "base/threading/thread_checker.h"
 #include "content/common/content_export.h"
 #include "third_party/libjingle/source/talk/app/webrtc/mediastreaminterface.h"
 #include "third_party/WebKit/public/platform/WebMediaStream.h"
@@ -79,6 +80,7 @@ class CONTENT_EXPORT RemoteMediaStreamImpl {
     base::WeakPtr<RemoteMediaStreamImpl> media_stream_;
     const scoped_refptr<base::SingleThreadTaskRunner> main_thread_;
     const scoped_refptr<webrtc::MediaStreamInterface> webrtc_stream_;
+    base::ThreadChecker ctor_thread_;
   };
 
   void OnChanged(scoped_ptr<RemoteAudioTrackAdapters> audio_tracks,
--- content/renderer/media/webrtc/media_stream_remote_video_source.cc
+++ content/renderer/media/webrtc/media_stream_remote_video_source.cc
@@ -153,9 +153,15 @@ MediaStreamRemoteVideoSource::Observer::state() const {
   return state_;
 }
 
-MediaStreamRemoteVideoSource::Observer::~Observer() {
+void MediaStreamRemoteVideoSource::Observer::Unregister() {
   DCHECK(main_thread_->BelongsToCurrentThread());
   track_->UnregisterObserver(this);
+  track_ = nullptr;
+}
+
+MediaStreamRemoteVideoSource::Observer::~Observer() {
+  DCHECK(main_thread_->BelongsToCurrentThread());
+  DCHECK(!track_.get());
 }
 
 void MediaStreamRemoteVideoSource::Observer::SetSource(
@@ -198,6 +204,7 @@ MediaStreamRemoteVideoSource::MediaStreamRemoteVideoSource(
 
 MediaStreamRemoteVideoSource::~MediaStreamRemoteVideoSource() {
   DCHECK(CalledOnValidThread());
+  observer_->Unregister();
   observer_ = nullptr;
 }
 
