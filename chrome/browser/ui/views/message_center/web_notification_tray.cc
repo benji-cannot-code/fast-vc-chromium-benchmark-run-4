75f80027d15a0e2c7219633534da26d791f887cb
pkotwicz <pkotwicz@chromium.org>
2014-11-05 11:42:38 -0800

    Hack to avoid leaving zombie system tray icons on KDE
    It is not possible to remove an icon from the system tray once it is added to
    the system tray on KDE (apart for terminating the application) Avoid recreating
    system tray icons each time that the number of visible notifications drops to
    zero.
    
    BUG=419673
    TEST=Manual, see bug
    
    Review URL: https://codereview.chromium.org/699953002
    
    Cr-Commit-Position: refs/heads/master@{#302850}

--- chrome/browser/ui/views/message_center/web_notification_tray.cc
+++ chrome/browser/ui/views/message_center/web_notification_tray.cc
@@ -32,6 +32,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/strings/grit/ui_strings.h"
 #include "ui/views/widget/widget.h"
 
+#if defined(OS_LINUX)
+#include "base/environment.h"
+#include "base/nix/xdg_util.h"
+#endif
+
 namespace {
 
 // Tray constants
@@ -59,6 +64,20 @@ gfx::ImageSkia* GetIcon(int unread_count, bool is_quiet_mode) {
   }
 
   return rb.GetImageSkiaNamed(resource_id);
+}
+
+bool CanDestroyStatusIcon() {
+#if defined(OS_LINUX)
+  // Avoid creating multiple system tray icons on KDE4 and newer versions of KDE
+  // because the OS does not support removing system tray icons.
+  // TODO(pkotwicz): This is a hack for the sake of M40. Fix this properly.
+  scoped_ptr<base::Environment> env(base::Environment::Create());
+  base::nix::DesktopEnvironment desktop_environment =
+      base::nix::GetDesktopEnvironment(env.get());
+  return desktop_environment != base::nix::DESKTOP_ENVIRONMENT_KDE4;
+#else
+  return true;
+#endif
 }
 
 }  // namespace
@@ -277,7 +296,8 @@ void WebNotificationTray::UpdateStatusIcon() {
     tool_tip = l10n_util::GetStringUTF16(IDS_MESSAGE_CENTER_TOOLTIP);
   }
 
-  if (message_center()->GetVisibleNotifications().empty()) {
+  if (message_center()->GetVisibleNotifications().empty() &&
+      CanDestroyStatusIcon()) {
     DestroyStatusIcon();
     return;
   }
