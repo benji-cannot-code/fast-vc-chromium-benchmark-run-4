87886e3a2eb588bd828f1c428de0de9d5c05cafd
tzik <tzik@chromium.org>
2014-11-05 06:53:01 -0800

    Remove #include of excluded file from folder_image.cc
    views/ is excluded in app_list.gyp on toolkit_views==0 case.
    This #include implicitly depends on generated ui/accessibility/ax_enums.h, without required dependency entry in .gyp.
    
    TBR=mgiuca@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/701143002
    
    Cr-Commit-Position: refs/heads/master@{#302798}

--- ui/app_list/folder_image.cc
+++ ui/app_list/folder_image.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/app_list/app_list_constants.h"
 #include "ui/app_list/app_list_item.h"
 #include "ui/app_list/app_list_item_list.h"
-#include "ui/app_list/views/contents_view.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/geometry/point.h"
