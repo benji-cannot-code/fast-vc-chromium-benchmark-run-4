b45d31d054a3720b44b364a084ca3eba63340578
christiank <christiank@opera.com>
2014-11-05 02:49:14 -0800

    Update touch selection to only modify one selection point at a time.
    This is made in preparation for enabling selection auto scroll using touch
    handles.
    
    BUG=340658
    
    Review URL: https://codereview.chromium.org/657803002
    
    Cr-Commit-Position: refs/heads/master@{#302776}

--- content/browser/renderer_host/render_widget_host_unittest.cc
+++ content/browser/renderer_host/render_widget_host_unittest.cc
@@ -1302,7 +1302,7 @@ TEST_F(RenderWidgetHostTest, InputRouterReceivesHandleInputEvent_ACK) {
 TEST_F(RenderWidgetHostTest, InputRouterReceivesMoveCaret_ACK) {
   host_->SetupForInputRouterTest();
 
-  host_->OnMessageReceived(ViewHostMsg_MoveCaret_ACK(0));
+  host_->OnMessageReceived(InputHostMsg_MoveCaret_ACK(0));
 
   EXPECT_TRUE(host_->mock_input_router()->message_received_);
 }
@@ -1310,7 +1310,7 @@ TEST_F(RenderWidgetHostTest, InputRouterReceivesMoveCaret_ACK) {
 TEST_F(RenderWidgetHostTest, InputRouterReceivesSelectRange_ACK) {
   host_->SetupForInputRouterTest();
 
-  host_->OnMessageReceived(ViewHostMsg_SelectRange_ACK(0));
+  host_->OnMessageReceived(InputHostMsg_SelectRange_ACK(0));
 
   EXPECT_TRUE(host_->mock_input_router()->message_received_);
 }
