60d347dce735c274f882d932351e56da9edbb0ea
Robert Sesek <rsesek@chromium.org>
2014-11-05 14:45:51 -0500

    Disable some failing AppWindowInteractiveTest tests on 10.9.
    BUG=430551
    TBR=tapted@chromium.org
    
    Review URL: https://codereview.chromium.org/706583003
    
    Cr-Commit-Position: refs/heads/master@{#302852}

--- chrome/browser/apps/app_window_interactive_uitest.cc
+++ chrome/browser/apps/app_window_interactive_uitest.cc
@@ -87,9 +87,9 @@ class AppWindowInteractiveTest : public extensions::PlatformAppBrowserTest {
 };
 
 IN_PROC_BROWSER_TEST_F(AppWindowInteractiveTest, ESCLeavesFullscreenWindow) {
-// This test is flaky on MacOS 10.6.
+// This test is flaky on MacOS 10.6 and 10.9.
 #if defined(OS_MACOSX) && !defined(OS_IOS)
-  if (base::mac::IsOSSnowLeopard())
+  if (base::mac::IsOSSnowLeopard() || base::mac::IsOSMavericks())
     return;
 #endif
 
@@ -241,9 +241,9 @@ IN_PROC_BROWSER_TEST_F(AppWindowInteractiveTest,
 
 IN_PROC_BROWSER_TEST_F(AppWindowInteractiveTest,
                        ESCDoesNotLeaveFullscreenDOM) {
-// This test is flaky on MacOS 10.6.
+// This test is flaky on MacOS 10.6 and 10.9.
 #if defined(OS_MACOSX) && !defined(OS_IOS)
-  if (base::mac::IsOSSnowLeopard())
+  if (base::mac::IsOSSnowLeopard() || base::mac::IsOSMavericks())
     return;
 #endif
 
@@ -299,9 +299,9 @@ IN_PROC_BROWSER_TEST_F(AppWindowInteractiveTest,
 // and 'overrideEscFullscreen'.
 IN_PROC_BROWSER_TEST_F(AppWindowInteractiveTest,
                        ESCDoesNotLeaveFullscreenOldPermission) {
-// This test is flaky on MacOS 10.6.
+// This test is flaky on MacOS 10.6 and 10.9.
 #if defined(OS_MACOSX) && !defined(OS_IOS)
-  if (base::mac::IsOSSnowLeopard())
+  if (base::mac::IsOSSnowLeopard() || base::mac::IsOSMavericks())
     return;
 #endif
 
