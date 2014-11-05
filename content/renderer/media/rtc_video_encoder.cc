4d8a210ee22064f815f34251d08ee66f19d22fe9
hshi <hshi@chromium.org>
2014-11-04 18:28:54 -0800

    Deprecate CAST1 payload.
    BUG=430323
    TEST=trybot and manual testing of tab casting
    R=hclam@chromium.org
    TBR=dalecurtis@chromium.org
    
    Review URL: https://codereview.chromium.org/695643003
    
    Cr-Commit-Position: refs/heads/master@{#302747}

--- content/renderer/media/rtc_video_encoder.cc
+++ content/renderer/media/rtc_video_encoder.cc
@@ -47,8 +47,6 @@ media::VideoCodecProfile WebRTCVideoCodecToVideoCodecProfile(
           return media::H264PROFILE_MAIN;
       }
     }
-    case webrtc::kVideoCodecGeneric:
-      return media::H264PROFILE_MAIN;
     default:
       NOTREACHED() << "Unrecognized video codec type";
       return media::VIDEO_CODEC_PROFILE_UNKNOWN;
@@ -715,9 +713,7 @@ void RTCVideoEncoder::ReturnEncodedImage(scoped_ptr<webrtc::EncodedImage> image,
   memset(&header, 0, sizeof(header));
   switch (video_codec_type_) {
     case webrtc::kVideoCodecVP8:
-    case webrtc::kVideoCodecGeneric:
       // Generate a header describing a single fragment.
-      // Note that webrtc treats the generic-type payload as an opaque buffer.
       header.VerifyAndAllocateFragmentationHeader(1);
       header.fragmentationOffset[0] = 0;
       header.fragmentationLength[0] = image->_length;
