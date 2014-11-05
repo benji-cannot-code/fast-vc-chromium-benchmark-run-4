d5adf6e2098fafe5454aab9c76afc267bccf3038
Sadrul Habib Chowdhury <sadrul@chromium.org>
2014-11-05 16:50:13 -0500

    athena: Make sure the ActivityViewModel is always initialized correctly.
    The ActivityViewModel should always be initialized, whether the Activity
    creates its own views::Widget or not.
    
    BUG=430030
    R=oshima@chromium.org
    
    Review URL: https://codereview.chromium.org/704623003
    
    Cr-Commit-Position: refs/heads/master@{#302877}

--- athena/activity/activity_manager_impl.cc
+++ athena/activity/activity_manager_impl.cc
@@ -35,7 +35,6 @@ views::Widget* CreateWidget(Activity* activity) {
   params.delegate =
       new ActivityWidgetDelegate(activity->GetActivityViewModel());
   widget->Init(params);
-  activity->GetActivityViewModel()->Init();
   return widget;
 }
 
@@ -73,6 +72,8 @@ void ActivityManagerImpl::AddActivity(Activity* activity) {
   activities_.insert(activities_.begin(), activity);
   views::Widget* widget = CreateWidget(activity);
   widget->GetNativeView()->AddObserver(this);
+  activity->GetActivityViewModel()->Init();
+
   FOR_EACH_OBSERVER(ActivityManagerObserver,
                     observers_,
                     OnActivityStarted(activity));
