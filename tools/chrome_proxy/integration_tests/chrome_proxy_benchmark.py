962b95b6349e6efccff2f3c2a036883472c4201a
sclittle <sclittle@chromium.org>
2014-11-06 14:56:38 -0800

    Add a data reduction proxy telemetry test for chrome proxy client type.
    This change adds a data reduction proxy telemetry test for the chrome
    proxy client type directive.
    
    BUG=414536
    
    Review URL: https://codereview.chromium.org/698883007
    
    Cr-Commit-Position: refs/heads/master@{#303109}

--- tools/chrome_proxy/integration_tests/chrome_proxy_benchmark.py
+++ tools/chrome_proxy/integration_tests/chrome_proxy_benchmark.py
@@ -77,6 +77,13 @@ class ChromeProxyClientVersion(benchmark.Benchmark):
 
 
 @benchmark.Enabled('android')
+class ChromeProxyClientType(benchmark.Benchmark):
+  tag = 'client_type'
+  test = measurements.ChromeProxyClientType
+  page_set = pagesets.ClientTypePageSet
+
+
+@benchmark.Enabled('android')
 class ChromeProxyBypass(benchmark.Benchmark):
   tag = 'bypass'
   test = measurements.ChromeProxyBypass
