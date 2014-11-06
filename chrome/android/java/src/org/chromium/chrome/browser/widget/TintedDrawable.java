2f7058a09cfd2693ecd3a214d59f68a0c30b3f38
Aurimas Liutikas <aurimas@google.com>
2014-11-06 14:50:32 -0800

    Prepare for switching TabSwitcherDrawable to TintedDrawable.
    BUG=427719
    R=dtrainor@chromium.org
    
    Review URL: https://codereview.chromium.org/703303002
    
    Cr-Commit-Position: refs/heads/master@{#303107}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/TintedDrawable.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/TintedDrawable.java
@@ -19,7 +19,10 @@ import org.chromium.chrome.R;
  * bitmap drawable states using chrome:tint attribute in XML.
  */
 public class TintedDrawable extends BitmapDrawable {
-    private ColorStateList mTint;
+    /**
+     * The set of colors that just be used for tinting this bitmap drawable.
+     */
+    protected ColorStateList mTint;
 
     public TintedDrawable(Resources res, Bitmap bitmap) {
         super(res, bitmap);
@@ -28,8 +31,9 @@ public class TintedDrawable extends BitmapDrawable {
 
     @Override
     protected boolean onStateChange(int[] state) {
-        updateTintColor();
-        return super.onStateChange(state);
+        boolean ret = updateTintColor();
+        super.onStateChange(state);
+        return ret;
     }
 
     @Override
