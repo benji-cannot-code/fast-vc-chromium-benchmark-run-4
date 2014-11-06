10ee919feca3c718685913ae2f666b65e7a67645
danakj <danakj@chromium.org>
2014-11-06 14:05:49 -0800

    cc: DCHECK that pile size == layer size during draw if pile isn't empty.
    Ensure that when the pile and layer size don't match during push
    properties that the pile is invalidated and given an empty size, since
    it means Update was skipped (layer isn't part of the frame) and the
    pile is not longer up to date.
    
    In AppendQuads only dcheck the layer bounds() == pile's size when the
    pile is not empty. A layer may animate or scroll into the viewport
    while it was not part of the main thread frame. We will checkerboard in
    that case.
    
    Adds a test to ensure we don't consider the pile to be solid color once
    we make it empty to avoid drawing a solid color if that isn't correct
    in this scenario. An empty pile shouldn't be used to draw with.
    
    R=enne, vmpstr
    BUG=430894
    
    Review URL: https://codereview.chromium.org/711503002
    
    Cr-Commit-Position: refs/heads/master@{#303097}

--- cc/resources/picture_pile.cc
+++ cc/resources/picture_pile.cc
@@ -539,9 +539,7 @@ bool PicturePile::UpdateAndExpandInvalidation(
 
 void PicturePile::SetEmptyBounds() {
   tiling_.SetTilingSize(gfx::Size());
-  picture_map_.clear();
-  has_any_recordings_ = false;
-  recorded_viewport_ = gfx::Rect();
+  Clear();
 }
 
 void PicturePile::DetermineIfSolidColor() {
