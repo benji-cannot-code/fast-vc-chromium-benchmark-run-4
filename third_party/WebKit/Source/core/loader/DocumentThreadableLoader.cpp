e772098ea0c8556bfbece27a3ac5f921c36b73f3
tyoshino@chromium.org <tyoshino@chromium.org>
2014-11-06 09:48:16 +0000

    [DocumentThreadableLoader] Rename notifyResponseReceived to reportResponseReceived
    "notifySomething" style method naming is often used to notify its
    client about completion. This is just for inspector and console logging.
    It's surprising that we see a function looks like notifying completion
    called inside access control failure path. Use reportSomething that
    is often used to methods that report to inspector or writing some log.
    
    R=mkwst
    BUG=none
    
    Review URL: https://codereview.chromium.org/700413002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184905 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/DocumentThreadableLoader.cpp
+++ third_party/WebKit/Source/core/loader/DocumentThreadableLoader.cpp
@@ -381,7 +381,7 @@ void DocumentThreadableLoader::handlePreflightResponse(const ResourceResponse& r
     CrossOriginPreflightResultCache::shared().appendEntry(securityOrigin()->toString(), m_actualRequest->url(), preflightResult.release());
 }
 
-void DocumentThreadableLoader::notifyResponseReceived(unsigned long identifier, const ResourceResponse& response)
+void DocumentThreadableLoader::reportResponseReceived(unsigned long identifier, const ResourceResponse& response)
 {
     DocumentLoader* loader = m_document.frame()->loader().documentLoader();
     TRACE_EVENT_INSTANT1(TRACE_DISABLED_BY_DEFAULT("devtools.timeline"), "ResourceReceiveResponse", "data", InspectorReceiveResponseEvent::data(identifier, m_document.frame(), response));
@@ -396,7 +396,7 @@ void DocumentThreadableLoader::handleResponse(unsigned long identifier, const Re
     ASSERT(m_client);
 
     if (m_actualRequest) {
-        notifyResponseReceived(identifier, response);
+        reportResponseReceived(identifier, response);
         handlePreflightResponse(response);
         return;
     }
@@ -417,7 +417,7 @@ void DocumentThreadableLoader::handleResponse(unsigned long identifier, const Re
     if (!m_sameOriginRequest && m_options.crossOriginRequestPolicy == UseAccessControl) {
         String accessControlErrorDescription;
         if (!passesAccessControlCheck(response, effectiveAllowCredentials(), securityOrigin(), accessControlErrorDescription)) {
-            notifyResponseReceived(identifier, response);
+            reportResponseReceived(identifier, response);
             m_client->didFailAccessControlCheck(ResourceError(errorDomainBlinkInternal, 0, response.url().string(), accessControlErrorDescription));
             return;
         }
