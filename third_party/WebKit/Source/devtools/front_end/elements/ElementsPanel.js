14689d2dc1408e4981a96919c0cc1056847b6d06
vsevik@chromium.org <vsevik@chromium.org>
2014-11-06 11:03:03 +0000

    DevTools: Add super.handleShortcut call to ElementsPanel.handleShortcut()
    This fixes a search shortcut issue.
    
    R=pfeldman, apavlov
    
    Review URL: https://codereview.chromium.org/695323004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184907 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/elements/ElementsPanel.js
+++ third_party/WebKit/Source/devtools/front_end/elements/ElementsPanel.js
@@ -729,6 +729,10 @@ WebInspector.ElementsPanel.prototype = {
         }
 
         treeOutline.handleShortcut(event);
+        if (event.handled)
+            return;
+
+        WebInspector.Panel.prototype.handleShortcut.call(this, event);
     },
 
     /**
