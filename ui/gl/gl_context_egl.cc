fbbe9ce6660b0da5bbd00b60dbb117def6e6b36a
bajones <bajones@chromium.org>
2014-11-06 11:47:27 -0800

    Allow Windows to use system Vsync for a single window each swap
    This makes the common case (one browser window) smoother, but doesn't
    help multi-window smoothness much.
    
    BUG=422000
    
    Review URL: https://codereview.chromium.org/708483003
    
    Cr-Commit-Position: refs/heads/master@{#303074}

--- ui/gl/gl_context_egl.cc
+++ ui/gl/gl_context_egl.cc
@@ -184,6 +184,8 @@ void GLContextEGL::SetSwapInterval(int interval) {
   if (!eglSwapInterval(display_, interval)) {
     LOG(ERROR) << "eglSwapInterval failed with error "
                << GetLastEGLErrorString();
+  } else {
+    GLSurface::GetCurrent()->SetSwapInterval(interval);
   }
 }
 
