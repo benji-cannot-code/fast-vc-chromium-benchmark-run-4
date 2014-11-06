82f3e22d6bc70e4080a009f3c2720a49b8b6abb4
eroman <eroman@chromium.org>
2014-11-05 17:08:06 -0800

    Refactor: Extract some more functionality from RSA used by EC.
    * Moves some test helper functions to a shared file, for re-use by a subsequent EC change.
    * Moves some BigNumber helpers to shared location
    * Renames some properties in JSON RSA test file.
    
    BUG=399094
    
    Review URL: https://codereview.chromium.org/701903003
    
    Cr-Commit-Position: refs/heads/master@{#302933}

--- content/child/webcrypto/openssl/util_openssl.h
+++ content/child/webcrypto/openssl/util_openssl.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_CHILD_WEBCRYPTO_OPENSSL_UTIL_OPENSSL_H_
 #define CONTENT_CHILD_WEBCRYPTO_OPENSSL_UTIL_OPENSSL_H_
 
+#include <string>
 #include <vector>
 
 #include <openssl/ossl_typ.h>
@@ -73,6 +74,12 @@ Status ImportUnverifiedPkeyFromSpki(const CryptoData& key_data,
 Status ImportUnverifiedPkeyFromPkcs8(const CryptoData& key_data,
                                      int expected_pkey_id,
                                      crypto::ScopedEVP_PKEY* pkey);
+
+// Allocates a new BIGNUM given a std::string big-endian representation.
+BIGNUM* CreateBIGNUM(const std::string& n);
+
+// Converts a BIGNUM to a big endian byte array.
+std::vector<uint8_t> BIGNUMToVector(const BIGNUM* n);
 
 }  // namespace webcrypto
 
