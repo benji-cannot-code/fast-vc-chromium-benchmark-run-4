6d94816aac8d43bff40a8138591948a505a7d207
oshima <oshima@chromium.org>
2014-11-06 11:11:00 -0800

    JS dialogs for athena
    Adds athena impl for following client/host
    
    web_modal::ModalDialogHost
    ConstrainedWindowViewsClient
    JavaScriptDialogManagerClient
    
    This depends on https://codereview.chromium.org/666533007/
    
    BUG=410499
    TBR=yoz@chromium.org, wittman@chromium.rg, msw@chromium.org,  avi@chromium.org
    
    Review URL: https://codereview.chromium.org/692563002
    
    Cr-Commit-Position: refs/heads/master@{#303062}

--- athena/extensions/extensions_delegate.cc
+++ athena/extensions/extensions_delegate.cc
@@ -5,7 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "athena/extensions/public/extensions_delegate.h"
 
+#include "athena/extensions/athena_constrained_window_views_client.h"
+#include "athena/extensions/athena_javascript_native_dialog_factory.h"
 #include "base/logging.h"
+#include "extensions/components/javascript_dialog_extensions_client/javascript_dialog_extension_client_impl.h"
 
 namespace athena {
 namespace {
@@ -15,11 +18,15 @@ ExtensionsDelegate* instance = nullptr;
 }  // namespace
 
 ExtensionsDelegate::ExtensionsDelegate() {
+  InstallConstrainedWindowViewsClient();
+  InstallJavaScriptDialogExtensionsClient();
+  InstallJavaScriptNativeDialogFactory();
   DCHECK(!instance);
   instance = this;
 }
 
 ExtensionsDelegate::~ExtensionsDelegate() {
+  UninstallConstrainedWindowViewsClient();
   DCHECK(instance);
   instance = nullptr;
 }
