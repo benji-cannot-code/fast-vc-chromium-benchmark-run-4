ddf25a48bfebaaf12077b1a85810fb3be4002050
zea <zea@chromium.org>
2014-11-06 14:39:45 -0800

    Revert of [Android] Support unfocusable container views. (patchset #4 id:60001 of https://codereview.chromium.org/701233002/)
    Reason for revert:
    Introduced flake into WebRTCBrowserTest.* http://build.chromium.org/p/chromium.linux/buildstatus?builder=Android%20Tests%20%28dbg%29&number=23952
    
    Example:
    Value of: ExecuteScriptAndExtractString(shell()->web_contents(), javascript, &result)
      Actual: false
    Expected: true
    From javascript: ../../content/test/webrtc_content_browsertest_base.cc:71: Failure
    Failed
    [INFO:CONSOLE(883)] "Receiving remote stream...", source: http://127.0.0.1:50076/media/peerconnection-call.html (883)
    [INFO:CONSOLE(859)] "Receiving answer...", source: http://127.0.0.1:50076/media/peerconnection-call.html (859)
    [  FAILED  ] WebRtcBrowserTest.AddTwoMediaStreamsToOnePC, where TypeParam =  and GetParam() =  (2180 ms)
    
    Original issue's description:
    > [Android] Support unfocusable container views.
    >
    > If the ContentViewCore container view is not focusable (which can
    > happen when it's emebedded in an app via android webview), we
    > were clearing the RWHVA notion of focus when the RenderView was
    > swapped out. As the view is never focusable again, chromium never
    > considers this view focused and as such, text selection did not
    > function.
    >
    > We support this type of container view by always treating it focused
    > from Chromium's point of view.
    >
    > BUG=430859 (internal b/18066784)
    >
    > Committed: https://crrev.com/9074d9fc9119ef4d2223f6c993d1a1b67a0bea7c
    > Cr-Commit-Position: refs/heads/master@{#303071}
    
    TBR=jdduke@chromium.org,benm@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=430859 (internal b/18066784)
    
    Review URL: https://codereview.chromium.org/712433002
    
    Cr-Commit-Position: refs/heads/master@{#303104}

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -2365,10 +2365,7 @@ public class ContentViewCore
      * @see View#hasFocus()
      */
     @CalledByNative
-    private boolean hasFocus() {
-        // If the container view is not focusable, we consider it always focused from
-        // Chromium's point of view.
-        if (!mContainerView.isFocusable()) return true;
+    public boolean hasFocus() {
         return mContainerView.hasFocus();
     }
 
