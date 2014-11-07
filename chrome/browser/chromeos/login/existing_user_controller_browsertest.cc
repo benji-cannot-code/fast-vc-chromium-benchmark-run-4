69249b270f5edecce0fdfcd50344866edcaaedf2
bartfab <bartfab@chromium.org>
2014-11-07 04:37:39 -0800

    Fix leak in SupervisedUserCreationForbidden
    This CL fixes a memory leak in
    ExistingUserControllerUntrustedTest.SupervisedUserCreationForbidden.
    SetUserFlow() passes ownership without using a scoped_ptr, which is
    why I missed the ownership transfer and leaked the object.
    
    BUG=425574
    TEST=No more memory leak in the test
    
    Review URL: https://codereview.chromium.org/710513003
    
    Cr-Commit-Position: refs/heads/master@{#303225}

--- chrome/browser/chromeos/login/existing_user_controller_browsertest.cc
+++ chrome/browser/chromeos/login/existing_user_controller_browsertest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/supervised/supervised_user_creation_screen.h"
 #include "chrome/browser/chromeos/login/ui/mock_login_display.h"
 #include "chrome/browser/chromeos/login/ui/mock_login_display_host.h"
+#include "chrome/browser/chromeos/login/user_flow.h"
 #include "chrome/browser/chromeos/login/users/mock_user_manager.h"
 #include "chrome/browser/chromeos/login/users/scoped_user_manager_enabler.h"
 #include "chrome/browser/chromeos/login/wizard_controller.h"
@@ -87,6 +88,10 @@ const int kAutoLoginLongDelay = 10000;
 
 ACTION_P(CreateAuthenticator, user_context) {
   return new MockAuthenticator(arg0, user_context);
+}
+
+void DeleteUserFlow(UserFlow* user_flow) {
+  delete user_flow;
 }
 
 // Wait for cros settings to become permanently untrusted and run |callback|.
@@ -437,14 +442,16 @@ IN_PROC_BROWSER_TEST_F(ExistingUserControllerUntrustedTest,
 }
 
 IN_PROC_BROWSER_TEST_F(ExistingUserControllerUntrustedTest,
-                       DISABLED_SupervisedUserCreationForbidden) {
+                       SupervisedUserCreationForbidden) {
   MockBaseScreenDelegate mock_base_screen_delegate;
   SupervisedUserCreationScreenHandler supervised_user_creation_screen_handler;
   SupervisedUserCreationScreen supervised_user_creation_screen(
       &mock_base_screen_delegate,
       &supervised_user_creation_screen_handler);
 
-  EXPECT_CALL(*mock_user_manager_, SetUserFlow(kUsername, _)).Times(1);
+  EXPECT_CALL(*mock_user_manager_, SetUserFlow(kUsername, _))
+      .Times(1)
+      .WillOnce(WithArg<1>(Invoke(DeleteUserFlow)));
   supervised_user_creation_screen.AuthenticateManager(kUsername, kPassword);
 }
 
