866ad09c99c2c33ebc4bea50a9a6407114ec7a9f
jamiewalch <jamiewalch@chromium.org>
2014-11-07 14:59:15 -0800

    Use a dedicated message to inform the web-app of the connection type.
    I originally implemented this by parsing the log message because I thought we needed to support old plugins.
    However, since we only need this for the v2 app for now, which uses NaCl, there's no reason not to do it properly.
    
    Review URL: https://codereview.chromium.org/683713003
    
    Cr-Commit-Position: refs/heads/master@{#303316}

--- remoting/webapp/browser_test/mock_client_plugin.js
+++ remoting/webapp/browser_test/mock_client_plugin.js
@@ -144,6 +144,9 @@ remoting.MockClientPlugin.prototype.setConnectionStatusUpdateHandler =
   this.connectionStatusUpdateHandler_ = handler;
 };
 
+remoting.MockClientPlugin.prototype.setRouteChangedHandler =
+    function(handler) {};
+
 remoting.MockClientPlugin.prototype.setConnectionReadyHandler =
     function(handler) {};
 
