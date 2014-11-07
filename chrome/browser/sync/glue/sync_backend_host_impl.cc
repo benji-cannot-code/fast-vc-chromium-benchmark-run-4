ef9af1bebca84c3c13a160433e910b259bdda6a1
maxbogue <maxbogue@chromium.org>
2014-11-07 15:12:13 -0800

    Revert of Make sure the sync directory is deleted on sign-out. (patchset #19 id:350001 of https://codereview.chromium.org/682173002/)
    Reason for revert:
    Causing crashes: http://crbug.com/431421 and http://crbug.com/431354
    
    Original issue's description:
    > Make sure the sync directory is deleted on sign-out.
    >
    > Previously, if the user had turned off sync before signing out, the
    > directory would not be deleted correctly.
    >
    > BUG=424593
    >
    > Committed: https://crrev.com/6671560f497671bc28ccd50b5f1fb185a29f1248
    > Cr-Commit-Position: refs/heads/master@{#303174}
    
    TBR=maniscalco@chromium.org,zea@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=424593
    
    Review URL: https://codereview.chromium.org/708153004
    
    Cr-Commit-Position: refs/heads/master@{#303318}

--- chrome/browser/sync/glue/sync_backend_host_impl.cc
+++ chrome/browser/sync/glue/sync_backend_host_impl.cc
@@ -73,7 +73,7 @@ SyncBackendHostImpl::SyncBackendHostImpl(
     Profile* profile,
     invalidation::InvalidationService* invalidator,
     const base::WeakPtr<sync_driver::SyncPrefs>& sync_prefs,
-    const base::FilePath& directory_path)
+    const base::FilePath& sync_folder)
     : frontend_loop_(base::MessageLoop::current()),
       profile_(profile),
       name_(name),
@@ -86,7 +86,7 @@ SyncBackendHostImpl::SyncBackendHostImpl(
       weak_ptr_factory_(this) {
   core_ = new SyncBackendHostCore(
       name_,
-      directory_path,
+      profile_->GetPath().Append(sync_folder),
       sync_prefs_->HasSyncSetupCompleted(),
       weak_ptr_factory_.GetWeakPtr());
 }
@@ -102,6 +102,7 @@ void SyncBackendHostImpl::Initialize(
     const syncer::WeakHandle<syncer::JsEventHandler>& event_handler,
     const GURL& sync_service_url,
     const syncer::SyncCredentials& credentials,
+    bool delete_sync_data_folder,
     scoped_ptr<syncer::SyncManagerFactory> sync_manager_factory,
     scoped_ptr<syncer::UnrecoverableErrorHandler> unrecoverable_error_handler,
     syncer::ReportUnrecoverableErrorFunction
@@ -150,6 +151,7 @@ void SyncBackendHostImpl::Initialize(
       credentials,
       invalidator_ ? invalidator_->GetInvalidatorClientId() : "",
       sync_manager_factory.Pass(),
+      delete_sync_data_folder,
       sync_prefs_->GetEncryptionBootstrapToken(),
       sync_prefs_->GetKeystoreEncryptionBootstrapToken(),
       scoped_ptr<InternalComponentsFactory>(
@@ -561,7 +563,9 @@ void SyncBackendHostImpl::GetAllNodesForTypes(
 }
 
 void SyncBackendHostImpl::InitCore(scoped_ptr<DoInitializeOptions> options) {
-  core_->Initialize(options.Pass());
+  registrar_->sync_thread()->message_loop()->PostTask(FROM_HERE,
+      base::Bind(&SyncBackendHostCore::DoInitialize,
+                 core_.get(), base::Passed(&options)));
 }
 
 void SyncBackendHostImpl::RequestConfigureSyncer(
--- chrome/chrome_tests.gypi
+++ chrome/chrome_tests.gypi
@@ -1219,7 +1219,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'browser/sync/test/integration/single_client_backup_rollback_test.cc',
       'browser/sync/test/integration/single_client_bookmarks_sync_test.cc',
       'browser/sync/test/integration/single_client_dictionary_sync_test.cc',
-      'browser/sync/test/integration/single_client_directory_sync_test.cc',
       'browser/sync/test/integration/single_client_extensions_sync_test.cc',
       'browser/sync/test/integration/single_client_passwords_sync_test.cc',
       'browser/sync/test/integration/single_client_preferences_sync_test.cc',
