6a95552ec995f7d842cf65947035d7a0daf74664
fukino <fukino@chromium.org>
2014-11-06 22:39:25 -0800

    Fix type-check errors in file_manager/background.
    BUG=406995
    TEST=GYP_GENERATORS=ninja tools/gyp/gyp --depth . ui/file_manager/file_manager/background/js/compiled_resources.gyp && ninja -C out/Default
    
    Review URL: https://codereview.chromium.org/681273003
    
    Cr-Commit-Position: refs/heads/master@{#303199}

---
+++ third_party/closure_compiler/externs/command_line_private.js
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/** @fileoverview Externs generated from namespace: commandLinePrivate */
+
+/**
+ * @const
+ */
+chrome.commandLinePrivate = {};
+
+/**
+ * Returns whether a switch is specified on the command line when launching
+ * Chrome.
+ * @param {string} name The name of a command line switch, without leading
+ * "--", such as "enable-experimental-extension-apis".
+ * @param {function(boolean)} callback
+ */
+chrome.commandLinePrivate.hasSwitch = function(name, callback) {};
