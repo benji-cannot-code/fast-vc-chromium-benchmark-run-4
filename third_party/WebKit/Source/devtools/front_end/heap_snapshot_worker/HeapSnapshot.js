aa90f63ebfd3173b1d546935cd4b8bcabcb21d42
yurys@chromium.org <yurys@chromium.org>
2014-11-07 15:28:14 +0000

    DevTools: don't spam console with messages about unreachable objects in heap snapshot
    Log this messages into the DevTools' front-end's console instead.
    
    BUG=430812,426809
    
    Review URL: https://codereview.chromium.org/708903005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184972 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/heap_snapshot_worker/HeapSnapshot.js
+++ third_party/WebKit/Source/devtools/front_end/heap_snapshot_worker/HeapSnapshot.js
@@ -1569,7 +1569,7 @@ WebInspector.HeapSnapshot.prototype = {
 
             if (postOrderIndex === nodeCount || iteration > 1)
                 break;
-            var errors = new WebInspector.HeapSnapshotProblemReport("Error: Corrupted snapshot. " + (nodeCount - postOrderIndex) + " nodes are unreachable from the root:");
+            var errors = new WebInspector.HeapSnapshotProblemReport("Heap snapshot: " + (nodeCount - postOrderIndex) + " nodes are unreachable from the root. Following nodes have only weak retainers:");
             var dumpNode = this.rootNode();
             // Remove root from the result (last node in the array) and put it at the bottom of the stack so that it is
             // visited after all orphan nodes and their subgraphs.
@@ -1585,18 +1585,16 @@ WebInspector.HeapSnapshot.prototype = {
                         stackNodes[++stackTop] = i;
                         stackCurrentEdge[stackTop] = firstEdgeIndexes[i];
                         visited[i] = 1;
-                        errors.addError(dumpNode.name() + " @" + dumpNode.id() + " - node has only weak retainers.");
-                    } else {
                         errors.addError(dumpNode.name() + " @" + dumpNode.id());
                     }
                 }
             }
-            this._progress.reportProblem(errors.toString());
+            console.warn(errors.toString());
         }
 
         // If we already processed all orphan nodes that have only weak retainers and still have some orphans...
         if (postOrderIndex !== nodeCount) {
-            var errors = new WebInspector.HeapSnapshotProblemReport("Error: Still found " + (nodeCount - postOrderIndex) + " unreachable nodes:");
+            var errors = new WebInspector.HeapSnapshotProblemReport("Still found " + (nodeCount - postOrderIndex) + " unreachable nodes in heap snapshot:");
             var dumpNode = this.rootNode();
             // Remove root from the result (last node in the array) and put it at the bottom of the stack so that it is
             // visited after all orphan nodes and their subgraphs.
@@ -1612,7 +1610,7 @@ WebInspector.HeapSnapshot.prototype = {
             }
             nodeOrdinal2PostOrderIndex[rootNodeOrdinal] = postOrderIndex;
             postOrderIndex2NodeOrdinal[postOrderIndex++] = rootNodeOrdinal;
-            this._progress.reportProblem(errors.toString());
+            console.warn(errors.toString());
         }
 
         return {postOrderIndex2NodeOrdinal: postOrderIndex2NodeOrdinal, nodeOrdinal2PostOrderIndex: nodeOrdinal2PostOrderIndex};
