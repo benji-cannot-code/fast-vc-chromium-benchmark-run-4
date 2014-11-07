cde854516d6bbc29e9ce3383736bd5cb34233cb1
jonross <jonross@chromium.org>
2014-11-07 06:45:23 -0800

    Hide Mouse Cursor when entering Maximize Mode
    When disabling the touchpad also hide the cursor.
    
    TEST=Manual: place mouse in middle of screen, enter TouchView mode.
    BUG=429769
    
    Review URL: https://codereview.chromium.org/707013003
    
    Cr-Commit-Position: refs/heads/master@{#303234}

--- ash/wm/maximize_mode/scoped_disable_internal_mouse_and_keyboard_x11.cc
+++ ash/wm/maximize_mode/scoped_disable_internal_mouse_and_keyboard_x11.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/strings/string_util.h"
+#include "ui/aura/client/cursor_client.h"
 #include "ui/aura/client/screen_position_client.h"
 #include "ui/aura/env.h"
 #include "ui/aura/window.h"
@@ -84,6 +85,8 @@ ScopedDisableInternalMouseAndKeyboardX11::
       if (device_name == kInternalTouchpadName) {
         touchpad_device_id_ = xi_dev_list[i].deviceid;
         device_data_manager->DisableDevice(touchpad_device_id_);
+        aura::client::GetCursorClient(
+            Shell::GetInstance()->GetPrimaryRootWindow())->HideCursor();
       } else if (device_name == kInternalKeyboardName) {
         keyboard_device_id_ = xi_dev_list[i].deviceid;
         device_data_manager->DisableDevice(keyboard_device_id_);
