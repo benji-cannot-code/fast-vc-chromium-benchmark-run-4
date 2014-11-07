2a67c821d6c0b0835d49d35e1cc253efe6448be6
mukai <mukai@chromium.org>
2014-11-06 23:20:52 -0800

    Fixes minor style issues which was in r303001
    BUG=425717, 425724, 425719
    TBR=oshima@chromium.org
    TEST=none
    
    Review URL: https://codereview.chromium.org/700333004
    
    Cr-Commit-Position: refs/heads/master@{#303204}

--- athena/home/app_list_view_delegate.cc
+++ athena/home/app_list_view_delegate.cc
@@ -141,7 +141,7 @@ void AppListViewDelegate::ShowForProfileByPath(
 
 views::View* AppListViewDelegate::CreateStartPageWebView(
     const gfx::Size& size) {
-  return NULL;
+  return nullptr;
 }
 
 std::vector<views::View*> AppListViewDelegate::CreateCustomPageWebViews(
