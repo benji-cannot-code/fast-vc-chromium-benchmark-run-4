48b74409bab17df5c627cc2ae121df39a38d8298
tkent@chromium.org <tkent@chromium.org>
2014-11-07 07:12:51 +0000

    Remove bool flags of SQLiteDatabase::open and SQLiteFileSystem::openDatabase.
    They are always true.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/695423006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184956 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/webdatabase/Database.cpp
+++ third_party/WebKit/Source/modules/webdatabase/Database.cpp
@@ -430,7 +430,7 @@ bool Database::performOpenAndVerify(bool shouldSetVersionInNewDatabase, Database
 
     const int maxSqliteBusyWaitTime = 30000;
 
-    if (!m_sqliteDatabase.open(m_filename, true)) {
+    if (!m_sqliteDatabase.open(m_filename)) {
         reportOpenDatabaseResult(1, InvalidStateError, m_sqliteDatabase.lastError());
         errorMessage = formatErrorMessage("unable to open database", m_sqliteDatabase.lastError(), m_sqliteDatabase.lastErrorMsg());
         return false;
