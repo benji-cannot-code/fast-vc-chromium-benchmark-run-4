18b0fdd1c78dec08e828fd3b71f8a8cf916c6959
yurys@chromium.org <yurys@chromium.org>
2014-11-07 17:31:51 +0000

    DevTools: loading timeline pretends it is recording w/ 0 events collected.
    When loading timeline data from file the progress pane message is set to "Loading from file..."
    
    BUG=426123
    R=loislo@chromium.org
    
    Review URL: https://codereview.chromium.org/705273002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184980 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/timeline/TracingTimelineModel.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TracingTimelineModel.js
@@ -179,7 +179,7 @@ WebInspector.TracingTimelineModel.prototype = {
      */
     setEventsForTest: function(events)
     {
-        this._onTracingStarted();
+        this._startCollectingTraceEvents(false);
         this._tracingModel.addEvents(events);
         this._onTracingComplete();
     },
@@ -206,9 +206,17 @@ WebInspector.TracingTimelineModel.prototype = {
 
     _onTracingStarted: function()
     {
+        this._startCollectingTraceEvents(false);
+    },
+
+    /**
+     * @param {boolean} fromFile
+     */
+    _startCollectingTraceEvents: function(fromFile)
+    {
         this.reset();
         this._tracingModel.reset();
-        this.dispatchEventToListeners(WebInspector.TimelineModel.Events.RecordingStarted);
+        this.dispatchEventToListeners(WebInspector.TimelineModel.Events.RecordingStarted, { fromFile: fromFile });
     },
 
     /**
@@ -1047,7 +1055,7 @@ WebInspector.TracingModelLoader.prototype = {
             return;
 
         if (this._firstChunk) {
-            this._model._onTracingStarted();
+            this._model._startCollectingTraceEvents(true);
         } else {
             var commaIndex = json.indexOf(",");
             if (commaIndex !== -1)
