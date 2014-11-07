4b61a4c092b1633886649c0b3dc0fbda1c68adf8
tengs <tengs@chromium.org>
2014-11-06 23:17:08 -0800

    Introduce private API for fetching the user's profile image.
    BUG=406439
    TEST=manual
    
    Review URL: https://codereview.chromium.org/693343005
    
    Cr-Commit-Position: refs/heads/master@{#303203}

--- extensions/browser/extension_function_histogram_value.h
+++ extensions/browser/extension_function_histogram_value.h
@@ -981,6 +981,7 @@ enum HistogramValue {
   VPNPROVIDER_SENDPACKET,
   VPNPROVIDER_NOTIFYCONNECTIONSTATECHANGED,
   DOCUMENT_SCAN_SCAN,
+  EASYUNLOCKPRIVATE_GETUSERIMAGE,
   // Last entry: Add new entries above and ensure to update
   // tools/metrics/histograms/histograms.xml.
   ENUM_BOUNDARY
