66d6998c441761057f5bf9ac97b9a415bd30ca8b
bartfab <bartfab@chromium.org>
2014-11-07 14:01:31 -0800

    Update device disabled screen to match final design
    This CL updates the device disabling screen to match the final design.
    The layout, assets and strings have been approved by UI.
    
    In the final design, the device disabled screen always shows the domain
    that owns the device (if known).
    
    See the bug for screen shots.
    
    BUG=425574
    TEST=Updated unit and browser tests
    
    Review URL: https://codereview.chromium.org/711753002
    
    Cr-Commit-Position: refs/heads/master@{#303303}

--- chrome/browser/ui/webui/chromeos/login/device_disabled_screen_handler.h
+++ chrome/browser/ui/webui/chromeos/login/device_disabled_screen_handler.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_WEBUI_CHROMEOS_LOGIN_DEVICE_DISABLED_SCREEN_HANDLER_H_
 #define CHROME_BROWSER_UI_WEBUI_CHROMEOS_LOGIN_DEVICE_DISABLED_SCREEN_HANDLER_H_
 
-#include <string>
-
 #include "base/macros.h"
 #include "chrome/browser/chromeos/login/screens/device_disabled_screen_actor.h"
 #include "chrome/browser/ui/webui/chromeos/login/base_screen_handler.h"
@@ -22,7 +20,7 @@ class DeviceDisabledScreenHandler : public DeviceDisabledScreenActor,
   ~DeviceDisabledScreenHandler() override;
 
   // DeviceDisabledScreenActor:
-  void Show(const std::string& message) override;
+  void Show() override;
   void Hide() override;
   void SetDelegate(Delegate* delegate) override;
   void UpdateMessage(const std::string& message) override;
@@ -39,9 +37,6 @@ class DeviceDisabledScreenHandler : public DeviceDisabledScreenActor,
 
   // Indicates whether the screen should be shown right after initialization.
   bool show_on_init_;
-
-  // The message to show to the user.
-  std::string message_;
 
   DISALLOW_COPY_AND_ASSIGN(DeviceDisabledScreenHandler);
 };
