ccb5e2054a53b579697292eb7b5959d8972e4640
thestig <thestig@chromium.org>
2014-11-06 17:19:24 -0800

    Cleanup: Replace base::ASCIIToWide with base::ASCIIToUTF16.
    BUG=23581
    
    Review URL: https://codereview.chromium.org/689063002
    
    Cr-Commit-Position: refs/heads/master@{#303142}

--- ui/base/clipboard/clipboard_win.cc
+++ ui/base/clipboard/clipboard_win.cc
@@ -137,7 +137,7 @@ HGLOBAL CreateGlobalData(const std::basic_string<charT>& str) {
     ::GlobalUnlock(data);
   }
   return data;
-};
+}
 
 bool BitmapHasInvalidPremultipliedColors(const SkBitmap& bitmap) {
   for (int x = 0; x < bitmap.width(); ++x) {
@@ -239,7 +239,7 @@ bool Clipboard::FormatType::Equals(const FormatType& other) const {
 Clipboard::FormatType Clipboard::GetFormatType(
     const std::string& format_string) {
   return FormatType(
-      ::RegisterClipboardFormat(base::ASCIIToWide(format_string).c_str()));
+      ::RegisterClipboardFormat(base::ASCIIToUTF16(format_string).c_str()));
 }
 
 // static
@@ -728,7 +728,7 @@ void ClipboardWin::WriteBookmark(const char* title_data,
   bookmark.append(1, L'\n');
   bookmark.append(url_data, url_len);
 
-  base::string16 wide_bookmark = base::UTF8ToWide(bookmark);
+  base::string16 wide_bookmark = base::UTF8ToUTF16(bookmark);
   HGLOBAL glob = CreateGlobalData(wide_bookmark);
 
   WriteToClipboard(GetUrlWFormatType().ToFormatEtc().cfFormat, glob);
