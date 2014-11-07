947c6ae53b16bb0e1e26c68ce61b9d31e4f5ea1f
mvanouwerkerk <mvanouwerkerk@chromium.org>
2014-11-07 10:14:24 -0800

    Plumb Push API permission request through to chrome layer.
    This follows the pattern of Notifications permission requests.
    
    Depends on https://crrev.com/707833002/
    
    This is part of a series of changes in both Blink and Chromium.
    
    BUG=389194
    TEST=PushMessagingBrowserTest*
    TEST=LayoutTests/push_messaging/*
    
    Review URL: https://codereview.chromium.org/710433002
    
    Cr-Commit-Position: refs/heads/master@{#303258}

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -85,6 +85,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/notification_provider.h"
 #include "content/renderer/npapi/plugin_channel_host.h"
 #include "content/renderer/push_messaging_dispatcher.h"
+#include "content/renderer/push_permission_dispatcher.h"
 #include "content/renderer/render_frame_proxy.h"
 #include "content/renderer/render_process.h"
 #include "content/renderer/render_thread_impl.h"
@@ -3176,6 +3177,12 @@ blink::WebGeolocationClient* RenderFrameImpl::geolocationClient() {
   if (!geolocation_dispatcher_)
     geolocation_dispatcher_ = new GeolocationDispatcher(this);
   return geolocation_dispatcher_;
+}
+
+void RenderFrameImpl::requestPushPermission(blink::WebCallback* callback) {
+  if (!push_permission_dispatcher_)
+    push_permission_dispatcher_ = new PushPermissionDispatcher(this);
+  push_permission_dispatcher_->RequestPermission(callback);
 }
 
 blink::WebPushClient* RenderFrameImpl::pushClient() {
