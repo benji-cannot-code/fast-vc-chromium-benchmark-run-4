5d57fd1ed072601ba690e561403ba3d8aaa3804c
Dan Beam <dbeam@chromium.org>
2014-11-07 16:06:41 -0800

    Move sync setup related stuff back to options.
    It's only used from options now. It was used from the NTP as well; no longer.
    
    Depends on:
    https://codereview.chromium.org/702323002/
    https://codereview.chromium.org/703193003/
    
    R=estade@chromium.org
    BUG=none
    
    Review URL: https://codereview.chromium.org/703413002
    
    Cr-Commit-Position: refs/heads/master@{#303330}

--- chrome/browser/ui/webui/ntp/ntp_resource_cache.cc
+++ chrome/browser/ui/webui/ntp/ntp_resource_cache.cc
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/ntp/new_tab_page_handler.h"
 #include "chrome/browser/ui/webui/ntp/new_tab_ui.h"
 #include "chrome/browser/ui/webui/ntp/ntp_login_handler.h"
-#include "chrome/browser/ui/webui/sync_setup_handler.h"
 #include "chrome/browser/web_resource/notification_promo.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
