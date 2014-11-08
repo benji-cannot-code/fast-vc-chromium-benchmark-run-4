99d349951db3b55a32493f3605e6193725af9e67
wuhu <wuhu@google.com>
2014-11-07 18:37:23 -0800

    Remove wpr_ca_cert_path from browser backends.
    BUG=
    
    Review URL: https://codereview.chromium.org/698313007
    
    Cr-Commit-Position: refs/heads/master@{#303359}

--- tools/telemetry/telemetry/core/backends/browser_backend.py
+++ tools/telemetry/telemetry/core/backends/browser_backend.py
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import os
-
 from telemetry import decorators
 from telemetry.core import platform
 from telemetry.core import web_contents
@@ -26,7 +24,6 @@ class BrowserBackend(app_backend.AppBackend):
     self.browser_options = browser_options
     self._tab_list_backend_class = tab_list_backend
     self._forwarder_factory = None
-    self._wpr_ca_cert_path = None
 
   def SetBrowser(self, browser):
     super(BrowserBackend, self).SetApp(app=browser)
@@ -51,16 +48,6 @@ class BrowserBackend(app_backend.AppBackend):
   @property
   def wpr_mode(self):
     return self.browser_options.wpr_mode
-
-  @property
-  def wpr_ca_cert_path(self):
-    """Path to root certificate installed on browser (or None).
-
-    If this is set, web page replay will use it to sign HTTPS responses.
-    """
-    if self._wpr_ca_cert_path:
-      assert os.path.isfile(self._wpr_ca_cert_path)
-    return self._wpr_ca_cert_path
 
   @property
   def should_ignore_certificate_errors(self):
