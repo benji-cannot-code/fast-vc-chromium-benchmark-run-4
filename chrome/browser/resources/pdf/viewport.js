ac0bede77ec5a03c694907cc82927217ca57d87f
sammc <sammc@chromium.org>
2014-11-09 20:51:15 -0800

    OOP PDF: Use proportional height when calculating the most visible page.
    Previously, fit-to-page and fit-to-width calculated the most visible
    page as the page with the most area visible in the viewport. This CL
    changes that calculation to choose the page with the greatest proportion
    of its height visible in the viewport.
    
    BUG=431185
    
    Review URL: https://codereview.chromium.org/712873003
    
    Cr-Commit-Position: refs/heads/master@{#303424}

--- chrome/browser/resources/pdf/viewport.js
+++ chrome/browser/resources/pdf/viewport.js
@@ -4,19 +4,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 /**
- * Returns the area of the intersection of two rectangles.
+ * Returns the height of the intersection of two rectangles.
  * @param {Object} rect1 the first rect
  * @param {Object} rect2 the second rect
- * @return {number} the area of the intersection of the rects
+ * @return {number} the height of the intersection of the rects
  */
-function getIntersectionArea(rect1, rect2) {
-  var xOverlap = Math.max(0,
-      Math.min(rect1.x + rect1.width, rect2.x + rect2.width) -
-      Math.max(rect1.x, rect2.x));
-  var yOverlap = Math.max(0,
+function getIntersectionHeight(rect1, rect2) {
+  return Math.max(0,
       Math.min(rect1.y + rect1.height, rect2.y + rect2.height) -
       Math.max(rect1.y, rect2.y));
-  return xOverlap * yOverlap;
 }
 
 /**
@@ -299,31 +295,30 @@ Viewport.prototype = {
   },
 
   /**
-   * Returns the page with the most pixels in the current viewport.
+   * Returns the page with the greatest proportion of its height in the current
+   * viewport.
    * @return {int} the index of the most visible page.
    */
   getMostVisiblePage: function() {
     var firstVisiblePage = this.getPageAtY_(this.position.y / this.zoom_);
-    var mostVisiblePage = {number: 0, area: 0};
+    if (firstVisiblePage == this.pageDimensions_.length - 1)
+      return firstVisiblePage;
+
     var viewportRect = {
       x: this.position.x / this.zoom_,
       y: this.position.y / this.zoom_,
       width: this.size.width / this.zoom_,
       height: this.size.height / this.zoom_
     };
-    for (var i = firstVisiblePage; i < this.pageDimensions_.length; i++) {
-      var area = getIntersectionArea(this.pageDimensions_[i],
-                                     viewportRect);
-      // If we hit a page with 0 area overlap, we must have gone past the
-      // pages visible in the viewport so we can break.
-      if (area == 0)
-        break;
-      if (area > mostVisiblePage.area) {
-        mostVisiblePage.area = area;
-        mostVisiblePage.number = i;
-      }
-    }
-    return mostVisiblePage.number;
+    var firstVisiblePageVisibility = getIntersectionHeight(
+        this.pageDimensions_[firstVisiblePage], viewportRect) /
+        this.pageDimensions_[firstVisiblePage].height;
+    var nextPageVisibility = getIntersectionHeight(
+        this.pageDimensions_[firstVisiblePage + 1], viewportRect) /
+        this.pageDimensions_[firstVisiblePage + 1].height;
+    if (nextPageVisibility > firstVisiblePageVisibility)
+      return firstVisiblePage + 1;
+    return firstVisiblePage;
   },
 
   /**
