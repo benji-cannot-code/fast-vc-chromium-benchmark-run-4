007b5bc3d4b268c7db36db5a382210107a7e3024
bnc <bnc@chromium.org>
2014-11-10 09:48:50 -0800

    Decouple SPDY histogram offset from minimum version.
    Decouple SPDY histogram offset from minimum version so that when SPDY/2 (and
    further versions) are removed from the code, kProtoSPDYMiniumVersion can track
    the currently supported minimum version without messing with the histogram.
    
    BUG=431299
    
    Review URL: https://codereview.chromium.org/693973004
    
    Cr-Commit-Position: refs/heads/master@{#303491}

--- net/socket/next_proto.h
+++ net/socket/next_proto.h
@@ -26,6 +26,7 @@ enum NextProto {
 
   kProtoDeprecatedSPDY2 = 100,
   kProtoSPDYMinimumVersion = kProtoDeprecatedSPDY2,
+  kProtoSPDYHistogramOffset = kProtoDeprecatedSPDY2,
   kProtoSPDY3 = 101,
   kProtoSPDY31 = 102,
   // HTTP/2 draft-14 was 103,
