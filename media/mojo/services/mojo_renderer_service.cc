bccb0a2a8b69e4e78dc17cb3a889f62bb2d35e1b
xhwang <xhwang@chromium.org>
2014-11-10 12:46:42 -0800

    Remove |connection| from MojoRendererService's constructor.
    This parameter is not used.
    
    BUG=431776
    
    Review URL: https://codereview.chromium.org/716623002
    
    Cr-Commit-Position: refs/heads/master@{#303514}

--- media/mojo/services/mojo_renderer_service.cc
+++ media/mojo/services/mojo_renderer_service.cc
@@ -53,7 +53,7 @@ class MojoRendererApplication
   // mojo::InterfaceFactory<mojo::MediaRenderer> implementation.
   void Create(mojo::ApplicationConnection* connection,
               mojo::InterfaceRequest<mojo::MediaRenderer> request) override {
-    mojo::BindToRequest(new MojoRendererService(connection), &request);
+    mojo::BindToRequest(new MojoRendererService(), &request);
   }
 };
 
@@ -61,8 +61,7 @@ static void MojoTrampoline(const mojo::Closure& closure) {
   closure.Run();
 }
 
-MojoRendererService::MojoRendererService(
-    mojo::ApplicationConnection* connection)
+MojoRendererService::MojoRendererService()
     : state_(STATE_UNINITIALIZED),
       last_media_time_usec_(0),
       weak_factory_(this),
