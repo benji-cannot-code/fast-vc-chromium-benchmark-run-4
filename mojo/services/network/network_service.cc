64c19a3077e05a01889be7974ba585ede5c3502f
davemoore <davemoore@chromium.org>
2014-11-10 15:17:17 -0800

    Minor cleanup of network_service
    TBR=ben@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/714643003
    
    Cr-Commit-Position: refs/heads/master@{#303531}

--- mojo/services/network/network_service.cc
+++ mojo/services/network/network_service.cc
@@ -3,9 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/at_exit.h"
 #include "base/base_paths.h"
-#include "base/command_line.h"
 #include "base/files/file_path.h"
 #include "base/message_loop/message_loop.h"
 #include "base/path_service.h"
@@ -14,15 +12,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/application/application_connection.h"
 #include "mojo/public/cpp/application/application_delegate.h"
 #include "mojo/public/cpp/application/interface_factory.h"
-#include "mojo/public/cpp/bindings/interface_ptr.h"
 #include "mojo/services/network/network_context.h"
 #include "mojo/services/network/network_service_impl.h"
 
-class Delegate : public mojo::ApplicationDelegate,
-                 public mojo::InterfaceFactory<mojo::NetworkService> {
- public:
-  Delegate() {}
-
+class NetworkServiceDelegate
+    : public mojo::ApplicationDelegate,
+      public mojo::InterfaceFactory<mojo::NetworkService> {
+ private:
   void Initialize(mojo::ApplicationImpl* app) override {
     base::FilePath base_path;
     CHECK(PathService::Get(base::DIR_TEMP, &base_path));
@@ -45,12 +41,11 @@ class Delegate : public mojo::ApplicationDelegate,
         new mojo::NetworkServiceImpl(connection, context_.get()), &request);
   }
 
- private:
   scoped_ptr<mojo::NetworkContext> context_;
 };
 
 MojoResult MojoMain(MojoHandle shell_handle) {
-  mojo::ApplicationRunnerChromium runner(new Delegate);
+  mojo::ApplicationRunnerChromium runner(new NetworkServiceDelegate);
   runner.set_message_loop_type(base::MessageLoop::TYPE_IO);
   return runner.Run(shell_handle);
 }
