70283b010cd4383b213cf1b1ddcb92b099eac358
aandrey@chromium.org <aandrey@chromium.org>
2014-11-10 09:31:45 +0000

    DevTools: Show preview for ES6 MapIterator, SetIterator in console.
    BUG=427868
    R=yurys, vsevik
    
    Review URL: https://codereview.chromium.org/694753002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185029 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/console/ConsoleViewMessage.js
+++ third_party/WebKit/Source/devtools/front_end/console/ConsoleViewMessage.js
@@ -56,6 +56,7 @@ WebInspector.ConsoleViewMessage = function(consoleMessage, linkifier, nestingLev
         "node": this._formatParameterAsNode,
         "map": this._formatParameterAsObject,
         "set": this._formatParameterAsObject,
+        "iterator": this._formatParameterAsObject,
         "string": this._formatParameterAsString
     };
 }
