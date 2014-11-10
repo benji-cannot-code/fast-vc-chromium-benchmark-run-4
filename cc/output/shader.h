5375f5db4fee9511ac8626028629aa36c48bc3de
rosca <rosca@adobe.com>
2014-11-10 04:35:39 -0800

    Use blending shaders for background filters.
    Adding the missing blend modes (normal and screen) to shaders allows us
    to send background filters to shaders all the time. This way we avoid
    the inverse transform of the background texture, which leads to losing
    quality and speed.
    
    BUG=411079
    
    Review URL: https://codereview.chromium.org/676433002
    
    Cr-Commit-Position: refs/heads/master@{#303451}

--- cc/output/shader.h
+++ cc/output/shader.h
@@ -40,7 +40,9 @@ enum SamplerType {
 };
 
 enum BlendMode {
+  BlendModeNone,
   BlendModeNormal,
+  BlendModeScreen,
   BlendModeOverlay,
   BlendModeDarken,
   BlendModeLighten,
@@ -306,7 +308,7 @@ class FragmentTexBlendMode {
 
   BlendMode blend_mode() const { return blend_mode_; }
   void set_blend_mode(BlendMode blend_mode) { blend_mode_ = blend_mode; }
-  bool is_default_blend_mode() const { return blend_mode_ == BlendModeNormal; }
+  bool has_blend_mode() const { return blend_mode_ != BlendModeNone; }
 
  protected:
   FragmentTexBlendMode();
