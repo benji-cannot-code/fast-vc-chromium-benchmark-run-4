aec2baf95eda1a8213b3d273383b713e5b975d6c
samuong <samuong@chromium.org>
2014-11-10 18:39:53 -0800

    [chromedriver] Use CloseWebView() to close DevTools UI
    BUG=431062,chromedriver:973
    
    Review URL: https://codereview.chromium.org/697843003
    
    Cr-Commit-Position: refs/heads/master@{#303575}

--- chrome/test/chromedriver/chrome/devtools_http_client.cc
+++ chrome/test/chromedriver/chrome/devtools_http_client.cc
@@ -192,10 +192,7 @@ Status DevToolsHttpClient::CloseFrontends(const std::string& for_client_id) {
       return status;
 
     scoped_ptr<base::Value> result;
-    status = web_view->EvaluateScript(
-        std::string(),
-        "document.querySelector('*[id^=\"close-button-\"]').click();",
-        &result);
+    status = CloseWebView(*it);
     // Ignore disconnected error, because it may be closed already.
     if (status.IsError() && status.code() != kDisconnected)
       return status;
