e8909f1bbf7ec2e4f6ad084f2c79e337b5aafb9f
tyoshino@chromium.org <tyoshino@chromium.org>
2014-11-11 04:45:04 +0000

    Remove unnecessary methods and collapse function calls in the preloader code
    - Remove PreloadRequest::defer()
    - Remove ResourceFetcher::requestPreload(). The only user is preload()
      and is just calling requestPreload().
    - Move setDefer() call in HTMLResourcePreloader::preload() into
      resourceRequest() call
    - Make HTMLResourcePreloader::preload() private
    
    R=japhet
    BUG=none
    
    Review URL: https://codereview.chromium.org/717453002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185100 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/fetch/ResourceFetcher.cpp
+++ third_party/WebKit/Source/core/fetch/ResourceFetcher.cpp
@@ -1234,11 +1234,6 @@ void ResourceFetcher::decrementRequestCount(const Resource* res)
 
 void ResourceFetcher::preload(Resource::Type type, FetchRequest& request, const String& charset)
 {
-    requestPreload(type, request, charset);
-}
-
-void ResourceFetcher::requestPreload(Resource::Type type, FetchRequest& request, const String& charset)
-{
     // Ensure main resources aren't preloaded, since the cache can't actually reuse the preload.
     if (type == Resource::MainResource)
         return;
