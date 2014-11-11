28af6f9034c32a7c34566cf65451ae76d7088acc
calamity <calamity@chromium.org>
2014-11-10 17:23:04 -0800

    Implement disable-app-list-dismiss-on-blur for ChromeOS.
    This CL prevents the app list from being dismissed on ChromeOS
    from a defocus event.
    
    BUG=405400
    
    Review URL: https://codereview.chromium.org/698973006
    
    Cr-Commit-Position: refs/heads/master@{#303561}

--- ash/wm/app_list_controller.cc
+++ ash/wm/app_list_controller.cc
@@ -376,8 +376,10 @@ void AppListController::ProcessLocatedEvent(ui::LocatedEvent* event) {
   }
 
   aura::Window* window = view_->GetWidget()->GetNativeView()->parent();
-  if (!window->Contains(target))
+  if (!window->Contains(target) &&
+      !app_list::switches::ShouldNotDismissOnBlur()) {
     Dismiss();
+  }
 }
 
 void AppListController::UpdateBounds() {
@@ -414,7 +416,8 @@ void AppListController::OnWindowFocused(aura::Window* gained_focus,
     aura::Window* applist_container = applist_window->parent();
 
     if (applist_container->Contains(lost_focus) &&
-        (!gained_focus || !applist_container->Contains(gained_focus))) {
+        (!gained_focus || !applist_container->Contains(gained_focus)) &&
+        !app_list::switches::ShouldNotDismissOnBlur()) {
       Dismiss();
     }
   }
