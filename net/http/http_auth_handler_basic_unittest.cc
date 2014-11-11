bdf5970a592288eca12ceef86abc63bcb179a988
ricea <ricea@chromium.org>
2014-11-11 09:29:58 -0800

    Check that the callback is not null in GenerateAuthToken.
    Now that net::SocketStream is gone, GenerateAuthToken no longer needs
    to accept a null callback argument.
    
    Also update the Basic Auth test to use a non-null callback.
    
    BUG=
    TEST=net_unittests
    
    Review URL: https://codereview.chromium.org/714103002
    
    Cr-Commit-Position: refs/heads/master@{#303666}

--- net/http/http_auth_handler_basic_unittest.cc
+++ net/http/http_auth_handler_basic_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "net/base/net_errors.h"
+#include "net/base/test_completion_callback.h"
 #include "net/http/http_auth_challenge_tokenizer.h"
 #include "net/http/http_auth_handler_basic.h"
 #include "net/http/http_request_info.h"
@@ -42,8 +43,9 @@ TEST(HttpAuthHandlerBasicTest, GenerateAuthToken) {
                                 base::ASCIIToUTF16(tests[i].password));
     HttpRequestInfo request_info;
     std::string auth_token;
+    TestCompletionCallback callback;
     int rv = basic->GenerateAuthToken(&credentials, &request_info,
-                                      CompletionCallback(), &auth_token);
+                                      callback.callback(), &auth_token);
     EXPECT_EQ(OK, rv);
     EXPECT_STREQ(tests[i].expected_credentials, auth_token.c_str());
   }
