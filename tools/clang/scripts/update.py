5e7c55a79efc67174fa2138453c545f8007dec7e
hans <hans@chromium.org>
2014-11-11 14:51:56 -0800

    Win/Clang update script: build lld
    In preparation for setting up buildbots using lld to link Chromium
    make the update script checkout and build lld.
    
    BUG=82385
    
    Review URL: https://codereview.chromium.org/714143004
    
    Cr-Commit-Position: refs/heads/master@{#303734}

--- tools/clang/scripts/update.py
+++ tools/clang/scripts/update.py
@@ -33,6 +33,7 @@ LLVM_BUILD_DIR = os.path.join(CHROMIUM_DIR, 'third_party', 'llvm-build',
                               'Release+Asserts')
 COMPILER_RT_BUILD_DIR = os.path.join(LLVM_BUILD_DIR, '32bit-compiler-rt')
 CLANG_DIR = os.path.join(LLVM_DIR, 'tools', 'clang')
+LLD_DIR = os.path.join(LLVM_DIR, 'tools', 'lld')
 COMPILER_RT_DIR = os.path.join(LLVM_DIR, 'projects', 'compiler-rt')
 STAMP_FILE = os.path.join(LLVM_BUILD_DIR, 'cr_build_revision')
 
@@ -180,6 +181,7 @@ def UpdateClang():
 
   Checkout('LLVM', LLVM_REPO_URL + '/llvm/trunk', LLVM_DIR)
   Checkout('Clang', LLVM_REPO_URL + '/cfe/trunk', CLANG_DIR)
+  Checkout('LLD', LLVM_REPO_URL + '/lld/trunk', LLD_DIR)
   Checkout('compiler-rt', LLVM_REPO_URL + '/compiler-rt/trunk', COMPILER_RT_DIR)
 
   if not os.path.exists(LLVM_BUILD_DIR):
