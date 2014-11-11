b665df11533802457e238388b6f2f55d578a7538
hayato@chromium.org <hayato@chromium.org>
2014-11-11 10:19:53 +0000

    Remove unnecessary explicit keywords from a constructor with more than one parameters in css directory.
    BUG=None
    TEST=None; No behavior change
    
    Review URL: https://codereview.chromium.org/710293002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185114 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/SelectorChecker.h
+++ third_party/WebKit/Source/core/css/SelectorChecker.h
@@ -47,7 +47,7 @@ public:
     enum Match { SelectorMatches, SelectorFailsLocally, SelectorFailsAllSiblings, SelectorFailsCompletely };
     enum VisitedMatchType { VisitedMatchDisabled, VisitedMatchEnabled };
     enum Mode { ResolvingStyle = 0, CollectingStyleRules, CollectingCSSRules, QueryingRules, SharingRules };
-    explicit SelectorChecker(Document&, Mode);
+    SelectorChecker(Document&, Mode);
 
     struct SelectorCheckingContext {
         STACK_ALLOCATED();
