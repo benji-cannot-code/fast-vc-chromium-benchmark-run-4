0cf2a00cdfbe0541bef99da49c6573e5f4605ddf
hirono <hirono@chromium.org>
2014-11-11 23:51:24 -0800

    Files.app: Cache thumbnail DOM of GridView.
    The CL adds a cache to GridView's thumbnail to enable the view to create the
    items' DOM synchronously.
    
    The unused cache is removed just after updating the view's DOM in overriden the
    mergeItems method.
    
    BUG=316050
    TEST=copy files on Download directory and observe thumbnails.
    
    Review URL: https://codereview.chromium.org/712313002
    
    Cr-Commit-Position: refs/heads/master@{#303803}

--- ui/file_manager/file_manager/foreground/js/ui/preview_panel.js
+++ ui/file_manager/file_manager/foreground/js/ui/preview_panel.js
@@ -455,6 +455,7 @@ PreviewPanel.Thumbnails.prototype.loadThumbnails_ = function(selection) {
           this.volumeManager_,
           ThumbnailLoader.FillMode.FILL,
           FileGrid.ThumbnailQuality.LOW,
+          /* animation */ false,
           i == 0 && length == 1 ? this.setZoomedImage_.bind(this) : undefined);
     }
 
