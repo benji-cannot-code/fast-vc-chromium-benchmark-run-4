86c6bbce19a2c847bb7469e534521475bbfe1270
anujk.sharma <anujk.sharma@samsung.com>
2014-11-12 10:27:14 -0800

    [content/browser/download] Convert VLOGs to DVLOGs
    BUG=101424
    
    Review URL: https://codereview.chromium.org/716123002
    
    Cr-Commit-Position: refs/heads/master@{#303855}

--- content/browser/download/save_package.cc
+++ content/browser/download/save_package.cc
@@ -1072,9 +1072,9 @@ void SavePackage::OnReceivedSerializedHtmlData(const GURL& frame_url,
   if (flag == WebPageSerializerClient::AllFramesAreFinished) {
     for (SaveUrlItemMap::iterator it = in_progress_items_.begin();
          it != in_progress_items_.end(); ++it) {
-      VLOG(20) << " " << __FUNCTION__ << "()"
-               << " save_id = " << it->second->save_id()
-               << " url = \"" << it->second->url().spec() << "\"";
+      DVLOG(20) << " " << __FUNCTION__ << "()"
+                << " save_id = " << it->second->save_id()
+                << " url = \"" << it->second->url().spec() << "\"";
       BrowserThread::PostTask(
           BrowserThread::FILE, FROM_HERE,
           base::Bind(&SaveFileManager::SaveFinished,
@@ -1124,9 +1124,9 @@ void SavePackage::OnReceivedSerializedHtmlData(const GURL& frame_url,
 
   // Current frame is completed saving, call finish in file thread.
   if (flag == WebPageSerializerClient::CurrentFrameIsFinished) {
-    VLOG(20) << " " << __FUNCTION__ << "()"
-             << " save_id = " << save_item->save_id()
-             << " url = \"" << save_item->url().spec() << "\"";
+    DVLOG(20) << " " << __FUNCTION__ << "()"
+              << " save_id = " << save_item->save_id()
+              << " url = \"" << save_item->url().spec() << "\"";
     BrowserThread::PostTask(
         BrowserThread::FILE, FROM_HERE,
         base::Bind(&SaveFileManager::SaveFinished,
