bdb90fe16b22b0498700862f381c4a266a447e6d
newt <newt@chromium.org>
2014-11-11 17:39:37 -0800

    Add a callback for querying profile path.
    The previous method for querying the profile path was: call
    setPathValuesForAboutChrome(), wait a little while, cross your fingers,
    and call getProfilePathValue(). This was inherently racy.
    
    The new method is callback-based and is not racy.
    
    BUG=428869
    
    Review URL: https://codereview.chromium.org/712363004
    
    Cr-Commit-Position: refs/heads/master@{#303778}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/PrefServiceBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/PrefServiceBridge.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.preferences;
 
+import android.text.TextUtils;
+
 import org.chromium.base.CalledByNative;
 import org.chromium.base.ThreadUtils;
 import org.chromium.chrome.browser.search_engines.TemplateUrlService;
@@ -30,7 +32,7 @@ public final class PrefServiceBridge {
     public static final int SUPERVISED_USER_FILTERING_WARN = 1;
     public static final int SUPERVISED_USER_FILTERING_BLOCK = 2;
 
-    private static String sProfilePathValue;
+    private static String sProfilePath;
 
     // Object to notify when "clear browsing data" completes.
     private OnClearBrowsingDataListener mClearBrowsingDataListener;
@@ -72,6 +74,16 @@ public final class PrefServiceBridge {
         public String getOSVersion() {
             return mOSVersion;
         }
+    }
+
+    /**
+     * Callback to receive the profile path.
+     */
+    public interface ProfilePathCallback {
+        /**
+         * Called with the profile path, once it's available.
+         */
+        void onGotProfilePath(String profilePath);
     }
 
     /**
@@ -132,18 +144,29 @@ public final class PrefServiceBridge {
     }
 
     /**
-     * @return the About Chrome value for profile path.
+     * Don't use this. This will be deleted soon. Use getProfilePath() instead.
+     * TODO(newt): delete this once all callers are updated.
      */
     public String getProfilePathValue() {
-        return sProfilePathValue;
+        return "";
     }
 
     /**
-     * Set the About Chrome value for profile path.
+     * Returns the path to the user's profile directory via a callback. The callback may be
+     * called synchronously or asynchronously.
      */
+    public void getProfilePath(ProfilePathCallback callback) {
+        if (!TextUtils.isEmpty(sProfilePath)) {
+            callback.onGotProfilePath(sProfilePath);
+        } else {
+            nativeGetProfilePath(callback);
+        }
+    }
+
     @CalledByNative
-    public static void setProfilePathValue(String pathValue) {
-        sProfilePathValue = pathValue;
+    private static void onGotProfilePath(String profilePath, ProfilePathCallback callback) {
+        sProfilePath = profilePath;
+        callback.onGotProfilePath(profilePath);
     }
 
     public boolean isAcceptCookiesEnabled() {
@@ -601,12 +624,10 @@ public final class PrefServiceBridge {
     }
 
     /**
-     * Set profile path value needed for about chrome.
+     * Don't use this. Will be deleted soon.
+     * TODO(newt): delete this once all callers are updated.
      */
     public void setPathValuesForAboutChrome() {
-        if (sProfilePathValue == null) {
-            nativeSetPathValuesForAboutChrome();
-        }
     }
 
     /**
@@ -703,7 +724,7 @@ public final class PrefServiceBridge {
     private native void nativeSetCrashReporting(boolean reporting);
     private native boolean nativeCanPredictNetworkActions();
     private native AboutVersionStrings nativeGetAboutVersionStrings();
-    private native void nativeSetPathValuesForAboutChrome();
+    private native void nativeGetProfilePath(ProfilePathCallback callback);
     private native void nativeSetContextualSearchPreference(String preference);
     private native String nativeGetContextualSearchPreference();
     private native boolean nativeGetSearchSuggestEnabled();
