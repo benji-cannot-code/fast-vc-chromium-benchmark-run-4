216f7452b9310bbbc3349b63693c675261b47a84
amistry <amistry@chromium.org>
2014-11-12 12:13:14 -0800

    Stop the hotword detector instead of shutting it down completely when a session is no longer active.
    Constantly creating and destroying the NaCl plugin when opening/closing
    the NTP/google.com/launcher is inefficient use of CPU.
    
    After ~30 seconds of being idle, the hotword extension event page will
    get shut down taking the detector with it.
    
    BUG=397019
    
    Review URL: https://codereview.chromium.org/721443003
    
    Cr-Commit-Position: refs/heads/master@{#303884}

--- chrome/browser/resources/hotword/state_manager.js
+++ chrome/browser/resources/hotword/state_manager.js
@@ -207,11 +207,11 @@ cr.define('hotword', function() {
           this.hotwordStatus_.alwaysOnEnabled ||
           this.hotwordStatus_.trainingEnabled) {
         // Start the detector if there's a session and the user is unlocked, and
-        // shut it down otherwise.
+        // stops it otherwise.
         if (this.sessions_.length && !this.isLocked_)
           this.startDetector_();
         else
-          this.shutdownDetector_();
+          this.stopDetector_();
 
         if (!chrome.idle.onStateChanged.hasListener(
                 this.idleStateChangedListener_)) {
@@ -305,6 +305,17 @@ cr.define('hotword', function() {
           session.startedCb_();
           session.startedCb_ = null;
         }
+      }
+    },
+
+    /**
+     * Stops the hotword detector, if it's running.
+     * @private
+     */
+    stopDetector_: function() {
+      if (this.pluginManager_ && this.state_ == State_.RUNNING) {
+        this.state_ = State_.STOPPED;
+        this.pluginManager_.stopRecognizer();
       }
     },
 
