51ad8d795bc14311286a4f5971e90a0ae8d25bad
megjablon <megjablon@chromium.org>
2014-11-12 11:53:30 -0800

    Remove unnecessary proxy_retry_info.h include include in usage stats
    data_reduction_proxy_usage_stats.cc doesn't need the proxy_retry_info.h include.
    
    BUG=431026
    
    Review URL: https://codereview.chromium.org/706843003
    
    Cr-Commit-Position: refs/heads/master@{#303877}

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_usage_stats.cc
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_usage_stats.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_errors.h"
 #include "net/http/http_response_headers.h"
 #include "net/http/http_status_code.h"
-#include "net/proxy/proxy_retry_info.h"
 #include "net/proxy/proxy_server.h"
 #include "net/proxy/proxy_service.h"
 #include "net/url_request/url_request.h"
