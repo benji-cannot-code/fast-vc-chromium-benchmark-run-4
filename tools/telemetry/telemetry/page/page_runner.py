f87e186f38b771910512b1534875ea88abd95b19
nednguyen <nednguyen@google.com>
2014-11-12 14:47:51 -0800

    [Telemetry] Move ThermalThrottleCheck to outside of _RunPageAndHandleExceptionIfNeeded.
    Motivation: this patch does not introduce new functionality.
    The ThermalThrottleCheck is moved outside of
    _RunPageAndHandleExceptionIfNeeded so that later I can
    merge  _RunPageAndHandleExceptionIfNeeded and
    _RunPageAndProcessErrorIfNeeded, and merge methods like
    _RunState.WillRunPage & _RunState.PreparePgae().
    
    Other benefit of moving _RunPageAndHandleExceptionIfNeeded
    to Run(...) as shown in the patch is it helps making sure
    that thermal throttling check is always done regardless of
    what exceptions raised by _RunPageAndHandleExceptionIfNeeded.
    
    BUG=418278
    
    Review URL: https://codereview.chromium.org/713363002
    
    Cr-Commit-Position: refs/heads/master@{#303908}

--- tools/telemetry/telemetry/page/page_runner.py
+++ tools/telemetry/telemetry/page/page_runner.py
@@ -296,11 +296,8 @@ def _RunPageAndHandleExceptionIfNeeded(test, page_set, expectations,
       logging.info('Skip test for page %s because browser is not supported.'
                    % page.url)
       return
-
     expectation = expectations.GetExpectationForPage(state.browser, page)
-    _WaitForThermalThrottlingIfNeeded(state.platform)
     _RunPageAndProcessErrorIfNeeded(page, state, expectation, results)
-    _CheckThermalThrottling(state.platform)
     state.DidRunPage()
   except (exceptions.BrowserGoneException, exceptions.TabCrashException):
     state.TearDown()
@@ -382,6 +379,7 @@ def Run(test, page_set, expectations, finder_options, results):
         for _ in xrange(finder_options.page_repeat):
           results.WillRunPage(page)
           try:
+            _WaitForThermalThrottlingIfNeeded(state.platform)
             _RunPageAndHandleExceptionIfNeeded(
                 test, page_set, expectations, page, results, state)
           except Exception:
@@ -391,6 +389,7 @@ def Run(test, page_set, expectations, finder_options, results):
             state.TearDown()
             state = _RunState(test, finder_options)
           finally:
+            _CheckThermalThrottling(state.platform)
             discard_run = (test.discard_first_result and
                            page not in pages_with_discarded_first_result)
             if discard_run:
