4a77e2377165b07d2dbe4dfdd2f4122ee6e5bca3
hartmanng@chromium.org <hartmanng@chromium.org>
2014-11-12 22:43:18 +0000

    Ensure scrolling causes updateWidgetPositions even on nested frames.
    Previously, updateWidgetPositionsIfNeeded() (ie, the deferred call
    caused by scroll updates) was only called on the top-level frame,
    causing incorrectly positioned widgets in cases of multiple nested
    frames and scrollers.
    
    BUG=430069
    
    Review URL: https://codereview.chromium.org/719813002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185252 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/FrameView.cpp
+++ third_party/WebKit/Source/core/frame/FrameView.cpp
@@ -2539,8 +2539,6 @@ void FrameView::updateLayoutAndStyleForPainting()
 
     updateLayoutAndStyleIfNeededRecursive();
 
-    updateWidgetPositionsIfNeeded();
-
     RenderView* view = renderView();
     if (view) {
         TRACE_EVENT_INSTANT1(TRACE_DISABLED_BY_DEFAULT("devtools.timeline"), "UpdateLayerTree", "frame", m_frame.get());
@@ -2612,6 +2610,7 @@ void FrameView::updateLayoutAndStyleIfNeededRecursive()
     m_frame->document()->renderView()->assertRendererLaidOut();
 #endif
 
+    updateWidgetPositionsIfNeeded();
 }
 
 void FrameView::invalidateTreeIfNeededRecursive()
