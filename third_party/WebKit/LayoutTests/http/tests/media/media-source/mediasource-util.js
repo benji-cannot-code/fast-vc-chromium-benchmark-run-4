59e9728b749ed959125b388d69de1085944d70d5
henriks@opera.com <henriks@opera.com>
2014-11-12 08:21:22 +0000

    Implement seekable() according to the MediaSource specification
    BUG=432051
    R=philipj@opera.com
    
    Review URL: https://codereview.chromium.org/710173004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185191 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/media/media-source/mediasource-util.js
+++ third_party/WebKit/LayoutTests/http/tests/media/media-source/mediasource-util.js
@@ -400,4 +400,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         assert_equals(actual, expected, description);
     };
 
+    window['assertSeekableEquals'] = function(obj, expected, description)
+    {
+        var actual = timeRangesToString(obj.seekable);
+        assert_equals(actual, expected, description);
+    };
+
 })(window);
--- third_party/WebKit/Source/core/html/HTMLMediaElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLMediaElement.cpp
@@ -3353,6 +3353,9 @@ PassRefPtrWillBeRawPtr<TimeRanges> HTMLMediaElement::seekable() const
     if (!webMediaPlayer())
         return TimeRanges::create();
 
+    if (m_mediaSource)
+        return m_mediaSource->seekable();
+
     return TimeRanges::create(webMediaPlayer()->seekable());
 }
 
