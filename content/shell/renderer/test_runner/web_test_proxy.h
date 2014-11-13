7f00a145234192d2616160b64b05b9fa4295f477
mvanouwerkerk <mvanouwerkerk@chromium.org>
2014-11-13 03:46:30 -0800

    Delete unused method for getting push client.
    Depends on https://crrev.com/687933002/
    
    BUG=304341,350378
    
    Review URL: https://codereview.chromium.org/646653005
    
    Cr-Commit-Position: refs/heads/master@{#304016}

--- content/shell/renderer/test_runner/web_test_proxy.h
+++ content/shell/renderer/test_runner/web_test_proxy.h
@@ -400,9 +400,6 @@ class WebTestProxy : public Base, public WebTestProxyBase {
   virtual blink::WebString acceptLanguages() {
     return WebTestProxyBase::acceptLanguages();
   }
-  virtual blink::WebPushClient* webPushClient() {
-    return WebTestProxyBase::GetWebPushClient();
-  }
 
  private:
   DISALLOW_COPY_AND_ASSIGN(WebTestProxy);
