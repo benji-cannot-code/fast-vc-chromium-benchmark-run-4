a709ae2e157abc32ee807bc0172f6fcddfd5115b
shreeram.k@samsung.com <shreeram.k@samsung.com>
2014-11-13 04:17:29 +0000

    Math tag is not exposed in Accessibility.
    This CL will return MathRole from blink to chromium for math tag.
    
    BUG=429476
    
    Review URL: https://codereview.chromium.org/722653002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185264 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/accessibility/AXRenderObject.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXRenderObject.cpp
@@ -369,6 +369,9 @@ AccessibilityRole AXRenderObject::determineAccessibilityRole()
     if (node && node->hasTagName(dtTag))
         return DescriptionListTermRole;
 
+    if (node && (node->nodeName() == "math"))
+        return MathRole;
+
     if (node && (node->hasTagName(rpTag) || node->hasTagName(rtTag)))
         return AnnotationRole;
 
@@ -735,6 +738,9 @@ bool AXRenderObject::computeAccessibilityIsIgnored() const
         return false;
 
     if (roleValue() == DetailsRole)
+        return false;
+
+    if (roleValue() == MathRole)
         return false;
 
     if (roleValue() == MeterRole)
