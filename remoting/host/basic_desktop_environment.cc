4e08979296a71d88fb603419e4dea3774444b035
kelvinp <kelvinp@chromium.org>
2014-11-13 13:42:20 -0800

    Remote assistance on Chrome OS Part VIII - Compile on Ozone
    This CL makes It2Me host compile on platforms that uses Ozone.
    It replaces the X11-based LocalInputMonitor, MouseCursorMonitor and InputInjector
    with placeholder implementations for Chrome OS.
    An actual implementation is provided for the LocalInputMonitor with code ported from
    https://codereview.chromium.org/652293003/.
    
    BUG=431876
    
    Committed: https://crrev.com/18ffa032d49f63f8ae00e68ea7a870e61527bea5
    Cr-Commit-Position: refs/heads/master@{#303971}
    
    Review URL: https://codereview.chromium.org/700333007
    
    Cr-Commit-Position: refs/heads/master@{#304091}

--- remoting/host/basic_desktop_environment.cc
+++ remoting/host/basic_desktop_environment.cc
@@ -12,6 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_CHROMEOS)
 #include "remoting/host/chromeos/aura_desktop_capturer.h"
 #endif
+#if defined(OS_CHROMEOS) && defined(USE_OZONE)
+#include "remoting/host/chromeos/mouse_cursor_monitor_aura.h"
+#endif
 #include "remoting/host/client_session_control.h"
 #include "remoting/host/gnubby_auth_handler.h"
 #include "remoting/host/input_injector.h"
@@ -46,8 +49,13 @@ scoped_ptr<ScreenControls> BasicDesktopEnvironment::CreateScreenControls() {
 
 scoped_ptr<webrtc::MouseCursorMonitor>
 BasicDesktopEnvironment::CreateMouseCursorMonitor() {
+#if defined(OS_CHROMEOS) && defined(USE_OZONE)
+  return make_scoped_ptr(
+      new MouseCursorMonitorAura(*desktop_capture_options_));
+#else
   return make_scoped_ptr(webrtc::MouseCursorMonitor::CreateForScreen(
       *desktop_capture_options_, webrtc::kFullDesktopScreenId));
+#endif
 }
 
 std::string BasicDesktopEnvironment::GetCapabilities() const {
