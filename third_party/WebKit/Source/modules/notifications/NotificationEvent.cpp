2fe933a99f0af0148d1c4681a81f4df17964a7fd
peter@chromium.org <peter@chromium.org>
2014-11-13 15:24:36 +0000

    Introduce the NotificationEvent object.
    Notification events delivered to Service Workers (notificationclick
    and notificationerror) will receive an instance of the Notification
    object which they were triggered for, represented through a
    NotificationEvent. It's of the SW ExtendableEvent type.
    
    BUG=432527
    
    Review URL: https://codereview.chromium.org/718093002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185296 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/Source/modules/notifications/NotificationEvent.cpp
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "config.h"
+#include "modules/notifications/NotificationEvent.h"
+
+namespace blink {
+
+NotificationEventInit::NotificationEventInit()
+{
+}
+
+NotificationEvent::NotificationEvent()
+{
+}
+
+NotificationEvent::NotificationEvent(const AtomicString& type, const NotificationEventInit& initializer, WaitUntilObserver* observer)
+    : ExtendableEvent(type, initializer, observer)
+    , m_notification(initializer.notification)
+{
+}
+
+NotificationEvent::~NotificationEvent()
+{
+}
+
+const AtomicString& NotificationEvent::interfaceName() const
+{
+    return EventNames::NotificationEvent;
+}
+
+void NotificationEvent::trace(Visitor* visitor)
+{
+    visitor->trace(m_notification);
+    ExtendableEvent::trace(visitor);
+}
+
+} // namespace blink
