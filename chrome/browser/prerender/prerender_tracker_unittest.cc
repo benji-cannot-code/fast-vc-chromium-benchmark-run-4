228d01aa63fc02c257adbc4e10c2fc9bd9c6e4ec
gab <gab@chromium.org>
2014-11-13 08:40:15 -0800

    Add is_main_frame and parent_is_main_frame to ResourceRequestInfo::AllocateForTesting.
    These are required in https://codereview.chromium.org/516663002/
    
    BUG=412468
    TBR=kmadhusu, mkosiba
    
    Review URL: https://codereview.chromium.org/624073002
    
    Cr-Commit-Position: refs/heads/master@{#304034}

--- chrome/browser/prerender/prerender_tracker_unittest.cc
+++ chrome/browser/prerender/prerender_tracker_unittest.cc
@@ -227,13 +227,16 @@ TEST_F(PrerenderTrackerTest, PrerenderThrottledRedirectResume) {
       net::DEFAULT_PRIORITY,
       &delegate,
       NULL));
-  content::ResourceRequestInfo::AllocateForTesting(request.get(),
-                                                   content::RESOURCE_TYPE_IMAGE,
-                                                   NULL,
-                                                   kDefaultChildId,
-                                                   kDefaultRouteId,
-                                                   MSG_ROUTING_NONE,
-                                                   true);
+  content::ResourceRequestInfo::AllocateForTesting(
+      request.get(),
+      content::RESOURCE_TYPE_IMAGE,
+      NULL,
+      kDefaultChildId,
+      kDefaultRouteId,
+      MSG_ROUTING_NONE,
+      false,  // is_main_frame
+      false,  // parent_is_main_frame
+      true);  // is_async
 
   // Install a prerender throttle.
   PrerenderResourceThrottle throttle(request.get());
@@ -277,7 +280,9 @@ TEST_F(PrerenderTrackerTest, PrerenderThrottledRedirectMainFrame) {
       kDefaultChildId,
       kDefaultRouteId,
       MSG_ROUTING_NONE,
-      true);
+      true,   // is_main_frame
+      false,  // parent_is_main_frame
+      true);  // is_async
 
   // Install a prerender throttle.
   PrerenderResourceThrottle throttle(request.get());
@@ -312,13 +317,16 @@ TEST_F(PrerenderTrackerTest, PrerenderThrottledRedirectSyncXHR) {
       net::DEFAULT_PRIORITY,
       &delegate,
       NULL));
-  content::ResourceRequestInfo::AllocateForTesting(request.get(),
-                                                   content::RESOURCE_TYPE_XHR,
-                                                   NULL,
-                                                   kDefaultChildId,
-                                                   kDefaultRouteId,
-                                                   MSG_ROUTING_NONE,
-                                                   false);
+  content::ResourceRequestInfo::AllocateForTesting(
+      request.get(),
+      content::RESOURCE_TYPE_XHR,
+      NULL,
+      kDefaultChildId,
+      kDefaultRouteId,
+      MSG_ROUTING_NONE,
+      false,   // is_main_frame
+      false,   // parent_is_main_frame
+      false);  // is_async
 
   // Install a prerender throttle.
   PrerenderResourceThrottle throttle(request.get());
