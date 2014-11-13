a20b00f9c814faa269dced8fc9fb04007dc9f589
fmalita <fmalita@chromium.org>
2014-11-13 09:45:23 -0800

    Roll src/third_party/skia 5adbf1b:6987dca
    Summary of changes available at:
    https://chromium.googlesource.com/skia/+log/5adbf1b..6987dca
    
    CQ_EXTRA_TRYBOTS=tryserver.blink:linux_blink_rel,linux_blink_dbg
    TBR=jcgregorio@google.com,reed@google.com
    
    Review URL: https://codereview.chromium.org/728523002
    
    Cr-Commit-Position: refs/heads/master@{#304042}

--- skia/ext/vector_platform_device_emf_win.h
+++ skia/ext/vector_platform_device_emf_win.h
@@ -80,8 +80,7 @@ class VectorPlatformDeviceEmf : public SkBitmapDevice, public PlatformDevice {
   void LoadClipRegion();
 
  protected:
-  virtual SkBaseDevice* onCreateDevice(const SkImageInfo& info,
-                                       Usage usage) override;
+  virtual SkBaseDevice* onCreateCompatibleDevice(const CreateInfo& info) override;
 
  private:
   // Applies the SkPaint's painting properties in the current GDI context, if
