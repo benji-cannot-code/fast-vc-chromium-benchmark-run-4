3246b57fe3400fce1f2669a2320955d76a6950f3
Sadrul Habib Chowdhury <sadrul@chromium.org>
2014-11-13 00:03:38 -0500

    views: Return MODAL_TYPE_WINDOW since the dialog is browser-modal.
    The modality checks added in http://crrev.com/303124 catches this issue where
    a browser-modal window was being created with modal-type MODAL_TYPE_CHILD.
    Using the correct modal-type of MODAL_TYPE_WINDOW also fixes the focus issue
    in this dialog.
    
    BUG=420546
    R=msw@chromium.org
    
    Review URL: https://codereview.chromium.org/721753005
    
    Cr-Commit-Position: refs/heads/master@{#303988}

--- chrome/browser/ui/views/sync/profile_signin_confirmation_dialog_views.cc
+++ chrome/browser/ui/views/sync/profile_signin_confirmation_dialog_views.cc
@@ -154,7 +154,7 @@ void ProfileSigninConfirmationDialogViews::OnClosed() {
 }
 
 ui::ModalType ProfileSigninConfirmationDialogViews::GetModalType() const {
-  return ui::MODAL_TYPE_CHILD;
+  return ui::MODAL_TYPE_WINDOW;
 }
 
 void ProfileSigninConfirmationDialogViews::ViewHierarchyChanged(
