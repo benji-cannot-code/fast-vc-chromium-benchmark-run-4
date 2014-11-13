9cbd494936a5fbc72890beb34eca2d59eb619de1
zhenyu.shan@intel.com <zhenyu.shan@intel.com>
2014-11-13 19:07:31 +0000

    Remove multi-frame image decoder when the image is completely decoded
    Count decoded frames in ImageFrameGenerator, and remove remove multi-frame image decoders when the image is completely decoded
    
    This saves memory on pages with a lot of multi-frame images. data and discussion please see https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/KZ8EDtEZ9-c
    
    BUG=429614
    
    Review URL: https://codereview.chromium.org/688423004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185311 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/DeferredImageDecoderTest.cpp
+++ third_party/WebKit/Source/platform/graphics/DeferredImageDecoderTest.cpp
@@ -75,6 +75,7 @@ public:
         ImageDecodingStore::instance()->setCacheLimitInBytes(1024 * 1024);
         DeferredImageDecoder::setEnabled(true);
         m_data = SharedBuffer::create(whitePNG, sizeof(whitePNG));
+        m_frameCount = 1;
         OwnPtr<MockImageDecoder> decoder = MockImageDecoder::create(this);
         m_actualDecoder = decoder.get();
         m_actualDecoder->setSize(1, 1);
@@ -82,7 +83,6 @@ public:
         m_canvas.reset(SkCanvas::NewRasterN32(100, 100));
         ASSERT_TRUE(m_canvas.get());
         m_frameBufferRequestCount = 0;
-        m_frameCount = 1;
         m_repetitionCount = cAnimationNone;
         m_status = ImageFrame::FrameComplete;
         m_frameDuration = 0;
