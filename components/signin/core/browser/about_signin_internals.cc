1b23823db581c900a2fba58fe976bce663503c14
vadimt <vadimt@chromium.org>
2014-11-13 11:08:11 -0800

    Adding instrumentation to locate the source of jankiness.
    Prior instrumentations showed that invoking OnAccessTokenRequested observers in  OAuth2TokenService::StartRequestForClientWithContext
    alone is responsible for 23.5 janks per hour in UI thread. I need to instrument
    the code inside it to find out which part causes jank.
    
    This is a mechanical change that adds instrumentation required to locate the
    source of jankiness (i.e. a long-running fragment of code executed as a part of
    the task that causes jank) in the code. See the bug for details on what kind of
    jank we are after.
    A number of similar CLs were landed, and none of them caused issues. They've
    helped to find and fix janky code. The code of the instrumentation is highly
    optimized and is not expected to affect performance. The code simply creates a
    diagnostic task which is identical to ones created by PostTask or IPC message
    handlers. The task gets created only in developer build and in Canary channel.
    
    BUG=422460
    
    Review URL: https://codereview.chromium.org/721523002
    
    Cr-Commit-Position: refs/heads/master@{#304056}

--- components/signin/core/browser/about_signin_internals.cc
+++ components/signin/core/browser/about_signin_internals.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/time_formatting.h"
 #include "base/logging.h"
 #include "base/prefs/pref_service.h"
+#include "base/profiler/scoped_tracker.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "components/signin/core/browser/profile_oauth2_token_service.h"
@@ -223,6 +224,11 @@ void AboutSigninInternals::Shutdown() {
 }
 
 void AboutSigninInternals::NotifyObservers() {
+  // TODO(vadimt): Remove ScopedTracker below once crbug.com/422460 is fixed.
+  tracked_objects::ScopedTracker tracking_profile(
+      FROM_HERE_WITH_EXPLICIT_FUNCTION(
+          "422460 AboutSigninInternals::NotifyObservers"));
+
   scoped_ptr<base::DictionaryValue> signin_status_value =
       signin_status_.ToValue(client_->GetProductVersion());
   FOR_EACH_OBSERVER(AboutSigninInternals::Observer,
@@ -238,6 +244,11 @@ void AboutSigninInternals::OnAccessTokenRequested(
     const std::string& account_id,
     const std::string& consumer_id,
     const OAuth2TokenService::ScopeSet& scopes) {
+  // TODO(vadimt): Remove ScopedTracker below once crbug.com/422460 is fixed.
+  tracked_objects::ScopedTracker tracking_profile(
+      FROM_HERE_WITH_EXPLICIT_FUNCTION(
+          "422460 AboutSigninInternals::OnAccessTokenRequested"));
+
   TokenInfo* token = signin_status_.FindToken(account_id, consumer_id, scopes);
   if (token) {
     *token = TokenInfo(consumer_id, scopes);
