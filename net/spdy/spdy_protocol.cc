a819fce5397571703c4d8662ffa62a23382f3c3f
bnc <bnc@chromium.org>
2014-11-12 21:41:39 -0800

    Add HTTP/2 error code from latest draft.
    Add HTTP_1_1_REQUIRED error code for RST_STREAM and GOAWAY frames that was
    introduced in draft-15.  Also add INADEQUATE_SECURTY error code wherever it had
    been forgotten.
    
    This lands server change 79690279 by bnc in spdy_framer.cc and spdy_protocol.{h,cc}.
    
    BUG=431306
    
    Review URL: https://codereview.chromium.org/700583005
    
    Cr-Commit-Position: refs/heads/master@{#303993}

--- net/spdy/spdy_protocol.cc
+++ net/spdy/spdy_protocol.cc
@@ -373,9 +373,9 @@ bool SpdyConstants::IsValidRstStreamStatus(SpdyMajorVersion version,
       }
       */
 
-      // ENHANCE_YOUR_CALM is the last valid status code.
+      // HTTP_1_1_REQUIRED is the last valid status code.
       if (rst_stream_status_field >
-          SerializeRstStreamStatus(version, RST_STREAM_ENHANCE_YOUR_CALM)) {
+          SerializeRstStreamStatus(version, RST_STREAM_HTTP_1_1_REQUIRED)) {
         return false;
       }
 
@@ -437,6 +437,10 @@ SpdyRstStreamStatus SpdyConstants::ParseRstStreamStatus(
           return RST_STREAM_CONNECT_ERROR;
         case 11:
           return RST_STREAM_ENHANCE_YOUR_CALM;
+        case 12:
+          return RST_STREAM_INADEQUATE_SECURITY;
+        case 13:
+          return RST_STREAM_HTTP_1_1_REQUIRED;
       }
       break;
   }
@@ -500,6 +504,10 @@ int SpdyConstants::SerializeRstStreamStatus(
           return 10;
         case RST_STREAM_ENHANCE_YOUR_CALM:
           return 11;
+        case RST_STREAM_INADEQUATE_SECURITY:
+          return 12;
+        case RST_STREAM_HTTP_1_1_REQUIRED:
+          return 13;
         default:
           LOG(DFATAL) << "Unhandled RST_STREAM status "
                       << rst_stream_status;
@@ -590,6 +598,8 @@ SpdyGoAwayStatus SpdyConstants::ParseGoAwayStatus(SpdyMajorVersion version,
           return GOAWAY_ENHANCE_YOUR_CALM;
         case 12:
           return GOAWAY_INADEQUATE_SECURITY;
+        case 13:
+          return GOAWAY_HTTP_1_1_REQUIRED;
       }
       break;
   }
@@ -667,6 +677,7 @@ int SpdyConstants::SerializeGoAwayStatus(SpdyMajorVersion version,
         case GOAWAY_CONNECT_ERROR:
         case GOAWAY_ENHANCE_YOUR_CALM:
         case GOAWAY_INADEQUATE_SECURITY:
+        case GOAWAY_HTTP_1_1_REQUIRED:
           return 1;  // PROTOCOL_ERROR.
         default:
           LOG(DFATAL) << "Serializing unhandled GOAWAY status " << status;
@@ -701,6 +712,8 @@ int SpdyConstants::SerializeGoAwayStatus(SpdyMajorVersion version,
           return 11;
         case GOAWAY_INADEQUATE_SECURITY:
           return 12;
+        case GOAWAY_HTTP_1_1_REQUIRED:
+          return 13;
         default:
           LOG(DFATAL) << "Serializing unhandled GOAWAY status " << status;
           return -1;
