e54a4779b09c4754fbc1b7f43b13a9c75197aa5f
dcheng <dcheng@chromium.org>
2014-11-12 16:50:58 -0800

    Remove implicit conversions from scoped_refptr to T* in content/
    This patch was generated by running the rewrite_scoped_refptr clang tool
    on a Windows build.
    
    BUG=110610
    
    Review URL: https://codereview.chromium.org/719343002
    
    Cr-Commit-Position: refs/heads/master@{#303943}

--- content/child/npapi/webplugin_delegate_impl_win.cc
+++ content/child/npapi/webplugin_delegate_impl_win.cc
@@ -797,7 +797,7 @@ bool WebPluginDelegateImpl::WindowedReposition(
 }
 
 void WebPluginDelegateImpl::WindowedSetWindow() {
-  if (!instance_)
+  if (!instance_.get())
     return;
 
   if (!windowed_handle_) {
