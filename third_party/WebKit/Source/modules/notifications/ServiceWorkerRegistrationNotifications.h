029427efa43a0c86399e47fa0fcb477d9bb7db21
peter@chromium.org <peter@chromium.org>
2014-11-13 22:55:14 +0000

    Implement the ServiceWorkerRegistration.showNotification() method.
    This patch implements the showNotification() method by extending the
    ServiceWorkerRegistration object, and calling through to the Platform
    for requesting display of a persistent notification.
    
    BUG=432527
    
    Review URL: https://codereview.chromium.org/717273002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185326 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/Source/modules/notifications/ServiceWorkerRegistrationNotifications.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ServiceWorkerRegistrationNotifications_h
+#define ServiceWorkerRegistrationNotifications_h
+
+#include "bindings/core/v8/ScriptPromise.h"
+#include "modules/serviceworkers/ServiceWorkerRegistration.h"
+
+namespace blink {
+
+class NotificationOptions;
+class ScriptState;
+
+class ServiceWorkerRegistrationNotifications {
+public:
+    static ScriptPromise showNotification(ScriptState*, ServiceWorkerRegistration&, const String& title, const NotificationOptions&);
+};
+
+} // namespace blink
+
+#endif // ServiceWorkerRegistrationNotifications_h
