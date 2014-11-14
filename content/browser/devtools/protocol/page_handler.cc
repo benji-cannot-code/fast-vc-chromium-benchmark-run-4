6f16c1954e91f00b00b62d09108f8094a7e84b41
vkuzkokov <vkuzkokov@chromium.org>
2014-11-14 10:08:53 -0800

    DevTools: Throttle Screencast on slow connections (First part of 3-sided commit)
    BUG=397107
    
    Review URL: https://codereview.chromium.org/725133002
    
    Cr-Commit-Position: refs/heads/master@{#304235}

--- content/browser/devtools/protocol/page_handler.cc
+++ content/browser/devtools/protocol/page_handler.cc
@@ -353,6 +353,10 @@ Response PageHandler::StopScreencast() {
   return Response::FallThrough();
 }
 
+Response PageHandler::ScreencastFrameAck(int frame_number) {
+  return Response::OK();
+}
+
 Response PageHandler::HandleJavaScriptDialog(bool accept,
                                              const std::string* prompt_text) {
   base::string16 prompt_override;
