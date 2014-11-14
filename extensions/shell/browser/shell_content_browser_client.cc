b22d86e92cd699d8aac2b3b124f7a776a6e6fbe6
jamescook <jamescook@chromium.org>
2014-11-14 11:13:35 -0800

    app_shell: Add CreateShellBrowserMainParts to initialization
    This allows subclasses to swap in their own ShellBrowserMainParts to
    modify the startup sequence.
    
    BUG=none
    TEST=app_shell_browsertests
    
    Review URL: https://codereview.chromium.org/733553002
    
    Cr-Commit-Position: refs/heads/master@{#304247}

--- extensions/shell/browser/shell_content_browser_client.cc
+++ extensions/shell/browser/shell_content_browser_client.cc
@@ -70,7 +70,7 @@ content::BrowserContext* ShellContentBrowserClient::GetBrowserContext() {
 content::BrowserMainParts* ShellContentBrowserClient::CreateBrowserMainParts(
     const content::MainFunctionParams& parameters) {
   browser_main_parts_ =
-      new ShellBrowserMainParts(parameters, browser_main_delegate_);
+      CreateShellBrowserMainParts(parameters, browser_main_delegate_);
   return browser_main_parts_;
 }
 
@@ -218,6 +218,17 @@ void ShellContentBrowserClient::GetAdditionalAllowedSchemesForFileSystem(
   additional_allowed_schemes->push_back(kExtensionScheme);
 }
 
+content::DevToolsManagerDelegate*
+ShellContentBrowserClient::GetDevToolsManagerDelegate() {
+  return new content::ShellDevToolsManagerDelegate(GetBrowserContext());
+}
+
+ShellBrowserMainParts* ShellContentBrowserClient::CreateShellBrowserMainParts(
+    const content::MainFunctionParams& parameters,
+    ShellBrowserMainDelegate* browser_main_delegate) {
+  return new ShellBrowserMainParts(parameters, browser_main_delegate);
+}
+
 void ShellContentBrowserClient::AppendRendererSwitches(
     CommandLine* command_line) {
   // TODO(jamescook): Should we check here if the process is in the extension
@@ -243,11 +254,6 @@ const Extension* ShellContentBrowserClient::GetExtension(
       ExtensionRegistry::Get(site_instance->GetBrowserContext());
   return registry->enabled_extensions().GetExtensionOrAppByURL(
       site_instance->GetSiteURL());
-}
-
-content::DevToolsManagerDelegate*
-ShellContentBrowserClient::GetDevToolsManagerDelegate() {
-  return new content::ShellDevToolsManagerDelegate(GetBrowserContext());
 }
 
 }  // namespace extensions
