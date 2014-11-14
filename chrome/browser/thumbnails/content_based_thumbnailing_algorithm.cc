f9f1997c3cdbe7d3382af6400af6e9d29264417b
tommycli <tommycli@chromium.org>
2014-11-13 18:37:33 -0800

    Color_utils: Move image boringness scoring from thumbnails to ui/gfx.
    This is a spinoff of https://codereview.chromium.org/707623002/, and
    allows the CalculateBoringScore luma histogram metric to be reused in
    other parts of the code. (It's used in content/ in above patch).
    
    BUG=403800
    
    Review URL: https://codereview.chromium.org/720343002
    
    Cr-Commit-Position: refs/heads/master@{#304153}

--- chrome/browser/thumbnails/content_based_thumbnailing_algorithm.cc
+++ chrome/browser/thumbnails/content_based_thumbnailing_algorithm.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/thumbnails/simple_thumbnail_crop.h"
 #include "content/public/browser/browser_thread.h"
 #include "third_party/skia/include/core/SkBitmap.h"
+#include "ui/gfx/color_utils.h"
 #include "ui/gfx/scrollbar_size.h"
 #include "ui/gfx/size_conversions.h"
 #include "ui/gfx/skbitmap_operations.h"
@@ -77,7 +78,7 @@ void ContentBasedThumbnailingAlgorithm::ProcessBitmap(
   if (source_bitmap.width() <= target_thumbnail_size.width() ||
       source_bitmap.height() <= target_thumbnail_size.height()) {
     context->score.boring_score =
-        SimpleThumbnailCrop::CalculateBoringScore(source_bitmap);
+        color_utils::CalculateBoringScore(source_bitmap);
     context->score.good_clipping =
         (context->clip_result == CLIP_RESULT_WIDER_THAN_TALL ||
          context->clip_result == CLIP_RESULT_TALLER_THAN_WIDE ||
@@ -188,7 +189,7 @@ void ContentBasedThumbnailingAlgorithm::CreateRetargetedThumbnail(
                           base::TimeTicks::Now() - begin_compute_thumbnail);
   }
   context->score.boring_score =
-        SimpleThumbnailCrop::CalculateBoringScore(source_bitmap);
+      color_utils::CalculateBoringScore(source_bitmap);
   if (!processing_failed)
     context->score.boring_score *= kScoreBoostFromSuccessfulRetargeting;
   context->score.good_clipping =
