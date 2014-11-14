e59179d35bbc5a5df188763b400819d27f0f7996
siva.gunturi <siva.gunturi@samsung.com>
2014-11-13 17:55:13 -0800

    [Webgl-chromium] Test  to validate glinfo when  webglcontext fails.
    Now we read the GPU(Vendor,Renderer,Driver info) from
    chromium side and send it as part of status message when
    EventTypeNames::webglcontextcreationerror is being sent.
    This patch introduces the test for the above functionality.
    
    forceNextWebGLContextCreationToFail(bool) is introduced
    to fail the webgl context forcefully from JS-test runner
    utility.
    
    The below cl uses the test-runner code in this patch
    https://codereview.chromium.org/706193003/
    
    BUG=412400
    
    Review URL: https://codereview.chromium.org/706193003
    
    Cr-Commit-Position: refs/heads/master@{#304143}

--- content/shell/renderer/test_runner/test_runner.cc
+++ content/shell/renderer/test_runner/test_runner.cc
@@ -309,6 +309,8 @@ class TestRunnerBindings : public gin::Wrappable<TestRunnerBindings> {
 
   void NotImplemented(const gin::Arguments& args);
 
+  void ForceNextWebGLContextCreationToFail();
+
   base::WeakPtr<TestRunner> runner_;
 
   DISALLOW_COPY_AND_ASSIGN(TestRunnerBindings);
@@ -545,6 +547,8 @@ gin::ObjectTemplateBuilder TestRunnerBindings::GetObjectTemplateBuilder(
                  &TestRunnerBindings::SetMockPushClientError)
       .SetMethod("setBluetoothMockDataSet",
                  &TestRunnerBindings::SetBluetoothMockDataSet)
+      .SetMethod("forceNextWebGLContextCreationToFail",
+                 &TestRunnerBindings::ForceNextWebGLContextCreationToFail)
 
       // Properties.
       .SetProperty("platformName", &TestRunnerBindings::PlatformName)
@@ -1447,6 +1451,11 @@ bool TestRunnerBindings::InterceptPostMessage() {
 void TestRunnerBindings::SetInterceptPostMessage(bool value) {
   if (runner_)
     runner_->intercept_post_message_ = value;
+}
+
+void TestRunnerBindings::ForceNextWebGLContextCreationToFail() {
+  if (runner_)
+    runner_->ForceNextWebGLContextCreationToFail();
 }
 
 void TestRunnerBindings::NotImplemented(const gin::Arguments& args) {
@@ -2844,6 +2853,11 @@ void TestRunner::CapturePixelsAsyncThen(v8::Handle<v8::Function> callback) {
   proxy_->CapturePixelsAsync(base::Bind(&TestRunner::CapturePixelsCallback,
                                         weak_factory_.GetWeakPtr(),
                                         base::Passed(&task)));
+}
+
+void TestRunner::ForceNextWebGLContextCreationToFail() {
+  if (web_view_)
+    web_view_->forceNextWebGLContextCreationToFail();
 }
 
 void TestRunner::CopyImageAtAndCapturePixelsAsyncThen(
