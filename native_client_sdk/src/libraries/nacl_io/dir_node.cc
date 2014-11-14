ded633b98a591b69ad65d1405f536e630a21051f
Ben Smith <binji@chromium.org>
2014-11-13 18:24:42 -0800

    [NaCl SDK] Update stat times for memfs.
    BUG= https://code.google.com/p/naclports/issues/detail?id=159
    R=sbc@chromium.org
    
    Review URL: https://codereview.chromium.org/690583007
    
    Cr-Commit-Position: refs/heads/master@{#304151}

--- native_client_sdk/src/libraries/nacl_io/dir_node.cc
+++ native_client_sdk/src/libraries/nacl_io/dir_node.cc
@@ -29,6 +29,7 @@ DirNode::DirNode(Filesystem* filesystem, mode_t mode)
       cache_built_(false) {
   SetType(S_IFDIR);
   SetMode(mode);
+  UpdateTime(UPDATE_ATIME | UPDATE_MTIME | UPDATE_CTIME);
 }
 
 DirNode::~DirNode() {
@@ -66,12 +67,14 @@ Error DirNode::GetDents(size_t offs,
                         int* out_bytes) {
   AUTO_LOCK(node_lock_);
   BuildCache_Locked();
+  UpdateTime(UPDATE_ATIME);
   return cache_.GetDents(offs, pdir, size, out_bytes);
 }
 
 Error DirNode::Fchmod(mode_t mode) {
   AUTO_LOCK(node_lock_);
   SetMode(mode);
+  UpdateTime(UPDATE_CTIME);
   return 0;
 }
 
@@ -96,6 +99,8 @@ Error DirNode::AddChild(const std::string& name, const ScopedNode& node) {
     return EEXIST;
   }
 
+  UpdateTime(UPDATE_MTIME | UPDATE_CTIME);
+
   node->Link();
   map_[name] = node;
   ClearCache_Locked();
@@ -106,6 +111,7 @@ Error DirNode::RemoveChild(const std::string& name) {
   AUTO_LOCK(node_lock_);
   NodeMap_t::iterator it = map_.find(name);
   if (it != map_.end()) {
+    UpdateTime(UPDATE_MTIME | UPDATE_CTIME);
     it->second->Unlink();
     map_.erase(it);
     ClearCache_Locked();
--- native_client_sdk/src/libraries/nacl_io/memfs/mem_fs_node.cc
+++ native_client_sdk/src/libraries/nacl_io/memfs/mem_fs_node.cc
@@ -36,6 +36,7 @@ MemFsNode::MemFsNode(Filesystem* filesystem)
     data_(NULL),
     data_capacity_(0) {
   SetType(S_IFREG);
+  UpdateTime(UPDATE_ATIME | UPDATE_MTIME | UPDATE_CTIME);
 }
 
 MemFsNode::~MemFsNode() {
@@ -56,6 +57,10 @@ Error MemFsNode::Read(const HandleAttr& attr,
 
   if (attr.offs + count > size) {
     count = size - attr.offs;
+  }
+
+  if (count > 0) {
+    UpdateTime(UPDATE_ATIME);
   }
 
   memcpy(buf, data_ + attr.offs, count);
@@ -83,6 +88,10 @@ Error MemFsNode::Write(const HandleAttr& attr,
     }
   }
 
+  if (count > 0) {
+    UpdateTime(UPDATE_MTIME | UPDATE_CTIME);
+  }
+
   memcpy(data_ + attr.offs, buf, count);
   *out_bytes = static_cast<int>(count);
   return 0;
@@ -90,7 +99,11 @@ Error MemFsNode::Write(const HandleAttr& attr,
 
 Error MemFsNode::FTruncate(off_t new_size) {
   AUTO_LOCK(node_lock_);
-  return Resize(new_size);
+  Error error = Resize(new_size);
+  if (error == 0) {
+    UpdateTime(UPDATE_MTIME | UPDATE_CTIME);
+  }
+  return error;
 }
 
 Error MemFsNode::Resize(off_t new_length) {
@@ -137,6 +150,7 @@ Error MemFsNode::Futimens(const struct timespec times[2]) {
 Error MemFsNode::Fchmod(mode_t mode) {
   AUTO_LOCK(node_lock_);
   SetMode(mode);
+  UpdateTime(UPDATE_CTIME);
   return 0;
 }
 
