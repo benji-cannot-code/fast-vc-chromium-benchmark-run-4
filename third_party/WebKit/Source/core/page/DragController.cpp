11b3fc8058f491fd4fba3be677e912512f1c2648
dcheng@chromium.org <dcheng@chromium.org>
2014-11-14 02:33:08 +0000

    Don't abort drags if drag image generation fails.
    This unintuitively caused drags to end immediately if a page called
    setDragImage with a detached node or one that hadn't been through layout
    yet. There's nothing in the spec that says the drags should abort in
    this case either, so this makes Blink more spec-conformant.
    
    BUG=413795
    
    Review URL: https://codereview.chromium.org/724663002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185343 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/DragController.cpp
+++ third_party/WebKit/Source/core/page/DragController.cpp
@@ -932,8 +932,6 @@ bool DragController::startDrag(LocalFrame* src, const DragState& state, const Pl
         }
         doSystemDrag(dragImage.get(), dragLocation, mouseDraggedPoint, dataTransfer, src, true);
     } else if (state.m_dragType == DragSourceActionDHTML) {
-        if (!dragImage)
-            return false;
         doSystemDrag(dragImage.get(), dragLocation, dragOrigin, dataTransfer, src, false);
     } else {
         ASSERT_NOT_REACHED();
