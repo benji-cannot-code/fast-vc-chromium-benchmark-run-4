d2612dac0754fc7364710b905500556cdfa935e2
ccameron@chromium.org <ccameron@chromium.org>
2014-11-14 18:50:59 +0000

    Add development Blink setting for impl-side rubber-banding
    Add a flag to indicate if the compositor thread is performing the
    Mac overscroll rubber-band bounce effect.
    
    This is false by default and will be set to true by
    --enable-threaded-event-handling-mac until developement is finished,
    whereupon it will be true by default and removed.
    
    BUG=133097
    TBR=dglazkov
    
    Review URL: https://codereview.chromium.org/722353002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185385 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/FrameView.cpp
+++ third_party/WebKit/Source/core/frame/FrameView.cpp
@@ -1674,6 +1674,11 @@ bool FrameView::isRubberBandInProgress() const
     return false;
 }
 
+bool FrameView::rubberBandingOnCompositorThread() const
+{
+    return m_frame->settings()->rubberBandingOnCompositorThread();
+}
+
 HostWindow* FrameView::hostWindow() const
 {
     Page* page = frame().page();
