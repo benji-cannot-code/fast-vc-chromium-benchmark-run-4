0dceb28854af9d4364e8d9f3d03f75eab749e971
hugo.holgersson <hugo.holgersson@sonymobile.com>
2014-11-14 02:22:10 -0800

    Fix leaked external surface when several EME videos coexist
    Background: In the code we see that BrowserMediaPlayerManager can only hold
    one ExternalVideoSurfaceContainer and a ExternalVideoSurfaceContainer can
    only hold one SurfaceView.
    
    Problem: When ExternalVideoSurfaceContainer starts holding a new surface,
    createSurfaceView() will change mSurfaceView to the new SurfaceView. It
    does not release the old SurfaceView before it looses its reference to it.
    Leaving a SurfaceView behind could cause Z-fights with other SurfaceViews,
    such as fullscreen video playback.
    
    Solution: By deallocating player A's SurfaceView when player B requests one
    we ensure that no SurfaceView is leaked. To ensure deallocation we always
    call setActiveContainer(this) before createSurfaceView().
    
    BUG=431318
    TEST=Flip between two EME videos, see crbug.com/431318 for detailed info.
    
    Review URL: https://codereview.chromium.org/709003004
    
    Cr-Commit-Position: refs/heads/master@{#304191}

--- android_webview/java/src/org/chromium/android_webview/ExternalVideoSurfaceContainer.java
+++ android_webview/java/src/org/chromium/android_webview/ExternalVideoSurfaceContainer.java
@@ -119,9 +119,7 @@ public class ExternalVideoSurfaceContainer implements SurfaceHolder.Callback {
     protected void requestExternalVideoSurface(int playerId) {
         if (mPlayerId == playerId) return;
 
-        if (mPlayerId == INVALID_PLAYER_ID) {
-            setActiveContainer(this);
-        }
+        setActiveContainer(this);
 
         mPlayerId = playerId;
         initializeCurrentPositionOfSurfaceView();
@@ -170,6 +168,7 @@ public class ExternalVideoSurfaceContainer implements SurfaceHolder.Callback {
     }
 
     private void createSurfaceView() {
+        assert mSurfaceView == null;
         mSurfaceView = new NoPunchingSurfaceView(mContentViewCore.getContext());
         mSurfaceView.getHolder().addCallback(this);
         // SurfaceHoder.surfaceCreated() will be called after the SurfaceView is attached to
--- chromecast/browser/android/apk/src/org/chromium/chromecast/shell/ExternalVideoSurfaceContainer.java
+++ chromecast/browser/android/apk/src/org/chromium/chromecast/shell/ExternalVideoSurfaceContainer.java
@@ -125,9 +125,7 @@ public class ExternalVideoSurfaceContainer implements SurfaceHolder.Callback {
     protected void requestExternalVideoSurface(int playerId) {
         if (mPlayerId == playerId) return;
 
-        if (mPlayerId == INVALID_PLAYER_ID) {
-            setActiveContainer(this);
-        }
+        setActiveContainer(this);
 
         mPlayerId = playerId;
         initializeCurrentPositionOfSurfaceView();
@@ -176,6 +174,7 @@ public class ExternalVideoSurfaceContainer implements SurfaceHolder.Callback {
     }
 
     private void createSurfaceView() {
+        assert mSurfaceView == null;
         mSurfaceView = new NoPunchingSurfaceView(mContentViewCore.getContext());
         mSurfaceView.getHolder().addCallback(this);
         // SurfaceHoder.surfaceCreated() will be called after the SurfaceView is attached to
