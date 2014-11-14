c3b7534bb1d0d6abcc35cf0cc6e0bdda9dcc3589
dcheng@chromium.org <dcheng@chromium.org>
2014-11-14 00:57:32 +0000

    Remove EventHandler::m_didStartDrag.
    EventHandler maintains a ton of state, and making sure everything is in
    sync gets rather complicated. m_didStartDrag was only used as by gesture
    code, to detect if it should fall back to default handling.
    
    This patch changes handleDrag()'s CheckDragHysteresis parameter to
    DragIniator, which more accurately reflects how it's used. The updated
    parameter is also used to determine when default handling is needed,
    eliminating the need for the m_didStartDrag bool. Yay!
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/720693003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185337 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/EventHandler.h
+++ third_party/WebKit/Source/core/page/EventHandler.h
@@ -81,7 +81,7 @@ class WheelEvent;
 class Widget;
 
 enum AppendTrailingWhitespace { ShouldAppendTrailingWhitespace, DontAppendTrailingWhitespace };
-enum CheckDragHysteresis { ShouldCheckDragHysteresis, DontCheckDragHysteresis };
+enum class DragInitiator;
 
 class EventHandler : public NoBaseWillBeGarbageCollectedFinalized<EventHandler> {
     WTF_MAKE_NONCOPYABLE(EventHandler);
@@ -277,7 +277,7 @@ private:
 
     void clearDragDataTransfer();
 
-    bool handleDrag(const MouseEventWithHitTestResults&, CheckDragHysteresis);
+    bool handleDrag(const MouseEventWithHitTestResults&, DragInitiator);
     bool tryStartDrag(const MouseEventWithHitTestResults&);
     void clearDragState();
 
@@ -396,7 +396,6 @@ private:
     RefPtrWillBeMember<Scrollbar> m_scrollbarHandlingScrollGesture;
 
     double m_maxMouseMovedDuration;
-    bool m_didStartDrag;
 
     bool m_longTapShouldInvokeContextMenu;
 
