a6ae25a3120f6cf2111d05bf26a7be6c9257c319
andresantoso <andresantoso@chromium.org>
2014-11-13 18:41:46 -0800

    Mac: Fix favicon vertical positioning when dragging a tab.
    The tab view was initially created with height 25 and the icon view is
    positioned inside it. Later on, the height is changed to 26 and the icon view
    shifted because of its autoresizingMask is NSViewMinYMargin.
    
    BUG=428897
    
    Review URL: https://codereview.chromium.org/725783002
    
    Cr-Commit-Position: refs/heads/master@{#304155}

--- chrome/browser/ui/cocoa/tabs/tab_strip_controller.mm
+++ chrome/browser/ui/cocoa/tabs/tab_strip_controller.mm
@@ -595,7 +595,7 @@ NSImage* Overlay(NSImage* ground, NSImage* overlay, CGFloat alpha) {
 }
 
 + (CGFloat)defaultTabHeight {
-  return 26.0;
+  return [TabController defaultTabHeight];
 }
 
 + (CGFloat)defaultLeftIndentForControls {
