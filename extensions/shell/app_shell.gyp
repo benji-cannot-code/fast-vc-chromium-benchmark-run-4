e0c7c8ea549144cf47a4b68f7e1e8582453fb43e
zork <zork@chromium.org>
2014-11-14 11:36:37 -0800

    app_shell: Add SpeechRecognitionManagerDelegate.  This allows speech recognition to work in apps.
    BUG=430724
    
    Review URL: https://codereview.chromium.org/718363002
    
    Cr-Commit-Position: refs/heads/master@{#304251}

--- extensions/shell/app_shell.gyp
+++ extensions/shell/app_shell.gyp
@@ -105,6 +105,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/shell_runtime_api_delegate.h',
         'browser/shell_special_storage_policy.cc',
         'browser/shell_special_storage_policy.h',
+        'browser/shell_speech_recognition_manager_delegate.cc',
+        'browser/shell_speech_recognition_manager_delegate.h',
         'browser/shell_url_request_context_getter.cc',
         'browser/shell_url_request_context_getter.h',
         'browser/shell_web_contents_modal_dialog_manager.cc',
--- extensions/shell/browser/shell_content_browser_client.cc
+++ extensions/shell/browser/shell_content_browser_client.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/shell/browser/shell_browser_context.h"
 #include "extensions/shell/browser/shell_browser_main_parts.h"
 #include "extensions/shell/browser/shell_extension_system.h"
+#include "extensions/shell/browser/shell_speech_recognition_manager_delegate.h"
 #include "url/gurl.h"
 
 #if !defined(DISABLE_NACL)
@@ -191,6 +192,11 @@ void ShellContentBrowserClient::AppendExtraCommandLineSwitches(
       command_line->GetSwitchValueASCII(::switches::kProcessType);
   if (process_type == ::switches::kRendererProcess)
     AppendRendererSwitches(command_line);
+}
+
+content::SpeechRecognitionManagerDelegate*
+ShellContentBrowserClient::GetSpeechRecognitionManagerDelegate() {
+  return new speech::ShellSpeechRecognitionManagerDelegate();
 }
 
 content::BrowserPpapiHost*
