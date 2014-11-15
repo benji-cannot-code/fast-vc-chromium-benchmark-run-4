6bf98b2e19cdfc3c73303cf13836d8b3c9dfbba2
rdevlin.cronin <rdevlin.cronin@chromium.org>
2014-11-14 17:40:32 -0800

    [Extensions Toolbar] Fix toolbar animation on mac
    https://codereview.chromium.org/716643002/ introduced a bug on mac because
    it made animation times for the extension toolbar uniform (e.g., when we add or
    remove an extension), and cocoa didn't have a consistent method for watching
    animation. Introduce a notification to simplify it.
    
    TBR=avi@chromium.org (simple cocoa extensions-related changes; reviewed by extensions OWNER)
    
    Review URL: https://codereview.chromium.org/729773003
    
    Cr-Commit-Position: refs/heads/master@{#304330}

--- chrome/browser/ui/cocoa/extensions/browser_actions_container_view.mm
+++ chrome/browser/ui/cocoa/extensions/browser_actions_container_view.mm
@@ -18,6 +18,8 @@ NSString* const kBrowserActionGrippyDragFinishedNotification =
     @"BrowserActionGrippyDragFinishedNotification";
 NSString* const kBrowserActionGrippyWillDragNotification =
     @"BrowserActionGrippyWillDragNotification";
+NSString* const kBrowserActionsContainerWillAnimate =
+    @"BrowserActionsContainerWillAnimate";
 NSString* const kTranslationWithDelta =
     @"TranslationWithDelta";
 
@@ -174,6 +176,10 @@ const CGFloat kMinimumContainerWidth = 10.0;
     [[self animator] setFrame:newFrame];
     [NSAnimationContext endGrouping];
     animationEndFrame_ = newFrame;
+
+    [[NSNotificationCenter defaultCenter]
+        postNotificationName:kBrowserActionsContainerWillAnimate
+                      object:self];
   } else {
     [self setFrame:newFrame];
     [self setNeedsDisplay:YES];
