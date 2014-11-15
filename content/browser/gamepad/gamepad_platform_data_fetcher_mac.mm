c1aa2ed8ff50fa8bbe16eb9d640a712f2161daf1
bajones <bajones@chromium.org>
2014-11-14 16:53:26 -0800

    Exclude gamepads with no axes or buttons on Mac and Windows
    Also fixed a mac bug where disconnecting an Xinput device and connecting a non-Xinput device would assert.
    
    BUG=427233
    
    Review URL: https://codereview.chromium.org/724723004
    
    Cr-Commit-Position: refs/heads/master@{#304321}

--- content/browser/gamepad/gamepad_platform_data_fetcher_mac.mm
+++ content/browser/gamepad/gamepad_platform_data_fetcher_mac.mm
@@ -156,7 +156,7 @@ void GamepadPlatformDataFetcherMac::ValueChangedCallback(void* context,
   InstanceFromContext(context)->ValueChanged(ref);
 }
 
-void GamepadPlatformDataFetcherMac::AddButtonsAndAxes(NSArray* elements,
+bool GamepadPlatformDataFetcherMac::AddButtonsAndAxes(NSArray* elements,
                                                       size_t slot) {
   WebGamepad& pad = data_.items[slot];
   AssociatedData& associated = associated_[slot];
@@ -225,6 +225,8 @@ void GamepadPlatformDataFetcherMac::AddButtonsAndAxes(NSArray* elements,
         break;
     }
   }
+
+  return (pad.axesLength > 0 || pad.buttonsLength > 0);
 }
 
 size_t GamepadPlatformDataFetcherMac::GetEmptySlot() {
@@ -282,6 +284,9 @@ void GamepadPlatformDataFetcherMac::DeviceAdd(IOHIDDeviceRef device) {
   if (slot == WebGamepads::itemsLengthCap)
     return;
 
+  // Clear some state that may have been left behind by previous gamepads
+  memset(&associated_[slot], 0, sizeof(AssociatedData));
+
   NSNumber* vendor_id = CFToNSCast(CFCastStrict<CFNumberRef>(
       IOHIDDeviceGetProperty(device, CFSTR(kIOHIDVendorIDKey))));
   NSNumber* product_id = CFToNSCast(CFCastStrict<CFNumberRef>(
@@ -319,7 +324,9 @@ void GamepadPlatformDataFetcherMac::DeviceAdd(IOHIDDeviceRef device) {
 
   base::ScopedCFTypeRef<CFArrayRef> elements(
       IOHIDDeviceCopyMatchingElements(device, NULL, kIOHIDOptionsTypeNone));
-  AddButtonsAndAxes(CFToNSCast(elements), slot);
+
+  if (!AddButtonsAndAxes(CFToNSCast(elements), slot))
+    return;
 
   associated_[slot].hid.device_ref = device;
   data_.items[slot].connected = true;
