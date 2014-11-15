a02b7dfcb1a88b6609c824061bdd83915e57cc15
cjhopman <cjhopman@chromium.org>
2014-11-14 17:01:42 -0800

    Roll DomDistillerJS and some small changes to update_domdistiller_js.sh
    The update script now automatically creates a git commit with a
    reasonable message.
    
    The update script now fails if any command fails.
    
    Picked up changes:
    b6bb8a8 Print top-level timing information
    7a34f1d Initial header image detection on articles.
    c5e4880 Trim trailing spaces in dom_distiller client
    e7b138d Fix the ordering of extra timing in proto
    2eb1d6d Add a list of extra timings to timinginfo
    
    BUG=427636,431067
    
    Review URL: https://codereview.chromium.org/728023002
    
    Cr-Commit-Position: refs/heads/master@{#304322}

--- third_party/dom_distiller_js/update_domdistiller_js.sh
+++ third_party/dom_distiller_js/update_domdistiller_js.sh
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # installed.
 
 (
+  set -e
+
   dom_distiller_js_path=third_party/dom_distiller_js
   dom_distiller_js_package=$dom_distiller_js_path/package
   readme_chromium=$dom_distiller_js_path/README.chromium
@@ -55,18 +57,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   cp $tmpdir/LICENSE $dom_distiller_js_path/
   sed -i "s/Version: [0-9a-f]*/Version: $new_gitsha/" $readme_chromium
 
-  echo
-  echo
-  echo "---Generated commit message---"
-  echo
-  echo "Picked up changes:"
-  cat $changes
-  echo
-  cat $bugs
+  gen_message () {
+    echo "Roll DomDistillerJS"
+    echo
+    echo "Picked up changes:"
+    cat $changes
+    echo
+    cat $bugs
+  }
+
+  message=$tmpdir/message
+  gen_message > $message
 
   # Run checklicenses.py on the pulled files, but only print the output on
   # failures.
   tools/checklicenses/checklicenses.py $dom_distiller_js_path > $tmpdir/checklicenses.out || cat $tmpdir/checklicenses.out
+
+  git commit -a -F $message
 
   rm -rf $tmpdir
 )
