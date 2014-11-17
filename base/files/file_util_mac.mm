7ba6afe771071ea8cb2e8c8432641381f0c1c2f8
cmumford <cmumford@chromium.org>
2014-11-17 10:58:20 -0800

    base::CopyFile can copy *from* Android's content scheme.
    Some other select file functions can deal with Android's content scheme (i.e.
    content://...), but not CopyFile.
    
    BUG=416424
    
    Review URL: https://codereview.chromium.org/603683005
    
    Cr-Commit-Position: refs/heads/master@{#304446}

--- base/files/file_util_mac.mm
+++ base/files/file_util_mac.mm
@@ -15,15 +15,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_restrictions.h"
 
 namespace base {
-namespace internal {
 
-bool CopyFileUnsafe(const FilePath& from_path, const FilePath& to_path) {
+bool CopyFile(const FilePath& from_path, const FilePath& to_path) {
   ThreadRestrictions::AssertIOAllowed();
+  if (from_path.ReferencesParent() || to_path.ReferencesParent())
+    return false;
   return (copyfile(from_path.value().c_str(),
                    to_path.value().c_str(), NULL, COPYFILE_DATA) == 0);
 }
-
-}  // namespace internal
 
 bool GetTempDir(base::FilePath* path) {
   NSString* tmp = NSTemporaryDirectory();
