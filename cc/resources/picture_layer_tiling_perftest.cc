5d38c790fe830e24e7c8d3d5326877106627f8d6
vmpstr <vmpstr@chromium.org>
2014-11-17 15:40:49 -0800

    cc: Move update tile priorities to PictureLayerTilingSet.
    This patch moves UpdateTilePriorities loop from PictureLayerImpl to
    PictureLayerTilingSet.
    
    This depends on https://codereview.chromium.org/716283003/
    
    BUG=433048
    R=danakj, enne
    
    Review URL: https://codereview.chromium.org/731643002
    
    Cr-Commit-Position: refs/heads/master@{#304520}

--- cc/resources/picture_layer_tiling_perftest.cc
+++ cc/resources/picture_layer_tiling_perftest.cc
@@ -76,7 +76,7 @@ class PictureLayerTilingPerfTest : public testing::Test {
     timer_.Reset();
     do {
       picture_layer_tiling_->ComputeTilePriorityRects(
-          PENDING_TREE, viewport_rect, 1.f, timer_.NumLaps() + 1, Occlusion());
+          viewport_rect, 1.f, timer_.NumLaps() + 1, Occlusion());
       timer_.NextLap();
     } while (!timer_.HasTimeLimitExpired());
 
@@ -102,7 +102,7 @@ class PictureLayerTilingPerfTest : public testing::Test {
     timer_.Reset();
     do {
       picture_layer_tiling_->ComputeTilePriorityRects(
-          PENDING_TREE, viewport_rect, 1.f, timer_.NumLaps() + 1, Occlusion());
+          viewport_rect, 1.f, timer_.NumLaps() + 1, Occlusion());
 
       viewport_rect = gfx::Rect(viewport_rect.x() + xoffsets[offsetIndex],
                                 viewport_rect.y() + yoffsets[offsetIndex],
@@ -130,8 +130,8 @@ class PictureLayerTilingPerfTest : public testing::Test {
     picture_layer_tiling_ =
         PictureLayerTiling::Create(1, bounds, &picture_layer_tiling_client_);
     picture_layer_tiling_client_.set_tree(ACTIVE_TREE);
-    picture_layer_tiling_->ComputeTilePriorityRects(
-        ACTIVE_TREE, viewport, 1.0f, 1.0, Occlusion());
+    picture_layer_tiling_->ComputeTilePriorityRects(viewport, 1.0f, 1.0,
+                                                    Occlusion());
 
     timer_.Reset();
     do {
@@ -155,8 +155,8 @@ class PictureLayerTilingPerfTest : public testing::Test {
     picture_layer_tiling_ =
         PictureLayerTiling::Create(1, bounds, &picture_layer_tiling_client_);
     picture_layer_tiling_client_.set_tree(ACTIVE_TREE);
-    picture_layer_tiling_->ComputeTilePriorityRects(
-        ACTIVE_TREE, viewport, 1.0f, 1.0, Occlusion());
+    picture_layer_tiling_->ComputeTilePriorityRects(viewport, 1.0f, 1.0,
+                                                    Occlusion());
 
     timer_.Reset();
     do {
@@ -185,8 +185,8 @@ class PictureLayerTilingPerfTest : public testing::Test {
     picture_layer_tiling_ =
         PictureLayerTiling::Create(1, bounds, &picture_layer_tiling_client_);
     picture_layer_tiling_client_.set_tree(ACTIVE_TREE);
-    picture_layer_tiling_->ComputeTilePriorityRects(
-        ACTIVE_TREE, viewport, 1.0f, 1.0, Occlusion());
+    picture_layer_tiling_->ComputeTilePriorityRects(viewport, 1.0f, 1.0,
+                                                    Occlusion());
 
     timer_.Reset();
     TreePriority priorities[] = {SAME_PRIORITY_FOR_BOTH_TREES,
@@ -217,8 +217,8 @@ class PictureLayerTilingPerfTest : public testing::Test {
     picture_layer_tiling_ =
         PictureLayerTiling::Create(1, bounds, &picture_layer_tiling_client_);
     picture_layer_tiling_client_.set_tree(ACTIVE_TREE);
-    picture_layer_tiling_->ComputeTilePriorityRects(
-        ACTIVE_TREE, viewport, 1.0f, 1.0, Occlusion());
+    picture_layer_tiling_->ComputeTilePriorityRects(viewport, 1.0f, 1.0,
+                                                    Occlusion());
 
     TreePriority priorities[] = {SAME_PRIORITY_FOR_BOTH_TREES,
                                  SMOOTHNESS_TAKES_PRIORITY,
