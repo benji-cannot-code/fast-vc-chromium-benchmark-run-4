d833a8df69f9e0d3e2276f99bdb69e9ffb684410
jyasskin <jyasskin@chromium.org>
2014-11-17 10:44:07 -0800

    Avoid a string copy in URLRequestResourceBundleJob::GetData().
    This adds a URLRequestSimpleJob::GetStringPieceData method with which a subclass
    can provide its own storage for the result string, instead of having to copy the
    whole thing into the URLRequestSimpleJob.
    
    I also made base::IsStringUTF8 take a StringPiece to avoid a copy there.
    
    BUG=422489
    
    Review URL: https://codereview.chromium.org/725443003
    
    Cr-Commit-Position: refs/heads/master@{#304445}

--- net/url_request/url_request_simple_job.cc
+++ net/url_request/url_request_simple_job.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/compiler_specific.h"
+#include "base/memory/ref_counted_memory.h"
 #include "base/message_loop/message_loop.h"
 #include "base/profiler/scoped_tracker.h"
 #include "net/base/io_buffer.h"
@@ -51,10 +52,29 @@ bool URLRequestSimpleJob::ReadRawData(IOBuffer* buf, int buf_size,
   int remaining = byte_range_.last_byte_position() - data_offset_ + 1;
   if (buf_size > remaining)
     buf_size = remaining;
-  memcpy(buf->data(), data_.data() + data_offset_, buf_size);
+  memcpy(buf->data(), data_->front() + data_offset_, buf_size);
   data_offset_ += buf_size;
   *bytes_read = buf_size;
   return true;
+}
+
+int URLRequestSimpleJob::GetData(std::string* mime_type,
+                                 std::string* charset,
+                                 std::string* data,
+                                 const CompletionCallback& callback) const {
+  NOTREACHED();
+  return ERR_UNEXPECTED;
+}
+
+int URLRequestSimpleJob::GetRefCountedData(
+    std::string* mime_type,
+    std::string* charset,
+    scoped_refptr<base::RefCountedMemory>* data,
+    const CompletionCallback& callback) const {
+  scoped_refptr<base::RefCountedString> str_data(new base::RefCountedString());
+  int result = GetData(mime_type, charset, &str_data->data(), callback);
+  *data = str_data;
+  return result;
 }
 
 void URLRequestSimpleJob::StartAsync() {
@@ -83,11 +103,10 @@ void URLRequestSimpleJob::StartAsync() {
         FROM_HERE_WITH_EXPLICIT_FUNCTION(
             "422489 URLRequestSimpleJob::StartAsync 2"));
 
-    result = GetData(&mime_type_,
-                     &charset_,
-                     &data_,
-                     base::Bind(&URLRequestSimpleJob::OnGetDataCompleted,
-                                weak_factory_.GetWeakPtr()));
+    result =
+        GetRefCountedData(&mime_type_, &charset_, &data_,
+                          base::Bind(&URLRequestSimpleJob::OnGetDataCompleted,
+                                     weak_factory_.GetWeakPtr()));
   }
 
   if (result != ERR_IO_PENDING) {
@@ -108,7 +127,7 @@ void URLRequestSimpleJob::OnGetDataCompleted(int result) {
 
   if (result == OK) {
     // Notify that the headers are complete
-    if (!byte_range_.ComputeBounds(data_.size())) {
+    if (!byte_range_.ComputeBounds(data_->size())) {
       NotifyDone(URLRequestStatus(URLRequestStatus::FAILED,
                  ERR_REQUEST_RANGE_NOT_SATISFIABLE));
       return;
