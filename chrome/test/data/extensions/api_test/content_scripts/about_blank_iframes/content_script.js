9795e44d9ccd652d55049c21ba14717068a9853a
rob <rob@robwu.nl>
2014-11-17 15:56:20 -0800

    Invalidate the previous frame when the window object is cleared.
    When a javascript:-URLs is loaded and returns a non-void value, then
    Blink replaces the previously loaded document with the result, which
    replaces the existing document and view. These events do not trigger
    any document load notifications, which resulted in an invalid frame->
    content script mapping.
    
    This patch resolves this problem by invalidating the previous frame
    when the document is created instead of at provisional load). This
    patch depends on https://codereview.chromium.org/710443002.
    
    BUG=416907
    R=kalman@chromium.org
    
    Review URL: https://codereview.chromium.org/684143002
    
    Cr-Commit-Position: refs/heads/master@{#304522}

--- chrome/test/data/extensions/api_test/content_scripts/about_blank_iframes/content_script.js
+++ chrome/test/data/extensions/api_test/content_scripts/about_blank_iframes/content_script.js
@@ -3,4 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-chrome.extension.sendRequest(window.parent === window ? "parent" : "child");
+// Use onload event to make sure that the messages are first dispatched in
+// frames, and then in the top-level frame. This requires document_start or
+// document_end.
+window.addEventListener('load', function() {
+  chrome.extension.sendRequest(window.parent === window ? 'parent' : 'child');
+});
--- content/public/renderer/render_view_observer.h
+++ content/public/renderer/render_view_observer.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ipc/ipc_listener.h"
 #include "ipc/ipc_sender.h"
 #include "third_party/WebKit/public/platform/WebVector.h"
+#include "v8/include/v8.h"
 
 class GURL;
 
@@ -62,6 +63,7 @@ class CONTENT_EXPORT RenderViewObserver : public IPC::Listener,
                                       const blink::WebURLError& error) {}
   virtual void DidCommitProvisionalLoad(blink::WebLocalFrame* frame,
                                         bool is_new_navigation) {}
+  virtual void DidCreateNewDocument(blink::WebLocalFrame* frame) {}
   virtual void DidClearWindowObject(blink::WebLocalFrame* frame) {}
   virtual void DidCreateDocumentElement(blink::WebLocalFrame* frame) {}
   virtual void FrameCreated(blink::WebLocalFrame* parent,
