009528ce5899fc2856f539eca5605bfc997e4163
falken@chromium.org <falken@chromium.org>
2014-11-17 16:19:07 +0000

    Service Worker: FetchEvent.preventDefault() results in a network error
    Since the default behavior is to fallback to network, preventDefault()
    should cause a network error.
    
    As per spec: https://github.com/slightlyoff/ServiceWorker/issues/51
    (see commit f022c3a6d)
    
    Also add a test for calling FetchEvent.respondWith() with a rejected
    promise; this must also produce a network error.
    
    BUG=432825
    
    Review URL: https://codereview.chromium.org/733983002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185451 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/serviceworker/resources/fetch-event-network-error-worker.js
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Test that multiple fetch handlers do not confuse the implementation.
+self.addEventListener('fetch', function(event) {});
+
+self.addEventListener('fetch', function(event) {
+    var testcase = new URL(event.request.url).search;
+    switch (testcase) {
+    case '?reject':
+      event.respondWith(Promise.reject());
+      break;
+    case '?prevent-default':
+      event.preventDefault();
+      break;
+    case '?prevent-default-and-respond-with':
+      event.preventDefault();
+      break;
+    }
+  });
+
+self.addEventListener('fetch', function(event) {});
+
+self.addEventListener('fetch', function(event) {
+    var testcase = new URL(event.request.url).search;
+    if (testcase == '?prevent-default-and-respond-with')
+      event.respondWith(new Response('responding!'));
+  });
+
+self.addEventListener('fetch', function(event) {});
