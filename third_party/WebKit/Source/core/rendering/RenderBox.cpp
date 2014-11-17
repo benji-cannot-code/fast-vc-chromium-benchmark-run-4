91804d55e7257bb43d4e8e05e25ce9828af5551f
eae@chromium.org <eae@chromium.org>
2014-11-17 23:52:24 +0000

    Simplify RenderBox::topLeftLocationOffset
    Simplify the hot RenderBox::topLeftLocationOffset implementation to
    re-use the topLeftLocation logic and reduce the amount of required
    computation by operating exclusively on the location (instead of the
    rect) and by avoiding an unnecessary LayoutRect allocation.
    
    Bot telemetry data suggests a 1% to 4% performance improvement.
    
    R=chrishtr@chromium.org
    
    Review URL: https://codereview.chromium.org/732703003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185473 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderBox.cpp
+++ third_party/WebKit/Source/core/rendering/RenderBox.cpp
@@ -4324,21 +4324,12 @@ LayoutPoint RenderBox::flipForWritingModeIncludingColumns(const LayoutPoint& poi
 
 LayoutPoint RenderBox::topLeftLocation() const
 {
+    if (!UNLIKELY(document().containsAnyRareWritingMode()))
+        return location();
     RenderBlock* containerBlock = containingBlock();
     if (!containerBlock || containerBlock == this)
         return location();
     return containerBlock->flipForWritingModeForChild(this, location());
-}
-
-LayoutSize RenderBox::topLeftLocationOffset() const
-{
-    RenderBlock* containerBlock = containingBlock();
-    if (!containerBlock || containerBlock == this)
-        return locationOffset();
-
-    LayoutRect rect(frameRect());
-    containerBlock->flipForWritingMode(rect); // FIXME: This is wrong if we are an absolutely positioned object enclosed by a relative-positioned inline.
-    return LayoutSize(rect.x(), rect.y());
 }
 
 bool RenderBox::hasRelativeLogicalHeight() const
