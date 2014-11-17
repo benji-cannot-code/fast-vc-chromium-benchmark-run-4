5e4b18e8bfce9031ecec98d3939e147e76e641ca
Jochen Eisinger <jochen@chromium.org>
2014-11-17 11:07:15 +0100

    Temporarily disable two popup blocker tests
    They test behavior that will change in blink.
    
    BUG=431335
    TBR=mkwst@chromium.org
    
    Review URL: https://codereview.chromium.org/728363002
    
    Cr-Commit-Position: refs/heads/master@{#304399}

--- chrome/browser/ui/blocked_content/popup_blocker_browsertest.cc
+++ chrome/browser/ui/blocked_content/popup_blocker_browsertest.cc
@@ -250,8 +250,9 @@ IN_PROC_BROWSER_TEST_F(PopupBlockerBrowserTest,
       DontCheckTitle);
 }
 
+// Temporary. http://crbug.com/431335
 IN_PROC_BROWSER_TEST_F(PopupBlockerBrowserTest,
-                       PopupBlockedFakeClickOnAnchor) {
+                       DISABLED_PopupBlockedFakeClickOnAnchor) {
 #if defined(OS_WIN) && defined(USE_ASH)
   // Disable this test in Metro+Ash for now (http://crbug.com/262796).
   if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kAshBrowserTests))
@@ -452,7 +453,8 @@ IN_PROC_BROWSER_TEST_F(PopupBlockerBrowserTest, ClosableAfterNavigation) {
   close_observer.Wait();
 }
 
-IN_PROC_BROWSER_TEST_F(PopupBlockerBrowserTest, OpenerSuppressed) {
+// Temporary. http://crbug.com/431335
+IN_PROC_BROWSER_TEST_F(PopupBlockerBrowserTest, DISABLED_OpenerSuppressed) {
   RunCheckTest(browser(),
                "/popup_blocker/popup-openersuppressed.html",
                ExpectTab,
