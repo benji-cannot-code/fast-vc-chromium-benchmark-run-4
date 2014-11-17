fa000e715e602f562039edcc5820773dc1419a71
merkulova <merkulova@chromium.org>
2014-11-17 03:06:50 -0800

    Creating supervised users restricted for regular users if they are supervised.
    Initial CL https://chromereviews.googleplex.com/103087013/
    
    BUG=419009
    
    Review URL: https://codereview.chromium.org/720723002
    
    Cr-Commit-Position: refs/heads/master@{#304407}

--- chrome/browser/chromeos/login/users/chrome_user_manager.h
+++ chrome/browser/chromeos/login/users/chrome_user_manager.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_CHROMEOS_LOGIN_USERS_CHROME_USER_MANAGER_H_
 
 #include "base/basictypes.h"
+#include "components/user_manager/user.h"
 #include "components/user_manager/user_manager_base.h"
 
 namespace base {
@@ -30,6 +31,11 @@ class ChromeUserManager : public user_manager::UserManagerBase {
   // Returns current ChromeUserManager or NULL if instance hasn't been
   // yet initialized.
   static ChromeUserManager* Get();
+
+  // Helper method for sorting out of user list only users that can create
+  // supervised users.
+  static user_manager::UserList GetUsersAllowedAsSupervisedUserManagers(
+      const user_manager::UserList& user_list);
 
   virtual MultiProfileUserController* GetMultiProfileUserController() = 0;
   virtual UserImageManager* GetUserImageManager(const std::string& user_id) = 0;
--- components/user_manager/user_manager.h
+++ components/user_manager/user_manager.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/user_manager/user.h"
 #include "components/user_manager/user_manager_export.h"
+#include "components/user_manager/user_type.h"
 
 namespace chromeos {
 class ScopedUserManagerEnabler;
@@ -114,8 +115,13 @@ class USER_MANAGER_EXPORT UserManager {
   // multi-profile session will still be part of this list as long as they
   // are regular users (i.e. not a public session/supervised etc.).
   // Returns an empty list in case when primary user is not a regular one or
-  // has a policy that prohibids it to be part of multi-profile session.
+  // has a policy that prohibits it to be part of multi-profile session.
   virtual UserList GetUsersAllowedForMultiProfile() const = 0;
+
+  // Returns list of users allowed for supervised user creation.
+  // Returns an empty list in cases when supervised user creation or adding new
+  // users is restricted.
+  virtual UserList GetUsersAllowedForSupervisedUsersCreation() const = 0;
 
   // Returns a list of users who are currently logged in.
   virtual const UserList& GetLoggedInUsers() const = 0;
@@ -235,6 +241,11 @@ class USER_MANAGER_EXPORT UserManager {
   // previously set by a |SaveUserDisplayEmail| call).
   // Otherwise, returns |user_id| itself.
   virtual std::string GetUserDisplayEmail(const std::string& user_id) const = 0;
+
+  // Saves user's type for user |user_id| into local state preferences.
+  // Ignored If there is no such user.
+  virtual void SaveUserType(const std::string& user_id,
+                            const UserType& user_type) = 0;
 
   // Returns true if current user is an owner.
   virtual bool IsCurrentUserOwner() const = 0;
