68bcf8ec44d655b813d6760154aa272f1bef21ff
nednguyen <nednguyen@google.com>
2014-11-17 15:28:59 -0800

    [Telemetry] Move test.DidRunTest call to shared_page_state.TearDown().
    The main motivation is to remove the usage of browser instance in page_runner
    (along with https://codereview.chromium.org/737463002/).
    
    One behavior change to notice is that the original implementation calls
    DidRunTest exactly once, but this patch may make DidRunTest called multiple
    times.
    
    There are 3 perf tests that overrides page_test.DidRunTest:
    dom_perf.DidRunTest: which doesn't touch browser.
    peacekeeper.DidRunTest: which doesn't touch browser.
    page_cycler.DidRunTest: use browser to get the IO metric.
    
    The reason why page test has DidRunTest hook is that it provides the way to
    summarize statistics for a *single browser instance* across many tests. If
    TearDown is called the second time, the browser must be restarted hence makes
    the DidRunTest's computation meaningless. This scenario does create test
    failures, so the dashboard should be adjusted to ignore the summary metrics in
    case this failure happens.
    
    BUG=418278
    
    Review URL: https://codereview.chromium.org/721443005
    
    Cr-Commit-Position: refs/heads/master@{#304515}

--- tools/telemetry/telemetry/page/page_runner.py
+++ tools/telemetry/telemetry/page/page_runner.py
@@ -103,7 +103,7 @@ def _RunPageAndHandleExceptionIfNeeded(test, page_set, expectations,
     ProcessError()
   except (exceptions.TabCrashException, exceptions.BrowserGoneException):
     ProcessError()
-    state.TearDown()
+    state.TearDown(results)
     if test.is_multi_tab_test:
       logging.error('Aborting multi-tab test after browser or tab crashed at '
                     'page %s' % page.url)
@@ -200,7 +200,7 @@ def Run(test, page_set, expectations, finder_options, results):
             # Tear down & restart the state for unhandled exceptions thrown by
             # _RunPageAndHandleExceptionIfNeeded.
             results.AddValue(failure.FailureValue(page, sys.exc_info()))
-            state.TearDown()
+            state.TearDown(results)
             state = shared_page_state.SharedPageState(test, finder_options)
           finally:
             _CheckThermalThrottling(state.platform)
@@ -213,8 +213,7 @@ def Run(test, page_set, expectations, finder_options, results):
           logging.error('Too many failures. Aborting.')
           test.RequestExit()
   finally:
-    test.DidRunTest(state.browser, results)
-    state.TearDown()
+    state.TearDown(results)
 
 
 def _ShuffleAndFilterPageSet(page_set, finder_options):
