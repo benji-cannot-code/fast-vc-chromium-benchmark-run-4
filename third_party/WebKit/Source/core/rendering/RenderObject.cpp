967f7644e948481af00be91d0505bd62055ff1ff
c.shu@samsung.com <c.shu@samsung.com>
2014-11-18 01:41:37 +0000

    Remove unused code.
    Review URL: https://codereview.chromium.org/734203002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185483 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderObject.cpp
+++ third_party/WebKit/Source/core/rendering/RenderObject.cpp
@@ -1175,11 +1175,6 @@ void RenderObject::invalidatePaintRectangle(const LayoutRect& r) const
     invalidatePaintUsingContainer(paintInvalidationContainer, dirtyRect, PaintInvalidationRectangle);
 }
 
-IntRect RenderObject::pixelSnappedAbsoluteClippedOverflowRect() const
-{
-    return pixelSnappedIntRect(absoluteClippedOverflowRect());
-}
-
 void RenderObject::invalidateTreeIfNeeded(const PaintInvalidationState& paintInvalidationState)
 {
     ASSERT(!needsLayout());
