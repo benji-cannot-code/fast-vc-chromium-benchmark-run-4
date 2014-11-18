5562a35e425fc8ecc9b8354e10b5cb335f4eb177
reillyg <reillyg@chromium.org>
2014-11-18 13:42:45 -0800

    Improve the dialog title and text for the USB device picker.
    This change modifies the "call to action" in the USB device picker
    dialog to make it clearer to the user that a) they are being asked to
    select a USB device (or one or more devices) and b) the application
    is requesting this action.
    
    BUG=434467
    
    Review URL: https://codereview.chromium.org/730753004
    
    Cr-Commit-Position: refs/heads/master@{#304672}

--- chrome/browser/ui/views/extensions/device_permissions_dialog_view.cc
+++ chrome/browser/ui/views/extensions/device_permissions_dialog_view.cc
@@ -88,6 +88,7 @@ DevicePermissionsDialogView::DevicePermissionsDialogView(
 
   views::Label* label = new views::Label(prompt_->GetPromptMessage());
   label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
+  label->SetMultiLine(true);
   AddChildView(label);
 
   std::vector<ui::TableColumn> table_columns;
