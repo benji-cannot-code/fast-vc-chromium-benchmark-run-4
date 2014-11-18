66b0c9163f5f37b04907721c37ad524179229751
cjhopman <cjhopman@chromium.org>
2014-11-17 19:39:38 -0800

    Greatly improve (non-android) java support
    This change fixes java_library and java_prebuilt to actually construct a
    classpath based on their dependents. It adds java_binary that is similar
    to java_library but also creates a script in $root_build_dir/bin/ to
    actually run a java jar (it builds in the classpath and main class so
    that it can just be run like a normal executable).
    
    Adds the flags "android" and "has_android_platform" to the java_library
    template. An android_library can depend on a java_library only if the
    java_libary has "android == true". A java_library can depend on an
    android_library only if the java_libary has
    "has_android_platform == true".
    
    This merges several of the paths for compiling java between android and
    non-android implementations into a single one that just takes a flag
    controlling it's behavior (enabling/disabling dex, for example).
    
    Adds a very simple java_binary target. This can be run to ensure it works,
    output is printed, and exit code is propagated.
    
    TBR=brettw
    BUG=359249,383646,417843
    
    Review URL: https://codereview.chromium.org/687633003
    
    Cr-Commit-Position: refs/heads/master@{#304555}

---
+++ build/android/gyp/test/java/org/chromium/helloworld/HelloWorldMain.java
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.helloworld;
+
+public class HelloWorldMain {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.exit(Integer.parseInt(args[0]));
+        }
+        HelloWorldPrinter.print();
+    }
+}
+
