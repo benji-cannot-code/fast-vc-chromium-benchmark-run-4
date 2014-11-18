566e6306cf0f65267d5f5722ba31aae5f8c207c0
loislo@chromium.org <loislo@chromium.org>
2014-11-18 17:20:25 +0000

    [DevTools] rename higilightObjectBySnapshotObjectId into revealObjectBySnapshotObjectId
    and do select for the node instead of highlight.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/736673003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185536 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/profiler/HeapSnapshotGridNodes.js
+++ third_party/WebKit/Source/devtools/front_end/profiler/HeapSnapshotGridNodes.js
@@ -104,6 +104,14 @@ WebInspector.HeapSnapshotGridNode.ChildrenProvider.prototype = {
 
 WebInspector.HeapSnapshotGridNode.prototype = {
     /**
+     * @return {!WebInspector.HeapSnapshotSortableDataGrid}
+     */
+    heapSnapshotDataGrid: function()
+    {
+        return this._dataGrid;
+    },
+
+    /**
      * @return {!WebInspector.HeapSnapshotGridNode.ChildrenProvider}
      */
     createProvider: function()
@@ -1055,9 +1063,9 @@ WebInspector.HeapSnapshotConstructorNode.prototype = {
 
     /**
      * @param {number} snapshotObjectId
-     * @param {function(boolean)} callback
+     * @param {function(?WebInspector.HeapSnapshotGridNode, ?WebInspector.HeapSnapshotGridNode)} callback
      */
-    revealNodeBySnapshotObjectId: function(snapshotObjectId, callback)
+    populateNodeBySnapshotObjectId: function(snapshotObjectId, callback)
     {
         /**
          * @this {WebInspector.HeapSnapshotConstructorNode}
@@ -1075,7 +1083,7 @@ WebInspector.HeapSnapshotConstructorNode.prototype = {
         {
             if (nodePosition === -1) {
                 this.collapse();
-                callback(false);
+                callback(null, null);
             } else {
                 this._populateChildren(nodePosition, null, didPopulateChildren.bind(this, nodePosition));
             }
@@ -1087,12 +1095,7 @@ WebInspector.HeapSnapshotConstructorNode.prototype = {
          */
         function didPopulateChildren(nodePosition)
         {
-            var child = this.childForPosition(nodePosition);
-            if (child) {
-                this._dataGrid.revealTreeNode([this, child]);
-                this._dataGrid.highlightNode(/** @type {!WebInspector.HeapSnapshotGridNode} */ (child));
-            }
-            callback(!!child);
+            callback(this, /** @type {?WebInspector.HeapSnapshotGridNode} */(this.childForPosition(nodePosition)));
         }
 
         this._dataGrid.resetNameFilter();
--- third_party/WebKit/Source/devtools/front_end/profiler/HeapSnapshotView.js
+++ third_party/WebKit/Source/devtools/front_end/profiler/HeapSnapshotView.js
@@ -477,7 +477,7 @@ WebInspector.HeapSnapshotView.prototype = {
     showObject: function(snapshotObjectId, perspectiveName)
     {
         if (snapshotObjectId <= this._profile.maxJSObjectId)
-            this.highlightLiveObject(perspectiveName, snapshotObjectId);
+            this.selectLiveObject(perspectiveName, snapshotObjectId);
         else
             this._parentDataDisplayDelegate.showObject(snapshotObjectId, perspectiveName);
     },
@@ -577,24 +577,18 @@ WebInspector.HeapSnapshotView.prototype = {
 
     searchCanceled: function()
     {
-        if (this._searchResults) {
-            for (var i = 0; i < this._searchResults.length; ++i) {
-                var node = this._searchResults[i].node;
-                delete node._searchMatched;
-                node.refresh();
-            }
-        }
-
         this._currentSearchResultIndex = -1;
         this._searchResults = [];
     },
 
     /**
-     * @param {boolean} found
+     * @param {?WebInspector.HeapSnapshotGridNode} parentNode
+     * @param {?WebInspector.HeapSnapshotGridNode} node
      */
-    _didHighlightById: function(found)
+    _selectRevealedNode: function(parentNode, node)
     {
-        // Do not remove. It needs for heap snapshot search tests.
+        if (node)
+            node.select();
     },
 
     /**
@@ -619,7 +613,7 @@ WebInspector.HeapSnapshotView.prototype = {
         if (query.charAt(0) === "@") {
             var snapshotNodeId = parseInt(query.substring(1), 10);
             if (!isNaN(snapshotNodeId))
-                this._dataGrid.highlightObjectByHeapSnapshotId(String(snapshotNodeId), this._didHighlightById.bind(this));
+                this._dataGrid.revealObjectByHeapSnapshotId(String(snapshotNodeId), this._selectRevealedNode.bind(this));
             return;
         }
 
@@ -912,7 +906,7 @@ WebInspector.HeapSnapshotView.prototype = {
      * @param {string} perspectiveName
      * @param {!HeapProfilerAgent.HeapSnapshotObjectId} snapshotObjectId
      */
-    highlightLiveObject: function(perspectiveName, snapshotObjectId)
+    selectLiveObject: function(perspectiveName, snapshotObjectId)
     {
         this._changePerspectiveAndWait(perspectiveName, didChangePerspective.bind(this));
 
@@ -921,12 +915,12 @@ WebInspector.HeapSnapshotView.prototype = {
          */
         function didChangePerspective()
         {
-            this._dataGrid.highlightObjectByHeapSnapshotId(snapshotObjectId, didHighlightObject);
+            this._dataGrid.revealObjectByHeapSnapshotId(snapshotObjectId, didRevealObject);
         }
 
-        function didHighlightObject(found)
+        function didRevealObject(parentNode, node)
         {
-            if (!found)
+            if (!node)
                 WebInspector.console.error("Cannot find corresponding heap snapshot node");
         }
     },
