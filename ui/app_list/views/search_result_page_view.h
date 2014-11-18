cfadb3bc12bd0d0931e0b3248c6610432e9534a3
calamity <calamity@chromium.org>
2014-11-18 00:10:54 -0800

    Have SearchResultPageView store SearchResultContainerViews.
    This CL makes SearchResultPageView store a vector of SearchResultContainerViews
    rather than managing each type of child view explicitly.
    
    This is in preparation of sorting the search views by score.
    
    BUG=416756
    
    Review URL: https://codereview.chromium.org/730433002
    
    Cr-Commit-Position: refs/heads/master@{#304576}

--- ui/app_list/views/search_result_page_view.h
+++ ui/app_list/views/search_result_page_view.h
@@ -10,15 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "ui/app_list/app_list_export.h"
 #include "ui/app_list/app_list_model.h"
-#include "ui/app_list/views/search_result_container_view.h"
 #include "ui/views/view.h"
 
 namespace app_list {
 
 class AppListMainView;
 class AppListViewDelegate;
-class SearchResultListView;
-class SearchResultTileItemListView;
+class SearchResultContainerView;
 
 // The start page for the experimental app list.
 class APP_LIST_EXPORT SearchResultPageView : public views::View {
@@ -32,8 +30,13 @@ class APP_LIST_EXPORT SearchResultPageView : public views::View {
   void ChildPreferredSizeChanged(views::View* child) override;
 
  private:
-  SearchResultListView* results_view_;  // Owned by views hierarchy.
-  SearchResultTileItemListView* tiles_view_;  // Owned by views hierarchy.
+  void AddSearchResultContainerView(
+      AppListModel::SearchResults* result_model,
+      SearchResultContainerView* result_container);
+
+  // The SearchResultContainerViews that compose the search page. All owned by
+  // the views hierarchy.
+  std::vector<SearchResultContainerView*> result_container_views_;
 
   DISALLOW_COPY_AND_ASSIGN(SearchResultPageView);
 };
