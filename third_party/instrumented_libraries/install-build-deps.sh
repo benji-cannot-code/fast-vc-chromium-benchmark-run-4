2460652e7021b8508010a8a8db6199db74a3ab65
earthdok <earthdok@chromium.org>
2014-11-18 07:27:59 -0800

    Instrumented libraries: enable Goobuntu source repos in install-build-deps.sh.
    Before this change users had to enter this command manually.
    
    BUG=none
    R=glider@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/735743002
    
    Cr-Commit-Position: refs/heads/master@{#304606}

--- third_party/instrumented_libraries/install-build-deps.sh
+++ third_party/instrumented_libraries/install-build-deps.sh
@@ -7,6 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 # Script to install build dependencies of packages which we instrument.
 
+# Enable source repositories in Goobuntu.
+if hash goobuntu-config 2> /dev/null
+then
+  sudo goobuntu-config set include_deb_src true
+fi
+
 # TODO(earthdok): find a way to pull the list from the build config.
 common_packages="\
 atk1.0 \
