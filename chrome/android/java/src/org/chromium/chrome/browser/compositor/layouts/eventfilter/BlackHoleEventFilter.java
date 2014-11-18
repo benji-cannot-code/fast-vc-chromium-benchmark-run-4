de8353125da7af8dbf671447f5667603b0210c93
jaekyun <jaekyun@chromium.org>
2014-11-17 21:29:51 -0800

    Upstream ChromeAnimation and eventfilters
    https://chrome-internal-review.googlesource.com/184299/ is a counter part in downstream.
    
    BUG=428834, 431854
    
    Review URL: https://codereview.chromium.org/727273002
    
    Cr-Commit-Position: refs/heads/master@{#304563}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/eventfilter/BlackHoleEventFilter.java
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.compositor.layouts.eventfilter;
+
+import android.view.MotionEvent;
+
+/**
+ * A {@link BlackHoleEventFilter} eats all the events coming its way with no side effects.
+ */
+public class BlackHoleEventFilter extends EventFilter {
+    /**
+     * Creates a {@link BlackHoleEventFilter}.
+     */
+    public BlackHoleEventFilter(EventFilterHost host) {
+        super(host);
+    }
+
+    @Override
+    public boolean onInterceptTouchEventInternal(MotionEvent e, boolean isKeyboardShowing) {
+        return true;
+    }
+
+    @Override
+    public boolean onTouchEventInternal(MotionEvent e) {
+        return true;
+    }
+}
