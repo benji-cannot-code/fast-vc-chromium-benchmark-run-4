b176b5357055f358207ce5bff6ddb2a6bb3a4591
mathiash <mathiash@opera.com>
2014-11-18 08:01:55 -0800

    Make WebContentsObserver pass more calls to Java.
    BUG=428791
    
    Review URL: https://codereview.chromium.org/686213003
    
    Cr-Commit-Position: refs/heads/master@{#304613}

--- content/public/android/java/src/org/chromium/content/browser/WebContentsObserver.java
+++ content/public/android/java/src/org/chromium/content/browser/WebContentsObserver.java
@@ -23,6 +23,14 @@ public abstract class WebContentsObserver {
         mNativeWebContentsObserverAndroid = nativeInit(webContents);
     }
 
+    /**
+     * Called when the RenderView of the current RenderViewHost is ready, e.g. because we recreated
+     * it after a crash.
+     */
+    @CalledByNative
+    public void renderViewReady() {
+    }
+
     @CalledByNative
     public void renderProcessGone(boolean wasOomProtected) {
     }
@@ -97,6 +105,13 @@ public abstract class WebContentsObserver {
      */
     @CalledByNative
     public void didNavigateAnyFrame(String url, String baseUrl, boolean isReload) {
+    }
+
+    /**
+     * Called once the window.document object of the main frame was created.
+     */
+    @CalledByNative
+    public void documentAvailableInMainFrame() {
     }
 
     /**
