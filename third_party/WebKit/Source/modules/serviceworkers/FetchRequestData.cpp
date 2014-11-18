adc9631f532c9e5584d1d60bdda905978425ae09
horo@chromium.org <horo@chromium.org>
2014-11-18 02:42:03 +0000

    [ServiceWorker] Stop using FetchRequestData::createRestrictedCopy().
    The concept of "restricted copy of request" is removed from the spec.
    https://github.com/whatwg/fetch/commit/a898f9a2941350aa625aa79b24673628ac2b2a8e
    
    This cl make more conformance with the current spec.
    
    BUG=NA
    TEST=http/tests/serviceworker/
    
    Review URL: https://codereview.chromium.org/724673002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185491 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/serviceworkers/FetchRequestData.cpp
+++ third_party/WebKit/Source/modules/serviceworkers/FetchRequestData.cpp
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "FetchRequestData.h"
 
-#include "core/dom/Document.h"
 #include "core/dom/ExecutionContext.h"
 #include "core/fetch/ResourceLoaderOptions.h"
 #include "core/loader/ThreadableLoader.h"
@@ -33,29 +32,6 @@ FetchRequestData* FetchRequestData::create(const WebServiceWorkerRequest& webReq
     request->m_referrer.setURL(webRequest.referrer());
     request->setMode(webRequest.mode());
     request->setCredentials(webRequest.credentialsMode());
-    return request;
-}
-
-FetchRequestData* FetchRequestData::createRestrictedCopy(ExecutionContext* context, PassRefPtr<SecurityOrigin> origin) const
-{
-    // "To make a restricted copy of a request |request|, run these steps:
-    // 1. Let |r| be a new request whose url is |request|'s url, method is
-    // |request|'s method, header list is a copy of |request|'s header list,
-    // body is a tee of |request|'s body, client is entry settings object's
-    // global object, origin is entry settings object's origin, referrer is
-    // |client|, context is |connect|, mode is |request|'s mode, and credentials
-    //  mode is |request|'s credentials mode."
-    FetchRequestData* request = FetchRequestData::create();
-    request->m_url = m_url;
-    request->m_method = m_method;
-    request->m_headerList = m_headerList->createCopy();
-    request->m_blobDataHandle = m_blobDataHandle;
-    request->m_origin = origin;
-    request->m_referrer.setClient();
-    request->m_context = WebURLRequest::RequestContextFetch;
-    request->m_mode = m_mode;
-    request->m_credentials = m_credentials;
-    // "2. Return |r|."
     return request;
 }
 
