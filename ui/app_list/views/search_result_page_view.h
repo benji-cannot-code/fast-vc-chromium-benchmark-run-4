cce0d3e5fb0519b1633689fec7edc2726143c998
calamity <calamity@chromium.org>
2014-11-18 18:27:57 -0800

    Add a selection index to SearchResultPageView.
    This CL adds a selection index to SearchResultPageView which selects
    between its child SearchResultContainerViews.
    
    BUG=416756
    
    Review URL: https://codereview.chromium.org/722113005
    
    Cr-Commit-Position: refs/heads/master@{#304749}

--- ui/app_list/views/search_result_page_view.h
+++ ui/app_list/views/search_result_page_view.h
@@ -30,6 +30,9 @@ class APP_LIST_EXPORT SearchResultPageView : public views::View {
   void ChildPreferredSizeChanged(views::View* child) override;
 
  private:
+  void SetSelectedIndex(int index);
+  bool IsValidSelectionIndex(int index);
+
   void AddSearchResultContainerView(
       AppListModel::SearchResults* result_model,
       SearchResultContainerView* result_container);
@@ -37,6 +40,8 @@ class APP_LIST_EXPORT SearchResultPageView : public views::View {
   // The SearchResultContainerViews that compose the search page. All owned by
   // the views hierarchy.
   std::vector<SearchResultContainerView*> result_container_views_;
+
+  int selected_index_;
 
   DISALLOW_COPY_AND_ASSIGN(SearchResultPageView);
 };
