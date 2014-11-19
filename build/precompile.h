a60fc540be891be8953b2e35a78d421f0d255a9d
davidben <davidben@chromium.org>
2014-11-19 12:34:17 -0800

    Remove wincrypt.h from precompiled headers.
    wincrypt.h and OpenSSL conflict on a symbol, worked around by
    crypto/wincrypt_shim.h. This means that wincrypt.h can't be included
    directly. Remove it from the set of precompiled headers.
    
    BUG=338884
    
    Review URL: https://codereview.chromium.org/720373004
    
    Cr-Commit-Position: refs/heads/master@{#304880}

--- build/precompile.h
+++ build/precompile.h
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <Windows.h>
 #include <dwmapi.h>
 #include <shellapi.h>
-#include <wincrypt.h>  // 4
 #include <wtypes.h>  // 2
 
 // Defines in atlbase.h cause conflicts; if we could figure out how
