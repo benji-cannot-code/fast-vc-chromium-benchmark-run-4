91b2336472090d7c0527622c9d3ed79737ec5b17
estade@chromium.org <estade@chromium.org>
2014-11-19 01:17:18 +0000

    Inform Chrome client of changes to an input's datalist.
    This causes Chrome to update any already-visible datalist dropdowns when options are added or removed.
    
    BUG=428790
    
    Review URL: https://codereview.chromium.org/738473003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185562 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/ChromeClientImpl.h
+++ third_party/WebKit/Source/web/ChromeClientImpl.h
@@ -170,6 +170,7 @@ public:
     virtual void didChangeValueInTextField(HTMLFormControlElement&) override;
     virtual void didEndEditingOnTextField(HTMLInputElement&) override;
     virtual void handleKeyboardEventOnTextField(HTMLInputElement&, KeyboardEvent&) override;
+    virtual void textFieldDataListChanged(HTMLFormControlElement&) override;
 
     virtual void didCancelCompositionOnSelectionChange() override;
     virtual void willSetInputMethodState() override;
