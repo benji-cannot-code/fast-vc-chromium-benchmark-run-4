02fc1e523004234e5d23d9c0b4556fcfcb4eb3ca
mikecase <mikecase@chromium.org>
2014-11-19 12:02:05 -0800

    Add option to push files to device using isolate for instrumentation tests.
    BUG=400499
    
    Review URL: https://codereview.chromium.org/689293002
    
    Cr-Commit-Position: refs/heads/master@{#304870}

--- build/android/pylib/instrumentation/test_options.py
+++ build/android/pylib/instrumentation/test_options.py
@@ -23,4 +23,5 @@ InstrumentationOptions = collections.namedtuple('InstrumentationOptions', [
     'test_apk_jar_path',
     'test_runner',
     'test_support_apk_path',
-    'device_flags'])
+    'device_flags',
+    'isolate_file_path'])
