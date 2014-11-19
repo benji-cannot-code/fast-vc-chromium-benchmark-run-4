3ecddc4e664466b1735fde1a5373ae6a64d427eb
msimonides <msimonides@opera.com>
2014-11-19 00:32:29 -0800

    Don't crash when granting permissions to active tab.
    It seems unlikely for active tab permissions to be granted to a tab that
    is closing but on the other hand there is nothing that would prevent a
    race condition in which the RenderView is destroyed before the
    ExtensionMsg_UpdateTabSpecificPermissions message is processed.
    
    The ExtensionMsg_{Update,Clear}TabSpecificPermissions message is changed
    to a ROUTED message so that it is sent to the specific RenderView directly.
    This removes the need to find the RenderView by tab_id which could fail.
    
    BUG=396232
    
    Review URL: https://codereview.chromium.org/708713003
    
    Cr-Commit-Position: refs/heads/master@{#304784}

--- chrome/browser/extensions/active_tab_permission_granter.cc
+++ chrome/browser/extensions/active_tab_permission_granter.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "content/public/browser/navigation_details.h"
 #include "content/public/browser/navigation_entry.h"
+#include "content/public/browser/render_view_host.h"
 #include "content/public/browser/web_contents.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/common/extension_messages.h"
@@ -66,9 +67,11 @@ void ActiveTabPermissionGranter::GrantIfRequested(const Extension* extension) {
     const content::NavigationEntry* navigation_entry =
         web_contents()->GetController().GetVisibleEntry();
     if (navigation_entry) {
-      Send(new ExtensionMsg_UpdateTabSpecificPermissions(
+      content::RenderViewHost* render_view_host =
+          web_contents()->GetRenderViewHost();
+      render_view_host->Send(new ExtensionMsg_UpdateTabSpecificPermissions(
+          render_view_host->GetRoutingID(),
           navigation_entry->GetURL(),
-          tab_id_,
           extension->id(),
           new_hosts));
       // If more things ever need to know about this, we should consider making
@@ -134,7 +137,10 @@ void ActiveTabPermissionGranter::ClearActiveExtensionsAndNotify() {
     extension_ids.push_back((*it)->id());
   }
 
-  Send(new ExtensionMsg_ClearTabSpecificPermissions(tab_id_, extension_ids));
+  content::RenderViewHost* render_view_host =
+      web_contents()->GetRenderViewHost();
+  render_view_host->Send(new ExtensionMsg_ClearTabSpecificPermissions(
+          render_view_host->GetRoutingID(), extension_ids));
   granted_extensions_.Clear();
 }
 
