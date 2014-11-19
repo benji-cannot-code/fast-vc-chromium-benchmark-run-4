807998f7c51e57c307d188425f55aa7fc44a44c4
enne <enne@chromium.org>
2014-11-18 16:10:50 -0800

    cc: Test one copy in mask unit tests
    R=reveman@chromium.org
    
    Review URL: https://codereview.chromium.org/739003002
    
    Cr-Commit-Position: refs/heads/master@{#304708}

--- cc/test/test_in_process_context_provider.cc
+++ cc/test/test_in_process_context_provider.cc
@@ -130,6 +130,7 @@ TestInProcessContextProvider::ContextCapabilities() {
   ContextProvider::Capabilities capabilities;
   capabilities.gpu.image = true;
   capabilities.gpu.texture_rectangle = true;
+  capabilities.gpu.sync_query = true;
 
   return capabilities;
 }
