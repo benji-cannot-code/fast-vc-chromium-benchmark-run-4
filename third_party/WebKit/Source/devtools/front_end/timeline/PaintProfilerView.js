b4ae2930e106fc723687fd95e6d43551336bcd68
caseq@chromium.org <caseq@chromium.org>
2014-11-19 09:10:19 +0000

    Paint Profiler: show correct pie chart immediately when profile is available
    We used to update pie chart only upon window resize, so the one initially
    displayed was empty.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/732383002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185578 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/timeline/PaintProfilerView.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/PaintProfilerView.js
@@ -83,6 +83,7 @@ WebInspector.PaintProfilerView.prototype = {
 
         if (!this._snapshot) {
             this._update();
+            this._pieChart.setTotal(0);
             return;
         }
         this._progressBanner.classList.remove("hidden");
@@ -97,6 +98,7 @@ WebInspector.PaintProfilerView.prototype = {
             this._progressBanner.classList.add("hidden");
             this._profiles = profiles;
             this._update();
+            this._updatePieChart();
         }
     },
 
@@ -175,8 +177,14 @@ WebInspector.PaintProfilerView.prototype = {
     _onWindowChanged: function()
     {
         this.dispatchEventToListeners(WebInspector.PaintProfilerView.Events.WindowChanged);
+        this._updatePieChart();
+        if (this._updateImageTimer)
+            return;
+        this._updateImageTimer = setTimeout(this._updateImage.bind(this), 100);
+    },
 
-        // Update pie chart
+    _updatePieChart: function()
+    {
         var window = this.windowBoundaries();
         var totalTime = 0;
         var timeByCategory = {};
@@ -193,10 +201,6 @@ WebInspector.PaintProfilerView.prototype = {
         this._pieChart.setTotal(totalTime / this._profiles.length);
         for (var color in timeByCategory)
           this._pieChart.addSlice(timeByCategory[color] / this._profiles.length, color);
-
-        if (this._updateImageTimer)
-            return;
-        this._updateImageTimer = setTimeout(this._updateImage.bind(this), 100);
     },
 
     /**
