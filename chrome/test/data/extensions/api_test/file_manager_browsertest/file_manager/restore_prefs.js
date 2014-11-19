b041d197aa334bcf6a82f00b5b849a17aa2d1e89
fukino <fukino@chromium.org>
2014-11-19 01:24:13 -0800

    Make folders always precede files in the file list.
    BUG=433165
    TEST=run browser_tests --gtest_filter=*FileManager*
    
    Review URL: https://codereview.chromium.org/729163002
    
    Cr-Commit-Position: refs/heads/master@{#304790}

--- chrome/test/data/extensions/api_test/file_manager_browsertest/file_manager/restore_prefs.js
+++ chrome/test/data/extensions/api_test/file_manager_browsertest/file_manager/restore_prefs.js
@@ -11,8 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 testcase.restoreSortColumn = function() {
   var appId;
   var EXPECTED_FILES = TestEntryInfo.getExpectedRows([
-    ENTRIES.world,
     ENTRIES.photos,
+    ENTRIES.world,
     ENTRIES.desktop,
     ENTRIES.hello,
     ENTRIES.beautiful
