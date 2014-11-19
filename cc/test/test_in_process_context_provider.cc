58dcae8e43358dfb52ef2bbd52c322ad0bc67eb4
machenbach <machenbach@chromium.org>
2014-11-19 00:40:57 -0800

    Revert of cc: Test one copy in mask unit tests (patchset #1 id:1 of https://codereview.chromium.org/739003002/)
    Reason for revert:
    [Sheriff] Speculative revert for making PixelResourceTest/LayerTreeHostMasksPixelTest.MaskOfClippedLayer/2 flaky.
    
    Starting here:
    http://build.chromium.org/p/chromium.win/builders/Win%207%20Tests%20x64%20%281%29/builds/18677
    
    Original issue's description:
    > cc: Test one copy in mask unit tests
    >
    > R=reveman@chromium.org
    >
    > Committed: https://crrev.com/1f1b197adbd5fdf449229646b26835f0df71e76f
    > Cr-Commit-Position: refs/heads/master@{#304708}
    
    TBR=reveman@chromium.org,enne@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/735963002
    
    Cr-Commit-Position: refs/heads/master@{#304786}

--- cc/test/test_in_process_context_provider.cc
+++ cc/test/test_in_process_context_provider.cc
@@ -130,7 +130,6 @@ TestInProcessContextProvider::ContextCapabilities() {
   ContextProvider::Capabilities capabilities;
   capabilities.gpu.image = true;
   capabilities.gpu.texture_rectangle = true;
-  capabilities.gpu.sync_query = true;
 
   return capabilities;
 }
