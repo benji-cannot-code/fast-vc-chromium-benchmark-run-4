0f566b36f63bb1d22cb9622c52085cfa4987fc83
timvolodine <timvolodine@chromium.org>
2014-11-19 12:19:42 -0800

    Android: add UMA histogram for Device Orientation with backup sensors.
    Since crrev.com/661413003 Device Orientation API on android uses backup
    sensors when ROTATION_VECTOR is not available. Currently UMA is logged
    based on ROTATION_VECTOR only. This patch replaces the old histogram to
    reflect the new option of using accelerometer + magnetic field sensors.
    
    BUG=347507
    
    Review URL: https://codereview.chromium.org/696113003
    
    Cr-Commit-Position: refs/heads/master@{#304874}

--- content/browser/device_sensors/sensor_manager_android.cc
+++ content/browser/device_sensors/sensor_manager_android.cc
@@ -17,8 +17,17 @@ using base::android::AttachCurrentThread;
 
 namespace {
 
-static void updateRotationVectorHistogram(bool value) {
-  UMA_HISTOGRAM_BOOLEAN("InertialSensor.RotationVectorAndroidAvailable", value);
+enum OrientationSensorType {
+  NOT_AVAILABLE = 0,
+  ROTATION_VECTOR = 1,
+  ACCELEROMETER_MAGNETIC = 2,
+  ORIENTATION_SENSOR_MAX = 3,
+};
+
+static void UpdateDeviceOrientationHistogram(OrientationSensorType type) {
+  UMA_HISTOGRAM_ENUMERATION("InertialSensor.DeviceOrientationSensorAndroid",
+                            type,
+                            ORIENTATION_SENSOR_MAX);
 }
 
 }
@@ -69,7 +78,8 @@ void SensorManagerAndroid::GotOrientation(
 
   if (!is_orientation_buffer_ready_) {
     SetOrientationBufferReadyStatus(true);
-    updateRotationVectorHistogram(!is_using_backup_sensors_for_orientation_);
+    UpdateDeviceOrientationHistogram(is_using_backup_sensors_for_orientation_
+        ? ACCELEROMETER_MAGNETIC : ROTATION_VECTOR);
   }
 }
 
@@ -313,11 +323,12 @@ bool SensorManagerAndroid::StartFetchingDeviceOrientationData(
     SetOrientationBufferReadyStatus(!success);
   }
 
-  if (!success)
-    updateRotationVectorHistogram(false);
-  else
+  if (!success) {
+    UpdateDeviceOrientationHistogram(NOT_AVAILABLE);
+  } else {
     is_using_backup_sensors_for_orientation_ =
         isUsingBackupSensorsForOrientation();
+  }
 
   return success;
 }
