f33f7ceee2a8aeceae8783ed795bc928bbc28bda
gunsch <gunsch@chromium.org>
2014-11-19 15:49:21 -0800

    Chromecast buildfix: webui_cast.h was removed.
    R=lcwu@chromium.org,byungchul@chromium.org
    BUG=None
    
    Review URL: https://codereview.chromium.org/725243005
    
    Cr-Commit-Position: refs/heads/master@{#304929}

--- chromecast/browser/cast_browser_main_parts.cc
+++ chromecast/browser/cast_browser_main_parts.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromecast/browser/metrics/cast_metrics_service_client.h"
 #include "chromecast/browser/service/cast_service.h"
 #include "chromecast/browser/url_request_context_factory.h"
-#include "chromecast/browser/webui/webui_cast.h"
 #include "chromecast/common/chromecast_config.h"
 #include "chromecast/common/platform_client_auth.h"
 #include "chromecast/net/network_change_notifier_cast.h"
