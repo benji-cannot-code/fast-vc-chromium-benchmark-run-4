8491356b7f5e7d6e7df53ad9f79e17ddcab040ef
rhodovan.u-szeged@partner.samsung.com <rhodovan.u-szeged@partner.samsung.com>
2014-11-20 10:21:39 +0000

    Fix stroke color definition of canvas context.
    The stroke's color of a canvas can be given with the combination
    of a CSS color and an optional alpha value. The color part can be
    defined with both basic or extended color keywords. Using a basic keyword
    with an alpha value works fine but it does not work with extended colors.
    The reason is that blink::CanvasStyle::createFromStringWithOverrideAlpha
    is not prepared for getting a system color. This is basically due to
    a missing case branch. The patch supplies this missing branch.
    
    R=rob.buis@samsung.org,schenney@chromium.org
    BUG=434812
    
    Review URL: https://codereview.chromium.org/725913003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185656 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/canvas/CanvasStyle.cpp
+++ third_party/WebKit/Source/core/html/canvas/CanvasStyle.cpp
@@ -149,6 +149,7 @@ PassRefPtrWillBeRawPtr<CanvasStyle> CanvasStyle::createFromStringWithOverrideAlp
     ColorParseResult parseResult = parseColor(rgba, color);
     switch (parseResult) {
     case ParsedRGBA:
+    case ParsedSystemColor:
         return adoptRefWillBeNoop(new CanvasStyle(colorWithOverrideAlpha(rgba, alpha)));
     case ParsedCurrentColor:
         return adoptRefWillBeNoop(new CanvasStyle(CurrentColorWithOverrideAlpha, alpha));
