f6a24ef691cfa6be9778f30a125746b40fd25fd9
cjhopman <cjhopman@chromium.org>
2014-11-19 16:30:06 -0800

    Make ProtoDatabase properly handle failed Init
    When Init fails, further Save/Load calls should just fail (and not
    crash). This makes the underlying LevelDB properly handle the Init failed case
    and adds tests for that.
    
    Adds LevelDB::InitWithOptions to make it easy to have the Init fail in tests.
    
    Review URL: https://codereview.chromium.org/735823004
    
    Cr-Commit-Position: refs/heads/master@{#304944}

--- components/leveldb_proto/leveldb_database.h
+++ components/leveldb_proto/leveldb_database.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace leveldb {
 class DB;
+struct Options;
 }  // namespace leveldb
 
 namespace leveldb_proto {
@@ -28,6 +29,8 @@ class LevelDB {
   LevelDB();
   virtual ~LevelDB();
 
+  virtual bool InitWithOptions(const base::FilePath& database_dir,
+                               const leveldb::Options& options);
   virtual bool Init(const base::FilePath& database_dir);
   virtual bool Save(const base::StringPairs& pairs_to_save,
                     const std::vector<std::string>& keys_to_remove);
--- components/leveldb_proto/proto_database_impl_unittest.cc
+++ components/leveldb_proto/proto_database_impl_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/leveldb_proto/testing/proto/test.pb.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/leveldatabase/src/include/leveldb/options.h"
 
 using base::MessageLoop;
 using base::ScopedTempDir;
@@ -393,6 +394,23 @@ TEST(ProtoDatabaseImplLevelDBTest, TestDBSaveAndLoad) {
 
 TEST(ProtoDatabaseImplLevelDBTest, TestDBCloseAndReopen) {
   TestLevelDBSaveAndLoad(true);
+}
+
+TEST(ProtoDatabaseImplLevelDBTest, TestDBInitFail) {
+  ScopedTempDir temp_dir;
+  ASSERT_TRUE(temp_dir.CreateUniqueTempDir());
+
+  leveldb::Options options;
+  options.create_if_missing = false;
+  scoped_ptr<LevelDB> db(new LevelDB());
+
+  KeyValueVector save_entries;
+  std::vector<std::string> load_entries;
+  KeyVector remove_keys;
+
+  EXPECT_FALSE(db->InitWithOptions(temp_dir.path(), options));
+  EXPECT_FALSE(db->Load(&load_entries));
+  EXPECT_FALSE(db->Save(save_entries, remove_keys));
 }
 
 }  // namespace leveldb_proto
