0d04fd86c1d7afdba7c6777668e7f86413eb5c34
miletus <miletus@chromium.org>
2014-11-20 12:17:27 -0800

    Format profiler/trace files cloud url
    This CL adds ProfilerValue which holds the cloud url for the profiler files
    and can be added using results.AddValue(ProfilerValue()).
    
    When doing format output, ProfilerValue and TraceValue will put their
    cloud url in the key named "cloud_url".
    
    BUG=431904
    TEST= see #8 at crbug.com/431904
    
    Review URL: https://codereview.chromium.org/731233003
    
    Cr-Commit-Position: refs/heads/master@{#305071}

--- tools/telemetry/telemetry/value/trace_unittest.py
+++ tools/telemetry/telemetry/value/trace_unittest.py
@@ -8,6 +8,7 @@ import unittest
 
 from telemetry.page import page_set
 from telemetry.timeline import tracing_timeline_data
+from telemetry.unittest_util import system_stub
 from telemetry.value import trace
 
 class TestBase(unittest.TestCase):
@@ -17,6 +18,13 @@ class TestBase(unittest.TestCase):
     self.page_set.AddPageWithDefaultRunNavigate("http://www.baz.com/")
     self.page_set.AddPageWithDefaultRunNavigate("http://www.foo.com/")
 
+    self._cloud_storage_stub = system_stub.Override(trace, ['cloud_storage'])
+
+  def tearDown(self):
+    if self._cloud_storage_stub:
+      self._cloud_storage_stub.Restore()
+      self._cloud_storage_stub = None
+
   @property
   def pages(self):
     return self.page_set.pages
@@ -25,8 +33,11 @@ class ValueTest(TestBase):
   def testAsDict(self):
     v = trace.TraceValue(
         None, tracing_timeline_data.TracingTimelineData({'test' : 1}))
-    fh_id = v.GetAssociatedFileHandle().id
-
+    fh = v.GetAssociatedFileHandle()
+    trace.cloud_storage.SetCalculatedHashesForTesting(
+        {fh.GetAbsPath(): 123, })
+    bucket = trace.cloud_storage.PUBLIC_BUCKET
+    cloud_url = v.UploadToCloud(bucket)
     d = v.AsDict()
-
-    self.assertEqual(d['file_id'], fh_id)
+    self.assertEqual(d['file_id'], fh.id)
+    self.assertEqual(d['cloud_url'], cloud_url)
