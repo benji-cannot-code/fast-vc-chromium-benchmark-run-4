240318460839a6cd25cf709ceeed7ca9f8ae568c
tkent@chromium.org <tkent@chromium.org>
2014-11-20 06:16:44 +0000

    Oilpan: Decrease frequency of conservative GC.
    Oilpan GC can waste time to trace unused DOM wrappers referring to
    on-heap objects.  In order to avoid it, this CL sets higher thresholds
    to trigger conservative GC if a V8 GC didn't happen.
    
    This CL doesn't change the behavior of precise GC.
    
    This CL improves LargeDistributionWithoutLayout test:
        30% regression -> 14% regression
        59 GCs -> 19 GCs
    
    BUG=420515
    
    Review URL: https://codereview.chromium.org/734623002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185646 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/heap/ThreadState.h
+++ third_party/WebKit/Source/platform/heap/ThreadState.h
@@ -405,6 +405,7 @@ public:
     bool gcRequested();
     void setGCRequested();
     void clearGCRequested();
+    void didV8GC();
 
     // shouldGC and shouldForceConservativeGC implement the heuristics
     // that are used to determine when to collect garbage. If
@@ -771,6 +772,7 @@ private:
     bool m_atSafePoint;
     Vector<Interruptor*> m_interruptors;
     bool m_gcRequested;
+    bool m_didV8GCAfterLastGC;
     bool m_forcePreciseGCForTesting;
     volatile int m_sweepRequested;
     bool m_sweepInProgress;
