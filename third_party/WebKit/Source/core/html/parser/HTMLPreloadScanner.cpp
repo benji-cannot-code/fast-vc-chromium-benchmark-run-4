0cd8f39cf6304f556052c9525fc0c2aa53217f50
kouhei@chromium.org <kouhei@chromium.org>
2014-11-20 03:01:06 +0000

    Disable preload on <meta> content security policy enforced documents.
    Before this CL, HTMLResourcePreloader invoked preload requests without
    taking CSP rules from <meta> tags into account.
    
    This CL workarounds the issue by disabling preloads when scanner finds
    <meta http-equiv='Content-Security-Policy'> tags.
    
    LayoutTests/http/tests/security/contentSecurityPolicy/1.1/scriptnonce-redirect.html 
    - <script> tag reordered to force preloader scan URLs.
    
    BUG=434230
    
    Review URL: https://codereview.chromium.org/737663002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185639 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/parser/HTMLPreloadScanner.cpp
+++ third_party/WebKit/Source/core/html/parser/HTMLPreloadScanner.cpp
@@ -375,6 +375,7 @@ TokenPreloadScanner::TokenPreloadScanner(const KURL& documentURL, PassRefPtr<Med
     , m_inStyle(false)
     , m_inPicture(false)
     , m_isAppCacheEnabled(false)
+    , m_isCSPEnabled(false)
     , m_templateCount(0)
     , m_mediaValues(mediaValues)
 {
@@ -387,7 +388,7 @@ TokenPreloadScanner::~TokenPreloadScanner()
 TokenPreloadScannerCheckpoint TokenPreloadScanner::createCheckpoint()
 {
     TokenPreloadScannerCheckpoint checkpoint = m_checkpoints.size();
-    m_checkpoints.append(Checkpoint(m_predictedBaseElementURL, m_inStyle, m_isAppCacheEnabled, m_templateCount));
+    m_checkpoints.append(Checkpoint(m_predictedBaseElementURL, m_inStyle, m_isAppCacheEnabled, m_isCSPEnabled, m_templateCount));
     return checkpoint;
 }
 
@@ -398,6 +399,7 @@ void TokenPreloadScanner::rewindTo(TokenPreloadScannerCheckpoint checkpointIndex
     m_predictedBaseElementURL = checkpoint.predictedBaseElementURL;
     m_inStyle = checkpoint.inStyle;
     m_isAppCacheEnabled = checkpoint.isAppCacheEnabled;
+    m_isCSPEnabled = checkpoint.isCSPEnabled;
     m_templateCount = checkpoint.templateCount;
     m_cssScanner.reset();
     m_checkpoints.clear();
@@ -418,6 +420,10 @@ void TokenPreloadScanner::scanCommon(const Token& token, const SegmentedString& 
 {
     // Disable preload for documents with AppCache.
     if (m_isAppCacheEnabled)
+        return;
+
+    // http://crbug.com/434230 Disable preload for documents with CSP <meta> tags
+    if (m_isCSPEnabled)
         return;
 
     switch (token.type()) {
@@ -466,6 +472,13 @@ void TokenPreloadScanner::scanCommon(const Token& token, const SegmentedString& 
         if (match(tagImpl, htmlTag) && token.getAttributeItem(manifestAttr)) {
             m_isAppCacheEnabled = true;
             return;
+        }
+        if (match(tagImpl, metaTag)) {
+            const typename Token::Attribute* equivAttribute = token.getAttributeItem(http_equivAttr);
+            if (equivAttribute && equalIgnoringCase(String(equivAttribute->value), "content-security-policy")) {
+                m_isCSPEnabled = true;
+                return;
+            }
         }
 
         if (RuntimeEnabledFeatures::pictureEnabled() && (match(tagImpl, pictureTag))) {
