705c70e486e4e631665dcf46aeb1e7a9b066bda6
chrishtr@chromium.org <chrishtr@chromium.org>
2014-11-20 06:23:59 +0000

    [Slimming Paint] Implement a ClipDisplayItem for clips generated in BoxPainter.
    The setup code for this more or less duplicates what is already present in LayerPainter.
    
    Note however that the order of the rounded rect and non-rounded rect clips is reversed
    in the old box painter code, which was changed in this CL. This should not matter.
    
    BUG=434560
    
    Review URL: https://codereview.chromium.org/736023004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185647 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/paint/ClipRecorder.h
+++ third_party/WebKit/Source/core/paint/ClipRecorder.h
@@ -37,7 +37,6 @@ class EndClipDisplayItem : public DisplayItem {
 public:
     EndClipDisplayItem(const RenderLayerModelObject* renderer) : DisplayItem(renderer, EndClip) { }
 
-private:
     virtual void replay(GraphicsContext*) override;
 };
 
