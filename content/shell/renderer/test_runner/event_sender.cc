17ad9e58992c0f8683b6c03043eaf8ddb7df4b6f
bokan <bokan@chromium.org>
2014-11-20 14:03:49 -0800

    Don't set page scale limits inside EventSender::setPageScaleFactor.
    This prevents proper testing of pinch-zoom as the limits change on every zoom
    and limits are used to calculate the main frame size in virtual-viewport pinch
    to zoom. This is part of a 3-sided patch:
    
    1) Add a setPageScaleFactorLimits to EventSender
    2) Change existing layout tests that use setPageScaleFactor to use this method
       to set up the limits.
    3) This patch removes the setPageScaleLimits call inside setPageScaleFactor.
    
    BUG=435151
    
    Review URL: https://codereview.chromium.org/738463003
    
    Cr-Commit-Position: refs/heads/master@{#305084}

--- content/shell/renderer/test_runner/event_sender.cc
+++ content/shell/renderer/test_runner/event_sender.cc
@@ -1487,7 +1487,6 @@ void EventSender::SetPageZoomFactor(double zoom_factor) {
 }
 
 void EventSender::SetPageScaleFactor(float scale_factor, int x, int y) {
-  view_->setPageScaleFactorLimits(scale_factor, scale_factor);
   view_->setPageScaleFactor(scale_factor, WebPoint(x, y));
 }
 
