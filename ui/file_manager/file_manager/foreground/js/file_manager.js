961d93b742a8b33cea78f470fa5241fc8df2e649
hirono <hirono@chromium.org>
2014-11-20 10:38:54 -0800

    Refoctoring FileSelection and FileSelectionHandler.
     * Add JSDoc.
     * Add EventType enum.
     * Create completeInit class to wait for initialization completion.
     * Add FileSelectionHandler#isAvailable.
    
    BUG=267281
    TEST=None
    
    Review URL: https://codereview.chromium.org/736663002
    
    Cr-Commit-Position: refs/heads/master@{#305044}

--- ui/file_manager/file_manager/foreground/js/file_manager.js
+++ ui/file_manager/file_manager/foreground/js/file_manager.js
@@ -386,6 +386,12 @@ FileManager.prototype = /** @struct */ {
     return this.historyLoader_;
   },
   /**
+   * @return {MetadataCache}
+   */
+  get metadataCache() {
+    return this.metadataCache_;
+  },
+  /**
    * @return {FileManagerUI}
    */
   get ui() {
@@ -1806,7 +1812,7 @@ var BOTTOM_MARGIN_FOR_PREVIEW_PANEL_PX = 52;
 
     var entry = selection.entries[0];
     if (entry.isDirectory) {
-      this.onDirectoryAction_(entry);
+      this.onDirectoryAction_(/** @type {!DirectoryEntry} */(entry));
     } else {
       this.dispatchSelectionAction_();
     }
@@ -2191,7 +2197,8 @@ var BOTTOM_MARGIN_FOR_PREVIEW_PANEL_PX = 52;
           // directory.
           if (!item.hasAttribute('renaming')) {
             event.preventDefault();
-            this.onDirectoryAction_(selection.entries[0]);
+            this.onDirectoryAction_(
+                /** @type {DirectoryEntry} */(selection.entries[0]));
           }
         } else if (this.dispatchSelectionAction_()) {
           event.preventDefault();
