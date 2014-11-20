00ea4e76fa2b7eebecb5d9150c436a04096f6a38
eranm <eranm@chromium.org>
2014-11-20 14:37:59 -0800

    Certificate Transparency: Remove an unnecessary boolean.
    BUG=
    
    Review URL: https://codereview.chromium.org/744763002
    
    Cr-Commit-Position: refs/heads/master@{#305092}

--- chrome/browser/net/packed_ct_ev_whitelist.cc
+++ chrome/browser/net/packed_ct_ev_whitelist.cc
@@ -108,14 +108,11 @@ bool PackedEVCertsWhitelist::UncompressEVWhitelist(
 }
 
 PackedEVCertsWhitelist::PackedEVCertsWhitelist(
-    const std::string& compressed_whitelist)
-    : is_whitelist_valid_(false) {
+    const std::string& compressed_whitelist) {
   if (!UncompressEVWhitelist(compressed_whitelist, &whitelist_)) {
     whitelist_.clear();
     return;
   }
-
-  is_whitelist_valid_ = true;
 }
 
 PackedEVCertsWhitelist::~PackedEVCertsWhitelist() {
@@ -135,5 +132,5 @@ bool PackedEVCertsWhitelist::ContainsCertificateHash(
 }
 
 bool PackedEVCertsWhitelist::IsValid() const {
-  return is_whitelist_valid_;
+  return whitelist_.size() > 0;
 }
