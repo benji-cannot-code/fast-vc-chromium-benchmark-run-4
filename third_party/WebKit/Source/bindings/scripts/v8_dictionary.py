b0488a65cbff182f314a6d7bcdae0fecc3aee33c
jinho.bang@samsung.com <jinho.bang@samsung.com>
2014-11-20 14:14:43 +0000

    Support for [Clamp] and [EnforceRange] to IDL dictionary.
    We should be able to use [Clamp][1] and [EnforceRange][2] on dictionary
    as well as interface or method. They will be used in some dictionaries such as
    Geolocation API[3], Web Crypto API[4] and so on.
    
    [1] http://www.w3.org/TR/WebIDL/#Clamp
    [2] http://www.w3.org/TR/WebIDL/#EnforceRange
    [3] http://dev.w3.org/geo/api/spec-source.html#position-options
    [4] http://www.w3.org/TR/WebCryptoAPI/#dfn-RsaKeyGenParams
    
    
    BUG=321462
    
    Review URL: https://codereview.chromium.org/724733002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185667 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/scripts/v8_dictionary.py
+++ third_party/WebKit/Source/bindings/scripts/v8_dictionary.py
@@ -21,8 +21,6 @@ DICTIONARY_H_INCLUDES = frozenset([
 
 DICTIONARY_CPP_INCLUDES = frozenset([
     'bindings/core/v8/ExceptionState.h',
-    # FIXME: Remove this, http://crbug.com/321462
-    'bindings/core/v8/Dictionary.h',
 ])
 
 
@@ -100,6 +98,9 @@ def member_context(member):
         'name': member.name,
         'setter_name': setter_name_for_dictionary_member(member),
         'v8_default_value': v8_default_value,
+        'v8_value_to_local_cpp_value': idl_type.v8_value_to_local_cpp_value(
+            member.extended_attributes, member.name + 'Value',
+            member.name, isolate='isolate'),
     }
 
 
--- third_party/WebKit/Source/bindings/tests/results/core/V8TestDictionaryDerived.cpp
+++ third_party/WebKit/Source/bindings/tests/results/core/V8TestDictionaryDerived.cpp
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "V8TestDictionaryDerivedImplementedAs.h"
 
-#include "bindings/core/v8/Dictionary.h"
 #include "bindings/core/v8/ExceptionState.h"
 #include "bindings/core/v8/V8TestDictionary.h"
 
@@ -27,22 +26,20 @@ void V8TestDictionaryDerivedImplementedAs::toImpl(v8::Isolate* isolate, v8::Hand
     if (exceptionState.hadException())
         return;
 
-    // FIXME: Do not use Dictionary and DictionaryHelper
-    // https://crbug.com/321462
-    Dictionary dictionary(v8Value, isolate);
-    // FIXME: Remove this v8::TryCatch once the code is switched from
-    // Dictionary/DictionaryHelper to something that uses ExceptionState.
+    v8::Local<v8::Object> v8Object = v8Value->ToObject(isolate);
     v8::TryCatch block;
-    String derivedStringMember;
-    if (DictionaryHelper::getWithUndefinedOrNullCheck(dictionary, "derivedStringMember", derivedStringMember)) {
+    v8::Local<v8::Value> derivedStringMemberValue = v8Object->Get(v8String(isolate, "derivedStringMember"));
+    if (!derivedStringMemberValue.IsEmpty() && !isUndefinedOrNull(derivedStringMemberValue)) {
+        TOSTRING_VOID(V8StringResource<>, derivedStringMember, derivedStringMemberValue);
         impl.setDerivedStringMember(derivedStringMember);
     } else if (block.HasCaught()) {
         exceptionState.rethrowV8Exception(block.Exception());
         return;
     }
 
-    String derivedStringMemberWithDefault;
-    if (DictionaryHelper::getWithUndefinedOrNullCheck(dictionary, "derivedStringMemberWithDefault", derivedStringMemberWithDefault)) {
+    v8::Local<v8::Value> derivedStringMemberWithDefaultValue = v8Object->Get(v8String(isolate, "derivedStringMemberWithDefault"));
+    if (!derivedStringMemberWithDefaultValue.IsEmpty() && !isUndefinedOrNull(derivedStringMemberWithDefaultValue)) {
+        TOSTRING_VOID(V8StringResource<>, derivedStringMemberWithDefault, derivedStringMemberWithDefaultValue);
         impl.setDerivedStringMemberWithDefault(derivedStringMemberWithDefault);
     } else if (block.HasCaught()) {
         exceptionState.rethrowV8Exception(block.Exception());
