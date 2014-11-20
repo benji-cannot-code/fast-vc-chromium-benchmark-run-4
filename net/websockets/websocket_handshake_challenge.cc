e4e886b93c296bcfee3737988215645c686ac9fc
ricea <ricea@chromium.org>
2014-11-20 01:57:07 -0800

    Rename websocket_handshake_handler.* to websocket_handshake_challenge.*
    Simplify the calling convention of ComputeSecWebSocketAccept to just return a string, and add a test for it.
    
    BUG=
    TEST=net_unittests
    
    Review URL: https://codereview.chromium.org/683033003
    
    Cr-Commit-Position: refs/heads/master@{#304996}

---
+++ net/websockets/websocket_handshake_challenge.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "net/websockets/websocket_handshake_challenge.h"
+
+#include "base/base64.h"
+#include "base/sha1.h"
+#include "net/websockets/websocket_handshake_constants.h"
+
+namespace net {
+
+std::string ComputeSecWebSocketAccept(const std::string& key) {
+  std::string accept;
+  std::string hash = base::SHA1HashString(key + websockets::kWebSocketGuid);
+  base::Base64Encode(hash, &accept);
+  return accept;
+}
+
+}  // namespace net
