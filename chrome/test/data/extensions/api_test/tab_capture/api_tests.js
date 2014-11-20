a9dede5ab9e4fe335d3a87cb9f38f55b07d6cc2c
miu <miu@chromium.org>
2014-11-19 16:25:11 -0800

    Misc clean-up of TabCaptureApiTests; and re-enable more tests.
    1. Consolidated ApiTestsAudio (and api_tests_audio.*) into ApiTests (and api_tests.*).
    2. Removed checks for WinXP, since the real problem is likely this: http://crbug.com/177163
    3. Removed duplication.
    4. Minor comment clean-ups/additions.
    
    BUG=294431,326319
    
    Review URL: https://codereview.chromium.org/734853003
    
    Cr-Commit-Position: refs/heads/master@{#304941}

--- chrome/test/data/extensions/api_test/tab_capture/api_tests.js
+++ chrome/test/data/extensions/api_test/tab_capture/api_tests.js
@@ -46,7 +46,7 @@ chrome.test.runTests([
       }
 
       // Step 4: After the second tab is closed, check that getCapturedTabs()
-      // returns no info at all about the second tab.  http://crbug.com/338445
+      // returns no info at all about the second tab.
       chrome.tabs.onRemoved.addListener(function() {
         tabCapture.getCapturedTabs(function checkNoInfos(infos) {
           checkInfoForSecondTabHasStatus(infos, null);
@@ -106,6 +106,14 @@ chrome.test.runTests([
     });
   },
 
+  function onlyAudio() {
+    tabCapture.capture({audio: true}, function(stream) {
+      chrome.test.assertTrue(!!stream);
+      stream.stop();
+      chrome.test.succeed();
+    });
+  },
+
   function noAudioOrVideoRequested() {
     // If not specified, video is not requested.
     tabCapture.capture({audio: false}, function(stream) {
@@ -113,5 +121,4 @@ chrome.test.runTests([
       chrome.test.succeed();
     });
   }
-
 ]);
--- chrome/test/data/extensions/api_test/tab_capture/constraints.js
+++ chrome/test/data/extensions/api_test/tab_capture/constraints.js
@@ -21,7 +21,7 @@ chrome.test.runTests([
     });
   },
 
-  function supportsOptionalMediaConstraints() {
+  function rejectsOptionalMediaConstraints() {
     chrome.tabCapture.capture({
       video: true,
       audio: true,
@@ -36,6 +36,32 @@ chrome.test.runTests([
     }, function(stream) {
       chrome.test.assertTrue(!stream);
       chrome.test.succeed();
+    });
+  },
+
+  function rejectsInvalidConstraints() {
+    chrome.tabCapture.capture({
+      video: true,
+      audio: true,
+      videoConstraints: {
+        mandatory: {
+          notValid: '123'
+        }
+      }
+    }, function(stream) {
+      chrome.test.assertTrue(!stream);
+
+      chrome.tabCapture.capture({
+        audio: true,
+        audioConstraints: {
+          mandatory: {
+            notValid: '123'
+          }
+        }
+      }, function(stream) {
+        chrome.test.assertTrue(!stream);
+        chrome.test.succeed();
+      });
     });
   }
 ]);
