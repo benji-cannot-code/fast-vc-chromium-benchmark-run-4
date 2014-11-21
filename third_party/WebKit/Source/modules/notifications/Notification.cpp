9006deccf57eee5fd28e48b133de404f57a9614f
peter@chromium.org <peter@chromium.org>
2014-11-21 15:06:08 +0000

    Mark the Notification as being closed before dispatching the event.
    This avoids a situation in which we can crash when the developer calls
    the Notification.close() method in the "onclose" event of a Notification.
    
    BUG=435201
    
    Review URL: https://codereview.chromium.org/745713003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185800 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/notifications/Notification.cpp
+++ third_party/WebKit/Source/modules/notifications/Notification.cpp
@@ -140,8 +140,8 @@ void Notification::dispatchErrorEvent()
 
 void Notification::dispatchCloseEvent()
 {
-    dispatchEvent(Event::create(EventTypeNames::close));
     m_state = NotificationStateClosed;
+    dispatchEvent(Event::create(EventTypeNames::close));
 }
 
 TextDirection Notification::direction() const
