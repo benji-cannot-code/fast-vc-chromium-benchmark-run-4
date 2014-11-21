9f8bf6bc22a14ad11a1ac3ce843f190bc136a9a9
horo <horo@chromium.org>
2014-11-21 10:46:06 -0800

    [ServiceWorker] support OPTIONS request for ServiceWorker [2/3 chromium]
    Currently we can't handle OPTIONS request in the ServiceWorker.
    
    This check is done in content/browser/service_worker/service_worker_request_handler.cc.
    But we should support OPTIONS request except for CORS preflight request.
    
    [1/3] blink: https://codereview.chromium.org/738033002/
    Set SkipServiceWorker flag for CORS preflight request.
    [2/3] chromium: https://codereview.chromium.org/716083003/
    Remove IsMethodSupportedForServiceWorker check in service_worker_request_handler.cc
    [3/3] blink: https://codereview.chromium.org/743653002/
    Cleanup and add update LayoutTest.
    
    BUG=434660
    
    Review URL: https://codereview.chromium.org/716083003
    
    Cr-Commit-Position: refs/heads/master@{#305250}

--- content/browser/service_worker/service_worker_request_handler.cc
+++ content/browser/service_worker/service_worker_request_handler.cc
@@ -49,13 +49,6 @@ class ServiceWorkerRequestInterceptor
   DISALLOW_COPY_AND_ASSIGN(ServiceWorkerRequestInterceptor);
 };
 
-// This is work around to avoid hijacking CORS preflight.
-// TODO(horo): Remove this check when we implement "HTTP fetch" correctly.
-// http://fetch.spec.whatwg.org/#concept-http-fetch
-bool IsMethodSupportedForServiceWorker(const std::string& method) {
-  return method != "OPTIONS";
-}
-
 }  // namespace
 
 void ServiceWorkerRequestHandler::InitializeHandler(
@@ -71,10 +64,8 @@ void ServiceWorkerRequestHandler::InitializeHandler(
     RequestContextType request_context_type,
     RequestContextFrameType frame_type,
     scoped_refptr<ResourceRequestBody> body) {
-  if (!request->url().SchemeIsHTTPOrHTTPS() ||
-      !IsMethodSupportedForServiceWorker(request->method())) {
+  if (!request->url().SchemeIsHTTPOrHTTPS())
     return;
-  }
 
   if (!context_wrapper || !context_wrapper->context() ||
       provider_id == kInvalidServiceWorkerProviderId) {
