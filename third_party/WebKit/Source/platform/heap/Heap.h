324480767c348607d53cc42a610e8d861820d8cf
timvolodine@chromium.org <timvolodine@chromium.org>
2014-11-21 16:06:09 +0000

    Revert of Avoid calling osPageSize() from hot path (patchset #2 id:20001 of https://codereview.chromium.org/740223002/)
    Reason for revert:
    speculative revert, this patch likely breaks a number of browser tests on the linux_chromium_asan_rel bot.
    
    http://build.chromium.org/p/tryserver.chromium.linux/builders/linux_chromium_asan_rel/builds/11965
    
    
    Original issue's description:
    > Avoid calling osPageSize() from hot path
    > 
    > Before this CL, osPageSize() was invoked in hot code paths, like
    > Heap::popAndInvokeTraceCallback which is called per object graph node.
    > This CL avoids calling osPageSize() from those hot code paths by replacing them with WTF::kSystemPageSize.
    > 
    > BUG=420515
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=185743
    
    TBR=oilpan-reviews@chromium.org,haraken@chromium.org,sigbjornf@opera.com,kouhei@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=420515
    
    Review URL: https://codereview.chromium.org/752583002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185803 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/heap/Heap.h
+++ third_party/WebKit/Source/platform/heap/Heap.h
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/LinkedHashSet.h"
 #include "wtf/ListHashSet.h"
 #include "wtf/OwnPtr.h"
-#include "wtf/PageAllocator.h"
 #include "wtf/PassRefPtr.h"
 #include "wtf/ThreadSafeRefCounted.h"
 
@@ -112,11 +111,13 @@ template<typename T, typename RootsAccessor = ThreadLocalPersistents<ThreadingTr
 class TracedValue;
 #endif
 
+PLATFORM_EXPORT size_t osPageSize();
+
 // Blink heap pages are set up with a guard page before and after the
 // payload.
 inline size_t blinkPagePayloadSize()
 {
-    return blinkPageSize - 2 * WTF::kSystemPageSize;
+    return blinkPageSize - 2 * osPageSize();
 }
 
 // Blink heap pages are aligned to the Blink heap page size.
@@ -153,7 +154,7 @@ inline Address blinkPageAddress(Address address)
 // aligned.
 inline bool isPageHeaderAddress(Address address)
 {
-    return !((reinterpret_cast<uintptr_t>(address) & blinkPageOffsetMask) - WTF::kSystemPageSize);
+    return !((reinterpret_cast<uintptr_t>(address) & blinkPageOffsetMask) - osPageSize());
 }
 #endif
 
@@ -164,7 +165,7 @@ inline bool isPageHeaderAddress(Address address)
 PLATFORM_EXPORT inline BaseHeapPage* pageHeaderFromObject(const void* object)
 {
     Address address = reinterpret_cast<Address>(const_cast<void*>(object));
-    return reinterpret_cast<BaseHeapPage*>(blinkPageAddress(address) + WTF::kSystemPageSize);
+    return reinterpret_cast<BaseHeapPage*>(blinkPageAddress(address) + osPageSize());
 }
 
 // Large allocations are allocated as separate objects and linked in a
@@ -504,7 +505,7 @@ public:
     virtual bool contains(Address addr) override
     {
         Address blinkPageStart = roundToBlinkPageStart(address());
-        ASSERT(blinkPageStart == address() - WTF::kSystemPageSize); // Page is at aligned address plus guard page size.
+        ASSERT(blinkPageStart == address() - osPageSize()); // Page is at aligned address plus guard page size.
         return blinkPageStart <= addr && addr < blinkPageStart + blinkPageSize;
     }
 
