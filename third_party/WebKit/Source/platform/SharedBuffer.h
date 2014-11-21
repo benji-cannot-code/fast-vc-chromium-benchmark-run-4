9cbbf061285254497a901bc3f744b6d1c39c2473
yukishiino@chromium.org <yukishiino@chromium.org>
2014-11-21 15:20:59 +0000

    bindings: Removes use of ArrayBuffer from platform/SharedBuffer.
    As part of the transition from ArrayBuffer to DOMArrayBuffer,
    removes use of ArrayBuffer from platform/SharedBuffer.
    
    BUG=235436
    TBR=tkent@chromium.org
    
    Review URL: https://codereview.chromium.org/747963002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185802 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/SharedBuffer.h
+++ third_party/WebKit/Source/platform/SharedBuffer.h
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/PlatformExport.h"
 #include "platform/PurgeableVector.h"
 #include "third_party/skia/include/core/SkData.h"
-#include "wtf/ArrayBuffer.h"
 #include "wtf/Forward.h"
 #include "wtf/OwnPtr.h"
 #include "wtf/RefCounted.h"
@@ -83,10 +82,10 @@ public:
     //      }
     unsigned getSomeData(const char*& data, unsigned position = 0) const;
 
-    // Creates an ArrayBuffer and copies this SharedBuffer's contents to that
-    // ArrayBuffer without merging segmented buffers into a flat buffer. If
-    // allocation of an ArrayBuffer fails, returns 0.
-    PassRefPtr<ArrayBuffer> getAsArrayBuffer() const;
+    // Returns the content data into "dest" as a flat buffer. "byteLength" must
+    // exactly match with size(). Returns true on success, otherwise the content
+    // of "dest" is not guaranteed.
+    bool getAsBytes(void* dest, unsigned byteLength) const;
 
     // Creates an SkData and copies this SharedBuffer's contents to that
     // SkData without merging segmented buffers into a flat buffer.
