22ca2ece81e2624532858fc616b2b75b00209830
rdevlin.cronin <rdevlin.cronin@chromium.org>
2014-11-21 13:42:03 -0800

    Move BrowserActionsBarBrowserTest.Visibility to be cross-platform
    BUG=422381
    
    Review URL: https://codereview.chromium.org/738603002
    
    Cr-Commit-Position: refs/heads/master@{#305288}

--- chrome/browser/ui/cocoa/extensions/browser_action_test_util_mac.mm
+++ chrome/browser/ui/cocoa/extensions/browser_action_test_util_mac.mm
@@ -7,12 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/mac/foundation_util.h"
 #include "base/strings/sys_string_conversions.h"
+#include "chrome/browser/extensions/extension_toolbar_model.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/toolbar/toolbar_action_view_controller.h"
 #import "chrome/browser/ui/cocoa/browser_window_cocoa.h"
 #import "chrome/browser/ui/cocoa/browser_window_controller.h"
 #import "chrome/browser/ui/cocoa/extensions/browser_action_button.h"
 #import "chrome/browser/ui/cocoa/extensions/browser_actions_controller.h"
+#import "chrome/browser/ui/cocoa/extensions/browser_actions_container_view.h"
 #import "chrome/browser/ui/cocoa/extensions/extension_popup_controller.h"
 #import "chrome/browser/ui/cocoa/info_bubble_window.h"
 #import "chrome/browser/ui/cocoa/toolbar/toolbar_controller.h"
@@ -41,6 +43,13 @@ int BrowserActionTestUtil::NumberOfBrowserActions() {
 
 int BrowserActionTestUtil::VisibleBrowserActions() {
   return [GetController(browser_) visibleButtonCount];
+}
+
+bool BrowserActionTestUtil::IsChevronShowing() {
+  BrowserActionsController* controller = GetController(browser_);
+  // The magic "18" comes from kChevronWidth in browser_actions_controller.mm.
+  return ![controller chevronIsHidden] &&
+         NSWidth([[controller containerView] animationEndFrame]) >= 18;
 }
 
 void BrowserActionTestUtil::InspectPopup(int index) {
@@ -85,6 +94,11 @@ bool BrowserActionTestUtil::HasPopup() {
 gfx::Size BrowserActionTestUtil::GetPopupSize() {
   NSRect bounds = [[[ExtensionPopupController popup] view] bounds];
   return gfx::Size(NSSizeToCGSize(bounds.size));
+}
+
+void BrowserActionTestUtil::SetIconVisibilityCount(size_t icons) {
+  extensions::ExtensionToolbarModel::Get(browser_->profile())->
+      SetVisibleIconCount(icons);
 }
 
 bool BrowserActionTestUtil::HidePopup() {
