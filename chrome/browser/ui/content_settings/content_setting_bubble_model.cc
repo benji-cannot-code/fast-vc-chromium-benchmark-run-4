d6fb5a835de1375331c68c06dacb300c4345d38b
vabr <vabr@chromium.org>
2014-11-21 08:35:21 -0800

    ContentSettingBubbleModel: move RPHState into the .cc file
    The RPHState enum is an implementation detail, and should not be in the header file.
    
    BUG=395085
    
    Review URL: https://codereview.chromium.org/745263002
    
    Cr-Commit-Position: refs/heads/master@{#305225}

--- chrome/browser/ui/content_settings/content_setting_bubble_model.cc
+++ chrome/browser/ui/content_settings/content_setting_bubble_model.cc
@@ -51,6 +51,14 @@ namespace {
 
 const int kAllowButtonIndex = 0;
 
+// These states must match the order of appearance of the radio buttons
+// in the XIB file for the Mac port.
+enum RPHState {
+  RPH_ALLOW = 0,
+  RPH_BLOCK,
+  RPH_IGNORE,
+};
+
 struct ContentSettingsTypeIdEntry {
   ContentSettingsType type;
   int id;
