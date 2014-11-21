b5e37778fa9ca6334e268d33d7668973ae3a943f
sky <sky@chromium.org>
2014-11-20 20:37:50 -0800

    Makes SurfaceAggregator properly pass through opacity
    BUG=434110
    TEST=SurfaceAggregatorValidSurfaceTest.OpacityCopied
    R=jamesr@chromium.org
    
    Review URL: https://codereview.chromium.org/734813002
    
    Cr-Commit-Position: refs/heads/master@{#305164}

--- cc/surfaces/surface_aggregator_test_helpers.h
+++ cc/surfaces/surface_aggregator_test_helpers.h
@@ -31,9 +31,10 @@ struct Quad {
     return quad;
   }
 
-  static Quad SurfaceQuad(SurfaceId surface_id) {
+  static Quad SurfaceQuad(SurfaceId surface_id, float opacity) {
     Quad quad;
     quad.material = DrawQuad::SURFACE_CONTENT;
+    quad.opacity = opacity;
     quad.surface_id = surface_id;
     return quad;
   }
@@ -48,6 +49,7 @@ struct Quad {
   DrawQuad::Material material;
   // Set when material==DrawQuad::SURFACE_CONTENT.
   SurfaceId surface_id;
+  float opacity;
   // Set when material==DrawQuad::SOLID_COLOR.
   SkColor color;
   // Set when material==DrawQuad::RENDER_PASS.
@@ -56,6 +58,7 @@ struct Quad {
  private:
   Quad()
       : material(DrawQuad::INVALID),
+        opacity(1.f),
         color(SK_ColorWHITE),
         render_pass_id(-1, -1) {}
 };
