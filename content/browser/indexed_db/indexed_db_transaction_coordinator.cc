63b4a59108a840f398a902f93e1dd4023044c455
dgrogan <dgrogan@chromium.org>
2014-11-20 19:11:01 -0800

    IDB: Make readonly transactions wait for earlier readwrite transactions
    The spec recently tightened to more explicitly match the
    behavior of non-chrome browsers:
    https://www.w3.org/Bugs/Public/show_bug.cgi?id=25251
    
    Blink layout tests are waiting over in
    https://codereview.chromium.org/745553002/
    
    BUG=432259
    
    Review URL: https://codereview.chromium.org/742033002
    
    Cr-Commit-Position: refs/heads/master@{#305152}

--- content/browser/indexed_db/indexed_db_transaction_coordinator.cc
+++ content/browser/indexed_db/indexed_db_transaction_coordinator.cc
@@ -147,8 +147,6 @@ bool IndexedDBTransactionCoordinator::CanStartTransaction(
       return true;
 
     case blink::WebIDBTransactionModeReadOnly:
-      return true;
-
     case blink::WebIDBTransactionModeReadWrite:
       return !DoSetsIntersect(transaction->scope(), locked_scope);
   }
