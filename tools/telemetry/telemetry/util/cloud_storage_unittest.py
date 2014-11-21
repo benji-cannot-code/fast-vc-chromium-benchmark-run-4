8a95c81e017a87b16308b09cf85b2de08b3967ff
miletus <miletus@chromium.org>
2014-11-20 20:06:28 -0800

    Accessible cloud url to internal bucket
    BUG=431904, 433456
    
    Review URL: https://codereview.chromium.org/745753002
    
    Cr-Commit-Position: refs/heads/master@{#305160}

---
+++ tools/telemetry/telemetry/util/cloud_storage_unittest.py
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import unittest
+
+from telemetry.util import cloud_storage
+
+class CloudStorageUnitTest(unittest.TestCase):
+
+  def _FakeRunCommand(self, cmd):
+    pass
+
+  def testValidCloudUrl(self):
+    cloud_storage._RunCommand = self._FakeRunCommand
+    remote_path = 'test-remote-path.html'
+    local_path = 'test-local-path.html'
+    cloud_url = cloud_storage.Insert(cloud_storage.PUBLIC_BUCKET,
+                                     remote_path, local_path)
+    self.assertEqual('https://console.developers.google.com/m/cloudstorage'
+                     '/b/chromium-telemetry/o/test-remote-path.html',
+                     cloud_url)
