1b7747d03a4ee007352c99894f052675b4c47079
tbarzic <tbarzic@chromium.org>
2014-11-21 14:06:21 -0800

    Change chrome.easyUnlockPrivate.getSignInChallenge signature
    Changes chrome.easyUnlockPrivate.getSignInChallenge take in a nonce
    and return the nonce singed by TPM (in addition to the sign-in
    challenge).
    Note that this does not yet implement the nonce signing logic.
    
    The nonce signature will be used by Easy Sign-in protocol instead of
    currently used signature, which is generated using a hardcoded private
    key.
    
    Note that the extension using this function will be updated to use the
    new signature prior to landing this.
    
    BUG=409027
    TEST=Easy Sign-in still works
    
    Review URL: https://codereview.chromium.org/734913002
    
    Cr-Commit-Position: refs/heads/master@{#305296}

--- chrome/browser/extensions/api/easy_unlock_private/easy_unlock_private_api.h
+++ chrome/browser/extensions/api/easy_unlock_private/easy_unlock_private_api.h
@@ -284,7 +284,7 @@ class EasyUnlockPrivateGetRemoteDevicesFunction : public SyncExtensionFunction {
 };
 
 class EasyUnlockPrivateGetSignInChallengeFunction :
-    public SyncExtensionFunction {
+    public AsyncExtensionFunction {
  public:
   DECLARE_EXTENSION_FUNCTION("easyUnlockPrivate.getSignInChallenge",
                              EASYUNLOCKPRIVATE_GETSIGNINCHALLENGE)
@@ -293,8 +293,11 @@ class EasyUnlockPrivateGetSignInChallengeFunction :
  private:
   ~EasyUnlockPrivateGetSignInChallengeFunction() override;
 
-  // SyncExtensionFunction:
-  bool RunSync() override;
+  // AsyncExtensionFunction:
+  bool RunAsync() override;
+
+  // Called when the challenge and the signed nonce have been generated.
+  void OnDone(const std::string& challenge, const std::string& signed_nonce);
 
   DISALLOW_COPY_AND_ASSIGN(EasyUnlockPrivateGetSignInChallengeFunction);
 };
