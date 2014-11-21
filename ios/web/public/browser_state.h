f7764cd47379a3d0ca7a161a88b4fccfc59d6df0
sdefresne <sdefresne@chromium.org>
2014-11-21 02:33:05 -0800

    Revert of Expand web::BrowserState to add GetPath() method (patchset #2 id:60001 of https://codereview.chromium.org/740353002/)
    Reason for revert:
    Committed wrong version (without fixing method comments).
    
    Original issue's description:
    > Expand web::BrowserState to add GetPath() method
    >
    > Add a method to web::BrowserState returning the path where the
    > BrowserState data is stored.
    >
    > BUG=429756
    >
    > Committed: https://crrev.com/d537d6c355be1a51bbb808e9da214f5595d75f3d
    > Cr-Commit-Position: refs/heads/master@{#305191}
    
    TBR=blundell@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=429756
    
    Review URL: https://codereview.chromium.org/747913002
    
    Cr-Commit-Position: refs/heads/master@{#305197}

--- ios/web/public/browser_state.h
+++ ios/web/public/browser_state.h
@@ -8,10 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/supports_user_data.h"
 
-namespace base {
-class FilePath;
-}
-
 namespace net {
 class URLRequestContextGetter;
 }
@@ -27,9 +23,6 @@ class BrowserState : public base::SupportsUserData {
 
   // Return whether this BrowserState is incognito. Default is false.
   virtual bool IsOffTheRecord() const = 0;
-
-  // Retrieves the path where the BrowserState data is stored.
-  virtual base::FilePath GetPath() const = 0;
 
   // Returns the request context information associated with this
   // BrowserState.
