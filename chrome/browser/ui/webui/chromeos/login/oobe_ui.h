c273f6590ecc877dd60c1c0273615636240e134c
ygorshenin <ygorshenin@chromium.org>
2014-11-21 09:32:44 -0800

    Added initialization of the ErrorScreen to the OobeUI.
    ErrorScreen is not initialized when OOBE is passed and device is rebooted.
    OobeUI is initialized in the case, but WizardController isn't, so
    ErrorScreenHandler acts without ErrorScreen.
    
    In the CL I'm proposing to explicitly initialize ErrorScreen in OobeUI,
    as it's used by SigninScreenHandler, and, probably, will be used by a
    SigninScree/GaiaScreen.
    
    BUG=chrome-os-partner:33889
    TEST=manual tests on a ChromeOS device
    
    Review URL: https://codereview.chromium.org/744083002
    
    Cr-Commit-Position: refs/heads/master@{#305232}

--- chrome/browser/ui/webui/chromeos/login/oobe_ui.h
+++ chrome/browser/ui/webui/chromeos/login/oobe_ui.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/memory/ref_counted.h"
+#include "base/memory/scoped_ptr.h"
 #include "base/observer_list.h"
 #include "chrome/browser/chromeos/login/ui/oobe_display.h"
 #include "chrome/browser/ui/webui/chromeos/login/core_oobe_handler.h"
@@ -26,7 +27,9 @@ class AppLaunchSplashScreenActor;
 class BaseScreenHandler;
 class ControllerPairingScreenActor;
 class DeviceDisabledScreenActor;
+class ErrorScreen;
 class ErrorScreenHandler;
+class GaiaScreenHandler;
 class HostPairingScreenActor;
 class KioskAppMenuHandler;
 class KioskEnableScreenActor;
@@ -34,7 +37,6 @@ class LoginScreenContext;
 class NativeWindowDelegate;
 class NetworkDropdownHandler;
 class NetworkStateInformer;
-class GaiaScreenHandler;
 class SigninScreenHandler;
 class SigninScreenHandlerDelegate;
 class UpdateScreenHandler;
@@ -224,6 +226,8 @@ class OobeUI : public OobeDisplay,
   std::vector<BaseScreenHandler*> handlers_;  // Non-owning pointers.
 
   KioskAppMenuHandler* kiosk_app_menu_handler_;  // Non-owning pointers.
+
+  scoped_ptr<ErrorScreen> error_screen_;
 
   // Id of the current oobe/login screen.
   Screen current_screen_;
