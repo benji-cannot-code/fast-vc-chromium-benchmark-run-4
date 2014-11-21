335dbf917ff370b3f695e2343cc3ea30a4adcc5f
dfalcantara <dfalcantara@chromium.org>
2014-11-20 16:52:15 -0800

    Add annotation to stop tests from running in tabbed mode
    Need an annotation to run tests only in Document mode, if
    it's available.
    
    BUG=394854
    
    Review URL: https://codereview.chromium.org/738183004
    
    Cr-Commit-Position: refs/heads/master@{#305127}

---
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/util/DisableInTabbedMode.java
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.test.util;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * This annotation can be used to mark a test that should be enabled only in Document mode.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DisableInTabbedMode {
+}
