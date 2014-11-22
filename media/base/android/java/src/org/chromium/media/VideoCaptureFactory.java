72dd984087248b16992dc361f633a21785353c42
mcasas <mcasas@chromium.org>
2014-11-21 18:46:36 -0800

    Android Video Capture: Added static methods for device & capabilities enumeration using Camera2 API
    Also, renamed the new class
    VideoCaptureAndroid2->VideoCaptureCamera2.
    (respecting [1])
    
    [1] https://code.google.com/p/chromium/issues/detail?id=418052#c2
    
    BUG=418052
    
    Review URL: https://codereview.chromium.org/611283002
    
    Cr-Commit-Position: refs/heads/master@{#305347}

--- media/base/android/java/src/org/chromium/media/VideoCaptureFactory.java
+++ media/base/android/java/src/org/chromium/media/VideoCaptureFactory.java
@@ -7,6 +7,7 @@ package org.chromium.media;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.util.Log;
 
 import org.chromium.base.CalledByNative;
@@ -68,19 +69,28 @@ class VideoCaptureFactory {
                 if (PackageManager.PERMISSION_GRANTED
                         == appContext.getPackageManager().checkPermission(
                                 "android.permission.CAMERA", appContext.getPackageName())) {
-                    sNumberOfSystemCameras = VideoCaptureAndroid.getNumberOfCameras();
+                    if (isLReleaseOrLater()) {
+                        sNumberOfSystemCameras =
+                                VideoCaptureCamera2.getNumberOfCameras(appContext);
+                    } else {
+                        sNumberOfSystemCameras = VideoCaptureAndroid.getNumberOfCameras();
+                        if (isSpecialDevice()) {
+                            Log.d(TAG, "Special device: " + android.os.Build.MODEL);
+                            sNumberOfSystemCameras += VideoCaptureTango.numberOfCameras();
+                        }
+                    }
                 } else {
                     sNumberOfSystemCameras = 0;
                     Log.w(TAG, "Missing android.permission.CAMERA permission, "
                                   + "no system camera available.");
                 }
             }
-            if (!isSpecialDevice()) {
-                return sNumberOfSystemCameras;
-            }
-            Log.d(TAG, "Special device: " + android.os.Build.MODEL);
-            return sNumberOfSystemCameras + VideoCaptureTango.numberOfCameras();
+            return sNumberOfSystemCameras;
         }
+    }
+
+    private static boolean isLReleaseOrLater() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.L;
     }
 
     // Factory methods.
@@ -102,14 +112,20 @@ class VideoCaptureFactory {
     }
 
     @CalledByNative
-    static String getDeviceName(int id) {
+    static String getDeviceName(int id, Context appContext) {
+        if (isLReleaseOrLater()) {
+            return VideoCaptureCamera2.getName(id, appContext);
+        }
         return (ChromiumCameraInfo.isSpecialCamera(id))
                 ? VideoCaptureTango.getName(ChromiumCameraInfo.toSpecialCameraId(id))
                 : VideoCaptureAndroid.getName(id);
     }
 
     @CalledByNative
-    static VideoCapture.CaptureFormat[] getDeviceSupportedFormats(int id) {
+    static VideoCapture.CaptureFormat[] getDeviceSupportedFormats(Context appContext, int id) {
+        if (isLReleaseOrLater()) {
+            return VideoCaptureCamera2.getDeviceSupportedFormats(appContext, id);
+        }
         return ChromiumCameraInfo.isSpecialCamera(id)
                 ? VideoCaptureTango.getDeviceSupportedFormats(
                         ChromiumCameraInfo.toSpecialCameraId(id))
