21b2b2fd0925543198d45e004a6671349ff37f57
nednguyen <nednguyen@google.com>
2014-11-21 16:02:15 -0800

    [Telemetry] Only serialize trace for json results.
    BUG=435284
    
    Review URL: https://codereview.chromium.org/742353002
    
    Cr-Commit-Position: refs/heads/master@{#305322}

--- tools/telemetry/telemetry/results/json_output_formatter.py
+++ tools/telemetry/telemetry/results/json_output_formatter.py
@@ -10,7 +10,7 @@ from telemetry.results import output_formatter
 from telemetry.util import file_handle
 
 
-def ResultsAsDict(page_test_results, benchmark_metadata, output_dir):
+def ResultsAsDict(page_test_results, benchmark_metadata):
   """Takes PageTestResults to a dict serializable to JSON.
 
   To serialize results as JSON we first convert them to a dict that can be
@@ -21,7 +21,6 @@ def ResultsAsDict(page_test_results, benchmark_metadata, output_dir):
   Args:
     page_test_results: a PageTestResults object
     benchmark_metadata: a benchmark.BenchmarkMetadata object
-    output_dir: the directory that results are being output to.
   """
   result_dict = {
     'format_version': '0.2',
@@ -32,21 +31,9 @@ def ResultsAsDict(page_test_results, benchmark_metadata, output_dir):
                         page_test_results.all_page_specific_values],
     'pages': {p.id: p.AsDict() for p in _GetAllPages(page_test_results)}
   }
-
-  file_ids_to_paths = _OutputTraceFiles(page_test_results, output_dir)
-  if file_ids_to_paths:
-    result_dict['files'] = file_ids_to_paths
+  if page_test_results.serialized_trace_file_ids_to_paths:
+    result_dict['files'] = page_test_results.serialized_trace_file_ids_to_paths
   return result_dict
-
-
-def _OutputTraceFiles(page_test_results, output_dir):
-  file_handles = page_test_results.all_file_handles
-  if not file_handles:
-    return {}
-  trace_dir = os.path.join(output_dir, 'trace_files')
-  if not os.path.isdir(trace_dir):
-    os.makedirs(trace_dir)
-  return file_handle.OutputFiles(file_handles, trace_dir)
 
 
 def _GetAllPages(page_test_results):
@@ -56,10 +43,9 @@ def _GetAllPages(page_test_results):
 
 
 class JsonOutputFormatter(output_formatter.OutputFormatter):
-  def __init__(self, output_stream, output_dir, benchmark_metadata):
+  def __init__(self, output_stream, benchmark_metadata):
     super(JsonOutputFormatter, self).__init__(output_stream)
     self._benchmark_metadata = benchmark_metadata
-    self._output_dir = output_dir
 
   @property
   def benchmark_metadata(self):
@@ -67,7 +53,6 @@ class JsonOutputFormatter(output_formatter.OutputFormatter):
 
   def Format(self, page_test_results):
     json.dump(
-        ResultsAsDict(
-            page_test_results, self.benchmark_metadata, self._output_dir),
+        ResultsAsDict(page_test_results, self.benchmark_metadata),
         self.output_stream)
     self.output_stream.write('\n')
