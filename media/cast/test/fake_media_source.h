221c3189205d20b0447f481cb2812a00cd313b0f
hclam <hclam@chromium.org>
2014-11-22 06:34:25 -0800

    Cast Streaming: Measure PSNR and SSIM in simulation
    Add a couple features to cast_simulator:
    * Measures PSNR and SSIM from the decoded output.
    * Writes YUV decoded frames into a file.
    
    PSNR and SSIM can be used to measure the quality impact of encoder
    changes. Writing to YUV files help to perform manual inspection.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/737203002
    
    Cr-Commit-Position: refs/heads/master@{#305362}

--- media/cast/test/fake_media_source.h
+++ media/cast/test/fake_media_source.h
@@ -47,9 +47,11 @@ class FakeMediaSource {
   // |task_runner| is to schedule decoding tasks.
   // |clock| is used by this source but is not owned.
   // |video_config| is the desired video config.
+  // |keep_frames| is true if all VideoFrames are saved in a queue.
   FakeMediaSource(scoped_refptr<base::SingleThreadTaskRunner> task_runner,
                   base::TickClock* clock,
-                  const VideoSenderConfig& video_config);
+                  const VideoSenderConfig& video_config,
+                  bool keep_frames);
   ~FakeMediaSource();
 
   // Transcode this file as the source of video and audio frames.
@@ -60,6 +62,8 @@ class FakeMediaSource {
              scoped_refptr<VideoFrameInput> video_frame_input);
 
   const VideoSenderConfig& get_video_config() const { return video_config_; }
+
+  scoped_refptr<media::VideoFrame> PopOldestInsertedVideoFrame();
 
  private:
   bool is_transcoding_audio() const { return audio_stream_index_ >= 0; }
@@ -100,6 +104,7 @@ class FakeMediaSource {
 
   const scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
   const VideoSenderConfig video_config_;
+  const bool keep_frames_;
   scoped_refptr<AudioFrameInput> audio_frame_input_;
   scoped_refptr<VideoFrameInput> video_frame_input_;
   uint8 synthetic_count_;
@@ -136,6 +141,7 @@ class FakeMediaSource {
   scoped_ptr<media::AudioTimestampHelper> audio_sent_ts_;
 
   std::queue<scoped_refptr<VideoFrame> > video_frame_queue_;
+  std::queue<scoped_refptr<VideoFrame> > inserted_video_frame_queue_;
   int64 video_first_pts_;
   bool video_first_pts_set_;
   base::TimeDelta last_video_frame_timestamp_;
