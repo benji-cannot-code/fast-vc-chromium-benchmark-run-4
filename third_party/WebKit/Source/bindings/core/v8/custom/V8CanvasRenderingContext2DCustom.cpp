56318c7013c59097ee496171805029d1a4955a2a
junov@chromium.org <junov@chromium.org>
2014-11-22 02:56:05 +0000

    Remove non-standard API entry points in CanvasRenderingContext2D
    Rmoving the API entry points for the following 2d canvas methods:
    setAlpha
    setCompositingOperation
    setLineWidth
    setLineCap
    setLineJoin
    setMiterLimit
    clearShadow
    setStrokeColor
    setFillColor
    drawImageFromRect
    setShadow
    
    According to usage statistics, these entry points are not used anywhere
    on the web. They are undocumented and redundant with existing standard
    APIs.
    
    BUG=363198,363165,363166,363180,363181,363184,363185,363186,363188,363189,363191
    
    Review URL: https://codereview.chromium.org/408813007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185835 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/core/v8/custom/V8CanvasRenderingContext2DCustom.cpp
+++ third_party/WebKit/Source/bindings/core/v8/custom/V8CanvasRenderingContext2DCustom.cpp
@@ -79,7 +79,7 @@ void V8CanvasRenderingContext2D::strokeStyleAttributeSetterCustom(v8::Local<v8::
         impl->setStrokeStyle(canvasStyle);
     } else {
         TOSTRING_VOID(V8StringResource<>, colorString, value);
-        impl->setStrokeColor(colorString);
+        impl->setStrokeStyle(CanvasStyle::createFromString(colorString));
     }
 }
 
@@ -96,7 +96,7 @@ void V8CanvasRenderingContext2D::fillStyleAttributeSetterCustom(v8::Local<v8::Va
         impl->setFillStyle(canvasStyle);
     } else {
         TOSTRING_VOID(V8StringResource<>, colorString, value);
-        impl->setFillColor(colorString);
+        impl->setFillStyle(CanvasStyle::createFromString(colorString));
     }
 }
 
