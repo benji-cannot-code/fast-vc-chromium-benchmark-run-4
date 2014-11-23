dc75f341a73d2ceb56878659d503a7d28b84c0f5
luken <luken@chromium.org>
2014-11-23 06:12:31 -0800

    We previously had a setting in chrome defaults but this deprecates that for a command line flag and utility function that always defers to the flag but otherwise returns a default based on operating system. The CL then removes some of the code that depended on the default for a more robust direct check by the render_view_impl. This should also help with some of the CrOS UI bugs that link disambiguation created with views that didn't necessarily initialize their RendererPreferences.
    BUG=433007,428163
    TBR=jar
    
    Review URL: https://codereview.chromium.org/725243002
    
    Cr-Commit-Position: refs/heads/master@{#305385}

--- chrome/browser/renderer_preferences_util.cc
+++ chrome/browser/renderer_preferences_util.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(TOOLKIT_VIEWS)
-#include "chrome/browser/defaults.h"
 #include "ui/views/controls/textfield/textfield.h"
 #endif
 
@@ -71,14 +70,6 @@ void UpdateFromSystemSettings(content::RendererPreferences* prefs,
 
 #if defined(TOOLKIT_VIEWS)
   prefs->caret_blink_interval = views::Textfield::GetCaretBlinkMs() / 1000.0;
-  if (browser_defaults::kShowLinkDisambiguationPopup) {
-    prefs->tap_multiple_targets_strategy =
-        content::TapMultipleTargetsStrategy::
-            TAP_MULTIPLE_TARGETS_STRATEGY_POPUP;
-  } else {
-    prefs->tap_multiple_targets_strategy =
-        content::TapMultipleTargetsStrategy::TAP_MULTIPLE_TARGETS_STRATEGY_NONE;
-  }
 #endif
 
 #if defined(USE_AURA) && defined(OS_LINUX) && !defined(OS_CHROMEOS)
--- ui/base/ui_base_switches_util.h
+++ ui/base/ui_base_switches_util.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace switches {
 
+UI_BASE_EXPORT bool IsLinkDisambiguationPopupEnabled();
 UI_BASE_EXPORT bool IsTextInputFocusManagerEnabled();
 UI_BASE_EXPORT bool IsTouchDragDropEnabled();
 UI_BASE_EXPORT bool IsTouchEditingEnabled();
