7fb6f5d6039db15ad07c3e2c3bf973e93c374ac0
perkj <perkj@chromium.org>
2014-11-23 08:19:18 -0800

    Replace use of deprecated WebMediaStreamSource::initialize In order to implement MediaStreamTrack.remote and MediaStreamtTrack.readonly, these properties must be set when the source is initialized.
    BUG=418540
    
    Review URL: https://codereview.chromium.org/710043002
    
    Cr-Commit-Position: refs/heads/master@{#305389}

--- content/shell/renderer/test_runner/mock_web_user_media_client.cc
+++ content/shell/renderer/test_runner/mock_web_user_media_client.cc
@@ -171,7 +171,8 @@ void MockWebUserMediaClient::requestUserMedia(
       WebMediaStreamSource source;
       source.initialize("MockAudioDevice#1",
                         WebMediaStreamSource::TypeAudio,
-                        "Mock audio device");
+                        "Mock audio device",
+                        false /* remote */, true /* readonly */);
       audio_tracks[0].initialize(source);
     }
 
@@ -179,7 +180,8 @@ void MockWebUserMediaClient::requestUserMedia(
       WebMediaStreamSource source;
       source.initialize("MockVideoDevice#1",
                         WebMediaStreamSource::TypeVideo,
-                        "Mock video device");
+                        "Mock video device",
+                        false /* remote */, true /* readonly */);
       video_tracks[0].initialize(source);
     }
 
