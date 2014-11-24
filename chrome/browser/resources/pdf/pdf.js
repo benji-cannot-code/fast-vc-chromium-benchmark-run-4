f83c31957ae7cf6113c3444afa18fc9ca075f6fb
sammc <sammc@chromium.org>
2014-11-23 17:44:52 -0800

    OOP PDF: Add whether a resource is embedded to StreamInfo.
    Previously, the out-of-process PDF extension managed page zoom for all
    PDFs. This is not correct for embedded PDFs, which should not affect or
    be affected by page zoom. This CL makes that information available to
    the extension and uses it to decide not manage page zoom for embedded
    PDFs.
    
    BUG=433713
    TEST=Run with --out-of-process-pdf. Open a page embedding a PDF. Zooming
    should affect the page, including the size of the PDF viewport, but not
    the zoom level of the PDF.
    
    Review URL: https://codereview.chromium.org/726343002
    
    Cr-Commit-Position: refs/heads/master@{#305394}

--- chrome/browser/resources/pdf/pdf.js
+++ chrome/browser/resources/pdf/pdf.js
@@ -85,7 +85,7 @@ function PDFViewer(streamDetails) {
   }
   this.plugin_.setAttribute('headers', headers);
 
-  if (window.top == window)
+  if (!this.streamDetails.embedded)
     this.plugin_.setAttribute('full-frame', '');
   document.body.appendChild(this.plugin_);
 
@@ -114,7 +114,7 @@ function PDFViewer(streamDetails) {
   document.onkeydown = this.handleKeyEvent_.bind(this);
 
   // Set up the zoom API.
-  if (chrome.tabs) {
+  if (this.shouldManageZoom_()) {
     chrome.tabs.setZoomSettings(this.streamDetails.tabId,
                                 {mode: 'manual', scope: 'per-tab'},
                                 this.afterZoom_.bind(this));
@@ -424,7 +424,7 @@ PDFViewer.prototype = {
   afterZoom_: function() {
     var position = this.viewport_.position;
     var zoom = this.viewport_.zoom;
-    if (chrome.tabs && !this.setZoomInProgress_) {
+    if (this.shouldManageZoom_() && !this.setZoomInProgress_) {
       this.setZoomInProgress_ = true;
       chrome.tabs.setZoom(this.streamDetails.tabId, zoom,
                           this.setZoomComplete_.bind(this, zoom));
@@ -573,6 +573,17 @@ PDFViewer.prototype = {
    */
   sendScriptingMessage_: function(message) {
     window.parent.postMessage(message, '*');
+  },
+
+  /**
+   * @private
+   * Return whether this PDFViewer should manage zoom for its containing page.
+   * @return {boolean} Whether this PDFViewer should manage zoom for its
+   *     containing page.
+   */
+  shouldManageZoom_: function() {
+    return !!(chrome.tabs && !this.streamDetails.embedded &&
+              this.streamDetails.tabId != -1);
   },
 
   /**
