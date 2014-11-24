42e776dc4c53f2f4a82fe31fa1ac13301677ed30
dgozman@chromium.org <dgozman@chromium.org>
2014-11-24 11:12:42 +0000

    [DevTools] Remove unused embedder messages.
    BUG=none
    
    Review URL: https://codereview.chromium.org/749473002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185868 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/host/InspectorFrontendHost.js
+++ third_party/WebKit/Source/devtools/front_end/host/InspectorFrontendHost.js
@@ -131,25 +131,11 @@ InspectorFrontendHostAPI.prototype = {
     setInspectedPageBounds: function(bounds) { },
 
     /**
-     * Requests inspected page to be placed atop of the inspector frontend
-     * with passed insets from the frontend sides, respecting minimum size passed.
-     * @param {{top: number, left: number, right: number, bottom: number}} insets
-     * @param {{width: number, height: number}} minSize
-     */
-    setContentsResizingStrategy: function(insets, minSize) { },
-
-    /**
      * @param {string} shortcuts
      */
     setWhitelistedShortcuts: function(shortcuts) { },
 
     inspectElementCompleted: function() { },
-
-    /**
-     * @param {number} x
-     * @param {number} y
-     */
-    moveWindowBy: function(x, y) { },
 
     /**
      * @param {string} url
@@ -382,25 +368,7 @@ WebInspector.InspectorFrontendHostStub.prototype = {
     {
     },
 
-    /**
-     * Requests inspected page to be placed atop of the inspector frontend
-     * with passed insets from the frontend sides, respecting minimum size passed.
-     * @param {{top: number, left: number, right: number, bottom: number}} insets
-     * @param {{width: number, height: number}} minSize
-     */
-    setContentsResizingStrategy: function(insets, minSize)
-    {
-    },
-
     inspectElementCompleted: function()
-    {
-    },
-
-    /**
-     * @param {number} x
-     * @param {number} y
-     */
-    moveWindowBy: function(x, y)
     {
     },
 
