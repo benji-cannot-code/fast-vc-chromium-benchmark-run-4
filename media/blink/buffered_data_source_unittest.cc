c69acbc4ea585a0bcbc17a992c7b12ee1ab5ae41
dalecurtis <dalecurtis@chromium.org>
2014-11-24 10:43:35 -0800

    Allow network retries for failed network loads.
    Gives network failures the same retry convention allowed for cache
    misses. This allows suspend/resume sequences to playback without
    issue.
    
    Deep in the depths of net/ the URLRequestJob will self-cancel if
    it detects a suspend event via base::PowerMonitor.  Ultimately this
    results in a blink::WebURLLoaderClient::didFail() event which will
    abort any outstanding read operations.
    
    The fix is to retry failed read operations, up to a limit, after a
    short delay; this is easily handled in BufferedDataSource.
    
    Notably, this does not fix failed reads that happen during loader
    startup and will result in a tossing all cached data.
    
    BUG=421698
    TEST=unittest, manual verification
    
    Review URL: https://codereview.chromium.org/747633002
    
    Cr-Commit-Position: refs/heads/master@{#305453}

--- media/blink/buffered_data_source_unittest.cc
+++ media/blink/buffered_data_source_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/message_loop/message_loop.h"
+#include "base/run_loop.h"
 #include "media/base/media_log.h"
 #include "media/base/mock_filters.h"
 #include "media/base/test_helpers.h"
@@ -18,7 +19,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using ::testing::_;
 using ::testing::Assign;
+using ::testing::DoAll;
 using ::testing::Invoke;
+using ::testing::InvokeWithoutArgs;
 using ::testing::InSequence;
 using ::testing::NiceMock;
 using ::testing::StrictMock;
@@ -408,6 +411,34 @@ TEST_F(BufferedDataSourceTest, Http_Retry) {
   ReceiveData(kDataSize);
 
   EXPECT_TRUE(data_source_->loading());
+  Stop();
+}
+
+TEST_F(BufferedDataSourceTest, Http_RetryOnError) {
+  InitializeWith206Response();
+
+  // Read to advance our position.
+  EXPECT_CALL(*this, ReadCallback(kDataSize));
+  EXPECT_CALL(host_, AddBufferedByteRange(0, kDataSize - 1));
+  ReadAt(0);
+  ReceiveData(kDataSize);
+
+  // Issue a pending read but trigger an error to force a retry.
+  EXPECT_CALL(*this, ReadCallback(kDataSize));
+  EXPECT_CALL(host_, AddBufferedByteRange(kDataSize, (kDataSize * 2) - 1));
+  ReadAt(kDataSize);
+  base::RunLoop run_loop;
+  EXPECT_CALL(*data_source_, CreateResourceLoader(_, _))
+      .WillOnce(
+          DoAll(InvokeWithoutArgs(&run_loop, &base::RunLoop::Quit),
+                Invoke(data_source_.get(),
+                       &MockBufferedDataSource::CreateMockResourceLoader)));
+  loader()->didFail(url_loader(), response_generator_->GenerateError());
+  run_loop.Run();
+  Respond(response_generator_->Generate206(kDataSize));
+  ReceiveData(kDataSize);
+  FinishLoading();
+  EXPECT_FALSE(data_source_->loading());
   Stop();
 }
 
