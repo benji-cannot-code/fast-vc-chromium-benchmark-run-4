575277bf7222b4a306c6d4afcabe332fe5fee030
samuong <samuong@chromium.org>
2014-11-23 20:43:03 -0800

    [chromedriver] Remove touch_single_tap, update atoms d0c863a:f639196
    BUG=
    TBR=stgao@chromium.org
    
    Review URL: https://codereview.chromium.org/731963005
    
    Cr-Commit-Position: refs/heads/master@{#305399}

--- chrome/test/chromedriver/element_commands.cc
+++ chrome/test/chromedriver/element_commands.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/values.h"
 #include "chrome/test/chromedriver/basic_types.h"
-#include "chrome/test/chromedriver/chrome/browser_info.h"
 #include "chrome/test/chromedriver/chrome/chrome.h"
 #include "chrome/test/chromedriver/chrome/js.h"
 #include "chrome/test/chromedriver/chrome/status.h"
@@ -76,21 +75,6 @@ Status SendKeysToElement(
   }
 
   return SendKeysOnWindow(web_view, key_list, true, &session->sticky_modifiers);
-}
-
-Status ExecuteTouchSingleTapAtom(
-    Session* session,
-    WebView* web_view,
-    const std::string& element_id,
-    const base::DictionaryValue& params,
-    scoped_ptr<base::Value>* value) {
-  base::ListValue args;
-  args.Append(CreateElement(element_id));
-  return web_view->CallFunction(
-      session->GetCurrentFrameId(),
-      webdriver::atoms::asString(webdriver::atoms::TOUCH_SINGLE_TAP),
-      args,
-      value);
 }
 
 }  // namespace
@@ -203,11 +187,6 @@ Status ExecuteTouchSingleTap(
     const std::string& element_id,
     const base::DictionaryValue& params,
     scoped_ptr<base::Value>* value) {
-  // Fall back to javascript atom for pre-m30 Chrome.
-  if (session->chrome->GetBrowserInfo()->build_no < 1576)
-    return ExecuteTouchSingleTapAtom(
-        session, web_view, element_id, params, value);
-
   WebPoint location;
   Status status = GetElementClickableLocation(
       session, web_view, element_id, &location);
