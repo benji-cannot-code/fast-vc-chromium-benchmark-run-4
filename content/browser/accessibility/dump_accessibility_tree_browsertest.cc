0201dba62c2be48dfb0133ffa8c126cb54bda193
shreeram.k <shreeram.k@samsung.com>
2014-11-24 10:20:30 -0800

    Disabling AX test for select.html.
    This will be enabled once this change rolls in https://codereview.chromium.org/753123002/
    
    BUG=169564
    
    Review URL: https://codereview.chromium.org/753113003
    
    Cr-Commit-Position: refs/heads/master@{#305446}

--- content/browser/accessibility/dump_accessibility_tree_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_tree_browsertest.cc
@@ -1020,7 +1020,8 @@ IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilitySection) {
   RunTest(FILE_PATH_LITERAL("section.html"));
 }
 
-IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilitySelect) {
+IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest,
+                       DISABLED_AccessibilitySelect) {
   RunTest(FILE_PATH_LITERAL("select.html"));
 }
 
