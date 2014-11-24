177d7b2cd7152d44776753fb3974339d6f1515e9
andersr@opera.com <andersr@opera.com>
2014-11-24 12:22:59 +0000

    Respect fallback colors when interpolating text-decoration-color.
    Currently, we interpolate the incorrect colors if one of the fallback
    colors (other than currentColor) are used for text decoration. This happens
    because we bypass the fallback code when creating the AnimatableValues.
    
    This patch moves the color retrieving code for text decoration into
    the main RenderStyle::colorIncludingCallback function, and changes callers
    to use visitedDependentColor/colorIncludingFallback also for
    CSSPropertyTextDecorationColor. This is better, because you don't have to
    do anything special with the text decoration color compared to all other
    colors.
    
    R=alancutter@chromium.org
    BUG=435546
    
    Review URL: https://codereview.chromium.org/746173002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185870 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/style/RenderStyle.h
+++ third_party/WebKit/Source/core/rendering/style/RenderStyle.h
@@ -1504,8 +1504,6 @@ public:
     bool lastChildState() const { return noninherited_flags.lastChildState; }
     void setLastChildState() { setUnique(); noninherited_flags.lastChildState = true; }
 
-    StyleColor visitedDependentDecorationStyleColor() const;
-    Color visitedDependentDecorationColor() const;
     Color visitedDependentColor(int colorProperty) const;
 
     void setHasExplicitlyInheritedProperties() { noninherited_flags.explicitInheritance = true; }
@@ -1777,6 +1775,7 @@ private:
     StyleColor visitedLinkTextFillColor() const { return rareInheritedData->visitedLinkTextFillColor(); }
     StyleColor visitedLinkTextStrokeColor() const { return rareInheritedData->visitedLinkTextStrokeColor(); }
 
+    StyleColor decorationColorIncludingFallback(bool visitedLink) const;
     Color colorIncludingFallback(int colorProperty, bool visitedLink) const;
 
     Color stopColor() const { return svgStyle().stopColor(); }
