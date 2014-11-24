ef255658f2104c7679bcc06d22b32d56c1037581
treib <treib@chromium.org>
2014-11-24 04:35:48 -0800

    Revert of Revert of Supervised user SafeSites: Expose & use API key from src-internal (patchset #1 id:1 of https://codereview.chromium.org/753683004/)
    Reason for revert:
    Relanding because the original reason for reverting looks like a flake rather than an issue with the CL.
    
    Original issue's description:
    > Revert of Supervised user SafeSites: Expose & use API key from src-internal (patchset #5 id:80001 of https://codereview.chromium.org/690423003/)
    >
    > Reason for revert:
    > Build breakage on Mac:
    >
    > https://build.chromium.org/p/chromium.mac/builders/Mac%20Builder/builds/14318/steps/steps/logs/stdio
    >
    > Original issue's description:
    > > Supervised user SafeSites: Expose & use API key from src-internal
    > >
    > > Key was added to src-internal in https://chromereviews.googleplex.com/113767014/
    > >
    > > BUG=417722
    > >
    > > Committed: https://crrev.com/2ee5f146d57d07bbad3804715e06590b850711f9
    > > Cr-Commit-Position: refs/heads/master@{#305412}
    >
    > TBR=rogerta@chromium.org,bauerb@chromium.org,treib@chromium.org
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=417722
    >
    > Committed: https://crrev.com/13c88c66f60e6364478a411e8ccf0bfd689f0a96
    > Cr-Commit-Position: refs/heads/master@{#305413}
    
    TBR=rogerta@chromium.org,bauerb@chromium.org,tnagel@chromium.org
    NOTRY=true
    BUG=417722
    
    Review URL: https://codereview.chromium.org/756593002
    
    Cr-Commit-Position: refs/heads/master@{#305416}

--- chrome/browser/supervised_user/experimental/supervised_user_async_url_checker.h
+++ chrome/browser/supervised_user/experimental/supervised_user_async_url_checker.h
@@ -33,11 +33,9 @@ class SupervisedUserAsyncURLChecker : net::URLFetcherDelegate {
       CheckCallback;
 
   SupervisedUserAsyncURLChecker(net::URLRequestContextGetter* context,
-                                const std::string& cx,
-                                const std::string& api_key);
+                                const std::string& cx);
   SupervisedUserAsyncURLChecker(net::URLRequestContextGetter* context,
                                 const std::string& cx,
-                                const std::string& api_key,
                                 size_t cache_size);
   virtual ~SupervisedUserAsyncURLChecker();
 
@@ -53,14 +51,11 @@ class SupervisedUserAsyncURLChecker : net::URLFetcherDelegate {
     bool uncertain;
   };
 
-  void SetApiKey(const std::string& api_key);
-
   // net::URLFetcherDelegate implementation.
   virtual void OnURLFetchComplete(const net::URLFetcher* source) override;
 
   net::URLRequestContextGetter* context_;
   std::string cx_;
-  std::string api_key_;
 
   ScopedVector<Check> checks_in_progress_;
 
