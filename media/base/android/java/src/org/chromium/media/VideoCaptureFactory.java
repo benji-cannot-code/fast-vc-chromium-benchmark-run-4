a2fb175ac9011ed847981f1aa22b0873235cda97
mcasas <mcasas@chromium.org>
2014-11-24 15:37:44 -0800

    Android Video Capture: Rename L with LOLLIPOP in Version check.
    BUG=343894
    TBR=qinmin@chromium.org, acleung@chromium.org
    
    Review URL: https://codereview.chromium.org/756773002
    
    Cr-Commit-Position: refs/heads/master@{#305520}

--- media/base/android/java/src/org/chromium/media/VideoCaptureFactory.java
+++ media/base/android/java/src/org/chromium/media/VideoCaptureFactory.java
@@ -90,7 +90,7 @@ class VideoCaptureFactory {
     }
 
     private static boolean isLReleaseOrLater() {
-        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.L;
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
     }
 
     // Factory methods.
