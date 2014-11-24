1e10f3c54c9a52d0daae58b4a9940228934d8505
dcheng <dcheng@chromium.org>
2014-11-24 11:13:43 -0800

    Fix implicit conversion of scoped_refptr<T> to T* in components/nacl
    Since it's generally an anti-pattern to keep a pointer to a
    MessageLoopProxy without ref'ing it, the message_loop_ member is now
    a scoped_refptr<T>.
    
    BUG=110610
    
    Review URL: https://codereview.chromium.org/750263002
    
    Cr-Commit-Position: refs/heads/master@{#305466}

--- components/nacl/common/nacl_debug_exception_handler_win.cc
+++ components/nacl/common/nacl_debug_exception_handler_win.cc
@@ -14,15 +14,15 @@ namespace {
 
 class DebugExceptionHandler : public base::PlatformThread::Delegate {
  public:
-  DebugExceptionHandler(base::ProcessHandle nacl_process,
-                        const std::string& startup_info,
-                        base::MessageLoopProxy* message_loop,
-                        const base::Callback<void(bool)>& on_connected)
+  DebugExceptionHandler(
+      base::ProcessHandle nacl_process,
+      const std::string& startup_info,
+      const scoped_refptr<base::MessageLoopProxy>& message_loop,
+      const base::Callback<void(bool)>& on_connected)
       : nacl_process_(nacl_process),
         startup_info_(startup_info),
         message_loop_(message_loop),
-        on_connected_(on_connected) {
-  }
+        on_connected_(on_connected) {}
 
   virtual void ThreadMain() override {
     // In the Windows API, the set of processes being debugged is
@@ -55,7 +55,7 @@ class DebugExceptionHandler : public base::PlatformThread::Delegate {
  private:
   base::win::ScopedHandle nacl_process_;
   std::string startup_info_;
-  base::MessageLoopProxy* message_loop_;
+  const scoped_refptr<base::MessageLoopProxy> message_loop_;
   base::Callback<void(bool)> on_connected_;
 
   DISALLOW_COPY_AND_ASSIGN(DebugExceptionHandler);
@@ -66,7 +66,7 @@ class DebugExceptionHandler : public base::PlatformThread::Delegate {
 void NaClStartDebugExceptionHandlerThread(
     base::ProcessHandle nacl_process,
     const std::string& startup_info,
-    base::MessageLoopProxy* message_loop,
+    const scoped_refptr<base::MessageLoopProxy>& message_loop,
     const base::Callback<void(bool)>& on_connected) {
   // The new PlatformThread will take ownership of the
   // DebugExceptionHandler object, which will delete itself on exit.
--- components/nacl/common/nacl_debug_exception_handler_win.h
+++ components/nacl/common/nacl_debug_exception_handler_win.h
@@ -7,13 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_NACL_COMMON_NACL_DEBUG_EXCEPTION_HANDLER_WIN_H_
 
 #include "base/callback.h"
+#include "base/memory/ref_counted.h"
 #include "base/message_loop/message_loop.h"
 #include "base/process/process.h"
 
 void NaClStartDebugExceptionHandlerThread(
     base::ProcessHandle nacl_process,
     const std::string& startup_info,
-    base::MessageLoopProxy* message_loop,
+    const scoped_refptr<base::MessageLoopProxy>& message_loop,
     const base::Callback<void(bool)>& on_connected);
 
 #endif  // COMPONENTS_NACL_COMMON_NACL_DEBUG_EXCEPTION_HANDLER_WIN_H_
