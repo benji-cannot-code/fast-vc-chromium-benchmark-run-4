7e24d301b13369c31748254f3579c09d93d50d07
chrishtr@chromium.org <chrishtr@chromium.org>
2014-11-24 23:50:43 +0000

    Remove non-clip painting code from BoxClipper.
    This allows us to simplify some surrounding code as well.
    
    Review URL: https://codereview.chromium.org/747533002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185908 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/paint/BoxClipper.cpp
+++ third_party/WebKit/Source/core/paint/BoxClipper.cpp
@@ -16,11 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-BoxClipper::BoxClipper(RenderBox& box, PaintInfo& paintInfo, const LayoutPoint& accumulatedOffset, ContentsClipBehavior contentsClipBehavior)
+BoxClipper::BoxClipper(RenderBox& box, const PaintInfo& paintInfo, const LayoutPoint& accumulatedOffset, ContentsClipBehavior contentsClipBehavior)
     : m_pushedClip(false)
     , m_accumulatedOffset(accumulatedOffset)
     , m_paintInfo(paintInfo)
-    , m_originalPhase(paintInfo.phase)
     , m_box(box)
 {
     if (m_paintInfo.phase == PaintPhaseBlockBackground || m_paintInfo.phase == PaintPhaseSelfOutline || m_paintInfo.phase == PaintPhaseMask)
@@ -51,15 +50,6 @@ BoxClipper::BoxClipper(RenderBox& box, PaintInfo& paintInfo, const LayoutPoint& 
             conservativeClipRect.move(m_box.scrolledContentOffset());
         if (conservativeClipRect.contains(contentsVisualOverflow))
             return;
-    }
-
-    if (m_paintInfo.phase == PaintPhaseOutline) {
-        m_paintInfo.phase = PaintPhaseChildOutlines;
-    } else if (m_paintInfo.phase == PaintPhaseChildBlockBackground) {
-        m_paintInfo.phase = PaintPhaseBlockBackground;
-        // FIXME: refactor to not call paintObject from within a clipping helper.
-        m_box.paintObject(m_paintInfo, m_accumulatedOffset);
-        m_paintInfo.phase = PaintPhaseChildBlockBackgrounds;
     }
 
     DisplayItem::Type clipType = DisplayItem::ClipBoxForeground;
@@ -123,15 +113,6 @@ BoxClipper::~BoxClipper()
         m_box.view()->viewDisplayList().add(endClipDisplayItem.release());
     } else {
         endClipDisplayItem->replay(m_paintInfo.context);
-    }
-
-    // FIXME: refactor to not call paintObject from within a clipping helper.
-    if (m_originalPhase == PaintPhaseOutline) {
-        m_paintInfo.phase = PaintPhaseSelfOutline;
-        m_box.paintObject(m_paintInfo, m_accumulatedOffset);
-        m_paintInfo.phase = m_originalPhase;
-    } else if (m_originalPhase == PaintPhaseChildBlockBackground) {
-        m_paintInfo.phase = m_originalPhase;
     }
 }
 
