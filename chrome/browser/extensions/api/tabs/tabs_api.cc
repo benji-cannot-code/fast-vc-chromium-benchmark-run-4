d7657b140f5cb9dac9a6b2b4d231e11737aaa340
dgozman <dgozman@chromium.org>
2014-11-24 09:34:04 -0800

    Make undocked DevTools window invisible to extensions API.
    BUG=427226
    
    Review URL: https://codereview.chromium.org/734983003
    
    Cr-Commit-Position: refs/heads/master@{#305440}

--- chrome/browser/extensions/api/tabs/tabs_api.cc
+++ chrome/browser/extensions/api/tabs/tabs_api.cc
@@ -856,6 +856,11 @@ bool TabsQueryFunction::RunSync() {
     if (!include_incognito() && GetProfile() != browser->profile())
       continue;
 
+    if (!browser->extension_window_controller()->IsVisibleToExtension(
+            extension())) {
+      continue;
+    }
+
     if (window_id >= 0 && window_id != ExtensionTabUtil::GetWindowId(browser))
       continue;
 
