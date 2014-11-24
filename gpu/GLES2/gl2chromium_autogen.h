3b1c8c4ab78a38f08860792b7063a3bc1c9f6835
zmo <zmo@chromium.org>
2014-11-24 15:52:34 -0800

    Add a second unsafe ES3 API in command buffer.
    glCopuBufferSubData
    
    Also, I disabled validator for unsafe functions.
    
    BUG=429053
    TEST=gpu_unittests
    R=bajones@chromium.org
    TBR=sievers@chromium.org
    
    Review URL: https://codereview.chromium.org/754873002
    
    Cr-Commit-Position: refs/heads/master@{#305522}

--- gpu/GLES2/gl2chromium_autogen.h
+++ gpu/GLES2/gl2chromium_autogen.h
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define glCompileShader GLES2_GET_FUN(CompileShader)
 #define glCompressedTexImage2D GLES2_GET_FUN(CompressedTexImage2D)
 #define glCompressedTexSubImage2D GLES2_GET_FUN(CompressedTexSubImage2D)
+#define glCopyBufferSubData GLES2_GET_FUN(CopyBufferSubData)
 #define glCopyTexImage2D GLES2_GET_FUN(CopyTexImage2D)
 #define glCopyTexSubImage2D GLES2_GET_FUN(CopyTexSubImage2D)
 #define glCreateProgram GLES2_GET_FUN(CreateProgram)
--- gpu/command_buffer/client/gles2_c_lib_autogen.h
+++ gpu/command_buffer/client/gles2_c_lib_autogen.h
@@ -117,6 +117,14 @@ void GLES2CompressedTexSubImage2D(GLenum target,
   gles2::GetGLContext()->CompressedTexSubImage2D(
       target, level, xoffset, yoffset, width, height, format, imageSize, data);
 }
+void GLES2CopyBufferSubData(GLenum readtarget,
+                            GLenum writetarget,
+                            GLintptr readoffset,
+                            GLintptr writeoffset,
+                            GLsizeiptr size) {
+  gles2::GetGLContext()->CopyBufferSubData(readtarget, writetarget, readoffset,
+                                           writeoffset, size);
+}
 void GLES2CopyTexImage2D(GLenum target,
                          GLint level,
                          GLenum internalformat,
@@ -1061,6 +1069,10 @@ extern const NameToFunc g_gles2_function_table[] = {
     {
      "glCompressedTexSubImage2D",
      reinterpret_cast<GLES2FunctionPointer>(glCompressedTexSubImage2D),
+    },
+    {
+     "glCopyBufferSubData",
+     reinterpret_cast<GLES2FunctionPointer>(glCopyBufferSubData),
     },
     {
      "glCopyTexImage2D",
