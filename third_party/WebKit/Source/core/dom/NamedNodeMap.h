ce090fbb715172ef333f59469ffeee933825da1b
philipj@opera.com <philipj@opera.com>
2014-11-25 20:54:10 +0000

    Let NamedNodeMap take and return Attr instead of Node
    https://dom.spec.whatwg.org/#namednodemap
    
    The Attr arguments are also made non-optional.
    
    This makes for less error handling in C++.
    
    R=mkwst@chromium.org
    
    Review URL: https://codereview.chromium.org/755213002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185973 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/NamedNodeMap.h
+++ third_party/WebKit/Source/core/dom/NamedNodeMap.h
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class Node;
+class Attr;
 class ExceptionState;
 
 class NamedNodeMap final : public NoBaseWillBeGarbageCollected<NamedNodeMap>, public ScriptWrappable {
@@ -54,16 +54,16 @@ public:
 
     // Public DOM interface.
 
-    PassRefPtrWillBeRawPtr<Node> getNamedItem(const AtomicString&) const;
-    PassRefPtrWillBeRawPtr<Node> removeNamedItem(const AtomicString& name, ExceptionState&);
+    PassRefPtrWillBeRawPtr<Attr> getNamedItem(const AtomicString&) const;
+    PassRefPtrWillBeRawPtr<Attr> removeNamedItem(const AtomicString& name, ExceptionState&);
 
-    PassRefPtrWillBeRawPtr<Node> getNamedItemNS(const AtomicString& namespaceURI, const AtomicString& localName) const;
-    PassRefPtrWillBeRawPtr<Node> removeNamedItemNS(const AtomicString& namespaceURI, const AtomicString& localName, ExceptionState&);
+    PassRefPtrWillBeRawPtr<Attr> getNamedItemNS(const AtomicString& namespaceURI, const AtomicString& localName) const;
+    PassRefPtrWillBeRawPtr<Attr> removeNamedItemNS(const AtomicString& namespaceURI, const AtomicString& localName, ExceptionState&);
 
-    PassRefPtrWillBeRawPtr<Node> setNamedItem(Node*, ExceptionState&);
-    PassRefPtrWillBeRawPtr<Node> setNamedItemNS(Node*, ExceptionState&);
+    PassRefPtrWillBeRawPtr<Attr> setNamedItem(Attr*, ExceptionState&);
+    PassRefPtrWillBeRawPtr<Attr> setNamedItemNS(Attr*, ExceptionState&);
 
-    PassRefPtrWillBeRawPtr<Node> item(unsigned index) const;
+    PassRefPtrWillBeRawPtr<Attr> item(unsigned index) const;
     size_t length() const;
 
     Element* element() const { return m_element; }
