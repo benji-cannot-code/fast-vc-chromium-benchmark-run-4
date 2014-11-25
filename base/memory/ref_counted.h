1073b83953f216a4488621d74c6b546b69e087f5
dcheng <dcheng@chromium.org>
2014-11-24 17:49:27 -0800

    Disable implicit conversion of scoped_refptr<T> to T* on Windows.
    Finally!!!
    
    BUG=110610
    
    Review URL: https://codereview.chromium.org/748073004
    
    Cr-Commit-Position: refs/heads/master@{#305558}

--- base/memory/ref_counted.h
+++ base/memory/ref_counted.h
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_collision_warner.h"
 #include "build/build_config.h"
 
-#if defined(OS_LINUX) || defined(OS_MACOSX) || defined(OS_IOS) || defined(OS_ANDROID)
+#if defined(OS_LINUX) || defined(OS_MACOSX) || defined(OS_IOS) || defined(OS_ANDROID) || defined(OS_WIN)
 #define DISABLE_SCOPED_REFPTR_CONVERSION_OPERATOR
 #endif
 
