46d0e48f43b52d09b8184f263e6aa2a3f75e62f7
chrishtr@chromium.org <chrishtr@chromium.org>
2014-11-25 00:12:00 +0000

    Fix use-after-free in BeginTransformDisplayItem.
    It was referring by reference to a stack-allocated variable.
    Instead, copy it.
    
    BUG=436029
    
    Review URL: https://codereview.chromium.org/752223003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185910 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/paint/TransformDisplayItem.h
+++ third_party/WebKit/Source/core/paint/TransformDisplayItem.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define TransformDisplayItem_h
 
 #include "core/paint/ViewDisplayList.h"
+#include "platform/transforms/TransformationMatrix.h"
 
 namespace blink {
 
@@ -20,7 +21,7 @@ public:
 #ifndef NDEBUG
     virtual WTF::String asDebugString() const override;
 #endif
-    const TransformationMatrix& m_transform;
+    const TransformationMatrix m_transform;
 };
 
 class EndTransformDisplayItem : public DisplayItem {
