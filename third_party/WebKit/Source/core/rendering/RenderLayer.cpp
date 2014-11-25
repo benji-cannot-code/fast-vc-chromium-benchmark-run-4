f00ef1336f0ebeef066e664a937afa6b720c51a6
eae@chromium.org <eae@chromium.org>
2014-11-25 16:04:19 +0000

    Rect/Size/Point cleanup for FrameView, RenderBox, and RenderLayer
    Further Rect/Size/Point cleanup in Source/core to avoid unnecessary work
    (like computing a rect when only the location is needed) and unnecessary
    type conversions between points and sizes. All remaining call sites that
    call [rect]().location() do no meaningful work in the rect call and just
    return a pre-computed rectangle.
    
    BUG=432192
    R=pdr@chromium.org
    
    Review URL: https://codereview.chromium.org/759443002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185951 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderLayer.cpp
+++ third_party/WebKit/Source/core/rendering/RenderLayer.cpp
@@ -1355,7 +1355,7 @@ static inline const RenderLayer* accumulateOffsetTowardsAncestor(const RenderLay
 
             location += (fixedContainerCoords - ancestorCoords);
         } else {
-            location += toSize(layer->location());
+            location += layer->location();
         }
         return ancestorLayer;
     }
@@ -1404,7 +1404,7 @@ static inline const RenderLayer* accumulateOffsetTowardsAncestor(const RenderLay
     if (!parentLayer)
         return 0;
 
-    location += toSize(layer->location());
+    location += layer->location();
     return parentLayer;
 }
 
