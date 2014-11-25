2f58b47f36e56a6f6f25ba074f5411b8296e1fd3
deepak.m1 <deepak.m1@samsung.com>
2014-11-24 22:11:36 -0800

    Preserve IME state when clearing the selection
    When a paste or cut operation is performed in an editable region, and
    the action bar is dismissed, the IME state should be preserved. Ensure
    this by always using the editable region bit when clearing the selection
    upon action bar dismissal.
    
    BUG=435810
    
    Review URL: https://codereview.chromium.org/747773003
    
    Cr-Commit-Position: refs/heads/master@{#305594}

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -1328,7 +1328,7 @@ public class ContentViewCore
     }
 
     private void clearUserSelection() {
-        if (isSelectionEditable()) {
+        if (mFocusedNodeEditable) {
             if (mInputConnection != null) {
                 int selectionEnd = Selection.getSelectionEnd(mEditable);
                 mInputConnection.setSelection(selectionEnd, selectionEnd);
