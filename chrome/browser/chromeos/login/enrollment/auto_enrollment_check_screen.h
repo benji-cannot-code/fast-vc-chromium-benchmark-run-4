6a00b7a4237634c0b4fa1798d42a2472a0e0eb44
dzhioev <dzhioev@chromium.org>
2014-11-25 05:42:39 -0800

    Prevented race in auto enrollment check screen.
    There was an race in AutoEnrollmentCheckScreen::Show.
    Sometimes Start() makes decision and notifies WizardController about completion
    synchronously, which in turn activates the next screep of OOBE. Only after that
    control returns to AutoEnrollmentCheckScreen::Show and it shows auto enrollment
    check screen. We shouldn't show the screen in this case.
    
    BUG=423771
    TEST=manually
    
    Review URL: https://codereview.chromium.org/724123002
    
    Cr-Commit-Position: refs/heads/master@{#305631}

--- chrome/browser/chromeos/login/enrollment/auto_enrollment_check_screen.h
+++ chrome/browser/chromeos/login/enrollment/auto_enrollment_check_screen.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "chrome/browser/chromeos/login/enrollment/auto_enrollment_check_screen_actor.h"
 #include "chrome/browser/chromeos/login/enrollment/auto_enrollment_controller.h"
 #include "chrome/browser/chromeos/login/screens/error_screen.h"
@@ -85,9 +86,13 @@ class AutoEnrollmentCheckScreen
   // Configures the error screen.
   void ShowErrorScreen(ErrorScreen::ErrorState error_state);
 
-  // Signals completion. No further code should run after a call to this
-  // function as the owner might destroy |this| in response.
+  // Asynchronously signals completion. The owner might destroy |this| in
+  // response, so no code should be run after the completion of a message loop
+  // task, in which this function was called.
   void SignalCompletion();
+
+  // Returns whether enrollment check was completed and decision was made.
+  bool IsCompleted() const;
 
   // Checks if the enrollment status check is needed. It can be disabled either
   // by command line flags, build configuration or might have finished already.
@@ -103,6 +108,8 @@ class AutoEnrollmentCheckScreen
   policy::AutoEnrollmentState auto_enrollment_state_;
 
   scoped_ptr<ErrorScreensHistogramHelper> histogram_helper_;
+
+  base::WeakPtrFactory<AutoEnrollmentCheckScreen> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(AutoEnrollmentCheckScreen);
 };
