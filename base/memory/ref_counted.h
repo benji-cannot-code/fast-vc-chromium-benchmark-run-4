229671e11ffc5c4901b07152dd102513b7528d29
dcheng <dcheng@chromium.org>
2014-11-24 21:40:56 -0800

    Remove unneeded #define in ref_counted.h
    Now that the implicit conversion to T* can be removed, the preprocessor
    define DISABLE_SCOPED_REFPTR_CONVERSION_OPERATOR is no longer needed.
    
    BUG=110610
    
    Review URL: https://codereview.chromium.org/759503002
    
    Cr-Commit-Position: refs/heads/master@{#305591}

--- base/memory/ref_counted.h
+++ base/memory/ref_counted.h
@@ -18,10 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_collision_warner.h"
 #include "build/build_config.h"
 
-#if defined(OS_LINUX) || defined(OS_MACOSX) || defined(OS_IOS) || defined(OS_ANDROID) || defined(OS_WIN)
-#define DISABLE_SCOPED_REFPTR_CONVERSION_OPERATOR
-#endif
-
 namespace base {
 
 namespace subtle {
@@ -298,12 +294,6 @@ class scoped_refptr {
 
   T* get() const { return ptr_; }
 
-#if !defined(DISABLE_SCOPED_REFPTR_CONVERSION_OPERATOR)
-  // Allow scoped_refptr<C> to be used in boolean expression
-  // and comparison operations.
-  operator T*() const { return ptr_; }
-#endif
-
   T& operator*() const {
     assert(ptr_ != NULL);
     return *ptr_;
@@ -344,7 +334,6 @@ class scoped_refptr {
     swap(&r.ptr_);
   }
 
-#if defined(DISABLE_SCOPED_REFPTR_CONVERSION_OPERATOR)
   template <typename U>
   bool operator==(const scoped_refptr<U>& rhs) const {
     return ptr_ == rhs.get();
@@ -359,7 +348,6 @@ class scoped_refptr {
   bool operator<(const scoped_refptr<U>& rhs) const {
     return ptr_ < rhs.get();
   }
-#endif
 
  protected:
   T* ptr_;
@@ -390,8 +378,8 @@ scoped_refptr<T> make_scoped_refptr(T* t) {
   return scoped_refptr<T>(t);
 }
 
-#if defined(DISABLE_SCOPED_REFPTR_CONVERSION_OPERATOR)
-// Temporary operator overloads to facilitate the transition...
+// Temporary operator overloads to facilitate the transition. See
+// https://crbug.com/110610.
 template <typename T, typename U>
 bool operator==(const scoped_refptr<T>& lhs, const U* rhs) {
   return lhs.get() == rhs;
@@ -416,6 +404,5 @@ template <typename T>
 std::ostream& operator<<(std::ostream& out, const scoped_refptr<T>& p) {
   return out << p.get();
 }
-#endif  // defined(DISABLE_SCOPED_REFPTR_CONVERSION_OPERATOR)
 
 #endif  // BASE_MEMORY_REF_COUNTED_H_
