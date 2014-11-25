c024302895e290f10ed5b231f87d587fcde4884a
aurimas <aurimas@chromium.org>
2014-11-25 11:06:42 -0800

    Start reading checked state for image buttons in the menu.
    BUG=436286
    
    Review URL: https://codereview.chromium.org/753333003
    
    Cr-Commit-Position: refs/heads/master@{#305670}

--- chrome/android/java/src/org/chromium/chrome/browser/appmenu/AppMenuAdapter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/appmenu/AppMenuAdapter.java
@@ -16,7 +16,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ImageView.ScaleType;
 import android.widget.LinearLayout;
@@ -315,12 +314,15 @@ class AppMenuAdapter extends BaseAdapter {
         return convertView;
     }
 
-    private void setupImageButton(ImageButton button, final MenuItem item) {
+    private void setupImageButton(TintedImageButton button, final MenuItem item) {
         // Store and recover the level of image as button.setimageDrawable
         // resets drawable to default level.
         int currentLevel = item.getIcon().getLevel();
         button.setImageDrawable(item.getIcon());
         item.getIcon().setLevel(currentLevel);
+        if (item.isChecked()) {
+            button.setTint(button.getResources().getColorStateList(R.color.blue_mode_tint));
+        }
         button.setEnabled(item.isEnabled());
         button.setFocusable(item.isEnabled());
         button.setContentDescription(item.getTitleCondensed());
