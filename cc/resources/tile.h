8a532dede95546b836634993aeb64227f8f4aa76
vmpstr <vmpstr@chromium.org>
2014-11-25 13:24:51 -0800

    cc: Clean-up of ManageTileState
    This patch removes some unused functionality that was left over from
    when iterators went in.
    
    R=reveman
    
    Review URL: https://codereview.chromium.org/760673002
    
    Cr-Commit-Position: refs/heads/master@{#305698}

--- cc/resources/tile.h
+++ cc/resources/tile.h
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cc {
 
+class TileManager;
+
 class CC_EXPORT Tile : public RefCountedManaged<Tile> {
  public:
   enum TileRasterFlags { USE_PICTURE_ANALYSIS = 1 << 0 };
