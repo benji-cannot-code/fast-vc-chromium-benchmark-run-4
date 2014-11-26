175ce009250815eae7c39570cddcf00fc48d0a97
dmazzoni <dmazzoni@chromium.org>
2014-11-25 16:26:53 -0800

    Only load inline text boxes for the node that has accessibility focus.
    This is a significant optimization for Android accessibility; avoiding
    loading inline text boxes on the whole tree makes page loads 20% faster,
    but we can still get all of the benefit by tracking which node has
    accessibility focus and loading inline text boxes for only that subtree,
    asynchronously.
    
    Depends on: https://codereview.chromium.org/700373007
    
    BUG=378810
    
    Review URL: https://codereview.chromium.org/743413002
    
    Cr-Commit-Position: refs/heads/master@{#305737}

--- content/browser/accessibility/browser_accessibility_manager_android.cc
+++ content/browser/accessibility/browser_accessibility_manager_android.cc
@@ -123,6 +123,9 @@ void BrowserAccessibilityManagerAndroid::NotifyAccessibilityEvent(
   if (event_type == ui::AX_EVENT_HIDE)
     return;
 
+  if (event_type == ui::AX_EVENT_TREE_CHANGED)
+    return;
+
   if (event_type == ui::AX_EVENT_HOVER) {
     HandleHoverEvent(node);
     return;
@@ -714,6 +717,12 @@ bool BrowserAccessibilityManagerAndroid::PreviousAtGranularity(
   }
 
   return true;
+}
+
+void BrowserAccessibilityManagerAndroid::SetAccessibilityFocus(
+    JNIEnv* env, jobject obj, jint id) {
+  if (delegate_)
+    delegate_->AccessibilitySetAccessibilityFocus(id);
 }
 
 void BrowserAccessibilityManagerAndroid::OnRootChanged(ui::AXNode* new_root) {
