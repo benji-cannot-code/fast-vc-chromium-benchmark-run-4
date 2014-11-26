c3547fc2573bc2932a9455a77926f96008f63ebf
alph@chromium.org <alph@chromium.org>
2014-11-26 17:10:28 +0000

    DevTools: Promisify Profiler domain in the protocol
    Review URL: https://codereview.chromium.org/747033003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186040 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/profiler/CPUProfileView.js
+++ third_party/WebKit/Source/devtools/front_end/profiler/CPUProfileView.js
@@ -524,11 +524,10 @@ WebInspector.CPUProfileType.prototype = {
             return;
 
         /**
-         * @param {?string} error
-         * @param {?ProfilerAgent.CPUProfile} profile
+         * @param {!ProfilerAgent.CPUProfile} profile
          * @this {WebInspector.CPUProfileType}
          */
-        function didStopProfiling(error, profile)
+        function didStopProfiling(profile)
         {
             if (!this._profileBeingRecorded)
                 return;
@@ -538,7 +537,7 @@ WebInspector.CPUProfileType.prototype = {
             this.setProfileBeingRecorded(null);
             this.dispatchEventToListeners(WebInspector.ProfileType.Events.ProfileComplete, recordedProfile);
         }
-        this._profileBeingRecorded.target().cpuProfilerModel.stopRecording(didStopProfiling.bind(this));
+        this._profileBeingRecorded.target().cpuProfilerModel.stopRecording().then(didStopProfiling.bind(this));
     },
 
     /**
