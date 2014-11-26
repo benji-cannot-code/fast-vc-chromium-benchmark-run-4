c42823ed2dbd6dfbd25e0c05f8e12c649534fcb5
jln <jln@chromium.org>
2014-11-26 14:38:35 -0800

    Linux sandbox: Use ScopedFD in SandboxBPF
    Use a ScopedFD to hold a file descriptor to /proc/self/task in
    the SandboxBPF class.
    
    BUG=434820
    
    Review URL: https://codereview.chromium.org/725343005
    
    Cr-Commit-Position: refs/heads/master@{#305906}

--- sandbox/linux/sandbox_linux_test_sources.gypi
+++ sandbox/linux/sandbox_linux_test_sources.gypi
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'seccomp-bpf/bpf_tests_unittest.cc',
         'seccomp-bpf/codegen_unittest.cc',
         'seccomp-bpf/errorcode_unittest.cc',
+        'seccomp-bpf/sandbox_bpf_unittest.cc',
         'seccomp-bpf/syscall_iterator_unittest.cc',
         'seccomp-bpf/syscall_unittest.cc',
       ],
--- sandbox/linux/seccomp-bpf/sandbox_bpf.h
+++ sandbox/linux/seccomp-bpf/sandbox_bpf.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include "base/files/scoped_file.h"
 #include "base/macros.h"
 #include "base/memory/scoped_ptr.h"
 #include "sandbox/linux/seccomp-bpf/codegen.h"
@@ -69,10 +70,11 @@ class SANDBOX_EXPORT SandboxBPF {
   // The sandbox needs to be able to access files in "/proc/self/task/". If
   // this directory is not accessible when "StartSandbox()" gets called, the
   // caller must provide an already opened file descriptor by calling
-  // "set_proc_task_fd()".
+  // "SetProcTaskFd()".
   // The sandbox becomes the new owner of this file descriptor and will
-  // eventually close it when "StartSandbox()" executes.
-  void set_proc_task_fd(int proc_task_fd);
+  // close it when "StartSandbox()" executes or when the sandbox object
+  // disappears.
+  void SetProcTaskFd(base::ScopedFD proc_task_fd);
 
   // Checks whether a particular system call number is valid on the current
   // architecture.
@@ -107,7 +109,7 @@ class SANDBOX_EXPORT SandboxBPF {
   // been configured with SetSandboxPolicy().
   void InstallFilter(bool must_sync_threads);
 
-  int proc_task_fd_;
+  base::ScopedFD proc_task_fd_;
   bool sandbox_has_started_;
   scoped_ptr<bpf_dsl::Policy> policy_;
 
