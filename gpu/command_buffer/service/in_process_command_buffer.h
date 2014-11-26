dd0a21cb9a443c243ba8a39736e46228ce4b40ea
boliu <boliu@chromium.org>
2014-11-25 20:36:22 -0800

    gpu: Make in-process default service global
    This is a minor clean up to avoid creating/destroying multiple gpu
    threads when using the in-process command buffer. Removes some
    complexity in code and keeps a consistent thread id, just like the
    cross-process command buffer.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/727093004
    
    Cr-Commit-Position: refs/heads/master@{#305782}

--- gpu/command_buffer/service/in_process_command_buffer.h
+++ gpu/command_buffer/service/in_process_command_buffer.h
@@ -208,8 +208,6 @@ class GPU_EXPORT InProcessCommandBuffer : public CommandBuffer,
   void PumpCommands();
   void PerformIdleWork();
 
-  static scoped_refptr<Service> GetDefaultService();
-
   // Members accessed on the gpu thread (possibly with the exception of
   // creation):
   bool context_lost_;
