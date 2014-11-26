7e49523b3276b15bcd6c2a0dca5ad297a0078d0f
kevers <kevers@chromium.org>
2014-11-26 10:55:55 -0800

    Fix emoji support for the system VK.
    BUG=436630
    
    Review URL: https://codereview.chromium.org/760743002
    
    Cr-Commit-Position: refs/heads/master@{#305844}

--- third_party/google_input_tools/src/chrome/os/inputview/elements/layout/extendedlayout.js
+++ third_party/google_input_tools/src/chrome/os/inputview/elements/layout/extendedlayout.js
@@ -127,7 +127,7 @@ ExtendedLayout.prototype.resize = function(width, height) {
         this.getChildAt(i));
     child.resize(width, height);
   }
-  this.getElement().style.width = width * this.getChildCount();
+  this.getElement().style.width = width * this.getChildCount() + 'px';
   goog.base(this, 'resize', width * this.getChildCount(), height);
 };
 
@@ -157,7 +157,7 @@ ExtendedLayout.prototype.calculate_ = function() {
 ExtendedLayout.prototype.gotoPage = function(pageNum) {
   var width = goog.style.getSize(this.getElement()).width;
   var childNum = this.getChildCount();
-  this.elem.style.marginLeft = 0 - width / childNum * pageNum;
+  this.elem.style.marginLeft = 0 - width / childNum * pageNum + 'px';
 };
 
 
--- ui/keyboard/resources/inputview_adapter.js
+++ ui/keyboard/resources/inputview_adapter.js
@@ -173,6 +173,10 @@ function registerInputviewApi() {
     }
   }
 
+  function commitText_(text) {
+    chrome.virtualKeyboardPrivate.insertText(text, logIfError_);
+  }
+
   /**
    * Retrieve the preferred keyboard configuration.
    * @param {function} callback The callback function for processing the
@@ -278,6 +282,7 @@ function registerInputviewApi() {
   }
 
   window.inputview = {
+    commitText: commitText_,
     getKeyboardConfig: getKeyboardConfig_,
     getInputMethods: getInputMethods_,
     getCurrentInputMethod: getCurrentInputMethod_,
@@ -294,6 +299,8 @@ function registerInputviewApi() {
   registerFunction('chrome.runtime.sendMessage', function(message) {
     if (message.type == 'send_key_event')
       sendKeyEvent_(message.keyData);
+    else if (message.type == 'commit_text')
+      commitText_(message.text);
     else
       defaultSendMessage(message);
   });
