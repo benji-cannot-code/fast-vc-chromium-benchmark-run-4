1ec3002b6d1219233d2564d11536f74218065245
dzhioev <dzhioev@chromium.org>
2014-11-26 06:18:22 -0800

    Enrollment logic was extracted from EnrollmentScreen(Handler) in a helper class.
    The new class contains the code that retrieves tokens from
    profile (moved from EnrollmentScreenHandler), the code that starts
    enrollment given a token (moved from EnrollmentScreen), the code
    that reports UMA related to tokens fetch and enrollment (moved from
    EnrollmentScreen) and the code that cleans profile of authentication
    data and revokes tokens if enrollment failed for some reason (moved
    from EnrollmentScreenHandler).
    
    EnterpriseEnrollmentHelper provides two entry points:
    * EnrollUsingContext, that receives profile containing authentication data.
    * EnrollUsingToken, that receives authentication token.
    
    The main reason for this change, is that enrollment functionality
    is needed during host/controller pairing. So if we want to avoid
    code duplication, we need to extract enrollment code somewhere.
    
    The second reason, EnrollmentScreenHandler is supposed to be a purely UI class.
    
    This CL doesn't change behavior, everything is supposed to work as it worked before.
    
    BUG=405150
    TEST=manually,enrollment works,hotrod enrollment works
    
    Review URL: https://codereview.chromium.org/518513002
    
    Cr-Commit-Position: refs/heads/master@{#305807}

--- chrome/browser/ui/webui/chromeos/login/enrollment_screen_handler.h
+++ chrome/browser/ui/webui/chromeos/login/enrollment_screen_handler.h
@@ -7,22 +7,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_WEBUI_CHROMEOS_LOGIN_ENROLLMENT_SCREEN_HANDLER_H_
 
 #include <string>
-#include <vector>
 
 #include "base/basictypes.h"
-#include "base/callback_forward.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/scoped_vector.h"
-#include "chrome/browser/browsing_data/browsing_data_remover.h"
 #include "chrome/browser/chromeos/login/enrollment/enrollment_screen_actor.h"
+#include "chrome/browser/chromeos/login/enrollment/enterprise_enrollment_helper.h"
 #include "chrome/browser/chromeos/login/ui/webui_login_view.h"
 #include "chrome/browser/extensions/signin/scoped_gaia_auth_extension.h"
 #include "chrome/browser/ui/webui/chromeos/login/base_screen_handler.h"
 #include "chrome/browser/ui/webui/chromeos/login/network_state_informer.h"
-
-namespace policy {
-class PolicyOAuth2TokenFetcher;
-}
 
 namespace chromeos {
 
@@ -33,7 +27,6 @@ class ErrorScreensHistogramHelper;
 class EnrollmentScreenHandler
     : public BaseScreenHandler,
       public EnrollmentScreenActor,
-      public BrowsingDataRemover::Observer,
       public NetworkStateInformer::NetworkStateInformerObserver,
       public WebUILoginView::FrameObserver {
  public:
@@ -52,21 +45,17 @@ class EnrollmentScreenHandler
   virtual void PrepareToShow() override;
   virtual void Show() override;
   virtual void Hide() override;
-  virtual void FetchOAuthToken() override;
-  virtual void ResetAuth(const base::Closure& callback) override;
   virtual void ShowSigninScreen() override;
   virtual void ShowEnrollmentSpinnerScreen() override;
   virtual void ShowLoginSpinnerScreen() override;
   virtual void ShowAuthError(const GoogleServiceAuthError& error) override;
   virtual void ShowEnrollmentStatus(policy::EnrollmentStatus status) override;
-  virtual void ShowUIError(UIError error_code) override;
+  virtual void ShowOtherError(
+      EnterpriseEnrollmentHelper::OtherError error_code) override;
 
   // Implements BaseScreenHandler:
   virtual void Initialize() override;
   virtual void DeclareLocalizedValues(LocalizedValuesBuilder* builder) override;
-
-  // Implements BrowsingDataRemover::Observer:
-  virtual void OnBrowsingDataRemoverDone() override;
 
   // Implements NetworkStateInformer::NetworkStateInformerObserver
   virtual void UpdateState(ErrorScreenActor::ErrorReason reason) override;
@@ -99,10 +88,6 @@ class EnrollmentScreenHandler
   // Display the given i18n string as a progress message.
   void ShowWorking(int message_id);
 
-  // Handles completion of the OAuth2 token fetch attempt.
-  void OnTokenFetched(const std::string& token,
-                      const GoogleServiceAuthError& error);
-
   // Shows the screen.
   void DoShow();
 
@@ -129,16 +114,6 @@ class EnrollmentScreenHandler
 
   // Whether an enrollment attempt has failed.
   bool enrollment_failed_once_;
-
-  // This intentionally lives here and not in the controller, since it needs to
-  // execute requests in the context of the profile that displays the webui.
-  scoped_ptr<policy::PolicyOAuth2TokenFetcher> oauth_fetcher_;
-
-  // The browsing data remover instance currently active, if any.
-  BrowsingDataRemover* browsing_data_remover_;
-
-  // The callbacks to invoke after browsing data has been cleared.
-  std::vector<base::Closure> auth_reset_callbacks_;
 
   // Latest enrollment frame error.
   net::Error frame_error_;
