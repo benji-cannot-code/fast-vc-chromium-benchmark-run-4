23cbf584c3a907ad68a23f5bde15bf9ae0c7ef11
mlerman <mlerman@chromium.org>
2014-11-26 14:10:53 -0800

    Temporarily disable extensions and sync while a profile is locked.
    Place all the heavy lifting and logic in profiles code, with extensions providing a new collection for holding "locked" extensions.
    
    This replaces CL 697143003.
    
    BUG=354124
    
    Review URL: https://codereview.chromium.org/695133005
    
    Cr-Commit-Position: refs/heads/master@{#305897}

--- chrome/browser/profiles/profile_window.cc
+++ chrome/browser/profiles/profile_window.cc
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_avatar_icon_util.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/signin/account_reconcilor_factory.h"
+#include "chrome/browser/sync/profile_sync_service.h"
+#include "chrome/browser/sync/profile_sync_service_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_dialogs.h"
 #include "chrome/browser/ui/profile_chooser_constants.h"
@@ -29,6 +31,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/core/common/profile_management_switches.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/user_metrics.h"
+
+#if defined(ENABLE_EXTENSIONS)
+#include "chrome/browser/extensions/extension_service.h"
+#include "extensions/browser/extension_prefs.h"
+#include "extensions/browser/extension_registry.h"
+#include "extensions/browser/extension_registry_factory.h"
+#include "extensions/browser/extension_system.h"
+#endif  // defined(ENABLE_EXTENSIONS)
 
 #if !defined(OS_IOS)
 #include "chrome/browser/ui/browser_finder.h"
@@ -45,6 +55,20 @@ namespace {
 
 const char kNewProfileManagementExperimentInternalName[] =
     "enable-new-profile-management";
+
+#if defined(ENABLE_EXTENSIONS)
+void BlockExtensions(Profile* profile) {
+  ExtensionService* extension_service =
+      extensions::ExtensionSystem::Get(profile)->extension_service();
+  extension_service->BlockAllExtensions();
+}
+
+void UnblockExtensions(Profile* profile) {
+  ExtensionService* extension_service =
+      extensions::ExtensionSystem::Get(profile)->extension_service();
+  extension_service->UnblockAllExtensions();
+}
+#endif  // defined(ENABLE_EXTENSIONS)
 
 // Handles running a callback when a new Browser for the given profile
 // has been completely created.
@@ -98,6 +122,19 @@ void OpenBrowserWindowForProfile(
     is_process_startup = chrome::startup::IS_PROCESS_STARTUP;
     is_first_run = chrome::startup::IS_FIRST_RUN;
   }
+
+#if defined(ENABLE_EXTENSIONS)
+  // The signin bit will still be set if the profile is being unlocked and the
+  // browser window for it is opening. As part of this unlock process, unblock
+  // all the extensions.
+  const ProfileInfoCache& cache =
+      g_browser_process->profile_manager()->GetProfileInfoCache();
+  int index = cache.GetIndexOfProfileWithPath(profile->GetPath());
+  if (!profile->IsGuestSession() &&
+      cache.ProfileIsSigninRequiredAtIndex(index)) {
+    UnblockExtensions(profile);
+  }
+#endif  // defined(ENABLE_EXTENSIONS)
 
   // If |always_create| is false, and we have a |callback| to run, check
   // whether a browser already exists so that we can run the callback. We don't
@@ -298,11 +335,17 @@ void CloseGuestProfileWindows() {
 }
 
 void LockBrowserCloseSuccess(const base::FilePath& profile_path) {
-  ProfileInfoCache* cache =
-      &g_browser_process->profile_manager()->GetProfileInfoCache();
+  ProfileManager* profile_manager = g_browser_process->profile_manager();
+  ProfileInfoCache* cache = &profile_manager->GetProfileInfoCache();
 
   cache->SetProfileSigninRequiredAtIndex(
       cache->GetIndexOfProfileWithPath(profile_path), true);
+
+#if defined(ENABLE_EXTENSIONS)
+  // Profile guaranteed to exist for it to have been locked.
+  BlockExtensions(profile_manager->GetProfileByPath(profile_path));
+#endif  // defined(ENABLE_EXTENSIONS)
+
   chrome::HideTaskManager();
   UserManager::Show(profile_path,
                     profiles::USER_MANAGER_NO_TUTORIAL,
--- extensions/common/extension.h
+++ extensions/common/extension.h
@@ -70,6 +70,7 @@ class Extension : public base::RefCountedThreadSafe<Extension> {
     // DEPRECATED: Special state for component extensions.
     // Maintained as a placeholder since states may be stored to disk.
     ENABLED_COMPONENT_DEPRECATED,
+    // Add new states here as this enum is stored in prefs.
     NUM_STATES
   };
 
@@ -542,6 +543,7 @@ struct UnloadedExtensionInfo {
     REASON_TERMINATE,         // Extension has terminated.
     REASON_BLACKLIST,         // Extension has been blacklisted.
     REASON_PROFILE_SHUTDOWN,  // Profile is being shut down.
+    REASON_LOCK_ALL,          // All extensions for the profile are blocked.
   };
 
   Reason reason;
