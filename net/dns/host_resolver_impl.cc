3861b1f00e0f1f6d37989c1a4cfd560725d09368
vadimt <vadimt@chromium.org>
2014-11-25 16:53:40 -0800

    Instrumenting host_resolver_impl.cc to locate the source of jankiness
    See the bug for why we are here.
    
    BUG=436634
    
    Review URL: https://codereview.chromium.org/758263002
    
    Cr-Commit-Position: refs/heads/master@{#305745}

--- net/dns/host_resolver_impl.cc
+++ net/dns/host_resolver_impl.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_loop_proxy.h"
 #include "base/metrics/field_trial.h"
 #include "base/metrics/histogram.h"
+#include "base/profiler/scoped_tracker.h"
 #include "base/stl_util.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
@@ -481,6 +482,11 @@ class HostResolverImpl::Request {
 
   // Prepare final AddressList and call completion callback.
   void OnComplete(int error, const AddressList& addr_list) {
+    // TODO(vadimt): Remove ScopedTracker below once crbug.com/436634 is fixed.
+    tracked_objects::ScopedTracker tracking_profile(
+        FROM_HERE_WITH_EXPLICIT_FUNCTION(
+            "436634 HostResolverImpl::Request::OnComplete"));
+
     DCHECK(!was_canceled());
     if (error == OK)
       *addresses_ = EnsurePortOnAddressList(addr_list, info_.port());
@@ -680,6 +686,11 @@ class HostResolverImpl::ProcTask
                         const uint32 attempt_number,
                         int error,
                         const int os_error) {
+    // TODO(vadimt): Remove ScopedTracker below once crbug.com/436634 is fixed.
+    tracked_objects::ScopedTracker tracking_profile1(
+        FROM_HERE_WITH_EXPLICIT_FUNCTION(
+            "436634 HostResolverImpl::ProcTask::OnLookupComplete1"));
+
     DCHECK(origin_loop_->BelongsToCurrentThread());
     // If results are empty, we should return an error.
     bool empty_list_on_ok = (error == OK && results.empty());
@@ -740,6 +751,11 @@ class HostResolverImpl::ProcTask
     }
     net_log_.EndEvent(NetLog::TYPE_HOST_RESOLVER_IMPL_PROC_TASK,
                       net_log_callback);
+
+    // TODO(vadimt): Remove ScopedTracker below once crbug.com/436634 is fixed.
+    tracked_objects::ScopedTracker tracking_profile2(
+        FROM_HERE_WITH_EXPLICIT_FUNCTION(
+            "436634 HostResolverImpl::ProcTask::OnLookupComplete2"));
 
     callback_.Run(error, results_);
   }
@@ -1471,6 +1487,11 @@ class HostResolverImpl::Job : public PrioritizedDispatcher::Job,
   void OnProcTaskComplete(base::TimeTicks start_time,
                           int net_error,
                           const AddressList& addr_list) {
+    // TODO(vadimt): Remove ScopedTracker below once crbug.com/436634 is fixed.
+    tracked_objects::ScopedTracker tracking_profile(
+        FROM_HERE_WITH_EXPLICIT_FUNCTION(
+            "436634 HostResolverImpl::Job::OnProcTaskComplete"));
+
     DCHECK(is_proc_running());
 
     if (!resolver_->resolved_known_ipv6_hostname_ &&
@@ -1620,6 +1641,11 @@ class HostResolverImpl::Job : public PrioritizedDispatcher::Job,
   // Performs Job's last rites. Completes all Requests. Deletes this.
   void CompleteRequests(const HostCache::Entry& entry,
                         base::TimeDelta ttl) {
+    // TODO(vadimt): Remove ScopedTracker below once crbug.com/436634 is fixed.
+    tracked_objects::ScopedTracker tracking_profile1(
+        FROM_HERE_WITH_EXPLICIT_FUNCTION(
+            "436634 HostResolverImpl::Job::CompleteRequests1"));
+
     CHECK(resolver_.get());
 
     // This job must be removed from resolver's |jobs_| now to make room for a
@@ -1668,6 +1694,11 @@ class HostResolverImpl::Job : public PrioritizedDispatcher::Job,
                         (entry.error != ERR_HOST_RESOLVER_QUEUE_TOO_LARGE);
     if (did_complete)
       resolver_->CacheResult(key_, entry, ttl);
+
+    // TODO(vadimt): Remove ScopedTracker below once crbug.com/436634 is fixed.
+    tracked_objects::ScopedTracker tracking_profile2(
+        FROM_HERE_WITH_EXPLICIT_FUNCTION(
+            "436634 HostResolverImpl::Job::CompleteRequests2"));
 
     // Complete all of the requests that were attached to the job.
     for (RequestsList::const_iterator it = requests_.begin();
