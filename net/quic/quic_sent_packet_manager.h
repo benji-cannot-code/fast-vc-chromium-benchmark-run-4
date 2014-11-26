90a0ca0f8a4bd3eb93f45111111cb35b6a9e34be
rtenneti <rtenneti@chromium.org>
2014-11-25 17:10:09 -0800

    Land Recent QUIC Changes.
    Ensure CWND is in sensible range when doing QUIC bandwidth resumption.
    Protected by existing flag
    FLAGS_quic_enable_bandwidth_resumption_experiment (which defaults to
    true in chromium).
    
    Merge internal change: 80731275
    https://codereview.chromium.org/751373002/
    
    Deprecate rolled out flag FLAGS_quic_allow_more_open_streams.
    
    Merge internal change: 80726192
    https://codereview.chromium.org/752033003/
    
    Add per-flow RTT to QUIC's SendAlgorithmSimulator.
    
    Merge internal change: 80596663
    https://codereview.chromium.org/756833002/
    
    Add bool return value to SendAlgorithm::ResumeConnectionState.
    
    The following is the internal CL description:
    
    "Add varz to track initial CWND in QUIC bandwidth resumption experiment."
    
    Merge internal change: 80414582
    https://codereview.chromium.org/757853002/
    
    R=rch@chromium.org
    
    Review URL: https://codereview.chromium.org/759093002
    
    Cr-Commit-Position: refs/heads/master@{#305751}

--- net/quic/quic_sent_packet_manager.h
+++ net/quic/quic_sent_packet_manager.h
@@ -102,7 +102,8 @@ class NET_EXPORT_PRIVATE QuicSentPacketManager {
   virtual void SetFromConfig(const QuicConfig& config);
 
   // Pass the CachedNetworkParameters to the send algorithm.
-  void ResumeConnectionState(
+  // Returns true if this changes the initial connection state.
+  bool ResumeConnectionState(
       const CachedNetworkParameters& cached_network_params);
 
   void SetNumOpenStreams(size_t num_streams);
