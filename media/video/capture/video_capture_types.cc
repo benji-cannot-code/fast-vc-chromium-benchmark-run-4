ffb923d69bc90ae58566aceed52b277311f93b7c
zhaoze.zhou <zhaoze.zhou@partner.samsung.com>
2014-11-25 16:51:48 -0800

    Fix PPIXEL_FORMAT_NV21 returned string
    BUG=436651
    
    In the case of PIXEL_FORMAT_NV21, the PixelFormatToString()
    function will return "YV12". This cl modify it to "NV21"
    
    Review URL: https://codereview.chromium.org/762513002
    
    Cr-Commit-Position: refs/heads/master@{#305742}

--- media/video/capture/video_capture_types.cc
+++ media/video/capture/video_capture_types.cc
@@ -56,7 +56,7 @@ std::string VideoCaptureFormat::PixelFormatToString(VideoPixelFormat format) {
   case PIXEL_FORMAT_MJPEG:
     return "MJPEG";
   case PIXEL_FORMAT_NV21:
-    return "YV12";
+    return "NV21";
   case PIXEL_FORMAT_YV12:
     return "YV12";
   case PIXEL_FORMAT_TEXTURE:
