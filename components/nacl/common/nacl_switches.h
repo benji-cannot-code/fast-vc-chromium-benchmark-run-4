098d56e74b38d082dc63c9cb8f259209530aa4aa
hidehiko <hidehiko@chromium.org>
2014-11-25 18:19:44 -0800

    Partially revert crrev.com/741943003 for ASAN bot build failure fix.
    BUG=435432
    TEST=Ran trybots.
    
    Review URL: https://codereview.chromium.org/750013002
    
    Cr-Commit-Position: refs/heads/master@{#305759}

--- components/nacl/common/nacl_switches.h
+++ components/nacl/common/nacl_switches.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace switches {
 
-#if !defined(DISABLE_NACL)
 // All switches in alphabetical order. The switches should be documented
 // alongside the definition of their values in the .cc file.
 extern const char kDisablePnaclCrashThrottling[];
@@ -24,7 +23,6 @@ extern const char kNaClGdb[];
 extern const char kNaClLoaderNonSfiProcess[];
 extern const char kNaClLoaderProcess[];
 extern const char kUseNaClHelperNonSfi[];
-#endif  // !defined(DISABLE_NACL)
 
 }  // namespace switches
 
