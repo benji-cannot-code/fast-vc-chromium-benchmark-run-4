a3d014faabf19d8950a6d12a28b717d39f90d470
rob.buis@samsung.com <rob.buis@samsung.com>
2014-11-26 00:57:13 +0000

    Make getComputedStyle throw for incorrect first parameter
    When the first parameter is null, undefined or not an Element
    type, throw an exception.
    
    Matches Firefox and IE behavior.
    
    BUG=431915
    
    Review URL: https://codereview.chromium.org/752713002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185988 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/LocalDOMWindow.cpp
+++ third_party/WebKit/Source/core/frame/LocalDOMWindow.cpp
@@ -1385,9 +1385,7 @@ StyleMedia* LocalDOMWindow::styleMedia() const
 
 PassRefPtrWillBeRawPtr<CSSStyleDeclaration> LocalDOMWindow::getComputedStyle(Element* elt, const String& pseudoElt) const
 {
-    if (!elt)
-        return nullptr;
-
+    ASSERT(elt);
     return CSSComputedStyleDeclaration::create(elt, false, pseudoElt);
 }
 
