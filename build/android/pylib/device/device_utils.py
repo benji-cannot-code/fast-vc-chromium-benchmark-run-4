1b2913d1cd96e71a12dddce3c2e0c9b254c15367
jeremy <jeremy@chromium.org>
2014-11-27 02:49:16 -0800

    Fix android forwarder on Mac
    device_utils.SetProp() requires a string as the value argument.
    Also modify SetProp()/GetProp() to assert the type of these parameters for a clearer error message.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/754523003
    
    Cr-Commit-Position: refs/heads/master@{#305975}

--- build/android/pylib/device/device_utils.py
+++ build/android/pylib/device/device_utils.py
@@ -984,6 +984,9 @@ class DeviceUtils(object):
     Raises:
       CommandTimeoutError on timeout.
     """
+    assert isinstance(property_name, basestring), (
+        "property_name is not a string: %r" % property_name)
+
     cache_key = '_prop:' + property_name
     if cache and cache_key in self._cache:
       return self._cache[cache_key]
@@ -1017,6 +1020,10 @@ class DeviceUtils(object):
         set on the device (e.g. because it is not rooted).
       CommandTimeoutError on timeout.
     """
+    assert isinstance(property_name, basestring), (
+        "property_name is not a string: %r" % property_name)
+    assert isinstance(value, basestring), "value is not a string: %r" % value
+
     self.RunShellCommand(['setprop', property_name, value], check_return=True)
     if property_name in self._cache:
       del self._cache[property_name]
