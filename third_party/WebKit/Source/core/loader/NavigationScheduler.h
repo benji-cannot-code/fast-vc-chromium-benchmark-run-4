b1645f7209fb2a225aad311a9d21829aa85e7200
dcheng@chromium.org <dcheng@chromium.org>
2014-11-27 10:19:58 +0000

    Block location.reload() in beforeunload event handler.
    NavigationScheduler only checked for BeforeUnload load blocking on one
    of the shouldScheduleNavigation() overloads, so location.reload() was
    not being subject to the same policy checks.
    
    BUG=331670
    
    Review URL: https://codereview.chromium.org/753103007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186102 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/NavigationScheduler.h
+++ third_party/WebKit/Source/core/loader/NavigationScheduler.h
@@ -89,7 +89,7 @@ public:
     void trace(Visitor*);
 
 private:
-    bool shouldScheduleNavigation() const;
+    bool shouldScheduleReload() const;
     bool shouldScheduleNavigation(const String& url) const;
 
     void timerFired(Timer<NavigationScheduler>*);
