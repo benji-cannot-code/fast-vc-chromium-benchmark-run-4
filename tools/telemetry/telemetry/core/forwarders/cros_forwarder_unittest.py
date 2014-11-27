406ee0d316a165e7639558005a5577c128753958
slamm <slamm@chromium.org>
2014-11-26 16:09:19 -0800

    [Telemetry] Refactor CrOS SSH forwarding flags.
    BUG=
    
    Review URL: https://codereview.chromium.org/709843002
    
    Cr-Commit-Position: refs/heads/master@{#305921}

---
+++ tools/telemetry/telemetry/core/forwarders/cros_forwarder_unittest.py
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import unittest
+
+from telemetry.core import forwarders
+from telemetry.core.forwarders import cros_forwarder
+
+# pylint: disable=W0212
+class ForwardingAgsTest(unittest.TestCase):
+  port_pairs = forwarders.PortPairs(
+      http=forwarders.PortPair(111, 222),
+      https=forwarders.PortPair(333, 444),
+      dns=None)
+
+  def testForwardingArgsReverse(self):
+    forwarding_args = cros_forwarder.CrOsSshForwarder._ForwardingArgs(
+        forwarding_flag='-R', host_ip='5.5.5.5', port_pairs=self.port_pairs)
+    self.assertEqual(
+        ['-R222:5.5.5.5:111', '-R444:5.5.5.5:333'],
+        forwarding_args)
+
+  def testForwardingArgs(self):
+    forwarding_args = cros_forwarder.CrOsSshForwarder._ForwardingArgs(
+        forwarding_flag='-L', host_ip='2.2.2.2', port_pairs=self.port_pairs)
+    self.assertEqual(
+        ['-L111:2.2.2.2:222', '-L333:2.2.2.2:444'],
+        forwarding_args)
+
+  def testBadForwardingFlagRaises(self):
+    with self.assertRaises(AssertionError):
+        _ = cros_forwarder.CrOsSshForwarder._ForwardingArgs(
+            '-X', '1.1.1.1', self.port_pairs)
