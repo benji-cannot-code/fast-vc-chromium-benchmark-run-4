9e7e5813e2355f18638237d6c18d52d14d33deac
sammc <sammc@chromium.org>
2014-11-26 17:09:41 -0800

    OOP PDF: Abort streams after a timeout of 10 seconds.
    If the PDF plugin does not finish reading from a stream, that stream is
    left open. Future requests to the same URL will wait until the original
    stream has completed or has been aborted. This CL adds a timeout to
    abort the stream of 10 seconds to the following situations where  a
    stream can be left incomplete:
    1. The stream is passed to the PDF extension background page but not
    claimed by a PDF viewer instance.
    2. The stream is claimed by a PDF instance which fails to read the
    entire stream.
    The former timeout is cancelled if the latter is started.
    
    This is a temporary work-around abandoned streams blocking further
    access to their resources, until the streamsPrivate is made more robust
    to this issue.
    
    BUG=436345
    
    Review URL: https://codereview.chromium.org/763463002
    
    Cr-Commit-Position: refs/heads/master@{#305932}

--- chrome/browser/resources/pdf/background.js
+++ chrome/browser/resources/pdf/background.js
@@ -7,6 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   'use strict';
 
   /**
+   * The timeout in ms before which a stream is closed.
+   * @const {number}
+   */
+  var STREAM_TIMEOUT = 10000;
+
+  /**
    * A map of view ID (which identifies a particular PDF viewer instance) to
    * stream object.
    * @type {Object.<string, Object>}
@@ -26,13 +32,35 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
    * and we can pass it the stream. We can then delete the corresponding map
    * entries.
    * @param {string} viewId The ID of the view to initialize with a stream.
+   * @return {boolean} Whether the stream has been sent to the viewer.
    */
   function flush(viewId) {
     if (viewId in streams && viewId in pluginInitFunctions) {
-      pluginInitFunctions[viewId](streams[viewId]);
+      var streamDetails = streams[viewId];
+      // If the stream has been aborted we pass the stream details to the PDF
+      // viewer with the stream URL removed. This allows the viewer to report an
+      // error to the user rather than trying to load from an invalid stream
+      // URL.
+      if (streamDetails.aborted)
+        delete streamDetails.streamUrl;
+      pluginInitFunctions[viewId](streamDetails);
       delete streams[viewId];
       delete pluginInitFunctions[viewId];
+      if (!streamDetails.aborted) {
+        // Once a stream has been opened, by the plugin, it is safe to abort.
+        // Abort after STREAM_TIMEOUT in case the plugin was closed without
+        // reading from the stream.
+        //
+        // This is a temporary work-around until the streamsPrivate API is made
+        // more robust to its clients failing to finish reading from or abort
+        // streams.
+        setTimeout(function() {
+          chrome.streamsPrivate.abort(streamDetails.streamUrl);
+        }, STREAM_TIMEOUT);
+      }
+      return true;
     }
+    return false;
   }
 
   /**
@@ -45,7 +73,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       // Store the stream until we are contacted by the PDF viewer that owns the
       // stream.
       streams[streamDetails.viewId] = streamDetails;
-      flush(streamDetails.viewId);
+      if (!flush(streamDetails.viewId)) {
+        // If a stream has not been claimed by a PDF viewer instance within
+        // STREAM_TIMEOUT, abort the stream.
+        setTimeout(function() {
+          if (streamDetails.viewId in streams) {
+            chrome.streamsPrivate.abort(streamDetails.streamUrl);
+            streamDetails.aborted = true;
+          }
+        }, STREAM_TIMEOUT);
+      }
     }
   );
 
