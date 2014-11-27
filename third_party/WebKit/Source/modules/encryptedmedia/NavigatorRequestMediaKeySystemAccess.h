9bf9968fbbe6be62b31b20e3b736c45a292b8992
jrummell@chromium.org <jrummell@chromium.org>
2014-11-27 01:21:39 +0000

    Replace MediaKeySystemOptions with MediaKeySystemConfiguration.
    The latest EME spec has changed the parameters passed to
    navigator.requestMediaKeySystemAccess(). Updating the idl to
    reflect this.
        
    BUG=425186
    TEST=all existing EME tests pass
    
    Review URL: https://codereview.chromium.org/759443005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186076 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/encryptedmedia/NavigatorRequestMediaKeySystemAccess.h
+++ third_party/WebKit/Source/modules/encryptedmedia/NavigatorRequestMediaKeySystemAccess.h
@@ -6,14 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NavigatorRequestMediaKeySystemAccess_h
 #define NavigatorRequestMediaKeySystemAccess_h
 
-#include "bindings/core/v8/Dictionary.h"
 #include "bindings/core/v8/ScriptPromise.h"
-#include "bindings/core/v8/V8Binding.h"
-#include "bindings/modules/v8/V8MediaKeySystemOptions.h"
+#include "bindings/modules/v8/V8MediaKeySystemConfiguration.h"
 #include "core/frame/Navigator.h"
-#include "modules/encryptedmedia/MediaKeySystemOptions.h"
+#include "modules/encryptedmedia/MediaKeySystemConfiguration.h"
 #include "platform/Supplementable.h"
-#include "platform/heap/Handle.h"
+#include "wtf/Vector.h"
 
 namespace blink {
 
@@ -32,12 +30,12 @@ public:
         ScriptState*,
         Navigator&,
         const String& keySystem,
-        const Vector<MediaKeySystemOptions>& supportedConfigurations);
+        const Vector<MediaKeySystemConfiguration>& supportedConfigurations);
 
     ScriptPromise requestMediaKeySystemAccess(
         ScriptState*,
         const String& keySystem,
-        const Vector<MediaKeySystemOptions>& supportedConfigurations);
+        const Vector<MediaKeySystemConfiguration>& supportedConfigurations);
 
     virtual void trace(Visitor*) override;
 
