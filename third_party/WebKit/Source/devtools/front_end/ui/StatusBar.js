84e92e95cf310d5b0f64d1283dc484afef0f21e9
pfeldman@chromium.org <pfeldman@chromium.org>
2014-11-27 11:28:59 +0000

    DevTools: status bar titles are missing.
    BUG=437166
    
    Review URL: https://codereview.chromium.org/765713002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186108 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ui/StatusBar.js
+++ third_party/WebKit/Source/devtools/front_end/ui/StatusBar.js
@@ -329,7 +329,7 @@ WebInspector.StatusBarButtonBase = function(title, className, states)
     else
         this._state = "0";
 
-    this.title = title;
+    this.setTitle(title);
     this.className = className;
 }
 
@@ -371,6 +371,14 @@ WebInspector.StatusBarButtonBase.prototype = {
     enabled: function()
     {
         return this._enabled;
+    },
+
+    /**
+     * @return {string}
+     */
+    title: function()
+    {
+        return this._title;
     },
 
     /**
@@ -470,7 +478,7 @@ WebInspector.StatusBarButtonBase.prototype = {
     _showOptions: function()
     {
         var buttons = this._longClickOptionsData.buttonsProvider();
-        var mainButtonClone = new WebInspector.StatusBarButton(this.title, this.className, this._states);
+        var mainButtonClone = new WebInspector.StatusBarButton(this.title(), this.className, this._states);
         mainButtonClone.addEventListener("click", this._clicked, this);
         mainButtonClone.setState(this.state());
         buttons.push(mainButtonClone);
@@ -784,6 +792,7 @@ WebInspector.StatusBarStatesSettingButton = function(className, states, titles, 
     this.addEventListener("click", onClickBound, this);
 
     this._states = states;
+    /** @type {!Array.<!WebInspector.StatusBarButton>} */
     this._buttons = [];
     for (var index = 0; index < states.length; index++) {
         var button = new WebInspector.StatusBarButton(titles[index], className, states.length);
@@ -828,7 +837,7 @@ WebInspector.StatusBarStatesSettingButton.prototype = {
 
         var defaultState = this._defaultState();
         this.setState(defaultState);
-        this.title = this._buttons[this._states.indexOf(defaultState)].title;
+        this.setTitle(this._buttons[this._states.indexOf(defaultState)].title());
     },
 
     /**
