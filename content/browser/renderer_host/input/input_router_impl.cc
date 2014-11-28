d11b5746d4f644eb8fc185718d2a737814329d28
majidvp <majidvp@chromium.org>
2014-11-28 07:44:23 -0800

    Remove a type cast in preparation for a type change in blink
    The type cast would have resulted in a compile time failure after
    |blink::WebTouchEvent.cancelable| is switched from int to bool in CL
    743883005.
    
    Required for CL: https://codereview.chromium.org/743883005/
    
    BUG=435608
    
    Review URL: https://codereview.chromium.org/758033003
    
    Cr-Commit-Position: refs/heads/master@{#306090}

--- content/browser/renderer_host/input/input_router_impl.cc
+++ content/browser/renderer_host/input/input_router_impl.cc
@@ -39,6 +39,7 @@ using blink::WebInputEvent;
 using blink::WebKeyboardEvent;
 using blink::WebMouseEvent;
 using blink::WebMouseWheelEvent;
+using blink::WebTouchEvent;
 
 namespace content {
 namespace {
@@ -370,8 +371,8 @@ void InputRouterImpl::OfferToHandlers(const WebInputEvent& input_event,
   // cancelable (respect ACK disposition) or not.
   bool ignores_ack = WebInputEventTraits::IgnoresAckDisposition(input_event);
   if (WebInputEvent::isTouchEventType(input_event.type)) {
-    DCHECK_NE(static_cast<int>(ignores_ack),
-              static_cast<const blink::WebTouchEvent&>(input_event).cancelable);
+    const WebTouchEvent& touch = static_cast<const WebTouchEvent&>(input_event);
+    DCHECK_NE(ignores_ack, !!touch.cancelable);
   }
 
   // If we don't care about the ack disposition, send the ack immediately.
