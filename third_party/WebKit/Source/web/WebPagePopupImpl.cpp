a015071acbdeabbbc7fb08ed53eef81ab0431fe0
tkent@chromium.org <tkent@chromium.org>
2014-11-28 03:21:04 +0000

    Allow a popup to dispatch events in WebPagePopupImpl::closePopup().
    Avoid bogus assertion failures.
    This CL doesn't change any behavior in production.
    
    BUG=339199,409503
    
    Review URL: https://codereview.chromium.org/758203004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186149 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebPagePopupImpl.cpp
+++ third_party/WebKit/Source/web/WebPagePopupImpl.cpp
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/rendering/RenderView.h"
 #include "modules/accessibility/AXObject.h"
 #include "modules/accessibility/AXObjectCacheImpl.h"
+#include "platform/EventDispatchForbiddenScope.h"
 #include "platform/LayoutTestSupport.h"
 #include "platform/TraceEvent.h"
 #include "platform/heap/Handle.h"
@@ -421,6 +422,11 @@ void WebPagePopupImpl::close()
 
 void WebPagePopupImpl::closePopup()
 {
+    // This function can be called in EventDispatchForbiddenScope for the main
+    // document, and the following operations dispatch some events.  It's safe
+    // because web authors can't listen the events.
+    EventDispatchForbiddenScope::AllowUserAgentEvents allowEvents;
+
     if (m_page) {
         toLocalFrame(m_page->mainFrame())->loader().stopAllLoaders();
         ASSERT(m_page->deprecatedLocalMainFrame()->localDOMWindow());
