e9cce914bc49d4b88cb09f8b1efe9e8f77311167
bashi <bashi@chromium.org>
2014-11-27 21:58:03 -0800

    IDL: Add error handlings to avoid infinite loops
    The IDL parser lacks some error propagation rules, which cause
    infinite loops when an IDL file contains syntax errors. This
    CL fixes following two cases:
    (1) Missing arguments:
      interface I {
        void foo(DOMString arg, );
      };
    (2) Unexpected "attribute" in dictionary
      dictionary D {
        attribute DOMString member = "";
      };
    
    BUG=435794
    
    Review URL: https://codereview.chromium.org/761223002
    
    Cr-Commit-Position: refs/heads/master@{#306049}

--- tools/idl_parser/idl_parser.py
+++ tools/idl_parser/idl_parser.py
@@ -284,6 +284,12 @@ class IDLParser(object):
     """Dictionary : DICTIONARY error ';'"""
     p[0] = self.BuildError(p, 'Dictionary')
 
+  # [11.2] Error recovery for regular Dictionary
+  # (for errors inside dictionary definition)
+  def p_DictionaryError2(self, p):
+    """Dictionary : DICTIONARY identifier Inheritance '{' error"""
+    p[0] = self.BuildError(p, 'Dictionary')
+
   # [12]
   def p_DictionaryMembers(self, p):
     """DictionaryMembers : ExtendedAttributeList DictionaryMember DictionaryMembers
@@ -590,6 +596,11 @@ class IDLParser(object):
                  |"""
     if len(p) > 1:
       p[0] = ListFromConcat(p[2], p[3])
+
+  # [54.1] Arguments error recovery
+  def p_ArgumentsError(self, p):
+    """Arguments : ',' error"""
+    p[0] = self.BuildError(p, 'Arguments')
 
   # [55]
   def p_Argument(self, p):
