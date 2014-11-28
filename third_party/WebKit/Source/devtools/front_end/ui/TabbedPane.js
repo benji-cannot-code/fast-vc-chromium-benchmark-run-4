f2f2431728dc5712d99c8032562848952e54373c
apavlov@chromium.org <apavlov@chromium.org>
2014-11-28 14:38:20 +0000

    DevTools: Fix advanced search field focus on shortcut when creating the view
    R=vsevik
    BUG=437467
    
    Review URL: https://codereview.chromium.org/765063002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186182 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ui/TabbedPane.js
+++ third_party/WebKit/Source/devtools/front_end/ui/TabbedPane.js
@@ -1210,8 +1210,8 @@ WebInspector.ExtensibleTabbedPaneController.prototype = {
          */
         function viewLoaded(view)
         {
-            var shouldFocus = this._tabbedPane.visibleView.element.isSelfOrAncestor(WebInspector.currentFocusElement());
             this._tabbedPane.changeTabView(tabId, view);
+            var shouldFocus = this._tabbedPane.visibleView.element.isSelfOrAncestor(WebInspector.currentFocusElement());
             if (shouldFocus)
                 view.focus();
         }
