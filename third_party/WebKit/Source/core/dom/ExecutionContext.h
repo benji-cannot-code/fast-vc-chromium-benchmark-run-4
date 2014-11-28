a2bea141900918ad58e0668ac4384bdfccbf0783
dominicc@chromium.org <dominicc@chromium.org>
2014-11-28 04:00:33 +0000

    Move DOMTimer nesting level to the ExecutionContext.
    DOMTimers can be created from worker threads as well as the main
    thread; as a consequence reads and writes to this static timer nesting
    level member were happening racily.
    
    Fix that by giving each ExecutionContext its own timer.
    
    I have evidence that timers have other race conditions. This fixes
    just one of them.
    
    BUG=431638
    TEST=Do a debug TSAN build of Chrome [1] and run the ClusterFuzz repro.
         Verify that DOMTimer::create does not appear in the log.
    
    [1] http://www.chromium.org/developers/testing/threadsanitizer-tsan-v2
    
    Review URL: https://codereview.chromium.org/761213003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186151 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/ExecutionContext.h
+++ third_party/WebKit/Source/core/dom/ExecutionContext.h
@@ -122,6 +122,9 @@ public:
 
     void didChangeTimerAlignmentInterval();
 
+    int timerNestingLevel() { return m_timerNestingLevel; }
+    void setTimerNestingLevel(int level) { m_timerNestingLevel = level; }
+
     SandboxFlags sandboxFlags() const { return m_sandboxFlags; }
     bool isSandboxed(SandboxFlags mask) const { return m_sandboxFlags & mask; }
     void enforceSandboxFlags(SandboxFlags mask);
@@ -160,6 +163,7 @@ private:
     int m_circularSequentialID;
     typedef HashMap<int, OwnPtr<DOMTimer> > TimeoutMap;
     TimeoutMap m_timeouts;
+    int m_timerNestingLevel;
 
     bool m_inDispatchErrorEvent;
     class PendingException;
--- third_party/WebKit/Source/core/frame/DOMTimer.cpp
+++ third_party/WebKit/Source/core/frame/DOMTimer.cpp
@@ -47,8 +47,6 @@ static const double oneMillisecond = 0.001;
 // the smallest possible interval timer.
 static const double minimumInterval = 0.004;
 
-static int timerNestingLevel = 0;
-
 static inline bool shouldForwardUserGesture(int interval, int nestingLevel)
 {
     return UserGestureIndicator::processingUserGesture()
@@ -91,7 +89,7 @@ void DOMTimer::removeByID(ExecutionContext* context, int timeoutID)
 DOMTimer::DOMTimer(ExecutionContext* context, PassOwnPtr<ScheduledAction> action, int interval, bool singleShot, int timeoutID)
     : SuspendableTimer(context)
     , m_timeoutID(timeoutID)
-    , m_nestingLevel(timerNestingLevel + 1)
+    , m_nestingLevel(context->timerNestingLevel() + 1)
     , m_action(action)
 {
     ASSERT(timeoutID > 0);
@@ -119,7 +117,7 @@ int DOMTimer::timeoutID() const
 void DOMTimer::fired()
 {
     ExecutionContext* context = executionContext();
-    timerNestingLevel = m_nestingLevel;
+    context->setTimerNestingLevel(m_nestingLevel);
     ASSERT(!context->activeDOMObjectsAreSuspended());
     // Only the first execution of a multi-shot timer should get an affirmative user gesture indicator.
     UserGestureIndicator gestureIndicator(m_userGestureToken.release());
@@ -151,6 +149,8 @@ void DOMTimer::fired()
     // Delete timer before executing the action for one-shot timers.
     OwnPtr<ScheduledAction> action = m_action.release();
 
+    LifecycleContext<ExecutionContext>::Observer observer(context);
+
     // This timer is being deleted; no access to member variables allowed after this point.
     context->removeTimeoutByID(m_timeoutID);
 
@@ -159,7 +159,8 @@ void DOMTimer::fired()
     InspectorInstrumentation::didFireTimer(cookie);
     TRACE_EVENT_INSTANT1(TRACE_DISABLED_BY_DEFAULT("devtools.timeline"), "UpdateCounters", "data", InspectorUpdateCountersEvent::data());
 
-    timerNestingLevel = 0;
+    if (observer.lifecycleContext())
+        context->setTimerNestingLevel(0);
 }
 
 void DOMTimer::contextDestroyed()
