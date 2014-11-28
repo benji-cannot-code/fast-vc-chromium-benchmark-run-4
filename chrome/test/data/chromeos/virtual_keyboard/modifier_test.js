40e026f3931b3dec4ebb198e2a430e9311787069
stevet <stevet@chromium.org>
2014-11-28 04:43:28 -0800

    Add a test to ensure that Control is a sticky modifier key.
    BUG=None
    TEST=BrowserTests --gtest_filter=VirtualKeyboardBrowserTest.ModifierTest
    
    Review URL: https://codereview.chromium.org/764813003
    
    Cr-Commit-Position: refs/heads/master@{#306073}

---
+++ chrome/test/data/chromeos/virtual_keyboard/modifier_test.js
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/*
+ * Copyright 2014 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/**
+ * Test that the Control modifier key is sticky.
+ */
+function testControlKeyStickyAsync(testDoneCallback) {
+  var testCallback = function() {
+    mockTap(findKeyById('ControlLeft'));
+    var send = chrome.virtualKeyboardPrivate.sendKeyEvent;
+    send.addExpectation({
+      type: 'keydown',
+      charValue: 0,
+      keyCode: 65,
+      modifiers: Modifier.CONTROL
+    });
+    send.addExpectation({
+      type: 'keyup',
+      charValue: 0,
+      keyCode: 65,
+      modifiers: Modifier.CONTROL
+    });
+    mockTap(findKey('a'));
+
+    // Ensure that the control key is no longer sticking. i.e. Ensure that
+    // typing 'a' on its own results in only 'a'.
+    mockTypeCharacter('a', 0x41, Modifier.NONE);
+
+    testDoneCallback();
+  };
+  var config = {
+    keyset: 'us',
+    languageCode: 'en',
+    passwordLayout: 'us',
+    name: 'English'
+  };
+  onKeyboardReady(testCallback, config);
+}
