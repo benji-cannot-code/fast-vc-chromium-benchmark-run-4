73d891c44af156ca2007788d50b7259755583036
rsorokin <rsorokin@chromium.org>
2014-11-28 08:13:55 -0800

    Fixed UI stuck on Network screen after rebooting on FRE screen
    Made ErrorScreen getting set during the first show of the enrollment screen. Without it there was a case when error screen was not set because notification about network state was not sent.
    
    Now using message "loginUILoaded" instead of "authPageLoaded" as a signal that Enrollment frame is loaded.
    
    Disabled double click on "Back" button on FRE screen which also caused a bug.
    
    BUG=432334
    TEST=manual on daisy
    
    Review URL: https://codereview.chromium.org/763073002
    
    Cr-Commit-Position: refs/heads/master@{#306095}

--- chrome/browser/ui/webui/chromeos/login/enrollment_screen_handler.h
+++ chrome/browser/ui/webui/chromeos/login/enrollment_screen_handler.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_vector.h"
 #include "chrome/browser/chromeos/login/enrollment/enrollment_screen_actor.h"
 #include "chrome/browser/chromeos/login/enrollment/enterprise_enrollment_helper.h"
+#include "chrome/browser/chromeos/login/screens/error_screen_actor.h"
 #include "chrome/browser/chromeos/login/ui/webui_login_view.h"
 #include "chrome/browser/extensions/signin/scoped_gaia_auth_extension.h"
 #include "chrome/browser/ui/webui/chromeos/login/base_screen_handler.h"
@@ -70,6 +71,8 @@ class EnrollmentScreenHandler
   void HandleRetry();
   void HandleFrameLoadingCompleted(int status);
 
+  void UpdateStateInternal(ErrorScreenActor::ErrorReason reason,
+                           bool force_update);
   void SetupAndShowOfflineMessage(NetworkStateInformer::State state,
                                   ErrorScreenActor::ErrorReason reason);
   void HideOfflineMessage(NetworkStateInformer::State state,
@@ -117,6 +120,9 @@ class EnrollmentScreenHandler
 
   // Latest enrollment frame error.
   net::Error frame_error_;
+
+  // True if screen was not shown yet.
+  bool first_show_;
 
   // Network state informer used to keep signin screen up.
   scoped_refptr<NetworkStateInformer> network_state_informer_;
