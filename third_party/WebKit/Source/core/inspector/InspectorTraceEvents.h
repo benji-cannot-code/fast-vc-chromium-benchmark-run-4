ad9bb186696ca24b5cf250d74245b9741864c988
alph@chromium.org <alph@chromium.org>
2014-11-28 13:51:58 +0000

    DevTools: Move "frame" field inside "data" in trace events.
    Review URL: https://codereview.chromium.org/751393003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186178 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorTraceEvents.h
+++ third_party/WebKit/Source/core/inspector/InspectorTraceEvents.h
@@ -232,6 +232,11 @@ public:
     static PassRefPtr<TraceEvent::ConvertableToTraceFormat> data(RenderObject*);
 };
 
+class InspectorUpdateLayerTreeEvent {
+public:
+    static PassRefPtr<TraceEvent::ConvertableToTraceFormat> data(LocalFrame*);
+};
+
 class InspectorEvaluateScriptEvent {
 public:
     static PassRefPtr<TraceEvent::ConvertableToTraceFormat> data(LocalFrame*, const String& url, int lineNumber);
@@ -249,12 +254,12 @@ public:
 
 class InspectorInvalidateLayoutEvent {
 public:
-    static PassRefPtr<TraceEvent::ConvertableToTraceFormat> data();
+    static PassRefPtr<TraceEvent::ConvertableToTraceFormat> data(LocalFrame*);
 };
 
 class InspectorRecalculateStylesEvent {
 public:
-    static PassRefPtr<TraceEvent::ConvertableToTraceFormat> data();
+    static PassRefPtr<TraceEvent::ConvertableToTraceFormat> data(LocalFrame*);
 };
 
 class InspectorEventDispatchEvent {
