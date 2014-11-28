f4c9b9138b30c54b2c073983b2a5629458d72b3d
hirono <hirono@chromium.org>
2014-11-27 21:18:57 -0800

    Files.app: Move UI event hanlders from FileManager to UIController.
    BUG=267281
    TEST=None
    
    Review URL: https://codereview.chromium.org/757273002
    
    Cr-Commit-Position: refs/heads/master@{#306048}

--- ui/file_manager/file_manager/foreground/js/compiled_resources.gyp
+++ ui/file_manager/file_manager/foreground/js/compiled_resources.gyp
@@ -109,6 +109,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           './ui/share_dialog.js',
           './ui/suggest_apps_dialog.js',
           './ui/tree.css.js',
+          './main_window_component.js',
           './volume_manager_wrapper.js',
           './metadata/byte_reader.js',
         ],
--- ui/file_manager/file_manager/foreground/js/main_scripts.js
+++ ui/file_manager/file_manager/foreground/js/main_scripts.js
@@ -129,6 +129,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //<include src="ui/share_dialog.js">
 //<include src="ui/suggest_apps_dialog.js">
 //<include src="ui/tree.css.js">
+//<include src="main_window_component.js">
 //<include src="volume_manager_wrapper.js">
 
 // // For accurate load performance tracking place main.js should be
