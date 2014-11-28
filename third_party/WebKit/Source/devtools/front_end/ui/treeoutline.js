bb096b7c2f7bf56519d3f64ee1fde7e405f95559
vsevik@chromium.org <vsevik@chromium.org>
2014-11-28 17:12:14 +0000

    DevTools: Prefer nextSibling to previousSibling when moving treeElement selection after element removal.
    R=pfeldman
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/764893002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186190 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ui/treeoutline.js
+++ third_party/WebKit/Source/devtools/front_end/ui/treeoutline.js
@@ -178,10 +178,10 @@ TreeContainerNode.prototype = {
 
         var parent = child.parent;
         if (child.deselect()) {
-            if (child.previousSibling)
-                child.previousSibling.select();
-            else if (child.nextSibling)
+            if (child.nextSibling)
                 child.nextSibling.select();
+            else if (child.previousSibling)
+                child.previousSibling.select();
             else
                 parent.select();
         }
