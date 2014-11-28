18afb575eadeadb2cb311a9ce1c4f765fb446b2e
mkwst@chromium.org <mkwst@chromium.org>
2014-11-28 11:14:42 +0000

    Stop sniffing 'image/*' into script.
    If a resource is served with an image MIME type, we should not sniff it
    into script when loaded via <script>. See [1] and [2] for examples of
    the badness this can cause.
    
    [1]: http://iamajin.blogspot.in/2014/11/when-gifs-serve-javascript.html
    [2]: http://jklmnn.de/imagejs/
    
    I hope this change is web-compatible, but adding a UseCounter just to
    make sure we can keep an eye on things as it rolls out through Beta
    and Stable.
    
    BUG=433049
    
    Review URL: https://codereview.chromium.org/755993003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186168 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/ScriptLoader.cpp
+++ third_party/WebKit/Source/core/dom/ScriptLoader.cpp
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/fetch/FetchRequest.h"
 #include "core/fetch/ResourceFetcher.h"
 #include "core/fetch/ScriptResource.h"
+#include "core/frame/UseCounter.h"
 #include "core/html/HTMLScriptElement.h"
 #include "core/html/imports/HTMLImport.h"
 #include "core/html/parser/HTMLParserIdioms.h"
@@ -342,6 +343,12 @@ void ScriptLoader::executeScript(const ScriptSourceCode& sourceCode, double* com
         ScriptResource* resource = m_resource ? m_resource.get() : sourceCode.resource();
         if (resource && !resource->mimeTypeAllowedByNosniff()) {
             contextDocument->addConsoleMessage(ConsoleMessage::create(SecurityMessageSource, ErrorMessageLevel, "Refused to execute script from '" + resource->url().elidedString() + "' because its MIME type ('" + resource->mimeType() + "') is not executable, and strict MIME type checking is enabled."));
+            return;
+        }
+
+        if (resource && resource->mimeType().lower().startsWith("image/")) {
+            contextDocument->addConsoleMessage(ConsoleMessage::create(SecurityMessageSource, ErrorMessageLevel, "Refused to execute script from '" + resource->url().elidedString() + "' because its MIME type ('" + resource->mimeType() + "') is not executable."));
+            UseCounter::count(frame, UseCounter::BlockedSniffingImageToScript);
             return;
         }
 
