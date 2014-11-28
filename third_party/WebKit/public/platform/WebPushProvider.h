7de22acb9ad7994645c62fc0b7ee6c28a8b8c017
mvanouwerkerk@chromium.org <mvanouwerkerk@chromium.org>
2014-11-28 12:19:27 +0000

    Push API: define platform path for registration and getting permission status. [1/2]
    This is part of a pair of patches:
    [1] https://codereview.chromium.org/739533003/ (THIS PATCH)
    [2] https://codereview.chromium.org/739533003/
    
    BUG=389194
    
    Review URL: https://codereview.chromium.org/739533003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186175 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/public/platform/WebPushProvider.h
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WebPushProvider_h
+#define WebPushProvider_h
+
+#include "public/platform/WebCallbacks.h"
+#include "public/platform/WebCommon.h"
+#include "public/platform/WebPushPermissionStatus.h"
+
+namespace blink {
+
+class WebServiceWorkerRegistration;
+struct WebPushError;
+struct WebPushRegistration;
+
+typedef WebCallbacks<WebPushRegistration, WebPushError> WebPushRegistrationCallbacks;
+typedef WebCallbacks<WebPushPermissionStatus, void> WebPushPermissionStatusCallbacks;
+
+class WebPushProvider {
+public:
+    virtual ~WebPushProvider() { }
+
+    // Takes ownership of the WebPushRegistrationCallbacks.
+    // Does not take ownership of the WebServiceWorkerRegistration.
+    virtual void registerPushMessaging(WebServiceWorkerRegistration*, WebPushRegistrationCallbacks*) { BLINK_ASSERT_NOT_REACHED(); }
+
+    // Takes ownership of the WebPushPermissionStatusCallbacks.
+    // Does not take ownership of the WebServiceWorkerRegistration.
+    virtual void getPermissionStatus(WebServiceWorkerRegistration*, WebPushPermissionStatusCallbacks*) { BLINK_ASSERT_NOT_REACHED(); }
+};
+
+} // namespace blink
+
+#endif // WebPushProvider_h
