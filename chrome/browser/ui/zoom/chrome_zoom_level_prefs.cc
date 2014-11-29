6c0ed490401d37fd45bc1067ff553154daf9e10e
wjmaclean <wjmaclean@chromium.org>
2014-11-29 06:39:51 -0800

    Remove unnecessary include file.
    Remove include for <sstream> from chrome_zoom_level_prefs.cc as it is
    not used.
    
    TBR=wjmaclean@chromium.org
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/770503003
    
    Cr-Commit-Position: refs/heads/master@{#306121}

--- chrome/browser/ui/zoom/chrome_zoom_level_prefs.cc
+++ chrome/browser/ui/zoom/chrome_zoom_level_prefs.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/zoom/chrome_zoom_level_prefs.h"
 
-#include <sstream>
-
 #include "base/bind.h"
 #include "base/prefs/json_pref_store.h"
 #include "base/prefs/pref_filter.h"
