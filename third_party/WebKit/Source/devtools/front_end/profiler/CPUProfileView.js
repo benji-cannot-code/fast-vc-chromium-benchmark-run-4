6fab9b8732daca303caf2da7b64e609f2eaac2e2
loislo@chromium.org <loislo@chromium.org>
2014-11-29 09:04:12 +0000

    DevTools: calculate event offset lazily. So we win about 40% of speed.
    With the patch we do not concatenate strings with eventDelimiter.
    Also we do not create Blobs for calculating offsets.
    As result the speed of loading 349Mb timeline was decreased.
    Originaly it was about 20sec, after patch it became 12sec.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/768603002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186201 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/profiler/CPUProfileView.js
+++ third_party/WebKit/Source/devtools/front_end/profiler/CPUProfileView.js
@@ -763,12 +763,12 @@ WebInspector.CPUProfileHeader.prototype = {
             return;
         }
         /**
-         * @param {boolean} success
+         * @param {number} fileSize
          * @this {WebInspector.CPUProfileHeader}
          */
-        function didWriteToTempFile(success)
+        function didWriteToTempFile(fileSize)
         {
-            if (!success)
+            if (!fileSize)
                 this._failedToCreateTempFile = true;
             tempFile.finishWriting();
             this._notifyTempFileReady();
