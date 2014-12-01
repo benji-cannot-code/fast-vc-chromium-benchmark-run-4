7932e004928db14fff8499d3948e82838deaa08d
joaodasilva <joaodasilva@chromium.org>
2014-12-01 05:29:43 -0800

    Removing myself from OWNERS and related files.
    TBR=mnissler@chromium.org
    BUG=None
    
    Review URL: https://codereview.chromium.org/770743002
    
    Cr-Commit-Position: refs/heads/master@{#306183}

--- components/policy/resources/PRESUBMIT.py
+++ components/policy/resources/PRESUBMIT.py
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 # If this presubmit check fails or misbehaves, please complain to
-# mnissler@chromium.org, pastarmovj@chromium.org or joaodasilva@chromium.org.
+# mnissler@chromium.org, bartfab@chromium.org or atwilson@chromium.org.
 
 import itertools
 import sys
