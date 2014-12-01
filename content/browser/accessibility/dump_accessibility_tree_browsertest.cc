902a13c4a081657d477249d3e31d81291f0ec944
ramya.v <ramya.v@samsung.com>
2014-11-30 23:45:43 -0800

    Add DumpAccessibilityTree tests (16 of 20)
    ARIA Role: directory, slider, tooltip, treegrid
    ARIA state/property: aria-multiline
    
    BUG=170588
    
    Review URL: https://codereview.chromium.org/754363002
    
    Cr-Commit-Position: refs/heads/master@{#306160}

--- content/browser/accessibility/dump_accessibility_tree_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_tree_browsertest.cc
@@ -400,6 +400,10 @@ IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityAriaDialog) {
   RunTest(FILE_PATH_LITERAL("aria-dialog.html"));
 }
 
+IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityAriaDirectory) {
+  RunTest(FILE_PATH_LITERAL("aria-directory.html"));
+}
+
 IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityAriaExpanded) {
   RunTest(FILE_PATH_LITERAL("aria-expanded.html"));
 }
@@ -495,6 +499,11 @@ IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest,
   RunTest(FILE_PATH_LITERAL("aria-menuitemradio.html"));
 }
 
+IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest,
+                       AccessibilityAriaMultiline) {
+  RunTest(FILE_PATH_LITERAL("aria-multiline.html"));
+}
+
 IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityAriaNavigation) {
   RunTest(FILE_PATH_LITERAL("aria-navigation.html"));
 }
@@ -567,6 +576,10 @@ IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityAriaSort) {
   RunTest(FILE_PATH_LITERAL("aria-sort.html"));
 }
 
+IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityAriaSlider) {
+  RunTest(FILE_PATH_LITERAL("aria-slider.html"));
+}
+
 IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest,
                        AccessibilityAriaSpinButton) {
   RunTest(FILE_PATH_LITERAL("aria-spinbutton.html"));
@@ -589,8 +602,16 @@ IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityAriaToolbar) {
   RunTest(FILE_PATH_LITERAL("aria-toolbar.html"));
 }
 
+IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityAriaTooltip) {
+  RunTest(FILE_PATH_LITERAL("aria-tooltip.html"));
+}
+
 IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityAriaTree) {
   RunTest(FILE_PATH_LITERAL("aria-tree.html"));
+}
+
+IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityAriaTreeGrid) {
+  RunTest(FILE_PATH_LITERAL("aria-treegrid.html"));
 }
 
 IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityAriaValueMin) {
