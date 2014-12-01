46509b824b16b139d644ae4dce641109b19e6674
sammc <sammc@chromium.org>
2014-12-01 15:44:26 -0800

    Identify ArrayBuffers by their string representation in json_schema.js.
    Previously, ArrayBuffers were identified by comparing their constructor
    to the current context's ArrayBuffer constructor. This caused
    ArrayBuffers from other contexts to be identified as "object" instead of
    "binary".
    
    BUG=389016
    
    Review URL: https://codereview.chromium.org/684403002
    
    Cr-Commit-Position: refs/heads/master@{#306294}

--- extensions/renderer/json_schema_unittest.cc
+++ extensions/renderer/json_schema_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "extensions/renderer/module_system_test.h"
 #include "extensions/renderer/v8_schema_registry.h"
+#include "gin/dictionary.h"
 #include "grit/extensions_renderer_resources.h"
 
 namespace extensions {
@@ -73,6 +74,18 @@ TEST_F(JsonSchemaTest, TestIntegerBounds) {
 }
 
 TEST_F(JsonSchemaTest, TestType) {
+  gin::Dictionary array_buffer_container(
+      env()->isolate(),
+      env()->CreateGlobal("otherContextArrayBufferContainer"));
+  {
+    // Create an ArrayBuffer in another v8 context and pass it to the test
+    // through a global.
+    scoped_ptr<ModuleSystemTestEnvironment> other_env(CreateEnvironment());
+    v8::Context::Scope scope(other_env->context()->v8_context());
+    v8::Handle<v8::ArrayBuffer> array_buffer(
+        v8::ArrayBuffer::New(env()->isolate(), 1));
+    array_buffer_container.Set("value", array_buffer);
+  }
   TestFunction("testType");
 }
 
