240fa62ad3b2b99e73a9589eae3f8287c1cbcbf1
mgiuca <mgiuca@chromium.org>
2014-11-30 20:23:23 -0800

    Fixed AppsGridViewTest failures with experimental app list enabled.
    Now if IsExperimentalAppListEnabled returns true, the tests still pass.
    
    1. Fixed AppsGridViewTest::GetItemTileRectAt to take padding into
       account.
    
    2. MouseDragMaxItemsInFolderWithMovement drags to a point inside the
       tile rect, not 1px outside.
    
    3. Double size of test grid view (from 320x240 to 640x480).
       MouseDragItemReorder was failing due to increased size of the tiles
       (test drop targets were outside the window bounds).
    
    BUG=437436
    
    Review URL: https://codereview.chromium.org/768713003
    
    Cr-Commit-Position: refs/heads/master@{#306148}

--- ui/app_list/views/apps_grid_view_unittest.cc
+++ ui/app_list/views/apps_grid_view_unittest.cc
@@ -36,9 +36,6 @@ const int kCols = 2;
 const int kRows = 2;
 const int kTilesPerPage = kCols * kRows;
 
-const int kWidth = 320;
-const int kHeight = 240;
-
 class PageFlipWaiter : public PaginationModelObserver {
  public:
   PageFlipWaiter(base::MessageLoopForUI* ui_loop, PaginationModel* model)
@@ -97,7 +94,8 @@ class AppsGridViewTest : public views::ViewsTestBase {
 
     apps_grid_view_.reset(new AppsGridView(NULL));
     apps_grid_view_->SetLayout(kCols, kRows);
-    apps_grid_view_->SetBoundsRect(gfx::Rect(gfx::Size(kWidth, kHeight)));
+    apps_grid_view_->SetBoundsRect(
+        gfx::Rect(apps_grid_view_->GetPreferredSize()));
     apps_grid_view_->SetModel(model_.get());
     apps_grid_view_->SetItemList(model_->top_level_item_list());
 
@@ -134,7 +132,7 @@ class AppsGridViewTest : public views::ViewsTestBase {
 
     gfx::Insets insets(apps_grid_view_->GetInsets());
     gfx::Rect rect(gfx::Point(insets.left(), insets.top()),
-                   GetItemViewAt(0)->bounds().size());
+                   AppsGridView::GetTotalTileSize());
     rect.Offset(col * rect.width(), row * rect.height());
     return rect;
   }
@@ -438,6 +436,7 @@ TEST_F(AppsGridViewTest, MouseDragMaxItemsInFolderWithMovement) {
   // Drag the new item to the left so that the grid reorders.
   gfx::Point from = GetItemTileRectAt(0, 1).CenterPoint();
   gfx::Point to = GetItemTileRectAt(0, 0).bottom_left();
+  to.Offset(0, -1);  // Get a point inside the rect.
   AppListItemView* dragged_view = SimulateDrag(AppsGridView::MOUSE, from, to);
   test_api_->LayoutToIdealBounds();
 
