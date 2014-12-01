456534c9f7ff35795ec4ddbfe4b15682e2237b27
kcarattini <kcarattini@chromium.org>
2014-12-01 15:43:43 -0800

    Adds a notification for always-on hotwording.
    Images have been crushed with tools/resources/optimize-png-files.sh
    
    BUG=436682
    
    Review URL: https://codereview.chromium.org/763733002
    
    Cr-Commit-Position: refs/heads/master@{#306293}

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -631,6 +631,7 @@ extern const char kVideoCaptureAllowedUrls[];
 
 extern const char kHotwordSearchEnabled[];
 extern const char kHotwordAlwaysOnSearchEnabled[];
+extern const char kHotwordAlwaysOnNotificationSeen[];
 extern const char kHotwordAudioLoggingEnabled[];
 extern const char kHotwordAudioHistoryEnabled[];
 extern const char kHotwordPreviousLanguage[];
