3f0744d7d92eb78b5a0a2854e6bf21c1c61931fa
pfeldman@chromium.org <pfeldman@chromium.org>
2014-12-01 09:25:32 +0000

    DevTools: fix timeline view after migration to the shadow split.
    R=caseq@chromium.org
    
    Review URL: https://codereview.chromium.org/766883003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186220 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/timeline/TimelineView.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TimelineView.js
@@ -32,14 +32,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /**
  * @constructor
- * @extends {WebInspector.HBox}
+ * @extends {WebInspector.VBox}
  * @implements {WebInspector.TimelineModeView}
  * @param {!WebInspector.TimelineModeViewDelegate} delegate
  * @param {!WebInspector.TimelineModel} model
  */
 WebInspector.TimelineView = function(delegate, model)
 {
-    WebInspector.HBox.call(this);
+    WebInspector.VBox.call(this);
     this.element.classList.add("timeline-view");
 
     this._delegate = delegate;
@@ -54,7 +54,11 @@ WebInspector.TimelineView = function(delegate, model)
 
     this._recordsView = this._createRecordsView();
     this._recordsView.addEventListener(WebInspector.SplitView.Events.SidebarSizeChanged, this._sidebarResized, this);
+
+    this._topGapElement = this.element.createChild("div", "timeline-gap");
     this._recordsView.show(this.element);
+    this._bottomGapElement = this.element.createChild("div", "timeline-gap");
+
     this._headerElement = this.element.createChild("div", "fill");
     this._headerElement.id = "timeline-graph-records-header";
 
@@ -86,11 +90,13 @@ WebInspector.TimelineView.prototype = {
      */
     _createRecordsView: function()
     {
-        var recordsView = new WebInspector.SplitView(true, false, "timelinePanelRecorsSplitViewState");
-        this._containerElement = recordsView.element;
+        this._containerElement = this.element;
         this._containerElement.tabIndex = 0;
         this._containerElement.id = "timeline-container";
         this._containerElement.addEventListener("scroll", this._onScroll.bind(this), false);
+
+        var recordsView = new WebInspector.SplitView(true, false, "timelinePanelRecorsSplitViewState");
+        recordsView.element.style.flex = "1 0 auto";
 
         // Create records list in the records sidebar.
         var sidebarView = new WebInspector.VBox();
@@ -109,9 +115,7 @@ WebInspector.TimelineView.prototype = {
         this._itemsGraphsElement.id = "timeline-graphs";
 
         // Create gap elements
-        this._topGapElement = this._itemsGraphsElement.createChild("div", "timeline-gap");
         this._graphRowsElement = this._itemsGraphsElement.createChild("div");
-        this._bottomGapElement = this._itemsGraphsElement.createChild("div", "timeline-gap");
         this._expandElements = this._itemsGraphsElement.createChild("div");
         this._expandElements.id = "orphan-expand-elements";
 
@@ -296,15 +300,14 @@ WebInspector.TimelineView.prototype = {
     wasShown: function()
     {
         this._presentationModel.refreshRecords();
-        WebInspector.HBox.prototype.wasShown.call(this);
+        WebInspector.VBox.prototype.wasShown.call(this);
     },
 
     _onScroll: function(event)
     {
         this._closeRecordDetails();
         this._scrollTop = this._containerElement.scrollTop;
-        var dividersTop = Math.max(0, this._scrollTop);
-        this._timelineGrid.setScrollAndDividerTop(this._scrollTop, dividersTop);
+        this._headerElement.style.top = this._scrollTop + "px";
         this._scheduleRefresh(true, true);
     },
 
@@ -538,15 +541,12 @@ WebInspector.TimelineView.prototype = {
         }
 
         // Resize gaps first.
-        this._topGapElement.style.height = (startIndex * rowHeight) + "px";
-        this._recordsView.sidebarView().element.firstElementChild.style.flexBasis = (startIndex * rowHeight + headerHeight) + "px";
+        var topGapSize = startIndex * rowHeight;
+        this._topGapElement.style.height = topGapSize + "px";
         this._bottomGapElement.style.height = (recordsInWindow.length - endIndex) * rowHeight + "px";
-        var rowsHeight = headerHeight + recordsInWindow.length * rowHeight;
-        var totalHeight = Math.max(this._containerElementHeight, rowsHeight);
-
-        this._recordsView.mainView().element.style.height = totalHeight + "px";
-        this._recordsView.sidebarView().element.style.height = totalHeight + "px";
-        this._recordsView.resizerElement().style.height = totalHeight + "px";
+        this._recordsView.element.style.height = Math.max(this._containerElementHeight, (endIndex - startIndex) * rowHeight) + "px";
+        this._timelineGrid.setScrollTop(this._scrollTop - topGapSize);
+        this._recordsView.onResize();
 
         // Update visible rows.
         var listRowElement = this._sidebarListElement.firstChild;
@@ -566,7 +566,7 @@ WebInspector.TimelineView.prototype = {
                 if (lastChildIndex >= startIndex && lastChildIndex < endIndex) {
                     var expandElement = new WebInspector.TimelineExpandableElement(this._expandElements);
                     var positions = this._calculator.computeBarGraphWindowPosition(record);
-                    expandElement._update(record, i, positions.left - this._expandOffset, positions.width);
+                    expandElement._update(record, i, topGapSize, positions.left - this._expandOffset, positions.width);
                 }
             } else {
                 if (!listRowElement) {
@@ -579,7 +579,7 @@ WebInspector.TimelineView.prototype = {
                 }
 
                 listRowElement.row.update(record, visibleTop);
-                graphRowElement.row.update(record, this._calculator, this._expandOffset, i);
+                graphRowElement.row.update(record, this._calculator, this._expandOffset, i, topGapSize);
                 if (this._lastSelectedRecord === record) {
                     listRowElement.row.renderAsSelected(true);
                     graphRowElement.row.renderAsSelected(true);
@@ -602,7 +602,7 @@ WebInspector.TimelineView.prototype = {
             graphRowElement = nextElement;
         }
 
-        this._itemsGraphsElement.insertBefore(this._graphRowsElement, this._bottomGapElement);
+        this._itemsGraphsElement.appendChild(this._graphRowsElement);
         this._itemsGraphsElement.appendChild(this._expandElements);
         this._adjustScrollPosition(recordsInWindow.length * rowHeight + headerHeight);
 
@@ -895,7 +895,7 @@ WebInspector.TimelineView.prototype = {
         }
     },
 
-    __proto__: WebInspector.HBox.prototype
+    __proto__: WebInspector.VBox.prototype
 }
 
 /**
@@ -1186,8 +1186,9 @@ WebInspector.TimelineRecordGraphRow.prototype = {
      * @param {!WebInspector.TimelineCalculator} calculator
      * @param {number} expandOffset
      * @param {number} index
+     * @param {number} topGapSize
      */
-    update: function(presentationRecord, calculator, expandOffset, index)
+    update: function(presentationRecord, calculator, expandOffset, index, topGapSize)
     {
         this._record = presentationRecord;
         var record = presentationRecord.record();
@@ -1200,7 +1201,7 @@ WebInspector.TimelineRecordGraphRow.prototype = {
         this._barElement.style.width = barPosition.width + "px";
         this._barCpuElement.style.left = barPosition.left + "px";
         this._barCpuElement.style.width = barPosition.cpuWidth + "px";
-        this._expandElement._update(presentationRecord, index, barPosition.left - expandOffset, barPosition.width);
+        this._expandElement._update(presentationRecord, index, topGapSize, barPosition.left - expandOffset, barPosition.width);
         this._record.setGraphRow(this);
     },
 
@@ -1270,14 +1271,15 @@ WebInspector.TimelineExpandableElement.prototype = {
     /**
      * @param {!WebInspector.TimelinePresentationModel.Record} record
      * @param {number} index
+     * @param {number} topGapSize
      * @param {number} left
      * @param {number} width
      */
-    _update: function(record, index, left, width)
+    _update: function(record, index, topGapSize, left, width)
     {
         const rowHeight = WebInspector.TimelinePanel.rowHeight;
         if (record.visibleChildrenCount() || record.expandable()) {
-            this._element.style.top = index * rowHeight + "px";
+            this._element.style.top = (index * rowHeight - topGapSize) + "px";
             this._element.style.left = left + "px";
             this._element.style.width = Math.max(12, width + 25) + "px";
             if (!record.collapsed()) {
