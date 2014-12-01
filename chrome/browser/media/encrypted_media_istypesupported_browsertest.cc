03b7cd8c1478d410d663b3cbf9ad0c37b4e4aedf
sandersd <sandersd@chromium.org>
2014-12-01 12:39:08 -0800

    Fix browsertests for component-based Widevine.
    Previously the tests expected a component-based Widevine to be loaded but not to register any supported codecs; now that registration always includes audio codecs, this changes the test to not load the CDM at all.
    
    BUG=437419
    
    Review URL: https://codereview.chromium.org/773523003
    
    Cr-Commit-Position: refs/heads/master@{#306243}

--- chrome/browser/media/encrypted_media_istypesupported_browsertest.cc
+++ chrome/browser/media/encrypted_media_istypesupported_browsertest.cc
@@ -47,7 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Note: Widevine is not available on platforms using components because
 // RegisterPepperCdm() cannot set the codecs.
 // TODO(ddorwin): Enable these tests after we have the ability to use the CUS
-// in these tests. See http://crbug.com/311724.
+// in these tests. See http://crbug.com/356833.
 #if defined(WIDEVINE_CDM_AVAILABLE) && !defined(WIDEVINE_CDM_IS_COMPONENT)
 #define EXPECT_WV EXPECT_TRUE
 #define EXPECT_WVMP4 EXPECT_TRUE
@@ -217,8 +217,12 @@ class EncryptedMediaIsTypeSupportedTest : public InProcessBrowserTest {
 #else
     pepper_plugin.append(pepper_type_for_key_system);
 #endif
+// TODO(sandersd): A component CDM registered this way declares support for
+// audio codecs, but not the other codecs we expect. http://crbug.com/356833.
+#if !defined(WIDEVINE_CDM_IS_COMPONENT)
     command_line->AppendSwitchNative(switches::kRegisterPepperPlugins,
                                      pepper_plugin);
+#endif
   }
 
   void LoadTestPage() {
@@ -850,11 +854,7 @@ IN_PROC_BROWSER_TEST_F(
 
 IN_PROC_BROWSER_TEST_F(EncryptedMediaIsTypeSupportedWidevineTest,
                        Widevine_Basic) {
-#if defined(WIDEVINE_CDM_AVAILABLE) && defined(WIDEVINE_CDM_IS_COMPONENT)
-  EXPECT_TRUE(IsConcreteSupportedKeySystem(kWidevineAlpha));
-#else
   EXPECT_WV(IsConcreteSupportedKeySystem(kWidevineAlpha));
-#endif
   EXPECT_WV(IsSupportedKeySystemWithMediaMimeType(
       "video/webm", no_codecs(), kWidevineAlpha));
 }
