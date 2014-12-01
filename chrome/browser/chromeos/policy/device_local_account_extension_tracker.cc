759b57348ffc3678e3b0e06f72dd2091b813da74
joaodasilva <joaodasilva@chromium.org>
2014-12-01 03:17:04 -0800

    Fix reloading of policy for extensions in public sessions.
    Public sessions have a policy broker outside the public session that is
    used to precache the policy for that public account before the session
    is started (for example, right after enrollment). This broker also has
    a tracker of the extensions for that account that updates the
    SchemaRegistry, so that the ComponentCloudPolicyService fetches the
    policy for those extensions before they are installed.
    
    However, once the session starts and the extensions are loaded, the
    SchemaRegistry will have the real Schemas that are used to validate
    the policy data.
    
    The bug is that the tracker at the broker is still overriding the
    Schema with a stub whenever the policy is reloaded. This replaces
    the valid Schema with an empty one, and all the policies get
    filtered out due to this glitch.
    
    BUG=437273
    
    Review URL: https://codereview.chromium.org/761163002
    
    Cr-Commit-Position: refs/heads/master@{#306173}

--- chrome/browser/chromeos/policy/device_local_account_extension_tracker.cc
+++ chrome/browser/chromeos/policy/device_local_account_extension_tracker.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/core/common/policy_map.h"
 #include "components/policy/core/common/policy_namespace.h"
 #include "components/policy/core/common/schema.h"
+#include "components/policy/core/common/schema_map.h"
 #include "components/policy/core/common/schema_registry.h"
 #include "extensions/browser/pref_names.h"
 
@@ -75,6 +76,17 @@ void DeviceLocalAccountExtensionTracker::UpdateFromStore() {
 
   for (base::DictionaryValue::Iterator it(*dict); !it.IsAtEnd(); it.Advance()) {
     PolicyNamespace ns(POLICY_DOMAIN_EXTENSIONS, it.key());
+    if (schema_registry_->schema_map()->GetSchema(ns)) {
+      // Important detail: Don't register the component again if it already
+      // has a schema! If the session already started for this public session
+      // then the real Schema for the extension has already been set by the
+      // ManagedValueStoreCache::ExtensionTracker. Do not override that schema
+      // with an invalid one now, or the policy for the extension will be
+      // dropped.
+      // However, if the forcelist is updated then we need to register the new
+      // component ID so that its remote policy data can be fetched.
+      continue;
+    }
     schema_registry_->RegisterComponent(ns, Schema());
   }
 
