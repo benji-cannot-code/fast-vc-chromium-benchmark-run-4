f648905ea4e33b76b17afbbe8c08a835db055dea
lionel.g.landwerlin <lionel.g.landwerlin@intel.com>
2014-12-01 04:54:42 -0800

    ozone: implement ui::PlatformWindow.SetCapture/ReleaseCapture
    BUG=423383
    TEST=none
    
    Review URL: https://codereview.chromium.org/763343002
    
    Cr-Commit-Position: refs/heads/master@{#306180}

--- ui/ozone/platform/dri/dri_window_manager.cc
+++ ui/ozone/platform/dri/dri_window_manager.cc
@@ -21,7 +21,9 @@ gfx::PointF GetDefaultCursorLocation(DriWindow* window) {
 }  // namespace
 
 DriWindowManager::DriWindowManager(DriGpuPlatformSupportHost* sender)
-    : last_allocated_widget_(0), cursor_(new DriCursor(this, sender)) {
+    : last_allocated_widget_(0),
+      cursor_(new DriCursor(this, sender)),
+      event_grabber_(gfx::kNullAcceleratedWidget) {
 }
 
 DriWindowManager::~DriWindowManager() {
@@ -52,6 +54,8 @@ void DriWindowManager::RemoveWindow(gfx::AcceleratedWidget widget) {
 
   if (cursor_->GetCursorWindow() == widget)
     ResetCursorLocation();
+  if (event_grabber_ == widget)
+    event_grabber_ = gfx::kNullAcceleratedWidget;
 }
 
 DriWindow* DriWindowManager::GetWindow(gfx::AcceleratedWidget widget) {
@@ -81,6 +85,18 @@ void DriWindowManager::ResetCursorLocation() {
   }
 
   cursor_->MoveCursorTo(cursor_widget, location);
+}
+
+void DriWindowManager::GrabEvents(gfx::AcceleratedWidget widget) {
+  if (event_grabber_ != gfx::kNullAcceleratedWidget)
+    return;
+  event_grabber_ = widget;
+}
+
+void DriWindowManager::UngrabEvents(gfx::AcceleratedWidget widget) {
+  if (event_grabber_ != widget)
+    return;
+  event_grabber_ = gfx::kNullAcceleratedWidget;
 }
 
 }  // namespace ui
