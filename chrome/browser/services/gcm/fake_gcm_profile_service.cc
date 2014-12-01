bb46866160a9b026fbbeb6b9bfe5c384d55fa6e1
mlamouri <mlamouri@chromium.org>
2014-12-01 13:20:48 -0800

    [Push] Unregister if there is no available SW.
    When there is no registered service worker, instead of dropping the
    message, the PushMessagingService will unregister the web app to which
    the message was delivered.
    
    BUG=437817
    
    Review URL: https://codereview.chromium.org/766223002
    
    Cr-Commit-Position: refs/heads/master@{#306254}

--- chrome/browser/services/gcm/fake_gcm_profile_service.cc
+++ chrome/browser/services/gcm/fake_gcm_profile_service.cc
@@ -144,6 +144,9 @@ void FakeGCMProfileService::UnregisterFinished(const std::string& app_id) {
   CustomFakeGCMDriver* custom_driver =
       static_cast<CustomFakeGCMDriver*>(driver());
   custom_driver->OnUnregisterFinished(app_id, result);
+
+  if (!unregister_callback_.is_null())
+    unregister_callback_.Run(app_id);
 }
 
 void FakeGCMProfileService::SendFinished(
@@ -163,6 +166,11 @@ void FakeGCMProfileService::SendFinished(
 void FakeGCMProfileService::AddExpectedUnregisterResponse(
     GCMClient::Result result) {
   unregister_responses_.push_back(result);
+}
+
+void FakeGCMProfileService::SetUnregisterCallback(
+    const UnregisterCallback& callback) {
+  unregister_callback_ = callback;
 }
 
 }  // namespace gcm
--- chrome/browser/services/gcm/fake_gcm_profile_service.h
+++ chrome/browser/services/gcm/fake_gcm_profile_service.h
@@ -21,6 +21,8 @@ namespace gcm {
 // Acts as a bridge between GCM API and GCMClient layer for testing purposes.
 class FakeGCMProfileService : public GCMProfileService {
  public:
+  typedef base::Callback<void(const std::string&)> UnregisterCallback;
+
   // Helper function to be used with
   // KeyedService::SetTestingFactory().
   static KeyedService* Build(content::BrowserContext* context);
@@ -36,6 +38,8 @@ class FakeGCMProfileService : public GCMProfileService {
                     const GCMClient::OutgoingMessage& message);
 
   void AddExpectedUnregisterResponse(GCMClient::Result result);
+
+  void SetUnregisterCallback(const UnregisterCallback& callback);
 
   const GCMClient::OutgoingMessage& last_sent_message() const {
     return last_sent_message_;
@@ -66,6 +70,7 @@ class FakeGCMProfileService : public GCMProfileService {
   std::list<GCMClient::Result> unregister_responses_;
   GCMClient::OutgoingMessage last_sent_message_;
   std::string last_receiver_id_;
+  UnregisterCallback unregister_callback_;
 
   DISALLOW_COPY_AND_ASSIGN(FakeGCMProfileService);
 };
