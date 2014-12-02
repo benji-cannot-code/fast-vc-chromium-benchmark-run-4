020cd78250e00b9f4fca43f78ff92cdb40509290
apavlov@chromium.org <apavlov@chromium.org>
2014-12-02 16:59:55 +0000

    DevTools: Do not log errors when calling WI.InspectorView.showCloseableViewInDrawer()
    R=vsevik
    
    Review URL: https://codereview.chromium.org/772063002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186312 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ui/TabbedPane.js
+++ third_party/WebKit/Source/devtools/front_end/ui/TabbedPane.js
@@ -1172,7 +1172,7 @@ WebInspector.ExtensibleTabbedPaneController = function(tabbedPane, extensionPoin
     this._extensionPoint = extensionPoint;
     this._viewCallback = viewCallback;
     this._tabOrders = {};
-    /** @type {!Object.<string, !Promise.<!WebInspector.View>>} */
+    /** @type {!Object.<string, !Promise.<?WebInspector.View>>} */
     this._promiseForId = {};
 
     this._tabbedPane.setRetainTabOrder(true, this._tabOrderComparator.bind(this));
@@ -1231,10 +1231,12 @@ WebInspector.ExtensibleTabbedPaneController.prototype = {
 
         /**
          * @this {WebInspector.ExtensibleTabbedPaneController}
-         * @param {!WebInspector.View} view
+         * @param {?WebInspector.View} view
          */
         function viewLoaded(view)
         {
+            if (!view)
+                return;
             this._tabbedPane.changeTabView(tabId, view);
             var shouldFocus = this._tabbedPane.visibleView.element.isSelfOrAncestor(WebInspector.currentFocusElement());
             if (shouldFocus)
@@ -1252,19 +1254,19 @@ WebInspector.ExtensibleTabbedPaneController.prototype = {
 
     /**
      * @param {string} id
-     * @return {!Promise.<!WebInspector.View>}
+     * @return {!Promise.<?WebInspector.View>}
      */
     viewForId: function(id)
     {
         if (this._views.has(id))
-            return Promise.resolve(/** @type {!WebInspector.View} */ (this._views.get(id)));
+            return Promise.resolve(/** @type {?WebInspector.View} */ (this._views.get(id)));
         if (!this._extensions.has(id))
-            return Promise.reject(new Error("No view registered for given type and id: " + this._extensionPoint + ", " + id));
+            return Promise.resolve(/** @type {?WebInspector.View} */ (null));
         if (this._promiseForId[id])
             return this._promiseForId[id];
 
         var promise = this._extensions.get(id).instancePromise();
-        this._promiseForId[id] = /** @type {!Promise.<!WebInspector.View>} */ (promise);
+        this._promiseForId[id] = /** @type {!Promise.<?WebInspector.View>} */ (promise);
         return promise.then(cacheView.bind(this));
 
         /**
