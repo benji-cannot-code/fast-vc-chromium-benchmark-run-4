2d82f158b3340f97fbb8e43a39933ee6c37e5944
tkent@chromium.org <tkent@chromium.org>
2014-12-02 07:20:44 +0000

    Oilpan: Implement in-place vector backing shrink.
    The new Allocator function vectorBackingShrink should return true if
    the allocator needs to do nothing or it succeeds in-place shrink.
    
    For HeapVector, Vector::shrinkCapacity allocated new buffer and copied its
    content in almost all cases. With DefaultAllocator, it happens only if new
    bucket is different from the old bucket.
    
    This CL improves blink_perf.shadow_dom tests by 2-8%.
    
    BUG=420515
    
    Review URL: https://codereview.chromium.org/758263004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186282 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/heap/Heap.h
+++ third_party/WebKit/Source/platform/heap/Heap.h
@@ -793,6 +793,7 @@ public:
 
     PLATFORM_EXPORT void promptlyFreeObject(Header*);
     PLATFORM_EXPORT bool expandObject(Header*, size_t);
+    void shrinkObject(Header*, size_t);
 
 private:
     void addPageToHeap(const GCInfo*);
@@ -1411,6 +1412,11 @@ public:
     PLATFORM_EXPORT static void vectorBackingFree(void* address);
     PLATFORM_EXPORT static void hashTableBackingFree(void* address);
     PLATFORM_EXPORT static bool vectorBackingExpand(void*, size_t);
+    static inline bool vectorBackingShrink(void* address, size_t quantizedCurrentSize, size_t quantizedShrinkedSize)
+    {
+        vectorBackingShrinkInternal(address, quantizedCurrentSize, quantizedShrinkedSize);
+        return true;
+    }
 
     static void free(void* address) { }
     template<typename T>
@@ -1500,6 +1506,7 @@ private:
     static void backingFree(void*);
     template<typename HeapTraits, typename HeapType, typename HeaderType>
     static bool backingExpand(void*, size_t);
+    PLATFORM_EXPORT static void vectorBackingShrinkInternal(void*, size_t quantizedCurrentSize, size_t quantizedShrunkSize);
 
     template<typename T, size_t u, typename V> friend class WTF::Vector;
     template<typename T, typename U, typename V, typename W> friend class WTF::HashSet;
