bd6795c3c91d746dce8da03cdd3b2328fb7011c7
pkasting <pkasting@chromium.org>
2014-12-02 13:38:01 -0800

    Use QuicPacketCount for the number of dropped packets.
    BUG=81439
    TEST=none
    
    Review URL: https://codereview.chromium.org/775523002
    
    Cr-Commit-Position: refs/heads/master@{#306463}

--- net/tools/quic/quic_socket_utils.h
+++ net/tools/quic/quic_socket_utils.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "net/base/ip_endpoint.h"
+#include "net/quic/quic_bandwidth.h"
 #include "net/quic/quic_types.h"
 
 namespace net {
@@ -29,7 +30,7 @@ class QuicSocketUtils {
   // If the msghdr contains an SO_RXQ_OVFL entry, this will set dropped_packets
   // to the correct value and return true. Otherwise it will return false.
   static bool GetOverflowFromMsghdr(struct msghdr *hdr,
-                                    uint32 *dropped_packets);
+                                    QuicPacketCount *dropped_packets);
 
   // Sets either IP_PKTINFO or IPV6_PKTINFO on the socket, based on
   // address_family.  Returns the return code from setsockopt.
@@ -53,7 +54,7 @@ class QuicSocketUtils {
   static int ReadPacket(int fd,
                         char* buffer,
                         size_t buf_len,
-                        uint32* dropped_packets,
+                        QuicPacketCount* dropped_packets,
                         IPAddressNumber* self_address,
                         IPEndPoint* peer_address);
 
