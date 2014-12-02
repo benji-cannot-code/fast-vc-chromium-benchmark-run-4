72ba8e532be9d0ff34828cacb28b7be6ba38a454
pfeldman <pfeldman@chromium.org>
2014-12-02 09:44:34 -0800

    Revert of Change to Shill's IPConfig objects. (patchset #1 id:1 of https://codereview.chromium.org/762243002/)
    Reason for revert:
    Breaks the build: http://build.chromium.org/p/chromium.webkit/builders/Linux%20ChromiumOS%20Tests%20%281%29/builds/7051
    
    Original issue's description:
    > Change to Shill's IPConfig objects.
    >
    > Before a recent Shill change, Static and Saved IP config properties were stored in toplevel properties with prefixed keys like kStaticIPAddressProperty.
    > This change makes use of Shill now exposing these properties nested under a StaticIPConfig and SavedIPConfig dictionary.
    >
    > This also makes the translation between a list of nameservers and a comma-separated string obsolete.
    >
    > BUG=411289
    >
    > Committed: https://crrev.com/ac2d359b101e4c97455125958db4a999bf26135f
    > Cr-Commit-Position: refs/heads/master@{#306400}
    
    TBR=stevenjb@chromium.org,pneubeck@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=411289
    
    Review URL: https://codereview.chromium.org/776603002
    
    Cr-Commit-Position: refs/heads/master@{#306412}

--- chromeos/network/onc/onc_translator_onc_to_shill.cc
+++ chromeos/network/onc/onc_translator_onc_to_shill.cc
@@ -29,6 +29,17 @@ namespace onc {
 
 namespace {
 
+bool ConvertListValueToStringVector(const base::ListValue& string_list,
+                                    std::vector<std::string>* result) {
+  for (size_t i = 0; i < string_list.GetSize(); ++i) {
+    std::string str;
+    if (!string_list.GetString(i, &str))
+      return false;
+    result->push_back(str);
+  }
+  return true;
+}
+
 scoped_ptr<base::StringValue> ConvertValueToString(const base::Value& value) {
   std::string str;
   if (!value.GetAsString(&str))
@@ -61,6 +72,7 @@ class LocalTranslator {
   void TranslateVPN();
   void TranslateWiFi();
   void TranslateEAP();
+  void TranslateStaticIPConfig();
   void TranslateNetworkConfiguration();
 
   // Copies all entries from |onc_object_| to |shill_dictionary_| for which a
@@ -97,6 +109,8 @@ class LocalTranslator {
 void LocalTranslator::TranslateFields() {
   if (onc_signature_ == &kNetworkConfigurationSignature)
     TranslateNetworkConfiguration();
+  else if (onc_signature_ == &kStaticIPConfigSignature)
+    TranslateStaticIPConfig();
   else if (onc_signature_ == &kEthernetSignature)
     TranslateEthernet();
   else if (onc_signature_ == &kVPNSignature)
@@ -127,6 +141,20 @@ void LocalTranslator::TranslateEthernet() {
   CopyFieldsAccordingToSignature();
 }
 
+
+void LocalTranslator::TranslateStaticIPConfig() {
+  const base::ListValue* onc_nameservers = NULL;
+  if (onc_object_->GetListWithoutPathExpansion(::onc::ipconfig::kNameServers,
+                                               &onc_nameservers)) {
+    std::vector<std::string> onc_nameservers_vector;
+    ConvertListValueToStringVector(*onc_nameservers, &onc_nameservers_vector);
+    std::string shill_nameservers = JoinString(onc_nameservers_vector, ',');
+    shill_dictionary_->SetStringWithoutPathExpansion(
+        shill::kStaticIPNameServersProperty, shill_nameservers);
+  }
+
+  CopyFieldsAccordingToSignature();
+}
 
 void LocalTranslator::TranslateOpenVPN() {
   // SaveCredentials needs special handling when translating from Shill -> ONC
