7677c272429683af74a3a7af23543d839f470085
yusufo <yusufo@chromium.org>
2014-12-02 15:17:54 -0800

    Add VisibleForTesting to InterstitialPage APIs
    These APIs will be used for downstream testing the theme color
    related UI changes.
    
    BUG=434499
    
    Review URL: https://codereview.chromium.org/776683002
    
    Cr-Commit-Position: refs/heads/master@{#306485}

--- content/public/android/java/src/org/chromium/content/browser/webcontents/WebContentsImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/webcontents/WebContentsImpl.java
@@ -7,6 +7,7 @@ package org.chromium.content.browser.webcontents;
 
 import org.chromium.base.CalledByNative;
 import org.chromium.base.JNINamespace;
+import org.chromium.base.VisibleForTesting;
 import org.chromium.content_public.browser.JavaScriptCallback;
 import org.chromium.content_public.browser.NavigationController;
 import org.chromium.content_public.browser.NavigationTransitionDelegate;
@@ -110,6 +111,7 @@ import org.chromium.content_public.browser.WebContents;
         nativeAddStyleSheetByURL(mNativeWebContentsAndroid, url);
     }
 
+    @VisibleForTesting
     @Override
     public void showInterstitialPage(
             String url, long interstitialPageDelegateAndroid) {
