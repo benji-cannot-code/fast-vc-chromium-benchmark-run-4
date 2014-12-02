732c8a226fd9e12690b86eec53d9514738529ca7
mdempsky <mdempsky@chromium.org>
2014-12-01 16:26:33 -0800

    Add STP to third_party
    BUG=414363
    
    Review URL: https://codereview.chromium.org/656373002
    
    Cr-Commit-Position: refs/heads/master@{#306306}

---
+++ third_party/stp/genastkinds.py
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import os.path
+import subprocess
+import sys
+
+(script, source, out,) = sys.argv[1:]
+
+subprocess.check_call([
+  "perl", script,
+  "--file", source,
+])
+os.rename("ASTKind.cpp", os.path.join(out, "src/lib/AST/ASTKind.cpp"))
+os.rename("ASTKind.h", os.path.join(out, "src/include/stp/AST/ASTKind.h"))
