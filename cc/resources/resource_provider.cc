ac9584fc4a2c038df61485e067d83dfef71e9f71
pkotwicz <pkotwicz@chromium.org>
2014-12-02 07:13:58 -0800

    Revert of cc: Added RED_8 to the ResourceFormat enum (patchset #2 id:20001 of https://codereview.chromium.org/735493003/)
    Reason for revert:
    Reverting the CL because it broke the CrOS ozone compile. See http://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Ozone%20Builder/builds/7795/steps/compile/logs/stdio
    
    Original issue's description:
    > cc: Added RED_8 to the ResourceFormat enum
    >
    > New ResourceFormat, RED_8, which translates to GL_UNSIGNED_BYTE on format
    > GL_RED_EXT.  This will be used during YUV playback.
    >
    > At the moment nothing uses it.
    >
    > BUG=435379
    >
    > Committed: https://crrev.com/7550d17cfdab5d173f37e5b58cbd2cfeef3d0ed0
    > Cr-Commit-Position: refs/heads/master@{#306383}
    
    TBR=vmpstr@chromium.org,danakj@chromium.org,reveman@chromium.org,hendrikw@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=435379
    
    Review URL: https://codereview.chromium.org/775663002
    
    Cr-Commit-Position: refs/heads/master@{#306384}

--- cc/resources/resource_provider.cc
+++ cc/resources/resource_provider.cc
@@ -77,7 +77,6 @@ GLenum TextureToStorageFormat(ResourceFormat format) {
     case LUMINANCE_8:
     case RGB_565:
     case ETC1:
-    case RED_8:
       NOTREACHED();
       break;
   }
@@ -96,7 +95,6 @@ bool IsFormatSupportedForStorage(ResourceFormat format, bool use_bgra) {
     case LUMINANCE_8:
     case RGB_565:
     case ETC1:
-    case RED_8:
       return false;
   }
   return false;
@@ -128,7 +126,6 @@ gfx::GpuMemoryBuffer::Format ToGpuMemoryBufferFormat(ResourceFormat format) {
     case LUMINANCE_8:
     case RGB_565:
     case ETC1:
-    case RED_8:
       break;
   }
   NOTREACHED();
