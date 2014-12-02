36444ec3dd10282e12e5802784231a9575a365e3
reillyg <reillyg@chromium.org>
2014-12-02 14:31:30 -0800

    Show an incognito connectability on each tab that is requesting.
    The previous change was too agressive in coalesing user permission
    requests as now only one tab will display an infobar for a given
    extension and origin. This causes confusion as there is no indication
    why a given page is not making any progress. This patch expands the
    tracking done by IncognitoConnectability so that tabs requesting
    connectability each show an infobar and accepting or denying one
    dismisses them all (for a given extension and origin). Other ways of
    dismissing an infobar still only resolve the requests for that
    particular tab, such as navigating away from the page.
    
    This is similar to the tracking done by PermissionQueueController.
    
    BUG=345741
    
    Review URL: https://codereview.chromium.org/753363004
    
    Cr-Commit-Position: refs/heads/master@{#306476}

--- chrome/test/data/extensions/api_test/messaging/externally_connectable/sites/assertions.js
+++ chrome/test/data/extensions/api_test/messaging/externally_connectable/sites/assertions.js
@@ -251,6 +251,12 @@ window.assertions = {
     });
   },
 
+  trySendMessage: function(extensionId) {
+    chrome.runtime.sendMessage(extensionId, kMessage, function(response) {
+      // The result is unimportant. All that matters is the attempt.
+    });
+  },
+
   areAnyRuntimePropertiesDefined: function(names) {
     var result = false;
     if (chrome.runtime) {
