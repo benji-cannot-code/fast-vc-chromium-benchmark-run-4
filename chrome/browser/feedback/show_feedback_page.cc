2cc0be582e6ec9ba723de6f0fa7684daa422598a
xdai <xdai@chromium.org>
2014-12-01 17:42:31 -0800

    Fix the issue that feedback window launches on wrong desktop for teleported window.
    patch from issue 734903003 at patchset 80001 (http://crrev.com/734903003#ps80001)
    
    BUG=352927
    
    Review URL: https://codereview.chromium.org/738303003
    
    Cr-Commit-Position: refs/heads/master@{#306318}

--- chrome/browser/feedback/show_feedback_page.cc
+++ chrome/browser/feedback/show_feedback_page.cc
@@ -9,7 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/feedback_private/feedback_private_api.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/ui/ash/multi_user/multi_user_util.h"
+#include "chrome/browser/ui/ash/multi_user/multi_user_window_manager.h"
 #include "chrome/browser/ui/browser_finder.h"
+#include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "content/public/browser/web_contents.h"
 #include "url/gurl.h"
@@ -61,6 +64,20 @@ void ShowFeedbackPage(Browser* browser,
   // We do not want to launch on an OTR profile.
   profile = profile->GetOriginalProfile();
   DCHECK(profile);
+
+#if defined(OS_CHROMEOS)
+  // Obtains the display profile ID on which the Feedback window should show.
+  chrome::MultiUserWindowManager* const window_manager =
+      chrome::MultiUserWindowManager::GetInstance();
+  const std::string display_profile_id =
+      window_manager && browser
+          ? window_manager->GetUserPresentingWindow(
+                browser->window()->GetNativeWindow())
+          : "";
+  profile = display_profile_id.empty()
+                ? profile
+                : multi_user_util::GetProfileFromUserID(display_profile_id);
+#endif
 
   extensions::FeedbackPrivateAPI* api =
       extensions::FeedbackPrivateAPI::GetFactoryInstance()->Get(profile);
