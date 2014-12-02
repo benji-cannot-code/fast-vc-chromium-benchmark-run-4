2bb45710dd5762fe7eeb437af9b91bf9884bb72c
maclellant <maclellant@chromium.org>
2014-12-02 10:38:37 -0800

    Added OnConfigLoaded() hook to ChromecastConfig.
    This hook will be used by the platform to run logic that depends
    on the pref file being loaded.
    
    Review URL: https://codereview.chromium.org/749623003
    
    Cr-Commit-Position: refs/heads/master@{#306420}

--- chromecast/common/chromecast_config.cc
+++ chromecast/common/chromecast_config.cc
@@ -50,6 +50,7 @@ void ChromecastConfig::Create(PrefRegistrySimple* registry) {
   DCHECK(g_instance_ == NULL);
   g_instance_ = new ChromecastConfig();
   g_instance_->Load(registry);
+  g_instance_->OnConfigLoaded();
 }
 
 // static
--- chromecast/common/chromecast_config_simple.cc
+++ chromecast/common/chromecast_config_simple.cc
@@ -10,4 +10,7 @@ namespace chromecast {
 void ChromecastConfig::RegisterPlatformPrefs(PrefRegistrySimple* registry) {
 }
 
+void ChromecastConfig::OnConfigLoaded() {
+}
+
 }  // namespace chromecast
