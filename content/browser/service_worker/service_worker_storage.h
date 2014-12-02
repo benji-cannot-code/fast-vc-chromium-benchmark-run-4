919c8bfb346639af7571e535bd245328587eacda
nhiroki <nhiroki@chromium.org>
2014-12-01 20:10:32 -0800

    ServiceWorker: Add per-registration key/value storage mechanism in SWDatabase
    This CL provides a storage mechanism to read/write arbitrary data associated
    with a registration. When a registration is deleted, all associated data are
    also deleted.
    
    BUG=435519
    TEST=content_unittests --gtest_filter=ServiceWorkerStorage*:ServiceWorkerDatabase*
    
    Review URL: https://codereview.chromium.org/745583002
    
    Cr-Commit-Position: refs/heads/master@{#306339}

--- content/browser/service_worker/service_worker_storage.h
+++ content/browser/service_worker/service_worker_storage.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <deque>
 #include <map>
 #include <set>
+#include <string>
 #include <vector>
 
 #include "base/bind.h"
@@ -58,6 +59,9 @@ class CONTENT_EXPORT ServiceWorkerStorage
   typedef base::Callback<
       void(ServiceWorkerStatusCode status, bool are_equal)>
           CompareCallback;
+  typedef base::Callback<
+      void(const std::string& data, ServiceWorkerStatusCode status)>
+          GetUserDataCallback;
 
   ~ServiceWorkerStorage() override;
 
@@ -137,6 +141,21 @@ class CONTENT_EXPORT ServiceWorkerStorage
   void CompareScriptResources(int64 lhs_id, int64 rhs_id,
                               const CompareCallback& callback);
 
+  // Provide a storage mechanism to read/write arbitrary data associated with
+  // a registration. Each registration has its own key namespace. Stored data
+  // is deleted when the associated registraton is deleted.
+  void GetUserData(int64 registration_id,
+                   const std::string& key,
+                   const GetUserDataCallback& callback);
+  void StoreUserData(int64 registration_id,
+                     const GURL& origin,
+                     const std::string& key,
+                     const std::string& data,
+                     const StatusCallback& callback);
+  void ClearUserData(int64 registration_id,
+                     const std::string& key,
+                     const StatusCallback& callback);
+
   // Deletes the storage and starts over.
   void DeleteAndStartOver(const StatusCallback& callback);
 
@@ -164,6 +183,7 @@ class CONTENT_EXPORT ServiceWorkerStorage
   void PurgeResources(const ResourceList& resources);
 
  private:
+  friend class ServiceWorkerStorageTest;
   friend class ServiceWorkerResourceStorageTest;
   friend class ServiceWorkerControlleeRequestHandlerTest;
   friend class ServiceWorkerContextRequestHandlerTest;
@@ -222,6 +242,9 @@ class CONTENT_EXPORT ServiceWorkerStorage
       const ServiceWorkerDatabase::RegistrationData& data,
       const ResourceList& resources,
       ServiceWorkerDatabase::Status status)> FindInDBCallback;
+  typedef base::Callback<void(
+      const std::string& data,
+      ServiceWorkerDatabase::Status)> GetUserDataInDBCallback;
   typedef base::Callback<void(const std::vector<int64>& resource_ids,
                               ServiceWorkerDatabase::Status status)>
       GetResourcesCallback;
@@ -279,6 +302,16 @@ class CONTENT_EXPORT ServiceWorkerStorage
       bool origin_is_deletable,
       const ServiceWorkerDatabase::RegistrationData& deleted_version,
       const std::vector<int64>& newly_purgeable_resources,
+      ServiceWorkerDatabase::Status status);
+  void DidStoreUserData(
+      const StatusCallback& callback,
+      ServiceWorkerDatabase::Status status);
+  void DidGetUserData(
+      const GetUserDataCallback& callback,
+      const std::string& data,
+      ServiceWorkerDatabase::Status status);
+  void DidDeleteUserData(
+      const StatusCallback& callback,
       ServiceWorkerDatabase::Status status);
   void ReturnFoundRegistration(
       const FindRegistrationCallback& callback,
@@ -351,6 +384,12 @@ class CONTENT_EXPORT ServiceWorkerStorage
       int64 registration_id,
       const GURL& origin,
       const FindInDBCallback& callback);
+  static void GetUserDataInDB(
+      ServiceWorkerDatabase* database,
+      scoped_refptr<base::SequencedTaskRunner> original_task_runner,
+      int64 registration_id,
+      const std::string& key,
+      const GetUserDataInDBCallback& callback);
   static void DeleteAllDataForOriginsFromDB(
       ServiceWorkerDatabase* database,
       const std::set<GURL>& origins);
