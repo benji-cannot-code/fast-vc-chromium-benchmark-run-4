b27a725a244c816b7885106b9909923e95ef417f
wangxianzhu@chromium.org <wangxianzhu@chromium.org>
2014-12-03 19:51:48 +0000

    Fully invalidate for 'resize' css property
    - Fully invalidate when a RenderBox having 'resize' css property is
      resized;
    - Fully invalidate when 'resize' property changes.
    
    BUG=438101
    TEST=fast/repaint/textarea-appearance-none-resize-handle.html
    TEST=fast/repaint/textarea-resize-property-change.html
    
    Review URL: https://codereview.chromium.org/736003003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186448 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/style/RenderStyle.cpp
+++ third_party/WebKit/Source/core/rendering/style/RenderStyle.cpp
@@ -653,6 +653,9 @@ bool RenderStyle::diffNeedsPaintInvalidationObject(const RenderStyle& other) con
             return true;
     }
 
+    if (resize() != other.resize())
+        return true;
+
     return false;
 }
 
