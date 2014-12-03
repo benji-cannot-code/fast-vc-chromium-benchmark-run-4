f61eb648759f3bdc72e54d0e33f49f01cec8f1d8
jbudorick <jbudorick@chromium.org>
2014-12-03 15:27:48 -0800

    [Android] Add a gtest test instance for the test runner's platform mode.
    BUG=428729
    
    Review URL: https://codereview.chromium.org/781573002
    
    Cr-Commit-Position: refs/heads/master@{#306709}

--- build/android/pylib/base/test_instance_factory.py
+++ build/android/pylib/base/test_instance_factory.py
@@ -3,10 +3,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from pylib import constants
+from pylib.gtest import gtest_test_instance
+from pylib.utils import isolator
 
-def CreateTestInstance(_args, error_func):
 
-  # TODO(jbudorick) Add gtest test instance.
+def CreateTestInstance(args, error_func):
+
+  if args.command == 'gtest':
+    return gtest_test_instance.GtestTestInstance(
+        args, isolator.Isolator(constants.ISOLATE_DEPS_DIR))
   # TODO(jbudorick) Add instrumentation test instance.
-  error_func('No test instances currently supported.')
+
+  error_func('Unable to create %s test instance.' % args.command)
 
