5c79290e2d78ece23e55688e396295f4f7be5069
hendrikw <hendrikw@chromium.org>
2014-12-03 15:49:34 -0800

    cc: Show GPU rasterization status within the HUD
    LayerTreeHost determines the status, passes it to LayerTreeHostImpl.
    
    HUD adds the new line of text before the memory usage.
    
    BUG=434613
    
    Review URL: https://codereview.chromium.org/774943005
    
    Cr-Commit-Position: refs/heads/master@{#306713}

--- cc/trees/layer_tree_host.h
+++ cc/trees/layer_tree_host.h
@@ -67,6 +67,7 @@ class UIResourceRequest;
 struct PendingPageScaleAnimation;
 struct RenderingStats;
 struct ScrollAndScaleSet;
+enum class GpuRasterizationStatus;
 
 // Provides information on an Impl's rendering capabilities back to the
 // LayerTreeHost.
@@ -209,6 +210,7 @@ class CC_EXPORT LayerTreeHost {
   }
   void SetHasGpuRasterizationTrigger(bool has_trigger);
   bool UseGpuRasterization() const;
+  GpuRasterizationStatus GetGpuRasterizationStatus() const;
 
   void SetViewportSize(const gfx::Size& device_viewport_size);
   void SetTopControlsLayoutHeight(float height);
