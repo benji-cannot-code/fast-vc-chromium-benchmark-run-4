b640332870be9f9aabd31d15fb357271663dfe67
jam <jam@chromium.org>
2014-12-03 10:38:07 -0800

    Revert of Revert "[Android] Switch b/a/test_runner.py from optparse to argparse." (patchset #1 id:1 of https://codereview.chromium.org/775863004/)
    Reason for revert:
    this broke 8a9f8022215576925f29e07f0b203cd59e4183fc org.chromium.chrome.browser.omnibox.OmniboxUrlEmphasizerTest#testShortSecureHTTPSUrlWithLightColors
    
    Original issue's description:
    > Revert "[Android] Switch b/a/test_runner.py from optparse to argparse."
    >
    > This reverts commit 8a9f8022215576925f29e07f0b203cd59e4183fc.
    >
    > It broke tests on several bots because the list of accepted GTest suites
    > became restricted to those included in a certain set.
    >
    > TBR=jbudorick@chromium.org, klundberg@chromium.org
    > NOTRY=true
    > BUG=438603
    >
    > Committed: https://crrev.com/bd4e73d47bd0914f6bb43432efa76e90750de962
    > Cr-Commit-Position: refs/heads/master@{#306610}
    
    TBR=perezju@chromium.org,jbudorick@chromium.org,peter@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=438603
    
    Review URL: https://codereview.chromium.org/776973003
    
    Cr-Commit-Position: refs/heads/master@{#306632}

--- build/android/pylib/base/test_run_factory.py
+++ build/android/pylib/base/test_run_factory.py
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-def CreateTestRun(_options, _env, _test_instance, error_func):
+def CreateTestRun(_args, _env, _test_instance, error_func):
 
   # TODO(jbudorick) Add local gtest test runs
   # TODO(jbudorick) Add local instrumentation test runs.
