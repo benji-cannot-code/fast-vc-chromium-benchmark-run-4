d012ea56698ba29de6a1f28104a6e203c2440832
davidben <davidben@chromium.org>
2014-12-03 11:41:51 -0800

    Re-revert of "Remove SSLClientAuthHandler's RDH dependency." (https://codereview.chromium.org/596873002)
    Reason for revert:
    Causes browser crash if URL request is cancelled during client cert loading.
    
    This is a reland of https://codereview.chromium.org/766463002/ which reverted
    https://codereview.chromium.org/596873002. The revert was reverted because it
    introduced a memory leak.
    
    The source of this memory leak was a reference cycle in
    chrome/browser/chromeos/net/client_cert_filter_chromeos.cc whose fix is
    included in this CL. If InitIfSlotsAvailable returns true (synchronously), the
    callback should not be retained. This was not a problem before as the code in
    question was introduced after https://codereview.chromium.org/596873002 which
    unrefcounted an object.
    
    After this is merged into M-40, https://codereview.chromium.org/596873002
    should be reworked with the crash fixed as this reference cycle is too subtle
    and the object really needn't be reference-counted.
    
    TBR=mmenke@chromium.org,benm@chromium.org,jam@chromium.org
    
    BUG=422765, 427844, 376003
    
    Review URL: https://codereview.chromium.org/773003004
    
    Cr-Commit-Position: refs/heads/master@{#306649}

--- chrome/browser/ui/cocoa/ssl_client_certificate_selector_cocoa.h
+++ chrome/browser/ui/cocoa/ssl_client_certificate_selector_cocoa.h
@@ -17,10 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/ui/cocoa/constrained_window/constrained_window_custom_sheet.h"
 #import "chrome/browser/ui/cocoa/constrained_window/constrained_window_sheet_controller.h"
 
-namespace content {
-class BrowserContext;
-}
-
 class ConstrainedWindowMac;
 @class SFChooseIdentityPanel;
 class SSLClientAuthObserverCocoaBridge;
@@ -46,7 +42,7 @@ class SSLClientAuthObserverCocoaBridge;
 
 @property (readonly, nonatomic) SFChooseIdentityPanel* panel;
 
-- (id)initWithBrowserContext:(const content::BrowserContext*)browserContext
+- (id)initWithNetworkSession:(const net::HttpNetworkSession*)networkSession
              certRequestInfo:(net::SSLCertRequestInfo*)certRequestInfo
                     callback:(const chrome::SelectCertificateCallback&)callback;
 - (void)displayForWebContents:(content::WebContents*)webContents;
