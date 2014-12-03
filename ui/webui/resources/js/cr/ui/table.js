c01ae16062ddf832195dd657ef37d0f7b670b52a
fukino <fukino@chromium.org>
2014-12-02 21:32:29 -0800

    Add file_manager and image_loader to Buildbot compilation.
    BUG=406995
    TEST=GYP_GENERATORS=ninja gyp --depth . third_party/closure_compiler/compiled_resources.gyp && ninja -C out/Default
    
    Review URL: https://codereview.chromium.org/777563002
    
    Cr-Commit-Position: refs/heads/master@{#306556}

--- ui/webui/resources/js/cr/ui/table.js
+++ ui/webui/resources/js/cr/ui/table.js
@@ -313,7 +313,7 @@ cr.define('cr.ui', function() {
           // Force styles based on hasElementFocus to take effect.
           list.redraw();
         }
-      });
+      }, 0);
     },
 
     /**
