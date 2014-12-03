2f45393bab9c987c899834b431ed4c3800a09ea5
joaodasilva <joaodasilva@chromium.org>
2014-12-03 02:51:31 -0800

    Add callbacks for requests serviced to TestRequestInterceptor.
    This enables test code to pass a RunLoop::QuitClosure to the interceptor
    and wait until the next pending request is serviced.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/769293002
    
    Cr-Commit-Position: refs/heads/master@{#306584}

--- chrome/browser/policy/cloud/test_request_interceptor.cc
+++ chrome/browser/policy/cloud/test_request_interceptor.cc
@@ -7,10 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <limits>
 #include <queue>
+#include <vector>
 
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/message_loop/message_loop_proxy.h"
 #include "base/run_loop.h"
 #include "base/sequenced_task_runner.h"
 #include "content/public/browser/browser_thread.h"
@@ -169,9 +171,13 @@ class TestRequestInterceptor::Delegate : public net::URLRequestInterceptor {
       net::NetworkDelegate* network_delegate) const override;
 
   void GetPendingSize(size_t* pending_size) const;
+  void AddRequestServicedCallback(const base::Closure& callback);
   void PushJobCallback(const JobCallback& callback);
 
  private:
+  static void InvokeRequestServicedCallbacks(
+      scoped_ptr<std::vector<base::Closure>> callbacks);
+
   const std::string hostname_;
   scoped_refptr<base::SequencedTaskRunner> io_task_runner_;
 
@@ -179,6 +185,10 @@ class TestRequestInterceptor::Delegate : public net::URLRequestInterceptor {
   // const method; it can't reenter though, because it runs exclusively on
   // the IO thread.
   mutable std::queue<JobCallback> pending_job_callbacks_;
+
+  // Queue of pending request serviced callbacks. Mutable for the same reason
+  // as |pending_job_callbacks_|.
+  mutable std::vector<base::Closure> request_serviced_callbacks_;
 };
 
 TestRequestInterceptor::Delegate::Delegate(
@@ -204,6 +214,17 @@ net::URLRequestJob* TestRequestInterceptor::Delegate::MaybeInterceptRequest(
     return BadRequestJobCallback(request, network_delegate);
   }
 
+  // Invoke any callbacks that are waiting for the next request to be serviced
+  // after this job is serviced.
+  if (!request_serviced_callbacks_.empty()) {
+    scoped_ptr<std::vector<base::Closure>> callbacks(
+        new std::vector<base::Closure>);
+    callbacks->swap(request_serviced_callbacks_);
+    io_task_runner_->PostTask(
+        FROM_HERE, base::Bind(&Delegate::InvokeRequestServicedCallbacks,
+                              base::Passed(&callbacks)));
+  }
+
   JobCallback callback = pending_job_callbacks_.front();
   pending_job_callbacks_.pop();
   return callback.Run(request, network_delegate);
@@ -215,10 +236,23 @@ void TestRequestInterceptor::Delegate::GetPendingSize(
   *pending_size = pending_job_callbacks_.size();
 }
 
+void TestRequestInterceptor::Delegate::AddRequestServicedCallback(
+    const base::Closure& callback) {
+  CHECK(io_task_runner_->RunsTasksOnCurrentThread());
+  request_serviced_callbacks_.push_back(callback);
+}
+
 void TestRequestInterceptor::Delegate::PushJobCallback(
     const JobCallback& callback) {
   CHECK(io_task_runner_->RunsTasksOnCurrentThread());
   pending_job_callbacks_.push(callback);
+}
+
+// static
+void TestRequestInterceptor::Delegate::InvokeRequestServicedCallbacks(
+    scoped_ptr<std::vector<base::Closure>> callbacks) {
+  for (const auto& p : *callbacks)
+    p.Run();
 }
 
 TestRequestInterceptor::TestRequestInterceptor(const std::string& hostname,
@@ -248,6 +282,18 @@ size_t TestRequestInterceptor::GetPendingSize() {
                              base::Unretained(delegate_),
                              &pending_size));
   return pending_size;
+}
+
+void TestRequestInterceptor::AddRequestServicedCallback(
+    const base::Closure& callback) {
+  base::Closure post_callback =
+      base::Bind(base::IgnoreResult(&base::MessageLoopProxy::PostTask),
+                 base::MessageLoopProxy::current(),
+                 FROM_HERE,
+                 callback);
+  PostToIOAndWait(base::Bind(&Delegate::AddRequestServicedCallback,
+                             base::Unretained(delegate_),
+                             post_callback));
 }
 
 void TestRequestInterceptor::PushJobCallback(const JobCallback& callback) {
