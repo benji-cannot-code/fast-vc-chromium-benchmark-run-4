3b114d0d1ea1c4c42da449bbb6a315851e2f243f
jbudorick <jbudorick@chromium.org>
2014-12-02 16:16:27 -0800

    [Android] Switch b/a/test_runner.py from optparse to argparse.
    BUG=428729
    
    Review URL: https://codereview.chromium.org/753363002
    
    Cr-Commit-Position: refs/heads/master@{#306501}

--- build/android/pylib/constants.py
+++ build/android/pylib/constants.py
@@ -203,6 +203,8 @@ PYTHON_UNIT_TEST_SUITES = {
 
 LOCAL_MACHINE_TESTS = ['junit', 'python']
 VALID_ENVIRONMENTS = ['local']
+VALID_TEST_TYPES = ['gtest', 'instrumentation', 'junit', 'linker', 'monkey',
+                    'perf', 'python', 'uiautomator']
 
 
 def GetBuildType():
