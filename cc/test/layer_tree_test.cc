93747fafbec4d68b19af037d365a64795db03190
vmpstr <vmpstr@chromium.org>
2014-12-03 08:10:06 -0800

    cc: Ensure to issue only one NotifiyReadyTo* callback per manage tiles.
    This patch ensures that we only issue a single notification that we're
    ready to either activate or draw per manage tiles call.
    
    Previously it was possible that we might issue extra notifications
    due to tile manager's internal rescheduling.
    
    Comes with a test that fails without the patch and passes with the
    patch.
    
    R=reveman, danakj
    
    Review URL: https://codereview.chromium.org/779443003
    
    Cr-Commit-Position: refs/heads/master@{#306616}

--- cc/test/layer_tree_test.cc
+++ cc/test/layer_tree_test.cc
@@ -154,6 +154,11 @@ class ThreadProxyForTest : public ThreadProxy {
     test_hooks_->ScheduledActionBeginOutputSurfaceCreation();
   }
 
+  void ScheduledActionManageTiles() override {
+    ThreadProxy::ScheduledActionManageTiles();
+    test_hooks_->ScheduledActionManageTiles();
+  }
+
   ThreadProxyForTest(
       TestHooks* test_hooks,
       LayerTreeHost* host,
