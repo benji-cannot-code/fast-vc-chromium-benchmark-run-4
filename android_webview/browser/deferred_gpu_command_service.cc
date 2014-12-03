518d435c1562efb059919a394cbb5bce4838def7
boliu <boliu@chromium.org>
2014-12-03 13:13:48 -0800

    aw: Add more trace events to drawing functions
    Add more events with an eye towards debugging as well as accounting
    all the time spent on RenderThread.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/778643003
    
    Cr-Commit-Position: refs/heads/master@{#306676}

--- android_webview/browser/deferred_gpu_command_service.cc
+++ android_webview/browser/deferred_gpu_command_service.cc
@@ -153,6 +153,7 @@ DeferredGpuCommandService::shader_translator_cache() {
 }
 
 void DeferredGpuCommandService::RunTasks() {
+  TRACE_EVENT0("android_webview", "DeferredGpuCommandService::RunTasks");
   bool has_more_tasks;
   {
     base::AutoLock lock(tasks_lock_);
--- android_webview/browser/shared_renderer_state.cc
+++ android_webview/browser/shared_renderer_state.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "android_webview/browser/scoped_app_gl_state_restore.h"
 #include "android_webview/public/browser/draw_gl.h"
 #include "base/bind.h"
+#include "base/debug/trace_event_argument.h"
 #include "base/lazy_instance.h"
 #include "base/location.h"
 
@@ -258,7 +259,10 @@ bool SharedRendererState::ReturnedResourcesEmptyOnUI() const {
 }
 
 void SharedRendererState::DrawGL(AwDrawGLInfo* draw_info) {
+  TRACE_EVENT0("android_webview", "DrawFunctor");
   if (draw_info->mode == AwDrawGLInfo::kModeSync) {
+    TRACE_EVENT_INSTANT0("android_webview", "kModeSync",
+                         TRACE_EVENT_SCOPE_THREAD);
     if (hardware_renderer_)
       hardware_renderer_->CommitFrame();
     return;
