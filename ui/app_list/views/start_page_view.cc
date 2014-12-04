96ee7c2048a4e3c3f8a5775d093cce4bfd0c9ae9
mgiuca <mgiuca@chromium.org>
2014-12-03 19:42:39 -0800

    Experimental app list: Place the search box in the center on start page.
    Previously, it would always be at the top of the page. Now it will be in
    the center when in the start state, and animate up and down as
    appropriate.
    
    BUG=437674
    
    Review URL: https://codereview.chromium.org/768083002
    
    Cr-Commit-Position: refs/heads/master@{#306758}

--- ui/app_list/views/start_page_view.cc
+++ ui/app_list/views/start_page_view.cc
@@ -49,7 +49,7 @@ const int kTileSpacing = 10;
 // (because it is visible on many different pages).
 class SearchBoxSpacerView : public views::View {
  public:
-  SearchBoxSpacerView(const gfx::Size& search_box_size)
+  explicit SearchBoxSpacerView(const gfx::Size& search_box_size)
       : size_(kStartPageSearchBoxWidth, search_box_size.height()) {}
 
   ~SearchBoxSpacerView() override {}
@@ -177,6 +177,10 @@ void StartPageView::Layout() {
 }
 
 void StartPageView::OnContainerSelected(bool from_bottom) {
+}
+
+gfx::Rect StartPageView::GetSearchBoxBounds() const {
+  return search_box_spacer_view_->bounds();
 }
 
 int StartPageView::Update() {
