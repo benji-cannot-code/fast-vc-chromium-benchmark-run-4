b3281caf0bb46d66439bc1903f8ec76372478a76
eustas@chromium.org <eustas@chromium.org>
2014-12-04 22:50:40 +0000

    DevTools: NetworkPanel: Hightlight navigation requests.
    Screenshot: http://picpaste.com/t4pTWi6f.png
    
    BUG=119644
    
    Review URL: https://codereview.chromium.org/732263005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186547 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/network/NetworkDataGridNode.js
+++ third_party/WebKit/Source/devtools/front_end/network/NetworkDataGridNode.js
@@ -42,6 +42,7 @@ WebInspector.NetworkDataGridNode = function(parentView, request)
     this._request = request;
     this._linkifier = new WebInspector.Linkifier();
     this._staleGraph = true;
+    this._isNavigationRequest = false;
 }
 
 WebInspector.NetworkDataGridNode._hoveredRowSymbol = Symbol("hoveredRow");
@@ -53,6 +54,12 @@ WebInspector.NetworkDataGridNode.prototype = {
     request: function()
     {
         return this._request;
+    },
+
+    markAsNavigationRequest: function()
+    {
+        this._isNavigationRequest = true;
+        this.refresh();
     },
 
     /**
@@ -74,6 +81,7 @@ WebInspector.NetworkDataGridNode.prototype = {
         this._initiatorCell = null;
 
         this._element.classList.toggle("network-error-row", this._isFailed());
+        this._element.classList.toggle("network-navigation-row", this._isNavigationRequest);
         WebInspector.SortableDataGridNode.prototype.createCells.call(this);
 
         this._updateGraph();
