47efc925e44bd1196bbe69044430455627e8ff36
dbeam <dbeam@chromium.org>
2014-12-03 21:43:56 -0800

    downloads: defer showing "No downloads" message until downloads are received.
    R=benjhayden@chromium.org
    BUG=437227
    
    Review URL: https://codereview.chromium.org/778853002
    
    Cr-Commit-Position: refs/heads/master@{#306769}

--- chrome/browser/resources/downloads/downloads.js
+++ chrome/browser/resources/downloads/downloads.js
@@ -112,8 +112,6 @@ function Downloads() {
   cr.ui.decorate('command', cr.ui.Command);
   document.addEventListener('canExecute', this.onCanExecute_.bind(this));
   document.addEventListener('command', this.onCommand_.bind(this));
-
-  this.updateResults();
 }
 
 /**
