b1c9d5c2b382762029027fe4bb10227ef41ef826
hashimoto <hashimoto@chromium.org>
2014-12-03 23:41:55 -0800

    Remove dbus::ObjectProxy::filter_added_
    Bus::Add/RemoveFilterFunction is already performing duplication check.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/747903004
    
    Cr-Commit-Position: refs/heads/master@{#306781}

--- dbus/bus.cc
+++ dbus/bus.cc
@@ -691,7 +691,7 @@ void Bus::Send(DBusMessage* request, uint32* serial) {
   CHECK(success) << "Unable to allocate memory";
 }
 
-bool Bus::AddFilterFunction(DBusHandleMessageFunction filter_function,
+void Bus::AddFilterFunction(DBusHandleMessageFunction filter_function,
                             void* user_data) {
   DCHECK(connection_);
   AssertOnDBusThread();
@@ -702,17 +702,16 @@ bool Bus::AddFilterFunction(DBusHandleMessageFunction filter_function,
       filter_functions_added_.end()) {
     VLOG(1) << "Filter function already exists: " << filter_function
             << " with associated data: " << user_data;
-    return false;
+    return;
   }
 
   const bool success = dbus_connection_add_filter(
       connection_, filter_function, user_data, NULL);
   CHECK(success) << "Unable to allocate memory";
   filter_functions_added_.insert(filter_data_pair);
-  return true;
 }
 
-bool Bus::RemoveFilterFunction(DBusHandleMessageFunction filter_function,
+void Bus::RemoveFilterFunction(DBusHandleMessageFunction filter_function,
                                void* user_data) {
   DCHECK(connection_);
   AssertOnDBusThread();
@@ -724,12 +723,11 @@ bool Bus::RemoveFilterFunction(DBusHandleMessageFunction filter_function,
     VLOG(1) << "Requested to remove an unknown filter function: "
             << filter_function
             << " with associated data: " << user_data;
-    return false;
+    return;
   }
 
   dbus_connection_remove_filter(connection_, filter_function, user_data);
   filter_functions_added_.erase(filter_data_pair);
-  return true;
 }
 
 void Bus::AddMatch(const std::string& match_rule, DBusError* error) {
@@ -949,11 +947,8 @@ void Bus::ListenForServiceOwnerChangeInternal(
   if (!Connect() || !SetUpAsyncOperations())
     return;
 
-  if (service_owner_changed_listener_map_.empty()) {
-    bool filter_added =
-        AddFilterFunction(Bus::OnServiceOwnerChangedFilter, this);
-    DCHECK(filter_added);
-  }
+  if (service_owner_changed_listener_map_.empty())
+    AddFilterFunction(Bus::OnServiceOwnerChangedFilter, this);
 
   ServiceOwnerChangedListenerMap::iterator it =
       service_owner_changed_listener_map_.find(service_name);
@@ -1027,11 +1022,8 @@ void Bus::UnlistenForServiceOwnerChangeInternal(
   // And remove |service_owner_changed_listener_map_| entry.
   service_owner_changed_listener_map_.erase(it);
 
-  if (service_owner_changed_listener_map_.empty()) {
-    bool filter_removed =
-        RemoveFilterFunction(Bus::OnServiceOwnerChangedFilter, this);
-    DCHECK(filter_removed);
-  }
+  if (service_owner_changed_listener_map_.empty())
+    RemoveFilterFunction(Bus::OnServiceOwnerChangedFilter, this);
 }
 
 dbus_bool_t Bus::OnAddWatch(DBusWatch* raw_watch) {
--- dbus/object_manager.cc
+++ dbus/object_manager.cc
@@ -156,8 +156,7 @@ void ObjectManager::CleanUp() {
   if (!setup_success_)
     return;
 
-  if (!bus_->RemoveFilterFunction(&ObjectManager::HandleMessageThunk, this))
-    LOG(ERROR) << "Failed to remove filter function";
+  bus_->RemoveFilterFunction(&ObjectManager::HandleMessageThunk, this);
 
   ScopedDBusError error;
   bus_->RemoveMatch(match_rule_, error.get());
@@ -217,10 +216,7 @@ bool ObjectManager::SetupMatchRuleAndFilter() {
           kPropertiesInterface,
           kPropertiesChanged);
 
-  if (!bus_->AddFilterFunction(&ObjectManager::HandleMessageThunk, this)) {
-    LOG(ERROR) << "ObjectManager failed to add filter function";
-    return false;
-  }
+  bus_->AddFilterFunction(&ObjectManager::HandleMessageThunk, this);
 
   ScopedDBusError error;
   bus_->AddMatch(match_rule, error.get());
