a926dc7558c102e1b11f943cc06bff4b04fa1694
skobes@chromium.org <skobes@chromium.org>
2014-12-04 23:43:00 +0000

    Make --dump-render-tree include layers hidden by clipping.
    This allows layout tests to test content that is scrolled out of view, e.g.:
    
    <div style="overflow: scroll; height: 100px">
    <div style="position: relative; top: 100px">
      this content is currently not displayed by --dump-render-tree
    </div>
    </div>
    
    BUG=438888
    
    Review URL: https://codereview.chromium.org/777703003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186551 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebLocalFrameImpl.cpp
+++ third_party/WebKit/Source/web/WebLocalFrameImpl.cpp
@@ -1488,7 +1488,7 @@ WebString WebLocalFrameImpl::contentAsMarkup() const
 
 WebString WebLocalFrameImpl::renderTreeAsText(RenderAsTextControls toShow) const
 {
-    RenderAsTextBehavior behavior = RenderAsTextBehaviorNormal;
+    RenderAsTextBehavior behavior = RenderAsTextShowAllLayers;
 
     if (toShow & RenderAsTextDebug)
         behavior |= RenderAsTextShowCompositedLayers | RenderAsTextShowAddresses | RenderAsTextShowIDAndClass | RenderAsTextShowLayerNesting;
