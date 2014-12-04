37df09a2bebb756bf8af0d561b82572f88163e46
sheckylin <sheckylin@chromium.org>
2014-12-04 00:23:59 -0800

    Add input device setting APIs to autotestPrivate
    The CL adds APIs in autotestPrivate so that one can set input
    device settings from an autotest. These settings appears as
    touchpad/mouse related settings in the user preference page.
    
    Contributed by sheckylin@chromium.org
    
    BUG=434471,400022
    TEST=Manual
    
    Review URL: https://codereview.chromium.org/764273002
    
    Cr-Commit-Position: refs/heads/master@{#306785}

--- chrome/browser/chromeos/system/input_device_settings.h
+++ chrome/browser/chromeos/system/input_device_settings.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/logging.h"
+#include "chromeos/chromeos_export.h"
 
 namespace chromeos {
 namespace system {
@@ -147,7 +148,7 @@ class MouseSettings {
 };
 
 // Interface for configuring input device settings.
-class InputDeviceSettings {
+class CHROMEOS_EXPORT InputDeviceSettings {
  public:
   typedef base::Callback<void(bool)> DeviceExistsCallback;
 
