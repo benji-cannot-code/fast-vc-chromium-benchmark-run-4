a41990dff44f8cc472f0eeea0f22f36d4c6a8eba
myid.o.shin@gmail.com <myid.o.shin@gmail.com>
2014-12-04 23:41:42 +0000

    Fix the problem to keep the selection when clicking the substring out of range
    The problem is that it's not consistent to keep the selection when clicking the
    substiring in/out of range. We need the additional condition missing
    from http://crrev.com/507533002
    
    BUG=32865,438102
    R=yosin@chromium.org
    R=rbyers@chromium.org
    TEST=fast/forms/setrangetext-out-of-range.html
    
    Review URL: https://codereview.chromium.org/764313004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186550 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/EventHandler.cpp
+++ third_party/WebKit/Source/core/page/EventHandler.cpp
@@ -562,7 +562,7 @@ bool EventHandler::handleMousePressEventSingleClick(const MouseEventWithHitTestR
             granularity = m_frame->selection().granularity();
             newSelection.expandUsingGranularity(m_frame->selection().granularity());
         }
-    } else {
+    } else if (m_selectionInitiationState != ExtendedSelection) {
         newSelection = expandSelectionToRespectUserSelectAll(innerNode, VisibleSelection(visiblePos));
     }
 
@@ -2991,7 +2991,8 @@ void EventHandler::notifyElementActivated()
 
 void EventHandler::notifySelectionChanged()
 {
-    m_selectionInitiationState = ExtendedSelection;
+    if (m_frame->selection().isRange())
+        m_selectionInitiationState = ExtendedSelection;
 }
 
 bool EventHandler::handleAccessKey(const PlatformKeyboardEvent& evt)
