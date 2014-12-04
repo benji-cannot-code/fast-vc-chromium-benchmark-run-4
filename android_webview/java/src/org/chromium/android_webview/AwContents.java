c64bd48a4763507a53694ca8efc3e00f2c4257c5
boliu <boliu@chromium.org>
2014-12-03 18:21:37 -0800

    aw: Use explicitly named trace event in java
    TraceEvent gets and parses the calling stack to get the name otherwise,
    which is super slow on performance sensitive code.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/780713003
    
    Cr-Commit-Position: refs/heads/master@{#306748}

--- android_webview/java/src/org/chromium/android_webview/AwContents.java
+++ android_webview/java/src/org/chromium/android_webview/AwContents.java
@@ -1053,10 +1053,10 @@ public class AwContents implements SmartClipProvider {
 
     public void onDraw(Canvas canvas) {
         try {
-            TraceEvent.begin();
+            TraceEvent.begin("AwContents.onDraw");
             mAwViewMethods.onDraw(canvas);
         } finally {
-            TraceEvent.end();
+            TraceEvent.end("AwContents.onDraw");
         }
     }
 
