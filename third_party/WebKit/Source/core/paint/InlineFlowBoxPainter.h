4d2013f69098795af47ba180aee02eb789de8d89
fs@opera.com <fs@opera.com>
2014-12-04 19:09:52 +0000

    Remove duplicated code for image-strip computation in InlineFlowBoxPainter
    The code to compute the "strip"/"slice" of an inline was duplicated for
    the border, mask and fill cases.
    Move this code to a new method paintRectForImageStrip.
    
    BUG=423071
    
    Review URL: https://codereview.chromium.org/752373003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186531 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/paint/InlineFlowBoxPainter.h
+++ third_party/WebKit/Source/core/paint/InlineFlowBoxPainter.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/rendering/style/ShadowData.h"
 #include "platform/graphics/GraphicsTypes.h"
+#include "platform/text/TextDirection.h"
 
 namespace blink {
 
@@ -16,6 +17,7 @@ class FillLayer;
 class InlineFlowBox;
 class LayoutPoint;
 class LayoutRect;
+class LayoutSize;
 class LayoutUnit;
 struct PaintInfo;
 class RenderStyle;
@@ -32,6 +34,7 @@ private:
     void paintFillLayer(const PaintInfo&, const Color&, const FillLayer&, const LayoutRect&, CompositeOperator);
     void paintBoxShadow(const PaintInfo&, RenderStyle*, ShadowStyle, const LayoutRect&);
     LayoutRect roundedFrameRectClampedToLineTopAndBottomIfNeeded() const;
+    LayoutRect paintRectForImageStrip(const LayoutPoint&, const LayoutSize&, TextDirection) const;
 
     InlineFlowBox& m_inlineFlowBox;
 };
