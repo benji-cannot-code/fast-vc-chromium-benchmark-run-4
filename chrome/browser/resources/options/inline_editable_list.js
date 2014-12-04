44662cc79414d0e422240a6d26779c2dcc63a68b
fukino <fukino@chromium.org>
2014-12-03 21:54:28 -0800

    Fix closure errors in options.
    BUG=none
    
    Review URL: https://codereview.chromium.org/773393002
    
    Cr-Commit-Position: refs/heads/master@{#306772}

--- chrome/browser/resources/options/inline_editable_list.js
+++ chrome/browser/resources/options/inline_editable_list.js
@@ -685,7 +685,8 @@ cr.define('options', function() {
     * @protected
     */
     getInitialFocusableItem: function() {
-      return this.getListItemByIndex(0);
+      return /** @type {options.InlineEditableItem} */(
+          this.getListItemByIndex(0));
     },
   };
 
