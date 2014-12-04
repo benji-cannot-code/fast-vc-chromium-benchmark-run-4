2e43c700aa236f2c9b7299800df4db0af056e72a
dfalcantara <dfalcantara@chromium.org>
2014-12-04 10:57:16 -0800

    Upstream historical tab logic
    Moves code from ChromeTab to Tab and TabState.
    
    BUG=415747
    
    Review URL: https://codereview.chromium.org/774183004
    
    Cr-Commit-Position: refs/heads/master@{#306861}

--- chrome/android/java/src/org/chromium/chrome/browser/Tab.java
+++ chrome/android/java/src/org/chromium/chrome/browser/Tab.java
@@ -1201,6 +1201,14 @@ public class Tab {
     }
 
     /**
+     * Restores a tab either frozen or from state.
+     * TODO(aurimas): investigate reducing the visibility of this method after TabModel refactoring.
+     */
+    public void createHistoricalTab() {
+        nativeCreateHistoricalTab(mNativeTabAndroid);
+    }
+
+    /**
      * @return An unused id.
      */
     private static int generateNextId() {
@@ -1243,4 +1251,5 @@ public class Tab {
             String title);
     private native boolean nativePrint(long nativeTabAndroid);
     private native Bitmap nativeGetDefaultFavicon(long nativeTabAndroid);
+    private native void nativeCreateHistoricalTab(long nativeTabAndroid);
 }
