421a5e05705aee73bdea331207437cc7806fe3e1
dpolukhin <dpolukhin@chromium.org>
2014-12-04 07:22:40 -0800

    Fix unintialized usage in chromeos::LoginManagerTest
    BUG=438223
    TEST=existing tests
    
    Review URL: https://codereview.chromium.org/781513002
    
    Cr-Commit-Position: refs/heads/master@{#306814}

--- chrome/browser/chromeos/login/screenshot_testing/screenshot_testing_mixin.cc
+++ chrome/browser/chromeos/login/screenshot_testing/screenshot_testing_mixin.cc
@@ -14,7 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromeos {
 
-ScreenshotTestingMixin::ScreenshotTestingMixin() {
+ScreenshotTestingMixin::ScreenshotTestingMixin()
+    : enable_test_screenshots_(false) {
 }
 
 ScreenshotTestingMixin::~ScreenshotTestingMixin() {
