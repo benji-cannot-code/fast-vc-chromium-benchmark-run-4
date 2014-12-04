bda3bc793c293db1b8c3e144d47bb4aa9e953fb0
mstensho@opera.com <mstensho@opera.com>
2014-12-04 19:30:48 +0000

    Remove RenderFlowThread::offsetFromLogicalTopOfFirstRegion().
    What RenderBlock::offsetFromLogicalTopOfFirstPage() does via its
    layout state also suffices for flowthread-based multicol.
    
    This also made 'flow thread layout state' vanish into thin air.
    
    R=jchaffraix@chromium.org
    
    Review URL: https://codereview.chromium.org/775753002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186532 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderView.h
+++ third_party/WebKit/Source/core/rendering/RenderView.h
@@ -161,8 +161,8 @@ public:
     double layoutViewportWidth() const;
     double layoutViewportHeight() const;
 
-    void pushLayoutState(LayoutState&);
-    void popLayoutState();
+    void pushLayoutState(LayoutState& layoutState) { m_layoutState = &layoutState; }
+    void popLayoutState() { ASSERT(m_layoutState); m_layoutState = m_layoutState->next(); }
     virtual void invalidateTreeIfNeeded(const PaintInvalidationState&) override final;
 
 private:
