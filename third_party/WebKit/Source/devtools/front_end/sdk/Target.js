e0058309a1c3f811e5fa39809802058d7682e317
dmurph@chromium.org <dmurph@chromium.org>
2014-12-04 21:46:48 +0000

    Check existance of models on targets before event work
    BUG=
    
    Review URL: https://codereview.chromium.org/773953002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186542 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sdk/Target.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/Target.js
@@ -389,7 +389,8 @@ WebInspector.TargetManager.prototype = {
     {
         for (var i = 0; i < this._targets.length; ++i) {
             var model = this._targets[i]._modelByConstructor.get(modelClass);
-            model.addEventListener(eventType, listener, thisObject);
+            if (model)
+                model.addEventListener(eventType, listener, thisObject);
         }
         if (!this._modelListeners[eventType])
             this._modelListeners[eventType] = [];
@@ -409,7 +410,8 @@ WebInspector.TargetManager.prototype = {
 
         for (var i = 0; i < this._targets.length; ++i) {
             var model = this._targets[i]._modelByConstructor.get(modelClass);
-            model.removeEventListener(eventType, listener, thisObject);
+            if (model)
+                model.removeEventListener(eventType, listener, thisObject);
         }
 
         var listeners = this._modelListeners[eventType];
@@ -480,7 +482,8 @@ WebInspector.TargetManager.prototype = {
             var listeners = this._modelListeners[eventType];
             for (var i = 0; i < listeners.length; ++i) {
                 var model = target._modelByConstructor.get(listeners[i].modelClass);
-                model.addEventListener(eventType, listeners[i].listener, listeners[i].thisObject);
+                if (model)
+                    model.addEventListener(eventType, listeners[i].listener, listeners[i].thisObject);
             }
         }
     },
@@ -505,7 +508,8 @@ WebInspector.TargetManager.prototype = {
             var listeners = this._modelListeners[eventType];
             for (var i = 0; i < listeners.length; ++i) {
                 var model = target._modelByConstructor.get(listeners[i].modelClass);
-                model.removeEventListener(eventType, listeners[i].listener, listeners[i].thisObject);
+                if (model)
+                    model.removeEventListener(eventType, listeners[i].listener, listeners[i].thisObject);
             }
         }
     },
