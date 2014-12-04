f876344671637b1caddda7e20c930ab39cdf57ab
ppi <ppi@chromium.org>
2014-12-04 05:55:06 -0800

    Allow the content client to override browser Mojo services.
    This allows the content embedder to inject its own implementations of
    Mojo services available in the browser.
    
    BUG=421796
    BUG=416426
    
    Review URL: https://codereview.chromium.org/772433004
    
    Cr-Commit-Position: refs/heads/master@{#306808}

--- content/public/browser/content_browser_client.cc
+++ content/public/browser/content_browser_client.cc
@@ -321,6 +321,13 @@ net::CookieStore* ContentBrowserClient::OverrideCookieStoreForRenderProcess(
   return NULL;
 }
 
+bool ContentBrowserClient::CheckMediaAccessPermission(
+    BrowserContext* browser_context,
+    const GURL& security_origin,
+    MediaStreamType type) {
+  return false;
+}
+
 #if defined(OS_WIN)
 const wchar_t* ContentBrowserClient::GetResourceDllName() {
   return NULL;
@@ -334,12 +341,5 @@ ContentBrowserClient::OverrideCreateExternalVideoSurfaceContainer(
   return NULL;
 }
 #endif
-
-bool ContentBrowserClient::CheckMediaAccessPermission(
-    BrowserContext* browser_context,
-    const GURL& security_origin,
-    MediaStreamType type) {
-  return false;
-}
 
 }  // namespace content
