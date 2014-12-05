c7fa413d86655414f1d26de99fd3072d4dfa4d4e
kelvinp <kelvinp@chromium.org>
2014-12-05 15:35:58 -0800

    Remove flag for Remote Assistance on Chrome OS
    This CL enables Remote Assistance on Chrome OS by default.
    
    BUG=439582
    
    Review URL: https://codereview.chromium.org/782823002
    
    Cr-Commit-Position: refs/heads/master@{#307113}

--- chrome/browser/extensions/api/messaging/native_message_host_chromeos.cc
+++ chrome/browser/extensions/api/messaging/native_message_host_chromeos.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/bind_helpers.h"
-#include "base/command_line.h"
 #include "base/json/json_reader.h"
 #include "base/json/json_writer.h"
 #include "base/location.h"
@@ -21,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/messaging/native_messaging_test_util.h"
 #include "components/policy/core/common/policy_service.h"
 #include "extensions/common/constants.h"
-#include "extensions/common/switches.h"
 #include "extensions/common/url_pattern.h"
 #include "net/url_request/url_request_context_getter.h"
 #include "remoting/host/chromoting_host_context.h"
@@ -97,19 +95,15 @@ struct BuiltInHost {
 };
 
 scoped_ptr<NativeMessageHost> CreateIt2MeHost() {
-  if (CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kEnableRemoteAssistance)) {
-    scoped_ptr<remoting::It2MeHostFactory> host_factory(
-        new remoting::It2MeHostFactory());
-    host_factory->set_policy_service(g_browser_process->policy_service());
-    scoped_ptr<remoting::ChromotingHostContext> context =
-        remoting::ChromotingHostContext::CreateForChromeOS(
-            make_scoped_refptr(g_browser_process->system_request_context()));
-    scoped_ptr<NativeMessageHost> host(new remoting::It2MeNativeMessagingHost(
-        context.Pass(), host_factory.Pass()));
-    return host.Pass();
-  }
-  return nullptr;
+  scoped_ptr<remoting::It2MeHostFactory> host_factory(
+      new remoting::It2MeHostFactory());
+  host_factory->set_policy_service(g_browser_process->policy_service());
+  scoped_ptr<remoting::ChromotingHostContext> context =
+      remoting::ChromotingHostContext::CreateForChromeOS(
+          make_scoped_refptr(g_browser_process->system_request_context()));
+  scoped_ptr<NativeMessageHost> host(new remoting::It2MeNativeMessagingHost(
+      context.Pass(), host_factory.Pass()));
+  return host.Pass();
 }
 
 // If you modify the list of allowed_origins, don't forget to update
