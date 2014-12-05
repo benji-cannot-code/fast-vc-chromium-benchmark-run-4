91397cfc31433c088fbfd53dbe75d7b0d2d0e124
rob.buis@samsung.com <rob.buis@samsung.com>
2014-12-05 22:21:11 +0000

    Be more strict about multiple documentElements
    We used to allow (mistakenly) multiple documentElements
    when appending a DocumentFragment to a document, as
    docfragment-multiple-document-elements.html shows.
    
    This patch detects that case by making canReplaceChild
    also handle appending/inserting. To make this more clear
    the method is now called canAcceptChild.
    
    Finally fix the FIXME by making canAcceptChild throw
    more specific exception messages than before.
    
    BUG=434111
    
    Review URL: https://codereview.chromium.org/779173002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186625 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -3259,9 +3259,9 @@ bool Document::childTypeAllowed(NodeType type) const
     return false;
 }
 
-bool Document::canReplaceChild(const Node& newChild, const Node& oldChild) const
+bool Document::canAcceptChild(const Node& newChild, const Node* oldChild, ExceptionState& exceptionState) const
 {
-    if (oldChild.nodeType() == newChild.nodeType())
+    if (oldChild && oldChild->nodeType() == newChild.nodeType())
         return true;
 
     int numDoctypes = 0;
@@ -3269,11 +3269,11 @@ bool Document::canReplaceChild(const Node& newChild, const Node& oldChild) const
 
     // First, check how many doctypes and elements we have, not counting
     // the child we're about to remove.
-    for (Node& c : NodeTraversal::childrenOf(*this)) {
-        if (c == oldChild)
+    for (Node& child : NodeTraversal::childrenOf(*this)) {
+        if (oldChild && *oldChild == child)
             continue;
 
-        switch (c.nodeType()) {
+        switch (child.nodeType()) {
         case DOCUMENT_TYPE_NODE:
             numDoctypes++;
             break;
@@ -3287,13 +3287,15 @@ bool Document::canReplaceChild(const Node& newChild, const Node& oldChild) const
 
     // Then, see how many doctypes and elements might be added by the new child.
     if (newChild.isDocumentFragment()) {
-        for (Node& c : NodeTraversal::childrenOf(toDocumentFragment(newChild))) {
-            switch (c.nodeType()) {
+        for (Node& child : NodeTraversal::childrenOf(toDocumentFragment(newChild))) {
+            switch (child.nodeType()) {
             case ATTRIBUTE_NODE:
             case CDATA_SECTION_NODE:
             case DOCUMENT_FRAGMENT_NODE:
             case DOCUMENT_NODE:
             case TEXT_NODE:
+                exceptionState.throwDOMException(HierarchyRequestError, "Nodes of type '" + newChild.nodeName() +
+                    "' may not be inserted inside nodes of type '#document'.");
                 return false;
             case COMMENT_NODE:
             case PROCESSING_INSTRUCTION_NODE:
@@ -3313,6 +3315,8 @@ bool Document::canReplaceChild(const Node& newChild, const Node& oldChild) const
         case DOCUMENT_FRAGMENT_NODE:
         case DOCUMENT_NODE:
         case TEXT_NODE:
+            exceptionState.throwDOMException(HierarchyRequestError, "Nodes of type '" + newChild.nodeName() +
+                "' may not be inserted inside nodes of type '#document'.");
             return false;
         case COMMENT_NODE:
         case PROCESSING_INSTRUCTION_NODE:
@@ -3326,8 +3330,12 @@ bool Document::canReplaceChild(const Node& newChild, const Node& oldChild) const
         }
     }
 
-    if (numElements > 1 || numDoctypes > 1)
+    if (numElements > 1 || numDoctypes > 1) {
+        exceptionState.throwDOMException(HierarchyRequestError,
+            String::format("Only one %s on document allowed.",
+            numElements > 1 ? "element" : "doctype"));
         return false;
+    }
 
     return true;
 }
