4b35d8f23d6bde8319c7e872c1f014d453e32b53
dfalcantara <dfalcantara@chromium.org>
2014-12-04 19:16:20 -0800

    Upstream LaunchType in Tab
    Moves the field and the accessor upstream.
    
    Setting to NOTRY=true because tests pass on relevant bots (linux doesn't use that Java file).
    
    BUG=415747
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/779183002
    
    Cr-Commit-Position: refs/heads/master@{#306965}

--- chrome/android/java/src/org/chromium/chrome/browser/Tab.java
+++ chrome/android/java/src/org/chromium/chrome/browser/Tab.java
@@ -28,6 +28,7 @@ import org.chromium.chrome.browser.dom_distiller.DomDistillerFeedbackReporter;
 import org.chromium.chrome.browser.infobar.AutoLoginProcessor;
 import org.chromium.chrome.browser.infobar.InfoBarContainer;
 import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.chrome.browser.tabmodel.TabModel.TabLaunchType;
 import org.chromium.chrome.browser.toolbar.ToolbarModel;
 import org.chromium.content.browser.ContentView;
 import org.chromium.content.browser.ContentViewClient;
@@ -153,6 +154,16 @@ public class Tab {
 
     /** Whether or not the TabState has changed. */
     private boolean mIsTabStateDirty = true;
+
+    /**
+     * Saves how this tab was launched (from a link, external app, etc) so that
+     * we can determine the different circumstances in which it should be
+     * closed. For example, a tab opened from an external app should be closed
+     * when the back stack is empty and the user uses the back hardware key. A
+     * standard tab however should be kept open and the entire activity should
+     * be moved to the background.
+     */
+    private final TabLaunchType mLaunchType;
 
     /**
      * A default {@link ChromeContextMenuItemDelegate} that supports some of the context menu
@@ -372,7 +383,7 @@ public class Tab {
      * @param window    An instance of a {@link WindowAndroid}.
      */
     public Tab(int id, boolean incognito, Context context, WindowAndroid window) {
-        this(id, INVALID_TAB_ID, incognito, context, window);
+        this(id, INVALID_TAB_ID, incognito, context, window, null);
     }
 
     /**
@@ -383,7 +394,8 @@ public class Tab {
      * @param context   An instance of a {@link Context}.
      * @param window    An instance of a {@link WindowAndroid}.
      */
-    public Tab(int id, int parentId, boolean incognito, Context context, WindowAndroid window) {
+    public Tab(int id, int parentId, boolean incognito, Context context, WindowAndroid window,
+            TabLaunchType type) {
         // We need a valid Activity Context to build the ContentView with.
         assert context == null || context instanceof Activity;
 
@@ -394,6 +406,7 @@ public class Tab {
         mContext = context;
         mApplicationContext = context != null ? context.getApplicationContext() : null;
         mWindowAndroid = window;
+        mLaunchType = type;
         if (mContext != null) {
             mNumPixel16DP = (int) (DeviceDisplayInfo.create(mContext).getDIPScale() * 16);
         }
@@ -1251,6 +1264,13 @@ public class Tab {
      */
     public void createHistoricalTab() {
         nativeCreateHistoricalTab(mNativeTabAndroid);
+    }
+
+    /**
+     * @return The reason the Tab was launched.
+     */
+    public TabLaunchType getLaunchType() {
+        return mLaunchType;
     }
 
     /**
