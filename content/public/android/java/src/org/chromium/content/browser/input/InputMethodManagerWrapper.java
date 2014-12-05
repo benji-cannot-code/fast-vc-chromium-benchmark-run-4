61396a2c62e3b66300b28cef3fa3e577c0ae7ee2
yukawa <yukawa@chromium.org>
2014-12-04 23:53:42 -0800

    Replace #updateCursor with #updateCursorAnchorInfo
    This is a groundwork to enable InputMethodManager#updateCursorAnchorInfo
    support for Android 5.0.
    
    Note that this CL only updates InputMethodManagerWrapper and no one is
    using the new API yet.  Hence this CL is supposed to have no impact on
    the production.
    
    In Android 5.0, InputMethodManager#updateCursor and
    InputMethodManager#isWatchingCursor are deprecated in favor of a new API
    called InputMethodManager#updateCursorAnchorInfo.  This CL updates
    InputMethodManagerWrapper accordingly.
    
    BUG=424866
    TEST=unittest
    
    Review URL: https://codereview.chromium.org/755853004
    
    Cr-Commit-Position: refs/heads/master@{#306996}

--- content/public/android/java/src/org/chromium/content/browser/input/InputMethodManagerWrapper.java
+++ content/public/android/java/src/org/chromium/content/browser/input/InputMethodManagerWrapper.java
@@ -5,10 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.content.browser.input;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.os.Build;
 import android.os.IBinder;
 import android.os.ResultReceiver;
 import android.view.View;
+import android.view.inputmethod.CursorAnchorInfo;
 import android.view.inputmethod.InputMethodManager;
 
 /**
@@ -64,22 +67,13 @@ public class InputMethodManagerWrapper {
     }
 
     /**
-     * @see android.view.inputmethod.InputMethodManager#isWatchingCursor(View)
+     * @see android.view.inputmethod.InputMethodManager#updateCursorAnchorInfo(View,
+     * CursorAnchorInfo)
      */
-    @SuppressWarnings("deprecation")
-    public boolean isWatchingCursor(View view) {
-        // TODO(aurimas): InputMethodManager.isWatchingCursor() was deprecated in L. Fix
-        // this once the final Android L SDK is released.
-        return getInputMethodManager().isWatchingCursor(view);
-    }
-
-    /**
-     * @see android.view.inputmethod.InputMethodManager#updateCursor(View, int, int, int, int)
-     */
-    @SuppressWarnings("deprecation")
-    public void updateCursor(View view, int left, int top, int right, int bottom) {
-        // TODO(aurimas): InputMethodManager.updateCursor() was deprecated in L. Fix
-        // this once the final Android L SDK is released.
-        getInputMethodManager().updateCursor(view, left, top, right, bottom);
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public void updateCursorAnchorInfo(View view, CursorAnchorInfo cursorAnchorInfo) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getInputMethodManager().updateCursorAnchorInfo(view, cursorAnchorInfo);
+        }
     }
 }
