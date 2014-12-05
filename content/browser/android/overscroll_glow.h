d377d71c5e9a592c2ef629a898df5a2f080e9caa
tfarina <tfarina@chromium.org>
2014-12-04 18:17:10 -0800

    Cleanup: Update the path to vector2d_f.h header.
    The geometry headers were moved from /ui/gfx/ to ui/gfx/geometry, so
    we are updating the include paths to fix this.
    
    BUG=395370
    TEST=gfx_unittests
    TBR=sky@chromium.org,danakj@chromium.org
    
    Review URL: https://codereview.chromium.org/781863004
    
    Cr-Commit-Position: refs/heads/master@{#306960}

--- content/browser/android/overscroll_glow.h
+++ content/browser/android/overscroll_glow.h
@@ -9,8 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/time/time.h"
-#include "ui/gfx/size_f.h"
-#include "ui/gfx/vector2d_f.h"
+#include "ui/gfx/geometry/size_f.h"
+#include "ui/gfx/geometry/vector2d_f.h"
 
 namespace cc {
 class Layer;
--- content/public/browser/android/synchronous_compositor_client.h
+++ content/public/browser/android/synchronous_compositor_client.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "ui/gfx/geometry/size_f.h"
-#include "ui/gfx/vector2d_f.h"
+#include "ui/gfx/geometry/vector2d_f.h"
 
 namespace content {
 
