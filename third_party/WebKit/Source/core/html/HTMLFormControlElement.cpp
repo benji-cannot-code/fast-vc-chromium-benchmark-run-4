9632b8082f8075c8ba2598246acd94cc7c39d5ef
thestig@chromium.org <thestig@chromium.org>
2014-12-05 08:53:02 +0000

    Call didAssociateFormControl() for form control elements without a form owner.
    BUG=428919
    
    Review URL: https://codereview.chromium.org/753073004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186574 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLFormControlElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLFormControlElement.cpp
@@ -254,6 +254,11 @@ Node::InsertionNotificationRequest HTMLFormControlElement::insertedInto(Containe
     HTMLElement::insertedInto(insertionPoint);
     FormAssociatedElement::insertedInto(insertionPoint);
     fieldSetAncestorsSetNeedsValidityCheck(insertionPoint);
+
+    // Trigger for elements outside of forms.
+    if (!formOwner() && insertionPoint->inDocument())
+        document().didAssociateFormControl(this);
+
     return InsertionDone;
 }
 
@@ -267,6 +272,7 @@ void HTMLFormControlElement::removedFrom(ContainerNode* insertionPoint)
     setNeedsWillValidateCheck();
     HTMLElement::removedFrom(insertionPoint);
     FormAssociatedElement::removedFrom(insertionPoint);
+    document().removeFormAssociation(this);
 }
 
 void HTMLFormControlElement::willChangeForm()
