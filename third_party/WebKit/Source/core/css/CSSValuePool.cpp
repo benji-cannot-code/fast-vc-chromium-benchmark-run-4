80eae8cf503fcc2a4e28c724ae7ccdb51fb84b2f
sigbjornf@opera.com <sigbjornf@opera.com>
2014-12-05 20:35:47 +0000

    Oilpan: fix build after r186604.
    TBR=oilpan-reviews,haraken
    BUG=431689
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/750863004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186622 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/CSSValuePool.cpp
+++ third_party/WebKit/Source/core/css/CSSValuePool.cpp
@@ -157,6 +157,7 @@ void CSSValuePool::trace(Visitor* visitor)
     visitor->trace(m_inheritedValue);
     visitor->trace(m_implicitInitialValue);
     visitor->trace(m_explicitInitialValue);
+    visitor->trace(m_unsetValue);
     visitor->trace(m_identifierValueCache);
     visitor->trace(m_colorValueCache);
     visitor->trace(m_colorTransparent);
