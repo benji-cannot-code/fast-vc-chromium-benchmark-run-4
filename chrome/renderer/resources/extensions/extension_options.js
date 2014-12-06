6eda359105384b47f06b77bc54e895db9ccfd067
paulmeyer <paulmeyer@chromium.org>
2014-12-06 10:47:51 -0800

    Added setAutoSize to GuestView's API.
    BUG=431002, 434226
    
    TBR=rockot@chromium.org
    
    Review URL: https://codereview.chromium.org/784433002
    
    Cr-Commit-Position: refs/heads/master@{#307184}

--- chrome/renderer/resources/extensions/extension_options.js
+++ chrome/renderer/resources/extensions/extension_options.js
@@ -134,7 +134,7 @@ ExtensionOptionsImpl.prototype.handleAttributeMutation =
     if (!this.guest.getId())
       return;
 
-    GuestViewInternal.setAutoSize(this.guest.getId(), {
+    this.guest.setAutoSize({
       'enableAutoSize': this.element.hasAttribute('autosize'),
       'min': {
         'width': parseInt(this.minwidth || 0),
@@ -183,7 +183,7 @@ ExtensionOptionsImpl.prototype.resize =
   if (newHeight > this.minheight)
     this.minheight = newHeight;
 
-  GuestViewInternal.setAutoSize(this.guest.getId(), {
+  this.guest.setAutoSize({
     'enableAutoSize': this.element.hasAttribute('autosize'),
     'min': {
       'width': parseInt(this.minwidth || 0),
