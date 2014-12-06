8e0781818285e45bdea99b586d8c133e9aed9e64
thestig <thestig@chromium.org>
2014-12-05 16:29:53 -0800

    Linux: Switch the .deb package from lzma to xz compression. Set max compression.
    Review URL: https://codereview.chromium.org/779413002
    
    Cr-Commit-Position: refs/heads/master@{#307122}

--- chrome/installer/linux/debian/build.sh
+++ chrome/installer/linux/debian/build.sh
@@ -111,7 +111,7 @@ do_package() {
   if [ -f "${DEB_CONTROL}" ]; then
     gen_control
   fi
-  fakeroot dpkg-deb -Zlzma -b "${STAGEDIR}" .
+  fakeroot dpkg-deb -Zxz -z9 -b "${STAGEDIR}" .
 }
 
 # Remove temporary files and unwanted packaging output.
