eafbe863e6a123144315a6c7dcf7e32ac598c0f7
tfarina@chromium.org <tfarina@chromium.org>
2014-12-07 20:03:10 +0000

    Cleanup: Remove manual tests of blackberry.
    This is a platform that is a non-goal for Blink, at least for the moment.
    
    BUG=None
    R=peter@chromium.org
    
    Review URL: https://codereview.chromium.org/780003004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186649 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/ManualTests/blackberry/http-auth-on-200.php
+++
@@ -1,7 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-<?php
-  header('WWW-Authenticate: Basic realm="FAIL"');
-  header('HTTP/1.0 200 OK');
-  header('Content-Type: text/plain');
-  echo 'PASS if you did not see an authentication dialog';
-?>
