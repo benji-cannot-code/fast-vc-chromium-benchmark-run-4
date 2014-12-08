62b13404ef17e9d03d2c4bf4f8645c36440e682e
jsbell@chromium.org <jsbell@chromium.org>
2014-12-08 18:13:05 +0000

    Convert IDBVersionChangeEventInit to a real IDL dictionary
    Teaches Nullable<> to accept nullptr_t to make callers more readble.
    
    R=bashi1@chromium.org,haraken@chromium.org
    BUG=433179
    
    Review URL: https://codereview.chromium.org/748913005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186714 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/indexeddb/IDBVersionChangeEvent.cpp
+++ third_party/WebKit/Source/modules/indexeddb/IDBVersionChangeEvent.cpp
@@ -31,11 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-IDBVersionChangeEventInit::IDBVersionChangeEventInit()
-    : oldVersion(0)
-{
-}
-
 IDBVersionChangeEvent::IDBVersionChangeEvent()
     : m_dataLoss(WebIDBDataLossNone)
 {
@@ -52,13 +47,13 @@ IDBVersionChangeEvent::IDBVersionChangeEvent(const AtomicString& eventType, unsi
 
 IDBVersionChangeEvent::IDBVersionChangeEvent(const AtomicString& eventType, const IDBVersionChangeEventInit& initializer)
     : Event(eventType, false /*canBubble*/, false /*cancelable*/)
-    , m_oldVersion(initializer.oldVersion)
-    , m_newVersion(initializer.newVersion)
+    , m_oldVersion(initializer.oldVersion())
+    , m_newVersion(nullptr)
     , m_dataLoss(WebIDBDataLossNone)
 {
-    if (initializer.dataLoss.isEmpty() || initializer.dataLoss == "none")
-        m_dataLoss = WebIDBDataLossNone;
-    else if (initializer.dataLoss == "total")
+    if (initializer.hasNewVersion())
+        m_newVersion = initializer.newVersion();
+    if (initializer.dataLoss() == "total")
         m_dataLoss = WebIDBDataLossTotal;
 }
 
--- third_party/WebKit/Source/modules/indexeddb/IDBVersionChangeEvent.h
+++ third_party/WebKit/Source/modules/indexeddb/IDBVersionChangeEvent.h
@@ -32,20 +32,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "modules/EventModules.h"
 #include "modules/indexeddb/IDBAny.h"
 #include "modules/indexeddb/IDBRequest.h"
+#include "modules/indexeddb/IDBVersionChangeEventInit.h"
 #include "public/platform/WebIDBTypes.h"
 #include "wtf/PassRefPtr.h"
 #include "wtf/RefPtr.h"
 #include "wtf/text/WTFString.h"
 
 namespace blink {
-
-struct IDBVersionChangeEventInit : public EventInit {
-    IDBVersionChangeEventInit();
-
-    unsigned long long oldVersion;
-    Nullable<unsigned long long> newVersion;
-    String dataLoss;
-};
 
 class IDBVersionChangeEvent final : public Event {
     DEFINE_WRAPPERTYPEINFO();
