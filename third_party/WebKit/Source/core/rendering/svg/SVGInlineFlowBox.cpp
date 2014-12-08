ba7f4ae5771b4a2f7ce8b67debdf96fd23e53b12
pdr@chromium.org <pdr@chromium.org>
2014-12-08 04:37:22 +0000

    Remove left over paintSelectionBackground function in SVGInlineFlowBox
    This function is now unused and can be removed. I found this while
    moving SVG to slimming paint (https://codereview.chromium.org/770173004).
    
    Review URL: https://codereview.chromium.org/782243002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186661 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/svg/SVGInlineFlowBox.cpp
+++ third_party/WebKit/Source/core/rendering/svg/SVGInlineFlowBox.cpp
@@ -29,11 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-void SVGInlineFlowBox::paintSelectionBackground(const PaintInfo& paintInfo)
-{
-    SVGInlineFlowBoxPainter(*this).paintSelectionBackground(paintInfo);
-}
-
 void SVGInlineFlowBox::paint(const PaintInfo& paintInfo, const LayoutPoint& paintOffset, LayoutUnit, LayoutUnit)
 {
     SVGInlineFlowBoxPainter(*this).paint(paintInfo, paintOffset);
