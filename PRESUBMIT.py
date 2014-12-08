ba4bd866d602f747653bd0fa0693a69228319657
scottmg <scottmg@chromium.org>
2014-12-08 10:31:28 -0800

    Add presubmit to ensure GN formatting
    Check added here: https://codereview.chromium.org/779513005/
    
    R=phajdan.jr@chromium.org
    BUG=348474
    
    Review URL: https://codereview.chromium.org/779383004
    
    Cr-Commit-Position: refs/heads/master@{#307282}

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -1569,6 +1569,8 @@ def CheckChangeOnUpload(input_api, output_api):
   results.extend(_CheckValidHostsInDEPS(input_api, output_api))
   results.extend(_CheckJavaStyle(input_api, output_api))
   results.extend(_CheckUmaHistogramChanges(input_api, output_api))
+  results.extend(
+      input_api.canned_checks.CheckGNFormatted(input_api, output_api))
   return results
 
 
