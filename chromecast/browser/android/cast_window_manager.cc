b668336701ade29be0af18b54f82545f8b4b1e77
byungchul <byungchul@chromium.org>
2014-12-08 10:00:37 -0800

    Fix compilation error for android cast_shell.
    BUG=
    TBR=gunsch@chromium.org,lcwu1@chromium.org
    
    Review URL: https://codereview.chromium.org/787673002
    
    Cr-Commit-Position: refs/heads/master@{#307275}

--- chromecast/browser/android/cast_window_manager.cc
+++ chromecast/browser/android/cast_window_manager.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/scoped_java_ref.h"
 #include "base/bind.h"
 #include "base/lazy_instance.h"
+#include "base/prefs/pref_service.h"
 #include "chromecast/browser/android/cast_window_android.h"
 #include "chromecast/browser/cast_browser_context.h"
 #include "chromecast/browser/cast_browser_main_parts.h"
