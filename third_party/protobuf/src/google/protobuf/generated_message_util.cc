f938a172de4098975d51f8c8cfa8fd95e74ad5a0
dcheng <dcheng@chromium.org>
2014-12-08 12:59:20 -0800

    Uninline several internal protobuf helpers.
    This saves 576KiB on a release build.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/778203003
    
    Cr-Commit-Position: refs/heads/master@{#307332}

--- third_party/protobuf/src/google/protobuf/generated_message_util.cc
+++ third_party/protobuf/src/google/protobuf/generated_message_util.cc
@@ -55,6 +55,11 @@ void InitEmptyString() {
   empty_string_ = new std::string();
 }
 
+const ::std::string& GetEmptyString() {
+  GoogleOnceInit(&empty_string_once_init_, &InitEmptyString);
+  return *empty_string_;
+}
+
 }  // namespace internal
 }  // namespace protobuf
 }  // namespace google
