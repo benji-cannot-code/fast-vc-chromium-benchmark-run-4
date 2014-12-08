75ea219e66c020f63ab07a54cacd66adaff4c287
paulmeyer <paulmeyer@chromium.org>
2014-12-08 08:00:18 -0800

    <webview>: Moved parseSrcAttribute() into the SrcAttribute object as parse().
    This is a more obvious place for this function since it is specifically associated with webview's src attribute.
    
    BUG=431002
    
    Review URL: https://codereview.chromium.org/785553002
    
    Cr-Commit-Position: refs/heads/master@{#307257}

--- extensions/renderer/resources/guest_view/web_view_attributes.js
+++ extensions/renderer/resources/guest_view/web_view_attributes.js
@@ -89,10 +89,8 @@ AllowTransparencyAttribute.prototype.handleMutation = function(oldValue,
     return;
   }
 
-  WebViewInternal.setAllowTransparency(
-      this.webViewImpl.guest.getId(),
-      this.webViewImpl.attributes[
-          WebViewConstants.ATTRIBUTE_ALLOWTRANSPARENCY].getValue());
+  WebViewInternal.setAllowTransparency(this.webViewImpl.guest.getId(),
+                                       this.getValue());
 };
 
 // Attribute used to define the demension limits of autosizing.
@@ -212,7 +210,7 @@ SrcAttribute.prototype.handleMutation = function(oldValue, newValue) {
     this.setValueIgnoreMutation(oldValue);
     return;
   }
-  this.webViewImpl.parseSrcAttribute();
+  this.parse();
 };
 
 // The purpose of this mutation observer is to catch assignment to the src
@@ -237,6 +235,26 @@ SrcAttribute.prototype.setupMutationObserver =
     attributeFilter: [this.name]
   };
   this.observer.observe(this.webViewImpl.element, params);
+};
+
+SrcAttribute.prototype.parse = function() {
+  if (!this.webViewImpl.elementAttached ||
+      !this.webViewImpl.attributes[
+        WebViewConstants.ATTRIBUTE_PARTITION].validPartitionId ||
+      !this.getValue()) {
+    return;
+  }
+
+  if (!this.webViewImpl.guest.getId()) {
+    if (this.webViewImpl.beforeFirstNavigation) {
+      this.webViewImpl.beforeFirstNavigation = false;
+      this.webViewImpl.createGuest();
+    }
+    return;
+  }
+
+  // Navigate to |src|.
+  WebViewInternal.navigate(this.webViewImpl.guest.getId(), this.getValue());
 };
 
 // -----------------------------------------------------------------------------
