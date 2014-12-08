fb2393064b9a9c5a23273a1a9ebc229c681ba320
simonhatch <simonhatch@chromium.org>
2014-12-08 13:00:35 -0800

    [bisect] - Fix sync annotation.
    The build/run steps use the version from the config, but the sync is still using the resolved git hash. Change this so they all match.
    
    http://build.chromium.org/p/tryserver.chromium.perf/builders/linux_perf_bisect/builds/1121
    
    Syncing [14ef64e8d0bfbf89cde4b0a9d265785370ab4806]
    
    becomes
    
    Syncing [307025]
    
    BUG=
    
    Review URL: https://codereview.chromium.org/782753002
    
    Cr-Commit-Position: refs/heads/master@{#307334}

--- tools/run-bisect-perf-regression.py
+++ tools/run-bisect-perf-regression.py
@@ -332,6 +332,8 @@ def _GetStepAnnotationStringsDict(config):
         'build2': 'Building [%s]' % config['bad_revision'],
         'run1': 'Running [%s]' % config['good_revision'],
         'run2': 'Running [%s]' % config['bad_revision'],
+        'sync1': 'Syncing [%s]' % config['good_revision'],
+        'sync2': 'Syncing [%s]' % config['bad_revision'],
         'results_label1': config['good_revision'],
         'results_label2': config['bad_revision'],
     }
@@ -346,11 +348,14 @@ def _GetStepAnnotationStringsDict(config):
     }
 
 
-def _RunBuildStepForPerformanceTest(bisect_instance, build_string, revision):
+def _RunBuildStepForPerformanceTest(bisect_instance,
+                                    build_string,
+                                    sync_string,
+                                    revision):
   if revision:
-    bisect_utils.OutputAnnotationStepStart('Syncing [%s]' % revision)
+    bisect_utils.OutputAnnotationStepStart(sync_string)
     if not source_control.SyncToRevision(revision, 'gclient'):
-      raise RuntimeError('Failed to sync to [%s].' % revision)
+      raise RuntimeError('Failed [%s].' % sync_string)
     bisect_utils.OutputAnnotationStepClosed()
 
   bisect_utils.OutputAnnotationStepStart(build_string)
@@ -404,7 +409,10 @@ def _RunPerformanceTest(config):
   annotations_dict = _GetStepAnnotationStringsDict(config)
   b = bisect_perf_regression.BisectPerformanceMetrics(opts, os.getcwd())
 
-  _RunBuildStepForPerformanceTest(b, annotations_dict['build1'], revisions[0])
+  _RunBuildStepForPerformanceTest(b,
+                                  annotations_dict.get('build1'),
+                                  annotations_dict.get('sync1'),
+                                  revisions[0])
 
   results_with_patch = _RunCommandStepForPerformanceTest(
       b, opts, True, True, annotations_dict['results_label1'],
@@ -420,7 +428,10 @@ def _RunPerformanceTest(config):
                            os.path.join('third_party', 'WebKit'))
   bisect_utils.OutputAnnotationStepClosed()
 
-  _RunBuildStepForPerformanceTest(b, annotations_dict['build2'], revisions[1])
+  _RunBuildStepForPerformanceTest(b,
+                                  annotations_dict.get('build2'),
+                                  annotations_dict.get('sync2'),
+                                  revisions[1])
 
   results_without_patch = _RunCommandStepForPerformanceTest(
       b, opts, False, True, annotations_dict['results_label2'],
