27f9eaeec596860dea3fb3c6f21ea84e9374b91c
Blurrech <Blurrech@gmail.com>
2014-12-08 02:44:00 -0800

    Remove unused extension files.
    BUG=302083
    
    Review URL: https://codereview.chromium.org/785803002
    
    Cr-Commit-Position: refs/heads/master@{#307217}

--- chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
+++ chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
@@ -158,7 +158,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(ENABLE_EXTENSIONS)
 #include "chrome/browser/extensions/extension_web_ui.h"
-#include "chrome/browser/ui/webui/extensions/extension_info_ui.h"
 #include "chrome/browser/ui/webui/extensions/extensions_ui.h"
 #include "chrome/browser/ui/webui/voicesearch_ui.h"
 #include "chrome/common/extensions/extension_constants.h"
