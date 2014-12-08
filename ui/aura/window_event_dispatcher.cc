d161d7039096b79e3992c48a33c7eab720f6b1ce
tdanderson <tdanderson@chromium.org>
2014-12-08 13:21:59 -0800

    Do not forward input events to Widget during Widget destruction
    Set the target handler of an aura::Window to null early
    in the Window destructor to stop input events from
    being forwarded to the hosted views::Widget since its
    event processor (RootView) may have already been
    destroyed.
    
    Furthermore, check for a null RootView in
    TouchUMA::FindGestureActionType() since this code
    may be reached during the pretarget phase of event
    processing at the same time the widget is being
    destroyed, in which case its root view will have already
    been destroyed.
    
    BUG=430582
    TEST=WindowSelectorTest.ClickOnWindowDuringTouch,
         WindowTest.NoCrashOnWindowDelete,
         WindowTest.MouseEnterExitWithWindowAppearAndDelete,
         WindowTest.MouseEnterExitWithParentDelete,
         WindowTest.MouseEventsOnLeafWindowChange,
         WindowTest.MouseEventsOnNonLeafWindowDelete,
         WidgetTest.NoCrashOnWidgetDelete,
         WidgetTest.NoCrashOnWidgetDeleteWithPendingEvents
    
    Review URL: https://codereview.chromium.org/753623005
    
    Cr-Commit-Position: refs/heads/master@{#307338}

--- ui/aura/window_event_dispatcher.cc
+++ ui/aura/window_event_dispatcher.cc
@@ -556,7 +556,6 @@ void WindowEventDispatcher::OnWindowDestroying(Window* window) {
   if (!host_->window()->Contains(window))
     return;
 
-  DispatchMouseExitToHidingWindow(window);
   SynthesizeMouseMoveAfterChangeToWindow(window);
 }
 
