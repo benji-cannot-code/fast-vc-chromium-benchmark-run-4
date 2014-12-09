ebd9b7a47e16196019ce0dfd51326a82ffb47cfa
treib <treib@chromium.org>
2014-12-09 04:03:33 -0800

    Use Profile::IsLegacySupervised instead of IsSupervised where appropriate
    TBRing simple changes:
    TBR=rogerta@chromium.org (for chrome_signin_client.cc)
    TBR=zea@chromium.org (for supervised_user_signin_manager_wrapper.cc)
    BUG=
    
    Review URL: https://codereview.chromium.org/783393003
    
    Cr-Commit-Position: refs/heads/master@{#307453}

--- chrome/browser/sync/supervised_user_signin_manager_wrapper.cc
+++ chrome/browser/sync/supervised_user_signin_manager_wrapper.cc
@@ -27,7 +27,7 @@ SigninManagerBase* SupervisedUserSigninManagerWrapper::GetOriginal() {
 
 std::string SupervisedUserSigninManagerWrapper::GetEffectiveUsername() const {
 #if defined(ENABLE_SUPERVISED_USERS)
-  if (!original_->IsAuthenticated() && profile_->IsSupervised())
+  if (profile_->IsLegacySupervised())
     return supervised_users::kSupervisedUserPseudoEmail;
 #endif
   return original_->GetAuthenticatedUsername();
@@ -35,7 +35,7 @@ std::string SupervisedUserSigninManagerWrapper::GetEffectiveUsername() const {
 
 std::string SupervisedUserSigninManagerWrapper::GetAccountIdToUse() const {
 #if defined(ENABLE_SUPERVISED_USERS)
-  if (!original_->IsAuthenticated() && profile_->IsSupervised())
+  if (profile_->IsLegacySupervised())
     return supervised_users::kSupervisedUserPseudoEmail;
 #endif
   return original_->GetAuthenticatedAccountId();
@@ -43,7 +43,7 @@ std::string SupervisedUserSigninManagerWrapper::GetAccountIdToUse() const {
 
 std::string SupervisedUserSigninManagerWrapper::GetSyncScopeToUse() const {
 #if defined(ENABLE_SUPERVISED_USERS)
-  if (!original_->IsAuthenticated() && profile_->IsSupervised())
+  if (profile_->IsLegacySupervised())
     return GaiaConstants::kChromeSyncSupervisedOAuth2Scope;
 #endif
   return GaiaConstants::kChromeSyncOAuth2Scope;
