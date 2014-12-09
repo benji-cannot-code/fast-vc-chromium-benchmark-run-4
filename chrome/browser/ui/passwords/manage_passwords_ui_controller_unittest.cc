1c627811ff5e5ef0506f4a7600468c4381d569d5
vabr <vabr@chromium.org>
2014-12-09 05:37:45 -0800

    PasswordFormManager takes WeakPtr<PasswordManagerDriver>
    PFM instances are owned by PasswordManager, which lives as long as a
    WebContents does. Drivers associated to PFMs are bound to frames.
    PasswordManager keeps PFMs alive until the main frame navigation,
    and even longer (if they carry the provisionally saved password).
    This means that a PFM can outlive the associated driver.
    
    On its creation, the PFM calls the PasswordStore to get the stored
    credentials. While PFM lives on the UI thread, the store works on
    DB thread, and the results are returned asynchronously. The PFM normally
    uses the services of the driver to process the obtained results. If
    the results come later than the frame and driver were deleted, this leads
    to use-after-free. Also, if the frame is already deleted, there is no
    harm in skipping the work done by driver on the results, because that
    is always bound to operations on the frame, like (auto)filling.
    
    Therefore, this CL makes PFM hold a WeakPtr (instead of a naked pointer)
    to the driver, and check before each use its validity.
    
    BUG=439534
    
    Review URL: https://codereview.chromium.org/786823002
    
    Cr-Commit-Position: refs/heads/master@{#307461}

--- chrome/browser/ui/passwords/manage_passwords_ui_controller_unittest.cc
+++ chrome/browser/ui/passwords/manage_passwords_ui_controller_unittest.cc
@@ -121,7 +121,7 @@ TEST_F(ManagePasswordsUIControllerTest, PasswordSubmitted) {
   password_manager::StubPasswordManagerDriver driver;
   scoped_ptr<password_manager::PasswordFormManager> test_form_manager(
       new password_manager::PasswordFormManager(
-          NULL, &client, &driver, test_form(), false));
+          NULL, &client, driver.AsWeakPtr(), test_form(), false));
   controller()->OnPasswordSubmitted(test_form_manager.Pass());
   EXPECT_EQ(password_manager::ui::PENDING_PASSWORD_AND_BUBBLE_STATE,
             controller()->state());
@@ -143,7 +143,7 @@ TEST_F(ManagePasswordsUIControllerTest, PasswordSaved) {
   password_manager::StubPasswordManagerDriver driver;
   scoped_ptr<password_manager::PasswordFormManager> test_form_manager(
       new password_manager::PasswordFormManager(
-          NULL, &client, &driver, test_form(), false));
+          NULL, &client, driver.AsWeakPtr(), test_form(), false));
   controller()->OnPasswordSubmitted(test_form_manager.Pass());
 
   ManagePasswordsIconMock mock;
@@ -158,7 +158,7 @@ TEST_F(ManagePasswordsUIControllerTest, PasswordBlacklisted) {
   password_manager::StubPasswordManagerDriver driver;
   scoped_ptr<password_manager::PasswordFormManager> test_form_manager(
       new password_manager::PasswordFormManager(
-          NULL, &client, &driver, test_form(), false));
+          NULL, &client, driver.AsWeakPtr(), test_form(), false));
   controller()->OnPasswordSubmitted(test_form_manager.Pass());
 
   ManagePasswordsIconMock mock;
@@ -173,7 +173,7 @@ TEST_F(ManagePasswordsUIControllerTest, QuickNavigations) {
   password_manager::StubPasswordManagerDriver driver;
   scoped_ptr<password_manager::PasswordFormManager> test_form_manager(
       new password_manager::PasswordFormManager(
-          NULL, &client, &driver, test_form(), false));
+          NULL, &client, driver.AsWeakPtr(), test_form(), false));
   controller()->OnPasswordSubmitted(test_form_manager.Pass());
   ManagePasswordsIconMock mock;
   controller()->UpdateIconAndBubbleState(&mock);
@@ -200,7 +200,7 @@ TEST_F(ManagePasswordsUIControllerTest, SlowNavigations) {
   password_manager::StubPasswordManagerDriver driver;
   scoped_ptr<password_manager::PasswordFormManager> test_form_manager(
       new password_manager::PasswordFormManager(
-          NULL, &client, &driver, test_form(), false));
+          NULL, &client, driver.AsWeakPtr(), test_form(), false));
   controller()->OnPasswordSubmitted(test_form_manager.Pass());
   ManagePasswordsIconMock mock;
   controller()->UpdateIconAndBubbleState(&mock);
@@ -228,7 +228,7 @@ TEST_F(ManagePasswordsUIControllerTest, PasswordSubmittedToNonWebbyURL) {
   password_manager::StubPasswordManagerDriver driver;
   scoped_ptr<password_manager::PasswordFormManager> test_form_manager(
       new password_manager::PasswordFormManager(
-          NULL, &client, &driver, test_form(), false));
+          NULL, &client, driver.AsWeakPtr(), test_form(), false));
   controller()->OnPasswordSubmitted(test_form_manager.Pass());
   EXPECT_EQ(password_manager::ui::INACTIVE_STATE, controller()->state());
   EXPECT_FALSE(controller()->PasswordPendingUserDecision());
@@ -328,7 +328,7 @@ TEST_F(ManagePasswordsUIControllerTest, AutomaticPasswordSave) {
   password_manager::StubPasswordManagerDriver driver;
   scoped_ptr<password_manager::PasswordFormManager> test_form_manager(
       new password_manager::PasswordFormManager(
-          NULL, &client, &driver, test_form(), false));
+          NULL, &client, driver.AsWeakPtr(), test_form(), false));
 
   controller()->OnAutomaticPasswordSave(test_form_manager.Pass());
   EXPECT_EQ(password_manager::ui::CONFIRMATION_STATE, controller()->state());
--- components/password_manager/core/browser/password_manager_driver.h
+++ components/password_manager/core/browser/password_manager_driver.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 #include "base/strings/string16.h"
 
 namespace autofill {
@@ -26,7 +27,8 @@ class PasswordManager;
 
 // Interface that allows PasswordManager core code to interact with its driver
 // (i.e., obtain information from it and give information to it).
-class PasswordManagerDriver {
+class PasswordManagerDriver
+    : public base::SupportsWeakPtr<PasswordManagerDriver> {
  public:
   PasswordManagerDriver() {}
   virtual ~PasswordManagerDriver() {}
