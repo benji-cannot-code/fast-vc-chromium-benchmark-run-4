07ef582099c1fd236e31484b47d9bfa212d5ebd7
avi <avi@chromium.org>
2014-12-09 14:25:13 -0800

    If the renderer drops a navigation, make sure the spinner doesn't spin endlessly.
    BUG=431853
    TEST=none
    
    Review URL: https://codereview.chromium.org/790843002
    
    Cr-Commit-Position: refs/heads/master@{#307567}

--- content/browser/web_contents/web_contents_impl.h
+++ content/browser/web_contents/web_contents_impl.h
@@ -353,6 +353,7 @@ class CONTENT_EXPORT WebContentsImpl
   void RenderFrameDeleted(RenderFrameHost* render_frame_host) override;
   void DidStartLoading(RenderFrameHost* render_frame_host,
                        bool to_different_document) override;
+  void DidStopLoading(RenderFrameHost* render_frame_host) override;
   void SwappedOut(RenderFrameHost* render_frame_host) override;
   void DidDeferAfterResponseStarted(
       const TransitionLayerData& transition_data) override;
@@ -883,9 +884,6 @@ class CONTENT_EXPORT WebContentsImpl
 
   // Calculates the progress of the current load and notifies the delegate.
   void SendLoadProgressChanged();
-
-  // Called once when the last frame on the page has stopped loading.
-  void DidStopLoading(RenderFrameHost* render_frame_host);
 
   // Misc non-view stuff -------------------------------------------------------
 
