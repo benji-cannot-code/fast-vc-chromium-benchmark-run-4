65188c250da11c72faed3b3e8da274cf090d48de
hirono <hirono@chromium.org>
2014-12-08 23:16:42 -0800

    Files.app: Move GalleryJSTest resources to the same directory with production code.
    BUG=439349
    TEST=browser_tests --gtest_filter="GalleryJsTest.*"
    
    Review URL: https://codereview.chromium.org/792453003
    
    Cr-Commit-Position: refs/heads/master@{#307428}

--- chrome/browser/chromeos/file_manager/gallery_jstest.cc
+++ chrome/browser/chromeos/file_manager/gallery_jstest.cc
@@ -5,19 +5,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "base/path_service.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test_utils.h"
+#include "net/base/filename_util.h"
 
 class GalleryJsTest : public InProcessBrowserTest {
  public:
   // Runs all test functions in |file|, waiting for them to complete.
   void RunTest(const base::FilePath& file) {
-    const GURL url = ui_test_utils::GetTestUrl(
-        base::FilePath(FILE_PATH_LITERAL("gallery/unit_tests")), file);
+    base::FilePath root_path;
+    ASSERT_TRUE(PathService::Get(base::DIR_SOURCE_ROOT, &root_path));
+
+    const GURL url = net::FilePathToFileURL(
+        root_path.Append(FILE_PATH_LITERAL("ui/file_manager/gallery"))
+            .Append(file));
     ui_test_utils::NavigateToURL(browser(), url);
 
     content::WebContents* const web_contents =
@@ -31,5 +37,5 @@ class GalleryJsTest : public InProcessBrowserTest {
 
 IN_PROC_BROWSER_TEST_F(GalleryJsTest, ExifEncoderTest) {
   RunTest(base::FilePath(
-      FILE_PATH_LITERAL("exif_encoder_unittest.html")));
+      FILE_PATH_LITERAL("js/image_editor/exif_encoder_unittest.html")));
 }
