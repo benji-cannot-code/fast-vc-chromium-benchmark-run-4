73e976eae65ac12252d4319c66a0a272cb10ab04
ccameron <ccameron@chromium.org>
2014-12-09 11:56:32 -0800

    Add argument to pass elastic overscroll delta to Blink (Chrome side)
    Blink will need to use this argument to translate input events during
    overscroll.
    
    R=enne
    TBR=jam
    BUG=133097
    
    Review URL: https://codereview.chromium.org/783373002
    
    Cr-Commit-Position: refs/heads/master@{#307525}

--- cc/trees/layer_tree_host_client.h
+++ cc/trees/layer_tree_host_client.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gfx {
 class Vector2d;
+class Vector2dF;
 }
 
 namespace cc {
@@ -28,10 +29,12 @@ class LayerTreeHostClient {
   virtual void BeginMainFrame(const BeginFrameArgs& args) = 0;
   virtual void DidBeginMainFrame() = 0;
   virtual void Layout() = 0;
-  virtual void ApplyViewportDeltas(const gfx::Vector2d& inner_delta,
-                                   const gfx::Vector2d& outer_delta,
-                                   float page_scale,
-                                   float top_controls_delta) = 0;
+  virtual void ApplyViewportDeltas(
+      const gfx::Vector2d& inner_delta,
+      const gfx::Vector2d& outer_delta,
+      const gfx::Vector2dF& elastic_overscroll_delta,
+      float page_scale,
+      float top_controls_delta) = 0;
   virtual void ApplyViewportDeltas(const gfx::Vector2d& scroll_delta,
                                    float page_scale,
                                    float top_controls_delta) = 0;
