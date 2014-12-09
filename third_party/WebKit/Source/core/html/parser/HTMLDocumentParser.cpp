5fbb48d079c2ad5d6304ac4fb503992f349ca542
pmeenan@chromium.org <pmeenan@chromium.org>
2014-12-09 00:35:24 +0000

    Revert of Revert of Yield parser before scripts if a lot of DOM changes were made (patchset #2 id:20001 of https://codereview.chromium.org/768133002/)
    Reason for revert:
    Reverting the revert (re-landing the original).  The crash that triggered the revert test only ever happened once and can not be reproduced with or without the CL (and the CL wouldn't logically have anything to do with it other than being the last thing that touched code near the call stack).
    
    Original issue's description:
    > Revert of Yield parser before scripts if a lot of DOM changes were made (patchset #2 id:20001 of https://codereview.chromium.org/741493004/)
    > 
    > Reason for revert:
    > Testing to see if revert fixes https://code.google.com/p/chromium/issues/detail?id=435816
    > 
    > If it does then the best solution is to find the actual root cause and re-land or at least tweak the yielding logic to make it more likely to only yield for the specific cases we are targeting.
    > 
    > This is a manual patch set as the auto-generated revert from the original patch would not apply: https://codereview.chromium.org/779653002/
    > 
    > Original issue's description:
    > > Yield parser before scripts if a lot of DOM changes were made
    > > 
    > > Yield the main parser thread when processing chunks from the background
    > > parser if a script block is about to be started and a large number of
    > > DOM elements have been parsed in the current session.
    > > 
    > > This significantly helps initial render (6-8% in testing) at a slight
    > > cost to DOM Content Loaded (~2% in testing) generally but has a large
    > > benefit in particular for sites that put scripts at the end of their
    > > document (which is a common best-practice).
    > > 
    > > BUG=427757
    > > 
    > > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=185567
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=186473
    
    TBR=kouhei@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/784973003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186743 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/parser/HTMLDocumentParser.cpp
+++ third_party/WebKit/Source/core/html/parser/HTMLDocumentParser.cpp
@@ -413,7 +413,7 @@ void HTMLDocumentParser::discardSpeculationsAndResumeFrom(PassOwnPtr<ParsedChunk
     HTMLParserThread::shared()->postTask(bind(&BackgroundHTMLParser::resumeFrom, m_backgroundParser, checkpoint.release()));
 }
 
-void HTMLDocumentParser::processParsedChunkFromBackgroundParser(PassOwnPtr<ParsedChunk> popChunk)
+size_t HTMLDocumentParser::processParsedChunkFromBackgroundParser(PassOwnPtr<ParsedChunk> popChunk)
 {
     TRACE_EVENT0("blink", "HTMLDocumentParser::processParsedChunkFromBackgroundParser");
 
@@ -432,6 +432,7 @@ void HTMLDocumentParser::processParsedChunkFromBackgroundParser(PassOwnPtr<Parse
 
     OwnPtr<ParsedChunk> chunk(popChunk);
     OwnPtr<CompactHTMLTokenStream> tokens = chunk->tokens.release();
+    size_t elementTokenCount = 0;
 
     HTMLParserThread::shared()->postTask(bind(&BackgroundHTMLParser::startedChunkWithCheckpoint, m_backgroundParser, chunk->inputCheckpoint));
 
@@ -444,6 +445,9 @@ void HTMLDocumentParser::processParsedChunkFromBackgroundParser(PassOwnPtr<Parse
 
     for (Vector<CompactHTMLToken>::const_iterator it = tokens->begin(); it != tokens->end(); ++it) {
         ASSERT(!isWaitingForScripts());
+
+        if (!chunk->startingScript && (it->type() == HTMLToken::StartTag || it->type() == HTMLToken::EndTag))
+            elementTokenCount++;
 
         if (document()->frame() && document()->frame()->navigationScheduler().locationChangePending()) {
 
@@ -485,6 +489,8 @@ void HTMLDocumentParser::processParsedChunkFromBackgroundParser(PassOwnPtr<Parse
     // This leaves "script", "style" and "svg" nodes text nodes intact.
     if (!isStopped())
         m_treeBuilder->flush(FlushIfAtTextLimit);
+
+    return elementTokenCount;
 }
 
 void HTMLDocumentParser::pumpPendingSpeculations()
@@ -517,7 +523,8 @@ void HTMLDocumentParser::pumpPendingSpeculations()
     SpeculationsPumpSession session(m_pumpSpeculationsSessionNestingLevel, contextForParsingSession());
     while (!m_speculations.isEmpty()) {
         ASSERT(!isScheduledForResume());
-        processParsedChunkFromBackgroundParser(m_speculations.takeFirst());
+        size_t elementTokenCount = processParsedChunkFromBackgroundParser(m_speculations.takeFirst());
+        session.addedElementTokens(elementTokenCount);
 
         // Always check isParsing first as m_document may be null.
         // Surprisingly, isScheduledForResume() may be set here as a result of
@@ -526,7 +533,7 @@ void HTMLDocumentParser::pumpPendingSpeculations()
         if (!isParsing() || isWaitingForScripts() || isScheduledForResume())
             break;
 
-        if (m_speculations.isEmpty() || m_parserScheduler->yieldIfNeeded(session))
+        if (m_speculations.isEmpty() || m_parserScheduler->yieldIfNeeded(session, m_speculations.first()->startingScript))
             break;
     }
 
--- third_party/WebKit/Source/core/html/parser/HTMLParserScheduler.h
+++ third_party/WebKit/Source/core/html/parser/HTMLParserScheduler.h
@@ -60,9 +60,12 @@ public:
     ~SpeculationsPumpSession();
 
     double elapsedTime() const;
+    void addedElementTokens(size_t count);
+    size_t processedElementTokens() const { return m_processedElementTokens; }
 
 private:
     double m_startTime;
+    size_t m_processedElementTokens;
 };
 
 class HTMLParserScheduler {
@@ -77,7 +80,7 @@ public:
     bool isScheduledForResume() const { return m_isSuspendedWithActiveTimer || m_continueNextChunkTimer.isActive(); }
 
     void scheduleForResume();
-    bool yieldIfNeeded(const SpeculationsPumpSession&);
+    bool yieldIfNeeded(const SpeculationsPumpSession&, bool startingScript);
 
     /**
      * Can only be called if this scheduler is suspended. If this is called,
@@ -94,7 +97,7 @@ public:
 private:
     explicit HTMLParserScheduler(HTMLDocumentParser*);
 
-    bool shouldYield(const SpeculationsPumpSession&) const;
+    bool shouldYield(const SpeculationsPumpSession&, bool startingScript) const;
     void continueNextChunkTimerFired(Timer<HTMLParserScheduler>*);
 
     HTMLDocumentParser* m_parser;
