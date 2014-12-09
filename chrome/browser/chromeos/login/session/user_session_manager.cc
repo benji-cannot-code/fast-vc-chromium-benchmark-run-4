a7ec77b1e156b4387fe6e228eb23d4dba4e2cfce
eranm <eranm@chromium.org>
2014-12-09 01:11:36 -0800

    Certificate Transparency: EV certificates whitelist support for ChromeOS
    This is a small change to store the EV certificates whitelist in the user's profile, in a similar
    manner to the CRL Set.
    That means that after the initial component update for the EV whitelist,
    users will have the EV whitelist available and will get the EV indicator
    immediately after logging in to ChromeOS, rather than having to wait for
    the component updater every time.
    
    Implementation is similar to CRLSet as well: During registration of the EVWhitelistComponentInstaller a task is also posted to do an initial load from disk.
    
    BUG=339128
    
    Review URL: https://codereview.chromium.org/657613002
    
    Cr-Commit-Position: refs/heads/master@{#307437}

--- chrome/browser/chromeos/login/session/user_session_manager.cc
+++ chrome/browser/chromeos/login/session/user_session_manager.cc
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/policy/browser_policy_connector_chromeos.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/chromeos/settings/cros_settings.h"
+#include "chrome/browser/component_updater/ev_whitelist_component_installer.h"
 #include "chrome/browser/first_run/first_run.h"
 #include "chrome/browser/google/google_brand_chromeos.h"
 #include "chrome/browser/lifetime/application_lifetime.h"
@@ -971,6 +972,7 @@ void UserSessionManager::FinalizePrepareProfile(Profile* profile) {
     InitRlz(profile);
     InitializeCerts(profile);
     InitializeCRLSetFetcher(user);
+    InitializeEVCertificatesWhitelistComponent(user);
   }
 
   UpdateEasyUnlockKeys(user_context_);
@@ -1196,6 +1198,18 @@ void UserSessionManager::InitializeCRLSetFetcher(
     CRLSetFetcher* crl_set = g_browser_process->crl_set_fetcher();
     if (crl_set && cus)
       crl_set->StartInitialLoad(cus, path);
+  }
+}
+
+void UserSessionManager::InitializeEVCertificatesWhitelistComponent(
+    const user_manager::User* user) {
+  const std::string username_hash = user->username_hash();
+  component_updater::ComponentUpdateService* cus =
+      g_browser_process->component_updater();
+  if (!username_hash.empty() && cus) {
+    const base::FilePath path =
+        ProfileHelper::GetProfilePathByUserIdHash(username_hash);
+    RegisterEVWhitelistComponent(cus, path);
   }
 }
 
