fb654b5c00c4b41be1cd9de21be4b71cc0b380b7
nhiroki <nhiroki@chromium.org>
2014-12-08 20:20:47 -0800

    ServiceWorker: Notify observers when the storage gets recovered from corruption state
    This introduces OnStorageWiped event in ServiceWorkerContextObserver. The event
    is notified when the storage gets recovered from the corruption state.
    
    Note: OnRegistrationDelete events are not delivered in this case because we
    cannot retrieve registration data to be deleted from the corrupted database.
    
    BUG=438075
    TEST=content_unittests --gtest_filter=ServiceWorkerContextTest.*
    
    Review URL: https://codereview.chromium.org/772333005
    
    Cr-Commit-Position: refs/heads/master@{#307414}

--- content/browser/service_worker/service_worker_context_wrapper.cc
+++ content/browser/service_worker/service_worker_context_wrapper.cc
@@ -325,6 +325,8 @@ void ServiceWorkerContextWrapper::DidDeleteAndStartOver(
   }
   context_core_.reset(new ServiceWorkerContextCore(context_core_.get(), this));
   DVLOG(1) << "Restarted ServiceWorkerContextCore successfully.";
+
+  observer_list_->Notify(&ServiceWorkerContextObserver::OnStorageWiped);
 }
 
 }  // namespace content
