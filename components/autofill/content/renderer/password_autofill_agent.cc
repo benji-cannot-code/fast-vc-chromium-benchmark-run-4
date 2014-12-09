44c9890d93e9ed904c93b3c69ffada9d096dd299
jww <jww@chromium.org>
2014-12-09 13:02:37 -0800

    Support for password manager suggestions on password fields.
    This adds support for the password manager to, optionally, request
    suggestions to be displayed on a password field rather than the
    traditional choice of a username field. While not used right now, this
    is a prerequisite for implementing "fill on account select" as an
    experiment since there needs to be a way for users to fill on select if
    the username field is uneditable.
    
    This is implemented by adding an option to the autofill IPC message that
    the field to receive suggestions is a password field, not a username
    field. Thus, this the password manager (a) knows to only show one
    suggestion for the specified username, and (b) knows that there should
    be a title in th dropdown menu.
    
    BUG=410963
    
    Review URL: https://codereview.chromium.org/767353002
    
    Cr-Commit-Position: refs/heads/master@{#307549}

--- components/autofill/content/renderer/password_autofill_agent.cc
+++ components/autofill/content/renderer/password_autofill_agent.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/content/renderer/form_autofill_util.h"
 #include "components/autofill/content/renderer/password_form_conversion_utils.h"
 #include "components/autofill/content/renderer/renderer_save_password_progress_logger.h"
+#include "components/autofill/core/common/autofill_constants.h"
 #include "components/autofill/core/common/form_field_data.h"
 #include "components/autofill/core/common/password_form.h"
 #include "components/autofill/core/common/password_form_fill_data.h"
@@ -1069,9 +1070,12 @@ bool PasswordAutofillAgent::ShowSuggestionPopup(
                                  bounding_box.y() * scale,
                                  bounding_box.width() * scale,
                                  bounding_box.height() * scale);
+  int options = 0;
+  if (show_all)
+    options |= SHOW_ALL;
   Send(new AutofillHostMsg_ShowPasswordSuggestions(
       routing_id(), key_it->second, field.text_direction, user_input.value(),
-      show_all, bounding_box_scaled));
+      options, bounding_box_scaled));
 
   bool suggestions_present = false;
   if (GetSuggestionsStats(fill_data, user_input.value(), show_all,
