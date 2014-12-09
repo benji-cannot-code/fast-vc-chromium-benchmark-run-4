86cc6e58b53de789d6368370c682e63d0caa2606
slamm <slamm@chromium.org>
2014-12-09 15:28:10 -0800

    Basic android app implementation.
    BUG=417542,439346
    
    Review URL: https://codereview.chromium.org/776883004
    
    Cr-Commit-Position: refs/heads/master@{#307583}

---
+++ tools/telemetry/telemetry/user_story/android/__init__.py
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from telemetry import user_story
+from telemetry.user_story.android import shared_app_state
+
+class AppStory(user_story.UserStory):
+  def __init__(self, start_intent, name='', labels=None):
+    super(AppStory, self).__init__(
+        shared_app_state.SharedAppState, name, labels)
+    self.start_intent = start_intent
+
+  def RunPageInteractions(self):
+    # TODO(chrishenry): Remove method once TimelineBasedMeasurement calls Run().
+    self.Run()
+
+  def Run(self):
+    """Execute the interactions with the applications."""
+    raise NotImplementedError
