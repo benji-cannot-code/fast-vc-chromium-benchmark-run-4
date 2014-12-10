03d009e5d9c1a2b07f9c2bb18ea0066d7d595996
zhenw <zhenw@chromium.org>
2014-12-09 18:31:03 -0800

    Navigation transitions (web to native app): Revert exit transition (Chrome side)
    We need a way to revert exit transition and show hidden transition elements when coming back from native app (when back button is pressed).
    
    This is the Chrome side of the CL. Blink side CL is here: https://codereview.chromium.org/653223002/
    
    BUG=370696
    
    Review URL: https://codereview.chromium.org/775843004
    
    Cr-Commit-Position: refs/heads/master@{#307621}

--- content/public/android/java/src/org/chromium/content/browser/webcontents/WebContentsImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/webcontents/WebContentsImpl.java
@@ -201,6 +201,14 @@ import org.chromium.content_public.browser.WebContents;
     }
 
     /**
+     * Revert the effect of exit transition.
+     */
+    @Override
+    public void revertExitTransition() {
+        nativeRevertExitTransition(mNativeWebContentsAndroid);
+    }
+
+    /**
      * Hide transition elements.
      */
     public void hideTransitionElements(String cssSelector) {
@@ -316,6 +324,7 @@ import org.chromium.content_public.browser.WebContents;
             String markup);
     private native void nativeBeginExitTransition(long nativeWebContentsAndroid,
             String cssSelector, boolean exitToNativeApp);
+    private native void nativeRevertExitTransition(long nativeWebContentsAndroid);
     private native void nativeHideTransitionElements(long nativeWebContentsAndroid,
             String cssSelector);
     private native void nativeShowTransitionElements(long nativeWebContentsAndroid,
