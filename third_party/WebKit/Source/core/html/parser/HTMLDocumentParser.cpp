b89d5be82d752cb04d72354354a7ce37657e6a74
marja@chromium.org <marja@chromium.org>
2014-12-10 10:06:35 +0000

    Script loading: Handle script cancellations gracefully.
    HTMLDocumentParser was using isStopping() to decide whether it's in the "should
    work on parsing blocking scripts" or "should work on deferred scripts" state,
    and didn't take into account that it can already be in the Stopped state, if the
    page load is cancelled.
    
    BUG=435596
    
    Review URL: https://codereview.chromium.org/775033003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186841 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/parser/HTMLDocumentParser.cpp
+++ third_party/WebKit/Source/core/html/parser/HTMLDocumentParser.cpp
@@ -979,6 +979,11 @@ void HTMLDocumentParser::notifyScriptLoaded(Resource* cachedResource)
 
     ASSERT(m_scriptRunner);
     ASSERT(!isExecutingScript());
+
+    if (isStopped()) {
+        return;
+    }
+
     if (isStopping()) {
         attemptToRunDeferredScriptsAndEnd();
         return;
