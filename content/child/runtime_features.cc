b4255fa4da3ea724e4a6ba5f8a7ac1798650be94
lushnikov <lushnikov@chromium.org>
2014-12-10 08:37:30 -0800

    Revert of Delete the enable/disable layer squashing flags and settings. (patchset #1 id:1 of https://codereview.chromium.org/785323002/)
    Reason for revert:
    The patch caused multiple blink layout tests to fail.
    
    Test expectations dashboard: http://goo.gl/NvoO5S
    Example failed build bot: http://build.chromium.org/p/chromium.webkit/builders/WebKit%20Linux/builds/41154
    
    Original issue's description:
    > Delete the enable/disable layer squashing flags and settings.
    >
    > TBR=jam,jochen
    >
    > Committed: https://crrev.com/f56bfaf1de4e66047ea9dda4cc7e31f074fb80df
    > Cr-Commit-Position: refs/heads/master@{#307620}
    
    TBR=chrishtr@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/789163004
    
    Cr-Commit-Position: refs/heads/master@{#307714}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -176,6 +176,9 @@ void SetRuntimeFeaturesDefaultsAndUpdateFromArgs(
   if (command_line.HasSwitch(switches::kEnablePreciseMemoryInfo))
     WebRuntimeFeatures::enablePreciseMemoryInfo(true);
 
+  if (command_line.HasSwitch(switches::kEnableLayerSquashing))
+    WebRuntimeFeatures::enableLayerSquashing(true);
+
   if (command_line.HasSwitch(switches::kEnableNetworkInformation) ||
       command_line.HasSwitch(
           switches::kEnableExperimentalWebPlatformFeatures)) {
