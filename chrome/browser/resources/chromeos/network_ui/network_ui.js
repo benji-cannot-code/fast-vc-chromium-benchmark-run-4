47995baa3d161cef4ebef6fe657e889c7867add1
stevenjb <stevenjb@chromium.org>
2014-12-10 12:47:56 -0800

    DeviceEventLog cleanup
    This CL does the following:
    * Fixes the formatting for the network log in chrome://network
    * Adds a LOGIN type for login events
    * Uses a string for 'type' in GetString to allow multiple types (or non- types) to be passed)
    
    Add login type, use type strings in GetAsType
    
    BUG=293739
    
    Review URL: https://codereview.chromium.org/784223003
    
    Cr-Commit-Position: refs/heads/master@{#307753}

--- chrome/browser/resources/chromeos/network_ui/network_ui.js
+++ chrome/browser/resources/chromeos/network_ui/network_ui.js
@@ -89,8 +89,7 @@ var NetworkUI = (function() {
       'logEntryFormat',
       timestamp,
       fileinfo,
-      logEntry['event'],
-      logEntry['description']);
+      logEntry['event']);
     res.appendChild(createLevelTag(level));
     res.appendChild(textWrapper);
     return res;
