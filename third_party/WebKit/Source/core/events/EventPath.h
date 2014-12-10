26bd535c51952eb51709692e82092a0f2d327c41
tkent@chromium.org <tkent@chromium.org>
2014-12-10 08:43:24 +0000

    Oilpan: Vectors with inline capacity should need destructors.
    In order to recycle vector backings without GC, we'd like to deallocate
    them explicitly.  Vectors with inline capacity requires finalization with
    this CL.
    
    Vectors with inline capacity are often used as temporary buffers.  So,
    applying this only to them makes sense.
    
    * platform/heap/*
      Add InlineVectorBackingHeap, and implement new Allocator functions.
    
    * Vector.h
      Add hasInlineCapacity flag to VectorBufferBase template to call
      new Allocator functions.
      Remove HeapVectorWithInlineCapacityDestructorBase. Template
      specialization for on-heap inline capacity vectors are unnecessary.
    
    * DefaultAllocator.h
      Add new Allocator functions.  They just call existing functions.
    
    * CSSValuePool.h, EventPath.h, AsyncCallStackTracker.h:
      Need to change them to GarbageCollectedFinalized because they
      have vector members with inline capacity.
    
    * RenderTableSection.h
      Adjust VectorTraits for CellStruct to avoid compile-time assertion
      failure.  This is harmless because skipping destructors of HeapVector
      with inline capacity is safe.
    
    BUG=420515
    
    Review URL: https://codereview.chromium.org/783983004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186835 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/events/EventPath.h
+++ third_party/WebKit/Source/core/events/EventPath.h
@@ -43,7 +43,7 @@ class TouchEvent;
 class TouchList;
 class TreeScope;
 
-class EventPath final : public NoBaseWillBeGarbageCollected<EventPath> {
+class EventPath final : public NoBaseWillBeGarbageCollectedFinalized<EventPath> {
 public:
     explicit EventPath(Event&);
     explicit EventPath(Node&);
