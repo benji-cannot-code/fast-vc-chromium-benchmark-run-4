0af7789a442cd959a32de85a2921f041c1cd3667
sagarmittal <sagarmittal@google.com>
2014-12-10 13:39:14 -0800

    Delete blobs in IndexedDBContextImpl::DeleteForOrigin
    BUG=435631
    
    Review URL: https://codereview.chromium.org/770153004
    
    Cr-Commit-Position: refs/heads/master@{#307771}

--- content/browser/indexed_db/indexed_db_browsertest.cc
+++ content/browser/indexed_db/indexed_db_browsertest.cc
@@ -162,7 +162,6 @@ class IndexedDBBrowserTest : public ContentBrowserTest {
   }
 
   virtual void DidGetDiskUsage(int64 bytes) {
-    EXPECT_GT(bytes, 0);
     disk_usage_ = bytes;
   }
 
@@ -336,6 +335,7 @@ IN_PROC_BROWSER_TEST_F(IndexedDBBrowserTestWithVersion123456Schema,
   EXPECT_GT(original_size, 0);
   SimpleTest(GetTestUrl("indexeddb", "open_bad_db.html"));
   int64 new_size = RequestDiskUsage();
+  EXPECT_GT(new_size, 0);
   EXPECT_NE(original_size, new_size);
 }
 
@@ -350,6 +350,7 @@ IN_PROC_BROWSER_TEST_F(IndexedDBBrowserTestWithVersion987654SSVData,
   EXPECT_GT(original_size, 0);
   SimpleTest(GetTestUrl("indexeddb", "open_bad_db.html"));
   int64 new_size = RequestDiskUsage();
+  EXPECT_GT(new_size, 0);
   EXPECT_NE(original_size, new_size);
 }
 
@@ -364,6 +365,7 @@ IN_PROC_BROWSER_TEST_F(IndexedDBBrowserTestWithCorruptLevelDB,
   EXPECT_GT(original_size, 0);
   SimpleTest(GetTestUrl("indexeddb", "open_bad_db.html"));
   int64 new_size = RequestDiskUsage();
+  EXPECT_GT(new_size, 0);
   EXPECT_NE(original_size, new_size);
 }
 
@@ -378,6 +380,7 @@ IN_PROC_BROWSER_TEST_F(IndexedDBBrowserTestWithMissingSSTFile,
   EXPECT_GT(original_size, 0);
   SimpleTest(GetTestUrl("indexeddb", "open_missing_table.html"));
   int64 new_size = RequestDiskUsage();
+  EXPECT_GT(new_size, 0);
   EXPECT_NE(original_size, new_size);
 }
 
@@ -404,6 +407,22 @@ IN_PROC_BROWSER_TEST_F(IndexedDBBrowserTest, CanDeleteWhenOverQuotaTest) {
 
 IN_PROC_BROWSER_TEST_F(IndexedDBBrowserTest, BlobsCountAgainstQuota) {
   SimpleTest(GetTestUrl("indexeddb", "blobs_use_quota.html"));
+}
+
+IN_PROC_BROWSER_TEST_F(IndexedDBBrowserTest, DeleteForOriginDeletesBlobs) {
+  SimpleTest(GetTestUrl("indexeddb", "write_20mb_blob.html"));
+  int64 size = RequestDiskUsage();
+  // This assertion assumes that we do not compress blobs.
+  EXPECT_GT(size, 20 << 20 /* 20 MB */);
+  GetContext()->TaskRunner()->PostTask(
+      FROM_HERE, base::Bind(&IndexedDBContextImpl::DeleteForOrigin,
+                            GetContext(), GURL("file:///")));
+  scoped_refptr<base::ThreadTestHelper> helper(
+      new base::ThreadTestHelper(BrowserMainLoop::GetInstance()
+                                     ->indexed_db_thread()
+                                     ->message_loop_proxy()));
+  ASSERT_TRUE(helper->Run());
+  EXPECT_EQ(0, RequestDiskUsage());
 }
 
 namespace {
