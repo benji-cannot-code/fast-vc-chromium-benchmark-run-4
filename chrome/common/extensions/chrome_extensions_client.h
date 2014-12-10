ccbb39a842d053391b0addd282ad2e2ea3c56a09
sashab <sashab@chromium.org>
2014-12-10 02:54:48 -0800

    Added CoalescedPermissionMessages to ManifestPermissions
    Added a new type, CoalescedPermissionMessage, which represents a
    permission message made up of 0 or more permissions. Also started a
    refactor to move IDs out of APIPermission::ID and PermissionMessage::ID
    and into their own common class.
    
    Added a new method GetPermissions() to ManifestPermission, which allows
    manifest permissions to specify their own custom permissions (and hence
    custom messages) for apps with that permission. Updated all 5 subclasses
    (automation, bluetooth, sockets, UI overrides hanlder and the mock
    manifest).
    
    Also added another FilterHostPermissions() method to ExtensionsClient
    that can create CoalescedPermissionMessages.
    
    BUG=398257
    
    Committed: https://crrev.com/45d827a6ba247ef55275c52ade2dfddbaeb87a9f
    Cr-Commit-Position: refs/heads/master@{#307630}
    
    Review URL: https://codereview.chromium.org/750353003
    
    Cr-Commit-Position: refs/heads/master@{#307677}

--- chrome/common/extensions/chrome_extensions_client.h
+++ chrome/common/extensions/chrome_extensions_client.h
@@ -36,6 +36,9 @@ class ChromeExtensionsClient : public ExtensionsClient {
       const URLPatternSet& hosts,
       URLPatternSet* new_hosts,
       std::set<PermissionMessage>* messages) const override;
+  void FilterHostPermissions(const URLPatternSet& hosts,
+                             URLPatternSet* new_hosts,
+                             PermissionIDSet* permissions) const override;
   void SetScriptingWhitelist(const ScriptingWhitelist& whitelist) override;
   const ScriptingWhitelist& GetScriptingWhitelist() const override;
   URLPatternSet GetPermittedChromeSchemeHosts(
