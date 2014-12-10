f484bb0d0ec155e62e13318b3ba18206eabe0e8d
andresantoso <andresantoso@chromium.org>
2014-12-10 15:37:41 -0800

    MacViews: Fix for DropdownBarHost missing symbols
    MacViews browser was missing these symbols:
    (1) DropdownBarHost::SetHostViewNative
    (2) DropdownBarHost::SetWidgetPositionNative
    
    Fixed (1) by stubbing it for Mac.
    Fixed (2) by making it cross-platform.
    Also deleted the unused parameter |no_redraw|.
    
    BUG=425229
    
    Review URL: https://codereview.chromium.org/789913002
    
    Cr-Commit-Position: refs/heads/master@{#307793}

--- chrome/browser/ui/views/dropdown_bar_host_mac.mm
+++ chrome/browser/ui/views/dropdown_bar_host_mac.mm
@@ -1,14 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "chrome/browser/ui/views/dropdown_bar_host.h"
 
-#include "ui/aura/window.h"
-#include "ui/views/view_constants_aura.h"
-#include "ui/views/widget/widget.h"
-
 void DropdownBarHost::SetHostViewNative(views::View* host_view) {
-  host_->GetNativeView()->SetProperty(views::kHostViewKey, host_view);
+  // Nothing to do.
 }
