5b1d1e4463c56c12331e2997eaffeb3d6ed59983
tfarina <tfarina@chromium.org>
2014-12-09 17:39:25 -0800

    Cleanup: Remove --no-message-box switch from ui/base switches.
    It is probably better to just remove it, as it probably does not have
    many users.
    
    BUG=439769
    R=sky@chromium.org,pkasting@chromium.org
    
    Review URL: https://codereview.chromium.org/747393004
    
    Cr-Commit-Position: refs/heads/master@{#307612}

--- ui/base/win/message_box_win.cc
+++ ui/base/win/message_box_win.cc
@@ -5,9 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/base/win/message_box_win.h"
 
-#include "base/command_line.h"
 #include "base/i18n/rtl.h"
-#include "ui/base/ui_base_switches.h"
 
 namespace ui {
 
@@ -18,9 +16,6 @@ int MessageBox(HWND hwnd,
                const base::string16& text,
                const base::string16& caption,
                UINT flags) {
-  if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kNoMessageBox))
-    return IDOK;
-
   UINT actual_flags = flags;
   if (base::i18n::IsRTL())
     actual_flags |= MB_RIGHT | MB_RTLREADING;
