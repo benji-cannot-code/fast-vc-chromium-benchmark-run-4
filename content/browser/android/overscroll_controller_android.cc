e8db24f0de422593198840b152ba010ca0d75cb4
jdduke <jdduke@chromium.org>
2014-12-09 17:26:08 -0800

    [Android] Use NavigationController for pull-to-refresh activation
    When pull-to-refresh is triggered, use the NavigationController to
    trigger the reload instead of WebContents::ReloadFocusedFrame. This is
    consistent with how toolbar-triggered reloads are performed.
    
    Also reduce the animation time for a single reload animation rotation.
    
    BUG=440406,428429
    
    Review URL: https://codereview.chromium.org/792623003
    
    Cr-Commit-Position: refs/heads/master@{#307610}

--- content/browser/android/overscroll_controller_android.cc
+++ content/browser/android/overscroll_controller_android.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/android/edge_effect_l.h"
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/common/input/did_overscroll_params.h"
+#include "content/public/browser/navigation_controller.h"
 #include "content/public/common/content_switches.h"
 #include "third_party/WebKit/public/web/WebInputEvent.h"
 #include "ui/base/android/window_android_compositor.h"
@@ -276,7 +277,7 @@ void OverscrollControllerAndroid::TriggerRefresh() {
     return;
 
   triggered_refresh_active_ = true;
-  web_contents()->ReloadFocusedFrame(false);
+  web_contents()->GetController().Reload(true);
 }
 
 bool OverscrollControllerAndroid::IsStillRefreshing() const {
