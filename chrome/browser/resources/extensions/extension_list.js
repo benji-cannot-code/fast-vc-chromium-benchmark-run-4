e9884b061e5c86cfa3d418639f69171ef76734e7
cschuet <cschuet@chromium.org>
2014-12-10 05:16:09 -0800

    Add controlled setting indicators for policy-installed extensions.
    Unified representation of policy-installed extensions between settings
    and extensions page.
    
    BUG=110366
    TEST=force-install extension through policy and check that indicators appears.
    
    Review URL: https://codereview.chromium.org/647733002
    
    Cr-Commit-Position: refs/heads/master@{#307688}

--- chrome/browser/resources/extensions/extension_list.js
+++ chrome/browser/resources/extensions/extension_list.js
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *            order: number,
  *            packagedApp: boolean,
  *            path: (string|undefined),
+ *            policyText: (string|undefined),
  *            prettifiedPath: (string|undefined),
  *            recommendedInstall: boolean,
  *            runtimeErrors: (Array.<RuntimeError>|undefined),
@@ -171,17 +172,19 @@ cr.define('options', function() {
       if (!extension.enabled || extension.terminated)
         node.classList.add('inactive-extension');
 
-      if (extension.managedInstall ||
-          extension.dependentExtensions.length > 0) {
-        node.classList.add('may-not-modify');
-        node.classList.add('may-not-remove');
+      var classes = [];
+      if (extension.managedInstall) {
+        classes.push('policy-controlled', 'may-not-modify');
+      } else if (extension.dependentExtensions.length > 0) {
+        classes.push('may-not-remove', 'may-not-modify');
       } else if (extension.recommendedInstall) {
-        node.classList.add('may-not-remove');
+        classes.push('may-not-remove');
       } else if (extension.suspiciousInstall ||
                  extension.corruptInstall ||
                  extension.updateRequiredByPolicy) {
-        node.classList.add('may-not-modify');
+        classes.push('may-not-modify');
       }
+      node.classList.add.apply(node.classList, classes);
 
       var idToHighlight = this.getIdQueryParam_();
       if (node.id == idToHighlight)
@@ -363,6 +366,14 @@ cr.define('options', function() {
                                      extension.updateRequiredByPolicy ||
                                      extension.dependentExtensions.length > 0;
         enable.querySelector('input').disabled = enableCheckboxDisabled;
+
+        if (extension.managedInstall) {
+          var indicator = new cr.ui.ControlledIndicator();
+          indicator.classList.add('controlled-extension-indicator');
+          indicator.setAttribute('controlled-by', 'policy');
+          indicator.setAttribute('textpolicy', extension.policyText);
+          node.querySelector('.enable-controls').appendChild(indicator);
+        }
 
         if (!enableCheckboxDisabled) {
           enable.addEventListener('click', function(e) {
