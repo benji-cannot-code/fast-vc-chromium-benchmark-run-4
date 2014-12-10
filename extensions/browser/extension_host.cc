a60f262bbe72514604d514c264451fd16af82b12
oysteine <oysteine@chromium.org>
2014-12-10 10:06:38 -0800

    Notify the webcontents of the background page of extensions that it's hidden, if we're running in the right ThrottleBackgroundExtensionPages field trial.
    This means the background pages will behave similarly to hidden tabs in terms of resources, including JS timer throttling
    
    R=yoz,kalman,asvitkine
    BUG=424661
    
    Review URL: https://codereview.chromium.org/732803004
    
    Cr-Commit-Position: refs/heads/master@{#307733}

--- extensions/browser/extension_host.cc
+++ extensions/browser/extension_host.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/singleton.h"
 #include "base/memory/weak_ptr.h"
 #include "base/message_loop/message_loop.h"
+#include "base/metrics/field_trial.h"
 #include "base/metrics/histogram.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
@@ -190,6 +191,16 @@ void ExtensionHost::CreateRenderViewNow() {
   LoadInitialURL();
   if (IsBackgroundPage()) {
     DCHECK(IsRenderViewLive());
+    if (extension_) {
+      std::string group_name = base::FieldTrialList::FindFullName(
+          "ThrottleExtensionBackgroundPages");
+      if ((group_name == "ThrottlePersistent" &&
+           extensions::BackgroundInfo::HasPersistentBackgroundPage(
+               extension_)) ||
+          group_name == "ThrottleAll") {
+        host_contents_->WasHidden();
+      }
+    }
     // Connect orphaned dev-tools instances.
     delegate_->OnRenderViewCreatedForBackgroundPage(this);
   }
