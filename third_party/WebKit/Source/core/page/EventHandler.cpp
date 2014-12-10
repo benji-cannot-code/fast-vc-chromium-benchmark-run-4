6a5a9ee34050184e2c8c8616cff821aff902821a
jdduke@chromium.org <jdduke@chromium.org>
2014-12-10 00:50:56 +0000

    Disable touch adjustment for mobile-optimized viewports
    Use the existing |WebViewImpl::shouldDisableDesktopWorkarounds()| signal
    to suppress touch adjustment for tap-related events. When the content
    is optimized for the device viewport, the adjustment should not be
    necessary.
    
    BUG=417534
    
    Review URL: https://codereview.chromium.org/780223004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186818 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/EventHandler.cpp
+++ third_party/WebKit/Source/core/page/EventHandler.cpp
@@ -2547,6 +2547,8 @@ bool EventHandler::shouldApplyTouchAdjustment(const PlatformGestureEvent& event)
 {
     if (m_frame->settings() && !m_frame->settings()->touchAdjustmentEnabled())
         return false;
+    if (m_frame->page() && m_frame->page()->chrome().client().shouldDisableDesktopWorkarounds())
+        return false;
     return !event.area().isEmpty();
 }
 
