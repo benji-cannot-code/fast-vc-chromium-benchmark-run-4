e06278bba7d82e988420f57b1244472847174179
enne <enne@chromium.org>
2014-12-10 14:00:42 -0800

    cc: Make blending pixel tests use impl-side painting
    R=reveman@chromium.org,rosca@adobe.com
    BUG=439621
    
    Review URL: https://codereview.chromium.org/739893004
    
    Cr-Commit-Position: refs/heads/master@{#307775}

--- cc/test/layer_tree_pixel_resource_test.cc
+++ cc/test/layer_tree_pixel_resource_test.cc
@@ -218,6 +218,9 @@ void LayerTreeHostPixelResourceTest::RunPixelResourceTest(
   if (!IsTestCaseSupported(test_case_))
     return;
   RunPixelTest(test_type_, content_root, file_name);
+
+  if (layer_tree_host())
+    EXPECT_TRUE(layer_tree_host()->settings().impl_side_painting);
 }
 
 ParameterizedPixelResourceTest::ParameterizedPixelResourceTest()
