a9e546c10aa25b8939c8ff707bb7de6e6387cd9d
hashimoto <hashimoto@chromium.org>
2014-12-10 09:41:41 -0800

    Reduce page_cycler repeat count from 10 to 6
    Reduce the count to avoid timeout.
    
    BUG=429709
    TEST=tools/perf/run_benchmark page_cycler.typical_25
    
    Review URL: https://codereview.chromium.org/789753002
    
    Cr-Commit-Position: refs/heads/master@{#307726}

--- tools/perf/benchmarks/page_cycler.py
+++ tools/perf/benchmarks/page_cycler.py
@@ -9,7 +9,7 @@ from telemetry import benchmark
 
 
 class _PageCycler(benchmark.Benchmark):
-  options = {'pageset_repeat': 10}
+  options = {'pageset_repeat': 6}
 
   @classmethod
   def AddBenchmarkCommandLineArgs(cls, parser):
@@ -82,7 +82,7 @@ class PageCyclerNetsimTop10(_PageCycler):
   options = {
       'cold_load_percent': 100,
       'extra_wpr_args_as_string': '--shaping_type=proxy --net=cable',
-      'pageset_repeat': 5,
+      'pageset_repeat': 6,
   }
 
   def CreatePageTest(self, options):
