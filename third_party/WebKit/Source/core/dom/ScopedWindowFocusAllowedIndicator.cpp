d24be52fa23862cbab15da9cff257ee566f297d5
mlamouri@chromium.org <mlamouri@chromium.org>
2014-12-10 15:30:02 +0000

    Bind Window focus ability to the ExecutionContext instead of the process.
    WindowFocusAllowedIndicator is using a static variable that allows
    the entire process to focus a window. In addition, it is entirely
    stack based which prevents asynchronous methods to use it properly.
    
    By binding the window focus ability to the ExecutionContext,
    we can allow asynchronous methods but also, reduces the chances
    of unrelated scripts to focus windows when they should not be
    able to. Those two concerns are slightly related because an
    ExecutionContext will now be able to focus a window for a
    long period of time, thus, we do not want other ExecutionContext
    to be able to do that in the meantime.
    
    This is part of a three-sided CL:
    Part 1: <this>
    Part 2: https://codereview.chromium.org/786403003
    Part 3: https://codereview.chromium.org/722423004
    
    BUG=440740
    
    Review URL: https://codereview.chromium.org/777483004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186849 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/Source/core/dom/ScopedWindowFocusAllowedIndicator.cpp
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "config.h"
+#include "core/dom/ScopedWindowFocusAllowedIndicator.h"
+
+#include "core/dom/ExecutionContext.h"
+
+namespace blink {
+
+ScopedWindowFocusAllowedIndicator::ScopedWindowFocusAllowedIndicator(
+    ExecutionContext* context)
+    : ContextLifecycleObserver(context)
+{
+    if (executionContext())
+        executionContext()->allowWindowFocus();
+}
+
+ScopedWindowFocusAllowedIndicator::~ScopedWindowFocusAllowedIndicator()
+{
+    if (executionContext())
+        executionContext()->consumeWindowFocus();
+}
+
+} // namespace blink
---
+++ third_party/WebKit/Source/core/dom/ScopedWindowFocusAllowedIndicator.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ScopedWindowFocusAllowedIndicator_h
+#define ScopedWindowFocusAllowedIndicator_h
+
+#include "core/dom/ContextLifecycleObserver.h"
+#include "wtf/Noncopyable.h"
+
+namespace blink {
+
+class ExecutionContext;
+
+class ScopedWindowFocusAllowedIndicator : ContextLifecycleObserver {
+    WTF_MAKE_NONCOPYABLE(ScopedWindowFocusAllowedIndicator);
+public:
+    explicit ScopedWindowFocusAllowedIndicator(ExecutionContext*);
+    ~ScopedWindowFocusAllowedIndicator();
+};
+
+} // namespace blink
+
+#endif // ScopedWindowFocusAllowedIndicator_h
