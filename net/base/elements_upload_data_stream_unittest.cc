0c7fe497a296211fca97a71397d31238757cd8e3
haavardm <haavardm@opera.com>
2014-12-10 06:59:43 -0800

    Fix crashes in ElementsUploadDataStreamTest
    standard lib reports:
    error: attempt to insert with an iterator range
    [__first, __last) from this container.
    
    When compiling standard lib from gcc 4.9 with C++11, insert
    cannot take the same vector as both source and destination.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/789953002
    
    Cr-Commit-Position: refs/heads/master@{#307699}

--- net/base/elements_upload_data_stream_unittest.cc
+++ net/base/elements_upload_data_stream_unittest.cc
@@ -670,8 +670,8 @@ TEST_F(ElementsUploadDataStreamTest, InitToReset) {
       new ElementsUploadDataStream(element_readers_.Pass(), 0));
 
   std::vector<char> expected_data(kTestData, kTestData + kTestDataSize);
-  expected_data.insert(expected_data.end(), expected_data.begin(),
-                       expected_data.begin() + kTestDataSize);
+  expected_data.insert(expected_data.end(), kTestData,
+                       kTestData + kTestDataSize);
 
   // Call Init().
   TestCompletionCallback init_callback1;
@@ -729,8 +729,8 @@ TEST_F(ElementsUploadDataStreamTest, InitDuringAsyncInit) {
       new ElementsUploadDataStream(element_readers_.Pass(), 0));
 
   std::vector<char> expected_data(kTestData, kTestData + kTestDataSize);
-  expected_data.insert(expected_data.end(), expected_data.begin(),
-                       expected_data.begin() + kTestDataSize);
+  expected_data.insert(expected_data.end(), kTestData,
+                       kTestData + kTestDataSize);
 
   // Start Init.
   TestCompletionCallback init_callback1;
@@ -778,8 +778,8 @@ TEST_F(ElementsUploadDataStreamTest, InitDuringAsyncRead) {
       new ElementsUploadDataStream(element_readers_.Pass(), 0));
 
   std::vector<char> expected_data(kTestData, kTestData + kTestDataSize);
-  expected_data.insert(expected_data.end(), expected_data.begin(),
-                       expected_data.begin() + kTestDataSize);
+  expected_data.insert(expected_data.end(), kTestData,
+                       kTestData + kTestDataSize);
 
   // Call Init().
   TestCompletionCallback init_callback1;
