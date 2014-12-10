6f3102568ff446e9bfd9b427192cab4da7938585
mvanouwerkerk <mvanouwerkerk@chromium.org>
2014-12-10 12:01:46 -0800

    Push API: pass ServiceWorkerRegistration for registration from document [switchover 4/6]
    Depends on https://codereview.chromium.org/786013002/ (Push API: pass ServiceWorkerRegistration to client for registration from document.)
    
    This part 4 of a series of 6 patches. See https://crbug.com/440076 for the plan to land them.
    
    BUG=389194, 440076
    
    Review URL: https://codereview.chromium.org/788693002
    
    Cr-Commit-Position: refs/heads/master@{#307746}

--- content/renderer/push_messaging_dispatcher.cc
+++ content/renderer/push_messaging_dispatcher.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/utf_string_conversions.h"
 #include "content/child/service_worker/web_service_worker_provider_impl.h"
+#include "content/child/service_worker/web_service_worker_registration_impl.h"
 #include "content/common/push_messaging_messages.h"
 #include "content/renderer/manifest/manifest_manager.h"
 #include "content/renderer/render_frame_impl.h"
@@ -14,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/platform/WebPushError.h"
 #include "third_party/WebKit/public/platform/WebPushRegistration.h"
 #include "third_party/WebKit/public/platform/WebServiceWorkerProvider.h"
+#include "third_party/WebKit/public/platform/WebServiceWorkerRegistration.h"
 #include "third_party/WebKit/public/platform/WebString.h"
 #include "url/gurl.h"
 
@@ -48,14 +50,26 @@ void PushMessagingDispatcher::registerPushMessaging(
     blink::WebPushRegistrationCallbacks* callbacks,
     blink::WebServiceWorkerProvider* service_worker_provider) {
   DCHECK(callbacks);
-  RenderFrameImpl::FromRoutingID(routing_id())->manifest_manager()->GetManifest(
-      base::Bind(&PushMessagingDispatcher::DoRegister,
-                 base::Unretained(this),
-                 callbacks,
-                 service_worker_provider));
+  RenderFrameImpl::FromRoutingID(routing_id())
+      ->manifest_manager()
+      ->GetManifest(base::Bind(&PushMessagingDispatcher::DoRegisterOld,
+                               base::Unretained(this), callbacks,
+                               service_worker_provider));
 }
 
-void PushMessagingDispatcher::DoRegister(
+void PushMessagingDispatcher::registerPushMessaging(
+    blink::WebServiceWorkerRegistration* service_worker_registration,
+    blink::WebPushRegistrationCallbacks* callbacks) {
+  DCHECK(service_worker_registration);
+  DCHECK(callbacks);
+  RenderFrameImpl::FromRoutingID(routing_id())
+      ->manifest_manager()
+      ->GetManifest(base::Bind(&PushMessagingDispatcher::DoRegister,
+                               base::Unretained(this),
+                               service_worker_registration, callbacks));
+}
+
+void PushMessagingDispatcher::DoRegisterOld(
     blink::WebPushRegistrationCallbacks* callbacks,
     blink::WebServiceWorkerProvider* service_worker_provider,
     const Manifest& manifest) {
@@ -71,13 +85,39 @@ void PushMessagingDispatcher::DoRegister(
     return;
   }
 
+  Send(new PushMessagingHostMsg_RegisterFromDocumentOld(
+      routing_id(), request_id,
+      manifest.gcm_sender_id.is_null()
+          ? std::string()
+          : base::UTF16ToUTF8(manifest.gcm_sender_id.string()),
+      manifest.gcm_user_visible_only, service_worker_provider_id));
+}
+
+void PushMessagingDispatcher::DoRegister(
+    blink::WebServiceWorkerRegistration* service_worker_registration,
+    blink::WebPushRegistrationCallbacks* callbacks,
+    const Manifest& manifest) {
+  int request_id = registration_callbacks_.Add(callbacks);
+  int64 service_worker_registration_id =
+      static_cast<WebServiceWorkerRegistrationImpl*>(
+          service_worker_registration)->registration_id();
+
+  std::string sender_id =
+      manifest.gcm_sender_id.is_null()
+          ? std::string()
+          : base::UTF16ToUTF8(manifest.gcm_sender_id.string());
+  if (sender_id.empty()) {
+    OnRegisterFromDocumentError(request_id,
+                                PUSH_REGISTRATION_STATUS_NO_SENDER_ID);
+    return;
+  }
+
   Send(new PushMessagingHostMsg_RegisterFromDocument(
       routing_id(), request_id,
       manifest.gcm_sender_id.is_null()
           ? std::string()
           : base::UTF16ToUTF8(manifest.gcm_sender_id.string()),
-      manifest.gcm_user_visible_only,
-      service_worker_provider_id));
+      manifest.gcm_user_visible_only, service_worker_registration_id));
 }
 
 void PushMessagingDispatcher::getPermissionStatus(
