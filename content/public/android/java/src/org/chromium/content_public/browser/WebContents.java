576f2e4ce65f6cd3dded0082de8eb66d0d7d19e6
yusufo <yusufo@chromium.org>
2014-12-10 10:42:34 -0800

    Move VisibleForTesting from WebContents implementation to interface
    This was added to the implementation by mistake. This annotation is needed
    for proguard to not strip these calls because they will be used for downstream
    tests.
    
    BUG=434499
    
    Review URL: https://codereview.chromium.org/787123004
    
    Cr-Commit-Position: refs/heads/master@{#307738}

--- content/public/android/java/src/org/chromium/content_public/browser/WebContents.java
+++ content/public/android/java/src/org/chromium/content_public/browser/WebContents.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.content_public.browser;
 
+import org.chromium.base.VisibleForTesting;
+
 /**
  * The WebContents Java wrapper to allow communicating with the native WebContents object.
  */
@@ -76,6 +78,7 @@ public interface WebContents {
      * @param url The URL being blocked by the interstitial.
      * @param delegate The delegate handling the interstitial.
      */
+    @VisibleForTesting
     public void showInterstitialPage(
             String url, long interstitialPageDelegateAndroid);
 
