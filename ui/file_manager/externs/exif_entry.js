8a3c14ce9004ddf0ab22059fd40b50432d7cc72d
yawano <yawano@chromium.org>
2014-12-10 22:13:11 -0800

    Add type annotations to gallery/js/image_editor/exif_encoder.js.
    BUG=433728
    TEST=out/Release/browser_tests --gtest_filter=GalleryJsTest.ExifEncoderTest
    
    Review URL: https://codereview.chromium.org/794513003
    
    Cr-Commit-Position: refs/heads/master@{#307868}

---
+++ ui/file_manager/externs/exif_entry.js
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Type definition of exif entry.
+ * @typedef {{id:!Exif.Tag, format:number, componentCount:number,
+ *     value:(undefined|*)}}
+ */
+var ExifEntry;
