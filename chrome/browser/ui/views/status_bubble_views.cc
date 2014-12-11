fe6e0fe66f14181a6cc41061ace5baa338f4a1ae
andresantoso <andresantoso@chromium.org>
2014-12-11 12:21:23 -0800

    MacViews: Implement event monitoring for a specific window
    ManagePasswordBubbleView wants to monitor events on a specific window,
    currently using aura specific code that won't work for MacViews.
    views::EventMonitor is enhanced to support monitoring events on a window,
    with both Mac and Aura implementations.
    
    Also includes minor compile fixes in ZoomBubbleView and StatusBubbleView.
    
    BUG=425229
    TEST=EventMonitorAuraTest, EventMonitorMacTest
    
    Review URL: https://codereview.chromium.org/730833006
    
    Cr-Commit-Position: refs/heads/master@{#307960}

--- chrome/browser/ui/views/status_bubble_views.cc
+++ chrome/browser/ui/views/status_bubble_views.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
-#include "ash/wm/window_state.h"
 #include "base/bind.h"
 #include "base/i18n/rtl.h"
 #include "base/message_loop/message_loop.h"
@@ -18,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_util.h"
 #include "third_party/skia/include/core/SkPaint.h"
 #include "third_party/skia/include/core/SkRect.h"
-#include "ui/aura/window.h"
 #include "ui/base/theme_provider.h"
 #include "ui/gfx/animation/animation_delegate.h"
 #include "ui/gfx/animation/linear_animation.h"
@@ -35,6 +33,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/root_view.h"
 #include "ui/views/widget/widget.h"
 #include "url/gurl.h"
+
+#if defined(USE_ASH)
+#include "ash/wm/window_state.h"
+#endif
 
 // The alpha and color of the bubble's shadow.
 static const SkColor kShadowColor = SkColorSetARGB(30, 0, 0, 0);
@@ -581,13 +583,14 @@ void StatusBubbleViews::Init() {
     params.parent = frame->GetNativeView();
     params.context = frame->GetNativeWindow();
     popup_->Init(params);
-    popup_->GetNativeView()->SetName("StatusBubbleViews");
     // We do our own animation and don't want any from the system.
     popup_->SetVisibilityChangedAnimationsEnabled(false);
     popup_->SetOpacity(0x00);
     popup_->SetContentsView(view_);
+#if defined(USE_ASH)
     ash::wm::GetWindowState(popup_->GetNativeWindow())->
         set_ignored_by_shelf(true);
+#endif
     RepositionPopup();
   }
 }
