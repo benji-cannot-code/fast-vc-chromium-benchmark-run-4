7f03664f53b6217d97c3b6a9299372c370e91dd0
jbauman <jbauman@chromium.org>
2014-12-10 19:21:28 -0800

    Check client_ in SurfaceDisplayOutputSurface::SwapBuffersComplete
    This may now be called in the destructor, where client_ is NULL.
    
    Review URL: https://codereview.chromium.org/794963002
    
    Cr-Commit-Position: refs/heads/master@{#307847}

--- content/browser/compositor/surface_display_output_surface.cc
+++ content/browser/compositor/surface_display_output_surface.cc
@@ -85,7 +85,8 @@ void SurfaceDisplayOutputSurface::ReturnResources(
 }
 
 void SurfaceDisplayOutputSurface::SwapBuffersComplete(bool drawn) {
-  client_->DidSwapBuffersComplete();
+  if (client_)
+    client_->DidSwapBuffersComplete();
 }
 
 }  // namespace content
