b48c8bcd438f7076bfcea41eb5c59ad24239533d
mathp <mathp@chromium.org>
2014-12-11 13:03:59 -0800

    [AppList] Use local favicons/icons with URLSuggestionResult
    If available, will use local icons. Otherwise fallback to server provided image.
    
    BUG=440487
    TBR=stevenjb
    
    Review URL: https://codereview.chromium.org/795863002
    
    Cr-Commit-Position: refs/heads/master@{#307973}

--- chrome/browser/ui/app_list/search/suggestions/suggestions_search_provider.h
+++ chrome/browser/ui/app_list/search/suggestions/suggestions_search_provider.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/app_list/search_provider.h"
 
 class AppListControllerDelegate;
+class FaviconService;
 class Profile;
 
 namespace suggestions {
@@ -38,6 +39,7 @@ class SuggestionsSearchProvider : public SearchProvider {
 
   Profile* profile_;
   AppListControllerDelegate* list_controller_;
+  FaviconService* favicon_service_;
   suggestions::SuggestionsService* suggestions_service_;
 
   // For callbacks may be run after destruction.
