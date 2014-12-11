3ff7e278069647f6ae3cb4cda9e019fc5d54f3a4
bengr <bengr@chromium.org>
2014-12-11 14:20:48 -0800

    Data reduction proxy bypass with test server
    Include tests that verify the interaction of the data
    reduction proxy's bypass mechanism and a real server.
    
    BUG=438866
    
    Review URL: https://codereview.chromium.org/749793002
    
    Cr-Commit-Position: refs/heads/master@{#307982}

--- components/data_reduction_proxy/core/common/data_reduction_proxy_params.h
+++ components/data_reduction_proxy/core/common/data_reduction_proxy_params.h
@@ -281,6 +281,10 @@ class DataReductionProxyParams {
   virtual std::string GetDefaultProbeURL() const;
   virtual std::string GetDefaultWarmupURL() const;
 
+ protected:
+  GURL origin_;
+  GURL fallback_origin_;
+
  private:
   // Checks if the primary and fallback data reduction proxies are in the retry
   // map. Returns true if the request is bypassed by both data reduction
@@ -294,8 +298,6 @@ class DataReductionProxyParams {
 
   DataReductionProxyParams& operator=(const DataReductionProxyParams& params);
 
-  GURL origin_;
-  GURL fallback_origin_;
   GURL ssl_origin_;
   GURL alt_origin_;
   GURL alt_fallback_origin_;
