e9bc51222972641fd5fc181e5a12a4a78b893c9c
yhirano@chromium.org <yhirano@chromium.org>
2014-12-11 15:25:53 +0000

    Expose Fetch API related classes to Window and WorkerGlobalScope.
    Expose Request, Response and Headers to Window and WorkerGlobalScope under
    "GlobalFetch" runtime enabled flag.
    
    BUG=436770
    
    Review URL: https://codereview.chromium.org/771323004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186953 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/fetch/resources/request.js
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+var global = this;
+if (global.importScripts) {
+    // Worker case
+    importScripts('/resources/testharness.js');
+}
+
+test(function(test) {
+    var request = new Request('');
+    assert_equals(request.constructor, Request);
+}, 'Create request');
+
+test(function(test) {
+    var response = new Response;
+    assert_equals(response.constructor, Response);
+}, 'Create response');
+
+test(function(test) {
+    var headers = new Headers;
+    assert_equals(headers.constructor, Headers);
+}, 'Create headers');
+
+if (global.importScripts) {
+    // Worker case
+    done();
+}
