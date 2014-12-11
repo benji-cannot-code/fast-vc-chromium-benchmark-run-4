2bbb6ab93320bfb06f58cff11db2b619f51113b8
magjed <magjed@chromium.org>
2014-12-10 17:23:03 -0800

    VideoResourceUpdater: Reuse resources for same frame, even if ResourceProvider has references to it
    This CL is a follow up to https://codereview.chromium.org/759143003/.
    The problem with the previous CL is that it doesn't always reuse the
    resources, because they may still be in use by ResourceProvider. When
    VideoLayerImpl::DidDraw is called, it will try to free the resources by
    calling:
    for (size_t i = 0; i < frame_resources_.size(); ++i)
      resource_provider->DeleteResource(frame_resources_[i]);
    ...
    void ResourceProvider::DeleteResource(ResourceId id) {
      ...
      if (resource->exported_count > 0 || resource->lock_for_read_count > 0) {
        resource->marked_for_deletion = true;
        return;
      } else {
        DeleteResourceInternal(it, Normal);
      }
    }
    It is common that the "marked_for_deletion" path is used, and subsequent
    calls to VideoLayerImpl::WillDraw may be forced to upload the data
    again. This CL refactors VideoResourceUpdater so it can reuse the
    uploaded data in those cases.
    
    BUG=437653
    
    Review URL: https://codereview.chromium.org/787723002
    
    Cr-Commit-Position: refs/heads/master@{#307823}

--- cc/resources/video_resource_updater.h
+++ cc/resources/video_resource_updater.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CC_RESOURCES_VIDEO_RESOURCE_UPDATER_H_
 #define CC_RESOURCES_VIDEO_RESOURCE_UPDATER_H_
 
+#include <list>
 #include <vector>
 
 #include "base/basictypes.h"
@@ -79,6 +80,9 @@ class CC_EXPORT VideoResourceUpdater
     gfx::Size resource_size;
     ResourceFormat resource_format;
     gpu::Mailbox mailbox;
+    // The balance between the number of times this resource has been returned
+    // from CreateForSoftwarePlanes vs released in RecycleResource.
+    int ref_count;
     // These last three members will be used for identifying the data stored in
     // this resource, and uniquely identifies a media::VideoFrame plane. The
     // frame pointer will only be used for pointer comparison, i.e. the
@@ -101,7 +105,13 @@ class CC_EXPORT VideoResourceUpdater
                                        int plane_index,
                                        PlaneResource* plane_resource);
 
-  void DeleteResource(unsigned resource_id);
+  // This needs to be a container where iterators can be erased without
+  // invalidating other iterators.
+  typedef std::list<PlaneResource> ResourceList;
+  ResourceList::iterator AllocateResource(const gfx::Size& plane_size,
+                                          ResourceFormat format,
+                                          bool has_mailbox);
+  void DeleteResource(ResourceList::iterator resource_it);
   bool VerifyFrame(const scoped_refptr<media::VideoFrame>& video_frame);
   VideoFrameExternalResources CreateForHardwarePlanes(
       const scoped_refptr<media::VideoFrame>& video_frame);
@@ -109,7 +119,7 @@ class CC_EXPORT VideoResourceUpdater
       const scoped_refptr<media::VideoFrame>& video_frame);
 
   static void RecycleResource(base::WeakPtr<VideoResourceUpdater> updater,
-                              PlaneResource data,
+                              unsigned resource_id,
                               uint32 sync_point,
                               bool lost_resource,
                               BlockingTaskRunner* main_thread_task_runner);
@@ -123,10 +133,9 @@ class CC_EXPORT VideoResourceUpdater
   ResourceProvider* resource_provider_;
   scoped_ptr<media::SkCanvasVideoRenderer> video_renderer_;
 
-  std::vector<unsigned> all_resources_;
   // Recycle resources so that we can reduce the number of allocations and
   // data transfers.
-  std::vector<PlaneResource> recycled_resources_;
+  ResourceList all_resources_;
 
   DISALLOW_COPY_AND_ASSIGN(VideoResourceUpdater);
 };
