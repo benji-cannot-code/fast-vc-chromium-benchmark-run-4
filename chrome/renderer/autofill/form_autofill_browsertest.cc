ff7ae13046cb3a1a7d50c1f8409102138d6e7022
thestig <thestig@chromium.org>
2014-12-10 16:47:10 -0800

    Autofill: Move test only methods to test file and remove some dead code.
    Review URL: https://codereview.chromium.org/795443003
    
    Cr-Commit-Position: refs/heads/master@{#307809}

--- chrome/renderer/autofill/form_autofill_browsertest.cc
+++ chrome/renderer/autofill/form_autofill_browsertest.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/web/WebDocument.h"
 #include "third_party/WebKit/public/web/WebElement.h"
 #include "third_party/WebKit/public/web/WebElementCollection.h"
+#include "third_party/WebKit/public/web/WebExceptionCode.h"
 #include "third_party/WebKit/public/web/WebFormControlElement.h"
 #include "third_party/WebKit/public/web/WebFormElement.h"
 #include "third_party/WebKit/public/web/WebInputElement.h"
@@ -30,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using base::ASCIIToUTF16;
 using blink::WebElement;
+using blink::WebExceptionCode;
 using blink::WebFormControlElement;
 using blink::WebFormElement;
 using blink::WebFrame;
@@ -38,6 +40,8 @@ using blink::WebSelectElement;
 using blink::WebString;
 using blink::WebTextAreaElement;
 using blink::WebVector;
+
+namespace autofill {
 
 namespace {
 
@@ -85,9 +89,54 @@ static const char kFormHtml[] =
     "  <INPUT type='submit' name='reply-send' value='Send'/>"
     "</FORM>";
 
-}  // namespace
+std::string RetrievalMethodToString(
+    const WebElementDescriptor::RetrievalMethod& method) {
+  switch (method) {
+    case WebElementDescriptor::CSS_SELECTOR:
+      return "CSS_SELECTOR";
+    case WebElementDescriptor::ID:
+      return "ID";
+    case WebElementDescriptor::NONE:
+      return "NONE";
+  }
+  NOTREACHED();
+  return "UNKNOWN";
+}
 
-namespace autofill {
+bool ClickElement(const blink::WebDocument& document,
+                  const WebElementDescriptor& element_descriptor) {
+  WebString web_descriptor = WebString::fromUTF8(element_descriptor.descriptor);
+  blink::WebElement element;
+
+  switch (element_descriptor.retrieval_method) {
+    case WebElementDescriptor::CSS_SELECTOR: {
+      WebExceptionCode ec = 0;
+      element = document.querySelector(web_descriptor, ec);
+      if (ec)
+        DVLOG(1) << "Query selector failed. Error code: " << ec << ".";
+      break;
+    }
+    case WebElementDescriptor::ID:
+      element = document.getElementById(web_descriptor);
+      break;
+    case WebElementDescriptor::NONE:
+      return true;
+  }
+
+  if (element.isNull()) {
+    DVLOG(1) << "Could not find "
+             << element_descriptor.descriptor
+             << " by "
+             << RetrievalMethodToString(element_descriptor.retrieval_method)
+             << ".";
+    return false;
+  }
+
+  element.simulateClick();
+  return true;
+}
+
+}  // namespace
 
 class FormAutofillTest : public ChromeRenderViewTest {
  public:
@@ -247,11 +296,6 @@ class FormAutofillTest : public ChromeRenderViewTest {
       EXPECT_EQ(expected_value.utf8(), value.utf8());
 
     EXPECT_EQ(field_case.should_be_autofilled, element.isAutofilled());
-  }
-
-  static void FillFormForAllFieldsWrapper(const FormData& form,
-                                       const WebInputElement& element) {
-    FillFormForAllElements(form, element.form());
   }
 
   static void FillFormIncludingNonFocusableElementsWrapper(
