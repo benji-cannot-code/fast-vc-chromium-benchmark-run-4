dec2c1a2b779903d3313ae6be0e30bfe417a2c87
wfh <wfh@chromium.org>
2014-12-11 11:50:48 -0800

    Win32k lockdown: move to chrome://flags, add UMA, add Finch.
    Supplying via command line is not sustainable because shortcuts are sometimes sanitized, so moving to a flag so a persistent state can be set.
    
    Also add a Finch experiment to enable win32k lockdown, and an UMA metric to report when it's enabled (either by Finch or flags)
    
    BUG=365160
    
    Review URL: https://codereview.chromium.org/792873004
    
    Cr-Commit-Position: refs/heads/master@{#307953}

--- content/renderer/pepper/resource_creation_impl.cc
+++ content/renderer/pepper/resource_creation_impl.cc
@@ -141,8 +141,7 @@ PP_Resource ResourceCreationImpl::CreateImageData(PP_Instance instance,
   // TODO(ananta)
   // Look into whether this causes a loss of functionality. From cursory
   // testing things seem to work well.
-  if (CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kEnableWin32kRendererLockDown) &&
+  if (switches::IsWin32kRendererLockdownEnabled() &&
       base::win::GetVersion() >= base::win::VERSION_WIN8) {
     return CreateImageDataSimple(instance, format, size, init_to_zero);
   }
