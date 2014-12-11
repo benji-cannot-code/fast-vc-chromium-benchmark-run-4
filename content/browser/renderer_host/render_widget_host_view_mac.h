38f3754073686adea6202ddfdd7b75e369fc8f7e
anujk.sharma <anujk.sharma@samsung.com>
2014-12-11 09:04:43 -0800

    Fix WeakPtrFactory member ordering in content
    Changing in the intialization order of WeakPtrFactory such that all member variables should appear before the WeakPtrFactory to ensure that any WeakPtrs to Controller are invalidated before its members variable's destructors are executed, rendering them invalid.
    
    BUG=303818
    
    Review URL: https://codereview.chromium.org/794083002
    
    Cr-Commit-Position: refs/heads/master@{#307915}

--- content/browser/renderer_host/render_widget_host_view_mac.h
+++ content/browser/renderer_host/render_widget_host_view_mac.h
@@ -522,9 +522,6 @@ class CONTENT_EXPORT RenderWidgetHostViewMac
   // RenderWidgetHostViewGuest.
   bool is_guest_view_hack_;
 
-  // Factory used to safely scope delayed calls to ShutdownHost().
-  base::WeakPtrFactory<RenderWidgetHostViewMac> weak_factory_;
-
   // selected text on the renderer.
   std::string selected_text_;
 
@@ -551,6 +548,9 @@ class CONTENT_EXPORT RenderWidgetHostViewMac
 
   // The current caret bounds.
   gfx::Rect caret_rect_;
+
+  // Factory used to safely scope delayed calls to ShutdownHost().
+  base::WeakPtrFactory<RenderWidgetHostViewMac> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(RenderWidgetHostViewMac);
 };
