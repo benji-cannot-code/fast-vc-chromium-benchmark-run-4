35d31c96ff7fcb9b6500105fff1400aad5c4eff7
henriks@opera.com <henriks@opera.com>
2014-12-11 16:55:41 +0000

    Do not use inbandTrackIndex() when assigning track index
    Simply append the track at the end instead.
    
    BUG=440777
    R=philipj@opera.com
    
    Review URL: https://codereview.chromium.org/754963009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186964 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/track/InbandTextTrack.h
+++ third_party/WebKit/Source/core/html/track/InbandTextTrack.h
@@ -44,7 +44,6 @@ public:
     static PassRefPtrWillBeRawPtr<InbandTextTrack> create(blink::WebInbandTextTrack*);
     virtual ~InbandTextTrack();
 
-    size_t inbandTrackIndex();
     virtual void setTrackList(TextTrackList*) override;
 
 private:
