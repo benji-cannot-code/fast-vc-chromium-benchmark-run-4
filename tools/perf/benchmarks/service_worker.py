2af22d433a96bf01b7e9ed8f0931eb39b325912b
miletus <miletus@chromium.org>
2014-12-11 09:55:56 -0800

    [telemetry] Make thread_times measurment dump its trace value to results
    So that when --upload-results option is specified, the trace log can
    be uploaded to cloud.
    
    BUG=440982
    TEST=run thread_times.key_silk_cases with --upload-results and check that
    the trace log is uploaded to cloud.
    
    Review URL: https://codereview.chromium.org/792213002
    
    Cr-Commit-Position: refs/heads/master@{#307925}

--- tools/perf/benchmarks/service_worker.py
+++ tools/perf/benchmarks/service_worker.py
@@ -104,7 +104,7 @@ class _ServiceWorkerMeasurement(page_test.PageTest):
 
   def ValidateAndMeasurePage(self, page, tab, results):
     tab.WaitForDocumentReadyStateToBeComplete(40)
-    self._timeline_controller.Stop(tab)
+    self._timeline_controller.Stop(tab, results)
 
     # Retrieve TRACE_EVENTs
     timeline_metric = _ServiceWorkerTimelineMetric()
@@ -154,7 +154,7 @@ class _ServiceWorkerMicroBenchmarkMeasurement(page_test.PageTest):
 
   def ValidateAndMeasurePage(self, page, tab, results):
     tab.WaitForJavaScriptExpression('window.done', 40)
-    self._timeline_controller.Stop(tab)
+    self._timeline_controller.Stop(tab, results)
 
     # Measure JavaScript-land
     json = tab.EvaluateJavaScript('window.results || {}')
