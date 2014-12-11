adaea9eecd95cfc90f4bed271416105517a18b8b
dcheng@chromium.org <dcheng@chromium.org>
2014-12-11 20:11:38 +0000

    Move ScriptController::clearForClose() earlier in the detach sequence.
    Chrome extensions run script in response to willReleaseScriptContext,
    and it's weird to run script in a frame that's already partially
    detached.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/792383002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186979 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -2151,7 +2151,6 @@ void Document::attach(const AttachContext& context)
 
 void Document::detach(const AttachContext& context)
 {
-    ScriptForbiddenScope forbidScript;
     ASSERT(isActive());
     m_lifecycle.advanceTo(DocumentLifecycle::Stopping);
 
--- third_party/WebKit/Source/core/frame/LocalFrame.cpp
+++ third_party/WebKit/Source/core/frame/LocalFrame.cpp
@@ -65,6 +65,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/svg/SVGDocumentExtensions.h"
 #include "platform/DragImage.h"
 #include "platform/RuntimeEnabledFeatures.h"
+#include "platform/ScriptForbiddenScope.h"
 #include "platform/graphics/GraphicsContext.h"
 #include "platform/graphics/ImageBuffer.h"
 #include "platform/text/TextStream.h"
@@ -267,11 +268,12 @@ void LocalFrame::detach()
     if (!client())
         return;
     m_loader.detach();
-    setView(nullptr);
-    willDetachFrameHost();
     // Notify ScriptController that the frame is closing, since its cleanup ends up calling
     // back to FrameLoaderClient via WindowProxy.
     script().clearForClose();
+    ScriptForbiddenScope forbidScript;
+    setView(nullptr);
+    willDetachFrameHost();
     InspectorInstrumentation::frameDetachedFromParent(this);
     Frame::detach();
 #if ENABLE(OILPAN)
