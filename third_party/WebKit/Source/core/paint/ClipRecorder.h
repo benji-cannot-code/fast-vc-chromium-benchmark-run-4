8dc3ada21a4f1c0fe6a36d020f5972816d46f03a
pdr@chromium.org <pdr@chromium.org>
2014-12-11 02:06:10 +0000

    Cleanup ClipRecorder for slimming paint
    This is a simple cleanup patch for some crufty ClipRecorder code. I
    found these issues while working on a float clip recorder for SVG.
    
    Review URL: https://codereview.chromium.org/793103002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186901 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/paint/ClipRecorder.h
+++ third_party/WebKit/Source/core/paint/ClipRecorder.h
@@ -12,20 +12,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class RenderLayer;
-class RoundedRect;
-struct PaintInfo;
-
 class ClipRecorder {
 public:
-    ClipRecorder(RenderLayerModelObject&, const PaintInfo&, const LayoutRect& clipRect);
+    ClipRecorder(RenderLayerModelObject&, const PaintInfo&, const LayoutRect&);
     ~ClipRecorder();
 
     static DisplayItem::Type paintPhaseToClipType(PaintPhase);
+
 private:
-    LayoutRect m_clipRect;
     const PaintInfo& m_paintInfo;
-    RenderLayerModelObject& m_canvas;
+    RenderLayerModelObject& m_renderer;
 };
 
 } // namespace blink
