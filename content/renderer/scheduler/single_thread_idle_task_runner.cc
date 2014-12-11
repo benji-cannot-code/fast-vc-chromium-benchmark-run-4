92671ac8d2111eebc1bfee2af04e547ce405916d
rmcilroy <rmcilroy@chromium.org>
2014-12-11 05:23:14 -0800

    content: Add tracing to SingleThreadIdleTaskRunner
    Add tracing of idle task execution, allotted time and whether it overran its deadline.
    
    BUG=391005
    
    Review URL: https://codereview.chromium.org/712923003
    
    Cr-Commit-Position: refs/heads/master@{#307891}

--- content/renderer/scheduler/single_thread_idle_task_runner.cc
+++ content/renderer/scheduler/single_thread_idle_task_runner.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/renderer/scheduler/single_thread_idle_task_runner.h"
 
+#include "base/debug/trace_event.h"
 #include "base/location.h"
 
 namespace content {
@@ -33,7 +34,18 @@ void SingleThreadIdleTaskRunner::PostIdleTask(
 void SingleThreadIdleTaskRunner::RunTask(IdleTask idle_task) {
   base::TimeTicks deadline;
   deadline_supplier_.Run(&deadline);
+  TRACE_EVENT1(TRACE_DISABLED_BY_DEFAULT("renderer.scheduler"),
+               "SingleThreadIdleTaskRunner::RunTask", "allotted_time_ms",
+               (deadline - base::TimeTicks::Now()).InMillisecondsF());
   idle_task.Run(deadline);
+  bool is_tracing;
+  TRACE_EVENT_CATEGORY_GROUP_ENABLED(
+      TRACE_DISABLED_BY_DEFAULT("renderer.scheduler"), &is_tracing);
+  if (is_tracing && base::TimeTicks::Now() > deadline) {
+    TRACE_EVENT_INSTANT0(TRACE_DISABLED_BY_DEFAULT("renderer.scheduler"),
+                         "SingleThreadIdleTaskRunner::DidOverrunDeadline",
+                         TRACE_EVENT_SCOPE_THREAD);
+  }
 }
 
 }  // namespace content
