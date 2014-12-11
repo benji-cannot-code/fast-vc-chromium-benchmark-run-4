50b9d7360dcca3abe5bc4f622b765dae01250e37
maxbogue <maxbogue@chromium.org>
2014-12-10 18:53:08 -0800

    Add boolean histogram Sync.Attachments.DownloadChecksumResult.
    BUG=436134
    
    Review URL: https://codereview.chromium.org/771323003
    
    Cr-Commit-Position: refs/heads/master@{#307845}

--- sync/internal_api/attachments/attachment_downloader_impl.cc
+++ sync/internal_api/attachments/attachment_downloader_impl.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base64.h"
 #include "base/bind.h"
 #include "base/message_loop/message_loop.h"
+#include "base/metrics/histogram.h"
 #include "base/metrics/sparse_histogram.h"
 #include "base/sys_byteorder.h"
 #include "net/base/load_flags.h"
@@ -168,6 +169,8 @@ void AttachmentDownloaderImpl::OnURLFetchComplete(
     } else {
       result = DOWNLOAD_SUCCESS;
     }
+    UMA_HISTOGRAM_BOOLEAN("Sync.Attachments.DownloadChecksumResult",
+                          result == DOWNLOAD_SUCCESS);
   } else if (response_code == net::HTTP_UNAUTHORIZED) {
     // Server tells us we've got a bad token so invalidate it.
     OAuth2TokenServiceRequest::InvalidateToken(token_service_provider_.get(),
