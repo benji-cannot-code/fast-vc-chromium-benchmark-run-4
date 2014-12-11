44ed88eb42c51e9c994a18b9be891da3b7315a3c
sigbjornf@opera.com <sigbjornf@opera.com>
2014-12-11 10:31:46 +0000

    Oilpan: move InspectorOverlay to the heap.
    Thereby avoiding its Persistent references.
    
    R=haraken
    BUG=340522
    
    Review URL: https://codereview.chromium.org/783923005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186935 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorController.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorController.cpp
@@ -138,6 +138,7 @@ void InspectorController::trace(Visitor* visitor)
     visitor->trace(m_instrumentingAgents);
     visitor->trace(m_injectedScriptManager);
     visitor->trace(m_state);
+    visitor->trace(m_overlay);
     visitor->trace(m_domAgent);
     visitor->trace(m_animationAgent);
     visitor->trace(m_pageAgent);
@@ -289,7 +290,7 @@ void InspectorController::disconnectFrontend()
 
     m_inspectorFrontend.clear();
 
-    // relese overlay page resources
+    // Release overlay page resources
     m_overlay->freePage();
     InspectorInstrumentation::frontendDeleted();
     InspectorInstrumentation::unregisterInstrumentingAgents(m_instrumentingAgents.get());
