b8ed71d6f27d6d52640252d5ac804986781087fe
wangxianzhu@chromium.org <wangxianzhu@chromium.org>
2014-12-11 01:00:45 +0000

    Cache and reuse DrawingDisplayItems
    - Combine m_paintListRenderers and m_invalidated hash sets in
      DisplayItemList into m_cachedClients because we only need to know
      which clients have display items validly cached.
    
    - m_cachedClients is filled during updatePaintList(). Client is removed
      when the client it's invalidated.
    
    - Clear the whole DisplayItemList when a GraphicsLayer is fully
      invalidated. This happens when the compositing state of layer changes
      and all renderers in the layer should be invalidated, we invalidate
      the GraphicsLayer directly instead of invalidating each renderer.
    
    - Create CachedDisplayItem instead of DrawingDisplayItem during recording
      if a renderer is validly cached.
    
    TEST=ViewDisplayListTest.CachedDisplayItems
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=186817
    
    Review URL: https://codereview.chromium.org/762603003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186894 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/paint/DisplayItem.h
+++ third_party/WebKit/Source/platform/graphics/paint/DisplayItem.h
@@ -104,7 +104,9 @@ private:
         Id(DisplayItemClient c, Type t)
             : client(c)
             , type(t)
-        { }
+        {
+            ASSERT(client);
+        }
 
         const DisplayItemClient client;
         const Type type;
--- third_party/WebKit/Source/platform/graphics/paint/DrawingRecorder.cpp
+++ third_party/WebKit/Source/platform/graphics/paint/DrawingRecorder.cpp
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/RuntimeEnabledFeatures.h"
 #include "platform/graphics/GraphicsContext.h"
 #include "platform/graphics/GraphicsLayer.h"
+#include "platform/graphics/paint/CachedDisplayItem.h"
 #include "platform/graphics/paint/DisplayItemList.h"
 #include "platform/graphics/paint/DrawingDisplayItem.h"
 #include "third_party/skia/include/core/SkPicture.h"
@@ -24,6 +25,7 @@ DrawingRecorder::DrawingRecorder(GraphicsContext* context, const DisplayItemClie
     , m_displayItemClient(displayItemClient)
     , m_displayItemType(displayItemType)
     , m_bounds(bounds)
+    , m_canUseCachedDrawing(false)
 {
     if (!RuntimeEnabledFeatures::slimmingPaintEnabled())
         return;
@@ -33,6 +35,9 @@ DrawingRecorder::DrawingRecorder(GraphicsContext* context, const DisplayItemClie
     s_inDrawingRecorder = true;
 #endif
 
+    m_canUseCachedDrawing = context->displayItemList()->clientCacheIsValid(displayItemClient);
+    // FIXME: beginRecording only if !m_canUseCachedDrawing or ENABLE(ASSERT)
+    // after all painters call canUseCachedDrawing().
     m_context->beginRecording(bounds);
 }
 
@@ -45,16 +50,26 @@ DrawingRecorder::~DrawingRecorder()
     s_inDrawingRecorder = false;
 #endif
 
-    RefPtr<const SkPicture> picture = m_context->endRecording();
-    if (!picture || !picture->approximateOpCount())
-        return;
-    OwnPtr<DrawingDisplayItem> drawingItem = DrawingDisplayItem::create(m_displayItemClient, m_displayItemType, picture);
+    OwnPtr<DisplayItem> displayItem;
+
+    if (m_canUseCachedDrawing) {
+        // FIXME: endRecording only if ENABLE(ASSERT), and enable the following ASSERT
+        // after all painters call canUseCachedDrawing().
+        RefPtr<const SkPicture> picture = m_context->endRecording();
+        // ASSERT(!picture || !picture->approximateOpCount());
+        displayItem = CachedDisplayItem::create(m_displayItemClient, m_displayItemType);
+    } else {
+        RefPtr<const SkPicture> picture = m_context->endRecording();
+        if (!picture || !picture->approximateOpCount())
+            return;
+        displayItem = DrawingDisplayItem::create(m_displayItemClient, m_displayItemType, picture);
+    }
+
 #ifndef NDEBUG
-    drawingItem->setClientDebugString(m_clientDebugString);
+    displayItem->setClientDebugString(m_clientDebugString);
 #endif
 
-    ASSERT(m_context->displayItemList());
-    m_context->displayItemList()->add(drawingItem.release());
+    m_context->displayItemList()->add(displayItem.release());
 }
 
 #ifndef NDEBUG
