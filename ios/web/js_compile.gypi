a31e2f6afaffe53ea4f2bcf3e31f3fcbbe702642
droger <droger@chromium.org>
2014-12-11 04:56:13 -0800

    Upstream injected JavaScript for the iOS web layer
    Review URL: https://codereview.chromium.org/788253002
    
    Cr-Commit-Position: refs/heads/master@{#307890}

---
+++ ios/web/js_compile.gypi
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+{
+  # TODO(ios): Enable javascript compilation. See http://crbug.com/429756
+  'rules': [
+    {
+      'rule_name': 'jscompilation',
+      'extension': 'js',
+      'outputs': [
+        '<(SHARED_INTERMEDIATE_DIR)/<(RULE_INPUT_NAME)',
+      ],
+      'action': [
+        'cp',
+        '<(RULE_INPUT_PATH)',
+        '<@(_outputs)',
+      ],
+    },
+  ],  # rule_name: jscompilation
+}
---
+++ ios/web/web_state/js/resources/base.js
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This file adheres to closure-compiler conventions in order to enable
+// compilation with ADVANCED_OPTIMIZATIONS. In particular, members that are to
+// be accessed externally should be specified in this['style'] as opposed to
+// this.style because member identifiers are minified by default.
+// See http://goo.gl/FwOgy
+
+// This object is checked on the main app to know when to inject (or not).
+window['__gCrWeb'] = {};
+
