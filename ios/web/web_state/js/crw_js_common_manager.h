4a6874eec5cde057fa3013802ed8055d0e93156e
droger <droger@chromium.org>
2014-12-12 06:37:42 -0800

    Upstream JavaScript injection for iOS
    Review URL: https://codereview.chromium.org/790803002
    
    Cr-Commit-Position: refs/heads/master@{#308072}

---
+++ ios/web/web_state/js/crw_js_common_manager.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_WEB_WEB_STATE_JS_CRW_JS_COMMON_MANAGER_H_
+#define IOS_WEB_WEB_STATE_JS_CRW_JS_COMMON_MANAGER_H_
+
+#import "ios/web/public/web_state/js/crw_js_injection_manager.h"
+
+@class CRWJSInjectionReceiver;
+
+// Loads JavaScript file common.js which contains common JavaScript methods that
+// can be called by other CRWJSInjectionManagers.
+@interface CRWJSCommonManager : CRWJSInjectionManager
+@end
+
+#endif  // IOS_WEB_WEB_STATE_JS_CRW_JS_COMMON_MANAGER_H_
