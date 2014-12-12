1147ddd4ccafe2721a3a53d2d23a53942b0d6b8c
sagarmittal <sagarmittal@google.com>
2014-12-11 16:20:47 -0800

    IDB: Remove unused variable IndexedDBInfo::path_
    R=cmumford@chromium.org
    
    Review URL: https://codereview.chromium.org/783393002
    
    Cr-Commit-Position: refs/heads/master@{#308007}

--- chrome/browser/ui/cocoa/content_settings/cookie_details_unittest.mm
+++ chrome/browser/ui/cocoa/content_settings/cookie_details_unittest.mm
@@ -146,11 +146,9 @@ TEST_F(CookiesDetailsTest, CreateForTreeIndexedDB) {
   GURL origin("http://moose.org/");
   int64 size = 1234;
   base::Time last_modified = base::Time::Now();
-  base::FilePath file_path;
   content::IndexedDBInfo info(origin,
                               size,
                               last_modified,
-                              file_path,
                               0);
 
   details.reset([[CocoaCookieDetails alloc] initWithIndexedDBInfo:&info]);
