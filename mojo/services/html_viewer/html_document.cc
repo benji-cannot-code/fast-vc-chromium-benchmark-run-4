122ab1506f711b1c5a09c95dc216c32fd587e929
xhwang <xhwang@chromium.org>
2014-12-12 06:36:15 -0800

    Move Load() from HTMLDocument::OnEmbed() to the constructor.
    TBR=aa@chromium.org
    
    Review URL: https://codereview.chromium.org/796963003
    
    Cr-Commit-Position: refs/heads/master@{#308070}

--- mojo/services/html_viewer/html_document.cc
+++ mojo/services/html_viewer/html_document.cc
@@ -102,6 +102,7 @@ HTMLDocument::HTMLDocument(
   exported_services_.AddService(this);
   exported_services_.AddService(&view_manager_client_factory_);
   WeakBindToPipe(&exported_services_, provider.PassMessagePipe());
+  Load(response_.Pass());
 }
 
 HTMLDocument::~HTMLDocument() {
@@ -121,8 +122,6 @@ void HTMLDocument::OnEmbed(
   root_ = root;
   embedder_service_provider_ = embedder_service_provider.Pass();
   navigator_host_.set_service_provider(embedder_service_provider_.get());
-
-  Load(response_.Pass());
 
   blink::WebSize root_size(root_->bounds().width, root_->bounds().height);
   web_view_->resize(root_size);
