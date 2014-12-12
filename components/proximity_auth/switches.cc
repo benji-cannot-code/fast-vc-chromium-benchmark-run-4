d6c154dcbb5c7955ecdb33d660e6bff619123e52
xiyuan <xiyuan@chromium.org>
2014-12-11 18:14:28 -0800

    easy-unlock: Get rid of extra flag.
    BUG=439638
    
    Review URL: https://codereview.chromium.org/797693002
    
    Cr-Commit-Position: refs/heads/master@{#308027}

--- components/proximity_auth/switches.cc
+++ components/proximity_auth/switches.cc
@@ -12,9 +12,6 @@ namespace switches {
 // by CryptAuth.
 const char kCryptAuthHTTPHost[] = "cryptauth-http-host";
 
-// Disable Easy sign-in.
-const char kDisableEasySignin[] = "disable-easy-signin";
-
 // Disable Easy unlock.
 const char kDisableEasyUnlock[] = "disable-easy-unlock";
 
