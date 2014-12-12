068fcc20fec12855d8a97ced63a52b0a7d4ed167
rvargas <rvargas@chromium.org>
2014-12-12 14:42:03 -0800

    Update browser/media to use the new version of LaunchProcess.
    BUG=417532
    
    Review URL: https://codereview.chromium.org/789023002
    
    Cr-Commit-Position: refs/heads/master@{#308185}

--- chrome/browser/media/test_license_server.h
+++ chrome/browser/media/test_license_server.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 #include "base/memory/scoped_ptr.h"
-#include "base/process/process_handle.h"
+#include "base/process/process.h"
 
 class TestLicenseServerConfig;
 
@@ -28,8 +28,8 @@ class TestLicenseServer {
  private:
   // License server configuration class used to obtain server paths, etc.
   scoped_ptr<TestLicenseServerConfig> server_config_;
-  // Process handle for the license server.
-  base::ProcessHandle license_server_process_;
+  // Process for the license server.
+  base::Process license_server_process_;
 
   DISALLOW_COPY_AND_ASSIGN(TestLicenseServer);
 };
