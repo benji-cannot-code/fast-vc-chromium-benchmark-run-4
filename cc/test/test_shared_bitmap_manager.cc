1aea081aa8b95e76db4da1db0146103af51e2a4b
jbauman <jbauman@chromium.org>
2014-12-11 16:59:42 -0800

    Remove GetBitmapForSharedMemory from cc::SharedBitmapManager
    This is only used in specific circumstances in the renderer, so move it to ChildSharedBitmapManager.
    
    Review URL: https://codereview.chromium.org/795843002
    
    Cr-Commit-Position: refs/heads/master@{#308014}

--- cc/test/test_shared_bitmap_manager.cc
+++ cc/test/test_shared_bitmap_manager.cc
@@ -64,12 +64,4 @@ scoped_ptr<SharedBitmap> TestSharedBitmapManager::GetSharedBitmapFromId(
   return make_scoped_ptr(new UnownedSharedBitmap(bitmap_map_[id], id));
 }
 
-scoped_ptr<SharedBitmap> TestSharedBitmapManager::GetBitmapForSharedMemory(
-    base::SharedMemory* memory) {
-  base::AutoLock lock(lock_);
-  SharedBitmapId id = SharedBitmap::GenerateId();
-  bitmap_map_[id] = memory;
-  return make_scoped_ptr(new UnownedSharedBitmap(memory, id));
-}
-
 }  // namespace cc
