b3a527c1d1b8dd579f67d444a11664ab0906d7aa
dcheng@chromium.org <dcheng@chromium.org>
2014-12-12 11:24:55 +0000

    Adapt WindowProxy to handle both local and remote frames.
    BUG=425623
    
    Review URL: https://codereview.chromium.org/796603002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187013 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/core/v8/WindowProxy.h
+++ third_party/WebKit/Source/bindings/core/v8/WindowProxy.h
@@ -45,15 +45,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class LocalFrame;
+class Frame;
 class HTMLDocument;
 class SecurityOrigin;
 
-// WindowProxy represents all the per-global object state for a LocalFrame that
+// WindowProxy represents all the per-global object state for a Frame that
 // persist between navigations.
 class WindowProxy final : public NoBaseWillBeGarbageCollectedFinalized<WindowProxy> {
 public:
-    static PassOwnPtrWillBeRawPtr<WindowProxy> create(LocalFrame*, DOMWrapperWorld&, v8::Isolate*);
+    static PassOwnPtrWillBeRawPtr<WindowProxy> create(Frame*, DOMWrapperWorld&, v8::Isolate*);
 
     ~WindowProxy();
     void trace(Visitor*);
@@ -83,7 +83,7 @@ public:
     DOMWrapperWorld& world() { return *m_world; }
 
 private:
-    WindowProxy(LocalFrame*, PassRefPtr<DOMWrapperWorld>, v8::Isolate*);
+    WindowProxy(Frame*, PassRefPtr<DOMWrapperWorld>, v8::Isolate*);
     bool initialize();
 
     enum GlobalDetachmentBehavior {
@@ -107,7 +107,7 @@ private:
     void createContext();
     bool installDOMWindow();
 
-    RawPtrWillBeMember<LocalFrame> m_frame;
+    RawPtrWillBeMember<Frame> m_frame;
     v8::Isolate* m_isolate;
     RefPtr<ScriptState> m_scriptState;
     RefPtr<DOMWrapperWorld> m_world;
