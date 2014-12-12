d97b0898aa92a06374b174922a79e96a82e9b78a
rnephew <rnephew@chromium.org>
2014-12-12 11:08:55 -0800

    Add AMP support to test runner.
    BUG=
    
    Review URL: https://codereview.chromium.org/745793002
    
    Cr-Commit-Position: refs/heads/master@{#308139}

--- build/android/pylib/base/environment_factory.py
+++ build/android/pylib/base/environment_factory.py
@@ -3,10 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from pylib.remote.device import remote_device_environment
 
-def CreateEnvironment(_args, error_func):
+def CreateEnvironment(args, error_func):
 
   # TODO(jbudorick) Add local device environment.
   # TODO(jbudorick) Add local machine environment.
-  error_func('No environments currently supported.')
 
+  if args.environment == 'remote_device':
+    return remote_device_environment.RemoteDeviceEnvironment(args,
+                                                             error_func)
+  error_func('Unable to create %s environment.' % args.environment)
--- build/android/pylib/base/test_instance_factory.py
+++ build/android/pylib/base/test_instance_factory.py
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 from pylib import constants
 from pylib.gtest import gtest_test_instance
 from pylib.utils import isolator
+from pylib.uirobot import uirobot_test_instance
+
 
 
 def CreateTestInstance(args, error_func):
@@ -13,7 +15,8 @@ def CreateTestInstance(args, error_func):
   if args.command == 'gtest':
     return gtest_test_instance.GtestTestInstance(
         args, isolator.Isolator(constants.ISOLATE_DEPS_DIR))
+  if args.command == 'uirobot':
+    return uirobot_test_instance.UirobotTestInstance(args)
   # TODO(jbudorick) Add instrumentation test instance.
 
   error_func('Unable to create %s test instance.' % args.command)
-
