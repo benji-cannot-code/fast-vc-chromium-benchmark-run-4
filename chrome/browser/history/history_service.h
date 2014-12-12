1344c14bb12589ac5a0f10434b38126bb998d600
paritosh.in <paritosh.in@samsung.com>
2014-12-12 10:54:40 -0800

    Removing HistoryService::profile() method
    As per review comments on the issue
    https://codereview.chromium.org/573553004/#msg3
    removing HistoryService::profile() method.
    
    BUG=430070
    
    Review URL: https://codereview.chromium.org/783963003
    
    Cr-Commit-Position: refs/heads/master@{#308132}

--- chrome/browser/history/history_service.h
+++ chrome/browser/history/history_service.h
@@ -54,10 +54,6 @@ class FilePath;
 class Thread;
 }
 
-namespace safe_browsing {
-class LastDownloadFinder;
-}
-
 namespace visitedlink {
 class VisitedLinkMaster;
 }
@@ -544,7 +540,6 @@ class HistoryService : public content::NotificationObserver,
   friend class HistoryURLProviderTest;
   friend class history::InMemoryURLIndexTest;
   template<typename Info, typename Callback> friend class DownloadRequest;
-  friend class safe_browsing::LastDownloadFinder;
   friend class PageUsageRequest;
   friend class RedirectRequest;
   friend class SyncBookmarkDataTypeControllerTest;
@@ -858,9 +853,6 @@ class HistoryService : public content::NotificationObserver,
     ScheduleTask(priority, base::Bind(func, history_backend_.get(),
                                       a, b, c, d, e));
   }
-
-  // TODO(sdefresne): http://crbug.com/430070 remove this method.
-  Profile* profile() { return profile_; }
 
   base::ThreadChecker thread_checker_;
 
