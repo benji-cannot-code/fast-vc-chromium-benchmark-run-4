c71764f879a66c4a4c6a437b8f49b06e0fd8540e
rtenneti <rtenneti@chromium.org>
2014-12-12 19:27:28 -0800

    Add UMA Histogram Manager to Cronet.
    Part of change to collect, upload and expose histogram data
    for Cronet.
    
    BUG=441466
    
    Review URL: https://codereview.chromium.org/794493002
    
    Cr-Commit-Position: refs/heads/master@{#308239}

---
+++ components/cronet/android/histogram_manager.cc
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/cronet/android/histogram_manager.h"
+
+#include <string>
+#include <vector>
+
+#include "base/android/jni_array.h"
+#include "components/metrics/histogram_manager.h"
+
+#include "jni/HistogramManager_jni.h"
+
+namespace cronet {
+
+// Explicitly register static JNI functions.
+bool HistogramManagerRegisterJni(JNIEnv* env) {
+  return RegisterNativesImpl(env);
+}
+
+static jbyteArray GetHistogramDeltas(JNIEnv* env, jobject jcaller) {
+  std::vector<uint8> data;
+  if (!metrics::HistogramManager::GetInstance()->GetDeltas(&data))
+    return NULL;
+  return base::android::ToJavaByteArray(env, &data[0], data.size()).Release();
+}
+
+}  // namespace cronet
