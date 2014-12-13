da66351c7b2789ae05d74dbc22469f4beff04cef
cmumford <cmumford@chromium.org>
2014-12-13 00:17:32 -0800

    LevelDB: Removed global prefix from base namespace: "::base" -> "base"
    Review URL: https://codereview.chromium.org/800783003
    
    Cr-Commit-Position: refs/heads/master@{#308265}

--- third_party/leveldatabase/env_chromium.h
+++ third_party/leveldatabase/env_chromium.h
@@ -140,7 +140,7 @@ class ChromiumEnv : public leveldb::Env,
   bool make_backup_;
 
  private:
-  static const char* FileErrorString(::base::File::Error error);
+  static const char* FileErrorString(base::File::Error error);
 
   virtual void DidCreateNewFile(const std::string& fname);
   virtual bool DoesDirNeedSync(const std::string& fname);
@@ -194,8 +194,8 @@ class ChromiumEnv : public leveldb::Env,
 
   base::FilePath test_directory_;
 
-  ::base::Lock mu_;
-  ::base::ConditionVariable bgsignal_;
+  base::Lock mu_;
+  base::ConditionVariable bgsignal_;
   bool started_bgthread_;
 
   // Entry per Schedule() call
