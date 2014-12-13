2bbbf9556f49b2de25964224716e56a86fd30238
pdr@chromium.org <pdr@chromium.org>
2014-12-13 11:56:17 +0000

    Factor out SVG text fragment painting
    This patch factors out SVG text fragment painting into a helper
    function and cleans up some old code in SVGInlineTextBoxPainter. This
    patch has been split off from https://codereview.chromium.org/770173004.
    
    Review URL: https://codereview.chromium.org/798823002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187095 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/svg/SVGInlineTextBox.cpp
+++ third_party/WebKit/Source/core/rendering/svg/SVGInlineTextBox.cpp
@@ -171,7 +171,6 @@ LayoutRect SVGInlineTextBox::localSelectionRect(int startPosition, int endPositi
 
 void SVGInlineTextBox::paint(const PaintInfo& paintInfo, const LayoutPoint& paintOffset, LayoutUnit, LayoutUnit)
 {
-
     SVGInlineTextBoxPainter(*this).paint(paintInfo, paintOffset);
 }
 
