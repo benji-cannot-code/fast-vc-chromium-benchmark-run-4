d13a9756f2cda995c87813eae12e810dd864783f
dnicoara <dnicoara@chromium.org>
2014-12-12 20:28:56 -0800

    Remove synchronous GetDisplays() and Configure() from NativeDisplayDelegate
    Cleaning up since the entry points should not be used/needed anymore.
    Also removing the Configure() calls from TakeControl() and replace them
    with a call to probe and configure displays.
    
    BUG=429746
    
    Review URL: https://codereview.chromium.org/796263002
    
    Cr-Commit-Position: refs/heads/master@{#308250}

--- ui/ozone/platform/dri/native_display_delegate_dri.h
+++ ui/ozone/platform/dri/native_display_delegate_dri.h
@@ -28,6 +28,11 @@ class NativeDisplayDelegateDri : public NativeDisplayDelegate {
                                      bool is_interlaced,
                                      float refresh_rate);
 
+  std::vector<DisplaySnapshot*> GetDisplays();
+  bool Configure(const DisplaySnapshot& output,
+                 const DisplayMode* mode,
+                 const gfx::Point& origin);
+
   // NativeDisplayDelegate overrides:
   void Initialize() override;
   void GrabServer() override;
@@ -37,12 +42,8 @@ class NativeDisplayDelegateDri : public NativeDisplayDelegate {
   void SyncWithServer() override;
   void SetBackgroundColor(uint32_t color_argb) override;
   void ForceDPMSOn() override;
-  std::vector<DisplaySnapshot*> GetDisplays() override;
   void GetDisplays(const GetDisplaysCallback& callback) override;
   void AddMode(const DisplaySnapshot& output, const DisplayMode* mode) override;
-  bool Configure(const DisplaySnapshot& output,
-                 const DisplayMode* mode,
-                 const gfx::Point& origin) override;
   void Configure(const DisplaySnapshot& output,
                  const DisplayMode* mode,
                  const gfx::Point& origin,
