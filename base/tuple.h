83181757648520a2ff2372a3e0f59ea78d4a747f
mdempsky <mdempsky@chromium.org>
2014-12-15 15:26:13 -0800

    tuple: leave fields uninitialized in default constructors
    Using "x()" in TupleLeaf's default constructor causes primitive types
    (e.g., integers and pointers) to be zero initialized, whereas
    previously Tuple left them indeterminate.  Arguably zero-initializing
    is better, but the change was inadvertant and results in a measurable
    code size increase, so this CL reverts it (at least for now).
    
    BUG=440806
    
    Review URL: https://codereview.chromium.org/791883003
    
    Cr-Commit-Position: refs/heads/master@{#308455}

--- base/tuple.h
+++ base/tuple.h
@@ -116,7 +116,7 @@ struct TupleBaseImpl<IndexSequence<Ns...>, Ts...> : TupleLeaf<Ns, Ts>... {
 
 template <size_t N, typename T>
 struct TupleLeaf {
-  TupleLeaf() : x() {}
+  TupleLeaf() {}
   explicit TupleLeaf(typename TupleTraits<T>::ParamType x) : x(x) {}
 
   T& get() { return x; }
@@ -131,7 +131,7 @@ struct TupleLeaf {
 #define DEFINE_TUPLE_LEAF(N, x)                                        \
   template <typename T>                                                \
   struct TupleLeaf<N, T> {                                             \
-    TupleLeaf() : x() {}                                               \
+    TupleLeaf() {}                                                     \
     explicit TupleLeaf(typename TupleTraits<T>::ParamType x) : x(x) {} \
                                                                        \
     T& get() { return x; }                                             \
