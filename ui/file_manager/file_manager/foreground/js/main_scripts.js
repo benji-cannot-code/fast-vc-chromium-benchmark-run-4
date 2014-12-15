b79dd5163e27f49f667cf03f351f0fad417a9d5f
smckay <smckay@chromium.org>
2014-12-15 13:07:02 -0800

    Include MTP devices in Cloud Import happiness. Nuke "VolumeManager.isType". Add test coverage of importer_common methods. Make all "DCIM" dir-name matching case-insensitive.
    BUG=420680
    TEST=browser_test: FileManagerJsTest.*
    
    Review URL: https://codereview.chromium.org/778123006
    
    Cr-Commit-Position: refs/heads/master@{#308414}

--- ui/file_manager/file_manager/foreground/js/main_scripts.js
+++ ui/file_manager/file_manager/foreground/js/main_scripts.js
@@ -75,10 +75,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 //<include src="../../common/js/async_util.js">
 //<include src="../../common/js/file_type.js">
-//<include src="../../common/js/importer_common.js">
 //<include src="../../common/js/volume_manager_common.js">
 //<include src="../../common/js/util.js">
 //<include src="../../common/js/progress_center_common.js">
+//<include src="../../common/js/importer_common.js">
 //
 //<include src="ui/combobutton.js">
 //<include src="ui/commandbutton.js">
