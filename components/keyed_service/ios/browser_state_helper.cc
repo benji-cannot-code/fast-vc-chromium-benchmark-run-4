cfdff854a063f799847b8414eaad68e4d8b92bb6
sdefresne <sdefresne@chromium.org>
2014-12-15 00:10:43 -0800

    Fix global variable name to follow style guide
    BUG=None
    
    Review URL: https://codereview.chromium.org/799073007
    
    Cr-Commit-Position: refs/heads/master@{#308325}

--- components/keyed_service/ios/browser_state_helper.cc
+++ components/keyed_service/ios/browser_state_helper.cc
@@ -32,19 +32,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // to find the associated web::BrowserState (there is a 1:1 mapping).
 
 namespace {
-BrowserStateFromContextFn gBrowserStateFromContext = nullptr;
+BrowserStateFromContextFn g_browser_state_from_context = nullptr;
 }  // namespace
 
 void SetBrowserStateFromContextHelper(BrowserStateFromContextFn helper) {
-  gBrowserStateFromContext = helper;
+  g_browser_state_from_context = helper;
 }
 
 web::BrowserState* BrowserStateFromContext(base::SupportsUserData* context) {
   web::BrowserState* state = nullptr;
   if (context) {
     state = web::BrowserState::FromSupportsUserData(context);
-    if (!state && gBrowserStateFromContext)
-      state = gBrowserStateFromContext(context);
+    if (!state && g_browser_state_from_context)
+      state = g_browser_state_from_context(context);
     DCHECK(state) << "cannot convert context to web::BrowserState";
   }
   return state;
