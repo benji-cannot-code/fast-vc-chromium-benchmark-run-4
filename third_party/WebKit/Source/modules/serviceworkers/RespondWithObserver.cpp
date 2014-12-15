c558f9a33e13468b64a2139ed4f1ebd17664f31e
horo@chromium.org <horo@chromium.org>
2014-12-15 12:55:13 +0000

    [ServiceWorker] Send the body of the response using Stream from RespondWithObserver.
    In current implementation we send the response body from the ServiceWorker to the browser process using the blob handle.
    But to support progressive loading, BodyStreamBuffer will be used as the body data of Response objects which are created in FetchManager.
    
    RespondWithObserver::respondWith() receives a promise.
    And when the promise is fulfilled with the the response which body is stream.
     - Create a blink::Stream object.
     - Set WebServiceWorkerResponse's StreamURL.
     - Send the body of the response to the browser process progressively using the blink::Stream.
    
    This change depends on https://codereview.chromium.org/786893004/.
    
    BUG=436424
    
    Review URL: https://codereview.chromium.org/754083005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187140 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/serviceworkers/RespondWithObserver.cpp
+++ third_party/WebKit/Source/modules/serviceworkers/RespondWithObserver.cpp
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/V8Binding.h"
 #include "bindings/modules/v8/V8Response.h"
 #include "core/dom/ExecutionContext.h"
+#include "core/streams/Stream.h"
+#include "modules/serviceworkers/BodyStreamBuffer.h"
 #include "modules/serviceworkers/ServiceWorkerGlobalScopeClient.h"
 #include "platform/RuntimeEnabledFeatures.h"
 #include "public/platform/WebServiceWorkerResponse.h"
@@ -20,6 +22,67 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <v8.h>
 
 namespace blink {
+namespace {
+
+class StreamUploader : public BodyStreamBuffer::Observer {
+public:
+    StreamUploader(BodyStreamBuffer* buffer, PassRefPtrWillBeRawPtr<Stream> outStream)
+        : m_buffer(buffer), m_outStream(outStream)
+    {
+    }
+    ~StreamUploader() override { }
+    void onWrite() override
+    {
+        bool needToFlush = false;
+        while (RefPtr<DOMArrayBuffer> buf = m_buffer->read()) {
+            needToFlush = true;
+            m_outStream->addData(static_cast<const char*>(buf->data()), buf->byteLength());
+        }
+        if (needToFlush)
+            m_outStream->flush();
+    }
+    void onClose() override
+    {
+        m_outStream->finalize();
+        cleanup();
+    }
+    void onError() override
+    {
+        // If the stream is aborted soon after the stream is registered to the
+        // StreamRegistry, ServiceWorkerURLRequestJob may not notice the error
+        // and continue waiting forever.
+        // FIXME: Add new message to report the error to the browser process.
+        m_outStream->abort();
+        cleanup();
+    }
+    void trace(Visitor* visitor)
+    {
+        BodyStreamBuffer::Observer::trace(visitor);
+        visitor->trace(m_buffer);
+        visitor->trace(m_outStream);
+    }
+    void start()
+    {
+        m_buffer->registerObserver(this);
+        onWrite();
+        if (m_buffer->hasError())
+            return onError();
+        if (m_buffer->isClosed())
+            return onClose();
+    }
+
+private:
+    void cleanup()
+    {
+        m_buffer->unregisterObserver();
+        m_buffer.clear();
+        m_outStream.clear();
+    }
+    Member<BodyStreamBuffer> m_buffer;
+    RefPtrWillBeMember<Stream> m_outStream;
+};
+
+} // namespace
 
 class RespondWithObserver::ThenFunction final : public ScriptFunction {
 public:
@@ -139,6 +202,24 @@ void RespondWithObserver::responseWasFulfilled(const ScriptValue& value)
         return;
     }
     response->setBodyUsed();
+    if (response->streamAccessed()) {
+        // FIXME: We don't support returning the stream accessed Response to the
+        // page.
+        executionContext()->addConsoleMessage(ConsoleMessage::create(JSMessageSource, ErrorMessageLevel, "Returning the stream accessed Response to the page is not supported."));
+        responseWasRejected();
+        return;
+    }
+    if (response->internalBuffer()) {
+        WebServiceWorkerResponse webResponse;
+        response->populateWebServiceWorkerResponse(webResponse);
+        RefPtrWillBeMember<Stream> outStream(Stream::create(executionContext(), ""));
+        webResponse.setStreamURL(outStream->url());
+        ServiceWorkerGlobalScopeClient::from(executionContext())->didHandleFetchEvent(m_eventID, webResponse);
+        StreamUploader* uploader = new StreamUploader(response->internalBuffer(), outStream);
+        uploader->start();
+        m_state = Done;
+        return;
+    }
     WebServiceWorkerResponse webResponse;
     response->populateWebServiceWorkerResponse(webResponse);
     ServiceWorkerGlobalScopeClient::from(executionContext())->didHandleFetchEvent(m_eventID, webResponse);
