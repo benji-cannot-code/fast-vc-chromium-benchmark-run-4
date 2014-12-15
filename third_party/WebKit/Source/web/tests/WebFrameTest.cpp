bbbe38884d546d94a1a9c91853696ecdcb10b72c
danakj@chromium.org <danakj@chromium.org>
2014-12-15 21:32:39 +0000

    Remove setSurfaceReady from WebLayerTreeView.
    This function is not called from blink code, only from RenderWidget
    now.
    
    R=enne
    
    Review URL: https://codereview.chromium.org/801063002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187181 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/tests/WebFrameTest.cpp
+++ third_party/WebKit/Source/web/tests/WebFrameTest.cpp
@@ -4279,7 +4279,6 @@ public:
     CompositedSelectionBoundsTestLayerTreeView() : m_selectionCleared(false) { }
     virtual ~CompositedSelectionBoundsTestLayerTreeView() { }
 
-    virtual void setSurfaceReady()  override { }
     virtual void setRootLayer(const WebLayer&)  override { }
     virtual void clearRootLayer()  override { }
     virtual void setViewportSize(const WebSize& deviceViewportSize)  override { }
