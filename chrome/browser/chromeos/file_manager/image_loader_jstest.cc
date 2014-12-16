6c7a5e465bbf12d92ff07c8663981c324917f14f
hirono <hirono@chromium.org>
2014-12-16 00:35:18 -0800

    Add ImageLoaderJsTest class.
    BUG=442619
    TEST=None
    
    Review URL: https://codereview.chromium.org/802253003
    
    Cr-Commit-Position: refs/heads/master@{#308552}

---
+++ chrome/browser/chromeos/file_manager/image_loader_jstest.cc
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/file_manager/file_manager_jstest_base.h"
+
+class ImageLoaderJsTest : public FileManagerJsTestBase {
+ protected:
+  ImageLoaderJsTest() : FileManagerJsTestBase(
+      base::FilePath(FILE_PATH_LITERAL("ui/file_manager/image_loader"))) {}
+};
