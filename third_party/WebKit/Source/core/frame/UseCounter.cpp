8ad859d138c50f90002c19cbd40f26b47a2c9fb5
philipj@opera.com <philipj@opera.com>
2014-12-16 14:13:36 +0000

    Deprecate Window.webkitURL
    Intent to Deprecate:
    https://groups.google.com/a/chromium.org/d/msg/blink-dev/EsJE9wb321I/zzW9Zr3D1xYJ
    
    BUG=330691,348985
    
    Review URL: https://codereview.chromium.org/804733002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187252 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/UseCounter.cpp
+++ third_party/WebKit/Source/core/frame/UseCounter.cpp
@@ -831,6 +831,9 @@ String UseCounter::deprecationMessage(Feature feature)
     case ShadowRootGetElementsByTagNameNS:
         return "ShadowRoot.getElementsByTagNameNS() is deprecated. Please use 'querySelectorAll' instead";
 
+    case PrefixedWindowURL:
+        return replacedBy("webkitURL", "URL");
+
     // Features that aren't deprecated don't have a deprecation message.
     default:
         return String();
