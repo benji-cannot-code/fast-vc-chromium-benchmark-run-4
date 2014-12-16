2883041a887075215834f504536e4949e416e4ed
mukai <mukai@chromium.org>
2014-12-15 18:34:12 -0800

    Refactor partial screenshot region selector.
    Previously PartialScreenshotView is a view inside of a frameless
    window, which is actually problematic because it needs to deal
    with several window manager concepts such like mouse captures
    and activations.
    
    This is now built with ui::Layer and EventHandler, therefore
    it makes no effects.
    
    Also this CL moves the file location to a new directory
    ash/utility, because this is nothing related to the window manager.
    
    BUG=330348
    R=oshima@chromium.org
    TEST=the new test covers.
    
    Review URL: https://codereview.chromium.org/805793004
    
    Cr-Commit-Position: refs/heads/master@{#308508}

--- ash/accelerators/accelerator_controller.cc
+++ ash/accelerators/accelerator_controller.cc
@@ -42,11 +42,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/tray/system_tray_notifier.h"
 #include "ash/system/web_notification/web_notification_tray.h"
 #include "ash/touch/touch_hud_debug.h"
+#include "ash/utility/partial_screenshot_controller.h"
 #include "ash/volume_control_delegate.h"
 #include "ash/wm/maximize_mode/maximize_mode_controller.h"
 #include "ash/wm/mru_window_tracker.h"
 #include "ash/wm/overview/window_selector_controller.h"
-#include "ash/wm/partial_screenshot_view.h"
 #include "ash/wm/power_button_controller.h"
 #include "ash/wm/window_cycle_controller.h"
 #include "ash/wm/window_state.h"
@@ -406,7 +406,7 @@ void HandleSwitchIme(ImeControlDelegate* ime_control_delegate,
 void HandleTakePartialScreenshot(ScreenshotDelegate* screenshot_delegate) {
   base::RecordAction(UserMetricsAction("Accel_Take_Partial_Screenshot"));
   if (screenshot_delegate) {
-    ash::PartialScreenshotView::StartPartialScreenshot(
+    ash::PartialScreenshotController::StartPartialScreenshotSession(
         screenshot_delegate);
   }
 }
