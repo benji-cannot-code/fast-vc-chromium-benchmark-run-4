43d86f00491a04251e8cdfcd32966fae43846103
bokan <bokan@chromium.org>
2014-12-16 08:10:19 -0800

    Revert of telemetry: Clean up SurfaceFlinger statistics (patchset #8 id:140001 of https://codereview.chromium.org/765773004/)
    Reason for revert:
    Perf sheriff here, speculatively reverting due to crbug.com/442752, it shows up in many blamelists (but not all as the test wasn't running due to another bug). Sorry if unrelated.
    
    Original issue's description:
    > PERF-SHERIFFS: This patch will change all smoothness and SurfaceFlinger metrics on Android. Some metrics may go up or down significantly, with the new values being more accurate.
    >
    > This patch cleans up the integration of SurfaceFlinger statistics by pulling the
    > raw timestamps into the timeline and calculating all metrics in
    > SmoothnessMetric. This approach also improves the metrics, because the
    > timeline ranges can now be properly bounded (no more percentiles!) and the
    > existing smoothness metrics can also use the more accurate SurfaceFlinger
    > timestamps.
    >
    > BUG=351916
    >
    > Committed: https://crrev.com/ebae600f54ed1e77eeba916635310fb176c0fb6c
    > Cr-Commit-Position: refs/heads/master@{#308488}
    
    TBR=tonyg@chromium.org,dtu@chromium.org,nednguyen@google.com,chrishenry@google.com,skyostil@google.com,sullivan@chromium.org,skyostil@chromium.org,jbudorick@chromium.org,ernstm@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=351916
    
    Review URL: https://codereview.chromium.org/809753002
    
    Cr-Commit-Position: refs/heads/master@{#308594}

--- tools/telemetry/telemetry/core/platform/ios_platform_backend.py
+++ tools/telemetry/telemetry/core/platform/ios_platform_backend.py
@@ -44,6 +44,9 @@ class IosPlatformBackend(posix_platform_backend.PosixPlatformBackend):
   def StopMonitoringPower(self):
     raise NotImplementedError()
 
+  def GetRawDisplayFrameRateMeasurements(self):
+    raise NotImplementedError()
+
   def FlushEntireSystemCache(self):
     raise NotImplementedError()
 
@@ -54,6 +57,12 @@ class IosPlatformBackend(posix_platform_backend.PosixPlatformBackend):
     raise NotImplementedError()
 
   def IsThermallyThrottled(self):
+    raise NotImplementedError()
+
+  def StartRawDisplayFrameRateMeasurement(self):
+    raise NotImplementedError()
+
+  def StopRawDisplayFrameRateMeasurement(self):
     raise NotImplementedError()
 
   def GetSystemTotalPhysicalMemory(self):
