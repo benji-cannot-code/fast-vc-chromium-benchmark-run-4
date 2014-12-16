1dfd6fb667d43f24c92901580eb9ab6c5ad64996
danakj <danakj@chromium.org>
2014-12-15 16:06:23 -0800

    cc: Make the PictureLayerImpl raster source null until commit.
    No need to make a raster source that is never used.
    
    R=enne, vmpstr
    BUG=387116
    
    Review URL: https://codereview.chromium.org/809433003
    
    Cr-Commit-Position: refs/heads/master@{#308466}

--- cc/quads/draw_quad_unittest.cc
+++ cc/quads/draw_quad_unittest.cc
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/quads/texture_draw_quad.h"
 #include "cc/quads/tile_draw_quad.h"
 #include "cc/quads/yuv_video_draw_quad.h"
-#include "cc/resources/picture_pile_impl.h"
+#include "cc/test/fake_picture_pile_impl.h"
 #include "cc/test/geometry_test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/skia/include/effects/SkBlurImageFilter.h"
@@ -690,7 +690,9 @@ TEST(DrawQuadTest, CopyPictureDrawQuad) {
   ResourceFormat texture_format = RGBA_8888;
   gfx::Rect content_rect(30, 40, 20, 30);
   float contents_scale = 3.141592f;
-  scoped_refptr<RasterSource> raster_source = PicturePileImpl::Create();
+  scoped_refptr<RasterSource> raster_source =
+      FakePicturePileImpl::CreateEmptyPile(gfx::Size(100, 100),
+                                           gfx::Size(100, 100));
   CREATE_SHARED_STATE();
 
   CREATE_QUAD_9_NEW(PictureDrawQuad, opaque_rect, visible_rect, tex_coord_rect,
@@ -939,7 +941,9 @@ TEST_F(DrawQuadIteratorTest, DISABLED_PictureDrawQuad) {
   ResourceFormat texture_format = RGBA_8888;
   gfx::Rect content_rect(30, 40, 20, 30);
   float contents_scale = 3.141592f;
-  scoped_refptr<PicturePileImpl> raster_source = PicturePileImpl::Create();
+  scoped_refptr<RasterSource> raster_source =
+      FakePicturePileImpl::CreateEmptyPile(gfx::Size(100, 100),
+                                           gfx::Size(100, 100));
 
   CREATE_SHARED_STATE();
   CREATE_QUAD_9_NEW(PictureDrawQuad, opaque_rect, visible_rect, tex_coord_rect,
--- cc/resources/picture_pile_impl.h
+++ cc/resources/picture_pile_impl.h
@@ -31,7 +31,6 @@ namespace cc {
 
 class CC_EXPORT PicturePileImpl : public RasterSource {
  public:
-  static scoped_refptr<PicturePileImpl> Create();
   static scoped_refptr<PicturePileImpl> CreateFromPicturePile(
       const PicturePile* other);
 
