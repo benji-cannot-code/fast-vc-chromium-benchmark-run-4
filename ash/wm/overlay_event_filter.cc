899fe2bc0037bb0f4b8d95ee6ad9195540a01574
mukai <mukai@chromium.org>
2014-12-15 21:26:32 -0800

    Revert of Refactor partial screenshot region selector. (patchset #3 id:40001 of https://codereview.chromium.org/805793004/)
    Reason for revert:
    valgrind failure: http://build.chromium.org/p/chromium.memory.fyi/builders/Chromium%20OS%20%28valgrind%29%283%29/builds/35270/steps/memory%20test%3A%20ash_unittests/logs/stdio
    
    Original issue's description:
    > Refactor partial screenshot region selector.
    >
    > Previously PartialScreenshotView is a view inside of a frameless
    > window, which is actually problematic because it needs to deal
    > with several window manager concepts such like mouse captures
    > and activations.
    >
    > This is now built with ui::Layer and EventHandler, therefore
    > it makes no effects.
    >
    > Also this CL moves the file location to a new directory
    > ash/utility, because this is nothing related to the window manager.
    >
    > BUG=330348
    > R=oshima@chromium.org
    > TEST=the new test covers.
    >
    > Committed: https://crrev.com/9a0e3f0b67e057451570cfb254cbf76135d59768
    > Cr-Commit-Position: refs/heads/master@{#308508}
    
    TBR=oshima@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=330348
    
    Review URL: https://codereview.chromium.org/808693003
    
    Cr-Commit-Position: refs/heads/master@{#308531}

--- ash/wm/overlay_event_filter.cc
+++ ash/wm/overlay_event_filter.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/wm/overlay_event_filter.h"
 
+#include "ash/wm/partial_screenshot_view.h"
 #include "ui/aura/window.h"
 #include "ui/aura/window_delegate.h"
 #include "ui/events/event.h"
