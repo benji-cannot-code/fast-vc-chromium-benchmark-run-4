27a19343fd281d7ef4b9307b88985fac262b43e9
keishi@chromium.org <keishi@chromium.org>
2014-12-16 05:43:03 +0000

    Regression: Add back support for hr elements in select listbox
    Regression caused by r178354.
    
    BUG=346582
    
    Review URL: https://codereview.chromium.org/801283003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187221 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLSelectElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLSelectElement.cpp
@@ -1775,7 +1775,7 @@ void HTMLSelectElement::trace(Visitor* visitor)
 void HTMLSelectElement::didAddUserAgentShadowRoot(ShadowRoot& root)
 {
     RefPtrWillBeRawPtr<HTMLContentElement> content = HTMLContentElement::create(document());
-    content->setAttribute(selectAttr, "option,optgroup");
+    content->setAttribute(selectAttr, "option,optgroup,hr");
     root.appendChild(content);
 }
 
