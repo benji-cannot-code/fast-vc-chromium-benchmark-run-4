edd2fc56a9032743c66f60b90b6ae70c2f5a423e
jl <jl@opera.com>
2014-12-16 07:05:09 -0800

    IDL: various adjustments to match Web IDL specification better
    Notably adds support for maplike<type, type>, setlike<type>, iterable<type>
    and legacyiterable<type> declarations.
    
    Also changes some interface member grammar productions around a bit, and
    drops the deprecated Attribute production, which Blink's parser no longer
    references.
    
    Review URL: https://codereview.chromium.org/803933003
    
    Cr-Commit-Position: refs/heads/master@{#308584}

--- tools/idl_parser/idl_ppapi_parser.py
+++ tools/idl_parser/idl_ppapi_parser.py
@@ -241,6 +241,14 @@ class IDLPPAPIParser(IDLParser):
     """ """
     pass
 
+  def p_EnumValueListComma(self, p):
+    """ """
+    pass
+
+  def p_EnumValueListString(self, p):
+    """ """
+    pass
+
   # We only support:
   #    [ identifier ]
   #    [ identifier ( ArgumentList )]
