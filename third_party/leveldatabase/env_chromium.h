7692510fcf7194b640874c60ccc36b94418505ba
cmumford <cmumford@chromium.org>
2014-12-15 16:04:57 -0800

    IndexedDB: ParseMethodAndError takes a leveldb::Status instance.
    ParseMethodAndError took a string as first param, but all callers started with a
    Status object from which they derived the string. Simplified impl by just
    passing in the Status object.
    
    Review URL: https://codereview.chromium.org/805043006
    
    Cr-Commit-Position: refs/heads/master@{#308465}

--- third_party/leveldatabase/env_chromium.h
+++ third_party/leveldatabase/env_chromium.h
@@ -62,7 +62,7 @@ enum ErrorParsingResult {
   NONE,
 };
 
-ErrorParsingResult ParseMethodAndError(const char* string,
+ErrorParsingResult ParseMethodAndError(const leveldb::Status& status,
                                        MethodID* method,
                                        int* error);
 int GetCorruptionCode(const leveldb::Status& status);
