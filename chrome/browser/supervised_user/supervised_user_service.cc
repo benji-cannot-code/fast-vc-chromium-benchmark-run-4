65d8297e77062199802f06f912d731acc09f7f61
treib <treib@chromium.org>
2014-12-16 01:14:49 -0800

    Cleanup: Remove SupervisedUserService::Delegate::IsChildAccount
    Followup to https://codereview.chromium.org/787803002/
    
    BUG=
    
    Review URL: https://codereview.chromium.org/787423004
    
    Cr-Commit-Position: refs/heads/master@{#308557}

--- chrome/browser/supervised_user/supervised_user_service.cc
+++ chrome/browser/supervised_user/supervised_user_service.cc
@@ -66,10 +66,6 @@ using base::DictionaryValue;
 using base::UserMetricsAction;
 using content::BrowserThread;
 
-bool SupervisedUserService::Delegate::IsChildAccount() const {
-  return false;
-}
-
 base::FilePath SupervisedUserService::Delegate::GetBlacklistPath() const {
   return base::FilePath();
 }
