15bdbe2d05265083bfdf330299e9a518cac2b998
gusfernandez <gusfernandez@chromium.org>
2014-12-15 16:07:35 -0800

    Release GPU resources only after renderer GPU channels are closed.
    The RelinquishGpuResource message from the browser process may
    come before or after the rederer process closes it GPU channel.
    As such, we need to wait until only the Browser GPU channel is
    open until we can actually release the default off-screen
    PBuffer surface and the EGL display.
    
    This is a follow-up to https://codereview.chromium.org/712343003
    
    Bug: 18724066
    BUG=432268
    
    Review URL: https://codereview.chromium.org/800103005
    
    Cr-Commit-Position: refs/heads/master@{#308468}

--- content/common/gpu/gpu_channel_manager.cc
+++ content/common/gpu/gpu_channel_manager.cc
@@ -102,6 +102,7 @@ GpuChannelManager::GpuChannelManager(MessageRouter* router,
       channel_(channel),
       filter_(
           new GpuChannelManagerMessageFilter(gpu_memory_buffer_factory_.get())),
+      relinquish_resources_pending_(false),
       weak_factory_(this) {
   DCHECK(router_);
   DCHECK(io_message_loop);
@@ -138,6 +139,7 @@ GpuChannelManager::shader_translator_cache() {
 void GpuChannelManager::RemoveChannel(int client_id) {
   Send(new GpuHostMsg_DestroyChannel(client_id));
   gpu_channels_.erase(client_id);
+  CheckRelinquishGpuResources();
 }
 
 int GpuChannelManager::GenerateRouteID() {
@@ -225,6 +227,7 @@ void GpuChannelManager::OnCloseChannel(
        iter != gpu_channels_.end(); ++iter) {
     if (iter->second->GetChannelName() == channel_handle.name) {
       gpu_channels_.erase(iter);
+      CheckRelinquishGpuResources();
       return;
     }
   }
@@ -328,6 +331,7 @@ void GpuChannelManager::LoseAllContexts() {
 
 void GpuChannelManager::OnLoseAllContexts() {
   gpu_channels_.clear();
+  CheckRelinquishGpuResources();
 }
 
 gfx::GLSurface* GpuChannelManager::GetDefaultOffscreenSurface() {
@@ -339,19 +343,27 @@ gfx::GLSurface* GpuChannelManager::GetDefaultOffscreenSurface() {
 }
 
 void GpuChannelManager::OnRelinquishResources() {
-  if (default_offscreen_surface_.get()) {
-    default_offscreen_surface_->DestroyAndTerminateDisplay();
-    default_offscreen_surface_ = nullptr;
-  }
+  relinquish_resources_pending_ = true;
+  CheckRelinquishGpuResources();
+}
+
+void GpuChannelManager::CheckRelinquishGpuResources() {
+  if (relinquish_resources_pending_ && gpu_channels_.size() <= 1) {
+    relinquish_resources_pending_ = false;
+    if (default_offscreen_surface_.get()) {
+      default_offscreen_surface_->DestroyAndTerminateDisplay();
+      default_offscreen_surface_ = NULL;
+    }
 #if defined(USE_OZONE)
-  ui::OzonePlatform::GetInstance()
-      ->GetGpuPlatformSupport()
-      ->RelinquishGpuResources(
-          base::Bind(&GpuChannelManager::OnResourcesRelinquished,
-                     weak_factory_.GetWeakPtr()));
+    ui::OzonePlatform::GetInstance()
+        ->GetGpuPlatformSupport()
+        ->RelinquishGpuResources(
+            base::Bind(&GpuChannelManager::OnResourcesRelinquished,
+                       weak_factory_.GetWeakPtr()));
 #else
-  OnResourcesRelinquished();
+    OnResourcesRelinquished();
 #endif
+  }
 }
 
 void GpuChannelManager::OnResourcesRelinquished() {
