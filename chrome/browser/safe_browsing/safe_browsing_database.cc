8c442d2e330859e5aaff35b765a99d673a382d7b
gab <gab@chromium.org>
2014-12-16 13:46:06 -0800

    Fix a race condition in SafeBrowsingDatabaseNew::IsCsdWhitelistKillSwitchOn()
    Extracted from https://codereview.chromium.org/744183002/
    
    TBR=mattm
    BUG=442891
    
    Review URL: https://codereview.chromium.org/793663003
    
    Cr-Commit-Position: refs/heads/master@{#308672}

--- chrome/browser/safe_browsing/safe_browsing_database.cc
+++ chrome/browser/safe_browsing/safe_browsing_database.cc
@@ -1680,5 +1680,6 @@ bool SafeBrowsingDatabaseNew::IsMalwareIPMatchKillSwitchOn() {
 }
 
 bool SafeBrowsingDatabaseNew::IsCsdWhitelistKillSwitchOn() {
+  base::AutoLock locked(lookup_lock_);
   return csd_whitelist_.second;
 }
