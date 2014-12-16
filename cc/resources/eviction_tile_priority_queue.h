0d294253fdaa2616da579f6b822615dbb7ec9410
e.hakkinen <e.hakkinen@samsung.com>
2014-12-16 01:54:37 -0800

    cc: Optimise shared tile handling in eviction tile priority queue.
    As tiling set eviction queue twin pairs do not return shared tiles
    twice, eviction tile priority queue does not have to keep track of
    shared tiles and filter out duplicated tiles, any more.
    
    Review URL: https://codereview.chromium.org/798973002
    
    Cr-Commit-Position: refs/heads/master@{#308560}

--- cc/resources/eviction_tile_priority_queue.h
+++ cc/resources/eviction_tile_priority_queue.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CC_RESOURCES_EVICTION_TILE_PRIORITY_QUEUE_H_
 #define CC_RESOURCES_EVICTION_TILE_PRIORITY_QUEUE_H_
 
+#include <set>
 #include <utility>
 #include <vector>
 
@@ -33,8 +34,8 @@ class CC_EXPORT EvictionTilePriorityQueue {
     scoped_ptr<TilingSetEvictionQueue> active_queue;
     scoped_ptr<TilingSetEvictionQueue> pending_queue;
 
-    // TODO(vmpstr): Investigate removing this.
-    std::vector<Tile*> returned_shared_tiles;
+    // Set of returned tiles (excluding the current one) for DCHECKing.
+    std::set<const Tile*> returned_tiles_for_debug;
   };
 
   EvictionTilePriorityQueue();
