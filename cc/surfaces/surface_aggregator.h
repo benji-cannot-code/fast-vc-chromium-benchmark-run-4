90ed3fb605f4c633b4530ce0d77ec5984e40efd9
jbauman <jbauman@chromium.org>
2014-12-16 15:14:31 -0800

    Accumulate content_to_target_transform for nested Surfaces.
    All the transforms need to be combined together if there are multiple layers in the nesting.
    
    Review URL: https://codereview.chromium.org/804903007
    
    Cr-Commit-Position: refs/heads/master@{#308689}

--- cc/surfaces/surface_aggregator.h
+++ cc/surfaces/surface_aggregator.h
@@ -43,6 +43,7 @@ class CC_SURFACES_EXPORT SurfaceAggregator {
                            SurfaceId surface_id);
 
   void HandleSurfaceQuad(const SurfaceDrawQuad* surface_quad,
+                         const gfx::Transform& content_to_target_transform,
                          RenderPass* dest_pass);
   void CopySharedQuadState(const SharedQuadState* source_sqs,
                            const gfx::Transform& content_to_target_transform,
