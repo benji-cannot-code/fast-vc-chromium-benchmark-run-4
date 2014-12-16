5eb9016ec5c1c2926971551daf47f15e560c2b95
ssid <ssid@chromium.org>
2014-12-16 08:04:02 -0800

    Removed discrepancy in low-end-device-mode flag
    Removed the flag low-end-device-mode and added two flags enable-low-end-device-mode and disable-low-end-device-mode. This removes discrepancies between the flag values (integers) when used in different platforms and makes it simple to use with the just the flags and no values passed.
    
    BUG=437778
    
    Review URL: https://codereview.chromium.org/761783004
    
    Cr-Commit-Position: refs/heads/master@{#308593}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1316,7 +1316,8 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kEnableWebRtcHWH264Encoding,
     switches::kWebRtcMaxCaptureFramerate,
 #endif
-    switches::kLowEndDeviceMode,
+    switches::kEnableLowEndDeviceMode,
+    switches::kDisableLowEndDeviceMode,
 #if defined(OS_ANDROID)
     switches::kDisableGestureRequirementForMediaPlayback,
     switches::kDisableWebRTC,
