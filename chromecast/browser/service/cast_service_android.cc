abc6613955cb5e762675e825188f4b28fca37698
byungchul <byungchul@chromium.org>
2014-12-17 02:23:10 -0800

    Fix compilation error for android.
    BUG=
    TBR=guncsh@chromium.org,lcwu@chromium.org
    
    Review URL: https://codereview.chromium.org/809693004
    
    Cr-Commit-Position: refs/heads/master@{#308774}

--- chromecast/browser/service/cast_service_android.cc
+++ chromecast/browser/service/cast_service_android.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromecast/browser/service/cast_service_android.h"
 
+#include "base/bind.h"
 #include "chromecast/android/chromecast_config_android.h"
 #include "chromecast/browser/metrics/cast_metrics_service_client.h"
 
