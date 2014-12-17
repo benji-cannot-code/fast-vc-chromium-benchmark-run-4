b6d88869387162f16498fcdce34f0807f040b3df
apatole@nvidia.com <apatole@nvidia.com>
2014-12-17 17:02:24 +0000

    Consider multisample buffers when reporting externally allocated memory to V8
    On a device that supports MSAA, browser sometimes runs out of GPU
    memory when canvases and antialised 3D contexts are created
    repeatatively. This is because multisample buffers are not considered
    when reporting externally allocated memory to add pressure on V8 GC.
    This change modifies code to also report multisample buffers memory
    when antialiased contexts are created and device supports MSAA. This
    ensures enough memory pressure is added to V8 and GC occurs frequently
    to avoid browser running out of GPU memory.
    
    BUG=437800
    
    Review URL: https://codereview.chromium.org/772483003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187384 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/canvas/WebGLRenderingContextBase.h
+++ third_party/WebKit/Source/core/html/canvas/WebGLRenderingContextBase.h
@@ -376,6 +376,9 @@ public:
 
     virtual void trace(Visitor*) override;
 
+    // Returns approximate gpu memory allocated per pixel.
+    int externallyAllocatedBytesPerPixel();
+
     class TextureUnitState {
         ALLOW_ONLY_INLINE_ALLOCATION();
     public:
