3e7d80dc178565fe668da976b9ec6ae231b4e648
xunjieli <xunjieli@chromium.org>
2014-12-17 11:18:01 -0800

    Move mIntentFilter initialization before registerReceiver() is called
    It is possible to call registerReceiver() before mIntentFilter
    is initialized. This Cl moves the initialization earlier to
    avoid a crash.
    
    Review URL: https://codereview.chromium.org/814573002
    
    Cr-Commit-Position: refs/heads/master@{#308835}

--- net/android/java/src/org/chromium/net/NetworkChangeNotifierAutoDetect.java
+++ net/android/java/src/org/chromium/net/NetworkChangeNotifierAutoDetect.java
@@ -175,13 +175,14 @@ public class NetworkChangeNotifierAutoDetect extends BroadcastReceiver
         mConnectionType = getCurrentConnectionType(networkState);
         mWifiSSID = getCurrentWifiSSID(networkState);
         mMaxBandwidthMbps = getCurrentMaxBandwidthInMbps(networkState);
+        mIntentFilter =
+                new NetworkConnectivityIntentFilter(mWifiManagerDelegate.getHasWifiPermission());
+
         if (alwaysWatchForChanges) {
             registerReceiver();
         } else {
             ApplicationStatus.registerApplicationStateListener(this);
         }
-        mIntentFilter =
-                new NetworkConnectivityIntentFilter(mWifiManagerDelegate.getHasWifiPermission());
     }
 
     /**
