184dc0d8cc78aed06ec16188f2863a59aea832a0
twellington <twellington@chromium.org>
2014-12-17 15:52:04 -0800

    Redesign autofill preferences screen
    Styling on main autofill preference screen; changed month spinner text
    on AutofillCredCardEditor.
    
    BUG=414438
    
    Review URL: https://codereview.chromium.org/798693004
    
    Cr-Commit-Position: refs/heads/master@{#308882}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/autofill/AutofillPreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/autofill/AutofillPreferences.java
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.preferences.autofill;
 
 import android.app.Activity;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
@@ -31,6 +33,8 @@ public class AutofillPreferences extends PreferenceFragment
     public static final String SETTINGS_ORIGIN = "Chrome settings";
     private static final int SUMMARY_CHAR_LENGTH = 40;
     private static final String PREF_AUTOFILL_SWITCH = "autofill_switch";
+    private static final String PREF_AUTOFILL_PROFILES = "autofill_profiles";
+    private static final String PREF_AUTOFILL_CREDIT_CARDS = "autofill_credit_cards";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -52,12 +56,27 @@ public class AutofillPreferences extends PreferenceFragment
             }
         });
 
+        setPreferenceCategoryIcons();
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         rebuildLists();
         return true;
+    }
+
+    private void setPreferenceCategoryIcons() {
+        Drawable plusIcon = getResources().getDrawable(R.drawable.plus);
+        plusIcon.mutate();
+        plusIcon.setColorFilter(getResources().getColor(R.color.pref_accent_color),
+                PorterDuff.Mode.SRC_IN);
+        findPreference(PREF_AUTOFILL_PROFILES).setIcon(plusIcon);
+
+        plusIcon = getResources().getDrawable(R.drawable.plus);
+        plusIcon.mutate();
+        plusIcon.setColorFilter(getResources().getColor(R.color.pref_accent_color),
+                PorterDuff.Mode.SRC_IN);
+        findPreference(PREF_AUTOFILL_CREDIT_CARDS).setIcon(plusIcon);
     }
 
     /**
@@ -71,7 +90,7 @@ public class AutofillPreferences extends PreferenceFragment
     // Always clears the list before building/rebuilding.
     private void rebuildProfileList() {
         // Add an edit preference for each current Chrome profile.
-        PreferenceGroup profileCategory = (PreferenceGroup) findPreference("autofill_profiles");
+        PreferenceGroup profileCategory = (PreferenceGroup) findPreference(PREF_AUTOFILL_PROFILES);
         profileCategory.removeAll();
         for (AutofillProfile profile : PersonalDataManager.getInstance().getProfiles()) {
             // Add an item on the current page...
@@ -94,7 +113,8 @@ public class AutofillPreferences extends PreferenceFragment
     }
 
     private void rebuildCreditCardList() {
-        PreferenceGroup profileCategory = (PreferenceGroup) findPreference("autofill_credit_cards");
+        PreferenceGroup profileCategory =
+                (PreferenceGroup) findPreference(PREF_AUTOFILL_CREDIT_CARDS);
         profileCategory.removeAll();
         for (CreditCard card : PersonalDataManager.getInstance().getCreditCards()) {
             // Add an item on the current page...
