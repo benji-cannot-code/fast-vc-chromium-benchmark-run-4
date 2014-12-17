8ad5dcb283abc05bbf3be46f7a3a0172142fb318
dnicoara <dnicoara@chromium.org>
2014-12-16 19:31:44 -0800

    [Ozone-DRI] Make sure to reply to all configuration requests
    If we're not replying the browser side will continue waiting for responses
    delaying other configuration requests.
    
    BUG=none
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/811633005
    
    Cr-Commit-Position: refs/heads/master@{#308741}

--- ui/ozone/platform/dri/dri_gpu_platform_support.cc
+++ ui/ozone/platform/dri/dri_gpu_platform_support.cc
@@ -137,6 +137,7 @@ void DriGpuPlatformSupport::OnConfigureNativeDisplay(
   DisplaySnapshot* display = ndd_->FindDisplaySnapshot(id);
   if (!display) {
     LOG(ERROR) << "There is no display with ID " << id;
+    sender_->Send(new OzoneHostMsg_DisplayConfigured(id, false));
     return;
   }
 
@@ -162,6 +163,7 @@ void DriGpuPlatformSupport::OnConfigureNativeDisplay(
     LOG(ERROR) << "Failed to find mode: size=" << mode_param.size.ToString()
                << " is_interlaced=" << mode_param.is_interlaced
                << " refresh_rate=" << mode_param.refresh_rate;
+    sender_->Send(new OzoneHostMsg_DisplayConfigured(id, false));
     return;
   }
 
@@ -171,10 +173,13 @@ void DriGpuPlatformSupport::OnConfigureNativeDisplay(
 
 void DriGpuPlatformSupport::OnDisableNativeDisplay(int64_t id) {
   DisplaySnapshot* display = ndd_->FindDisplaySnapshot(id);
+  bool success = false;
   if (display)
-    ndd_->Configure(*display, NULL, gfx::Point());
+    success = ndd_->Configure(*display, NULL, gfx::Point());
   else
     LOG(ERROR) << "There is no display with ID " << id;
+
+  sender_->Send(new OzoneHostMsg_DisplayConfigured(id, success));
 }
 
 void DriGpuPlatformSupport::OnTakeDisplayControl() {
