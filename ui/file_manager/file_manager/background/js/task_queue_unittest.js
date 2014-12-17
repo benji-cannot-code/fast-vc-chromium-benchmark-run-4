b43fbebf31b75cc6b54a8603be5dd19eedcc27c2
kenobi <kenobi@chromium.org>
2014-12-16 19:42:59 -0800

    Switch MediaImportHandler over to use TaskQueue.
    This CL does the following:
    - transition the Task over to an Observer model, which allows clients more flexibility in tracking tasks.
    - make the ImportTask a subclass of TaskQueue.Task
    - switch the MediaImportHandler over to use the TaskQueue
    - fix all unit tests.
    
    BUG=420680
    
    Review URL: https://codereview.chromium.org/809563004
    
    Cr-Commit-Position: refs/heads/master@{#308742}

--- ui/file_manager/file_manager/background/js/task_queue_unittest.js
+++ ui/file_manager/file_manager/background/js/task_queue_unittest.js
@@ -28,9 +28,11 @@ function setUp() {
  * A Task subclass for testing.
  * @constructor
  * @extends {importer.TaskQueue.BaseTask}
+ *
+ * @param {string} taskId
  */
-var TestTask = function() {
-  importer.TaskQueue.Task.call(this);
+var TestTask = function(taskId) {
+  importer.TaskQueue.BaseTask.call(this, taskId);
 
   /** @type {boolean} */
   this.wasRun = false;
@@ -52,6 +54,21 @@ TestTask.prototype.run = function() {
   this.runResolver_(this);
 };
 
+/** Sends a quick error notification. */
+TestTask.prototype.notifyError = function() {
+  this.notify(importer.TaskQueue.UpdateType.ERROR);
+};
+
+/** Sends a quick success notification. */
+TestTask.prototype.notifySuccess = function() {
+  this.notify(importer.TaskQueue.UpdateType.SUCCESS);
+};
+
+/** Sends a quick progress notification. */
+TestTask.prototype.notifyProgress = function() {
+  this.notify(importer.TaskQueue.UpdateType.PROGRESS);
+};
+
 /** @return {!Promise} A promise that settles once #run is called. */
 TestTask.prototype.whenRun = function() {
   return this.runPromise_;
@@ -59,15 +76,15 @@ TestTask.prototype.whenRun = function() {
 
 // Verifies that a queued task gets run.
 function testRunsTask(callback) {
-  var task = new TestTask();
+  var task = new TestTask('task0');
   queue.queueTask(task);
   reportPromise(task.whenRun(), callback);
 }
 
 // Verifies that multiple queued tasks get run.
 function testRunsTasks(callback) {
-  var task0 = new TestTask();
-  var task1 = new TestTask();
+  var task0 = new TestTask('task0');
+  var task1 = new TestTask('task1');
 
   // Make the tasks call Task#notifySuccess when they are run.
   task0.whenRun().then(function(task) { task.notifySuccess(); });
@@ -83,7 +100,7 @@ function testRunsTasks(callback) {
 
 // Verifies that the active callback triggers when the queue starts doing work
 function testOnActiveCalled(callback) {
-  var task = new TestTask();
+  var task = new TestTask('task0');
 
   // Make a promise that resolves when the active callback is triggered.
   var whenActive = new Promise(function(resolve) {
@@ -102,7 +119,7 @@ function testOnActiveCalled(callback) {
 
 // Verifies that the idle callback triggers when the queue is empty.
 function testOnIdleCalled(callback) {
-  var task = new TestTask();
+  var task = new TestTask('task0');
 
   task.whenRun().then(function(task) { task.notifySuccess(); });
 
@@ -124,7 +141,7 @@ function testOnIdleCalled(callback) {
 
 // Verifies that the update callback is called when a task reports progress.
 function testProgressUpdate(callback) {
-  var task = new TestTask();
+  var task = new TestTask('task0');
 
   // Get the task to report some progress, then success, when it's run.
   task.whenRun()
@@ -156,7 +173,7 @@ function testProgressUpdate(callback) {
 // Verifies that the update callback is called to report successful task
 // completion.
 function testSuccessUpdate(callback) {
-  var task = new TestTask();
+  var task = new TestTask('task0');
 
   // Get the task to report success when it's run.
   task.whenRun().then(function(task) { task.notifySuccess(); });
@@ -177,7 +194,7 @@ function testSuccessUpdate(callback) {
 
 // Verifies that the update callback is called to report task errors.
 function testErrorUpdate(callback) {
-  var task = new TestTask();
+  var task = new TestTask('task0');
 
   // Get the task to report an error when it's run.
   task.whenRun().then(function(task) { task.notifyError(); });
