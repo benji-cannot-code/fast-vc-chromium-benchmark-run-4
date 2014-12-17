2de490c5bf08065bae0117c33550bc84d6f9bba4
mostynb@opera.com <mostynb@opera.com>
2014-12-17 00:24:39 +0000

    replace COMPILE_ASSERT with static_assert in core/rendering/
    BUG=442514
    
    Review URL: https://codereview.chromium.org/811843003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187306 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/InlineBox.cpp
+++ third_party/WebKit/Source/core/rendering/InlineBox.cpp
@@ -47,7 +47,7 @@ struct SameSizeAsInlineBox {
 #endif
 };
 
-COMPILE_ASSERT(sizeof(InlineBox) == sizeof(SameSizeAsInlineBox), InlineBox_size_guard);
+static_assert(sizeof(InlineBox) == sizeof(SameSizeAsInlineBox), "InlineBox should stay small");
 
 #if ENABLE(ASSERT)
 
--- third_party/WebKit/Source/core/rendering/PaintPhase.cpp
+++ third_party/WebKit/Source/core/rendering/PaintPhase.cpp
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 // DisplayItem types must be kept in sync with PaintPhase.
-COMPILE_ASSERT((unsigned)DisplayItem::DrawingPaintPhaseBlockBackground == (unsigned)PaintPhaseBlockBackground, DisplayItem_Type_should_stay_in_sync);
-COMPILE_ASSERT((unsigned)DisplayItem::DrawingPaintPhaseClippingMask == (unsigned)PaintPhaseClippingMask, DisplayItem_Type_should_stay_in_sync);
+static_assert((unsigned)DisplayItem::DrawingPaintPhaseBlockBackground == (unsigned)PaintPhaseBlockBackground, "DisplayItem Type should stay in sync");
+static_assert((unsigned)DisplayItem::DrawingPaintPhaseClippingMask == (unsigned)PaintPhaseClippingMask, "DisplayItem Type should stay in sync");
 
 } // namespace blink
