feb8a3d6412e8870e859a7ba1f18f507b51e6361
mukai <mukai@chromium.org>
2014-12-17 02:11:39 -0800

    Allows OnAppTerminating at AshTestBase::TearDown().
    Adding OnAppTerminating causes some failures. This CL also includes
    the fixes for them.
    
    OverlayEventFilterTest.CancelAtActivating:
    - OverlayEventFilter::Cancel() is called, but that does not cleanup
      |delegate_|, therefore further OnAppTerminating() invokes Cancel()
      to already canceled delegate_.
    
    PartialScreenshotViewTest.DontStartOverOverlay:
    - delegate should be deactivated before it's removed.
    
    RootWindowControllerTest.DontDeleteWindowsNotOwnedByParent:
    - this test case explicitly invokes CloseAllChildWindows(), therefore
      it cleans up all of the containers.
    - SessionStateAnimatorImpl assumed containers still exist when
      OnAppTerminating() is invoked.
    
    WindowManagerTest.TestCursorClientObserver:
    - observer_a is added to cursor_manager, but not destructed without
      calling RemoveObserver().
    
    BUG=None
    R=oshima@chromium.org
    TEST=ash_unittests
    
    Review URL: https://codereview.chromium.org/809603008
    
    Cr-Commit-Position: refs/heads/master@{#308772}

--- ash/test/ash_test_base.cc
+++ ash/test/ash_test_base.cc
@@ -161,6 +161,7 @@ void AshTestBase::SetUp() {
 
 void AshTestBase::TearDown() {
   teardown_called_ = true;
+  Shell::GetInstance()->OnAppTerminating();
   // Flush the message loop to finish pending release tasks.
   RunAllPendingInMessageLoop();
 
