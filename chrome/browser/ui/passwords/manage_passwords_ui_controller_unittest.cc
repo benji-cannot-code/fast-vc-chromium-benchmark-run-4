971a3875724809f8be6704f1a70492f428bbddda
mkwst <mkwst@chromium.org>
2014-12-17 02:10:25 -0800

    Credential Manager: Pipe federated credentials through to UI layer.
    This patch adjusts the credential manager dispatcher's filters for responses
    to 'request()' which attempt to grab federated credentials, and lays the
    groundwork for doing something useful with them in the UI layer.
    
    Currently that groundwork just consists of piping the values around and
    ensuring that nothing explodes. Future CLs will adjust the UI to ensure
    that users can make a meaningful choice.
    
    BUG=400674
    R=vabr@chromium.org,vasilii@chromium.org
    
    Review URL: https://codereview.chromium.org/809693002
    
    Cr-Commit-Position: refs/heads/master@{#308771}

--- chrome/browser/ui/passwords/manage_passwords_ui_controller_unittest.cc
+++ chrome/browser/ui/passwords/manage_passwords_ui_controller_unittest.cc
@@ -339,10 +339,12 @@ TEST_F(ManagePasswordsUIControllerTest, AutomaticPasswordSave) {
 }
 
 TEST_F(ManagePasswordsUIControllerTest, ChooseCredential) {
-  ScopedVector<autofill::PasswordForm> credentials;
-  credentials.push_back(new autofill::PasswordForm(test_form()));
+  ScopedVector<autofill::PasswordForm> local_credentials;
+  local_credentials.push_back(new autofill::PasswordForm(test_form()));
+  ScopedVector<autofill::PasswordForm> federated_credentials;
   EXPECT_TRUE(controller()->OnChooseCredentials(
-      credentials.Pass(),
+      local_credentials.Pass(),
+      federated_credentials.Pass(),
       base::Bind(&ManagePasswordsUIControllerTest::CredentialCallback,
                  base::Unretained(this))));
   EXPECT_EQ(password_manager::ui::CREDENTIAL_REQUEST_AND_BUBBLE_STATE,
@@ -365,10 +367,12 @@ TEST_F(ManagePasswordsUIControllerTest, ChooseCredential) {
 }
 
 TEST_F(ManagePasswordsUIControllerTest, ChooseCredentialCancel) {
-  ScopedVector<autofill::PasswordForm> credentials;
-  credentials.push_back(new autofill::PasswordForm(test_form()));
+  ScopedVector<autofill::PasswordForm> local_credentials;
+  local_credentials.push_back(new autofill::PasswordForm(test_form()));
+  ScopedVector<autofill::PasswordForm> federated_credentials;
   EXPECT_TRUE(controller()->OnChooseCredentials(
-      credentials.Pass(),
+      local_credentials.Pass(),
+      federated_credentials.Pass(),
       base::Bind(&ManagePasswordsUIControllerTest::CredentialCallback,
                  base::Unretained(this))));
   EXPECT_EQ(password_manager::ui::CREDENTIAL_REQUEST_AND_BUBBLE_STATE,
