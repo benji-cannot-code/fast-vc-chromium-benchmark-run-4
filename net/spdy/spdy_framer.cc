94d3c13a1a0bebe2c0c62ac415d6fbbc904f0b80
bnc <bnc@chromium.org>
2014-12-18 12:07:46 -0800

    Reprioritize upon receiving a HTTP/2 PRIORITY frame.
    Reprioritize an HTTP/2 stream when we receive a PRIORITY frame for that stream. Just stick with linear priorities for now, dependencies/tree can come later.
    
    This lands server change 81441315 by mlavan.
    
    BUG=345769
    
    Review URL: https://codereview.chromium.org/787773003
    
    Cr-Commit-Position: refs/heads/master@{#309063}

--- net/spdy/spdy_framer.cc
+++ net/spdy/spdy_framer.cc
@@ -2869,7 +2869,7 @@ SpdyFrame* SpdyFramer::SerializeAltSvc(const SpdyAltSvcIR& altsvc) {
   return builder.take();
 }
 
-SpdyFrame* SpdyFramer::SerializePriority(const SpdyPriorityIR& priority) {
+SpdyFrame* SpdyFramer::SerializePriority(const SpdyPriorityIR& priority) const {
   DCHECK_LT(SPDY3, protocol_version());
   size_t size = GetPrioritySize();
 
