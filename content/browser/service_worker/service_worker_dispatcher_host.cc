ba33b1308d12dd4c163de41209064f35475138ba
falken <falken@chromium.org>
2014-12-18 00:10:24 -0800

    Tweak error message when Service Worker is inaccessible due to content settings
    This change mimics IndexedDB's "The user denied permission to access the database."
    (see IDBFactory.cpp)
    "The browser has disabled Service Worker" is a bit too strong.
    
    BUG=439810
    
    Review URL: https://codereview.chromium.org/809293002
    
    Cr-Commit-Position: refs/heads/master@{#308968}

--- content/browser/service_worker/service_worker_dispatcher_host.cc
+++ content/browser/service_worker/service_worker_dispatcher_host.cc
@@ -32,11 +32,12 @@ namespace content {
 
 namespace {
 
-const char kShutdownErrorMessage[] =
-    "The Service Worker system has shutdown.";
-const char kDisabledErrorMessage[] = "The browser has disabled Service Worker.";
 const char kNoDocumentURLErrorMessage[] =
     "No URL is associated with the caller's document.";
+const char kShutdownErrorMessage[] =
+    "The Service Worker system has shutdown.";
+const char kUserDeniedPermissionMessage[] =
+    "The user denied permission to use Service Worker.";
 
 const uint32 kFilteredMessageClasses[] = {
   ServiceWorkerMsgStart,
@@ -315,8 +316,8 @@ void ServiceWorkerDispatcherHost::OnRegisterServiceWorker(
     Send(new ServiceWorkerMsg_ServiceWorkerRegistrationError(
         thread_id,
         request_id,
-        WebServiceWorkerError::ErrorTypeDisabled,
-        base::ASCIIToUTF16(kDisabledErrorMessage)));
+        WebServiceWorkerError::ErrorTypeUnknown,
+        base::ASCIIToUTF16(kUserDeniedPermissionMessage)));
     return;
   }
 
@@ -391,8 +392,8 @@ void ServiceWorkerDispatcherHost::OnUnregisterServiceWorker(
     Send(new ServiceWorkerMsg_ServiceWorkerUnregistrationError(
         thread_id,
         request_id,
-        WebServiceWorkerError::ErrorTypeDisabled,
-        base::ASCIIToUTF16(kDisabledErrorMessage)));
+        WebServiceWorkerError::ErrorTypeUnknown,
+        base::ASCIIToUTF16(kUserDeniedPermissionMessage)));
     return;
   }
 
@@ -466,8 +467,8 @@ void ServiceWorkerDispatcherHost::OnGetRegistration(
     Send(new ServiceWorkerMsg_ServiceWorkerGetRegistrationError(
         thread_id,
         request_id,
-        WebServiceWorkerError::ErrorTypeDisabled,
-        base::ASCIIToUTF16(kDisabledErrorMessage)));
+        WebServiceWorkerError::ErrorTypeUnknown,
+        base::ASCIIToUTF16(kUserDeniedPermissionMessage)));
     return;
   }
 
