62ffb34982c1f1049b1ab2d58bf431b28ad23efc
hong.zheng@intel.com <hong.zheng@intel.com>
2014-12-18 04:05:51 +0000

    Add an interface to prune resources and release memory in MemoryCache, especially when memory pressure is critical
    Trigger to prune in CL:
    https://codereview.chromium.org/757933004
    
    BUG=
    
    Review URL: https://codereview.chromium.org/774253007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187429 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/fetch/MemoryCache.cpp
+++ third_party/WebKit/Source/core/fetch/MemoryCache.cpp
@@ -256,10 +256,12 @@ size_t MemoryCache::liveCapacity() const
     return m_capacity - deadCapacity();
 }
 
-void MemoryCache::pruneLiveResources()
+void MemoryCache::pruneLiveResources(PruneStrategy strategy)
 {
     ASSERT(!m_prunePending);
     size_t capacity = liveCapacity();
+    if (strategy == MaximalPrune)
+        capacity = 0;
     if (!m_liveSize || (capacity && m_liveSize <= capacity))
         return;
 
@@ -284,7 +286,7 @@ void MemoryCache::pruneLiveResources()
             if (current->m_resource->isLoaded() && current->m_resource->decodedSize()) {
                 // Check to see if the remaining resources are too new to prune.
                 double elapsedTime = m_pruneFrameTimeStamp - current->m_lastDecodedAccessTime;
-                if (elapsedTime < m_delayBeforeLiveDecodedPrune)
+                if (strategy == AutomaticPrune && elapsedTime < m_delayBeforeLiveDecodedPrune)
                     return;
 
                 // Destroy our decoded data if possible. This will remove us
@@ -300,9 +302,11 @@ void MemoryCache::pruneLiveResources()
     }
 }
 
-void MemoryCache::pruneDeadResources()
+void MemoryCache::pruneDeadResources(PruneStrategy strategy)
 {
     size_t capacity = deadCapacity();
+    if (strategy == MaximalPrune)
+        capacity = 0;
     if (!m_deadSize || (capacity && m_deadSize <= capacity))
         return;
 
@@ -727,11 +731,11 @@ void MemoryCache::prune(Resource* justReleasedResource)
     double currentTime = WTF::currentTime();
     if (m_prunePending) {
         if (currentTime - m_pruneTimeStamp >= m_maxPruneDeferralDelay) {
-            pruneNow(currentTime);
+            pruneNow(currentTime, AutomaticPrune);
         }
     } else {
         if (currentTime - m_pruneTimeStamp >= m_maxPruneDeferralDelay) {
-            pruneNow(currentTime); // Delay exceeded, prune now.
+            pruneNow(currentTime, AutomaticPrune); // Delay exceeded, prune now.
         } else {
             // Defer.
             blink::Platform::current()->currentThread()->addTaskObserver(this);
@@ -755,7 +759,7 @@ void MemoryCache::prune(Resource* justReleasedResource)
 
         // As a last resort, prune immediately
         if (m_deadSize > m_maxDeferredPruneDeadCapacity)
-            pruneNow(currentTime);
+            pruneNow(currentTime, AutomaticPrune);
     }
 }
 
@@ -767,10 +771,16 @@ void MemoryCache::didProcessTask()
 {
     // Perform deferred pruning
     ASSERT(m_prunePending);
-    pruneNow(WTF::currentTime());
+    pruneNow(WTF::currentTime(), AutomaticPrune);
 }
 
-void MemoryCache::pruneNow(double currentTime)
+void MemoryCache::pruneAll()
+{
+    double currentTime = WTF::currentTime();
+    pruneNow(currentTime, MaximalPrune);
+}
+
+void MemoryCache::pruneNow(double currentTime, PruneStrategy strategy)
 {
     if (m_prunePending) {
         m_prunePending = false;
@@ -778,8 +788,8 @@ void MemoryCache::pruneNow(double currentTime)
     }
 
     TemporaryChange<bool> reentrancyProtector(m_inPruneResources, true);
-    pruneDeadResources(); // Prune dead first, in case it was "borrowing" capacity from live.
-    pruneLiveResources();
+    pruneDeadResources(strategy); // Prune dead first, in case it was "borrowing" capacity from live.
+    pruneLiveResources(strategy);
     m_pruneFrameTimeStamp = FrameView::currentFrameTimeStamp();
     m_pruneTimeStamp = currentTime;
 }
