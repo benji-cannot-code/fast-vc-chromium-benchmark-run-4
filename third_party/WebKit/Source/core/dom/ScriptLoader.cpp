28e52b73aff63f17a1a5cbc81195d6c245c88a65
mkwst@chromium.org <mkwst@chromium.org>
2014-12-18 15:36:30 +0000

    SRI: Parse '?ct=' parameters, and verify specified types.
    As defined at https://w3c.github.io/webappsec/specs/subresourceintegrity/#integrity-metadata-1
    
    BUG=443554,443382
    
    Review URL: https://codereview.chromium.org/787793004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187465 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/ScriptLoader.cpp
+++ third_party/WebKit/Source/core/dom/ScriptLoader.cpp
@@ -345,7 +345,7 @@ void ScriptLoader::executeScript(const ScriptSourceCode& sourceCode, double* com
             return;
         }
 
-        if (!SubresourceIntegrity::CheckSubresourceIntegrity(*m_element, sourceCode.source(), sourceCode.resource()->url()))
+        if (!SubresourceIntegrity::CheckSubresourceIntegrity(*m_element, sourceCode.source(), sourceCode.resource()->url(), sourceCode.resource()->mimeType()))
             return;
     }
 
