221d848478e198b8f35afabdb3672c99d64df294
apavlov@chromium.org <apavlov@chromium.org>
2014-12-18 16:08:29 +0000

    DevTools: Do not dispatch click events on disabled dt-radio elements
    R=vsevik
    BUG=443478
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/811633011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187470 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ui/UIUtils.js
+++ third_party/WebKit/Source/devtools/front_end/ui/UIUtils.js
@@ -1373,7 +1373,7 @@ function createCheckboxLabel(title, checked)
      */
     function radioClickHandler(event)
     {
-        if (this.radioElement.checked)
+        if (this.radioElement.checked || this.radioElement.disabled)
             return;
         this.radioElement.checked = true;
         this.radioElement.dispatchEvent(new Event("change"));
