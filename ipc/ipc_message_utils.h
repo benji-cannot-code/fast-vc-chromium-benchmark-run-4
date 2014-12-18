8fb0aeb11ccbe235b44934487427edfc1c18cfcb
dmichael <dmichael@chromium.org>
2014-12-17 17:53:14 -0800

    Add "override" to ParamDeserializer::SerializeOutputParameters
    Currently, the Clang style checker ignores templates. In order to turn it on,
    we first must fix latent style violations. This CL covers ipc/
    
    BUG=441916
    TBR=jam
    
    Review URL: https://codereview.chromium.org/814693002
    
    Cr-Commit-Position: refs/heads/master@{#308921}

--- ipc/ipc_message_utils.h
+++ ipc/ipc_message_utils.h
@@ -866,7 +866,8 @@ class ParamDeserializer : public MessageReplyDeserializer {
  public:
   explicit ParamDeserializer(const RefTuple& out) : out_(out) { }
 
-  bool SerializeOutputParameters(const IPC::Message& msg, PickleIterator iter) {
+  bool SerializeOutputParameters(const IPC::Message& msg,
+                                 PickleIterator iter) override {
     return ReadParam(&msg, &iter, &out_);
   }
 
