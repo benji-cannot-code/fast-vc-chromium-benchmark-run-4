41658295736aea3144f36ba271e8e5ca2e262767
hirono <hirono@chromium.org>
2014-12-17 23:08:53 -0800

    ImageLoader: Stop to create cache for data: URI in image loader extension.
    BUG=442614
    TEST=None
    
    Review URL: https://codereview.chromium.org/816613002
    
    Cr-Commit-Position: refs/heads/master@{#308961}

---
+++ ui/file_manager/image_loader/cache_unittest.js
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+function testCreateCacheKey() {
+  var key = Cache.createKey({url: 'http://example.com/image.jpg'});
+  assertTrue(!!key);
+}
+
+function testNotCreateCacheKey() {
+  var key = Cache.createKey({url: 'data:xxx'});
+  assertFalse(!!key);
+
+  var key = Cache.createKey({url: 'DaTa:xxx'});
+  assertFalse(!!key);
+}
