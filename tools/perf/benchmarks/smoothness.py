147150a5e438ffa83e4c859bc91c794a0708539e
ksakamoto <ksakamoto@chromium.org>
2014-12-17 17:56:32 -0800

    Revert "[Telemetry] Split key_mobile_sites pages to key_mobile_sites_smooth and key_mobile_sites_repaint"
    This reverts commit 68689c53a539564a078ebe3473eb99748561b3b4.
    
    broke telemetry_perf_unittests
    http://build.chromium.org/p/chromium.linux/buildstatus?builder=Linux%20Tests&number=18027
    http://build.chromium.org/p/chromium.mac/buildstatus?builder=Mac10.8%20Tests&number=1928
    http://build.chromium.org/p/chromium.mac/buildstatus?builder=Mac10.6%20Tests&number=1887
    
      Traceback (most recent call last):
        File "/mnt/data/b/build/slave/Linux_Tests/build/src/tools/telemetry/telemetry/decorators.py", line 55, in wrapper
          func(*args, **kwargs)
        File "/mnt/data/b/build/slave/Linux_Tests/build/src/tools/perf/page_sets/page_set_unittest.py", line 18, in testSmoke
          self.RunSmokeTest(page_sets_dir, top_level_dir)
        File "/mnt/data/b/build/slave/Linux_Tests/build/src/tools/telemetry/telemetry/unittest_util/page_set_smoke_test.py", line 145, in RunSmokeTest
          page_set = page_set_class()
        File "/mnt/data/b/build/slave/Linux_Tests/build/src/tools/perf/page_sets/key_mobile_sites_smooth.py", line 132, in __init__
          self.AddPage(_CreatePageClassWithSmoothInteractions(page_class)(self))
      AttributeError: 'KeyMobileSitesSmoothPageSet' object has no attribute 'AddPage'
    
    TBR=nednguyen
    NOTRY=true
    BUG=418375
    
    Review URL: https://codereview.chromium.org/792803003
    
    Cr-Commit-Position: refs/heads/master@{#308923}

--- tools/perf/benchmarks/smoothness.py
+++ tools/perf/benchmarks/smoothness.py
@@ -47,7 +47,7 @@ class SmoothnessKeyMobileSites(benchmark.Benchmark):
 
   http://www.chromium.org/developers/design-documents/rendering-benchmarks"""
   test = smoothness.Smoothness
-  page_set = page_sets.KeyMobileSitesSmoothPageSet
+  page_set = page_sets.KeyMobileSitesPageSet
 
 
 class SmoothnessToughAnimationCases(benchmark.Benchmark):
@@ -82,7 +82,7 @@ class SmoothnessGpuRasterizationKeyMobileSites(benchmark.Benchmark):
   """
   tag = 'gpu_rasterization'
   test = smoothness.Smoothness
-  page_set = page_sets.KeyMobileSitesSmoothPageSet
+  page_set = page_sets.KeyMobileSitesPageSet
   def CustomizeBrowserOptions(self, options):
     silk_flags.CustomizeBrowserOptionsForGpuRasterization(options)
 
@@ -94,7 +94,7 @@ class SmoothnessSyncScrollKeyMobileSites(benchmark.Benchmark):
   """
   tag = 'sync_scroll'
   test = smoothness.Smoothness
-  page_set = page_sets.KeyMobileSitesSmoothPageSet
+  page_set = page_sets.KeyMobileSitesPageSet
   def CustomizeBrowserOptions(self, options):
     silk_flags.CustomizeBrowserOptionsForSyncScrolling(options)
 
