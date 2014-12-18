5c5e90dc4fd3713821f4da2c00d2ad0f17d968d6
chrishenry <chrishenry@google.com>
2014-12-17 18:01:21 -0800

    Move max_failures option from PageTest to benchmark.
    BUG=440101
    
    Review URL: https://codereview.chromium.org/790673005
    
    Cr-Commit-Position: refs/heads/master@{#308924}

--- content/test/gpu/gpu_tests/webgl_conformance.py
+++ content/test/gpu/gpu_tests/webgl_conformance.py
@@ -67,7 +67,7 @@ def _WebGLTestMessages(tab):
 
 class WebglConformanceValidator(page_test.PageTest):
   def __init__(self):
-    super(WebglConformanceValidator, self).__init__(max_failures=10)
+    super(WebglConformanceValidator, self).__init__()
 
   def ValidateAndMeasurePage(self, page, tab, results):
     if not _DidWebGLTestSucceed(tab):
@@ -99,6 +99,9 @@ class WebglConformancePage(page_module.Page):
 class WebglConformance(benchmark_module.Benchmark):
   """Conformance with Khronos WebGL Conformance Tests"""
   test = WebglConformanceValidator
+
+  def __init__(self):
+    super(WebglConformance, self).__init__(max_failures=10)
 
   @classmethod
   def AddBenchmarkCommandLineArgs(cls, group):
