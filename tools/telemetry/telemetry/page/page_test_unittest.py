a14d8773ffcee8314f6719ecbd52bb023fd3546f
slamm <slamm@chromium.org>
2014-12-17 23:56:46 -0800

    [Telemetry] Remove test references from user_story_runner.
    Move is_multi_tab_test down to shared_page_state.
    Remove test.RequestExit and test.IsExiting.
    
    BUG=440101
    
    Review URL: https://codereview.chromium.org/801773004
    
    Cr-Commit-Position: refs/heads/master@{#308964}

--- tools/telemetry/telemetry/page/page_test_unittest.py
+++ tools/telemetry/telemetry/page/page_test_unittest.py
@@ -5,9 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import json
 import os
+import unittest
 
 from telemetry import decorators
-from telemetry.core import exceptions
 from telemetry.core import wpr_modes
 from telemetry.page import page as page_module
 from telemetry.page import page_set
@@ -149,3 +149,32 @@ class PageTestUnitTest(page_test_test_case.PageTestTestCase):
     measurement = PageTestWithAction()
     self.RunMeasurement(measurement, ps, options=self._options)
     self.assertTrue(page.run_test_action_called)
+
+
+class MultiTabPageTestUnitTest(unittest.TestCase):
+  def testNoTabForPageReturnsFalse(self):
+    class PageTestWithoutTabForPage(page_test.PageTest):
+      def ValidateAndMeasurePage(self, *_):
+        pass
+    test = PageTestWithoutTabForPage()
+    self.assertFalse(test.is_multi_tab_test)
+
+  def testHasTabForPageReturnsTrue(self):
+    class PageTestWithTabForPage(page_test.PageTest):
+      def ValidateAndMeasurePage(self, *_):
+        pass
+      def TabForPage(self, *_):
+        pass
+    test = PageTestWithTabForPage()
+    self.assertTrue(test.is_multi_tab_test)
+
+  def testHasTabForPageInAncestor(self):
+    class PageTestWithTabForPage(page_test.PageTest):
+      def ValidateAndMeasurePage(self, *_):
+        pass
+      def TabForPage(self, *_):
+        pass
+    class PageTestWithTabForPageInParent(PageTestWithTabForPage):
+      pass
+    test = PageTestWithTabForPageInParent()
+    self.assertTrue(test.is_multi_tab_test)
