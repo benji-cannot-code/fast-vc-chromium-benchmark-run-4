aa136ef230c107c62367b26c5c39d2105ccb7bcb
reed@google.com <reed@google.com>
2014-12-18 16:42:28 +0000

    NewRasterPMColor -> NewRasterN32Premul
    refactoring to remove deprecated api
    
    BUG=
    
    Review URL: https://codereview.chromium.org/810263002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187475 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/Canvas2DLayerManagerTest.cpp
+++ third_party/WebKit/Source/platform/graphics/Canvas2DLayerManagerTest.cpp
@@ -137,7 +137,7 @@ protected:
         manager.init(10, 10);
         {
             OwnPtr<MockWebGraphicsContext3D> webContext = adoptPtr(new MockWebGraphicsContext3D);
-            RefPtr<SkSurface> surface1 = adoptRef(SkSurface::NewRasterPMColor(1, 1));
+            RefPtr<SkSurface> surface1 = adoptRef(SkSurface::NewRasterN32Premul(1, 1));
             OwnPtr<SkDeferredCanvas> canvas1 = adoptPtr(SkDeferredCanvas::Create(surface1.get()));
             FakeCanvas2DLayerBridgePtr layer1(adoptRef(new FakeCanvas2DLayerBridge(webContext.get(), canvas1.release(), surface1.release())));
             EXPECT_EQ((size_t)0, manager.m_bytesAllocated);
@@ -150,7 +150,7 @@ protected:
             layer1->storageAllocatedForRecordingChanged(1);
             EXPECT_EQ((size_t)1, manager.m_bytesAllocated);
             {
-                RefPtr<SkSurface> surface2 = adoptRef(SkSurface::NewRasterPMColor(1, 1));
+                RefPtr<SkSurface> surface2 = adoptRef(SkSurface::NewRasterN32Premul(1, 1));
                 OwnPtr<SkDeferredCanvas> canvas2 = adoptPtr(SkDeferredCanvas::Create(surface2.get()));
                 FakeCanvas2DLayerBridgePtr layer2(adoptRef(new FakeCanvas2DLayerBridge(webContext.get(), canvas2.release(), surface2.release())));
                 EXPECT_EQ((size_t)1, manager.m_bytesAllocated);
@@ -168,7 +168,7 @@ protected:
         OwnPtr<MockWebGraphicsContext3D> webContext = adoptPtr(new MockWebGraphicsContext3D);
         Canvas2DLayerManager& manager = Canvas2DLayerManager::get();
         manager.init(10, 5);
-        RefPtr<SkSurface> surface = adoptRef(SkSurface::NewRasterPMColor(1, 1));
+        RefPtr<SkSurface> surface = adoptRef(SkSurface::NewRasterN32Premul(1, 1));
         OwnPtr<SkDeferredCanvas> canvas = adoptPtr(SkDeferredCanvas::Create(surface.get()));
         FakeCanvas2DLayerBridgePtr layer(adoptRef(new FakeCanvas2DLayerBridge(webContext.get(), canvas.release(), surface.release())));
         layer->fakeFreeableBytes(10);
@@ -186,7 +186,7 @@ protected:
         OwnPtr<MockWebGraphicsContext3D> webContext = adoptPtr(new MockWebGraphicsContext3D);
         Canvas2DLayerManager& manager = Canvas2DLayerManager::get();
         manager.init(20, 5);
-        RefPtr<SkSurface> surface = adoptRef(SkSurface::NewRasterPMColor(1, 1));
+        RefPtr<SkSurface> surface = adoptRef(SkSurface::NewRasterN32Premul(1, 1));
         OwnPtr<SkDeferredCanvas> canvas = adoptPtr(SkDeferredCanvas::Create(surface.get()));
         FakeCanvas2DLayerBridgePtr layer(adoptRef(new FakeCanvas2DLayerBridge(webContext.get(), canvas.release(), surface.release())));
         layer->fakeFreeableBytes(5);
@@ -206,7 +206,7 @@ protected:
         OwnPtr<MockWebGraphicsContext3D> webContext = adoptPtr(new MockWebGraphicsContext3D);
         Canvas2DLayerManager& manager = Canvas2DLayerManager::get();
         manager.init(10, 5);
-        RefPtr<SkSurface> surface = adoptRef(SkSurface::NewRasterPMColor(1, 1));
+        RefPtr<SkSurface> surface = adoptRef(SkSurface::NewRasterN32Premul(1, 1));
         OwnPtr<SkDeferredCanvas> canvas = adoptPtr(SkDeferredCanvas::Create(surface.get()));
         FakeCanvas2DLayerBridgePtr layer(adoptRef(new FakeCanvas2DLayerBridge(webContext.get(), canvas.release(), surface.release())));
         EXPECT_FALSE(manager.isInList(layer.get()));
@@ -221,7 +221,7 @@ protected:
         OwnPtr<MockWebGraphicsContext3D> webContext = adoptPtr(new MockWebGraphicsContext3D);
         Canvas2DLayerManager& manager = Canvas2DLayerManager::get();
         manager.init(10, 5);
-        RefPtr<SkSurface> surface = adoptRef(SkSurface::NewRasterPMColor(1, 1));
+        RefPtr<SkSurface> surface = adoptRef(SkSurface::NewRasterN32Premul(1, 1));
         OwnPtr<SkDeferredCanvas> canvas = adoptPtr(SkDeferredCanvas::Create(surface.get()));
         FakeCanvas2DLayerBridgePtr layer(adoptRef(new FakeCanvas2DLayerBridge(webContext.get(), canvas.release(), surface.release())));
         layer->fakeFreeableBytes(1); // Not enough freeable bytes, will cause aggressive eviction by flushing
@@ -272,7 +272,7 @@ protected:
     {
         OwnPtr<MockWebGraphicsContext3D> webContext = adoptPtr(new MockWebGraphicsContext3D);
         Canvas2DLayerManager::get().init(10, 10);
-        RefPtr<SkSurface> surface = adoptRef(SkSurface::NewRasterPMColor(1, 1));
+        RefPtr<SkSurface> surface = adoptRef(SkSurface::NewRasterN32Premul(1, 1));
         OwnPtr<SkDeferredCanvas> canvas = adoptPtr(SkDeferredCanvas::Create(surface.get()));
         FakeCanvas2DLayerBridgePtr layer(adoptRef(new FakeCanvas2DLayerBridge(webContext.get(), canvas.release(), surface.release())));
         Platform::current()->currentThread()->postTask(new DeferredFrameTestTask(this, layer.get(), true));
