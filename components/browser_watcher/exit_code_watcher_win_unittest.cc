ad71f4c215b50ea06aab2ab52b39cda345f80fb9
siggi <siggi@chromium.org>
2014-12-19 07:20:51 -0800

    Add a console control handler chrome_watcher.dll.
    This will hopefully allow conclusively answering whether the problem is
    at ExitWindows-time.
    
    R=erikwright@chromium.org, mpearson@chromium.org
    BUG=412384
    
    Review URL: https://codereview.chromium.org/811603003
    
    Cr-Commit-Position: refs/heads/master@{#309201}

--- components/browser_watcher/exit_code_watcher_win_unittest.cc
+++ components/browser_watcher/exit_code_watcher_win_unittest.cc
@@ -221,6 +221,7 @@ TEST_F(BrowserWatcherTest, ExitCodeWatcherOnExitedProcess) {
   ASSERT_NO_FATAL_FAILURE(sleeper.Kill(kExitCode, true));
 
   watcher.WaitForExit();
+  EXPECT_EQ(kExitCode, watcher.exit_code());
 
   VerifyWroteExitCode(sleeper.process().pid(), kExitCode);
 }
