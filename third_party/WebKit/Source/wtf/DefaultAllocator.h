9e53828e17030663e2357788a622672656c6ceba
keishi@chromium.org <keishi@chromium.org>
2014-12-19 08:16:45 +0000

    Oilpan: Expand HashTableBacking buffer when possible
    If we can expand the tail of the backing storage to the new capacity, we just expand the storage instead of reallocating the storage.
    
    Unlike expanding the vector backing storage, we pay some overhead because we need to allocate a temporary buffer to copy the data and rehash from there. But this still improves performance because we pack the objects more densely thus reducing the number of cache lines the CPU needs to fetch. And also decreasing the area GC needs to sweep.
    
    BUG=420515
    
    Review URL: https://codereview.chromium.org/776813003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187516 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/DefaultAllocator.h
+++ third_party/WebKit/Source/wtf/DefaultAllocator.h
@@ -106,6 +106,11 @@ public:
     {
         return reinterpret_cast<Return>(fastMalloc(size));
     }
+
+    WTF_EXPORT static inline bool expandHashTableBacking(void*, size_t)
+    {
+        return false;
+    }
     static void free(void* address)
     {
         fastFree(address);
