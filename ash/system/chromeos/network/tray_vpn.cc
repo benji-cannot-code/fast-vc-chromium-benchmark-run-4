4109634093993dd1d55e9b3775e8d848211b8dac
Reilly Grant <reillyg@chromium.org>
2014-12-18 16:16:19 -0800

    Revert "Add logging for slow device events, limit network UI update rate"
    This reverts commit 9cfc3f8f868ec7c38f1e5025554c0caf837750a5.
    
    A number of interactive_ui_tests where broken by this change:
    http://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Tests%20%281%29/builds/53716
    
    BUG=
    TBR=satorux@chromium.org,oshima@chromium.org,stevenjb@chromium.org
    
    Review URL: https://codereview.chromium.org/815023002
    
    Cr-Commit-Position: refs/heads/master@{#309111}

--- ash/system/chromeos/network/tray_vpn.cc
+++ ash/system/chromeos/network/tray_vpn.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/tray/tray_constants.h"
 #include "ash/system/tray/tray_item_more.h"
 #include "ash/system/tray/tray_popup_label_button.h"
-#include "chromeos/device_event_log.h"
 #include "chromeos/network/network_state.h"
 #include "chromeos/network/network_state_handler.h"
 #include "grit/ash_strings.h"
@@ -40,7 +39,7 @@ class VpnDefaultView : public TrayItemMore,
     Update();
   }
 
-  ~VpnDefaultView() override {
+  virtual ~VpnDefaultView() {
     ui::network_icon::NetworkIconAnimation::GetInstance()->RemoveObserver(this);
   }
 
@@ -69,7 +68,9 @@ class VpnDefaultView : public TrayItemMore,
   }
 
   // ui::network_icon::AnimationObserver
-  void NetworkIconChanged() override { Update(); }
+  virtual void NetworkIconChanged() override {
+    Update();
+  }
 
  private:
   void GetNetworkStateHandlerImageAndLabel(gfx::ImageSkia* image,
@@ -83,8 +84,8 @@ class VpnDefaultView : public TrayItemMore,
       *image = ui::network_icon::GetImageForDisconnectedNetwork(
           ui::network_icon::ICON_TYPE_DEFAULT_VIEW, shill::kTypeVPN);
       if (label) {
-        *label =
-            l10n_util::GetStringUTF16(IDS_ASH_STATUS_TRAY_VPN_DISCONNECTED);
+        *label = l10n_util::GetStringUTF16(
+            IDS_ASH_STATUS_TRAY_VPN_DISCONNECTED);
       }
       *animating = false;
       return;
@@ -104,7 +105,9 @@ class VpnDefaultView : public TrayItemMore,
 }  // namespace tray
 
 TrayVPN::TrayVPN(SystemTray* system_tray)
-    : SystemTrayItem(system_tray), default_(NULL), detailed_(NULL) {
+    : SystemTrayItem(system_tray),
+      default_(NULL),
+      detailed_(NULL) {
   network_state_observer_.reset(new TrayNetworkStateObserver(this));
 }
 
@@ -164,11 +167,20 @@ void TrayVPN::UpdateAfterLoginStatusChange(user::LoginStatus status) {
 void TrayVPN::UpdateAfterShelfAlignmentChange(ShelfAlignment alignment) {
 }
 
-void TrayVPN::NetworkStateChanged() {
+void TrayVPN::NetworkStateChanged(bool list_changed) {
   if (default_)
     default_->Update();
+  if (detailed_) {
+    if (list_changed)
+      detailed_->NetworkListChanged();
+    else
+      detailed_->ManagerChanged();
+  }
+}
+
+void TrayVPN::NetworkServiceChanged(const chromeos::NetworkState* network) {
   if (detailed_)
-    detailed_->Update();
+    detailed_->NetworkServiceChanged(network);
 }
 
 }  // namespace ash
--- chromeos/network/network_state_handler.cc
+++ chromeos/network/network_state_handler.cc
@@ -543,7 +543,6 @@ void NetworkStateHandler::UpdateNetworkServiceProperty(
     const std::string& service_path,
     const std::string& key,
     const base::Value& value) {
-  SCOPED_NET_LOG_IF_SLOW();
   bool changed = false;
   NetworkState* network = GetModifiableNetworkState(service_path);
   if (!network)
@@ -603,7 +602,6 @@ void NetworkStateHandler::UpdateNetworkServiceProperty(
 void NetworkStateHandler::UpdateDeviceProperty(const std::string& device_path,
                                                const std::string& key,
                                                const base::Value& value) {
-  SCOPED_NET_LOG_IF_SLOW();
   DeviceState* device = GetModifiableDeviceState(device_path);
   if (!device)
     return;
@@ -681,7 +679,6 @@ void NetworkStateHandler::TechnologyListChanged() {
 
 void NetworkStateHandler::ManagedStateListChanged(
     ManagedState::ManagedType type) {
-  SCOPED_NET_LOG_IF_SLOW();
   if (type == ManagedState::MANAGED_TYPE_NETWORK) {
     SortNetworkList();
     UpdateNetworkStats();
@@ -821,7 +818,6 @@ void NetworkStateHandler::UpdateGuid(NetworkState* network) {
 }
 
 void NetworkStateHandler::NotifyDeviceListChanged() {
-  SCOPED_NET_LOG_IF_SLOW();
   NET_LOG_DEBUG("NOTIFY:DeviceListChanged",
                 base::StringPrintf("Size:%" PRIuS, device_list_.size()));
   FOR_EACH_OBSERVER(NetworkStateHandlerObserver, observers_,
@@ -871,7 +867,6 @@ NetworkStateHandler::ManagedStateList* NetworkStateHandler::GetManagedList(
 
 void NetworkStateHandler::OnNetworkConnectionStateChanged(
     NetworkState* network) {
-  SCOPED_NET_LOG_IF_SLOW();
   DCHECK(network);
   std::string event = "NetworkConnectionStateChanged";
   if (network->path() == default_network_path_) {
@@ -895,7 +890,6 @@ void NetworkStateHandler::OnNetworkConnectionStateChanged(
 
 void NetworkStateHandler::NotifyDefaultNetworkChanged(
     const NetworkState* default_network) {
-  SCOPED_NET_LOG_IF_SLOW();
   NET_LOG_EVENT("NOTIFY:DefaultNetworkChanged", GetLogName(default_network));
   FOR_EACH_OBSERVER(NetworkStateHandlerObserver, observers_,
                     DefaultNetworkChanged(default_network));
@@ -903,7 +897,6 @@ void NetworkStateHandler::NotifyDefaultNetworkChanged(
 
 void NetworkStateHandler::NotifyNetworkPropertiesUpdated(
     const NetworkState* network) {
-  SCOPED_NET_LOG_IF_SLOW();
   NET_LOG_DEBUG("NOTIFY:NetworkPropertiesUpdated", GetLogName(network));
   FOR_EACH_OBSERVER(NetworkStateHandlerObserver, observers_,
                     NetworkPropertiesUpdated(network));
@@ -911,14 +904,12 @@ void NetworkStateHandler::NotifyNetworkPropertiesUpdated(
 
 void NetworkStateHandler::NotifyDevicePropertiesUpdated(
     const DeviceState* device) {
-  SCOPED_NET_LOG_IF_SLOW();
   NET_LOG_DEBUG("NOTIFY:DevicePropertiesUpdated", GetLogName(device));
   FOR_EACH_OBSERVER(NetworkStateHandlerObserver, observers_,
                     DevicePropertiesUpdated(device));
 }
 
 void NetworkStateHandler::NotifyScanCompleted(const DeviceState* device) {
-  SCOPED_NET_LOG_IF_SLOW();
   NET_LOG_DEBUG("NOTIFY:ScanCompleted", GetLogName(device));
   FOR_EACH_OBSERVER(NetworkStateHandlerObserver, observers_,
                     ScanCompleted(device));
