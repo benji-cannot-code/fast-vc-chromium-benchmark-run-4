81d7923458a2d194b7ede3898a0aa63df5460f72
jrummell <jrummell@chromium.org>
2014-12-19 14:20:27 -0800

    Remove GetUsableKeyIds()
    As this was never called from blink, and is no longer part of the EME
    spec, removing this functionality.
    
    BUG=428384
    TEST=existing EME tests pass
    
    Review URL: https://codereview.chromium.org/814083002
    
    Cr-Commit-Position: refs/heads/master@{#309279}

--- media/mojo/services/mojo_cdm_service.cc
+++ media/mojo/services/mojo_cdm_service.cc
@@ -98,15 +98,6 @@ void MojoCdmService::RemoveSession(
       scoped_ptr<SimpleCdmPromise>(new SimpleMojoCdmPromise(callback)));
 }
 
-void MojoCdmService::GetUsableKeyIds(
-    const mojo::String& session_id,
-    const mojo::Callback<void(mojo::CdmPromiseResultPtr,
-                              mojo::Array<mojo::Array<uint8_t>>)>& callback) {
-  cdm_->GetUsableKeyIds(
-      session_id.To<std::string>(),
-      scoped_ptr<KeyIdsPromise>(new KeyIdsMojoCdmPromise(callback)));
-}
-
 void MojoCdmService::GetCdmContext(
     int32_t cdm_id,
     mojo::InterfaceRequest<mojo::Decryptor> decryptor) {
