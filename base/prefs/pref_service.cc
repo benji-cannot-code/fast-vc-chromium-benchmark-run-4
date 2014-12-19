06f00aec7538f25a641eac2d861a1576cd0b8129
khannans <khannans@chromium.org>
2014-12-19 08:04:25 -0800

    Allow managing Camera and Location for Supervised Users
    BUG=440721
    
    Review URL: https://codereview.chromium.org/789173002
    
    Cr-Commit-Position: refs/heads/master@{#309208}

--- base/prefs/pref_service.cc
+++ base/prefs/pref_service.cc
@@ -241,6 +241,12 @@ bool PrefService::IsManagedPreference(const std::string& pref_name) const {
   return pref && pref->IsManaged();
 }
 
+bool PrefService::IsPreferenceManagedByCustodian(
+    const std::string& pref_name) const {
+  const Preference* pref = FindPreference(pref_name);
+  return pref && pref->IsManagedByCustodian();
+}
+
 bool PrefService::IsUserModifiablePreference(
     const std::string& pref_name) const {
   const Preference* pref = FindPreference(pref_name);
@@ -516,6 +522,10 @@ const base::Value* PrefService::Preference::GetRecommendedValue() const {
 
 bool PrefService::Preference::IsManaged() const {
   return pref_value_store()->PrefValueInManagedStore(name_);
+}
+
+bool PrefService::Preference::IsManagedByCustodian() const {
+  return pref_value_store()->PrefValueInSupervisedStore(name_.c_str());
 }
 
 bool PrefService::Preference::IsRecommended() const {
