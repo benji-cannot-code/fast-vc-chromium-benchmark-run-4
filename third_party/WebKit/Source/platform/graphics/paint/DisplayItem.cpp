8f7eeda4bcf0e82582ae7b42b6e27a88e2f16a4c
chrishtr@chromium.org <chrishtr@chromium.org>
2014-12-19 20:06:58 +0000

    Add a drawing recorder for column rules.
    BUG=441547
    
    Review URL: https://codereview.chromium.org/801643008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187558 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/paint/DisplayItem.cpp
+++ third_party/WebKit/Source/platform/graphics/paint/DisplayItem.cpp
@@ -59,6 +59,7 @@ WTF::String DisplayItem::typeAsDebugString(DisplayItem::Type type)
     case ScrollbarCorner: return "ScrollbarCorner";
     case Scrollbar: return "Scrollbar";
     case Resizer: return "Resizer";
+    case ColumnRules: return "ColumnRules";
     }
     ASSERT_NOT_REACHED();
     return "Unknown";
