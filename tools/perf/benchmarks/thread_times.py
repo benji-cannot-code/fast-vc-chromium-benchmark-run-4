53be858aa320cbefe847eded06d4ed0318142901
fmeawad <fmeawad@chromium.org>
2014-12-18 16:08:51 -0800

    Disable thread_times.tough_compositor_cases on windows
    Very flaky on windows perf bots.
    
    BUG=443781
    
    Review URL: https://codereview.chromium.org/790073003
    
    Cr-Commit-Position: refs/heads/master@{#309109}

--- tools/perf/benchmarks/thread_times.py
+++ tools/perf/benchmarks/thread_times.py
@@ -54,6 +54,7 @@ class ThreadTimesSimpleMobileSites(_ThreadTimes):
   page_set = page_sets.SimpleMobileSitesPageSet
 
 
+@benchmark.Disabled('win') # crbug.com/443781
 class ThreadTimesCompositorCases(_ThreadTimes):
   """Measures timeline metrics while performing smoothness action on
   tough compositor cases, using software rasterization.
