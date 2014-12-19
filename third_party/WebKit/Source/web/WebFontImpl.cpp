85e4aafd42cf9cec4d11802ab779f81f56bc42fb
tfarina@chromium.org <tfarina@chromium.org>
2014-12-19 14:24:08 +0000

    Cleanup: Remove unused skia/ext includes from web files.
    BUG=13714
    TEST=compiles
    R=peter@chromium.org
    TBR=jochen@chromium.org
    
    Review URL: https://codereview.chromium.org/819483003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187533 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebFontImpl.cpp
+++ third_party/WebKit/Source/web/WebFontImpl.cpp
@@ -41,7 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "public/platform/WebRect.h"
 #include "public/web/WebFontDescription.h"
 #include "public/web/WebTextRun.h"
-#include <skia/ext/platform_canvas.h>
 
 namespace blink {
 
--- third_party/WebKit/Source/web/WebPopupMenuImpl.cpp
+++ third_party/WebKit/Source/web/WebPopupMenuImpl.cpp
@@ -55,7 +55,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "web/PopupContainer.h"
 #include "web/PopupMenuChromium.h"
 #include "web/WebInputEventConversion.h"
-#include <skia/ext/platform_canvas.h>
 
 namespace blink {
 
