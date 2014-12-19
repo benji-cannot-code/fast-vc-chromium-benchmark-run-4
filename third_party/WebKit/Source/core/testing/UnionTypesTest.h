acebac9ac5ae3ba9007f18625badc1c28c92b245
jsbell@chromium.org <jsbell@chromium.org>
2014-12-19 05:25:09 +0000

    IDL: Support sequence/array types in Unions
    Introduce support for sequences within unions,
    e.g. (DOMString or sequence<DOMString>), as
    seen in such specs as Indexed DB and Fetch.
    
    Currently such cases need to be handled by using method
    overloads or untyped Dictionary.
    
    R=haraken@chromium.org,bashi@chromium.org
    BUG=240176
    
    Review URL: https://codereview.chromium.org/812933002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187507 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/testing/UnionTypesTest.h
+++ third_party/WebKit/Source/core/testing/UnionTypesTest.h
@@ -20,8 +20,8 @@ public:
     }
     virtual ~UnionTypesTest() { }
 
-    void doubleOrStringAttribute(DoubleOrString&);
-    void setDoubleOrStringAttribute(const DoubleOrString&);
+    void doubleOrStringOrStringArrayAttribute(DoubleOrStringOrStringArray&);
+    void setDoubleOrStringOrStringArrayAttribute(const DoubleOrStringOrStringArray&);
 
     String doubleOrStringArg(DoubleOrString&);
     String doubleOrStringArrayArg(Vector<DoubleOrString>&);
@@ -29,6 +29,9 @@ public:
 
     String nodeListOrElementArg(NodeListOrElement&);
     String nodeListOrElementOrNullArg(NodeListOrElement&);
+
+    String doubleOrStringOrStringArrayArg(const DoubleOrStringOrStringArray&);
+    String doubleOrStringOrStringSequenceArg(const DoubleOrStringOrStringSequence&);
 
     void trace(Visitor*) { }
 
@@ -42,10 +45,12 @@ private:
         SpecificTypeNone,
         SpecificTypeDouble,
         SpecificTypeString,
+        SpecificTypeStringArray,
     };
     AttributeSpecificType m_attributeType;
     double m_attributeDouble;
     String m_attributeString;
+    Vector<String> m_attributeStringArray;
 };
 
 } // namespace blink
