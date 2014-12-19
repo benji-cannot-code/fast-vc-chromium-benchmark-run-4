ccd342771f5a01f595cd5f87af90e823da4fe208
alexst <alexst@chromium.org>
2014-12-19 15:37:10 -0800

    Ozone interface from VaapiVideoAccelerator patch in issue 490233002
    http://crrev.com/490233002
    
    Landing approved and reviewed changes to simplify the original patch.
    
    BUG=403058
    
    Review URL: https://codereview.chromium.org/802533004
    
    Cr-Commit-Position: refs/heads/master@{#309293}

--- ui/ozone/platform/dri/gbm_surface_factory.cc
+++ ui/ozone/platform/dri/gbm_surface_factory.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/common/egl_util.h"
 #include "ui/ozone/platform/dri/dri_window_delegate_impl.h"
 #include "ui/ozone/platform/dri/dri_window_delegate_manager.h"
+#include "ui/ozone/platform/dri/dri_wrapper.h"
 #include "ui/ozone/platform/dri/gbm_buffer.h"
 #include "ui/ozone/platform/dri/gbm_surface.h"
 #include "ui/ozone/platform/dri/gbm_surfaceless.h"
@@ -88,6 +89,11 @@ void GbmSurfaceFactory::InitializeGpu(
 
 intptr_t GbmSurfaceFactory::GetNativeDisplay() {
   return reinterpret_cast<intptr_t>(device_);
+}
+
+int GbmSurfaceFactory::GetDrmFd() {
+  DCHECK(drm_);
+  return drm_->get_fd();
 }
 
 const int32* GbmSurfaceFactory::GetEGLSurfaceProperties(
