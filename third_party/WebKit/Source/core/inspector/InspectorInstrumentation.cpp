6bd67e50871e8ffeb899e180e51604fadee8f3f9
zhaoze.zhou@partner.samsung.com <zhaoze.zhou@partner.samsung.com>
2014-12-19 17:07:01 +0000

    Use C++11 range-based for loop in Source/core/inspector
    BUG=None
    
    Review URL: https://codereview.chromium.org/800113002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187546 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorInstrumentation.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorInstrumentation.cpp
@@ -119,9 +119,7 @@ void willDestroyResourceImpl(Resource* cachedResource)
 {
     if (!instrumentingAgentsSet)
         return;
-    HashSet<InstrumentingAgents*>::iterator end = instrumentingAgentsSet->end();
-    for (HashSet<InstrumentingAgents*>::iterator it = instrumentingAgentsSet->begin(); it != end; ++it) {
-        InstrumentingAgents* instrumentingAgents = *it;
+    for (InstrumentingAgents* instrumentingAgents: *instrumentingAgentsSet) {
         if (InspectorResourceAgent* inspectorResourceAgent = instrumentingAgents->inspectorResourceAgent())
             inspectorResourceAgent->willDestroyResource(cachedResource);
     }
