566b18446c26ff9be136ba004206c822e09b6446
dnicoara <dnicoara@chromium.org>
2014-12-18 18:09:11 -0800

    [Ozone-Evdev] Fix clang warnings
    BUG=none
    
    Review URL: https://codereview.chromium.org/814523003
    
    Cr-Commit-Position: refs/heads/master@{#309127}

--- ui/events/ozone/evdev/libgestures_glue/gesture_timer_provider.cc
+++ ui/events/ozone/evdev/libgestures_glue/gesture_timer_provider.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/timer/timer.h"
 
 // libgestures requires that this be in the top level namespace.
-class GesturesTimer {
+struct GesturesTimer {
  public:
   GesturesTimer() : callback_(NULL), callback_data_(NULL) {}
   ~GesturesTimer() {}
