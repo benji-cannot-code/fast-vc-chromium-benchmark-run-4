817bee0e847770e33241d275511db9979e7194db
kouhei@chromium.org <kouhei@chromium.org>
2014-12-19 11:06:12 +0000

    Implement InlinedGlobalMarkingVisitor
    This CL introduces inlined version of MarkingVisitor<GlobalMarking>.
    InlinedGlobalMarkingVisitor emulates an Visitor*, but all time critial
    methods are inlined.
    
    This CL doesn't yet enable use of InlinedGlobalMarkingVisitor.
    InlinedGlobalMarkingVisitor is to be enabled in future CLs.
    
    Most of the existing implementation of MarkingVisitor is
    moved to MarkingVisitorImpl.h, so that implementation is
    shared between InlinedGlobalMarkingVisitor and MarkingVisitor<>.
    
    BUG=420515
    
    Review URL: https://codereview.chromium.org/816563002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187521 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/heap/Handle.h
+++ third_party/WebKit/Source/platform/heap/Handle.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define Handle_h
 
 #include "platform/heap/Heap.h"
+#include "platform/heap/InlinedGlobalMarkingVisitor.h"
 #include "platform/heap/ThreadState.h"
 #include "platform/heap/Visitor.h"
 #include "wtf/Functional.h"
