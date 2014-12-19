a18a6a3fc4dde60c9894a5827b547a5430d754c5
tfarina@chromium.org <tfarina@chromium.org>
2014-12-19 05:48:07 +0000

    Cleanup: Remove skia includes that are not being used in ImageFrameGenerator.cpp.
    BUG=13714
    TEST=compiles
    R=tomhudson@chromium.org
    TBR=senorblanco@chromium.org
    
    Review URL: https://codereview.chromium.org/805413005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187511 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/ImageFrameGenerator.cpp
+++ third_party/WebKit/Source/platform/graphics/ImageFrameGenerator.cpp
@@ -33,9 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/graphics/ImageDecodingStore.h"
 #include "platform/image-decoders/ImageDecoder.h"
 
-#include "skia/ext/image_operations.h"
-#include "third_party/skia/include/core/SkMallocPixelRef.h"
-
 namespace blink {
 
 // Creates a SkPixelRef such that the memory for pixels is given by an external body.
