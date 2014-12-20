8fd11d7448dc093ebb8d6f70e58a622348690d99
jrummell <jrummell@chromium.org>
2014-12-19 16:38:53 -0800

    Remove EME Ready event
    The event is no longer generated by CDMs and is not part of the spec
    anymore.
    
    The prefixed API (old EME v0.1b) still uses the event, so it continues
    to be simulated (generated after LoadSession() and AddKey() calls)
    for users using that API.
    
    BUG=428384
    TEST=existing EME tests pass
    
    Review URL: https://codereview.chromium.org/814063003
    
    Cr-Commit-Position: refs/heads/master@{#309312}

--- content/renderer/media/crypto/proxy_media_keys.h
+++ content/renderer/media/crypto/proxy_media_keys.h
@@ -30,7 +30,6 @@ class ProxyMediaKeys : public media::MediaKeys, public media::CdmContext {
       const GURL& security_origin,
       RendererCdmManager* manager,
       const media::SessionMessageCB& session_message_cb,
-      const media::SessionReadyCB& session_ready_cb,
       const media::SessionClosedCB& session_closed_cb,
       const media::SessionErrorCB& session_error_cb,
       const media::SessionKeysChangeCB& session_keys_change_cb,
@@ -90,7 +89,6 @@ class ProxyMediaKeys : public media::MediaKeys, public media::CdmContext {
 
   ProxyMediaKeys(RendererCdmManager* manager,
                  const media::SessionMessageCB& session_message_cb,
-                 const media::SessionReadyCB& session_ready_cb,
                  const media::SessionClosedCB& session_closed_cb,
                  const media::SessionErrorCB& session_error_cb);
 
@@ -117,7 +115,6 @@ class ProxyMediaKeys : public media::MediaKeys, public media::CdmContext {
   int cdm_id_;
 
   media::SessionMessageCB session_message_cb_;
-  media::SessionReadyCB session_ready_cb_;
   media::SessionClosedCB session_closed_cb_;
   media::SessionErrorCB session_error_cb_;
 
--- content/renderer/pepper/content_decryptor_delegate.cc
+++ content/renderer/pepper/content_decryptor_delegate.cc
@@ -324,7 +324,6 @@ ContentDecryptorDelegate::~ContentDecryptorDelegate() {
 void ContentDecryptorDelegate::Initialize(
     const std::string& key_system,
     const media::SessionMessageCB& session_message_cb,
-    const media::SessionReadyCB& session_ready_cb,
     const media::SessionClosedCB& session_closed_cb,
     const media::SessionErrorCB& session_error_cb,
     const media::SessionKeysChangeCB& session_keys_change_cb,
@@ -335,7 +334,6 @@ void ContentDecryptorDelegate::Initialize(
   key_system_ = key_system;
 
   session_message_cb_ = session_message_cb;
-  session_ready_cb_ = session_ready_cb;
   session_closed_cb_ = session_closed_cb;
   session_error_cb_ = session_error_cb;
   session_keys_change_cb_ = session_keys_change_cb;
@@ -799,13 +797,9 @@ void ContentDecryptorDelegate::OnSessionExpirationChange(
 }
 
 void ContentDecryptorDelegate::OnSessionReady(PP_Var web_session_id) {
-  if (session_ready_cb_.is_null())
-    return;
-
-  StringVar* web_session_id_string = StringVar::FromPPVar(web_session_id);
-  DCHECK(web_session_id_string);
-
-  session_ready_cb_.Run(web_session_id_string->value());
+  // Ready events no longer generated.
+  // TODO(jrummell): Remove event from Pepper.
+  NOTREACHED();
 }
 
 void ContentDecryptorDelegate::OnSessionClosed(PP_Var web_session_id) {
