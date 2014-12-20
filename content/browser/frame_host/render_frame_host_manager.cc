9df244a449327f28544a80323ba1b3f3129619de
nick <nick@chromium.org>
2014-12-19 16:16:41 -0800

    Call RenderFrameCreated even for swapped-out RenderFrameHosts.
    Fix bug 438951.
    
    BUG=438951
    
    Review URL: https://codereview.chromium.org/818643002
    
    Cr-Commit-Position: refs/heads/master@{#309304}

--- content/browser/frame_host/render_frame_host_manager.cc
+++ content/browser/frame_host/render_frame_host_manager.cc
@@ -1123,9 +1123,8 @@ scoped_ptr<RenderFrameHostImpl> RenderFrameHostManager::CreateRenderFrame(
 
   // TODO(nasko): Remove the following CHECK once cross-site navigation no
   // longer relies on swapped out RFH for the top-level frame.
-  if (!frame_tree_node_->IsMainFrame()) {
+  if (!frame_tree_node_->IsMainFrame())
     CHECK(!swapped_out);
-  }
 
   scoped_ptr<RenderFrameHostImpl> new_render_frame_host;
   bool success = true;
@@ -1183,8 +1182,7 @@ scoped_ptr<RenderFrameHostImpl> RenderFrameHostManager::CreateRenderFrame(
           new_render_frame_host->GetSiteInstance(), frame_tree_node_);
       proxy_hosts_[instance->GetId()] = proxy;
       proxy_routing_id = proxy->GetRoutingID();
-      if (frame_tree_node_->IsMainFrame())
-        proxy->TakeFrameHostOwnership(new_render_frame_host.Pass());
+      proxy->TakeFrameHostOwnership(new_render_frame_host.Pass());
     }
 
     success =
@@ -1199,15 +1197,18 @@ scoped_ptr<RenderFrameHostImpl> RenderFrameHostManager::CreateRenderFrame(
         DCHECK(new_render_frame_host.get());
         success = InitRenderFrame(new_render_frame_host.get());
       }
-      if (success) {
-        if (view_routing_id_ptr)
-          *view_routing_id_ptr = render_view_host->GetRoutingID();
-        // If a brand new RFH was created, announce it to observers.
-        if (new_render_frame_host) {
-          render_frame_delegate_->RenderFrameCreated(
-              new_render_frame_host.get());
-        }
-      }
+    }
+
+    if (success) {
+      if (view_routing_id_ptr)
+        *view_routing_id_ptr = render_view_host->GetRoutingID();
+
+      // A brand new RenderFrame was created by one of the Init calls above.
+      // Announce it to observers.
+      if (swapped_out)
+        render_frame_delegate_->RenderFrameCreated(proxy->render_frame_host());
+      else
+        render_frame_delegate_->RenderFrameCreated(new_render_frame_host.get());
     }
   }
 
