82aa93ea5436e7c3c30d12cd529ac417170ddc1e
dcheng <dcheng@chromium.org>
2014-12-20 10:51:11 -0800

    Revert of Refactor Vaapi video decoder/encoder in preparation of Freon support (patchset #6 id:100001 of https://codereview.chromium.org/807853005/)
    Reason for revert:
    Caused build failure on Linux ChromiumOS GN:
    ninja: error: '../../content/common/content/common/gpu/media/va_x11.sigs',
    needed by 'obj/content/common/libva_generate_stubs.inputdeps.stamp', missing
    and no known rule to make it
    
    Original issue's description:
    > Refactor Vaapi video decoder/encoder in preparation of Freon support
    >
    > This introduces VaapiPicture which abstracts the output picture for the VaapiVideoDecodeAccelerator.
    > This also splits out the x11 specific functions of LibVA into their own signature file.
    > And finally this adds back GLImageGLX to match the code path that will be used on Freon, using GLImageLinuxDMABuffer.
    >
    > BUG=403058
    > TEST=video_decode_accelerator_unittest and video_encode_accelerator_unittest
    >
    > Committed: https://crrev.com/289e96375e75be0d9158b86df2bd4faf2d7b2a64
    > Cr-Commit-Position: refs/heads/master@{#309355}
    
    TBR=reveman@chromium.org,posciak@chromium.org,scherkus@chromium.org,piman@chromium.org,lionel.g.landwerlin@intel.com
    NOTREECHECKS=true
    NOTRY=true
    BUG=403058
    
    Review URL: https://codereview.chromium.org/804353003
    
    Cr-Commit-Position: refs/heads/master@{#309356}

--- ui/gl/gl_image_glx.cc
+++ ui/gl/gl_image_glx.cc
@@ -62,8 +62,6 @@ unsigned PixmapDepth(unsigned internalformat) {
   switch (internalformat) {
     case GL_RGBA:
       return 32u;
-    case GL_RGB:
-      return 24u;
     default:
       NOTREACHED();
       return 0u;
