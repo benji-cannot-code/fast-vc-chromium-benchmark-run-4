1f42557ff476babfb63bf07c5ace90036c20ea2f
hashimoto <hashimoto@chromium.org>
2014-12-19 21:07:10 -0800

    Fix SysfsPowerMonitor to let it work even if |browser| is None
    measurements/tab_switching.py creates power.PowerMetric (which calls StartMonitoringPower() in __init__) in WillStartBrowser() where |browser| is still None.
    SysfsPowerMonitor should not expect |browser| to be always valid.
    
    With this change, tab_switching.top_10 can output power related values as the result.
    
    BUG=443077, 433442
    TEST=tools/perf/run_benchmark --browser=cros-chrome --remote=172.23.69.61 tab_switching.top_10
    
    Review URL: https://codereview.chromium.org/818653002
    
    Cr-Commit-Position: refs/heads/master@{#309337}

--- tools/telemetry/telemetry/core/platform/power_monitor/android_dumpsys_power_monitor.py
+++ tools/telemetry/telemetry/core/platform/power_monitor/android_dumpsys_power_monitor.py
@@ -24,6 +24,7 @@ class DumpsysPowerMonitor(sysfs_power_monitor.SysfsPowerMonitor):
         platform_backend: A LinuxBasedPlatformBackend instance.
     """
     super(DumpsysPowerMonitor, self).__init__(platform_backend)
+    self._browser = None
     self._device = device
 
   def CanMonitorPower(self):
@@ -31,6 +32,7 @@ class DumpsysPowerMonitor(sysfs_power_monitor.SysfsPowerMonitor):
 
   def StartMonitoringPower(self, browser):
     super(DumpsysPowerMonitor, self).StartMonitoringPower(browser)
+    self._browser = browser
     # Disable the charging of the device over USB. This is necessary because the
     # device only collects information about power usage when the device is not
     # charging.
@@ -38,8 +40,8 @@ class DumpsysPowerMonitor(sysfs_power_monitor.SysfsPowerMonitor):
 
   def StopMonitoringPower(self):
     if self._browser:
-      # pylint: disable=W0212
       package = self._browser._browser_backend.package
+      self._browser = None
     cpu_stats = super(DumpsysPowerMonitor, self).StopMonitoringPower()
     self._device.old_interface.EnableUsbCharging()
     # By default, 'dumpsys batterystats' measures power consumption during the
