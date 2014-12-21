2baffa355cb1d510adba3c08419f2073d6d4e0ce
tfarina <tfarina@chromium.org>
2014-12-20 19:38:33 -0800

    Cleanup: Update the path to safe_integer_conversions.h header.
    The geometry headers were moved from /ui/gfx/ to ui/gfx/geometry, so
    we are updating the include paths to fix this.
    
    BUG=395370
    TEST=ui_base_unittests && gfx_unittests
    TBR=sky@chromium.org,danakj@chromium.org
    
    Review URL: https://codereview.chromium.org/820913002
    
    Cr-Commit-Position: refs/heads/master@{#309372}

--- ui/gfx/animation/tween.cc
+++ ui/gfx/animation/tween.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/numerics/safe_conversions.h"
 #include "ui/gfx/geometry/cubic_bezier.h"
-#include "ui/gfx/safe_integer_conversions.h"
+#include "ui/gfx/geometry/safe_integer_conversions.h"
 
 namespace gfx {
 
