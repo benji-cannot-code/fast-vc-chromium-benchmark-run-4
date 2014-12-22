02ed580ce51033a21a9caff2201063156dd64a01
gunsch <gunsch@chromium.org>
2014-12-22 15:52:09 -0800

    Chromecast on Android buildfix: don't try to create a RendererFactory.
    The CMA code is not linked in for Android builds.
    
    R=erickung@chromium.org
    TBR=lcwu@chromium.org
    BUG=None
    
    Review URL: https://codereview.chromium.org/821793002
    
    Cr-Commit-Position: refs/heads/master@{#309492}

--- chromecast/renderer/cast_content_renderer_client.cc
+++ chromecast/renderer/cast_content_renderer_client.cc
@@ -74,6 +74,7 @@ void CastContentRendererClient::AddKeySystems(
   AddChromecastPlatformKeySystems(key_systems);
 }
 
+#if !defined(OS_ANDROID)
 scoped_ptr<::media::RendererFactory>
 CastContentRendererClient::CreateMediaRendererFactory(
     ::content::RenderFrame* render_frame) {
@@ -85,6 +86,7 @@ CastContentRendererClient::CreateMediaRendererFactory(
       new chromecast::media::CmaMediaRendererFactory(
           render_frame->GetRoutingID()));
 }
+#endif
 
 blink::WebPrescientNetworking*
 CastContentRendererClient::GetPrescientNetworking() {
