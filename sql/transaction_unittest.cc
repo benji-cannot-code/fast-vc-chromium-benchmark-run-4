5162d76ba8ac8bb6d98a7e94ddad3e1f91485d19
dcheng <dcheng@chromium.org>
2014-12-22 15:00:24 -0800

    Standardize usage of virtual/override/final specifiers in sql/.
    The Google C++ style guide states:
    
      Explicitly annotate overrides of virtual functions or virtual
      destructors with an override or (less frequently) final specifier.
      Older (pre-C++11) code will use the virtual keyword as an inferior
      alternative annotation. For clarity, use exactly one of override,
      final, or virtual when declaring an override.
    
    To better conform to these guidelines, the following constructs have
    been rewritten:
    
    - if a base class has a virtual destructor, then:
        virtual ~Foo();                   ->  ~Foo() override;
    - virtual void Foo() override;        ->  void Foo() override;
    - virtual void Foo() override final;  ->  void Foo() final;
    
    This patch was automatically generated. The clang plugin can generate
    fixit hints, which are suggested edits when it is 100% sure it knows how
    to fix a problem. The hints from the clang plugin were applied to the
    source tree using the tool in https://codereview.chromium.org/598073004.
    
    Several formatting edits by clang-format were manually reverted, due to
    mangling of some of the more complicate IPC macros.
    
    BUG=417463
    
    Review URL: https://codereview.chromium.org/817403002
    
    Cr-Commit-Position: refs/heads/master@{#309480}

--- sql/transaction_unittest.cc
+++ sql/transaction_unittest.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class SQLTransactionTest : public testing::Test {
  public:
-  virtual void SetUp() {
+  void SetUp() override {
     ASSERT_TRUE(temp_dir_.CreateUniqueTempDir());
     ASSERT_TRUE(db_.Open(
         temp_dir_.path().AppendASCII("SQLTransactionTest.db")));
@@ -21,9 +21,7 @@ class SQLTransactionTest : public testing::Test {
     ASSERT_TRUE(db().Execute("CREATE TABLE foo (a, b)"));
   }
 
-  virtual void TearDown() {
-    db_.Close();
-  }
+  void TearDown() override { db_.Close(); }
 
   sql::Connection& db() { return db_; }
 
