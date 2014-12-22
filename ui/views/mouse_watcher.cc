d28ea7fce72630d3ec840e8e92bda8d17c7494bc
dcheng <dcheng@chromium.org>
2014-12-22 14:52:57 -0800

    Standardize usage of virtual/override/final specifiers in ui/.
    The Google C++ style guide states:
    
      Explicitly annotate overrides of virtual functions or virtual
      destructors with an override or (less frequently) final specifier.
      Older (pre-C++11) code will use the virtual keyword as an inferior
      alternative annotation. For clarity, use exactly one of override,
      final, or virtual when declaring an override.
    
    To better conform to these guidelines, the following constructs have
    been rewritten:
    
    - if a base class has a virtual destructor, then:
        virtual ~Foo();                   ->  ~Foo() override;
    - virtual void Foo() override;        ->  void Foo() override;
    - virtual void Foo() override final;  ->  void Foo() final;
    
    This patch was automatically generated. The clang plugin can generate
    fixit hints, which are suggested edits when it is 100% sure it knows how
    to fix a problem. The hints from the clang plugin were applied to the
    source tree using the tool in https://codereview.chromium.org/598073004.
    
    Several formatting edits by clang-format were manually reverted, due to
    mangling of some of the more complicate IPC macros.
    
    BUG=417463
    
    Review URL: https://codereview.chromium.org/819943002
    
    Cr-Commit-Position: refs/heads/master@{#309479}

--- ui/views/mouse_watcher.cc
+++ ui/views/mouse_watcher.cc
@@ -31,7 +31,7 @@ class MouseWatcher::Observer : public ui::EventHandler {
   }
 
   // ui::EventHandler implementation:
-  virtual void OnMouseEvent(ui::MouseEvent* event) override {
+  void OnMouseEvent(ui::MouseEvent* event) override {
     switch (event->type()) {
       case ui::ET_MOUSE_MOVED:
       case ui::ET_MOUSE_DRAGGED:
