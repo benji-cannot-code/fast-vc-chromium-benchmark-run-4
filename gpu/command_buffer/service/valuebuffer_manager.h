d65626c06a5f96448aff840a5400594b159a1c13
qsr <qsr@chromium.org>
2014-12-22 06:07:18 -0800

    Remove unused header.
    TBR=piman@chromium.org
    
    Review URL: https://codereview.chromium.org/816373002
    
    Cr-Commit-Position: refs/heads/master@{#309421}

--- gpu/command_buffer/service/valuebuffer_manager.h
+++ gpu/command_buffer/service/valuebuffer_manager.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "gpu/command_buffer/common/value_state.h"
 #include "gpu/gpu_export.h"
-#include "ipc/ipc_sender.h"
 
 namespace content {
 class GpuChannel;
