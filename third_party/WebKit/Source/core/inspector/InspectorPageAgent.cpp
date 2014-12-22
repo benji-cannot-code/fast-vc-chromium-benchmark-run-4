c70d2af88139a2e4ef53a2a3cee3c8d2b69e8b9a
samli@chromium.org <samli@chromium.org>
2014-12-22 12:26:47 +0000

    Devtools Animations: Load the current animations playback rate
    When animations playback rate is set on a document, the UI does not
    load with the current value when the inspector is reopened. This change
    reads the current playback rate when the animations control pane is
    created.
    
    BUG=432368
    
    Review URL: https://codereview.chromium.org/816313002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187614 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorPageAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorPageAgent.cpp
@@ -1469,6 +1469,11 @@ void InspectorPageAgent::setShowViewportSizeOnResize(ErrorString*, bool show, co
     m_state->setBoolean(PageAgentState::showGridOnResize, asBool(showGrid));
 }
 
+void InspectorPageAgent::animationsPlaybackRate(ErrorString*, double* playbackRate)
+{
+    *playbackRate = toLocalFrame(m_page->mainFrame())->document()->timeline().playbackRate();
+}
+
 void InspectorPageAgent::setAnimationsPlaybackRate(ErrorString*, double playbackRate)
 {
     for (Frame* frame = m_page->mainFrame(); frame; frame = frame->tree().traverseNext()) {
