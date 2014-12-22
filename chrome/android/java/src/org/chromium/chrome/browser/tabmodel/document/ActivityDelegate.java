50416885e7d1acd5ca5c9fdea1400bc290503d27
dfalcantara <dfalcantara@chromium.org>
2014-12-22 14:38:03 -0800

    Move DocumentTabModelSelector and related classes upstream
    * Moves various classes upstream.
    * Changes ActivityDelegate to use the DocumentUtils to access
      the task's baseIntent instead of calling getTaskInfo() directly.
    * Findbugs file changed for fields that are only read downstream.
    
    Submitting with NOTRY as true to get around findbugs submission issues.
    
    BUG=443773
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/821063002
    
    Cr-Commit-Position: refs/heads/master@{#309473}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/document/ActivityDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/document/ActivityDelegate.java
@@ -19,6 +19,7 @@ import android.text.TextUtils;
 import org.chromium.base.ApplicationStatus;
 import org.chromium.chrome.browser.Tab;
 import org.chromium.chrome.browser.UrlConstants;
+import org.chromium.chrome.browser.document.DocumentUtils;
 import org.chromium.chrome.browser.tabmodel.document.DocumentTabModel.Entry;
 
 import java.util.ArrayList;
@@ -85,7 +86,7 @@ public class ActivityDelegate {
         ActivityManager activityManager =
                 (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
         for (ActivityManager.AppTask task : activityManager.getAppTasks()) {
-            Intent intent = task.getTaskInfo().baseIntent;
+            Intent intent = DocumentUtils.getBaseIntentFromTask(task);
             if (!isValidActivity(isIncognito, intent)) continue;
 
             int tabId = getTabIdFromIntent(intent);
@@ -121,7 +122,7 @@ public class ActivityDelegate {
         ActivityManager activityManager =
                 (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
         for (ActivityManager.AppTask task : activityManager.getAppTasks()) {
-            Intent intent = task.getTaskInfo().baseIntent;
+            Intent intent = DocumentUtils.getBaseIntentFromTask(task);
             int taskId = getTabIdFromIntent(intent);
             if (taskId == tabId && isValidActivity(isIncognito, intent)) return task;
         }
