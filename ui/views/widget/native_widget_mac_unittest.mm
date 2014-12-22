c824586f99fe4b992f03e79748597ced9af1201b
tapted <tapted@chromium.org>
2014-12-22 14:52:00 -0800

    Fix NativeWidgetMacTest.HideAndShowExternally
    This regressed in r308886 because the unit test changed from having no
    activation policy to having a "Regular" activation policy.
    
    With a regular activation policy, calls to [NSApp hide/show*:] are
    asynchronous, so the run loop needs to be flushed afterwards.
    
    Also adds an extra check for [NSWindow makeKeyAndOrderFront:]. It
    doesn't need a runloop flush for the visibility change, but it should
    have coverage anyway.
    
    BUG=378134
    
    Review URL: https://codereview.chromium.org/809903009
    
    Cr-Commit-Position: refs/heads/master@{#309478}

--- ui/views/widget/native_widget_mac_unittest.mm
+++ ui/views/widget/native_widget_mac_unittest.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Cocoa/Cocoa.h>
 
+#include "base/run_loop.h"
 #include "ui/views/test/test_widget_observer.h"
 #include "ui/views/test/widget_test.h"
 
@@ -86,12 +87,18 @@ TEST_F(NativeWidgetMacTest, HideAndShowExternally) {
   // Test when hiding the entire application. This doesn't send an orderOut:
   // to the NSWindow.
   [NSApp hide:nil];
+  // When the activation policy is NSApplicationActivationPolicyRegular, the
+  // calls via NSApp are asynchronous, and the run loop needs to be flushed.
+  // With NSApplicationActivationPolicyProhibited, the following RunUntilIdle
+  // calls are superfluous, but don't hurt.
+  base::RunLoop().RunUntilIdle();
   EXPECT_FALSE(widget->IsVisible());
   EXPECT_FALSE([ns_window isVisible]);
   EXPECT_EQ(3, observer.gained_visible_count());
   EXPECT_EQ(3, observer.lost_visible_count());
 
   [NSApp unhideWithoutActivation];
+  base::RunLoop().RunUntilIdle();
   EXPECT_TRUE(widget->IsVisible());
   EXPECT_TRUE([ns_window isVisible]);
   EXPECT_EQ(4, observer.gained_visible_count());
@@ -99,14 +106,29 @@ TEST_F(NativeWidgetMacTest, HideAndShowExternally) {
 
   // Hide again to test unhiding with an activation.
   [NSApp hide:nil];
+  base::RunLoop().RunUntilIdle();
   EXPECT_EQ(4, observer.lost_visible_count());
   [NSApp unhide:nil];
+  base::RunLoop().RunUntilIdle();
   EXPECT_EQ(5, observer.gained_visible_count());
+
+  // Hide again to test makeKeyAndOrderFront:.
+  [ns_window orderOut:nil];
+  EXPECT_FALSE(widget->IsVisible());
+  EXPECT_FALSE([ns_window isVisible]);
+  EXPECT_EQ(5, observer.gained_visible_count());
+  EXPECT_EQ(5, observer.lost_visible_count());
+
+  [ns_window makeKeyAndOrderFront:nil];
+  EXPECT_TRUE(widget->IsVisible());
+  EXPECT_TRUE([ns_window isVisible]);
+  EXPECT_EQ(6, observer.gained_visible_count());
+  EXPECT_EQ(5, observer.lost_visible_count());
 
   // No change when closing.
   widget->CloseNow();
-  EXPECT_EQ(4, observer.lost_visible_count());
-  EXPECT_EQ(5, observer.gained_visible_count());
+  EXPECT_EQ(5, observer.lost_visible_count());
+  EXPECT_EQ(6, observer.gained_visible_count());
 }
 
 // A view that counts calls to OnPaint().
