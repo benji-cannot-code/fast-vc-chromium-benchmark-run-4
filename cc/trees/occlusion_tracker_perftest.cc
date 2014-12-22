1d25512030a1488a5fe8cd0577e97e2b31f48c47
awoloszyn <awoloszyn@chromium.org>
2014-12-22 13:40:24 -0800

    Fix for cc_perftests failing
    This was recently introduced by https://codereview.chromium.org/465853004/
    
    BUG=444629
    
    Review URL: https://codereview.chromium.org/821743003
    
    Cr-Commit-Position: refs/heads/master@{#309465}

--- cc/trees/occlusion_tracker_perftest.cc
+++ cc/trees/occlusion_tracker_perftest.cc
@@ -47,6 +47,7 @@ class OcclusionTrackerPerfTest : public testing::Test {
     host_impl_->InitializeRenderer(FakeOutputSurface::Create3d());
 
     scoped_ptr<LayerImpl> root_layer = LayerImpl::Create(active_tree(), 1);
+    root_layer->SetHasRenderSurface(true);
     active_tree()->SetRootLayer(root_layer.Pass());
   }
 
