30baae6318ef12ffee86529a364dc17dfe46a89b
pkotwicz <pkotwicz@chromium.org>
2014-12-22 15:27:44 -0800

    Fix crash when the source browser window is deleted during a drag
    This CL makes Widget::RunShellDrag() and DesktopDragDropClientWin not crash if
    the browser window is deleted during a drag-drop operation. An extension can
    delete the browser window during the drag-drop operation.
    
    BUG=440534
    TEST=BookmarkBarViewTest22.*
    R=pkasting
    TBR=sky (The files in ui/views/widget are identical to the original CL)
    
    Review URL: https://codereview.chromium.org/819793002
    
    Cr-Commit-Position: refs/heads/master@{#309484}

--- chrome/test/base/view_event_test_base.cc
+++ chrome/test/base/view_event_test_base.cc
@@ -89,6 +89,7 @@ void ViewEventTestBase::SetUp() {
   platform_part_.reset(ViewEventTestPlatformPart::Create(context_factory));
   gfx::NativeWindow context = platform_part_->GetContext();
   window_ = views::Widget::CreateWindowWithContext(this, context);
+  window_->Show();
 }
 
 void ViewEventTestBase::TearDown() {
