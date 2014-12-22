6f7417cefcb67793b598f560d565f2bf05a88c50
tfarina <tfarina@chromium.org>
2014-12-21 18:49:49 -0800

    skia/ext: Remove unused SkFontHost.h include from image_operations.cc
    Basically SkFontHost is deprecated and SkPixelGeometry should be used
    when possible, but since nothing in ImageOperations is using it, removed
    this unused include.
    
    BUG=None
    TEST=compiles
    TBR=reed@google.com
    
    Review URL: https://codereview.chromium.org/817913003
    
    Cr-Commit-Position: refs/heads/master@{#309385}

--- skia/ext/image_operations.cc
+++ skia/ext/image_operations.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "skia/ext/convolver.h"
 #include "third_party/skia/include/core/SkColorPriv.h"
-#include "third_party/skia/include/core/SkFontHost.h"
 #include "third_party/skia/include/core/SkRect.h"
 
 namespace skia {
