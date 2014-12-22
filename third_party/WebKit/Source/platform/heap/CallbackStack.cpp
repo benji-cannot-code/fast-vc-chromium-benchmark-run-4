8718e6acc38a1bf2f2e060821e40424979314b0a
vchigrin@yandex-team.ru <vchigrin@yandex-team.ru>
2014-12-22 22:26:17 +0000

    Exclude some unneeded files, fix compiler warning.
    Review URL: https://codereview.chromium.org/786323003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187629 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/heap/CallbackStack.cpp
+++ third_party/WebKit/Source/platform/heap/CallbackStack.cpp
@@ -116,9 +116,6 @@ CallbackStack::Item* CallbackStack::popSlow()
         if (Item* item = m_first->pop())
             return item;
     }
-
-    ASSERT_NOT_REACHED();
-    return 0;
 }
 
 void CallbackStack::invokeEphemeronCallbacks(Visitor* visitor)
