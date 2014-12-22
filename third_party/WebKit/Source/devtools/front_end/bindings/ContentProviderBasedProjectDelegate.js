61e1fb56b45f5ea766e8b6f0cb2ea95cfa004832
pfeldman@chromium.org <pfeldman@chromium.org>
2014-12-22 15:44:13 +0000

    DevTools: merge ProjectStore into ProjectDelegate.
    Review URL: https://codereview.chromium.org/816203002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187619 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/bindings/ContentProviderBasedProjectDelegate.js
+++ third_party/WebKit/Source/devtools/front_end/bindings/ContentProviderBasedProjectDelegate.js
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
  /**
  * @constructor
+ * @extends {WebInspector.Object}
  * @implements {WebInspector.ProjectDelegate}
  * @param {!WebInspector.Workspace} workspace
  * @param {string} id
@@ -38,12 +39,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 WebInspector.ContentProviderBasedProjectDelegate = function(workspace, id, type)
 {
+    WebInspector.Object.call(this);
     this._type = type;
     /** @type {!Object.<string, !WebInspector.ContentProvider>} */
     this._contentProviders = {};
     this._workspace = workspace;
     this._id = id;
-    this._projectStore = workspace.addProject(id, this);
+    workspace.addProject(id, this);
 }
 
 WebInspector.ContentProviderBasedProjectDelegate.prototype = {
@@ -345,7 +347,7 @@ WebInspector.ContentProviderBasedProjectDelegate.prototype = {
             return path;
         var fileDescriptor = new WebInspector.FileDescriptor(parentPath, name, originURL, url, contentProvider.contentType());
         this._contentProviders[path] = contentProvider;
-        this._projectStore.addFile(fileDescriptor);
+        this.dispatchEventToListeners(WebInspector.ProjectDelegate.Events.FileAdded, fileDescriptor);
         return path;
     },
 
@@ -355,7 +357,7 @@ WebInspector.ContentProviderBasedProjectDelegate.prototype = {
     removeFile: function(path)
     {
         delete this._contentProviders[path];
-        this._projectStore.removeFile(path);
+        this.dispatchEventToListeners(WebInspector.ProjectDelegate.Events.FileRemoved, path);
     },
 
     /**
@@ -370,6 +372,8 @@ WebInspector.ContentProviderBasedProjectDelegate.prototype = {
     {
         this._contentProviders = {};
         this._workspace.removeProject(this._id);
-        this._projectStore = this._workspace.addProject(this._id, this);
-    }
+        this._workspace.addProject(this._id, this);
+    },
+
+    __proto__: WebInspector.Object.prototype
 }
