103075a4e93cff23b29eb2f7cc4b448ccbc302a9
vivek.vg <vivek.vg@samsung.com>
2014-12-22 06:37:10 -0800

    Tests in browser_tests target should use ChromeRenderViewTest as the base class.
    Currently these tests are utilizing the content::RenderViewTest as the base class
    whereas all other *_browsertest.cc are using ChromeRenderViewTest as the base class.
    
    Due to this, the migration of inline CSS to GRD being done in [1] is failing
    for browsertests target as listed here [2] in the build log.
    
    In order to fix this, these tests should use the ChromeRenderViewTest as their base class.
    In doing so, the ChromeRenderViewTest loads ChromeContentClient which in turn loads the correct
    pak file i.e. resources.pak required for the migration.
    
    This CL adds the extensions/DEPS rule to allow the inclusion of chrome_render_view_test.h.
    
    [1] https://codereview.chromium.org/573553002
    [2] http://build.chromium.org/p/tryserver.chromium.linux/builders/linux_chromium_rel_ng/builds/14184
    
    Review URL: https://codereview.chromium.org/818563003
    
    Cr-Commit-Position: refs/heads/master@{#309424}

--- chrome/renderer/autofill/page_click_tracker_browsertest.cc
+++ chrome/renderer/autofill/page_click_tracker_browsertest.cc
@@ -4,10 +4,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/basictypes.h"
+#include "chrome/test/base/chrome_render_view_test.h"
 #include "components/autofill/content/renderer/page_click_listener.h"
 #include "components/autofill/content/renderer/page_click_tracker.h"
 #include "content/public/renderer/render_view.h"
-#include "content/public/test/render_view_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/WebKit/public/web/WebDocument.h"
 #include "third_party/WebKit/public/web/WebInputElement.h"
@@ -42,10 +42,10 @@ class TestPageClickListener : public PageClickListener {
   bool was_focused_;
 };
 
-class PageClickTrackerTest : public content::RenderViewTest {
+class PageClickTrackerTest : public ChromeRenderViewTest {
  protected:
   void SetUp() override {
-    content::RenderViewTest::SetUp();
+    ChromeRenderViewTest::SetUp();
 
     // RenderView creates PageClickTracker but it doesn't keep it around.
     // Rather than make it do so for the test, we create a new object.
@@ -72,7 +72,7 @@ class PageClickTrackerTest : public content::RenderViewTest {
     textarea_.reset();
     test_listener_.ClearResults();
     page_click_tracker_.reset();
-    content::RenderViewTest::TearDown();
+    ChromeRenderViewTest::TearDown();
   }
 
   // Simulates a click on the given element and then waits for the stack
--- extensions/renderer/script_context_browsertest.cc
+++ extensions/renderer/script_context_browsertest.cc
@@ -3,9 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/test/base/chrome_render_view_test.h"
 #include "content/public/renderer/render_frame.h"
 #include "content/public/test/frame_load_waiter.h"
-#include "content/public/test/render_view_test.h"
 #include "extensions/renderer/script_context.h"
 #include "third_party/WebKit/public/web/WebDocument.h"
 #include "third_party/WebKit/public/web/WebLocalFrame.h"
@@ -16,7 +16,7 @@ using blink::WebFrame;
 namespace extensions {
 namespace {
 
-class ScriptContextTest : public content::RenderViewTest {
+class ScriptContextTest : public ChromeRenderViewTest {
  protected:
   GURL GetEffectiveDocumentURL(const WebFrame* frame) {
     return ScriptContext::GetEffectiveDocumentURL(
