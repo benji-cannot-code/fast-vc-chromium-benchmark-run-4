745e709ca769cfd3c936df75e972dbd60bd535e5
raymes <raymes@chromium.org>
2014-12-22 20:13:55 -0800

    Queue postMessage messages destined for a MimeHandlerView guest
    Currently if postMessage messages are sent to a MimeHandlerView guest before
    it has loaded, they will just be dropped. It also isn't possible to know
    when the guest has fully loaded. This CL queues messages until the guest
    has fully loaded and then dispatches them to the guest so that none are lost.
    
    BUG=415858
    
    Review URL: https://codereview.chromium.org/811263002
    
    Cr-Commit-Position: refs/heads/master@{#309534}

--- extensions/browser/guest_view/mime_handler_view/mime_handler_view_guest.h
+++ extensions/browser/guest_view/mime_handler_view/mime_handler_view_guest.h
@@ -60,6 +60,7 @@ class MimeHandlerViewGuest : public GuestView<MimeHandlerViewGuest>,
   bool SaveFrame(const GURL& url, const content::Referrer& referrer) override;
 
   // content::WebContentsObserver implementation.
+  void DocumentOnLoadCompletedInMainFrame() override;
   bool OnMessageReceived(const IPC::Message& message) override;
 
  private:
