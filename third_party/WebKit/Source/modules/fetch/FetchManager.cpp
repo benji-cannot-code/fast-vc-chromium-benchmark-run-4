d34544e77bc4758ea8d067728bc5862cd71e0058
haraken@chromium.org <haraken@chromium.org>
2014-12-23 19:12:43 +0000

    Oilpan: Move GlobalFetch and FetchManager to Oilpan's heap
    In preparation for moving ActiveDOMObject to Oilpan's heap, this CL moves GlobalFetch to Oilpan's heap.
    At the same time, this CL moves FetchManager to Oilpan's heap as well because FetchManager
    is pointed to by the GlobalFetch.
    
    BUG=340522
    
    Review URL: https://codereview.chromium.org/786183003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187654 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/fetch/FetchManager.cpp
+++ third_party/WebKit/Source/modules/fetch/FetchManager.cpp
@@ -384,4 +384,9 @@ void FetchManager::onLoaderFinished(Loader* loader)
     OwnPtr<Loader> p = m_loaders.take(loader);
 }
 
+void FetchManager::trace(Visitor* visitor)
+{
+    visitor->trace(m_executionContext);
+}
+
 } // namespace blink
