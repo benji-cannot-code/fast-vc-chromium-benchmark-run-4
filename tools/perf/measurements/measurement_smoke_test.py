e966238bcd13ae78c7c3db03bb404f17dcba50cf
nednguyen <nednguyen@google.com>
2014-12-23 10:17:05 -0800

    [Telemetry] Rename RunRepaint to RunPageInteractions
    BUG=418375
    
    Review URL: https://codereview.chromium.org/820063003
    
    Cr-Commit-Position: refs/heads/master@{#309559}

--- tools/perf/measurements/measurement_smoke_test.py
+++ tools/perf/measurements/measurement_smoke_test.py
@@ -20,9 +20,8 @@ from telemetry.util import classes
 # crbug.com/418375
 _ACTION_NAMES_WHITE_LIST = (
   '',
-  'RunPageInteractions',
   'RunSmoothness',
-  'RunRepaint',
+  'RunPageInteractions',
   'RunNavigateSteps',
 )
 
