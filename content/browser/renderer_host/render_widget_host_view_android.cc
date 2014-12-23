631026a4579caca0396803e7f9385565432fa959
eustas <eustas@chromium.org>
2014-12-22 23:12:59 -0800

    Add ability to copy unscaled output pixels.
    BUG=427418
    
    Review URL: https://codereview.chromium.org/802343002
    
    Cr-Commit-Position: refs/heads/master@{#309542}

--- content/browser/renderer_host/render_widget_host_view_android.cc
+++ content/browser/renderer_host/render_widget_host_view_android.cc
@@ -972,7 +972,8 @@ void RenderWidgetHostViewAndroid::CopyFromCompositingSurface(
                  start_time,
                  readback_layer,
                  callback));
-  request->set_area(src_subrect_in_pixel);
+  if (!src_subrect_in_pixel.IsEmpty())
+    request->set_area(src_subrect_in_pixel);
   readback_layer->RequestCopyOfOutput(request.Pass());
 }
 
@@ -1286,6 +1287,20 @@ void RenderWidgetHostViewAndroid::SynchronousCopyContents(
     const gfx::Size& dst_size_in_pixel,
     ReadbackRequestCallback& callback,
     const SkColorType color_type) {
+  gfx::Size input_size_in_pixel;
+  if (src_subrect_in_pixel.IsEmpty())
+    input_size_in_pixel = content_size_in_layer_;
+  else
+    input_size_in_pixel = src_subrect_in_pixel.size();
+
+  gfx::Size output_size_in_pixel;
+  if (dst_size_in_pixel.IsEmpty())
+    output_size_in_pixel = input_size_in_pixel;
+  else
+    output_size_in_pixel = dst_size_in_pixel;
+  int output_width = output_size_in_pixel.width();
+  int output_height = output_size_in_pixel.height();
+
   SynchronousCompositor* compositor =
       SynchronousCompositorImpl::FromID(host_->GetProcess()->GetID(),
                                         host_->GetRoutingID());
@@ -1295,14 +1310,14 @@ void RenderWidgetHostViewAndroid::SynchronousCopyContents(
   }
 
   SkBitmap bitmap;
-  bitmap.allocPixels(SkImageInfo::Make(dst_size_in_pixel.width(),
-                                       dst_size_in_pixel.height(),
+  bitmap.allocPixels(SkImageInfo::Make(output_width,
+                                       output_height,
                                        color_type,
                                        kPremul_SkAlphaType));
   SkCanvas canvas(bitmap);
   canvas.scale(
-      (float)dst_size_in_pixel.width() / (float)src_subrect_in_pixel.width(),
-      (float)dst_size_in_pixel.height() / (float)src_subrect_in_pixel.height());
+      (float)output_width / (float)input_size_in_pixel.width(),
+      (float)output_height / (float)input_size_in_pixel.height());
   compositor->DemandDrawSw(&canvas);
   callback.Run(bitmap, READBACK_SUCCESS);
 }
@@ -1806,9 +1821,15 @@ void RenderWidgetHostViewAndroid::PrepareTextureCopyOutputResult(
   if (!result->HasTexture() || result->IsEmpty() || result->size().IsEmpty())
     return;
 
+  gfx::Size output_size_in_pixel;
+  if (dst_size_in_pixel.IsEmpty())
+    output_size_in_pixel = result->size();
+  else
+    output_size_in_pixel = dst_size_in_pixel;
+
   scoped_ptr<SkBitmap> bitmap(new SkBitmap);
-  if (!bitmap->tryAllocPixels(SkImageInfo::Make(dst_size_in_pixel.width(),
-                                                dst_size_in_pixel.height(),
+  if (!bitmap->tryAllocPixels(SkImageInfo::Make(output_size_in_pixel.width(),
+                                                output_size_in_pixel.height(),
                                                 color_type,
                                                 kOpaque_SkAlphaType))) {
     return;
@@ -1836,7 +1857,7 @@ void RenderWidgetHostViewAndroid::PrepareTextureCopyOutputResult(
       texture_mailbox.sync_point(),
       result->size(),
       gfx::Rect(result->size()),
-      dst_size_in_pixel,
+      output_size_in_pixel,
       pixels,
       color_type,
       base::Bind(&CopyFromCompositingSurfaceFinished,
