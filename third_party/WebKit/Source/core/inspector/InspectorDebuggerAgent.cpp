b196b051dedc26bbf9d2c155e51c5b4669af2edd
yurys@chromium.org <yurys@chromium.org>
2014-12-23 19:13:19 +0000

    DevTools: always create new instance of AsyncCallChain in createAsyncCallChain
    A new instance of call chain is now created even in case current call stack is empty (this should be addressed separately so that we don't lose description of the call stack).
    
    This CL also breaks friendship of InspectorDebuggerAgent with AsyncCallChain and makes AsyncCallChain essentially immutable. Its length can be limited only in constructor.
    
    This change allows us to eventually change InspectorDebuggerAgent so that it returned async operation id instead of AsyncCallChain. The embedder doesn't use any fields on AsyncCallChain and I'd rather have least possible API surface for the async operations.
    
    BUG=439376
    
    Review URL: https://codereview.chromium.org/820493003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187656 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorDebuggerAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorDebuggerAgent.cpp
@@ -1099,8 +1099,6 @@ void InspectorDebuggerAgent::getPromiseById(ErrorString* errorString, int promis
 
 const AsyncCallChain* InspectorDebuggerAgent::currentAsyncCallChain() const
 {
-    if (m_currentAsyncCallChain)
-        m_currentAsyncCallChain->ensureMaxAsyncCallChainDepth(m_maxAsyncCallStackDepth);
     return m_currentAsyncCallChain.get();
 }
 
@@ -1110,12 +1108,11 @@ PassRefPtrWillBeRawPtr<AsyncCallChain> InspectorDebuggerAgent::createAsyncCallCh
     if (callFrames.isEmpty()) {
         if (!m_currentAsyncCallChain)
             return nullptr;
-        didCreateAsyncCallChain(m_currentAsyncCallChain.get());
-        return m_currentAsyncCallChain; // Propogate async call stack chain.
+        RefPtrWillBeRawPtr<AsyncCallChain> chain = AsyncCallChain::create(nullptr, m_currentAsyncCallChain.get(), m_maxAsyncCallStackDepth);
+        didCreateAsyncCallChain(chain.get());
+        return chain.release(); // Propagate async call stack chain.
     }
-    RefPtrWillBeRawPtr<AsyncCallChain> chain = adoptRefWillBeNoop(m_currentAsyncCallChain ? new AsyncCallChain(*m_currentAsyncCallChain) : new AsyncCallChain());
-    chain->ensureMaxAsyncCallChainDepth(m_maxAsyncCallStackDepth - 1);
-    chain->m_callStacks.prepend(adoptRefWillBeNoop(new AsyncCallStack(description, callFrames)));
+    RefPtrWillBeRawPtr<AsyncCallChain> chain = AsyncCallChain::create(adoptRefWillBeNoop(new AsyncCallStack(description, callFrames)), m_currentAsyncCallChain.get(), m_maxAsyncCallStackDepth);
     didCreateAsyncCallChain(chain.get());
     return chain.release();
 }
