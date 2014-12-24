7aa1107fd24eeceb6684c3913876031024ae3b73
kouhei@chromium.org <kouhei@chromium.org>
2014-12-24 07:52:37 +0000

    Templatize visitor arguments for TraceTrait mark and trace methods.
    This CL replaces Visitor* in TraceTrait<T>::{mark,trace} methods arguments
    to template parameter VisitorDispatcher.
    
    This is to prepare for InlinedGlobalMarkingVisitor implementaion, so that
    those methods can also be used to dispatch
    TraceTrait<T>::mark(InlinedGlobalMarkingVisitor) in addition to the current
    TraceTrait<T>::mark(Visitor*) usage.
    
    BUG=420515
    
    Review URL: https://codereview.chromium.org/807003004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187674 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/heap/Visitor.h
+++ third_party/WebKit/Source/platform/heap/Visitor.h
@@ -200,13 +200,14 @@ class TraceTrait {
 public:
     // Default implementation of TraceTrait<T>::trace just statically
     // dispatches to the trace method of the class T.
-    template<typename TraceDispatcher>
-    static void trace(TraceDispatcher visitor, void* self)
+    template<typename VisitorDispatcher>
+    static void trace(VisitorDispatcher visitor, void* self)
     {
         TraceCompatibilityAdaptor<T>::trace(visitor, static_cast<T*>(self));
     }
 
-    static void mark(Visitor* visitor, const T* t)
+    template<typename VisitorDispatcher>
+    static void mark(VisitorDispatcher visitor, const T* t)
     {
         DefaultTraceTrait<T>::mark(visitor, t);
     }
@@ -670,7 +671,8 @@ template<typename T, size_t N>
 struct OffHeapCollectionTraceTrait<WTF::Vector<T, N, WTF::DefaultAllocator> > {
     typedef WTF::Vector<T, N, WTF::DefaultAllocator> Vector;
 
-    static void trace(Visitor* visitor, const Vector& vector)
+    template<typename VisitorDispatcher>
+    static void trace(VisitorDispatcher visitor, const Vector& vector)
     {
         if (vector.isEmpty())
             return;
@@ -683,7 +685,8 @@ template<typename T, size_t N>
 struct OffHeapCollectionTraceTrait<WTF::Deque<T, N> > {
     typedef WTF::Deque<T, N> Deque;
 
-    static void trace(Visitor* visitor, const Deque& deque)
+    template<typename VisitorDispatcher>
+    static void trace(VisitorDispatcher visitor, const Deque& deque)
     {
         if (deque.isEmpty())
             return;
@@ -704,7 +707,8 @@ public:
 template<typename T>
 class DefaultTraceTrait<T, false> {
 public:
-    static void mark(Visitor* visitor, const T* t)
+    template<typename VisitorDispatcher>
+    static void mark(VisitorDispatcher visitor, const T* t)
     {
         // Default mark method of the trait just calls the two-argument mark
         // method on the visitor. The second argument is the static trace method
@@ -744,7 +748,8 @@ public:
 template<typename T>
 class DefaultTraceTrait<T, true> {
 public:
-    static void mark(Visitor* visitor, const T* self)
+    template<typename VisitorDispatcher>
+    static void mark(VisitorDispatcher visitor, const T* self)
     {
         if (!self)
             return;
