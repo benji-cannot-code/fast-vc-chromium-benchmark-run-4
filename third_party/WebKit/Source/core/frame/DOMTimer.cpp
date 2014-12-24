83fc9e577ccf786229556110f001deb542e8b4a1
enne@chromium.org <enne@chromium.org>
2014-12-24 01:17:31 +0000

    Revert of Oilpan: Remove redundant LifecycleObserver from DOMTimer::fired (patchset #1 id:1 of https://codereview.chromium.org/823773003/)
    Reason for revert:
    Makes ASAN bots red.
    
    STDERR: ==8== WARNING: MemorySanitizer: use-of-uninitialized-value
    STDERR:     #0 0x7f4ebe7c540e in blink::DOMTimer::fired() third_party/WebKit/Source/core/frame/DOMTimer.cpp:168:9
    STDERR:     #1 0x7f4ec5bc8b4c in blink::ThreadTimers::sharedTimerFiredInternal() third_party/WebKit/Source/platform/ThreadTimers.cpp:137:9
    STDERR:     #2 0x7f4ec5bc7fef in blink::ThreadTimers::sharedTimerFired() third_party/WebKit/Source/platform/ThreadTimers.cpp:107:5
    STDERR:     #3 0x7f4ec451bae7 in Run base/callback.h:396:12
    STDERR:     #4 0x7f4ec451bae7 in base::Timer::RunScheduledTask() base/timer/timer.cc:211:0
    
    Original issue's description:
    > Oilpan: Remove redundant LifecycleObserver from DOMTimer::fired
    > 
    > It is redundant to create a LifecycleObserver in DOMTimer::fired, because DOMTimer is an ActiveDOMObject and thus it can just peek executionContext() to know whether its associated ExecutionContext is already gone or not.
    > 
    > BUG=340522
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=187649
    
    NOTRY=true
    TBR=oilpan-reviews@chromium.org,sigbjornf@opera.com,haraken@chromium.org
    BUG=340522
    
    Review URL: https://codereview.chromium.org/821313002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187668 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/DOMTimer.cpp
+++ third_party/WebKit/Source/core/frame/DOMTimer.cpp
@@ -124,7 +124,6 @@ int DOMTimer::timeoutID() const
 void DOMTimer::fired()
 {
     ExecutionContext* context = executionContext();
-    ASSERT(context);
     context->setTimerNestingLevel(m_nestingLevel);
     ASSERT(!context->activeDOMObjectsAreSuspended());
     // Only the first execution of a multi-shot timer should get an affirmative user gesture indicator.
@@ -157,6 +156,8 @@ void DOMTimer::fired()
     // Delete timer before executing the action for one-shot timers.
     OwnPtr<ScheduledAction> action = m_action.release();
 
+    LifecycleContext<ExecutionContext>::Observer observer(context);
+
     // This timer is being deleted; no access to member variables allowed after this point.
     context->removeTimeoutByID(m_timeoutID);
 
@@ -165,8 +166,7 @@ void DOMTimer::fired()
     InspectorInstrumentation::didFireTimer(cookie);
     TRACE_EVENT_INSTANT1(TRACE_DISABLED_BY_DEFAULT("devtools.timeline"), "UpdateCounters", "data", InspectorUpdateCountersEvent::data());
 
-    // ExecutionContext might be already gone when we executed action->execute().
-    if (executionContext())
+    if (observer.lifecycleContext())
         context->setTimerNestingLevel(0);
 }
 
