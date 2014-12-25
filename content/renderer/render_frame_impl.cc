562d1670a122198d1e8211ead9f414bd1ee4a4e7
avi <avi@chromium.org>
2014-12-25 14:57:15 -0800

    Clean up unneeded params from PrepareRenderViewForNavigation.
    BUG=431853
    TEST=no change
    
    Review URL: https://codereview.chromium.org/822343002
    
    Cr-Commit-Position: refs/heads/master@{#309651}

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -962,10 +962,8 @@ void RenderFrameImpl::OnNavigate(const FrameMsg_Navigate_Params& params) {
   WebURLRequest::CachePolicy cache_policy =
       WebURLRequest::UseProtocolCachePolicy;
   if (!RenderFrameImpl::PrepareRenderViewForNavigation(
-      params.common_params.url, params.common_params.navigation_type,
-      params.commit_params.page_state, true, is_history_navigation,
-      params.current_history_list_offset, params.page_id, &is_reload,
-      &cache_policy)) {
+      params.common_params.url, true, is_history_navigation,
+      params.current_history_list_offset, &is_reload, &cache_policy)) {
     Send(new FrameHostMsg_DidDropNavigation(routing_id_));
     return;
   }
@@ -3759,10 +3757,9 @@ void RenderFrameImpl::OnCommitNavigation(
   WebURLRequest::CachePolicy cache_policy =
       WebURLRequest::UseProtocolCachePolicy;
   if (!RenderFrameImpl::PrepareRenderViewForNavigation(
-      common_params.url, common_params.navigation_type,
-      commit_params.page_state, false /* check_for_stale_navigation */,
+      common_params.url, false /* check_for_stale_navigation */,
       is_history_navigation, -1 /* current_history_list_offset; TODO(clamy)*/,
-      -1, &is_reload, &cache_policy)) {
+      &is_reload, &cache_policy)) {
     return;
   }
 
@@ -4183,12 +4180,9 @@ RenderFrameImpl::CreateRendererFactory() {
 
 bool RenderFrameImpl::PrepareRenderViewForNavigation(
     const GURL& url,
-    FrameMsg_Navigate_Type::Value navigate_type,
-    const PageState& state,
     bool check_for_stale_navigation,
     bool is_history_navigation,
     int current_history_list_offset,
-    int32 page_id,
     bool* is_reload,
     WebURLRequest::CachePolicy* cache_policy) {
   MaybeHandleDebugURL(url);
