5176ee72063220173a071f9f38cac47740f23b7f
kouhei@chromium.org <kouhei@chromium.org>
2014-12-26 07:16:11 +0000

    Remove unnecessary include from Heap.cpp
    BUG=None
    
    Review URL: https://codereview.chromium.org/806733008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187708 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/Init.cpp
+++ third_party/WebKit/Source/core/Init.cpp
@@ -57,7 +57,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/FontFamilyNames.h"
 #include "platform/Partitions.h"
 #include "platform/PlatformThreadData.h"
-#include "platform/heap/Heap.h"
 #include "wtf/text/StringStatics.h"
 
 namespace blink {
