2ac4b0285028b028a556494166e2119aa1dcc18c
aiolos@chromium.org <aiolos@chromium.org>
2014-12-29 19:38:08 +0000

    Revert of Eliminate FloatRect::inflate() calls for simple <rect> stroke hittest (patchset #1 id:1 of https://codereview.chromium.org/826103002/)
    Reason for revert:
    Speculative revert for Issue 445410:	199.3%-223.4%
    regression in blink_perf.svg at 309693:309695.
    
    BUG=445410
    
    Original issue's description:
    > Eliminate FloatRect::inflate() calls for simple <rect> stroke hittest
    > 
    > In RenderSVGRect::shapeDependentStrokeContains(), eliminate calls to
    > inflate(). Utilize rect symmetry to simplify the hit testing calculations.
    > 
    > This is a follow-up to http://src.chromium.org/viewvc/blink?view=rev&rev=187728
    > 
    > BUG=350338
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=187730
    
    TBR=pdr@chromium.org,fs@opera.com,ed@opera.com,dtrebbien@gmail.com
    NOTREECHECKS=true
    NOTRY=true
    BUG=350338
    
    Review URL: https://codereview.chromium.org/817353004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187755 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/svg/RenderSVGRect.cpp
+++ third_party/WebKit/Source/core/rendering/svg/RenderSVGRect.cpp
@@ -31,8 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/rendering/svg/RenderSVGRect.h"
 #include "core/svg/SVGRectElement.h"
 
-#include <cmath>
-
 namespace blink {
 
 RenderSVGRect::RenderSVGRect(SVGRectElement* node)
@@ -92,18 +90,14 @@ bool RenderSVGRect::shapeDependentStrokeContains(const FloatPoint& point)
     }
 
     const float halfStrokeWidth = this->strokeWidth() / 2;
-    const float halfWidth = m_fillBoundingBox.width() / 2;
-    const float halfHeight = m_fillBoundingBox.height() / 2;
-
-    const FloatPoint fillBoundingBoxCenter = FloatPoint(m_fillBoundingBox.x() + halfWidth, m_fillBoundingBox.y() + halfHeight);
-    const float absDeltaX = std::abs(point.x() - fillBoundingBoxCenter.x());
-    const float absDeltaY = std::abs(point.y() - fillBoundingBoxCenter.y());
-
-    if (!(absDeltaX <= halfWidth + halfStrokeWidth && absDeltaY <= halfHeight + halfStrokeWidth))
+    FloatRect outerStrokeRect = m_fillBoundingBox;
+    outerStrokeRect.inflate(halfStrokeWidth);
+    if (!outerStrokeRect.contains(point, FloatRect::InsideOrOnStroke))
         return false;
 
-    return (halfWidth - halfStrokeWidth <= absDeltaX)
-        || (halfHeight - halfStrokeWidth <= absDeltaY);
+    FloatRect innerStrokeRect = m_fillBoundingBox;
+    innerStrokeRect.inflate(-halfStrokeWidth);
+    return !innerStrokeRect.contains(point, FloatRect::InsideButNotOnStroke);
 }
 
 bool RenderSVGRect::shapeDependentFillContains(const FloatPoint& point, const WindRule fillRule) const
