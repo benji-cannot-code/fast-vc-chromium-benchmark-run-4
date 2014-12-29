e8ab142f939e265675035fea1270ab81c1037546
jbroman@chromium.org <jbroman@chromium.org>
2014-12-29 17:30:25 +0000

    Remove FloatBoxExtent.
    Its only use treats it like a FloatRect.
    
    This also fixes incorrect rounding in Font::floatWidthForComplexText, which was
    rounding under the wrong assumption that HarfBuzzShaper's output should be
    treated as a set of outsets.
    
    BUG=444921,445050
    
    Review URL: https://codereview.chromium.org/818653003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187749 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/blink_platform.gypi
+++ third_party/WebKit/Source/platform/blink_platform.gypi
@@ -413,7 +413,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'geometry/DoublePoint.h',
       'geometry/DoubleSize.cpp',
       'geometry/DoubleSize.h',
-      'geometry/FloatBoxExtent.h',
       'geometry/FloatPoint.cpp',
       'geometry/FloatPoint.h',
       'geometry/FloatPoint3D.cpp',
--- third_party/WebKit/Source/platform/fonts/shaping/HarfBuzzShaper.h
+++ third_party/WebKit/Source/platform/fonts/shaping/HarfBuzzShaper.h
@@ -33,8 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define HarfBuzzShaper_h
 
 #include "hb.h"
-#include "platform/geometry/FloatBoxExtent.h"
 #include "platform/geometry/FloatPoint.h"
+#include "platform/geometry/FloatRect.h"
 #include "platform/text/TextRun.h"
 #include "wtf/HashSet.h"
 #include "wtf/OwnPtr.h"
@@ -64,7 +64,7 @@ public:
     float totalWidth() { return m_totalWidth; }
     int offsetForPosition(float targetX);
     FloatRect selectionRect(const FloatPoint&, int height, int from, int to);
-    FloatBoxExtent glyphBoundingBox() const { return m_glyphBoundingBox; }
+    const FloatRect& glyphBoundingBox() const { return m_glyphBoundingBox; }
 
 private:
     class HarfBuzzRun {
@@ -157,7 +157,7 @@ private:
     ForTextEmphasisOrNot m_forTextEmphasis;
 
     float m_totalWidth;
-    FloatBoxExtent m_glyphBoundingBox;
+    FloatRect m_glyphBoundingBox;
     HashSet<const SimpleFontData*>* m_fallbackFonts;
 
     friend struct CachedShapingResults;
