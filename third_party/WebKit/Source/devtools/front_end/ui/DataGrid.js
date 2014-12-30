22a7681bdb5928b0814f4f6f257947b32c801f74
aandrey@chromium.org <aandrey@chromium.org>
2014-12-30 11:43:26 +0000

    DevTools: Improve Promise pane UI.
    BUG=442724
    R=pfeldman, eustas
    
    Review URL: https://codereview.chromium.org/792213004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187778 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ui/DataGrid.js
+++ third_party/WebKit/Source/devtools/front_end/ui/DataGrid.js
@@ -1386,8 +1386,7 @@ WebInspector.DataGridNode.prototype = {
         if (child.parent === this)
             throw("insertChild: Node is already a child of this node.");
 
-        if (child.parent)
-            child.parent.removeChild(child);
+        child.remove();
 
         this.children.splice(index, 0, child);
         this.hasChildren = true;
@@ -1415,6 +1414,12 @@ WebInspector.DataGridNode.prototype = {
             child._attach();
         if (!this.revealed)
             child.revealed = false;
+    },
+
+    remove: function()
+    {
+        if (this.parent)
+            this.parent.removeChild(this);
     },
 
     /**
