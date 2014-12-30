70febafe566908f139c49c345861dcc42b73a548
vmpstr <vmpstr@chromium.org>
2014-12-30 11:54:23 -0800

    cc: Clean up tile size usage.
    This patch attempts to make a clear separation between the texture
    size that the tile uses and the content rect. Currently, both of these
    are the same. However, I want to try and see if it is possible to
    make content_rect be exactly the area that will be drawn by the raster
    source (ie, clipped to raster source (or tiling data) bounds).
    
    Also, since the size will be retrieved from the resource instead
    of tile, it should make it possible to give tiles textures that
    are bigger than content rect (to maximize reuse). However, that should
    be a separate discussion.
    
    R=enne
    
    Review URL: https://codereview.chromium.org/797023004
    
    Cr-Commit-Position: refs/heads/master@{#309771}

--- cc/resources/picture_layer_tiling_set.h
+++ cc/resources/picture_layer_tiling_set.h
@@ -127,8 +127,6 @@ class CC_EXPORT PictureLayerTilingSet {
     gfx::Rect geometry_rect() const;
     // Texture rect (in texels) for geometry_rect
     gfx::RectF texture_rect() const;
-    // Texture size in texels
-    gfx::Size texture_size() const;
 
     Tile* operator->() const;
     Tile* operator*() const;
