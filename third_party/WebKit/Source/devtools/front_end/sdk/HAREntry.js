f313f74aca9d5339211eafa61db097a97ed019f1
eustas@chromium.org <eustas@chromium.org>
2014-12-30 07:41:09 +0000

    DevTools: HAR export: add custom transfer size response field.
    This will be useful for developers who use SPDY, as headerSize and bodySize
    are unknown in that case.
    
    BUG=444587
    
    Review URL: https://codereview.chromium.org/825243002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187768 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sdk/HAREntry.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/HAREntry.js
@@ -105,6 +105,7 @@ WebInspector.HAREntry.prototype = {
             redirectURL: this._request.responseHeaderValue("Location") || "",
             headersSize: headersText ? headersText.length : -1,
             bodySize: this.responseBodySize,
+            _transferSize: this._request.transferSize,
             _error: this._request.localizedFailDescription
         };
     },
@@ -143,7 +144,7 @@ WebInspector.HAREntry.prototype = {
                 if (values[i] >= 0)
                     return values[i];
             }
-            console.assert(false, "Incomplete requet timing information.");
+            console.assert(false, "Incomplete request timing information.");
         }
 
         var blocked = firstNonNegative([timing.dnsStart, timing.connectStart, timing.sendStart]);
