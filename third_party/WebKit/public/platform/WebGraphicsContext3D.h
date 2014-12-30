5344800aa3df0e36135daa3e4438d123e71dff6f
siva.gunturi@samsung.com <siva.gunturi@samsung.com>
2014-12-30 04:34:27 +0000

    Layout test for validating GLinfo on Webglcontextcreationerror.
    This test uses the forceNextWebGLContextCreationToFail(bool)
    to fail the webgl context forcefully from JS-test runner
    utility and validates the status message for GLinfo generated
    by webglcontextcreationerror event.
    
    BUG=412440
    
    Review URL: https://codereview.chromium.org/715413003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187766 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebGraphicsContext3D.h
+++ third_party/WebKit/public/platform/WebGraphicsContext3D.h
@@ -65,6 +65,7 @@ struct WebGLInfo {
     WebString vendorInfo;
     WebString rendererInfo;
     WebString driverVersion;
+    WebString contextInfoCollectionFailure;
 };
 
 // This interface abstracts the operations performed by the
