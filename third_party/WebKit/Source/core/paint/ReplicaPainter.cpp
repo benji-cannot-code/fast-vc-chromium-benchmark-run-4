30442906dae7f8b8dd68fb959e79cedbfe90605f
chrishtr@chromium.org <chrishtr@chromium.org>
2014-12-30 23:38:11 +0000

    [Slimming Paint] Fix mask painting for replaced/reflection content; add fast/css.
    Enable virtual/slimmingpaint/fast/css layout tests.
    
    Review URL: https://codereview.chromium.org/808163003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187793 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/paint/ReplicaPainter.cpp
+++ third_party/WebKit/Source/core/paint/ReplicaPainter.cpp
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/paint/ReplicaPainter.h"
 
 #include "core/paint/LayerPainter.h"
+#include "core/paint/RenderDrawingRecorder.h"
 #include "core/rendering/GraphicsContextAnnotator.h"
 #include "core/rendering/PaintInfo.h"
 #include "core/rendering/RenderLayer.h"
@@ -31,6 +32,8 @@ void ReplicaPainter::paint(const PaintInfo& paintInfo, const LayoutPoint& paintO
         PaintLayerFlags flags = PaintLayerHaveTransparency | PaintLayerAppliedTransform | PaintLayerUncachedClipRects | PaintLayerPaintingReflection;
         LayerPainter(*m_renderReplica.layer()->parent()).paintLayer(paintInfo.context, paintingInfo, flags);
     } else if (paintInfo.phase == PaintPhaseMask) {
+        LayoutRect paintRect(adjustedPaintOffset, m_renderReplica.size());
+        RenderDrawingRecorder renderDrawingRecorder(paintInfo.context, m_renderReplica, paintInfo.phase, paintRect);
         m_renderReplica.paintMask(paintInfo, adjustedPaintOffset);
     }
 }
