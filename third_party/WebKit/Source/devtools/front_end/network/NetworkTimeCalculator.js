926083952ad6b9aeda9694afabd35966042b2bf5
eustas@chromium.org <eustas@chromium.org>
2014-12-31 12:58:52 +0000

    DevTools: NetworkPanel: add "show primary load waterfall" feature.
    See demo screenshot in crbug.
    
    BUG=438774
    
    Review URL: https://codereview.chromium.org/786303003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187805 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/network/NetworkTimeCalculator.js
+++ third_party/WebKit/Source/devtools/front_end/network/NetworkTimeCalculator.js
@@ -60,6 +60,8 @@ WebInspector.NetworkTimeCalculator = function(startAtZero)
 {
     this.startAtZero = startAtZero;
     this._boundryChangedEventThrottler = new WebInspector.Throttler(0);
+    /** @type {?WebInspector.NetworkTimeBoundary} */
+    this._window = null;
 }
 
 /** @enum {string} */
@@ -84,6 +86,15 @@ WebInspector.NetworkTimeCalculator._fromCacheFormat = new WebInspector.UIStringF
 
 WebInspector.NetworkTimeCalculator.prototype = {
     /**
+     * @param {?WebInspector.NetworkTimeBoundary} window
+     */
+    setWindow: function(window)
+    {
+        this._window = window;
+        this._boundaryChanged();
+    },
+
+    /**
      * @override
      * @return {number}
      */
@@ -99,7 +110,7 @@ WebInspector.NetworkTimeCalculator.prototype = {
      */
     computePosition: function(time)
     {
-        return (time - this._minimumBoundary) / this.boundarySpan() * this._workingArea;
+        return (time - this.minimumBoundary()) / this.boundarySpan() * this._workingArea;
     },
 
     /**
@@ -119,7 +130,7 @@ WebInspector.NetworkTimeCalculator.prototype = {
      */
     minimumBoundary: function()
     {
-        return this._minimumBoundary;
+        return this._window ? this._window.minimum : this._minimumBoundary;
     },
 
     /**
@@ -137,7 +148,7 @@ WebInspector.NetworkTimeCalculator.prototype = {
      */
     maximumBoundary: function()
     {
-        return this._maximumBoundary;
+        return this._window ? this._window.maximum : this._maximumBoundary;
     },
 
     /**
@@ -154,7 +165,7 @@ WebInspector.NetworkTimeCalculator.prototype = {
      */
     boundarySpan: function()
     {
-        return this._maximumBoundary - this._minimumBoundary;
+        return this.maximumBoundary() - this.minimumBoundary();
     },
 
     reset: function()
@@ -187,17 +198,17 @@ WebInspector.NetworkTimeCalculator.prototype = {
     computeBarGraphPercentages: function(request)
     {
         if (request.startTime !== -1)
-            var start = ((request.startTime - this._minimumBoundary) / this.boundarySpan()) * 100;
+            var start = ((request.startTime - this.minimumBoundary()) / this.boundarySpan()) * 100;
         else
             var start = 0;
 
         if (request.responseReceivedTime !== -1)
-            var middle = ((request.responseReceivedTime - this._minimumBoundary) / this.boundarySpan()) * 100;
+            var middle = ((request.responseReceivedTime - this.minimumBoundary()) / this.boundarySpan()) * 100;
         else
             var middle = (this.startAtZero ? start : 100);
 
         if (request.endTime !== -1)
-            var end = ((request.endTime - this._minimumBoundary) / this.boundarySpan()) * 100;
+            var end = ((request.endTime - this.minimumBoundary()) / this.boundarySpan()) * 100;
         else
             var end = (this.startAtZero ? middle : 100);
 
@@ -220,7 +231,7 @@ WebInspector.NetworkTimeCalculator.prototype = {
         // of a specific event. If startAtZero is set, then this is useless, and we
         // want to return 0.
         if (eventTime !== -1 && !this.startAtZero)
-            return ((eventTime - this._minimumBoundary) / this.boundarySpan()) * 100;
+            return ((eventTime - this.minimumBoundary()) / this.boundarySpan()) * 100;
 
         return 0;
     },
