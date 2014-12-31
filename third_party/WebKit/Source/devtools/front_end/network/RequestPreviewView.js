a5024b6861933c3f4a81504029a1fa565c9dc58e
eustas@chromium.org <eustas@chromium.org>
2014-12-31 11:59:20 +0000

    DevTools: NetworkPanel: add preview for XML
    Screenshot:
    http://picpaste.com/liYoo3Cm.png
    
    BUG=99281
    
    Review URL: https://codereview.chromium.org/810903003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187802 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/network/RequestPreviewView.js
+++ third_party/WebKit/Source/devtools/front_end/network/RequestPreviewView.js
@@ -78,15 +78,32 @@ WebInspector.RequestPreviewView.prototype = {
     },
 
     /**
+     * @return {string}
+     */
+    _requestContent: function()
+    {
+        var content = this.request.content;
+        return this.request.contentEncoded ? window.atob(content || "") : (content || "");
+    },
+
+    /**
      * @return {?WebInspector.RequestJSONView}
      */
     _jsonView: function()
     {
-        var request = this.request;
-        var content = request.content;
-        content = request.contentEncoded ? window.atob(content || "") : (content || "");
+        var content = this._requestContent();
         var parsedJSON = WebInspector.RequestJSONView.parseJSON(content);
         return parsedJSON ? new WebInspector.RequestJSONView(this.request, parsedJSON) : null;
+    },
+
+    /**
+     * @return {?WebInspector.XMLView}
+     */
+    _xmlView: function()
+    {
+        var content = this._requestContent();
+        var parsedXML = WebInspector.XMLView.parseXML(content, this.request.mimeType);
+        return parsedXML ? new WebInspector.XMLView(parsedXML) : null;
     },
 
     /**
@@ -122,6 +139,10 @@ WebInspector.RequestPreviewView.prototype = {
             if (htmlErrorPreview)
                 return htmlErrorPreview;
         }
+
+        var xmlView = this._xmlView();
+        if (xmlView)
+            return xmlView;
 
         if (this.request.resourceType() === WebInspector.resourceTypes.XHR) {
             var jsonView = this._jsonView();
