1e7f59b70a4a3c910b2ace4dc481dfdc642311b9
tfarina <tfarina@chromium.org>
2015-01-04 07:11:31 -0800

    ui/gfx: Cleanup - Remove forward geometry headers.
    All the includes in Chromium and Blink were updated to the right paths in
    ui/gfx/geometry. So now we can remove these headers from ui/gfx.
    
    BUG=395370
    EST=compiles
    TBR=sky@chromium.org,danakj@chromium.org
    
    Review URL: https://codereview.chromium.org/836633002
    
    Cr-Commit-Position: refs/heads/master@{#309885}

--- content/common/gpu/media/vaapi_drm_picture.h
+++ content/common/gpu/media/vaapi_drm_picture.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/memory/ref_counted.h"
 #include "content/common/gpu/media/vaapi_picture.h"
-#include "ui/gfx/size.h"
+#include "ui/gfx/geometry/size.h"
 
 namespace gfx {
 class GLImageLinuxDMABuffer;
