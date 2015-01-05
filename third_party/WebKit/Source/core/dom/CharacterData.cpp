91e970f85cdad00766e214ad4c9effba658df8a8
rob.buis@samsung.com <rob.buis@samsung.com>
2015-01-05 20:51:53 +0000

    Devirtualize offsetInCharacters
    This method does not need to be virtual since we can just ask
    isCharacterDataNode(), since only for CharacterData nodes
    offsetInCharacters was returning true.
    
    Review URL: https://codereview.chromium.org/792953006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187867 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/CharacterData.cpp
+++ third_party/WebKit/Source/core/dom/CharacterData.cpp
@@ -208,9 +208,4 @@ int CharacterData::maxCharacterOffset() const
     return static_cast<int>(length());
 }
 
-bool CharacterData::offsetInCharacters() const
-{
-    return true;
-}
-
 } // namespace blink
