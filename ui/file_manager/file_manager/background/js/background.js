e12c5f5c0ce7b208163a275adc4b945dbe19ebcf
smckay <smckay@chromium.org>
2015-01-05 12:14:30 -0800

    Manually plumb in history updates when import copy stage is complete.
    This effectively enables:
    1) visible badging of previously imported files
    2) previously copied/imported file deduplication
    
    BUG=420680
    TEST=browser_test: FileManagerJsTest.*
    
    Review URL: https://codereview.chromium.org/802343006
    
    Cr-Commit-Position: refs/heads/master@{#309948}

--- ui/file_manager/file_manager/background/js/background.js
+++ ui/file_manager/file_manager/background/js/background.js
@@ -94,7 +94,8 @@ function FileBrowserBackground() {
    */
   this.mediaImportHandler =
       new importer.MediaImportHandler(
-          this.fileOperationManager);
+          this.fileOperationManager,
+          this.historyLoader);
 
   /**
    * Promise of string data.
