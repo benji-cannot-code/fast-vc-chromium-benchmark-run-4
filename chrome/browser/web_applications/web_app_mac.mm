a117c2084c57c5d6465dc584a4bcc50bf074180a
mitchelljones <mitchelljones@chromium.org>
2015-01-04 18:34:32 -0800

    "Create application shortcut" dialog box now dismisses on Esc.
    BUG=444542
    
    Review URL: https://codereview.chromium.org/823273005
    
    Cr-Commit-Position: refs/heads/master@{#309889}

--- chrome/browser/web_applications/web_app_mac.mm
+++ chrome/browser/web_applications/web_app_mac.mm
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/shell_integration.h"
 #include "chrome/browser/ui/app_list/app_list_service.h"
+#include "chrome/browser/ui/cocoa/key_equivalent_constants.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
@@ -1038,11 +1039,11 @@ void CreateAppShortcutInfoLoaded(
 
   NSButton* continue_button = [alert
       addButtonWithTitle:l10n_util::GetNSString(IDS_CREATE_SHORTCUTS_COMMIT)];
-  [continue_button setKeyEquivalent:@""];
+  [continue_button setKeyEquivalent:kKeyEquivalentReturn];
 
   NSButton* cancel_button =
       [alert addButtonWithTitle:l10n_util::GetNSString(IDS_CANCEL)];
-  [cancel_button setKeyEquivalent:@"\r"];
+  [cancel_button setKeyEquivalent:kKeyEquivalentEscape];
 
   [alert setMessageText:l10n_util::GetNSString(IDS_CREATE_SHORTCUTS_LABEL)];
   [alert setAlertStyle:NSInformationalAlertStyle];
