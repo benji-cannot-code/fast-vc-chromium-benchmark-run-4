14797496ee7fa6235b874af813903a932ae94111
satorux <satorux@chromium.org>
2015-01-04 18:40:27 -0800

    Remove chrome://net-internals#logs
    This Chrome OS only feature is now broken and no longer necessary
    because logs can now be seen from chrome://system.
    
    BUG=443503
    TEST=open chrome:net-internals and confirm that "logs" menu item is gone.
    
    Review URL: https://codereview.chromium.org/812323002
    
    Cr-Commit-Position: refs/heads/master@{#309890}

--- chrome/browser/resources/net_internals/index.js
+++ chrome/browser/resources/net_internals/index.js
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <include src="sockets_view.js">
 <include src="spdy_view.js">
 <include src="modules_view.js">
-<include src="logs_view.js">
 <include src="prerender_view.js">
 <include src="chromeos_view.js">
 <include src="bandwidth_view.js">
--- chrome/test/data/webui/net_internals/main.js
+++ chrome/test/data/webui/net_internals/main.js
@@ -38,7 +38,6 @@ TEST_F('NetInternalsTest', 'netInternalsTourTabs', function() {
     modules: true,
     tests: true,
     hsts: true,
-    logs: cr.isChromeOS,
     prerender: true,
     bandwidth: true,
     chromeos: cr.isChromeOS,
