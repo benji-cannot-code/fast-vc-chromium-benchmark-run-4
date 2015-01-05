7e3c36850ebf1f01244d87e2fb629133ccef92d6
bondd <bondd@chromium.org>
2015-01-05 13:55:44 -0800

    Make shift+tab focus close(aka delete) buttons on autofillEditAddress page.
    BUG=440646
    
    Review URL: https://codereview.chromium.org/821863002
    
    Cr-Commit-Position: refs/heads/master@{#309962}

--- chrome/browser/resources/options/autofill_options_list.js
+++ chrome/browser/resources/options/autofill_options_list.js
@@ -152,6 +152,8 @@ cr.define('options.autofillOptions', function() {
 
       this.addEventListener('commitedit', this.onEditCommitted_);
       this.closeButtonFocusAllowed = true;
+      this.setFocusableColumnIndex(this.input, 0);
+      this.setFocusableColumnIndex(this.closeButtonElement, 1);
     },
 
     /**
