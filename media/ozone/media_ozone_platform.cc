d99fb912dcdd689bf1a7c8a419eae571d98f3c64
gunsch <gunsch@chromium.org>
2015-01-05 14:55:38 -0800

    virtual/override specifier cleanup in media/.
    These are changes detected by Chromecast not covered by the Linux build.
    
    See: https://codereview.chromium.org/831863003
    
    R=xhwang@chromium.org
    BUG=417463
    
    Review URL: https://codereview.chromium.org/829413002
    
    Cr-Commit-Position: refs/heads/master@{#309979}

--- media/ozone/media_ozone_platform.cc
+++ media/ozone/media_ozone_platform.cc
@@ -18,7 +18,7 @@ class MediaOzonePlatformStub : public MediaOzonePlatform {
  public:
   MediaOzonePlatformStub() {}
 
-  virtual ~MediaOzonePlatformStub() {}
+  ~MediaOzonePlatformStub() override {}
 
  private:
   DISALLOW_COPY_AND_ASSIGN(MediaOzonePlatformStub);
