489b7b204fb5d05b2286f6bd620433fd30062603
kouhei@chromium.org <kouhei@chromium.org>
2015-01-05 01:51:14 +0000

    Make CharacterData::parserAppendData use common update codepath
    CharacterData has two method to append data, CharacterData::appendData and CharacterData::parserAppendData. The latter is only used from changes made from parser.
    
    Before this CL, CharacterData::parserAppendData used different codepath from other CharacterData:: methods.
    This CL changes it to use the same setDataAndUpdate helper method, so that mutation observer is properly fired.
    
    A new argument UpdateSource is added so that setDataAndUpdate can skip DOMMutationEvents, processing instruction, and selection update which are irrelevant to parser changes.
    Spec: https://html.spec.whatwg.org/multipage/syntax.html#insert-a-character
    
    TEST=http/tests/dom/mutationobserver-parserappend-childlist.php
    BUG=440781
    
    Review URL: https://codereview.chromium.org/818633003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187836 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/CharacterData.cpp
+++ third_party/WebKit/Source/core/dom/CharacterData.cpp
@@ -52,7 +52,7 @@ void CharacterData::setData(const String& data)
 
     unsigned oldLength = length();
 
-    setDataAndUpdate(nonNullData, 0, oldLength, nonNullData.length());
+    setDataAndUpdate(nonNullData, 0, oldLength, nonNullData.length(), UpdateFromNonParser);
     document().didRemoveText(this, 0, oldLength);
 }
 
@@ -66,28 +66,18 @@ String CharacterData::substringData(unsigned offset, unsigned count, ExceptionSt
     return m_data.substring(offset, count);
 }
 
-void CharacterData::parserAppendData(const String& string)
+void CharacterData::parserAppendData(const String& data)
 {
-    unsigned oldLength = m_data.length();
-    m_data = m_data + string;
+    String newStr = m_data + data;
 
-    ASSERT(!renderer() || isTextNode());
-    if (isTextNode())
-        toText(this)->updateTextRenderer(oldLength, 0);
-
-    document().incDOMTreeVersion();
-
-    if (parentNode()) {
-        ContainerNode::ChildrenChange change = {ContainerNode::TextChanged, previousSibling(), nextSibling(), ContainerNode::ChildrenChangeSourceParser};
-        parentNode()->childrenChanged(change);
-    }
+    setDataAndUpdate(newStr, m_data.length(), 0, data.length(), UpdateFromParser);
 }
 
 void CharacterData::appendData(const String& data)
 {
     String newStr = m_data + data;
 
-    setDataAndUpdate(newStr, m_data.length(), 0, data.length());
+    setDataAndUpdate(newStr, m_data.length(), 0, data.length(), UpdateFromNonParser);
 
     // FIXME: Should we call textInserted here?
 }
@@ -102,7 +92,7 @@ void CharacterData::insertData(unsigned offset, const String& data, ExceptionSta
     String newStr = m_data;
     newStr.insert(data, offset);
 
-    setDataAndUpdate(newStr, offset, 0, data.length(), recalcStyleBehavior);
+    setDataAndUpdate(newStr, offset, 0, data.length(), UpdateFromNonParser, recalcStyleBehavior);
 
     document().didInsertText(this, offset, data.length());
 }
@@ -134,7 +124,7 @@ void CharacterData::deleteData(unsigned offset, unsigned count, ExceptionState& 
     String newStr = m_data;
     newStr.remove(offset, realCount);
 
-    setDataAndUpdate(newStr, offset, realCount, 0, recalcStyleBehavior);
+    setDataAndUpdate(newStr, offset, realCount, 0, UpdateFromNonParser, recalcStyleBehavior);
 
     document().didRemoveText(this, offset, realCount);
 }
@@ -149,7 +139,7 @@ void CharacterData::replaceData(unsigned offset, unsigned count, const String& d
     newStr.remove(offset, realCount);
     newStr.insert(data, offset);
 
-    setDataAndUpdate(newStr, offset, realCount, data.length());
+    setDataAndUpdate(newStr, offset, realCount, data.length(), UpdateFromNonParser);
 
     // update the markers for spell checking and grammar checking
     document().didRemoveText(this, offset, realCount);
@@ -171,7 +161,7 @@ void CharacterData::setNodeValue(const String& nodeValue)
     setData(nodeValue);
 }
 
-void CharacterData::setDataAndUpdate(const String& newData, unsigned offsetOfReplacedData, unsigned oldLength, unsigned newLength, RecalcStyleBehavior recalcStyleBehavior)
+void CharacterData::setDataAndUpdate(const String& newData, unsigned offsetOfReplacedData, unsigned oldLength, unsigned newLength, UpdateSource source, RecalcStyleBehavior recalcStyleBehavior)
 {
     String oldData = m_data;
     m_data = newData;
@@ -180,17 +170,19 @@ void CharacterData::setDataAndUpdate(const String& newData, unsigned offsetOfRep
     if (isTextNode())
         toText(this)->updateTextRenderer(offsetOfReplacedData, oldLength, recalcStyleBehavior);
 
-    if (nodeType() == PROCESSING_INSTRUCTION_NODE)
-        toProcessingInstruction(this)->didAttributeChanged();
+    if (source != UpdateFromParser) {
+        if (nodeType() == PROCESSING_INSTRUCTION_NODE)
+            toProcessingInstruction(this)->didAttributeChanged();
 
-    if (document().frame())
-        document().frame()->selection().didUpdateCharacterData(this, offsetOfReplacedData, oldLength, newLength);
+        if (document().frame())
+            document().frame()->selection().didUpdateCharacterData(this, offsetOfReplacedData, oldLength, newLength);
+    }
 
     document().incDOMTreeVersion();
-    didModifyData(oldData);
+    didModifyData(oldData, source);
 }
 
-void CharacterData::didModifyData(const String& oldData)
+void CharacterData::didModifyData(const String& oldData, UpdateSource source)
 {
     if (OwnPtrWillBeRawPtr<MutationObserverInterestGroup> mutationRecipients = MutationObserverInterestGroup::createForCharacterDataMutation(*this))
         mutationRecipients->enqueueMutationRecord(MutationRecord::createCharacterData(this, oldData));
@@ -200,7 +192,10 @@ void CharacterData::didModifyData(const String& oldData)
         parentNode()->childrenChanged(change);
     }
 
-    if (!isInShadowTree()) {
+    // Skip DOM mutation events if the modification is from parser.
+    // Note that mutation observer events will still fire.
+    // Spec: https://html.spec.whatwg.org/multipage/syntax.html#insert-a-character
+    if (source != UpdateFromParser && !isInShadowTree()) {
         if (document().hasListenerType(Document::DOMCHARACTERDATAMODIFIED_LISTENER))
             dispatchScopedEvent(MutationEvent::create(EventTypeNames::DOMCharacterDataModified, true, nullptr, oldData, m_data));
         dispatchSubtreeModifiedEvent();
