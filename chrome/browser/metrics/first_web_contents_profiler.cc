82d8c9dfeeba6f3fd7f53c50784311039c73b05a
erikchen <erikchen@chromium.org>
2015-01-05 14:07:44 -0800

    Collect Startup.FirstWebContents metrics on all channels.
    I was running an experiment to see if reducing the bucket width of the
    histograms would increase the precision of the measurements (specifically the
    50th and 90th percentiles). The answer is negative. Having more buckets
    resulted in a difference of less than 0.1%. This CL removes the experimental
    metrics, and replaces them with Startup.FirstWebContents.MainFrameLoad and
    Startup.FirstWebContents.NonEmptyPaint, using standard bucket ranges.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/816883003
    
    Cr-Commit-Position: refs/heads/master@{#309964}

--- chrome/browser/metrics/first_web_contents_profiler.cc
+++ chrome/browser/metrics/first_web_contents_profiler.cc
@@ -13,22 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_iterator.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
-#include "chrome/common/chrome_version_info.h"
-
-namespace {
-
-const int kHistogramMinTimeMilliseconds = 200;
-const int kHistogramMaxTimeSeconds = 45;
-
-// There is a lot of noise in the dev channel. One possible cause is that the
-// bucket ranges are too wide. The bucket count is chosen such that the size of
-// the bucket is 1% of its minimum. Notice that this also means that
-// subsequent buckets will satisfy Min(bucket N + 1) / Min(bucket N) = 1.01.
-// Then we want X, such that 1.01 ^ X = 45 * 1000 / 200. Some quick math shows
-// that X = 544.
-const int kHistogramBucketCount = 544;
-
-}  // namespace
 
 scoped_ptr<FirstWebContentsProfiler>
 FirstWebContentsProfiler::CreateProfilerForFirstWebContents(
@@ -44,12 +28,6 @@ FirstWebContentsProfiler::CreateProfilerForFirstWebContents(
     }
   }
   return nullptr;
-}
-
-bool FirstWebContentsProfiler::ShouldCollectMetrics() {
-  chrome::VersionInfo::Channel channel = chrome::VersionInfo::GetChannel();
-  return channel == chrome::VersionInfo::CHANNEL_CANARY ||
-         channel == chrome::VersionInfo::CHANNEL_DEV;
 }
 
 FirstWebContentsProfiler::FirstWebContentsProfiler(
@@ -70,19 +48,8 @@ void FirstWebContentsProfiler::DidFirstVisuallyNonEmptyPaint() {
   if (!process_creation_time_.is_null()) {
     base::TimeDelta elapsed = base::Time::Now() - process_creation_time_;
 
-    // TODO(erikchen): Revisit these metrics once data has been collected to
-    // determine whether using more buckets reduces noise and provides higher
-    // quality information.
-    UMA_HISTOGRAM_CUSTOM_TIMES(
-        "Startup.Experimental.FirstWebContents.NonEmptyPaint."
-        "ManyBuckets",
-        elapsed,
-        base::TimeDelta::FromMilliseconds(kHistogramMinTimeMilliseconds),
-        base::TimeDelta::FromSeconds(kHistogramMaxTimeSeconds),
-        kHistogramBucketCount);
-    UMA_HISTOGRAM_LONG_TIMES_100(
-        "Startup.Experimental.FirstWebContents.NonEmptyPaint.StandardBuckets",
-        elapsed);
+    UMA_HISTOGRAM_LONG_TIMES_100("Startup.FirstWebContents.NonEmptyPaint",
+                                 elapsed);
   }
 
   if (IsFinishedCollectingMetrics())
@@ -97,19 +64,8 @@ void FirstWebContentsProfiler::DocumentOnLoadCompletedInMainFrame() {
   if (!process_creation_time_.is_null()) {
     base::TimeDelta elapsed = base::Time::Now() - process_creation_time_;
 
-    // TODO(erikchen): Revisit these metrics once data has been collected to
-    // determine whether using more buckets reduces noise and provides higher
-    // quality information.
-    UMA_HISTOGRAM_CUSTOM_TIMES(
-        "Startup.Experimental.FirstWebContents.MainFrameLoad."
-        "ManyBuckets",
-        elapsed,
-        base::TimeDelta::FromMilliseconds(kHistogramMinTimeMilliseconds),
-        base::TimeDelta::FromSeconds(kHistogramMaxTimeSeconds),
-        kHistogramBucketCount);
-    UMA_HISTOGRAM_LONG_TIMES_100(
-        "Startup.Experimental.FirstWebContents.MainFrameLoad.StandardBuckets",
-        elapsed);
+    UMA_HISTOGRAM_LONG_TIMES_100("Startup.FirstWebContents.MainFrameLoad",
+                                 elapsed);
   }
 
   if (IsFinishedCollectingMetrics())
