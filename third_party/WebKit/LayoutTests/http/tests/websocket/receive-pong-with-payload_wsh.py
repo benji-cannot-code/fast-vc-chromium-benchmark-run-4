d07f03664faf135c7f4eeaf767c3521af1998e01
ricea@chromium.org <ricea@chromium.org>
2015-01-06 09:16:17 +0000

    Layout test to verify pong message from the server.
    Issue crbug.com/443651 says that Chrome crashes on receiving pong
    messages with a payload. This does not appear to be true, but there
    is not an existing layout test testing this case, so add one.
    
    BUG=443651
    TEST=layout tests
    
    Review URL: https://codereview.chromium.org/788923007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187905 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/websocket/receive-pong-with-payload_wsh.py
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+def web_socket_do_extra_handshake(request):
+    pass
+
+
+def web_socket_transfer_data(request):
+    request.ws_stream._send_pong("{payload: 'yes'}")
+    request.ws_stream.send_message("sent pong")
+    line = request.ws_stream.receive_message()
