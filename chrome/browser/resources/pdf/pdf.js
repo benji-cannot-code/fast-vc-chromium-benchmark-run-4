8d2b76c63e7e8b0e67b20a9ce753798e7969084c
deepak.m1 <deepak.m1@samsung.com>
2015-01-05 19:51:55 -0800

    Fix for moving to previous url from PDF page in OOP case.
    alt,shift,ctrl are not considered while handling left or right arrow
    key in OOP case in PDF.
    Check added so that when left/right arrow key is selected then navigation
    with in the page will happen when alt,ctrl,shift are not selected.
    else default handling happen.
    
    BUG=446162
    
    Review URL: https://codereview.chromium.org/830853003
    
    Cr-Commit-Position: refs/heads/master@{#310045}

--- chrome/browser/resources/pdf/pdf.js
+++ chrome/browser/resources/pdf/pdf.js
@@ -199,14 +199,16 @@ PDFViewer.prototype = {
         pageDownHandler();
         return;
       case 37:  // Left arrow key.
-        // Go to the previous page if there are no horizontal scrollbars.
-        if (!this.viewport_.documentHasScrollbars().x) {
-          this.viewport_.goToPage(this.viewport_.getMostVisiblePage() - 1);
-          // Since we do the movement of the page.
-          e.preventDefault();
-        } else if (fromScriptingAPI) {
-          position.x -= Viewport.SCROLL_INCREMENT;
-          this.viewport.position = position;
+        if (!(e.altKey || e.ctrlKey || e.metaKey || e.shiftKey)) {
+          // Go to the previous page if there are no horizontal scrollbars.
+          if (!this.viewport_.documentHasScrollbars().x) {
+            this.viewport_.goToPage(this.viewport_.getMostVisiblePage() - 1);
+            // Since we do the movement of the page.
+            e.preventDefault();
+          } else if (fromScriptingAPI) {
+            position.x -= Viewport.SCROLL_INCREMENT;
+            this.viewport.position = position;
+          }
         }
         return;
       case 38:  // Up arrow key.
@@ -216,14 +218,16 @@ PDFViewer.prototype = {
         }
         return;
       case 39:  // Right arrow key.
-        // Go to the next page if there are no horizontal scrollbars.
-        if (!this.viewport_.documentHasScrollbars().x) {
-          this.viewport_.goToPage(this.viewport_.getMostVisiblePage() + 1);
-          // Since we do the movement of the page.
-          e.preventDefault();
-        } else if (fromScriptingAPI) {
-          position.x += Viewport.SCROLL_INCREMENT;
-          this.viewport.position = position;
+        if (!(e.altKey || e.ctrlKey || e.metaKey || e.shiftKey)) {
+          // Go to the next page if there are no horizontal scrollbars.
+          if (!this.viewport_.documentHasScrollbars().x) {
+            this.viewport_.goToPage(this.viewport_.getMostVisiblePage() + 1);
+            // Since we do the movement of the page.
+            e.preventDefault();
+          } else if (fromScriptingAPI) {
+            position.x += Viewport.SCROLL_INCREMENT;
+            this.viewport.position = position;
+          }
         }
         return;
       case 40:  // Down arrow key.
