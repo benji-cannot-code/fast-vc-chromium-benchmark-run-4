a8192480bb41741b050c02e8c62e520b9d7daac1
peria <peria@chromium.org>
2015-01-06 10:14:48 -0800

    Changed to check if either precise GC or conservative GC events run in tests.
    Updating Oilpan's infrastructure, its behavior may change.
    We expect that a GC runs in this test, but we cannot promise that
    it is a precise GC.
    
    Background:
    We expected precise GC ran in those tests, but at some point,
    they changed to call conservative GCs.
    
    BUG=446301
    TEST=./tools/perf/run_tests --browser=content-shell-release OilpanGCTimesTest
    
    Review URL: https://codereview.chromium.org/818653004
    
    Cr-Commit-Position: refs/heads/master@{#310111}

--- tools/perf/measurements/oilpan_gc_times_unittest.py
+++ tools/perf/measurements/oilpan_gc_times_unittest.py
@@ -4,8 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 from measurements import oilpan_gc_times
-from telemetry import decorators
-from telemetry.core import wpr_modes
 from telemetry.unittest_util import options_for_unittests
 from telemetry.unittest_util import page_test_test_case
 
@@ -19,22 +17,24 @@ class OilpanGCTimesTest(page_test_test_case.PageTestTestCase):
   def setUp(self):
     self._options = options_for_unittests.GetCopy()
 
-  @decorators.Disabled
   def testForSmoothness(self):
     ps = self.CreatePageSetFromFileInUnittestDataDir('create_many_objects.html')
     measurement = oilpan_gc_times.OilpanGCTimesForSmoothness()
     results = self.RunMeasurement(measurement, ps, options=self._options)
     self.assertEquals(0, len(results.failures))
 
-    precise_mark = results.FindAllPageSpecificValuesNamed('oilpan_precise_mark')
-    self.assertLess(0, len(precise_mark))
+    precise = results.FindAllPageSpecificValuesNamed('oilpan_precise_mark')
+    conservative = results.FindAllPageSpecificValuesNamed(
+        'oilpan_conservative_mark')
+    self.assertLess(0, len(precise) + len(conservative))
 
-  @decorators.Disabled
   def testForBlinkPerf(self):
     ps = self.CreatePageSetFromFileInUnittestDataDir('create_many_objects.html')
     measurement = oilpan_gc_times.OilpanGCTimesForBlinkPerf()
     results = self.RunMeasurement(measurement, ps, options=self._options)
     self.assertEquals(0, len(results.failures))
 
-    precise_mark = results.FindAllPageSpecificValuesNamed('oilpan_precise_mark')
-    self.assertLess(0, len(precise_mark))
+    precise = results.FindAllPageSpecificValuesNamed('oilpan_precise_mark')
+    conservative = results.FindAllPageSpecificValuesNamed(
+        'oilpan_conservative_mark')
+    self.assertLess(0, len(precise) + len(conservative))
