b0cdd47a3d0ccd10ced5560cc17e47c2c05230df
sigbjornf@opera.com <sigbjornf@opera.com>
2015-01-06 19:33:18 +0000

    Oilpan: fix build after r187923.
    TBR=oilpan-reviews,haraken
    BUG=425623
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/836023005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187929 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/core/v8/WindowProxyManager.h
+++ third_party/WebKit/Source/bindings/core/v8/WindowProxyManager.h
@@ -19,7 +19,7 @@ class ScriptState;
 class SecurityOrigin;
 class WindowProxy;
 
-class WindowProxyManager : public NoBaseWillBeGarbageCollected<WindowProxyManager> {
+class WindowProxyManager : public NoBaseWillBeGarbageCollectedFinalized<WindowProxyManager> {
 public:
     static PassOwnPtrWillBeRawPtr<WindowProxyManager> create(Frame*);
 
