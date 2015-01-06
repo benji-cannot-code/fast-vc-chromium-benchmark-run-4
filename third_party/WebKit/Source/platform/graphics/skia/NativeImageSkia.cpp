d87a57ec407ac9f3ead1a82381933d14a4fad3c3
chrishtr@chromium.org <chrishtr@chromium.org>
2015-01-06 15:12:37 +0000

    [Slim. P.] Don't scale raster buffers by content scale w/slimming paint.
    If needed, we can find an alternate way to raster that is not content
    scale-dependent.
    
    BUG=446265,382491
    
    Review URL: https://codereview.chromium.org/812743004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187917 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/skia/NativeImageSkia.cpp
+++ third_party/WebKit/Source/platform/graphics/skia/NativeImageSkia.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/graphics/skia/NativeImageSkia.h"
 
 #include "platform/PlatformInstrumentation.h"
+#include "platform/RuntimeEnabledFeatures.h"
 #include "platform/TraceEvent.h"
 #include "platform/geometry/FloatRect.h"
 #include "platform/graphics/DeferredImageDecoder.h"
@@ -104,9 +105,6 @@ void NativeImageSkia::drawPattern(
         return; // nothing to draw
 
     SkMatrix totalMatrix = context->getTotalMatrix();
-    AffineTransform ctm = context->getCTM();
-    SkScalar ctmScaleX = ctm.xScale();
-    SkScalar ctmScaleY = ctm.yScale();
     totalMatrix.preScale(scale.width(), scale.height());
 
     // Figure out what size the bitmap will be in the destination. The
@@ -147,6 +145,15 @@ void NativeImageSkia::drawPattern(
     SkBitmap bitmapToPaint;
     bitmap().extractSubset(&bitmapToPaint, enclosingIntRect(normSrcRect));
     if (!repeatSpacing.isZero()) {
+        SkScalar ctmScaleX = 1.0;
+        SkScalar ctmScaleY = 1.0;
+
+        if (!RuntimeEnabledFeatures::slimmingPaintEnabled()) {
+            AffineTransform ctm = context->getCTM();
+            ctmScaleX = ctm.xScale();
+            ctmScaleY = ctm.yScale();
+        }
+
         bitmapToPaint = createBitmapWithSpace(
             bitmapToPaint,
             repeatSpacing.width() * ctmScaleX / scale.width(),
