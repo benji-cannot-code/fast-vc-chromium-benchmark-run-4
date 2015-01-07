c3b7672e0cd9d90fda10ab8a28f602a8b022422e
anujk.sharma <anujk.sharma@samsung.com>
2015-01-07 11:11:13 -0800

    Renaming "AudioBuffers" to "AudioFrames". // TODO(xhwang): Rename this to AudioFrames
    BUG=446829
    
    Review URL: https://codereview.chromium.org/826783003
    
    Cr-Commit-Position: refs/heads/master@{#310328}

--- content/renderer/pepper/content_decryptor_delegate.h
+++ content/renderer/pepper/content_decryptor_delegate.h
@@ -197,7 +197,7 @@ class ContentDecryptorDelegate {
   bool DeserializeAudioFrames(PP_Resource audio_frames,
                               size_t data_size,
                               media::SampleFormat sample_format,
-                              media::Decryptor::AudioBuffers* frames);
+                              media::Decryptor::AudioFrames* frames);
 
   void SatisfyAllPendingCallbacksOnError();
 
