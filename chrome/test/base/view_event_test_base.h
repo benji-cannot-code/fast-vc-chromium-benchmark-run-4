eaebca6e6d4512280ca23c755d5543a09cd20a18
spang <spang@chromium.org>
2015-01-07 10:07:10 -0800

    chrome: test: Remove double quit from ViewEventTestBase & use RunLoop
    The "double quit" logic in ViewEventTestBase::Done() isn't robust, uses
    deprecated APIs, and hides problems with nested loop unwind:
    
    (1) It doesn't try to unwind more than one level of nesting. Tests with
    one inner loop might work, but two or more will definitely hang unless
    they terminate the inner loops themselves.
    
    (2) With one nested loop, it still may not work because
    MessageLoop::Quit() means to quit only once idle, and the loop may not
    become idle before the second quit runs. So both calls to Quit() can
    terminate the inner loop, leaving the outer loop running forever (as in
    bug 444428).
    
    We can't clean up loops left behind by tests correctly without using
    deprecated APIs on MessageLoop such as QuitNow(). Instead, leave loop
    termination to the creator of each RunLoop and avoid any other special
    behavior in ViewEventTestBase::Done(). If a test creates menus or other
    nested loops, it should terminate them before calling Done().
    
    BUG=446335
    TEST=interactive_ui_tests
    
    Review URL: https://codereview.chromium.org/830423004
    
    Cr-Commit-Position: refs/heads/master@{#310321}

--- chrome/test/base/view_event_test_base.h
+++ chrome/test/base/view_event_test_base.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/compiler_specific.h"
 #include "base/message_loop/message_loop.h"
+#include "base/run_loop.h"
 #include "base/threading/thread.h"
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -150,6 +151,8 @@ class ViewEventTestBase : public views::WidgetDelegate,
   scoped_ptr<ViewEventTestPlatformPart> platform_part_;
 
   views::TestViewsDelegate views_delegate_;
+
+  base::RunLoop run_loop_;
 
   DISALLOW_COPY_AND_ASSIGN(ViewEventTestBase);
 };
