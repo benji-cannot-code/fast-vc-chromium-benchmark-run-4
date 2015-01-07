850a8f1e30ee6a021fe5cf89b04e52aaecde4ee8
posciak <posciak@chromium.org>
2015-01-07 02:29:08 -0800

    V4L2VDA/vdaunittest: Allow VP9 format if supported.
    Allow VP9 decode if HW/driver supports it. Instead of hardcoding supported
    formats, let the format setting logic query the device whether it supports
    the requested codec type.
    
    BUG=430638
    TEST=video playback,vdaunittest
    
    Review URL: https://codereview.chromium.org/839703002
    
    Cr-Commit-Position: refs/heads/master@{#310262}

--- content/common/gpu/media/v4l2_video_device.cc
+++ content/common/gpu/media/v4l2_video_device.cc
@@ -10,6 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/gpu/media/generic_v4l2_video_device.h"
 #include "content/common/gpu/media/tegra_v4l2_video_device.h"
 
+// TODO(posciak): remove this once V4L2 headers are updated.
+#define V4L2_PIX_FMT_VP9 v4l2_fourcc('V', 'P', '9', '0')
+
 namespace content {
 
 V4L2Device::~V4L2Device() {}
@@ -76,6 +79,9 @@ uint32 V4L2Device::VideoCodecProfileToV4L2PixFmt(
   } else if (profile >= media::VP8PROFILE_MIN &&
              profile <= media::VP8PROFILE_MAX) {
     return V4L2_PIX_FMT_VP8;
+  } else if (profile >= media::VP9PROFILE_MIN &&
+             profile <= media::VP9PROFILE_MAX) {
+    return V4L2_PIX_FMT_VP9;
   } else {
     LOG(FATAL) << "Add more cases as needed";
     return 0;
