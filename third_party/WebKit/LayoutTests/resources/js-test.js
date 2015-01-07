44f56b4ad4a6f1a6d038afbc79d3f75e0ed7a0f0
kouhei@chromium.org <kouhei@chromium.org>
2015-01-07 05:33:27 +0000

    Use setTimeOut(, 0) for asyncGC
    per suggestion from haraken@.
    
    setTimeOut(, 1) doesn't make any difference from setTimeOut(, 0).
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/836263002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187950 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/resources/js-test.js
+++ third_party/WebKit/LayoutTests/resources/js-test.js
@@ -702,7 +702,7 @@ function shouldHaveHadError(message)
 function asyncGC(callback) {
     GCController.collectAll();
     // FIXME: we need a better way of waiting for chromium events to happen
-    setTimeout(callback, 1);
+    setTimeout(callback, 0);
 }
 
 function gc() {
@@ -727,7 +727,7 @@ function asyncMinorGC(callback) {
     else
         testFailed("Minor GC is available only when you enable the --expose-gc option in V8.");
     // FIXME: we need a better way of waiting for chromium events to happen
-    setTimeout(callback, 1);
+    setTimeout(callback, 0);
 }
 
 function setPrintTestResultsLazily() {
