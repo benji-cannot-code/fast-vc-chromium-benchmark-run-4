89cfd11cec59720191d6c048e6d8334b98469eca
ankitkumar <ankitkumar@chromium.org>
2015-01-06 22:50:55 -0800

    Added code to hide tab manager on navigation.
    Currently only when 'Go' button is pressed using keyboard tab manager
    was hiding. But if tab manager is shown then tab Manager was not
    hiding in following scenarios:
    1. Selecting prev/next buttons through options menu.
    2. Typing some text in omnibox and clicking on any suggestions.
    
    Added code to hide tab manager whenever load progress is changed.
    
    BUG=429977
    
    Review URL: https://codereview.chromium.org/739463002
    
    Cr-Commit-Position: refs/heads/master@{#310243}

--- chrome/android/shell/java/src/org/chromium/chrome/shell/ChromeShellTab.java
+++ chrome/android/shell/java/src/org/chromium/chrome/shell/ChromeShellTab.java
@@ -42,9 +42,9 @@ public class ChromeShellTab extends Tab {
             ContentViewClient contentViewClient, TabManager tabManager) {
         super(false, context, window);
         initialize(0, null, false);
+        mTabManager = tabManager;
         setContentViewClient(contentViewClient);
         loadUrlWithSanitization(url);
-        mTabManager = tabManager;
     }
 
     /**
@@ -113,6 +113,7 @@ public class ChromeShellTab extends Tab {
             extends TabChromeWebContentsDelegateAndroid {
         @Override
         public void onLoadStarted() {
+            mTabManager.hideTabSwitcher();
             mIsLoading = true;
             super.onLoadStarted();
         }
