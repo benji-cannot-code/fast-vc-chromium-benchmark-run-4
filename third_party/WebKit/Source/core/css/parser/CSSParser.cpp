d18694d8ef08830319702a1c4056b3d716e37ca9
timloh@chromium.org <timloh@chromium.org>
2015-01-07 05:12:06 +0000

    CSS Parser: Implement parseKeyframeKeyList
    This patch adds support for parsing keyframe keylists in the css-syntax
    based parser.
    
    The equivalent logic for the Bison parser is in the grammar productions
    (internal_keyframe_)key_list and in StyleKeyframe::createKeyList.
    
    BUG=330389
    
    Review URL: https://codereview.chromium.org/808483003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187948 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/parser/CSSParser.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSParser.cpp
@@ -104,6 +104,8 @@ PassRefPtrWillBeRawPtr<ImmutableStylePropertySet> CSSParser::parseInlineStyleDec
 
 PassOwnPtr<Vector<double> > CSSParser::parseKeyframeKeyList(const String& keyList)
 {
+    if (RuntimeEnabledFeatures::newCSSParserEnabled())
+        return CSSParserImpl::parseKeyframeKeyList(keyList);
     return BisonCSSParser(strictCSSParserContext()).parseKeyframeKeyList(keyList);
 }
 
--- third_party/WebKit/Source/core/css/parser/CSSParserImpl.h
+++ third_party/WebKit/Source/core/css/parser/CSSParserImpl.h
@@ -35,6 +35,8 @@ public:
     static PassRefPtrWillBeRawPtr<StyleRuleBase> parseRule(const String&, const CSSParserContext&);
     static void parseStyleSheet(const String&, const CSSParserContext&, StyleSheetContents*);
 
+    static PassOwnPtr<Vector<double>> parseKeyframeKeyList(const String&);
+
 private:
     enum RuleListType {
         TopLevelRuleList,
@@ -69,6 +71,8 @@ private:
     void consumeDeclarationList(CSSParserTokenRange, CSSRuleSourceData::Type);
     void consumeDeclaration(CSSParserTokenRange, CSSRuleSourceData::Type);
     void consumeDeclarationValue(CSSParserTokenRange, CSSPropertyID, bool important, CSSRuleSourceData::Type);
+
+    static PassOwnPtr<Vector<double>> consumeKeyframeKeyList(CSSParserTokenRange);
 
     // FIXME: Can we build StylePropertySets directly?
     // FIXME: Investigate using a smaller inline buffer
