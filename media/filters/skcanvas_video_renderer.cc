db063de09dd2163f49b63f95bb606ae2738efa87
posciak <posciak@chromium.org>
2015-01-06 22:37:59 -0800

    V4L2VDA: Generalize EGLImage import and query driver for output formats.
    Generalize CreateEGLImage() from being NV12M-specific to support any
    format. Add a method to query whether current platform supports importing
    a given V4L2/DRM format into EGLImages and use it in V4L2VDA.
    Dynamically query the driver for supported V4L2 output formats and choose
    one we can use for EGLImage import.
    
    TEST=Video playback for affected platforms
    BUG=chromium:430638,chrome-os-partner:33728,chromium:350197
    
    Review URL: https://codereview.chromium.org/839523002
    
    Cr-Commit-Position: refs/heads/master@{#310241}

--- media/filters/skcanvas_video_renderer.cc
+++ media/filters/skcanvas_video_renderer.cc
@@ -53,6 +53,7 @@ bool IsYUV(media::VideoFrame::Format format) {
 #if defined(VIDEO_HOLE)
     case VideoFrame::HOLE:
 #endif  // defined(VIDEO_HOLE)
+    case VideoFrame::ARGB:
       return false;
   }
   NOTREACHED() << "Invalid videoframe format provided: " << format;
@@ -74,6 +75,7 @@ bool IsJPEGColorSpace(media::VideoFrame::Format format) {
 #if defined(VIDEO_HOLE)
     case VideoFrame::HOLE:
 #endif  // defined(VIDEO_HOLE)
+    case VideoFrame::ARGB:
       return false;
   }
   NOTREACHED() << "Invalid videoframe format provided: " << format;
@@ -199,7 +201,6 @@ void ConvertVideoFrameToRGBPixels(
       break;
 
     case media::VideoFrame::NATIVE_TEXTURE: {
-      DCHECK_EQ(video_frame->format(), media::VideoFrame::NATIVE_TEXTURE);
       SkBitmap tmp;
       tmp.installPixels(
           SkImageInfo::MakeN32Premul(video_frame->visible_rect().width(),
@@ -209,6 +210,8 @@ void ConvertVideoFrameToRGBPixels(
       video_frame->ReadPixelsFromNativeTexture(tmp);
       break;
     }
+
+    case media::VideoFrame::ARGB:
     default:
       NOTREACHED();
       break;
