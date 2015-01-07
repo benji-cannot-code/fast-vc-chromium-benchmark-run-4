591f160eebade459fc08324af5e2239998510284
ananta@chromium.org <ananta@chromium.org>
2015-01-07 04:25:05 +0000

    Blink side of the change to pass the system font metrics from the browser to the renderer on Windows
    This change is needed to support Win32K lockdown mode in the renderer
    process which prevents it from making user32/gdi32 calls which call into
    win32k.sys on Windows 8+. Currently the renderer process crashes in the
    Win32K lockdown mode while attempting to retrieve system font metrics.
    
    Changes in this patch are as below:-
    
    1. Updated the WebFontRendering interface with static methods to set the
       menu font, small caption font and status font metrics.
    
    2. These methods are implemented by the FontCache class on Windows.
    
    3. Changed the RenderThemeChromiumFontProvider class to retrieve the
       font metrics from the FontCache class before querying the system
       for the same.
    
    BUG=442158
    
    Review URL: https://codereview.chromium.org/790733005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187943 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderThemeChromiumFontProviderWin.cpp
+++ third_party/WebKit/Source/core/rendering/RenderThemeChromiumFontProviderWin.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/rendering/RenderThemeChromiumFontProvider.h"
 
 #include "core/CSSValueKeywords.h"
+#include "platform/fonts/FontCache.h"
 #include "platform/fonts/FontDescription.h"
 #include "platform/win/HWndDC.h"
 #include "platform/win/SystemInfo.h"
@@ -108,24 +109,39 @@ void RenderThemeChromiumFontProvider::systemFont(CSSValueID systemFontID, FontSt
 
     switch (systemFontID) {
     case CSSValueSmallCaption: {
-        NONCLIENTMETRICS metrics;
-        getNonClientMetrics(&metrics);
-        fontSize = systemFontSize(metrics.lfSmCaptionFont);
-        fontFamily = AtomicString(metrics.lfSmCaptionFont.lfFaceName, wcslen(metrics.lfSmCaptionFont.lfFaceName));
+        fontSize = FontCache::smallCaptionFontHeight();
+        if (fontSize) {
+            fontFamily = FontCache::smallCaptionFontFamily();
+        } else {
+            NONCLIENTMETRICS metrics;
+            getNonClientMetrics(&metrics);
+            fontSize = systemFontSize(metrics.lfSmCaptionFont);
+            fontFamily = AtomicString(metrics.lfSmCaptionFont.lfFaceName, wcslen(metrics.lfSmCaptionFont.lfFaceName));
+        }
         break;
     }
     case CSSValueMenu: {
-        NONCLIENTMETRICS metrics;
-        getNonClientMetrics(&metrics);
-        fontSize = systemFontSize(metrics.lfMenuFont);
-        fontFamily = AtomicString(metrics.lfMenuFont.lfFaceName, wcslen(metrics.lfMenuFont.lfFaceName));
+        fontSize = FontCache::menuFontHeight();
+        if (fontSize) {
+            fontFamily = FontCache::menuFontFamily();
+        } else {
+            NONCLIENTMETRICS metrics;
+            getNonClientMetrics(&metrics);
+            fontSize = systemFontSize(metrics.lfMenuFont);
+            fontFamily = AtomicString(metrics.lfMenuFont.lfFaceName, wcslen(metrics.lfMenuFont.lfFaceName));
+        }
         break;
     }
     case CSSValueStatusBar: {
-        NONCLIENTMETRICS metrics;
-        getNonClientMetrics(&metrics);
-        fontSize = systemFontSize(metrics.lfStatusFont);
-        fontFamily = metrics.lfStatusFont.lfFaceName;
+        fontSize = FontCache::statusFontHeight();
+        if (fontSize) {
+            fontFamily = FontCache::statusFontFamily();
+        } else {
+            NONCLIENTMETRICS metrics;
+            getNonClientMetrics(&metrics);
+            fontSize = systemFontSize(metrics.lfStatusFont);
+            fontFamily = metrics.lfStatusFont.lfFaceName;
+        }
         break;
     }
     case CSSValueWebkitMiniControl:
