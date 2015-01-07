ce5404b1a61ead68ee2226e65d5e965e3b30c89d
nednguyen <nednguyen@google.com>
2015-01-07 08:23:52 -0800

    [Telemetry] Move DisplayTracing (for surface_flinger_stat)  to tracing_controller.
    BUG=
    
    Review URL: https://codereview.chromium.org/837043004
    
    Cr-Commit-Position: refs/heads/master@{#310295}

--- tools/telemetry/telemetry/core/platform/tracing_options.py
+++ tools/telemetry/telemetry/core/platform/tracing_options.py
@@ -18,6 +18,8 @@ class TracingOptions(object):
   Options:
          enable_chrome_trace: a boolean that specifies whether to enable
                             chrome tracing.
+         enable_platform_display_trace: a boolean that specifies whether to
+                            platform display tracing.
          record_mode: can be any mode in RECORD_MODES. This corresponds to
                     record modes in chrome (see
                     TraceRecordMode in base/debug/trace_event_impl.h for more
@@ -25,6 +27,7 @@ class TracingOptions(object):
   """
   def __init__(self):
     self.enable_chrome_trace = False
+    self.enable_platform_display_trace = False
     self._record_mode = RECORD_AS_MUCH_AS_POSSIBLE
 
   @property
