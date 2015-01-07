6a633976c488fecfee1ced60b5fbc67effde893b
skyostil <skyostil@chromium.org>
2015-01-07 08:41:42 -0800

    telemetry: Compress output from --profiler=trace
    Compress the json traces generated by --profiler=trace with zip to
    make them easier to attach to bugs and share. Note that about:tracing
    will transparently decompress such files so there is no need to unzip
    them manually.
    
    BUG=438518
    
    Review URL: https://codereview.chromium.org/833383002
    
    Cr-Commit-Position: refs/heads/master@{#310301}

--- tools/telemetry/telemetry/core/platform/profiler/trace_profiler.py
+++ tools/telemetry/telemetry/core/platform/profiler/trace_profiler.py
@@ -3,7 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import codecs
+import os
+import StringIO
+import zipfile
 
 from telemetry.core.platform import profiler
 from telemetry.core.platform import tracing_options
@@ -41,10 +43,13 @@ class TraceProfiler(profiler.Profiler):
     self._browser_backend.StopTracing(trace_result_builder)
     trace_result = trace_result_builder.AsData()
 
-    trace_file = '%s.json' % self._output_path
+    trace_file = '%s.zip' % self._output_path
 
-    with codecs.open(trace_file, 'w', encoding='utf-8') as f:
-      trace_result.Serialize(f)
+    with zipfile.ZipFile(trace_file, 'w', zipfile.ZIP_DEFLATED) as z:
+      trace_data = StringIO.StringIO()
+      trace_result.Serialize(trace_data)
+      trace_name = '%s.json' % os.path.basename(self._output_path)
+      z.writestr(trace_name, trace_data.getvalue())
 
     print 'Trace saved as %s' % trace_file
     print 'To view, open in chrome://tracing'
