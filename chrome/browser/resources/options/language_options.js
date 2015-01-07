a6fd3a8824201a6cd1bdfe0cbb4d181cc0d29b3b
michaelpg <michaelpg@chromium.org>
2015-01-07 13:38:08 -0800

    Fix input method UMA metrics
    BUG=433991
    R=stevenjb@chromium.org
    
    Review URL: https://codereview.chromium.org/842683002
    
    Cr-Commit-Position: refs/heads/master@{#310374}

--- chrome/browser/resources/options/language_options.js
+++ chrome/browser/resources/options/language_options.js
@@ -851,7 +851,8 @@ cr.define('options', function() {
       }
 
       chrome.send('coreOptionsUserMetricsAction',
-                  ['Options_Languages_InputMethodCheckbox']);
+                  ['Options_Languages_InputMethodCheckbox' +
+                   (checkbox.checked ? '_Enable' : '_Disable')]);
     },
 
     /**
