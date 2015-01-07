1f6c850226ac5f063c830e89fd2eb96b2e2c3ba7
rnephew <rnephew@chromium.org>
2015-01-07 11:34:53 -0800

    Clean up logging and add device randomization.
    If no device_name is picked and os_version is, will pick random device with
    given os; and vice versa. If neither is picked, both are randomly picked.
    
    Clean up logging. Disable appurify/requests logging if verbosity is
    less than 2.
    
    BUG=428729
    
    Review URL: https://codereview.chromium.org/814653003
    
    Cr-Commit-Position: refs/heads/master@{#310337}

--- build/android/pylib/remote/device/remote_device_helper.py
+++ build/android/pylib/remote/device/remote_device_helper.py
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 """Common functions and Exceptions for remote_device_*"""
 
-
 class RemoteDeviceError(Exception):
   """Exception to throw when problems occur with remote device service."""
   pass
