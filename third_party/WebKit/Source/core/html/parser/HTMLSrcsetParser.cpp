0a056ab8b00d2a3c3743639e23cf461209cca3d7
yoav@yoav.ws <yoav@yoav.ws>
2015-01-07 13:50:16 +0000

    Got rid of useless signed int
    An error in a recently added function signature resulted in it returning
    a signed int rather than an unsigned one. This CL fixes that.
    There's no functionality change, so no tests.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/836093006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187967 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/parser/HTMLSrcsetParser.cpp
+++ third_party/WebKit/Source/core/html/parser/HTMLSrcsetParser.cpp
@@ -324,7 +324,7 @@ static void parseImageCandidatesFromSrcsetAttribute(const String& attribute, Vec
         parseImageCandidatesFromSrcsetAttribute<UChar>(attribute, attribute.characters16(), attribute.length(), imageCandidates, document);
 }
 
-static int selectionLogic(Vector<ImageCandidate>& imageCandidates, float deviceScaleFactor, bool ignoreSrc)
+static unsigned selectionLogic(Vector<ImageCandidate>& imageCandidates, float deviceScaleFactor, bool ignoreSrc)
 {
     unsigned i = 0;
 
