4f3f765fcc47f904cd811e8a48e1ada8b91b9ac9
paulmiller <paulmiller@chromium.org>
2015-01-07 15:49:15 -0800

    Workaround crash on pasting text in a WebView
    BUG=446717
    
    Review URL: https://codereview.chromium.org/812673008
    
    Cr-Commit-Position: refs/heads/master@{#310411}

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -1935,9 +1935,20 @@ public class ContentViewCore
         return mActionHandler;
     }
 
+    // crbug.com/446717
+    private void actionModeInvalidateWAR() {
+        assert mActionMode != null;
+
+        try {
+            mActionMode.invalidate();
+        } catch (NullPointerException e) {
+            Log.w(TAG, "Ignoring NPE from ActionMode.invalidate() as workaround for L", e);
+        }
+    }
+
     private void showSelectActionBar() {
         if (mActionMode != null) {
-            mActionMode.invalidate();
+            actionModeInvalidateWAR();
             return;
         }
 
@@ -2283,7 +2294,7 @@ public class ContentViewCore
                         compositionEnd, isNonImeChange);
             }
 
-            if (mActionMode != null) mActionMode.invalidate();
+            if (mActionMode != null) actionModeInvalidateWAR();
         } finally {
             TraceEvent.end("ContentViewCore.updateImeAdapter");
         }
