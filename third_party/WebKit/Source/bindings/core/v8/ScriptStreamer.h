0d9dfd3235c4f64e05788756ebf4f1471e6075ab
marja@chromium.org <marja@chromium.org>
2015-01-07 16:47:23 +0000

    Cached data for scripts: make the cache key depend on encoding.
    A script can be (successfully) interpreted with different encodings, depending
    on the page it appears in. The cache doesn't know anything about encodings, but
    the cached data is specific to one encoding. If we later load the script from
    the cache and interpret it with a different encoding, the cached data is not
    valid for that encoding.
    
    BUG=441130
    R=jochen@chromium.org
    
    Review URL: https://codereview.chromium.org/806223005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187980 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/core/v8/ScriptStreamer.h
+++ third_party/WebKit/Source/bindings/core/v8/ScriptStreamer.h
@@ -66,7 +66,10 @@ public:
     void suppressStreaming();
     bool streamingSuppressed() const { return m_streamingSuppressed; }
 
-    unsigned cachedDataType() const;
+    v8::ScriptCompiler::CompileOptions compileOptions() const
+    {
+        return m_compileOptions;
+    }
 
     void addClient(ScriptResourceClient* client)
     {
