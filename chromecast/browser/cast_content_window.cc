64d936b6efbaa3acaf33ae784eb4ad0315c3e78b
gunsch <gunsch@chromium.org>
2015-01-07 09:53:41 -0800

    Chromecast: makes CastContentWindow a WebContentsObserver.
    This allows for common WebContentsObserver functionality to live within
    CastContentWindow instead of in WebApplication.
    
    R=lcwu@chromium.org,derekjchow@chromium.org
    BUG=None
    
    Review URL: https://codereview.chromium.org/826933006
    
    Cr-Commit-Position: refs/heads/master@{#310315}

--- chromecast/browser/cast_content_window.cc
+++ chromecast/browser/cast_content_window.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromecast/browser/cast_content_window.h"
 
 #include "base/threading/thread_restrictions.h"
+#include "chromecast/base/metrics/cast_metrics_helper.h"
 #include "content/public/browser/web_contents.h"
 #include "ipc/ipc_message.h"
 
@@ -104,8 +105,12 @@ scoped_ptr<content::WebContents> CastContentWindow::CreateWebContents(
   create_params.initial_size = initial_size;
   content::WebContents* web_contents = content::WebContents::Create(
       create_params);
-
+  content::WebContentsObserver::Observe(web_contents);
   return make_scoped_ptr(web_contents);
+}
+
+void CastContentWindow::DidFirstVisuallyNonEmptyPaint() {
+  metrics::CastMetricsHelper::GetInstance()->LogTimeToFirstPaint();
 }
 
 }  // namespace chromecast
--- chromecast/browser/cast_content_window.h
+++ chromecast/browser/cast_content_window.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/scoped_ptr.h"
+#include "content/public/browser/web_contents_observer.h"
 
 namespace aura {
 class WindowTreeHost;
@@ -24,12 +25,12 @@ class Size;
 
 namespace chromecast {
 
-class CastContentWindow {
+class CastContentWindow : public content::WebContentsObserver {
  public:
   CastContentWindow();
 
   // Removes the window from the screen.
-  ~CastContentWindow();
+  ~CastContentWindow() override;
 
   // Create a window with the given size for |web_contents|.
   void CreateWindowTree(const gfx::Size& initial_size,
@@ -38,6 +39,9 @@ class CastContentWindow {
   scoped_ptr<content::WebContents> CreateWebContents(
       const gfx::Size& initial_size,
       content::BrowserContext* browser_context);
+
+  // content::WebContentsObserver implementation:
+  void DidFirstVisuallyNonEmptyPaint() override;
 
  private:
 #if defined(USE_AURA)
