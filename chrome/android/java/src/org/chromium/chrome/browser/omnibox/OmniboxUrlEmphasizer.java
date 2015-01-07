9e28a7f71b7bf34ac3c989f313270c632a5a6693
wajahat.s <wajahat.s@samsung.com>
2015-01-06 22:48:00 -0800

    Resolve lint warnings related to DefaultLocale and SimpleDateFormat
    The following lint warnings needs to be fixed as they are legitimate:
    
    (1) org/chromium/chrome/browser/omnibox/OmniboxUrlEmphasizer.java
        Implicitly using the default locale is a common source of bugs:
        Use `toLowerCase(Locale)` instead: DefaultLocale [warning]
    
    (2) org/chromium/chrome/browser/preferences/autofill/AutofillCreditCardEditor.java
        To get local formatting use `getDateInstance()`, `getDateTimeInstance()`,
        or `getTimeInstance()`, or use `new SimpleDateFormat(String template, Locale locale)`
        with for example `Locale.US` for ASCII dates.: SimpleDateFormat [warning]
    
    Fixing these warning by specifying the locale in respective classes.
    If a locale cannot be determined, Locale.getDefault() in case of (2).
    
    BUG=None.
    
    Review URL: https://codereview.chromium.org/800903004
    
    Cr-Commit-Position: refs/heads/master@{#310242}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/OmniboxUrlEmphasizer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/OmniboxUrlEmphasizer.java
@@ -15,6 +15,8 @@ import org.chromium.chrome.R;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.ui.toolbar.ToolbarModelSecurityLevel;
 
+import java.util.Locale;
+
 /**
  * A helper class that emphasizes the various components of a URL. Useful in the
  * Omnibox and Origin Info dialog where different parts of the URL should appear
@@ -288,7 +290,8 @@ public class OmniboxUrlEmphasizer {
 
         int startSchemeIndex = emphasizeResponse.schemeStart;
         int endSchemeIndex = emphasizeResponse.schemeStart + emphasizeResponse.schemeLength;
-        String scheme = url.subSequence(startSchemeIndex, endSchemeIndex).toString().toLowerCase();
+        String scheme = url.subSequence(startSchemeIndex, endSchemeIndex).toString().toLowerCase(
+                Locale.US);
 
         if (scheme.equals("http") || scheme.equals("https")) {
             return emphasizeResponse.hostStart + emphasizeResponse.hostLength;
