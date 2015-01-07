d7d2ee6bf65d0e23931c673cf2c2327892f4e44c
jrummell@chromium.org <jrummell@chromium.org>
2015-01-07 04:32:43 +0000

    Change EME SessionType to include both types of persistent sessions.
    SessionType can now be "temporary", "persistent-license", or
    "persistent-release-message". "persistent-release-message" is not yet
    supported on the Chromium side, so using it will fail.
    
    BUG=224786
    TEST=existing EME tests pass
    
    Review URL: https://codereview.chromium.org/831403003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187944 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/encryptedmedia/MediaKeySession.cpp
+++ third_party/WebKit/Source/modules/encryptedmedia/MediaKeySession.cpp
@@ -58,7 +58,8 @@ namespace {
 
 // The list of possible values for |sessionType|.
 const char* kTemporary = "temporary";
-const char* kPersistent = "persistent";
+const char* kPersistentLicense = "persistent-license";
+const char* kPersistentReleaseMessage = "persistent-release-message";
 
 // Minimum and maximum length for session ids.
 enum {
@@ -294,7 +295,7 @@ private:
 
 MediaKeySession* MediaKeySession::create(ScriptState* scriptState, MediaKeys* mediaKeys, const String& sessionType)
 {
-    ASSERT(sessionType == kTemporary || sessionType == kPersistent);
+    ASSERT(isValidSessionType(sessionType));
     RefPtrWillBeRawPtr<MediaKeySession> session = new MediaKeySession(scriptState, mediaKeys, sessionType);
     session->suspendIfNeeded();
     return session.get();
@@ -302,7 +303,7 @@ MediaKeySession* MediaKeySession::create(ScriptState* scriptState, MediaKeys* me
 
 bool MediaKeySession::isValidSessionType(const String& sessionType)
 {
-    return (sessionType == kTemporary || sessionType == kPersistent);
+    return (sessionType == kTemporary || sessionType == kPersistentLicense || sessionType == kPersistentReleaseMessage);
 }
 
 MediaKeySession::MediaKeySession(ScriptState* scriptState, MediaKeys* mediaKeys, const String& sessionType)
@@ -458,11 +459,12 @@ ScriptPromise MediaKeySession::load(ScriptState* scriptState, const String& sess
             scriptState, DOMException::create(InvalidAccessError, "The sessionId parameter is empty."));
     }
 
-    // 4. If this object's session type is not "persistent", return a promise
-    //    rejected with a new DOMException whose name is "InvalidAccessError".
-    if (m_sessionType != kPersistent) {
+    // 4. If this object's session type is not "persistent-license" or
+    //    "persistent-release-message", return a promise rejected with a
+    //    new DOMException whose name is InvalidAccessError.
+    if (m_sessionType != kPersistentLicense && m_sessionType != kPersistentReleaseMessage) {
         return ScriptPromise::rejectWithDOMException(
-            scriptState, DOMException::create(InvalidAccessError, "The session type is not 'persistent'."));
+            scriptState, DOMException::create(InvalidAccessError, "The session type is not persistent."));
     }
 
     // 5. Let media keys be the MediaKeys object that created this object.
@@ -575,11 +577,12 @@ ScriptPromise MediaKeySession::remove(ScriptState* scriptState)
             scriptState, DOMException::create(InvalidStateError, "The session is not callable."));
     }
 
-    // 2. If this object's session type is not "persistent", return a promise
-    //    rejected with a new DOMException whose name is "InvalidAccessError".
-    if (m_sessionType != kPersistent) {
+    // 2. If this object's session type is not "persistent-license" or
+    //    "persistent-release-message", return a promise rejected with a
+    //    new DOMException whose name is InvalidAccessError.
+    if (m_sessionType != kPersistentLicense && m_sessionType != kPersistentReleaseMessage) {
         return ScriptPromise::rejectWithDOMException(
-            scriptState, DOMException::create(InvalidAccessError, "The session type is not 'persistent'."));
+            scriptState, DOMException::create(InvalidAccessError, "The session type is not persistent."));
     }
 
     // 3. If the Session Close algorithm has been run on this object, return a
