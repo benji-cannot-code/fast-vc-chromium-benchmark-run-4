c31eec166acdeb213712d96919c37922de434164
alexst <alexst@chromium.org>
2015-01-08 14:29:25 -0800

    GPU main could be very janky depending on what is being rendered, whenever possible, we need to handle cursor moves on GPU IO thread.
    BUG=
    
    Review URL: https://codereview.chromium.org/795933008
    
    Cr-Commit-Position: refs/heads/master@{#310614}

--- ui/ozone/platform/dri/dri_gpu_platform_support.h
+++ ui/ozone/platform/dri/dri_gpu_platform_support.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/scoped_ptr_hash_map.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/scoped_vector.h"
+#include "ipc/message_filter.h"
 #include "ui/gfx/native_widget_types.h"
 #include "ui/ozone/public/gpu_platform_support.h"
 
@@ -83,6 +84,7 @@ class DriGpuPlatformSupport : public GpuPlatformSupport {
 
   scoped_ptr<NativeDisplayDelegateDri> ndd_;
   ScopedVector<GpuPlatformSupport> handlers_;
+  scoped_refptr<IPC::MessageFilter> filter_;
 };
 
 }  // namespace ui
