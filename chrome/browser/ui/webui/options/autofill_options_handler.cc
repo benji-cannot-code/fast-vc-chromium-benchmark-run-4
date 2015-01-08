a332a20049c509d078fe6ec29ba271bc438475f3
estade <estade@chromium.org>
2015-01-07 21:22:05 -0800

    Create initial settings UI for wallet integration.
    Due to lack of mocks, this is the simplest and ugliest UI imaginable.
    
    BUG=446698
    
    Review URL: https://codereview.chromium.org/839683002
    
    Cr-Commit-Position: refs/heads/master@{#310455}

--- chrome/browser/ui/webui/options/autofill_options_handler.cc
+++ chrome/browser/ui/webui/options/autofill_options_handler.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/bind_helpers.h"
+#include "base/command_line.h"
 #include "base/guid.h"
 #include "base/logging.h"
 #include "base/strings/string16.h"
@@ -21,12 +22,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/autofill/country_combobox_model.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/generated_resources.h"
+#include "components/autofill/content/browser/wallet/wallet_service_url.h"
 #include "components/autofill/core/browser/autofill_country.h"
 #include "components/autofill/core/browser/autofill_profile.h"
 #include "components/autofill/core/browser/credit_card.h"
 #include "components/autofill/core/browser/personal_data_manager.h"
 #include "components/autofill/core/browser/phone_number_i18n.h"
 #include "components/autofill/core/common/autofill_constants.h"
+#include "components/autofill/core/common/autofill_switches.h"
 #include "content/public/browser/web_ui.h"
 #include "third_party/libaddressinput/messages.h"
 #include "third_party/libaddressinput/src/cpp/include/libaddressinput/address_ui.h"
@@ -332,6 +335,17 @@ void AutofillOptionsHandler::GetLocalizedValues(
   localized_strings->SetString("helpUrl", autofill::kHelpURL);
   SetAddressOverlayStrings(localized_strings);
   SetCreditCardOverlayStrings(localized_strings);
+
+  localized_strings->SetBoolean(
+      "enableAutofillWalletIntegration",
+      base::CommandLine::ForCurrentProcess()->HasSwitch(
+          autofill::switches::kEnableWalletCardImport));
+  localized_strings->SetString(
+      "manageWalletAddressesUrl",
+      autofill::wallet::GetManageAddressesUrl(0).spec());
+  localized_strings->SetString(
+      "manageWalletPaymentMethodsUrl",
+      autofill::wallet::GetManageInstrumentsUrl(0).spec());
 }
 
 void AutofillOptionsHandler::InitializeHandler() {
@@ -381,6 +395,10 @@ void AutofillOptionsHandler::RegisterMessages() {
   web_ui()->RegisterMessageCallback(
       "validatePhoneNumbers",
       base::Bind(&AutofillOptionsHandler::ValidatePhoneNumbers,
+                 base::Unretained(this)));
+  web_ui()->RegisterMessageCallback(
+      "remaskServerCards",
+      base::Bind(&AutofillOptionsHandler::RemaskServerCards,
                  base::Unretained(this)));
 }
 
@@ -674,6 +692,18 @@ void AutofillOptionsHandler::ValidatePhoneNumbers(const base::ListValue* args) {
 
   web_ui()->CallJavascriptFunction(
     "AutofillEditAddressOverlay.setValidatedPhoneNumbers", *list_value);
+}
+
+void AutofillOptionsHandler::RemaskServerCards(const base::ListValue* args) {
+  const std::vector<CreditCard*>& cards = personal_data_->GetCreditCards();
+  for (const auto card : cards) {
+    CreditCard card_copy = *card;
+    if (card_copy.record_type() == CreditCard::FULL_SERVER_CARD) {
+      card_copy.set_record_type(CreditCard::MASKED_SERVER_CARD);
+      card_copy.SetNumber(card->LastFourDigits());
+      personal_data_->UpdateServerCreditCard(card_copy);
+    }
+  }
 }
 
 bool AutofillOptionsHandler::IsPersonalDataLoaded() const {
