a99cb0bedcb06ca623d3e2788bf84f7cabdc4f4a
vadimt <vadimt@chromium.org>
2015-01-07 16:15:53 -0800

    Instrumenting URLRequestFileJob::ReadRawData internals for jank. Prior instrumentations showed: URLRequestFileJob::ReadRawData = 16 jph
    Instrumenting further.
    
    BUG=423948
    
    Review URL: https://codereview.chromium.org/792953004
    
    Cr-Commit-Position: refs/heads/master@{#310416}

--- net/base/file_stream.cc
+++ net/base/file_stream.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/base/file_stream.h"
 
+#include "base/profiler/scoped_tracker.h"
 #include "net/base/file_stream_context.h"
 #include "net/base/net_errors.h"
 
@@ -57,6 +58,10 @@ int FileStream::Seek(base::File::Whence whence,
 int FileStream::Read(IOBuffer* buf,
                      int buf_len,
                      const CompletionCallback& callback) {
+  // TODO(vadimt): Remove ScopedTracker below once crbug.com/423948 is fixed.
+  tracked_objects::ScopedTracker tracking_profile(
+      FROM_HERE_WITH_EXPLICIT_FUNCTION("423948 FileStream::Read"));
+
   if (!IsOpen())
     return ERR_UNEXPECTED;
 
