25ec0c37408b08a2df263163861920f3f8379da9
gunsch <gunsch@chromium.org>
2015-01-08 12:07:50 -0800

    Chromecast: wires up CMA in CastContentBrowserClient.
    R=lcwu@chromium.org,erickung@chromium.org
    BUG=408189
    
    Review URL: https://codereview.chromium.org/841433002
    
    Cr-Commit-Position: refs/heads/master@{#310576}

--- chromecast/browser/cast_content_browser_client.cc
+++ chromecast/browser/cast_content_browser_client.cc
@@ -15,8 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromecast/browser/cast_network_delegate.h"
 #include "chromecast/browser/devtools/cast_dev_tools_delegate.h"
 #include "chromecast/browser/geolocation/cast_access_token_store.h"
+#include "chromecast/browser/media/cma_message_filter_host.h"
 #include "chromecast/browser/url_request_context_factory.h"
 #include "chromecast/common/cast_paths.h"
+#include "chromecast/common/chromecast_switches.h"
 #include "chromecast/common/global_descriptors.h"
 #include "components/crash/app/breakpad_linux.h"
 #include "components/dns_prefetch/browser/net_message_filter.h"
@@ -63,6 +65,11 @@ void CastContentBrowserClient::RenderProcessWillLaunch(
       new dns_prefetch::NetMessageFilter(
           url_request_context_factory_->host_resolver()));
   host->AddFilter(net_message_filter.get());
+#if !defined(OS_ANDROID)
+  scoped_refptr<media::CmaMessageFilterHost> cma_message_filter(
+      new media::CmaMessageFilterHost(host->GetID()));
+  host->AddFilter(cma_message_filter.get());
+#endif  // !defined(OS_ANDROID)
 }
 
 net::URLRequestContextGetter* CastContentBrowserClient::CreateRequestContext(
@@ -104,6 +111,8 @@ void CastContentBrowserClient::AppendExtraCommandLineSwitches(
 
   std::string process_type =
       command_line->GetSwitchValueNative(switches::kProcessType);
+  base::CommandLine* browser_command_line =
+      base::CommandLine::ForCurrentProcess();
 
   // Renderer process command-line
   if (process_type == switches::kRendererProcess) {
@@ -112,6 +121,9 @@ void CastContentBrowserClient::AppendExtraCommandLineSwitches(
 #if defined(OS_ANDROID)
     command_line->AppendSwitch(switches::kForceUseOverlayEmbeddedVideo);
 #endif  // defined(OS_ANDROID)
+
+    if (browser_command_line->HasSwitch(switches::kEnableCmaMediaPipeline))
+      command_line->AppendSwitch(switches::kEnableCmaMediaPipeline);
   }
 }
 
