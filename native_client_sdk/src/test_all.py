a85c25312649d15f7af60205232e5a006d34df17
sbc <sbc@chromium.org>
2015-01-08 03:54:41 -0800

    [NaCl SDK] Store python coverage output data in 'out' directory
    Also add --quick argument to test_all.py that excludes
    large tests (those that take seconds rather than ms).
    
    Review URL: https://codereview.chromium.org/835773003
    
    Cr-Commit-Position: refs/heads/master@{#310481}

--- native_client_sdk/src/test_all.py
+++ native_client_sdk/src/test_all.py
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Top level script for running all python unittests in the NaCl SDK
+"""Top level script for running all python unittests in the NaCl SDK.
 """
 
 from __future__ import print_function
@@ -36,6 +36,9 @@ PKG_VER = os.path.join(PKG_VER_DIR, 'package_version.py')
 EXTRACT_PACKAGES = ['nacl_x86_glibc']
 TOOLCHAIN_OUT = os.path.join(build_paths.OUT_DIR, 'sdk_tests', 'toolchain')
 
+# List of modules containing unittests. The goal is to keep the total
+# runtime of these tests under 2 seconds. Any slower tests should go
+# in TEST_MODULES_BIG.
 TEST_MODULES = [
     'build_artifacts_test',
     'build_version_test',
@@ -51,25 +54,35 @@ TEST_MODULES = [
     'oshelpers_test',
     'parse_dsc_test',
     'quote_test',
-    'sdktools_commands_test',
     'sdktools_config_test',
-    'sdktools_test',
     'sel_ldr_test',
     'update_nacl_manifest_test',
     'verify_filelist_test',
     'verify_ppapi_test',
 ]
 
+
+# Slower tests. For example the 'sdktools' are mostly slower system tests
+# that longer to run.  If --quick is passed then we don't run these.
+TEST_MODULES_BIG = [
+    'sdktools_commands_test',
+    'sdktools_test',
+]
+
+
 def ExtractToolchains():
-  subprocess.check_output([sys.executable, PKG_VER,
-                           '--packages', ','.join(EXTRACT_PACKAGES),
-                           '--tar-dir', TAR_DIR,
-                           '--dest-dir', TOOLCHAIN_OUT,
-                           'extract'])
+  cmd = [sys.executable, PKG_VER,
+         '--packages', ','.join(EXTRACT_PACKAGES),
+         '--tar-dir', TAR_DIR,
+         '--dest-dir', TOOLCHAIN_OUT,
+         'extract']
+  subprocess.check_call(cmd)
+
 
 def main(args):
   parser = argparse.ArgumentParser(description=__doc__)
   parser.add_argument('-v', '--verbose', action='store_true')
+  parser.add_argument('--quick', action='store_true')
   options = parser.parse_args(args)
 
   # Some of the unit tests use parts of toolchains. Extract to TOOLCHAIN_OUT.
@@ -77,7 +90,11 @@ def main(args):
   ExtractToolchains()
 
   suite = unittest.TestSuite()
-  for module_name in TEST_MODULES:
+  modules = TEST_MODULES
+  if not options.quick:
+    modules += TEST_MODULES_BIG
+
+  for module_name in modules:
     module = __import__(module_name)
     suite.addTests(unittest.defaultTestLoader.loadTestsFromModule(module))
 
@@ -89,6 +106,7 @@ def main(args):
   print('Running unittests...')
   result = unittest.TextTestRunner(verbosity=verbosity).run(suite)
   return int(not result.wasSuccessful())
+
 
 if __name__ == '__main__':
   sys.exit(main(sys.argv[1:]))
