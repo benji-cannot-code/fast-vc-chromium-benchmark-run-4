f558c0c67300152a6bfb2ac7e80cf1214138e755
noel@chromium.org <noel@chromium.org>
2015-01-08 12:03:35 +0000

    Make WEBPImageDecoder noncopyable
    The other image decoders are WTF noncopyable: somehow the WEBP decoder
    was overlooked when that change was made.
    
    TBR=pdr@chromium.org
    BUG=404544
    
    Review URL: https://codereview.chromium.org/839733002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188035 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/image-decoders/webp/WEBPImageDecoder.h
+++ third_party/WebKit/Source/platform/image-decoders/webp/WEBPImageDecoder.h
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class PLATFORM_EXPORT WEBPImageDecoder : public ImageDecoder {
+    WTF_MAKE_NONCOPYABLE(WEBPImageDecoder);
 public:
     WEBPImageDecoder(ImageSource::AlphaOption, ImageSource::GammaAndColorProfileOption, size_t maxDecodedBytes);
     virtual ~WEBPImageDecoder();
