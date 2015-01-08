0c9c09d4c303fed79221e3842825834822ca9445
tobiasjs <tobiasjs@google.com>
2015-01-08 11:40:52 -0800

    Make unix socket Get{Peer|Local}Address return more rational errors.
    Rather than return ERR_NOT_IMPLEMENTED it seems sensible to
    return ERR_SOCKET_NOT_CONNECTED in the case that the socket
    is not connected, but unconditionally return ERR_ADDRESS_INVALID
    if connected (because there's no IP addr or port associated
    with either end of the socket).
    
    BUG=431412
    
    Review URL: https://codereview.chromium.org/841993002
    
    Cr-Commit-Position: refs/heads/master@{#310564}

--- net/socket/unix_domain_client_socket_posix.cc
+++ net/socket/unix_domain_client_socket_posix.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/posix/eintr_wrapper.h"
+#include "net/base/ip_endpoint.h"
 #include "net/base/net_errors.h"
 #include "net/base/net_util.h"
 #include "net/socket/socket_libevent.h"
@@ -99,13 +100,25 @@ bool UnixDomainClientSocket::IsConnectedAndIdle() const {
 }
 
 int UnixDomainClientSocket::GetPeerAddress(IPEndPoint* address) const {
-  NOTIMPLEMENTED();
-  return ERR_NOT_IMPLEMENTED;
+  // Unix domain sockets have no valid associated addr/port;
+  // return either not connected or address invalid.
+  DCHECK(address);
+
+  if (!IsConnected())
+    return ERR_SOCKET_NOT_CONNECTED;
+
+  return ERR_ADDRESS_INVALID;
 }
 
 int UnixDomainClientSocket::GetLocalAddress(IPEndPoint* address) const {
-  NOTIMPLEMENTED();
-  return ERR_NOT_IMPLEMENTED;
+  // Unix domain sockets have no valid associated addr/port;
+  // return either not connected or address invalid.
+  DCHECK(address);
+
+  if (!socket_)
+    return ERR_SOCKET_NOT_CONNECTED;
+
+  return ERR_ADDRESS_INVALID;
 }
 
 const BoundNetLog& UnixDomainClientSocket::NetLog() const {
