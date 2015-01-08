a552dded66a89ddd1a47467c8b8139993d3e7cf3
vadimt <vadimt@chromium.org>
2015-01-07 16:00:04 -0800

    Instrumenting QuicStreamFactory::Job internals for jank.
    Prior instrumentations showed:
    422516 QuicStreamFactory::Job::DoConnect = 36 jph
    422516 QuicStreamFactory::Job::DoConnect2 = 4.3 jph
    
    BUG=422516
    
    Review URL: https://codereview.chromium.org/830183002
    
    Cr-Commit-Position: refs/heads/master@{#310414}

--- net/quic/quic_crypto_client_stream.cc
+++ net/quic/quic_crypto_client_stream.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/quic/quic_crypto_client_stream.h"
 
 #include "base/metrics/histogram.h"
+#include "base/profiler/scoped_tracker.h"
 #include "net/quic/crypto/crypto_protocol.h"
 #include "net/quic/crypto/crypto_utils.h"
 #include "net/quic/crypto/null_encrypter.h"
@@ -176,6 +177,11 @@ static const int kMaxClientHellos = 3;
 
 void QuicCryptoClientStream::DoHandshakeLoop(
     const CryptoHandshakeMessage* in) {
+  // TODO(vadimt): Remove ScopedTracker below once crbug.com/422516 is fixed.
+  tracked_objects::ScopedTracker tracking_profile(
+      FROM_HERE_WITH_EXPLICIT_FUNCTION(
+          "422516 QuicCryptoClientStream::DoHandshakeLoop"));
+
   QuicCryptoClientConfig::CachedState* cached =
       crypto_config_->LookupOrCreate(server_id_);
 
