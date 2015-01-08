7d8fae52de4a66bd31d3e4c4a3b1d4d2b7835af2
kouhei@chromium.org <kouhei@chromium.org>
2015-01-08 11:50:57 +0000

    Avoid calling Heap::increaseMarkedObjectSize from inner loop
    Heap::increaseMarkedObjectSize is implemented as atomic add, which is slow.
    Minimize its call by bulk updating it.
    
    BUG=420515
    
    Review URL: https://codereview.chromium.org/844703002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188033 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/heap/Heap.cpp
+++ third_party/WebKit/Source/platform/heap/Heap.cpp
@@ -1500,6 +1500,7 @@ void HeapPage::sweep(ThreadHeap* heap)
 {
     clearObjectStartBitMap();
 
+    size_t markedObjectSize = 0;
     Address startOfGap = payload();
     for (Address headerAddress = startOfGap; headerAddress < payloadEnd(); ) {
         HeapObjectHeader* header = reinterpret_cast<HeapObjectHeader*>(headerAddress);
@@ -1540,11 +1541,13 @@ void HeapPage::sweep(ThreadHeap* heap)
             heap->addToFreeList(startOfGap, headerAddress - startOfGap);
         header->unmark();
         headerAddress += header->size();
-        Heap::increaseMarkedObjectSize(header->size());
+        markedObjectSize += header->size();
         startOfGap = headerAddress;
     }
     if (startOfGap != payloadEnd())
         heap->addToFreeList(startOfGap, payloadEnd() - startOfGap);
+
+    Heap::increaseMarkedObjectSize(markedObjectSize);
 }
 
 void HeapPage::markUnmarkedObjectsDead()
