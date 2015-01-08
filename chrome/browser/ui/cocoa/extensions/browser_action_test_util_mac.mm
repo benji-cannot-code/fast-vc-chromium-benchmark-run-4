dd8ee58d1d3bd34bac5766824f5a944c22c3088e
rdevlin.cronin <rdevlin.cronin@chromium.org>
2015-01-07 17:26:53 -0800

    [Extensions Toolbar] Cleanup browser_action_test_util
    Remove BrowserActionTestUtil::[Disable|Enable]Animations() and
    BrowserActionTestUtil::SetIconVisibilityCount().  These just performed
    single-line actions that were already platform-independent, and so the callers
    can just inline it.
    
    Review URL: https://codereview.chromium.org/835173002
    
    Cr-Commit-Position: refs/heads/master@{#310428}

--- chrome/browser/ui/cocoa/extensions/browser_action_test_util_mac.mm
+++ chrome/browser/ui/cocoa/extensions/browser_action_test_util_mac.mm
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/mac/foundation_util.h"
 #include "base/strings/sys_string_conversions.h"
-#include "chrome/browser/extensions/extension_toolbar_model.h"
 #include "chrome/browser/ui/browser.h"
 #import "chrome/browser/ui/cocoa/browser_window_cocoa.h"
 #import "chrome/browser/ui/cocoa/browser_window_controller.h"
@@ -21,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/ui/cocoa/themed_window.h"
 #import "chrome/browser/ui/cocoa/toolbar/toolbar_controller.h"
 #include "chrome/browser/ui/toolbar/toolbar_action_view_controller.h"
-#include "chrome/browser/ui/toolbar/toolbar_actions_bar.h"
 #include "grit/theme_resources.h"
 #include "ui/base/theme_provider.h"
 #include "ui/gfx/geometry/rect.h"
@@ -109,11 +107,6 @@ gfx::Size BrowserActionTestUtil::GetPopupSize() {
   return gfx::Size(NSSizeToCGSize(bounds.size));
 }
 
-void BrowserActionTestUtil::SetIconVisibilityCount(size_t icons) {
-  extensions::ExtensionToolbarModel::Get(browser_->profile())->
-      SetVisibleIconCount(icons);
-}
-
 bool BrowserActionTestUtil::HidePopup() {
   ExtensionPopupController* controller = [ExtensionPopupController popup];
   // The window must be gone or we'll fail a unit test with windows left open.
@@ -146,16 +139,6 @@ bool BrowserActionTestUtil::OverflowedActionButtonWantsToRun() {
   WrenchToolbarButtonCell* cell =
       base::mac::ObjCCastStrict<WrenchToolbarButtonCell>([wrenchButton cell]);
   return [cell overflowedToolbarActionWantsToRun];
-}
-
-// static
-void BrowserActionTestUtil::DisableAnimations() {
-  ToolbarActionsBar::disable_animations_for_testing_ = true;
-}
-
-// static
-void BrowserActionTestUtil::EnableAnimations() {
-  ToolbarActionsBar::disable_animations_for_testing_ = false;
 }
 
 // static
