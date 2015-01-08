14728919d8912caa862827d73e2ee492db720b45
rouslan <rouslan@chromium.org>
2015-01-07 17:09:17 -0800

    [autofill] Use default country code in settings when none selected.
    If you do not select a country when adding an address in autofill
    settings, then the default country code is used. This enables preserving
    the address format (field positions and labels) the next time you edit
    this address.
    
    BUG=445709
    
    Review URL: https://codereview.chromium.org/794433004
    
    Cr-Commit-Position: refs/heads/master@{#310425}

--- chrome/browser/resources/options/autofill_edit_address_overlay.js
+++ chrome/browser/resources/options/autofill_edit_address_overlay.js
@@ -243,7 +243,7 @@ cr.define('options', function() {
         inputFields['state'] || '',
         inputFields['postalCode'] || '',
         inputFields['sortingCode'] || '',
-        inputFields['country'] || '',
+        inputFields['country'] || loadTimeData.getString('defaultCountryCode'),
         inputFields['phone'] || [],
         inputFields['email'] || [],
         this.languageCode_,
