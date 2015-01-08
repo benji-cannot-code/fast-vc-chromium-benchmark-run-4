df0f64fc9977cde02ff26cd12ac5b5afdc241d72
satorux <satorux@chromium.org>
2015-01-07 17:53:01 -0800

    Fix weird includes of extension_prefs_unittest.h
    Having /./ in #includes paths is weird and not necessary.
    
    BUG=313284
    TEST=everything builds as before
    
    Review URL: https://codereview.chromium.org/827843005
    
    Cr-Commit-Position: refs/heads/master@{#310437}

--- chrome/browser/extensions/chrome_app_sorting_unittest.cc
+++ chrome/browser/extensions/chrome_app_sorting_unittest.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 
-#include "chrome/browser/extensions/./extension_prefs_unittest.h"
+#include "chrome/browser/extensions/extension_prefs_unittest.h"
 #include "components/crx_file/id_util.h"
 #include "extensions/common/constants.h"
 #include "extensions/common/manifest_constants.h"
--- chrome/browser/extensions/extension_prefs_unittest.cc
+++ chrome/browser/extensions/extension_prefs_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/extensions/./extension_prefs_unittest.h"
+#include "chrome/browser/extensions/extension_prefs_unittest.h"
 
 #include "base/basictypes.h"
 #include "base/files/scoped_temp_dir.h"
