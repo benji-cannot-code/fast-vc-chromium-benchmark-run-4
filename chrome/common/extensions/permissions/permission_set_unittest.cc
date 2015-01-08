7a37df5055009ab410ca239d252dfacf3264c9d9
reillyg <reillyg@chromium.org>
2015-01-07 22:44:34 -0800

    Remove the "Access your input devices" permission message.
    As with the "Access your USB devices" permission message that was
    removed in 400b26297782e70ed15e40d83b876e3abde7b9cc this permission
    message duplicates the messages generated by the "usbDevices"
    permission. I also consider it "overly alarmist" as the use case
    for the chrome.hid API is often unrelated to input devices.
    
    For example, the blink(1) sample app* has following single permission
    string after this change, "Access any blink(1) from ThingM via USB."
    
    * https://chrome.google.com/webstore/detail/kcpjgiicabigbjejdjnkflkdkjknkdch
    
    BUG=
    
    Review URL: https://codereview.chromium.org/840983002
    
    Cr-Commit-Position: refs/heads/master@{#310458}

--- chrome/common/extensions/permissions/permission_set_unittest.cc
+++ chrome/common/extensions/permissions/permission_set_unittest.cc
@@ -766,6 +766,7 @@ TEST(PermissionsTest, PermissionMessages) {
 
   // Platform apps.
   skip.insert(APIPermission::kBrowser);
+  skip.insert(APIPermission::kHid);
   skip.insert(APIPermission::kFileSystem);
   skip.insert(APIPermission::kFileSystemProvider);
   skip.insert(APIPermission::kFileSystemRetainEntries);
