3a47a75843b7dbf48bf61f825f7fc7c78b82fb2d
johnme <johnme@chromium.org>
2015-01-08 09:26:51 -0800

    Push API: Remove AddAppHandlerIfNecessary to prevent DCHECK
    Previously we would try to add the app handler twice for the first
    registration - once in AddAppHandlerIfNecessary called by RegisterFrom*,
    then again in IncreasePushRegistrationCount called by RegisterEnd.
    
    Adding the app handler a second time causes GCMDriver to DCHECK.
    
    It's ok to just add it in IncreasePushRegistrationCount (it was never
    actually necessary to have a GCMAppHandler to register).
    
    BUG=436453
    
    Review URL: https://codereview.chromium.org/823993003
    
    Cr-Commit-Position: refs/heads/master@{#310523}

--- chrome/browser/services/gcm/push_messaging_service_impl.cc
+++ chrome/browser/services/gcm/push_messaging_service_impl.cc
@@ -248,13 +248,6 @@ void PushMessagingServiceImpl::RegisterFromDocument(
     return;
   }
 
-  // TODO(johnme): This is probably redundant due to
-  // https://codereview.chromium.org/756063002, or even if it isn't it'll
-  // interfere with auto-removing the app handler, so should be removed.
-  // If this is registering for the first time then the driver does not have
-  // this as an app handler and registration would fail.
-  AddAppHandlerIfNecessary();
-
   content::RenderFrameHost* render_frame_host =
       content::RenderFrameHost::FromID(renderer_id, render_frame_id);
   if (!render_frame_host)
@@ -315,10 +308,6 @@ void PushMessagingServiceImpl::RegisterFromWorker(
                 content::PUSH_REGISTRATION_STATUS_LIMIT_REACHED);
     return;
   }
-
-  // If this is registering for the first time then the driver does not have
-  // this as an app handler and registration would fail.
-  AddAppHandlerIfNecessary();
 
   GURL embedding_origin = requesting_origin;
   blink::WebPushPermissionStatus permission_status =
@@ -455,13 +444,6 @@ bool PushMessagingServiceImpl::HasPermission(const GURL& origin) {
 
   return permission_context->GetPermissionStatus(origin, origin) ==
       CONTENT_SETTING_ALLOW;
-}
-
-void PushMessagingServiceImpl::AddAppHandlerIfNecessary() {
-  if (gcm_profile_service_->driver()->GetAppHandler(
-          kPushMessagingApplicationIdPrefix) != this)
-    gcm_profile_service_->driver()->AddAppHandler(
-        kPushMessagingApplicationIdPrefix, this);
 }
 
 }  // namespace gcm
