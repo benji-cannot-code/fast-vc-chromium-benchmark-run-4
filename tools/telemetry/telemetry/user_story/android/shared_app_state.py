57d18818ae91c25774487b6e78ebc6031fd1a2e0
ariblue <ariblue@google.com>
2015-01-08 19:25:03 -0800

    Enable accessing webviews for android user stories.
    This is done by passing the android app object (which has the new GetProcesses,
    GetProcess, and GetWebViews functions).
    
    BUG=
    
    Review URL: https://codereview.chromium.org/842963003
    
    Cr-Commit-Position: refs/heads/master@{#310699}

--- tools/telemetry/telemetry/user_story/android/shared_app_state.py
+++ tools/telemetry/telemetry/user_story/android/shared_app_state.py
@@ -45,6 +45,10 @@ class SharedAppState(shared_user_story_state.SharedUserStoryState):
         self._android_platform, android_platform.AndroidPlatform)
 
   @property
+  def app(self):
+    return self._android_app
+
+  @property
   def platform(self):
     return self._android_platform
 
@@ -55,8 +59,7 @@ class SharedAppState(shared_user_story_state.SharedUserStoryState):
         user_story.start_intent)
 
   def RunUserStory(self, results):
-    # TODO(chrishenry): Implement this properly.
-    self._current_user_story.Run()
+    self._current_user_story.Run(self)
 
   def DidRunUserStory(self, results):
     if self._android_app:
