761670e13927983d6b852b73a46354a3271422ac
erg <erg@chromium.org>
2015-01-09 13:39:23 -0800

    Revert of Mark entries as imported in response to transfer update events. (patchset #4 id:60001 of https://codereview.chromium.org/840023005/)
    Reason for revert:
    Speculative revert to fix flaky failure of DefaultActionDialog/FileManagerBrowserTest.Test/1 on Linux ChromiumOS Tests (1).
    
    In the run where this patch was committed, the above test started failing flakily; it passes sometimes. This bot doesn't look like it has a history of this test being flaky. Here are two runs where it failed:
    
    https://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Tests%20%281%29/builds/54433
    https://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Tests%20%281%29/builds/54436
    
    Original issue's description:
    > Mark entries as imported in response to transfer update events.
    >
    > Hook up to chrome.fileManagerPrivate.onFileTransfersUpdated to monitor sync events.
    > When a sync is 'completed' mark the entry as IMPORTED in import history using the URL.
    > The absence of a live Entity at this point necessitated some refactoring of ImportHistory:
    >   * Keep an index of destination URL to key...allowing us to look up copy records by destinationURL.
    >   * Update copy records to include source and destination URLs.
    >     - SourceURL was added as we need a way to identify which source entry has been updated.
    >     - This knowledge is required in file manager to determin if the grid view (of the removable device) should be refreshed.
    >   * Cleanup record reading and writing...drop the user of opt args. Pull use of the "record" Array up one level in the code.
    >
    > BUG=420680
    > TEST=browser_test: FileManagerJsTest.*
    >
    > Committed: https://crrev.com/fe8e7b21676456106df67f878df48d3853e66ba2
    > Cr-Commit-Position: refs/heads/master@{#310824}
    
    TBR=kenobi@chromium.org,hirono@chromium.org,smckay@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=420680
    
    Review URL: https://codereview.chromium.org/826143003
    
    Cr-Commit-Position: refs/heads/master@{#310861}

--- ui/file_manager/file_manager/background/js/import_history_unittest.js
+++ ui/file_manager/file_manager/background/js/import_history_unittest.js
@@ -41,24 +41,19 @@ var testPromise;
 
 // Set up the test components.
 function setUp() {
-  setupChromeApis();
-  testFileSystem = new MockFileSystem('abc-123', 'filesystem:abc-123');
   testFileEntry = new MockFileEntry(
       testFileSystem,
       FILE_PATH, {
         size: FILE_SIZE,
         modificationTime: FILE_LAST_MODIFIED
       });
+  testFileSystem = new MockFileSystem('abc-123', 'filesystem:abc-123');
   testFileSystem.entries[FILE_PATH] = testFileEntry;
 
   storage = new TestRecordStorage();
 
   var history = new importer.PersistentImportHistory(storage);
   historyProvider = history.refresh();
-}
-
-function tearDown() {
-  testPromise = null;
 }
 
 function testHistoryWasCopiedFalseForUnknownEntry(callback) {
@@ -94,16 +89,6 @@ function testHistoryWasImportedTrueForKnownEntrySetAtRuntime(callback) {
   reportPromise(testPromise, callback);
 }
 
-function testHistoryWasCopiedTrueForKnownEntryLoadedFromStorage(callback) {
-  // TestRecordWriter is pre-configured with this entry.
-  testPromise = historyProvider.then(
-      function(history) {
-        history.wasCopied(testFileEntry, GOOGLE_DRIVE).then(assertTrue);
-      });
-
-  reportPromise(testPromise, callback);
-}
-
 function testCopyChangeFiresChangedEvent(callback) {
   testPromise = historyProvider.then(
       function(history) {
@@ -120,27 +105,6 @@ function testCopyChangeFiresChangedEvent(callback) {
                             recorder.getLastArguments()[0]['state']);
                       });
             });
-      });
-
-  reportPromise(testPromise, callback);
-}
-
-function testMarkImportedByUrl(callback) {
-  var destinationUrl = 'filesystem:chrome-extension://abc/photos/splosion.jpg';
-  testPromise = historyProvider.then(
-      function(history) {
-        return history.markCopied(testFileEntry, SPACE_CAMP, destinationUrl)
-            .then(
-                function() {
-                  return history.markImportedByUrl(destinationUrl)
-                      .then(
-                          function() {
-                            return history.wasImported(
-                                testFileEntry,
-                                SPACE_CAMP)
-                                .then(assertTrue);
-                          });
-                });
       });
 
   reportPromise(testPromise, callback);
@@ -309,14 +273,13 @@ function testHistoryLoaderIntegration(callback) {
 }
 
 /**
- * Installs stub APIs.
+ * Shared error handler.
+ * @param {function()} callback
+ * @param {*} error
  */
-function setupChromeApis() {
-  chrome = {};
-  chrome.fileManagerPrivate = {};
-  chrome.fileManagerPrivate.onFileTransfersUpdated = {
-    addListener: function() {}
-  };
+function handleError(callback, error) {
+  console.error(error && (error.stack || error) || 'Unspecified test error.');
+  callback(/* error */ true);
 }
 
 /**
