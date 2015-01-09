8989cd253eecc570e728fb8156bf2ad9d394596d
xiyuan <xiyuan@chromium.org>
2015-01-08 18:55:23 -0800

    cros: Update help for enable dev UI.
    BUG=444108
    
    Review URL: https://codereview.chromium.org/843873002
    
    Cr-Commit-Position: refs/heads/master@{#310690}

--- chrome/browser/ui/webui/chromeos/login/enable_debugging_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/enable_debugging_screen_handler.cc
@@ -288,7 +288,7 @@ void EnableDebuggingScreenHandler::HandleOnLearnMore() {
       Profile::FromWebUI(web_ui()),
       NULL,
       GetNativeWindow(),
-      l10n_util::GetStringUTF16(IDS_ENABLE_DEBUGGING_SCREEN_TITLE),
+      base::string16(),
       data_url);
   dialog->Show();
 }
