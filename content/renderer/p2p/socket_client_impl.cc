757dff90c4f052120370eb2afa687e117f6cab5d
guoweis <guoweis@chromium.org>
2015-01-09 12:27:15 -0800

    Added a CHECK assert which will cause crash when missing send completion signal is detected.
    1. change in_fligth_packets_size_ to not only tracking size but also packet_id
    2. pass back the packet_id in the path of completion signal such that it could
    be matched with what's been tracked in in_flight_packets_size
    
    BUG=427555
    
    Review URL: https://codereview.chromium.org/759923003
    
    Cr-Commit-Position: refs/heads/master@{#310844}

--- content/renderer/p2p/socket_client_impl.cc
+++ content/renderer/p2p/socket_client_impl.cc
@@ -16,8 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-uint64 GetUniqueId(uint32 random_socket_id, uint32 packet_id) {
-  uint64 uid = random_socket_id;
+uint64_t GetUniqueId(uint32 random_socket_id, uint32 packet_id) {
+  uint64_t uid = random_socket_id;
   uid <<= 32;
   uid |= packet_id;
   return uid;
@@ -70,31 +70,33 @@ void P2PSocketClientImpl::DoInit(P2PSocketType type,
       type, socket_id_, local_address, remote_address));
 }
 
-void P2PSocketClientImpl::SendWithDscp(
-    const net::IPEndPoint& address,
-    const std::vector<char>& data,
-    const rtc::PacketOptions& options) {
+uint64_t P2PSocketClientImpl::Send(const net::IPEndPoint& address,
+                                   const std::vector<char>& data,
+                                   const rtc::PacketOptions& options) {
+  uint64_t unique_id = GetUniqueId(random_socket_id_, ++next_packet_id_);
   if (!ipc_message_loop_->BelongsToCurrentThread()) {
     ipc_message_loop_->PostTask(
-        FROM_HERE, base::Bind(
-            &P2PSocketClientImpl::SendWithDscp, this, address, data, options));
-    return;
+        FROM_HERE, base::Bind(&P2PSocketClientImpl::SendWithPacketId, this,
+                              address, data, options, unique_id));
+    return unique_id;
   }
 
   // Can send data only when the socket is open.
   DCHECK(state_ == STATE_OPEN || state_ == STATE_ERROR);
   if (state_ == STATE_OPEN) {
-    uint64 unique_id = GetUniqueId(random_socket_id_, ++next_packet_id_);
-    TRACE_EVENT_ASYNC_BEGIN0("p2p", "Send", unique_id);
-    dispatcher_->SendP2PMessage(new P2PHostMsg_Send(socket_id_, address, data,
-                                                    options, unique_id));
+    SendWithPacketId(address, data, options, unique_id);
   }
+
+  return unique_id;
 }
 
-void P2PSocketClientImpl::Send(const net::IPEndPoint& address,
-                               const std::vector<char>& data) {
-  rtc::PacketOptions options(rtc::DSCP_DEFAULT);
-  SendWithDscp(address, data, options);
+void P2PSocketClientImpl::SendWithPacketId(const net::IPEndPoint& address,
+                                           const std::vector<char>& data,
+                                           const rtc::PacketOptions& options,
+                                           uint64_t packet_id) {
+  TRACE_EVENT_ASYNC_BEGIN0("p2p", "Send", packet_id);
+  dispatcher_->SendP2PMessage(
+      new P2PHostMsg_Send(socket_id_, address, data, options, packet_id));
 }
 
 void P2PSocketClientImpl::SetOption(P2PSocketOption option,
@@ -197,17 +199,20 @@ void P2PSocketClientImpl::DeliverOnIncomingTcpConnection(
   }
 }
 
-void P2PSocketClientImpl::OnSendComplete() {
+void P2PSocketClientImpl::OnSendComplete(
+    const P2PSendPacketMetrics& send_metrics) {
   DCHECK(ipc_message_loop_->BelongsToCurrentThread());
 
   delegate_message_loop_->PostTask(
-      FROM_HERE, base::Bind(&P2PSocketClientImpl::DeliverOnSendComplete, this));
+      FROM_HERE, base::Bind(&P2PSocketClientImpl::DeliverOnSendComplete, this,
+                            send_metrics));
 }
 
-void P2PSocketClientImpl::DeliverOnSendComplete() {
+void P2PSocketClientImpl::DeliverOnSendComplete(
+    const P2PSendPacketMetrics& send_metrics) {
   DCHECK(delegate_message_loop_->BelongsToCurrentThread());
   if (delegate_)
-    delegate_->OnSendComplete();
+    delegate_->OnSendComplete(send_metrics);
 }
 
 void P2PSocketClientImpl::OnError() {
