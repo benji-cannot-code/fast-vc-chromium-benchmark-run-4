3d18ef9a64fe6a3f50a785be956ed236d0668b29
jbudorick@chromium.org <jbudorick@chromium.org>
2015-01-09 17:58:58 +0000

    Revert of Re-enable the activation behavior of media elements (click to play/pause) (patchset #3 id:40001 of https://codereview.chromium.org/830183005/)
    Reason for revert:
    breaks androidwebview_instrumentation_tests on Android Tests (dbg) on chromium.webkit starting with http://build.chromium.org/p/chromium.webkit/builders/Android%20Tests%20%28dbg%29/builds/23717
    
    Original issue's description:
    > Re-enable the activation behavior of media elements (click to play/pause)
    > 
    > This revives most of the code and tests from an earlier revert:
    > https://codereview.chromium.org/249483002
    > 
    > The "Don't fire events when interacting with <video controls>" bug has
    > been fixed, so the native controls and scripted controls will no
    > longer compete for control:
    > https://codereview.chromium.org/406213002
    > 
    > The definition of "restrained media controller" has been simplified in
    > spec and implementation since the last time:
    > https://codereview.chromium.org/262463003
    > 
    > BUG=354746
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=188141
    
    TBR=fs@opera.com,aberent@chromium.org,philipj@opera.com
    NOTREECHECKS=true
    NOTRY=true
    BUG=354746
    
    Review URL: https://codereview.chromium.org/839293002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188160 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/shadow/MediaControlElements.h
+++ third_party/WebKit/Source/core/html/shadow/MediaControlElements.h
@@ -130,6 +130,7 @@ private:
     explicit MediaControlOverlayPlayButtonElement(MediaControls&);
 
     virtual const AtomicString& shadowPseudoId() const override;
+    virtual void defaultEventHandler(Event*) override;
     virtual bool keepEventInNode(Event*) override;
 };
 
