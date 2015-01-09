c3eb41d9275b576414b740a306ae6dfc2e7ed3c8
tedchoc <tedchoc@chromium.org>
2015-01-09 14:53:53 -0800

    Fix < JBMR2, os.Trace only exists in API 18+.
    NOTRY=true
    BUG=
    
    Review URL: https://codereview.chromium.org/822933003
    
    Cr-Commit-Position: refs/heads/master@{#310874}

--- base/android/java/src/org/chromium/base/ResourceExtractor.java
+++ base/android/java/src/org/chromium/base/ResourceExtractor.java
@@ -5,12 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.base;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Trace;
 import android.preference.PreferenceManager;
 import android.util.Log;
@@ -62,11 +64,11 @@ public class ResourceExtractor {
             }
 
             String timestampFile = null;
-            Trace.beginSection("checkPakTimeStamp");
+            beginTraceSection("checkPakTimeStamp");
             try {
                 timestampFile = checkPakTimestamp(outputDir);
             } finally {
-                Trace.endSection();
+                endTraceSection();
             }
             if (timestampFile != null) {
                 deleteFiles();
@@ -110,7 +112,7 @@ public class ResourceExtractor {
             Pattern paksToInstall = Pattern.compile(p.toString());
 
             AssetManager manager = mContext.getResources().getAssets();
-            Trace.beginSection("WalkAssets");
+            beginTraceSection("WalkAssets");
             try {
                 // Loop through every asset file that we have in the APK, and look for the
                 // ones that we need to extract by trying to match the Patterns that we
@@ -131,7 +133,7 @@ public class ResourceExtractor {
 
                     InputStream is = null;
                     OutputStream os = null;
-                    Trace.beginSection("ExtractResource");
+                    beginTraceSection("ExtractResource");
                     try {
                         is = manager.open(file);
                         os = new FileOutputStream(output);
@@ -167,7 +169,7 @@ public class ResourceExtractor {
                             if (os != null) {
                                 os.close();
                             }
-                            Trace.endSection(); // ExtractResource
+                            endTraceSection(); // ExtractResource
                         }
                     }
                 }
@@ -180,7 +182,7 @@ public class ResourceExtractor {
                 deleteFiles();
                 return;
             } finally {
-                Trace.endSection(); // WalkAssets
+                endTraceSection(); // WalkAssets
             }
 
             // Finished, write out a timestamp file if we need to.
@@ -204,11 +206,11 @@ public class ResourceExtractor {
             // doInBackgroundImpl) when it will be possible. This is currently
             // not doable since the native library is not loaded yet, and the
             // TraceEvent calls are dropped before this point.
-            Trace.beginSection("ResourceExtractor.ExtractTask.doInBackground");
+            beginTraceSection("ResourceExtractor.ExtractTask.doInBackground");
             try {
                 doInBackgroundImpl();
             } finally {
-                Trace.endSection();
+                endTraceSection();
             }
             return null;
         }
@@ -256,6 +258,18 @@ public class ResourceExtractor {
 
             // timestamp file is already up-to date.
             return null;
+        }
+
+        @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+        private void beginTraceSection(String section) {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR2) return;
+            Trace.beginSection(section);
+        }
+
+        @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+        private void endTraceSection() {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR2) return;
+            Trace.endSection();
         }
     }
 
