2643dfc10e18b8dac9d01a9925ab167e979f91b8
mtomasz <mtomasz@chromium.org>
2015-01-09 00:38:44 -0800

    [fsp] Fix unhappy MSAN build bots.
    Before, an uninitialized class was being used in queue.h, which is illegal, but
    for some strange reason fails on MSAN build bots only.
    
    TEST=Compiles on MSAN.
    BUG=None
    
    Review URL: https://codereview.chromium.org/813403005
    
    Cr-Commit-Position: refs/heads/master@{#310741}

--- chrome/browser/chromeos/file_system_provider/queue.cc
+++ chrome/browser/chromeos/file_system_provider/queue.cc
@@ -13,16 +13,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 namespace file_system_provider {
 
-struct Queue::Task {
-  Task() : token(0), completed(false) {}
-  Task(size_t token, const AbortableCallback& callback)
-      : token(token), completed(false), callback(callback) {}
+Queue::Task::Task() : token(0), completed(false) {
+}
 
-  size_t token;
-  bool completed;
-  AbortableCallback callback;
-  AbortCallback abort_callback;
-};
+Queue::Task::Task(size_t token, const AbortableCallback& callback)
+    : token(token), completed(false), callback(callback) {
+}
+
+Queue::Task::~Task() {
+}
 
 Queue::Queue(size_t max_in_parallel)
     : max_in_parallel_(max_in_parallel),
