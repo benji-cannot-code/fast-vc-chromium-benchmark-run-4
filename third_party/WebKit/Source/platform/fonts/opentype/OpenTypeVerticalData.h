a9ef4fa0a5871f74f448daeeed110fb06690de6d
kojii@chromium.org <kojii@chromium.org>
2015-01-09 05:39:15 +0000

    Vertical flow in SimplePath
    This patch changes the default code path of vertical flow text back to the
    simple code path. This was changed to complex path in crbug.com/408992, but
    it turned out that it degrades the performance by 100-150%.
    
    By changing the default path back, but keep all the good changes in the bug,
    we could make the complex path fast enough to re-enable by default later.
    
    This patch also contains a cleanup of Font::drawGlyphs() and eliminates
    Font::paintGlyphsVertical() completely. Most of simple path for vertical flow
    code were merged to complex path code, except only 4 lines, so that we could
    remove them easier when we're ready to swich.
    
    BUG=445589
    
    Review URL: https://codereview.chromium.org/826413002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188117 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/fonts/opentype/OpenTypeVerticalData.h
+++ third_party/WebKit/Source/platform/fonts/opentype/OpenTypeVerticalData.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "platform/PlatformExport.h"
 #include "platform/fonts/Glyph.h"
+#include "platform/fonts/GlyphPage.h"
 #include "wtf/HashMap.h"
 #include "wtf/PassRefPtr.h"
 #include "wtf/RefCounted.h"
@@ -48,17 +49,17 @@ public:
     bool isOpenType() const { return !m_advanceWidths.isEmpty(); }
     bool hasVerticalMetrics() const { return !m_advanceHeights.isEmpty(); }
     float advanceHeight(const SimpleFontData*, Glyph) const;
-
+    void getVerticalTranslationsForGlyphs(const SimpleFontData*, const Glyph*, size_t, float* outXYArray) const;
+    void substituteWithVerticalGlyphs(const SimpleFontData*, GlyphPage*, unsigned offset, unsigned length) const;
     bool inFontCache() const { return m_inFontCache; }
     void setInFontCache(bool inFontCache) { m_inFontCache = inFontCache; }
-
-    void getVerticalTranslationsForGlyphs(const SimpleFontData*, const Glyph*, size_t, float* outXYArray) const;
 
 private:
 
     explicit OpenTypeVerticalData(const FontPlatformData&);
 
     void loadMetrics(const FontPlatformData&);
+    void loadVerticalGlyphSubstitutions(const FontPlatformData&);
     bool hasVORG() const { return !m_vertOriginY.isEmpty(); }
 
     HashMap<Glyph, Glyph> m_verticalGlyphMap;
