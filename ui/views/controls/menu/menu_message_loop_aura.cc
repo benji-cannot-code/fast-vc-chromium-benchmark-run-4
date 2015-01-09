720926c21daa07bc4660642dc210a2b5e1abf5de
spang <spang@chromium.org>
2015-01-08 17:26:25 -0800

    views: menu: Enforce nested dispatcher destruction ordering
    Nested dispatchers must be destroyed in the opposite order they are
    installed. We shouldn't restore the dispatcher in QuitNow() if the nested
    loop is still running, since another dispatcher might be stacked on top
    of it (e.g. drag & drop).
    
    This fixes a regression since r310020 ("views: menu: Remove
    MenuMessageLoop::ShouldQuitNow"). Before that change, we explicitly
    checked for drag & drop operations, and delayed message loop termination
    if one was in progress. Since it now terminates regardless of DnD state,
    the dispatcher gets destroyed earlier and (incorrectly) before the DnD
    dispatcher is destroyed. Fix this by delaying dispatcher destruction
    until our MenuMessageLoop actually returns.
    
    BUG=447098
    TEST=interactive_ui_tests
    
    Review URL: https://codereview.chromium.org/809313010
    
    Cr-Commit-Position: refs/heads/master@{#310651}

--- ui/views/controls/menu/menu_message_loop_aura.cc
+++ ui/views/controls/menu/menu_message_loop_aura.cc
@@ -157,10 +157,9 @@ void MenuMessageLoopAura::Run(MenuController* controller,
   }
 #else
   internal::MenuEventDispatcher event_dispatcher(controller);
-  scoped_ptr<ui::ScopedEventDispatcher> old_dispatcher =
-      nested_dispatcher_.Pass();
+  scoped_ptr<ui::ScopedEventDispatcher> dispatcher_override;
   if (ui::PlatformEventSource::GetInstance()) {
-    nested_dispatcher_ =
+    dispatcher_override =
         ui::PlatformEventSource::GetInstance()->OverrideDispatcher(
             &event_dispatcher);
   }
@@ -179,15 +178,12 @@ void MenuMessageLoopAura::Run(MenuController* controller,
     message_loop_quit_ = run_loop.QuitClosure();
     run_loop.Run();
   }
-  nested_dispatcher_ = old_dispatcher.Pass();
 #endif
 }
 
 void MenuMessageLoopAura::QuitNow() {
   CHECK(!message_loop_quit_.is_null());
   message_loop_quit_.Run();
-  // Restore the previous dispatcher.
-  nested_dispatcher_.reset();
 }
 
 void MenuMessageLoopAura::ClearOwner() {
