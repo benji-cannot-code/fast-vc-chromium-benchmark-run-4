56243d1ced3a677e5b7a2c00572e523ee04a076c
jl@opera.com <jl@opera.com>
2015-01-09 10:15:25 +0000

    IDL: Support iterable<>, maplike<> and setlike<> syntax
    This adds support for the basic syntax (in idl_definitions.py) but no
    handling of the additional parsed information beyond what the [Iterable]
    extended attribute already does. Extended attributes are currently not
    supported on the iterable<>, maplike<> or setlike<> definitions.
    
    In existing interfaces, replace [Iterable] with what the specification
    has, or in the case of MIDIInputMap and MIDIOutputMap, what seems to be
    the intention. In the Iterator interface, [Iterable] is still used,
    since it's somewhat unclear what it ought to be replaced with.
    
    This results in no changes in generated code.
    
    BUG=432683
    
    Review URL: https://codereview.chromium.org/841973002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188135 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/fetch/Headers.h
+++ third_party/WebKit/Source/modules/fetch/Headers.h
@@ -43,7 +43,7 @@ public:
     bool has(const String& key, ExceptionState&);
     void set(const String& key, const String& value, ExceptionState&);
 
-    // Iterable
+    // iterable<ByteString, ByteString>
     Iterator* iterator(ScriptState*, ExceptionState&);
 
     void setGuard(Guard guard) { m_guard = guard; }
