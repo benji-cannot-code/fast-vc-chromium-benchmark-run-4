9a40a9d15f64a105882edc658f4a2668457c71ee
nhiroki <nhiroki@chromium.org>
2015-01-09 02:33:18 -0800

    ServiceWorker: ReadRegistration should return NOT_FOUND when the database is not initialized
    ServiceWorkerDatabase::ReadRegistration and ReadUserData should return NOT_FOUND
    when the database hasn't been initialized yet, but the current implementation
    wrongly returns STATUS_OK without any valid data. This CL fixes it.
    
    BUG=447469
    TEST=content_unittests --gtest_filter=ServiceWorkerDatabaseTest.*
    
    Review URL: https://codereview.chromium.org/846443005
    
    Cr-Commit-Position: refs/heads/master@{#310749}

--- content/browser/service_worker/service_worker_database.cc
+++ content/browser/service_worker/service_worker_database.cc
@@ -506,7 +506,9 @@ ServiceWorkerDatabase::Status ServiceWorkerDatabase::ReadRegistration(
   DCHECK(resources);
 
   Status status = LazyOpen(false);
-  if (IsNewOrNonexistentDatabase(status) || status != STATUS_OK)
+  if (IsNewOrNonexistentDatabase(status))
+    return STATUS_ERROR_NOT_FOUND;
+  if (status != STATUS_OK)
     return status;
 
   RegistrationData value;
@@ -709,7 +711,9 @@ ServiceWorkerDatabase::Status ServiceWorkerDatabase::ReadUserData(
   DCHECK(user_data);
 
   Status status = LazyOpen(false);
-  if (IsNewOrNonexistentDatabase(status) || status != STATUS_OK)
+  if (IsNewOrNonexistentDatabase(status))
+    return STATUS_ERROR_NOT_FOUND;
+  if (status != STATUS_OK)
     return status;
 
   const std::string key = CreateUserDataKey(registration_id, user_data_name);
@@ -730,7 +734,9 @@ ServiceWorkerDatabase::Status ServiceWorkerDatabase::WriteUserData(
   DCHECK(!user_data_name.empty());
 
   Status status = LazyOpen(false);
-  if (IsNewOrNonexistentDatabase(status) || status != STATUS_OK)
+  if (IsNewOrNonexistentDatabase(status))
+    return STATUS_ERROR_NOT_FOUND;
+  if (status != STATUS_OK)
     return status;
 
   // There should be the registration specified by |registration_id|.
