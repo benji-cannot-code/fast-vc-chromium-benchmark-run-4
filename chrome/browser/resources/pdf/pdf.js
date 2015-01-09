828c49f6c846d47a70019657c65c290aa3a08623
keishi <keishi@chromium.org>
2015-01-08 21:58:01 -0800

    Revert of Add print and getSelectedText functions to the OOP PDF scripting API (patchset #3 id:40001 of https://codereview.chromium.org/843463002/)
    Reason for revert:
    Win7 Tests fail the test PDFExtensionTest.BasicPlugin.
    
    API Test Error in testGetSelectedText
    
    http://build.chromium.org/p/chromium.webkit/builders/Win7%20Tests/builds/7823/steps/browser_tests/logs/BasicPlugin
    
    Original issue's description:
    > Add print and getSelectedText functions to the OOP PDF scripting API
    >
    > These are needed by some internal users (see the bug for more details).
    > A test is added for selectAll/getSelectedText.
    >
    > BUG=415858
    >
    > Committed: https://crrev.com/7b1df224a957da8376e0ae71ab2d53dabf658b0a
    > Cr-Commit-Position: refs/heads/master@{#310663}
    
    TBR=sammc@chromium.org,raymes@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=415858
    
    Review URL: https://codereview.chromium.org/843923002
    
    Cr-Commit-Position: refs/heads/master@{#310716}

--- chrome/browser/resources/pdf/pdf.js
+++ chrome/browser/resources/pdf/pdf.js
@@ -377,9 +377,6 @@ PDFViewer.prototype = {
         else
           this.passwordScreen_.deny();
         break;
-      case 'getSelectedTextReply':
-        this.sendScriptingMessage_(message.data);
-        break;
       case 'goToPage':
         this.viewport_.goToPage(message.data.page);
         break;
@@ -529,10 +526,7 @@ PDFViewer.prototype = {
   handleScriptingMessage: function(message) {
     switch (message.data.type.toString()) {
       case 'getAccessibilityJSON':
-      case 'getSelectedText':
       case 'loadPreviewPage':
-      case 'print':
-      case 'selectAll':
         this.plugin_.postMessage(message.data);
         break;
       case 'resetPrintPreviewMode':
