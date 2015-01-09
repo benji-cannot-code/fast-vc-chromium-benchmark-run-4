e49f1782d807ce3bbfd790cdeb9f29b0b850c685
chrishenry <chrishenry@google.com>
2015-01-09 15:58:59 -0800

    Add app_backend.platform_backend property.
    This adds symmetry with app.platform and allow DevToolsClientBackend
    to retrieve the platform_backend instance.
    
    Collateral cleanup: Also remove app_backend.devtools_client.
    
    BUG=443897
    
    Review URL: https://codereview.chromium.org/834873003
    
    Cr-Commit-Position: refs/heads/master@{#310899}

--- tools/telemetry/telemetry/core/backends/android_app_backend.py
+++ tools/telemetry/telemetry/core/backends/android_app_backend.py
@@ -13,8 +13,8 @@ from telemetry.core.backends import app_backend
 
 class AndroidAppBackend(app_backend.AppBackend):
   def __init__(self, android_platform_backend, start_intent):
-    super(AndroidAppBackend, self).__init__(app_type=start_intent.package)
-    self._android_platform_backend = android_platform_backend
+    super(AndroidAppBackend, self).__init__(
+        start_intent.package, android_platform_backend)
     self._default_process_name = start_intent.package
     self._start_intent = start_intent
     self._is_running = False
@@ -22,7 +22,7 @@ class AndroidAppBackend(app_backend.AppBackend):
 
   @property
   def _adb(self):
-    return self._android_platform_backend.adb
+    return self.platform_backend.adb
 
   def Start(self):
     """Start an Android app and wait for it to finish launching.
@@ -38,7 +38,7 @@ class AndroidAppBackend(app_backend.AppBackend):
 
   def Close(self):
     self._is_running = False
-    self._android_platform_backend.KillApplication(self._start_intent.package)
+    self.platform_backend.KillApplication(self._start_intent.package)
 
   def IsAppRunning(self):
     return self._is_running
@@ -54,7 +54,7 @@ class AndroidAppBackend(app_backend.AppBackend):
       process_filter = lambda n: re.match('^' + self._default_process_name, n)
 
     processes = set()
-    ps_output = self._android_platform_backend.GetPsOutput(['pid', 'name'])
+    ps_output = self.platform_backend.GetPsOutput(['pid', 'name'])
     for pid, name in ps_output:
       if not process_filter(name):
         continue
--- tools/telemetry/telemetry/core/backends/app_backend.py
+++ tools/telemetry/telemetry/core/backends/app_backend.py
@@ -5,10 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 
 class AppBackend(object):
-  def __init__(self, app_type):
+  def __init__(self, app_type, platform_backend):
     super(AppBackend, self).__init__()
     self._app = None
     self._app_type = app_type
+    self._platform_backend = platform_backend
 
   def __del__(self):
     self.Close()
@@ -29,12 +30,8 @@ class AppBackend(object):
     raise NotImplementedError
 
   @property
-  def devtools_client(self):
-    """Returns the DevToolsClientBackend instance.
-
-    Implementation may return None if no DevTools server is expected.
-    """
-    raise NotImplementedError
+  def platform_backend(self):
+    return self._platform_backend
 
   def Start(self):
     raise NotImplementedError
