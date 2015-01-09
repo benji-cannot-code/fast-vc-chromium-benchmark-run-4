f9a9924feca168e080c01aeabf8cfbacfd4e5e46
mohan.reddy <mohan.reddy@samsung.com>
2015-01-09 12:03:57 -0800

    Removing the flags "auth-schemes", "auth-server-whitelist" and "auth-negotiate-delegate-whitelist"
    BUG=374951
    
    Review URL: https://codereview.chromium.org/836843003
    
    Cr-Commit-Position: refs/heads/master@{#310835}

--- chrome/browser/prefs/command_line_pref_store.cc
+++ chrome/browser/prefs/command_line_pref_store.cc
@@ -25,10 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 const CommandLinePrefStore::StringSwitchToPreferenceMapEntry
     CommandLinePrefStore::string_switch_map_[] = {
       { switches::kLang, prefs::kApplicationLocale },
-      { switches::kAuthSchemes, prefs::kAuthSchemes },
-      { switches::kAuthServerWhitelist, prefs::kAuthServerWhitelist },
-      { switches::kAuthNegotiateDelegateWhitelist,
-          prefs::kAuthNegotiateDelegateWhitelist },
       { switches::kGSSAPILibraryName, prefs::kGSSAPILibraryName },
       { data_reduction_proxy::switches::kDataReductionProxy,
           data_reduction_proxy::prefs::kDataReductionProxy },
