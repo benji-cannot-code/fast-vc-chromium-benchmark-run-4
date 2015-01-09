e798cfef4c590f0205e1bf0ee044e358f72e15fd
phoglund <phoglund@chromium.org>
2015-01-09 00:14:18 -0800

    Added WebRTC audio quality test using fake device; improved fake device
    This finally adds a end-to-end WebRTC audio quality test which includes
    getUserMedia and the capture path (except, obviously, the os-specific
    code which actually interacts with the capture device etc).
    
    This patch also makes the audio input device generic. It will now
    convert its input .wav file to suit the audio bus. I had to do this
    because the PESQ tests require files to be mono, 16-bit and so on.
    
    BUG=446859
    
    Review URL: https://codereview.chromium.org/831693004
    
    Cr-Commit-Position: refs/heads/master@{#310736}

--- media/audio/fake_audio_input_stream.h
+++ media/audio/fake_audio_input_stream.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/audio/audio_io.h"
 #include "media/audio/audio_parameters.h"
 #include "media/audio/sounds/wav_audio_handler.h"
+#include "media/base/audio_converter.h"
 
 namespace media {
 
@@ -26,7 +27,7 @@ class AudioManagerBase;
 
 // This class can either generate a beep sound or play audio from a file.
 class MEDIA_EXPORT FakeAudioInputStream
-    : public AudioInputStream {
+    : public AudioInputStream, AudioConverter::InputCallback {
  public:
   static AudioInputStream* MakeFakeStream(
       AudioManagerBase* manager, const AudioParameters& params);
@@ -84,11 +85,16 @@ class MEDIA_EXPORT FakeAudioInputStream
   scoped_ptr<media::AudioBus> audio_bus_;
   scoped_ptr<uint8[]> wav_file_data_;
   scoped_ptr<media::WavAudioHandler> wav_audio_handler_;
+  scoped_ptr<media::AudioConverter> file_audio_converter_;
   int wav_file_read_pos_;
 
   // Allows us to run tasks on the FakeAudioInputStream instance which are
   // bound by its lifetime.
   base::WeakPtrFactory<FakeAudioInputStream> weak_factory_;
+
+  // If running in file mode, this provides audio data from wav_audio_handler_.
+  double ProvideInput(AudioBus* audio_bus,
+                      base::TimeDelta buffer_delay) override;
 
   DISALLOW_COPY_AND_ASSIGN(FakeAudioInputStream);
 };
