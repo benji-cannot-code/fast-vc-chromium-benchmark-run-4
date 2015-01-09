e34bf34f66bbfb3bea5ea3f0d6dec66c2a9f4efc
smckay <smckay@chromium.org>
2015-01-09 11:39:50 -0800

    Mark entries as imported in response to transfer update events.
    Hook up to chrome.fileManagerPrivate.onFileTransfersUpdated to monitor sync events.
    When a sync is 'completed' mark the entry as IMPORTED in import history using the URL.
    The absence of a live Entity at this point necessitated some refactoring of ImportHistory:
      * Keep an index of destination URL to key...allowing us to look up copy records by destinationURL.
      * Update copy records to include source and destination URLs.
        - SourceURL was added as we need a way to identify which source entry has been updated.
        - This knowledge is required in file manager to determin if the grid view (of the removable device) should be refreshed.
      * Cleanup record reading and writing...drop the user of opt args. Pull use of the "record" Array up one level in the code.
    
    BUG=420680
    TEST=browser_test: FileManagerJsTest.*
    
    Review URL: https://codereview.chromium.org/840023005
    
    Cr-Commit-Position: refs/heads/master@{#310824}

--- ui/file_manager/file_manager/background/js/import_history_unittest.js
+++ ui/file_manager/file_manager/background/js/import_history_unittest.js
@@ -41,19 +41,24 @@ var testPromise;
 
 // Set up the test components.
 function setUp() {
+  setupChromeApis();
+  testFileSystem = new MockFileSystem('abc-123', 'filesystem:abc-123');
   testFileEntry = new MockFileEntry(
       testFileSystem,
       FILE_PATH, {
         size: FILE_SIZE,
         modificationTime: FILE_LAST_MODIFIED
       });
-  testFileSystem = new MockFileSystem('abc-123', 'filesystem:abc-123');
   testFileSystem.entries[FILE_PATH] = testFileEntry;
 
   storage = new TestRecordStorage();
 
   var history = new importer.PersistentImportHistory(storage);
   historyProvider = history.refresh();
+}
+
+function tearDown() {
+  testPromise = null;
 }
 
 function testHistoryWasCopiedFalseForUnknownEntry(callback) {
@@ -89,6 +94,16 @@ function testHistoryWasImportedTrueForKnownEntrySetAtRuntime(callback) {
   reportPromise(testPromise, callback);
 }
 
+function testHistoryWasCopiedTrueForKnownEntryLoadedFromStorage(callback) {
+  // TestRecordWriter is pre-configured with this entry.
+  testPromise = historyProvider.then(
+      function(history) {
+        history.wasCopied(testFileEntry, GOOGLE_DRIVE).then(assertTrue);
+      });
+
+  reportPromise(testPromise, callback);
+}
+
 function testCopyChangeFiresChangedEvent(callback) {
   testPromise = historyProvider.then(
       function(history) {
@@ -105,6 +120,27 @@ function testCopyChangeFiresChangedEvent(callback) {
                             recorder.getLastArguments()[0]['state']);
                       });
             });
+      });
+
+  reportPromise(testPromise, callback);
+}
+
+function testMarkImportedByUrl(callback) {
+  var destinationUrl = 'filesystem:chrome-extension://abc/photos/splosion.jpg';
+  testPromise = historyProvider.then(
+      function(history) {
+        return history.markCopied(testFileEntry, SPACE_CAMP, destinationUrl)
+            .then(
+                function() {
+                  return history.markImportedByUrl(destinationUrl)
+                      .then(
+                          function() {
+                            return history.wasImported(
+                                testFileEntry,
+                                SPACE_CAMP)
+                                .then(assertTrue);
+                          });
+                });
       });
 
   reportPromise(testPromise, callback);
@@ -273,13 +309,14 @@ function testHistoryLoaderIntegration(callback) {
 }
 
 /**
- * Shared error handler.
- * @param {function()} callback
- * @param {*} error
+ * Installs stub APIs.
  */
-function handleError(callback, error) {
-  console.error(error && (error.stack || error) || 'Unspecified test error.');
-  callback(/* error */ true);
+function setupChromeApis() {
+  chrome = {};
+  chrome.fileManagerPrivate = {};
+  chrome.fileManagerPrivate.onFileTransfersUpdated = {
+    addListener: function() {}
+  };
 }
 
 /**
