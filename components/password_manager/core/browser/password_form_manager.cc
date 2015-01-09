8d9b599d06f462f226557874c65cbc4b0ed1f52f
vabr <vabr@chromium.org>
2015-01-09 14:29:29 -0800

    Mark const methods of PasswordFormManager as const.
    This is another random clean-up which I noticed when doing other work.
    It's Friday night, let's fix it! :)
    
    R=mkwst@chromium.org
    BUG=447558
    
    Review URL: https://codereview.chromium.org/843133002
    
    Cr-Commit-Position: refs/heads/master@{#310870}

--- components/password_manager/core/browser/password_form_manager.cc
+++ components/password_manager/core/browser/password_form_manager.cc
@@ -100,7 +100,7 @@ PasswordFormManager::~PasswordFormManager() {
     LogPasswordGenerationSubmissionEvent(PASSWORD_NOT_SUBMITTED);
 }
 
-int PasswordFormManager::GetActionsTaken() {
+int PasswordFormManager::GetActionsTaken() const {
   return user_action_ + kUserActionMax * (manager_action_ +
          kManagerActionMax * submit_result_);
 }
@@ -155,7 +155,7 @@ PasswordFormManager::MatchResultMask PasswordFormManager::DoesManage(
   return result;
 }
 
-bool PasswordFormManager::IsBlacklisted() {
+bool PasswordFormManager::IsBlacklisted() const {
   DCHECK_EQ(state_, POST_MATCHING_PHASE);
   if (preferred_match_ && preferred_match_->blacklisted_by_user)
     return true;
@@ -202,12 +202,12 @@ void PasswordFormManager::PermanentlyBlacklist() {
   SaveAsNewLogin(false);
 }
 
-bool PasswordFormManager::IsNewLogin() {
+bool PasswordFormManager::IsNewLogin() const {
   DCHECK_EQ(state_, POST_MATCHING_PHASE);
   return is_new_login_;
 }
 
-bool PasswordFormManager::IsPendingCredentialsPublicSuffixMatch() {
+bool PasswordFormManager::IsPendingCredentialsPublicSuffixMatch() const {
   return pending_credentials_.IsPublicSuffixMatch();
 }
 
@@ -215,14 +215,14 @@ void PasswordFormManager::SetHasGeneratedPassword() {
   has_generated_password_ = true;
 }
 
-bool PasswordFormManager::HasGeneratedPassword() {
+bool PasswordFormManager::HasGeneratedPassword() const {
   // This check is permissive, as the user may have generated a password and
   // then edited it in the form itself. However, even in this case the user
   // has already given consent, so we treat these cases the same.
   return has_generated_password_;
 }
 
-bool PasswordFormManager::HasValidPasswordForm() {
+bool PasswordFormManager::HasValidPasswordForm() const {
   DCHECK_EQ(state_, POST_MATCHING_PHASE);
   // Non-HTML password forms (primarily HTTP and FTP autentication)
   // do not contain username_element and password_element values.
@@ -399,7 +399,7 @@ void PasswordFormManager::FetchMatchingLoginsFromPasswordStore(
   password_store->GetLogins(observed_form_, prompt_policy, this);
 }
 
-bool PasswordFormManager::HasCompletedMatching() {
+bool PasswordFormManager::HasCompletedMatching() const {
   return state_ == POST_MATCHING_PHASE;
 }
 
