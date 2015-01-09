2f77d5f02f53e7553fe4cef4b493a53610fe67b8
tengs <tengs@chromium.org>
2015-01-08 18:49:23 -0800

    Add easyUnlockPrivate.getConnection API to allow querying for a Bluetooth connection's RSSI and TX power.
    BUG=403419
    
    Review URL: https://codereview.chromium.org/813303003
    
    Cr-Commit-Position: refs/heads/master@{#310687}

--- chrome/browser/extensions/api/easy_unlock_private/easy_unlock_private_api.h
+++ chrome/browser/extensions/api/easy_unlock_private/easy_unlock_private_api.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/memory/scoped_ptr.h"
+#include "device/bluetooth/bluetooth_device.h"
+#include "extensions/browser/api/bluetooth/bluetooth_extension_function.h"
 #include "extensions/browser/api/bluetooth_socket/bluetooth_socket_api.h"
 #include "extensions/browser/browser_context_keyed_api_factory.h"
 #include "extensions/browser/extension_function.h"
@@ -346,6 +348,25 @@ class EasyUnlockPrivateGetUserImageFunction : public SyncExtensionFunction {
   bool RunSync() override;
 
   DISALLOW_COPY_AND_ASSIGN(EasyUnlockPrivateGetUserImageFunction);
+};
+
+class EasyUnlockPrivateGetConnectionInfoFunction
+    : public core_api::BluetoothExtensionFunction {
+ public:
+  DECLARE_EXTENSION_FUNCTION("easyUnlockPrivate.getConnectionInfo",
+                             EASYUNLOCKPRIVATE_GETCONNECTIONINFO)
+  EasyUnlockPrivateGetConnectionInfoFunction();
+
+ private:
+  ~EasyUnlockPrivateGetConnectionInfoFunction() override;
+
+  // BluetoothExtensionFunction:
+  bool DoWork(scoped_refptr<device::BluetoothAdapter> adapter) override;
+
+  void OnConnectionInfo(
+      const device::BluetoothDevice::ConnectionInfo& connection_info);
+
+  DISALLOW_COPY_AND_ASSIGN(EasyUnlockPrivateGetConnectionInfoFunction);
 };
 
 }  // namespace api
