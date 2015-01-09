132bf8d406ec0885859f119496c51aaab2b0d0b7
rob.buis@samsung.com <rob.buis@samsung.com>
2015-01-09 15:45:40 +0000

    Make media query feature values more strict
    Before this patch MediaQueryData::addParserValue would
    accept any kind of css token, including url() and '#' types.
    With this patch addParserValue becomes more restrictive and
    only allows number, percentage, dimension, delimiter and
    ident token types. To detect when some other token type
    is used, make it return a bool and reflect this by renaming
    to tryAddParserValue.
    
    Behavior matches Firefox.
    
    BUG=446393
    
    Review URL: https://codereview.chromium.org/836373002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188154 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/parser/MediaQueryParser.cpp
+++ third_party/WebKit/Source/core/css/parser/MediaQueryParser.cpp
@@ -146,8 +146,10 @@ void MediaQueryParser::readFeatureValue(CSSParserTokenType type, const CSSParser
     if (type == DimensionToken && token.unitType() == CSSPrimitiveValue::CSS_UNKNOWN) {
         m_state = SkipUntilComma;
     } else {
-        m_mediaQueryData.addParserValue(type, token);
-        m_state = ReadFeatureEnd;
+        if (m_mediaQueryData.tryAddParserValue(type, token))
+            m_state = ReadFeatureEnd;
+        else
+            m_state = SkipUntilBlockEnd;
     }
 }
 
@@ -159,7 +161,7 @@ void MediaQueryParser::readFeatureEnd(CSSParserTokenType type, const CSSParserTo
         else
             m_state = SkipUntilComma;
     } else if (type == DelimiterToken && token.delimiter() == '/') {
-        m_mediaQueryData.addParserValue(type, token);
+        m_mediaQueryData.tryAddParserValue(type, token);
         m_state = ReadFeatureValue;
     } else {
         m_state = SkipUntilBlockEnd;
@@ -254,7 +256,7 @@ bool MediaQueryData::addExpression()
     return isValid;
 }
 
-void MediaQueryData::addParserValue(CSSParserTokenType type, const CSSParserToken& token)
+bool MediaQueryData::tryAddParserValue(CSSParserTokenType type, const CSSParserToken& token)
 {
     CSSParserValue value;
     if (type == NumberToken || type == PercentageToken || type == DimensionToken) {
@@ -265,15 +267,18 @@ void MediaQueryData::addParserValue(CSSParserTokenType type, const CSSParserToke
         value.iValue = token.delimiter();
         value.id = CSSValueInvalid;
         value.isInt = false;
-    } else {
+    } else if (type == IdentToken) {
         CSSParserString tokenValue;
         tokenValue.init(token.value());
         value.unit = CSSPrimitiveValue::CSS_IDENT;
         value.string = tokenValue;
         value.id = cssValueKeywordID(tokenValue);
         value.isInt = false;
+    } else {
+        return false;
     }
     m_valueList.addValue(value);
+    return true;
 }
 
 void MediaQueryData::setMediaType(const String& mediaType)
