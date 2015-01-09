201fc9fd5c20f91ae0c3d03dd4b42314eb08fd1b
rtenneti <rtenneti@chromium.org>
2015-01-09 12:06:22 -0800

    QUIC - Deleted unused FLAGS_track_retransmission_history flag.
    R=rch@chromium.org
    
    Review URL: https://codereview.chromium.org/843673006
    
    Cr-Commit-Position: refs/heads/master@{#310836}

--- net/quic/quic_flags.cc
+++ net/quic/quic_flags.cc
@@ -5,14 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/quic/quic_flags.h"
 
-// TODO(rtenneti): Remove this.
-// Do not flip this flag until the flakiness of the
-// net/tools/quic/end_to_end_test is fixed.
-// If true, then QUIC connections will track the retransmission history of a
-// packet so that an ack of a previous transmission will ack the data of all
-// other transmissions.
-bool FLAGS_track_retransmission_history = false;
-
 bool FLAGS_quic_allow_oversized_packets_for_test = false;
 
 // When true, the use time based loss detection instead of nack.
