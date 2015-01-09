6aab5295b6f1f1b06ba699a13dabcc340d1d2f44
kalman <kalman@chromium.org>
2015-01-09 15:57:35 -0800

    Add preferred size support and events to guestview, and use them in extensionoptions to fix resize bug.
    BUG=442033
    R=fsamuel@chromium.org,rdevlin.cronin@chromium.org
    
    Review URL: https://codereview.chromium.org/842913003
    
    Cr-Commit-Position: refs/heads/master@{#310898}

--- chrome/renderer/resources/extensions/extension_options_events.js
+++ chrome/renderer/resources/extensions/extension_options_events.js
@@ -25,6 +25,10 @@ var EXTENSION_OPTIONS_EVENTS = {
       handler.handleSizeChangedEvent(event, extensionOptionsEvent);
     },
     fields:['newWidth', 'newHeight', 'oldWidth', 'oldHeight']
+  },
+  'preferredsizechanged': {
+    evt: CreateEvent('extensionOptionsInternal.onPreferredSizeChanged'),
+    fields:['width', 'height']
   }
 }
 
