bbedec4b0fc9a9cb97362eda736131a86a6e2990
dtrainor <dtrainor@chromium.org>
2015-01-08 16:54:18 -0800

    Simplify Event Filter Logic
    Remove some unused event filter logic.
    
    BUG=428821
    
    Review URL: https://codereview.chromium.org/817523002
    
    Cr-Commit-Position: refs/heads/master@{#310643}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/eventfilter/AreaGestureEventFilter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/eventfilter/AreaGestureEventFilter.java
@@ -14,7 +14,7 @@ import android.view.MotionEvent;
  * screen.
  */
 public class AreaGestureEventFilter extends GestureEventFilter {
-    private RectF mTriggerRect;
+    private final RectF mTriggerRect = new RectF();
 
     /**
      * Creates a {@link AreaGestureEventFilter}.
@@ -61,7 +61,11 @@ public class AreaGestureEventFilter extends GestureEventFilter {
      * @param rect The area that events should be stolen from in dp.
      */
     public void setEventArea(RectF rect) {
-        mTriggerRect = rect;
+        if (rect == null) {
+            mTriggerRect.setEmpty();
+        } else {
+            mTriggerRect.set(rect);
+        }
     }
 
     @Override
