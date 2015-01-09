a97065dba172cad6dc9a7700a018f220f7a07314
prashhir@cisco.com <prashhir@cisco.com>
2015-01-09 03:33:34 +0000

    Input type url with line break and leading-trailing whitespace should be stripped.
    According to spec https://html.spec.whatwg.org/multipage/forms.html#url-state-%28type=url%29,
    we have to strip the line break and leading-trailing whitespaces from the url value, so added santization
    algorithm to strip the line break and leading-trailing whitespaces.
    Firefox works according to spec and strips the linebreak and whitespaces from value.
    
    BUG=446108
    
    Review URL: https://codereview.chromium.org/836113002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188106 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/forms/URLInputType.cpp
+++ third_party/WebKit/Source/core/html/forms/URLInputType.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/InputTypeNames.h"
 #include "core/html/HTMLInputElement.h"
+#include "core/html/parser/HTMLParserIdioms.h"
 #include "platform/text/PlatformLocale.h"
 #include "wtf/PassOwnPtr.h"
 
@@ -52,6 +53,12 @@ void URLInputType::countUsage()
 const AtomicString& URLInputType::formControlType() const
 {
     return InputTypeNames::url;
+}
+
+String URLInputType::sanitizeValue(const String& proposedValue) const
+{
+    String value = proposedValue.removeCharacters(isHTMLLineBreak);
+    return stripLeadingAndTrailingHTMLSpaces(value);
 }
 
 bool URLInputType::typeMismatchFor(const String& value) const
