2bbd45a2afa867427886aef14c252d66c7da2541
aiolos <aiolos@chromium.org>
2015-01-08 22:39:12 -0800

    Move archive related unittest data into it's own folder.
    BUG=447462
    
    Review URL: https://codereview.chromium.org/838383002
    
    Cr-Commit-Position: refs/heads/master@{#310718}

--- tools/telemetry/unittest_data/archive_files/test_simple_two_page_set.py
+++ tools/telemetry/unittest_data/archive_files/test_simple_two_page_set.py
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 from telemetry.page.page_set import PageSet
 
 
-class TestSimpleOnePageSet(PageSet):
+class TestSimpleTwoPageSet(PageSet):
   def __init__(self):
-    super(TestSimpleOnePageSet, self).__init__(
-      archive_data_file='data/test.json',
+    super(TestSimpleTwoPageSet, self).__init__(
+      archive_data_file='data/archive_files/test.json',
       credentials_path='data/credential',
       user_agent_type='desktop')
