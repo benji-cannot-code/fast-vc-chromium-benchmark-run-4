efb34ae7cbacd04a82677dc758ce2e6168163e9a
reillyg <reillyg@chromium.org>
2015-01-09 16:13:48 -0800

    UsbDeviceHandleImpl must hold a scoped_refptr to UsbDeviceImpl.
    When a USB device is disconnected the last reference to the UsbDevice
    object representing it may be freed. Outstanding device handles should
    retain a reference to the UsbDevice object until they themselves are
    closed (which is done in UsbDeviceImpl::OnDisconnect). This call
    releases the circular reference between UsbDeviceImpl and its open
    UsbDeviceHandleImpl children.
    
    Otherwise the UsbDeviceImpl will outlive the UsbDeviceHandleImpl and
    UsbDeviceHandleImpl::GetDevice will attempt to call AddRef on an invalid
    pointer.
    
    BUG=447761
    
    Review URL: https://codereview.chromium.org/835313003
    
    Cr-Commit-Position: refs/heads/master@{#310907}

--- device/usb/usb_device_handle_impl.cc
+++ device/usb/usb_device_handle_impl.cc
@@ -478,7 +478,7 @@ void LIBUSB_CALL UsbDeviceHandleImpl::Transfer::PlatformCallback(
 }
 
 UsbDeviceHandleImpl::UsbDeviceHandleImpl(scoped_refptr<UsbContext> context,
-                                         UsbDeviceImpl* device,
+                                         scoped_refptr<UsbDeviceImpl> device,
                                          PlatformUsbDeviceHandle handle)
     : device_(device),
       handle_(handle),
@@ -496,7 +496,7 @@ UsbDeviceHandleImpl::~UsbDeviceHandleImpl() {
 }
 
 scoped_refptr<UsbDevice> UsbDeviceHandleImpl::GetDevice() const {
-  return static_cast<UsbDevice*>(device_);
+  return device_;
 }
 
 void UsbDeviceHandleImpl::Close() {
