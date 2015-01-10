1db819cb7a1c103ec94826a87fb650ebad5a96e7
gunsch <gunsch@chromium.org>
2015-01-09 18:30:36 -0800

    Chromecast buildfix: include missing headers.
    R=lcwu@chromium.org
    TBR=lcwu@chromium.org
    BUG=None
    
    Review URL: https://codereview.chromium.org/844683005
    
    Cr-Commit-Position: refs/heads/master@{#310942}

--- chromecast/browser/cast_content_browser_client.cc
+++ chromecast/browser/cast_content_browser_client.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromecast/browser/cast_content_browser_client.h"
 
+#include <string>
+
+#include "base/base_switches.h"
 #include "base/command_line.h"
 #include "base/files/scoped_file.h"
 #include "base/i18n/rtl.h"
