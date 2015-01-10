178ad55c3516a623e816d036d8a5be1f9fabb9b4
sky <sky@chromium.org>
2015-01-09 16:30:28 -0800

    Don't show status bar updates from originating renderer when fullscreen
    In fullscreen mode WebContents has two renderviews. One for the
    fullscreen display, and one for the originating page. It's entirely
    possible that the renderer from the originating page tries to update
    the status bar url. This seems to mostly occur because we can delay mouse
    events so that when transitioning to fullscreen we can end up sending
    a mouse event to the originating renderer after we're fullscreen,
    which can result in the originating page trying to update the target
    url of the status bar.
    
    When fullscreen we should only care about updates from the fullscreen
    renderer.
    
    R=creis@chromium.org
    BUG=380005
    TEST=see bug
    
    Review URL: https://codereview.chromium.org/811523005
    
    Cr-Commit-Position: refs/heads/master@{#310915}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -2594,6 +2594,7 @@ void WebContentsImpl::DidNavigateMainFramePreCommit(
 }
 
 void WebContentsImpl::DidNavigateMainFramePostCommit(
+    RenderFrameHostImpl* render_frame_host,
     const LoadCommittedDetails& details,
     const FrameHostMsg_DidCommitProvisionalLoad_Params& params) {
   if (details.is_navigation_to_different_page()) {
@@ -2603,7 +2604,7 @@ void WebContentsImpl::DidNavigateMainFramePostCommit(
     // clicking on a link); see bugs 1184641 and 980803. We don't want to
     // clear the bubble when a user navigates to a named anchor in the same
     // page.
-    UpdateTargetURL(GURL());
+    UpdateTargetURL(render_frame_host->GetRenderViewHost(), GURL());
   }
 
   if (!details.is_in_page) {
@@ -3671,7 +3672,15 @@ void WebContentsImpl::UpdateState(RenderViewHost* rvh,
   controller_.NotifyEntryChanged(entry, entry_index);
 }
 
-void WebContentsImpl::UpdateTargetURL(const GURL& url) {
+void WebContentsImpl::UpdateTargetURL(RenderViewHost* render_view_host,
+                                      const GURL& url) {
+  if (fullscreen_widget_routing_id_ != MSG_ROUTING_NONE) {
+    // If we're fullscreen only update the url if it's from the fullscreen
+    // renderer.
+    RenderWidgetHostView* fs = GetFullscreenRenderWidgetHostView();
+    if (fs && fs->GetRenderWidgetHost() != render_view_host)
+      return;
+  }
   if (delegate_)
     delegate_->UpdateTargetURL(this, url);
 }
