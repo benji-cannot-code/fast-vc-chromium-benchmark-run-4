4dea62a564b49ee24425b1ea3a5395392c4d685c
mukai <mukai@chromium.org>
2015-01-12 13:08:01 -0800

    Re-layout the text labels when DSF has been changed.
    When DSF is changed, it will change the used font hinting data
    as crrev.com/308462. This means views::Label's preferred size
    can be different, and if the new layout is smaller than
    the assigned bounds, the text could be elided.
    
    BUG=441439
    R=sky@chromium.org, oshima@chromium.org
    TEST=on kip, change the internal display's DSF, and make sure
      the time info in the system tray isn't elided
    
    Review URL: https://codereview.chromium.org/821133002
    
    Cr-Commit-Position: refs/heads/master@{#311108}

--- ui/views/widget/root_view.cc
+++ ui/views/widget/root_view.cc
@@ -228,6 +228,10 @@ void RootView::LocaleChanged() {
   View::PropagateLocaleChanged();
 }
 
+void RootView::DeviceScaleFactorChanged(float device_scale_factor) {
+  View::PropagateDeviceScaleFactorChanged(device_scale_factor);
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // RootView, FocusTraversable implementation:
 
