f6c97d59898917686ace793f368d382181f80eb4
samli@chromium.org <samli@chromium.org>
2015-01-12 04:34:52 +0000

    Animation: Allow animations to be retriggered if timeline time changes
    Currently, timeline time is assumed to monotonically increase. This
    change fixes this assumption by allowing finished animations to return
    to playing if the current time is explicitly set on the animation
    timeline.
    
    BUG=434576
    
    Review URL: https://codereview.chromium.org/840213002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188200 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/AnimationPlayer.cpp
+++ third_party/WebKit/Source/core/animation/AnimationPlayer.cpp
@@ -121,6 +121,9 @@ void AnimationPlayer::setCurrentTime(double newCurrentTime)
 
     m_currentTimePending = false;
     setCurrentTimeInternal(newCurrentTime / 1000, TimingUpdateOnDemand);
+
+    if (calculatePlayState() == Finished)
+        m_startTime = calculateStartTime(newCurrentTime);
 }
 
 void AnimationPlayer::setCurrentTimeInternal(double newCurrentTime, TimingUpdateReason reason)
@@ -156,6 +159,11 @@ void AnimationPlayer::setCurrentTimeInternal(double newCurrentTime, TimingUpdate
 void AnimationPlayer::updateCurrentTimingState(TimingUpdateReason reason)
 {
     if (m_held) {
+        if (!isNull(m_startTime) && m_timeline && !limited(calculateCurrentTime()) && playStateInternal() == Finished) {
+            m_held = false;
+            setCurrentTimeInternal(calculateCurrentTime(), reason);
+            return;
+        }
         setCurrentTimeInternal(m_holdTime, reason);
         return;
     }
@@ -330,7 +338,6 @@ double AnimationPlayer::calculateStartTime(double currentTime) const
 
 double AnimationPlayer::calculateCurrentTime() const
 {
-    ASSERT(!m_held);
     if (isNull(m_startTime) || !m_timeline)
         return 0;
     return (m_timeline->effectiveTime() - m_startTime) * m_playbackRate;
