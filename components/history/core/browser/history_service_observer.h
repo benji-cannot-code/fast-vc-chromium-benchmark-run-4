294f141fd62afc35c87ec4b4a32f62c007e0263a
abhishek.a21 <abhishek.a21@samsung.com>
2015-01-12 01:24:26 -0800

    Eliminate sending NOTIFICATION_HISTORY_KEYWORD_SEARCH_TERM_UPDATED
    Added HistoryKeywordSearchTermUpdated inside HistoryBackendObserver
    and HistoryServiceObserver.
    
    Will be removed notification and its detail from chrome_notification_types.
    
    BUG=373321
    
    Review URL: https://codereview.chromium.org/808123003
    
    Cr-Commit-Position: refs/heads/master@{#311027}

--- components/history/core/browser/history_service_observer.h
+++ components/history/core/browser/history_service_observer.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "components/history/core/browser/history_types.h"
+#include "components/history/core/browser/keyword_id.h"
 
 class HistoryService;
 
@@ -48,6 +49,15 @@ class HistoryServiceObserver {
 
   // Is called to notify when |history_service| is being deleted.
   virtual void HistoryServiceBeingDeleted(HistoryService* history_service) {}
+
+  // Sent when a keyword search term is updated.
+  //
+  // |row| contains the URL information for search |term|.
+  // |keyword_id| associated with a URL and search term.
+  virtual void OnKeywordSearchTermUpdated(HistoryService* history_service,
+                                          const URLRow& row,
+                                          KeywordID keyword_id,
+                                          const base::string16& term) {}
 
  private:
   DISALLOW_COPY_AND_ASSIGN(HistoryServiceObserver);
