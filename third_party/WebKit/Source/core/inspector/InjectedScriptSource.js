b0629924d72598de0826846d399c9fa9f9813a5a
aandrey@chromium.org <aandrey@chromium.org>
2015-01-12 15:49:57 +0000

    DevTools: Fix console not showing array items inherited from prototype.
    BUG=320632
    R=pfeldman
    
    Review URL: https://codereview.chromium.org/844563003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188228 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InjectedScriptSource.js
+++ third_party/WebKit/Source/core/inspector/InjectedScriptSource.js
@@ -150,12 +150,14 @@ function nullifyObjectProto(obj)
 }
 
 /**
- * @param {*} obj
+ * @param {number|string} obj
  * @return {boolean}
  */
 function isUInt32(obj)
 {
-    return typeof obj === "number" && obj >>> 0 === obj && (obj > 0 || 1 / obj > 0);
+    if (typeof obj === "number")
+        return obj >>> 0 === obj && (obj > 0 || 1 / obj > 0);
+    return "" + (obj >>> 0) === obj;
 }
 
 /**
@@ -168,8 +170,10 @@ function isArrayLike(obj)
     if (typeof obj !== "object")
         return false;
     try {
-        if (typeof obj.splice === "function")
-            return isUInt32(obj.length);
+        if (typeof obj.splice === "function") {
+            var len = obj.length;
+            return typeof len === "number" && isUInt32(len);
+        }
     } catch (e) {
     }
     return false;
@@ -1363,13 +1367,13 @@ InjectedScript.RemoteObject.prototype = {
                 preview.lossless = false;
                 continue;
             }
-            if (!descriptor.enumerable && !descriptor.isOwn)
-                continue;
 
             var name = descriptor.name;
             if (name === "__proto__")
                 continue;
             if (this.subtype === "array" && name === "length")
+                continue;
+            if (!descriptor.enumerable && !descriptor.isOwn && !(this.subtype === "array" && isUInt32(name)))
                 continue;
 
             if (!("value" in descriptor)) {
