77f4839dd293615dfaf0d084449902bf35f9256b
yusufo <yusufo@chromium.org>
2015-01-12 14:03:16 -0800

    Move broadcasting the load progress to observers to its own method
    This is for having a shared upstream and downstream notify implementation
    
    BUG=344589
    
    Review URL: https://codereview.chromium.org/847863002
    
    Cr-Commit-Position: refs/heads/master@{#311118}

--- chrome/android/java/src/org/chromium/chrome/browser/Tab.java
+++ chrome/android/java/src/org/chromium/chrome/browser/Tab.java
@@ -330,9 +330,7 @@ public class Tab implements ViewGroup.OnHierarchyChangeListener,
             extends ChromeWebContentsDelegateAndroid {
         @Override
         public void onLoadProgressChanged(int progress) {
-            for (TabObserver observer : mObservers) {
-                observer.onLoadProgressChanged(Tab.this, progress);
-            }
+            notifyLoadProgress(progress);
         }
 
         @Override
@@ -1552,6 +1550,14 @@ public class Tab implements ViewGroup.OnHierarchyChangeListener,
         while (observers.hasNext()) {
             observers.next().onTitleUpdated(this);
         }
+    }
+
+    /**
+     * Notify the observers that the load progress has changed.
+     * @param progress The current percentage of progress.
+     */
+    protected void notifyLoadProgress(int progress) {
+        for (TabObserver observer : mObservers) observer.onLoadProgressChanged(Tab.this, progress);
     }
 
     protected void notifyFaviconChanged() {
