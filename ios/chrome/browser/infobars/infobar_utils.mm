fd0fb8a9c8d1ba55aaa25aef24d6f3df342fe509
sdefresne <sdefresne@chromium.org>
2015-01-12 07:38:03 -0800

    Introduce iOS implementation of InfoBarManager
    InfoBarManagerImpl implements infobars::InfoBarManager and is bound to
    a web::WebState.
    
    Add missing dependency on ui/base and ui/gfx to ios_chrome_browser.
    
    BUG=438238
    
    Review URL: https://codereview.chromium.org/845773003
    
    Cr-Commit-Position: refs/heads/master@{#311054}

---
+++ ios/chrome/browser/infobars/infobar_utils.mm
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/chrome/browser/infobars/infobar_utils.h"
+
+#include "base/mac/scoped_nsobject.h"
+#include "base/memory/scoped_ptr.h"
+#include "components/infobars/core/confirm_infobar_delegate.h"
+#include "ios/chrome/browser/infobars/confirm_infobar_controller.h"
+#include "ios/chrome/browser/infobars/infobar.h"
+
+scoped_ptr<infobars::InfoBar> CreateConfirmInfoBar(
+    scoped_ptr<ConfirmInfoBarDelegate> delegate) {
+  scoped_ptr<InfoBarIOS> infobar(new InfoBarIOS(delegate.Pass()));
+  base::scoped_nsobject<ConfirmInfoBarController> controller(
+      [[ConfirmInfoBarController alloc] initWithDelegate:infobar.get()]);
+  infobar->SetController(controller);
+  return infobar.Pass();
+}
