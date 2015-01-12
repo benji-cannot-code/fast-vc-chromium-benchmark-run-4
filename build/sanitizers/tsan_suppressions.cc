b2bf10cede14a7ffa67b7a3f3f19eb66b3727a05
jyasskin <jyasskin@chromium.org>
2015-01-12 12:31:07 -0800

    Suppress a race between using a ChildGpuMemoryBufferManager and destroying it.
    BUG=430533
    TBR=reveman@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/828893003
    
    Cr-Commit-Position: refs/heads/master@{#311103}

--- build/sanitizers/tsan_suppressions.cc
+++ build/sanitizers/tsan_suppressions.cc
@@ -313,6 +313,9 @@ char kTSanDefaultSuppressions[] =
 // https://crbug.com/447461
 "race:net::SSLConfig::SSLConfig\n"
 
+// https://crbug.com/430533
+"race:content::ChildGpuMemoryBufferManager::~ChildGpuMemoryBufferManager\n"
+
 // End of suppressions.
 ;  // Please keep this semicolon.
 
