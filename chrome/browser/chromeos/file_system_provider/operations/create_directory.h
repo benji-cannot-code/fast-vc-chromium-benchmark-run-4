3b159603700d443a0c3b9280707f727ec455654b
hashimoto <hashimoto@chromium.org>
2015-01-12 23:12:44 -0800

    file_system_provider: Remove unused private fields
    BUG=447445
    
    Review URL: https://codereview.chromium.org/848743006
    
    Cr-Commit-Position: refs/heads/master@{#311228}

--- chrome/browser/chromeos/file_system_provider/operations/create_directory.h
+++ chrome/browser/chromeos/file_system_provider/operations/create_directory.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "chrome/browser/chromeos/file_system_provider/operations/operation.h"
 #include "chrome/browser/chromeos/file_system_provider/provided_file_system_info.h"
-#include "chrome/browser/chromeos/file_system_provider/provided_file_system_interface.h"
 #include "chrome/browser/chromeos/file_system_provider/request_value.h"
 #include "storage/browser/fileapi/async_file_util.h"
 
@@ -49,7 +48,6 @@ class CreateDirectory : public Operation {
 
  private:
   base::FilePath directory_path_;
-  ProvidedFileSystemInterface::OpenFileMode mode_;
   bool recursive_;
   const storage::AsyncFileUtil::StatusCallback callback_;
 
--- chrome/browser/chromeos/file_system_provider/operations/delete_entry.h
+++ chrome/browser/chromeos/file_system_provider/operations/delete_entry.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "chrome/browser/chromeos/file_system_provider/operations/operation.h"
 #include "chrome/browser/chromeos/file_system_provider/provided_file_system_info.h"
-#include "chrome/browser/chromeos/file_system_provider/provided_file_system_interface.h"
 #include "chrome/browser/chromeos/file_system_provider/request_value.h"
 #include "storage/browser/fileapi/async_file_util.h"
 
@@ -48,7 +47,6 @@ class DeleteEntry : public Operation {
 
  private:
   base::FilePath entry_path_;
-  ProvidedFileSystemInterface::OpenFileMode mode_;
   bool recursive_;
   const storage::AsyncFileUtil::StatusCallback callback_;
 
