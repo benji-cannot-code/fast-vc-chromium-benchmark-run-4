8ef321c401ffea9d1ccffd0ad1e2b4f72dc3ff08
wjmaclean <wjmaclean@chromium.org>
2015-01-13 08:02:23 -0800

    Remove unused include in chrome_page_zoom.cc.
    The include chrome/browser/profiles/profile.h is not actually used by
    chrome_page_zoom, so removing it.
    
    BUG=none
    TBR=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/809273004
    
    Cr-Commit-Position: refs/heads/master@{#311274}

--- chrome/browser/chrome_page_zoom.cc
+++ chrome/browser/chrome_page_zoom.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/prefs/pref_service.h"
 #include "chrome/browser/chrome_page_zoom_constants.h"
-#include "chrome/browser/profiles/profile.h"
 #include "components/ui/zoom/zoom_controller.h"
 #include "content/public/browser/host_zoom_map.h"
 #include "content/public/browser/render_view_host.h"
