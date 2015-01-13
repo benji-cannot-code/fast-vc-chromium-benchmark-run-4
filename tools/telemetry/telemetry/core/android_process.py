2736e7cf88e5cf978317c506051d35b0158dae32
chrishenry <chrishenry@google.com>
2015-01-13 12:00:45 -0800

    Remove Telemetry's Chrome tracing dependency on BrowserBackend. TracingController now directly communicates with DevToolsClientBackend instead.
    This allows us to use TracingController on Android app as well.
    
    BUG=443897
    
    Review URL: https://codereview.chromium.org/842303002
    
    Cr-Commit-Position: refs/heads/master@{#311315}

--- tools/telemetry/telemetry/core/android_process.py
+++ tools/telemetry/telemetry/core/android_process.py
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 from telemetry.core import web_contents
 from telemetry.core.backends import adb_commands
 from telemetry.core.backends.chrome_inspector import devtools_client_backend
-from telemetry.core.backends.chrome_inspector import inspector_backend
 
 class WebViewNotFoundException(Exception):
   pass
@@ -30,19 +29,16 @@ class AndroidProcess(object):
     return self._name
 
   @property
-  def _webview_port(self):
+  def _remote_devtools_port(self):
     return 'localabstract:webview_devtools_remote_%s' % str(self.pid)
 
   def _UpdateDevToolsClient(self):
     if self._devtools_client is None:
-      self._app_backend._android_platform_backend.ForwardHostToDevice(
-          self._local_port, self._webview_port)
-      candidate_devtools_client = devtools_client_backend.DevToolsClientBackend(
-          self._local_port, self._app_backend)
-      # TODO(ariblue): Don't create a DevToolsClientBackend before confirming
-      # that a devtools agent exists. This involves a minor refactor of IsAlive.
-      if candidate_devtools_client.IsAlive():
-        self._devtools_client = candidate_devtools_client
+      self._app_backend.platform_backend.ForwardHostToDevice(
+          self._local_port, self._remote_devtools_port)
+      if devtools_client_backend.IsDevToolsAgentAvailable(self._local_port):
+        self._devtools_client = devtools_client_backend.DevToolsClientBackend(
+            self._local_port, self._remote_devtools_port, self._app_backend)
 
   def GetWebViews(self):
     webviews = []
