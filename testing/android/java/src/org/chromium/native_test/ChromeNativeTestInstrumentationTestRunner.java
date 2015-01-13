1dbe950ea3c095882714e39b89d870e6367aa54e
jbudorick <jbudorick@chromium.org>
2015-01-13 15:49:28 -0800

    [Android] Add file-based gtest filtering for gtests on AMP.
    BUG=428729
    
    Review URL: https://codereview.chromium.org/810193007
    
    Cr-Commit-Position: refs/heads/master@{#311363}

--- testing/android/java/src/org/chromium/native_test/ChromeNativeTestInstrumentationTestRunner.java
+++ testing/android/java/src/org/chromium/native_test/ChromeNativeTestInstrumentationTestRunner.java
@@ -28,9 +28,13 @@ import java.util.regex.Pattern;
  *  An Instrumentation that runs tests based on ChromeNativeTestActivity.
  */
 public class ChromeNativeTestInstrumentationTestRunner extends Instrumentation {
+    // TODO(jbudorick): Remove this extra when b/18981674 is fixed.
+    public static final String EXTRA_ONLY_OUTPUT_FAILURES =
+            "org.chromium.native_test.ChromeNativeTestInstrumentationTestRunner."
+                    + "OnlyOutputFailures";
 
     private static final String TAG = "ChromeNativeTestInstrumentationTestRunner";
-    private static final Pattern RE_TEST_OUTPUT = Pattern.compile("\\[ *([^ ]*) *\\] ?([^ ]*) .*");
+    private static final Pattern RE_TEST_OUTPUT = Pattern.compile("\\[ *([^ ]*) *\\] ?([^ ]+) .*");
 
     private static interface ResultsBundleGenerator {
         public Bundle generate(Map<String, TestResult> rawResults);
@@ -40,6 +44,7 @@ public class ChromeNativeTestInstrumentationTestRunner extends Instrumentation {
     private String mCommandLineFlags;
     private Bundle mLogBundle;
     private ResultsBundleGenerator mBundleGenerator;
+    private boolean mOnlyOutputFailures;
 
     @Override
     public void onCreate(Bundle arguments) {
@@ -47,6 +52,7 @@ public class ChromeNativeTestInstrumentationTestRunner extends Instrumentation {
         mCommandLineFlags = arguments.getString(ChromeNativeTestActivity.EXTRA_COMMAND_LINE_FLAGS);
         mLogBundle = new Bundle();
         mBundleGenerator = new RobotiumBundleGenerator();
+        mOnlyOutputFailures = arguments.containsKey(EXTRA_ONLY_OUTPUT_FAILURES);
         start();
     }
 
@@ -118,17 +124,27 @@ public class ChromeNativeTestInstrumentationTestRunner extends Instrumentation {
             for (String l = r.readLine(); l != null && !l.equals("<<ScopedMainEntryLogger");
                     l = r.readLine()) {
                 Matcher m = RE_TEST_OUTPUT.matcher(l);
+                boolean isFailure = false;
                 if (m.matches()) {
                     if (m.group(1).equals("RUN")) {
                         results.put(m.group(2), TestResult.UNKNOWN);
                     } else if (m.group(1).equals("FAILED")) {
                         results.put(m.group(2), TestResult.FAILED);
+                        isFailure = true;
+                        mLogBundle.putString(Instrumentation.REPORT_KEY_STREAMRESULT, l + "\n");
+                        sendStatus(0, mLogBundle);
                     } else if (m.group(1).equals("OK")) {
                         results.put(m.group(2), TestResult.PASSED);
                     }
                 }
-                mLogBundle.putString(Instrumentation.REPORT_KEY_STREAMRESULT, l + "\n");
-                sendStatus(0, mLogBundle);
+
+                // TODO(jbudorick): mOnlyOutputFailures is a workaround for b/18981674. Remove it
+                // once that issue is fixed.
+                if (!mOnlyOutputFailures || isFailure) {
+                    mLogBundle.putString(Instrumentation.REPORT_KEY_STREAMRESULT, l + "\n");
+                    sendStatus(0, mLogBundle);
+                }
+                Log.i(TAG, l);
             }
         } catch (InterruptedException e) {
             Log.e(TAG, "Interrupted while waiting for FIFO file creation: " + e.toString());
@@ -169,6 +185,9 @@ public class ChromeNativeTestInstrumentationTestRunner extends Instrumentation {
                         ++testsPassed;
                         break;
                     case FAILED:
+                        // TODO(jbudorick): Remove this log message once AMP execution and
+                        // results handling has been stabilized.
+                        Log.d(TAG, "FAILED: " + entry.getKey());
                         ++testsFailed;
                         break;
                     default:
@@ -179,11 +198,12 @@ public class ChromeNativeTestInstrumentationTestRunner extends Instrumentation {
             }
 
             StringBuilder resultBuilder = new StringBuilder();
-            resultBuilder.append("\nOK (" + Integer.toString(testsPassed) + " tests)");
             if (testsFailed > 0) {
                 resultBuilder.append(
                         "\nFAILURES!!! Tests run: " + Integer.toString(rawResults.size())
                         + ", Failures: " + Integer.toString(testsFailed) + ", Errors: 0");
+            } else {
+                resultBuilder.append("\nOK (" + Integer.toString(testsPassed) + " tests)");
             }
             resultsBundle.putString(Instrumentation.REPORT_KEY_STREAMRESULT,
                     resultBuilder.toString());
