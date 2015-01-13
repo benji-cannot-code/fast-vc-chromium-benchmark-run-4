38b346ab69507ce8b655a58e0381245fc6529ec3
sanjoy.pal <sanjoy.pal@samsung.com>
2015-01-13 03:26:49 -0800

    Updating MakeMenuItemStringsFor() to test icon attribute of menuitems.
    BUG=87553
    
    Review URL: https://codereview.chromium.org/805153005
    
    Cr-Commit-Position: refs/heads/master@{#311259}

--- content/public/common/menu_item.cc
+++ content/public/common/menu_item.cc
@@ -18,6 +18,7 @@ MenuItem::MenuItem()
 
 MenuItem::MenuItem(const MenuItem& item)
     : label(item.label),
+      icon(item.icon),
       tool_tip(item.tool_tip),
       type(item.type),
       action(item.action),
