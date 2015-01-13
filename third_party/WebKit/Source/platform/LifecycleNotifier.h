88d963da22b66e6aeba763e9ba3e35db7e66e7a9
haraken@chromium.org <haraken@chromium.org>
2015-01-13 01:34:24 +0000

    Revert 188142 "LifecycleObservers can be destructed while Lifecy..."
    This revert was not a safe way to fix crbug.com/447879.
    I'll revert the revert and land a right fix for crbug.com/447879.
    
    > LifecycleObservers can be destructed while LifecycleNotifier is iterating the observers
    > 
    > Note: This is a problem that happens in non-Oilpan.
    > 
    > LifecycleObservers can be destructed while LifecycleNotifier is iterating
    > the LifecycleObservers in LifecycleNotifier::contextDestroyed().
    > This is a problem because it confuses the iteration.
    > 
    > Specifically, the problem happens in AsyncCallTracker::ExecutionContextData.
    > See the crash report for more details.
    > 
    > At first, I tried to avoid AsyncCallTracker::ExecutionContextData from
    > getting destructed in contextDestroyed(), but noticed that it requires
    > a weird hack (e.g., delayed destruction etc). At the same time, I noticed that
    > we already have the same problem in ContextLifecycleNotifier and we've supported
    > destructions of ActiveDOMObjects while iterating the ActiveDOMObjects
    > (See ContextLifecycleNotifier.cpp). Thus I decided to use the same hack
    > for the LifcycleNotifier. In other words, this CL fixes the crash by
    > supporting destructions of LifecycleObservers during LifecycleNotifer::contextDestroyed().
    > 
    > As commented in the code, these hacks will be removed
    > once we ship Oilpan by default for LifecycleObservers.
    > 
    > BUG=446985
    > TEST=none, couldn't reproduce the crash.
    > 
    > Review URL: https://codereview.chromium.org/837233003
    
    TBR=haraken@chromium.org
    
    Review URL: https://codereview.chromium.org/850623002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188265 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/LifecycleNotifier.h
+++ third_party/WebKit/Source/platform/LifecycleNotifier.h
@@ -74,6 +74,7 @@ protected:
         IteratingNone,
         IteratingOverAll,
         IteratingOverActiveDOMObjects,
+        IteratingOverContextObservers,
         IteratingOverDocumentObservers,
         IteratingOverPageObservers,
         IteratingOverDOMWindowObservers
@@ -110,18 +111,9 @@ inline void LifecycleNotifier<T>::notifyContextDestroyed()
         return;
 
     TemporaryChange<IterationType> scope(this->m_iterating, IteratingOverAll);
-    Vector<Observer*> snapshotOfObservers;
-    copyToVector(m_observers, snapshotOfObservers);
-    for (Observer* observer : snapshotOfObservers) {
-        // FIXME: Oilpan: At the moment, it's possible that the Observer is
-        // destructed during the iteration. Once we enable Oilpan by default
-        // for Observers, we can remove the hack by making m_observers
-        // a HeapHashSet<WeakMember<Observers>>. (i.e., we can just iterate
-        // m_observers without taking a snapshot).
-        if (m_observers.contains(observer)) {
-            ASSERT(observer->lifecycleContext() == m_context);
-            observer->contextDestroyed();
-        }
+    for (Observer* observer : m_observers) {
+        ASSERT(observer->lifecycleContext() == m_context);
+        observer->contextDestroyed();
     }
     m_didCallContextDestroyed = true;
 }
@@ -136,6 +128,7 @@ inline void LifecycleNotifier<T>::addObserver(typename LifecycleNotifier<T>::Obs
 template<typename T>
 inline void LifecycleNotifier<T>::removeObserver(typename LifecycleNotifier<T>::Observer* observer)
 {
+    RELEASE_ASSERT(m_iterating != IteratingOverAll);
     m_observers.remove(observer);
 }
 
