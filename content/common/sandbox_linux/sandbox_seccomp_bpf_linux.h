818cd89263eb8c89a3712602b42f9615a3511139
dati91 <dati91@gmail.com>
2015-01-13 01:53:10 -0800

    Build fail on ARM64/linux
    Removed a forward decleration of sandbox policy and replaced it with an include.
    This resolves the build fail on ARM64/linux.
    
    BUG=447980
    
    Review URL: https://codereview.chromium.org/827283004
    
    Cr-Commit-Position: refs/heads/master@{#311249}

--- content/common/sandbox_linux/sandbox_seccomp_bpf_linux.h
+++ content/common/sandbox_linux/sandbox_seccomp_bpf_linux.h
@@ -11,12 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/files/scoped_file.h"
 #include "base/memory/scoped_ptr.h"
-
-namespace sandbox {
-namespace bpf_dsl {
-class Policy;
-}
-}
+#include "sandbox/linux/bpf_dsl/policy.h"
 
 namespace content {
 
