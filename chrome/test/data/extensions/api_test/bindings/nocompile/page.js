005fce91312a40e8fd57f7f83921a2692abbed42
kalman <kalman@chromium.org>
2015-01-13 11:01:18 -0800

    Add a 'nocompile' option to Extension feature files, and annotate all Devtools APIs with it. Devtools APIs are written by hand and don't need to be compiled (moreover, it crashes when you try).
    BUG=356133
    R=mek@chromium.org
    
    Review URL: https://codereview.chromium.org/850603003
    
    Cr-Commit-Position: refs/heads/master@{#311302}

---
+++ chrome/test/data/extensions/api_test/bindings/nocompile/page.js
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Devtools APIs are all marked nocompile, so they shouldn't exist.
+chrome.test.assertEq(undefined, chrome.devtools);
+chrome.test.assertEq('undefined', typeof chrome.devtools);
+chrome.test.assertFalse(chrome.hasOwnProperty('devtools'));
+chrome.test.succeed();
