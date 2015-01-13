8a55e7068a2415c8f4a6230086be817942e20b57
jsbell@chromium.org <jsbell@chromium.org>
2015-01-13 23:25:28 +0000

    Remove IDBAny::KeyPathType
    We never "maybe" return a keypath from a method/attribute, so it
    doesn't need to be included in the IDBAny variant type. Make the
    conversion to ScriptValue explicit, and clean up the overly clever
    IDBAny constructors.
    
    R=cmumford@chromium.org,haraken@chromium.org
    
    Review URL: https://codereview.chromium.org/832043004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188336 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/indexeddb/IDBAny.cpp
+++ third_party/WebKit/Source/modules/indexeddb/IDBAny.cpp
@@ -108,7 +108,7 @@ const IDBKey* IDBAny::key() const
 
 const IDBKeyPath& IDBAny::keyPath() const
 {
-    ASSERT(m_type == KeyPathType || m_type == BufferKeyAndKeyPathType);
+    ASSERT(m_type == BufferKeyAndKeyPathType);
     return m_idbKeyPath;
 }
 
@@ -186,13 +186,6 @@ IDBAny::IDBAny(PassRefPtr<SharedBuffer> value, const Vector<WebBlobInfo>* blobIn
 IDBAny::IDBAny(IDBKey* key)
     : m_type(KeyType)
     , m_idbKey(key)
-    , m_integer(0)
-{
-}
-
-IDBAny::IDBAny(const IDBKeyPath& value)
-    : m_type(KeyPathType)
-    , m_idbKeyPath(value)
     , m_integer(0)
 {
 }
