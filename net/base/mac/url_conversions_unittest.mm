d042f3f07bee49d10f2131b12ce4e1a3ff200855
dcheng <dcheng@chromium.org>
2015-01-12 17:57:49 -0800

    Update {virtual,override,final} for net/ to follow C++11 style.
    This patch was automatically generated using a variation of
    https://codereview.chromium.org/598073004 on a Mac build.
    
    BUG=417463
    
    Review URL: https://codereview.chromium.org/840123002
    
    Cr-Commit-Position: refs/heads/master@{#311177}

--- net/base/mac/url_conversions_unittest.mm
+++ net/base/mac/url_conversions_unittest.mm
@@ -16,7 +16,7 @@ namespace {
 
 class URLConversionTest : public ::testing::Test {
  protected:
-  virtual void SetUp() {
+  void SetUp() override {
     testData_.reset([[NSArray alloc]
         initWithObjects:
             // Simple URL with protocol
