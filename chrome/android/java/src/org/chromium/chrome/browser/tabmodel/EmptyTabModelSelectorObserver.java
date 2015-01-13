3b45228630877e074ed4feab0f4ae2ef70887438
tedchoc <tedchoc@chromium.org>
2015-01-13 11:57:35 -0800

    [Android] Add tab model selected observer method to the selector.
    BUG=344589
    
    Review URL: https://codereview.chromium.org/846123002
    
    Cr-Commit-Position: refs/heads/master@{#311313}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/EmptyTabModelSelectorObserver.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/EmptyTabModelSelectorObserver.java
@@ -20,4 +20,7 @@ public class EmptyTabModelSelectorObserver implements TabModelSelectorObserver {
     public void onNewTabCreated(Tab tab) {
     }
 
+    @Override
+    public void onTabModelSelected(TabModel newModel, TabModel oldModel) {
+    }
 }
