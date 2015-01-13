2a0dd9c30cc95d756dd7303f0cd0ab90a7e5a769
Bacek <Bacek@yandex-team.ru>
2015-01-13 14:19:31 -0800

    Fix crash in handling PushStreams
    "pushed_it" created early can point to expired entry.
    DeleteExpiredPushedStreams will remove actual entry invalidating "pushed_it"
    iterator. std::map::insert will crash when invalid iterator passed as hint.
    
    Swap inserting new element and removing expired ones so iterator will stay
    valid until late.
    
    BUG=443490
    R=bnc@chromium.org, rch@chromium.org
    
    Review URL: https://codereview.chromium.org/813993002
    
    Cr-Commit-Position: refs/heads/master@{#311345}

--- net/spdy/spdy_session.cc
+++ net/spdy/spdy_session.cc
@@ -2721,12 +2721,12 @@ bool SpdySession::TryCreatePushStream(SpdyStreamId stream_id,
 
   last_compressed_frame_len_ = 0;
 
-  DeleteExpiredPushedStreams();
   PushedStreamMap::iterator inserted_pushed_it =
       unclaimed_pushed_streams_.insert(
           pushed_it,
           std::make_pair(gurl, PushedStreamInfo(stream_id, time_func_())));
   DCHECK(inserted_pushed_it != pushed_it);
+  DeleteExpiredPushedStreams();
 
   InsertActivatedStream(stream.Pass());
 
