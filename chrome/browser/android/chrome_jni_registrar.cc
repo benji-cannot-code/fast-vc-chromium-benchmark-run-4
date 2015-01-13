039bd21e332e16f0da81d5b34d08673b10905f15
newt <newt@chromium.org>
2015-01-13 11:44:38 -0800

    Upstream partner customizations.
    This upstreams code to support partner customizations, i.e. partner bookmarks,
    disabling incognito mode, and setting a homepage.
    
    BUG=444675
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/839663003
    
    Cr-Commit-Position: refs/heads/master@{#311310}

--- chrome/browser/android/chrome_jni_registrar.cc
+++ chrome/browser/android/chrome_jni_registrar.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/appmenu/app_menu_drag_helper.h"
 #include "chrome/browser/android/banners/app_banner_manager.h"
 #include "chrome/browser/android/bookmarks/bookmarks_bridge.h"
+#include "chrome/browser/android/bookmarks/partner_bookmarks_reader.h"
 #include "chrome/browser/android/chrome_web_contents_delegate_android.h"
 #include "chrome/browser/android/chromium_application.h"
 #include "chrome/browser/android/compositor/layer_title_cache.h"
@@ -188,6 +189,8 @@ static base::android::RegistrationMethod kChromeRegisteredMethods[] = {
     {"OmniboxUrlEmphasizer",
      OmniboxUrlEmphasizer::RegisterOmniboxUrlEmphasizer},
     {"OmniboxViewUtil", OmniboxViewUtil::RegisterOmniboxViewUtil},
+    {"PartnerBookmarksReader",
+     PartnerBookmarksReader::RegisterPartnerBookmarksReader},
     {"PasswordGenerationPopup",
      autofill::PasswordGenerationPopupViewAndroid::Register},
     {"PasswordUIViewAndroid",
