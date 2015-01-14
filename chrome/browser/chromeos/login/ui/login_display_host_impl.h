7b1a5458f4e01f749ef6207e6fbac6e78dfd3f1d
dmichael <dmichael@chromium.org>
2015-01-13 16:42:22 -0800

    Fix WeakPtrFactory member in LoginDisplayHostImpl
    This forces WeakPtrs to be invalidated before other members' destructors run.
    See the bug or the documentation of WeakPtrFactory for more information. I'm
    in the process of trying to turn on a clang check for this (see the bug).
    
    BUG=303818
    R=stevenjb@chromium.org
    
    Review URL: https://codereview.chromium.org/841103003
    
    Cr-Commit-Position: refs/heads/master@{#311373}

--- chrome/browser/chromeos/login/ui/login_display_host_impl.h
+++ chrome/browser/chromeos/login/ui/login_display_host_impl.h
@@ -210,8 +210,6 @@ class LoginDisplayHostImpl : public LoginDisplayHost,
 
   content::NotificationRegistrar registrar_;
 
-  base::WeakPtrFactory<LoginDisplayHostImpl> pointer_factory_;
-
   // Default LoginDisplayHost.
   static LoginDisplayHost* default_host_;
 
@@ -326,6 +324,7 @@ class LoginDisplayHostImpl : public LoginDisplayHost,
   scoped_ptr<aura::Window> login_screen_container_;
 #endif
 
+  base::WeakPtrFactory<LoginDisplayHostImpl> pointer_factory_;
   base::WeakPtrFactory<LoginDisplayHostImpl> animation_weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(LoginDisplayHostImpl);
