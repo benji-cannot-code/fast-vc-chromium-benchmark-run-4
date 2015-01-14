cdd72ec82009d3378d5d136d8a61883134d09a55
kouhei@chromium.org <kouhei@chromium.org>
2015-01-14 23:11:24 +0000

    ASSERT that Visitor::mark is only called when inGC()
    This CL adds an ASSERT to check that Visitor::mark is only called when
    all threads are parked and the gc state is at ThreadState::inGC().
    
    The current exception to this is ephemeron weak processing, where it call
    trace() to see if an element is dead. This should be refactored in future
    so that it would check without calling trace(), but it is left as
    an exception as of now.
    
    BUG=210454
    
    Review URL: https://codereview.chromium.org/824513004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188420 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/heap/Visitor.h
+++ third_party/WebKit/Source/platform/heap/Visitor.h
@@ -345,6 +345,7 @@ public:
             return;
 #if ENABLE(ASSERT)
         TraceTrait<T>::checkGCInfo(t);
+        Derived::fromHelper(this)->checkMarkingAllowed();
 #endif
         TraceTrait<T>::mark(Derived::fromHelper(this), t);
 
@@ -633,6 +634,10 @@ public:
 
     inline bool isGlobalMarkingVisitor() const { return m_isGlobalMarkingVisitor; }
 
+#if ENABLE(ASSERT)
+    virtual void setAllowMarkingForHashTableWeakProcessing(bool) { }
+#endif
+
 protected:
     explicit Visitor(VisitorType type)
         : m_isGlobalMarkingVisitor(type == GlobalMarkingVisitorType)
@@ -649,6 +654,10 @@ protected:
 
     void* m_hostObject;
     String m_hostName;
+#endif
+
+#if ENABLE(ASSERT)
+    virtual void checkMarkingAllowed() { }
 #endif
 
 private:
