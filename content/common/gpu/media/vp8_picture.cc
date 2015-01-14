31c21561fb177d1e53000b8fd7fda07d2e24e338
posciak <posciak@chromium.org>
2015-01-14 04:58:09 -0800

    Reland: Add accelerated video decoder interface, VP8 and H.264 implementations and hook up to V4L2SVDA.
    This is a reland as the previous CL has an issue with BUILD.gn and failed gn build.
    
    An AcceleratedVideoDecoder is a video decoder that requires support from an
    external accelerator (typically a hardware accelerator) to partially
    offload the decode process after parsing stream headers, and performing
    reference frame and state management.
    
    In this design, the hardware-independent decoder implementation interfaces
    with a HW-specific Accelerator to offload last stages of the decode process.
    
    Add the interface for AcceleratedVideoDecoder, decoders for VP8 and H264,
    V4L2-specific Accelerators for VP8 and H264 and a common accelerator/client
    class for V4L2.
    
    TEST=vdatest VP8/H264, mp4 local video playback, apprtc decode
    BUG=chrome-os-partner:33728
    TBR=wuchengli@chromium.org,kcwu@chromium.org,owenlin@chromium.org,xhwang@chromium.org,scherkus@chromium.org,dalecurtis@chromium.org
    
    Review URL: https://codereview.chromium.org/813693006
    
    Cr-Commit-Position: refs/heads/master@{#311464}

---
+++ content/common/gpu/media/vp8_picture.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/common/gpu/media/vp8_picture.h"
+
+namespace content {
+
+VP8Picture::VP8Picture() {
+}
+
+VP8Picture::~VP8Picture() {
+}
+
+V4L2VP8Picture* VP8Picture::AsV4L2VP8Picture() {
+  return nullptr;
+}
+
+}  // namespace content
