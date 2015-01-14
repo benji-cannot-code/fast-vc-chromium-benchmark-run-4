35ef30a5caf0694f76b99ae1b8d57598b88871e2
ariblue <ariblue@google.com>
2015-01-14 14:43:01 -0800

    Create AndroidCommandLineBackend to handle setting up cmdline args
    BUG=448204,441959
    
    Review URL: https://codereview.chromium.org/811703007
    
    Cr-Commit-Position: refs/heads/master@{#311564}

---
+++ tools/telemetry/telemetry/core/backends/android_command_line_backend_unittest.py
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import unittest
+
+from telemetry.core.backends import android_command_line_backend
+
+
+class AndroidCommandLineBackendTest(unittest.TestCase):
+
+  def testQuoteIfNeededNoEquals(self):
+    string = 'value'
+    self.assertEqual(string,
+                     android_command_line_backend._QuoteIfNeeded(string))
+
+  def testQuoteIfNeededNoSpaces(self):
+    string = 'key=valueA'
+    self.assertEqual(string,
+                     android_command_line_backend._QuoteIfNeeded(string))
+
+  def testQuoteIfNeededAlreadyQuoted(self):
+    string = "key='valueA valueB'"
+    self.assertEqual(string,
+                     android_command_line_backend._QuoteIfNeeded(string))
+
+  def testQuoteIfNeeded(self):
+    string = 'key=valueA valueB'
+    expected_output = "key='valueA valueB'"
+    self.assertEqual(expected_output,
+                     android_command_line_backend._QuoteIfNeeded(string))
