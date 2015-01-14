f75604511a46bf83ad9826f1552659febf69853a
Paweł Hajdan, Jr <phajdan.jr@chromium.org>
2015-01-14 11:06:52 +0100

    Extract UnitTestLauncherDelegate declaration to a header
    This is a preparation for defining some methods in an ios-specific
    .cc file.
    
    BUG=426870
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/848903002
    
    Cr-Commit-Position: refs/heads/master@{#311440}

--- base/test/launcher/unit_test_launcher.h
+++ base/test/launcher/unit_test_launcher.h
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define BASE_TEST_LAUNCHER_UNIT_TEST_LAUNCHER_H_
 
 #include "base/callback.h"
+#include "base/files/file_path.h"
+#include "base/test/launcher/test_launcher.h"
 
 namespace base {
 
@@ -31,6 +33,74 @@ int LaunchUnitTests(int argc,
                     bool use_job_objects,
                     const RunTestSuiteCallback& run_test_suite);
 #endif  // defined(OS_WIN)
+
+// Test launcher delegate for unit tests (mostly to support batching).
+class UnitTestLauncherDelegate : public TestLauncherDelegate {
+ public:
+  explicit UnitTestLauncherDelegate(size_t batch_limit, bool use_job_objects);
+  ~UnitTestLauncherDelegate() override;
+
+ private:
+  struct GTestCallbackState {
+    GTestCallbackState();
+    ~GTestCallbackState();
+
+    TestLauncher* test_launcher;
+    std::vector<std::string> test_names;
+    FilePath output_file;
+  };
+
+  // TestLauncherDelegate:
+  bool ShouldRunTest(const std::string& test_case_name,
+                     const std::string& test_name) override;
+  size_t RunTests(TestLauncher* test_launcher,
+                  const std::vector<std::string>& test_names) override;
+  size_t RetryTests(TestLauncher* test_launcher,
+                    const std::vector<std::string>& test_names) override;
+
+  // Runs tests serially, each in its own process.
+  void RunSerially(TestLauncher* test_launcher,
+                   const std::vector<std::string>& test_names);
+
+  // Runs tests in batches (each batch in its own process).
+  void RunBatch(TestLauncher* test_launcher,
+                const std::vector<std::string>& test_names);
+
+  // Callback for batched tests.
+  void GTestCallback(const GTestCallbackState& callback_state,
+                     int exit_code,
+                     const TimeDelta& elapsed_time,
+                     bool was_timeout,
+                     const std::string& output);
+
+  // Callback for serialized tests.
+  void SerialGTestCallback(const GTestCallbackState& callback_state,
+                           const std::vector<std::string>& test_names,
+                           int exit_code,
+                           const TimeDelta& elapsed_time,
+                           bool was_timeout,
+                           const std::string& output);
+
+  // Interprets test results and reports to the test launcher. Returns true
+  // on success.
+  static bool ProcessTestResults(TestLauncher* test_launcher,
+                                 const std::vector<std::string>& test_names,
+                                 const base::FilePath& output_file,
+                                 const std::string& output,
+                                 int exit_code,
+                                 bool was_timeout,
+                                 std::vector<std::string>* tests_to_relaunch);
+
+  ThreadChecker thread_checker_;
+
+  // Maximum number of tests to run in a single batch.
+  size_t batch_limit_;
+
+  // Determines whether we use job objects on Windows.
+  bool use_job_objects_;
+
+  DISALLOW_COPY_AND_ASSIGN(UnitTestLauncherDelegate);
+};
 
 }   // namespace base
 
