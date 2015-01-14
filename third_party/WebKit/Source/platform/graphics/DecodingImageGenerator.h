8cf06dc84c9ba80d69fbadfe7c9244a0aa518dd7
reed@google.com <reed@google.com>
2015-01-14 03:35:25 +0000

    Move impl for WebImageGenerator out of header
    follow-on chrome CL : https://codereview.chromium.org/844033003/
    
    BUG=
    
    Review URL: https://codereview.chromium.org/846033002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188351 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/DecodingImageGenerator.h
+++ third_party/WebKit/Source/platform/graphics/DecodingImageGenerator.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "SkImageGenerator.h"
 #include "SkImageInfo.h"
 
+#include "platform/PlatformExport.h"
 #include "wtf/RefPtr.h"
 
 class SkData;
@@ -43,7 +44,7 @@ class ImageFrameGenerator;
 //
 // This class does not own an ImageDecode. It does not own encoded data. It serves
 // as and adapter to ImageFrameGenerator which actually performs decoding.
-class DecodingImageGenerator final : public SkImageGenerator {
+class PLATFORM_EXPORT DecodingImageGenerator final : public SkImageGenerator {
 public:
     static SkImageGenerator* create(SkData*);
 
