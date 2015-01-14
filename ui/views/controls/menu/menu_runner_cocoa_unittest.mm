435da6541f10ad2a0e7b3b321eb9f316aa5d3012
tapted <tapted@chromium.org>
2015-01-14 11:10:45 -0800

    MacViews: Remove redundant virtuals in NativeWidgetMac and co.
    Strict virtual annotation checks were enabled on Mac in r311365.
    
    This gets toolkit_views for Mac on board.
    
    BUG=417463
    
    Review URL: https://codereview.chromium.org/850843003
    
    Cr-Commit-Position: refs/heads/master@{#311528}

--- ui/views/controls/menu/menu_runner_cocoa_unittest.mm
+++ ui/views/controls/menu/menu_runner_cocoa_unittest.mm
@@ -16,9 +16,9 @@ namespace test {
 class MenuRunnerCocoaTest : public ViewsTestBase {
  public:
   MenuRunnerCocoaTest() : runner_(NULL) {}
-  virtual ~MenuRunnerCocoaTest() {}
+  ~MenuRunnerCocoaTest() override {}
 
-  virtual void SetUp() override {
+  void SetUp() override {
     ViewsTestBase::SetUp();
 
     menu_.reset(new ui::SimpleMenuModel(NULL));
@@ -28,7 +28,7 @@ class MenuRunnerCocoaTest : public ViewsTestBase {
     EXPECT_FALSE(runner_->IsRunning());
   }
 
-  virtual void TearDown() override {
+  void TearDown() override {
     if (runner_) {
       runner_->Release();
       runner_ = NULL;
