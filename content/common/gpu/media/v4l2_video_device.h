f012b10be2cd2e4a344fc8723f455212d1ce4fa8
posciak <posciak@chromium.org>
2015-01-14 02:37:05 -0800

    Add accelerated video decoder interface, VP8 and H.264 implementations and hook up to V4L2SVDA.
    An AcceleratedVideoDecoder is a video decoder that requires support from an
    external accelerator (typically a hardware accelerator) to partially
    offload the decode process after parsing stream headers, and performing
    reference frame and state management.
    
    In this design, the hardware-independent decoder implementation interfaces
    with a HW-specific Accelerator to offload last stages of the decode process.
    
    Add the interface for AcceleratedVideoDecoder, decoders for VP8 and H264,
    V4L2-specific Accelerators for VP8 and H264 and a common accelerator/client
    class for V4L2.
    
    TEST=vdatest VP8/H264, mp4 local video playback, apprtc decode
    BUG=chrome-os-partner:33728
    
    Review URL: https://codereview.chromium.org/833063003
    
    Cr-Commit-Position: refs/heads/master@{#311448}

--- content/common/gpu/media/v4l2_video_device.h
+++ content/common/gpu/media/v4l2_video_device.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_COMMON_GPU_MEDIA_V4L2_VIDEO_DEVICE_H_
 #define CONTENT_COMMON_GPU_MEDIA_V4L2_VIDEO_DEVICE_H_
 
+#include "base/memory/ref_counted.h"
 #include "media/base/video_decoder_config.h"
 #include "media/base/video_frame.h"
 #include "ui/gfx/geometry/size.h"
@@ -17,17 +18,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-class V4L2Device {
+class V4L2Device : public base::RefCountedThreadSafe<V4L2Device> {
  public:
   // Utility format conversion functions
   static media::VideoFrame::Format V4L2PixFmtToVideoFrameFormat(uint32 format);
   static uint32 VideoFrameFormatToV4L2PixFmt(media::VideoFrame::Format format);
-  static uint32 VideoCodecProfileToV4L2PixFmt(media::VideoCodecProfile profile);
+  static uint32 VideoCodecProfileToV4L2PixFmt(media::VideoCodecProfile profile,
+                                              bool slice_based);
   static uint32_t V4L2PixFmtToDrmFormat(uint32_t format);
   // Convert format requirements requested by a V4L2 device to gfx::Size.
   static gfx::Size CodedSizeFromV4L2Format(struct v4l2_format format);
-
-  virtual ~V4L2Device();
 
   enum Type {
     kDecoder,
@@ -37,7 +37,7 @@ class V4L2Device {
 
   // Creates and initializes an appropriate V4L2Device of |type| for the
   // current platform and returns a scoped_ptr<V4L2Device> on success, or NULL.
-  static scoped_ptr<V4L2Device> Create(Type type);
+  static scoped_refptr<V4L2Device> Create(Type type);
 
   // Parameters and return value are the same as for the standard ioctl() system
   // call.
@@ -99,6 +99,10 @@ class V4L2Device {
 
   // Returns the preferred V4L2 input format or 0 if don't care.
   virtual uint32 PreferredInputFormat() = 0;
+
+ protected:
+  friend class base::RefCountedThreadSafe<V4L2Device>;
+  virtual ~V4L2Device();
 };
 
 }  //  namespace content
