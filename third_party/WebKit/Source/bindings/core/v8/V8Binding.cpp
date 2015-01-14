48f2b6ecd1bc32592793f27630d962520d8c1498
kinuko@chromium.org <kinuko@chromium.org>
2015-01-14 10:28:29 +0000

    Inline v8ArrayBufferAllocator in V8Initializer::initializeMainThread
    As it's the only callsite, and in order to ensure that its initialization doesn't have any race issues.
    
    BUG=373133
    TEST=trybots
    
    Review URL: https://codereview.chromium.org/847983002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188371 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/core/v8/V8Binding.cpp
+++ third_party/WebKit/Source/bindings/core/v8/V8Binding.cpp
@@ -61,7 +61,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/xml/XPathNSResolver.h"
 #include "platform/EventTracer.h"
 #include "platform/JSONValues.h"
-#include "wtf/ArrayBufferContents.h"
 #include "wtf/MainThread.h"
 #include "wtf/MathExtras.h"
 #include "wtf/StdLibExtras.h"
@@ -94,33 +93,6 @@ v8::Local<v8::Value> createMinimumArityTypeErrorForConstructor(v8::Isolate* isol
 void setMinimumArityTypeError(ExceptionState& exceptionState, unsigned expected, unsigned provided)
 {
     exceptionState.throwTypeError(ExceptionMessages::notEnoughArguments(expected, provided));
-}
-
-class ArrayBufferAllocator : public v8::ArrayBuffer::Allocator {
-    virtual void* Allocate(size_t size) override
-    {
-        void* data;
-        WTF::ArrayBufferContents::allocateMemory(size, WTF::ArrayBufferContents::ZeroInitialize, data);
-        return data;
-    }
-
-    virtual void* AllocateUninitialized(size_t size) override
-    {
-        void* data;
-        WTF::ArrayBufferContents::allocateMemory(size, WTF::ArrayBufferContents::DontInitialize, data);
-        return data;
-    }
-
-    virtual void Free(void* data, size_t size) override
-    {
-        WTF::ArrayBufferContents::freeMemory(data, size);
-    }
-};
-
-v8::ArrayBuffer::Allocator* v8ArrayBufferAllocator()
-{
-    DEFINE_STATIC_LOCAL(ArrayBufferAllocator, arrayBufferAllocator, ());
-    return &arrayBufferAllocator;
 }
 
 PassRefPtrWillBeRawPtr<NodeFilter> toNodeFilter(v8::Handle<v8::Value> callback, v8::Handle<v8::Object> creationContext, ScriptState* scriptState)
--- third_party/WebKit/Source/bindings/core/v8/V8Initializer.cpp
+++ third_party/WebKit/Source/bindings/core/v8/V8Initializer.cpp
@@ -56,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/heap/AddressSanitizer.h"
 #include "platform/scheduler/Scheduler.h"
 #include "public/platform/Platform.h"
+#include "wtf/ArrayBufferContents.h"
 #include "wtf/RefPtr.h"
 #include "wtf/ThreadSpecific.h"
 #include "wtf/text/WTFString.h"
@@ -212,6 +213,27 @@ public:
     const int m_lineNumber;
     const int m_columnNumber;
     const RefPtrWillBeMember<ScriptCallStack> m_callStack;
+};
+
+class ArrayBufferAllocator : public v8::ArrayBuffer::Allocator {
+    virtual void* Allocate(size_t size) override
+    {
+        void* data;
+        WTF::ArrayBufferContents::allocateMemory(size, WTF::ArrayBufferContents::ZeroInitialize, data);
+        return data;
+    }
+
+    virtual void* AllocateUninitialized(size_t size) override
+    {
+        void* data;
+        WTF::ArrayBufferContents::allocateMemory(size, WTF::ArrayBufferContents::DontInitialize, data);
+        return data;
+    }
+
+    virtual void Free(void* data, size_t size) override
+    {
+        WTF::ArrayBufferContents::freeMemory(data, size);
+    }
 };
 
 } // namespace
@@ -444,7 +466,8 @@ void V8Initializer::initializeMainThreadIfNeeded()
         return;
     initialized = true;
 
-    gin::IsolateHolder::Initialize(gin::IsolateHolder::kNonStrictMode, v8ArrayBufferAllocator());
+    DEFINE_STATIC_LOCAL(ArrayBufferAllocator, arrayBufferAllocator, ());
+    gin::IsolateHolder::Initialize(gin::IsolateHolder::kNonStrictMode, &arrayBufferAllocator);
 
     v8::Isolate* isolate = V8PerIsolateData::initialize();
 
