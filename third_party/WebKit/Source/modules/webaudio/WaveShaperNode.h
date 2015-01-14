458c70f11acb1fcb620ddbf5e19658b3a663b16e
rtoy@chromium.org <rtoy@chromium.org>
2015-01-14 17:16:32 +0000

    WaveShaperNode.oversample does not throw exceptions.
    Update IDL to indicate that the setter does not throw exceptions.  The
    code wasn't throwing exceptions anyway, and the WebAudio does not say
    exceptions should be thrown.
    
    BUG=448209
    TEST=already covered by dom-exceptions.html.
    
    Review URL: https://codereview.chromium.org/810453006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188398 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/webaudio/WaveShaperNode.h
+++ third_party/WebKit/Source/modules/webaudio/WaveShaperNode.h
@@ -47,7 +47,7 @@ public:
     void setCurve(DOMFloat32Array*, ExceptionState&);
     DOMFloat32Array* curve();
 
-    void setOversample(const String& , ExceptionState&);
+    void setOversample(const String&);
     String oversample() const;
 
 private:
