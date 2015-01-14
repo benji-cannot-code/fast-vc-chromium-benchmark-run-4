cc07840577471ff79833f6da03fcbdd7c868f851
rockot <rockot@chromium.org>
2015-01-14 15:26:11 -0800

    chrome.identity API: Restrict gaia ID access
    This restricts the availability of obfuscated GAIA ID
    info (from chrome.identity.getProfileUserInfo) to apps
    which specify the identity.email manifest permission.
    
    BUG=415641
    
    Review URL: https://codereview.chromium.org/784283006
    
    Cr-Commit-Position: refs/heads/master@{#311570}

--- chrome/browser/extensions/api/identity/identity_api.cc
+++ chrome/browser/extensions/api/identity/identity_api.cc
@@ -853,8 +853,8 @@ ExtensionFunction::ResponseAction IdentityGetProfileUserInfoFunction::Run() {
   if (extension()->permissions_data()->HasAPIPermission(
           APIPermission::kIdentityEmail)) {
     profile_user_info.email = account.email;
+    profile_user_info.id = account.gaia;
   }
-  profile_user_info.id = account.gaia;
 
   return RespondNow(OneArgument(profile_user_info.ToValue().release()));
 }
