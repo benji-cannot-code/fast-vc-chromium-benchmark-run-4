de5a4edfc6a08d3c2d371c254be3c0135fa5eb4e
rsadam <rsadam@chromium.org>
2015-01-13 18:31:05 -0800

    Moves smart deploy UI into the IME tray.
    BUG=447642, 443766
    TEST=TrayIMETest.PerformActionOnDetailedView, TrayIMETest.HidesOnA11yEnabled, TrayIMETest.HiddenWithNoIMEs
    
    Review URL: https://codereview.chromium.org/843603004
    
    Cr-Commit-Position: refs/heads/master@{#311394}

--- ash/system/tray/system_tray.cc
+++ ash/system/tray/system_tray.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/audio/tray_audio.h"
 #include "ash/system/bluetooth/tray_bluetooth.h"
 #include "ash/system/date/tray_date.h"
-#include "ash/system/ime/tray_ime.h"
 #include "ash/system/status_area_widget.h"
 #include "ash/system/tray/system_tray_delegate.h"
 #include "ash/system/tray/system_tray_item.h"
@@ -60,6 +59,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/chromeos/tray_caps_lock.h"
 #include "ash/system/chromeos/tray_display.h"
 #include "ash/system/chromeos/tray_tracing.h"
+#include "ash/system/ime/tray_ime_chromeos.h"
 #include "ash/system/tray/media_security/multi_profile_media_tray_item.h"
 #include "ui/message_center/message_center.h"
 #elif defined(OS_WIN)
@@ -202,7 +202,6 @@ void SystemTray::CreateItems(SystemTrayDelegate* delegate) {
   AddTrayItem(new TrayUpdate(this));
   AddTrayItem(tray_date_);
 #elif defined(OS_LINUX)
-  AddTrayItem(new TrayIME(this));
   AddTrayItem(tray_accessibility_);
   AddTrayItem(new TrayBluetooth(this));
   AddTrayItem(new TrayUpdate(this));
