e21ab816c9f09201b5469d2db40093f067e5781e
andrewhayden <andrewhayden@chromium.org>
2015-01-14 08:12:32 -0800

    Add explicit support for discovering the GpuProcess to Telemetry's 'Model'.
    BUG=
    
    Review URL: https://codereview.chromium.org/816833006
    
    Cr-Commit-Position: refs/heads/master@{#311486}

--- tools/telemetry/telemetry/timeline/trace_event_importer.py
+++ tools/telemetry/telemetry/timeline/trace_event_importer.py
@@ -223,6 +223,7 @@ class TraceEventTimelineImporter(importer.TimelineImporter):
     self._CreateAsyncSlices()
     self._CreateFlowSlices()
     self._SetBrowserProcess()
+    self._SetGpuProcess()
     self._CreateExplicitObjects()
     self._CreateImplicitObjects()
 
@@ -396,3 +397,8 @@ class TraceEventTimelineImporter(importer.TimelineImporter):
     for thread in self._model.GetAllThreads():
       if thread.name == 'CrBrowserMain':
         self._model.browser_process = thread.parent
+
+  def _SetGpuProcess(self):
+    for thread in self._model.GetAllThreads():
+      if thread.name == 'CrGpuMain':
+        self._model.gpu_process = thread.parent
