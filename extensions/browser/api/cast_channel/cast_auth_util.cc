bf8e6915d4d734182bc967f81d0275cd87825cfa
vadimgo <vadimgo@chromium.org>
2015-01-13 21:38:05 -0800

    Cast audio only policy enforcement support.
    BUG=447734
    
    Review URL: https://codereview.chromium.org/807723004
    
    Cr-Commit-Position: refs/heads/master@{#311415}

--- extensions/browser/api/cast_channel/cast_auth_util.cc
+++ extensions/browser/api/cast_channel/cast_auth_util.cc
@@ -18,6 +18,9 @@ namespace {
 
 const char* const kParseErrorPrefix = "Failed to parse auth message: ";
 
+const unsigned char kAudioOnlyPolicy[] =
+    {0x06, 0x0A, 0x2B, 0x06, 0x01, 0x04, 0x01, 0xD6, 0x79, 0x02, 0x05, 0x02};
+
 // Extracts an embedded DeviceAuthMessage payload from an auth challenge reply
 // message.
 AuthResult ParseAuthMessage(const CastMessage& challenge_reply,
@@ -55,7 +58,8 @@ AuthResult ParseAuthMessage(const CastMessage& challenge_reply,
 
 }  // namespace
 
-AuthResult::AuthResult() : error_type(ERROR_NONE), nss_error_code(0) {
+AuthResult::AuthResult()
+    : error_type(ERROR_NONE), nss_error_code(0), channel_policies(POLICY_NONE) {
 }
 
 AuthResult::~AuthResult() {
@@ -102,7 +106,12 @@ AuthResult AuthenticateChallengeReply(const CastMessage& challenge_reply,
     return result;
   }
 
-  return AuthResult();
+  if (response.client_auth_certificate().find(reinterpret_cast<const char*>(
+          kAudioOnlyPolicy)) != std::string::npos) {
+    result.channel_policies |= AuthResult::POLICY_AUDIO_ONLY;
+  }
+
+  return result;
 }
 
 }  // namespace cast_channel
