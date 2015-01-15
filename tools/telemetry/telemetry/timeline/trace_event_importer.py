5c0ca17e0731411341c2ff0979879a23549093b2
rch <rch@chromium.org>
2015-01-14 16:44:56 -0800

    Revert of Add explicit support for discovering the GpuProcess to Telemetry's 'Model'. (patchset #1 id:1 of https://codereview.chromium.org/816833006/)
    Reason for revert:
    Since this CL landed, we've been seeing flakes in Mac 10.9 telemetry_unittests. It's not clear that this CL is responsible, but the timing is suspicious.
    
    Recent 10.9 failures:
    https://build.chromium.org/p/chromium.mac/builders/Mac10.9%20Tests%20(dbg)
    
    Detailed log: https://build.chromium.org/p/chromium.mac/builders/Mac10.9%20Tests%20%28dbg%29/builds/1965/steps/telemetry_unittests/logs/stdio
    
    Original issue's description:
    > Add explicit support for discovering the GpuProcess to Telemetry's 'Model'.
    >
    > BUG=
    >
    > Committed: https://crrev.com/f6ade006723de6a100b6dd6b545cb5916b197925
    > Cr-Commit-Position: refs/heads/master@{#311486}
    
    TBR=skyostil@chromium.org,tonyg@chromium.org,andrewhayden@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=
    
    Review URL: https://codereview.chromium.org/819643006
    
    Cr-Commit-Position: refs/heads/master@{#311592}

--- tools/telemetry/telemetry/timeline/trace_event_importer.py
+++ tools/telemetry/telemetry/timeline/trace_event_importer.py
@@ -223,7 +223,6 @@ class TraceEventTimelineImporter(importer.TimelineImporter):
     self._CreateAsyncSlices()
     self._CreateFlowSlices()
     self._SetBrowserProcess()
-    self._SetGpuProcess()
     self._CreateExplicitObjects()
     self._CreateImplicitObjects()
 
@@ -397,8 +396,3 @@ class TraceEventTimelineImporter(importer.TimelineImporter):
     for thread in self._model.GetAllThreads():
       if thread.name == 'CrBrowserMain':
         self._model.browser_process = thread.parent
-
-  def _SetGpuProcess(self):
-    for thread in self._model.GetAllThreads():
-      if thread.name == 'CrGpuMain':
-        self._model.gpu_process = thread.parent
