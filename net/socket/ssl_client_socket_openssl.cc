04ca879beee9a97943a5f6d7c6079843f3d16db5
bnc <bnc@chromium.org>
2015-01-15 13:02:26 -0800

    Fix dangling pointer in OpenSSL NPN fallback.
    A copy assignment to |std::string proto| copies data, then |*out| takes its
    address, and |proto| goes out of scope, freeing |**out|.  This CL replaces that
    with a |const char*|, so that |*out| would point to the literal |char[]| defined
    in NextProtoToString.
    
    BUG=448428
    
    Review URL: https://codereview.chromium.org/849243003
    
    Cr-Commit-Position: refs/heads/master@{#311732}

--- net/socket/ssl_client_socket_openssl.cc
+++ net/socket/ssl_client_socket_openssl.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <openssl/bio.h>
 #include <openssl/err.h>
 #include <openssl/ssl.h>
+#include <string.h>
 
 #include "base/bind.h"
 #include "base/callback_helpers.h"
@@ -1901,9 +1902,10 @@ int SSLClientSocketOpenSSL::SelectNextProtoCallback(unsigned char** out,
 
   // If we didn't find a protocol, we select the first one from our list.
   if (npn_status_ == kNextProtoNoOverlap) {
-    const std::string proto = NextProtoToString(ssl_config_.next_protos[0]);
-    *out = reinterpret_cast<uint8*>(const_cast<char*>(proto.data()));
-    *outlen = proto.size();
+    // NextProtoToString returns a pointer to a static string.
+    const char* proto = NextProtoToString(ssl_config_.next_protos[0]);
+    *out = reinterpret_cast<unsigned char*>(const_cast<char*>(proto));
+    *outlen = strlen(proto);
   }
 
   npn_proto_.assign(reinterpret_cast<const char*>(*out), *outlen);
