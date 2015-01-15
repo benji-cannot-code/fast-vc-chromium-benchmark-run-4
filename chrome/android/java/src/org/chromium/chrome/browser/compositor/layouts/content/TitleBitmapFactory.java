3f801a363f8f1a8a8d0071f002cf3c7c2d2222f8
wajahat.s <wajahat.s@samsung.com>
2015-01-14 22:02:55 -0800

    Use Math.ceil() instead of FloatMath.ceil() in TitleBitmapFactory class.
    Below lint warnings were reported which is legitimate:
    org/chromium/chrome/browser/compositor/layouts/content/TitleBitmapFactory.class
    Use `java.lang.Math#ceil` instead of `android.util.FloatMath#ceil()`
    since it is faster as of API 8: FloatMath [warning]
    
    FloatMath.ceil() will be significantly slower
    on versions of Android with a JIT. The equivalent Math.ceil() is faster hence using the same.
    
    BUG=None.
    
    Review URL: https://codereview.chromium.org/836123007
    
    Cr-Commit-Position: refs/heads/master@{#311631}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/content/TitleBitmapFactory.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/content/TitleBitmapFactory.java
@@ -16,7 +16,6 @@ import android.graphics.drawable.Drawable;
 import android.text.Layout;
 import android.text.TextPaint;
 import android.text.TextUtils;
-import android.util.FloatMath;
 import android.util.Log;
 import android.view.InflateException;
 
@@ -74,7 +73,7 @@ public class TitleBitmapFactory {
         mTextPaint.density = res.getDisplayMetrics().density;
 
         FontMetrics textFontMetrics = mTextPaint.getFontMetrics();
-        mTextHeight = FloatMath.ceil(textFontMetrics.bottom - textFontMetrics.top);
+        mTextHeight = (float) Math.ceil(textFontMetrics.bottom - textFontMetrics.top);
         mTextYOffset = -textFontMetrics.top;
 
         mFaviconDimension = res.getDimensionPixelSize(R.dimen.compositor_tab_title_favicon_size);
@@ -131,7 +130,7 @@ public class TitleBitmapFactory {
         try {
             boolean drawText = !TextUtils.isEmpty(title);
             int textWidth =
-                    drawText ? (int) FloatMath.ceil(Layout.getDesiredWidth(title, mTextPaint)) : 0;
+                    drawText ? (int) Math.ceil(Layout.getDesiredWidth(title, mTextPaint)) : 0;
             // Minimum 1 width bitmap to avoid createBitmap function's IllegalArgumentException,
             // when textWidth == 0.
             Bitmap b = Bitmap.createBitmap(Math.max(Math.min(mMaxWidth, textWidth), 1), mViewHeight,
