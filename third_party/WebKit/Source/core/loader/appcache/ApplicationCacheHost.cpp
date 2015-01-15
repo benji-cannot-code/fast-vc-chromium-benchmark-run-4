695a25e35fb1c2501fae99f3710c73f215b63423
japhet@chromium.org <japhet@chromium.org>
2015-01-15 07:24:38 +0000

    Make client-triggered navigations more friendly to RemoteFrames
    * Change most callers of NavigationScheduler::scheduleLocationChange() to go through Frame::navigate(), which will proxy the navigation request if the target frame is remote.
    * Move some helpers from LocalDOMWindow to DOMWindow and teach them about RemoteFrames.
    * Move LocalDOMWindow::setLocation() to Location (its only user) and bring it closer to being remote-friendly.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/799903003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188452 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/appcache/ApplicationCacheHost.cpp
+++ third_party/WebKit/Source/core/loader/appcache/ApplicationCacheHost.cpp
@@ -121,7 +121,7 @@ void ApplicationCacheHost::selectCacheWithManifest(const KURL& manifestURL)
         // during navigation.
         // see ApplicationCacheGroup::selectCache()
         LocalFrame* frame = m_documentLoader->frame();
-        frame->navigationScheduler().scheduleLocationChange(frame->document(), frame->document()->url());
+        frame->navigate(*frame->document(), frame->document()->url(), true);
     }
 }
 
