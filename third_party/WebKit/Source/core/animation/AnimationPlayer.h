83d42cf1f5e98df740cc944c5bd8f4838fa7c4f6
dstockwell@chromium.org <dstockwell@chromium.org>
2015-01-15 03:04:00 +0000

    Web Animations: Update AnimationPlayer constructor to match spec
    http://w3c.github.io/web-animations/#dom-animationplayer-animationplayersource-timeline
    
    Per spec, players are now constructed in the idle state. Also cleans up some
    other logic that is no longer needed, such as the separate
    AnimationTimeline::createAnimationPlayer.
    
    Note: This is not added to AnimationPlayer.idl as we can't make a constructor
    be RuntimeEnabled.
    
    Covered by existing tests as no change in behavior.
    
    Review URL: https://codereview.chromium.org/841533005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188437 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/AnimationPlayer.h
+++ third_party/WebKit/Source/core/animation/AnimationPlayer.h
@@ -63,7 +63,7 @@ public:
     };
 
     ~AnimationPlayer();
-    static PassRefPtrWillBeRawPtr<AnimationPlayer> create(ExecutionContext*, AnimationTimeline&, AnimationNode*);
+    static PassRefPtrWillBeRawPtr<AnimationPlayer> create(AnimationNode*, AnimationTimeline*);
 
     // Returns whether the player is finished.
     bool update(TimingUpdateReason);
--- third_party/WebKit/Source/core/animation/AnimationPlayerTest.cpp
+++ third_party/WebKit/Source/core/animation/AnimationPlayerTest.cpp
@@ -59,7 +59,7 @@ protected:
         document = Document::create();
         document->animationClock().resetTimeForTesting();
         timeline = AnimationTimeline::create(document.get());
-        player = timeline->createAnimationPlayer(0);
+        player = timeline->play(0);
         player->setStartTime(0);
         player->setSource(makeAnimation().get());
     }
@@ -94,7 +94,7 @@ protected:
 TEST_F(AnimationAnimationPlayerTest, InitialState)
 {
     setUpWithoutStartingTimeline();
-    player = timeline->createAnimationPlayer(0);
+    player = timeline->play(0);
     EXPECT_EQ(AnimationPlayer::Pending, player->playStateInternal());
     EXPECT_EQ(0, player->currentTimeInternal());
     EXPECT_FALSE(player->paused());
@@ -635,7 +635,7 @@ TEST_F(AnimationAnimationPlayerTest, SetPlaybackRateMax)
 
 TEST_F(AnimationAnimationPlayerTest, SetSource)
 {
-    player = timeline->createAnimationPlayer(0);
+    player = timeline->play(0);
     player->setStartTime(0);
     RefPtrWillBeRawPtr<AnimationNode> source1 = makeAnimation();
     RefPtrWillBeRawPtr<AnimationNode> source2 = makeAnimation();
@@ -673,7 +673,7 @@ TEST_F(AnimationAnimationPlayerTest, SetSourceUnlimitsAnimationPlayer)
 
 TEST_F(AnimationAnimationPlayerTest, EmptyAnimationPlayersDontUpdateEffects)
 {
-    player = timeline->createAnimationPlayer(0);
+    player = timeline->play(0);
     player->update(TimingUpdateOnDemand);
     EXPECT_EQ(std::numeric_limits<double>::infinity(), player->timeToEffectChange());
 
@@ -684,7 +684,7 @@ TEST_F(AnimationAnimationPlayerTest, EmptyAnimationPlayersDontUpdateEffects)
 TEST_F(AnimationAnimationPlayerTest, AnimationPlayersDisassociateFromSource)
 {
     AnimationNode* animationNode = player->source();
-    AnimationPlayer* player2 = timeline->createAnimationPlayer(animationNode);
+    AnimationPlayer* player2 = timeline->play(animationNode);
     EXPECT_EQ(0, player->source());
     player->setSource(animationNode);
     EXPECT_EQ(0, player2->source());
@@ -697,7 +697,7 @@ TEST_F(AnimationAnimationPlayerTest, AnimationPlayersReturnTimeToNextEffect)
     timing.iterationDuration = 1;
     timing.endDelay = 1;
     RefPtrWillBeRawPtr<Animation> animation = Animation::create(0, nullptr, timing);
-    player = timeline->createAnimationPlayer(animation.get());
+    player = timeline->play(animation.get());
     player->setStartTime(0);
 
     simulateFrame(0);
@@ -798,7 +798,7 @@ TEST_F(AnimationAnimationPlayerTest, AttachedAnimationPlayers)
 
     Timing timing;
     RefPtrWillBeRawPtr<Animation> animation = Animation::create(element.get(), nullptr, timing);
-    RefPtrWillBeRawPtr<AnimationPlayer> player = timeline->createAnimationPlayer(animation.get());
+    RefPtrWillBeRawPtr<AnimationPlayer> player = timeline->play(animation.get());
     simulateFrame(0);
     timeline->serviceAnimations(TimingUpdateForAnimationFrame);
     EXPECT_EQ(1U, element->activeAnimations()->players().find(player.get())->value);
@@ -810,8 +810,8 @@ TEST_F(AnimationAnimationPlayerTest, AttachedAnimationPlayers)
 
 TEST_F(AnimationAnimationPlayerTest, HasLowerPriority)
 {
-    RefPtrWillBeRawPtr<AnimationPlayer> player1 = timeline->createAnimationPlayer(0);
-    RefPtrWillBeRawPtr<AnimationPlayer> player2 = timeline->createAnimationPlayer(0);
+    RefPtrWillBeRawPtr<AnimationPlayer> player1 = timeline->play(0);
+    RefPtrWillBeRawPtr<AnimationPlayer> player2 = timeline->play(0);
     EXPECT_TRUE(AnimationPlayer::hasLowerPriority(player1.get(), player2.get()));
 }
 
