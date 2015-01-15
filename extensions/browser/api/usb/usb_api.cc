1208f36784768e3cfe823201d497a5994f4f6f21
reillyg <reillyg@chromium.org>
2015-01-15 09:10:01 -0800

    Remove 'success' parameter from chrome.usb.setConfiguration.
    A boolean parameter indicating success is not necessary because Chrome
    APIs also set the chrome.runtime.lastError value when an error is
    encountered. That pattern is also incompatible with the ResponseAction
    interface in ExtensionFunction as it is impossible to return a result
    and an error at the same time.
    
    This API function is still dev-channel only.
    
    BUG=411715
    
    Review URL: https://codereview.chromium.org/832483003
    
    Cr-Commit-Position: refs/heads/master@{#311682}

--- extensions/browser/api/usb/usb_api.cc
+++ extensions/browser/api/usb/usb_api.cc
@@ -859,9 +859,7 @@ void UsbSetConfigurationFunction::AsyncWorkStart() {
     return;
   }
 
-  if (device_handle->SetConfiguration(parameters_->configuration_value)) {
-    SetResult(new base::FundamentalValue(true));
-  } else {
+  if (!device_handle->SetConfiguration(parameters_->configuration_value)) {
     SetError(kErrorCannotSetConfiguration);
   }
   AsyncWorkCompleted();
