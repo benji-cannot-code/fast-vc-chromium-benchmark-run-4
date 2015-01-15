0219671273512697825733d06feec56182114f6f
ginkage <ginkage@chromium.org>
2015-01-15 10:57:29 -0800

    Disable caching "Chrome Hotword Shared Module" extension (temporary).
    BUG=447077
    
    Review URL: https://codereview.chromium.org/832683004
    
    Cr-Commit-Position: refs/heads/master@{#311707}

--- chrome/browser/extensions/updater/extension_cache_impl.h
+++ chrome/browser/extensions/updater/extension_cache_impl.h
@@ -54,6 +54,9 @@ class ExtensionCacheImpl : public ExtensionCache,
   // Callback that is called when local cache is ready.
   void OnCacheInitialized();
 
+  // Check if this extension is allowed to be cached.
+  bool CachingAllowed(const std::string& id);
+
   // Cache implementation that uses local cache dir.
   scoped_ptr<LocalExtensionCache> cache_;
 
