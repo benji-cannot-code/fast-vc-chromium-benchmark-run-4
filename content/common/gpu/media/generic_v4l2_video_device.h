cb9aefab3cfaf3c95a638a6ccd254badb38363fe
posciak <posciak@chromium.org>
2015-01-15 06:06:00 -0800

    Reland: Add accelerated video decoder interface, VP8 and H.264 implementations and hook up to V4L2SVDA.
    This is a reland of https://chromiumcodereview.appspot.com/833063003/,
    which failed on some CrOS ARM bots due to the new headers not being present on
    all ARM boards. With this iteration I'm additionally gating the CL on
    use_v4lplugin, which is a workaround to further limit this to be
    compiled on relevant platforms only, previously it was supposed to be
    present on all ARMs and rely on dynamic selection of the proper codec
    class. This will be restored and fixed shortly.
    
    An AcceleratedVideoDecoder is a video decoder that requires support from an
    external accelerator (typically a hardware accelerator) to partially
    offload the decode process after parsing stream headers, and performing
    reference frame and state management.
    
    In this design, the hardware-independent decoder implementation interfaces
    with a HW-specific Accelerator to offload last stages of the decode process.
    
    Add the interface for AcceleratedVideoDecoder, decoders for VP8 and H264,
    V4L2-specific Accelerators for VP8 and H264 and a common accelerator/client
    class for V4L2.
    
    TBR=wuchengli@chromium.org,kcwu@chromium.org,owenlin@chromium.org,scherkus@chromium.org,dalecurtis@chromium.org
    R=wuchengli@chromium.org,kcwu@chromium.org,owenlin@chromium.org,scherkus@chromium.org,dalecurtis@chromium.org
    TEST=vdatest VP8/H264, mp4 local video playback, apprtc decode
    BUG=chrome-os-partner:33728
    
    Review URL: https://codereview.chromium.org/801053006
    
    Cr-Commit-Position: refs/heads/master@{#311661}

--- content/common/gpu/media/generic_v4l2_video_device.h
+++ content/common/gpu/media/generic_v4l2_video_device.h
@@ -16,7 +16,6 @@ namespace content {
 class GenericV4L2Device : public V4L2Device {
  public:
   explicit GenericV4L2Device(Type type);
-  virtual ~GenericV4L2Device();
 
   // V4L2Device implementation.
   int Ioctl(int request, void* arg) override;
@@ -44,6 +43,7 @@ class GenericV4L2Device : public V4L2Device {
   uint32 PreferredInputFormat() override;
 
  private:
+  ~GenericV4L2Device() override;
   const Type type_;
 
   // The actual device fd.
