f4fc868109f710163e4f26ff3c2b8f4ab9f02680
mstensho@opera.com <mstensho@opera.com>
2015-01-15 20:29:50 +0000

    [Slimming paint] [New Multicolumn] Repaint column rules when they change.
    And DON'T paint them when they don't change, or we'll get an assertion failure.
    
    Column rules are painted by an anonymous RenderMultiColumnSet, while
    -webkit-column-rule is specified on the parent multicol, so some extra work is
    required to properly invalidate the column set(s).
    
    BUG=449118
    R=chrishtr@chromium.org
    
    Review URL: https://codereview.chromium.org/834653003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188489 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderMultiColumnFlowThread.h
+++ third_party/WebKit/Source/core/rendering/RenderMultiColumnFlowThread.h
@@ -153,6 +153,8 @@ public:
 
     bool recalculateColumnHeights();
 
+    void columnRuleStyleDidChange();
+
 protected:
     RenderMultiColumnFlowThread();
     void setProgressionIsInline(bool isInline) { m_progressionIsInline = isInline; }
