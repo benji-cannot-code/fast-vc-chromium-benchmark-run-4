085f930fb31900b6b61006f1fef94bb5743cff05
cmumford@chromium.org <cmumford@chromium.org>
2015-01-15 02:31:42 +0000

    Indexed DB: Eliminate variables shadowing other variables.
    Address a few static analysis warnings of variables whose names shadow those in
    the outer scope. Specifically this warning:
    
      "warning C6246: Local declaration of 'tokenType' hides declaration of the same name in outer scope."
    
    Note: This change does not fix any known bugs - only warnings.
    
    Analysis run by brucedawson@
    
    Review URL: https://codereview.chromium.org/826023003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188434 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/indexeddb/IDBKeyPath.cpp
+++ third_party/WebKit/Source/modules/indexeddb/IDBKeyPath.cpp
@@ -156,7 +156,7 @@ void IDBParseKeyPath(const String& keyPath, Vector<String>& elements, IDBKeyPath
     while (1) {
         switch (state) {
         case Identifier : {
-            IDBKeyPathLexer::TokenType tokenType = lexer.currentTokenType();
+            tokenType = lexer.currentTokenType();
             ASSERT(tokenType == IDBKeyPathLexer::TokenIdentifier);
 
             String element = lexer.currentElement();
@@ -174,7 +174,7 @@ void IDBParseKeyPath(const String& keyPath, Vector<String>& elements, IDBKeyPath
             break;
         }
         case Dot: {
-            IDBKeyPathLexer::TokenType tokenType = lexer.currentTokenType();
+            tokenType = lexer.currentTokenType();
             ASSERT(tokenType == IDBKeyPathLexer::TokenDot);
 
             tokenType = lexer.nextTokenType();
