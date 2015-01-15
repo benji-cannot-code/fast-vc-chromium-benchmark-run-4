0eef4b8a44cbf9ad5ea904ab52221f4bd421dda3
skuhne <skuhne@chromium.org>
2015-01-15 07:53:09 -0800

    Turing the memory pressure based memory management on by default for ChromeOS
    BUG=448841
    TEST=covered by existing tests
    
    Review URL: https://codereview.chromium.org/851003002
    
    Cr-Commit-Position: refs/heads/master@{#311670}

--- content/browser/browser_main_loop.cc
+++ content/browser/browser_main_loop.cc
@@ -489,8 +489,8 @@ void BrowserMainLoop::MainMessageLoopStart() {
   InitializeMainThread();
 
 #if defined(OS_CHROMEOS)
-  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
-          chromeos::switches::kUseMemoryPressureSystemChromeOS)) {
+  if (!base::CommandLine::ForCurrentProcess()->HasSwitch(
+          chromeos::switches::kDisableMemoryPressureSystemChromeOS)) {
     memory_pressure_observer_.reset(new base::MemoryPressureObserverChromeOS);
   }
 #endif
