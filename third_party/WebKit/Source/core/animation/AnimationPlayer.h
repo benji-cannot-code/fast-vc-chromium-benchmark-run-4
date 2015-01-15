b190c488df4abbfe12bd8b881a1ec25415ab69bb
dstockwell@chromium.org <dstockwell@chromium.org>
2015-01-15 04:05:55 +0000

    Web Animations: Fix regression where transition end event did not fire
    After r188203[1] when a transition finished in a on-demand timing update
    the corresponding player would be cancelled before the transition end
    event was fired. This change reverts the parts of the previous patch
    where we stopped waiting for the internal finished flag to be set on the
    player, we must wait for the flag to be set to ensure that the animation
    is both finished and has had a final animation frame timing update.
    
    [1] http://src.chromium.org/viewvc/blink?revision=188203&view=revision
    
    BUG=448369
    
    Review URL: https://codereview.chromium.org/843913003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188443 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/AnimationPlayer.h
+++ third_party/WebKit/Source/core/animation/AnimationPlayer.h
@@ -98,6 +98,7 @@ public:
 
     bool playing() const { return !(playStateInternal() == Idle || limited() || m_paused || m_isPausedForTesting); }
     bool limited() const { return limited(currentTimeInternal()); }
+    bool finishedInternal() const { return m_finished; }
 
     DEFINE_ATTRIBUTE_EVENT_LISTENER(finish);
 
