cf1a060c4c5151ab8173dba511bc673ea0ad8f08
jdduke@chromium.org <jdduke@chromium.org>
2015-01-15 21:12:42 +0000

    Revert of Disable touch adjustment for mobile-optimized viewports (patchset #2 id:20001 of https://codereview.chromium.org/780223004/)
    Reason for revert:
    Disabling touch adjustment on mobile sites makes checkbox touch
    interaction exceedingly difficult.
    
    Original issue's description:
    > Disable touch adjustment for mobile-optimized viewports
    > 
    > Use the existing |WebViewImpl::shouldDisableDesktopWorkarounds()| signal
    > to suppress touch adjustment for tap-related events. When the content
    > is optimized for the device viewport, the adjustment should not be
    > necessary.
    > 
    > BUG=417534
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=186818
    
    TBR=aelias@chromium.org,rbyers@chromium.org
    BUG=417534,448532
    
    Review URL: https://codereview.chromium.org/825353009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188494 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/ChromeClientImpl.cpp
+++ third_party/WebKit/Source/web/ChromeClientImpl.cpp
@@ -804,11 +804,6 @@ void ChromeClientImpl::requestPointerUnlock()
     return m_webView->requestPointerUnlock();
 }
 
-bool ChromeClientImpl::shouldDisableDesktopWorkarounds()
-{
-    return m_webView->shouldDisableDesktopWorkarounds();
-}
-
 void ChromeClientImpl::annotatedRegionsChanged()
 {
     WebViewClient* client = m_webView->client();
