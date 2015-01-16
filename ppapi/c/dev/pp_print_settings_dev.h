009572b5d591dbdaade0125c1b1782a02f6aa981
raymes <raymes@chromium.org>
2015-01-15 19:29:21 -0800

    Remove unused fields from PP_PdfPrintPresetOptions_Dev
    Work was started which would use these fields to convey information to the
    printing system but this work was never finished (see https://code.google.com/p/chromium/issues/detail?id=169120).
    
    This fields are never initialized.
    
    BUG=448798
    
    Review URL: https://codereview.chromium.org/828233004
    
    Cr-Commit-Position: refs/heads/master@{#311831}

--- ppapi/c/dev/pp_print_settings_dev.h
+++ ppapi/c/dev/pp_print_settings_dev.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * found in the LICENSE file.
  */
 
-/* From dev/pp_print_settings_dev.idl modified Wed Jun 13 09:14:31 2012. */
+/* From dev/pp_print_settings_dev.idl modified Fri Jan 16 13:30:14 2015. */
 
 #ifndef PPAPI_C_DEV_PP_PRINT_SETTINGS_DEV_H_
 #define PPAPI_C_DEV_PP_PRINT_SETTINGS_DEV_H_
@@ -48,14 +48,6 @@ typedef enum {
   PP_PRINTSCALINGOPTION_SOURCE_SIZE = 2
 } PP_PrintScalingOption_Dev;
 PP_COMPILE_ASSERT_SIZE_IN_BYTES(PP_PrintScalingOption_Dev, 4);
-
-typedef enum {
-  PP_PRINTDUPLEXMODE_NONE = 0,
-  PP_PRINTDUPLEXMODE_SIMPLEX = 1,
-  PP_PRINTDUPLEXMODE_LONG_EDGE = 2,
-  PP_PRINTDUPLEXMODE_SHORT_EDGE = 3
-} PP_PrintDuplexMode_Dev;
-PP_COMPILE_ASSERT_SIZE_IN_BYTES(PP_PrintDuplexMode_Dev, 4);
 /**
  * @}
  */
@@ -77,12 +69,6 @@ struct PP_PrintSettings_Dev {
   PP_PrintOutputFormat_Dev format;
 };
 PP_COMPILE_ASSERT_STRUCT_SIZE_IN_BYTES(PP_PrintSettings_Dev, 60);
-
-struct PP_PrintRange_Dev {
-  int32_t from;
-  int32_t to;
-};
-PP_COMPILE_ASSERT_STRUCT_SIZE_IN_BYTES(PP_PrintRange_Dev, 8);
 /**
  * @}
  */
