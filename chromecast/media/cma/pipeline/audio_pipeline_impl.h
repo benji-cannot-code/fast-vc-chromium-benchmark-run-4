aa84a4943ed1c3570872a40957d1a42ca22757b7
halliwell <halliwell@chromium.org>
2015-01-16 09:44:28 -0800

    Fix chromium-style errors for WeakPtrFactory member order in Chromecast
    This check just got turned on upstream, see crrev.com/803603003
    
    BUG=
    
    Review URL: https://codereview.chromium.org/857573004
    
    Cr-Commit-Position: refs/heads/master@{#311920}

--- chromecast/media/cma/pipeline/audio_pipeline_impl.h
+++ chromecast/media/cma/pipeline/audio_pipeline_impl.h
@@ -68,8 +68,8 @@ class AudioPipelineImpl : public AudioPipeline {
 
   ::media::PipelineStatistics previous_stats_;
 
-  base::WeakPtrFactory<AudioPipelineImpl> weak_factory_;
   base::WeakPtr<AudioPipelineImpl> weak_this_;
+  base::WeakPtrFactory<AudioPipelineImpl> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(AudioPipelineImpl);
 };
