d1f4e3978a864a97746db36f628e53316309f1c0
boliu <boliu@chromium.org>
2015-01-16 14:03:51 -0800

    gpu: Disable EGL upload and threaded mailbox combination
    This disables EGL upload and uses idle upload everywhere in Android
    WebView.
    
    Since EGL uploader is being phased out already and this is a blocker for
    features later, just disable the EGL uploader in webview and remove the
    workaround for it in MailboxManagerSync.
    
    BUG=448168
    
    Review URL: https://codereview.chromium.org/855683002
    
    Cr-Commit-Position: refs/heads/master@{#311962}

--- gpu/command_buffer/service/async_pixel_transfer_manager_android.cc
+++ gpu/command_buffer/service/async_pixel_transfer_manager_android.cc
@@ -5,12 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "gpu/command_buffer/service/async_pixel_transfer_manager.h"
 
+#include "base/command_line.h"
 #include "base/debug/trace_event.h"
 #include "base/sys_info.h"
 #include "gpu/command_buffer/service/async_pixel_transfer_manager_egl.h"
 #include "gpu/command_buffer/service/async_pixel_transfer_manager_idle.h"
 #include "gpu/command_buffer/service/async_pixel_transfer_manager_stub.h"
 #include "gpu/command_buffer/service/async_pixel_transfer_manager_sync.h"
+#include "gpu/command_buffer/service/gpu_switches.h"
 #include "ui/gl/gl_context.h"
 #include "ui/gl/gl_implementation.h"
 
@@ -76,6 +78,13 @@ bool AllowTransferThreadForGpu() {
 AsyncPixelTransferManager* AsyncPixelTransferManager::Create(
     gfx::GLContext* context) {
   DCHECK(context->IsCurrent(NULL));
+  // Threaded mailbox uses EGLImage which conflicts with EGL uploader.
+  // The spec only allows one EGL image per sibling group, but currently the
+  // image handle cannot be shared between the threaded mailbox code and
+  // AsyncPixelTransferManagerEGL.
+  bool uses_threaded_mailboxes =
+      base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kEnableThreadedTextureMailboxes);
   switch (gfx::GetGLImplementation()) {
     case gfx::kGLImplementationEGLGLES2:
       DCHECK(context);
@@ -85,6 +94,7 @@ AsyncPixelTransferManager* AsyncPixelTransferManager::Create(
           context->HasExtension("EGL_KHR_image_base") &&
           context->HasExtension("EGL_KHR_gl_texture_2D_image") &&
           context->HasExtension("GL_OES_EGL_image") &&
+          !uses_threaded_mailboxes &&
           AllowTransferThreadForGpu()) {
         TRACE_EVENT0("gpu", "AsyncPixelTransferManager_CreateWithThread");
         return new AsyncPixelTransferManagerEGL;
