ef12413db5f6685e601fc3f0fc1367ec6a1e87f9
sky <sky@chromium.org>
2015-01-16 11:21:47 -0800

    Adds ScopedVector::push_back(scoped_ptr)
    Having a push_back(scoped_ptr) helps to make ownership clear.
    
    R=thakis@chromium.org
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/855003005
    
    Cr-Commit-Position: refs/heads/master@{#311935}

--- base/memory/scoped_vector.h
+++ base/memory/scoped_vector.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/logging.h"
+#include "base/memory/scoped_ptr.h"
 #include "base/move.h"
 #include "base/stl_util.h"
 
@@ -65,6 +66,7 @@ class ScopedVector {
   reference back() { return v_.back(); }
 
   void push_back(T* elem) { v_.push_back(elem); }
+  void push_back(scoped_ptr<T> elem) { v_.push_back(elem.release()); }
 
   void pop_back() {
     DCHECK(!empty());
