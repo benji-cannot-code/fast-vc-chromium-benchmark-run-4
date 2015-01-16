a1601df45e1ad6659575d80b41cd28b5deffd06f
sergiyb@chromium.org <sergiyb@chromium.org>
2015-01-16 12:56:57 +0000

    Removed obsolote mac_gpu builder
    BUG=426897
    R=kbr@chromium.org,phajdan.jr@chromium.org,eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/814813002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188532 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/PRESUBMIT.py
+++ third_party/WebKit/PRESUBMIT.py
@@ -360,7 +360,6 @@ def GetPreferredTryMasters(project, change):
         },
         'tryserver.chromium.gpu': {
             'linux_gpu': set(['defaulttests']),
-            'mac_gpu': set(['defaulttests']),
             'win_gpu': set(['defaulttests']),
         }
     }
