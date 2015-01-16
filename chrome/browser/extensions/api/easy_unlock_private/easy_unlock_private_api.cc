53e24e359b5eedb5081117194b30aeee306c9515
tengs <tengs@chromium.org>
2015-01-16 13:25:38 -0800

    Only allow easyUnlockPrivate.getConnectionInfo API if proper flag is switched on.
    BUG=403419
    
    Review URL: https://codereview.chromium.org/820643006
    
    Cr-Commit-Position: refs/heads/master@{#311951}

--- chrome/browser/extensions/api/easy_unlock_private/easy_unlock_private_api.cc
+++ chrome/browser/extensions/api/easy_unlock_private/easy_unlock_private_api.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/bind.h"
+#include "base/command_line.h"
 #include "base/lazy_instance.h"
 #include "base/memory/linked_ptr.h"
 #include "base/strings/utf_string_conversions.h"
@@ -20,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/extensions/api/easy_unlock_private.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/proximity_auth/bluetooth_util.h"
+#include "components/proximity_auth/switches.h"
 #include "content/public/browser/browser_thread.h"
 #include "extensions/browser/browser_context_keyed_api_factory.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -727,6 +729,13 @@ EasyUnlockPrivateGetConnectionInfoFunction::
 
 bool EasyUnlockPrivateGetConnectionInfoFunction::DoWork(
     scoped_refptr<device::BluetoothAdapter> adapter) {
+  if (!base::CommandLine::ForCurrentProcess()->HasSwitch(
+        proximity_auth::switches::kEnableProximityDetection)) {
+    SetError("Turn on 'enable-easy-unlock-proximity-detection' flag.");
+    SendResponse(false);
+    return true;
+  }
+
   scoped_ptr<easy_unlock_private::GetConnectionInfo::Params> params =
       easy_unlock_private::GetConnectionInfo::Params::Create(*args_);
   EXTENSION_FUNCTION_VALIDATE(params);
