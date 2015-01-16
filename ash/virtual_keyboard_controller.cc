1af31a02cd2ce06526a8eec8f73a95f2ad39f907
rsadam <rsadam@chromium.org>
2015-01-15 17:08:11 -0800

    Updates SmartDeploy to respect EnableVirtualKeyboard flag.
    BUG=448880
    TEST=VirtualKeyboardControllerAlwaysEnabledTest.DoesNotSuppressKeyboard
    
    Review URL: https://codereview.chromium.org/855443002
    
    Cr-Commit-Position: refs/heads/master@{#311805}

--- ash/virtual_keyboard_controller.cc
+++ ash/virtual_keyboard_controller.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/shell.h"
 #include "ash/wm/maximize_mode/maximize_mode_controller.h"
+#include "ash_switches.h"
 #include "base/command_line.h"
 #include "base/strings/string_util.h"
 #include "ui/events/devices/device_data_manager.h"
@@ -24,6 +25,10 @@ namespace {
 
 // Checks whether smart deployment is enabled.
 bool IsSmartVirtualKeyboardEnabled() {
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+          keyboard::switches::kEnableVirtualKeyboard)) {
+    return false;
+  }
   return base::CommandLine::ForCurrentProcess()->HasSwitch(
       keyboard::switches::kEnableAutoVirtualKeyboard);
 }
