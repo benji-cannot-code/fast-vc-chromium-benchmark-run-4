668e8fcbbab537fac435a304f79de8eb2dff8156
rickyz <rickyz@chromium.org>
2015-01-15 16:28:25 -0800

    Convert DropFileSystemAccess to use ForkWithFlags.
    Previously, this used a thread, but this did not work within a PID
    namespace because /proc/<tid> can refer to a completely different
    process.
    
    BUG=312380
    
    Review URL: https://codereview.chromium.org/853583002
    
    Cr-Commit-Position: refs/heads/master@{#311771}

--- sandbox/linux/services/credentials_unittest.cc
+++ sandbox/linux/services/credentials_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sys/types.h>
 #include <unistd.h>
 
+#include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/files/scoped_file.h"
 #include "base/logging.h"
@@ -22,13 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace sandbox {
 
 namespace {
-
-bool DirectoryExists(const char* path) {
-  struct stat dir;
-  errno = 0;
-  int ret = stat(path, &dir);
-  return -1 != ret || ENOENT != errno;
-}
 
 bool WorkingDirectoryIsRoot() {
   char current_dir[PATH_MAX];
@@ -142,7 +136,7 @@ SANDBOX_TEST(Credentials, DISABLE_ON_LSAN(DropFileSystemAccessIsSafe)) {
   // Probably missing kernel support.
   if (!Credentials::MoveToNewUserNS()) return;
   CHECK(Credentials::DropFileSystemAccess());
-  CHECK(!DirectoryExists("/proc"));
+  CHECK(!base::DirectoryExists(base::FilePath("/proc")));
   CHECK(WorkingDirectoryIsRoot());
   // We want the chroot to never have a subdirectory. A subdirectory
   // could allow a chroot escape.
