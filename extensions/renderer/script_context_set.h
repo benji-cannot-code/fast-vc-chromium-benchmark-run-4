002632b00df411be27703dfdee0991c93d16a0f5
kouhei <kouhei@chromium.org>
2015-01-15 21:08:49 -0800

    Revert of Debugging patch to help track down skipped cleanup of ScriptContexts. (patchset #3 id:40001 of https://codereview.chromium.org/853793002/)
    Reason for revert:
    Causing dom_perf to crash in perf bots.
    
    BUG=449415
    
    Original issue's description:
    > Debugging patch to help track down skipped cleanup of ScriptContexts.
    >
    > The current theory is that cleanup for some ScriptContexts is getting
    > skipped, due to willReleaseScriptContext() notifications not being sent.
    > Alias a few things into the debug crash dump to try to narrow down what
    > could be causing this.
    >
    > BUG=441968
    >
    > Committed: https://crrev.com/b97e8b25a46aeeecd26cba5ebba9a8b5ecf4a4e7
    > Cr-Commit-Position: refs/heads/master@{#311802}
    
    TBR=kalman@chromium.org,dcheng@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=441968
    
    Review URL: https://codereview.chromium.org/858493002
    
    Cr-Commit-Position: refs/heads/master@{#311844}

--- extensions/renderer/script_context_set.h
+++ extensions/renderer/script_context_set.h
@@ -19,10 +19,6 @@ namespace base {
 class ListValue;
 }
 
-namespace blink {
-class WebFrame;
-}
-
 namespace content {
 class RenderView;
 }
@@ -51,10 +47,6 @@ class ScriptContextSet {
   // it asynchronously. After this call returns the context object will still
   // be valid, but its frame() pointer will be cleared.
   void Remove(ScriptContext* context);
-
-  // Do not use this method. It has been temporarily added for debugging
-  // purposes (see https://crbug.com/441968).
-  void RemoveForFrame(blink::WebFrame* frame);
 
   // Returns a copy to protect against changes.
   typedef std::set<ScriptContext*> ContextSet;
