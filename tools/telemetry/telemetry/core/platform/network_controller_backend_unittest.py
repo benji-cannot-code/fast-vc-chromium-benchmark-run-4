3dca63497bcb3b0e50f6c3c972f1872592f7a65a
ariblue <ariblue@google.com>
2015-01-15 18:58:36 -0800

    LocalServerController currently has a dependency on browser_backend. We want to instead depend on platform_backend.
    To remedy this, we move the forwarder_factory property and GetRemotePort()
    function from browser_backends to the corresponding platform_backends.
    
    BUG=423962
    
    Review URL: https://codereview.chromium.org/814123004
    
    Cr-Commit-Position: refs/heads/master@{#311825}

--- tools/telemetry/telemetry/core/platform/network_controller_backend_unittest.py
+++ tools/telemetry/telemetry/core/platform/network_controller_backend_unittest.py
@@ -16,6 +16,10 @@ class FakePlatformBackend(object):
   def wpr_ca_cert_path(self):
     return None
 
+  @property
+  def forwarder_factory(self):
+    return FakeForwarderFactory()
+
 
 class FakeForwarderFactory(object):
   def __init__(self):
@@ -32,7 +36,6 @@ class FakeBrowserBackend(object):
       http=forwarders.PortPair(*http_ports),
       https=forwarders.PortPair(*https_ports),
       dns=forwarders.PortPair(*dns_ports) if dns_ports else None)
-    self.forwarder_factory = FakeForwarderFactory()
 
 
 class FakeReplayServer(object):
