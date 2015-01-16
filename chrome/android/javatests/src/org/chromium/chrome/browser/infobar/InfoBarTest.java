a5038c9aab6f9d0f50846627986a1dfc107ba6be
newt <newt@chromium.org>
2015-01-15 16:30:54 -0800

    Disable geolocation infobar tests.
    These are briefly broken because location is disabled on test devices,
    and the mechanism for mocking out system location settings was broken.
    To be fixed shortly.
    
    BUG=449341
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/849363002
    
    Cr-Commit-Position: refs/heads/master@{#311774}

--- chrome/android/javatests/src/org/chromium/chrome/browser/infobar/InfoBarTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/infobar/InfoBarTest.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.infobar;
 
 import static org.chromium.base.test.util.ScalableTimeout.scaleTimeout;
 
+import android.test.FlakyTest;
 import android.test.suitebuilder.annotation.MediumTest;
 import android.test.suitebuilder.annotation.Smoke;
 
@@ -73,9 +74,13 @@ public class InfoBarTest extends ChromeShellTestBase {
 
     /**
      * Verify Geolocation creates an InfoBar.
+     *
+     * @Smoke
+     * @MediumTest
+     *
+     * Bug: http://crbug.com/449341
      */
-    @Smoke
-    @MediumTest
+    @FlakyTest
     @Feature({"Browser", "Main"})
     public void testInfoBarForGeolocation() throws InterruptedException {
         loadUrlWithSanitization(TestHttpServerClient.getUrl(GEOLOCATION_PAGE));
@@ -97,8 +102,11 @@ public class InfoBarTest extends ChromeShellTestBase {
     /**
      * Verify Geolocation creates an InfoBar and that it's destroyed when navigating back.
      *
+     * @MediumTest
+     *
+     * Bug: http://crbug.com/449341
      */
-    @MediumTest
+    @FlakyTest
     @Feature({"Browser"})
     public void testInfoBarForGeolocationDisappearsOnBack() throws InterruptedException {
         loadUrlWithSanitization(HELLO_WORLD_URL);
