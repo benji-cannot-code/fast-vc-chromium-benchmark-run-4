da50f79a8710b03782558d086e66d7a748a1cfbc
tfarina <tfarina@chromium.org>
2015-01-16 15:26:53 -0800

    webkit: Cleanup - make everything private as much as possible.
    This helps to reduce the public surface of this API and makes clear what can
    be accessed by the clients/consumers of this API.
    
    BUG=338338
    TEST=None
    R=piman@chromium.org
    
    Review URL: https://codereview.chromium.org/858643002
    
    Cr-Commit-Position: refs/heads/master@{#311976}

--- webkit/common/gpu/context_provider_in_process.cc
+++ webkit/common/gpu/context_provider_in_process.cc
@@ -9,9 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/callback_helpers.h"
-#include "base/strings/string_split.h"
 #include "base/strings/stringprintf.h"
 #include "cc/output/managed_memory_policy.h"
+#include "gpu/blink/webgraphicscontext3d_in_process_command_buffer_impl.h"
 #include "gpu/command_buffer/client/gles2_implementation.h"
 #include "webkit/common/gpu/grcontext_for_webgraphicscontext3d.h"
 
