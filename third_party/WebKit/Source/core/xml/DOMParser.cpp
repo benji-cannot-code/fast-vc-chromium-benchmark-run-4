da72d94631f5a9c4b260ff34da1d146c009d7522
philipj@opera.com <philipj@opera.com>
2015-01-17 01:19:59 +0000

    Sync the DOMParser interface with spec
    https://dvcs.w3.org/hg/innerhtml/raw-file/tip/index.html#the-domparser-interface
    
    Review URL: https://codereview.chromium.org/855023002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188568 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/xml/DOMParser.cpp
+++ third_party/WebKit/Source/core/xml/DOMParser.cpp
@@ -26,20 +26,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-PassRefPtrWillBeRawPtr<Document> DOMParser::parseFromString(const String& str, const String& contentType, ExceptionState& exceptionState)
+PassRefPtrWillBeRawPtr<Document> DOMParser::parseFromString(const String& str, const String& type)
 {
-    // HTML5 is very explicit about which types we're allowed to support here:
-    // http://domparsing.spec.whatwg.org/#the-domparser-interface
-    if (contentType != "text/html"
-        && contentType != "text/xml"
-        && contentType != "application/xml"
-        && contentType != "application/xhtml+xml"
-        && contentType != "image/svg+xml") {
-        exceptionState.throwTypeError("Unsupported mime-type specified.");
-        return nullptr;
-    }
-
-    RefPtrWillBeRawPtr<Document> doc = DOMImplementation::createDocument(contentType, 0, KURL(), false);
+    RefPtrWillBeRawPtr<Document> doc = DOMImplementation::createDocument(type, 0, KURL(), false);
     doc->setContent(str);
     return doc.release();
 }
--- third_party/WebKit/Source/core/xml/DOMParser.h
+++ third_party/WebKit/Source/core/xml/DOMParser.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class Document;
-class ExceptionState;
 
 class DOMParser final : public RefCountedWillBeGarbageCollected<DOMParser>, public ScriptWrappable {
     DEFINE_WRAPPERTYPEINFO();
@@ -39,7 +38,7 @@ public:
         return adoptRefWillBeNoop(new DOMParser);
     }
 
-    PassRefPtrWillBeRawPtr<Document> parseFromString(const String&, const String& contentType, ExceptionState&);
+    PassRefPtrWillBeRawPtr<Document> parseFromString(const String&, const String& type);
 
     void trace(Visitor*) { }
 
