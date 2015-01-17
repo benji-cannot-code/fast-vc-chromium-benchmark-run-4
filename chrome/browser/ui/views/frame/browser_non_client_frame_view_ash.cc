fc18e7032e4944df1a4d5f5b87300e378da13e95
xdai <xdai@chromium.org>
2015-01-16 19:20:37 -0800

    Make avatar badged on the top left of the browser window in ChromeOS draggable.
    Also add two test cases:
    1) Teleport a browser window to another desktop. Avatar icon should show on the top left corner of the teleported browser window. (Test case for bug 433420)
    2) The avatar icon of the teleported browser window should be draggable.
    
    BUG=342933
    
    Review URL: https://codereview.chromium.org/843153002
    
    Cr-Commit-Position: refs/heads/master@{#312016}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_ash.cc
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_ash.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/extensions/extension_util.h"
+#include "chrome/browser/profiles/profiles_state.h"
 #include "chrome/browser/themes/theme_properties.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_commands.h"
@@ -247,7 +248,15 @@ int BrowserNonClientFrameViewAsh::NonClientHitTest(const gfx::Point& point) {
   // See if the point is actually within either of the avatar menu buttons.
   if (hit_test == HTCAPTION && avatar_button() &&
       ConvertedHitTest(this, avatar_button(), point)) {
+#if defined(OS_CHROMEOS)
+    // In ChromeOS, a browser window which has an avatar badging on the top
+    // left corner means it's a teleported browser window. We should treat the
+    // avatar as part of the browser non client frame (e.g., clicking on it
+    // allows the user to drag the browser window around.)
+    return HTCAPTION;
+#else
     return HTCLIENT;
+#endif
   }
 
   if (hit_test == HTCAPTION && new_avatar_button() &&
