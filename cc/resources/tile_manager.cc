2abd326933617e3e4e155695596163cd8853b467
tfarina <tfarina@chromium.org>
2015-01-16 19:33:37 -0800

    cc: Cleanup - remove unused private fields.
    BUG=447445
    TEST=cc_unittests
    R=danakj@chromium.org
    
    Review URL: https://codereview.chromium.org/838603003
    
    Cr-Commit-Position: refs/heads/master@{#312017}

--- cc/resources/tile_manager.cc
+++ cc/resources/tile_manager.cc
@@ -136,10 +136,8 @@ class RasterTaskImpl : public RasterTask {
 class ImageDecodeTaskImpl : public ImageDecodeTask {
  public:
   ImageDecodeTaskImpl(SkPixelRef* pixel_ref,
-                      int layer_id,
                       const base::Callback<void(bool was_canceled)>& reply)
       : pixel_ref_(skia::SharePtr(pixel_ref)),
-        layer_id_(layer_id),
         reply_(reply) {}
 
   // Overridden from Task:
@@ -163,7 +161,6 @@ class ImageDecodeTaskImpl : public ImageDecodeTask {
 
  private:
   skia::RefPtr<SkPixelRef> pixel_ref_;
-  int layer_id_;
   const base::Callback<void(bool was_canceled)> reply_;
 
   DISALLOW_COPY_AND_ASSIGN(ImageDecodeTaskImpl);
@@ -751,7 +748,6 @@ scoped_refptr<ImageDecodeTask> TileManager::CreateImageDecodeTask(
     SkPixelRef* pixel_ref) {
   return make_scoped_refptr(new ImageDecodeTaskImpl(
       pixel_ref,
-      tile->layer_id(),
       base::Bind(&TileManager::OnImageDecodeTaskCompleted,
                  base::Unretained(this),
                  tile->layer_id(),
