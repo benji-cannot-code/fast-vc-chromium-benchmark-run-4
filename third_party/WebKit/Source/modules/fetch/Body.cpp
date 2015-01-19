7e215d663b857892ac9ccfaa26baab38a9a7842a
yhirano@chromium.org <yhirano@chromium.org>
2015-01-19 08:09:59 +0000

    Add use counters for Fetch API.
    This CL adds two use counters: Fetch and FetchBodyStream. Fetch counts the
    number of fetch() function calls. FetchBodyStream is logged when .body
    attribute is accessed in Body mixin. Note that both events in SharedWorker and
    ServiceWorker are not counted.
    
    BUG=436770, 435393
    R=tyoshino
    
    Review URL: https://codereview.chromium.org/838003006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188602 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/fetch/Body.cpp
+++ third_party/WebKit/Source/modules/fetch/Body.cpp
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/fileapi/Blob.h"
 #include "core/fileapi/FileReaderLoader.h"
 #include "core/fileapi/FileReaderLoaderClient.h"
+#include "core/frame/UseCounter.h"
 #include "core/streams/UnderlyingSource.h"
 #include "modules/fetch/BodyStreamBuffer.h"
 
@@ -238,6 +239,7 @@ ScriptPromise Body::text(ScriptState* scriptState)
 
 ReadableStream* Body::body()
 {
+    UseCounter::count(executionContext(), UseCounter::FetchBodyStream);
     if (!m_streamAccessed) {
         m_streamAccessed = true;
         if (m_stream->isPulling()) {
