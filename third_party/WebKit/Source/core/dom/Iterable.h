7c8db2a5a7fb125235ad40363b3ff3c2b11dde66
jl@opera.com <jl@opera.com>
2015-01-19 16:32:04 +0000

    Drop (empty) virtual destructor from Iterable<> class
    It's not needed, and needlessly taints garbage collected sub-classes such
    that they need to be marked as "finalized".
    
    BUG=432683
    
    Review URL: https://codereview.chromium.org/848953003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188636 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Iterable.h
+++ third_party/WebKit/Source/core/dom/Iterable.h
@@ -15,8 +15,6 @@ namespace blink {
 template <typename KeyType, typename ValueType>
 class Iterable {
 public:
-    virtual ~Iterable() { }
-
     Iterator* keys(ScriptState* scriptState, ExceptionState& exceptionState)
     {
         IterationSource* source = this->startIteration(scriptState, exceptionState);
