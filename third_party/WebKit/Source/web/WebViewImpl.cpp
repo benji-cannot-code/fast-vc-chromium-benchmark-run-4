5ff3543abfccc3eda4784f955db9cfb7fc716b4a
bokan@chromium.org <bokan@chromium.org>
2015-01-19 17:42:01 +0000

    Refactor of page scale limit initialization. Part 5/5
    This patch cleans up how default page scale limits are initialized. This patch
    is part 5/5; it removes the API stubs that are no longer necessary due to the
    changes in patch 4. The combined changes can be seen in
    https://codereview.chromium.org/843623002/ and
    https://codereview.chromium.org/827423006
    
    Page scale limits in Blink are defined inside the PageScaleConstraintsSet
    class. This class has three PageScaleConstraints objects:
    
    defaultConstraints
    pageDefinedConstraints
    userAgentConstraints
    
    Each overriding the constraint above it. Prior to this patch, the default
    constraints were statically set so the minimum scale was 0.25 and maximum was
    5.0, Android's desired limits. To prevent desktop browsers from scaling below 1,
    RenderViewImpl would set the userAgentConstraints to [1,4]. This is fragmented
    and problematic if we ever want to allow pages to set constraints on desktop
    browsers.
    
    This patch does a number of things:
    
    1) Make the default constraints mutable so they can be made different for
       different platforms. Expose a method in WebView to allow the content layer to
       modify it. Move the platform-specific default values into WebPreferences in
       the content layer and out of Blink. Hook up initialization to set the
       defaults when initializing Blink. Both desktop and Android now set the
       desired limits to the defaultConstraints. For desktop, this is equivalent to
       the existing behavior since the viewport meta tag is disabled,
       pageDefinedConstraints will never be set.
    2) Rename setPageScaleFactorLimits to setPageScaleFactorLimitsOverride. This
       method is ambiguous. Since it sets the userAgentConstraints, adding
       "override" makes it clear that these will supersede other constraints. It's
       also consistent with setInitialPageScaleOverride which sets the initial scale
       on userAgentConstraints. Also removed some related methods from the WebView
       interface that are no longer needed.
    3) Since a minimum scale is now always available, remove the condition in
       PageScaleConstraintsSet::mainFrameSize that sets the frame size to the
       content width.
    4) Use -1 as the default initial scale on all platforms. Previously, this was
       used on Android so that the absence of initial scale would use min-scale and
       pages would load fully zoomed out. Since the min-scale on desktop is 1, we
       can use -1 as the initial scale as well.
    
    TBR=aelias@chromium.org,rbyers@chromium.org,dgozman@chromium.org
    BUG=413285, 437034
    
    Review URL: https://codereview.chromium.org/851543003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188643 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebViewImpl.cpp
+++ third_party/WebKit/Source/web/WebViewImpl.cpp
@@ -3197,10 +3197,6 @@ void WebViewImpl::setInitialPageScaleOverride(float initialPageScaleFactorOverri
     setUserAgentPageScaleConstraints(constraints);
 }
 
-void WebViewImpl::setPageScaleFactorLimits(float minPageScale, float maxPageScale)
-{
-}
-
 void WebViewImpl::setIgnoreViewportTagScaleLimits(bool ignore)
 {
     PageScaleConstraints constraints = m_pageScaleConstraintsSet.userAgentConstraints();
