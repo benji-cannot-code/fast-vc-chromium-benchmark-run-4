f534d15704c5ff278dfbf47dffc87f4fa2348367
raymes <raymes@chromium.org>
2015-01-19 12:56:15 -0800

    Prevent UAF of RenderFrames from GuestViewContainer
    Current the RenderFrame a GuestViewContainer is associated with can be
    accessed even after it is destroyed. We should track the destruction with
    a RenderFrameObserver and avoid accesses in that case.
    
    BUG=449574
    
    Review URL: https://codereview.chromium.org/822483007
    
    Cr-Commit-Position: refs/heads/master@{#312143}

--- extensions/renderer/guest_view/extensions_guest_view_container.cc
+++ extensions/renderer/guest_view/extensions_guest_view_container.cc
@@ -58,6 +58,9 @@ ExtensionsGuestViewContainer::AttachRequest::~AttachRequest() {
 }
 
 void ExtensionsGuestViewContainer::AttachRequest::PerformRequest() {
+  if (!container()->render_frame())
+    return;
+
   // Step 1, send the attach params to extensions/.
   container()->render_frame()->Send(
       new ExtensionHostMsg_AttachGuest(container()->render_view_routing_id(),
@@ -116,6 +119,9 @@ ExtensionsGuestViewContainer::DetachRequest::~DetachRequest() {
 }
 
 void ExtensionsGuestViewContainer::DetachRequest::PerformRequest() {
+  if (!container()->render_frame())
+    return;
+
   container()->render_frame()->DetachGuest(container()->element_instance_id());
 }
 
--- extensions/renderer/guest_view/guest_view_container.h
+++ extensions/renderer/guest_view/guest_view_container.h
@@ -6,11 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef EXTENSIONS_RENDERER_GUEST_VIEW_GUEST_VIEW_CONTAINER_H_
 #define EXTENSIONS_RENDERER_GUEST_VIEW_GUEST_VIEW_CONTAINER_H_
 
+#include "base/memory/scoped_ptr.h"
 #include "content/public/renderer/browser_plugin_delegate.h"
-#include "content/public/renderer/render_frame_observer.h"
 #include "ipc/ipc_message.h"
 
 namespace extensions {
+
+namespace {
+class RenderFrameLifetimeObserver;
+}  // namespace
 
 class GuestViewContainer : public content::BrowserPluginDelegate {
  public:
@@ -19,6 +23,8 @@ class GuestViewContainer : public content::BrowserPluginDelegate {
 
   // Queries whether GuestViewContainer is interested in the |message|.
   static bool HandlesMessage(const IPC::Message& message);
+
+  void RenderFrameDestroyed();
 
   // BrowserPluginDelegate implementation.
   void SetElementInstanceID(int element_instance_id) override;
@@ -30,7 +36,8 @@ class GuestViewContainer : public content::BrowserPluginDelegate {
  private:
   int element_instance_id_;
   const int render_view_routing_id_;
-  content::RenderFrame* const render_frame_;
+  content::RenderFrame* render_frame_;
+  scoped_ptr<RenderFrameLifetimeObserver> render_frame_lifetime_observer_;
 
   DISALLOW_COPY_AND_ASSIGN(GuestViewContainer);
 };
