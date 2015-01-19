35a4dc51856bac4a22194a88ce3bb1392dadf1cc
kouhei@chromium.org <kouhei@chromium.org>
2015-01-19 06:33:07 +0000

    Use VisitorDispatcher on TraceIfEnabled/ObjectAliveTrait
    This CL migrates TraceIfEnabled/ObjectAliveTrait to use VisitorDispatcher,
    so that they will accept InlinedGlobalMarkingVisitor.
    
    BUG=420515
    
    Review URL: https://codereview.chromium.org/855863002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188595 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/heap/Visitor.h
+++ third_party/WebKit/Source/platform/heap/Visitor.h
@@ -325,7 +325,8 @@ struct OffHeapCollectionTraceTrait;
 
 template<typename T>
 struct ObjectAliveTrait {
-    static bool isHeapObjectAlive(Visitor*, T*);
+    template<typename VisitorDispatcher>
+    static bool isHeapObjectAlive(VisitorDispatcher, T*);
 };
 
 // VisitorHelper contains common implementation of Visitor helper methods.
@@ -783,7 +784,8 @@ template<typename T, bool = NeedsAdjustAndMark<T>::value> class DefaultObjectAli
 template<typename T>
 class DefaultObjectAliveTrait<T, false> {
 public:
-    static bool isHeapObjectAlive(Visitor* visitor, T* obj)
+    template<typename VisitorDispatcher>
+    static bool isHeapObjectAlive(VisitorDispatcher visitor, T* obj)
     {
         return visitor->isMarked(obj);
     }
@@ -792,13 +794,16 @@ public:
 template<typename T>
 class DefaultObjectAliveTrait<T, true> {
 public:
-    static bool isHeapObjectAlive(Visitor* visitor, T* obj)
+    template<typename VisitorDispatcher>
+    static bool isHeapObjectAlive(VisitorDispatcher visitor, T* obj)
     {
         return obj->isHeapObjectAlive(visitor);
     }
 };
 
-template<typename T> bool ObjectAliveTrait<T>::isHeapObjectAlive(Visitor* visitor, T* obj)
+template<typename T>
+template<typename VisitorDispatcher>
+bool ObjectAliveTrait<T>::isHeapObjectAlive(VisitorDispatcher visitor, T* obj)
 {
     return DefaultObjectAliveTrait<T>::isHeapObjectAlive(visitor, obj);
 }
