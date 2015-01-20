9caf90c8bf4c4bb3ebb383d664c6307abd3e9401
timvolodine <timvolodine@chromium.org>
2015-01-20 09:21:23 -0800

    Move origin url validation check to PermissionContextBase class.
    Currently the validity check for the origin url and embedding url is performed
    within the permissions related code on a case by case basis, i.e. for each API
    separately. This patch moves the check to the base class to have a uniform
    code path for all permissions in chrome. Also added a test.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/815743003
    
    Cr-Commit-Position: refs/heads/master@{#312217}

--- chrome/browser/content_settings/permission_context_base.cc
+++ chrome/browser/content_settings/permission_context_base.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/website_settings/permission_bubble_manager.h"
 #include "chrome/common/pref_names.h"
+#include "components/content_settings/core/browser/content_settings_utils.h"
 #include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/content_settings/core/common/permission_request_id.h"
 #include "content/public/browser/browser_thread.h"
@@ -83,11 +84,24 @@ void PermissionContextBase::DecidePermission(
     const BrowserPermissionCallback& callback) {
   DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
 
+  if (!requesting_origin.is_valid() || !embedding_origin.is_valid()) {
+    DVLOG(1)
+        << "Attempt to use " << content_settings::GetTypeName(permission_type_)
+        << " from an invalid URL: " << requesting_origin
+        << "," << embedding_origin
+        << " (" << content_settings::GetTypeName(permission_type_)
+        << " is not supported in popups)";
+    NotifyPermissionSet(id, requesting_origin, embedding_origin,
+                        callback, false /* persist */, false /* granted */);
+    return;
+  }
+
   ContentSetting content_setting =
       profile_->GetHostContentSettingsMap()
           ->GetContentSettingAndMaybeUpdateLastUsage(
               requesting_origin, embedding_origin, permission_type_,
               std::string());
+
   switch (content_setting) {
     case CONTENT_SETTING_BLOCK:
       NotifyPermissionSet(id, requesting_origin, embedding_origin, callback,
