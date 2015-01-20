89958c24738c2a7c979afc5b374571f082e7501d
cjhopman <cjhopman@chromium.org>
2015-01-20 14:37:20 -0800

    Revert of Roll DomDistillerJS (patchset #4 id:60001 of https://codereview.chromium.org/803963004/)
    Reason for revert:
    Breaks tests.
    
    Original issue's description:
    > Roll DomDistillerJS
    >
    > Picked up changes:
    > 56f7579 Rewrite SimilarSiblingContentExpansion
    > 2fda06e Fix printing of logged non-ascii characters
    > 8f4713b Only strip one stack frame from JS exceptions
    > efa4dce Remove unnecessary indent from code generation.
    > 6134342 Fix TerminatingBlocksFinderTest
    > 1f237e8 Make TerminatingBlocksFinder use a RegExp
    > 80a34cf Change LogUtil.LOG_BUILDER to a String
    > 9b2cdb2 Move most tests from test/ to jstest/
    > 169cb83 Add JavaScript.parseFloat and parseInt
    > 22cebe8 Change how TextBlock nodes are tracked
    > 9be60c8 Add correct imports for generated JsTestEntryImpl
    > 6b17bca Convert uses of emulated Pattern/Matcher to RegExp
    > c6bbc1e Add StringUtil.countWords and simplify TextBlockBuilder
    > 7e2865e Simplify TextBlockBuilder
    > c73a770 Split the handling of TextNodes out of BHTMLCH
    > 8fad736 Make test debugging more better
    > 3e1e81e Handle equally scored candidate lead images.
    > a7ec3c8 Make JsTestEntry report correct success result
    > d2213f3 Remove unused TextBlock statistics
    > 755df35 Add some more fancy gwt flags
    > 6380f5c Add ElementAction that simplifies TagAction+labelStack
    > 870b65e Move ignoring elements into FilteringDomVisitor.
    > 48aaaa2 Use JUnit-style null assertions in OpenGraphProtocolParserTest
    > 4ba963b querySelectorAll usage in parseMetaTags is covered in jstests
    > 6bb3e26 Add DomDistillerJsTestCase
    > 3f5efb3 Flesh out jstest environment
    > ef708c4 Use bash in install-build-deps.sh
    > 9ef7a04 Add protobuf and xvfb packages to build deps.
    > 33b9a04 Implement rest of the Assert interface
    > d75ad6b Correct function name
    > 1d72a60 Scope repeated value for JSON converter.
    > e482386 Fix install-build-deps.sh for missed package
    > da40e84 Make jstests not fail the build
    > 0d6b80e Add automated driver for jstests
    > 92ae3ba Add better stack traces for jstests
    > a97c624 Modular heuristics for images
    > 3bf867e Add framework for running tests in Chrome
    > cea631f Add .clang-format file to enable git cl format
    > fc23a51 Make image source absolute.
    > 486fd14 Use java 7 and closure compiler
    > 0dbffc4 Fix TextDocumentConstruction expectations
    > 05c76c0 Remove gwt-2.5.1 and gwt_exporter
    > ed4df25 Roll gwt to 2.7.0 (from 2.5.1)
    > 6dc14d3 Optimize SchemaOrgParser.parse for speed
    > d7c6805 Optimize OpenGraphProtocolParser.parseMetaTags for speed
    > 64df32e Add fine-grained speed profiling inside MarkupParser
    > cd97d4f Remove a bunch of unused stuff
    > bf5347c Refactor handling of labels and tagactions
    > 03cbda8 Rewrite DomWalker.walk
    > ee46df0 Create a simple reader mode extension
    >
    > NOTRY=true
    > BUG=431067,435264,435371,440102,440977
    >
    > Committed: https://crrev.com/9edb8e0c4856aad4277f4bdb6889f1514c77cd8b
    > Cr-Commit-Position: refs/heads/master@{#312288}
    
    TBR=nyquist@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=431067,435264,435371,440102,440977
    
    Review URL: https://codereview.chromium.org/844043004
    
    Cr-Commit-Position: refs/heads/master@{#312289}

--- components/dom_distiller/core/javascript/domdistiller.js
+++ components/dom_distiller/core/javascript/domdistiller.js
@@ -15,15 +15,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       // This include will be processed at build time by grit.
       <include src="../../../../third_party/dom_distiller_js/package/js/domdistiller.js"/>
     }
-    var context = Object.create(window);
-    context.setTimeout = function() {};
-    context.clearTimeout = function() {};
+    context = Object.create(window);
     initialize(context);
 
     // The OPTIONS placeholder will be replaced with the DomDistillerOptions at
     // runtime.
-    var distiller = context.com.dom_distiller.client.DomDistiller;
-    var res = distiller.applyWithOptions($$OPTIONS);
+    res = context.com.dom_distiller.DomDistiller.applyWithOptions($$OPTIONS);
     return res;
   } catch (e) {
     window.console.error("Error during distillation: " + e);
