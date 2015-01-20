c7bcfb8cbf15a6eda94fe1ac84557fd3e679fec3
timurrrr <timurrrr@chromium.org>
2015-01-20 02:50:40 -0800

    IWYU: Add a missing <vector> include
    BUG=449076
    TBR=nona@chromium.org,shuchen@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/860773002
    
    Cr-Commit-Position: refs/heads/master@{#312176}

--- ui/base/ime/chromeos/ime_bridge.h
+++ ui/base/ime/chromeos/ime_bridge.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define UI_BASE_IME_CHROMEOS_IME_BRIDGE_H_
 
 #include <string>
+#include <vector>
 #include "base/basictypes.h"
 #include "base/callback.h"
 #include "base/strings/string16.h"
