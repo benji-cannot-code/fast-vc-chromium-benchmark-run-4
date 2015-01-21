ebe834f176fe8ecc0637dc92166e3bc913955167
rnephew <rnephew@chromium.org>
2015-01-21 13:26:37 -0800

    Add Instrumentation test support to remote device
    This is the minimum to trigger/collect instrumentation tests.
    There is still more to be done before this is working 100%.
    
    BUG=448627
    
    Review URL: https://codereview.chromium.org/832493005
    
    Cr-Commit-Position: refs/heads/master@{#312457}

--- build/android/pylib/instrumentation/instrumentation_test_instance.py
+++ build/android/pylib/instrumentation/instrumentation_test_instance.py
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import logging
 import os
 import pickle
+import re
 import sys
 
 from pylib import cmd_helper
@@ -25,6 +26,7 @@ import unittest_util
 _DEFAULT_ANNOTATIONS = [
     'Smoke', 'SmallTest', 'MediumTest', 'LargeTest',
     'EnormousTest', 'IntegrationTest']
+_NATIVE_CRASH_RE = re.compile('native crash', re.IGNORECASE)
 _PICKLE_FORMAT_VERSION = 10
 
 
@@ -271,6 +273,10 @@ class InstrumentationTestInstance(test_instance.TestInstance):
         self._flags.extend([flag for flag in stripped_lines if flag])
 
   @property
+  def suite(self):
+    return 'instrumentation'
+
+  @property
   def apk_under_test(self):
     return self._apk_under_test
 
@@ -458,6 +464,33 @@ class InstrumentationTestInstance(test_instance.TestInstance):
             'annotations': a,
         })
     return inflated_tests
+
+  @staticmethod
+  def GenerateMultiTestResult(errors, statuses):
+    INSTR_STATUS_CODE_START = 1
+    results = []
+    skip_counter = 1
+    for status_code, bundle in statuses:
+      if status_code != INSTR_STATUS_CODE_START:
+        # TODO(rnephew): Make skipped tests still output test name. This is only
+        # there to give skipped tests a unique name so they are counted
+        if 'test_skipped' in bundle:
+          test_name = str(skip_counter)
+          skip_counter += 1
+        else:
+          test_name = '%s#%s' % (
+              ''.join(bundle.get('class', [''])),
+              ''.join(bundle.get('test', [''])))
+
+        results.append(
+            GenerateTestResult(test_name, [(status_code, bundle)], 0, 0))
+    for error in errors:
+      if _NATIVE_CRASH_RE.search(error):
+        results.append(
+            base_test_result.BaseTestResult(
+            'Crash detected', base_test_result.ResultType.CRASH))
+
+    return results
 
   @staticmethod
   def ParseAmInstrumentRawOutput(raw_output):
