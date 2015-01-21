e4f497a6b1ae464e9803e44a8cb48c4066e39ed1
hiroshige@chromium.org <hiroshige@chromium.org>
2015-01-21 09:36:06 +0000

    [Fetch] Add layout tests for filtered response
    BUG=444493
    
    Review URL: https://codereview.chromium.org/817113005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188727 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/fetch/resources/filtered-response.php
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+// Headers filtered in 'basic filtered response'
+header('Set-Cookie: cookie3=test-cookie');
+header('Set-Cookie2: cookie4=test-cookie2');
+
+// Headers NOT filtered in 'CORS filtered response'
+header('Cache-Control: private, no-store, no-cache, must-revalidate');
+header('Content-Language: test-content-language');
+header('Content-Type: test-content-type');
+header('Expires: test-expires');
+header('Last-Modified: test-last-modified');
+header('Pragma: test-pragma');
+if (isset($_GET['ACEHeaders']))
+    header("Access-Control-Expose-Headers: {$_GET['ACEHeaders']}");
+
+// Other headers
+header('X-test: test-x-test');
+header('X-test2: test-x-test2');
+
+header('Access-Control-Allow-Origin: *');
+
+echo "Success.";
+?>
