78f30248fcc689c02bacbc0755c47892ec0c7b32
xiaolingbao <xiaolingbao@chromium.org>
2015-01-21 13:29:09 -0800

    Make elevation icon size correct in different DPI settings
    BUG=449731
    
    Review URL: https://codereview.chromium.org/849543003
    
    Cr-Commit-Position: refs/heads/master@{#312458}

--- chrome/browser/ui/views/elevation_icon_setter.cc
+++ chrome/browser/ui/views/elevation_icon_setter.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/win_util.h"
 #include "base/win/windows_version.h"
 #include "ui/gfx/icon_util.h"
+#include "ui/gfx/win/dpi.h"
 #endif
 
 
@@ -72,8 +73,15 @@ ElevationIconSetter::~ElevationIconSetter() {
 
 void ElevationIconSetter::SetButtonIcon(scoped_ptr<SkBitmap> icon) {
   if (icon) {
-    button_->SetImage(views::Button::STATE_NORMAL,
-                      gfx::ImageSkia::CreateFrom1xBitmap(*icon));
+    float device_scale_factor = 1.0f;
+#if defined(OS_WIN)
+    // Windows gives us back a correctly-scaled image for the current DPI, so
+    // mark this image as having been scaled for the current DPI already.
+    device_scale_factor = gfx::GetDPIScale();
+#endif
+    button_->SetImage(
+        views::Button::STATE_NORMAL,
+        gfx::ImageSkia(gfx::ImageSkiaRep(*icon, device_scale_factor)));
     button_->SizeToPreferredSize();
     if (button_->parent())
       button_->parent()->Layout();
