3bcf89e8e5fad525dd6f67e3e961370ae5049219
lushnikov@chromium.org <lushnikov@chromium.org>
2015-01-21 17:27:22 +0000

    DevTools: do not override anchor clicks in StylesSidebarPane
    StylesSidebarPane should not consume clicks over linkified anchors.
    
    BUG=449099
    R=apavlov
    
    Review URL: https://codereview.chromium.org/862933003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188756 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/elements/StylesSidebarPane.js
+++ third_party/WebKit/Source/devtools/front_end/elements/StylesSidebarPane.js
@@ -1939,6 +1939,9 @@ WebInspector.StylePropertiesSection.prototype = {
         if (this._checkWillCancelEditing())
             return;
 
+        if (event.target.enclosingNodeOrSelfWithNodeName("a"))
+            return;
+
         if (event.target.classList.contains("header") || this.element.classList.contains("read-only") || event.target.enclosingNodeOrSelfWithClass("media")) {
             event.consume();
             return;
