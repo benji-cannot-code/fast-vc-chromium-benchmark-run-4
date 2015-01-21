8dd9c48103fb9af5b3268616cb6a87721dccd06a
pneubeck <pneubeck@chromium.org>
2015-01-21 06:14:35 -0800

    Enable NetworkConnectionHandler and AutoConnectHandler unittests in debug.
    Speed improvements by using smaller certs/keys and probably because of the simpler pem/pk8 files instead of the pkcs12 should make these tests again fast enough for debug bots.
    
    While there, replace usage of ScopedTestNSSChromeOSUser by the simpler ScopedTestNSSDB and remove the dependency on nss_util_internal.h.
    
    BUG=418369
    
    Review URL: https://codereview.chromium.org/840163004
    
    Cr-Commit-Position: refs/heads/master@{#312349}

--- chromeos/network/auto_connect_handler_unittest.cc
+++ chromeos/network/auto_connect_handler_unittest.cc
@@ -38,9 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/cros_system_api/dbus/service_constants.h"
 
-// http://crbug.com/418369
-#ifdef NDEBUG
-
 namespace chromeos {
 
 namespace {
@@ -472,5 +469,3 @@ TEST_F(AutoConnectHandlerTest, ManualConnectAbortsReconnectAfterLogin) {
 }
 
 }  // namespace chromeos
-
-#endif
