afa75334aaa32baa0ee703902a65dd8d40830476
dalecurtis <dalecurtis@chromium.org>
2015-01-21 03:09:24 -0800

    Avoid double task trampoline for Pipeline state transitions.
    Previously the initialization of ChunkDemuxer and Seeking would
    result in a back to back trampoline of callback tasks, this can
    lead to odd interleaving of error states when multiple threads
    are involved.
    
    In recent history we've stated it's always the callees job to
    ensure callbacks are run on the right thread, this change further
    clarifies the contract for Renderers and Demuxers that the callback
    given to Initialize() must only be run after Initialize() has
    returned.
    
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/827013005
    
    Cr-Commit-Position: refs/heads/master@{#312313}

--- media/filters/renderer_impl.cc
+++ media/filters/renderer_impl.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/single_thread_task_runner.h"
 #include "media/base/audio_renderer.h"
+#include "media/base/bind_to_current_loop.h"
 #include "media/base/demuxer_stream_provider.h"
 #include "media/base/time_source.h"
 #include "media/base/video_renderer.h"
@@ -78,8 +79,8 @@ void RendererImpl::Initialize(DemuxerStreamProvider* demuxer_stream_provider,
   paint_cb_ = paint_cb;
   ended_cb_ = ended_cb;
   error_cb_ = error_cb;
-
   init_cb_ = init_cb;
+
   state_ = STATE_INITIALIZING;
   InitializeAudioRenderer();
 }
