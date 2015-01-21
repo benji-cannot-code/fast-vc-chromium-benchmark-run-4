9f82b56e29646e60a68ee18d26eb85b396284c7c
hirono <hirono@chromium.org>
2015-01-21 06:04:27 -0800

    Files.app: Track the hidden property instead of disabled property in PreviewPanelModel.
    Previously the model refers the disabled property of commands. But for
    cloud-import command, we need to show disabled but not hidden command UI.
    
    BUG=438947
    TEST=FileManagerJsTest.PreviewPanelModelTest
    
    Review URL: https://codereview.chromium.org/853843002
    
    Cr-Commit-Position: refs/heads/master@{#312337}

--- ui/file_manager/file_manager/foreground/js/preview_panel_model.js
+++ ui/file_manager/file_manager/foreground/js/preview_panel_model.js
@@ -5,13 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /**
  * @param {PreviewPanelModel.VisibilityType} initialVisibilityType
- * @param {!Array<!cr.ui.Command>} autoVisibilityCommands
  * @extends {cr.EventTarget}
  * @constructor
  * @struct
  * @suppress {checkStructDictInheritance}
  */
-function PreviewPanelModel(initialVisibilityType, autoVisibilityCommands) {
+function PreviewPanelModel(initialVisibilityType) {
   cr.EventTarget.call(this);
 
   /**
@@ -25,7 +24,9 @@ function PreviewPanelModel(initialVisibilityType, autoVisibilityCommands) {
    * @const
    * @private
    */
-  this.autoVisibilityCommands_ = autoVisibilityCommands;
+  this.cloudImportCommands_ = Array.prototype.slice.call(
+      document.querySelectorAll('command.cloud-import'));
+  cr.ui.decorate('command.cloud-import', cr.ui.Command);
 
   /**
    * FileSelection to be displayed.
@@ -42,11 +43,10 @@ function PreviewPanelModel(initialVisibilityType, autoVisibilityCommands) {
    */
   this.visible = false;
 
-  for (var i = 0; i < this.autoVisibilityCommands_.length; i++) {
-    this.autoVisibilityCommands_[i].addEventListener(
-        'disabledChange', this.updateVisibility_.bind(this));
+  for (var i = 0; i < this.cloudImportCommands_.length; i++) {
+    this.cloudImportCommands_[i].addEventListener(
+        'hiddenChange', this.updateVisibility_.bind(this));
   }
-
   this.updateVisibility_();
 }
 
@@ -93,8 +93,8 @@ PreviewPanelModel.prototype.updateVisibility_ = function() {
     case PreviewPanelModel.VisibilityType.AUTO:
       newVisible =
           this.selection_.entries.length !== 0 ||
-          this.autoVisibilityCommands_.some(function(command) {
-            return !command.disabled;
+          this.cloudImportCommands_.some(function(command) {
+            return !command.hidden;
           });
       break;
     case PreviewPanelModel.VisibilityType.ALWAYS_HIDDEN:
