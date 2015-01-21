6fb929126f542fc4a6d5cfea36b54e02d1cc8189
jungjik.lee <jungjik.lee@samsung.com>
2015-01-21 06:06:16 -0800

    cc: refactor of MathUtil::AddToTracedValue
    Pass the name to MathUtilAddToTracedValue
    and decide the json value type internally.
    so that we could add the value without knowing
    the value type.
    
    Review URL: https://codereview.chromium.org/853393002
    
    Cr-Commit-Position: refs/heads/master@{#312339}

--- cc/trees/layer_tree_host_impl.cc
+++ cc/trees/layer_tree_host_impl.cc
@@ -3299,9 +3299,8 @@ void LayerTreeHostImpl::AsValueWithFrameInto(
     ActivationStateAsValueInto(state);
     state->EndDictionary();
   }
-  state->BeginDictionary("device_viewport_size");
-  MathUtil::AddToTracedValue(device_viewport_size_, state);
-  state->EndDictionary();
+  MathUtil::AddToTracedValue("device_viewport_size", device_viewport_size_,
+                             state);
 
   std::set<const Tile*> tiles;
   active_tree_->GetAllTilesForTracing(&tiles);
