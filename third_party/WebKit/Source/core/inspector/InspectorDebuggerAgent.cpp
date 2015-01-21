f4cc5883f16e20c9aec64263deeefb42b6d230aa
sigbjornf@opera.com <sigbjornf@opera.com>
2015-01-21 13:48:48 +0000

    Oilpan: fix build after r188733.
    TBR=oilpan-reviews
    BUG=439376
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/863953002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188739 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorDebuggerAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorDebuggerAgent.cpp
@@ -1548,7 +1548,7 @@ void InspectorDebuggerAgent::trace(Visitor* visitor)
     visitor->trace(m_listener);
     visitor->trace(m_v8AsyncCallTracker);
     visitor->trace(m_promiseTracker);
-    visitor->trace(m_asyncOperationsForStepInto);
+    visitor->trace(m_asyncOperations);
     visitor->trace(m_currentAsyncCallChain);
     visitor->trace(m_asyncCallTrackingListeners);
 #endif
