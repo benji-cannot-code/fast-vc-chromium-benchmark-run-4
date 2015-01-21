bdd042ceb8cf818e1d818f72f28408cdb0cb5710
mustaq <mustaq@chromium.org>
2015-01-21 12:48:50 -0800

    Fixed ui::TouchEvent rotation angle out-of-bound issue.
    The rotation angle in ui::TouchEvent should lie within 0 and 90 as per
    the spec:
      https://dvcs.w3.org/hg/webevents/raw-file/default/touchevents.html
    We have seen cases where the angle we got from hardware is not within
    the limits. This CL keeps the angle value sane.
    
    BUG=438256
    
    Review URL: https://codereview.chromium.org/800163005
    
    Cr-Commit-Position: refs/heads/master@{#312447}

--- ui/events/event.cc
+++ ui/events/event.cc
@@ -536,6 +536,7 @@ TouchEvent::TouchEvent(const base::NativeEvent& native_event)
       1);
 
   latency()->AddLatencyNumber(INPUT_EVENT_LATENCY_UI_COMPONENT, 0, 0);
+  fixRotationAngle();
 
   if (type() == ET_TOUCH_PRESSED)
     IncrementTouchIdRefCount(native_event);
@@ -574,6 +575,7 @@ TouchEvent::TouchEvent(EventType type,
       force_(force),
       may_cause_scrolling_(false) {
   latency()->AddLatencyNumber(INPUT_EVENT_LATENCY_UI_COMPONENT, 0, 0);
+  fixRotationAngle();
 }
 
 TouchEvent::~TouchEvent() {
@@ -600,6 +602,13 @@ void TouchEvent::DisableSynchronousHandling() {
   DispatcherApi dispatcher_api(this);
   dispatcher_api.set_result(
       static_cast<EventResult>(result() | ER_DISABLE_SYNC_HANDLING));
+}
+
+void TouchEvent::fixRotationAngle() {
+  while (rotation_angle_ < 0)
+    rotation_angle_ += 180;
+  while (rotation_angle_ >= 180)
+    rotation_angle_ -= 180;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
