082352655e24a60e705b81ccb86c65f3ac773310
asvitkine <asvitkine@chromium.org>
2015-01-21 12:50:37 -0800

    Optimize MetricsDetails classes to avoid a lot of redundant work.
    Prior to this change, every time MetricsDetails is used, the code
    would collect memory usage information from all running browsers,
    even if the intention was to just get information about Chrome (e.g.
    when logging Memory.* histograms).
    
    On some platforms, like Mac, this was particularly expensive as
    getting information about other processes involves running PS or
    TOP as a sub-process. This would happen for all users opted-in
    to UMA reporting shortly after start up and then periodically.
    
    This CL adds a mode param to the implementation that specifies
    whether the client is interested in metrics about all browsers or
    just Chrome processes.
    
    Also, this CL modernizes some of the related code - such as
    using C++11 for loops and moving functions to the anonymous
    namespace.
    
    BUG=448834
    
    Review URL: https://codereview.chromium.org/857063002
    
    Cr-Commit-Position: refs/heads/master@{#312449}

--- chrome/browser/feedback/system_logs/log_sources/memory_details_log_source.cc
+++ chrome/browser/feedback/system_logs/log_sources/memory_details_log_source.cc
@@ -46,7 +46,7 @@ void MemoryDetailsLogSource::Fetch(const SysLogsSourceCallback& callback) {
 
   scoped_refptr<SystemLogsMemoryHandler>
       handler(new SystemLogsMemoryHandler(callback));
-  handler->StartFetch();
+  handler->StartFetch(MemoryDetails::FROM_CHROME_ONLY);
 }
 
 }  // namespace system_logs
--- chrome/browser/memory_details_android.cc
+++ chrome/browser/memory_details_android.cc
@@ -105,6 +105,7 @@ ProcessData* MemoryDetails::ChromeBrowser() {
 }
 
 void MemoryDetails::CollectProcessData(
+    CollectionMode mode,
     const std::vector<ProcessMemoryInformation>& chrome_processes) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::FILE));
 
