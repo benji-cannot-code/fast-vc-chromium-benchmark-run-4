b1d448fb3dc77ee4a439c714f6f0642fcb5082b2
newt <newt@chromium.org>
2015-01-22 14:28:36 -0800

    Fix accessibility for Switches.
    TalkBack doesn't recognize the SwitchCompat class, so it reads events
    with that classname as "Checkbox". This works around the bug by marking
    accessibility events from SwitchCompat with the Switch class name, which
    TalkBack recognizes.
    
    BUG=441702
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/868803004
    
    Cr-Commit-Position: refs/heads/master@{#312690}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/HomepagePreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/HomepagePreferences.java
@@ -7,7 +7,6 @@ package org.chromium.chrome.browser.preferences;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v7.widget.SwitchCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -24,6 +23,7 @@ import org.chromium.chrome.R;
 import org.chromium.chrome.browser.UrlUtilities;
 import org.chromium.chrome.browser.partnercustomizations.HomepageManager;
 import org.chromium.chrome.browser.partnercustomizations.PartnerBrowserCustomizations;
+import org.chromium.chrome.browser.widget.ChromeSwitchCompat;
 import org.chromium.ui.UiUtils;
 
 /**
@@ -31,7 +31,7 @@ import org.chromium.ui.UiUtils;
  */
 public class HomepagePreferences extends Fragment {
     private HomepageManager mHomepageManager;
-    private SwitchCompat mHomepageSwitch;
+    private ChromeSwitchCompat mHomepageSwitch;
     private TextView mHomepageSwitchLabel;
     private String mCustomUriCache;
     private EditText mCustomUriEditText;
@@ -64,7 +64,7 @@ public class HomepagePreferences extends Fragment {
     }
 
     private void initHomepageSwitch(View rootView) {
-        mHomepageSwitch = (SwitchCompat) rootView.findViewById(R.id.homepage_switch);
+        mHomepageSwitch = (ChromeSwitchCompat) rootView.findViewById(R.id.homepage_switch);
         mHomepageSwitchLabel = (TextView) rootView.findViewById(R.id.homepage_switch_label);
 
         boolean isHomepageEnabled = mHomepageManager.getPrefHomepageEnabled();
