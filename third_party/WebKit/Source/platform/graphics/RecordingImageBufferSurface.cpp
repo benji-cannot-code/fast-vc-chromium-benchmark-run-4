4a211e2b0d97221fa7bbb9f379a5ff3a9b2b8e32
junov@chromium.org <junov@chromium.org>
2015-01-22 19:21:52 +0000

    Optimizing overdraw in display list 2d canvas
    This change aims to reset the recording for the current frame a draw
    operation is encounters the overwrites the entire frame.  This change
    also takes care of removing the RegionTracker class, which was now
    only being used by display list canvas and was overkill for the
    purposes of the display list canvas implementation. The region
    tracker was replaced by much simpler clear notification in the
    GraphicsContext class.
    
    BUG=446256
    TEST=GraphicsContext.* platform unit tests
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=188782
    
    Review URL: https://codereview.chromium.org/840743005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188827 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/RecordingImageBufferSurface.cpp
+++ third_party/WebKit/Source/platform/graphics/RecordingImageBufferSurface.cpp
@@ -21,6 +21,7 @@ RecordingImageBufferSurface::RecordingImageBufferSurface(const IntSize& size, Pa
     : ImageBufferSurface(size, opacityMode)
     , m_imageBuffer(0)
     , m_frameWasCleared(true)
+    , m_didRecordDrawCommandsInCurrentFrame(false)
     , m_fallbackFactory(fallbackFactory)
 {
     initializeCurrentFrame();
@@ -36,9 +37,8 @@ bool RecordingImageBufferSurface::initializeCurrentFrame()
     m_currentFrame->beginRecording(size().width(), size().height(), &rTreeFactory);
     if (m_imageBuffer) {
         m_imageBuffer->resetCanvas(m_currentFrame->getRecordingCanvas());
-        m_imageBuffer->context()->setRegionTrackingMode(GraphicsContext::RegionTrackingOverwrite);
     }
-
+    m_didRecordDrawCommandsInCurrentFrame = false;
     return true;
 }
 
@@ -46,12 +46,12 @@ void RecordingImageBufferSurface::setImageBuffer(ImageBuffer* imageBuffer)
 {
     m_imageBuffer = imageBuffer;
     if (m_currentFrame && m_imageBuffer) {
-        m_imageBuffer->context()->setRegionTrackingMode(GraphicsContext::RegionTrackingOverwrite);
+        m_imageBuffer->context()->setClient(this);
         m_imageBuffer->resetCanvas(m_currentFrame->getRecordingCanvas());
     }
     if (m_fallbackSurface) {
         m_fallbackSurface->setImageBuffer(imageBuffer);
-        m_imageBuffer->context()->setRegionTrackingMode(GraphicsContext::RegionTrackingDisabled);
+        m_imageBuffer->context()->setClient(0);
     }
 }
 
@@ -85,7 +85,7 @@ void RecordingImageBufferSurface::fallBackToRasterCanvas()
     }
 
     if (m_imageBuffer) {
-        m_imageBuffer->context()->setRegionTrackingMode(GraphicsContext::RegionTrackingDisabled);
+        m_imageBuffer->context()->setClient(0);
         m_imageBuffer->resetCanvas(m_fallbackSurface->canvas());
         m_imageBuffer->context()->setAccelerated(m_fallbackSurface->isAccelerated());
     }
@@ -136,13 +136,20 @@ void RecordingImageBufferSurface::finalizeFrame(const FloatRect &dirtyRect)
         fallBackToRasterCanvas();
 }
 
-void RecordingImageBufferSurface::didClearCanvas()
+void RecordingImageBufferSurface::willOverwriteCanvas()
 {
-    if (m_fallbackSurface) {
-        m_fallbackSurface->didClearCanvas();
-        return;
-    }
     m_frameWasCleared = true;
+    m_previousFrame.clear();
+    if (m_didRecordDrawCommandsInCurrentFrame) {
+        // Discard previous draw commands
+        m_currentFrame->endRecording();
+        initializeCurrentFrame();
+    }
+}
+
+void RecordingImageBufferSurface::didDraw()
+{
+    m_didRecordDrawCommandsInCurrentFrame = true;
 }
 
 bool RecordingImageBufferSurface::finalizeFrameInternal()
@@ -158,8 +165,7 @@ bool RecordingImageBufferSurface::finalizeFrameInternal()
         return m_currentFrame;
     }
 
-    IntRect canvasRect(IntPoint(0, 0), size());
-    if (!m_frameWasCleared && !m_imageBuffer->context()->opaqueRegion().asRect().contains(canvasRect)) {
+    if (!m_frameWasCleared) {
         return false;
     }
 
--- third_party/WebKit/Source/web/WebScrollbarThemePainter.cpp
+++ third_party/WebKit/Source/web/WebScrollbarThemePainter.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/scroll/Scrollbar.h"
 #include "platform/scroll/ScrollbarTheme.h"
 #include "public/platform/WebRect.h"
+#include "third_party/skia/include/core/SkCanvas.h"
 
 namespace blink {
 
