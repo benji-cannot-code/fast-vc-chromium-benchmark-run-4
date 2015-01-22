15262d54af518ef06d5fb148934e2dc49fb3f8ab
nhiroki@chromium.org <nhiroki@chromium.org>
2015-01-22 08:16:11 +0000

    ServiceWorker: Remove unused scope() from ServiceWorker (Blink)
    The scope URL should be referred to via ServiceWorkerRegistration.
    
    Chromium-side: https://codereview.chromium.org/863163002
    
    
    BUG=n/a
    TEST=run-webkit-tests http/tests/serviceworker/
    
    Review URL: https://codereview.chromium.org/865793002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188796 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebServiceWorker.h
+++ third_party/WebKit/public/platform/WebServiceWorker.h
@@ -59,7 +59,6 @@ public:
     // not ready for state change notifications.
     virtual void proxyReadyChanged() { }
 
-    virtual WebURL scope() const { return WebURL(); }
     virtual WebURL url() const { return WebURL(); }
     virtual WebServiceWorkerState state() const { return WebServiceWorkerStateUnknown; }
 
