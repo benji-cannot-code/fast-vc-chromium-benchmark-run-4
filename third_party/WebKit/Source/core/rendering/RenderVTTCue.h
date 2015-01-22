defe7802cf6a859372d88aee46cd38662c497fcf
fs@opera.com <fs@opera.com>
2015-01-22 18:47:38 +0000

    Update spec. step numbering in RenderVTTCue
    Update spec steps from a later draft. No code-changes except for the
    rounding added in steps 6-7 and, moving the containingBlock() call in
    initializeLayoutParameters() and renaming of m_defaultPosition to
    m_specifiedPosition and some variable-scoping changes in
    repositionCueSnapToLinesSet.
    
    BUG=301580
    
    Review URL: https://codereview.chromium.org/862933004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188823 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderVTTCue.h
+++ third_party/WebKit/Source/core/rendering/RenderVTTCue.h
@@ -57,7 +57,7 @@ private:
     void repositionCueSnapToLinesNotSet();
 
     RawPtrWillBeMember<VTTCue> m_cue;
-    LayoutPoint m_defaultPosition;
+    LayoutPoint m_specifiedPosition;
 };
 
 } // namespace blink
