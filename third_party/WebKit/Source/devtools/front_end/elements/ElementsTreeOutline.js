8a127bc76cf30a681f88af857089f394242a4a84
lushnikov@chromium.org <lushnikov@chromium.org>
2015-01-22 16:11:58 +0000

    DevTools: hide image preview popover on tree outline reset
    BUG=439923
    R=pfeldman
    
    Review URL: https://codereview.chromium.org/864303002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188819 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/elements/ElementsTreeOutline.js
+++ third_party/WebKit/Source/devtools/front_end/elements/ElementsTreeOutline.js
@@ -1137,6 +1137,14 @@ WebInspector.ElementsTreeOutline.prototype = {
         effectiveNode.resolveToObject("", resolvedNode);
     },
 
+    _reset: function()
+    {
+        this.rootDOMNode = null;
+        this.selectDOMNode(null, false);
+        this._popoverHelper.hidePopover();
+        delete this._clipboardNodeData;
+    },
+
     __proto__: TreeOutline.prototype
 }
 
@@ -1505,13 +1513,11 @@ WebInspector.ElementsTreeUpdater.prototype = {
 
     _reset: function()
     {
-        this._treeOutline.rootDOMNode = null;
-        this._treeOutline.selectDOMNode(null, false);
+        this._treeOutline._reset();
         this._domModel.hideDOMNodeHighlight();
         this._recentlyModifiedNodes.clear();
         this._recentlyModifiedParentNodes.clear();
         this._updateInfos.clear();
-        delete this._treeOutline._clipboardNodeData;
     },
 
     /**
