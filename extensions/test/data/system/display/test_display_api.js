8e108a2afbbf7c1a9f76c1c7d3e3778480dd6961
rockot <rockot@chromium.org>
2015-01-22 12:26:36 -0800

    s/app_shell_browsertests/extensions_browsertests/g
    We would like to use app_shell as the basis for all
    content-browser integration tests within the
    extensions layer.
    
    This simply moves the test target up to the parent
    //extensions path and renames it to
    extensions_browsertests
    
    BUG=388893
    R=yoz@chromium.org
    
    Review URL: https://codereview.chromium.org/797023006
    
    Cr-Commit-Position: refs/heads/master@{#312669}

--- extensions/test/data/system/display/test_display_api.js
+++ extensions/test/data/system/display/test_display_api.js
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 // system.display api test
-// app_shell_browsertests --gtest_filter=SystemDisplayApiTest.*
+// extensions_browsertests --gtest_filter=SystemDisplayApiTest.*
 
 chrome.test.runTests([
   function testGet() {
