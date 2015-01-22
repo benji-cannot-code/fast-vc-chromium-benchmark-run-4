8de592a4be92715512897b6fcc3f3a6445d92cfa
dcheng <dcheng@chromium.org>
2015-01-21 22:29:52 -0800

    Don't crash DRT if dumpFilenameBeingDragged() is called outside a drag.
    BUG=450155
    
    Review URL: https://codereview.chromium.org/857303004
    
    Cr-Commit-Position: refs/heads/master@{#312574}

--- content/shell/renderer/test_runner/event_sender.cc
+++ content/shell/renderer/test_runner/event_sender.cc
@@ -1560,6 +1560,9 @@ void EventSender::SetTouchCancelable(bool cancelable) {
 }
 
 void EventSender::DumpFilenameBeingDragged() {
+  if (current_drag_data_.isNull())
+    return;
+
   WebString filename;
   WebVector<WebDragData::Item> items = current_drag_data_.items();
   for (size_t i = 0; i < items.size(); ++i) {
