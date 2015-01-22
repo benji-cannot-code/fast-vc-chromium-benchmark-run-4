0a352cdaaa518483b96cee1ef3088d2b02636c08
mkwst@chromium.org <mkwst@chromium.org>
2015-01-22 18:49:44 +0000

    MIX: Rename the CSP directive.
    To match the spec change in https://github.com/w3c/webappsec/commit/d9907898755234c3d3967e56227a9fd3ab480ef3
    
    Review URL: https://codereview.chromium.org/856753003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188824 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/csp/ContentSecurityPolicy.h
+++ third_party/WebKit/Source/core/frame/csp/ContentSecurityPolicy.h
@@ -91,7 +91,7 @@ public:
 
     // Mixed Content Directive
     // https://w3c.github.io/webappsec/specs/mixedcontent/#strict-mode
-    static const char StrictMixedContentChecking[];
+    static const char BlockAllMixedContent[];
 
     enum ReportingStatus {
         SendReport,
