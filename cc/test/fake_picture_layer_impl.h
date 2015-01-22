36223a89d6a7260aa20e62f00abc2fdaf5fbea28
hush <hush@chromium.org>
2015-01-21 17:57:39 -0800

    Cache viewport rect for tile priority in UpdateTiles
    PictureLayerImpls in a tree should continue using the same viewport rect
    for tile priority until UpdateTiles() is called with the new value of
    viewport rect for tile priority. Otherwise, a PictureLayerImpl could be
    in an inconsistent state that could lead to a wrong decision about
    AllTilesRequiredForActivationAreReadyToDraw.
    
    This CL caches the viewport rect for tile priority in PictureLayerImpl.
    
    BUG= 449008
    
    Review URL: https://codereview.chromium.org/819643004
    
    Cr-Commit-Position: refs/heads/master@{#312523}

--- cc/test/fake_picture_layer_impl.h
+++ cc/test/fake_picture_layer_impl.h
@@ -78,7 +78,6 @@ class FakePictureLayerImpl : public PictureLayerImpl {
   using PictureLayerImpl::CleanUpTilingsOnActiveLayer;
   using PictureLayerImpl::CanHaveTilings;
   using PictureLayerImpl::MinimumContentsScale;
-  using PictureLayerImpl::GetViewportForTilePriorityInContentSpace;
   using PictureLayerImpl::SanityCheckTilingState;
   using PictureLayerImpl::GetRecycledTwinLayer;
   using PictureLayerImpl::UpdateRasterSource;
@@ -107,6 +106,10 @@ class FakePictureLayerImpl : public PictureLayerImpl {
 
   gfx::Rect visible_rect_for_tile_priority() {
     return visible_rect_for_tile_priority_;
+  }
+
+  gfx::Rect viewport_rect_for_tile_priority_in_content_space() {
+    return viewport_rect_for_tile_priority_in_content_space_;
   }
 
   void set_fixed_tile_size(const gfx::Size& size) { fixed_tile_size_ = size; }
