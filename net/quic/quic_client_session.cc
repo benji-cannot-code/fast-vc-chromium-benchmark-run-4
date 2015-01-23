c3dd2ff4ed0914cebfadc89d6adb30979b053fb7
vadimt <vadimt@chromium.org>
2015-01-23 10:15:44 -0800

    Further instrumentations of QuicStreamFactory::CreateSession to find janks Jankiness of "422516 QuicStreamFactory::CreateSession57" is 35 jph. Instrumenting further.
    BUG=422516
    
    Review URL: https://codereview.chromium.org/870053002
    
    Cr-Commit-Position: refs/heads/master@{#312878}

--- net/quic/quic_client_session.cc
+++ net/quic/quic_client_session.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_loop.h"
 #include "base/metrics/histogram.h"
 #include "base/metrics/sparse_histogram.h"
+#include "base/profiler/scoped_tracker.h"
 #include "base/stl_util.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/values.h"
@@ -176,6 +177,11 @@ QuicClientSession::QuicClientSession(
       num_packets_read_(0),
       going_away_(false),
       weak_factory_(this) {
+  // TODO(vadimt): Remove ScopedTracker below once crbug.com/422516 is fixed.
+  tracked_objects::ScopedTracker tracking_profile(
+      FROM_HERE_WITH_EXPLICIT_FUNCTION(
+          "422516 QuicClientSession::QuicClientSession"));
+
   connection->set_debug_visitor(logger_);
   IPEndPoint address;
   if (socket && socket->GetLocalAddress(&address) == OK &&
