9b73d526db40797e6bc77d89ab48ce26a659b3fc
mseaborn <mseaborn@chromium.org>
2015-01-22 20:39:15 -0800

    NaCl: Remove now-unused callback plumbing for handling "init_done" SRPC call
    StartupInitializationComplete()/init_done no longer needs to call any
    callbacks, since these have all become no-ops.
    
    BUG=428030
    TEST=NaCl*Core
    
    Review URL: https://codereview.chromium.org/870753002
    
    Cr-Commit-Position: refs/heads/master@{#312771}

--- ppapi/native_client/src/trusted/plugin/service_runtime.h
+++ ppapi/native_client/src/trusted/plugin/service_runtime.h
@@ -74,8 +74,7 @@ class PluginReverseInterface: public nacl::ReverseInterface {
  public:
   PluginReverseInterface(nacl::WeakRefAnchor* anchor,
                          PP_Instance pp_instance,
-                         ServiceRuntime* service_runtime,
-                         pp::CompletionCallback init_done_cb);
+                         ServiceRuntime* service_runtime);
 
   virtual ~PluginReverseInterface();
 
@@ -115,8 +114,6 @@ class PluginReverseInterface: public nacl::ReverseInterface {
   NaClMutex mu_;
   NaClCondVar cv_;
   bool shutting_down_;
-
-  pp::CompletionCallback init_done_cb_;
 };
 
 //  ServiceRuntime abstracts a NativeClient sel_ldr instance.
@@ -125,8 +122,7 @@ class ServiceRuntime {
   ServiceRuntime(Plugin* plugin,
                  PP_Instance pp_instance,
                  bool main_service_runtime,
-                 bool uses_nonsfi_mode,
-                 pp::CompletionCallback init_done_cb);
+                 bool uses_nonsfi_mode);
   // The destructor terminates the sel_ldr process.
   ~ServiceRuntime();
 
