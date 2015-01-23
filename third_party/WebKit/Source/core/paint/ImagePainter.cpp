e2c7b88948683c1351bba85cb36a8b2e48b51e30
chrishtr@chromium.org <chrishtr@chromium.org>
2015-01-23 21:21:46 +0000

    [Slimming Paint] Fix drawing recorder for image focus rings to enclose save/restore.
    Review URL: https://codereview.chromium.org/872843003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188905 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/paint/ImagePainter.cpp
+++ third_party/WebKit/Source/core/paint/ImagePainter.cpp
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/rendering/RenderReplaced.h"
 #include "core/rendering/TextRunConstructor.h"
 #include "platform/geometry/LayoutPoint.h"
-#include "platform/graphics/GraphicsContextStateSaver.h"
 #include "platform/graphics/Path.h"
 
 namespace blink {
@@ -61,18 +60,21 @@ void ImagePainter::paintAreaElementFocusRing(const PaintInfo& paintInfo)
     if (!outlineWidth)
         return;
 
-    // FIXME: Clip path instead of context when Skia pathops is ready.
-    // https://crbug.com/251206
-    GraphicsContextStateSaver savedContext(*paintInfo.context);
     IntRect focusRect = m_renderImage.absoluteContentBox();
-    RenderDrawingRecorder recorder(paintInfo.context, m_renderImage, paintInfo.phase, focusRect);
-    if (recorder.canUseCachedDrawing())
+
+    RenderDrawingRecorder drawingRecorder(paintInfo.context, m_renderImage, paintInfo.phase, focusRect);
+    if (drawingRecorder.canUseCachedDrawing())
         return;
 
+    // FIXME: Clip path instead of context when Skia pathops is ready.
+    // https://crbug.com/251206
+
+    paintInfo.context->save();
     paintInfo.context->clip(focusRect);
     paintInfo.context->drawFocusRing(path, outlineWidth,
         areaElementStyle->outlineOffset(),
         m_renderImage.resolveColor(areaElementStyle, CSSPropertyOutlineColor));
+    paintInfo.context->restore();
 }
 
 void ImagePainter::paintReplaced(const PaintInfo& paintInfo, const LayoutPoint& paintOffset)
