0dcda95e2d6103ba158367c8dd66d0db7aefa9f6
felt <felt@chromium.org>
2015-01-23 07:43:56 -0800

    Add accessibility labels to the permission bubble (Views)
    dtseng took a look at the accessibility labeling of the permission bubble and
    found:
    
        alert: page action
        static text : <address> wants to use your location
        button: Allow
        button: Block
        link: (unlabeled)
        image: (unabeled)
        button: close
    
    This CL adds labels to the images. However, the labels are simply the empty
    string because the images are redundant with the accompanying permission
    type. (It doesn't make sense to put "location" next to "location".)
    
    This CL also adds a label for the allow combobox, which I noticed was missing
    a label.
    
    I am still unsure what the unlabeled link is, so that issue remains.
    
    BUG=382650
    
    Review URL: https://codereview.chromium.org/818063002
    
    Cr-Commit-Position: refs/heads/master@{#312851}

--- chrome/browser/ui/views/website_settings/permissions_bubble_view.cc
+++ chrome/browser/ui/views/website_settings/permissions_bubble_view.cc
@@ -264,6 +264,7 @@ PermissionsBubbleDelegateView::PermissionsBubbleDelegateView(
     views::ImageView* icon = new views::ImageView();
     icon->SetImage(bundle.GetImageSkiaNamed(requests.at(index)->GetIconID()));
     icon->SetImageSize(gfx::Size(kIconSize, kIconSize));
+    icon->SetTooltipText(base::string16());  // Redundant with the text fragment
     label_container->AddChildView(icon);
     views::Label* label =
         new views::Label(requests.at(index)->GetMessageTextFragment());
@@ -331,6 +332,8 @@ PermissionsBubbleDelegateView::PermissionsBubbleDelegateView(
         new CustomizeAllowComboboxModel());
     allow_combobox->set_listener(this);
     allow_combobox->SetStyle(views::Combobox::STYLE_ACTION);
+    allow_combobox->SetAccessibleName(
+        l10n_util::GetStringUTF16(IDS_PERMISSION_ALLOW_COMBOBOX));
     button_layout->AddView(allow_combobox);
     allow_combobox_ = allow_combobox;
   }
