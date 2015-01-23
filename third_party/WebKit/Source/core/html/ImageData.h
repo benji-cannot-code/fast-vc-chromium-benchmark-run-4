7458b7749f6098e4e4f694a25d3d6f42e75f8133
sigbjornf@opera.com <sigbjornf@opera.com>
2015-01-23 02:36:18 +0000

    Fix ImageData constructor infelicity.
    Correctly handle the optional third argument for the overload
    
     ImageData(Uint8ClampedArray d, unsigned long w, optional unsigned long h)
    
    It should not throw if not supplied, but should throw if supplied and zero.
    
    Also, delegate the object interface typecheck to generated bindings code.
    
    R=junov
    BUG=338804
    
    Review URL: https://codereview.chromium.org/871563003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188847 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/ImageData.h
+++ third_party/WebKit/Source/core/html/ImageData.h
@@ -47,6 +47,7 @@ public:
     static PassRefPtrWillBeRawPtr<ImageData> create(const IntSize&);
     static PassRefPtrWillBeRawPtr<ImageData> create(const IntSize&, PassRefPtr<DOMUint8ClampedArray>);
     static PassRefPtrWillBeRawPtr<ImageData> create(unsigned width, unsigned height, ExceptionState&);
+    static PassRefPtrWillBeRawPtr<ImageData> create(DOMUint8ClampedArray*, unsigned width, ExceptionState&);
     static PassRefPtrWillBeRawPtr<ImageData> create(DOMUint8ClampedArray*, unsigned width, unsigned height, ExceptionState&);
 
     IntSize size() const { return m_size; }
@@ -62,6 +63,8 @@ public:
 private:
     explicit ImageData(const IntSize&);
     ImageData(const IntSize&, PassRefPtr<DOMUint8ClampedArray>);
+
+    static bool validateConstructorArguments(DOMUint8ClampedArray*, unsigned width, unsigned&, ExceptionState&);
 
     IntSize m_size;
     RefPtr<DOMUint8ClampedArray> m_data;
