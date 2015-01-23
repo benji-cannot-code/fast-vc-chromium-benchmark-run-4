bb0f6a74556e25b85deca7437de95fe1a89cffc3
dongseong.hwang@intel.com <dongseong.hwang@intel.com>
2015-01-23 17:24:14 +0000

    Use SkXfermode::Mode in GraphicsContext
    BUG=425656
    
    Review URL: https://codereview.chromium.org/868983003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188895 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/paint/BoxPainter.cpp
+++ third_party/WebKit/Source/core/paint/BoxPainter.cpp
@@ -465,7 +465,7 @@ void BoxPainter::paintFillLayerExtended(RenderBoxModelObject& obj, const PaintIn
             if (isOpaqueRoot && !skipBaseColor) {
                 paintRootBackgroundColor(obj, paintInfo, rect, bgColor);
             } else if (bgColor.alpha()) {
-                context->fillRect(backgroundRect, bgColor, context->compositeOperationDeprecated());
+                context->fillRect(backgroundRect, bgColor, context->compositeOperation());
             }
         }
     }
@@ -589,7 +589,7 @@ void BoxPainter::paintRootBackgroundColor(RenderObject& obj, const PaintInfo& pa
 
     Color baseColor = obj.view()->frameView()->baseBackgroundColor();
     bool shouldClearDocumentBackground = obj.document().settings() && obj.document().settings()->shouldClearDocumentBackground();
-    CompositeOperator operation = shouldClearDocumentBackground ? CompositeCopy : context->compositeOperationDeprecated();
+    SkXfermode::Mode operation = shouldClearDocumentBackground ? SkXfermode::kSrc_Mode : context->compositeOperation();
 
     // If we have an alpha go ahead and blend with the base background color.
     if (baseColor.alpha()) {
--- third_party/WebKit/Source/platform/graphics/GraphicsContext.cpp
+++ third_party/WebKit/Source/platform/graphics/GraphicsContext.cpp
@@ -567,14 +567,14 @@ void GraphicsContext::drawPicture(const SkPicture* picture)
     m_canvas->drawPicture(picture);
 }
 
-void GraphicsContext::compositePicture(SkPicture* picture, const FloatRect& dest, const FloatRect& src, CompositeOperator op, WebBlendMode blendMode)
+void GraphicsContext::compositePicture(SkPicture* picture, const FloatRect& dest, const FloatRect& src, SkXfermode::Mode op)
 {
     ASSERT(m_canvas);
     if (contextDisabled() || !picture)
         return;
 
     SkPaint picturePaint;
-    picturePaint.setXfermodeMode(WebCoreCompositeToSkiaComposite(op, blendMode));
+    picturePaint.setXfermodeMode(op);
     m_canvas->save();
     SkRect sourceBounds = WebCoreFloatRectToSKRect(src);
     SkRect skBounds = WebCoreFloatRectToSKRect(dest);
@@ -1118,12 +1118,12 @@ void GraphicsContext::drawTiledImage(Image* image, const IntRect& dest, const In
 }
 
 void GraphicsContext::drawImageBuffer(ImageBuffer* image, const FloatRect& dest,
-    const FloatRect* src, CompositeOperator op, WebBlendMode blendMode)
+    const FloatRect* src, SkXfermode::Mode op)
 {
     if (contextDisabled() || !image)
         return;
 
-    image->draw(this, dest, src, op, blendMode);
+    image->draw(this, dest, src, op);
 }
 
 void GraphicsContext::writePixels(const SkImageInfo& info, const void* pixels, size_t rowBytes, int x, int y)
@@ -1633,13 +1633,13 @@ void GraphicsContext::setCTM(const AffineTransform& affine)
     setMatrix(affineTransformToSkMatrix(affine));
 }
 
-void GraphicsContext::fillRect(const FloatRect& rect, const Color& color, CompositeOperator op)
+void GraphicsContext::fillRect(const FloatRect& rect, const Color& color, SkXfermode::Mode op)
 {
     if (contextDisabled())
         return;
 
     SkXfermode::Mode previousOperation = compositeOperation();
-    setCompositeOperation(WebCoreCompositeToSkiaComposite(op));
+    setCompositeOperation(op);
     fillRect(rect, color);
     setCompositeOperation(previousOperation);
 }
