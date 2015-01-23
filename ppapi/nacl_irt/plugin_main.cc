2def9c8d71658ef30e67abbf4084364aebb9772a
mseaborn <mseaborn@chromium.org>
2015-01-22 19:19:22 -0800

    NaCl: Remove now-unneeded call to the "init_done" SRPC method
    Since we removed Plugin::NexeFileDidOpenContinuation() and
    Plugin::BitcodeDidTranslateContinuation(), this "init_done" call no
    longer has any effect.  It no longer has the effect of calling
    StartPpapiProxy().  That is now triggered by a Chrome IPC message.
    
    BUG=428030
    TEST=NaCl*Core
    
    Review URL: https://codereview.chromium.org/864913003
    
    Cr-Commit-Position: refs/heads/master@{#312757}

--- ppapi/nacl_irt/plugin_main.cc
+++ ppapi/nacl_irt/plugin_main.cc
@@ -19,10 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/proxy/plugin_globals.h"
 #include "ppapi/shared_impl/ppb_audio_shared.h"
 
-#if defined(__native_client__)
-#include "native_client/src/shared/srpc/nacl_srpc_ppapi_plugin_internal.h"
-#endif
-
 void PpapiPluginRegisterThreadCreator(
     const struct PP_ThreadFunctions* thread_functions) {
   // Initialize all classes that need to create threads that call back into
@@ -33,14 +29,6 @@ void PpapiPluginRegisterThreadCreator(
 int PpapiPluginMain() {
   base::MessageLoop loop;
   ppapi::proxy::PluginGlobals plugin_globals;
-
-#if defined(OS_NACL_SFI)
-  // This is currently needed so that the NaCl reverse service calls the
-  // StartupInitializationComplete() SRPC method, which unblocks plugin
-  // startup on the Chromium side.
-  // TODO(mseaborn): Remove the need to call this.
-  NaClPluginLowLevelInitializationComplete();
-#endif
 
   ppapi::PpapiDispatcher ppapi_dispatcher(
       ppapi::GetIOThread()->message_loop_proxy(),
