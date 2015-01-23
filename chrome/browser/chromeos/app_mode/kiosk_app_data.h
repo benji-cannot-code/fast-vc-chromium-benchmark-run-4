8577677e6a6a320bfc0afad60ed4020bd31e9b0c
xiyuan <xiyuan@chromium.org>
2015-01-22 19:14:19 -0800

    kiosk: Load private app meta from crx.
    - Extract app meta from crx if the app is from private store;
    - Get rid of unused raw icon;
    
    BUG=444729
    
    Review URL: https://codereview.chromium.org/862123003
    
    Cr-Commit-Position: refs/heads/master@{#312751}

--- chrome/browser/chromeos/app_mode/kiosk_app_data.h
+++ chrome/browser/chromeos/app_mode/kiosk_app_data.h
@@ -18,10 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class Profile;
 
-namespace base {
-class RefCountedString;
-}
-
 namespace extensions {
 class Extension;
 class WebstoreDataFetcher;
@@ -67,20 +63,25 @@ class KioskAppData : public base::SupportsWeakPtr<KioskAppData>,
   // Loads app data from the app installed in the given profile.
   void LoadFromInstalledApp(Profile* profile, const extensions::Extension* app);
 
+  // Sets full path of the cache crx. The crx would be used to extract meta
+  // data for private apps.
+  void SetCachedCrx(const base::FilePath& crx_file);
+
   // Returns true if web store data fetching is in progress.
   bool IsLoading() const;
+
+  // Returns true if the update url points to Webstore.
+  bool IsFromWebStore() const;
 
   const std::string& app_id() const { return app_id_; }
   const std::string& user_id() const { return user_id_; }
   const std::string& name() const { return name_; }
   const GURL& update_url() const { return update_url_; }
   const gfx::ImageSkia& icon() const { return icon_; }
-  const base::RefCountedString* raw_icon() const {
-    return raw_icon_.get();
-  }
   Status status() const { return status_; }
 
  private:
+  class CrxLoader;
   class IconLoader;
   class WebstoreDataParser;
 
@@ -102,8 +103,7 @@ class KioskAppData : public base::SupportsWeakPtr<KioskAppData>,
   void OnExtensionIconLoaded(const gfx::Image& icon);
 
   // Callbacks for IconLoader.
-  void OnIconLoadSuccess(const scoped_refptr<base::RefCountedString>& raw_icon,
-                         const gfx::ImageSkia& icon);
+  void OnIconLoadSuccess(const gfx::ImageSkia& icon);
   void OnIconLoadFailure();
 
   // Callbacks for WebstoreDataParser
@@ -126,6 +126,12 @@ class KioskAppData : public base::SupportsWeakPtr<KioskAppData>,
                              const char* key,
                              std::string* value);
 
+  // Extracts meta data from crx file when loading from Webstore and local
+  // cache fails.
+  void MaybeLoadFromCrx();
+
+  void OnCrxLoadFinished(const CrxLoader* crx_loader);
+
   KioskAppDataDelegate* delegate_;  // not owned.
   Status status_;
 
@@ -134,10 +140,11 @@ class KioskAppData : public base::SupportsWeakPtr<KioskAppData>,
   std::string name_;
   GURL update_url_;
   gfx::ImageSkia icon_;
-  scoped_refptr<base::RefCountedString> raw_icon_;
 
   scoped_ptr<extensions::WebstoreDataFetcher> webstore_fetcher_;
   base::FilePath icon_path_;
+
+  base::FilePath crx_file_;
 
   DISALLOW_COPY_AND_ASSIGN(KioskAppData);
 };
