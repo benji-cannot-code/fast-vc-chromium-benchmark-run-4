d6fe561908af9d376ffeb80df40512b2c5b98a22
petrcermak <petrcermak@chromium.org>
2015-01-23 05:35:25 -0800

    [memory-inspector] disable native tracing inside Chrome App
    This patch disables native tracing inside the Memory Inspector Chrome
    App because it is currently not supported.
    
    BUG=448399
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/867163002
    
    Cr-Commit-Position: refs/heads/master@{#312830}

--- tools/memory_inspector/memory_inspector/frontends/www_content/js/storage.js
+++ tools/memory_inspector/memory_inspector/frontends/www_content/js/storage.js
@@ -26,6 +26,9 @@ this.onDomReady_ = function() {
       .click(this.dumpMmapForSelectedSnapshot_.bind(this));
   $('#storage-dump-nheap').button({icons:{primary: 'ui-icon-calculator'}})
       .click(this.dumpNheapForSelectedSnapshot_.bind(this));
+  if (window.DISABLE_NATIVE_TRACING) {
+    $('#storage-dump-nheap').hide();
+  }
 
   // Create the table.
   this.table_ = new google.visualization.Table($('#storage-table')[0]);
@@ -37,6 +40,10 @@ this.reload = function() {
 }
 
 this.onListAjaxResponse_ = function(data) {
+  if (window.DISABLE_NATIVE_TRACING) {
+    // Do not show the native heap column if native tracing is disabled.
+    data.cols.pop();
+  }
   this.tableData_ = new google.visualization.DataTable(data);
   this.redraw();
 };
@@ -140,13 +147,15 @@ this.rebuildMenu_ = function() {
             this.profileMmapForSelectedSnapshots.bind(this, rule)));
   }, this);
 
-  this.profileMenu_.append(
-      $('<li/>').addClass('header').text('Native heap rules'));
-  profiler.rulesets['nheap'].forEach(function(rule) {
+  if (!window.DISABLE_NATIVE_TRACING) {
     this.profileMenu_.append(
-        $('<li/>').text(rule).click(
-            this.profileNativeForSelectedSnapshots.bind(this, rule)));
-  }, this);
+        $('<li/>').addClass('header').text('Native heap rules'));
+    profiler.rulesets['nheap'].forEach(function(rule) {
+      this.profileMenu_.append(
+          $('<li/>').text(rule).click(
+              this.profileNativeForSelectedSnapshots.bind(this, rule)));
+    }, this);
+  }
 
   this.profileMenu_.menu();
 };
