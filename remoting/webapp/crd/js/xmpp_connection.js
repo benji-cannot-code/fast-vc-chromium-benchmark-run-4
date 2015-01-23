02aacf6757e41b49e2b2ad4caadf8c073b602051
Sergey Ulanov <sergeyu@chromium.org>
2015-01-23 09:58:27 -0800

    Fix assert in remoting.XmppConnection
    BUG=451107
    R=kelvinp@chromium.org
    
    Review URL: https://codereview.chromium.org/870823002
    
    Cr-Commit-Position: refs/heads/master@{#312872}

--- remoting/webapp/crd/js/xmpp_connection.js
+++ remoting/webapp/crd/js/xmpp_connection.js
@@ -326,7 +326,8 @@ remoting.XmppConnection.prototype.onRead_ = function(readInfo) {
     this.streamParser_.appendData(readInfo.data);
   }
 
-  if (!this.startTlsPending_) {
+  if (!this.startTlsPending_ &&
+      this.state_ != remoting.SignalStrategy.State.CLOSED) {
     this.tryRead_();
   }
 };
