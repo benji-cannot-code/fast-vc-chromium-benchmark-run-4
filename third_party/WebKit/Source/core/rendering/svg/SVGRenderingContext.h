e83b1233a370aeabfd7561a1fada0466b64a8131
pdr@chromium.org <pdr@chromium.org>
2015-01-23 20:20:42 +0000

    Refactor SVGRenderingContext to manage a PaintInfo for content painting
    This patch lets SVGRenderingContext manage the child paint info
    for all SVG painters which will allow a future patch to swap in
    a custom recording GraphicsContext to support SVG filters.
    
    ChildPaintInfo has been renamed paintInfoWithoutFiltering to make it
    clear that the rendering context's paint info should be used if
    filtering is desired.
    
    SVGRenderingContext now manages a paint info which will later have a
    custom graphics context on it that has filtering applied. This lets us
    remove the saved paint rect as well.
    
    Review URL: https://codereview.chromium.org/861043002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188901 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/svg/SVGRenderingContext.h
+++ third_party/WebKit/Source/core/rendering/svg/SVGRenderingContext.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/paint/CompositingRecorder.h"
 #include "core/paint/FloatClipRecorder.h"
+#include "core/rendering/PaintInfo.h"
 #include "core/rendering/svg/RenderSVGResourceClipper.h"
 #include "platform/graphics/paint/ClipPathRecorder.h"
 #include "platform/transforms/AffineTransform.h"
@@ -35,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class RenderObject;
-struct PaintInfo;
 class RenderSVGResourceFilter;
 class RenderSVGResourceMasker;
 class SVGResources;
@@ -52,9 +52,9 @@ private:
 class SVGRenderingContext {
     STACK_ALLOCATED();
 public:
-    SVGRenderingContext(RenderObject& object, PaintInfo& paintInfo)
+    SVGRenderingContext(RenderObject& object, const PaintInfo& paintInfo)
         : m_object(&object)
-        , m_paintInfo(&paintInfo)
+        , m_paintInfo(paintInfo)
         , m_filter(nullptr)
         , m_clipper(nullptr)
         , m_clipperState(RenderSVGResourceClipper::ClipperNotApplied)
@@ -65,6 +65,8 @@ public:
     { }
 
     ~SVGRenderingContext();
+
+    PaintInfo& paintInfo() { return m_paintInfo; }
 
     // Return true if these operations aren't necessary or if they are successfully applied.
     bool applyClipMaskAndFilterIfNecessary();
@@ -88,8 +90,7 @@ private:
     bool isIsolationInstalled() const;
 
     RawPtrWillBeMember<RenderObject> m_object;
-    RawPtrWillBeMember<PaintInfo> m_paintInfo;
-    IntRect m_savedPaintRect;
+    PaintInfo m_paintInfo;
     RawPtrWillBeMember<RenderSVGResourceFilter> m_filter;
     RawPtrWillBeMember<RenderSVGResourceClipper> m_clipper;
     RenderSVGResourceClipper::ClipperState m_clipperState;
