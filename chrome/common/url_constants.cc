e32d699b40f26e2ea14a480816772733e9404ba6
isherman <isherman@chromium.org>
2015-01-22 21:04:39 -0800

    [Smart Lock] Update help center link to use the latest feature name.
    The feature was previously named "Easy Unlock", but is now named "Smart Lock".
    
    BUG=none
    TEST=none
    R=tengs@chromium.org
    
    Review URL: https://codereview.chromium.org/872553002
    
    Cr-Commit-Position: refs/heads/master@{#312772}

--- chrome/common/url_constants.cc
+++ chrome/common/url_constants.cc
@@ -731,6 +731,6 @@ const char kMac32BitDeprecationURL[] =
 #endif
 
 const char kEasyUnlockLearnMoreUrl[] =
-    "https://support.google.com/chromebook/?p=easy_unlock";
+    "https://support.google.com/chromebook/?p=smart_lock";
 
 }  // namespace chrome
