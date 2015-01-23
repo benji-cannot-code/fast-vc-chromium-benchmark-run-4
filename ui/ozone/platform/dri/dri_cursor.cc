2832f0b032b101bc0cf3b47b9e4894918a506402
dnicoara <dnicoara@chromium.org>
2015-01-23 15:43:49 -0800

    [Ozone-DRI] Check the host_id when resetting the cursor IPC callback
    When the GPU process crashes we can get an OnChannelEstablished() before
    the OnChannelDestroyed() is received. Use the host_id to figure out if
    the OnChannelDestroyed() should be processed.
    
    BUG=451584
    
    Review URL: https://codereview.chromium.org/872093002
    
    Cr-Commit-Position: refs/heads/master@{#312968}

--- ui/ozone/platform/dri/dri_cursor.cc
+++ ui/ozone/platform/dri/dri_cursor.cc
@@ -193,6 +193,7 @@ void DriCursor::OnChannelEstablished(
 #endif
   DCHECK(ui_task_runner_->BelongsToCurrentThread());
   base::AutoLock lock(state_.lock);
+  state_.host_id = host_id;
   state_.send_runner = send_runner;
   state_.send_callback = send_callback;
   // Initial set for this GPU process will happen after the window
@@ -202,8 +203,11 @@ void DriCursor::OnChannelEstablished(
 void DriCursor::OnChannelDestroyed(int host_id) {
   DCHECK(ui_task_runner_->BelongsToCurrentThread());
   base::AutoLock lock(state_.lock);
-  state_.send_runner = NULL;
-  state_.send_callback.Reset();
+  if (state_.host_id == host_id) {
+    state_.host_id = -1;
+    state_.send_runner = NULL;
+    state_.send_callback.Reset();
+  }
 }
 
 bool DriCursor::OnMessageReceived(const IPC::Message& message) {
@@ -268,7 +272,8 @@ void DriCursor::SendLocked(IPC::Message* message) {
   delete message;
 }
 
-DriCursor::CursorState::CursorState() : window(gfx::kNullAcceleratedWidget) {
+DriCursor::CursorState::CursorState()
+    : window(gfx::kNullAcceleratedWidget), host_id(-1) {
 }
 
 DriCursor::CursorState::~CursorState() {
