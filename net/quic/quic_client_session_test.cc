6b9431c60080f8b34737546558df3d9ed9423b01
rch <rch@chromium.org>
2015-01-22 16:30:41 -0800

    Do not pool QUIC sessions when privacy mode mismatches.
    BUG=451164
    
    Review URL: https://codereview.chromium.org/868803003
    
    Cr-Commit-Position: refs/heads/master@{#312714}

--- net/quic/quic_client_session_test.cc
+++ net/quic/quic_client_session_test.cc
@@ -157,11 +157,11 @@ TEST_P(QuicClientSessionTest, CanPool) {
   session_.OnProofVerifyDetailsAvailable(details);
   CompleteCryptoHandshake();
 
-
-  EXPECT_TRUE(session_.CanPool("www.example.org"));
-  EXPECT_TRUE(session_.CanPool("mail.example.org"));
-  EXPECT_TRUE(session_.CanPool("mail.example.com"));
-  EXPECT_FALSE(session_.CanPool("mail.google.com"));
+  EXPECT_TRUE(session_.CanPool("www.example.org", PRIVACY_MODE_DISABLED));
+  EXPECT_FALSE(session_.CanPool("www.example.org", PRIVACY_MODE_ENABLED));
+  EXPECT_TRUE(session_.CanPool("mail.example.org", PRIVACY_MODE_DISABLED));
+  EXPECT_TRUE(session_.CanPool("mail.example.com", PRIVACY_MODE_DISABLED));
+  EXPECT_FALSE(session_.CanPool("mail.google.com", PRIVACY_MODE_DISABLED));
 }
 
 TEST_P(QuicClientSessionTest, ConnectionPooledWithTlsChannelId) {
@@ -179,10 +179,10 @@ TEST_P(QuicClientSessionTest, ConnectionPooledWithTlsChannelId) {
   CompleteCryptoHandshake();
   QuicClientSessionPeer::SetChannelIDSent(&session_, true);
 
-  EXPECT_TRUE(session_.CanPool("www.example.org"));
-  EXPECT_TRUE(session_.CanPool("mail.example.org"));
-  EXPECT_FALSE(session_.CanPool("mail.example.com"));
-  EXPECT_FALSE(session_.CanPool("mail.google.com"));
+  EXPECT_TRUE(session_.CanPool("www.example.org", PRIVACY_MODE_DISABLED));
+  EXPECT_TRUE(session_.CanPool("mail.example.org", PRIVACY_MODE_DISABLED));
+  EXPECT_FALSE(session_.CanPool("mail.example.com", PRIVACY_MODE_DISABLED));
+  EXPECT_FALSE(session_.CanPool("mail.google.com", PRIVACY_MODE_DISABLED));
 }
 
 TEST_P(QuicClientSessionTest, ConnectionNotPooledWithDifferentPin) {
@@ -205,7 +205,7 @@ TEST_P(QuicClientSessionTest, ConnectionNotPooledWithDifferentPin) {
   CompleteCryptoHandshake();
   QuicClientSessionPeer::SetChannelIDSent(&session_, true);
 
-  EXPECT_FALSE(session_.CanPool("mail.example.org"));
+  EXPECT_FALSE(session_.CanPool("mail.example.org", PRIVACY_MODE_DISABLED));
 }
 
 TEST_P(QuicClientSessionTest, ConnectionPooledWithMatchingPin) {
@@ -227,7 +227,7 @@ TEST_P(QuicClientSessionTest, ConnectionPooledWithMatchingPin) {
   CompleteCryptoHandshake();
   QuicClientSessionPeer::SetChannelIDSent(&session_, true);
 
-  EXPECT_TRUE(session_.CanPool("mail.example.org"));
+  EXPECT_TRUE(session_.CanPool("mail.example.org", PRIVACY_MODE_DISABLED));
 }
 
 }  // namespace
