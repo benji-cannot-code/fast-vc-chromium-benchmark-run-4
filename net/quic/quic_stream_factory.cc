2d5037c5c17761c8e46b11df774e22b1ddb321a9
rtenneti <rtenneti@chromium.org>
2015-01-22 17:53:50 -0800

    QUIC - Deleted disable_loading_server_info_for_new_servers field trial. Changed the code not to load server config if there is no alternative protocol entry for QUIC. During our experiments, we found that disable_loading_server_info_for_new_servers being set to true has reduced the latency until crypto handshake completion.
    This is a revert of https://codereview.chromium.org/811073004/
    
    We check for alternative protocols only if there is a http_server_properties_ object.
    
    R=rch@chromium.org
    
    Review URL: https://codereview.chromium.org/858323002
    
    Cr-Commit-Position: refs/heads/master@{#312741}

--- net/quic/quic_stream_factory.cc
+++ net/quic/quic_stream_factory.cc
@@ -566,7 +566,6 @@ QuicStreamFactory::QuicStreamFactory(
     bool always_require_handshake_confirmation,
     bool disable_connection_pooling,
     int load_server_info_timeout,
-    bool disable_loading_server_info_for_new_servers,
     float load_server_info_timeout_srtt_multiplier,
     bool enable_truncated_connection_ids,
     const QuicTagVector& connection_options)
@@ -587,8 +586,6 @@ QuicStreamFactory::QuicStreamFactory(
           always_require_handshake_confirmation),
       disable_connection_pooling_(disable_connection_pooling),
       load_server_info_timeout_ms_(load_server_info_timeout),
-      disable_loading_server_info_for_new_servers_(
-          disable_loading_server_info_for_new_servers),
       load_server_info_timeout_srtt_multiplier_(
           load_server_info_timeout_srtt_multiplier),
       enable_truncated_connection_ids_(enable_truncated_connection_ids),
@@ -653,7 +650,7 @@ int QuicStreamFactory::Create(const HostPortPair& host_port_pair,
   QuicServerInfo* quic_server_info = nullptr;
   if (quic_server_info_factory_) {
     bool load_from_disk_cache = true;
-    if (disable_loading_server_info_for_new_servers_) {
+    if (http_server_properties_) {
       const AlternateProtocolMap& alternate_protocol_map =
           http_server_properties_->alternate_protocol_map();
       AlternateProtocolMap::const_iterator it =
