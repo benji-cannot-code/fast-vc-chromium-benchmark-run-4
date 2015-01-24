e57de5a2c5d34b22e9ce73ea23ed16ffe62ef459
jdduke <jdduke@chromium.org>
2015-01-23 16:43:51 -0800

    Ease selection dragging between large/small text lines
    Previously, the coordinate sent for adjusting the text selection was
    offset from the line by half the line height. The side effect is that
    dragging up/down from large lines, particularly to smaller lines, was
    difficult, and sometimes even impossible.
    
    Resolve this by using the smaller of fixed size offset and half the
    line height. This eases dragging the selection bounds between lines
    that differ greatly in size.
    
    BUG=450640
    
    Review URL: https://codereview.chromium.org/872083002
    
    Cr-Commit-Position: refs/heads/master@{#312982}

--- ui/touch_selection/touch_selection_controller.cc
+++ ui/touch_selection/touch_selection_controller.cc
@@ -11,6 +11,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ui {
 namespace {
 
+gfx::Vector2dF ComputeLineOffsetFromBottom(const SelectionBound& bound) {
+  gfx::Vector2dF line_offset =
+      gfx::ScaleVector2d(bound.edge_top() - bound.edge_bottom(), 0.5f);
+  // An offset of 5 DIPs is sufficient for most line sizes. For small lines,
+  // using half the line height avoids synthesizing a point on a line above
+  // (or below) the intended line.
+  const gfx::Vector2dF kMaxLineOffset(5.f, 5.f);
+  line_offset.SetToMin(kMaxLineOffset);
+  line_offset.SetToMax(-kMaxLineOffset);
+  return line_offset;
+}
+
 TouchHandleOrientation ToTouchHandleOrientation(SelectionBound::Type type) {
   switch (type) {
     case SelectionBound::LEFT:
@@ -240,7 +252,7 @@ void TouchSelectionController::OnHandleDragUpdate(const TouchHandle& handle,
                                                   const gfx::PointF& position) {
   // As the position corresponds to the bottom left point of the selection
   // bound, offset it by half the corresponding line height.
-  gfx::Vector2dF line_offset = &handle == end_selection_handle_.get()
+  gfx::Vector2dF line_offset = &handle == start_selection_handle_.get()
                                    ? GetStartLineOffset()
                                    : GetEndLineOffset();
   gfx::PointF line_position = position + line_offset;
@@ -409,11 +421,11 @@ const gfx::PointF& TouchSelectionController::GetEndPosition() const {
 }
 
 gfx::Vector2dF TouchSelectionController::GetStartLineOffset() const {
-  return gfx::ScaleVector2d(start_.edge_top() - start_.edge_bottom(), 0.5f);
+  return ComputeLineOffsetFromBottom(start_);
 }
 
 gfx::Vector2dF TouchSelectionController::GetEndLineOffset() const {
-  return gfx::ScaleVector2d(end_.edge_top() - end_.edge_bottom(), 0.5f);
+  return ComputeLineOffsetFromBottom(end_);
 }
 
 bool TouchSelectionController::GetStartVisible() const {
