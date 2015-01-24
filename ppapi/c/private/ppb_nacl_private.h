3a39a2f2ad62036905b30ba2654b1d68723c8097
mseaborn <mseaborn@chromium.org>
2015-01-23 22:19:30 -0800

    NaCl: Simplify the plumbing for PPAPI's DevInterfacesEnabled flag
    There's no need for this flag to be sent from ppb_nacl_private_impl.cc
    to plugin.cc and back again.  It can stay within
    ppb_nacl_private_impl.cc.
    
    This merges two error-handling checks in LaunchSelLdr().
    
    BUG=239656
    TEST=browser_tests
    
    Review URL: https://codereview.chromium.org/874603002
    
    Cr-Commit-Position: refs/heads/master@{#313010}

--- ppapi/c/private/ppb_nacl_private.h
+++ ppapi/c/private/ppb_nacl_private.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * found in the LICENSE file.
  */
 
-/* From private/ppb_nacl_private.idl modified Fri Jan 23 07:36:29 2015. */
+/* From private/ppb_nacl_private.idl modified Fri Jan 23 09:09:44 2015. */
 
 #ifndef PPAPI_C_PRIVATE_PPB_NACL_PRIVATE_H_
 #define PPAPI_C_PRIVATE_PPB_NACL_PRIVATE_H_
@@ -212,8 +212,7 @@ struct PP_NaClFileInfo {
 struct PPB_NaCl_Private_1_0 {
   /* Launches NaCl's sel_ldr process.  Returns PP_EXTERNAL_PLUGIN_OK on success
    * and writes a NaClHandle to imc_handle. Returns PP_EXTERNAL_PLUGIN_FAILED on
-   * failure. The |enable_ppapi_dev| parameter controls whether GetInterface
-   * returns 'Dev' interfaces to the NaCl plugin.
+   * failure.
    * The |nexe_file_info| is currently used only in non-SFI mode. It is the
    * file handle for the main nexe file, which should be initially loaded.
    * LaunchSelLdr takes the ownership of the file handle.
@@ -225,7 +224,6 @@ struct PPB_NaCl_Private_1_0 {
                        const char* alleged_url,
                        const struct PP_NaClFileInfo* nexe_file_info,
                        PP_Bool uses_nonsfi_mode,
-                       PP_Bool enable_ppapi_dev,
                        PP_NaClAppProcessType process_type,
                        void* imc_handle,
                        struct PP_CompletionCallback callback);
@@ -307,7 +305,6 @@ struct PPB_NaCl_Private_1_0 {
    * plugin.
    */
   void (*ProcessNaClManifest)(PP_Instance instance, const char* program_url);
-  PP_Bool (*DevInterfacesEnabled)(PP_Instance instance);
   PP_Bool (*GetManifestProgramURL)(PP_Instance instance,
                                    struct PP_Var* full_url,
                                    struct PP_PNaClOptions* pnacl_options,
