6a53424e34fc35b5e957a2b4703ff7230db37325
estade@chromium.org <estade@chromium.org>
2015-01-24 03:12:11 +0000

    remove old version of focusedNodeChanged
    see https://codereview.chromium.org/851393002/
    depends on https://codereview.chromium.org/812033016/
    
    BUG=433486
    
    Review URL: https://codereview.chromium.org/870433002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188922 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/ChromeClientImpl.cpp
+++ third_party/WebKit/Source/web/ChromeClientImpl.cpp
@@ -200,8 +200,6 @@ void ChromeClientImpl::takeFocus(WebFocusType type)
 
 void ChromeClientImpl::focusedNodeChanged(Node* fromNode, Node* toNode)
 {
-    // FIXME: remove.
-    m_webView->client()->focusedNodeChanged(WebNode(toNode));
     m_webView->client()->focusedNodeChanged(WebNode(fromNode), WebNode(toNode));
 
     WebURL focusURL;
