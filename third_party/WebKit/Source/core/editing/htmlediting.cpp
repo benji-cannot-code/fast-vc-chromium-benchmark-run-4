b2154a4cf7ac1fad136fdb893c0ef3e1b5ec3ed1
yoichio@chromium.org <yoichio@chromium.org>
2015-01-26 06:43:07 +0000

    [Editing][Regression] fix crash in FrameSelection::respondToNodeModification.
    FrameSelection::respondToNodeModification crashes if |m_selection.start()| and
     |m_selection.end()| are not in a same DOM tree.
    
    Add "TreeScope* Position::commonTreeScope(Position, Position)" to get a common
     TreeScope if exists and use it in the function to check.
    
    BUG=450496
    
    Review URL: https://codereview.chromium.org/873603002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188943 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/htmlediting.cpp
+++ third_party/WebKit/Source/core/editing/htmlediting.cpp
@@ -76,7 +76,7 @@ int comparePositions(const Position& a, const Position& b)
 {
     ASSERT(a.isNotNull());
     ASSERT(b.isNotNull());
-    const TreeScope* commonScope = a.containerNode()->treeScope().commonAncestorTreeScope(b.containerNode()->treeScope());
+    const TreeScope* commonScope = Position::commonAncestorTreeScope(a, b);
 
     ASSERT(commonScope);
     if (!commonScope)
