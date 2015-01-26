75362df28965aec181428d1edca266b041a77d22
fs@opera.com <fs@opera.com>
2015-01-26 03:03:27 +0000

    VTTCue: Refactor display parameter handling
    Move VTTCue::m_display* into a separate struct (VTTDisplayParameters) and
    let VTTCue::calculateDisplayParameters return said struct.
    The returned struct is then passed to VTTCueBox for application.
    Also drop VTTCue::m_computedLinePosition.
    Also move the call to createVTTNodeTree out of calculateDisplayParameters
    to allow marking the latter const.
    The method VTTCue::getPositionCoordinates is folded into
    calculateDisplayParameters.
    Additional mark some methods const (calculateComputedLinePosition,
    settingName), and drop the 'videoSize' argument from
    TextTrackCue::updateDisplay (and callees) since it isn't used ATM.
    
    BUG=301580
    
    Review URL: https://codereview.chromium.org/869623002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188939 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/track/vtt/VTTCue.h
+++ third_party/WebKit/Source/core/html/track/vtt/VTTCue.h
@@ -42,6 +42,15 @@ class ExecutionContext;
 class VTTCue;
 class VTTScanner;
 
+struct VTTDisplayParameters {
+    VTTDisplayParameters();
+
+    FloatPoint position;
+    float size;
+    CSSValueID direction;
+    CSSValueID writingMode;
+};
+
 class VTTCueBox final : public HTMLDivElement {
 public:
     static PassRefPtrWillBeRawPtr<VTTCueBox> create(Document& document, VTTCue* cue)
@@ -50,7 +59,7 @@ public:
     }
 
     VTTCue* getCue() const { return m_cue; }
-    void applyCSSProperties(const IntSize& videoSize);
+    void applyCSSProperties(const VTTDisplayParameters&);
 
     virtual void trace(Visitor*) override;
 
@@ -101,7 +110,7 @@ public:
     const String& regionId() const { return m_regionId; }
     void setRegionId(const String&);
 
-    virtual void updateDisplay(const IntSize& videoSize, HTMLDivElement& container) override;
+    virtual void updateDisplay(HTMLDivElement& container) override;
 
     virtual void updateDisplayTree(double movieTime) override;
     virtual void removeDisplayTree() override;
@@ -109,14 +118,7 @@ public:
 
     void markFutureAndPastNodes(ContainerNode*, double previousTimestamp, double movieTime);
 
-    float calculateComputedLinePosition();
-
-    FloatPoint getCSSPosition() const;
-
-    CSSValueID getCSSAlignment() const;
-    float getCSSSize() const;
-    CSSValueID getCSSWritingDirection() const;
-    CSSValueID getCSSWritingMode() const;
+    float calculateComputedLinePosition() const;
 
     enum WritingDirection {
         Horizontal = 0,
@@ -134,7 +136,7 @@ public:
         Right,
         NumberOfAlignments
     };
-    CueAlignment getAlignment() const { return m_cueAlignment; }
+    CueAlignment cueAlignment() const { return m_cueAlignment; }
 
     virtual ExecutionContext* executionContext() const override;
 
@@ -150,19 +152,18 @@ private:
     Document& document() const;
 
     VTTCueBox& ensureDisplayTree();
-    PassRefPtrWillBeRawPtr<VTTCueBox> getDisplayTree(const IntSize& videoSize);
+    PassRefPtrWillBeRawPtr<VTTCueBox> getDisplayTree();
 
     virtual void cueDidChange() override;
 
     void createVTTNodeTree();
     void copyVTTNodeToDOMTree(ContainerNode* vttNode, ContainerNode* root);
 
-    FloatPoint getPositionCoordinates() const;
     bool lineIsAuto() const;
-
-    void calculateDisplayParameters();
-    float calculateComputedTextPosition() const;
     bool textPositionIsAuto() const;
+
+    VTTDisplayParameters calculateDisplayParameters() const;
+    float calculateComputedTextPosition() const;
     CueAlignment calculateComputedCueAlignment() const;
 
     enum CueSetting {
@@ -174,11 +175,10 @@ private:
         Align,
         RegionId
     };
-    CueSetting settingName(VTTScanner&);
+    CueSetting settingName(VTTScanner&) const;
 
     String m_text;
     float m_linePosition;
-    float m_computedLinePosition;
     float m_textPosition;
     float m_cueSize;
     WritingDirection m_writingDirection;
@@ -188,10 +188,6 @@ private:
     RefPtrWillBeMember<DocumentFragment> m_vttNodeTree;
     RefPtrWillBeMember<HTMLDivElement> m_cueBackgroundBox;
     RefPtrWillBeMember<VTTCueBox> m_displayTree;
-
-    CSSValueID m_displayDirection;
-    float m_displaySize;
-    FloatPoint m_displayPosition;
 
     bool m_snapToLines : 1;
     bool m_displayTreeShouldChange : 1;
