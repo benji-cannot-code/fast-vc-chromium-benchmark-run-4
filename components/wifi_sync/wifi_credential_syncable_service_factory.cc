0e82e1a982832b826528cd005c53bf27f2e58be4
quiche <quiche@chromium.org>
2015-01-26 12:12:55 -0800

    wifi_sync: implement ACTION_ADD in WifiCredentialSyncableService
    Implement support for adding networks. Specifically:
    1) When an ACTION_ADD change is received from sync, apply that
       change to the platform's network configruation.
    2) Provide a method that upper layers can call, to add a network
       to Chrome Sync.
    
    BUG=chromium:431435
    TEST=components_unittests --gtest_filter="Wifi*"
    
    Review URL: https://codereview.chromium.org/836303004
    
    Cr-Commit-Position: refs/heads/master@{#313109}

--- components/wifi_sync/wifi_credential_syncable_service_factory.cc
+++ components/wifi_sync/wifi_credential_syncable_service_factory.cc
@@ -5,10 +5,43 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/wifi_sync/wifi_credential_syncable_service_factory.h"
 
+#include "base/logging.h"
+#include "base/memory/scoped_ptr.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "components/wifi_sync/wifi_credential_syncable_service.h"
 
+#if defined(OS_CHROMEOS)
+#include "chromeos/login/login_state.h"
+#include "chromeos/network/network_handler.h"
+#include "components/wifi_sync/wifi_config_delegate_chromeos.h"
+#endif
+
 namespace wifi_sync {
+
+namespace {
+
+scoped_ptr<WifiConfigDelegate> BuildConfigDelegate(
+    content::BrowserContext* context) {
+#if defined(OS_CHROMEOS)
+  const chromeos::LoginState* login_state = chromeos::LoginState::Get();
+  DCHECK(login_state->IsUserLoggedIn());
+  DCHECK(!login_state->primary_user_hash().empty());
+  // TODO(quiche): Verify that |context| is the primary user's context.
+
+  // Note: NetworkHandler is a singleton that is managed by
+  // ChromeBrowserMainPartsChromeos, and destroyed after all
+  // KeyedService instances are destroyed.
+  chromeos::NetworkHandler* network_handler = chromeos::NetworkHandler::Get();
+  return make_scoped_ptr(new WifiConfigDelegateChromeOs(
+      login_state->primary_user_hash(),
+      network_handler->managed_network_configuration_handler()));
+#else
+  NOTREACHED();
+  return nullptr;
+#endif
+}
+
+}  // namespace
 
 // static
 WifiCredentialSyncableService*
@@ -39,7 +72,7 @@ KeyedService* WifiCredentialSyncableServiceFactory::BuildServiceInstanceFor(
     content::BrowserContext* context) const {
   // TODO(quiche): Figure out if this behaves properly for multi-profile.
   // crbug.com/430681.
-  return new WifiCredentialSyncableService();
+  return new WifiCredentialSyncableService(BuildConfigDelegate(context));
 }
 
 }  // namespace wifi_sync
