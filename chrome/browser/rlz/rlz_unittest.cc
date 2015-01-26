613778410eb4575cf49181823a2e2f759aefe6db
tbarzic <tbarzic@chromium.org>
2015-01-26 11:19:27 -0800

    Fix {virtual,override,final} build failure on official CrOS build
    BUG=417463
    TBR=gwilson@chromium.org
    
    Review URL: https://codereview.chromium.org/878573005
    
    Cr-Commit-Position: refs/heads/master@{#313097}

--- chrome/browser/rlz/rlz_unittest.cc
+++ chrome/browser/rlz/rlz_unittest.cc
@@ -138,7 +138,7 @@ class TestRLZTracker : public RLZTracker {
   }
 
 #if defined(OS_CHROMEOS)
-  virtual bool ScheduleClearRlzState() override {
+  bool ScheduleClearRlzState() override {
     return !assume_not_ui_thread_;
   }
 #endif
