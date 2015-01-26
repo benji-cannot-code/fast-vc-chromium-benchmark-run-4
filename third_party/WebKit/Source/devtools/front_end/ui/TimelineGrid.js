aa8e16504cb660a2e66d693292a02975ddea2d1a
apavlov@chromium.org <apavlov@chromium.org>
2015-01-26 16:39:48 +0000

    DevTools: Extract TimelineGrid-related CSS into a dedicated stylesheet
    R=lushnikov, pfeldman
    
    Review URL: https://codereview.chromium.org/872323004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188968 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ui/TimelineGrid.js
+++ third_party/WebKit/Source/devtools/front_end/ui/TimelineGrid.js
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 WebInspector.TimelineGrid = function()
 {
     this.element = createElement("div");
+    this.element.appendChild(WebInspector.View.createStyleElement("ui/timelineGrid.css"));
 
     this._dividersElement = this.element.createChild("div", "resources-dividers");
 
