81c98ae94223225bb2c91dbde2fb2b6e2d79a063
nednguyen <nednguyen@google.com>
2015-01-26 14:57:58 -0800

    [Telemetry] Fix exception handling in user story runner.
    Currently, unrecoverable exception raised during a user story run does not
    create a failure value for that user story. This leads to confusing log output
    by gtest_progress_reporter.
    
    This patch fixes this by catching unrecoverable exception and add failure value
    to the currently user story value. This patch also improve unittest in
    user_story_runner_unittest to make sure that the stdout output captures the
    raise exceptions.
    
    BUG=448827
    
    Review URL: https://codereview.chromium.org/875993002
    
    Cr-Commit-Position: refs/heads/master@{#313161}

--- tools/telemetry/telemetry/user_story/user_story_runner.py
+++ tools/telemetry/telemetry/user_story/user_story_runner.py
@@ -17,7 +17,6 @@ from telemetry.core import util
 from telemetry.core import wpr_modes
 from telemetry.page import page_set as page_set_module
 from telemetry.page import page_test
-from telemetry.page import shared_page_state
 from telemetry.page.actions import page_action
 from telemetry.results import results_options
 from telemetry.user_story import user_story_filter
@@ -109,6 +108,11 @@ def _RunUserStoryAndProcessErrorIfNeeded(expectations, user_story, results,
   except page_action.PageActionNotSupported as e:
     results.AddValue(
         skip.SkipValue(user_story, 'Unsupported page action: %s' % e))
+  except Exception:
+    results.AddValue(
+        failure.FailureValue(
+            user_story, sys.exc_info(), 'Unhandlable exception raised.'))
+    raise
   else:
     if expectation == 'fail':
       logging.warning(
