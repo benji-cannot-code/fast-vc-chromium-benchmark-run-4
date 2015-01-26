aad8587899e5226701b44e114ffc0fee295127c5
primiano <primiano@chromium.org>
2015-01-26 02:17:15 -0800

    [memory-inspector] UI fixes + bump version number for release.
    BUG=448399
    TBR=petrcercmak@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/867263002
    
    Cr-Commit-Position: refs/heads/master@{#313057}

--- tools/memory_inspector/classification_rules/default/mmap-android.py
+++ tools/memory_inspector/classification_rules/default/mmap-android.py
@@ -77,7 +77,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   'children': [
     {
       'name': 'GPU',
-      'mmap_file': r'(nv)|(mali)',
+      'mmap_file': r'(nv)|(mali)|(kgsl)',
     },
   ],
 },
