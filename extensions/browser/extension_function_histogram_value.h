98b10015f2887758933cb1f677542a7b7a7e8d06
isherman <isherman@chromium.org>
2015-01-27 15:57:56 -0800

    [Smart Lock] Add a private API to show an error bubble anchored to the Smart Lock app window.
    BUG=406439
    TEST=none
    R=tengs@chromium.org
    
    Review URL: https://codereview.chromium.org/843023002
    
    Cr-Commit-Position: refs/heads/master@{#313408}

--- extensions/browser/extension_function_histogram_value.h
+++ extensions/browser/extension_function_histogram_value.h
@@ -1012,6 +1012,7 @@ enum HistogramValue {
   PRINTERPROVIDERINTERNAL_REPORTPRINTERS,
   FILESYSTEMPROVIDER_GET,
   FILEMANAGERPRIVATE_SEARCHFILESBYHASHES,
+  EASYUNLOCKPRIVATE_SHOWERRORBUBBLE,
   // Last entry: Add new entries above and ensure to update
   // tools/metrics/histograms/histograms.xml.
   ENUM_BOUNDARY
