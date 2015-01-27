07dc62e0ca367286a829163f0ea7d1f96abb4b53
dconnelly <dconnelly@chromium.org>
2015-01-27 08:42:11 -0800

    Revert of Update SSL/malware/net error interstitial design (patchset #10 id:180001 of https://codereview.chromium.org/865943002/)
    Reason for revert:
    ErrorPageTests are failing on Linux ChromiumOS:
    
    http://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Tests%20%281%29/builds/55305
    
    http://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Tests%20%281%29/builds/55306
    
    http://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Tests%20%281%29/builds/55307
    
    Original issue's description:
    > + On small screens the navigation buttons are anchored to the bottom of the screen.
    > + Tweaks to the text colour
    > + Change button styling of safe browsing page
    >
    > BUG=422882
    >
    > Committed: https://crrev.com/7ee5b21903f8a466e8f8e995e62354deceb987f0
    > Cr-Commit-Position: refs/heads/master@{#313280}
    
    TBR=arv@chromium.org,felt@chromium.org,edwardjung@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=422882
    
    Review URL: https://codereview.chromium.org/866723005
    
    Cr-Commit-Position: refs/heads/master@{#313288}

--- chrome/renderer/resources/neterror.js
+++ chrome/renderer/resources/neterror.js
@@ -4,17 +4,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 function toggleHelpBox() {
-  var helpBoxOuter = document.getElementById('details');
+  var helpBoxOuter = document.getElementById('help-box-outer');
   helpBoxOuter.classList.toggle('hidden');
   var detailsButton = document.getElementById('details-button');
   if (helpBoxOuter.classList.contains('hidden'))
     detailsButton.innerText = detailsButton.detailsText;
   else
     detailsButton.innerText = detailsButton.hideDetailsText;
-
-  // Details appears over the main content on small screens.
-  if (mobileNav)
-    document.getElementById('main-content').classList.toggle('hidden');
 }
 
 function diagnoseErrors() {
@@ -58,8 +54,6 @@ function updateIconClass(classList, newClass) {
   if (newClass == 'icon-offline') {
     document.body.classList.add('offline');
     new Runner('.interstitial-wrapper');
-  } else {
-    document.body.classList.add('neterror');
   }
 }
 
@@ -142,7 +136,7 @@ function onDocumentLoad() {
   if (loadTimeData.valueExists('summary') &&
           !loadTimeData.getValue('summary').msg) {
     detailsButton.style.display = 'none';
-    document.getElementById('details').style.display = 'block';
+    document.getElementById('help-box-outer').style.display = 'block';
   }
 
   // Show control buttons.
