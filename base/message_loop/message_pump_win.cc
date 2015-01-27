809611cf7a0bab89406b16a7ff722a92540935a6
miu <miu@chromium.org>
2015-01-26 22:43:14 -0800

    [Windows] Fix for Browser IO MessageLoop spinning the CPU.
    This corrects an integer overflow issue which resulted in the Browser IO
    MessageLoop spinning, eating up 100% of a CPU core.  The conversion from
    a double to a 32-bit int was sometimes out-of-range: A large positive
    double value was being assigned as a negative int value.  Because of
    this, MessagePumpWin acted as if a delayed task was past-due for running
    and refused to sleep the thread.  Downstream code paths would instead
    see the task as "in the future" and then refuse to run it and remove it
    from the queue.  This resulted in a "never-wait, never-dequeue" bad
    spinning state.
    
    While researching this problem, a number of code points have been
    exposed that are erroneously posting delayed tasks with huge delays,
    likely due to bad math on 64-bit platforms.  Added DLOG warnings to
    whine at developers to avoid this in the future.
    
    See bug for additional details.
    
    BUG=450045
    TBR=darin@chromium.org
    
    Review URL: https://codereview.chromium.org/873393004
    
    Cr-Commit-Position: refs/heads/master@{#313238}

--- base/message_loop/message_pump_win.cc
+++ base/message_loop/message_pump_win.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/message_loop/message_pump_win.h"
 
+#include <limits>
 #include <math.h>
 
 #include "base/debug/trace_event.h"
@@ -71,12 +72,13 @@ int MessagePumpWin::GetCurrentDelay() const {
   double timeout =
       ceil((delayed_work_time_ - TimeTicks::Now()).InMillisecondsF());
 
-  // If this value is negative, then we need to run delayed work soon.
-  int delay = static_cast<int>(timeout);
-  if (delay < 0)
-    delay = 0;
-
-  return delay;
+  // Range check the |timeout| while converting to an integer.  If the |timeout|
+  // is negative, then we need to run delayed work soon.  If the |timeout| is
+  // "overflowingly" large, that means a delayed task was posted with a
+  // super-long delay.
+  return timeout < 0 ? 0 :
+      (timeout > std::numeric_limits<int>::max() ?
+       std::numeric_limits<int>::max() : static_cast<int>(timeout));
 }
 
 //-----------------------------------------------------------------------------
