2191a4e396628a3d410d552fa20d7548d1a13c3c
k.czech@samsung.com <k.czech@samsung.com>
2015-01-27 14:26:57 +0000

    Aria table row and column headers are not exposed in accessibility
    Aria table row and column headers should also be exposed
    in accessibility as in html tables.
    
    BUG=451047
    
    Review URL: https://codereview.chromium.org/869253002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189033 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/accessibility/AXARIAGridCell.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXARIAGridCell.cpp
@@ -51,6 +51,18 @@ PassRefPtr<AXARIAGridCell> AXARIAGridCell::create(RenderObject* renderer, AXObje
     return adoptRef(new AXARIAGridCell(renderer, axObjectCache));
 }
 
+bool AXARIAGridCell::isAriaColumnHeader() const
+{
+    const AtomicString& role = getAttribute(HTMLNames::roleAttr);
+    return equalIgnoringCase(role, "columnheader");
+}
+
+bool AXARIAGridCell::isAriaRowHeader() const
+{
+    const AtomicString& role = getAttribute(HTMLNames::roleAttr);
+    return equalIgnoringCase(role, "rowheader");
+}
+
 AXObject* AXARIAGridCell::parentTable() const
 {
     AXObject* parent = parentObjectUnignored();
@@ -120,6 +132,17 @@ void AXARIAGridCell::columnIndexRange(pair<unsigned, unsigned>& columnRange)
 
     // as far as I can tell, grid cells cannot span columns
     columnRange.second = 1;
+}
+
+AccessibilityRole AXARIAGridCell::scanToDecideHeaderRole()
+{
+    if (isAriaRowHeader())
+        return RowHeaderRole;
+
+    if (isAriaColumnHeader())
+        return ColumnHeaderRole;
+
+    return CellRole;
 }
 
 } // namespace blink
