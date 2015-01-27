d3fd02746fc2c3e99ad89fd4460c038368bc8f44
falken <falken@chromium.org>
2015-01-27 04:57:43 -0800

    Support the Service-Worker-Allowed header
    This allows a site to customize Service Worker's path restriction
    policy, which by default means a script can only control a scope within
    the directory of the script.
    
    Service-Worker-Allowed is an optional HTTP response header for a Service
    Worker script. The value is a URL that is the maximum allowed scope that
    the script can control. If it's a relative URL, it's relative to the
    script URL. For example, when included in the response for a script at
    "http://www.example.com/resources/sw.js", a value of ".." means that
    sw.js can control a max scope of "/", rather than the default max scope
    of "resources/".
    
    As spec'd at:
    https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#update-algorithm
    
    Blink-side test:
    https://codereview.chromium.org/868973004/
    
    BUG=436747
    
    Review URL: https://codereview.chromium.org/866403002
    
    Cr-Commit-Position: refs/heads/master@{#313263}

--- content/browser/service_worker/service_worker_write_to_cache_job.cc
+++ content/browser/service_worker/service_worker_write_to_cache_job.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/service_worker/service_worker_context_core.h"
 #include "content/browser/service_worker/service_worker_disk_cache.h"
 #include "content/browser/service_worker/service_worker_metrics.h"
+#include "content/browser/service_worker/service_worker_utils.h"
 #include "content/common/service_worker/service_worker_types.h"
 #include "net/base/io_buffer.h"
 #include "net/base/net_errors.h"
@@ -38,6 +39,8 @@ const char kClientAuthenticationError[] =
     "Client authentication was required to fetch the script.";
 const char kRedirectError[] =
     "The script resource is behind a redirect, which is disallowed.";
+const char kServiceWorkerAllowed[] = "Service-Worker-Allowed";
+
 }
 
 ServiceWorkerWriteToCacheJob::ServiceWorkerWriteToCacheJob(
@@ -397,7 +400,7 @@ void ServiceWorkerWriteToCacheJob::OnResponseStarted(
       return;
     }
   }
-  // To prevent most user-uploaded content from being used as a serviceworker.
+
   if (version_->script_url() == url_) {
     std::string mime_type;
     request->GetMimeType(&mime_type);
@@ -413,7 +416,11 @@ void ServiceWorkerWriteToCacheJob::OnResponseStarted(
                             error_message);
       return;
     }
+
+    if (!CheckPathRestriction(request))
+      return;
   }
+
   WriteHeadersToCache();
 }
 
@@ -443,6 +450,25 @@ void ServiceWorkerWriteToCacheJob::OnReadCompleted(
   did_notify_finished_ = true;
   SetStatus(net::URLRequestStatus());  // Clear the IO_PENDING status
   NotifyReadComplete(0);
+}
+
+bool ServiceWorkerWriteToCacheJob::CheckPathRestriction(
+    net::URLRequest* request) {
+  std::string service_worker_allowed;
+  const net::HttpResponseHeaders* headers = request->response_headers();
+  bool has_header = headers->EnumerateHeader(nullptr, kServiceWorkerAllowed,
+                                             &service_worker_allowed);
+
+  std::string error_message;
+  if (!ServiceWorkerUtils::IsPathRestrictionSatisfied(
+          version_->scope(), url_,
+          has_header ? &service_worker_allowed : nullptr, &error_message)) {
+    AsyncNotifyDoneHelper(net::URLRequestStatus(net::URLRequestStatus::FAILED,
+                                                net::ERR_INSECURE_RESPONSE),
+                          error_message);
+    return false;
+  }
+  return true;
 }
 
 void ServiceWorkerWriteToCacheJob::AsyncNotifyDoneHelper(
