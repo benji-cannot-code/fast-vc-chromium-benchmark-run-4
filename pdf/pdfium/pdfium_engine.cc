99b7aa6e7956691df97169aca2a59a1aff0314a6
alexandrec <alexandrec@chromium.org>
2015-01-26 17:44:50 -0800

    Introduce new test for bookmark loading.
    BUG=110020
    
    Committed: https://crrev.com/50567ebbd5ca2ff79d87382addc11d25658776d2
    Cr-Commit-Position: refs/heads/master@{#312076}
    
    Review URL: https://codereview.chromium.org/840493002
    
    Cr-Commit-Position: refs/heads/master@{#313198}

--- pdf/pdfium/pdfium_engine.cc
+++ pdf/pdfium/pdfium_engine.cc
@@ -2367,7 +2367,6 @@ int PDFiumEngine::GetNumberOfPages() {
   return pages_.size();
 }
 
-
 pp::VarArray PDFiumEngine::GetBookmarks() {
   pp::VarDictionary dict = TraverseBookmarks(doc_, NULL);
   // The root bookmark contains no useful information.
