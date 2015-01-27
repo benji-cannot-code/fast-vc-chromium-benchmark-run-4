ca520182ed0c2a38aa30c7831b57213901239c64
fukino <fukino@chromium.org>
2015-01-27 05:36:29 -0800

    Enable tree view for all volumes.
    This CL enables tree navigation view for all volumes by making VolumeItem as a subclass of DirectoryItem.
    VolumeItem for Google Drive has special treatments so that Google Drive always reveals its root directory and fake entries(Shared with me, Recent, and Offline).
    Drive shortcuts are not shown as a tree intentionally.
    
    BUG=398291
    TEST=run browser_tests
    
    Review URL: https://codereview.chromium.org/864773002
    
    Cr-Commit-Position: refs/heads/master@{#313268}

--- ui/file_manager/file_manager/foreground/js/file_manager_commands.js
+++ ui/file_manager/file_manager/foreground/js/file_manager_commands.js
@@ -48,7 +48,6 @@ CommandUtil.getCommandEntry = function(element) {
     // element is a DirectoryTree.
     return element.selectedItem ? element.selectedItem.entry : null;
   } else if (element instanceof DirectoryItem ||
-             element instanceof VolumeItem ||
              element instanceof ShortcutItem) {
     // element are sub items in DirectoryTree.
     return element.entry;
