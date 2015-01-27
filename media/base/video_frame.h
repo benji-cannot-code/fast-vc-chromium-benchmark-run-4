6978d2a0822b7ab83fd294577e54e67e12204850
achaulk <achaulk@chromium.org>
2015-01-27 10:24:06 -0800

    Plumb allow_overlay flag for video path into cc
    This will allow hardware video overlays to be enabled in the compositor
    
    Review URL: https://codereview.chromium.org/806413004
    
    Cr-Commit-Position: refs/heads/master@{#313310}

--- media/base/video_frame.h
+++ media/base/video_frame.h
@@ -115,7 +115,8 @@ class MEDIA_EXPORT VideoFrame : public base::RefCountedThreadSafe<VideoFrame> {
       const gfx::Rect& visible_rect,
       const gfx::Size& natural_size,
       base::TimeDelta timestamp,
-      const ReadPixelsCB& read_pixels_cb);
+      const ReadPixelsCB& read_pixels_cb,
+      bool allow_overlay);
 
 #if !defined(MEDIA_FOR_CAST_IOS)
   // Read pixels from the native texture backing |*this| and write
@@ -300,6 +301,8 @@ class MEDIA_EXPORT VideoFrame : public base::RefCountedThreadSafe<VideoFrame> {
   // Returns the shared-memory handle, if present
   base::SharedMemoryHandle shared_memory_handle() const;
 
+  bool allow_overlay() const { return allow_overlay_; }
+
 #if defined(OS_POSIX)
   // Returns backing dmabuf file descriptor for given |plane|, if present.
   int dmabuf_fd(size_t plane) const;
@@ -410,6 +413,8 @@ class MEDIA_EXPORT VideoFrame : public base::RefCountedThreadSafe<VideoFrame> {
   uint32 release_sync_point_;
 
   const bool end_of_stream_;
+
+  bool allow_overlay_;
 
   DISALLOW_IMPLICIT_CONSTRUCTORS(VideoFrame);
 };
