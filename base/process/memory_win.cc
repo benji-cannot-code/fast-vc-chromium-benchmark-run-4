37dfb0431cc5722685e3f6fd08a331857912ec31
cpu <cpu@chromium.org>
2015-01-27 10:13:45 -0800

    Fix EnableTerminationOnOutOfMemory for malloc
    Currently only crashes on new and new[]. With the change it will also
    crash when there is no memory for malloc as well.
    
    BUG=434397
    TEST=none.
    
    Committed: https://crrev.com/22d64abdc792516fd0f4895b26ae3a73c85239e0
    Cr-Commit-Position: refs/heads/master@{#310320}
    
    Review URL: https://codereview.chromium.org/836733006
    
    Cr-Commit-Position: refs/heads/master@{#313307}

--- base/process/memory_win.cc
+++ base/process/memory_win.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/process/memory.h"
 
+#include <new.h>
 #include <psapi.h>
 
 #include "base/logging.h"
@@ -14,14 +15,18 @@ namespace base {
 
 namespace {
 
-void OnNoMemory() {
-  // Kill the process. This is important for security, since WebKit doesn't
-  // NULL-check many memory allocations. If a malloc fails, returns NULL, and
-  // the buffer is then used, it provides a handy mapping of memory starting at
-  // address 0 for an attacker to utilize.
+#pragma warning(push)
+#pragma warning(disable: 4702)
+
+int OnNoMemory(size_t) {
+  // Kill the process. This is important for security since most of code
+  // does not check the result of memory allocation.
   __debugbreak();
   _exit(1);
+  return 0;
 }
+
+#pragma warning(pop)
 
 // HeapSetInformation function pointer.
 typedef BOOL (WINAPI* HeapSetFn)(HANDLE, HEAP_INFORMATION_CLASS, PVOID, SIZE_T);
@@ -69,7 +74,8 @@ void EnableTerminationOnHeapCorruption() {
 }
 
 void EnableTerminationOnOutOfMemory() {
-  std::set_new_handler(&OnNoMemory);
+  _set_new_handler(&OnNoMemory);
+  _set_new_mode(1);
 }
 
 HMODULE GetModuleFromAddress(void* address) {
--- content/app/content_main_runner.cc
+++ content/app/content_main_runner.cc
@@ -487,6 +487,7 @@ class ContentMainRunnerImpl : public ContentMainRunner {
   int Initialize(const ContentMainParams& params) override {
     ui_task_ = params.ui_task;
 
+    base::EnableTerminationOnOutOfMemory();
 #if defined(OS_WIN)
     RegisterInvalidParamHandler();
     ui::win::CreateATLModuleIfNeeded();
@@ -603,7 +604,6 @@ class ContentMainRunnerImpl : public ContentMainRunner {
 
     if (!delegate_ || delegate_->ShouldEnableTerminationOnHeapCorruption())
       base::EnableTerminationOnHeapCorruption();
-    base::EnableTerminationOnOutOfMemory();
 
 #if !defined(OS_IOS)
     SetProcessTitleFromCommandLine(argv);
