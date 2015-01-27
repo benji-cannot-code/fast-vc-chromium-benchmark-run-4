940eed034ca36ba5e0ed513b9a052d7fccd14fc9
bungeman <bungeman@chromium.org>
2015-01-26 17:41:02 -0800

    Move to SkStreamAsset for SkTypeface streams.
    SkTypeface is moving to use SkStreamAsset instead of SkStream. The
    implementations are already assuming this, and all users are in
    practice already creating the correct types. This change simply
    clarifies existing practice and documents actual restrictions.
    
    The Skia side change will be https://codereview.chromium.org/869763002/
    
    Review URL: https://codereview.chromium.org/870133002
    
    Cr-Commit-Position: refs/heads/master@{#313196}

--- content/common/font_config_ipc_linux.cc
+++ content/common/font_config_ipc_linux.cc
@@ -21,11 +21,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "skia/ext/skia_utils_base.h"
 #include "third_party/skia/include/core/SkData.h"
 #include "third_party/skia/include/core/SkStream.h"
+#include "third_party/skia/include/core/SkTypeface.h"
 
 namespace content {
 
 // Return a stream from the file descriptor, or NULL on failure.
-SkStream* StreamFromFD(int fd) {
+SkStreamAsset* StreamFromFD(int fd) {
   skia::RefPtr<SkData> data = skia::AdoptRef(SkData::NewFromFD(fd));
   if (!data) {
     return NULL;
@@ -95,7 +96,7 @@ bool FontConfigIPC::matchFamilyName(const char familyName[],
   return true;
 }
 
-SkStream* FontConfigIPC::openStream(const FontIdentity& identity) {
+SkStreamAsset* FontConfigIPC::openStream(const FontIdentity& identity) {
   TRACE_EVENT0("sandbox_ipc", "FontConfigIPC::openStream");
   Pickle request;
   request.WriteInt(METHOD_OPEN);
@@ -119,7 +120,7 @@ SkStream* FontConfigIPC::openStream(const FontIdentity& identity) {
     return NULL;
   }
 
-  SkStream* stream = StreamFromFD(result_fd);
+  SkStreamAsset* stream = StreamFromFD(result_fd);
   CloseFD(result_fd);
   return stream;
 }
