fc235ed69ddd0a248420ae3294a7d469b5ddca58
robhogan@gmail.com <robhogan@gmail.com>
2015-01-27 19:13:04 +0000

    Ensure we remove floats from descendants even if they have become inline
    This is a second go at https://codereview.chromium.org/828163002/. My solution
    there was wrong. The real problem was that we were failing to remove the float
    from the float-lists of all its parents descendants. The reason this has started
    happening is because we can now make children inline while leaving their float
    lists in place: this was introduced in https://codereview.chromium.org/253313005.
    
    So if we are trying to remove a float from float-lists, inspect its children
    even if they are inline.
    
    BUG=448067, 445285
    
    Review URL: https://codereview.chromium.org/850143002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189049 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderHTMLCanvas.cpp
+++ third_party/WebKit/Source/core/rendering/RenderHTMLCanvas.cpp
@@ -72,16 +72,14 @@ void RenderHTMLCanvas::canvasSizeChanged()
     if (!preferredLogicalWidthsDirty())
         setPreferredLogicalWidthsDirty();
 
-    if (selfNeedsLayout())
-        return;
-
     LayoutSize oldSize = size();
     updateLogicalWidth();
     updateLogicalHeight();
     if (oldSize == size())
         return;
 
-    setNeedsLayout();
+    if (!selfNeedsLayout())
+        setNeedsLayout();
 }
 
 CompositingReasons RenderHTMLCanvas::additionalCompositingReasons() const
