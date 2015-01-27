a2dbec5794e4594ccbcb7faec5fcd365a5e10dbb
dvadym <dvadym@chromium.org>
2015-01-27 04:15:24 -0800

    Libsecret kill switch by Finch.
    This is client side, the server config is in CL 84424958
    
    BUG=450225
    
    Review URL: https://codereview.chromium.org/851053003
    
    Cr-Commit-Position: refs/heads/master@{#313260}

--- chrome/browser/password_manager/password_store_factory.cc
+++ chrome/browser/password_manager/password_store_factory.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/password_manager/native_backend_gnome_x.h"
 #endif
 #if defined(USE_LIBSECRET)
+#include "base/metrics/field_trial.h"
 #include "chrome/browser/password_manager/native_backend_libsecret.h"
 #endif
 #include "chrome/browser/password_manager/native_backend_kwallet_x.h"
@@ -54,6 +55,11 @@ namespace {
 #if !defined(OS_CHROMEOS) && defined(USE_X11)
 const LocalProfileId kInvalidLocalProfileId =
     static_cast<LocalProfileId>(0);
+#endif
+
+#if defined(USE_LIBSECRET)
+const char kLibsecretFieldTrialName[] = "Libsecret";
+const char kLibsecretFieldTrialDisabledGroupName[] = "Disabled";
 #endif
 
 void ReportOsPassword(password_manager_util::OsPasswordStatus status) {
@@ -221,12 +227,15 @@ KeyedService* PasswordStoreFactory::BuildServiceInstanceFor(
              desktop_env == base::nix::DESKTOP_ENVIRONMENT_UNITY ||
              desktop_env == base::nix::DESKTOP_ENVIRONMENT_XFCE) {
 #if defined(USE_LIBSECRET)
-    VLOG(1) << "Trying libsecret for password storage.";
-    backend.reset(new NativeBackendLibsecret(id));
-    if (backend->Init())
-      VLOG(1) << "Using libsecret keyring for password storage.";
-    else
-      backend.reset();
+    if (base::FieldTrialList::FindFullName(kLibsecretFieldTrialName) !=
+        kLibsecretFieldTrialDisabledGroupName) {
+      VLOG(1) << "Trying libsecret for password storage.";
+      backend.reset(new NativeBackendLibsecret(id));
+      if (backend->Init())
+        VLOG(1) << "Using libsecret keyring for password storage.";
+      else
+        backend.reset();
+    }
 #endif  // defined(USE_LIBSECRET)
     if (!backend.get()) {
 #if defined(USE_GNOME_KEYRING)
