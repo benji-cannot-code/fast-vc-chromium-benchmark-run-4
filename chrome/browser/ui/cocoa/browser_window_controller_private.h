fd5838a40c4a58366fd59f735c21b1ddccfd6a70
erikchen <erikchen@chromium.org>
2015-01-26 18:29:19 -0800

    Reland "mac: Layer back the root view of the browser window on OSX 10.9+."
    The original CL was reverted because it caused non-deterministic failures in
    browser tests that used the file tab-restore-visibility.html. The test file had
    an edge case that caused thrashing and prevented the page from loading. This
    has since been fixed.
    
    > BrowserWindowController always layer-backs the contentView of the window. If
    > Chrome were linked against OSX 10.9, this would also cause the root view to be
    > layer backed. Since Chrome is linked against OSX 10.6, the root view by default
    > is not layer backed.
    >
    > BrowserWindowController is going to have a custom AppKit fullscreen animation,
    > which requires that the root view is layer backed. While it is possible to
    > implement a custom AppKit fullscreen animation as early as OSX 10.7, the
    > effects of layer backing the root view on OSX 10.7 are murky. It seems much
    > safer to just layer back the root view on OSX 10.9+, and only enable the custom
    > AppKit fullscreen animation on OSX 10.9+.
    >
    > As far as I know, this change has no detrimental side effects. There is no
    > spike in memory usage
    > (https://code.google.com/p/chromium/issues/detail?id=396264#c11), presumably
    > because NSThemeFrame implements -wantsUpdateLayer on OSX 10.9+.
    >
    > BUG=413009, 414527
    > Committed: https://crrev.com/8dec34f8f3504352e4eef95b2aaa36c561aea930
    > Cr-Commit-Position: refs/heads/master@{#311191}
    
    BUG=413009, 414527
    TBR=rsesek@chromium.org
    
    Review URL: https://codereview.chromium.org/878673006
    
    Cr-Commit-Position: refs/heads/master@{#313207}

--- chrome/browser/ui/cocoa/browser_window_controller_private.h
+++ chrome/browser/ui/cocoa/browser_window_controller_private.h
@@ -160,6 +160,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // out of AppKit Fullscreen.
 - (BOOL)shouldUseCustomAppKitFullscreenTransition;
 
+// Whether the root view of the window should be layer backed.
+- (BOOL)wantsRootViewToBeLayerBacked;
+
 @end  // @interface BrowserWindowController(Private)
 
 #endif  // CHROME_BROWSER_UI_COCOA_BROWSER_WINDOW_CONTROLLER_PRIVATE_H_
