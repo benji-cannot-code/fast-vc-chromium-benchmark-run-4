cee003d4adec86c6b96edbfe822a5c17e838d3f0
spang <spang@chromium.org>
2015-01-26 17:57:22 -0800

    ozone: evdev: Move touch ui::Event transformation to EventFactoryEvdev
    This is part of a series moving IO on evdev devices to a fast thread,
    since UI thread is too slow for the cursor.
    
    DeviceDataManager is a UI thread object, so move final transformation
    of touches into EventFactoryEvdev prior to dispatch into UI. This
    will avoid sharing state across threads.
    
    BUG=449710
    TEST=boot link_freon & move cursor
    
    Review URL: https://codereview.chromium.org/874713002
    
    Cr-Commit-Position: refs/heads/master@{#313204}

---
+++ ui/events/ozone/evdev/event_dispatch_callback.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/events/ozone/evdev/event_dispatch_callback.h"
+
+namespace ui {
+
+TouchEventParams::TouchEventParams(int device_id,
+                                   int touch_id,
+                                   EventType type,
+                                   const gfx::PointF& location,
+                                   const gfx::Vector2dF& radii,
+                                   float pressure,
+                                   const base::TimeDelta& timestamp)
+    : device_id(device_id),
+      touch_id(touch_id),
+      type(type),
+      location(location),
+      radii(radii),
+      pressure(pressure),
+      timestamp(timestamp) {
+}
+
+TouchEventParams::TouchEventParams(const TouchEventParams& other) = default;
+
+TouchEventParams::~TouchEventParams() {
+}
+
+}  // namspace ui
--- ui/events/ozone/events_ozone.gyp
+++ ui/events/ozone/events_ozone.gyp
@@ -82,6 +82,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'evdev/event_converter_evdev_impl.h',
       'evdev/event_device_info.cc',
       'evdev/event_device_info.h',
+      'evdev/event_dispatch_callback.cc',
+      'evdev/event_dispatch_callback.h',
       'evdev/event_factory_evdev.cc',
       'evdev/event_factory_evdev.h',
       'evdev/event_modifiers_evdev.cc',
