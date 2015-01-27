68ee73431a73d488a5e9e2a71e6fa15cc2e371fd
hendrikw <hendrikw@chromium.org>
2015-01-27 11:55:42 -0800

    cc: Prevent resources from being allocated for solid tiles
    If the tile is solid, don't create a resource.  In tile manager,
    check for null resources before attempting to release.
    
    BUG=445919
    
    Review URL: https://codereview.chromium.org/884453003
    
    Cr-Commit-Position: refs/heads/master@{#313330}

--- cc/resources/tile_manager.cc
+++ cc/resources/tile_manager.cc
@@ -831,8 +831,10 @@ void TileManager::UpdateTileDrawInfo(
 
   if (analysis.is_solid_color) {
     draw_info.set_solid_color(analysis.solid_color);
-    resource_pool_->ReleaseResource(resource.Pass());
+    if (resource)
+      resource_pool_->ReleaseResource(resource.Pass());
   } else {
+    DCHECK(resource);
     draw_info.set_use_resource();
     draw_info.resource_ = resource.Pass();
   }
