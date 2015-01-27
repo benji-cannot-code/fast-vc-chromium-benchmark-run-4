d13c90bb8b6afb67e771f88fb32eb1dc35f46d17
raymes <raymes@chromium.org>
2015-01-27 00:49:39 -0800

    OOP PDF: Ensure the load callback gets fired multiple times for print preview
    Previously we weren't firing the document load callback multiple times
    when print preview was reset with a new document. This was causing
    print preview to hang.
    
    BUG=452366
    
    Review URL: https://codereview.chromium.org/880813002
    
    Cr-Commit-Position: refs/heads/master@{#313246}

--- chrome/browser/resources/pdf/pdf_scripting_api.js
+++ chrome/browser/resources/pdf/pdf_scripting_api.js
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @param {Object} plugin the plugin element containing the pdf viewer.
  */
 function PDFScriptingAPI(window, plugin) {
+  this.loaded_ = false;
   this.pendingScriptingMessages_ = [];
   this.setPlugin(plugin);
 
@@ -29,10 +30,9 @@ function PDFScriptingAPI(window, plugin) {
                                         event.data.viewportHeight);
         break;
       case 'documentLoaded':
-        if (this.loadCallback_) {
+        this.loaded_ = true;
+        if (this.loadCallback_)
           this.loadCallback_();
-          this.loadCallback_ = null;
-        }
         break;
       case 'getAccessibilityJSONReply':
         if (this.accessibilityCallback_) {
@@ -74,6 +74,11 @@ PDFScriptingAPI.prototype = {
     this.plugin_ = plugin;
 
     if (this.plugin_) {
+      // Send a message to ensure the postMessage channel is initialized which
+      // allows us to receive messages.
+      this.sendMessage_({
+        type: 'initialize'
+      });
       // Flush pending messages.
       while (this.pendingScriptingMessages_.length > 0)
         this.sendMessage_(this.pendingScriptingMessages_.shift());
@@ -92,17 +97,11 @@ PDFScriptingAPI.prototype = {
    * Sets the callback which will be run when the PDF document has finished
    * loading. If the document is already loaded, it will be run immediately.
    * @param {Function} callback the callback to be called.
-   * @return {boolean} false if there is a callback already set and true
-   *     otherwise.
    */
   setLoadCallback: function(callback) {
-    if (this.loadCallback_)
-      return false;
     this.loadCallback_ = callback;
-    this.sendMessage_({
-      type: 'isDocumentLoaded'
-    });
-    return true;
+    if (this.loaded_ && this.loadCallback_)
+      this.loadCallback_();
   },
 
   /**
@@ -113,6 +112,7 @@ PDFScriptingAPI.prototype = {
    * @param {boolean} modifiable whether or not the document is modifiable.
    */
   resetPrintPreviewMode: function(url, grayscale, pageNumbers, modifiable) {
+    this.loaded_ = false;
     this.sendMessage_({
       type: 'resetPrintPreviewMode',
       url: url,
