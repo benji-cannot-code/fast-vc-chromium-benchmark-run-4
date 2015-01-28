fd97cc35ece54da07fb55476445bf2f904541f50
sheckylin <sheckylin@chromium.org>
2015-01-27 20:25:14 -0800

    Dump property values for the touch log source
    The CL dumps gesture property values for touch devices that run
    with the CrOS gesture library. The format is designed to be
    backward-compatible with the X11 behaviour.
    
    Contributed by sheckylin@chromium.org
    
    BUG=450159
    TEST=samus ChromeOS build
    
    Review URL: https://codereview.chromium.org/864253002
    
    Cr-Commit-Position: refs/heads/master@{#313446}

--- chrome/browser/chromeos/system_logs/touch_log_source_ozone.cc
+++ chrome/browser/chromeos/system_logs/touch_log_source_ozone.cc
@@ -7,12 +7,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/touch/touch_hud_debug.h"
 #include "base/bind.h"
+#include "base/bind_helpers.h"
+#include "base/callback.h"
 #include "base/command_line.h"
 #include "base/json/json_string_value_serializer.h"
 #include "base/logging.h"
+#include "base/message_loop/message_loop.h"
 #include "base/process/launch.h"
 #include "components/feedback/feedback_util.h"
 #include "content/public/browser/browser_thread.h"
+#include "ui/ozone/public/input_controller.h"
+#include "ui/ozone/public/ozone_platform.h"
 
 using content::BrowserThread;
 
@@ -20,7 +25,26 @@ namespace {
 
 const char kHUDLogDataKey[] = "hud_log";
 
-void GetTouchLogsOzone(system_logs::SystemLogsResponse* response) {
+// The prefix "hack-33025" was historically chosen in http://crbug.com/139715.
+// We continue to go with it in order to be compatible with the existing touch
+// log processing toolchain.
+const char kDeviceStatusLogDataKey[] = "hack-33025-touchpad";
+
+// Callback for handing the outcome of GetTouchDeviceStatus(). Appends the
+// collected log to the SystemLogsResponse map.
+void OnStatusLogCollected(scoped_ptr<system_logs::SystemLogsResponse> response,
+                          const system_logs::SysLogsSourceCallback& callback,
+                          scoped_ptr<std::string> log) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+  (*response)[kDeviceStatusLogDataKey] = *log;
+
+  BrowserThread::PostTask(
+      BrowserThread::UI, FROM_HERE,
+      base::Bind(callback, base::Owned(response.release())));
+}
+
+// Collect touch HUD debug logs. This needs to be done on the UI thread.
+void CollectTouchHudDebugLog(system_logs::SystemLogsResponse* response) {
   scoped_ptr<base::DictionaryValue> dictionary =
       ash::TouchHudDebug::GetAllAsDictionary();
   if (!dictionary->empty()) {
@@ -30,10 +54,6 @@ void GetTouchLogsOzone(system_logs::SystemLogsResponse* response) {
     if (json.Serialize(*dictionary) && !touch_log.empty())
       (*response)[kHUDLogDataKey] = touch_log;
   }
-
-  // TODO(sheckylin): Add ozone touch log collection implementation. See
-  // http://crbug.com/400022.
-  NOTIMPLEMENTED();
 }
 
 }  // namespace
@@ -44,10 +64,12 @@ void TouchLogSource::Fetch(const SysLogsSourceCallback& callback) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
   DCHECK(!callback.is_null());
 
-  SystemLogsResponse* response = new SystemLogsResponse;
-  BrowserThread::PostBlockingPoolTaskAndReply(
-      FROM_HERE, base::Bind(&GetTouchLogsOzone, response),
-      base::Bind(callback, base::Owned(response)));
+  scoped_ptr<SystemLogsResponse> response(new SystemLogsResponse);
+  CollectTouchHudDebugLog(response.get());
+
+  // Collect touch device status logs.
+  ui::OzonePlatform::GetInstance()->GetInputController()->GetTouchDeviceStatus(
+      base::Bind(&OnStatusLogCollected, base::Passed(&response), callback));
 }
 
 }  // namespace system_logs
