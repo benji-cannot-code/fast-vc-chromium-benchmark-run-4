90a0b4c43898bdcbd0572c46dc6692738969758a
yutak@chromium.org <yutak@chromium.org>
2015-01-28 06:03:32 +0000

    Oilpan: Build fix after r 189089.
    BUG=none
    TBR=oilpan-reviews@chromium.org
    
    Review URL: https://codereview.chromium.org/885583002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189092 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/RemoteFrame.cpp
+++ third_party/WebKit/Source/core/frame/RemoteFrame.cpp
@@ -38,6 +38,7 @@ void RemoteFrame::trace(Visitor* visitor)
 {
     visitor->trace(m_view);
     visitor->trace(m_domWindow);
+    visitor->trace(m_windowProxyManager);
     Frame::trace(visitor);
 }
 
