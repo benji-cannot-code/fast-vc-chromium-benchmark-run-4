c9675fe489604bb224a67fe0719eba21d3a31998
reillyg <reillyg@chromium.org>
2015-01-28 10:47:43 -0800

    Observe UsbService from the FILE thread in DevicePermissionsManager.
    As in the UsbEventRouter the UsbService must be observed from the FILE
    thread (until bug 427985 is resolved, which will be soon). This change
    fixes a DCHECK(CalledOnValidThread()) in UsbService::AddObserver when
    permission to access an ephemeral (no serial number) device is added and
    the DevicePermissionsManager starts listening for disconnection of that
    device.
    
    BUG=452652
    
    Review URL: https://codereview.chromium.org/882813002
    
    Cr-Commit-Position: refs/heads/master@{#313548}

--- extensions/browser/api/device_permissions_manager.h
+++ extensions/browser/api/device_permissions_manager.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_checker.h"
 #include "components/keyed_service/content/browser_context_keyed_service_factory.h"
 #include "components/keyed_service/core/keyed_service.h"
+#include "content/public/browser/browser_thread.h"
 #include "device/usb/usb_service.h"
 #include "extensions/browser/process_manager.h"
 #include "extensions/browser/process_manager_observer.h"
@@ -135,8 +136,7 @@ class DevicePermissions {
 // Manages saved device permissions for all extensions.
 class DevicePermissionsManager : public KeyedService,
                                  public base::NonThreadSafe,
-                                 public ProcessManagerObserver,
-                                 public device::UsbService::Observer {
+                                 public ProcessManagerObserver {
  public:
   static DevicePermissionsManager* Get(content::BrowserContext* context);
 
@@ -172,6 +172,8 @@ class DevicePermissionsManager : public KeyedService,
   void Clear(const std::string& extension_id);
 
  private:
+  class FileThreadHelper;
+
   friend class DevicePermissionsManagerFactory;
   FRIEND_TEST_ALL_PREFIXES(DevicePermissionsManagerTest, SuspendExtension);
 
@@ -180,19 +182,18 @@ class DevicePermissionsManager : public KeyedService,
 
   DevicePermissions* Get(const std::string& extension_id) const;
   DevicePermissions* GetOrInsert(const std::string& extension_id);
+  void OnDeviceRemoved(scoped_refptr<device::UsbDevice> device);
 
   // ProcessManagerObserver implementation
   void OnBackgroundHostClose(const std::string& extension_id) override;
-
-  // device::UsbService::Observer implementation
-  void OnDeviceRemoved(scoped_refptr<device::UsbDevice> device) override;
 
   content::BrowserContext* context_;
   std::map<std::string, DevicePermissions*> extension_id_to_device_permissions_;
   ScopedObserver<ProcessManager, ProcessManagerObserver>
       process_manager_observer_;
-  ScopedObserver<device::UsbService, device::UsbService::Observer>
-      usb_service_observer_;
+  FileThreadHelper* helper_;
+
+  base::WeakPtrFactory<DevicePermissionsManager> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(DevicePermissionsManager);
 };
