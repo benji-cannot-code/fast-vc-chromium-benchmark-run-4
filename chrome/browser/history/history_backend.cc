962dec100777662ef5b9c132ac2e5cc488a8351e
sdefresne <sdefresne@chromium.org>
2015-01-28 03:57:05 -0800

    Remove unused #include in //chrome/browser/history
    BUG=None
    
    Review URL: https://codereview.chromium.org/882023004
    
    Cr-Commit-Position: refs/heads/master@{#313482}

--- chrome/browser/history/history_backend.cc
+++ chrome/browser/history/history_backend.cc
@@ -24,14 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
-#include "chrome/browser/chrome_notification_types.h"
-#include "chrome/browser/history/in_memory_history_backend.h"
 #include "chrome/browser/history/in_memory_history_backend.h"
 #include "chrome/browser/history/typed_url_syncable_service.h"
-#include "chrome/browser/history/typed_url_syncable_service.h"
-#include "chrome/common/chrome_constants.h"
 #include "chrome/common/importer/imported_favicon_usage.h"
-#include "chrome/common/url_constants.h"
 #include "components/favicon_base/select_favicon_frames.h"
 #include "components/history/core/browser/download_constants.h"
 #include "components/history/core/browser/download_row.h"
@@ -49,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/gfx/codec/png_codec.h"
 #include "url/gurl.h"
+#include "url/url_constants.h"
 
 #if defined(OS_ANDROID)
 #include "chrome/browser/history/android/android_provider_backend.h"
