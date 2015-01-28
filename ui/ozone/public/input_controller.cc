97117f7181a850fb6fe1b87e90dfd6a0ddb98b55
sheckylin <sheckylin@chromium.org>
2015-01-28 11:04:10 -0800

    Dump property values for the touch log source
    The CL dumps gesture property values for touch devices that run
    with the CrOS gesture library. The format is designed to be
    backward-compatible with the X11 behaviour.
    
    Contributed by sheckylin@chromium.org
    
    BUG=450159
    TEST=samus ChromeOS build
    
    Committed: https://crrev.com/8b1fb4fdf2b13b71e969cd541e500549163f742b
    Cr-Commit-Position: refs/heads/master@{#313446}
    
    Review URL: https://codereview.chromium.org/864253002
    
    Cr-Commit-Position: refs/heads/master@{#313555}

--- ui/ozone/public/input_controller.cc
+++ ui/ozone/public/input_controller.cc
@@ -37,6 +37,7 @@ class StubInputController : public InputController {
   void SetMouseSensitivity(int value) override;
   void SetPrimaryButtonRight(bool right) override;
   void SetTapToClickPaused(bool state) override;
+  void GetTouchDeviceStatus(const GetTouchDeviceStatusReply& reply) override;
   void DisableInternalTouchpad() override;
   void EnableInternalTouchpad() override;
   void DisableInternalKeyboardExceptKeys(
@@ -123,6 +124,11 @@ void StubInputController::SetPrimaryButtonRight(bool right) {
 
 void StubInputController::SetTapToClickPaused(bool state) {
   NOTIMPLEMENTED();
+}
+
+void StubInputController::GetTouchDeviceStatus(
+    const GetTouchDeviceStatusReply& reply) {
+  reply.Run(scoped_ptr<std::string>(new std::string));
 }
 
 void StubInputController::DisableInternalTouchpad() {
