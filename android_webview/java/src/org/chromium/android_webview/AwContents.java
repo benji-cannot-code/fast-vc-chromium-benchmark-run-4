ea6ec722eb76c75cc187ff356aec0a5f4f9b29c1
paulmiller <paulmiller@chromium.org>
2015-01-28 15:36:50 -0800

    Return void in zoomBy plumbing to match zoomBy public API
    BUG=410900
    
    Review URL: https://codereview.chromium.org/881383002
    
    Cr-Commit-Position: refs/heads/master@{#313617}

--- android_webview/java/src/org/chromium/android_webview/AwContents.java
+++ android_webview/java/src/org/chromium/android_webview/AwContents.java
@@ -1711,7 +1711,8 @@ public class AwContents implements SmartClipProvider {
         if (!canZoomIn()) {
             return false;
         }
-        return zoomBy(1.25f);
+        zoomBy(1.25f);
+        return true;
     }
 
     /**
@@ -1723,7 +1724,8 @@ public class AwContents implements SmartClipProvider {
         if (!canZoomOut()) {
             return false;
         }
-        return zoomBy(0.8f);
+        zoomBy(0.8f);
+        return true;
     }
 
     /**
@@ -1731,12 +1733,12 @@ public class AwContents implements SmartClipProvider {
      */
     // This method uses the term 'zoom' for legacy reasons, but relates
     // to what chrome calls the 'page scale factor'.
-    public boolean zoomBy(float delta) {
-        if (isDestroyed()) return false;
+    public void zoomBy(float delta) {
+        if (isDestroyed()) return;
         if (delta < 0.01f || delta > 100.0f) {
             throw new IllegalStateException("zoom delta value outside [0.01, 100] range.");
         }
-        return mContentViewCore.pinchByDelta(delta);
+        mContentViewCore.pinchByDelta(delta);
     }
 
     /**
