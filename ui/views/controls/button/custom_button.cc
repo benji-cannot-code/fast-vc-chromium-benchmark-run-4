addeff1f5fd91ec0e7e9f7d25b77e68558ba1360
Sadrul Habib Chowdhury <sadrul@chromium.org>
2015-01-28 10:27:23 -0500

    views: Remove dead code in CustomButton.
    CustomButtonStateChangedDelegate was itnroduced in http://crrev.com/159994, and
    its use was removed in http://crrev.com/185369 It hasn't been used since. So
    remove the code.
    
    BUG=none
    R=tfarina@chromium.org
    
    Review URL: https://codereview.chromium.org/872473004
    
    Cr-Commit-Position: refs/heads/master@{#313515}

--- ui/views/controls/button/custom_button.cc
+++ ui/views/controls/button/custom_button.cc
@@ -74,8 +74,6 @@ void CustomButton::SetState(ButtonState state) {
 
   state_ = state;
   StateChanged();
-  if (state_changed_delegate_.get())
-    state_changed_delegate_->StateChanged(state_);
   SchedulePaint();
 }
 
