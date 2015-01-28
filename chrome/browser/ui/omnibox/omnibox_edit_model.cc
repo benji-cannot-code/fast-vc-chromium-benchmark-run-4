6f325738e3ff5baf3c90fd090a21622c9afe7931
vadimt <vadimt@chromium.org>
2015-01-28 08:57:44 -0800

    Instrumenting more of WM_ message processing for jank This covers some Omnibox stuff, which is probably responsible for the jank in keyboard messages processing and drilling down some of issues. There are some issues that require advanced investigations that are not included in this CL: 1. PeekMessage, which is probably processing hooks 2. DispatchMessage, maybe, having same issues 3. GetQueueStatus (!!!) 4. DefWndProc
    BUG=440919
    
    Review URL: https://codereview.chromium.org/879153002
    
    Cr-Commit-Position: refs/heads/master@{#313529}

--- chrome/browser/ui/omnibox/omnibox_edit_model.cc
+++ chrome/browser/ui/omnibox/omnibox_edit_model.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/format_macros.h"
 #include "base/metrics/histogram.h"
 #include "base/prefs/pref_service.h"
+#include "base/profiler/scoped_tracker.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
@@ -554,6 +555,10 @@ void OmniboxEditModel::Revert() {
 void OmniboxEditModel::StartAutocomplete(
     bool has_selected_text,
     bool prevent_inline_autocomplete) {
+  // TODO(vadimt): Remove ScopedTracker below once crbug.com/440919 is fixed.
+  tracked_objects::ScopedTracker tracking_profile(
+      FROM_HERE_WITH_EXPLICIT_FUNCTION(
+          "440919 OmniboxEditModel::StartAutocomplete"));
   size_t cursor_position;
   if (inline_autocomplete_text_.empty()) {
     // Cursor position is equivalent to the current selection's end.
