829711d615e057d19bad2175a8bad1e443f46b90
xhwang <xhwang@chromium.org>
2015-01-28 13:09:59 -0800

    media: Rename PERMISSION_PROTECTED_MEDIA to PERMISSION_PROTECTED_MEDIA_IDENTIFIER.
    This makes the naming consistent with other use cases, e.g. CONTENT_SETTINGS_TYPE_PROTECTED_MEDIA_IDENTIFIER, ProtectedMediaIdentifierPermissionContext etc.
    
    BUG=446263
    TEST=Renaming only.
    
    Review URL: https://codereview.chromium.org/881023002
    
    Cr-Commit-Position: refs/heads/master@{#313582}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -1914,7 +1914,7 @@ void ChromeContentBrowserClient::RequestPermission(
                               user_gesture,
                               result_callback);
       break;
-    case content::PERMISSION_PROTECTED_MEDIA:
+    case content::PERMISSION_PROTECTED_MEDIA_IDENTIFIER:
 #if defined(OS_ANDROID)
       ProtectedMediaIdentifierPermissionContextFactory::GetForProfile(profile)
           ->RequestPermission(web_contents,
@@ -1963,7 +1963,7 @@ content::PermissionStatus ChromeContentBrowserClient::GetPermissionStatus(
     case content::PERMISSION_GEOLOCATION:
       context = GeolocationPermissionContextFactory::GetForProfile(profile);
       break;
-    case content::PERMISSION_PROTECTED_MEDIA:
+    case content::PERMISSION_PROTECTED_MEDIA_IDENTIFIER:
 #if defined(OS_ANDROID)
       context = ProtectedMediaIdentifierPermissionContextFactory::GetForProfile(
           profile);
@@ -2019,7 +2019,7 @@ void ChromeContentBrowserClient::CancelPermissionRequest(
       GeolocationPermissionContextFactory::GetForProfile(profile)
           ->CancelPermissionRequest(web_contents, request_id);
       break;
-    case content::PERMISSION_PROTECTED_MEDIA:
+    case content::PERMISSION_PROTECTED_MEDIA_IDENTIFIER:
 #if defined(OS_ANDROID)
       ProtectedMediaIdentifierPermissionContextFactory::GetForProfile(profile)
           ->CancelPermissionRequest(web_contents, request_id);
@@ -2049,7 +2049,7 @@ static ContentSettingsType PermissionToContentSetting(
     case content::PERMISSION_GEOLOCATION:
       return CONTENT_SETTINGS_TYPE_GEOLOCATION;
 #if defined(OS_ANDROID) || defined(OS_CHROMEOS)
-    case content::PERMISSION_PROTECTED_MEDIA:
+    case content::PERMISSION_PROTECTED_MEDIA_IDENTIFIER:
       return CONTENT_SETTINGS_TYPE_PROTECTED_MEDIA_IDENTIFIER;
 #endif
     default:
