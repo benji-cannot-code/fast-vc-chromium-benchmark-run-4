54fae84dae84d4dffbfa90240de2ce1fc912e253
kinuko@chromium.org <kinuko@chromium.org>
2015-01-28 12:24:46 +0000

    Initialize KURL::blankURL() in CoreInitializer::init()
    So that DEFINE_LOCAL_STATIC in blankURL() becomes safe.
    
    BUG=373140
    
    Review URL: https://codereview.chromium.org/879933002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189105 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/Init.cpp
+++ third_party/WebKit/Source/core/Init.cpp
@@ -57,6 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/FontFamilyNames.h"
 #include "platform/Partitions.h"
 #include "platform/PlatformThreadData.h"
+#include "platform/weborigin/KURL.h"
 #include "wtf/text/StringStatics.h"
 
 namespace blink {
@@ -104,6 +105,7 @@ void CoreInitializer::init()
     QualifiedName::init();
     Partitions::init();
     EventTracer::initialize();
+    KURL::initialize();
 
     registerEventFactory();
 
