ca3d815b5e0a10cd66bdd41b4e674d92c7cb6cae
tfarina <tfarina@chromium.org>
2015-01-28 06:35:56 -0800

    content: Cleanup - remove unused 'WebKit' forward declaration.
    BUG=None
    R=michaeln@chromium.org
    
    Review URL: https://codereview.chromium.org/871243002
    
    Cr-Commit-Position: refs/heads/master@{#313506}

--- content/renderer/service_worker/embedded_worker_dispatcher.h
+++ content/renderer/service_worker/embedded_worker_dispatcher.h
@@ -14,10 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 struct EmbeddedWorkerMsg_StartWorker_Params;
 class GURL;
 
-namespace WebKit {
-class WebEmbeddedWorker;
-}
-
 namespace content {
 
 // A tiny dispatcher which handles embedded worker start/stop messages.
