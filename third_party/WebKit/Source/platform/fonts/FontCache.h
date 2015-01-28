02e56d8eb87681e19aabf82de14eb1f7da0b15cf
bungeman@chromium.org <bungeman@chromium.org>
2015-01-28 02:28:52 +0000

    Remove no longer used include from FontCache.h.
    The include uscript.h is no longer required in this header.
    
    Review URL: https://codereview.chromium.org/869263003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189077 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/fonts/FontCache.h
+++ third_party/WebKit/Source/platform/fonts/FontCache.h
@@ -50,10 +50,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 struct IDWriteFactory;
 #endif
 
-#if OS(ANDROID)
-#include <unicode/uscript.h>
-#endif
-
 class SkTypeface;
 
 namespace blink {
