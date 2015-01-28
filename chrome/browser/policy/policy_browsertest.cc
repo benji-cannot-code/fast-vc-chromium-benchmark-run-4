6ced0ad5e3ab73fcbeabc8ea20d106267602001b
asvitkine <asvitkine@chromium.org>
2015-01-28 06:32:19 -0800

    Expose API on VariationsService to set the restrict mode param.
    This paves the way to allowing logic living elsewhere in Chrome to
    specify a value for this param (which will come in a later CL).
    
    Also, cleans up a couple areas of the code, including cleaning up the
    unit test for the URL generating function to abstract away the policy
    pref code and using a weak pointer instead of base::Unretained() in
    a callback.
    
    BUG=452605
    
    Review URL: https://codereview.chromium.org/883803002
    
    Cr-Commit-Position: refs/heads/master@{#313504}

--- chrome/browser/policy/policy_browsertest.cc
+++ chrome/browser/policy/policy_browsertest.cc
@@ -3549,7 +3549,7 @@ IN_PROC_BROWSER_TEST_F(PolicyVariationsServiceTest, VariationsURLIsValid) {
 
   const GURL url =
       chrome_variations::VariationsService::GetVariationsServerURL(
-          g_browser_process->local_state());
+          g_browser_process->local_state(), std::string());
   EXPECT_TRUE(StartsWithASCII(url.spec(), default_variations_url, true));
   std::string value;
   EXPECT_TRUE(net::GetValueForKeyInQuery(url, "restrict", &value));
