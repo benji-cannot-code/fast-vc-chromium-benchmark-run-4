1dbee8fcb85b42a5e13b3bf5fb1b29b29b17fff0
lukasza <lukasza@chromium.org>
2015-01-28 12:29:01 -0800

    Merged PolicyServiceWatcher into PolicyWatcher.
    BUG=368321
    
    Review URL: https://codereview.chromium.org/867463006
    
    Cr-Commit-Position: refs/heads/master@{#313570}

--- remoting/host/policy_hack/policy_watcher_unittest.cc
+++ remoting/host/policy_hack/policy_watcher_unittest.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "policy/policy_constants.h"
 #include "remoting/host/dns_blackhole_checker.h"
 #include "remoting/host/policy_hack/mock_policy_callback.h"
-#include "remoting/host/policy_hack/policy_service_watcher.h"
 #include "remoting/host/policy_hack/policy_watcher.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -36,7 +35,7 @@ class PolicyWatcherTest : public testing::Test {
 
     // Retaining a raw pointer to keep control over policy contents.
     policy_loader_ = new policy::FakeAsyncPolicyLoader(message_loop_proxy_);
-    policy_watcher_ = PolicyServiceWatcher::CreateFromPolicyLoader(
+    policy_watcher_ = PolicyWatcher::CreateFromPolicyLoader(
         message_loop_proxy_, make_scoped_ptr(policy_loader_));
 
     nat_true_.SetBoolean(policy::key::kRemoteAccessHostFirewallTraversal, true);
