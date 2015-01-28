04e3e4b0dd25e832a1543421aaa655fea41b455a
pfeldman@chromium.org <pfeldman@chromium.org>
2015-01-28 21:17:13 +0000

    DevTools: restore worker debugging with source maps, cover with tests.
    Review URL: https://codereview.chromium.org/870893003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189139 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sdk/ContentProviders.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/ContentProviders.js
@@ -192,13 +192,11 @@ WebInspector.ConcatenatedScriptsContentProvider.prototype = {
 /**
  * @constructor
  * @implements {WebInspector.ContentProvider}
- * @param {!WebInspector.Target} target
  * @param {string} sourceURL
  * @param {!WebInspector.ResourceType} contentType
  */
-WebInspector.CompilerSourceMappingContentProvider = function(target, sourceURL, contentType)
+WebInspector.CompilerSourceMappingContentProvider = function(sourceURL, contentType)
 {
-    this._target = target;
     this._sourceURL = sourceURL;
     this._contentType = contentType;
 }
@@ -228,7 +226,7 @@ WebInspector.CompilerSourceMappingContentProvider.prototype = {
      */
     requestContent: function(callback)
     {
-        this._target.networkAgent().loadResourceForFrontend(this._target.resourceTreeModel.mainFrame.id, this._sourceURL, undefined, contentLoaded.bind(this));
+        WebInspector.NetworkManager.loadResourceForFrontend(this._sourceURL, undefined, contentLoaded.bind(this));
 
         /**
          * @param {?Protocol.Error} error
