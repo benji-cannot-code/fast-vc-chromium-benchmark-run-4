1366ada0db1046da665757fecf062b9ae903b902
jamescook <jamescook@chromium.org>
2015-01-28 08:27:04 -0800

    Revert of Dump property values for the touch log source (patchset #11 id:200001 of https://codereview.chromium.org/864253002/)
    Reason for revert:
    Broke the arm-generic-freon builder on Chrome OS:
    
    https://uberchromegw.corp.google.com/i/chromeos/builders/arm-generic_freon%20nightly%20chromium%20PFQ/builds/549/steps/BuildPackages/logs/stdio
    
    See crbug.com/452911 - looks like a printf format specifier is wrong on 32 bit
    
    Original issue's description:
    > Dump property values for the touch log source
    >
    > The CL dumps gesture property values for touch devices that run
    > with the CrOS gesture library. The format is designed to be
    > backward-compatible with the X11 behaviour.
    >
    > Contributed by sheckylin@chromium.org
    >
    > BUG=450159
    > TEST=samus ChromeOS build
    >
    > Committed: https://crrev.com/8b1fb4fdf2b13b71e969cd541e500549163f742b
    > Cr-Commit-Position: refs/heads/master@{#313446}
    
    TBR=achuith@chromium.org,spang@chromium.org,alexst@chromium.org,adlr@chromium.org,miletus@chromium.org,sheckylin@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=450159
    
    Review URL: https://codereview.chromium.org/881353002
    
    Cr-Commit-Position: refs/heads/master@{#313522}

--- ui/ozone/public/input_controller.cc
+++ ui/ozone/public/input_controller.cc
@@ -37,7 +37,6 @@ class StubInputController : public InputController {
   void SetMouseSensitivity(int value) override;
   void SetPrimaryButtonRight(bool right) override;
   void SetTapToClickPaused(bool state) override;
-  void GetTouchDeviceStatus(const GetTouchDeviceStatusReply& reply) override;
   void DisableInternalTouchpad() override;
   void EnableInternalTouchpad() override;
   void DisableInternalKeyboardExceptKeys(
@@ -124,11 +123,6 @@ void StubInputController::SetPrimaryButtonRight(bool right) {
 
 void StubInputController::SetTapToClickPaused(bool state) {
   NOTIMPLEMENTED();
-}
-
-void StubInputController::GetTouchDeviceStatus(
-    const GetTouchDeviceStatusReply& reply) {
-  reply.Run(scoped_ptr<std::string>(new std::string));
 }
 
 void StubInputController::DisableInternalTouchpad() {
