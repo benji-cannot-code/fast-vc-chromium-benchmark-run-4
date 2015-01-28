0c5a2cff0a5dfef67d3c33f9c182d57568ef8c56
qyearsley <qyearsley@chromium.org>
2015-01-27 16:27:02 -0800

    Use builder_type when requesting/fetching builds, and add support for full linux builds.
    Changes in this CL:
     - In request_build.py, add a URL for linux try bot.
     - Add sample config for functional bisect.
     - Make perf-related config parameters optional.
     - Use builder type when starting build try jobs, and when fetching builds.
    
    A few other changes:
     - Update bisect_perf_regression.py file docstring.
     - Rename _BuilderTryjob to _StartBuilderTryJob and refactor.
     - Remove unused code in request_build.py.
    
    Proposed follow-up:
     - Move functions related to builder to be methods of one class, so that the builder-specific info/logic (e.g. builder bot and archive location) is in one place.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/806943007
    
    Cr-Commit-Position: refs/heads/master@{#313415}

--- tools/run-bisect-perf-regression.py
+++ tools/run-bisect-perf-regression.py
@@ -149,13 +149,7 @@ def _ValidatePerfConfigFile(config_contents):
   Returns:
     True if valid.
   """
-  required_parameters = [
-      'command',
-      'repeat_count',
-      'truncate_percent',
-      'max_time_minutes',
-  ]
-  return _ValidateConfigFile(config_contents, required_parameters)
+  return _ValidateConfigFile(config_contents, required_parameters=['command'])
 
 
 def _ValidateBisectConfigFile(config_contents):
@@ -170,16 +164,9 @@ def _ValidateBisectConfigFile(config_contents):
   Returns:
     True if valid.
   """
-  required_params = [
-      'command',
-      'good_revision',
-      'bad_revision',
-      'metric',
-      'repeat_count',
-      'truncate_percent',
-      'max_time_minutes',
-  ]
-  return _ValidateConfigFile(config_contents, required_params)
+  return _ValidateConfigFile(
+      config_contents,
+      required_parameters=['command', 'good_revision', 'bad_revision'])
 
 
 def _OutputFailedResults(text_to_print):
@@ -499,13 +486,13 @@ def _RunBisectionScript(
       '--command', config['command'],
       '--good_revision', config['good_revision'],
       '--bad_revision', config['bad_revision'],
-      '--metric', config['metric'],
       '--working_directory', working_directory,
       '--output_buildbot_annotations'
   ]
 
   # Add flags for any optional config parameters if given in the config.
   options = [
+      ('metric', '--metric'),
       ('repeat_count', '--repeat_test_count'),
       ('truncate_percent', '--truncate_percent'),
       ('max_time_minutes', '--max_time_minutes'),
