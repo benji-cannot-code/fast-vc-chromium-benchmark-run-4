fde6d23ef9dfe016607c135f5904e50e2bb32201
spang <spang@chromium.org>
2015-01-29 11:50:12 -0800

    ozone: Fix chromium-style warnings in rendering_helper.cc
    This fixes the linux_chromium_chromeos_ozone_rel_ng bot:
    
    ../../content/common/gpu/media/rendering_helper.cc:105:31: error: [chromium-style] Overriding method must be marked with 'override' or 'final'.
      virtual ~StubOzoneDelegate() {}
                                  ^
                                   override
    
    TBR=posciak (red build)
    NOTRY=true
    BUG=none
    TEST=linux_chromium_chromeos_ozone_rel_ng trybot
    
    Review URL: https://codereview.chromium.org/875573004
    
    Cr-Commit-Position: refs/heads/master@{#313763}

--- content/common/gpu/media/rendering_helper.cc
+++ content/common/gpu/media/rendering_helper.cc
@@ -103,7 +103,7 @@ class RenderingHelper::StubOzoneDelegate : public ui::PlatformWindowDelegate {
     platform_window_ = ui::OzonePlatform::GetInstance()->CreatePlatformWindow(
         this, gfx::Rect());
   }
-  virtual ~StubOzoneDelegate() {}
+  ~StubOzoneDelegate() override {}
 
   void OnBoundsChanged(const gfx::Rect& new_bounds) override {}
 
