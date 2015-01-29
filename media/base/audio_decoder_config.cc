8c3a991b2a8e0e0651284048cdd1772c29593a11
mikhal <mikhal@chromium.org>
2015-01-28 18:50:48 -0800

    Adding an ALAC enum to AudioDecoderConfig
    This CL enables using the ALAC codec within the media pipeline.
    This does not add ALAC support, as the ALAC codec is not part of the current build.
    
    BUG=441343
    
    Review URL: https://codereview.chromium.org/881603002
    
    Cr-Commit-Position: refs/heads/master@{#313654}

--- media/base/audio_decoder_config.cc
+++ media/base/audio_decoder_config.cc
@@ -151,6 +151,8 @@ std::string AudioDecoderConfig::GetHumanReadableCodecName() const {
       return "pcm_mulaw";
     case kCodecOpus:
       return "opus";
+    case kCodecALAC:
+      return "alac";
   }
   NOTREACHED();
   return "";
--- media/base/sample_format.cc
+++ media/base/sample_format.cc
@@ -21,6 +21,7 @@ int SampleFormatToBytesPerChannel(SampleFormat sample_format) {
     case kSampleFormatS32:
     case kSampleFormatF32:
     case kSampleFormatPlanarF32:
+    case kSampleFormatPlanarS32:
       return 4;
   }
 
@@ -44,6 +45,8 @@ const char* SampleFormatToString(SampleFormat sample_format) {
       return "Signed 16-bit planar";
     case kSampleFormatPlanarF32:
       return "Float 32-bit planar";
+    case kSampleFormatPlanarS32:
+      return "Signed 32-bit planar";
   }
   NOTREACHED() << "Invalid sample format provided: " << sample_format;
   return "";
