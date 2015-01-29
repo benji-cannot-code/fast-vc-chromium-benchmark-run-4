9db9542b5c2bc34466aabc5b0c46eaf5e3544047
melandory <melandory@chromium.org>
2015-01-29 10:09:53 -0800

    [Password manager tests] Makes logs which we pass to sheet cleaner.
    Instead of logging full exception we log only message.
    
    R=vabr@chromium.org
    BUG=435249
    
    Review URL: https://codereview.chromium.org/889453006
    
    Cr-Commit-Position: refs/heads/master@{#313735}

--- components/test/data/password_manager/automated_tests/environment.py
+++ components/test/data/password_manager/automated_tests/environment.py
@@ -374,11 +374,10 @@ class Environment:
         self.ClearCache(True)
         websitetest.SuccessfulLoginTest()
         self.ClearCache(True)
-      except Exception:
+      except Exception as e:
         successful = False
-        error = traceback.format_exc()
       self.tests_results.append(TestResult(websitetest.name, "normal",
-          successful, escape(error)))
+          successful, e.message))
 
 
   def PromptTestList(self, websitetests):
@@ -398,11 +397,10 @@ class Environment:
       try:
         websitetest.was_run = True
         websitetest.PromptTest()
-      except Exception:
+      except Exception as e:
         successful = False
-        error = traceback.format_exc()
       self.tests_results.append(TestResult(websitetest.name, "prompt",
-          successful, escape(error)))
+          successful, e.message))
 
   def Quit(self):
     """Closes the tests."""
