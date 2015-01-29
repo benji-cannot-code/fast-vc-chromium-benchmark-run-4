13e15b868a409fc94ef2969db63a5cc3054e99a3
cjhopman <cjhopman@chromium.org>
2015-01-28 17:38:38 -0800

    Roll DomDistillerJS
    Picked up changes:
    197d576 Delete a bunch of gwt test environment-specific things
    ae707d0 Remove DomToSaxVisitor and rename some classes
    e8b2fcd Add a WebDocument class
    52c4790 Support verbose output in jstest
    ac006fe Remove ant target "test.dev" and "test.prod"
    7db0bf4 Trim trailing spaces in java source code
    0338d85 Fix all invalid URLs in tests
    2489c81 Fix "Support test.filter for jstests"
    442f8ee Reorganize all the code in the project.
    2044b6f Support test.filter for jstests
    ddb81f8 Disable some tests in PagingLinksFinderTest
    82f505c Move PagingLinksFinderTest from test/ to jstest/
    2f9ec7d Add a devtools panel to inject extraction script into the inspected window
    9962247 Remove BoilerpipeProcessingException and other unused things.
    491c7ce Use pre-compiled regexes for many things in PagingLinksFinder
    e26e5ff Fix "Move ElementActionTest and TextBlockBuilderTest from test/ to jstest/"
    6893a1a Move ElementActionTest and TextBlockBuilderTest from test/ to jstest/
    6a97c63 Add util/TextBlockBuilderTest into DomDistillerTestSuite
    d331041 Fix ElementActionTest
    
    BUG=440102,440977
    
    Review URL: https://codereview.chromium.org/884323002
    
    Cr-Commit-Position: refs/heads/master@{#313648}

--- components/dom_distiller/core/javascript/domdistiller.js
+++ components/dom_distiller/core/javascript/domdistiller.js
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     // The OPTIONS placeholder will be replaced with the DomDistillerOptions at
     // runtime.
-    var distiller = context.com.dom_distiller.client.DomDistiller;
+    var distiller = context.org.chromium.distiller.DomDistiller;
     var res = distiller.applyWithOptions($$OPTIONS);
     return res;
   } catch (e) {
