9d7b3ec46549d7af06a222b5b270c317dc7952d7
nektar@chromium.org <nektar@chromium.org>
2015-01-29 00:55:45 +0000

    Exposed aria-sort to the browser.
    BUG=434127
    R=dmazzoni@chromium.org
    
    Review URL: https://codereview.chromium.org/801513004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189150 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/accessibility/AXObject.h
+++ third_party/WebKit/Source/modules/accessibility/AXObject.h
@@ -250,6 +250,14 @@ enum AccessibilityTextDirection {
     AccessibilityTextDirectionBottomToTop
 };
 
+enum SortDirection {
+    SortDirectionUndefined = 0,
+    SortDirectionNone,
+    SortDirectionAscending,
+    SortDirectionDescending,
+    SortDirectionOther
+};
+
 enum AccessibilityExpanded {
     ExpandedUndefined = 0,
     ExpandedCollapsed,
@@ -478,6 +486,7 @@ public:
     virtual bool supportsARIAFlowTo() const { return false; }
     virtual bool supportsARIAOwns() const { return false; }
     bool supportsRangeValue() const;
+    virtual SortDirection sortDirection() const { return SortDirectionUndefined; }
 
     // ARIA trees.
     // Used by an ARIA tree to get all its rows.
