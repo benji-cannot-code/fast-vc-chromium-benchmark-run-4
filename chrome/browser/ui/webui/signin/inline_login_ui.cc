c843948b23828acf90ac4816bce54b49eb79cedf
rsorokin <rsorokin@chromium.org>
2015-01-29 06:19:03 -0800

    Fixed crash when looking for iframe when gaia is being hosted in webview
    BUG=426316
    TEST=manual
    
    Review URL: https://codereview.chromium.org/882353002
    
    Cr-Commit-Position: refs/heads/master@{#313701}

--- chrome/browser/ui/webui/signin/inline_login_ui.cc
+++ chrome/browser/ui/webui/signin/inline_login_ui.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/guest_view/guest_view_manager.h"
 #include "grit/browser_resources.h"
 #if defined(OS_CHROMEOS)
+#include "chrome/browser/chromeos/login/startup_utils.h"
 #include "chrome/browser/ui/webui/chromeos/login/inline_login_handler_chromeos.h"
 #else
 #include "chrome/browser/ui/webui/signin/inline_login_handler_impl.h"
@@ -96,7 +97,11 @@ content::RenderFrameHost* InlineLoginUI::GetAuthIframe(
     const GURL& parent_origin,
     const std::string& parent_frame_name) {
   std::set<content::RenderFrameHost*> frame_set;
-  if (switches::IsEnableWebviewBasedSignin()) {
+  bool is_webview = switches::IsEnableWebviewBasedSignin();
+#if defined(OS_CHROMEOS)
+  is_webview = is_webview || chromeos::StartupUtils::IsWebviewSigninEnabled();
+#endif
+  if (is_webview) {
     extensions::GuestViewManager* manager =
         extensions::GuestViewManager::FromBrowserContext(
             web_contents->GetBrowserContext());
