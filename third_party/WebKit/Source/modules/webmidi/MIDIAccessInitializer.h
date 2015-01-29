a70ece70bead19e61f650d965eca86266db080de
sigbjornf@opera.com <sigbjornf@opera.com>
2015-01-29 18:30:53 +0000

    Dispose of MIDIAccessInitializer on contextDestroyed() notification.
    If a contextDestroyed() notification is issued, dispose of the
    MIDIAccessInitializer then. With Oilpan enabled, this can be the last
    opportunity to dispose via the ExecutionContext, as the weak reference
    to the context may well have been cleared by the time the prefinalizer
    runs.
    
    R=haraken
    BUG=420515
    
    Review URL: https://codereview.chromium.org/864943003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189204 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/webmidi/MIDIAccessInitializer.h
+++ third_party/WebKit/Source/modules/webmidi/MIDIAccessInitializer.h
@@ -67,9 +67,12 @@ private:
     ScriptPromise start();
     void dispose();
 
+    virtual void contextDestroyed() override;
+
     OwnPtr<MIDIAccessor> m_accessor;
     Vector<PortDescriptor> m_portDescriptors;
     bool m_requestSysex;
+    bool m_hasBeenDisposed;
 };
 
 } // namespace blink
