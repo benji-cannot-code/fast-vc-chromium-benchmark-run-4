fd065251992892d9b1afe6b4de30fb81ed2fae38
danakj <danakj@chromium.org>
2015-01-29 13:38:54 -0800

    cc: Fix flakiness in LayerTreeHostCopyRequestTestMultipleDrawsHiddenCopyRequest
    This test was expecting the main thread copy request to happen before a
    new draw happens on the compositor thread which is racey. Instead do
    this comparison on the main thread by posting and trying to end the test
    after the draw happens but checking for the copy request result there
    on the main thread instead of on the impl thread before posting.
    
    R=vmpstr, weiliangc
    BUG=439649
    
    Review URL: https://codereview.chromium.org/888663003
    
    Cr-Commit-Position: refs/heads/master@{#313797}

--- cc/test/layer_tree_test.h
+++ cc/test/layer_tree_test.h
@@ -145,7 +145,6 @@ class LayerTreeTest : public testing::Test, public TestHooks {
   void PostSetVisibleToMainThread(bool visible);
   void PostSetNextCommitForcesRedrawToMainThread();
   void PostCompositeImmediatelyToMainThread();
-  void PostEndTestToMainThread();
 
   void DoBeginTest();
   void Timeout();
