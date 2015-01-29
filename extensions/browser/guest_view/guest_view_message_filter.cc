dd409848c7ba35b76172594034e750b3bd879fa7
fsamuel <fsamuel@chromium.org>
2015-01-29 06:52:58 -0800

    GuestViewMessageFilter: Fix include
    BUG=none
    TBR=lazyboy@chromium.org
    
    Review URL: https://codereview.chromium.org/865453003
    
    Cr-Commit-Position: refs/heads/master@{#313704}

--- extensions/browser/guest_view/guest_view_message_filter.cc
+++ extensions/browser/guest_view/guest_view_message_filter.cc
@@ -3,14 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "extensions/browser/extension_message_filter.h"
+#include "extensions/browser/guest_view/guest_view_message_filter.h"
 
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_view_host.h"
 #include "extensions/browser/guest_view/guest_view_base.h"
 #include "extensions/browser/guest_view/guest_view_manager.h"
-#include "extensions/browser/guest_view/guest_view_message_filter.h"
 #include "extensions/browser/guest_view/mime_handler_view/mime_handler_view_constants.h"
 #include "extensions/browser/guest_view/mime_handler_view/mime_handler_view_guest.h"
 #include "extensions/common/extension_messages.h"
