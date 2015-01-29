555b47c3d3f3d2c58a85db839ba63841305a89a2
fukino <fukino@chromium.org>
2015-01-28 18:37:22 -0800

    Re-layout grid view and add check marks for selected items.
    Changes are:
    - Changed layout of grid view.
    - Made thumbnails fill entire item area.
    - Use check marks to indicate selected items.
    
    BUG=402344
    TEST=run browser_tests
    
    Review URL: https://codereview.chromium.org/885653002
    
    Cr-Commit-Position: refs/heads/master@{#313651}

--- ui/file_manager/file_manager/foreground/js/ui/file_grid.js
+++ ui/file_manager/file_manager/foreground/js/ui/file_grid.js
@@ -106,7 +106,7 @@ FileGrid.prototype.updateListItemsMetadata = function(type, entries) {
                                   this.metadataCache_,
                                   this.volumeManager_,
                                   this.historyLoader_,
-                                  ThumbnailLoader.FillMode.AUTO,
+                                  ThumbnailLoader.FillMode.FILL,
                                   FileGrid.ThumbnailQuality.LOW,
                                   /* animation */ false);
   }
@@ -182,11 +182,15 @@ FileGrid.decorateThumbnail = function(
         metadataCache,
         volumeManager,
         historyLoader,
-        ThumbnailLoader.FillMode.AUTO,
+        ThumbnailLoader.FillMode.FILL,
         FileGrid.ThumbnailQuality.LOW,
         /* animation */ !previousBox);
   }
   frame.appendChild(box);
+
+  var checkmark = li.ownerDocument.createElement('div');
+  checkmark.className = 'checkmark';
+  frame.appendChild(checkmark);
 
   var bottom = li.ownerDocument.createElement('div');
   bottom.className = 'thumbnail-bottom';
