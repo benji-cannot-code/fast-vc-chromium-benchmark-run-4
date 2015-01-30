5353b5cf3175ec3f0b3463b70393533e0b61209e
dbeam <dbeam@chromium.org>
2015-01-29 16:51:03 -0800

    options/guest mode: only hide the 'Show advanced settings...' link when ... there's no advanced settings.
    R=mlerman@chromium.org
    BUG=452804
    
    Review URL: https://codereview.chromium.org/886843002
    
    Cr-Commit-Position: refs/heads/master@{#313843}

--- chrome/browser/resources/options/browser_options.js
+++ chrome/browser/resources/options/browser_options.js
@@ -141,7 +141,7 @@ cr.define('options', function() {
           }
         };
       } else {
-        $('advanced-settings-expander').hidden = true;
+        $('advanced-settings-footer').hidden = true;
         $('advanced-settings').hidden = true;
       }
 
