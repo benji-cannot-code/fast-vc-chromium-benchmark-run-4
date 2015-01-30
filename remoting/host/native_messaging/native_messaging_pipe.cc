1ac3ef0eaf71f9c2eda893cfdc37dee52bf743df
sergeyu <sergeyu@chromium.org>
2015-01-30 12:02:24 -0800

    Shutdown the It2Me host cleanly.
    Previously It2Me host was shutting down before letting all shutdown
    tasks finish, which would often cause crashes.
    Now NativeMessagingPipe is responsible for host and channel deletion
    and the task runner for the main thread doesn't stop until the host
    is destroyed.
    
    BUG=453016
    
    Review URL: https://codereview.chromium.org/887853002
    
    Cr-Commit-Position: refs/heads/master@{#313967}

--- remoting/host/native_messaging/native_messaging_pipe.cc
+++ remoting/host/native_messaging/native_messaging_pipe.cc
@@ -20,11 +20,9 @@ NativeMessagingPipe::~NativeMessagingPipe() {
 
 void NativeMessagingPipe::Start(
     scoped_ptr<extensions::NativeMessageHost> host,
-    scoped_ptr<extensions::NativeMessagingChannel> channel,
-    const base::Closure& quit_closure) {
+    scoped_ptr<extensions::NativeMessagingChannel> channel) {
   host_ = host.Pass();
   channel_ = channel.Pass();
-  quit_closure_ = quit_closure;
   channel_->Start(this);
 }
 
@@ -35,8 +33,8 @@ void NativeMessagingPipe::OnMessage(scoped_ptr<base::Value> message) {
 }
 
 void NativeMessagingPipe::OnDisconnect() {
-  if (!quit_closure_.is_null())
-    base::ResetAndReturn(&quit_closure_).Run();
+  host_.reset();
+  channel_.reset();
 }
 
 void NativeMessagingPipe::PostMessageFromNativeHost(
@@ -46,8 +44,8 @@ void NativeMessagingPipe::PostMessageFromNativeHost(
 }
 
 void NativeMessagingPipe::CloseChannel(const std::string& error_message) {
-  if (!quit_closure_.is_null())
-    base::ResetAndReturn(&quit_closure_).Run();
+  host_.reset();
+  channel_.reset();
 }
 
 }  // namespace remoting
