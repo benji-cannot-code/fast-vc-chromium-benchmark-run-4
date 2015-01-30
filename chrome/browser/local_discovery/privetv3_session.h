876b31b85f4c453cd6db7bfcfd7580ee976943d7
vitalybuka <vitalybuka@chromium.org>
2015-01-30 04:11:59 -0800

    Implement of privet/v3/auth after pairing.
    After successfull pairing session will call auth API to get access token.
    
    BUG=449864
    
    Review URL: https://codereview.chromium.org/880373004
    
    Cr-Commit-Position: refs/heads/master@{#313901}

--- chrome/browser/local_discovery/privetv3_session.h
+++ chrome/browser/local_discovery/privetv3_session.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/callback.h"
+#include "base/gtest_prod_util.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/scoped_vector.h"
 #include "base/memory/weak_ptr.h"
@@ -57,7 +58,7 @@ class PrivetV3Session {
   void StartPairing(PairingType pairing_type, const ResultCallback& callback);
 
   // Confirms pairing code by calling /privet/v3/pairing/confirm.
-  // TODO(vitalybuka): Call /privet/v3/pairing/auth after pairing.
+  // Calls /privet/v3/pairing/auth after pairing.
   void ConfirmCode(const std::string& code, const ResultCallback& callback);
 
   // TODO(vitalybuka): Make HTTPS request to device with certificate validation.
@@ -67,6 +68,7 @@ class PrivetV3Session {
 
  private:
   friend class PrivetV3SessionTest;
+  FRIEND_TEST_ALL_PREFIXES(PrivetV3SessionTest, Pairing);
 
   void OnInfoDone(const InitCallback& callback,
                   Result result,
@@ -77,6 +79,10 @@ class PrivetV3Session {
   void OnPairingConfirmDone(const ResultCallback& callback,
                             Result result,
                             const base::DictionaryValue& response);
+  void OnAuthenticateDone(const ResultCallback& callback,
+                          Result result,
+                          const base::DictionaryValue& response);
+
   void RunCallback(const base::Closure& callback);
   void StartGetRequest(const std::string& api, const MessageCallback& callback);
   void StartPostRequest(const std::string& api,
