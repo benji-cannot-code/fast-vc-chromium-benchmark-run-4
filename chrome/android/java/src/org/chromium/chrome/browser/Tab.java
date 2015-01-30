3b86fb0eee13326b7f940a1fbe7b331513b93cc8
changwan <changwan@chromium.org>
2015-01-29 19:48:04 -0800

    This is the necessary change in the upstream to fix the issue.
    Pipe DidStartNavigationToPendingEntry to WebContentsObserver and TabObserver.
    
    BUG=442726
    
    Review URL: https://codereview.chromium.org/877163005
    
    Cr-Commit-Position: refs/heads/master@{#313866}

--- chrome/android/java/src/org/chromium/chrome/browser/Tab.java
+++ chrome/android/java/src/org/chromium/chrome/browser/Tab.java
@@ -579,6 +579,13 @@ public class Tab implements ViewGroup.OnHierarchyChangeListener,
                 observer.onDidDetachInterstitialPage(Tab.this);
             }
         }
+
+        @Override
+        public void didStartNavigationToPendingEntry(String url) {
+            for (TabObserver observer : mObservers) {
+                observer.onDidStartNavigationToPendingEntry(Tab.this, url);
+            }
+        }
     }
 
     /**
