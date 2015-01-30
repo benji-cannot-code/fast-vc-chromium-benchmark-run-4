f60bbad94efda951813733a7e06f5a5442f1f4a5
perkj <perkj@chromium.org>
2015-01-30 08:16:26 -0800

    Revert of Update SSL/malware/net error interstitial design (patchset #3 id:40001 of https://codereview.chromium.org/882973003/)
    Reason for revert:
    
    revert_reason: Fails ChromeOS tests.
    
    ErrorPageTest.DNSError_DoClickLink
    ErrorPageTest.DNSError_DoReload
    
    https://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Tests%20%281%29/builds/55516
    
    Original issue's description:
    > Update SSL/malware/net error interstitial design
    > + On small screens the navigation buttons are anchored to the bottom of the screen.
    > + Tweaks to the text colour
    > + Change button styling of safe browsing page
    >
    > BUG=422882
    >
    > Committed: https://crrev.com/c2e35628285c2113775566cd85c52320e6fcf565
    > Cr-Commit-Position: refs/heads/master@{#313910}
    
    TBR=arv@chromium.org,edwardjung@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=422882
    
    Review URL: https://codereview.chromium.org/889983002
    
    Cr-Commit-Position: refs/heads/master@{#313921}

--- chrome/browser/resources/chromeos/neterror.js
+++ chrome/browser/resources/chromeos/neterror.js
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 function toggleHelpBox() {
-  var helpBoxOuter = $('details');
+  var helpBoxOuter = $('help-box-outer');
   helpBoxOuter.classList.toggle('hidden');
   var detailsButton = $('details-button');
   if (helpBoxOuter.classList.contains('hidden')) {
