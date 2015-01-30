db6fe105fa94009a093b877857189f78b25f3e1e
jiajia.qin@intel.com <jiajia.qin@intel.com>
2015-01-30 02:17:22 +0000

    Add MediaStream.active attribute
    This CL added MediaStream.active attribute, onactive and oninactive events according to the spec
    http://w3c.github.io/mediacapture-main/getusermedia.html#widl-MediaStream-active
    
    BUG=445084
    
    Review URL: https://codereview.chromium.org/827673002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189225 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/mediastream/MediaStreamDescriptor.h
+++ third_party/WebKit/Source/platform/mediastream/MediaStreamDescriptor.h
@@ -81,6 +81,9 @@ public:
     void addRemoteTrack(MediaStreamComponent*);
     void removeRemoteTrack(MediaStreamComponent*);
 
+    bool active() const { return m_active; }
+    void setActive(bool active) { m_active = active; }
+
     bool ended() const { return m_ended; }
     void setEnded() { m_ended = true; }
 
@@ -95,6 +98,7 @@ private:
     String m_id;
     Vector<RefPtr<MediaStreamComponent>> m_audioComponents;
     Vector<RefPtr<MediaStreamComponent>> m_videoComponents;
+    bool m_active;
     bool m_ended;
 
     OwnPtr<ExtraData> m_extraData;
