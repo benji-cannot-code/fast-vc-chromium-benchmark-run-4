628d3d784f7c647177622c1172fbb10a2e46e57d
merkulova <merkulova@chromium.org>
2015-01-30 03:15:26 -0800

    Child accounts icons added to Chrome OS notifications.
    BUG=434643
    TBR=jennyz@chromium.org
    
    Review URL: https://codereview.chromium.org/871373011
    
    Cr-Commit-Position: refs/heads/master@{#313897}

--- ui/login/account_picker/user_pod_row.js
+++ ui/login/account_picker/user_pod_row.js
@@ -1034,8 +1034,9 @@ cr.define('login', function() {
     },
 
     customizeUserPodPerUserType: function() {
-      if ((this.user_.supervisedUser || this.user_.childUser)
-          && !this.user_.isDesktopUser) {
+      if (this.user_.isChildUser && !this.user_.isDesktopUser) {
+        this.setUserPodIconType('child');
+      } else if (this.user_.isSupervisedUser && !this.user_.isDesktopUser) {
         this.setUserPodIconType('supervised');
       } else if (this.multiProfilesPolicyApplied) {
         // Mark user pod as not focusable which in addition to the grayed out
