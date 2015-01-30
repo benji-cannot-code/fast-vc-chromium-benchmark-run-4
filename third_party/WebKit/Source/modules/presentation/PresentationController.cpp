7d1a842990b572f1d28f55f68fb7f9229a1102de
avayvod@chromium.org <avayvod@chromium.org>
2015-01-30 11:32:24 +0000

    Remove unused method onClientDestroyed.
    BUG=None
    
    Review URL: https://codereview.chromium.org/887683003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189244 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/presentation/PresentationController.cpp
+++ third_party/WebKit/Source/modules/presentation/PresentationController.cpp
@@ -70,11 +70,6 @@ bool PresentationController::isAvailableChangeWatched() const
     return m_presentation->isAvailableChangeWatched();
 }
 
-void PresentationController::onClientDestroyed()
-{
-    m_client = nullptr;
-}
-
 void PresentationController::updateAvailableChangeWatched(bool watched)
 {
     if (m_client)
