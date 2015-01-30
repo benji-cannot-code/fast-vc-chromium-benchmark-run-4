ce89cf1c749a07bd62e6f90861ae1dbb4298e30d
kinuko@chromium.org <kinuko@chromium.org>
2015-01-30 04:46:26 +0000

    Remove racy DEFINE_STATIC_LOCAL in NavigatorID::platformName()
    Replace it with AtomicallyInitializedStatic, assuming that
    calling it does not need to be terribly fast.
    
    BUG=367982
    TEST=fast/workers with http://crrev.com/874203002
    
    Review URL: https://codereview.chromium.org/871383003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189230 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/NavigatorID.cpp
+++ third_party/WebKit/Source/core/frame/NavigatorID.cpp
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/frame/NavigatorID.h"
 
 #if !defined(WEBCORE_NAVIGATOR_PLATFORM) && OS(POSIX) && !OS(MACOSX)
-#include "wtf/StdLibExtras.h"
+#include "wtf/Threading.h"
 #include <sys/utsname.h>
 #endif
 
@@ -73,7 +73,7 @@ String NavigatorID::platform()
     return "Win32";
 #else // Unix-like systems
     struct utsname osname;
-    DEFINE_STATIC_LOCAL(String, platformName, (uname(&osname) >= 0 ? String(osname.sysname) + String(" ") + String(osname.machine) : emptyString()));
+    AtomicallyInitializedStaticReference(String, platformName, new String(uname(&osname) >= 0 ? String(osname.sysname) + String(" ") + String(osname.machine) : emptyString()));
     return platformName;
 #endif
 }
