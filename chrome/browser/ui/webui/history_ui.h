ce5bd49957eb9e1ed25d0f1395da02c9c17aa9dc
sdefresne <sdefresne@chromium.org>
2015-01-30 08:42:40 -0800

    Componentize WebHistoryService
    Instead of using the factories to retrieve the dependended upon keyed service,
    pass them explicitly to WebHistoryService from the WebHistoryServiceFactory.
    
    Fix the dependency of WebHistoryService to add the service used and to remove
    the service unused.
    
    Move file into //components/history/core/browser, remove unnecessary #include
    and fix files that did not #include all the files they used.
    
    BUG=371835
    
    Review URL: https://codereview.chromium.org/882753002
    
    Cr-Commit-Position: refs/heads/master@{#313926}

--- chrome/browser/ui/webui/history_ui.h
+++ chrome/browser/ui/webui/history_ui.h
@@ -14,10 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/cancelable_task_tracker.h"
 #include "base/timer/timer.h"
 #include "base/values.h"
-#include "chrome/browser/history/web_history_service.h"
 #include "components/history/core/browser/history_service_observer.h"
+#include "components/history/core/browser/web_history_service.h"
 #include "content/public/browser/web_ui_controller.h"
 #include "content/public/browser/web_ui_message_handler.h"
+#include "ui/base/layout.h"
 
 class HistoryService;
 class ProfileSyncService;
