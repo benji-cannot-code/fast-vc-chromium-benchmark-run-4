05055afb44f193ac8970a142430a23b6a5ef8b01
sergiyb <sergiyb@chromium.org>
2015-01-30 12:33:04 -0800

    Removed references to linux_gpu, which has been removed
    BUG=426897
    R=kbr@chromium.org,phajdan.jr@chromium.org,zmo@chromium.org
    
    Review URL: https://codereview.chromium.org/800013002
    
    Cr-Commit-Position: refs/heads/master@{#313978}

--- PRESUBMIT_test.py
+++ PRESUBMIT_test.py
@@ -599,8 +599,6 @@ class TryServerMasterTest(unittest.TestCase):
   def testTryServerMasters(self):
     bots = {
         'tryserver.chromium.gpu': [
-            'linux_gpu',
-            'linux_gpu_triggered_tests',
             'win_gpu',
             'win_gpu_triggered_tests',
         ],
