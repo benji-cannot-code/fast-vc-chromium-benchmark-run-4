8797a826968cf2ada3e228133f6b94ba94d4ee03
fs@opera.com <fs@opera.com>
2015-01-30 10:30:56 +0000

    Make sure a container size is always set for SVG images in <svg:image>
    If a referenced image had the same dimensions as the referencing <image>,
    a container size would not be set. This could cause the wrong scaling (etc)
    to be applied if there existed another reference to the same image resource
    that ended up applying a different scale-factor.
    To ensure that a container size is set in this case, determine if the
    referenced image has a container size set or not by checking if what
    the "base" image (as returned by ImageResource::image()) differs from the
    RenderObject-specific image (as returned by
    ImageResource::imageForRenderer)
    
    BUG=453169
    
    Review URL: https://codereview.chromium.org/889613002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189241 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/svg/RenderSVGImage.cpp
+++ third_party/WebKit/Source/core/rendering/svg/RenderSVGImage.cpp
@@ -78,6 +78,15 @@ FloatSize RenderSVGImage::computeImageViewportSize(ImageResource& cachedImage) c
     return intrinsicRatio;
 }
 
+static bool containerSizeIsSetForRenderer(ImageResource& cachedImage, const RenderObject* renderer)
+{
+    const Image* image = cachedImage.image();
+    // If a container size has been specified for this renderer, then
+    // imageForRenderer() will return the SVGImageForContainer while image()
+    // will return the underlying SVGImage.
+    return !image->isSVGImage() || image != cachedImage.imageForRenderer(renderer);
+}
+
 bool RenderSVGImage::updateImageViewport()
 {
     SVGImageElement* image = toSVGImageElement(element());
@@ -91,7 +100,8 @@ bool RenderSVGImage::updateImageViewport()
     ImageResource* cachedImage = m_imageResource->cachedImage();
     if (cachedImage && cachedImage->usesImageContainerSize()) {
         FloatSize imageViewportSize = computeImageViewportSize(*cachedImage);
-        if (LayoutSize(imageViewportSize) != m_imageResource->imageSize(style()->effectiveZoom())) {
+        if (LayoutSize(imageViewportSize) != m_imageResource->imageSize(style()->effectiveZoom())
+            || !containerSizeIsSetForRenderer(*cachedImage, this)) {
             m_imageResource->setContainerSizeForRenderer(roundedIntSize(imageViewportSize));
             updatedViewport = true;
         }
