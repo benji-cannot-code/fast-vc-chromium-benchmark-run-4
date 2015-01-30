a39d95eeb54970bebcee3ccad77913f2fb703480
smckay <smckay@chromium.org>
2015-01-29 20:41:00 -0800

    Correctly handle out of storage space and "actively importing" states in controller. Changes in task state trigger import controller updates (triggering scanning and UI updates). Update test to work with new async update model.
    Also: Fix media importer code to pass logical destination when marking a file imported.
    
    +satorux for chrome/browser/chromeos/extensions/file_manager/private_api_strings.cc
    BUG=420680
    TEST=browser_test: FileManagerJsTest.*
    
    Review URL: https://codereview.chromium.org/888693002
    
    Cr-Commit-Position: refs/heads/master@{#313869}

--- ui/file_manager/file_manager/background/js/media_import_handler_unittest.js
+++ ui/file_manager/file_manager/background/js/media_import_handler_unittest.js
@@ -73,8 +73,10 @@ function testImportMedia(callback) {
   ]);
 
   var scanResult = new TestScanResult(media);
-  var importTask =
-      mediaImporter.importFromScanResult(scanResult, destinationFactory);
+  var importTask = mediaImporter.importFromScanResult(
+        scanResult,
+        importer.Destination.GOOGLE_DRIVE,
+        destinationFactory);
   var whenImportDone = new Promise(
       function(resolve, reject) {
         importTask.addObserver(
@@ -112,8 +114,10 @@ function testUpdatesHistoryAfterImport(callback) {
   ]);
 
   var scanResult = new TestScanResult(entries);
-  var importTask =
-      mediaImporter.importFromScanResult(scanResult, destinationFactory);
+  var importTask = mediaImporter.importFromScanResult(
+      scanResult,
+      importer.Destination.GOOGLE_DRIVE,
+      destinationFactory);
   var whenImportDone = new Promise(
       function(resolve, reject) {
         importTask.addObserver(
@@ -163,8 +167,10 @@ function testImportCancellation(callback) {
   var EXPECTED_COPY_COUNT = 3;
 
   var scanResult = new TestScanResult(media);
-  var importTask =
-      mediaImporter.importFromScanResult(scanResult, destinationFactory);
+  var importTask = mediaImporter.importFromScanResult(
+      scanResult,
+      importer.Destination.GOOGLE_DRIVE,
+      destinationFactory);
   var whenImportCancelled = new Promise(
       function(resolve, reject) {
         importTask.addObserver(
@@ -216,8 +222,10 @@ function testImportWithDuplicates(callback) {
   var EXPECTED_COPY_COUNT = 3;
 
   var scanResult = new TestScanResult(media);
-  var importTask =
-      mediaImporter.importFromScanResult(scanResult, destinationFactory);
+  var importTask = mediaImporter.importFromScanResult(
+      scanResult,
+      importer.Destination.GOOGLE_DRIVE,
+      destinationFactory);
   var whenImportDone = new Promise(
       function(resolve, reject) {
         importTask.addObserver(
