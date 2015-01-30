60ff46db3d9524af6f2ef07f5d2c5cfe382d247e
primiano <primiano@chromium.org>
2015-01-30 09:08:37 -0800

    Mechanical rename of tracing includes for /ppapi
    This is just a mechanical rename of tracing includes.
    See crrev.com/837303004 and the related bug for motivations.
    
    BUG=451032
    TBR=dmichael@chromium.org
    
    Review URL: https://codereview.chromium.org/885283004
    
    Cr-Commit-Position: refs/heads/master@{#313928}

--- ppapi/proxy/host_dispatcher.cc
+++ ppapi/proxy/host_dispatcher.cc
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ppapi/proxy/host_dispatcher.h"
 
-#include "base/debug/trace_event.h"
 #include "base/logging.h"
+#include "base/trace_event/trace_event.h"
 #include "ppapi/c/private/ppb_proxy_private.h"
 #include "ppapi/c/ppb_var.h"
 #include "ppapi/proxy/host_var_serialization_rules.h"
--- ppapi/proxy/plugin_dispatcher.cc
+++ ppapi/proxy/plugin_dispatcher.cc
@@ -8,9 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 
 #include "base/compiler_specific.h"
-#include "base/debug/trace_event.h"
 #include "base/logging.h"
 #include "base/message_loop/message_loop.h"
+#include "base/trace_event/trace_event.h"
 #include "ipc/ipc_message.h"
 #include "ipc/ipc_sync_channel.h"
 #include "ipc/ipc_sync_message_filter.h"
