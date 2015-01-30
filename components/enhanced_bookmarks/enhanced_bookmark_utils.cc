0ce94456c51de710d8acb514f397950149de362c
lpromero <lpromero@chromium.org>
2015-01-30 07:23:44 -0800

    ★ Add Bookmarks Bar to the list of primary permanent nodes.
    As per
    https://docs.google.com/document/d/1aerLjBm5jc7OILH0BDZmDX8npqop38zfnSO824LnH00,
    all permanent nodes are treated equally.
    
    BUG=453024
    
    Review URL: https://codereview.chromium.org/884933002
    
    Cr-Commit-Position: refs/heads/master@{#313918}

--- components/enhanced_bookmarks/enhanced_bookmark_utils.cc
+++ components/enhanced_bookmarks/enhanced_bookmark_utils.cc
@@ -47,6 +47,7 @@ std::vector<const BookmarkNode*> PrimaryPermanentNodes(BookmarkModel* model) {
   std::vector<const BookmarkNode*> nodes;
   nodes.push_back(model->other_node());
   nodes.push_back(model->mobile_node());
+  nodes.push_back(model->bookmark_bar_node());
   return nodes;
 }
 
@@ -64,12 +65,6 @@ std::vector<const BookmarkNode*> RootLevelFolders(BookmarkModel* model) {
         root_level_folders.push_back(node);
     }
   }
-
-  // Add the bookmark bar if it has children.
-  const BookmarkNode* bb_node = model->bookmark_bar_node();
-  if (bb_node->child_count() > 0)
-    root_level_folders.push_back(bb_node);
-
   return root_level_folders;
 }
 
