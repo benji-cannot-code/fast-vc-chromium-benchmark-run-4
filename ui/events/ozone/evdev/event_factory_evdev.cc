88d755f7e456d426529d5d702d9d2d3fdfd446b6
spang <spang@chromium.org>
2015-01-29 18:21:13 -0800

    ozone: evdev: Move GesturePropertyProvider to InputDeviceFactoryEvdev
    This is part of a series moving IO on evdev devices to a fast thread,
    since UI thread is too slow for the cursor.
    
    This properties object owns memory accessed by libgestures, and so needs
    to be be on the same thread as any gestures devices. So move it from
    EventFactoryEvdev into InputDeviceFactoryEvdev.
    
    BUG=449710
    TEST=boot link_freon & move mouse
    
    Review URL: https://codereview.chromium.org/868213002
    
    Cr-Commit-Position: refs/heads/master@{#313860}

--- ui/events/ozone/evdev/event_factory_evdev.cc
+++ ui/events/ozone/evdev/event_factory_evdev.cc
@@ -20,10 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/ozone/evdev/input_device_factory_evdev.h"
 #include "ui/events/ozone/evdev/input_injector_evdev.h"
 
-#if defined(USE_EVDEV_GESTURES)
-#include "ui/events/ozone/evdev/libgestures_glue/gesture_property_provider.h"
-#endif
-
 namespace ui {
 
 EventFactoryEvdev::EventFactoryEvdev(CursorDelegateEvdev* cursor,
@@ -35,16 +31,7 @@ EventFactoryEvdev::EventFactoryEvdev(CursorDelegateEvdev* cursor,
           base::Bind(&EventFactoryEvdev::PostUiEvent, base::Unretained(this))),
       keyboard_(&modifiers_, keyboard_layout, dispatch_callback_),
       cursor_(cursor),
-#if defined(USE_EVDEV_GESTURES)
-      gesture_property_provider_(new GesturePropertyProvider),
-#endif
-      input_controller_(&keyboard_,
-                        &button_map_
-#if defined(USE_EVDEV_GESTURES)
-                        ,
-                        gesture_property_provider_.get()
-#endif
-                            ),
+      input_controller_(&keyboard_, &button_map_),
       initialized_(false),
       weak_ptr_factory_(this) {
   DCHECK(device_manager_);
@@ -57,12 +44,8 @@ void EventFactoryEvdev::Init() {
   DCHECK(!initialized_);
 
   // Set up device factory.
-  input_device_factory_.reset(
-      new InputDeviceFactoryEvdev(this, base::ThreadTaskRunnerHandle::Get(),
-#if defined(USE_EVDEV_GESTURES)
-                                  gesture_property_provider_.get(),
-#endif
-                                  cursor_));
+  input_device_factory_.reset(new InputDeviceFactoryEvdev(
+      this, base::ThreadTaskRunnerHandle::Get(), cursor_));
   // TODO(spang): This settings interface is really broken. crbug.com/450899
   input_controller_.SetInputDeviceFactory(input_device_factory_.get());
 
--- ui/events/ozone/evdev/input_controller_evdev.h
+++ ui/events/ozone/evdev/input_controller_evdev.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/basictypes.h"
-#include "ui/events/ozone/evdev/event_device_info.h"
 #include "ui/events/ozone/evdev/events_ozone_evdev_export.h"
 #include "ui/ozone/public/input_controller.h"
 
@@ -19,20 +18,11 @@ class InputDeviceFactoryEvdev;
 class KeyboardEvdev;
 class MouseButtonMapEvdev;
 
-#if defined(USE_EVDEV_GESTURES)
-class GesturePropertyProvider;
-#endif
-
 // Ozone InputController implementation for the Linux input subsystem ("evdev").
 class EVENTS_OZONE_EVDEV_EXPORT InputControllerEvdev : public InputController {
  public:
   InputControllerEvdev(KeyboardEvdev* keyboard,
-                       MouseButtonMapEvdev* button_map
-#if defined(USE_EVDEV_GESTURES)
-                       ,
-                       GesturePropertyProvider* gesture_property_provider
-#endif
-                       );
+                       MouseButtonMapEvdev* button_map);
   ~InputControllerEvdev() override;
 
   // Initialize device factory. This would be in the constructor if it was
@@ -67,14 +57,6 @@ class EVENTS_OZONE_EVDEV_EXPORT InputControllerEvdev : public InputController {
   void EnableInternalKeyboard() override;
 
  private:
-  // Set a property value for all devices of one type.
-  void SetIntPropertyForOneType(const EventDeviceType type,
-                                const std::string& name,
-                                int value);
-  void SetBoolPropertyForOneType(const EventDeviceType type,
-                                 const std::string& name,
-                                 bool value);
-
   // Factory for devices. Needed to update device config.
   InputDeviceFactoryEvdev* input_device_factory_;
 
@@ -83,11 +65,6 @@ class EVENTS_OZONE_EVDEV_EXPORT InputControllerEvdev : public InputController {
 
   // Mouse button map.
   MouseButtonMapEvdev* button_map_;
-
-#if defined(USE_EVDEV_GESTURES)
-  // Gesture library property provider.
-  GesturePropertyProvider* gesture_property_provider_;
-#endif
 
   DISALLOW_COPY_AND_ASSIGN(InputControllerEvdev);
 };
