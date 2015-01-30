b63264eda6ee52326dad7b3a6b834a03c686d14f
vitalybuka <vitalybuka@chromium.org>
2015-01-29 17:20:30 -0800

    Undef of GetMessage is not needed.
    Conflict was fixed in https://codereview.chromium.org/889663003/
    
    Review URL: https://codereview.chromium.org/893463002
    
    Cr-Commit-Position: refs/heads/master@{#313850}

--- remoting/protocol/v2_authenticator.cc
+++ remoting/protocol/v2_authenticator.cc
@@ -14,10 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using crypto::P224EncryptedKeyExchange;
 
-#if defined(_WIN32) && defined(GetMessage)
-#undef GetMessage
-#endif
-
 namespace remoting {
 namespace protocol {
 
