e381b4b3da6b2177ee028e5c7463e2b47995f0e3
spang <spang@chromium.org>
2015-01-29 23:45:58 -0800

    ozone: evdev: Remove extra PostTask during dispatch from EventFactoryEvdev
    Since device processing is moved off-thread, we can't enter the device
    code from nested UI message loops. So we can remove the task hop in
    PostUiEvent and dispatch synchronously.
    
    WarpCursorTo() did need update to do its own post, as it is the last
    remaining case of dispatch that doesn't start in its own task. Everything
    else already posts in ForwardingDeviceEventDispatcher.
    
    We can stack-allocate the ui::Events now, too. They previously needed
    heap allocation to extend their lifetime through the task hop.
    
    BUG=449710
    TEST=boot link_freon & move mouse
    
    Review URL: https://codereview.chromium.org/858333006
    
    Cr-Commit-Position: refs/heads/master@{#313879}

--- ui/events/ozone/evdev/event_converter_test_util.cc
+++ ui/events/ozone/evdev/event_converter_test_util.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/ozone/device/device_manager.h"
 #include "ui/events/ozone/evdev/device_event_dispatcher_evdev.h"
 #include "ui/events/ozone/evdev/event_factory_evdev.h"
+#include "ui/events/platform/platform_event_dispatcher.h"
 
 namespace ui {
 
@@ -87,8 +88,10 @@ class TestEventFactoryEvdev : public EventFactoryEvdev {
   ~TestEventFactoryEvdev() override {}
 
  private:
-  void PostUiEvent(scoped_ptr<Event> event) override {
-    callback_.Run(event.Pass());
+  uint32_t DispatchEvent(PlatformEvent platform_event) override {
+    Event* event = static_cast<Event*>(platform_event);
+    callback_.Run(event);
+    return POST_DISPATCH_NONE;
   }
 
   EventDispatchCallback callback_;
