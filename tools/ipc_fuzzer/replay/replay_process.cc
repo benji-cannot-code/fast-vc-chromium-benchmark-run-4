87b4f432fd42e1945869b68b303025edf285ec49
morrita <morrita@chromium.org>
2015-01-30 14:36:42 -0800

    IPC Fuzzer: Initialize mojo in the replay process.
    Without this, ChannelMojo doesn't work.
    
    R=inferno@chromium.org, tsepez@chromium.org
    TBR=jamesr@chromium.org
    BUG=453571
    
    Review URL: https://codereview.chromium.org/867903006
    
    Cr-Commit-Position: refs/heads/master@{#314010}

--- tools/ipc_fuzzer/replay/replay_process.cc
+++ tools/ipc_fuzzer/replay/replay_process.cc
@@ -17,8 +17,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ipc/ipc_descriptors.h"
 #include "ipc/ipc_switches.h"
 #include "ipc/mojo/ipc_channel_mojo.h"
+#include "third_party/mojo/src/mojo/edk/embedder/configuration.h"
+#include "third_party/mojo/src/mojo/edk/embedder/embedder.h"
+#include "third_party/mojo/src/mojo/edk/embedder/simple_platform_support.h"
 
 namespace ipc_fuzzer {
+
+// TODO(morrita): content::InitializeMojo() should be used once it becomes
+// a public API. See src/content/app/mojo/mojo_init.cc
+void InitializeMojo() {
+  mojo::embedder::GetConfiguration()->max_message_num_bytes =
+      64 * 1024 * 1024;
+  mojo::embedder::Init(scoped_ptr<mojo::embedder::PlatformSupport>(
+      new mojo::embedder::SimplePlatformSupport()));
+}
 
 ReplayProcess::ReplayProcess()
     : io_thread_("Chrome_ChildIOThread"),
@@ -60,6 +72,8 @@ bool ReplayProcess::Initialize(int argc, const char** argv) {
   g_fds->Set(kPrimaryIPCChannel,
              kPrimaryIPCChannel + base::GlobalDescriptors::kBaseDescriptor);
 #endif
+
+  InitializeMojo();
 
   return true;
 }
