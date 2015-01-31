a88e5ced7ecdd24dab2f56681e97d9d81784a099
simonhong <simonhong@chromium.org>
2015-01-31 07:47:15 -0800

    cc: Control defer_commits logic by Scheduler
    So far, EarlyOut_DeferCommits is controlled by Proxy.
    Because Scheduler doesn't know about this deferring, it triggers next
    BeginMainFrame when main thread want to defer a commit.
    This CL moves the EarlyOut_DeferCommits logic from Proxy to
    Scheduler so Scheduler can stop next BeginMainFrame until defer commit is off.
    
    R=brianderson@chromium.org, danakj@chromium.org, skyostil@chromium.org
    BUG=382572, 453787
    TEST=cc_unittests
    
    Committed: https://crrev.com/1625b74468e4dd55f54ba209f8e91d05493875b9
    Cr-Commit-Position: refs/heads/master@{#313816}
    
    Review URL: https://codereview.chromium.org/337693005
    
    Cr-Commit-Position: refs/heads/master@{#314057}

--- cc/trees/single_thread_proxy.h
+++ cc/trees/single_thread_proxy.h
@@ -115,13 +115,14 @@ class CC_EXPORT SingleThreadProxy : public Proxy,
   // Called by the legacy path where RenderWidget does the scheduling.
   void CompositeImmediately(base::TimeTicks frame_begin_time);
 
- private:
+ protected:
   SingleThreadProxy(
       LayerTreeHost* layer_tree_host,
       LayerTreeHostSingleThreadClient* client,
       scoped_refptr<base::SingleThreadTaskRunner> main_task_runner,
       scoped_ptr<BeginFrameSource> external_begin_frame_source);
 
+ private:
   void BeginMainFrame();
   void BeginMainFrameAbortedOnImplThread(CommitEarlyOutReason reason);
   void DoAnimate();
@@ -155,7 +156,6 @@ class CC_EXPORT SingleThreadProxy : public Proxy,
 
   bool inside_draw_;
   bool defer_commits_;
-  bool commit_was_deferred_;
   bool commit_requested_;
   bool inside_synchronous_composite_;
 
