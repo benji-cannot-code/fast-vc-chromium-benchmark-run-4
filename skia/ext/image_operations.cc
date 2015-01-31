fff8b0f6d253fa64e2d3615e9e1b21c0ca3c6029
primiano <primiano@chromium.org>
2015-01-31 09:46:29 -0800

    Mechanical rename of tracing includes for /skia
    This is just a mechanical rename of tracing includes.
    See crrev.com/837303004 and the related bug for motivations.
    
    BUG=451032
    TBR=tomhudson@google.com
    
    Review URL: https://codereview.chromium.org/893533005
    
    Cr-Commit-Position: refs/heads/master@{#314060}

--- skia/ext/image_operations.cc
+++ skia/ext/image_operations.cc
@@ -12,10 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // TODO(pkasting): skia/ext should not depend on base/!
 #include "base/containers/stack_container.h"
-#include "base/debug/trace_event.h"
 #include "base/logging.h"
 #include "base/metrics/histogram.h"
 #include "base/time/time.h"
+#include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
 #include "skia/ext/convolver.h"
 #include "third_party/skia/include/core/SkColorPriv.h"
