6640b30b8294216c31b72e0a8e864f2e0b8ddbb0
jochen@chromium.org <jochen@chromium.org>
2015-02-02 15:16:19 +0000

    Sanitize referrer headers set by workers
    BUG=453737
    R=horo@chromium.org,mkwst@chromium.org
    
    Review URL: https://codereview.chromium.org/890723003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189341 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/WorkerThreadableLoader.cpp
+++ third_party/WebKit/Source/core/loader/WorkerThreadableLoader.cpp
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/network/ResourceRequest.h"
 #include "platform/network/ResourceResponse.h"
 #include "platform/weborigin/Referrer.h"
+#include "platform/weborigin/SecurityPolicy.h"
 #include "public/platform/Platform.h"
 #include "public/platform/WebWaitableEvent.h"
 #include "wtf/MainThread.h"
@@ -134,7 +135,7 @@ void WorkerThreadableLoader::MainThreadBridge::mainThreadCreateLoader(ExecutionC
     Document* document = toDocument(context);
 
     OwnPtr<ResourceRequest> request(ResourceRequest::adopt(requestData));
-    request->setHTTPReferrer(Referrer(outgoingReferrer, ReferrerPolicyDefault));
+    request->setHTTPReferrer(SecurityPolicy::generateReferrer(ReferrerPolicyDefault, request->url(), outgoingReferrer));
     resourceLoaderOptions.requestInitiatorContext = WorkerContext;
     thisPtr->m_mainThreadLoader = DocumentThreadableLoader::create(*document, thisPtr, *request, options, resourceLoaderOptions);
     if (!thisPtr->m_mainThreadLoader) {
