e63336896f5f480f25d452cd7e8fbcbc65b06b2b
dschuyler <dschuyler@chromium.org>
2015-02-02 10:14:26 -0800

    Remove and deprecate SEARCH_SUGGEST_ANSWER.  This type is no longer used in the UI code, since answers can be attached to suggestions of any type.
    BUG=430513
    
    Review URL: https://codereview.chromium.org/815273002
    
    Cr-Commit-Position: refs/heads/master@{#314167}

--- components/omnibox/autocomplete_match.cc
+++ components/omnibox/autocomplete_match.cc
@@ -156,7 +156,6 @@ int AutocompleteMatch::TypeToIcon(Type type) {
       IDR_OMNIBOX_SEARCH,         // CONTACT_DEPRECATED
       IDR_OMNIBOX_HTTP,           // BOOKMARK_TITLE
       IDR_OMNIBOX_HTTP,           // NAVSUGGEST_PERSONALIZED
-      IDR_OMNIBOX_SEARCH,         // SEARCH_SUGGEST_ANSWER
   };
 #else
   static const int kIcons[] = {
@@ -178,7 +177,6 @@ int AutocompleteMatch::TypeToIcon(Type type) {
       IDR_OMNIBOX_SEARCH,         // CONTACT_DEPRECATED
       IDR_OMNIBOX_HTTP,           // BOOKMARK_TITLE
       IDR_OMNIBOX_HTTP,           // NAVSUGGEST_PERSONALIZED
-      IDR_OMNIBOX_SEARCH,         // SEARCH_SUGGEST_ANSWER
   };
 #endif
   static_assert(arraysize(kIcons) == AutocompleteMatchType::NUM_TYPES,
@@ -354,8 +352,7 @@ bool AutocompleteMatch::IsSpecializedSearchType(Type type) {
   return type == AutocompleteMatchType::SEARCH_SUGGEST_ENTITY ||
          type == AutocompleteMatchType::SEARCH_SUGGEST_INFINITE ||
          type == AutocompleteMatchType::SEARCH_SUGGEST_PERSONALIZED ||
-         type == AutocompleteMatchType::SEARCH_SUGGEST_PROFILE ||
-         type == AutocompleteMatchType::SEARCH_SUGGEST_ANSWER;
+         type == AutocompleteMatchType::SEARCH_SUGGEST_PROFILE;
 }
 
 // static
