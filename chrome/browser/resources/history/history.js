b87e324fc693201262af9b8aabe44387ac7ab9c3
Dan Beam <dbeam@chromium.org>
2015-02-02 08:17:39 -0800

    Fix closure compile.
    TBR=tbreisacher@chromium.org
    BUG=none
    TEST=http://build.chromium.org/p/chromium.fyi/builders/Closure%20Compilation%20Linux goes green
    
    Review URL: https://codereview.chromium.org/895663002
    
    Cr-Commit-Position: refs/heads/master@{#314155}

--- chrome/browser/resources/history/history.js
+++ chrome/browser/resources/history/history.js
@@ -1937,7 +1937,7 @@ function load() {
       searchField.blur();  // Dismiss the keyboard.
   };
 
-  var removeMenu = $('remove-visit');
+  var removeMenu = getRequiredElement('remove-visit');
   // Decorate remove-visit before disabling/hiding because the values are
   // overwritten when decorating a MenuItem that has a Command.
   cr.ui.decorate(removeMenu, MenuItem);
