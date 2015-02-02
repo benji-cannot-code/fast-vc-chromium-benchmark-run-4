26347e0db3a75a81a8b51eb1c79d04614119981b
mlerman <mlerman@chromium.org>
2015-02-02 10:15:53 -0800

    Only store leading 13 bits of password hash.
    Also, take ownership for a related but un-owned histogram.
    
    BUG=433130
    TBR=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/862103002
    
    Cr-Commit-Position: refs/heads/master@{#314168}

--- chrome/browser/signin/signin_manager_factory.cc
+++ chrome/browser/signin/signin_manager_factory.cc
@@ -104,7 +104,7 @@ void SigninManagerFactory::RegisterProfilePrefs(
       prefs::kSignedInTime,
       base::Time().ToInternalValue(),
       user_prefs::PrefRegistrySyncable::UNSYNCABLE_PREF);
-  chrome::RegisterLocalAuthPrefs(registry);
+  LocalAuth::RegisterLocalAuthPrefs(registry);
 }
 
 // static
