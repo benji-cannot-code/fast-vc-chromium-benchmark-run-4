d931f270c836e71ea80dadb1e7a0fbaa7853761d
bnc <bnc@chromium.org>
2015-02-02 12:17:03 -0800

    Add AlternateProtocolInfo to HttpStreamFactoryImpl::Job.
    Add AlternateProtocolInfo member to HttpStreamFactoryImpl::Job.  Add
    AlternateProtocolInfo argument to constructor, remove it from MarkAsAlternate.
    This will be necessary in MaybeMarkAlternateProtocolBroken to pass it on to
    SetBrokenAlternateProtocol().  See https://crrev.com/665083009 for context.
    
    BUG=392575
    
    Review URL: https://codereview.chromium.org/871753006
    
    Cr-Commit-Position: refs/heads/master@{#314180}

--- net/http/http_stream_factory_impl_job.cc
+++ net/http/http_stream_factory_impl_job.cc
@@ -170,6 +170,7 @@ void HttpStreamFactoryImpl::Job::MarkAsAlternate(
     AlternateProtocolInfo alternate) {
   DCHECK(!original_url_.get());
   original_url_.reset(new GURL(original_url));
+  alternate_protocol_ = alternate;
   if (alternate.protocol == QUIC) {
     DCHECK(session_->params().enable_quic);
     using_quic_ = true;
@@ -1455,6 +1456,7 @@ void HttpStreamFactoryImpl::Job::ReportJobSuccededForRequest() {
 void HttpStreamFactoryImpl::Job::MarkOtherJobComplete(const Job& job) {
   DCHECK_EQ(STATUS_RUNNING, other_job_status_);
   other_job_status_ = job.job_status_;
+  other_job_alternate_protocol_ = job.alternate_protocol_;
   MaybeMarkAlternateProtocolBroken();
 }
 
