15c234e54254c70f86faed5cd384f3e3658c0e79
yawano <yawano@chromium.org>
2015-02-02 01:30:55 -0800

    Change ImageLoaderClient to return width and height of image.
    ListThumbnailLoader(http://crrev.com/885323002) directly returns dataUrl, width and height of thumbnail image instead of attaching it to image element.
    We want to directly obtain dataUrl, width and height of thumbnail from ImageLoaderClient.
    
    BUG=438050
    TEST=Manually tested.
    
    Review URL: https://codereview.chromium.org/890313002
    
    Cr-Commit-Position: refs/heads/master@{#314120}

--- ui/file_manager/image_loader/cache.js
+++ ui/file_manager/image_loader/cache.js
@@ -28,7 +28,7 @@ Cache.DB_NAME = 'image-loader';
  * @type {number}
  * @const
  */
-Cache.DB_VERSION = 11;
+Cache.DB_VERSION = 12;
 
 /**
  * Memory limit for images data in bytes.
@@ -216,10 +216,12 @@ Cache.prototype.evictCache_ = function(
  *
  * @param {string} key Cache key.
  * @param {string} data Image data.
+ * @param {number} width Image width.
+ * @param {number} height Image height.
  * @param {number} timestamp Last modification timestamp. Used to detect
  *     if the cache entry becomes out of date.
  */
-Cache.prototype.saveImage = function(key, data, timestamp) {
+Cache.prototype.saveImage = function(key, data, width, height, timestamp) {
   if (!this.db_) {
     console.warn('Cache database not available.');
     return;
@@ -229,6 +231,8 @@ Cache.prototype.saveImage = function(key, data, timestamp) {
     var metadataEntry = {
       key: key,
       timestamp: timestamp,
+      width: width,
+      height: height,
       size: data.length,
       lastLoadTimestamp: Date.now()};
     var dataEntry = {key: key, data: data};
@@ -257,7 +261,8 @@ Cache.prototype.saveImage = function(key, data, timestamp) {
  * @param {string} key Cache key.
  * @param {number} timestamp Last modification timestamp. If different
  *     that the one in cache, then the entry will be invalidated.
- * @param {function(string)} onSuccess Success callback with the image's data.
+ * @param {function(string, number, number)} onSuccess Success callback with
+ *     the image's data, width, height.
  * @param {function()} onFailure Failure callback.
  */
 Cache.prototype.loadImage = function(key, timestamp, onSuccess, onFailure) {
@@ -304,7 +309,7 @@ Cache.prototype.loadImage = function(key, timestamp, onSuccess, onFailure) {
       // image data.
       metadataEntry.lastLoadTimestamp = Date.now();
       metadataStore.put(metadataEntry);  // Added asynchronously.
-      onSuccess(dataEntry.data);
+      onSuccess(dataEntry.data, metadataEntry.width, metadataEntry.height);
     }
   }.bind(this);
 
