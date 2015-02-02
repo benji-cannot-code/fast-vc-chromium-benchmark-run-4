18604ab5966f45548be89e70bdcac767f6becbcd
tfarina <tfarina@chromium.org>
2015-02-01 18:55:42 -0800

    Fix 'gn check' errors in //ui/* targets.
    Tested with the following configs:
    
    Linux:
    $ gn gen out_gn/Debug
    $ gn check out_gn/Debug
    
    Android:
    $ gn gen --args='os="android" cpu_arch="arm"' out_gn_android/Debug
    $ gn check out_gn_android/Debug
    
    Chromeos:
    $ gn gen --args='os="chromeos"' out_gn_chromeos/Debug
    $ gn check out_gn_chromeos/Debug
    
    BUG=367595, 376000
    TEST=see above
    R=brettw@chromium.org
    TBR=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/872243004
    
    Cr-Commit-Position: refs/heads/master@{#314102}

--- ui/app_list/views/app_list_main_view_unittest.cc
+++ ui/app_list/views/app_list_main_view_unittest.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/app_list/views/contents_view.h"
 #include "ui/app_list/views/search_box_view.h"
 #include "ui/app_list/views/test/apps_grid_view_test_api.h"
-#include "ui/aura/window.h"
 #include "ui/events/test/event_generator.h"
 #include "ui/views/test/views_test_base.h"
 #include "ui/views/view_model.h"
