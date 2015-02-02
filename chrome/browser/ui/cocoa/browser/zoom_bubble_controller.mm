62afa38780ae90fca797766c8f682432cf5b2e2f
wjmaclean <wjmaclean@chromium.org>
2015-02-02 07:16:10 -0800

    [OSX] Allow one zoom bubble to override another.
    At present, if a user changes zoom on OSX while the zoom bubble is
    already being shown, the new zoom bubble (with the most up to date
    information) is not shown. This CL changes this behaviour by allowing
    a new zoom bubble to be created, and replace the existing one.
    
    Example: load a PDF and repeatedly click the zoom +/- controls in the
    lower-right corner of the viewer.
    
    BUG=444995
    
    Review URL: https://codereview.chromium.org/886883002
    
    Cr-Commit-Position: refs/heads/master@{#314147}

--- chrome/browser/ui/cocoa/browser/zoom_bubble_controller.mm
+++ chrome/browser/ui/cocoa/browser/zoom_bubble_controller.mm
@@ -76,6 +76,8 @@ void SetZoomBubbleAutoCloseDelayForTesting(NSTimeInterval time_interval) {
 
 @implementation ZoomBubbleController
 
+@synthesize delegate = delegate_;
+
 - (id)initWithParentWindow:(NSWindow*)parentWindow
                   delegate:(ZoomBubbleControllerDelegate*)delegate {
   base::scoped_nsobject<InfoBubbleWindow> window(
@@ -127,6 +129,10 @@ void SetZoomBubbleAutoCloseDelayForTesting(NSTimeInterval time_interval) {
 }
 
 - (void)onZoomChanged {
+  // |delegate_| may be set null by this object's owner.
+  if (!delegate_)
+    return;
+
   // TODO(shess): It may be appropriate to close the window if
   // |contents| or |zoomController| are NULL.  But they can be NULL in
   // tests.
@@ -170,8 +176,11 @@ void SetZoomBubbleAutoCloseDelayForTesting(NSTimeInterval time_interval) {
 }
 
 - (void)windowWillClose:(NSNotification*)notification {
-  delegate_->OnClose();
-  delegate_ = NULL;
+  // |delegate_| may be set null by this object's owner.
+  if (delegate_) {
+    delegate_->OnClose();
+    delegate_ = NULL;
+  }
   [NSObject cancelPreviousPerformRequestsWithTarget:self
                                            selector:@selector(autoCloseBubble)
                                              object:nil];
