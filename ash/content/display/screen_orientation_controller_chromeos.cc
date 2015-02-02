1f0abf66ec6fa7845ead2c65008a258fc8dcd918
jonross <jonross@chromium.org>
2015-02-02 12:58:23 -0800

    Screen Orientation Controller allow rotation for some orientations
    Some orientation locks allow rotation between the two angles of the given orientation. For landscape 0/180, for portrait 90/270.
    
    Update ScreenOrientationController to check the orientaiton of a rotation lock. If the orientation supports rotation, allow accelerometer events to change the rotation.
    
    TEST=ScreenOrientationControllerTest.LandscapeOrientationAllowsRotation, ScreenOrientationControllerTest.PortraitOrientationAllowsRotation, ScreenOrientationControllerTest.OrientationLockDisallowsRotation, ScreenOrientationControllerTest.UserRotationLockDisallowsRotation
    BUG=396760
    
    Review URL: https://codereview.chromium.org/869393004
    
    Cr-Commit-Position: refs/heads/master@{#314186}

--- ash/content/display/screen_orientation_controller_chromeos.cc
+++ ash/content/display/screen_orientation_controller_chromeos.cc
@@ -67,6 +67,7 @@ ScreenOrientationController::ScreenOrientationController()
     : natural_orientation_(GetDisplayNaturalOrientation()),
       ignore_display_configuration_updates_(false),
       rotation_locked_(false),
+      rotation_locked_orientation_(blink::WebScreenOrientationLockAny),
       user_rotation_(gfx::Display::ROTATE_0),
       current_rotation_(gfx::Display::ROTATE_0) {
   content::ScreenOrientationProvider::SetDelegate(this);
@@ -95,6 +96,8 @@ void ScreenOrientationController::SetRotationLocked(bool rotation_locked) {
   if (rotation_locked_ == rotation_locked)
     return;
   rotation_locked_ = rotation_locked;
+  if (!rotation_locked_)
+    rotation_locked_orientation_ = blink::WebScreenOrientationLockAny;
   FOR_EACH_OBSERVER(Observer, observers_,
                     OnRotationLockChanged(rotation_locked_));
   DisplayManager* display_manager = Shell::GetInstance()->display_manager();
@@ -144,7 +147,7 @@ void ScreenOrientationController::OnWindowDestroying(aura::Window* window) {
 
 void ScreenOrientationController::OnAccelerometerUpdated(
     const ui::AccelerometerUpdate& update) {
-  if (rotation_locked_)
+  if (rotation_locked_ && !CanRotateInLockedState())
     return;
   if (!update.has(ui::ACCELEROMETER_SOURCE_SCREEN))
     return;
@@ -235,6 +238,7 @@ void ScreenOrientationController::LockRotation(
 
 void ScreenOrientationController::LockRotationToOrientation(
     blink::WebScreenOrientationLockType lock_orientation) {
+  rotation_locked_orientation_ = lock_orientation;
   switch (lock_orientation) {
     case blink::WebScreenOrientationLockAny:
       SetRotationLocked(false);
@@ -286,9 +290,6 @@ void ScreenOrientationController::LockRotationToSecondaryOrientation(
 
 void ScreenOrientationController::LockToRotationMatchingOrientation(
     blink::WebScreenOrientationLockType lock_orientation) {
-  // TODO(jonross): Update MaximizeModeController to allow rotation between
-  // two angles of an orientation (e.g. from ROTATE_0 to ROTATE_180, and from
-  // ROTATE_90 to ROTATE_270)
   DisplayManager* display_manager = Shell::GetInstance()->display_manager();
   if (!display_manager->HasInternalDisplay())
     return;
@@ -358,7 +359,8 @@ void ScreenOrientationController::HandleScreenRotation(
   else if (angle < 270.0f)
     new_rotation = gfx::Display::ROTATE_180;
 
-  if (new_rotation != current_rotation_)
+  if (new_rotation != current_rotation_ &&
+      IsRotationAllowedInLockedState(new_rotation))
     SetDisplayRotation(new_rotation);
 }
 
@@ -389,6 +391,31 @@ void ScreenOrientationController::RemoveLockingWindow(aura::Window* window) {
     Shell::GetInstance()->activation_client()->RemoveObserver(this);
   window->RemoveObserver(this);
   ApplyLockForActiveWindow();
+}
+
+bool ScreenOrientationController::IsRotationAllowedInLockedState(
+    gfx::Display::Rotation rotation) {
+  if (!rotation_locked_)
+    return true;
+
+  if (!CanRotateInLockedState())
+    return false;
+
+  if (natural_orientation_ == rotation_locked_orientation_) {
+    return rotation == gfx::Display::ROTATE_0 ||
+           rotation == gfx::Display::ROTATE_180;
+  } else {
+    return rotation == gfx::Display::ROTATE_90 ||
+           rotation == gfx::Display::ROTATE_270;
+  }
+  return false;
+}
+
+bool ScreenOrientationController::CanRotateInLockedState() {
+  return rotation_locked_orientation_ ==
+             blink::WebScreenOrientationLockLandscape ||
+         rotation_locked_orientation_ ==
+             blink::WebScreenOrientationLockPortrait;
 }
 
 }  // namespace ash
