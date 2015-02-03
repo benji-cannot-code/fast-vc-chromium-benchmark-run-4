af9748d72368b3ac1c73561848b5947d5bf61258
alexandrec <alexandrec@chromium.org>
2015-02-03 00:26:46 -0800

    Ctrl-G now selects page number in Material Design PDF Viewer
    Previously, typing Ctrl-G would pop up the "find-text" box, appearing to be an alias for Ctrl-F.
    Doing so will now highlight the page number in the viewer-page-selector, allowing for a quick and easy method to go to another page.
    Changed viewer-page-selector to use a combination of paper-input-decorator and input instead of a sole paper-input. This affords us greater flexibility and should ease maintenance.
    
    BUG=55920
    
    Review URL: https://codereview.chromium.org/888343003
    
    Cr-Commit-Position: refs/heads/master@{#314291}

--- chrome/browser/resources/pdf/elements/viewer-page-selector/viewer-page-selector.js
+++ chrome/browser/resources/pdf/elements/viewer-page-selector/viewer-page-selector.js
@@ -33,5 +33,9 @@ Polymer('viewer-page-selector', {
   docLengthChanged: function() {
     var numDigits = this.docLength.toString().length;
     this.$.pageselector.style.width = (numDigits * DIGIT_LENGTH) + 'em';
+  },
+
+  select: function() {
+    this.$.input.select();
   }
 });
