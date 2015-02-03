679933dd53732047df381978d0ec0903d6291256
rickyz <rickyz@chromium.org>
2015-02-02 16:32:22 -0800

    Pass the size of the array to GetModuleFileName.
    TCHAR may be a WCHAR, so sizeof would allow GetModuleFileName to write
    twice the size of the buffer.
    
    Thanks to Ilya Berdichevsky for the bug and fix.
    
    BUG=446464
    
    Review URL: https://codereview.chromium.org/898493003
    
    Cr-Commit-Position: refs/heads/master@{#314223}

--- content/browser/accessibility/browser_accessibility_state_impl_win.cc
+++ content/browser/accessibility/browser_accessibility_state_impl_win.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <psapi.h>
 
 #include "base/files/file_path.h"
+#include "base/macros.h"
 #include "base/metrics/histogram.h"
 #include "base/strings/string_util.h"
 
@@ -57,7 +58,7 @@ void BrowserAccessibilityStateImpl::UpdatePlatformSpecificHistograms() {
   size_t module_count = bytes_required / sizeof(HMODULE);
   for (size_t i = 0; i < module_count; i++) {
     TCHAR filename[MAX_PATH];
-    GetModuleFileName(modules[i], filename, sizeof(filename));
+    GetModuleFileName(modules[i], filename, arraysize(filename));
     base::string16 module_name(base::FilePath(filename).BaseName().value());
     if (LowerCaseEqualsASCII(module_name, "fsdomsrv.dll"))
       jaws = true;
