c9a61f42ea533bc798ac1bd7f320214a696a227b
meacer <meacer@chromium.org>
2015-02-03 11:59:26 -0800

    Captive Portal Interstitial: Use SSID even if connection type isn't Wi-Fi.
    |IsWiFiConnection| isn't accurate on some platforms. Whenever SSID is available,
    use it and ignore |IsWiFiConnection|. This CL also adds a delegate to
    |CaptivePortalBlockingPage| for better testing.
    
    BUG=451272
    
    Review URL: https://codereview.chromium.org/894153003
    
    Cr-Commit-Position: refs/heads/master@{#314391}

--- chrome/browser/ssl/captive_portal_blocking_page.h
+++ chrome/browser/ssl/captive_portal_blocking_page.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/macros.h"
+#include "base/memory/scoped_ptr.h"
 #include "chrome/browser/interstitials/security_interstitial_page.h"
 #include "url/gurl.h"
 
@@ -29,6 +30,16 @@ class CaptivePortalBlockingPage : public SecurityInterstitialPage {
   // Interstitial type, for testing.
   static const void* kTypeForTesting;
 
+  class Delegate {
+   public:
+    virtual ~Delegate() {}
+
+    // Returns true if the connection is a Wi-Fi connection.
+    virtual bool IsWifiConnection() const = 0;
+    // Returns the SSID of the connected Wi-Fi network, if any.
+    virtual std::string GetWiFiSSID() const = 0;
+  };
+
   CaptivePortalBlockingPage(content::WebContents* web_contents,
                             const GURL& request_url,
                             const GURL& login_url,
@@ -38,13 +49,7 @@ class CaptivePortalBlockingPage : public SecurityInterstitialPage {
   // SecurityInterstitialPage method:
   const void* GetTypeForTesting() const override;
 
-  void SetWiFiConnectionForTesting(bool is_wifi_connection) {
-    is_wifi_connection_ = is_wifi_connection;
-  }
-
-  void SetWiFiSSIDForTesting(const std::string& wifi_ssid) {
-    wifi_ssid_ = wifi_ssid;
-  }
+  void SetDelegateForTesting(Delegate* delegate) { delegate_.reset(delegate); }
 
  protected:
   // SecurityInterstitialPage methods:
@@ -58,11 +63,7 @@ class CaptivePortalBlockingPage : public SecurityInterstitialPage {
  private:
   // URL of the login page, opened when the user clicks the "Connect" button.
   GURL login_url_;
-  // True if on a Wi-Fi connection.
-  bool is_wifi_connection_;
-  // SSID of the connected network if the connection is a Wi-Fi connection.
-  std::string wifi_ssid_;
-
+  scoped_ptr<Delegate> delegate_;
   base::Callback<void(bool)> callback_;
 
   DISALLOW_COPY_AND_ASSIGN(CaptivePortalBlockingPage);
