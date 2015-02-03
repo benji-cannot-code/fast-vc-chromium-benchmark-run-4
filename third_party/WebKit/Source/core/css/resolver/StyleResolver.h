4f5e62987b7cc3f5a005b4cbbef2225702ba2160
andersr@opera.com <andersr@opera.com>
2015-02-03 12:02:00 +0000

    Split property priority functions out of StyleResolver.
    This makes it easier to access the priority from other (future)
    files which shouldn't depend on StyleResolver.
    
    R=timloh@chromium.org
    
    Review URL: https://codereview.chromium.org/892643002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189408 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/resolver/StyleResolver.h
+++ third_party/WebKit/Source/core/css/resolver/StyleResolver.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/css/SelectorFilter.h"
 #include "core/css/SiblingTraversalStrategies.h"
 #include "core/css/TreeBoundaryCrossingRules.h"
+#include "core/css/resolver/CSSPropertyPriority.h"
 #include "core/css/resolver/MatchedPropertiesCache.h"
 #include "core/css/resolver/StyleBuilder.h"
 #include "core/css/resolver/StyleResourceLoader.h"
@@ -227,23 +228,13 @@ private:
     bool applyAnimatedProperties(StyleResolverState&, const Element* animatingElement);
     void applyCallbackSelectors(StyleResolverState&);
 
-    enum StyleApplicationPass {
-        HighPriorityProperties,
-        LowPriorityProperties
-    };
-    template <StyleResolver::StyleApplicationPass pass>
-    static inline CSSPropertyID firstCSSPropertyId();
-    template <StyleResolver::StyleApplicationPass pass>
-    static inline CSSPropertyID lastCSSPropertyId();
-    template <StyleResolver::StyleApplicationPass pass>
-    static inline bool isPropertyForPass(CSSPropertyID);
-    template <StyleApplicationPass pass>
+    template <CSSPropertyPriority priority>
     void applyMatchedProperties(StyleResolverState&, const MatchResult&, bool important, int startIndex, int endIndex, bool inheritedOnly);
-    template <StyleApplicationPass pass>
+    template <CSSPropertyPriority priority>
     void applyProperties(StyleResolverState&, const StylePropertySet* properties, bool isImportant, bool inheritedOnly, PropertyWhitelistType = PropertyWhitelistNone);
-    template <StyleApplicationPass pass>
+    template <CSSPropertyPriority priority>
     void applyAnimatedProperties(StyleResolverState&, const WillBeHeapHashMap<CSSPropertyID, RefPtrWillBeMember<Interpolation> >&);
-    template <StyleResolver::StyleApplicationPass pass>
+    template <CSSPropertyPriority priority>
     void applyAllProperty(StyleResolverState&, CSSValue*, bool inheritedOnly);
 
     void matchPageRules(MatchResult&, RuleSet*, bool isLeftPage, bool isFirstPage, const String& pageName);
