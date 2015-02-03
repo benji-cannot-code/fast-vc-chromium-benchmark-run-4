73e4696c2b9b97cad66457e6a07647b60a6d3219
calamity <calamity@chromium.org>
2015-02-03 15:52:13 -0800

    Add a static Google Doodle to the experimental app list start page.
    This CL adds a static Google Doodle to the experimental app list start
    page by downloading a JSON blob and using the contained doodle URL as an
    image source in the app list start page WebContents.
    
    BUG=453205
    
    Review URL: https://codereview.chromium.org/871193006
    
    Cr-Commit-Position: refs/heads/master@{#314442}

--- chrome/browser/ui/app_list/start_page_service.h
+++ chrome/browser/ui/app_list/start_page_service.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/keyed_service/core/keyed_service.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_observer.h"
+#include "net/url_request/url_fetcher_delegate.h"
 #include "ui/app_list/speech_ui_model_observer.h"
 
 namespace content {
@@ -32,6 +33,10 @@ struct SpeechRecognitionSessionPreamble;
 
 namespace extensions {
 class Extension;
+}
+
+namespace net {
+class URLFetcher;
 }
 
 class Profile;
@@ -46,6 +51,7 @@ class StartPageObserver;
 // and hosts the start page contents.
 class StartPageService : public KeyedService,
                          public content::WebContentsObserver,
+                         public net::URLFetcherDelegate,
                          public SpeechRecognizerDelegate {
  public:
   typedef std::vector<scoped_refptr<const extensions::Extension> >
@@ -113,6 +119,12 @@ class StartPageService : public KeyedService,
   void LoadContents();
   void UnloadContents();
 
+  // Fetch the Google Doodle JSON data and update the app list start page.
+  void FetchDoodleJson();
+
+  // net::URLFetcherDelegate overrides:
+  void OnURLFetchComplete(const net::URLFetcher* source) override;
+
   // KeyedService overrides:
   void Shutdown() override;
 
@@ -152,6 +164,8 @@ class StartPageService : public KeyedService,
   scoped_ptr<AudioStatus> audio_status_;
 #endif
   scoped_ptr<NetworkChangeObserver> network_change_observer_;
+
+  scoped_ptr<net::URLFetcher> doodle_fetcher_;
 
   base::WeakPtrFactory<StartPageService> weak_factory_;
 
