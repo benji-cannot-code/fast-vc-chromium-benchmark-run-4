86eae3e079a843f8adeeb7cc8affbd153897c8b3
noms <noms@chromium.org>
2015-02-02 20:11:18 -0800

    [Mac] Allow "Enter" to activate the buttons in the new avatar bubble.
    Despite this not being the OSX way, users are complaining that Enter doesn't
    activate the buttons in the menu, because it used to. I think the easiest
    way is to wrap all buttons in a view that responds to Enter, and activates
    the button accordingly.
    
    BUG=454467
    TEST=Start Chrome with --enable-new-avatar-menu. Open the avatar bubble and
    tab through items. Pressing enter should perform the button's action.
    
    Review URL: https://codereview.chromium.org/890143005
    
    Cr-Commit-Position: refs/heads/master@{#314265}

--- chrome/browser/ui/cocoa/profiles/profile_chooser_controller.mm
+++ chrome/browser/ui/cocoa/profiles/profile_chooser_controller.mm
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #import <Cocoa/Cocoa.h>
+#import <Carbon/Carbon.h>  // kVK_Return.
 
 #import "chrome/browser/ui/cocoa/profiles/profile_chooser_controller.h"
 
@@ -787,6 +788,16 @@ class ActiveProfileObserverBridge : public AvatarMenuObserver,
 
   NSColor* backgroundColor = isHighlighted ? hoverColor_ : backgroundColor_;
   [[self cell] setBackgroundColor:backgroundColor];
+}
+
+-(void)keyDown:(NSEvent*)event {
+  // Since there is no default button in the bubble, it is safe to activate
+  // all buttons on Enter as well, and be consistent with the Windows
+  // implementation.
+  if ([event keyCode] == kVK_Return)
+    [self performClick:self];
+  else
+    [super keyDown:event];
 }
 
 - (BOOL)canBecomeKeyView {
