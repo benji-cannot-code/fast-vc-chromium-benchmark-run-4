c61f7160fedc941a144a3a0f402cbec5d36019c6
dnicoara <dnicoara@chromium.org>
2015-02-03 08:11:21 -0800

    [Ozone-DRI] Remove the extra helper thread and reuse the IO one
    Now that we have access to the IO thread, pass the IO task runner to
    the main thread and use that to register and listen for page flip
    events.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/891703006
    
    Cr-Commit-Position: refs/heads/master@{#314341}

--- ui/ozone/platform/dri/dri_gpu_platform_support.h
+++ ui/ozone/platform/dri/dri_gpu_platform_support.h
@@ -11,13 +11,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_vector.h"
 #include "ipc/message_filter.h"
 #include "ui/gfx/native_widget_types.h"
-#include "ui/ozone/platform/dri/dri_helper_thread.h"
 #include "ui/ozone/public/gpu_platform_support.h"
 
 class SkBitmap;
 
 namespace base {
 class FilePath;
+class SingleThreadTaskRunner;
 }
 
 namespace gfx {
@@ -78,17 +78,18 @@ class DriGpuPlatformSupport : public GpuPlatformSupport {
   void OnAddGraphicsDevice(const base::FilePath& path);
   void OnRemoveGraphicsDevice(const base::FilePath& path);
 
-  void OnFilterAdded();
+  void SetIOTaskRunner(
+      const scoped_refptr<base::SingleThreadTaskRunner>& io_task_runner);
 
   IPC::Sender* sender_;                       // Not owned.
   DriWrapper* drm_;                           // Not owned.
   DriWindowDelegateManager* window_manager_;  // Not owned.
   ScreenManager* screen_manager_;             // Not owned.
 
-  DriHelperThread helper_thread_;
   scoped_ptr<NativeDisplayDelegateDri> ndd_;
   ScopedVector<GpuPlatformSupport> handlers_;
   scoped_refptr<IPC::MessageFilter> filter_;
+  scoped_refptr<base::SingleThreadTaskRunner> io_task_runner_;
 };
 
 }  // namespace ui
