5cd7638d24ac9a8d5b50e311061032f40ec42bd7
chrishtr@chromium.org <chrishtr@chromium.org>
2015-02-03 01:39:28 +0000

    Add show() methods for debugging to FloatRoundedRect (try 2)
    Now with #include <stdio.h>
    
    Revert "Revert of Add show() methods for debugging to FloatRoundedRect. (patchset #2 id:20001 of https://codereview.chromium.org/885193002/)"
    
    This reverts commit fb47e171a3efcfdae02c3e0f473114557af7dff3.
    
    Review URL: https://codereview.chromium.org/893033002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189378 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/geometry/FloatRoundedRect.cpp
+++ third_party/WebKit/Source/platform/geometry/FloatRoundedRect.cpp
@@ -35,6 +35,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
+#ifndef NDEBUG
+#include <stdio.h>
+#endif
+
 namespace blink {
 
 FloatRoundedRect::FloatRoundedRect(float x, float y, float width, float height)
@@ -137,6 +141,15 @@ void FloatRoundedRect::Radii::expand(float topWidth, float bottomWidth, float le
         m_bottomRight.setHeight(m_bottomRight.height() + bottomWidth);
     }
 }
+
+#ifndef NDEBUG
+void FloatRoundedRect::Radii::show()
+{
+    fprintf(stderr, "topLeft=[%f,%f], topRight=[%f,%f], bottomLeft=[%f,%f], bottomRight=[%f,%f]\n",
+        topLeft().width(), topLeft().height(), topRight().width(), topRight().height(),
+        bottomLeft().width(), bottomLeft().height(), bottomRight().width(), bottomRight().height());
+}
+#endif
 
 static inline float cornerRectIntercept(float y, const FloatRect& cornerRect)
 {
@@ -336,5 +349,14 @@ void FloatRoundedRect::adjustRadii()
     m_radii.scale(widthRatio < heightRatio ? widthRatio : heightRatio);
 }
 
+#ifndef NDEBUG
+void FloatRoundedRect::show()
+{
+    fprintf(stderr, "FloatRoundedRect:\n rect: ");
+    m_rect.show();
+    fprintf(stderr, " radii: ");
+    m_radii.show();
+}
+#endif
 
 } // namespace blink
