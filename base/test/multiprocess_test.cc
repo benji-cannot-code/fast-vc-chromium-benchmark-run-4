f3045f24d352ca8811a306ca1056177fa4dc0b3f
rickyz <rickyz@chromium.org>
2015-02-03 00:44:28 -0800

    Add the ability to change directories before execing.
    BUG=387037
    
    Review URL: https://codereview.chromium.org/885423003
    
    Cr-Commit-Position: refs/heads/master@{#314294}

--- base/test/multiprocess_test.cc
+++ base/test/multiprocess_test.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base_switches.h"
 #include "base/command_line.h"
+#include "base/files/file_path.h"
+#include "base/files/file_util.h"
 
 namespace base {
 
@@ -27,7 +29,9 @@ Process SpawnMultiProcessTestChild(
 #endif  // !defined(OS_ANDROID)
 
 CommandLine GetMultiProcessTestChildBaseCommandLine() {
-  return *CommandLine::ForCurrentProcess();
+  CommandLine cmd_line = *CommandLine::ForCurrentProcess();
+  cmd_line.SetProgram(MakeAbsoluteFilePath(cmd_line.GetProgram()));
+  return cmd_line;
 }
 
 // MultiProcessTest ------------------------------------------------------------
