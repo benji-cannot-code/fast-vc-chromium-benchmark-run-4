1473a3de1a883d85a08d7cec2eeb01c1f832f013
mariakhomenko <mariakhomenko@chromium.org>
2015-02-02 16:41:55 -0800

    Delete GetTopAsynchronousMatch.
    The function is no longer used by anyone.
    
    Review URL: https://codereview.chromium.org/858173004
    
    Cr-Commit-Position: refs/heads/master@{#314228}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/AutocompleteController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/AutocompleteController.java
@@ -290,14 +290,6 @@ public class AutocompleteController {
                 mNativeAutocompleteControllerAndroid, selectedIndex, elapsedTimeSinceInputChange);
     }
 
-    /**
-     * @param query User input text.
-     * @return The top synchronous suggestion from the autocomplete controller.
-     */
-    public OmniboxSuggestion getTopSynchronousMatch(String query) {
-        return nativeGetTopSynchronousMatch(mNativeAutocompleteControllerAndroid, query);
-    }
-
     @VisibleForTesting
     protected native long nativeInit(Profile profile);
     private native void nativeStart(long nativeAutocompleteControllerAndroid, String text,
@@ -319,8 +311,6 @@ public class AutocompleteController {
     private native String nativeUpdateMatchDestinationURLWithQueryFormulationTime(
             long nativeAutocompleteControllerAndroid, int selectedIndex,
             long elapsedTimeSinceInputChange);
-    private native OmniboxSuggestion nativeGetTopSynchronousMatch(
-            long nativeAutocompleteControllerAndroid, String query);
 
     /**
      * Given a search query, this will attempt to see if the query appears to be portion of a
