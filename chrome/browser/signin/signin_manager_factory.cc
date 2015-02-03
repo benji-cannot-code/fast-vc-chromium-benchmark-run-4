bc68e324cd8c5a0c62cc2a84d8cb0a8f407db43f
kozyatinskiy <kozyatinskiy@chromium.org>
2015-02-03 02:16:42 -0800

    Revert "Only store leading 13 bits of password hash."
    This reverts commit c511691cc06fd3a92f27e688795872e61efff40a.
    
    CL added SetUpgradeAndCheckCredentials test.
    This test failed on Mac 10.6 build bot.
    
    TBR=mlerman@chromium.org,
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=none
    
    Review URL: https://codereview.chromium.org/875983008
    
    Cr-Commit-Position: refs/heads/master@{#314302}

--- chrome/browser/signin/signin_manager_factory.cc
+++ chrome/browser/signin/signin_manager_factory.cc
@@ -104,7 +104,7 @@ void SigninManagerFactory::RegisterProfilePrefs(
       prefs::kSignedInTime,
       base::Time().ToInternalValue(),
       user_prefs::PrefRegistrySyncable::UNSYNCABLE_PREF);
-  LocalAuth::RegisterLocalAuthPrefs(registry);
+  chrome::RegisterLocalAuthPrefs(registry);
 }
 
 // static
--- chrome/browser/ui/views/profiles/profile_chooser_view.cc
+++ chrome/browser/ui/views/profiles/profile_chooser_view.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_metrics.h"
 #include "chrome/browser/profiles/profile_window.h"
 #include "chrome/browser/profiles/profiles_state.h"
+#include "chrome/browser/signin/local_auth.h"
 #include "chrome/browser/signin/profile_oauth2_token_service_factory.h"
 #include "chrome/browser/signin/signin_header_helper.h"
 #include "chrome/browser/signin/signin_manager_factory.h"
