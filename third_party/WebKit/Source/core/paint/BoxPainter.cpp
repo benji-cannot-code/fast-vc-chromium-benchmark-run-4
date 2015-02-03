6bcac0a271105533b7a2913340d146b835d4bc45
chrishtr@chromium.org <chrishtr@chromium.org>
2015-02-03 00:03:30 +0000

    Constrain radii when painting box shadows.
    Including shadow spread and blur may result in violations of maximum rounded rect
    radii constraints. Handle that by explicitly constraining after applying spread
    and blur.
    
    This CL is a targeted fix appropriate for M41 merging. A better cleanup will happen
    after.
    
    BUG=451600,453957
    
    Review URL: https://codereview.chromium.org/878333006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189374 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/paint/BoxPainter.cpp
+++ third_party/WebKit/Source/core/paint/BoxPainter.cpp
@@ -1885,7 +1885,6 @@ void BoxPainter::paintBoxShadow(const PaintInfo& info, const LayoutRect& paintRe
     GraphicsContext* context = info.context;
     if (!s->boxShadow())
         return;
-
     FloatRoundedRect border = (shadowStyle == Inset) ? s->getRoundedInnerBorderFor(paintRect, includeLogicalLeftEdge, includeLogicalRightEdge)
         : s->getRoundedBorderFor(paintRect, includeLogicalLeftEdge, includeLogicalRightEdge);
 
@@ -1986,6 +1985,7 @@ void BoxPainter::paintBoxShadow(const PaintInfo& info, const LayoutRect& paintRe
                         roundedFillRect.shrinkRadii(-shadowSpread);
                     if (!roundedFillRect.isRenderable())
                         roundedFillRect.adjustRadii();
+                    roundedFillRect.constrainRadii();
                     context->fillRoundedRect(roundedFillRect, Color::black);
                 }
             } else {
