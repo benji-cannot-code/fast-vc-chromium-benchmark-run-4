21dbaa5a6e17942fa267929ec6ccdc56da150172
ccameron <ccameron@chromium.org>
2015-02-04 10:26:43 -0800

    Revert of Remove usages of Timeline commands in telemetry (patchset #4 id:60001 of https://codereview.chromium.org/895043002/)
    Reason for revert:
    Windows telemetry unit tests are failing intermittently after this patch.
    
    Sample failing build:
    http://build.chromium.org/p/chromium.win/builders/Vista%20Tests%20%281%29/builds/53177
    
    Output:
    [1/1] telemetry.core.backends.chrome_inspector.inspector_network_unittest.InspectorNetworkTabTest.testHTTPResponseTimelineRecorder failed unexpectedly:
      Could not find Flash at E:\b\build\slave\Vista_Tests__1_\build\src\third_party\adobe\flash\binaries\ppapi\win\pepflashplayer.dll. Continuing without Flash.
      To run with Flash, check it out via http://go/read-src-internal
      Traceback (most recent call last):
        File "E:\b\build\slave\Vista_Tests__1_\build\src\tools\telemetry\telemetry\decorators.py", line 55, in wrapper
          func(*args, **kwargs)
        File "E:\b\build\slave\Vista_Tests__1_\build\src\tools\telemetry\telemetry\core\backends\chrome_inspector\inspector_network_unittest.py", line 40, in testHTTPResponseTimelineRecorder
          self.assertEqual(test.responses_count, len(events))
      AssertionError: 1 != 2
    
    Original issue's description:
    > Remove usages of Timeline commands in telemetry
    >
    > We are going to remove Timeline domain from the remote debugging protocol soon. This CL fixes remaining usages of timeline commands.
    >
    > BUG=448318
    >
    > Committed: https://crrev.com/9ca9eb0c7668c7a7e1f595ea0c4f3bca221e9350
    > Cr-Commit-Position: refs/heads/master@{#314522}
    
    TBR=caseq@chromium.org,pfeldman@chromium.org,marja@chromium.org,yurys@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=448318
    
    Review URL: https://codereview.chromium.org/903523002
    
    Cr-Commit-Position: refs/heads/master@{#314589}

---
+++ tools/telemetry/telemetry/core/backends/chrome_inspector/timeline_recorder.py
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+class TimelineRecorder(object):
+  """Interface for classes that can record timeline raw events."""
+  def Start(self):
+    """Starts recording."""
+    raise NotImplementedError
+
+  def Stop(self):
+    """Stops recording and returns timeline event data."""
+    raise NotImplementedError
