e27ccb2567f4a953d7154e345d291e7fece32626
jincheol.jo <jincheol.jo@navercorp.com>
2015-02-03 23:23:45 -0800

    make BeginFrameSourceMixIn's SetNeedsBeginFrame final
    BUG=429391
    
    Review URL: https://codereview.chromium.org/891953003
    
    Cr-Commit-Position: refs/heads/master@{#314516}

--- cc/scheduler/begin_frame_source.h
+++ cc/scheduler/begin_frame_source.h
@@ -139,7 +139,7 @@ class CC_EXPORT BeginFrameSourceMixIn : public BeginFrameSource {
 
   // BeginFrameSource
   bool NeedsBeginFrames() const override;
-  void SetNeedsBeginFrames(bool needs_begin_frames) override;
+  void SetNeedsBeginFrames(bool needs_begin_frames) final;
   void DidFinishFrame(size_t remaining_frames) override {}
   void AddObserver(BeginFrameObserver* obs) final;
   void RemoveObserver(BeginFrameObserver* obs) final;
@@ -262,8 +262,10 @@ class CC_EXPORT BeginFrameSourceMultiplexer : public BeginFrameSourceMixIn,
 
   // BeginFrameSource
   bool NeedsBeginFrames() const override;
-  void SetNeedsBeginFrames(bool needs_begin_frames) override;
   void DidFinishFrame(size_t remaining_frames) override;
+
+  // BeginFrameSourceMixIn
+  void OnNeedsBeginFramesChange(bool needs_begin_frames) override;
 
   // Tracing
   void AsValueInto(base::debug::TracedValue* dict) const override;
