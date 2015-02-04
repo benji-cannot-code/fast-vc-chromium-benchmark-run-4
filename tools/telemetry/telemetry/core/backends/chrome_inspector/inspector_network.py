ff739c295d56394e0ecff66441c337e3a4f9937c
yurys <yurys@chromium.org>
2015-02-04 00:16:21 -0800

    Remove usages of Timeline commands in telemetry
    We are going to remove Timeline domain from the remote debugging protocol soon. This CL fixes remaining usages of timeline commands.
    
    BUG=448318
    
    Review URL: https://codereview.chromium.org/895043002
    
    Cr-Commit-Position: refs/heads/master@{#314522}

--- tools/telemetry/telemetry/core/backends/chrome_inspector/inspector_network.py
+++ tools/telemetry/telemetry/core/backends/chrome_inspector/inspector_network.py
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import logging
 
 from telemetry.core import util
-from telemetry.core.backends.chrome_inspector import timeline_recorder
 
 
 class InspectorNetworkException(Exception):
@@ -211,9 +210,8 @@ class InspectorNetwork(object):
     return self._timeline_recorder
 
 
-class TimelineRecorder(timeline_recorder.TimelineRecorder):
+class TimelineRecorder(object):
   def __init__(self, inspector_network):
-    super(TimelineRecorder, self).__init__()
     self._inspector_network = inspector_network
     self._is_recording = False
 
