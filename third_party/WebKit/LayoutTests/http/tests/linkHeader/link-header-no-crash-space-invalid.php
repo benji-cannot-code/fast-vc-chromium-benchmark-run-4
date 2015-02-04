5d4576a3097e8bd2f6b27865ba72dea330764d72
yoav@yoav.ws <yoav@yoav.ws>
2015-02-04 10:47:41 +0000

    Add a Link header parser.
    This CL adds a parser for Link headers,
    that would enable to support them for various features that can benefit from such support.
    Resource Hints (and the non-standard hints: dns-prefetch, prefetch, etc) are likely to be the first users.
    
    This is a re-submit of 889883002 which got reverted.
    
    BUG=58456
    
    Review URL: https://codereview.chromium.org/889413005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189477 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/linkHeader/link-header-no-crash-space-invalid.php
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+header("Link: </images/cat.jpg> wut=sup ; rel =prefetch  \t  ;");
+?>
+<!DOCTYPE html>
+<script>
+if (window.testRunner)
+    testRunner.dumpAsText();
+</script>
+This test passes if we didn't crash.
