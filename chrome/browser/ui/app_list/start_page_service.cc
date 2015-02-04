e5e6f118bbc5d37b99a4403b538133814d9d9029
calamity <calamity@chromium.org>
2015-02-04 00:31:58 -0800

    Stop unfinished speech query from appearing in app list reopen.
    This CL fixes an issue where a speech query in the app list, that was
    interrupted by the app list closing, would complete when the app list
    was reopened.
    
    BUG=440061
    
    Review URL: https://codereview.chromium.org/886353004
    
    Cr-Commit-Position: refs/heads/master@{#314523}

--- chrome/browser/ui/app_list/start_page_service.cc
+++ chrome/browser/ui/app_list/start_page_service.cc
@@ -349,6 +349,7 @@ void StartPageService::AppListHidden() {
   if (HotwordService::IsExperimentalHotwordingEnabled() &&
       speech_recognizer_) {
     speech_recognizer_->Stop();
+    speech_recognizer_.reset();
   }
 
 #if defined(OS_CHROMEOS)
