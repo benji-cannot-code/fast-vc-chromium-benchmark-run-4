ca8747e60237694d0d20d6f01439d92a366e651b
chrishtr@chromium.org <chrishtr@chromium.org>
2015-02-04 18:54:27 +0000

    [Slimming Paint] Add a drawing recorder for DetailsMarkerPainter::paint, and add fast/html
    BUG=446648
    
    Review URL: https://codereview.chromium.org/892343002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189520 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/paint/DetailsMarkerPainter.cpp
+++ third_party/WebKit/Source/core/paint/DetailsMarkerPainter.cpp
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/paint/DetailsMarkerPainter.h"
 
 #include "core/paint/BlockPainter.h"
+#include "core/paint/RenderDrawingRecorder.h"
 #include "core/rendering/PaintInfo.h"
 #include "core/rendering/RenderDetailsMarker.h"
 #include "platform/geometry/LayoutPoint.h"
@@ -26,6 +27,10 @@ void DetailsMarkerPainter::paint(const PaintInfo& paintInfo, const LayoutPoint& 
     overflowRect.moveBy(boxOrigin);
 
     if (!paintInfo.rect.intersects(pixelSnappedIntRect(overflowRect)))
+        return;
+
+    RenderDrawingRecorder renderDrawingRecorder(paintInfo.context, m_renderDetailsMarker, paintInfo.phase, overflowRect);
+    if (renderDrawingRecorder.canUseCachedDrawing())
         return;
 
     const Color color(m_renderDetailsMarker.resolveColor(CSSPropertyColor));
