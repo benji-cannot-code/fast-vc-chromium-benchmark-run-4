0c0cf09658f5f3890421888ff1b0b011d65c58d3
aruslan <aruslan@chromium.org>
2015-02-04 13:46:16 -0800

    Temporary logging to help ANR dissection
    BUG=446578
    
    Review URL: https://codereview.chromium.org/893003004
    
    Cr-Commit-Position: refs/heads/master@{#314644}

--- chrome/browser/android/provider/chrome_browser_provider.cc
+++ chrome/browser/android/provider/chrome_browser_provider.cc
@@ -299,7 +299,9 @@ class RemoveAllUserBookmarksTask : public BookmarkModelObserverTask {
 
   static void RunOnUIThread(BookmarkModel* model) {
     DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+    LOG(ERROR) << "begin model->RemoveAllUserBookmarks";
     model->RemoveAllUserBookmarks();
+    LOG(ERROR) << "after model->RemoveAllUserBookmarks";
   }
 
  private:
@@ -1516,8 +1518,10 @@ ScopedJavaLocalRef<jobject> ChromeBrowserProvider::GetEditableBookmarkFolders(
 }
 
 void ChromeBrowserProvider::RemoveAllUserBookmarks(JNIEnv* env, jobject obj) {
+  LOG(ERROR) << "begin ChromeBrowserProvider::RemoveAllUserBookmarks";
   RemoveAllUserBookmarksTask task(bookmark_model_);
   task.Run();
+  LOG(ERROR) << "end ChromeBrowserProvider::RemoveAllUserBookmarks";
 }
 
 ScopedJavaLocalRef<jobject> ChromeBrowserProvider::GetBookmarkNode(
