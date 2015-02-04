bebee53f38b3bb5702589197cb0bc01cd8a93a6a
kelvinp <kelvinp@chromium.org>
2015-02-04 14:40:43 -0800

    Manual revert of Timezone auto update option should be available by default.
    Reason for revert:
    4000+ browser test are failing on ChromeOS after this patch and this is the only patch that touches ChromeOS in the blamelist.
    
    Seems like with the switch enabled, we are creating the GetTimezoneResolver that leaks the g_browser_process->system_request_context()
    
    Sample failure:
    http://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Tests%20%28dbg%29%281%29/builds/37149
    
    Original issue's description:
    > https://codereview.chromium.org/898503002
    > Timezone auto update option should be available by default.
    >
    > This CL makes "timezone auto update" feature available (but > switched off by
    > default) to users. And adds flag to disable this feature.
    >
    > BUG=416494
    > TEST=manual
    >
    > Committed:
    > https://crrev.com/13bb9e13b4719c96112ca8770d2c7b48c23e150d
    > Cr-Commit-Position: refs/heads/master@{#314615}
    
    TBR=alemate
    TEST=MANUAL
    NOTRY=true
    NOTREECHECKS=true
    NOPRESUBMIT=true
    
    Review URL: https://codereview.chromium.org/899113002
    
    Cr-Commit-Position: refs/heads/master@{#314663}

--- chrome/browser/chromeos/login/users/chrome_user_manager_impl.cc
+++ chrome/browser/chromeos/login/users/chrome_user_manager_impl.cc
@@ -1049,8 +1049,8 @@ void ChromeUserManagerImpl::UpdateNumberOfUsers() {
 }
 
 void ChromeUserManagerImpl::UpdateUserTimeZoneRefresher(Profile* profile) {
-  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
-          chromeos::switches::kDisableTimeZoneTrackingOption)) {
+  if (!base::CommandLine::ForCurrentProcess()->HasSwitch(
+          chromeos::switches::kEnableTimeZoneTrackingOption)) {
     return;
   }
 
