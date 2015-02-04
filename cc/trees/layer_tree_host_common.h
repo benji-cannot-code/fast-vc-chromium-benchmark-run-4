69ab3df34c1a6d356b6e97ecfd9b89e3fbf697f0
miletus <miletus@chromium.org>
2015-02-04 12:44:49 -0800

    Remove scroll_delta flooring hack
    Now that fractional scroll offset can pass through cc main
    thread and Blink, we can remove the scroll_delta flooring hack
    and send the full fractional scroll_delta from impl to main.
    
    Note that this will break the old pinch viewport code path (
    which is to be removed soon) since it still only expects integer
    scroll offset.
    
    BUG=414283
    
    Review URL: https://codereview.chromium.org/870363005
    
    Cr-Commit-Position: refs/heads/master@{#314626}

--- cc/trees/layer_tree_host_common.h
+++ cc/trees/layer_tree_host_common.h
@@ -152,9 +152,7 @@ class CC_EXPORT LayerTreeHostCommon {
 
   struct ScrollUpdateInfo {
     int layer_id;
-    // TODO(miletus) : Use ScrollOffset once LayerTreeHost/Blink fully supports
-    // franctional scroll offset.
-    gfx::Vector2d scroll_delta;
+    gfx::Vector2dF scroll_delta;
   };
 };
 
