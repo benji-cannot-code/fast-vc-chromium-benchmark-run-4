964b21d1499fccf6412084392ffcc33fb1c009ea
calamity <calamity@chromium.org>
2015-02-03 20:39:57 -0800

    Make app list start page doodle clickable and have alt text.
    This CL makes the app list start page doodle clickable by implementing
    the necessary methods on the StartPageWebContentsDelegate and adding
    the appropriate JS in the start page WebUI.
    
    BUG=453205
    
    Review URL: https://codereview.chromium.org/887853003
    
    Cr-Commit-Position: refs/heads/master@{#314504}

--- chrome/browser/ui/app_list/start_page_service.cc
+++ chrome/browser/ui/app_list/start_page_service.cc
@@ -24,6 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/app_list/speech_recognizer.h"
 #include "chrome/browser/ui/app_list/start_page_observer.h"
 #include "chrome/browser/ui/app_list/start_page_service_factory.h"
+#include "chrome/browser/ui/browser_navigator.h"
+#include "chrome/browser/ui/browser_tabstrip.h"
+#include "chrome/browser/ui/scoped_tabbed_browser_displayer.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
@@ -108,7 +111,7 @@ class StartPageService::ProfileDestroyObserver
 class StartPageService::StartPageWebContentsDelegate
     : public content::WebContentsDelegate {
  public:
-  StartPageWebContentsDelegate() {}
+  explicit StartPageWebContentsDelegate(Profile* profile) : profile_(profile) {}
   ~StartPageWebContentsDelegate() override {}
 
   void RequestMediaAccessPermission(
@@ -126,7 +129,50 @@ class StartPageService::StartPageWebContentsDelegate
         ->CheckMediaAccessPermission(web_contents, security_origin, type);
   }
 
+  void AddNewContents(content::WebContents* source,
+                      content::WebContents* new_contents,
+                      WindowOpenDisposition disposition,
+                      const gfx::Rect& initial_pos,
+                      bool user_gesture,
+                      bool* was_blocked) override {
+    chrome::ScopedTabbedBrowserDisplayer displayer(
+        profile_, chrome::GetActiveDesktop());
+    // Force all links to open in a new tab, even if they were trying to open a
+    // new window.
+    disposition =
+        disposition == NEW_BACKGROUND_TAB ? disposition : NEW_FOREGROUND_TAB;
+    chrome::AddWebContents(displayer.browser(),
+                           nullptr,
+                           new_contents,
+                           disposition,
+                           initial_pos,
+                           user_gesture,
+                           was_blocked);
+  }
+
+  content::WebContents* OpenURLFromTab(
+      content::WebContents* source,
+      const content::OpenURLParams& params) override {
+    // Force all links to open in a new tab, even if they were trying to open a
+    // window.
+    chrome::NavigateParams new_tab_params(
+        static_cast<Browser*>(nullptr), params.url, params.transition);
+    if (params.disposition == NEW_BACKGROUND_TAB) {
+      new_tab_params.disposition = NEW_BACKGROUND_TAB;
+    } else {
+      new_tab_params.disposition = NEW_FOREGROUND_TAB;
+      new_tab_params.window_action = chrome::NavigateParams::SHOW_WINDOW;
+    }
+
+    new_tab_params.initiating_profile = profile_;
+    chrome::Navigate(&new_tab_params);
+
+    return new_tab_params.target_contents;
+  }
+
  private:
+  Profile* profile_;
+
   DISALLOW_COPY_AND_ASSIGN(StartPageWebContentsDelegate);
 };
 
@@ -499,7 +545,7 @@ void StartPageService::WebUILoaded() {
 void StartPageService::LoadContents() {
   contents_.reset(content::WebContents::Create(
       content::WebContents::CreateParams(profile_)));
-  contents_delegate_.reset(new StartPageWebContentsDelegate());
+  contents_delegate_.reset(new StartPageWebContentsDelegate(profile_));
   contents_->SetDelegate(contents_delegate_.get());
 
   // The ZoomController needs to be created before the web contents is observed
