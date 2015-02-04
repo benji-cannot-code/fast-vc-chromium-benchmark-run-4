8581f36479e2e7df55398a71d879f9f902a5f083
hiroshige@chromium.org <hiroshige@chromium.org>
2015-02-04 10:01:09 +0000

    [Fetch] Add Response.ok
    BUG=455094
    
    Review URL: https://codereview.chromium.org/886063002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189469 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/fetch/Response.h
+++ third_party/WebKit/Source/modules/fetch/Response.h
@@ -47,6 +47,7 @@ public:
     String type() const;
     String url() const;
     unsigned short status() const;
+    bool ok() const;
     String statusText() const;
     Headers* headers() const;
 
