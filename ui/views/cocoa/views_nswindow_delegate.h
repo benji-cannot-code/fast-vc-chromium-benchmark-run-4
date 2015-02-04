d41c495a403d4cf31af3ae9ad39df45cd516fdac
tapted <tapted@chromium.org>
2015-02-04 14:24:48 -0800

    MacViews: Implement NativeWidgetMac::SetCursor()
    Tracking areas in Cocoa are a horrible mess. 10.5 semi-deprecated
    NSWindow cursorRects, and introduced [NSResponder cursorUpdate:]. Both
    are supported and they react with each other in strange ways.
    Intercepting events for toolkit-views SetCapture seems to further
    complicate things.
    
    After numerous dead-ends, this CL implements SetCursor() by overriding
    cursorUpdate: on NativeWidgetMac's NSWindow override. This works because
    cursorUpdate: messages are forwarded along the responder chain (starting
    from varied and somewhat unpredictable starting points), and usually end
    up at the NSWindow.
    
    Rather than adding more NSTrackingArea cruft, the one in
    BridgedContentView is updated to handle cursor updates. This ensures the
    cursor is reset to "normal" whenever it leaves the content area.
    
    Installing a global event monitor also resets the mouse cursor to an
    arrow. I tried tracing this with NSObjCMessageLoggingEnabled, but the
    reset isn't done with Objective-C. To fix, "remind" AppKit what the
    cursor should be after setting capture.
    
    See code comments for all the other traps.
    
    Testing tracking rectangles with simulated events is pretty much a lost
    cause, but NativeWidgetMacTest.SetCursor() is added which gets some
    coverage by sending an event directly to [NSWindow cursorUpdate:], and
    using the ui::test::EventGenerator to forward events to toolkit-views to
    actually request new cursors to set.
    
    BUG=454698
    
    Review URL: https://codereview.chromium.org/891003004
    
    Cr-Commit-Position: refs/heads/master@{#314660}

--- ui/views/cocoa/views_nswindow_delegate.h
+++ ui/views/cocoa/views_nswindow_delegate.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Cocoa/Cocoa.h>
 
+#import "base/mac/scoped_nsobject.h"
+
 namespace views {
 class NativeWidgetMac;
 class BridgedNativeWidget;
@@ -18,11 +20,16 @@ class BridgedNativeWidget;
 @interface ViewsNSWindowDelegate : NSObject<NSWindowDelegate> {
  @private
   views::BridgedNativeWidget* parent_;  // Weak. Owns this.
+  base::scoped_nsobject<NSCursor> cursor_;
 }
 
 // The NativeWidgetMac that created the window this is attached to. Returns
 // NULL if not created by NativeWidgetMac.
 @property(nonatomic, readonly) views::NativeWidgetMac* nativeWidgetMac;
+
+// If set, the cursor set in -[NSResponder updateCursor:] when the window is
+// reached along the responder chain.
+@property(retain, nonatomic) NSCursor* cursor;
 
 // Initialize with the given |parent|.
 - (id)initWithBridgedNativeWidget:(views::BridgedNativeWidget*)parent;
--- ui/views/cocoa/views_nswindow_delegate.mm
+++ ui/views/cocoa/views_nswindow_delegate.mm
@@ -24,6 +24,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return parent_->native_widget_mac();
 }
 
+- (NSCursor*)cursor {
+  return cursor_.get();
+}
+
+- (void)setCursor:(NSCursor*)newCursor {
+  if (cursor_.get() == newCursor)
+    return;
+
+  cursor_.reset([newCursor retain]);
+  [parent_->ns_window() resetCursorRects];
+}
+
 - (void)onWindowOrderWillChange:(NSWindowOrderingMode)orderingMode {
   parent_->OnVisibilityChangedTo(orderingMode != NSWindowOut);
 }
