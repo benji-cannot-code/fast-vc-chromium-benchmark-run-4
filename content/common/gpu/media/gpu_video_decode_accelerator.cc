3a01fddb4b2dd242bee4fc8ead3e44515035f9d7
posciak <posciak@chromium.org>
2015-02-04 21:07:17 -0800

    Revert of Clean up V4L2 codec-related flags and defines (patchset #3 id:40001 of https://codereview.chromium.org/828063009/)
    Reason for revert:
    x86 Chrome compile failure due to https://code.google.com/p/chromium/issues/detail?id=455545.
    
    Original issue's description:
    > Clean up V4L2 codec-related flags and defines
    >
    > All arm platforms support V4L2 SVDA by default and use_v4l2_codec also governs V4L2 VDA/VEA now.
    > Generates libv4l2 stub in GN file.
    >
    > BUG=449883
    > TEST=build chrome and vda unittest on peach_pit, veyron_pinky, squawks,
    > and panther_freon
    >
    > Committed: https://crrev.com/d5eeecc577f274bb8ed9d1954f263d83b5f00050
    > Cr-Commit-Position: refs/heads/master@{#314536}
    
    TBR=piman@chromium.org,wuchengli@chromium.org,henryhsu@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=449883
    
    Review URL: https://codereview.chromium.org/902773003
    
    Cr-Commit-Position: refs/heads/master@{#314760}

--- content/common/gpu/media/gpu_video_decode_accelerator.cc
+++ content/common/gpu/media/gpu_video_decode_accelerator.cc
@@ -32,15 +32,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #elif defined(OS_MACOSX)
 #include "content/common/gpu/media/vt_video_decode_accelerator.h"
 #elif defined(OS_CHROMEOS)
-#if defined(USE_V4L2_CODEC)
+#if defined(ARCH_CPU_ARMEL) && defined(USE_LIBV4L2)
 #include "content/common/gpu/media/v4l2_slice_video_decode_accelerator.h"
+#endif  // defined(ARCH_CPU_ARMEL)
+#if defined(ARCH_CPU_ARMEL) || (defined(USE_OZONE) && defined(USE_V4L2_CODEC))
 #include "content/common/gpu/media/v4l2_video_decode_accelerator.h"
 #include "content/common/gpu/media/v4l2_video_device.h"
+// defined(ARCH_CPU_ARMEL) || (defined(USE_OZONE) && defined(USE_V4L2_CODEC))
 #endif
 #if defined(ARCH_CPU_X86_FAMILY)
 #include "content/common/gpu/media/vaapi_video_decode_accelerator.h"
 #include "ui/gl/gl_implementation.h"
-#endif
+#endif  // defined(ARCH_CPU_X86_FAMILY)
 #elif defined(USE_OZONE)
 #include "media/ozone/media_ozone_platform.h"
 #elif defined(OS_ANDROID)
@@ -301,7 +304,8 @@ GpuVideoDecodeAccelerator::CreateDXVAVDA() {
 scoped_ptr<media::VideoDecodeAccelerator>
 GpuVideoDecodeAccelerator::CreateV4L2VDA() {
   scoped_ptr<media::VideoDecodeAccelerator> decoder;
-#if defined(OS_CHROMEOS) && defined(USE_V4L2_CODEC)
+#if defined(OS_CHROMEOS) && (defined(ARCH_CPU_ARMEL) || \
+    (defined(USE_OZONE) && defined(USE_V4L2_CODEC)))
   scoped_refptr<V4L2Device> device = V4L2Device::Create(V4L2Device::kDecoder);
   if (device.get()) {
     decoder.reset(new V4L2VideoDecodeAccelerator(
@@ -319,7 +323,7 @@ GpuVideoDecodeAccelerator::CreateV4L2VDA() {
 scoped_ptr<media::VideoDecodeAccelerator>
 GpuVideoDecodeAccelerator::CreateV4L2SliceVDA() {
   scoped_ptr<media::VideoDecodeAccelerator> decoder;
-#if defined(OS_CHROMEOS) && defined(USE_V4L2_CODEC)
+#if defined(OS_CHROMEOS) && defined(ARCH_CPU_ARMEL) && defined(USE_LIBV4L2)
   scoped_refptr<V4L2Device> device = V4L2Device::Create(V4L2Device::kDecoder);
   if (device.get()) {
     decoder.reset(new V4L2SliceVideoDecodeAccelerator(
