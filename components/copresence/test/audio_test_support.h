92279fea313f45203dd26b163cac1aaf2eca0359
ckehoe <ckehoe@chromium.org>
2015-02-04 19:26:59 -0800

    This removes some unused code and simplifies some dependencies. I'm planning to pull the audio code out into its own component, and it'll be clearer if I do minimal refactoring along the way.
    Review URL: https://codereview.chromium.org/901753002
    
    Cr-Commit-Position: refs/heads/master@{#314733}

--- components/copresence/test/audio_test_support.h
+++ components/copresence/test/audio_test_support.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef COMPONENTS_COPRESENCE_TEST_AUDIO_TEST_SUPPORT_
-#define COMPONENTS_COPRESENCE_TEST_AUDIO_TEST_SUPPORT_
+#ifndef COMPONENTS_COPRESENCE_TEST_AUDIO_TEST_SUPPORT_H_
+#define COMPONENTS_COPRESENCE_TEST_AUDIO_TEST_SUPPORT_H_
 
 #include <cstddef>
 
@@ -21,15 +21,10 @@ namespace copresence {
 // Populate random samples given a random seed into the samples array.
 void PopulateSamples(int random_seed, size_t size, float* samples);
 
-// Create an audio bus populated with random samples.
-scoped_ptr<media::AudioBus> CreateRandomAudio(int random_seed,
-                                              int channels,
-                                              int samples);
-
 // Create an ref counted audio bus populated with random samples.
 scoped_refptr<media::AudioBusRefCounted>
     CreateRandomAudioRefCounted(int random_seed, int channels, int samples);
 
 }  // namespace copresence
 
-#endif  // COMPONENTS_COPRESENCE_TEST_AUDIO_TEST_SUPPORT_
+#endif  // COMPONENTS_COPRESENCE_TEST_AUDIO_TEST_SUPPORT_H_
--- components/copresence/test/stub_whispernet_client.h
+++ components/copresence/test/stub_whispernet_client.h
@@ -11,16 +11,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/macros.h"
+#include "base/memory/ref_counted.h"
 #include "components/copresence/public/whispernet_client.h"
+#include "components/copresence/tokens.h"
+#include "media/base/audio_bus.h"
 
 namespace copresence {
 
 // A simple WhispernetClient for testing.
 class StubWhispernetClient final : public WhispernetClient {
  public:
-  // Constructor. The client can optionally be configured to respond
-  // as if Initialize() has completed. By default it does not.
-  explicit StubWhispernetClient(bool complete_initialization = false);
+  // Constructor. |samples| and |tokens|, if specified,
+  // will be returned for any encoding and decoding requests.
+  StubWhispernetClient(
+      scoped_refptr<media::AudioBusRefCounted> samples =
+          scoped_refptr<media::AudioBusRefCounted>(),
+      const std::vector<AudioToken>& tokens = std::vector<AudioToken>());
 
   ~StubWhispernetClient() override;
 
@@ -44,11 +50,10 @@ class StubWhispernetClient final : public WhispernetClient {
   SuccessCallback GetInitializedCallback() override;
 
  private:
-  bool complete_initialization_;
   TokensCallback tokens_cb_;
   SamplesCallback samples_cb_;
-  std::vector<AudioToken> tokens_;
   scoped_refptr<media::AudioBusRefCounted> samples_;
+  std::vector<AudioToken> tokens_;
 
   DISALLOW_COPY_AND_ASSIGN(StubWhispernetClient);
 };
