baca3e64cca00c7bdd9e774e676b274ff346b9ad
benjhayden@chromium.org <benjhayden@chromium.org>
2015-02-05 18:16:49 +0000

    Renderer.LineLayoutMs UMA histogram to measure line layout time.
    Review URL: https://codereview.chromium.org/877293003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189578 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/FrameView.h
+++ third_party/WebKit/Source/core/frame/FrameView.h
@@ -552,6 +552,8 @@ public:
     // Returns the scrollable area for the frame.
     ScrollableArea* scrollableArea();
 
+    void lineLayoutTime(double ms);
+
 protected:
     // Scroll the content via the compositor.
     bool scrollContentsFastPath(const IntSize& scrollDelta);
@@ -825,6 +827,8 @@ private:
     bool m_shouldDrawPanScrollIcon;
 
     bool m_clipsRepaints;
+
+    double m_lineLayoutMs;
 };
 
 inline void FrameView::incrementVisuallyNonEmptyCharacterCount(unsigned count)
