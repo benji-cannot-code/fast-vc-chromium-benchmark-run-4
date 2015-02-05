c156fe243e275d98580430d019c45127ad39cf13
timvolodine <timvolodine@chromium.org>
2015-02-05 12:25:33 -0800

    Clean-up refactoring of chrome_content_browser_client to use GetPermissionContext consistently.
    Eliminate some duplicate code now that PermissionContextBase is the base class
    for all other permission contexts and we have a helper function
    GetPermissionContext to obtain the proper permission context class depending
    on permission type. Also move PermissionToContentSetting to anonymous namespace.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/896173002
    
    Cr-Commit-Position: refs/heads/master@{#314866}

--- chrome/browser/services/gcm/push_messaging_permission_context.h
+++ chrome/browser/services/gcm/push_messaging_permission_context.h
@@ -26,6 +26,9 @@ class PushMessagingPermissionContext : public PermissionContextBase {
       const GURL& requesting_origin,
       const GURL& embedding_origin) const override;
 
+  void CancelPermissionRequest(content::WebContents* web_contents,
+                               const PermissionRequestID& id) override;
+
  protected:
   // PermissionContextBase:
   void DecidePermission(content::WebContents* web_contents,
