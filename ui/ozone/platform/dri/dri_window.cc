5be74e4e7c47457d0d3ff96af2373ad79880d001
spang <spang@chromium.org>
2015-02-05 11:56:44 -0800

    ozone: dri: Re-add call to show cursor after bounds change
    This fixes some cases of the cursor disappearing temporarily after a
    bounds change that started after r313336 ("Constrain the cursor when
    overscan insets are set").
    
    We hide the cursor during bounds changes, so we must make sure to show
    it again afterwards.
    
    BUG=427619
    TEST=On link_freon, change the display resolution via Ctrl+Shift+Plus or
    Ctrl+Shift+Minus, and make sure the cursor is visible afterwards.
    
    Review URL: https://codereview.chromium.org/896343002
    
    Cr-Commit-Position: refs/heads/master@{#314862}

--- ui/ozone/platform/dri/dri_window.cc
+++ ui/ozone/platform/dri/dri_window.cc
@@ -179,6 +179,7 @@ void DriWindow::OnChannelDestroyed() {
 void DriWindow::SendBoundsChange() {
   cursor_->PrepareForBoundsChange(widget_);
   sender_->Send(new OzoneGpuMsg_WindowBoundsChanged(widget_, bounds_));
+  cursor_->ConfineCursorToBounds(widget_, GetCursorConfinedBounds());
 }
 
 }  // namespace ui
