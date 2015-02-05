c719bcbf18e86d3f5b8f72cee94fb5a3bdf2311c
dmazzoni <dmazzoni@chromium.org>
2015-02-05 07:28:48 -0800

    Suppress accessibility events when user is navigating away.
    When the user navigates to a new page, stop sending accessibility
    events on the old page.
    
    BUG=421116,450409
    
    Review URL: https://codereview.chromium.org/830053004
    
    Cr-Commit-Position: refs/heads/master@{#314812}

--- content/browser/accessibility/browser_accessibility_manager.cc
+++ content/browser/accessibility/browser_accessibility_manager.cc
@@ -80,6 +80,7 @@ BrowserAccessibilityManager::BrowserAccessibilityManager(
       factory_(factory),
       tree_(new ui::AXSerializableTree()),
       focus_(NULL),
+      user_is_navigating_away_(false),
       osk_state_(OSK_ALLOWED) {
   tree_->SetDelegate(this);
 }
@@ -92,6 +93,7 @@ BrowserAccessibilityManager::BrowserAccessibilityManager(
       factory_(factory),
       tree_(new ui::AXSerializableTree()),
       focus_(NULL),
+      user_is_navigating_away_(false),
       osk_state_(OSK_ALLOWED) {
   tree_->SetDelegate(this);
   Initialize(initial_tree);
@@ -151,6 +153,22 @@ void BrowserAccessibilityManager::OnWindowFocused() {
 void BrowserAccessibilityManager::OnWindowBlurred() {
   if (focus_)
     NotifyAccessibilityEvent(ui::AX_EVENT_BLUR, GetFromAXNode(focus_));
+}
+
+void BrowserAccessibilityManager::UserIsNavigatingAway() {
+  user_is_navigating_away_ = true;
+}
+
+void BrowserAccessibilityManager::UserIsReloading() {
+  user_is_navigating_away_ = true;
+}
+
+void BrowserAccessibilityManager::NavigationSucceeded() {
+  user_is_navigating_away_ = false;
+}
+
+void BrowserAccessibilityManager::NavigationFailed() {
+  user_is_navigating_away_ = false;
 }
 
 void BrowserAccessibilityManager::GotMouseDown() {
