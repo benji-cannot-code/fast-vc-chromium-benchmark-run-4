96c8e0afebc999db6294880847c05cc8e5e3ccb3
jchuang <jchuang@chromium.org>
2015-02-05 10:00:59 -0800

    Image Capture PPAPI: remove unused APIs
    1. Remove ppb_image_capture_config_private.
    2. Remove PPB_ImageCapture_Private.{Set|Get}Config
    3. Remove PPB_ImageCapture_Private.CaptureStillImage
    
    BUG=387547
    TEST=None
    
    Review URL: https://codereview.chromium.org/884483007
    
    Cr-Commit-Position: refs/heads/master@{#314834}

--- ppapi/tests/all_c_includes.h
+++ ppapi/tests/all_c_includes.h
@@ -118,7 +118,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/c/private/ppb_flash_menu.h"
 #include "ppapi/c/private/ppb_flash_message_loop.h"
 #include "ppapi/c/private/ppb_host_resolver_private.h"
-#include "ppapi/c/private/ppb_image_capture_config_private.h"
 #include "ppapi/c/private/ppb_image_capture_private.h"
 #include "ppapi/c/private/ppb_input_event_private.h"
 #include "ppapi/c/private/ppb_instance_private.h"
--- ppapi/thunk/ppb_image_capture_private_thunk.cc
+++ ppapi/thunk/ppb_image_capture_private_thunk.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// From private/ppb_image_capture_private.idl modified Wed Nov  5 14:29:15 2014.
+// From private/ppb_image_capture_private.idl modified Thu Feb  5 22:47:43 2015.
 
 #include "ppapi/c/pp_completion_callback.h"
 #include "ppapi/c/pp_errors.h"
@@ -20,14 +20,13 @@ namespace {
 
 PP_Resource Create(PP_Instance instance,
                    struct PP_Var camera_source_id,
-                   PPB_ImageCapture_Private_ErrorCallback error_callback,
                    void* user_data) {
   VLOG(4) << "PPB_ImageCapture_Private::Create()";
   EnterResourceCreation enter(instance);
   if (enter.failed())
     return 0;
   return enter.functions()->CreateImageCapturePrivate(
-      instance, camera_source_id, error_callback, user_data);
+      instance, camera_source_id, user_data);
 }
 
 PP_Bool IsImageCapture(PP_Resource resource) {
@@ -44,26 +43,6 @@ int32_t Close(PP_Resource resource, struct PP_CompletionCallback callback) {
   return enter.SetResult(enter.object()->Close(enter.callback()));
 }
 
-int32_t SetConfig(PP_Resource image_capture,
-                  PP_Resource config,
-                  struct PP_CompletionCallback callback) {
-  VLOG(4) << "PPB_ImageCapture_Private::SetConfig()";
-  EnterResource<PPB_ImageCapture_API> enter(image_capture, callback, true);
-  if (enter.failed())
-    return enter.retval();
-  return enter.SetResult(enter.object()->SetConfig(config, enter.callback()));
-}
-
-int32_t GetConfig(PP_Resource image_capture,
-                  PP_Resource* config,
-                  struct PP_CompletionCallback callback) {
-  VLOG(4) << "PPB_ImageCapture_Private::GetConfig()";
-  EnterResource<PPB_ImageCapture_API> enter(image_capture, callback, true);
-  if (enter.failed())
-    return enter.retval();
-  return enter.SetResult(enter.object()->GetConfig(config, enter.callback()));
-}
-
 int32_t GetCameraCapabilities(PP_Resource image_capture,
                               PP_Resource* capabilities,
                               struct PP_CompletionCallback callback) {
@@ -75,28 +54,8 @@ int32_t GetCameraCapabilities(PP_Resource image_capture,
       enter.object()->GetCameraCapabilities(capabilities, enter.callback()));
 }
 
-int32_t CaptureStillImage(
-    PP_Resource image_capture,
-    PPB_ImageCapture_Private_ShutterCallback shutter_callback,
-    PPB_ImageCapture_Private_PreviewCallback preview_callback,
-    PPB_ImageCapture_Private_JpegCallback jpeg_callback,
-    int64_t* sequence_id) {
-  VLOG(4) << "PPB_ImageCapture_Private::CaptureStillImage()";
-  EnterResource<PPB_ImageCapture_API> enter(image_capture, true);
-  if (enter.failed())
-    return enter.retval();
-  return enter.object()->CaptureStillImage(shutter_callback, preview_callback,
-                                           jpeg_callback, sequence_id);
-}
-
-const PPB_ImageCapture_Private_0_1 g_ppb_imagecapture_private_thunk_0_1 = {
-    &Create,
-    &IsImageCapture,
-    &Close,
-    &SetConfig,
-    &GetConfig,
-    &GetCameraCapabilities,
-    &CaptureStillImage};
+const PPB_ImageCapture_Private_0_1 g_ppb_imagecapture_private_thunk_0_1 =
+    {&Create, &IsImageCapture, &Close, &GetCameraCapabilities};
 
 }  // namespace
 
