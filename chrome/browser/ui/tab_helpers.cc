007f12853b825873992d3f1a861df8f066c77a7c
ianwen <ianwen@chromium.org>
2015-02-05 12:56:12 -0800

    [Enhanced Bookmark]Upstream image fetching code in android
    This CL added the image fetching code that previously lived in clank private repository.
    
    Also I moved android enhanced_bookmark_bridge from
    chrome/browser/android/enhanced_bookmarks/ to chrome/browser/enhanced_bookmarks/android,
    since the latter is a more unified place to put all files in.(I deleted the
    android/enhanced_bookmarks folder).
    
    Last but not least, I made some small changes to the internal implementation of
    bookmark_image_service, making it more readable.
    
    The previously private code can be found in code search:
    https://cs.corp.google.com/#clankium/src/clank/native/framework/chrome/enhanced_bookmarks/
    
    Downstream CL: https://chrome-internal-review.googlesource.com/#/c/195438/
    BUG=454623
    
    Review URL: https://codereview.chromium.org/899653003
    
    Cr-Commit-Position: refs/heads/master@{#314872}

--- chrome/browser/ui/tab_helpers.cc
+++ chrome/browser/ui/tab_helpers.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_ANDROID)
 #include "chrome/browser/android/webapps/single_tab_mode_tab_helper.h"
+#include "chrome/browser/enhanced_bookmarks/android/enhanced_bookmark_tab_helper.h"
 #include "chrome/browser/ui/android/context_menu_helper.h"
 #include "chrome/browser/ui/android/window_android_helper.h"
 #else
@@ -162,6 +163,7 @@ void TabHelpers::AttachTabHelpers(WebContents* web_contents) {
 
 #if defined(OS_ANDROID)
   ContextMenuHelper::CreateForWebContents(web_contents);
+  EnhancedBookmarkTabHelper::CreateForWebContents(web_contents);
   SingleTabModeTabHelper::CreateForWebContents(web_contents);
   WindowAndroidHelper::CreateForWebContents(web_contents);
 #else
