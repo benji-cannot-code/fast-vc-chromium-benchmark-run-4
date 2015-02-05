b633f69018a6e252f2e20465b6c3aab11edfd34b
dcheng <dcheng@chromium.org>
2015-02-05 00:14:22 -0800

    Revert of Fix the histograms whitelist for the OOP PDF plugin. (patchset #1 id:1 of https://codereview.chromium.org/897863003/)
    Reason for revert:
    Introduced 2 new static initializers:
    
    pepper_uma_host.cc (anonymous namespace)::kWhitelistedPluginBaseNames
    pepper_uma_host.cc __init_array_end+0x5e8
    
    Original issue's description:
    > Fix the histograms whitelist for the OOP PDF plugin.
    >
    > BUG=405305
    >
    > Committed: https://crrev.com/ad1fecf3841f539281c8d6ff3a873bfc9c11571d
    > Cr-Commit-Position: refs/heads/master@{#314764}
    
    TBR=raymes@chromium.org,thestig@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=405305
    
    Review URL: https://codereview.chromium.org/901863002
    
    Cr-Commit-Position: refs/heads/master@{#314776}

--- chrome/renderer/pepper/pepper_uma_host.cc
+++ chrome/renderer/pepper/pepper_uma_host.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram.h"
 #include "base/sha1.h"
 #include "base/strings/string_number_conversions.h"
-#include "chrome/common/chrome_content_client.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/render_messages.h"
 #include "chrome/renderer/chrome_content_renderer_client.h"
@@ -32,11 +31,11 @@ namespace {
 
 const char* const kPredefinedAllowedUMAOrigins[] = {
     "6EAED1924DB611B6EEF2A664BD077BE7EAD33B8F",  // see http://crbug.com/317833
-    "4EB74897CB187C7633357C2FE832E0AD6A44883A",  // see http://crbug.com/317833
+    "4EB74897CB187C7633357C2FE832E0AD6A44883A"   // see http://crbug.com/317833
 };
 
 const char* const kWhitelistedHistogramPrefixes[] = {
-    "22F67DA2061FFC4DC9A4974036348D9C38C22919",  // see http://crbug.com/390221
+    "22F67DA2061FFC4DC9A4974036348D9C38C22919"  // see http://crbug.com/390221
 };
 
 const char* const kWhitelistedPluginBaseNames[] = {
@@ -44,7 +43,7 @@ const char* const kWhitelistedPluginBaseNames[] = {
     kWidevineCdmAdapterFileName,  // see http://crbug.com/368743
                                   // and http://crbug.com/410630
 #endif
-    ChromeContentClient::kPDFPluginPath,
+    "libpdf.so"                   // see http://crbug.com/405305
 };
 
 std::string HashPrefix(const std::string& histogram) {
@@ -108,12 +107,13 @@ bool PepperUMAHost::IsHistogramAllowed(const std::string& histogram) {
   }
 
   if (IsPluginWhitelisted() &&
-      ContainsKey(allowed_histogram_prefixes_, HashPrefix(histogram))) {
+      allowed_histogram_prefixes_.find(HashPrefix(histogram)) !=
+          allowed_histogram_prefixes_.end()) {
     return true;
   }
 
-  if (ContainsKey(allowed_plugin_base_names_,
-                  plugin_base_name_.MaybeAsASCII())) {
+  if (allowed_plugin_base_names_.find(plugin_base_name_.MaybeAsASCII()) !=
+      allowed_plugin_base_names_.end()) {
     return true;
   }
 
