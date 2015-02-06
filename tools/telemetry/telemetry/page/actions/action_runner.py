78c5581a6c84e81140777d9e7e4bed382d7b1907
skyostil <skyostil@chromium.org>
2015-02-06 07:06:35 -0800

    Revert of [telemetry] Add a V8GCTimes measurement and benchmarks. (patchset #2 id:20001 of https://codereview.chromium.org/898673005/)
    Reason for revert:
    Failing on mac:
    
    http://chromegw/i/chromium.perf/builders/Mac%2010.9%20Perf%20%281%29/builds/2017
    
    Original issue's description:
    > [telemetry] Add a V8GCTimes measurement and benchmarks.
    >
    > Replaces the v8_stats metric with a v8_gc_times
    > measurement.  This new measurement measures the amount of
    > time spent in GC during the whole benchmark rather than
    > just during gestures.
    >
    > The following gc metrics are measured:
    >   v8_gc_incremental_marking
    >   v8_gc_incremental_marking_idle_deadline_overrun
    >   v8_gc_incremental_marking_outside_idle
    >   v8_gc_incremental_marking_percentage_idle
    >   v8_gc_mark_compactor
    >   v8_gc_mark_compactor_idle_deadline_overrun
    >   v8_gc_mark_compactor_outside_idle
    >   v8_gc_mark_compactor_percentage_idle
    >   v8_gc_scavenger
    >   v8_gc_scavenger_idle_deadline_overrun
    >   v8_gc_scavenger_outside_idle
    >   v8_gc_scavenger_percentage_idle
    >   v8_gc_total
    >   v8_gc_total_idle_deadline_overrun
    >   v8_gc_total_outside_idle
    >
    > It also measures the duration of the measurement and the
    > amount of CPU time used.
    >
    > The v8_garbage_collection_cases benchmark is modified to
    > use this new v8_gc_times measurement, and v8.top_25_smooth
    > and v8.key_mobile_sites_smooth benchmarks have been added.
    >
    > Since v8_garbage_collection_cases was the only benchmark to
    > use the is_fast metric type, and these measurements are now
    > done as part of the v8_gc_times measurement now, this CL
    > also removes the fast metric type.
    >
    > Perf Sherrifs: Since this CL changes he way in which the GC
    > measurements are done it may cause a movement in the
    > v8_garbage_collection_cases benchmarks.
    >
    > Committed: https://crrev.com/452de5927e54e1da0d57a4b2df8b447f4d442360
    > Cr-Commit-Position: refs/heads/master@{#314868}
    
    TBR=hpayer@chromium.org,rmcilroy@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/904903002
    
    Cr-Commit-Position: refs/heads/master@{#315035}

--- tools/telemetry/telemetry/page/actions/action_runner.py
+++ tools/telemetry/telemetry/page/actions/action_runner.py
@@ -34,8 +34,8 @@ class ActionRunner(object):
     action.WillRunAction(self._tab)
     action.RunAction(self._tab)
 
-  def BeginInteraction(self, label, is_smooth=False, is_responsive=False,
-                       repeatable=False):
+  def BeginInteraction(self, label, is_fast=False, is_smooth=False,
+                       is_responsive=False, repeatable=False):
     """Marks the beginning of an interaction record.
 
     An interaction record is a labeled time period containing
@@ -47,6 +47,8 @@ class ActionRunner(object):
     Args:
       label: A label for this particular interaction. This can be any
           user-defined string, but must not contain '/'.
+      is_fast: Whether to measure how fast the browser completes necessary work
+          for this interaction record. See fast_metric.py for details.
       is_smooth: Whether to check for smoothness metrics for this interaction.
       is_responsive: Whether to check for responsiveness metrics for
           this interaction.
@@ -55,6 +57,8 @@ class ActionRunner(object):
           have the same flags.
     """
     flags = []
+    if is_fast:
+      flags.append(timeline_interaction_record.IS_FAST)
     if is_smooth:
       flags.append(timeline_interaction_record.IS_SMOOTH)
     if is_responsive:
@@ -66,8 +70,8 @@ class ActionRunner(object):
     interaction.Begin()
     return interaction
 
-  def BeginGestureInteraction(self, label, is_smooth=False, is_responsive=False,
-                              repeatable=False):
+  def BeginGestureInteraction(self, label, is_fast=False, is_smooth=False,
+                              is_responsive=False, repeatable=False):
     """Marks the beginning of a gesture-based interaction record.
 
     This is similar to normal interaction record, but it will
@@ -81,6 +85,8 @@ class ActionRunner(object):
     Args:
       label: A label for this particular interaction. This can be any
           user-defined string, but must not contain '/'.
+      is_fast: Whether to measure how fast the browser completes necessary work
+          for this interaction record. See fast_metric.py for details.
       is_smooth: Whether to check for smoothness metrics for this interaction.
       is_responsive: Whether to check for responsiveness metrics for
           this interaction.
@@ -88,8 +94,8 @@ class ActionRunner(object):
           as this interaction. All interactions with the same logical name must
           have the same flags.
     """
-    return self.BeginInteraction('Gesture_' + label, is_smooth, is_responsive,
-                                 repeatable)
+    return self.BeginInteraction('Gesture_' + label, is_fast, is_smooth,
+                                 is_responsive, repeatable)
 
   def NavigateToPage(self, page, timeout_in_seconds=60):
     """Navigates to the given page.
