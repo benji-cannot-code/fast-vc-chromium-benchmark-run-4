e5a33a704cfb3532678beebf06da42f7ad827608
dcheng <dcheng@chromium.org>
2015-02-05 19:32:20 -0800

    Update {virtual,override,final} to follow C++11 style in base.
    The Google style guide states that only one of {virtual,override,final}
    should be used for each declaration, since override implies virtual and
    final implies both virtual and override.
    
    This patch was automatically generated with an OS=android build using a
    variation of https://codereview.chromium.org/598073004.
    
    BUG=417463
    
    Review URL: https://codereview.chromium.org/900613004
    
    Cr-Commit-Position: refs/heads/master@{#314951}

--- base/android/scoped_java_ref_unittest.cc
+++ base/android/scoped_java_ref_unittest.cc
@@ -41,7 +41,7 @@ void DeleteLocalRef(JNIEnv* env, jobject obj) {
 
 class ScopedJavaRefTest : public testing::Test {
  protected:
-  virtual void SetUp() {
+  void SetUp() override {
     g_local_refs = 0;
     g_global_refs = 0;
     JNIEnv* env = AttachCurrentThread();
@@ -56,7 +56,7 @@ class ScopedJavaRefTest : public testing::Test {
     hooked_functions.DeleteLocalRef = &DeleteLocalRef;
   }
 
-  virtual void TearDown() {
+  void TearDown() override {
     JNIEnv* env = AttachCurrentThread();
     env->functions = g_previous_functions;
   }
