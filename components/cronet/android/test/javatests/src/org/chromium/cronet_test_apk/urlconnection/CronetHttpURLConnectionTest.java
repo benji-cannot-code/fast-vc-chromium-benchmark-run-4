dc7d8d40c825f241f36d99e9ab93f789c09b3f0b
xunjieli <xunjieli@chromium.org>
2015-02-06 10:28:20 -0800

    [Cronet] Adds header field location test
    BUG=398997
    
    Review URL: https://codereview.chromium.org/885783008
    
    Cr-Commit-Position: refs/heads/master@{#315073}

--- components/cronet/android/test/javatests/src/org/chromium/cronet_test_apk/urlconnection/CronetHttpURLConnectionTest.java
+++ components/cronet/android/test/javatests/src/org/chromium/cronet_test_apk/urlconnection/CronetHttpURLConnectionTest.java
@@ -545,6 +545,7 @@ public class CronetHttpURLConnectionTest extends CronetTestBase {
         connection.setInstanceFollowRedirects(false);
         assertEquals(302, connection.getResponseCode());
         assertEquals("Found", connection.getResponseMessage());
+        assertEquals("/success.txt", connection.getHeaderField("Location"));
         assertEquals(NativeTestServer.getFileURL("/redirect.html"),
                 connection.getURL().toString());
         connection.disconnect();
@@ -560,6 +561,7 @@ public class CronetHttpURLConnectionTest extends CronetTestBase {
                 (HttpURLConnection) url.openConnection();
         assertEquals(302, connection.getResponseCode());
         assertEquals("Found", connection.getResponseMessage());
+        assertEquals("/success.txt", connection.getHeaderField("Location"));
         assertEquals(NativeTestServer.getFileURL("/redirect.html"),
                 connection.getURL().toString());
         connection.disconnect();
