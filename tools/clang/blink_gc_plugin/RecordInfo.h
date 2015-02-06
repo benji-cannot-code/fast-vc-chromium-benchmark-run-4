bf67ae43665cca2b55f68cd06cd19f31666d224e
hans <hans@chromium.org>
2015-02-05 18:11:14 -0800

    Roll Clang 223108:228129
    Also introduce a build number in the package name, stop versioning
    the Blink GC plugin, and remove repackage.sh.
    
    BUG=444089
    
    Review URL: https://codereview.chromium.org/881763003
    
    Cr-Commit-Position: refs/heads/master@{#314946}

--- tools/clang/blink_gc_plugin/RecordInfo.h
+++ tools/clang/blink_gc_plugin/RecordInfo.h
@@ -24,6 +24,7 @@ class RecordCache;
 class GraphPoint {
  public:
   GraphPoint() : traced_(false) {}
+  virtual ~GraphPoint() {}
   void MarkTraced() { traced_ = true; }
   bool IsProperlyTraced() { return traced_ || !NeedsTracing().IsNeeded(); }
   virtual const TracingStatus NeedsTracing() = 0;
