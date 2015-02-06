e5a74739eb4e6fae562684346c3222177854e880
aandrey@chromium.org <aandrey@chromium.org>
2015-02-06 14:12:33 +0000

    DevTools: [CanvasProfiler] Catch up with recent API updates.
    Capture "ellipse" and "resetTransform" methods of canvas 2D context which have been public for awhile.
    
    R=caseq
    
    Review URL: https://codereview.chromium.org/889893003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189645 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InjectedScriptCanvasModuleSource.js
+++ third_party/WebKit/Source/core/inspector/InjectedScriptCanvasModuleSource.js
@@ -3147,7 +3147,8 @@ CanvasRenderingContext2DResource.PathMethods = [
     "bezierCurveTo",
     "arcTo",
     "arc",
-    "rect"
+    "rect",
+    "ellipse"
 ];
 
 /**
@@ -3159,7 +3160,8 @@ CanvasRenderingContext2DResource.TransformationMatrixMethods = [
     "rotate",
     "translate",
     "transform",
-    "setTransform"
+    "setTransform",
+    "resetTransform"
 ];
 
 /**
@@ -3297,7 +3299,7 @@ CanvasRenderingContext2DResource.prototype = {
     /**
      * @param {!Call} call
      */
-    pushCall_setTransform: function(call)
+    pushCall_setOrResetTransform: function(call)
     {
         var saveCallIndex = this._lastIndexOfMatchingSaveCall();
         var index = this._lastIndexOfAnyCall(CanvasRenderingContext2DResource.PathMethods);
@@ -3409,14 +3411,14 @@ CanvasRenderingContext2DResource.prototype = {
             lastIndexOfBeginPath = this._lastIndexOfAnyCall(["beginPath"], index - 1);
         }
 
-        // Remove all TRASFORMATION MATRIX methods before restore() or setTransform() but after any PATH or corresponding save() method.
-        var lastRestore = this._lastIndexOfAnyCall(["restore", "setTransform"]);
+        // Remove all TRASFORMATION MATRIX methods before restore(), setTransform() or resetTransform() but after any PATH or corresponding save() method.
+        var lastRestore = this._lastIndexOfAnyCall(["restore", "setTransform", "resetTransform"]);
         while (lastRestore !== -1) {
             var saveCallIndex = this._lastIndexOfMatchingSaveCall(lastRestore - 1);
             var index = this._lastIndexOfAnyCall(CanvasRenderingContext2DResource.PathMethods, lastRestore - 1);
             index = Math.max(index, saveCallIndex);
             this._removeCallsFromLog(CanvasRenderingContext2DResource.TransformationMatrixMethods, index + 1, lastRestore);
-            lastRestore = this._lastIndexOfAnyCall(["restore", "setTransform"], index - 1);
+            lastRestore = this._lastIndexOfAnyCall(["restore", "setTransform", "resetTransform"], index - 1);
         }
 
         // Remove all save-restore consecutive pairs.
@@ -3482,7 +3484,7 @@ CanvasRenderingContext2DResource.prototype = {
             wrapFunctions["createPattern"] = Resource.WrapFunction.resourceFactoryMethod(LogEverythingResource, "CanvasPattern");
 
             for (var i = 0, methodName; methodName = CanvasRenderingContext2DResource.TransformationMatrixMethods[i]; ++i)
-                stateModifyingWrapFunction(methodName, methodName === "setTransform" ? this.pushCall_setTransform : undefined);
+                stateModifyingWrapFunction(methodName, (methodName === "setTransform" || methodName === "resetTransform") ? this.pushCall_setOrResetTransform : undefined);
             for (var i = 0, methodName; methodName = CanvasRenderingContext2DResource.PathMethods[i]; ++i)
                 stateModifyingWrapFunction(methodName, methodName === "beginPath" ? this.pushCall_beginPath : undefined);
 
