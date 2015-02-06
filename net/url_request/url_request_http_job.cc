0bb7f9201cec531390ba8435e2726666cd471e7f
battre <battre@chromium.org>
2015-02-06 09:21:02 -0800

    DumpWithoutCrashing() if URLRequestHttpJob is killed while waiting for callback
    BUG=432398
    
    Review URL: https://codereview.chromium.org/892413007
    
    Cr-Commit-Position: refs/heads/master@{#315052}

--- net/url_request/url_request_http_job.cc
+++ net/url_request/url_request_http_job.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/compiler_specific.h"
 #include "base/debug/alias.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/file_version_info.h"
 #include "base/message_loop/message_loop.h"
 #include "base/metrics/field_trial.h"
@@ -290,6 +291,12 @@ void URLRequestHttpJob::Start() {
 }
 
 void URLRequestHttpJob::Kill() {
+  if (awaiting_callback_) {
+    // TODO(battre) crbug.com/289715
+    // Simulate a crash to see who kills the job while it is waiting for a
+    // callback. This should not happen, see URLRequest::OrphanJob().
+    base::debug::DumpWithoutCrashing();
+  }
   if (!transaction_.get())
     return;
 
