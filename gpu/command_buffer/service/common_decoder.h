65b6de305db236a91d9665fff22a933f9f143105
zmo <zmo@chromium.org>
2015-02-06 13:58:07 -0800

    Refactor for GPU command buffer PUTSTR code generator.
    This is a pure code moving around CL.  No logic change at all.
    
    So we add helper functions to pack/unpack strings to/from a bucket, so
    the code generator is now much simpler.
    
    This is to get the pack/unpack functions to be used by glGetUniformIndices.
    
    BUG=429053
    TEST=gpu_unittests, webgl_conformance
    R=sievers@chromium.org
    
    Review URL: https://codereview.chromium.org/902943002
    
    Cr-Commit-Position: refs/heads/master@{#315118}

--- gpu/command_buffer/service/common_decoder.h
+++ gpu/command_buffer/service/common_decoder.h
@@ -15,6 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/service/cmd_parser.h"
 #include "gpu/gpu_export.h"
 
+// Forwardly declare a few GL types to avoid including GL header files.
+typedef int GLsizei;
+typedef int GLint;
+
 namespace gpu {
 
 class CommandBufferEngine;
@@ -82,6 +86,13 @@ class GPU_EXPORT CommonDecoder : NON_EXPORTED_BASE(public AsyncAPIInterface) {
     // distrinquish between empty string and no string. Returns False if there
     // is no string.
     bool GetAsString(std::string* str);
+
+    // Gets the bucket data as strings.
+    // On success, the number of strings are in |_count|, the string data are
+    // in |_string|, and string sizes are in |_length|..
+    bool GetAsStrings(GLsizei* _count,
+                      std::vector<char*>* _string,
+                      std::vector<GLint>* _length);
 
    private:
     bool OffsetSizeValid(size_t offset, size_t size) const {
