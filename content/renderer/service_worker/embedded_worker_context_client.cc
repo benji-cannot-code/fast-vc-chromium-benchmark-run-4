4bd9cbf49f009578ccb015ea120e0b8a247d3503
nhiroki <nhiroki@chromium.org>
2015-02-05 20:55:58 -0800

    ServiceWorker: Support SWRegistration.unregister() in SWGlobalScope [1/2]
    SWRegistration.unregister() depends on WebSWProvider, but it's not available
    in SWGlobalScope because the provider isn't supplied on the worker startup
    sequence. This series of CLs make it available and support unregister() in
    the service worker context.
    
    This CL implements createServiceWorkerProvider() to supply a provider and
    sets the document URL of its provider host to the worker script URL in
    order to allow unregister() call on SWGlobalScope.
    
    [1] Chromium: THIS PATCH
    [2] Blink: https://codereview.chromium.org/900793002/
    
    BUG=452910
    TEST=compile
    
    Review URL: https://codereview.chromium.org/893363003
    
    Cr-Commit-Position: refs/heads/master@{#314979}

--- content/renderer/service_worker/embedded_worker_context_client.cc
+++ content/renderer/service_worker/embedded_worker_context_client.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/child/service_worker/service_worker_provider_context.h"
 #include "content/child/service_worker/service_worker_registration_handle_reference.h"
 #include "content/child/service_worker/web_service_worker_impl.h"
+#include "content/child/service_worker/web_service_worker_provider_impl.h"
 #include "content/child/service_worker/web_service_worker_registration_impl.h"
 #include "content/child/thread_safe_sender.h"
 #include "content/child/worker_task_runner.h"
@@ -367,6 +368,16 @@ EmbeddedWorkerContextClient::createServiceWorkerNetworkProvider(
 
   // Blink is responsible for deleting the returned object.
   return new WebServiceWorkerNetworkProviderImpl();
+}
+
+blink::WebServiceWorkerProvider*
+EmbeddedWorkerContextClient::createServiceWorkerProvider() {
+  DCHECK(main_thread_proxy_->RunsTasksOnCurrentThread());
+  DCHECK(provider_context_);
+
+  // Blink is responsible for deleting the returned object.
+  return new WebServiceWorkerProviderImpl(
+      thread_safe_sender(), provider_context_.get());
 }
 
 void EmbeddedWorkerContextClient::postMessageToClient(
