96df193b0d44c442f88f187dca84e01346f11e6e
jadeg@chromium.org <jadeg@chromium.org>
2015-02-06 05:29:37 +0000

    Implement Length Properties in String Keyframe
    Updating coverage of CSSProperties that use LengthStyleInterpolation
    
    Implements
    CSSPropertyFlexBasis
    CSSPropertyWebkitBorderHorizontalSpacing
    CSSPropertyWebkitBorderVerticalSpacing
    CSSPropertyWebkitColumnGap
    CSSPropertyWebkitColumnWidth
    
    BUG=443039
    
    Review URL: https://codereview.chromium.org/898223002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189615 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/StringKeyframe.cpp
+++ third_party/WebKit/Source/core/animation/StringKeyframe.cpp
@@ -110,6 +110,7 @@ PassRefPtrWillBeRawPtr<Interpolation> StringKeyframe::PropertySpecificKeyframe::
     case CSSPropertyBorderLeftWidth:
     case CSSPropertyBorderRightWidth:
     case CSSPropertyBorderTopWidth:
+    case CSSPropertyFlexBasis:
     case CSSPropertyFontSize:
     case CSSPropertyHeight:
     case CSSPropertyMaxHeight:
@@ -124,6 +125,10 @@ PassRefPtrWillBeRawPtr<Interpolation> StringKeyframe::PropertySpecificKeyframe::
     case CSSPropertyPaddingTop:
     case CSSPropertyPerspective:
     case CSSPropertyShapeMargin:
+    case CSSPropertyWebkitBorderHorizontalSpacing:
+    case CSSPropertyWebkitBorderVerticalSpacing:
+    case CSSPropertyWebkitColumnGap:
+    case CSSPropertyWebkitColumnWidth:
     case CSSPropertyWidth:
         range = RangeNonNegative;
         // Fall through
