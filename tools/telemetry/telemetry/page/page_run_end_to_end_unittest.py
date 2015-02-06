fcb1a66017a75d3b865adca21971ff98b4ee401d
slamm <slamm@chromium.org>
2015-02-05 17:30:01 -0800

    Add end-to-end page run tests that exercise Web Page Replay.
    BUG=431686
    
    Review URL: https://codereview.chromium.org/902013002
    
    Cr-Commit-Position: refs/heads/master@{#314940}

--- tools/telemetry/telemetry/page/page_run_end_to_end_unittest.py
+++ tools/telemetry/telemetry/page/page_run_end_to_end_unittest.py
@@ -4,10 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 import os
-import tempfile
 import unittest
 import shutil
-import sys
 import tempfile
 
 from telemetry import benchmark
@@ -25,8 +23,7 @@ from telemetry.unittest_util import options_for_unittests
 from telemetry.unittest_util import system_stub
 from telemetry.user_story import user_story_runner
 from telemetry.util import exception_formatter as exception_formatter_module
-from telemetry.value import scalar
-from telemetry.value import string
+from unittest_data.page_sets import example_domain
 
 
 # pylint: disable=bad-super-call
@@ -117,7 +114,8 @@ class PageRunEndToEndTests(unittest.TestCase):
 
     class Test(page_test.PageTest):
       def __init__(self, *args):
-        super(Test, self).__init__(*args)
+        super(Test, self).__init__(
+            *args, needs_browser_restart_after_each_page=True)
         self.run_count = 0
 
       def RestartBrowserBeforeEachPage(self):
@@ -529,4 +527,27 @@ class PageRunEndToEndTests(unittest.TestCase):
     test = MultipleTabsTest()
     with self.assertRaises(page_test.MultiTabTestAppCrashError):
       self._RunPageTestThatRaisesAppCrashException(test, max_failures=1)
+
+  def testWebPageReplay(self):
+    ps = example_domain.ExampleDomainPageSet()
+    expectations = test_expectations.TestExpectations()
+    body = []
+    class TestWpr(page_test.PageTest):
+      def ValidateAndMeasurePage(self, _, tab, __):
+        body.append(tab.EvaluateJavaScript('document.body.innerText'))
+    test = TestWpr()
+    options = options_for_unittests.GetCopy()
+    options.output_formats = ['none']
+    options.suppress_gtest_report = True
+    SetUpUserStoryRunnerArguments(options)
+    results = results_options.CreateResults(EmptyMetadataForTest(), options)
+
+    user_story_runner.Run(test, ps, expectations, options, results)
+
+    self.longMessage = True
+    self.assertIn('Example Domain', body[0], msg='URL: %s' % ps.pages[0].url)
+    self.assertIn('Example Domain', body[1], msg='URL: %s' % ps.pages[1].url)
+
+    self.assertEquals(2, len(GetSuccessfulPageRuns(results)))
+    self.assertEquals(0, len(results.failures))
 
