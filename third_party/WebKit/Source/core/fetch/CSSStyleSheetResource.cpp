612cc326c04aba67bc52424fb6b40fa53231ff70
dpranke@chromium.org <dpranke@chromium.org>
2015-02-06 23:32:39 +0000

    Revert "CSS: Drop the quirks-mode exception for CSS MIME types."
    This is a speculative revert to see if it unblocks the Blnk roll;
    we're seeing various failures in PPAPI input event tests and
    sheriff-o-matic's build triangulation is pointing at this rev.
    
    TBR=mkwst@chromium.org
    BUG=419383
    
    Review URL: https://codereview.chromium.org/904143002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189711 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/fetch/CSSStyleSheetResource.cpp
+++ third_party/WebKit/Source/core/fetch/CSSStyleSheetResource.cpp
@@ -78,11 +78,11 @@ void CSSStyleSheetResource::didAddClient(ResourceClient* c)
         static_cast<StyleSheetResourceClient*>(c)->setCSSStyleSheet(m_resourceRequest.url(), m_response.url(), encoding(), this);
 }
 
-const String CSSStyleSheetResource::sheetText(bool* hasValidMIMEType) const
+const String CSSStyleSheetResource::sheetText(bool enforceMIMEType, bool* hasValidMIMEType) const
 {
     ASSERT(!isPurgeable());
 
-    if (!m_data || m_data->isEmpty() || !canUseSheet(hasValidMIMEType))
+    if (!m_data || m_data->isEmpty() || !canUseSheet(enforceMIMEType, hasValidMIMEType))
         return String();
 
     if (!m_decodedSheetText.isNull())
@@ -126,10 +126,13 @@ void CSSStyleSheetResource::destroyDecodedDataIfPossible()
     setDecodedSize(0);
 }
 
-bool CSSStyleSheetResource::canUseSheet(bool* hasValidMIMEType) const
+bool CSSStyleSheetResource::canUseSheet(bool enforceMIMEType, bool* hasValidMIMEType) const
 {
     if (errorOccurred())
         return false;
+
+    if (!enforceMIMEType && !hasValidMIMEType)
+        return true;
 
     // This check exactly matches Firefox. Note that we grab the Content-Type
     // header directly because we want to see what the value is BEFORE content
@@ -141,6 +144,8 @@ bool CSSStyleSheetResource::canUseSheet(bool* hasValidMIMEType) const
     bool typeOK = mimeType().isEmpty() || equalIgnoringCase(mimeType(), "text/css") || equalIgnoringCase(mimeType(), "application/x-unknown-content-type");
     if (hasValidMIMEType)
         *hasValidMIMEType = typeOK;
+    if (!enforceMIMEType)
+        return true;
     return typeOK;
 }
 
