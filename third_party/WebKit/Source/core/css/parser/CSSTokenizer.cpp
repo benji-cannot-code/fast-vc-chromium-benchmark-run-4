7f72b967dbafed953c7708eee3e4c28d221ea780
timloh@chromium.org <timloh@chromium.org>
2015-02-06 05:24:44 +0000

    CSS Tokenizer: Emit replacement characters as appropriate for escapes
    The css-syntax specification says to emit replacement characters for
    escapes of surrogate characters, null characters, and characters above
    the maximum input code point. This patch updates our tokenizer to match
    this behaviour.
    
    http://dev.w3.org/csswg/css-syntax/#consume-escaped-code-point
    
    Fixes fast/css/parsing-css-surrogate-pairs.html and makes our result
    correct for fast/css/string-quote-binary.html (existing expectation is
    wrong).
    
    BUG=424988
    
    Review URL: https://codereview.chromium.org/879313003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189614 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/parser/CSSTokenizer.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSTokenizer.cpp
@@ -638,7 +638,7 @@ String CSSTokenizer::consumeName()
 }
 
 // http://dev.w3.org/csswg/css-syntax/#consume-an-escaped-code-point
-UChar CSSTokenizer::consumeEscape()
+UChar32 CSSTokenizer::consumeEscape()
 {
     UChar cc = consume();
     ASSERT(!isNewLine(cc));
@@ -653,8 +653,9 @@ UChar CSSTokenizer::consumeEscape()
         };
         consumeSingleWhitespaceIfNext();
         bool ok = false;
-        UChar codePoint = hexChars.toString().toUIntStrict(&ok, 16);
-        if (!ok)
+        UChar32 codePoint = hexChars.toString().toUIntStrict(&ok, 16);
+        ASSERT(ok);
+        if (codePoint == 0 || (0xD800 <= codePoint && codePoint <= 0xDFFF) || codePoint > 0x10FFFF)
             return WTF::Unicode::replacementCharacter;
         return codePoint;
     }
--- third_party/WebKit/Source/core/css/parser/CSSTokenizer.h
+++ third_party/WebKit/Source/core/css/parser/CSSTokenizer.h
@@ -44,7 +44,7 @@ private:
 
     bool consumeIfNext(UChar);
     String consumeName();
-    UChar consumeEscape();
+    UChar32 consumeEscape();
 
     bool nextTwoCharsAreValidEscape();
     bool nextCharsAreNumber(UChar);
