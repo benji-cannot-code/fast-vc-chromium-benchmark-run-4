f5bbd70e457cfcfb239877dd52ffe8ca49116ead
dcheng <dcheng@chromium.org>
2015-02-05 19:34:00 -0800

    Update {virtual,override,final} to follow C++11 style in chrome.
    The Google style guide states that only one of {virtual,override,final}
    should be used for each declaration, since override implies virtual and
    final implies both virtual and override.
    
    This patch was automatically generated with an OS=android build using a
    variation of https://codereview.chromium.org/598073004.
    
    BUG=417463
    
    Review URL: https://codereview.chromium.org/900183004
    
    Cr-Commit-Position: refs/heads/master@{#314953}

--- chrome/android/shell/chrome_main_delegate_chrome_shell_android.h
+++ chrome/android/shell/chrome_main_delegate_chrome_shell_android.h
@@ -11,9 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class ChromeMainDelegateChromeShellAndroid : public ChromeMainDelegateAndroid {
  public:
   ChromeMainDelegateChromeShellAndroid();
-  virtual ~ChromeMainDelegateChromeShellAndroid();
+  ~ChromeMainDelegateChromeShellAndroid() override;
 
-  virtual bool BasicStartupComplete(int* exit_code) override;
+  bool BasicStartupComplete(int* exit_code) override;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(ChromeMainDelegateChromeShellAndroid);
