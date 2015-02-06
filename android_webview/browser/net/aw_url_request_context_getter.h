500a6fbbafe02bcf5db90e52a310f72cf84fd23a
bengr <bengr@chromium.org>
2015-02-06 13:59:55 -0800

    Hook SetKeyOnIO in Webview to DataReductionProxyIOData
    AwURLRequestContextGetter::SetKeyOnIO now calls into the
    DataReductionProxyAuthRequestHandler that is managed by
    DataReductionProxyIOData.
    
    BUG=453519
    
    Review URL: https://codereview.chromium.org/881893004
    
    Cr-Commit-Position: refs/heads/master@{#315119}

--- android_webview/browser/net/aw_url_request_context_getter.h
+++ android_webview/browser/net/aw_url_request_context_getter.h
@@ -24,10 +24,6 @@ class URLRequestContext;
 class URLRequestJobFactory;
 }
 
-namespace data_reduction_proxy {
-class DataReductionProxyAuthRequestHandler;
-}
-
 namespace android_webview {
 
 class AwNetworkDelegate;
@@ -43,9 +39,6 @@ class AwURLRequestContextGetter : public net::URLRequestContextGetter {
   net::URLRequestContext* GetURLRequestContext() override;
   scoped_refptr<base::SingleThreadTaskRunner> GetNetworkTaskRunner()
       const override;
-
-  data_reduction_proxy::DataReductionProxyAuthRequestHandler*
-      GetDataReductionProxyAuthRequestHandler() const;
 
   // NetLog is thread-safe, so clients can call this method from arbitrary
   // threads (UI and IO).
@@ -76,8 +69,6 @@ class AwURLRequestContextGetter : public net::URLRequestContextGetter {
   scoped_ptr<net::NetLog> net_log_;
   scoped_ptr<net::URLRequestContext> url_request_context_;
   scoped_ptr<net::ProxyConfigService> proxy_config_service_;
-  scoped_ptr<data_reduction_proxy::DataReductionProxyAuthRequestHandler>
-      data_reduction_proxy_auth_request_handler_;
   scoped_ptr<net::URLRequestJobFactory> job_factory_;
   scoped_ptr<net::HttpTransactionFactory> main_http_factory_;
 
