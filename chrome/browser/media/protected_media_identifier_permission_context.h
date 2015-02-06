1809f3185e283acf34ec8e73bb4241f206f4f6c0
xhwang <xhwang@chromium.org>
2015-02-05 17:04:33 -0800

    media: Support simultaneous permission requests from multiple WebContents in ProtectedMediaIdentifierPermissionContext.
    ProtectedMediaIdentifierPermissionContext is shared by multiple WebContents.
    Even though we don't support multiple permission requests from the same page
    (WebContents), we should support permission requests from multiple WebContents.
    
    Test step:
    1. Make sure no allowed or blocked exception in content settings.
    2. Open a tab/extension, navigate to a page that requests permission. A prompt
    should appear. Don't do anything.
    3. Open another tab/extension, navigate to a page that requests permission. A
    prompt should also appear.
    4. Play with allow/block/dismiss and see whether permission status is as
    expected and whether content settings are updated correctly.
    
    BUG=446263
    TEST=See above.
    
    Review URL: https://codereview.chromium.org/903873002
    
    Cr-Commit-Position: refs/heads/master@{#314933}

--- chrome/browser/media/protected_media_identifier_permission_context.h
+++ chrome/browser/media/protected_media_identifier_permission_context.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/common/permission_request_id.h"
 
 #if defined(OS_CHROMEOS)
+#include <map>
+
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/chromeos/attestation/platform_verification_dialog.h"
 #include "chrome/browser/chromeos/attestation/platform_verification_flow.h"
@@ -59,6 +61,7 @@ class ProtectedMediaIdentifierPermissionContext
 
 #if defined(OS_CHROMEOS)
   void OnPlatformVerificationResult(
+      content::WebContents* web_contents,
       const PermissionRequestID& id,
       const GURL& requesting_origin,
       const GURL& embedding_origin,
@@ -66,11 +69,13 @@ class ProtectedMediaIdentifierPermissionContext
       chromeos::attestation::PlatformVerificationFlow::ConsentResponse
           response);
 
-  // ID for the pending permission request. Invalid when no request is pending,
-  // or the request has been canceled.
-  PermissionRequestID pending_id_;
-
-  views::Widget* widget_;
+  // |this| is shared among multiple WebContents, so we could receive multiple
+  // permission requests. This map tracks all pending requests. Note that we
+  // only allow one request per WebContents.
+  typedef std::map<content::WebContents*,
+                   std::pair<views::Widget*, PermissionRequestID>>
+      PendingRequestMap;
+  PendingRequestMap pending_requests_;
 
   // Must be the last member, to ensure that it will be
   // destroyed first, which will invalidate weak pointers
