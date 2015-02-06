4ddecc50f1da981f7d634726da7f68f6482e8cc8
sammc <sammc@chromium.org>
2015-02-05 22:46:28 -0800

    Add handle waiting to StashBackend.
    This change adds support for monitoring stashed handles for being
    readable. This will be used to relaunch suspended extension processes
    when a persistent service sends a message or data to its client in the
    suspended extension process.
    
    BUG=389016
    
    Review URL: https://codereview.chromium.org/648853007
    
    Cr-Commit-Position: refs/heads/master@{#314989}

--- extensions/browser/mojo/stash_backend.h
+++ extensions/browser/mojo/stash_backend.h
@@ -6,9 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef EXTENSIONS_BROWSER_MOJO_STASH_BACKEND_H_
 #define EXTENSIONS_BROWSER_MOJO_STASH_BACKEND_H_
 
-#include <vector>
-
-#include "base/memory/linked_ptr.h"
+#include "base/callback.h"
+#include "base/memory/scoped_vector.h"
 #include "base/memory/weak_ptr.h"
 #include "extensions/common/mojo/stash.mojom.h"
 #include "third_party/mojo/src/mojo/public/cpp/bindings/interface_request.h"
@@ -18,7 +17,7 @@ namespace extensions {
 // A backend that provides access to StashService for a single extension.
 class StashBackend {
  public:
-  StashBackend();
+  explicit StashBackend(const base::Closure& on_handle_readable);
   ~StashBackend();
 
   // Creates a StashService that forwards calls to this StashBackend and bind it
@@ -33,8 +32,19 @@ class StashBackend {
   mojo::Array<StashedObjectPtr> RetrieveStash();
 
  private:
+  class StashEntry;
+
+  // Invoked when a handle is readable.
+  void OnHandleReady();
+
   // The objects that have been stashed.
-  mojo::Array<StashedObjectPtr> stashed_objects_;
+  ScopedVector<StashEntry> stashed_objects_;
+
+  // The callback to call when a handle is readable.
+  const base::Closure on_handle_readable_;
+
+  // Whether a handle has become readable since the last RetrieveStash() call.
+  bool has_notified_;
 
   base::WeakPtrFactory<StashBackend> weak_factory_;
 
--- extensions/extensions_tests.gyp
+++ extensions/extensions_tests.gyp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         '../third_party/mojo/mojo_edk.gyp:mojo_js_lib',
         '../third_party/mojo/mojo_edk.gyp:mojo_system_impl',
         '../third_party/mojo/mojo_public.gyp:mojo_cpp_bindings',
+        '../third_party/mojo/mojo_public.gyp:mojo_application_bindings',
         'common/api/api.gyp:cast_channel_proto',
         'extensions.gyp:extensions_common',
         'extensions.gyp:extensions_renderer',
