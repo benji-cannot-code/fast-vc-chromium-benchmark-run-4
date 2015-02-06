81ed10b97668c7d66a806cf13d380c87ee2f1a55
pavely <pavely@chromium.org>
2015-02-06 12:52:47 -0800

    Unregister GCMInvalidationBridge from GCMDriver when user signs out.
    Currently when user signs out GCMInvalidationBridge stays registered
    with GCMDriver preventing it from shutting down.
    GCMInvalidationBridge needs to unregister when user signs out.
    
    BUG=446349
    R=jianli@chromium.org
    
    Review URL: https://codereview.chromium.org/899353002
    
    Cr-Commit-Position: refs/heads/master@{#315103}

--- components/invalidation/gcm_invalidation_bridge.cc
+++ components/invalidation/gcm_invalidation_bridge.cc
@@ -281,6 +281,23 @@ void GCMInvalidationBridge::RegisterFinished(
                  result));
 }
 
+void GCMInvalidationBridge::Unregister() {
+  DCHECK(CalledOnValidThread());
+  // No-op if GCMClient is disabled.
+  if (gcm_driver_ == NULL)
+    return;
+
+  gcm_driver_->Unregister(
+      kInvalidationsAppId,
+      base::Bind(&GCMInvalidationBridge::UnregisterFinishedNoOp));
+}
+
+// static
+void GCMInvalidationBridge::UnregisterFinishedNoOp(
+    gcm::GCMClient::Result result) {
+  // No-op.
+}
+
 void GCMInvalidationBridge::SubscribeForIncomingMessages() {
   // No-op if GCMClient is disabled.
   if (gcm_driver_ == NULL)
@@ -357,6 +374,5 @@ void GCMInvalidationBridge::OnDisconnected() {
                  core_,
                  false));
 }
-
 
 }  // namespace invalidation
