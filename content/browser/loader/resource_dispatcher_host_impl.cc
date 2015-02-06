8b9bd833f79b2fe23d59bf6709a58f53fa1c71bb
pkasting <pkasting@chromium.org>
2015-02-05 19:49:16 -0800

    Adding instrumentation to locate the source of jankiness.
    Mechanical change that adds instrumentation required to locate the source of
    jankiness (i.e. a long-running fragment of code executed as a part of the task
    that causes jank) in the code. See the bug for details on what kind of jank we
    are after.  A number of similar CLs were landed, and none of them caused issues.
    The code of the instrumentation is highly optimized and is not expected to
    affect performance. The code simply creates a diagnostic task which is identical
    to ones created by PostTask or IPC message handlers.
    
    BUG=455884,455942,455952
    TEST=none
    TBR=mmenke,michaeln,zea
    
    Review URL: https://codereview.chromium.org/900793009
    
    Cr-Commit-Position: refs/heads/master@{#314970}

--- content/browser/loader/resource_dispatcher_host_impl.cc
+++ content/browser/loader/resource_dispatcher_host_impl.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_loop.h"
 #include "base/metrics/histogram.h"
 #include "base/metrics/sparse_histogram.h"
+#include "base/profiler/scoped_tracker.h"
 #include "base/stl_util.h"
 #include "base/third_party/dynamic_annotations/dynamic_annotations.h"
 #include "content/browser/appcache/appcache_interceptor.h"
@@ -2152,6 +2153,11 @@ void LoadInfoUpdateCallback(const LoadInfoMap& info_map) {
 }  // namespace
 
 void ResourceDispatcherHostImpl::UpdateLoadStates() {
+  // TODO(pkasting): Remove ScopedTracker below once crbug.com/455952 is
+  // fixed.
+  tracked_objects::ScopedTracker tracking_profile(
+      FROM_HERE_WITH_EXPLICIT_FUNCTION(
+          "455952 ResourceDispatcherHostImpl::UpdateLoadStates"));
   // Populate this map with load state changes, and then send them on to the UI
   // thread where they can be passed along to the respective RVHs.
   LoadInfoMap info_map;
--- net/http/http_stream_factory_impl_job.cc
+++ net/http/http_stream_factory_impl_job.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/logging.h"
+#include "base/profiler/scoped_tracker.h"
 #include "base/stl_util.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
@@ -437,6 +438,10 @@ int HttpStreamFactoryImpl::Job::OnHostResolution(
 }
 
 void HttpStreamFactoryImpl::Job::OnIOComplete(int result) {
+  // TODO(pkasting): Remove ScopedTracker below once crbug.com/455884 is fixed.
+  tracked_objects::ScopedTracker tracking_profile(
+      FROM_HERE_WITH_EXPLICIT_FUNCTION(
+          "455884 HttpStreamFactoryImpl::Job::OnIOComplete"));
   RunLoop(result);
 }
 
