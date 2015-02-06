c773b8e64ce994bf87cc082d7b44614b458cdb75
jrummell@chromium.org <jrummell@chromium.org>
2015-02-06 03:53:04 +0000

    Check InitDataType is a registered Initialization Data Type
    Code to verify whether a key system supports a particular InitDataType
    is best done on the Chromium side. Replace the existing check with one
    that simply verifies InitDataType is a valid registered value.
    
    BUG=385874
    TEST=updated test passes
    
    Review URL: https://codereview.chromium.org/898153002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189609 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/encryptedmedia/MediaKeySession.cpp
+++ third_party/WebKit/Source/modules/encryptedmedia/MediaKeySession.cpp
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/ContentDecryptionModuleResult.h"
 #include "platform/ContentType.h"
 #include "platform/Logging.h"
-#include "platform/MIMETypeRegistry.h"
 #include "platform/Timer.h"
 #include "public/platform/WebContentDecryptionModule.h"
 #include "public/platform/WebContentDecryptionModuleException.h"
@@ -77,29 +76,6 @@ enum {
 
 namespace blink {
 
-static bool isKeySystemSupportedWithInitDataType(const String& keySystem, const String& initDataType)
-{
-    ASSERT(!keySystem.isEmpty());
-
-    // FIXME: Replace the isSupportedEncryptedMediaMIMEType() call with an
-    // explicit initDataType check. For now, we must convert an explicit set of
-    // initDataTypes to the correct MIME type. http://crbug.com/385874.
-    String contentType;
-    if (initDataType == "webm") {
-        contentType = "video/webm";
-    } else if (initDataType == "cenc") {
-        contentType = "video/mp4";
-    } else if (initDataType == "keyids") {
-        contentType = initDataType; // This will fail.
-    } else {
-        // Until the call below correctly handles initDataTypes, we must reject
-        // everything else, including the MIME types it accepts.
-        return false;
-    }
-
-    return MIMETypeRegistry::isSupportedEncryptedMediaMIMEType(keySystem, contentType, "");
-}
-
 // Checks that |sessionId| looks correct and returns whether all checks pass.
 static bool isValidSessionId(const String& sessionId)
 {
@@ -116,6 +92,13 @@ static bool isValidSessionId(const String& sessionId)
     }
 
     return true;
+}
+
+// Checks that |initDataType| is a registered Initialization Data Type.
+static bool isRegisteredInitDataType(const String& initDataType)
+{
+    // List from https://w3c.github.io/encrypted-media/initdata-format-registry.html
+    return initDataType == "cenc" || initDataType == "keyids" || initDataType == "webm";
 }
 
 static String ConvertKeyStatusToString(const WebEncryptedMediaKeyInformation::KeyStatus status)
@@ -457,9 +440,12 @@ ScriptPromise MediaKeySession::generateRequest(ScriptState* scriptState, const S
     //    Initialization Data Type, return a promise rejected with a new
     //    DOMException whose name is NotSupportedError. String comparison
     //    is case-sensitive.
-    if (!isKeySystemSupportedWithInitDataType(m_keySystem, initDataType)) {
+    //    (blink side doesn't know what the CDM supports, so the proper check
+    //     will be done on the Chromium side. However, we can verify that
+    //     |initDataType| is one of the registered values.)
+    if (!isRegisteredInitDataType(initDataType)) {
         return ScriptPromise::rejectWithDOMException(
-            scriptState, DOMException::create(NotSupportedError, "The initialization data type '" + initDataType + "' is not supported by the key system."));
+            scriptState, DOMException::create(NotSupportedError, "The initialization data type '" + initDataType + "' is not a registered Initialization Data Type."));
     }
 
     // 6. Let init data be a copy of the contents of the initData parameter.
