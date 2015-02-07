5ddb0aadb88ec6fad673918a800f87e71f5f1f66
lof84 <lof84@yandex-team.ru>
2015-02-06 17:32:35 -0800

    Fix double lock of texture resource in software renderer
    If in software rendering mode, a copy request
    of the layer is not in the root or first render pass,
    there will be double locking resource crash.
    Now we first release old lock and then try to
    obtain lock again.
    
    R=danakj@chromium.org
    
    TEST=SoftwareRendererTest.DoubleLockTextureCrash
    
    Review URL: https://codereview.chromium.org/899183003
    
    Cr-Commit-Position: refs/heads/master@{#315155}

--- cc/output/gl_renderer.cc
+++ cc/output/gl_renderer.cc
@@ -2713,6 +2713,8 @@ bool GLRenderer::BindFramebufferToTexture(DrawingFrame* frame,
                                           const gfx::Rect& target_rect) {
   DCHECK(texture->id());
 
+  // Explicitly release lock, otherwise we can crash when try to lock
+  // same texture again.
   current_framebuffer_lock_ = nullptr;
 
   SetStencilEnabled(false);
