3fe41cc82aa571e97f25ffd167d2a052ff3cd6df
lushnikov@chromium.org <lushnikov@chromium.org>
2015-02-07 01:33:01 +0000

    DevTools: [Console] fix console "search/SaveAs" behaviors
    Console messages might have "Style" tags inside their DOM structure.
    We should anticipate this and do not treat style/script nodes as
    text nodes.
    
    BUG=456089
    R=sergeyv,apavlov
    
    Review URL: https://codereview.chromium.org/899143004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189729 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/console/ConsoleViewMessage.js
+++ third_party/WebKit/Source/devtools/front_end/console/ConsoleViewMessage.js
@@ -256,7 +256,7 @@ WebInspector.ConsoleViewMessage.prototype = {
     _formattedMessageText: function()
     {
         this.formattedMessage();
-        return this._messageElement.textContent;
+        return this._messageElement.deepTextContent();
     },
 
     /**
@@ -994,19 +994,6 @@ WebInspector.ConsoleViewMessage.prototype = {
         WebInspector.removeSearchResultsHighlight(this._formattedMessage, WebInspector.highlightedSearchResultClassName);
     },
 
-    _highlightSearchResultsInElement: function(regexObject, element)
-    {
-        regexObject.lastIndex = 0;
-        var text = element.textContent;
-        var match = regexObject.exec(text);
-        var matchRanges = [];
-        while (match) {
-            matchRanges.push(new WebInspector.SourceRange(match.index, match[0].length));
-            match = regexObject.exec(text);
-        }
-        WebInspector.highlightSearchResults(element, matchRanges);
-    },
-
     /**
      * @return {boolean}
      */
@@ -1317,7 +1304,7 @@ WebInspector.ConsoleViewMessage.prototype = {
     {
         if (!this._messageElement)
             return "";
-        return this._messageElement.textContent;
+        return this._messageElement.deepTextContent();
     },
 
     /**
