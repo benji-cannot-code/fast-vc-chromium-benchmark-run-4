fc81218ce2cd7c140fc335caea03203288b27c19
aelias@chromium.org <aelias@chromium.org>
2015-02-07 05:14:34 +0000

    Normalize top controls offset to (0, 1), Blink-side.
    Since top controls height can now be changed, storing top controls
    offset as an absolute value causes problems.  Normalizing its stored
    format to (0, 1) means we we don't need to adjust it when the height
    changes.
    
    On the Blink side, Blink now needs to hear about the top controls
    height, not just the "layout height" (effect on WebViewImpl size, which
    can be 0), so that it can apply the absolute value to FrameView.
    
    The new boolean m_topControlsOffsetIsNormalized is just a temporary hack
    to allow this patch to be a no-op until the Chromium-side lands.  I'll
    delete it in the same patch as WebWidget::setTopControlsLayoutHeight().
    
    See also Chromium-side patch: https://codereview.chromium.org/901813002
    
    BUG=430635
    
    Review URL: https://codereview.chromium.org/882683003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189737 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/web/WebWidget.h
+++ third_party/WebKit/public/web/WebWidget.h
@@ -139,7 +139,7 @@ public:
     virtual void applyViewportDeltas(
         const WebSize& scrollDelta,
         float scaleFactor,
-        float topControlsDelta) { }
+        float topControlsShownRatioDelta) { }
 
     // Applies viewport related properties during a commit from the compositor
     // thread.
@@ -150,7 +150,7 @@ public:
         const WebSize& mainFrameDelta,
         const WebFloatSize& elasticOverscrollDelta,
         float scaleFactor,
-        float topControlsDelta) { }
+        float topControlsShownRatioDelta) { }
 
     // Applies viewport related properties during a commit from the compositor
     // thread.
@@ -159,7 +159,7 @@ public:
         const WebFloatSize& mainFrameDelta,
         const WebFloatSize& elasticOverscrollDelta,
         float scaleFactor,
-        float topControlsDelta) { }
+        float topControlsShownRatioDelta) { }
 
     // Called to inform the WebWidget that mouse capture was lost.
     virtual void mouseCaptureLost() { }
@@ -272,8 +272,12 @@ public:
     // but not the select popup.
     virtual WebPagePopup* pagePopup() const { return 0; }
 
-    // Sets the height subtracted from the Widget to accomodate the top controls.
+    // TODO(aelias): Delete this after Blink roll.
     virtual void setTopControlsLayoutHeight(float) { }
+
+    // Notification about the top controls height.  If the boolean is true, then
+    // the embedder shrunk the WebView size by the top controls height.
+    virtual void setTopControlsHeight(float height, bool topControlsShrinkLayoutSize) { }
 
 protected:
     ~WebWidget() { }
