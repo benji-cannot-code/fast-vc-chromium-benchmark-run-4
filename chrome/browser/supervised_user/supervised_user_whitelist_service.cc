2e2bcc603ba046d87449ac0d1600177b9ab1958c
bauerb <bauerb@chromium.org>
2015-02-08 07:42:10 -0800

    Add metrics for supervised user whitelist installation and uninstallation, and absolute count.
    BUG=436459
    
    Review URL: https://codereview.chromium.org/905763002
    
    Cr-Commit-Position: refs/heads/master@{#315235}

--- chrome/browser/supervised_user/supervised_user_whitelist_service.cc
+++ chrome/browser/supervised_user/supervised_user_whitelist_service.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/files/file_path.h"
 #include "base/metrics/histogram.h"
+#include "base/metrics/user_metrics.h"
+#include "base/metrics/user_metrics_action.h"
 #include "base/prefs/pref_service.h"
 #include "base/prefs/scoped_user_pref_update.h"
 #include "base/strings/string_split.h"
@@ -62,6 +64,7 @@ void SupervisedUserWhitelistService::Init() {
     bool new_installation = false;
     RegisterWhitelist(it.key(), name, new_installation);
   }
+  UMA_HISTOGRAM_COUNTS_100("ManagedUsers.Whitelist.Count", whitelists->size());
 
   // Register whitelists specified on the command line.
   base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
@@ -258,6 +261,8 @@ syncer::SyncError SupervisedUserWhitelistService::ProcessSyncChanges(
 void SupervisedUserWhitelistService::AddNewWhitelist(
     base::DictionaryValue* pref_dict,
     const sync_pb::ManagedUserWhitelistSpecifics& whitelist) {
+  base::RecordAction(base::UserMetricsAction("ManagedUsers_Whitelist_Added"));
+
   bool new_installation = true;
   RegisterWhitelist(whitelist.id(), whitelist.name(), new_installation);
   scoped_ptr<base::DictionaryValue> dict(new base::DictionaryValue);
@@ -274,6 +279,8 @@ void SupervisedUserWhitelistService::SetWhitelistProperties(
 void SupervisedUserWhitelistService::RemoveWhitelist(
     base::DictionaryValue* pref_dict,
     const std::string& id) {
+  base::RecordAction(base::UserMetricsAction("ManagedUsers_Whitelist_Removed"));
+
   pref_dict->RemoveWithoutPathExpansion(id, NULL);
   installer_->UnregisterWhitelist(id);
   UnloadWhitelist(id);
