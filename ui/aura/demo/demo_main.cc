64c67d147e9b1e562a82f9bedbaab1b4bcdbc3a5
ckocagil <ckocagil@chromium.org>
2015-02-09 09:05:43 -0800

    Create PowerMonitor while starting aura_demo and views_examples_exe
    R=sky
    
    Review URL: https://codereview.chromium.org/910453002
    
    Cr-Commit-Position: refs/heads/master@{#315325}

--- ui/aura/demo/demo_main.cc
+++ ui/aura/demo/demo_main.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/icu_util.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/message_loop/message_loop.h"
+#include "base/power_monitor/power_monitor.h"
+#include "base/power_monitor/power_monitor_device_source.h"
 #include "third_party/skia/include/core/SkXfermode.h"
 #include "ui/aura/client/default_capture_client.h"
 #include "ui/aura/client/window_tree_client.h"
@@ -126,6 +128,9 @@ int DemoMain() {
 
   // Create the message-loop here before creating the root window.
   base::MessageLoopForUI message_loop;
+
+  base::PowerMonitor power_monitor(make_scoped_ptr(
+      new base::PowerMonitorDeviceSource));
 
   aura::Env::CreateInstance(true);
   aura::Env::GetInstance()->set_context_factory(context_factory.get());
