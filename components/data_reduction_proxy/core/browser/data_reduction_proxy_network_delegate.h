442625499d9799a2f21325deaebcb79b2b7ee5fd
jiayl <jiayl@chromium.org>
2015-02-09 15:22:37 -0800

    Revert of DataReductionProxyStatisticsPrefs should support WeakPtr (patchset #10 id:450001 of https://codereview.chromium.org/888713002/)
    Reason for revert:
    Suspected to cause compile failure:
    
    http://build.chromium.org/p/chromium.mac/builders/iOS%20Device/builds/23577/steps/compile/logs/stdio
    
    Original issue's description:
    > DataReductionProxyStatisticsPrefs should support WeakPtr
    >
    > The DRPStatisticsPrefs object is referenced via a raw pointer in
    > DRPNetworkDelegate and DRPSettings. It should instead be referenced
    > via WeakPtr.
    >
    > BUG=453155,455559,444939
    >
    > Committed: https://crrev.com/149bd770d674e8125f81cd649a8deece3a851682
    > Cr-Commit-Position: refs/heads/master@{#315419}
    
    TBR=bengr@chromium.org,sgurun@chromium.org,mmenke@chromium.org,megjablon@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=453155,455559,444939
    
    Review URL: https://codereview.chromium.org/896713003
    
    Cr-Commit-Position: refs/heads/master@{#315424}

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_network_delegate.h
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_network_delegate.h
@@ -7,10 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_DATA_REDUCTION_PROXY_CORE_BROWSER_DATA_REDUCTION_PROXY_NETWORK_DELEGATE_H_
 
 #include "base/basictypes.h"
-#include "base/gtest_prod_util.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
-#include "base/memory/weak_ptr.h"
 #include "base/values.h"
 #include "components/data_reduction_proxy/core/browser/data_reduction_proxy_metrics.h"
 #include "net/base/layered_network_delegate.h"
@@ -71,7 +69,7 @@ class DataReductionProxyNetworkDelegate : public net::LayeredNetworkDelegate {
   // report UMA.
   void InitStatisticsPrefsAndUMA(
       scoped_refptr<base::SingleThreadTaskRunner> ui_task_runner,
-      const base::WeakPtr<DataReductionProxyStatisticsPrefs>& statistics_prefs,
+      DataReductionProxyStatisticsPrefs* statistics_prefs,
       BooleanPrefMember* data_reduction_proxy_enabled,
       DataReductionProxyUsageStats* usage_stats);
 
@@ -86,8 +84,6 @@ class DataReductionProxyNetworkDelegate : public net::LayeredNetworkDelegate {
   base::Value* SessionNetworkStatsInfoToValue() const;
 
  private:
-  FRIEND_TEST_ALL_PREFIXES(DataReductionProxyNetworkDelegateTest, TotalLengths);
-
   // Called as the proxy is being resolved for |url|. Allows the delegate to
   // override the proxy resolution decision made by ProxyService. The delegate
   // may override the decision by modifying the ProxyInfo |result|.
@@ -124,13 +120,6 @@ class DataReductionProxyNetworkDelegate : public net::LayeredNetworkDelegate {
                                int64 original_content_length,
                                DataReductionProxyRequestType request_type);
 
-  // Records daily data savings statistics to prefs and reports data savings
-  // UMA.
-  void UpdateContentLengthPrefs(int received_content_length,
-                                int original_content_length,
-                                bool data_reduction_proxy_enabled,
-                                DataReductionProxyRequestType request_type);
-
   scoped_refptr<base::SingleThreadTaskRunner> ui_task_runner_;
 
   // Total size of all content (excluding headers) that has been received
@@ -152,8 +141,7 @@ class DataReductionProxyNetworkDelegate : public net::LayeredNetworkDelegate {
   DataReductionProxyAuthRequestHandler*
       data_reduction_proxy_auth_request_handler_;
 
-  base::WeakPtr<DataReductionProxyStatisticsPrefs>
-      data_reduction_proxy_statistics_prefs_;
+  DataReductionProxyStatisticsPrefs* data_reduction_proxy_statistics_prefs_;
 
   const DataReductionProxyConfigurator* configurator_;
 
