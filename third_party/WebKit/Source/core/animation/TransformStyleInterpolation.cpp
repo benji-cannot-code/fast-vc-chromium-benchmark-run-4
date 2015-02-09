83de1dec310b0ef3fe7abb909ff9493f574cbcd8
kouhei@chromium.org <kouhei@chromium.org>
2015-02-09 01:18:34 +0000

    Oilpan: Fix compile failure from r189762
    BUG=445292
    TBR=oilpan-reviews
    
    Review URL: https://codereview.chromium.org/909763002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189769 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/TransformStyleInterpolation.cpp
+++ third_party/WebKit/Source/core/animation/TransformStyleInterpolation.cpp
@@ -171,6 +171,7 @@ void TransformStyleInterpolation::apply(StyleResolverState& state) const
 
 void TransformStyleInterpolation::trace(Visitor* visitor)
 {
+    visitor->trace(m_types);
     StyleInterpolation::trace(visitor);
 }
 
