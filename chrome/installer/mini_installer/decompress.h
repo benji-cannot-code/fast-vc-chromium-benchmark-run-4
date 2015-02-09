6d9352b963d4f1fb554acdedc761391cf40b201a
qsr <qsr@chromium.org>
2015-02-09 05:52:38 -0800

    Fully qualify usage of ArraySizeHelper to prevent conflict.
    mojo redefines ArraySizeHelper in the mojo namespace. When arraysize is used
    in the mojo namespace, with both version in scope, the compiler, on android, is complaining that the reference is
    not unique. To prevent this, qualify the usage of ArraySizeHelper in
    base/macros.h
    
    Review URL: https://codereview.chromium.org/804023002
    
    Cr-Commit-Position: refs/heads/master@{#315290}

--- chrome/installer/mini_installer/decompress.h
+++ chrome/installer/mini_installer/decompress.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // arraysize borrowed from basictypes.h
 template <typename T, size_t N>
 char (&ArraySizeHelper(T (&array)[N]))[N];
-#define arraysize(array) (sizeof(ArraySizeHelper(array)))
+#define arraysize(array) (sizeof(::ArraySizeHelper(array)))
 
 namespace mini_installer {
 
