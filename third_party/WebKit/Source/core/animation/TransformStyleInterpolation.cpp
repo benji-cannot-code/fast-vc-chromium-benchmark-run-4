862da8f611d62d47fbd844b57cf83856775fe12a
kouhei@chromium.org <kouhei@chromium.org>
2015-02-09 01:30:09 +0000

    Revert of Oilpan: Fix compile failure from r189762 (patchset #1 id:1 of https://codereview.chromium.org/909763002/)
    Reason for revert:
    Broke android build
    
    Original issue's description:
    > Oilpan: Fix compile failure from r189762
    > 
    > BUG=445292
    > TBR=oilpan-reviews
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=189769
    
    TBR=oilpan-reviews@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=445292
    
    Review URL: https://codereview.chromium.org/880743003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189770 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/TransformStyleInterpolation.cpp
+++ third_party/WebKit/Source/core/animation/TransformStyleInterpolation.cpp
@@ -171,7 +171,6 @@ void TransformStyleInterpolation::apply(StyleResolverState& state) const
 
 void TransformStyleInterpolation::trace(Visitor* visitor)
 {
-    visitor->trace(m_types);
     StyleInterpolation::trace(visitor);
 }
 
