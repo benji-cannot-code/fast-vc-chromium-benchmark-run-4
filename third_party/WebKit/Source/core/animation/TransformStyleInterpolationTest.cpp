6716035db13106e6a7d6e7c2a8e302178a8d8f40
haraken@chromium.org <haraken@chromium.org>
2015-02-09 03:54:12 +0000

    Oilpan: One more build fix after r189762
    TBR=kouhei@chromium.org
    
    Review URL: https://codereview.chromium.org/909793002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189778 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/TransformStyleInterpolationTest.cpp
+++ third_party/WebKit/Source/core/animation/TransformStyleInterpolationTest.cpp
@@ -27,7 +27,7 @@ protected:
         return TransformStyleInterpolation::interpolableValueToTransform(value, types);
     }
 
-    static PassRefPtrWillBeRawPtr<CSSValue> roundTrip(CSSValue& value, HeapVector<CSSTransformValue::TransformOperationType> types)
+    static PassRefPtrWillBeRawPtr<CSSValue> roundTrip(CSSValue& value, Vector<CSSTransformValue::TransformOperationType> types)
     {
         return interpolableValueToTransform(transformToInterpolableValue(value).get(), types);
     }
@@ -132,7 +132,7 @@ TEST_F(AnimationTransformStyleInterpolationTest, ZeroTransform)
     transform->append(CSSPrimitiveValue::create(0, CSSPrimitiveValue::CSS_NUMBER));
     result->append(transform);
 
-    WillBeHeapVector<CSSTransformValue::TransformOperationType> types = WillBeHeapVector<CSSTransformValue::TransformOperationType>(19);
+    Vector<CSSTransformValue::TransformOperationType> types = Vector<CSSTransformValue::TransformOperationType>(19);
     types.append(CSSTransformValue::PerspectiveTransformOperation);
     types.append(CSSTransformValue::TranslateTransformOperation);
     types.append(CSSTransformValue::TranslateXTransformOperation);
@@ -260,7 +260,7 @@ TEST_F(AnimationTransformStyleInterpolationTest, SingleUnitTransform)
     transform->append(CSSPrimitiveValue::create(10, CSSPrimitiveValue::CSS_NUMBER));
     result->append(transform);
 
-    WillBeHeapVector<CSSTransformValue::TransformOperationType> types = WillBeHeapVector<CSSTransformValue::TransformOperationType>(19);
+    Vector<CSSTransformValue::TransformOperationType> types = Vector<CSSTransformValue::TransformOperationType>(19);
     types.append(CSSTransformValue::PerspectiveTransformOperation);
     types.append(CSSTransformValue::TranslateTransformOperation);
     types.append(CSSTransformValue::TranslateXTransformOperation);
