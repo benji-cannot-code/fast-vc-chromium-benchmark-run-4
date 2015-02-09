7f22fe7d3a2712db07dfdcac569e8769bd5deb14
sigbjornf@opera.com <sigbjornf@opera.com>
2015-02-09 10:25:43 +0000

    Reduce code duplication across sendBeacon() overloads.
    Rework the construction of the Beacon requests across the various supported
    payload types, avoiding duplication of type-independent boilerplate code.
    
    On linux64-release, reduces code size from 30k to 15k.
    
    R=mkwst
    BUG=
    
    Review URL: https://codereview.chromium.org/864163004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189790 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/BeaconLoader.h
+++ third_party/WebKit/Source/core/loader/BeaconLoader.h
@@ -32,8 +32,7 @@ public:
     static bool sendBeacon(LocalFrame*, int, const KURL&, PassRefPtrWillBeRawPtr<DOMFormData>, int&);
 
 private:
-    static void prepareRequest(LocalFrame*, ResourceRequest&);
-    static void issueRequest(LocalFrame*, ResourceRequest&);
+    class Sender;
 };
 
 } // namespace blink
