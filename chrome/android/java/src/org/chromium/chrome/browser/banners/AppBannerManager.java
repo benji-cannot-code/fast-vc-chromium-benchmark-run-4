79233035799c8a773f4e516bb56c77086dd94e89
dfalcantara <dfalcantara@chromium.org>
2015-02-09 14:15:17 -0800

    Don't fire error messages if an install kills Chrome
    Die silently if the Play Store starts installing an app
    promoted by the webpage and Chrome dies in the background
    before returning.  In this case, the infobar will disappear
    before the user gets back anyway.
    
    BUG=453170
    
    Review URL: https://codereview.chromium.org/913593002
    
    Cr-Commit-Position: refs/heads/master@{#315406}

--- chrome/android/java/src/org/chromium/chrome/browser/banners/AppBannerManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/banners/AppBannerManager.java
@@ -177,8 +177,7 @@ public class AppBannerManager extends EmptyTabObserver {
             // Try installing the app.  If the installation was kicked off, return false to prevent
             // the infobar from disappearing.
             return !mTab.getWindowAndroid().showIntent(
-                    appData.installIntent(), createIntentCallback(appData),
-                    R.string.low_memory_error);
+                    appData.installIntent(), createIntentCallback(appData), null);
         }
     }
 
@@ -216,16 +215,7 @@ public class AppBannerManager extends EmptyTabObserver {
 
     @CalledByNative
     private void showAppDetails(AppData appData) {
-        WindowAndroid.IntentCallback emptyCallback = new WindowAndroid.IntentCallback() {
-            @Override
-            public void onIntentCompleted(WindowAndroid window, int resultCode,
-                    ContentResolver contentResolver, Intent data) {
-                // Do nothing.
-            }
-        };
-
-        mTab.getWindowAndroid().showIntent(
-                appData.detailsIntent(), emptyCallback, R.string.low_memory_error);
+        mTab.getWindowAndroid().showIntent(appData.detailsIntent(), null, null);
     }
 
     @CalledByNative
