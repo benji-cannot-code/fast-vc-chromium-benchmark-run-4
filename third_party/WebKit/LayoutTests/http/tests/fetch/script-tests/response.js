5c223d5fce22bafba0f229f7d8138169eaac89ca
hiroshige@chromium.org <hiroshige@chromium.org>
2015-02-09 12:50:08 +0000

    [Fetch] Test invalid header names/values, method names, mode, and credentials more comprehensively
    BUG=444493, 454665
    
    Review URL: https://codereview.chromium.org/900473005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189793 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/fetch/script-tests/response.js
+++ third_party/WebKit/LayoutTests/http/tests/fetch/script-tests/response.js
@@ -202,9 +202,7 @@ test(function() {
           assert_false(response.ok, 'Response.ok must be false for ' + status);
       });
 
-    var invalidNames = ['', '(', ')', '<', '>', '@', ',', ';', ':', '\\', '"',
-                        '/', '[', ']', '?', '=', '{', '}', '\u3042', 'a(b'];
-    invalidNames.forEach(function(name) {
+    INVALID_HEADER_NAMES.forEach(function(name) {
         assert_throws(
           {name: 'TypeError'},
           function() {
@@ -222,8 +220,7 @@ test(function() {
           'new Response with headers with an invalid name (' + name +
           ') should throw');
       });
-    var invalidValues = ['test \r data', 'test \n data'];
-    invalidValues.forEach(function(value) {
+    INVALID_HEADER_VALUES.forEach(function(value) {
         assert_throws(
           {name: 'TypeError'},
           function() {
