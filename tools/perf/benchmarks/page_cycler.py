8312d8da0e9df9fce9db769f683dfda5dd110e2f
nednguyen <nednguyen@google.com>
2015-02-09 15:31:15 -0800

    Add key_mobile_sites_cycler.py that contains page sets that does not do scrolling.
    This is needed so that page_cycler's action_name_to_run's
    field can later be changed from empty string to
    'RunPageInteractions'.
    
    BUG=418375
    
    Review URL: https://codereview.chromium.org/912493003
    
    Cr-Commit-Position: refs/heads/master@{#315433}

--- tools/perf/benchmarks/page_cycler.py
+++ tools/perf/benchmarks/page_cycler.py
@@ -182,7 +182,7 @@ class PageCyclerTop10Mobile(_PageCycler):
 @benchmark.Disabled
 class PageCyclerKeyMobileSites(_PageCycler):
   """Page load time benchmark for key mobile sites."""
-  page_set = page_sets.KeyMobileSitesSmoothPageSet
+  page_set = page_sets.KeyMobileSitesCyclerPageSet
 
   @classmethod
   def Name(cls):
