61c0bdc51f4d5476cbd48e7857ab8c9b7362ad8a
dtseng <dtseng@chromium.org>
2015-02-09 11:24:07 -0800

    Reland #2: Ensure WebView notifies desktop automation on creation, destruction, and change
    Original issue: https://codereview.chromium.org/880063002
    Previous reland attempt: https://codereview.chromium.org/895623003/
    
    This cl changes the way in which ChromeVox initially verbalizes a page which led to flakeyness in
      chromevox_tests --gtest_filter=BackgroundTest.*
    
    To attempt and address the flakeyness,
    - chromevox_e2e_test_base.js and chromevox_next_e2e_test_base.js now explicitly separates listening to page load events from the chrome.automation tree and the chrome.tabs APIs.
    They are named: runWithLoadedTree, runWithLoadedTab, and runWithTab. The last method allows a caller to create a tab, but not wait for it to load (based on the tab status being 'complete').
    - each test in background_test now no longer waits for ChromeVox to speak 'start' (which formerly could have been triggered by a loadComplete automation event).
    - tab creation is still done via the tabs API, but listening for load is done via chrome.automation. (i.e. add a listener for an automation tree to load completely, then create a tab).
    
    It is not known at this time if this cl will continue to cause flakes in the above tests. Try bots and local runs yielded no flakeyness. The next step may be to introduce logging to identify the source of the flakeyness.
    
    TBR=tfarina
    
    Review URL: https://codereview.chromium.org/890013006
    
    Cr-Commit-Position: refs/heads/master@{#315362}

--- chrome/browser/resources/chromeos/chromevox/cvox2/background/output.js
+++ chrome/browser/resources/chromeos/chromevox/cvox2/background/output.js
@@ -14,6 +14,8 @@ goog.require('AutomationUtil.Dir');
 goog.require('cursors.Cursor');
 goog.require('cursors.Range');
 goog.require('cursors.Unit');
+goog.require('cvox.AbstractEarcons');
+goog.require('cvox.NavBraille');
 goog.require('cvox.Spannable');
 goog.require('cvox.ValueSelectionSpan');
 goog.require('cvox.ValueSpan');
