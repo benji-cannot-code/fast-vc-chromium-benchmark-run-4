1b3743b8baea4bd4ac18430f7f9972227798b870
rune@opera.com <rune@opera.com>
2015-02-10 18:35:15 +0000

    Check visited color for visual diff.
    The color which is actually used for painting needs to be checked when
    figuring out if we need to do a paint invalidation. We are checking both
    the color for :link and :visited so that the visited state is not exposed
    through repaint timing.
    
    This is a problem for other colors as well. If this approach is OK'ed,
    I'll go ahead and fix the other ones.
    
    BUG=439812
    
    Review URL: https://codereview.chromium.org/900443006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189918 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/style/LayoutStyle.cpp
+++ third_party/WebKit/Source/core/layout/style/LayoutStyle.cpp
@@ -697,6 +697,7 @@ void LayoutStyle::updatePropertySpecificDifferences(const LayoutStyle& other, St
 
     if (!diff.needsPaintInvalidation()) {
         if (inherited->color != other.inherited->color
+            || inherited->visitedLinkColor != other.inherited->visitedLinkColor
             || inherited_flags.m_textUnderline != other.inherited_flags.m_textUnderline
             || visual->textDecoration != other.visual->textDecoration) {
             diff.setTextOrColorChanged();
