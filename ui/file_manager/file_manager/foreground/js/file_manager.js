f252603cb248a286cb22340b9b891858f825ec0a
hirono <hirono@chromium.org>
2015-02-09 20:09:02 -0800

    Files.app: Use new metadata model in FileTable.
    BUG=410766
    TEST=None
    
    Review URL: https://codereview.chromium.org/904813002
    
    Cr-Commit-Position: refs/heads/master@{#315489}

--- ui/file_manager/file_manager/foreground/js/file_manager.js
+++ ui/file_manager/file_manager/foreground/js/file_manager.js
@@ -741,6 +741,7 @@ FileManager.prototype = /** @struct */ {
     assert(this.volumeManager_);
     assert(this.historyLoader_);
     assert(this.dialogDom_);
+    assert(this.fileSystemMetadata_);
 
     // Cache nodes we'll be manipulating.
     var dom = this.dialogDom_;
@@ -754,6 +755,7 @@ FileManager.prototype = /** @struct */ {
     FileTable.decorate(
         table,
         this.metadataCache_,
+        this.fileSystemMetadata_,
         this.volumeManager_,
         this.historyLoader_,
         this.dialogType == DialogType.FULL_PAGE);
