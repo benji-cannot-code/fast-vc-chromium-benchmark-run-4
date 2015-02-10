9e9556709f9d561f3c9b6d155b627ca22e004aa8
dyen <dyen@chromium.org>
2015-02-10 14:42:12 -0800

    Modified the page set names for trace tests.
    The page set names for trace_test and device_trace_test now all begin
    with "TraceTest" and "DeviceTraceTest" instead of "Pixel".
    
    Additionally, the Pixel.Canvas2DRedBox test has been re-enabled and
    disabled for TraceTest.Canvas2DRedBox only.
    
    R=kbr@chromium.org
    BUG=456928
    TESTS=trybots
    
    Review URL: https://codereview.chromium.org/912033002
    
    Cr-Commit-Position: refs/heads/master@{#315663}

--- content/test/gpu/gpu_tests/trace_test.py
+++ content/test/gpu/gpu_tests/trace_test.py
@@ -77,7 +77,7 @@ class _TraceTestBase(benchmark.Benchmark):
   """Base class for the trace tests."""
   def CreatePageSet(self, options):
     # Utilize pixel tests page set as a set of simple pages to load.
-    page_set = page_sets.PixelTestsPageSet()
+    page_set = page_sets.PixelTestsPageSet(base_name=self.name)
     for page in page_set.pages:
       page.script_to_evaluate_on_commit = test_harness_script
     return page_set
@@ -86,6 +86,7 @@ class _TraceTestBase(benchmark.Benchmark):
 class TraceTest(_TraceTestBase):
   """Tests GPU traces are plumbed through properly."""
   test = _TraceValidator
+  name = 'TraceTest'
 
   @classmethod
   def Name(cls):
@@ -98,6 +99,7 @@ class TraceTest(_TraceTestBase):
 class DeviceTraceTest(_TraceTestBase):
   """Tests GPU Device traces show up on devices that support it."""
   test = _DeviceTraceValidator
+  name = 'DeviceTraceTest'
 
   @classmethod
   def Name(cls):
