2bb1c7cb60f7e7132182795c14ff2d19588ae7ab
alph@chromium.org <alph@chromium.org>
2015-02-10 13:42:12 +0000

    DevTools: add call stack to EventDispatch event.
    Review URL: https://codereview.chromium.org/912773002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189900 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorTraceEvents.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorTraceEvents.cpp
@@ -659,6 +659,7 @@ PassRefPtr<TraceEvent::ConvertableToTraceFormat> InspectorEventDispatchEvent::da
 {
     RefPtr<TracedValue> value = TracedValue::create();
     value->setString("type", event.type());
+    setCallStack(value.get());
     return value.release();
 }
 
