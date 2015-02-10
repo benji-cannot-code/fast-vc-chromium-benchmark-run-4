e2971379fb4981cddfc8ecf6e57a6983629a99f1
jeremyim <jeremyim@chromium.org>
2015-02-10 11:18:15 -0800

    Perform ClientHello padding if the field trial is enabled for google.com and its subdomains.
    BUG=440443
    
    Review URL: https://codereview.chromium.org/869393005
    
    Cr-Commit-Position: refs/heads/master@{#315612}

--- chrome/browser/net/ssl_config_service_manager_pref.cc
+++ chrome/browser/net/ssl_config_service_manager_pref.cc
@@ -20,14 +20,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "components/content_settings/core/browser/content_settings_utils.h"
 #include "components/content_settings/core/common/content_settings.h"
+#include "components/google/core/browser/google_util.h"
 #include "content/public/browser/browser_thread.h"
 #include "net/socket/ssl_client_socket.h"
 #include "net/ssl/ssl_cipher_suite_names.h"
 #include "net/ssl/ssl_config_service.h"
+#include "url/gurl.h"
 
 using content::BrowserThread;
 
 namespace {
+
+// Field trial for ClientHello padding.
+const char kClientHelloFieldTrialName[] = "FastRadioPadding";
+const char kClientHelloFieldTrialEnabledGroupName[] = "Enabled";
 
 // Converts a ListValue of StringValues into a vector of strings. Any Values
 // which cannot be converted will be skipped.
@@ -97,6 +103,8 @@ class SSLConfigServicePref : public net::SSLConfigService {
   // Store SSL config settings in |config|. Must only be called from IO thread.
   void GetSSLConfig(net::SSLConfig* config) override;
 
+  bool SupportsFastradioPadding(const GURL& url) override;
+
  private:
   // Allow the pref watcher to update our internal state.
   friend class SSLConfigServiceManagerPref;
@@ -115,6 +123,11 @@ class SSLConfigServicePref : public net::SSLConfigService {
 
 void SSLConfigServicePref::GetSSLConfig(net::SSLConfig* config) {
   *config = cached_config_;
+}
+
+bool SSLConfigServicePref::SupportsFastradioPadding(const GURL& url) {
+  return google_util::IsGoogleHostname(url.host(),
+                                       google_util::ALLOW_SUBDOMAIN);
 }
 
 void SSLConfigServicePref::SetNewSSLConfig(
@@ -287,6 +300,12 @@ void SSLConfigServiceManagerPref::GetSSLConfigFromPrefs(
   config->disabled_cipher_suites = disabled_cipher_suites_;
   // disabling False Start also happens to disable record splitting.
   config->false_start_enabled = !ssl_record_splitting_disabled_.GetValue();
+
+  base::StringPiece group =
+      base::FieldTrialList::FindFullName(kClientHelloFieldTrialName);
+  if (group.starts_with(kClientHelloFieldTrialEnabledGroupName)) {
+    config->fastradio_padding_enabled = true;
+  }
 }
 
 void SSLConfigServiceManagerPref::OnDisabledCipherSuitesChange(
--- net/socket/ssl_client_socket_openssl.cc
+++ net/socket/ssl_client_socket_openssl.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/environment.h"
 #include "base/memory/singleton.h"
 #include "base/metrics/histogram.h"
-#include "base/metrics/sparse_histogram.h"
 #include "base/profiler/scoped_tracker.h"
 #include "base/strings/string_piece.h"
 #include "base/synchronization/lock.h"
@@ -474,12 +473,16 @@ int SSLClientSocketOpenSSL::Connect(const CompletionCallback& callback) {
   int rv = Init();
   if (rv != OK) {
     net_log_.EndEventWithNetErrorCode(NetLog::TYPE_SSL_CONNECT, rv);
-    UMA_HISTOGRAM_SPARSE_SLOWLY("Net.SSL_Connection_Error", std::abs(rv));
     return rv;
   }
 
   // Set SSL to client mode. Handshake happens in the loop below.
   SSL_set_connect_state(ssl_);
+
+  // Enable fastradio padding.
+  SSL_enable_fastradio_padding(ssl_,
+                               ssl_config_.fastradio_padding_enabled &&
+                                   ssl_config_.fastradio_padding_eligible);
 
   GotoState(STATE_HANDSHAKE);
   rv = DoHandshakeLoop(OK);
@@ -487,7 +490,6 @@ int SSLClientSocketOpenSSL::Connect(const CompletionCallback& callback) {
     user_connect_callback_ = callback;
   } else {
     net_log_.EndEventWithNetErrorCode(NetLog::TYPE_SSL_CONNECT, rv);
-    UMA_HISTOGRAM_SPARSE_SLOWLY("Net.SSL_Connection_Error", std::abs(rv));
     if (rv < OK)
       OnHandshakeCompletion();
   }
@@ -1367,7 +1369,6 @@ void SSLClientSocketOpenSSL::OnHandshakeIOComplete(int result) {
   int rv = DoHandshakeLoop(result);
   if (rv != ERR_IO_PENDING) {
     net_log_.EndEventWithNetErrorCode(NetLog::TYPE_SSL_CONNECT, rv);
-    UMA_HISTOGRAM_SPARSE_SLOWLY("Net.SSL_Connection_Error", std::abs(rv));
     DoConnectCallback(rv);
   }
 }
