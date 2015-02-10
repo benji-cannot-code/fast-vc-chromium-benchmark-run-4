61f307bfe327dcbc12e770981ab09d8df64216b5
rchlodnicki <rchlodnicki@opera.com>
2015-02-10 01:28:43 -0800

    Add missing include header
    Needed for the Profile::FromBrowserContext call. Happens to work due to
    some other includes that pull in profile.h header.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/910743003
    
    Cr-Commit-Position: refs/heads/master@{#315523}

--- chrome/browser/guest_view/web_view/chrome_web_view_guest_delegate.cc
+++ chrome/browser/guest_view/web_view/chrome_web_view_guest_delegate.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/chrome_extension_web_contents_observer.h"
 #include "chrome/browser/favicon/favicon_tab_helper.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/renderer_context_menu/render_view_context_menu.h"
 #include "chrome/browser/ui/pdf/chrome_pdf_web_contents_helper_client.h"
 #include "chrome/common/chrome_version_info.h"
