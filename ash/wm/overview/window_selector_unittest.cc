41204dd37db322589b67e6e2c4bd3897fc92c23e
nsatragno <nsatragno@chromium.org>
2015-02-10 10:35:47 -0800

    Modified OverviewMode's LabelButton bounds to cover the entire item.
    This way, we don't have to worry about custom targeting. A minor change
    had to be done to the LabelButton view code to avoid printing text on
    a view border.
    
    BUG=453441
    
    Review URL: https://codereview.chromium.org/872113004
    
    Cr-Commit-Position: refs/heads/master@{#315596}

--- ash/wm/overview/window_selector_unittest.cc
+++ ash/wm/overview/window_selector_unittest.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/test/test_shelf_delegate.h"
 #include "ash/wm/maximize_mode/maximize_mode_controller.h"
 #include "ash/wm/mru_window_tracker.h"
-#include "ash/wm/overview/overview_window_button.h"
 #include "ash/wm/overview/window_grid.h"
 #include "ash/wm/overview/window_selector.h"
 #include "ash/wm/overview/window_selector_controller.h"
@@ -217,8 +216,7 @@ class WindowSelectorTest : public test::AshTestBase {
   }
 
   views::LabelButton* GetLabelButtonView(ash::WindowSelectorItem* window) {
-    return static_cast<views::LabelButton*>(
-        window->overview_window_button_->window_label_->GetContentsView());
+    return window->window_label_button_view_;
   }
 
   // Tests that a window is contained within a given WindowSelectorItem, and
@@ -374,12 +372,13 @@ TEST_F(WindowSelectorTest, ClickOnWindowDuringTouch) {
                                            window1_bounds.CenterPoint());
 
   // Clicking on |window2| while touching on |window1| should not cause a
-  // crash, and |window2| should be selected.
+  // crash, overview mode should be disengaged and |window2| should be active.
   const int kTouchId = 19;
   event_generator.PressTouchId(kTouchId);
   event_generator.MoveMouseToCenterOf(window2.get());
   event_generator.ClickLeftButton();
   EXPECT_FALSE(IsSelecting());
+  EXPECT_TRUE(wm::IsActiveWindow(window2.get()));
   event_generator.ReleaseTouchId(kTouchId);
 
   ToggleOverview();
@@ -928,14 +927,8 @@ TEST_F(WindowSelectorTest, CreateLabelUnderWindow) {
 
   // Labels are located based on target_bounds, not the actual window item
   // bounds.
-  gfx::Rect target_bounds(window_item->target_bounds());
-  gfx::Rect expected_label_bounds(target_bounds.x(),
-                                  target_bounds.bottom() - label->
-                                      GetPreferredSize().height(),
-                                  target_bounds.width(),
-                                  label->GetPreferredSize().height());
-  gfx::Rect real_label_bounds = label->GetWidget()->GetNativeWindow()->bounds();
-  EXPECT_EQ(real_label_bounds, expected_label_bounds);
+  gfx::Rect label_bounds = label->GetWidget()->GetNativeWindow()->bounds();
+  EXPECT_EQ(label_bounds, window_item->target_bounds());
 }
 
 // Tests that overview updates the window positions if the display orientation
