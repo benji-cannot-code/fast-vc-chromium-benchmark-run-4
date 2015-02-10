b5eeb80ff2296bf67156cc208f77431e5346b38f
dfalcantara <dfalcantara@chromium.org>
2015-02-09 20:15:44 -0800

    Revert "Remove old references to AppBannerInfoBarDelegate JNI"
    This reverts commit cd40fb9daed725fd788783519d346636fc0eff38.
    
    Function name was being used in an alternate location.
    
    BUG=453170
    TBR=newt
    
    Review URL: https://codereview.chromium.org/896063004
    
    Cr-Commit-Position: refs/heads/master@{#315492}

--- chrome/browser/android/banners/app_banner_infobar_delegate.cc
+++ chrome/browser/android/banners/app_banner_infobar_delegate.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/android/banners/app_banner_infobar_delegate.h"
 
+#include "base/android/jni_android.h"
 #include "base/strings/string16.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/ui/android/infobars/app_banner_infobar.h"
