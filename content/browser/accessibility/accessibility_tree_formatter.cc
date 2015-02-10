078af5dec5b71e3d3c2825191b3dae211d80283e
nektar <nektar@chromium.org>
2015-02-10 13:05:46 -0800

    Worked around a trybot change by replacing spaces with "+" for the indentation in accessibility browser tests.
    R=dmazzoni
    
    Review URL: https://codereview.chromium.org/906573003
    
    Cr-Commit-Position: refs/heads/master@{#315635}

--- content/browser/accessibility/accessibility_tree_formatter.cc
+++ content/browser/accessibility/accessibility_tree_formatter.cc
@@ -18,7 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 namespace {
-const int kIndentSpaces = 4;
+const char kIndentSymbol = '+';
+const int kIndentSymbolCount = 2;
 const char* kSkipString = "@NO_DUMP";
 const char* kChildrenDictAttr = "children";
 }
@@ -77,7 +78,8 @@ void AccessibilityTreeFormatter::RecursiveBuildAccessibilityTree(
 
 void AccessibilityTreeFormatter::RecursiveFormatAccessibilityTree(
     const base::DictionaryValue& dict, base::string16* contents, int depth) {
-  base::string16 indent = base::string16(depth * kIndentSpaces, ' ');
+  base::string16 indent = base::string16(depth * kIndentSymbolCount,
+                                         kIndentSymbol);
   base::string16 line = indent + ToString(dict);
   if (line.find(base::ASCIIToUTF16(kSkipString)) != base::string16::npos)
     return;
