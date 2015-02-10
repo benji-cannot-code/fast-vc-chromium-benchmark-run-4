61b63033adc20262c6477de5e313f5e14b4d3876
kouhei@chromium.org <kouhei@chromium.org>
2015-02-10 06:17:18 +0000

    ServiceWorker/Oilpan: base::trace call should be placed at the end of trace method definition
    Follow up to r189863 as pointed out by haraken@
    https://codereview.chromium.org/894873005/
    
    BUG=455524
    
    Review URL: https://codereview.chromium.org/911033002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189874 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/serviceworkers/RespondWithObserver.cpp
+++ third_party/WebKit/Source/modules/serviceworkers/RespondWithObserver.cpp
@@ -58,9 +58,9 @@ public:
     }
     DEFINE_INLINE_TRACE()
     {
-        BodyStreamBuffer::Observer::trace(visitor);
         visitor->trace(m_buffer);
         visitor->trace(m_outStream);
+        BodyStreamBuffer::Observer::trace(visitor);
     }
     void start()
     {
