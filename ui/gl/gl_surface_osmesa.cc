34457b50f1159a77d4702fb8852ad0711909d8d6
mbarbella <mbarbella@chromium.org>
2015-02-10 10:30:06 -0800

    Check dimensions and allocation result in GLSurfaceOSMesa::Resize.
    BUG=425854
    R=kbr@chromium.org
    
    Review URL: https://codereview.chromium.org/907053003
    
    Cr-Commit-Position: refs/heads/master@{#315594}

--- ui/gl/gl_surface_osmesa.cc
+++ ui/gl/gl_surface_osmesa.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/logging.h"
+#include "base/numerics/safe_math.h"
 #include "third_party/mesa/src/include/GL/osmesa.h"
 #include "ui/gl/gl_bindings.h"
 #include "ui/gl/gl_context.h"
@@ -51,8 +52,17 @@ bool GLSurfaceOSMesa::Resize(const gfx::Size& new_size) {
   // Preserve the old buffer.
   scoped_ptr<int32[]> old_buffer(buffer_.release());
 
+  base::CheckedNumeric<int> checked_size = sizeof(buffer_[0]);
+  checked_size *= new_size.width();
+  checked_size *= new_size.height();
+  if (!checked_size.IsValid())
+    return false;
+
   // Allocate a new one.
   buffer_.reset(new int32[new_size.GetArea()]);
+  if (!buffer_.get())
+    return false;
+
   memset(buffer_.get(), 0, new_size.GetArea() * sizeof(buffer_[0]));
 
   // Copy the old back buffer into the new buffer.
