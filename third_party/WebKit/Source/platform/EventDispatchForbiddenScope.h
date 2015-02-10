b21642efaee34cc92b4abadf868c883c1f6fc77b
tasak@google.com <tasak@google.com>
2015-02-10 09:12:37 +0000

    EventDispatchForbiddenScope::s_count should not be defined in core.
    EventDispatchForbiddenScope is defined in platform. So s_count should be also
    defined in platform, not ContainerNode.cpp.
    
    BUG=358074
    
    Review URL: https://codereview.chromium.org/858383007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189879 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/EventDispatchForbiddenScope.h
+++ third_party/WebKit/Source/platform/EventDispatchForbiddenScope.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef EventDispatchForbiddenScope_h
 #define EventDispatchForbiddenScope_h
 
+#include "platform/PlatformExport.h"
+#include "wtf/Assertions.h"
 #include "wtf/MainThread.h"
 #include "wtf/TemporaryChange.h"
 
@@ -53,7 +55,7 @@ public:
     };
 
 private:
-    static unsigned s_count;
+    PLATFORM_EXPORT static unsigned s_count;
 };
 
 #else
