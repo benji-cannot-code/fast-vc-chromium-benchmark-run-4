4905223f00c2fc877d74f774a0b32eefe8cfc457
ericwilligers@chromium.org <ericwilligers@chromium.org>
2015-02-10 06:04:07 +0000

    Define type cast macros for CSS basic shapes
    Replace explicit static casts to CSSBasicShape types with macros.
    
    New predicates are defined: isEllipse(), isPolygon(), isCircle(), isShapeInset()
    
    BUG=443006
    
    Review URL: https://codereview.chromium.org/911503002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189872 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/CSSBasicShapes.cpp
+++ third_party/WebKit/Source/core/css/CSSBasicShapes.cpp
@@ -130,7 +130,7 @@ bool CSSBasicShapeCircle::equals(const CSSBasicShape& shape) const
     if (shape.type() != CSSBasicShapeCircleType)
         return false;
 
-    const CSSBasicShapeCircle& other = static_cast<const CSSBasicShapeCircle&>(shape);
+    const CSSBasicShapeCircle& other = toCSSBasicShapeCircle(shape);
     return compareCSSValuePtr(m_centerX, other.m_centerX)
         && compareCSSValuePtr(m_centerY, other.m_centerY)
         && compareCSSValuePtr(m_radius, other.m_radius)
@@ -211,7 +211,7 @@ bool CSSBasicShapeEllipse::equals(const CSSBasicShape& shape) const
     if (shape.type() != CSSBasicShapeEllipseType)
         return false;
 
-    const CSSBasicShapeEllipse& other = static_cast<const CSSBasicShapeEllipse&>(shape);
+    const CSSBasicShapeEllipse& other = toCSSBasicShapeEllipse(shape);
     return compareCSSValuePtr(m_centerX, other.m_centerX)
         && compareCSSValuePtr(m_centerY, other.m_centerY)
         && compareCSSValuePtr(m_radiusX, other.m_radiusX)
@@ -289,7 +289,7 @@ bool CSSBasicShapePolygon::equals(const CSSBasicShape& shape) const
     if (shape.type() != CSSBasicShapePolygonType)
         return false;
 
-    const CSSBasicShapePolygon& rhs = static_cast<const CSSBasicShapePolygon&>(shape);
+    const CSSBasicShapePolygon& rhs = toCSSBasicShapePolygon(shape);
 
     if (!compareCSSValuePtr(m_referenceBox, rhs.m_referenceBox))
         return false;
@@ -425,7 +425,7 @@ bool CSSBasicShapeInset::equals(const CSSBasicShape& shape) const
     if (shape.type() != CSSBasicShapeInsetType)
         return false;
 
-    const CSSBasicShapeInset& other = static_cast<const CSSBasicShapeInset&>(shape);
+    const CSSBasicShapeInset& other = toCSSBasicShapeInset(shape);
     return compareCSSValuePtr(m_top, other.m_top)
         && compareCSSValuePtr(m_right, other.m_right)
         && compareCSSValuePtr(m_bottom, other.m_bottom)
