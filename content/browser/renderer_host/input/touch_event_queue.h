c890dbf464514faa177d06fca271c227f9e948bf
jdduke <jdduke@chromium.org>
2015-02-09 16:04:44 -0800

    Simplify TouchEventQueue pointer bookkeeping
    Rather than tracking consumer existence for each pointer separately,
    instead simply track whether any pointer had a consumer. This leads to
    a more consistent touch stream, as seen by the renderer, and simplifies
    logic in the TouchEventQueue.
    
    BUG=456056,416497
    
    Review URL: https://codereview.chromium.org/914473002
    
    Cr-Commit-Position: refs/heads/master@{#315443}

--- content/browser/renderer_host/input/touch_event_queue.h
+++ content/browser/renderer_host/input/touch_event_queue.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/content_export.h"
 #include "content/common/input/input_event_ack_state.h"
 #include "third_party/WebKit/public/web/WebInputEvent.h"
-#include "ui/events/gesture_detection/bitset_32.h"
 #include "ui/gfx/geometry/point_f.h"
 
 namespace content {
@@ -165,12 +164,6 @@ class CONTENT_EXPORT TouchEventQueue {
   typedef std::deque<CoalescedWebTouchEvent*> TouchQueue;
   TouchQueue touch_queue_;
 
-  // Maps whether each active pointer has a consumer (i.e., a touch point has a
-  // valid consumer iff |touch_consumer_states[pointer.id]| is true.).
-  // TODO(jdduke): Consider simply tracking whether *any* touchstart had a
-  // consumer, crbug.com/416497.
-  ui::BitSet32 touch_consumer_states_;
-
   // Position of the first touch in the most recent sequence forwarded to the
   // client.
   gfx::PointF touch_sequence_start_position_;
@@ -186,6 +179,9 @@ class CONTENT_EXPORT TouchEventQueue {
 
   // Whether the renderer has at least one touch handler.
   bool has_handlers_;
+
+  // Whether any pointer in the touch sequence reported having a consumer.
+  bool has_handler_for_current_sequence_;
 
   // Whether to allow any remaining touches for the current sequence. Note that
   // this is a stricter condition than an empty |touch_consumer_states_|, as it
