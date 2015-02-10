14a00e27764badb8aa3e44f6a9bba977c2b7ca4d
mef <mef@chromium.org>
2015-02-10 07:37:16 -0800

    [Cronet] Fix testSetUseCaches failure.
    BUG=
    
    Review URL: https://codereview.chromium.org/911123002
    
    Cr-Commit-Position: refs/heads/master@{#315568}

--- components/cronet/android/test/src/org/chromium/cronet_test_apk/CronetTestActivity.java
+++ components/cronet/android/test/src/org/chromium/cronet_test_apk/CronetTestActivity.java
@@ -83,7 +83,7 @@ public class CronetTestActivity extends Activity {
 
         mUrlRequestContext = initRequestContext();
         mStreamHandlerFactory = new CronetURLStreamHandlerFactory(
-                getApplicationContext(), null);
+                getApplicationContext(), getContextConfig());
         mHistogramManager = HistogramManager.createHistogramManager();
 
         if (LIBRARY_INIT_CRONET_ONLY.equals(initString)) {
