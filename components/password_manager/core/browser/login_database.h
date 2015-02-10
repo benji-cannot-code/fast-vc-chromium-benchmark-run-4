ddd746804a4fe0ffb3af191a9c2332eb48d55adf
vasilii <vasilii@chromium.org>
2015-02-10 10:12:47 -0800

    PasswordStoreMac shouldn't notify observers when it deletes a non-existent form.
    BUG=396900
    
    Review URL: https://codereview.chromium.org/892503003
    
    Cr-Commit-Position: refs/heads/master@{#315591}

--- components/password_manager/core/browser/login_database.h
+++ components/password_manager/core/browser/login_database.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/compiler_specific.h"
 #include "base/files/file_path.h"
 #include "base/memory/scoped_vector.h"
 #include "base/pickle.h"
@@ -53,7 +54,7 @@ class LoginDatabase {
 
   // Removes |form| from the list of remembered password forms. Returns true if
   // |form| was successfully removed from the database.
-  bool RemoveLogin(const autofill::PasswordForm& form);
+  bool RemoveLogin(const autofill::PasswordForm& form) WARN_UNUSED_RESULT;
 
   // Removes all logins created from |delete_begin| onwards (inclusive) and
   // before |delete_end|. You may use a null Time value to do an unbounded
