125ad8df144c81bfc682bffbd190e8b0228dcdb4
wjmaclean <wjmaclean@chromium.org>
2015-02-10 01:31:33 -0800

    Fix flickering window by deparenting before shutting bubble.
    This cl revises https://codereview.chromium.org/886883002 in order to
    fix the window-flickering issue by de-parenting the bubble window
    prior to closing it.
    
    BUG=444995
    
    Review URL: https://codereview.chromium.org/887003006
    
    Cr-Commit-Position: refs/heads/master@{#315524}

--- chrome/browser/ui/cocoa/location_bar/zoom_decoration.mm
+++ chrome/browser/ui/cocoa/location_bar/zoom_decoration.mm
@@ -24,6 +24,7 @@ ZoomDecoration::ZoomDecoration(LocationBarViewMac* owner)
 
 ZoomDecoration::~ZoomDecoration() {
   [bubble_ closeWithoutAnimation];
+  bubble_.delegate = nil;
 }
 
 bool ZoomDecoration::UpdateIfNecessary(
@@ -49,8 +50,12 @@ bool ZoomDecoration::UpdateIfNecessary(
 }
 
 void ZoomDecoration::ShowBubble(BOOL auto_close) {
-  if (bubble_)
-    return;
+  if (bubble_) {
+    bubble_.delegate = nil;
+    [[bubble_.window parentWindow] removeChildWindow:bubble_.window];
+    [bubble_.window orderOut:nil];
+    [bubble_ closeWithoutAnimation];
+  }
 
   content::WebContents* web_contents = owner_->GetWebContents();
   if (!web_contents)
@@ -139,6 +144,7 @@ content::WebContents* ZoomDecoration::GetWebContents() {
 }
 
 void ZoomDecoration::OnClose() {
+  bubble_.delegate = nil;
   bubble_ = nil;
 
   // If the page is at default zoom then hiding the zoom decoration
