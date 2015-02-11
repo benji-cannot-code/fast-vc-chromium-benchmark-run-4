562405d7d67231468f8d410a95552ccf03fec6e9
garykac <garykac@chromium.org>
2015-02-11 09:54:10 -0800

    [Chromoting] For AppRemoting, load product name from manifest.
    Rather than using the generic "Chromium App Streaming" name for dialogs and other UX elements, this cl extracts the application name from the manifest.
    
    This cl adds Application.getApplicationName and replaces calls to getMessage('PRODUCT_NAME') to use the API instead. For DesktopRemoting, this simply calls getMessage(). For AppRemoting, it gets the name from the manifest.
    
    The template_lg.html file (which still refers to PRODUCT_NAME_APP_STREAMING) will be updated in a separate cl.
    
    NOTRY=True
    BUG=
    
    Review URL: https://codereview.chromium.org/899933003
    
    Cr-Commit-Position: refs/heads/master@{#315790}

--- remoting/webapp/crd/js/remoting.js
+++ remoting/webapp/crd/js/remoting.js
@@ -75,7 +75,7 @@ remoting.getExtensionInfo = function() {
   var v2OrLegacy = base.isAppsV2() ? " (v2)" : " (legacy)";
   var manifest = chrome.runtime.getManifest();
   if (manifest && manifest.version) {
-    var name = chrome.i18n.getMessage('PRODUCT_NAME');
+    var name = remoting.app.getApplicationName();
     return name + ' version: ' + manifest.version + v2OrLegacy;
   } else {
     return 'Failed to get product version. Corrupt manifest?';
@@ -215,4 +215,3 @@ function isWindowed_(callback) {
     console.error('chome.tabs is not available.');
   }
 }
-
