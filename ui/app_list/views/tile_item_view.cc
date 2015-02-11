26a74ea0816c352ad5ecce827d89e934bbd8f7db
calamity <calamity@chromium.org>
2015-02-10 21:40:24 -0800

    Update app list start page layout.
    This CL updates the app list start page layout to match the latest mocks.
    
    BUG=442024
    
    Review URL: https://codereview.chromium.org/904363003
    
    Cr-Commit-Position: refs/heads/master@{#315737}

--- ui/app_list/views/tile_item_view.cc
+++ ui/app_list/views/tile_item_view.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 const int kTileSize = 90;
-const int kTileHorizontalPadding = 10;
+const int kIconTitleSpacing = 6;
 
 }  // namespace
 
@@ -28,7 +28,7 @@ TileItemView::TileItemView()
       title_(new views::Label),
       selected_(false) {
   views::BoxLayout* layout_manager = new views::BoxLayout(
-      views::BoxLayout::kVertical, kTileHorizontalPadding, 0, 0);
+      views::BoxLayout::kVertical, 0, 0, kIconTitleSpacing);
   layout_manager->set_main_axis_alignment(
       views::BoxLayout::MAIN_AXIS_ALIGNMENT_CENTER);
   SetLayoutManager(layout_manager);
