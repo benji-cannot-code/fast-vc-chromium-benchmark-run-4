2aac2c8851be17964dddb8e9a19e3911331df6bd
danakj <danakj@chromium.org>
2015-02-11 13:07:47 -0800

    cc: Stop passing OcclusionTracker to RenderSurfaceImpl.
    This moves the code to grab the Occlusion off the tracker out to
    LayerTreeHostImpl. But the Occlusion depends on which draw transform
    to use, which depends on it being a replica or not.
    
    Since the decision to AppendQuads for a replica or not happens in
    LayerTreeHostImpl, we can branch there to decide which draw transform,
    occlusion, mask, and debug borders to use so that the logic is not
    split between two sites. Now |for_replica| is not passed to
    RenderSurfaceImpl::AppendQuads either.
    
    This removes one need to use an OcclusionTracker when walking layers
    to draw.
    
    R=enne, vmpstr
    BUG=446751
    
    Review URL: https://codereview.chromium.org/914813003
    
    Cr-Commit-Position: refs/heads/master@{#315834}

--- cc/test/layer_test_common.h
+++ cc/test/layer_test_common.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "cc/quads/render_pass.h"
 #include "cc/test/fake_layer_tree_host.h"
-#include "cc/test/mock_occlusion_tracker.h"
 #include "cc/trees/layer_tree_host_impl.h"
 
 #define EXPECT_SET_NEEDS_COMMIT(expect, code_to_test)                 \
@@ -129,7 +128,6 @@ class LayerTestCommon {
     scoped_ptr<FakeLayerTreeHost> host_;
     scoped_ptr<LayerImpl> root_layer_impl_;
     scoped_ptr<RenderPass> render_pass_;
-    MockOcclusionTracker<LayerImpl> occlusion_tracker_;
   };
 };
 
