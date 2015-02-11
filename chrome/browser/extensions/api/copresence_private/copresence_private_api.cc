a8b2be4e8c6c8ee9aec7df35b09d31aee15ee76c
ckehoe <ckehoe@chromium.org>
2015-02-11 14:13:39 -0800

    API cleanup
    Review URL: https://codereview.chromium.org/918723002
    
    Cr-Commit-Position: refs/heads/master@{#315851}

--- chrome/browser/extensions/api/copresence_private/copresence_private_api.cc
+++ chrome/browser/extensions/api/copresence_private/copresence_private_api.cc
@@ -18,6 +18,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace extensions {
 
+namespace SendFound = api::copresence_private::SendFound;
+namespace SendSamples = api::copresence_private::SendSamples;
+namespace SendDetect = api::copresence_private::SendDetect;
+namespace SendInitialized = api::copresence_private::SendInitialized;
+
 // Copresence Private functions.
 
 copresence::WhispernetClient* CopresencePrivateFunction::GetWhispernetClient() {
@@ -33,8 +38,7 @@ ExtensionFunction::ResponseAction CopresencePrivateSendFoundFunction::Run() {
     return RespondNow(NoArguments());
   }
 
-  scoped_ptr<api::copresence_private::SendFound::Params> params(
-      api::copresence_private::SendFound::Params::Create(*args_));
+  scoped_ptr<SendFound::Params> params(SendFound::Params::Create(*args_));
   EXTENSION_FUNCTION_VALIDATE(params.get());
   std::vector<copresence::AudioToken> tokens;
   for (size_t i = 0; i < params->tokens.size(); ++i) {
@@ -52,12 +56,11 @@ ExtensionFunction::ResponseAction CopresencePrivateSendSamplesFunction::Run() {
     return RespondNow(NoArguments());
   }
 
-  scoped_ptr<api::copresence_private::SendSamples::Params> params(
-      api::copresence_private::SendSamples::Params::Create(*args_));
+  scoped_ptr<SendSamples::Params> params(SendSamples::Params::Create(*args_));
   EXTENSION_FUNCTION_VALIDATE(params.get());
 
   scoped_refptr<media::AudioBusRefCounted> samples =
-      media::AudioBusRefCounted::Create(1,
+      media::AudioBusRefCounted::Create(1,  // Mono
                                         params->samples.size() / sizeof(float));
 
   memcpy(samples->channel(0), vector_as_array(&params->samples),
@@ -76,8 +79,7 @@ ExtensionFunction::ResponseAction CopresencePrivateSendDetectFunction::Run() {
     return RespondNow(NoArguments());
   }
 
-  scoped_ptr<api::copresence_private::SendDetect::Params> params(
-      api::copresence_private::SendDetect::Params::Create(*args_));
+  scoped_ptr<SendDetect::Params> params(SendDetect::Params::Create(*args_));
   EXTENSION_FUNCTION_VALIDATE(params.get());
 
   GetWhispernetClient()->GetDetectBroadcastCallback().Run(params->detected);
@@ -92,8 +94,8 @@ CopresencePrivateSendInitializedFunction::Run() {
     return RespondNow(NoArguments());
   }
 
-  scoped_ptr<api::copresence_private::SendInitialized::Params> params(
-      api::copresence_private::SendInitialized::Params::Create(*args_));
+  scoped_ptr<SendInitialized::Params> params(
+      SendInitialized::Params::Create(*args_));
   EXTENSION_FUNCTION_VALIDATE(params.get());
 
   GetWhispernetClient()->GetInitializedCallback().Run(params->success);
