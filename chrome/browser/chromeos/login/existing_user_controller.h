7b64d274ca985606caf1fd89f1e8a1ee49d9bd42
bartfab <bartfab@chromium.org>
2015-02-11 09:01:12 -0800

    Prevent login as new user on disabled devices
    When a device is disabled, the ExistingUserController should not permit
    any login attempts. Due to an oversight, the logic for this was present
    for existing users but not for new users. With this CL, new user logins
    are also prevented when a device is disabled (or its CrosSettings are
    not trusted).
    
    BUG=457573
    TEST=Extended browser test
    
    Review URL: https://codereview.chromium.org/917433006
    
    Cr-Commit-Position: refs/heads/master@{#315781}

--- chrome/browser/chromeos/login/existing_user_controller.h
+++ chrome/browser/chromeos/login/existing_user_controller.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/basictypes.h"
+#include "base/callback_forward.h"
 #include "base/compiler_specific.h"
 #include "base/gtest_prod_util.h"
 #include "base/memory/scoped_ptr.h"
@@ -220,6 +221,18 @@ class ExistingUserController : public LoginDisplay::Delegate,
   // cancelled. If |start_public_session_timer| is true than public session
   // auto-login timer is started.
   void PerformLoginFinishedActions(bool start_public_session_timer);
+
+  // Invokes |continuation| after verifying that the device is not disabled.
+  void ContinueLoginIfDeviceNotDisabled(const base::Closure& continuation);
+
+  // Signs in as a new user. This is a continuation of CompleteLogin() that gets
+  // invoked after it has been verified that the device is not disabled.
+  void DoCompleteLogin(const UserContext& user_context);
+
+  // Signs in as a known user. This is a continuation of Login() that gets
+  // invoked after it has been verified that the device is not disabled.
+  void DoLogin(const UserContext& user_context,
+               const SigninSpecifics& specifics);
 
   // Public session auto-login timer.
   scoped_ptr<base::OneShotTimer<ExistingUserController> > auto_login_timer_;
