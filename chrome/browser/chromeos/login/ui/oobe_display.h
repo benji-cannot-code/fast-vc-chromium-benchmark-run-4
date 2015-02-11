68782653f72a394ab9f48ada372e77f2a9ba05ee
merkulova <merkulova@chromium.org>
2015-02-11 09:46:07 -0800

    HID-detection screen moved to screenContext.
    BUG=433797
    
    Review URL: https://codereview.chromium.org/898453002
    
    Cr-Commit-Position: refs/heads/master@{#315788}

--- chrome/browser/chromeos/login/ui/oobe_display.h
+++ chrome/browser/chromeos/login/ui/oobe_display.h
@@ -23,7 +23,7 @@ class EnrollmentScreenActor;
 class ErrorScreenActor;
 class EulaView;
 class GaiaScreenHandler;
-class HIDDetectionScreenActor;
+class HIDDetectionView;
 class HostPairingScreenActor;
 class KioskAutolaunchScreenActor;
 class KioskEnableScreenActor;
@@ -89,7 +89,7 @@ class OobeDisplay {
   virtual WrongHWIDScreenActor* GetWrongHWIDScreenActor() = 0;
   virtual AutoEnrollmentCheckScreenActor*
       GetAutoEnrollmentCheckScreenActor() = 0;
-  virtual HIDDetectionScreenActor* GetHIDDetectionScreenActor() = 0;
+  virtual HIDDetectionView* GetHIDDetectionView() = 0;
   virtual SupervisedUserCreationScreenHandler*
       GetSupervisedUserCreationScreenActor() = 0;
   virtual AppLaunchSplashScreenActor* GetAppLaunchSplashScreenActor() = 0;
