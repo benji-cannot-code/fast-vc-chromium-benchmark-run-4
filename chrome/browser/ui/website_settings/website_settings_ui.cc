ea0f72b3a2461175ee89076a6e3b478f25813171
palmer <palmer@chromium.org>
2015-02-10 16:42:19 -0800

    Limit the display of permissions in the Origin Info Bubble, so that only those settings that are not the defaults are shown. Re-order the permissions so that the most important permissions are shown first. Show a Site Settings link/button to allow users to see the non-default settings.
    BUG=444244
    
    Review URL: https://codereview.chromium.org/810893003
    
    Cr-Commit-Position: refs/heads/master@{#315691}

--- chrome/browser/ui/website_settings/website_settings_ui.cc
+++ chrome/browser/ui/website_settings/website_settings_ui.cc
@@ -16,7 +16,7 @@ namespace {
 
 const int kInvalidResourceID = -1;
 
-// The resource id's for the strings that are displayed on the permissions
+// The resource IDs for the strings that are displayed on the permissions
 // button if the permission setting is managed by policy.
 const int kPermissionButtonTextIDPolicyManaged[] = {
     kInvalidResourceID,
@@ -29,7 +29,7 @@ static_assert(arraysize(kPermissionButtonTextIDPolicyManaged) ==
               CONTENT_SETTING_NUM_SETTINGS,
               "kPermissionButtonTextIDPolicyManaged array size is incorrect");
 
-// The resource id's for the strings that are displayed on the permissions
+// The resource IDs for the strings that are displayed on the permissions
 // button if the permission setting is managed by an extension.
 const int kPermissionButtonTextIDExtensionManaged[] = {
     kInvalidResourceID,
@@ -43,7 +43,7 @@ static_assert(arraysize(kPermissionButtonTextIDExtensionManaged) ==
               "kPermissionButtonTextIDExtensionManaged array size is "
               "incorrect");
 
-// The resource id's for the strings that are displayed on the permissions
+// The resource IDs for the strings that are displayed on the permissions
 // button if the permission setting is managed by the user.
 const int kPermissionButtonTextIDUserManaged[] = {
     kInvalidResourceID,
@@ -56,7 +56,7 @@ static_assert(arraysize(kPermissionButtonTextIDUserManaged) ==
               CONTENT_SETTING_NUM_SETTINGS,
               "kPermissionButtonTextIDUserManaged array size is incorrect");
 
-// The resource id's for the strings that are displayed on the permissions
+// The resource IDs for the strings that are displayed on the permissions
 // button if the permission setting is the global default setting.
 const int kPermissionButtonTextIDDefaultSetting[] = {
     kInvalidResourceID,
