bf3cb781e59435632999430ca7e943646e566966
ananta <ananta@chromium.org>
2015-02-10 19:12:09 -0800

    Remove the scoped tracker for FileStream::Context::Read on Windows.
    The underlying bug with ReadFile causing the IO thread to block at times has been addressed by moving
    the call to the worker pool.
    
    BUG=423948
    
    Review URL: https://codereview.chromium.org/913993002
    
    Cr-Commit-Position: refs/heads/master@{#315720}

--- net/base/file_stream_context_win.cc
+++ net/base/file_stream_context_win.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_loop.h"
 #include "base/message_loop/message_loop_proxy.h"
 #include "base/metrics/histogram.h"
-#include "base/profiler/scoped_tracker.h"
 #include "base/task_runner.h"
 #include "base/threading/worker_pool.h"
 #include "net/base/io_buffer.h"
@@ -75,10 +74,6 @@ FileStream::Context::~Context() {
 int FileStream::Context::Read(IOBuffer* buf,
                               int buf_len,
                               const CompletionCallback& callback) {
-  // TODO(vadimt): Remove ScopedTracker below once crbug.com/423948 is fixed.
-  tracked_objects::ScopedTracker tracking_profile(
-      FROM_HERE_WITH_EXPLICIT_FUNCTION("423948 FileStream::Context::Read"));
-
   CHECK(!async_in_progress_);
   DCHECK(!async_read_initiated_);
   DCHECK(!async_read_completed_);
