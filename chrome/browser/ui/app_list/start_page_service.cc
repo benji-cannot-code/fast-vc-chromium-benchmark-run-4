2ab4615721128f1c481695e093d5320655f2275e
calamity <calamity@chromium.org>
2015-02-11 22:22:57 -0800

    Make start page logo web contents transparent.
    This CL makes the start page's logo WebView transparent. This means we
    won't need to update the background color of the start page view any
    more.
    
    BUG=442021,457750
    
    Review URL: https://codereview.chromium.org/911373002
    
    Cr-Commit-Position: refs/heads/master@{#315937}

--- chrome/browser/ui/app_list/start_page_service.cc
+++ chrome/browser/ui/app_list/start_page_service.cc
@@ -38,6 +38,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_registrar.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/notification_source.h"
+#include "content/public/browser/render_view_host.h"
+#include "content/public/browser/render_widget_host_view.h"
 #include "content/public/browser/speech_recognition_session_preamble.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_delegate.h"
@@ -552,6 +554,8 @@ void StartPageService::LoadContents() {
       content::Referrer(),
       ui::PAGE_TRANSITION_AUTO_TOPLEVEL,
       std::string());
+  contents_->GetRenderViewHost()->GetView()->SetBackgroundColor(
+        SK_ColorTRANSPARENT);
 }
 
 void StartPageService::UnloadContents() {
