0003df2b1dff40bb48a9c6cf0d10dc0e4a6105b6
hendrikw <hendrikw@chromium.org>
2015-02-12 12:05:29 -0800

    cc: Translate filter when texture is larger than quad rect
    The passed in rendered content sits on the bottom-left of the texture,
    with y=down (or top-right with y=up, makes no difference), but the alpha
    threshold filter's region sits at top-left with y=down.
    
    When the texture is blitted onto the scratch texture through the filter,
    we normally line up correctly because the texture sizes are the same so
    it doesn't matter if the textures exist at the top or bottom (because they
    fill the whole texture).
    
    When the texture is larger than the quad rect, the filter no longer lines
    up with the content because the content sits at the bottom, and the filter
    is on the top.  This will happen when we minimize an application on chromeOS
    that has an alpha threshold filter (ie hangouts).
    
    The fix simply translates the filter by the difference in size between the
    texture height and quad rect height.
    
    There's also a test.
    
    Review URL: https://codereview.chromium.org/909353003
    
    Cr-Commit-Position: refs/heads/master@{#316035}

--- cc/test/layer_tree_pixel_test.cc
+++ cc/test/layer_tree_pixel_test.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/layers/texture_layer.h"
 #include "cc/output/copy_output_request.h"
 #include "cc/output/copy_output_result.h"
+#include "cc/output/direct_renderer.h"
 #include "cc/resources/texture_mailbox.h"
 #include "cc/test/paths.h"
 #include "cc/test/pixel_comparator.h"
@@ -67,6 +68,9 @@ void LayerTreePixelTest::CommitCompleteOnThread(LayerTreeHostImpl* impl) {
       impl->pending_tree() ? impl->pending_tree() : impl->active_tree();
   if (commit_tree->source_frame_number() != 0)
     return;
+
+  DirectRenderer* renderer = static_cast<DirectRenderer*>(impl->renderer());
+  renderer->SetEnlargePassTextureAmountForTesting(enlarge_texture_amount_);
 
   gfx::Rect viewport = impl->DeviceViewport();
   // The viewport has a 0,0 origin without external influence.
