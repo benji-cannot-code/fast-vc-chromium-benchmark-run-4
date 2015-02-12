2f2006bd7906e92a8bd761e5180f8c120c2cf6b1
jln <jln@chromium.org>
2015-02-11 20:53:04 -0800

    Namespace sandbox: add important security checks
    When engaging the namespace sandbox, add important checks that the process
    is single threaded and has no directory file descriptor open.
    
    As part of this change, move the function engaging the namespace
    sandbox from the Zygote to the LinuxSandbox class.
    
    BUG=457377, 312380
    
    Review URL: https://codereview.chromium.org/915823002
    
    Cr-Commit-Position: refs/heads/master@{#315932}

---
+++ content/common/sandbox_linux/sandbox_debug_handling_linux.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_COMMON_SANDBOX_LINUX_SANDBOX_DEBUG_HANDLING_LINUX_H_
+#define CONTENT_COMMON_SANDBOX_LINUX_SANDBOX_DEBUG_HANDLING_LINUX_H_
+
+#include "base/macros.h"
+
+namespace content {
+
+class SandboxDebugHandling {
+ public:
+  // Depending on the command line, set the current process as
+  // non dumpable. Also set any signal handlers for sandbox
+  // debugging.
+  static bool SetDumpableStatusAndHandlers();
+
+ private:
+  DISALLOW_IMPLICIT_CONSTRUCTORS(SandboxDebugHandling);
+};
+
+}  // namespace content
+
+#endif  // CONTENT_COMMON_SANDBOX_LINUX_SANDBOX_DEBUG_HANDLING_LINUX_H_
