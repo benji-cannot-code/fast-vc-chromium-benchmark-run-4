42cd21b4b2ae7e5480c4fdb33fd09a9df3e86f0a
scottmg <scottmg@chromium.org>
2015-02-12 13:23:40 -0800

    win: Stop drag downloads from downloading as soon as they hit a target
    This is very similar to https://codereview.chromium.org/13979012/ but for
    Aura.
    
    Previously, a download would start as soon as the drag left the chrome window
    and if it was a big file, this would hang the UI until it completed. With
    this, the download doesn't start until the drop.
    
    There's an outstanding problem that if the download is blocked by safe
    browsing (or whatever it's called) then that infobar is effectively tab-
    modal until it's interacted with, because the download neither completes nor
    cancels until that's decided. But I think that's a different problem so
    that can get fixed in a subsequent CL.
    
    R=sky@chromium.org,dcheng@chromium.org
    BUG=390991
    
    Review URL: https://codereview.chromium.org/914213003
    
    Cr-Commit-Position: refs/heads/master@{#316050}

--- ui/base/dragdrop/drag_source_win.cc
+++ ui/base/dragdrop/drag_source_win.cc
@@ -5,9 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/base/dragdrop/drag_source_win.h"
 
+#include "ui/base/dragdrop/os_exchange_data_provider_win.h"
+
 namespace ui {
 
-DragSourceWin::DragSourceWin() : cancel_drag_(false) {
+DragSourceWin::DragSourceWin() : cancel_drag_(false), data_(nullptr) {
 }
 
 HRESULT DragSourceWin::QueryContinueDrag(BOOL escape_pressed, DWORD key_state) {
@@ -51,6 +53,12 @@ ULONG DragSourceWin::AddRef() {
 ULONG DragSourceWin::Release() {
   base::RefCountedThreadSafe<DragSourceWin>::Release();
   return 0;
+}
+
+void DragSourceWin::OnDragSourceDrop() {
+  DCHECK(data_);
+  ui::OSExchangeDataProviderWin::GetDataObjectImpl(*data_)
+      ->set_in_drag_loop(false);
 }
 
 }  // namespace ui
--- ui/base/dragdrop/drag_source_win.h
+++ ui/base/dragdrop/drag_source_win.h
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 
+class OSExchangeData;
+
 // A base IDropSource implementation. Handles notifications sent by an active
 // drag-drop operation as the user mouses over other drop targets on their
 // system. This object tells Windows whether or not the drag should continue,
@@ -41,14 +43,21 @@ class UI_BASE_EXPORT DragSourceWin
   ULONG __stdcall AddRef();
   ULONG __stdcall Release();
 
+  // Used to set the active data object for the current drag operation. The
+  // caller must ensure that |data| is not destroyed before the nested drag loop
+  // terminates.
+  void set_data(const OSExchangeData* data) { data_ = data; }
+
  protected:
   virtual void OnDragSourceCancel() {}
-  virtual void OnDragSourceDrop() {}
+  virtual void OnDragSourceDrop();
   virtual void OnDragSourceMove() {}
 
  private:
   // Set to true if we want to cancel the drag operation.
   bool cancel_drag_;
+
+  const OSExchangeData* data_;
 
   DISALLOW_COPY_AND_ASSIGN(DragSourceWin);
 };
