d25688150adaf799420fb5c22e2672ec8bc3ed84
hiroshige@chromium.org <hiroshige@chromium.org>
2015-02-12 12:45:48 +0000

    [Fetch] Split layout tests
    Move tests that call fetch() with mode='no-cors' to separate files.
    - block-mixed-content-nocors.js
    - fetch-access-control-nocors.js
      (from fetch-access-control.js and fetch-access-control-cors.js)
    - fetch-access-control-auth-nocors.js
    - fetch-access-control-cookie-nocors.js
    - fetch-access-control-redirect-nocors.js
    
    Tests with 'mode=no-cors' in URLs are moved.
    Tests without mode parameter in URLs are also moved and marked by
    onlyOnServiceWorkerProxiedTest(), because these tests are testing that
    default mode in serviceworker-proxied tests is no-cors.
    Tests that create Request with mode='no-cors' but do not call fetch() with
    such requests are not moved.
    
    Split other fetch-access-control* tests:
    - fetch-access-control-cors-preflight.js
      (from fetch-access-control-cors.js)
    - fetch-access-control-redirect-credentials.js
    - fetch-access-control-redirect-loop.js
      (from fetch-access-control-redirect.js)
    
    BUG=457157
    
    Review URL: https://codereview.chromium.org/921673003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@190048 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/fetch/script-tests/fetch-access-control-redirect.js
+++ third_party/WebKit/LayoutTests/http/tests/fetch/script-tests/fetch-access-control-redirect.js
@@ -70,28 +70,9 @@ var TEST_TARGETS = [
    [methodIsPUT, authCheck1]],
 
   [REDIRECT_URL + encodeURIComponent(BASE_URL) +
-   '&mode=no-cors&method=GET&headers=CUSTOM',
-   [fetchResolved, hasContentLength, hasServerHeader, hasBody, typeBasic],
-   [methodIsGET, noCustomHeader, authCheck1]],
-
-  [REDIRECT_URL + encodeURIComponent(BASE_URL) +
    '&mode=cors&method=GET&headers=CUSTOM',
    [fetchResolved, hasContentLength, hasServerHeader, hasBody, typeBasic],
    [methodIsGET, hasCustomHeader, authCheck1]],
-
-  // Credential test
-  [REDIRECT_URL + encodeURIComponent(BASE_URL) +
-   '&mode=cors&credentials=omit&method=GET',
-   [fetchResolved, hasContentLength, hasServerHeader, hasBody, typeBasic],
-   [methodIsGET, authCheckNone]],
-  [REDIRECT_URL + encodeURIComponent(BASE_URL) +
-   '&mode=cors&credentials=include&method=GET',
-   [fetchResolved, hasContentLength, hasServerHeader, hasBody, typeBasic],
-   [methodIsGET, authCheck1]],
-  [REDIRECT_URL + encodeURIComponent(BASE_URL) +
-   '&mode=cors&credentials=same-origin&method=GET',
-   [fetchResolved, hasContentLength, hasServerHeader, hasBody, typeBasic],
-   [methodIsGET, authCheck1]],
 
   // Redirect: same origin -> other origin
   [REDIRECT_URL + encodeURIComponent(OTHER_BASE_URL) +
@@ -103,39 +84,6 @@ var TEST_TARGETS = [
   [REDIRECT_URL + encodeURIComponent(OTHER_BASE_URL) +
    '&mode=same-origin&method=PUT',
    [fetchRejected]],
-
-  [REDIRECT_URL + encodeURIComponent(OTHER_BASE_URL) +
-   '&mode=no-cors&method=GET&headers=CUSTOM',
-   [fetchResolved, noContentLength, noServerHeader, noBody, typeOpaque],
-   onlyOnServiceWorkerProxiedTest([methodIsGET, noCustomHeader, authCheck2])],
-
-  // Status code tests for mode="no-cors"
-  // The 301 redirect response changes POST method to GET method.
-  [REDIRECT_URL + encodeURIComponent(OTHER_BASE_URL) +
-   '&mode=no-cors&method=POST&Status=301',
-   [fetchResolved, noContentLength, noServerHeader, noBody, typeOpaque],
-   onlyOnServiceWorkerProxiedTest([methodIsGET, authCheck2])],
-  // The 302 redirect response changes POST method to GET method.
-  [REDIRECT_URL + encodeURIComponent(OTHER_BASE_URL) +
-   '&mode=no-cors&method=POST',
-   [fetchResolved, noContentLength, noServerHeader, noBody, typeOpaque],
-   onlyOnServiceWorkerProxiedTest([methodIsGET, authCheck2])],
-  // GET method must be used for 303 redirect.
-  [REDIRECT_URL + encodeURIComponent(OTHER_BASE_URL) +
-   '&mode=no-cors&method=POST&Status=303',
-   [fetchResolved, noContentLength, noServerHeader, noBody, typeOpaque],
-   onlyOnServiceWorkerProxiedTest([methodIsGET, authCheck2])],
-  // The 307 redirect response doesn't change the method.
-  [REDIRECT_URL + encodeURIComponent(OTHER_BASE_URL) +
-   '&mode=no-cors&method=POST&Status=307',
-   [fetchResolved, noContentLength, noServerHeader, noBody, typeOpaque],
-   onlyOnServiceWorkerProxiedTest([methodIsPOST, authCheck2])],
-  // The 308 redirect response doesn't change the method.
-  // FIXME: disabled due to https://crbug.com/451938
-  // [REDIRECT_URL + encodeURIComponent(OTHER_BASE_URL) +
-  // '&mode=no-cors&method=POST&Status=308',
-  // [fetchResolved, noContentLength, noServerHeader, noBody, typeOpaque],
-  // onlyOnServiceWorkerProxiedTest([methodIsPOST, authCheck2])],
 
   [REDIRECT_URL + encodeURIComponent(OTHER_BASE_URL) +
    '&mode=cors&method=GET',
@@ -195,52 +143,6 @@ var TEST_TARGETS = [
    [fetchResolved, hasContentLength, hasServerHeader, hasBody, typeCors],
    [methodIsGET, authCheckNone]],
 
-  // Credential test
-  [REDIRECT_URL + encodeURIComponent(OTHER_BASE_URL) +
-   '&mode=cors&credentials=omit&method=GET',
-   [fetchRejected]],
-  [REDIRECT_URL + encodeURIComponent(OTHER_BASE_URL) +
-   '&mode=cors&credentials=include&method=GET',
-   [fetchRejected]],
-  [REDIRECT_URL + encodeURIComponent(OTHER_BASE_URL) +
-   '&mode=cors&credentials=same-origin&method=GET',
-   [fetchRejected]],
-
-  [REDIRECT_URL +
-   encodeURIComponent(OTHER_BASE_URL + '&ACAOrigin=' + BASE_ORIGIN + '') +
-   '&mode=cors&credentials=omit&method=GET',
-   [fetchResolved, noContentLength, noServerHeader, hasBody, typeCors],
-   [methodIsGET, authCheckNone]],
-  [REDIRECT_URL +
-   encodeURIComponent(OTHER_BASE_URL +
-                      '&ACAOrigin=' + BASE_ORIGIN + '&ACACredentials=true') +
-   '&mode=cors&credentials=omit&method=GET',
-   [fetchResolved, noContentLength, noServerHeader, hasBody, typeCors],
-   [methodIsGET, authCheckNone]],
-
-  [REDIRECT_URL +
-   encodeURIComponent(OTHER_BASE_URL + '&ACAOrigin=' + BASE_ORIGIN + '') +
-   '&mode=cors&credentials=include&method=GET',
-   [fetchRejected]],
-  [REDIRECT_URL +
-   encodeURIComponent(OTHER_BASE_URL +
-                      '&ACAOrigin=' + BASE_ORIGIN + '&ACACredentials=true') +
-   '&mode=cors&credentials=include&method=GET',
-   [fetchResolved, noContentLength, noServerHeader, hasBody, typeCors],
-   [methodIsGET, authCheck2]],
-
-  [REDIRECT_URL +
-   encodeURIComponent(OTHER_BASE_URL + '&ACAOrigin=' + BASE_ORIGIN + '') +
-   '&mode=cors&credentials=same-origin&method=GET',
-   [fetchResolved, noContentLength, noServerHeader, hasBody, typeCors],
-   [methodIsGET, authCheckNone]],
-  [REDIRECT_URL +
-   encodeURIComponent(OTHER_BASE_URL +
-                      '&ACAOrigin=' + BASE_ORIGIN + '&ACACredentials=true') +
-   '&mode=cors&credentials=same-origin&method=GET',
-   [fetchResolved, noContentLength, noServerHeader, hasBody, typeCors],
-   [methodIsGET, authCheckNone]],
-
   // Redirect: other origin -> same origin
   [OTHER_REDIRECT_URL + encodeURIComponent(BASE_URL) +
    '&mode=same-origin&method=GET',
@@ -248,43 +150,6 @@ var TEST_TARGETS = [
   [OTHER_REDIRECT_URL + encodeURIComponent(BASE_URL) +
    '&mode=same-origin&method=POST',
    [fetchRejected]],
-
-  [OTHER_REDIRECT_URL + encodeURIComponent(BASE_URL) +
-   '&mode=no-cors&method=GET',
-   [fetchResolved, noContentLength, noServerHeader, noBody, typeOpaque],
-   onlyOnServiceWorkerProxiedTest([methodIsGET, authCheck1])],
-  [OTHER_REDIRECT_URL + encodeURIComponent(BASE_URL) +
-   '&mode=no-cors&method=GET&headers=CUSTOM',
-   [fetchResolved, noContentLength, noServerHeader, noBody, typeOpaque],
-   onlyOnServiceWorkerProxiedTest([methodIsGET, noCustomHeader, authCheck1])],
-
-  // Status code tests for mode="no-cors"
-  // The 301 redirect response MAY change the request method from POST to GET.
-  [OTHER_REDIRECT_URL + encodeURIComponent(BASE_URL) +
-   '&mode=no-cors&method=POST&Status=301',
-   [fetchResolved, noContentLength, noServerHeader, noBody, typeOpaque],
-   onlyOnServiceWorkerProxiedTest([methodIsGET, authCheck1])],
-  // The 302 redirect response MAY change the request method from POST to GET.
-  [OTHER_REDIRECT_URL + encodeURIComponent(BASE_URL) +
-   '&mode=no-cors&method=POST',
-   [fetchResolved, noContentLength, noServerHeader, noBody, typeOpaque],
-   onlyOnServiceWorkerProxiedTest([methodIsGET, authCheck1])],
-  // GET method must be used for 303 redirect.
-  [OTHER_REDIRECT_URL + encodeURIComponent(BASE_URL) +
-   '&mode=no-cors&method=POST&Status=303',
-   [fetchResolved, noContentLength, noServerHeader, noBody, typeOpaque],
-   onlyOnServiceWorkerProxiedTest([methodIsGET, authCheck1])],
-  // The 307 redirect response MUST NOT change the method.
-  [OTHER_REDIRECT_URL + encodeURIComponent(BASE_URL) +
-   '&mode=no-cors&method=POST&Status=307',
-   [fetchResolved, noContentLength, noServerHeader, noBody, typeOpaque],
-   onlyOnServiceWorkerProxiedTest([methodIsPOST, authCheck1])],
-  // The 308 redirect response MUST NOT change the method.
-  // FIXME: disabled due to https://crbug.com/451938
-  // [OTHER_REDIRECT_URL + encodeURIComponent(BASE_URL) +
-  //  '&mode=no-cors&method=POST&Status=308',
-  //  [fetchResolved, noContentLength, noServerHeader, noBody, typeOpaque],
-  //  onlyOnServiceWorkerProxiedTest([methodIsPOST, authCheck1])],
 
   [OTHER_REDIRECT_URL + encodeURIComponent(BASE_URL) +
    '&mode=cors&method=GET',
@@ -340,45 +205,10 @@ var TEST_TARGETS = [
    '&mode=cors&method=GET&headers=CUSTOM&ACAOrigin=*',
    [fetchRejected]],
 
-  // Credentials test
-  [OTHER_REDIRECT_URL + encodeURIComponent(BASE_URL + 'ACAOrigin=*') +
-   '&mode=cors&credentials=omit&method=GET&ACAOrigin=*',
-   [fetchResolved, noContentLength, noServerHeader, hasBody, typeCors],
-   [methodIsGET, authCheckNone]],
-  [OTHER_REDIRECT_URL + encodeURIComponent(BASE_URL + 'ACAOrigin=*') +
-   '&mode=cors&credentials=include&method=GET&ACAOrigin=*',
-   [fetchRejected]],
-  [OTHER_REDIRECT_URL + encodeURIComponent(BASE_URL + 'ACAOrigin=*') +
-   '&mode=cors&credentials=same-origin&method=GET&ACAOrigin=*',
-   [fetchResolved, noContentLength, noServerHeader, hasBody, typeCors],
-   [methodIsGET, authCheckNone]],
-  [OTHER_REDIRECT_URL +
-   encodeURIComponent(BASE_URL + 'ACAOrigin=null&ACACredentials=true') +
-   '&mode=cors&credentials=omit&method=GET' +
-   '&ACAOrigin=' + BASE_ORIGIN + '&ACACredentials=true',
-   [fetchResolved, noContentLength, noServerHeader, hasBody, typeCors],
-   [methodIsGET, authCheckNone]],
-  [OTHER_REDIRECT_URL +
-   encodeURIComponent(BASE_URL + 'ACAOrigin=null&ACACredentials=true') +
-   '&mode=cors&credentials=include&method=GET' +
-   '&ACAOrigin=' + BASE_ORIGIN + '&ACACredentials=true',
-   [fetchResolved, noContentLength, noServerHeader, hasBody, typeCors],
-   [methodIsGET, authCheck1]],
-  [OTHER_REDIRECT_URL +
-   encodeURIComponent(BASE_URL + 'ACAOrigin=null&ACACredentials=true') +
-   '&mode=cors&credentials=same-origin&method=GET' +
-   '&ACAOrigin=' + BASE_ORIGIN + '&ACACredentials=true',
-   [fetchResolved, noContentLength, noServerHeader, hasBody, typeCors],
-   [methodIsGET, authCheckNone]],
-
   // Redirect: other origin -> other origin
   [OTHER_REDIRECT_URL + encodeURIComponent(OTHER_BASE_URL) +
    '&mode=same-origin&method=GET',
    [fetchRejected]],
-  [OTHER_REDIRECT_URL + encodeURIComponent(OTHER_BASE_URL) +
-   '&mode=no-cors&method=GET',
-   [fetchResolved, noContentLength, noServerHeader, noBody, typeOpaque],
-   onlyOnServiceWorkerProxiedTest([methodIsGET, authCheck2])],
   [OTHER_REDIRECT_URL + encodeURIComponent(OTHER_BASE_URL) +
    '&mode=cors&method=GET',
    [fetchRejected]],
@@ -403,7 +233,6 @@ var TEST_TARGETS = [
    '&mode=cors&method=GET&ACAOrigin=' + BASE_ORIGIN + '',
    [fetchResolved, noContentLength, noServerHeader, hasBody, typeCors],
    [methodIsGET, authCheckNone]],
-
 
   // Status code tests for mode="cors"
   // The 301 redirect response MAY change the request method from POST to GET.
@@ -455,45 +284,6 @@ var TEST_TARGETS = [
      'ACAOrigin=' + BASE_ORIGIN + '&ACAHeaders=x-serviceworker-test') +
    '&mode=cors&method=GET&headers=CUSTOM' +
    '&ACAOrigin=' + BASE_ORIGIN + '&ACAHeaders=x-serviceworker-test',
-   [fetchRejected]],
-
-  // Redirect loop: same origin -> same origin
-  [REDIRECT_LOOP_URL + encodeURIComponent(BASE_URL) + '&Count=20',
-   [fetchResolved, hasContentLength, hasBody, typeBasic],
-   [methodIsGET, authCheck1]],
-  [REDIRECT_LOOP_URL + encodeURIComponent(BASE_URL) + '&Count=21',
-   [fetchRejected]],
-
-  // Redirect loop: same origin -> other origin
-  [REDIRECT_LOOP_URL + encodeURIComponent(OTHER_BASE_URL + '&ACAOrigin=*') +
-   '&Count=20&mode=cors&method=GET',
-   [fetchResolved, noContentLength, noServerHeader, hasBody, typeCors],
-   [methodIsGET, authCheckNone]],
-  // FIXME: due to the current implementation of Chromium,
-  // Count=21 is resolved, Count=22 is rejected.
-  // https://crbug.com/353768
-  [REDIRECT_LOOP_URL + encodeURIComponent(OTHER_BASE_URL + '&ACAOrigin=*') +
-   '&Count=22&mode=cors&method=GET',
-   [fetchRejected]],
-
-  // Redirect loop: other origin -> same origin
-  [OTHER_REDIRECT_LOOP_URL + encodeURIComponent(BASE_URL + 'ACAOrigin=*') +
-   '&Count=20&mode=cors&method=GET&ACAOrigin=*',
-   [fetchResolved, noContentLength, noServerHeader, hasBody, typeCors],
-   [methodIsGET, authCheckNone]],
-  [OTHER_REDIRECT_LOOP_URL + encodeURIComponent(BASE_URL + 'ACAOrigin=*') +
-   '&Count=21&mode=cors&method=GET&ACAOrigin=*',
-   [fetchRejected]],
-
-  // Redirect loop: other origin -> other origin
-  [OTHER_REDIRECT_LOOP_URL +
-   encodeURIComponent(OTHER_BASE_URL + 'ACAOrigin=*') +
-   '&Count=20&mode=cors&method=GET&ACAOrigin=*',
-   [fetchResolved, noContentLength, noServerHeader, hasBody, typeCors],
-   [methodIsGET, authCheckNone]],
-  [OTHER_REDIRECT_LOOP_URL +
-   encodeURIComponent(OTHER_BASE_URL + 'ACAOrigin=*') +
-   '&Count=21&mode=cors&method=GET&ACAOrigin=*',
    [fetchRejected]],
 ];
 
