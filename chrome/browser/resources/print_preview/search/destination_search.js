c0c3a2f414934b159a4ea880ee668579ef608edf
dbeam <dbeam@chromium.org>
2015-02-11 19:02:33 -0800

    print_preview: fix visual glitch when dismissing cloud print promo.
    The dialog should use the freed promo space for more destinations.
    
    R=alekseys@chromium.org
    BUG=none
    TEST=visual
    
    Review URL: https://codereview.chromium.org/918923002
    
    Cr-Commit-Position: refs/heads/master@{#315917}

--- chrome/browser/resources/print_preview/search/destination_search.js
+++ chrome/browser/resources/print_preview/search/destination_search.js
@@ -680,6 +680,7 @@ cr.define('print_preview', function() {
      */
     onCloudprintPromoCloseButtonClick_: function() {
       setIsVisible(this.getChildElement('.cloudprint-promo'), false);
+      this.reflowLists_();
     },
 
     /**
