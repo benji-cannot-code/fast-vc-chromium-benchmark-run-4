dc663a327a101ffd68839586024ae84b3b71c17c
fs@opera.com <fs@opera.com>
2015-02-12 20:04:02 +0000

    Use SVGLength-references in the SVGLengthContext API
    Convert all the PassRefPtrWillBeRawPtr<SVGLength>-typed arguments to
    const SVGLength&. This avoids unnecessary ref-count churn (the caller will
    always own a reference anyway). As a side-effect footprint is reduced by
    ~2k. (resolveRectangle<T> can now be properly inlined and other methods
    shrink a bit.)
    
    Review URL: https://codereview.chromium.org/921823002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@190070 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/svg/LayoutSVGResourcePattern.cpp
+++ third_party/WebKit/Source/core/layout/svg/LayoutSVGResourcePattern.cpp
@@ -94,7 +94,7 @@ PassOwnPtr<PatternData> LayoutSVGResourcePattern::buildPatternData(const LayoutO
     FloatRect clientBoundingBox = object.objectBoundingBox();
     FloatRect tileBounds = SVGLengthContext::resolveRectangle(element(),
         attributes.patternUnits(), clientBoundingBox,
-        attributes.x(), attributes.y(), attributes.width(), attributes.height());
+        *attributes.x(), *attributes.y(), *attributes.width(), *attributes.height());
     if (tileBounds.isEmpty())
         return nullptr;
 
