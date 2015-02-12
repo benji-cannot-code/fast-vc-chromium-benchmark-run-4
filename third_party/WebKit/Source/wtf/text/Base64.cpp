ccd828035c4c7cc492012de05c8b85badab0855d
jshin@chromium.org <jshin@chromium.org>
2015-02-12 00:28:47 +0000

    Make all the source files ASCII-only in Blink.
    Non-ASCII characters, either encoded in UTF-8 or in Latin-1, lead to
    C4819 warning by MSVC on Windows when the default codepage is CJK.
    
    MSVC does not offer a way to interpret input source files as UTF-8
    unless they have a UTF-8 BOM at the beginning. UTF-8 BOM does not work
    well with compilers on Linux/Mac.
    
    BUG=454858
    TEST=On Windows, set the default code page (for non-Unicode apps) to one
    of CJK (932,936,949,950) and build Blink. No C4819 warning should be
    emitted.
    
    R=dominik.rottsches@intel.com, jochen@chromium.org, rune@opera.com, thakis@chromium.org
    
    Review URL: https://codereview.chromium.org/894883002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@190012 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/text/Base64.cpp
+++ third_party/WebKit/Source/wtf/text/Base64.cpp
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
    This code is based on the java implementation in HTTPClient
-   package by Ronald Tschalär Copyright (C) 1996-1999.
+   package by Ronald Tschalaer Copyright (C) 1996-1999.
 */
 
 #include "config.h"
