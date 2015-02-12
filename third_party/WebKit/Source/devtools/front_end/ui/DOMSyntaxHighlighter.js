c73527b2bf9617b62e5226d27746f70dc06a8da7
lushnikov@chromium.org <lushnikov@chromium.org>
2015-02-12 11:31:24 +0000

    DevTools: use newline instead of <br> in DOMSyntaxHighlighter
    This patch moves DOMSyntaxHighlighter to insert newline nodes instead
    of <br> tags. This ensures that nodeWithCode.textContent returns the
    same result for both highlighted and not-yet-highlighted nodes.
    
    R=pfeldman
    
    Review URL: https://codereview.chromium.org/916853002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@190041 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ui/DOMSyntaxHighlighter.js
+++ third_party/WebKit/Source/devtools/front_end/ui/DOMSyntaxHighlighter.js
@@ -85,7 +85,7 @@ WebInspector.DOMSyntaxHighlighter.prototype = {
                     node.createTextChild(plainText);
                 }
                 if (i < lines.length - 1)
-                    node.createChild("br");
+                    node.createTextChild("\n");
             }
         }
 
