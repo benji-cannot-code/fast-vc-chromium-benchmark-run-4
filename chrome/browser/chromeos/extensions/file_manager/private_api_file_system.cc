9fa868160c276eee3d75ba8cd4fc5d461f142cd6
kenobi <kenobi@chromium.org>
2015-02-12 19:35:47 -0800

    Add a fileManagerPrivate function to access the UMA enabled flag.
    The goal is to toggle analytics- and UMA-based tracking together.
    
    BUG=457736
    
    Review URL: https://codereview.chromium.org/916833003
    
    Cr-Commit-Position: refs/heads/master@{#316151}

--- chrome/browser/chromeos/extensions/file_manager/private_api_file_system.cc
+++ chrome/browser/chromeos/extensions/file_manager/private_api_file_system.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/fileapi/file_system_backend.h"
 #include "chrome/browser/drive/drive_api_util.h"
 #include "chrome/browser/drive/event_logger.h"
+#include "chrome/browser/metrics/chrome_metrics_service_accessor.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/common/extensions/api/file_manager_private.h"
@@ -837,6 +838,12 @@ void FileManagerPrivateSearchFilesByHashesFunction::OnSearchByHashes(
   }
   SetResult(result.release());
   SendResponse(true);
+}
+
+ExtensionFunction::ResponseAction
+FileManagerPrivateIsUMAEnabledFunction::Run() {
+  return RespondNow(OneArgument(new base::FundamentalValue(
+      ChromeMetricsServiceAccessor::IsMetricsReportingEnabled())));
 }
 
 }  // namespace extensions
