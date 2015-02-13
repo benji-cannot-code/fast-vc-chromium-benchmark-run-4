43a48dea6cf4fc5f62a8155ab6d0eb6c90d37b64
tasak@google.com <tasak@google.com>
2015-02-13 07:43:29 +0000

    Add CoreExport.h and ModulesExport.h for linking core and modules separately.
    CoreExport.h and ModulesExport.h are almost the same as PlatformExport.h,
    because core and modules are exported in the same way as platform.
    
    BUG=358074
    
    Review URL: https://codereview.chromium.org/902943004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@190122 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/Source/core/CoreExport.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CoreExport_h
+#define CoreExport_h
+
+#if !defined(LINK_CORE_MODULES_SEPARATELY)
+#define LINK_CORE_MODULES_SEPARATELY 0
+#endif
+
+#if LINK_CORE_MODULES_SEPARATELY && defined(COMPONENT_BUILD)
+#if defined(WIN32)
+#if defined(BLINK_CORE_IMPLEMENTATION) && BLINK_CORE_IMPLEMENTATION
+#define CORE_EXPORT __declspec(dllexport)
+#else
+#define CORE_EXPORT __declspec(dllimport)
+#endif
+#else // defined(WIN32)
+#define CORE_EXPORT __attribute__((visibility("default")))
+#endif
+#else // defined(COMPONENT_BUILD)
+#define CORE_EXPORT
+#endif
+
+#endif // CoreExport_h
