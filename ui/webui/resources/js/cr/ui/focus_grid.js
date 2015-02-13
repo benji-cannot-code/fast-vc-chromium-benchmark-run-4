778a17faa506eb9ba2cdb52dae0205930d426d2c
hcarmona <hcarmona@chromium.org>
2015-02-12 19:18:29 -0800

    Fix bug that prevented FocusGrids from being nested.
    This is necessary for adding keyboard shortcuts to chrome://extensions.
    
    BUG=427672
    
    Review URL: https://codereview.chromium.org/921043003
    
    Cr-Commit-Position: refs/heads/master@{#316148}

--- ui/webui/resources/js/cr/ui/focus_grid.js
+++ ui/webui/resources/js/cr/ui/focus_grid.js
@@ -87,7 +87,7 @@ cr.define('cr.ui', function() {
      */
     getRowIndexForTarget: function(target) {
       for (var i = 0; i < this.rows.length; ++i) {
-        if (this.rows[i].contains(target))
+        if (this.rows[i].focusableElements.indexOf(target) >= 0)
           return i;
       }
       return -1;
