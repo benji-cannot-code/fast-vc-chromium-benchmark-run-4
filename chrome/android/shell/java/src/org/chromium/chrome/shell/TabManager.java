7a4f4f592704eab420f9f666c7f7070ce577f54c
divya.bansal <divya.bansal@samsung.com>
2015-02-13 03:02:26 -0800

    Hide Url bar in tab switcher mode.
    These changes are to make behaviour of chrome shell
    similar to chrome by hiding Url bar when tab switcher is shown.
    
    BUG=458042
    
    Review URL: https://codereview.chromium.org/914083003
    
    Cr-Commit-Position: refs/heads/master@{#316202}

--- chrome/android/shell/java/src/org/chromium/chrome/shell/TabManager.java
+++ chrome/android/shell/java/src/org/chromium/chrome/shell/TabManager.java
@@ -11,7 +11,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
@@ -228,7 +227,7 @@ public class TabManager extends LinearLayout {
             assert parent == mContentViewHolder;
             mContentViewHolder.removeView(mTabModelWrapper);
         }
-        mToolbar.updateToolBarButtonState();
+        mToolbar.updateToolbarState();
     }
 
     /**
@@ -246,10 +245,7 @@ public class TabManager extends LinearLayout {
         if (mTabModelWrapper.getParent() == null) {
             mContentViewHolder.addView(mTabModelWrapper);
         }
-        mToolbar.updateToolBarButtonState();
-        InputMethodManager mImm = (InputMethodManager) getContext().getSystemService(
-                Context.INPUT_METHOD_SERVICE);
-        mImm.hideSoftInputFromWindow(mContentViewHolder.getWindowToken(), 0);
+        mToolbar.updateToolbarState();
     }
 
     /**
