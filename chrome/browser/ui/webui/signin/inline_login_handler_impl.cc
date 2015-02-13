57207aec6bdd08cb15477cfb165e9b5682c37a51
mlerman <mlerman@chromium.org>
2015-02-13 13:18:08 -0800

    Reauth causes a MergeSession, so the cookie jar gets re-authed too.
    BUG=456905
    
    Review URL: https://codereview.chromium.org/913123005
    
    Cr-Commit-Position: refs/heads/master@{#316290}

--- chrome/browser/ui/webui/signin/inline_login_handler_impl.cc
+++ chrome/browser/ui/webui/signin/inline_login_handler_impl.cc
@@ -134,8 +134,8 @@ void InlineSigninHelper::OnClientOAuthSuccess(const ClientOAuthResult& result) {
 
   signin_metrics::Source source = signin::GetSourceForPromoURL(current_url_);
 
-  std::string primary_email =
-      SigninManagerFactory::GetForProfile(profile_)->GetAuthenticatedUsername();
+  SigninManager* signin_manager = SigninManagerFactory::GetForProfile(profile_);
+  std::string primary_email = signin_manager->GetAuthenticatedUsername();
   if (gaia::AreEmailsSame(email_, primary_email) &&
       source == signin_metrics::SOURCE_REAUTH &&
       switches::IsNewProfileManagement() &&
@@ -158,6 +158,9 @@ void InlineSigninHelper::OnClientOAuthSuccess(const ClientOAuthResult& result) {
           handler_,
           signin::ShouldShowAccountManagement(current_url_)));
     }
+
+    if (source == signin_metrics::SOURCE_REAUTH)
+      signin_manager->MergeSigninCredentialIntoCookieJar();
   } else {
     ProfileSyncService* sync_service =
         ProfileSyncServiceFactory::GetForProfile(profile_);
