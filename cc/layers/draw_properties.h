4de10a24a1ed977f838302b4422087e5468bcd9e
danakj <danakj@chromium.org>
2015-02-13 14:12:16 -0800

    cc: Make occlusion a draw property.
    We were computing occlusion twice, once in UpdateDrawProperties
    and once in CalculateRenderPasses. Now just do it once in
    UpdateDrawProperties, which means we don't have to do that work
    at all if we draw again without dirtying the render surface layer
    list.
    
    This moves the call to UpdateTiles out of the loop for occlusion,
    instead calling it directly on each of the picture layers on the
    tree, meaning it does not have to be virtual anymore.
    
    This makes it possible for us to call UpdateTiles on picture layers
    that were not part of the RenderSurfaceLayerList. Though for now
    I left the behaviour as is with a TODO.
    
    Also removed all the old LayerTreeHostOcclusionTests. Added a few
    new ones to ensure occlusion gets saved correctly in layers and
    surfaces. The test cases that would be missing, I moved to be
    OcclusionTrackerTests.
    
    R=enne, vmpstr
    BUG=446751
    
    Review URL: https://codereview.chromium.org/915083004
    
    Cr-Commit-Position: refs/heads/master@{#316306}

--- cc/layers/draw_properties.h
+++ cc/layers/draw_properties.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CC_LAYERS_DRAW_PROPERTIES_H_
 
 #include "base/memory/scoped_ptr.h"
+#include "cc/trees/occlusion.h"
 #include "third_party/skia/include/core/SkXfermode.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/transform.h"
@@ -50,6 +51,9 @@ struct CC_EXPORT DrawProperties {
 
   // Transforms objects from content space to screen space (viewport space).
   gfx::Transform screen_space_transform;
+
+  // Known occlusion above the layer mapped to the content space of the layer.
+  Occlusion occlusion_in_content_space;
 
   // DrawProperties::opacity may be different than LayerType::opacity,
   // particularly in the case when a RenderSurface re-parents the layer's
