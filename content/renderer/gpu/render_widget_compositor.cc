a9af27002eb99ba346c0cf8fed2603158f5a73ff
wangxianzhu <wangxianzhu@chromium.org>
2015-02-13 10:23:29 -0800

    Enable partial recording if slimmingPaintDisplayItemCache is enabled
    BUG=444163
    R=enne@chromium.org
    TBR=kbr@chromium.org (trivial change in render_widget_compositor.cc which is really related to cc/)
    
    Review URL: https://codereview.chromium.org/913163004
    
    Cr-Commit-Position: refs/heads/master@{#316244}

--- content/renderer/gpu/render_widget_compositor.cc
+++ content/renderer/gpu/render_widget_compositor.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/platform/WebSelectionBound.h"
 #include "third_party/WebKit/public/platform/WebSize.h"
 #include "third_party/WebKit/public/web/WebKit.h"
+#include "third_party/WebKit/public/web/WebRuntimeFeatures.h"
 #include "third_party/WebKit/public/web/WebWidget.h"
 #include "ui/gfx/frame_time.h"
 #include "ui/gfx/hud_font.h"
@@ -196,7 +197,12 @@ void RenderWidgetCompositor::Initialize() {
       !compositor_deps_->IsElasticOverscrollEnabled();
   settings.accelerated_animation_enabled =
       !cmd->HasSwitch(cc::switches::kDisableThreadedAnimation);
-  settings.use_display_lists = cmd->HasSwitch(switches::kEnableSlimmingPaint);
+  if (cmd->HasSwitch(switches::kEnableSlimmingPaint)) {
+    settings.use_display_lists = true;
+    blink::WebRuntimeFeatures::enableSlimmingPaint(true);
+    settings.record_full_layer =
+        !blink::WebRuntimeFeatures::slimmingPaintDisplayItemCacheEnabled();
+  }
 
   settings.default_tile_size = CalculateDefaultTileSize();
   if (cmd->HasSwitch(switches::kDefaultTileWidth)) {
