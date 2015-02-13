8611e2d387019123bb9f9eaa277b6d75b307ce39
tkent <tkent@chromium.org>
2015-02-12 18:32:56 -0800

    Revert of Add test_runner hook to dump drag image. (patchset #3 id:40001 of https://codereview.chromium.org/904833004/)
    Reason for revert:
    Caused pixel test flakiness.
    
    Original issue's description:
    > Add test_runner hook to dump drag image.
    >
    > This is used by:
    > https://codereview.chromium.org/886323005/
    >
    > BUG=451759
    >
    > Committed: https://crrev.com/3726d659908604350bc03ef02d1ccc5cdc097dd5
    > Cr-Commit-Position: refs/heads/master@{#315915}
    
    TBR=junov@chromium.org,jackhou@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=451759
    
    Review URL: https://codereview.chromium.org/925743002
    
    Cr-Commit-Position: refs/heads/master@{#316136}

--- content/shell/renderer/test_runner/web_test_proxy.h
+++ content/shell/renderer/test_runner/web_test_proxy.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/memory/scoped_ptr.h"
 #include "content/shell/renderer/test_runner/web_task.h"
-#include "third_party/WebKit/public/platform/WebImage.h"
 #include "third_party/WebKit/public/platform/WebRect.h"
 #include "third_party/WebKit/public/platform/WebScreenInfo.h"
 #include "third_party/WebKit/public/platform/WebURLError.h"
@@ -46,6 +45,7 @@ class WebDataSource;
 class WebDragData;
 class WebFileChooserCompletion;
 class WebFrame;
+class WebImage;
 class WebLocalFrame;
 class WebMIDIAccessor;
 class WebMIDIAccessorClient;
@@ -254,8 +254,6 @@ class WebTestProxyBase {
   blink::WebWidget* web_widget_;
 
   WebTaskList task_list_;
-
-  blink::WebImage drag_image_;
 
   scoped_ptr<SpellCheckClient> spellcheck_;
   scoped_ptr<MockWebUserMediaClient> user_media_client_;
