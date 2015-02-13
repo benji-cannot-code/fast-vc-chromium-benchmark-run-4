e7a50f44c18a94780f7f89389e8f8abd73ae0063
dmazzoni <dmazzoni@chromium.org>
2015-02-13 13:33:12 -0800

    Update BrowserAccessibilityManager to support Lollipop SDK.
    Renames KitKatBrowserAccessibilityManager to LollipopBrowserAccessibilityManager
    because we were never able to use the KitKat variant due to KitKat framework
    bugs.
    
    Adds a new method specifically for adding actions to AccessibilityNodeInfo,
    so we can use the new API in Lollipop, and suppresses the deprecation warnings
    in the non-Lollipop implementation.
    
    This gets rid of all of the remaining deprecation warnings in content/.
    
    BUG=405174
    
    Review URL: https://codereview.chromium.org/925923003
    
    Cr-Commit-Position: refs/heads/master@{#316293}

--- content/browser/accessibility/browser_accessibility_manager_android.cc
+++ content/browser/accessibility/browser_accessibility_manager_android.cc
@@ -254,12 +254,9 @@ jboolean BrowserAccessibilityManagerAndroid::PopulateAccessibilityNodeInfo(
   Java_BrowserAccessibilityManager_setAccessibilityNodeInfoBooleanAttributes(
       env, obj, info,
       id,
-      node->CanScrollForward(),
-      node->CanScrollBackward(),
       node->IsCheckable(),
       node->IsChecked(),
       node->IsClickable(),
-      node->IsEditableText(),
       node->IsEnabled(),
       node->IsFocusable(),
       node->IsFocused(),
@@ -267,6 +264,16 @@ jboolean BrowserAccessibilityManagerAndroid::PopulateAccessibilityNodeInfo(
       node->IsScrollable(),
       node->IsSelected(),
       node->IsVisibleToUser());
+  Java_BrowserAccessibilityManager_addAccessibilityNodeInfoActions(
+      env, obj, info,
+      id,
+      node->CanScrollForward(),
+      node->CanScrollBackward(),
+      node->IsClickable(),
+      node->IsEditableText(),
+      node->IsEnabled(),
+      node->IsFocusable(),
+      node->IsFocused());
   Java_BrowserAccessibilityManager_setAccessibilityNodeInfoClassName(
       env, obj, info,
       base::android::ConvertUTF8ToJavaString(env, node->GetClassName()).obj());
@@ -293,8 +300,7 @@ jboolean BrowserAccessibilityManagerAndroid::PopulateAccessibilityNodeInfo(
       absolute_rect.width(), absolute_rect.height(),
       is_root);
 
-  // New KitKat APIs
-  Java_BrowserAccessibilityManager_setAccessibilityNodeInfoKitKatAttributes(
+  Java_BrowserAccessibilityManager_setAccessibilityNodeInfoLollipopAttributes(
       env, obj, info,
       node->CanOpenPopup(),
       node->IsContentInvalid(),
@@ -393,8 +399,7 @@ jboolean BrowserAccessibilityManagerAndroid::PopulateAccessibilityEvent(
       break;
   }
 
-  // Backwards-compatible fallback for new KitKat APIs.
-  Java_BrowserAccessibilityManager_setAccessibilityEventKitKatAttributes(
+  Java_BrowserAccessibilityManager_setAccessibilityEventLollipopAttributes(
       env, obj, event,
       node->CanOpenPopup(),
       node->IsContentInvalid(),
