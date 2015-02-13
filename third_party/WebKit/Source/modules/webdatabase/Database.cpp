f6d0408a7bd861ece4541fb5762d390f2b0e16ca
landell@opera.com <landell@opera.com>
2015-02-13 03:28:41 +0000

    Add missing include file to Database.cpp
    Review URL: https://codereview.chromium.org/918103003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@190099 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/webdatabase/Database.cpp
+++ third_party/WebKit/Source/modules/webdatabase/Database.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "modules/webdatabase/Database.h"
 
 #include "core/dom/ExceptionCode.h"
+#include "core/dom/ExecutionContext.h"
 #include "core/dom/ExecutionContextTask.h"
 #include "core/html/VoidCallback.h"
 #include "core/inspector/ConsoleMessage.h"
