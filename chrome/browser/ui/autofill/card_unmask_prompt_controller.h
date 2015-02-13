a2a5cb974902ab2acab5e92235d6bd200fad91a2
estade <estade@chromium.org>
2015-02-13 13:45:26 -0800

    Add checkbox to card unmasking prompt
    - Add pref to make checkbox sticky
    - Implement checkbox as per-card, not per-device
    - Implement UI on Views
    
    BUG=448587
    
    Review URL: https://codereview.chromium.org/910043002
    
    Cr-Commit-Position: refs/heads/master@{#316298}

--- chrome/browser/ui/autofill/card_unmask_prompt_controller.h
+++ chrome/browser/ui/autofill/card_unmask_prompt_controller.h
@@ -20,7 +20,8 @@ class CardUnmaskPromptController {
   virtual void OnUnmaskDialogClosed() = 0;
   virtual void OnUnmaskResponse(const base::string16& cvc,
                                 const base::string16& exp_month,
-                                const base::string16& exp_year) = 0;
+                                const base::string16& exp_year,
+                                bool should_store_pan) = 0;
 
   // State.
   virtual content::WebContents* GetWebContents() = 0;
@@ -28,6 +29,7 @@ class CardUnmaskPromptController {
   virtual base::string16 GetInstructionsMessage() const = 0;
   virtual int GetCvcImageRid() const = 0;
   virtual bool ShouldRequestExpirationDate() const = 0;
+  virtual bool GetStoreLocallyStartState() const = 0;
   virtual bool InputTextIsValid(const base::string16& input_text) const = 0;
 };
 
--- components/autofill/core/browser/card_unmask_delegate.cc
+++ components/autofill/core/browser/card_unmask_delegate.cc
@@ -7,7 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 
-CardUnmaskDelegate::UnmaskResponse::UnmaskResponse() {}
+CardUnmaskDelegate::UnmaskResponse::UnmaskResponse()
+    : should_store_pan(false) {}
 CardUnmaskDelegate::UnmaskResponse::~UnmaskResponse() {}
 
 }  // namespace autofill
