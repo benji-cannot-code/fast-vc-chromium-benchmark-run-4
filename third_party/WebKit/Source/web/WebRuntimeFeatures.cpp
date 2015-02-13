ff607ba7d5ac427cb54cae28540ff86c89d57d05
peter@chromium.org <peter@chromium.org>
2015-02-13 15:16:00 +0000

    Promote Web Notifications and the Push API to stable features.
    Both APIs have received three LGTMs in their Intent to Ship threads, so
    update the runtime feature statuses accordingly.
    
    Web Notifications for Service Workers:
        https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/Cr7_GRSd2zM
    
    Push API:
        https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/UdGlL9PtBLo
    
    Because we need to disable support for older Android versions and for
    the Android WebView, this is part of a two-sided patch.
    
      [1] This patch.
      [2] https://codereview.chromium.org/914373003
    
    BUG=432527, 350378, 90795
    
    Review URL: https://codereview.chromium.org/919033003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@190149 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebRuntimeFeatures.cpp
+++ third_party/WebKit/Source/web/WebRuntimeFeatures.cpp
@@ -319,6 +319,11 @@ void WebRuntimeFeatures::enableReducedReferrerGranularity(bool enable)
     RuntimeEnabledFeatures::setReducedReferrerGranularityEnabled(enable);
 }
 
+void WebRuntimeFeatures::enablePushMessaging(bool enable)
+{
+    RuntimeEnabledFeatures::setPushMessagingEnabled(enable);
+}
+
 void WebRuntimeFeatures::enablePushMessagingData(bool enable)
 {
     RuntimeEnabledFeatures::setPushMessagingDataEnabled(enable);
