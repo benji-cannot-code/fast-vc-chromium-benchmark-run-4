bde037b7cefd6e930afc17e16060c0c3d91df544
bruthig <bruthig@chromium.org>
2015-02-13 07:13:52 -0800

    Implemented swipe to close in overview mode.
    BUG=393668
    
    Review URL: https://codereview.chromium.org/690103008
    
    Cr-Commit-Position: refs/heads/master@{#316217}

--- ash/wm/overview/window_selector.cc
+++ ash/wm/overview/window_selector.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/overview/window_selector.h"
 
 #include <algorithm>
+#include <functional>
 #include <set>
 #include <vector>
 
--- ash/wm/overview/window_selector_controller.cc
+++ ash/wm/overview/window_selector_controller.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "ash/ash_switches.h"
 #include "ash/metrics/user_metrics_recorder.h"
 #include "ash/root_window_controller.h"
 #include "ash/session/session_state_delegate.h"
@@ -16,12 +17,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/overview/window_selector.h"
 #include "ash/wm/window_state.h"
 #include "ash/wm/window_util.h"
+#include "base/command_line.h"
 #include "base/metrics/histogram.h"
 #include "ui/aura/window.h"
 
 namespace ash {
 
-WindowSelectorController::WindowSelectorController() {
+WindowSelectorController::WindowSelectorController()
+    : swipe_to_close_disabled_(base::CommandLine::ForCurrentProcess()->
+        HasSwitch(switches::kAshDisableSwipeToCloseInOverviewMode)) {
 }
 
 WindowSelectorController::~WindowSelectorController() {
