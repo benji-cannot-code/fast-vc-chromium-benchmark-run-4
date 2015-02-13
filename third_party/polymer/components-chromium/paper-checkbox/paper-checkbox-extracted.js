be021de0632fe0eb90b5fdad1fb98cf62ddff91f
orenb <orenb@chromium.org>
2015-02-13 15:25:47 -0800

    Update paper-checkbox to 0.5.4.
    The latest <paper-checkbox> matches the Material Design spec. The
    MD-Settings page will make use of this button in a follow-up CL.
    
    Since paper-checkbox isn't currently used in Chromium there shouldn't
    be any side effects for this change.
    
    BUG=457513
    NOPRESUBMIT=true
    
    Review URL: https://codereview.chromium.org/913353002
    
    Cr-Commit-Position: refs/heads/master@{#316321}

--- third_party/polymer/components-chromium/paper-checkbox/paper-checkbox-extracted.js
+++ third_party/polymer/components-chromium/paper-checkbox/paper-checkbox-extracted.js
@@ -18,19 +18,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     toggles: true,
 
     checkedChanged: function() {
-      var cl = this.$.checkbox.classList;
-      cl.toggle('checked', this.checked);
-      cl.toggle('unchecked', !this.checked);
-      cl.toggle('checkmark', !this.checked);
-      cl.toggle('box', this.checked);
+      this.$.checkbox.classList.toggle('checked', this.checked);
       this.setAttribute('aria-checked', this.checked ? 'true': 'false');
+      this.$.checkmark.classList.toggle('hidden', !this.checked);
       this.fire('core-change');
     },
 
     checkboxAnimationEnd: function() {
-      var cl = this.$.checkbox.classList;
-      cl.toggle('checkmark', this.checked && !cl.contains('checkmark'));
-      cl.toggle('box', !this.checked && !cl.contains('box'));
+      var cl = this.$.checkmark.classList;
+      cl.toggle('hidden', !this.checked && cl.contains('hidden'));
     }
 
   });
