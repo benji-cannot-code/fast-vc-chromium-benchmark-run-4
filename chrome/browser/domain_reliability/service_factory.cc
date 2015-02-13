8399c3e48d06bb2d50d66eaec50fdb081c45ed5e
ttuttle <ttuttle@chromium.org>
2015-02-12 18:53:28 -0800

    Domain Reliability: Check proper prefs on Android and Chrome OS
    There are different prefs used for crash/metrics reporting on Android
    and Chrome OS; check the right ones on the right platforms.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/920923002
    
    Cr-Commit-Position: refs/heads/master@{#316140}

--- chrome/browser/domain_reliability/service_factory.cc
+++ chrome/browser/domain_reliability/service_factory.cc
@@ -15,6 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "content/public/browser/browser_context.h"
 
+#if defined(OS_CHROMEOS)
+#include "chrome/browser/chromeos/settings/cros_settings.h"
+#endif  // defined(OS_CHROMEOS)
+
 namespace domain_reliability {
 
 namespace {
@@ -29,6 +33,21 @@ bool IsDomainReliabilityMonitoringEnabled() {
   if (command_line->HasSwitch(switches::kEnableDomainReliability))
     return true;
   return base::FieldTrialList::FindFullName("DomRel-Enable") == "enable";
+}
+
+bool IsMetricsReportingEnabled() {
+#if defined(OS_CHROMEOS)
+  bool enabled;
+  chromeos::CrosSettings::Get()->GetBoolean(chromeos::kStatsReportingPref,
+                                            &enabled);
+  return enabled;
+#elif defined(OS_ANDROID)
+  return g_browser_process->local_state()->GetBoolean(
+      prefs::kCrashReportingEnabled);
+#else
+  return g_browser_process->local_state()->GetBoolean(
+      prefs::kMetricsReportingEnabled);
+#endif
 }
 
 }  // namespace
@@ -60,10 +79,8 @@ KeyedService* DomainReliabilityServiceFactory::BuildServiceInstanceFor(
   if (!IsDomainReliabilityMonitoringEnabled())
     return NULL;
 
-  if (!g_browser_process->local_state()->GetBoolean(
-          prefs::kMetricsReportingEnabled)) {
+  if (!IsMetricsReportingEnabled())
     return NULL;
-  }
 
   return DomainReliabilityService::Create(
       kDomainReliabilityUploadReporterString);
